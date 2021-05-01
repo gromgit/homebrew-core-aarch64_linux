55f57491485f77b8cfc3d5923a5d61ffc1a7cb07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-01 18:13:45 +0000

    flash: add 2.7.1 bottle.

--- Formula/flash.rb
+++ Formula/flash.rb
@@ -5,6 +5,10 @@ class Flash < Formula
   sha256 "879057fea97c791a812e5c990d4ea07effd02406d3a267a9b24285c31ea6db3f"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "921d89c4e79695dc2ca1daa343c4be623cbbe64020d1d1d277892b5145a2f627"
+  end
+
   def install
     bin.install "flash"
   end
