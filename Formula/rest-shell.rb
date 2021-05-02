c862c1e935ac8646e97e1cf9937a50a4c203c808
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-02 05:08:47 +0100

    rest-shell: remove `bottle :unneeded`
    Also:
    - add an `openjdk` dependency, since `rest-shell` looks for a Java
      runtime.
    - add a comment to specify version explicitly at version bump. The
      version is currently not parsed correctly, but we postpone fixing this
      to the version bump to avoid having to increment `version_scheme`.
    
    Closes #76443.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rest-shell.rb
+++ Formula/rest-shell.rb
@@ -2,14 +2,16 @@ class RestShell < Formula
   desc "Shell to work with Spring HATEOAS-compliant REST resources"
   homepage "https://github.com/spring-projects/rest-shell"
   url "http://download.gopivotal.com/rest-shell/1.2.1/rest-shell-1.2.1.RELEASE.tar.gz"
+  # Specify version explicitly at version bump:
+  # version "1.2.1"
   sha256 "0ecfa67d005cc0d51e7a3a26c4dacc53aa12012f0e757332a2fa40c5e780c2d6"
   license "Apache-2.0"
 
-  bottle :unneeded
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
-    bin.write_exec_script libexec/"bin/rest-shell"
+    (bin/"rest-shell").write_env_script libexec/"bin/rest-shell", Language::Java.overridable_java_home_env
   end
 
   test do
