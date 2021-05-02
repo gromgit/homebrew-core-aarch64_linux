2fc5e61c5b21e86615cf1079a7a958967de7df24
Indrajit Raychaudhuri <irc@indrajit.com>
2021-05-01 21:10:56 -0500

    zsh-completions: Install in standard completion path
    Install zsh-completions in standard zsh_completion path. This should
    also avoid unnecessary `$fpath` customization for the user.
    
    Additionally, the test has been updated to validate correctness of
    installation without any rc customization.
    
    Also, add `license` attribute.
    
    Closes #76425.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zsh-completions.rb
+++ Formula/zsh-completions.rb
@@ -3,6 +3,8 @@ class ZshCompletions < Formula
   homepage "https://github.com/zsh-users/zsh-completions"
   url "https://github.com/zsh-users/zsh-completions/archive/0.32.0.tar.gz"
   sha256 "d2d20836fb60d2e5de11b08f1a8373484dc01260d224e64c6de9eec44137fa63"
+  license "MIT-Modern-Variant"
+  revision 1
   head "https://github.com/zsh-users/zsh-completions.git"
 
   bottle do
@@ -10,20 +12,16 @@ class ZshCompletions < Formula
   end
 
   def install
-    pkgshare.install Dir["src/_*"]
-    inreplace "#{share}/zsh-completions/_ghc", "/usr/local", HOMEBREW_PREFIX
+    inreplace "src/_ghc", "/usr/local", HOMEBREW_PREFIX
+    zsh_completion.install Dir["src/*"]
   end
 
   def caveats
     <<~EOS
       To activate these completions, add the following to your .zshrc:
 
-        if type brew &>/dev/null; then
-          FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-
-          autoload -Uz compinit
-          compinit
-        fi
+        autoload -Uz compinit
+        compinit
 
       You may also need to force rebuild `zcompdump`:
 
@@ -38,10 +36,9 @@ class ZshCompletions < Formula
 
   test do
     (testpath/"test.zsh").write <<~EOS
-      fpath=(#{pkgshare} $fpath)
       autoload _ack
       which _ack
     EOS
-    assert_match(/^_ack/, shell_output("/bin/zsh test.zsh"))
+    assert_match(/^_ack/, shell_output("/bin/zsh -fd test.zsh"))
   end
 end
