537e39625ffecf9b09e132b2ea7d3fad7c574334
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-02 05:11:10 +0000

    httpstat: add 1.3.1 bottle.

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -5,6 +5,10 @@ class Httpstat < Formula
   sha256 "7bfaa0428fe806ad4a68fc2db0aedf378f2e259d53f879372835af4ef14a6d41"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "f096e9674bc79400144b98e5480ef1ee9b3d74687bab3f6132d134de961d2c86"
+  end
+
   def install
     bin.install "httpstat.py" => "httpstat"
   end
