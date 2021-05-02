21dd30fc2acc15d3c0b505494b11356c3ae6a84d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-02 02:31:19 +0000

    bzr-extmerge: add 1.0.0 bottle.

--- Formula/bzr-extmerge.rb
+++ Formula/bzr-extmerge.rb
@@ -4,6 +4,10 @@ class BzrExtmerge < Formula
   url "https://launchpad.net/bzr-extmerge/trunk/1.0.0/+download/bzr-extmerge-1.0.0.tar.gz"
   sha256 "1b86d3a54fe669db19bc2a42a09eae52e449cc3ece8234377fd213e834f69cc0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8aa67f424683414f0e2553170e9835caf54aad70e99517ad79da66a46c3ff6bd"
+  end
+
   depends_on "bazaar"
 
   def install
