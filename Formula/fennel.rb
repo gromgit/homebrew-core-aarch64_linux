3a88f08f8e6afb9b5ecc5709047fe6fae51a6080
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-02 22:08:10 +0000

    fennel: update 0.9.2 bottle.

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -6,10 +6,7 @@ class Fennel < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bf79c94551f3aff22a1a817d1309c3bced68e11fb5053e3c10cec83d78a1e37f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d6f6dcceb6eb8cbd5194a9cbb0a8aa7e945edf567fdbdf300aaff7a64ce31583"
-    sha256 cellar: :any_skip_relocation, catalina:      "b82322722a1721123bc711a537d7588a8407112baca4299cc1f21790a1619eed"
-    sha256 cellar: :any_skip_relocation, mojave:        "3aec2e7184f24ca615307c1f6cc20c9565ba303f3009ae6c5982891b0fc117c8"
+    sha256 cellar: :any_skip_relocation, all: "677b84572e580da6bda35f79b8fb8ced96356061530653c9d246f2ef15353c1e"
   end
 
   depends_on "lua"
