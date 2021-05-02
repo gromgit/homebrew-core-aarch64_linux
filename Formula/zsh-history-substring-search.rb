995da67044a0a1f4a0198b0495cf5790620a88f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-02 02:31:44 +0000

    zsh-history-substring-search: add 1.0.2 bottle.

--- Formula/zsh-history-substring-search.rb
+++ Formula/zsh-history-substring-search.rb
@@ -4,6 +4,10 @@ class ZshHistorySubstringSearch < Formula
   url "https://github.com/zsh-users/zsh-history-substring-search/archive/v1.0.2.tar.gz"
   sha256 "c1bb21490bd31273fb511b23000fb7caf49c258a79c4b8842f3e1f2ff76fd84c"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "556fffc8eceb018b5079b6e4ff3b152e7223b4f5f9416d1d210409aceabf4203"
+  end
+
   def install
     pkgshare.install "zsh-history-substring-search.zsh"
   end
