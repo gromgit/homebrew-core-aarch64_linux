cc15f15ba4d15f4a3db446ebb377e28280a7af66
rui <rui@chenrui.dev>
2021-05-02 00:12:20 +0000

    zoxide 0.7.0
    * zoxide 0.7.0
    * Update zoxide.rb
    
    Closes #76403.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -1,8 +1,8 @@
 class Zoxide < Formula
   desc "Shell extension to navigate your filesystem faster"
   homepage "https://github.com/ajeetdsouza/zoxide"
-  url "https://github.com/ajeetdsouza/zoxide/archive/v0.6.0.tar.gz"
-  sha256 "fe93aa4d1fe8d4f94b302335f30bc543c7a1664df2a3fde73e6253c74f576c35"
+  url "https://github.com/ajeetdsouza/zoxide/archive/v0.7.0.tar.gz"
+  sha256 "9a9b0aa82a647fa834e4ade483af292e50080758af25c5260c425420879d9691"
   license "MIT"
 
   bottle do
