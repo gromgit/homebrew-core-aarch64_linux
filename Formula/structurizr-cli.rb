f9f94b3f20458e7f450cd035d79fa063ad380c08
rui <rui@chenrui.dev>
2021-05-03 12:06:03 +0000

    structurizr-cli 1.10.1
    * structurizr-cli 1.10.1
    * structurizr-cli: remove `bottle :unneeded`
    
    Closes #76504.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -1,11 +1,9 @@
 class StructurizrCli < Formula
   desc "Command-line utility for Structurizr"
   homepage "https://structurizr.com"
-  url "https://github.com/structurizr/cli/releases/download/v1.10.0/structurizr-cli-1.10.0.zip"
-  sha256 "c9810e934ef9b5cf27dbb50392599b6de6451969b0232acdacc526d56d4b735d"
+  url "https://github.com/structurizr/cli/releases/download/v1.10.1/structurizr-cli-1.10.1.zip"
+  sha256 "e3dd87efc719e8085dcad5330d06894bf5b66348e2625bed9260afb320b08960"
   license "Apache-2.0"
-
-  bottle :unneeded
 
   depends_on "openjdk"
 
