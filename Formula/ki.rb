e7d0a0691bb54b93546a1b10fce8a199d1bcecbe
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-05-03 08:15:02 +0000

    ki: add 0.3.3 bottle.

--- Formula/ki.rb
+++ Formula/ki.rb
@@ -6,6 +6,13 @@ class Ki < Formula
   license "Apache-2.0"
   head "https://github.com/Kotlin/kotlin-interactive-shell.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a6343063d109cbf342ccb89419286d3e83105996d718d818be06cc41874b8e80"
+    sha256 cellar: :any_skip_relocation, big_sur:       "1c10b8affdb159a68b64768036d7a7b251cc10719b4f98b1d908d1c00a367ef3"
+    sha256 cellar: :any_skip_relocation, catalina:      "4b199ebd023f4f8f227f6e508f344bf0c53f4451717dae335e55400232044e2c"
+    sha256 cellar: :any_skip_relocation, mojave:        "54967e266b370fc3d1eab2a56325db4500a2e310fa00eb34417c23860d26eb06"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk"
 
