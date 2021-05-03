183e470fe75ef671ef0c54938f48b081bcec9cda
Rui Chen <rui@chenrui.dev>
2021-05-03 14:20:16 -0400

    cake: remove `bottle :unneeded`
    Closes #76523.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cake.rb
+++ Formula/cake.rb
@@ -1,14 +1,9 @@
 class Cake < Formula
-  # As discussed with chenrui333 in this PR: https://github.com/Homebrew/homebrew-core/pull/55500#issuecomment-636390974
-  # Cake uses the pre-release setting on all releases.  This will change
-  # once we ship version 1.0.0, which is likely going to be our next release.
   desc "Cross platform build automation system with a C# DSL"
   homepage "https://cakebuild.net/"
   url "https://github.com/cake-build/cake/releases/download/v1.1.0/Cake-bin-net461-v1.1.0.zip"
   sha256 "e556aaf3675728ce349569bd19b32492c36a9d9ad97acf3e1e007bab8f6eddfa"
   license "MIT"
-
-  bottle :unneeded
 
   depends_on "mono"
 
