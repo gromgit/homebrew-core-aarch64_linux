5a2cc1c437f3f611b38ce76a1d9df29b03f59168
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 02:25:11 +0000

    asdf: add 0.8.0_1 bottle.

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -7,6 +7,10 @@ class Asdf < Formula
   revision 1
   head "https://github.com/asdf-vm/asdf.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "24d40cb939c41de5c2ee5f7e52aa631884d56a0566cfc65a63bf3ddb42344d24"
+  end
+
   depends_on "autoconf"
   depends_on "automake"
   depends_on "coreutils"
