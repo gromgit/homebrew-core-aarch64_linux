fd96511a54edbdacbbb53ac90b2492deab121819
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-04 06:19:35 +0100

    open-mpi: remove `op-avx` from `enable-mca-no-build`
    The `op-avx` flag disables building features that take advantage of AVX
    instructions.
    
    Since `open-mpi` is able to detect CPU features at runtime, disabling
    this is not necessary, and simply leads to lower performance for users
    with newer machines.
    
    See also:
    
        https://github.com/open-mpi/ompi/issues/8306
        https://github.com/open-mpi/ompi/pull/8361
    
    Closes #76619.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/open-mpi.rb
+++ Formula/open-mpi.rb
@@ -4,6 +4,7 @@ class OpenMpi < Formula
   url "https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.bz2"
   sha256 "e24f7a778bd11a71ad0c14587a7f5b00e68a71aa5623e2157bafee3d44c07cda"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url :homepage
@@ -64,7 +65,7 @@ class OpenMpi < Formula
       --disable-dependency-tracking
       --disable-silent-rules
       --enable-ipv6
-      --enable-mca-no-build=op-avx,reachable-netlink
+      --enable-mca-no-build=reachable-netlink
       --with-libevent=#{Formula["libevent"].opt_prefix}
       --with-sge
     ]
