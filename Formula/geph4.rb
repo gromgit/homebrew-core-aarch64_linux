c6be46c13a98e7ec96b4f8998e6387b64ff44363
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 02:26:02 +0000

    geph4: update 4.4.1 bottle.

--- Formula/geph4.rb
+++ Formula/geph4.rb
@@ -6,10 +6,10 @@ class Geph4 < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8873855fd73357ffec65de348b42a3d615bfa2e4e25ed74cdce1ab82995d7cb1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b184b00aaf36882bdf57b07a660f9cba08d8fa8f47a57456e56af7bdf59f2091"
-    sha256 cellar: :any_skip_relocation, catalina:      "b16cb448b221ddd3d11ae8b064fa0da1c7c4e606bc3e2d1e728284e9704b13e7"
-    sha256 cellar: :any_skip_relocation, mojave:        "2e36010649cef8637720934939d0488cbd9a13533c2d93b880e11972d0614601"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "540f50cec4ba84adfdb667a38be3faf26f5eb290e834a5c7fe8a3d82b96612ae"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c85445268a639e9f519d9b7dca9fc58f67a2802ff8242008bf1dcb97af0ce2ad"
+    sha256 cellar: :any_skip_relocation, catalina:      "13982c2d6a62cfa11e4dd97ac57b935340a62dc0997d26103a35fae83fd0f3a9"
+    sha256 cellar: :any_skip_relocation, mojave:        "eac0932aa51e7638b82f1718f717716d573bc0dd5c15db1da8cd6ebe11015b2d"
   end
 
   depends_on "rust" => :build
