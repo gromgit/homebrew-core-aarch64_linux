7e18a0356fd44e365fb32f48afd2cdfdde070fd9
Rui Chen <rui@chenrui.dev>
2021-05-03 23:55:32 -0400

    gptfdisk 1.0.7
    * gptfdisk 1.0.7
    * remove patch
    * update build patch
    
    Closes #76593.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gptfdisk.rb
+++ Formula/gptfdisk.rb
@@ -1,8 +1,8 @@
 class Gptfdisk < Formula
   desc "Text-mode partitioning tools"
   homepage "https://www.rodsbooks.com/gdisk/"
-  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.6/gptfdisk-1.0.6.tar.gz"
-  sha256 "ddc551d643a53f0bd4440345d3ae32c49b04a797e9c01036ea460b6bb4168ca8"
+  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.7/gptfdisk-1.0.7.tar.gz"
+  sha256 "754004b7f85b279287c7ac3c0469b1d7e0eae043a97a2e587b0560ca5f3828c0"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -16,9 +16,7 @@ class Gptfdisk < Formula
 
   uses_from_macos "ncurses"
 
-  # Fix Big Sur compilation issue with 1.0.5; the physical *.dylib files
-  # are no longer present directly on the filesystem, but the linker still
-  # knows what to do.
+  # update linker path for libncurses
   patch :DATA
 
   def install
@@ -38,15 +36,15 @@ class Gptfdisk < Formula
 end
 
 __END__
-diff -ur a/Makefile.mac b/Makefile.mac
---- a/Makefile.mac	2020-02-17 22:34:11.000000000 +0000
-+++ b/Makefile.mac	2020-12-05 22:12:04.000000000 +0000
-@@ -21,7 +21,7 @@
+diff --git a/Makefile.mac b/Makefile.mac
+index ea21fa6..b50bb34 100644
+--- a/Makefile.mac
++++ b/Makefile.mac
+@@ -21,7 +21,7 @@ gdisk:	$(LIB_OBJS) gpttext.o gdisk.o
  #	$(CXX) $(LIB_OBJS) -L/usr/lib -licucore gpttext.o gdisk.o -o gdisk
  
  cgdisk: $(LIB_OBJS) cgdisk.o gptcurses.o
--	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o /usr/lib/libncurses.dylib $(LDFLAGS) $(FATBINFLAGS) -o cgdisk
-+	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o -L/usr/lib -lncurses $(LDFLAGS) $(FATBINFLAGS) -o cgdisk
+-	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o /usr/local/Cellar/ncurses/6.2/lib/libncurses.dylib $(LDFLAGS) -o cgdisk
++	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o -L/usr/lib -lncurses $(LDFLAGS) -o cgdisk
+ #	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o /usr/lib/libncurses.dylib $(LDFLAGS) -o cgdisk
  #	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o $(LDFLAGS) -licucore -lncurses -o cgdisk
- 
- sgdisk: $(LIB_OBJS) gptcl.o sgdisk.o
