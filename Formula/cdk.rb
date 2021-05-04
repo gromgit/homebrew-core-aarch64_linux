7472b7e083af59847793f0c90939b21b835bfc8d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-04 16:17:36 +0000

    cdk: update 5.0-20210324 bottle.

--- Formula/cdk.rb
+++ Formula/cdk.rb
@@ -11,10 +11,10 @@ class Cdk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7dd0481393a5fc27b44df7ee3df3f54003a6740c4d7e0a8aaee2aa7758688754"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9803485a0a9bc47cccc0f33b094118a4f90fa38273ae5f7b4d9b192f43d3c038"
-    sha256 cellar: :any_skip_relocation, catalina:      "6bf69e8c6bcd620b0819d36279685682309dfed28a6e6fef450d1303ca3f28ce"
-    sha256 cellar: :any_skip_relocation, mojave:        "c42a077663368d0a0ee0dfea749a4981d3ed093bdd86bafc50204999b9e0c756"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "418408498d8bac3d93334a17e443bf3e184d9d75f4f5004b5f061ae2414d7076"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ef4d022fd2b85806ca3f5d39974ebfec3a15840adc2ec8afac23fe3591d24626"
+    sha256 cellar: :any_skip_relocation, catalina:      "da1e033bf75a24554fd51c1c255434d8b25759ec728f9371b9fe65651ef65db7"
+    sha256 cellar: :any_skip_relocation, mojave:        "ae5301085365b01984a54f22ed421ed6c249afb2d7ba8426aed2d4c54111fbe5"
   end
 
   uses_from_macos "ncurses"
