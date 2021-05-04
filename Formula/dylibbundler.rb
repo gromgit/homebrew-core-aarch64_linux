623a450f6c6985e63822f6dc920cd8ddccb2e669
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 12:07:40 +0000

    dylibbundler: update 1.0.0 bottle.

--- Formula/dylibbundler.rb
+++ Formula/dylibbundler.rb
@@ -7,12 +7,10 @@ class Dylibbundler < Formula
   head "https://github.com/auriamg/macdylibbundler.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "475b571e44abd0e57c924e004b94dba1fd1600b11fb6e7094afdab42406865d8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2c4d2e3d71590903992d5735f32189c532533dffb3d1874f08932afe33c0e5b8"
-    sha256 cellar: :any_skip_relocation, catalina:      "0794eea61309318e5aa8686a5781cbd5c534b1f9b481d38502a7343007cfe77e"
-    sha256 cellar: :any_skip_relocation, mojave:        "f2554553b0c00165394e41ade50712f490331d7bf084792abc2cb4f12ae1164e"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "60b4e47bfbb3450f6901e6c104d37530940e9cc22abacaacbe37eb4539b820c6"
-    sha256 cellar: :any_skip_relocation, sierra:        "c8f470a6e3c0c5eaf632dd384f5098f0e59f60ab2c873482424f7c6729a4fe07"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d7560a5028b5bb8300dcf33492c54f7fc148c9cd7af0ae58aac40184cb7d28de"
+    sha256 cellar: :any_skip_relocation, big_sur:       "62008e896c348f9714b20c04696595d0e858dd56457845de973c8089b83bde66"
+    sha256 cellar: :any_skip_relocation, catalina:      "ca4b42c902bd1ac60982c99415bb32e7faf3a7be6ef2f40c6961c2e6828daeab"
+    sha256 cellar: :any_skip_relocation, mojave:        "401e1ed1a81e08b88c5c3515677b8b5acbaa11c8c5b9f5ea854a3e8aaa3a4a33"
   end
 
   def install
