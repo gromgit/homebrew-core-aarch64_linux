89e3b9d9156b0db07523413097322e439cb7d9ef
Rui Chen <rui@chenrui.dev>
2021-05-03 23:14:26 -0400

    collector-sidecar 1.1.0
    * collector-sidecar 1.1.0
    * update homepage url
    * update build
    
    Closes #76578.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/collector-sidecar.rb
+++ Formula/collector-sidecar.rb
@@ -1,8 +1,9 @@
 class CollectorSidecar < Formula
   desc "Manage log collectors through Graylog"
-  homepage "https://github.com/Graylog2/collector-sidecar"
-  url "https://github.com/Graylog2/collector-sidecar/archive/1.0.2.tar.gz"
-  sha256 "ee7ddb725d3475656df0bb08476e64c7f919acfc011a338b4532249363778130"
+  homepage "https://www.graylog.org/"
+  url "https://github.com/Graylog2/collector-sidecar.git",
+    tag:      "1.1.0",
+    revision: "89c722567033ea48b42678d2303693aa6ddee775"
   license "GPL-3.0"
 
   bottle do
@@ -14,32 +15,19 @@ class CollectorSidecar < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "6e09f805d30b96d2650a6541fddbda8a55d6ef74d7de7e96c642df5d2cd7d18b"
   end
 
-  depends_on "glide" => :build
   depends_on "go" => :build
   depends_on "mercurial" => :build
   depends_on "filebeat"
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "auto"
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-    (buildpath/"src/github.com/Graylog2/collector-sidecar").install buildpath.children
+    ldflags = %W[
+      -s -w
+      -X github.com/Graylog2/collector-sidecar/common.GitRevision=#{Utils.git_head}
+      -X github.com/Graylog2/collector-sidecar/common.CollectorVersion=#{version}
+    ]
 
-    cd "src/github.com/Graylog2/collector-sidecar" do
-      inreplace "main.go", "/etc", etc
-
-      inreplace "sidecar-example.yml" do |s|
-        s.gsub! "/usr", HOMEBREW_PREFIX
-        s.gsub! "/etc", etc
-        s.gsub! "/var", var
-      end
-
-      system "glide", "install"
-      system "make", "build"
-      (etc/"graylog/sidecar/sidecar.yml").install "sidecar-example.yml"
-      bin.install "graylog-sidecar"
-      prefix.install_metafiles
-    end
+    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "-o", bin/"graylog-sidecar"
+    (etc/"graylog/sidecar/sidecar.yml").install "sidecar-example.yml"
   end
 
   plist_options manual: "graylog-sidecar"
