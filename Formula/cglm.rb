5c91c4e2b8b0a17a368a7413e927d9bb9e150803
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-04 16:17:20 +0000

    cglm: update 0.8.2 bottle.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -6,10 +6,10 @@ class Cglm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "55f7f2b59e6aa0c4461dffb74b2e058649d33785371a3f46352c8c332ce49668"
-    sha256 cellar: :any, big_sur:       "573a6e6574facd161228dc7eaecee9d18ddf8451748a1913854b1c9fd2d10fe9"
-    sha256 cellar: :any, catalina:      "fee2fc016362262fd139e6ccfb7fcff3814e03b8c5d1d117ac5d149885b1101d"
-    sha256 cellar: :any, mojave:        "14e8e9b0e99b29115100a96ef2cbf2539c6a14bfd4d4608eb91b02538603ab61"
+    sha256 cellar: :any, arm64_big_sur: "d71507b61912470913e98d1720b385162b4f0fd0cdc8d895e939d809e00fa636"
+    sha256 cellar: :any, big_sur:       "ed08a26dddf43f709d962c8f691c4bbee9df9aae21bfb97faecfcc4b05d2be72"
+    sha256 cellar: :any, catalina:      "6a50abbcea2f44d990401a51426f4cb7e30d8306fd000322f987561ca1f515fd"
+    sha256 cellar: :any, mojave:        "20de453a0e9fce13370416237d809023872c6e3aefcd2689a4a381c2e3cf1c91"
   end
 
   depends_on "autoconf" => :build
