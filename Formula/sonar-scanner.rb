0934537f96f06410f8223a825e9be9fbedd604d4
esrodrigbr1 <bruno.rodriguez@es.nestle.com>
2021-05-04 11:03:41 +0200

    sonar-scanner 4.6.1.2450
    * sonar-scanner 4.6.1.2450
    * sonar-scanner: fix build
      Also remove `bottle :unneeded`.
    
    Closes #76626.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sonar-scanner.rb
+++ Formula/sonar-scanner.rb
@@ -1,12 +1,12 @@
 class SonarScanner < Formula
   desc "Launcher to analyze a project with SonarQube"
   homepage "https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner"
-  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.5.0.2216.zip"
-  sha256 "a271a933d14da6e8705d58996d30afd0b4afc93c0bfe957eb377bed808c4fa89"
+  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.1.2450.zip"
+  sha256 "8a9781fdc75f26d7800c904ad839e2d4d29a24714c597867215f4ae820bc0fa3"
   license "LGPL-3.0-or-later"
   head "https://github.com/SonarSource/sonar-scanner-cli.git"
 
-  bottle :unneeded
+  depends_on "openjdk"
 
   def install
     rm_rf Dir["bin/*.bat"]
@@ -14,7 +14,7 @@ class SonarScanner < Formula
     bin.install libexec/"bin/sonar-scanner"
     etc.install libexec/"conf/sonar-scanner.properties"
     ln_s etc/"sonar-scanner.properties", libexec/"conf/sonar-scanner.properties"
-    bin.env_script_all_files libexec/"bin/", SONAR_SCANNER_HOME: libexec
+    bin.env_script_all_files libexec/"bin/", SONAR_SCANNER_HOME: libexec, JAVA_HOME: Formula["openjdk"].opt_prefix
   end
 
   test do
