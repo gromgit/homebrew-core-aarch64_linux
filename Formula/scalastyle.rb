f7ba61745c02f648aa8e017c84926ed7fa83d99e
Sam Ford <1584702+samford@users.noreply.github.com>
2021-05-05 09:03:30 -0400

    scalastyle: add livecheck

--- Formula/scalastyle.rb
+++ Formula/scalastyle.rb
@@ -4,6 +4,13 @@ class Scalastyle < Formula
   url "https://oss.sonatype.org/content/repositories/releases/org/scalastyle/scalastyle_2.12/1.0.0/scalastyle_2.12-1.0.0-batch.jar"
   sha256 "e9dafd97be0d00f28c1e8bfcab951d0e5172b262a1d41da31d1fd65d615aedcb"
 
+  # In a filename like `scalastyle_2.12-1.0.0-batch.jar`, the first version is
+  # the Scala version (2.12) and the second is the Scalastyle version (1.0.0).
+  livecheck do
+    url :homepage
+    regex(/href=.*?scalastyle[._-]v?\d+(?:\.\d+)+-(\d+(?:\.\d+)+)-batch\.jar/i)
+  end
+
   bottle :unneeded
 
   resource "default_config" do
