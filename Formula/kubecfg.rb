8c9c5bdbd4a0a9cd6420fda6aa394039af8f8681
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-05 15:07:49 +0000

    kubecfg: update 0.19.0 bottle.

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -6,10 +6,10 @@ class Kubecfg < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "261b8676357b1492c50ed21b62bce7ba57eb845b634e8c0c296e66c7ff73e2bc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "437a63b11e2781d08865dca5f78ea1eabab2b514b1c715eb206a4a0e9b418bd7"
-    sha256 cellar: :any_skip_relocation, catalina:      "fdfcc51faa2ddaad019820b05cc23df77990358d2ff51a7407a8929cee001b04"
-    sha256 cellar: :any_skip_relocation, mojave:        "f9f677e3d7904f705e34ef49ad30874b0a278ed6c7d88b134849bcd7840c7a98"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e11a13820f37cc769e705894e7e11fd2a495b6092075df5daa0fed1f6e2794d4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "eac0db5a3b40af567c0cf1032fdb2f9347d6476f95563600865cbf1a4fe789cb"
+    sha256 cellar: :any_skip_relocation, catalina:      "cde39f5351689bf133491b63454dae9cc9658a27d2df2ee807e519bddce43ef7"
+    sha256 cellar: :any_skip_relocation, mojave:        "608b542658fc450ee26c47f1ff3debe8f7e70b71c73ce430c5730ec71cb72642"
   end
 
   depends_on "go" => :build
