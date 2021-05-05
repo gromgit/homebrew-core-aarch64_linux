523545ebd6fc04611109aa8c3415b28ae54014e0
EccentricHyphus37 <69630953+EccentricHyphus37@users.noreply.github.com>
2021-05-05 17:32:59 +0530

    snappy 1.1.9

--- Formula/snappy.rb
+++ Formula/snappy.rb
@@ -1,8 +1,8 @@
 class Snappy < Formula
   desc "Compression/decompression library aiming for high speed"
   homepage "https://google.github.io/snappy/"
-  url "https://github.com/google/snappy/archive/1.1.8.tar.gz"
-  sha256 "16b677f07832a612b0836178db7f374e414f94657c138e6993cbfc5dcc58651f"
+  url "https://github.com/google/snappy/archive/1.1.9.tar.gz"
+  sha256 "75c1fbb3d618dd3a0483bff0e26d0a92b495bbe5059c8b4f1c962b478b6e06e7"
   license "BSD-3-Clause"
   head "https://github.com/google/snappy.git"
 
@@ -17,15 +17,43 @@ class Snappy < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
+  # Fix issue where Mojave clang fails due to entering a __GNUC__ block
+  on_macos do
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1100
+  end
+
+  fails_with :clang do
+    build 1100
+    cause "error: invalid output constraint '=@ccz' in asm"
+  end
+
+  # Fix issue where `snappy` setting -fno-rtti causes build issues on `folly`
+  # `folly` issue ref: https://github.com/facebook/folly/issues/1583
+  patch :DATA
+
   def install
-    system "cmake", ".", *std_cmake_args
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+    on_macos do
+      ENV.llvm_clang if DevelopmentTools.clang_build_version <= 1100
+    end
+
+    # Disable tests/benchmarks used for Snappy development
+    args = std_cmake_args + %w[
+      -DSNAPPY_BUILD_TESTS=OFF
+      -DSNAPPY_BUILD_BENCHMARKS=OFF
+    ]
+
+    system "cmake", ".", *args
     system "make", "install"
     system "make", "clean"
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *args
     system "make", "install"
   end
 
   test do
+    # Force use of Clang on Mojave
+    on_macos { ENV.clang }
+
     (testpath/"test.cpp").write <<~EOS
       #include <assert.h>
       #include <snappy.h>
@@ -48,3 +76,20 @@ class Snappy < Formula
     system "./test"
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 672561e..2f97b73 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -76,10 +76,6 @@ else(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+   # Disable C++ exceptions.
+   string(REGEX REPLACE "-fexceptions" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions")
+-
+-  # Disable RTTI.
+-  string(REGEX REPLACE "-frtti" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti")
+ endif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+ 
+ # BUILD_SHARED_LIBS is a standard CMake variable, but we declare it here to make
