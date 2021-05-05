a84689c7557c96dcf648fed27031cae6d2d3aa8f
Vlad Ilyushchenko <bluestreak@gmail.com>
2021-05-05 13:57:37 +0100

    questdb 6.0.0
    build: release 6.0.0
    
    ```
    wget https://github.com/questdb/questdb/releases/download/6.0.0/questdb-6.0.0-no-jre-bin.tar.gz
    shasum -a 256 questdb-6.0.0-no-jre-bin.tar.gz
    08e52a4b5220bdfcd10d47f7d3a276f2070180dd0fd743dbaf10b98922f02e33  questdb-6.0.0-no-jre-bin.tar.gz
    ```
    
    Closes #76971.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/questdb.rb
+++ Formula/questdb.rb
@@ -1,8 +1,8 @@
 class Questdb < Formula
   desc "Time Series Database"
   homepage "https://questdb.io"
-  url "https://github.com/questdb/questdb/releases/download/5.0.6.1/questdb-5.0.6.1-no-jre-bin.tar.gz"
-  sha256 "0c6f1ebc33305d40463b09c05c20cc22cb3a2f6e4f054a10513f434a5faebf6e"
+  url "https://github.com/questdb/questdb/releases/download/6.0.0/questdb-6.0.0-no-jre-bin.tar.gz"
+  sha256 "08e52a4b5220bdfcd10d47f7d3a276f2070180dd0fd743dbaf10b98922f02e33"
   license "Apache-2.0"
 
   bottle :unneeded
