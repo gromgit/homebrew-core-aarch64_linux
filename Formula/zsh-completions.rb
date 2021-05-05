511203fc28ef7be7f3929234fcdb2c1cd52fd244
Caleb Xu <calebcenter@live.com>
2021-05-05 17:53:56 -0400

    zsh-completions 0.33.0
    Closes #76722.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zsh-completions.rb
+++ Formula/zsh-completions.rb
@@ -1,10 +1,9 @@
 class ZshCompletions < Formula
   desc "Additional completion definitions for zsh"
   homepage "https://github.com/zsh-users/zsh-completions"
-  url "https://github.com/zsh-users/zsh-completions/archive/0.32.0.tar.gz"
-  sha256 "d2d20836fb60d2e5de11b08f1a8373484dc01260d224e64c6de9eec44137fa63"
+  url "https://github.com/zsh-users/zsh-completions/archive/0.33.0.tar.gz"
+  sha256 "39452d383d0718aa2c830edba1aa32f0ee1e40002ef6932d88699a888bd58c29"
   license "MIT-Modern-Variant"
-  revision 1
   head "https://github.com/zsh-users/zsh-completions.git"
 
   bottle do
