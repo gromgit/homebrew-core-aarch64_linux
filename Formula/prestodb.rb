556ee11b609882c72672cf878095fa07078edcfd
rui <rui@chenrui.dev>
2021-05-05 00:36:35 +0000

    prestodb 0.252
    * prestodb 0.252
    * prestodb: update resource
      Also remove `bottle :unneeded`.
    
    Closes #76663.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prestodb.rb
+++ Formula/prestodb.rb
@@ -1,8 +1,8 @@
 class Prestodb < Formula
   desc "Distributed SQL query engine for big data"
   homepage "https://prestodb.io"
-  url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-server/0.251/presto-server-0.251.tar.gz"
-  sha256 "aeff229a351aea145f25373dffd919d75ce349bf5189c88b9347c47c2201b1b0"
+  url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-server/0.252/presto-server-0.252.tar.gz"
+  sha256 "42035069b182e8d6effc7f439082072368062bfd2e71834d2f775d2fb4fb72f7"
   license "Apache-2.0"
 
   # The source of the Presto download page at https://prestodb.io/download.html
@@ -16,15 +16,13 @@ class Prestodb < Formula
     regex(/latest_presto_version.*?(\d+(?:\.\d+)+)/i)
   end
 
-  bottle :unneeded
-
   depends_on "openjdk"
 
   conflicts_with "prestosql", because: "both install `presto` and `presto-server` binaries"
 
   resource "presto-cli" do
-    url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-cli/0.251/presto-cli-0.251-executable.jar"
-    sha256 "2c868c778efdfe1a120f24b1f8dfa99b5f1013421bd49696fea6d4ab65b4fcb6"
+    url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-cli/0.252/presto-cli-0.252-executable.jar"
+    sha256 "a7fca41bf1adc6f6717ac27ef83f988447d2671ba41967ca852770c46a5ca084"
   end
 
   def install
