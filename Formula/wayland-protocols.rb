001cb8909f42658e7dc590917913ee8696c9061b
Caleb Xu <calebcenter@live.com>
2021-05-05 18:33:42 -0400

    wayland-protocols 1.21 (#76721)

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -1,8 +1,8 @@
 class WaylandProtocols < Formula
   desc "Additional Wayland protocols"
   homepage "https://wayland.freedesktop.org"
-  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.20.tar.xz"
-  sha256 "9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7"
+  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.21.tar.xz"
+  sha256 "b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5"
   license "MIT"
 
   depends_on "pkg-config" => [:build, :test]
