2f5175f243a4f79c33eb23dc3766989c04d1e737
rui <rui@chenrui.dev>
2021-05-05 08:34:18 +0000

    syncthing 1.16.1
    Closes #76680.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/syncthing.rb
+++ Formula/syncthing.rb
@@ -1,8 +1,8 @@
 class Syncthing < Formula
   desc "Open source continuous file synchronization application"
   homepage "https://syncthing.net/"
-  url "https://github.com/syncthing/syncthing/archive/v1.16.0.tar.gz"
-  sha256 "1245efe448aa736c209aecfe8abb21677f25887b15c72fb0f352281ef74cb5e3"
+  url "https://github.com/syncthing/syncthing/archive/v1.16.1.tar.gz"
+  sha256 "9994380a94b298e8266fdd40238bcff3ade2c21752b9cff98ec4218fe84731ad"
   license "MPL-2.0"
   head "https://github.com/syncthing/syncthing.git", branch: "main"
 
