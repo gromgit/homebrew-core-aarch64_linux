35cafd9bc794515fd769d249804e2b6c1664ae8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-06 05:08:20 +0000

    gistit: update 0.1.4 bottle.

--- Formula/gistit.rb
+++ Formula/gistit.rb
@@ -7,14 +7,10 @@ class Gistit < Formula
   head "https://github.com/jrbasso/gistit.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b84985ef5d5eb68a1260a2dea107c6ff4ec29ce42bb1f33321f5074cc088c457"
-    sha256 cellar: :any, big_sur:       "041276422d7633ae3b0ae151939e41619387935a7b5afdcae919aeb6dd4927f1"
-    sha256 cellar: :any, catalina:      "8a5288aec9e18273db915b7c2ac077157549f1b496a0fd98e878d34ec9be66fa"
-    sha256 cellar: :any, mojave:        "56cf73e5a5c9742640a146b1b37ed2aea557b356b2e5a96ff437130a6aab4dfb"
-    sha256 cellar: :any, high_sierra:   "decb56c455eb39e379b94b6832281ea06ae8a42c745eb6c108c15883a0ef2fad"
-    sha256 cellar: :any, sierra:        "269b7429070e11980d6764f2f6bd1d870d2e391cfd919f948159cb35cfab1184"
-    sha256 cellar: :any, el_capitan:    "b968a2885e3ac3e0c717fdabb9986149a39d58d031476d4ef3ada7a9e1ad07e9"
-    sha256 cellar: :any, yosemite:      "052536cb990d1c5ea4b48612026dfb13addd03cbc06ed8c6b42f3636eb6771a7"
+    sha256 cellar: :any, arm64_big_sur: "ad2652284c1907697535715d31eec9dfb558fe123b8cfe6aabf76ef0bd858cc7"
+    sha256 cellar: :any, big_sur:       "090920bf2761a37d9b9877386a1c0b4466ba80a8c412e807a7a03de14239a3a0"
+    sha256 cellar: :any, catalina:      "844955e49de622786a9a676e91b767926ff9953c950db2affa98f6d82978899f"
+    sha256 cellar: :any, mojave:        "c55986f583c7d8744c4009f7856d00568ee5c3a31836075dd8b44af7b9807284"
   end
 
   depends_on "autoconf" => :build
