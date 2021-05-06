943f9746c22227c5b4c40a58363d5839d623a4cd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-06 13:11:06 +0100

    opencoarrays: revision bump (gcc 11.1.0)

--- Formula/opencoarrays.rb
+++ Formula/opencoarrays.rb
@@ -4,6 +4,7 @@ class Opencoarrays < Formula
   url "https://github.com/sourceryinstitute/OpenCoarrays/releases/download/2.9.2/OpenCoarrays-2.9.2.tar.gz"
   sha256 "6c200ca49808c75b0a2dfa984304643613b6bc77cc0044bee093f9afe03698f7"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/sourceryinstitute/opencoarrays.git"
 
   bottle do
