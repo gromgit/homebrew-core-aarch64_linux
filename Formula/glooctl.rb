b358d4c6857811f638ab6548979750ad7f3ffb2c
rui <rui@chenrui.dev>
2021-05-06 18:39:18 +0000

    glooctl 1.7.4
    Closes #76790.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -2,8 +2,8 @@ class Glooctl < Formula
   desc "Envoy-Powered API Gateway"
   homepage "https://docs.solo.io/gloo/latest/"
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.7.3",
-      revision: "852b1c95da17a81f5e8cd95722dd5c761f3da035"
+      tag:      "v1.7.4",
+      revision: "246a39b206446aa2de4f131ac886fd861bf870f8"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git"
 
