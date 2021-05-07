80abb30e86eb002c469398a01934e4e3f707dd53
rui <rui@chenrui.dev>
2021-05-07 12:46:28 +0000

    s2n 1.0.7
    Closes #76832.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "a2d09943b8c99bbfa4a4352766fac1f81429cf5bdd63e6eeebaac1a77dc2b8a6"
+  url "https://github.com/aws/s2n-tls/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "6978d731425d92f4891c7849549a29e0ec002fb76809b1c9da40dd265067fe3d"
   license "Apache-2.0"
 
   livecheck do
