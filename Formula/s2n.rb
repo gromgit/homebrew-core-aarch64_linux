60021cdad169acf498d96e40a5c6309a50da5873
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-07 18:12:04 +0000

    s2n: update 1.0.7 bottle.

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -11,10 +11,10 @@ class S2n < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f9e492912aa8e024e45d557d9c442b16d75d1bb3c3c212b763ac2376be5f38da"
-    sha256 cellar: :any, big_sur:       "c93f7a594b282245129acd15a1eed093c1f6072bf393da0be266d5b115bd77ce"
-    sha256 cellar: :any, catalina:      "ebfa1265ec9fb37ceeb78d4fbc5174c268b2cf3e9b71929ee7e4da32a0a89fb8"
-    sha256 cellar: :any, mojave:        "cda50594ffd340359e91b926b5be8fb6355cbc1d95364cb4de4f03fc97a26224"
+    sha256 cellar: :any, arm64_big_sur: "00f6e95e5bafa03dce8d56ea32ca4ed7b7766293e48dba09152447dee3e149ce"
+    sha256 cellar: :any, big_sur:       "d0b377e22e337e74aade64f7869ab5772602d748752b51672c8023d7cbe9f729"
+    sha256 cellar: :any, catalina:      "79762e208b9a209e18f3b726f6c65afe2805e7dd287cee47eb26b46f1f716fc1"
+    sha256 cellar: :any, mojave:        "6d54a09202b68182cdfd85ba2a2abf366bb5f84b1123f4043e469d8fb9c705f9"
   end
 
   depends_on "cmake" => :build
