d05b062d9c535d0ac9ce0987f478199e78590339
rui <rui@chenrui.dev>
2021-05-07 18:38:12 +0000

    croc 9.1.2
    Closes #76846.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -1,8 +1,8 @@
 class Croc < Formula
   desc "Securely send things from one computer to another"
   homepage "https://github.com/schollz/croc"
-  url "https://github.com/schollz/croc/archive/v9.1.1.tar.gz"
-  sha256 "ca151e59d7a655f2f4cdb06669f568254c2c73f499c2f828055e142702a6a415"
+  url "https://github.com/schollz/croc/archive/v9.1.2.tar.gz"
+  sha256 "f5dc5aa37cf179f86982080a067218d0fccf8fead9b5b25bc3f1f9181e82ab26"
   license "MIT"
   head "https://github.com/schollz/croc.git"
 
