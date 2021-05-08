36ac0b055b7dd115ae446307ddc7dcebc6fc3068
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-08 16:29:20 +0000

    bork: add 0.12.0 bottle.

--- Formula/bork.rb
+++ Formula/bork.rb
@@ -6,6 +6,11 @@ class Bork < Formula
   license "Apache-2.0"
   head "https://github.com/skylarmacdonald/bork.git", branch: "main"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "289203782efd4a00e6a3e664780f1c3d5fcc0353f38546557b50bd606b24c9f0"
+  end
+
   def install
     files = %w[types/shells.sh types/pipsi.sh types/cask.sh test/type-pipsi.bats test/type-cask.bats]
     inreplace files, "/usr/local/", HOMEBREW_PREFIX
