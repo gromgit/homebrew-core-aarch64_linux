d8557ff95d1dd50d667521877771f72182e70219
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-08 10:06:42 +0000

    swiftformat: update 0.48.1 bottle.

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -7,10 +7,10 @@ class Swiftformat < Formula
   head "https://github.com/nicklockwood/SwiftFormat.git", shallow: false
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bca1fc4ac0264ff950c14eb3c72c0fa321baa99fb60178266da19b680d67147b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fa1af90b137298039eab4205a9a8733fbba4f9e5cf0322197674ce6f1582f0e0"
-    sha256 cellar: :any_skip_relocation, catalina:      "35bc84d95bcd8e5066669c6383f999507b1d1a8a08f3aae321f89e2944ea39e2"
-    sha256 cellar: :any_skip_relocation, mojave:        "b5274f542e17907e806ca6e6d1ca630a538bc2e98330329bca5a1568d76a7267"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "937b21c64f3aec2eaf9d0234f681013e1026f3110bafac9348a58e893305c266"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e5217aee36051fd378912b4620786b5892ec4b69343e06f6b94747ab08e1c0ea"
+    sha256 cellar: :any_skip_relocation, catalina:      "203eaac0a2c2f18121937d3c065a867a748e7539f8a716d8e55358d626850ec3"
+    sha256 cellar: :any_skip_relocation, mojave:        "cc3176e0ed768de2ab13ed28cb41c6efa010382fa2862896c474ff54ce385bfa"
   end
 
   depends_on xcode: ["10.1", :build]
