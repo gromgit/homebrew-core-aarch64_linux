c8fa441eaeaa6c640d9f54b783dff47b51b2c14c
FX Coudert <fxcoudert@gmail.com>
2021-05-09 11:39:50 +0000

    openfast: update 2.5.0_1 bottle.

--- Formula/openfast.rb
+++ Formula/openfast.rb
@@ -7,9 +7,9 @@ class Openfast < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, big_sur:  "59580daa33a07c7e6e82457b95c1c29f9a0c7771e3660f05a540055f61d4de84"
-    sha256 cellar: :any, catalina: "65291fb3a065b7f618be694e47e97acaf676a6e791577f32166c8097cfb897a3"
-    sha256 cellar: :any, mojave:   "8c7af9c78eb329aa2e6804080862c8996693ee35fb076aef1142cbdc4e09766a"
+    sha256 cellar: :any, big_sur:  "4b6211261ec4a1f093cb5a04b3c423eee0ff4dfb4b280c25ba9bdce239397ca5"
+    sha256 cellar: :any, catalina: "bccfffda9f7d31e3645a193b6c6b0a27bfb0f4f1ed7af9c6b569f07877a95554"
+    sha256 cellar: :any, mojave:   "ce8b7d2cbc9d809660ababc4d015fd415f22065b04218d6c449ea7a327f6023b"
   end
 
   depends_on "cmake" => :build
