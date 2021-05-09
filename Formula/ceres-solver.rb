14c986b00f70bef53fea2f80617be58e7aef914b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-09 09:33:03 +0100

    ceres-solver: revision bump (glog 0.5.0)

--- Formula/ceres-solver.rb
+++ Formula/ceres-solver.rb
@@ -4,7 +4,7 @@ class CeresSolver < Formula
   url "http://ceres-solver.org/ceres-solver-2.0.0.tar.gz"
   sha256 "10298a1d75ca884aa0507d1abb0e0f04800a92871cd400d4c361b56a777a7603"
   license "BSD-3-Clause"
-  revision 2
+  revision 3
   head "https://ceres-solver.googlesource.com/ceres-solver.git"
 
   bottle do
