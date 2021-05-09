17fb25009a4876e9369cb621e798b758eb0c7fe6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-09 21:07:21 +0000

    rgbds: update 0.5.1 bottle.

--- Formula/rgbds.rb
+++ Formula/rgbds.rb
@@ -12,10 +12,10 @@ class Rgbds < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "67fbe0746e879838e5a9ad6999bec6f45889a650b29b2d97e3766630d6de2103"
-    sha256 cellar: :any, big_sur:       "b202aecfdd3276b36c0dfff5434074753b19a298db6de61a0d6efefa341975fd"
-    sha256 cellar: :any, catalina:      "94c1c67c1589868066218924f006f457436504cf49ebaaac36947d333b34cc3f"
-    sha256 cellar: :any, mojave:        "0799a1f702a1333d592c97c093ba1096ce22766a60aca9870c5c6294e023b9a6"
+    sha256 cellar: :any, arm64_big_sur: "44cca5083d32dfa42a6d7d120079eabff0c375ff432851de28a61a943725ceb8"
+    sha256 cellar: :any, big_sur:       "3f02c082066bc835f042a37ae3e972cb58a77a615634f299e33f301dab46b6c1"
+    sha256 cellar: :any, catalina:      "9927685009fa383bc393eb32e36cb44d1db0cce12f06640b323f44d6589ad50c"
+    sha256 cellar: :any, mojave:        "e778e85fdb6ea1b00d1d25648e6da9fbfe92c35779dd6be1ae62abc8eb459077"
   end
 
   depends_on "bison" => :build
