f2ef2c219c004f4404d803cae3687f67ec2f7e4d
Rui Chen <rui@chenrui.dev>
2021-05-10 01:00:35 -0400

    nodeenv 1.6.0
    * nodeenv 1.6.0
    * nodeenv: fix test for Apple Silicon
    
    Closes #76931.
    
    Co-authored-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nodeenv.rb
+++ Formula/nodeenv.rb
@@ -1,8 +1,8 @@
 class Nodeenv < Formula
   desc "Node.js virtual environment builder"
   homepage "https://github.com/ekalinin/nodeenv"
-  url "https://github.com/ekalinin/nodeenv/archive/1.5.0.tar.gz"
-  sha256 "c79669818bf010d8539cc9a53729b6be089bb482daac15bca2bf4196b2343284"
+  url "https://github.com/ekalinin/nodeenv/archive/1.6.0.tar.gz"
+  sha256 "87b04b0832d38bd825e004e7c8ed82d75524efaa6b8a72e3a55088d93e4e17f7"
   license "BSD-3-Clause"
 
   bottle :unneeded
@@ -12,13 +12,13 @@ class Nodeenv < Formula
   end
 
   test do
-    system bin/"nodeenv", "--node=0.10.40", "--prebuilt", "env-0.10.40-prebuilt"
+    system bin/"nodeenv", "--node=16.0.0", "--prebuilt", "env-16.0.0-prebuilt"
     # Dropping into the virtualenv itself requires sourcing activate which
     # isn't easy to deal with. This ensures current Node installed & functional.
-    ENV.prepend_path "PATH", testpath/"env-0.10.40-prebuilt/bin"
+    ENV.prepend_path "PATH", testpath/"env-16.0.0-prebuilt/bin"
 
     (testpath/"test.js").write "console.log('hello');"
     assert_match "hello", shell_output("node test.js")
-    assert_match "v0.10.40", shell_output("node -v")
+    assert_match "v16.0.0", shell_output("node -v")
   end
 end
