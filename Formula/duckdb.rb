725b3d53cbe4bc791eb2afcadc5b0dce944103d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-10 05:10:35 +0000

    duckdb: update 0.2.6 bottle.

--- Formula/duckdb.rb
+++ Formula/duckdb.rb
@@ -7,10 +7,10 @@ class Duckdb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "af129c44a8fbdfbee227e27175e6d36924bc38b9b0561dc37f22640432dc8a5e"
-    sha256 cellar: :any, big_sur:       "487edb3243770fbee6104c74dbd98dbe65e1fa6db917dc532a82dfca8e93fcd3"
-    sha256 cellar: :any, catalina:      "9bfe7742ec2fccecdd3a0a3bfc43008b353dad093d79bda164aa81b70e6f204b"
-    sha256 cellar: :any, mojave:        "2b8e9c06f8c7781a705f7d09fbce4e8c1da878a52f7e11548e08d7d5d8fb6174"
+    sha256 cellar: :any, arm64_big_sur: "c8ae8cb83c0e90060d2404f5484f9a341273430e73fecd83878e71ed408a11d1"
+    sha256 cellar: :any, big_sur:       "6db79be340d21aed37a8d10f25d9f8dbcbbb7fea94535037e019cc9de88b58b0"
+    sha256 cellar: :any, catalina:      "ee6034cc5856f527c4fc45a55a4a284272e18728ea0407d47f3fa2523b372f1f"
+    sha256 cellar: :any, mojave:        "03a8b8f6fa84d2198428ebcfffb3b52590048416bf3bd3484ea0a705e434545c"
   end
 
   depends_on "cmake" => :build
