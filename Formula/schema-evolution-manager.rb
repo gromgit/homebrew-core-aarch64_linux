0f8c965433dae87e5dae3a300c70f2be56903f0a
Rui Chen <rui@chenrui.dev>
2021-05-10 01:06:40 -0400

    schema-evolution-manager 0.9.45
    Closes #76938.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/schema-evolution-manager.rb
+++ Formula/schema-evolution-manager.rb
@@ -1,8 +1,8 @@
 class SchemaEvolutionManager < Formula
   desc "Manage postgresql database schema migrations"
   homepage "https://github.com/mbryzek/schema-evolution-manager"
-  url "https://github.com/mbryzek/schema-evolution-manager/archive/0.9.43.tar.gz"
-  sha256 "69a3b2b9ef23f2af97b34742c7c33f2f3889a1e7d471cf9e19138b20d8cfe944"
+  url "https://github.com/mbryzek/schema-evolution-manager/archive/0.9.45.tar.gz"
+  sha256 "268a7a09a2175ed081593843fd03f330afb42710b2af0c9bc456b5e8546057c2"
   license "Apache-2.0"
 
   bottle do
