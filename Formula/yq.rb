34ee83f79e9fa4cf687984d157d37d9bb367dfde
Branch Vincent <branchevincent@gmail.com>
2021-05-10 13:38:18 -0400

    yq 4.8.0
    Closes #76976.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -1,8 +1,8 @@
 class Yq < Formula
   desc "Process YAML documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/v4.7.1.tar.gz"
-  sha256 "19a7c43aaac678065f436ddfdf8b0a75dd3883984f4b9548cabdf53eb09932f9"
+  url "https://github.com/mikefarah/yq/archive/v4.8.0.tar.gz"
+  sha256 "bc95ceacb4857890363d83c234ed6ca225cec385500f09783de6f91a2ca08ea4"
   license "MIT"
 
   bottle do
