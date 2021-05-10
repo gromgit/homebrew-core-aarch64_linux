8cc12248659c65c5b15c80fe80a4f2ffb28b489d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-10 07:08:51 +0000

    kumactl: update 1.1.5 bottle.

--- Formula/kumactl.rb
+++ Formula/kumactl.rb
@@ -11,10 +11,10 @@ class Kumactl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "950cc92a1cb226c36717a592ed9cb644bb9c3863cbcfc00b1ec443399fc2c469"
-    sha256 cellar: :any_skip_relocation, big_sur:       "76a8669744149c14f90d37c48afd09b5c113308e0138569f1cd08f639653237c"
-    sha256 cellar: :any_skip_relocation, catalina:      "9b59d185452ce5340f329f637aa8d88ef56576fce39fbe72dfe6b74849aee129"
-    sha256 cellar: :any_skip_relocation, mojave:        "eeb940cb8abe532acb788269c8ef2c015e8a0f60014e0a0bfc295ccb2fb87639"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9233d1b17c0792a62ef60155d9a8ae5f8aadb7fb048faee06c7ef42ce747c816"
+    sha256 cellar: :any_skip_relocation, big_sur:       "61b0a2e73ab791408205c991bb785fcfac6322672509114bd8998f7cdb2edf0e"
+    sha256 cellar: :any_skip_relocation, catalina:      "ba48aff7ef62800c99f722f95711c576cf3bcb7e43d54751e80e69a7fe61d3ac"
+    sha256 cellar: :any_skip_relocation, mojave:        "af1082eb473a07722c876b31447a65f3d7d8921c377c9138ca8e352694528720"
   end
 
   depends_on "go" => :build
