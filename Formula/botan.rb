696a43962a1d72881dfd76519b03f82e35e3ca29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-10 05:09:34 +0000

    botan: update 2.18.1 bottle.

--- Formula/botan.rb
+++ Formula/botan.rb
@@ -7,10 +7,10 @@ class Botan < Formula
   head "https://github.com/randombit/botan.git"
 
   bottle do
-    sha256 arm64_big_sur: "107ac4cae8611953a670a141d2719bdca508b54c83119397d2cbd4dc649c35d9"
-    sha256 big_sur:       "3d8536823d45cec56da28939e54b4f1e4d33fd84e136d80d9f6c7067bf46a81c"
-    sha256 catalina:      "68ae9bc8f5953f106fe248f7b0a209add68429275eac66d41b6ac15234433a0b"
-    sha256 mojave:        "496c983f8ec163f1da762b62e4ebe7f21b1fc4186eddf7b6fd2adaf9eb6e9db8"
+    sha256 arm64_big_sur: "49d7c6c498d9eeb8d5d8be5b1dfe36107c499bb651df544028e9fec7cdac814d"
+    sha256 big_sur:       "c95c511ab524fe403fb6ca322e1e4b1075d010e1ed6fe8523589297157ef1209"
+    sha256 catalina:      "de7d2bcd91abe81ef297b2134e1aa4bd622f5c9764614e3af0fc04b4a39ad75a"
+    sha256 mojave:        "281575e69fdacaa33127379de2da8d4a5e1951fd98da54ed448655cb959d8149"
   end
 
   depends_on "pkg-config" => :build
