6ee014ea47158ab71985c5c2c893979d63b9753f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-11 19:12:09 +0000

    pssh: update 2.3.1_4 bottle.

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -9,12 +9,10 @@ class Pssh < Formula
   revision 4
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f7e13419d61916a39cc1dfdca7d41e75b83ad569e752e706660a61bf5d27116d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6840ae494e87f8913579af27d25d7324608df3b52769041ca608a22851271688"
-    sha256 cellar: :any_skip_relocation, catalina:      "97b41f49d31808abac8379f9d5891be7cecff34bc183a42b0f6fd5ae1d9fe835"
-    sha256 cellar: :any_skip_relocation, mojave:        "2755e4052daf1641f2db79119443ea4552da5db3c578ed9dd779c86f96b35a78"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "3eab96d7837cfab4b2c28ad5458e1c68ceb0d75c480e87613ca1872e58b2bf55"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "55cce3be0cd183f6179aeb4b51618774fe98cb3e4df078d1fff9ee3e555d2a54"
+    sha256 cellar: :any_skip_relocation, big_sur:       "94ad17e8296472da13da212b912f9edd9fd849566a21eee7b3ad7686f4500e0b"
+    sha256 cellar: :any_skip_relocation, catalina:      "94ad17e8296472da13da212b912f9edd9fd849566a21eee7b3ad7686f4500e0b"
+    sha256 cellar: :any_skip_relocation, mojave:        "94ad17e8296472da13da212b912f9edd9fd849566a21eee7b3ad7686f4500e0b"
   end
 
   depends_on "python@3.9"
