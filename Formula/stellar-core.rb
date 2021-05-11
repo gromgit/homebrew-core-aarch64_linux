079ff5ee4f56177faf206ffaa7e5b7b218a08421
Rui Chen <rui@chenrui.dev>
2021-05-11 17:23:16 -0400

    stellar-core 17.0.0
    Closes #77057.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v16.0.0",
-      revision: "0e35ac6ef382391096dbe4443197051452a3ce50"
+      tag:      "v17.0.0",
+      revision: "096f6a766ed6aa08e13519371e842b90f01df841"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git"
 
