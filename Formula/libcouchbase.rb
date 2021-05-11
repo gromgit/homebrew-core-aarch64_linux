3202b7b1328b75f0346621ac6c28161dc2d4cd8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-11 07:07:31 +0000

    libcouchbase: update 3.1.3 bottle.

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -7,10 +7,10 @@ class Libcouchbase < Formula
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
-    sha256 arm64_big_sur: "6962430ba79b80749d29f905d07b668b1778f12d97a8a4017ba20bcf39b7a891"
-    sha256 big_sur:       "2a01ad8826792dc95ecd647239c13c1de851827f479d63bc0db4c989caec61e5"
-    sha256 catalina:      "f5c195a992c89a4e0af4b8a7141f64023bde628d8fb1dc265cdba8854804a858"
-    sha256 mojave:        "878cc7968ccbe72f2da292b9e40719c0353926c197f41f49f0cc362c45235cc4"
+    sha256 arm64_big_sur: "72815d6444601b6ffb4961321669a6c806e1efce92c76e6f14a3773eb7c17c30"
+    sha256 big_sur:       "53e18993450e62ce593f0562e954dd62157cf82f2279c44b984fb13a34cf76ac"
+    sha256 catalina:      "1a7d3d96544331a8e43df4400a8e4bd4451350d7a0fe6dcb91fae05bf41ca50c"
+    sha256 mojave:        "d457d270cd478f5a52c33b2de47041033a13da57d618febc98d8c0c152392bfc"
   end
 
   depends_on "cmake" => :build
