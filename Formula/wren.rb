dd318bc8eb00ec1009a3e6d6917f8ce3acdf12e0
Rui Chen <rui@chenrui.dev>
2021-05-11 17:26:10 -0400

    wren 0.4.0
    Closes #77061.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wren.rb
+++ Formula/wren.rb
@@ -1,8 +1,8 @@
 class Wren < Formula
   desc "Small, fast, class-based concurrent scripting language"
   homepage "https://wren.io"
-  url "https://github.com/wren-lang/wren/archive/0.3.0.tar.gz"
-  sha256 "c566422b52a18693f57b15ae4c9459604e426ea64eddb5fbf2844d8781aa4eb7"
+  url "https://github.com/wren-lang/wren/archive/0.4.0.tar.gz"
+  sha256 "23c0ddeb6c67a4ed9285bded49f7c91714922c2e7bb88f42428386bf1cf7b339"
   license "MIT"
 
   livecheck do
