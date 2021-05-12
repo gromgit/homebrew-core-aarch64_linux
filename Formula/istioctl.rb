50511bcc1e12e65d8936cb0018087d3de49a8ac9
Lukas Auerbeck <auerbeck@youniqx.com>
2021-05-13 00:25:08 +0200

    istioctl 1.10.0
    * istioctl 1.9.5
    * istioctl 1.10.0
    * istioctl: add logic for macOS ARM build
    
    Closes #77139.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -2,8 +2,8 @@ class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://istio.io/"
   url "https://github.com/istio/istio.git",
-      tag:      "1.9.4",
-      revision: "13fb8ac89420d8cc5b3f895adc614233e805a61c"
+      tag:      "1.10.0",
+      revision: "d26cba7e341587453ffeb978f5cf6fbc32f346f8"
   license "Apache-2.0"
   head "https://github.com/istio/istio.git"
 
@@ -21,14 +21,23 @@ class Istioctl < Formula
     ENV["HUB"] = "docker.io/istio"
     ENV["BUILD_WITH_CONTAINER"] = "0"
 
-    system "make", "gen-charts", "istioctl", "istioctl.completion"
     dirpath = nil
     on_macos do
-      dirpath = "darwin_amd64"
+      if Hardware::CPU.arm?
+        # Fix missing "amd64" for macOS ARM in istio/common/scripts/setup_env.sh
+        # Can remove when upstream adds logic to detect `$(uname -m) == "arm64"`
+        ENV["TARGET_ARCH"] = "arm64"
+
+        dirpath = "darwin_arm64"
+      else
+        dirpath = "darwin_amd64"
+      end
     end
     on_linux do
       dirpath = "linux_amd64"
     end
+
+    system "make", "istioctl", "istioctl.completion"
     cd "out/#{dirpath}" do
       bin.install "istioctl"
       bash_completion.install "release/istioctl.bash"
