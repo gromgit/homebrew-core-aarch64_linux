626a8d2760c1cc754025c044e590a42217edc9dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-12 06:06:52 +0000

    libpqxx: update 7.5.2 bottle.

--- Formula/libpqxx.rb
+++ Formula/libpqxx.rb
@@ -6,10 +6,10 @@ class Libpqxx < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5d7d1131c04dc41a6277ab06f953e180fef6979be60d854e4fa02df7111e59f8"
-    sha256 cellar: :any, big_sur:       "941aa261f18765bfed6888c94baaf7a0ad495ef8272229537a881e1ac8be5504"
-    sha256 cellar: :any, catalina:      "f0304c955169cdec57747a49acb59556001944ddd83d6d53601aaa9806f25d4f"
-    sha256 cellar: :any, mojave:        "e0fa3edf4874999fbf4062c9083c74d1517e974f1bb882b7f842a0d731cd5870"
+    sha256 cellar: :any, arm64_big_sur: "8233c21f3363d037f6f79cf7754ac6bf515a307a1c090a3182e5a064859d526c"
+    sha256 cellar: :any, big_sur:       "df129caeb02ff03f2b146f6558471f105ff3ba67868e186d011f98444d40ee8a"
+    sha256 cellar: :any, catalina:      "b442129051871681bbb6205dcee1730baf1a8ed2f07ff6119700b3376c23c069"
+    sha256 cellar: :any, mojave:        "caa8680e0f0ef430fc9c31e4212054f2d6aca355bf68164637deac6d307c1c3b"
   end
 
   depends_on "pkg-config" => :build
