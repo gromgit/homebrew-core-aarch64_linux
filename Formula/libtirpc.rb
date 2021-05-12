12a385a59bd210a041aefd7bd9d465bc69fc0a68
rui <chenrui333@gmail.com>
2021-05-12 02:25:34 -0400

    libtirpc 1.3.2 (#77104)

--- Formula/libtirpc.rb
+++ Formula/libtirpc.rb
@@ -1,8 +1,8 @@
 class Libtirpc < Formula
   desc "Port of Sun's Transport-Independent RPC library to Linux"
   homepage "https://sourceforge.net/projects/libtirpc/"
-  url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.3.1/libtirpc-1.3.1.tar.bz2"
-  sha256 "245895caf066bec5e3d4375942c8cb4366adad184c29c618d97f724ea309ee17"
+  url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.3.2/libtirpc-1.3.2.tar.bz2"
+  sha256 "e24eb88b8ce7db3b7ca6eb80115dd1284abc5ec32a8deccfed2224fc2532b9fd"
   license "BSD-3-Clause"
 
   depends_on "krb5"
