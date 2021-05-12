fa24c9f08153b3dbf86df1e4c6432fdb205748ef
darklore <zodiac.brave.story@gmail.com>
2021-05-12 16:51:45 +0900

    digdag: remove unnecessary intermediate script
    Closes #77152.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -4,6 +4,7 @@ class Digdag < Formula
   url "https://dl.digdag.io/digdag-0.10.0.jar"
   sha256 "0a3aed836d8af1a47ed53dda63c02ce3ecfec6b564d55b556a18b122dec7f3d7"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://github.com/treasure-data/digdag.git"
@@ -16,9 +17,7 @@ class Digdag < Formula
 
   def install
     libexec.install "digdag-#{version}.jar"
-    (libexec/"bin").write_jar_script libexec/"digdag-#{version}.jar", "digdag"
-    (libexec/"bin/digdag").chmod 0755
-    (bin/"digdag").write_env_script libexec/"bin/digdag", Language::Java.java_home_env("1.8")
+    bin.write_jar_script libexec/"digdag-#{version}.jar", "digdag", java_version: "1.8"
   end
 
   test do
