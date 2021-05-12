563efa4590214ede5288fc4a42ec862cbe901301
Rui Chen <rui@chenrui.dev>
2021-05-11 23:56:52 -0400

    eless 0.7
    Closes #77095.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/eless.rb
+++ Formula/eless.rb
@@ -1,8 +1,8 @@
 class Eless < Formula
   desc "Better `less` using Emacs view-mode and Bash"
   homepage "https://eless.scripter.co/"
-  url "https://github.com/kaushalmodi/eless/archive/v0.6.tar.gz"
-  sha256 "a691a56da6d92f279e46c10d72d3ef6e4951f0e30092ca394622b6e94aae551b"
+  url "https://github.com/kaushalmodi/eless/archive/v0.7.tar.gz"
+  sha256 "de7a7891a20a5e7f25c0b5df812edaea87ab0d3336d41821a24e2d248aaf4abc"
   license "MIT"
 
   bottle do
