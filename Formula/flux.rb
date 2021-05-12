2f3dc4784665f48d26cae6d4460f12a662d6de44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-12 06:08:01 +0000

    flux: update 0.115.0 bottle.

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -13,10 +13,10 @@ class Flux < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd91c49404174c1ba144c7a0e38d7adcfc4544bfa32e9c3a3db2146f2d6dc2fd"
-    sha256 cellar: :any,                 big_sur:       "da4f31757fb58d9187f063c72c11146521af858459f9f0c821c6bc366ed262f8"
-    sha256 cellar: :any,                 catalina:      "68b8c13a44ca0d0ec04e3a2dd5bcb4988226335e693a62ccda0d17dcda46577b"
-    sha256 cellar: :any,                 mojave:        "e7065e47c8f418e010fad574a7458752934d36cb68683254eb3d4cf2b77e8ab4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2b6c6cb0f75d7f08a0f276eeefd21b64f503abdb3e1ec20cabbb163bb9087452"
+    sha256 cellar: :any,                 big_sur:       "94e9db3fc9a8ab1edad9ef010eaebadf1a0b5ec5b7cee48d59a8b6f977b22251"
+    sha256 cellar: :any,                 catalina:      "5c6ab736c144359eadb3b249767b7b4a04475b71c9ce10869dd6ef108f035c24"
+    sha256 cellar: :any,                 mojave:        "40d383f6b7e6fa77b735ea62bd399a0024fa773f47a2aaa6d2acc2199b37adcf"
   end
 
   depends_on "go" => :build
