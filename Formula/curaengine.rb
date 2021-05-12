66e696b77a80f2365c9ad5af78be82b286ce5ae1
Rui Chen <rui@chenrui.dev>
2021-05-11 23:41:40 -0400

    curaengine 4.9.1
    * curaengine 4.9.1
    * update resource urls
    
    Closes #77085.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/curaengine.rb
+++ Formula/curaengine.rb
@@ -1,8 +1,8 @@
 class Curaengine < Formula
   desc "C++ 3D printing GCode generator"
   homepage "https://github.com/Ultimaker/CuraEngine"
-  url "https://github.com/Ultimaker/CuraEngine/archive/4.9.0.tar.gz"
-  sha256 "202edbdd3c376765009dc9b8883ab0480d9c501c7ce7c9cfc2c383969ea908c2"
+  url "https://github.com/Ultimaker/CuraEngine/archive/4.9.1.tar.gz"
+  sha256 "eb422ef388bee09d26bbf3bade489359bf1fd59fd04d8452ac5d6ab3ba5e3894"
   license "AGPL-3.0-or-later"
   version_scheme 1
   head "https://github.com/Ultimaker/CuraEngine.git"
@@ -29,12 +29,12 @@ class Curaengine < Formula
   # The version tag in these resources (e.g., `/1.2.3/`) should be changed as
   # part of updating this formula to a new version.
   resource "fdmextruder_defaults" do
-    url "https://raw.githubusercontent.com/Ultimaker/Cura/4.9.0/resources/definitions/fdmextruder.def.json"
+    url "https://raw.githubusercontent.com/Ultimaker/Cura/4.9.1/resources/definitions/fdmextruder.def.json"
     sha256 "d9b38fdf02d1dcdc6ee7401118ca9468236adb860786361e453f1eeb54c95b1f"
   end
 
   resource "fdmprinter_defaults" do
-    url "https://raw.githubusercontent.com/Ultimaker/Cura/4.9.0/resources/definitions/fdmprinter.def.json"
+    url "https://raw.githubusercontent.com/Ultimaker/Cura/4.9.1/resources/definitions/fdmprinter.def.json"
     sha256 "bc4ed29f1c4191ccf6fa76b0a2857aca7ab6ed2a5f526364f52910f330dfa6a6"
   end
 
