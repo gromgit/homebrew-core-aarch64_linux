550c498ca7d9b9f6f75a2358fae066881f39094f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-13 19:14:28 +0000

    numcpp: update 2.4.2 bottle.

--- Formula/numcpp.rb
+++ Formula/numcpp.rb
@@ -7,10 +7,7 @@ class Numcpp < Formula
   head "https://github.com/dpilger26/NumCpp.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fac92d42c82fc628b8441e1ed25fc156e7f96cbc4b438b74ecd396b0a5e2cf06"
-    sha256 cellar: :any_skip_relocation, big_sur:       "07e219c25f8ddd7b80e801b1f37cb1305af54fb907ab3f0c53bf66f4a8610c4c"
-    sha256 cellar: :any_skip_relocation, catalina:      "ccbd4b3aa14f6a509aaf214162c3f1582acf374cafa9e2c8e9c4f1e3f0e11247"
-    sha256 cellar: :any_skip_relocation, mojave:        "26c19afedc51463577541ce31f7641fa545bc73d61c7a563322281b0da7206e9"
+    sha256 cellar: :any_skip_relocation, all: "51d96b14f97021af57e0fe9186e0bfca585d8482d5f375ee4d46247cba800e37"
   end
 
   depends_on "cmake" => :build
