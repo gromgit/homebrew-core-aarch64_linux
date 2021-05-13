c32c610255f122e256a605cb76a5f8794679008a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-13 21:24:19 +0000

    leveldb: update 1.23 bottle.

--- Formula/leveldb.rb
+++ Formula/leveldb.rb
@@ -6,9 +6,10 @@ class Leveldb < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "bb5f8bc871e315e4ae36f011052f2b92e35040cc03ef8d448093e7be1bdfe6ac"
-    sha256 cellar: :any, catalina: "299f9004aa344b2ac164fdeee5a077c3e45335f3527cb8f2e67b46acf88b185a"
-    sha256 cellar: :any, mojave:   "b4d54e51eef8d5d538830f555561fa4cc5f1b275b45588eae364d79de6b1d716"
+    sha256 cellar: :any, arm64_big_sur: "377ac4b779d9ac24295b99c5f859dc8f78f473e62a90849f09eeec7a72d872d2"
+    sha256 cellar: :any, big_sur:       "bb5f8bc871e315e4ae36f011052f2b92e35040cc03ef8d448093e7be1bdfe6ac"
+    sha256 cellar: :any, catalina:      "299f9004aa344b2ac164fdeee5a077c3e45335f3527cb8f2e67b46acf88b185a"
+    sha256 cellar: :any, mojave:        "b4d54e51eef8d5d538830f555561fa4cc5f1b275b45588eae364d79de6b1d716"
   end
 
   depends_on "cmake" => :build
