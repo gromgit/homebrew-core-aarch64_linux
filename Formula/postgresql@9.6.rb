5ecc720c14385761774fd4aefce9bddc9b1f0e7a
Rui Chen <rui@chenrui.dev>
2021-05-13 13:36:25 -0400

    postgresql@9.6 9.6.22
    Closes #77216.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/postgresql@9.6.rb
+++ Formula/postgresql@9.6.rb
@@ -1,8 +1,8 @@
 class PostgresqlAT96 < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v9.6.21/postgresql-9.6.21.tar.bz2"
-  sha256 "930feaef28885c97ec40c26ab6221903751eeb625de92b22602706d7d47d1634"
+  url "https://ftp.postgresql.org/pub/source/v9.6.22/postgresql-9.6.22.tar.bz2"
+  sha256 "3d32cd101025a0556813397c69feff3df3d63736adb8adeaf365c522f39f2930"
   license "PostgreSQL"
 
   livecheck do
