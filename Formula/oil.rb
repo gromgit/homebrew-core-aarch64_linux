089be8056c604f6bb251eaf03bc0d59d3e1c9731
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-13 19:13:47 +0000

    oil: update 0.8.11 bottle.

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -11,10 +11,10 @@ class Oil < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "e47bb7ac29bd5bee2334a75e9b3b33e2a14f61540268498da543f9c0f2e2db26"
-    sha256 big_sur:       "2e53867c72bca15e9ca9aca7abff90ce130842ae3ea351d38f93a088486537b4"
-    sha256 catalina:      "2019afa3e50a5a2a83d8350c31a8c51eac3e40a12343e9ecf411e7198694920d"
-    sha256 mojave:        "0b452d3ce2826a66f9143487bff4bbf9f88169538903c201c62b815c8fb7c162"
+    sha256 arm64_big_sur: "27306bd8d63fc6a585124f2759a614197ba2b1cabd0efc06a7f6eca3539a2873"
+    sha256 big_sur:       "7c0920ee0af6b53588b26b6ef53ce72001a4b14007107d395cfee61fbc4d3d83"
+    sha256 catalina:      "0ff823bbbeddc607bd24bcc26bdb1ddd73d2cb98f0f7a332af2218a77f27885e"
+    sha256 mojave:        "f293cf234e3f2a5dd92587608b88e3211715333025269c922b422493a0a6eff7"
   end
 
   depends_on "readline"
