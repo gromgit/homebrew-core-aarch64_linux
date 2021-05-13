59ceb64765d4769767e751526e5c39d3e91dde24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-13 19:14:24 +0000

    nuclei: update 2.3.7 bottle.

--- Formula/nuclei.rb
+++ Formula/nuclei.rb
@@ -7,10 +7,10 @@ class Nuclei < Formula
   head "https://github.com/projectdiscovery/nuclei.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "00b604baf984876fa13adddfb70590ca37e64c83d78a4eab684aae52d69e5665"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8b43cae055b41bcbf4efa8ac97430be10bd492153b246ec1230cc5c2bef5ed58"
-    sha256 cellar: :any_skip_relocation, catalina:      "999ce4ed94a9aa6e377078782a2441bda7b2868179f221dc62611ffed8385d30"
-    sha256 cellar: :any_skip_relocation, mojave:        "51cce4cc30be90d1e2933c1f84ce9c0a16dc4200548bb1dd6b43a6ea46e738f3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6e78cbcaee02785e7a7642576bfa478d4e25fd74f0de9d3069001c8b45aa48c3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f6bba8546c9112c735584a90a46959405394ff0b50629148eb6786aef2aa42ce"
+    sha256 cellar: :any_skip_relocation, catalina:      "7d54a4de80dab9bc4b4405542ce908464e76396c444c9e764a2eac832ba624c2"
+    sha256 cellar: :any_skip_relocation, mojave:        "754a3364077f561c3e97dd940b1668278594c87163d51642c432f1df886d97cd"
   end
 
   depends_on "go" => :build
