791ba95a707c0bdebc0410bfbc3f003a6a71cfee
Colin Taylor <cjntaylor@gmail.com>
2021-05-13 13:59:31 -0400

    autoconf@2.69 2.69 (new formula)
    This is needed for #77725. Also, we'll need it any time we patch a
    formula that requires Autoconf 2.69 to regenerate its configure script
    (e.g. any `erlang` version).
    
    Previously proposed in #77227.
    
    Closes #77725.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/autoconf@2.69.rb
@@ -0,0 +1,50 @@
+class AutoconfAT269 < Formula
+  desc "Automatic configure script builder"
+  homepage "https://www.gnu.org/software/autoconf"
+  url "https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
+  mirror "https://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
+  sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
+  license all_of: [
+    "GPL-3.0-or-later",
+    "GPL-3.0-or-later" => { with: "Autoconf-exception-3.0" },
+  ]
+
+  keg_only :versioned_formula
+
+  depends_on "m4"
+  uses_from_macos "perl"
+
+  def install
+    on_macos do
+      ENV["PERL"] = "/usr/bin/perl"
+
+      # force autoreconf to look for and use our glibtoolize
+      inreplace "bin/autoreconf.in", "libtoolize", "glibtoolize"
+      # also touch the man page so that it isn't rebuilt
+      inreplace "man/autoreconf.1", "libtoolize", "glibtoolize"
+    end
+
+    system "./configure", "--prefix=#{prefix}", "--with-lispdir=#{elisp}"
+    system "make", "install"
+
+    rm_f info/"standards.info"
+  end
+
+  test do
+    cp prefix/"share/autoconf/autotest/autotest.m4", "autotest.m4"
+    system bin/"autoconf", "autotest.m4"
+
+    (testpath/"configure.ac").write <<~EOS
+      AC_INIT([hello], [1.0])
+      AC_CONFIG_SRCDIR([hello.c])
+      AC_PROG_CC
+      AC_OUTPUT
+    EOS
+    (testpath/"hello.c").write "int foo(void) { return 42; }"
+
+    system bin/"autoconf"
+    system "./configure"
+    assert_predicate testpath/"config.status", :exist?
+    assert_match(/\nCC=.*#{ENV.cc}/, (testpath/"config.log").read)
+  end
+end
