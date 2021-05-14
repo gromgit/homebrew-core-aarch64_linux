22b477e9191aa81f1b887128f7e244585d3e1132
Rui Chen <rui@chenrui.dev>
2021-05-14 16:08:46 -0400

    terrascan 1.6.0
    * terrascan 1.6.0
    * remove patch
    * update test
    
    Closes #77325.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terrascan.rb
+++ Formula/terrascan.rb
@@ -1,8 +1,8 @@
 class Terrascan < Formula
   desc "Detect compliance and security violations across Infrastructure as Code"
   homepage "https://www.accurics.com/products/terrascan/"
-  url "https://github.com/accurics/terrascan/archive/v1.5.1.tar.gz"
-  sha256 "0ef2490c711fc089e05a926ba1935be62f60bd20b7226906e207382aaae48f84"
+  url "https://github.com/accurics/terrascan/archive/v1.6.0.tar.gz"
+  sha256 "4ff014832f5d4e85ee275930639705a8ad9123eb4691591e6645fc76f1b0eb95"
   license "Apache-2.0"
   head "https://github.com/accurics/terrascan.git"
 
@@ -14,12 +14,6 @@ class Terrascan < Formula
   end
 
   depends_on "go" => :build
-
-  # Fixes version, remove in next release.
-  patch do
-    url "https://github.com/accurics/terrascan/commit/d8fd9c4bae5b12ffbe8d7c7e1a1d67042dfd8edf.patch?full_index=1"
-    sha256 "994c4bc35899286edf48baad868b7482e5dc1090ad8a7dceffe25e4df438ca2f"
-  end
 
   def install
     system "go", "build", *std_go_args, "./cmd/terrascan"
@@ -41,7 +35,7 @@ class Terrascan < Formula
     EOS
 
     expected = <<~EOS
-      \tPolicies Validated  :\t158
+      \tPolicies Validated  :\t203
       \tViolated Policies   :\t0
       \tLow                 :\t0
       \tMedium              :\t0
