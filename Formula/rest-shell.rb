2e97072fd9c5a60c851061d24771c77c06042549
Thierry Moisan <thierry.moisan@gmail.com>
2021-05-13 20:09:46 -0400

    rest-shell: downgrade to openjdk@11

--- Formula/rest-shell.rb
+++ Formula/rest-shell.rb
@@ -6,16 +6,17 @@ class RestShell < Formula
   # version "1.2.1"
   sha256 "0ecfa67d005cc0d51e7a3a26c4dacc53aa12012f0e757332a2fa40c5e780c2d6"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "887fccddf93ec8546b97c3a2351b3111d7869eb5576b401b9c19e6ec49147b2f"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@11"
 
   def install
     libexec.install Dir["*"]
-    (bin/"rest-shell").write_env_script libexec/"bin/rest-shell", Language::Java.overridable_java_home_env
+    (bin/"rest-shell").write_env_script libexec/"bin/rest-shell", Language::Java.overridable_java_home_env("11")
   end
 
   test do
