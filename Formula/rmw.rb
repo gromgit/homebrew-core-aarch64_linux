5d3f9bec317b4d400e5d7e305c38018c9bfd2849
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-14 04:40:39 +0000

    rmw: add 0.7.09 bottle.

--- Formula/rmw.rb
+++ Formula/rmw.rb
@@ -6,6 +6,13 @@ class Rmw < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/theimpossibleastronaut/rmw.git"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "383d2b2a02e8fcfd416e4b2ba106049bef27bdb3fc3f37f8fcaeea62b827b2f4"
+    sha256 cellar: :any, big_sur:       "0f7135823daa0e0da7f172b24a173b31f39f9f44224fb0b1d623cdc9ef9a2077"
+    sha256 cellar: :any, catalina:      "1d7ad66efc9a6b312b58ba471ed53a7de0ef30e879a6b02d321794d0856a61e5"
+    sha256 cellar: :any, mojave:        "9ae98484dc5bc1bc0ecea7ef1c1edc42249a9841844283bac995cb5c6817375b"
+  end
+
   # Slightly buggy with system ncurses
   # https://github.com/theimpossibleastronaut/rmw/issues/205
   depends_on "ncurses"
