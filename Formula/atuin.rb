473ac1185610de4a8e3635360ec12e722da1cda2
Nanda H Krishna <me@nandahkrishna.com>
2021-05-14 07:31:22 +0000

    atuin: add 0.7.1 bottle.

--- Formula/atuin.rb
+++ Formula/atuin.rb
@@ -5,6 +5,13 @@ class Atuin < Formula
   sha256 "0e1cd2104c6fa4cd2d6f30ae9771ed3f22f78a4e0e7b5243e42abb753b792c38"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "73a3722439f584b2e0a636f5a87ff08a2a05f3f9d92ed9ec483b82af5069eecc"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b20e08fa6b7025c5a724db1f18a16acdd30950a46eec82f9f1a33852b71a27a9"
+    sha256 cellar: :any_skip_relocation, catalina:      "a8cf5ccab9ba3d5782a502e1e2e299e765b2b5e99a6f7adbe723648da80931ee"
+    sha256 cellar: :any_skip_relocation, mojave:        "3004a871e9de5e74d1cfb5f1e6aa84a36e5e4826f5fb7da0fe9e8c23fe7d08eb"
+  end
+
   depends_on "rust" => :build
 
   def install
