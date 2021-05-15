a64445aca8575f6f5a32d88c5cfa0ff68f37ab77
Thierry Moisan <thierry.moisan@gmail.com>
2021-05-15 16:16:52 -0400

    ktlint: downgrade to openjdk@11

--- Formula/ktlint.rb
+++ Formula/ktlint.rb
@@ -4,17 +4,18 @@ class Ktlint < Formula
   url "https://github.com/pinterest/ktlint/releases/download/0.41.0/ktlint"
   sha256 "438bd098e5e8acc966940480b025af7020bdaa66698c7d76042416314100e183"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "3e579a1c6a074922e7280071c9cbd87f1604d645b8a1b694b9ecc95739e95111"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@11"
 
   def install
     libexec.install "ktlint"
     (libexec/"ktlint").chmod 0755
-    (bin/"ktlint").write_env_script libexec/"ktlint", JAVA_HOME: Formula["openjdk"].opt_prefix
+    (bin/"ktlint").write_env_script libexec/"ktlint", JAVA_HOME: Formula["openjdk@11"].opt_prefix
   end
 
   test do
