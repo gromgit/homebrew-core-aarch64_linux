401817af49c41703136e5689d0334876b087e955
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-15 19:30:21 +0000

    elan-init: add 1.0.3 bottle.

--- Formula/elan-init.rb
+++ Formula/elan-init.rb
@@ -6,6 +6,12 @@ class ElanInit < Formula
   license "Apache-2.0"
   head "https://github.com/leanprover/elan.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, big_sur:  "5186f119f7d51aaba350572cf201667337bba9acaaace66ceb93dcbea64127bc"
+    sha256 cellar: :any_skip_relocation, catalina: "000a57fb44eb1837edea14451ca57cb42563ae976226c36cb0cb4202c7f7aa68"
+    sha256 cellar: :any_skip_relocation, mojave:   "2801b216d9b44a1d0df88dd576f01bc27bdc69812c255a824107f8b990b28bd3"
+  end
+
   depends_on "rust" => :build
   # elan-init will run on arm64 Macs, but will fetch Leans that are x86_64.
   depends_on arch: :x86_64
