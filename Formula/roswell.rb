03d4fbe947863fef3fe96ceaa7d3511294c468a4
Nanda H Krishna <me@nandahkrishna.com>
2021-05-15 20:22:10 +0000

    roswell: update 21.05.14.109 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -7,9 +7,10 @@ class Roswell < Formula
   head "https://github.com/roswell/roswell.git"
 
   bottle do
-    sha256 big_sur:  "9cc57eb8499901954640e50f20cf1935f206a6f415d1812d4c875eec1361cf9c"
-    sha256 catalina: "274105c09c47e4aa6d586f970dd5f44da0e664db7f1c8a8f2b04691b2894c164"
-    sha256 mojave:   "2b7ec4f359c16791326ac1a8ef064ba40e6dc758ed0b6b9c277cc13c12c1a6cf"
+    sha256 arm64_big_sur: "785be1d8190523c58b91aba74636b61e1dc395d00d993dc4b82e901d04017c10"
+    sha256 big_sur:       "9cc57eb8499901954640e50f20cf1935f206a6f415d1812d4c875eec1361cf9c"
+    sha256 catalina:      "274105c09c47e4aa6d586f970dd5f44da0e664db7f1c8a8f2b04691b2894c164"
+    sha256 mojave:        "2b7ec4f359c16791326ac1a8ef064ba40e6dc758ed0b6b9c277cc13c12c1a6cf"
   end
 
   depends_on "autoconf" => :build
