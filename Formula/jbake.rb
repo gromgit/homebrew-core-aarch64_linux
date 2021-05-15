b2f4676bc57060599710aabe7d07d3ba88710669
Manik Magar <manik.magar@gmail.com>
2021-05-15 12:05:38 -0400

    jbake 2.6.7 (#77352)

--- Formula/jbake.rb
+++ Formula/jbake.rb
@@ -1,8 +1,8 @@
 class Jbake < Formula
   desc "Java based static site/blog generator"
   homepage "https://jbake.org/"
-  url "https://github.com/jbake-org/jbake/releases/download/v2.6.6/jbake-2.6.6-bin.zip"
-  sha256 "4783f5bc33a3a5eb24f5557cd61c79128397271d8e145cd74be33547662e5274"
+  url "https://github.com/jbake-org/jbake/releases/download/v2.6.7/jbake-2.6.7-bin.zip"
+  sha256 "8d9c2b70fbf26415c5b3e530088b8b7fd1d236d3ce2c98a9c03fff4734bced39"
   license "MIT"
 
   bottle :unneeded
