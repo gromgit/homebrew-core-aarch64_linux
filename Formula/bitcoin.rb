b75713bbaf69fd82d026324d6d1e8a985062b3f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-15 17:24:22 +0000

    bitcoin: update 0.21.1 bottle.

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -12,9 +12,9 @@ class Bitcoin < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:  "f9235205e7c1befe37fa1663c5f25a9dfe03198ff9db8e439d116109fb12948c"
-    sha256 cellar: :any, catalina: "1908a1b6dc0f0ded7091db58cfe74d7540f36636c1599b71a2016c50f71ab7fe"
-    sha256 cellar: :any, mojave:   "05343622704d9ca898949fc46973a7a1e7b911530e5de13d6c5d0a51777671f2"
+    sha256 cellar: :any, big_sur:  "c0fc6169ebc38c3ac88562ee05f4e47f8496b4fd132e1f0c2e5f58388b3cbda3"
+    sha256 cellar: :any, catalina: "cece0dd423980991501b1ab1a8d11ea1056df766ccd30dbb1ee09c1e1444a92d"
+    sha256 cellar: :any, mojave:   "0ee27408b8cc28d13cb707c376287716369c6ca59d2215f0effb0a8813f3e647"
   end
 
   depends_on "autoconf" => :build
