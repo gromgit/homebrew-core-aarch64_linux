972395d1640259c050efb02cc462327f5b558dc1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-16 01:38:30 +0000

    ipinfo-cli: add 1.1.2 bottle.

--- Formula/ipinfo-cli.rb
+++ Formula/ipinfo-cli.rb
@@ -5,6 +5,13 @@ class IpinfoCli < Formula
   sha256 "4172c686b15d1a92f39b3490eea4f56325d7b5f9e6696c1d12c4aedc9706a2d6"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f27e301bf0e680fe12ad8e44be36e345c3edaee34f76ca31a842dc6852c505da"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b440bf6805db145f6c36b5a23415276bb852f6d73a2e0118eb14749257b05d1c"
+    sha256 cellar: :any_skip_relocation, catalina:      "b7dd8f1405beb25da25e84f9d37950251edfa9d7df700dd3dcf27f38057ebf9e"
+    sha256 cellar: :any_skip_relocation, mojave:        "e0021ff4d0089ae5e6d08c6a3682080b755df964dc45c9a0e81425949ac8d6ee"
+  end
+
   depends_on "go" => :build
 
   conflicts_with "ipinfo", because: "ipinfo and ipinfo-cli install the same binaries"
