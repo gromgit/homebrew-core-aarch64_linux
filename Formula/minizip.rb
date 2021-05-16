dafce7504e6bc9f283f28e088c8203cbd9b52ef5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-16 21:41:12 +0100

    minizip: update `conflicts_with` (#77418)
    `minizip2` was renamed to `minizip-ng` in #73488.
    
    Also, update the license. Ref:
    
        https://metadata.ftp-master.debian.org/changelogs//main/m/minizip/minizip_1.1-8_copyright

--- Formula/minizip.rb
+++ Formula/minizip.rb
@@ -3,6 +3,7 @@ class Minizip < Formula
   homepage "https://www.winimage.com/zLibDll/minizip.html"
   url "https://zlib.net/zlib-1.2.11.tar.gz"
   sha256 "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
+  license "Zlib"
 
   livecheck do
     url "https://zlib.net/"
@@ -25,7 +26,7 @@ class Minizip < Formula
 
   uses_from_macos "zlib"
 
-  conflicts_with "minizip2",
+  conflicts_with "minizip-ng",
     because: "both install a `libminizip.a` library"
 
   def install
