c3af2f5e98b67834e39d906ae7197899c0a10a4c
Rui Chen <rui@chenrui.dev>
2021-05-16 01:54:42 -0400

    lsix 1.7.4
    Closes #77395.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lsix.rb
+++ Formula/lsix.rb
@@ -1,8 +1,8 @@
 class Lsix < Formula
   desc "Shows thumbnails in terminal using sixel graphics"
   homepage "https://github.com/hackerb9/lsix"
-  url "https://github.com/hackerb9/lsix/archive/1.7.3.tar.gz"
-  sha256 "88f1855b0272a307758d344a861370c087432ed8f28cc95696659eb0ed7eb960"
+  url "https://github.com/hackerb9/lsix/archive/1.7.4.tar.gz"
+  sha256 "6079d0b46e4d56cd0a16f31c9f9fd13da2717cdb990033daa820534b6dc4034e"
   license "GPL-3.0-only"
 
   bottle do
