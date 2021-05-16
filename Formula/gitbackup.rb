b6718cf7303f2144d12787699e7a78a3d0e7a9e1
Steve Hoeksema <steve@kotiri.com>
2021-05-17 10:56:15 +1200

    gitbackup 0.6 (new formula)
    Remove unnecessary regex
    
    Closes #77421.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gitbackup.rb
@@ -0,0 +1,18 @@
+class Gitbackup < Formula
+  desc "Tool to backup your Bitbucket, GitHub and GitLab repositories"
+  homepage "https://github.com/amitsaha/gitbackup"
+  url "https://github.com/amitsaha/gitbackup/archive/refs/tags/0.6.tar.gz"
+  sha256 "66edd50945a689abcd07aad5047317dc17cb17ec5e813f63c3520394696a6fcb"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    assert_match "Please specify the git service type", shell_output("#{bin}/gitbackup 2>&1", 1)
+  end
+end
