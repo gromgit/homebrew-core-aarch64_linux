e0c0cd06b12e537e9d4c5e27b322bdef9de8490c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-16 18:16:13 +0000

    dialog: update 1.3-20210509 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -11,10 +11,10 @@ class Dialog < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "36a7ae1f6265699cd3b75a0c6c3d583fe1516439cc779bf8a5ee4606d1cb8357"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4fb719fa5573402c89dfa1ec9662b49727837f408883fe1b634519e87d670ca9"
-    sha256 cellar: :any_skip_relocation, catalina:      "5e2c06ad6adbb69ac46cc75b1a71ffeb9becb918e54484563e2da16dfdab5a31"
-    sha256 cellar: :any_skip_relocation, mojave:        "34d01a1614762e0ce58b0920ca7ad8433e0d34a77c13b21c5c1e71728afde6ae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3426b2e8d639857b7c7b4ff8cfd7d1b907d40e9b52268e3cde2c6fca860bae8f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "38bdd48f63fded5254924dda25fdc060c96bc0cb9181068bc54326212f065ccc"
+    sha256 cellar: :any_skip_relocation, catalina:      "f40fc293690fcda948cec968736b8b08120ad46262a65324ff3ff86f5ef91fae"
+    sha256 cellar: :any_skip_relocation, mojave:        "ce1ff4e4585bd9b656ea533f1fc04ca398ff4d8fc0879389ec7d13470ac74b41"
   end
 
   uses_from_macos "ncurses"
