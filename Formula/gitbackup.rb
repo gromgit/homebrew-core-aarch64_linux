6dac5aa5b17f84b391005874e10631b8fcb4deda
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-05-17 07:13:26 +0000

    gitbackup: add 0.6 bottle.

--- Formula/gitbackup.rb
+++ Formula/gitbackup.rb
@@ -5,6 +5,13 @@ class Gitbackup < Formula
   sha256 "66edd50945a689abcd07aad5047317dc17cb17ec5e813f63c3520394696a6fcb"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b7be6e6c6769e5e9ddf40052e9e2f346f0b99bc6fa463fce8064c6a14e9f3742"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3770d11ffc568d1bc4e037c99af6bcfd1cc448e015d7dc5603f1277f4a08b21f"
+    sha256 cellar: :any_skip_relocation, catalina:      "3770d11ffc568d1bc4e037c99af6bcfd1cc448e015d7dc5603f1277f4a08b21f"
+    sha256 cellar: :any_skip_relocation, mojave:        "3770d11ffc568d1bc4e037c99af6bcfd1cc448e015d7dc5603f1277f4a08b21f"
+  end
+
   depends_on "go" => :build
 
   def install
