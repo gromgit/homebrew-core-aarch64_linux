4e1357315e0d810690cdb99b83a64c3514f77d26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-17 07:10:27 +0000

    topgrade: update 6.9.1 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -6,10 +6,10 @@ class Topgrade < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3da7cf504c7a57aea479491572c6ad96d51277726a8974846998d5e25858cd6b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8fb57953c72284b268d9978c685a2529ae60b67e9f3b10b516a24605a27d9da6"
-    sha256 cellar: :any_skip_relocation, catalina:      "74ef76681d766111efa2cc6320aa1ccf98a40fe74b923349a78449c6ef532e9c"
-    sha256 cellar: :any_skip_relocation, mojave:        "3a0ca99c149b1a0f63f5f748564633673a58154e8e947fd8ac208baa24d38446"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4e0c11970d1aa7074249ba22055ad46cf8a1e438e15cd86dd49c2cb222280477"
+    sha256 cellar: :any_skip_relocation, big_sur:       "908af0d54fd7c7948d8b987ae8dc7d74c1eb63e0d4df193705b95289bfc2d274"
+    sha256 cellar: :any_skip_relocation, catalina:      "9790c78bcc07419629bae49f9658248673814247ef187b531fadee0bae28a077"
+    sha256 cellar: :any_skip_relocation, mojave:        "3f41637cc41cdb9867b49a9b17070bc504c54e3be8b97224857fb504bdeb403c"
   end
 
   depends_on "rust" => :build
