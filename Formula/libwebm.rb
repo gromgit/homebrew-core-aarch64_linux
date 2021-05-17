1de49362f2728064063e163a64f9b15579a6e3f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-17 16:10:08 +0000

    libwebm: update 1.0.0.28 bottle.

--- Formula/libwebm.rb
+++ Formula/libwebm.rb
@@ -6,14 +6,10 @@ class Libwebm < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1eb4f710924f50df98fc45e1f51305698daf70336198bdc74f551f2ea8e7edfa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "37c6bf256f4a45d3765d671c3a923875adfccd49d8c038ca1e07c6ba181341ac"
-    sha256 cellar: :any_skip_relocation, catalina:      "548a7393b909a559e08fbd6a0783ada345e0ef08b59f9b44588cc99d4420a040"
-    sha256 cellar: :any_skip_relocation, mojave:        "4238e3823e0e467e06492563d7f8c7603751419568ba621b6f644ad4ee5a30e6"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "0df7605cdc3aff926c0ffbcf5d72cf12933781083890eeeacc10df82b317b7c2"
-    sha256 cellar: :any_skip_relocation, sierra:        "36f647efcc9d72881ad8998df30e3268ec0b69b81c872fc381e3d7126fa2da6e"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "784418b8fc6006788c3a7c867cf675532fb7b86299ff9f8fb85d946c2e8cbc38"
-    sha256 cellar: :any_skip_relocation, yosemite:      "c6c99d02e47ed6ec17821ab9386e49b40ffad45e30f58fdbae62395dc16def18"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1b142108d31d44af8d338ae68873c2b215860c592c6bf14ae2c074c6f1624737"
+    sha256 cellar: :any_skip_relocation, big_sur:       "80dad8a52ceff398465dc0aa2c0a1cf4fd045cd0d82096c75e064d8fe2b947bc"
+    sha256 cellar: :any_skip_relocation, catalina:      "29cb3c088a8b5ae9c8e6e0c24b57709245e17bafb7d3413f7869f3bdca914c99"
+    sha256 cellar: :any_skip_relocation, mojave:        "d3bc6cbe15a957e620353e165589f81e1ae8065600bc696206efe6bc129cf8ce"
   end
 
   depends_on "cmake" => :build
