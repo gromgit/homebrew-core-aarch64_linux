de9334776fea956db1014791ccf87e48b95f6974
miccal <miccal.matthews@gmail.com>
2021-05-17 13:58:39 +0800

    openimageio: revision bump

--- Formula/openimageio.rb
+++ Formula/openimageio.rb
@@ -4,7 +4,7 @@ class Openimageio < Formula
   url "https://github.com/OpenImageIO/oiio/archive/Release-2.2.14.0.tar.gz"
   sha256 "e41b4b6958d318250caa1d1f167863a915a99062273583bb96457101d54e89cd"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
   head "https://github.com/OpenImageIO/oiio.git"
 
   livecheck do
