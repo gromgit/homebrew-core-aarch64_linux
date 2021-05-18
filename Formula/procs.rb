f7da5d02ada436a84ba355678953e0d2e3e55a2d
Julian Berman <Julian@GrayVines.com>
2021-05-18 18:55:27 -0400

    procs: style improvements
    Closes #77365.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -17,16 +17,16 @@ class Procs < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    system "#{bin}/procs", "--completion", "bash"
-    system "#{bin}/procs", "--completion", "fish"
-    system "#{bin}/procs", "--completion", "zsh"
+    system bin/"procs", "--completion", "bash"
+    system bin/"procs", "--completion", "fish"
+    system bin/"procs", "--completion", "zsh"
     bash_completion.install "procs.bash" => "procs"
     fish_completion.install "procs.fish"
     zsh_completion.install "_procs"
   end
 
   test do
-    output = shell_output("#{bin}/procs")
+    output = shell_output(bin/"procs")
     count = output.lines.count
     assert count > 2
     assert output.start_with?(" PID:")
