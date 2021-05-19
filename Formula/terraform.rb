3c38105d953366c7ae4ebf25ae17860ba567743b
Rui Chen <rui@chenrui.dev>
2021-05-19 15:33:53 -0400

    terraform 0.15.4
    Closes #77621.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,8 +1,8 @@
 class Terraform < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/hashicorp/terraform/archive/v0.15.3.tar.gz"
-  sha256 "173487c8d305052461266d710bb1353590fee1f312ce714e2aa3423c3f2f1937"
+  url "https://github.com/hashicorp/terraform/archive/v0.15.4.tar.gz"
+  sha256 "c94add2b677d6cbc2ca88d9eb154bdca0df26cdd397d44953a3ac9cdf7305480"
   license "MPL-2.0"
   head "https://github.com/hashicorp/terraform.git"
 
