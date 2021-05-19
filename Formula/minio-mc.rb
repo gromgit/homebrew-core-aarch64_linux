efc44a0d73efdce5dff5c04f2b6414f9afe5a1c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-19 09:13:25 +0000

    minio-mc: update 20210518033944 bottle.

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -17,10 +17,10 @@ class MinioMc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e04118a9ca544a31758eab246f50357486729325136147359c5051ee331fcc4b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "eb3f9e7a0c4288b942623e4acc437e3b84a98e73b290ce51e7f629b21c05fbfc"
-    sha256 cellar: :any_skip_relocation, catalina:      "bd172c657d4245f1e12174bc1940651f4d37811c4bd9a5543ad43ec5565badb7"
-    sha256 cellar: :any_skip_relocation, mojave:        "8a5da007670a35ccda8f9c31183fe3130b5ae6b87f5b49609863f6354e863869"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5d89f39198093c04601d3318e94b4cfc09a9bddfcbca25b967b4688ec04b381d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "45456e0e492ba4f9030cbf51472251c4ecc1c7a925dbbfd38d49dad96fd7c14a"
+    sha256 cellar: :any_skip_relocation, catalina:      "c8cb3b8668d4b442ff77115e14cd88fd6e0c537e2fa6cdd5bd0ef7ab98166366"
+    sha256 cellar: :any_skip_relocation, mojave:        "c8b6403b19ae655535db32b1cb02a5e7698272a1b9ae8cb8feba973bcdd0d1b1"
   end
 
   depends_on "go" => :build
