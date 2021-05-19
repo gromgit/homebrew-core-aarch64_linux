2c4481e59412173f4341e7004f32337aae6edb3c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-19 21:50:28 +0000

    ehco: add 1.0.6 bottle.

--- Formula/ehco.rb
+++ Formula/ehco.rb
@@ -5,6 +5,13 @@ class Ehco < Formula
   sha256 "326c468c3790ad01031e52ccb4efcfa5e331d2198fdb13137749c67e8eaacf38"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f5bb2716a01d4cede9dfdf1bdcf6f1170ac5ada0647cf55ac13d313e4af61489"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0735f491493dc56c2655dba4c1cdbe585860bd3e9cd934c00341cce9d836352a"
+    sha256 cellar: :any_skip_relocation, catalina:      "1010c92ff010f913a575284a644707b835bf842dedf4850132ee161dd52ec0e1"
+    sha256 cellar: :any_skip_relocation, mojave:        "de0558b7bc58fd084626cb1423c90a3fc74ab4f803b3db9142b8fa36cd713c9a"
+  end
+
   depends_on "go" => :build
 
   def install
