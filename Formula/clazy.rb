c99d377c132954a567931d7600bcbd89f0982b0d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-19 14:27:14 +0100

    clazy: depend on `llvm@11`
    This still has linkage with `llvm@11` despite the formula going out of
    its way to avoid it.
    
    Unfortunately, this gets missed by `brew linkage --test`. See
    Homebrew/hombrew-test-bot#611.
    
    Closes #77602.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clazy.rb
+++ Formula/clazy.rb
@@ -4,7 +4,7 @@ class Clazy < Formula
   url "https://download.kde.org/stable/clazy/1.9/src/clazy-1.9.tar.xz"
   sha256 "4c6c2e473e6aa011cc5fab120ebcffec3fc11a9cc677e21ad8c3ea676eb076f8"
   license "LGPL-2.0-or-later"
-  revision 1
+  revision 2
   head "https://invent.kde.org/sdk/clazy.git"
 
   livecheck do
@@ -20,18 +20,15 @@ class Clazy < Formula
   end
 
   depends_on "cmake"   => [:build, :test]
-  depends_on "llvm@11" => [:build, :test]
   depends_on "qt"      => :test
-
   depends_on "coreutils"
+  depends_on "llvm@11"
 
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
   def install
-    # avoid use llvm libc++
-    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm@11"].opt_lib
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
