7cd6474dbf1a27f16d4a771dfaf503602057417e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-19 08:13:59 +0000

    osrm-backend: update 5.25.0 bottle.

--- Formula/osrm-backend.rb
+++ Formula/osrm-backend.rb
@@ -12,10 +12,10 @@ class OsrmBackend < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "fe1d2680e22150a35b6d5c76157c867e3faed556bb127fe2b57eff79858ac668"
-    sha256 cellar: :any, big_sur:       "e6dfd1fc4adacbf0b54be1b1c10218fa18b6ea186ea802df3791a023b95f5705"
-    sha256 cellar: :any, catalina:      "fd6cad0c66a91a963006b7f961d3fbdce9d4ecf47acb4c578f23602f8025512e"
-    sha256 cellar: :any, mojave:        "b4dd6eb3f99a2caf3f733beb93046bb69813a3b3b32cb73410527b03e20a5c93"
+    sha256 cellar: :any, arm64_big_sur: "dc65b0d0046277182590cc6d3b16b5436a637a0c87d0d6ca3b81bf4ee4aa138a"
+    sha256 cellar: :any, big_sur:       "5d461962ca9a86f733ee2223080fd63c9ef1bd1c0fbd6820d29dfe1ba46b1f75"
+    sha256 cellar: :any, catalina:      "07166bddc5e210fd5c697c9dd168667522c83260c143a2f956dcf079507e0141"
+    sha256 cellar: :any, mojave:        "002af11b6c8a248e91417f997eb13d9a6a624bbd34795ba961ff48e184f872a5"
   end
 
   depends_on "cmake" => :build
