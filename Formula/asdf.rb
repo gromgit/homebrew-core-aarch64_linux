69ad150e39f453110f5c99f6913d6368f549b82a
Rui Chen <rui@chenrui.dev>
2021-05-18 20:26:30 -0400

    asdf 0.8.1
    * asdf 0.8.1
    * update test
    
    Closes #77524.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Caleb Xu <3536482+alebcay@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -1,10 +1,9 @@
 class Asdf < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://asdf-vm.com/"
-  url "https://github.com/asdf-vm/asdf/archive/v0.8.0.tar.gz"
-  sha256 "9b667ca135c194f38d823c62cc0dc3dbe00d7a9f60caa0c06ecb3047944eadfa"
+  url "https://github.com/asdf-vm/asdf/archive/v0.8.1.tar.gz"
+  sha256 "6ca280287dcb687ec12f0c37e4e193de390cdab68f2b2a0e271e3a4f1e20bd2e"
   license "MIT"
-  revision 1
   head "https://github.com/asdf-vm/asdf.git"
 
   bottle do
@@ -34,7 +33,7 @@ class Asdf < Formula
 
   test do
     output = shell_output("#{bin}/asdf plugin-list 2>&1", 1)
-    assert_match "Oohes nooes ~! No plugins installed", output
+    assert_match "No plugins installed", output
     assert_match "Update command disabled.", shell_output("#{bin}/asdf update", 42)
   end
 end
