ca683a631dee04a7cceb3140d721af8004ed85a5
rui <chenrui333@gmail.com>
2021-05-20 10:40:26 -0400

    wildfly-as 23.0.2 (#76283)
    * wildfly-as 23.0.2
    
    * wildfly-as: remove `bottle :unneeded`
    
    See #75943.
    
    * use `bottle :unneeded` for now
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>

--- Formula/wildfly-as.rb
+++ Formula/wildfly-as.rb
@@ -1,8 +1,8 @@
 class WildflyAs < Formula
   desc "Managed application runtime for building applications"
   homepage "https://www.wildfly.org/"
-  url "https://download.jboss.org/wildfly/23.0.1.Final/wildfly-23.0.1.Final.tar.gz"
-  sha256 "5622e62bf210f983d1678fd14b2d218b13446ca83a80f04bbe8cdc6ea276945e"
+  url "https://download.jboss.org/wildfly/23.0.2.Final/wildfly-23.0.2.Final.tar.gz"
+  sha256 "6525f6372a8dbddb84d7e3a466dbef1e046253c2bcd682c29fd0f4c1ec606fc4"
   license "LGPL-2.1-or-later"
 
   livecheck do
