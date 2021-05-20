0c896eefb08a8af6c3371f86c75c6cb53d64a56a
Alexander Bayandin <a.bayandin@gmail.com>
2021-05-20 10:52:46 +0000

    php-cs-fixer@2: add 2.19.0 bottle.

--- Formula/php-cs-fixer@2.rb
+++ Formula/php-cs-fixer@2.rb
@@ -5,6 +5,10 @@ class PhpCsFixerAT2 < Formula
   sha256 "dcaf7556479a1cfc7ece11a203c4247d61fabe86e509b0e29539111f6b82018c"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "5f80ed35e0644fa2eafc81ab81b4320187f3257c0819fd43e70e6f7cc2003b3d"
+  end
+
   keg_only :versioned_formula
   uses_from_macos "php", since: :el_capitan
 
