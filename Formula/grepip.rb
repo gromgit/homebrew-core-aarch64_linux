e42576f4fa1e50fcf03e12b95c5f9da5c9982e83
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-20 09:11:41 +0000

    grepip: add 1.0.2 bottle.

--- Formula/grepip.rb
+++ Formula/grepip.rb
@@ -5,6 +5,13 @@ class Grepip < Formula
   sha256 "f9258f9f5c2bfdb4278c6cd456072664be7588fc6a8b665d35cbd6fa29f57568"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4eb71156e81900093ab56725a8f8936139f379b342493c706843c14f868260c6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "262686a16d0c33db482872981fba872a57d40a0f3c5c87e84eb61ca7209c14f9"
+    sha256 cellar: :any_skip_relocation, catalina:      "96e604188f0f2eaaf4071cee4fd71deefcd3600356543d1c62d2e92a3e32cba4"
+    sha256 cellar: :any_skip_relocation, mojave:        "4a24e339d049473574048ee2167644ec77ed7f5b965b4f7b02fbcddd98b39166"
+  end
+
   depends_on "go" => :build
 
   def install
