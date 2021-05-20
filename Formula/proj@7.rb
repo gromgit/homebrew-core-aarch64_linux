a269262b96de0b8e1fb63ff1ac6f4b3352dea7db
Alexander Bayandin <a.bayandin@gmail.com>
2021-05-20 18:11:42 +0100

    proj@7 7.2.1 (new formula)

---
+++ Formula/proj@7.rb
@@ -0,0 +1,50 @@
+class ProjAT7 < Formula
+  desc "Cartographic Projections Library"
+  homepage "https://proj.org/"
+  url "https://github.com/OSGeo/PROJ/releases/download/7.2.1/proj-7.2.1.tar.gz"
+  sha256 "b384f42e5fb9c6d01fe5fa4d31da2e91329668863a684f97be5d4760dbbf0a14"
+  license "MIT"
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2020-03-01", because: :unsupported
+
+  depends_on "pkg-config" => :build
+  depends_on "libtiff"
+
+  uses_from_macos "curl"
+  uses_from_macos "sqlite"
+
+  skip_clean :la
+
+  # The datum grid files are required to support datum shifting
+  resource "datumgrid" do
+    url "https://download.osgeo.org/proj/proj-datumgrid-1.8.zip"
+    sha256 "b9838ae7e5f27ee732fb0bfed618f85b36e8bb56d7afb287d506338e9f33861e"
+  end
+
+  def install
+    (buildpath/"nad").install resource("datumgrid")
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test").write <<~EOS
+      45d15n 71d07w Boston, United States
+      40d40n 73d58w New York, United States
+      48d51n 2d20e Paris, France
+      51d30n 7'w London, England
+    EOS
+    match = <<~EOS
+      -4887590.49\t7317961.48 Boston, United States
+      -5542524.55\t6982689.05 New York, United States
+      171224.94\t5415352.81 Paris, France
+      -8101.66\t5707500.23 London, England
+    EOS
+
+    output = shell_output("#{bin}/proj +proj=poly +ellps=clrk66 -r #{testpath}/test")
+    assert_equal match, output
+  end
+end
