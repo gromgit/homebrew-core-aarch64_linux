f08ed788c717e9f5e56fb17741ce6ebcd3549984
Nanda H Krishna <me@nandahkrishna.com>
2021-05-20 23:14:10 +0000

    marp-cli: add 1.1.1 bottle.

--- Formula/marp-cli.rb
+++ Formula/marp-cli.rb
@@ -7,6 +7,13 @@ class MarpCli < Formula
   sha256 "62711d9298f96a9634020d31a05561ad076e59e06749b0be643a20ccb8533770"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c7497d7d226b8ace2ba4f24a231c946d13683acdf9ec40a91fd37c66e6e80aee"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9fbc8ace15dc205f82033d8636df4c56e96fea5c544557ec114753fce91cadc4"
+    sha256 cellar: :any_skip_relocation, catalina:      "9fbc8ace15dc205f82033d8636df4c56e96fea5c544557ec114753fce91cadc4"
+    sha256 cellar: :any_skip_relocation, mojave:        "9fbc8ace15dc205f82033d8636df4c56e96fea5c544557ec114753fce91cadc4"
+  end
+
   depends_on "node"
 
   def install
