fe87614ef2fd83055e54ee2de3edbcd198ca7a24
Caleb Xu <calebcenter@live.com>
2021-05-20 01:26:48 -0400

    flyway 7.9.1
    Closes #77655.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.9.0/flyway-commandline-7.9.0.tar.gz"
-  sha256 "b6f8f27ee96f6d882a5de539f8604508c830747d89c95c3ea80def17da358dcd"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.9.1/flyway-commandline-7.9.1.tar.gz"
+  sha256 "404b56983e0db44544dae1c5c0771e1b25fe9e4a7d5f95450c75152a5a167c81"
   license "Apache-2.0"
 
   livecheck do
