b1414fe477da050156eae65659f58d039cf57ea7
Michael Cho <cho-m@tuta.io>
2021-05-21 12:29:37 -0700

    mkvalidator 0.6.0
    Closes #77763.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mkvalidator.rb
+++ Formula/mkvalidator.rb
@@ -1,8 +1,9 @@
 class Mkvalidator < Formula
   desc "Tool to verify Matroska and WebM files for spec conformance"
   homepage "https://www.matroska.org/downloads/mkvalidator.html"
-  url "https://downloads.sourceforge.net/project/matroska/mkvalidator/mkvalidator-0.5.2.tar.bz2"
-  sha256 "2e2a91062f6bf6034e8049646897095b5fc7a1639787d5fe0fcef1f1215d873b"
+  url "https://downloads.sourceforge.net/project/matroska/mkvalidator/mkvalidator-0.6.0.tar.bz2"
+  sha256 "f9eaa2138fade7103e6df999425291d2947c5355294239874041471e3aa243f0"
+  license "BSD-3-Clause"
 
   livecheck do
     url :stable
@@ -18,21 +19,17 @@ class Mkvalidator < Formula
     sha256 cellar: :any_skip_relocation, el_capitan:  "6c253cdf3c824b6e37af7cca51bf05a930785286bc83ec367e10500d9645519c"
   end
 
+  depends_on "cmake" => :build
+
   resource "tests" do
     url "https://github.com/dunn/garbage/raw/c0e682836e5237eef42a000e7d00dcd4b6dcebdb/test.mka"
     sha256 "6d7cc62177ec3f88c908614ad54b86dde469dbd2b348761f6512d6fc655ec90c"
   end
 
   def install
-    ENV.deparallelize # Otherwise there are races
-
-    # Reported 2 Nov 2017 https://github.com/Matroska-Org/foundation-source/issues/31
-    inreplace "configure", "\r", "\n"
-
-    system "./configure"
+    system "cmake", ".", *std_cmake_args
     system "make", "-C", "mkvalidator"
-    bindir = `corec/tools/coremake/system_output.sh`.chomp
-    bin.install "release/#{bindir}/mkvalidator"
+    bin.install "mkvalidator/mkvalidator"
   end
 
   test do
