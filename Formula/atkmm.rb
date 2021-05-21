87f83193fd1da34c088a27b640e116152ba70e00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-21 11:07:12 +0000

    atkmm: update 2.36.1 bottle.

--- Formula/atkmm.rb
+++ Formula/atkmm.rb
@@ -6,10 +6,10 @@ class Atkmm < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1b1aabce33bb159b047e9a7f39164f0969832fda522d79b3a2b70c642323869e"
-    sha256 cellar: :any, big_sur:       "5089524e732962aac25fb780a671bdb8736cde11dd8565adeeab2f23fcfe9d8b"
-    sha256 cellar: :any, catalina:      "5a92889fa4d5326f5a750d076f02b9b352da60896328f2e20f16da813f442c98"
-    sha256 cellar: :any, mojave:        "169e793e210a156583b3bf6126fb4f1e049f7ca0113ba2a6a4c37efb0d38a458"
+    sha256 cellar: :any, arm64_big_sur: "82e89f908e2a3cc349c8478eef60d7b755f968d3523f037b8b78ea4edc4031ba"
+    sha256 cellar: :any, big_sur:       "4b147e97566fe8f13c6e72e1d350da5c8bbabe3abb6f4966eec44f227c9fa807"
+    sha256 cellar: :any, catalina:      "09b0e72f5d9fd8e2a3328202d8c2b8fc7f896cdd208ab51fa75bb87c79eee53d"
+    sha256 cellar: :any, mojave:        "a3ed94c51be9ae22f3a3c1d6650ade4037748f2c8602db9b647effd71b4d5108"
   end
 
   depends_on "meson" => :build
