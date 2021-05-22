8fecf514a8fd3bc58d682814a0c389b898a63a8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-22 16:03:00 +0000

    daemon: update 0.8 bottle.

--- Formula/daemon.rb
+++ Formula/daemon.rb
@@ -11,14 +11,10 @@ class Daemon < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a634d876fc4382b34bfd5f3b564a62a251e3eb3b6d07ab03a9c27f25617c44f5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2d16b2615f5df9838e2d75351fae720910beae254d62f0970b64cddedeb289b8"
-    sha256 cellar: :any_skip_relocation, catalina:      "8cc2278936a35f9ae2c0952e4be5c9e06970386f3c9c5ae528b18c69902e9220"
-    sha256 cellar: :any_skip_relocation, mojave:        "0fd225e226dd07c3f51836f47bf9829dd095a46a13a5b78c3a0e9df3c5820683"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "ca2b1016c1bbe48002f70b7beb86063943dadabcb670db9f90f1c259cb34d623"
-    sha256 cellar: :any_skip_relocation, sierra:        "bfc116e8f0853cdf5b4abc38b1f000c90708823bf49c5237f8ec453400a5d606"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "ad4f8ad9e7deeb0039c6c603b0108fb6733abe425c49fa6344f762e26b49cf2d"
-    sha256 cellar: :any_skip_relocation, yosemite:      "f48000af3631f28d47d01d3d89a1f03e7c4f7eac4a81ab7db9c38a1ce9ff66cd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c41545411b2f4b31dc059df14eb622bfd30586c1f2bd3af7f4e0514bb5001277"
+    sha256 cellar: :any_skip_relocation, big_sur:       "224349f2fd389edc70b78ef94b2df74251da1c659792b0dea21b9955fb57efae"
+    sha256 cellar: :any_skip_relocation, catalina:      "e631aea609e5e1b623b39b735c0a76e391ba117c920ff5fc9d185ff8e9ea332f"
+    sha256 cellar: :any_skip_relocation, mojave:        "df18db1a4c13107967c7e78e398b658823036279e4baf0c94ea5504d298f2d0c"
   end
 
   def install
