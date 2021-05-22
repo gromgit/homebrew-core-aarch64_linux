1823f56b70500b7688244f96dc3fc712dd8d09bf
Michael Cho <cho-m@tuta.io>
2021-05-22 13:09:43 -0700

    gradle: downgrade to openjdk@11 for ARM (upcoming jdk17-ea)
    Closes #77855.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -4,7 +4,7 @@ class Gradle < Formula
   url "https://services.gradle.org/distributions/gradle-7.0.2-all.zip"
   sha256 "13bf8d3cf8eeeb5770d19741a59bde9bd966dd78d17f1bbad787a05ef19d1c2d"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://services.gradle.org/distributions/"
@@ -15,12 +15,22 @@ class Gradle < Formula
     sha256 cellar: :any_skip_relocation, all: "0d659be86c309f4ac28003a0593d366f3c0fd9e85eb14d34542e6c7545fe5a6a"
   end
 
-  depends_on "openjdk"
+  # gradle currently does not support Java 17
+  if Hardware::CPU.arm?
+    depends_on "openjdk@11"
+  else
+    depends_on "openjdk"
+  end
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin docs lib src]
-    (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
+    env = if Hardware::CPU.arm?
+      Language::Java.overridable_java_home_env("11")
+    else
+      Language::Java.overridable_java_home_env
+    end
+    (bin/"gradle").write_env_script libexec/"bin/gradle", env
   end
 
   test do
