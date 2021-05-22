94b9bd225933ccf4e207bc6b2ed4e55e13c30ece
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-22 16:09:01 +0000

    libspatialite: update 5.0.1_1 bottle.

--- Formula/libspatialite.rb
+++ Formula/libspatialite.rb
@@ -14,10 +14,10 @@ class Libspatialite < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "18ecddab7cd064038e15a9e023fc1cb94dfabcb4ea1dbd527e19754c80829445"
-    sha256 cellar: :any, big_sur:       "eea8b32b264f5183558158fe3d76cfd676bb731620f67fc1da66519f28171b6b"
-    sha256 cellar: :any, catalina:      "054c1286dcdffded43a8cf768eac32c4f20e9870bc0b0e0aa04c40fb31280836"
-    sha256 cellar: :any, mojave:        "c136e8503fda01a139f00fbc6ac5e3b24989961a17485c28e231b355090e98e4"
+    sha256 cellar: :any, arm64_big_sur: "dc8b7f36b4f7aba60fdb088ec17ca69bddb353fed99a1e6d0200590a0ee0adb2"
+    sha256 cellar: :any, big_sur:       "f740afb58268b4babe07a123e1b7857b754abea2ffced3b37b8edf7140408652"
+    sha256 cellar: :any, catalina:      "abf71f3882743f1bebf560c66a0f211f0298f0498ed0093b29d1feaa6c5c425f"
+    sha256 cellar: :any, mojave:        "64a4c76c3cc6aa7ae20c510a4688182af14bb9f1da38bed1409dcd6a1a333a5d"
   end
 
   head do
