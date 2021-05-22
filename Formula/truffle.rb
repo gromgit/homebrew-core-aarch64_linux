2d4d7b2e8e7a07e3a9a79dfcda7842f9df2141dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-22 10:07:16 +0000

    truffle: update 5.3.7 bottle.

--- Formula/truffle.rb
+++ Formula/truffle.rb
@@ -8,10 +8,10 @@ class Truffle < Formula
   license "MIT"
 
   bottle do
-    sha256 arm64_big_sur: "f89c21f5ecc30608673cdce8ccaf62178d73e69d5475628d7bc5a4947ac10c3f"
-    sha256 big_sur:       "764b3a67eed44ac7b90ec63312326251abed167da1c421a88e7e03d740a659b1"
-    sha256 catalina:      "914a221b5d35993a6f11d17253a25ab26af930e7ab842665fc2b185ab3a6de19"
-    sha256 mojave:        "fd5eaf735352faa4430a4437b4f5b865e7d545ca164daebec86a8ca141327ab9"
+    sha256 arm64_big_sur: "7141e78fcc75b403ee9aca5ef63b02f4d98158d0af745257ecbd456f675e17ef"
+    sha256 big_sur:       "a0cd17bbf6ec536b9653cf3e36fd8d12ee74406e4e8ecc2f196b5975ba85a785"
+    sha256 catalina:      "0b13f1c52c929b04079109689ecc69c2f114e9d12ec0b5b787e2a6f61f6becf0"
+    sha256 mojave:        "fc45be69ecbe080cc00421012d5da7aabaa269ee4ff571934bf675dd856c9c3c"
   end
 
   depends_on "node"
