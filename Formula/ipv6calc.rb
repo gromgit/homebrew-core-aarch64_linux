950f15ee40b32bfa9b7cb5537d2c36f61e3cd879
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-22 19:14:58 +0000

    ipv6calc: update 3.1.1 bottle.

--- Formula/ipv6calc.rb
+++ Formula/ipv6calc.rb
@@ -6,10 +6,10 @@ class Ipv6calc < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e17e06249b6fe16ec89c97604f933feae4f2b796ea74b2f7affe932f9c314a41"
-    sha256 cellar: :any_skip_relocation, big_sur:       "096cdd06c1d9296e2a823fcb8fd9318261f3e9114cd95c7acdddbbc967445f11"
-    sha256 cellar: :any_skip_relocation, catalina:      "f802589e58b5a2b28357418bd919058db4ddc4cadd9f27f4e67f1c892e63a9dd"
-    sha256 cellar: :any_skip_relocation, mojave:        "d3916c02853129681d10f5d16772314fbd0243ab9641784fba9484009c0cced7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8eeb311271874e67e665e0c1cc32fafd38805a4a81d30ea31197e1c2ceaf104c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c1bbb3371e2bf0c3364dc9edd87152aadb85e75342535646a7022f7324b607e0"
+    sha256 cellar: :any_skip_relocation, catalina:      "cbbfa59bb3d93e1954dd7c6ecb718baddb1063c6929d57655313c2f4fb29d88c"
+    sha256 cellar: :any_skip_relocation, mojave:        "20e5085d6ef92183355f83b5aa2097d4b740685dbfb2143ac355bc49bfe51eff"
   end
 
   def install
