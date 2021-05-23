9653858cc0db26793a504efab2ea74bba968b12d
Rylan Polster <rslpolster@gmail.com>
2021-05-23 11:54:05 -0400

    swift-format: replace `pour_bottle?` block with symbol

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -20,10 +20,7 @@ class SwiftFormat < Formula
 
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
-  pour_bottle? do
-    reason "The bottle needs the Xcode CLT to be installed."
-    satisfy { MacOS::CLT.installed? }
-  end
+  pour_bottle? only_if: :clt_installed
 
   depends_on xcode: ["12.5", :build]
 
