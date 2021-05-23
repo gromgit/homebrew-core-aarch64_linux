d09456d3e28d5a17773a311cff66f7b56a61b528
Rylan Polster <rslpolster@gmail.com>
2021-05-23 11:54:01 -0400

    gcc@5: replace `pour_bottle?` block with symbol

--- Formula/gcc@5.rb
+++ Formula/gcc@5.rb
@@ -17,12 +17,7 @@ class GccAT5 < Formula
 
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
-  pour_bottle? do
-    on_macos do
-      reason "The bottle needs the Xcode CLT to be installed."
-      satisfy { MacOS::CLT.installed? }
-    end
-  end
+  pour_bottle? only_if: :clt_installed
 
   depends_on maximum_macos: [:high_sierra, :build]
 
