d0a029701786d9b5d044e3d3e944e1217adcfac9
Rylan Polster <rslpolster@gmail.com>
2021-05-23 11:54:03 -0400

    llvm: replace `pour_bottle?` block with symbol

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -21,12 +21,7 @@ class Llvm < Formula
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
-  pour_bottle? do
-    on_macos do
-      reason "The bottle needs the Xcode CLT to be installed."
-      satisfy { MacOS::CLT.installed? }
-    end
-  end
+  pour_bottle? only_if: :clt_installed
 
   keg_only :provided_by_macos
 
