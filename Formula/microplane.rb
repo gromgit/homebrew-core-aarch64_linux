60808a64329ade786c1fd7717429d1936cab4b67
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-23 20:05:43 +0000

    microplane: update 0.0.32 bottle.

--- Formula/microplane.rb
+++ Formula/microplane.rb
@@ -7,10 +7,10 @@ class Microplane < Formula
   head "https://github.com/Clever/microplane.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1b7a872f754154279db15e1a0b5a046556ed066df8651031245afcbcacea17c4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8c10da9b7a68a8d6c055a95227581f35641c787d165be6e250bd12061fd51a8d"
-    sha256 cellar: :any_skip_relocation, catalina:      "4138178d221fcdae854a4afd3a0b5de02206e2a31be5579d80ac0406cab19342"
-    sha256 cellar: :any_skip_relocation, mojave:        "1a98222eb9f5ef3044c64ca0c6031a1f976cca5d2a3d54f56fe1df98bcc3889e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3d696707ea7c919e4ae08ddc692c5f9d41dfa638da82d5d307bc968669312619"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bd74b43fb93f9c547805597ccb637ef797952dfd5d7727af0ee89a3badc3509c"
+    sha256 cellar: :any_skip_relocation, catalina:      "5b44d11cef464106edb0127d85a3588e3dde5b5353a2e7ea4aeca47779a22eac"
+    sha256 cellar: :any_skip_relocation, mojave:        "e381893ee34523c3997b743518433f69cbac74abfd5bf7eab70d0eb16439ca05"
   end
 
   depends_on "go" => :build
