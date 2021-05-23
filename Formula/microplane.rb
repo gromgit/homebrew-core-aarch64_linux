1ef37d50f0c736ed95c09e567c384df23435c065
Rui Chen <rui@chenrui.dev>
2021-05-23 14:34:58 -0400

    microplane 0.0.32
    Closes #77883.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/microplane.rb
+++ Formula/microplane.rb
@@ -1,8 +1,8 @@
 class Microplane < Formula
   desc "CLI tool to make git changes across many repos"
   homepage "https://github.com/Clever/microplane"
-  url "https://github.com/Clever/microplane/archive/v0.0.31.tar.gz"
-  sha256 "acd5410c433498f081fe5bd51609734ce5715f675738d5955da9764774954177"
+  url "https://github.com/Clever/microplane/archive/v0.0.32.tar.gz"
+  sha256 "0332ada57696caab83699784125881b1a0341d6dbaad113de4ade37b188653db"
   license "Apache-2.0"
   head "https://github.com/Clever/microplane.git"
 
