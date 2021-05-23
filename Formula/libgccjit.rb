83ed7af67161e8a50bfdcbb7177b387be6298c9f
Rylan Polster <rslpolster@gmail.com>
2021-05-23 11:54:03 -0400

    libgccjit: replace `pour_bottle?` block with symbol

--- Formula/libgccjit.rb
+++ Formula/libgccjit.rb
@@ -36,10 +36,7 @@ class Libgccjit < Formula
 
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
-  pour_bottle? do
-    reason "The bottle needs the Xcode CLT to be installed."
-    satisfy { MacOS::CLT.installed? }
-  end
+  pour_bottle? only_if: :clt_installed
 
   depends_on "gcc" => :test
   depends_on "gmp"
