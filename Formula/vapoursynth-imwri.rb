79e419783e95388f2d8e1f0a39c31710d27bac30
Rui Chen <rui@chenrui.dev>
2021-05-23 19:40:01 -0400

    vapoursynth-imwri 53

--- Formula/vapoursynth-imwri.rb
+++ Formula/vapoursynth-imwri.rb
@@ -1,10 +1,9 @@
 class VapoursynthImwri < Formula
   desc "VapourSynth filters - ImageMagick HDRI writer/reader"
   homepage "http://www.vapoursynth.com"
-  url "https://github.com/vapoursynth/vapoursynth/archive/R52.tar.gz"
-  sha256 "4d5dc7950f4357da695d29708bc98013bc3e0bd72fc5d697f8c91ce3c4a4b2ac"
+  url "https://github.com/vapoursynth/vapoursynth/archive/R53.tar.gz"
+  sha256 "78e2c5311b2572349ff7fec2e16311e9e4f6acdda78673872206ab660eadf7c8"
   license "LGPL-2.1-or-later"
-  revision 2
   head "https://github.com/vapoursynth/vapoursynth.git"
 
   bottle do
