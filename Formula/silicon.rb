3a833d18d31b5be02c51f67edf5a4f93c11b44e1
Alexander Bayandin <a.bayandin@gmail.com>
2021-05-24 11:00:24 +0000

    silicon: update 0.4.1 bottle.

--- Formula/silicon.rb
+++ Formula/silicon.rb
@@ -6,10 +6,10 @@ class Silicon < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:  "52f4652a0e234859610c0fe2ff510c6360548af5b275182fe42d1db55434d3ee"
-    sha256 cellar: :any_skip_relocation, catalina: "3ed95061624448f3cdb724c2ba8e195fabfcbee846fd66dfab08167bc748b562"
-    sha256 cellar: :any_skip_relocation, mojave:   "05a64aecf5f99a0e195aee75422356f0246381df8c11c31b891b223dab487ab7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d258ffeb373ddd0d6b6bd36660fdd45a36564536c1fb4f43db5c8f1cc0ae8e28"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ed9e6e76ee8b694818de87106ee9c9608781ac5d18f4744ea8d197ba61664fcb"
+    sha256 cellar: :any_skip_relocation, catalina:      "59cd0ab050ed82e525ea507063e26c025de1b18cc7a5eac82119f6a4e42747b0"
+    sha256 cellar: :any_skip_relocation, mojave:        "4b458312692e17e124910bc35b093da0bf6f45909d3ab0bf06ab74816b27961b"
   end
 
   depends_on "rust" => :build
