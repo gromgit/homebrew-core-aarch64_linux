0130a831a578abeaf486806b107f52bd1f7b3dd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-24 15:07:57 +0000

    qmmp: update 1.5.0 bottle.

--- Formula/qmmp.rb
+++ Formula/qmmp.rb
@@ -20,9 +20,9 @@ class Qmmp < Formula
   end
 
   bottle do
-    sha256 big_sur:  "314ceb6432da04c39b83c600d22bafee2a680b6c34ef47c238e160181583e7bb"
-    sha256 catalina: "53def71f6922fb2f7f7cfd3557d321011f82015d8d7a259c5aac0f17d0da986b"
-    sha256 mojave:   "2471e481be569677e931cb5588e53d9359bb3ff8b403b8a0051d8c77ff4e7702"
+    sha256 big_sur:  "9dee25c49a89bedc44b8a45cce9c1eee237f0d04b2765a930e2482baed663f3c"
+    sha256 catalina: "3a59c33536865917e8426e7076d6e33aa465add0f0d89a60edb1dded6ef475ab"
+    sha256 mojave:   "7ff2b3f1d2b6adb30d9a8051e1c19e822eaac3643483d16150a15116d0179879"
   end
 
   depends_on "cmake" => :build
