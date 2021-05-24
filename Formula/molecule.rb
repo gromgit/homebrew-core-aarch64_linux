13edd2d2f24900945a107ac47a235a6e990ea56f
Nanda H Krishna <me@nandahkrishna.com>
2021-05-24 04:59:35 +0000

    molecule: update 3.3.1 bottle.

--- Formula/molecule.rb
+++ Formula/molecule.rb
@@ -8,10 +8,11 @@ class Molecule < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1b50d8f192667233746d1937e13f35b8dd81106a262c67edffcec3ed31122077"
-    sha256 cellar: :any, big_sur:       "7f9027ffbd422c407d60f559690329cfbaaaf3e953b1bed8d0aa9c68d2ed96ea"
-    sha256 cellar: :any, catalina:      "28de17a489575812547782b2bf7947faf4d81f46a5d7299ea007020d43adaac0"
-    sha256 cellar: :any, mojave:        "9ef3240ad149d4f885af9f1a5139b74f9dd084c26475df9b2693b2f9dff32585"
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "1d5de2bc92509056999e4d4b67c45d9cbd6bc3c119ba82f968083fad31df04ea"
+    sha256 cellar: :any, big_sur:       "47ba5fad180a90b63128bd9082ebb9cd36d14c283db7ec1c9ffd16c56f4c9ba6"
+    sha256 cellar: :any, catalina:      "cbe3109e96ef69ac66ced418e9371833b43d56a834fb174cb5ec08e4abc474d7"
+    sha256 cellar: :any, mojave:        "07e705ecd5510ff37ca6b9643213430f48e28e6ca66d75eb5658e566e7567081"
   end
 
   depends_on "rust" => :build
