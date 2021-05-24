f6fa3fdb79a317dac256df11b63e0d0a2f1ea876
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-24 01:34:08 +0000

    sassc: update 3.6.2 bottle.

--- Formula/sassc.rb
+++ Formula/sassc.rb
@@ -8,12 +8,10 @@ class Sassc < Formula
   head "https://github.com/sass/sassc.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c38dc9980bd3b118217bbb51b44685ee1cb8338b6c7dabac2a34e0ad0cda18ad"
-    sha256 cellar: :any, big_sur:       "81448c2610a270d7a77a24f63c4f587a81e1b03186456a3dd8345f002a5474fc"
-    sha256 cellar: :any, catalina:      "b3e76afb48cf7789113e29f01389bdc5d7f13faf3e9b7f28f4bf9ef352363b0f"
-    sha256 cellar: :any, mojave:        "34e0739d4967b537d4836780cefcb47910ce8c5b8201e9f864d10c3d34801237"
-    sha256 cellar: :any, high_sierra:   "4461eb8cf88f6fbbfe0d15b0efd0449cc95e3e46873af0f972769594786c32ea"
-    sha256 cellar: :any, sierra:        "e54c8d0ddc93a8212c9c39f9e8c853a6b19ae1fc2ba3bee650785215441aa60e"
+    sha256 cellar: :any, arm64_big_sur: "ae86a3868be7edf32aadf47abd949d3806789f5edf319c4d86120a05fee9053d"
+    sha256 cellar: :any, big_sur:       "fe3a719ec1b2b01385924b8cb3bbb758d006ff3dbd75b1c3691ce09a43d1ebcd"
+    sha256 cellar: :any, catalina:      "0826a1c50657da448806febb03694bce523e60ac56e7dd0de7362fe4b41f2277"
+    sha256 cellar: :any, mojave:        "6aa4de7c6d9b1b64beda27f0c06a6d8c9224616b74ae48ea8e706f166b374ce9"
   end
 
   depends_on "autoconf" => :build
