c3f074fc403746713795f8aad7781d93b871523e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-24 17:28:08 +0000

    immudb: update 1.0.0 bottle.

--- Formula/immudb.rb
+++ Formula/immudb.rb
@@ -11,10 +11,10 @@ class Immudb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4dfc7de40c10a03df02696f5bf7772681f596b4b3ae2f9d751fecb2d1aea84d3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bb4d3f0eec18d8e134be2b19fbe53f05fb7b1e95ebec985c67a16c3db6ad0426"
-    sha256 cellar: :any_skip_relocation, catalina:      "a565f456efcc0ffe09bd376006f7484fa3c99dc36f199d1a8a7f87211b9e0534"
-    sha256 cellar: :any_skip_relocation, mojave:        "aea6f08192f986e61ac105cd1a6b7ca142e1b32dc5002ee33251d7c42daf15bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "09529367063d75cd5d49e47322e48e408805234624e586aef11264b8cab6349b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a7cb5382bf0bbd23c3c406201d6ff46a6b917233312f0f1851ebed4a3d8efb90"
+    sha256 cellar: :any_skip_relocation, catalina:      "2fe1d5127c7eefd97980c9700ecfe2d0b8fa3a2fa5030e39c889314c9665bf45"
+    sha256 cellar: :any_skip_relocation, mojave:        "760bc5ebf53840b82798ba3dfe8566a95edb38ad7bf0c21ab7c48e4082d16d46"
   end
 
   depends_on "go" => :build
