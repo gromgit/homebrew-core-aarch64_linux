4682fdf8ae56b437407a030b432607aefd45290b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-25 05:50:24 +0000

    caire: add 1.3.0 bottle.

--- Formula/caire.rb
+++ Formula/caire.rb
@@ -6,6 +6,13 @@ class Caire < Formula
   license "MIT"
   head "https://github.com/esimov/caire.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0090dc37161ce7aff26f2fe47f56908355440c59909381c7c2bc420d0a9a02f3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8ab75ba3776f3325a6e534585a3f0caeca4b7b984337ada3adda768018361484"
+    sha256 cellar: :any_skip_relocation, catalina:      "a330a5bbbc00f82e32950265d39ddb5499b6190f8eb02c500a18d46b5b795bff"
+    sha256 cellar: :any_skip_relocation, mojave:        "5aa3249d567a0e668e2b0437942270a7834868293eceab2a33a1e6335ebdcf2d"
+  end
+
   depends_on "go" => :build
 
   def install
