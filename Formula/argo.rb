37a63d470a2a15574d374e3e46b2770f5844a45f
Rylan Polster <rslpolster@gmail.com>
2021-05-25 00:43:01 +0000

    argo: update 3.0.6 bottle.

--- Formula/argo.rb
+++ Formula/argo.rb
@@ -7,9 +7,9 @@ class Argo < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "95b1855240151d3abc65b47995c888cde8c5edfdd95736193ab753ea7f11a1ac"
-    sha256 cellar: :any_skip_relocation, catalina: "92cf8a57cee116d5185c6f34d58c2ad06b514088855106bd6f9024618bd399db"
-    sha256 cellar: :any_skip_relocation, mojave:   "e2bed94e7519871d90d15692e6f7bb6397ccc04996874b5553f0ff99c283e3bf"
+    sha256 cellar: :any_skip_relocation, big_sur:  "004b305266765554e161b707300337fdcc59d127c93e044b658f5297abdf5bb4"
+    sha256 cellar: :any_skip_relocation, catalina: "75450503a2e0c7b2d55f429f6086d8bc731ed89d431f1a68646ca4577e143150"
+    sha256 cellar: :any_skip_relocation, mojave:   "c0bfe189479134411a7d8055c32c2cae67138fdb17205bd740c128b70b026186"
   end
 
   depends_on "go" => :build
