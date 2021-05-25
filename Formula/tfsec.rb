308df4f24625479f3d75996f456061b151c679a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-25 22:07:52 +0000

    tfsec: update 0.39.39 bottle.

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -11,10 +11,10 @@ class Tfsec < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a2fd41d694823bd4b4c6ef8b8df178848b189481eedae91a4e3a1138327944b1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ebe2bb1c77eda2829aa96c6dc1398072f096bec35419eb5ec45f92eee39a36ca"
-    sha256 cellar: :any_skip_relocation, catalina:      "e8e2090a0eb1d0ad3c83ffc184973519328a60b0dc082435bb48f44ed336c4e0"
-    sha256 cellar: :any_skip_relocation, mojave:        "f84e82b513a0ce7baf3203d5e180894be36b309c16ff2498a293852eb7b66bfe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "47011dca657c2239ea422fbfe5b3b66f457366ca6e581874683847d324fd67d6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ce37f7ab2a6dade3839c5d769be6517b7d60d93e0f988e9644fa9e8afc0bc480"
+    sha256 cellar: :any_skip_relocation, catalina:      "96c12afdff3f4c3493d4f883c7de67c542d2e03ee058a96e05b5024869f6715e"
+    sha256 cellar: :any_skip_relocation, mojave:        "b8606519350fcae8cc568862cd23cbc3982211492a1da13b7e6587fd2fbd69a2"
   end
 
   depends_on "go" => :build
