6e2dc082762b151e612bb45bc65993e190c4ec82
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-25 12:37:38 +0100

    screenfetch: remove `shallow: false`
    This is a no-op since Homebrew/brew#11328.

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -4,7 +4,7 @@ class Screenfetch < Formula
   url "https://github.com/KittyKatt/screenFetch/archive/v3.9.1.tar.gz"
   sha256 "aa97dcd2a8576ae18de6c16c19744aae1573a3da7541af4b98a91930a30a3178"
   license "GPL-3.0"
-  head "https://github.com/KittyKatt/screenFetch.git", shallow: false
+  head "https://github.com/KittyKatt/screenFetch.git"
 
   bottle :unneeded
 
