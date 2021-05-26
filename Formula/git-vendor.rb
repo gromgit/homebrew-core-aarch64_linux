c8e11251c40e333ba1cabc9c58ef21aaf866e86d
Rui Chen <rui@chenrui.dev>
2021-05-26 10:43:06 -0400

    git-vendor 1.2.1
    * git-vendor 1.2.1
    * git-vendor: remove patch
    
    Closes #78087.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-vendor.rb
+++ Formula/git-vendor.rb
@@ -1,20 +1,13 @@
 class GitVendor < Formula
   desc "Command for managing git vendored dependencies"
   homepage "https://brettlangdon.github.io/git-vendor"
-  url "https://github.com/brettlangdon/git-vendor/archive/v1.2.0.tar.gz"
-  sha256 "d464f048769dfe7125d6c2453ec3b19b53920d9ebc80d0b28bab05011481ce89"
+  url "https://github.com/brettlangdon/git-vendor/archive/v1.2.1.tar.gz"
+  sha256 "7e2fb2f0299b8af152b5c9fd16133d50263a52dc8ecdc7a06111f7c0c5afc184"
   license "MIT"
   head "https://github.com/brettlangdon/git-vendor.git"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "4a32c52d725602c25e460a58d24f99029753aab5a215530bc0717968841bf8f3"
-  end
-
-  # Fix issue with /bin/sh builtin echo not supporting -n option
-  # Remove in the next release
-  patch do
-    url "https://github.com/brettlangdon/git-vendor/commit/c58c212f24fe54b0c77860da5185fc2bf3b77986.patch?full_index=1"
-    sha256 "5393c3a03856014ef1609ebca52006c23ead1f72c4737740704642492467f7b5"
   end
 
   def install
