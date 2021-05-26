867f67ae8c786f5f33384d2ee1d3e063d5056b2d
Rui Chen <rui@chenrui.dev>
2021-05-26 01:28:58 -0400

    grunt-completion 1.4.3
    Closes #78052.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grunt-completion.rb
+++ Formula/grunt-completion.rb
@@ -1,8 +1,8 @@
 class GruntCompletion < Formula
   desc "Bash and Zsh completion for Grunt"
   homepage "https://gruntjs.com/"
-  url "https://github.com/gruntjs/grunt-cli/archive/v1.4.2.tar.gz"
-  sha256 "ee02d056f5bc1c5628c2ac6c1c34546b9d9ee86b8360ec62ec360bf05d97d693"
+  url "https://github.com/gruntjs/grunt-cli/archive/v1.4.3.tar.gz"
+  sha256 "3bf07d807d61adbf04fa93589a7dd58fbc4da7a5f1febfd8a99b3ccb0d682009"
   license "MIT"
   head "https://github.com/gruntjs/grunt-cli.git"
 
