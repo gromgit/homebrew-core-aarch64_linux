5c98a1a2d43a587bb2a58f9bbc99374de6ce3771
Michka Popoff <michkapopoff@gmail.com>
2021-05-26 22:26:58 +0200

    ranger: depend on python@3.9, simplify test for Linux compatibility
    Fix license
    Remove bottle :unneeded
    
    Closes #78105.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ranger.rb
+++ Formula/ranger.rb
@@ -3,10 +3,10 @@ class Ranger < Formula
   homepage "https://ranger.github.io"
   url "https://ranger.github.io/ranger-1.9.3.tar.gz"
   sha256 "ce088a04c91c25263a9675dc5c43514b7ec1b38c8ea43d9a9d00923ff6cdd251"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
   head "https://github.com/ranger/ranger.git"
 
-  bottle :unneeded
+  depends_on "python@3.9"
 
   def install
     man1.install "doc/ranger.1"
@@ -16,6 +16,6 @@ class Ranger < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("script -q /dev/null #{bin}/ranger --version")
+    assert_match version.to_s, shell_output("#{bin}/ranger --version")
   end
 end
