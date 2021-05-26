8932fd30ebaf2976be4213c1a8ba53178b1b140a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-26 13:26:43 +0000

    universal-ctags: add p5.9.20210523.0 bottle.

--- Formula/universal-ctags.rb
+++ Formula/universal-ctags.rb
@@ -7,6 +7,13 @@ class UniversalCtags < Formula
   license "GPL-2.0-only"
   head "https://github.com/universal-ctags/ctags.git"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "48182f751f5f6efaa9516d4ed948e59b440b4e7fb0ad7b3ed654cbb7e2f4f44b"
+    sha256 cellar: :any, big_sur:       "40e0a5ebd29473d1233c7e147e85ef84a01af9019c773816adb55a4fed9fd9d1"
+    sha256 cellar: :any, catalina:      "a650abfb9ef8f9e33785c24a9230006db034b14e1dd4a4499a3a4ab56545534c"
+    sha256 cellar: :any, mojave:        "867f69719f9325ce53746f57cca64cb6ea063001abcf53261d5591d848c308c6"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "docutils" => :build
