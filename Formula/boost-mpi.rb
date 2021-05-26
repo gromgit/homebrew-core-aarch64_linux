c519602a86d41b0743795eb1834424e2bea59001
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-26 01:22:11 +0100

    boost-mpi: update 1.76.0 bottle.

--- Formula/boost-mpi.rb
+++ Formula/boost-mpi.rb
@@ -7,11 +7,10 @@ class BoostMpi < Formula
   head "https://github.com/boostorg/boost.git"
 
   bottle do
-    rebuild 2
-    sha256 arm64_big_sur: "38f6d5dddcdd925515838fd5f7bf55816443419e793fab9a145b4e6f2ce6148e"
-    sha256 big_sur:       "304716b2419c77790b2915ae8ea1df2c6290ce19db2526f497316dcde871d016"
-    sha256 catalina:      "c64692ab8cfde0cf5353806cfe5f2dd413e60864f07ee14524398288430f36c5"
-    sha256 mojave:        "541bcc69465a535743300f1548c4216ba7feea989e076abf00b0ddc98b5098c7"
+    sha256 arm64_big_sur: "8289fe7bb5a684360ab11462bf4312024a620854714ce02d6553e32274e5bfeb"
+    sha256 big_sur:       "76544350ace536b0af831854f3ce18a5c101155a132001685bcfa3ea411bbb94"
+    sha256 catalina:      "d3e1dfd88b6d683581efb1c0d732076eaa634d42d6e8d3de05ebec949f512740"
+    sha256 mojave:        "fc0b30274d5d1eaf5f66b7c733e8c516ddfc864beddc0932eb6ee3ddd2457e6c"
   end
 
   # Test with cmake to avoid issues like:
