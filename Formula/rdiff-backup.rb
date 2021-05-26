b8f1e72835a714daa2611318ad6ab34cc6bf2999
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-26 23:55:44 +0000

    rdiff-backup: update 2.0.5_1 bottle.

--- Formula/rdiff-backup.rb
+++ Formula/rdiff-backup.rb
@@ -7,10 +7,11 @@ class RdiffBackup < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, big_sur:     "da5cbef995206de251e217fbfb1a0594d3222c26c20c003df78a8aad6e855b8f"
-    sha256 cellar: :any, catalina:    "1597986cbff907a671f1449f59b0f456344e27b9104f7139d40f001b67d27477"
-    sha256 cellar: :any, mojave:      "875e5fcc8ae219dc64d5c2f3e435313f69fd4d0ea595d01ff3ae70fe5873b547"
-    sha256 cellar: :any, high_sierra: "215f96cb06d3e4c64dd52081fb22cfa6658ab25bd3fd76b52a128c70b5c93838"
+    sha256 cellar: :any, arm64_big_sur: "8ca9789a8e2456096ab851fe8277004201bc2f9ca66adf6934a6a942f9eaf3d3"
+    sha256 cellar: :any, big_sur:       "da5cbef995206de251e217fbfb1a0594d3222c26c20c003df78a8aad6e855b8f"
+    sha256 cellar: :any, catalina:      "1597986cbff907a671f1449f59b0f456344e27b9104f7139d40f001b67d27477"
+    sha256 cellar: :any, mojave:        "875e5fcc8ae219dc64d5c2f3e435313f69fd4d0ea595d01ff3ae70fe5873b547"
+    sha256 cellar: :any, high_sierra:   "215f96cb06d3e4c64dd52081fb22cfa6658ab25bd3fd76b52a128c70b5c93838"
   end
 
   depends_on "librsync"
