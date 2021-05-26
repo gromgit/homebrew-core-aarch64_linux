6f8cdcaaa0b6266160faaa89de2b04166d88bcf7
Rui Chen <rui@chenrui.dev>
2021-05-26 10:46:27 -0400

    git-if 1.3.6
    Closes #78088.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-if.rb
+++ Formula/git-if.rb
@@ -1,9 +1,9 @@
 class GitIf < Formula
   desc "Glulx interpreter that is optimized for speed"
   homepage "https://ifarchive.org/indexes/if-archiveXprogrammingXglulxXinterpretersXgit.html"
-  url "https://ifarchive.org/if-archive/programming/glulx/interpreters/git/git-135.zip"
-  version "1.3.5"
-  sha256 "4bdfae2e1ab085740efddf99d43ded6a044f1f2df274f753737e5f0e402fc4e9"
+  url "https://ifarchive.org/if-archive/programming/glulx/interpreters/git/git-136.zip"
+  version "1.3.6"
+  sha256 "28d8d7b8d81dac65c19f0b994d54b3c34b182045e86ca399fea65934918d1cf3"
   license "MIT"
   head "https://github.com/DavidKinder/Git.git"
 
