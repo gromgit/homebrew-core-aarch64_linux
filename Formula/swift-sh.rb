800d0e060e638aa6d5d4c6e4f4bd8e750780a072
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-26 00:16:24 +0000

    swift-sh: update 2.3.0 bottle.

--- Formula/swift-sh.rb
+++ Formula/swift-sh.rb
@@ -7,10 +7,10 @@ class SwiftSh < Formula
   head "https://github.com/mxcl/swift-sh.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "368220eaccf4e5d1f52f2bb53c6029ba600dd321de54fef4fdfbe6c965c2716b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9a5f73b88dc332198e97b18c539c0298fbb60690ff61f01735f28c490b12f364"
-    sha256 cellar: :any_skip_relocation, catalina:      "999595646476e2704d01011e10440281c24c638d46bb61eeffad6c9f3d5b4619"
-    sha256 cellar: :any_skip_relocation, mojave:        "3e0f348e5ccb6c22e41e3dc819eb5f8c2228b4874bac5cd24a61a823c97fabad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d90522741b5bfdac8d8d79375cce90984af0967bca1e84e8f5f9758b29e36616"
+    sha256 cellar: :any_skip_relocation, big_sur:       "490aaac152cd73af4711850ef3bf137d3db6ae8217f384dff4a9551dfafc5783"
+    sha256 cellar: :any_skip_relocation, catalina:      "d3993862ace65ec747846eadfad6110157e2de946e0b416355e6bbe321d7ca6a"
+    sha256 cellar: :any_skip_relocation, mojave:        "c7ece92d3a20f75c5a7551fb1a46b0750912e29021bae73a9e5e7f8d8e2397b3"
   end
 
   depends_on xcode: ["11.0", :build]
