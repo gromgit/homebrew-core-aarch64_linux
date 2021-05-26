cb4919f5f2346e1731b2bb4c2ed4e52eb7f9bd53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-26 15:11:34 +0000

    git-if: update 1.3.6 bottle.

--- Formula/git-if.rb
+++ Formula/git-if.rb
@@ -8,14 +8,10 @@ class GitIf < Formula
   head "https://github.com/DavidKinder/Git.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "77774c3b7c788df05c109ad9c40dd256ce0a6d23d39708151acdf39f31e6bb3f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "516c050a97d3f364c0fd9358a3e98d7a71e1a99ab948d02ac7dca05ed43a33f8"
-    sha256 cellar: :any_skip_relocation, catalina:      "f87fdd2951218631adc744bc2c9f1d83f3230bc49256f45735fc2d611f49dcc8"
-    sha256 cellar: :any_skip_relocation, mojave:        "37dc94c423003dedaf313ffa9343879ecb5d72f277a2f250100481cde240420e"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "bedbf580c8b073c7dfcd6bbb470aee7c14fb31d2c3ec54b4be8fd2cf8545e577"
-    sha256 cellar: :any_skip_relocation, sierra:        "7c09116244a4c04a46337a0453d519523204233fc33d2d60c89c4b9469498380"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "c38c41e66ca16a1ef9627112980fa49411b870c2438086db35f6dd9053f99850"
-    sha256 cellar: :any_skip_relocation, yosemite:      "e727f112e350e8a12b87094715800e9c2abc03f2d45ad521c0d78e4c6bfff3ad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1cbdac43ba193647b00e4dca570d079cfdf975e520c445e98d9352d51100b91c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "56d9c7c7ba0b996340fddcfe108ff45a3e813330f9af771f6b9d398be0b7129e"
+    sha256 cellar: :any_skip_relocation, catalina:      "f866a6a21977f9fd16230087f0e5239d8c8b37f7a158fcbb8d257a225e222774"
+    sha256 cellar: :any_skip_relocation, mojave:        "f4785e352c5810e9642f490e5c27d02db1ea35c2167cb6c58d88d55002501e7c"
   end
 
   depends_on "glktermw" => :build
