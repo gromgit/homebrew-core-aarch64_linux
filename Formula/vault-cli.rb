e4c0b45a3aaf1397f3ac642096b91623ac963e4e
Michael Cho <cho-m@tuta.io>
2021-05-26 23:28:55 -0700

    vault-cli 3.5.0 (#78162)

--- Formula/vault-cli.rb
+++ Formula/vault-cli.rb
@@ -1,8 +1,8 @@
 class VaultCli < Formula
   desc "Subversion-like utility to work with Jackrabbit FileVault"
   homepage "https://jackrabbit.apache.org/filevault/index.html"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.4.10/vault-cli-3.4.10-bin.tar.gz"
-  sha256 "ff50b7f38b4398ffbc215f5f10113c1700ac2ab1960644b31601da3d6cef23ae"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.5.0/vault-cli-3.5.0-bin.tar.gz"
+  sha256 "aaa7b3d34ec2951d78526a6056bf675d2eec8498a94e58017cbf12402753fb66"
   license "Apache-2.0"
   head "https://github.com/apache/jackrabbit-filevault.git"
 
