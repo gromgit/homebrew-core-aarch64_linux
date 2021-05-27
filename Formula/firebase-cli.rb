9817c2e0e16e86b2345da49604a78022b60d56bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-27 02:50:40 +0000

    firebase-cli: update 9.12.0 bottle.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -9,10 +9,10 @@ class FirebaseCli < Formula
   head "https://github.com/firebase/firebase-tools.git"
 
   bottle do
-    sha256                               arm64_big_sur: "1c6ca355dc6b8b54f24bbe02cc02d222e302f27f2b27d2f12680ea1f2dba6af5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1f3ad182090d3525aaf22eaefd31847e7fb20e1abc2cabde734c4be2a69a04dd"
-    sha256 cellar: :any_skip_relocation, catalina:      "1f3ad182090d3525aaf22eaefd31847e7fb20e1abc2cabde734c4be2a69a04dd"
-    sha256 cellar: :any_skip_relocation, mojave:        "1f3ad182090d3525aaf22eaefd31847e7fb20e1abc2cabde734c4be2a69a04dd"
+    sha256                               arm64_big_sur: "9987aebaada519de0203ba9702058c4fcab15014d08bbd5eba216b176b6d6dd7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3c1b084be5c7718b9f26988bb7ebb1efdf078b882623b06d8ff35eda0e22a87d"
+    sha256 cellar: :any_skip_relocation, catalina:      "3c1b084be5c7718b9f26988bb7ebb1efdf078b882623b06d8ff35eda0e22a87d"
+    sha256 cellar: :any_skip_relocation, mojave:        "3c1b084be5c7718b9f26988bb7ebb1efdf078b882623b06d8ff35eda0e22a87d"
   end
 
   depends_on "node"
