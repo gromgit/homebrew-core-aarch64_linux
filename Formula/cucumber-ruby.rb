df3a0cad62d02f983149f1704921e719a2a69ebf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-27 06:23:42 +0000

    cucumber-ruby: update 6.1.0 bottle.

--- Formula/cucumber-ruby.rb
+++ Formula/cucumber-ruby.rb
@@ -6,9 +6,9 @@ class CucumberRuby < Formula
   license "MIT"
 
   bottle do
-    sha256               big_sur:  "3242cbdc2387d58308c92b4ff9cfdae04e9be67ce7e50681b862a03cd0a6e964"
-    sha256 cellar: :any, catalina: "f9ac5ad41fb8363c4577609cd347b08a00f28a61a4df1485d4b31d54e7b54b6c"
-    sha256 cellar: :any, mojave:   "27fec91ee466eca329af0c3ac503a6b5d20c558fb38b034509ee09efc56534c9"
+    sha256               big_sur:  "d1053be0ce5fb238c45bd404d5662c518d007d9c46ce72c889d0962fe357d7d6"
+    sha256 cellar: :any, catalina: "730af3e598806b9f40503460b8ea418f27765398036cbcdbf978554c1e21e67e"
+    sha256 cellar: :any, mojave:   "5f68b35ebba784234f0ebf9ad2a68794eeb678a47291d077aafabe2dfadf4d46"
   end
 
   depends_on "pkg-config" => :build
