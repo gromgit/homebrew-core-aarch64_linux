89ffb7b1e61f96207e34e95b27b85edd98894531
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-27 06:23:15 +0000

    embree: update 3.13.0 bottle.

--- Formula/embree.rb
+++ Formula/embree.rb
@@ -7,9 +7,10 @@ class Embree < Formula
   head "https://github.com/embree/embree.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "147073c0b4202455903fcc3a3e76bca5bfc8d3626e3cba03569ab28e8165be03"
-    sha256 cellar: :any, catalina: "0a5d7be8af333327b71067dd361cc23018bb416aca55cea94d0a478151470011"
-    sha256 cellar: :any, mojave:   "8138f4d8085ff6487008954faac6929a5b16dea7d37875030417dbe6502f09bf"
+    sha256 cellar: :any, arm64_big_sur: "34ebcc3b29a0d7194d2bd99992e0cdf6a1a65c4359ca069ab2871fd07e91994d"
+    sha256 cellar: :any, big_sur:       "ccb92b5bdc9349ea27dce5ca3d7f439e562a005ee8c9156890e9e87527bd945a"
+    sha256 cellar: :any, catalina:      "a5018cde142cea96f08ccf89e4b653d3a2cf521c8c884abf10b1e9b176f75dd6"
+    sha256 cellar: :any, mojave:        "f31316bbd32d20997392a8bb35d6dfd8e9103e77a46382411d445a92d23f5595"
   end
 
   depends_on "cmake" => :build
