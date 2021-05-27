1becb30f127498477db65b30476d8d0e9884f6c3
Nanda H Krishna <me@nandahkrishna.com>
2021-05-27 09:15:49 +0000

    range2cidr: add 1.0.0 bottle.

--- Formula/range2cidr.rb
+++ Formula/range2cidr.rb
@@ -5,6 +5,13 @@ class Range2cidr < Formula
   sha256 "caf6627b361ce690a884ccbb98c229d07dcf73e453af625638b7508113e1b0df"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5afa2bd9b03bf007db38f7dca0b129b0d2094a4309ce983aeb855e0a784119c7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "543dde8b3d0ef72d2884e13891a46dda2a9492dc601159efb41380960ebcc054"
+    sha256 cellar: :any_skip_relocation, catalina:      "2c506624fce0d1edf09071016fa5ad6f3860e8e9673f6d4145edcfee7e5c72f5"
+    sha256 cellar: :any_skip_relocation, mojave:        "80bc3497649b621d76f27936cf58b33be3371efbf3f38dbff8ccaeff76c5e3dc"
+  end
+
   depends_on "go" => :build
 
   def install
