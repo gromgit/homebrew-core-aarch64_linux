a4edc4feba2aa466da430a67d03ded8efc3394e4
Dongjoon Hyun <dongjoon@apache.org>
2021-05-27 16:42:07 -0700

    apache-spark 3.1.2 (#78200)

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,10 +1,10 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz"
-  mirror "https://archive.apache.org/dist/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz"
-  version "3.1.1"
-  sha256 "814ad7da8b57e6b62e6ffb54c6f7292823e8eb76c5c1bb1cd97a57c477ee8f07"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz"
+  mirror "https://archive.apache.org/dist/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz"
+  version "3.1.2"
+  sha256 "0d9cf9dbbb3b4215afebe7fa4748b012e406dd1f1ad2a61b993ac04adcb94eaa"
   license "Apache-2.0"
   head "https://github.com/apache/spark.git"
 
