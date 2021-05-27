1beb1dea0515daab704259d11e2322718ea39b9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-27 02:49:39 +0000

    nspr: update 4.31 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -11,10 +11,10 @@ class Nspr < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2fc4d4bfd5f394070d583edd6608b1de7f65ab4840443eddf149c249d1e7e0a9"
-    sha256 cellar: :any, big_sur:       "f2d71db58997266b9f0aa518ce1bdbd7648d4138e6673a01087f18967023f8ae"
-    sha256 cellar: :any, catalina:      "f1f0f7f48b791b7d08b59600b9d0e47b26872f3168b506b2b53cd01a632c93d8"
-    sha256 cellar: :any, mojave:        "3655c56ecc592a029200493c30930b2ecf3f4911ee2af72ac01d4fe1fa88126c"
+    sha256 cellar: :any, arm64_big_sur: "cc68066c04e374c88ae7d20ebf7093d5a58b1203481272de39554d5b0c46fbfe"
+    sha256 cellar: :any, big_sur:       "097bb45f7541b47ecb45f3476da7aa0249f66681255c10a4f6081452083716e1"
+    sha256 cellar: :any, catalina:      "683ef5685207c5c368c0ac2f0db3235f5b0f5d30d3fa53fcff1b805f815ab4de"
+    sha256 cellar: :any, mojave:        "76cf099f7d02535352ac4bfd5b1c76bde37d891e40923cf0a32e1b8513e011e8"
   end
 
   def install
