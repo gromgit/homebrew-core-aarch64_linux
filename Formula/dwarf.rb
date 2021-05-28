ac7dedd21a3f8b334d1205e8673d95b75c927075
Michka Popoff <michkapopoff@gmail.com>
2021-05-28 12:17:07 +0200

    dwarf: provide alternate test for Linux (#78199)

--- Formula/dwarf.rb
+++ Formula/dwarf.rb
@@ -38,7 +38,13 @@ class Dwarf < Formula
       }
     EOS
     system ENV.cc, "test.c", "-o", "test"
-    output = shell_output("#{bin}/dwarf -c 'pp $mac' test")
-    assert_equal "magic: 0xfeedfacf (-17958193)", output.lines[0].chomp
+    on_macos do
+      output = shell_output("#{bin}/dwarf -c 'pp $mac' test")
+      assert_equal "magic: 0xfeedfacf (-17958193)", output.lines[0].chomp
+    end
+    on_linux do
+      # pp may not be installed on Linux, test something else
+      assert_match "main header: elf", shell_output("#{bin}/dwarf -p test")
+    end
   end
 end
