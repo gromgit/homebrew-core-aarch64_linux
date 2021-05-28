d98d148f8acc47a66ebf45ca60b7adcdc7b7d463
Dustin Rodrigues <dust.rod@gmail.com>
2021-05-28 02:40:55 +0000

    esptool: update 3.1 bottle.

--- Formula/esptool.rb
+++ Formula/esptool.rb
@@ -8,10 +8,10 @@ class Esptool < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "29caa2064c93bf60c30012081d2f9ff98c7e6a2197613fd19bcb59225e8aa1a5"
-    sha256 cellar: :any, big_sur:       "9d11746f99a1363c06b7fd1f2462d095268c1557dab200f389d66d59bfceecf9"
-    sha256 cellar: :any, catalina:      "5070c01db9a963f97c27265e87e5e5afb52c824aa02fc1c4dc0a69a019e88771"
-    sha256 cellar: :any, mojave:        "8d912a267508314803f308abb07620c80ec0440c84cb93c28357df2366ee037d"
+    sha256 cellar: :any, arm64_big_sur: "e2916044d6ce93795993340af2904bc088324c54b238f0663f672b35d5d2ff01"
+    sha256 cellar: :any, big_sur:       "286a2dbf4c4243d0fc244c07d9e41d2eea34e1b60d686b310eb1246f216854b2"
+    sha256 cellar: :any, catalina:      "c88e132957e5b312946aea44eb0aded7dab11a64fa629190af16ab4f317d496b"
+    sha256 cellar: :any, mojave:        "0905b46bbefd70aa8a0666651cd51b0664b8390f85c1353d087aab0de5559a1a"
   end
 
   depends_on "rust" => :build
