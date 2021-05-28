9d35cec33fff45ccc2379156ea030238c71eed3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-28 12:21:01 +0000

    diffoscope: update 176 bottle.

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -8,10 +8,10 @@ class Diffoscope < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ac0075bb0cafdd9f8c56826f4a2c89d121ca7736dc04e7eec0149e61485f2149"
-    sha256 cellar: :any_skip_relocation, big_sur:       "865d95809b9e21e8af9648730bc157db5c15517c8ba18fcd194c0afc071a89be"
-    sha256 cellar: :any_skip_relocation, catalina:      "710a47a9208fd71ca632dac2c9c59e66597aa7cb2fd59ae7980180cfac5913e7"
-    sha256 cellar: :any_skip_relocation, mojave:        "ea155379881388ab64a02f2b368f8bad0fccb24dbf9a8f0e2547204ec296b65e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "153105ef926dd33e5387ecfc4a22c29ff27511ddcfed096cefec6f935f9be92a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "42936da97f489fc698644c792b14b134e8703a932ad3ca637bbf374a88ed1c57"
+    sha256 cellar: :any_skip_relocation, catalina:      "f7f4a7355f9afc5bdf822ec70c7c3082b3cf5f726427ff1ddab66beaed8c7ef0"
+    sha256 cellar: :any_skip_relocation, mojave:        "4ab18751140fe1082d4dad39ab57959598ebe2e6e567d996009508271ad81f30"
   end
 
   depends_on "libarchive"
