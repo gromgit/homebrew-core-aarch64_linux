68ec1080a4e3db26acc33476669faf7a85cd5d35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-28 01:37:04 +0000

    opa: update 0.29.1 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -7,10 +7,10 @@ class Opa < Formula
   head "https://github.com/open-policy-agent/opa.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e1ac93edf390fa6ae61f71060ccd2bc5fee1743b8826919d4a9a27dce3884e9c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ef48d8bffd455072906d6367509bebba2909f652fdd0ef4ee44fc2b980e4c793"
-    sha256 cellar: :any_skip_relocation, catalina:      "f0396952b79b701f2edec59f9526c9d379f1a4c4d5b90a94354e83f5e00408b5"
-    sha256 cellar: :any_skip_relocation, mojave:        "a012eb7c26bfa3aef3c5a26d37cb930afbfb224508916166708a7cb0d9a37915"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3bf6e1b4e7be4268c2d9a2e00294f52b21c46da8e06ebb57703d917fcc6334a8"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f390fa4f734bed44cd383657149c2472a8509fced4d2753a978858613ef8ff9b"
+    sha256 cellar: :any_skip_relocation, catalina:      "0b7a0a0bf7156b578e360dabe477a71831e8735d54f6bdf68cb79c7e111204f4"
+    sha256 cellar: :any_skip_relocation, mojave:        "18904713f7c110bcca8444d8d11174886dc5cee2bf60ccbdb94b021a5af19638"
   end
 
   depends_on "go" => :build
