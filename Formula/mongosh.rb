d1166d0d94fcf8f56cc4d819a84d2ddb5a3e95ba
Michael Rose <michael_rose@gmx.de>
2021-05-28 12:28:30 +0200

    mongosh 0.14.0 (new formula)
    Closes #78239.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/mongosh.rb
@@ -0,0 +1,21 @@
+require "language/node"
+
+class Mongosh < Formula
+  desc "MongoDB Shell to connect, configure, query, and work with your MongoDB database"
+  homepage "https://github.com/mongodb-js/mongosh#readme"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.14.0.tgz"
+  sha256 "7b5a140689b4460a8b87008e6b7e7cb19acbc6e6cd1ab713e1a8923f3a995ca8"
+  license "Apache-2.0"
+
+  depends_on "node@14"
+
+  def install
+    system "#{Formula["node@14"].bin}/npm", "install", *Language::Node.std_npm_install_args(libexec)
+    (bin/"mongosh").write_env_script libexec/"bin/mongosh", PATH: "#{Formula["node@14"].opt_bin}:$PATH"
+  end
+
+  test do
+    assert_match "ECONNREFUSED 0.0.0.0:1", shell_output("#{bin}/mongosh \"mongodb://0.0.0.0:1\" 2>&1", 1)
+    assert_match "#ok#", shell_output("#{bin}/mongosh --nodb --eval \"print('#ok#')\"")
+  end
+end
