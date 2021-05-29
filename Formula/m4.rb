f2f661e4b759c578f97fcd3f03aa186e8f2e5852
MoonFruit <dkmoonfruit@gmail.com>
2021-05-29 10:51:22 +0800

    m4 1.4.19
    Closes #81768.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/m4.rb
+++ Formula/m4.rb
@@ -1,9 +1,9 @@
 class M4 < Formula
   desc "Macro processing language"
   homepage "https://www.gnu.org/software/m4"
-  url "https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz"
-  mirror "https://ftpmirror.gnu.org/m4/m4-1.4.18.tar.xz"
-  sha256 "f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07"
+  url "https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.xz"
+  mirror "https://ftpmirror.gnu.org/m4/m4-1.4.19.tar.xz"
+  sha256 "63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -16,28 +16,6 @@ class M4 < Formula
   end
 
   keg_only :provided_by_macos
-
-  # Fix crash from usage of %n in dynamic format strings on High Sierra
-  # Patch credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
-  if MacOS.version >= :high_sierra
-    patch :p0 do
-      url "https://raw.githubusercontent.com/macports/macports-ports/edf0ee1e2cf/devel/m4/files/secure_snprintf.patch"
-      sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
-    end
-  end
-
-  on_linux do
-    # Glibc no longer provides several required symbols since version 2.28.
-    # Gnulib, bundled into m4, and the dependent of those removed symbols
-    # would no longer compile without an upstream fix applied.
-    # - https://github.com/coreutils/gnulib/commit/4af4a4a71827c0bc5e0ec67af23edef4f15cee8e#diff-5bcce8ce55246264586c4aed2a45ff89
-    # Patch the copy of gnulib bundled with the m4 1.4.18 distribution.
-    # - https://lists.gnu.org/archive/html/bug-m4/2018-08/msg00000.html
-    patch do
-      url "https://raw.githubusercontent.com/archlinux/svntogit-packages/19e203625ecdf223400d523f3f8344f6ce96e0c2/trunk/m4-1.4.18-glibc-change-work-around.patch"
-      sha256 "fc9b61654a3ba1a8d6cd78ce087e7c96366c290bc8d2c299f09828d793b853c8"
-    end
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
