a5adfdbea923a5ac187d059c58871da13c0541e8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-29 08:08:27 +0000

    matterbridge: add 1.22.1 bottle.

--- Formula/matterbridge.rb
+++ Formula/matterbridge.rb
@@ -5,6 +5,13 @@ class Matterbridge < Formula
   sha256 "c95f34e4e58534ec4a20677ab76911714cdd07cc585981c52be5b6bbf40f4d78"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0f7d48db4c678ee01d7641ca23783123afd2abe4cada84dd0fe42b6fbfc0cecd"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9f401ee30df6117685ecff3f77218aad5d6967292eec2e3654d53dba7940d5d1"
+    sha256 cellar: :any_skip_relocation, catalina:      "c645a03c31460a503da13fe3273a8591e60f912a8e88d8e4f4bd6bf788598602"
+    sha256 cellar: :any_skip_relocation, mojave:        "c5cf1f479e6fa88acebd2367160fba769cef7d6d7a5779340496948fb5ae04e6"
+  end
+
   depends_on "go" => :build
 
   def install
