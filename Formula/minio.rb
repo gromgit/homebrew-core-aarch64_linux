d27863af8f23bb9290458665275965e36230263d
Rui Chen <rui@chenrui.dev>
2021-05-29 00:17:44 -0400

    minio 20210527220631
    * minio 20210527220631
    * update code style
    
    Closes #78313.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -2,9 +2,9 @@ class Minio < Formula
   desc "High Performance, Kubernetes Native Object Storage"
   homepage "https://min.io"
   url "https://github.com/minio/minio.git",
-      tag:      "RELEASE.2021-05-26T00-22-46Z",
-      revision: "3fff50120bb17700fc9b8d88ce6fe4a71fcbc3f0"
-  version "20210526002246"
+      tag:      "RELEASE.2021-05-27T22-06-31Z",
+      revision: "89bb9f17d73882127203e07c63d288d77525f1c6"
+  version "20210527220631"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/minio.git"
 
@@ -31,13 +31,15 @@ class Minio < Formula
     else
       release = `git tag --points-at HEAD`.chomp
       version = release.gsub(/RELEASE\./, "").chomp.gsub(/T(\d+)-(\d+)-(\d+)Z/, 'T\1:\2:\3Z')
-      proj = "github.com/minio/minio"
 
-      system "go", "build", *std_go_args, "-ldflags", <<~EOS
-        -X #{proj}/cmd.Version=#{version}
-        -X #{proj}/cmd.ReleaseTag=#{release}
-        -X #{proj}/cmd.CommitID=#{Utils.git_head}
-      EOS
+      ldflags = %W[
+        -s -w
+        -X github.com/minio/minio/cmd.Version=#{version}
+        -X github.com/minio/minio/cmd.ReleaseTag=#{release}
+        -X github.com/minio/minio/cmd.CommitID=#{Utils.git_head}
+      ]
+
+      system "go", "build", *std_go_args(ldflags: ldflags.join(" "))
     end
   end
 
