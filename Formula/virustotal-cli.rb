9ad07245e64edace6c19b9e63be41917e48e6ae7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-29 06:39:35 +0000

    virustotal-cli: update 0.9.7 bottle.

--- Formula/virustotal-cli.rb
+++ Formula/virustotal-cli.rb
@@ -6,10 +6,10 @@ class VirustotalCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2d59a45d938c717eb4a973fdf0c9cd66d34275823dfb0ef8b7af19c2a35921e7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3140b175d8ffc0311f328dd84879cb9cb0a6926dab057d714fd8f7185551cf41"
-    sha256 cellar: :any_skip_relocation, catalina:      "19de8c8e8db5fb3f45eb14fe1ad1beba7ff4824e1bf7ac16911f0a3c2e486390"
-    sha256 cellar: :any_skip_relocation, mojave:        "da799e16b4017767887fa424b13c0cc846a4eb25d31e2f63d50903d581de8b6c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "41f0fa1b0a8744878323478f8bb5189c70d21dddc1a7e326294e31f7c44464ef"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ce8bd2abe3a1137debb26192a4bf43faf2064a54cbf4f7eabf94256317973baa"
+    sha256 cellar: :any_skip_relocation, catalina:      "bf9f0b5b6577a82a8410774087d6ef894ba68a4e465dd8a5e0c9093c5828bab6"
+    sha256 cellar: :any_skip_relocation, mojave:        "a66cbde56c35079ad8af5c80c28ae892c57f0c750fa64be992b207a645235a8a"
   end
 
   depends_on "go" => :build
