3b9ed46790b182403aab5f18768bf85faf005bae
Caleb Xu <calebcenter@live.com>
2021-05-29 12:07:23 -0400

    pmd 6.35.0
    Closes #78365.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.34.0/pmd-bin-6.34.0.zip"
-  sha256 "fb2f85fd36a243116340b6aafcbbb74eac982c27f8c22cd0860f1e08aef63a4c"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.35.0/pmd-bin-6.35.0.zip"
+  sha256 "2beee89f0c6cb7044962d825b788ce0f856f6a5beba66b0d6f2ab0e6d36e5412"
   license "BSD-4-Clause"
 
   bottle do
