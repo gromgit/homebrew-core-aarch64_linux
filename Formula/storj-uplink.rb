4e9e8f1b2edaf6d3ea8d4d454bbfedf5e0a7114e
Nanda H Krishna <me@nandahkrishna.com>
2021-05-29 08:13:11 +0000

    storj-uplink: add 1.30.2 bottle.

--- Formula/storj-uplink.rb
+++ Formula/storj-uplink.rb
@@ -5,6 +5,13 @@ class StorjUplink < Formula
   sha256 "e26d9bbea734245bf63171224b30661c864bafc699cf93ffb268bac25cd74ab5"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "be143d195b4e7dedb904649ed701007d5fbfddddaee168f4ef4d41196a152f07"
+    sha256 cellar: :any_skip_relocation, big_sur:       "caa8f81fc92c06e909694ec21ee407fdf9e4a2175d8d810c763a42f42d21a8c6"
+    sha256 cellar: :any_skip_relocation, catalina:      "120ae1a842fd49f3a704f49db27e62151966cc300d43ef253055ec930aa50126"
+    sha256 cellar: :any_skip_relocation, mojave:        "aa9ec7d043e869e1082766c51d216b6fc9b6c5f3668002fc656fab03f3390f7b"
+  end
+
   depends_on "go" => :build
 
   def install
