ac908adeda8bceed1f128d1920c736e2371cb6ee
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-31 20:56:57 +0000

    code-minimap: add 0.6.0 bottle.

--- Formula/code-minimap.rb
+++ Formula/code-minimap.rb
@@ -5,6 +5,13 @@ class CodeMinimap < Formula
   sha256 "6f67ed03b33863186c4811b1c8acc6b0cc9c6c63f2bd5b54dbf703f7fc561711"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f798db57c9bec0e39bc8f7f30f8445e3df53032d1686ec2549e37218873d2f5e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "34a54ec8b3e68ada2fe286dc61f7626de51b1ee29360be034566f34bc468a24e"
+    sha256 cellar: :any_skip_relocation, catalina:      "27b9629e0fb3cdc4e2629ae150ffc13ce8cb6c5448f02f888395d53219b2ec59"
+    sha256 cellar: :any_skip_relocation, mojave:        "490595ba892b4bfd1170af45ca829af08015a371f0eedb7e41d7b9db51c9e8a4"
+  end
+
   depends_on "rust" => :build
 
   def install
