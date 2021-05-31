8820a30fc63e80759b5b7c905cb001c198cbd8ff
frranck <frranck@users.noreply.github.com>
2021-05-31 18:32:08 +0200

    mrboom 5.2
    * Update mrboom.rb
    * mrboom 5.2
    * mrboom 5.2
    
    Closes #78423.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mrboom.rb
+++ Formula/mrboom.rb
@@ -1,8 +1,9 @@
 class Mrboom < Formula
   desc "Eight player Bomberman clone"
   homepage "http://mrboom.mumblecore.org/"
-  url "https://github.com/Javanaise/mrboom-libretro/releases/download/5.0/MrBoom-src-5.0.tar.gz"
-  sha256 "88bd9fee69a9d3a5f3edafde9cfb5f9a8ca415041281fc19c1e7bc2a75407829"
+  url "https://github.com/Javanaise/mrboom-libretro/releases/download/5.2/MrBoom-src-5.2.454d614.tar.gz"
+  version "5.2"
+  sha256 "50e4fe4bc74b23ac441499c756c4575dfe9faab9e787a3ab942a856ac63cf10d"
   license "MIT"
 
   bottle do
@@ -27,12 +28,12 @@ class Mrboom < Formula
     require "expect"
     require "timeout"
     PTY.spawn(bin/"mrboom", "-m", "-f 0", "-z") do |r, _w, pid|
-      sleep 1
+      sleep 15
       Process.kill "SIGINT", pid
       assert_match "monster", r.expect(/monster/, 10)[0]
     ensure
       begin
-        Timeout.timeout(10) do
+        Timeout.timeout(30) do
           Process.wait pid
         end
       rescue Timeout::Error
