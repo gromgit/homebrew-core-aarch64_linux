21313bd16a438a8f2a62cf3523946ee0f9f47da4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-01 15:44:44 +0100

    vapoursynth-imwri: update 53_1 bottle.

--- Formula/vapoursynth-imwri.rb
+++ Formula/vapoursynth-imwri.rb
@@ -8,10 +8,10 @@ class VapoursynthImwri < Formula
   head "https://github.com/vapoursynth/vapoursynth.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5601a793accfb1cba1376f9307d8c9e87356d974d389e1c415f75666f9d1cc19"
-    sha256 cellar: :any, big_sur:       "b37640258be83b5951cfe8c77cb9e34c00c37c04814df7f108da0280a63e2e3d"
-    sha256 cellar: :any, catalina:      "ac5547b7872e34409f4ed5a9f7d11985eaffc7c690f95c06ac2c596c9609b3ca"
-    sha256 cellar: :any, mojave:        "2e2b12d1569d8c4b6a4ccf6b49b7e3a7c7ab41fbd57ed5279b55c06e223d824a"
+    sha256 cellar: :any, arm64_big_sur: "b18dd1a4f43d95579c48d35f8ec579a706f6ed0ea104cf25dee995defd426ec8"
+    sha256 cellar: :any, big_sur:       "e9e17493e1e3f2ab020be8425ab9d17266b53f91e0c5277c156a2970f665ba75"
+    sha256 cellar: :any, catalina:      "be4e734c5d4cb506ff006ddd17d9fd536f3035f26315bac49bb151088b8ead71"
+    sha256 cellar: :any, mojave:        "e109782020a18bcf8b8a3c9ddddbd593abfabbc8e3513bee26abcfd1bf35d4d8"
   end
 
   depends_on "autoconf" => :build
