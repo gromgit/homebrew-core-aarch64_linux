db3179cf0212d38d933d593b5c93aa77d68577b0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-01 15:44:43 +0100

    imagemagick: update 7.0.11-14 bottle.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -12,10 +12,10 @@ class Imagemagick < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "8558d9b466e9eeb424a949bf697a5e984485fd3063bf37e9d802290608e1f607"
-    sha256 big_sur:       "12d2e44653f8f3820d98b55d7c3a6525fd28453177aea8ee7f22f0852b12c59a"
-    sha256 catalina:      "ac0d9eb95650a06a95dbc081577ba5594cd0601bc77fdc499373b19bf125d649"
-    sha256 mojave:        "89406424a61a22600a38ee0e0e08098f72b67fb70b89b3d3236066641dcb3572"
+    sha256 arm64_big_sur: "32909f83820c8b18ee8ddb690de29c5ddd01fe280852927f9f397d75231966a5"
+    sha256 big_sur:       "87e8a20ffe7dbc81738a3fed301f2a2a98e2ff978aec19b4a391a16cb0c170c3"
+    sha256 catalina:      "3d2ac7eec2612826b6323b4292e352a3fa6216f9ff98695c6362c2ee4f716aa5"
+    sha256 mojave:        "085d6db7aa0e7647492e195a0736ea14e6b56b32f0ef9462490f31f40a34af71"
   end
 
   depends_on "pkg-config" => :build
