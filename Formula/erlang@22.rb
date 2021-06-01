5c02110c2d6e654b54c93c467f66dec8b162890f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-01 06:56:17 +0000

    erlang@22: update 22.3.4.20 bottle.

--- Formula/erlang@22.rb
+++ Formula/erlang@22.rb
@@ -12,10 +12,10 @@ class ErlangAT22 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0d2b2567843b4260f55c3f0b12370862ea470a581becc62730eb777af39b05fa"
-    sha256 cellar: :any, big_sur:       "4abea181707cc3cbdd6007ac66b432dfd4976024deff6a1fe10c3b00458cbacd"
-    sha256 cellar: :any, catalina:      "d3d2afe30bbaf547288f16323bbc4850e36e5922dbcbc5360d360f02df31824f"
-    sha256 cellar: :any, mojave:        "97f3795134f43f5dc56675e5435cffcda3c041ccf484c8c3ee317d27c5b8675d"
+    sha256 cellar: :any, arm64_big_sur: "bc0d156048890a61fb4700ed63f3aa4a9044fe8210e05d5f1d183c1e0a0fc6a4"
+    sha256 cellar: :any, big_sur:       "a672a846a5223b8f7fe8b37e906bb328fbf86212cb57a35542a44c19a2f03d6e"
+    sha256 cellar: :any, catalina:      "8fee4999da6690c83dc44b2f2a39026309a695d514a58ceb49fa80c62e5b238c"
+    sha256 cellar: :any, mojave:        "820a6698e8a740c0a5b92b04a67030aaa465d2e84b905194d29a71aa3a1f5260"
   end
 
   keg_only :versioned_formula
