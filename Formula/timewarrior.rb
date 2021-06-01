883da067dd1babf5107fd682f2e36a0bca473fb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-01 06:55:48 +0000

    timewarrior: update 1.4.3 bottle.

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -7,11 +7,10 @@ class Timewarrior < Formula
   head "https://github.com/GothenburgBitFactory/timewarrior.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b725f7ad884389a11b3428f43ab9d823e179a5aa679a6506ea0b67eb65007c25"
-    sha256 cellar: :any_skip_relocation, big_sur:       "40d7ce6f5802ca1a755f6a722ebcabc68b574632007d95ef109ad99c2e7a4902"
-    sha256 cellar: :any_skip_relocation, catalina:      "4c872f73c14a7219179b3f468d5ceb0739b79ace42e69126f9b3549eac9cba94"
-    sha256 cellar: :any_skip_relocation, mojave:        "336d8bbaf618d17901774358a58772579318a405e7020c63583538283b1f2165"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "c15692ae447c6364eb7c74665e3b9d8acb01be7f31eb0f4ebaf92d7c7dc3f874"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e28eba2534652ce27707ed9c254f1ee5e975075d828f55a921a0364431fdbd27"
+    sha256 cellar: :any_skip_relocation, big_sur:       "101e508c5c0a5103c47ee108cabb0e62d236005625a9825c63410a9e0b045243"
+    sha256 cellar: :any_skip_relocation, catalina:      "eb42768965768800442869fab32f020e4d0895a69dbc5272799876bb92b4c6b1"
+    sha256 cellar: :any_skip_relocation, mojave:        "18f0a0d7a5077f4d45f796b54ef93146656321dc07fdd1d044e7fedac4f98506"
   end
 
   depends_on "cmake" => :build
