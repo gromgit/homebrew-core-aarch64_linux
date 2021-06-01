561bcd1d7d0345ecb0e359e9eb983ebc70c09c3f
jmechnich <joerg.mechnich@gmail.com>
2021-06-01 22:07:58 +0200

    gpg-tui 0.1.5 (new formula)
    Closes #78491.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gpg-tui.rb
@@ -0,0 +1,40 @@
+class GpgTui < Formula
+  desc "Manage your GnuPG keys with ease! 🔐"
+  homepage "https://github.com/orhun/gpg-tui"
+  url "https://github.com/orhun/gpg-tui/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "eccfaa3089605bb6df2cc49df208a93cc452d8a819b444bf82dfeb90739135ea"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "gnupg"
+  depends_on "gpgme"
+  depends_on "libgpg-error"
+  depends_on "libxcb"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    ENV["OUT_DIR"] = buildpath
+    system bin/"completions"
+    bash_completion.install "gpg-tui.bash"
+    fish_completion.install "gpg-tui.fish"
+    zsh_completion.install "_gpg-tui"
+
+    rm_f bin/"completions"
+    rm_f Dir[prefix/".crates*"]
+  end
+
+  test do
+    require "pty"
+    require "io/console"
+
+    (testpath/"gpg-tui").mkdir
+    r, w, pid = PTY.spawn "#{bin}/gpg-tui"
+    r.winsize = [80, 43]
+    sleep 1
+    w.write "q"
+    assert_match(/^.*<.*list.*pub.*>.*$/, r.read)
+  ensure
+    Process.kill("TERM", pid)
+  end
+end
