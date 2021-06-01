e5faf791fdb1df83c30e45d3d85a5ab48a700f25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-01 12:27:01 +0000

    operator-sdk: update 1.8.0 bottle.

--- Formula/operator-sdk.rb
+++ Formula/operator-sdk.rb
@@ -8,10 +8,10 @@ class OperatorSdk < Formula
   head "https://github.com/operator-framework/operator-sdk.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "02c8e5496e2170ee48b3edd85250efe6ee74e29bfac6851f6ee91c9a1cfc6fd9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e08d2dc21573f33febbe11ab5cb7b3efd0146770b2dd1215113a101204fabff9"
-    sha256 cellar: :any_skip_relocation, catalina:      "64b5db0a220d235fb52cb222121c18100aa63fe721a46c005893acced4a580fa"
-    sha256 cellar: :any_skip_relocation, mojave:        "264bd5eb11ace894a48820e060787d2c9facd8160c0267b18ebf825351efce27"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "864267e4c586a03de14a612f604ad0c46d27b96b333f048721ec7cc5af8dc836"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c097f8d768bd4ef739d6ef71eabfa5139ea2c6ca114143e182cf2268ddf9a55d"
+    sha256 cellar: :any_skip_relocation, catalina:      "4f83d038d343bc16f5ab9b5814247a4a396968f66513215c2ce5e6a9e5090a67"
+    sha256 cellar: :any_skip_relocation, mojave:        "2a2eaa53558d93460be9da719bddc034eefd1de14733cd604252725d269256a2"
   end
 
   depends_on "go"
