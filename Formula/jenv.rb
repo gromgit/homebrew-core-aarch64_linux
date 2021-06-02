268da91d776128e1467b6ff16a60b51cf9de2e25
David Xia <davidxia@users.noreply.github.com>
2021-06-02 17:54:51 -0400

    jenv: install fish function symlinks, update caveats (#76864)
    * jenv: change caveat for fish shell
    
    The caveat isn't compatible with fish shell.
    This PR changes the caveat for fish shell to show the
    commands specified in the upstream's README here \[1\].
    
    This PR does not change the caveat for other shells.
    
    fixes https://github.com/jenv/jenv/issues/315
    
    I have tested this PR and get the output below.
    
    With bash set as default shell (`chsh -s $(which bash)`):
    
    ```
    brew info jenv
    
    jenv: stable 0.5.4, HEAD
    Manage your Java environment
    https://www.jenv.be/
    /opt/homebrew/Cellar/jenv/0.5.4 (82 files, 72.3KB) *
      Built from source on 2021-05-07 at 17:19:07
    From: https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/jenv.rb
    License: MIT
    ==> Options
    --HEAD
    	Install HEAD version
    ==> Caveats
    To activate jenv, add the following to your /Users/dxia/.bash_profile:
    
      export PATH="$HOME/.jenv/bin:$PATH"
      eval "$(jenv init -)"
    ==> Analytics
    install: 9,672 (30 days), 30,663 (90 days), 129,619 (365 days)
    install-on-request: 9,670 (30 days), 30,635 (90 days), 128,558 (365 days)
    build-error: 0 (30 days)
    ```
    
    With fish set as default shell:
    
    ```
    brew info jenv
    
    jenv: stable 0.5.4, HEAD
    Manage your Java environment
    https://www.jenv.be/
    /opt/homebrew/Cellar/jenv/0.5.4 (82 files, 72.3KB) *
      Built from source on 2021-05-07 at 17:19:07
    From: https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/jenv.rb
    License: MIT
    ==> Options
    --HEAD
    	Install HEAD version
    ==> Caveats
    To activate jenv, run the following commands:
    
      echo 'status --is-interactive; and source (jenv init -|psub)' >> ~/.config/fish/config.fish"
      ln -sf /opt/homebrew/opt/jenv/libexec/fish/jenv.fish ~/.config/fish/functions/jenv.fish"
      ln -sf /opt/homebrew/opt/jenv/libexec/fish/export.fish ~/.config/fish/functions/export.fish"
    
    ==> Analytics
    install: 9,672 (30 days), 30,663 (90 days), 129,619 (365 days)
    install-on-request: 9,670 (30 days), 30,635 (90 days), 128,558 (365 days)
    build-error: 0 (30 days)
    ```
    
    1: https://github.com/jenv/jenv/blob/9bbc5ebfe6f38252a1a0f28897c7ae52e6a483a0/README.md
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Wes Morgan <github@wesmorgan.me>

--- Formula/jenv.rb
+++ Formula/jenv.rb
@@ -12,15 +12,22 @@ class Jenv < Formula
   def install
     libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/jenv"
+    fish_function.install_symlink Dir[libexec/"fish/*.fish"]
   end
 
   def caveats
-    <<~EOS
-      To activate jenv, add the following to your #{shell_profile}:
-
-        export PATH="$HOME/.jenv/bin:$PATH"
-        eval "$(jenv init -)"
-    EOS
+    if preferred == :fish
+      <<~EOS
+        To activate jenv, run the following commands:
+          echo 'status --is-interactive; and source (jenv init -|psub)' >> #{shell_profile}
+      EOS
+    else
+      <<~EOS
+        To activate jenv, add the following to your #{shell_profile}:
+          export PATH="$HOME/.jenv/bin:$PATH"
+          eval "$(jenv init -)"
+      EOS
+    end
   end
 
   test do
