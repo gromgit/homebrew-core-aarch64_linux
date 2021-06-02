a7787dca7148f19b0ef36f3f6f5f28c21dc88199
rui <chenrui333@gmail.com>
2021-06-02 01:17:53 -0400

    basex 9.5.2 (#78496)

--- Formula/basex.rb
+++ Formula/basex.rb
@@ -1,9 +1,9 @@
 class Basex < Formula
   desc "Light-weight XML database and XPath/XQuery processor"
   homepage "https://basex.org"
-  url "https://files.basex.org/releases/9.5.1/BaseX951.zip"
-  version "9.5.1"
-  sha256 "bfcb356c232fcc6bb14a875bc83f1ebeb6d4604c6aaae6d8bd63376b6b03b5f8"
+  url "https://files.basex.org/releases/9.5.2/BaseX952.zip"
+  version "9.5.2"
+  sha256 "82c61d94bf88d140c499af8d78be544b37fc73831e6744624d4ed4eb836281e6"
   license "BSD-3-Clause"
 
   livecheck do
