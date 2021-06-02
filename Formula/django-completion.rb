6c3bc191ab16273acc8f2e7e957cfa8a5132f5fe
Rui Chen <rui@chenrui.dev>
2021-06-02 13:32:37 -0400

    django-completion 3.2.4
    Closes #78557.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://www.djangoproject.com/"
-  url "https://github.com/django/django/archive/3.2.3.tar.gz"
-  sha256 "854da43b18bade10f5d0c3a01919fa396037f4630b142ad81aa365295b3bb733"
+  url "https://github.com/django/django/archive/3.2.4.tar.gz"
+  sha256 "7d909a324986f961e5407d0cab072f20c9b60a861164f1a045d08850d1d5acc2"
   license "BSD-3-Clause"
   head "https://github.com/django/django.git"
 
