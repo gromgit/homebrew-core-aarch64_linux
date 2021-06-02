329bb66ee301bb7bfbccc862b4eabb04d9eff80e
Rui Chen <rui@chenrui.dev>
2021-06-01 20:53:58 -0400

    libolm 3.2.4
    Closes #78515.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libolm.rb
+++ Formula/libolm.rb
@@ -1,8 +1,8 @@
 class Libolm < Formula
   desc "Implementation of the Double Ratchet cryptographic ratchet"
   homepage "https://gitlab.matrix.org/matrix-org/olm"
-  url "https://gitlab.matrix.org/matrix-org/olm/-/archive/3.2.3/olm-3.2.3.tar.gz"
-  sha256 "59e698b53dc62d4812d968275f4e44ec335f4ad60db37e06baf6c6bf4ec3ba3f"
+  url "https://gitlab.matrix.org/matrix-org/olm/-/archive/3.2.4/olm-3.2.4.tar.gz"
+  sha256 "d6033363fe27bb1ad88940f2acfff5517b1ba9e32b556f5b0fa4c365e5ec892a"
   license "Apache-2.0"
 
   bottle do
