975214c0588684890a0732d7cabddba96d0d70f4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-02 21:44:36 +0000

    argocd-autopilot: add 0.1.10 bottle.

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -6,6 +6,13 @@ class ArgocdAutopilot < Formula
       revision: "646b3faa598281d0404c4b85443361597173c6c7"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4d0cb9353327224ee04332921d5acc88155d92a58d3f33201959e9a3b13d89ab"
+    sha256 cellar: :any_skip_relocation, big_sur:       "331e8fffb55e6327fc60df586246be84e1b9c1cce2dae94e5c3e0d4ff511f807"
+    sha256 cellar: :any_skip_relocation, catalina:      "e183527bbf7b27b290efe4ae9a73906bb72713b4190b3193f991b92780443272"
+    sha256 cellar: :any_skip_relocation, mojave:        "8e52e84b3fd240a11e990afad06acc70650891321aaef3b599a93105b76b29d7"
+  end
+
   depends_on "go" => :build
 
   def install
