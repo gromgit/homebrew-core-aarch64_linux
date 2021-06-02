ae975e360bb64982dd65118e78417a33c084fb27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-02 16:34:43 +0000

    pdftoipe: update 7.2.20.1_10 bottle.

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -7,10 +7,10 @@ class Pdftoipe < Formula
   revision 10
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c54d66d8475f078c9ab51fd0d0ce140c581072d4b107c5fb4de15c4ac7c9b05e"
-    sha256 cellar: :any, big_sur:       "4d4ef2a27b6a87242238380d37fdb228ba3d77e0aba0ab085850844588d8d618"
-    sha256 cellar: :any, catalina:      "80d7de3d886fca532162dd265dbc121eea9b3e6c9a989a848048c4165e40ed0a"
-    sha256 cellar: :any, mojave:        "1a7aaad3dd9a5d77c4b5c24b9fb50b281f7cad9249c356abf414e194269fc20d"
+    sha256 cellar: :any, arm64_big_sur: "4f92fbb31e4351fd4f18de880b11a4f211d9129608e1d69a998065ae6f7c2cf0"
+    sha256 cellar: :any, big_sur:       "e16cd0ace3188156bd07f56073d1670407178662ad6554cdb7c62697807de0d5"
+    sha256 cellar: :any, catalina:      "abc528000696cc93745b45345ad85fb752914a50e85686b9340ad23497a767e1"
+    sha256 cellar: :any, mojave:        "1e5b6d720afc7a4f0cf8eb66b5637d5a9d85bb3512da7449d06cac3aa43a2c70"
   end
 
   depends_on "pkg-config" => :build
