a58d17c199a438ff837f1ee2d8673f1e8c8ee775
Rui Chen <rui@chenrui.dev>
2021-06-02 13:40:23 -0400

    gsl 2.7
    Closes #78559.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -1,9 +1,9 @@
 class Gsl < Formula
   desc "Numerical library for C and C++"
   homepage "https://www.gnu.org/software/gsl/"
-  url "https://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz"
-  sha256 "b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8"
+  url "https://ftp.gnu.org/gnu/gsl/gsl-2.7.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.7.tar.gz"
+  sha256 "efbbf3785da0e53038be7907500628b466152dbc3c173a87de1b5eba2e23602b"
   license "GPL-3.0-or-later"
 
   bottle do
