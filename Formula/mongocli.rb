364fed45dbe631a202e12c375c434daca53fa73a
Gustavo Bazan <gssbzn@gmail.com>
2021-06-02 20:14:26 +0100

    mongocli 1.16.0 (new formula)
    Closes #78704.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/mongocli.rb
@@ -0,0 +1,29 @@
+class Mongocli < Formula
+  desc "MongoDB CLI enables you to manage your MongoDB in the Cloud"
+  homepage "https://github.com/mongodb/mongocli"
+  url "https://github.com/mongodb/mongocli/archive/refs/tags/v1.16.0.tar.gz"
+  sha256 "184f0a13ca0cfb642203727b4971622a761c77ac85066fc51689356690f5e2d0"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    with_env(
+      MCLI_VERSION: version.to_s,
+      MCLI_GIT_SHA: "homebrew-release",
+    ) do
+      system "make", "build"
+    end
+    bin.install "bin/mongocli"
+
+    (bash_completion/"mongocli").write Utils.safe_popen_read(bin/"mongocli", "completion", "bash")
+    (fish_completion/"mongocli.fish").write Utils.safe_popen_read(bin/"mongocli", "completion", "fish")
+    (zsh_completion/"_mongocli").write Utils.safe_popen_read(bin/"mongocli", "completion", "zsh")
+  end
+
+  test do
+    assert_match "mongocli version: #{version}", shell_output("#{bin}/mongocli --version")
+    assert_match "Error: missing credentials", shell_output("#{bin}/mongocli iam projects ls 2>&1", 1)
+    assert_match "PROFILE NAME", shell_output("#{bin}/mongocli config ls")
+  end
+end
