b1c1cb313a7bd3a6ad2407b529aeb63899da41b1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-02 13:51:37 +0000

    gpg-tui: add 0.1.5 bottle.

--- Formula/gpg-tui.rb
+++ Formula/gpg-tui.rb
@@ -5,6 +5,13 @@ class GpgTui < Formula
   sha256 "eccfaa3089605bb6df2cc49df208a93cc452d8a819b444bf82dfeb90739135ea"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "a9775e6c21edac62cc8bbdcfec8f858ee0694fa17dc7ccad47c6d636f1414221"
+    sha256 cellar: :any, big_sur:       "e15ed192dc093a8eaf2aa76459e729e8901a9ccecaf957b53eb41a4a88aa668e"
+    sha256 cellar: :any, catalina:      "6c05eaf37cbbc14b204642cb69de853772ad14fadb9fcc1789dabd4e283c46c6"
+    sha256 cellar: :any, mojave:        "dfc772db8af73ce6208f3af0a9da3c0f2d4799d2c771f0ff2bb670b2d0a2063c"
+  end
+
   depends_on "rust" => :build
   depends_on "gnupg"
   depends_on "gpgme"
