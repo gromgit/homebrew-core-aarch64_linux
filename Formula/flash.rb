cd515f21c2223a136e245b0c8a3505a706c64359
Rui Chen <rui@chenrui.dev>
2021-06-03 00:53:21 -0400

    flash 2.7.2
    Closes #78607.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flash.rb
+++ Formula/flash.rb
@@ -1,8 +1,8 @@
 class Flash < Formula
   desc "Command-line script to flash SD card images of any kind"
   homepage "https://github.com/hypriot/flash"
-  url "https://github.com/hypriot/flash/releases/download/2.7.1/flash"
-  sha256 "879057fea97c791a812e5c990d4ea07effd02406d3a267a9b24285c31ea6db3f"
+  url "https://github.com/hypriot/flash/releases/download/2.7.2/flash"
+  sha256 "571d9e6424b275859a9273029a2321245888ab201dbae1a3ec57a6ef708adce1"
   license "MIT"
 
   bottle do
