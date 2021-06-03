123a0f284d3334d9a0ccd2e1206863d34c4995e2
Michael Cho <cho-m@tuta.io>
2021-06-03 07:04:00 -0700

    wiremock-standalone 2.28.0
    Closes #78640.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wiremock-standalone.rb
+++ Formula/wiremock-standalone.rb
@@ -1,21 +1,16 @@
 class WiremockStandalone < Formula
   desc "Simulator for HTTP-based APIs"
   homepage "http://wiremock.org/docs/running-standalone/"
-  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-standalone/2.27.2/wiremock-standalone-2.27.2.jar"
-  sha256 "00b14f9d6399a9bba96139f84354f981d9fd1c3f4cf6938d36391f292f76382b"
+  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.28.0/wiremock-jre8-standalone-2.28.0.jar"
+  sha256 "d48710086f13108843976e0910f08114d5de65fb7274d8c34b55372a101e52b6"
   license "Apache-2.0"
   head "https://github.com/tomakehurst/wiremock.git"
-
-  bottle :unneeded
 
   depends_on "openjdk"
 
   def install
-    libexec.install "wiremock-standalone-#{version}.jar"
-    (bin/"wiremock").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/wiremock-standalone-#{version}.jar" "$@"
-    EOS
+    libexec.install "wiremock-jre8-standalone-#{version}.jar"
+    bin.write_jar_script libexec/"wiremock-jre8-standalone-#{version}.jar", "wiremock"
   end
 
   test do
