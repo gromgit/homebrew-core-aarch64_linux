3ba9208ee8447de871259531efc6c6727cdc4b4b
Luke Arms <luke@linacreative.com>
2021-06-03 14:33:04 +1000

    osinfo-db: new formula

---
+++ Formula/osinfo-db.rb
@@ -0,0 +1,22 @@
+class OsinfoDb < Formula
+  desc "Osinfo database of operating systems for virtualization provisioning tools"
+  homepage "https://libosinfo.org/"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20210531.tar.xz"
+  sha256 "e46d769692e5bd985abe2e376209adafec33db77f10b35999c71b7a140963ec0"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://releases.pagure.org/libosinfo/?C=M&O=D"
+    regex(/href=.*?osinfo-db[._-]v?(\d+(?:\.\d+)*)\.t/i)
+  end
+
+  depends_on "osinfo-db-tools" => [:build, :test]
+
+  def install
+    system "osinfo-db-import", "--dir=#{share}/osinfo", cached_download
+  end
+
+  test do
+    system "osinfo-db-validate", "--system"
+  end
+end
