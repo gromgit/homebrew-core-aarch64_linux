73265b5110c49d155bf7d49097a5fc29964235c8
Rui Chen <rui@chenrui.dev>
2021-06-03 01:02:06 -0400

    openimageio 2.2.15.0
    Closes #78619.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openimageio.rb
+++ Formula/openimageio.rb
@@ -1,10 +1,9 @@
 class Openimageio < Formula
   desc "Library for reading, processing and writing images"
   homepage "https://openimageio.org/"
-  url "https://github.com/OpenImageIO/oiio/archive/Release-2.2.14.0.tar.gz"
-  sha256 "e41b4b6958d318250caa1d1f167863a915a99062273583bb96457101d54e89cd"
+  url "https://github.com/OpenImageIO/oiio/archive/Release-2.2.15.0.tar.gz"
+  sha256 "5861e2533d7c8f7ca61631abe99e3882f94502617a7ff3ad654f8ea48e0fbd16"
   license "BSD-3-Clause"
-  revision 3
   head "https://github.com/OpenImageIO/oiio.git"
 
   livecheck do
