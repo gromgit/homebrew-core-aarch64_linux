6200c6e69bdaab245f2b4e1c278cdf51cf95de9e
fʗɑρ <kapfab@github.somesite.net>
2021-06-03 21:23:47 +0200

    sonar-scanner: fix OpenJDK dependency
    Closes #78661.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sonar-scanner.rb
+++ Formula/sonar-scanner.rb
@@ -4,13 +4,14 @@ class SonarScanner < Formula
   url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472.zip"
   sha256 "344bfeff44b09a11082b4a4646b1ed14f213feb00a5cd6d01c86f3767cb32471"
   license "LGPL-3.0-or-later"
+  revision 1
   head "https://github.com/SonarSource/sonar-scanner-cli.git"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "06bb168ce2572d35e6db64466fa5e0eb0e5f94aac94cd70e41fad709ee523992"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@11"
 
   def install
     rm_rf Dir["bin/*.bat"]
@@ -18,7 +19,7 @@ class SonarScanner < Formula
     bin.install libexec/"bin/sonar-scanner"
     etc.install libexec/"conf/sonar-scanner.properties"
     ln_s etc/"sonar-scanner.properties", libexec/"conf/sonar-scanner.properties"
-    bin.env_script_all_files libexec/"bin/", SONAR_SCANNER_HOME: libexec, JAVA_HOME: Formula["openjdk"].opt_prefix
+    bin.env_script_all_files libexec/"bin/", SONAR_SCANNER_HOME: libexec, JAVA_HOME: Formula["openjdk@11"].opt_prefix
   end
 
   test do
