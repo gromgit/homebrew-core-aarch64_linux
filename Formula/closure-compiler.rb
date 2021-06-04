1cd0277d4ab18ae120e952cfde9ac2065e40ec7e
Rui Chen <rui@chenrui.dev>
2021-06-04 00:43:52 -0400

    closure-compiler 20210601
    * closure-compiler 20210601
    * closure-compiler: remove bottle: :unneeded
    
    Closes #78692.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -1,16 +1,14 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://developers.google.com/closure/compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20210505/closure-compiler-v20210505.jar"
-  sha256 "0936c500f316d8a7a4e5739ab7cbed21f822e47e257cf2b8196e40793d125678"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20210601/closure-compiler-v20210601.jar"
+  sha256 "64f161c65a3dba42c9a4f5a79db335ac0e757fcba486cd7928205ac4c258ef08"
   license "Apache-2.0"
 
   livecheck do
     url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/"
     regex(/href=.*?v?(\d{8})/i)
   end
-
-  bottle :unneeded
 
   depends_on "openjdk"
 
