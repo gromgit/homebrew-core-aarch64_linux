169df835ea08ff76505c26bca4ac1e917d41b5bb
Michael Cho <cho-m@tuta.io>
2021-06-04 03:07:10 -0700

    ubertooth 2020-12-R1
    Closes #78712.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ubertooth.rb
+++ Formula/ubertooth.rb
@@ -1,10 +1,10 @@
 class Ubertooth < Formula
   desc "Host tools for Project Ubertooth"
   homepage "https://github.com/greatscottgadgets/ubertooth/wiki"
-  url "https://github.com/greatscottgadgets/ubertooth/releases/download/2018-12-R1/ubertooth-2018-12-R1.tar.xz"
-  version "2018-12-R1"
-  sha256 "0042daa79db0f4148a0255cdf05aa57006e23ac36edf7024e9e99ccc4892867b"
-  license "GPL-2.0"
+  url "https://github.com/greatscottgadgets/ubertooth/releases/download/2020-12-R1/ubertooth-2020-12-R1.tar.xz"
+  version "2020-12-R1"
+  sha256 "93a4ce7af8eddcc299d65aff8dd3a0455293022f7fea4738b286353f833bf986"
+  license "GPL-2.0-or-later"
   head "https://github.com/greatscottgadgets/ubertooth.git"
 
   bottle do
