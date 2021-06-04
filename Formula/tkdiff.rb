d159d1b0cf5529ae79067db83e792de8bf6a9ddf
Michael Cho <cho-m@tuta.io>
2021-06-04 09:47:52 -0700

    tkdiff 5.2.1
    Closes #78727.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tkdiff.rb
+++ Formula/tkdiff.rb
@@ -1,17 +1,15 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.1/tkdiff-5-1.zip"
-  version "5.1"
-  sha256 "016569ac1e10b4bfd7c6ce34fece75ee486577580087666dcbb8c36fd7eee87a"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.2.1/tkdiff-5-2-1.zip"
+  version "5.2.1"
+  sha256 "942bf2431b409ecfdcd1789b9911888f69908e63e02c923bb5b04b2aeb14633d"
   license "GPL-2.0-only"
 
   livecheck do
     url :stable
     regex(%r{url=.*?/tkdiff/v?(\d+(?:\.\d+)+)/[^"]+?\.zip}i)
   end
-
-  bottle :unneeded
 
   def install
     bin.install "tkdiff"
