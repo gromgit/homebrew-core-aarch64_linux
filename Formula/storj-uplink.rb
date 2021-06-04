9bb86696418004e321e82535ad1755ef04747905
Sam Ford <1584702+samford@users.noreply.github.com>
2021-06-04 15:40:15 -0400

    storj-uplink: add livecheck

--- Formula/storj-uplink.rb
+++ Formula/storj-uplink.rb
@@ -5,6 +5,11 @@ class StorjUplink < Formula
   sha256 "e26d9bbea734245bf63171224b30661c864bafc699cf93ffb268bac25cd74ab5"
   license "AGPL-3.0-only"
 
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "be143d195b4e7dedb904649ed701007d5fbfddddaee168f4ef4d41196a152f07"
     sha256 cellar: :any_skip_relocation, big_sur:       "caa8f81fc92c06e909694ec21ee407fdf9e4a2175d8d810c763a42f42d21a8c6"
