ff188944df5a808befc975675ce3fd84e8d8121c
Rui Chen <rui@chenrui.dev>
2021-06-04 17:22:07 -0400

    artifactory 6.23.21
    Closes #78752.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/artifactory.rb
+++ Formula/artifactory.rb
@@ -3,8 +3,8 @@ class Artifactory < Formula
   homepage "https://www.jfrog.com/artifactory/"
   # v7 is available but does contain a number of pre-builts that need to be avoided.
   # Note that just using the source archive is not sufficient.
-  url "https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/6.23.19/jfrog-artifactory-oss-6.23.19.zip"
-  sha256 "44de0b5e8306124343558cfc703a1e7aaf4bf340a11e29780412dca82027bfe9"
+  url "https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/6.23.21/jfrog-artifactory-oss-6.23.21.zip"
+  sha256 "086263fdf259bd9115c9e313f8401ae321c3cd57a7be8823ef8416254c41e2a1"
   license "AGPL-3.0-or-later"
 
   livecheck do
