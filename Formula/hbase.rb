455665fb48a42f7c8b8c9efcffc07322194d8c0c
Caleb Xu <calebcenter@live.com>
2021-06-04 08:06:02 -0400

    hbase 2.4.3
    Closes #78717.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hbase.rb
+++ Formula/hbase.rb
@@ -1,9 +1,9 @@
 class Hbase < Formula
   desc "Hadoop database: a distributed, scalable, big data store"
   homepage "https://hbase.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=hbase/2.4.2/hbase-2.4.2-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/hbase/2.4.2/hbase-2.4.2-bin.tar.gz"
-  sha256 "829e5b324fdcb7d291183d7a4c6ba0a7419d2d03ef487a8a137c359fcd073bae"
+  url "https://www.apache.org/dyn/closer.lua?path=hbase/2.4.3/hbase-2.4.3-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/hbase/2.4.3/hbase-2.4.3-bin.tar.gz"
+  sha256 "139fef2135ede2345c479c28c15013c35576852a06b5ea3dbd18710c2837b351"
   license "Apache-2.0"
 
   bottle do
