b5243c2927a6685cc93f64fb4a95e96b488d5c94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-04 05:36:11 +0000

    chafa: update 1.6.1 bottle.

--- Formula/chafa.rb
+++ Formula/chafa.rb
@@ -11,10 +11,10 @@ class Chafa < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5b9d9896282474c7835a356d4be5b1affa9e674ce741d83001062dbd7928de1e"
-    sha256 cellar: :any, big_sur:       "4033db8327ba1738f613f7892bb086a960f5c0239fed8a203ff18600548f98ae"
-    sha256 cellar: :any, catalina:      "cd05130e3b8679f1d31f1f64c8bb56103aca6545b44a2964a32d4b474b2fcf5e"
-    sha256 cellar: :any, mojave:        "efcc8ce344097a5e19db9129d7c30cd2852a74d5033eaf3bfcab62d4d7470d62"
+    sha256 cellar: :any, arm64_big_sur: "da7d3ed292b63f22c33d46bf1b029c7eae4b1dcd1ed70dac7d6ea283159e919b"
+    sha256 cellar: :any, big_sur:       "ace4589475844621942acda0985939f6c47e186aae7a223d69bbe518f2f86c7b"
+    sha256 cellar: :any, catalina:      "f2318ad62d9921d5ae1dfd2c1625102145f873ce8d0b5f6d3edcde7875fcaf1d"
+    sha256 cellar: :any, mojave:        "9e9615a1c143088bed4f8ae1b3bf0d0b2e9ddf3a5b9f69bf968313df4e0aaf4d"
   end
 
   depends_on "pkg-config" => :build
