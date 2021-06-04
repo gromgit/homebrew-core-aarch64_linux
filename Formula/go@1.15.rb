7631d31d80d61c06b4c9e6f4d0c5b11bacda5a53
Rui Chen <rui@chenrui.dev>
2021-06-03 20:49:12 -0400

    go@1.15 1.15.13
    Closes #78679.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/go@1.15.rb
+++ Formula/go@1.15.rb
@@ -1,9 +1,9 @@
 class GoAT115 < Formula
   desc "Go programming environment (1.15)"
   homepage "https://golang.org"
-  url "https://golang.org/dl/go1.15.12.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.15.12.src.tar.gz"
-  sha256 "1c6911937df4a277fa74e7b7efc3d08594498c4c4adc0b6c4ae3566137528091"
+  url "https://golang.org/dl/go1.15.13.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.15.13.src.tar.gz"
+  sha256 "99069e7223479cce4553f84f874b9345f6f4045f27cf5089489b546da619a244"
   license "BSD-3-Clause"
 
   livecheck do
