2f20c1ebc98d47b8bcd7382e7b51a38610513a89
Anmol Khirbat <anmol@khirbat.net>
2021-06-05 09:52:26 -0700

    x86_64-elf-gdb 10.2

--- Formula/x86_64-elf-gdb.rb
+++ Formula/x86_64-elf-gdb.rb
@@ -1,9 +1,9 @@
 class X8664ElfGdb < Formula
-  desc "GNU debugger for i386-elf cross development"
+  desc "GNU debugger for x86_64-elf cross development"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-10.1.tar.xz"
-  sha256 "f82f1eceeec14a3afa2de8d9b0d3c91d5a3820e23e0a01bbb70ef9f0276b62c0"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-10.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-10.2.tar.xz"
+  sha256 "aaa1223d534c9b700a8bec952d9748ee1977513f178727e1bee520ee000b4f29"
   license "GPL-3.0-or-later"
   head "https://sourceware.org/git/binutils-gdb.git"
 
@@ -15,20 +15,30 @@ class X8664ElfGdb < Formula
     sha256 high_sierra:   "bc725f791779a9400ab2b11d485ebba7e71aced9e6cef3365db614dbb273205c"
   end
 
+  depends_on "x86_64-elf-gcc" => :test
   depends_on "python@3.9"
   depends_on "xz"
 
-  conflicts_with "gdb", because: "both install include/gdb, share/gdb and share/info"
-  conflicts_with "i386-elf-gdb", because: "both install include/gdb, share/gdb and share/info"
+  uses_from_macos "zlib"
+
+  # Fix for https://sourceware.org/bugzilla/show_bug.cgi?id=26949#c8
+  # Remove when upstream includes this commit
+  # https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;h=b413232211bf
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/242630de4b54d6c57721e12ce88988a0f4e41202/gdb/gdb-10.2.patch"
+    sha256 "36652e9d97037266650a3b31f9f39539c4b376d31016fa4fc325dc0aa7930acc"
+  end
 
   def install
     args = %W[
       --target=x86_64-elf
       --prefix=#{prefix}
+      --datarootdir=#{share}/x86_64-elf-gdb
       --disable-debug
       --disable-dependency-tracking
       --with-lzma
       --with-python=#{Formula["python@3.9"].opt_bin}/python3
+      --with-system-zlib
       --disable-binutils
     ]
 
@@ -38,9 +48,14 @@ class X8664ElfGdb < Formula
 
       system "make", "install-gdb"
     end
+
+    mv include/"gdb", include/"x86_64-elf-gdb"
   end
 
   test do
-    system "#{bin}/x86_64-elf-gdb", "#{bin}/x86_64-elf-gdb", "-configuration"
+    (testpath/"test.c").write "void _start(void) {}"
+    system "#{Formula["x86_64-elf-gcc"].bin}/x86_64-elf-gcc", "-g", "-nostdlib", "test.c"
+    assert_match "Symbol \"_start\" is a function at address 0x",
+          shell_output("#{bin}/x86_64-elf-gdb -batch -ex 'info address _start' a.out")
   end
 end
