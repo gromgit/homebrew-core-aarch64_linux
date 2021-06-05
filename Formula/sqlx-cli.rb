67bd2ea152c355abd99b193d2294861aaf7c4b23
Dawid Dziurla <dawidd0811@gmail.com>
2021-06-05 11:20:45 +0000

    sqlx-cli: add 0.5.5 bottle.

--- Formula/sqlx-cli.rb
+++ Formula/sqlx-cli.rb
@@ -5,6 +5,13 @@ class SqlxCli < Formula
   sha256 "4e51566090a23dd5c9e1fabb70f3b15db8e2e25303f3fc9074ae81bfc7e48276"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e41d0e307e9d17e8966aed83c29fad8373e82bb9d5a6a3dc6f37a133032277b8"
+    sha256 cellar: :any_skip_relocation, big_sur:       "11f11e3eda3869c7cd10e8fd10625f6db88b558e9bc631727dd2b41b6ae6ac7e"
+    sha256 cellar: :any_skip_relocation, catalina:      "cbbc676a115e5d2ddf94ec8c78fc83e50898a3189c51079608baa72f039792d4"
+    sha256 cellar: :any_skip_relocation, mojave:        "5ce17dc84ca3c80c39e379d2f3e9d7f6bf8d612af07850dc3188d1bfd7edcb10"
+  end
+
   depends_on "rust" => :build
 
   def install
