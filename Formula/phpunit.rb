da88a3f820d19886b89d021411ef2b74cdd5b571
homebrew-updater <bepsvpt/homebrew-updater>
2021-06-05 13:00:11 +0800

    phpunit 9.5.5
    * phpunit 9.5.5
    * phpunit: remove revision
    
    Closes #78784.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,10 +1,9 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.5.4.phar"
-  sha256 "79caa5773272293ebcfcd4bfd68ecac7707fc71019bfb17aac6884ef0a11d005"
+  url "https://phar.phpunit.de/phpunit-9.5.5.phar"
+  sha256 "8136c06c3a91ec3967bcc28bb8924921035b6d3283ed35abca238dfabeb04669"
   license "BSD-3-Clause"
-  revision 1
 
   livecheck do
     url "https://phar.phpunit.de/"
