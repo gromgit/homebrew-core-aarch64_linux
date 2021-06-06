56cb050bec822a57a00deb7ca1620bd192c4b64c
Nanda H Krishna <me@nandahkrishna.com>
2021-06-06 22:20:57 +0000

    typedb: add 2.1.2 bottle.

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -5,6 +5,10 @@ class Typedb < Formula
   sha256 "b420bdf20ea7db7956e36c4bc2ddf7f42f3ea4f1523363b398578da476bfd86e"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "eb1c1d2c30e60d3f3a4838574effa9a93dc130e175268cb3c2b6d889c931e6f3"
+  end
+
   depends_on "openjdk@11"
 
   def install
