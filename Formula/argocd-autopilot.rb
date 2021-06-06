4f97d2b85b0612e2fa3a1975a3a667297a694763
roi.kramer <roi.kramer@codefresh.io>
2021-06-06 11:17:59 +0300

    argocd-autopilot: set DEV_MODE to false
    Closes #78824.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -5,6 +5,7 @@ class ArgocdAutopilot < Formula
       tag:      "v0.1.10",
       revision: "646b3faa598281d0404c4b85443361597173c6c7"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "4d0cb9353327224ee04332921d5acc88155d92a58d3f33201959e9a3b13d89ab"
@@ -16,7 +17,7 @@ class ArgocdAutopilot < Formula
   depends_on "go" => :build
 
   def install
-    system "make", "cli-package"
+    system "make", "cli-package", "DEV_MODE=false"
     bin.install "dist/argocd-autopilot"
   end
 
