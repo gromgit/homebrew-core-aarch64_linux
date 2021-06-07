492c98c39eae5cee21b36fffaca03b99e6ebd9e4
Nanda H Krishna <me@nandahkrishna.com>
2021-06-07 07:46:33 +0000

    libmobi: add 0.6 bottle.

--- Formula/libmobi.rb
+++ Formula/libmobi.rb
@@ -5,6 +5,13 @@ class Libmobi < Formula
   sha256 "c35bd44279575bf8b102d23eba482805bfc1e5a49df8414d851507e8ea811c5d"
   license "LGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "c5278432f0d13b9e7df78a64f94f554fd8a18810e109a62b556e1ee135c5e9c7"
+    sha256 cellar: :any, big_sur:       "09fcc2ffd8033d5406a770484b0dc6c18fec71a67264c77ad4f93af4302c8697"
+    sha256 cellar: :any, catalina:      "dda06a2599862e896c17decedb77f6d2a3dae354c4b17fe33ab21ff242a7b73c"
+    sha256 cellar: :any, mojave:        "aa60c60d6d6443b2a5a80ac507c554576f601ff06dd069577b7be89ab3a17f34"
+  end
+
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
