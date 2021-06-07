6914fc166411fb9bbf6a69a4d620dc4257e54efa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-07 07:36:26 +0000

    dhall-json: update 1.7.7 bottle.

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -7,9 +7,9 @@ class DhallJson < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "d1dd64ef3b551cb9bd3ec4de1ddc336457ca588421b2b5dab1f6ecbc609fd4b0"
-    sha256 cellar: :any_skip_relocation, catalina: "b75a7ef02eefee86b173f6e8c7b6eff5e3247a319b86a2d3f37abc86e7230cee"
-    sha256 cellar: :any_skip_relocation, mojave:   "004e35525351878b319875287e858d58b4f0acad0839fc3a00122364c8048c2d"
+    sha256 cellar: :any_skip_relocation, big_sur:  "651c2889bce3cd07dbd893c44ef8039b625eb2954d72d54e466f59bbc896a2e8"
+    sha256 cellar: :any_skip_relocation, catalina: "264c20e5927ecfca56518c9954b71a466b1ef1843fb290902837533484021c85"
+    sha256 cellar: :any_skip_relocation, mojave:   "224146b1c09892063a492f8fcbdc2f312abf439587da413414377d85b6e419aa"
   end
 
   depends_on "cabal-install" => :build
