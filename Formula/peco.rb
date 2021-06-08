2b6d3337cd18a5659dc490a197e43afb8cfd48ec
Alexander Bayandin <a.bayandin@gmail.com>
2021-06-08 10:27:08 +0100

    peco 0.5.10
    Closes #78923.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/peco.rb
+++ Formula/peco.rb
@@ -1,8 +1,8 @@
 class Peco < Formula
   desc "Simplistic interactive filtering tool"
   homepage "https://github.com/peco/peco"
-  url "https://github.com/peco/peco/archive/v0.5.8.tar.gz"
-  sha256 "90d87503265c12f8583f5c6bc19c83eba7a2e15219a6339d5041628aa48c4705"
+  url "https://github.com/peco/peco/archive/refs/tags/v0.5.10.tar.gz"
+  sha256 "781c2effc4f6a58d9ff96fb0fc8b0fba3aab56a91a34933d68c5de3aea5fe3f6"
   license "MIT"
   head "https://github.com/peco/peco.git"
 
