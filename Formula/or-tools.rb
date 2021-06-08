306e5074ecdb965a565d6f3ca7edaa33c00f7080
Thierry Moisan <thierry.moisan@gmail.com>
2021-06-08 13:55:34 -0400

    or-tools: revision bump for protobuf 3.17.3

--- Formula/or-tools.rb
+++ Formula/or-tools.rb
@@ -4,7 +4,7 @@ class OrTools < Formula
   url "https://github.com/google/or-tools/archive/v9.0.tar.gz"
   sha256 "fa7700b614ea2a5b2b6e37b76874bd2c3f04a80f03cbbf7871a2d2d5cd3a6091"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/google/or-tools.git"
 
   livecheck do
