4db8b35e9a88fdd68c4540d54b740dee58fd800e
Rui Chen <rui@chenrui.dev>
2021-06-08 10:14:13 -0400

    serverless 2.45.0
    Closes #78935.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://www.serverless.com/"
-  url "https://github.com/serverless/serverless/archive/v2.44.0.tar.gz"
-  sha256 "f1418e66dd71719a3b10b09847437baa125529f0528d69195a83cd2256b568bf"
+  url "https://github.com/serverless/serverless/archive/v2.45.0.tar.gz"
+  sha256 "602b4214bbc36fa094aed000e148cba89f2a48b30702570eb317c1e007d463eb"
   license "MIT"
 
   bottle do
