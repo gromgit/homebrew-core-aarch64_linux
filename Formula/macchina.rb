b0276dde0489e25eaa63592989d5d1902c95b860
Rui Chen <rui@chenrui.dev>
2021-06-09 02:24:38 -0400

    macchina 0.9.2
    Closes #79002.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -1,8 +1,8 @@
 class Macchina < Formula
   desc "System information fetcher, with an emphasis on performance and minimalism"
   homepage "https://github.com/Macchina-CLI/macchina"
-  url "https://github.com/Macchina-CLI/macchina/archive/v0.9.1.tar.gz"
-  sha256 "fa60093f580ea481647883b75c7b20045b6254ae80c4c1781dfa2e0dd89c6124"
+  url "https://github.com/Macchina-CLI/macchina/archive/v0.9.2.tar.gz"
+  sha256 "f5ac11a49470639d81424b208fc5d95b111a77b97138af7fce347ad3daae8005"
   license "MIT"
 
   bottle do
