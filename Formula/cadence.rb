9165476152a2fa1cef562e16bad570a262d0ca11
Rui Chen <rui@chenrui.dev>
2021-06-09 02:26:25 -0400

    cadence 0.17.0
    Closes #79003.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -1,8 +1,8 @@
 class Cadence < Formula
   desc "Resource-oriented smart contract programming language"
   homepage "https://github.com/onflow/cadence"
-  url "https://github.com/onflow/cadence/archive/v0.16.1.tar.gz"
-  sha256 "1e54e4eef29c3c445bbf245701e78a5c88493f5d5ede3389b81018c75dea4c89"
+  url "https://github.com/onflow/cadence/archive/v0.17.0.tar.gz"
+  sha256 "144ce46cbf9ed2b357db324ed83dd240ca09f4bdafab7fdd8b65f88988d81b7b"
   license "Apache-2.0"
   head "https://github.com/onflow/cadence.git"
 
