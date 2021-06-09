bd760a1217446953769dfa0459bb90ab117c1a90
Nanda H Krishna <me@nandahkrishna.com>
2021-06-10 04:26:56 +0530

    taskwarrior-tui: build manpage on ARM
    Closes #79042.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -18,20 +18,20 @@ class TaskwarriorTui < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "f0f49cec64f7aa6f485cd15eb7f97288e874a86a3f319135a0b14ab2e4744e0e"
   end
 
-  depends_on "pandoc" => :build unless Hardware::CPU.arm?
+  depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "task"
 
   def install
     system "cargo", "install", *std_cargo_args
-    unless Hardware::CPU.arm?
-      args = %w[
-        --standalone
-        --to=man
-      ]
-      system "pandoc", *args, "docs/taskwarrior-tui.1.md", "-o", "taskwarrior-tui.1"
-      man1.install "taskwarrior-tui.1"
-    end
+
+    args = %w[
+      --standalone
+      --to=man
+    ]
+    system "pandoc", *args, "docs/taskwarrior-tui.1.md", "-o", "taskwarrior-tui.1"
+    man1.install "taskwarrior-tui.1"
+
     bash_completion.install "completions/taskwarrior-tui.bash"
     fish_completion.install "completions/taskwarrior-tui.fish"
     zsh_completion.install "completions/_taskwarrior-tui"
