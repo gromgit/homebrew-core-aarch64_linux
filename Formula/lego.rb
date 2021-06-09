f1ca26b52992ddf3689560170bfa2099859a195c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-09 07:14:14 +0000

    lego: update 4.4.0 bottle.

--- Formula/lego.rb
+++ Formula/lego.rb
@@ -6,10 +6,10 @@ class Lego < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "98cf7e41c76fcc8f436778c5102d3d813b76fbb38e6d41b890fde18c77a47dd0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0c5efe9cc6e0888bb78e53e089e7c1ac16f15f71e6894c1cbadab80e12b552db"
-    sha256 cellar: :any_skip_relocation, catalina:      "0304f6caefbffc8376d48aaf77e9de929e9e9036a3f825575660b45cd21db558"
-    sha256 cellar: :any_skip_relocation, mojave:        "84bcdd94d87727e85526053220e2baa01195efc9ef795c9d2ac4e596074dbc18"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bde260758c983529fde3c977a23e4e3e4e664749ddb97540c635720b49306c3e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2c675b222bc4565645416446e6d1332b4feceec7a86dd43b8a6d7831b4247a7a"
+    sha256 cellar: :any_skip_relocation, catalina:      "f6161edd3d53d528adb7914e0b3bd8138445defad7b16ccc9e33c445c30ee191"
+    sha256 cellar: :any_skip_relocation, mojave:        "ee323c32f8bc460f203dc515c1cc0e32facc76844ff9bd31e5629ef42de8914b"
   end
 
   depends_on "go" => :build
