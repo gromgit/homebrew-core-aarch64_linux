b447c3e70328483e010262bd4e75becf534bca45
Nanda H Krishna <me@nandahkrishna.com>
2021-06-09 23:11:27 +0000

    git-ftp: update 1.6.0 bottle.

--- Formula/git-ftp.rb
+++ Formula/git-ftp.rb
@@ -7,10 +7,11 @@ class GitFtp < Formula
   head "https://github.com/git-ftp/git-ftp.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "2e3d8573c71ae26fdac0d0d8952e625b5a14d90118a6a413604eac8c3a6f6eb6"
-    sha256 cellar: :any, catalina:    "0a61ca11e69370dfecfd3c82d6d03aeec377bf9db660658403556ea71b84bae0"
-    sha256 cellar: :any, mojave:      "f878c4015697794bb8b2c3f034a167b750d3871c0d320d903536128f01880ca2"
-    sha256 cellar: :any, high_sierra: "63c8b94fd89eb635d8c2056efdf933de45dca7fdb04793b620750f8b338fbb88"
+    sha256 cellar: :any, arm64_big_sur: "22f0e6b0a0c16aa110711333e1a44b77d65fe851049db51fb200f33a2e2be534"
+    sha256 cellar: :any, big_sur:       "2e3d8573c71ae26fdac0d0d8952e625b5a14d90118a6a413604eac8c3a6f6eb6"
+    sha256 cellar: :any, catalina:      "0a61ca11e69370dfecfd3c82d6d03aeec377bf9db660658403556ea71b84bae0"
+    sha256 cellar: :any, mojave:        "f878c4015697794bb8b2c3f034a167b750d3871c0d320d903536128f01880ca2"
+    sha256 cellar: :any, high_sierra:   "63c8b94fd89eb635d8c2056efdf933de45dca7fdb04793b620750f8b338fbb88"
   end
 
   depends_on "pandoc" => :build
