bfbd05e644d13d6640f1854de02f4c3a63c42e87
Nanda H Krishna <me@nandahkrishna.com>
2021-06-09 23:15:25 +0000

    ngs: update 0.2.12 bottle.

--- Formula/ngs.rb
+++ Formula/ngs.rb
@@ -7,9 +7,10 @@ class Ngs < Formula
   head "https://github.com/ngs-lang/ngs.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "f92d46bbd5c75caadce87ba7856fd49367cdeae26f94f9875cad7bc3f87187db"
-    sha256 cellar: :any, catalina: "56844fed9b44e5d1cca3634051245eb43f6758f995e1a0ffd8b919e17df51510"
-    sha256 cellar: :any, mojave:   "d5d04636b7d4a6de1028fedbe36fe15c1938d7dd5d5e09a9bfda0680e39d17ad"
+    sha256 cellar: :any, arm64_big_sur: "fc20adda0f39a4e22d54081c55b3299dced4078c96f5e7869d1903ac66c843ea"
+    sha256 cellar: :any, big_sur:       "f92d46bbd5c75caadce87ba7856fd49367cdeae26f94f9875cad7bc3f87187db"
+    sha256 cellar: :any, catalina:      "56844fed9b44e5d1cca3634051245eb43f6758f995e1a0ffd8b919e17df51510"
+    sha256 cellar: :any, mojave:        "d5d04636b7d4a6de1028fedbe36fe15c1938d7dd5d5e09a9bfda0680e39d17ad"
   end
 
   depends_on "cmake" => :build
