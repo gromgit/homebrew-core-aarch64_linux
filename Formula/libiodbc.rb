03a157700777bc64d156ebe9e7b83df6921e5a69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-09 03:19:51 +0000

    libiodbc: update 3.52.15 bottle.

--- Formula/libiodbc.rb
+++ Formula/libiodbc.rb
@@ -6,10 +6,10 @@ class Libiodbc < Formula
   license any_of: ["BSD-3-Clause", "LGPL-2.0-only"]
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9179490e07b80540b31a531babb6168cbd0ea2e10dc994a38f6e5ff8ad2c9f67"
-    sha256 cellar: :any, big_sur:       "981db56543c7f668350a12248aa9047de688e9223bf502a93762ec0e53867d40"
-    sha256 cellar: :any, catalina:      "288802927c2719db23449d5df0ca88c7b9c267c5755a9fa742f5c031d31db552"
-    sha256 cellar: :any, mojave:        "40f53ec2a76bac0bb87697bd2e9cd06ee68d4e83c0e73cd901e569227d6df486"
+    sha256 cellar: :any, arm64_big_sur: "5147321e5b94a093ccb0e3ecf942cc69022c37a79ddac432185be9053353a797"
+    sha256 cellar: :any, big_sur:       "ee25a27296ec772e888b1631f4f937ddba2e848c550f3ae0af70abb3c5089cf9"
+    sha256 cellar: :any, catalina:      "1ef55cd149e392eca7c0708cc24a928d5b762e672e2651902b8fea24f2d76f20"
+    sha256 cellar: :any, mojave:        "e8afda31c8d863560d0b45da2e0b09f452daca6bbe6b37762366743843f0f3b3"
   end
 
   depends_on "autoconf" => :build
