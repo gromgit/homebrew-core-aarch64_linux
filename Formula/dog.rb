e03cf809e2e81b2d6e20f007cb4c50dd39e65f0f
Nanda H Krishna <me@nandahkrishna.com>
2021-06-09 22:58:30 +0000

    dog: update 0.1.0 bottle.

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -7,10 +7,11 @@ class Dog < Formula
   head "https://github.com/ogham/dog.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "0666eb89b99b25ee0c5b2064f2a7a2781a325aded61691b707c6e93c512c652f"
-    sha256 cellar: :any_skip_relocation, catalina:    "21f901c6abb536f0a7bc00d97df6e561aec1acff1759a96a5f69bd51eac1da98"
-    sha256 cellar: :any_skip_relocation, mojave:      "095b6dbb23b96504a6f0e0a292a7157c78e900825e92f4c140cc29643a294e29"
-    sha256 cellar: :any_skip_relocation, high_sierra: "fef7ea4d925e230ba9e75cb051a888263c2bc10fcf26a96899cea2cc8891a302"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9ea5dead4f4fbe142d855e32dbb2a01a311836bdbdec746c62eb21e1c5a8b159"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0666eb89b99b25ee0c5b2064f2a7a2781a325aded61691b707c6e93c512c652f"
+    sha256 cellar: :any_skip_relocation, catalina:      "21f901c6abb536f0a7bc00d97df6e561aec1acff1759a96a5f69bd51eac1da98"
+    sha256 cellar: :any_skip_relocation, mojave:        "095b6dbb23b96504a6f0e0a292a7157c78e900825e92f4c140cc29643a294e29"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "fef7ea4d925e230ba9e75cb051a888263c2bc10fcf26a96899cea2cc8891a302"
   end
 
   depends_on "just" => :build
