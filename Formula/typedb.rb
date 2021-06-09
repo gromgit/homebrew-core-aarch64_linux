bf0f9f5799481a91ff20423f2ed3a251b2665284
Caleb Xu <calebcenter@live.com>
2021-06-08 21:06:37 -0400

    typedb 2.1.3
    Closes #78973.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -1,8 +1,8 @@
 class Typedb < Formula
   desc "Distributed hyper-relational database for knowledge engineering"
   homepage "https://vaticle.com/"
-  url "https://github.com/vaticle/typedb/releases/download/2.1.2/typedb-all-mac-2.1.2.zip"
-  sha256 "b420bdf20ea7db7956e36c4bc2ddf7f42f3ea4f1523363b398578da476bfd86e"
+  url "https://github.com/vaticle/typedb/releases/download/2.1.3/typedb-all-mac-2.1.3.zip"
+  sha256 "17eb3ab0746f52a5c1dfb7f896fbc7381e06f815c88a35dfdc2a085d27a53745"
   license "AGPL-3.0-or-later"
 
   bottle do
