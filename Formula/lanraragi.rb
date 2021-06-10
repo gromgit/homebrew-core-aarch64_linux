d8cdbeb9e2bdf1c0800f15428cd5fc4ca42731d6
Rui Chen <rui@chenrui.dev>
2021-06-09 20:52:59 -0400

    lanraragi 0.7.9
    * lanraragi 0.7.9
    * remove patch
    * update resources
    
    Closes #79060.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lanraragi.rb
+++ Formula/lanraragi.rb
@@ -3,22 +3,10 @@ require "language/node"
 class Lanraragi < Formula
   desc "Web application for archival and reading of manga/doujinshi"
   homepage "https://github.com/Difegue/LANraragi"
+  url "https://github.com/Difegue/LANraragi/archive/v.0.7.9.tar.gz"
+  sha256 "ca14c0d27d9ad28aba835f14c904406414c4094cab904d1d763a0c7608afa5b6"
   license "MIT"
   head "https://github.com/Difegue/LANraragi.git"
-
-  # Remove patch and `stable` block at version bump
-  stable do
-    url "https://github.com/Difegue/LANraragi/archive/v.0.7.8.tar.gz"
-    sha256 "e7deffd7f5b4528d7a7ddeab412d8230571e37d5a5eb8a0f6606e4e6655c22c9"
-
-    # Allow setting `LRR_TEMP_DIRECTORY` to fix test
-    # https://github.com/Difegue/LANraragi/issues/469
-    # Remove at version bump
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/2f6f05abc781d85f891e0b87cda821e5c069abff/lanraragi/tempdir.patch"
-      sha256 "d14dfd68a32e7c0805a488f89644c73ca6472546edfd6118bd6726593adb3b81"
-    end
-  end
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "dfb800e786cac097575e60706c4c08df229a2afc32a9fec859ee81b1503f3bb2"
@@ -43,13 +31,13 @@ class Lanraragi < Formula
   uses_from_macos "libarchive"
 
   resource "Image::Magick" do
-    url "https://cpan.metacpan.org/authors/id/J/JC/JCRISTY/PerlMagick-7.0.10.tar.gz"
-    sha256 "1d5272d71b5cb44c30cd84b09b4dc5735b850de164a192ba191a9b35568305f4"
+    url "https://cpan.metacpan.org/authors/id/J/JC/JCRISTY/Image-Magick-7.0.11-1.tar.gz"
+    sha256 "734bee16656af5bca94000419d912518842ba6460ac2d0ff07e3e5a0103272e2"
   end
 
   resource "libarchive-headers" do
-    url "https://opensource.apple.com/tarballs/libarchive/libarchive-83.40.4.tar.gz"
-    sha256 "20ad61b1301138bc7445e204dd9e9e49145987b6655bbac39f6cad3c75b10369"
+    url "https://opensource.apple.com/tarballs/libarchive/libarchive-83.100.2.tar.gz"
+    sha256 "e54049be1b1d4f674f33488fdbcf5bb9f9390db5cc17a5b34cbeeb5f752b207a"
   end
 
   resource "Archive::Peek::Libarchive" do
