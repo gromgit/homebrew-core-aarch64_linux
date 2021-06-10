647112682d4d5ab43711e43ada826b2ed9bff705
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-10 02:31:47 +0000

    ugrep: update 3.3.2 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -6,10 +6,10 @@ class Ugrep < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_big_sur: "a51cc5afdda6ac8b32fa1bb75002c27241347ad14bade2d0756dc2dae516f6fd"
-    sha256 big_sur:       "3cac2cdbea43d69fab1856fc935b8604b8ba721d4250ded9d8b782d0bdecc7df"
-    sha256 catalina:      "84cdb5c42c1f050d38180e606e476450d340dc4ddce1f455b950fa0f521764cd"
-    sha256 mojave:        "64fbbd4df2ad154b6226117dc3926bc8225f505ddb6b9f38b232090fb3b29132"
+    sha256 arm64_big_sur: "bb9fe087ecb8f5cf5638220cc337904e784c7e0246387b496299381c4dfe89a3"
+    sha256 big_sur:       "3b5bdc4bf53f6a9ab76889216abcfa2f60bf6f538453a43fc3ae923250b908e3"
+    sha256 catalina:      "59d376a23a1afa1e6869add2d5f36eb425d36befaca7ee8586502aae0dbdf93a"
+    sha256 mojave:        "0f588ffaf85e7405cacde36206fefc859836fd67f40e9c1b3190713ad4b1af21"
   end
 
   depends_on "pcre2"
