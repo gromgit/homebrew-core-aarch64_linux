8c6eccda1616af924aea41489440de4d8dadf91f
Rui Chen <rui@chenrui.dev>
2021-06-11 00:48:24 -0400

    re-flex 3.0.7
    Closes #79134.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "Regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v3.0.6.tar.gz"
-  sha256 "23c03a0f1bdf45b4f27edacbef15b2d57586d2928e81de87ea4bb7f13d9851da"
+  url "https://github.com/Genivia/RE-flex/archive/v3.0.7.tar.gz"
+  sha256 "2121a055f294ba81dfee149e1a07d6d00d81b467bc44d3feacc9ea4d7e0362d5"
   license "BSD-3-Clause"
 
   bottle do
