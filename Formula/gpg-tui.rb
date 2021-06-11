456afb876f95c978590f5bf9e276692e7412aae2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-11 06:07:53 +0000

    gpg-tui: update 0.4.1 bottle.

--- Formula/gpg-tui.rb
+++ Formula/gpg-tui.rb
@@ -6,10 +6,10 @@ class GpgTui < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1e149278fb3515d297b3f1a019de3d17cda1fb94b3e0ed75035b001d22f14d1c"
-    sha256 cellar: :any, big_sur:       "eba1aea45ce9d124a5ab412e633cee840848d1767f22764c809e6707e7e777a0"
-    sha256 cellar: :any, catalina:      "b36bd03e3b44a7daad1abb3cc7ca0a78b4e29abcbf676a9791bdd62e58e9e720"
-    sha256 cellar: :any, mojave:        "8046f5196da30d5b297b740b48f214f383ea0d147806818c45b7a5ade97f8910"
+    sha256 cellar: :any, arm64_big_sur: "21f6377efdd94fe8c42ada631a217711c4acbe47d2a1f736f8fe23a08699a893"
+    sha256 cellar: :any, big_sur:       "0ae12b66b1b6f114b65d49111b25c319cf74055178cfae2451ca4bd893ff1d41"
+    sha256 cellar: :any, catalina:      "91bbca2cb93de4c4fe930d563a74f29c7d5a7c7cf0ba2f8769883d5a12cb53ad"
+    sha256 cellar: :any, mojave:        "29b65d84ad6b6bc5c87287e45e7c61010cd396c23d3725b9612d7bd586865d87"
   end
 
   depends_on "rust" => :build
