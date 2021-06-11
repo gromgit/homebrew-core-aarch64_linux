fd409bd2af455d5fe596fbc2ff7c1173d90d7b04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-11 08:12:39 +0000

    re-flex: update 3.0.7 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -6,10 +6,10 @@ class ReFlex < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0320022e47a1cd77e47b97ea57d188db729e96a6475b15a8a4c4ab47b666cdfa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "25db2d95d2e08b8b5f216d0af67e769cd1c0e8dc7530abad14d4303f08f981b6"
-    sha256 cellar: :any_skip_relocation, catalina:      "6577b7d550f87fb18f7379e8b93823b4b5262a9f173062d0852afa18ac7d64e9"
-    sha256 cellar: :any_skip_relocation, mojave:        "b071537d78265ec8de66fb32b7fa4a18dde206fce2da8a4b076c3613dd4cddd8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1041a854a0de92f8ba5230cbcbdd841e3ac76fac93888fba232e9027833478c2"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e6edac151062c1b4079f1d54c2781ca23d18e702ee1d45e3e564d3004838f85c"
+    sha256 cellar: :any_skip_relocation, catalina:      "31cc9b8ffb19cbbc0d1218a3cd04e89666c9bc5d061faa91fe0f2fb0950d0c76"
+    sha256 cellar: :any_skip_relocation, mojave:        "32bb81f4af0fe2c310acc5643288d23bc2b9ee7386e59b9943bdb355abeea99d"
   end
 
   depends_on "pcre2"
