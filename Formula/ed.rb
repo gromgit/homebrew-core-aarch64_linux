840b068111f0c618ac78b816b7d9b2920df50bf5
Michka Popoff <michkapopoff@gmail.com>
2021-06-11 07:28:54 +0200

    ed: do not prefix with g on Linux (#79096)

--- Formula/ed.rb
+++ Formula/ed.rb
@@ -18,35 +18,51 @@ class Ed < Formula
   def install
     ENV.deparallelize
 
-    system "./configure", "--prefix=#{prefix}", "--program-prefix=g"
+    args = ["--prefix=#{prefix}"]
+    on_macos do
+      args << "--program-prefix=g"
+    end
+
+    system "./configure", *args
     system "make"
     system "make", "install"
 
-    %w[ed red].each do |prog|
-      (libexec/"gnubin").install_symlink bin/"g#{prog}" => prog
-      (libexec/"gnuman/man1").install_symlink man1/"g#{prog}.1" => "#{prog}.1"
+    on_macos do
+      %w[ed red].each do |prog|
+        (libexec/"gnubin").install_symlink bin/"g#{prog}" => prog
+        (libexec/"gnuman/man1").install_symlink man1/"g#{prog}.1" => "#{prog}.1"
+      end
     end
 
     libexec.install_symlink "gnuman" => "man"
   end
 
   def caveats
-    <<~EOS
-      All commands have been installed with the prefix "g".
-      If you need to use these commands with their normal names, you
-      can add a "gnubin" directory to your PATH from your bashrc like:
-        PATH="#{opt_libexec}/gnubin:$PATH"
-    EOS
+    on_macos do
+      <<~EOS
+        All commands have been installed with the prefix "g".
+        If you need to use these commands with their normal names, you
+        can add a "gnubin" directory to your PATH from your bashrc like:
+          PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
   end
 
   test do
     testfile = testpath/"test"
     testfile.write "Hello world\n"
 
-    pipe_output("#{bin}/ged -s #{testfile}", ",s/o//\nw\n", 0)
-    assert_equal "Hell world\n", testfile.read
+    on_macos do
+      pipe_output("#{bin}/ged -s #{testfile}", ",s/o//\nw\n", 0)
+      assert_equal "Hell world\n", testfile.read
 
-    pipe_output("#{opt_libexec}/gnubin/ed -s #{testfile}", ",s/l//g\nw\n", 0)
-    assert_equal "He word\n", testfile.read
+      pipe_output("#{opt_libexec}/gnubin/ed -s #{testfile}", ",s/l//g\nw\n", 0)
+      assert_equal "He word\n", testfile.read
+    end
+
+    on_linux do
+      pipe_output("#{bin}/ed -s #{testfile}", ",s/o//\nw\n", 0)
+      assert_equal "Hell world\n", testfile.read
+    end
   end
 end
