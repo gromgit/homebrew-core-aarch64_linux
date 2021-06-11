6d1e0cd4884eaf9fa623438600d3f44e8ff9e667
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-11 08:12:51 +0000

    radare2: update 5.3.1 bottle.

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -7,10 +7,10 @@ class Radare2 < Formula
   head "https://github.com/radareorg/radare2.git"
 
   bottle do
-    sha256 arm64_big_sur: "1f45c3a96040a660ef2de272c9f46378587d20f6067a7f2b9749a307b52079f0"
-    sha256 big_sur:       "3a1ff714c7e9db0488102ad7728ea475a20758b28663c8c01e7ee0e12367edd4"
-    sha256 catalina:      "ddacf5e9ff5600929694b63a85648d27b5752dfdab2e82e39fe3f123f49d9258"
-    sha256 mojave:        "dad8e41a3bccdad7eacec3be1bf1b23c731d3d9d039719c284da25c21a2f3118"
+    sha256 arm64_big_sur: "fb6a2b27a04b3c58eb9cc9f3c11966c6497190f8500a574610d48b1df8884416"
+    sha256 big_sur:       "52563870e763b9012aa676db4751232ecbfeee0c3b3d57c7ac4f968fca7114b4"
+    sha256 catalina:      "8e96a5811ccc5fd5898459442a7c70b9c143246374bb1801d472ecf779a23123"
+    sha256 mojave:        "f7f4295250789184496dde21eda27a2145e3df37629f9a90ffe93d69f901044a"
   end
 
   def install
