7a4e609f4155a14d47ff7ea18e2b527ffcf66098
Nanda H Krishna <me@nandahkrishna.com>
2021-06-12 18:06:41 +0000

    slides: add 0.1.2 bottle.

--- Formula/slides.rb
+++ Formula/slides.rb
@@ -6,6 +6,13 @@ class Slides < Formula
   license "MIT"
   head "https://github.com/maaslalani/slides.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4c20d2700108e6f2980c50d784f13e5dbf074edf69f0604328959ca243bb587b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "98b260f9abf8584d5e9aba3477e77a9c3fc9b81d6b266c83686df02c260d2fd0"
+    sha256 cellar: :any_skip_relocation, catalina:      "93a837175fe373a66eb848aebadd826051b05e1b04c7698b26253dce5f8775ac"
+    sha256 cellar: :any_skip_relocation, mojave:        "9e66e59d0b58600cba3fd649762dd25d864169be1f37caddf947a3d4a2610657"
+  end
+
   depends_on "go" => :build
 
   def install
