12dbc763d08e77ab0a151a719f06d2fbf9522d83
Maas Lalani <maaslalani0@gmail.com>
2021-06-12 10:13:21 -0400

    slides 0.1.2 (new formula)
    Closes #79196.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/slides.rb
@@ -0,0 +1,36 @@
+class Slides < Formula
+  desc "Terminal based presentation tool"
+  homepage "https://github.com/maaslalani/slides"
+  url "https://github.com/maaslalani/slides/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "195f5269ef78733081fa0034add5dca9c5c855ee01bd990795741cb70e385f23"
+  license "MIT"
+  head "https://github.com/maaslalani/slides.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    (testpath/"test.md").write <<-MARKDOWN
+    # Slide 1
+    Content
+
+    ---
+
+    # Slide 2
+    More Content
+    MARKDOWN
+
+    # Bubbletea-based apps are hard to test even under PTY.spawn (or via
+    # expect) because they rely on vt100-like answerback support, such as
+    # "<ESC>[6n" to report the cursor position. For now we just run the command
+    # for a second and see that it tried to send some ANSI out of it.
+    require "pty"
+    r, _, pid = PTY.spawn "#{bin}/slides test.md"
+    sleep 1
+    Process.kill("TERM", pid)
+    assert_match(/\e\[/, r.read)
+  end
+end
