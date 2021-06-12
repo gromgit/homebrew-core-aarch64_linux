391ed9b0d6ff0b3c997da2d16b009926eb5cc216
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-12 14:29:24 +0000

    dhall: update 1.39.0 bottle.

--- Formula/dhall.rb
+++ Formula/dhall.rb
@@ -6,9 +6,10 @@ class Dhall < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "3033cf19e4ae915221dbe28210be943c3f4dc29e0b06d6835ad1385ddb74e0be"
-    sha256 cellar: :any_skip_relocation, catalina: "1d5b510e3ae69e57f7b4579d568a11c5074fc6ff12ebb7f7bd406180a0f13bc6"
-    sha256 cellar: :any_skip_relocation, mojave:   "0b7b23cff6a700ecbf39d33fa2d1f1217f94c05123c18106e6acb22ba4024025"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "aa391ecec1a629c2a1634c8d873bb8fb469708a04f08662a74f7104503837868"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3033cf19e4ae915221dbe28210be943c3f4dc29e0b06d6835ad1385ddb74e0be"
+    sha256 cellar: :any_skip_relocation, catalina:      "1d5b510e3ae69e57f7b4579d568a11c5074fc6ff12ebb7f7bd406180a0f13bc6"
+    sha256 cellar: :any_skip_relocation, mojave:        "0b7b23cff6a700ecbf39d33fa2d1f1217f94c05123c18106e6acb22ba4024025"
   end
 
   depends_on "cabal-install" => :build
