b527f16498e3bba75a5f22c7ce611a131d2e8d63
Thierry Moisan <thierry.moisan@gmail.com>
2021-06-12 15:55:05 -0400

    flyway 7.10.0
    * flyway 7.10.0
    * Update flyway.rb
    
    Closes #79207.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,16 +1,14 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.9.2/flyway-commandline-7.9.2.tar.gz"
-  sha256 "cc5f27e408c96b328de6c07587cfa678e3c22f72cacdc8a34e75380bdb82a31f"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.10.0/flyway-commandline-7.10.0.tar.gz"
+  sha256 "317114f3183f5bbcaeabe64c6c12d5cc777a2e7f6c7cf3c895a3dfa5da8d9333"
   license "Apache-2.0"
 
   livecheck do
     url "https://flywaydb.org/documentation/usage/maven/"
     regex(/&lt;version&gt;.*?v?(\d+(?:\.\d+)+)&lt;/im)
   end
-
-  bottle :unneeded
 
   depends_on "openjdk"
 
