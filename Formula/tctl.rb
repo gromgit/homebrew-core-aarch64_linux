fb6ff5b1eb1b4d0a45a806ed524f50ff9c11a01c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-12 14:07:17 +0000

    tctl: update 1.10.2 bottle.

--- Formula/tctl.rb
+++ Formula/tctl.rb
@@ -6,10 +6,10 @@ class Tctl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ec241cf40b10dc3c679fbbcdfeb9b4a8405e2903be7b0b91f4bc6be746a6dfb4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9b338a81315d89b26627ccc0fd88b28eed697fb131fe8859e1feb8474cc91ce2"
-    sha256 cellar: :any_skip_relocation, catalina:      "c85d85169d68f2eeb0bbf7f9099eae2c01979a30e2f4b00b8b927a77bec7ecb0"
-    sha256 cellar: :any_skip_relocation, mojave:        "2d98be5fcbb70d4b70288f795c7335c803c8353533d26a85d16815bbae54affd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "099fd3f17ed5dc2b212cb056c84abd25410cd44dd965ccc918564d7850fedf3e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "1605ceaaab47a29f6f3d62255a1fe2e71f872fe0a991a766e80bf47e8146af3c"
+    sha256 cellar: :any_skip_relocation, catalina:      "3b1b866985c070952cec1bce7b430f00411e5a51ebde8740ddd71dc9110a7282"
+    sha256 cellar: :any_skip_relocation, mojave:        "da94b6283f3138470c32c70c2eee5cef2e46748069585b9722d2b7d890cd2829"
   end
 
   depends_on "go" => :build
