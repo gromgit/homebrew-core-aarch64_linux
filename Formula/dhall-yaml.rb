f1c116a988b6deaffd8cea9ef472f6e90f7128a3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-12 14:35:51 +0000

    dhall-yaml: update 1.2.7 bottle.

--- Formula/dhall-yaml.rb
+++ Formula/dhall-yaml.rb
@@ -7,9 +7,10 @@ class DhallYaml < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "8345b848010e3920b06d53cbd271b572bf7d1f6d74219b6e81fb4f8a438de578"
-    sha256 cellar: :any_skip_relocation, catalina: "e4022dec09fd40dde84c6699be5b82d058937ec290383e405c2da70f53df208d"
-    sha256 cellar: :any_skip_relocation, mojave:   "8404c619dac4c59dec00546088de2b0e8580368210b8a31fdfd0781ba58d0fd4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6faeee1096a47ee1b85c90c9810c6a202493cbc68a3951c68958c13616344230"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8345b848010e3920b06d53cbd271b572bf7d1f6d74219b6e81fb4f8a438de578"
+    sha256 cellar: :any_skip_relocation, catalina:      "e4022dec09fd40dde84c6699be5b82d058937ec290383e405c2da70f53df208d"
+    sha256 cellar: :any_skip_relocation, mojave:        "8404c619dac4c59dec00546088de2b0e8580368210b8a31fdfd0781ba58d0fd4"
   end
 
   depends_on "cabal-install" => :build
