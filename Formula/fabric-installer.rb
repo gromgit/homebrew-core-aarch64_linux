baf9717612dacb8e97a39b1e0506cbcc1da47c33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-12 23:06:22 +0000

    fabric-installer: add 0.7.4 bottle.

--- Formula/fabric-installer.rb
+++ Formula/fabric-installer.rb
@@ -5,6 +5,10 @@ class FabricInstaller < Formula
   sha256 "192d60fb544a45edca589a4f73d9d3df93a7f7b68a407c0403e9e1802faf7668"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4c309d182810665ba504617c038a5af04bc3c6aba5b04034753227bda46ff78b"
+  end
+
   depends_on "openjdk"
 
   def install
