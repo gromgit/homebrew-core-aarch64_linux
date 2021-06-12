7b834b2312810dafb474b0a7ad7a3bb79c6721ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-12 17:15:01 +0000

    libbinio: update 1.5 bottle.

--- Formula/libbinio.rb
+++ Formula/libbinio.rb
@@ -6,14 +6,10 @@ class Libbinio < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "242943ef59b0240db7e61f8ba587bafcb68f080d66dea8b8bb461d2e292fdaaa"
-    sha256 cellar: :any, big_sur:       "892d24b5664bb470d0b90f714b4d2f9d8bc9bc62f857f003b4736bd5efd9a5f2"
-    sha256 cellar: :any, catalina:      "9557b784c8c13985e1cbdf62fec7b49fd85766c82c2a3b01a4ce4596f36249ef"
-    sha256 cellar: :any, mojave:        "09c61e01936f68d4f64648fa195150c8c7d82e0fa636e9f159687a293d5feab4"
-    sha256 cellar: :any, high_sierra:   "176f0a11a333240770e59ada053e1656081a324debb64b96afa942a86e18f28a"
-    sha256 cellar: :any, sierra:        "beba76f7c3b6c54228c8972cfa01d1cb06d309d870f23ce6aad457c23e11742f"
-    sha256 cellar: :any, el_capitan:    "a0e373df44eee915d0f9259fb8627df92bfe3db8547bf66a9918f5c398342709"
-    sha256 cellar: :any, yosemite:      "99e8bdd47cde67290e0854c8854c0eef32a995ff10cbf1f991ca37834d60e0a4"
+    sha256 cellar: :any, arm64_big_sur: "6490acae30e6e1047e5770b4aab37e4c043488c5c2e8a8919962208c1da2cdd4"
+    sha256 cellar: :any, big_sur:       "5ca7b1faccab23de4efee72cfe82244e419c40c78775f09a01e5669aeed4a8e1"
+    sha256 cellar: :any, catalina:      "157efedae7e8169175623b12f91d8a786fdde524075a2a98a956e910aed5e507"
+    sha256 cellar: :any, mojave:        "4c5b9085fdfe812e664f342e9da9504c24f2f2055c2ef939764dd4bd96025a23"
   end
 
   def install
