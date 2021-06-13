5484df2e3f0d467e5e69e88849ac99644c4e4057
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-13 20:08:16 +0000

    vcsh: add 1.20190621 bottle.

--- Formula/vcsh.rb
+++ Formula/vcsh.rb
@@ -6,6 +6,10 @@ class Vcsh < Formula
   sha256 "178ddf6f7bba15bcc295a08247070665e5b799af64753e21c7fac68f72296ca8"
   license "GPL-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "d6adf58cb690fa63602d8056d347bf55dde6ebd090c6f82d1bbbbc78b137d93b"
+  end
+
   def install
     bin.install "vcsh"
     man1.install "vcsh.1"
