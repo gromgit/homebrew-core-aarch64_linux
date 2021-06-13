0580840c133a9d89800fedd4138ad0daa12d446b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-13 09:06:49 +0000

    calabash: add 1.3.0-100 bottle.

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -5,6 +5,10 @@ class Calabash < Formula
   sha256 "75e4f9e6ed655a778a6cdf92feeb1e3f75a995bd5e623dfff7418e8c20823c45"
   license "GPL-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b35fca9a2745fa7bea9a71a9eec85071eba850af9d7df2c8132b81152f1ca6ce"
+  end
+
   depends_on "saxon"
 
   def install
