a11d7e25d4baa41207345cb294e496ecc020ec1c
Rui Chen <rui@chenrui.dev>
2021-06-12 23:17:43 -0400

    calabash 1.3.0-100
    * calabash 1.3.0-100
    * calabash: remove `bottle :unneeded`
    
    Closes #79223.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -1,11 +1,9 @@
 class Calabash < Formula
   desc "XProc (XML Pipeline Language) implementation"
   homepage "https://xmlcalabash.com/"
-  url "https://github.com/ndw/xmlcalabash1/releases/download/1.2.5-100/xmlcalabash-1.2.5-100.zip"
-  sha256 "1ce954e9563d9acc7a6dbcfaccbc3ec82e1cb938fdf816d5c5e57fa73b566fc4"
+  url "https://github.com/ndw/xmlcalabash1/releases/download/1.3.0-100/xmlcalabash-1.3.0-100.zip"
+  sha256 "75e4f9e6ed655a778a6cdf92feeb1e3f75a995bd5e623dfff7418e8c20823c45"
   license "GPL-2.0"
-
-  bottle :unneeded
 
   depends_on "saxon"
 
