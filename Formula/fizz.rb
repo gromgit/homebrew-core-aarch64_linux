b86524ec96c404d02c721089975a4f701c4ca658
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-14 14:13:11 +0000

    fizz: update 2021.06.14.00 bottle.

--- Formula/fizz.rb
+++ Formula/fizz.rb
@@ -7,10 +7,10 @@ class Fizz < Formula
   head "https://github.com/facebookincubator/fizz.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5b776a7426267ce856d90a6f49664da5f067daf2c676885e6c07c544612e74a7"
-    sha256 cellar: :any, big_sur:       "725e6c7d347b5afa2da607938631302f13f73008485d9ec03ca4dcac1f3a0fc0"
-    sha256 cellar: :any, catalina:      "1214604ea2101a4e6a122b18951a0dc18e52174cba1ce1c5c2421bd9596b3782"
-    sha256 cellar: :any, mojave:        "889f5e96b8cdf5bca53c25c24d5acd85a7690da1444265250e084dcf56eca45b"
+    sha256 cellar: :any, arm64_big_sur: "ef86ea814834d924cb092bbf98be44bfaaa09751ce364d0cd621a1333e55a3b1"
+    sha256 cellar: :any, big_sur:       "56af4adff8f9d350fd414aa6d7f21c9c62f8a8bc22dd2d79abea2fc417c059d8"
+    sha256 cellar: :any, catalina:      "89f7e6313a1b8e90c1aa0278afaa75d793d4fd737752f95ce6ca4c552c3a333e"
+    sha256 cellar: :any, mojave:        "a375a873c339bb30943def2567beb73f2783a6daaa6798515049647cc0b0d25e"
   end
 
   depends_on "cmake" => :build
