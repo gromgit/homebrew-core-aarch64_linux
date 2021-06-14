bdd1f67a4ae145d6f918947a611759599fd7f719
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-14 07:10:07 +0000

    cpm: update 0.997004 bottle.

--- Formula/cpm.rb
+++ Formula/cpm.rb
@@ -7,10 +7,10 @@ class Cpm < Formula
   head "https://github.com/skaji/cpm.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "24e03d34f10f88d7a450d83a5061e15a3c0e2b9148591e55bef939841d509774"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c5e6faef15405105b73e63c6ac49b1b32b45a5d80b55eca399ad3ce37b06ea95"
-    sha256 cellar: :any_skip_relocation, catalina:      "085d3d7363f0e44b19fc5ba3eb5789dc65a928c60716d013520af3d515d03c74"
-    sha256 cellar: :any_skip_relocation, mojave:        "2425e1296cf3bd5e6afde77a2cf63975e048cd6f18f9809dc860a38f94202ba5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c5647bdfac179a43aa9275ea4e98d3518d6b2e8eefd3e34ad45414a9bdbacb0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "87c5db7eb413d24f80b450dc52bba0b8486a3efff4c9cfb03fd41d0a2845973c"
+    sha256 cellar: :any_skip_relocation, catalina:      "87c5db7eb413d24f80b450dc52bba0b8486a3efff4c9cfb03fd41d0a2845973c"
+    sha256 cellar: :any_skip_relocation, mojave:        "87c5db7eb413d24f80b450dc52bba0b8486a3efff4c9cfb03fd41d0a2845973c"
   end
 
   depends_on "perl"
