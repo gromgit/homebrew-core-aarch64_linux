beaafe6bbc00c2cdf8445e635feab515c903b843
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-14 23:15:14 +0000

    source-to-image: update 1.3.1 bottle.

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -8,10 +8,11 @@ class SourceToImage < Formula
   head "https://github.com/openshift/source-to-image.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "885c70eb74a6c0faf5cebdf1468820a26ce8ac08a555821c3d419c7725e09256"
-    sha256 cellar: :any_skip_relocation, catalina:    "3fbf3469cf68fa605bbac9b3cb726ffc5c1f485d27dcacd4b9310e24e8d165e4"
-    sha256 cellar: :any_skip_relocation, mojave:      "c576266fcc9e09cfae7ea91d9bc6f76b4aad025d087cf11acfe94218cdfe1774"
-    sha256 cellar: :any_skip_relocation, high_sierra: "29fb2fc7a031e904e743264878f9a4010e7c5d6aa0a3091ea0ec1038f312a5ca"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b5ac084c3947f1729b436f7928760e3b33e26d1929dde39a6f53baee93103b38"
+    sha256 cellar: :any_skip_relocation, big_sur:       "885c70eb74a6c0faf5cebdf1468820a26ce8ac08a555821c3d419c7725e09256"
+    sha256 cellar: :any_skip_relocation, catalina:      "3fbf3469cf68fa605bbac9b3cb726ffc5c1f485d27dcacd4b9310e24e8d165e4"
+    sha256 cellar: :any_skip_relocation, mojave:        "c576266fcc9e09cfae7ea91d9bc6f76b4aad025d087cf11acfe94218cdfe1774"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "29fb2fc7a031e904e743264878f9a4010e7c5d6aa0a3091ea0ec1038f312a5ca"
   end
 
   depends_on "go" => :build
