14bf5041ee0f70bddaf2113b7cfb071a37ce244a
Michka Popoff <michkapopoff@gmail.com>
2021-06-15 23:55:26 +0200

    go@1.10: workaround permission issues on Linux (#79397)
    This formula is unsupported but we will add the workaround for
    Linux nevertheless, to finalise the core merge

--- Formula/go@1.10.rb
+++ Formula/go@1.10.rb
@@ -47,6 +47,12 @@ class GoAT110 < Formula
   end
 
   def install
+    on_linux do
+      # Fixes: Error: Failure while executing: ../bin/ldd ../line-clang.elf: Permission denied
+      chmod "+x", Dir.glob("src/debug/dwarf/testdata/*.elf")
+      chmod "+x", Dir.glob("src/debug/elf/testdata/*-exec")
+    end
+
     (buildpath/"gobootstrap").install resource("gobootstrap")
     ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
 
