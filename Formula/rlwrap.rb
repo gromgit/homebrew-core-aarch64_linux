e04b286904d300514062a99d9b375994835933c8
MoonFruit <dkmoonfruit@gmail.com>
2021-06-15 20:16:31 +0800

    rlwrap 0.45.2
    Closes #79371.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rlwrap.rb
+++ Formula/rlwrap.rb
@@ -1,8 +1,8 @@
 class Rlwrap < Formula
   desc "Readline wrapper: adds readline support to tools that lack it"
   homepage "https://github.com/hanslub42/rlwrap"
-  url "https://github.com/hanslub42/rlwrap/archive/v0.45.1.tar.gz"
-  sha256 "ed9a198191685efcd2f68a9071b9c65b8705f81f2f6bea935032b3f92860bde7"
+  url "https://github.com/hanslub42/rlwrap/archive/v0.45.2.tar.gz"
+  sha256 "7197559f193918cc8782421b5b1313abbde5e3b965a5f91f9ee25aee9b172ec5"
   license "GPL-2.0-or-later"
   head "https://github.com/hanslub42/rlwrap.git"
 
