a6ebcd3930fbcaf0c476676bbbf5a53990ca3490
Alexander Bayandin <a.bayandin@gmail.com>
2021-06-15 17:40:11 +0100

    libtorch 1.9.0

--- Formula/libtorch.rb
+++ Formula/libtorch.rb
@@ -4,10 +4,9 @@ class Libtorch < Formula
   desc "Tensors and dynamic neural networks"
   homepage "https://pytorch.org/"
   url "https://github.com/pytorch/pytorch.git",
-      tag:      "v1.8.1",
-      revision: "56b43f4fec1f76953f15a627694d4bba34588969"
+      tag:      "v1.9.0",
+      revision: "d69c22dd61a2f006dcfe1e3ea8468a3ecaf931aa"
   license "BSD-3-Clause"
-  revision 1
 
   livecheck do
     url :stable
@@ -33,14 +32,9 @@ class Libtorch < Formula
     sha256 "607774cbba28732bfa802b54baa7484215f530991055bb562efbed5b2f20a45e"
   end
 
-  resource "typing" do
-    url "https://files.pythonhosted.org/packages/05/d9/6eebe19d46bd05360c9a9aae822e67a80f9242aabbfc58b641b957546607/typing-3.7.4.3.tar.gz"
-    sha256 "1187fb9c82fd670d10aa07bbb6cfcfe4bdda42d6fab8d5134f04e8c4d0b71cc9"
-  end
-
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/16/06/0f7367eafb692f73158e5c5cbca1aec798cdf78be5167f6415dd4205fa32/typing_extensions-3.7.4.3.tar.gz"
-    sha256 "99d4073b617d30288f569d3f13d2bd7548c3a7e4c8de87db09a9d29bb3a4a60c"
+    url "https://files.pythonhosted.org/packages/aa/55/62e2d4934c282a60b4243a950c9dbfa01ae7cac0e8d6c0b5315b87432c81/typing_extensions-3.10.0.0.tar.gz"
+    sha256 "50b6f157849174217d0656f99dc82fe932884fb250826c18350e159ec6cdf342"
   end
 
   def install
