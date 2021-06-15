a48394a442f22f1dd15d99d934ea57885b35091d
Fabian Ritzmann <fabian@ritzmann.dev>
2021-06-15 22:43:12 +0300

    px 1.4.0 (new formula)
    Closes #79399.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/px.rb
@@ -0,0 +1,30 @@
+class Px < Formula
+  include Language::Python::Virtualenv
+
+  desc "Ps and top for human beings (px / ptop)"
+  homepage "https://github.com/walles/px"
+  url "https://github.com/walles/px.git",
+      tag:      "1.4.0",
+      revision: "aea41c2a67a8b83e644e11d8ac152e2c123d0ee1"
+  license "MIT"
+
+  depends_on "python@3.9"
+  depends_on "six"
+
+  uses_from_macos "lsof"
+
+  # For updates: https://pypi.org/project/python-dateutil/#files
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/be/ed/5bbc91f03fa4c839c4c7360375da77f9659af5f7086b7a7bdda65771c8e0/python-dateutil-2.8.1.tar.gz"
+    sha256 "73ebfe9dbf22e832286dafa60473e4cd239f8592f699aa5adaf10050e6e1823c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    split_first_line = pipe_output("#{bin}/px --no-pager").lines.first.split
+    assert_equal %w[PID COMMAND USERNAME CPU CPUTIME RAM COMMANDLINE], split_first_line
+  end
+end
