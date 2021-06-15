e9838f316c2c1c2fa17223fbc5025f7b5beb73fc
Sean Molenaar <smillerdev@me.com>
2021-06-15 11:00:21 +0200

    fail2ban: service instead of plist

--- Formula/fail2ban.rb
+++ Formula/fail2ban.rb
@@ -116,25 +116,8 @@ class Fail2ban < Formula
 
   plist_options startup: true
 
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/fail2ban-client</string>
-          <string>-x</string>
-          <string>start</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"fail2ban-client", "-x", "start"]
   end
 
   test do
