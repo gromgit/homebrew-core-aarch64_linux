11cd19e81fbf50ca1aab91c0181196d378783e3e
Sean Molenaar <smillerdev@me.com>
2021-06-15 09:40:22 +0200

    cassandra: service instead of plist

--- Formula/cassandra.rb
+++ Formula/cassandra.rb
@@ -140,30 +140,10 @@ class Cassandra < Formula
     (bin/"cqlsh.py").write_env_script libexec/"bin/cqlsh.py", PYTHONPATH: pypath
   end
 
-  plist_options manual: "cassandra -f"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>KeepAlive</key>
-          <true/>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{opt_bin}/cassandra</string>
-              <string>-f</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>WorkingDirectory</key>
-          <string>#{var}/lib/cassandra</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"cassandra", "-f"]
+    keep_alive true
+    working_dir var/"lib/cassandra"
   end
 
   test do
