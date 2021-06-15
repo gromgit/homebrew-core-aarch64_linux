5a7746ba5b861145ba711dac127e0d59f60f47ce
Alexander Zigelski <crunchtime-ali@users.noreply.github.com>
2021-06-15 00:24:39 +0000

    jetty-runner 9.4.42.v20210604
    * jetty-runner 9.4.42.v20210604
    * jetty-runner: remove `bottle :unneeded`
    
    Closes #79343.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -1,17 +1,15 @@
 class JettyRunner < Formula
   desc "Use Jetty without an installed distribution"
   homepage "https://www.eclipse.org/jetty/"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.41.v20210516/jetty-runner-9.4.41.v20210516.jar"
-  version "9.4.41.v20210516"
-  sha256 "b37ed2e58c85e1e43d5afd81113afef071c07d11159e5183c21ee9709c6d7f9d"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.42.v20210604/jetty-runner-9.4.42.v20210604.jar"
+  version "9.4.42.v20210604"
+  sha256 "2d505df89b15160195398b7e7c9400ef73ad21beefabbf426def9b20c744b110"
   license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
     url "https://www.eclipse.org/jetty/download.php"
     regex(/href=.*?jetty-distribution[._-]v?(\d+(?:\.\d+)+(?:\.v\d+)?)\.t/i)
   end
-
-  bottle :unneeded
 
   depends_on "openjdk"
 
