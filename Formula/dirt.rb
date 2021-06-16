64ec26c08e79239993bdf5454c810a431c948f80
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 21:13:47 +0000

    dirt: update 1.1_1 bottle.

--- Formula/dirt.rb
+++ Formula/dirt.rb
@@ -8,13 +8,14 @@ class Dirt < Formula
   head "https://github.com/tidalcycles/Dirt.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "ac69059e87e5682411a7442619985d95d79ee67af88366bcea28cc89e35ef46a"
-    sha256 cellar: :any, catalina:    "2e9cf5a28852453f9ec5394bb2218fe3366e2cc6ef133e2dda847fbfa71ee968"
-    sha256 cellar: :any, mojave:      "f90972cf61d77071fec9ab429f8a88a03738699b7e223b30c8655d5c64fede74"
-    sha256 cellar: :any, high_sierra: "b889891f8186b244161241e9c81d20afad20c31bd592fbf6860658334f314d39"
-    sha256 cellar: :any, sierra:      "63847bffb4de9fa0cf57a1aea8a6bc1d713b8b0a1243ada27e6dd9d4aa21ccc1"
-    sha256 cellar: :any, el_capitan:  "96b6e1e120bb8be5a051cdca4534d569afe5cae61abdcaf808cdef7af94042af"
-    sha256 cellar: :any, yosemite:    "ae94ee15ddb686a63120bea12e2991a5357711fcfcf0ed5c09f7aa6e2d6c3a4f"
+    sha256 cellar: :any, arm64_big_sur: "c011771c2775cdbf313084355baf6734da930ad9fae75326775cfb27481482a4"
+    sha256 cellar: :any, big_sur:       "ac69059e87e5682411a7442619985d95d79ee67af88366bcea28cc89e35ef46a"
+    sha256 cellar: :any, catalina:      "2e9cf5a28852453f9ec5394bb2218fe3366e2cc6ef133e2dda847fbfa71ee968"
+    sha256 cellar: :any, mojave:        "f90972cf61d77071fec9ab429f8a88a03738699b7e223b30c8655d5c64fede74"
+    sha256 cellar: :any, high_sierra:   "b889891f8186b244161241e9c81d20afad20c31bd592fbf6860658334f314d39"
+    sha256 cellar: :any, sierra:        "63847bffb4de9fa0cf57a1aea8a6bc1d713b8b0a1243ada27e6dd9d4aa21ccc1"
+    sha256 cellar: :any, el_capitan:    "96b6e1e120bb8be5a051cdca4534d569afe5cae61abdcaf808cdef7af94042af"
+    sha256 cellar: :any, yosemite:      "ae94ee15ddb686a63120bea12e2991a5357711fcfcf0ed5c09f7aa6e2d6c3a4f"
   end
 
   depends_on "jack"
