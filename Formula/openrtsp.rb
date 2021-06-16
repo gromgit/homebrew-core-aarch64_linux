01cdca1c29223fff71663a5e6b0d8540618f00ad
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 16:10:03 +0000

    openrtsp: update 2020.11.05 bottle.

--- Formula/openrtsp.rb
+++ Formula/openrtsp.rb
@@ -8,10 +8,11 @@ class Openrtsp < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "fce2e67f55b717cd6889b5f2bc4e21bcde69acc87ed561f5a5bab17dc1aafe8a"
-    sha256 cellar: :any, catalina:    "4dfd0982dd0e9480a654f8b3d85ac4e66b60ab582306a8aafa9ad060eb86051d"
-    sha256 cellar: :any, mojave:      "c7bc407cea25d6f3a7e89237f8241067622a630903c4649091fcf3843c9820c3"
-    sha256 cellar: :any, high_sierra: "c99d793ff2f28434edbadc70d466a7316ef7d7b8095002d78090218a9b4abe76"
+    sha256 cellar: :any, arm64_big_sur: "802f88bf6d8c831a729fdcedcc949d121f9969762f985fd532cd14f2a31c97da"
+    sha256 cellar: :any, big_sur:       "fce2e67f55b717cd6889b5f2bc4e21bcde69acc87ed561f5a5bab17dc1aafe8a"
+    sha256 cellar: :any, catalina:      "4dfd0982dd0e9480a654f8b3d85ac4e66b60ab582306a8aafa9ad060eb86051d"
+    sha256 cellar: :any, mojave:        "c7bc407cea25d6f3a7e89237f8241067622a630903c4649091fcf3843c9820c3"
+    sha256 cellar: :any, high_sierra:   "c99d793ff2f28434edbadc70d466a7316ef7d7b8095002d78090218a9b4abe76"
   end
 
   # could not get it build since 2020.11.22
