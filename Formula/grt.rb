b1e6a307d376e331ccd6968f1c2a4d196a07728d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 21:51:18 +0000

    grt: update 0.2.4 bottle.

--- Formula/grt.rb
+++ Formula/grt.rb
@@ -7,6 +7,7 @@ class Grt < Formula
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "e7fb295223786373ecfb9a7bfd323b234a2c9034f9fc2f4705ff1209be93dcfb"
+    sha256 cellar: :any, big_sur:       "1d4777cee44f7b99247cc34cda090ca59e720a14f20b9d2336d21e06bc309d4a"
     sha256 cellar: :any, catalina:      "35705ad1d151228e4e8c647cf16fc5b7578b4b777c54e94ebbcc8e19d26bd1f5"
     sha256 cellar: :any, mojave:        "830332fffbe52f7eb282eca343977fa27e4ae4978bc9f409df264cba5f37e38c"
     sha256 cellar: :any, high_sierra:   "d5a55b3a6b4ca75d3ef37ba47f43ed033215e0d9b5f5a26f738e484f3e428667"
