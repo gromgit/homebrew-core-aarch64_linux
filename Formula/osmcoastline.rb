6cc71d75ef8281c4ca24ca5ffd2078b5ede89c85
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 21:18:28 +0000

    osmcoastline: update 2.3.0_1 bottle.

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -7,9 +7,10 @@ class Osmcoastline < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, big_sur:  "50cfe5960393f3787b938697d26f620ed4218e9a65a16d24e9b4b9ac82bfafa5"
-    sha256 cellar: :any, catalina: "cf181ab2a5da968d965d6712e5756de4d30b950bbfe8eef8f54907be8aeffef6"
-    sha256 cellar: :any, mojave:   "b5329fbfabf3ed6f2754c4fafb24ef9f35680e9391c911887c4c0037c33fa430"
+    sha256 cellar: :any, arm64_big_sur: "0306a9c8556638a4d128bc15d9adc68736a3022333a55beeb227af97eb30a9aa"
+    sha256 cellar: :any, big_sur:       "50cfe5960393f3787b938697d26f620ed4218e9a65a16d24e9b4b9ac82bfafa5"
+    sha256 cellar: :any, catalina:      "cf181ab2a5da968d965d6712e5756de4d30b950bbfe8eef8f54907be8aeffef6"
+    sha256 cellar: :any, mojave:        "b5329fbfabf3ed6f2754c4fafb24ef9f35680e9391c911887c4c0037c33fa430"
   end
 
   depends_on "cmake" => :build
