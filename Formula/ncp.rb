044fc94888c595b3a2896020da769501da0504d6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 21:13:23 +0000

    ncp: update 1.2.4 bottle.

--- Formula/ncp.rb
+++ Formula/ncp.rb
@@ -7,11 +7,12 @@ class Ncp < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, catalina:    "26d9db0b0da97a1556e5b20afd08c82be35fd7f8530ab63110d83fb1ba542ac2"
-    sha256 cellar: :any_skip_relocation, mojave:      "345fcd7aaa3fbe0352a0109c986389db0cec91b4603829b4f36998ad866e5aca"
-    sha256 cellar: :any_skip_relocation, high_sierra: "2c56e482f1608eeb3925f4db0b1ef782fb1644d2a7318e522a4a3c7ba7efcdd4"
-    sha256 cellar: :any_skip_relocation, sierra:      "d261de84549f890b4a3ca4c9f9d72ec25dd5a66c77fbccf6c29577c49a3c0866"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "08f17ef57ee62eb3ca90c7c033fba68edc2822d93689ddabdcaa49913c98f369"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "73488953306c5c0079e2a01b0d6e89dc2d7b91a2cd410230b95ce81b2aac4430"
+    sha256 cellar: :any_skip_relocation, catalina:      "26d9db0b0da97a1556e5b20afd08c82be35fd7f8530ab63110d83fb1ba542ac2"
+    sha256 cellar: :any_skip_relocation, mojave:        "345fcd7aaa3fbe0352a0109c986389db0cec91b4603829b4f36998ad866e5aca"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "2c56e482f1608eeb3925f4db0b1ef782fb1644d2a7318e522a4a3c7ba7efcdd4"
+    sha256 cellar: :any_skip_relocation, sierra:        "d261de84549f890b4a3ca4c9f9d72ec25dd5a66c77fbccf6c29577c49a3c0866"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "08f17ef57ee62eb3ca90c7c033fba68edc2822d93689ddabdcaa49913c98f369"
   end
 
   depends_on "libowfat"
