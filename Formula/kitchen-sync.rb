01512440f2f7eb82f9497b7d7a2bcadc1876c238
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 16:09:51 +0000

    kitchen-sync: update 2.11 bottle.

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -12,9 +12,10 @@ class KitchenSync < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:  "2b6b8db77a59ac10cb719ddabc427941c78d43612fd945145c96f054bb5654a7"
-    sha256 cellar: :any, catalina: "03ebfd56228b524fad48bad6017146a213fb9f2bff088ad1b78a50d8a6d40855"
-    sha256 cellar: :any, mojave:   "8dadd7bb9c24df3191f3c8e512be6e34ededaf7d062a57662b45ae9a1934c394"
+    sha256 cellar: :any, arm64_big_sur: "df684d4f93303e9e558a0e76e183cf3d2f023dcc72ea89636187da767043f167"
+    sha256 cellar: :any, big_sur:       "2b6b8db77a59ac10cb719ddabc427941c78d43612fd945145c96f054bb5654a7"
+    sha256 cellar: :any, catalina:      "03ebfd56228b524fad48bad6017146a213fb9f2bff088ad1b78a50d8a6d40855"
+    sha256 cellar: :any, mojave:        "8dadd7bb9c24df3191f3c8e512be6e34ededaf7d062a57662b45ae9a1934c394"
   end
 
   depends_on "cmake" => :build
