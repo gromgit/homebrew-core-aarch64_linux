9611f84b259265c8f3b7406d411eb34d58a95830
Dawid Dziurla <dawidd0811@gmail.com>
2021-06-16 17:10:55 +0000

    bgpq4: add 0.0.7 bottle.

--- Formula/bgpq4.rb
+++ Formula/bgpq4.rb
@@ -11,6 +11,13 @@ class Bgpq4 < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3a5b6d4d65725eea1d40ed0192633d2265eb9011c5e7df3f3d2cfc33b2cb673c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "80d33aa9cb61cd994b2322d2709e7cdaa56887f0c92c8342f6eed501e78edd66"
+    sha256 cellar: :any_skip_relocation, catalina:      "0e49b63edcd89b6eab9c979ae6e8da2f2ac30d0bc26242a1db2cf2d0233441ca"
+    sha256 cellar: :any_skip_relocation, mojave:        "8644ec33178ee7e9b4b1293d77273c0ddcb11d6e4fa3aafb4e051af2dc4a5e07"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
