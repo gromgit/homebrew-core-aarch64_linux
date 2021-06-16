5c54358e7313ea2af4bce4543360799d7c3904d7
Alexander Bayandin <a.bayandin@gmail.com>
2021-06-16 11:46:37 +0100

    fb-client: rename conflicting formula

--- Formula/fb-client.rb
+++ Formula/fb-client.rb
@@ -22,7 +22,7 @@ class FbClient < Formula
   depends_on "curl"
   depends_on "python@3.9"
 
-  conflicts_with "findbugs", because: "findbugs and fb-client both install a `fb` binary"
+  conflicts_with "spotbugs", because: "both install a `fb` binary"
 
   resource "pycurl" do
     url "https://files.pythonhosted.org/packages/50/1a/35b1d8b8e4e23a234f1b17a8a40299fd550940b16866c9a1f2d47a04b969/pycurl-7.43.0.6.tar.gz"
