0a9df0fcedb7fd4a9f560b3bd5065d034119a99a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-16 16:08:25 +0000

    inform6: update 6.34-6.12.4-1 bottle.

--- Formula/inform6.rb
+++ Formula/inform6.rb
@@ -8,9 +8,10 @@ class Inform6 < Formula
   head "https://gitlab.com/DavidGriffith/inform6unix.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "a4b3d047e0686c3104ce51b8780b12202c6c3b9486ce3e386e1ac5e7acbe8944"
-    sha256 cellar: :any_skip_relocation, catalina: "4fa26f001f5e273bca4b561f2e7c3783d55fb7ab69f3bb098f109f72a789cc95"
-    sha256 cellar: :any_skip_relocation, mojave:   "0c0de1e012507b4450e610d93e25497f13fef4c11f5d47dfdcd76cf7755f2c46"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c596c279a29befbca82053e12594b6ab3524c205f969b4747bc9e8ae6a8f1cf4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a4b3d047e0686c3104ce51b8780b12202c6c3b9486ce3e386e1ac5e7acbe8944"
+    sha256 cellar: :any_skip_relocation, catalina:      "4fa26f001f5e273bca4b561f2e7c3783d55fb7ab69f3bb098f109f72a789cc95"
+    sha256 cellar: :any_skip_relocation, mojave:        "0c0de1e012507b4450e610d93e25497f13fef4c11f5d47dfdcd76cf7755f2c46"
   end
 
   resource "Adventureland.inf" do
