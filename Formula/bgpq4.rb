0b829b0253d6270eaea5bf3f1b5fd19a4017424d
Arnoud Vermeer <avermeer@tucows.com>
2021-06-16 10:00:07 +0200

    bgpq4 0.0.7 (new formula)
    Closes #79296.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/bgpq4.rb
@@ -0,0 +1,32 @@
+class Bgpq4 < Formula
+  desc "BGP filtering automation for Cisco, Juniper, BIRD and OpenBGPD routers"
+  homepage "https://github.com/bgp/bgpq4"
+  url "https://github.com/bgp/bgpq4/archive/refs/tags/0.0.7.tar.gz"
+  sha256 "c39af7d2a8b0f4cf61543e776677baaf067aa087181851572211d168258f7f9f"
+  license "BSD-2-Clause"
+  head "https://github.com/bgp/bgpq4.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    system "./bootstrap"
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    output = <<~EOS
+      no ip prefix-list NN
+      ! generated prefix-list NN is empty
+      ip prefix-list NN deny 0.0.0.0/0
+    EOS
+
+    assert_match output, shell_output("#{bin}/bgpq4 AS-ANY")
+  end
+end
