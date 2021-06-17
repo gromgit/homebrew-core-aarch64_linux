d7cf1ec4932327c57202af95761f7235b1a1b220
JCount <JCount42@gmail.com>
2021-06-17 16:42:52 -0400

    launchctl-completion: add license (#79527)
    Add MIT license.

--- Formula/launchctl-completion.rb
+++ Formula/launchctl-completion.rb
@@ -3,6 +3,7 @@ class LaunchctlCompletion < Formula
   homepage "https://github.com/CamJN/launchctl-completion"
   url "https://github.com/CamJN/launchctl-completion/archive/v1.0.tar.gz"
   sha256 "b21c39031fa41576d695720b295dce57358c320964f25d19a427798d0f0df7a0"
+  license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "14ff31532b6b8df8c32ffaad844a0a7fa5d1d12b0fc9df3f3497bb4a38b67945"
