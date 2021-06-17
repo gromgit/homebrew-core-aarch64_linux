beb452495331862ce99ba0903fc91bb7ee3b44ef
JCount <JCount42@gmail.com>
2021-06-17 18:22:29 -0400

    apm-bash-completion: add license (#79529)
    Add MIT license.

--- Formula/apm-bash-completion.rb
+++ Formula/apm-bash-completion.rb
@@ -3,6 +3,7 @@ class ApmBashCompletion < Formula
   homepage "https://github.com/vigo/apm-bash-completion"
   url "https://github.com/vigo/apm-bash-completion/archive/1.0.0.tar.gz"
   sha256 "1043a7f19eabe69316ea483830fb9f78d6c90853aaf4dd7ed60007af7f0d6e9d"
+  license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "2801988299d591aeafbce230b0425f6f0b5737d0a37e702c47a19c4729cc492f"
