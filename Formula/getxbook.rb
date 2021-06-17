1b46c04d31037d35942d487afa228ec18208d34f
Sam Ford <1584702+samford@users.noreply.github.com>
2021-06-17 08:17:06 -0400

    getxbook: add license, livecheck
    Co-authored-by: JCount <JCount42@gmail.com>

--- Formula/getxbook.rb
+++ Formula/getxbook.rb
@@ -3,7 +3,13 @@ class Getxbook < Formula
   homepage "https://njw.name/getxbook"
   url "https://njw.name/getxbook/getxbook-1.2.tar.xz"
   sha256 "7a4b1636ecb6dace814b818d9ff6a68167799b81ac6fc4dca1485efd48cf1c46"
+  license "ISC"
   revision 1
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?getxbook[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "dc9c26a35297ffa8f3b40051b6293104dfb3bc03cb2ceccf56d77e0b8e5d7b6f"
