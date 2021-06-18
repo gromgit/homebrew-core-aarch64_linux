d7ae6f83336495160300edd158e28184e83642dc
Rui Chen <rui@chenrui.dev>
2021-06-18 13:30:01 -0400

    deja-gnu 1.6.3
    Closes #79578.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/deja-gnu.rb
+++ Formula/deja-gnu.rb
@@ -1,9 +1,9 @@
 class DejaGnu < Formula
   desc "Framework for testing other programs"
   homepage "https://www.gnu.org/software/dejagnu/"
-  url "https://ftp.gnu.org/gnu/dejagnu/dejagnu-1.6.2.tar.gz"
-  mirror "https://ftpmirror.gnu.org/dejagnu/dejagnu-1.6.2.tar.gz"
-  sha256 "0d0671e1b45189c5fc8ade4b3b01635fb9eeab45cf54f57db23e4c4c1a17d261"
+  url "https://ftp.gnu.org/gnu/dejagnu/dejagnu-1.6.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/dejagnu/dejagnu-1.6.3.tar.gz"
+  sha256 "87daefacd7958b4a69f88c6856dbd1634261963c414079d0c371f589cd66a2e3"
   license "GPL-3.0"
 
   bottle do
