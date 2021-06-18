83758f6ca4de78ad22a79e9313103a085e5f8e54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-18 21:07:05 +0000

    editorconfig: update 0.12.5 bottle.

--- Formula/editorconfig.rb
+++ Formula/editorconfig.rb
@@ -7,11 +7,10 @@ class Editorconfig < Formula
   head "https://github.com/editorconfig/editorconfig-core-c.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c6d2a8fe0234a4cc7919238c801c2a57c729f50e8d70ee6078d412cff6ef5d08"
-    sha256 cellar: :any, big_sur:       "78142363e9004adc7286f2393e1bfd663dc55d85225d84da75c285d3dfa021e4"
-    sha256 cellar: :any, catalina:      "efae02b7bab638b75b39abf29163349119b993697210e3dfeca5456f610241ec"
-    sha256 cellar: :any, mojave:        "523459616f8fdf7507c66c4c531e329e8bf37c08633e72401de47fdd010990a6"
-    sha256 cellar: :any, high_sierra:   "0f41e7e368a435f1680195d86b7eabbfd46f40a0905bd8dd8b52e199d92fc3f3"
+    sha256 cellar: :any, arm64_big_sur: "8de5a49262229377ff5f97f670c729626c7c2a688c14cb901e1d6342eee99f92"
+    sha256 cellar: :any, big_sur:       "35b906670a62d96a889967f904b9b157ca860f72993be36f4085081df24d23d1"
+    sha256 cellar: :any, catalina:      "b6cac809fe16ed3e788d3bab017f128a6c960652104ed541791cca02b6e4bc16"
+    sha256 cellar: :any, mojave:        "553e730194b67c667c214f992fd25bd44974d7c9f6812641be65c4347370c124"
   end
 
   depends_on "cmake" => :build
