c98e30e7420acdcb563f29bb712f26283769f636
Rui Chen <rui@chenrui.dev>
2021-06-18 12:32:07 -0400

    terragrunt 0.30.6
    Closes #79569.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.30.4.tar.gz"
-  sha256 "0c20f0251f012d018c684e0e7c566f1c6facd3c4f662a4391cdb066673cc0091"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.30.6.tar.gz"
+  sha256 "927eb56c6b4ceca4a6cac145c559505df7a3e3ff45655fe89de8a0fafe0c60dd"
   license "MIT"
 
   bottle do
