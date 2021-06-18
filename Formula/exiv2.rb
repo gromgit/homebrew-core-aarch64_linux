2d82287de38e3e7410ba6a7a3b861e0d2d7a8e62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-18 23:07:32 +0000

    exiv2: update 0.27.4 bottle.

--- Formula/exiv2.rb
+++ Formula/exiv2.rb
@@ -12,11 +12,10 @@ class Exiv2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f3b4f6da40ef2fbb4da84cc592a1fd2618f1007b4104451b0afa214f14e53125"
-    sha256 cellar: :any, big_sur:       "1d3b44a02c0ebe2ee46ced38a59cf81c60f12a0990debb8b14479431195a572e"
-    sha256 cellar: :any, catalina:      "607f8322cba23a92185541c3b8ee245e7ff339becda5364e1ea6c2168015375c"
-    sha256 cellar: :any, mojave:        "f4ed492ccb45b869000b2cc514ae507422624f6413057ee158ea80b772e182fb"
-    sha256 cellar: :any, high_sierra:   "cd1d11df6b535b1ccfb3458cef28a7662c1e2b7213382e8292abbe00526c7b52"
+    sha256 cellar: :any, arm64_big_sur: "2bde081b463a93a9672b7a842c9e37c55795643077b8b71b06b77381579cb8e4"
+    sha256 cellar: :any, big_sur:       "2142d3d9ac41a438aec051a364d76458a7e3be1e2b5b9aa5568f56094ed8b928"
+    sha256 cellar: :any, catalina:      "b07c163f12af0b32df8f5cf728bf8751312c9f04962df2c2cb00b4b13f9ef8da"
+    sha256 cellar: :any, mojave:        "97f623c1b9562b8e39354829c59b766c2c964234899c60e8a982c90c915d59dc"
   end
 
   depends_on "cmake" => :build
