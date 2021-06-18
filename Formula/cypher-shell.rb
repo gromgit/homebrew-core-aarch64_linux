dbd66ccf6573fd598fbe6836402e4a59659066d6
Nikhil Jha <hi@nikhiljha.com>
2021-06-17 20:28:33 -0700

    cypher-shell 4.2.2
    Closes #79549.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cypher-shell.rb
+++ Formula/cypher-shell.rb
@@ -1,25 +1,19 @@
 class CypherShell < Formula
   desc "Command-line shell where you can execute Cypher against Neo4j"
   homepage "https://github.com/neo4j/cypher-shell"
-  url "https://github.com/neo4j/cypher-shell/releases/download/4.1.3/cypher-shell.zip"
-  sha256 "9e61b40e33e23847ac3493c1f381a34c92ab67c14b849fc472cc703e16c921e8"
+  url "https://github.com/neo4j/cypher-shell/releases/download/4.2.2/cypher-shell.zip"
+  sha256 "b57623b045a252e3b46f4bb0c7db4bc5ddb080f248bb866fc27023a7c9118b91"
   license "GPL-3.0"
-  revision 1
   version_scheme 1
 
-  bottle :unneeded
-
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
     rm_f Dir["bin/*.bat"]
 
     # Needs the jar, but cannot go in bin
-    share.install ["cypher-shell.jar"]
-
-    # Copy the bin
-    bin.install ["cypher-shell"]
-    bin.env_script_all_files(share, NEO4J_HOME: ENV["NEO4J_HOME"])
+    libexec.install Dir["cypher-shell{,.jar}"]
+    (bin/"cypher-shell").write_env_script libexec/"cypher-shell", Language::Java.overridable_java_home_env("11")
   end
 
   test do
