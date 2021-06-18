c8deca215369b3b1ddd63dece848c8fc86baa63b
Rui Chen <rui@chenrui.dev>
2021-06-18 13:48:54 -0400

    knot 3.0.7
    Closes #79594.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.0.6.tar.xz"
-  sha256 "63756ac5a00c3e4a066ed231a287faef5963a9183d77326e30bf0644cdf74f86"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.0.7.tar.xz"
+  sha256 "2bad8be0be95c8f54a26d1e16299e65f31ae1b34bd6ad3819aa50e7b40521484"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
