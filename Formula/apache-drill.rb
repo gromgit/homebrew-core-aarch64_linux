bcd6edc81e82c07b1988ca8467d9eb564d091e4b
Rui Chen <rui@chenrui.dev>
2021-06-18 13:27:38 -0400

    apache-drill 1.19.0
    * apache-drill 1.19.0
    * apache-drill: remove `bottle :unneeded`
    * apache-drill: try with `openjdk@11`
    * apache-drill: remove unnecessary files
      `.bat` files are Windows-only, and we don't need `.txt` files in `bin`.
    
    Closes #79575.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apache-drill.rb
+++ Formula/apache-drill.rb
@@ -1,9 +1,9 @@
 class ApacheDrill < Formula
   desc "Schema-free SQL Query Engine for Hadoop, NoSQL and Cloud Storage"
   homepage "https://drill.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=drill/drill-1.18.0/apache-drill-1.18.0.tar.gz"
-  mirror "https://archive.apache.org/dist/drill/drill-1.18.0/apache-drill-1.18.0.tar.gz"
-  sha256 "e58fc1bed0fe3a0ef101a0705404e887af3f79beaccde8b225e884c06d336597"
+  url "https://www.apache.org/dyn/closer.lua?path=drill/drill-1.19.0/apache-drill-1.19.0.tar.gz"
+  mirror "https://archive.apache.org/dist/drill/drill-1.19.0/apache-drill-1.19.0.tar.gz"
+  sha256 "9374711a08252ff2ac7116c33b6feb627448f109c6e0834cb66d5b8587e6276b"
   license "Apache-2.0"
 
   livecheck do
@@ -11,14 +11,14 @@ class ApacheDrill < Formula
     regex(/href=.*?apache-drill[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  bottle :unneeded
-
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
+    rm_f Dir["bin/*.bat"]
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("11"))
+    rm_f Dir["#{bin}/*.txt"]
   end
 
   test do
