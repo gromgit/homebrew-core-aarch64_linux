0ea72e51ab3b00ced38e6e3fd9c5b5acf4922324
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-18 13:15:36 +0000

    grafana: update 8.0.3 bottle.

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -7,10 +7,10 @@ class Grafana < Formula
   head "https://github.com/grafana/grafana.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "34646d03c1a24067962a07d12d86b9ed58da38b06fc2281b344ca7af5440e7cd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4d0414ea29d73e1f4430d72c9897b35731f2043e4dee81e394e1a10cc0a8e808"
-    sha256 cellar: :any_skip_relocation, catalina:      "5b85d1954014757ec61df7e90124bb8c946d8459bcd01853c0037adcad122697"
-    sha256 cellar: :any_skip_relocation, mojave:        "38033d1ffff98e828f086287ef800dae2497bc0aa6b624a3be7df7d5a5112069"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "feb8ca6c54a08d3aec98c8e528659fd2972c115ae4f7a7a06c994c272d86d548"
+    sha256 cellar: :any_skip_relocation, big_sur:       "eb4a1e345784b4d8cef6783f54a97b31931a2db0bdaa11e0d680adb0cc502abd"
+    sha256 cellar: :any_skip_relocation, catalina:      "1a9a19c6867393e2b69baf63242ed02a973bab9e5300d330ef159308c75b51d0"
+    sha256 cellar: :any_skip_relocation, mojave:        "43c8c61cf9ff5ee5a7ca6f558bb6770629fe2015a202bd89e6cb5cf4bf13c67f"
   end
 
   depends_on "go" => :build
