d3df5015f5ec3cd7ef802d9fa71f816f6bf69122
Michka Popoff <michkapopoff@gmail.com>
2021-06-18 09:54:40 +0200

    nspr: disable workaround on Linux (#79473)

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -30,8 +30,11 @@ class Nspr < Formula
         --enable-64bit
       ]
       system "./configure", *args
-      # Remove the broken (for anyone but Firefox) install_name
-      inreplace "config/autoconf.mk", "-install_name @executable_path/$@ ", "-install_name #{lib}/$@ "
+
+      on_macos do
+        # Remove the broken (for anyone but Firefox) install_name
+        inreplace "config/autoconf.mk", "-install_name @executable_path/$@ ", "-install_name #{lib}/$@ "
+      end
 
       system "make"
       system "make", "install"
