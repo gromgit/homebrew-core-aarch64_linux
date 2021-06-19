582b68f38e94ce712e0d3f5521f2834fb1a6661d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-19 17:14:16 +0000

    stern: update 1.19.0 bottle.

--- Formula/stern.rb
+++ Formula/stern.rb
@@ -7,10 +7,10 @@ class Stern < Formula
   head "https://github.com/stern/stern.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2d4eb663c230a5531461272845ebe49b682126aeb271d048a39e336c2a7ecd4f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1cdc09acfdb8ddb9dc2f3865bd330e2dce44743ee9979e8cacc7a89e804e34d3"
-    sha256 cellar: :any_skip_relocation, catalina:      "d693aab66a01eef79db5962b1dad628ce53d3061732cd86c06faa76b3000144d"
-    sha256 cellar: :any_skip_relocation, mojave:        "e0d9f83aa8ed11f4e1136f37ce1b6671d4ef29b98dee49634a1d50f22a962069"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b274de5c5f3c9cf97aa64a6375d05103ec241becee3e6cbd02fe9a9407bed551"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9815b17dd2796aa4040207fe40731d4a9e1fe19cd581175f18a160f6162bfff2"
+    sha256 cellar: :any_skip_relocation, catalina:      "4474cdd9d0ba47b09a4fd4a837d0c5c00d451b1f96290ebefedcb1837f0dc947"
+    sha256 cellar: :any_skip_relocation, mojave:        "96b38e3a0d220d9278aef074196f576964a9f62173f00cf5a6c9cbb138129dff"
   end
 
   depends_on "go" => :build
