ef0600a5b1c4dffbdd751b70e261f7884ae92488
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-19 21:06:02 +0000

    corsixth: update 0.65 bottle.

--- Formula/corsixth.rb
+++ Formula/corsixth.rb
@@ -7,10 +7,10 @@ class Corsixth < Formula
   head "https://github.com/CorsixTH/CorsixTH.git"
 
   bottle do
-    sha256 arm64_big_sur: "fb20eddb21a89396791d6ab3dfdf13c0bde91c44ba0a6f068b59194b361c4690"
-    sha256 big_sur:       "9852913d485e6fce557001d16f78dac562b205c44810d93b133b539c02ed0436"
-    sha256 catalina:      "bad3d139e3cac3c277a9bea632819fe27b90abfd7d5305813f839d78f5854ca6"
-    sha256 mojave:        "a68aaf41d6feda1bbed25fa4fbb7ff73dc4b5049e23c13fb9377b22cb23c17a4"
+    sha256 arm64_big_sur: "9cb15180b7b5b1ce5a5cc8db33bc51e86d99d4f31f85cafc798732e936a8c43b"
+    sha256 big_sur:       "4383665253bff0c2b2bd48ad55c9834f49e4718656ddadfdb70a8cb440d47196"
+    sha256 catalina:      "611611a2db4ba0265ca31748dd5ba61b3b43f3bd996e42bef75bbb8ad99c5e85"
+    sha256 mojave:        "c6b63dfbf4056ddf0fd48ad3538906ec0a2bfbc31ef8fe8301e04e12cf6792f3"
   end
 
   depends_on "cmake" => :build
