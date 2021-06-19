7e5303de7142687df92768cf9d2e970069b2053b
Thierry Moisan <thierry.moisan@gmail.com>
2021-06-18 21:28:59 -0400

    inxi: fix audit

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -2,7 +2,6 @@ class Inxi < Formula
   desc "Full featured CLI system information tool"
   homepage "https://smxi.org/docs/inxi.htm"
   url "https://github.com/smxi/inxi/archive/3.3.04-1.tar.gz"
-  version "3.3.04-1"
   sha256 "1f029f5f667e0396ab9f6ba2c51cf7937ea38bf504d1621a8c50e19205a7a671"
   license "GPL-3.0-or-later"
   head "https://github.com/smxi/inxi.git"
