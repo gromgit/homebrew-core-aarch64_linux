eed9800a707b38c753e04262351b5a198f9c1450
Nanda H Krishna <me@nandahkrishna.com>
2021-06-20 17:43:47 +0000

    baidupcs-go: add 3.8.1 bottle.

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -6,6 +6,13 @@ class BaidupcsGo < Formula
   license "Apache-2.0"
   head "https://github.com/qjfoidnh/BaiduPCS-Go.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "44c0d9f421ac131574538bc0c2d5f7828a0df65a351bcb6c35adfa65f15d821c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "57ade07be37404c68a016fed277677e09259f75efcc88b90c26ca2027977a8c0"
+    sha256 cellar: :any_skip_relocation, catalina:      "80e4dfe983b5d5afeaa6c3706813965b56633d957efc050f9f6c6559bf82c232"
+    sha256 cellar: :any_skip_relocation, mojave:        "3cfda0f5181e860ac54a09ca3401e19f51b6bc57ea725bd96c73442f640bb594"
+  end
+
   depends_on "go" => :build
 
   def install
