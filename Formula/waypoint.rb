8387063b5ce510c40153b8fe4209ed129aee1795
Rui Chen <rui@chenrui.dev>
2021-06-20 01:23:19 -0400

    waypoint 0.4.0 (new formula)
    * waypoint 0.4.0 (new formula)
    * Update waypoint.rb
    
    Closes #79662.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/waypoint.rb
@@ -0,0 +1,26 @@
+class Waypoint < Formula
+  desc "Tool to build, deploy, and release any application on any platform"
+  homepage "https://www.waypointproject.io/"
+  url "https://github.com/hashicorp/waypoint/archive/v0.4.0.tar.gz"
+  sha256 "fd4e98f8c1e8ae18ad98374d52daa07dce6d97ae424cf831e69950a59ed22699"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/waypoint.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+
+  def install
+    system "make", "bin"
+    bin.install "waypoint"
+  end
+
+  test do
+    assert_match "Initial Waypoint configuration created!", shell_output("#{bin}/waypoint init")
+    assert_match "# An application to deploy.", File.read("waypoint.hcl")
+
+    assert_match "! failed to create client: no server connection configuration found",
+      shell_output("#{bin}/waypoint server bootstrap 2>&1", 1)
+
+    assert_match version.to_s, shell_output("#{bin}/waypoint version")
+  end
+end
