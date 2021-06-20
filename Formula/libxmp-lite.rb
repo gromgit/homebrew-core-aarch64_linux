e65747da3178be3984d3ba37dbf54270bcc22944
Rui Chen <rui@chenrui.dev>
2021-06-19 21:58:43 -0400

    libxmp-lite 4.5.0

--- Formula/libxmp-lite.rb
+++ Formula/libxmp-lite.rb
@@ -1,8 +1,8 @@
 class LibxmpLite < Formula
   desc "Lite libxmp"
   homepage "https://xmp.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/xmp/libxmp/4.4.1/libxmp-lite-4.4.1.tar.gz"
-  sha256 "bce9cbdaa19234e08e62660c19ed9a190134262066e7f8c323ea8ad2ac20dc39"
+  url "https://downloads.sourceforge.net/project/xmp/libxmp/4.5.0/libxmp-lite-4.5.0.tar.gz"
+  sha256 "19a019abd5a3ddf449cd20ca52cfe18970f6ab28abdffdd54cff563981a943bb"
   license "MIT"
 
   bottle do
@@ -14,15 +14,6 @@ class LibxmpLite < Formula
     sha256 cellar: :any, sierra:        "d1ed5c1803f622508c3e20bb9c48f9bc644d0d639574aaa298724dd0aa17262d"
     sha256 cellar: :any, el_capitan:    "a8fcd7a5ab446a221b7444b90191656175f6060a0730a703e4f862c4f49690f4"
     sha256 cellar: :any, yosemite:      "448d0a4bcd651c44551a1d3785de1c0181cce1ee374cd7903a629cb200a3011d"
-  end
-
-  # Remove for > 4.4.1
-  # Fix build failure "dyld: Symbol not found: _it_loader"
-  # Upstream commit "libxmp-lite building (wrong format loaders)"
-  # Already in master. Original PR 6 Nov 2016 https://github.com/cmatsuoka/libxmp/pull/82
-  patch :p2 do
-    url "https://github.com/cmatsuoka/libxmp/commit/a028835.patch?full_index=1"
-    sha256 "74b8689dcc23943168c6ae6afbda94dbcca78d08caae860b31ff573610ec5f92"
   end
 
   def install
