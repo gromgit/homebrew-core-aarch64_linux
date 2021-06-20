281d4f1da8f51c5c7a1bebb05be9a8e776672099
Nanda H Krishna <me@nandahkrishna.com>
2021-06-20 17:46:18 +0000

    plow: add 1.0.0 bottle.

--- Formula/plow.rb
+++ Formula/plow.rb
@@ -5,6 +5,13 @@ class Plow < Formula
   sha256 "facfea2bb32282c9e64d9447d14f721151dac6ad0da8a77a1135f2dcd89f3814"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2267bd8006cc3a1cfb2bd089e653d786d4aacf7d6a7df62b6e24e1bd7b077593"
+    sha256 cellar: :any_skip_relocation, big_sur:       "67dfe479f72d454eb9644e8480936f56b21c38cb99c1946856753950889a4af1"
+    sha256 cellar: :any_skip_relocation, catalina:      "f5400c99cd7b1f29079a142d67ca0073a70e7016bffdee12357e8f2ec8b4bfd6"
+    sha256 cellar: :any_skip_relocation, mojave:        "943124c877463a64925cad75a45b0dd591bf424741473cf37bfe7dcdd5f9c015"
+  end
+
   depends_on "go" => :build
 
   def install
