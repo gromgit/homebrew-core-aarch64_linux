9a2b5b6e2cb845832dfb621126d1b472b287758c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-20 19:06:43 +0000

    deja-gnu: update 1.6.3 bottle.

--- Formula/deja-gnu.rb
+++ Formula/deja-gnu.rb
@@ -7,12 +7,10 @@ class DejaGnu < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c8699cac95a51707a687e2a43de55517c9c19a49cdb34ea70a6ce869ee4ba4e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5bad10e6433487c43ec72f99cee16123953b6834ab714302474b5135b99a9ed5"
-    sha256 cellar: :any_skip_relocation, catalina:      "894df4a4de1df0698f3539db58615fa63bcee77f723eba6efd8d1700ce0bb827"
-    sha256 cellar: :any_skip_relocation, mojave:        "eea1adea3a1c062fd6ba0e85fefebe9f9036736a9d3a3744cec79123390270f3"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "eea1adea3a1c062fd6ba0e85fefebe9f9036736a9d3a3744cec79123390270f3"
-    sha256 cellar: :any_skip_relocation, sierra:        "5c1100eaf8ae4f28b1c4311241ddff8e0d195d0d241e106051bc60490d28d0e5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "aae2cc17f126ef380eadd7d9a4909320f38d3b62c5722a2f9a777090cfe61720"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0369e312459148a10582a3d6f100167b44a80cfec0f03223099ecdaab7097551"
+    sha256 cellar: :any_skip_relocation, catalina:      "0369e312459148a10582a3d6f100167b44a80cfec0f03223099ecdaab7097551"
+    sha256 cellar: :any_skip_relocation, mojave:        "0369e312459148a10582a3d6f100167b44a80cfec0f03223099ecdaab7097551"
   end
 
   head do
