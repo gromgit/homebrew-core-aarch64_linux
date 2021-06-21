f27194186ef79164a792502ebab6ab3ec3de8df2
Adrian Cole <adrian@tetrate.io>
2021-06-21 11:45:45 +0800

    envoy@1.17 1.17.3 (new formula)
    Closes #79695.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/envoy@1.17.rb
@@ -0,0 +1,52 @@
+class EnvoyAT117 < Formula
+  desc "Cloud-native high-performance edge/middle/service proxy"
+  homepage "https://www.envoyproxy.io"
+  url "https://github.com/envoyproxy/envoy.git",
+      tag:      "v1.17.3",
+      revision: "46bf743b97d0d3f01ff437b2f10cc0bd9cdfe6e4"
+  license "Apache-2.0"
+
+  keg_only :versioned_formula
+  # https://github.com/envoyproxy/envoy/blob/main/RELEASES.md#release-schedule
+  deprecate! date: "2022-01-11", because: :unsupported
+
+  depends_on "automake" => :build
+  depends_on "bazelisk" => :build
+  depends_on "cmake" => :build
+  depends_on "coreutils" => :build
+  depends_on "go" => :build
+  depends_on "libtool" => :build
+  depends_on "ninja" => :build
+  depends_on macos: :catalina
+
+  def install
+    args = %w[
+      -c
+      opt
+      --curses=no
+      --show_task_finish
+      --verbose_failures
+      --action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin
+      --test_output=all
+    ]
+    system Formula["bazelisk"].opt_bin/"bazelisk", "build", *args, "//source/exe:envoy-static"
+    bin.install "bazel-bin/source/exe/envoy-static" => "envoy"
+    pkgshare.install "configs", "examples"
+  end
+
+  test do
+    port = free_port
+
+    cp pkgshare/"configs/envoyproxy_io_proxy.yaml", testpath/"envoy.yaml"
+    inreplace "envoy.yaml" do |s|
+      s.gsub! "port_value: 9901", "port_value: #{port}"
+      s.gsub! "port_value: 10000", "port_value: #{free_port}"
+    end
+
+    fork do
+      exec bin/"envoy", "-c", "envoy.yaml"
+    end
+    sleep 10
+    assert_match "HEALTHY", shell_output("curl -s 127.0.0.1:#{port}/clusters?format=json")
+  end
+end
