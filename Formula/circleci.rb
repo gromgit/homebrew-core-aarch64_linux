20714d2ce90db8aefaf17608050b9bee62520775
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-21 21:07:19 +0000

    circleci: update 0.1.15384 bottle.

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -9,10 +9,10 @@ class Circleci < Formula
   head "https://github.com/CircleCI-Public/circleci-cli.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "46c107700e8fd2e46ab04c98b02c345a3f4d2c7438d5e65c517410bc6a68098e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cdb41db7e4427bbbb8bd0d308de6930cde264d5ec7c68cf341d82af803237b3c"
-    sha256 cellar: :any_skip_relocation, catalina:      "a028274a3b7eba6ed036835f9e0f8cc6a9900adfddb190953919444b246de36d"
-    sha256 cellar: :any_skip_relocation, mojave:        "c752bf21ecb6b2fad282af6055b8f23d5fe4db71c4a2effb7324029e94d92c86"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5f9731efd6131c48b7ca1beaea4f0d87a857bb2902648121fc942c798f8572f4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a661fcd82c8704d4232379bd7e7f507084ca5d622ec84855b4bfbf13a6388d54"
+    sha256 cellar: :any_skip_relocation, catalina:      "10526439610c545e20052e4af1eae59c52d591781cea415d7b15db4d1375ecff"
+    sha256 cellar: :any_skip_relocation, mojave:        "d2108c5308ec544c39353e9e44a01cf9756f7afe0a7c435becfc7e49dbea3ecf"
   end
 
   depends_on "go" => :build
