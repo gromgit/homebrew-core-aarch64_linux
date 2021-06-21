8ac90864d63908aee288d3732f216906fa9c3988
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-21 15:12:12 +0000

    isort: update 5.9.1 bottle.

--- Formula/isort.rb
+++ Formula/isort.rb
@@ -13,10 +13,10 @@ class Isort < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7851ef1b59b101c768f455196f1a226811ff4f89eed5ce855422cf1a254ecff1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0ae4d37f79fed6ac903ce991e19888864673fef72f544e4e99e994d880437e52"
-    sha256 cellar: :any_skip_relocation, catalina:      "95af85973a39cafec59d9f21a50608e09881ae7b5e70926616dcdf7ad70b7a98"
-    sha256 cellar: :any_skip_relocation, mojave:        "253112ca0152460c548df9fac3d6607d73090950241d3af59282dabd74b8a607"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3b6ee338a2f23a497ca00366c61f37b715706853f71f24f86364312333be5373"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9f74871a6fac08d40dab69862f01cde96891370f21dee524f051f614c2710d52"
+    sha256 cellar: :any_skip_relocation, catalina:      "4e49aad4bce47aadd0a00cf6579e567a5d21eac7e487c61b980c820b8e928ae1"
+    sha256 cellar: :any_skip_relocation, mojave:        "120c5d1d55fefe15d0d921553785574efe85e3bef376941c39caf3c691d85eea"
   end
 
   depends_on "python@3.9"
