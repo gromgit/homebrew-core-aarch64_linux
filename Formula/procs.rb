e7ca30c4ef4b81dc8454bc7e9859900378d1ccd6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-22 16:12:41 +0000

    procs: update 0.11.9 bottle.

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -6,10 +6,10 @@ class Procs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3ea499c87e23aa0ccc4b09fab476cf4f024c7a22849746e1491c0f784a4c2f94"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6a5bcfed2d9478f408a731190ba0123834b3a26d46acfe8a0f3ff07371926a61"
-    sha256 cellar: :any_skip_relocation, catalina:      "19c8493771d14e992c85103fbebade854e7099bc95013751468c077e42d8181e"
-    sha256 cellar: :any_skip_relocation, mojave:        "a9d81106f9e8cb246dcf9ba80e102594e52b52f248d930c6e8b0b3443f021710"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "043b84aa56abfde49ee039d289d61a509d22aca391d38d89fe76c0c7a0cfaf71"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bfab01d261a0c4cddb58b37910e1b468161d2f714440527ff64ab0be03d0786c"
+    sha256 cellar: :any_skip_relocation, catalina:      "1947a7acae8681b50278812ce9c61ca31330d9dd6fb02fd8a59405e86f67af68"
+    sha256 cellar: :any_skip_relocation, mojave:        "6fd08826c214192439fe5589500292002de415280651cd1d13d5e29cd6121bd5"
   end
 
   depends_on "rust" => :build
