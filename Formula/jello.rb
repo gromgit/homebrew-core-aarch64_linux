6dd4508014736d4256bc0e799bbf46dd08174542
Nanda H Krishna <me@nandahkrishna.com>
2021-06-22 18:30:46 +0000

    jello: add 1.4.2 bottle.

--- Formula/jello.rb
+++ Formula/jello.rb
@@ -7,6 +7,13 @@ class Jello < Formula
   sha256 "4848e52d92efee8053cb29acb3ae0d09086b4e037d637db50783708ee5aa5aa9"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a9405c6a0354e8007cf25dd40b460f91fe477f20e80e7a24270d044e3ff6b1a7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8c342af99e9ad5e4ede36050bad7d8fc63a3a24bc4776e12f1fb41d7ad634a5f"
+    sha256 cellar: :any_skip_relocation, catalina:      "8c342af99e9ad5e4ede36050bad7d8fc63a3a24bc4776e12f1fb41d7ad634a5f"
+    sha256 cellar: :any_skip_relocation, mojave:        "8c342af99e9ad5e4ede36050bad7d8fc63a3a24bc4776e12f1fb41d7ad634a5f"
+  end
+
   depends_on "python@3.9"
 
   resource "Pygments" do
