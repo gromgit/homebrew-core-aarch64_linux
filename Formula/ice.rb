1ddc8d1cb09669fc459bc14b6e3e97376e157a0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-22 22:07:43 +0000

    ice: update 3.7.6 bottle.

--- Formula/ice.rb
+++ Formula/ice.rb
@@ -11,10 +11,9 @@ class Ice < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9f6864341db930e3fec14ee0e108e2c71d268441d6890c76f57e873475daaf99"
-    sha256 cellar: :any, big_sur:       "3a61df370da3e0ee676eaabe0470a0bc75296dc0dd3d4a62bd02e7a84829a50a"
-    sha256 cellar: :any, catalina:      "ce6660264a1b883917ca7437e173ec954d5b552f9f1fcbda82ae3a8594668dd4"
-    sha256 cellar: :any, mojave:        "d1b6db2cd443c0cf57990bea3651fefeca93e22a6542657967617560e483e78a"
+    sha256 cellar: :any, arm64_big_sur: "aba8e77b6144ab02730670f94205ea2de3efd4581b6e1f167ae1ae48bd5405ae"
+    sha256 cellar: :any, big_sur:       "0c63ce1c5ea37d98b1cd64411e5c6d9e445330c44ab6cb864bd1995b1d5fb91f"
+    sha256 cellar: :any, catalina:      "107f893606aa135531ca17cbb0328f1e664040d36c5373e83a856ba525ba3647"
   end
 
   depends_on "lmdb"
