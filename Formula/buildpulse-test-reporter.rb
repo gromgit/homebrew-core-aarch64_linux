c32d698d638e7c76eaa6fc467d785ef53a644def
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-22 19:30:08 +0100

    buildpulse-test-reporter 0.15.0 (new formula)
    This formula fails the audit, but we may have need of it in
    Homebrew/brew (cf. Homebrew/brew#11578), so I think this will still be
    useful to add to homebrew-core.

---
+++ Formula/buildpulse-test-reporter.rb
@@ -0,0 +1,27 @@
+class BuildpulseTestReporter < Formula
+  desc "Connect your CI to BuildPulse to detect, track, and rank flaky tests"
+  homepage "https://buildpulse.io"
+  url "https://github.com/buildpulse/test-reporter/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "5119c189341bec12531fa85ac504a3c88b5ca08f090f2ee92d6cef667d66684d"
+  license "MIT"
+  head "https://github.com/buildpulse/test-reporter.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    goldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+      -X main.Commit=#{tap.user}
+    ].join(" ")
+    system "go", "build", *std_go_args(ldflags: goldflags), "./cmd/test-reporter"
+  end
+
+  test do
+    binary = bin/name
+    assert_match version.to_s, shell_output("#{binary} --version")
+
+    fake_dir = "im-not-real"
+    assert_match "Received args: #{fake_dir}", shell_output("#{binary} submit #{fake_dir}", 1)
+  end
+end
