d89646f8288205932d5d76d67d84f55196da236e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-22 17:16:16 +0000

    libcoap: update 4.3.0 bottle.

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -6,11 +6,10 @@ class Libcoap < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d8f66e021f0ed8d227696e8e37d97085a77624aaa391fdd63f4f00326a101289"
-    sha256 cellar: :any, big_sur:       "f57bd53021361886f6928bbb0156dd642bd39dbdd459e5a69951560e4cc47050"
-    sha256 cellar: :any, catalina:      "344f2a098d9f1767d50135fbf4ae3bdf893a079ebf8a54f248811673fa437e39"
-    sha256 cellar: :any, mojave:        "012f1efcb1655479c531df4db98eb481d83971751edffb99b4ca8c50592cd27c"
-    sha256 cellar: :any, high_sierra:   "a68df19a4ca87c677173c14b534848592bb35e46a715ca066bcd114f8c735236"
+    sha256 cellar: :any, arm64_big_sur: "57300cff98f6ad59e6c4cf95de26bf0a4dbcf0fb64d2d911d6bdddadf63da1dc"
+    sha256 cellar: :any, big_sur:       "f82b84bbe8f8db3b33810627b2a6d68462fc93a82daa6385a79afe7738534ae5"
+    sha256 cellar: :any, catalina:      "4e2fa796da56bec1271a091dc6262b812ece25b989c62be4e4d4d08943dfbe7d"
+    sha256 cellar: :any, mojave:        "09ad4d7b446860842318c4f21a4ee112e1dda916a36f9023811d7abb7bd66001"
   end
 
   depends_on "autoconf" => :build
