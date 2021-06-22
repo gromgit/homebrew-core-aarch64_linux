470ff5b867042b87775bc9a7ac217f708c5078f2
Caleb Xu <calebcenter@live.com>
2021-06-22 08:27:53 -0400

    sentencepiece 0.1.96
    Closes #79764.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sentencepiece.rb
+++ Formula/sentencepiece.rb
@@ -1,8 +1,8 @@
 class Sentencepiece < Formula
   desc "Unsupervised text tokenizer and detokenizer"
   homepage "https://github.com/google/sentencepiece"
-  url "https://github.com/google/sentencepiece/archive/v0.1.95.tar.gz"
-  sha256 "99d6fc87e9a111d964dbfa85bdb0fd2eab7c469b3154db056588e974fc5b9866"
+  url "https://github.com/google/sentencepiece/archive/v0.1.96.tar.gz"
+  sha256 "5198f31c3bb25e685e9e68355a3bf67a1db23c9e8bdccc33dc015f496a44df7a"
   license "Apache-2.0"
   head "https://github.com/google/sentencepiece.git"
 
