1c8aa348a6a770fa3e9d0753c43535136ba217a1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-21 18:22:14 -0700

    sqlite: update homepage, livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -1,14 +1,17 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
-  homepage "https://sqlite.org/"
+  homepage "https://sqlite.org/index.html"
   url "https://sqlite.org/2021/sqlite-autoconf-3350500.tar.gz"
   version "3.35.5"
   sha256 "f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0"
   license "blessing"
 
   livecheck do
-    url "https://sqlite.org/news.html"
-    regex(%r{v?(\d+(?:\.\d+)+)</h3>}i)
+    url :homepage
+    regex(%r{href=.*?releaselog/v?(\d+(?:[._]\d+)+)\.html}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match&.first&.gsub("_", ".") }
+    end
   end
 
   bottle do
