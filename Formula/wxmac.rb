f5e3982c5c1f83906ba3f8aef422dda3e5a1dda0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-22 16:54:34 +0100

    wxmac 3.1.5
    Upstream labels this as a "Development Release", but I believe this is
    actually the version we want to package in Homebrew/core. We do the same
    for some other formulae (e.g. gnupg).
    
    What upstream mean by "Development Release" is that this is the version
    that gets new features. What they call a "stable" is probably more
    accurately called a long-term support release. [1] Updates to this
    branch are bug-fixes only and not new features.
    
    This appears to be the version vendored by `wxpython`, so we should
    hopefully be able to use this in place of the bundled one.
    
    [1] The section on the stable release on their downloads page says that
    the API for this version has been stable since 11 November 2013.

--- Formula/wxmac.rb
+++ Formula/wxmac.rb
@@ -1,15 +1,14 @@
 class Wxmac < Formula
   desc "Cross-platform C++ GUI toolkit (wxWidgets for macOS)"
   homepage "https://www.wxwidgets.org"
-  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.5.1/wxWidgets-3.0.5.1.tar.bz2"
-  sha256 "440f6e73cf5afb2cbf9af10cec8da6cdd3d3998d527598a53db87099524ac807"
+  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxWidgets-3.1.5.tar.bz2"
+  sha256 "d7b3666de33aa5c10ea41bb9405c40326e1aeb74ee725bb88f90f1d50270a224"
   license "wxWindows"
-  revision 2
   head "https://github.com/wxWidgets/wxWidgets.git"
 
   livecheck do
     url :stable
-    regex(/^v?(\d+\.\d*[02468](?:\.\d+)*)$/i)
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
   bottle do
@@ -42,8 +41,7 @@ class Wxmac < Formula
       "--enable-std_string",
       "--enable-svg",
       "--enable-unicode",
-      "--enable-webkit",
-      "--enable-webview",
+      "--enable-webviewwebkit",
       "--with-expat",
       "--with-libjpeg",
       "--with-libpng",
@@ -59,6 +57,7 @@ class Wxmac < Formula
       # Set with-macosx-version-min to avoid configure defaulting to 10.5
       args << "--with-macosx-version-min=#{MacOS.version}"
       args << "--with-osx_cocoa"
+      args << "--with-libiconv"
     end
 
     system "./configure", *args
@@ -69,6 +68,10 @@ class Wxmac < Formula
     # using wx-config can find both wxmac and wxpython headers,
     # which are linked to the same place
     inreplace "#{bin}/wx-config", prefix, HOMEBREW_PREFIX
+
+    # Move some files out of the way to prevent conflict with `wxmac-stable`
+    bin.install_symlink "#{bin}/wx-config" => "wx-config-#{version.major_minor}"
+    (share/"wx"/version.major_minor).install share/"aclocal", share/"bakefile"
   end
 
   test do
