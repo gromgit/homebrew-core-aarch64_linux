f59e15bf3354f2954851ca5c8c176c71a38b84cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-22 08:08:54 +0000

    neo4j: update 4.3.1 bottle.

--- Formula/neo4j.rb
+++ Formula/neo4j.rb
@@ -12,7 +12,7 @@ class Neo4j < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "30c352ee7c7d6e5030f7e3744ea257c9581da2f27c27c21abab7fd87fc7e5350"
+    sha256 cellar: :any_skip_relocation, all: "cf0b5d73eb2da03ef6cec78476783ed906d6bb415da1ae02d15d6337743ff7b7"
   end
 
   depends_on "openjdk@11"
