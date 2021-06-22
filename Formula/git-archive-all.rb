19d950bb24d5a6000987fa34c60f012949e06859
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-22 20:00:07 +0000

    git-archive-all: update 1.23.0_1 bottle.

--- Formula/git-archive-all.rb
+++ Formula/git-archive-all.rb
@@ -10,10 +10,7 @@ class GitArchiveAll < Formula
   head "https://github.com/Kentzo/git-archive-all.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6b56c146d2bc161c6214878d142cd67bc646ade222b7e45aab1691f7f3987a37"
-    sha256 cellar: :any_skip_relocation, big_sur:       "27e8df90701d0399573a59237fdaa2a1c233ebd1e9007686df7e8a0b2b7d3be4"
-    sha256 cellar: :any_skip_relocation, catalina:      "7a8f0e71281afa27399fab199e975f0d717c0593526701d2f43eac2f096a41b9"
-    sha256 cellar: :any_skip_relocation, mojave:        "a375cfca74cda33d29bc74ed712e0dedb0495c56a1378a25009edcabcbdb44fc"
+    sha256 cellar: :any_skip_relocation, all: "2c5a291b77ebe4a1f5fc2f3a0a2de81bf6b25478940494f8e314f8f267debd04"
   end
 
   depends_on "python@3.9"
