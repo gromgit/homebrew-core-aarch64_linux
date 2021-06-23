73500bd3af0eb66e5c0846949530e8fbe4ce88ba
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-23 22:53:07 +0000

    wasmtime: add 0.28.0 bottle.

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -12,6 +12,13 @@ class Wasmtime < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "136433d0ec5369a6685d9933c3302a74836b3fecb69dcaf66bda9622bb42172d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "31f0a85a18b265a95a88cef8bb953ba3b5f2793a73cdae86e84cb09362710c5b"
+    sha256 cellar: :any_skip_relocation, catalina:      "741e62eb7b97c4a9e6d0e55b7476966db477782bebb7c3856dfddf447fbd8cde"
+    sha256 cellar: :any_skip_relocation, mojave:        "991ea5ad43a53a43d1b5caabfacdea18ec9396affa167f499fdd3867079e3c0b"
+  end
+
   depends_on "rust" => :build
 
   def install
