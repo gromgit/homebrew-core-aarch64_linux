c4307bf0c5fa83317779ac3e94ef0c1378c0700d
lucky <>
2021-06-23 15:59:45 +0300

    wasmtime 0.28.0 (new formula)
    Closes #79880.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/wasmtime.rb
@@ -0,0 +1,27 @@
+class Wasmtime < Formula
+  desc "Standalone JIT-style runtime for WebAssembly, using Cranelift"
+  homepage "https://wasmtime.dev/"
+  url "https://github.com/bytecodealliance/wasmtime.git",
+      tag:      "v0.28.0",
+      revision: "e8b8947956a7fff6ce1475fafe6a13f15073570c"
+  license "Apache-2.0"
+  head "https://github.com/bytecodealliance/wasmtime.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    wasm = ["0061736d0100000001070160027f7f017f030201000707010373756d00000a09010700200020016a0b"].pack("H*")
+    (testpath/"sum.wasm").write(wasm)
+    assert_equal "3\n",
+      shell_output("#{bin}/wasmtime #{testpath/"sum.wasm"} --invoke sum 1 2")
+  end
+end
