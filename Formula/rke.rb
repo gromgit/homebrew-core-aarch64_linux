e7a1d990cb897b4f32d6de52c4e422b829752f7d
Rui Chen <rui@chenrui.dev>
2021-06-23 02:16:26 -0400

    rke 1.2.9
    Closes #79867.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -1,8 +1,8 @@
 class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/latest/en/"
-  url "https://github.com/rancher/rke/archive/v1.2.8.tar.gz"
-  sha256 "c42a8f088884dbe0f2c01756d2fb56780b5e7cc92234f386b773996782423529"
+  url "https://github.com/rancher/rke/archive/v1.2.9.tar.gz"
+  sha256 "958f1be715be516bc782896d6d77b0665054ec6545f94d1b423f24edec1a77f0"
   license "Apache-2.0"
 
   bottle do
