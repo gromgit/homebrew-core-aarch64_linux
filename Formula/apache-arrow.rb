7e51609c31623310e9f2db98ccaae8c228e10adc
Rui Chen <rui@chenrui.dev>
2021-06-23 00:31:05 -0400

    apache-arrow: revision bump (thrift 0.14.2)
    Closes #79852.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -5,6 +5,7 @@ class ApacheArrow < Formula
   mirror "https://archive.apache.org/dist/arrow/arrow-4.0.1/apache-arrow-4.0.1.tar.gz"
   sha256 "75ccbfa276b925c6b1c978a920ff2f30c4b0d3fdf8b51777915b6f69a211896e"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/apache/arrow.git"
 
   bottle do
