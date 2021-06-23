6cbebaf43acfedc532dc0ee6d99d0e54fc270e33
Rui Chen <rui@chenrui.dev>
2021-06-23 00:04:05 -0400

    wtf 20210618
    * wtf 20210618
    * wtf: remove `bottle :unneeded`
    
    Closes #79857.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,16 +1,14 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20210416.tar.gz"
-  sha256 "af3c5c92f4f76e14fcf2a821e520af09043781b8f17fc8a6e8d115fc9f18e1b7"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20210618.tar.gz"
+  sha256 "c626373b128b2dfdfc393b8d8c1b26d6f69b2bcee5a7c8a03c4e5d396cb714e1"
   license :public_domain
 
   livecheck do
     url :stable
     regex(%r{url=.*?/wtf[._-]v?(\d{6,8})\.t}i)
   end
-
-  bottle :unneeded
 
   def install
     inreplace %w[wtf wtf.6], "/usr/share", share
