67e9fdd69600bbce6da87a13ef32e9d02af57d21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-24 11:06:57 +0000

    luajit-openresty: update 2.1-20210510 bottle.

--- Formula/luajit-openresty.rb
+++ Formula/luajit-openresty.rb
@@ -19,10 +19,10 @@ class LuajitOpenresty < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1206d1abe22d5ce6c2be8899c5829172f25264ea2b888926116c0b5aa21eedbd"
-    sha256 cellar: :any, big_sur:       "c4058bf652e63d1fa42b1453a7f1f100f1c8702c874d778693318d2e903ce465"
-    sha256 cellar: :any, catalina:      "31b27b774a37110992c1f4430f2a5b5059dca29dd05e744654a16ae06adee76d"
-    sha256 cellar: :any, mojave:        "c9ab34e0917b1574651b031fc1d4488e91777e77ae9661d9afe30c0444996a5e"
+    sha256 cellar: :any, arm64_big_sur: "96e3a886e94e5db4b61c56abb5901143e33e532b4f12cd8da1a0afd92ec56fa1"
+    sha256 cellar: :any, big_sur:       "e25634b88ac6fac6a0b9b10d0ba3f3b44d2becdef2459e95bfab7c4367035e9f"
+    sha256 cellar: :any, catalina:      "6d969910e7805c1e4655a43321370e68f150efbb0825b12add00ecacdea75513"
+    sha256 cellar: :any, mojave:        "c80ab72984ae032b2a04cd4ac8d4c759ca3fe8a0a33f6b0252b2556cf4a3cc79"
   end
 
   keg_only "it conflicts with the LuaJIT formula"
