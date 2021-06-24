fd3b04ca2a1f60f5e5d33a9835bb68a3976ea103
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-24 15:30:18 +0000

    gambit: update 16.0.1_2 bottle.

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -7,10 +7,10 @@ class Gambit < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "45b7d50d9ab796456c5688aabcffa048f39c5d8698e914f2c9d4d308d1f795a9"
-    sha256 cellar: :any, big_sur:       "e5b2c33a83a81fedeaf73e1e62186864a52031c6046fb7a69f2d5a0e44d881f4"
-    sha256 cellar: :any, catalina:      "05ab5f78b317c4128110507c07b8eb94914361624248defae542008d9a9d62d5"
-    sha256 cellar: :any, mojave:        "74b93be01bbbdd562b00977b6d3f442c0edcfde923ab973c1c62b61e9afd33ad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8baa71e381f33562fc0580da41d111451900d28d20e162d0d86b82d10a8e21a8"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c036470f1e05014eb81ed12d66897c67501e51aa82569e928bf645ff273f6d98"
+    sha256 cellar: :any_skip_relocation, catalina:      "4847905d44b80902b4d059b37bbf22aff4f1a4a760270aea1db539b3632a93ab"
+    sha256 cellar: :any_skip_relocation, mojave:        "34ab61a2b2a66ee44a9098e49c519eab78e90d11029abe027844f7d3a341ab22"
   end
 
   depends_on "autoconf" => :build
