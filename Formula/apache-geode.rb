8166f133b7ff1fde5ad2e446230c10eb700293be
Owen Nichols <onichols@apache.org>
2021-06-24 15:45:27 -0700

    apache-geode 1.13.3
    * apache-geode 1.13.3
    * remove bottle :unneeded and use JDK11
    
    Closes #79940.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apache-geode.rb
+++ Formula/apache-geode.rb
@@ -1,21 +1,19 @@
 class ApacheGeode < Formula
   desc "In-memory Data Grid for fast transactional data processing"
   homepage "https://geode.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=geode/1.13.2/apache-geode-1.13.2.tgz"
-  mirror "https://archive.apache.org/dist/geode/1.13.2/apache-geode-1.13.2.tgz"
-  mirror "https://downloads.apache.org/geode/1.13.2/apache-geode-1.13.2.tgz"
-  sha256 "bc2d2e9bcec8f2fdfa2419894962314a3e3569fa617c3e2f6beca8097197ad02"
+  url "https://www.apache.org/dyn/closer.lua?path=geode/1.13.3/apache-geode-1.13.3.tgz"
+  mirror "https://archive.apache.org/dist/geode/1.13.3/apache-geode-1.13.3.tgz"
+  mirror "https://downloads.apache.org/geode/1.13.3/apache-geode-1.13.3.tgz"
+  sha256 "c725d01336b94e4c47a921825aa1616872754def202ee1c72074156c3a3848d1"
   license "Apache-2.0"
 
-  bottle :unneeded
-
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
     rm_f "bin/gfsh.bat"
     bash_completion.install "bin/gfsh-completion.bash" => "gfsh"
     libexec.install Dir["*"]
-    (bin/"gfsh").write_env_script libexec/"bin/gfsh", Language::Java.java_home_env("1.8")
+    (bin/"gfsh").write_env_script libexec/"bin/gfsh", Language::Java.java_home_env("11")
   end
 
   test do
