cccffbd133e1bdbc3f5e36ed2f1e7ee10f81da87
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 15:52:19 +0100

    ievms: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/ievms.rb
+++ Formula/ievms.rb
@@ -4,8 +4,6 @@ class Ievms < Formula
   url "https://github.com/xdissent/ievms/archive/v0.3.3.tar.gz"
   sha256 "95cafdc295998712c3e963dc4a397d6e6a823f6e93f2c119e9be928b036163be"
 
-  bottle :unneeded
-
   depends_on "unar"
 
   def install
