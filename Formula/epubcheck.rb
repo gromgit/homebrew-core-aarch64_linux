612f06779d77d9ebf12356488a7d7ae7c10b145e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-25 22:10:23 +0000

    epubcheck: add 4.2.5 bottle.

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -5,6 +5,10 @@ class Epubcheck < Formula
   sha256 "2f725b7f491bd7e215fe3fb5492d0554207c838c35165cc195963ddcde39c887"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8250fd1ac4c60a8e20a15573b813510653a146284b4cdfa008a2600efeb32d43"
+  end
+
   depends_on "openjdk"
 
   def install
