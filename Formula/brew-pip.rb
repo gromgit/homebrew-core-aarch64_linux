ca401c2f3451f0f2024b46abfba6236e68bbe04a
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 15:40:12 +0100

    brew-pip: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/brew-pip.rb
+++ Formula/brew-pip.rb
@@ -5,8 +5,6 @@ class BrewPip < Formula
   sha256 "9049a6db97188560404d8ecad2a7ade72a4be4338d5241097d3e3e8e215cda28"
   license "MIT"
 
-  bottle :unneeded
-
   def install
     bin.install "bin/brew-pip"
   end
