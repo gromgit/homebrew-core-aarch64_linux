a28044509ee788197b52af9f30584329950bf2e1
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 17:09:21 +0100

    zzz: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943
    
    Closes #80008.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zzz.rb
+++ Formula/zzz.rb
@@ -5,8 +5,6 @@ class Zzz < Formula
   sha256 "8c8958b65a74ab1081ce1a950af6d360166828bdb383d71cc8fe37ddb1702576"
   head "https://github.com/Orc/Zzz.git"
 
-  bottle :unneeded
-
   # No test is possible: this has no --help or --version, it just
   # sleeps the Mac instantly.
   def install
