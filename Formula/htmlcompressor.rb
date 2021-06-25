f52096c322b0ae5bb18af74598046ccf2318fc55
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 15:52:17 +0100

    htmlcompressor: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/htmlcompressor.rb
+++ Formula/htmlcompressor.rb
@@ -4,8 +4,6 @@ class Htmlcompressor < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/htmlcompressor/htmlcompressor-1.5.3.jar"
   sha256 "88894e330cdb0e418e805136d424f4c262236b1aa3683e51037cdb66310cb0f9"
 
-  bottle :unneeded
-
   depends_on "openjdk"
 
   def install
