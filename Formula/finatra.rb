75a5f8e256664bc4475887165ce73e9a2f54a836
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-25 22:10:26 +0000

    finatra: add 1.5.3 bottle.

--- Formula/finatra.rb
+++ Formula/finatra.rb
@@ -5,6 +5,10 @@ class Finatra < Formula
   sha256 "aa4fab5ccdc012da9edf4650addf54b6ba64eb7e6a5e88d8c76e68e4d89216de"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4647f53656631f55bef9d4a4c3ef66adafa598c3e48a65be4199a1cdc33bf5dc"
+  end
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"finatra"
