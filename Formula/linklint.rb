0c77afec9f6d42e15ca8e902e57f3bc5b4ca12a9
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 16:07:28 +0100

    linklint: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/linklint.rb
+++ Formula/linklint.rb
@@ -4,8 +4,6 @@ class Linklint < Formula
   url "http://linklint.org/download/linklint-2.3.5.tar.gz"
   sha256 "ecaee456a3c2d6a3bd18a580d6b09b6b7b825df3e59f900270fe3f84ec3ac9c7"
 
-  bottle :unneeded
-
   def install
     mv "READ_ME.txt", "README"
 
