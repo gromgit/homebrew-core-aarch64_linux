eb8977431aba9a5f333430de623060a6046ec4c9
Nick Rolfe <nickrolfe@github.com>
2021-06-25 19:51:15 +0100

    debianutils: fix exponential backtracking in livecheck regex (#80016)

--- Formula/debianutils.rb
+++ Formula/debianutils.rb
@@ -7,7 +7,7 @@ class Debianutils < Formula
 
   livecheck do
     url "https://packages.qa.debian.org/d/debianutils.html"
-    regex(/href=.*?debianutils[._-]v?(\d+(?:.\d+)+).dsc/i)
+    regex(/href=.*?debianutils[._-]v?(\d+(?:\.\d+)+).dsc/i)
   end
 
   bottle do
