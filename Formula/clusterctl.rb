fd63507269f29623300941531044a886d2419dff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-25 07:06:49 +0000

    clusterctl: update 0.4.0 bottle.

--- Formula/clusterctl.rb
+++ Formula/clusterctl.rb
@@ -11,10 +11,10 @@ class Clusterctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "20a813817660bfc45c70d01fec814fa1155febbc6d917e3c55d8e058ea0a2603"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fce4aed624f692146b54755af147e3b40b97226aec1bfe7cef36752b67dd0ef6"
-    sha256 cellar: :any_skip_relocation, catalina:      "ee868f60877c705914e9db906b1990c33d35aebf91db26f4e65117087cc6e66f"
-    sha256 cellar: :any_skip_relocation, mojave:        "8a1ed5fbdf08a7928612a5dfb0d6fd082c013b03f552fdc51728b70df5dc827f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d7dbe58662801cc61010395c0ab0599bf41f0f66ecaa8a2a2edb0dc22695df5d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "86cc35474c11ea9869cce7b4c6aec6eae2d71f1cd334b08576a9dbe8dc1cdea2"
+    sha256 cellar: :any_skip_relocation, catalina:      "fdacdcc5c272ce5ab9bb84ec0dbb3560888221072aa71fde46fb280cb33be621"
+    sha256 cellar: :any_skip_relocation, mojave:        "6f97b2dcdbbd79194265ca46715f7bc6511acd9a6db4ee33496f7fdf8baa0f82"
   end
 
   depends_on "go" => :build
