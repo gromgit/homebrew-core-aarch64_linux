f57bd2af1689632d44cd19ddd9a777c3b8e1731a
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 17:04:06 +0100

    tgenv: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/tgenv.rb
+++ Formula/tgenv.rb
@@ -11,8 +11,6 @@ class Tgenv < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle :unneeded
-
   uses_from_macos "unzip"
 
   conflicts_with "terragrunt", because: "tgenv symlinks terragrunt binaries"
