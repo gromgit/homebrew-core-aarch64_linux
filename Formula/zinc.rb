62ed27f37a9b2cd0dda39c25f1b46abd5ce8d00d
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 17:09:19 +0100

    zinc: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/zinc.rb
+++ Formula/zinc.rb
@@ -4,14 +4,14 @@ class Zinc < Formula
   url "https://downloads.typesafe.com/zinc/0.3.15/zinc-0.3.15.tgz"
   sha256 "5ec4df3fa2cbb271d65a5b478c940a9da6ef4902aa8c9d41a76dd253e3334ca7"
 
-  bottle :unneeded
-
   deprecate! date: "2018-06-10", because: :repo_archived
+
+  depends_on "openjdk@11"
 
   def install
     rm_f Dir["bin/ng/{linux,win}*"]
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/zinc"
+    (bin/"zinc").write_env_script libexec/"bin/zinc", Language::Java.overridable_java_home_env("11")
   end
 
   test do
