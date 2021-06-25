d80533a1466e3e926008c3d1725abb89a8e8c9d5
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 16:58:23 +0100

    sendemail: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/sendemail.rb
+++ Formula/sendemail.rb
@@ -6,8 +6,6 @@ class Sendemail < Formula
   sha256 "6dd7ef60338e3a26a5e5246f45aa001054e8fc984e48202e4b0698e571451ac0"
   license "GPL-2.0+"
 
-  bottle :unneeded
-
   # Reported upstream: https://web.archive.org/web/20191013154932/caspian.dotconf.net/menu/Software/SendEmail/#comment-1119965648
   patch do
     url "https://raw.githubusercontent.com/mogaal/sendemail/e785a6d284884688322c9b39c0f64e20a43ea825/debian/patches/fix_ssl_version.patch"
