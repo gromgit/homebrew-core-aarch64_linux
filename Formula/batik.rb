aea2b0dcfc940f0bc5ba06104a3e30fd6033b7ca
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 15:40:07 +0100

    batik: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/batik.rb
+++ Formula/batik.rb
@@ -6,8 +6,6 @@ class Batik < Formula
   sha256 "0f4eaa81be70752e406a19f39c39c018848f09963f08fa8974640547052c4618"
   license "Apache-2.0"
 
-  bottle :unneeded
-
   depends_on "openjdk"
 
   def install
