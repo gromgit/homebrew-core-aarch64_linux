5bfe8a23a0e7f772c449faaa2cb668bbbb4619ae
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 15:38:15 +0100

    aws-cfn-tools: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/aws-cfn-tools.rb
+++ Formula/aws-cfn-tools.rb
@@ -6,8 +6,6 @@ class AwsCfnTools < Formula
   sha256 "382e3e951833fd77235fae41c1742224d68bdf165e1ace4200ee88c01ac29a90"
   revision 1
 
-  bottle :unneeded
-
   depends_on "ec2-api-tools"
   depends_on "openjdk"
 
