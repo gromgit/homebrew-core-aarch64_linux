728b42b2173cf16be565a564d14f926a64e217fb
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 16:50:39 +0100

    ppss: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/ppss.rb
+++ Formula/ppss.rb
@@ -5,8 +5,6 @@ class Ppss < Formula
   sha256 "25d819a97d8ca04a27907be4bfcc3151712837ea12a671f1a3c9e58bc025360f"
   license "GPL-3.0"
 
-  bottle :unneeded
-
   def install
     bin.install "ppss"
   end
