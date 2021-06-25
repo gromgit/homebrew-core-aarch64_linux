1b020e0301d614e380a4dfe18dc8630de70b3ca3
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 15:52:27 +0100

    jpegrescan: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/jpegrescan.rb
+++ Formula/jpegrescan.rb
@@ -5,8 +5,6 @@ class Jpegrescan < Formula
   sha256 "a8522e971d11c904f4b61af665c3be800f26404e2b14f5f80c675b4a72a42b32"
   head "https://github.com/kud/jpegrescan.git"
 
-  bottle :unneeded
-
   depends_on "jpeg"
 
   def install
