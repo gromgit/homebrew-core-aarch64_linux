4f8cf68e8b5893c27a58fb2bef3ac238404ff7ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-25 07:06:53 +0000

    kcptun: update 20210624 bottle.

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -7,10 +7,10 @@ class Kcptun < Formula
   head "https://github.com/xtaci/kcptun.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8ab8b8114de4af975ace979ee4baefb67b7ae04b0895d0863993f08dc574f193"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cc75f03bc0f50c583a5e0f96213676e497488f1874d957807d894ca046ddda3a"
-    sha256 cellar: :any_skip_relocation, catalina:      "003ec31729751a51c32c13c44d8dcc255550d300d38a9267167b8e9455a79212"
-    sha256 cellar: :any_skip_relocation, mojave:        "dbc0ec286493d29d1df9c9c7bde3290339efa894bed73474caad58926e58c717"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6e345408ba16132ad0013c13015df96fc97fdf4661e67d4f0fc01c574a203097"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2cd2c509194e43eafd7b14cb3374e2b96b2438f0c8430aea762915fcfc1d5971"
+    sha256 cellar: :any_skip_relocation, catalina:      "cd5687433da66043168a885f2ce895232fa7e44564a4ac73ae2875eaf1310fa2"
+    sha256 cellar: :any_skip_relocation, mojave:        "19f310dd4027105d9dc76a526571f9e286c1d77f530de3012a9130daf3f806d9"
   end
 
   depends_on "go" => :build
