8b8c383e60df99b3cab9eedb02efef0a5451c3fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-25 22:10:25 +0000

    fff: add 2.2 bottle.

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -5,6 +5,10 @@ class Fff < Formula
   sha256 "45f6e1091986c892ea45e1ac82f2d7f5417cfb343dc569d2625b5980e6bcfb62"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3f3ef09db2afedd44ec883cfc9a050400e3a4c1526b9465185386a8494283467"
+  end
+
   def install
     bin.install "fff"
     man1.install "fff.1"
