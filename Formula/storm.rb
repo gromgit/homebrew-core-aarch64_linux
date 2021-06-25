899ae9a69fe1b5e1149b2a6ded5e6509f59cf075
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 16:58:29 +0100

    storm: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -6,13 +6,13 @@ class Storm < Formula
   sha256 "f621163f349a8e85130bc3d2fbb34e3b08f9c039ccac5474f3724e47a3a38675"
   license "Apache-2.0"
 
-  bottle :unneeded
+  depends_on "openjdk"
 
   conflicts_with "stormssh", because: "both install 'storm' binary"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/storm"
+    (bin/"storm").write_env_script libexec/"bin/storm", Language::Java.overridable_java_home_env
   end
 
   test do
