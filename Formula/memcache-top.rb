e074b72c7d7e178c97660d84e524c14de17ce565
Mike McQuaid <mike@mikemcquaid.com>
2021-06-25 16:07:33 +0100

    memcache-top: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/memcache-top.rb
+++ Formula/memcache-top.rb
@@ -4,8 +4,6 @@ class MemcacheTop < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/memcache-top/memcache-top-v0.6"
   sha256 "d5f896a9e46a92988b782e340416312cc480261ce8a5818db45ccd0da8a0f22a"
 
-  bottle :unneeded
-
   def install
     bin.install "memcache-top-v#{version}" => "memcache-top"
   end
