4b00ff3aa72f5466985dbb117a71811efa80fb87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:10:37 +0000

    storm: add 2.2.0 bottle.

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -6,6 +6,10 @@ class Storm < Formula
   sha256 "f621163f349a8e85130bc3d2fbb34e3b08f9c039ccac5474f3724e47a3a38675"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0b7ee5cb1ed1a1454bd11d0c62f0953214d689fb904b7abc14bc92ac9462f096"
+  end
+
   depends_on "openjdk"
 
   conflicts_with "stormssh", because: "both install 'storm' binary"
