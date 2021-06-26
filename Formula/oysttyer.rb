eaab79b60833b4c6ce58ddffe2218d7630ae90c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 12:10:25 +0000

    oysttyer: add 2.10.0 bottle.

--- Formula/oysttyer.rb
+++ Formula/oysttyer.rb
@@ -5,6 +5,10 @@ class Oysttyer < Formula
   sha256 "3c0ce1c7b112f2db496cc75a6e76c67f1cad956f9e7812819c6ae7a979b2baea"
   head "https://github.com/oysttyer/oysttyer.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a1700139674a1c4bdfb17dc7b5c7d513f835ac100fd419ac07672cdd6f78ec1b"
+  end
+
   def install
     bin.install "oysttyer.pl" => "oysttyer"
   end
