b61ae97ff94efb19f5d724ea532af3a9ba457c52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:09:29 +0000

    modman: add 1.14 bottle.

--- Formula/modman.rb
+++ Formula/modman.rb
@@ -5,6 +5,10 @@ class Modman < Formula
   sha256 "58ac5b27b11def9ba162881c3687f2085c06a6ed4cfb496bafdc64ce1a2eaac6"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c533a04301f9104213accc8724198bdb249e186fc36866697c94d4ce0f4d56c2"
+  end
+
   def install
     bin.install "modman"
     bash_completion.install "bash_completion" => "modman.bash"
