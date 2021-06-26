94f3d708fae03e9a737e5547388268217950f9e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 12:10:24 +0000

    nave: add 3.2.2 bottle.

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -6,6 +6,13 @@ class Nave < Formula
   license "ISC"
   head "https://github.com/isaacs/nave.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9e7f74d74b71447c296c6b5873047496a1988e4c7c20fef6af032f7621a15528"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6358f668346849c8058739a2bb84757647e2b3612be49446ee849cdd76e102eb"
+    sha256 cellar: :any_skip_relocation, catalina:      "6358f668346849c8058739a2bb84757647e2b3612be49446ee849cdd76e102eb"
+    sha256 cellar: :any_skip_relocation, mojave:        "6358f668346849c8058739a2bb84757647e2b3612be49446ee849cdd76e102eb"
+  end
+
   def install
     bin.install "nave.sh" => "nave"
   end
