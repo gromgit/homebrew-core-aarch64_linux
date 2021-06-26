b5c7eb0aabf0bd4df3ab7c53d2668da8a82081e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:44 +0000

    burl: add 1.0.1 bottle.

--- Formula/burl.rb
+++ Formula/burl.rb
@@ -4,6 +4,10 @@ class Burl < Formula
   url "https://github.com/tj/burl/archive/1.0.1.tar.gz"
   sha256 "634949b7859ddf7c75a89123608998f8dac8ced8c601fa2c2717569caeaa54e5"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "eb8a86db7325cafed76e441f755837b1771117724edf90ab523cbce98656cf85"
+  end
+
   def install
     bin.install "bin/burl"
   end
