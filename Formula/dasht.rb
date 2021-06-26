2badb8830c94e9a4690a82e67bc61f8f33a08652
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:34 +0000

    dasht: add 2.4.0 bottle.

--- Formula/dasht.rb
+++ Formula/dasht.rb
@@ -5,6 +5,10 @@ class Dasht < Formula
   sha256 "5ea43b0f7461e124d46b991892dedc8dcf506ccd5e9dc94324f7bdf6e580ff73"
   license "ISC"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "94ad1a43dc67fa38cef44f163f2577fbc8821cef94f5bb8cf00ca999d72e0364"
+  end
+
   depends_on "socat"
   depends_on "sqlite"
   depends_on "w3m"
