15c046a30544de8ea76108cb509456bd9260c181
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:10:38 +0000

    sendemail: add 1.56 bottle.

--- Formula/sendemail.rb
+++ Formula/sendemail.rb
@@ -6,6 +6,13 @@ class Sendemail < Formula
   sha256 "6dd7ef60338e3a26a5e5246f45aa001054e8fc984e48202e4b0698e571451ac0"
   license "GPL-2.0+"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "de9826d68c6ca9666b3ec31462cee9d6ad435dfae72273ffd36856aa5dc95339"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3d2d838108374473524945ae1cada342aad8e25f77216d15dbf8aae7ff0ded76"
+    sha256 cellar: :any_skip_relocation, catalina:      "3d2d838108374473524945ae1cada342aad8e25f77216d15dbf8aae7ff0ded76"
+    sha256 cellar: :any_skip_relocation, mojave:        "3d2d838108374473524945ae1cada342aad8e25f77216d15dbf8aae7ff0ded76"
+  end
+
   # Reported upstream: https://web.archive.org/web/20191013154932/caspian.dotconf.net/menu/Software/SendEmail/#comment-1119965648
   patch do
     url "https://raw.githubusercontent.com/mogaal/sendemail/e785a6d284884688322c9b39c0f64e20a43ea825/debian/patches/fix_ssl_version.patch"
