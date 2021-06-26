0d3bd49e4dfbfcb027ecec59f1ff30fa3af203a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:25 +0000

    cpanminus: add 1.9019 bottle.

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -12,6 +12,13 @@ class Cpanminus < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "78c0fc8d2fcb14cb94e62d972f06ae6a1762846471eb5d0669909121c24fa08d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6a9b5bde63d8c5860788c67470c9dffcfe12036d38e331ad4c5028455ad45a79"
+    sha256 cellar: :any_skip_relocation, catalina:      "6a9b5bde63d8c5860788c67470c9dffcfe12036d38e331ad4c5028455ad45a79"
+    sha256 cellar: :any_skip_relocation, mojave:        "6a9b5bde63d8c5860788c67470c9dffcfe12036d38e331ad4c5028455ad45a79"
+  end
+
   def install
     cd "App-cpanminus" do
       bin.install "cpanm"
