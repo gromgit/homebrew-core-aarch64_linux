ec83fdfe9f32248bf2721f1336e78f3a51e5dcc2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-26 12:40:45 +0000

    makeicns: update 1.4.10a bottle.

--- Formula/makeicns.rb
+++ Formula/makeicns.rb
@@ -6,12 +6,13 @@ class Makeicns < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, catalina:    "c2a5afff3eee709316951ad70c8244fe5c628ae98fdb2e15ea607c7638733d63"
-    sha256 cellar: :any_skip_relocation, mojave:      "16d2135a49e22ffe920567c7ac382d5f706ef7ce5de377750553a0e59414819a"
-    sha256 cellar: :any_skip_relocation, high_sierra: "c40907f2d30603bdfe8402e90cbb35209b46cee1e7967d0ab06c21d5d7935eb8"
-    sha256 cellar: :any_skip_relocation, sierra:      "3a673790c42724f75b905713e269f8bfa3e54bb64fde48130164c68b5656c871"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "96f91bccf728f040931c2816156a7c5de739ae91e63191795cd108d0a46370ac"
-    sha256 cellar: :any_skip_relocation, yosemite:    "40c3d4befe2d4625d7013ea40f307b4f5b26e122a6dad51706a25bb22734f075"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ad6aa042b2648b49a2be3b77324845be6121f10efd13c4be4c543024f9a8cecc"
+    sha256 cellar: :any_skip_relocation, catalina:      "c2a5afff3eee709316951ad70c8244fe5c628ae98fdb2e15ea607c7638733d63"
+    sha256 cellar: :any_skip_relocation, mojave:        "16d2135a49e22ffe920567c7ac382d5f706ef7ce5de377750553a0e59414819a"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "c40907f2d30603bdfe8402e90cbb35209b46cee1e7967d0ab06c21d5d7935eb8"
+    sha256 cellar: :any_skip_relocation, sierra:        "3a673790c42724f75b905713e269f8bfa3e54bb64fde48130164c68b5656c871"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "96f91bccf728f040931c2816156a7c5de739ae91e63191795cd108d0a46370ac"
+    sha256 cellar: :any_skip_relocation, yosemite:      "40c3d4befe2d4625d7013ea40f307b4f5b26e122a6dad51706a25bb22734f075"
   end
 
   patch :p0 do
