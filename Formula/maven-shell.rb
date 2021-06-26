6e5d8496ddf35a11fb8c113a8257ebc399ba8124
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:09:30 +0000

    maven-shell: add 1.1.0 bottle.

--- Formula/maven-shell.rb
+++ Formula/maven-shell.rb
@@ -5,6 +5,10 @@ class MavenShell < Formula
   sha256 "584008d726bf6f90271f4ccd03b549773cbbe62ba7e92bf131e67df3ac5a41ac"
   license "EPL-1.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "07937b1a530b436e89a72f73244f782228dc02ee2b31e761e052e5367704a39e"
+  end
+
   def install
     # Remove windows files.
     rm_f Dir["bin/*.bat"]
