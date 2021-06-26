b0759396847f1fed7aafb9260073dc4f11821c83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:43 +0000

    bzr-upload: add 1.1.0 bottle.

--- Formula/bzr-upload.rb
+++ Formula/bzr-upload.rb
@@ -4,6 +4,10 @@ class BzrUpload < Formula
   url "https://launchpad.net/bzr-upload/1.1/1.1.0/+download/bzr-upload-1.1.0.tar.gz"
   sha256 "a48fc56d83114d9ab946cc358a5b33cb05e134787be135eb0a499317d6dec7fc"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "1ed703bbd059cad6589230219b1425f5bfc4d67c15819da557e6b72813b0b476"
+  end
+
   depends_on "bazaar"
 
   def install
