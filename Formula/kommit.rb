ae67e8a3265c901866fb4cb271ffca45ba7ef41f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:09:36 +0000

    kommit: add 1.1.0 bottle.

--- Formula/kommit.rb
+++ Formula/kommit.rb
@@ -5,6 +5,10 @@ class Kommit < Formula
   sha256 "c51e87c9719574feb9841fdcbd6d1a43b73a45afeca25e1312d2699fdf730161"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "1fdd89c3c3c5e6c30817ddfa6e41ae587f80d05afe22b729fa4de0541e28e5a8"
+  end
+
   def install
     bin.install "bin/git-kommit"
   end
