bbf2b7467533f697916039756735e5b0f9746d23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:49 +0000

    blackbox: add 2.0.0 bottle.

--- Formula/blackbox.rb
+++ Formula/blackbox.rb
@@ -5,6 +5,10 @@ class Blackbox < Formula
   sha256 "0a8fee39dc46436472528ea3a5743c42ebefc068519545fe6fca57041f42deae"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "6910c4f3a0b2b04288b9ca0eedc866e6b83d14ac5276d4eb46651a0bb0c14333"
+  end
+
   depends_on "gnupg"
 
   def install
