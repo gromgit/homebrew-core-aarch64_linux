d83ba6726a97ca881700aae16600d7216b9c6f22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:10:34 +0000

    spring-completion: add 0.0.1 bottle.

--- Formula/spring-completion.rb
+++ Formula/spring-completion.rb
@@ -6,6 +6,13 @@ class SpringCompletion < Formula
   license "Unlicense"
   head "https://github.com/jacaetevha/spring_bash_completion.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c188dbaccd58b79b5596c7d0c34cd3a1a997cea7f6131393b11205a77964c2a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "1110b2611a3d8dc08fe1731e46692b9d77234e60aba14602f0ad1f9380933eeb"
+    sha256 cellar: :any_skip_relocation, catalina:      "1110b2611a3d8dc08fe1731e46692b9d77234e60aba14602f0ad1f9380933eeb"
+    sha256 cellar: :any_skip_relocation, mojave:        "1110b2611a3d8dc08fe1731e46692b9d77234e60aba14602f0ad1f9380933eeb"
+  end
+
   def install
     bash_completion.install "spring.bash" => "spring"
   end
