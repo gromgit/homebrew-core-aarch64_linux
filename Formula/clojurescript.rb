ae8805f850623d4a80edd64e72eba47abab9cb0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:27 +0000

    clojurescript: add 1.10.866 bottle.

--- Formula/clojurescript.rb
+++ Formula/clojurescript.rb
@@ -12,6 +12,10 @@ class Clojurescript < Formula
     regex(%r{href=.*?/tag/r?(\d+(?:\.\d+)+)["' >]}i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "96f5fb870831289f84b565bc7fd72a75e59a0b234986c0077fac9b3665dc7235"
+  end
+
   depends_on "openjdk"
 
   def install
