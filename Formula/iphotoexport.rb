a090c0b09bd84b120f0c43cfd8c1fdcb4d9337c8
Michael Cho <cho-m@tuta.io>
2021-06-26 14:53:15 -0700

    iphotoexport: remove `bottle :unneeded` and deprecate
    Closes #80108.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/iphotoexport.rb
+++ Formula/iphotoexport.rb
@@ -4,17 +4,18 @@ class Iphotoexport < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/iphotoexport/iphotoexport-1.6.4.zip"
   sha256 "85644b5be1541580a35f1ea6144d832267f1284ac3ca23fe9bcd9eda5aaea5d3"
 
-  bottle :unneeded
+  # Replaced by Phoshare app, which was discontinued on 2012-10-10.
+  # The current version (1.6.4) is from 2010-05-15.
+  deprecate! date: "2021-06-27", because: :deprecated_upstream
 
   depends_on "exiftool"
 
   def install
-    unzip_dir = "#{name}-#{version}"
     # Change hardcoded exiftool path
-    inreplace "#{unzip_dir}/tilutil/exiftool.py", "/usr/bin/exiftool", "exiftool"
+    inreplace "tilutil/exiftool.py", "/usr/bin/exiftool", "exiftool"
 
-    prefix.install Dir["#{unzip_dir}/*"]
-    bin.install_symlink prefix+"iphotoexport.py" => "iphotoexport"
+    prefix.install Dir["*"]
+    bin.install_symlink prefix/"iphotoexport.py" => "iphotoexport"
   end
 
   test do
