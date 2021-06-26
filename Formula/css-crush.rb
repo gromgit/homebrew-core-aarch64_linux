fb4e029449302f490ea5667979fe1c1306240848
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:31 +0000

    css-crush: add 3.0.1 bottle.

--- Formula/css-crush.rb
+++ Formula/css-crush.rb
@@ -6,6 +6,10 @@ class CssCrush < Formula
   license "MIT"
   head "https://github.com/peteboere/css-crush.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "06eec82e0757d06c7780b1695c31946d52e02e17af3566434023ac8262ddc28b"
+  end
+
   def install
     libexec.install Dir["*"]
     (bin+"csscrush").write <<~EOS
