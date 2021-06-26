982552e0297714f9b495c880a8d6b6dcaef21c8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 21:10:49 +0000

    aws-keychain: add 3.0.0 bottle.

--- Formula/aws-keychain.rb
+++ Formula/aws-keychain.rb
@@ -5,6 +5,10 @@ class AwsKeychain < Formula
   sha256 "3c9882d3b516b629303ca9a045fc50f6eb75fda25cd2452f10c47eda205e051f"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ba317cf159adf639b08c5153cc69ded7671e948c7041a3fa441a6f34db6ec0ba"
+  end
+
   depends_on :macos
 
   def install
