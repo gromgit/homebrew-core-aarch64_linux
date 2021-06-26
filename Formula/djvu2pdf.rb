d83418e46d12de3daa3bb368aa8f2965d065128e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:35 +0000

    djvu2pdf: add 0.9.2 bottle.

--- Formula/djvu2pdf.rb
+++ Formula/djvu2pdf.rb
@@ -9,6 +9,10 @@ class Djvu2pdf < Formula
     regex(/href=.*?djvu2pdf[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "712580b5fb3dc550722146cdce9ce17c9928565a29433bd0697cb231691e566f"
+  end
+
   depends_on "djvulibre"
   depends_on "ghostscript"
 
