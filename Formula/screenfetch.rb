f7e2fdc6b038bfefbd89c07a6ea783e012a0660f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:10:34 +0000

    screenfetch: add 3.9.1 bottle.

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -6,6 +6,13 @@ class Screenfetch < Formula
   license "GPL-3.0"
   head "https://github.com/KittyKatt/screenFetch.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6f7e61ea4717eef72e68b006bcef5d6ff1aab08f7ba25f0a5c6b8e014ffb530b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ab904d997e7f65041ea053d21da1b9acc385913f7d4051c7338bea85353a390b"
+    sha256 cellar: :any_skip_relocation, catalina:      "ab904d997e7f65041ea053d21da1b9acc385913f7d4051c7338bea85353a390b"
+    sha256 cellar: :any_skip_relocation, mojave:        "ab904d997e7f65041ea053d21da1b9acc385913f7d4051c7338bea85353a390b"
+  end
+
   def install
     bin.install "screenfetch-dev" => "screenfetch"
     man1.install "screenfetch.1"
