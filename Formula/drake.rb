5d45819549a25e02459d2a42fc1ab2bd40a9eee9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:27 +0000

    drake: add 1.0.3 bottle.

--- Formula/drake.rb
+++ Formula/drake.rb
@@ -6,6 +6,12 @@ class Drake < Formula
   license "EPL-1.0"
   head "https://github.com/Factual/drake.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, big_sur:  "6496f23e1c0b226eefaff9b6866943f55c6ae07539b0f9f640a5dcb1ee7bfb3f"
+    sha256 cellar: :any_skip_relocation, catalina: "0bba821e663244edaa7e34dbd5fa1d5a2f4bbb7aff22b641babca781543a40a6"
+    sha256 cellar: :any_skip_relocation, mojave:   "631e9bb8583c3d54d4aba7fc0d60e59cd5cf2c815679019be8f0f48872fe7363"
+  end
+
   depends_on arch: :x86_64 # openjdk@8 is not supported on ARM
   depends_on "openjdk@8"
 
