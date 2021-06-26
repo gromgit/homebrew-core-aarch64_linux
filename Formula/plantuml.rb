7a06ca0fe33155c67337789266451a9c1bc0bc6d
Rui Chen <rui@chenrui.dev>
2021-06-26 11:38:04 -0400

    plantuml 1.2021.8
    * plantuml 1.2021.8
    * plantuml: remove `bottle :unneeded`
    
    Closes #80093.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2021.7/plantuml.1.2021.7.jar"
-  sha256 "da1408c14a64c4b1c61be931f007a448a275a8ef229cbf319e4a347652c2d4a8"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2021.8/plantuml.1.2021.8.jar"
+  sha256 "12614d3b7792e5c718c6fb4c399ae1476fce7ae705e0e5b6ac08b84cfbde7111"
   license "GPL-3.0-or-later"
   version_scheme 1
 
@@ -10,8 +10,6 @@ class Plantuml < Formula
     url :stable
     regex(%r{url=.*?/plantuml[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
-
-  bottle :unneeded
 
   depends_on "graphviz"
   depends_on "openjdk"
