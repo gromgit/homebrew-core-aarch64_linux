4fbe9dd40a3fc7cd9433f7a29e3a18a2cb2e2cf9
Nanda H Krishna <me@nandahkrishna.com>
2021-06-26 05:25:21 +0000

    tomcat@7: add 7.0.109 bottle.

--- Formula/tomcat@7.rb
+++ Formula/tomcat@7.rb
@@ -10,6 +10,10 @@ class TomcatAT7 < Formula
     url :stable
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "d48be488e8e6e6d930b0eb897b6b52d137e2485437d97064c2381dfa5ec76e91"
+  end
+
   keg_only :versioned_formula
 
   depends_on "openjdk"
