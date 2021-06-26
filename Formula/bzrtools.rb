de3064ee9ddc0d35e44fc771d9d5c49bee2477d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:40 +0000

    bzrtools: add 2.6.0 bottle.

--- Formula/bzrtools.rb
+++ Formula/bzrtools.rb
@@ -15,6 +15,10 @@ class Bzrtools < Formula
     regex(%r{<div class="version">\s*Latest version is (.+)\s*</div>}i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "27337af0179d9a1a9897def816bfc6c9e85df08186bdb72918bc6327c2b7c2db"
+  end
+
   depends_on "bazaar"
 
   def install
