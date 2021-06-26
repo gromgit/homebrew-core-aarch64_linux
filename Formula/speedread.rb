323024b64caf5d268b4cef87aa7a04fda49d1300
Michael Cho <cho-m@tuta.io>
2021-06-26 00:19:16 -0700

    speedread: remove `bottle :unneeded`
    Closes #80079.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/speedread.rb
+++ Formula/speedread.rb
@@ -1,12 +1,20 @@
 class Speedread < Formula
   desc "Simple terminal-based rapid serial visual presentation (RSVP) reader"
   homepage "https://github.com/pasky/speedread"
-  url "https://github.com/pasky/speedread/archive/v1.0.tar.gz"
-  sha256 "a65f5bec427e66893663bcfc49a22e43169dd35976302eaed467eec2a5aafc1b"
   license "MIT"
   head "https://github.com/pasky/speedread.git"
 
-  bottle :unneeded
+  stable do
+    url "https://github.com/pasky/speedread/archive/v1.0.tar.gz"
+    sha256 "a65f5bec427e66893663bcfc49a22e43169dd35976302eaed467eec2a5aafc1b"
+
+    # Fix error with macOS 11 Perl: "The encoding pragma is no longer supported"
+    # Remove this in next release
+    patch do
+      url "https://github.com/pasky/speedread/commit/24c3946d14f7f310a7012be1f9d3cbccf0b16765.patch?full_index=1"
+      sha256 "df8d576ad920f32c6dc3e934f4883f7b2d43fb7d2453bb007620513100df076a"
+    end
+  end
 
   def install
     bin.install "speedread"
