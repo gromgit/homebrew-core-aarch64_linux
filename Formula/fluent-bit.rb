048428ea2a9f4b0d0f7d703a4cfcdc8ebd461711
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 14:06:02 +0000

    fluent-bit: update 1.7.9 bottle.

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -12,10 +12,10 @@ class FluentBit < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4ee1823841ea4039f3850502c17b2b1cc0f1aa8ee7e49b0ec81250985dd1664b"
-    sha256 cellar: :any, big_sur:       "c9fa51a3d352cfac250c8deefbd0917ac7d4f295a0490d86759e4138a85a5a40"
-    sha256 cellar: :any, catalina:      "924f6a517016a879fdd4ce77d9ac80fea0d3a0589f1e9f39e2aeb6b68ad22a57"
-    sha256 cellar: :any, mojave:        "3a13904044dd24b0fb49fd834597d88ff250ce795be7c337495d2c791271064a"
+    sha256 cellar: :any, arm64_big_sur: "4c6731bc92a8e6fc7b50763fc19428d9a04b3f92e4dc1d7ce6cf5a394b331eb0"
+    sha256 cellar: :any, big_sur:       "50671feeaae23a717b962663c1c42cd7a4efe9b0f5ce2b4c6e9442423606de8f"
+    sha256 cellar: :any, catalina:      "c583a91963b78fe48e2b64aa43094e7a16e49437b09de197697ca964f569f61f"
+    sha256 cellar: :any, mojave:        "7766c659470a00bef9844f3d3518af9bbad8f916be503ccf59ad201624aa9a09"
   end
 
   depends_on "bison" => :build
