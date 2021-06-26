abcf5ba9c11e96c9d943dd5c98d19728833f2121
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:09:33 +0000

    minimesos: add 0.13.0_1 bottle.

--- Formula/minimesos.rb
+++ Formula/minimesos.rb
@@ -6,6 +6,13 @@ class Minimesos < Formula
   license "Apache-2.0"
   revision 1
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ed75cc748a76989016cc7cc69e07baf1b82eb9b6c2b5a16482fa289b733a8dc1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "630f067087841be66ac8118b072d4619c4aa1c3e28f07f6ac48c2b94483a164a"
+    sha256 cellar: :any_skip_relocation, catalina:      "630f067087841be66ac8118b072d4619c4aa1c3e28f07f6ac48c2b94483a164a"
+    sha256 cellar: :any_skip_relocation, mojave:        "630f067087841be66ac8118b072d4619c4aa1c3e28f07f6ac48c2b94483a164a"
+  end
+
   deprecate! date: "2018-06-22", because: :repo_archived
 
   def install
