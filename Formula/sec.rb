2a4966196c8da7488154e50cd3916672a36ebeaa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:10:30 +0000

    sec: add 2.9.0 bottle.

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -5,6 +5,13 @@ class Sec < Formula
   sha256 "741154d25db69706e2200e119b5cd32d65ae0b803d9c0faefcccfbcfe1c97503"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7ae34d4172afb4ead92b0ea30f0eef8b11262504cc74b35d92cc2de64841d4e6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0ca90f406c4776e7f7b4b20db45f8d960458b8e5e66789846e58359b4ca2c0fb"
+    sha256 cellar: :any_skip_relocation, catalina:      "0ca90f406c4776e7f7b4b20db45f8d960458b8e5e66789846e58359b4ca2c0fb"
+    sha256 cellar: :any_skip_relocation, mojave:        "0ca90f406c4776e7f7b4b20db45f8d960458b8e5e66789846e58359b4ca2c0fb"
+  end
+
   def install
     bin.install "sec"
     man1.install "sec.man" => "sec.1"
