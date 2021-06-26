5a997338ee5a6244f7aaffca5a08e4867f5061f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:09:36 +0000

    mysql-search-replace: add 4.1.2 bottle.

--- Formula/mysql-search-replace.rb
+++ Formula/mysql-search-replace.rb
@@ -5,6 +5,10 @@ class MysqlSearchReplace < Formula
   sha256 "3da4b2af67bb820534c0e8d8dc6b87f4b38be6fe2410df90177a39dc24ae4593"
   license "GPL-3.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "08b03d69eae7a4b2f89ead89f79ac09cd0bd093da29e0255329c308fd559ff43"
+  end
+
   def install
     libexec.install "srdb.class.php"
     libexec.install "srdb.cli.php" => "srdb"
