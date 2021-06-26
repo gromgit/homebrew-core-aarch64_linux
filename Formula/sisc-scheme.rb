10d1d06bda055c1c3234bfd4556d610cc3a1bee2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:10:29 +0000

    sisc-scheme: add 1.16.6 bottle.

--- Formula/sisc-scheme.rb
+++ Formula/sisc-scheme.rb
@@ -4,6 +4,13 @@ class SiscScheme < Formula
   url "https://downloads.sourceforge.net/project/sisc/SISC%20Lite/1.16.6/sisc-lite-1.16.6.tar.gz"
   sha256 "7a2f1ee46915ef885282f6df65f481b734db12cfd97c22d17b6c00df3117eea8"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f65d81b8af3efb3351510362fdde92e8b9fc5a32eaba361a438abed4fb265991"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
+    sha256 cellar: :any_skip_relocation, catalina:      "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
+    sha256 cellar: :any_skip_relocation, mojave:        "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
+  end
+
   def install
     libexec.install Dir["*"]
     (bin/"sisc").write <<~EOS
