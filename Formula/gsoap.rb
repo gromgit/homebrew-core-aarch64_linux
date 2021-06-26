5fac652de369ea6f9b96b6d1e5886784910ed9ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 06:07:35 +0000

    gsoap: update 2.8.115 bottle.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -11,10 +11,10 @@ class Gsoap < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "1311ed76cfd1b4fe0a477f83e15c6a4acf294b7b8d7a11c0c61e5dffcb3c6a38"
-    sha256 big_sur:       "4f0d9e8d8cdc0621ebd2794ca9919308c83814241ad0dc1debe8eb8d44577024"
-    sha256 catalina:      "9a9488933737f3f9018f5d54cc3cb292dff93e801734ad8651e93be60e1b0b33"
-    sha256 mojave:        "e91456ed8510a9b53aa6b015f80b666d1173e85b3c389e745843f24beb24a754"
+    sha256 arm64_big_sur: "b9023155e61e18f169163e0d17e4ea130f95dc6ade6bb89e9a56e8458d6b8e54"
+    sha256 big_sur:       "bfa8373d47f03c3461f7d78f9098540ee6417ae65105c01d0a1d1488a60f1c92"
+    sha256 catalina:      "bb792ad3a6cafe63c81bcdda836be6c5f7968fc2d797852ea7511b71ab0bb478"
+    sha256 mojave:        "2d38a218fb7c0cf4f7aa298af3d2cada1a47b84d5e8dc22cd7ed7bb5195ac250"
   end
 
   depends_on "autoconf" => :build
