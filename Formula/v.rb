6eef00a442318f0dd37bb70e9bac384f59708897
Nanda H Krishna <me@nandahkrishna.com>
2021-06-26 05:25:17 +0000

    v: add 1.1 bottle.

--- Formula/v.rb
+++ Formula/v.rb
@@ -5,6 +5,13 @@ class V < Formula
   sha256 "6483ef1248dcbc6f360b0cdeb9f9c11879815bd18b0c4f053a18ddd56a69b81f"
   head "https://github.com/rupa/v.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3fc81e5ce826641b151e30b303496e791296a2864ded5756d6bf47408aadec12"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bbee3c584bca388ac94dcd3dc701c395fc4f44418dcd0f213033cec8f4f41b17"
+    sha256 cellar: :any_skip_relocation, catalina:      "bbee3c584bca388ac94dcd3dc701c395fc4f44418dcd0f213033cec8f4f41b17"
+    sha256 cellar: :any_skip_relocation, mojave:        "bbee3c584bca388ac94dcd3dc701c395fc4f44418dcd0f213033cec8f4f41b17"
+  end
+
   def install
     bin.install "v"
     man1.install "v.1"
