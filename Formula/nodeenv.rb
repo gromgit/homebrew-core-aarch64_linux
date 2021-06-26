52222627af0ee3d16e059defba9d224ec8e511ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 12:10:26 +0000

    nodeenv: add 1.6.0 bottle.

--- Formula/nodeenv.rb
+++ Formula/nodeenv.rb
@@ -5,6 +5,10 @@ class Nodeenv < Formula
   sha256 "87b04b0832d38bd825e004e7c8ed82d75524efaa6b8a72e3a55088d93e4e17f7"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "1e5b23648954dc4570499e605ed8ea5d3eb6124b1cc776b98ee9b4601872aec7"
+  end
+
   def install
     bin.install "nodeenv.py" => "nodeenv"
   end
