e5cf52663f0e3a550200178110c3ceecdbafd2a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:07 +0000

    webkit2png: add 0.7 bottle.

--- Formula/webkit2png.rb
+++ Formula/webkit2png.rb
@@ -4,6 +4,10 @@ class Webkit2png < Formula
   url "https://github.com/paulhammond/webkit2png/archive/v0.7.tar.gz"
   sha256 "9b810edb6f54cc23ba86b0212f203e6c3bbafc3cbdb62b9c33887548c91015bf"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ad209d841f88f9b5d3a969e2493d853237c89234bd09dfc3d1aa2106832d2d7d"
+  end
+
   def install
     bin.install "webkit2png"
   end
