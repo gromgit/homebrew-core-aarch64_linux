b332549676af2e83ec1df602e0377a392b331415
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 07:07:21 +0000

    sbt: update 1.5.4 bottle.

--- Formula/sbt.rb
+++ Formula/sbt.rb
@@ -12,7 +12,7 @@ class Sbt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "50364b69d068eb212ace88d85062007ebfbb8abba6b5af0d73023bde17d634ff"
+    sha256 cellar: :any_skip_relocation, all: "601371df40c3a14474d68d4547475e4a878111e9f42a9214ad0741a829a19618"
   end
 
   depends_on arch: :x86_64
