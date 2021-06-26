6555a273ead9495671332572da502f3ef1f2e9ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:07 +0000

    xcv: add 1.0.1 bottle.

--- Formula/xcv.rb
+++ Formula/xcv.rb
@@ -5,6 +5,10 @@ class Xcv < Formula
   sha256 "f2898f78bb05f4334073adb8cdb36de0f91869636a7770c8e955cee8758c0644"
   head "https://github.com/busterc/xcv.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a715d85b4b84704d9f4f01949e017e0ed875e812b2c200ce7ba75102f8fb9769"
+  end
+
   def install
     bin.install "xcv"
   end
