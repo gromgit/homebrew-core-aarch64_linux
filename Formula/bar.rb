eb1c043af0ab156aa8595d48c602397c9e1332f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:48 +0000

    bar: add 1.4 bottle.

--- Formula/bar.rb
+++ Formula/bar.rb
@@ -4,6 +4,10 @@ class Bar < Formula
   url "http://www.theiling.de/downloads/bar-1.4-src.tar.bz2"
   sha256 "8034c405b6aa0d474c75ef9356cde1672b8b81834edc7bd94fc91e8ae097033e"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9f0dd0a974b69b3420bc0fd4620506ff0308f1b94409e571daee22087b0ebb8a"
+  end
+
   def install
     bin.install "bar"
   end
