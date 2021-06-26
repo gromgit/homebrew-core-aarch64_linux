5f6f9e86ab40188cbb21f7f0bfbb134c54a08da9
Michael Cho <cho-m@tuta.io>
2021-06-25 23:24:03 -0700

    couchpotatoserver: remove `bottle :unneeded` and use service
    Closes #80078.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/couchpotatoserver.rb
+++ Formula/couchpotatoserver.rb
@@ -6,41 +6,14 @@ class Couchpotatoserver < Formula
   license "GPL-3.0"
   head "https://github.com/CouchPotato/CouchPotatoServer.git"
 
-  bottle :unneeded
-
   def install
     prefix.install_metafiles
     libexec.install Dir["*"]
     (bin+"couchpotatoserver").write(startup_script)
   end
 
-  def caveats
-    "CouchPotatoServer defaults to port 5050."
-  end
-
-  plist_options manual: "couchpotatoserver"
-
-  def plist
-    <<~EOS
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>Program</key>
-          <string>#{opt_bin}/couchpotatoserver</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>--quiet</string>
-            <string>--daemon</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>UserName</key>
-          <string>#{`whoami`.chomp}</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"couchpotatoserver", "--quiet"]
   end
 
   def startup_script
