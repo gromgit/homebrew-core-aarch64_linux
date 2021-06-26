559c70dad3b33c3ad09658210e0579bb73f78a09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 11:09:28 +0000

    doubledown: add 0.0.2 bottle.

--- Formula/doubledown.rb
+++ Formula/doubledown.rb
@@ -6,6 +6,10 @@ class Doubledown < Formula
   license "BSD-2-Clause"
   head "https://github.com/devstructure/doubledown.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "78f3d65bdfb3d5184fc3a10c8b161e9e41a4d14715c284d2a179ebf5fbe52209"
+  end
+
   def install
     bin.install Dir["bin/*"]
     man1.install Dir["man/man1/*.1"]
