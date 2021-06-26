ef0df526bdea026cd5923fba8ece4483431b8700
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:39 +0000

    briss: add 0.9 bottle.

--- Formula/briss.rb
+++ Formula/briss.rb
@@ -4,6 +4,10 @@ class Briss < Formula
   url "https://downloads.sourceforge.net/project/briss/release%200.9/briss-0.9.tar.gz"
   sha256 "45dd668a9ceb9cd59529a9fefe422a002ee1554a61be07e6fc8b3baf33d733d9"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "95064b9aa616b6f63cd7f5746cec3865a0441c070aed78716868cbd5aa500ab9"
+  end
+
   depends_on "openjdk"
 
   def install
