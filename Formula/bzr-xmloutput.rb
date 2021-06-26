fb73012af2c1c2e15d234a1aba158cfddbe455d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:46 +0000

    bzr-xmloutput: add 0.8.8 bottle.

--- Formula/bzr-xmloutput.rb
+++ Formula/bzr-xmloutput.rb
@@ -4,6 +4,10 @@ class BzrXmloutput < Formula
   url "https://launchpad.net/bzr-xmloutput/trunk/0.8.8/+download/bzr-xmloutput-0.8.8.tar.gz"
   sha256 "73b9b2f6ce4d9910031df7fd153d56d14f833c20a106f099bee5a33463f73b36"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "cb0f2bde2a3d23624cf9d0215149d0f72c1c52f9be66fcfea51e833255a42245"
+  end
+
   depends_on "bazaar"
 
   def install
