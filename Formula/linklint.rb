f9550ad5b79f71750969e0ab60f3425b8f676fc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:09:27 +0000

    linklint: add 2.3.5 bottle.

--- Formula/linklint.rb
+++ Formula/linklint.rb
@@ -4,6 +4,10 @@ class Linklint < Formula
   url "http://linklint.org/download/linklint-2.3.5.tar.gz"
   sha256 "ecaee456a3c2d6a3bd18a580d6b09b6b7b825df3e59f900270fe3f84ec3ac9c7"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ef392eb8173eedd8caad7b9ac1d3aa0354fe9aeec2c6fe902b02bf1e9966518a"
+  end
+
   def install
     mv "READ_ME.txt", "README"
 
