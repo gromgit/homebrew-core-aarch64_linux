3ac0f31cd6e65f99805aedb459ae64e616da98cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-26 03:08:41 +0000

    batik: add 1.14 bottle.

--- Formula/batik.rb
+++ Formula/batik.rb
@@ -6,6 +6,10 @@ class Batik < Formula
   sha256 "0f4eaa81be70752e406a19f39c39c018848f09963f08fa8974640547052c4618"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "cd0b605d3bafd020006126b0dd7d65396bd73f28b5effa9b3b9bf07e75cbe039"
+  end
+
   depends_on "openjdk"
 
   def install
