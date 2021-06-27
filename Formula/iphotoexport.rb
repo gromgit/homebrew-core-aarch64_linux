67d7349383fd9056644ff248a640fe70b8402d95
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 23:06:24 +0000

    iphotoexport: add 1.6.4 bottle.

--- Formula/iphotoexport.rb
+++ Formula/iphotoexport.rb
@@ -4,6 +4,10 @@ class Iphotoexport < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/iphotoexport/iphotoexport-1.6.4.zip"
   sha256 "85644b5be1541580a35f1ea6144d832267f1284ac3ca23fe9bcd9eda5aaea5d3"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9b61d6e01dd01bc1ab24a32284ce269495beb77a6f516c8b30e5c86003b66333"
+  end
+
   # Replaced by Phoshare app, which was discontinued on 2012-10-10.
   # The current version (1.6.4) is from 2010-05-15.
   deprecate! date: "2021-06-27", because: :deprecated_upstream
