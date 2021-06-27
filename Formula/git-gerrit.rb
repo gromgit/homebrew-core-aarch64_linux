4d85c6085fd1df150a62676dca0a362f96899701
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 08:06:33 +0000

    git-gerrit: add 0.3.0 bottle.

--- Formula/git-gerrit.rb
+++ Formula/git-gerrit.rb
@@ -6,6 +6,13 @@ class GitGerrit < Formula
   license "BSD-3-Clause"
   head "https://github.com/fbzhong/git-gerrit.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
+    sha256 cellar: :any_skip_relocation, big_sur:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+    sha256 cellar: :any_skip_relocation, catalina:      "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+    sha256 cellar: :any_skip_relocation, mojave:        "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+  end
+
   def install
     prefix.install "bin"
     bash_completion.install "completion/git-gerrit-completion.bash"
