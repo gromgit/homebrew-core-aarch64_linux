3082a087b719ae47e0fab775e6a9202e886f7e95
Dawid Dziurla <dawidd0811@gmail.com>
2021-06-27 17:15:02 +0000

    jpeg: update 9d bottle.

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -12,11 +12,12 @@ class Jpeg < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e511143cd72a76572dabe52cef0034996b2ed24334f1b3466ba339746230e37b"
-    sha256 cellar: :any, big_sur:       "c565929a4901365a3408b57275802f943625c1e29e1b48a186edd2e97d8c0bdb"
-    sha256 cellar: :any, catalina:      "8f7b82a952fb3937889c7f22da1403e5338cd320495917eb26b0c5b2e614791c"
-    sha256 cellar: :any, mojave:        "b931e7725c83275c56f962b51b83c10f31a01ac8d823c6722edaf16d893970b1"
-    sha256 cellar: :any, high_sierra:   "64286932634fbe1e0d07eacda334d2f4967b20bce0737424df56ec5eaa34ccca"
+    sha256 cellar: :any,                 arm64_big_sur: "e511143cd72a76572dabe52cef0034996b2ed24334f1b3466ba339746230e37b"
+    sha256 cellar: :any,                 big_sur:       "c565929a4901365a3408b57275802f943625c1e29e1b48a186edd2e97d8c0bdb"
+    sha256 cellar: :any,                 catalina:      "8f7b82a952fb3937889c7f22da1403e5338cd320495917eb26b0c5b2e614791c"
+    sha256 cellar: :any,                 mojave:        "b931e7725c83275c56f962b51b83c10f31a01ac8d823c6722edaf16d893970b1"
+    sha256 cellar: :any,                 high_sierra:   "64286932634fbe1e0d07eacda334d2f4967b20bce0737424df56ec5eaa34ccca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "87555fde40181720eab295d1599bc75e6a80520eb2dfc8d90197c933e646b3ed"
   end
 
   def install
