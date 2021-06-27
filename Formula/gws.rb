4d9f019ad6e9fb19e4172b5dc3c4b34087f0cfd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:17 +0000

    gws: add 0.2.0 bottle.

--- Formula/gws.rb
+++ Formula/gws.rb
@@ -5,6 +5,10 @@ class Gws < Formula
   sha256 "f92b7693179c2522c57edd578abdb90b08f6e2075ed27abd4af56c1283deab1a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9d7e8f5b330c140793909af7a9717b943ce88b144c6bf25dce4c768b30df739c"
+  end
+
   depends_on "bash"
 
   def install
