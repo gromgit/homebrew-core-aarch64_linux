6222680a43a8886a3fd42e627fe36696ebd43ad0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:16 +0000

    git-recent: add 1.1.1 bottle.

--- Formula/git-recent.rb
+++ Formula/git-recent.rb
@@ -5,6 +5,10 @@ class GitRecent < Formula
   sha256 "790c0de09ea19948b3b0ad642d82c30ee20c8d14a04b761fa2a2f716dc19eedc"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "99a91fd45ae79225c569a3acdf442380c60701ac987a9fb6c965dda22132b2c2"
+  end
+
   depends_on macos: :sierra
 
   def install
