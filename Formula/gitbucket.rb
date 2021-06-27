6fb20c4469ed011302fa63e1007e24e80a8e26cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:25 +0000

    gitbucket: add 4.35.3 bottle.

--- Formula/gitbucket.rb
+++ Formula/gitbucket.rb
@@ -5,6 +5,10 @@ class Gitbucket < Formula
   sha256 "19cf2233f76171beda543fa11812365f409f807c07210ab83d57cb8252d66ebe"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "d993565ae3717b98cb81878650164ba91c8af24d2238d78d1999640b49c54b3a"
+  end
+
   head do
     url "https://github.com/gitbucket/gitbucket.git"
     depends_on "ant" => :build
