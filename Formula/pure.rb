58d01b26b2e266a2113170d5409ff12aba03b8a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:07:00 +0000

    pure: add 1.16.0 bottle.

--- Formula/pure.rb
+++ Formula/pure.rb
@@ -5,6 +5,10 @@ class Pure < Formula
   sha256 "152ffa2cffb6b79e10b78c0fb1de0b8461bae87822439d10ea9c749836060eca"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0fdaf19fad7766f7f2fecb0c5622c84d6098520b8d22115d6a3a591eba32134c"
+  end
+
   depends_on "zsh" => :test
   depends_on "zsh-async"
 
