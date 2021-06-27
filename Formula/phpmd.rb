447a96b7bf381d8449d2b27390c85d8f394a400c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:06:58 +0000

    phpmd: add 2.10.1 bottle.

--- Formula/phpmd.rb
+++ Formula/phpmd.rb
@@ -5,6 +5,10 @@ class Phpmd < Formula
   sha256 "c516e4a90c72ccf8170dcc806621c0f0a7ed553b8853aca085e7c38d2e187e61"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "70cff6045cc309d7bbf953341b667208b9e330d9acfc579f5986294e6d9b87ef"
+  end
+
   def install
     bin.install "phpmd.phar" => "phpmd"
   end
