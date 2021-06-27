982d0a4cd46d2c6cccf5fd232a719f298f2fb84f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:08:46 +0000

    jslint4java: add 2.0.5 bottle.

--- Formula/jslint4java.rb
+++ Formula/jslint4java.rb
@@ -4,6 +4,10 @@ class Jslint4java < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/jslint4java/jslint4java-2.0.5-dist.zip"
   sha256 "078240b17256a0472f9981d68f11556238658ebaa67be49ea49958adafc96a81"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "42851a1611e21e6ea7b35510783b3165bd2f34606345931bd781ab993f3b0c38"
+  end
+
   depends_on "openjdk"
 
   def install
