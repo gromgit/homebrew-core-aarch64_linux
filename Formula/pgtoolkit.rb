7da8e72af764bb60fa0d87070996ec0e39dc8a16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:06:57 +0000

    pgtoolkit: add 1.0.2 bottle.

--- Formula/pgtoolkit.rb
+++ Formula/pgtoolkit.rb
@@ -5,6 +5,10 @@ class Pgtoolkit < Formula
   sha256 "d86f34c579a4c921b77f313d4c7efbf4b12695df89e6b68def92ffa0332a7351"
   license "PostgreSQL"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "855d67c931981dc3d62ce39fb663f0ece72efc919001352be2902ae1ad491b12"
+  end
+
   def install
     bin.install "fatpack/pgcompact"
     doc.install %w[CHANGES.md LICENSE.md README.md TODO.md]
