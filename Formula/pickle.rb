d58e2b3df99145aa6490d6bd733e43da5f064e02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:06:58 +0000

    pickle: add 0.7.2 bottle.

--- Formula/pickle.rb
+++ Formula/pickle.rb
@@ -5,6 +5,13 @@ class Pickle < Formula
   sha256 "4fee7dbfe44c93c939fbc7d4b1929a3255e2d042b0e46685dac80a5e181c102e"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3f40656f0fd73dc814fdfe25e760934de5c88e2d589cda78d03382a4ac7e0668"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0c6cab0f396d7fb92bf9a4c9f34b4ee8270cc7dc90bcbcf3161b85434c877bb8"
+    sha256 cellar: :any_skip_relocation, catalina:      "0c6cab0f396d7fb92bf9a4c9f34b4ee8270cc7dc90bcbcf3161b85434c877bb8"
+    sha256 cellar: :any_skip_relocation, mojave:        "0c6cab0f396d7fb92bf9a4c9f34b4ee8270cc7dc90bcbcf3161b85434c877bb8"
+  end
+
   depends_on "php"
 
   def install
