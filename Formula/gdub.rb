1f3b89e9cd85869e5760c953c34e8aa9398c37da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:19 +0000

    gdub: add 0.2.0 bottle.

--- Formula/gdub.rb
+++ Formula/gdub.rb
@@ -5,6 +5,10 @@ class Gdub < Formula
   sha256 "aa3da76752b597e60094a67971f35dfe20377390d21b3ae3b45b7b7040e9a268"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b2381d533ecbab2d5c7e984f2b8a59601a1a50c0fd16de0942de59613f423a3e"
+  end
+
   # "This project is obsolete. Please use 'gng' instead.":
   # https://github.com/gdubw/gng
   deprecate! date: "2021-01-05", because: :repo_archived
