e651cd0135195591e0bb6a8ff640bcef9806ad5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:15 +0000

    gcutil: add 1.16.1 bottle.

--- Formula/gcutil.rb
+++ Formula/gcutil.rb
@@ -4,6 +4,10 @@ class Gcutil < Formula
   url "https://dl.google.com/dl/cloudsdk/release/artifacts/gcutil-1.16.1.zip"
   sha256 "31f438c9ce3471f1404340e3411239b28b63f117d17776271fee1e1a352f3877"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e5de2c3ea2450af30d6797f821b9ccb3c3da8c47f3086f1c2ccb063e91e9c68c"
+  end
+
   def install
     libexec.install "gcutil", "lib"
     bin.install_symlink libexec/"gcutil"
