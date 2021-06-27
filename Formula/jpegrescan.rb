afe7e5510c28e05d38e66e2971bcff1cf5c3045b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:08:46 +0000

    jpegrescan: add 1.1.0 bottle.

--- Formula/jpegrescan.rb
+++ Formula/jpegrescan.rb
@@ -5,6 +5,10 @@ class Jpegrescan < Formula
   sha256 "a8522e971d11c904f4b61af665c3be800f26404e2b14f5f80c675b4a72a42b32"
   head "https://github.com/kud/jpegrescan.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a09a6149fe87f3cc7a2f54b9b3c538bc6881d3b4e7edc6f947ed54d29d4ad473"
+  end
+
   depends_on "jpeg"
 
   def install
