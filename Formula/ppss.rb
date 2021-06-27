9cb95aa851a32000603277b13acfd07dcb193229
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:06:56 +0000

    ppss: add 2.97 bottle.

--- Formula/ppss.rb
+++ Formula/ppss.rb
@@ -5,6 +5,10 @@ class Ppss < Formula
   sha256 "25d819a97d8ca04a27907be4bfcc3151712837ea12a671f1a3c9e58bc025360f"
   license "GPL-3.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ff7ab7dc16968b6fc8674f5ddc75e36d5c45ab140518a3263b136aae14eea81d"
+  end
+
   def install
     bin.install "ppss"
   end
