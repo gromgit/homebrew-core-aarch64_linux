2c75034264b1b88df35fe851eed5e5b8a53b67a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 07:07:02 +0000

    pax-runner: add 1.8.6 bottle.

--- Formula/pax-runner.rb
+++ Formula/pax-runner.rb
@@ -5,6 +5,10 @@ class PaxRunner < Formula
   version "1.8.6"
   sha256 "42a650efdedcb48dca89f3e4272a9e2e1dcc6bc84570dbb176b5e578ca1ce2d4"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "cdaa1633b1cc5310e7e6d5edd558316c794d37869976494d6b1b4d465ce1129d"
+  end
+
   def install
     (bin+"pax-runner").write <<~EOS
       #!/bin/sh
