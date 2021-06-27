04d63c048a990e033feb6adcd75058933494a934
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:21 +0000

    gimme: add 1.5.4 bottle.

--- Formula/gimme.rb
+++ Formula/gimme.rb
@@ -5,6 +5,10 @@ class Gimme < Formula
   sha256 "71036f892b3cae08f29be6fd4c69fb20d9b003ec80a24221d73e995e12ab0fe0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "955a907946bda06e3e0eeef3af936182915ddfae8040eb744a394a47a570e9d0"
+  end
+
   def install
     bin.install "gimme"
   end
