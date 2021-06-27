40e69fd93811d914ececb790ad5980f487fa82f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 20:09:24 +0000

    gibo: add 2.2.4 bottle.

--- Formula/gibo.rb
+++ Formula/gibo.rb
@@ -5,6 +5,13 @@ class Gibo < Formula
   sha256 "35debd3e345caf8eeb4441a3877b7e33c98caec5f5c5e2e61da1cb1a263aec4b"
   license "Unlicense"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "af671cfa2e8503e978d8623829d236932fb1bc9e5e4df3ef709d5ecb8c9f91c1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "106c82cc27f9897f960c131a543cd93c4ca449c4004aebfc7f07cc46f0059157"
+    sha256 cellar: :any_skip_relocation, catalina:      "106c82cc27f9897f960c131a543cd93c4ca449c4004aebfc7f07cc46f0059157"
+    sha256 cellar: :any_skip_relocation, mojave:        "106c82cc27f9897f960c131a543cd93c4ca449c4004aebfc7f07cc46f0059157"
+  end
+
   def install
     bin.install "gibo"
     bash_completion.install "shell-completions/gibo-completion.bash"
