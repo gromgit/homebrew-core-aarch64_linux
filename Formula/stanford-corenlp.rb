e2d56e59489af0a026e1ba66db113ab4341b00d2
Sam Ford <1584702+samford@users.noreply.github.com>
2021-06-27 21:49:12 -0400

    stanford-corenlp 4.2.2
    Closes #80162.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stanford-corenlp.rb
+++ Formula/stanford-corenlp.rb
@@ -1,9 +1,14 @@
 class StanfordCorenlp < Formula
   desc "Java suite of core NLP tools"
   homepage "https://stanfordnlp.github.io/CoreNLP/"
-  url "https://nlp.stanford.edu/software/stanford-corenlp-4.2.0.zip"
-  sha256 "984aff1c431311a7dd5b4f0cb3bec4f5ba81da37aecc0d020b270571aafc5385"
+  url "https://nlp.stanford.edu/software/stanford-corenlp-4.2.2.zip"
+  sha256 "42f0bd84b815b15658a17aeabe5ab8c5ba1d4e5a3785969fe7be8588209f02cc"
   license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?stanford-corenlp[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e1580a6e899a81fa4ff31dc192521b9fe028b8612a2973a9935b3578e999c89e"
