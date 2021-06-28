dadd7034d328395abdd3d76dc26840e0cdb3cddb
andy5995 <andy400-dev@yahoo.com>
2021-06-28 16:08:06 -0500

    rmw 0.8.0
    Closes #80199.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rmw.rb
+++ Formula/rmw.rb
@@ -1,8 +1,8 @@
 class Rmw < Formula
   desc "Safe-remove utility for the command-line"
   homepage "https://remove-to-waste.info/"
-  url "https://github.com/theimpossibleastronaut/rmw/releases/download/v0.7.09/rmw-0.7.09.tar.gz"
-  sha256 "d0a85944d03e7ec2a94b25d6d6ac92880fd0d3f63d90bb9ed56b16418fd41c69"
+  url "https://github.com/theimpossibleastronaut/rmw/releases/download/v0.8.0/rmw-0.8.0.tar.gz"
+  sha256 "17b11b771befb89adbd7764fb897dff1a32eed34f8cd4d8b495da7f2a66de3b8"
   license "GPL-3.0-or-later"
   head "https://github.com/theimpossibleastronaut/rmw.git"
 
@@ -18,16 +18,20 @@ class Rmw < Formula
     sha256 cellar: :any, mojave:        "9ae98484dc5bc1bc0ecea7ef1c1edc42249a9841844283bac995cb5c6817375b"
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
   # Slightly buggy with system ncurses
   # https://github.com/theimpossibleastronaut/rmw/issues/205
   depends_on "ncurses"
 
   def install
-    system "./configure", "--enable-debug=no",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
