fae1f0328173abf4c51977737ae5f69eac29e39b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-29 21:06:04 +0000

    swagger-codegen@2: update 2.4.21 bottle.

--- Formula/swagger-codegen@2.rb
+++ Formula/swagger-codegen@2.rb
@@ -11,9 +11,9 @@ class SwaggerCodegenAT2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "61cfae47db4d83734155edb210c038510bbe9c9e6db083faf38588ea8424959d"
-    sha256 cellar: :any_skip_relocation, catalina: "e61e995f40b4c66561ffc942cffd4134f276c6a3a19641d228ff9c2101e4f1b1"
-    sha256 cellar: :any_skip_relocation, mojave:   "ddb6b822a399753579b60f69a84c6edd40bdc1b11d2c9d1783d34cc40bba5c60"
+    sha256 cellar: :any_skip_relocation, big_sur:  "ee749950ee8cdd3ab857f180639c55a52fba2376b07651a0ebc20ac0545aafdd"
+    sha256 cellar: :any_skip_relocation, catalina: "356f53ea21ff99adaf4a8f6d92b050ef73b1215b2571a83dd7f6b94cfcdc61ba"
+    sha256 cellar: :any_skip_relocation, mojave:   "b4a4565fb71e64da0a0bdb792117421a64997eb52347f7bb2392681fbd910e9c"
   end
 
   keg_only :versioned_formula
