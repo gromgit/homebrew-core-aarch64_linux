99c3f66ec8f8022edd5e1978e317ee6e329504da
Dario Vladović <d.vladimyr@gmail.com>
2021-06-30 00:33:05 +0200

    ronn: add livecheck
    For the reasoning behind this change see the following post:
    https://github.com/Homebrew/homebrew-core/pull/80167#issuecomment-870232507

--- Formula/ronn.rb
+++ Formula/ronn.rb
@@ -5,6 +5,11 @@ class Ronn < Formula
   sha256 "808aa6668f636ce03abba99c53c2005cef559a5099f6b40bf2c7aad8e273acb4"
   license "MIT"
 
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     sha256 arm64_big_sur: "5c1633f7c2bbed5e9570abdfeab0e09267d5950cc1dd1e1407390e78f7061c99"
     sha256 big_sur:       "039c6b58bcb2a23599731b0616f86367f599a18ca1e9ac6aabad00c759d51e1c"
