080debca515f613e9fa12daebdba8f29488aecfb
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-06-29 08:18:32 +0000

    jrsonnet: add 0.3.8 bottle.

--- Formula/jrsonnet.rb
+++ Formula/jrsonnet.rb
@@ -6,6 +6,13 @@ class Jrsonnet < Formula
   license "MIT"
   head "https://github.com/CertainLach/jrsonnet.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ecc44b14e9e1d6b12ef3eab6f8d90930d5671d099b19b6242d790077ca986139"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b2578d9a53ff9449f8673fe75e448d9d94f63275047d91be2b9ff8fd00a53730"
+    sha256 cellar: :any_skip_relocation, catalina:      "5fdb422182ccecaae6e07e654efc7a9cf612e251c2ecf1c16427d2edec4f64b5"
+    sha256 cellar: :any_skip_relocation, mojave:        "70f069a8f7fc8d73ed57c30efa82b4351068c32c73ac915eb540618450cdbecc"
+  end
+
   depends_on "rust" => :build
 
   def install
