56e24abb77bb6091336c4d652a03051ebb1e0a7b
miccal <miccal.matthews@gmail.com>
2021-07-01 07:27:39 +0800

    gnu-smalltalk: revision bump (libffi 3.4.2)

--- Formula/gnu-smalltalk.rb
+++ Formula/gnu-smalltalk.rb
@@ -5,7 +5,7 @@ class GnuSmalltalk < Formula
   mirror "https://ftpmirror.gnu.org/smalltalk/smalltalk-3.2.5.tar.xz"
   sha256 "819a15f7ba8a1b55f5f60b9c9a58badd6f6153b3f987b70e7b167e7755d65acc"
   license "GPL-2.0"
-  revision 9
+  revision 10
   head "https://github.com/gnu-smalltalk/smalltalk.git"
 
   bottle do
