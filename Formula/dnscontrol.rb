ff9c3346b66b81fe87eb0d03258014a8c6da3614
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-06-30 11:17:31 +0000

    dnscontrol: update 3.10.0 bottle.

--- Formula/dnscontrol.rb
+++ Formula/dnscontrol.rb
@@ -12,10 +12,10 @@ class Dnscontrol < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f97bce34833e31eff42e94301a7412052c6b9f965b410320a207ef58a09109ed"
-    sha256 cellar: :any_skip_relocation, big_sur:       "dc113a15d6cb60307fc1e5c181b47cccbcf664d26e90bf3d9c4d2d4d616575a8"
-    sha256 cellar: :any_skip_relocation, catalina:      "7292ecff37f7ad67a091a9c6e2ca58eebd212e44ae73261b803f0f77b6f60f30"
-    sha256 cellar: :any_skip_relocation, mojave:        "13c1fb3a2ff2df3fd8a24e14e06430b9d8c1c27502c88b5ef9ceac0d9dbbeba9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0e719b4f3dd5985b85b413dabaa87e692af31539bfa082c387f4d8fb14544877"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a5cb6d0616898a7adb9d7cf69d073def4f2ff144da90f24a722099d90c045a94"
+    sha256 cellar: :any_skip_relocation, catalina:      "d0c59069462b1862d460dca8523e1bc4f24b5b6b67d6db8113a2333f221723cb"
+    sha256 cellar: :any_skip_relocation, mojave:        "3e5f21140661c0197ecf4fdba75cb91171cad2af09492ca0faeef9ca5f099dee"
   end
 
   depends_on "go" => :build
