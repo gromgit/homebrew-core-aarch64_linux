4ea1305f8a2b50c4abe2d8efb2eaffe0606c4791
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-30 04:41:57 +0000

    libaio: add 0.3.112 bottle.

--- Formula/libaio.rb
+++ Formula/libaio.rb
@@ -5,6 +5,10 @@ class Libaio < Formula
   sha256 "b7cf93b29bbfb354213a0e8c0e82dfcf4e776157940d894750528714a0af2272"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "259d47affeaf26f081c49737f2121dc63ac5692c9752ecab4f1e333c81d19b53"
+  end
+
   depends_on :linux
 
   def install
