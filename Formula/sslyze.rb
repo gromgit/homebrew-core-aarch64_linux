caa8b54907f7ec6859ffe4e79776d9485fb2fc36
miccal <miccal.matthews@gmail.com>
2021-07-01 07:27:46 +0800

    sslyze: revision bump (libffi 3.4.2)

--- Formula/sslyze.rb
+++ Formula/sslyze.rb
@@ -4,6 +4,7 @@ class Sslyze < Formula
   desc "SSL scanner"
   homepage "https://github.com/nabla-c0d3/sslyze"
   license "AGPL-3.0-only"
+  revision 1
 
   stable do
     url "https://files.pythonhosted.org/packages/d5/b9/195ada85f8577e5b99a45338974e6de2d81aabeccee303fe66e455e91125/sslyze-4.1.0.tar.gz"
