8157ddb858703cd4a7b0289b3f9b17eb47d349b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-30 17:17:54 +0000

    hcloud: update 1.25.0 bottle.

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -6,10 +6,10 @@ class Hcloud < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dfcd4be30cb1cf354c552b67c426b520a56e54d67955468a789defae22875d55"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2c45503344f075ecd8265205e2f4229685231b4ab2b13f20e3d56d1d9110081b"
-    sha256 cellar: :any_skip_relocation, catalina:      "0f89d2f3a3ee0eeed4dc456c856e6aac854f7cc4eec4dff3de89c67ac705c64e"
-    sha256 cellar: :any_skip_relocation, mojave:        "849520b12d7d442d033657163ef3e04a1afc1dd4b95daaae5c1ff83503d4c070"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "82d19b10b267039429b75f0dd7aaae00c28572d4838d1536f1f26779c075f78b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "332ac8bdf5dd7cfb17d7bf10b454c384766ebc5e554a99a4c51ffd508ec1d889"
+    sha256 cellar: :any_skip_relocation, catalina:      "7a05179cdb306920d30cf93babc4b2b06c94d83d39ae8d01cf2d875549b4996a"
+    sha256 cellar: :any_skip_relocation, mojave:        "9784796504ea3ab242d89ae7136c2f0fe72e9e9ea0bbc2291b9cad3ecac4335c"
   end
 
   depends_on "go" => :build
