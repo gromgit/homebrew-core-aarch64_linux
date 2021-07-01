9eb00566b2fb2ab6f7deaab955c56c0d1795674e
Caleb Xu <calebcenter@live.com>
2021-07-01 09:06:59 -0400

    netpbm 10.86.23
    Closes #80378.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4107"
-  version "10.86.22"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4115"
+  version "10.86.23"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
