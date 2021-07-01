da914aa01e3d9e81bf96dea497d5294b69250188
Adrian Ho <aho+vm-catalina@03s.net>
2021-07-02 01:14:19 +0800

    tup: add macOS FUSE rejection info

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -15,7 +15,7 @@ class Tup < Formula
   depends_on "pkg-config" => :build
 
   on_macos do
-    disable! date: "2021-04-08", because: "requires FUSE"
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
   end
 
   on_linux do
@@ -29,6 +29,18 @@ class Tup < Formula
     man1.install "tup.1"
     doc.install (buildpath/"docs").children
     pkgshare.install "contrib/syntax"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
