6965cc8ca38b9d14c2a1d3af1d85579fb6ee6467
Adrian Ho <aho+vm-catalina@03s.net>
2021-07-02 01:14:17 +0800

    fuse-zip: add macOS FUSE rejection info

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -19,7 +19,7 @@ class FuseZip < Formula
   depends_on "libzip"
 
   on_macos do
-    disable! date: "2021-04-08", because: "requires FUSE"
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
   end
 
   on_linux do
@@ -28,6 +28,18 @@ class FuseZip < Formula
 
   def install
     system "make", "prefix=#{prefix}", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
