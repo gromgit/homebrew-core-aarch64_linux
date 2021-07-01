a987a5fb597e17b159e5f5ce5811fec001e45a60
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-01 21:44:11 +0000

    libyaml: update 0.2.5 bottle.

--- Formula/libyaml.rb
+++ Formula/libyaml.rb
@@ -11,11 +11,12 @@ class Libyaml < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "fe1082f3475a144261b41e2c3e0728b9331911b1cbfadfbc1f3d70d454709154"
-    sha256 cellar: :any, big_sur:       "83547fba540a38c30705a59a2e746952c68857212e823c6ee97c186e088f75cd"
-    sha256 cellar: :any, catalina:      "56d3549b342cffb181e3eb05356697bbb362b9733c73e0eeff9b637ecf92cd23"
-    sha256 cellar: :any, mojave:        "a04988b3868cfadf7bcaff6b753b59388cbea70b38f2fa41a25229150d073696"
-    sha256 cellar: :any, high_sierra:   "d3e22ad09c3d6872c5f7ee7c7f1146c9f14c178ff4c3a3488a20bf584bc854d5"
+    sha256 cellar: :any,                 arm64_big_sur: "fe1082f3475a144261b41e2c3e0728b9331911b1cbfadfbc1f3d70d454709154"
+    sha256 cellar: :any,                 big_sur:       "83547fba540a38c30705a59a2e746952c68857212e823c6ee97c186e088f75cd"
+    sha256 cellar: :any,                 catalina:      "56d3549b342cffb181e3eb05356697bbb362b9733c73e0eeff9b637ecf92cd23"
+    sha256 cellar: :any,                 mojave:        "a04988b3868cfadf7bcaff6b753b59388cbea70b38f2fa41a25229150d073696"
+    sha256 cellar: :any,                 high_sierra:   "d3e22ad09c3d6872c5f7ee7c7f1146c9f14c178ff4c3a3488a20bf584bc854d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "354677a745b6c62109e792ddbd0cbdaf9e6a471d84fdbde3a7d9bae36d832da8"
   end
 
   depends_on "autoconf" => :build
