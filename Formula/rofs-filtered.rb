eea766e2baafe88a1381e09e931a9894632af146
Adrian Ho <aho+vm-catalina@03s.net>
2021-07-02 01:14:18 +0800

    rofs-filtered: add macOS FUSE rejection info

--- Formula/rofs-filtered.rb
+++ Formula/rofs-filtered.rb
@@ -15,7 +15,7 @@ class RofsFiltered < Formula
   depends_on "cmake" => :build
 
   on_macos do
-    disable! date: "2021-04-08", because: "requires FUSE"
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
   end
 
   on_linux do
@@ -26,6 +26,18 @@ class RofsFiltered < Formula
     mkdir "build" do
       system "cmake", "..", "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}", *std_cmake_args
       system "make", "install"
+    end
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
     end
   end
 end
