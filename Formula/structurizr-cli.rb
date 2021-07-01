6934a1fa532b17e8b497b944553340e9cd21eeee
Rui Chen <rui@chenrui.dev>
2021-06-30 21:33:48 -0400

    structurizr-cli 1.12.0
    Closes #80352.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -1,8 +1,8 @@
 class StructurizrCli < Formula
   desc "Command-line utility for Structurizr"
   homepage "https://structurizr.com"
-  url "https://github.com/structurizr/cli/releases/download/v1.11.0/structurizr-cli-1.11.0.zip"
-  sha256 "c07547da5a96ee764de1501f3d1488ca1b02f481bc669e2ad7c55aa0c6208a4e"
+  url "https://github.com/structurizr/cli/releases/download/v1.12.0/structurizr-cli-1.12.0.zip"
+  sha256 "69c3995531256282e0da7c366e87dd7dc84f4c07ce70760736e4ed0d6001c347"
   license "Apache-2.0"
 
   bottle do
@@ -19,6 +19,7 @@ class StructurizrCli < Formula
   test do
     expected_output = <<~EOS.strip
       Structurizr CLI v#{version}
+      Structurizr DSL v#{version}
       Usage: structurizr push|pull|lock|unlock|export|validate|list [options]
     EOS
     result = pipe_output("#{bin}/structurizr-cli").strip
