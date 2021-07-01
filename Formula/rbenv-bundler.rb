1cde7eb8cbc7b239ca2364865191521e792659e3
Roy Liu <carsomyr@gmail.com>
2021-07-01 02:08:35 -0400

    rbenv-bundler 1.0.1
    Closes #80364.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rbenv-bundler.rb
+++ Formula/rbenv-bundler.rb
@@ -1,10 +1,9 @@
 class RbenvBundler < Formula
   desc "Makes shims aware of bundle install paths"
   homepage "https://github.com/carsomyr/rbenv-bundler"
-  url "https://github.com/carsomyr/rbenv-bundler/archive/1.00.tar.gz"
-  sha256 "84f5456b1ac8ea4554db8fa17c99b5f5d2eafcd647f85088a5d36e805c092ba7"
+  url "https://github.com/carsomyr/rbenv-bundler/archive/1.0.1.tar.gz"
+  sha256 "6840d4165242da4606cd246ee77d484a91ee926331c5a6f840847ce189f54d74"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/carsomyr/rbenv-bundler.git"
 
   bottle do
