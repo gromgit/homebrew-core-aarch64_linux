408a178accb19a1bfb60c7d25b8c235df6308705
Rui Chen <rui@chenrui.dev>
2021-07-01 12:29:45 -0400

    libvirt 7.5.0
    Closes #80404.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -1,8 +1,8 @@
 class Libvirt < Formula
   desc "C virtualization API"
   homepage "https://www.libvirt.org"
-  url "https://libvirt.org/sources/libvirt-7.4.0.tar.xz"
-  sha256 "b366d73dee6ce77a226bedef592e0620ceb8e22e5998f60768017f79fc4ead26"
+  url "https://libvirt.org/sources/libvirt-7.5.0.tar.xz"
+  sha256 "90107b49032c2f3022c84076c649135187fbc70c8efd16427c6c4b6d613c2895"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
 
   livecheck do
