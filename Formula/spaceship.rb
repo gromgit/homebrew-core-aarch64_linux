9168013803454b8014ed87ef4ba11166c30496a3
Denys Dovhan <denysdovhan@gmail.com>
2021-07-01 13:51:59 +0300

    spaceship: update formula metadata (#80367)

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -1,10 +1,10 @@
 class Spaceship < Formula
   desc "Zsh prompt for Astronauts"
-  homepage "https://denysdovhan.com/spaceship-prompt"
-  url "https://github.com/denysdovhan/spaceship-prompt/archive/v3.12.6.tar.gz"
+  homepage "https://spaceship-prompt.sh"
+  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v3.12.6.tar.gz"
   sha256 "251e6bd72326ea4eb1e51632df8ab4bef6fc541849b870014459515280c60be5"
   license "MIT"
-  head "https://github.com/denysdovhan/spaceship-prompt.git"
+  head "https://github.com/spaceship-prompt/spaceship-prompt.git"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "aa63f75dd40c9ae84243a925451e37853dc565f886f70d8b3b4f377bf945d208"
