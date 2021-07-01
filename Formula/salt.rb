79c867c2332a6dead657843ad5f7daffcbfd36fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-01 14:13:10 +0000

    salt: update 3003.1_1 bottle.

--- Formula/salt.rb
+++ Formula/salt.rb
@@ -10,10 +10,10 @@ class Salt < Formula
   head "https://github.com/saltstack/salt.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "43a191cac9880b988e733085de42b59cb05142274683eb9662a5f7639760fa7d"
-    sha256 cellar: :any, big_sur:       "6f583bb006059cd86964b63894eb659a71e800c6be558fd18327bc2d9d78d212"
-    sha256 cellar: :any, catalina:      "f8033b15ede56595cca2c2d393a51ddc5bf7d9707fbc0da0c42f736bdf375937"
-    sha256 cellar: :any, mojave:        "d23b4a500e8c352f919e740ec68283d3d02368657566ac8eaaa10fe1505db116"
+    sha256 cellar: :any, arm64_big_sur: "64d2ec05ea8e96028d8e4932921968f447f51f78b22e039adddf88698f6a5d3d"
+    sha256 cellar: :any, big_sur:       "4361480356b9abafe2bb980d949607457f9a8b70dc7f3ee70c711d17728adcf4"
+    sha256 cellar: :any, catalina:      "70b57c11b9a3afe5671c5e2aa91208c70528a2eac272e2fc2d269aa4b3ea4cdc"
+    sha256 cellar: :any, mojave:        "303d0de97608541d8573e4b2c733b12b6ad116921159d535fc21c764cb1cdf27"
   end
 
   depends_on "swig" => :build
