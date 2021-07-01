9ab134d9e414b2993018b7ab7f843930b12337a6
Adrian Ho <aho+vm-catalina@03s.net>
2021-07-02 01:14:15 +0800

    avfs: add macOS FUSE rejection info

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -16,7 +16,7 @@ class Avfs < Formula
   depends_on "xz"
 
   on_macos do
-    disable! date: "2021-04-08", because: "requires FUSE"
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
   end
 
   on_linux do
@@ -36,6 +36,18 @@ class Avfs < Formula
 
     system "./configure", *args
     system "make", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
