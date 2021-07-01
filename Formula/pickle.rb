26a9bc1124d1758f245abc01ee8720cb057da5f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-01 12:12:12 +0000

    pickle: update 0.7.4 bottle.

--- Formula/pickle.rb
+++ Formula/pickle.rb
@@ -6,10 +6,10 @@ class Pickle < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3f40656f0fd73dc814fdfe25e760934de5c88e2d589cda78d03382a4ac7e0668"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0c6cab0f396d7fb92bf9a4c9f34b4ee8270cc7dc90bcbcf3161b85434c877bb8"
-    sha256 cellar: :any_skip_relocation, catalina:      "0c6cab0f396d7fb92bf9a4c9f34b4ee8270cc7dc90bcbcf3161b85434c877bb8"
-    sha256 cellar: :any_skip_relocation, mojave:        "0c6cab0f396d7fb92bf9a4c9f34b4ee8270cc7dc90bcbcf3161b85434c877bb8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "56cb810f374eeb4752ba6b25e18d14e07a44be86f3052884fcc690edd20f5a9f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6929dde45a9d39e24b03d5b684ed89e5f41f396938483bc619d26b7686a23d02"
+    sha256 cellar: :any_skip_relocation, catalina:      "6929dde45a9d39e24b03d5b684ed89e5f41f396938483bc619d26b7686a23d02"
+    sha256 cellar: :any_skip_relocation, mojave:        "6929dde45a9d39e24b03d5b684ed89e5f41f396938483bc619d26b7686a23d02"
   end
 
   depends_on "php"
