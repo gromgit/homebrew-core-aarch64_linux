f722068162b0a8a93da32b899402e3079a692d76
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-02 06:08:44 +0000

    lm-sensors: add 3.6.0 bottle.

--- Formula/lm-sensors.rb
+++ Formula/lm-sensors.rb
@@ -6,6 +6,10 @@ class LmSensors < Formula
   sha256 "0591f9fa0339f0d15e75326d0365871c2d4e2ed8aa1ff759b3a55d3734b7d197"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
+  bottle do
+    sha256 x86_64_linux: "9cab2dd7da319a2eda20dffe27d52292f3e66926fabba8ac1f2204fa0885af3d"
+  end
+
   depends_on "bison" => :build
   depends_on "flex" => :build
   depends_on :linux
