bad01da74cadaf75159b12f3a47253044684e2ba
Rui Chen <rui@chenrui.dev>
2021-07-02 14:32:23 -0400

    wasm-pack 0.10.0
    * wasm-pack 0.10.0
    * wasm-pack: add license
    
    Closes #80470.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wasm-pack.rb
+++ Formula/wasm-pack.rb
@@ -1,8 +1,9 @@
 class WasmPack < Formula
   desc "Your favorite rust -> wasm workflow tool!"
   homepage "https://rustwasm.github.io/wasm-pack/"
-  url "https://github.com/rustwasm/wasm-pack/archive/v0.9.1.tar.gz"
-  sha256 "56930d1f15bbcc85771e9b8e5c1b7bb6734b2c410c353ecd11eae828e35d5fb0"
+  url "https://github.com/rustwasm/wasm-pack/archive/v0.10.0.tar.gz"
+  sha256 "1b89ebb73853b749fdb2bc49474e17cca5658edd3b40723405a6a6cc3e3927fd"
+  license "Apache-2.0"
   head "https://github.com/rustwasm/wasm-pack.git"
 
   bottle do
