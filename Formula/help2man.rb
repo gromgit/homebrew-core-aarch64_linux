39eb604e69b8b0a267c6e6d1d4ac65253d81f568
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-02 05:53:54 +0000

    help2man: update 1.48.3_1 bottle.

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -12,6 +12,7 @@ class Help2man < Formula
     sha256 cellar: :any,                 big_sur:       "daa1e20f7225b1b4e64b2b022712a767d238a1dbe07dda53c63dfe1f0f0f8275"
     sha256 cellar: :any,                 catalina:      "337cedb9b9f97c1a1f4e08f3ff79142501d85da4f997c1859cd1dcb05653a836"
     sha256 cellar: :any,                 mojave:        "7d2a720ba3e5020ae28f32cc5705ffd9c8ec7b8b52aed3272d7d43e82eb91c3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3df16786bbbf321fa53130eade6395de200c6c2b03d5eefc48968bdd9ce5f817"
   end
 
   depends_on "gettext" if Hardware::CPU.intel?
