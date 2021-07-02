f24f25f4020d25684c97c0ab2f8410a0b81c1879
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-02 21:25:05 +0000

    libssh2: update 1.9.0_1 bottle.

--- Formula/libssh2.rb
+++ Formula/libssh2.rb
@@ -12,12 +12,13 @@ class Libssh2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2840147f112db1ef9e353e104e8aff64fc6572a9194b3ded9cc006deaa46560c"
-    sha256 cellar: :any, big_sur:       "f9a31faed068d80fff508f2a08d1c8b7213a626d9c38dde22db9a8173a85c6ee"
-    sha256 cellar: :any, catalina:      "2c4dcf8149663f9a133deac5bc42ce308d1ced90227cac391ca30b0ab2d381f9"
-    sha256 cellar: :any, mojave:        "327c56ad6a54894e5ef9aa3019d2444d32f1d0fba80925940100e517dd3109c9"
-    sha256 cellar: :any, high_sierra:   "ee29f44ef6fb59242fc7ee1747f02df2287722af4a45319289c9ee224367ba06"
-    sha256 cellar: :any, sierra:        "769fbbdc4e67b8de15c269f66a6efe86c5b0195df56e7e46b44377a572800efa"
+    sha256 cellar: :any,                 arm64_big_sur: "2840147f112db1ef9e353e104e8aff64fc6572a9194b3ded9cc006deaa46560c"
+    sha256 cellar: :any,                 big_sur:       "f9a31faed068d80fff508f2a08d1c8b7213a626d9c38dde22db9a8173a85c6ee"
+    sha256 cellar: :any,                 catalina:      "2c4dcf8149663f9a133deac5bc42ce308d1ced90227cac391ca30b0ab2d381f9"
+    sha256 cellar: :any,                 mojave:        "327c56ad6a54894e5ef9aa3019d2444d32f1d0fba80925940100e517dd3109c9"
+    sha256 cellar: :any,                 high_sierra:   "ee29f44ef6fb59242fc7ee1747f02df2287722af4a45319289c9ee224367ba06"
+    sha256 cellar: :any,                 sierra:        "769fbbdc4e67b8de15c269f66a6efe86c5b0195df56e7e46b44377a572800efa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7b18dca8b0e6a33c68c33ecb8af854de88fc2df59c14d5f20fb95f1a9903f32"
   end
 
   head do
