7213dcf1c9e11b9653c67f7960eaa7b513bcbc13
Rui Chen <rui@chenrui.dev>
2021-07-02 14:56:11 -0400

    synfig: revision bump (openexr 3.0.5)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/synfig.rb
+++ Formula/synfig.rb
@@ -6,7 +6,7 @@ class Synfig < Formula
   mirror "https://github.com/synfig/synfig/releases/download/v1.4.1/synfig-1.4.1.tar.gz"
   sha256 "f90f1dcff1a409bbe2bfbb2cf42b7019cf735bec3223722db2feba6e198d1bc1"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
   head "https://svn.code.sf.net/p/synfig/code/"
 
   livecheck do
