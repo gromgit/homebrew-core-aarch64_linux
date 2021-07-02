1a41c81f4f74c78c49ce94bbc3108d108a0d5789
Rui Chen <rui@chenrui.dev>
2021-07-01 20:13:29 -0400

    emscripten 2.0.25
    * emscripten 2.0.25
    * emscripten: update resources
    
    Closes #80426.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Emscripten < Formula
   desc "LLVM bytecode to JavaScript compiler"
   homepage "https://emscripten.org/"
-  url "https://github.com/emscripten-core/emscripten/archive/2.0.24.tar.gz"
-  sha256 "df670696c201dcc4e86f5aab7a92cf7c6a7899d9fcc807a67a4293ac37b0ef93"
+  url "https://github.com/emscripten-core/emscripten/archive/2.0.25.tar.gz"
+  sha256 "9d3a8c8b45a2938025733f5deae51fd9b3b24d91eed17cac2767a223d21fcd82"
   license all_of: [
     "Apache-2.0", # binaryen
     "Apache-2.0" => { with: "LLVM-exception" }, # llvm
@@ -33,7 +33,7 @@ class Emscripten < Formula
   # See llvm resource below for instructions on how to update this.
   resource "binaryen" do
     url "https://github.com/WebAssembly/binaryen.git",
-        revision: "83ae39af1f8ffe67856b16f7ee13de066169b48f"
+        revision: "10ef52d62468aec5762742930630e882dc5e5c0b"
   end
 
   # emscripten needs argument '-fignore-exceptions', which is only available in llvm >= 12
@@ -44,7 +44,7 @@ class Emscripten < Formula
   # Then use the listed llvm_project_revision for the resource below.
   resource "llvm" do
     url "https://github.com/llvm/llvm-project.git",
-        revision: "91f147792e815d401ae408989992f3c1530cc18a"
+        revision: "3644726a78e37823b1687a7aa8d186e91570ffe2"
   end
 
   def install
