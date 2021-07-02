6476695ec98fb1530b9583ba5d2c96fd31ad2399
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-02 21:21:58 +0000

    make: update 4.3 bottle.

--- Formula/make.rb
+++ Formula/make.rb
@@ -13,6 +13,7 @@ class Make < Formula
     sha256 catalina:      "39fc5ebff5ff708c2e3eea597b9f2eb79b910a122d30c3ac9bb93ebe313f030c"
     sha256 mojave:        "0c0a08eef68bcd78b0345f5f57a6efffcc7be877bcb3b803f39ac8916b882477"
     sha256 high_sierra:   "429177235322c3209e1657bea36364cd84222075b636939f6ed93a1cd04aeb21"
+    sha256 x86_64_linux:  "dd17cdf0a93ef30324250694ea5baec67c037e1d71c1cab8fe6432d66758fd62"
   end
 
   conflicts_with "remake", because: "both install texinfo files for make"
