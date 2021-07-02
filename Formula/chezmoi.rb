96c2bb5d996b8d844d4d4216c0b7f3a2ac3b399f
Tom Payne <twpayne@gmail.com>
2021-07-02 23:33:09 +0200

    chezmoi 2.1.0
    Closes #80481.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -2,8 +2,8 @@ class Chezmoi < Formula
   desc "Manage your dotfiles across multiple diverse machines, securely"
   homepage "https://chezmoi.io/"
   url "https://github.com/twpayne/chezmoi.git",
-      tag:      "v2.0.16",
-      revision: "745e198f0477cfee4a7b4bcfc385d51cd80806a0"
+      tag:      "v2.1.0",
+      revision: "509bda706a9046c36d14fc84ba93d4dc975a7171"
   license "MIT"
   head "https://github.com/twpayne/chezmoi.git"
 
