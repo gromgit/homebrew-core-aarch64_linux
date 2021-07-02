fd6a13bf9738c0c1490f126e998e87bbcbde3e61
Nanda H Krishna <me@nandahkrishna.com>
2021-07-02 18:41:54 +0000

    kalker: add 1.0.0 bottle.

--- Formula/kalker.rb
+++ Formula/kalker.rb
@@ -5,6 +5,13 @@ class Kalker < Formula
   sha256 "3dad381c20fb0ee8c03f0e3e888c4a937123df3cb948f488c057eccd9b352419"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "79ad383108f3c073108c0a0defeba5375e2d59746d5db274fa97db60f587a824"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3c8b07d6feabc9d8d4a813b11999de0bda08e5470140479d0b29eb4506777787"
+    sha256 cellar: :any_skip_relocation, catalina:      "6046b0d6fe943a55fcf34e0ee1aaccb93242df68a463d384656ba39297517e77"
+    sha256 cellar: :any_skip_relocation, mojave:        "0009cbb40dac094063773bdef5f0fedba5b7f633629d1a9b76b5023b6b3eed92"
+  end
+
   depends_on "rust" => :build
 
   def install
