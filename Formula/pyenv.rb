44e870e99bf0655b01cea6c0bc92ab58d811167a
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2021-07-03 15:39:22 +0800

    pyenv 2.0.3
    Closes #80488.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -1,8 +1,8 @@
 class Pyenv < Formula
   desc "Python version management"
   homepage "https://github.com/pyenv/pyenv"
-  url "https://github.com/pyenv/pyenv/archive/v2.0.2.tar.gz"
-  sha256 "04b1dae496208edd1ab4d3572a22978af9dd43ac25c53fa005431ebd24df9596"
+  url "https://github.com/pyenv/pyenv/archive/v2.0.3.tar.gz"
+  sha256 "929c7dfcdf3ca3f584e177ede96950aeef40d3300bf68560add9dd1fb9059156"
   license "MIT"
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git"
