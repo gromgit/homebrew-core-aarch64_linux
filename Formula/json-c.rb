94546a356f8177ce5522cf18c4e0c43240d1f989
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-03 20:21:55 +0000

    json-c: update 0.15 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -13,11 +13,12 @@ class JsonC < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "cfe16365846be7c8bbbab9cf5eaea4edc861f2174b37b9cc520d6dd0023c23d8"
-    sha256 cellar: :any, big_sur:       "11990ad17649041f31c96e7c383e9eb6a8e1cd7491c0ff9a8ee89ab66d2a11ba"
-    sha256 cellar: :any, catalina:      "60d15ece3fb1fdc8722785de8243c2261222f674e998509375522a1de75497ea"
-    sha256 cellar: :any, mojave:        "6ab7f776315184769ed74115f614996401eae4577c36144ba4cdd1d41427d0cf"
-    sha256 cellar: :any, high_sierra:   "a211a34a52b452386cf6e23f8f27cc9d088e64d2793bae7a4b3a7a069d31a88a"
+    sha256 cellar: :any,                 arm64_big_sur: "cfe16365846be7c8bbbab9cf5eaea4edc861f2174b37b9cc520d6dd0023c23d8"
+    sha256 cellar: :any,                 big_sur:       "11990ad17649041f31c96e7c383e9eb6a8e1cd7491c0ff9a8ee89ab66d2a11ba"
+    sha256 cellar: :any,                 catalina:      "60d15ece3fb1fdc8722785de8243c2261222f674e998509375522a1de75497ea"
+    sha256 cellar: :any,                 mojave:        "6ab7f776315184769ed74115f614996401eae4577c36144ba4cdd1d41427d0cf"
+    sha256 cellar: :any,                 high_sierra:   "a211a34a52b452386cf6e23f8f27cc9d088e64d2793bae7a4b3a7a069d31a88a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e09dc03017ac91fe1b6db9c6e5ec2c580dd7ce72ba91ba46422f51b3e236d004"
   end
 
   depends_on "cmake" => :build
