20eb446d788b5f24139e974ccc02df92a5652df3
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-03 20:45:32 +0000

    wayland-protocols: add 1.21 bottle.

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -5,6 +5,10 @@ class WaylandProtocols < Formula
   sha256 "b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f1744e0eccc76810d17efaf1d05599140667a48731ac8c1409fd4dbbab7232c6"
+  end
+
   depends_on "pkg-config" => [:build, :test]
   depends_on "wayland" => :build
   depends_on :linux
