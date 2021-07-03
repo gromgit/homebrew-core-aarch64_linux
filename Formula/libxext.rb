36c6206c9f7b44743beb93ad39d346c2967f1dd7
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-03 14:59:20 +0000

    libxext: update 1.3.4 bottle.

--- Formula/libxext.rb
+++ Formula/libxext.rb
@@ -6,11 +6,12 @@ class Libxext < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "24e44ef107138f015271fcd5aaa400403594adf7c64cf4a628b0cfe44d4e9fc6"
-    sha256 cellar: :any, big_sur:       "8a037408ba5c4c95c33af0d022edd631b744823bb9fa522a06b502ed9bf1fbc5"
-    sha256 cellar: :any, catalina:      "20cc49734eba43e2e9f058fa12f3782c76ac232fada3f6d297f91dca6e0582be"
-    sha256 cellar: :any, mojave:        "3f2da07d877e158f41231d088f0ffe5551132beaf2f3df683dae0ac2c11817cb"
-    sha256 cellar: :any, high_sierra:   "0070b8ea70006d011aac1c617e1a5f88caa2ae351b637858f828f859cb72d813"
+    sha256 cellar: :any,                 arm64_big_sur: "24e44ef107138f015271fcd5aaa400403594adf7c64cf4a628b0cfe44d4e9fc6"
+    sha256 cellar: :any,                 big_sur:       "8a037408ba5c4c95c33af0d022edd631b744823bb9fa522a06b502ed9bf1fbc5"
+    sha256 cellar: :any,                 catalina:      "20cc49734eba43e2e9f058fa12f3782c76ac232fada3f6d297f91dca6e0582be"
+    sha256 cellar: :any,                 mojave:        "3f2da07d877e158f41231d088f0ffe5551132beaf2f3df683dae0ac2c11817cb"
+    sha256 cellar: :any,                 high_sierra:   "0070b8ea70006d011aac1c617e1a5f88caa2ae351b637858f828f859cb72d813"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc7d5b80cf1ac00d0ffff27be679d38c0b7c8e8ee8ceb1747bb1f8ac9e5773d0"
   end
 
   depends_on "pkg-config" => :build
