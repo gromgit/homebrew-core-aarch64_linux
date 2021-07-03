ebc028a32bf5ee7a33f47f8a9904e48b21643642
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-03 04:35:02 +0200

    flac: add download mirror
    Use download mirror for livecheck and sort directory listing to show
    the latest version first.

--- Formula/flac.rb
+++ Formula/flac.rb
@@ -2,10 +2,11 @@ class Flac < Formula
   desc "Free lossless audio codec"
   homepage "https://xiph.org/flac/"
   url "https://downloads.xiph.org/releases/flac/flac-1.3.3.tar.xz"
+  mirror "https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.3.tar.xz"
   sha256 "213e82bd716c9de6db2f98bcadbc4c24c7e2efe8c75939a1a84e28539c4e1748"
 
   livecheck do
-    url "https://downloads.xiph.org/releases/flac/"
+    url "https://ftp.osuosl.org/pub/xiph/releases/flac/?C=M&O=D"
     regex(/href=.*?flac[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
