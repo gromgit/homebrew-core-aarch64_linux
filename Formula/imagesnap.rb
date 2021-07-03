835849097a009ce825bd50827fc6fd598914af9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-03 17:09:50 +0000

    imagesnap: update 0.2.14 bottle.

--- Formula/imagesnap.rb
+++ Formula/imagesnap.rb
@@ -6,10 +6,10 @@ class Imagesnap < Formula
   license :public_domain
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "18669a90a51230e2bdd331646376a9d253a70887f7d0bc5026f3ae8d80e99c2c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9d376efb1d620a467c115484d22472555e630651c7a1b59423d6d4db9879b969"
-    sha256 cellar: :any_skip_relocation, catalina:      "ca0082c02a7b09d664c91cf94bcf1dd9f181fcaf855c4152e2530b44bfd86d75"
-    sha256 cellar: :any_skip_relocation, mojave:        "ebe22a833280f18db1a3c0045660ed59da9873b81b6543c354737c875bcaf2a4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d0b0e448b487416847f57dd541f031b3f35b369d43e0317293f99043ed60b820"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d388402a4c5cbbd2ffe370dcc792075688fa55baedcd093c00f02b262a3ca942"
+    sha256 cellar: :any_skip_relocation, catalina:      "29524d3af5547a05136b5cb91a73e596a4aaa64f5a9fb437207814240439a3b8"
+    sha256 cellar: :any_skip_relocation, mojave:        "58b490ef6922ecc4fbd89b66275417562ce18fb3b7107804117a75a56b006dc9"
   end
 
   depends_on xcode: :build
