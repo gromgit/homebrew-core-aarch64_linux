8693484cc7775107e5189705620e082e525729d1
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-03 04:38:15 +0200

    theora: add download mirror
    Use download mirror for livecheck and sort directory listing to show
    the latest version first.

--- Formula/theora.rb
+++ Formula/theora.rb
@@ -2,10 +2,11 @@ class Theora < Formula
   desc "Open video compression format"
   homepage "https://www.theora.org/"
   url "https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
+  mirror "https://ftp.osuosl.org/pub/xiph/releases/theora/libtheora-1.1.1.tar.bz2"
   sha256 "b6ae1ee2fa3d42ac489287d3ec34c5885730b1296f0801ae577a35193d3affbc"
 
   livecheck do
-    url "https://www.theora.org/downloads/"
+    url "https://ftp.osuosl.org/pub/xiph/releases/theora/?C=M&O=D"
     regex(/href=.*?libtheora[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
