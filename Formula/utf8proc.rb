c175df5a72b94ea93aef29ef08c848f8db554cdf
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-03 11:33:19 +0000

    utf8proc: update 2.6.1 bottle.

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -6,10 +6,11 @@ class Utf8proc < Formula
   license all_of: ["MIT", "Unicode-DFS-2015"]
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e3845f14873623bba6259adc3f2b129d7a2ca41683764de6335eda07a30c3de7"
-    sha256 cellar: :any, big_sur:       "7d57bb4f93fdfd377fda65e74b9bf1cb7b2b4875a4481682a4b2a39cfe65d3e7"
-    sha256 cellar: :any, catalina:      "cf77fdee400e5692c3b78f0fc599e575b071b11a70ff1f3443e22c8dd14bd0fe"
-    sha256 cellar: :any, mojave:        "1bee383abb84a7f9921e1c6b3aa26de0c8c79295c4ae184959521bf537ba4552"
+    sha256 cellar: :any,                 arm64_big_sur: "e3845f14873623bba6259adc3f2b129d7a2ca41683764de6335eda07a30c3de7"
+    sha256 cellar: :any,                 big_sur:       "7d57bb4f93fdfd377fda65e74b9bf1cb7b2b4875a4481682a4b2a39cfe65d3e7"
+    sha256 cellar: :any,                 catalina:      "cf77fdee400e5692c3b78f0fc599e575b071b11a70ff1f3443e22c8dd14bd0fe"
+    sha256 cellar: :any,                 mojave:        "1bee383abb84a7f9921e1c6b3aa26de0c8c79295c4ae184959521bf537ba4552"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e574cf4da70696401aa9ebad3062929851ce5ee722216d67b454447a276e6a08"
   end
 
   def install
