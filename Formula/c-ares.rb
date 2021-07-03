16a3d5be9fe2e90e217c7c1f5e35343133b7c1a6
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-03 21:10:29 +0000

    c-ares: update 1.17.1 bottle.

--- Formula/c-ares.rb
+++ Formula/c-ares.rb
@@ -12,10 +12,11 @@ class CAres < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "63627c4d2e4698ba13b82aeb2a10f3aef3a7bcbb7b459c265dbd840e91e5b175"
-    sha256 cellar: :any, big_sur:       "514de64e48f4d2c6e448547a30ba03f613b899f30f97f9026740c59eb3f49aeb"
-    sha256 cellar: :any, catalina:      "3fc1e6a9c560039998b288db7dfb268c87db614841a6fa1048880b8b6bdd6e4c"
-    sha256 cellar: :any, mojave:        "8785faa759b2f10fcaefef1e7398b9ffe79b76b2339b4bc4b552fd9c418b1097"
+    sha256 cellar: :any,                 arm64_big_sur: "63627c4d2e4698ba13b82aeb2a10f3aef3a7bcbb7b459c265dbd840e91e5b175"
+    sha256 cellar: :any,                 big_sur:       "514de64e48f4d2c6e448547a30ba03f613b899f30f97f9026740c59eb3f49aeb"
+    sha256 cellar: :any,                 catalina:      "3fc1e6a9c560039998b288db7dfb268c87db614841a6fa1048880b8b6bdd6e4c"
+    sha256 cellar: :any,                 mojave:        "8785faa759b2f10fcaefef1e7398b9ffe79b76b2339b4bc4b552fd9c418b1097"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "644b650919c3d688f650c70fbd8f2015d906fc65997be2e3b4830fd804aa9c6c"
   end
 
   depends_on "cmake" => :build
