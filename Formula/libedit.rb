7bb697986a5b42b08853e3204c277dfa1e63317d
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-04 14:08:46 +0000

    libedit: update 20210522-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -12,10 +12,11 @@ class Libedit < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6a4159ed273ba498e091391dd5e84908be57294a8accc14ea88d17439e283df5"
-    sha256 cellar: :any, big_sur:       "f3a3b11ac4be1ece253f272d3a1b596571e430041ccf813280417764b62f5264"
-    sha256 cellar: :any, catalina:      "24bbdac27e417b37afbcbbee7c93ffed7e6fdfc27d32c8201e2bcb13fa129fff"
-    sha256 cellar: :any, mojave:        "b229a23679ba4228e241bb957d5db3ca6ab139b7997e793961ab60cca8850e6d"
+    sha256 cellar: :any,                 arm64_big_sur: "6a4159ed273ba498e091391dd5e84908be57294a8accc14ea88d17439e283df5"
+    sha256 cellar: :any,                 big_sur:       "f3a3b11ac4be1ece253f272d3a1b596571e430041ccf813280417764b62f5264"
+    sha256 cellar: :any,                 catalina:      "24bbdac27e417b37afbcbbee7c93ffed7e6fdfc27d32c8201e2bcb13fa129fff"
+    sha256 cellar: :any,                 mojave:        "b229a23679ba4228e241bb957d5db3ca6ab139b7997e793961ab60cca8850e6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb9f29cc995a3093417cbc4f4906111432da622e0d6ce4cc2e0e767fa99a197d"
   end
 
   keg_only :provided_by_macos
