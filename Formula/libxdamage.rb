b7ba32b6607cffdea251c3919587e9bcf1b98e52
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-04 07:53:06 +0000

    libxdamage: update 1.1.5 bottle.

--- Formula/libxdamage.rb
+++ Formula/libxdamage.rb
@@ -6,11 +6,12 @@ class Libxdamage < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bbd5ef8f7408de369198e66bc6aa8a75ddc798c444ebd7f03b885bc25ccfb136"
-    sha256 cellar: :any, big_sur:       "68750157664e1290e30b15ad95dbfcc1be2748b85e7eaef0851f2c0f56f043e3"
-    sha256 cellar: :any, catalina:      "2c09f29dfafe280bc0179dfe6ad82b623459e6bec07fefac41cf6b3e52385100"
-    sha256 cellar: :any, mojave:        "ea0aee131addc90c4b4ba6e0d8c4f8cdfd39dc034a7bfc3e841c408042ad8906"
-    sha256 cellar: :any, high_sierra:   "5c0ca5debb8c99cfed432fa2299e4a280ca81f8988aaacf44e0c0194d89ca7ac"
+    sha256 cellar: :any,                 arm64_big_sur: "bbd5ef8f7408de369198e66bc6aa8a75ddc798c444ebd7f03b885bc25ccfb136"
+    sha256 cellar: :any,                 big_sur:       "68750157664e1290e30b15ad95dbfcc1be2748b85e7eaef0851f2c0f56f043e3"
+    sha256 cellar: :any,                 catalina:      "2c09f29dfafe280bc0179dfe6ad82b623459e6bec07fefac41cf6b3e52385100"
+    sha256 cellar: :any,                 mojave:        "ea0aee131addc90c4b4ba6e0d8c4f8cdfd39dc034a7bfc3e841c408042ad8906"
+    sha256 cellar: :any,                 high_sierra:   "5c0ca5debb8c99cfed432fa2299e4a280ca81f8988aaacf44e0c0194d89ca7ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b55a0d21cbb4d4ad796564955f10369d59d80e7ba156c95cce2f82486cfcdf7"
   end
 
   depends_on "pkg-config" => :build
