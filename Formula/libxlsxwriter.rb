bf39b5b586569c01dcda57fb16c3c57e5cece1ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-04 01:27:57 +0000

    libxlsxwriter: update 1.0.8 bottle.

--- Formula/libxlsxwriter.rb
+++ Formula/libxlsxwriter.rb
@@ -7,10 +7,10 @@ class Libxlsxwriter < Formula
   head "https://github.com/jmcnamara/libxlsxwriter.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "98dc5b126bb7384519f4653c17ee9c42ce8d20bd71fc10fae15aac346e0db7c1"
-    sha256 cellar: :any, big_sur:       "2e4a46d19d9b8a2845d4bbe7e182210050940932bd4cc32204129086353e8806"
-    sha256 cellar: :any, catalina:      "c62b8c975d8f53a9841f34fb456647bb43e31db66c535cc2fb2740ccafb44aaa"
-    sha256 cellar: :any, mojave:        "5bf4db3205b2bc60dd8ad5735210064d45274bebd3ea6e65754a446bd848773a"
+    sha256 cellar: :any, arm64_big_sur: "b24239371e3016003b7fefea422ce3d2dd1747657857c808bbfcf56654c6c3e8"
+    sha256 cellar: :any, big_sur:       "299b869885d640cf1185a32976611e3ab37cd09e4d7be58ff71e5f2279251703"
+    sha256 cellar: :any, catalina:      "aa40933699f35aefabf4a5ef3f20d346512e03a1d9cbc262475a97871c855014"
+    sha256 cellar: :any, mojave:        "6b9ca979929ee06f76aad98d05704f9a16fa618799669f5021d75296a55818d0"
   end
 
   uses_from_macos "zlib"
