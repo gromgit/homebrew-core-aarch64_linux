3e1497b45cb4093718ce7937ab853af5fc204212
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-04 03:36:34 +0000

    sqsmover: add 0.3.14 bottle.

--- Formula/sqsmover.rb
+++ Formula/sqsmover.rb
@@ -6,6 +6,13 @@ class Sqsmover < Formula
   license "Apache-2.0"
   head "https://github.com/mercury2269/sqsmover.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "af88364e07133113f9edd4fb5d93dce01ef57c934a79a61c9b631193fa5df55e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a22d1ea6a666d648d77fc688a8757be60125a66b3c9250b7e04b96f66c7bbda1"
+    sha256 cellar: :any_skip_relocation, catalina:      "2e0cd9bde59865a7a1c8900999698135845017d76ba359a07ccac8554d0fb937"
+    sha256 cellar: :any_skip_relocation, mojave:        "7fbc42e0fbb2e7fb96d13aef9a50c75e1cd9f80240d0e978ee6a58a1e3b38724"
+  end
+
   depends_on "go" => :build
 
   def install
