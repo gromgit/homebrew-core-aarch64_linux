89e506f539b09f8f67258a61655699020d5130b1
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-04 20:37:34 +0000

    libxinerama: update 1.1.4 bottle.

--- Formula/libxinerama.rb
+++ Formula/libxinerama.rb
@@ -6,11 +6,12 @@ class Libxinerama < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f372ba853bd1ce9ef1c38d575e1ac3f2e7d5bdcba4ed938a87dfb017c9c0d5f2"
-    sha256 cellar: :any, big_sur:       "65b34fb9bd42fdf249ed942d470061da35456794b860863fd6997dd1b3c665fc"
-    sha256 cellar: :any, catalina:      "7a008044d1824d0585b13cba7021ec038bda6485a8789337cd6a9fd305b83233"
-    sha256 cellar: :any, mojave:        "c69df5fe02995fbbf085b26da9ae1af4b890b2d57d1e8d115253706486297e5f"
-    sha256 cellar: :any, high_sierra:   "27a945b6652dd0c36b3360ca55de687dd350feca08fc0e32bafe71d06edca377"
+    sha256 cellar: :any,                 arm64_big_sur: "f372ba853bd1ce9ef1c38d575e1ac3f2e7d5bdcba4ed938a87dfb017c9c0d5f2"
+    sha256 cellar: :any,                 big_sur:       "65b34fb9bd42fdf249ed942d470061da35456794b860863fd6997dd1b3c665fc"
+    sha256 cellar: :any,                 catalina:      "7a008044d1824d0585b13cba7021ec038bda6485a8789337cd6a9fd305b83233"
+    sha256 cellar: :any,                 mojave:        "c69df5fe02995fbbf085b26da9ae1af4b890b2d57d1e8d115253706486297e5f"
+    sha256 cellar: :any,                 high_sierra:   "27a945b6652dd0c36b3360ca55de687dd350feca08fc0e32bafe71d06edca377"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "027ffdecf7d30762d956b780ceed90425624cc36a225f3b4a45a0853c5badcdd"
   end
 
   depends_on "pkg-config" => :build
