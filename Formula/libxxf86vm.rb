81ab84d2d059e9f9da10e37282229c045b7b39f5
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-04 14:08:45 +0000

    libxxf86vm: update 1.1.4 bottle.

--- Formula/libxxf86vm.rb
+++ Formula/libxxf86vm.rb
@@ -6,11 +6,12 @@ class Libxxf86vm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c2f52435112da27491da0d3f8aae83ac4153bc17b2c8840d553c0556792eac52"
-    sha256 cellar: :any, big_sur:       "3443b4465d3839bcee9bbf60a1028b0acfa45877f9b8bfd2654ca42ad1087d46"
-    sha256 cellar: :any, catalina:      "0a4d6a9d0b98bd8b8cd2aa2025b6ee80a19ffca34744fed599ef0e754d9e810b"
-    sha256 cellar: :any, mojave:        "c89b765023eb0b6820cbf776d6af714363dc6ebd1ea000e1e53a99fa79be4a0d"
-    sha256 cellar: :any, high_sierra:   "37cf2b440d8d4e7cda6aa70d070cd45427cebc61781b6c1b3aec153bf82d638d"
+    sha256 cellar: :any,                 arm64_big_sur: "c2f52435112da27491da0d3f8aae83ac4153bc17b2c8840d553c0556792eac52"
+    sha256 cellar: :any,                 big_sur:       "3443b4465d3839bcee9bbf60a1028b0acfa45877f9b8bfd2654ca42ad1087d46"
+    sha256 cellar: :any,                 catalina:      "0a4d6a9d0b98bd8b8cd2aa2025b6ee80a19ffca34744fed599ef0e754d9e810b"
+    sha256 cellar: :any,                 mojave:        "c89b765023eb0b6820cbf776d6af714363dc6ebd1ea000e1e53a99fa79be4a0d"
+    sha256 cellar: :any,                 high_sierra:   "37cf2b440d8d4e7cda6aa70d070cd45427cebc61781b6c1b3aec153bf82d638d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b356b86fb40bc67fab58f385afbf48d74996a776ee8d06b2c403df225bf66635"
   end
 
   depends_on "pkg-config" => :build
