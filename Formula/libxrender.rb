fcd08749cf3b5619f74a9bda8901f557d49d7bc2
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-04 12:21:39 +0000

    libxrender: update 0.9.10 bottle.

--- Formula/libxrender.rb
+++ Formula/libxrender.rb
@@ -6,11 +6,12 @@ class Libxrender < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "46243f05a17674c00950dddc105b33aa479af7d605533d1aeada27d4d89d4275"
-    sha256 cellar: :any, big_sur:       "65fbad7a9da818086574a40880db58c8f2457db77aee1982d141cd09dad7e3a9"
-    sha256 cellar: :any, catalina:      "cb7f48876d362f919ed1c34ece8ec5abb16f6e414a6119655e3948fffab5dfab"
-    sha256 cellar: :any, mojave:        "77563596957d673a9f4acb0cb4f1e1d28c3b99e7f4a13f0a1dd1e3e403c454b9"
-    sha256 cellar: :any, high_sierra:   "b1bf08a6c6c6827af5a3472ac979c9328b780da52c1d234d385ef1fc3b0771b6"
+    sha256 cellar: :any,                 arm64_big_sur: "46243f05a17674c00950dddc105b33aa479af7d605533d1aeada27d4d89d4275"
+    sha256 cellar: :any,                 big_sur:       "65fbad7a9da818086574a40880db58c8f2457db77aee1982d141cd09dad7e3a9"
+    sha256 cellar: :any,                 catalina:      "cb7f48876d362f919ed1c34ece8ec5abb16f6e414a6119655e3948fffab5dfab"
+    sha256 cellar: :any,                 mojave:        "77563596957d673a9f4acb0cb4f1e1d28c3b99e7f4a13f0a1dd1e3e403c454b9"
+    sha256 cellar: :any,                 high_sierra:   "b1bf08a6c6c6827af5a3472ac979c9328b780da52c1d234d385ef1fc3b0771b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10b4f9c57a6af40dda0ca45ddecb325f9de9bee28959ed11fc0fa52d2accf562"
   end
 
   depends_on "pkg-config" => :build
