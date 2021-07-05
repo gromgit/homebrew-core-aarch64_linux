e5d914abd6f9e9e5e5623798c0d48d5d7e9e2001
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-05 21:04:38 +0000

    graphite2: update 1.3.14 bottle.

--- Formula/graphite2.rb
+++ Formula/graphite2.rb
@@ -7,11 +7,12 @@ class Graphite2 < Formula
   head "https://github.com/silnrsi/graphite.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "544e2c344f6c0a7c2c3cb6541150f0d0d91cd1100460dac9c6a08578823f91c3"
-    sha256 cellar: :any, big_sur:       "ddc468a1eec491aed5d5b05b22d0cffa38b6059d87eab747301011507fcf6366"
-    sha256 cellar: :any, catalina:      "0831f474c920b66bbeab3f93a91fa019b82bfffcdd40e369fdab76372700e980"
-    sha256 cellar: :any, mojave:        "2f3abb971be03141e9eea54b87c6861d72865bd76fde73ae3161d64c40d51cd9"
-    sha256 cellar: :any, high_sierra:   "62e39dce0ae0440ac164edaab6e1351520bc5414ad509fc0b8d5c890500785bd"
+    sha256 cellar: :any,                 arm64_big_sur: "544e2c344f6c0a7c2c3cb6541150f0d0d91cd1100460dac9c6a08578823f91c3"
+    sha256 cellar: :any,                 big_sur:       "ddc468a1eec491aed5d5b05b22d0cffa38b6059d87eab747301011507fcf6366"
+    sha256 cellar: :any,                 catalina:      "0831f474c920b66bbeab3f93a91fa019b82bfffcdd40e369fdab76372700e980"
+    sha256 cellar: :any,                 mojave:        "2f3abb971be03141e9eea54b87c6861d72865bd76fde73ae3161d64c40d51cd9"
+    sha256 cellar: :any,                 high_sierra:   "62e39dce0ae0440ac164edaab6e1351520bc5414ad509fc0b8d5c890500785bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0e811b079268bc99d1b6253fc0c979e76325e91e294fd0596349dd0285e847b9"
   end
 
   depends_on "cmake" => :build
