2107c9832cf9d02202af58a73fc7c5ccecbbe380
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-05 21:35:39 +0000

    abseil: update 20210324.2 bottle.

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -7,10 +7,11 @@ class Abseil < Formula
   head "https://github.com/abseil/abseil-cpp.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e6f4665315350736b5cd574f4316ebba4d10cfeda0531602f358c1c013ae9256"
-    sha256 cellar: :any, big_sur:       "75980931b499a49b4294b57b6a2c664258758f55255b8733bdfbd9c97df7d9c7"
-    sha256 cellar: :any, catalina:      "d0d9a804df91a4a70f5eec48ae7f434bb71befe5e9e48f7ac6a93322f5397453"
-    sha256 cellar: :any, mojave:        "dec3e39d5010921e76890d4d7343eb777715784f5d6d218329a768707f585f87"
+    sha256 cellar: :any,                 arm64_big_sur: "e6f4665315350736b5cd574f4316ebba4d10cfeda0531602f358c1c013ae9256"
+    sha256 cellar: :any,                 big_sur:       "75980931b499a49b4294b57b6a2c664258758f55255b8733bdfbd9c97df7d9c7"
+    sha256 cellar: :any,                 catalina:      "d0d9a804df91a4a70f5eec48ae7f434bb71befe5e9e48f7ac6a93322f5397453"
+    sha256 cellar: :any,                 mojave:        "dec3e39d5010921e76890d4d7343eb777715784f5d6d218329a768707f585f87"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "346f592e8d302b18fc688cdf4134a1b1879991c8789eec8214375d6439af25eb"
   end
 
   depends_on "cmake" => :build
