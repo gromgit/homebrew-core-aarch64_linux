2bf8a414543a1338bcd0af260e42b1c8de6e97c4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-05 23:44:59 +0000

    byacc: update 20210619 bottle.

--- Formula/byacc.rb
+++ Formula/byacc.rb
@@ -15,6 +15,7 @@ class Byacc < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "3ace0af560e8b91c4d6b4755c6701d13b7f4757a9e7d523f44ae0cba45e8bdb5"
     sha256 cellar: :any_skip_relocation, catalina:      "4c074897b184addd2e0ae251c39901c4099fd92b194240c5d22c010226c0ce55"
     sha256 cellar: :any_skip_relocation, mojave:        "49a84ba13ab75461bd8b116f42f3217dd554a425f343d17900ea60007cd4b29f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a35f06a724ecd4733b3e4259c8868a233eeaeecf369d265f3ee0be02b5fdd91"
   end
 
   def install
