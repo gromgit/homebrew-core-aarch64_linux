9976c0ef07deb61d8bfefd1868a798941eff88b3
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-05 05:26:51 +0000

    xinput: update 1.6.3 bottle.

--- Formula/xinput.rb
+++ Formula/xinput.rb
@@ -6,10 +6,11 @@ class Xinput < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "db714eb643b851dec6aa996c3673b932af70e191d404c8a187b408a9578f7fef"
-    sha256 cellar: :any, big_sur:       "dab36aa6df662605e220ffce42106ed9eb6668088c425773335b04a59ba29575"
-    sha256 cellar: :any, catalina:      "5bda55eccff0b10605378a85932776af6105fff7ae85593e2f10d13e9adca128"
-    sha256 cellar: :any, mojave:        "afad4cf5c8b632f1e8873eee07a2bfd694efb1466dcafc0d4c8c233e370c4195"
+    sha256 cellar: :any,                 arm64_big_sur: "db714eb643b851dec6aa996c3673b932af70e191d404c8a187b408a9578f7fef"
+    sha256 cellar: :any,                 big_sur:       "dab36aa6df662605e220ffce42106ed9eb6668088c425773335b04a59ba29575"
+    sha256 cellar: :any,                 catalina:      "5bda55eccff0b10605378a85932776af6105fff7ae85593e2f10d13e9adca128"
+    sha256 cellar: :any,                 mojave:        "afad4cf5c8b632f1e8873eee07a2bfd694efb1466dcafc0d4c8c233e370c4195"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "41d3a585aefaa9032b34865cced5bdff19339574ca7d4753ec9c64fb03b7b804"
   end
 
   depends_on "pkg-config" => :build
