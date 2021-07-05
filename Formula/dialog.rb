84750683374efd5e8a6e026503a2c1166f400b59
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-05 21:37:04 +0000

    dialog: update 1.3-20210621 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -15,6 +15,7 @@ class Dialog < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "990bbe5e7a36febc23c79bdbbc454075d695a54dd730e4dab8465d7a66ecab0d"
     sha256 cellar: :any_skip_relocation, catalina:      "87d270268a38de209e9b6fde7c963e4467554d7f0777425f2aa55a3701f25559"
     sha256 cellar: :any_skip_relocation, mojave:        "c05946dd7b57aa11f42ccccd0aab7c9b1506e8093c9dcc60588046aadd4b15dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2e3820b717ffdec0e6fe7fce8da31784263ebac9ecd49713677dccf5cc5446b"
   end
 
   uses_from_macos "ncurses"
