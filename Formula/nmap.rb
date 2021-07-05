dda7522173539d447e061c1d2f5142ebb4ae70bc
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-05 21:43:37 +0000

    nmap: update 7.91 bottle.

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -17,6 +17,7 @@ class Nmap < Formula
     sha256 catalina:      "5592fb8c2fe633a6339ee61901122c075a4b44c002e2887bddfb2c4b3aa2885f"
     sha256 mojave:        "ba808d31033d996488fdf56664de1cf424fc942db794ab7030d40a1caad93aa8"
     sha256 high_sierra:   "b9a5b9d54fb0af76b1ce343e94f142b3421309fbeb81078d73e41bc2a9d862ea"
+    sha256 x86_64_linux:  "1b532fb1c4570e3f849c5b9888046c116af14a0421c824f082a87f2462ce59cc"
   end
 
   depends_on "openssl@1.1"
