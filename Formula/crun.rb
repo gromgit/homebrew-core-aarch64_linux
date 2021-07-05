731793dc109d24ae8fb3f7080614e3d507b06f60
Henry Chu <henrytech@outlook.com>
2021-07-05 10:30:20 +0800

    crun 0.20.1 (new formula)
    * crun: new formula
    * crun: Add checking for test output
    * crun: Fixed version issue
    * crun: Updated the test block
    * added build dependencies
    
    Closes #80569.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/crun.rb
@@ -0,0 +1,31 @@
+class Crun < Formula
+  desc "Fast and lightweight fully featured OCI runtime and C library"
+  homepage "https://github.com/containers/crun"
+  url "https://github.com/containers/crun/releases/download/0.20.1/crun-0.20.1.tar.xz"
+  sha256 "d118b9749e0ea90de1bd8bb1314f319f43559bbd275cd9331f2d96cfd1ccf9e0"
+  license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "go-md2man" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
+
+  depends_on "libcap"
+  depends_on "libseccomp"
+  depends_on :linux
+  depends_on "systemd"
+  depends_on "yajl"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "", shell_output("crun --rootless=true list -q").strip
+  end
+end
