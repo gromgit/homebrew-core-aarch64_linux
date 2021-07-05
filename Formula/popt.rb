912dd605db8e794f3282c40f2c70ca081f6f4884
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-05 23:40:24 +0000

    popt: update 1.18 bottle.

--- Formula/popt.rb
+++ Formula/popt.rb
@@ -16,11 +16,12 @@ class Popt < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3ebf58493631c204386f60a189af7cc4127240644a0ce4713dd4af4374e45823"
-    sha256 cellar: :any, big_sur:       "460da044609e1821375dd248e5bb1a317a0f5e3f0431c4ab19532b2cfa6d7e7a"
-    sha256 cellar: :any, catalina:      "a5cbf26e1779c73865c7785adc163117465d321338aa6970dd4980cff4a2ec91"
-    sha256 cellar: :any, mojave:        "5d602602f195811a3ea8c29ce4540cca6e1f36890a08b5a262facf3f74a85cf1"
-    sha256 cellar: :any, high_sierra:   "554ba881a515b18e53d1abd7f67a544af42d811ea14283e75b13ae5fc2056024"
+    sha256 cellar: :any,                 arm64_big_sur: "3ebf58493631c204386f60a189af7cc4127240644a0ce4713dd4af4374e45823"
+    sha256 cellar: :any,                 big_sur:       "460da044609e1821375dd248e5bb1a317a0f5e3f0431c4ab19532b2cfa6d7e7a"
+    sha256 cellar: :any,                 catalina:      "a5cbf26e1779c73865c7785adc163117465d321338aa6970dd4980cff4a2ec91"
+    sha256 cellar: :any,                 mojave:        "5d602602f195811a3ea8c29ce4540cca6e1f36890a08b5a262facf3f74a85cf1"
+    sha256 cellar: :any,                 high_sierra:   "554ba881a515b18e53d1abd7f67a544af42d811ea14283e75b13ae5fc2056024"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b1fca748f705b965b222943f75fd97cdef1bfb3e98b478b49745cf50a89f7fa"
   end
 
   def install
