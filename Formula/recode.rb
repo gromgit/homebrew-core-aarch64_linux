0c1578b2f23abcdf00457c39ecc77939b73693dd
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-05 21:39:27 +0000

    recode: update 3.7.9 bottle.

--- Formula/recode.rb
+++ Formula/recode.rb
@@ -6,10 +6,11 @@ class Recode < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9bca3845a8c324b5f9546c2c9de7881ee2a91b47513a39188fbc65d5c119b3bd"
-    sha256 cellar: :any, big_sur:       "322571c853f461cd7f85afde9da5895996479b8aafddaa729081c10f4b319c57"
-    sha256 cellar: :any, catalina:      "e049f7705d6f397a3a4bb87e31cc43eeb8ab7f0958b5efb3daccbab2a77aaa96"
-    sha256 cellar: :any, mojave:        "27ba44840e12f588f741d2a7477ac8e6c6d3df22b09b426c617c87b98518d5c8"
+    sha256 cellar: :any,                 arm64_big_sur: "9bca3845a8c324b5f9546c2c9de7881ee2a91b47513a39188fbc65d5c119b3bd"
+    sha256 cellar: :any,                 big_sur:       "322571c853f461cd7f85afde9da5895996479b8aafddaa729081c10f4b319c57"
+    sha256 cellar: :any,                 catalina:      "e049f7705d6f397a3a4bb87e31cc43eeb8ab7f0958b5efb3daccbab2a77aaa96"
+    sha256 cellar: :any,                 mojave:        "27ba44840e12f588f741d2a7477ac8e6c6d3df22b09b426c617c87b98518d5c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4eaa20bd35e3d6937e2c8fa962572eda4998ef7504049b9592a62b3f133c1edd"
   end
 
   depends_on "libtool" => :build
