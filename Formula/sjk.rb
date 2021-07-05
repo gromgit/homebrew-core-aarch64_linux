4097f32ad845a30e5f1741ab5b86843f6c9ee55a
Caleb Xu <calebcenter@live.com>
2021-07-05 19:30:20 -0400

    sjk 0.18
    Closes #80611.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -1,8 +1,8 @@
 class Sjk < Formula
   desc "Swiss Java Knife"
   homepage "https://github.com/aragozin/jvm-tools"
-  url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.17/sjk-plus-0.17.jar"
-  sha256 "4f3d1b8b5c1c85b7fdcec1ebc880bac6d81da5d924fd445ce7e221e6728705d7"
+  url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.18/sjk-plus-0.18.jar"
+  sha256 "583284174dde91c32acdef01550d376b2e15be8573e5d99007651451f5bc8854"
   license "Apache-2.0"
 
   bottle do
