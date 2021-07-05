76f6b5761ee3a651a29514f675fe0a49e755fb64
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-05 20:40:10 +0000

    nettle: update 3.7.3 bottle.

--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -7,10 +7,11 @@ class Nettle < Formula
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3cd41002e6358b07ca506ed09fb07473d61c1c2717b630f8d708c74ab5d06704"
-    sha256 cellar: :any, big_sur:       "80fa0a047c3d08ccb47c8820a566d187365e8ea75e0cdf903ef0653d3aa3bb67"
-    sha256 cellar: :any, catalina:      "f1f1c41bf3dadabc748a34bba26b8771e4e36ae0815be4a83d1d317d90fa3c2e"
-    sha256 cellar: :any, mojave:        "f7900666b8d57164a5e770008390ea3fe2c519941e3f28daba9ef91dad4e5e69"
+    sha256 cellar: :any,                 arm64_big_sur: "3cd41002e6358b07ca506ed09fb07473d61c1c2717b630f8d708c74ab5d06704"
+    sha256 cellar: :any,                 big_sur:       "80fa0a047c3d08ccb47c8820a566d187365e8ea75e0cdf903ef0653d3aa3bb67"
+    sha256 cellar: :any,                 catalina:      "f1f1c41bf3dadabc748a34bba26b8771e4e36ae0815be4a83d1d317d90fa3c2e"
+    sha256 cellar: :any,                 mojave:        "f7900666b8d57164a5e770008390ea3fe2c519941e3f28daba9ef91dad4e5e69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7c56819e2bc18baf900853311685108aab2cda8fc4963ca095ba893235ef1cb"
   end
 
   depends_on "gmp"
