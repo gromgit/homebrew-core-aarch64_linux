ed9b0bd426ab52ddd751f86d82298c797880f75e
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-05 08:14:04 +0000

    libcap: add 2.27 bottle.

--- Formula/libcap.rb
+++ Formula/libcap.rb
@@ -5,6 +5,10 @@ class Libcap < Formula
   sha256 "dac1792d0118bee6aae6ba7fb93ff1602c6a9bda812fd63916eee1435b9c486a"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c32acc483373e3079418a9fd64e7a1826caa82184b51a5294038cb4feed79418"
+  end
+
   depends_on :linux
 
   def install
