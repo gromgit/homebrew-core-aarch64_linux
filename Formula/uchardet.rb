08de2c30a258d544c729be574da7be08b19f3c5f
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-05 11:13:31 +0000

    uchardet: update 0.0.7 bottle.

--- Formula/uchardet.rb
+++ Formula/uchardet.rb
@@ -6,11 +6,12 @@ class Uchardet < Formula
   head "https://gitlab.freedesktop.org/uchardet/uchardet.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "33989aa08f1ffc6f71f536c2a3cb28aabcc774e90da2aa40db110048f4b38238"
-    sha256 cellar: :any, big_sur:       "f5125cd193034470e278536d8b5d2498e99ddbd5c2853e214fe24fe845cc56d7"
-    sha256 cellar: :any, catalina:      "34bd4791834a762da961de136a3cad253800a98deea2d1dededf8efb465f215d"
-    sha256 cellar: :any, mojave:        "a254139ee777de77d5907dfeb47d4306f2e5e5f0b3775edfedcbf1fcf217fdbe"
-    sha256 cellar: :any, high_sierra:   "a89237dd88e4190826ce6fb4a8a625711636db7d6c4ba10d5ddd56d37347e868"
+    sha256 cellar: :any,                 arm64_big_sur: "33989aa08f1ffc6f71f536c2a3cb28aabcc774e90da2aa40db110048f4b38238"
+    sha256 cellar: :any,                 big_sur:       "f5125cd193034470e278536d8b5d2498e99ddbd5c2853e214fe24fe845cc56d7"
+    sha256 cellar: :any,                 catalina:      "34bd4791834a762da961de136a3cad253800a98deea2d1dededf8efb465f215d"
+    sha256 cellar: :any,                 mojave:        "a254139ee777de77d5907dfeb47d4306f2e5e5f0b3775edfedcbf1fcf217fdbe"
+    sha256 cellar: :any,                 high_sierra:   "a89237dd88e4190826ce6fb4a8a625711636db7d6c4ba10d5ddd56d37347e868"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a85799a4ea86198a27dcd64601a9bf3445baa3dca32762ca6616c9c78a79583"
   end
 
   depends_on "cmake" => :build
