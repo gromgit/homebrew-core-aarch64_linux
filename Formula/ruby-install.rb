834268ace9b57db83423a572fcab987e8da64e94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-05 06:09:13 +0000

    ruby-install: update 0.8.2 bottle.

--- Formula/ruby-install.rb
+++ Formula/ruby-install.rb
@@ -7,10 +7,10 @@ class RubyInstall < Formula
   head "https://github.com/postmodern/ruby-install.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2bebe2b7b3f62bf84c92a2e7f13a30be214cb7dc0a849479102c1826e7974fcc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a7f9e33147703ddd362a07bb361f381b99ff5d936b1102108812ed08c4ccf386"
-    sha256 cellar: :any_skip_relocation, catalina:      "0116171fc93d09c4893e7b88e8202b641f028f7e74817aff8f44952f792ad8b0"
-    sha256 cellar: :any_skip_relocation, mojave:        "6ab943b1a3882cb2e81099dc9870e364dcbbd4d40685227f2b6a89fcc37e896c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d19d4a89f08d447d522387e362b5e4627de5438e06396e2035fe735dc192d7de"
+    sha256 cellar: :any_skip_relocation, big_sur:       "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
+    sha256 cellar: :any_skip_relocation, catalina:      "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
+    sha256 cellar: :any_skip_relocation, mojave:        "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
   end
 
   def install
