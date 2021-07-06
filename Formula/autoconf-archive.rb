89d088543c31b3c796cf5818af190459ed54065d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 03:00:58 +0000

    autoconf-archive: update 2021.02.19 bottle.

--- Formula/autoconf-archive.rb
+++ Formula/autoconf-archive.rb
@@ -11,6 +11,7 @@ class AutoconfArchive < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c86fcd19fecc5c52847daebba5f81c768eb2d1c3f80cc3f8bee7ef1d0eb6e08c"
     sha256 cellar: :any_skip_relocation, catalina:      "5eebb7b244cba512dc9ebb16d6ec6743d7c3859079af58213027adbdad6807de"
     sha256 cellar: :any_skip_relocation, mojave:        "d2957cc212b85b0627cfb70760d7ab7d18dd984d838f9b62f8267b09220831ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7534edb1ee8fc202616422b29a63001f4b126c3af117759d13cf4d84b20ef580"
   end
 
   # autoconf-archive is useless without autoconf
