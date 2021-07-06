39a6efedd82e8dbef24e19e1d9d3099d6fae2398
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-06 22:47:55 +0000

    zssh: update 1.5c bottle.

--- Formula/zssh.rb
+++ Formula/zssh.rb
@@ -11,12 +11,13 @@ class Zssh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, catalina:    "6b9bce24c13dd2e979cdae57892e1b595bfcbd1d342bb81419dda378b8439495"
-    sha256 cellar: :any_skip_relocation, mojave:      "0b1567c1d4aef681ff463f058a884eead039fb0c50a1c03820a03c9f67786b52"
-    sha256 cellar: :any_skip_relocation, high_sierra: "9cb26f1bd359977406fae945abd311b2cdc5770570e6350f2ac278bfbe458f5b"
-    sha256 cellar: :any_skip_relocation, sierra:      "49e01bb86097999f21f3d96b0f9cd63a975d4fd52f6e286d42ceee16ee996eb7"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "04212f19c1d9a6b97fd56ffe937606f1779849fdf04b93e3f285889599845c8f"
-    sha256 cellar: :any_skip_relocation, yosemite:    "94280569f9e1c1deb9d8c3be4256cd501399fd51758f8e2ea6d77fd9f1b6ef2e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9b567d08589f3ba595994ca4906bfde9fe2651ef246bbd872f332e9cc8993a60"
+    sha256 cellar: :any_skip_relocation, catalina:      "6b9bce24c13dd2e979cdae57892e1b595bfcbd1d342bb81419dda378b8439495"
+    sha256 cellar: :any_skip_relocation, mojave:        "0b1567c1d4aef681ff463f058a884eead039fb0c50a1c03820a03c9f67786b52"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "9cb26f1bd359977406fae945abd311b2cdc5770570e6350f2ac278bfbe458f5b"
+    sha256 cellar: :any_skip_relocation, sierra:        "49e01bb86097999f21f3d96b0f9cd63a975d4fd52f6e286d42ceee16ee996eb7"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "04212f19c1d9a6b97fd56ffe937606f1779849fdf04b93e3f285889599845c8f"
+    sha256 cellar: :any_skip_relocation, yosemite:      "94280569f9e1c1deb9d8c3be4256cd501399fd51758f8e2ea6d77fd9f1b6ef2e"
   end
 
   depends_on "autoconf" => :build
