6be3c38cb50969d7424bff133f562b023c548aff
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 01:08:16 +0000

    double-conversion: update 3.1.5_1 bottle.

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -8,11 +8,12 @@ class DoubleConversion < Formula
   head "https://github.com/google/double-conversion.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "928fbd4a31967ec090b4b292b1a212fec7eb75f27443493d0c175ca8bb56a9dc"
-    sha256 cellar: :any, big_sur:       "0f7c08daace9fc854f8526a7699102f40de9898fa1e6b05a0199b5da3c9e1a7d"
-    sha256 cellar: :any, catalina:      "20b93e20891d48912ffbfbdf3ef470f7305684df2381ef93056a11cedd95c65f"
-    sha256 cellar: :any, mojave:        "ec700c89a4f1794170b4466f5a0a100b6eafee7cb0a794e55ea53de18114a1d3"
-    sha256 cellar: :any, high_sierra:   "9b54153b09683b8fa40160588792385e04f6be56ba355c5a530a2209b9f0526d"
+    sha256 cellar: :any,                 arm64_big_sur: "928fbd4a31967ec090b4b292b1a212fec7eb75f27443493d0c175ca8bb56a9dc"
+    sha256 cellar: :any,                 big_sur:       "0f7c08daace9fc854f8526a7699102f40de9898fa1e6b05a0199b5da3c9e1a7d"
+    sha256 cellar: :any,                 catalina:      "20b93e20891d48912ffbfbdf3ef470f7305684df2381ef93056a11cedd95c65f"
+    sha256 cellar: :any,                 mojave:        "ec700c89a4f1794170b4466f5a0a100b6eafee7cb0a794e55ea53de18114a1d3"
+    sha256 cellar: :any,                 high_sierra:   "9b54153b09683b8fa40160588792385e04f6be56ba355c5a530a2209b9f0526d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2796bb25c0134e6c05a40d43f8b417af7ba8d5216838c6e980b429c685d1793"
   end
 
   depends_on "cmake" => :build
