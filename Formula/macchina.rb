8041e57ac19d1e0caea1bf4da5c98ed8d0195de2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 05:51:02 +0000

    macchina: update 0.9.2 bottle.

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -10,6 +10,7 @@ class Macchina < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "07783179d4fa09142731337bfc4cf053b64daf6d1062fa9b822d7cf5ab4af401"
     sha256 cellar: :any_skip_relocation, catalina:      "556670ff3ebd99d5ba897cb634104a98b74e3c23aad6e99113bb7c08a130da2f"
     sha256 cellar: :any_skip_relocation, mojave:        "163c3c266eb2112acc0535827ca55f73467a59ca9e68f808805bebd360773760"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4186da39adb2e09bfe11ae343099a6abdb656610e1a45f8cd39f27ea8000ddc9"
   end
 
   depends_on "rust" => :build
