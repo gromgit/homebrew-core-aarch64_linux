05c4360851f3391e903a050729643d6dbe65ead1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 08:26:50 +0000

    molecule: update 3.3.4 bottle.

--- Formula/molecule.rb
+++ Formula/molecule.rb
@@ -8,10 +8,11 @@ class Molecule < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c105d19533a2d86d905b055cfa8ca1d23a28ab663fafe0d2ccbee57f3f21c52b"
-    sha256 cellar: :any, big_sur:       "3f57acb83ba22d358afebf9c737112bc50ecca2e590805fdb7833982b103eeeb"
-    sha256 cellar: :any, catalina:      "149f16037510bf1e2c2c7094750c6880308e7ce41ec9ab5ae8560c54f9ea2ee1"
-    sha256 cellar: :any, mojave:        "e9a863066e28643df1351de7006a5f03bc1d349c246d0e8e724ccd4742182d63"
+    sha256 cellar: :any,                 arm64_big_sur: "c105d19533a2d86d905b055cfa8ca1d23a28ab663fafe0d2ccbee57f3f21c52b"
+    sha256 cellar: :any,                 big_sur:       "3f57acb83ba22d358afebf9c737112bc50ecca2e590805fdb7833982b103eeeb"
+    sha256 cellar: :any,                 catalina:      "149f16037510bf1e2c2c7094750c6880308e7ce41ec9ab5ae8560c54f9ea2ee1"
+    sha256 cellar: :any,                 mojave:        "e9a863066e28643df1351de7006a5f03bc1d349c246d0e8e724ccd4742182d63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9eba16c6d88f82aafb925df6bb582472b521a18426520b6f95b44e70d7ec3ced"
   end
 
   depends_on "rust" => :build
