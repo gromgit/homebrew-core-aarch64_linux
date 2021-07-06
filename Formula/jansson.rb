3b1e8fe254882efc14861df0fd2857a30f04d7fa
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 01:38:17 +0000

    jansson: update 2.13.1 bottle.

--- Formula/jansson.rb
+++ Formula/jansson.rb
@@ -11,11 +11,12 @@ class Jansson < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8e2f0225f8aa78bf75a1c61c8ee85e7dbd0292ca0aa9f186c9da960cdeb2b85a"
-    sha256 cellar: :any, big_sur:       "7f9717b979d7d66db054be03cfebb72e088e413b5d6125c7cd735db064897671"
-    sha256 cellar: :any, catalina:      "e6a942f77821fd65810d4bc20e6938364a5e40cd7c8510c4b090731573bd0088"
-    sha256 cellar: :any, mojave:        "587acdadd1ea8bcf22c316f55a32084f530280a7e24f0864e0e420718d0d1b7f"
-    sha256 cellar: :any, high_sierra:   "38085c147eb40d58df8a91a44e7544d4ceb248aa25f54bdd8a3b10c1a214d9e9"
+    sha256 cellar: :any,                 arm64_big_sur: "8e2f0225f8aa78bf75a1c61c8ee85e7dbd0292ca0aa9f186c9da960cdeb2b85a"
+    sha256 cellar: :any,                 big_sur:       "7f9717b979d7d66db054be03cfebb72e088e413b5d6125c7cd735db064897671"
+    sha256 cellar: :any,                 catalina:      "e6a942f77821fd65810d4bc20e6938364a5e40cd7c8510c4b090731573bd0088"
+    sha256 cellar: :any,                 mojave:        "587acdadd1ea8bcf22c316f55a32084f530280a7e24f0864e0e420718d0d1b7f"
+    sha256 cellar: :any,                 high_sierra:   "38085c147eb40d58df8a91a44e7544d4ceb248aa25f54bdd8a3b10c1a214d9e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68f0ccce7120f3a62dec4886fa286cab0bda3a126608f707cf1732662442c1a0"
   end
 
   def install
