24ee822d1e5db9eb38e0240d63e4f4bc397590b0
Michihito Shigemura <i.am.shige@gmail.com>
2021-07-06 23:34:23 +0900

    jenkins 2.301
    Closes #80650.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins.io/"
-  url "http://mirrors.jenkins.io/war/2.300/jenkins.war"
-  sha256 "2f6aa548373b038af4fb6a4d6eaa5d13679510008f1712532732bf77c55b9670"
+  url "http://mirrors.jenkins.io/war/2.301/jenkins.war"
+  sha256 "de0bb6df28721cf8177fe0a6166cf8d6d451e30adb013988e341d8e42e9a0130"
   license "MIT"
 
   livecheck do
