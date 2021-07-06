3e32e9c82cd026806c16ebcdd69d12f8e95acc26
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 23:50:06 +0000

    akamai: update 1.2.1 bottle.

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -6,9 +6,10 @@ class Akamai < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "842b169770e4426d42f8b5263d6f9b0dea954f9f02a682c66faeae73f30646c2"
-    sha256 cellar: :any_skip_relocation, catalina: "6ec819f683c492b61fb18fad43515d7ce85c4c00d80bcc6ff3b2cc6ef098bf3f"
-    sha256 cellar: :any_skip_relocation, mojave:   "9a34412f05ecbbae48a65983548495cd0ebc5ad234f358163f714accfd44091a"
+    sha256 cellar: :any_skip_relocation, big_sur:      "842b169770e4426d42f8b5263d6f9b0dea954f9f02a682c66faeae73f30646c2"
+    sha256 cellar: :any_skip_relocation, catalina:     "6ec819f683c492b61fb18fad43515d7ce85c4c00d80bcc6ff3b2cc6ef098bf3f"
+    sha256 cellar: :any_skip_relocation, mojave:       "9a34412f05ecbbae48a65983548495cd0ebc5ad234f358163f714accfd44091a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "273809b294a7cd451de7aac6a324e548105a97697677ef201590274998044ff4"
   end
 
   depends_on "go" => :build
