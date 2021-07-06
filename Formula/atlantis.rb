6899a476d34f44b112b9bdab99c965cf45e50af2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 23:58:52 +0000

    atlantis: update 0.17.2 bottle.

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -11,6 +11,7 @@ class Atlantis < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b97db8a025b7d52cd5d01f27506f44f922453b26995b7282e95526f74f027c69"
     sha256 cellar: :any_skip_relocation, catalina:      "ab59468b03fe78a253123c3886887b66c4c19c4b30599c4f899be0019bf836da"
     sha256 cellar: :any_skip_relocation, mojave:        "9ca8bcd80bb64544b6ca731a248a42b101b5e98cfb6931c55e46d35c89402646"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f9e89a7a52f1df0b884f40caf8a333959cfb6f16c66cb57f9cbb565fc2a31f1"
   end
 
   depends_on "go" => :build
