e07e5e7e3c2a9662144bf7c98494f27949799e03
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 23:48:48 +0000

    act: update 0.2.23 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -11,6 +11,7 @@ class Act < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a103cc9bf8400f08024dd4480ab6ea9f6997547f68284840c07786a98105aac3"
     sha256 cellar: :any_skip_relocation, catalina:      "71977c4592e29f6815027c79ea59b9faf62988364458f7daafb4dbff5c5e7b8f"
     sha256 cellar: :any_skip_relocation, mojave:        "66e0b453b7422e694d1085bd66a557c4a25999452f2ca805959fa6efda769132"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12fe917fd161712dd0bdeb91c0c82765a8c62de43e752b6d260cfc74c6bceedf"
   end
 
   depends_on "go" => :build
