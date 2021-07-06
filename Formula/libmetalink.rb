5a198700547eb9cf8a89cd26f3a5b9fb1afd78f1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 00:59:06 +0000

    libmetalink: update 0.1.3 bottle.

--- Formula/libmetalink.rb
+++ Formula/libmetalink.rb
@@ -11,14 +11,15 @@ class Libmetalink < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a5eec14410ca7f2ea831d91c273d6372bc851f930944d207f07f35245b077a91"
-    sha256 cellar: :any, big_sur:       "ec4a4a8f898432cccfa2198c0e877daf2d80c2ab5bc92a7a9134a3dd75529a87"
-    sha256 cellar: :any, catalina:      "a18e6199b4d775a32693aa38b80551d547eb1de58c5d7c788f706276ce296d5e"
-    sha256 cellar: :any, mojave:        "461d42fab1dd15b7df8144ef814a3b589c5be9177728b522ea73e6c52bec660f"
-    sha256 cellar: :any, high_sierra:   "037c9d82da0a2c77031515cc8618b28552c526e559b3d9cebb0f054cd66204e3"
-    sha256 cellar: :any, sierra:        "87711da9c52d75d2d6fed5829a70b0f6561f55fdcf1129266001b6e96bc69b1b"
-    sha256 cellar: :any, el_capitan:    "4ca3f82ac2e2520c677def661168c6ba14ee2b8f0139fbe33cf5be66745244d3"
-    sha256 cellar: :any, yosemite:      "e627cde406e135a735fd2586622d2fa05cf6ef9fda8a0ea08d09ddfce20dca75"
+    sha256 cellar: :any,                 arm64_big_sur: "a5eec14410ca7f2ea831d91c273d6372bc851f930944d207f07f35245b077a91"
+    sha256 cellar: :any,                 big_sur:       "ec4a4a8f898432cccfa2198c0e877daf2d80c2ab5bc92a7a9134a3dd75529a87"
+    sha256 cellar: :any,                 catalina:      "a18e6199b4d775a32693aa38b80551d547eb1de58c5d7c788f706276ce296d5e"
+    sha256 cellar: :any,                 mojave:        "461d42fab1dd15b7df8144ef814a3b589c5be9177728b522ea73e6c52bec660f"
+    sha256 cellar: :any,                 high_sierra:   "037c9d82da0a2c77031515cc8618b28552c526e559b3d9cebb0f054cd66204e3"
+    sha256 cellar: :any,                 sierra:        "87711da9c52d75d2d6fed5829a70b0f6561f55fdcf1129266001b6e96bc69b1b"
+    sha256 cellar: :any,                 el_capitan:    "4ca3f82ac2e2520c677def661168c6ba14ee2b8f0139fbe33cf5be66745244d3"
+    sha256 cellar: :any,                 yosemite:      "e627cde406e135a735fd2586622d2fa05cf6ef9fda8a0ea08d09ddfce20dca75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "583e662593473f7310e1c3e3bed77a89a4dc43aed5fd1ce6a12a7260ec69c5f5"
   end
 
   depends_on "pkg-config" => :build
