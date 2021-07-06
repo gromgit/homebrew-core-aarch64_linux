139462b3e605ac305a2b318c75a9257235acbb16
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 02:58:55 +0000

    libbluray: update 1.3.0 bottle.

--- Formula/libbluray.rb
+++ Formula/libbluray.rb
@@ -11,10 +11,11 @@ class Libbluray < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "fb78d5e9950654e6f5da0e1cec9543decf00f7b5c17d9dce898981badd065073"
-    sha256 cellar: :any, big_sur:       "c5bcd42c46908cfed9df378bf73b25a23482dbb6676bec1322fc902f51aad07c"
-    sha256 cellar: :any, catalina:      "acfe4417abf3c5169e7271e81043fd4fb6d26bcd2ea96a266b820c33e8492ac6"
-    sha256 cellar: :any, mojave:        "1321b4dba202cd31bd086cab15545128c8f00081378aa5e03598e54774f9a3e7"
+    sha256 cellar: :any,                 arm64_big_sur: "fb78d5e9950654e6f5da0e1cec9543decf00f7b5c17d9dce898981badd065073"
+    sha256 cellar: :any,                 big_sur:       "c5bcd42c46908cfed9df378bf73b25a23482dbb6676bec1322fc902f51aad07c"
+    sha256 cellar: :any,                 catalina:      "acfe4417abf3c5169e7271e81043fd4fb6d26bcd2ea96a266b820c33e8492ac6"
+    sha256 cellar: :any,                 mojave:        "1321b4dba202cd31bd086cab15545128c8f00081378aa5e03598e54774f9a3e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99d67c7d44abccfda5de4ea53b8393f006b39f9fb55138eae6cb04da78f7a854"
   end
 
   head do
