5e1af445004360c40bd832a04985693b3538f4e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-06 22:05:59 +0000

    croaring: update 0.3.2 bottle.

--- Formula/croaring.rb
+++ Formula/croaring.rb
@@ -7,10 +7,10 @@ class Croaring < Formula
   head "https://github.com/RoaringBitmap/CRoaring.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6b9b5a2a974922065b6532dee72f1d0eef18d0acc974d140b341c357c6d22e68"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0fb31a81cdf29a48b64c2145e22a7807fbeef260b90ebfb33b69792e600d1a91"
-    sha256 cellar: :any_skip_relocation, catalina:      "876be0d01194b15d750d331d6aa3ca38600e0c027892bd665417cd8e9ae5ebf0"
-    sha256 cellar: :any_skip_relocation, mojave:        "1e3f2c5daacd9fa4a9895b4cd989b5d3443884e6db0a854491759c5464a1ae1e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "03e6dad49603c022c982f4de466e0559822d4b405b7adb6d658592b35712efaf"
+    sha256 cellar: :any_skip_relocation, big_sur:       "14ee0bb4fe410a81bebaccd0d40f62d5ce63a5134e77fdc0df0d5ffbfd57e5bf"
+    sha256 cellar: :any_skip_relocation, catalina:      "0d6b6f08d97d12a6cbe43b1a8f3e451fd752de4d5742a306ffca72f82a4b800f"
+    sha256 cellar: :any_skip_relocation, mojave:        "85fbb6b99044964af0d54f4ae89089d65d57c05d500f9601be0b18188be2f18e"
   end
 
   depends_on "cmake" => :build
