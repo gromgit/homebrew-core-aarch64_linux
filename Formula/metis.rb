87737b5c084aecca7f7032472a07bae325207e48
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 00:42:09 +0000

    metis: update 5.1.0 bottle.

--- Formula/metis.rb
+++ Formula/metis.rb
@@ -10,14 +10,15 @@ class Metis < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ea93856908a2c1c60023dd2f849339d479b20ab4ae6d51623f9496f64993ca20"
-    sha256 cellar: :any, big_sur:       "bca0197271b673ba235c37334494b47250c9732e9a0164d8ee79948fc3cd4308"
-    sha256 cellar: :any, catalina:      "b410b124973bf31beb58806d4050b8dda1fb3dca679fc3443514025200fd4a37"
-    sha256 cellar: :any, mojave:        "f3cdcf0cc5af4ddd27a4550d4a73cffcb34058fe34604b09d453610460d24465"
-    sha256 cellar: :any, high_sierra:   "88b6965d941a87044150238387971c4bb94ed2ffca327affccaf311d666a2b4b"
-    sha256 cellar: :any, sierra:        "9c8deed80ece8c24e7ebccbce8410557b27afe711d3f59fccb7d781254d0cc34"
-    sha256 cellar: :any, el_capitan:    "54f75262475744bc6ad3ba66ac801e03c18bbac00a9bcf0ca9d05853f2022498"
-    sha256 cellar: :any, yosemite:      "b33c2fc2c8a1cdb9f48faf41201bdc6384090b8dbd6ed3eecd05264eb6431c0b"
+    sha256 cellar: :any,                 arm64_big_sur: "ea93856908a2c1c60023dd2f849339d479b20ab4ae6d51623f9496f64993ca20"
+    sha256 cellar: :any,                 big_sur:       "bca0197271b673ba235c37334494b47250c9732e9a0164d8ee79948fc3cd4308"
+    sha256 cellar: :any,                 catalina:      "b410b124973bf31beb58806d4050b8dda1fb3dca679fc3443514025200fd4a37"
+    sha256 cellar: :any,                 mojave:        "f3cdcf0cc5af4ddd27a4550d4a73cffcb34058fe34604b09d453610460d24465"
+    sha256 cellar: :any,                 high_sierra:   "88b6965d941a87044150238387971c4bb94ed2ffca327affccaf311d666a2b4b"
+    sha256 cellar: :any,                 sierra:        "9c8deed80ece8c24e7ebccbce8410557b27afe711d3f59fccb7d781254d0cc34"
+    sha256 cellar: :any,                 el_capitan:    "54f75262475744bc6ad3ba66ac801e03c18bbac00a9bcf0ca9d05853f2022498"
+    sha256 cellar: :any,                 yosemite:      "b33c2fc2c8a1cdb9f48faf41201bdc6384090b8dbd6ed3eecd05264eb6431c0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8daac6acbeaad9583ff26d72a8ad440ac41efb8656973213902bdfe66cd61e6"
   end
 
   depends_on "cmake" => :build
