d2e60dffeaad2d7f6af57eca59708ba616a2c9f7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 02:08:52 +0000

    aws-iam-authenticator: update 0.5.3 bottle.

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -12,6 +12,7 @@ class AwsIamAuthenticator < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a161d4ea3ef00ab85c8f0cea198e0dafde39cd3a76359b75ea22521c35eac7d6"
     sha256 cellar: :any_skip_relocation, catalina:      "192ace97a0c76ed0aceda060290db6abcde5af8ad7dfc30faaf5f4ed4ae92b6a"
     sha256 cellar: :any_skip_relocation, mojave:        "c7f23ccd6bdd852a7c1928df437381204aa9da22a2f41eb3a5776864838f39db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6d0c278ef6d1d19a160035e5ff0039a16121be7107acce9bfa55ab59054c2f4"
   end
 
   depends_on "go" => :build
