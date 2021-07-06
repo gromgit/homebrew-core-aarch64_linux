41e8651f9794d4e81a1b6bf5d8c5c837a279002a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 01:27:31 +0000

    tree-sitter: update 0.20.0 bottle.

--- Formula/tree-sitter.rb
+++ Formula/tree-sitter.rb
@@ -9,10 +9,11 @@ class TreeSitter < Formula
   head "https://github.com/tree-sitter/tree-sitter.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "cde3ebc240632b22ebb43e06e5a01016f1e90e77e740ef3df0ce0bfcc9d5d3ca"
-    sha256 cellar: :any, big_sur:       "5b790b15e898a45d27aadd9e513f46aef3c0de96dce110eced8ffc4bef37af37"
-    sha256 cellar: :any, catalina:      "11d66cc7ce50df263c5acae45b334520c7a3314422da3c7050ff9c511f860196"
-    sha256 cellar: :any, mojave:        "1bf537d6e22c72586f41cc75cb0f9a496243c5daab3f406f4849e69851ba09fd"
+    sha256 cellar: :any,                 arm64_big_sur: "cde3ebc240632b22ebb43e06e5a01016f1e90e77e740ef3df0ce0bfcc9d5d3ca"
+    sha256 cellar: :any,                 big_sur:       "5b790b15e898a45d27aadd9e513f46aef3c0de96dce110eced8ffc4bef37af37"
+    sha256 cellar: :any,                 catalina:      "11d66cc7ce50df263c5acae45b334520c7a3314422da3c7050ff9c511f860196"
+    sha256 cellar: :any,                 mojave:        "1bf537d6e22c72586f41cc75cb0f9a496243c5daab3f406f4849e69851ba09fd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d423bc3d4053a04da444b9f4de12a8d93c5b3e277616f225261fe7a91c2efd1a"
   end
 
   depends_on "node" => [:build, :test]
