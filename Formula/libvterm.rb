a218204c6591a31f0b5ea3111ee85ddded95f37c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-06 01:38:55 +0000

    libvterm: update 0.1.4 bottle.

--- Formula/libvterm.rb
+++ Formula/libvterm.rb
@@ -12,11 +12,12 @@ class Libvterm < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "969c3ccfed1e025aeb191eff20c8942ff54358553c3501bfb270973ec5a00bb0"
-    sha256 cellar: :any, big_sur:       "73b78f138e6575528d7f197f63897be167c712698744c5e11852a9e976e8d1d5"
-    sha256 cellar: :any, catalina:      "b62a78631bca9a723eb25dd924853ced974718df0847820c7c38f7f0d7fdc43c"
-    sha256 cellar: :any, mojave:        "56946cfa43a7bcf3b47086d61541b2a0541d636a362c788401f51b29cf6fa35f"
-    sha256 cellar: :any, high_sierra:   "518299bd4bde4aeb3063df624e4c474280ac15a8e65f612059c7d03717b143ac"
+    sha256 cellar: :any,                 arm64_big_sur: "969c3ccfed1e025aeb191eff20c8942ff54358553c3501bfb270973ec5a00bb0"
+    sha256 cellar: :any,                 big_sur:       "73b78f138e6575528d7f197f63897be167c712698744c5e11852a9e976e8d1d5"
+    sha256 cellar: :any,                 catalina:      "b62a78631bca9a723eb25dd924853ced974718df0847820c7c38f7f0d7fdc43c"
+    sha256 cellar: :any,                 mojave:        "56946cfa43a7bcf3b47086d61541b2a0541d636a362c788401f51b29cf6fa35f"
+    sha256 cellar: :any,                 high_sierra:   "518299bd4bde4aeb3063df624e4c474280ac15a8e65f612059c7d03717b143ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c133736c1d86744e12c3e4b8ee7ab373937de1daeabc9ffec4a35dce023dfea8"
   end
 
   depends_on "libtool" => :build
