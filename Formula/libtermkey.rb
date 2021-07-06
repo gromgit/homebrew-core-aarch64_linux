7d0c256965eeb17b8189ba6aa2cf849c9dbb6037
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-06 01:42:14 +0000

    libtermkey: update 0.22 bottle.

--- Formula/libtermkey.rb
+++ Formula/libtermkey.rb
@@ -11,12 +11,13 @@ class Libtermkey < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a9e13d645b016b670bb0d9c79928f5344928685f7d358480210f1af8729480e6"
-    sha256 cellar: :any, big_sur:       "afaf585bd31e32a5fd01a5c16a4c37af9e38524b72e03a76b61038c60f2ed113"
-    sha256 cellar: :any, catalina:      "293f71f6cb8498f734910ada7ffe4e8e2ea2cb6121455318053d2a5951b272a8"
-    sha256 cellar: :any, mojave:        "efa6360ccb50275ee143410e57b4ff47b2d6bafd97d6f4feeb3cb3ee02050a2c"
-    sha256 cellar: :any, high_sierra:   "e3b848de428f811c1879530c043145152bf4b0e599ed642aa2845904d13f7081"
-    sha256 cellar: :any, sierra:        "3f8ce77603619d85de7127f317e276c0cd38a461c545cafeb7c875e7c89fe467"
+    sha256 cellar: :any,                 arm64_big_sur: "a9e13d645b016b670bb0d9c79928f5344928685f7d358480210f1af8729480e6"
+    sha256 cellar: :any,                 big_sur:       "afaf585bd31e32a5fd01a5c16a4c37af9e38524b72e03a76b61038c60f2ed113"
+    sha256 cellar: :any,                 catalina:      "293f71f6cb8498f734910ada7ffe4e8e2ea2cb6121455318053d2a5951b272a8"
+    sha256 cellar: :any,                 mojave:        "efa6360ccb50275ee143410e57b4ff47b2d6bafd97d6f4feeb3cb3ee02050a2c"
+    sha256 cellar: :any,                 high_sierra:   "e3b848de428f811c1879530c043145152bf4b0e599ed642aa2845904d13f7081"
+    sha256 cellar: :any,                 sierra:        "3f8ce77603619d85de7127f317e276c0cd38a461c545cafeb7c875e7c89fe467"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9ac2c7fc0a6df84b0743233d1ab68984bc398b2c0550a4c61ab01ddfd9bd665"
   end
 
   depends_on "libtool" => :build
