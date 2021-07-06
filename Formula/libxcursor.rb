1be3a2e59e08e1417f3d416d8a7b2e9c2cf729a8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 05:50:39 +0000

    libxcursor: update 1.2.0 bottle.

--- Formula/libxcursor.rb
+++ Formula/libxcursor.rb
@@ -6,11 +6,12 @@ class Libxcursor < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ddae5ae0c6cad51f281edd9908df4752a16ee6934a0e20f4d53e06b6e6cb07f3"
-    sha256 cellar: :any, big_sur:       "ffaf742cbe0cf49e80345942765c872093a222cf42ceaee7e139080883dc343b"
-    sha256 cellar: :any, catalina:      "c8c48a2d78f7a5d23c1a1322ceac267d1d50ae8b7f1ad93c7a8b0e510afdf39e"
-    sha256 cellar: :any, mojave:        "fe9c7b74829d92a0330caa49aeaf03d52213f2d8671fe8a4840b2700fa1cb863"
-    sha256 cellar: :any, high_sierra:   "ac83b3e1c320f65c0dd7d66ef03abb3a5163ee8cb4b8932e14f008ae4cce5126"
+    sha256 cellar: :any,                 arm64_big_sur: "ddae5ae0c6cad51f281edd9908df4752a16ee6934a0e20f4d53e06b6e6cb07f3"
+    sha256 cellar: :any,                 big_sur:       "ffaf742cbe0cf49e80345942765c872093a222cf42ceaee7e139080883dc343b"
+    sha256 cellar: :any,                 catalina:      "c8c48a2d78f7a5d23c1a1322ceac267d1d50ae8b7f1ad93c7a8b0e510afdf39e"
+    sha256 cellar: :any,                 mojave:        "fe9c7b74829d92a0330caa49aeaf03d52213f2d8671fe8a4840b2700fa1cb863"
+    sha256 cellar: :any,                 high_sierra:   "ac83b3e1c320f65c0dd7d66ef03abb3a5163ee8cb4b8932e14f008ae4cce5126"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "852a63bc502fb767d36fee2515dcc2787b29a9259d66bb3e30e63a8369d6a05c"
   end
 
   depends_on "pkg-config" => :build
