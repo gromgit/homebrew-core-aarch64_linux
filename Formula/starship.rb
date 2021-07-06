27e7e20943d24d98a3a65497dcc850ca48500693
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 02:04:34 +0000

    starship: update 0.55.0 bottle.

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -11,6 +11,7 @@ class Starship < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e64f01ffb5fa4ac17f9cc5a66ef3a23f8009701af186bdd3b713641250dc278d"
     sha256 cellar: :any_skip_relocation, catalina:      "1c1f66391875a83830b383691dbca271746acd08a7603102f2af6fdaf6b03b99"
     sha256 cellar: :any_skip_relocation, mojave:        "d1818c378fe6916aea8c58a0bbcda9877da07db26610079771269bfa58807e62"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b3318dd9618ffbde59dc0e83304352ee283df1c4882ca17091b990959dead702"
   end
 
   depends_on "rust" => :build
