582e51915a4556f5ebfd031e6e576d81399cedec
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 05:57:11 +0000

    zellij: update 0.14.0 bottle.

--- Formula/zellij.rb
+++ Formula/zellij.rb
@@ -10,6 +10,7 @@ class Zellij < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "4ee82bca930fcf51f290363466c590d4f1f7b596f6f45a39e132a3994f824cc0"
     sha256 cellar: :any_skip_relocation, catalina:      "0445bb8917a1a3f4853cbb441505a6b337d83230e9904a1b4864f31a0ecc8c89"
     sha256 cellar: :any_skip_relocation, mojave:        "cad649e3a7e036f926d7ea5a7fe949995100b99bc636931a9871d026729bd4ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58aa10c0563662a3a2c330442ca4a7948474a601999b881b0d6bad162a18ee08"
   end
 
   depends_on "rust" => :build
