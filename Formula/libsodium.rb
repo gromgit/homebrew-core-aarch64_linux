65b87404c8be234fbb534c98edc83785f24c5b1e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 00:24:31 +0000

    libsodium: update 1.0.18_1 bottle.

--- Formula/libsodium.rb
+++ Formula/libsodium.rb
@@ -12,11 +12,12 @@ class Libsodium < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ab7029c599665005a9c9ec9e72c74bf4d543fd7a995d9af9cfe9e6c10de79177"
-    sha256 cellar: :any, big_sur:       "5afc5678e30a174c1e46f1e905124f2619e6d9815ac776836090c0bff85631d6"
-    sha256 cellar: :any, catalina:      "db372521cd0b1861a5b578bee22426f3a1f4f7cb3c382be1f842da4715dc65bd"
-    sha256 cellar: :any, mojave:        "55245bfcf6654b0914d3f7459b99a08c54ef2560587bf583a1c1aff4cfc81f28"
-    sha256 cellar: :any, high_sierra:   "fc972755eb60f4221d7b32e58fc0f94e99b913fefefc84c4c76dc4bca1c5c445"
+    sha256 cellar: :any,                 arm64_big_sur: "ab7029c599665005a9c9ec9e72c74bf4d543fd7a995d9af9cfe9e6c10de79177"
+    sha256 cellar: :any,                 big_sur:       "5afc5678e30a174c1e46f1e905124f2619e6d9815ac776836090c0bff85631d6"
+    sha256 cellar: :any,                 catalina:      "db372521cd0b1861a5b578bee22426f3a1f4f7cb3c382be1f842da4715dc65bd"
+    sha256 cellar: :any,                 mojave:        "55245bfcf6654b0914d3f7459b99a08c54ef2560587bf583a1c1aff4cfc81f28"
+    sha256 cellar: :any,                 high_sierra:   "fc972755eb60f4221d7b32e58fc0f94e99b913fefefc84c4c76dc4bca1c5c445"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ab2c66fc8ae6c1245b49c9bd7a32853c1b348afe7086d4c2d3baf5ea30bbac9"
   end
 
   head do
