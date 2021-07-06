c7d130fac04c4a82652d0b2ff82a626f49ab2f67
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 02:11:39 +0000

    geoip: update 1.6.12 bottle.

--- Formula/geoip.rb
+++ Formula/geoip.rb
@@ -8,10 +8,11 @@ class Geoip < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "fd8787e3b88f91d97504992874d3fd358c6865e03fc19c5123b8e7f1a691f0dd"
-    sha256 cellar: :any, big_sur:       "a3bcb8d9c69277cb943fa30f00715325ee1c4f0c3e9aeb58e0c4a288f6705313"
-    sha256 cellar: :any, catalina:      "140477567aa37b0418e1976bdc5f75bc0f933405c157e7170ae3441ee3fc74aa"
-    sha256 cellar: :any, mojave:        "e10d69d8595116bdb562b0ede63d7bd04f606c66becd3c03fb1e6d06ba99a964"
+    sha256 cellar: :any,                 arm64_big_sur: "fd8787e3b88f91d97504992874d3fd358c6865e03fc19c5123b8e7f1a691f0dd"
+    sha256 cellar: :any,                 big_sur:       "a3bcb8d9c69277cb943fa30f00715325ee1c4f0c3e9aeb58e0c4a288f6705313"
+    sha256 cellar: :any,                 catalina:      "140477567aa37b0418e1976bdc5f75bc0f933405c157e7170ae3441ee3fc74aa"
+    sha256 cellar: :any,                 mojave:        "e10d69d8595116bdb562b0ede63d7bd04f606c66becd3c03fb1e6d06ba99a964"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb045793a66f36a2737a3bdc3c8513c5822702778c223123dceceff61e847080"
   end
 
   resource "database" do
