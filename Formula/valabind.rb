31efb2d95403b142bbded1c9ad3fda0ee813a49f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 02:58:20 +0000

    valabind: update 1.8.0_1 bottle.

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -8,10 +8,11 @@ class Valabind < Formula
   head "https://github.com/radare/valabind.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2e1304ff87e55fd7d61e3ee82a1f2d4faad720b4817adfa6f5c6398a2a4c90e3"
-    sha256 cellar: :any, big_sur:       "e6a255cbcfa3065cb00ebdb5b5a72e08d96f1152b749313441a9ab0b824629fd"
-    sha256 cellar: :any, catalina:      "59ac7361a4a4bb36beb102f172ac1a4cfe3ca1e611498fc574f0fc5a2b2f4b2b"
-    sha256 cellar: :any, mojave:        "18b32d4e6409d4a73d23f226f475fc1c41680d4f7f5da31e1e73cd70f4958816"
+    sha256 cellar: :any,                 arm64_big_sur: "2e1304ff87e55fd7d61e3ee82a1f2d4faad720b4817adfa6f5c6398a2a4c90e3"
+    sha256 cellar: :any,                 big_sur:       "e6a255cbcfa3065cb00ebdb5b5a72e08d96f1152b749313441a9ab0b824629fd"
+    sha256 cellar: :any,                 catalina:      "59ac7361a4a4bb36beb102f172ac1a4cfe3ca1e611498fc574f0fc5a2b2f4b2b"
+    sha256 cellar: :any,                 mojave:        "18b32d4e6409d4a73d23f226f475fc1c41680d4f7f5da31e1e73cd70f4958816"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e71d3941372ab742d382c9390e29903df16274789716a945df409e1f4f674a0"
   end
 
   depends_on "pkg-config" => :build
