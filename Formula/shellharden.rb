8b2432140555b44fbeafa394d32372197537f8fd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 05:18:59 +0000

    shellharden: update 4.1.2 bottle.

--- Formula/shellharden.rb
+++ Formula/shellharden.rb
@@ -11,6 +11,7 @@ class Shellharden < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "85dd6255ff5f3410eafff6f5689bbcb7feac3f3f5d291139fc60597e2f46e5a5"
     sha256 cellar: :any_skip_relocation, mojave:        "fbe947b5d0596fb32bcbadddd904bf0b0d30c64053e00c13a41cc6ecf89d1e20"
     sha256 cellar: :any_skip_relocation, high_sierra:   "214a9dca5d9d013ada81c97e204c96815fd3376399c960d73973ffe4c7d653cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d115462336b859f378a17b608fcc19a5bffd8d0d4b7755da90ac24081e461c3"
   end
 
   depends_on "rust" => :build
