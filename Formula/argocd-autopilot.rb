e96d91a863bf5ecd2810604770c9f727a2837dfb
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 23:57:58 +0000

    argocd-autopilot: update 0.2.9 bottle.

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -11,6 +11,7 @@ class ArgocdAutopilot < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "82441cb9df70b71984a36105de21fbb3e55d292f467d723fc560f89a1c3293b0"
     sha256 cellar: :any_skip_relocation, catalina:      "361af476778ddefe1b505bc5236abc805a08c9e234f30d7c50461700c5e92f20"
     sha256 cellar: :any_skip_relocation, mojave:        "73481fc46726bb632a4f6bad51ec4c4828b04069d3b23e8da9d2097c3916a1f0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "22260a578b40b38d1ccedde8b890a2c804f1dfe2bc3073ca6d77234131a2c721"
   end
 
   depends_on "go" => :build
