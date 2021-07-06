ab0c17fefd8d0b2d9d581ff0e328d45ea965a968
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 01:31:15 +0000

    jq: update 1.6 bottle.

--- Formula/jq.rb
+++ Formula/jq.rb
@@ -13,12 +13,13 @@ class Jq < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "674b3ae41c399f1e8e44c271b0e6909babff9fcd2e04a2127d25e2407ea4dd33"
-    sha256 cellar: :any, big_sur:       "bf0f8577632af7b878b6425476f5b1ab9c3bf66d65affb0c455048a173a0b6bf"
-    sha256 cellar: :any, catalina:      "820a3c85fcbb63088b160c7edf125d7e55fc2c5c1d51569304499c9cc4b89ce8"
-    sha256 cellar: :any, mojave:        "71f0e76c5b22e5088426c971d5e795fe67abee7af6c2c4ae0cf4c0eb98ed21ff"
-    sha256 cellar: :any, high_sierra:   "dffcffa4ea13e8f0f2b45c5121e529077e135ae9a47254c32182231662ee9b72"
-    sha256 cellar: :any, sierra:        "bb4d19dc026c2d72c53eed78eaa0ab982e9fcad2cd2acc6d13e7a12ff658e877"
+    sha256 cellar: :any,                 arm64_big_sur: "674b3ae41c399f1e8e44c271b0e6909babff9fcd2e04a2127d25e2407ea4dd33"
+    sha256 cellar: :any,                 big_sur:       "bf0f8577632af7b878b6425476f5b1ab9c3bf66d65affb0c455048a173a0b6bf"
+    sha256 cellar: :any,                 catalina:      "820a3c85fcbb63088b160c7edf125d7e55fc2c5c1d51569304499c9cc4b89ce8"
+    sha256 cellar: :any,                 mojave:        "71f0e76c5b22e5088426c971d5e795fe67abee7af6c2c4ae0cf4c0eb98ed21ff"
+    sha256 cellar: :any,                 high_sierra:   "dffcffa4ea13e8f0f2b45c5121e529077e135ae9a47254c32182231662ee9b72"
+    sha256 cellar: :any,                 sierra:        "bb4d19dc026c2d72c53eed78eaa0ab982e9fcad2cd2acc6d13e7a12ff658e877"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2beea2c2c372ccf1081e9a5233fc3020470803254284aeecc071249d76b62338"
   end
 
   head do
