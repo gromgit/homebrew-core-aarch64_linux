7893e84440bdb921b2eec44d9516bfb66fbdfc0a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 06:15:51 +0000

    pyflow: update 0.3.1 bottle.

--- Formula/pyflow.rb
+++ Formula/pyflow.rb
@@ -10,6 +10,7 @@ class Pyflow < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "619d1a1095428a7dc3ba1367c4ca5a64dd49eff8d5a91cbfd7e9966b9be2cc5a"
     sha256 cellar: :any_skip_relocation, catalina:      "2658cf76995468b62f16119d3b6288081a0ab95ca410bc445754ee67099567bd"
     sha256 cellar: :any_skip_relocation, mojave:        "f25d6af683f972dbdc918433965c7b589356fd4323766b1b778120ef5bdce6fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8f83652f4f35796ff4f85b3f81fbf8f6e1a380c8bd14b330d611fd02281ce27"
   end
 
   depends_on "rust" => :build
