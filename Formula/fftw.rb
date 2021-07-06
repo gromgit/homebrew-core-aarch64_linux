3b80deec2c4b2d78c7fa73e48a801cac488e5d2b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 08:18:16 +0000

    fftw: update 3.3.9_1 bottle.

--- Formula/fftw.rb
+++ Formula/fftw.rb
@@ -12,10 +12,11 @@ class Fftw < Formula
   end
 
   bottle do
-    sha256               arm64_big_sur: "f0bcc63e25061ac29e5d8f2700beab98e8bdf8e2bb428ceb8e77018e004d2473"
-    sha256 cellar: :any, big_sur:       "8ee0fe663966dcc2ba924768dc921536873b172b024302f1f06e663237d11a29"
-    sha256 cellar: :any, catalina:      "e5c826687292998daa2f2e76d13325fde551b54450846c3190efde540a02650e"
-    sha256 cellar: :any, mojave:        "17af7472492ccf0704b958db622505872d7bca0f2d2f05869d07f1b01557c0ab"
+    sha256                               arm64_big_sur: "f0bcc63e25061ac29e5d8f2700beab98e8bdf8e2bb428ceb8e77018e004d2473"
+    sha256 cellar: :any,                 big_sur:       "8ee0fe663966dcc2ba924768dc921536873b172b024302f1f06e663237d11a29"
+    sha256 cellar: :any,                 catalina:      "e5c826687292998daa2f2e76d13325fde551b54450846c3190efde540a02650e"
+    sha256 cellar: :any,                 mojave:        "17af7472492ccf0704b958db622505872d7bca0f2d2f05869d07f1b01557c0ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d8acd47e1deb1964882d56f086c31890b3fdbfa7da9699b6ef161dc983f620e"
   end
 
   depends_on "open-mpi"
