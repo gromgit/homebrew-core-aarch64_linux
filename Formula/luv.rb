56dc38c3d5dc7651e1852fcf565ed6403871b685
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-06 12:40:21 +0000

    luv: update 1.41.0-0 bottle.

--- Formula/luv.rb
+++ Formula/luv.rb
@@ -7,10 +7,11 @@ class Luv < Formula
   head "https://github.com/luvit/luv.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9d8f0a28026c84843af527dc31766bcfb89761f16ad12ad0eeeb6104514693b0"
-    sha256 cellar: :any, big_sur:       "08713383f4168d0104f123d1cad78bdeb843f26527a7d68c74aba555883c5b60"
-    sha256 cellar: :any, catalina:      "b3c644231c6c5d55f82770cf8d0db7649347ec28dc430f6e983f17b9c19febc9"
-    sha256 cellar: :any, mojave:        "a6cb63c24bba31847e55d04c4426eb84c6eaf24efa38ed55c7d0dd3ff800b32f"
+    sha256 cellar: :any,                 arm64_big_sur: "9d8f0a28026c84843af527dc31766bcfb89761f16ad12ad0eeeb6104514693b0"
+    sha256 cellar: :any,                 big_sur:       "08713383f4168d0104f123d1cad78bdeb843f26527a7d68c74aba555883c5b60"
+    sha256 cellar: :any,                 catalina:      "b3c644231c6c5d55f82770cf8d0db7649347ec28dc430f6e983f17b9c19febc9"
+    sha256 cellar: :any,                 mojave:        "a6cb63c24bba31847e55d04c4426eb84c6eaf24efa38ed55c7d0dd3ff800b32f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18bbcd99587b8e80e50a35002792ca7ad4322d087e897ae62ddeab915f91e290"
   end
 
   depends_on "cmake" => :build
