edd5cae315c6ceef17f9e1dd9f147bb71767b5ce
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 23:50:55 +0000

    aliddns: update 0.0.9 bottle.

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -12,6 +12,7 @@ class Aliddns < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "cb06193d5ebd8d4faa2e358f6d40589653a0a593e7265e21dfc15bfc99816cab"
     sha256 cellar: :any_skip_relocation, catalina:      "5bb2c74ba522eaf8713fb5c04b5e726e0bca16c4cfc406d7b4523187646acfdb"
     sha256 cellar: :any_skip_relocation, mojave:        "a5f4c51abd1deb28e02f1dc97ea2fb4bcde0d297e0da9d6c5a3b4cfb0bf6c135"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "808da69175c1b53b4b11f001f4ac44a819e164de7c587b899e0060663af74183"
   end
 
   depends_on "go" => :build
