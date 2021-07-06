77d648c7557aca1de4d99a68fc475b20ffd3ff22
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 00:24:04 +0000

    mosh: update 1.3.2_16 bottle.

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -23,10 +23,11 @@ class Mosh < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "994d3f53c9af51c4bb759dd67de60a8bfffa5a2be1f5ffbd60477abe709b1801"
-    sha256 cellar: :any, big_sur:       "b297986eb2a108d1f38c75e90e12f19953a39bda71ff75860e060f15f26f17d0"
-    sha256 cellar: :any, catalina:      "b9f84223c2299ed1ecefcb98fa545d2a53613933280fbc45c9dec32e9d9a9902"
-    sha256 cellar: :any, mojave:        "0201d43d7aed512afa30e241423c12b4828f193a7fe3155c173cccb55be56ce8"
+    sha256 cellar: :any,                 arm64_big_sur: "994d3f53c9af51c4bb759dd67de60a8bfffa5a2be1f5ffbd60477abe709b1801"
+    sha256 cellar: :any,                 big_sur:       "b297986eb2a108d1f38c75e90e12f19953a39bda71ff75860e060f15f26f17d0"
+    sha256 cellar: :any,                 catalina:      "b9f84223c2299ed1ecefcb98fa545d2a53613933280fbc45c9dec32e9d9a9902"
+    sha256 cellar: :any,                 mojave:        "0201d43d7aed512afa30e241423c12b4828f193a7fe3155c173cccb55be56ce8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec64cfea75e3ef0eecdae39fafd071aac8e059cc7034f0105ad4b84f862a4778"
   end
 
   head do
