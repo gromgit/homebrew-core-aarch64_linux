e266bcafdb82f546ff12180c36ddf638413c04ab
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-06 06:14:43 +0000

    capstone: update 4.0.2 bottle.

--- Formula/capstone.rb
+++ Formula/capstone.rb
@@ -7,11 +7,12 @@ class Capstone < Formula
   head "https://github.com/aquynh/capstone.git", branch: "next"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5c2d67aeb32a36c76d1918ec10de347971b385fc73b3025c97639467dc5302e2"
-    sha256 cellar: :any, big_sur:       "21dd5b41e81b165e0419901103aa46ab8afee2be5453b2076c8f7a5b94fdf211"
-    sha256 cellar: :any, catalina:      "b434ee96e9d7c413e289340b280705a6c3b9929cf1859de865d88bc012c34396"
-    sha256 cellar: :any, mojave:        "c90885740ef54af155c2a0151dc85f728a3aa7ca304a45510e5524ac7fecb7fc"
-    sha256 cellar: :any, high_sierra:   "c6d974a3c237fc36bfea2042d95551f2be7197d37fc0df6c7b9ea2179cd01084"
+    sha256 cellar: :any,                 arm64_big_sur: "5c2d67aeb32a36c76d1918ec10de347971b385fc73b3025c97639467dc5302e2"
+    sha256 cellar: :any,                 big_sur:       "21dd5b41e81b165e0419901103aa46ab8afee2be5453b2076c8f7a5b94fdf211"
+    sha256 cellar: :any,                 catalina:      "b434ee96e9d7c413e289340b280705a6c3b9929cf1859de865d88bc012c34396"
+    sha256 cellar: :any,                 mojave:        "c90885740ef54af155c2a0151dc85f728a3aa7ca304a45510e5524ac7fecb7fc"
+    sha256 cellar: :any,                 high_sierra:   "c6d974a3c237fc36bfea2042d95551f2be7197d37fc0df6c7b9ea2179cd01084"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93d5e7b14aeacb27c38fa2164800458ea4e8deee2d0da2df8e042c616a63a199"
   end
 
   def install
