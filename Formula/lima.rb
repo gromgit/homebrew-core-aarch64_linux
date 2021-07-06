545eab7189d281f9e28d00033e57adf8ca4ddce5
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-07-06 16:21:53 +0900

    lima 0.5.0
    Closes #80626.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -1,8 +1,8 @@
 class Lima < Formula
   desc "Linux virtual machines"
   homepage "https://github.com/AkihiroSuda/lima"
-  url "https://github.com/AkihiroSuda/lima/archive/v0.4.0.tar.gz"
-  sha256 "482e4cff30103aac5bf93feb76721f1ada26a7618e503365040c4adae2a28043"
+  url "https://github.com/AkihiroSuda/lima/archive/v0.5.0.tar.gz"
+  sha256 "2fc3764947d3b8a51f61ac4eeb6160a737f0f982d68491916de7b398f0f43a67"
   license "Apache-2.0"
 
   bottle do
