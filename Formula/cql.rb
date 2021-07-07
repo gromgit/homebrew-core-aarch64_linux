8037bf2263a86df054773090dcb29a26be912fa6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:34:26 +0000

    cql: update 0.8.1 bottle.

--- Formula/cql.rb
+++ Formula/cql.rb
@@ -11,6 +11,7 @@ class Cql < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "cf8e7615d8f5c837efd97b0d5dfc1c0376522fa8823b3d12242470084e82fd81"
     sha256 cellar: :any_skip_relocation, catalina:      "56b5f1a6ac0916da4bf79ae54ce6be4d5d7fae8d943cc3bab400e66e79cd0aec"
     sha256 cellar: :any_skip_relocation, mojave:        "100623450a27784a84597f68cc03956ec715a7121cdffbe33d51126bb681392c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e63e0a586b7c7ffd735aa11519b7a7c2fdb65c34edd3f45339de8753a9616793"
   end
 
   depends_on "go" => :build
