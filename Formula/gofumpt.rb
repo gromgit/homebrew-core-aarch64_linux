f93b7f4721a06cc3cc8bc28a53e106d7ceb9f269
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:04:03 +0000

    gofumpt: update 0.1.1 bottle.

--- Formula/gofumpt.rb
+++ Formula/gofumpt.rb
@@ -10,6 +10,7 @@ class Gofumpt < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "3f1f3ea6ec89b3fc7de1a058c16700bdc0503eff30213ba90e5293d5941eb25e"
     sha256 cellar: :any_skip_relocation, catalina:      "6581f609fac3c41f657f1251263a2a110a95bf499cc32642727e0fb38718f83a"
     sha256 cellar: :any_skip_relocation, mojave:        "cf7c75d380cfac624114a7291bc5b0fcd2cfa3638af23781a0fcb48f1ae605ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40b0ff84f512a1f7fce1839042f72c52df4a005d1ae84226818f058cb8defc59"
   end
 
   depends_on "go"
