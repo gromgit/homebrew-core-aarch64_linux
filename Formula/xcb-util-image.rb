b72dabff6cc3843d5bcbdd8d1020c1aa3002993f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:12:31 +0000

    xcb-util-image: update 0.4.0 bottle.

--- Formula/xcb-util-image.rb
+++ Formula/xcb-util-image.rb
@@ -6,10 +6,11 @@ class XcbUtilImage < Formula
   license "X11"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d6d1fa4758e6e7f5cb8a2d738259cd63d8f5e165ca9dc0731e4fc74198948a06"
-    sha256 cellar: :any, big_sur:       "be6e0bdd4cd0ddde48bca0e424da9661ca5ecfa6f64f8184e88f1df4e44186f2"
-    sha256 cellar: :any, catalina:      "556a8960b5ee6b2290eb223df2dda18054b113b3284b91c4d10cdc3f905ef75c"
-    sha256 cellar: :any, mojave:        "3aeb055928e61fddb3473ea17005774aa6f0b0f32af8323f3da8963d836c7baf"
+    sha256 cellar: :any,                 arm64_big_sur: "d6d1fa4758e6e7f5cb8a2d738259cd63d8f5e165ca9dc0731e4fc74198948a06"
+    sha256 cellar: :any,                 big_sur:       "be6e0bdd4cd0ddde48bca0e424da9661ca5ecfa6f64f8184e88f1df4e44186f2"
+    sha256 cellar: :any,                 catalina:      "556a8960b5ee6b2290eb223df2dda18054b113b3284b91c4d10cdc3f905ef75c"
+    sha256 cellar: :any,                 mojave:        "3aeb055928e61fddb3473ea17005774aa6f0b0f32af8323f3da8963d836c7baf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "257c444348495e9fe4832ef14d7c800888981be7bb39929ade0cfe18ce5f45c8"
   end
 
   head do
