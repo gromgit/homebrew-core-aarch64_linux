d50d5943f49c13e658ddea0fde69e129a63d5689
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:10:18 +0000

    govc: update 0.26.0 bottle.

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -10,6 +10,7 @@ class Govc < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e227227b12c907f8c78d4682682db77779c2bfb198a0c30304ad07345701d058"
     sha256 cellar: :any_skip_relocation, catalina:      "30da7b9a4d22bab292dceb81a6d2fce17cfe8e0f398857da42eea150670d6b59"
     sha256 cellar: :any_skip_relocation, mojave:        "9fc6675cd73b3f08eefc0d6432b01be3035d2cf132352b3c4f9b4982318278e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "042bcbd83f35fea859a818684c1ef7f520ec7e4f1beb33003447a2f2706a8c72"
   end
 
   depends_on "go" => :build
