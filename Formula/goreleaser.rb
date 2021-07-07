45a6d7a4b3a1ea7d077466ccff8cbc20420fe734
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:09:00 +0000

    goreleaser: update 0.173.2 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -12,6 +12,7 @@ class Goreleaser < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "57f04caf7f03244520160f6523d651ebf286a121f37039d6b5a1c5741f17c79f"
     sha256 cellar: :any_skip_relocation, catalina:      "59d336f7b3ecfb31c588ec8ca68da2dd36042bf5cbe1276a44e6434b0036b120"
     sha256 cellar: :any_skip_relocation, mojave:        "51322565423f29f601cd5db88a3fa3204e32e9b73825e792efa135099593cd31"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb6e7fd630389679efe463d613254dd1fa14593660504a4495217308dd8b42ae"
   end
 
   depends_on "go" => :build
