09a514ba151ecc70fe23dfb9a300a68672345021
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 17:29:23 +0000

    xerces-c: update 3.2.3 bottle.

--- Formula/xerces-c.rb
+++ Formula/xerces-c.rb
@@ -8,10 +8,11 @@ class XercesC < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "ad9251257543aee28e08fbc5f433a5ad72c0065511c09d19c888bf28b5bbf21f"
-    sha256 cellar: :any, big_sur:       "6a561a0f4175e7da6790b2beaedf185516a118116402a674a6b936d5c3236575"
-    sha256 cellar: :any, catalina:      "743af0adcd563f604bf3f057d1144b9e11bc1b5a9e842f82d430301d2b5fc185"
-    sha256 cellar: :any, mojave:        "cf22a3e57f6e6e279e9eb476bb80c08d18979938f54d19347fd103ccdc7cf78e"
+    sha256 cellar: :any,                 arm64_big_sur: "ad9251257543aee28e08fbc5f433a5ad72c0065511c09d19c888bf28b5bbf21f"
+    sha256 cellar: :any,                 big_sur:       "6a561a0f4175e7da6790b2beaedf185516a118116402a674a6b936d5c3236575"
+    sha256 cellar: :any,                 catalina:      "743af0adcd563f604bf3f057d1144b9e11bc1b5a9e842f82d430301d2b5fc185"
+    sha256 cellar: :any,                 mojave:        "cf22a3e57f6e6e279e9eb476bb80c08d18979938f54d19347fd103ccdc7cf78e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9a5d21760453dc1ddc1ed3e9971a1304801f50b402bfc5461fe88df04915c3a5"
   end
 
   depends_on "cmake" => :build
