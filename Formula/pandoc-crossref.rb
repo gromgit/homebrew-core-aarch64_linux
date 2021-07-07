2cc4746b53947608bd6c20798c34056d418140d7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 20:07:52 +0000

    pandoc-crossref: update 0.3.12.0 bottle.

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -10,6 +10,7 @@ class PandocCrossref < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "777c6def219f6ce20fc702ff5b9da4cf404c083b4376947c46a98da6cfeae046"
     sha256 cellar: :any_skip_relocation, catalina:      "57d4c7b20861051984b105ffce52e3e7716ddd3484dd34b2068da1851ad35547"
     sha256 cellar: :any_skip_relocation, mojave:        "f5b01586f853381399a67e203a95e404184916cc6fe81edc5c3a0844b6762337"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae4f8dbc8a165ebc5adb838c4fe21acd1d674fa8b614837962c200aa93bbd027"
   end
 
   depends_on "cabal-install" => :build
