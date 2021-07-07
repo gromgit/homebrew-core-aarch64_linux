420effc9ff7ce428e6c0675857503c94a77e1a00
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 19:52:53 +0000

    diamond: update 2.0.10 bottle.

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -10,6 +10,7 @@ class Diamond < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1f7e9f48b8bc3bf12568321b0992ce0214cdd79cd1e695516640094a5003e572"
     sha256 cellar: :any_skip_relocation, catalina:      "68727dff481fa6a2bbebf6b71668ea2f61b151680d2e12bb4d9c8618fb4e0077"
     sha256 cellar: :any_skip_relocation, mojave:        "341585d235e779b2cec192764f4bf047faec95a140caf05f9f24edee56824eb2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60f30b28a4d99168736c7151ba810973f821e539ff317ed829cf96c1a9605f47"
   end
 
   depends_on "cmake" => :build
