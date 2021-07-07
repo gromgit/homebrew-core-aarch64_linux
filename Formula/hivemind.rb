a8488311f0cbca54d7c956b4af3fdafaca34921e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:12:14 +0000

    hivemind: update 1.0.6 bottle.

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -13,6 +13,7 @@ class Hivemind < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "8e6f70ca5e0c8eb1e42d47bee207ec5333b453660d808103a36d53c51a7fb59a"
     sha256 cellar: :any_skip_relocation, high_sierra:   "7a89018774693681975cea22dcdebe35df043507476d1318f195e6d194978693"
     sha256 cellar: :any_skip_relocation, sierra:        "4aa25b52b5c7fd3dc7ae29ab31cf19eeddde4e7685fd7e9838be0ea8cf09f3c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a2c31da1afe090e3752d4445e593cc0c311e9e54e2c18c3c9b7ff704b9eba776"
   end
 
   depends_on "go" => :build
