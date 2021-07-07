7e12184efe033d2b88032f7886e8aaffa7d0cab3
Michael Cho <cho-m@tuta.io>
2021-07-07 00:38:55 -0700

    ipython: fix build for Linux
    Closes #80709.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ipython.rb
+++ Formula/ipython.rb
@@ -131,7 +131,11 @@ class Ipython < Formula
 
     # install other resources
     ipykernel = resource("ipykernel")
-    (resources - [ipykernel]).each do |r|
+    res = resources - [ipykernel]
+    on_linux do
+      res -= [resource("appnope")]
+    end
+    res.each do |r|
       r.stage do
         system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
       end
