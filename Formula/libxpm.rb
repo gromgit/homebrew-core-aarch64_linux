84cf3b4936134dc11c2a8d98ea921a755551c737
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:32:40 +0000

    libxpm: update 3.5.13 bottle.

--- Formula/libxpm.rb
+++ Formula/libxpm.rb
@@ -6,11 +6,12 @@ class Libxpm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a776d9437e8557afeb4dd8e1e91e439ff049df1d8d256af9c34be49843ef7591"
-    sha256 cellar: :any, big_sur:       "484557bf3ce403fc097a70127b9db08cbed9b39372263493588e1539b60ac631"
-    sha256 cellar: :any, catalina:      "fbd3f2bbf058c081bd35672c0129a33efa38b7e599726be145d0b8b818549516"
-    sha256 cellar: :any, mojave:        "c3f788d5e8d2f0ec940af7c758acc0efce194cf526b19ab64bdeaba55e1b6793"
-    sha256 cellar: :any, high_sierra:   "72d7dc1306010048b85b9070287e8c9d5f5a24308b1a413080a4e129aa9bcc0f"
+    sha256 cellar: :any,                 arm64_big_sur: "a776d9437e8557afeb4dd8e1e91e439ff049df1d8d256af9c34be49843ef7591"
+    sha256 cellar: :any,                 big_sur:       "484557bf3ce403fc097a70127b9db08cbed9b39372263493588e1539b60ac631"
+    sha256 cellar: :any,                 catalina:      "fbd3f2bbf058c081bd35672c0129a33efa38b7e599726be145d0b8b818549516"
+    sha256 cellar: :any,                 mojave:        "c3f788d5e8d2f0ec940af7c758acc0efce194cf526b19ab64bdeaba55e1b6793"
+    sha256 cellar: :any,                 high_sierra:   "72d7dc1306010048b85b9070287e8c9d5f5a24308b1a413080a4e129aa9bcc0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd04dae0697e928535158273fb70b168c732b84d395ffd28f5a47835fc9bf536"
   end
 
   depends_on "gettext" => :build
