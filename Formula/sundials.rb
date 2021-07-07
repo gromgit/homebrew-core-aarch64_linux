c746940d982c3088edd2bf9edf13f714bdcf474d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-07 20:02:12 +0000

    sundials: update 5.7.0_1 bottle.

--- Formula/sundials.rb
+++ Formula/sundials.rb
@@ -12,10 +12,11 @@ class Sundials < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "1fb71d31c96c648b7e37f8dd45fc8ee95254778cb55ef4dbb3544b1e81835115"
-    sha256 big_sur:       "d48ec042ebd3769d0f7653578641f563ccd288e79e57f265885a44ce11c5801f"
-    sha256 catalina:      "3463bb365a98201d60246485ff5342494a109fef8a082dc504019bd3729a59c3"
-    sha256 mojave:        "33ca24ba24c03fac2a2f4c964d5227935f1ce97bdcb6c37d0b59dd233836746e"
+    sha256                               arm64_big_sur: "1fb71d31c96c648b7e37f8dd45fc8ee95254778cb55ef4dbb3544b1e81835115"
+    sha256                               big_sur:       "d48ec042ebd3769d0f7653578641f563ccd288e79e57f265885a44ce11c5801f"
+    sha256                               catalina:      "3463bb365a98201d60246485ff5342494a109fef8a082dc504019bd3729a59c3"
+    sha256                               mojave:        "33ca24ba24c03fac2a2f4c964d5227935f1ce97bdcb6c37d0b59dd233836746e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a28fd4b0325c725b6e771b2baff5243a61799033863b2fdcd5e0eadbc9dc44b"
   end
 
   depends_on "cmake" => :build
