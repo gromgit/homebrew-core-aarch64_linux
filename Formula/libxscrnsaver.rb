1746f265e319e6783c877e6eec06fe1b100a5c59
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 07:09:29 +0000

    libxscrnsaver: update 1.2.3 bottle.

--- Formula/libxscrnsaver.rb
+++ Formula/libxscrnsaver.rb
@@ -6,11 +6,12 @@ class Libxscrnsaver < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a7e75a15ac1d4fc2b9dd81a0d0bcb5ae1ff457c52ad5440938e5c3ccc4b6289e"
-    sha256 cellar: :any, big_sur:       "76e43f0d5a786ac9f6689e2c02956a8519e512dca746f882403fbec960e4291f"
-    sha256 cellar: :any, catalina:      "d90c91c9058ec7f2bcac9b2b9b83a5dd76096acd88d09c93edee9abaa02707a5"
-    sha256 cellar: :any, mojave:        "f57eb48a438ab0556e3401ba7b0b049392d11faa2de214ab533e9d444cbf65f2"
-    sha256 cellar: :any, high_sierra:   "44c025315c63c131e89f1fbb4949a0bae4b56bc76ea9e2db320c058e245a3e43"
+    sha256 cellar: :any,                 arm64_big_sur: "a7e75a15ac1d4fc2b9dd81a0d0bcb5ae1ff457c52ad5440938e5c3ccc4b6289e"
+    sha256 cellar: :any,                 big_sur:       "76e43f0d5a786ac9f6689e2c02956a8519e512dca746f882403fbec960e4291f"
+    sha256 cellar: :any,                 catalina:      "d90c91c9058ec7f2bcac9b2b9b83a5dd76096acd88d09c93edee9abaa02707a5"
+    sha256 cellar: :any,                 mojave:        "f57eb48a438ab0556e3401ba7b0b049392d11faa2de214ab533e9d444cbf65f2"
+    sha256 cellar: :any,                 high_sierra:   "44c025315c63c131e89f1fbb4949a0bae4b56bc76ea9e2db320c058e245a3e43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66f775d8f9e82b3e661113eb6287a4b695343642002323192e99d0240bc825e9"
   end
 
   depends_on "pkg-config" => :build
