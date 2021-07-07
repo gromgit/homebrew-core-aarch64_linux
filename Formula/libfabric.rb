40604a3dad6b3c58adbb752d519199f7e2db37e5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 18:17:24 +0000

    libfabric: update 1.13.0 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -7,10 +7,11 @@ class Libfabric < Formula
   head "https://github.com/ofiwg/libfabric.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d70fb55fb5fa73564492482956b460b163c7170ee8063502315d919e774c9d36"
-    sha256 cellar: :any, big_sur:       "c3aa52601f5c00b8c7590349e6c55ac49a9f0d0bd11413d264b5c7d0089fbc30"
-    sha256 cellar: :any, catalina:      "bc75cab6edaf0b5288dbb30fc3bbcc403231fb0cf7edbe06e2fb24b7ccae4633"
-    sha256 cellar: :any, mojave:        "906289db2df6c86e5016f15534aced96c67392e1a1e3f72b606882077684d402"
+    sha256 cellar: :any,                 arm64_big_sur: "d70fb55fb5fa73564492482956b460b163c7170ee8063502315d919e774c9d36"
+    sha256 cellar: :any,                 big_sur:       "c3aa52601f5c00b8c7590349e6c55ac49a9f0d0bd11413d264b5c7d0089fbc30"
+    sha256 cellar: :any,                 catalina:      "bc75cab6edaf0b5288dbb30fc3bbcc403231fb0cf7edbe06e2fb24b7ccae4633"
+    sha256 cellar: :any,                 mojave:        "906289db2df6c86e5016f15534aced96c67392e1a1e3f72b606882077684d402"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "691e2168f24f4a1b8d06d1964379e6bf897c81f0613f6c269551a4fed9b59f1e"
   end
 
   depends_on "autoconf" => :build
