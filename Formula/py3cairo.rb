623b06c7ba822d73abeda248e9e41a3e258972cb
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 17:37:17 +0000

    py3cairo: update 1.20.1 bottle.

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -6,10 +6,11 @@ class Py3cairo < Formula
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a4b8c6a9079f79e22396249ffbdf9f62f895fa8b7af83e038313f96f0dec2c2d"
-    sha256 cellar: :any, big_sur:       "ea6e1887539c142f3b24890521e9181fac8738d5fa2344c9e4c0734ea5b2b9a8"
-    sha256 cellar: :any, catalina:      "a14c31fed107d6d3b3bf5ef8b067de63c020106be3e71f17285a0f3d028cec78"
-    sha256 cellar: :any, mojave:        "1751fed8776a62fb799b5d025babe2692cb49647ff061e0527d8f010bab06a36"
+    sha256 cellar: :any,                 arm64_big_sur: "a4b8c6a9079f79e22396249ffbdf9f62f895fa8b7af83e038313f96f0dec2c2d"
+    sha256 cellar: :any,                 big_sur:       "ea6e1887539c142f3b24890521e9181fac8738d5fa2344c9e4c0734ea5b2b9a8"
+    sha256 cellar: :any,                 catalina:      "a14c31fed107d6d3b3bf5ef8b067de63c020106be3e71f17285a0f3d028cec78"
+    sha256 cellar: :any,                 mojave:        "1751fed8776a62fb799b5d025babe2692cb49647ff061e0527d8f010bab06a36"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6b4152ae7579767ca0e49526a309686712628a0526098ab24af93603bc60edd"
   end
 
   depends_on "pkg-config" => :build
