50bda4699598918bd8bd0e0ea750797e18acf49b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:50:10 +0000

    dockerize: update 0.6.1 bottle.

--- Formula/dockerize.rb
+++ Formula/dockerize.rb
@@ -12,6 +12,7 @@ class Dockerize < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "3eae1414df1ef1786653e916ce2690e6636e89ff6e20bb0b1588b3b87f8b48a6"
     sha256 cellar: :any_skip_relocation, catalina:      "dee3a4b81443d1ceb646f6c945b05162b704df6135501c618d428f54f7f741fa"
     sha256 cellar: :any_skip_relocation, mojave:        "78278dc4fcb9699bd3e16f6a07371b369674e05b868e89bbaf80f86cdcb423a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "efb59f2e819a96effb506bdd11f488f77106407628330ba5ccf3b9edbe14697c"
   end
 
   depends_on "go" => :build
