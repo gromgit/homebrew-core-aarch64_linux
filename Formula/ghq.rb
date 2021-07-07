8507420a0f07093272793635457b36a52855573a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:59:07 +0000

    ghq: update 1.2.1 bottle.

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -12,6 +12,7 @@ class Ghq < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "82fd83decb4539c9c0050d59a240c37149cf61ff5d6b6b6e47e7d6cba83c4d2d"
     sha256 cellar: :any_skip_relocation, catalina:      "8a337adf03ab11238253364863890654ebb92b6cc0b602658c54017059f4f6b5"
     sha256 cellar: :any_skip_relocation, mojave:        "983535fe9c12471da7b4cb2c72035dd602e525f3bcc26579d2d865a5c9cc263c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7a52d8fb22234e9c3ee2ab8ab48fae4a47a0b7c2b13da13fa96de8377b9f8c4"
   end
 
   depends_on "go" => :build
