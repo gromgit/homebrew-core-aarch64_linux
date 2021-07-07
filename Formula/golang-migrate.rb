a2635acb69ead99f1e7c5a5afd8388240811dc0f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:05:17 +0000

    golang-migrate: update 4.14.1 bottle.

--- Formula/golang-migrate.rb
+++ Formula/golang-migrate.rb
@@ -10,6 +10,7 @@ class GolangMigrate < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5c61a106d9970b0f9b14e78e1523894d57b50cd0473f7d5a1fb1a9161dbff159"
     sha256 cellar: :any_skip_relocation, catalina:      "a77af5282af35e0d073e82140b091eedf0b478c19aea36f1b06738690989cebb"
     sha256 cellar: :any_skip_relocation, mojave:        "8fa3758e979f09c171388887c831a6518e3f8df67b07668b6c8cebf76b19a653"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fae2f9b6663ac11a178757ecc67a0407a89191efbdb76302a665566951b7ca34"
   end
 
   depends_on "go" => :build
