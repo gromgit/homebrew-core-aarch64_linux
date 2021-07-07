3ff6731bfb9dbdff7e94ca43627e771208896ec3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:04:15 +0000

    aerc: update 0.5.2 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -10,6 +10,7 @@ class Aerc < Formula
     sha256 big_sur:       "bfbdc6552a248e34e2a85f24664568a8bfc80a3941eb01bc05f9c9b97b6bc811"
     sha256 catalina:      "52d1557638048defbd2c5dfc83ecd929acbbecbd3ee40a6dd8c2460a2232f81d"
     sha256 mojave:        "fe2c485199cdf423c32e27d69ebc5a2af2b355b6a8b2e24ff467a4fc23899b4d"
+    sha256 x86_64_linux:  "fec7ba00c4eab6ae87ea98bad514b50d43c06189e8eece0f63beaa3487d84ab7"
   end
 
   depends_on "go" => :build
