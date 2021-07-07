d2d4670ccd8ed1494f406c4cdf668da0893a00c3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 17:14:35 +0000

    tcsh: update 6.22.04 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -16,6 +16,7 @@ class Tcsh < Formula
     sha256 big_sur:       "4c2d74ed1c5d926ad51dd171479095078db420a0d9e15615044ee6ab86186462"
     sha256 catalina:      "302f0d9d4300c8285431ffbaac65b470d5007059da22da68bffe2f1b5709872f"
     sha256 mojave:        "ccaa49f83f4a1bfac216bc86a9b2a99bd0ba2131d81322b652c584b024248be7"
+    sha256 x86_64_linux:  "825ea20b88a3bd49d8b0a6164e20689f56ce63eedabd928dd785d3af90713a0b"
   end
 
   uses_from_macos "ncurses"
