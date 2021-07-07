9bed1abda7b24b682c86794353054335e97745ad
Rui Chen <rui@chenrui.dev>
2021-07-07 11:29:37 -0400

    ncc 0.28.6 (new formula)
    Closes #80737.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/ncc.rb
@@ -0,0 +1,31 @@
+require "language/node"
+
+class Ncc < Formula
+  desc "Compile a Node.js project into a single file"
+  homepage "https://github.com/vercel/ncc"
+  url "https://registry.npmjs.org/@vercel/ncc/-/ncc-0.28.6.tgz"
+  sha256 "1d59e41eaea1c38f83805ef89774b9c76786c5729b54df00a2cdfc74915d6254"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"input.js").write <<~EOS
+      function component() {
+        const element = document.createElement('div');
+        element.innerHTML = 'Hello' + ' ' + 'webpack';
+        return element;
+      }
+
+      document.body.appendChild(component());
+    EOS
+
+    system bin/"ncc", "build", "input.js", "-o", "dist"
+    assert_match "document.createElement", File.read("dist/index.js")
+  end
+end
