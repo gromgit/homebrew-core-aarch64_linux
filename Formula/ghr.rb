200e5feaf1ca0264fed2e1e4b62a5d176c625f0b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:58:47 +0000

    ghr: update 0.14.0 bottle.

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -10,6 +10,7 @@ class Ghr < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9daff45d2137be191f4906d69385b64afe3f049dc656475585d1c7589a4dcfc1"
     sha256 cellar: :any_skip_relocation, catalina:      "af8668bdf5cf37170d1476fdfeb3df0a81c8dcc83e63f22914d037560a096a37"
     sha256 cellar: :any_skip_relocation, mojave:        "91b37053dae7ad067b16ac4e54a64ce43d71c1c7552b42635012c21a770f4f69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63ac6cdfcd910505644cfc262a1462fa10426c1460a0889924887e85fec69e36"
   end
 
   depends_on "go" => :build
