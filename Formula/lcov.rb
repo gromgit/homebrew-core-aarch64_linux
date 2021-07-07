501d630a262772705a9eb21ab23d6d74cedcaf10
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 19:36:19 +0000

    lcov: update 1.15 bottle.

--- Formula/lcov.rb
+++ Formula/lcov.rb
@@ -16,6 +16,7 @@ class Lcov < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "1c84487473440a6f7971ecf25f2b8b5022d23a230d16e863825b43944788e3be"
     sha256 cellar: :any_skip_relocation, mojave:        "41ebe534e6bf4166e88d0eb59ac04d28df457a86fb514fc610ca485386bd06b4"
     sha256 cellar: :any_skip_relocation, high_sierra:   "9c3a3586283d61ae1f1ce30145b613ebdc50e28a7656cf4b4f4e935408f4c147"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8ebcc60880b409d88b26b9d2b90938997b2ea6c9b0400684d166d920e547004"
   end
 
   uses_from_macos "perl"
