4745ae7559000dcec147cfce11ee5833f8e1d649
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 18:18:26 +0000

    beagle: update 3.1.2_1 bottle.

--- Formula/beagle.rb
+++ Formula/beagle.rb
@@ -12,10 +12,11 @@ class Beagle < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:     "1ad3ce2956827daace31c7d2606c8149afdcf305f8e3688c8cfe5b0da429d3ad"
-    sha256 cellar: :any, catalina:    "de8fe667e01d1e204c669980753cf9ef84b4d2406ab52c0882d6d9108d2dc7eb"
-    sha256 cellar: :any, mojave:      "fe9ae7aaa01df98d34b5cbd7dce8abd9ac840f2bb54797851b42a056ee258e01"
-    sha256 cellar: :any, high_sierra: "d2a42acc06fa4bf26c25b4d63a96c0d6bf46f7b1a21bacf91ef70fbff58e77b4"
+    sha256 cellar: :any,                 big_sur:      "1ad3ce2956827daace31c7d2606c8149afdcf305f8e3688c8cfe5b0da429d3ad"
+    sha256 cellar: :any,                 catalina:     "de8fe667e01d1e204c669980753cf9ef84b4d2406ab52c0882d6d9108d2dc7eb"
+    sha256 cellar: :any,                 mojave:       "fe9ae7aaa01df98d34b5cbd7dce8abd9ac840f2bb54797851b42a056ee258e01"
+    sha256 cellar: :any,                 high_sierra:  "d2a42acc06fa4bf26c25b4d63a96c0d6bf46f7b1a21bacf91ef70fbff58e77b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "52b37fa051dd8fb96e14830f0acb5d33b3356183bd5f223f2b0dd29917866a20"
   end
 
   depends_on "autoconf" => :build
