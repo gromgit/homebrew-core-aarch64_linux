8e1e13702ad7c5749f1a0157c60a0b5362505e86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-07 08:08:49 +0000

    caf: update 0.18.4 bottle.

--- Formula/caf.rb
+++ Formula/caf.rb
@@ -8,10 +8,10 @@ class Caf < Formula
   head "https://github.com/actor-framework/actor-framework.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2eb5251871e94fcab790645d0ade26bdd53405a5c3638378d86a887b1a4c4217"
-    sha256 cellar: :any, big_sur:       "78181544230244ed5103e328e09bc59a67fd9c31a333f492d3278f39646944be"
-    sha256 cellar: :any, catalina:      "6d278d761ac996928c2c26ef5dc30a8bc1b82a7ed112b878a329dbac0641170d"
-    sha256 cellar: :any, mojave:        "2c5155b10c92de8ea6273193175d9d65c32c5eed643d0a3a2adfa72ecef5964b"
+    sha256 cellar: :any, arm64_big_sur: "6053f87537cdf91ba19b7f8940acdaef9ab87d923cbe30a0a360562a210347b6"
+    sha256 cellar: :any, big_sur:       "8e6a39fb13a1b4ff4309a009c905b220e91dd3c3bfa6815a0d0ca6759fc809f8"
+    sha256 cellar: :any, catalina:      "61aab01245ca1831e5929881f91786a9c8b57ea78095c4d0bc183d5f0ab20f34"
+    sha256 cellar: :any, mojave:        "c08113af94dd885082960e53d8a50829726a40892db615b70301c696229d6412"
   end
 
   depends_on "cmake" => :build
