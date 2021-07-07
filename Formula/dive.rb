d6730b38af61c9d6891fed8daf811103e3496d11
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:13:25 +0000

    dive: update 0.10.0 bottle.

--- Formula/dive.rb
+++ Formula/dive.rb
@@ -11,6 +11,7 @@ class Dive < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "4fdeb80d1d57527ef5887c98a5db2235e73edb7b3d0fe32d42f0cfcf0ea49780"
     sha256 cellar: :any_skip_relocation, catalina:      "8c17b54b9370b6bc8d36dde60d42b71a7ef0fc3d700bd67893fe04c8ec1f69f9"
     sha256 cellar: :any_skip_relocation, mojave:        "3e67c19a68dc97ba2b66e57886923b5abee437c74e83a76e289a1fc491c0416f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "795372731d4e6cd97f76b98f84c9c03878f18277be95f3f94033990f05abc6ba"
   end
 
   depends_on "go" => :build
