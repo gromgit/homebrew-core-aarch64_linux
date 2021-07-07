073b92694cc9e41f5a2f34d76486746a7cbf4953
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:03:32 +0000

    gmailctl: update 0.8.0 bottle.

--- Formula/gmailctl.rb
+++ Formula/gmailctl.rb
@@ -10,6 +10,7 @@ class Gmailctl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e72f9006fd0c10ef79ecd4c2f875d1ee984d6ee40a6a717894b29cae9d3324fb"
     sha256 cellar: :any_skip_relocation, catalina:      "e191cdefb1f75f799f610aeaadd454c27d3aa90527d3bbf69ee804ca5c94dfa8"
     sha256 cellar: :any_skip_relocation, mojave:        "57d9dc005423a078971fd79d5c2382c07f17b4536a445d571cef47d025d28ffb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58ce36e464f9c0d38c8bba1c2bb46bbdcc1e8bb848808da06b04a35007e763e1"
   end
 
   depends_on "go" => :build
