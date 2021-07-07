10fbb5e8b4a0cadb0e44a0742e5675f7bc4c0b35
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-07 01:05:36 +0000

    tidy-html5: update 5.6.0 bottle.

--- Formula/tidy-html5.rb
+++ Formula/tidy-html5.rb
@@ -12,13 +12,14 @@ class TidyHtml5 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3120baf4b5155c0ef5baa272f5a756909cb683c9c9447bdeaf7a07c388376370"
-    sha256 cellar: :any, big_sur:       "9c4ed7860ed418e2a018690ee52c83c1a520004bd9d97f7eb6760cf4e82f2af2"
-    sha256 cellar: :any, catalina:      "fb2134180fbdb92cc10f3fad33c769073adceb7796e465db7dbc3778f7d3547a"
-    sha256 cellar: :any, mojave:        "bd3ca7dc82a913c8576716cbcc957260251132f6dd7b8c526c9ef0c4674faf0f"
-    sha256 cellar: :any, high_sierra:   "af9633f1578980fe3d4351c3d71b4b83cc79f814d87310e4b7d05830c53c9621"
-    sha256 cellar: :any, sierra:        "6c8f843d25d6964b18d4c2fa15aaf2606b36decbbe65c31b38a7982e499a9d28"
-    sha256 cellar: :any, el_capitan:    "48416711a2f1a080e9eae1ecba30773ee48eae98181e25c6ae5ace07cb7ac8ee"
+    sha256 cellar: :any,                 arm64_big_sur: "3120baf4b5155c0ef5baa272f5a756909cb683c9c9447bdeaf7a07c388376370"
+    sha256 cellar: :any,                 big_sur:       "9c4ed7860ed418e2a018690ee52c83c1a520004bd9d97f7eb6760cf4e82f2af2"
+    sha256 cellar: :any,                 catalina:      "fb2134180fbdb92cc10f3fad33c769073adceb7796e465db7dbc3778f7d3547a"
+    sha256 cellar: :any,                 mojave:        "bd3ca7dc82a913c8576716cbcc957260251132f6dd7b8c526c9ef0c4674faf0f"
+    sha256 cellar: :any,                 high_sierra:   "af9633f1578980fe3d4351c3d71b4b83cc79f814d87310e4b7d05830c53c9621"
+    sha256 cellar: :any,                 sierra:        "6c8f843d25d6964b18d4c2fa15aaf2606b36decbbe65c31b38a7982e499a9d28"
+    sha256 cellar: :any,                 el_capitan:    "48416711a2f1a080e9eae1ecba30773ee48eae98181e25c6ae5ace07cb7ac8ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4be7f0202177d1ffc0de72680f7584e6d35aa2e18c7567c26d8b5beaca019a9b"
   end
 
   depends_on "cmake" => :build
