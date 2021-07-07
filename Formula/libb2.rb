eae3b2e625a07a1d3bf23a5eea5fb0c7a8a04071
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:16:21 +0000

    libb2: update 0.98.1 bottle.

--- Formula/libb2.rb
+++ Formula/libb2.rb
@@ -6,12 +6,13 @@ class Libb2 < Formula
   license "CC0-1.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7713b483f3474a3531c5830bbc8de7ad1903989e55e5da3ff5bfd85e87c5c045"
-    sha256 cellar: :any, big_sur:       "7e21b980288ef9449cb44a4b2d33a0d0772b0482165c9ee5f12d42b71b357bc0"
-    sha256 cellar: :any, catalina:      "fb9f331b6c556a09558cf8098c3934f3f9196c3076e2511fd6ed816439fb8936"
-    sha256 cellar: :any, mojave:        "bbd333a0a89e6a38445aba0170b14b516edad300c30d6f4239b66a130c446959"
-    sha256 cellar: :any, high_sierra:   "6e9156db268cea377f7050c4e9ebf1ee3065fef76a11c40e03e700a23b1bef36"
-    sha256 cellar: :any, sierra:        "9b909b878c01b5bb3284ba4d0937352e0df54b27e491fa796dfb6d3e67f989a1"
+    sha256 cellar: :any,                 arm64_big_sur: "7713b483f3474a3531c5830bbc8de7ad1903989e55e5da3ff5bfd85e87c5c045"
+    sha256 cellar: :any,                 big_sur:       "7e21b980288ef9449cb44a4b2d33a0d0772b0482165c9ee5f12d42b71b357bc0"
+    sha256 cellar: :any,                 catalina:      "fb9f331b6c556a09558cf8098c3934f3f9196c3076e2511fd6ed816439fb8936"
+    sha256 cellar: :any,                 mojave:        "bbd333a0a89e6a38445aba0170b14b516edad300c30d6f4239b66a130c446959"
+    sha256 cellar: :any,                 high_sierra:   "6e9156db268cea377f7050c4e9ebf1ee3065fef76a11c40e03e700a23b1bef36"
+    sha256 cellar: :any,                 sierra:        "9b909b878c01b5bb3284ba4d0937352e0df54b27e491fa796dfb6d3e67f989a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e12bfdb9ca31174e1f644bd68f89f7de0354b7661569b59cde6c8a6de2d8a24"
   end
 
   def install
