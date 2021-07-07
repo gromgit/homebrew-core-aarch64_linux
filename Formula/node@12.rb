e6c3497ca179d2d4400169e9309c29cfdbaa9c34
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:37:21 +0000

    node@12: update 12.22.3 bottle.

--- Formula/node@12.rb
+++ Formula/node@12.rb
@@ -11,10 +11,11 @@ class NodeAT12 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c5e43bbfb4ed9b5866bd0f5aefb5cb40f06504138ec57bd92a595eec01ac823b"
-    sha256 cellar: :any, big_sur:       "09dce109119522b874a50633fce1b9e03c3a4f6d8e8fc17b56bc6aaabace8214"
-    sha256 cellar: :any, catalina:      "2a7c221af40ac971028770eaf5b005b0a1135b9c206da77aa57b1805b2632d30"
-    sha256 cellar: :any, mojave:        "1eefa8281d0ed8b35492b1fb57a6f579341de05325766c6f0587f282455473df"
+    sha256 cellar: :any,                 arm64_big_sur: "c5e43bbfb4ed9b5866bd0f5aefb5cb40f06504138ec57bd92a595eec01ac823b"
+    sha256 cellar: :any,                 big_sur:       "09dce109119522b874a50633fce1b9e03c3a4f6d8e8fc17b56bc6aaabace8214"
+    sha256 cellar: :any,                 catalina:      "2a7c221af40ac971028770eaf5b005b0a1135b9c206da77aa57b1805b2632d30"
+    sha256 cellar: :any,                 mojave:        "1eefa8281d0ed8b35492b1fb57a6f579341de05325766c6f0587f282455473df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e83be53c90a0e2b72d6f97509fe19c6dc008c1f39ce54f3bf2be8806e2f05d9d"
   end
 
   keg_only :versioned_formula
