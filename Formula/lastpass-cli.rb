a6966f0d6fa6d7ad5cebdbda21189a6a298d9f1a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 18:08:31 +0000

    lastpass-cli: update 1.3.3_1 bottle.

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -9,11 +9,12 @@ class LastpassCli < Formula
 
   bottle do
     rebuild 4
-    sha256 cellar: :any, arm64_big_sur: "1bccfb715b94c569d943c9ce5833f74628397abf1311f74c49d6e5d4b25b847b"
-    sha256 cellar: :any, big_sur:       "10f9224c8bfebae0cf12df72e6144ba3a309956b1efcce574975cd21cec930c5"
-    sha256 cellar: :any, catalina:      "8643f81d13a40ef8b86efe83fbee1b41b22c492b7725bebab83dcb2d253fd603"
-    sha256 cellar: :any, mojave:        "f1b7c42dd889f597ef06f0bd72bb1b273c21dc91e5f3e313da8599254954a7ae"
-    sha256 cellar: :any, high_sierra:   "62629472aafb7e4927d8ab5e9d7189c913e3249a172d0445ffe7eda31b642eb7"
+    sha256 cellar: :any,                 arm64_big_sur: "1bccfb715b94c569d943c9ce5833f74628397abf1311f74c49d6e5d4b25b847b"
+    sha256 cellar: :any,                 big_sur:       "10f9224c8bfebae0cf12df72e6144ba3a309956b1efcce574975cd21cec930c5"
+    sha256 cellar: :any,                 catalina:      "8643f81d13a40ef8b86efe83fbee1b41b22c492b7725bebab83dcb2d253fd603"
+    sha256 cellar: :any,                 mojave:        "f1b7c42dd889f597ef06f0bd72bb1b273c21dc91e5f3e313da8599254954a7ae"
+    sha256 cellar: :any,                 high_sierra:   "62629472aafb7e4927d8ab5e9d7189c913e3249a172d0445ffe7eda31b642eb7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3ea59e2bc30ccd28e1f516be98d1f170fc0fa7ff333938212879d44dfc84477"
   end
 
   depends_on "asciidoc" => :build
