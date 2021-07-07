17e1acb2018cf754a47abd2cc7ed8176a20f8f1c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 17:18:45 +0000

    saml2aws: update 2.31.0 bottle.

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -11,6 +11,7 @@ class Saml2aws < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "2d1d99a82f8237a0a71213007a3af5e1c32e9baad5e0128301bdae2a260e97b2"
     sha256 cellar: :any_skip_relocation, catalina:      "7323761199de5a2c415e8876c801ecee5beab58530403864c2df2df1e8898598"
     sha256 cellar: :any_skip_relocation, mojave:        "c4a3ecc94ca7f0001291f7c3d26d15db0508188a7c3e088f091db2b8e20506e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bbe81f00cc0e032439441da8a7a74164c0ca8361a21141e5e70b9cc587146087"
   end
 
   depends_on "go" => :build
