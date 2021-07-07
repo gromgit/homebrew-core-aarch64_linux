81e515771df86d427766ef898fbe44fe2c510d5f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:02:25 +0000

    buildifier: update 4.0.1 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -10,6 +10,7 @@ class Buildifier < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "dfec354e37e8027cfc1adbccd17d88e8ea4781d5653389534fbfb802b71d42d7"
     sha256 cellar: :any_skip_relocation, catalina:      "105cd56c1b0933b8f2afbe283df6a03f7bc038b8b20daa194e5b6e7fc3ed9e6c"
     sha256 cellar: :any_skip_relocation, mojave:        "7898e9f197210a0bb89f8674d07da5160d77790f7cf58507c47718d332214948"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45f8da4e8698fef34fa1e3cc6e7cefeaba060f7026925e4be8aa6b898506ddf9"
   end
 
   depends_on "go" => :build
