ee5d6bbf964c23990068161a81384219cbe5e245
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:00:51 +0000

    docker-machine-driver-vmware: update 0.1.3 bottle.

--- Formula/docker-machine-driver-vmware.rb
+++ Formula/docker-machine-driver-vmware.rb
@@ -11,6 +11,7 @@ class DockerMachineDriverVmware < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5028fce4f23eba9047fa977e0db88861d99bcc0dc2b79b0c2920ea68569b970f"
     sha256 cellar: :any_skip_relocation, catalina:      "d0ce7d4804d39392cfede2ecc19fbe763d39b4684306d0cb36efb19359668c53"
     sha256 cellar: :any_skip_relocation, mojave:        "515e8951062268846b9c3ec85aa2b29c6f093cba35eed91a3503344fd5aa288a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cfe899d44e93a330c23fb56f24df7b98decb3aa4829f2f69eee63fac804ef8dd"
   end
 
   depends_on "go" => :build
