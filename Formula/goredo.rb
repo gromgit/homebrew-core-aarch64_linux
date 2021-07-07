c4c387dcd45b5d26670e69fa0262bdea3cadcf0a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:06:13 +0000

    goredo: update 1.3.0 bottle.

--- Formula/goredo.rb
+++ Formula/goredo.rb
@@ -11,6 +11,7 @@ class Goredo < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "0fcc3fc1c0af0ea05c96284f6db82379dd843689e75bcded1fc8527b49da049d"
     sha256 cellar: :any_skip_relocation, catalina:      "9e03c6cb942991c25a1ca78e6a09dafc795bd19a65a498e14541a3116b709eab"
     sha256 cellar: :any_skip_relocation, mojave:        "d953588f9a8511c9f6eac5c6b9f9c6e51bb8776123d5e7a00eee39dd9e77c9e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92b6af80813a618d528b4ac00657903505eaaa497459382cd96e83feec64e66e"
   end
 
   depends_on "go" => :build
