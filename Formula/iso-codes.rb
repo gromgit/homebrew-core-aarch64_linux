0ef640937f4e74424dfa81f3a3f0a227ba01ada1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 16:56:57 +0000

    iso-codes: update 4.6.0 bottle.

--- Formula/iso-codes.rb
+++ Formula/iso-codes.rb
@@ -11,6 +11,7 @@ class IsoCodes < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b187032bee03cffb5e57f2a609f31010b3572a7dc20e74863f5058fe084a59c2"
     sha256 cellar: :any_skip_relocation, catalina:      "e842954d1e655188d47362cabce2af5585dd5f9f7687f6d70c14e951374918b2"
     sha256 cellar: :any_skip_relocation, mojave:        "172c9bad5734b7fa56f77c1f2a73e9db2f7d9492c3b40a11941b57b294cc554a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c1c30e3cc2b1f15d09b4d71f1631bc577d987e016031b72df4672f0c44d3ecd"
   end
 
   depends_on "gettext" => :build
