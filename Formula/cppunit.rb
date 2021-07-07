9b0afe6cd931cc7056bfe5c9e0c75e3507bed198
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:27:54 +0000

    cppunit: update 1.15.1 bottle.

--- Formula/cppunit.rb
+++ Formula/cppunit.rb
@@ -11,11 +11,12 @@ class Cppunit < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "42291951a34e6fae578a9c25d2a5c399dc1e13ec3f700c017f96d7576acabbf1"
-    sha256 cellar: :any, big_sur:       "8890cb5c9b85681b735c3756d301df29beb98e2c0d0b10f2fad073e6f1870101"
-    sha256 cellar: :any, catalina:      "3e194b84577c733e5641f305a1cb5cc76355f13037898afc56c3096f98bb78fe"
-    sha256 cellar: :any, mojave:        "1c107efb84d656dd5327aa8cf13e6cbce8db7542aacba98ae98a2b05940b16ff"
-    sha256 cellar: :any, high_sierra:   "08a339bc38db169bce2f5eb0fc0b940bc82562c37274aa770668f681aeca4386"
+    sha256 cellar: :any,                 arm64_big_sur: "42291951a34e6fae578a9c25d2a5c399dc1e13ec3f700c017f96d7576acabbf1"
+    sha256 cellar: :any,                 big_sur:       "8890cb5c9b85681b735c3756d301df29beb98e2c0d0b10f2fad073e6f1870101"
+    sha256 cellar: :any,                 catalina:      "3e194b84577c733e5641f305a1cb5cc76355f13037898afc56c3096f98bb78fe"
+    sha256 cellar: :any,                 mojave:        "1c107efb84d656dd5327aa8cf13e6cbce8db7542aacba98ae98a2b05940b16ff"
+    sha256 cellar: :any,                 high_sierra:   "08a339bc38db169bce2f5eb0fc0b940bc82562c37274aa770668f681aeca4386"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1024a59b0bca4572448d7ed2e1152688d1b9be616d3b0644c15de8cd982c32a5"
   end
 
   def install
