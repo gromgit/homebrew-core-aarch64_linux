a99f197430e4d72782e6c1274fe60ffcc10ec205
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 19:34:07 +0000

    tesseract: update 4.1.1 bottle.

--- Formula/tesseract.rb
+++ Formula/tesseract.rb
@@ -12,11 +12,12 @@ class Tesseract < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "038495152035dbed8ed578eab3c98c911d608ff50ac02ceb8f8408c762d01a27"
-    sha256 cellar: :any, big_sur:       "6d49823b55a5093041b94bad0fb34e3a06c13d7ec0c677765ee64a88a3608fc0"
-    sha256 cellar: :any, catalina:      "81ff467946d9c85151c86819034cd183a983b4a3fa10374c7f039a5ec3ef0d82"
-    sha256 cellar: :any, mojave:        "34eee505fccec07eaab30f14c46f9688db9f3aa578306d47bbcd31801b0b849d"
-    sha256 cellar: :any, high_sierra:   "6b64585454bcca9b62945b284000723d76afad15b5e80109ca6cdc699ae50e25"
+    sha256 cellar: :any,                 arm64_big_sur: "038495152035dbed8ed578eab3c98c911d608ff50ac02ceb8f8408c762d01a27"
+    sha256 cellar: :any,                 big_sur:       "6d49823b55a5093041b94bad0fb34e3a06c13d7ec0c677765ee64a88a3608fc0"
+    sha256 cellar: :any,                 catalina:      "81ff467946d9c85151c86819034cd183a983b4a3fa10374c7f039a5ec3ef0d82"
+    sha256 cellar: :any,                 mojave:        "34eee505fccec07eaab30f14c46f9688db9f3aa578306d47bbcd31801b0b849d"
+    sha256 cellar: :any,                 high_sierra:   "6b64585454bcca9b62945b284000723d76afad15b5e80109ca6cdc699ae50e25"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66b1efe4ccce2f26b0a64b293260042fce0f2b0d5fcdf4a5f63c2887ba5bee11"
   end
 
   depends_on "autoconf" => :build
