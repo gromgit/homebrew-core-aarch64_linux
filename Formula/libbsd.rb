2c7ba1981788cdaaffbd2f86c2019ea26ab60b37
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 18:14:05 +0000

    libbsd: add 0.11.3 bottle.

--- Formula/libbsd.rb
+++ Formula/libbsd.rb
@@ -5,6 +5,10 @@ class Libbsd < Formula
   sha256 "ff95cf8184151dacae4247832f8d4ea8800fa127dbd15033ecfe839f285b42a1"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "47ff83f8f952f71030f61444df19635a5eb3d98edc346be4ee3955efc8f7d4fa"
+  end
+
   depends_on "libmd"
   depends_on :linux
 
