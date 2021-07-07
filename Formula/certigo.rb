91034b3c34450af3e71778b651ed962003cac2dc
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 00:06:39 +0000

    certigo: update 1.12.1 bottle.

--- Formula/certigo.rb
+++ Formula/certigo.rb
@@ -13,6 +13,7 @@ class Certigo < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "9cb3d249c87ed65409a4e4a0e7841bbb8ab9192dea06df8f78f28f0fcbec4550"
     sha256 cellar: :any_skip_relocation, mojave:        "85d39ea2806bbd5ea750486132343d2dc36d5cc37ac0048d4561c40d20826fde"
     sha256 cellar: :any_skip_relocation, high_sierra:   "0ec7c22fe619af5e5178f4387f2731909ff02d4379ec62784f3625d2a63c358c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "917a031d006d106895ca2da9894087129f9f9d7e58ae11c7c7e49427a433f1be"
   end
 
   depends_on "go" => :build
