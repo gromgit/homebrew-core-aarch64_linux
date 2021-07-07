93e3cdab987a6709169f108073cd5498ecdc828e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 19:36:10 +0000

    xapian: update 1.4.18 bottle.

--- Formula/xapian.rb
+++ Formula/xapian.rb
@@ -12,10 +12,11 @@ class Xapian < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f4f208630ce41f77203d5674665cc68ed2d9ef523aadfe59bbb9b603b3c50d78"
-    sha256 cellar: :any, big_sur:       "5221d8356199601091b9d08fd9d46f5b6cc735ccbcfbaf0a88f9a740ecc282a2"
-    sha256 cellar: :any, catalina:      "29142b83f9c5366b5a102475a92dfb779915764f1143b48a3f3fc881ea4ada07"
-    sha256 cellar: :any, mojave:        "c97b7ab978b2afa9341c96cd3f41205dca022663951c4bf5516ab8eabe64d7ed"
+    sha256 cellar: :any,                 arm64_big_sur: "f4f208630ce41f77203d5674665cc68ed2d9ef523aadfe59bbb9b603b3c50d78"
+    sha256 cellar: :any,                 big_sur:       "5221d8356199601091b9d08fd9d46f5b6cc735ccbcfbaf0a88f9a740ecc282a2"
+    sha256 cellar: :any,                 catalina:      "29142b83f9c5366b5a102475a92dfb779915764f1143b48a3f3fc881ea4ada07"
+    sha256 cellar: :any,                 mojave:        "c97b7ab978b2afa9341c96cd3f41205dca022663951c4bf5516ab8eabe64d7ed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e77c1412ab0f38e3acd480edbbf5a41a082ce3aad4db12ebc5cd287beccd0bc8"
   end
 
   depends_on "sphinx-doc" => :build
