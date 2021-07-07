3e81765a3296741e3abdbfd11a5f1c4601860f25
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 19:19:52 +0000

    iperf3: update 3.10.1 bottle.

--- Formula/iperf3.rb
+++ Formula/iperf3.rb
@@ -6,10 +6,11 @@ class Iperf3 < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7acd6b3fedf49e5614f825f928d24b035e93db311c25797d94b8ef880b78afe0"
-    sha256 cellar: :any, big_sur:       "3c7b4daa79c385333b89c08740c477c633d45eba240610f7d43f26a3397555b3"
-    sha256 cellar: :any, catalina:      "a3d90dab71e047c3c585dc6dfbb63b240b3842e02af8fdaee665bb13b3d71c82"
-    sha256 cellar: :any, mojave:        "dea44691e2528c58d8f30d5b0b60d2407bb3ba17db5c3d0327fc29442f911847"
+    sha256 cellar: :any,                 arm64_big_sur: "7acd6b3fedf49e5614f825f928d24b035e93db311c25797d94b8ef880b78afe0"
+    sha256 cellar: :any,                 big_sur:       "3c7b4daa79c385333b89c08740c477c633d45eba240610f7d43f26a3397555b3"
+    sha256 cellar: :any,                 catalina:      "a3d90dab71e047c3c585dc6dfbb63b240b3842e02af8fdaee665bb13b3d71c82"
+    sha256 cellar: :any,                 mojave:        "dea44691e2528c58d8f30d5b0b60d2407bb3ba17db5c3d0327fc29442f911847"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d078e2b5840746763092d35eede028ccf1b0db86274597ce064c14e5652cae7"
   end
 
   head do
