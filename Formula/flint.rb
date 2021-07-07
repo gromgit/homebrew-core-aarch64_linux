fa0dfebe45d5445b4fe8a3513a6fc05ddc841dea
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:14:44 +0000

    flint: update 2.7.1_2 bottle.

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -8,10 +8,11 @@ class Flint < Formula
   head "https://github.com/wbhart/flint2.git", branch: "trunk"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "76a41ad303e485cad7795c7668a488945a4aa1e15b31d0cf60aa67417d818ea8"
-    sha256 cellar: :any, big_sur:       "6b6b6fe83f4e2827f57c673598791048b364efe8c554dc7293bfc1594d5aa9e9"
-    sha256 cellar: :any, catalina:      "a98cb754d82e32e1072548661fe76bf4ba37388a0497ca5ca7466c45679faab4"
-    sha256 cellar: :any, mojave:        "6d223d4c47e57d7080d3e9c70aee75256153268cd75682972dcdc43b8a8eda6d"
+    sha256 cellar: :any,                 arm64_big_sur: "76a41ad303e485cad7795c7668a488945a4aa1e15b31d0cf60aa67417d818ea8"
+    sha256 cellar: :any,                 big_sur:       "6b6b6fe83f4e2827f57c673598791048b364efe8c554dc7293bfc1594d5aa9e9"
+    sha256 cellar: :any,                 catalina:      "a98cb754d82e32e1072548661fe76bf4ba37388a0497ca5ca7466c45679faab4"
+    sha256 cellar: :any,                 mojave:        "6d223d4c47e57d7080d3e9c70aee75256153268cd75682972dcdc43b8a8eda6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2daf8784bcb28bff46223174582587e1fe712504ca391a1877c3e14c0e898b2e"
   end
 
   depends_on "gmp"
