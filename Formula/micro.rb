9ab6edabfa1ea3617a374fae29d067d235511834
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:29:45 +0000

    micro: update 2.0.9 bottle.

--- Formula/micro.rb
+++ Formula/micro.rb
@@ -12,6 +12,7 @@ class Micro < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "876f97a93335c70abf96d30d1bca0a4e6dc68f532205a7843451f25184764e97"
     sha256 cellar: :any_skip_relocation, catalina:      "5d95bee552c50d29d65f4b8bc075a7b2d7d0dddfd756d608e02897ab93fbf2ef"
     sha256 cellar: :any_skip_relocation, mojave:        "64717e9a4926f13813f47ac3461c4c610367f316b672bd570fee6d9721beffc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0239bac895317d828285292ddb5c3306b3c30a412befd0aaedc0d9c4a4ed60f2"
   end
 
   depends_on "go" => :build
