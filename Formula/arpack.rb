27c1406843e76a2f6801a51a5b75f405e6a05089
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-07 20:02:16 +0000

    arpack: update 3.8.0_1 bottle.

--- Formula/arpack.rb
+++ Formula/arpack.rb
@@ -8,10 +8,11 @@ class Arpack < Formula
   head "https://github.com/opencollab/arpack-ng.git"
 
   bottle do
-    sha256               arm64_big_sur: "ce6c690e2da971fd3c2b1b481e2a1a63c74e45ab308abc8212d4e8622ab57fb3"
-    sha256 cellar: :any, big_sur:       "d7e7dcfe9877c71a43edc35191855abab902424d99eb30835092e01d9bbb042d"
-    sha256 cellar: :any, catalina:      "42b2c8c8c61ff92e7c3c96d3735decfd5bb00b5741bc66ee7b77a4c10f338ad4"
-    sha256 cellar: :any, mojave:        "c77c3b77ef86bea4c18757c674af5775bc33463f1f59cf49ed7ad9fbb9b6d3b9"
+    sha256                               arm64_big_sur: "ce6c690e2da971fd3c2b1b481e2a1a63c74e45ab308abc8212d4e8622ab57fb3"
+    sha256 cellar: :any,                 big_sur:       "d7e7dcfe9877c71a43edc35191855abab902424d99eb30835092e01d9bbb042d"
+    sha256 cellar: :any,                 catalina:      "42b2c8c8c61ff92e7c3c96d3735decfd5bb00b5741bc66ee7b77a4c10f338ad4"
+    sha256 cellar: :any,                 mojave:        "c77c3b77ef86bea4c18757c674af5775bc33463f1f59cf49ed7ad9fbb9b6d3b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a5266781353da90fd500b49f0d583dc7f5c345b4c43522698f5f456d8a8c4298"
   end
 
   depends_on "autoconf" => :build
