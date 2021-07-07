dbb0a0fc3be81c9f6ab6d901ed258e0fe32d8097
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 18:00:31 +0000

    tinyxml2: update 9.0.0 bottle.

--- Formula/tinyxml2.rb
+++ Formula/tinyxml2.rb
@@ -7,10 +7,11 @@ class Tinyxml2 < Formula
   head "https://github.com/leethomason/tinyxml2.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a5c5e7ea6dcc446b1f7d38441ac4a226afa14b3e5e5eb890d3105edf54f91db6"
-    sha256 cellar: :any, big_sur:       "4df58f65bc6629e1884225503622e07f26e52a9690e24a6e959dd1304b11dbb8"
-    sha256 cellar: :any, catalina:      "d09e9f6a1833923fea9528a056c663cb5e05b71afacc1fcec7b9b6fbeb30772f"
-    sha256 cellar: :any, mojave:        "84db2d094fa220b2269cd97bed3fb50edfd23061f6ab9dece09a82562e73a975"
+    sha256 cellar: :any,                 arm64_big_sur: "a5c5e7ea6dcc446b1f7d38441ac4a226afa14b3e5e5eb890d3105edf54f91db6"
+    sha256 cellar: :any,                 big_sur:       "4df58f65bc6629e1884225503622e07f26e52a9690e24a6e959dd1304b11dbb8"
+    sha256 cellar: :any,                 catalina:      "d09e9f6a1833923fea9528a056c663cb5e05b71afacc1fcec7b9b6fbeb30772f"
+    sha256 cellar: :any,                 mojave:        "84db2d094fa220b2269cd97bed3fb50edfd23061f6ab9dece09a82562e73a975"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ff49aa9a2ab33353cab7cf34aee1013b3ba5a925b49dbccf9f7eee915affe02f"
   end
 
   depends_on "cmake" => :build
