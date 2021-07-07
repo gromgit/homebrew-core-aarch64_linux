33196d7092a22c9a40f14f7699745538c41c6bb7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:08:52 +0000

    less: update 581.2_1 bottle.

--- Formula/less.rb
+++ Formula/less.rb
@@ -12,10 +12,11 @@ class Less < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f23024af4d1356f53bb5878fbbb41bd5da0943db524e9317a5f1e90a3ae88af2"
-    sha256 cellar: :any, big_sur:       "3a13526e660b8d2b6725a48000be76f4d79e4178e516c137b807968e0faabdd8"
-    sha256 cellar: :any, catalina:      "e92c22994edb092737b72ea43911063ae2fdafe8b98692fbcadaced1f0b31f74"
-    sha256 cellar: :any, mojave:        "ac57fc123c84d43490749f55e7e6ed0605821fbbe62fc0d503c71c00f568137b"
+    sha256 cellar: :any,                 arm64_big_sur: "f23024af4d1356f53bb5878fbbb41bd5da0943db524e9317a5f1e90a3ae88af2"
+    sha256 cellar: :any,                 big_sur:       "3a13526e660b8d2b6725a48000be76f4d79e4178e516c137b807968e0faabdd8"
+    sha256 cellar: :any,                 catalina:      "e92c22994edb092737b72ea43911063ae2fdafe8b98692fbcadaced1f0b31f74"
+    sha256 cellar: :any,                 mojave:        "ac57fc123c84d43490749f55e7e6ed0605821fbbe62fc0d503c71c00f568137b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e9ca3d9ff14379dd9083cb9c44883f15059bcf53ee75774e45edb40593d6e1d"
   end
 
   head do
