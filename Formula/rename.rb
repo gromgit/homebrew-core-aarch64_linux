13cd0b0abfe0400a889c2eb5447ceb9e6ac93aa8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 08:59:11 +0000

    rename: update 1.601 bottle.

--- Formula/rename.rb
+++ Formula/rename.rb
@@ -11,6 +11,7 @@ class Rename < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "308b9f76cf8386eb9c5835204233f0869cc566d9995b383a5215649e8b1c7a48"
     sha256 cellar: :any_skip_relocation, catalina:      "2f1c70cacb289e2286bc6ec1e47319d197c2f0d74f8474b303aa2cb9aad8bb0e"
     sha256 cellar: :any_skip_relocation, mojave:        "2f1c70cacb289e2286bc6ec1e47319d197c2f0d74f8474b303aa2cb9aad8bb0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a88a0c2cd57632633f5ab3e9078f22903b3c12f0520276c3e408570ff10dee14"
   end
 
   depends_on "pod2man" => :build
