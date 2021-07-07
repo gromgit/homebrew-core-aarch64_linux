abfef89b744bd4880739856e6e2686befaa8344a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-07 01:27:13 +0000

    juju: update 2.9.5 bottle.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -18,6 +18,7 @@ class Juju < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "21647fb5ac56df194ca34801d303811fcab7e48febee613a95e86775f06a4ed0"
     sha256 cellar: :any_skip_relocation, catalina:      "0dfb4de7fc247ae09e0f80c7818ba6b6d5376ffc849e1ff4b8815c7943af3f94"
     sha256 cellar: :any_skip_relocation, mojave:        "22464ef3006925ad094e955a70d6dcb0006d72f71a5cabf7427bffa03be139ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2cef8aff248b31b800fa111ae1a45eb3ca7c18203f2c0d2ccf145894eaf51bd"
   end
 
   depends_on "go" => :build
