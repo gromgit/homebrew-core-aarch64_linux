1cf1bf2f5317d3509af62c0bbe6174e9a7c1b115
Nanda H Krishna <me@nandahkrishna.com>
2021-07-07 16:56:27 +0000

    graphqurl: add 1.0.1 bottle.

--- Formula/graphqurl.rb
+++ Formula/graphqurl.rb
@@ -7,6 +7,13 @@ class Graphqurl < Formula
   sha256 "c6dfb4106d5b8b0860c0df5dffd0cc75095d280ad4841bda25a6ef0b9a75e199"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "138b68d4fffc64cd4ce86e07b618ccfa561aa15a700e8c08c66b20b65797ba70"
+    sha256 cellar: :any_skip_relocation, big_sur:       "5300156ac1794e98e8e9e8f261f469ae7a6749631dfd55c8374054f425e83cb4"
+    sha256 cellar: :any_skip_relocation, catalina:      "5300156ac1794e98e8e9e8f261f469ae7a6749631dfd55c8374054f425e83cb4"
+    sha256 cellar: :any_skip_relocation, mojave:        "5300156ac1794e98e8e9e8f261f469ae7a6749631dfd55c8374054f425e83cb4"
+  end
+
   depends_on "node"
 
   def install
