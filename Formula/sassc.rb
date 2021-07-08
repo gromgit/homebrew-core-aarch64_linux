6045f321d81e23e791a03d2f44e2cf337fd446a1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 05:19:33 +0000

    sassc: update 3.6.2 bottle.

--- Formula/sassc.rb
+++ Formula/sassc.rb
@@ -8,10 +8,11 @@ class Sassc < Formula
   head "https://github.com/sass/sassc.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ae86a3868be7edf32aadf47abd949d3806789f5edf319c4d86120a05fee9053d"
-    sha256 cellar: :any, big_sur:       "fe3a719ec1b2b01385924b8cb3bbb758d006ff3dbd75b1c3691ce09a43d1ebcd"
-    sha256 cellar: :any, catalina:      "0826a1c50657da448806febb03694bce523e60ac56e7dd0de7362fe4b41f2277"
-    sha256 cellar: :any, mojave:        "6aa4de7c6d9b1b64beda27f0c06a6d8c9224616b74ae48ea8e706f166b374ce9"
+    sha256 cellar: :any,                 arm64_big_sur: "ae86a3868be7edf32aadf47abd949d3806789f5edf319c4d86120a05fee9053d"
+    sha256 cellar: :any,                 big_sur:       "fe3a719ec1b2b01385924b8cb3bbb758d006ff3dbd75b1c3691ce09a43d1ebcd"
+    sha256 cellar: :any,                 catalina:      "0826a1c50657da448806febb03694bce523e60ac56e7dd0de7362fe4b41f2277"
+    sha256 cellar: :any,                 mojave:        "6aa4de7c6d9b1b64beda27f0c06a6d8c9224616b74ae48ea8e706f166b374ce9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4575b459543c822d40a074f73591b1865790aa8977e7dc12148c10c858d55203"
   end
 
   depends_on "autoconf" => :build
