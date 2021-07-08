d8ae4906ee8d06dcd7da72b386d87f8099545c5f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 20:41:43 +0000

    miniupnpc: update 2.2.2 bottle.

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -14,10 +14,11 @@ class Miniupnpc < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "8895acaf9fa55bb5cd0f5feb6cd104512657494035ae1650a8b73dccdded5b0f"
-    sha256 cellar: :any, big_sur:       "e1295aef25a9cfbcbbace72d1a2a76aff88c5334848b4ce2f88fde84a326ff62"
-    sha256 cellar: :any, catalina:      "5b3510471e85184f82cb7bc594d819c6a303e44d2853b726c708c1b9b2fba245"
-    sha256 cellar: :any, mojave:        "59b55d5ef7cb08a7ee55b6c06f7313b58ed520e4f84cf74ca77e6066c3e08d1e"
+    sha256 cellar: :any,                 arm64_big_sur: "8895acaf9fa55bb5cd0f5feb6cd104512657494035ae1650a8b73dccdded5b0f"
+    sha256 cellar: :any,                 big_sur:       "e1295aef25a9cfbcbbace72d1a2a76aff88c5334848b4ce2f88fde84a326ff62"
+    sha256 cellar: :any,                 catalina:      "5b3510471e85184f82cb7bc594d819c6a303e44d2853b726c708c1b9b2fba245"
+    sha256 cellar: :any,                 mojave:        "59b55d5ef7cb08a7ee55b6c06f7313b58ed520e4f84cf74ca77e6066c3e08d1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a9b866d13fd88d34f1c0728cf08dc86556c473d42e486409ec1d95919db3659b"
   end
 
   def install
