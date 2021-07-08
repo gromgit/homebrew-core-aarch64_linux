4821300a8cba7d5cf6203bbf6b1a3c6a472844c2
Rui Chen <rui@chenrui.dev>
2021-07-08 00:50:35 -0400

    ballerina 1.2.14
    * Update binary name: ballerina to bal
    * Add bal alias
    
    Closes #80776.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,8 +1,8 @@
 class Ballerina < Formula
   desc "Programming Language for Network Distributed Applications"
   homepage "https://ballerina.io"
-  url "https://dist.ballerina.io/downloads/1.2.13/ballerina-1.2.13.zip"
-  sha256 "ba2b6cbf09f5129a72afa3f494da5c7304d9321b32c4a1504c5a2b11644c2c57"
+  url "https://dist.ballerina.io/downloads/1.2.14/ballerina-1.2.14.zip"
+  sha256 "9a8fe617235dfbc29c6d099ac1acf3c47b6b482e44cab769eadfe28d766745d6"
   license "Apache-2.0"
 
   livecheck do
@@ -21,9 +21,9 @@ class Ballerina < Formula
     # Remove Windows files
     rm Dir["bin/*.bat"]
 
-    chmod 0755, "bin/ballerina"
+    chmod 0755, "bin/bal"
 
-    bin.install "bin/ballerina"
+    bin.install "bin/bal"
     libexec.install Dir["*"]
     bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
   end
@@ -35,7 +35,7 @@ class Ballerina < Formula
         io:println("Hello, World!");
       }
     EOS
-    output = shell_output("#{bin}/ballerina run helloWorld.bal")
+    output = shell_output("#{bin}/bal run helloWorld.bal")
     assert_equal "Hello, World!", output.chomp
   end
 end
