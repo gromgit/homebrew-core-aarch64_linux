c732319930e25d0ab5a77044cecd3f3271bb9f27
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-08 02:20:56 +0000

    nano: update 5.8 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -15,6 +15,7 @@ class Nano < Formula
     sha256 big_sur:       "39c0fdeb6069c928f0a71b9affa20fcc330ddf55e1f39178cf246a0a3806830f"
     sha256 catalina:      "f48cedf405592c2e8479b26ff199a1ce339a97b511eb7afb368e638f7e09f5b6"
     sha256 mojave:        "450b2c6435a9e91d32b2c7caca61b800bb2d8ccf8218bc7c58a4e0d0b7e3efcf"
+    sha256 x86_64_linux:  "05ff586576fe27da161ac36ca18c272a378ec842440525011f8f87dded0e766c"
   end
 
   depends_on "pkg-config" => :build
