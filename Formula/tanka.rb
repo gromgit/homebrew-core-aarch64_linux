fe96529e0df72b624cb225ea81f67325b2d35c67
Rui Chen <rui@chenrui.dev>
2021-07-08 09:27:20 -0400

    tanka 0.17.1
    Closes #80822.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tanka.rb
+++ Formula/tanka.rb
@@ -2,8 +2,8 @@ class Tanka < Formula
   desc "Flexible, reusable and concise configuration for Kubernetes using Jsonnet"
   homepage "https://tanka.dev"
   url "https://github.com/grafana/tanka.git",
-      tag:      "v0.17.0",
-      revision: "3a1289b985301dc55bc24e95bcb1d3e1bdc2e825"
+      tag:      "v0.17.1",
+      revision: "3bc0931b899432ab1e9edcff9c4e81a1bd3250b7"
   license "Apache-2.0"
   head "https://github.com/grafana/tanka.git", branch: "main"
 
