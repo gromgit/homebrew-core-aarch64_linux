67092f7655df663938d4b542a10b20fd09b6d6d2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-08 01:36:17 +0000

    rbenv: update 1.1.2 bottle.

--- Formula/rbenv.rb
+++ Formula/rbenv.rb
@@ -7,12 +7,13 @@ class Rbenv < Formula
   head "https://github.com/rbenv/rbenv.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "19d0b9e3b11bcacdf692e78248c1b5764f7d3f4b58f2b83ad7b39562863ba82e"
-    sha256 cellar: :any, big_sur:       "60b045c8843745c45d01616ee3f71b91f6a16ee09c47e23a7817a3edabeaccfd"
-    sha256 cellar: :any, catalina:      "503ed6d818502f00f031b9f49461934e252b9bfba2876e90a326fc27bb1052d6"
-    sha256 cellar: :any, mojave:        "d1019098dee8d037587069398e5ad04e6d736f834dc44ae73943bec46b10b260"
-    sha256 cellar: :any, high_sierra:   "b5984102794a9d39388ca1f6ec77965aeea29b971cc00cb5af8ede8ee6c926d6"
-    sha256 cellar: :any, sierra:        "873175a851e5aa4f5b3438072030b945c252f08a9a07760c64dc045e2cce4724"
+    sha256 cellar: :any,                 arm64_big_sur: "19d0b9e3b11bcacdf692e78248c1b5764f7d3f4b58f2b83ad7b39562863ba82e"
+    sha256 cellar: :any,                 big_sur:       "60b045c8843745c45d01616ee3f71b91f6a16ee09c47e23a7817a3edabeaccfd"
+    sha256 cellar: :any,                 catalina:      "503ed6d818502f00f031b9f49461934e252b9bfba2876e90a326fc27bb1052d6"
+    sha256 cellar: :any,                 mojave:        "d1019098dee8d037587069398e5ad04e6d736f834dc44ae73943bec46b10b260"
+    sha256 cellar: :any,                 high_sierra:   "b5984102794a9d39388ca1f6ec77965aeea29b971cc00cb5af8ede8ee6c926d6"
+    sha256 cellar: :any,                 sierra:        "873175a851e5aa4f5b3438072030b945c252f08a9a07760c64dc045e2cce4724"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "47259687d198e3ec4ccceec09a1701a0804381ad73e8b01fb9756736ce8b78f6"
   end
 
   depends_on "ruby-build"
