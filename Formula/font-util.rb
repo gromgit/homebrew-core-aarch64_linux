cf8ece5a41c9037ee3cd34dbbe8b21aba04e047e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 22:51:24 +0000

    font-util: update 1.3.2 bottle.

--- Formula/font-util.rb
+++ Formula/font-util.rb
@@ -11,6 +11,7 @@ class FontUtil < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "3b1ccaebd2a3efc9cd2daac2e9e740362349a1847e4c169ebe3d475ff3b31a8d"
     sha256 cellar: :any_skip_relocation, mojave:        "c606e6261604b92184fad13ea3fa0cdf377fe09bd3280389ae3c2b32b15b4785"
     sha256 cellar: :any_skip_relocation, high_sierra:   "e5a86e6341ec516e06c3c22cc347ef5329add351e8aeb1f3aac3cf981dd81d51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bdf7a30d254fd3e0d6c6df366dc79b0c32ce1fe1aa07350d58548822f21b55e"
   end
 
   depends_on "pkg-config" => [:build, :test]
