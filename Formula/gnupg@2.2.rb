1539e93f9e84eb3862d7cc677c4cd69116f378b2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 08:57:17 +0000

    gnupg@2.2: update 2.2.29 bottle.

--- Formula/gnupg@2.2.rb
+++ Formula/gnupg@2.2.rb
@@ -15,6 +15,7 @@ class GnupgAT22 < Formula
     sha256 big_sur:       "9c62310955d92e2d226a53e545d4419e7404109b75972dee8bcdd2b6ab64a4e7"
     sha256 catalina:      "8613f0020417b185db21aa7be556aef7cfacc4c6b07f21083bbee2770ae1f8b4"
     sha256 mojave:        "43f2a5ca049960284a7434b6c6ec4c09f7782fa9f13af7878dacf2d142caa1f9"
+    sha256 x86_64_linux:  "57eb2151612b2a32102ae2c3a48e11c0cee53b528b884e164e1bc0ccf7c90566"
   end
 
   keg_only :versioned_formula
