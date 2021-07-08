b1a54ad63917b851044e1db4b6f206b5d4cd2785
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 22:30:07 +0000

    libserdes: update 6.2.0 bottle.

--- Formula/libserdes.rb
+++ Formula/libserdes.rb
@@ -8,10 +8,11 @@ class Libserdes < Formula
   head "https://github.com/confluentinc/libserdes.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "29cd5069db8ce93a2aae253ad3436046e40ddf0e31cfb713022a0e80a8e0d872"
-    sha256 cellar: :any, big_sur:       "ae01cfc3bec7b2ae9778b5701d1e30de2bfa9fd8a9dc64d0a05280ef5bcc7b47"
-    sha256 cellar: :any, catalina:      "8845db8f2c79e1f32a41de8a4c2786b986e619b8181b6626355cd31171202ebf"
-    sha256 cellar: :any, mojave:        "a771b808d04dab53fe808d430033676cf3adde32e01aad699114db50b504a62c"
+    sha256 cellar: :any,                 arm64_big_sur: "29cd5069db8ce93a2aae253ad3436046e40ddf0e31cfb713022a0e80a8e0d872"
+    sha256 cellar: :any,                 big_sur:       "ae01cfc3bec7b2ae9778b5701d1e30de2bfa9fd8a9dc64d0a05280ef5bcc7b47"
+    sha256 cellar: :any,                 catalina:      "8845db8f2c79e1f32a41de8a4c2786b986e619b8181b6626355cd31171202ebf"
+    sha256 cellar: :any,                 mojave:        "a771b808d04dab53fe808d430033676cf3adde32e01aad699114db50b504a62c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18f523375cd6d14a8c489899e9fc8554be18ad9e448616d985ce167b6f617122"
   end
 
   depends_on "avro-c"
