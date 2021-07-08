f49b1669b38c87c6b36897c555ba075f220f697c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 23:29:56 +0000

    terracognita: update 0.6.4 bottle.

--- Formula/terracognita.rb
+++ Formula/terracognita.rb
@@ -11,6 +11,7 @@ class Terracognita < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "7cf78bf712f39334a49d05faed61c57c5ae431b2da03e2978462c51c6108133a"
     sha256 cellar: :any_skip_relocation, catalina:      "ebe755ce13ebff04cacce0edf79b3af170c490c53265ef2fd3ef0ac87a3359da"
     sha256 cellar: :any_skip_relocation, mojave:        "2b38f5cdbd101cc038bfc6ec0570c9b989ae358e7ed604134e9bd41a8155b275"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a28151589641507d8c0da31316b3a25bd4332b63684903286ed4620e1b61fccd"
   end
 
   depends_on "go" => :build
