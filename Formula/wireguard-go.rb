a41f9231423eb7a27b22def65b48cb7d5bbf75f6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 20:42:36 +0000

    wireguard-go: update 0.0.20210424 bottle.

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -16,6 +16,7 @@ class WireguardGo < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9f3812acc99aaf982518460b33d67930b824e8e86bfd00e9303dd0fb7e94cd74"
     sha256 cellar: :any_skip_relocation, catalina:      "7067cc06c22612f886694f5471b1b7b7d196f9047c1939745f8140d5b1695f1f"
     sha256 cellar: :any_skip_relocation, mojave:        "be446fceccc238dbfe68c3c23cb03feb1e911245934bba66007c263bfeb9114c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93b2808f28f71bc2dca45268cd82a86db23b9f9264df87e0946eef577b62d984"
   end
 
   depends_on "go" => :build
