24adb2d4d727783b14b7f72a07d0df9310aa1c49
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 22:15:09 +0000

    gcc@7: update 7.5.0_4 bottle.

--- Formula/gcc@7.rb
+++ Formula/gcc@7.rb
@@ -16,9 +16,10 @@ class GccAT7 < Formula
   end
 
   bottle do
-    sha256 big_sur:  "f53816251cabd7c7cc5818af052ad0cccab189156fd63243be2b1bee21d8a0b7"
-    sha256 catalina: "359aee8f81fae1591bb685a6c38dbcace62d32d1ba7a69b01c15061ce29e61bb"
-    sha256 mojave:   "95659aa77c264356df8c2eb9d24800aba62f0d308b86d601e0d259885700aebf"
+    sha256 big_sur:      "f53816251cabd7c7cc5818af052ad0cccab189156fd63243be2b1bee21d8a0b7"
+    sha256 catalina:     "359aee8f81fae1591bb685a6c38dbcace62d32d1ba7a69b01c15061ce29e61bb"
+    sha256 mojave:       "95659aa77c264356df8c2eb9d24800aba62f0d308b86d601e0d259885700aebf"
+    sha256 x86_64_linux: "a4e1eccf168ea1fb7b02b186f3dbe9f5c3c44b95fb815ddf692b5f13698d78ca"
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
