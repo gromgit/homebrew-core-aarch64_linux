87f73f06a6c3b345b5c6cae213e884ea0b1be731
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 20:49:52 +0000

    libnet: update 1.2 bottle.

--- Formula/libnet.rb
+++ Formula/libnet.rb
@@ -6,11 +6,12 @@ class Libnet < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bc8839eea92ce445c790f503ec9342bbc254fba52751a3ac0ed90b5d13bed2f6"
-    sha256 cellar: :any, big_sur:       "9ecd86c12061ee31384cc784031ee4b0fb05e3ae79ff6c4c6b3f2e61690e8ad4"
-    sha256 cellar: :any, catalina:      "0ecfbf2539a6e051ca8aa5962c0ee7cb57ffd173cf654b0eec8152c1a3fbf133"
-    sha256 cellar: :any, mojave:        "cadba638a54f4d5646a3510439ab89317ed23df3c45b12704b78065bb127fbc4"
-    sha256 cellar: :any, high_sierra:   "44e7b11e8f900f9d6f8e0d1a5deed99c46078dd2dbc997937f713ce5a1ac0f38"
+    sha256 cellar: :any,                 arm64_big_sur: "bc8839eea92ce445c790f503ec9342bbc254fba52751a3ac0ed90b5d13bed2f6"
+    sha256 cellar: :any,                 big_sur:       "9ecd86c12061ee31384cc784031ee4b0fb05e3ae79ff6c4c6b3f2e61690e8ad4"
+    sha256 cellar: :any,                 catalina:      "0ecfbf2539a6e051ca8aa5962c0ee7cb57ffd173cf654b0eec8152c1a3fbf133"
+    sha256 cellar: :any,                 mojave:        "cadba638a54f4d5646a3510439ab89317ed23df3c45b12704b78065bb127fbc4"
+    sha256 cellar: :any,                 high_sierra:   "44e7b11e8f900f9d6f8e0d1a5deed99c46078dd2dbc997937f713ce5a1ac0f38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "755c46e11346388df5a8a3e2f50bf7bb449abfc889abfc561a1784e5d17c8b97"
   end
 
   depends_on "doxygen" => :build
