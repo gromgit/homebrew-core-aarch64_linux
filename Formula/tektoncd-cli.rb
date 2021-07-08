d28b36dad47c311643059d842dab5d3e6eaa3b37
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 05:21:50 +0000

    tektoncd-cli: update 0.19.1 bottle.

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -15,6 +15,7 @@ class TektoncdCli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "aca5a966205a0060e8abea04025809772245c25dcdc64976f28ee77f6481b181"
     sha256 cellar: :any_skip_relocation, catalina:      "3e3fa67ce9fa22f4fbe89be521407cceb6267f8302954a1557a7a83f8b9e8f56"
     sha256 cellar: :any_skip_relocation, mojave:        "9b274ec482c44afaabfe9805532864e714b51f96e4cd36ae9a952352756861de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df9c21d7eeb768b18a43ccd818bc4fe561acc5a25b164ed951d226d9c7bb4032"
   end
 
   depends_on "go" => :build
