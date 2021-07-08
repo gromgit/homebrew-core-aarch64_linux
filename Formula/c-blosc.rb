b2bc213b4f8c3d683102db2ba8ac03cab80347fd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 20:39:59 +0000

    c-blosc: update 1.21.0 bottle.

--- Formula/c-blosc.rb
+++ Formula/c-blosc.rb
@@ -6,10 +6,11 @@ class CBlosc < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6c256c5a6dd6a63e9cbc975fc1eb10e12f416b612c6513b71991b27f00ddbcd1"
-    sha256 cellar: :any, big_sur:       "e07d3e94bb0df9c9dad09f7093eac90fea34b22cd2f75a0ab8448e912d88ac07"
-    sha256 cellar: :any, catalina:      "217d36ff72906927ce554e653c700d9cde7a6f83d1698dc3b46a019c2ed7dfc6"
-    sha256 cellar: :any, mojave:        "4c52001ce5acf5b8f0c02563be063537788ba043c3c55badd10d8da618b7325d"
+    sha256 cellar: :any,                 arm64_big_sur: "6c256c5a6dd6a63e9cbc975fc1eb10e12f416b612c6513b71991b27f00ddbcd1"
+    sha256 cellar: :any,                 big_sur:       "e07d3e94bb0df9c9dad09f7093eac90fea34b22cd2f75a0ab8448e912d88ac07"
+    sha256 cellar: :any,                 catalina:      "217d36ff72906927ce554e653c700d9cde7a6f83d1698dc3b46a019c2ed7dfc6"
+    sha256 cellar: :any,                 mojave:        "4c52001ce5acf5b8f0c02563be063537788ba043c3c55badd10d8da618b7325d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9a536055c8966b86a5aae7197e0404831b12bea28add1b6d050e21a3d9975c63"
   end
 
   depends_on "cmake" => :build
