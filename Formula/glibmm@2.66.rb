c32b29aa83aa9d06bc5185df474dd1cd53e7a459
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 23:27:09 +0000

    glibmm@2.66: update 2.66.1 bottle.

--- Formula/glibmm@2.66.rb
+++ Formula/glibmm@2.66.rb
@@ -16,6 +16,7 @@ class GlibmmAT266 < Formula
     sha256 cellar: :any, big_sur:       "c569e5397d5036d4ba2eb562e9f88113b5be7ef9a631ea867f3426ddf29c5e89"
     sha256 cellar: :any, catalina:      "972e77c4e4ffaee50813406d0c98524e2f9aa84e8f39075b6cd3d76346604edc"
     sha256 cellar: :any, mojave:        "09aa0d0e43347d8bf94156004823840c39724d16632a51158a72687e3f707a2e"
+    sha256               x86_64_linux:  "3476ab13af317e82c15127e72162a0f32e6c1f02edca8ddd946e496c14eace1b"
   end
 
   depends_on "meson" => :build
