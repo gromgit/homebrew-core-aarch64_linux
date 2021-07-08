5a292be1f0e8c2bcacdcb36ab1eadc576f1cf35c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 21:25:56 +0000

    mmctl: update 5.36.0 bottle.

--- Formula/mmctl.rb
+++ Formula/mmctl.rb
@@ -17,6 +17,7 @@ class Mmctl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1e14c73707eb41c21ad5b9b6aea1850b56b71b79dd7fa6e8ccd8ec48e888c162"
     sha256 cellar: :any_skip_relocation, catalina:      "9c7223fc02910173dabd773352af3617e92e1cfacc37337e43f5606cfeeef13d"
     sha256 cellar: :any_skip_relocation, mojave:        "4db09ecf5831227464cf41b6bd512e37a079406c124d32ec93b4ce14987772cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b588231aa08d4972a9aed9e0d57d022090dcfb295a2a32a8c17cb766d561ea2"
   end
 
   depends_on "go" => :build
