dc29a7d78160b3054f4231c5b74a8cdb7a66fdca
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 22:25:44 +0000

    k6: update 0.33.0 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -10,6 +10,7 @@ class K6 < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9630025b40877144cdb0048f13ab64f429bae7ff0140e51fa4944db6493839a5"
     sha256 cellar: :any_skip_relocation, catalina:      "09a62bfb818944b4bf8c6683ef8c4b5e1b564b65c6fbd6c47751b690ac8f3831"
     sha256 cellar: :any_skip_relocation, mojave:        "8431419ffeaf40640ba0555b747e48a71fb65576d2121a4bbf25ab73c70e2b2f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d2759d43340a9dd20a54ffcd150b312ec848714ca4eb058dde4d36fce4643e7"
   end
 
   depends_on "go" => :build
