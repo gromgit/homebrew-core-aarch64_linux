82204d78fcfccf7c48b090229919ede96c86d402
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-08 21:02:10 +0000

    kakoune: update 2020.09.01 bottle.

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -24,10 +24,11 @@ class Kakoune < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "6a6af6d41f893e851691984867efeee176098f47c881d5fe77d8534bc4086375"
-    sha256 cellar: :any, big_sur:       "61064437727a2eb062b89941b25fb46c017d350e8947e867e72f51f591d030ad"
-    sha256 cellar: :any, catalina:      "9cb8ffd67651eab6f269daab2bbf4e66adf4f7dad4029a3285631a3615cf1514"
-    sha256 cellar: :any, mojave:        "00c9127f14d643eee79fc64d02874bc131dad426fb11580b5d4fa43a3a51007c"
+    sha256 cellar: :any,                 arm64_big_sur: "6a6af6d41f893e851691984867efeee176098f47c881d5fe77d8534bc4086375"
+    sha256 cellar: :any,                 big_sur:       "61064437727a2eb062b89941b25fb46c017d350e8947e867e72f51f591d030ad"
+    sha256 cellar: :any,                 catalina:      "9cb8ffd67651eab6f269daab2bbf4e66adf4f7dad4029a3285631a3615cf1514"
+    sha256 cellar: :any,                 mojave:        "00c9127f14d643eee79fc64d02874bc131dad426fb11580b5d4fa43a3a51007c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d1f01dd8fea918ffb45c5fa7e2f3d233637768eaa9d2b7a65d1df136f9a03364"
   end
 
   depends_on macos: :high_sierra # needs C++17
