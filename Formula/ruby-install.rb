deee368bd3cbe4084d19a69517a64bb7b3e7b940
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 18:59:13 +0000

    ruby-install: update 0.8.2 bottle.

--- Formula/ruby-install.rb
+++ Formula/ruby-install.rb
@@ -11,6 +11,7 @@ class RubyInstall < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
     sha256 cellar: :any_skip_relocation, catalina:      "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
     sha256 cellar: :any_skip_relocation, mojave:        "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d19d4a89f08d447d522387e362b5e4627de5438e06396e2035fe735dc192d7de"
   end
 
   def install
