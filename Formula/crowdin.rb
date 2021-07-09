da8e0d52c7d5c5453e69d27dc0e7e8aaf49e4f9e
Caleb Xu <calebcenter@live.com>
2021-07-09 00:37:31 -0400

    crowdin 3.6.4
    Closes #80863.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,8 +1,8 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://github.com/crowdin/crowdin-cli/releases/download/3.6.3/crowdin-cli.zip"
-  sha256 "4800e0ea6689db10f690d30a8a03c01940103ed211dacdc8362611a99a91bc55"
+  url "https://github.com/crowdin/crowdin-cli/releases/download/3.6.4/crowdin-cli.zip"
+  sha256 "5c298378fe49397b25ba64dbdaa7beb5274190330ae5ad796df15e1934d87588"
   license "MIT"
 
   livecheck do
