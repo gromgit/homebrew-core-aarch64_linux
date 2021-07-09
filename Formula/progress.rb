828e0ce9da00598fd892cd527e2280771a7d3a2f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 05:55:19 +0000

    progress: update 0.16 bottle.

--- Formula/progress.rb
+++ Formula/progress.rb
@@ -11,6 +11,7 @@ class Progress < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "21a1663abf8e1a60baf99a3ae46d41883cd5136fabbe77dd07987e0730b08ec0"
     sha256 cellar: :any_skip_relocation, catalina:      "804256c3be440464694fe8f0e9b94f323860f9fa2304c8984dc0e8cbbce43fab"
     sha256 cellar: :any_skip_relocation, mojave:        "5c834228aa86062624e1d2f6f3c386607d3151842218eb9c70529e1485ee1e5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31559faf7e06989812f6ea8069a8baf8de1b1abcd68c41dba11f5f2004595f65"
   end
 
   uses_from_macos "ncurses"
