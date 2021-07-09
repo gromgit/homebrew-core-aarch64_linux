df541527c95a975f2d0ea30e831cd62f4a67b8ea
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 00:17:41 +0000

    rdfind: update 1.4.1_1 bottle.

--- Formula/rdfind.rb
+++ Formula/rdfind.rb
@@ -11,11 +11,12 @@ class Rdfind < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "354f66f386e4ea54db4efcdeca59c897fa985ec2567ec7677eb62e1d975bbf6c"
-    sha256 cellar: :any, big_sur:       "ade2aaffee4c52e143fdcd0a22fae933e60e4c3d5057e4bc74a9aaffaf3c2146"
-    sha256 cellar: :any, catalina:      "e890406a4cbbd8d026a4c583644efa537433ac71c095a1e582b0454d85a87d00"
-    sha256 cellar: :any, mojave:        "489e104d2c5e5d939439f5b100cd97e19ed070181d355b49fbd1ad2b3320d789"
-    sha256 cellar: :any, high_sierra:   "2ce91e3b8a129c0fadb57fba46074e74e0d896287c23eb1844cd99e5eef093b1"
+    sha256 cellar: :any,                 arm64_big_sur: "354f66f386e4ea54db4efcdeca59c897fa985ec2567ec7677eb62e1d975bbf6c"
+    sha256 cellar: :any,                 big_sur:       "ade2aaffee4c52e143fdcd0a22fae933e60e4c3d5057e4bc74a9aaffaf3c2146"
+    sha256 cellar: :any,                 catalina:      "e890406a4cbbd8d026a4c583644efa537433ac71c095a1e582b0454d85a87d00"
+    sha256 cellar: :any,                 mojave:        "489e104d2c5e5d939439f5b100cd97e19ed070181d355b49fbd1ad2b3320d789"
+    sha256 cellar: :any,                 high_sierra:   "2ce91e3b8a129c0fadb57fba46074e74e0d896287c23eb1844cd99e5eef093b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f27f8e434303de8a0af302a38e6734b74f7044f3795bee5a5f2a8a0c2427145f"
   end
 
   depends_on "nettle"
