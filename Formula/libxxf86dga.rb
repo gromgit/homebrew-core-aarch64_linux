04cd1836e519f417a98902866ddc0c07f9f8f750
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 23:00:30 +0000

    libxxf86dga: update 1.1.5 bottle.

--- Formula/libxxf86dga.rb
+++ Formula/libxxf86dga.rb
@@ -6,11 +6,12 @@ class Libxxf86dga < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0201b2b16b731237e673e04fa85fbf60b51fc75c6e7918e1f0ae76e6560e8cbb"
-    sha256 cellar: :any, big_sur:       "f2df468f6664efce62f76994f334e5ddbcad8224fd7a38bf0379c4c9d9a4b0c4"
-    sha256 cellar: :any, catalina:      "15c9455a9f38b82ed8d6254ed5316426426fa5ae1451f3f0261adaf4c44f8c05"
-    sha256 cellar: :any, mojave:        "0c6b3b1caa96edeb68748295ecfd60001e50cffe3275dc156534d955bfc4951a"
-    sha256 cellar: :any, high_sierra:   "6d5cc78dafe39697dc39b8b061c74bff94046f9bf9819eded1c6575a5c4b9f4c"
+    sha256 cellar: :any,                 arm64_big_sur: "0201b2b16b731237e673e04fa85fbf60b51fc75c6e7918e1f0ae76e6560e8cbb"
+    sha256 cellar: :any,                 big_sur:       "f2df468f6664efce62f76994f334e5ddbcad8224fd7a38bf0379c4c9d9a4b0c4"
+    sha256 cellar: :any,                 catalina:      "15c9455a9f38b82ed8d6254ed5316426426fa5ae1451f3f0261adaf4c44f8c05"
+    sha256 cellar: :any,                 mojave:        "0c6b3b1caa96edeb68748295ecfd60001e50cffe3275dc156534d955bfc4951a"
+    sha256 cellar: :any,                 high_sierra:   "6d5cc78dafe39697dc39b8b061c74bff94046f9bf9819eded1c6575a5c4b9f4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e90a0ed3aaca33cb9473fcd74f0a7eaf4fb2301ada2d1d31f865941f92c81589"
   end
 
   depends_on "pkg-config" => :build
