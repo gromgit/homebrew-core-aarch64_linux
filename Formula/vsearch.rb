a8c94851496ee8dd9044600bfc0b6ae0f51720cc
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 23:29:16 +0000

    vsearch: update 2.17.1 bottle.

--- Formula/vsearch.rb
+++ Formula/vsearch.rb
@@ -10,6 +10,7 @@ class Vsearch < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d05f94ce99512a34a86b4175b475690c602717b9db66a397f7590f0fb7dddd48"
     sha256 cellar: :any_skip_relocation, catalina:      "f49645745a5e19d2769bf85107704efa369378998b2032020b8fe53c674d1742"
     sha256 cellar: :any_skip_relocation, mojave:        "a6b4c046412b638685e4a4e31fc66c1945f6c91fac9f657130a75ed76e3fbf4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc66ec8e17f721818e3b16f05e66c545c1167a78c19e423f93e6ae73aa4efd67"
   end
 
   depends_on "autoconf" => :build
