35a4a830db366a398962bc1a4700f684572c4fed
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 06:42:50 +0000

    gitlab-gem: update 4.17.0 bottle.

--- Formula/gitlab-gem.rb
+++ Formula/gitlab-gem.rb
@@ -10,6 +10,7 @@ class GitlabGem < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "7c4a0fb41401e668a957c23d934896ead423f78ee65099b451c34a5b5243224f"
     sha256 cellar: :any_skip_relocation, catalina:      "9fb83bdf349a57916534fd40a2e38db4a893b713207abe572117e21d21e9df7b"
     sha256 cellar: :any_skip_relocation, mojave:        "2bd6150b2c26c4b746ffc8c2f43c05b311ec33c08f88a82946d08e63f6dea9d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d46229291b4e82400ed94407d29c04390edba62b6b1e007eedbdefe9b7ba1136"
   end
 
   uses_from_macos "ruby", since: :catalina
