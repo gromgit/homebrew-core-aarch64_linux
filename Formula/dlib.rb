8dfbe1a7c3a995faafd613713f18e27c52eb657b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 23:03:35 +0000

    dlib: update 19.22 bottle.

--- Formula/dlib.rb
+++ Formula/dlib.rb
@@ -7,10 +7,11 @@ class Dlib < Formula
   head "https://github.com/davisking/dlib.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "db4b583ba9e3005a3c2d36fdc5ba78e2cb3ccdebd85216c8e7a00ccbdd81106e"
-    sha256 cellar: :any, big_sur:       "5d20cfc1befae91082d391364cb07dfc68d99713694ac81539d027e2138cc5bc"
-    sha256 cellar: :any, catalina:      "158a5e823cfda7ed8ef3ea9439d28b7c6508bb108f28fe8c94639bd35a9620e2"
-    sha256 cellar: :any, mojave:        "40748e73bb88c567e6ca5b991f8e4282e46422e625ec2dcc0ff0f11a5ed76f4c"
+    sha256 cellar: :any,                 arm64_big_sur: "db4b583ba9e3005a3c2d36fdc5ba78e2cb3ccdebd85216c8e7a00ccbdd81106e"
+    sha256 cellar: :any,                 big_sur:       "5d20cfc1befae91082d391364cb07dfc68d99713694ac81539d027e2138cc5bc"
+    sha256 cellar: :any,                 catalina:      "158a5e823cfda7ed8ef3ea9439d28b7c6508bb108f28fe8c94639bd35a9620e2"
+    sha256 cellar: :any,                 mojave:        "40748e73bb88c567e6ca5b991f8e4282e46422e625ec2dcc0ff0f11a5ed76f4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4f62972f4cf86bda1cca8d5163dfa544e7f96f0084ecd72333d51245685f207"
   end
 
   depends_on "cmake" => :build
