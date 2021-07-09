a4beda3f5a906bb9b588819c22dd8b4a54571d02
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 22:18:17 +0000

    vlang: update 0.2.2 bottle.

--- Formula/vlang.rb
+++ Formula/vlang.rb
@@ -16,6 +16,7 @@ class Vlang < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9be25862e7c69582ef1c8ee312e10e76988e5247439657f704078f56a5f6abc3"
     sha256 cellar: :any_skip_relocation, catalina:      "d4e2bde9c42995a3c348f1ede4a78fa579a22b601144cc0b8adeb009d310c1b7"
     sha256 cellar: :any_skip_relocation, mojave:        "0fe9d3b759400cee1a53c10f1a913a541d2f2f151822ff82454e953796c931cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb13270e51de665c0253e5c0f900bf88fcc24f870326ef6e1face32d7b6111fb"
   end
 
   resource "vc" do
