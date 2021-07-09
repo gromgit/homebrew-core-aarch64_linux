9ccc2296f39645eea2bfc5b7cb35518fb18dc178
Michael Cho <cho-m@tuta.io>
2021-07-09 09:54:00 -0700

    monkeysphere: fix runtime error on Big Sur and Linux
    Closes #80914.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/monkeysphere.rb
+++ Formula/monkeysphere.rb
@@ -1,9 +1,10 @@
 class Monkeysphere < Formula
   desc "Use the OpenPGP web of trust to verify ssh connections"
-  homepage "http://web.monkeysphere.info/"
+  homepage "https://web.monkeysphere.info/"
   url "https://deb.debian.org/debian/pool/main/m/monkeysphere/monkeysphere_0.44.orig.tar.gz"
   sha256 "6ac6979fa1a4a0332cbea39e408b9f981452d092ff2b14ed3549be94918707aa"
-  revision 3
+  license "GPL-3.0-or-later"
+  revision 4
   head "git://git.monkeysphere.info/monkeysphere"
 
   livecheck do
@@ -27,18 +28,35 @@ class Monkeysphere < Formula
 
   uses_from_macos "perl"
 
+  on_linux do
+    resource "Crypt::OpenSSL::Guess" do
+      url "https://cpan.metacpan.org/authors/id/A/AK/AKIYM/Crypt-OpenSSL-Guess-0.13.tar.gz"
+      sha256 "87c1dd7f0f80fcd3d1396bce9fd9962e7791e748dc0584802f8d10cc9585e743"
+    end
+  end
+
   resource "Crypt::OpenSSL::Bignum" do
     url "https://cpan.metacpan.org/authors/id/K/KM/KMX/Crypt-OpenSSL-Bignum-0.09.tar.gz"
     sha256 "234e72fb8396d45527e6fd45e43759c5c3f3a208cf8f29e6a22161a996fd42dc"
+  end
+
+  resource "Crypt::OpenSSL::RSA" do
+    url "https://cpan.metacpan.org/authors/id/T/TO/TODDR/Crypt-OpenSSL-RSA-0.31.tar.gz"
+    sha256 "4173403ad4cf76732192099f833fbfbf3cd8104e0246b3844187ae384d2c5436"
   end
 
   def install
     ENV.prepend_path "PATH", Formula["gnu-sed"].libexec/"gnubin"
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
 
-    resource("Crypt::OpenSSL::Bignum").stage do
-      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
-      system "make", "install"
+    res = resources
+    on_macos { res = [resource("Crypt::OpenSSL::Bignum")] if MacOS.version <= :catalina }
+
+    res.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make", "install"
+      end
     end
 
     ENV["PREFIX"] = prefix
