8d992fcbf3bce40b0f2bd49b1571d45b075f9693
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-09 16:11:59 +0000

    efm-langserver: update 0.0.34 bottle.

--- Formula/efm-langserver.rb
+++ Formula/efm-langserver.rb
@@ -8,11 +8,11 @@ class EfmLangserver < Formula
   head "https://github.com/mattn/efm-langserver.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cf1fe4444df6269c8fde32dab4cbfafd633720ef4a8e79381825da8a0e786c83"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a5d33694c505e7ad140ad56a165a1707f9adb690770b9190fd37833d46650321"
-    sha256 cellar: :any_skip_relocation, catalina:      "a5d33694c505e7ad140ad56a165a1707f9adb690770b9190fd37833d46650321"
-    sha256 cellar: :any_skip_relocation, mojave:        "a5d33694c505e7ad140ad56a165a1707f9adb690770b9190fd37833d46650321"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68b68f2d269614e57587b86605b5f94628ef921560462f4f721ea53d9184fade"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd68f0f61a26fd9ad8d1f992c8013c3df79d6097a3314f133f8fc63fdade8997"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2cb2b42f427eee054d89f9e12714524d52091763c6c5171a4195ccd7a1a0ed70"
+    sha256 cellar: :any_skip_relocation, catalina:      "2cb2b42f427eee054d89f9e12714524d52091763c6c5171a4195ccd7a1a0ed70"
+    sha256 cellar: :any_skip_relocation, mojave:        "2cb2b42f427eee054d89f9e12714524d52091763c6c5171a4195ccd7a1a0ed70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e133adbe3da135681890ebe868bd87acaf1e529cbeef1ac4e4d90516267ac358"
   end
 
   depends_on "go" => :build
