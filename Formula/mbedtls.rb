aab66c9ab3215cdd76783749be2678ce89e93278
EricFromCanada <enk3@outlook.com>
2021-07-09 15:15:57 -0400

    mbedtls 2.27.0

--- Formula/mbedtls.rb
+++ Formula/mbedtls.rb
@@ -1,8 +1,8 @@
 class Mbedtls < Formula
   desc "Cryptographic & SSL/TLS library"
   homepage "https://tls.mbed.org/"
-  url "https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.26.0.tar.gz"
-  sha256 "35d8d87509cd0d002bddbd5508b9d2b931c5e83747d087234cc7ad551d53fe05"
+  url "https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.27.0.tar.gz"
+  sha256 "4f6a43f06ded62aa20ef582436a39b65902e1126cbbe2fb17f394e9e9a552767"
   license "Apache-2.0"
   head "https://github.com/ARMmbed/mbedtls.git", branch: "development"
 
