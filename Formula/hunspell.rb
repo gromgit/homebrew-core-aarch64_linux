6ff1e7e98e55721182595f7e3375a81991a3266a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 22:15:41 +0000

    hunspell: update 1.7.0_2 bottle.

--- Formula/hunspell.rb
+++ Formula/hunspell.rb
@@ -8,10 +8,11 @@ class Hunspell < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "8d48ffd83009503b5a2c1b968ddd930e80f41731e02576d6dfea2f53fc50d97c"
-    sha256 cellar: :any, big_sur:       "20b0f5dc7973a4cca3a8c3ea5778938d169bde2d183210f64526df8bca9512f7"
-    sha256 cellar: :any, catalina:      "d5144178eba9ff325c297a5a0ae05f1caa9b1d567803250d5dbd86876e0718a3"
-    sha256 cellar: :any, mojave:        "eaabf6b66c21a94a7f6810287ff84e83fdef0ed6d3db635c8ad2c810d9f84d46"
+    sha256 cellar: :any,                 arm64_big_sur: "8d48ffd83009503b5a2c1b968ddd930e80f41731e02576d6dfea2f53fc50d97c"
+    sha256 cellar: :any,                 big_sur:       "20b0f5dc7973a4cca3a8c3ea5778938d169bde2d183210f64526df8bca9512f7"
+    sha256 cellar: :any,                 catalina:      "d5144178eba9ff325c297a5a0ae05f1caa9b1d567803250d5dbd86876e0718a3"
+    sha256 cellar: :any,                 mojave:        "eaabf6b66c21a94a7f6810287ff84e83fdef0ed6d3db635c8ad2c810d9f84d46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b57b04052406ed148c7c44e045586787293ecc9c27701e2e9c6308cdd63f3b9"
   end
 
   depends_on "autoconf" => :build
