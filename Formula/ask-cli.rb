c9023d25a0b36b14109fcf9d98bff3eb8ad5c23f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 19:24:54 +0000

    ask-cli: update 2.22.4 bottle.

--- Formula/ask-cli.rb
+++ Formula/ask-cli.rb
@@ -12,6 +12,7 @@ class AskCli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d294fde163d254091d70290b044ef203a8845936a368eb049ebd825b5b875fd9"
     sha256 cellar: :any_skip_relocation, catalina:      "1e9dc2f8c69bb0dfe2bfd78b78f724427d26f0f01e79b7cfe1c3c360e880aa85"
     sha256 cellar: :any_skip_relocation, mojave:        "30a69e559f02cf7fd6bb4f7190bd71703c18885a826eb9c13f7c71777c124193"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf390cee3e53fb6639ccadf2bc9dcab1d4c3e38e3aefb2b10cffa8c352d4d935"
   end
 
   depends_on "node"
