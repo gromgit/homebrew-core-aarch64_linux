173ee6c6a6b2c5aba0b434326e784e29d040bcda
Nanda H Krishna <me@nandahkrishna.com>
2021-07-09 07:43:34 +0000

    enscript: update 1.6.6_1 bottle.

--- Formula/enscript.rb
+++ Formula/enscript.rb
@@ -15,6 +15,7 @@ class Enscript < Formula
     sha256 mojave:        "a8bbba8f7d64eed40dd59a9db980b049ec786e148d31a0aeb92556959b4ad0b0"
     sha256 high_sierra:   "00045dff3bdf7ac98a19236838d7af7101cc1fc002e55550312042bb2e4d7426"
     sha256 sierra:        "c14fad6cfd67fa782beb7a425eb03c3ed0b8090ed751c37f5f5ec426808df25c"
+    sha256 x86_64_linux:  "d968c97391029600c54ace8362e7293202ca6227421d626dced22f21b2ccfa26"
   end
 
   depends_on "gettext"
