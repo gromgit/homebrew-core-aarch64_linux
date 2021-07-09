0495c5832ee4c6749f45ddc226da9ce3b1b42ed0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 22:49:46 +0000

    libxres: update 1.2.1 bottle.

--- Formula/libxres.rb
+++ Formula/libxres.rb
@@ -6,10 +6,11 @@ class Libxres < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "60935c75823a1601c976f1bcd0ac3376bf61b3a5662722c2490da69f7f69ba91"
-    sha256 cellar: :any, big_sur:       "993e7994f092301118aec29874cd7332ac5ddf2199dc38784f46aa2650bf54ff"
-    sha256 cellar: :any, catalina:      "98b7edc1c10775aa7fb949cb1cdc8e2a33a6f4a9de16b7e7d9366f3cd7ecdbeb"
-    sha256 cellar: :any, mojave:        "147c8a30297594a8da02cf5b46942d2c1408683adde5a7abe8ec272ff0452ada"
+    sha256 cellar: :any,                 arm64_big_sur: "60935c75823a1601c976f1bcd0ac3376bf61b3a5662722c2490da69f7f69ba91"
+    sha256 cellar: :any,                 big_sur:       "993e7994f092301118aec29874cd7332ac5ddf2199dc38784f46aa2650bf54ff"
+    sha256 cellar: :any,                 catalina:      "98b7edc1c10775aa7fb949cb1cdc8e2a33a6f4a9de16b7e7d9366f3cd7ecdbeb"
+    sha256 cellar: :any,                 mojave:        "147c8a30297594a8da02cf5b46942d2c1408683adde5a7abe8ec272ff0452ada"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0819e6b6456495c3fc61d885f2f07385fac6dcce43c1d78f81fb7c8ee91fa418"
   end
 
   depends_on "pkg-config" => :build
