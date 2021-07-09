210ef65cba7943eccae2467cdf02329ebbbd3c4a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 19:09:31 +0000

    ccls: update 0.20210330_1 bottle.

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -8,10 +8,11 @@ class Ccls < Formula
   head "https://github.com/MaskRay/ccls.git"
 
   bottle do
-    sha256 arm64_big_sur: "4b2edaf5fa08c9846ca7bc5cc20d36cbf6c6299af4df34527fbd511bbdebb5d7"
-    sha256 big_sur:       "cde35a76dbcadb48c962ded6e8e42e232e2a9084d46f01671b4a372a71dcc6cc"
-    sha256 catalina:      "86b111c8e74d39fb31558870c8b11b45625fab9573bbced33600319c53448530"
-    sha256 mojave:        "f8c02843ad68842a00b2b1d7015f3e73577b2717eef55c5c614231f0a4b57212"
+    sha256                               arm64_big_sur: "4b2edaf5fa08c9846ca7bc5cc20d36cbf6c6299af4df34527fbd511bbdebb5d7"
+    sha256                               big_sur:       "cde35a76dbcadb48c962ded6e8e42e232e2a9084d46f01671b4a372a71dcc6cc"
+    sha256                               catalina:      "86b111c8e74d39fb31558870c8b11b45625fab9573bbced33600319c53448530"
+    sha256                               mojave:        "f8c02843ad68842a00b2b1d7015f3e73577b2717eef55c5c614231f0a4b57212"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea19ed9887e21ba1cfec720431fac2ec6b28cd0464746f2f9930f39bea1a42e3"
   end
 
   depends_on "cmake" => :build
