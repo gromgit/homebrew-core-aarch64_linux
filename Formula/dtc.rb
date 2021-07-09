5cd167acb0a55adc6fabd964270cf1ea550d8b82
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 05:42:20 +0000

    dtc: update 1.6.1 bottle.

--- Formula/dtc.rb
+++ Formula/dtc.rb
@@ -11,10 +11,11 @@ class Dtc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3acf8dbcf556c2271a6162ee0d7e6aa4b1e2f2ffb8dd88541d44ed3a96eecdd5"
-    sha256 cellar: :any, big_sur:       "eed133a2432f709556facdfb154a34ce5d2bddd6b04c67de73421bee3eb35160"
-    sha256 cellar: :any, catalina:      "d1910b920613b43b1be2a70ad950139fcd5e908ab45bc9c68dbaba078c676599"
-    sha256 cellar: :any, mojave:        "98c38049acdae747c9939bbe271f4318e35d976687caaf6beea0527592e4274d"
+    sha256 cellar: :any,                 arm64_big_sur: "3acf8dbcf556c2271a6162ee0d7e6aa4b1e2f2ffb8dd88541d44ed3a96eecdd5"
+    sha256 cellar: :any,                 big_sur:       "eed133a2432f709556facdfb154a34ce5d2bddd6b04c67de73421bee3eb35160"
+    sha256 cellar: :any,                 catalina:      "d1910b920613b43b1be2a70ad950139fcd5e908ab45bc9c68dbaba078c676599"
+    sha256 cellar: :any,                 mojave:        "98c38049acdae747c9939bbe271f4318e35d976687caaf6beea0527592e4274d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "20225ca2fdcdf29d6fff97dddf7a26d39617bf4c6dc6adb3dc1da31f86de47fb"
   end
 
   depends_on "pkg-config" => :build
