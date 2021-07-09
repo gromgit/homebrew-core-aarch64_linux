a819238f0a8606120bc57667ffdb642275e1d2ea
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 23:11:37 +0000

    texlab: update 3.2.0 bottle.

--- Formula/texlab.rb
+++ Formula/texlab.rb
@@ -11,6 +11,7 @@ class Texlab < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "bc5bbf32d1c5faf7dd0996b992a2f9c78b27c3ed6187850521917ea61849b6e1"
     sha256 cellar: :any_skip_relocation, catalina:      "f8801c58a8bf07e9a4a9b506b11ce538fa17db718338f0fd1d962378a3791e8b"
     sha256 cellar: :any_skip_relocation, mojave:        "662267d2e6bd6d36cc5066b36807625d0de0fc03cb5c4709434872d51700803c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f17ba954bc373e843a9a1be540e6b08551c3e5fc9d35fb27f883369bcfa361ca"
   end
 
   depends_on "rust" => :build
