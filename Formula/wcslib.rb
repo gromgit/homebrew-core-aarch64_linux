f88ce0a9a2ddd26f6dd42acb0f48366ff245ecee
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 23:33:25 +0000

    wcslib: update 7.6 bottle.

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -11,10 +11,11 @@ class Wcslib < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1501d04bd38777379055060545973deadb4b61959291c7f86c48abf16cd20983"
-    sha256 cellar: :any, big_sur:       "f0d3979ff121d5d7429d930f87da36df8b1f2c36c41376d63882def0c042fe05"
-    sha256 cellar: :any, catalina:      "f6c3337cd1ea830d6f7034efefaba694a5e94d9f34e0ca79f29919e3b9128026"
-    sha256 cellar: :any, mojave:        "765fc09c8786be17fdad63cee17bc93b6006be12f5858d38c9095a3370e5afa6"
+    sha256 cellar: :any,                 arm64_big_sur: "1501d04bd38777379055060545973deadb4b61959291c7f86c48abf16cd20983"
+    sha256 cellar: :any,                 big_sur:       "f0d3979ff121d5d7429d930f87da36df8b1f2c36c41376d63882def0c042fe05"
+    sha256 cellar: :any,                 catalina:      "f6c3337cd1ea830d6f7034efefaba694a5e94d9f34e0ca79f29919e3b9128026"
+    sha256 cellar: :any,                 mojave:        "765fc09c8786be17fdad63cee17bc93b6006be12f5858d38c9095a3370e5afa6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c4d09ab4dc1123e9898330f7dbef839945b04166ecac63603e852d52766b9996"
   end
 
   depends_on "cfitsio"
