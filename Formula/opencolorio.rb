a1f24d142d839fb8c27e058014492e6fa1185660
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 04:16:46 +0000

    opencolorio: update 2.0.1 bottle.

--- Formula/opencolorio.rb
+++ Formula/opencolorio.rb
@@ -8,10 +8,11 @@ class Opencolorio < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "ff7921b54ca7e0589fff3dbf2e8f2b071b0f503f1657f4963d80749a8c5aedf0"
-    sha256 cellar: :any, big_sur:       "94fc18f52a783c11afd946c72b100bf16d570767d5934b245c03cabb3f9de715"
-    sha256 cellar: :any, catalina:      "8dcc8e3561f9e564239366ed88a2488b36af37c022e8b98f94e2d6cb1e718bde"
-    sha256 cellar: :any, mojave:        "c0fa4803986b6960a3a3578f6e453428864eff5fb7ed5f51fd6c505258cc0ca5"
+    sha256 cellar: :any,                 arm64_big_sur: "ff7921b54ca7e0589fff3dbf2e8f2b071b0f503f1657f4963d80749a8c5aedf0"
+    sha256 cellar: :any,                 big_sur:       "94fc18f52a783c11afd946c72b100bf16d570767d5934b245c03cabb3f9de715"
+    sha256 cellar: :any,                 catalina:      "8dcc8e3561f9e564239366ed88a2488b36af37c022e8b98f94e2d6cb1e718bde"
+    sha256 cellar: :any,                 mojave:        "c0fa4803986b6960a3a3578f6e453428864eff5fb7ed5f51fd6c505258cc0ca5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08c2c289893682d242e35d91bde98fab3cf11de92f1e4bae4ad64c34c4b4ce62"
   end
 
   depends_on "cmake" => :build
