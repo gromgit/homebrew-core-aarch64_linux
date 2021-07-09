17fa662e12cda7e6c26505e906f1395f0be813ca
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 08:22:30 +0000

    libexif: update 0.6.22 bottle.

--- Formula/libexif.rb
+++ Formula/libexif.rb
@@ -11,6 +11,7 @@ class Libexif < Formula
     sha256 catalina:      "7379f6990018006122bba69098864e8877e8e6e7be3af535f7e301d8ff097e98"
     sha256 mojave:        "c20d311fbd1846ce2603950ec9ad9b3b6e8202bf2f97e9aab328c05dc568fcfe"
     sha256 high_sierra:   "8b1c7cf6ec777090ce22ccf5c426867948a54da9378e0c9b91d85175eaea4f81"
+    sha256 x86_64_linux:  "69c7d9fd5fcab7af3afcfe802d5cb7b9a0523a6e83c637119c86b72a2356aea1"
   end
 
   depends_on "autoconf" => :build
