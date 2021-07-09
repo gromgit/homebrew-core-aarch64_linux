50f7b32fda486dd32c885f14c1ed007041d10fd7
Caleb Xu <calebcenter@live.com>
2021-07-09 00:36:20 -0400

    copilot 1.8.3
    Closes #80862.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -4,8 +4,8 @@ class Copilot < Formula
   desc "CLI tool for Amazon ECS and AWS Fargate"
   homepage "https://aws.github.io/copilot-cli/"
   url "https://github.com/aws/copilot-cli.git",
-      tag:      "v1.8.2",
-      revision: "3d9721285d30bfabeca677053ccf8daba5133200"
+      tag:      "v1.8.3",
+      revision: "4a347b8ff197087f607476033b9125934d216502"
   license "Apache-2.0"
   head "https://github.com/aws/copilot-cli.git"
 
