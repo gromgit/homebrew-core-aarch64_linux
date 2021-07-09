f3a579d6abe9503cac56230aedf3f971bed817d0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 19:17:24 +0000

    bash-snippets: update 1.23.0 bottle.

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -10,6 +10,7 @@ class BashSnippets < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c2a441dc6d5b21408f49f4869b00919354392682b3ef7eef2f908ab2f638dd8a"
     sha256 cellar: :any_skip_relocation, catalina:      "c2a441dc6d5b21408f49f4869b00919354392682b3ef7eef2f908ab2f638dd8a"
     sha256 cellar: :any_skip_relocation, mojave:        "c2a441dc6d5b21408f49f4869b00919354392682b3ef7eef2f908ab2f638dd8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e38f6d39c157dcf604f1ac774a16afe66472de96ebf612a7f409689a7074282"
   end
 
   conflicts_with "cheat", because: "both install a `cheat` executable"
