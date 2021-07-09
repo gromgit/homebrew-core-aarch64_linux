1198da72179dce2338851b3a3d2543902151aaca
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 05:45:36 +0000

    libdmx: update 1.1.4 bottle.

--- Formula/libdmx.rb
+++ Formula/libdmx.rb
@@ -6,11 +6,12 @@ class Libdmx < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c9aae9326ce9a74e1082a3af3678b61694a90c8e360bcdf4b78c369be0ff95bf"
-    sha256 cellar: :any, big_sur:       "e5d9c7b5d007505a52338b9d901576fa407af1bc802ee392412b81e0266be641"
-    sha256 cellar: :any, catalina:      "89fc7b694d6e0d2bd786f053bf9f8bb8aa2005f99319e6a75fad30dfcff7b831"
-    sha256 cellar: :any, mojave:        "53a22f968698ff43bd3e483a77cc1c1a1b9bcc4ef3cbdfc6ffa5039d7e6af6b1"
-    sha256 cellar: :any, high_sierra:   "d4b4e652d95db58f17afbf8d061cc161982b3726da03f403a05c14c8b99558a4"
+    sha256 cellar: :any,                 arm64_big_sur: "c9aae9326ce9a74e1082a3af3678b61694a90c8e360bcdf4b78c369be0ff95bf"
+    sha256 cellar: :any,                 big_sur:       "e5d9c7b5d007505a52338b9d901576fa407af1bc802ee392412b81e0266be641"
+    sha256 cellar: :any,                 catalina:      "89fc7b694d6e0d2bd786f053bf9f8bb8aa2005f99319e6a75fad30dfcff7b831"
+    sha256 cellar: :any,                 mojave:        "53a22f968698ff43bd3e483a77cc1c1a1b9bcc4ef3cbdfc6ffa5039d7e6af6b1"
+    sha256 cellar: :any,                 high_sierra:   "d4b4e652d95db58f17afbf8d061cc161982b3726da03f403a05c14c8b99558a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c279c15fc371a0d20032999a293ac5d706ef91f94c9b786ae753baf9d319d0b"
   end
 
   depends_on "pkg-config" => :build
