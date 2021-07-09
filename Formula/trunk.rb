936e4c940b558f63206609e119b6758d670022b9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 19:33:39 +0000

    trunk: update 0.11.0 bottle.

--- Formula/trunk.rb
+++ Formula/trunk.rb
@@ -11,6 +11,7 @@ class Trunk < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "56e401e680d3aac8ddc7a3b0f528143ee65649821c0b8ae16ec0f9986943ab30"
     sha256 cellar: :any_skip_relocation, catalina:      "d8da1a17df62f0fa0430d4046be4014bdd12b2b274d24cdac6b089831e824cc7"
     sha256 cellar: :any_skip_relocation, mojave:        "d1691cc33b907571250b5b1cccb9f42022f32e7dd30c0de4f7c1f28291b900cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3890254423b3f02008ee6180b0631ac44b1ab19f55e399602f942f26aec7aa90"
   end
 
   depends_on "rust" => :build
