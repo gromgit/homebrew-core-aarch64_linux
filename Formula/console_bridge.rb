ae94ba3f949729fef22cbea74f71c5bbaeb23dc0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 00:25:45 +0000

    console_bridge: update 1.0.1 bottle.

--- Formula/console_bridge.rb
+++ Formula/console_bridge.rb
@@ -6,11 +6,12 @@ class ConsoleBridge < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2de26293b962e5fd45cf7d33e3d867ec5f3e44248736594f668993cab66cd642"
-    sha256 cellar: :any, big_sur:       "c9f53becfc647527e7044c1419bf992936c825920bd27637b8d4cbe07a675f9c"
-    sha256 cellar: :any, catalina:      "0b499b94d9a1f14aebdbcdbfb21d95ce06a9c2a9160ecf317541bef7c76a1324"
-    sha256 cellar: :any, mojave:        "a8fe261af6240d0a5128c4a9d214457bc0f0d20b198172c8a84e95d9ffa3fbd9"
-    sha256 cellar: :any, high_sierra:   "0be0e4bb4c6a14c7f8725dc44443b5772e4b4a5b9da1df521cfb4d18d88c9437"
+    sha256 cellar: :any,                 arm64_big_sur: "2de26293b962e5fd45cf7d33e3d867ec5f3e44248736594f668993cab66cd642"
+    sha256 cellar: :any,                 big_sur:       "c9f53becfc647527e7044c1419bf992936c825920bd27637b8d4cbe07a675f9c"
+    sha256 cellar: :any,                 catalina:      "0b499b94d9a1f14aebdbcdbfb21d95ce06a9c2a9160ecf317541bef7c76a1324"
+    sha256 cellar: :any,                 mojave:        "a8fe261af6240d0a5128c4a9d214457bc0f0d20b198172c8a84e95d9ffa3fbd9"
+    sha256 cellar: :any,                 high_sierra:   "0be0e4bb4c6a14c7f8725dc44443b5772e4b4a5b9da1df521cfb4d18d88c9437"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46bbb8159d744a618988624b1eb93b012852230b8347cafabb2f2c14c4ad8caa"
   end
 
   depends_on "cmake" => :build
