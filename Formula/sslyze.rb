1f9cd25265f989da8f473e7352873db84ff24536
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 22:23:31 +0000

    sslyze: update 4.1.0 bottle.

--- Formula/sslyze.rb
+++ Formula/sslyze.rb
@@ -16,9 +16,10 @@ class Sslyze < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:  "bb90747b0ca6204eec778c0f6206e84c9082ead3d3cb57290b5868b42d49decd"
-    sha256 cellar: :any, catalina: "1ba1833fc32bf1779e2d1ae530305a7e4091385f4ce7cee14da61d2590d61229"
-    sha256 cellar: :any, mojave:   "517ddb6ff0b4e752fd16fba00fd98690ae18bb886b57e5cbf777299712379f21"
+    sha256 cellar: :any,                 big_sur:      "bb90747b0ca6204eec778c0f6206e84c9082ead3d3cb57290b5868b42d49decd"
+    sha256 cellar: :any,                 catalina:     "1ba1833fc32bf1779e2d1ae530305a7e4091385f4ce7cee14da61d2590d61229"
+    sha256 cellar: :any,                 mojave:       "517ddb6ff0b4e752fd16fba00fd98690ae18bb886b57e5cbf777299712379f21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "36f7889215fb9432dc747530f80fc27047d35775853de2d758aebf5b16f1d091"
   end
 
   head do
