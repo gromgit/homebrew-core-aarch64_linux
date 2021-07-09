763d8216f2ada2378a7b5d489bd3bf5887ea2683
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-09 05:48:22 +0000

    nativefier: update 44.0.6 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -12,6 +12,7 @@ class Nativefier < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "be08de210a106ccd5650acc595a032c14a994f66be4e36e332b30c9c01b71bd3"
     sha256 cellar: :any_skip_relocation, catalina:      "be08de210a106ccd5650acc595a032c14a994f66be4e36e332b30c9c01b71bd3"
     sha256 cellar: :any_skip_relocation, mojave:        "be08de210a106ccd5650acc595a032c14a994f66be4e36e332b30c9c01b71bd3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8fd990fe449f9a3e787d2dc8368ecd305ff5117161f42ddb398084345f34044"
   end
 
   depends_on "node"
