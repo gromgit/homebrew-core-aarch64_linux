dcae049ee016e2a99168853f0290e267cb754d5f
rui <rui@chenrui.dev>
2021-07-10 17:04:32 +0000

    nomino: update 1.1.0 bottle.

--- Formula/nomino.rb
+++ Formula/nomino.rb
@@ -11,6 +11,7 @@ class Nomino < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "51056d253a62760ef972f909d1425be9c63b2ddb10d337a0f3d8ad86aec97dca"
     sha256 cellar: :any_skip_relocation, catalina:      "1656706972c24f6b507d14c129321fba583b90903b19731230e854d483163773"
     sha256 cellar: :any_skip_relocation, mojave:        "5e23bb5c2901ed77aa0cb0b114916440e3369bf612a13d02f036aee6164982ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d65d7fe2ad0420b240271ca54c2e2ac2e3d8f5a63d249ae653aa5ef22acd2ec9"
   end
 
   depends_on "rust" => :build
