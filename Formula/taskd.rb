ef8a8da6f60e0eb2cbae6966f144e3a3de4221eb
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:44:02 +0000

    taskd: update 1.1.0_1 bottle.

--- Formula/taskd.rb
+++ Formula/taskd.rb
@@ -14,11 +14,12 @@ class Taskd < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "538ccd42a9ccb36722a35116bb1652217eeb33813683e14d194a3a8aab33bf35"
-    sha256 cellar: :any, big_sur:       "842ffc8f10936b3803281743196d37571057656ccfcd019364b8c5114ddc36cd"
-    sha256 cellar: :any, catalina:      "88580976ecb71d4f74d814ff06c88c2082565fee61c7ff8e7f506bce19b460d4"
-    sha256 cellar: :any, mojave:        "225bedd463f0344572ec985bbb49693dc0b6d5c095c87a5157bcfc437317c1d7"
-    sha256 cellar: :any, high_sierra:   "f9737943f0b2877414bf8c0d957a88d79010334a145be6420fd93f64b9569cb3"
+    sha256 cellar: :any,                 arm64_big_sur: "538ccd42a9ccb36722a35116bb1652217eeb33813683e14d194a3a8aab33bf35"
+    sha256 cellar: :any,                 big_sur:       "842ffc8f10936b3803281743196d37571057656ccfcd019364b8c5114ddc36cd"
+    sha256 cellar: :any,                 catalina:      "88580976ecb71d4f74d814ff06c88c2082565fee61c7ff8e7f506bce19b460d4"
+    sha256 cellar: :any,                 mojave:        "225bedd463f0344572ec985bbb49693dc0b6d5c095c87a5157bcfc437317c1d7"
+    sha256 cellar: :any,                 high_sierra:   "f9737943f0b2877414bf8c0d957a88d79010334a145be6420fd93f64b9569cb3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b29eee73430b07da2a84a6a321cdbecce2926f1766d600efd9128cf181079509"
   end
 
   depends_on "cmake" => :build
