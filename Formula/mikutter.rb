b965f8e813f86d0829bf6271e93b7db4ed576074
rui <rui@chenrui.dev>
2021-07-10 19:57:47 +0000

    mikutter: update 4.1.1_2 bottle.

--- Formula/mikutter.rb
+++ Formula/mikutter.rb
@@ -13,10 +13,11 @@ class Mikutter < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1a3ad524040a6b2f1a9328f231c51f49689731b419650c211a816d144baa3232"
-    sha256 cellar: :any, big_sur:       "28bbc7b323d3ecdaf288c71e469192475ecd331518568af06d62035e7f4c1577"
-    sha256 cellar: :any, catalina:      "81343393fd8217ae7ce465bc06965b7ecaf218fc8cab32f58fa5f29973c10ff3"
-    sha256 cellar: :any, mojave:        "b7c86be22b9559edc1ef689bb4d4470dda8083c90e7218165b67b009418411e5"
+    sha256 cellar: :any,                 arm64_big_sur: "1a3ad524040a6b2f1a9328f231c51f49689731b419650c211a816d144baa3232"
+    sha256 cellar: :any,                 big_sur:       "28bbc7b323d3ecdaf288c71e469192475ecd331518568af06d62035e7f4c1577"
+    sha256 cellar: :any,                 catalina:      "81343393fd8217ae7ce465bc06965b7ecaf218fc8cab32f58fa5f29973c10ff3"
+    sha256 cellar: :any,                 mojave:        "b7c86be22b9559edc1ef689bb4d4470dda8083c90e7218165b67b009418411e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9b47e9983d0db76b4261ebe5f7df3d258d62a64abda8fa67d22f0ff6cde6be4"
   end
 
   depends_on "gobject-introspection"
