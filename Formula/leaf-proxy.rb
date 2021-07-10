2b178d4237d054d7f2b39b102a1e2c9f3a5d56c5
rui <rui@chenrui.dev>
2021-07-10 17:05:55 +0000

    leaf-proxy: update 0.3.1 bottle.

--- Formula/leaf-proxy.rb
+++ Formula/leaf-proxy.rb
@@ -11,6 +11,7 @@ class LeafProxy < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "3acdb89d85419f3f767b19782bd3442c74baad9b0527471ab83bd9f74efa8dd9"
     sha256 cellar: :any_skip_relocation, catalina:      "31c45ca626144ad34a3bcf09440077cbaeb793851538e2046c581122f378ebe9"
     sha256 cellar: :any_skip_relocation, mojave:        "e0ea5b75bd333e98c716f9f9782aadb47e4e8d9c6b0e8f087f782eb8c67a58bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9473c11cdbfee47a2c7a8f6b8b803413a48f135378c747ac74de4d12dedf5a0c"
   end
 
   depends_on "rust" => :build
