96581a09f413b8263c72e13b2611d051ae7d0fc3
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:42:25 +0000

    foma: update 0.9.18 bottle.

--- Formula/foma.rb
+++ Formula/foma.rb
@@ -7,11 +7,12 @@ class Foma < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "8cac09b69356887a31f4d2314b9eb7a193ad21858b0cc43ade7d48a485e4b55d"
-    sha256 cellar: :any, big_sur:       "cdf3b3105f0207ddea3f5b0ba458b650cab22b1ac3db85896631ec5304cc5bf1"
-    sha256 cellar: :any, catalina:      "dc0a238f67280d9e15e50bc7064669f1715170c9a59d608537ed195801db0c9e"
-    sha256 cellar: :any, mojave:        "a3b11300d427959a0ca8aa908d6c43369a8c17889a63f56d7772c6c4fdaeee04"
-    sha256 cellar: :any, high_sierra:   "d223eaa3a2f821d24b5f3b5486494a1a029f96e1640d4fe6f3633e6ad53e14a9"
+    sha256 cellar: :any,                 arm64_big_sur: "8cac09b69356887a31f4d2314b9eb7a193ad21858b0cc43ade7d48a485e4b55d"
+    sha256 cellar: :any,                 big_sur:       "cdf3b3105f0207ddea3f5b0ba458b650cab22b1ac3db85896631ec5304cc5bf1"
+    sha256 cellar: :any,                 catalina:      "dc0a238f67280d9e15e50bc7064669f1715170c9a59d608537ed195801db0c9e"
+    sha256 cellar: :any,                 mojave:        "a3b11300d427959a0ca8aa908d6c43369a8c17889a63f56d7772c6c4fdaeee04"
+    sha256 cellar: :any,                 high_sierra:   "d223eaa3a2f821d24b5f3b5486494a1a029f96e1640d4fe6f3633e6ad53e14a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed4b46bd3f62ab26bbb0407019c2989448d3b9df0680ebb87266bdbfe5b3e9c9"
   end
 
   uses_from_macos "zlib"
