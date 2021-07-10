2f57efb73b8b08f7e21d1c13cac4f88e40e3a634
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 02:05:36 +0000

    libspatialite: update 5.0.1_1 bottle.

--- Formula/libspatialite.rb
+++ Formula/libspatialite.rb
@@ -14,10 +14,11 @@ class Libspatialite < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "dc8b7f36b4f7aba60fdb088ec17ca69bddb353fed99a1e6d0200590a0ee0adb2"
-    sha256 cellar: :any, big_sur:       "f740afb58268b4babe07a123e1b7857b754abea2ffced3b37b8edf7140408652"
-    sha256 cellar: :any, catalina:      "abf71f3882743f1bebf560c66a0f211f0298f0498ed0093b29d1feaa6c5c425f"
-    sha256 cellar: :any, mojave:        "64a4c76c3cc6aa7ae20c510a4688182af14bb9f1da38bed1409dcd6a1a333a5d"
+    sha256 cellar: :any,                 arm64_big_sur: "dc8b7f36b4f7aba60fdb088ec17ca69bddb353fed99a1e6d0200590a0ee0adb2"
+    sha256 cellar: :any,                 big_sur:       "f740afb58268b4babe07a123e1b7857b754abea2ffced3b37b8edf7140408652"
+    sha256 cellar: :any,                 catalina:      "abf71f3882743f1bebf560c66a0f211f0298f0498ed0093b29d1feaa6c5c425f"
+    sha256 cellar: :any,                 mojave:        "64a4c76c3cc6aa7ae20c510a4688182af14bb9f1da38bed1409dcd6a1a333a5d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e92d770bacb404f5e3573b8353ed78735fcb618f93dc070d220a9e9413700eb"
   end
 
   head do
