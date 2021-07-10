91ef3651eceb15a324af8481a93f331ed45e4eee
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:44:49 +0000

    cpio: update 2.13_2 bottle.

--- Formula/cpio.rb
+++ Formula/cpio.rb
@@ -12,6 +12,7 @@ class Cpio < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "1e2e8f240d9455593a653d4cc0759ee1a0596fe88641ad6a79d652f6596bb21b"
     sha256 cellar: :any_skip_relocation, mojave:        "566b73ec056c1441e84e5be4d8f22ae0e9eec609e340d56d9ba22ebefaa273c6"
     sha256 cellar: :any_skip_relocation, high_sierra:   "35cc00b8c97558822cc49cca1f40ba7a3a65af06be17317721ff471414c6f430"
+    sha256                               x86_64_linux:  "cc0acbb967d8ec3b91bda22a59afa341569d0b4e6635cd0e4050e364a6bc2524"
   end
 
   keg_only :shadowed_by_macos, "macOS provides cpio"
