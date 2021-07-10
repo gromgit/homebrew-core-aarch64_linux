88794f2f478e61d80859c3e4efb8c4be55ac31c0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:58:29 +0000

    libjwt: update 1.13.1 bottle.

--- Formula/libjwt.rb
+++ Formula/libjwt.rb
@@ -6,10 +6,11 @@ class Libjwt < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4d89729e216bebd3dcd95d7edca9050b125387a122e1525f2b647175074154aa"
-    sha256 cellar: :any, big_sur:       "6007f616df31c2f700524c3dab66d9f09f5b9bcca728ac7e1b848000aabece90"
-    sha256 cellar: :any, catalina:      "fb6e811d2e09405a322bccac174af800742fc0655a8e72a1220311eacec1b78a"
-    sha256 cellar: :any, mojave:        "97801be0001a6c9a180d425ffecc9cef4d51a2bd246c71ec4d60c7a0016ce490"
+    sha256 cellar: :any,                 arm64_big_sur: "4d89729e216bebd3dcd95d7edca9050b125387a122e1525f2b647175074154aa"
+    sha256 cellar: :any,                 big_sur:       "6007f616df31c2f700524c3dab66d9f09f5b9bcca728ac7e1b848000aabece90"
+    sha256 cellar: :any,                 catalina:      "fb6e811d2e09405a322bccac174af800742fc0655a8e72a1220311eacec1b78a"
+    sha256 cellar: :any,                 mojave:        "97801be0001a6c9a180d425ffecc9cef4d51a2bd246c71ec4d60c7a0016ce490"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a0fc4895581847c58742ebe3d19b818f7a563265d83fec569e95f2a4f25436c"
   end
 
   depends_on "autoconf" => :build
