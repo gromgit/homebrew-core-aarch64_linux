638365a0269787f0ea44186a044789edf0161a08
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-10 04:26:07 +0000

    pyinvoke: update 1.6.0 bottle.

--- Formula/pyinvoke.rb
+++ Formula/pyinvoke.rb
@@ -9,10 +9,10 @@ class Pyinvoke < Formula
   head "https://github.com/pyinvoke/invoke.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c1f2ddf912dcc04d8c2d29eb7ec303fc377664c3a6fe3a67ac556e8da34f9535"
-    sha256 cellar: :any_skip_relocation, big_sur:       "70f385b068f6f303a29679061c48b6e9adcf6db396bba3680f26d63d065d926c"
-    sha256 cellar: :any_skip_relocation, catalina:      "b1b285e271f44c86c60c87a3891a3285f4c269ce4faba18b148bb28a7b7db5b8"
-    sha256 cellar: :any_skip_relocation, mojave:        "685969f8e173f38d99e309231dc44be88c2431b6d2eda004e7c05bbf210802cd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d5299fac1aede1c601d70315a6c50877286372e3a57b41c6be1afeee3039f25d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a2603322a31663ae178003c4df6c6b22e9623a870244231288f5bfc8404e1eb9"
+    sha256 cellar: :any_skip_relocation, catalina:      "a2603322a31663ae178003c4df6c6b22e9623a870244231288f5bfc8404e1eb9"
+    sha256 cellar: :any_skip_relocation, mojave:        "a2603322a31663ae178003c4df6c6b22e9623a870244231288f5bfc8404e1eb9"
   end
 
   depends_on "python@3.9"
