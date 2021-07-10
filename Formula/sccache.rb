8528d217df9209dbcd0e38072b0e9f35e1bd133c
rui <rui@chenrui.dev>
2021-07-10 17:19:21 +0000

    sccache: update 0.2.15 bottle.

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -11,6 +11,7 @@ class Sccache < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "76080d09cb0b9bf50e7ef37609dc3e797b97b3c0f9deb4d71213b91524d67ab9"
     sha256 cellar: :any_skip_relocation, catalina:      "d79d0f596f68b457b821a2d16444a53a93faa198049e4810b1a9016ef39fc7fe"
     sha256 cellar: :any_skip_relocation, mojave:        "76a1c87457acd3fbdd5f6352726911d2d0a524afce4639617f7559e80b6ae849"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3af380852b2e95b2831efd6ac43320e57a64c1d396ba2d64c1fb62eee95040ec"
   end
 
   depends_on "rust" => :build
