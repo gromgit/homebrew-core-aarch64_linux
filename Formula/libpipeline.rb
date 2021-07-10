90107f7db84b0b5c1156c760614063389228d201
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:50:47 +0000

    libpipeline: update 1.5.3 bottle.

--- Formula/libpipeline.rb
+++ Formula/libpipeline.rb
@@ -11,10 +11,11 @@ class Libpipeline < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0a398cdb65f5321e356e7035a3bc2352dd3b92e2f37632d85a64fbdd0510d41d"
-    sha256 cellar: :any, big_sur:       "ab0b600e54ba2acd5878ed910af86518a905a1d53fdeca8d5a8abf363d09a584"
-    sha256 cellar: :any, catalina:      "efa57a53e202d19ae3afb04b55b85e37939a0d0cff1e6af9b40607c9acd8b6d0"
-    sha256 cellar: :any, mojave:        "337f3a1e0b07e0fcdece44321f28be0668be70fb914e5f37000bd6b42ffa188c"
+    sha256 cellar: :any,                 arm64_big_sur: "0a398cdb65f5321e356e7035a3bc2352dd3b92e2f37632d85a64fbdd0510d41d"
+    sha256 cellar: :any,                 big_sur:       "ab0b600e54ba2acd5878ed910af86518a905a1d53fdeca8d5a8abf363d09a584"
+    sha256 cellar: :any,                 catalina:      "efa57a53e202d19ae3afb04b55b85e37939a0d0cff1e6af9b40607c9acd8b6d0"
+    sha256 cellar: :any,                 mojave:        "337f3a1e0b07e0fcdece44321f28be0668be70fb914e5f37000bd6b42ffa188c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ab630181184892e74dd5af27c02b3ca1370e15cada9a0950b68c85934364cb7"
   end
 
   def install
