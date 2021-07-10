ce6f1e6fba108f94be7f2d7a460430fa0c81db47
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 16:48:17 +0200

    dislocker: fix build for Linux
    Closes #80965.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dislocker.rb
+++ Formula/dislocker.rb
@@ -3,7 +3,7 @@ class Dislocker < Formula
   homepage "https://github.com/Aorimn/dislocker"
   url "https://github.com/Aorimn/dislocker/archive/v0.7.1.tar.gz"
   sha256 "742fb5c1b3ff540368ced54c29eae8b488ae5a5fcaca092947e17c2d358a6762"
-  license "GPL-2.0"
+  license "GPL-2.0-only"
   revision 5
 
   depends_on "cmake" => :build
@@ -14,11 +14,15 @@ class Dislocker < Formula
   end
 
   on_linux do
-    depends_on "libfuse"
+    depends_on "libfuse@2"
   end
 
   def install
-    system "cmake", "-DCMAKE_DISABLE_FIND_PACKAGE_Ruby=TRUE", *std_cmake_args
+    args = std_cmake_args + %w[
+      -DCMAKE_DISABLE_FIND_PACKAGE_Ruby=TRUE
+    ]
+
+    system "cmake", *args, "."
     system "make"
     system "make", "install"
   end
