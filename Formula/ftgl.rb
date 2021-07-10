ca93ab931b55fd51bbf07bb59775d13eb0f130f7
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 09:56:10 +0000

    ftgl: update 2.1.3-rc5 bottle.

--- Formula/ftgl.rb
+++ Formula/ftgl.rb
@@ -10,14 +10,15 @@ class Ftgl < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "85368ec5c37bb2cffd87cd30775a78956708c71749f8da56239fd93e57cf576d"
-    sha256 cellar: :any, big_sur:       "1b39e663c0bedd0b915dd60c99c5f1acdfb3ae9717cd832134de15fd48736673"
-    sha256 cellar: :any, catalina:      "0f982773db625daa78a1c18c9e67315ede8c52850586d47d9b7a41ffcac91730"
-    sha256 cellar: :any, mojave:        "17370dd65d19b49dd8ca9d49d0da5010e5c5d1346122c46e0eec0c98b010fb13"
-    sha256 cellar: :any, high_sierra:   "f6da52f5e9f06f984aad457058876e88b5b7053288f40c87a17d7d5749936cd6"
-    sha256 cellar: :any, sierra:        "946a9530f7eae5c8f2bc71dfc91b3a8138ae2228cd441fd7cf39f047b957ce47"
-    sha256 cellar: :any, el_capitan:    "6462eb0b97ab120639f1a191f6e3a39419bbb813abd71f5c741303dbf0aed7fb"
-    sha256 cellar: :any, yosemite:      "26db05485600adfb7ead23d04fae9b1ee1d1a4b7ac304e1453ad83b4b2c39f64"
+    sha256 cellar: :any,                 arm64_big_sur: "85368ec5c37bb2cffd87cd30775a78956708c71749f8da56239fd93e57cf576d"
+    sha256 cellar: :any,                 big_sur:       "1b39e663c0bedd0b915dd60c99c5f1acdfb3ae9717cd832134de15fd48736673"
+    sha256 cellar: :any,                 catalina:      "0f982773db625daa78a1c18c9e67315ede8c52850586d47d9b7a41ffcac91730"
+    sha256 cellar: :any,                 mojave:        "17370dd65d19b49dd8ca9d49d0da5010e5c5d1346122c46e0eec0c98b010fb13"
+    sha256 cellar: :any,                 high_sierra:   "f6da52f5e9f06f984aad457058876e88b5b7053288f40c87a17d7d5749936cd6"
+    sha256 cellar: :any,                 sierra:        "946a9530f7eae5c8f2bc71dfc91b3a8138ae2228cd441fd7cf39f047b957ce47"
+    sha256 cellar: :any,                 el_capitan:    "6462eb0b97ab120639f1a191f6e3a39419bbb813abd71f5c741303dbf0aed7fb"
+    sha256 cellar: :any,                 yosemite:      "26db05485600adfb7ead23d04fae9b1ee1d1a4b7ac304e1453ad83b4b2c39f64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a53d8292298b4c6974e04fd8ab744860aec65b58149a704d9e2dad61aba0c4f6"
   end
 
   depends_on "freetype"
