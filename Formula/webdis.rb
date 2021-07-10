94bb4c4524cf6caedeb4692bddcc82fed0648f38
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:56:22 +0000

    webdis: update 0.1.15 bottle.

--- Formula/webdis.rb
+++ Formula/webdis.rb
@@ -6,10 +6,11 @@ class Webdis < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9bad540c5e1122e2b6f94bfc8401cb3e4ec5a87beb7f85b67637d8adf0f876e4"
-    sha256 cellar: :any, big_sur:       "aa7ce60364d343a7c643c2d667ef6a447a0c0d062d30d3ad25b7d4d141f569ff"
-    sha256 cellar: :any, catalina:      "b9b7b70e46c81533e46032a90b8c8f23395ac5a15eb89c3dd8aadbf2b4c266af"
-    sha256 cellar: :any, mojave:        "eae3a12f74750e5b7593e4ef3aa1f7ba3173f4cf31ccc8a4bb865b010de098f0"
+    sha256 cellar: :any,                 arm64_big_sur: "9bad540c5e1122e2b6f94bfc8401cb3e4ec5a87beb7f85b67637d8adf0f876e4"
+    sha256 cellar: :any,                 big_sur:       "aa7ce60364d343a7c643c2d667ef6a447a0c0d062d30d3ad25b7d4d141f569ff"
+    sha256 cellar: :any,                 catalina:      "b9b7b70e46c81533e46032a90b8c8f23395ac5a15eb89c3dd8aadbf2b4c266af"
+    sha256 cellar: :any,                 mojave:        "eae3a12f74750e5b7593e4ef3aa1f7ba3173f4cf31ccc8a4bb865b010de098f0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9981200e9463afd12fda01cb24906b2c670850209d08e71a0947b0bf742138e1"
   end
 
   depends_on "libevent"
