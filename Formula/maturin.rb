1e36d617cffd06f3bccaeb83150e0f7efa19c1b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-10 04:06:02 +0000

    maturin: update 0.11.1 bottle.

--- Formula/maturin.rb
+++ Formula/maturin.rb
@@ -7,11 +7,11 @@ class Maturin < Formula
   head "https://github.com/PyO3/maturin.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "082243e05496c2b78edf9e0327663236d5ba2beb21d47615bff6bb742e285958"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1640a1aeba079a87f05cb97386a2bd154143559bce7fb86461191693c9de40fc"
-    sha256 cellar: :any_skip_relocation, catalina:      "8851765dfa64f6e3ec6b65c83e8f6db7c3103f3f10f928438d4df3bcd318f472"
-    sha256 cellar: :any_skip_relocation, mojave:        "dd0873e9bf03784e0fecde129ffabe9dd03ce3486832d9a5be7b53f7d29ebfc2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "27f3b13aa13a4a10ec3a5c850947e960e2bb4514ee0c7e9b2165fa35d4081e31"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ed392a37bb38d6d592df6ebfd6537a09f1767e7428818822d5ebc92070672451"
+    sha256 cellar: :any_skip_relocation, big_sur:       "52beb70866e7f690075b8f4e064eafe5b3d4fed50d739c65a74b6cbdd924d17c"
+    sha256 cellar: :any_skip_relocation, catalina:      "d2cd0ef075e0c03f04d09cfd137e0eb453064bda222c60209345e61ea5886415"
+    sha256 cellar: :any_skip_relocation, mojave:        "f39c7d8a923f4ec21ee970ca296af04bf803cd5f4c98ea3b4f7c171265e34aa2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa17682d3bb33f974247ce897adb7d1dddf7b9717b62dbebe56a8590dce100a5"
   end
 
   depends_on "python@3.9" => :test
