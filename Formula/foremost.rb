1bc9045ed7bc861e64bd9feb977ac88ab63bc68f
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 09:49:30 +0000

    foremost: update 1.5.7_1 bottle.

--- Formula/foremost.rb
+++ Formula/foremost.rb
@@ -17,6 +17,7 @@ class Foremost < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "46ddc6fa415ef88bb90b4f14698b8051d6167a7e1763863bbdc4116eed590317"
     sha256 cellar: :any_skip_relocation, catalina:      "6be1f3b67ee3002ab30f9f7bc667f55b02d4311a99b4f974af34b6d0353a0139"
     sha256 cellar: :any_skip_relocation, mojave:        "a392a7045508e07e54fd7210de043758e9bf84ff0b0d13867a550a71665c51ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ef5c3ada1ea10d3c0a815cdac8f3bb5489897f81dcbd24fbb2edd49efe1188b"
   end
 
   def install
