674ed8413c07c1908fa2db44c8028a9545153efd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:57:58 +0000

    eureka: update 1.8.1 bottle.

--- Formula/eureka.rb
+++ Formula/eureka.rb
@@ -7,11 +7,12 @@ class Eureka < Formula
   head "https://github.com/simeg/eureka.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "90360b05ad3ef835c82c534e68ec79ad930eb2b0c6dafa635340f994851ce72b"
-    sha256 cellar: :any, big_sur:       "1aee228a339009bb98020df8f499f27353dc1bd4d301d357b86e130d0d17d906"
-    sha256 cellar: :any, catalina:      "b8fa76b3d16fb92968402f2057693af133270c72292307461f351b8950b9b329"
-    sha256 cellar: :any, mojave:        "2d31715b3b5aa38ed008b58e83d9ce6c9afdbe58f9f6d4bb3bf9195f3dc139ed"
-    sha256 cellar: :any, high_sierra:   "060f76e2626e9b30184f1cfe0a61f8b4ddf545c8f8a5d59c6a8d1b54f9548c4a"
+    sha256 cellar: :any,                 arm64_big_sur: "90360b05ad3ef835c82c534e68ec79ad930eb2b0c6dafa635340f994851ce72b"
+    sha256 cellar: :any,                 big_sur:       "1aee228a339009bb98020df8f499f27353dc1bd4d301d357b86e130d0d17d906"
+    sha256 cellar: :any,                 catalina:      "b8fa76b3d16fb92968402f2057693af133270c72292307461f351b8950b9b329"
+    sha256 cellar: :any,                 mojave:        "2d31715b3b5aa38ed008b58e83d9ce6c9afdbe58f9f6d4bb3bf9195f3dc139ed"
+    sha256 cellar: :any,                 high_sierra:   "060f76e2626e9b30184f1cfe0a61f8b4ddf545c8f8a5d59c6a8d1b54f9548c4a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c5137f7468531db56682bf215205fae9671abf35b4a835c0bab86bd1ad624a7"
   end
 
   depends_on "rust" => :build
