679b54ae904ef57ccfc16c761dc88df12e63b313
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:58:42 +0000

    mcpp: update 2.7.2 bottle.

--- Formula/mcpp.rb
+++ Formula/mcpp.rb
@@ -11,14 +11,15 @@ class Mcpp < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "b6b90e4e5f4b50a390a5cbd6d2c6664dd8d3212013e469c9d3c90d5bf67a774c"
-    sha256 cellar: :any, big_sur:       "ba1468299782ecb1de53cff2390096e7300f5c8f021cef623544d969a37240df"
-    sha256 cellar: :any, catalina:      "742a861cb7087caedaed90aa40c4780a1e6e4ad50be74ee64b251c6ae1ebe21c"
-    sha256 cellar: :any, mojave:        "40a63165c2df3feab3ed58c09a3f4b60daef5e112ec2f101f056aee56ca9819f"
-    sha256 cellar: :any, high_sierra:   "fe1489ca47b0d9e551b4aa1b6cb2a4135848be79e3982856442080f75fcb45d7"
-    sha256 cellar: :any, sierra:        "cdd368c63dc6403832c938967f8f099ec3d02acfcc5c75ab0426ad1cd213b045"
-    sha256 cellar: :any, el_capitan:    "0be73930b3dbc8bc247c9a26acbc6115d3f5f665daaabc9ab64606ac6793ace9"
-    sha256 cellar: :any, yosemite:      "612e3efb23a8165af204338a20bbc27ae8fa2ad345964c24d2d7a206dee0317a"
+    sha256 cellar: :any,                 arm64_big_sur: "b6b90e4e5f4b50a390a5cbd6d2c6664dd8d3212013e469c9d3c90d5bf67a774c"
+    sha256 cellar: :any,                 big_sur:       "ba1468299782ecb1de53cff2390096e7300f5c8f021cef623544d969a37240df"
+    sha256 cellar: :any,                 catalina:      "742a861cb7087caedaed90aa40c4780a1e6e4ad50be74ee64b251c6ae1ebe21c"
+    sha256 cellar: :any,                 mojave:        "40a63165c2df3feab3ed58c09a3f4b60daef5e112ec2f101f056aee56ca9819f"
+    sha256 cellar: :any,                 high_sierra:   "fe1489ca47b0d9e551b4aa1b6cb2a4135848be79e3982856442080f75fcb45d7"
+    sha256 cellar: :any,                 sierra:        "cdd368c63dc6403832c938967f8f099ec3d02acfcc5c75ab0426ad1cd213b045"
+    sha256 cellar: :any,                 el_capitan:    "0be73930b3dbc8bc247c9a26acbc6115d3f5f665daaabc9ab64606ac6793ace9"
+    sha256 cellar: :any,                 yosemite:      "612e3efb23a8165af204338a20bbc27ae8fa2ad345964c24d2d7a206dee0317a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7869ff9d2c9946dd38a7891ea70766983208df755ca0f206016211b3239701c8"
   end
 
   # stpcpy is a macro on macOS; trying to define it as an extern is invalid.
