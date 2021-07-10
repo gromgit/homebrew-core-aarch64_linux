f0641bf5a517569ffb8bf0127bdd902db80a733a
rui <rui@chenrui.dev>
2021-07-10 16:54:52 +0000

    genact: update 0.11.0 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -10,6 +10,7 @@ class Genact < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "faa9f1370b20ac2d6e92eaa5be5bf376f7ef8ba85dac841f735ccf491f9c4b4f"
     sha256 cellar: :any_skip_relocation, catalina:      "4d94d3fd54d7821be5684690520cfacb13f8d9d3741edd927b0f8b9829e1d63f"
     sha256 cellar: :any_skip_relocation, mojave:        "fb724747b034a14552cc66c88126ed22a6f4c001c9d63b3a437fdb8a4c2ad86e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f164ea3f660c60ca1071bba73b6d2b7d0ba3bc9e6e54126bfadf5ee4212d9238"
   end
 
   depends_on "rust" => :build
