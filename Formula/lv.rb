4babcc9b682815bc34f9df068fe3dc48005d74ef
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 09:49:23 +0000

    lv: update 4.51_1 bottle.

--- Formula/lv.rb
+++ Formula/lv.rb
@@ -15,11 +15,12 @@ class Lv < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "b96a459a6aa0f11cb8d498c71ab902b1b2bdd75bdf02aa5233366171f61d750a"
-    sha256 big_sur:       "0fea290739e05216d0ecc36266ba774cd27f70cf022c13b94b56e509a66bc44d"
-    sha256 catalina:      "74f154bdfaabb2819bfab9969a88addff7e0b08cca3aafe3ea13805fa588e68d"
-    sha256 mojave:        "491aa872d9c617f7d323aa368498f25728d25bbdf1e60fde272e62b149831c99"
-    sha256 high_sierra:   "90a79ade2abcd36772eb50db1c93298a67766d626a5316a3eeb1638312fbd377"
+    sha256                               arm64_big_sur: "b96a459a6aa0f11cb8d498c71ab902b1b2bdd75bdf02aa5233366171f61d750a"
+    sha256                               big_sur:       "0fea290739e05216d0ecc36266ba774cd27f70cf022c13b94b56e509a66bc44d"
+    sha256                               catalina:      "74f154bdfaabb2819bfab9969a88addff7e0b08cca3aafe3ea13805fa588e68d"
+    sha256                               mojave:        "491aa872d9c617f7d323aa368498f25728d25bbdf1e60fde272e62b149831c99"
+    sha256                               high_sierra:   "90a79ade2abcd36772eb50db1c93298a67766d626a5316a3eeb1638312fbd377"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16aa28d4dfb99fbffc482973e91282d1b7a4986f3cdc2638805228962143d949"
   end
 
   uses_from_macos "ncurses"
