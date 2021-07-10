4090d5e17a32df70007bd2caee771fa6ba2feb4f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:59:01 +0000

    go@1.13: update 1.13.15 bottle.

--- Formula/go@1.13.rb
+++ Formula/go@1.13.rb
@@ -8,10 +8,11 @@ class GoAT113 < Formula
 
   bottle do
     rebuild 1
-    sha256 big_sur:     "264e7eece63bd0c7934feb322e8b42d3ee2707003eb49ab2d243038a35582bee"
-    sha256 catalina:    "4c728bf1466cdd0d9fa6f9ed3a565e4a2203717730603189d8b5f2675a008c88"
-    sha256 mojave:      "d994ff2b27c191bb74dd44a373e254ef92472c1cb13e381cf6bf9d3d118bdaef"
-    sha256 high_sierra: "4c62c487e96657aabebc01f662c2506724e123a08bdf3ead0715945c9838429c"
+    sha256 big_sur:      "264e7eece63bd0c7934feb322e8b42d3ee2707003eb49ab2d243038a35582bee"
+    sha256 catalina:     "4c728bf1466cdd0d9fa6f9ed3a565e4a2203717730603189d8b5f2675a008c88"
+    sha256 mojave:       "d994ff2b27c191bb74dd44a373e254ef92472c1cb13e381cf6bf9d3d118bdaef"
+    sha256 high_sierra:  "4c62c487e96657aabebc01f662c2506724e123a08bdf3ead0715945c9838429c"
+    sha256 x86_64_linux: "f28b974155b8174bddf012622a17772bb72d56fac35d35a1ccef7564dc476675"
   end
 
   keg_only :versioned_formula
