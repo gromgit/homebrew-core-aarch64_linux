1db904ac6a19461a8315464f7e8f2bb3ce729b65
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:42:06 +0000

    ophcrack: update 3.8.0_1 bottle.

--- Formula/ophcrack.rb
+++ Formula/ophcrack.rb
@@ -8,12 +8,13 @@ class Ophcrack < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "fdc3b1275b2e59028aa2edc99bda3fb93f6ad139f9e5a34aa3e4382b5dc6208e"
-    sha256 cellar: :any, big_sur:       "d8a76360167fc2fa268916d26cb250b2f5d398c9cd21f6ae6470b145697b83d6"
-    sha256 cellar: :any, catalina:      "47361d9c18591930ce871fa3c7ab36eaa43003a8a5339238648787cdd748d962"
-    sha256 cellar: :any, mojave:        "0bdbfbee37e693edff5fc8f71c52f1fb12d6dd07c1e64aa1a20401df0789853a"
-    sha256 cellar: :any, high_sierra:   "a1061331c1e9b4a726c818005a3d795ba8c73b29ecd78a3828b5e5eafac18107"
-    sha256 cellar: :any, sierra:        "6229ee0c1e44192fa0d513b7e72e5c72e7fbd29b5ad7f61cd5c5824d76d49105"
+    sha256 cellar: :any,                 arm64_big_sur: "fdc3b1275b2e59028aa2edc99bda3fb93f6ad139f9e5a34aa3e4382b5dc6208e"
+    sha256 cellar: :any,                 big_sur:       "d8a76360167fc2fa268916d26cb250b2f5d398c9cd21f6ae6470b145697b83d6"
+    sha256 cellar: :any,                 catalina:      "47361d9c18591930ce871fa3c7ab36eaa43003a8a5339238648787cdd748d962"
+    sha256 cellar: :any,                 mojave:        "0bdbfbee37e693edff5fc8f71c52f1fb12d6dd07c1e64aa1a20401df0789853a"
+    sha256 cellar: :any,                 high_sierra:   "a1061331c1e9b4a726c818005a3d795ba8c73b29ecd78a3828b5e5eafac18107"
+    sha256 cellar: :any,                 sierra:        "6229ee0c1e44192fa0d513b7e72e5c72e7fbd29b5ad7f61cd5c5824d76d49105"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2bf28a5dccb7020470c149992f9afc7cf2d407b4a92be3fcf81855bd2951eb0"
   end
 
   depends_on "openssl@1.1"
