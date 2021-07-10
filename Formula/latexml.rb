a6c0631c9e4b8bf77680bd289e6f399accf9b38c
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:44:13 +0000

    latexml: update 0.8.5 bottle.

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -16,6 +16,7 @@ class Latexml < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5311fc896eb2d27a5fa149bea4d30e6be0f6693cde9da9709b32f11212f726e1"
     sha256 cellar: :any_skip_relocation, catalina:      "3af6c43c91d733210e1e75db13093c72aa91addc1cc8ae94cd716de6ec73ea37"
     sha256 cellar: :any_skip_relocation, mojave:        "f51f58dc03c62130191b1bf2bb223459da93cdb7a7a1c3052a227a4bdc67dfe9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8cfc70adb30f6d9427642a94fa48618b2242bcb2d11ec3c6256e6313fb12694"
   end
 
   uses_from_macos "libxml2"
