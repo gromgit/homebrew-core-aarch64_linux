7de6f617e8501129c1e496c1738ee7993431efb6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 01:03:34 +0000

    subfinder: update 2.4.8 bottle.

--- Formula/subfinder.rb
+++ Formula/subfinder.rb
@@ -11,6 +11,7 @@ class Subfinder < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "6caece489765bc37f6164278c96d5ef2a6bf081a3e70d0e9513d4dba803cda9f"
     sha256 cellar: :any_skip_relocation, catalina:      "88cab330e3cfb8325a8d295c512598670f626408e7a90520605f684c20089e06"
     sha256 cellar: :any_skip_relocation, mojave:        "35a8328f2fdc17c610ab78cf29dca7ef13a5cc77c6547983c1e81abdd6dcb939"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0443b0e2d76ee7252666a214cc498d9050dc63c8bdcfc05bcfd5e50eaca43bee"
   end
 
   depends_on "go" => :build
