822c99fa69be8891b53575603966ba531775bba8
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 21:01:42 +0200

    libepoxy: fix test for Linux (#80989)

--- Formula/libepoxy.rb
+++ Formula/libepoxy.rb
@@ -40,11 +40,14 @@ class Libepoxy < Formula
     (testpath/"test.c").write <<~EOS
 
       #include <epoxy/gl.h>
+      #ifdef OS_MAC
       #include <OpenGL/CGLContext.h>
       #include <OpenGL/CGLTypes.h>
       #include <OpenGL/OpenGL.h>
+      #endif
       int main()
       {
+          #ifdef OS_MAC
           CGLPixelFormatAttribute attribs[] = {0};
           CGLPixelFormatObj pix;
           int npix;
@@ -52,10 +55,13 @@ class Libepoxy < Formula
 
           CGLChoosePixelFormat( attribs, &pix, &npix );
           CGLCreateContext(pix, (void*)0, &ctx);
+          #endif
 
           glClear(GL_COLOR_BUFFER_BIT);
+          #ifdef OS_MAC
           CGLReleasePixelFormat(pix);
           CGLReleaseContext(pix);
+          #endif
           return 0;
       }
     EOS
