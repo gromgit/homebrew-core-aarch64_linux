c326e5007c045433d57648761412f1f8107f65e8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 01:20:21 +0000

    boost-bcp: update 1.76.0 bottle.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -11,6 +11,7 @@ class BoostBcp < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "16fbea4de8f872bac006d526c2bac139b04d0f1bf72741902c7f78825f8945a6"
     sha256 cellar: :any_skip_relocation, catalina:      "8dcc84a022e19a58ac5c7b3ab161cfc544c33607b81de0a0c80037d1f1050081"
     sha256 cellar: :any_skip_relocation, mojave:        "e592d4d8ef2d288683c39193ccdb35e9ef451f5217b7089c21ad7dae012f2084"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "beb100da3b36a96b90c2b73d59885e0c5701004caccea8478d40d33563a67999"
   end
 
   depends_on "boost-build" => :build
