f452330208d8b057ad76ba14aae880bf18770d9a
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 09:54:41 +0000

    pike: update 8.0.1116 bottle.

--- Formula/pike.rb
+++ Formula/pike.rb
@@ -17,6 +17,7 @@ class Pike < Formula
     sha256 big_sur:       "15805f34b5fa27d32c8a86cb69459d03300b11b2ec491597264823899f77568f"
     sha256 catalina:      "b34252e2bd9dfea5ffc5e56e10523658ff44ecc5686eabda34ce2c19a22faabc"
     sha256 mojave:        "1eacafb5514b416f75a39a50bdfc697123f66be10a892be1906fa674580f9298"
+    sha256 x86_64_linux:  "ff33dee60c50a36bbf9e5882a691e8062c4e338058b20f27d5edb0fc4989eb37"
   end
 
   depends_on "gmp"
