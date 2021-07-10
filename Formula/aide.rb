625d1e7547dde004bd5a95bcee0be65af89061d9
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:48:07 +0000

    aide: update 0.17.3 bottle.

--- Formula/aide.rb
+++ Formula/aide.rb
@@ -6,10 +6,11 @@ class Aide < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bc933e63d10ab2fdd5d974c32c3f0898f37bf9d95f3e74129eae1b860bc785c8"
-    sha256 cellar: :any, big_sur:       "39d24815ba23cd86b3a31c18e7e27b6c4a231dfbfb9e6fb035efec84a68b1ccf"
-    sha256 cellar: :any, catalina:      "b765af2fe87ab4494ddb51680f9fa151cf7d91b853d1a6bc5f1c1c0e14eda155"
-    sha256 cellar: :any, mojave:        "a24aedaabc8dfa8341e7713e43c994db019fb12747a497ac735a7864079cedb0"
+    sha256 cellar: :any,                 arm64_big_sur: "bc933e63d10ab2fdd5d974c32c3f0898f37bf9d95f3e74129eae1b860bc785c8"
+    sha256 cellar: :any,                 big_sur:       "39d24815ba23cd86b3a31c18e7e27b6c4a231dfbfb9e6fb035efec84a68b1ccf"
+    sha256 cellar: :any,                 catalina:      "b765af2fe87ab4494ddb51680f9fa151cf7d91b853d1a6bc5f1c1c0e14eda155"
+    sha256 cellar: :any,                 mojave:        "a24aedaabc8dfa8341e7713e43c994db019fb12747a497ac735a7864079cedb0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "296511a7306bbd01163594427a1ce69c92cbf83efc17f9de63c3ec590a881fd8"
   end
 
   head do
