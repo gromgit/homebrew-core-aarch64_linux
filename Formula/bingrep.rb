95487b3d363f6d7cf759a83be3bb7b949664d530
rui <rui@chenrui.dev>
2021-07-10 16:25:27 +0000

    bingrep: update 0.8.5 bottle.

--- Formula/bingrep.rb
+++ Formula/bingrep.rb
@@ -10,6 +10,7 @@ class Bingrep < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a4024e1291282371002c7af0a35fcb55e929300b0b60a4b2f97dfd0755af396e"
     sha256 cellar: :any_skip_relocation, catalina:      "bf59cab5aa3c7710fe2910aac21433ff8c10c4101b44df3669783d588178633f"
     sha256 cellar: :any_skip_relocation, mojave:        "3fde029ce5bc7b15c715543091868082ffcdd8f1be4d661061b8256c0289dcba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b7e69c3e209fd5c7ed4b1918ac822740948462dced68587a87113b0a155293a"
   end
 
   depends_on "rust" => :build
