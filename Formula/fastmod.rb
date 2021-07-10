415e8dfe43995bc50eb0d29af74403cb4610f75c
rui <rui@chenrui.dev>
2021-07-10 16:54:22 +0000

    fastmod: update 0.4.2 bottle.

--- Formula/fastmod.rb
+++ Formula/fastmod.rb
@@ -10,6 +10,7 @@ class Fastmod < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "09e462f4355d7dcac277c027280d87f5da45c07d97e20bb36cb1419e03e23137"
     sha256 cellar: :any_skip_relocation, catalina:      "b775cece5ec1a8af87f117eb9946569632c95b878242ef94370ecda978bd3989"
     sha256 cellar: :any_skip_relocation, mojave:        "48985e1b88574be11d01213776f125f4fbf45aaf4f4cdbf71c0341e1faf60a49"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b99ce5b1c88db72f09685bf9700f35d95a90aad6691902661f43b8c0acec5c2c"
   end
 
   depends_on "rust" => :build
