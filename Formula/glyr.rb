aa8e785351a1e3378e6042346dbcfa0605c1efca
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:57:56 +0000

    glyr: update 1.0.10_1 bottle.

--- Formula/glyr.rb
+++ Formula/glyr.rb
@@ -7,12 +7,13 @@ class Glyr < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b0f1519a6dde37bf695753060e8f783cffae1ea40c1c0f7ceba3134c21ec3f65"
-    sha256 cellar: :any, big_sur:       "70813cb0a175f7e1b470c334cd80ca97e255d9d1390c2868257ebfe159d85023"
-    sha256 cellar: :any, catalina:      "0a32bfceb64d33842aee008ca44e823062589323777efee2f15f013f18017a08"
-    sha256 cellar: :any, mojave:        "45d36208e031f97c1202824c6a6a0a9e97d777fae91ce7cddc3ca17c3168d31c"
-    sha256 cellar: :any, high_sierra:   "fb3ef9186aae754a62a466aae16471049bdaefcc168106fc6f0097e937115524"
-    sha256 cellar: :any, sierra:        "04cbfc6d3294d068b3a97bfb5235aed84b3e95478f8f3e873be17127142b07f6"
+    sha256 cellar: :any,                 arm64_big_sur: "b0f1519a6dde37bf695753060e8f783cffae1ea40c1c0f7ceba3134c21ec3f65"
+    sha256 cellar: :any,                 big_sur:       "70813cb0a175f7e1b470c334cd80ca97e255d9d1390c2868257ebfe159d85023"
+    sha256 cellar: :any,                 catalina:      "0a32bfceb64d33842aee008ca44e823062589323777efee2f15f013f18017a08"
+    sha256 cellar: :any,                 mojave:        "45d36208e031f97c1202824c6a6a0a9e97d777fae91ce7cddc3ca17c3168d31c"
+    sha256 cellar: :any,                 high_sierra:   "fb3ef9186aae754a62a466aae16471049bdaefcc168106fc6f0097e937115524"
+    sha256 cellar: :any,                 sierra:        "04cbfc6d3294d068b3a97bfb5235aed84b3e95478f8f3e873be17127142b07f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df73a2e61d24c31802aeead7557ebef37ce02523974b69b2cfa873faf9327501"
   end
 
   depends_on "cmake" => :build
