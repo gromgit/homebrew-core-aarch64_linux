fc272aed84a4a3ac11f846840b7344c7352ab0bc
rui <rui@chenrui.dev>
2021-07-10 16:36:44 +0000

    diesel: update 1.4.7 bottle.

--- Formula/diesel.rb
+++ Formula/diesel.rb
@@ -7,10 +7,11 @@ class Diesel < Formula
   head "https://github.com/diesel-rs/diesel.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7504ecbba4c99649dd56c2c0ac4df434a04251049d0364f685eec03165a1ee00"
-    sha256 cellar: :any, big_sur:       "3d28f59de8c707d09adf14916802d0ddb8db59d54a0318e6fcbb99c53477f0da"
-    sha256 cellar: :any, catalina:      "b63e15c3252fdc1907518309ab875447892ed03bd4de4a7853a0cee2f0284879"
-    sha256 cellar: :any, mojave:        "9da02b6126a83dc9a80e994ae21c93e46b839d28d5b544fdda9cc64f8fb25dc8"
+    sha256 cellar: :any,                 arm64_big_sur: "7504ecbba4c99649dd56c2c0ac4df434a04251049d0364f685eec03165a1ee00"
+    sha256 cellar: :any,                 big_sur:       "3d28f59de8c707d09adf14916802d0ddb8db59d54a0318e6fcbb99c53477f0da"
+    sha256 cellar: :any,                 catalina:      "b63e15c3252fdc1907518309ab875447892ed03bd4de4a7853a0cee2f0284879"
+    sha256 cellar: :any,                 mojave:        "9da02b6126a83dc9a80e994ae21c93e46b839d28d5b544fdda9cc64f8fb25dc8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd50b0f715f7328561b9e81d97b4d305dc468d54896695f0444da8e727c57c4c"
   end
 
   depends_on "rust" => [:build, :test]
