2724fd8bf0ed07489547fff385612dad2b70db6e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:49:53 +0000

    bzt: update 1.15.2 bottle.

--- Formula/bzt.rb
+++ Formula/bzt.rb
@@ -9,10 +9,11 @@ class Bzt < Formula
   head "https://github.com/Blazemeter/taurus.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "74f43e75579eaac72b2e1edb4ed9916a4b44c382ac0ff7e39a4e9d1c84a85259"
-    sha256 cellar: :any, big_sur:       "1e5c8fba04e0a590223e5930c1dd7d82b0c5d3886953a6b5509fe589df096d56"
-    sha256 cellar: :any, catalina:      "ba0af87b32de700c761871864520f5051ad564b61331e6923ad0031174a874a3"
-    sha256 cellar: :any, mojave:        "2861a7d8cea73f7e60f971ccc2fd820b8fbfe85ad56c48dba1381d36857c61e8"
+    sha256 cellar: :any,                 arm64_big_sur: "74f43e75579eaac72b2e1edb4ed9916a4b44c382ac0ff7e39a4e9d1c84a85259"
+    sha256 cellar: :any,                 big_sur:       "1e5c8fba04e0a590223e5930c1dd7d82b0c5d3886953a6b5509fe589df096d56"
+    sha256 cellar: :any,                 catalina:      "ba0af87b32de700c761871864520f5051ad564b61331e6923ad0031174a874a3"
+    sha256 cellar: :any,                 mojave:        "2861a7d8cea73f7e60f971ccc2fd820b8fbfe85ad56c48dba1381d36857c61e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ca050c502cb7ddadec7ce2ea34bbec2970846ad0531a6387aeda64a00f8efa3"
   end
 
   depends_on "rust" => :build
