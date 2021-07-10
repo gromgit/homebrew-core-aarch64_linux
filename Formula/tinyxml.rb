4fb2cfaa7362e120c94d911168873ff1c4787d27
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 01:21:46 +0000

    tinyxml: update 2.6.2 bottle.

--- Formula/tinyxml.rb
+++ Formula/tinyxml.rb
@@ -5,14 +5,15 @@ class Tinyxml < Formula
   sha256 "15bdfdcec58a7da30adc87ac2b078e4417dbe5392f3afb719f9ba6d062645593"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "04fccb4076db86eb901b710f5d50b01ea6e6cec907979aed5eb5135c9654e16d"
-    sha256 cellar: :any, big_sur:       "e98aaca0d889c322b5e2294495e7613e656773fb5f605a6239d8b85949011b99"
-    sha256 cellar: :any, catalina:      "7cc1ada5d273bec9f50a1809a9989306ec9601a037c06b362cee321fbdc5c0a7"
-    sha256 cellar: :any, mojave:        "c1fc1d7fa9e6934412294e921cde90bcfd107b68dbdddd9acf8cae4927190718"
-    sha256 cellar: :any, high_sierra:   "ec0f83018a9ff93c11b6a8c92483056b2771359a14aedfb6dc46e1ab078ce9ac"
-    sha256 cellar: :any, sierra:        "ef8c7bbbae6148e161b6f3369ede8bd3533a32847dc716000b46d26e6fb1c26c"
-    sha256 cellar: :any, el_capitan:    "16e6052892b43e68c45f5122b6802e9bc32001dc9478dfcd89511a24544660e5"
-    sha256 cellar: :any, yosemite:      "4b1df9cb229b04f9968621a52737d96e86fcd6c2ad8904ae8a5c324347845f50"
+    sha256 cellar: :any,                 arm64_big_sur: "04fccb4076db86eb901b710f5d50b01ea6e6cec907979aed5eb5135c9654e16d"
+    sha256 cellar: :any,                 big_sur:       "e98aaca0d889c322b5e2294495e7613e656773fb5f605a6239d8b85949011b99"
+    sha256 cellar: :any,                 catalina:      "7cc1ada5d273bec9f50a1809a9989306ec9601a037c06b362cee321fbdc5c0a7"
+    sha256 cellar: :any,                 mojave:        "c1fc1d7fa9e6934412294e921cde90bcfd107b68dbdddd9acf8cae4927190718"
+    sha256 cellar: :any,                 high_sierra:   "ec0f83018a9ff93c11b6a8c92483056b2771359a14aedfb6dc46e1ab078ce9ac"
+    sha256 cellar: :any,                 sierra:        "ef8c7bbbae6148e161b6f3369ede8bd3533a32847dc716000b46d26e6fb1c26c"
+    sha256 cellar: :any,                 el_capitan:    "16e6052892b43e68c45f5122b6802e9bc32001dc9478dfcd89511a24544660e5"
+    sha256 cellar: :any,                 yosemite:      "4b1df9cb229b04f9968621a52737d96e86fcd6c2ad8904ae8a5c324347845f50"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ade5525899de7063ade79d1b0dec70ceef3d0acc08e1dc1b55e937cb539ad38d"
   end
 
   depends_on "cmake" => :build
