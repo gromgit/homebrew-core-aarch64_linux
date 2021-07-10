586d855a8575e8829ce010c427dbbda481f95412
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 14:43:25 +0000

    castxml: update 0.4.3_1 bottle.

--- Formula/castxml.rb
+++ Formula/castxml.rb
@@ -13,10 +13,11 @@ class Castxml < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a621a2f54f8b157f47251f7d92032e4ecf9e4611995979533b5a9176f3849057"
-    sha256 cellar: :any, big_sur:       "baef1dc2e08d7d0a0f276c54a3f180d662abc1cd660a875aec2c6db8ddace248"
-    sha256 cellar: :any, catalina:      "7e97d6cb586ba2ffbcc00bcfaf146c5cf6838aaa01f6ef83a2c8033ab34f6acd"
-    sha256 cellar: :any, mojave:        "5a24c3bad04790815e3af774e12ad5f9d800318dfaf9bc6b8b3bef7b32db61ea"
+    sha256 cellar: :any,                 arm64_big_sur: "a621a2f54f8b157f47251f7d92032e4ecf9e4611995979533b5a9176f3849057"
+    sha256 cellar: :any,                 big_sur:       "baef1dc2e08d7d0a0f276c54a3f180d662abc1cd660a875aec2c6db8ddace248"
+    sha256 cellar: :any,                 catalina:      "7e97d6cb586ba2ffbcc00bcfaf146c5cf6838aaa01f6ef83a2c8033ab34f6acd"
+    sha256 cellar: :any,                 mojave:        "5a24c3bad04790815e3af774e12ad5f9d800318dfaf9bc6b8b3bef7b32db61ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "72e7dd670a57d6da3e1e9d7f56e22a372308231225b559e017ee84da8561b4bb"
   end
 
   depends_on "cmake" => :build
