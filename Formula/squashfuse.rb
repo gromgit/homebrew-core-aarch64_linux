0dd2d45a6ad672d33ae1cbf6f136f76d46f2c0cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-10 21:06:14 +0000

    squashfuse: update 0.1.104 bottle.

--- Formula/squashfuse.rb
+++ Formula/squashfuse.rb
@@ -6,11 +6,7 @@ class Squashfuse < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, catalina:    "7e2e0499c0b9f98beb398319c949d2a1d45de6a3f0b546ef1d55214f68522312"
-    sha256 cellar: :any, mojave:      "f4cb4305f7773fbf927d51a401453c3cdee1f1d48da2ef33d8fd41d526fa7c0d"
-    sha256 cellar: :any, high_sierra: "ada7e939ff42fcd9fb6b1fb81ab596463d6149ff592f73ca924b5b9dca5ddfc4"
-    sha256 cellar: :any, sierra:      "c1898c81ae091097ae2502ecbdebdd1831db302dd74b814003191007a4d5f018"
-    sha256 cellar: :any, el_capitan:  "bf4e6ca88d094fd7d92fbab61dd1c3a4e71b60d7668d23b6044c90e8167833c5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b55b7786381a508c76bfe3d278e2a669d28755ec59d458a728fd12b760802d2a"
   end
 
   depends_on "pkg-config" => :build
