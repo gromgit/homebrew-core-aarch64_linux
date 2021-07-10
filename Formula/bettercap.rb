bbc13c4dd1bdda80c058ef3807ca4d6899c17752
rui <rui@chenrui.dev>
2021-07-10 20:11:31 +0000

    bettercap: update 2.31.1 bottle.

--- Formula/bettercap.rb
+++ Formula/bettercap.rb
@@ -7,10 +7,11 @@ class Bettercap < Formula
   head "https://github.com/bettercap/bettercap.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "63f4451b9ab250464d26d27d4358c4c0d4f5aaadecb2bbfaac1e8f4b1c411d2b"
-    sha256 cellar: :any, big_sur:       "8fb85b3ad02828d25d5a8fc942ccb6cd9ac95f7fe276768060dd353b27b020ab"
-    sha256 cellar: :any, catalina:      "2fc1f3339e9c4a0143de65c4cdf0a32d6d7ce17c924ac46e71cdc2bebdadd413"
-    sha256 cellar: :any, mojave:        "16fa04f46a97b8919af31d58cb0815c440abb3fec255f4caca073242c05d88d8"
+    sha256 cellar: :any,                 arm64_big_sur: "63f4451b9ab250464d26d27d4358c4c0d4f5aaadecb2bbfaac1e8f4b1c411d2b"
+    sha256 cellar: :any,                 big_sur:       "8fb85b3ad02828d25d5a8fc942ccb6cd9ac95f7fe276768060dd353b27b020ab"
+    sha256 cellar: :any,                 catalina:      "2fc1f3339e9c4a0143de65c4cdf0a32d6d7ce17c924ac46e71cdc2bebdadd413"
+    sha256 cellar: :any,                 mojave:        "16fa04f46a97b8919af31d58cb0815c440abb3fec255f4caca073242c05d88d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec28652d5e5062adc58f29f54a38f1b66f1dd328039dc658c152341cc5d45ca7"
   end
 
   depends_on "go" => :build
