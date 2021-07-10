d7ec7390d3c8c3596c54221518187d0d65d0bc86
rui <rui@chenrui.dev>
2021-07-10 16:54:50 +0000

    git-interactive-rebase-tool: update 2.1.0 bottle.

--- Formula/git-interactive-rebase-tool.rb
+++ Formula/git-interactive-rebase-tool.rb
@@ -15,6 +15,7 @@ class GitInteractiveRebaseTool < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "4480f59a021edb031a3ef02b5cccae62719dad12f6ae7fc2413b75088be466ac"
     sha256 cellar: :any_skip_relocation, catalina:      "9223a3f962e2af3897cefb9e221a529be7701e964005d55436f7de9d97d1b573"
     sha256 cellar: :any_skip_relocation, mojave:        "bfab245c62dd4a37da0c275d8b4642ee86b5912153465da1e312a510fce60b80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c37316d7e0b5bc7d99d9979a001db0fbb348f6c6229ce5abfa7b50be39406c1"
   end
 
   depends_on "rust" => :build
