e93da12fc8f45cdb76194866af58e74ab28b32d2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:45:26 +0000

    psqlodbc: update 13.01.0000 bottle.

--- Formula/psqlodbc.rb
+++ Formula/psqlodbc.rb
@@ -10,10 +10,11 @@ class Psqlodbc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f8d95d1b2ba785dcb50b91b0b50e49b4f40d7f05b5417dc7eaf8b078ca88464a"
-    sha256 cellar: :any, big_sur:       "4bc2ee5d89cc5f84220abdcbcef1a42b30959cf19b071a46a13cb9c583ee9142"
-    sha256 cellar: :any, catalina:      "3d65ab705dbece7ef42f2fc975e3ee8f118a6fc1e12a51d9b34425cf83dfc860"
-    sha256 cellar: :any, mojave:        "9b8332fa7f0b03677744c1e48057e65f04140892cc143fa32858179c9f1cc38a"
+    sha256 cellar: :any,                 arm64_big_sur: "f8d95d1b2ba785dcb50b91b0b50e49b4f40d7f05b5417dc7eaf8b078ca88464a"
+    sha256 cellar: :any,                 big_sur:       "4bc2ee5d89cc5f84220abdcbcef1a42b30959cf19b071a46a13cb9c583ee9142"
+    sha256 cellar: :any,                 catalina:      "3d65ab705dbece7ef42f2fc975e3ee8f118a6fc1e12a51d9b34425cf83dfc860"
+    sha256 cellar: :any,                 mojave:        "9b8332fa7f0b03677744c1e48057e65f04140892cc143fa32858179c9f1cc38a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4befb4353cbd839bebed19fd661449af4f0c31b61063dadaa32bb318aaef1a80"
   end
 
   head do
