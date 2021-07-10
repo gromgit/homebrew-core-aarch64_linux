8c8ab12b94b05a145cc2a9f9dca5981b6b04e2f1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 01:04:38 +0000

    logrotate: update 3.18.1 bottle.

--- Formula/logrotate.rb
+++ Formula/logrotate.rb
@@ -6,10 +6,11 @@ class Logrotate < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7a35f391118adf13094e83d7ab66a0a0842828b6d17bb825a04a2c15ff0cb8b5"
-    sha256 cellar: :any, big_sur:       "6976c5e710c45560e47c8eaba409aaf71e607cec3e86eaa6396df5342685b720"
-    sha256 cellar: :any, catalina:      "82980e80bdd29e8b5d21661e5ffd283c654057912fb0d8621e30397f577dc1e4"
-    sha256 cellar: :any, mojave:        "2aeb1ee2d25cb426a1d7a746c54451afad0d5f00a24c6cf6f38eb5be2a0c4e5a"
+    sha256 cellar: :any,                 arm64_big_sur: "7a35f391118adf13094e83d7ab66a0a0842828b6d17bb825a04a2c15ff0cb8b5"
+    sha256 cellar: :any,                 big_sur:       "6976c5e710c45560e47c8eaba409aaf71e607cec3e86eaa6396df5342685b720"
+    sha256 cellar: :any,                 catalina:      "82980e80bdd29e8b5d21661e5ffd283c654057912fb0d8621e30397f577dc1e4"
+    sha256 cellar: :any,                 mojave:        "2aeb1ee2d25cb426a1d7a746c54451afad0d5f00a24c6cf6f38eb5be2a0c4e5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b450a79764d5a0bb3e4cfddf3bc337bb703c34386254b76dbcd45fd09ae72ab7"
   end
 
   depends_on "popt"
