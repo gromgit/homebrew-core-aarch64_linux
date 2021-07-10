93c80ef44fca9e95c8b21014739598b01dafa590
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-10 00:58:02 +0000

    softhsm: update 2.6.1 bottle.

--- Formula/softhsm.rb
+++ Formula/softhsm.rb
@@ -20,6 +20,7 @@ class Softhsm < Formula
     sha256 catalina:      "6da111cdadbcf0127882e2bec5b3844454fd9b4e00a08d1fa49aa2f389b7062c"
     sha256 mojave:        "b7abd86dfec3d10f5e5cde00f2bcd5e0e19e2d9674c50a431db1195c4655dfec"
     sha256 high_sierra:   "73c40f26209dbf29280c16aefdfb492c749d8e14e4cbf83dc2a5b566c22f6bc9"
+    sha256 x86_64_linux:  "ccf8a6487c0cdc7ed07929e739d98029daf389f9c0b8b2924357216ab8b8b8e7"
   end
 
   head do
