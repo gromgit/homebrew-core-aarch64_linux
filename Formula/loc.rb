c0a6280db5ca3bbde85c3a922f4df676f8b769c6
rui <rui@chenrui.dev>
2021-07-10 17:01:46 +0000

    loc: update 0.4.1 bottle.

--- Formula/loc.rb
+++ Formula/loc.rb
@@ -12,6 +12,7 @@ class Loc < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "2a8ac9341661cefa1221418aa2cb5cdd5207108ade6803ab5af34ca01d0aef13"
     sha256 cellar: :any_skip_relocation, mojave:        "008db46fed420d7ec698d46e059a4913368af4d8f0b2f4f8502a39ee392b830d"
     sha256 cellar: :any_skip_relocation, high_sierra:   "f4241a70db520e24c587649bf7b8db0f743afaf00b01ebee5934bee7e88ae42e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62f8a3305c22f0287483e3c1acbbd4110f4eeec3fc846dfeadbc0420bb51b6b6"
   end
 
   depends_on "rust" => :build
