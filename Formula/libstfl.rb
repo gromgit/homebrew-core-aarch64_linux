3f921698e9f5a1400ec40ad497fef4290ae974bf
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-10 18:17:00 +0200

    libstfl: fix build for Linux and bindings for macOS

--- Formula/libstfl.rb
+++ Formula/libstfl.rb
@@ -4,7 +4,7 @@ class Libstfl < Formula
   url "http://www.clifford.at/stfl/stfl-0.24.tar.gz"
   sha256 "d4a7aa181a475aaf8a8914a8ccb2a7ff28919d4c8c0f8a061e17a0c36869c090"
   license "LGPL-3.0-or-later"
-  revision 12
+  revision 13
 
   livecheck do
     url :homepage
@@ -19,69 +19,68 @@ class Libstfl < Formula
     sha256 cellar: :any, mojave:        "0164c390d3ccad76079fe8b5af6e8cec440036e4e97dd91c5a1c86848832c0ab"
   end
 
-  depends_on "python@3.9" => :build
+  depends_on "python@3.9" => [:build, :test]
   depends_on "swig" => :build
   depends_on "ruby"
 
-  uses_from_macos "perl"
+  uses_from_macos "perl" => [:build, :test]
+  uses_from_macos "ncurses"
+
+  # Fix Python logic to be compatible with Python 3
+  # Fix Ruby logic to use Homebrew Ruby's sitedir
+  patch :DATA
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
-
-    ENV.append "LIBS", "-lncurses -lruby"
-
     on_macos do
       ENV.append "LDLIBS", "-liconv"
-      ENV.append "LIBS", "-liconv"
+      ENV.append "LIBS", "-lncurses -lruby -liconv"
+
+      inreplace "stfl_internals.h", "ncursesw/ncurses.h", "ncurses.h"
+      inreplace %w[stfl.pc.in ruby/Makefile.snippet], "ncursesw", "ncurses"
+
+      inreplace "Makefile" do |s|
+        s.gsub! "ncursesw", "ncurses"
+        s.gsub! "-Wl,-soname,$(SONAME)", "-Wl"
+        s.gsub! "libstfl.so.$(VERSION)", "libstfl.$(VERSION).dylib"
+        s.gsub! "libstfl.so", "libstfl.dylib"
+      end
+
+      # Fix ncurses linkage for Perl bundle
+      inreplace "perl5/Makefile.PL", "-lncursesw", "-L#{MacOS.sdk_path}/usr/lib -lncurses"
     end
 
-    %w[
-      stfl.pc.in
-      perl5/Makefile.PL
-      ruby/Makefile.snippet
-    ].each do |f|
-      inreplace f, "ncursesw", "ncurses"
+    on_linux do
+      ENV.append "LIBS", "-lncursesw -lruby"
+      inreplace "Makefile", "$(LDLIBS) $^", "$^ $(LDLIBS)"
     end
 
-    inreplace "stfl_internals.h", "ncursesw/ncurses.h", "ncurses.h"
-
-    inreplace "Makefile" do |s|
-      s.gsub! "ncursesw", "ncurses"
-      s.gsub! "-Wl,-soname,$(SONAME)", "-Wl"
-      s.gsub! "libstfl.so.$(VERSION)", "libstfl.$(VERSION).dylib"
-      s.gsub! "libstfl.so", "libstfl.dylib"
-    end
-
+    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
     xy = Language::Python.major_minor_version "python3"
-    python_config = Formula["python@3.9"].opt_libexec/"bin/python-config"
+    py_cflags = Utils.safe_popen_read("python-config", "--cflags").chomp
 
     inreplace "python/Makefile.snippet" do |s|
       # Install into the site-packages in the Cellar (so uninstall works)
       s.change_make_var! "PYTHON_SITEARCH", lib/"python#{xy}/site-packages"
-      s.gsub! "lib-dynload/", ""
-      s.gsub! "ncursesw", "ncurses"
+      s.gsub! "gcc", "gcc #{py_cflags}"
 
       on_macos do
-        s.gsub! "gcc", "gcc -undefined dynamic_lookup #{`#{python_config} --cflags`.chomp}"
-        s.gsub! "-lncurses", "-lncurses -liconv"
-      end
-
-      on_linux do
-        s.gsub! "gcc", "gcc #{`#{python_config} --cflags`.chomp}"
+        s.gsub! "-lncursesw", "-lncurses -liconv"
+        s.gsub! "gcc", "gcc -undefined dynamic_lookup"
       end
     end
+
+    inreplace "perl5/Makefile.snippet", "perl Makefile.PL", "perl Makefile.PL INSTALL_BASE=#{libexec}"
 
     # Fails race condition of test:
     #   ImportError: dynamic module does not define init function (init_stfl)
     #   make: *** [python/_stfl.so] Error 1
     ENV.deparallelize
-
     system "make"
-
-    on_macos do
-      inreplace "perl5/Makefile", "Network/Library", libexec/"lib/perl5"
-    end
     system "make", "install", "prefix=#{prefix}"
+
+    on_linux do
+      lib.install_symlink "libstfl.so.#{version}" => "libstfl.so.#{version.major}"
+    end
   end
 
   test do
@@ -95,5 +94,51 @@ class Libstfl < Formula
     EOS
     system ENV.cxx, "test.cpp", "-L#{lib}", "-lstfl", "-o", "test"
     system "./test"
+
+    system Formula["python@3.9"].opt_bin/"python3", "-c", "import stfl"
+    system Formula["ruby"].opt_bin/"ruby", "-e", "require 'stfl'"
+
+    with_env(PERL5LIB: libexec/"lib/perl5") do
+      system "perl", "-e", "use stfl"
+    end
   end
 end
+
+__END__
+--- a/Makefile.cfg
++++ b/Makefile.cfg
+@@ -40,7 +40,7 @@ else
+ FOUND_PERL5 = 0
+ endif
+ 
+-ifneq ($(shell python -c 'print 1' 2>/dev/null),)
++ifneq ($(shell python -c 'print(1)' 2>/dev/null),)
+ FOUND_PYTHON = 1
+ else
+ FOUND_PYTHON = 0
+--- a/python/Makefile.snippet
++++ b/python/Makefile.snippet
+@@ -33,8 +33,6 @@ python/_stfl.so python/stfl.py python/stfl.pyc: libstfl.a stfl.h python/stfl.i s
+ 
+ install_python: python/_stfl.so python/stfl.py python/stfl.pyc
+ 	mkdir -p $(DESTDIR)$(PYTHON_SITEARCH)/
+-	mkdir -p $(DESTDIR)$(PYTHON_SITEARCH)/lib-dynload/
+-	cp python/_stfl.so $(DESTDIR)$(PYTHON_SITEARCH)/lib-dynload/
+-	cp python/stfl.pyc $(DESTDIR)$(PYTHON_SITEARCH)/
++	cp python/_stfl.so $(DESTDIR)$(PYTHON_SITEARCH)/
+ 	cp python/stfl.py $(DESTDIR)$(PYTHON_SITEARCH)/
+ 
+--- a/ruby/Makefile.snippet
++++ b/ruby/Makefile.snippet
+@@ -25,9 +25,9 @@ install: install_ruby
+ 
+ ruby/build_ok: libstfl.a stfl.h ruby/stfl.i swig/*.i
+ 	cd ruby && swig -ruby stfl.i && ruby extconf.rb
+-	$(MAKE) -C ruby clean && $(MAKE) -C ruby LIBS+="../libstfl.a -lncursesw" CFLAGS+="-pthread -I.." DLDFLAGS+="-pthread" DESTDIR=$(DESTDIR) prefix=$(prefix) sitedir=$(prefix)/$(libdir)/ruby
++	$(MAKE) -C ruby clean && $(MAKE) -C ruby LIBS+="../libstfl.a -lncursesw" CFLAGS+="-pthread -I.." DLDFLAGS+="-pthread" DESTDIR=$(DESTDIR) prefix=$(prefix) sitedir=$(prefix)/$(libdir)/ruby/site_ruby
+ 	touch ruby/build_ok
+ 
+ install_ruby: ruby/build_ok
+-	$(MAKE) -C ruby DESTDIR=$(DESTDIR) prefix=$(prefix) sitedir='$(DESTDIR)$(prefix)/$(libdir)/ruby' install
++	$(MAKE) -C ruby DESTDIR=$(DESTDIR) prefix=$(prefix) sitedir='$(DESTDIR)$(prefix)/$(libdir)/ruby/site_ruby' install
+ 
