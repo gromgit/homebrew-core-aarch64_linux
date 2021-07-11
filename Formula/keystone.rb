2433c51b81e21685a0ab651784c7b2978f2ec786
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 20:31:15 +0000

    keystone: update 0.9.2 bottle.

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -12,6 +12,7 @@ class Keystone < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "84cdef2aa8a5697ce2fc62e6ae1316f2dcca6fcd0f92d2bba68b399af9c48440"
     sha256 cellar: :any_skip_relocation, mojave:        "814feeee85e111a21fdd287fbed3fca3e1cd86be396dcba7612c3e1aec7dd4d3"
     sha256 cellar: :any_skip_relocation, high_sierra:   "77740af9b9e48baaf0a7d1dc4d74b883c1babbaab6a7e9bf65a00035b59c546d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf07b840547fc8884f1bca89610cd0012fcb29390d6556acaa2ead6b5be0a256"
   end
 
   depends_on "cmake" => :build
