817000ba0973e8ad084c544017757d605d15fe06
Sam Ford <1584702+samford@users.noreply.github.com>
2021-07-11 19:23:22 -0400

    sdl_image: add head

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -1,10 +1,21 @@
 class SdlImage < Formula
   desc "Image file loading library"
   homepage "https://www.libsdl.org/projects/SDL_image/release-1.2.html"
-  url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
-  sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
   license "Zlib"
   revision 7
+
+  stable do
+    url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
+    sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
+
+    # Fix graphical glitching
+    # https://github.com/Homebrew/homebrew-python/issues/281
+    # https://trac.macports.org/ticket/37453
+    patch :p0 do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/41996822/sdl_image/IMG_ImageIO.m.patch"
+      sha256 "c43c5defe63b6f459325798e41fe3fdf0a2d32a6f4a57e76a056e752372d7b09"
+    end
+  end
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "9720413694ba49519d1d1c5213607dbdf177939ae0ee081c03ab2c1d478e2fe3"
@@ -18,6 +29,14 @@ class SdlImage < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d8a3d6067fce20c398a6cfbe5ba87136f9d5968569a613b8a29e3bc3eef4817"
   end
 
+  head do
+    url "https://github.com/libsdl-org/SDL_image.git", branch: "SDL-1.2"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   # SDL 1.2 is deprecated, unsupported, and not recommended for new projects.
   deprecate! date: "2013-08-17", because: :deprecated_upstream
 
@@ -28,16 +47,10 @@ class SdlImage < Formula
   depends_on "sdl"
   depends_on "webp"
 
-  # Fix graphical glitching
-  # https://github.com/Homebrew/homebrew-python/issues/281
-  # https://trac.macports.org/ticket/37453
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/41996822/sdl_image/IMG_ImageIO.m.patch"
-    sha256 "c43c5defe63b6f459325798e41fe3fdf0a2d32a6f4a57e76a056e752372d7b09"
-  end
-
   def install
     inreplace "SDL_image.pc.in", "@prefix@", HOMEBREW_PREFIX
+
+    system "./autogen.sh" if build.head?
 
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
