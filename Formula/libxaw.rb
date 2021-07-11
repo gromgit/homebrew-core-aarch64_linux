ba96ccb6a30918d1594412d741e727d751a281be
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 22:12:27 +0000

    libxaw: update 1.0.14 bottle.

--- Formula/libxaw.rb
+++ Formula/libxaw.rb
@@ -10,6 +10,7 @@ class Libxaw < Formula
     sha256 big_sur:       "bceab125f7dc2fde90b23c68daf8d3a6b5fff65a0f3f3895abe750a74a328dc6"
     sha256 catalina:      "345ff906f7375ae71a550298fd482c849994ed25d0263822fe7ce8f3740db9f2"
     sha256 mojave:        "16cd8aec41f9df9798704213ac41b7e9013d1a8af9f4bda90bfb13d50e55f057"
+    sha256 x86_64_linux:  "fa7c50e5fd51af0f69ee8d2369e390b7961194f227d5d09c2fcb91691f54f54c"
   end
 
   depends_on "pkg-config" => :build
