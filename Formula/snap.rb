471b5216c5a076f5dc4421c2a02977415ef5a8f7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 20:39:04 +0000

    snap: update 2.46 bottle.

--- Formula/snap.rb
+++ Formula/snap.rb
@@ -17,6 +17,7 @@ class Snap < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9cb1d7db5a7f7854fecedf029d130977238a0ed9a8a32a4454225712d3542878"
     sha256 cellar: :any_skip_relocation, catalina:      "f60a56adf86fdc4c86b5d38b47f21c52e0459612c9ed7ae15905a4c838e51787"
     sha256 cellar: :any_skip_relocation, mojave:        "febbdc8548096fb9d0159c8b7cbaa4281ba8b868625264b254aab3ce4d2b7924"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ed92bc9ab2a0e2fe25f5b71d7d240f617a6e2e91e5bc191bb8fa3f49a9cbf5e"
   end
 
   depends_on "go" => :build
