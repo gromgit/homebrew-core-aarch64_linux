dcefd5a0c6567e1c9158047b8619a25718cab179
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 20:46:21 +0000

    v2ray-plugin: update 1.3.1 bottle.

--- Formula/v2ray-plugin.rb
+++ Formula/v2ray-plugin.rb
@@ -17,6 +17,7 @@ class V2rayPlugin < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "891f541e150a393ff20caa78eb79ef12f60929fb9e5b35826e2e639c46a61dc2"
     sha256 cellar: :any_skip_relocation, mojave:        "cb8ff7b812aa561f9e23935461968ba1c26cbe393c599aab4e1753b37702748b"
     sha256 cellar: :any_skip_relocation, high_sierra:   "f11b330c3dc9c445b757188057c93ce94de89f03f4adfa1a8c6405f5ba66b400"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ad295c462b215b5a36d4b8087f03675ef1e0e1508d53dfb96c63bc0be688d8c"
   end
 
   depends_on "go" => :build
