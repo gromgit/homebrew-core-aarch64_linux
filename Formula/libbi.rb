31f183e84ce9ef5c152444b8e5f770988c4c0264
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-12 00:11:58 +0100

    libbi: revision bump (hdf5 1.12.1)

--- Formula/libbi.rb
+++ Formula/libbi.rb
@@ -4,7 +4,7 @@ class Libbi < Formula
   url "https://github.com/lawmurray/LibBi/archive/1.4.5.tar.gz"
   sha256 "af2b6d30e1502f99a3950d63ceaf7d7275a236f4d81eff337121c24fbb802fbe"
   license "GPL-2.0-only"
-  revision 3
+  revision 4
   head "https://github.com/lawmurray/LibBi.git"
 
   bottle do
