beacfa4e786b56c4f6e8e43d429b4a8f21e4a311
Sam Ford <1584702+samford@users.noreply.github.com>
2021-07-11 17:59:14 -0400

    sdl_ttf: update homepage

--- Formula/sdl_ttf.rb
+++ Formula/sdl_ttf.rb
@@ -1,6 +1,6 @@
 class SdlTtf < Formula
   desc "Library for using TrueType fonts in SDL applications"
-  homepage "https://www.libsdl.org/projects/SDL_ttf/"
+  homepage "https://www.libsdl.org/projects/SDL_ttf/release-1.2.html"
   url "https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11.tar.gz"
   sha256 "724cd895ecf4da319a3ef164892b72078bd92632a5d812111261cde248ebcdb7"
   revision 1
