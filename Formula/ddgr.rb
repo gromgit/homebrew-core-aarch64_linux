92c72b3f5a1a7c500c58539206448353cc43614d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 21:02:32 +0000

    ddgr: update 1.9_1 bottle.

--- Formula/ddgr.rb
+++ Formula/ddgr.rb
@@ -14,6 +14,7 @@ class Ddgr < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "2e072910ed4e40203156efec17ffd926142f1dcbe452ee975f7bda67bf658e67"
     sha256 cellar: :any_skip_relocation, mojave:        "e790e6a0175047496f3b81e1226e4e55a1047c9b070dd18ffcd6825d17e840dd"
     sha256 cellar: :any_skip_relocation, high_sierra:   "57266892ebb4fe452b375f06ad756426eecbe7f144e4c6b79673cfce289d34fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8f03229225b4522915949ddd1f83c90112db84ce7c8ff2ede1e3884dcacf0b1"
   end
 
   depends_on "python@3.9"
