1e7e9f6a64d264b43064374b2328bfa1e8b8437e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 00:01:58 +0000

    datamash: update 1.7 bottle.

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -12,6 +12,7 @@ class Datamash < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "f592c4bda737ef924fb4c1642fb381db54c9ce246eb51d03a145dd28a8391406"
     sha256 cellar: :any_skip_relocation, mojave:        "6533f0decc607d6e3ce1ad1fdb7f5b30f99bbbcbacbba1bcd880486eef648189"
     sha256 cellar: :any_skip_relocation, high_sierra:   "b6100d066c3cf9d91b2bd4a8d8bcdc5fa453c6eb6a28d7cacb06659baa358e46"
+    sha256                               x86_64_linux:  "5bff297fd208a9f97975ffabe9c15b701d8dcafb8ac9a8d552c3c288c7d86dc3"
   end
 
   head do
