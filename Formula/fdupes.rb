5e1b11d14c3e25522faefd5fbb6363ea184575b3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-11 20:37:30 +0000

    fdupes: update 2.1.2 bottle.

--- Formula/fdupes.rb
+++ Formula/fdupes.rb
@@ -7,11 +7,12 @@ class Fdupes < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "90adb098f4e20620d34970ac9a959ae70acf8437756e9c339ed58e35a9553a45"
-    sha256 cellar: :any, big_sur:       "045b4c5514bf52f37e955a497b5b8356e1e7e4819e3ee6b67109cc9b8770362e"
-    sha256 cellar: :any, catalina:      "e77144bd7d4b3ed472590b0bb7cb99dea185cf57b5b645bb0558c312441624c0"
-    sha256 cellar: :any, mojave:        "d9504149274c97eb7edb268d43ff18ebd292046d4c5691ae7c7aa9d16b40b0b3"
-    sha256 cellar: :any, high_sierra:   "0bd9b7c00c454042c485b1839ce6cef7f42af21710aa0d83f64a51ab5b18bfe2"
+    sha256 cellar: :any,                 arm64_big_sur: "90adb098f4e20620d34970ac9a959ae70acf8437756e9c339ed58e35a9553a45"
+    sha256 cellar: :any,                 big_sur:       "045b4c5514bf52f37e955a497b5b8356e1e7e4819e3ee6b67109cc9b8770362e"
+    sha256 cellar: :any,                 catalina:      "e77144bd7d4b3ed472590b0bb7cb99dea185cf57b5b645bb0558c312441624c0"
+    sha256 cellar: :any,                 mojave:        "d9504149274c97eb7edb268d43ff18ebd292046d4c5691ae7c7aa9d16b40b0b3"
+    sha256 cellar: :any,                 high_sierra:   "0bd9b7c00c454042c485b1839ce6cef7f42af21710aa0d83f64a51ab5b18bfe2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf15c9f7944fb8a759bb16f42e2081b4e8f2372f6583f65f50701fa09d1b2a9b"
   end
 
   depends_on "pcre2"
