4a836cf6423661844665f7625fc527aeb658b5e4
Caleb Xu <calebcenter@live.com>
2021-07-12 15:34:44 -0400

    ccextractor: use brewed libs
    Closes #81104.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ccextractor.rb
+++ Formula/ccextractor.rb
@@ -3,7 +3,8 @@ class Ccextractor < Formula
   homepage "https://www.ccextractor.org/"
   url "https://github.com/CCExtractor/ccextractor/archive/v0.89.tar.gz"
   sha256 "bbe8d95347d0cf31bd26489b733fd959a7b98c681f14c59309bff54713fd539d"
-  license "GPL-2.0"
+  license "GPL-2.0-only"
+  revision 1
   head "https://github.com/ccextractor/ccextractor.git"
 
   bottle do
@@ -12,20 +13,49 @@ class Ccextractor < Formula
     sha256 cellar: :any_skip_relocation, mojave:   "f389af83990016793f29329e7de8ca7d58c09a25c7faff62bc61d0100d9ee425"
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "freetype"
+  depends_on "gpac"
+  depends_on "leptonica"
+  depends_on "libpng"
+  depends_on "protobuf-c"
+  depends_on "tesseract"
+  depends_on "utf8proc"
+
+  resource "test.mxf" do
+    url "https://raw.githubusercontent.com/alebcay/example-artifacts/5e8d84effab76c4653972ef72513fcee1d00d3c3/mxf/test.mxf"
+    sha256 "e027aca08a2cce64a9fb6623a85306b5481a2f1c3f97a06fd5d3d1b45192b12a"
+  end
+
+  # Patch build script to allow building with Homebrew libs rather than upstream's bundled libs
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/9bc4ef5a88b9a4d55dead30130aa79f8eee5faf7/ccextractor/unbundle-libs.patch"
+    sha256 "b610950e4ae54a8fce3f5952be6d909cb9790a9c46ff356f83e8d8255c7f1ed1"
+  end
+
   def install
-    cd "mac" do
-      system "./build.command"
+    # Multiple source files reference these dependencies with non-standard #include paths
+    ENV["EXTRA_INCLUDE"] = "-I#{Formula["leptonica"].include} -I#{Formula["protobuf-c"].include/"protobuf-c"}"
+
+    platform = "mac"
+    build_script = "./build.command"
+
+    on_linux do
+      platform = "linux"
+      build_script = "./build"
+    end
+
+    cd platform do
+      system build_script, "OCR"
       bin.install "ccextractor"
     end
     (pkgshare/"examples").install "docs/ccextractor.cnf.sample"
   end
 
   test do
-    touch testpath/"test"
-    pid = fork do
-      exec bin/"ccextractor", testpath/"test"
+    resource("test.mxf").stage do
+      system bin/"ccextractor", "test.mxf", "-out=txt"
+      assert_equal "This is a test video.", (Pathname.pwd/"test.txt").read.strip
     end
-    Process.wait(pid)
-    assert_predicate testpath/"test.srt", :exist?
   end
 end
