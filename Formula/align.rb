edb9b17006da16c09409fe3bfc32bee105195e32
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:41:34 +0000

    align: update 1.7.5 bottle.

--- Formula/align.rb
+++ Formula/align.rb
@@ -14,6 +14,7 @@ class Align < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "4d07f4f2ae948de293afdc80a5a736cf81da7c335cec1778f5b7304debda6599"
     sha256 cellar: :any_skip_relocation, el_capitan:    "c2c177c8be3b5a58e60f3a1f39d9fdd3cc3d39247d92be45142cd06ae80273bf"
     sha256 cellar: :any_skip_relocation, yosemite:      "caa9e8c3b3a9d946b95d5222b1518c5307499d57fe17f593ec3911f9cc6eace7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d1c578b01c461e06422ac5c75cd86044992f11ab65552b43b637e647e954baf"
   end
 
   conflicts_with "speech-tools", because: "both install `align` binaries"
