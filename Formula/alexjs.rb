15b98351208439feb2d42ceb462914d6c98ee8e9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:42:55 +0000

    alexjs: update 9.1.0 bottle.

--- Formula/alexjs.rb
+++ Formula/alexjs.rb
@@ -13,6 +13,7 @@ class Alexjs < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "e37814ad3315475e5484cc5c334089213a023ea443ccb2207702b8667f3e1a0e"
     sha256 cellar: :any_skip_relocation, mojave:        "7c2d9fedf79264bd59e73b7f062512e03afdd1e6a66152c233996471636549f1"
     sha256 cellar: :any_skip_relocation, high_sierra:   "462578faccc1fd6ba5df072a50c90c28522067e6cbba84a3b6a6dfa29c7a2eaf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f68677cdd5368c5da400dd06c99652dd90a30a0973d0cb4fc80fdadc994a5ea7"
   end
 
   depends_on "node"
