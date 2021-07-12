eaa9c1e156a78cc83b6bb6777b6de8b180c009c0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:53:05 +0000

    b2-tools: update 2.5.0 bottle.

--- Formula/b2-tools.rb
+++ Formula/b2-tools.rb
@@ -12,6 +12,7 @@ class B2Tools < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "75735af4817d7132832f71584cb5cedc231314956468d1cbedac36b25ef199b1"
     sha256 cellar: :any_skip_relocation, catalina:      "70ff4cca82288052b2965b2a6a5ba8a923430b13dc9df60d39df6da94b1673f6"
     sha256 cellar: :any_skip_relocation, mojave:        "f8c93c611413d4ec64082059d548997f05c467d4b7e4c8e577f70d1d88f91a78"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a5305474f0b0b6c3085a65b36f33a88e79ebcd25afab62229414d02a3a9d5d46"
   end
 
   depends_on "python@3.9"
