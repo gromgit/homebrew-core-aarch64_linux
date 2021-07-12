8538d41bcf336ad4e6d08ad7dbbb13d039a2941d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:39:52 +0000

    aescrypt: update 0.7 bottle.

--- Formula/aescrypt.rb
+++ Formula/aescrypt.rb
@@ -19,6 +19,7 @@ class Aescrypt < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "2250bd07f689721287269dc70c504b4f08ac2a02b5550ad9f0a51dca60ed6f9a"
     sha256 cellar: :any_skip_relocation, el_capitan:    "0cd940c7c9e59104746a8f83f92a06e703e7f98195a202d20516c03b588fd63f"
     sha256 cellar: :any_skip_relocation, yosemite:      "660c8a9266d7f85e699fb5bfabb82c508a66d303b2a2057c9c70a3c70fed43f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a2705c4423a86919b9309235a468b5cc1ac66200501bd255cefe0d26d1f07c6"
   end
 
   def install
