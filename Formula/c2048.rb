19a72f0313890ea7ef6601bd03c7aaf96ecd439b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 19:00:16 +0000

    c2048: update 0+20150805 bottle.

--- Formula/c2048.rb
+++ Formula/c2048.rb
@@ -16,6 +16,7 @@ class C2048 < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "d2f33783cf7cd2ac69eaed113d940aca31e02e5863fcdb40e200e3fe9a4d0623"
     sha256 cellar: :any_skip_relocation, el_capitan:    "8f9e75196f87718be0c572f731cecba0c8cd4e8dc35f8b3027392cd6e1c45f5d"
     sha256 cellar: :any_skip_relocation, yosemite:      "c06bde9e58788a1a4f16b6d0ace89be02cf07f86211e0c78af5fdaa7d70a3614"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a13bac58f461c8fff9257a3af3f01569d0bf819248ddbf5840bbf1fc9492adfa"
   end
 
   def install
