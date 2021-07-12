7458270669fa552a199447a266af27bc8a59a7e9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:57:43 +0000

    bonnie++: update 2.00a bottle.

--- Formula/bonnie++.rb
+++ Formula/bonnie++.rb
@@ -15,6 +15,7 @@ class Bonniexx < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "75e1876579c6638c1e4c0509af5c76950ae379b034e6a051d091593cb08c1ddd"
     sha256 cellar: :any_skip_relocation, catalina:      "83df0761686086ae64a3c08433613908d9c39d85daa7f81011b5bd70d2d5eb3d"
     sha256 cellar: :any_skip_relocation, mojave:        "c503806d5f1ad449a6943275fa93a3930fbbd7cd63b31ee873590d0219ded5b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0418a37628a09a44eeb05aecca9b6fc6149dcfdf40729d4a41f754f030fac51"
   end
 
   # Remove the #ifdef _LARGEFILE64_SOURCE macros which not only prohibits the
