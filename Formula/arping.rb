31d374a687c45f6bf755e8a9f00721380ddf4ab2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 06:58:41 +0000

    arping: update 2.22 bottle.

--- Formula/arping.rb
+++ Formula/arping.rb
@@ -6,10 +6,11 @@ class Arping < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6e050244ec556752ed5cd68f7d26f4b52e4b906483f959400d17aefb055fd36e"
-    sha256 cellar: :any, big_sur:       "64fe60c280fb3f08a7bd36366e833db62096fc6954e4f9c605285a8b6475ad67"
-    sha256 cellar: :any, catalina:      "d9820663549e09d488663a486753bb03ed0698ef6685c6a59d6eb6694f2573ef"
-    sha256 cellar: :any, mojave:        "a37a15e413cbab56d27e3a320d7c7aad1bc955b6452724e3f8e80cd4b0b3d7ba"
+    sha256 cellar: :any,                 arm64_big_sur: "6e050244ec556752ed5cd68f7d26f4b52e4b906483f959400d17aefb055fd36e"
+    sha256 cellar: :any,                 big_sur:       "64fe60c280fb3f08a7bd36366e833db62096fc6954e4f9c605285a8b6475ad67"
+    sha256 cellar: :any,                 catalina:      "d9820663549e09d488663a486753bb03ed0698ef6685c6a59d6eb6694f2573ef"
+    sha256 cellar: :any,                 mojave:        "a37a15e413cbab56d27e3a320d7c7aad1bc955b6452724e3f8e80cd4b0b3d7ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd9789660ceba232ef18cbca38e40c1f72c28c2f55fcc67410151b7a478dd6f0"
   end
 
   depends_on "autoconf" => :build
