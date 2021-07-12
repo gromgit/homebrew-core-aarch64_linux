de7ad420e06fae5657dd770b8aceaac330b22556
rui <rui@chenrui.dev>
2021-07-12 03:33:18 +0000

    gist: update 6.0.0 bottle.

--- Formula/gist.rb
+++ Formula/gist.rb
@@ -12,6 +12,7 @@ class Gist < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "1d756e91ae99001381dda127f1315af4363cb158e323d83c72f466a5ff7c3e36"
     sha256 cellar: :any_skip_relocation, mojave:        "af69b6fdaf48f811b2eb1789febe49677d74375da1e4b118b7753ff783f6ce0c"
     sha256 cellar: :any_skip_relocation, high_sierra:   "28f8947a2912459cc79536eed7cbc2af958c9282f31990741f2fd0f32fffa70a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1498973dae05c765735c2670f0af06c4c055507a249d55831c7d2b3013469e37"
   end
 
   uses_from_macos "ruby", since: :high_sierra
