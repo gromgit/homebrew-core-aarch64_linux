0642e04281649cea9f3fb715cefc4acaecfe8c59
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:42:48 +0000

    activemq-cpp: update 3.9.5_1 bottle.

--- Formula/activemq-cpp.rb
+++ Formula/activemq-cpp.rb
@@ -8,12 +8,13 @@ class ActivemqCpp < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "972d1a36b67866aa3181868044bce04ec8b70cc65e5ebf3e638d5b666c6585f5"
-    sha256 cellar: :any, big_sur:       "7fed9bcc79042fde9d0c97ac83a6fb738523772c4247c828ddc4d6b1154db8fb"
-    sha256 cellar: :any, catalina:      "c06d4253f9496b49b63c224637a97525b13ecb834884a3548adbdafe4dde0a73"
-    sha256 cellar: :any, mojave:        "024bf1c2c3ef8e612180b9f82c98f854235e8e371e01210c142304a762a30b3c"
-    sha256 cellar: :any, high_sierra:   "21855925e7e9ecfe125c959c84a6bce710ca409a2a33f4f8d396f45cc52a4ab9"
-    sha256 cellar: :any, sierra:        "c994de229e86fb7e80c846d6f2b44acba306014f334ba65550c15102214dbcb8"
+    sha256 cellar: :any,                 arm64_big_sur: "972d1a36b67866aa3181868044bce04ec8b70cc65e5ebf3e638d5b666c6585f5"
+    sha256 cellar: :any,                 big_sur:       "7fed9bcc79042fde9d0c97ac83a6fb738523772c4247c828ddc4d6b1154db8fb"
+    sha256 cellar: :any,                 catalina:      "c06d4253f9496b49b63c224637a97525b13ecb834884a3548adbdafe4dde0a73"
+    sha256 cellar: :any,                 mojave:        "024bf1c2c3ef8e612180b9f82c98f854235e8e371e01210c142304a762a30b3c"
+    sha256 cellar: :any,                 high_sierra:   "21855925e7e9ecfe125c959c84a6bce710ca409a2a33f4f8d396f45cc52a4ab9"
+    sha256 cellar: :any,                 sierra:        "c994de229e86fb7e80c846d6f2b44acba306014f334ba65550c15102214dbcb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "031771786d32a435a34662e00a2802fb282ca405401d1b017e91926e1a4ccd50"
   end
 
   depends_on "pkg-config" => :build
