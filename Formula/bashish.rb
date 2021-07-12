26248a461f983dbfc8335b5e24030b3bbe605bdb
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:53:10 +0000

    bashish: update 2.2.4 bottle.

--- Formula/bashish.rb
+++ Formula/bashish.rb
@@ -14,6 +14,7 @@ class Bashish < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "31134b56c7ad43b04ef186485af8581dbf8d8d8fcf615d259554d9c5adc7233f"
     sha256 cellar: :any_skip_relocation, el_capitan:    "114d2ce95e530c6850bc36a52a1053ecf05185d774ed499bd1725811b3c1b88c"
     sha256 cellar: :any_skip_relocation, yosemite:      "cb3bfee8b595277be04660817eb269e97744d5f49dcac431ae7473982ad5d405"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9de9a34d40236aa4fc76c38f69fa2b30b6d66bb1d6289a7552b543300abc753c"
   end
 
   depends_on "dialog"
