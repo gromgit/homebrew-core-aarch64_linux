13f5f1ed57d7fd8b29ebcff0bf19a517f08b2b2d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:40:51 +0000

    agedu: update 20200705 bottle.

--- Formula/agedu.rb
+++ Formula/agedu.rb
@@ -12,10 +12,11 @@ class Agedu < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "6b2aff0b1838cc529329fd0e178c05673b9e9879e3a8fc2910944d37ea027e0f"
-    sha256 cellar: :any_skip_relocation, catalina:    "d5b1ceb8b45632543c913d12641cb820335b0e99fd823c9404222f0e087edef4"
-    sha256 cellar: :any_skip_relocation, mojave:      "bb054128df68140a5cf8b7359a7d7c6357b13c08aa393b9481ff89124e614544"
-    sha256 cellar: :any_skip_relocation, high_sierra: "64a0584a579b71db75866548df7fef3eb7eb460023f3959aaf5e1c4d9e23bca8"
+    sha256 cellar: :any_skip_relocation, big_sur:      "6b2aff0b1838cc529329fd0e178c05673b9e9879e3a8fc2910944d37ea027e0f"
+    sha256 cellar: :any_skip_relocation, catalina:     "d5b1ceb8b45632543c913d12641cb820335b0e99fd823c9404222f0e087edef4"
+    sha256 cellar: :any_skip_relocation, mojave:       "bb054128df68140a5cf8b7359a7d7c6357b13c08aa393b9481ff89124e614544"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "64a0584a579b71db75866548df7fef3eb7eb460023f3959aaf5e1c4d9e23bca8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5079b0c9090045244900e90d0845b3eaef544fa624ba2f5658467ef229201e31"
   end
 
   depends_on "autoconf" => :build
