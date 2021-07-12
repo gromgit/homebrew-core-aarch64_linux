358b52acb95b8325866a562b951a0577961a08b4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:45:22 +0000

    apng2gif: update 1.8 bottle.

--- Formula/apng2gif.rb
+++ Formula/apng2gif.rb
@@ -6,14 +6,15 @@ class Apng2gif < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "b11d6a2f6d1eba7587c8541f65440b027596ae8b3b60ea23e080a237d0b215a3"
-    sha256 cellar: :any, big_sur:       "8c541ad0b322c10bacc60230d91daf242f3b7ebb8e5deb72860fe2dc1b8cb551"
-    sha256 cellar: :any, catalina:      "e602a9876003067007cdd579101e1fafa937e7a2ca328a0406e872d6be4f5705"
-    sha256 cellar: :any, mojave:        "f0f18d7ae3beaaac092bc06bccc3f5fdcd0c7de11df6ded61e8fde151d3e2276"
-    sha256 cellar: :any, high_sierra:   "810005bcbc32c60c7084b248eef3d007e756180842051f64385fb90cfac66c63"
-    sha256 cellar: :any, sierra:        "fa18274f18fb0d3a2b3f5c360c24587b805db3f4734972c350643c35b8677174"
-    sha256 cellar: :any, el_capitan:    "42d033ae0a661d75b588af8d7c0cdb67a81bfc481aa88665973d95d3e4fb64ec"
-    sha256 cellar: :any, yosemite:      "5456ec2b90086c84f2094972fa0dacc11de0abdde5346e3445a6b7d64b49201c"
+    sha256 cellar: :any,                 arm64_big_sur: "b11d6a2f6d1eba7587c8541f65440b027596ae8b3b60ea23e080a237d0b215a3"
+    sha256 cellar: :any,                 big_sur:       "8c541ad0b322c10bacc60230d91daf242f3b7ebb8e5deb72860fe2dc1b8cb551"
+    sha256 cellar: :any,                 catalina:      "e602a9876003067007cdd579101e1fafa937e7a2ca328a0406e872d6be4f5705"
+    sha256 cellar: :any,                 mojave:        "f0f18d7ae3beaaac092bc06bccc3f5fdcd0c7de11df6ded61e8fde151d3e2276"
+    sha256 cellar: :any,                 high_sierra:   "810005bcbc32c60c7084b248eef3d007e756180842051f64385fb90cfac66c63"
+    sha256 cellar: :any,                 sierra:        "fa18274f18fb0d3a2b3f5c360c24587b805db3f4734972c350643c35b8677174"
+    sha256 cellar: :any,                 el_capitan:    "42d033ae0a661d75b588af8d7c0cdb67a81bfc481aa88665973d95d3e4fb64ec"
+    sha256 cellar: :any,                 yosemite:      "5456ec2b90086c84f2094972fa0dacc11de0abdde5346e3445a6b7d64b49201c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f98dfacc9457ddd5742ff27448fdd3b51dbd1445a586447327404bcb0028b72a"
   end
 
   depends_on "libpng"
