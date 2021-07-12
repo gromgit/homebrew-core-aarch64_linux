a02a449f9853050a13081c5ae34d444b6f8c88a5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:58:21 +0000

    bogofilter: update 1.2.5 bottle.

--- Formula/bogofilter.rb
+++ Formula/bogofilter.rb
@@ -5,11 +5,12 @@ class Bogofilter < Formula
   sha256 "3248a1373bff552c500834adbea4b6caee04224516ae581fb25a4c6a6dee89ea"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2206ad532a38d489deb48bb9cafec00c9b98a09f621f7f208f95cc36387dafb4"
-    sha256 cellar: :any, big_sur:       "d6ad409edcabed2d32cc945c36151b3a0ae17258d9430f3192b912f1dd1050e8"
-    sha256 cellar: :any, catalina:      "2f2d4c414683f922e687d054e71619a0455560aac2522484132099fbddcc6a77"
-    sha256 cellar: :any, mojave:        "d7df5e0d29f4fcbc9eafc129ddfd993dc785ee3a4bf79b70b0dce9b5f31f7be4"
-    sha256 cellar: :any, high_sierra:   "c7998fa1651590e6aaf27f8fe014a7b0e305a48a02de4cdcb9ba53f1c84bd1e7"
+    sha256 cellar: :any,                 arm64_big_sur: "2206ad532a38d489deb48bb9cafec00c9b98a09f621f7f208f95cc36387dafb4"
+    sha256 cellar: :any,                 big_sur:       "d6ad409edcabed2d32cc945c36151b3a0ae17258d9430f3192b912f1dd1050e8"
+    sha256 cellar: :any,                 catalina:      "2f2d4c414683f922e687d054e71619a0455560aac2522484132099fbddcc6a77"
+    sha256 cellar: :any,                 mojave:        "d7df5e0d29f4fcbc9eafc129ddfd993dc785ee3a4bf79b70b0dce9b5f31f7be4"
+    sha256 cellar: :any,                 high_sierra:   "c7998fa1651590e6aaf27f8fe014a7b0e305a48a02de4cdcb9ba53f1c84bd1e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a74a36fca55ff920b663466e33ed22a127726da33b90f26b45abcc084074f33"
   end
 
   depends_on "berkeley-db"
