dd27477b7033322a920227db57520dde4d494afb
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 07:12:46 +0000

    libquvi: update 0.4.1_2 bottle.

--- Formula/libquvi.rb
+++ Formula/libquvi.rb
@@ -18,6 +18,7 @@ class Libquvi < Formula
     sha256 high_sierra:   "bb5a4201afd814e87ee496b8cefbcf126f0245d7b3c600039e71e7b355115bf7"
     sha256 sierra:        "9968d412860717f837082f0e9d225b741d8430a99a3d1c4e12b7a1cdc95cd456"
     sha256 el_capitan:    "d91506a098fa564598b4aecbad97a2fa30728fafd8ad82bf8c4ff4bedb8d6c0a"
+    sha256 x86_64_linux:  "86842f87a749f377843293787a9ce31911d10715e08e783cebe404d8ecd64e21"
   end
 
   depends_on "pkg-config" => :build
