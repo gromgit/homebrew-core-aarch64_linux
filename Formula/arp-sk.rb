4e6712374ed6c2ba0bead61ce6e7c71c1259ef89
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:48:14 +0000

    arp-sk: update 0.0.16_1 bottle.

--- Formula/arp-sk.rb
+++ Formula/arp-sk.rb
@@ -7,11 +7,12 @@ class ArpSk < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e9a3123cc035debcdac3582b5aa868cf8ab2f64d10c2ddac6e41df4df0121d52"
-    sha256 cellar: :any, big_sur:       "206b69b4456fabe2614dbf5c5ab2886530d2b238f18adb28545a9758fc9a4561"
-    sha256 cellar: :any, catalina:      "bc28c6d58a3838fac59ab625ab26a917b3b0282ac54a8f37a95034efd0740007"
-    sha256 cellar: :any, mojave:        "cbe02395698a24f9f835b7cba4128a308a15beefda6ad7e79cfd38d73823cdc2"
-    sha256 cellar: :any, high_sierra:   "67666cd80446c78b49deac3b8f2589ccbd140f32b739b662556a6dc7bda7b453"
+    sha256 cellar: :any,                 arm64_big_sur: "e9a3123cc035debcdac3582b5aa868cf8ab2f64d10c2ddac6e41df4df0121d52"
+    sha256 cellar: :any,                 big_sur:       "206b69b4456fabe2614dbf5c5ab2886530d2b238f18adb28545a9758fc9a4561"
+    sha256 cellar: :any,                 catalina:      "bc28c6d58a3838fac59ab625ab26a917b3b0282ac54a8f37a95034efd0740007"
+    sha256 cellar: :any,                 mojave:        "cbe02395698a24f9f835b7cba4128a308a15beefda6ad7e79cfd38d73823cdc2"
+    sha256 cellar: :any,                 high_sierra:   "67666cd80446c78b49deac3b8f2589ccbd140f32b739b662556a6dc7bda7b453"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a6745104a8b8035108f38a3f35ae90527790a02641cca54c29e99c962c74f16"
   end
 
   depends_on "libnet"
