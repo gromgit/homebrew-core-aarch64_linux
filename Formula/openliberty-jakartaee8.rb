6e620d95d9420bce184e9a3592d6d867014f1f0a
MoonFruit <dkmoonfruit@gmail.com>
2021-07-12 14:36:49 +0800

    openliberty-jakartaee8 21.0.0.7 (new formula)
    * openliberty-jakartaee8 21.0.0.7 (new formula)
    * Apply suggestions from code review
    
    Closes #81068.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/openliberty-jakartaee8.rb
@@ -0,0 +1,44 @@
+class OpenlibertyJakartaee8 < Formula
+  desc "Lightweight open framework for Java (Jakarta EE 8)"
+  homepage "https://openliberty.io"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-06-29_1900/openliberty-javaee8-21.0.0.7.zip"
+  sha256 "fe8db0f5ef34aebc81b0af8e3e511672b02c9efe51f0a40c3675b28752d280be"
+  license "EPL-1.0"
+
+  livecheck do
+    url "https://openliberty.io/api/builds/data"
+    regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm_rf Dir["bin/**/*.bat"]
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    (bin/"openliberty-jakartaee8").write_env_script "#{libexec}/bin/server",
+                                                    Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Open Liberty Jakarta EE 8 is:
+        #{opt_libexec}
+    EOS
+  end
+
+  test do
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"openliberty-jakartaee8", "start"
+      assert_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    ensure
+      system bin/"openliberty-jakartaee8", "stop"
+    end
+
+    refute_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    assert_match "<feature>javaee-8.0</feature>", (testpath/"servers/defaultServer/server.xml").read
+  end
+end
