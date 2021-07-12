caaf1677eba8c325dd762f014c4f74d740780b5f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:52:55 +0000

    bandit: update 1.7.0 bottle.

--- Formula/bandit.rb
+++ Formula/bandit.rb
@@ -13,6 +13,7 @@ class Bandit < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5932b6d2df12cb8f1143cca2a81a7c34233e1cf55d3a609398454f234be617b2"
     sha256 cellar: :any_skip_relocation, catalina:      "1b4c66854a3bad10e011d81c6568a9ac5c7194275b7e62e28003af7fd2fadd09"
     sha256 cellar: :any_skip_relocation, mojave:        "abe9b77aca9be261aeb1c6f3c9539edbcb0c67509a36d45e7f0e565c796374bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5deeb1aa1fb844d4c8c8a1344cf0905f3eecfd4b7d1db24a8c37814cad685846"
   end
 
   depends_on "python@3.9"
