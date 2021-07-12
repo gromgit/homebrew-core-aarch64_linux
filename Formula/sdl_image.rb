ba060f28f75fb7fbba3062721e1f7e54a319c7a5
Sam Ford <1584702+samford@users.noreply.github.com>
2021-07-12 10:32:36 -0400

    sdl_image: deprecate

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -6,11 +6,6 @@ class SdlImage < Formula
   license "Zlib"
   revision 7
 
-  livecheck do
-    url "https://www.libsdl.org/projects/SDL_image/release/"
-    regex(/href=.*?SDL_image[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "9720413694ba49519d1d1c5213607dbdf177939ae0ee081c03ab2c1d478e2fe3"
     sha256 cellar: :any,                 big_sur:       "67495888095b02d6716cc51f5a522f2a872c29de418f19210ecd586d23684b81"
@@ -22,6 +17,9 @@ class SdlImage < Formula
     sha256 cellar: :any,                 yosemite:      "3403edd53a6776bad8dc4390ef8204479f3af7c485e8a7a1f81f86f43b4a7b5c"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d8a3d6067fce20c398a6cfbe5ba87136f9d5968569a613b8a29e3bc3eef4817"
   end
+
+  # SDL 1.2 is deprecated, unsupported, and not recommended for new projects.
+  deprecate! date: "2013-08-17", because: :deprecated_upstream
 
   depends_on "pkg-config" => :build
   depends_on "jpeg"
