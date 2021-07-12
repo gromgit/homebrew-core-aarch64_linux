ea228d97748d39de46680fdaedcb5b39fd647f53
MoonFruit <dkmoonfruit@gmail.com>
2021-07-12 14:42:19 +0800

    openliberty-microprofile4 21.0.0.7 (new formula)
    * openliberty-microprofile4 21.0.0.7 (new formula)
    * Apply suggestions from code review
    
    Closes #81125.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/openliberty-microprofile4.rb
@@ -0,0 +1,44 @@
+class OpenlibertyMicroprofile4 < Formula
+  desc "Lightweight open framework for Java (Micro Profile 4)"
+  homepage "https://openliberty.io"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-06-29_1900/openliberty-microProfile4-21.0.0.7.zip"
+  sha256 "aeb5b837b731dbbf7c04fd60bd1f580a76b05a32ba9bc54a712d89eb342e2146"
+  license "EPL-1.0"
+
+  livecheck do
+    url "https://openliberty.io/api/builds/data"
+    regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm_rf Dir["bin/**/*.bat"]
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    (bin/"openliberty-microprofile4").write_env_script "#{libexec}/bin/server",
+                                                       Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Open Liberty Micro Profile 4 is:
+        #{opt_libexec}
+    EOS
+  end
+
+  test do
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"openliberty-microprofile4", "start"
+      assert_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    ensure
+      system bin/"openliberty-microprofile4", "stop"
+    end
+
+    refute_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    assert_match "<feature>microProfile-4.0</feature>", (testpath/"servers/defaultServer/server.xml").read
+  end
+end
