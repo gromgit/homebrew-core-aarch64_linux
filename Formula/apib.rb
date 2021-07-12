70c3105edb159099b8cb13eef9db714452fd92da
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-12 18:46:33 +0000

    apib: update 1.2.1 bottle.

--- Formula/apib.rb
+++ Formula/apib.rb
@@ -7,11 +7,12 @@ class Apib < Formula
   head "https://github.com/apigee/apib.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6c066e87db949d59e554c6c9b9e7657d249171dee7a2d3b7bc53b54f36daa762"
-    sha256 cellar: :any, big_sur:       "24b8a6e851fe6e262765131c025e8dddada1c740ed7f3aa978d4fbc3bec939c1"
-    sha256 cellar: :any, catalina:      "b925a48cd3a9047184b5373e4718e4142117a487467cea95be9f43f6c9951712"
-    sha256 cellar: :any, mojave:        "fd1f74b58c7a51240d463c8ec1203bce33677409aca81ba0ffba9ea718471433"
-    sha256 cellar: :any, high_sierra:   "aee416aff5715b96b81cfeda0be036a2510ffe760187e396c143a64bae2c25c2"
+    sha256 cellar: :any,                 arm64_big_sur: "6c066e87db949d59e554c6c9b9e7657d249171dee7a2d3b7bc53b54f36daa762"
+    sha256 cellar: :any,                 big_sur:       "24b8a6e851fe6e262765131c025e8dddada1c740ed7f3aa978d4fbc3bec939c1"
+    sha256 cellar: :any,                 catalina:      "b925a48cd3a9047184b5373e4718e4142117a487467cea95be9f43f6c9951712"
+    sha256 cellar: :any,                 mojave:        "fd1f74b58c7a51240d463c8ec1203bce33677409aca81ba0ffba9ea718471433"
+    sha256 cellar: :any,                 high_sierra:   "aee416aff5715b96b81cfeda0be036a2510ffe760187e396c143a64bae2c25c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5ae88648f2f468755ca4d54017d1c5d79481c5ab315e48f3d1eb3382933402c"
   end
 
   depends_on "cmake" => :build
