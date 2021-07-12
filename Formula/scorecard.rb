7fc0ddf4b8d37d7e3f248e9338ef152042619cee
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-07-12 07:49:31 +0000

    scorecard: add 2.0.0 bottle.

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -6,6 +6,13 @@ class Scorecard < Formula
   license "Apache-2.0"
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2be95fb6123f3030597290eca51adfef9aac4c9fe9475c8762334fc2126ead1e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fdeae02a92cb740341d4b9e28fe037c80f66b66b48d37c40ef2c261606ac7e31"
+    sha256 cellar: :any_skip_relocation, catalina:      "bdaf2b3695feb573360dc50193f48b7e8399e2e89eb145a2312a731de1b953d4"
+    sha256 cellar: :any_skip_relocation, mojave:        "80c11460a03d2d35b6a87e6e684908cd76cb446cce5268c2b0b5d77a84f499fe"
+  end
+
   depends_on "go" => :build
 
   def install
