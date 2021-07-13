8b7097a43c5bf955bf506ecee6e13661844d3395
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:50:14 +0000

    cppad: update 20210000.6 bottle.

--- Formula/cppad.rb
+++ Formula/cppad.rb
@@ -14,10 +14,11 @@ class Cppad < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8a7a1f1f68d98a89d466c02096464d8092be04507be2382b34a568733c97dd78"
-    sha256 cellar: :any, big_sur:       "557acdd155bb802f9e9c2a4eec1dd3e269874f8d9cd371127608c0f6b7e6ec6b"
-    sha256 cellar: :any, catalina:      "82e4a9a298f22f5ab0499df641591ddae39cb710dfe6e78ee8a2f52d5dd7f700"
-    sha256 cellar: :any, mojave:        "7a413468750c211a29ccc4d48b911aa36d620ea91aa1be1ec155d520375f0445"
+    sha256 cellar: :any,                 arm64_big_sur: "8a7a1f1f68d98a89d466c02096464d8092be04507be2382b34a568733c97dd78"
+    sha256 cellar: :any,                 big_sur:       "557acdd155bb802f9e9c2a4eec1dd3e269874f8d9cd371127608c0f6b7e6ec6b"
+    sha256 cellar: :any,                 catalina:      "82e4a9a298f22f5ab0499df641591ddae39cb710dfe6e78ee8a2f52d5dd7f700"
+    sha256 cellar: :any,                 mojave:        "7a413468750c211a29ccc4d48b911aa36d620ea91aa1be1ec155d520375f0445"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6548f67760ee32ee5687c8039af8053b5a4d3c1e45dbc641c02342a8c289f65"
   end
 
   depends_on "cmake" => :build
