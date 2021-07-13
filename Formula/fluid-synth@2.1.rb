0cd022823c762e6ed511c54405c60aa9657c92b4
Nanda H Krishna <me@nandahkrishna.com>
2021-07-14 00:40:28 +0530

    fluid-synth@2.1 2.1.9 (new formula)

---
+++ Formula/fluid-synth@2.1.rb
@@ -0,0 +1,45 @@
+class FluidSynthAT21 < Formula
+  desc "Real-time software synthesizer based on the SoundFont 2 specs"
+  homepage "https://www.fluidsynth.org"
+  url "https://github.com/FluidSynth/fluidsynth/archive/v2.1.9.tar.gz"
+  sha256 "365642cc64bafe0491149ad643ef7327877f99412d5abb93f1fa54e252028484"
+  license "LGPL-2.1-or-later"
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libsndfile"
+  depends_on "portaudio"
+
+  resource "example_midi" do
+    url "https://upload.wikimedia.org/wikipedia/commons/6/61/Drum_sample.mid"
+    sha256 "a1259360c48adc81f2c5b822f221044595632bd1a76302db1f9d983c44f45a30"
+  end
+
+  def install
+    args = std_cmake_args + %w[
+      -Denable-framework=OFF
+      -Denable-portaudio=ON
+      -DLIB_SUFFIX=
+      -Denable-dbus=OFF
+      -Denable-sdl2=OFF
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    pkgshare.install "sf2"
+  end
+
+  test do
+    # Synthesize wav file from example midi
+    resource("example_midi").stage testpath
+    wavout = testpath/"Drum_sample.wav"
+    system bin/"fluidsynth", "-F", wavout, pkgshare/"sf2/VintageDreamsWaves-v2.sf2", testpath/"Drum_sample.mid"
+    assert_predicate wavout, :exist?
+  end
+end
