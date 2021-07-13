0f1ef5ec76d7bac05f21a0677626119983d5b6b5
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-13 17:06:24 +0000

    uni: update 2.2.1 bottle.

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -10,6 +10,7 @@ class Uni < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5c17f77e18edbc5a7b25bb18978ed2615f2072e5c194b9eadc723979c6219ee9"
     sha256 cellar: :any_skip_relocation, catalina:      "c839b0b932d6440cf5877e532276a7276b0b5c4921e3429a15d8cf6e059022d7"
     sha256 cellar: :any_skip_relocation, mojave:        "31ff8bd67b8413298a8eb08a83ec35e9d17930fefbdbca090911970e64c065c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ddff8f6996cdd636995a6492d42a6c6e6181a39337e8d1dd2c4044ab88d317dc"
   end
 
   depends_on "go" => :build
