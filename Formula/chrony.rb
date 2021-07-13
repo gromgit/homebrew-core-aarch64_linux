b7748b6a71b418d6b23503c170b5a84122e5bf66
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:43:52 +0000

    chrony: update 4.1 bottle.

--- Formula/chrony.rb
+++ Formula/chrony.rb
@@ -15,6 +15,7 @@ class Chrony < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "ecba024c1c74b0f2d8094b06043a6ceafbc4bcd4cba944e3600792789adcfb4b"
     sha256 cellar: :any_skip_relocation, catalina:      "dfe728a9f5ecc085ba6582a2791e1e6a2f5d2ef609ee4f3da237e4442d016dbe"
     sha256 cellar: :any_skip_relocation, mojave:        "b908fae19168cb50d7d1fe33e2885ad19ff8bb1e190442aa3ee6ea6c47f4c72f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d7f08010b437e23b4debb9f0d278adf3eb84972a6810f2cb6daa0b81077084cc"
   end
 
   depends_on "nettle"
