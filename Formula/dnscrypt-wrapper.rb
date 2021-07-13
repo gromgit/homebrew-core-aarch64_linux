4510f2369de842aeb41dd0e65cb0e1dc69c5208d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 02:01:59 +0000

    dnscrypt-wrapper: update 0.4.2_1 bottle.

--- Formula/dnscrypt-wrapper.rb
+++ Formula/dnscrypt-wrapper.rb
@@ -8,11 +8,12 @@ class DnscryptWrapper < Formula
   head "https://github.com/Cofyc/dnscrypt-wrapper.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "6a4ce514c0ec1c41cf197b6201cba95ded39f0e7018755ec1bf42824aa8675d6"
-    sha256 cellar: :any, catalina:    "ea344feb10cc87d14e8b30cd3fcbbabed2b55f5c76014fcdc18aff49cb9fc5cd"
-    sha256 cellar: :any, mojave:      "129be9e2c08af0351401437f09a950f0d4050e99cce47da220561a3153d5334d"
-    sha256 cellar: :any, high_sierra: "226bbce3fbcc39a1619bfd77451c6e0cf0d0054b61696acc2617e4f30580e69b"
-    sha256 cellar: :any, sierra:      "92da097e90b1cd593efb8d0d1a16c24dd016aa93933a3036be671b5596c6af0d"
+    sha256 cellar: :any,                 big_sur:      "6a4ce514c0ec1c41cf197b6201cba95ded39f0e7018755ec1bf42824aa8675d6"
+    sha256 cellar: :any,                 catalina:     "ea344feb10cc87d14e8b30cd3fcbbabed2b55f5c76014fcdc18aff49cb9fc5cd"
+    sha256 cellar: :any,                 mojave:       "129be9e2c08af0351401437f09a950f0d4050e99cce47da220561a3153d5334d"
+    sha256 cellar: :any,                 high_sierra:  "226bbce3fbcc39a1619bfd77451c6e0cf0d0054b61696acc2617e4f30580e69b"
+    sha256 cellar: :any,                 sierra:       "92da097e90b1cd593efb8d0d1a16c24dd016aa93933a3036be671b5596c6af0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9100b272b791ea13c1909e604aa3adf0fab649c86bdb5775e870462c276bb7ac"
   end
 
   depends_on "autoconf" => :build
