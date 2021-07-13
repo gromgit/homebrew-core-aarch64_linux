6552b27c16e898ef4d1a6aec552572d1bf7f43f0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:41:38 +0000

    chordii: update 4.5.3b bottle.

--- Formula/chordii.rb
+++ Formula/chordii.rb
@@ -16,6 +16,7 @@ class Chordii < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "ed1f635a737973af4b9f4f784757cdf0ddbb3f946cb285917c171392a9b59d4a"
     sha256 cellar: :any_skip_relocation, mojave:        "def6b665fba55dfb8fa30269966e059b0a827f62a2338f73ea89c47a42fa7de7"
     sha256 cellar: :any_skip_relocation, high_sierra:   "1901080a06bb4728ec9858e4e548f68e044534b9d65dee1996f0590b56abc1a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8dc33f2a8fd3dda76dc87a7a919c7e590a4853a0736bc92c20d25e0d8382db6d"
   end
 
   def install
