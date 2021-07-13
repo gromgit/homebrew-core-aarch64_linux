540be7e6a8e8f35216ad6960b3a505594ae7d0ff
Michka Popoff <michkapopoff@gmail.com>
2021-07-13 02:36:36 +0200

    otf2bd: add alternate test for Linux (#81099)

--- Formula/otf2bdf.rb
+++ Formula/otf2bdf.rb
@@ -15,6 +15,13 @@ class Otf2bdf < Formula
 
   depends_on "freetype"
 
+  on_linux do
+    resource "test-font" do
+      url "https://raw.githubusercontent.com/paddykontschak/finder/master/fonts/LucidaGrande.ttc"
+      sha256 "e188b3f32f5b2d15dbf01e9b4480fed899605e287516d7c0de6809d8e7368934"
+    end
+  end
+
   resource "mkinstalldirs" do
     url "http://sofia.nmsu.edu/~mleisher/Software/otf2bdf/mkinstalldirs"
     sha256 "e7b13759bd5caac0976facbd1672312fe624dd172bbfd989ffcc5918ab21bfc1"
@@ -28,6 +35,13 @@ class Otf2bdf < Formula
   end
 
   test do
-    assert_match "MacRoman", shell_output("#{bin}/otf2bdf -et /System/Library/Fonts/LucidaGrande.ttc")
+    on_macos do
+      assert_match "MacRoman", shell_output("#{bin}/otf2bdf -et /System/Library/Fonts/LucidaGrande.ttc")
+    end
+    on_linux do
+      resource("test-font").stage do
+        assert_match "MacRoman", shell_output("#{bin}/otf2bdf -et LucidaGrande.ttc")
+      end
+    end
   end
 end
