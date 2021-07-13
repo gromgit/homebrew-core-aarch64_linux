c9c25c16a61fa2e33f1b652361845bf8f50346ad
Michael Cho <cho-m@tuta.io>
2021-07-12 21:40:36 -0700

    mscgen: update to https urls and add license (#81131)

--- Formula/mscgen.rb
+++ Formula/mscgen.rb
@@ -1,8 +1,9 @@
 class Mscgen < Formula
   desc "Parses Message Sequence Chart descriptions and produces images"
-  homepage "http://www.mcternan.me.uk/mscgen/"
-  url "http://www.mcternan.me.uk/mscgen/software/mscgen-src-0.20.tar.gz"
+  homepage "https://www.mcternan.me.uk/mscgen/"
+  url "https://www.mcternan.me.uk/mscgen/software/mscgen-src-0.20.tar.gz"
   sha256 "3c3481ae0599e1c2d30b7ed54ab45249127533ab2f20e768a0ae58d8551ddc23"
+  license "GPL-2.0-or-later"
   revision 3
 
   livecheck do
