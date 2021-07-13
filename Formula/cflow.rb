5ff0f5b263380b2d3007066fe4698c3174ffa54c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:37:54 +0000

    cflow: update 1.6 bottle.

--- Formula/cflow.rb
+++ Formula/cflow.rb
@@ -13,6 +13,7 @@ class Cflow < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "a96f9cf3cb35851c27ed602f6a05489da2d64e75ab6daccecc3e23156d9fe968"
     sha256 cellar: :any_skip_relocation, high_sierra:   "50a816924cb91e1c4055923285ea3ceb0d815b4641477906ae5f6abdae337d52"
     sha256 cellar: :any_skip_relocation, sierra:        "5e88f5310c34255947032f24227cf779aa8a42fe595f585605e814f001f4a151"
+    sha256                               x86_64_linux:  "06ad443c55fb5979f0986088dcf96b22b739d7c12d51d593c063bc70018de5d9"
   end
 
   def install
