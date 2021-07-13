7de88ca209065cd318e961d78f6b6a1db8b18b18
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:51:17 +0000

    crc32c: update 1.1.1 bottle.

--- Formula/crc32c.rb
+++ Formula/crc32c.rb
@@ -7,11 +7,12 @@ class Crc32c < Formula
   head "https://github.com/google/crc32c.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "82e70741895a3e29dc0f8e48b0dd10e13e8cd380012457c1bf984ace3fd6dd03"
-    sha256 cellar: :any, big_sur:       "a37c7daabc55476ee3828211b32a63f052af9feeff1430fe53be9cef2038a069"
-    sha256 cellar: :any, catalina:      "8ac4299583c3155c0410e246277214110bbbe453df5cc6b67694c67ba722bfbc"
-    sha256 cellar: :any, mojave:        "f5e232ed8a57eea6b226f4596f94281ea4ea5467c626e83a1576e74aee32711e"
-    sha256 cellar: :any, high_sierra:   "a8f21980c0fee7ffb9911b1eaa1bf7641940b4bb798a7dbd508ae60a6c1a46a8"
+    sha256 cellar: :any,                 arm64_big_sur: "82e70741895a3e29dc0f8e48b0dd10e13e8cd380012457c1bf984ace3fd6dd03"
+    sha256 cellar: :any,                 big_sur:       "a37c7daabc55476ee3828211b32a63f052af9feeff1430fe53be9cef2038a069"
+    sha256 cellar: :any,                 catalina:      "8ac4299583c3155c0410e246277214110bbbe453df5cc6b67694c67ba722bfbc"
+    sha256 cellar: :any,                 mojave:        "f5e232ed8a57eea6b226f4596f94281ea4ea5467c626e83a1576e74aee32711e"
+    sha256 cellar: :any,                 high_sierra:   "a8f21980c0fee7ffb9911b1eaa1bf7641940b4bb798a7dbd508ae60a6c1a46a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae8b8c0ec66cdb947aa6cb7a13e70f0c585ca2c8452489e05e82a2d6ebc1fe71"
   end
 
   depends_on "cmake" => :build
