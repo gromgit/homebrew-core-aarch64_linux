3e31190063bbc082b042800a5fe635fdb0b5f133
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:57:42 +0000

    detox: update 1.4.2 bottle.

--- Formula/detox.rb
+++ Formula/detox.rb
@@ -10,6 +10,7 @@ class Detox < Formula
     sha256 big_sur:       "9d468f782f96d667d59892911dc1fa7ef7b40b3f0410d46c3a75147c8a5b886b"
     sha256 catalina:      "83987429b9b768b6559420dd4656603ab6c963785e87bffd02a61e2be6557393"
     sha256 mojave:        "a371814287b76ac37a6f24a719da41d5fb7d4215a2be548b075c706b1f670165"
+    sha256 x86_64_linux:  "5f40b1f894af6814b279263b00f11e52c613b1ab833d0b764a9c5152aa5965b6"
   end
 
   depends_on "autoconf" => :build
