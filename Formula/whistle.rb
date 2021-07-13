8711efbc847466baf1ee7f071d018b548af75be0
rui <rui@chenrui.dev>
2021-07-13 03:21:46 +0000

    whistle: update 2.7.8 bottle.

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -8,7 +8,8 @@ class Whistle < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c1d8f782272048f6d7d0c2cf236a902a0e19c423c7e0752fcaa2041b1c33d976"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "24ed92d2b453af43bb0dd3f49f8429408cf1a6f700900f72eb2292212422b70c"
+    sha256 cellar: :any_skip_relocation, all:          "c1d8f782272048f6d7d0c2cf236a902a0e19c423c7e0752fcaa2041b1c33d976"
   end
 
   depends_on "node"
