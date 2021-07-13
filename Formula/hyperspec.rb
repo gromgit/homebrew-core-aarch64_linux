7902a46598bf300f889b3975db961db7359d3f82
Sam Ford <1584702+samford@users.noreply.github.com>
2021-07-13 19:02:21 -0400

    hyperspec: add livecheck

--- Formula/hyperspec.rb
+++ Formula/hyperspec.rb
@@ -5,6 +5,14 @@ class Hyperspec < Formula
   version "7.0"
   sha256 "1ac1666a9dc697dbd8881262cad4371bcd2e9843108b643e2ea93472ba85d7c3"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?HyperSpec[._-]v?(\d+(?:[.-]\d+)+)\.t/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match&.first&.gsub("-", ".") }
+    end
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f97fdd5f56b2c8d2ea223a0ce5950c225c198e3e1157d7bd8c7d691c65773404"
   end
