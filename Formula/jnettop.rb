cc110fa8cc6634207ade28b7b5df6d0d9145b07a
Michael Cho <cho-m@tuta.io>
2021-07-13 16:29:39 -0700

    jnettop: fix build for Linux (#81066)

--- Formula/jnettop.rb
+++ Formula/jnettop.rb
@@ -3,6 +3,7 @@ class Jnettop < Formula
   homepage "https://web.archive.org/web/20161127214942/jnettop.kubs.info/wiki/"
   url "https://downloads.sourceforge.net/project/jnettop/jnettop/0.13/jnettop-0.13.0.tar.gz"
   sha256 "a005d6fa775a85ff9ee91386e25505d8bdd93bc65033f1928327c98f5e099a62"
+  license "GPL-2.0-or-later"
   revision 2
 
   livecheck do
@@ -21,10 +22,18 @@ class Jnettop < Formula
   depends_on "pkg-config" => :build
   depends_on "glib"
 
+  uses_from_macos "libpcap"
+
   def install
     # Work around "-Werror,-Wimplicit-function-declaration" issues with
     # configure scripts on Xcode 12:
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    # Fix undefined reference to `g_thread_init'
+    on_linux do
+      inreplace "Makefile.in", "$(jnettop_LDFLAGS) $(jnettop_OBJECTS)",
+                               "$(jnettop_OBJECTS) $(AM_LDFLAGS) $(LDFLAGS) $(jnettop_LDFLAGS)"
+    end
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
