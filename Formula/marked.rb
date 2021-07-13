ea7a0d0901d9901303afbb0bdd95d70736dbb4ed
rui <rui@chenrui.dev>
2021-07-13 03:24:44 +0000

    marked: update 2.1.3 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -8,7 +8,8 @@ class Marked < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6cbc239d4c6ad70e200f880540830c506d2662f15bd0d925500b0e567fbda456"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e48abe8c593c80cffb44aa0246820bcdd763c6314cb6efe4c8b49511e6a78ead"
+    sha256 cellar: :any_skip_relocation, all:          "6cbc239d4c6ad70e200f880540830c506d2662f15bd0d925500b0e567fbda456"
   end
 
   depends_on "node"
