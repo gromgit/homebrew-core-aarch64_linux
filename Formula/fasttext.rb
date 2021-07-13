a298c27515d771d501414f493e6a01649ebaf115
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 20:25:47 +0000

    fasttext: update 0.9.2 bottle.

--- Formula/fasttext.rb
+++ Formula/fasttext.rb
@@ -7,11 +7,12 @@ class Fasttext < Formula
   head "https://github.com/facebookresearch/fastText.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3bfb7e1ab42dde74ac0692d2016f718e173b9e9dee093e408dda8f4c22ef1a8a"
-    sha256 cellar: :any, big_sur:       "3869650705430f8b682416be4e7c0a01c243a2f9517c6668027c6e9576f1e9c6"
-    sha256 cellar: :any, catalina:      "ec085551ced1f55b863a65aa60ad8f31d796002702b7effaaaafbf1490df867f"
-    sha256 cellar: :any, mojave:        "79f08167fb55b478829434be84d919c08c888563e0abbdeb66bc19cd3e82457f"
-    sha256 cellar: :any, high_sierra:   "4602a32c2a373ed97de8fd36bf1e998299682d45e465af39026a32a3a06fe574"
+    sha256 cellar: :any,                 arm64_big_sur: "3bfb7e1ab42dde74ac0692d2016f718e173b9e9dee093e408dda8f4c22ef1a8a"
+    sha256 cellar: :any,                 big_sur:       "3869650705430f8b682416be4e7c0a01c243a2f9517c6668027c6e9576f1e9c6"
+    sha256 cellar: :any,                 catalina:      "ec085551ced1f55b863a65aa60ad8f31d796002702b7effaaaafbf1490df867f"
+    sha256 cellar: :any,                 mojave:        "79f08167fb55b478829434be84d919c08c888563e0abbdeb66bc19cd3e82457f"
+    sha256 cellar: :any,                 high_sierra:   "4602a32c2a373ed97de8fd36bf1e998299682d45e465af39026a32a3a06fe574"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a90d77cb50372ab1172b354ee29e813f26543067beac35dd24b3da882352718"
   end
 
   depends_on "cmake" => :build
