94f5b89b270db12f04c9c3d628b07a5875aa0a32
rui <rui@chenrui.dev>
2021-07-13 03:10:18 +0000

    eleventy: update 0.12.1 bottle.

--- Formula/eleventy.rb
+++ Formula/eleventy.rb
@@ -13,6 +13,7 @@ class Eleventy < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "2b035df0dbb58ace4727c1af9a9de3140e2ba3b9fa5206d934cfd254577a5a0b"
     sha256 cellar: :any_skip_relocation, catalina:      "324dfe311d783ff2a507d1b55ec94bb3152d84ed26041450853032ad427237d7"
     sha256 cellar: :any_skip_relocation, mojave:        "8db3de4db3dd1a611585724b1f9ec38816a2573309caa0fdad88f46f67e3fbbb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "041abbc16db2ffcf6aad345b58d80234fa05e388969fff4707ef4dc623f5f9b4"
   end
 
   depends_on "node"
