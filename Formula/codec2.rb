32a4d6fd670efb4dde1810fa7b7c539e716cf2f4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:47:56 +0000

    codec2: update 0.9.2 bottle.

--- Formula/codec2.rb
+++ Formula/codec2.rb
@@ -7,10 +7,11 @@ class Codec2 < Formula
   license "LGPL-2.1"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "c28da7a7d230e8505859eafa28d9238ccadd97c4847f5ee5c269ffa24245ba49"
-    sha256 cellar: :any, catalina:    "d41040646585e5b91438818ff4dfdccc94b3a2567d31d6960710f8f2455bab04"
-    sha256 cellar: :any, mojave:      "460c2febeb64e913796ee3f161b8fa50bbc00904c99228d5122324ebadd91fe9"
-    sha256 cellar: :any, high_sierra: "5f4530a54adbc38253b993bdbb21da86b4d11725b2edcbec79e43ed739875208"
+    sha256 cellar: :any,                 big_sur:      "c28da7a7d230e8505859eafa28d9238ccadd97c4847f5ee5c269ffa24245ba49"
+    sha256 cellar: :any,                 catalina:     "d41040646585e5b91438818ff4dfdccc94b3a2567d31d6960710f8f2455bab04"
+    sha256 cellar: :any,                 mojave:       "460c2febeb64e913796ee3f161b8fa50bbc00904c99228d5122324ebadd91fe9"
+    sha256 cellar: :any,                 high_sierra:  "5f4530a54adbc38253b993bdbb21da86b4d11725b2edcbec79e43ed739875208"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "eb169319217f165aa027c09ed5591e246db1f357884bd63e26cc86ab0e029814"
   end
 
   depends_on "cmake" => :build
