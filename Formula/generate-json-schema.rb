9f0d9c058a515b826d7d62ac351aedcf5378bc9e
rui <rui@chenrui.dev>
2021-07-13 03:11:34 +0000

    generate-json-schema: update 2.6.0 bottle.

--- Formula/generate-json-schema.rb
+++ Formula/generate-json-schema.rb
@@ -16,6 +16,7 @@ class GenerateJsonSchema < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:   "3461301c038b8bb6e15b8e183661976e95ea7b7e0659d57f0f21ea2c0eb4e67c"
     sha256 cellar: :any_skip_relocation, sierra:        "a6ff075810774d44030a59a12032d302c64834d03c7aabeb32efb8dc86d276de"
     sha256 cellar: :any_skip_relocation, el_capitan:    "5a5b34d8e233d9b75648c39f8edada5077c8f6c6466bd3358f3f661062ccbe83"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12487b77022ffd791d6fbd7a50362a08f5209ba50ad1cc67c3558d63db1e9264"
   end
 
   depends_on "node"
