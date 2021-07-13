1bc8146717d009c7e230b10eac0fd13f4af2912a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 02:00:09 +0000

    disktype: update 9 bottle.

--- Formula/disktype.rb
+++ Formula/disktype.rb
@@ -20,6 +20,7 @@ class Disktype < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "18ed63d389b55d3dabb84e355323f303013acd46a1905c194b470cc74fc95e4f"
     sha256 cellar: :any_skip_relocation, el_capitan:    "c1f45dc2bdcec2e3b56741bf03d673f3a99534f851d1c77de59d6832d0f75236"
     sha256 cellar: :any_skip_relocation, yosemite:      "cc767e7be270b683021ecb2ef3dd16c77b05e9cdf34ed524c942a89514284f57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b0dcc67cc8fee509011e50ff1299b4205b424f83ee9aedff5d97fb2e603b6bc"
   end
 
   def install
