ef17ae0287fb23f72dfdb9e930c58d3a9bec8098
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 20:20:36 +0000

    enet: update 1.3.17 bottle.

--- Formula/enet.rb
+++ Formula/enet.rb
@@ -7,11 +7,12 @@ class Enet < Formula
   head "https://github.com/lsalzman/enet.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b2ef2e83fc0f527691e8352d39241277ab742569cc5278a357a53b19a42e700d"
-    sha256 cellar: :any, big_sur:       "bb861ad42df5152ac53708cdee14a599ff5e09a06cf3d438e88f7bc6b84590db"
-    sha256 cellar: :any, catalina:      "557052d4c6fb7e8c4329270730bd97b032f279c2cfafaa6ebbd32f7ff7e076bf"
-    sha256 cellar: :any, mojave:        "7df13b64c909df3368a91094abaaab1563f66ebcb276af0d318408977af08d2f"
-    sha256 cellar: :any, high_sierra:   "6fbf495f25b1df30003129b77167df08d26fbb576fa61a3f17ff7eba366bdd2a"
+    sha256 cellar: :any,                 arm64_big_sur: "b2ef2e83fc0f527691e8352d39241277ab742569cc5278a357a53b19a42e700d"
+    sha256 cellar: :any,                 big_sur:       "bb861ad42df5152ac53708cdee14a599ff5e09a06cf3d438e88f7bc6b84590db"
+    sha256 cellar: :any,                 catalina:      "557052d4c6fb7e8c4329270730bd97b032f279c2cfafaa6ebbd32f7ff7e076bf"
+    sha256 cellar: :any,                 mojave:        "7df13b64c909df3368a91094abaaab1563f66ebcb276af0d318408977af08d2f"
+    sha256 cellar: :any,                 high_sierra:   "6fbf495f25b1df30003129b77167df08d26fbb576fa61a3f17ff7eba366bdd2a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0531f7c9cb6c994d6a38b07ac6e303f81a8a4194dd7daaa6d6aec1d25155afb"
   end
 
   def install
