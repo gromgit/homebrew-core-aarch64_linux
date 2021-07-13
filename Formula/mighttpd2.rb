77953cbf0c6abfe746e611fdbf7620321bf06dea
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-13 17:20:50 +0000

    mighttpd2: update 3.4.6_1 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -12,6 +12,7 @@ class Mighttpd2 < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "bcea435a9feba47df19b64d9fac972a1df8f580647204b07a73b2ade2e14c479"
     sha256 cellar: :any_skip_relocation, mojave:        "68e563757fb405de41a4312c03f7b72da99586430ea8f0aff98fdab48213635f"
     sha256 cellar: :any_skip_relocation, high_sierra:   "7b033c6ce128310465134a09bae1ef3df9cb630db732167a06028c1a5773576e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6fedabb209c288efc21b4283c34609645a9882f18a525ca48b1e2ccfd1d70b10"
   end
 
   depends_on "cabal-install" => :build
