840d9acbb8b2bed763b0ce7697d6a449f3d7b30b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:46:24 +0000

    clair: update 4.1.1 bottle.

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -11,9 +11,10 @@ class Clair < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "06adb6658fcb4d5d936edbe9dda9a0f3f3cc138a7af5a45764442bc8bd253d30"
-    sha256 cellar: :any_skip_relocation, catalina: "64093c8898d8f042f9953e3a28616ce802b03ea66a6001dcf7ee04d08bc137fa"
-    sha256 cellar: :any_skip_relocation, mojave:   "4130ba98783424e5a0c6cec2c2de0765ebfb2a7c49a985099ef7c34012c53594"
+    sha256 cellar: :any_skip_relocation, big_sur:      "06adb6658fcb4d5d936edbe9dda9a0f3f3cc138a7af5a45764442bc8bd253d30"
+    sha256 cellar: :any_skip_relocation, catalina:     "64093c8898d8f042f9953e3a28616ce802b03ea66a6001dcf7ee04d08bc137fa"
+    sha256 cellar: :any_skip_relocation, mojave:       "4130ba98783424e5a0c6cec2c2de0765ebfb2a7c49a985099ef7c34012c53594"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7fc843061a77dd0fbc90183fdd4c79a5bdebedb30817d1faff25100d2209ad5e"
   end
 
   depends_on "go" => :build
