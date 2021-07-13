254968a0d4cbc68b39418f48a8a8ad98bff4d12b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 00:25:25 +0000

    cash-cli: update 4.2.1 bottle.

--- Formula/cash-cli.rb
+++ Formula/cash-cli.rb
@@ -13,6 +13,7 @@ class CashCli < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "f22f6404f47adb8a6c0253362d61fb529da4d6a71045a2902407ed112329310d"
     sha256 cellar: :any_skip_relocation, mojave:        "7aa6e66eef5defae364924b00859ad4d884a15563c52488462ab489676f02141"
     sha256 cellar: :any_skip_relocation, high_sierra:   "903fde1135bcc71b70d74b852084897a2708f1d87ad00c200c793600472c42aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e25e658a1049c359059b393257727a0ef80487633e0994a1451eb38e1f448955"
   end
 
   deprecate! date: "2021-04-23", because: :unmaintained
