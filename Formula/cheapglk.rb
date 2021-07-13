09ce9dd023a5ef870ac2718875753fa7880abc21
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:41:24 +0000

    cheapglk: update 1.0.6 bottle.

--- Formula/cheapglk.rb
+++ Formula/cheapglk.rb
@@ -15,6 +15,7 @@ class Cheapglk < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "d76d29db8ea0201fcef949e02cbddb1c06311dece796a263192ffef487a3aa2c"
     sha256 cellar: :any_skip_relocation, el_capitan:    "497a5399738c026d318d3213b764f20fb80ccea94181919fad2e80eb75086055"
     sha256 cellar: :any_skip_relocation, yosemite:      "8351c9dec39fd8e860e50a8e693e1c648def81c3ddcbdd3a856f656d585b0082"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af305413ea03f3b67064c61be1f26be3860370d87e774d4fc6c014ca66cdd380"
   end
 
   keg_only "it conflicts with other Glk libraries"
