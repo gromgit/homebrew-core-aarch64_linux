15d2b66e6a6ef7918dad23d483412b97313f3e93
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:56:04 +0000

    cquery: update 20180718 bottle.

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -10,10 +10,11 @@ class Cquery < Formula
 
   bottle do
     rebuild 1
-    sha256 big_sur:     "92e25635f16f15c7c34222616b2c4f33ba5605e16c010fb3ee9be24a7c8adab9"
-    sha256 catalina:    "fc7c73b7d9132c879399fc65a3556d9382d9617dd5e59bc10a51eaed228be5f8"
-    sha256 mojave:      "89a896183bd8e6635146263dc09c0f26464845f91637e5ca9557558aaa034139"
-    sha256 high_sierra: "555804325cb45d5450c0d8b47096b71b8445af5370a9fc1e71acb1a6e86d3398"
+    sha256 big_sur:      "92e25635f16f15c7c34222616b2c4f33ba5605e16c010fb3ee9be24a7c8adab9"
+    sha256 catalina:     "fc7c73b7d9132c879399fc65a3556d9382d9617dd5e59bc10a51eaed228be5f8"
+    sha256 mojave:       "89a896183bd8e6635146263dc09c0f26464845f91637e5ca9557558aaa034139"
+    sha256 high_sierra:  "555804325cb45d5450c0d8b47096b71b8445af5370a9fc1e71acb1a6e86d3398"
+    sha256 x86_64_linux: "96e88524a7f8a2e63b5e8bc825e3ec59a052a0577a6b4b4b91943cf16ec160ae"
   end
 
   deprecate! date: "2020-11-15", because: :repo_archived
