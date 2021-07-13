3e05d5922ccbac9fd4565ef135846337e6093c1d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:58:16 +0000

    dfmt: update 0.13.4 bottle.

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -8,9 +8,10 @@ class Dfmt < Formula
   head "https://github.com/dlang-community/dfmt.git", branch: "v0.x.x"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "00eceeab6157c27c75002f5239183b3902071c0aefe0b8892831115897edf197"
-    sha256 cellar: :any_skip_relocation, catalina: "a545590d0222235eb45c81f82d62b66a26aeb2156217d2c010bbb6b90201fd26"
-    sha256 cellar: :any_skip_relocation, mojave:   "27d62a6329e0c0b3d3e18d05600ad4fecf6dcd1cdd07eb150e292df593c7d92c"
+    sha256 cellar: :any_skip_relocation, big_sur:      "00eceeab6157c27c75002f5239183b3902071c0aefe0b8892831115897edf197"
+    sha256 cellar: :any_skip_relocation, catalina:     "a545590d0222235eb45c81f82d62b66a26aeb2156217d2c010bbb6b90201fd26"
+    sha256 cellar: :any_skip_relocation, mojave:       "27d62a6329e0c0b3d3e18d05600ad4fecf6dcd1cdd07eb150e292df593c7d92c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "74d9f0c77732a63098658c8853dc2340cabcdca1ac7382d104f58474c21f7771"
   end
 
   depends_on "dmd" => :build
