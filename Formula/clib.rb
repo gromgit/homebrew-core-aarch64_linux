461d45c23b19e8ee0ddd12f57318047443690875
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 01:48:56 +0000

    clib: update 2.7.0 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -16,6 +16,7 @@ class Clib < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "ba2d9b6112f7a183a28e06b00bdf01c3164dbdf05b6284b5a5d8c972c4a5ef59"
     sha256 cellar: :any_skip_relocation, catalina:      "19a1b473f5c392ce7a1f8ae8209918926777f32395a31199047af6cd22ff01a5"
     sha256 cellar: :any_skip_relocation, mojave:        "30f21023c47efeda8c7609f7ad09de5c866e903ab85d90f11227424836fbf248"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13bb925a61aec0575c5112f596752c71b48b42f43b37fd3da4ba6eb69f56ebc8"
   end
 
   uses_from_macos "curl"
