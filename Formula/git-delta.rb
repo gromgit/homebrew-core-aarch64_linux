3c1e1e79776598a5dc426e2cc0e58ac37f11fd7f
Dan Davison <dandavison7@gmail.com>
2021-07-13 10:46:36 +0000

    git-delta 0.8.3
    Closes #81150.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -1,8 +1,8 @@
 class GitDelta < Formula
   desc "Syntax-highlighting pager for git and diff output"
   homepage "https://github.com/dandavison/delta"
-  url "https://github.com/dandavison/delta/archive/0.8.2.tar.gz"
-  sha256 "b29db829a22f77538ce2e291e4c1b8f9aa7e714dbe5200c6353670a888b746d3"
+  url "https://github.com/dandavison/delta/archive/0.8.3.tar.gz"
+  sha256 "cf48d52d20a12e11a3a6afd436a75550e78fc39c358e85a75caa08b39e4e75c6"
   license "MIT"
   head "https://github.com/dandavison/delta.git"
 
