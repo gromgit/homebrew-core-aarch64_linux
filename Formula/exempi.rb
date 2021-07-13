68522a27479ee46b2e7d472742f9e3a07a67c398
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 20:24:09 +0000

    exempi: update 2.5.2 bottle.

--- Formula/exempi.rb
+++ Formula/exempi.rb
@@ -10,11 +10,12 @@ class Exempi < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9993c63e20e0f25bec9217f10830945e93554caeb9ed96530b31386205f3b963"
-    sha256 cellar: :any, big_sur:       "3dca3e311a819ad927266feecc2a0fa06a6baf196290655b5531ec02ea97dddd"
-    sha256 cellar: :any, catalina:      "3ef58fd5cbd177ac785cfab9b58f813ce24320a507243d9d7b6c940fd463564f"
-    sha256 cellar: :any, mojave:        "189bb3c57e78845c33678353cb877ad7cdedd665087c0a4525397f32088abc39"
-    sha256 cellar: :any, high_sierra:   "0843f9bc589fd3c9ed0f5dfd724ba60eea4832410a0b6ff831bdb22c6563eafd"
+    sha256 cellar: :any,                 arm64_big_sur: "9993c63e20e0f25bec9217f10830945e93554caeb9ed96530b31386205f3b963"
+    sha256 cellar: :any,                 big_sur:       "3dca3e311a819ad927266feecc2a0fa06a6baf196290655b5531ec02ea97dddd"
+    sha256 cellar: :any,                 catalina:      "3ef58fd5cbd177ac785cfab9b58f813ce24320a507243d9d7b6c940fd463564f"
+    sha256 cellar: :any,                 mojave:        "189bb3c57e78845c33678353cb877ad7cdedd665087c0a4525397f32088abc39"
+    sha256 cellar: :any,                 high_sierra:   "0843f9bc589fd3c9ed0f5dfd724ba60eea4832410a0b6ff831bdb22c6563eafd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7dad218c3c0e3e3df9efc69e1a84cafd8a992c31157e610f0a382403599a439"
   end
 
   depends_on "boost"
