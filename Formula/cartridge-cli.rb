64601e1cc9a1f9356e14a3ebb1eabe1de7c7a30c
rui <rui@chenrui.dev>
2021-07-13 05:03:16 +0000

    cartridge-cli: update 2.9.1 bottle.

--- Formula/cartridge-cli.rb
+++ Formula/cartridge-cli.rb
@@ -11,6 +11,7 @@ class CartridgeCli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "71cae5381ab4d0ab1a09d1d523cab9c39e48f786fbc65bdc1aed1fe641fd4eba"
     sha256 cellar: :any_skip_relocation, catalina:      "4e3f9ffa29ba823041fe460d116afeb9a37c7453bca4f658c9a88d0baff3fbf9"
     sha256 cellar: :any_skip_relocation, mojave:        "19eaa3369041b180f581a6b27d2816dde06eae5b214410e882fd9add29061a81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aed3b52eb3b2653ba45c1ac5b559474ecd60ee6576c0572520bc3e3bd4c4d948"
   end
 
   depends_on "go" => :build
