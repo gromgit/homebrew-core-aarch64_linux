72888790b9e8b79a724246f7f6c4fe54b4bc0ae1
Michael Cho <cho-m@tuta.io>
2021-07-13 00:06:15 -0700

    lc0: fix build for Linux (#81135)

--- Formula/lc0.rb
+++ Formula/lc0.rb
@@ -20,13 +20,34 @@ class Lc0 < Formula
   depends_on "python@3.9" => :build # required to compile .pb files
   depends_on "eigen"
 
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "gcc" # for C++17
+    depends_on "openblas"
+  end
+
+  fails_with gcc: "5"
+
   resource "network" do
     url "https://training.lczero.org/get_network?sha=00af53b081e80147172e6f281c01daf5ca19ada173321438914c730370aa4267", using: :nounzip
     sha256 "12df03a12919e6392f3efbe6f461fc0ff5451b4105f755503da151adc7ab6d67"
   end
 
+  # Fix compile error due to missing #include <condition_variable>
+  # Remove in the next release
+  patch do
+    url "https://github.com/LeelaChessZero/lc0/commit/997257a16fc74c848ab55e475c4d233d78838506.patch?full_index=1"
+    sha256 "5f4fb6b730a04bfd36567302bc4ad9d22f7f69c55c6fad35047a18d514210758"
+  end
+
   def install
-    system "meson", *std_meson_args, "-Dgtest=false", "build/release"
+    args = ["-Dgtest=false"]
+    on_linux do
+      args << "-Dopenblas_include=#{Formula["openblas"].opt_include}"
+      args << "-Dopenblas_libdirs=#{Formula["openblas"].opt_lib}"
+    end
+    system "meson", *std_meson_args, *args, "build/release"
 
     cd "build/release" do
       system "ninja", "-v"
@@ -34,11 +55,13 @@ class Lc0 < Formula
     end
 
     bin.write_exec_script libexec/"lc0"
-    libexec.install resource("network")
+    resource("network").stage { libexec.install Dir["*"].first => "42850.pb.gz" }
   end
 
   test do
     assert_match "Creating backend [blas]",
       shell_output("lc0 benchmark --backend=blas --nodes=1 --num-positions=1 2>&1")
+    assert_match "Creating backend [eigen]",
+      shell_output("lc0 benchmark --backend=eigen --nodes=1 --num-positions=1 2>&1")
   end
 end
