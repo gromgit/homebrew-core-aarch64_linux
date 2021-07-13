46c4270ad18ac06a9b89506a40389cd7055110b1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-13 00:22:22 +0000

    calceph: update 3.4.7 bottle.

--- Formula/calceph.rb
+++ Formula/calceph.rb
@@ -6,11 +6,12 @@ class Calceph < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0466e27596cec26f2fcce66fe699f4366e8e2e3c020c9c69a86e3dfe5982c81c"
-    sha256 cellar: :any, big_sur:       "f5aa0d2c90d1bf0fdd1b01e7d540a243552054274634cc429c0364fdcbafb44a"
-    sha256 cellar: :any, catalina:      "2ce6fa8b2b26b317a0bb5896a7bbb8a492d85788f2f7eda43cac8cc2dde7a3a4"
-    sha256 cellar: :any, mojave:        "6fe017217defc0a4746d5881f8ced6f2ae0af262f0fa6aef33c94721f45b1742"
-    sha256 cellar: :any, high_sierra:   "8b2a7955298fe618abe12328de07a6c991a47d15805b27f11f2df2f0bf88de0d"
+    sha256 cellar: :any,                 arm64_big_sur: "0466e27596cec26f2fcce66fe699f4366e8e2e3c020c9c69a86e3dfe5982c81c"
+    sha256 cellar: :any,                 big_sur:       "f5aa0d2c90d1bf0fdd1b01e7d540a243552054274634cc429c0364fdcbafb44a"
+    sha256 cellar: :any,                 catalina:      "2ce6fa8b2b26b317a0bb5896a7bbb8a492d85788f2f7eda43cac8cc2dde7a3a4"
+    sha256 cellar: :any,                 mojave:        "6fe017217defc0a4746d5881f8ced6f2ae0af262f0fa6aef33c94721f45b1742"
+    sha256 cellar: :any,                 high_sierra:   "8b2a7955298fe618abe12328de07a6c991a47d15805b27f11f2df2f0bf88de0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3fa740b18cb787d373365b0540f47e8260a000b7b6f9281cb13f82d847d7b97f"
   end
 
   depends_on "gcc" # for gfortran
