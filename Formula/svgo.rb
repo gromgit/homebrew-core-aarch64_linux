08fbf181589656489af746e4b85f844450fb5f07
rui <rui@chenrui.dev>
2021-07-13 03:18:02 +0000

    svgo: update 2.3.1 bottle.

--- Formula/svgo.rb
+++ Formula/svgo.rb
@@ -8,7 +8,8 @@ class Svgo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "251ed49ea2fd4f7823046087fc6181413816013b92a05977e5b2c992ea126353"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c5615e0ef5de74d724dd8ed4e7161513919d80bc8abb0347490aff347a0f8e95"
+    sha256 cellar: :any_skip_relocation, all:          "251ed49ea2fd4f7823046087fc6181413816013b92a05977e5b2c992ea126353"
   end
 
   depends_on "node"
