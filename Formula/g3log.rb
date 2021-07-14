4841de539c23a4bc1f5612e835e001ca56f33435
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 01:45:42 +0000

    g3log: update 1.3.4 bottle.

--- Formula/g3log.rb
+++ Formula/g3log.rb
@@ -6,10 +6,11 @@ class G3log < Formula
   license "Unlicense"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d6ab9b85de4f0bc70d278210ac4a89c2780b4a271dc474fdd2a4ac16933a3d38"
-    sha256 cellar: :any, big_sur:       "5e24eda970bf16a1d737e0112ef7e86651c6cdd29b14b6dd4beec2faf9f9d292"
-    sha256 cellar: :any, catalina:      "3325a5a22c63c02f6c3a7d9b35f533e579f369ff2871f7152d0ca4994bb049d3"
-    sha256 cellar: :any, mojave:        "f44e98ef652573827da51288539acb1122af634b79f61f8ec2687b7b5184e971"
+    sha256 cellar: :any,                 arm64_big_sur: "d6ab9b85de4f0bc70d278210ac4a89c2780b4a271dc474fdd2a4ac16933a3d38"
+    sha256 cellar: :any,                 big_sur:       "5e24eda970bf16a1d737e0112ef7e86651c6cdd29b14b6dd4beec2faf9f9d292"
+    sha256 cellar: :any,                 catalina:      "3325a5a22c63c02f6c3a7d9b35f533e579f369ff2871f7152d0ca4994bb049d3"
+    sha256 cellar: :any,                 mojave:        "f44e98ef652573827da51288539acb1122af634b79f61f8ec2687b7b5184e971"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6dbe7ea8df080a2e437eca1bc85bf23ff34a0b5c86078e112617189aea8487cc"
   end
 
   depends_on "cmake" => :build
