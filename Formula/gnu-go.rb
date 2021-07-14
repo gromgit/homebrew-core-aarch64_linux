c505d6d7374b4b1a03ac50f4f7558288e293863b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 05:05:01 +0000

    gnu-go: update 3.8_1 bottle.

--- Formula/gnu-go.rb
+++ Formula/gnu-go.rb
@@ -16,6 +16,7 @@ class GnuGo < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "25fa92bd5c129cb655ec06c441523ada5cbc90a560111c32f5b1246c8f7d124c"
     sha256 cellar: :any_skip_relocation, el_capitan:    "f845be5a48a89cf0e46322b4d3a64a86b9fd4793f6b98fee0c45de5e8e5eda69"
     sha256 cellar: :any_skip_relocation, yosemite:      "57731f2cb8dcb2959e85baebdb779989390688a70447b923b9d5c1ce8575da44"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99e7447f557b3af1f8c1e56d9a30cbe4315bd9f05ec734fb2fadac5887ab0474"
   end
 
   def install
