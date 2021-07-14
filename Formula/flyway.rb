98c22710d3e8ab825fc33820c20d03421265e504
Rui Chen <rui@chenrui.dev>
2021-07-14 02:40:09 -0400

    flyway 7.11.2
    Closes #81234.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.11.1/flyway-commandline-7.11.1.tar.gz"
-  sha256 "6f3712a75c4d80dd18926be388a1f8cac95a639656a2ce3fde85d68662553738"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.11.2/flyway-commandline-7.11.2.tar.gz"
+  sha256 "194dbfa7c2f4a74b264e9477bf5dda73e76b69253555603349ba4a6b9e7bed28"
   license "Apache-2.0"
 
   livecheck do
