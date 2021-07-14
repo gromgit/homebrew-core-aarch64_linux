3861b36072ff9665b9b00f8494906c19cee9897b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 05:11:05 +0000

    gmic: update 2.9.7 bottle.

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -12,10 +12,11 @@ class Gmic < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2a684946ab82b40f48894bac5628fd568555c79e412154415149c559b8855439"
-    sha256 cellar: :any, big_sur:       "fa3a9f34a4e6af5756037d3d0085cc95192fd2cf133e14985cd98314b46c0d2d"
-    sha256 cellar: :any, catalina:      "28d193071d9c956bb8002584cbd7e9439cb3b4fbbce4fb37106345dfcc3898e8"
-    sha256 cellar: :any, mojave:        "8a24235a837b3972a6f8b39b3ca219e676c7807ed83daf613576f1acb813faa2"
+    sha256 cellar: :any,                 arm64_big_sur: "2a684946ab82b40f48894bac5628fd568555c79e412154415149c559b8855439"
+    sha256 cellar: :any,                 big_sur:       "fa3a9f34a4e6af5756037d3d0085cc95192fd2cf133e14985cd98314b46c0d2d"
+    sha256 cellar: :any,                 catalina:      "28d193071d9c956bb8002584cbd7e9439cb3b4fbbce4fb37106345dfcc3898e8"
+    sha256 cellar: :any,                 mojave:        "8a24235a837b3972a6f8b39b3ca219e676c7807ed83daf613576f1acb813faa2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dcbd95fd0abcd042ed27b1a964c9c70a3ec9cb89a7e04111d4b955d9c218fa52"
   end
 
   depends_on "cmake" => :build
