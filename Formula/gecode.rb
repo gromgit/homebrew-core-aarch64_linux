54c1b904f2c53b8ef89bd82eab0ce93f685c3432
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 01:52:38 +0000

    gecode: update 6.2.0 bottle.

--- Formula/gecode.rb
+++ Formula/gecode.rb
@@ -10,12 +10,13 @@ class Gecode < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b8e2416b133d383ed7e338b98282c4dc4e517fb241c708ec88bdca810d569423"
-    sha256 cellar: :any, big_sur:       "fc2f5401e3d709dad5bbca740c3c084987c495712990627d213437c94a9c6e97"
-    sha256 cellar: :any, catalina:      "de386e8ea3dcdbce6d35fe62e0f38f0bf51c6844db35eb7a2f81aa5501fa9c0d"
-    sha256 cellar: :any, mojave:        "525b7649d716a0ccb5f47f29e93a07f1677cbe531c9c978656b04826ad1cb678"
-    sha256 cellar: :any, high_sierra:   "763d0d5da64075f5f64c3b7aee49a604680c266b1b6e4eeb8ffcfdb9e0d9ca0d"
-    sha256 cellar: :any, sierra:        "1bb46e60636f1431cc5bf4b9aed1a2f038da1fef0eaeb1c3130a9252924efd54"
+    sha256 cellar: :any,                 arm64_big_sur: "b8e2416b133d383ed7e338b98282c4dc4e517fb241c708ec88bdca810d569423"
+    sha256 cellar: :any,                 big_sur:       "fc2f5401e3d709dad5bbca740c3c084987c495712990627d213437c94a9c6e97"
+    sha256 cellar: :any,                 catalina:      "de386e8ea3dcdbce6d35fe62e0f38f0bf51c6844db35eb7a2f81aa5501fa9c0d"
+    sha256 cellar: :any,                 mojave:        "525b7649d716a0ccb5f47f29e93a07f1677cbe531c9c978656b04826ad1cb678"
+    sha256 cellar: :any,                 high_sierra:   "763d0d5da64075f5f64c3b7aee49a604680c266b1b6e4eeb8ffcfdb9e0d9ca0d"
+    sha256 cellar: :any,                 sierra:        "1bb46e60636f1431cc5bf4b9aed1a2f038da1fef0eaeb1c3130a9252924efd54"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f818c76e47ef63048f87fd2b75a6b379eef97b934079a427578c727e2fa21f2"
   end
 
   def install
