45cd5612a5a88d3f823106437fc90f6a19452679
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-14 10:50:46 +0000

    scry: add 0.9.1 bottle.

--- Formula/scry.rb
+++ Formula/scry.rb
@@ -6,6 +6,12 @@ class Scry < Formula
   license "MIT"
   head "https://github.com/crystal-lang-tools/scry.git"
 
+  bottle do
+    sha256 big_sur:  "8560c26448338c483c2e5aaa488d39156260fa2e891bec02520ae8b8d9e1bcd1"
+    sha256 catalina: "da67d98abe3010dc12f6a4152a89dd86a79e1dd098d67afec4a07bfe30c0ccdd"
+    sha256 mojave:   "0f243e9970f781e847c7b6906d743e08c1f684c37797ecfd962a1617f7ce8b8f"
+  end
+
   depends_on "bdw-gc"
   depends_on "crystal"
   depends_on "libevent"
