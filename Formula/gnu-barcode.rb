d4d3282e21504707af9845f51a07e71f155c9ce8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 05:04:57 +0000

    gnu-barcode: update 0.99 bottle.

--- Formula/gnu-barcode.rb
+++ Formula/gnu-barcode.rb
@@ -15,6 +15,7 @@ class GnuBarcode < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "7588bb4800b5c348e103ed92e8bcc2f38812b8fbf4e254315e6429b3961e9f05"
     sha256 cellar: :any_skip_relocation, el_capitan:    "1885abad5bc70c2e9952e131307ca7282d851856ebdea58dadc69f0e125a7c22"
     sha256 cellar: :any_skip_relocation, yosemite:      "819af5d364f041397c7c6b768829df7fcbd617f86194a1656b5523eeaed9415a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9cea83cbded5ce1bf557f237984a5085ba5b5534980c64e0b7d97e6756ab01dd"
   end
 
   # Patch and ac_cv_func_calloc_0_nonnull config addresses the following issue:
