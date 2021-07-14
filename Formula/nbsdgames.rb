57bd61b631f2f70827baa32cd599a4ea15a37416
rui <rui@chenrui.dev>
2021-07-14 17:48:40 +0000

    nbsdgames: add 4.1.1 bottle.

--- Formula/nbsdgames.rb
+++ Formula/nbsdgames.rb
@@ -6,6 +6,14 @@ class Nbsdgames < Formula
   license :public_domain
   head "https://github.com/abakh/nbsdgames.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4a0d3a3303316461210d74b93b5f25e6ecdbe332ed9262d2f6651510aea4d7bf"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0992d4835169bbdb6e036557ba6dab0459e405a6af9bab245e52b016dbc1e6ec"
+    sha256 cellar: :any_skip_relocation, catalina:      "1e5fdfafa10957f4740d1b8bfe8c28ffe1952f64883f923278f354fd1cc98817"
+    sha256 cellar: :any_skip_relocation, mojave:        "1d8fc4cf13fca03c4e517a41e811be09906d3f799a0fc8473b9c8d922c9135a3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b726aba152abea07b43fdb2473325d009ca67abaee063ccc1dfad495dc57c00"
+  end
+
   uses_from_macos "ncurses"
 
   def install
