bac81ac2057115905b56f59446dec718bded4e21
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 01:42:33 +0000

    flvstreamer: update 2.1c1 bottle.

--- Formula/flvstreamer.rb
+++ Formula/flvstreamer.rb
@@ -20,6 +20,7 @@ class Flvstreamer < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "e257779383d236611212078f2e6db28d457d51a282d6163806e0232134a046b0"
     sha256 cellar: :any_skip_relocation, el_capitan:    "5a4b649ce0f2c32bca4091f4867a37cca0e8ae2a292d4ef29aa2949530bdd651"
     sha256 cellar: :any_skip_relocation, yosemite:      "243e6ce44b77212ff84e3a739bf2b203c687bdcdd36b17ba24daa5335bf0a151"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99df5a533003260eea6277d1fa2bb80d0354b666a6a99195c46813e3313c701f"
   end
 
   conflicts_with "rtmpdump", because: "both install 'rtmpsrv', 'rtmpsuck' and 'streams' binary"
