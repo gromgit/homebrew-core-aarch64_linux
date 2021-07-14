a1c12f93d171e3e5159721ff1da87e9892ea5eb5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 05:10:27 +0000

    gocr: update 0.52_1 bottle.

--- Formula/gocr.rb
+++ Formula/gocr.rb
@@ -11,11 +11,12 @@ class Gocr < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a3639ecb89f70562106db696e43f8ccdbe770b812ba2cdc695637b5f8e8dba7f"
-    sha256 cellar: :any, big_sur:       "e2fecccba7638297e89075dd8a21bf64d124a9f4f341f2437411abadf90b1f33"
-    sha256 cellar: :any, catalina:      "d0408f223b941c6d81c0edd843ab5916475a4ea4b94892b548da6403e4c3af2a"
-    sha256 cellar: :any, mojave:        "d173d60e8d8f139b4e7e310b84d1bfc56e406eb026c51beba9d4b2facaac3ae1"
-    sha256 cellar: :any, high_sierra:   "2a5cfa5a815706b2ecb11658ad9132bba21de5304e4541118d8d061a5bb7779a"
+    sha256 cellar: :any,                 arm64_big_sur: "a3639ecb89f70562106db696e43f8ccdbe770b812ba2cdc695637b5f8e8dba7f"
+    sha256 cellar: :any,                 big_sur:       "e2fecccba7638297e89075dd8a21bf64d124a9f4f341f2437411abadf90b1f33"
+    sha256 cellar: :any,                 catalina:      "d0408f223b941c6d81c0edd843ab5916475a4ea4b94892b548da6403e4c3af2a"
+    sha256 cellar: :any,                 mojave:        "d173d60e8d8f139b4e7e310b84d1bfc56e406eb026c51beba9d4b2facaac3ae1"
+    sha256 cellar: :any,                 high_sierra:   "2a5cfa5a815706b2ecb11658ad9132bba21de5304e4541118d8d061a5bb7779a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4fc5af66eb1d5e1345c1599d57da5a4695f7d37975ba96bfa97dd481ec3a7b1c"
   end
 
   depends_on "jpeg"
