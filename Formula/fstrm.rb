2144a939b9655603becc432383f5c6a27d5b988e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 01:44:05 +0000

    fstrm: update 0.6.1 bottle.

--- Formula/fstrm.rb
+++ Formula/fstrm.rb
@@ -6,10 +6,11 @@ class Fstrm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "409e20e264b28337487a22ff762e8f7d1b4dc81bea1cd131c6d673a978d94e2f"
-    sha256 cellar: :any, big_sur:       "32c20ee504e029088d36ee45177137411beed0aaaac76ce287810cec71d3eea9"
-    sha256 cellar: :any, catalina:      "3b775d63b3594f2264b413184aad3fbb33990c07473e0db9db12c86bd0f19950"
-    sha256 cellar: :any, mojave:        "7f18a4569511492fdad064427c67fc88f988046c1fc6804a7973e1ae2911714e"
+    sha256 cellar: :any,                 arm64_big_sur: "409e20e264b28337487a22ff762e8f7d1b4dc81bea1cd131c6d673a978d94e2f"
+    sha256 cellar: :any,                 big_sur:       "32c20ee504e029088d36ee45177137411beed0aaaac76ce287810cec71d3eea9"
+    sha256 cellar: :any,                 catalina:      "3b775d63b3594f2264b413184aad3fbb33990c07473e0db9db12c86bd0f19950"
+    sha256 cellar: :any,                 mojave:        "7f18a4569511492fdad064427c67fc88f988046c1fc6804a7973e1ae2911714e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a1db14f84679ffb80882a5a00b733e671f82242d2a338ce485e180b4f40f1a0"
   end
 
   head do
