be25358429fc7bc8445d01739cb9dd9cdb2e7bd1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-14 13:12:30 +0000

    minisat: update 2.2.1 bottle.

--- Formula/minisat.rb
+++ Formula/minisat.rb
@@ -7,10 +7,11 @@ class Minisat < Formula
   head "https://github.com/stp/minisat.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b802117d6cc0fa96bedac9eaa086908687ad87c7a368558e47c2417d3d2b7146"
-    sha256 cellar: :any, big_sur:       "ecc424ea3cde4f1a8d7056802d384ce1ab9823393301cac432dc26260685437a"
-    sha256 cellar: :any, catalina:      "0940a0cb0c4c4d6130d1eb7aa698561d6b01904044ee57731ad2c12092e30e46"
-    sha256 cellar: :any, mojave:        "cc7b59b30490175c56a6068c97956c39da31068df2522376da99998761972319"
+    sha256 cellar: :any,                 arm64_big_sur: "b802117d6cc0fa96bedac9eaa086908687ad87c7a368558e47c2417d3d2b7146"
+    sha256 cellar: :any,                 big_sur:       "ecc424ea3cde4f1a8d7056802d384ce1ab9823393301cac432dc26260685437a"
+    sha256 cellar: :any,                 catalina:      "0940a0cb0c4c4d6130d1eb7aa698561d6b01904044ee57731ad2c12092e30e46"
+    sha256 cellar: :any,                 mojave:        "cc7b59b30490175c56a6068c97956c39da31068df2522376da99998761972319"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1277996ab9a57f7c617501f9b386c2bb18ac90e00293738c12222dd89d90a979"
   end
 
   depends_on "cmake" => :build
