2727654218dc72bd40eaee1a14d607640018bc28
MoonFruit <dkmoonfruit@gmail.com>
2021-07-14 14:02:17 +0800

    ant 1.10.11
    Closes #81228.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ant.rb
+++ Formula/ant.rb
@@ -1,9 +1,9 @@
 class Ant < Formula
   desc "Java build tool"
   homepage "https://ant.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=ant/binaries/apache-ant-1.10.10-bin.tar.xz"
-  mirror "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.tar.xz"
-  sha256 "55d9bf9874cc4a9704a6b8748f6dc56ad458565504ce894f17424786436e6b52"
+  url "https://www.apache.org/dyn/closer.lua?path=ant/binaries/apache-ant-1.10.11-bin.tar.xz"
+  mirror "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.11-bin.tar.xz"
+  sha256 "baa049855cdecbefa62539555824058e52412e5ebe8f102e1db944cb762e06d9"
   license "Apache-2.0"
   head "https://git-wip-us.apache.org/repos/asf/ant.git"
 
