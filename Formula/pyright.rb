44d90fd0ffd0e7277682b2a1273aa29fad3dfa47
David Baumgold <david@davidbaumgold.com>
2021-07-14 19:53:53 +0200

    pyright 1.1.156 (new formula)
    Closes #81267.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/pyright.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class Pyright < Formula
+  desc "Static type checker for Python"
+  homepage "https://github.com/microsoft/pyright"
+  url "https://registry.npmjs.org/pyright/-/pyright-1.1.156.tgz"
+  sha256 "07e15e9247180ea66f746fb30610ef4b0eccf0167b0860e8df42f7dc034df948"
+  license "MIT"
+  head "https://github.com/microsoft/pyright.git"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"broken.py").write <<~EOS
+      def wrong_types(a: int, b: int) -> str:
+          return a + b
+    EOS
+    output = pipe_output("#{bin}/pyright broken.py 2>&1")
+    assert_match 'error: Expression of type "int" cannot be assigned to return type "str"', output
+  end
+end
