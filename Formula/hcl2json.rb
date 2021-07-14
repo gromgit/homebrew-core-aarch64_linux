89736def5d4125f7c9d2ff0c961464c3b60e52c4
Nanda H Krishna <me@nandahkrishna.com>
2021-07-14 09:33:56 +0000

    hcl2json: add 0.3.3 bottle.

--- Formula/hcl2json.rb
+++ Formula/hcl2json.rb
@@ -6,6 +6,14 @@ class Hcl2json < Formula
   license "Apache-2.0"
   head "https://github.com/tmccombs/hcl2json.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "357414d0b2d4cfd38220d4e9d52b556fc431334f0fcd6d62d5fd3ff97b1b2f14"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
+    sha256 cellar: :any_skip_relocation, catalina:      "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
+    sha256 cellar: :any_skip_relocation, mojave:        "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b1b2da82eecafa95be718e656620b460e6a36bbb5acd8259bc66e41341b32ae0"
+  end
+
   depends_on "go" => :build
 
   def install
