4f9dfadf57b9ab3abd4e0cd3eb8eb850ebc92bd3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-14 04:40:44 +0000

    amtk: update 5.2.0 bottle.

--- Formula/amtk.rb
+++ Formula/amtk.rb
@@ -11,6 +11,7 @@ class Amtk < Formula
     sha256 catalina:      "89e24e19e0614b13d387b9c0d2ccf89ac15f485edf49c7c39bcaa4f80deba3c1"
     sha256 mojave:        "004425110c03c91144cfd53df0f6141b05e38d86b64e96303cd6760db9e66a42"
     sha256 high_sierra:   "67ad617a78c6922647c2af49225a5f4b8fd7eff3635d0e7f8b4320687b896b60"
+    sha256 x86_64_linux:  "816d3b14c924a5024ba23409a3af828a578325c0fe3eaeff9e4f81b553142ca9"
   end
 
   depends_on "gobject-introspection" => :build
