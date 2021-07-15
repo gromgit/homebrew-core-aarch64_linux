1f89edc6990f554b0800941db458d5fc4cef6b2c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:37:06 +0000

    kepubify: update 4.0.0 bottle.

--- Formula/kepubify.rb
+++ Formula/kepubify.rb
@@ -11,6 +11,7 @@ class Kepubify < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "da252af2e0a3f53a8716f2792276c2822f1c62a2025d10284b8967311dac5981"
     sha256 cellar: :any_skip_relocation, catalina:      "e210cb3bbcc242dd29e8c7849384a9d504b4f36ed40afcff50332a47cacac7ab"
     sha256 cellar: :any_skip_relocation, mojave:        "4c284abb8cb04ed19761df43c53dc35c8520f49f37b2bc1f8191fbaa643364de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bbce8b5b6062c05d593831f941b1d2ff1e9a2d25bd5aa66cc0801194b5207c3d"
   end
 
   depends_on "go" => :build
