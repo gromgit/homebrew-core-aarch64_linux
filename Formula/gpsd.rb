950e14b5a8b56d341c97e0abf1ded7279e2d0464
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:10:21 +0000

    gpsd: update 3.21 bottle.

--- Formula/gpsd.rb
+++ Formula/gpsd.rb
@@ -11,11 +11,12 @@ class Gpsd < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "94c0f86ec9ec850c0fd8f8b3069e6881dd895e57324c5eb46822406441208317"
-    sha256 cellar: :any, big_sur:       "a68031d9cbef4b9cfbdfed566b8d0e556ae8188cedd6f505ca65314588bbc7f4"
-    sha256 cellar: :any, catalina:      "e41f44df2cf96b33b2f62e65ff2ef9154d872bc8fac88b3bdaeb503246d77c2b"
-    sha256 cellar: :any, mojave:        "caafc4aea3632fdbe8df1ce265c025430a816d2ad7c26f973c254887ec6a2a8f"
-    sha256 cellar: :any, high_sierra:   "bc0775e450c0129fd71a4abd163a7645ac9b3e1698009b2735fafeb838e09e79"
+    sha256 cellar: :any,                 arm64_big_sur: "94c0f86ec9ec850c0fd8f8b3069e6881dd895e57324c5eb46822406441208317"
+    sha256 cellar: :any,                 big_sur:       "a68031d9cbef4b9cfbdfed566b8d0e556ae8188cedd6f505ca65314588bbc7f4"
+    sha256 cellar: :any,                 catalina:      "e41f44df2cf96b33b2f62e65ff2ef9154d872bc8fac88b3bdaeb503246d77c2b"
+    sha256 cellar: :any,                 mojave:        "caafc4aea3632fdbe8df1ce265c025430a816d2ad7c26f973c254887ec6a2a8f"
+    sha256 cellar: :any,                 high_sierra:   "bc0775e450c0129fd71a4abd163a7645ac9b3e1698009b2735fafeb838e09e79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eeb280e130d077b69e19bcd74ee415c5791d81e21a74d62248077f0937d6d5e9"
   end
 
   depends_on "scons" => :build
