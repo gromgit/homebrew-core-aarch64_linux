5033e8e3036aa50e510029a1fbc97747b340eb38
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:32:04 +0000

    jp2a: update 1.0.6_1 bottle.

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -13,11 +13,12 @@ class Jp2a < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "d76bf96595bd8e388e19adfaa4fc9d635f497f597ff1a9f1ba2abb3f1c2f3dc1"
-    sha256 cellar: :any, big_sur:       "8ab042d2d660fbc55e4a055e0618d85fbbd32029f703d35c59f3d12c0b000936"
-    sha256 cellar: :any, catalina:      "a9aa7c8893c63ad5621788af3813ed9758e09e0c79b9ba3a8262d5c17b2272f9"
-    sha256 cellar: :any, mojave:        "4e62b310889a384daf9058267ac0b1bdc73d2cb408f05b9e3d3072be52355bfd"
-    sha256 cellar: :any, high_sierra:   "8400fccf2a4459fe37ce0f3856459127f4f66ff002c356f36942462c0c9c3809"
+    sha256 cellar: :any,                 arm64_big_sur: "d76bf96595bd8e388e19adfaa4fc9d635f497f597ff1a9f1ba2abb3f1c2f3dc1"
+    sha256 cellar: :any,                 big_sur:       "8ab042d2d660fbc55e4a055e0618d85fbbd32029f703d35c59f3d12c0b000936"
+    sha256 cellar: :any,                 catalina:      "a9aa7c8893c63ad5621788af3813ed9758e09e0c79b9ba3a8262d5c17b2272f9"
+    sha256 cellar: :any,                 mojave:        "4e62b310889a384daf9058267ac0b1bdc73d2cb408f05b9e3d3072be52355bfd"
+    sha256 cellar: :any,                 high_sierra:   "8400fccf2a4459fe37ce0f3856459127f4f66ff002c356f36942462c0c9c3809"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06ca49b6a1b7d58b2a65e998e0e4636c72a13f5e4af51704c36950ef89818356"
   end
 
   depends_on "jpeg"
