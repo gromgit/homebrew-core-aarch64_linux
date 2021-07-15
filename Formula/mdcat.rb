33d45844cca56a5097b62104b803846c9dfbb2d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-15 01:29:07 +0000

    mdcat: update 0.23.1 bottle.

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -6,11 +6,11 @@ class Mdcat < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0d1886c20e053b97c43750f9bf953ebbb7ef3c66648dccb03b47076d0c5902d3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e52303d0ee7e8dd69f10290358f1e1b46457a56063173793ea2f3a8506d11bb0"
-    sha256 cellar: :any_skip_relocation, catalina:      "fb0faeb8cd359011ac2d73c1e6f18bd7204af76a2662a7696c02e5a055bc7380"
-    sha256 cellar: :any_skip_relocation, mojave:        "484e335714e81c352005b784bd64b20d42d6887e13642dca4e84b5194697e6fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d38d16de6d351b0a9889003cf1f7045091e542fc60761899ec299827f6f0f6c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f0c38e191e483f390a9b7324adf7a17e9ab5902b7db7bbff16c780c2a6583dec"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a6a1be8b27f8493192d3fc600e5796ef216fd5fc37d6f9a71b08656d63b3d088"
+    sha256 cellar: :any_skip_relocation, catalina:      "0841c94bc8910a3f45de9ca4a19b2354d00124ab1dfed9770adc6c7774e0cef5"
+    sha256 cellar: :any_skip_relocation, mojave:        "07cc85499ea9c642b08166404933bdaf19c8fd9730e6437b800be9a3f1cf8d1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0680c656186b15cca3033b6b4937f59000b59199de3cc432cc844401ea08af2"
   end
 
   depends_on "cmake" => :build
