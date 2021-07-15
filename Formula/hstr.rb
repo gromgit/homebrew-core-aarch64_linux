15634cabdb2a1637ec8ba45a61dbc2f24127f102
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:19:02 +0000

    hstr: update 2.3 bottle.

--- Formula/hstr.rb
+++ Formula/hstr.rb
@@ -6,10 +6,11 @@ class Hstr < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6f1758717c6c34317a775a0975864d66bdd2696f491859627a00641966ce6ece"
-    sha256 cellar: :any, big_sur:       "23a7e98369fa1ef412f6f374d24a1828b5131425b39b828688004cf6e4cedda8"
-    sha256 cellar: :any, catalina:      "eb5a750b04ba6c1908bde96a1bd7fb8733a80632953eeb5d43ad51b7747cc191"
-    sha256 cellar: :any, mojave:        "723f7aec818080a72e40a3eda2aad8669d5d952927a7894718946cc0b788ff27"
+    sha256 cellar: :any,                 arm64_big_sur: "6f1758717c6c34317a775a0975864d66bdd2696f491859627a00641966ce6ece"
+    sha256 cellar: :any,                 big_sur:       "23a7e98369fa1ef412f6f374d24a1828b5131425b39b828688004cf6e4cedda8"
+    sha256 cellar: :any,                 catalina:      "eb5a750b04ba6c1908bde96a1bd7fb8733a80632953eeb5d43ad51b7747cc191"
+    sha256 cellar: :any,                 mojave:        "723f7aec818080a72e40a3eda2aad8669d5d952927a7894718946cc0b788ff27"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab96608841a90734f14748aa02ef5c614c1dc0f6c6b61ada6d06a736a6656689"
   end
 
   depends_on "autoconf" => :build
