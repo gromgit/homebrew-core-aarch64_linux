f6f31f38e55927ab02e30beaadbc2627d781e373
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:24:32 +0000

    ioping: update 1.2 bottle.

--- Formula/ioping.rb
+++ Formula/ioping.rb
@@ -12,6 +12,7 @@ class Ioping < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "622678afe9bf88bae08cc264dece76f0abefc854915f5b3d5355cde767aa61e1"
     sha256 cellar: :any_skip_relocation, mojave:        "4c88038d68f17bbc405c5ed253542890e0fc1e44ece8650f1a68b6ff6df7fabf"
     sha256 cellar: :any_skip_relocation, high_sierra:   "9a5ee7cd526c89d70c75fe6fcf61d7b0a777d8bf3a823fe99348864a9838b6ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "821ef4ae7a4bb2a8a26f6b812556cbd3f079fc4bd87eeca9303d1dec1f8bc4dc"
   end
 
   def install
