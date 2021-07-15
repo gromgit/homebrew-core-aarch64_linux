2aefff094a925c24179b641eaf77ac7e3adf9210
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:46:34 +0000

    libcmph: update 2.0.2 bottle.

--- Formula/libcmph.rb
+++ Formula/libcmph.rb
@@ -5,11 +5,12 @@ class Libcmph < Formula
   sha256 "365f1e8056400d460f1ee7bfafdbf37d5ee6c78e8f4723bf4b3c081c89733f1e"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "30d22ddad3521ec07248910864e8caae7f8d959597663a9d21d2447c56e6639c"
-    sha256 cellar: :any, big_sur:       "f1cc2211ac56a2702405246535a55613855c3879885ca73aa65d76890c2aa0e5"
-    sha256 cellar: :any, catalina:      "c38019c153c728a28acbfe340cc86764285ec24edbdba5234b0593f83d355c22"
-    sha256 cellar: :any, mojave:        "d02c761bd6b52424528bfdcd56b8d469d7cdd2e55f625c719229edb7f011889c"
-    sha256 cellar: :any, high_sierra:   "abffeaf075db6387e636d43eb8fda9b76f02091bdb5533368306f899a46406c1"
+    sha256 cellar: :any,                 arm64_big_sur: "30d22ddad3521ec07248910864e8caae7f8d959597663a9d21d2447c56e6639c"
+    sha256 cellar: :any,                 big_sur:       "f1cc2211ac56a2702405246535a55613855c3879885ca73aa65d76890c2aa0e5"
+    sha256 cellar: :any,                 catalina:      "c38019c153c728a28acbfe340cc86764285ec24edbdba5234b0593f83d355c22"
+    sha256 cellar: :any,                 mojave:        "d02c761bd6b52424528bfdcd56b8d469d7cdd2e55f625c719229edb7f011889c"
+    sha256 cellar: :any,                 high_sierra:   "abffeaf075db6387e636d43eb8fda9b76f02091bdb5533368306f899a46406c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b4a556fa47365d3ebf9312acddf3fc64921094161bd6d6e1bcda3df92be70cd"
   end
 
   def install
