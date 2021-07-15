5d378448add0ce792e936bb64e27cf15a0411f50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-15 22:07:30 +0000

    crystal: update 1.1.0 bottle.

--- Formula/crystal.rb
+++ Formula/crystal.rb
@@ -19,10 +19,10 @@ class Crystal < Formula
   end
 
   bottle do
-    sha256 big_sur:      "2b3ed35fef1b42c1d2acc740edbdc72eb6849ec658f8680fef3f2c1c2a7d5378"
-    sha256 catalina:     "2f270445145ad381faf1e5a2d35d5400c084a38099c4ffb4b6a861dd712ee6ff"
-    sha256 mojave:       "00a853fa0bae01d2acf4013f4e5acc75db736145bf0e9b07719909e31b1401b0"
-    sha256 x86_64_linux: "55af7f38763fe23f2decfaa9f895b05e3510cffbd7a1ecd669c48366bb83dbe5"
+    sha256 big_sur:      "203fd3e96b08c633eb2d7613b8705443eed7c3a759acbc0be3c738d15cc52ac0"
+    sha256 catalina:     "a9eb1a0410a72bcaad10dac0c9e86b5edfa97d8d540610a759b1bfd20cf3a7d3"
+    sha256 mojave:       "2d002f451ded60bdee46d73dfcc4fcc76611bf542e83e6f6136a3529c064d3c0"
+    sha256 x86_64_linux: "4b80defa3c6475565ea3bd60c1378267e26330e0da37610f0ee4b3b28d241e7f"
   end
 
   head do
