4af263478fb7601baaab500d030fd282bf64b1e1
Nanda H Krishna <me@nandahkrishna.com>
2021-07-15 07:55:18 +0000

    mbedtls@2: add 2.27.0 bottle.

--- Formula/mbedtls@2.rb
+++ Formula/mbedtls@2.rb
@@ -11,6 +11,13 @@ class MbedtlsAT2 < Formula
     regex(/^v?(2(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "ad0abc939b4b50be556177673fe5bb3a08a4a8725936d106c0c2bf599b81387d"
+    sha256 cellar: :any, big_sur:       "1ee8bd0e453c70b8751f9f1fb307e7915f0eaf22bfcc2c39c3fc75af9344d310"
+    sha256 cellar: :any, catalina:      "fb6db7177cbeefc4478a32b1a1a78cc0442db7ec96cfc1760d15d221cea3b92d"
+    sha256 cellar: :any, mojave:        "8c8611c1a3dec140495803b9dd847e1ef5dc044deab964181124309cd0be950a"
+  end
+
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
