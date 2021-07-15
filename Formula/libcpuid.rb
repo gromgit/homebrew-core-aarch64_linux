3934f286112efeafa8b257bdee7f931dd474db93
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:47:11 +0000

    libcpuid: update 0.5.1 bottle.

--- Formula/libcpuid.rb
+++ Formula/libcpuid.rb
@@ -7,9 +7,10 @@ class Libcpuid < Formula
   head "https://github.com/anrieff/libcpuid.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "f7252b191ada11eee6bb25649cba4fda28be44c91ebcfd936e3508d3573bf4f1"
-    sha256 cellar: :any, catalina: "e954e21a3bb2ab10c1eb831af1626ccf9cbbe69e123a4da6d69975d59cfca867"
-    sha256 cellar: :any, mojave:   "9cb4e35df56ce25adcfc4c0a03f1a377aac54ec7e217bc9bb583df41eebcc8c2"
+    sha256 cellar: :any,                 big_sur:      "f7252b191ada11eee6bb25649cba4fda28be44c91ebcfd936e3508d3573bf4f1"
+    sha256 cellar: :any,                 catalina:     "e954e21a3bb2ab10c1eb831af1626ccf9cbbe69e123a4da6d69975d59cfca867"
+    sha256 cellar: :any,                 mojave:       "9cb4e35df56ce25adcfc4c0a03f1a377aac54ec7e217bc9bb583df41eebcc8c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "20afc15713aa6f9097f50da5ea44d06beda67451c70c52639d9bb973c0f5d26f"
   end
 
   depends_on "autoconf" => :build
