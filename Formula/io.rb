e631b80fd9c79cf54d8a3dc83a52b7753aed9650
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:26:05 +0000

    io: update 2017.09.06_1 bottle.

--- Formula/io.rb
+++ Formula/io.rb
@@ -8,10 +8,11 @@ class Io < Formula
   head "https://github.com/IoLanguage/io.git"
 
   bottle do
-    sha256 big_sur:     "fae9b76e33ac8a9f4dd4f3c2335b13b003c2bdc01b81c4a2efbf5d7435c51e15"
-    sha256 catalina:    "c4c862d20a8e4ddb1e6e588414a9e23ae2a17baa490e3beb621614aca7a8ca87"
-    sha256 mojave:      "48c37d6f30d8b01d391e7f4ef777b5087425d89a9df0077414769a59333db420"
-    sha256 high_sierra: "a061482b97c1ada8eea9d658f13fe0cfbfa223d97762b51611c4cab2de4c0273"
+    sha256 big_sur:      "fae9b76e33ac8a9f4dd4f3c2335b13b003c2bdc01b81c4a2efbf5d7435c51e15"
+    sha256 catalina:     "c4c862d20a8e4ddb1e6e588414a9e23ae2a17baa490e3beb621614aca7a8ca87"
+    sha256 mojave:       "48c37d6f30d8b01d391e7f4ef777b5087425d89a9df0077414769a59333db420"
+    sha256 high_sierra:  "a061482b97c1ada8eea9d658f13fe0cfbfa223d97762b51611c4cab2de4c0273"
+    sha256 x86_64_linux: "67983adeeab7db99c20e4e524211afe6d3d5bd04b13fca5dcc4cfd841743f146"
   end
 
   depends_on "cmake" => :build
