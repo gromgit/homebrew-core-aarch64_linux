aa74778d65be3edd47064752fe0c9bcc3db2ca39
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:09:06 +0000

    gqlplus: update 1.16_2 bottle.

--- Formula/gqlplus.rb
+++ Formula/gqlplus.rb
@@ -6,12 +6,13 @@ class Gqlplus < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e93b52a5967e87f0ccc282ad897c4d64b42e0bcf4e41f7ece17186f1fd36bcd6"
-    sha256 cellar: :any, big_sur:       "6b4b7972c9c29e749bb0546aa83f756e967aca10793ff70de9bb1711536d929a"
-    sha256 cellar: :any, catalina:      "2ffb1031a83fe666dc574d17d72b08781dd08e48f1dba88c5c67550472f819df"
-    sha256 cellar: :any, mojave:        "014190ba5c0c8e5bc88b0d434f6a05d4c26ab0b6e40b96d6fa00f37f02ee078a"
-    sha256 cellar: :any, high_sierra:   "324544d5383507812e0cc14a6e085697661944588ee1fe56477c67e2bc1c009d"
-    sha256 cellar: :any, sierra:        "9ad645ec60442256a86ad9bcd081b7adffd4d6ad4cdb47f10814020b53f3200d"
+    sha256 cellar: :any,                 arm64_big_sur: "e93b52a5967e87f0ccc282ad897c4d64b42e0bcf4e41f7ece17186f1fd36bcd6"
+    sha256 cellar: :any,                 big_sur:       "6b4b7972c9c29e749bb0546aa83f756e967aca10793ff70de9bb1711536d929a"
+    sha256 cellar: :any,                 catalina:      "2ffb1031a83fe666dc574d17d72b08781dd08e48f1dba88c5c67550472f819df"
+    sha256 cellar: :any,                 mojave:        "014190ba5c0c8e5bc88b0d434f6a05d4c26ab0b6e40b96d6fa00f37f02ee078a"
+    sha256 cellar: :any,                 high_sierra:   "324544d5383507812e0cc14a6e085697661944588ee1fe56477c67e2bc1c009d"
+    sha256 cellar: :any,                 sierra:        "9ad645ec60442256a86ad9bcd081b7adffd4d6ad4cdb47f10814020b53f3200d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0953b6a9b940124d8922f196387f0f9553d2a36824e41633da5643a0668619f"
   end
 
   depends_on "readline"
