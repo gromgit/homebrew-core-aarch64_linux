88804b78139239722aab5613198f6255d63e14dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-15 16:12:52 +0000

    wgcf: update 2.2.5 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -7,10 +7,10 @@ class Wgcf < Formula
   head "https://github.com/ViRb3/wgcf.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "661af7987b3d6cccbb1411ba153e0a6edaac1a5b98ebd072eadf483308debf89"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cd7f1f650844daf0fb7e3c2bc921248605c76df817119a0a391205736ea783f0"
-    sha256 cellar: :any_skip_relocation, catalina:      "0bdecda45a9f085e3ca91b04b410a64cfb4f45ae1ff13462c5255de9c075f184"
-    sha256 cellar: :any_skip_relocation, mojave:        "f3e0cb8c6e4173db279ddf93702dbf63b56f602836fb80eb45c892bd42db536a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "48eda9bda51cad2c18a6e344acc04bd426146ae017076ab750b0f50bad0e4efa"
+    sha256 cellar: :any_skip_relocation, big_sur:       "811ef23bfdc97ee82729d35dffa5a40e8e96ae0646d03ce4a0a1f49add45b502"
+    sha256 cellar: :any_skip_relocation, catalina:      "4e26badb10d7a26372983909f868595787ce64d4da49a97c8729934fd2d2bc92"
+    sha256 cellar: :any_skip_relocation, mojave:        "56e59da72fd6403cda4404c3791160269369d5df5e1ccdfda6a7ab5dd0ba0c9c"
   end
 
   depends_on "go" => :build
