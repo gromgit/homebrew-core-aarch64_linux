03d72e67c320ebf95a613ae0897575e6bdbf9e88
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:33:50 +0000

    jpeginfo: update 1.6.1_1 bottle.

--- Formula/jpeginfo.rb
+++ Formula/jpeginfo.rb
@@ -14,10 +14,11 @@ class Jpeginfo < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "883d13008806a89bd05f612ffd27940a5985f47ad9c950af76f719b6a781bb1e"
-    sha256 cellar: :any, big_sur:       "27bb3588438853fb065ef36885dfea66a2e066dddc7025ea8fd6295682ff8b83"
-    sha256 cellar: :any, catalina:      "0f0cc493a38a1a701a51f6aa2cada9b8f248c228a72ce30c451d5cab2906e8c5"
-    sha256 cellar: :any, mojave:        "71cbeda00d00f513847a88930a6851b00ab9811fb6ed37d0617eaee5e86decf3"
+    sha256 cellar: :any,                 arm64_big_sur: "883d13008806a89bd05f612ffd27940a5985f47ad9c950af76f719b6a781bb1e"
+    sha256 cellar: :any,                 big_sur:       "27bb3588438853fb065ef36885dfea66a2e066dddc7025ea8fd6295682ff8b83"
+    sha256 cellar: :any,                 catalina:      "0f0cc493a38a1a701a51f6aa2cada9b8f248c228a72ce30c451d5cab2906e8c5"
+    sha256 cellar: :any,                 mojave:        "71cbeda00d00f513847a88930a6851b00ab9811fb6ed37d0617eaee5e86decf3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3e4419c51af044e57adaf7d6d360bd99ad1bc16d4d837d060df84fe308bb4d2"
   end
 
   depends_on "autoconf" => :build
