4a5cbed1800b68c78462f6d6088afbec5c91cad9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:49:52 +0000

    libcue: update 2.2.1 bottle.

--- Formula/libcue.rb
+++ Formula/libcue.rb
@@ -6,13 +6,14 @@ class Libcue < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7619618f7bce8398ec7fe8ffd777012876cec3511a4bb595833770e9e631fed2"
-    sha256 cellar: :any, big_sur:       "cedf45a5d0ce2803b22527805777d7f185e4e9875f004dddca1ccd1000364d5d"
-    sha256 cellar: :any, catalina:      "17252ea3af3c3a75478e8526ae7856dadfa641d7eb050553503bfe383573e740"
-    sha256 cellar: :any, mojave:        "e3f6c16d235459f97b299627d359cb9fbe7526b8635e08521b1c36460b045162"
-    sha256 cellar: :any, high_sierra:   "209e548399503830e0f786c6faef21836aa350d67db644b9ad291703ebe2e9c5"
-    sha256 cellar: :any, sierra:        "14a6edb39d2887ad6beeb34dad944501d01f70480a529cb7e50d838833404f4f"
-    sha256 cellar: :any, el_capitan:    "27f8ab5419958ea5817e5e44b68f24ea2a0c27d12a664556b12f6789866d0da5"
+    sha256 cellar: :any,                 arm64_big_sur: "7619618f7bce8398ec7fe8ffd777012876cec3511a4bb595833770e9e631fed2"
+    sha256 cellar: :any,                 big_sur:       "cedf45a5d0ce2803b22527805777d7f185e4e9875f004dddca1ccd1000364d5d"
+    sha256 cellar: :any,                 catalina:      "17252ea3af3c3a75478e8526ae7856dadfa641d7eb050553503bfe383573e740"
+    sha256 cellar: :any,                 mojave:        "e3f6c16d235459f97b299627d359cb9fbe7526b8635e08521b1c36460b045162"
+    sha256 cellar: :any,                 high_sierra:   "209e548399503830e0f786c6faef21836aa350d67db644b9ad291703ebe2e9c5"
+    sha256 cellar: :any,                 sierra:        "14a6edb39d2887ad6beeb34dad944501d01f70480a529cb7e50d838833404f4f"
+    sha256 cellar: :any,                 el_capitan:    "27f8ab5419958ea5817e5e44b68f24ea2a0c27d12a664556b12f6789866d0da5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "750eabefb348e1577ac23fd2d4d3ead2c21ef1f2596236ab59054067322d59c8"
   end
 
   depends_on "cmake" => :build
