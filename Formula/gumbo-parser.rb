7140b53eab22684fb0b7482877e9e28401194840
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:12:18 +0000

    gumbo-parser: update 0.10.1 bottle.

--- Formula/gumbo-parser.rb
+++ Formula/gumbo-parser.rb
@@ -6,14 +6,15 @@ class GumboParser < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9812d6af063b978c1314c5bc6f1eedcb34d9e395174ba9b68932feb69ed3f2e0"
-    sha256 cellar: :any, big_sur:       "917387609673137f253bc7f1effcb26a710c8a315f4d194de0bed0c6e21fc3b2"
-    sha256 cellar: :any, catalina:      "c922c8ec4425cef96e3283bace0ffda97cdd5f4946ca151da69045c6ce80ef06"
-    sha256 cellar: :any, mojave:        "aa6ed085625f40a65ecead082bd711dcb16af9aed6f74372edb7dc19e44fba5c"
-    sha256 cellar: :any, high_sierra:   "ed0957fe59981b55c1baf149022a5b0f3a163f1a6eb6e03e402da2f018406b9f"
-    sha256 cellar: :any, sierra:        "7c911b3f74827405abdf92cb6f6265cf7185043af4101d851eb68c5e69ea71e6"
-    sha256 cellar: :any, el_capitan:    "56f5446eb431b628655748659a8a7479466e00addf7d90070464364a3f3cafa9"
-    sha256 cellar: :any, yosemite:      "02169cdaafcf9343bacf98e0e34b1f7383eb0b1b89385965d81796e110f8c38f"
+    sha256 cellar: :any,                 arm64_big_sur: "9812d6af063b978c1314c5bc6f1eedcb34d9e395174ba9b68932feb69ed3f2e0"
+    sha256 cellar: :any,                 big_sur:       "917387609673137f253bc7f1effcb26a710c8a315f4d194de0bed0c6e21fc3b2"
+    sha256 cellar: :any,                 catalina:      "c922c8ec4425cef96e3283bace0ffda97cdd5f4946ca151da69045c6ce80ef06"
+    sha256 cellar: :any,                 mojave:        "aa6ed085625f40a65ecead082bd711dcb16af9aed6f74372edb7dc19e44fba5c"
+    sha256 cellar: :any,                 high_sierra:   "ed0957fe59981b55c1baf149022a5b0f3a163f1a6eb6e03e402da2f018406b9f"
+    sha256 cellar: :any,                 sierra:        "7c911b3f74827405abdf92cb6f6265cf7185043af4101d851eb68c5e69ea71e6"
+    sha256 cellar: :any,                 el_capitan:    "56f5446eb431b628655748659a8a7479466e00addf7d90070464364a3f3cafa9"
+    sha256 cellar: :any,                 yosemite:      "02169cdaafcf9343bacf98e0e34b1f7383eb0b1b89385965d81796e110f8c38f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ae8a4b287fcbedb1cd48eeb3c26a257b2829401e23c0026dafb04461dcf07ae"
   end
 
   depends_on "autoconf" => :build
