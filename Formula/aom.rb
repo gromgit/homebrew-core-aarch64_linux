b473623b5f64e26cee73ffa581464284ec49f044
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 18:40:18 +0000

    aom: update 3.1.1 bottle.

--- Formula/aom.rb
+++ Formula/aom.rb
@@ -7,10 +7,11 @@ class Aom < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "dafc3da3568a3faf64f1fa180176de062fb2e5872bdebb89e6162cb606e411fd"
-    sha256 cellar: :any, big_sur:       "f8b17942fbb271c70fbe122f0937cd446acd7f86bcd792668f64fbcfb51d5e96"
-    sha256 cellar: :any, catalina:      "839bce0f6c0f486583dae83b50c154272b34dc9bd06bd6efd5a43f4fc91a63cd"
-    sha256 cellar: :any, mojave:        "80ca7af4ba75bf8c0ae577eb66bfaf16e7cd564c16f0639c6e50df1427fd6452"
+    sha256 cellar: :any,                 arm64_big_sur: "dafc3da3568a3faf64f1fa180176de062fb2e5872bdebb89e6162cb606e411fd"
+    sha256 cellar: :any,                 big_sur:       "f8b17942fbb271c70fbe122f0937cd446acd7f86bcd792668f64fbcfb51d5e96"
+    sha256 cellar: :any,                 catalina:      "839bce0f6c0f486583dae83b50c154272b34dc9bd06bd6efd5a43f4fc91a63cd"
+    sha256 cellar: :any,                 mojave:        "80ca7af4ba75bf8c0ae577eb66bfaf16e7cd564c16f0639c6e50df1427fd6452"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "378b71990fdca18bb8ef06dcb4f300980a4360f2ebfe14b88bfca84eed7ed9e9"
   end
 
   depends_on "cmake" => :build
