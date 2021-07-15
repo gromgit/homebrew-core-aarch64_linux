762ee4a1834503c368c86b4ebf6817868172fdd2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:48:36 +0000

    libcouchbase@2: update 2.10.9 bottle.

--- Formula/libcouchbase@2.rb
+++ Formula/libcouchbase@2.rb
@@ -5,10 +5,11 @@ class LibcouchbaseAT2 < Formula
   sha256 "6f6450121e0208005c17f7f4cdd9258a571bb22183f0bc08f11d75c207d55d0a"
   license "Apache-2.0"
   bottle do
-    sha256 arm64_big_sur: "5e0bb389630242e3db72c20cb884c501ddb0886f379adcb55b7db0b889e692f6"
-    sha256 big_sur:       "9ccf1d8d54a3b9bad9da0b4fef45da6320060f4675cc9c1e75a7dea605003ad2"
-    sha256 catalina:      "01b9bceacbd38205745952c98a7db51cc388049da2a950d26eab11ab4b8dee64"
-    sha256 mojave:        "fddb24ff1b03d1dbf91f2ac46096ebd113e5dde208e49430ac55de783fa9f897"
+    sha256                               arm64_big_sur: "5e0bb389630242e3db72c20cb884c501ddb0886f379adcb55b7db0b889e692f6"
+    sha256                               big_sur:       "9ccf1d8d54a3b9bad9da0b4fef45da6320060f4675cc9c1e75a7dea605003ad2"
+    sha256                               catalina:      "01b9bceacbd38205745952c98a7db51cc388049da2a950d26eab11ab4b8dee64"
+    sha256                               mojave:        "fddb24ff1b03d1dbf91f2ac46096ebd113e5dde208e49430ac55de783fa9f897"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3971935612366e72681c198f51154c76b05429edf559e1efd1b298234899492"
   end
 
   keg_only :versioned_formula
