e4c36bca5e2e3889cf151c0d28cbbfeb83508177
Sam Ford <1584702+samford@users.noreply.github.com>
2021-07-15 16:53:01 -0400

    negfix8: add livecheck

--- Formula/negfix8.rb
+++ Formula/negfix8.rb
@@ -4,6 +4,11 @@ class Negfix8 < Formula
   url "https://sites.google.com/site/negfix/downloads/negfix8.3.tgz"
   sha256 "2f360b0dd16ca986fbaebf5873ee55044cae591546b573bb17797cbf569515bd"
 
+  livecheck do
+    url "https://sites.google.com/site/negfix/downloads"
+    regex(/href=.*?negfix[._-]?v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "dc774cdde317803fe6a9f0b4d63531556e781467b1491407c94fc11509fa0997"
   end
