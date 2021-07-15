b1606fe08fbd5a185f30ad67fbe24ea09adb46cf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-15 09:00:45 +0800

    mbedtls@2 2.27.0 (new formula)
    MbedTLS 3.0 introduced a number of breaking changes, and all dependent
    formulae in Homebrew/core still require this version.

---
+++ Formula/mbedtls@2.rb
@@ -0,0 +1,48 @@
+class MbedtlsAT2 < Formula
+  desc "Cryptographic & SSL/TLS library"
+  homepage "https://tls.mbed.org/"
+  url "https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.27.0.tar.gz"
+  sha256 "4f6a43f06ded62aa20ef582436a39b65902e1126cbbe2fb17f394e9e9a552767"
+  license "Apache-2.0"
+  head "https://github.com/ARMmbed/mbedtls.git", branch: "development_2.x"
+
+  livecheck do
+    url :stable
+    regex(/^v?(2(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "python@3.9" => :build
+
+  def install
+    inreplace "include/mbedtls/config.h" do |s|
+      # enable pthread mutexes
+      s.gsub! "//#define MBEDTLS_THREADING_PTHREAD", "#define MBEDTLS_THREADING_PTHREAD"
+      # allow use of mutexes within mbed TLS
+      s.gsub! "//#define MBEDTLS_THREADING_C", "#define MBEDTLS_THREADING_C"
+    end
+
+    system "cmake", "-DUSE_SHARED_MBEDTLS_LIBRARY=On",
+                    "-DPython3_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3",
+                    *std_cmake_args
+    system "make"
+    system "make", "install"
+
+    # Why does Mbedtls ship with a "Hello World" executable. Let's remove that.
+    rm_f bin/"hello"
+    # Rename benchmark & selftest, which are awfully generic names.
+    mv bin/"benchmark", bin/"mbedtls-benchmark"
+    mv bin/"selftest", bin/"mbedtls-selftest"
+    # Demonstration files shouldn't be in the main bin
+    libexec.install bin/"mpi_demo"
+  end
+
+  test do
+    (testpath/"testfile.txt").write("This is a test file")
+    # Don't remove the space between the checksum and filename. It will break.
+    expected_checksum = "e2d0fe1585a63ec6009c8016ff8dda8b17719a637405a4e23c0ff81339148249  testfile.txt"
+    assert_equal expected_checksum, shell_output("#{bin}/generic_sum SHA256 testfile.txt").strip
+  end
+end
