c99eba5eaf095fd57ab4a42f1dc644e0c94299f1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:46:47 +0000

    libbtbb: update 2020-12-R1 bottle.

--- Formula/libbtbb.rb
+++ Formula/libbtbb.rb
@@ -10,10 +10,11 @@ class Libbtbb < Formula
   head "https://github.com/greatscottgadgets/libbtbb.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3f24dbcd8136188fe1ae8d8ff41dc5f228674b25bd0b4cd81c37a5e67528212d"
-    sha256 cellar: :any, big_sur:       "49bf40e0711721bffda654f8d1bb61912cb130b64dcee74d05918a70079d5caa"
-    sha256 cellar: :any, catalina:      "eaed5fcee578de521f01fca38fa0975b5c0f56a80f9391840bd7f815550d9f75"
-    sha256 cellar: :any, mojave:        "8378b76f21bc170c9def4e0fab0d7af8caaccb1afe4fd59e6e9fc3a1cd549c7a"
+    sha256 cellar: :any,                 arm64_big_sur: "3f24dbcd8136188fe1ae8d8ff41dc5f228674b25bd0b4cd81c37a5e67528212d"
+    sha256 cellar: :any,                 big_sur:       "49bf40e0711721bffda654f8d1bb61912cb130b64dcee74d05918a70079d5caa"
+    sha256 cellar: :any,                 catalina:      "eaed5fcee578de521f01fca38fa0975b5c0f56a80f9391840bd7f815550d9f75"
+    sha256 cellar: :any,                 mojave:        "8378b76f21bc170c9def4e0fab0d7af8caaccb1afe4fd59e6e9fc3a1cd549c7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85cfb3e1de889506ec89c584a3f11c013bd9699cc8c2cb3bdb321a82c5e6611c"
   end
 
   depends_on "cmake" => :build
