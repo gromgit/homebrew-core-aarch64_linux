fd94b074de85d6361fff2577331bc11c614f3afe
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-15 06:20:45 +0000

    hy: update 0.20.0 bottle.

--- Formula/hy.rb
+++ Formula/hy.rb
@@ -12,6 +12,7 @@ class Hy < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b3a8611e5647203bc52944ae8c3a7fb1c105e1dda9d65a6641fef45edfc1f48c"
     sha256 cellar: :any_skip_relocation, catalina:      "b8f9062329e87549cc63f63712e0a5790d9722905fd15765d82f997296f49c58"
     sha256 cellar: :any_skip_relocation, mojave:        "30734f23d8c216671a4eb3b8595288354e8f00f31da0da76f9022595c3daf496"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c7e161e87fe9e19ee309ddfceb1cfb56f9d16394cff58b402b920f75e0ef0bc6"
   end
 
   depends_on "python@3.9"
