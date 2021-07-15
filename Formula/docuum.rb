0c138a59dfe2f45b166b7d98ee1fa9032cfb7410
Stephan Boyer <stephan@stephanboyer.com>
2021-07-14 20:16:45 -0700

    docuum 0.20.2 (new formula)
    Closes #81292.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/docuum.rb
@@ -0,0 +1,28 @@
+class Docuum < Formula
+  desc "Perform least recently used (LRU) eviction of Docker images"
+  homepage "https://github.com/stepchowfun/docuum"
+  url "https://github.com/stepchowfun/docuum/archive/v0.20.2.tar.gz"
+  sha256 "001eb166900b6dba89abc4ed76640455e98090bbca321fadc7811225830f336b"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    started_successfully = false
+
+    Open3.popen3({ "NO_COLOR" => "true" }, "#{bin}/docuum") do |_, _, stderr, wait_thread|
+      stderr.each_line do |line|
+        if line.include?("Performing an initial vacuum on startup…")
+          Process.kill("TERM", wait_thread.pid)
+          started_successfully = true
+        end
+      end
+    end
+
+    assert(started_successfully, "Docuum did not start successfully.")
+  end
+end
