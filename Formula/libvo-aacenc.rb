48a6e4bcff4e3dc256eef5b94726f1a17bd350b4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:42:01 +0000

    libvo-aacenc: update 0.1.3 bottle.

--- Formula/libvo-aacenc.rb
+++ Formula/libvo-aacenc.rb
@@ -11,14 +11,15 @@ class LibvoAacenc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "cfc4dceab7f9a4a3037e00b26e046f310580ee5aa95906396052b91e22c89231"
-    sha256 cellar: :any, big_sur:       "4776106dbfb4f81523750d6540847a16c74c4e97ed8271bb9e5ab592386310b9"
-    sha256 cellar: :any, catalina:      "144e0c345d0567a74aba09cfec49fba8f409e2db5abcee96b598127cc5d722ad"
-    sha256 cellar: :any, mojave:        "099ac7d191241c476ab4ba7375dd05e2d965adc6a7638cc616a99a243cbd077b"
-    sha256 cellar: :any, high_sierra:   "761ecbbaaa2a944d077040692fc62fe2e929ec788ca7e23b3fb25e6ee1b88d3a"
-    sha256 cellar: :any, sierra:        "9430e86c9f25aa9fcccf0a19cc6125c9397c23b311b993b1adf83cbe330cd9b4"
-    sha256 cellar: :any, el_capitan:    "e9a59439f8eec4cdc4d273afb49cbd8f8357862d4d8c7c5d9d9d38588ec6d810"
-    sha256 cellar: :any, yosemite:      "cf63ddcb79e40b79264507393ed4fa1b223feecf4638f0e58fef464db722b554"
+    sha256 cellar: :any,                 arm64_big_sur: "cfc4dceab7f9a4a3037e00b26e046f310580ee5aa95906396052b91e22c89231"
+    sha256 cellar: :any,                 big_sur:       "4776106dbfb4f81523750d6540847a16c74c4e97ed8271bb9e5ab592386310b9"
+    sha256 cellar: :any,                 catalina:      "144e0c345d0567a74aba09cfec49fba8f409e2db5abcee96b598127cc5d722ad"
+    sha256 cellar: :any,                 mojave:        "099ac7d191241c476ab4ba7375dd05e2d965adc6a7638cc616a99a243cbd077b"
+    sha256 cellar: :any,                 high_sierra:   "761ecbbaaa2a944d077040692fc62fe2e929ec788ca7e23b3fb25e6ee1b88d3a"
+    sha256 cellar: :any,                 sierra:        "9430e86c9f25aa9fcccf0a19cc6125c9397c23b311b993b1adf83cbe330cd9b4"
+    sha256 cellar: :any,                 el_capitan:    "e9a59439f8eec4cdc4d273afb49cbd8f8357862d4d8c7c5d9d9d38588ec6d810"
+    sha256 cellar: :any,                 yosemite:      "cf63ddcb79e40b79264507393ed4fa1b223feecf4638f0e58fef464db722b554"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "baed718396b9a19dea7de513f7991b72afcb2aae17504a7bbbaf9f0a40d355e4"
   end
 
   def install
