c908d3b580a5ce87be606760fb938d3ce63d2b16
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:38:35 +0000

    libstatgrab: update 0.92 bottle.

--- Formula/libstatgrab.rb
+++ Formula/libstatgrab.rb
@@ -7,12 +7,13 @@ class Libstatgrab < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8aaf22584aedf2494b757faa7aa85b3a449b7b0a1f1e1bca3ac099e880e49248"
-    sha256 cellar: :any, big_sur:       "116a88f47d3d5125d68fdd30918f70abfba59ab7e31b2df71698bbaa0673616a"
-    sha256 cellar: :any, catalina:      "d3a41dfe112e21467ce51134b576e14678f982f1c838b6b624d96ad46edc7c88"
-    sha256 cellar: :any, mojave:        "bb1778c08b1b91cff873016e3a6f314d3a97a55db378e0870354bb64337ea50b"
-    sha256 cellar: :any, high_sierra:   "d7d932298fe68980389bf5b2c8f1d6ef41a6037630b4951996139c2277fbf6f4"
-    sha256 cellar: :any, sierra:        "17efc663227f42859add13c81e0b5fac1f3f3a0418c3d15b83363ea90c0b4a91"
+    sha256 cellar: :any,                 arm64_big_sur: "8aaf22584aedf2494b757faa7aa85b3a449b7b0a1f1e1bca3ac099e880e49248"
+    sha256 cellar: :any,                 big_sur:       "116a88f47d3d5125d68fdd30918f70abfba59ab7e31b2df71698bbaa0673616a"
+    sha256 cellar: :any,                 catalina:      "d3a41dfe112e21467ce51134b576e14678f982f1c838b6b624d96ad46edc7c88"
+    sha256 cellar: :any,                 mojave:        "bb1778c08b1b91cff873016e3a6f314d3a97a55db378e0870354bb64337ea50b"
+    sha256 cellar: :any,                 high_sierra:   "d7d932298fe68980389bf5b2c8f1d6ef41a6037630b4951996139c2277fbf6f4"
+    sha256 cellar: :any,                 sierra:        "17efc663227f42859add13c81e0b5fac1f3f3a0418c3d15b83363ea90c0b4a91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "744583871ace1afba9dcad1f6b1f6548b7c9472cb33ef0db4ef99074d37d34f4"
   end
 
   def install
