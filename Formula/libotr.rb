e7fde10b433bc1b2a69ba590c82234968f4553e5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:35:34 +0000

    libotr: update 4.1.1 bottle.

--- Formula/libotr.rb
+++ Formula/libotr.rb
@@ -11,14 +11,15 @@ class Libotr < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d3f0dce4e18d75daa3563be158054d7f5c92f4de80e5169dfaf7f9b785deed8d"
-    sha256 cellar: :any, big_sur:       "783eead021ab3ddb3897f8c40e91bd6aec58e5084ef1df3c45a98550591ad29c"
-    sha256 cellar: :any, catalina:      "b841026a4752756107affe9f6016da14ea5a9a0a48b33ccd461eced5cd89b64a"
-    sha256 cellar: :any, mojave:        "90da0033157a7771cf7239d038f36e0d613616f1918a168fa763f3e2eafc0106"
-    sha256 cellar: :any, high_sierra:   "0b340441feba4b325c3ff5c26a9e79b16294461f6f681ae42a2a5d45966e7391"
-    sha256 cellar: :any, sierra:        "9f0b214278e4cdf81a1a0c083f1aa45ba64430b449121c4d0596357952dcc93d"
-    sha256 cellar: :any, el_capitan:    "43d7a166cd12b611e7bf15dfa3865d18e573a81a218e2aeb0061d51203ecde39"
-    sha256 cellar: :any, yosemite:      "b3f215fd3952f7a97af36500365c3c017f23de107162f4c76b0e48355bd2a358"
+    sha256 cellar: :any,                 arm64_big_sur: "d3f0dce4e18d75daa3563be158054d7f5c92f4de80e5169dfaf7f9b785deed8d"
+    sha256 cellar: :any,                 big_sur:       "783eead021ab3ddb3897f8c40e91bd6aec58e5084ef1df3c45a98550591ad29c"
+    sha256 cellar: :any,                 catalina:      "b841026a4752756107affe9f6016da14ea5a9a0a48b33ccd461eced5cd89b64a"
+    sha256 cellar: :any,                 mojave:        "90da0033157a7771cf7239d038f36e0d613616f1918a168fa763f3e2eafc0106"
+    sha256 cellar: :any,                 high_sierra:   "0b340441feba4b325c3ff5c26a9e79b16294461f6f681ae42a2a5d45966e7391"
+    sha256 cellar: :any,                 sierra:        "9f0b214278e4cdf81a1a0c083f1aa45ba64430b449121c4d0596357952dcc93d"
+    sha256 cellar: :any,                 el_capitan:    "43d7a166cd12b611e7bf15dfa3865d18e573a81a218e2aeb0061d51203ecde39"
+    sha256 cellar: :any,                 yosemite:      "b3f215fd3952f7a97af36500365c3c017f23de107162f4c76b0e48355bd2a358"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6aa94cf47042090f80a21474fe5e29203fa5cda8e10900b1dc1d4065f93e716f"
   end
 
   depends_on "libgcrypt"
