fad384db1622684ccfe775897c1f61d380a1cc3b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 19:48:10 +0000

    mailcheck: update 1.91.2 bottle.

--- Formula/mailcheck.rb
+++ Formula/mailcheck.rb
@@ -6,13 +6,14 @@ class Mailcheck < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "59d3c8716efff8670b81cec68c47b0663ffa079938ee6aae55078770564fa481"
-    sha256 cellar: :any_skip_relocation, catalina:    "66fa586c21ec0cd9a842fcb99e8bbf822681c8858b864b14aa7d57ea89c47a99"
-    sha256 cellar: :any_skip_relocation, mojave:      "7ea23945f9750c34d71ff05c5f41c0f5352e3eecaf1c7cf485d4f51096b9dd4e"
-    sha256 cellar: :any_skip_relocation, high_sierra: "c630704fee3dea86402e7486295a13601077bd991e45f23d3ac841c95a9c4474"
-    sha256 cellar: :any_skip_relocation, sierra:      "8d33e3b08eef4dfaa7fa3d2c4e5f4a697cd2e5eb950c963f1f0845c0651da5ea"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "b7c134dc23431dfaa3f402b859b7154cab5e176711363bd884dc82ce896d7c7a"
-    sha256 cellar: :any_skip_relocation, yosemite:    "242b05a6e9b8ccc1ac70e22cbf89bc33a885e726d32509fad6b34a3bee123945"
+    sha256 cellar: :any_skip_relocation, big_sur:      "59d3c8716efff8670b81cec68c47b0663ffa079938ee6aae55078770564fa481"
+    sha256 cellar: :any_skip_relocation, catalina:     "66fa586c21ec0cd9a842fcb99e8bbf822681c8858b864b14aa7d57ea89c47a99"
+    sha256 cellar: :any_skip_relocation, mojave:       "7ea23945f9750c34d71ff05c5f41c0f5352e3eecaf1c7cf485d4f51096b9dd4e"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "c630704fee3dea86402e7486295a13601077bd991e45f23d3ac841c95a9c4474"
+    sha256 cellar: :any_skip_relocation, sierra:       "8d33e3b08eef4dfaa7fa3d2c4e5f4a697cd2e5eb950c963f1f0845c0651da5ea"
+    sha256 cellar: :any_skip_relocation, el_capitan:   "b7c134dc23431dfaa3f402b859b7154cab5e176711363bd884dc82ce896d7c7a"
+    sha256 cellar: :any_skip_relocation, yosemite:     "242b05a6e9b8ccc1ac70e22cbf89bc33a885e726d32509fad6b34a3bee123945"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "84fa4f1d288f0f8824334bb68621b8589b65e0d9e21a4ca0961a33aae5d0ef63"
   end
 
   def install
