742f7199fcce3564f6beabbb0c24f8cc92293145
Alexander M. Turek <me@derrabus.de>
2021-07-16 15:07:42 +0200

    psalm 4.8.1 (new formula)
    Closes #81367.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/psalm.rb
@@ -0,0 +1,91 @@
+class Psalm < Formula
+  desc "PHP Static Analysis Tool"
+  homepage "https://psalm.dev"
+  url "https://github.com/vimeo/psalm/releases/download/4.8.1/psalm.phar"
+  sha256 "896246540c669c8d21e62e7c3865bc23cf1e7980caa920cd04b4e38a60354faf"
+  license "MIT"
+
+  # Keg-relocation breaks the formula when it replaces `/usr/local` with a non-default prefix
+  pour_bottle? do
+    on_macos do
+      reason "The bottle needs to be installed into `#{Homebrew::DEFAULT_PREFIX}` on Intel macOS."
+      satisfy { HOMEBREW_PREFIX.to_s == Homebrew::DEFAULT_PREFIX || Hardware::CPU.arm? }
+    end
+  end
+
+  depends_on "composer" => :test
+
+  uses_from_macos "php"
+
+  def install
+    bin.install "psalm.phar" => "psalm"
+  end
+
+  test do
+    (testpath/"composer.json").write <<~EOS
+      {
+        "name": "homebrew/psalm-test",
+        "description": "Testing if Psalm has been installed properly.",
+        "type": "project",
+        "require": {
+          "php": ">=7.1.3"
+        },
+        "license": "MIT",
+        "autoload": {
+          "psr-4": {
+            "Homebrew\\\\PsalmTest\\\\": "src/"
+          }
+        },
+        "minimum-stability": "stable"
+      }
+    EOS
+
+    (testpath/"src/Email.php").write <<~EOS
+      <?php
+      declare(strict_types=1);
+
+      namespace Homebrew\\PsalmTest;
+
+      final class Email
+      {
+        private string $email;
+
+        private function __construct(string $email)
+        {
+          $this->ensureIsValidEmail($email);
+
+          $this->email = $email;
+        }
+
+        public static function fromString(string $email): self
+        {
+          return new self($email);
+        }
+
+        public function __toString(): string
+        {
+          return $this->email;
+        }
+
+        private function ensureIsValidEmail(string $email): void
+        {
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \\InvalidArgumentException(
+              sprintf(
+                '"%s" is not a valid email address',
+                $email
+              )
+            );
+          }
+        }
+      }
+    EOS
+
+    system "composer", "install"
+
+    assert_match "Config file created successfully. Please re-run psalm.",
+                 shell_output("#{bin}/psalm --init")
+    assert_match "No errors found!",
+                 shell_output("#{bin}/psalm")
+  end
+end
