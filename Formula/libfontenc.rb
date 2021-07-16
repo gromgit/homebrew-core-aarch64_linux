e2cdb40fbcf0278602ccff60da13b10e3afcaada
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:28:13 +0000

    libfontenc: update 1.1.4 bottle.

--- Formula/libfontenc.rb
+++ Formula/libfontenc.rb
@@ -6,11 +6,12 @@ class Libfontenc < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "113e1edd38c2ae3c0ca865e3ac90a7bb81e232855513fa598d08514fffde02cb"
-    sha256 cellar: :any, big_sur:       "05f64e556ed6c0576407b084aba036ff0fed95f831778ad8b6363b4fe6f0836e"
-    sha256 cellar: :any, catalina:      "b57f18c5d875f7ceded9115cd1971be92a3c3887c7c606ff5028ea1ddb160b1c"
-    sha256 cellar: :any, mojave:        "e79ca92c5f40a57da0b5745df2eb64fe532e90bbf292e4d97a4703d5b0e15791"
-    sha256 cellar: :any, high_sierra:   "83bf803c71ed38edf5204ee3bdd89f6569c4c04a41c64003f20ab67b7021f2f2"
+    sha256 cellar: :any,                 arm64_big_sur: "113e1edd38c2ae3c0ca865e3ac90a7bb81e232855513fa598d08514fffde02cb"
+    sha256 cellar: :any,                 big_sur:       "05f64e556ed6c0576407b084aba036ff0fed95f831778ad8b6363b4fe6f0836e"
+    sha256 cellar: :any,                 catalina:      "b57f18c5d875f7ceded9115cd1971be92a3c3887c7c606ff5028ea1ddb160b1c"
+    sha256 cellar: :any,                 mojave:        "e79ca92c5f40a57da0b5745df2eb64fe532e90bbf292e4d97a4703d5b0e15791"
+    sha256 cellar: :any,                 high_sierra:   "83bf803c71ed38edf5204ee3bdd89f6569c4c04a41c64003f20ab67b7021f2f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "47c5dabba4eda42bc8fe444660ef37074137c94fe265243628edd344dc353e4a"
   end
 
   depends_on "font-util" => :build
