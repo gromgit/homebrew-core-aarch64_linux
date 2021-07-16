d78986272acb3150b250f6198ac53fdc91d51617
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 19:45:45 +0000

    logswan: update 2.1.10 bottle.

--- Formula/logswan.rb
+++ Formula/logswan.rb
@@ -6,10 +6,11 @@ class Logswan < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "23442a9f241288d9733861abfca7cd6b3f99ce66b8787ec18799810731bba602"
-    sha256 cellar: :any, big_sur:       "68760ddf896f317d44496728987da7255c40f4ccc3612c40fd13dbb33f2d6662"
-    sha256 cellar: :any, catalina:      "b1eab00c396357a4d123a6747827774dab732d6d2e56e85fb9a32c7b22989cc2"
-    sha256 cellar: :any, mojave:        "914eaff19cac1e0e58bf0d61fc23d089f69b405e7d35d95b818ef0b6c2968840"
+    sha256 cellar: :any,                 arm64_big_sur: "23442a9f241288d9733861abfca7cd6b3f99ce66b8787ec18799810731bba602"
+    sha256 cellar: :any,                 big_sur:       "68760ddf896f317d44496728987da7255c40f4ccc3612c40fd13dbb33f2d6662"
+    sha256 cellar: :any,                 catalina:      "b1eab00c396357a4d123a6747827774dab732d6d2e56e85fb9a32c7b22989cc2"
+    sha256 cellar: :any,                 mojave:        "914eaff19cac1e0e58bf0d61fc23d089f69b405e7d35d95b818ef0b6c2968840"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b52e7243534aaa15c17a110660d12984d5f89b5ca924994ec20540da2a09caca"
   end
 
   depends_on "cmake" => :build
