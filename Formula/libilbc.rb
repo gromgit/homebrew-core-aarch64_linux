7e343a37d6a3772e03bee8de6e409b25f9f18c11
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:30:10 +0000

    libilbc: update 3.0.4 bottle.

--- Formula/libilbc.rb
+++ Formula/libilbc.rb
@@ -6,10 +6,11 @@ class Libilbc < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7b07dbf92042eb0f0692aec0381561eaa0a9c649347fd321ebf74cd22994813d"
-    sha256 cellar: :any, big_sur:       "affe65f4320a2940b69ec54687be6c5387e51d79f3fd418a5dc42924c99eeee0"
-    sha256 cellar: :any, catalina:      "b75ace51e88894a45e406c7fbe4b4cafc06932b0e5ce90480fdee203aa9ede83"
-    sha256 cellar: :any, mojave:        "496492e1aaecb1b41ba83eb033b75777ca08333edbb9e67bef23c933b5847cd5"
+    sha256 cellar: :any,                 arm64_big_sur: "7b07dbf92042eb0f0692aec0381561eaa0a9c649347fd321ebf74cd22994813d"
+    sha256 cellar: :any,                 big_sur:       "affe65f4320a2940b69ec54687be6c5387e51d79f3fd418a5dc42924c99eeee0"
+    sha256 cellar: :any,                 catalina:      "b75ace51e88894a45e406c7fbe4b4cafc06932b0e5ce90480fdee203aa9ede83"
+    sha256 cellar: :any,                 mojave:        "496492e1aaecb1b41ba83eb033b75777ca08333edbb9e67bef23c933b5847cd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11914a90369fc2c32d1373c3c8d5a98275c44de6e70301c23067b67310d6bba0"
   end
 
   depends_on "cmake" => :build
