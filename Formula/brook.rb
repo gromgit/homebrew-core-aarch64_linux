927bd0748ea5fed0c13ecf6313e29f6d93bf043b
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-16 19:27:31 +0000

    brook: update 20210401 bottle.

--- Formula/brook.rb
+++ Formula/brook.rb
@@ -10,6 +10,7 @@ class Brook < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5b0661f65db2b397e6dcf36b84b649e383835cf38df778a54ba95d9c8239dfd2"
     sha256 cellar: :any_skip_relocation, catalina:      "d3f484395c5fbf6c218b359ab37be696be4c8277b5cdd8b1441950755f62cac1"
     sha256 cellar: :any_skip_relocation, mojave:        "082279557c3e15d460334a8a7f5f0f028bd7ec0aa7328a7cd7e5149a4ce029c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "062e4fbf5db1df0bbe66de05086a0bd054047a4a1dcc1870bc36e30024d884ea"
   end
 
   depends_on "go" => :build
