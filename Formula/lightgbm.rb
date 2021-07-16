f1c2c7dfbc98fd7a2fbca916b3db005c2d010647
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:45:49 +0000

    lightgbm: update 3.2.1 bottle.

--- Formula/lightgbm.rb
+++ Formula/lightgbm.rb
@@ -7,10 +7,11 @@ class Lightgbm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ae442333df20db31769e25fedd3b32a4238eaa2a94f178c237fc567373f2a245"
-    sha256 cellar: :any, big_sur:       "84e713ec15bd966c737fc70b5650a725a21f3551df897a6160b2f27d8561d3a1"
-    sha256 cellar: :any, catalina:      "1c9178a701a721781329f0c4dc45bc955f84a5ee93f6f68fe817ae33168adcab"
-    sha256 cellar: :any, mojave:        "f714a735772ed4246dbf5bfff4c6c1f1b4d1cf36680488b16e80c19a1e3d3334"
+    sha256 cellar: :any,                 arm64_big_sur: "ae442333df20db31769e25fedd3b32a4238eaa2a94f178c237fc567373f2a245"
+    sha256 cellar: :any,                 big_sur:       "84e713ec15bd966c737fc70b5650a725a21f3551df897a6160b2f27d8561d3a1"
+    sha256 cellar: :any,                 catalina:      "1c9178a701a721781329f0c4dc45bc955f84a5ee93f6f68fe817ae33168adcab"
+    sha256 cellar: :any,                 mojave:        "f714a735772ed4246dbf5bfff4c6c1f1b4d1cf36680488b16e80c19a1e3d3334"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9bbae9728fa5306d0f12cdabf901c5b64df6a6a73ec9d0547e5c1290a751241a"
   end
 
   depends_on "cmake" => :build
