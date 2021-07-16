754b8a76ae6cdfc326e569d42e370f9484718e31
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 19:49:32 +0000

    loudmouth: update 1.5.4 bottle.

--- Formula/loudmouth.rb
+++ Formula/loudmouth.rb
@@ -6,10 +6,11 @@ class Loudmouth < Formula
   license "LGPL-2.1"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0b60046b8a592ab656ed824b75774f2e9e8f9749b0a5edb024190019c36da766"
-    sha256 cellar: :any, big_sur:       "d770f0cd1a81375c306d0bc6fdd81610d27bc844fd5086518aaa7f8fa6252a14"
-    sha256 cellar: :any, catalina:      "b83be4ad6fce30f484015b344d21e3e425860b3c8a2cb6a609e059611d03caf9"
-    sha256 cellar: :any, mojave:        "681944a95c5642a4651110e5d91d88acf335176b34d85f0f159aef291f07b38d"
+    sha256 cellar: :any,                 arm64_big_sur: "0b60046b8a592ab656ed824b75774f2e9e8f9749b0a5edb024190019c36da766"
+    sha256 cellar: :any,                 big_sur:       "d770f0cd1a81375c306d0bc6fdd81610d27bc844fd5086518aaa7f8fa6252a14"
+    sha256 cellar: :any,                 catalina:      "b83be4ad6fce30f484015b344d21e3e425860b3c8a2cb6a609e059611d03caf9"
+    sha256 cellar: :any,                 mojave:        "681944a95c5642a4651110e5d91d88acf335176b34d85f0f159aef291f07b38d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6052693231034f7a87a85eb4e56755786089171b9ce5ad1e0babe0a891af55d9"
   end
 
   head do
