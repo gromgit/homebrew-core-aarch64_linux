16acaf3a658b6d7dfbd2f1dd73dc086ed331da6e
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-16 19:28:01 +0000

    cadical: update 1.4.1 bottle.

--- Formula/cadical.rb
+++ Formula/cadical.rb
@@ -15,6 +15,7 @@ class Cadical < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "fc095f38bfc3402adecdb2e33f76353ef95248f8bf25cd1731fc89c93070f7f1"
     sha256 cellar: :any_skip_relocation, catalina:      "1a5c3d28eeaf54b30f08b4ab5f27bf9c0ac366d4cb4f5003fe04d71ef11a26e2"
     sha256 cellar: :any_skip_relocation, mojave:        "c88a8df9110d4996b3f5bd00f3227e6031aeaf89c8ad652d5b8412f7ef441dd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a316fb060adcad424dfa1f09b5c7d4ca40b5ead8aec27422478e9a26b6216069"
   end
 
   def install
