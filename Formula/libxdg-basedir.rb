244d6fa128b06bd6343e11164ddec964939ce27d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:42:08 +0000

    libxdg-basedir: update 1.2.3 bottle.

--- Formula/libxdg-basedir.rb
+++ Formula/libxdg-basedir.rb
@@ -6,10 +6,11 @@ class LibxdgBasedir < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "45fb150350fc290277e2fe56f834065d7694aa1018bc2ffaf2b4a22f20962212"
-    sha256 cellar: :any, big_sur:       "815e73cfc0be4d8091e83b4083bd583e2514a4768c553480d70be1a3e21d77c2"
-    sha256 cellar: :any, catalina:      "dc5854179a0d219e058f13e294625d4ebd755e82ca2302ce462b33f75d8113c0"
-    sha256 cellar: :any, mojave:        "228bec555704181d31f3f0baf6d95a5839483d4e16374eef5f91063e29f4e89b"
+    sha256 cellar: :any,                 arm64_big_sur: "45fb150350fc290277e2fe56f834065d7694aa1018bc2ffaf2b4a22f20962212"
+    sha256 cellar: :any,                 big_sur:       "815e73cfc0be4d8091e83b4083bd583e2514a4768c553480d70be1a3e21d77c2"
+    sha256 cellar: :any,                 catalina:      "dc5854179a0d219e058f13e294625d4ebd755e82ca2302ce462b33f75d8113c0"
+    sha256 cellar: :any,                 mojave:        "228bec555704181d31f3f0baf6d95a5839483d4e16374eef5f91063e29f4e89b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d160ef91d8ec2c931defacad6ff525a7e1336f42e8196e0625bc93a266e54f72"
   end
 
   depends_on "autoconf" => :build
