755c0c0b2c5f204d8e2ccdd25a0aa5f2cf3c4a46
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-16 23:49:43 +0200

    packer: set PACKER_PLUGIN_PATH to prefix
    Closes #81391.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -31,6 +31,9 @@ class Packer < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
+    # Allow packer to find plugins in Homebrew prefix
+    bin.env_script_all_files libexec/"bin", PACKER_PLUGIN_PATH: "$PACKER_PLUGIN_PATH:#{HOMEBREW_PREFIX/"bin"}"
+
     zsh_completion.install "contrib/zsh-completion/_packer"
   end
 
