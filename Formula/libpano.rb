ab4adcd060b7226505877c894027497f994201d1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:35:33 +0000

    libpano: update 13-2.9.20 bottle.

--- Formula/libpano.rb
+++ Formula/libpano.rb
@@ -11,9 +11,10 @@ class Libpano < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:  "b1cb70b0d3ec17309a8c71f4d30ead3cee9c72f4efd8d15b85c9a5821de6fea6"
-    sha256 cellar: :any, catalina: "07de3b8c00569f6d7fe5c813eec7e72708ee12022d85003a64c0959d87057a1e"
-    sha256 cellar: :any, mojave:   "8ed168e1c4b45fdc7815d6c275c0831f3b8450481cbd6b8ff8654d84f0486cba"
+    sha256 cellar: :any,                 big_sur:      "b1cb70b0d3ec17309a8c71f4d30ead3cee9c72f4efd8d15b85c9a5821de6fea6"
+    sha256 cellar: :any,                 catalina:     "07de3b8c00569f6d7fe5c813eec7e72708ee12022d85003a64c0959d87057a1e"
+    sha256 cellar: :any,                 mojave:       "8ed168e1c4b45fdc7815d6c275c0831f3b8450481cbd6b8ff8654d84f0486cba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4c9f6180154f15419ba4f2486eddd5f110098ebc3150b2007f867e9459c24b4b"
   end
 
   depends_on "jpeg"
