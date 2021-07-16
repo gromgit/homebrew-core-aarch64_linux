8ef9896d846724a8a55d9102afdea7db4caa5cad
Caleb Xu <calebcenter@live.com>
2021-07-16 10:58:15 -0400

    rhash 1.4.2
    Closes #81385.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rhash.rb
+++ Formula/rhash.rb
@@ -1,8 +1,8 @@
 class Rhash < Formula
   desc "Utility for computing and verifying hash sums of files"
   homepage "https://sourceforge.net/projects/rhash/"
-  url "https://downloads.sourceforge.net/project/rhash/rhash/1.4.1/rhash-1.4.1-src.tar.gz"
-  sha256 "430c812733e69b78f07ce30a05db69563450e41e217ae618507a4ce2e144a297"
+  url "https://downloads.sourceforge.net/project/rhash/rhash/1.4.2/rhash-1.4.2-src.tar.gz"
+  sha256 "600d00f5f91ef04194d50903d3c79412099328c42f28ff43a0bdb777b00bec62"
   license "0BSD"
   head "https://github.com/rhash/RHash.git"
 
@@ -13,8 +13,15 @@ class Rhash < Formula
     sha256 mojave:        "87ac3199498088f7d465dafefc6f014e10b4692ed3997895bbf1eb288dce8cdd"
   end
 
+  # configure: fix clang detection on macOS
+  # Patch accepted and merged upstream, remove on next release
+  patch do
+    url "https://github.com/rhash/RHash/commit/4dc506066cf1727b021e6352535a8bb315c3f8dc.patch?full_index=1"
+    sha256 "3fbfe4603d2ec5228fd198fc87ff3ee281e1f68d252c1afceaa15cba76e9b6b4"
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-gettext"
     system "make"
     system "make", "install"
     lib.install "librhash/#{shared_library("librhash")}"
