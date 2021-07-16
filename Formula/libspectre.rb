42f082b77a3560725d3caae095a0523c00c4ce24
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:39:25 +0000

    libspectre: update 0.2.9_2 bottle.

--- Formula/libspectre.rb
+++ Formula/libspectre.rb
@@ -12,10 +12,11 @@ class Libspectre < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "569048e256adf2200f0d78bb9e0e69124bfc95d048942a7e7302021d4f26a725"
-    sha256 cellar: :any, big_sur:       "dd24fcf3da06f8020b4c5439ca40d1b2eb17a518b62f205e8d34d4e259b0fd7e"
-    sha256 cellar: :any, catalina:      "b6010400519844d7e3c0ead42e613279dcf301b69775bbc75f8e93c2cadf797b"
-    sha256 cellar: :any, mojave:        "03c755d7c557ed3b53e73f398be9d2a04fd87143fa91d8dcbb010d8a36f313a0"
+    sha256 cellar: :any,                 arm64_big_sur: "569048e256adf2200f0d78bb9e0e69124bfc95d048942a7e7302021d4f26a725"
+    sha256 cellar: :any,                 big_sur:       "dd24fcf3da06f8020b4c5439ca40d1b2eb17a518b62f205e8d34d4e259b0fd7e"
+    sha256 cellar: :any,                 catalina:      "b6010400519844d7e3c0ead42e613279dcf301b69775bbc75f8e93c2cadf797b"
+    sha256 cellar: :any,                 mojave:        "03c755d7c557ed3b53e73f398be9d2a04fd87143fa91d8dcbb010d8a36f313a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9d02c4c372a70e15e3a180fc9f8abd855ec7d15086b1d3969aad657735156d0"
   end
 
   depends_on "ghostscript"
