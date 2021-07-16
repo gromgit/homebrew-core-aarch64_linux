98432a8dbffe297615fc4b6b5a4129c7e2f72842
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:25:34 +0000

    libdvdread: update 6.1.2 bottle.

--- Formula/libdvdread.rb
+++ Formula/libdvdread.rb
@@ -6,10 +6,11 @@ class Libdvdread < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d3e2a48d9d4f4e1e39a68645094be1536cae40fa0c2c6cff7907ec408d3c4439"
-    sha256 cellar: :any, big_sur:       "7d0a516f36d885df836072671f8885218a1684fbab69dad9761629ed87483640"
-    sha256 cellar: :any, catalina:      "08b72c46e9022170d991a626a84a073bd988f99db12bc145494306fc28f33d73"
-    sha256 cellar: :any, mojave:        "ddd7ecdfc66b0b12b4804550aaab8939f2bbd30bcd753ac9006f3db079589515"
+    sha256 cellar: :any,                 arm64_big_sur: "d3e2a48d9d4f4e1e39a68645094be1536cae40fa0c2c6cff7907ec408d3c4439"
+    sha256 cellar: :any,                 big_sur:       "7d0a516f36d885df836072671f8885218a1684fbab69dad9761629ed87483640"
+    sha256 cellar: :any,                 catalina:      "08b72c46e9022170d991a626a84a073bd988f99db12bc145494306fc28f33d73"
+    sha256 cellar: :any,                 mojave:        "ddd7ecdfc66b0b12b4804550aaab8939f2bbd30bcd753ac9006f3db079589515"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40dc2e4bc0128e1468141d4b3805a4c135576101ffdcf8872c5787291b15bdd5"
   end
 
   head do
