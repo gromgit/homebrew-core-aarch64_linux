716ac14bd5754bb9d7597fb76f66e7241db79ea5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:45:34 +0000

    lm4tools: update 0.1.3 bottle.

--- Formula/lm4tools.rb
+++ Formula/lm4tools.rb
@@ -6,12 +6,13 @@ class Lm4tools < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "2fca09b10fef4d8304ba4acdce164bbfc5f4fa9b8dd1eb6fcb60b8a58c7ac8d3"
-    sha256 cellar: :any, catalina:    "5d2e503a9c94226f9d3c6d1da1a54424be1c9a16279bcc94253ab0e2da2a3718"
-    sha256 cellar: :any, mojave:      "a0bb88705b97875de770b1979b5480521007b25efd627f092e178941b8ecd4ec"
-    sha256 cellar: :any, high_sierra: "9c65eb6694f74b513b707c237cf13bb6a54b9e4a188582355f78e94f9ac53407"
-    sha256 cellar: :any, sierra:      "3238455d6329e9749700b9c12c2e7459b63ea400fb0e7e6818b8c7c9b77b4e6d"
-    sha256 cellar: :any, el_capitan:  "7c6bd7ec1a220de95089d71f79baa61ce459ffa0d00d32af727435594ac7603a"
+    sha256 cellar: :any,                 big_sur:      "2fca09b10fef4d8304ba4acdce164bbfc5f4fa9b8dd1eb6fcb60b8a58c7ac8d3"
+    sha256 cellar: :any,                 catalina:     "5d2e503a9c94226f9d3c6d1da1a54424be1c9a16279bcc94253ab0e2da2a3718"
+    sha256 cellar: :any,                 mojave:       "a0bb88705b97875de770b1979b5480521007b25efd627f092e178941b8ecd4ec"
+    sha256 cellar: :any,                 high_sierra:  "9c65eb6694f74b513b707c237cf13bb6a54b9e4a188582355f78e94f9ac53407"
+    sha256 cellar: :any,                 sierra:       "3238455d6329e9749700b9c12c2e7459b63ea400fb0e7e6818b8c7c9b77b4e6d"
+    sha256 cellar: :any,                 el_capitan:   "7c6bd7ec1a220de95089d71f79baa61ce459ffa0d00d32af727435594ac7603a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5afa96bcdab63596529b9202a6c985a4a1cd634235b2a3ab099805046384e405"
   end
 
   depends_on "pkg-config" => :build
