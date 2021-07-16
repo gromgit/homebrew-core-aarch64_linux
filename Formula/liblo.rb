ca87ed7bbdd6a2094edcbcb8cd2fdb8efeecf186
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:30:04 +0000

    liblo: update 0.31 bottle.

--- Formula/liblo.rb
+++ Formula/liblo.rb
@@ -6,11 +6,12 @@ class Liblo < Formula
   license "LGPL-2.1"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "95b358e3f04623998f6c2d734599ec7e63b3c389f9d6e0cc9fc6311850929f55"
-    sha256 cellar: :any, big_sur:       "19eef0619f05faa15a7d5368973dcd3e5ed2e44291b56cc6ff72825fe8879845"
-    sha256 cellar: :any, catalina:      "aac4280d5e147a6baab53c252bbf7cda296fe5bdeceb26d7aa60acb10ecc5444"
-    sha256 cellar: :any, mojave:        "3310110ec91fb412b8d5c727bda03454aebec087d78ebada20bb53ad9582088e"
-    sha256 cellar: :any, high_sierra:   "034eaec236ee4df490d16db9998ec7a4d88223d929b333c8b08ade641bc74bcb"
+    sha256 cellar: :any,                 arm64_big_sur: "95b358e3f04623998f6c2d734599ec7e63b3c389f9d6e0cc9fc6311850929f55"
+    sha256 cellar: :any,                 big_sur:       "19eef0619f05faa15a7d5368973dcd3e5ed2e44291b56cc6ff72825fe8879845"
+    sha256 cellar: :any,                 catalina:      "aac4280d5e147a6baab53c252bbf7cda296fe5bdeceb26d7aa60acb10ecc5444"
+    sha256 cellar: :any,                 mojave:        "3310110ec91fb412b8d5c727bda03454aebec087d78ebada20bb53ad9582088e"
+    sha256 cellar: :any,                 high_sierra:   "034eaec236ee4df490d16db9998ec7a4d88223d929b333c8b08ade641bc74bcb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4abab6d8b5735e6b1dac973850d9608e71c644255c13b954365398daf8aeec4"
   end
 
   head do
