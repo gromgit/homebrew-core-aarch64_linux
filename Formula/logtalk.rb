fcbc0f11f1cba17e359852eccf650b83328ca793
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 19:45:32 +0000

    logtalk: update 3.48.0 bottle.

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -12,9 +12,10 @@ class Logtalk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "8e9dd986617f15016de9ca3de1ff04c377339d67bafe657cbec9b9efb961f627"
-    sha256 cellar: :any_skip_relocation, catalina: "4fa6488a7be05070b60ca6b2f011ac1a3d6fdfebc9ca2ebfe493caca42a5b2e7"
-    sha256 cellar: :any_skip_relocation, mojave:   "9eb6601db3d44df27539aeca4ba1c2fe55ee9d1365666b9e8fa750e5c9fdccac"
+    sha256 cellar: :any_skip_relocation, big_sur:      "8e9dd986617f15016de9ca3de1ff04c377339d67bafe657cbec9b9efb961f627"
+    sha256 cellar: :any_skip_relocation, catalina:     "4fa6488a7be05070b60ca6b2f011ac1a3d6fdfebc9ca2ebfe493caca42a5b2e7"
+    sha256 cellar: :any_skip_relocation, mojave:       "9eb6601db3d44df27539aeca4ba1c2fe55ee9d1365666b9e8fa750e5c9fdccac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "51f75c13a017af59c9d386e29ec342848de012b91418ef985d4cc76e187856e6"
   end
 
   depends_on "gnu-prolog"
