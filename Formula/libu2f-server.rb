097be54e66587cbd9b5fe6a03226f0cf4608e222
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:41:49 +0000

    libu2f-server: update 1.1.0_3 bottle.

--- Formula/libu2f-server.rb
+++ Formula/libu2f-server.rb
@@ -12,11 +12,12 @@ class Libu2fServer < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f1746f86987f50d9ffaf415b51420dba4dab8e64d620c99d63c6c9aed39d524b"
-    sha256 cellar: :any, big_sur:       "fa0d80a1f6345e7b7323a837ba5acb031f4728eb674c48a590ae1c3b0a6c3a01"
-    sha256 cellar: :any, catalina:      "3a5038a64a9820c04a4ad1067ebcf8076936474cc5c18a0d93f7c986adcf0169"
-    sha256 cellar: :any, mojave:        "a1d26284fa87629ecf5bf965433cd6eeba9eb151e064e22a47ca42a115d5e15c"
-    sha256 cellar: :any, high_sierra:   "03e06751297ad4aab253d7b1f742fd5c2ad8d79b35836eb132c3c82c20b485e6"
+    sha256 cellar: :any,                 arm64_big_sur: "f1746f86987f50d9ffaf415b51420dba4dab8e64d620c99d63c6c9aed39d524b"
+    sha256 cellar: :any,                 big_sur:       "fa0d80a1f6345e7b7323a837ba5acb031f4728eb674c48a590ae1c3b0a6c3a01"
+    sha256 cellar: :any,                 catalina:      "3a5038a64a9820c04a4ad1067ebcf8076936474cc5c18a0d93f7c986adcf0169"
+    sha256 cellar: :any,                 mojave:        "a1d26284fa87629ecf5bf965433cd6eeba9eb151e064e22a47ca42a115d5e15c"
+    sha256 cellar: :any,                 high_sierra:   "03e06751297ad4aab253d7b1f742fd5c2ad8d79b35836eb132c3c82c20b485e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "508085d21397af83fb5393afe912b5ac8804ee180832ef10dbce5d991889e391"
   end
 
   depends_on "check" => :build
