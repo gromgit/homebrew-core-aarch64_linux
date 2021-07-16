2c65885fe73982d26c7fdf8a58535213441af2df
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-16 00:47:39 +0000

    librest: update 0.8.1_3 bottle.

--- Formula/librest.rb
+++ Formula/librest.rb
@@ -6,11 +6,12 @@ class Librest < Formula
   revision 3
 
   bottle do
-    sha256 arm64_big_sur: "a565482a9685164d288df713142f23b91dda71fc9c694ea0c613c64320e4aa0a"
-    sha256 big_sur:       "366b341df1c40a648f2847b5f1c13efccfaeb4c3c9610c30fe7e5b3087d07186"
-    sha256 catalina:      "fb2e698cdf400c3f413a707132acd9b55139e2aa26da2f405f5eaebeace6573b"
-    sha256 mojave:        "dbaf452ac76dbc63e161ffc086aac7f6409614ca573c20fe02bd2e87d473e5b2"
-    sha256 high_sierra:   "5ab2748f8103ff622b6615f6427f21c7f9313b227824bd91429aa6f4c5c9c982"
+    sha256                               arm64_big_sur: "a565482a9685164d288df713142f23b91dda71fc9c694ea0c613c64320e4aa0a"
+    sha256                               big_sur:       "366b341df1c40a648f2847b5f1c13efccfaeb4c3c9610c30fe7e5b3087d07186"
+    sha256                               catalina:      "fb2e698cdf400c3f413a707132acd9b55139e2aa26da2f405f5eaebeace6573b"
+    sha256                               mojave:        "dbaf452ac76dbc63e161ffc086aac7f6409614ca573c20fe02bd2e87d473e5b2"
+    sha256                               high_sierra:   "5ab2748f8103ff622b6615f6427f21c7f9313b227824bd91429aa6f4c5c9c982"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be2f31807cddc48f6979d659856ed2fe5e1d641547e0dca5b8082575bee55952"
   end
 
   depends_on "gobject-introspection" => :build
