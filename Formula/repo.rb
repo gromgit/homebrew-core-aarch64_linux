81a9107134d7d6b5787002c906049596e2809ba1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-16 00:09:57 +0000

    repo: update 2.16.2 bottle.

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -10,7 +10,7 @@ class Repo < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "57ec891368aa640d52a893a271564d008b4fc8e6b43bff76bd1ac65355156a31"
+    sha256 cellar: :any_skip_relocation, all: "5c238b3247900c37b390916082a2acc4de2180465f9c9ac2a9d7eb63116cc2e7"
   end
 
   depends_on "python@3.9"
