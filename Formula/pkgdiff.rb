181315c3f67cbfa7155765bfcd882133ec5a602e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:12:22 +0000

    pkgdiff: update 1.7.2 bottle.

--- Formula/pkgdiff.rb
+++ Formula/pkgdiff.rb
@@ -14,6 +14,7 @@ class Pkgdiff < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "fc92f55909e0499c1d0054100183567465603ac85fa9f8b20d5ab1d84e36ceae"
     sha256 cellar: :any_skip_relocation, el_capitan:    "18895054433b4b050c3a863306c62a910be7fa4e36b0020a742c5c7541c0df65"
     sha256 cellar: :any_skip_relocation, yosemite:      "c566b1a44ed89a1b7b3547adf0a0e0fa784174e1acc4a1dd46a240ea6d09bbff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b6dfddc360e2756ff78483c44796a5b55e84c8ec6b5666615baf29e9f1db891"
   end
 
   depends_on "binutils"
