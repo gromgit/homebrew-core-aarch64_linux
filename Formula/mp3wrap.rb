64155de042d58df5a5d5acf1cba1ac8511acc6ab
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:31:39 +0000

    mp3wrap: update 0.5 bottle.

--- Formula/mp3wrap.rb
+++ Formula/mp3wrap.rb
@@ -18,6 +18,7 @@ class Mp3wrap < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:   "3c85e837e2dbcfcbbccb0b074ebfa9283c13d2453b206c246bc4d77600328dfb"
     sha256 cellar: :any_skip_relocation, sierra:        "0471701ab4f6b59423503b7c250376ba597a9f28d9962f6f9b35a107d58411ab"
     sha256 cellar: :any_skip_relocation, el_capitan:    "c65886799c1397eec33f48ef73774ad6a509fec44a18dec4a50c8755736f040a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e666ba56f6a93702e3a37b4dd6f8d908b6a16246ba9ad5467518c970f4ac30ab"
   end
 
   def install
