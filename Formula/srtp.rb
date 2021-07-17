a8d2b3a7e95e92e93fb42f63cc813e09116df533
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:40:01 +0000

    srtp: update 2.3.0 bottle.

--- Formula/srtp.rb
+++ Formula/srtp.rb
@@ -12,11 +12,12 @@ class Srtp < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "60ba6d403720966a354577c14dbacaa27980f292eb71dec3d32472e0aa2aa659"
-    sha256 cellar: :any, big_sur:       "c7d718635bf47976fd6458c37a3373be978e0254811d5249477a83bf4b885388"
-    sha256 cellar: :any, catalina:      "b96d4c3bb159a6f43d5bdd9cc0be0d8deecb06c95df19f2d9cc1f517ffc64ad6"
-    sha256 cellar: :any, mojave:        "4bbad999b46dd545aa32882e968d441f5d5e709dc8549ef79e3885dd49fcb964"
-    sha256 cellar: :any, high_sierra:   "5c70c41484064bbe25c31a19fc2cffc5cbea3de27e837a039b17767aeb1b57b8"
+    sha256 cellar: :any,                 arm64_big_sur: "60ba6d403720966a354577c14dbacaa27980f292eb71dec3d32472e0aa2aa659"
+    sha256 cellar: :any,                 big_sur:       "c7d718635bf47976fd6458c37a3373be978e0254811d5249477a83bf4b885388"
+    sha256 cellar: :any,                 catalina:      "b96d4c3bb159a6f43d5bdd9cc0be0d8deecb06c95df19f2d9cc1f517ffc64ad6"
+    sha256 cellar: :any,                 mojave:        "4bbad999b46dd545aa32882e968d441f5d5e709dc8549ef79e3885dd49fcb964"
+    sha256 cellar: :any,                 high_sierra:   "5c70c41484064bbe25c31a19fc2cffc5cbea3de27e837a039b17767aeb1b57b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68c5b4e79b015930a283afafcbe90b0c4d39f197fbbe549f7bad1691c10dace2"
   end
 
   depends_on "pkg-config" => :build
