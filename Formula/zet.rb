69e3aece9dbe52dbffa2563e20e96cdfc20defa4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-17 23:09:40 +0000

    zet: add 0.2.0 bottle.

--- Formula/zet.rb
+++ Formula/zet.rb
@@ -5,6 +5,14 @@ class Zet < Formula
   sha256 "b001632ecff545411908a9b365dbac6f930e563233547a4cb0ad210d3066952b"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c6e7d941904683befbbb15613f071a946a144add3cac297592c104d6928dc364"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4515a47a131a1cf5f99a142985563c852115ab85eb9930d763331295bc001c0c"
+    sha256 cellar: :any_skip_relocation, catalina:      "27a2d603392d8f3eb4334524a9806b8138c1ce6016be87c4c17536861e1b1343"
+    sha256 cellar: :any_skip_relocation, mojave:        "4aa63c78cb8fe11496f62fbbc3a8877640273c8171c28fa44be0fd0325b06a76"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5487a78a3b283e20c280db65537f0915d610a60a4ec91650eed85ebd71a9d420"
+  end
+
   depends_on "rust" => :build
 
   def install
