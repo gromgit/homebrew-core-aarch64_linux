a1d487707d9685bdf55a90811589bdaa68c17c7b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:17:14 +0000

    webify: update 1.5.0 bottle.

--- Formula/webify.rb
+++ Formula/webify.rb
@@ -12,6 +12,7 @@ class Webify < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "7b6543358b1c92e8e8cc71584ed52802a039c9327edc839dcc75216fbd23558c"
     sha256 cellar: :any_skip_relocation, mojave:        "8a58b27bcb9d6f9cd611b8f7dfb6192f617854cfcaf8638b388f6dd88ec40f70"
     sha256 cellar: :any_skip_relocation, high_sierra:   "9701f9952fb05880c48c5ca26d14807cf324c2210d4b45d0fb5408243d8d76cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9bfcea38ac0326979bfc1c189dd9bf3c19437a5d92c8cade9a41f3c1fe976d83"
   end
 
   depends_on "go" => :build
