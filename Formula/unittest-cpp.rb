07b9ff3d23798bad3b4ac2a30c505612ff98cf7d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:10:19 +0000

    unittest-cpp: update 2.0.0 bottle.

--- Formula/unittest-cpp.rb
+++ Formula/unittest-cpp.rb
@@ -6,14 +6,15 @@ class UnittestCpp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "462837c9588ccf8f585d9d82af071bb91a59f2bf3ef155ccc863c416491cab68"
-    sha256 cellar: :any, big_sur:       "ea9e79c44e4bc95225504ea78baf0ae87b440f3a555239725672b3b5b205ebc5"
-    sha256 cellar: :any, catalina:      "19a4cef9ba95b37528f2a88d280b0f4c77809d7553a8e5747cfd4e41363f2fce"
-    sha256 cellar: :any, mojave:        "9837dfbba5a3014097d3b406bd48e174a6a788d0c0b3107bd1fabeeb0ce6b89e"
-    sha256 cellar: :any, high_sierra:   "206f44c35a82fac519b64b8c4ae6bc397e360d8404e8279a24b906d7729efed2"
-    sha256 cellar: :any, sierra:        "91d028b464f32fcf6edda6b791be2b70d9b770934edd7af7d2b8ff24e9c5eb06"
-    sha256 cellar: :any, el_capitan:    "6136d8cdc420681130c59d9f77327ddad6b46a35d29da5be760b522c7456e2a2"
-    sha256 cellar: :any, yosemite:      "573f6f3a83ba0d2d0e3b54314a5eb93affd11f03bcc409d381ef8253d7e03c4c"
+    sha256 cellar: :any,                 arm64_big_sur: "462837c9588ccf8f585d9d82af071bb91a59f2bf3ef155ccc863c416491cab68"
+    sha256 cellar: :any,                 big_sur:       "ea9e79c44e4bc95225504ea78baf0ae87b440f3a555239725672b3b5b205ebc5"
+    sha256 cellar: :any,                 catalina:      "19a4cef9ba95b37528f2a88d280b0f4c77809d7553a8e5747cfd4e41363f2fce"
+    sha256 cellar: :any,                 mojave:        "9837dfbba5a3014097d3b406bd48e174a6a788d0c0b3107bd1fabeeb0ce6b89e"
+    sha256 cellar: :any,                 high_sierra:   "206f44c35a82fac519b64b8c4ae6bc397e360d8404e8279a24b906d7729efed2"
+    sha256 cellar: :any,                 sierra:        "91d028b464f32fcf6edda6b791be2b70d9b770934edd7af7d2b8ff24e9c5eb06"
+    sha256 cellar: :any,                 el_capitan:    "6136d8cdc420681130c59d9f77327ddad6b46a35d29da5be760b522c7456e2a2"
+    sha256 cellar: :any,                 yosemite:      "573f6f3a83ba0d2d0e3b54314a5eb93affd11f03bcc409d381ef8253d7e03c4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b00aa2974e4ab2714fb6d98ada6b1dc127a20641f05170196ecf58beea63d6a7"
   end
 
   head do
