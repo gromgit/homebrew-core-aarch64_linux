d5c4ae7c825d298ffbcec6ae05ecc56865b8167f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:49:40 +0000

    speech-tools: update 2.5.0_1 bottle.

--- Formula/speech-tools.rb
+++ Formula/speech-tools.rb
@@ -13,9 +13,10 @@ class SpeechTools < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, big_sur:  "cabb028b487d5baa5c9c1bb67a982a285ab2af2194ac429fb80f0675e2bd9f6e"
-    sha256 cellar: :any, catalina: "e25823939149f50f343c2e6bd8521b302067a0eb3106df6b40ff96b2d1a70c21"
-    sha256 cellar: :any, mojave:   "3ede4e21772a17e0c0a109151406ad82943ba77b0cad2249c0cac51e063d24ea"
+    sha256 cellar: :any,                 big_sur:      "cabb028b487d5baa5c9c1bb67a982a285ab2af2194ac429fb80f0675e2bd9f6e"
+    sha256 cellar: :any,                 catalina:     "e25823939149f50f343c2e6bd8521b302067a0eb3106df6b40ff96b2d1a70c21"
+    sha256 cellar: :any,                 mojave:       "3ede4e21772a17e0c0a109151406ad82943ba77b0cad2249c0cac51e063d24ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "367d922ef74f4977cd3fca29461b4814af733073cbba6cc066d5b85a729daf26"
   end
 
   depends_on "libomp"
