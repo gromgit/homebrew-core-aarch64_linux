5d2d51b24a630c61755a4beaea1c8f47f79bdcd9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 19:03:30 +0000

    sleef: update 3.5.1 bottle.

--- Formula/sleef.rb
+++ Formula/sleef.rb
@@ -7,10 +7,11 @@ class Sleef < Formula
   head "https://github.com/shibatch/sleef.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "1277b28f0a610af52bf3f646b4d893a08dd0d0d18004e04b439d47293b93b15d"
-    sha256 cellar: :any, catalina:    "87ddfa37e9405dfc66c35295295e5f09e497fb1bc8c07c9e0f38560aeb46398f"
-    sha256 cellar: :any, mojave:      "f9e95775f5e41924f42eae7c4ae1f567229a49989e1f8a380d55355e37680d9c"
-    sha256 cellar: :any, high_sierra: "353ca63589a038009ad9b39dadcf3ef4c46153f738fcfb07d58a25dfc37e945a"
+    sha256 cellar: :any,                 big_sur:      "1277b28f0a610af52bf3f646b4d893a08dd0d0d18004e04b439d47293b93b15d"
+    sha256 cellar: :any,                 catalina:     "87ddfa37e9405dfc66c35295295e5f09e497fb1bc8c07c9e0f38560aeb46398f"
+    sha256 cellar: :any,                 mojave:       "f9e95775f5e41924f42eae7c4ae1f567229a49989e1f8a380d55355e37680d9c"
+    sha256 cellar: :any,                 high_sierra:  "353ca63589a038009ad9b39dadcf3ef4c46153f738fcfb07d58a25dfc37e945a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c03a8e095894d761b41adf5346c9839c8b3ed2f443c097c48f83d7921ec04c76"
   end
 
   depends_on "cmake" => :build
