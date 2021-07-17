d4923386c05b61c16dbfdea4d5acda40445fe95e
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-17 21:56:23 +0000

    znapzend: update 0.21.0 bottle.

--- Formula/znapzend.rb
+++ Formula/znapzend.rb
@@ -11,6 +11,7 @@ class Znapzend < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "03e855b5e75b84add0bd121a3f729112c7874ee09c74c2408479e1df095bc96c"
     sha256 cellar: :any_skip_relocation, catalina:      "d4a36c0209fcb921c565bc9430ecf7a13ef3cfd226e32090c67af215c67b28ca"
     sha256 cellar: :any_skip_relocation, mojave:        "2435d387ff28213402b816f40fdbf34bb733db8216ca72e66459b188d9e25ae5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d434f71ee8faa538bbf39b2cc6c328274d9f45873098f03bd7f4b2b33b05da40"
   end
 
   uses_from_macos "perl"
