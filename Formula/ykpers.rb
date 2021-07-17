59fb0a9c6bb06732718d5fad6c4a6919b9dea7a6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:25:20 +0000

    ykpers: update 1.20.0_2 bottle.

--- Formula/ykpers.rb
+++ Formula/ykpers.rb
@@ -12,11 +12,12 @@ class Ykpers < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "512484b795857fd09d61e2fb5c186ff771295c90b809bdcc82fdcf76835b71a0"
-    sha256 cellar: :any, big_sur:       "31b2bafcc829e3cc6e85f5e1021075088a909ba4db51ec8f20b23db93f59d802"
-    sha256 cellar: :any, catalina:      "8c5ed1924d1059265589a221b8e2bb26a2bcd59f91ede210e3a1267412867f47"
-    sha256 cellar: :any, mojave:        "c2e6089348f9cc4f9c887eeb5975378749c42ea386ef12d7f84a3285b718dc45"
-    sha256 cellar: :any, high_sierra:   "79c240a018183c2f62eae6e7c22f631598b167d321a715f0983ff4653c1c2eee"
+    sha256 cellar: :any,                 arm64_big_sur: "512484b795857fd09d61e2fb5c186ff771295c90b809bdcc82fdcf76835b71a0"
+    sha256 cellar: :any,                 big_sur:       "31b2bafcc829e3cc6e85f5e1021075088a909ba4db51ec8f20b23db93f59d802"
+    sha256 cellar: :any,                 catalina:      "8c5ed1924d1059265589a221b8e2bb26a2bcd59f91ede210e3a1267412867f47"
+    sha256 cellar: :any,                 mojave:        "c2e6089348f9cc4f9c887eeb5975378749c42ea386ef12d7f84a3285b718dc45"
+    sha256 cellar: :any,                 high_sierra:   "79c240a018183c2f62eae6e7c22f631598b167d321a715f0983ff4653c1c2eee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eee945e8cc748d69622f20b470e327fee279b356b78be2df9a75dc10ab945f1d"
   end
 
   depends_on "pkg-config" => :build
