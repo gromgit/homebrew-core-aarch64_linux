0c8a329c9497923e827e86dc431b74e84b638c27
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:16:21 +0000

    poco: update 1.11.0 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -12,10 +12,11 @@ class Poco < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a9b4b2d1c1c24b81acea5947325bffd2af88426291857c3c23592659ce3957ca"
-    sha256 cellar: :any, big_sur:       "ec8ca74c0085ccb363451475afd0913463730f5129205ff1e76edf43bcafae1d"
-    sha256 cellar: :any, catalina:      "15372ab13fe7e6269b7038532c0a05675b12f71df5f5a69c4930d57a16a598c1"
-    sha256 cellar: :any, mojave:        "2bb42c67ece76fde31d9dd8d79fad23fecbf9ae33231f1601a335829b1f6f3c6"
+    sha256 cellar: :any,                 arm64_big_sur: "a9b4b2d1c1c24b81acea5947325bffd2af88426291857c3c23592659ce3957ca"
+    sha256 cellar: :any,                 big_sur:       "ec8ca74c0085ccb363451475afd0913463730f5129205ff1e76edf43bcafae1d"
+    sha256 cellar: :any,                 catalina:      "15372ab13fe7e6269b7038532c0a05675b12f71df5f5a69c4930d57a16a598c1"
+    sha256 cellar: :any,                 mojave:        "2bb42c67ece76fde31d9dd8d79fad23fecbf9ae33231f1601a335829b1f6f3c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "52fd3511305d75b4c25b2a562a6c3f741cce5abe050f3107a8b9fa2310f9c8de"
   end
 
   depends_on "cmake" => :build
