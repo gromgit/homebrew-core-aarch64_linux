160c6b4f3a533004b1b6c682891fc2461aba9b8b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:01:01 +0000

    tiny-fugue: update 5.0b8_2 bottle.

--- Formula/tiny-fugue.rb
+++ Formula/tiny-fugue.rb
@@ -18,6 +18,7 @@ class TinyFugue < Formula
     sha256 catalina:      "d10777dd98ae76a048caed1179f7a65f8ee59256dcb94cfcd89ac1da0e135209"
     sha256 mojave:        "ea162f2b1644a44d95a2847ec34133661008fff66306e3eda790a25f253f2165"
     sha256 high_sierra:   "b1ddefa5c2a52f3399f5a90c0586d65e5e7ccc9940715cbe682a1a30e8dc6e76"
+    sha256 x86_64_linux:  "c92a44ad82e402fb01b555a22f7e276a344d799b1b666ef76286a3397617770c"
   end
 
   depends_on "libnet"
