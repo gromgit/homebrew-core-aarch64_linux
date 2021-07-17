ce88a2fe00040a19d437204f228dd464d2f7d215
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:00:46 +0000

    tmx: update 1.4.0 bottle.

--- Formula/tmx.rb
+++ Formula/tmx.rb
@@ -6,10 +6,11 @@ class Tmx < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "770cdb601ea6b496a29832960cb5fd79626a99f55f01c635985aa921f3e5f31d"
-    sha256 cellar: :any, big_sur:       "91e9846b6d59e0694918753e357736c229c2a70d8021fdbaa2eb506e5be746c2"
-    sha256 cellar: :any, catalina:      "1013715fdb263f6d6985c9145a5dbc05d2e41ba6c4aa28af766f0bc82a87f2c5"
-    sha256 cellar: :any, mojave:        "060eab2a5090afed9dfbf6ca716a2867b956be2222e6a623a5b98774bf06ef6e"
+    sha256 cellar: :any,                 arm64_big_sur: "770cdb601ea6b496a29832960cb5fd79626a99f55f01c635985aa921f3e5f31d"
+    sha256 cellar: :any,                 big_sur:       "91e9846b6d59e0694918753e357736c229c2a70d8021fdbaa2eb506e5be746c2"
+    sha256 cellar: :any,                 catalina:      "1013715fdb263f6d6985c9145a5dbc05d2e41ba6c4aa28af766f0bc82a87f2c5"
+    sha256 cellar: :any,                 mojave:        "060eab2a5090afed9dfbf6ca716a2867b956be2222e6a623a5b98774bf06ef6e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ab5a06f59f128524607cfc604d1f7a5b40875572bd4516524fd61bb5db4ba13"
   end
 
   depends_on "cmake" => :build
