fbcfccab4e8e81afbb48670021ff8db635b4396c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:19:17 +0000

    wemux: update 3.2.0 bottle.

--- Formula/wemux.rb
+++ Formula/wemux.rb
@@ -13,6 +13,7 @@ class Wemux < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "5fb4eaf177d1766716003032bfc632d02ebed302c57e00dc752ed3de4b9cf1f6"
     sha256 cellar: :any_skip_relocation, mojave:        "5fb4eaf177d1766716003032bfc632d02ebed302c57e00dc752ed3de4b9cf1f6"
     sha256 cellar: :any_skip_relocation, high_sierra:   "5fb4eaf177d1766716003032bfc632d02ebed302c57e00dc752ed3de4b9cf1f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0141da5136178a7dd857688ba9ec076c9a03f04bbe2710cd925619561885e947"
   end
 
   depends_on "tmux"
