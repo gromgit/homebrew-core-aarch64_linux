7a8df08322fef49f3b37cf7c3f23c6214a6f9084
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:26:54 +0000

    raptor: update 2.0.15 bottle.

--- Formula/raptor.rb
+++ Formula/raptor.rb
@@ -10,14 +10,15 @@ class Raptor < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2970bdade24bb5ec9afe4b79e89234016147cb506f6b450bcfd66c50fce1cede"
-    sha256 cellar: :any, big_sur:       "5b58712f0ba9fc647c6b241f80ce3697e25c851f80b26cb8f89f28809905126f"
-    sha256 cellar: :any, catalina:      "d0be0655a9546fe278830f8b60a19f66dcc10a77445e2bf2cf76ad61a0a802e2"
-    sha256 cellar: :any, mojave:        "cbfcd2b8ce9fa8701a15eba572e766e3a9b3b19eba8438d41618e6ee3ab4501d"
-    sha256 cellar: :any, high_sierra:   "194f9a66e1aeccb23185abd6f1f1408422594dfd54cad5460c5a5ac3f5e0c48c"
-    sha256 cellar: :any, sierra:        "dba42ee8efa54c0b5722f0e12a7a585c2a9fe07a22aea6aec9124aaf09860f1d"
-    sha256 cellar: :any, el_capitan:    "cc2feb9cd1d6f9eb0f511853f19c43bc96d143de6a73edfd3cdb854647d1d14a"
-    sha256 cellar: :any, yosemite:      "c23392012fb5edffbe800771eaee6ae81d288e62c477c27332474f2ed342d1b3"
+    sha256 cellar: :any,                 arm64_big_sur: "2970bdade24bb5ec9afe4b79e89234016147cb506f6b450bcfd66c50fce1cede"
+    sha256 cellar: :any,                 big_sur:       "5b58712f0ba9fc647c6b241f80ce3697e25c851f80b26cb8f89f28809905126f"
+    sha256 cellar: :any,                 catalina:      "d0be0655a9546fe278830f8b60a19f66dcc10a77445e2bf2cf76ad61a0a802e2"
+    sha256 cellar: :any,                 mojave:        "cbfcd2b8ce9fa8701a15eba572e766e3a9b3b19eba8438d41618e6ee3ab4501d"
+    sha256 cellar: :any,                 high_sierra:   "194f9a66e1aeccb23185abd6f1f1408422594dfd54cad5460c5a5ac3f5e0c48c"
+    sha256 cellar: :any,                 sierra:        "dba42ee8efa54c0b5722f0e12a7a585c2a9fe07a22aea6aec9124aaf09860f1d"
+    sha256 cellar: :any,                 el_capitan:    "cc2feb9cd1d6f9eb0f511853f19c43bc96d143de6a73edfd3cdb854647d1d14a"
+    sha256 cellar: :any,                 yosemite:      "c23392012fb5edffbe800771eaee6ae81d288e62c477c27332474f2ed342d1b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e12ca0982ee8eebad9a39d5f77ef329dd9e56fdbfa9e3d083648bfecba8d6d7"
   end
 
   uses_from_macos "libxml2"
