e4ecbb074c023064d0a53aaef528a5777bcb1697
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 19:01:51 +0000

    slowhttptest: update 1.8.2_1 bottle.

--- Formula/slowhttptest.rb
+++ Formula/slowhttptest.rb
@@ -8,11 +8,12 @@ class Slowhttptest < Formula
   head "https://github.com/shekyan/slowhttptest.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "cc98e77420edf6c9304650871991d7df7f89dd99381a63f021bdef192d9b1e37"
-    sha256 cellar: :any, big_sur:       "8414f5f6736cdaac257f0c96ecf0a72526c80595b7e966d26a0c99aaba25a8dc"
-    sha256 cellar: :any, catalina:      "7cd865ac1b118d8ef7bdf0b540f56140ff4254e7a38d2b22d520c9bd1158df5d"
-    sha256 cellar: :any, mojave:        "f4da64ee55ba56ffaff0d383954d0e13577326dbca30b431d5d89775dcfb396e"
-    sha256 cellar: :any, high_sierra:   "3ffeaec203cd16a00aeb0bf239dfe5b32087e35a74dd5c6917bd3e7a2a09848f"
+    sha256 cellar: :any,                 arm64_big_sur: "cc98e77420edf6c9304650871991d7df7f89dd99381a63f021bdef192d9b1e37"
+    sha256 cellar: :any,                 big_sur:       "8414f5f6736cdaac257f0c96ecf0a72526c80595b7e966d26a0c99aaba25a8dc"
+    sha256 cellar: :any,                 catalina:      "7cd865ac1b118d8ef7bdf0b540f56140ff4254e7a38d2b22d520c9bd1158df5d"
+    sha256 cellar: :any,                 mojave:        "f4da64ee55ba56ffaff0d383954d0e13577326dbca30b431d5d89775dcfb396e"
+    sha256 cellar: :any,                 high_sierra:   "3ffeaec203cd16a00aeb0bf239dfe5b32087e35a74dd5c6917bd3e7a2a09848f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f6da4dc3f06b54d5bb48e22ef34810b779c950bfb108cc49ee8970975030879"
   end
 
   # Remove these in next version
