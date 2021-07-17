c46da1c056971ed9749694893c47b25b0bf68ad0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:38:36 +0000

    mvtools: update 23_1 bottle.

--- Formula/mvtools.rb
+++ Formula/mvtools.rb
@@ -8,10 +8,11 @@ class Mvtools < Formula
   head "https://github.com/dubhater/vapoursynth-mvtools.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "df691836b6052e38806e3e4a662f0b5da22120f8f586ad6ea388e2673dcf01b3"
-    sha256 cellar: :any, catalina:    "01785cf0cea2080cb2b875df545e027aaaf339fbbddeca53fd5dae8f39bf4726"
-    sha256 cellar: :any, mojave:      "0809f0353e48e30d8628bbe2124cebfa0ebd1a6add77e2d27798ce968dadb84d"
-    sha256 cellar: :any, high_sierra: "0a1bab6b74375cb11959d2100e562bb2cc8124da7115b754975cd70c31e676b2"
+    sha256 cellar: :any,                 big_sur:      "df691836b6052e38806e3e4a662f0b5da22120f8f586ad6ea388e2673dcf01b3"
+    sha256 cellar: :any,                 catalina:     "01785cf0cea2080cb2b875df545e027aaaf339fbbddeca53fd5dae8f39bf4726"
+    sha256 cellar: :any,                 mojave:       "0809f0353e48e30d8628bbe2124cebfa0ebd1a6add77e2d27798ce968dadb84d"
+    sha256 cellar: :any,                 high_sierra:  "0a1bab6b74375cb11959d2100e562bb2cc8124da7115b754975cd70c31e676b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "915cd8e779a5143a86f77cecc9efae2029eda0194358b52e69c4e59811c20c6f"
   end
 
   depends_on "autoconf" => :build
