bb44cf978623814ae3e87d1f2a103aef984eb93b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:44:06 +0000

    stuntman: update 1.2.16 bottle.

--- Formula/stuntman.rb
+++ Formula/stuntman.rb
@@ -17,6 +17,7 @@ class Stuntman < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "e337d1ad8978b0bb926bca46992575b686145f9e8eb43dbc990e4efe08539722"
     sha256 cellar: :any_skip_relocation, mojave:        "d1b2a91211d57f057081fba43d0ed6ae3b05c40114b1e77f0cd3c0189f7ad07c"
     sha256 cellar: :any_skip_relocation, high_sierra:   "a7055d814d7645e408d92ffeba5ff5c1215302bdf4411bbf02e8d49ff40115a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "901fccf90a4f4fad5f18e3c8f429f22504244db3a12ecbc1ef354ad5baf7b988"
   end
 
   depends_on "boost" => :build
