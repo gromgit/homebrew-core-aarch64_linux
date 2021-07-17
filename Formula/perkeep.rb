77824018ecb9d35e842ec4e757fbc90c2b98e642
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:08:57 +0000

    perkeep: update 0.11 bottle.

--- Formula/perkeep.rb
+++ Formula/perkeep.rb
@@ -21,6 +21,7 @@ class Perkeep < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "893d6cfc23b18401987de13cb5630bb259f9d3d4d0de56ebaa7d2f3f7e93333a"
     sha256 cellar: :any_skip_relocation, catalina:      "c676479c6b5f7e5bbee45c7b0d31b26c05915195c2ab7b61156ac46257b14cb6"
     sha256 cellar: :any_skip_relocation, mojave:        "e05528f7efbb84fa9bbb39a68f3d0bb48073806a204eba8d0f70a52871ed83fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec90f3dab9f58090d10ab37c5043d98832493e60af73794cc668584c6fe5c524"
   end
 
   depends_on "go" => :build
