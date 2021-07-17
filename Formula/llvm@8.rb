1b69ae8a53f7dc34f24dfa7570c0077a34cd9021
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-17 14:45:48 +0000

    llvm@8: update 8.0.1_3 bottle.

--- Formula/llvm@8.rb
+++ Formula/llvm@8.rb
@@ -7,6 +7,7 @@ class LlvmAT8 < Formula
   revision 3
 
   bottle do
+    sha256 cellar: :any,                 big_sur:      "3807a935c2ea14dab5336a75a8ed9d9f06c656673608c338b4b519a22f267c9c"
     sha256 cellar: :any,                 catalina:     "ab099d84e5f0a58ea37172fd85753336d855fc25e9459ceff12ddc2dbb56ef71"
     sha256 cellar: :any,                 mojave:       "ee795cbebce64f79bbcf7c42526093df7bd2e5e986a721197bca5cf6c822e87a"
     sha256 cellar: :any,                 high_sierra:  "3f80b7119307b128b1e3ae8a2fea97a9878afb5a7436a7d35615b1e743bc7622"
