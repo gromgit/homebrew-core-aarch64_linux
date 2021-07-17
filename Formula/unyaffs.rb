10caf58b9e417876adedcf4ac7b88f627744f5d3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:11:50 +0000

    unyaffs: update 0.9.7 bottle.

--- Formula/unyaffs.rb
+++ Formula/unyaffs.rb
@@ -12,6 +12,7 @@ class Unyaffs < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "54982f10cb8c866e7370886765744c109f3566717f7af6f397e8a83a7ca65520"
     sha256 cellar: :any_skip_relocation, catalina:      "0319fb2b8ee918808e30a0bb5deef42abaf7d4afe35cff538b4ed513f06de16e"
     sha256 cellar: :any_skip_relocation, mojave:        "1c3b921af84a9fee0bb8faf7d420ff2a3d6e6a4e42aeec235d8587a8ccd5da61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3059fb9a522e08ac50f2f45eb62e373bce28b64fb23442d9e4822aaf9d0cb37"
   end
 
   def install
