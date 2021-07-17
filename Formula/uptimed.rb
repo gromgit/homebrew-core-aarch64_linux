37011c9ea174c5d2419c0d5c85ba7793a4c76834
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:12:12 +0000

    uptimed: update 0.4.3 bottle.

--- Formula/uptimed.rb
+++ Formula/uptimed.rb
@@ -6,11 +6,12 @@ class Uptimed < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8efbddfdb0eef471f4d0f50a55cf791d422d918d1a4f10b68e8383f7408caa08"
-    sha256 cellar: :any, big_sur:       "fc45435953f14fc7a182884dfa4e2672213c71675c9e73fb6a3799a140e66caa"
-    sha256 cellar: :any, catalina:      "1e20c4955ff14a05da57be77e08e163e164e41995411c21aeaa5a5bf3919fb7c"
-    sha256 cellar: :any, mojave:        "d5d96957debd223a243d71dc0d9858d19179c94841f6640822b1db841c0bfd48"
-    sha256 cellar: :any, high_sierra:   "8585595184bf697772b292e123f63c97513e242c7d04194c9e1990d60fcef571"
+    sha256 cellar: :any,                 arm64_big_sur: "8efbddfdb0eef471f4d0f50a55cf791d422d918d1a4f10b68e8383f7408caa08"
+    sha256 cellar: :any,                 big_sur:       "fc45435953f14fc7a182884dfa4e2672213c71675c9e73fb6a3799a140e66caa"
+    sha256 cellar: :any,                 catalina:      "1e20c4955ff14a05da57be77e08e163e164e41995411c21aeaa5a5bf3919fb7c"
+    sha256 cellar: :any,                 mojave:        "d5d96957debd223a243d71dc0d9858d19179c94841f6640822b1db841c0bfd48"
+    sha256 cellar: :any,                 high_sierra:   "8585595184bf697772b292e123f63c97513e242c7d04194c9e1990d60fcef571"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ddcee77c17f262b3ec8aaa7a71472fba2b76d328ccb2d0e3aa2a2ea35ab7732"
   end
 
   depends_on "autoconf" => :build
