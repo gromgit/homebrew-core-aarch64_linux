8df3e435701982c4aad218c365dea47a5b58c2fe
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:42:54 +0000

    startup-notification: update 0.12_1 bottle.

--- Formula/startup-notification.rb
+++ Formula/startup-notification.rb
@@ -7,11 +7,12 @@ class StartupNotification < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d5cb6d07fb21b5bf6c2276de876642a3b8579c4d4f4b962532b3c1c831ba4f93"
-    sha256 cellar: :any, big_sur:       "17601558b8e72930f3917e9c7373d620a37e6cbf987172e3134f87a2ccc60af0"
-    sha256 cellar: :any, catalina:      "bdb8f9123099562853461f5299108f7cbfac9be39ea3ab9ad6b3853c288ba5c9"
-    sha256 cellar: :any, mojave:        "c4fcbad957b22a8999a0bc87a3c2b0b2b6b94654b3f6213f5903025574ae4c76"
-    sha256 cellar: :any, high_sierra:   "60f0a0ce0a2954f53fa9f4b5dfc3aeb99aa5607801f340b506ea172bb1e381f3"
+    sha256 cellar: :any,                 arm64_big_sur: "d5cb6d07fb21b5bf6c2276de876642a3b8579c4d4f4b962532b3c1c831ba4f93"
+    sha256 cellar: :any,                 big_sur:       "17601558b8e72930f3917e9c7373d620a37e6cbf987172e3134f87a2ccc60af0"
+    sha256 cellar: :any,                 catalina:      "bdb8f9123099562853461f5299108f7cbfac9be39ea3ab9ad6b3853c288ba5c9"
+    sha256 cellar: :any,                 mojave:        "c4fcbad957b22a8999a0bc87a3c2b0b2b6b94654b3f6213f5903025574ae4c76"
+    sha256 cellar: :any,                 high_sierra:   "60f0a0ce0a2954f53fa9f4b5dfc3aeb99aa5607801f340b506ea172bb1e381f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45873a4a273a11b66436459b7130de94bcb32470a3a4376ef74b2803e2494462"
   end
 
   depends_on "pkg-config" => [:build, :test]
