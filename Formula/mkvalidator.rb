4010fde838ad861617ff8b3e045db21a4a5b3a75
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:26:11 +0000

    mkvalidator: update 0.6.0 bottle.

--- Formula/mkvalidator.rb
+++ Formula/mkvalidator.rb
@@ -15,6 +15,7 @@ class Mkvalidator < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "cef2881fd23f1e0b7c465080379f8564b00da6db94cf28f5da272ec19f565014"
     sha256 cellar: :any_skip_relocation, catalina:      "98fa360ee6e7ebc233784d62c599bc07bd92131159bdbc64d233ad99f883a99e"
     sha256 cellar: :any_skip_relocation, mojave:        "4e2d50be71341f0a47591587ce3cc428da8493af87ea2bcdcca64c86cacd44f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83b78fb761e9d2bccbc88b9893d6760f445eef59c2d122dace22541ef1e04791"
   end
 
   depends_on "cmake" => :build
