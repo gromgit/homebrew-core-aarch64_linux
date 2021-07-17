d067036b7d00e46b54c0e8d9ca24ca293db8e1c3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:26:41 +0000

    onednn: update 2.3 bottle.

--- Formula/onednn.rb
+++ Formula/onednn.rb
@@ -7,10 +7,11 @@ class Onednn < Formula
   head "https://github.com/oneapi-src/onednn.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "24016ef894b97391b8ba2327c092634e0fa6d56e5309a4c5b8abc2ea0e050273"
-    sha256 cellar: :any, big_sur:       "af0289ce4e3b217a32171692346f35b1989e686b2735fc537f99a66b64c6a0d5"
-    sha256 cellar: :any, catalina:      "77d28b51805d59a0c1fd1edc5de02c44c01949dd01fc46b1b3d6cbf4926fc416"
-    sha256 cellar: :any, mojave:        "5601c8dadace159795a6fc788949d73a70d2b0faa08c8178bdba67e923297d21"
+    sha256 cellar: :any,                 arm64_big_sur: "24016ef894b97391b8ba2327c092634e0fa6d56e5309a4c5b8abc2ea0e050273"
+    sha256 cellar: :any,                 big_sur:       "af0289ce4e3b217a32171692346f35b1989e686b2735fc537f99a66b64c6a0d5"
+    sha256 cellar: :any,                 catalina:      "77d28b51805d59a0c1fd1edc5de02c44c01949dd01fc46b1b3d6cbf4926fc416"
+    sha256 cellar: :any,                 mojave:        "5601c8dadace159795a6fc788949d73a70d2b0faa08c8178bdba67e923297d21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8139db4a2a12236e956bd2b34b86fcf524bba9e213628420c1714c6e8ee370b"
   end
 
   depends_on "cmake" => :build
