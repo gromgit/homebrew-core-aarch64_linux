c823188a14d9266a6e4fb549b4c2e8da55fbf0ff
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:39:04 +0000

    mysql-connector-c++: update 8.0.25 bottle.

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -10,10 +10,11 @@ class MysqlConnectorCxx < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "da7e6c838d9544e91cad96f5617fa23de2e4f840c141aee312e5211324aa58ee"
-    sha256 cellar: :any, big_sur:       "f59f257af671acd600a06866dd58bafdea6713e4302f927e55c03420654936a1"
-    sha256 cellar: :any, catalina:      "db1c0ddf7b343b3e078d01356d0e5184ba423dc8400746dfb52df85c07c3210c"
-    sha256 cellar: :any, mojave:        "46abfd0b0d36f325ca400272b2c331dc1c07581b8dcb1818aa9def027b417a52"
+    sha256 cellar: :any,                 arm64_big_sur: "da7e6c838d9544e91cad96f5617fa23de2e4f840c141aee312e5211324aa58ee"
+    sha256 cellar: :any,                 big_sur:       "f59f257af671acd600a06866dd58bafdea6713e4302f927e55c03420654936a1"
+    sha256 cellar: :any,                 catalina:      "db1c0ddf7b343b3e078d01356d0e5184ba423dc8400746dfb52df85c07c3210c"
+    sha256 cellar: :any,                 mojave:        "46abfd0b0d36f325ca400272b2c331dc1c07581b8dcb1818aa9def027b417a52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18fb7ab92da17320cbba024a629f065fe105b81a83eacff0daf1844559524aa6"
   end
 
   depends_on "boost" => :build
