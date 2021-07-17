18f5dd52e289fecde40fb11f23d9932beca0a069
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 06:54:36 +0000

    nudoku: update 2.1.0 bottle.

--- Formula/nudoku.rb
+++ Formula/nudoku.rb
@@ -11,6 +11,7 @@ class Nudoku < Formula
     sha256 big_sur:       "ec43523a92bb16fadd4ab7fe3f9189cb13849fc3919bf4525b9074a471453745"
     sha256 catalina:      "e6aa92aabc32164b2cc69d55d4a243f6aafdd6d6a109a40339d6546548153174"
     sha256 mojave:        "3afc049f852d205bff4b674e751fb1db83d73ab82ba15e5f38a1ba9c33ffd2e5"
+    sha256 x86_64_linux:  "a866bdb43994c69ce7aa6dadc3b50446df14c1daaf4df89bbc095236c80fac07"
   end
 
   depends_on "autoconf" => :build
