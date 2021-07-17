f5800e24c59aae5ba9b2818ef091d5e7b1104e94
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:16:07 +0000

    primesieve: update 7.6 bottle.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -6,9 +6,10 @@ class Primesieve < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "50ab785aea91644c88fae8d3a75118dbebd2206a99942d1dd5d813df5afd1d56"
-    sha256 cellar: :any, catalina: "46b89a5aebe93c324af325e1a4f15dd84a334564569c5a6816f998396289192c"
-    sha256 cellar: :any, mojave:   "379ba3585b2ac86f43f58654ee9c8d9bc8e552bfda6d3b476399744880f85f2a"
+    sha256 cellar: :any,                 big_sur:      "50ab785aea91644c88fae8d3a75118dbebd2206a99942d1dd5d813df5afd1d56"
+    sha256 cellar: :any,                 catalina:     "46b89a5aebe93c324af325e1a4f15dd84a334564569c5a6816f998396289192c"
+    sha256 cellar: :any,                 mojave:       "379ba3585b2ac86f43f58654ee9c8d9bc8e552bfda6d3b476399744880f85f2a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "53f9b8899168e786d258c628223aa9d0c18776001dbd75b69428187179eceb2d"
   end
 
   depends_on "cmake" => :build
