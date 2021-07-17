e7043e999b3e5af3723cfa45b96e87446b9397bf
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:33:38 +0000

    roundup: update 0.0.6 bottle.

--- Formula/roundup.rb
+++ Formula/roundup.rb
@@ -15,6 +15,7 @@ class Roundup < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "255515246130477d53aa39d0289b2840af33a937d7169a1dba297380d1eb02da"
     sha256 cellar: :any_skip_relocation, el_capitan:    "77ff95001e3a2de6eedd4d5702e5e418b7c4ecfa6855af7b479e1e978249882f"
     sha256 cellar: :any_skip_relocation, yosemite:      "5dd0f6d1e64f54b3bb389411f95cd823b75e31f073e739d78793fca4b21e8e59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2549ae6529389a3b88e83126f39ea6accb7a47321da5a90b7edb7115ed1161d"
   end
 
   def install
