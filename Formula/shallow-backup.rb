520012480610c1ea67115a498e451985d4d28ee0
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 19:09:41 +0000

    shallow-backup: update 5.0.1_1 bottle.

--- Formula/shallow-backup.rb
+++ Formula/shallow-backup.rb
@@ -15,6 +15,7 @@ class ShallowBackup < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e1ad3d711a2c18d700033fc80c16f21ebf40a1a0ba40d41352fe5c3e13339a5a"
     sha256 cellar: :any_skip_relocation, catalina:      "180c2239489893d858f7a12d729d07c47d0eed52bb77b0fc31dedba920697672"
     sha256 cellar: :any_skip_relocation, mojave:        "84fe6770d371202f7f6bda5e55b7dc1d54744faa38a25ed498d36eab4ef8be5f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7f06faa5e61303fa3e2442245af5b07b3dfb8830e67a3c130ebe542ad4306f3"
   end
 
   depends_on "python@3.9"
