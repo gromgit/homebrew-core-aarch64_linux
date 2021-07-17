1e203a4279f440b1ce63ee253a847f08acde370b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:44:46 +0000

    sdb: update 1.8.0 bottle.

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -7,10 +7,11 @@ class Sdb < Formula
   head "https://github.com/radare/sdb.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b0a860ce797a943bba94c3cd5f2b26629a79ffa7c924b01f0a1361130f90a7d6"
-    sha256 cellar: :any, big_sur:       "5e21c95c80e644bb031539b6d485f2dcd0d98d2f048c750ad912f78ade9b3fa7"
-    sha256 cellar: :any, catalina:      "97abc888fd762445486fa5c635fadba3423d5249332f2fb9b4f8183227375b6e"
-    sha256 cellar: :any, mojave:        "5f96b1c74c0b16f6171c0eb5d132b7ff08e8c80e71b2db080bedd681ef5875ad"
+    sha256 cellar: :any,                 arm64_big_sur: "b0a860ce797a943bba94c3cd5f2b26629a79ffa7c924b01f0a1361130f90a7d6"
+    sha256 cellar: :any,                 big_sur:       "5e21c95c80e644bb031539b6d485f2dcd0d98d2f048c750ad912f78ade9b3fa7"
+    sha256 cellar: :any,                 catalina:      "97abc888fd762445486fa5c635fadba3423d5249332f2fb9b4f8183227375b6e"
+    sha256 cellar: :any,                 mojave:        "5f96b1c74c0b16f6171c0eb5d132b7ff08e8c80e71b2db080bedd681ef5875ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d362cdbebb027ed947f15050c48f96f40488533b96049b8ec9c678c52083604e"
   end
 
   depends_on "pkg-config" => :build
