fd2b37dd372924429a39acc97c01c32077d82653
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:20:15 +0000

    wput: update 0.6.2 bottle.

--- Formula/wput.rb
+++ Formula/wput.rb
@@ -6,13 +6,14 @@ class Wput < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "2a5d49be96808777f249d96b6f86e2e0e0e301be0e929ba1eaea99cf79cacf42"
-    sha256 cellar: :any_skip_relocation, catalina:    "77703d5dfb1bde183ccc207ee5e3f14b1a677acc697806a2b16f00c56cc0595e"
-    sha256 cellar: :any_skip_relocation, mojave:      "563c5204880172786cbbfc75dafa818e670ac5d1a67fdbe8bea1dd2588587eab"
-    sha256 cellar: :any_skip_relocation, high_sierra: "e01d35805cd00e8f4d9ba1ab989104d66dc4150648a2288f5f49eea5c17b5025"
-    sha256 cellar: :any_skip_relocation, sierra:      "0a8c4296a3e14d8b420f65464293b000dd1bd2e33a802c92e1812f0c267d3f0f"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "8e4eeb941d98dc0313b87682b7ae659bbceac59426cf0483c2ae2676cf5b924b"
-    sha256 cellar: :any_skip_relocation, yosemite:    "97bc045a03ddd01106304530a453a47693fbd5f3419090310c91a187e1d23931"
+    sha256 cellar: :any_skip_relocation, big_sur:      "2a5d49be96808777f249d96b6f86e2e0e0e301be0e929ba1eaea99cf79cacf42"
+    sha256 cellar: :any_skip_relocation, catalina:     "77703d5dfb1bde183ccc207ee5e3f14b1a677acc697806a2b16f00c56cc0595e"
+    sha256 cellar: :any_skip_relocation, mojave:       "563c5204880172786cbbfc75dafa818e670ac5d1a67fdbe8bea1dd2588587eab"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "e01d35805cd00e8f4d9ba1ab989104d66dc4150648a2288f5f49eea5c17b5025"
+    sha256 cellar: :any_skip_relocation, sierra:       "0a8c4296a3e14d8b420f65464293b000dd1bd2e33a802c92e1812f0c267d3f0f"
+    sha256 cellar: :any_skip_relocation, el_capitan:   "8e4eeb941d98dc0313b87682b7ae659bbceac59426cf0483c2ae2676cf5b924b"
+    sha256 cellar: :any_skip_relocation, yosemite:     "97bc045a03ddd01106304530a453a47693fbd5f3419090310c91a187e1d23931"
+    sha256                               x86_64_linux: "0fb27e180b9a6f8ef2b3508530874b467449fdac55a347c63f2e86ca360db073"
   end
 
   # The patch is to skip inclusion of malloc.h only on OSX. Upstream:
