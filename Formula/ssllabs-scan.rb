3ba864fe19ba2554a16f7a46f13c153b9f03e562
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:42:42 +0000

    ssllabs-scan: update 1.5.0 bottle.

--- Formula/ssllabs-scan.rb
+++ Formula/ssllabs-scan.rb
@@ -11,6 +11,7 @@ class SsllabsScan < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "bedc2ae76ea62f469af8ca941f894016b053edd0c598e4845bcbe95e73c344a3"
     sha256 cellar: :any_skip_relocation, catalina:      "01c7e2503e8793f79149a3115dffff286ab0db876ccbfe6d8bb11ed54f27ba38"
     sha256 cellar: :any_skip_relocation, mojave:        "eb44e540aba0e6a209ab3820a168184f39ddd470673093c7a3dc87a0e70eab42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9298cfa3c9eb7b7effb0f924f9998e63406d3bfacd4b4df9785a0371138699e2"
   end
 
   depends_on "go" => :build
