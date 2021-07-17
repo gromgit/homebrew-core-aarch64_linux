ec0245fbc599fc13b3e9b11c489a834d79cc26da
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:25:16 +0000

    mjpegtools: update 2.2.0 bottle.

--- Formula/mjpegtools.rb
+++ Formula/mjpegtools.rb
@@ -6,10 +6,11 @@ class Mjpegtools < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4e6e97aa6b84f1269e344708cc2fa5b4fd4cadf7c27da94a1a450a5d7fef45fa"
-    sha256 cellar: :any, big_sur:       "cb977ade8f3c1a4949029932bd55be08ffa562b3404dd5594d63885296504f32"
-    sha256 cellar: :any, catalina:      "535d927e3f9bbb2bee320d5d254242491b500d01cd7619f10a81a77df4fa4f6a"
-    sha256 cellar: :any, mojave:        "d7a3825d1a3fa3b3863ecdf95d88ff17758f61f8d1ef0ea27b38f3a6fa7d165e"
+    sha256 cellar: :any,                 arm64_big_sur: "4e6e97aa6b84f1269e344708cc2fa5b4fd4cadf7c27da94a1a450a5d7fef45fa"
+    sha256 cellar: :any,                 big_sur:       "cb977ade8f3c1a4949029932bd55be08ffa562b3404dd5594d63885296504f32"
+    sha256 cellar: :any,                 catalina:      "535d927e3f9bbb2bee320d5d254242491b500d01cd7619f10a81a77df4fa4f6a"
+    sha256 cellar: :any,                 mojave:        "d7a3825d1a3fa3b3863ecdf95d88ff17758f61f8d1ef0ea27b38f3a6fa7d165e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e005dd2e68a1392fc0e62bfbd8fb1d1c9ae304e8fa41674c4b52f1975896f217"
   end
 
   depends_on "pkg-config" => :build
