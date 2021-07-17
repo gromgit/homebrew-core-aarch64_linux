f6fb6a5dee0193bc42a8993ba2918579db5e96bd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:32:15 +0000

    roll: update 2.5.0 bottle.

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -12,6 +12,7 @@ class Roll < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "c1c2251d5ac3b6a03065877f6c211f326909c7710f85441adf0d9987815b6c35"
     sha256 cellar: :any_skip_relocation, high_sierra:   "0fe1da205f9d37a29489ac8b437cefcd915041c47ddf95cae577369b46d8d7ea"
     sha256 cellar: :any_skip_relocation, sierra:        "f9c7a833dad6d95fe4ee83fccf0890f49dee962e9965a624cf5d483d86c72eab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f2c41f037525c89e5948f37436173949875ad99a85e8767c83165648b09a842"
   end
 
   head do
