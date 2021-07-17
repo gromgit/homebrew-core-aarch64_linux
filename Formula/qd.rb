6bd9d111416212a89d4fb1554cfc73946e4aa9ee
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:23:27 +0000

    qd: update 2.3.22 bottle.

--- Formula/qd.rb
+++ Formula/qd.rb
@@ -11,11 +11,12 @@ class Qd < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "a142fb3e68694ee7529043e985fd898069553ec2ca37483336b6ead7246e28bc"
-    sha256 cellar: :any, big_sur:       "380c30f837eed2027f1f1a353cfca4b5f71f551e504c26b2ab635cba4918681b"
-    sha256 cellar: :any, catalina:      "9700e6163692f31c736ddd74f535305fef730e021c4ca9f85b5860926397e330"
-    sha256 cellar: :any, mojave:        "53e4efc8ab2d1c18b1c4198bed031eb1b97b4431b1c0a4e8e4195c9b01659098"
-    sha256 cellar: :any, high_sierra:   "0ff67d07426a90d2897f0f69da0bd91bedb8a40ea52c0201c80225dd6c22510d"
+    sha256 cellar: :any,                 arm64_big_sur: "a142fb3e68694ee7529043e985fd898069553ec2ca37483336b6ead7246e28bc"
+    sha256 cellar: :any,                 big_sur:       "380c30f837eed2027f1f1a353cfca4b5f71f551e504c26b2ab635cba4918681b"
+    sha256 cellar: :any,                 catalina:      "9700e6163692f31c736ddd74f535305fef730e021c4ca9f85b5860926397e330"
+    sha256 cellar: :any,                 mojave:        "53e4efc8ab2d1c18b1c4198bed031eb1b97b4431b1c0a4e8e4195c9b01659098"
+    sha256 cellar: :any,                 high_sierra:   "0ff67d07426a90d2897f0f69da0bd91bedb8a40ea52c0201c80225dd6c22510d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "44a350161048cb1feeb7b325a8d3a55a448a81bd1d928b67a36d3a685d50aa97"
   end
 
   depends_on "gcc" # for gfortran
