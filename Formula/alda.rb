00b671c2c782476c112fd308faef85a2eecc82e5
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-07-17 11:09:23 -0700

    alda 2.0.1 (new formula)
    Closes #81417.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/alda.rb
@@ -0,0 +1,31 @@
+class Alda < Formula
+  desc "Music programming language for musicians"
+  homepage "https://alda.io"
+  url "https://github.com/alda-lang/alda/archive/refs/tags/release-2.0.1.tar.gz"
+  sha256 "82227e04dd70f43b34af00350387844a38c1317eadbc0daf1267a8933d7b5c64"
+  license "EPL-2.0"
+
+  depends_on "go" => :build
+  depends_on "gradle" => :build
+  depends_on "openjdk"
+
+  def install
+    pkgshare.install "examples"
+    cd "client" do
+      system "go", "generate"
+      system "go", "build", *std_go_args
+    end
+    cd "player" do
+      system "gradle", "build"
+      libexec.install "build/libs/alda-player-fat.jar"
+      bin.write_jar_script libexec/"alda-player-fat.jar", "alda-player"
+    end
+  end
+
+  test do
+    (testpath/"hello.alda").write "piano: c8 d e f g f e d c2."
+    json_output = JSON.parse(shell_output("#{bin}/alda parse -f hello.alda 2>/dev/null"))
+    midi_notes = json_output["events"].map { |event| event["midi-note"] }
+    assert_equal [60, 62, 64, 65, 67, 65, 64, 62, 60], midi_notes
+  end
+end
