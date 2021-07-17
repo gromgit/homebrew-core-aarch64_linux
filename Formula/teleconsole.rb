e4d26187e99f8ca9a9488c7a1b842ce1c400e765
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:55:02 +0000

    teleconsole: update 0.4.0 bottle.

--- Formula/teleconsole.rb
+++ Formula/teleconsole.rb
@@ -8,11 +8,12 @@ class Teleconsole < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "a7d96f387532f40298ff90dc062994e933fc009500279421a16182b063f39446"
-    sha256 cellar: :any_skip_relocation, catalina:    "fd114a850d3e9eb653e6ed08f53224bd81219c7bcfbd2459440b68a0e96711dc"
-    sha256 cellar: :any_skip_relocation, mojave:      "4a5a767d1097e9e8580e3d3ad77d01b8b840ef622092983d713333ed90d2db0d"
-    sha256 cellar: :any_skip_relocation, high_sierra: "d53e81606f1e85b59bd3ee364e006989187f5cce884b33fb77b104b931a7e3c5"
-    sha256 cellar: :any_skip_relocation, sierra:      "c74fa8ac5e92c39a3f0d869b9e8bd44d32ab67ed0748b5548a0700287dfbe817"
+    sha256 cellar: :any_skip_relocation, big_sur:      "a7d96f387532f40298ff90dc062994e933fc009500279421a16182b063f39446"
+    sha256 cellar: :any_skip_relocation, catalina:     "fd114a850d3e9eb653e6ed08f53224bd81219c7bcfbd2459440b68a0e96711dc"
+    sha256 cellar: :any_skip_relocation, mojave:       "4a5a767d1097e9e8580e3d3ad77d01b8b840ef622092983d713333ed90d2db0d"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "d53e81606f1e85b59bd3ee364e006989187f5cce884b33fb77b104b931a7e3c5"
+    sha256 cellar: :any_skip_relocation, sierra:       "c74fa8ac5e92c39a3f0d869b9e8bd44d32ab67ed0748b5548a0700287dfbe817"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c143c1ce6d0119de540cd748544c68d55ea414e610c58cf6c02cb3cd9717c307"
   end
 
   deprecate! date: "2021-05-24", because: :repo_archived
