68548d8b4410b635407d0eeee04ff2305a9f5623
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:02:47 +0000

    tofrodos: update 1.7.13 bottle.

--- Formula/tofrodos.rb
+++ Formula/tofrodos.rb
@@ -19,6 +19,7 @@ class Tofrodos < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "3d5363cda2170ce2fbcb7e03c84f715b62ead1e5646000dd06395f5677fd2269"
     sha256 cellar: :any_skip_relocation, el_capitan:    "4a2b22ff08d0fb65c80be7359be2f04d12b70f4e6d490b96cb819ea69b3e3d88"
     sha256 cellar: :any_skip_relocation, yosemite:      "4a5427c6870c3d4822ef4da3ddd8d79c18b91e5b7f14edb4aa449a53da70114e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b0f32616234ccfd1984a590ef37cd25cf15ba64fe0316f1f0a2d8fdea2cfc6f3"
   end
 
   def install
