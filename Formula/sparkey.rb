8afb498abb99624273a90da0e61b0b1f4a5da1b9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:48:07 +0000

    sparkey: update 1.0.0_1 bottle.

--- Formula/sparkey.rb
+++ Formula/sparkey.rb
@@ -7,11 +7,12 @@ class Sparkey < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1b2b1cc05fd4af9994aa34e57bc8767bdb567455e27458e1a9ac38e340603c68"
-    sha256 cellar: :any, big_sur:       "6f469c28584124f46a7fa9835dee3311ef02d5a48f2d8fb8c8eb29f2c6688986"
-    sha256 cellar: :any, catalina:      "b7e64101995d257df010edb67bafcd60745f09c7b0ebb9650c817eb7343f1899"
-    sha256 cellar: :any, mojave:        "438c323c343b7aade2da46316d24bcc4d5c7a95910a43914d70125af14a17636"
-    sha256 cellar: :any, high_sierra:   "4acbb473ce3be942b808af45789ccb7ede8199c728f7c381cd0dda1a105c8a9e"
+    sha256 cellar: :any,                 arm64_big_sur: "1b2b1cc05fd4af9994aa34e57bc8767bdb567455e27458e1a9ac38e340603c68"
+    sha256 cellar: :any,                 big_sur:       "6f469c28584124f46a7fa9835dee3311ef02d5a48f2d8fb8c8eb29f2c6688986"
+    sha256 cellar: :any,                 catalina:      "b7e64101995d257df010edb67bafcd60745f09c7b0ebb9650c817eb7343f1899"
+    sha256 cellar: :any,                 mojave:        "438c323c343b7aade2da46316d24bcc4d5c7a95910a43914d70125af14a17636"
+    sha256 cellar: :any,                 high_sierra:   "4acbb473ce3be942b808af45789ccb7ede8199c728f7c381cd0dda1a105c8a9e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b9dcde71d3afafa413ad52116f880796bd49f1a715159a794275fb4bd366de1"
   end
 
   depends_on "autoconf" => :build
