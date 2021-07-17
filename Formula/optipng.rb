13f84fbcfae0d0759c103ec84a6ce62903ec7c8e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:00:46 +0000

    optipng: update 0.7.7 bottle.

--- Formula/optipng.rb
+++ Formula/optipng.rb
@@ -8,10 +8,11 @@ class Optipng < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "796af028b1dea8b680e40103712976b4f9df285df553db06d2643779630c716c"
-    sha256 cellar: :any, big_sur:       "5cee26efb92016f057a55b2711a08c4a0350046b7c0b1d969c75a913caf66fc2"
-    sha256 cellar: :any, catalina:      "3d423dfa59e07122f70e2a15026289dfc6884798ac76898065dbe587256c6e35"
-    sha256 cellar: :any, mojave:        "bd44fa66b00a6ee0340a9a5b239d22823787fcaa26312783b21c0f4afc39fd0b"
+    sha256 cellar: :any,                 arm64_big_sur: "796af028b1dea8b680e40103712976b4f9df285df553db06d2643779630c716c"
+    sha256 cellar: :any,                 big_sur:       "5cee26efb92016f057a55b2711a08c4a0350046b7c0b1d969c75a913caf66fc2"
+    sha256 cellar: :any,                 catalina:      "3d423dfa59e07122f70e2a15026289dfc6884798ac76898065dbe587256c6e35"
+    sha256 cellar: :any,                 mojave:        "bd44fa66b00a6ee0340a9a5b239d22823787fcaa26312783b21c0f4afc39fd0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e515f83962de17813907616541a6e9bdcb3b51a64db9f4c58abd8c5960a47764"
   end
 
   depends_on "libpng"
