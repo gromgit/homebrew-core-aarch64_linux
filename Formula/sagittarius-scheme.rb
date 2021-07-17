ba45b566f988f0cc6a2239f79c5de94888e476e1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 19:05:25 +0000

    sagittarius-scheme: update 0.9.7 bottle.

--- Formula/sagittarius-scheme.rb
+++ Formula/sagittarius-scheme.rb
@@ -6,10 +6,11 @@ class SagittariusScheme < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 big_sur:     "571c191c879cfa536eb60639ad65f82bc30f2782c18ed747dae167254f1e1e8e"
-    sha256 catalina:    "7199d76824cd0de62da70f2eff3db66f0944c2851e4ee868359b7bdbe4685994"
-    sha256 mojave:      "d350853fda37321efe29a14f988b3039126559e489b2fb93a41ca538ca7a2f29"
-    sha256 high_sierra: "8824ccf2baa439cc953d8b373010e5f00fabb51dd6837f68323993b69549bb84"
+    sha256                               big_sur:      "571c191c879cfa536eb60639ad65f82bc30f2782c18ed747dae167254f1e1e8e"
+    sha256                               catalina:     "7199d76824cd0de62da70f2eff3db66f0944c2851e4ee868359b7bdbe4685994"
+    sha256                               mojave:       "d350853fda37321efe29a14f988b3039126559e489b2fb93a41ca538ca7a2f29"
+    sha256                               high_sierra:  "8824ccf2baa439cc953d8b373010e5f00fabb51dd6837f68323993b69549bb84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e622aaaef6440cec0bb67fc4e4300eac3dff64aaa116ffcd1ab9cc81b9e05ac6"
   end
 
   depends_on "cmake" => :build
