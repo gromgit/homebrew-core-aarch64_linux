7e6b54a8607bc00debf3b92905312934841b4385
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:13:41 +0000

    usbredir: update 0.10.0 bottle.

--- Formula/usbredir.rb
+++ Formula/usbredir.rb
@@ -15,6 +15,7 @@ class Usbredir < Formula
     sha256 cellar: :any, big_sur:       "26d2040e073333ad5e1aba9594f88c4450e7a9ee9780c8cc458267bea7e8c7c6"
     sha256 cellar: :any, catalina:      "1986f37f4c043ee0822fb0cc61049ab57a73871e94e3e64ad1896861be49890b"
     sha256 cellar: :any, mojave:        "04d2e58a8479304dba8f207283c5f740ea1a74440ecb3780aa4182025b268384"
+    sha256               x86_64_linux:  "01398edf07804566c8b618707679e37d8ef1b485349ee7d4eccb8a7cb2613913"
   end
 
   depends_on "meson" => :build
