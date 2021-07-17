37cff5925739e3e995ca7f7256e67af81431b039
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:12:45 +0000

    pjproject: update 2.10 bottle.

--- Formula/pjproject.rb
+++ Formula/pjproject.rb
@@ -13,10 +13,11 @@ class Pjproject < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, big_sur:     "b62ae1e3e6b33e093d69968bf0fa6708634075fb500e6cfb88d07a90d47a85cd"
-    sha256 cellar: :any, catalina:    "ce9e2f67c5ae7148b7c7883ac3c6dbcc9dd7892695af93c02dc44b3e52f109dd"
-    sha256 cellar: :any, mojave:      "26c273e3e975fc955f3c8ffb03c8332629fd42f123a4144645adb30817f9f428"
-    sha256 cellar: :any, high_sierra: "114939ba488f6f78f1d337d27eb1873aacfb9c55788b60543f6dbab7e23f745e"
+    sha256 cellar: :any,                 big_sur:      "b62ae1e3e6b33e093d69968bf0fa6708634075fb500e6cfb88d07a90d47a85cd"
+    sha256 cellar: :any,                 catalina:     "ce9e2f67c5ae7148b7c7883ac3c6dbcc9dd7892695af93c02dc44b3e52f109dd"
+    sha256 cellar: :any,                 mojave:       "26c273e3e975fc955f3c8ffb03c8332629fd42f123a4144645adb30817f9f428"
+    sha256 cellar: :any,                 high_sierra:  "114939ba488f6f78f1d337d27eb1873aacfb9c55788b60543f6dbab7e23f745e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9c21e926d07a2d72369a433fc3421a80a0078827326831202dd78533c57da7ac"
   end
 
   depends_on macos: :high_sierra # Uses Security framework API enum cases introduced in 10.13.4
