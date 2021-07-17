c90ddf1e5b924fa28e0d304b04d18ac358fd49ba
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:48:31 +0000

    spatialindex: update 1.9.3 bottle.

--- Formula/spatialindex.rb
+++ Formula/spatialindex.rb
@@ -7,11 +7,12 @@ class Spatialindex < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5786e51306b202e51c9a81b4bc6c7b593027c8e88b777d142e486bf012eebe34"
-    sha256 cellar: :any, big_sur:       "49ef8e79ee6c7348b438d2b174effd66a2a7136a12c58645a0a37a5c22740ce3"
-    sha256 cellar: :any, catalina:      "fbcbfaf6510137f3168a0dc57cbac8c8b1435094b1ede9d35a30fa6ccaea28f4"
-    sha256 cellar: :any, mojave:        "a5cbdfb3acddb053e596fc56e7653559581923e48ed6815503fffc47c7a16660"
-    sha256 cellar: :any, high_sierra:   "cace27981cc1e5143a48e8b700d6823dff9d8049140683e0e536c476894ede91"
+    sha256 cellar: :any,                 arm64_big_sur: "5786e51306b202e51c9a81b4bc6c7b593027c8e88b777d142e486bf012eebe34"
+    sha256 cellar: :any,                 big_sur:       "49ef8e79ee6c7348b438d2b174effd66a2a7136a12c58645a0a37a5c22740ce3"
+    sha256 cellar: :any,                 catalina:      "fbcbfaf6510137f3168a0dc57cbac8c8b1435094b1ede9d35a30fa6ccaea28f4"
+    sha256 cellar: :any,                 mojave:        "a5cbdfb3acddb053e596fc56e7653559581923e48ed6815503fffc47c7a16660"
+    sha256 cellar: :any,                 high_sierra:   "cace27981cc1e5143a48e8b700d6823dff9d8049140683e0e536c476894ede91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc7ec6fab28b23661a825c46d29b861fca6a6115048be5f42355da59deed84bb"
   end
 
   depends_on "cmake" => :build
