3802ff7a279ec8dc8d82c4138dd8697052e6479b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 19:20:42 +0000

    shc: update 4.0.3 bottle.

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -13,6 +13,7 @@ class Shc < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "ff3c55ef1d10c16066e97a20143dbd1e7781ceb9a2c5c8b46d140f6711bc79fa"
     sha256 cellar: :any_skip_relocation, high_sierra:   "c19f4586119be579006eace517045998138d83a17e2b5c8ec00ad73ea007b68c"
     sha256 cellar: :any_skip_relocation, sierra:        "6e1834ac7b4cc64ba972a59189512bb9ff9e0ec307df78f9e0fc1fee42378f6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a659b8f040806dcf7303f42a3cc50eb61acf894c9d2066acefd897dc71f1452"
   end
 
   def install
