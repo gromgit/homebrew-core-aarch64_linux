6289da5a454fcce2c37a0bc70521c43515ff4d4f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 06:47:34 +0000

    nanomsg: update 1.1.5 bottle.

--- Formula/nanomsg.rb
+++ Formula/nanomsg.rb
@@ -12,12 +12,13 @@ class Nanomsg < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "87ca8b11ff0c9645bcf2d30e25ef03ee957ac9187677ec03d194417ef76d3e5e"
-    sha256 big_sur:       "86a6e6d8764bc3b276ff9f96d28390153d5287c22293124203af015e62df50d3"
-    sha256 catalina:      "81cd453e3fdf65da66a54fda36c84248a1eb923ac92125fd14bdf68989aeb9b7"
-    sha256 mojave:        "95609047c54b0207587db3a5b3cc8985b35fc922fe8785c63d4d2a44a78ff57f"
-    sha256 high_sierra:   "11390e904a94e60865186a846af14565b379ec84942a9bc512ba4e5e3ea7ec85"
-    sha256 sierra:        "95192ebc59926ff064d7f4cff5ebf9037c7549af61d2f1c23375827c91b88282"
+    sha256                               arm64_big_sur: "87ca8b11ff0c9645bcf2d30e25ef03ee957ac9187677ec03d194417ef76d3e5e"
+    sha256                               big_sur:       "86a6e6d8764bc3b276ff9f96d28390153d5287c22293124203af015e62df50d3"
+    sha256                               catalina:      "81cd453e3fdf65da66a54fda36c84248a1eb923ac92125fd14bdf68989aeb9b7"
+    sha256                               mojave:        "95609047c54b0207587db3a5b3cc8985b35fc922fe8785c63d4d2a44a78ff57f"
+    sha256                               high_sierra:   "11390e904a94e60865186a846af14565b379ec84942a9bc512ba4e5e3ea7ec85"
+    sha256                               sierra:        "95192ebc59926ff064d7f4cff5ebf9037c7549af61d2f1c23375827c91b88282"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7b86a0e81583a24dabfbbb58cf5d72b7b107cfd8dfd7b8f1f652de0dd3db9b4"
   end
 
   depends_on "cmake" => :build
