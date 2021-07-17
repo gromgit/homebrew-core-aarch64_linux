07846069e6f5d3a44ee02270f6b4fce802de1728
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:27:52 +0000

    megatools: update 1.10.3 bottle.

--- Formula/megatools.rb
+++ Formula/megatools.rb
@@ -11,11 +11,12 @@ class Megatools < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9aa259fd94e583acf13b382bbf3f300632862741b34f78d8f853b976306bc224"
-    sha256 cellar: :any, big_sur:       "cf95741a2c3766205c7e24a56018b07ba5716a6c2ae889ecd35d3bd9990a5f02"
-    sha256 cellar: :any, catalina:      "88c7b8cf60517507c7d6e7d9709b53bca671d949c7363c117e27ffb7d860f855"
-    sha256 cellar: :any, mojave:        "21844a1f366aec458b92ad00debef361388aca790bdd43583ebe51df22e7f68d"
-    sha256 cellar: :any, high_sierra:   "0f295ea8f68a858f114ef09bd4f53b82c5a401664e16beee28af7cca2d1aef5c"
+    sha256 cellar: :any,                 arm64_big_sur: "9aa259fd94e583acf13b382bbf3f300632862741b34f78d8f853b976306bc224"
+    sha256 cellar: :any,                 big_sur:       "cf95741a2c3766205c7e24a56018b07ba5716a6c2ae889ecd35d3bd9990a5f02"
+    sha256 cellar: :any,                 catalina:      "88c7b8cf60517507c7d6e7d9709b53bca671d949c7363c117e27ffb7d860f855"
+    sha256 cellar: :any,                 mojave:        "21844a1f366aec458b92ad00debef361388aca790bdd43583ebe51df22e7f68d"
+    sha256 cellar: :any,                 high_sierra:   "0f295ea8f68a858f114ef09bd4f53b82c5a401664e16beee28af7cca2d1aef5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56f981aea4ce43496323a83ecd3e1c9c6de26ed9b68300700774aeb7f50ad535"
   end
 
   depends_on "asciidoc" => :build
