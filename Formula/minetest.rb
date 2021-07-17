fd40435ee67f5db61ed0cedb2c6c0409dacc0e9e
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-07-17 12:44:41 -0700

    minetest 5.4.1
    Closes #80188.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minetest.rb
+++ Formula/minetest.rb
@@ -2,15 +2,14 @@ class Minetest < Formula
   desc "Free, open source voxel game engine and game"
   homepage "https://www.minetest.net/"
   license "LGPL-2.1-or-later"
-  revision 1
 
   stable do
-    url "https://github.com/minetest/minetest/archive/5.4.0.tar.gz"
-    sha256 "6e9b299e156651be9bcf973a9232cff32215de31dfae5ea770a71d1757cab014"
+    url "https://github.com/minetest/minetest/archive/5.4.1.tar.gz"
+    sha256 "de9e4410583c845c104b4be25f9d0b8743d8573c120149b8910ae2519f9ab14e"
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game/archive/5.4.0.tar.gz"
-      sha256 "520d2056085ec11e8806cf5a8f928537797d27a86704770bf408c113ea9881cb"
+      url "https://github.com/minetest/minetest_game/archive/5.4.1.tar.gz"
+      sha256 "b4bfa0755b88230cf4bdb6af6a0951dd1248f6cdf87fecc340e43ac12c80b0b2"
     end
   end
 
@@ -49,6 +48,7 @@ class Minetest < Formula
     args << "-DBUILD_CLIENT=1" << "-DBUILD_SERVER=0"
     args << "-DENABLE_FREETYPE=1" << "-DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'"
     args << "-DENABLE_GETTEXT=1" << "-DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}"
+    args << "-DIRRLICHT_LIBRARY=#{Formula["irrlicht"].opt_frameworks}/IrrFramework.framework"
 
     system "cmake", ".", *args
     system "make", "package"
