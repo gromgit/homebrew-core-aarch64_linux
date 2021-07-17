c091e136dbf6009a2e7b1d0eeb009ac12eaa8f3d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:23:57 +0000

    xmlrpc-c: update 1.51.07 bottle.

--- Formula/xmlrpc-c.rb
+++ Formula/xmlrpc-c.rb
@@ -6,10 +6,11 @@ class XmlrpcC < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d2f823ee08ce4b5cf1db9b3c7df182b213c4e4757e89207de1147def940544be"
-    sha256 cellar: :any, big_sur:       "a270cdb18af53d135dc5b46e772ebde0b0174aa4edcb334a3522ee79b7301f8b"
-    sha256 cellar: :any, catalina:      "8f2f988116b29088d76766843fa5d2eafc2f33d919990baeb31f67fd9b8c0b53"
-    sha256 cellar: :any, mojave:        "b058d27a7d2a0ba5a265186a5c4bcb26c9a9a0c9d4a60e639b30a72a2d4a7169"
+    sha256 cellar: :any,                 arm64_big_sur: "d2f823ee08ce4b5cf1db9b3c7df182b213c4e4757e89207de1147def940544be"
+    sha256 cellar: :any,                 big_sur:       "a270cdb18af53d135dc5b46e772ebde0b0174aa4edcb334a3522ee79b7301f8b"
+    sha256 cellar: :any,                 catalina:      "8f2f988116b29088d76766843fa5d2eafc2f33d919990baeb31f67fd9b8c0b53"
+    sha256 cellar: :any,                 mojave:        "b058d27a7d2a0ba5a265186a5c4bcb26c9a9a0c9d4a60e639b30a72a2d4a7169"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e5b7b127d74d52f7301aec65d8f868f8da9c3049113c9a746033995f0f9e261"
   end
 
   uses_from_macos "curl"
