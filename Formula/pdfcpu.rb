10fbfeb1dea6cee660cf6c84d384019bc32453b2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 07:06:46 +0000

    pdfcpu: update 0.3.12 bottle.

--- Formula/pdfcpu.rb
+++ Formula/pdfcpu.rb
@@ -10,6 +10,7 @@ class Pdfcpu < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "64d26dfa6484ee00026f17cd54d7cb5e6a78c7b6196c2957cf66329a9af1f58c"
     sha256 cellar: :any_skip_relocation, catalina:      "67a0237f80fe1a7ccbed4e0e316bd63c721aef8323b62474e1f80d9aa63e57c0"
     sha256 cellar: :any_skip_relocation, mojave:        "7bac58c6ae404426fa9151c1ec3c2d944309792ce5c4e9a2d97d02eef4b5d20d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "01947d541fb75c87b9dffbdbaa21ac6ff6132b1e4af70d1b749f54fb29046db6"
   end
 
   depends_on "go" => :build
