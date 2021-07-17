f25d81ff3cf4a8c6abe45d5132670cd667813ff1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-17 17:38:50 +0000

    vala-language-server: add 0.48.3 bottle.

--- Formula/vala-language-server.rb
+++ Formula/vala-language-server.rb
@@ -6,6 +6,13 @@ class ValaLanguageServer < Formula
   sha256 "6f3b34bcb4e049c299ae3d5433153e4b685b0bace0ea8d761ffea266714ce841"
   license "LGPL-2.1-only"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "66e6aeafb3cebc38b59abc884dc2b5f8a477f7be38f5af65da07221810c2344b"
+    sha256 cellar: :any, big_sur:       "19023fd359d99b7d63e5ca8f1ace69973f1fa9b20478cb17fa83d405a5b265b2"
+    sha256 cellar: :any, catalina:      "c663948f5cef1359e3eb4b72ff1e27d756ebada28a224f7927a9564e681180b3"
+    sha256 cellar: :any, mojave:        "e68b665fffd86e8b10dfa803cf1ffa2ac8cb0a39ecdda1bacf15e48fc8c6be26"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
