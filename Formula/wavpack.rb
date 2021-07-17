73ac061b196a380a4e5d926f1ccbfa392e13ca15
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 16:14:52 +0000

    wavpack: update 5.4.0 bottle.

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -6,10 +6,11 @@ class Wavpack < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3e51f2f593fffc58cd550236d6a9711900841d388d6ac7833c09f272270d5156"
-    sha256 cellar: :any, big_sur:       "711535e6329af607e974ee9e3e957fd82b056af39e95a78226bd9e8f9a0025dd"
-    sha256 cellar: :any, catalina:      "29360357a18df2827e8acb41fdd874495b62f431deb230c96b8d5dcca5f6d316"
-    sha256 cellar: :any, mojave:        "89815dc5f1b4af0d647d2b1d3ad484ddce39208958bb315793240aa7be0c4565"
+    sha256 cellar: :any,                 arm64_big_sur: "3e51f2f593fffc58cd550236d6a9711900841d388d6ac7833c09f272270d5156"
+    sha256 cellar: :any,                 big_sur:       "711535e6329af607e974ee9e3e957fd82b056af39e95a78226bd9e8f9a0025dd"
+    sha256 cellar: :any,                 catalina:      "29360357a18df2827e8acb41fdd874495b62f431deb230c96b8d5dcca5f6d316"
+    sha256 cellar: :any,                 mojave:        "89815dc5f1b4af0d647d2b1d3ad484ddce39208958bb315793240aa7be0c4565"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6258f61ddd3478cbbeffc8cd939dd65222d209214a1f291859f2c00108039e3"
   end
 
   head do
