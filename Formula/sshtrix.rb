cb2a569194eec6033b35c54bcc988e20911798a8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:41:18 +0000

    sshtrix: update 0.0.3 bottle.

--- Formula/sshtrix.rb
+++ Formula/sshtrix.rb
@@ -11,12 +11,13 @@ class Sshtrix < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bb7eefcc513933225daa50cac41e6216d890910f5bec5f0003c20e9205082608"
-    sha256 cellar: :any, big_sur:       "b3962b5211858eb4f6e1478665bfbb578c1f9d1c393237b841f9261aab4cdbf9"
-    sha256 cellar: :any, catalina:      "e115c5a6f3378af5a0ab4297673bb7b659dd20054ab91d818b083ce13951e7da"
-    sha256 cellar: :any, mojave:        "a54f2c867dd6539824cc69888975d3cd2041b4b922183262d29fcdf655391cfa"
-    sha256 cellar: :any, high_sierra:   "dd567f106a7fe8a7a6f9e2474b284109e1dbcb14ed847163d1f65f4d69467f93"
-    sha256 cellar: :any, sierra:        "dafb3bc8c14e729cbbfbf8dc6a9ce789e01732a644aa84b243af24f2bd92ce19"
+    sha256 cellar: :any,                 arm64_big_sur: "bb7eefcc513933225daa50cac41e6216d890910f5bec5f0003c20e9205082608"
+    sha256 cellar: :any,                 big_sur:       "b3962b5211858eb4f6e1478665bfbb578c1f9d1c393237b841f9261aab4cdbf9"
+    sha256 cellar: :any,                 catalina:      "e115c5a6f3378af5a0ab4297673bb7b659dd20054ab91d818b083ce13951e7da"
+    sha256 cellar: :any,                 mojave:        "a54f2c867dd6539824cc69888975d3cd2041b4b922183262d29fcdf655391cfa"
+    sha256 cellar: :any,                 high_sierra:   "dd567f106a7fe8a7a6f9e2474b284109e1dbcb14ed847163d1f65f4d69467f93"
+    sha256 cellar: :any,                 sierra:        "dafb3bc8c14e729cbbfbf8dc6a9ce789e01732a644aa84b243af24f2bd92ce19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4dfdb5cd8d621a41c636565b8e775d48e418feaf2f5a07dc773902d41cd13dd5"
   end
 
   depends_on "libssh"
