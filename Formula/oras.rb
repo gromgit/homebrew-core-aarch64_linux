9116fdb4be48959629e6b13b03bf1f63111de5a2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 06:59:54 +0000

    oras: update 0.12.0 bottle.

--- Formula/oras.rb
+++ Formula/oras.rb
@@ -10,6 +10,7 @@ class Oras < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "18d86fcc6965357bb00cc945ef6ee888f874eb487bb341a1389cf8f9cac626d1"
     sha256 cellar: :any_skip_relocation, catalina:      "d08f9849ddb604f0fd61574afd8f2dad30fc38022b14b547c2abd50bb6ef97d9"
     sha256 cellar: :any_skip_relocation, mojave:        "ef76f4634fe95c6c000f90c3ddb323b8edf694f60e5e6eed4e416bef3b874f15"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64c8e274d78dad6bf79c17b8cdc2d5e31738ff761704bef3e7be4f550305ad9c"
   end
 
   depends_on "go" => :build
