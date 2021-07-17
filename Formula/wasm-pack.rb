b363c121a9a2a62d1069f56d313981e5110a1266
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-17 22:04:24 +0000

    wasm-pack: update 0.10.0 bottle.

--- Formula/wasm-pack.rb
+++ Formula/wasm-pack.rb
@@ -7,9 +7,10 @@ class WasmPack < Formula
   head "https://github.com/rustwasm/wasm-pack.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "49e5f6aec0c98af3fe8d3145ea5e70d036823a173a4f16ec0829504239c2f0e0"
-    sha256 cellar: :any_skip_relocation, catalina: "e3d7080de0f2a638a92818bceb7d744461bc4876f61c9e4432b23daae7c586ad"
-    sha256 cellar: :any_skip_relocation, mojave:   "29fab21c9cff3ce852b31580c4cd6777918fdaabd16124f0cfb9fdfdc02c3a60"
+    sha256 cellar: :any_skip_relocation, big_sur:      "49e5f6aec0c98af3fe8d3145ea5e70d036823a173a4f16ec0829504239c2f0e0"
+    sha256 cellar: :any_skip_relocation, catalina:     "e3d7080de0f2a638a92818bceb7d744461bc4876f61c9e4432b23daae7c586ad"
+    sha256 cellar: :any_skip_relocation, mojave:       "29fab21c9cff3ce852b31580c4cd6777918fdaabd16124f0cfb9fdfdc02c3a60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a4c5163c60c0a49e0de91a27af51bd0e329c5684a6c85c76aa6e5ca9d6f6a3d6"
   end
 
   depends_on "rust" => :build
