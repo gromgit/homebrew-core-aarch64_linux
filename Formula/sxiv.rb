b0bca26f504fb56a1b52d27945d07ef8c8a0c608
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 15:45:42 +0000

    sxiv: update 26_1 bottle.

--- Formula/sxiv.rb
+++ Formula/sxiv.rb
@@ -8,11 +8,12 @@ class Sxiv < Formula
   head "https://github.com/muennich/sxiv.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "11aff8aaab1a32a0694672b802f9399d5002f1871329054671273a2d919b4d5d"
-    sha256 cellar: :any, big_sur:       "0fbf88dbb8f6744d36254023302ea2c88521bd4b8b8172eff00c7dfe2bfd4495"
-    sha256 cellar: :any, catalina:      "caafa51424cd97f030b9156aeba0ba64f6ab5821197453136a240c7ca38869d9"
-    sha256 cellar: :any, mojave:        "14b4f8a7137ea1ff12dde1d0a8cda063227e48d77ba75d93ecbde6193584d2cf"
-    sha256 cellar: :any, high_sierra:   "b8f60f5b9bb6987f0042ac485eb0d4c5c5c3cdc4ea4c32fc13def537e51d39dc"
+    sha256 cellar: :any,                 arm64_big_sur: "11aff8aaab1a32a0694672b802f9399d5002f1871329054671273a2d919b4d5d"
+    sha256 cellar: :any,                 big_sur:       "0fbf88dbb8f6744d36254023302ea2c88521bd4b8b8172eff00c7dfe2bfd4495"
+    sha256 cellar: :any,                 catalina:      "caafa51424cd97f030b9156aeba0ba64f6ab5821197453136a240c7ca38869d9"
+    sha256 cellar: :any,                 mojave:        "14b4f8a7137ea1ff12dde1d0a8cda063227e48d77ba75d93ecbde6193584d2cf"
+    sha256 cellar: :any,                 high_sierra:   "b8f60f5b9bb6987f0042ac485eb0d4c5c5c3cdc4ea4c32fc13def537e51d39dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83e501a472f4a821202cfc62a9076c35655d340e127b6465384ceb9d535ddf86"
   end
 
   depends_on "giflib"
