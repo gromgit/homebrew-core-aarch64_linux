f041a92b6c9f636c845f9ad4fe5ec1e707448879
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 03:34:53 +0000

    mpop: update 1.4.13 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -10,6 +10,7 @@ class Mpop < Formula
     sha256 big_sur:       "8666c6f36ee3f3ed758139f4aceb22128b2c05c9ccaf47a538ef649d6daf598b"
     sha256 catalina:      "651b41a0c11959ff187840e7bb0f036912a2611555b12c2d6ca5536d9356bdb2"
     sha256 mojave:        "7e0f134fcfd8d0d032b53a24a9d6c19cc92f5856a1f5fbfc432e7bf13ec51dd8"
+    sha256 x86_64_linux:  "ad69b17457afb8dab7e6827efe8f78603eaa69a85073fdc02b0d457a2fc599e0"
   end
 
   depends_on "pkg-config" => :build
