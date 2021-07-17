45464a6a5d073247b2c080581580b56fc01456e7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-17 06:53:10 +0000

    nift: update 2.4.12 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -6,9 +6,10 @@ class Nift < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "643dbd50106a96d8af8e1071c49fb6c41522f7d3384d0e438810d48e5503c7ab"
-    sha256 cellar: :any, catalina: "dbf48067fac536bfe804c35c19c6198bae0c0d29107be3e4512d31a37485fd96"
-    sha256 cellar: :any, mojave:   "d51812440b4e4b8df56ee07b377e2d2a4cee7d84233377218e9ed3fc5e9e68e1"
+    sha256 cellar: :any,                 big_sur:      "643dbd50106a96d8af8e1071c49fb6c41522f7d3384d0e438810d48e5503c7ab"
+    sha256 cellar: :any,                 catalina:     "dbf48067fac536bfe804c35c19c6198bae0c0d29107be3e4512d31a37485fd96"
+    sha256 cellar: :any,                 mojave:       "d51812440b4e4b8df56ee07b377e2d2a4cee7d84233377218e9ed3fc5e9e68e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e8fed0b8f27357e6414545556caf0c1bffd34273b5966e17c8ef4f554eb2c4a8"
   end
 
   depends_on "luajit-openresty"
