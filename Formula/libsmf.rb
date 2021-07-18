1c8b828c99fb59abe04c21d5264a182ba48180aa
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 13:58:25 +0000

    libsmf: update 1.3_1 bottle.

--- Formula/libsmf.rb
+++ Formula/libsmf.rb
@@ -6,11 +6,12 @@ class Libsmf < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, big_sur:     "02243fbcfb6de40f0c04b2341132e19c946be2b9fdf017f1838b3043aeddcedb"
-    sha256 cellar: :any, catalina:    "fa858ef4b6b179d578663bbdb0d5c7490ea75a3921713e577a7f848faa99b601"
-    sha256 cellar: :any, mojave:      "bbe040e330a998499e078129097a07f2c5de9fff9c5f26a638e6f5248badda3b"
-    sha256 cellar: :any, high_sierra: "7a4b394b51e89bd781fcce0514b3cc58656da63fa2e317186e47828e2c271320"
-    sha256 cellar: :any, sierra:      "45aedd028eb76b2dfbb6fa3ba9b3fc809e7265411d5d7760997a71503ebae41a"
+    sha256 cellar: :any,                 big_sur:      "02243fbcfb6de40f0c04b2341132e19c946be2b9fdf017f1838b3043aeddcedb"
+    sha256 cellar: :any,                 catalina:     "fa858ef4b6b179d578663bbdb0d5c7490ea75a3921713e577a7f848faa99b601"
+    sha256 cellar: :any,                 mojave:       "bbe040e330a998499e078129097a07f2c5de9fff9c5f26a638e6f5248badda3b"
+    sha256 cellar: :any,                 high_sierra:  "7a4b394b51e89bd781fcce0514b3cc58656da63fa2e317186e47828e2c271320"
+    sha256 cellar: :any,                 sierra:       "45aedd028eb76b2dfbb6fa3ba9b3fc809e7265411d5d7760997a71503ebae41a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5670da1ec13772870636e0cfe96e851f021ebe997a3307725331cbea22062246"
   end
 
   depends_on "pkg-config" => :build
