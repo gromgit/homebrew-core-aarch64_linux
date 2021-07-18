22969d8e319c25fd79c95bd99f63f2a288498cb4
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 15:02:42 +0000

    sisc-scheme: update 1.16.6 bottle.

--- Formula/sisc-scheme.rb
+++ Formula/sisc-scheme.rb
@@ -9,6 +9,7 @@ class SiscScheme < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
     sha256 cellar: :any_skip_relocation, catalina:      "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
     sha256 cellar: :any_skip_relocation, mojave:        "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f65d81b8af3efb3351510362fdde92e8b9fc5a32eaba361a438abed4fb265991"
   end
 
   def install
