42d0448fd7dbd6e6de3a6966666d3112f5c1b4b9
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 13:57:28 +0000

    libdvdnav: update 6.1.1 bottle.

--- Formula/libdvdnav.rb
+++ Formula/libdvdnav.rb
@@ -5,10 +5,11 @@ class Libdvdnav < Formula
   sha256 "c191a7475947d323ff7680cf92c0fb1be8237701885f37656c64d04e98d18d48"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e3ea0ddda7b96b799c2a67fd6687c25679001e2dc3893f200c70d4a599bc3996"
-    sha256 cellar: :any, big_sur:       "cabd25ecc0df8a3729e7196737e56041d8d6b9f369972d66de1ade19b4bfbafb"
-    sha256 cellar: :any, catalina:      "ded7214f830c32676e5a64c2836b5498e44aeaa4967c5753a89c48af66edeaf7"
-    sha256 cellar: :any, mojave:        "4fe58e754e7174ef7013a89a0620e05b8131bd50ed1de2c54e8b837db81fc4de"
+    sha256 cellar: :any,                 arm64_big_sur: "e3ea0ddda7b96b799c2a67fd6687c25679001e2dc3893f200c70d4a599bc3996"
+    sha256 cellar: :any,                 big_sur:       "cabd25ecc0df8a3729e7196737e56041d8d6b9f369972d66de1ade19b4bfbafb"
+    sha256 cellar: :any,                 catalina:      "ded7214f830c32676e5a64c2836b5498e44aeaa4967c5753a89c48af66edeaf7"
+    sha256 cellar: :any,                 mojave:        "4fe58e754e7174ef7013a89a0620e05b8131bd50ed1de2c54e8b837db81fc4de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0aadfc77f6807e5067f04391b0da4b6a0173c0219552f90ee300e17bd5679d9"
   end
 
   head do
