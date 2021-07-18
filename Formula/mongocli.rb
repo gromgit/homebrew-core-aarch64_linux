d5e5628c4c9b8bc47e5b90e62c45b1d197ae00cc
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 14:23:48 +0000

    mongocli: update 1.19.0 bottle.

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -10,6 +10,7 @@ class Mongocli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "0d4898f1a7d22be2af306012ed4a8493ca99d480fb817541c3527a092b6d5964"
     sha256 cellar: :any_skip_relocation, catalina:      "6ae56c92041de89d54d7f5bd461091d68211ab86134c9042dbfad273979885a5"
     sha256 cellar: :any_skip_relocation, mojave:        "754c5f2e43d520522af351171376473f46707322e00f6d2776ee7c22957446b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56fa68e47f3116aa9390995e7c4190799dacb56f8d539bab39d734f2db087859"
   end
 
   depends_on "go" => :build
