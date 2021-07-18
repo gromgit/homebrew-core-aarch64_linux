916047833122bb784e8b1024a651f665adc49c62
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 13:57:27 +0000

    libcddb: update 1.3.2_4 bottle.

--- Formula/libcddb.rb
+++ Formula/libcddb.rb
@@ -7,10 +7,11 @@ class Libcddb < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "5c01ee6149ed61a23ad7d8a2c09250fedf3b605638552fe82057cf77b0ac61f1"
-    sha256 cellar: :any, big_sur:       "e19fbf67a440482346f40076ceae29a8b72590ef1376e6c5454d9f7814984e3b"
-    sha256 cellar: :any, catalina:      "ca3cb9caeed526ef59a167293871d7b739c2ee6271571225dd1640f4af101140"
-    sha256 cellar: :any, mojave:        "534e9e7afc756a552c414b224d86ffa84c9966bbccf3a7d781a6b55a482e9bdf"
+    sha256 cellar: :any,                 arm64_big_sur: "5c01ee6149ed61a23ad7d8a2c09250fedf3b605638552fe82057cf77b0ac61f1"
+    sha256 cellar: :any,                 big_sur:       "e19fbf67a440482346f40076ceae29a8b72590ef1376e6c5454d9f7814984e3b"
+    sha256 cellar: :any,                 catalina:      "ca3cb9caeed526ef59a167293871d7b739c2ee6271571225dd1640f4af101140"
+    sha256 cellar: :any,                 mojave:        "534e9e7afc756a552c414b224d86ffa84c9966bbccf3a7d781a6b55a482e9bdf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a96b2ab16f2b983fa13921bc81d7b368a594620efd857d84ee8fb1667a18799d"
   end
 
   depends_on "pkg-config" => :build
