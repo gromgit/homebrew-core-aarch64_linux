7480a787bfc3b647fb5d9d86fd532f70ba10a1f4
Henry Chu <henrytech@outlook.com>
2021-07-18 11:49:21 +0800

    fuse-overlayfs 1.6: new formula
    Closes #81444.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/fuse-overlayfs.rb
@@ -0,0 +1,33 @@
+class FuseOverlayfs < Formula
+  desc "FUSE implementation for overlayfs"
+  homepage "https://github.com/containers/fuse-overlayfs"
+  url "https://github.com/containers/fuse-overlayfs/archive/refs/tags/v1.6.tar.gz"
+  sha256 "5606c5c1e4e0d7063f3f50a72c906f23b7a505784d20dc40fff9e4b2b802d5f4"
+  license "GPL-3.0-or-later"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "libfuse"
+  depends_on :linux
+
+  def install
+    system "autoreconf", "-fis"
+    system "./configure", "--prefix=/", "--sbindir=/sbin"
+    system "make"
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  test do
+    mkdir "lowerdir/a"
+    mkdir "lowerdir/b"
+    mkdir "up"
+    mkdir "workdir"
+    mkdir "merged"
+    test_cmd = "fuse-overlayfs -o lowerdir=lowerdir/a:lowerdir/b,upperdir=up,workdir=workdir merged 2>&1"
+    output = shell_output(test_cmd, 1)
+    assert_match "fuse: device not found, try 'modprobe fuse' first", output
+    assert_match "fuse-overlayfs: cannot mount: No such file or directory", output
+  end
+end
