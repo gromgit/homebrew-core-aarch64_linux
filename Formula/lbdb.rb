410646768ccce89c885470cadfb66c59ab0fc1db
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 13:56:39 +0000

    lbdb: update 0.49 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -16,6 +16,7 @@ class Lbdb < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "9c79b0bbc8d13e16259bce83501a2d3f285f3ca2c6675f49cdb36a4ef4366fa1"
     sha256 cellar: :any_skip_relocation, mojave:        "71c3c794994b1ec594bdbbef70848c0e5bb43ab1f19e1edff7e0d17e9a2f9e98"
     sha256 cellar: :any_skip_relocation, high_sierra:   "03f4287b844059e3bc9787c97bc22b903a57067add3c3fe2913d2ca53537d34f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16f661d1451e25627a72e7eb3dd822179a0ec3b4fe6e1212c6e0ae9de4eddf4a"
   end
 
   depends_on "abook"
