220de1f14aab2f949c8829efd624e5934ad584ff
Henry Chu <henrytech@outlook.com>
2021-07-18 10:49:07 +0800

    conmon 2.0.29: new formula
    Closes #81442.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/conmon.rb
@@ -0,0 +1,22 @@
+class Conmon < Formula
+  desc "OCI container runtime monitor"
+  homepage "https://github.com/containers/conmon"
+  url "https://github.com/containers/conmon/archive/refs/tags/v2.0.29.tar.gz"
+  sha256 "eb4d5e157671a61b88786e44094775194e30e1d0ad0b9d50035532ece78dbc28"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "glib"
+  depends_on :linux
+  depends_on "systemd"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}", "LIBEXECDIR=#{libexec}"
+  end
+
+  test do
+    assert_match "conmon: Container ID not provided. Use --cid", shell_output("conmon 2>&1", 1)
+  end
+end
