33b73e89e596d33a59c7b7039ab4f1af811401f3
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 14:03:38 +0000

    lima: update 0.5.0 bottle.

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -10,6 +10,7 @@ class Lima < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "f9045fddf6a0d61c061136284e656e2e6ce36c5a659e73917448412b9a540813"
     sha256 cellar: :any_skip_relocation, catalina:      "500df539541fc9870bad905df46619f129c146d43ac50e3b15dcbfa205caf3b4"
     sha256 cellar: :any_skip_relocation, mojave:        "310bf7d4c23bc70c2d83ad319a2eba9d11cf43f4e9adfab3fbc09dd679d55377"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "061affb4464aae135909a465c1248876bb59eb90b89c21811a5bfefd440c991c"
   end
 
   depends_on "go" => :build
