95dac6c787fce3fa69fa7941bef2583eadfb13b2
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 15:03:15 +0000

    server-go: update 1.1.72 bottle.

--- Formula/server-go.rb
+++ Formula/server-go.rb
@@ -12,9 +12,10 @@ class ServerGo < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "602538b6b3e893a1d9176ebc7a555c9dc92706a05efdbb1175aafae3f673bc75"
-    sha256 cellar: :any_skip_relocation, catalina: "741c6e3b54c1cba333b5b47cc11de242777b3cf98caf217a8416deb6009207da"
-    sha256 cellar: :any_skip_relocation, mojave:   "0200890e437d3b0e8f708764e4c088206909fff421ef8a443aa15c52f3e8e96b"
+    sha256 cellar: :any_skip_relocation, big_sur:      "602538b6b3e893a1d9176ebc7a555c9dc92706a05efdbb1175aafae3f673bc75"
+    sha256 cellar: :any_skip_relocation, catalina:     "741c6e3b54c1cba333b5b47cc11de242777b3cf98caf217a8416deb6009207da"
+    sha256 cellar: :any_skip_relocation, mojave:       "0200890e437d3b0e8f708764e4c088206909fff421ef8a443aa15c52f3e8e96b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "377df189aff65121c492bfa741a38ea6c1a1c49b23994d11a820afe4498e736a"
   end
 
   depends_on "go" => :build
