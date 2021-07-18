f2675e11b3deb293e1f28a81680826b8825dfe88
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 14:25:49 +0000

    mbelib: update 1.3.0 bottle.

--- Formula/mbelib.rb
+++ Formula/mbelib.rb
@@ -7,14 +7,15 @@ class Mbelib < Formula
   head "https://github.com/szechyjs/mbelib.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "053dd044423318deba18dbccbbd1d85efec94b507dd5646beb7b6c3d32064010"
-    sha256 cellar: :any, big_sur:       "508ed0ed1f9603c7c3e50accea0e201d391f673b63a4acb71574827fddcbb1ef"
-    sha256 cellar: :any, catalina:      "fb29c40fb9af7c0303d9f7929e61941e8c10c8aad57662f366a671d3a73be116"
-    sha256 cellar: :any, mojave:        "85f9f705e2e25ea205b637ad34bdc1e3d24734e646e6e6e53d39ab085a691303"
-    sha256 cellar: :any, high_sierra:   "710bc1a0458b96c12c0a3b675a3410b1d86257ceb36370fd94952891e1a9b744"
-    sha256 cellar: :any, sierra:        "45f0f9fafbe773fab43f621c62ce0c117c1d9a01fe32528b8b18fa6e94671a22"
-    sha256 cellar: :any, el_capitan:    "8cd7158aaccceca6fe78a8031f1d58189b269b0dee86a10c349d3d514c4e33e2"
-    sha256 cellar: :any, yosemite:      "30b0fc540b32e244a1cf26719d97a4c57432dbd1537b22a3651fa3d43e1d285b"
+    sha256 cellar: :any,                 arm64_big_sur: "053dd044423318deba18dbccbbd1d85efec94b507dd5646beb7b6c3d32064010"
+    sha256 cellar: :any,                 big_sur:       "508ed0ed1f9603c7c3e50accea0e201d391f673b63a4acb71574827fddcbb1ef"
+    sha256 cellar: :any,                 catalina:      "fb29c40fb9af7c0303d9f7929e61941e8c10c8aad57662f366a671d3a73be116"
+    sha256 cellar: :any,                 mojave:        "85f9f705e2e25ea205b637ad34bdc1e3d24734e646e6e6e53d39ab085a691303"
+    sha256 cellar: :any,                 high_sierra:   "710bc1a0458b96c12c0a3b675a3410b1d86257ceb36370fd94952891e1a9b744"
+    sha256 cellar: :any,                 sierra:        "45f0f9fafbe773fab43f621c62ce0c117c1d9a01fe32528b8b18fa6e94671a22"
+    sha256 cellar: :any,                 el_capitan:    "8cd7158aaccceca6fe78a8031f1d58189b269b0dee86a10c349d3d514c4e33e2"
+    sha256 cellar: :any,                 yosemite:      "30b0fc540b32e244a1cf26719d97a4c57432dbd1537b22a3651fa3d43e1d285b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2aa416bb9571e03c0bb7b877a44f08e874f069367367ca6c3bd6cefb87ecd70d"
   end
 
   depends_on "cmake" => :build
