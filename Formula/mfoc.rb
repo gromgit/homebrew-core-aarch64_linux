d70e748b6b04659d42f0cbac9e493a2af7419f3a
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-18 14:26:11 +0000

    mfoc: update 0.10.7_1 bottle.

--- Formula/mfoc.rb
+++ Formula/mfoc.rb
@@ -7,11 +7,12 @@ class Mfoc < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3cc80a2304a700b31494408fe1ee6472f51c8e5b10923b3ebd4eb912e0de6856"
-    sha256 cellar: :any, big_sur:       "c125e9e825aab3635d44128051d40413637725c6eded47b89c3727f3b8c04621"
-    sha256 cellar: :any, catalina:      "14c431c29b0b0e746d1533606ab13097a84b853c13d4399672027cf9256dad32"
-    sha256 cellar: :any, mojave:        "ff9f6c43ef70b8ae6fee40c43cf5f0acd6f72acd5507874e75d82703aeed5fc3"
-    sha256 cellar: :any, high_sierra:   "83a0236f5971e007e67e620730d458f8dcdcb7ff7770cc97c07407a771dbf69a"
+    sha256 cellar: :any,                 arm64_big_sur: "3cc80a2304a700b31494408fe1ee6472f51c8e5b10923b3ebd4eb912e0de6856"
+    sha256 cellar: :any,                 big_sur:       "c125e9e825aab3635d44128051d40413637725c6eded47b89c3727f3b8c04621"
+    sha256 cellar: :any,                 catalina:      "14c431c29b0b0e746d1533606ab13097a84b853c13d4399672027cf9256dad32"
+    sha256 cellar: :any,                 mojave:        "ff9f6c43ef70b8ae6fee40c43cf5f0acd6f72acd5507874e75d82703aeed5fc3"
+    sha256 cellar: :any,                 high_sierra:   "83a0236f5971e007e67e620730d458f8dcdcb7ff7770cc97c07407a771dbf69a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42a1e94b179175e30631ae1d85e59a8d106def94007da48b2c98ccf09e16b13f"
   end
 
   depends_on "pkg-config" => :build
