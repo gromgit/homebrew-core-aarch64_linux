295e134de0a63f3eb9c38b059dc5c418bf0a6ded
miccal <miccal.matthews@gmail.com>
2021-07-19 05:23:21 +0800

    imagemagick@6 6.9.12-19
    Closes #81474.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -1,8 +1,8 @@
 class ImagemagickAT6 < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://legacy.imagemagick.org/"
-  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.12-18.tar.xz"
-  sha256 "518deb588bc9c0b767acc76f196c4f8129a4927c086df9d9dc7fd96e953ed6c5"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.12-19.tar.xz"
+  sha256 "18467901b2266d8918c3d977b09b824213a6b388afaf14b21a890390cb16b9b7"
   license "ImageMagick"
   head "https://github.com/imagemagick/imagemagick6.git"
 
