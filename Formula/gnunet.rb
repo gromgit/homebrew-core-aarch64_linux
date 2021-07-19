5edeb2a47a434953c0dd3a1b97de5a006d8e1e00
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-19 05:40:54 +0000

    gnunet: update 0.14.1 bottle.

--- Formula/gnunet.rb
+++ Formula/gnunet.rb
@@ -7,10 +7,11 @@ class Gnunet < Formula
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "913d3a808c3beaacaa7aa0307c200af943f539bc5203a4b2c62e6e6e9919eee8"
-    sha256 cellar: :any, big_sur:       "b97c192441e2b2836c0dfead9f2e85cfea8ae0889e34510436be4b3dc427b944"
-    sha256 cellar: :any, catalina:      "fa620c92930b29e01dc564f14867462126cf508692be8b9c3ca5c716419c6f7c"
-    sha256 cellar: :any, mojave:        "8f032fccf8877f461e5ccf87d059a9649ca056493b984a931ddc97317f45512a"
+    sha256 cellar: :any,                 arm64_big_sur: "913d3a808c3beaacaa7aa0307c200af943f539bc5203a4b2c62e6e6e9919eee8"
+    sha256 cellar: :any,                 big_sur:       "b97c192441e2b2836c0dfead9f2e85cfea8ae0889e34510436be4b3dc427b944"
+    sha256 cellar: :any,                 catalina:      "fa620c92930b29e01dc564f14867462126cf508692be8b9c3ca5c716419c6f7c"
+    sha256 cellar: :any,                 mojave:        "8f032fccf8877f461e5ccf87d059a9649ca056493b984a931ddc97317f45512a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "77d500a6ec053927f4df368c4a9c230844b50f30e787d14a268becf8d6c95355"
   end
 
   depends_on "pkg-config" => :build
