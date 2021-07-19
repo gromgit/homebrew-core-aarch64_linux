a677e060d843d2d869f086bddba8bfadbf9cc07a
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-19 22:02:30 +0000

    net-tools: add 2.10 bottle.

--- Formula/net-tools.rb
+++ Formula/net-tools.rb
@@ -5,6 +5,11 @@ class NetTools < Formula
   sha256 "b262435a5241e89bfa51c3cabd5133753952f7a7b7b93f32e08cb9d96f580d69"
   license "GPL-2.0-or-later"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e08cb177a10583b6af76c29fa3c49c930d057aaa759946b72a579205d3fc093f"
+  end
+
   depends_on "libdnet"
   depends_on :linux
 
