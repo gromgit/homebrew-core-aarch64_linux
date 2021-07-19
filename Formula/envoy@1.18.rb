aca3d99f59b4dd7e3120c6b0086307ddf41e3ca6
Adrian Cole <adrian@tetrate.io>
2021-07-19 13:23:37 +1000

    envoy@1.18 1.18.3 (new formula)
    This replaces envoy@1.17.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/envoy@1.18.rb
+++ Formula/envoy@1.18.rb
@@ -1,19 +1,14 @@
-class EnvoyAT117 < Formula
+class EnvoyAT118 < Formula
   desc "Cloud-native high-performance edge/middle/service proxy"
-  homepage "https://www.envoyproxy.io"
+  homepage "https://www.envoyproxy.io/index.html"
   url "https://github.com/envoyproxy/envoy.git",
-      tag:      "v1.17.3",
-      revision: "46bf743b97d0d3f01ff437b2f10cc0bd9cdfe6e4"
+      tag:      "v1.18.3",
+      revision: "98c1c9e9a40804b93b074badad1cdf284b47d58b"
   license "Apache-2.0"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "e82ae3a1c65513df10b3db1a8e77d28cd213b694cd25d0e0c5315e7437269407"
-    sha256 cellar: :any_skip_relocation, catalina: "e5ad329ea6f0e29890db94a5fe57ba9be76bc9c033eda6a2e4f41e08a3f3311f"
-  end
 
   keg_only :versioned_formula
   # https://github.com/envoyproxy/envoy/blob/main/RELEASES.md#release-schedule
-  deprecate! date: "2022-01-11", because: :unsupported
+  deprecate! date: "2022-04-15", because: :unsupported
 
   depends_on "automake" => :build
   depends_on "bazelisk" => :build
@@ -24,15 +19,20 @@ class EnvoyAT117 < Formula
   depends_on "ninja" => :build
   depends_on macos: :catalina
 
+  # Work around xcode 12 incompatibility until envoyproxy/envoy#17393
+  patch do
+    url "https://github.com/envoyproxy/envoy/commit/3b49166dc0841b045799e2c37bdf1ca9de98d5b1.patch?full_index=1"
+    sha256 "e65fe24a29795606ea40aaa675c68751687e72911b737201e9714613b62b0f02"
+  end
+
   def install
-    args = %w[
+    args = %W[
       -c
       opt
       --curses=no
       --show_task_finish
       --verbose_failures
-      --action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin
-      --test_output=all
+      --action_env=PATH=#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin
     ]
     system Formula["bazelisk"].opt_bin/"bazelisk", "build", *args, "//source/exe:envoy-static"
     bin.install "bazel-bin/source/exe/envoy-static" => "envoy"
