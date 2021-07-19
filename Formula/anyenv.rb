b97c32fe524e96d4fb677d3dc7e7d00700441d30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-19 09:11:48 +0000

    anyenv: update 1.1.4 bottle.

--- Formula/anyenv.rb
+++ Formula/anyenv.rb
@@ -6,11 +6,11 @@ class Anyenv < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e29c4f22d906b1a84c4ba3a088d5e4ffee99254cd528fb94e1a1a3d8d60bb307"
-    sha256 cellar: :any_skip_relocation, big_sur:       "381377f93374c1b1c1b0154c5f08c91dbf33a948a2ac47933a4bbd5d7c0d8ee8"
-    sha256 cellar: :any_skip_relocation, catalina:      "381377f93374c1b1c1b0154c5f08c91dbf33a948a2ac47933a4bbd5d7c0d8ee8"
-    sha256 cellar: :any_skip_relocation, mojave:        "381377f93374c1b1c1b0154c5f08c91dbf33a948a2ac47933a4bbd5d7c0d8ee8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9125ca9a0f5816044c54fb7cf41366633ef78d233172d3039e7f9bd172847874"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "64a65deada2b73eea41aaf1072d015c193e46cc38d524e3770d8e319b56b54fe"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
+    sha256 cellar: :any_skip_relocation, catalina:      "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
+    sha256 cellar: :any_skip_relocation, mojave:        "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64a65deada2b73eea41aaf1072d015c193e46cc38d524e3770d8e319b56b54fe"
   end
 
   def install
