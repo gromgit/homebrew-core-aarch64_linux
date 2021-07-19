fcac675830c4471de1931a25ad6a78fa08bbc2e1
Caleb Xu <calebcenter@live.com>
2021-07-19 11:10:21 -0400

    net-tools: fix non-interactive configuration step
    Closes #81510.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/net-tools.rb
+++ Formula/net-tools.rb
@@ -5,12 +5,14 @@ class NetTools < Formula
   sha256 "b262435a5241e89bfa51c3cabd5133753952f7a7b7b93f32e08cb9d96f580d69"
   license "GPL-2.0-or-later"
 
-  depends_on "gettext" => :build
   depends_on "libdnet"
   depends_on :linux
 
   def install
-    system "yes '' | make config"
+    # Support non-interactive configuration
+    inreplace "configure.sh", "IFS='@' read ans || exit 1", ""
+
+    system "make", "config"
     system "make"
     system "make", "DESTDIR=#{prefix}", "install"
   end
