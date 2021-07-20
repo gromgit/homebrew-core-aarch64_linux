00191993499f473acecdaf256c276ecbbe183449
Charlie <2747302+CharlieC3@users.noreply.github.com>
2021-07-20 16:07:36 -0400

    clarinet 0.14.2
    * clarinet 0.14.2
    * clarinet revert license change
    
    Closes #81575.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -1,8 +1,8 @@
 class Clarinet < Formula
   desc "Command-line tool and runtime for the Clarity smart contract language"
   homepage "https://github.com/hirosystems/clarinet"
-  url "https://github.com/hirosystems/clarinet/archive/v0.14.1.tar.gz"
-  sha256 "33ff6bc312f27fc450406091b67fac16be48b553b309f61538e8d0b16dbbb191"
+  url "https://github.com/hirosystems/clarinet/archive/v0.14.2.tar.gz"
+  sha256 "a3335960bd7c2529b7a11744205030ce849ccaa539f34da57d13c1fff1379d38"
   license "GPL-3.0-only"
   head "https://github.com/hirosystems/clarinet.git", branch: "main"
 
