da3db9c2f7d32400a8f1ee0ab6e3775d7c5baf32
Michael Cho <cho-m@tuta.io>
2021-07-19 22:54:00 -0700

    erlang@21: revision bump (wxmac -> wxwidgets)

--- Formula/erlang@21.rb
+++ Formula/erlang@21.rb
@@ -5,7 +5,7 @@ class ErlangAT21 < Formula
   url "https://github.com/erlang/otp/releases/download/OTP-21.3.8.24/otp_src_21.3.8.24.tar.gz"
   sha256 "a82de871d7ba40fd256558b23a3b4c1539e6c7ece7507d6eb2b00330c6135012"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
@@ -25,7 +25,7 @@ class ErlangAT21 < Formula
   depends_on "libtool" => :build
   depends_on arch: :x86_64
   depends_on "openssl@1.1"
-  depends_on "wxmac" # for GUI apps like observer
+  depends_on "wxwidgets" # for GUI apps like observer
 
   resource "man" do
     url "https://www.erlang.org/download/otp_doc_man_21.3.tar.gz"
