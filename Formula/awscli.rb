6b2a07a97d0a8964b4d203eedcddbfae887b4926
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-20 23:08:33 +0000

    awscli: update 2.2.21 bottle.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -9,11 +9,11 @@ class Awscli < Formula
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "3e8fba8b885387ee5faf5e08d27f1da792a2932abaa9335b6896233a1996ae18"
-    sha256                               big_sur:       "9c5088e48a0d91e1090b7e14d9e467e4fbc9dc36bde73f8c28ea4fc5cf52c617"
-    sha256                               catalina:      "c1db2e159f78cfd0db5d18bfd233708802d29df665ef08d9f49c39047f294800"
-    sha256                               mojave:        "e83481f4c0cf3ec443dcae7d25057fd12e46186e83f8c90c2ec4c050976e002e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8fac0c61931142d310a18b27632736ea6d28a5e560df4f2a8a46959d845e9663"
+    sha256 cellar: :any,                 arm64_big_sur: "025500318aa04af00d79c23da3d71fed3565ad40b6f07b0669cdd5655b73c73b"
+    sha256                               big_sur:       "9f5b5037d5eed44c7d86cf8b6d7c27cb47eb8350de18654da98e88749dcd2e4b"
+    sha256                               catalina:      "955204f33ad8e9207f96fd45bf5351174a4bc341df93d6e426a7b1651f34191a"
+    sha256                               mojave:        "4249963c3145597a0d34bb2c635b1eb393ca7d21bba6c68c292abd50a53814c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0517b17c74804a786b63d8b40bab0703167aa0052aad9858d6eafb7035f66d2a"
   end
 
   depends_on "cmake" => :build
