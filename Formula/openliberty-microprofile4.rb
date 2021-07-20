9eee9ca05425ecb1e873ae26d031f32d2389f8ec
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-20 12:11:50 +0000

    openliberty-microprofile4: add 21.0.0.7 bottle.

--- Formula/openliberty-microprofile4.rb
+++ Formula/openliberty-microprofile4.rb
@@ -10,6 +10,10 @@ class OpenlibertyMicroprofile4 < Formula
     regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "dbac7a81fcba8b3badeadcd0311a2fb2d4efe80fc114cb5068bdbe4a5269e12d"
+  end
+
   depends_on "openjdk"
 
   def install
