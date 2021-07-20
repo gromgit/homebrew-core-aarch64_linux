67ff8040197c56cd4b9c96ffdb118c405d56a1d5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-20 14:44:09 +0800

    composer: use `:default_prefix` as `pour_bottle?` reason
    See Homebrew/brew#11742.

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -19,11 +19,8 @@ class Composer < Formula
   end
 
   # Keg-relocation breaks the formula when it replaces `/usr/local` with a non-default prefix
-  pour_bottle? do
-    on_macos do
-      reason "The bottle needs to be installed into `#{Homebrew::DEFAULT_PREFIX}` on Intel macOS."
-      satisfy { HOMEBREW_PREFIX.to_s == Homebrew::DEFAULT_PREFIX || Hardware::CPU.arm? }
-    end
+  on_macos do
+    pour_bottle? only_if: :default_prefix if Hardware::CPU.intel?
   end
 
   uses_from_macos "php"
