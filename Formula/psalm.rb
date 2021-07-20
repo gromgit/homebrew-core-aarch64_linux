6c026d72e51a9915451483cc515834297d394b48
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-20 14:44:11 +0800

    psalm: use `:default_prefix` as `pour_bottle?` reason
    See Homebrew/brew#11742.

--- Formula/psalm.rb
+++ Formula/psalm.rb
@@ -12,17 +12,14 @@ class Psalm < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "a54b71a894f31a2c1e5cae10fb0e394fcdc5f874c7d4ce6c66533a4a13605241"
   end
 
-  # Keg-relocation breaks the formula when it replaces `/usr/local` with a non-default prefix
-  pour_bottle? do
-    on_macos do
-      reason "The bottle needs to be installed into `#{Homebrew::DEFAULT_PREFIX}` on Intel macOS."
-      satisfy { HOMEBREW_PREFIX.to_s == Homebrew::DEFAULT_PREFIX || Hardware::CPU.arm? }
-    end
-  end
-
   depends_on "composer" => :test
 
   uses_from_macos "php"
+
+  # Keg-relocation breaks the formula when it replaces `/usr/local` with a non-default prefix
+  on_macos do
+    pour_bottle? only_if: :default_prefix if Hardware::CPU.intel?
+  end
 
   def install
     bin.install "psalm.phar" => "psalm"
