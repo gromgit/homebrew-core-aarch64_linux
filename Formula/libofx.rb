f45c8d599b74218c0d88a27be7d5fb9224c8377e
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-07-20 15:15:16 +0000

    libofx: update 0.10.2 bottle.

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -15,6 +15,7 @@ class Libofx < Formula
     sha256 big_sur:       "ff79aca1e58097d1eda8e2bd0b67d76e897a4146b1be0355adb38528911ec526"
     sha256 catalina:      "0c663487948a3090f30bb2f9d034132c9c5ff7c034efc703c49746d573e22d45"
     sha256 mojave:        "5a8264996840f6844933633f245463f7d6c7a5c625d6688a89083a393d04f650"
+    sha256 x86_64_linux:  "f56550e600a49e15e7e86b6adbd6fc9ec21d8e3b248588449f9df0ac1e2047be"
   end
 
   depends_on "open-sp"
