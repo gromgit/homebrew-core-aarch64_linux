0090779d59d0ff3655f189b95d748baf54b401cf
Jim Derry <balthisar@gmail.com>
2021-07-21 18:43:25 -0400

    tidy-html5: fix checksum (#81646)
    Fixes sha256 of the release, due to improper retagging of the release on our end.

--- Formula/tidy-html5.rb
+++ Formula/tidy-html5.rb
@@ -2,7 +2,7 @@ class TidyHtml5 < Formula
   desc "Granddaddy of HTML tools, with support for modern standards"
   homepage "https://www.html-tidy.org/"
   url "https://github.com/htacg/tidy-html5/archive/5.8.0.tar.gz"
-  sha256 "2fc78c4369cde9a80f4ae3961880bd003ac31e8b160f6b9422645bab3be5a6cf"
+  sha256 "59c86d5b2e452f63c5cdb29c866a12a4c55b1741d7025cf2f3ce0cde99b0660e"
   license "Zlib"
   head "https://github.com/htacg/tidy-html5.git", branch: "next"
 
