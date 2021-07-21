f16783f23c663d651e6ccc1e1c0ab28bc0485690
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-21 13:53:46 +0000

    ns-3: add 3.34 bottle.

--- Formula/ns-3.rb
+++ Formula/ns-3.rb
@@ -5,6 +5,14 @@ class Ns3 < Formula
   sha256 "a565d46a73ff7de68808535d93884f59a6ed7c9faa94de1248ed4f59fb6d5d3d"
   license "GPL-2.0-only"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_big_sur: "fd95713bcbe8aed7dbf18ad8331eb5008e9701795f625049ba8a9d19b5fe70e2"
+    sha256 cellar: :any,                 big_sur:       "fc184536dfacb01cf9293483168462d7dcf3eaf568c34eef015af49c0346bd93"
+    sha256 cellar: :any,                 catalina:      "c0dcd1239ea1897664ff798bc3c940828469121859cb8fa1aa8c6973974dc8f2"
+    sha256 cellar: :any,                 mojave:        "998ffc954790eb1e4870117b8fd06c44875578f0520e23d19cc07867cdb5cc68"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7d2d4dbeaf9749639f2015b6c19f36d00906dea01cf82a75475992752cd705c"
+  end
+
   depends_on "boost" => :build
   depends_on "python@3.9" => [:build, :test]
 
