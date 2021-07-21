ea2383dd0ff3ded926926c4eafd589c4b685cecc
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-07-21 00:45:54 +0000

    fcl: update 0.6.1 bottle.

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -21,11 +21,12 @@ class Fcl < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "8df5c85bcdd524bda6a6d9d90a862de8e517e79222371fad9593013d9abbdec2"
-    sha256 big_sur:       "64e20a01b7a05f081b6b01ebe88722a0afec56514940130c0720e2ee5e5325df"
-    sha256 catalina:      "05a5dfa094009376e5915ad14289490fa370462153102eb43e402d50663a23f4"
-    sha256 mojave:        "7fc28b6f1bd196e83873f61617e590af68ebf861cfc76af9d892c8ef40b25601"
-    sha256 high_sierra:   "625f6117a551777a1f12eba3253886a441a5a00e2759218b9566d40bb9f3ab2c"
+    sha256                               arm64_big_sur: "8df5c85bcdd524bda6a6d9d90a862de8e517e79222371fad9593013d9abbdec2"
+    sha256                               big_sur:       "64e20a01b7a05f081b6b01ebe88722a0afec56514940130c0720e2ee5e5325df"
+    sha256                               catalina:      "05a5dfa094009376e5915ad14289490fa370462153102eb43e402d50663a23f4"
+    sha256                               mojave:        "7fc28b6f1bd196e83873f61617e590af68ebf861cfc76af9d892c8ef40b25601"
+    sha256                               high_sierra:   "625f6117a551777a1f12eba3253886a441a5a00e2759218b9566d40bb9f3ab2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66c5eebffe537c48af33a456b80579565df2439c13ba11f7ed7d7a9875669117"
   end
 
   depends_on "cmake" => :build
