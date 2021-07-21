cd3d21ba3a2c1041b64191b82cee3bffcbe2efdf
Eric Nielsen <eric@amalgamar.com.br>
2021-07-21 18:15:39 -0500

    asdf: install into libexec
    because asdf will only partially work if the ASDF_DIR is resolved to
    `/usr/local/`. E.g. just running `asdf` will print
    
        cat: /usr/local/VERSION: No such file or directory
        version:
    
        cat: /usr/local/help.txt: No such file or directory
    
        PLUGIN direnv
          asdf direnv
          asdf direnv hook asdf
    
        "Late but latest"
        -- Rajinikanth
    
    instead of the version and the complete help message.
    
    It should be installed as the instructions in
    https://asdf-vm.com/#/core-manage-asdf?id=add-to-your-shell
    as the ASDF_DIR is resolved in
    https://github.com/asdf-vm/asdf/blob/ccf76dd7e60de8a3de03cd3574f570dc1f9dbc55/lib/utils.bash#L24-L29
    after utils.bash is sourced in
    https://github.com/asdf-vm/asdf/blob/ccf76dd7e60de8a3de03cd3574f570dc1f9dbc55/bin/asdf#L4
    
    So to resolve the ASDF_DIR correctly as `/usr/local/opt/asdf`, the
    `/usr/local/opt/asdf/bin/asdf` script must be directly executed.
    
    If lib and bin files were symlinked instead, then `/usr/local/bin/asdf`
    would be executed, resolving the ASDF_DIR incorrectly to `/usr/local`.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -4,6 +4,7 @@ class Asdf < Formula
   url "https://github.com/asdf-vm/asdf/archive/v0.8.1.tar.gz"
   sha256 "6ca280287dcb687ec12f0c37e4e193de390cdab68f2b2a0e271e3a4f1e20bd2e"
   license "MIT"
+  revision 1
   head "https://github.com/asdf-vm/asdf.git"
 
   bottle do
@@ -19,19 +20,37 @@ class Asdf < Formula
   depends_on "readline"
   depends_on "unixodbc"
 
-  conflicts_with "homeshick",
-    because: "asdf and homeshick both install files in lib/commands"
-
   def install
     bash_completion.install "completions/asdf.bash"
     fish_completion.install "completions/asdf.fish"
     zsh_completion.install "completions/_asdf"
-    libexec.install "bin/private"
-    prefix.install Dir["*"]
-    touch prefix/"asdf_updates_disabled"
+    libexec.install Dir["*"]
+    touch libexec/"asdf_updates_disabled"
+
+    # TODO: Remove these placeholders on 31 August 2022
+    bin.write_exec_script libexec/"bin/asdf"
+    (prefix/"asdf.sh").write ". #{libexec}/asdf.sh\n"
+    (prefix/"asdf.fish").write "source #{libexec}/asdf.fish\n"
+    (lib/"asdf.sh").write ". #{libexec}/lib/asdf.sh\n"
+    (lib/"asdf.fish").write "source #{libexec}/lib/asdf.fish\n"
+  end
+
+  def caveats
+    s = "To use asdf, add the following line to your #{shell_profile}:\n"
+
+    s += if preferred == :fish
+      "  source #{opt_libexec}/asdf.fish\n\n"
+    else
+      "  . #{opt_libexec}/asdf.sh\n\n"
+    end
+
+    s += "Restart your terminal for the settings to take effect."
+
+    s
   end
 
   test do
+    assert_match version.to_s, shell_output("#{bin}/asdf version")
     output = shell_output("#{bin}/asdf plugin-list 2>&1", 1)
     assert_match "No plugins installed", output
     assert_match "Update command disabled.", shell_output("#{bin}/asdf update", 42)
