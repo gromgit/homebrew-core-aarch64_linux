a1056c165cf8272e5076abd36e1bc16b46ccce3d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-21 19:47:22 +0000

    gtkmm: update 2.24.5_8 bottle.

--- Formula/gtkmm.rb
+++ Formula/gtkmm.rb
@@ -13,10 +13,11 @@ class Gtkmm < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "ae2cb84a696c040281a8131961c755865f4ba96ef223e77cf8e5a8d02b88edb8"
-    sha256 cellar: :any, big_sur:       "cf3e818aadeda99afd5c51cdfd8ae950bdf56ce16c090d78f23e5a80631f6f13"
-    sha256 cellar: :any, catalina:      "bc967efcc4b25a56a79089c73db15a7fc61d5d83a62bd5c899777f7169f2e437"
-    sha256 cellar: :any, mojave:        "1ed0b8b0445bcb223f2d20112004ead1c8b5d598f9e0012180831e069375b6f6"
+    sha256 cellar: :any,                 arm64_big_sur: "ae2cb84a696c040281a8131961c755865f4ba96ef223e77cf8e5a8d02b88edb8"
+    sha256 cellar: :any,                 big_sur:       "cf3e818aadeda99afd5c51cdfd8ae950bdf56ce16c090d78f23e5a80631f6f13"
+    sha256 cellar: :any,                 catalina:      "bc967efcc4b25a56a79089c73db15a7fc61d5d83a62bd5c899777f7169f2e437"
+    sha256 cellar: :any,                 mojave:        "1ed0b8b0445bcb223f2d20112004ead1c8b5d598f9e0012180831e069375b6f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "81c914f8dc798c013483fb31330a152eb4c1bdc62bd68a0a96de61202c94659b"
   end
 
   depends_on "pkg-config" => :build
