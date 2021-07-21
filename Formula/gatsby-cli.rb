bf5d70510a52881b2669f41ec91dbe631424e94a
Rui Chen <rui@chenrui.dev>
2021-07-21 12:24:05 -0400

    gatsby-cli 3.10.0

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -4,8 +4,8 @@ class GatsbyCli < Formula
   desc "Gatsby command-line interface"
   homepage "https://www.gatsbyjs.org/docs/gatsby-cli/"
   # gatsby-cli should only be updated every 10 releases on multiples of 10
-  url "https://registry.npmjs.org/gatsby-cli/-/gatsby-cli-3.9.0.tgz"
-  sha256 "da0bcfecbd3abf8c6b68984c24888e0d4aa0172c6979cffe926701cd5bf73d79"
+  url "https://registry.npmjs.org/gatsby-cli/-/gatsby-cli-3.10.0.tgz"
+  sha256 "91ded5611f62dc948a3903f5d6a541877f8e4fd5f8920989e5742d0dece8fd74"
   license "MIT"
 
   bottle do
