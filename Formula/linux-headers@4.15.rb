3529df9c60a9bc548b2d8746985a4b7e914bca99
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-21 08:25:47 +0000

    linux-headers@4.15: add 4.15.18 bottle.

--- Formula/linux-headers@4.15.rb
+++ Formula/linux-headers@4.15.rb
@@ -4,6 +4,10 @@ class LinuxHeadersAT415 < Formula
   url "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.15.18.tar.gz"
   sha256 "ca13fa5c6e3a6b434556530d92bc1fc86532c2f4f5ae0ed766f6b709322d6495"
   license "GPL-2.0-only"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "720cb4d024febce4f9195b11ac452d707c5a22abe79b1aa3c802e8a56a7cabae"
+  end
   keg_only :versioned_formula
 
   depends_on :linux
