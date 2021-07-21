6d0a8338e0ce4ce8238f1046abfe9b4e1bbfeb0c
Low Peng Hiang <low_peng_hiang@tech.gov.sg>
2021-07-21 17:03:07 +0800

    tfsec 0.50.5
    Closes #81595.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/aquasecurity/tfsec/archive/v0.48.7.tar.gz"
-  sha256 "f9587b3d77a3af92994282cf4196d2274d75a9d2f1bc730b5d7d852c9808bde4"
+  url "https://github.com/aquasecurity/tfsec/archive/v0.50.5.tar.gz"
+  sha256 "e3ed041b5187eccfb356eab8eb7c113638f2e155b5ffe24de2f21bcec15a0feb"
   license "MIT"
   head "https://github.com/aquasecurity/tfsec.git"
 
