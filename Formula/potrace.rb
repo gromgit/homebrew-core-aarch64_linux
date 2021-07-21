662de7bcc863a14113bbc64606e1a5914ddeb01a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-21 02:13:48 +0000

    potrace: update 1.16 bottle.

--- Formula/potrace.rb
+++ Formula/potrace.rb
@@ -12,11 +12,12 @@ class Potrace < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "80d3d0256f9b7add7d3835f6c84f30afec6a4893f2fcd2aa44b07ebe95876c7f"
-    sha256 cellar: :any, big_sur:       "3b5294deed86179a4e496236fb882eb0b7ad3c020741e2a1398861b545062712"
-    sha256 cellar: :any, catalina:      "c3f357a8bd6460384400acd00dab0d8571ad0b1543a81e5b9d5ff49d1ece4fa1"
-    sha256 cellar: :any, mojave:        "3ad69cce4edecea6e5170b766201845b703a98bbac3c5272ef6a045f828643e2"
-    sha256 cellar: :any, high_sierra:   "56d821a4d3579bedf64ebf5357fc04f214cb2efbea7ddb681b202e684e71d97e"
+    sha256 cellar: :any,                 arm64_big_sur: "80d3d0256f9b7add7d3835f6c84f30afec6a4893f2fcd2aa44b07ebe95876c7f"
+    sha256 cellar: :any,                 big_sur:       "3b5294deed86179a4e496236fb882eb0b7ad3c020741e2a1398861b545062712"
+    sha256 cellar: :any,                 catalina:      "c3f357a8bd6460384400acd00dab0d8571ad0b1543a81e5b9d5ff49d1ece4fa1"
+    sha256 cellar: :any,                 mojave:        "3ad69cce4edecea6e5170b766201845b703a98bbac3c5272ef6a045f828643e2"
+    sha256 cellar: :any,                 high_sierra:   "56d821a4d3579bedf64ebf5357fc04f214cb2efbea7ddb681b202e684e71d97e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d47c98611b15465f29e55972018135927d42929e5901153ad99c6579de32863c"
   end
 
   uses_from_macos "zlib"
