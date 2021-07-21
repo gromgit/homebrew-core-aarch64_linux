ad8e694d63878931d500de1b62ffabc936842855
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-21 17:43:10 +0800

    nutcracker: use Homebrew libyaml
    Resolves a CI failure seen in #78294.
    
    Closes #81596.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nutcracker.rb
+++ Formula/nutcracker.rb
@@ -4,6 +4,7 @@ class Nutcracker < Formula
   url "https://github.com/twitter/twemproxy/archive/0.5.0.tar.gz"
   sha256 "73f305d8525abbaaa6a5f203c1fba438f99319711bfcb2bb8b2f06f0d63d1633"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/twitter/twemproxy.git"
 
   bottle do
@@ -17,6 +18,14 @@ class Nutcracker < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "libyaml"
+
+  # Use Homebrew libyaml instead of the vendored one.
+  # Adapted from Debian's equivalent patch.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/0e1ac7ef20e83159554b6522380b1c4b48ce4f2f/nutcracker/use-system-libyaml.patch"
+    sha256 "9105f2bd784f291da5c3f3fb4f6876e62ab7a6f78256f81f5574d593924e424c"
+  end
 
   def install
     system "autoreconf", "-ivf"
