1edbb64cb181ca7e618bca7c9619a8e78197e10b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-22 08:07:55 +0000

    metricbeat: update 7.13.4 bottle.

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -8,11 +8,11 @@ class Metricbeat < Formula
   head "https://github.com/elastic/beats.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "79b1171c112ea35866351c40fc23b1c8fc9ad330f9675aa9651786b90816546c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7629f4f1ae8743739c5c9b347355ae224b4f22ed110eb0fd91eaa338a1a4d1f5"
-    sha256 cellar: :any_skip_relocation, catalina:      "5cebd32fd991e8e170fe580c5a2914a63737798f263f655f680389b3024207e5"
-    sha256 cellar: :any_skip_relocation, mojave:        "ceeed2cbc02f39ca6cc5a806e69747dd1f413c327679adf5914b263be1d6d940"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb456eb086078f8eba872437e122c773b17ed5164aa676c4e3702df16f3e5351"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5a206243da0b7f3297a55ea7ca482729b9c6132c9f5726df82063d675b9cfbb1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "aa9b4c2d57a4eea7794663726017d1c5dcc57bc060133c5c27522ef728ac5c1a"
+    sha256 cellar: :any_skip_relocation, catalina:      "17d5150868a846a1023e447ce43280eaa3cd752107441283a7f8c762a8aa1fc4"
+    sha256 cellar: :any_skip_relocation, mojave:        "621ce0a7deaecb13497a13aa2a879f4307dfa4aedc7626b24a1c8b1456a2ebcc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0e201e4bfc8144a70e66cf947ba573e1aab71e15d56cc321d204370c743d318"
   end
 
   depends_on "go" => :build
