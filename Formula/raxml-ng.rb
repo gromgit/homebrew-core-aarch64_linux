ad5ea4c61a52494781b87aee8d28a5473966a73c
Caleb Xu <calebcenter@live.com>
2021-07-21 22:24:35 -0400

    raxml-ng 1.0.3
    Closes #81697.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/raxml-ng.rb
+++ Formula/raxml-ng.rb
@@ -2,8 +2,8 @@ class RaxmlNg < Formula
   desc "RAxML Next Generation: faster, easier-to-use and more flexible"
   homepage "https://sco.h-its.org/exelixis/web/software/raxml/"
   url "https://github.com/amkozlov/raxml-ng.git",
-      tag:      "1.0.2",
-      revision: "411611611793e53c992717d869ca64370f2e4789"
+      tag:      "1.0.3",
+      revision: "55aeb1c38cfda54cfd9a416b30a87f08b15a94e5"
   license "AGPL-3.0-or-later"
 
   bottle do
