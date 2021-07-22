418e0b93f9c7226b1ec496421474fdc529659bca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-22 03:07:37 +0000

    code-minimap: update 0.6.1 bottle.

--- Formula/code-minimap.rb
+++ Formula/code-minimap.rb
@@ -6,11 +6,11 @@ class CodeMinimap < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f798db57c9bec0e39bc8f7f30f8445e3df53032d1686ec2549e37218873d2f5e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "34a54ec8b3e68ada2fe286dc61f7626de51b1ee29360be034566f34bc468a24e"
-    sha256 cellar: :any_skip_relocation, catalina:      "27b9629e0fb3cdc4e2629ae150ffc13ce8cb6c5448f02f888395d53219b2ec59"
-    sha256 cellar: :any_skip_relocation, mojave:        "490595ba892b4bfd1170af45ca829af08015a371f0eedb7e41d7b9db51c9e8a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bf1ebd18938f21a4fe695f78325e128374c6a5d7d35a47f42cb45e2b451a7f6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eb2b39a8a79976931f805356dd7d53f399edfa08933ecab84829353cfb721b0a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "60c121e75de1556e987debfdea99956e53850096ebd4e4e92d65a7e126040470"
+    sha256 cellar: :any_skip_relocation, catalina:      "58739e29a7c5a211eb68487fea0abac801eef75ebbc01fc6b0836230a6720359"
+    sha256 cellar: :any_skip_relocation, mojave:        "11e218821612a7678eabf80f82911f8f61bcdbba4b5a5a18be7130714a88fde2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d6d712b06fffca95fe634af673502ec5100a562b20d92d0593182059c3b5933"
   end
 
   depends_on "rust" => :build
