56f5c509131fa3188585f2d87fbdbcdb4d18bc61
Sam Ford <1584702+samford@users.noreply.github.com>
2021-07-22 17:47:10 -0400

    tokyo-cabinet: update homepage and stable urls (#81731)

--- Formula/tokyo-cabinet.rb
+++ Formula/tokyo-cabinet.rb
@@ -1,7 +1,7 @@
 class TokyoCabinet < Formula
   desc "Lightweight database library"
-  homepage "https://fallabs.com/tokyocabinet/"
-  url "https://fallabs.com/tokyocabinet/tokyocabinet-1.4.48.tar.gz"
+  homepage "https://dbmx.net/tokyocabinet/"
+  url "https://dbmx.net/tokyocabinet/tokyocabinet-1.4.48.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/t/tokyocabinet/tokyocabinet_1.4.48.orig.tar.gz"
   sha256 "a003f47c39a91e22d76bc4fe68b9b3de0f38851b160bbb1ca07a4f6441de1f90"
   license "LGPL-2.1-or-later"
