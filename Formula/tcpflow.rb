916502d0ab78c4b2d380579641d34cd5c8b8f50a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-22 00:49:53 +0000

    tcpflow: update 1.6.1 bottle.

--- Formula/tcpflow.rb
+++ Formula/tcpflow.rb
@@ -11,10 +11,11 @@ class Tcpflow < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "45666c536a212cbc2b76a6663e051f432e4b82910a440d5fa6cebad4562e70f9"
-    sha256 cellar: :any, big_sur:       "ec65cbfeff09cd48c9accca03cf14a733034b96f0d01d47cbcf43ef9e0e859de"
-    sha256 cellar: :any, catalina:      "78b9e40f778060e2a0a277dfa1ff2d3ee720be679f8ade7b98e274ace2a05e7c"
-    sha256 cellar: :any, mojave:        "752820d85c73654edd4b2eef81a36d6d3be542e8cb6f7f62af7906b0740ba98f"
+    sha256 cellar: :any,                 arm64_big_sur: "45666c536a212cbc2b76a6663e051f432e4b82910a440d5fa6cebad4562e70f9"
+    sha256 cellar: :any,                 big_sur:       "ec65cbfeff09cd48c9accca03cf14a733034b96f0d01d47cbcf43ef9e0e859de"
+    sha256 cellar: :any,                 catalina:      "78b9e40f778060e2a0a277dfa1ff2d3ee720be679f8ade7b98e274ace2a05e7c"
+    sha256 cellar: :any,                 mojave:        "752820d85c73654edd4b2eef81a36d6d3be542e8cb6f7f62af7906b0740ba98f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0a2015d8bb5ac2ddca983dd577325a59288527fc77309fe628cdf7a6e55922e"
   end
 
   head do
