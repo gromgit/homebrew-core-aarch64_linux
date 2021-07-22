d9faad5e93b57e454315213f25d48224d16fd195
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-22 08:07:45 +0000

    libedit: update 20210714-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -12,11 +12,11 @@ class Libedit < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "6a4159ed273ba498e091391dd5e84908be57294a8accc14ea88d17439e283df5"
-    sha256 cellar: :any,                 big_sur:       "f3a3b11ac4be1ece253f272d3a1b596571e430041ccf813280417764b62f5264"
-    sha256 cellar: :any,                 catalina:      "24bbdac27e417b37afbcbbee7c93ffed7e6fdfc27d32c8201e2bcb13fa129fff"
-    sha256 cellar: :any,                 mojave:        "b229a23679ba4228e241bb957d5db3ca6ab139b7997e793961ab60cca8850e6d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb9f29cc995a3093417cbc4f4906111432da622e0d6ce4cc2e0e767fa99a197d"
+    sha256 cellar: :any,                 arm64_big_sur: "13d4e88e23a6c82ca458d3ca287af597b27ac211d258ef825bc0a4ab3111a80b"
+    sha256 cellar: :any,                 big_sur:       "75d0b470e0478010fbaa2708db5eefba895d71d32efe79bc3eee389e26375bb7"
+    sha256 cellar: :any,                 catalina:      "b28c79aa687f372834237a8a13ff274ff91bbb378aaa7bdd055411a0817427a7"
+    sha256 cellar: :any,                 mojave:        "b011a5735eee4db2740abc9cc25703e9dcf89da5f79e5d210a3a1e8609f9205e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08fede13bbc6910dbb2cc33aac6d4846b933713118e19503e09490f2404d3994"
   end
 
   keg_only :provided_by_macos
