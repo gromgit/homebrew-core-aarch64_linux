150e50ff5b332c7ed42833b8aeb3f20092ae957a
EricFromCanada <enk3@outlook.com>
2021-07-22 12:12:26 -0400

    openrct2 0.3.4
    Closes #81719.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openrct2.rb
+++ Formula/openrct2.rb
@@ -2,10 +2,9 @@ class Openrct2 < Formula
   desc "Open source re-implementation of RollerCoaster Tycoon 2"
   homepage "https://openrct2.io/"
   url "https://github.com/OpenRCT2/OpenRCT2.git",
-      tag:      "v0.3.3",
-      revision: "3f65f282d7332c284dcb5daaf4c278b7e9da9b92"
+      tag:      "v0.3.4",
+      revision: "e0daac9442dec8bf1502690f6310af50c7bb5f25"
   license "GPL-3.0-only"
-  revision 1
   head "https://github.com/OpenRCT2/OpenRCT2.git", branch: "develop"
 
   bottle do
@@ -35,8 +34,8 @@ class Openrct2 < Formula
   end
 
   resource "objects" do
-    url "https://github.com/OpenRCT2/objects/archive/v1.0.21.tar.gz"
-    sha256 "31129188916dc9ba2318d851e03393ce55782f121ab7c9d97544abdc7bbc92ab"
+    url "https://github.com/OpenRCT2/objects/archive/v1.2.1.tar.gz"
+    sha256 "07816ab18779ab5988d737e1c21c25f0d95404c82919758dfdc44fdd3edf8ab5"
   end
 
   def install
