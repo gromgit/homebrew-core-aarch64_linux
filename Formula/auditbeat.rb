ee34af06fc9ff1d55bf66e9a2226dfb9410b4af0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-22 08:08:17 +0000

    auditbeat: update 7.13.4 bottle.

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -8,11 +8,11 @@ class Auditbeat < Formula
   head "https://github.com/elastic/beats.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6b0939195c32886f717f464246f9eb6fd5d1ec2153881004a95b51b996485bb9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6a11de67523e4f6abd2d746245621d08e010cc60b1b46318449151892934e8cc"
-    sha256 cellar: :any_skip_relocation, catalina:      "7ab18e392772281ac141437a51ee2ea57feef124b1e293be45ed5800682b9db1"
-    sha256 cellar: :any_skip_relocation, mojave:        "dc93a121037a7bb93b961ee4c7cd00b3eac86e48e88b3fce31a775ad984b9c74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d95e4b228724a43dbe6916519a2c562156baa2354008036d66893e681a09919c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8d7ec19320ff1bd8e256ffd7c62a42e648d8915c5171bb1f894d59e77037540f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "dfca3a3795e58330637afba0733d016d4d4b51a168fddb9192bc91e265fb8f33"
+    sha256 cellar: :any_skip_relocation, catalina:      "455cfc084407dfc9b1404abebc99a1eb76b41ba522e82b3a1b9e2b8b50f188a0"
+    sha256 cellar: :any_skip_relocation, mojave:        "88d80c0391620dd0f4d19e7f75be77212d4b4db9cb2ef4356caa704f78a61e1a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd458bfe9dac99a9e0fd990f38fea420ea719042d9073ee03835c43acf5ba0f9"
   end
 
   depends_on "go" => :build
