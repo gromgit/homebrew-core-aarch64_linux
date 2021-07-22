1c8b1b12ba2be25402a4d767c2b4a20dcffb59d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-22 05:05:56 +0000

    libmwaw: update 0.3.20 bottle.

--- Formula/libmwaw.rb
+++ Formula/libmwaw.rb
@@ -6,11 +6,11 @@ class Libmwaw < Formula
   license any_of: ["LGPL-2.1-or-later", "MPL-2.0"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "4ddbbeb71f424b659ab4f6f7d4c9d2bf94668c38c3f49a60ed9e26e7ce75a0e9"
-    sha256 cellar: :any,                 big_sur:       "c370cf36d334e14e9f464878ec3f871b1957d9553e5873b338e9f5e9c36c9de8"
-    sha256 cellar: :any,                 catalina:      "d012e7151f89ad3a2a05e9fc06eb916e7e3a306c36a983bdf39d6f5e66aaf38a"
-    sha256 cellar: :any,                 mojave:        "7b092ef5379479401b41e4984ba99292977738dc7f6c87512f7f68a9b6c7bfc6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c6a9ede442c9fbe2e285c14911709d4af0dc9e8c5dcb0bd0d9d87b041f8d7b4"
+    sha256 cellar: :any,                 arm64_big_sur: "19ee70bb69fcb13f204c761f6aab0aa65338920a0bb076f4b478e231432cc333"
+    sha256 cellar: :any,                 big_sur:       "d01b3328f59d9bae2b955b955825af7521ed97713735d2143e687367cf3b00e3"
+    sha256 cellar: :any,                 catalina:      "8f25e345c0fd4a4025d3bdcbcedb6325919d2c231de2f7a586440f3e95f18714"
+    sha256 cellar: :any,                 mojave:        "eac21b3e50dbac8d892fdfd629ae111a105c12b7b16d6397f535e202ea30f367"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c665a5811e38db40f415790baf26d066958ce0a99329db9ae837621b338ca434"
   end
 
   depends_on "pkg-config" => :build
