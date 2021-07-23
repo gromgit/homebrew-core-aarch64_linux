a5133b6ddb326f1a3a7664b2a93803032005a34c
Rui Chen <rui@chenrui.dev>
2021-07-23 00:30:41 -0400

    repo 2.16.3
    Closes #81763.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -4,8 +4,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.16.2",
-      revision: "927d29a8afa26a3f1aec841f2150c65e3931ef9b"
+      tag:      "v2.16.3",
+      revision: "8e983bbc0f5f48aa38d0e1c5a37766ce121d28eb"
   license "Apache-2.0"
   version_scheme 1
 
