8479f34383d5ec2d4f2cd76bd35c3ebda4c097f3
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2021-07-23 02:29:42 +0000

    conmon: add 2.0.29 bottle.

--- Formula/conmon.rb
+++ Formula/conmon.rb
@@ -5,6 +5,10 @@ class Conmon < Formula
   sha256 "eb4d5e157671a61b88786e44094775194e30e1d0ad0b9d50035532ece78dbc28"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fefd7bbd590a91c392a4a2b30d058adec7ffcebcfe024647e11292c20009ca27"
+  end
+
   depends_on "go" => :build
   depends_on "pkg-config" => :build
 
