d234f097cd605fcf4f3380e7a81c6641acfa9442
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-24 12:07:46 +0000

    dmagnetic: update 0.32 bottle.

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -11,10 +11,10 @@ class Dmagnetic < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cf25009d9eff9196c36d27402aebae7e7ecf90a5a8029162ec6f33d6457ce9ed"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5e7d9d39d36fbeb673598c7974ee8dbf6fc542ea00e73eeed6253db6c0fc67bf"
-    sha256 cellar: :any_skip_relocation, catalina:      "f83ef8d3c11c5d64095de0e634155e3f19a42ca32b13da4bfeb9956e9ee30c5f"
-    sha256 cellar: :any_skip_relocation, mojave:        "0ad1e3399a09a71dbd26744250bddf5468403e1db84057406e52b35ab4b374e4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e8780abff0b126cca9e22f89a1e84c6cb9187c5638743764329b84df8063098a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bd532ceab76672bdbcb72e04c88eda4095c60f1c14bea92ca44055a175e886da"
+    sha256 cellar: :any_skip_relocation, catalina:      "785b5fdcba51200e389341550837690f9796139e50770831d8d08a36a2acfafb"
+    sha256 cellar: :any_skip_relocation, mojave:        "31873834919bd8ddd75ce8c608f8866894c369c1bcc1fa46e4778bb6531199ba"
   end
 
   def install
