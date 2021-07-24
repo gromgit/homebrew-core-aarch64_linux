4ed10ab50c422b3c8f5e74122e97bf00132ed75d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-24 18:07:40 +0000

    mapcidr: update 0.0.8 bottle.

--- Formula/mapcidr.rb
+++ Formula/mapcidr.rb
@@ -7,11 +7,11 @@ class Mapcidr < Formula
   head "https://github.com/projectdiscovery/mapcidr.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b68929f41f451e75384cf189b7a8d3fe0538229e2895c45a38327a701be4f23f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0b07fd6dcc2c3299d06eac557f7fcea37bdf4a70db8bfc236c7552877b588476"
-    sha256 cellar: :any_skip_relocation, catalina:      "e4e8bfcfd4725b2dd469d091fc0ec0c8ae5f31d678fe194e6726e35974290b79"
-    sha256 cellar: :any_skip_relocation, mojave:        "2fc496dae5bc671e9a9998b505402578ed01996ce5d06fc40b2e432e265db402"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e1aac80d6c319f09bbe62f068dd4c328ec173b411210753988d7745f00f7ca7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ee9e513d9c3207c4ea60e968b24d681e8aaaeb889007007815cfd7dd23b36f37"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f4dbc663d7818217dd387c99c08a8ddc9e596202360d48a69ab2d28edc2f5d46"
+    sha256 cellar: :any_skip_relocation, catalina:      "6108174a8882be51d7630f7147adbc078270fc6b5e304271a0370e9555bf44a4"
+    sha256 cellar: :any_skip_relocation, mojave:        "b8c7fe7d85135eb8a9d366b1eb95ebf708dd0b21af32f8193934c8f70dd87aa4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b19f4cc9371f930349a0f8567a48745c722e2f49b222cfb0101d4a22413b8b05"
   end
 
   depends_on "go" => :build
