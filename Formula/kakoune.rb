b71af260dfb22fc6dbb1d7a95634d15c4c0a4754
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-24 14:06:31 +0000

    kakoune: update 2020.09.01 bottle.

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -23,12 +23,12 @@ class Kakoune < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "6a6af6d41f893e851691984867efeee176098f47c881d5fe77d8534bc4086375"
-    sha256 cellar: :any,                 big_sur:       "61064437727a2eb062b89941b25fb46c017d350e8947e867e72f51f591d030ad"
-    sha256 cellar: :any,                 catalina:      "9cb8ffd67651eab6f269daab2bbf4e66adf4f7dad4029a3285631a3615cf1514"
-    sha256 cellar: :any,                 mojave:        "00c9127f14d643eee79fc64d02874bc131dad426fb11580b5d4fa43a3a51007c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d1f01dd8fea918ffb45c5fa7e2f3d233637768eaa9d2b7a65d1df136f9a03364"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_big_sur: "87457c197389b0e6a8346db08f8b69f544aab44f88cac1c6cf79327422b4528e"
+    sha256 cellar: :any,                 big_sur:       "0f94939ffdfce93cdc4f8ab527a36429ad2cdf9a1f42600baad7ff972835ee6a"
+    sha256 cellar: :any,                 catalina:      "daa8001d4739938d1b34a09c96160fb0e5e9525b57fa2c1949a2e15cc2159323"
+    sha256 cellar: :any,                 mojave:        "f489d4936caeda3e2e6aba22d4db82245cff1291bca5cf93d26a676e7147866f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97f34ea01f31cb9e5bba22b49ae1995b9bcb8c8dd5538c81aacc65b4a2904104"
   end
 
   depends_on macos: :high_sierra # needs C++17
