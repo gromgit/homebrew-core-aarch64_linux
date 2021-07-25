3a8e852cda8ac552f6a4181228f911787b7270c4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-25 14:48:09 +0000

    sslscan: update 2.0.10 bottle.

--- Formula/sslscan.rb
+++ Formula/sslscan.rb
@@ -12,10 +12,11 @@ class Sslscan < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f5b4783554adaf8668bd962997f47b666acc0e5b83c5dcf32744371e0a19e5fd"
-    sha256 cellar: :any, big_sur:       "dd0b57a82a99814e2e21c8d8b076207b7cc1824ce4f2ae6e10ad57eb318c3f89"
-    sha256 cellar: :any, catalina:      "e13086894f20487eee91a42160b7b4d891851e26a23184be8139bca2f0392022"
-    sha256 cellar: :any, mojave:        "30d8baa596df4a6ccade6d02bb77f73747d0c5dae2518bf0082f9aaf2fd8351b"
+    sha256 cellar: :any,                 arm64_big_sur: "f5b4783554adaf8668bd962997f47b666acc0e5b83c5dcf32744371e0a19e5fd"
+    sha256 cellar: :any,                 big_sur:       "dd0b57a82a99814e2e21c8d8b076207b7cc1824ce4f2ae6e10ad57eb318c3f89"
+    sha256 cellar: :any,                 catalina:      "e13086894f20487eee91a42160b7b4d891851e26a23184be8139bca2f0392022"
+    sha256 cellar: :any,                 mojave:        "30d8baa596df4a6ccade6d02bb77f73747d0c5dae2518bf0082f9aaf2fd8351b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4922f8701dfe20d818eee4fcf6ba010b7da4c927ad0389e00181eb993a4d0d71"
   end
 
   depends_on "openssl@1.1"
