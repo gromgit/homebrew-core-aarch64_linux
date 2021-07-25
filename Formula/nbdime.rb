2002aa1b88ca697150e4bfc283c36a973b7b97e5
Michka Popoff <michkapopoff@gmail.com>
2021-07-25 12:44:21 +0200

    nbdime: appnope resource is macOS only

--- Formula/nbdime.rb
+++ Formula/nbdime.rb
@@ -17,9 +17,11 @@ class Nbdime < Formula
 
   depends_on "python@3.9"
 
-  resource "appnope" do
-    url "https://files.pythonhosted.org/packages/26/34/0f3a5efac31f27fabce64645f8c609de9d925fe2915304d1a40f544cff0e/appnope-0.1.0.tar.gz"
-    sha256 "8b995ffe925347a2138d7ac0fe77155e4311a0ea6d6da4f5128fe4b3cbe5ed71"
+  on_macos do
+    resource "appnope" do
+      url "https://files.pythonhosted.org/packages/26/34/0f3a5efac31f27fabce64645f8c609de9d925fe2915304d1a40f544cff0e/appnope-0.1.0.tar.gz"
+      sha256 "8b995ffe925347a2138d7ac0fe77155e4311a0ea6d6da4f5128fe4b3cbe5ed71"
+    end
   end
 
   resource "argon2-cffi" do
