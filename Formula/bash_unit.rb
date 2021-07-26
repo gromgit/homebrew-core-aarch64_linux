b8dd039b857de8e4295b7e694ae220bfc93e6909
Johan Bergström <bugs@bergstroem.nu>
2021-07-26 16:26:34 +0200

    bash_unit 1.7.2 (new formula)
    Closes #81923.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/bash_unit.rb
@@ -0,0 +1,24 @@
+class BashUnit < Formula
+  desc "Bash unit testing enterprise edition framework for professionals"
+  homepage "https://github.com/pgrange/bash_unit"
+  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v1.7.2.tar.gz"
+  sha256 "eabb078da18eb4c4eb1db8211d1197bd651e1d837466218338059cc590e33958"
+  license "GPL-3.0-only"
+
+  uses_from_macos "bc" => :test
+
+  def install
+    bin.install "bash_unit"
+    man1.install "docs/man/man1/bash_unit.1"
+  end
+
+  test do
+    (testpath/"test.sh").write <<~EOS
+      test_addition() {
+        RES="$(echo 2+2 | bc)"
+        assert_equals "${RES}" "4"
+      }
+    EOS
+    assert "addition", shell_output("#{bin}/bash_unit test.sh")
+  end
+end
