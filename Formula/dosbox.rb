0f658fa97a83141a4930b8cadeb4137c94994cd3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-26 04:53:49 +0000

    dosbox: update 0.74-3 bottle.

--- Formula/dosbox.rb
+++ Formula/dosbox.rb
@@ -6,12 +6,13 @@ class Dosbox < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "999bf1d034d6cd7eae80c5439fc07bd5681ccc315edd872872050adcf76dffc7"
-    sha256 cellar: :any, big_sur:       "7adbfaa213d56b44eb98645794f954e298dda776f37d5106e40c563704f1a7ab"
-    sha256 cellar: :any, catalina:      "b204c9a07dce5bf4f476c9912f177481a69e8843045ab19d01f3e016d875dceb"
-    sha256 cellar: :any, mojave:        "de46ee6c3c638829ba3b9dc3ee009811d26a19359d10804b9ff93706df2a6863"
-    sha256 cellar: :any, high_sierra:   "66b1b073b1ae7db629c64f66249254aefcb8fb6585c065c858a364bd258785d4"
-    sha256 cellar: :any, sierra:        "3bd2c41c7f76e214c0964acec02723d2a2a611eca92cf5edb93c029333a78adf"
+    sha256 cellar: :any,                 arm64_big_sur: "999bf1d034d6cd7eae80c5439fc07bd5681ccc315edd872872050adcf76dffc7"
+    sha256 cellar: :any,                 big_sur:       "7adbfaa213d56b44eb98645794f954e298dda776f37d5106e40c563704f1a7ab"
+    sha256 cellar: :any,                 catalina:      "b204c9a07dce5bf4f476c9912f177481a69e8843045ab19d01f3e016d875dceb"
+    sha256 cellar: :any,                 mojave:        "de46ee6c3c638829ba3b9dc3ee009811d26a19359d10804b9ff93706df2a6863"
+    sha256 cellar: :any,                 high_sierra:   "66b1b073b1ae7db629c64f66249254aefcb8fb6585c065c858a364bd258785d4"
+    sha256 cellar: :any,                 sierra:        "3bd2c41c7f76e214c0964acec02723d2a2a611eca92cf5edb93c029333a78adf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19c0a42d20e64b6e6f769d958a02325f0c2a0fabc09c4c56d2a9c711e49cc2a1"
   end
 
   head do
