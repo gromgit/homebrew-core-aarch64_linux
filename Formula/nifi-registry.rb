df5cd36770829df6462c8de405ec6b0c43f47d0f
Sean Molenaar <smillerdev@me.com>
2021-07-26 10:37:23 +0200

    nifi-registry 1.14.0
    Closes #81917.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nifi-registry.rb
+++ Formula/nifi-registry.rb
@@ -1,11 +1,10 @@
 class NifiRegistry < Formula
   desc "Centralized storage & management of NiFi/MiNiFi shared resources"
   homepage "https://nifi.apache.org/registry"
-  url "https://www.apache.org/dyn/closer.lua?path=nifi/nifi-registry/nifi-registry-0.8.0/nifi-registry-0.8.0-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/nifi/nifi-registry/nifi-registry-0.8.0/nifi-registry-087.0-bin.tar.gz"
-  sha256 "db9f390eb4e1f99fb861c437be95729dfcb203f49de4b9cb3130a612a4ae288d"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.14.0/nifi-registry-1.14.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.14.0/nifi-registry-1.14.0-bin.tar.gz"
+  sha256 "4fac3cab66e0e2eba7d9f7bed27f3783942e01e67c79bba404309bbf9b5262a9"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "6ecd426a5b78a3ac0ee0199fb9e4c592af64bad183db6580d28703195a0a956e"
