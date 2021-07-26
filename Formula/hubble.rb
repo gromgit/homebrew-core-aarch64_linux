9c3122762c7ee74081884c5fc9fd77a39bdbb071
Nanda H Krishna <me@nandahkrishna.com>
2021-07-26 13:02:21 +0000

    hubble: add 0.8.1 bottle.

--- Formula/hubble.rb
+++ Formula/hubble.rb
@@ -5,6 +5,14 @@ class Hubble < Formula
   sha256 "ebb54783c57e7997986dcc5a73f4f845015920b857dd348abd7a02e5d3af65ec"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "113047c23ad679c1f20bc2677b8cf4b8358088847c669642d056b2560acfae5c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6388ff464e1778aafb74b83fc86c7c812e971ccf67bf85cb9c54909c6a9242fd"
+    sha256 cellar: :any_skip_relocation, catalina:      "10f774f91b48cd518944070f13668920948e62faf93340a10dc66f12bdda989c"
+    sha256 cellar: :any_skip_relocation, mojave:        "10be5c09d955f9a54ab7f92742093906077df7c13ae54b7c0c72589bf16357a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c14ac1d48f9db3af7711eb16baba0f36de2adb5c748fbcf1684ddacf7232141b"
+  end
+
   depends_on "go" => :build
 
   def install
