8afc2b12f3674ca80e83ab62e4dbf2bbe876abea
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-26 08:09:44 +0000

    archi-steam-farm: update 5.1.0.9_1 bottle.

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -14,9 +14,7 @@ class ArchiSteamFarm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "16bc55a12a0957f01360d26ba97cf3e8970a11ae7563bc0ccda5f26b2a09f3c9"
-    sha256 cellar: :any_skip_relocation, catalina: "8b6857a931593bcb24aee55236fbb7a9ecf5d5331ab732ca9530892206911ac8"
-    sha256 cellar: :any_skip_relocation, mojave:   "3ef2bb7a15b61fd19ceb48b371bebdfc8ae04e1ec95e117c8cf9cee77025919a"
+    sha256 cellar: :any_skip_relocation, all: "10439853b50da7d457721024536f97d522703684da732e256ff73faf4f9a9b51"
   end
 
   depends_on "dotnet"
