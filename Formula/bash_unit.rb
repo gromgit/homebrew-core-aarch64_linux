23912e104f8484a63a9662a1b4dc90585fc6c0df
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-27 00:32:07 +0000

    bash_unit: add 1.7.2 bottle.

--- Formula/bash_unit.rb
+++ Formula/bash_unit.rb
@@ -5,6 +5,10 @@ class BashUnit < Formula
   sha256 "eabb078da18eb4c4eb1db8211d1197bd651e1d837466218338059cc590e33958"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4a81ca0cb8db0163c26fc2de9970baa8f9d5332436a60033ca2db865364b53cd"
+  end
+
   uses_from_macos "bc" => :test
 
   def install
