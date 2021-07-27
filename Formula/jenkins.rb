855fed54e100f2d0c82ab17fe82ba1b0631c9630
Caleb Xu <calebcenter@live.com>
2021-07-27 10:11:51 -0400

    jenkins 2.303
    Closes #81958.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins.io/"
-  url "http://mirrors.jenkins.io/war/2.302/jenkins.war"
-  sha256 "49d0c8961d13fb9505df8c2bb4aa70581dad99aba581f1d39a0051225d7da338"
+  url "http://mirrors.jenkins.io/war/2.303/jenkins.war"
+  sha256 "4dfe49cd7422ec4317a7c7a7c083f40fa475a58a7747bd94187b2cf222006ac0"
   license "MIT"
 
   livecheck do
