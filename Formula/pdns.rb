0d8b843182eb879e7a564454a6955a7e90ae555a
Caleb Xu <calebcenter@live.com>
2021-07-27 10:18:01 -0400

    pdns 4.5.1
    * pdns 4.5.1
    * pdns: use gcc on Linux
      pdns needs a compiler that supports C++17.
    * Update Formula/pdns.rb
    
    Closes #81959.
    
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pdns.rb
+++ Formula/pdns.rb
@@ -1,8 +1,8 @@
 class Pdns < Formula
   desc "Authoritative nameserver"
   homepage "https://www.powerdns.com"
-  url "https://downloads.powerdns.com/releases/pdns-4.4.1.tar.bz2"
-  sha256 "03fa7c181c666a5fc44a49affe7666bd385d46c1fe15088caff175967e85ab6c"
+  url "https://downloads.powerdns.com/releases/pdns-4.5.1.tar.bz2"
+  sha256 "74d63c7aa0474de3c2137bb808164691a1a3a62942d2a9a70b648cd277923f9b"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -34,6 +34,12 @@ class Pdns < Formula
   depends_on "sqlite"
 
   uses_from_macos "curl"
+
+  on_linux do
+    depends_on "gcc" => :build # for C++17
+  end
+
+  fails_with gcc: "5"
 
   def install
     args = %W[
