333f2ce5345bbc11b905d0bd38fc4b959a15a088
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-27 01:30:14 +0000

    automake: update 1.16.4 bottle.

--- Formula/automake.rb
+++ Formula/automake.rb
@@ -7,11 +7,11 @@ class Automake < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91656222dff012c7434026ff250fcd92fd5746e60a383ef27547559b6bbfe4f5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "11f09c63a49b30078f91bd00b8bed2408422100764cb7b039e8f96941aec3dfc"
-    sha256 cellar: :any_skip_relocation, catalina:      "5f83d4723ee9f33c4a90d62c4bce9d200c4c74cc32d207e4f4d2bdaaede9fb7f"
-    sha256 cellar: :any_skip_relocation, mojave:        "52796a1b6c737797964b119a5cf170a24fc55e32a43841e4690cce1cc24fed1a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32cfaaad01d5f24626f9180b4d0292df2a254278825e502308c99d4ae6c4e606"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8e1bc79fe3243bb6853728d2471bddd0997aa8a6e04c79f023e6a358f8891dea"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c1f2f46b26a2ffa42f86165e0421d80728b6f97c6b42b61c9ff1e57199f36f64"
+    sha256 cellar: :any_skip_relocation, catalina:      "c1f2f46b26a2ffa42f86165e0421d80728b6f97c6b42b61c9ff1e57199f36f64"
+    sha256 cellar: :any_skip_relocation, mojave:        "c1f2f46b26a2ffa42f86165e0421d80728b6f97c6b42b61c9ff1e57199f36f64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4df29c31b1e8b00c289aa857bf70dfd6eb189e7959864a8047aaaf4f4d8d699"
   end
 
   depends_on "autoconf"
