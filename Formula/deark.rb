f50aed0acdda79675b9a5133cf0b898fd5a7e2c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-28 19:07:33 +0000

    deark: update 1.5.9 bottle.

--- Formula/deark.rb
+++ Formula/deark.rb
@@ -11,11 +11,11 @@ class Deark < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a907384f204e41ce39db43e23da0047c01a34d17a87e67d9d41ca5859807fb7a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "09ad8b8b146e4df4b0955be83fd168c842e47cb0f5dd94f5ce90df139a6a8831"
-    sha256 cellar: :any_skip_relocation, catalina:      "e4416ed82488c439a1085bb9464abbbd082d6b85e350455c054f31b5978bb728"
-    sha256 cellar: :any_skip_relocation, mojave:        "5667c9616961ca8b8da43979903aaac15a65a27f88bf4242327c2954fa1cb862"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4637a49630e6b17c56580067507ef4fa95f5413d46c10ff2fbf810434389fa1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c9a466bdfadf7be7d424bd644f825e39e946f2db70189a2fefcab2b4d447314f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ce395c466420484460ed1e2387c4a989a99586f52bf0c32ec76c2af28eacebac"
+    sha256 cellar: :any_skip_relocation, catalina:      "254c3f11d539f1115b805e91f6af98201f816dd30104b36f2b250ebd7f8d38ca"
+    sha256 cellar: :any_skip_relocation, mojave:        "d8d7cfd4a923be61019683d657513cbf749da3bee7e804642b3bc5a99ea5776b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4175cbafb12714c7be36937adad34769f787d4c2609fe6a2e61ceae3f1afe9cc"
   end
 
   def install
