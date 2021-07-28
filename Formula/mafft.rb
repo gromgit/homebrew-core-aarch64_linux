5be9ffb111a0a072f1059821d78597385ca6d0c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-28 01:29:49 +0000

    mafft: update 7.487 bottle.

--- Formula/mafft.rb
+++ Formula/mafft.rb
@@ -12,11 +12,11 @@ class Mafft < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91b03104ff5f30757dbedd718a78cd645ad16a1a6688abf325a7b8fc348a8b33"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9dc1f412ecf13f900a82a0599c89b59770135d67f60ab4f1341c2c614fdbdb03"
-    sha256 cellar: :any_skip_relocation, catalina:      "713146b4b684418a6aefe6001f828cf601b5250068e8f26b036a68ea915585bc"
-    sha256 cellar: :any_skip_relocation, mojave:        "bb7e9f17d362f385847dc71fac8d184f8e2f2b46683fcbe41bfb77bfe7cb3a91"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "29c04482a84df4657e79ea63da9177aa2ea522680cccdea996f268446829afee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1f2d130b42194e1fc0fc8e04fc09b50ea8a2e0b4221b6247471036ff09f1ba47"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2128b37a707fdef7a6c45f19b8abcfefca4ebe37077d2a783711a568b5a4b7eb"
+    sha256 cellar: :any_skip_relocation, catalina:      "3dc223c186c006a5646bb6de73badbbd2b327c8bca7e2d2a342ff28dd1f4f92b"
+    sha256 cellar: :any_skip_relocation, mojave:        "0b6a7c8d20f75c44fc690974ebeee2d7ee7ddcb8f1f5e0b56da5c35b42aaaa53"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8fa1cf45a410954e6b3697f2003580d62f3f1885cafbd28d2f80a7cc66fb293"
   end
 
   def install
