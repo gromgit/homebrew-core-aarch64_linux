ef84f253073bd0077f375bee806efedddc134ca9
Denys Dovhan <denysdovhan@gmail.com>
2021-07-28 17:49:03 +0300

    spaceship 3.14.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #82062.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -1,8 +1,8 @@
 class Spaceship < Formula
   desc "Zsh prompt for Astronauts"
   homepage "https://spaceship-prompt.sh"
-  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v3.13.4.tar.gz"
-  sha256 "c5691a555a75d4c117845e22d92149b91a8ffa8864b9988f99ca8fda07dabe80"
+  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v3.14.0.tar.gz"
+  sha256 "89b208b9d4ea7d43f120a129458dfd45d70337b3f829a3fc11abf13dd9ba0640"
   license "MIT"
   head "https://github.com/spaceship-prompt/spaceship-prompt.git"
 
