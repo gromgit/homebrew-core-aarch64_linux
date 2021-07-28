eb745f82970f780e9160000975dc4c492d179007
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-28 01:29:33 +0000

    trunk: update 0.12.0 bottle.

--- Formula/trunk.rb
+++ Formula/trunk.rb
@@ -7,11 +7,11 @@ class Trunk < Formula
   head "https://github.com/thedodd/trunk.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "db029be98fee3199772344f6b0dbac2c618dd9803b554ec80c299b1ba587c391"
-    sha256 cellar: :any_skip_relocation, big_sur:       "56e401e680d3aac8ddc7a3b0f528143ee65649821c0b8ae16ec0f9986943ab30"
-    sha256 cellar: :any_skip_relocation, catalina:      "d8da1a17df62f0fa0430d4046be4014bdd12b2b274d24cdac6b089831e824cc7"
-    sha256 cellar: :any_skip_relocation, mojave:        "d1691cc33b907571250b5b1cccb9f42022f32e7dd30c0de4f7c1f28291b900cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3890254423b3f02008ee6180b0631ac44b1ab19f55e399602f942f26aec7aa90"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "879dff359548f9f1a2221c6597015de932a611c4427fbd7afca7e641bfe23ea1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "22dc948e539e051d408d101730b71217918075c0b21e9d20619df6b5cdf311f1"
+    sha256 cellar: :any_skip_relocation, catalina:      "ecbc3d073742cd8b777a65ef58e025307abae7e4c5e6d88074e4aea4d324d195"
+    sha256 cellar: :any_skip_relocation, mojave:        "36a8c526efa9b9b7221b3a6c8210a2c9adff899c7e25d7aa3a3393d49093474a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e84f59b2b2391f11e8b95d4d0bfa0da9ed4a856b8908933ba0b2176c71e6967"
   end
 
   depends_on "rust" => :build
