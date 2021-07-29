73bd63e757007d7f57274e6073e48e45df020acc
Rui Chen <rui@chenrui.dev>
2021-07-29 01:16:09 -0400

    wcslib 7.7

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -1,8 +1,8 @@
 class Wcslib < Formula
   desc "Library and utilities for the FITS World Coordinate System"
   homepage "https://www.atnf.csiro.au/people/mcalabre/WCS/"
-  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-7.6.tar.bz2"
-  sha256 "54bb8c92167a7f13f8aa73fc4b8a09785256d2c98213467debc7a282a431318b"
+  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-7.7.tar.bz2"
+  sha256 "a708e6b4e90e2cd6427434715b591bb9c3d4005c99725ec494b8e0bed2de5355"
   license "GPL-3.0-or-later"
 
   livecheck do
