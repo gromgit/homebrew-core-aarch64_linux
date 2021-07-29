55f228cc22350347a05cce9c50332dbc9016ecd3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-29 04:44:03 +0000

    libgnt: update 2.14.2 bottle.

--- Formula/libgnt.rb
+++ Formula/libgnt.rb
@@ -16,6 +16,7 @@ class Libgnt < Formula
     sha256 cellar: :any, big_sur:       "4f5d3a4eac1bc6ced4b77e166d17c69ae675aafab501a1c6a160d96fb4d16767"
     sha256 cellar: :any, catalina:      "97281ee7db978e4a605173bd5abf7fa88db1d641f374189f534db8072afa4e82"
     sha256 cellar: :any, mojave:        "066da1b998539c7f36e25b57606d20621ca02be456b708ae9e6b59b99eccb7e5"
+    sha256               x86_64_linux:  "af9f29ebc47897bc9725ff680c35318503166e46faabd67172b48a532bacb904"
   end
 
   depends_on "gtk-doc" => :build
