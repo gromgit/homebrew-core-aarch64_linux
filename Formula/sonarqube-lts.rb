45fc709a914669b1f40bd6fcaf6a282fcbf84a40
Sean Molenaar <smillerdev@me.com>
2021-07-30 15:03:39 +0200

    sonarqube-lts: port linux changes and add service

--- Formula/sonarqube-lts.rb
+++ Formula/sonarqube-lts.rb
@@ -28,34 +28,23 @@ class SonarqubeLts < Formula
 
   def install
     # Delete native bin directories for other systems
-    rm_rf Dir["bin/{linux,windows}-*"]
+    remove = "linux"
+    keep = "macosx-universal"
+    on_linux do
+      remove = "macosx"
+      keep = "linux-x86"
+    end
+
+    rm_rf Dir["bin/{#{remove},windows}-*"]
 
     libexec.install Dir["*"]
 
-    (bin/"sonar").write_env_script libexec/"bin/macosx-universal-64/sonar.sh",
+    (bin/"sonar").write_env_script libexec/"bin/#{keep}-64/sonar.sh",
       Language::Java.overridable_java_home_env("11")
   end
 
-  plist_options manual: "sonar console"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-          <string>#{opt_bin}/sonar</string>
-          <string>start</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"sonar", "start"]
   end
 
   test do
