05e74086b48d6c4fab2dc4e3319cb09513e27125
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-30 09:27:16 +0000

    encfs: update 1.9.5_3 bottle.

--- Formula/encfs.rb
+++ Formula/encfs.rb
@@ -10,10 +10,11 @@ class Encfs < Formula
   head "https://github.com/vgough/encfs.git"
 
   bottle do
-    sha256 catalina:    "c41dd4f6c6eae27645695e7540a6e1ec25cd4a15756e5f5ed97a345cd39372fc"
-    sha256 mojave:      "1cc308274ff04d95ab12bc39be227517dbf264e5cf811d72b153d6f84b06c0cb"
-    sha256 high_sierra: "137944ecee75c5d82634bf1458316c4d64d841ed9f92a4638ad266503f92b66f"
-    sha256 sierra:      "79e5d3548036ae74ed956bea6d9c4ab7f2e12faf7b49b541da9a72476159a557"
+    sha256 catalina:     "c41dd4f6c6eae27645695e7540a6e1ec25cd4a15756e5f5ed97a345cd39372fc"
+    sha256 mojave:       "1cc308274ff04d95ab12bc39be227517dbf264e5cf811d72b153d6f84b06c0cb"
+    sha256 high_sierra:  "137944ecee75c5d82634bf1458316c4d64d841ed9f92a4638ad266503f92b66f"
+    sha256 sierra:       "79e5d3548036ae74ed956bea6d9c4ab7f2e12faf7b49b541da9a72476159a557"
+    sha256 x86_64_linux: "2c8863fccfcdb4e42cca450763879b7238b5a506a9385b25c3a3c3e2bf27c69d"
   end
 
   depends_on "cmake" => :build
