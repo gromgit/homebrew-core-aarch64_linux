5f846c595e614eb2baf076b4c90e59e7316d88f1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-30 22:34:27 +0000

    xsane: update 0.999_4 bottle.

--- Formula/xsane.rb
+++ Formula/xsane.rb
@@ -12,10 +12,11 @@ class Xsane < Formula
   end
 
   bottle do
-    sha256 catalina:    "1b87500430dca49d717fa39d92214da59f08d4f7ec63ea477056bc5b2b920de4"
-    sha256 mojave:      "93064b6ec70657f6815a0aba5d52c8b7e54e9ef6f223c608351b790887c62b92"
-    sha256 high_sierra: "4242d28d56f5ed634f7f8632d41e441e1cbeaf60a362628796a436ba2f8eac11"
-    sha256 sierra:      "f247067d49d44f8c0662cd64c99524f13c4b3a18ef7e49a19dd377bb449c859d"
+    sha256 catalina:     "1b87500430dca49d717fa39d92214da59f08d4f7ec63ea477056bc5b2b920de4"
+    sha256 mojave:       "93064b6ec70657f6815a0aba5d52c8b7e54e9ef6f223c608351b790887c62b92"
+    sha256 high_sierra:  "4242d28d56f5ed634f7f8632d41e441e1cbeaf60a362628796a436ba2f8eac11"
+    sha256 sierra:       "f247067d49d44f8c0662cd64c99524f13c4b3a18ef7e49a19dd377bb449c859d"
+    sha256 x86_64_linux: "133b3933b827ee30467074ccda832be05c14563290f83cb3e829ac25c088a5dc"
   end
 
   depends_on "pkg-config" => :build
