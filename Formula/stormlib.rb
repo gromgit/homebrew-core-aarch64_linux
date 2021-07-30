8e02054b6413eddb7e983ff2aca4d0ed642a8a62
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-30 23:31:31 +0000

    stormlib: update 9.23 bottle.

--- Formula/stormlib.rb
+++ Formula/stormlib.rb
@@ -7,10 +7,11 @@ class Stormlib < Formula
   head "https://github.com/ladislav-zezula/StormLib.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "16d13a201008b0f6c145e80d28ced76f29af97dfcfce05d1bc2dac84ac0dba33"
-    sha256 cellar: :any, big_sur:       "12177d76e3bac8c67baba52c812a855642e780624d7a75f1e826b10811de35b4"
-    sha256 cellar: :any, catalina:      "686a27d3793a4a80858f442d1feda9d5880e21e687c152067136b4bb27c6fa50"
-    sha256 cellar: :any, mojave:        "0270b8a31bf89afd8a81a0b8e36f3a967e196f024a3900fdf24ef5ab1b26a422"
+    sha256 cellar: :any,                 arm64_big_sur: "16d13a201008b0f6c145e80d28ced76f29af97dfcfce05d1bc2dac84ac0dba33"
+    sha256 cellar: :any,                 big_sur:       "12177d76e3bac8c67baba52c812a855642e780624d7a75f1e826b10811de35b4"
+    sha256 cellar: :any,                 catalina:      "686a27d3793a4a80858f442d1feda9d5880e21e687c152067136b4bb27c6fa50"
+    sha256 cellar: :any,                 mojave:        "0270b8a31bf89afd8a81a0b8e36f3a967e196f024a3900fdf24ef5ab1b26a422"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14d33ffa96557afdff6f9b3476005b83c6dbcd51994d0b7fed03d2af0484d263"
   end
 
   depends_on "cmake" => :build
