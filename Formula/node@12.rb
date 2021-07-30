12f53cc7d4a383e0e3313dad7d9a34e39352fe8b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-30 08:07:25 +0000

    node@12: update 12.22.4 bottle.

--- Formula/node@12.rb
+++ Formula/node@12.rb
@@ -11,11 +11,11 @@ class NodeAT12 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "c5e43bbfb4ed9b5866bd0f5aefb5cb40f06504138ec57bd92a595eec01ac823b"
-    sha256 cellar: :any,                 big_sur:       "09dce109119522b874a50633fce1b9e03c3a4f6d8e8fc17b56bc6aaabace8214"
-    sha256 cellar: :any,                 catalina:      "2a7c221af40ac971028770eaf5b005b0a1135b9c206da77aa57b1805b2632d30"
-    sha256 cellar: :any,                 mojave:        "1eefa8281d0ed8b35492b1fb57a6f579341de05325766c6f0587f282455473df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e83be53c90a0e2b72d6f97509fe19c6dc008c1f39ce54f3bf2be8806e2f05d9d"
+    sha256 cellar: :any,                 arm64_big_sur: "8bc4096ea4cb6b71f611df989092fee053a91f50c67f11871420d2e70194da7c"
+    sha256 cellar: :any,                 big_sur:       "57553159a9532200cc460a9a0fe4446f85befbaae7762e5ff905953ca89942b4"
+    sha256 cellar: :any,                 catalina:      "ffad227d4604309efe90e3d1fe998721957fe610c8b9722a5d49feca31484c52"
+    sha256 cellar: :any,                 mojave:        "dfc114139b1ef6d3560f34ac373470d75f1a47be7369c60778be9e75da37e3f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "537de64f5452e84886f33191cb809404c51e3db3e335b07238a0efd768322e7e"
   end
 
   keg_only :versioned_formula
