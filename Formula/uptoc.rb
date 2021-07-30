315b4a7784ca85114b31b55ae893ec31bf796a74
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-30 22:54:33 +0000

    uptoc: update 1.4.3 bottle.

--- Formula/uptoc.rb
+++ Formula/uptoc.rb
@@ -12,6 +12,7 @@ class Uptoc < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "40353c235e30210ab737684f85d6c444b580192eccbf50d84fbae0fa8a64c27b"
     sha256 cellar: :any_skip_relocation, catalina:      "7a2f3982a14ad4176b17832e87dfb2a41fd10d87e0e35ade341b8baa17d3b7ab"
     sha256 cellar: :any_skip_relocation, mojave:        "bd9cc58bd67f536070d5c34cd10733ba9744cfffc8aa3ef74b2ab6d558c32c2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac376a91915fef295b2fc42dcc20591f5fc63246308ffe11d920a9ecf6ae59dc"
   end
 
   depends_on "go" => :build
