751bb1159b733c714380d201f2e68cfdd1da60e1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-07-30 13:28:09 +0000

    libsvm: update 3.25 bottle.

--- Formula/libsvm.rb
+++ Formula/libsvm.rb
@@ -13,10 +13,11 @@ class Libsvm < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ae3fff4762882360a23280215cafdc93d3da1d598816d34cfb0e7b6a922180e3"
-    sha256 cellar: :any, big_sur:       "27481a34d2af64572b48b268c8e47d7700aab4ec9dc2978216c471d512a4cb81"
-    sha256 cellar: :any, catalina:      "ad8d34d17fdca6b25374eff11b5f2b1067d19894358b23c04b3b8ac0f82180b1"
-    sha256 cellar: :any, mojave:        "485eec232bd3cc0619494de50cac4ef4a5b65761d5d6a062c62e07d9a0007e31"
+    sha256 cellar: :any,                 arm64_big_sur: "ae3fff4762882360a23280215cafdc93d3da1d598816d34cfb0e7b6a922180e3"
+    sha256 cellar: :any,                 big_sur:       "27481a34d2af64572b48b268c8e47d7700aab4ec9dc2978216c471d512a4cb81"
+    sha256 cellar: :any,                 catalina:      "ad8d34d17fdca6b25374eff11b5f2b1067d19894358b23c04b3b8ac0f82180b1"
+    sha256 cellar: :any,                 mojave:        "485eec232bd3cc0619494de50cac4ef4a5b65761d5d6a062c62e07d9a0007e31"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1383dac0311d28b61447e56c92affea59d3404c1a83abc5674276056ebe752c"
   end
 
   def install
