28918d5d409acc2a669205d1877477d70374e7e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-30 19:07:12 +0000

    inja: update 3.3.0 bottle.

--- Formula/inja.rb
+++ Formula/inja.rb
@@ -7,11 +7,7 @@ class Inja < Formula
   head "https://github.com/pantor/inja.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3f5ebe5bbf698c4830d4c4aa2508053c085c2f531aa4255f3d1aa1040d5b01e3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fe3c6b65fe9f72935ac560e8c670b1dbe751fec0a02e9108a9898d6315ecd991"
-    sha256 cellar: :any_skip_relocation, catalina:      "99ce29b9041c1723c165118582179d0541f788549d68fa269ec71ab09fdebdb8"
-    sha256 cellar: :any_skip_relocation, mojave:        "0a912572fad4b9ac680aa0df36b4a0a24dd051af1534695f93922975f7e9fbc8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df6c70b4612cddcd313e63d7aeb42905f6adbe31aa30ed4314b113d9c0ca8a72"
+    sha256 cellar: :any_skip_relocation, all: "173097b1702c4d03d7f6010dba26a9163cde204d021762cc8108cb171a6e27b2"
   end
 
   depends_on "cmake" => :build
