51ca55b35b18353990473541eda0ea36db35091b
topazape <topazape@gmail.com>
2021-07-30 22:53:36 +0900

    pspg 5.3.1
    Closes #82181.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/5.3.0.tar.gz"
-  sha256 "8ed30fea8e06f65d29ce4362d1b57e43c75363d46fe23de442d91eaf7a27602d"
+  url "https://github.com/okbob/pspg/archive/5.3.1.tar.gz"
+  sha256 "496e1f568e346ebd62d9a62a32691cd975d36311e3bb15dc347d14e7b2cd6c67"
   license "BSD-2-Clause"
   head "https://github.com/okbob/pspg.git"
 
