b494364155693b15dc6b81382239bedb5afa0ba5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-30 16:10:20 +0000

    eksctl: update 0.59.0 bottle.

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -8,11 +8,11 @@ class Eksctl < Formula
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7d909f637ead2bcd9b3d6c3896beb5e6504f1154fa3ef00abfe64d1d30d4c0fc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "29c094ef01bb2d690a3b22f67043e16066d0ea561608e40c6160977da0998ae3"
-    sha256 cellar: :any_skip_relocation, catalina:      "9c3d12a56c25711516d77f79c46e136da21286a00750ed8ff871c2e0f75bbceb"
-    sha256 cellar: :any_skip_relocation, mojave:        "ec393d7a6ae44b221f36f6cf948633550a1437c1dc61133a481fe0065c13e3c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d747e60d8a4048d263cf8cd13458734388af94128c4eaf00e4b4dfecd487ca57"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2ab6a8eb1892e075f41b16e90f3f843d3d2b201f53f8124db1dbf0ea73608a27"
+    sha256 cellar: :any_skip_relocation, big_sur:       "21beb0c4e77bfd120a33cddace30e9a24b3bea8a61d92a471e63dbb85025d25a"
+    sha256 cellar: :any_skip_relocation, catalina:      "cc8f148883f8a6e10a43f0c67a380870a08a16d9155fc6a97b650d1fd65c5d43"
+    sha256 cellar: :any_skip_relocation, mojave:        "b84d56c05336c6574770b2dff0de930b96032e4a5559a8ed4cabf81714c63908"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97fd10800f1e4a5f7f1bf065b8b917e96225361e371f13c22cffa62614e85d8a"
   end
 
   depends_on "counterfeiter" => :build
