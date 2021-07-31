0bf66e52d0c1b4379b47e92512825ddadde57371
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-31 05:07:33 +0000

    terraform-inventory: update 0.10 bottle.

--- Formula/terraform-inventory.rb
+++ Formula/terraform-inventory.rb
@@ -7,13 +7,11 @@ class TerraformInventory < Formula
   head "https://github.com/adammck/terraform-inventory.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1acd7cb77565f8b7838a0421529c24e48d2f703e0ec8d180f3b3f0a8bdb308f8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d17cfd5dce0dfbc136f1508da7c24894edf479492698be1e7fecd0787d6aefed"
-    sha256 cellar: :any_skip_relocation, catalina:      "a645460f72cd2fb823d603325439b39a7b8c493a2c3b833d87a484bbc0dfe7ba"
-    sha256 cellar: :any_skip_relocation, mojave:        "9f34bba5205c0fc87ddf7c95ce8532b85fc7cbb515dea9cc211f70fab2aeb643"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "a9500dab587c5078fe62ae2ab5eff2376ecad8d29208a60fe195debfdeea5e78"
-    sha256 cellar: :any_skip_relocation, sierra:        "6b30bf29fe2e83c3bb75c16ce83731c7b212f5f48c3db787501cf1fbb8c37d19"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "245a07df6c433118da995d18a1133e96566f8a9cc52546a0a405fae0372c91a3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "df26181ba3116beae2b5582eb6725c2c8d9ddad018be827f6d07d252cfc019c0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
+    sha256 cellar: :any_skip_relocation, catalina:      "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
+    sha256 cellar: :any_skip_relocation, mojave:        "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "853cf36916d4bb9834f0f6b491e347a109a2930d350a1a31b8e7b8233720c20b"
   end
 
   depends_on "go" => :build
