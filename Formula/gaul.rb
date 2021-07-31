0092cf2b0421135365fd6229bdabe34041d3bd78
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-31 07:40:47 +0000

    gaul: update 0.1850-0 bottle.

--- Formula/gaul.rb
+++ Formula/gaul.rb
@@ -6,13 +6,14 @@ class Gaul < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "e6a64a500ac22aec1a76616d86ea2f70449dfa30d37543faf9a135c2f98e1a07"
-    sha256 cellar: :any, catalina:    "f2f98c2f7d23ae7c1862702c6d17d4449bbcc2164940d9157ea12b97deadb273"
-    sha256 cellar: :any, mojave:      "0f60116cbca6bb8986ffbd291d34a22c6426ad4c22bcedca2873aa24ab237eeb"
-    sha256 cellar: :any, high_sierra: "f1b6b4fedb8820b14b6384d612b16a1acca71efa26a0d81881c1730720518765"
-    sha256 cellar: :any, sierra:      "5dcd424881f8395070bf534b8bd480279a17cbf8a5784ba2be7dffdbfbc85f51"
-    sha256 cellar: :any, el_capitan:  "0a6fb9c8ae17bb0785cc9c9da0fa0b3bf5fd6ca69b1ef8516b800d0d28d77360"
-    sha256 cellar: :any, yosemite:    "8b0cb8b79f456faf4b7a8f9af2c788290b3e2eb1785f120875f2b72b4159fbf5"
+    sha256 cellar: :any,                 big_sur:      "e6a64a500ac22aec1a76616d86ea2f70449dfa30d37543faf9a135c2f98e1a07"
+    sha256 cellar: :any,                 catalina:     "f2f98c2f7d23ae7c1862702c6d17d4449bbcc2164940d9157ea12b97deadb273"
+    sha256 cellar: :any,                 mojave:       "0f60116cbca6bb8986ffbd291d34a22c6426ad4c22bcedca2873aa24ab237eeb"
+    sha256 cellar: :any,                 high_sierra:  "f1b6b4fedb8820b14b6384d612b16a1acca71efa26a0d81881c1730720518765"
+    sha256 cellar: :any,                 sierra:       "5dcd424881f8395070bf534b8bd480279a17cbf8a5784ba2be7dffdbfbc85f51"
+    sha256 cellar: :any,                 el_capitan:   "0a6fb9c8ae17bb0785cc9c9da0fa0b3bf5fd6ca69b1ef8516b800d0d28d77360"
+    sha256 cellar: :any,                 yosemite:     "8b0cb8b79f456faf4b7a8f9af2c788290b3e2eb1785f120875f2b72b4159fbf5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "86d80af0c4bdef2186dccac01b0046ca2ca2c81c484b7c5f279553b2e190b53c"
   end
 
   def install
