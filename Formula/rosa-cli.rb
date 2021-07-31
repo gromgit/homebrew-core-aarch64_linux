644a5384970bc3aaa3b63f1ca0ea90cc91b241e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-31 04:07:40 +0000

    rosa-cli: update 1.1.0 bottle.

--- Formula/rosa-cli.rb
+++ Formula/rosa-cli.rb
@@ -13,11 +13,11 @@ class RosaCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "637bc63a9a87158cb43069330b9df3b175b60bb0c9afbc1a7ad1deb24e29fd6f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d91d264307b1ab814bd4deb5ed34a13999420d859288d3a9e88fdfc6362a7f8a"
-    sha256 cellar: :any_skip_relocation, catalina:      "02978ca867911741f210a58d6a518eaa443ae0b4c7776165dfd31b062b254d3e"
-    sha256 cellar: :any_skip_relocation, mojave:        "1c77eb6e2ce4d27fbe09dafbf6e2d5cbedb79cd8420b1cf40c300202ed055825"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4cf41d1dbfc39507f78ae6c1f0a908f56ba97456d205490058ce745f0c2be67d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5dcec3f86d9979a562273df74cbe65e545ec1b70dfe83a51f4bf54268cca0139"
+    sha256 cellar: :any_skip_relocation, big_sur:       "182b8a52f2aa47cf39aab4b6382e95392d7d66c1f8d88ac6cebc50c60f91a9ae"
+    sha256 cellar: :any_skip_relocation, catalina:      "c2d0edcb46b67d64662b058b7c9638c003e56988192772b02d7f8c9c7f2625b7"
+    sha256 cellar: :any_skip_relocation, mojave:        "6a67b7ecb48c0b0e806d2d1249e69143448ca97f7f426779508f386df63d364b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6260306bb676cdcd95d46c567376a75e6c344ab31b1fd58154f263568e1b0e5"
   end
 
   depends_on "go" => :build
