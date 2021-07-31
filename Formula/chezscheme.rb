4d56f3a60e68c116db198ff351f350906735b9e3
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-31 03:42:05 +0000

    chezscheme: update 9.5.4_1 bottle.

--- Formula/chezscheme.rb
+++ Formula/chezscheme.rb
@@ -7,10 +7,11 @@ class Chezscheme < Formula
   revision 1
 
   bottle do
-    sha256 big_sur:     "8eae7ff25a1c3b5a7d4192048f53f51904b997eda6c4c7a558e19a0cd461babe"
-    sha256 catalina:    "54d8553a07253c0215f8654f1c54e9ac03734160544ac9823896601e86dd2c81"
-    sha256 mojave:      "a1f551093249b315e309f75e4174995b2d7f475376dc97c78f43e5302ec4b3a6"
-    sha256 high_sierra: "cb114681db205f5b31da0c3b7906dba46633275b369f6bc3adef4e0411880982"
+    sha256                               big_sur:      "8eae7ff25a1c3b5a7d4192048f53f51904b997eda6c4c7a558e19a0cd461babe"
+    sha256                               catalina:     "54d8553a07253c0215f8654f1c54e9ac03734160544ac9823896601e86dd2c81"
+    sha256                               mojave:       "a1f551093249b315e309f75e4174995b2d7f475376dc97c78f43e5302ec4b3a6"
+    sha256                               high_sierra:  "cb114681db205f5b31da0c3b7906dba46633275b369f6bc3adef4e0411880982"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bf56d05405a7f9259111292ef50ee73229be14f28fca347d725da5fcfba5af17"
   end
 
   depends_on "libx11" => :build
