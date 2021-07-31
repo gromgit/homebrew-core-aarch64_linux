f1cc723f731fb3cd1cb1bf965c8635971e00e18d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-31 08:26:07 +0000

    gtkextra: update 3.3.4_3 bottle.

--- Formula/gtkextra.rb
+++ Formula/gtkextra.rb
@@ -7,10 +7,11 @@ class Gtkextra < Formula
   revision 3
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "841d46dfdaee00be8a853e8069db2b8ca1fbbfcaf298360411b6f9c0a0706da6"
-    sha256 cellar: :any, big_sur:       "3c35df2372587b0cc5bde265a9ff06774ec70651ac5aa103639dc41e669ae3b7"
-    sha256 cellar: :any, catalina:      "17ba389425eea1e26e308f07b94a3f8637645e83a7b8314681f2285e09996d9b"
-    sha256 cellar: :any, mojave:        "d154740567dfe6c084d3ba87d2afb32e9be63b370f85828e01cd5a3ec164d18f"
+    sha256 cellar: :any,                 arm64_big_sur: "841d46dfdaee00be8a853e8069db2b8ca1fbbfcaf298360411b6f9c0a0706da6"
+    sha256 cellar: :any,                 big_sur:       "3c35df2372587b0cc5bde265a9ff06774ec70651ac5aa103639dc41e669ae3b7"
+    sha256 cellar: :any,                 catalina:      "17ba389425eea1e26e308f07b94a3f8637645e83a7b8314681f2285e09996d9b"
+    sha256 cellar: :any,                 mojave:        "d154740567dfe6c084d3ba87d2afb32e9be63b370f85828e01cd5a3ec164d18f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63ff3052d9c03812e4c97cf79786a4395b9cf431588fc7f771331d049bbe6127"
   end
 
   depends_on "pkg-config" => :build
