b60dc72610c57395a86cad0999bf6a396c093842
homebrew-updater <bepsvpt/homebrew-updater>
2021-08-01 00:00:10 +0800

    phpunit 9.5.8
    Closes #82319.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.5.7.phar"
-  sha256 "930b5d60a922604946252637c6d90b71416f64d071d1966f73d14c3569e623ae"
+  url "https://phar.phpunit.de/phpunit-9.5.8.phar"
+  sha256 "11f27cf3f9522241fe234e9bf5813667207a074ac92089aac26d502ffc5e9517"
   license "BSD-3-Clause"
 
   livecheck do
