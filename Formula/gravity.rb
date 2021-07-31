9e78d6d0b7a46f67dc88d0eca884708064bac5f1
Rui Chen <rui@chenrui.dev>
2021-07-31 13:28:20 -0400

    gravity 0.8.5
    Closes #82331.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gravity.rb
+++ Formula/gravity.rb
@@ -1,8 +1,8 @@
 class Gravity < Formula
   desc "Embeddable programming language"
   homepage "https://marcobambini.github.io/gravity/"
-  url "https://github.com/marcobambini/gravity/archive/0.8.3.tar.gz"
-  sha256 "b35f3f8a82f508be3bb3b746c5b1b9bb924171ea110480a209bce4ca4a9d5539"
+  url "https://github.com/marcobambini/gravity/archive/0.8.5.tar.gz"
+  sha256 "5ef70c940cd1f3fec5ca908fb10af60731750d62ba39bee08cb4711b72917e1d"
   license "MIT"
 
   bottle do
