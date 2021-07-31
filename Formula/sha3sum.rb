89c694533056a6442d8b4eb89ce277d1afc4e564
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-07-31 14:14:55 +0000

    sha3sum: update 1.2.1 bottle.

--- Formula/sha3sum.rb
+++ Formula/sha3sum.rb
@@ -6,10 +6,11 @@ class Sha3sum < Formula
   license "ISC"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "90742599d666acfad990ce1247b16f12e4c7c120f2a18aacd09161f8630ef40a"
-    sha256 cellar: :any, big_sur:       "6bc4ecb769628672f3d40e61986f7a581a1fc42c67fc3a27ae684cd5a9ed2c78"
-    sha256 cellar: :any, catalina:      "86132112430a1b8e5b9c22ab18e0c7e9ef037bbfdb1e17739a61834bfbdc55c0"
-    sha256 cellar: :any, mojave:        "f163e7452142623f7e819066f8f137d7e9b311026514bf2d8a44c7b45f4fab07"
+    sha256 cellar: :any,                 arm64_big_sur: "90742599d666acfad990ce1247b16f12e4c7c120f2a18aacd09161f8630ef40a"
+    sha256 cellar: :any,                 big_sur:       "6bc4ecb769628672f3d40e61986f7a581a1fc42c67fc3a27ae684cd5a9ed2c78"
+    sha256 cellar: :any,                 catalina:      "86132112430a1b8e5b9c22ab18e0c7e9ef037bbfdb1e17739a61834bfbdc55c0"
+    sha256 cellar: :any,                 mojave:        "f163e7452142623f7e819066f8f137d7e9b311026514bf2d8a44c7b45f4fab07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32512065a15428036cf70071b620ecc3828384b00e83649cd6c0e2505877b2e9"
   end
 
   depends_on "libkeccak"
