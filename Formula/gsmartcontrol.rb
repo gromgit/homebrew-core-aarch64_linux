73da2a1911f7c1fb1e9ec002bb6fc77a6957eea4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-31 16:28:08 +0000

    gsmartcontrol: update 1.1.3_9 bottle.

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -10,6 +10,7 @@ class Gsmartcontrol < Formula
     sha256 big_sur:       "64de2b67064449818ce0cf15d7ba8330e9fe408359bc0b4a384f6cd0c5fb217d"
     sha256 catalina:      "8733d9d0433c4d316395ca033f9be5f20fe0506a7df92ac634077f0f204ad8cb"
     sha256 mojave:        "f68c2a19c127cd7fbcdffc8ef5202bcd80f8892e182efee2ba2e25ee04b173e6"
+    sha256 x86_64_linux:  "43063bb647f4783d5ae54c6ebbaeba2864822b7ad40c77ec3c91067220c45bfb"
   end
 
   depends_on "pkg-config" => :build
