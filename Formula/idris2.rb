494a595b7f06f8add235b88ac3438bf09d32acd4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-31 08:42:45 +0000

    idris2: update 0.4.0_1 bottle.

--- Formula/idris2.rb
+++ Formula/idris2.rb
@@ -8,9 +8,10 @@ class Idris2 < Formula
   head "https://github.com/idris-lang/Idris2.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "3beffca58897ca836c87544b2a4a1096e70cb0e74e50a1a8b43f6aa4c3a41b75"
-    sha256 cellar: :any, catalina: "ad0955e64d0e51cb847765addaf6d850167a9a0edb25323a6446797a55ba4a4a"
-    sha256 cellar: :any, mojave:   "13c5484d58c87098bb63ee6f861eae120d55b41b43764fae0a07c383658aaf96"
+    sha256 cellar: :any,                 big_sur:      "3beffca58897ca836c87544b2a4a1096e70cb0e74e50a1a8b43f6aa4c3a41b75"
+    sha256 cellar: :any,                 catalina:     "ad0955e64d0e51cb847765addaf6d850167a9a0edb25323a6446797a55ba4a4a"
+    sha256 cellar: :any,                 mojave:       "13c5484d58c87098bb63ee6f861eae120d55b41b43764fae0a07c383658aaf96"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1eed0bfe631249c078c0ecee22e086d71a07d2b6c12b359e17ae47556a3e0386"
   end
 
   depends_on "coreutils" => :build
