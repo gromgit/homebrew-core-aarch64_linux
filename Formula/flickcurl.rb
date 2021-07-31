50ba6899daf8c3c7426bed86f6abd3d6a2b7ca77
Michael Cho <cho-m@tuta.io>
2021-07-31 00:25:43 -0700

    flickcurl: update urls to https
    Closes #82304.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flickcurl.rb
+++ Formula/flickcurl.rb
@@ -1,7 +1,7 @@
 class Flickcurl < Formula
   desc "Library for the Flickr API"
-  homepage "http://librdf.org/flickcurl/"
-  url "http://download.dajobe.org/flickcurl/flickcurl-1.26.tar.gz"
+  homepage "https://librdf.org/flickcurl/"
+  url "https://download.dajobe.org/flickcurl/flickcurl-1.26.tar.gz"
   sha256 "ff42a36c7c1c7d368246f6bc9b7d792ed298348e5f0f5d432e49f6803562f5a3"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
