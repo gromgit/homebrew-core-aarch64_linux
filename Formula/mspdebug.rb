0cc4c71599212e07f2e3ea0befbfa167da825f1b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-07-31 20:43:43 +0000

    mspdebug: update 0.25 bottle.

--- Formula/mspdebug.rb
+++ Formula/mspdebug.rb
@@ -7,14 +7,15 @@ class Mspdebug < Formula
   head "https://github.com/dlbeer/mspdebug.git"
 
   bottle do
-    sha256 arm64_big_sur: "2c4c83e755286f97432ced3adb0e81a15e2241715e82135a6cc758999d621cfd"
-    sha256 big_sur:       "bd15ded651cbe43d819ae5f6a52d90cfafd51306d42a2f2b6c98d1b70ed4873f"
-    sha256 catalina:      "4e512b296b8a655fbe8632afca020866f6499c461fb715aef5c4eb6bdda88034"
-    sha256 mojave:        "4d5d8c35966a0000b010bbaea7c2c403ff4921d1306d34d752ccceb3f3d3b155"
-    sha256 high_sierra:   "4124d4fbd9e191d941153962bb74aed50cc200c473b5ad5850610a1bc85f87b4"
-    sha256 sierra:        "e16447e04c99d74b8cdc49a063c230c64d09e34402d0221542594f3aacac5940"
-    sha256 el_capitan:    "22fc92bc5a594451eb0d0b943bce812619302c795fdad0ca4305c059ccf10a88"
-    sha256 yosemite:      "8b23c23287fc9ab143921257a1859f8ac0dbb9e093261dfe931ec7d6a3548d97"
+    sha256                               arm64_big_sur: "2c4c83e755286f97432ced3adb0e81a15e2241715e82135a6cc758999d621cfd"
+    sha256                               big_sur:       "bd15ded651cbe43d819ae5f6a52d90cfafd51306d42a2f2b6c98d1b70ed4873f"
+    sha256                               catalina:      "4e512b296b8a655fbe8632afca020866f6499c461fb715aef5c4eb6bdda88034"
+    sha256                               mojave:        "4d5d8c35966a0000b010bbaea7c2c403ff4921d1306d34d752ccceb3f3d3b155"
+    sha256                               high_sierra:   "4124d4fbd9e191d941153962bb74aed50cc200c473b5ad5850610a1bc85f87b4"
+    sha256                               sierra:        "e16447e04c99d74b8cdc49a063c230c64d09e34402d0221542594f3aacac5940"
+    sha256                               el_capitan:    "22fc92bc5a594451eb0d0b943bce812619302c795fdad0ca4305c059ccf10a88"
+    sha256                               yosemite:      "8b23c23287fc9ab143921257a1859f8ac0dbb9e093261dfe931ec7d6a3548d97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "591e9d4d6bf5cedceda3db088a416b6c597acdcd65d40856c8f5fd69c49b8c7c"
   end
 
   depends_on "hidapi"
