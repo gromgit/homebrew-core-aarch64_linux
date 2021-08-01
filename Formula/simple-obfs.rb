40ce37e225a96a5b42144ca32974e2ddd86be40d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-01 22:29:22 +0000

    simple-obfs: update 0.0.5_1 bottle.

--- Formula/simple-obfs.rb
+++ Formula/simple-obfs.rb
@@ -8,13 +8,14 @@ class SimpleObfs < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a79c2a9ba2a6473853a0907355290287b5f9faf4dcd808e1819608f0498b236f"
-    sha256 cellar: :any, big_sur:       "6fc8b9d2aef7c813449e5298e1fcbcb7e5f1ba6da4c34b9e4b3c3a5e0005110e"
-    sha256 cellar: :any, catalina:      "64ac7bb71b3dd0a0d087d7f981c53516abfb294f709d84cb969b192456310c51"
-    sha256 cellar: :any, mojave:        "7d00695065a2e780f6a93d98d3d2a96ebe4c02fe48e52e30cea4fefe353100e8"
-    sha256 cellar: :any, high_sierra:   "08024887dc9fba3f56425181dd34dba1ecf185dad688b85d20a7b70ec07afbae"
-    sha256 cellar: :any, sierra:        "831de4a180d61c801397ead63a0130d8d2eb102afb526ef81bcecb2f9d1d029b"
-    sha256 cellar: :any, el_capitan:    "eccfcd8d4016297999d730fd185624b42e903f7dfac43bd6227c337c2b3aafea"
+    sha256 cellar: :any,                 arm64_big_sur: "a79c2a9ba2a6473853a0907355290287b5f9faf4dcd808e1819608f0498b236f"
+    sha256 cellar: :any,                 big_sur:       "6fc8b9d2aef7c813449e5298e1fcbcb7e5f1ba6da4c34b9e4b3c3a5e0005110e"
+    sha256 cellar: :any,                 catalina:      "64ac7bb71b3dd0a0d087d7f981c53516abfb294f709d84cb969b192456310c51"
+    sha256 cellar: :any,                 mojave:        "7d00695065a2e780f6a93d98d3d2a96ebe4c02fe48e52e30cea4fefe353100e8"
+    sha256 cellar: :any,                 high_sierra:   "08024887dc9fba3f56425181dd34dba1ecf185dad688b85d20a7b70ec07afbae"
+    sha256 cellar: :any,                 sierra:        "831de4a180d61c801397ead63a0130d8d2eb102afb526ef81bcecb2f9d1d029b"
+    sha256 cellar: :any,                 el_capitan:    "eccfcd8d4016297999d730fd185624b42e903f7dfac43bd6227c337c2b3aafea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0032166acd2a834dce65e5cc5c1ad7b9871b33acc2661fc489d1e4204cc9e6d"
   end
 
   depends_on "asciidoc" => :build
