c962b23d0ec7b895b8f6e744dffbca447aa0f2ec
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-01 22:09:51 +0000

    libforensic1394: update 0.2 bottle.

--- Formula/libforensic1394.rb
+++ Formula/libforensic1394.rb
@@ -7,14 +7,15 @@ class Libforensic1394 < Formula
   head "https://github.com/FreddieWitherden/libforensic1394.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e199817199f736890486c3667735acf01876563584bdcde56b2671ac85707ba9"
-    sha256 cellar: :any, big_sur:       "f7405930e26a2b4c9cbe939e6121fe0c20fc3b68015f899e91ba4bba75bdbdbb"
-    sha256 cellar: :any, catalina:      "4b9746197d1e43ee78530b552e42a6e6cc96908267db1685076f6e283983ab0f"
-    sha256 cellar: :any, mojave:        "90e2abdc0baef51cfb97b8f9d130f99e2ad5e1eda990f7e6fc29c0e0d1e2f79a"
-    sha256 cellar: :any, high_sierra:   "5e919cf8bce0747630324f0c203bbd1aef4d7e17d278f42bcbece48da2229c8f"
-    sha256 cellar: :any, sierra:        "e747c5c6797d48070c4a4199fe38021cd0164a052e14b21005b9caf4a47a6e3c"
-    sha256 cellar: :any, el_capitan:    "d850e7c3a04b206c6219c75ba0a00723e9a25d0c97831de289320ef0cc076aae"
-    sha256 cellar: :any, yosemite:      "b64837090b557e25444999bfc41e2023f8fc2ced465ef7ccc067938fe0ec2f2c"
+    sha256 cellar: :any,                 arm64_big_sur: "e199817199f736890486c3667735acf01876563584bdcde56b2671ac85707ba9"
+    sha256 cellar: :any,                 big_sur:       "f7405930e26a2b4c9cbe939e6121fe0c20fc3b68015f899e91ba4bba75bdbdbb"
+    sha256 cellar: :any,                 catalina:      "4b9746197d1e43ee78530b552e42a6e6cc96908267db1685076f6e283983ab0f"
+    sha256 cellar: :any,                 mojave:        "90e2abdc0baef51cfb97b8f9d130f99e2ad5e1eda990f7e6fc29c0e0d1e2f79a"
+    sha256 cellar: :any,                 high_sierra:   "5e919cf8bce0747630324f0c203bbd1aef4d7e17d278f42bcbece48da2229c8f"
+    sha256 cellar: :any,                 sierra:        "e747c5c6797d48070c4a4199fe38021cd0164a052e14b21005b9caf4a47a6e3c"
+    sha256 cellar: :any,                 el_capitan:    "d850e7c3a04b206c6219c75ba0a00723e9a25d0c97831de289320ef0cc076aae"
+    sha256 cellar: :any,                 yosemite:      "b64837090b557e25444999bfc41e2023f8fc2ced465ef7ccc067938fe0ec2f2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c997e0a787bfd5280159d056a014bbf9eda79db06cc1873d9126712a5f6ece2f"
   end
 
   depends_on "cmake" => :build
