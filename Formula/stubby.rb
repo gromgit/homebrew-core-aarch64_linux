425d7cf8d28a9d18159b5dd908dc0ee3fc59924d
Adrian Ho <the.gromgit@gmail.com>
2021-08-01 14:49:56 +0800

    stubby: service block + clean up test
    Supports https://github.com/Homebrew/linuxbrew-core/issues/23986
    
    Closes #82355.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stubby.rb
+++ Formula/stubby.rb
@@ -29,29 +29,10 @@ class Stubby < Formula
     system "make", "install"
   end
 
-  plist_options startup: true, manual: "sudo stubby -C #{HOMEBREW_PREFIX}/etc/stubby/stubby.yml"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>KeepAlive</key>
-          <true/>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/stubby</string>
-            <string>-C</string>
-            <string>#{etc}/stubby/stubby.yml</string>
-          </array>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"stubby", "-C", etc/"stubby/stubby.yml"]
+    keep_alive true
+    run_type :immediate
   end
 
   test do
@@ -78,7 +59,6 @@ class Stubby < Formula
     end
     sleep 2
 
-    output = shell_output("dig @127.0.0.1 -p 5553 getdnsapi.net")
-    assert_match "status: NOERROR", output
+    assert_match "status: NOERROR", shell_output("dig @127.0.0.1 -p 5553 getdnsapi.net")
   end
 end
