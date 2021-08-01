d7bab575bc9eb4647af5695561a52f8bd640780c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-01 04:07:26 +0000

    awscurl: update 0.24 bottle.

--- Formula/awscurl.rb
+++ Formula/awscurl.rb
@@ -9,11 +9,11 @@ class Awscurl < Formula
   head "https://github.com/okigan/awscurl.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "adad9b16b9ba0446ca1b984d20ba8eaf00173ffd2e86fb5d8644c05ace467169"
-    sha256 cellar: :any,                 big_sur:       "acee4c8f1abe9c5fd79537e3047252a1e463d3500d7d0991b0f27b1422a48a62"
-    sha256 cellar: :any,                 catalina:      "88c5978dea4124ccf5e1501e0077bdf4b070bce69450af2a80189e63f87f3a04"
-    sha256 cellar: :any,                 mojave:        "4222129e83e93919b8759bbc1dc49b9a6e80a182bc72bcbd3d117d19b2c11135"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0f23308f32902118cbc8e58e8993823f4d8a67bef12adc5ba05edf5a9f210e1"
+    sha256 cellar: :any,                 arm64_big_sur: "99eeee8b8c25ac9eca8935f086d7049009a23dd6cdff955932b69684eb5fc092"
+    sha256 cellar: :any,                 big_sur:       "a350c60b20a1dc88c929ecae6481c4e4e3bf84a7e3938035d22ea6f56a0df590"
+    sha256 cellar: :any,                 catalina:      "0f39410104d050c7a2063e03a172dd20534082b13ade4839c754e37b837df693"
+    sha256 cellar: :any,                 mojave:        "a97fdbf33c8cf3e98917ee32eaba8db79f28f69f89510c1326499c8ca19f456b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8aab85f689c56b139b609c80933d48c9256437779c8b998ee99b4b9351e689fd"
   end
 
   depends_on "rust" => :build
