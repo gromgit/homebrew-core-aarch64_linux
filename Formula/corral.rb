d0b6db00c22002a7f6d56b3ed1bd2e21ca4a5bfd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-01 20:14:02 +0000

    corral: update 0.5.3 bottle.

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -7,9 +7,10 @@ class Corral < Formula
   head "https://github.com/ponylang/corral.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "8bf55316637ccd844945fb01255fd17adc6c32450468ce0f4fa6419c3fb721d7"
-    sha256 cellar: :any_skip_relocation, catalina: "01fe7de9d69e7a37b2c1a246efc666c0ebc472e50e541dfd5f998861b9abd808"
-    sha256 cellar: :any_skip_relocation, mojave:   "89f5021d6f6ac5a1b92a6c622788dabe904fd16d631ae6546540171eafd2b671"
+    sha256 cellar: :any_skip_relocation, big_sur:      "8bf55316637ccd844945fb01255fd17adc6c32450468ce0f4fa6419c3fb721d7"
+    sha256 cellar: :any_skip_relocation, catalina:     "01fe7de9d69e7a37b2c1a246efc666c0ebc472e50e541dfd5f998861b9abd808"
+    sha256 cellar: :any_skip_relocation, mojave:       "89f5021d6f6ac5a1b92a6c622788dabe904fd16d631ae6546540171eafd2b671"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2468019691e114d92f96bddff1532c5a8e7129d6b0d65314386590cf50786596"
   end
 
   depends_on "ponyc"
