2b3df4198281325e6d76c7c6e47a39ddb9faccce
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-01 22:26:59 +0000

    simple-amqp-client: update 2.5.1 bottle.

--- Formula/simple-amqp-client.rb
+++ Formula/simple-amqp-client.rb
@@ -7,11 +7,12 @@ class SimpleAmqpClient < Formula
   head "https://github.com/alanxz/SimpleAmqpClient.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f66fc5dd89e9899a536b516486a88484a9f6e375ff2a452c686812fe2dadbc23"
-    sha256 cellar: :any, big_sur:       "f2dd44e2182fa7da6d5e9dcc255ad06138b09be9e0619ecd07135e6fc2c35405"
-    sha256 cellar: :any, catalina:      "97ceed4ae134cb5f01dc3c5efdafaccf3374aee7c748217eba9bb8624edb74dc"
-    sha256 cellar: :any, mojave:        "42bf1dcae157dc5e3ad6c274cfff63e0599d1c1fa2ed634696a26ec499e6b18f"
-    sha256 cellar: :any, high_sierra:   "0df2d53228ce5b30d670a67b36b8440158d4773c55c206456fc2762c7e820cec"
+    sha256 cellar: :any,                 arm64_big_sur: "f66fc5dd89e9899a536b516486a88484a9f6e375ff2a452c686812fe2dadbc23"
+    sha256 cellar: :any,                 big_sur:       "f2dd44e2182fa7da6d5e9dcc255ad06138b09be9e0619ecd07135e6fc2c35405"
+    sha256 cellar: :any,                 catalina:      "97ceed4ae134cb5f01dc3c5efdafaccf3374aee7c748217eba9bb8624edb74dc"
+    sha256 cellar: :any,                 mojave:        "42bf1dcae157dc5e3ad6c274cfff63e0599d1c1fa2ed634696a26ec499e6b18f"
+    sha256 cellar: :any,                 high_sierra:   "0df2d53228ce5b30d670a67b36b8440158d4773c55c206456fc2762c7e820cec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d1b3f446e95fe780934b1fde99b9bd2f880d6516910d76ca3a98ddbac5340a25"
   end
 
   depends_on "cmake" => :build
