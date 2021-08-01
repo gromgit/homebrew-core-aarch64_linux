356fdd474acc4abda0cfcdccbf0f428a142b7fba
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-01 10:43:40 +0000

    wildmidi: add 0.4.4 bottle.

--- Formula/wildmidi.rb
+++ Formula/wildmidi.rb
@@ -5,6 +5,13 @@ class Wildmidi < Formula
   sha256 "6f267c8d331e9859906837e2c197093fddec31829d2ebf7b958cf6b7ae935430"
   license all_of: ["GPL-3.0-only", "LGPL-3.0-only"]
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "d3911b6c060df898e6e8e5358a956afaf9bb042083e05be6f7af4f255b80c8bf"
+    sha256 cellar: :any, big_sur:       "258eb43be9a818dd975315669930ac09bc3c4da1c349e4dfe3c03685dc2b26e4"
+    sha256 cellar: :any, catalina:      "ca98c79414e1efd3e398c658aa16ede8af301cb46bafede7a8d1cce56bd45f38"
+    sha256 cellar: :any, mojave:        "48432227a38bc4252423f60d1eb507057ef7212ae01ee27df2a2882de851d893"
+  end
+
   depends_on "cmake" => :build
 
   def install
