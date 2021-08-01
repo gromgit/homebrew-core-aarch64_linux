718f1c6e0fe67870391378416fafacb8a60d3b44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-01 15:06:35 +0000

    watchexec: update 1.17.1 bottle.

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -6,11 +6,11 @@ class Watchexec < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fae78ca10f73e1d8ee781833cc261c2b6e03edc171850d62f95f2cc260a9d407"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9f0113bd03beb3085248f631fb9904d216546588ba29e6d957efb69e5e444550"
-    sha256 cellar: :any_skip_relocation, catalina:      "4e366f31613625af8853315fa402d528ed89d557a9cd48eaf28c40b94da196f4"
-    sha256 cellar: :any_skip_relocation, mojave:        "3c0da8f7c2b134721a9c8dd87abd59036bcc8007c25037b2420e0543772c082b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a5586ec13fd7f384f32301a5a851469c6aac9670e4d95f3d492b79d8264550c7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d83500db72a7e0dff3fc8fd8e2526e11e6d25a104ab155dc967fcc519a082b50"
+    sha256 cellar: :any_skip_relocation, big_sur:       "dd7198eb2dbb92f608a8a8eeb254d2810182f3d647f303831a5bfeeab5ab51c8"
+    sha256 cellar: :any_skip_relocation, catalina:      "52aa742b6924b99190019d0347660734d5c846a8c0e79b5b1e5658f2c1b24659"
+    sha256 cellar: :any_skip_relocation, mojave:        "9d243c2296dc406c164045adbedee423c4b0a3b74f9a8aaad1676f1a96e16d8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ca6f89236f83d52799189baa14c733c7532e9245834ed31a07d4d367579a45d"
   end
 
   depends_on "rust" => :build
