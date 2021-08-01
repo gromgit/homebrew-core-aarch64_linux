1616e976df8cea3010e6e0cc6650423245f30c3d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-01 03:34:02 +0000

    grpcui: update 1.1.0 bottle.

--- Formula/grpcui.rb
+++ Formula/grpcui.rb
@@ -11,6 +11,7 @@ class Grpcui < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "c6c62dbe114c2bb1de1a0c19d7845bdf2fe758e1810721772888776332a28897"
     sha256 cellar: :any_skip_relocation, mojave:        "9f5c2e8b3bdc9e0d4609819503f4a10de72cba88b7fb4aa614e5c315975f7436"
     sha256 cellar: :any_skip_relocation, high_sierra:   "af7abe614d2e96ce599ec1683ad9ec94c51daaf5d72613ad9c4bdedb7c9a4495"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5981bed2db80a0144c44f02f06fe9a6ad6c1b6012106faabe561e56f0e9833e7"
   end
 
   depends_on "go" => :build
