dbeedd7124ec5fdd8c6947ab75803e6292a93c1e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-01 20:23:15 +0000

    ipopt: update 3.14.2 bottle.

--- Formula/ipopt.rb
+++ Formula/ipopt.rb
@@ -7,10 +7,11 @@ class Ipopt < Formula
   head "https://github.com/coin-or/Ipopt.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c6a4280932cadfb741ff4706e3ceb84536b052238116f2303e842f78b9064bd6"
-    sha256 cellar: :any, big_sur:       "94b08bf303e9e23a052472539b848cecb27854f0a1794d78ce80c858d6670c1e"
-    sha256 cellar: :any, catalina:      "5a33a2af001eeee408f2d5aec1607459c0788dd62df7903c7b29bd61875d57bc"
-    sha256 cellar: :any, mojave:        "13a54a41b13a57ca5d2d27d19471d65162cb2902e48fff00a297cb1b923cd8db"
+    sha256 cellar: :any,                 arm64_big_sur: "c6a4280932cadfb741ff4706e3ceb84536b052238116f2303e842f78b9064bd6"
+    sha256 cellar: :any,                 big_sur:       "94b08bf303e9e23a052472539b848cecb27854f0a1794d78ce80c858d6670c1e"
+    sha256 cellar: :any,                 catalina:      "5a33a2af001eeee408f2d5aec1607459c0788dd62df7903c7b29bd61875d57bc"
+    sha256 cellar: :any,                 mojave:        "13a54a41b13a57ca5d2d27d19471d65162cb2902e48fff00a297cb1b923cd8db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56287e52d66b58ccd442827ac84f180c5a85a5513d69794e8fc367ab3b1e0d16"
   end
 
   depends_on "openjdk" => :build
