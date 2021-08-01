fdc557bd0410ed994632afe7a34176d5b156b694
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-01 08:57:57 +0000

    tundra: update 2.15 bottle.

--- Formula/tundra.rb
+++ Formula/tundra.rb
@@ -12,9 +12,10 @@ class Tundra < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:  "dbbb68b203aa7c4550f6c16fcf5ad2ad848c2f5620cbbd8218398d69671bc3e7"
-    sha256 cellar: :any_skip_relocation, catalina: "f36ca8b0357c6687cc29a58cca525de2f5f2abbd8d1ec69137cbc5a511745492"
-    sha256 cellar: :any_skip_relocation, mojave:   "d89e65ad931ef48f287108e2a06e5b64f34ecb82a00c6b0413833b867c27c764"
+    sha256 cellar: :any_skip_relocation, big_sur:      "dbbb68b203aa7c4550f6c16fcf5ad2ad848c2f5620cbbd8218398d69671bc3e7"
+    sha256 cellar: :any_skip_relocation, catalina:     "f36ca8b0357c6687cc29a58cca525de2f5f2abbd8d1ec69137cbc5a511745492"
+    sha256 cellar: :any_skip_relocation, mojave:       "d89e65ad931ef48f287108e2a06e5b64f34ecb82a00c6b0413833b867c27c764"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e37ea4c934066117b10d4f133c74a5b0527dca0d3509410ddeb3ccb02f3be22a"
   end
 
   depends_on "googletest" => :build
