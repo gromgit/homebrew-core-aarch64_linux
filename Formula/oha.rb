a16d3150a62b1400a86ee88c84e3298db707693a
Michael Cho <cho-m@tuta.io>
2021-08-02 12:41:03 -0700

    oha: fix build for Linux (#82426)

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -15,6 +15,12 @@ class Oha < Formula
 
   depends_on "rust" => :build
 
+  uses_from_macos "openssl@1.1"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
