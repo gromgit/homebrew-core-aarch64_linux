f434a502c5056d46fd5dd74baf07c15227f5d319
Skylar MacDonald <skylar@skylarmacdonald.com>
2021-08-02 14:40:45 +0100

    bork 0.13.0
    Update Bork to version 0.13.0
    
    Closes #82434.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bork.rb
+++ Formula/bork.rb
@@ -1,8 +1,8 @@
 class Bork < Formula
   desc "Bash-Operated Reconciling Kludge"
   homepage "https://github.com/skylarmacdonald/bork"
-  url "https://github.com/skylarmacdonald/bork/archive/v0.12.0.tar.gz"
-  sha256 "525f797a5ad01734d298852a038c2a2cb338ee9bb038c560bb20ecf142b1588b"
+  url "https://github.com/skylarmacdonald/bork/archive/v0.13.0.tar.gz"
+  sha256 "5eaca1ebd984121df008b93c43ac259a455db7ccf13da1b1465d704e1faab563"
   license "Apache-2.0"
   head "https://github.com/skylarmacdonald/bork.git", branch: "main"
 
@@ -15,12 +15,13 @@ class Bork < Formula
     files = %w[types/shells.sh types/pipsi.sh types/cask.sh test/type-pipsi.bats test/type-cask.bats]
     inreplace files, "/usr/local/", HOMEBREW_PREFIX
 
-    prefix.install %w[bin docs lib test types]
+    man1.install "docs/bork.1"
+    prefix.install %w[bin lib test types]
   end
 
   test do
     expected_output = "checking: directory #{testpath}/foo\r" \
-                      "missing: directory #{testpath}/foo \n" \
+                      "missing: directory #{testpath}/foo           \n" \
                       "verifying : directory #{testpath}/foo\n" \
                       "* success\n"
     assert_match expected_output, shell_output("#{bin}/bork do ok directory #{testpath}/foo", 1)
