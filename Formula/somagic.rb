196741ca930b3fdbdeaf4be8d2fb0337116d97aa
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-02 00:08:44 +0000

    somagic: update 1.1 bottle.

--- Formula/somagic.rb
+++ Formula/somagic.rb
@@ -5,14 +5,15 @@ class Somagic < Formula
   sha256 "3a9dd78a47335a6d041cd5465d28124612dad97939c56d7c10e000484d78a320"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5a8bb98812f68221bf3db7667aa281e9c18b111837bf1b5167adf30e498b53ff"
-    sha256 cellar: :any, big_sur:       "5451a0d35aa0aedcb43d1aeb6e080804333f8f7abaa090ceab62b1d02482389f"
-    sha256 cellar: :any, catalina:      "41d2479b3d2a267bbcd8c5db4ea7a8fe04c120d260d2ac9f087bd386012a3971"
-    sha256 cellar: :any, mojave:        "c2a69924be6f0d397b244955cfa841567ecd3171dc2674a4ac9748f49f58a44b"
-    sha256 cellar: :any, high_sierra:   "b6c11695d2c25a49a4a2c5795764a83615a214630bc25914e65fc691662617fc"
-    sha256 cellar: :any, sierra:        "377ecbdc01ebaab2acf1101aa00bbf5554e7d56b1b630baa28ef70d9deb10811"
-    sha256 cellar: :any, el_capitan:    "ed8a82423daaabaca0a7ab203edc68b3c0a1a1d617eb24d46486dfa974e9eb4f"
-    sha256 cellar: :any, yosemite:      "9c87f9d7a694509b446ce726cedcb731c0185b589a1cdfa96c0346f883a75e5d"
+    sha256 cellar: :any,                 arm64_big_sur: "5a8bb98812f68221bf3db7667aa281e9c18b111837bf1b5167adf30e498b53ff"
+    sha256 cellar: :any,                 big_sur:       "5451a0d35aa0aedcb43d1aeb6e080804333f8f7abaa090ceab62b1d02482389f"
+    sha256 cellar: :any,                 catalina:      "41d2479b3d2a267bbcd8c5db4ea7a8fe04c120d260d2ac9f087bd386012a3971"
+    sha256 cellar: :any,                 mojave:        "c2a69924be6f0d397b244955cfa841567ecd3171dc2674a4ac9748f49f58a44b"
+    sha256 cellar: :any,                 high_sierra:   "b6c11695d2c25a49a4a2c5795764a83615a214630bc25914e65fc691662617fc"
+    sha256 cellar: :any,                 sierra:        "377ecbdc01ebaab2acf1101aa00bbf5554e7d56b1b630baa28ef70d9deb10811"
+    sha256 cellar: :any,                 el_capitan:    "ed8a82423daaabaca0a7ab203edc68b3c0a1a1d617eb24d46486dfa974e9eb4f"
+    sha256 cellar: :any,                 yosemite:      "9c87f9d7a694509b446ce726cedcb731c0185b589a1cdfa96c0346f883a75e5d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d574fbe24e39eaf6e35a4d8317c689ae890983a801e8abf8e5dc0a2941a1104b"
   end
 
   depends_on "libgcrypt"
