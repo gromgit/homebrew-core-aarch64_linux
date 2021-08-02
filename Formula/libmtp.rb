430a0d1bd998223554ca69a4933d0f3c1670c269
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-02 20:06:49 +0000

    libmtp: update 1.1.18 bottle.

--- Formula/libmtp.rb
+++ Formula/libmtp.rb
@@ -6,11 +6,12 @@ class Libmtp < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4af12c090f3214200d4a37b9511c1fc1ba0269b40f26c0e9c45c4dbfe2c64474"
-    sha256 cellar: :any, big_sur:       "5ebeb1696d5c7af72cb4a14f905dbde2cd871334ea392e7e8ff0305159c09aa1"
-    sha256 cellar: :any, catalina:      "9b305e731b8d8608b688bb5c8bb98192d4879eb944fd4b08c09daadf367b68fc"
-    sha256 cellar: :any, mojave:        "e4c497e80277170743a4ff8ddde06687a01f3afb053088b921b8399796f630ae"
-    sha256 cellar: :any, high_sierra:   "704cd1e718e42dc9284ca020a11c1788d8a222cb8a4ca939d6b289cd17cf86ad"
+    sha256 cellar: :any,                 arm64_big_sur: "4af12c090f3214200d4a37b9511c1fc1ba0269b40f26c0e9c45c4dbfe2c64474"
+    sha256 cellar: :any,                 big_sur:       "5ebeb1696d5c7af72cb4a14f905dbde2cd871334ea392e7e8ff0305159c09aa1"
+    sha256 cellar: :any,                 catalina:      "9b305e731b8d8608b688bb5c8bb98192d4879eb944fd4b08c09daadf367b68fc"
+    sha256 cellar: :any,                 mojave:        "e4c497e80277170743a4ff8ddde06687a01f3afb053088b921b8399796f630ae"
+    sha256 cellar: :any,                 high_sierra:   "704cd1e718e42dc9284ca020a11c1788d8a222cb8a4ca939d6b289cd17cf86ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80b10da726a70f661e1d6a3d58e1fcbaab5afe23c7165a5588e8558710fd6887"
   end
 
   depends_on "pkg-config" => :build
