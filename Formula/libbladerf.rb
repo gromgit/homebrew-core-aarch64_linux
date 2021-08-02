5ed19a32124cfc6dd0518247e712d8295b297e33
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-02 20:08:06 +0000

    libbladerf: update 2021.02 bottle.

--- Formula/libbladerf.rb
+++ Formula/libbladerf.rb
@@ -13,10 +13,11 @@ class Libbladerf < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "8aad9ec5a110be875915aefb2eb22a0027939aa41c9f19beacf369990012c96b"
-    sha256 big_sur:       "7f31ea534f7b3e45b6aa757e437a81e463b88adead45f70583de6528c8d07c1c"
-    sha256 catalina:      "6f663f523dfb1cada7a8637c2a8715f7da520fd7c53872a97212f776ca7639d9"
-    sha256 mojave:        "6cf6caa846b8cab56722c692912e37485598d7bc415ffbe722e961bb1f219cff"
+    sha256                               arm64_big_sur: "8aad9ec5a110be875915aefb2eb22a0027939aa41c9f19beacf369990012c96b"
+    sha256                               big_sur:       "7f31ea534f7b3e45b6aa757e437a81e463b88adead45f70583de6528c8d07c1c"
+    sha256                               catalina:      "6f663f523dfb1cada7a8637c2a8715f7da520fd7c53872a97212f776ca7639d9"
+    sha256                               mojave:        "6cf6caa846b8cab56722c692912e37485598d7bc415ffbe722e961bb1f219cff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d8bfc858fbdd0a9c6b98ad595e0aae81bbf22286df3e73f994ce3d6617d020f"
   end
 
   depends_on "cmake" => :build
