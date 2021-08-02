18b8cc875d40458f402ddb9d3905146a6738bc22
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-02 02:22:35 +0000

    ndenv: update 0.4.0 bottle.

--- Formula/ndenv.rb
+++ Formula/ndenv.rb
@@ -11,6 +11,7 @@ class Ndenv < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "11134806587add67781fb03d7be2fd2322029e77e4b744d927fba9afbe6e1b82"
     sha256 cellar: :any_skip_relocation, catalina:      "11134806587add67781fb03d7be2fd2322029e77e4b744d927fba9afbe6e1b82"
     sha256 cellar: :any_skip_relocation, mojave:        "11134806587add67781fb03d7be2fd2322029e77e4b744d927fba9afbe6e1b82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50382fe45272fa7f72497bb0f40a02fd4bf9e293eca303777b9e154021f7c501"
   end
 
   depends_on "node-build"
