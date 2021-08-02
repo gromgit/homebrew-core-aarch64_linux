5d28d556fd99db8ca7d7e770f4230560b8711d8c
Branch Vincent <branchevincent@gmail.com>
2021-08-01 22:29:35 -0400

    pilosa: replace `plist` with `service`

--- Formula/pilosa.rb
+++ Formula/pilosa.rb
@@ -24,33 +24,10 @@ class Pilosa < Formula
     end
   end
 
-  plist_options manual: "pilosa server"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{opt_bin}/pilosa</string>
-              <string>server</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <dict>
-              <key>SuccessfulExit</key>
-              <false/>
-          </dict>
-          <key>WorkingDirectory</key>
-          <string>#{var}</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"pilosa", "server"]
+    keep_alive true
+    working_dir var
   end
 
   test do
