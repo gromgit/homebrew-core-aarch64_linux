e27b3bd1a3e8143b973037b08bffe236ee140abc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-02 08:12:29 +0000

    functionalplus: update 0.2.15 bottle.

--- Formula/functionalplus.rb
+++ Formula/functionalplus.rb
@@ -8,7 +8,7 @@ class Functionalplus < Formula
   head "https://github.com/Dobiasd/FunctionalPlus.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2d9d54699712e47f98c9ca5444d9010ebfe97abf94b411ca0d63889003782771"
+    sha256 cellar: :any_skip_relocation, all: "ab75da7d297b2258e1e906027bd3d5abe6b7464339288730eae5fec22b931715"
   end
 
   depends_on "cmake" => :build
