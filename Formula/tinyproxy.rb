83a01e92acbf39cd0467e6a2963b55ba28ee7e6c
Branch Vincent <branchevincent@gmail.com>
2021-08-02 17:25:54 -0400

    tinyproxy: replace `plist` with `service`

--- Formula/tinyproxy.rb
+++ Formula/tinyproxy.rb
@@ -47,30 +47,10 @@ class Tinyproxy < Formula
     (var/"run/tinyproxy").mkpath
   end
 
-  plist_options manual: "tinyproxy"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <false/>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{opt_bin}/tinyproxy</string>
-              <string>-d</string>
-          </array>
-          <key>WorkingDirectory</key>
-          <string>#{HOMEBREW_PREFIX}</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"tinyproxy", "-d"]
+    keep_alive false
+    working_dir HOMEBREW_PREFIX
   end
 
   test do
