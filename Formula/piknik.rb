e4260f50e4d5164c625ce29447c02ba0c1dadcd6
Branch Vincent <branchevincent@gmail.com>
2021-08-01 22:29:35 -0400

    piknik: replace `plist` with `service`

--- Formula/piknik.rb
+++ Formula/piknik.rb
@@ -28,26 +28,8 @@ class Piknik < Formula
     EOS
   end
 
-  plist_options manual: "piknik -server"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/piknik</string>
-            <string>-server</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"piknik", "-server"]
   end
 
   test do
