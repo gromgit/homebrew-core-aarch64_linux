7bddfaf4dc625d1db65797931e00b8146df929a0
Caleb Xu <calebcenter@live.com>
2021-08-03 09:17:51 -0400

    diffutils 3.8
    Closes #82488.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -1,9 +1,9 @@
 class Diffutils < Formula
   desc "File comparison utilities"
   homepage "https://www.gnu.org/s/diffutils/"
-  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.7.tar.xz"
-  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.7.tar.xz"
-  sha256 "b3a7a6221c3dc916085f0d205abf6b8e1ba443d4dd965118da364a1dc1cb3a26"
+  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.8.tar.xz"
+  sha256 "a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec"
   license "GPL-3.0"
 
   bottle do
