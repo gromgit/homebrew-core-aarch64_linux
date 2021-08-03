f8ab537ab8cfc92849536df764e5e867fa65c1f3
Branch Vincent <branchevincent@gmail.com>
2021-08-02 21:40:55 -0400

    vsftpd: replace `plist` with `service`
    Closes #82463.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vsftpd.rb
+++ Formula/vsftpd.rb
@@ -52,26 +52,9 @@ class Vsftpd < Formula
     EOS
   end
 
-  plist_options startup: true, manual: "sudo vsftpd"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{sbin}/vsftpd</string>
-          <string>#{etc}/vsftpd.conf</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-      </plist>
-    EOS
+  plist_options startup: true
+  service do
+    run [opt_sbin/"vsftpd", etc/"vsftpd.conf"]
   end
 
   test do
