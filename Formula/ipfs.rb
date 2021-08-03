e1045e88fc9f38026c144a883149e0af131fc194
Branch Vincent <branchevincent@gmail.com>
2021-08-03 00:14:55 -0400

    ipfs: replace `plist` with `service`

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -32,26 +32,8 @@ class Ipfs < Formula
     bash_completion.install "misc/completion/ipfs-completion.bash"
   end
 
-  plist_options manual: "ipfs daemon"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/ipfs</string>
-          <string>daemon</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"ipfs", "daemon"]
   end
 
   test do
