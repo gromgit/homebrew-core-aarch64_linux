0fb1f008fa373b3bedccdee256668811848ffc2e
Branch Vincent <branchevincent@gmail.com>
2021-08-03 19:24:46 -0400

    h2o: replace `plist` with `service`

--- Formula/h2o.rb
+++ Formula/h2o.rb
@@ -60,29 +60,9 @@ class H2o < Formula
     EOS
   end
 
-  plist_options manual: "h2o"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{opt_bin}/h2o</string>
-              <string>-c</string>
-              <string>#{etc}/h2o/h2o.conf</string>
-          </array>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"h2o", "-c", etc/"h2o/h2o.conf"]
+    keep_alive true
   end
 
   test do
