158c199e3e45bb336c9de8b4de824598c8c6a698
Alexander Bayandin <a.bayandin@gmail.com>
2021-08-03 11:48:41 +0100

    vowpal-wabbit: revision bump (fmt 8.0.1)

--- Formula/vowpal-wabbit.rb
+++ Formula/vowpal-wabbit.rb
@@ -6,6 +6,7 @@ class VowpalWabbit < Formula
       tag:      "8.11.0",
       revision: "96ed8316de4391b77f4f29af69f885552a644769"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/VowpalWabbit/vowpal_wabbit.git"
 
   bottle do
