2b4a941b45dda4c2c0a4fde5cf63186aa96fdf25
Michael Cho <cho-m@tuta.io>
2021-08-02 19:48:30 -0700

    watchman 2021.08.02.00
    Closes #82474.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -1,8 +1,8 @@
 class Watchman < Formula
   desc "Watch files and take action when they change"
   homepage "https://github.com/facebook/watchman"
-  url "https://github.com/facebook/watchman/archive/v2021.06.07.00.tar.gz"
-  sha256 "d5e1e8f4812f0ecd58c108f5844de726a0701bd5dfebe04c4f2a6c61dda0b256"
+  url "https://github.com/facebook/watchman/archive/v2021.08.02.00.tar.gz"
+  sha256 "8994793334422d5101a087aadec55aa2d73979c0b4b63368dfc1a9ac5e547a2d"
   license "Apache-2.0"
   head "https://github.com/facebook/watchman.git"
 
@@ -14,7 +14,6 @@ class Watchman < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "googletest" => :build
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "boost"
@@ -27,7 +26,29 @@ class Watchman < Formula
   depends_on "pcre"
   depends_on "python@3.9"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  # The `googletest` formula (v1.11+) currently causes build failures.
+  # On macOS: watchman_string.h:114:16: error: no member named 'data' in 'watchman_pending_fs'
+  # On Linux: gtest-printers.h:211:33: error: no match for 'operator<<'
+  # Use https://github.com/facebook/watchman/blob/#{version}/build/fbcode_builder/manifests/googletest
+  resource "googletest" do
+    url "https://github.com/google/googletest/archive/release-1.10.0.tar.gz"
+    sha256 "9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c716dcb"
+  end
+
   def install
+    resource("googletest").stage do
+      cmake_args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] }
+      system "cmake", ".", *cmake_args, "-DCMAKE_INSTALL_PREFIX=#{buildpath}/googletest"
+      system "make", "install"
+    end
+    ENV["GTest_DIR"] = ENV["GMock_DIR"] = buildpath/"googletest"
+
     system "cmake", "-S", ".", "-B", "build",
                     "-DBUILD_SHARED_LIBS=ON",
                     "-DWATCHMAN_VERSION_OVERRIDE=#{version}",
