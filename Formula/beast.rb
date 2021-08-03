1722fde37d73a074a29c60bb4d3bd3c1cf36e662
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-03 19:38:33 +0000

    beast: update 1.10.4_1 bottle.

--- Formula/beast.rb
+++ Formula/beast.rb
@@ -13,10 +13,11 @@ class Beast < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "90bc7bcf414bfc4d9a68e7dbade089260bb12483939a78a136cdb9b2ea1a3bcb"
-    sha256 cellar: :any_skip_relocation, catalina:    "5f4b312595410d83df9099dc15657241dc4cb758d58a5836565127275a6fb912"
-    sha256 cellar: :any_skip_relocation, mojave:      "d441fd3733557c8de6c227663566e9ac668562a7ecf113504a8c604490752763"
-    sha256 cellar: :any_skip_relocation, high_sierra: "2c157d2d74ef17b3fcf8f5cf11d62d1b7ba939f0d7d48872d83706cbeb2b2908"
+    sha256 cellar: :any_skip_relocation, big_sur:      "90bc7bcf414bfc4d9a68e7dbade089260bb12483939a78a136cdb9b2ea1a3bcb"
+    sha256 cellar: :any_skip_relocation, catalina:     "5f4b312595410d83df9099dc15657241dc4cb758d58a5836565127275a6fb912"
+    sha256 cellar: :any_skip_relocation, mojave:       "d441fd3733557c8de6c227663566e9ac668562a7ecf113504a8c604490752763"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "2c157d2d74ef17b3fcf8f5cf11d62d1b7ba939f0d7d48872d83706cbeb2b2908"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cce35bb9ee56a4a093d8893c401609b868eea6782d269d716af31a620d0f82ab"
   end
 
   depends_on "ant" => :build
