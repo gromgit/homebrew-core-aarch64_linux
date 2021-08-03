ff6edb961a358d0dacfccbbde908cb8b4077ff4e
Rui Chen <rui@chenrui.dev>
2021-08-03 11:06:43 -0400

    bundletool 1.7.1
    Closes #82512.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bundletool.rb
+++ Formula/bundletool.rb
@@ -1,8 +1,8 @@
 class Bundletool < Formula
   desc "Command-line tool to manipulate Android App Bundles"
   homepage "https://github.com/google/bundletool"
-  url "https://github.com/google/bundletool/releases/download/1.7.0/bundletool-all-1.7.0.jar"
-  sha256 "2a9202b8e0be1d416b9223b2a81d873097589af5eb009342cecee2cd5b1ba36b"
+  url "https://github.com/google/bundletool/releases/download/1.7.1/bundletool-all-1.7.1.jar"
+  sha256 "72d930df2b692347abcfa787d2dcfe4d08c19812ab7aedbc8db5546aa4fcb7a2"
   license "Apache-2.0"
 
   bottle do
