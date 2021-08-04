2bb6839a5b0c818210c81a7e3f15ae326ca488f4
Tom Forbes <tom@tomforb.es>
2021-08-04 01:37:36 +0100

    newrelic-cli 0.32.2
    Closes #82592.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,8 +1,8 @@
 class NewrelicCli < Formula
   desc "Command-line interface for New Relic"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.32.1.tar.gz"
-  sha256 "4e9c4fda4aec0b5ab677724b3052e982dee1d0923a7719d2f105bc0b7f9a19f7"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.32.2.tar.gz"
+  sha256 "95f20e1ac5409c2dd2e25b5bdc54805733e030d6c92076f3bde6545049bc4f62"
   license "Apache-2.0"
   head "https://github.com/newrelic/newrelic-cli.git"
 
