caba7d8ef80c9570b27dcdc03662fe0f78f6d14b
danielnachun <daniel.nachun@gmail.com>
2021-08-03 21:47:10 -0700

    sqliteodbc: upstream Linux fixes

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -26,12 +26,14 @@ class Sqliteodbc < Formula
     ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra
 
     lib.mkdir
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-odbc=#{Formula["unixodbc"].opt_prefix}",
-                          "--with-sqlite3=#{Formula["sqlite"].opt_prefix}"
+    args = ["--with-odbc=#{Formula["unixodbc"].opt_prefix}",
+            "--with-sqlite3=#{Formula["sqlite"].opt_prefix}"]
+    on_linux { args << "--with-libxml2=#{Formula["libxml2"].opt_prefix}" }
+
+    system "./configure", "--prefix=#{prefix}", *args
     system "make"
     system "make", "install"
-    lib.install_symlink "#{lib}/libsqlite3odbc.dylib" => "libsqlite3odbc.so"
+    on_macos { lib.install_symlink lib/"libsqlite3odbc.dylib" => "libsqlite3odbc.so" }
   end
 
   test do
