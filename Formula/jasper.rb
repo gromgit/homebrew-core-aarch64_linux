3af342e547581fe28fa07d097e499242aa02d3ed
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-04 05:26:23 +0000

    jasper: update 2.0.33 bottle.

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -11,11 +11,11 @@ class Jasper < Formula
   end
 
   bottle do
-    sha256                               arm64_big_sur: "b3d001bfed26a9aba810a8b65ef0ccd01500d81afefca702c202f809c7e0fc24"
-    sha256                               big_sur:       "3b56f3d1d584df4483b5d35d5d75235bd621201ae7678f58d624b86f523ab6d2"
-    sha256                               catalina:      "fce68cb17b6de9af96e493e97091bd4679701884da4f2f413ead6985f511497d"
-    sha256                               mojave:        "5e9f20212c7caa5aafd9f12ee56d1e73d179ad62f9a3ac45a8843d09f6e24aea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "feefb3e6313f8bed579aa786814d2099f6364646d6e9c7b53d97bd9260df06bd"
+    sha256 cellar: :any,                 arm64_big_sur: "d3a5b5039c95970e08dc6265aa845e4a4e675987519fd1cfe5ae3990b0b236b8"
+    sha256 cellar: :any,                 big_sur:       "7462315306489ccf06bddad20b9ba97b5872574f8155dfa7f5d316d905da76b8"
+    sha256 cellar: :any,                 catalina:      "7c7ae386ed0221d5b04f50b0258afddda977ec8e968f3dcb1e099a1826bb30d7"
+    sha256 cellar: :any,                 mojave:        "4c6c520094341ba62068984ed5ee928c992c942d3040956818a92c6a43f26724"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f37c44732a8782727150b229ad8dc04bd4f2261b95be05c5d11bebbf197326d"
   end
 
   depends_on "cmake" => :build
