12fe2bfc595cadfe93e170e6f03fc0211378f973
Tom Forbes <tom@tomforb.es>
2021-08-04 01:48:24 +0100

    pacapt 3.0.4
    Closes #82617.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pacapt.rb
+++ Formula/pacapt.rb
@@ -1,8 +1,8 @@
 class Pacapt < Formula
   desc "Package manager in the style of Arch's pacman"
   homepage "https://github.com/icy/pacapt"
-  url "https://github.com/icy/pacapt/archive/v3.0.3.tar.gz"
-  sha256 "b9e984f9ed81fcf9d1aaf270b18c512ccc7443fc15412530e4b2ec3c754bf4a6"
+  url "https://github.com/icy/pacapt/archive/v3.0.4.tar.gz"
+  sha256 "3f62e211fcabfe951a1369bef684996fda65faceafcd2ceeb4b738cf43622c4d"
   license "Fair"
 
   bottle do
