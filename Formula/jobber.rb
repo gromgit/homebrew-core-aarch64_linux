49a42907504feb9cc4150ae03355849717e90105
Branch Vincent <branchevincent@gmail.com>
2021-08-04 17:07:35 -0400

    jobber: replace `plist` with `service`
    Closes #82695.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jobber.rb
+++ Formula/jobber.rb
@@ -24,28 +24,11 @@ class Jobber < Formula
   end
 
   plist_options startup: true
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>Program</key>
-          <string>#{libexec}/jobbermaster</string>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/jobber.log</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/jobber.log</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run libexec/"jobbermaster"
+    keep_alive true
+    log_path var/"log/jobber.log"
+    error_log_path var/"log/jobber.log"
   end
 
   test do
