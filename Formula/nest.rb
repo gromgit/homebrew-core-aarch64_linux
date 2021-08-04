1929697120e10702e6e70e12660477066daab660
Tom Forbes <tom@tomforb.es>
2021-08-04 01:10:04 +0100

    nest 3.0
    * nest 3.0
    * nest: fix build; remove buildtime tests and deps
    
    Closes #82550.
    
    Co-authored-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nest.rb
+++ Formula/nest.rb
@@ -1,8 +1,8 @@
 class Nest < Formula
   desc "Neural Simulation Tool (NEST) with Python3 bindings (PyNEST)"
   homepage "https://www.nest-simulator.org/"
-  url "https://github.com/nest/nest-simulator/archive/v2.20.1.tar.gz"
-  sha256 "df3d32b5899d5d444f708037b290f889ac6ff8eae6b7be9e9faee2c0d660d8e5"
+  url "https://github.com/nest/nest-simulator/archive/v3.0.tar.gz"
+  sha256 "d481ea67f3251fe3aadf5252ab0a999172f0cd5536c5985366d271d772e686e6"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -19,22 +19,11 @@ class Nest < Formula
   depends_on "numpy"
   depends_on "python@3.9"
   depends_on "readline"
-  depends_on "scipy"
-
-  resource "nose" do
-    url "https://files.pythonhosted.org/packages/58/a5/0dc93c3ec33f4e281849523a5a913fa1eea9a3068acfa754d44d88107a44/nose-1.3.7.tar.gz"
-    sha256 "f1bffef9cbc82628f6e7d7b40d7e255aefaa1adb6a1b1d26c69a8b79e6208a98"
-  end
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
-    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
-  end
 
   def install
     args = ["-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}"]
 
     libomp = Formula["libomp"]
-    args << "-Dwith-python=3"
     args << "-Dwith-openmp=ON"
     args << "-Dwith-libraries=#{libomp.opt_lib}/libomp.dylib"
     args << "-DOpenMP_C_FLAGS=-Xpreprocessor\ -fopenmp\ -I#{libomp.opt_include}"
@@ -42,28 +31,11 @@ class Nest < Formula
     args << "-DOpenMP_CXX_FLAGS=-Xpreprocessor\ -fopenmp\ -I#{libomp.opt_include}"
     args << "-DOpenMP_CXX_LIB_NAMES=omp"
     args << "-DOpenMP_omp_LIBRARY=#{libomp.opt_lib}/libomp.dylib"
-    python = Formula["python@3.9"]
-    python_exec = python.opt_bin/"python3"
-
-    resource("nose").stage do
-      system python_exec, *Language::Python.setup_install_args(libexec)
-    end
-    resource("six").stage do
-      system python_exec, *Language::Python.setup_install_args(libexec)
-    end
-    version = Language::Python.major_minor_version python.opt_bin/"python3"
-    site_packages = "lib/python#{version}/site-packages"
-    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
-    (prefix/site_packages/"homebrew-nest.pth").write pth_contents
-
-    ENV.prepend_create_path "PATH", libexec/"bin"
-    ENV.prepend_create_path "PYTHONPATH", libexec/site_packages
 
     mkdir "build" do
       system "cmake", "..", *args
       system "make"
       system "make", "install"
-      system "make", "installcheck"
     end
 
     # Replace internally accessible gcc with externally accessible version
