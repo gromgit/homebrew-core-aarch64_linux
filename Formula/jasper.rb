560842a00068f528827f07e8f6bf1223beb90daa
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-04 14:42:55 +0800

    jasper: update homepage and livecheck

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -1,13 +1,13 @@
 class Jasper < Formula
   desc "Library for manipulating JPEG-2000 images"
-  homepage "https://github.com/jasper-software/jasper"
+  homepage "https://ece.engr.uvic.ca/~frodo/jasper/"
   url "https://github.com/jasper-software/jasper/releases/download/version-2.0.33/jasper-2.0.33.tar.gz"
   sha256 "28d28290cc2eaf70c8756d391ed8bcc8ab809a895b9a67ea6e89da23a611801a"
   license "JasPer-2.0"
 
   livecheck do
-    url :homepage
-    regex(/(\d+(?:\.\d+)+)$/i)
+    url :stable
+    regex(/^version[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
   bottle do
