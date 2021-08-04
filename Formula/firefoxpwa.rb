af0c0e2f2b9d762ccad6c301be0bea8d64b9a050
Filip Š <filip.stamcar@hotmail.com>
2021-08-04 21:32:23 +0200

    firefoxpwa 1.0.0 (new formula)
    * firefoxpwa 1.0.0 (new formula)
    * Make caveat less verbose
    * Use `opt_share` for manifest source
    
    Closes #82690.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/firefoxpwa.rb
@@ -0,0 +1,56 @@
+class Firefoxpwa < Formula
+  desc "Tool to install, manage and use Progressive Web Apps in Mozilla Firefox"
+  homepage "https://github.com/filips123/FirefoxPWA"
+  url "https://github.com/filips123/FirefoxPWA/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "47cb03a8f5773da235e360655a4ef93203d7d3ed760ebf6013692ec20239c2c1"
+  license "MPL-2.0"
+  head "https://github.com/filips123/FirefoxPWA.git"
+
+  depends_on "rust" => :build
+
+  def install
+    cd "native"
+
+    # Prepare the project to work with Homebrew
+    ENV["FFPWA_EXECUTABLES"] = bin
+    ENV["FFPWA_SYSDATA"] = share
+    system "bash", "./packages/brew/configure.sh", version, bin, libexec
+
+    # Build and install the project
+    system "cargo", "install", *std_cargo_args
+
+    # Install all files
+    libexec.install bin/"firefoxpwa-connector"
+    share.install "manifests/brew.json" => "firefoxpwa.json"
+    share.install "userchrome/"
+    bash_completion.install "target/release/completions/firefoxpwa.bash" => "firefoxpwa"
+    fish_completion.install "target/release/completions/firefoxpwa.fish"
+    zsh_completion.install "target/release/completions/_firefoxpwa"
+  end
+
+  def caveats
+    filename = "firefoxpwa.json"
+
+    source = opt_share
+    destination = "/Library/Application Support/Mozilla/NativeMessagingHosts"
+
+    on_linux do
+      destination = "/usr/lib/mozilla/native-messaging-hosts"
+    end
+
+    <<~EOS
+      To use the browser extension, manually link the app manifest with:
+        sudo mkdir -p #{destination}
+        sudo ln -sf "#{source}/#{filename}" "#{destination}/#{filename}"
+    EOS
+  end
+
+  test do
+    # Test version so we know if Homebrew configure script correctly sets it
+    assert_match "firefoxpwa #{version}", shell_output("#{bin}/firefoxpwa --version")
+
+    # Test launching non-existing site which should fail
+    output = shell_output("#{bin}/firefoxpwa site launch 00000000000000000000000000 2>&1", 1)
+    assert_includes output, "Site does not exist"
+  end
+end
