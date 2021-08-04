9ab88baf97b3a215dfc0595f3943980b9589f933
Tom Forbes <tom@tomforb.es>
2021-08-04 01:34:38 +0100

    glooctl 1.8.5
    Closes #82585.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -2,8 +2,8 @@ class Glooctl < Formula
   desc "Envoy-Powered API Gateway"
   homepage "https://docs.solo.io/gloo/latest/"
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.8.3",
-      revision: "e536d8b9fdf37ef3e705b72cbc5930c53c78bb45"
+      tag:      "v1.8.5",
+      revision: "f1b134aea13345e03f553e6141abdb7d722d2e3d"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git"
 
