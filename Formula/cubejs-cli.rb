3f949f2b5105bf806b5dce90e804bd61c7044551
rui <rui@chenrui.dev>
2021-08-05 17:13:41 +0000

    cubejs-cli: update 0.28.14 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -8,11 +8,11 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f2f8d3cd9c443d83a2dc86c069c2084ffd14a11b6bed40a0d8597d343a01b0c4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5c131abee692f3041a38559c98fabe8bab38efce21d95986143297fc1f216d17"
-    sha256 cellar: :any_skip_relocation, catalina:      "5c131abee692f3041a38559c98fabe8bab38efce21d95986143297fc1f216d17"
-    sha256 cellar: :any_skip_relocation, mojave:        "5c131abee692f3041a38559c98fabe8bab38efce21d95986143297fc1f216d17"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2f8d3cd9c443d83a2dc86c069c2084ffd14a11b6bed40a0d8597d343a01b0c4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "50840bc6cf86a548c98375527e3798e0a28711f8627efadf0a13710823a57707"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ce7ea9f026bc35a29c1d45a33639c59c51fe03184d56768c8a3617b486c2776a"
+    sha256 cellar: :any_skip_relocation, catalina:      "ce7ea9f026bc35a29c1d45a33639c59c51fe03184d56768c8a3617b486c2776a"
+    sha256 cellar: :any_skip_relocation, mojave:        "ce7ea9f026bc35a29c1d45a33639c59c51fe03184d56768c8a3617b486c2776a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50840bc6cf86a548c98375527e3798e0a28711f8627efadf0a13710823a57707"
   end
 
   depends_on "node"
