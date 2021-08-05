efb2232935c50db10e69c85876f4a2f9f1747ab4
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-05 05:26:05 +0000

    grin-wallet: update 5.0.3 bottle.

--- Formula/grin-wallet.rb
+++ Formula/grin-wallet.rb
@@ -6,9 +6,10 @@ class GrinWallet < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "a0dfda70b5c6cf1ed883e5af6b61c01aea7ddf18f6f22038aab71e06b5b9801a"
-    sha256 cellar: :any_skip_relocation, catalina: "4b43f745f1b82d9390cdaf4055c9d65e6bea850fe9ad7ebd2818789c33c45dbc"
-    sha256 cellar: :any_skip_relocation, mojave:   "3ce2c42866c3d02c527fac13c2393af01eabb9c9591ef25d5503aca073863f50"
+    sha256 cellar: :any_skip_relocation, big_sur:      "a0dfda70b5c6cf1ed883e5af6b61c01aea7ddf18f6f22038aab71e06b5b9801a"
+    sha256 cellar: :any_skip_relocation, catalina:     "4b43f745f1b82d9390cdaf4055c9d65e6bea850fe9ad7ebd2818789c33c45dbc"
+    sha256 cellar: :any_skip_relocation, mojave:       "3ce2c42866c3d02c527fac13c2393af01eabb9c9591ef25d5503aca073863f50"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c92e3242b540c4d6929d9ba81d8e3055db6e328d4f6977e388852692ca7273b1"
   end
 
   depends_on "rust" => :build
