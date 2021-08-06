5a9b09376a5d31c9d30f7d73ad8ff1c165bfbc1b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-06 12:03:18 +0000

    angle-grinder: update 0.17.0 bottle.

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -10,6 +10,7 @@ class AngleGrinder < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "74c4ae3251570186fe78ad4f55355e006a268d9eeab81405b61cd719aa931316"
     sha256 cellar: :any_skip_relocation, catalina:      "fd9b197c1537ef416c48ff759fb7889f4061aaca877b40f1b424c872e63e4dc8"
     sha256 cellar: :any_skip_relocation, mojave:        "29b8a96121053b1b604a21f93819c4ee9e6bfeafba7de24b7798d2367ab69b30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10e7831dee92f88d81165b878011b62c50c7cbab1d8a8f5e79cf389cfe6bec10"
   end
 
   depends_on "rust" => :build
