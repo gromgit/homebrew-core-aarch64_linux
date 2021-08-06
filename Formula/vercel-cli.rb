822e7d0c0a7ce45967a41835c7304c4098cf1b48
Dustin Rodrigues <dust.rod@gmail.com>
2021-08-05 21:59:53 -0400

    vercel-cli 23.1.0
    * vercel-cli 23.1.0
    * update the build
    * fix syntax
    * `vercel-cli` -> `vercel`
    
    Closes #82778.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vercel-cli.rb
+++ Formula/vercel-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class VercelCli < Formula
   desc "Command-line interface for Vercel"
   homepage "https://vercel.com/home"
-  url "https://registry.npmjs.org/vercel/-/vercel-23.0.1.tgz"
-  sha256 "4249cdb58fb3b2d843f9f9758c8e201b6f10b7773d30e13bddadab0e1b555d14"
+  url "https://registry.npmjs.org/vercel/-/vercel-23.1.0.tgz"
+  sha256 "ba8d10aaf3c7c7f1e4b32c0ac9f56fcc3ef2f938f131e8dd89e3b588997106cc"
   license "Apache-2.0"
 
   bottle do
@@ -17,12 +17,30 @@ class VercelCli < Formula
 
   depends_on "node"
 
+  on_macos do
+    depends_on "macos-term-size"
+  end
+
   def install
     rm Dir["dist/{*.exe,xsel}"]
     inreplace "dist/index.js", "exports.default = getUpdateCommand",
                                "exports.default = async()=>'brew upgrade vercel-cli'"
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    term_size_vendor_dir = libexec/"lib/node_modules/vercel/node_modules/term-size/vendor"
+    term_size_vendor_dir.rmtree # remove pre-built binaries
+
+    dist_dir = libexec/"lib/node_modules/vercel/dist"
+    rm_rf dist_dir/"term-size"
+
+    on_macos do
+      macos_dir = term_size_vendor_dir/"macos"
+      macos_dir.mkpath
+      # Replace the vendored pre-built term-size with one we build ourselves
+      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
+      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(dist_dir), dist_dir
+    end
   end
 
   test do
