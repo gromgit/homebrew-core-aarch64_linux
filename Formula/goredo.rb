1a80ab1b3ac2cc38b93ced08561ab1312b30d2e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-06 19:07:29 +0000

    goredo: update 1.13.0 bottle.

--- Formula/goredo.rb
+++ Formula/goredo.rb
@@ -12,11 +12,11 @@ class Goredo < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1328ba7ca75ef76ae7d29323d2c59b6b025475c548cb93489776025c6b07ff1d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "666734c6443d0cdd28e37af8df1e204fe5ea5ea7dd21d7ec4a0f237ce252b315"
-    sha256 cellar: :any_skip_relocation, catalina:      "666734c6443d0cdd28e37af8df1e204fe5ea5ea7dd21d7ec4a0f237ce252b315"
-    sha256 cellar: :any_skip_relocation, mojave:        "666734c6443d0cdd28e37af8df1e204fe5ea5ea7dd21d7ec4a0f237ce252b315"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8043c769b44e1cbedff262dc985563eb2757f0330b4336b347e46346876b92f5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3143cd64a56f93c4e533f11bfc714764e756de8259573f2744a77d3c0d98f0c6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6d63a4c70f75f5181e077c27f3b02b8f83d718c78fee5fff9338bad0f53905de"
+    sha256 cellar: :any_skip_relocation, catalina:      "6d63a4c70f75f5181e077c27f3b02b8f83d718c78fee5fff9338bad0f53905de"
+    sha256 cellar: :any_skip_relocation, mojave:        "6d63a4c70f75f5181e077c27f3b02b8f83d718c78fee5fff9338bad0f53905de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70e81407e6aac5aa36e16f5e99c275018ab1383c3833da8c92429f1f3fe337aa"
   end
 
   depends_on "go" => :build
