e33b65fb9f87ab2ab4dad320bf181b60a1b085db
Michael Cho <cho-m@tuta.io>
2021-08-06 16:16:44 -0700

    jp: fix build for Linux (#82387)

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -16,14 +16,22 @@ class Jp < Formula
 
   depends_on "go" => :build
 
+  # Fix build on ARM by adding a corresponding Makefile target
+  patch :DATA
+
   def install
     ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "auto"
     build_root = buildpath/"src/github.com/sgreben/jp"
     build_root.install Dir["*"]
     cd build_root do
-      system "make", "binaries/osx_x86_64/jp"
-      bin.install "binaries/osx_x86_64/jp"
+      arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+      os = "osx"
+      on_linux do
+        os = "linux"
+      end
+      system "make", "binaries/#{os}_#{arch}/jp"
+      bin.install "binaries/#{os}_#{arch}/jp"
     end
   end
 
@@ -31,3 +39,20 @@ class Jp < Formula
     pipe_output("#{bin}/jp -input csv -xy '[*][0,1]'", "0,0\n1,1\n", 0)
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index adc9d13..664b6af 100644
+--- a/Makefile
++++ b/Makefile
+@@ -90,3 +90,10 @@ release/$(APP)_$(VERSION)_linux_arm64.zip: binaries/linux_arm64/$(APP)
+ 
+ binaries/linux_arm64/$(APP): $(GOFILES)
+ 	GOOS=linux GOARCH=arm64 go build -ldflags "-X main.version=$(VERSION)" -o binaries/linux_arm64/$(APP) ./cmd/$(APP)
++
++release/$(APP)_$(VERSION)_osx_arm64.zip: binaries/osx_arm64/$(APP)
++	mkdir -p release
++	cd ./binaries/osx_arm64 && zip -r -D ../../release/$(APP)_$(VERSION)_osx_arm64.zip $(APP)
++
++binaries/osx_arm64/$(APP): $(GOFILES)
++	GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$(VERSION)" -o binaries/osx_arm64/$(APP) ./cmd/$(APP)
