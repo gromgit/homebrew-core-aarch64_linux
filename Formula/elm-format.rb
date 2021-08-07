b8a0844214b672f97650c1573ec9c9c0e0343f5e
Michael Cho <cho-m@tuta.io>
2021-08-07 11:08:55 -0700

    elm-format 0.8.5
    Closes #82910.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elm-format.rb
+++ Formula/elm-format.rb
@@ -2,10 +2,10 @@ class ElmFormat < Formula
   desc "Elm source code formatter, inspired by gofmt"
   homepage "https://github.com/avh4/elm-format"
   url "https://github.com/avh4/elm-format.git",
-      tag:      "0.8.4",
-      revision: "5bd4fbe591fe8b456160c180cb875ef60bc57890"
+      tag:      "0.8.5",
+      revision: "80f15d85ee71e1663c9b53903f2b5b2aa444a3be"
   license "BSD-3-Clause"
-  head "https://github.com/avh4/elm-format.git"
+  head "https://github.com/avh4/elm-format.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, big_sur:     "f7f6bb421efb3969733d33c7f6200af334bcd768d2128b14ba0280270b2fff30"
@@ -15,27 +15,28 @@ class ElmFormat < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@8.8" => :build
+  depends_on "haskell-stack" => :build
+  depends_on arch: :x86_64 # no ghc (OSX,AArch64) binary via `haskell-stack`
 
-  def build_elm_format_conf
-    <<~EOS
-      module Build_elm_format where
+  uses_from_macos "xz" => :build # for `haskell-stack` to unpack ghc
 
-      gitDescribe :: String
-      gitDescribe = "#{version}"
-    EOS
+  on_linux do
+    depends_on "gmp" # for `haskell-stack` to configure ghc
   end
 
   def install
-    defaults = buildpath/"generated/Build_elm_format.hs"
-    defaults.write(build_elm_format_conf)
+    # Currently, dependency constraints require an older `ghc` patch version than available
+    # in Homebrew. Try using Homebrew `ghc` on update. Optionally, consider adding `ghcup`
+    # as a lighter-weight alternative to `haskell-stack` for installing particular ghc version.
+    jobs = ENV.make_jobs
+    ENV.deparallelize { system "stack", "setup", "-j#{jobs}", "--stack-root", buildpath/".stack" }
+    ENV.prepend_path "PATH", Dir[buildpath/".stack/programs/*/ghc-*/bin"].first
+    system "cabal", "v2-update"
 
-    (buildpath/"elm-format").install Dir["*"]
-
-    cd "elm-format" do
-      system "cabal", "v2-update"
-      system "cabal", "v2-install", *std_cabal_v2_args
-    end
+    # Directly running `cabal v2-install` fails: Invalid file name in tar archive: "avh4-lib-0.0.0.1/../"
+    # Instead, we can use the upstream's build.sh script, which utilizes the Shake build system.
+    system "./build.sh", "--", "build"
+    bin.install "_build/elm-format"
   end
 
   test do
@@ -47,5 +48,7 @@ class ElmFormat < Formula
 
     system bin/"elm-format", "--elm-version=0.18", testpath/"Hello.elm", "--yes"
     system bin/"elm-format", "--elm-version=0.19", testpath/"Hello.elm", "--yes"
+
+    assert_match version.to_s, shell_output("#{bin}/elm-format --help")
   end
 end
