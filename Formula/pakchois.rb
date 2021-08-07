8c73836c15cda77aeb019324ed55a4262502cce9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-07 14:01:04 +0000

    pakchois: update 0.4 bottle.

--- Formula/pakchois.rb
+++ Formula/pakchois.rb
@@ -12,10 +12,11 @@ class Pakchois < Formula
 
   bottle do
     rebuild 3
-    sha256 cellar: :any, arm64_big_sur: "86e77a851ff2c0de31cf5e4f2587711b7b1bcc742fb878df1ce69fde836fd864"
-    sha256 cellar: :any, big_sur:       "fd91b09bb010ac37483a910b0431c6082903ee843a15f4cc767bde57ce0b7267"
-    sha256 cellar: :any, catalina:      "ca82f2950582bc54e46122eb71ff8e8acdc739772baf53ab2d545755f03303f8"
-    sha256 cellar: :any, mojave:        "cc98c7b706f27320ee7c673d906b4da22b402afe0d93b4c66f73a8cde86f7929"
+    sha256 cellar: :any,                 arm64_big_sur: "86e77a851ff2c0de31cf5e4f2587711b7b1bcc742fb878df1ce69fde836fd864"
+    sha256 cellar: :any,                 big_sur:       "fd91b09bb010ac37483a910b0431c6082903ee843a15f4cc767bde57ce0b7267"
+    sha256 cellar: :any,                 catalina:      "ca82f2950582bc54e46122eb71ff8e8acdc739772baf53ab2d545755f03303f8"
+    sha256 cellar: :any,                 mojave:        "cc98c7b706f27320ee7c673d906b4da22b402afe0d93b4c66f73a8cde86f7929"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9e96d8cef014042091db67065c8b02439cba4cfd381ca7651bc721ec120ad4a"
   end
 
   def install
