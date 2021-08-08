533ad9ec3eecd3ad4dfa154755270069631f1d32
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-08 04:20:34 +0000

    bgpstream: update 2.2.0 bottle.

--- Formula/bgpstream.rb
+++ Formula/bgpstream.rb
@@ -6,10 +6,11 @@ class Bgpstream < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5c28bee02acc7bc557119b71bc714a9a505aa91ef58d2e390c6d7f75753d0f25"
-    sha256 cellar: :any, big_sur:       "950968b0578b8d4131574c5fd985f56a1e10abd2d4aabdeeb408db2b323d6567"
-    sha256 cellar: :any, catalina:      "a684f4249475c2c0531fda4467adbba15b8e07c4e49b8ffc0366cae16fc76888"
-    sha256 cellar: :any, mojave:        "80262121246eb9431bedf0a64a12448ca1f2387caf17a8c1c7f20eaca6ca1069"
+    sha256 cellar: :any,                 arm64_big_sur: "5c28bee02acc7bc557119b71bc714a9a505aa91ef58d2e390c6d7f75753d0f25"
+    sha256 cellar: :any,                 big_sur:       "950968b0578b8d4131574c5fd985f56a1e10abd2d4aabdeeb408db2b323d6567"
+    sha256 cellar: :any,                 catalina:      "a684f4249475c2c0531fda4467adbba15b8e07c4e49b8ffc0366cae16fc76888"
+    sha256 cellar: :any,                 mojave:        "80262121246eb9431bedf0a64a12448ca1f2387caf17a8c1c7f20eaca6ca1069"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "da7354b8e52ae543d5e48aa9a7f2c1725f95c7581e8006c02845da8fcd3b78c5"
   end
 
   depends_on "librdkafka"
