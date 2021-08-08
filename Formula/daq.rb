7fbf816a92bd957e5b2d0eef06266334b246bdbd
Michael Cho <cho-m@tuta.io>
2021-08-07 20:06:01 -0700

    daq 3.0.4

--- Formula/daq.rb
+++ Formula/daq.rb
@@ -1,14 +1,11 @@
 class Daq < Formula
   desc "Network intrusion prevention and detection system"
   homepage "https://www.snort.org/"
-  url "https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz"
-  mirror "https://fossies.org/linux/misc/daq-2.0.7.tar.gz"
-  sha256 "bdc4e5a24d1ea492c39ee213a63c55466a2e8114b6a9abed609927ae13a7705e"
-
-  livecheck do
-    url "https://www.snort.org/downloads"
-    regex(/id=["']?snort_stable_version["']?.*?href=.*?daq[._-]v?(\d+(?:\.\d+)+)\.t/im)
-  end
+  url "https://github.com/snort3/libdaq/archive/v3.0.4.tar.gz"
+  mirror "https://fossies.org/linux/misc/libdaq-3.0.4.tar.gz"
+  sha256 "a376c7625d1442ddb7e3c75954c910cc9d64440e2f8f345981aa5fa6999ea206"
+  license "GPL-2.0-only"
+  head "https://github.com/snort3/libdaq.git"
 
   bottle do
     sha256 cellar: :any, big_sur:     "97b4ba11a541dc9720410792b363be14a190e5fbdb4f7ed473aef94f99dc0751"
@@ -20,55 +17,39 @@ class Daq < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
-  uses_from_macos "bison" => :build
-  uses_from_macos "flex" => :build
   uses_from_macos "libpcap"
 
-  # libpcap on >= 10.12 has pcap_lib_version() instead of pcap_version
-  # Reported 8 Oct 2017 to bugs AT snort DOT org
-  patch :p0, :DATA if MacOS.version >= :sierra
-
   def install
-    rm_f "./configure"
-    system "autoreconf", "-fiv"
-
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    ENV.deparallelize { system "make", "install" }
+    system "./bootstrap"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
   end
 
   test do
     (testpath/"test.c").write <<~EOS
-      #include <daq.h>
+      #include <assert.h>
       #include <stdio.h>
+      #include <daq.h>
+      #include <daq_module_api.h>
+
+      extern const DAQ_ModuleAPI_t pcap_daq_module_data;
+      static DAQ_Module_h static_modules[] = { &pcap_daq_module_data, NULL };
 
       int main()
       {
-        DAQ_Module_Info_t* list;
-        int size = daq_get_module_list(&list);
-        daq_free_module_list(list, size);
+        int rval = daq_load_static_modules(static_modules);
+        assert(rval == 1);
+        DAQ_Module_h module = daq_modules_first();
+        assert(module != NULL);
+        printf("[%s] - Type: 0x%x", daq_module_get_name(module), daq_module_get_type(module));
+        module = daq_modules_next();
+        assert(module == NULL);
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{lib}", "-ldaq", "-o", "test"
-    system "./test"
+    system ENV.cc, "test.c", "-L#{lib}", "-ldaq", "-ldaq_static_pcap", "-lpcap", "-lpthread", "-o", "test"
+    assert_match "[pcap] - Type: 0xb", shell_output("./test")
   end
 end
-
-__END__
---- ./m4/sf.m4
-+++ ./m4/sf.m4
-@@ -141,10 +141,9 @@
-     [[
-     #include <pcap.h>
-     #include <string.h>
--    extern char pcap_version[];
-     ]],
-     [[
--        if (strcmp(pcap_version, $1) < 0)
-+        if (strcmp(pcap_lib_version(), $1) < 0)
-             return 1;
-     ]])],
-     [daq_cv_libpcap_version_1x="yes"],
