227db6015412339d539a072f0cf6aed07c954183
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-08 07:05:45 +0000

    ifstat: update 1.1 bottle.

--- Formula/ifstat.rb
+++ Formula/ifstat.rb
@@ -19,6 +19,7 @@ class Ifstat < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "99eada14bfc555bd8f64d447bddd8a53c7325afed40446e5edbcfb5d7bdc7dd6"
     sha256 cellar: :any_skip_relocation, el_capitan:    "e3b3f843c9fba2770a49dd7abcdacc30aa6b5e57f06b5ed96f09d20ada58bd6f"
     sha256 cellar: :any_skip_relocation, yosemite:      "ecce408a9ae1a82c7b2457e5a5263ec760096e7e0e71cf0da1ce98523787bcbc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80bb1ac13a6750229f428e108b349523896fa1bbe1823300f07e366e58cfa1c9"
   end
 
   # Fixes 32/64 bit incompatibility for snow leopard
