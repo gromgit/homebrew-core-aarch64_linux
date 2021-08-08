a1ef4089277ef0c75218435a9df76e9a6f6538f0
Sam Ford <1584702+samford@users.noreply.github.com>
2021-08-08 19:04:14 -0400

    libpq: update homepage, livecheck

--- Formula/libpq.rb
+++ Formula/libpq.rb
@@ -1,13 +1,12 @@
 class Libpq < Formula
   desc "Postgres C API library"
-  homepage "https://www.postgresql.org/docs/12/libpq.html"
+  homepage "https://www.postgresql.org/docs/13/libpq.html"
   url "https://ftp.postgresql.org/pub/source/v13.3/postgresql-13.3.tar.bz2"
   sha256 "3cd9454fa8c7a6255b6743b767700925ead1b9ab0d7a0f9dcb1151010f8eb4a1"
   license "PostgreSQL"
 
   livecheck do
-    url "https://ftp.postgresql.org/pub/source/?C=M&O=A"
-    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
+    formula "postgresql"
   end
 
   bottle do
