6109adf210b90be83a93f70b98d59a08387ef260
Michael Cho <cho-m@tuta.io>
2021-08-07 22:42:45 -0700

    bcal: fix build for Linux (#82942)

--- Formula/bcal.rb
+++ Formula/bcal.rb
@@ -3,7 +3,7 @@ class Bcal < Formula
   homepage "https://github.com/jarun/bcal"
   url "https://github.com/jarun/bcal/archive/v2.2.tar.gz"
   sha256 "506d17d6df35fad636d3ced425afee5921cd2b21242099b78b369cfcb5716e23"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
@@ -16,6 +16,10 @@ class Bcal < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "68c94f34b56865694b7229d0211f8c8c97c3ec809a260cf0c8764524cb76b8fa"
     sha256 cellar: :any_skip_relocation, mojave:        "d6e4bac5e11d3eb1815d370fc3a8890906833104c8e3b8c9865376bf36f1e90e"
     sha256 cellar: :any_skip_relocation, high_sierra:   "4c32b10db17857b3e7c553e3accf24e0cc6fefc54361ede1790c6e46c72fbaf1"
+  end
+
+  on_linux do
+    depends_on "readline"
   end
 
   def install
