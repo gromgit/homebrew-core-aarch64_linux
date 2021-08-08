c12d5d77450e90487e2696b20294b2eedcfef3af
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-08 05:48:48 +0000

    bgpdump: update 1.6.2 bottle.

--- Formula/bgpdump.rb
+++ Formula/bgpdump.rb
@@ -11,11 +11,12 @@ class Bgpdump < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "547aa3e0a48f992ab4475b4f4b9203d46700fbde8588528382a7fc730157235c"
-    sha256 cellar: :any, big_sur:       "30a4765bc4c7decdb628df132d66bc675da867c5ed9631beac87dd99bce53713"
-    sha256 cellar: :any, catalina:      "f7c93574ccb3a6eaa05910009e26068f99f14082df78d3b2b0b84166488657e5"
-    sha256 cellar: :any, mojave:        "271ccd88799103255a673c6eafba9ec39320a8eb1a5a80bc8eef25ec508c31a6"
-    sha256 cellar: :any, high_sierra:   "441599b105e925cf6875f3e1d1a380cf94ec1069b214872173cd08736cd8671c"
+    sha256 cellar: :any,                 arm64_big_sur: "547aa3e0a48f992ab4475b4f4b9203d46700fbde8588528382a7fc730157235c"
+    sha256 cellar: :any,                 big_sur:       "30a4765bc4c7decdb628df132d66bc675da867c5ed9631beac87dd99bce53713"
+    sha256 cellar: :any,                 catalina:      "f7c93574ccb3a6eaa05910009e26068f99f14082df78d3b2b0b84166488657e5"
+    sha256 cellar: :any,                 mojave:        "271ccd88799103255a673c6eafba9ec39320a8eb1a5a80bc8eef25ec508c31a6"
+    sha256 cellar: :any,                 high_sierra:   "441599b105e925cf6875f3e1d1a380cf94ec1069b214872173cd08736cd8671c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab69320eab97e440488a15664f3defe5f2dd921bedfd30c23b80deefc5c954e0"
   end
 
   depends_on "autoconf" => :build
