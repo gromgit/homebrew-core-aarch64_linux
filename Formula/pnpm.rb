b55d4b7a1e758cccd6f54038a9c0806dc52b3227
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-08 22:18:59 +0800

    pnpm 6.12.1
    Closes #82954.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -3,8 +3,8 @@ class Pnpm < Formula
 
   desc "📦🚀 Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-6.11.5.tgz"
-  sha256 "8eb5380d696c7e5ff8d7f58e31d453fca02363991e6162fee872bd08dac5c10f"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-6.12.1.tgz"
+  sha256 "4037e78e0ec637e90c86ed5765cd4409c3deb0a6ca2a9864b202bb957e328ed9"
   license "MIT"
 
   livecheck do
