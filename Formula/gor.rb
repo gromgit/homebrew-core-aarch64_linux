7bc9cee2a586e03a74f94852e88e9e42dffd43fd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-09 06:01:37 +0000

    gor: update 1.3.0 bottle.

--- Formula/gor.rb
+++ Formula/gor.rb
@@ -17,6 +17,7 @@ class Gor < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "88b0c408516ecb9d26bc1e6e5c6cbacdb47be8f146bcd52aabb38b63be88b644"
     sha256 cellar: :any_skip_relocation, catalina:      "9cee6cb391872e35c5c8df506734c642612a4b5010bcaf06dee84fcc3af8b283"
     sha256 cellar: :any_skip_relocation, mojave:        "e781aff98642713d24adac0963b7b55cfc1b61e77bee065ef9cc455824789e25"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c24133864aad0cd66b269c13c6126699a98fd17b5a3a7e7842b6ef8fd6c8c7e8"
   end
 
   depends_on "go" => :build
