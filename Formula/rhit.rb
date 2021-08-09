cb13c37428e6f2edf16818dbf719068c4c43b5e0
Dawid Dziurla <dawidd0811@gmail.com>
2021-08-09 02:33:50 +0200

    rhit: fix build and test for Linux (#81227)

--- Formula/rhit.rb
+++ Formula/rhit.rb
@@ -15,8 +15,6 @@ class Rhit < Formula
 
   depends_on "rust" => :build
 
-  uses_from_macos "libiconv"
-
   resource "testdata" do
     url "https://raw.githubusercontent.com/Canop/rhit/c78d63b/test-data/access.log"
     sha256 "e9ec07d6c7267ec326aa3f28a02a8140215c2c769ac2fe51b6294152644165eb"
@@ -31,27 +29,32 @@ class Rhit < Formula
     require "io/console"
 
     resource("testdata").stage do
+      output = ""
       PTY.spawn("#{bin}/rhit --silent-load --length 0 --color no access.log") do |r, _w, _pid|
         r.winsize = [80, 130]
-        output = r.read.gsub(/\r?\n/, "\n")
-
-        assert_match <<~EOS, output
-          33,468 hits and 405M from 2021/01/22 to 2021/01/22
-          ┌──────────┬──────┬─────┬────────────────────┐
-          │   date   │ hits │bytes│0                33K│
-          ├──────────┼──────┼─────┼────────────────────┤
-          │2021/01/22│33,468│ 405M│████████████████████│
-          └──────────┴──────┴─────┴────────────────────┘
-        EOS
-        assert_match <<~EOS, output
-          HTTP status codes:
-          ┌─────┬─────┬────┬────┐
-          │ 2xx │ 3xx │4xx │5xx │
-          ├─────┼─────┼────┼────┤
-          │79.1%│14.9%│1.2%│4.8%│
-          └─────┴─────┴────┴────┘
-        EOS
+        begin
+          r.each_line { |line| output += line.gsub(/\r?\n/, "\n") }
+        rescue Errno::EIO
+          # GNU/Linux raises EIO when read is done on closed pty
+        end
       end
+
+      assert_match <<~EOS, output
+        33,468 hits and 405M from 2021/01/22 to 2021/01/22
+        ┌──────────┬──────┬─────┬────────────────────┐
+        │   date   │ hits │bytes│0                33K│
+        ├──────────┼──────┼─────┼────────────────────┤
+        │2021/01/22│33,468│ 405M│████████████████████│
+        └──────────┴──────┴─────┴────────────────────┘
+      EOS
+      assert_match <<~EOS, output
+        HTTP status codes:
+        ┌─────┬─────┬────┬────┐
+        │ 2xx │ 3xx │4xx │5xx │
+        ├─────┼─────┼────┼────┤
+        │79.1%│14.9%│1.2%│4.8%│
+        └─────┴─────┴────┴────┘
+      EOS
     end
   end
 end
