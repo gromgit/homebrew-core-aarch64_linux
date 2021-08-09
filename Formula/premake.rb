37e36e153695dcb2b0e761f991fd1b3bea2a6afe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-09 07:56:46 +0000

    premake: update 4.4-beta5 bottle.

--- Formula/premake.rb
+++ Formula/premake.rb
@@ -14,6 +14,7 @@ class Premake < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "cf128251e2798e7fd65919002b3adc627537c969dfaf62021ec6cd78fb7eeb12"
     sha256 cellar: :any_skip_relocation, mojave:        "b5fe3f9495148d2f374b048e72cfc3114be0195a9954d57c8c298fca568d2896"
     sha256 cellar: :any_skip_relocation, high_sierra:   "79e1f3b9c8ba609685ee343f2022aae2fb02cacecc84e44d817014fe7d3dabfc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d2ba7e57d942fb6296be5d0a7ede7d1e7f21775e9479d926960d8b038d07751"
   end
 
   # See: https://groups.google.com/g/premake-development/c/i1uA1Wk6zYM/m/kbp9q4Awu70J
