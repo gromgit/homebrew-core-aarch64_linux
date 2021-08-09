befff7112c5cbd88166ff4877a0947b92e401b97
MoonFruit <dkmoonfruit@gmail.com>
2021-08-09 10:11:41 +0800

    openliberty-webprofile8 21.0.0.8
    Closes #82994.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openliberty-webprofile8.rb
+++ Formula/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-06-29_1900/openliberty-webProfile8-21.0.0.7.zip"
-  sha256 "81ca14590f345457a9d5415acff3af59d7ae8d1b551c24256031f0b85f0da185"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-07-27_1323/openliberty-webProfile8-21.0.0.8.zip"
+  sha256 "bf023bace2ac7be3d348854964535c2ba699a3e9e10a6df688278ff11669ed2f"
   license "EPL-1.0"
 
   livecheck do
