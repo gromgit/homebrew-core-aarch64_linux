3aa489305199edd393dae26766519c0b0b145dbb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-09 03:07:12 +0000

    ghz-web: update 0.99.0 bottle.

--- Formula/ghz-web.rb
+++ Formula/ghz-web.rb
@@ -10,11 +10,11 @@ class GhzWeb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "95344a07ed48448946db436f7b3c231edc6724ba9a26431a3cc5f03e1ac30be2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f32c5695c0457b95e86bca9d4265f481b55211ebe3502d0e9948430aaa1f07d5"
-    sha256 cellar: :any_skip_relocation, catalina:      "84f46227bb34346420448acb9675d6fad145322f7d6d85b2d7805e200fef30de"
-    sha256 cellar: :any_skip_relocation, mojave:        "b219bdcf57302021f4c3d8d726ac26252ae6879689c4e7bd8c192db47044f8af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5c0cfce8ac127c3c2342ff17671b102d138be1de7fec159652b01989b26f558"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "65b251c68ed2e2b623139665ff9ff927943d775f0711edea521c8afc5d3a3478"
+    sha256 cellar: :any_skip_relocation, big_sur:       "49430e8edeb2e79fb259a57375a6d281654d59a7285f8b3f1058836bfb0e2cea"
+    sha256 cellar: :any_skip_relocation, catalina:      "403f019ffd0bdb4a6fa51bacbc0c68614bde02ed64bcfe898215a73b89450d23"
+    sha256 cellar: :any_skip_relocation, mojave:        "575666e2e170252b28bb342736462e6489f855ee6f9432ed2080c3d78d3fe722"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9371b033eb686c3d7bf2647c1af88f203fa6cd70cb60adcc303fe925c47eb4fa"
   end
 
   depends_on "go" => :build
