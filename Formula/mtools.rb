39362b7bc577011fb8502231723df592ac4a263c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-11 00:17:37 +0000

    mtools: update 4.0.35 bottle.

--- Formula/mtools.rb
+++ Formula/mtools.rb
@@ -7,11 +7,11 @@ class Mtools < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a4732fac3050d08cb7645158f7a695c6b1131863797f9522ef24816873e6ee6f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6b788824e4d9a861db566caae41dc0d7290232db79f434ca63c871bdd637913b"
-    sha256 cellar: :any_skip_relocation, catalina:      "bbf3f0335de4eb86bc042691965bd8a494cbb828701c760a620d2e3b1c945900"
-    sha256 cellar: :any_skip_relocation, mojave:        "ff61db59edf8286c99b002480db699fcdce81ae5f3570470fe59dbed24847ed4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "29c75e28e06f1ccac38e687e1b7885544e105bc3e7f674a87c92d241771b9ca3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "18cddaa9135b9523b2bf852c66cebf108a52c665faabb4eceb1dc8038f7b3265"
+    sha256 cellar: :any_skip_relocation, big_sur:       "5b327d09dd012e22085b77055ca713db9e2b0b054b58d2da0f446d977d4a7d17"
+    sha256 cellar: :any_skip_relocation, catalina:      "82f3ac919cf59793834c72e525d8b76f0249401a78c6bdc16eb70ea394f1b798"
+    sha256 cellar: :any_skip_relocation, mojave:        "eff0b4cc2fa5e3090634b2f25865593c240a654edeb0a0b21c628179ac8899d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56ebb016b728650f785c08f42c9ae191bcd163c34b5aaed9dae3352a394e5d97"
   end
 
   conflicts_with "multimarkdown", because: "both install `mmd` binaries"
