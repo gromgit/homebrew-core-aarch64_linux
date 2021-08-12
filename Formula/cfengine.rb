f46bd95eb986f8c9723ccb5de87e7e93f2941f2e
Sam Ford <1584702+samford@users.noreply.github.com>
2021-08-11 22:16:02 -0400

    cfengine: update livecheck

--- Formula/cfengine.rb
+++ Formula/cfengine.rb
@@ -6,8 +6,8 @@ class Cfengine < Formula
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later", "GPL-3.0-only", "LGPL-2.0-or-later"]
 
   livecheck do
-    url "https://cfengine.com/release-data/community/releases.json"
-    regex(/"version": ?"(\d+(?:\.\d+)+)"/i)
+    url "https://cfengine-package-repos.s3.amazonaws.com/release-data/community/releases.json"
+    regex(/["']version["']:\s*["'](\d+(?:\.\d+)+)["']/i)
   end
 
   bottle do
