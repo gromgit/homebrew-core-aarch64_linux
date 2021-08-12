f7e6f8cee012a371abaaa15ea43e8839e2e75e50
Rui Chen <rui@chenrui.dev>
2021-08-12 16:20:50 -0400

    rover 0.1.10
    Closes #83175.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rover.rb
+++ Formula/rover.rb
@@ -1,8 +1,8 @@
 class Rover < Formula
   desc "CLI for managing and maintaining data graphs with Apollo Studio"
   homepage "https://www.apollographql.com/docs/rover/"
-  url "https://github.com/apollographql/rover/archive/v0.1.8.tar.gz"
-  sha256 "3a559cef43acf49b5de84fe05d6a5fe12ec589a203ce944ebacd47530e0eff81"
+  url "https://github.com/apollographql/rover/archive/v0.1.10.tar.gz"
+  sha256 "3eac9afb1f65bde39011931b976e907f0f77598717201550d104bba41139d7e6"
   license "MIT"
   head "https://github.com/apollographql/rover.git", branch: "main"
 
