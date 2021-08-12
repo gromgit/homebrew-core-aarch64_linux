32c408356382a6f4cfff086ab93093b7c82a76d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-12 20:16:36 +0000

    serverless: update 2.54.0 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -8,10 +8,10 @@ class Serverless < Formula
   license "MIT"
 
   bottle do
-    sha256 arm64_big_sur: "3d761c90d102173b1d8cfddcedae0ad46b85ae98a171e0e8184143508f65129f"
-    sha256 big_sur:       "ab0e8af142131f3557bafc83f1ad242554cb123afd859c9e478162088516dcea"
-    sha256 catalina:      "38e3137e769673261ea677bb9384c5c0047efabd4411c1b9ccfec9ecfaaf216f"
-    sha256 mojave:        "cc58f7a60834115a9aa016aca4a85d689102aa5e850a1de16e6d2b2c64e3a024"
+    sha256 arm64_big_sur: "a53ce38b634137adc7d8e8880080128b8365b8a88646e61c0706dde205951359"
+    sha256 big_sur:       "89a4fbff843d4167873b820f20e8a3692bca47442a970119fc85b0eb6826bfe6"
+    sha256 catalina:      "f6e3d731a8d46f56498c4ae368f4c65f928d70325359e04046d8badf7ddf584b"
+    sha256 mojave:        "8d4458ce129683e8e5ef4e291c21a051004b912b691764576540c9743a585941"
   end
 
   depends_on "node"
