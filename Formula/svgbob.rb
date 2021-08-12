612a239a661619667e4bce6545e620e8118c8b2e
Rui Chen <rui@chenrui.dev>
2021-08-12 12:14:26 -0400

    svgbob 0.5.3
    Closes #83161.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/svgbob.rb
+++ Formula/svgbob.rb
@@ -1,8 +1,8 @@
 class Svgbob < Formula
   desc "Convert your ascii diagram scribbles into happy little SVG"
   homepage "https://ivanceras.github.io/svgbob-editor/"
-  url "https://github.com/ivanceras/svgbob/archive/0.5.2.tar.gz"
-  sha256 "86dfd9b8b0d548b75659257803618f493a126fdc7f2734cb8533ba240a1167e2"
+  url "https://github.com/ivanceras/svgbob/archive/0.5.3.tar.gz"
+  sha256 "f70ace62f08a64eaa912571d90e924d0271ae8d91c73314a2f277aa234a908a2"
   license "Apache-2.0"
   head "https://github.com/ivanceras/svgbob.git"
 
