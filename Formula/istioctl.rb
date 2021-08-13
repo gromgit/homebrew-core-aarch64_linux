1c7e3a8945eec67b8e93bff2e37a60b34b49e17b
Jonathan Meyers <jmymy@users.noreply.github.com>
2021-08-13 13:11:43 +0100

    istioctl 1.11.0
    * istioctl 1.11.0
    * istioctl: deparallelize
    
    Closes #83195.
    
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -2,8 +2,8 @@ class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://istio.io/"
   url "https://github.com/istio/istio.git",
-      tag:      "1.10.3",
-      revision: "61313778e0b785e401c696f5e92f47af069f96d0"
+      tag:      "1.11.0",
+      revision: "57d639a4fd19ee8c3559b9a4032f91e4d23c6f14"
   license "Apache-2.0"
   head "https://github.com/istio/istio.git"
 
@@ -18,6 +18,8 @@ class Istioctl < Formula
   depends_on "go-bindata" => :build
 
   def install
+    # make parallelization should be fixed in version > 1.11.0
+    ENV.deparallelize
     ENV["VERSION"] = version.to_s
     ENV["TAG"] = version.to_s
     ENV["ISTIO_VERSION"] = version.to_s
