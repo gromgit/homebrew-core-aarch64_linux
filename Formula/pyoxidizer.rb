7f268c0bb575d6e39f9dd93506089309fc45641d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-13 10:22:03 +0000

    pyoxidizer: update 0.17 bottle.

--- Formula/pyoxidizer.rb
+++ Formula/pyoxidizer.rb
@@ -15,6 +15,7 @@ class Pyoxidizer < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b43bf79a894e38941f7736be15b88a0bd9def143b3e8ae6a82eeb73f0bb7c9b4"
     sha256 cellar: :any_skip_relocation, big_sur:       "a3119a31eb9aaa47247c3838fc5a87917a30eb39b7244027a986951798ffeaeb"
     sha256 cellar: :any_skip_relocation, catalina:      "6e18726f0d0defa36648e0682695ff6a40a29c7db7974ab0552c8a7ae7bb0a45"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b86c3cfd894bd1687025d2883d516a37f9bbf125c83063cc524c8661f47ccd45"
   end
 
   depends_on "rust" => :build
