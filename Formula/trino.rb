20a0223e4ff2eb08a2bb5eb9d8305b6f40ea7185
Branch Vincent <branchevincent@gmail.com>
2021-08-12 21:17:40 -0400

    trino 360
    Closes #83185.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trino.rb
+++ Formula/trino.rb
@@ -1,69 +1,53 @@
 class Trino < Formula
   desc "Distributed SQL query engine for big data"
   homepage "https://trino.io"
-  url "https://search.maven.org/remotecontent?filepath=io/prestosql/presto-server/344/presto-server-344.tar.gz"
-  sha256 "9ae950f2901efd5cb1ca7d1bbd8a4cbb01d16dfe9c4fe702db2ee147ab841a8b"
+  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/360/trino-server-360.tar.gz"
+  sha256 "cd313867fd5461a26920ca6cb170413893853b0cc1acc4f454d4141a0790a50e"
   license "Apache-2.0"
 
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)*)/?["' >]}i)
+  end
+
+  depends_on "gnu-tar" => :build
+  depends_on arch: :x86_64
   depends_on "openjdk"
 
+  resource "trino-src" do
+    url "https://github.com/trinodb/trino/archive/360.tar.gz", using: :nounzip
+    sha256 "48fbe2f29f67b5c655d4de6eaec905c94f735c42df17e5509dc3c387e732aeb2"
+  end
+
   resource "trino-cli" do
-    url "https://search.maven.org/remotecontent?filepath=io/prestosql/presto-cli/344/presto-cli-344-executable.jar"
-    sha256 "92de6ce3afa29acea4e9527a1f2e20889008d72ee94cdd34b55c072fd805503e"
+    url "https://search.maven.org/remotecontent?filepath=io/trino/trino-cli/360/trino-cli-360-executable.jar"
+    sha256 "9203abd2ee3e36b7476aba23102b93e2adca7b030031fa3828e2320cb4c38be2"
   end
 
   def install
     libexec.install Dir["*"]
 
-    (libexec/"etc/node.properties").write <<~EOS
-      node.environment=dev
-      node.id=dev
-      node.data-dir=#{var}/trino/data
-    EOS
-
-    (libexec/"etc/jvm.config").write <<~EOS
-      -XX:+UseG1GC
-      -XX:G1HeapRegionSize=32M
-      -XX:+ExplicitGCInvokesConcurrent
-      -XX:+ExitOnOutOfMemoryError
-      -Djdk.attach.allowAttachSelf=true
-    EOS
-
-    (libexec/"etc/config.properties").write <<~EOS
-      coordinator=true
-      node-scheduler.include-coordinator=true
-      http-server.http.port=8080
-      query.max-memory=1GB
-      query.max-memory-per-node=1GB
-      discovery-server.enabled=true
-      discovery.uri=http://localhost:8080
-    EOS
-
-    (libexec/"etc/log.properties").write <<~EOS
-      io.prestosql=INFO
-    EOS
-
-    (libexec/"etc/catalog/jmx.properties").write <<~EOS
-      connector.name=jmx
-    EOS
+    # Manually untar, since macOS-bundled tar produces the error:
+    #   trino-360/plugin/trino-hive/src/test/resources/<truncated>.snappy.orc.crc: Failed to restore metadata
+    # Remove when https://github.com/trinodb/trino/issues/8877 is fixed
+    resource("trino-src").stage do |r|
+      ENV.prepend_path "PATH", Formula["gnu-tar"].opt_libexec/"gnubin"
+      system "tar", "-xzf", "trino-#{r.version}.tar.gz"
+      (libexec/"etc").install Dir["trino-#{r.version}/core/docker/default/etc/*"]
+      inreplace libexec/"etc/node.properties", "docker", "homebrew"
+      inreplace libexec/"etc/node.properties", "/data/trino", "#{var}/trino/data"
+    end
 
     (bin/"trino-server").write_env_script libexec/"bin/launcher", Language::Java.overridable_java_home_env
 
     resource("trino-cli").stage do
-      libexec.install "presto-cli-#{version}-executable.jar"
-      bin.write_jar_script libexec/"presto-cli-#{version}-executable.jar", "trino"
+      libexec.install "trino-cli-#{version}-executable.jar"
+      bin.write_jar_script libexec/"trino-cli-#{version}-executable.jar", "trino"
     end
   end
 
   def post_install
     (var/"trino/data").mkpath
-  end
-
-  def caveats
-    <<~EOS
-      Add connectors to #{opt_libexec}/etc/catalog/. See:
-      https://trino.io/docs/current/connector.html
-    EOS
   end
 
   service do
@@ -72,7 +56,21 @@ class Trino < Formula
   end
 
   test do
-    system bin/"trino-server", "run", "--help"
-    assert_match "Presto CLI #{version}", shell_output("#{bin}/trino --version").chomp
+    port = free_port
+    cp libexec/"etc/config.properties", testpath/"config.properties"
+    inreplace testpath/"config.properties", "8080", port.to_s
+    server = fork do
+      exec bin/"trino-server", "run", "--verbose",
+                                      "--data-dir", testpath,
+                                      "--config", testpath/"config.properties"
+    end
+    sleep 30
+
+    query = "SELECT state FROM system.runtime.nodes"
+    output = shell_output(bin/"trino --debug --server localhost:#{port} --execute '#{query}'")
+    assert_match "\"active\"", output
+  ensure
+    Process.kill("TERM", server)
+    Process.wait server
   end
 end
