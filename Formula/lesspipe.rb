811256eafb661a1dbdb4e05d256ca1e423bcbaf6
Caleb Xu <calebcenter@live.com>
2021-08-13 13:17:35 -0400

    lesspipe 1.88
    Closes #83215.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -1,9 +1,9 @@
 class Lesspipe < Formula
   desc "Input filter for the pager less"
   homepage "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html"
-  url "https://github.com/wofr06/lesspipe/archive/1.87.tar.gz"
-  sha256 "554095bc1d27514ca5480295f3f49a635bd65fd899489ab43b2a6d356258ad6c"
-  license "GPL-2.0"
+  url "https://github.com/wofr06/lesspipe/archive/1.88.tar.gz"
+  sha256 "442a178f40e0261144f03f1a31048e00d09c8d0fbd7107d99d183b54f10c7ac3"
+  license "GPL-2.0-only"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "d64f78aa39d7efe3b82a8ed8d73dab156d835471d9a828231e5543ef6b2ee63c"
@@ -32,6 +32,6 @@ class Lesspipe < Formula
     system "tar", "-cvzf", "homebrew.tar.gz", "file1.txt", "file2.txt"
 
     assert_predicate testpath/"homebrew.tar.gz", :exist?
-    assert_match "file2.txt", shell_output("tar tvzf homebrew.tar.gz | #{bin}/tarcolor")
+    assert_match "file2.txt", pipe_output(bin/"tarcolor", shell_output("tar -tvzf homebrew.tar.gz"))
   end
 end
