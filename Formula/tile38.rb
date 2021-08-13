3654ad8289f59445c06cce31af63e8f3bda46a66
Caleb Xu <calebcenter@live.com>
2021-08-13 15:53:44 -0400

    tile38 1.25.2
    Closes #83230.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tile38.rb
+++ Formula/tile38.rb
@@ -2,8 +2,8 @@ class Tile38 < Formula
   desc "In-memory geolocation data store, spatial index, and realtime geofence"
   homepage "https://tile38.com/"
   url "https://github.com/tidwall/tile38.git",
-      tag:      "1.25.1",
-      revision: "6e52e3a7eb49266b5c91d31debf44d2038892ebf"
+      tag:      "1.25.2",
+      revision: "3b77a24892365708bf9d766126af0e4f11c00d78"
   license "MIT"
   head "https://github.com/tidwall/tile38.git"
 
