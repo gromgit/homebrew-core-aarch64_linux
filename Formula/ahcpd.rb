831a07173e1a34f251ed7e711dc4eb173725e85a
Sam Ford <1584702+samford@users.noreply.github.com>
2021-08-13 20:36:48 -0400

    ahcpd: add livecheck

--- Formula/ahcpd.rb
+++ Formula/ahcpd.rb
@@ -5,6 +5,11 @@ class Ahcpd < Formula
   sha256 "a4622e817d2b2a9b878653f085585bd57f3838cc546cca6028d3b73ffcac0d52"
   license "MIT"
 
+  livecheck do
+    url "https://www.irif.fr/~jch/software/files/"
+    regex(/href=.*?ahcpd[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "49ef92eb18038f60e6419a5dfecd11be62f3b69cb4778c473050e5443e72ac06"
