2126f0a3c422d4d150eadc771da7dc96bf7d95f6
Rui Chen <rui@chenrui.dev>
2021-08-15 01:14:08 -0400

    rdfind 1.5.0
    * rdfind 1.5.0
    * rdfind: requires cxx11
    * rdfind: add license
    
    Closes #83318.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rdfind.rb
+++ Formula/rdfind.rb
@@ -1,9 +1,9 @@
 class Rdfind < Formula
   desc "Find duplicate files based on content (NOT file names)"
   homepage "https://rdfind.pauldreik.se/"
-  url "https://rdfind.pauldreik.se/rdfind-1.4.1.tar.gz"
-  sha256 "30c613ec26eba48b188d2520cfbe64244f3b1a541e60909ce9ed2efb381f5e8c"
-  revision 1
+  url "https://rdfind.pauldreik.se/rdfind-1.5.0.tar.gz"
+  sha256 "4150ed1256f7b12b928c65113c485761552b9496c433778aac3f9afc3e767080"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :homepage
@@ -22,6 +22,7 @@ class Rdfind < Formula
   depends_on "nettle"
 
   def install
+    ENV.cxx11
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
