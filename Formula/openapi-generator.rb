7814669b1e54819146f8836c11ff1be833b368c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-16 14:20:46 +0000

    openapi-generator: update 5.2.1 bottle.

--- Formula/openapi-generator.rb
+++ Formula/openapi-generator.rb
@@ -6,7 +6,7 @@ class OpenapiGenerator < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "66919ad4fbdba7a72fde1bbdd55fc64a047fd2710e161f02fe5ce4ce67b107cf"
+    sha256 cellar: :any_skip_relocation, all: "990cfa2b178ecdba850494c6aca3e5aad5e76303f33808d7fc6403c60b80262f"
   end
 
   head do
