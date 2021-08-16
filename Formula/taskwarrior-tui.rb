9cb5387cfef49c065d974ab303bf583e9971c9f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-16 01:26:17 +0000

    taskwarrior-tui: update 0.13.30 bottle.

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -12,11 +12,11 @@ class TaskwarriorTui < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "43dfabbc4d93c719825560da59aba929e33b6bedf8e1e3c99d9b4f502d408371"
-    sha256 cellar: :any_skip_relocation, big_sur:       "dbd737595be382b6e5e0369dd986b4d7ce084649a46712522f58f7471b3c4e98"
-    sha256 cellar: :any_skip_relocation, catalina:      "da8366e0156b9c38283b15a8721cc7c5387fcce435d8759de1c1425fe9a05669"
-    sha256 cellar: :any_skip_relocation, mojave:        "12e8871d117f4024c982f0c00d7d09083ac57ce325fb329c2d356ee0b9b8acbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e1baa8985ff235458478f76276b9ac65e3443cde6e9058efe6fbc657351467b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "11ddea87f689c9f16d84de8c1d69ad2544653603310530fa20e9260de30b6392"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ca1b33da5fb85b7286f2f016096793711d3a427346dfd7e3463b710cef2f9d92"
+    sha256 cellar: :any_skip_relocation, catalina:      "82290585c2173ae637bee99da71301ff482341cdbc5e19fed063502742c1c784"
+    sha256 cellar: :any_skip_relocation, mojave:        "cbdb40f09f1fc77fafcd2513ea2bb3a5bae37646610a2e8cab2c66e04fb37381"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bd5b0cd9625732ae8125b16f40afe707cc5cc682cafb3687b5e373b8b1e45a4"
   end
 
   depends_on "pandoc" => :build
