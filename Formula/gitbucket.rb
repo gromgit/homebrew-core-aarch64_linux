6b12c05a00f1d6ac7d2e9f2a2c21775e703707fb
Rui Chen <rui@chenrui.dev>
2021-08-16 00:56:04 -0400

    gitbucket 4.36.2
    Closes #83377.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gitbucket.rb
+++ Formula/gitbucket.rb
@@ -1,8 +1,8 @@
 class Gitbucket < Formula
   desc "Git platform powered by Scala offering"
   homepage "https://github.com/gitbucket/gitbucket"
-  url "https://github.com/gitbucket/gitbucket/releases/download/4.36.1/gitbucket.war"
-  sha256 "e836955d504ab5b347c29e76f46218b3d2390fee53b834d54c11f4c67c8558f5"
+  url "https://github.com/gitbucket/gitbucket/releases/download/4.36.2/gitbucket.war"
+  sha256 "d22089c0ba131c82c70c55bc9427b2ec095098815f96ca31f88edd7c2882d62d"
   license "Apache-2.0"
 
   bottle do
