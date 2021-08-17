adea58ffb2596795083145d47476401d4c2b0bf5
Leon Bohn <leon.bohn@rwth-aachen.de>
2021-08-17 21:48:18 +0200

    spot 2.9.8 (new formula)
    * Spot: 2.9.8 (new formula)
      The package is simply compiled according to the documentation
      on the website and no special flags or configuration arguments
      are used.
      The test that are run do the following
      - generate 20 random LTL formulas over the atomic propositions
      a b c d
      - transform them into various kinds of omega-automata and check
      that the produced automata are language equivalent
      This procedure ensures that the relevant tools have been compiled
      successfully and that their interaction works as intended.
    * Fix description, add make step and add livecheck
    * Moved livecheck block up and fixed description.
    * Added python3.9 as a dependency
    
    Closes #83450.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/spot.rb
@@ -0,0 +1,23 @@
+class Spot < Formula
+  desc "Platform for LTL and ω-automata manipulation"
+  homepage "https://spot.lrde.epita.fr/"
+  url "https://www.lrde.epita.fr/dload/spot/spot-2.9.8.tar.gz"
+  sha256 "b7f404bb90a335a5914384ecc3fc3a2021ff22c57ee97a40c07bb2ab40e20cf9"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://spot.lrde.epita.fr/install.html"
+    regex(/href=.*?spot[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "python@3.9" => :build
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"randltl -n20 a b c d | ltlcross 'ltl2tgba -H -D %f >%O' 'ltl2tgba -s %f >%O' 'ltl2tgba -DP %f >%O'"
+  end
+end
