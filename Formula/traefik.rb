bcd4b2c191fc522a88bb9e43fd9ff2e29ae83ebe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-17 05:02:35 +0000

    traefik 2.4.14
    Closes #83437.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/traefik.rb
+++ Formula/traefik.rb
@@ -1,8 +1,8 @@
 class Traefik < Formula
   desc "Modern reverse proxy"
   homepage "https://traefik.io/"
-  url "https://github.com/traefik/traefik/releases/download/v2.4.13/traefik-v2.4.13.src.tar.gz"
-  sha256 "b6ca4b74ead11c19bf9ab2e9ab6b08d05b6c80a5c63be0b2c943f57f9a1767e3"
+  url "https://github.com/traefik/traefik/releases/download/v2.4.14/traefik-v2.4.14.src.tar.gz"
+  sha256 "579ce84df70fac22cbb88800001925687e7ed52571b62d6b031d1e570438e689"
   license "MIT"
   head "https://github.com/traefik/traefik.git"
 
