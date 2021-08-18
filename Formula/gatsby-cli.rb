4c91cb5e3a739e3a51e48edfac82ac4447f34525
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-18 19:12:42 +0000

    gatsby-cli: update 3.12.0 bottle.

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -9,11 +9,11 @@ class GatsbyCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9839eab73887b164bffc495ebf08266ee627f820a9f90f9e3044a08551e49e96"
-    sha256 cellar: :any_skip_relocation, big_sur:       "664884e7472b88c135790fe6448fff49fa31d68ed3b1d9f9fc591f877d78a0e7"
-    sha256 cellar: :any_skip_relocation, catalina:      "664884e7472b88c135790fe6448fff49fa31d68ed3b1d9f9fc591f877d78a0e7"
-    sha256 cellar: :any_skip_relocation, mojave:        "664884e7472b88c135790fe6448fff49fa31d68ed3b1d9f9fc591f877d78a0e7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1581409b0619744f5836b358bbcd1ffe69d4a63f61a80863fe9d1122090ac129"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c5be9ad2cd5dfc253b7574ee765d5c4e546da1dcec9066161c8eb1fd1ca45da4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "73c3ec8aee12aa372e2cd7eb039cfcb27c297c03d2843e32c84ce9156912ff14"
+    sha256 cellar: :any_skip_relocation, catalina:      "a526cb71e4b058f5e71f31bb67314a0b30e068a1de5d46c8a9d99d01217470ca"
+    sha256 cellar: :any_skip_relocation, mojave:        "73c3ec8aee12aa372e2cd7eb039cfcb27c297c03d2843e32c84ce9156912ff14"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92b742ead2cb4edcc53624353f3f42c88545413e3bc639fc9bb4d19adeef86f3"
   end
 
   depends_on "node"
