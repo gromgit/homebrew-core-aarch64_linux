c1700179ceb4878d580dac84fa996fd41e898914
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-18 06:08:31 +0000

    traefik: update 2.5.0 bottle.

--- Formula/traefik.rb
+++ Formula/traefik.rb
@@ -7,11 +7,11 @@ class Traefik < Formula
   head "https://github.com/traefik/traefik.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "85226d89a43854f1812b786cd16057b9a895cead70599eec3f874cd7bca68632"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8553b217ad0a81ca75fa96349274305972ecdf968eb111cee2a1a9c660306466"
-    sha256 cellar: :any_skip_relocation, catalina:      "3936b6374da00b97e0004340c262b80fcabd6acf4513f2554f013d8076848546"
-    sha256 cellar: :any_skip_relocation, mojave:        "5328a56b3b38011233e698687c91fe510e0191143e6fa36be88dfaff9833cd60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "669b22ce5ce5b45fe5b2ecf91d9c7593643b9412e58364471a59b1182ea07cf1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4bff865bb9e5ede0842673406007370213a4d4c446d79e6505536d0a22cfff57"
+    sha256 cellar: :any_skip_relocation, big_sur:       "85d43c146efb229e1b18cff1ee7c84dfcac9149bcb2311f1b39c3e03e164b083"
+    sha256 cellar: :any_skip_relocation, catalina:      "d12e5f665f6b37d305c1a1e40ad6100dcb2a9308975579e1361e4066f121b3c0"
+    sha256 cellar: :any_skip_relocation, mojave:        "99ab4d67f75d153267d366508f474c851966a8eb2e90d47fbac8ff72260ffc66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7db79e92a2e9f9a2a2079339ffa0dbbfb45bae3845879092e743ba61e9b22663"
   end
 
   depends_on "go" => :build
