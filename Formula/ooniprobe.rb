5b4a091f617c3cb1e63c559f01a98cc0efc39f72
Rui Chen <rui@chenrui.dev>
2021-08-18 12:08:31 -0400

    ooniprobe 3.10.1
    Closes #83497.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ooniprobe.rb
+++ Formula/ooniprobe.rb
@@ -1,8 +1,8 @@
 class Ooniprobe < Formula
   desc "Network interference detection tool"
   homepage "https://ooni.org/"
-  url "https://github.com/ooni/probe-cli/archive/v3.10.0.tar.gz"
-  sha256 "0e8732c012b685d4533f608c3b0f554b89fa5834bd40a421c225789abcc47470"
+  url "https://github.com/ooni/probe-cli/archive/v3.10.1.tar.gz"
+  sha256 "2b81c14133f39ac91c4ea6761be7a27d768cd88989b52ae72376d1d7b69de322"
   license "BSD-3-Clause"
 
   livecheck do
