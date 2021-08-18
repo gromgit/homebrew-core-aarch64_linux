bdd253c2ddac93601a31d622ea4a8dbb9ef760fe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-18 11:04:16 +0000

    cruft: add 2.9.0 bottle.

--- Formula/cruft.rb
+++ Formula/cruft.rb
@@ -8,6 +8,14 @@ class Cruft < Formula
   license "MIT"
   head "https://github.com/cruft/cruft.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "361a6523d8d7f5d1fbac3f37bef521f7c9ebedf447565c611b15b5b384f97be8"
+    sha256 cellar: :any_skip_relocation, big_sur:       "11874704db77b579f93c0c7702836d9c2fdbac6ab1a8626af6abf0a66880103b"
+    sha256 cellar: :any_skip_relocation, catalina:      "14044cf86125f565a8a525f40e6c63bd0ddd1f9051c76b0f8cd4bb0a79e8fec3"
+    sha256 cellar: :any_skip_relocation, mojave:        "5ecddcc0e06e81bc3844129b41627981fc8ddfb6c03d2e5e98a8b26625dbf305"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9c1a2bf5ee260be975d8b64c7ec1b9cc89dc44a5422c4124ccd16524a24baf9"
+  end
+
   depends_on "poetry" => :build
   depends_on "python@3.9"
   depends_on "six"
