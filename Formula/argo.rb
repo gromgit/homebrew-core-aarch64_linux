1f30689d7e32f491f20170bf28604f1ed38fe95b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-18 21:11:50 +0000

    argo: update 3.1.7 bottle.

--- Formula/argo.rb
+++ Formula/argo.rb
@@ -7,11 +7,11 @@ class Argo < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "18fd9dae34ed23db2d3853fd3f97497b44cb07ae18bfb6d21a7305a7ab01a08a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bff2e001476bdbb67eac76dcbf70cbc75853cab14162b0c10e34c1a09ed8f336"
-    sha256 cellar: :any_skip_relocation, catalina:      "2d365dc50a7cfde7f756de6caba1f3a31f6863ac07ebd1ef12c43449cda682e7"
-    sha256 cellar: :any_skip_relocation, mojave:        "9212cc2a42f93a0b9b13f28cdc6a778b7ab97d174663a407906df1b4dc0d284f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84c96cba3cd8d0aaee1844a47c918319126c7fa3cfd90b4512146fb81ced0947"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6940caa433c4810661dad17c6031011dbea530487e71e45b4e2423f9f2aeedc1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0e5c4cc8245d883e498ddc17031c3583b15d059f8eec0ce657d6ae31163be1ba"
+    sha256 cellar: :any_skip_relocation, catalina:      "514aafddbb52e74c8452eed301922b87bcd92501da7c00ba53fa451500e03b33"
+    sha256 cellar: :any_skip_relocation, mojave:        "95f80d672a29bf978123660b6bad97710fe49dc735c411bca884164ab6765723"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "557e7bdf6ee7e30dbc6fdd0ff28961db3f6b6203244e7c5cb474c49db55774a0"
   end
 
   depends_on "go" => :build
