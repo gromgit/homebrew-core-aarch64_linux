f89f039fafd0c4418f07ca958ee22b30a312e2a7
Michka Popoff <michkapopoff@gmail.com>
2021-08-18 10:31:29 +0200

    flac: use brewed curl for url

--- Formula/flac.rb
+++ Formula/flac.rb
@@ -1,7 +1,7 @@
 class Flac < Formula
   desc "Free lossless audio codec"
   homepage "https://xiph.org/flac/"
-  url "https://downloads.xiph.org/releases/flac/flac-1.3.3.tar.xz"
+  url "https://downloads.xiph.org/releases/flac/flac-1.3.3.tar.xz", using: :homebrew_curl
   mirror "https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.3.tar.xz"
   sha256 "213e82bd716c9de6db2f98bcadbc4c24c7e2efe8c75939a1a84e28539c4e1748"
 
