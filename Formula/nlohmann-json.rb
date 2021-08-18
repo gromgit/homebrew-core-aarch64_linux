e62b56ce2c135e34cc38eb1523647db4e301436e
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-08-18 13:38:46 +0200

    nlohmann-json 3.10.0
    * updating the nlohmann-json formula
    * Defaulting to CMake
    * fixing the sha256
    * fixed a minor bug
    * bringing back the old test
    
    Closes #83479.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nlohmann-json.rb
+++ Formula/nlohmann-json.rb
@@ -1,10 +1,9 @@
 class NlohmannJson < Formula
   desc "JSON for modern C++"
   homepage "https://github.com/nlohmann/json"
-  url "https://github.com/nlohmann/json/archive/v3.9.1.tar.gz"
-  sha256 "4cf0df69731494668bdd6460ed8cb269b68de9c19ad8c27abc24cd72605b2d5b"
+  url "https://github.com/nlohmann/json/archive/v3.10.0.tar.gz"
+  sha256 "eb8b07806efa5f95b349766ccc7a8ec2348f3b2ee9975ad879259a371aea8084"
   license "MIT"
-  revision 1
   head "https://github.com/nlohmann/json.git", branch: "develop"
 
   bottle do
