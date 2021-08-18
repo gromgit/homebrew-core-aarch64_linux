eb151c57d8edfc6a802cf2716bf6af4abb796d42
nodenv machine <68817010+nodenv-bot@users.noreply.github.com>
2021-08-18 06:06:36 -0400

    node-build 4.9.51
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #83475.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.9.50.tar.gz"
-  sha256 "14f8727ea201533d047c0fc52e894f1aafbe15b796659d434124d2fa2ce29685"
+  url "https://github.com/nodenv/node-build/archive/v4.9.51.tar.gz"
+  sha256 "ed7c0dc4490d47cb0bb0d8c996324b82826176798a4bc3bc6cc77602e80ff49d"
   license "MIT"
   head "https://github.com/nodenv/node-build.git"
 
