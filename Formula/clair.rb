c99ee9d915af60d30947831153daf3d94aec4593
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-18 18:12:49 +0000

    clair: update 4.2.2 bottle.

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -11,10 +11,10 @@ class Clair < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "5870dd1e7d4cbf50e68224703683bc81c15ac1b76ff41319eaf8deb99e48198a"
-    sha256 cellar: :any_skip_relocation, catalina:     "376873953f5f3efd6b8a6fb62529133e9c89c53c2ec8a497a414d6ea60468479"
-    sha256 cellar: :any_skip_relocation, mojave:       "ca582213b6881cc257326eaa78e93f129b5cb903e139880fbba696ec8ef97445"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "45f1fa5ae209f8205ca5bdfd0a10389d5d241150bebff53c2a7514c94e66a82c"
+    sha256 cellar: :any_skip_relocation, big_sur:      "b5faebb8f68c655915947dba54810d42e190c6f5968daeebd2ddf4bbe0f6f578"
+    sha256 cellar: :any_skip_relocation, catalina:     "a99136e4561adbdd70cb68270035ae49ef38ced0ad76c75e215b4c27e12f7df3"
+    sha256 cellar: :any_skip_relocation, mojave:       "d6af1310160626469d81d9f14ee5c247bcb913f3c984210e444a2607a66d2bbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b60e0f16d3c41891a652aa8eb4878d203153cbc6502791666c058ab9f21cf281"
   end
 
   depends_on "go" => :build
