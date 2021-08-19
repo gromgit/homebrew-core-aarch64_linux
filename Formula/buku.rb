d653bb73ff251d04e571d0d58c351b8b777a843f
Nanda H Krishna <me@nandahkrishna.com>
2021-08-20 03:52:30 +0530

    buku: add `head` branch

--- Formula/buku.rb
+++ Formula/buku.rb
@@ -6,7 +6,7 @@ class Buku < Formula
   url "https://files.pythonhosted.org/packages/94/96/1d62b1346c07f8abc661fa499fef0acc7a110735e5b768b8899e58be8dc5/buku-4.6.tar.gz"
   sha256 "e598045dc6b41121f2b706355d41e771aca9d30df71880fdeaed6f2f670d8dd8"
   license "GPL-3.0-or-later"
-  head "https://github.com/jarun/buku.git"
+  head "https://github.com/jarun/buku.git", branch: "master"
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "df8722e84c15d946c4119c1adab73729e6e3f86d3e73daf56bdaf33a27739709"
