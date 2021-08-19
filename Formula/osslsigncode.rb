031897f7ad34bd7588897d6ca89683119d53148e
Caleb Xu <calebcenter@live.com>
2021-08-18 22:33:53 -0400

    osslsigncode 2.2
    Closes #83555.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osslsigncode.rb
+++ Formula/osslsigncode.rb
@@ -1,9 +1,9 @@
 class Osslsigncode < Formula
   desc "OpenSSL based Authenticode signing for PE/MSI/Java CAB files"
   homepage "https://github.com/mtrojnar/osslsigncode"
-  url "https://github.com/mtrojnar/osslsigncode/archive/2.1.tar.gz"
-  sha256 "1d142f4e0b9d490d6d7bc495dc57b8c322895b0e6ec474d04d5f6910d61e5476"
-  license "GPL-3.0"
+  url "https://github.com/mtrojnar/osslsigncode/archive/2.2.tar.gz"
+  sha256 "dd7d6867264d8967f354dd3933429afb806fb56b9a1e88c3a6f100ecee06d83e"
+  license "GPL-3.0-or-later"
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "8625c68db1963c4e359fd16862ccde8ae433889c441adb8892fc4cb0a63dc377"
@@ -18,19 +18,18 @@ class Osslsigncode < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "libgsf"
   depends_on "openssl@1.1"
 
   uses_from_macos "curl"
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--with-gsf", "--prefix=#{prefix}"
+    system "./bootstrap"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
   test do
     # Requires Windows PE executable as input, so we're just showing the version
-    assert_match "osslsigncode", shell_output("#{bin}/osslsigncode --version", 255)
+    assert_match "osslsigncode", shell_output("#{bin}/osslsigncode --version")
   end
 end
