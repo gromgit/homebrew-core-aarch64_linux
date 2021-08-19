8cfd972b1e2008fa7daaa56780b92a2ea3b1e2d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-19 15:21:18 +0000

    knot-resolver: update 5.4.1 bottle.

--- Formula/knot-resolver.rb
+++ Formula/knot-resolver.rb
@@ -12,11 +12,11 @@ class KnotResolver < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "d5ab757a24c78ef72e9e8e27888ed7b3e72973ca2b21cac2cfb8361882f332d6"
-    sha256 big_sur:       "3524936cbe69b7950b4509d296e4b93666f3755ff6af7c8b6f91209cf3753533"
-    sha256 catalina:      "a42f4cbacb1b36b47334394496957417d26dc61731fca130c0cb6eecdeb40742"
-    sha256 mojave:        "0a335e7cde3394155ac5a172dd881974cd0ea53c02e7c92766af315e1463d176"
-    sha256 x86_64_linux:  "fd8eee3900fbe46a70bf0db719b697709479406d275837dd4e7b420ef145188c"
+    sha256 arm64_big_sur: "c4cd7cccb96bd54ceb76b8f04c95269fc639474c85c917eb03206eaf6d8aee34"
+    sha256 big_sur:       "3ee0b70bb08e528285d2c1c211a0e933324b1f742c8499b42f3b1f17f141c425"
+    sha256 catalina:      "550aa7baeabf7ab4ad234cd8ec60112e56c43b9023cc66b9b456e1a9c2a93491"
+    sha256 mojave:        "78a20463807a612d481736d6e5dc8f9dacf6cbc6df96006bd1fd022efa9088b6"
+    sha256 x86_64_linux:  "0ba3e84407b27a59138f60f1e08df6ffc830e0fb87247ad156c991c5a5478e3f"
   end
 
   depends_on "meson" => :build
