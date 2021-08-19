99becffd9cd90a310be11d9b1764fc7c23287de9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-19 01:59:48 +0000

    hyperscan: update 5.4.0 bottle.

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -6,9 +6,10 @@ class Hyperscan < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "1e75b4699ac1040d24cbe81ddae60149be7179e09f450840bdcbe5fd0e4582dc"
-    sha256 cellar: :any, catalina: "2c5afe9775aad01d1bfb577cb80218bdf241c48d5b567ad85fe6bba68241c8d3"
-    sha256 cellar: :any, mojave:   "0564db4adcb7022d1691f482d12fdf3a2c0ea71079a749c90f2233340aebb98e"
+    sha256 cellar: :any,                 big_sur:      "1e75b4699ac1040d24cbe81ddae60149be7179e09f450840bdcbe5fd0e4582dc"
+    sha256 cellar: :any,                 catalina:     "2c5afe9775aad01d1bfb577cb80218bdf241c48d5b567ad85fe6bba68241c8d3"
+    sha256 cellar: :any,                 mojave:       "0564db4adcb7022d1691f482d12fdf3a2c0ea71079a749c90f2233340aebb98e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8e91525e2c275594bf2394e5664be60d6199784c9c2ab3b273a8531c38c2acc1"
   end
 
   depends_on "boost" => :build
