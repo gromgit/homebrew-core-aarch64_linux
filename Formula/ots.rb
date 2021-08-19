56a1f33edff63f3dc868e85c33e935770db558fd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-19 06:19:14 +0000

    ots: add 0.1.0 bottle.

--- Formula/ots.rb
+++ Formula/ots.rb
@@ -5,6 +5,14 @@ class Ots < Formula
   sha256 "06bfa9ba6e4f924adefa441875c890d7190c1aca7315e54c4fb39a4010692a09"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bf2a5321f6c45a4c883c2667479ef8950b271276b3b0c9936d0d0f93ecfd4035"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a26787dfccc678b5bd76263e6ab49ef9dc48ad0637e2fba9c95e7c6f5371fc8e"
+    sha256 cellar: :any_skip_relocation, catalina:      "2a696ca741d8de5712dac1b06bfc6199cf2e1abf686866f6880d6c91f1d8f047"
+    sha256 cellar: :any_skip_relocation, mojave:        "52bce111d1c7726ef06be50301838bf6926397d99fc39ac69d0242c32d972821"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ab87ee0c5b202bc18eb3fff61c040816181206c0d57128a14688801754d5c97"
+  end
+
   depends_on "go" => :build
 
   def install
