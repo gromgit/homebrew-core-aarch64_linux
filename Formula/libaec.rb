1a38480dcbe6739158f4c0a5807686086ea8b814
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-08-19 11:52:31 +0100

    libaec 1.0.5 (new formula)
    * libaec 1.0.5 (new formula)
    * fix heredoc 2 space indentation
    
    Closes #83572.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/libaec.rb
@@ -0,0 +1,50 @@
+class Libaec < Formula
+  desc "Adaptive Entropy Coding implementing Golomb-Rice algorithm"
+  homepage "https://gitlab.dkrz.de/k202009/libaec"
+  url "https://gitlab.dkrz.de/k202009/libaec/-/archive/v1.0.5/libaec-v1.0.5.tar.bz2"
+  sha256 "49cc260c3321a358a8d854444584eec58382c26cda3c4bcd156b4ba396075ae7"
+  license "BSD-2-Clause"
+  head "https://gitlab.dkrz.de/k202009/libaec.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+      system "make", "test"
+    end
+  end
+
+  test do
+    system "#{bin}/aec", "-v"
+    (testpath/"test.cpp").write <<~EOS
+      #include <cassert>
+      #include <cstddef>
+      #include <libaec.h>
+      int main() {
+        unsigned char * data = (unsigned char *) calloc(1024, sizeof(unsigned char));
+        unsigned char * compressed = (unsigned char *) calloc(1024, sizeof(unsigned char));
+        for(int i = 0; i < 1024; i++) { data[i] = (unsigned char)(i); }
+        struct aec_stream strm;
+        strm.bits_per_sample = 16;
+        strm.block_size      = 64;
+        strm.rsi             = 129;
+        strm.flags           = AEC_DATA_PREPROCESS | AEC_DATA_MSB;
+        strm.next_in         = data;
+        strm.avail_in        = 1024;
+        strm.next_out        = compressed;
+        strm.avail_out       = 1024;
+        assert(aec_encode_init(&strm) == 0);
+        assert(aec_encode(&strm, AEC_FLUSH) == 0);
+        assert(strm.total_out > 0);
+        assert(aec_encode_end(&strm) == 0);
+        free(data);
+        free(compressed);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-L#{lib}", "-laec", "-o", "test"
+    system "./test"
+  end
+end
