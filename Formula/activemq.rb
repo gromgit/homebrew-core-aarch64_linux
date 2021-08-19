f60fefb565200178ab7b0de628c165b5aeab383c
Caleb Xu <calebcenter@live.com>
2021-08-19 08:49:04 -0400

    activemq 5.16.3
    Closes #83575.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/activemq.rb
+++ Formula/activemq.rb
@@ -1,9 +1,9 @@
 class Activemq < Formula
   desc "Apache ActiveMQ: powerful open source messaging server"
   homepage "https://activemq.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=activemq/5.16.2/apache-activemq-5.16.2-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/activemq/5.16.2/apache-activemq-5.16.2-bin.tar.gz"
-  sha256 "212feca1ee4bc750befd45a735cbfef00c0c9aee451ef5116e991330c1ab105b"
+  url "https://www.apache.org/dyn/closer.lua?path=activemq/5.16.3/apache-activemq-5.16.3-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/activemq/5.16.3/apache-activemq-5.16.3-bin.tar.gz"
+  sha256 "1846da2985ec64253ecc41a54f1477731eb4750fe840a9dd9fdfee88e5c94252"
   license "Apache-2.0"
 
   bottle do
@@ -15,8 +15,18 @@ class Activemq < Formula
   def install
     on_macos do
       rm_rf Dir["bin/linux-x86-*"]
+
+      # Discard universal binaries without usable slices
+      rm_f "bin/macosx/libwrapper.jnilib"
+      rm_f "bin/macosx/wrapper" if Hardware::CPU.arm?
     end
+
+    on_linux do
+      rm_rf "bin/macosx"
+    end
+
     libexec.install Dir["*"]
+    deuniversalize_machos
     (bin/"activemq").write_env_script libexec/"bin/activemq", JAVA_HOME: Formula["openjdk"].opt_prefix
   end
 
