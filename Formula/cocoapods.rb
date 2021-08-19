0f81ec1482f5b620523970582baa5f517a7c4393
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-19 09:45:24 +0000

    cocoapods: update 1.10.2 bottle.

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -6,10 +6,11 @@ class Cocoapods < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6183dd8e0cf8e8c996465d9ef4faa630d670ccda38a9e105f9edc5e06aa9f4b5"
-    sha256               big_sur:       "700b2ec47e11d0b084a1305193ed4bc7cd09ce047939d778f2a6c0f3651bcd03"
-    sha256               catalina:      "1cbfa6ef8dc560f0758715638ad719884e222e646989ad39bd3d9a04364d1de2"
-    sha256 cellar: :any, mojave:        "4256c1df22de7e9ace48f888232e8cf469bff6e7ae2a0c9adb48fc65e71f1b43"
+    sha256 cellar: :any,                 arm64_big_sur: "6183dd8e0cf8e8c996465d9ef4faa630d670ccda38a9e105f9edc5e06aa9f4b5"
+    sha256                               big_sur:       "700b2ec47e11d0b084a1305193ed4bc7cd09ce047939d778f2a6c0f3651bcd03"
+    sha256                               catalina:      "1cbfa6ef8dc560f0758715638ad719884e222e646989ad39bd3d9a04364d1de2"
+    sha256 cellar: :any,                 mojave:        "4256c1df22de7e9ace48f888232e8cf469bff6e7ae2a0c9adb48fc65e71f1b43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eeb6497e79b5fbe5e85b601498173a231414b796da50a050a29724e3c3226bd7"
   end
 
   depends_on "pkg-config" => :build
