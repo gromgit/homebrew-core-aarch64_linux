27251e29056962430843d5f9769cb5df8876ed57
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-08-19 11:07:40 +0200

    librespot 0.2.0 (new formula)
    * librespot 0.2.0 (new formula)
    * Apply suggestions from code review
    * use tarball
    * add proper test
    * specifically use stderr
    * Update librespot.rb
    * Update librespot.rb
    * Update librespot.rb
    * Update librespot.rb
    
    Closes #83567.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/librespot.rb
@@ -0,0 +1,39 @@
+class Librespot < Formula
+  desc "Open Source Spotify client library"
+  homepage "https://github.com/librespot-org/librespot"
+  url "https://github.com/librespot-org/librespot/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "cd6e7ae63583584598dead53b5e8e4077ab37df6e3b59c2a7416459b9a649fc3"
+  license "MIT"
+  head "https://github.com/librespot-org/librespot.git", branch: "dev"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "alsa-lib"
+    depends_on "avahi"
+  end
+
+  def install
+    on_macos do
+      ENV["COREAUDIO_SDK_PATH"] = MacOS.sdk_path.to_s
+    end
+    system "cargo", "install", "--no-default-features", "--features", "rodio-backend,with-dns-sd", *std_cargo_args
+  end
+
+  test do
+    require "open3"
+    require "timeout"
+
+    Open3.popen3({ "RUST_LOG" => "DEBUG" }, "#{bin}/librespot", "-v") do |_, _, stderr, wait_thr|
+      Timeout.timeout(5) do
+        stderr.each do |line|
+          refute_match "ERROR", line
+          break if line.include? "Zeroconf server listening"
+        end
+      end
+    ensure
+      Process.kill("INT", wait_thr.pid)
+    end
+  end
+end
