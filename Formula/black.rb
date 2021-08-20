d2ef70d0e536978de05c61744784b334c1931722
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-20 04:13:12 +0000

    black: update 21.7b0 bottle.

--- Formula/black.rb
+++ Formula/black.rb
@@ -17,6 +17,7 @@ class Black < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "cab87ba9fcc15b5d43d94a10f0618b31906b34a1e302bcbbda6cc00dd7ac4a13"
     sha256 cellar: :any_skip_relocation, catalina:      "0e753b1b5e2d4876629911a2d8807da5bb9b4be20fe585853036ff2d46286345"
     sha256 cellar: :any_skip_relocation, mojave:        "372d2251db51bb0245fa86ddd1b5da89963a3af1ba2392b732df5555a5b4c96e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f90022601c71213effaed7625ef4c599a85ef155c462d7c1827e92e4ab098fa"
   end
 
   depends_on "python@3.9"
