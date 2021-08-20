a2357ae0a30a4526c0bf61db80fb66398600a1a0
Nanda H Krishna <me@nandahkrishna.com>
2021-08-20 08:08:12 +0530

    csshx: add `head` branch

--- Formula/csshx.rb
+++ Formula/csshx.rb
@@ -6,7 +6,7 @@ class Csshx < Formula
   sha256 "eaa9e52727c8b28dedc87398ed33ffa2340d6d0f3ea9d261749c715cb7a0e9c8"
   # same terms as Perl
   license "GPL-1.0"
-  head "https://github.com/brockgr/csshx.git"
+  head "https://github.com/brockgr/csshx.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "d073f3e8d11762fe8b72a3110d88a031b8f19c671f400827b68bba2adfb8b4ae"
