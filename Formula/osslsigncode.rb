7bdbc4d30ab8d39f8a6c8b1d26bcd4bcd6d37a56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-20 17:10:56 +0000

    osslsigncode: update 2.2 bottle.

--- Formula/osslsigncode.rb
+++ Formula/osslsigncode.rb
@@ -6,12 +6,11 @@ class Osslsigncode < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "8625c68db1963c4e359fd16862ccde8ae433889c441adb8892fc4cb0a63dc377"
-    sha256 cellar: :any,                 big_sur:       "80c746077ac49b3e448559fe14b4802b3c0f3b4b54d720969a164d7f679afc5e"
-    sha256 cellar: :any,                 catalina:      "964162e471801ec6335e1cb88fa7d71145a09acd7507f71d049af1edc6375f9e"
-    sha256 cellar: :any,                 mojave:        "6ce5ae481bea9b92e4baaf795dfbdaf6cb29d574189978012f641857ffe39113"
-    sha256 cellar: :any,                 high_sierra:   "2a70933b296047d0042df4e1c1361cab8d588ff70c36ef44f63ac01105ce32f6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6d0177bd1af3c5dc3ac4a16a9a5792951e6f2ba1967c868b11927f137bea5f4"
+    sha256 cellar: :any,                 arm64_big_sur: "d6311e4481c2b8b65c911bf24c42851557cdbd75801f92cf279b304953a2752a"
+    sha256 cellar: :any,                 big_sur:       "6e61b636105463514c06a1a85b69fc5c6a6820ca37c52bd77d9183ef43c18048"
+    sha256 cellar: :any,                 catalina:      "c9f8e8e0e6f50ee007996a837d369fd86a420d3b33ad96f93147e33b2c9e8b2a"
+    sha256 cellar: :any,                 mojave:        "673f0150f56426a6218d02743c1b3ed3e564a0bb454d335eda0f1795dd513dd1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68479dd084bd4a06ccba9a64756904b6ffea34d49acb93384371778b316ae07e"
   end
 
   depends_on "autoconf" => :build
