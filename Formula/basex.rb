2d28f10860a1a40f76ba5d249825145aeafb2e06
Alexander Holupirek <alex@holupirek.de>
2021-08-20 12:20:12 +0200

    basex 9.6
    Closes #83632.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/basex.rb
+++ Formula/basex.rb
@@ -1,9 +1,9 @@
 class Basex < Formula
   desc "Light-weight XML database and XPath/XQuery processor"
   homepage "https://basex.org"
-  url "https://files.basex.org/releases/9.5.2/BaseX952.zip"
-  version "9.5.2"
-  sha256 "82c61d94bf88d140c499af8d78be544b37fc73831e6744624d4ed4eb836281e6"
+  url "https://files.basex.org/releases/9.6/BaseX96.zip"
+  version "9.6"
+  sha256 "a0590b65fe885c7753505e5d39463cd4e1240fedccc5e808bab6780fbcb1d23c"
   license "BSD-3-Clause"
 
   livecheck do
