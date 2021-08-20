eab188cce18e37773fb414fed0bf3384f13defe2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-20 18:09:03 +0000

    drone-cli: update 1.3.1 bottle.

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -7,11 +7,11 @@ class DroneCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d78b6434793f9d71b197330f7bd1e021d9a6c348700d2d518daa8a348288ab6e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ce7174f76b2f79ea085e3297ef10f01ae13d057c84133079da16e72ec91563c3"
-    sha256 cellar: :any_skip_relocation, catalina:      "a3bb9a627f909962bc99c5248b9b211edeb848bb758f1adbdf8b65716f6f6dba"
-    sha256 cellar: :any_skip_relocation, mojave:        "0a5e2ff711da8547c2466675fe42104015b6032702db19ae4d0688646b72dec0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56442726986cc75e9e1465eca0602bedbc87f5a8d65408ed4f8fa6987499dbf6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "26568fe6920ab5e98485177180ec5654c2853d5515752f881d2d1c1828938c42"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3522d410b7cb07da627aec2fc3397dc29b581499301d5b9e762a9d060978fe8e"
+    sha256 cellar: :any_skip_relocation, catalina:      "9b2af8110063a073d3cd0e1b341ae2921cf98c96ecfb0ef0a2bcabd03e35c4cd"
+    sha256 cellar: :any_skip_relocation, mojave:        "319fdf558cd1e893674f460187965eca5fc0a9d88eae085588605e1e55d0b55e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e9ed9c52b32fc33067c6dd6d54162fd252effe714199a90e08bb5fb42fdddfa"
   end
 
   depends_on "go" => :build
