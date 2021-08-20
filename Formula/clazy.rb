ce80c902b3e3c2e3fbda11a202f88948db6c2950
Nanda H Krishna <me@nandahkrishna.com>
2021-08-20 08:07:57 +0530

    clazy: add `head` branch

--- Formula/clazy.rb
+++ Formula/clazy.rb
@@ -4,7 +4,7 @@ class Clazy < Formula
   url "https://download.kde.org/stable/clazy/1.10/src/clazy-1.10.tar.xz"
   sha256 "4ce6d55ffcddacdb005d847e0c329ade88a01e8e4f7590ffd2a9da367c1ba39d"
   license "LGPL-2.0-or-later"
-  head "https://invent.kde.org/sdk/clazy.git"
+  head "https://invent.kde.org/sdk/clazy.git", branch: "master"
 
   livecheck do
     url :head
