4b9b9fe896d49f25fde958a97f9b2728f3a4ee64
Nanda H Krishna <me@nandahkrishna.com>
2021-08-20 08:07:58 +0530

    cliclick: add `head` branch

--- Formula/cliclick.rb
+++ Formula/cliclick.rb
@@ -4,7 +4,7 @@ class Cliclick < Formula
   url "https://github.com/BlueM/cliclick/archive/5.0.1.tar.gz"
   sha256 "798fb8b26f6a42b5002ca58e018b91f7677162c4f035b38aee8d905829db64a7"
   license "BSD-3-Clause"
-  head "https://github.com/BlueM/cliclick.git"
+  head "https://github.com/BlueM/cliclick.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "38bed03738e5b08fd7956b06168522bf50649b89d77f9db7f11ede0556c769f7"
