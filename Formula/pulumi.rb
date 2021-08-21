8ba50d82d72fdb5ae6d1ce69144d0e9dfeeb27a0
Branch Vincent <branchevincent@gmail.com>
2021-08-21 10:56:54 -0400

    pulumi: support go 1.17
    Closes #83706.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -5,7 +5,7 @@ class Pulumi < Formula
       tag:      "v3.10.3",
       revision: "d8ebde1590088a21216329b429a83f7b39b512ef"
   license "Apache-2.0"
-  head "https://github.com/pulumi/pulumi.git"
+  head "https://github.com/pulumi/pulumi.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "840de4549e991543c3da2ccf90f53c23d1141b843f79889df9095a9ecb3369bc"
@@ -16,6 +16,12 @@ class Pulumi < Formula
   end
 
   depends_on "go" => :build
+
+  # Support go 1.17, remove after next release
+  patch do
+    url "https://github.com/pulumi/pulumi/commit/da2bd2ab043f2588b3a136846da1b97505326f25.patch?full_index=1"
+    sha256 "b0db2dab2996615cef4ca338232a4acca02f4ca4d8fd8322b6da51be697a9ef5"
+  end
 
   def install
     cd "./sdk" do
