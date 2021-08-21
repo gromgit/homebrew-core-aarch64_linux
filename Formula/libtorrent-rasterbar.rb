4f9f29b8697fc47b86ee74f433ef4c3a74aaf955
James Morris <jamesmorris2@gmail.com>
2021-08-21 11:20:40 -0400

    libtorrent-rasterbar 2.0.4
    Closes #83716.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -1,14 +1,14 @@
 class LibtorrentRasterbar < Formula
   desc "C++ bittorrent library with Python bindings"
   homepage "https://www.libtorrent.org/"
-  url "https://github.com/arvidn/libtorrent/releases/download/libtorrent-1.2.10/libtorrent-rasterbar-1.2.10.tar.gz"
-  sha256 "d0dd30bdc3926587c4241f4068d8e39628a6c1f9f6cf53195f0e9bc90017befb"
+  url "https://github.com/arvidn/libtorrent/releases/download/v2.0.4/libtorrent-rasterbar-2.0.4.tar.gz"
+  sha256 "55bcce16c4b85b8cccd20e7ff4a9fde92db66333a25d6504a83c0bb0a5f7f529"
   license "BSD-3-Clause"
-  revision 1
+  head "https://github.com/arvidn/libtorrent.git", branch: "RC_2_0"
 
   livecheck do
     url :stable
-    regex(/^libtorrent[._-]v?(\d+(?:[._]\d+)+)$/i)
+    regex(/^v?(\d+(?:[._]\d+)+)$/i)
   end
 
   bottle do
@@ -20,14 +20,7 @@ class LibtorrentRasterbar < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1827bbb007b0675d1a1848d608c8b25efaab6674938309e6c89b78a33bfb1708"
   end
 
-  head do
-    url "https://github.com/arvidn/libtorrent.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
-  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
   depends_on "boost"
   depends_on "boost-python3"
   depends_on "openssl@1.1"
@@ -36,30 +29,20 @@ class LibtorrentRasterbar < Formula
   conflicts_with "libtorrent-rakshasa", because: "they both use the same libname"
 
   def install
-    pyver = Language::Python.major_minor_version(Formula["python@3.9"].bin/"python3").to_s.delete(".")
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --prefix=#{prefix}
-      --enable-encryption
-      --enable-python-binding
-      --with-boost=#{Formula["boost"].opt_prefix}
-      --with-boost-python=boost_python#{pyver}-mt
-      PYTHON=python3
-      PYTHON_EXTRA_LIBS=#{`#{Formula["python@3.9"].opt_bin}/python3-config --libs --embed`.chomp}
-      PYTHON_EXTRA_LDFLAGS=#{`#{Formula["python@3.9"].opt_bin}/python3-config --ldflags`.chomp}
+    args = %w[
+      -DCMAKE_CXX_STANDARD=14
+      -Dencryption=ON
+      -Dpython-bindings=ON
+      -Dpython-egg-info=ON
     ]
+    args += std_cmake_args
 
-    if build.head?
-      system "./bootstrap.sh", *args
-    else
-      system "./configure", *args
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make"
+      system "make", "install"
     end
 
-    system "make", "install"
-
-    rm Dir["examples/Makefile*"]
     libexec.install "examples"
   end
 
@@ -84,7 +67,7 @@ class LibtorrentRasterbar < Formula
     end
 
     system ENV.cxx, libexec/"examples/make_torrent.cpp",
-                    "-std=c++11", *args, "-o", "test"
+                    "-std=c++14", *args, "-o", "test"
     system "./test", test_fixtures("test.mp3"), "-o", "test.torrent"
     assert_predicate testpath/"test.torrent", :exist?
   end
