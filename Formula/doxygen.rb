7ac176a4cb5b8c8aa55573c1e68f0aa2d6e6930b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-21 09:08:04 +0000

    doxygen: update 1.9.2 bottle.

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -8,11 +8,11 @@ class Doxygen < Formula
   head "https://github.com/doxygen/doxygen.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2b5b2de93ac8703785860bfaf3e14f3268a07dc29bac9def13172785bcac7c5f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b8e1ea1bb601d8cc7cd7cf66b67a544e9e5534c2793cee5ac90bef5d076ad1e5"
-    sha256 cellar: :any_skip_relocation, catalina:      "10e13f7bf6977bee6487366b3fc1dc55b4c191d5d505cb816997838504b3e0a4"
-    sha256 cellar: :any_skip_relocation, mojave:        "0422adc9bfa6e1558cdcca24f8f4266f0927cf4c10fe3e245fe8e3017a7717b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "271d0e58a246109803f08cb46a3f4a28092bdee17b7fa78b541f259554bb03a7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1a7ba50b992a11544f4a94ab93374eddeaef6aea5cfb2dfefb0c27a2976ef644"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a3c10247d05fe6a007ad97b1131e522eec0729288bee680dfd3e5a4cca2ee5fb"
+    sha256 cellar: :any_skip_relocation, catalina:      "d4651ac184617629b57a0842ecb267adb25c34fc0b61b08296d80ee68928b66d"
+    sha256 cellar: :any_skip_relocation, mojave:        "cab7c99f874c1a498ce9b27ebd863a46dd9940b75a86da8782eef952d49e709a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12a835bf9986bbbebd0f4441a9e64ffd15413fbcfb8cb199640f29721665d022"
   end
 
   depends_on "bison" => :build
