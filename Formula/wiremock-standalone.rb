99837652be5a23b51a5cfc5b82f5e298ab2f7ee5
Rui Chen <rui@chenrui.dev>
2021-08-22 16:57:42 -0400

    wiremock-standalone: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/wiremock-standalone.rb
+++ Formula/wiremock-standalone.rb
@@ -4,7 +4,7 @@ class WiremockStandalone < Formula
   url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.30.1/wiremock-jre8-standalone-2.30.1.jar"
   sha256 "6aa6d9fa536040ba3c9b062c1257b90c045672f3864b9b481496e6ff1bef91c1"
   license "Apache-2.0"
-  head "https://github.com/tomakehurst/wiremock.git"
+  head "https://github.com/tomakehurst/wiremock.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "0c8011cbd6be9826fb258c0d5c4d3f5f95f9438da1895542f99f36fbe0778b12"
