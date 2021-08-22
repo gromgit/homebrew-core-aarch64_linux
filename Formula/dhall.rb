f0bbd0d66ebf25af7116fb65f31cdf128dfba94e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-22 09:07:31 +0000

    dhall: update 1.40.0 bottle.

--- Formula/dhall.rb
+++ Formula/dhall.rb
@@ -6,11 +6,11 @@ class Dhall < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "aa391ecec1a629c2a1634c8d873bb8fb469708a04f08662a74f7104503837868"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3033cf19e4ae915221dbe28210be943c3f4dc29e0b06d6835ad1385ddb74e0be"
-    sha256 cellar: :any_skip_relocation, catalina:      "1d5b510e3ae69e57f7b4579d568a11c5074fc6ff12ebb7f7bd406180a0f13bc6"
-    sha256 cellar: :any_skip_relocation, mojave:        "0b7b23cff6a700ecbf39d33fa2d1f1217f94c05123c18106e6acb22ba4024025"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fa2b68bd7343b9b8e3f196da0e711032c05cc66524cb539ce3711a772fa7e1b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "af427f1180f96b9bcd9ff77347f861d19ac67b3b62e3511ffc0c1cb810483143"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d1c36be171b91a50d710af00cd7321d6fadb496a90583f73e6033e28035a143e"
+    sha256 cellar: :any_skip_relocation, catalina:      "ade8c77d54c09576a75f0e2f6bc98e0a2b7cceb4cf3e93c271a24258a3855e74"
+    sha256 cellar: :any_skip_relocation, mojave:        "66ddeb565cc75a33877d69553b1aba1724a3a54e780954f799a45c3c5634ab82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c6a0b5b4c1a5901808d503625e0dee108fc221c23c049263dfb544699840a57"
   end
 
   depends_on "cabal-install" => :build
