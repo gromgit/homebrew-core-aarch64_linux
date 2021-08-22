11f23678a9ba0d89bd523607470237efe743fb85
Rui Chen <rui@chenrui.dev>
2021-08-22 16:40:22 -0400

    x265: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/x265.rb
+++ Formula/x265.rb
@@ -4,7 +4,7 @@ class X265 < Formula
   url "https://bitbucket.org/multicoreware/x265_git/get/3.5.tar.gz"
   sha256 "5ca3403c08de4716719575ec56c686b1eb55b078c0fe50a064dcf1ac20af1618"
   license "GPL-2.0-only"
-  head "https://bitbucket.org/multicoreware/x265_git.git"
+  head "https://bitbucket.org/multicoreware/x265_git.git", branch: "master"
 
   bottle do
     rebuild 1
