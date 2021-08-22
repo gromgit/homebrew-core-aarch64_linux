2a666c3a3f72b93a35d83f81d0915319d45ad753
Rui Chen <rui@chenrui.dev>
2021-08-21 23:51:18 -0400

    zinit: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/zinit.rb
+++ Formula/zinit.rb
@@ -4,7 +4,7 @@ class Zinit < Formula
   url "https://github.com/zdharma/zinit/archive/refs/tags/v3.7.tar.gz"
   sha256 "dcd7ded70255a576a4612edb743650f83e688bd4a4b473bbdafeddb473bde3c9"
   license "MIT"
-  head "https://github.com/zdharma/zinit.git"
+  head "https://github.com/zdharma/zinit.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c8965601c60d2160e1595b890653f50ad60ea07cf29777f3f008c0f93635c00"
