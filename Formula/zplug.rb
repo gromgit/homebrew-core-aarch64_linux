c47d9cb2cfd6818fca6cf993ad913e9a600204a0
Rui Chen <rui@chenrui.dev>
2021-08-21 23:51:30 -0400

    zplug: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/zplug.rb
+++ Formula/zplug.rb
@@ -4,7 +4,7 @@ class Zplug < Formula
   url "https://github.com/zplug/zplug/archive/2.4.2.tar.gz"
   sha256 "82a51e8c388844acbfb64196623bede07eee2384f1fc30966eac880373aa9030"
   license "MIT"
-  head "https://github.com/zplug/zplug.git"
+  head "https://github.com/zplug/zplug.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "88f086071ba188267046f170817aee4ad59fcdd9d9b7ad183b639306c5b8ef29"
