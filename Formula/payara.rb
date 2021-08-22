b359f24d32f25ee320c32dfe2bbad67f994beaa1
Rui Chen <rui@chenrui.dev>
2021-08-22 14:04:03 -0400

    payara 5.2021.6
    Closes #83783.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/payara.rb
+++ Formula/payara.rb
@@ -1,8 +1,8 @@
 class Payara < Formula
   desc "Java EE application server forked from GlassFish"
   homepage "https://www.payara.fish"
-  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/5.2021.5/payara-5.2021.5.zip"
-  sha256 "52fbc7272823739030475d8df97d05a526677ed7bbe73e2933264a93ee7c7806"
+  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/5.2021.6/payara-5.2021.6.zip"
+  sha256 "635af1750cf291b800e98067bd80dfefd80608ee6709215671d11f115e94ff96"
   license any_of: [
     "CDDL-1.1",
     { "GPL-2.0-only" => { with: "Classpath-exception-2.0" } },
