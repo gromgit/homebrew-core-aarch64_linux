b1f90c273f354a02cb272a491e9a8060476a6c9f
Rui Chen <rui@chenrui.dev>
2021-08-22 16:40:27 -0400

    xcenv: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/xcenv.rb
+++ Formula/xcenv.rb
@@ -4,7 +4,7 @@ class Xcenv < Formula
   url "https://github.com/xcenv/xcenv/archive/v1.2.0.tar.gz"
   sha256 "bbb47394f9edbdabba886e5ca0155f0ef6aeae07b8a7564c652c8e260fac6d9f"
   license "MIT"
-  head "https://github.com/xcenv/xcenv.git"
+  head "https://github.com/xcenv/xcenv.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "21f8af03157f7f8e815d0efe4c99f9cc4a62cabbabf94fc500c0ac39fb37e537"
