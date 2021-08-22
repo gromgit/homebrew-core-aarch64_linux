6d4e3067b68e51c6a1ea3dbd71f3cce31d0bdeb1
Michael Cho <cho-m@tuta.io>
2021-08-21 23:23:23 -0700

    avrdude: use elfutils on Linux (#83744)

--- Formula/avrdude.rb
+++ Formula/avrdude.rb
@@ -13,14 +13,13 @@ class Avrdude < Formula
   end
 
   bottle do
-    sha256                               arm64_big_sur: "3ec216455cda0569e51e10c2ac522b9870a376e2f029b2c2747c618735fe7d36"
-    sha256                               big_sur:       "80bd53f8b78f172aaea62b9a58f6febfc4ac4b510969511ab0f3e06da9adb1bb"
-    sha256                               catalina:      "d3f4c82170fa37bacd6e1bc3276ba27e7a8ed2ea781b101b7899e7602393a15b"
-    sha256                               mojave:        "65fe6de6f540eb1c6ad94d35c847f8a5921cc9059ff044d1bc78f68cc8b8334b"
-    sha256                               high_sierra:   "b0cb94b5c4f01fcc870f286bca293218c98fda23d76397db8a831272f7087038"
-    sha256                               sierra:        "e8e26af5565cd897867d4e6e71e66e6e946e1e21eb4e27d3cd49f199f088fc5d"
-    sha256                               el_capitan:    "c953526dc893a9b162a109d074edf8bb71d7049c63990282edc994c63de90c44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f1656a49d44be0df3900bcea10bc7cfd14a956681de85aeffdaea451fd23e2c5"
+    sha256 arm64_big_sur: "3ec216455cda0569e51e10c2ac522b9870a376e2f029b2c2747c618735fe7d36"
+    sha256 big_sur:       "80bd53f8b78f172aaea62b9a58f6febfc4ac4b510969511ab0f3e06da9adb1bb"
+    sha256 catalina:      "d3f4c82170fa37bacd6e1bc3276ba27e7a8ed2ea781b101b7899e7602393a15b"
+    sha256 mojave:        "65fe6de6f540eb1c6ad94d35c847f8a5921cc9059ff044d1bc78f68cc8b8334b"
+    sha256 high_sierra:   "b0cb94b5c4f01fcc870f286bca293218c98fda23d76397db8a831272f7087038"
+    sha256 sierra:        "e8e26af5565cd897867d4e6e71e66e6e946e1e21eb4e27d3cd49f199f088fc5d"
+    sha256 el_capitan:    "c953526dc893a9b162a109d074edf8bb71d7049c63990282edc994c63de90c44"
   end
 
   head do
@@ -32,13 +31,20 @@ class Avrdude < Formula
   end
 
   depends_on "automake" => :build
-  depends_on "libelf"
   depends_on "libftdi0"
   depends_on "libhid"
   depends_on "libusb-compat"
 
   uses_from_macos "bison"
   uses_from_macos "flex"
+
+  on_macos do
+    depends_on "libelf"
+  end
+
+  on_linux do
+    depends_on "elfutils"
+  end
 
   def install
     # Workaround for ancient config files not recognizing aarch64 macos.
@@ -53,8 +59,7 @@ class Avrdude < Formula
       inreplace "bootstrap", /libtoolize/, "glibtoolize"
       system "./bootstrap"
     end
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make"
     system "make", "install"
   end
