4d32c9babb79109c360ddd7f3a9cca16327fc5cd
tscp <45612704+tddschn@users.noreply.github.com>
2021-08-22 16:43:06 +0800

    h2c 1.0 (new formula)
    * h2c fee2a036 (new formula)
    * Fix h2c test
    * Add 1 more test that tests h2c's core funtionality
    * Fix style issue: line too long
    * Use the stable release from upstream
    
    Closes #83757.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/h2c.rb
@@ -0,0 +1,19 @@
+class H2c < Formula
+  desc "Headers 2 curl"
+  homepage "https://curl.se/h2c/"
+  url "https://github.com/curl/h2c/archive/refs/tags/1.0.tar.gz"
+  sha256 "1c5e4d76131abb5151c89cc54945256509dad9d12cab36205aa5bcd7f8a311af"
+  license "MIT"
+
+  def install
+    bin.install "h2c"
+  end
+
+  test do
+    assert_match "h2c.pl [options] < file", shell_output("h2c --help")
+
+    # test if h2c can convert HTTP headers to curl options.
+    assert_match "curl --head --http1.1 --header Accept: --header \"Shoesize: 12\" --user-agent \"moo\" https://curl.haxx.se/",
+      shell_output("echo 'HEAD  / HTTP/1.1\nHost: curl.haxx.se\nUser-Agent: moo\nShoesize: 12' | h2c")
+  end
+end
