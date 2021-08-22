aee0cffbaaf6466d3582731ecd4f209395466deb
Rui Chen <rui@chenrui.dev>
2021-08-22 16:56:42 -0400

    wallpaper: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -5,7 +5,7 @@ class Wallpaper < Formula
   sha256 "4925247524535c0cc128dcc4d87f5538a5ce3b5d3a3c211127fd646ee00252b6"
   license "MIT"
   revision 1
-  head "https://github.com/sindresorhus/macos-wallpaper.git"
+  head "https://github.com/sindresorhus/macos-wallpaper.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "c287fd0c20f380e5da1a0d164b7199b8280f868dd994a86002f06481fc5db36a"
