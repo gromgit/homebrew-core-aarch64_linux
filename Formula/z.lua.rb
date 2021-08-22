ba05e88448bcd9e56d40332bc63b17f5678d55e5
Rui Chen <rui@chenrui.dev>
2021-08-21 23:50:52 -0400

    z.lua: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/z.lua.rb
+++ Formula/z.lua.rb
@@ -4,7 +4,7 @@ class ZLua < Formula
   url "https://github.com/skywind3000/z.lua/archive/1.8.13.tar.gz"
   sha256 "a1215d727527e358363687a7e0b3605a132465e60a915f8e99bb5338a9c62ec6"
   license "MIT"
-  head "https://github.com/skywind3000/z.lua.git"
+  head "https://github.com/skywind3000/z.lua.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "5e81f502f0d1aa8fcc350be77777e7dc6ee05bd04906762f4ce9a3fc285ceba5"
