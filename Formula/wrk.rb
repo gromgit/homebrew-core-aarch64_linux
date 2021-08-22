47b30e95aaffad1f408741938254bc2fac1b202b
Rui Chen <rui@chenrui.dev>
2021-08-22 16:58:02 -0400

    wrk: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/wrk.rb
+++ Formula/wrk.rb
@@ -3,7 +3,7 @@ class Wrk < Formula
   homepage "https://github.com/wg/wrk"
   url "https://github.com/wg/wrk/archive/4.1.0.tar.gz"
   sha256 "6fa1020494de8c337913fd139d7aa1acb9a020de6f7eb9190753aa4b1e74271e"
-  head "https://github.com/wg/wrk.git"
+  head "https://github.com/wg/wrk.git", branch: "master"
 
   bottle do
     rebuild 2
