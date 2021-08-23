3bb7c38e63ee61291df17a672e6b7abd713a829a
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-08-23 16:21:29 +0000

    cilium-cli: add 0.8.6 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -5,6 +5,14 @@ class CiliumCli < Formula
   sha256 "7d4adfc511206af56570ba90dd523bd31f1703c8f9b36bca90e4d0863dd06ab7"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b402009dc643fde2756a0b86b6553a5d76f3ad1c34c72248920c1f7539ce9681"
+    sha256 cellar: :any_skip_relocation, big_sur:       "694b6f5f58bbc16220c4d53bc8aee5fb8f1ccd0e55c91e3a720856c695d97155"
+    sha256 cellar: :any_skip_relocation, catalina:      "32c3bec63083873df4d1cc0921c7d31ff1846daf7e714ed6a62c56283dfc0a87"
+    sha256 cellar: :any_skip_relocation, mojave:        "f11604cc3f64ef66d0598cbd9f9c9433d8db51aeefae292926ea5ab28c7518ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb4784d82d170b1ed14058a8b829f094f0f870fe05424af32ef42985882abac4"
+  end
+
   depends_on "go" => :build
 
   def install
