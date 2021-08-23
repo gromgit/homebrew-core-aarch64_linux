38a3be5fff4c007588fead334f88940f3d5b88fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-23 08:12:53 +0000

    cargo-instruments: update 0.4.2 bottle.

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -6,10 +6,10 @@ class CargoInstruments < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "27d1412ae371b1e8a555a9ed66a79df685c170f45cdbda124c5f0c520ee997cd"
-    sha256 cellar: :any, big_sur:       "521c4f9983ffc40a9ec1da642c3d9757e1cb169cadd4f2c0d2f1480e074a0c94"
-    sha256 cellar: :any, catalina:      "022db59b1bd0cbd1c209a7d4837f510a1ce8db6d2d4eb7ced7924e2370dcee16"
-    sha256 cellar: :any, mojave:        "a18ede354dc88113c06e6a4f3a70f2408671b8fd5edaebb6498bdb0b8640cf62"
+    sha256 cellar: :any, arm64_big_sur: "b67c1ae46a3f04740faeb1686e4f8efec7d38630016bef9ccc4648a88942ddbe"
+    sha256 cellar: :any, big_sur:       "5f6ef51683a2cd8ed6fa66f749e0e3f7ea2e4abfe434c99df28eafbff9545eb2"
+    sha256 cellar: :any, catalina:      "38d6e711b92d8b0a519ce2674efeea7f6622d6306dac91bf0066ac2fb63f9360"
+    sha256 cellar: :any, mojave:        "03c10ff29c646e5231d924b8838eba1cd533ac261e1b73d5bf8eadea86e44c10"
   end
 
   depends_on "rust" => :build
