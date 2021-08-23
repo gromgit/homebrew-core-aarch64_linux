c9568d1fb57d14f6777f6d717d7576652a5a88e8
Rui Chen <rui@chenrui.dev>
2021-08-23 01:38:57 -0400

    docfx 2.58.1
    Closes #83806.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.58/docfx.zip"
-  sha256 "a6ad1b9983debae0613816d83f31211d7bad0aaaef947cb25fef888294c48ebe"
+  url "https://github.com/dotnet/docfx/releases/download/v2.58.1/docfx.zip"
+  sha256 "94085d7ee874c786909ba23e0f436c4aeffa3f6057a39d2229e70bd1b5e710ba"
   license "MIT"
 
   bottle do
