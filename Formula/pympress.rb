6b45cfccb1f46ec15358f58a1bb0b4240d218e84
Cimbali <me@cimba.li>
2021-08-23 14:27:04 +0000

    pympress 1.6.4
    Closes #83819.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pympress.rb
+++ Formula/pympress.rb
@@ -3,8 +3,8 @@ class Pympress < Formula
 
   desc "Simple and powerful dual-screen PDF reader designed for presentations"
   homepage "https://github.com/Cimbali/pympress/"
-  url "https://files.pythonhosted.org/packages/9b/69/9b60fab9cddc63ea11d3d16d07dc375951015cf2f360af1dbca2fff7fe15/pympress-1.6.3.tar.gz"
-  sha256 "7fe3a3134c7fdf27c960708b07ea27ed34fb309daf36ef3b487462ebba850c23"
+  url "https://files.pythonhosted.org/packages/30/15/076cbcb2fcd828da499db28bda2699bdadc73c16953c564fee6e3b6c28c8/pympress-1.6.4.tar.gz"
+  sha256 "f84b9dc4da0defab1dc3c39ba91837f51af7837b775194f0057d0045c8d2b04f"
   license "GPL-2.0-or-later"
   head "https://github.com/Cimbali/pympress.git"
 
@@ -22,8 +22,8 @@ class Pympress < Formula
   depends_on "python@3.9"
 
   resource "watchdog" do
-    url "https://files.pythonhosted.org/packages/f5/c2/d1ff8343cd38138561d2f08aba7b0566020485346097019f3a87773c96fc/watchdog-2.1.3.tar.gz"
-    sha256 "e5236a8e8602ab6db4b873664c2d356c365ab3cac96fbdec4970ad616415dd45"
+    url "https://files.pythonhosted.org/packages/c5/e9/fb0f9775c82b4df1815bb97ebac13383adddff4cf014aceefb7c02262675/watchdog-2.1.5.tar.gz"
+    sha256 "5563b005907613430ef3d4aaac9c78600dd5704e84764cb6deda4b3d72807f09"
   end
 
   def install
