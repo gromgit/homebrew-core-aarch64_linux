b2bb79971b2bced3f4cc12b3dfd22a5a80a1b42c
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-08-23 06:29:51 +0000

    libsndfile: update 1.0.31 bottle.

--- Formula/libsndfile.rb
+++ Formula/libsndfile.rb
@@ -11,10 +11,11 @@ class Libsndfile < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8e2fc3b0df09a21840f8643f644bd3a0bb3c3551d21f600b344f6b316d3ef44d"
-    sha256 cellar: :any, big_sur:       "a4a734e58220fc8615d86e4563e9a874447d568151b366aa94391dfe07c4e0fb"
-    sha256 cellar: :any, catalina:      "671a3cc9c7dafd89cbaffeccf4de826262c144184bf5779320c236e87e7636cc"
-    sha256 cellar: :any, mojave:        "8b2876610f9188e8125f636e85bcbd525343b216c6d0787954e78b88dfe8f101"
+    sha256 cellar: :any,                 arm64_big_sur: "8e2fc3b0df09a21840f8643f644bd3a0bb3c3551d21f600b344f6b316d3ef44d"
+    sha256 cellar: :any,                 big_sur:       "a4a734e58220fc8615d86e4563e9a874447d568151b366aa94391dfe07c4e0fb"
+    sha256 cellar: :any,                 catalina:      "671a3cc9c7dafd89cbaffeccf4de826262c144184bf5779320c236e87e7636cc"
+    sha256 cellar: :any,                 mojave:        "8b2876610f9188e8125f636e85bcbd525343b216c6d0787954e78b88dfe8f101"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3949f7d627ea3a217a8f9461ee8ad7f333ed48f0490db897a5f03d9dfd5995f5"
   end
 
   depends_on "autoconf" => :build
