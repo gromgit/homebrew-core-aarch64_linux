32c02e67c405fb78dfd2cfc80936a082b39a2f77
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-24 00:57:48 +0000

    ddd: update 3.3.12_1 bottle.

--- Formula/ddd.rb
+++ Formula/ddd.rb
@@ -9,10 +9,11 @@ class Ddd < Formula
 
   bottle do
     rebuild 2
-    sha256 big_sur:     "498ceb2dc933d2c85e7407f077d187c6cd799ba2f539694087134d038bb211d9"
-    sha256 catalina:    "df163eb838675a73c69913af1e1526a5c20e5cbeafa58836112ce4ae642a705a"
-    sha256 mojave:      "ef4ae2c46be3ad1aee12c52ca34d7606c3aa056250792a61c03af4581fe8e568"
-    sha256 high_sierra: "9fc9c568178424aeb25d6721c4faffb99a8bd7ef967ea0ae4e3464b65651d0b8"
+    sha256 big_sur:      "498ceb2dc933d2c85e7407f077d187c6cd799ba2f539694087134d038bb211d9"
+    sha256 catalina:     "df163eb838675a73c69913af1e1526a5c20e5cbeafa58836112ce4ae642a705a"
+    sha256 mojave:       "ef4ae2c46be3ad1aee12c52ca34d7606c3aa056250792a61c03af4581fe8e568"
+    sha256 high_sierra:  "9fc9c568178424aeb25d6721c4faffb99a8bd7ef967ea0ae4e3464b65651d0b8"
+    sha256 x86_64_linux: "b77e99734dc952ce417d949ba92e03e138f8f4b1a8224a4e3a98bc2822cea7b8"
   end
 
   depends_on "gdb" => :test
