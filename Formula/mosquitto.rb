9f3be22eab002c9331d8d989e2e94a5fde388d4a
Branch Vincent <branchevincent@gmail.com>
2021-08-24 00:24:38 -0400

    mosquitto: replace `plist` with `service`

--- Formula/mosquitto.rb
+++ Formula/mosquitto.rb
@@ -52,31 +52,10 @@ class Mosquitto < Formula
     EOS
   end
 
-  plist_options manual: "mosquitto -c #{HOMEBREW_PREFIX}/etc/mosquitto/mosquitto.conf"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/mosquitto</string>
-          <string>-c</string>
-          <string>#{etc}/mosquitto/mosquitto.conf</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <false/>
-        <key>WorkingDirectory</key>
-        <string>#{var}/mosquitto</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_sbin/"mosquitto", "-c", etc/"mosquitto/mosquitto.conf"]
+    keep_alive false
+    working_dir var/"mosquitto"
   end
 
   test do
