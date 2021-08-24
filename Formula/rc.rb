ef7817233bc5c7614b685de97ad118484f923df2
Caleb Xu <calebcenter@live.com>
2021-08-24 10:29:00 -0400

    rc: fix build on Linux

--- Formula/rc.rb
+++ Formula/rc.rb
@@ -4,6 +4,7 @@ class Rc < Formula
   url "https://web.archive.org/web/20200227085442/static.tobold.org/rc/rc-1.7.4.tar.gz"
   mirror "https://src.fedoraproject.org/repo/extras/rc/rc-1.7.4.tar.gz/f99732d7a8be3f15f81e99c3af46dc95/rc-1.7.4.tar.gz"
   sha256 "5ed26334dd0c1a616248b15ad7c90ca678ae3066fa02c5ddd0e6936f9af9bfd8"
+  license "Zlib"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "18c672af9e2e5d2e88ca29d57aec584aaa57daac97b9ac330d9f8164beb9ecce"
@@ -14,6 +15,8 @@ class Rc < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "627e45477eabd5854e3c5f39af5290befd43d03b385d1b20f0ce4b49636fd2d9"
   end
 
+  uses_from_macos "libedit"
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
@@ -23,6 +26,6 @@ class Rc < Formula
   end
 
   test do
-    system "#{bin}/rc", "-c", "echo Hello!"
+    assert_equal "Hello!", shell_output("#{bin}/rc -c 'echo Hello!'").chomp
   end
 end
