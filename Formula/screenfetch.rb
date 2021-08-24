4b463200f2c2894f0bfa528c5db27953bc0fda2e
Rui Chen <rui@chenrui.dev>
2021-08-24 00:55:40 -0400

    screenfetch: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -4,7 +4,7 @@ class Screenfetch < Formula
   url "https://github.com/KittyKatt/screenFetch/archive/v3.9.1.tar.gz"
   sha256 "aa97dcd2a8576ae18de6c16c19744aae1573a3da7541af4b98a91930a30a3178"
   license "GPL-3.0"
-  head "https://github.com/KittyKatt/screenFetch.git"
+  head "https://github.com/KittyKatt/screenFetch.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "6f7e61ea4717eef72e68b006bcef5d6ff1aab08f7ba25f0a5c6b8e014ffb530b"
