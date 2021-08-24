e77ab4cf528703e0ff1542b83a0ff30e360f34a3
Branch Vincent <branchevincent@gmail.com>
2021-08-23 21:34:26 -0400

    scm-manager: replace `plist` with `service`

--- Formula/scm-manager.rb
+++ Formula/scm-manager.rb
@@ -34,26 +34,8 @@ class ScmManager < Formula
     bin.write_jar_script libexec/"tools/scm-cli-client-#{version}-jar-with-dependencies.jar", "scm-cli-client", java_version: "1.8"
   end
 
-  plist_options manual: "scm-server start"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/scm-server</string>
-            <string>start</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"scm-server", "start"]
   end
 
   test do
