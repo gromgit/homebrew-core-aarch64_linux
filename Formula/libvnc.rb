a2ac081e4d277cdaaadad05aded94de2cf627411
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-24 06:39:50 +0000

    libvnc: update 0.9.13 bottle.

--- Formula/libvnc.rb
+++ Formula/libvnc.rb
@@ -11,10 +11,11 @@ class Libvnc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "20c74a915bd00103a38d68a25a24aa700b36f4b0f1882ce399f1ad45b9b4a3c5"
-    sha256 cellar: :any, big_sur:       "ea5e78dfaf457a33519debdc579dfd868f6503ef93b526d0f57fcab73997f298"
-    sha256 cellar: :any, catalina:      "908c0d7fa104abe781cc67b0a9ebff7e2208cf3cbfb4b7acd770d3d92bb14c9d"
-    sha256 cellar: :any, mojave:        "4744d5940eb9095e9d7ec8a731c8f611a252e5548237d3d338a2334766b38825"
+    sha256 cellar: :any,                 arm64_big_sur: "20c74a915bd00103a38d68a25a24aa700b36f4b0f1882ce399f1ad45b9b4a3c5"
+    sha256 cellar: :any,                 big_sur:       "ea5e78dfaf457a33519debdc579dfd868f6503ef93b526d0f57fcab73997f298"
+    sha256 cellar: :any,                 catalina:      "908c0d7fa104abe781cc67b0a9ebff7e2208cf3cbfb4b7acd770d3d92bb14c9d"
+    sha256 cellar: :any,                 mojave:        "4744d5940eb9095e9d7ec8a731c8f611a252e5548237d3d338a2334766b38825"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a38e3225e47344136ae3e0e51c0f1875d25ad7f7b49f319c87383262f842938"
   end
 
   depends_on "cmake" => :build
