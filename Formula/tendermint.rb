5788f5fe6fc778c3084321195a4bc2b1e5d358ed
Rui Chen <rui@chenrui.dev>
2021-08-24 00:26:52 -0400

    tendermint: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -4,7 +4,7 @@ class Tendermint < Formula
   url "https://github.com/tendermint/tendermint/archive/v0.34.12.tar.gz"
   sha256 "ffbf7db05a52f15462df33e7fd14c81291eb55dabf2cb686e8113f992dc8b06a"
   license "Apache-2.0"
-  head "https://github.com/tendermint/tendermint.git"
+  head "https://github.com/tendermint/tendermint.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "d069288a129924e9c6c8c80af2ccaf78b71b724c794e1830ebbbba6669d09f0d"
