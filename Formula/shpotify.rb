cd37a7d053674db1f09a47ce3a9732460195d8c9
Rui Chen <rui@chenrui.dev>
2021-08-24 00:55:46 -0400

    shpotify: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/shpotify.rb
+++ Formula/shpotify.rb
@@ -4,7 +4,7 @@ class Shpotify < Formula
   url "https://github.com/hnarayanan/shpotify/archive/2.1.tar.gz"
   sha256 "b41d8798687be250d0306ac0c5a79420fa46619c5954286711a5d63c86a6c071"
   license "MIT"
-  head "https://github.com/hnarayanan/shpotify.git"
+  head "https://github.com/hnarayanan/shpotify.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e15fab27ffc271dd4fbb5317540b1628e30b79cb5cdaed0f51eb956873876565"
