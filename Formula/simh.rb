9e64c1ee7ae12c2bc211c6bd6c193e71880d263f
Rui Chen <rui@chenrui.dev>
2021-08-24 00:55:48 -0400

    simh: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/simh.rb
+++ Formula/simh.rb
@@ -5,7 +5,7 @@ class Simh < Formula
   version "3.11.1"
   sha256 "c8a2fc62bfa9369f75935950512a4cac204fd813ce6a9a222b2c6a76503befdb"
   license "MIT"
-  head "https://github.com/simh/simh.git"
+  head "https://github.com/simh/simh.git", branch: "master"
 
   # At the time this check was added, the "latest" release on GitHub was several
   # versions behind the actual latest version, so we check the Git tags instead.
