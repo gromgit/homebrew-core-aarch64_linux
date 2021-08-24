914cb9c161ecb49669d7811a351cd2c073d369a2
Michael Cho <cho-m@tuta.io>
2021-08-23 19:31:09 -0700

    speex: use brewed curl for url
    Closes #83846.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -1,7 +1,7 @@
 class Speex < Formula
   desc "Audio codec designed for speech"
   homepage "https://speex.org/"
-  url "https://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz"
+  url "https://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz", using: :homebrew_curl
   mirror "https://ftp.osuosl.org/pub/xiph/releases/speex/speex-1.2.0.tar.gz"
   sha256 "eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094"
   license "BSD-3-Clause"
@@ -27,9 +27,7 @@ class Speex < Formula
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
