0567e93dea10a9d70be6abd7ca21a2adc41b1459
Branch Vincent <branchevincent@gmail.com>
2021-08-23 21:34:26 -0400

    shairport-sync: replace `plist` with `service`

--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -54,33 +54,11 @@ class ShairportSync < Formula
     (var/"run").mkpath
   end
 
-  plist_options manual: "shairport-sync"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/shairport-sync</string>
-          <string>--use-stderr</string>
-          <string>--verbose</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/#{name}.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/#{name}.log</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"shairport-sync", "--use-stderr", "--verbose"]
+    keep_alive true
+    log_path var/"log/shairport-sync.log"
+    error_log_path var/"log/shairport-sync.log"
   end
 
   test do
