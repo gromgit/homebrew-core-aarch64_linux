5cd4eafd562cd5db6e1193e20b07ed6572907785
Rui Chen <rui@chenrui.dev>
2021-08-24 00:55:43 -0400

    serveit: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/serveit.rb
+++ Formula/serveit.rb
@@ -5,7 +5,7 @@ class Serveit < Formula
   sha256 "5bbefdca878aab4a8c8a0c874c02a0a033cf4321121c9e006cb333d9bd7b6d52"
   license "MIT"
   revision 1
-  head "https://github.com/garybernhardt/serveit.git"
+  head "https://github.com/garybernhardt/serveit.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e17a21fd5705de1b2e96656ad703f51c8f1781601f295ff7028cfc859bd0ca44"
