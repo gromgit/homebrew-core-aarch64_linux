29a7241ca93e091d73fb83cc33507189d0815493
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-24 17:25:20 +0000

    sox: update 14.4.2_3 bottle.

--- Formula/sox.rb
+++ Formula/sox.rb
@@ -6,12 +6,13 @@ class Sox < Formula
   revision 3
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8070949420a9a02f3d5e1a99bd460d064e34c361798bae5c4554ac8e1aeb2d49"
-    sha256 cellar: :any, big_sur:       "e3f62a35b06c9e79516f575a923b3aafc5357f370f4ae5c2812c67c8862ae11c"
-    sha256 cellar: :any, catalina:      "fc412be07e577c2161763dfb509f4fb43f4fe3bca206a1b0b370687df0a264fa"
-    sha256 cellar: :any, mojave:        "4906207f83bd0f4ea1a67d040891711e9a9e8830216e451072f2957ca566b83d"
-    sha256 cellar: :any, high_sierra:   "c0bb4ba7ec922d9a8c71c2ba84e28c66c67e4fdeae970011ea45e937f43c18bd"
-    sha256 cellar: :any, sierra:        "dc8c294bb96c0b7ebc3ade73476c6031664bb8e81a32ece87ce84f815deeced5"
+    sha256 cellar: :any,                 arm64_big_sur: "8070949420a9a02f3d5e1a99bd460d064e34c361798bae5c4554ac8e1aeb2d49"
+    sha256 cellar: :any,                 big_sur:       "e3f62a35b06c9e79516f575a923b3aafc5357f370f4ae5c2812c67c8862ae11c"
+    sha256 cellar: :any,                 catalina:      "fc412be07e577c2161763dfb509f4fb43f4fe3bca206a1b0b370687df0a264fa"
+    sha256 cellar: :any,                 mojave:        "4906207f83bd0f4ea1a67d040891711e9a9e8830216e451072f2957ca566b83d"
+    sha256 cellar: :any,                 high_sierra:   "c0bb4ba7ec922d9a8c71c2ba84e28c66c67e4fdeae970011ea45e937f43c18bd"
+    sha256 cellar: :any,                 sierra:        "dc8c294bb96c0b7ebc3ade73476c6031664bb8e81a32ece87ce84f815deeced5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "812ddfd499335d3ff3e096b6153ef60e86326b5fb2cffb3e2ae02e8be6bc98c2"
   end
 
   depends_on "pkg-config" => :build
