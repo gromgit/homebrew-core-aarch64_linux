0dd86c532e43d673d223645f8b994011f038025a
Rui Chen <rui@chenrui.dev>
2021-08-24 00:55:52 -0400

    slugify: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/slugify.rb
+++ Formula/slugify.rb
@@ -4,7 +4,7 @@ class Slugify < Formula
   url "https://github.com/benlinton/slugify/archive/v1.0.1.tar.gz"
   sha256 "f6873b062119d3eaa7d89254fc6e241debf074da02e3189f12e08b372af096e5"
   license "MIT"
-  head "https://github.com/benlinton/slugify.git"
+  head "https://github.com/benlinton/slugify.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "73fa2281dd5bc54549c488774feb2d262b0a4e80c4bedf2e017f90e74293076d"
