1371a1192200b7168ac0ab79a51a01e81aa70389
Rui Chen <rui@chenrui.dev>
2021-08-24 00:26:52 -0400

    terminal-notifier: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/terminal-notifier.rb
+++ Formula/terminal-notifier.rb
@@ -4,7 +4,7 @@ class TerminalNotifier < Formula
   url "https://github.com/julienXX/terminal-notifier/archive/2.0.0.tar.gz"
   sha256 "6f22a7626e4e68e88df2005a5f256f7d3b432dbf4c0f8a0c15c968d9e38bf84c"
   license "MIT"
-  head "https://github.com/julienXX/terminal-notifier.git"
+  head "https://github.com/julienXX/terminal-notifier.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "d1268e236f13f5bb4cd5fead9cf54cfb54ceefb98e34861bd39cf3c7e6ef34cf"
