654f92dd0f73e81d5df6a041fdf0634f6286e680
Rui Chen <rui@chenrui.dev>
2021-08-24 00:26:50 -0400

    tccutil: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tccutil.rb
+++ Formula/tccutil.rb
@@ -4,7 +4,7 @@ class Tccutil < Formula
   url "https://github.com/jacobsalmela/tccutil/archive/v1.2.10.tar.gz"
   sha256 "8a6f9961b37322dee0f20a70b4299a23f16f3023a549147a4410e3f322dbfa22"
   license "GPL-2.0-or-later"
-  head "https://github.com/jacobsalmela/tccutil.git"
+  head "https://github.com/jacobsalmela/tccutil.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "2f9f812b661c081405e4dfd04a1c6b22003898d6c978e4af797e6d079ee803ce"
