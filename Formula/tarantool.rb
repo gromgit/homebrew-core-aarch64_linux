17c7aa61bd9d9957bfdb49352a7f2fc577cf1e1c
Rui Chen <rui@chenrui.dev>
2021-08-24 00:26:49 -0400

    tarantool: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tarantool.rb
+++ Formula/tarantool.rb
@@ -6,7 +6,7 @@ class Tarantool < Formula
   license "BSD-2-Clause"
   revision 1
   version_scheme 1
-  head "https://github.com/tarantool/tarantool.git"
+  head "https://github.com/tarantool/tarantool.git", branch: "master"
 
   bottle do
     sha256 big_sur:  "5e98389b6a0239a8aeea044669945e4d80ec14dd8dfe6c58f4d1ebd91a28b366"
