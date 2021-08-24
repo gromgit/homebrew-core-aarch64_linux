2e28e781d535a18955a5fcb3820a6d97aad3aad3
Branch Vincent <branchevincent@gmail.com>
2021-08-23 21:34:26 -0400

    shadowsocks-libev: replace `plist` with `service`

--- Formula/shadowsocks-libev.rb
+++ Formula/shadowsocks-libev.rb
@@ -52,29 +52,9 @@ class ShadowsocksLibev < Formula
     system "make", "install"
   end
 
-  plist_options manual: "#{HOMEBREW_PREFIX}/opt/shadowsocks-libev/bin/ss-local -c #{HOMEBREW_PREFIX}/etc/shadowsocks-libev.json"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/ss-local</string>
-            <string>-c</string>
-            <string>#{etc}/shadowsocks-libev.json</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"ss-local", "-c", etc/"shadowsocks-libev.json"]
+    keep_alive true
   end
 
   test do
