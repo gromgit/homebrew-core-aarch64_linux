22415e9ccce22d3d15e1ffc08ed66d4ee50d9dc1
Rui Chen <rui@chenrui.dev>
2021-08-24 00:01:28 -0400

    unbound: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -4,7 +4,7 @@ class Unbound < Formula
   url "https://nlnetlabs.nl/downloads/unbound/unbound-1.13.2.tar.gz"
   sha256 "0a13b547f3b92a026b5ebd0423f54c991e5718037fd9f72445817f6a040e1a83"
   license "BSD-3-Clause"
-  head "https://github.com/NLnetLabs/unbound.git"
+  head "https://github.com/NLnetLabs/unbound.git", branch: "master"
 
   # We check the GitHub repo tags instead of
   # https://nlnetlabs.nl/downloads/unbound/ since the first-party site has a
