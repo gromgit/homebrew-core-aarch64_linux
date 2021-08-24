7d3686af433497ee0bf84c8c6c418a63c7a6ad09
Branch Vincent <branchevincent@gmail.com>
2021-08-24 00:24:37 -0400

    manticoresearch: replace `plist` with `service`

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -47,32 +47,10 @@ class Manticoresearch < Formula
     (var/"manticore/data").mkpath
   end
 
-  plist_options manual: "searchd --config #{HOMEBREW_PREFIX}/etc/manticore/manticore.conf"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <false/>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{opt_bin}/searchd</string>
-              <string>--config</string>
-              <string>#{etc}/manticore/manticore.conf</string>
-              <string>--nodetach</string>
-          </array>
-          <key>WorkingDirectory</key>
-          <string>#{HOMEBREW_PREFIX}</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"searchd", "--config", etc/"manticore/manticore.conf", "--nodetach"]
+    keep_alive false
+    working_dir HOMEBREW_PREFIX
   end
 
   test do
