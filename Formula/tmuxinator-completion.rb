a4c223f3dbe4dd160e933981f2ce41e65c65dfe2
Rui Chen <rui@chenrui.dev>
2021-08-24 00:27:02 -0400

    tmuxinator-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tmuxinator-completion.rb
+++ Formula/tmuxinator-completion.rb
@@ -4,7 +4,7 @@ class TmuxinatorCompletion < Formula
   url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.1.tar.gz"
   sha256 "1d79dd13beaaf2ed24c8a76410c1a41bedc7785498e199534fa3928bbf8aab01"
   license "MIT"
-  head "https://github.com/tmuxinator/tmuxinator.git"
+  head "https://github.com/tmuxinator/tmuxinator.git", branch: "master"
 
   livecheck do
     formula "tmuxinator"
