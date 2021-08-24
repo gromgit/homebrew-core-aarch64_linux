8e36e32ed69ea7a31b02219801cfd12280d46c47
Josh Holtz <me@joshholtz.com>
2021-08-23 22:58:35 -0600

    fastlane 2.192.0
    Closes #83854.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.191.0.tar.gz"
-  sha256 "ce69c4110805f0847d112e1214fad4739b2449d8b981bae64341e43667ac4efc"
+  url "https://github.com/fastlane/fastlane/archive/2.192.0.tar.gz"
+  sha256 "e9f193075065c8617f4e52b3dbb0b671aedf76505d639887e961de3483c78a0e"
   license "MIT"
   head "https://github.com/fastlane/fastlane.git"
 
