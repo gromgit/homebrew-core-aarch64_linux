f41d30bd83cd55ae5f36ce77ba72224284f829ef
Michael Cho <cho-m@tuta.io>
2021-08-24 09:53:47 -0700

    sdl2_image: fix test for Linux (#83857)

--- Formula/sdl2_image.rb
+++ Formula/sdl2_image.rb
@@ -60,7 +60,7 @@ class Sdl2Image < Formula
           return success;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{lib}", "-lSDL2_image", "-o", "test"
+    system ENV.cc, "test.c", "-I#{Formula["sdl2"].opt_include}/SDL2", "-L#{lib}", "-lSDL2_image", "-o", "test"
     system "./test"
   end
 end
