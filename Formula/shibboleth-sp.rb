d7c4c1408a655d14ae8246acfe03c8be0e6d883c
Branch Vincent <branchevincent@gmail.com>
2021-08-23 21:34:26 -0400

    shibboleth-sp: replace `plist` with `service`

--- Formula/shibboleth-sp.rb
+++ Formula/shibboleth-sp.rb
@@ -56,31 +56,10 @@ class ShibbolethSp < Formula
     (var/"cache/shibboleth").mkpath
   end
 
-  plist_options startup: true, manual: "shibd"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/shibd</string>
-          <string>-F</string>
-          <string>-f</string>
-          <string>-p</string>
-          <string>#{var}/run/shibboleth/shibd.pid</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-      </dict>
-      </plist>
-    EOS
+  plist_options startup: true
+  service do
+    run [opt_sbin/"shibd", "-F", "-f", "-p", var/"run/shibboleth/shibd.pid"]
+    keep_alive true
   end
 
   test do
