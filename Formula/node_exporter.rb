af9196a207fa0e60c67b117def36219ce27bbedc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-24 07:12:08 +0000

    node_exporter: update 1.2.2 bottle.

--- Formula/node_exporter.rb
+++ Formula/node_exporter.rb
@@ -12,11 +12,12 @@ class NodeExporter < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "10c3e725f35828e54645d90f4e8865068de1122cd0f55b2fbde812e871c2e45d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f1c1467f8db86b9578363d2e0705b2b150ac5843a7a780d82db57c4bab305574"
-    sha256 cellar: :any_skip_relocation, catalina:      "69c66da1f5837d53c694ec8210807fdec373fbcda411ef9e567339713a9c6431"
-    sha256 cellar: :any_skip_relocation, mojave:        "df2e4a99a7060703980b73f23ae1d7628a74d5f6ce098577dc0877ce5ec7fd23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ae95a4372832bd1bd2f76ffadf74ba51db0437c6731e5cc29719b7eb0c83806"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "08defe8d74459977bc0bf5b5da1e0c09bada5943fd6bbb78d053deb0f6ad1505"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bbac191f8d01fe6a3cd41a389d02ac800e6b823ddb0a5bb9fc60f4d7e59da41c"
+    sha256 cellar: :any_skip_relocation, catalina:      "71aa1e6052258257c504b3968027c43238e62ab583efaf1937670ba25defce19"
+    sha256 cellar: :any_skip_relocation, mojave:        "d79d282260ddd651b834688c74b5255d74ccd0c67d9c504efe71882503ebd6a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5937f64800a35b0205c36e679d7d7f83042092e8e9cb5ef71fb145b22e4e296c"
   end
 
   depends_on "go" => :build
