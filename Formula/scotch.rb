14a6bb0b9c1e8a6f0996c2abede9d2c6bb65f69a
Rui Chen <rui@chenrui.dev>
2021-08-24 00:55:40 -0400

    scotch: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/scotch.rb
+++ Formula/scotch.rb
@@ -4,7 +4,7 @@ class Scotch < Formula
   url "https://gitlab.inria.fr/scotch/scotch/-/archive/v6.1.1/scotch-v6.1.1.tar.bz2"
   sha256 "543ab2f8998658e5a6123231bfb3736cfbb939f330c656f8a7913d967a933f00"
   license "CECILL-C"
-  head "https://gitlab.inria.fr/scotch/scotch.git"
+  head "https://gitlab.inria.fr/scotch/scotch.git", branch: "master"
 
   livecheck do
     url :stable
