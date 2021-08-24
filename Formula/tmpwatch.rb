183d96b33a05f55c6af9cb27e223b6c7072c6537
Rui Chen <rui@chenrui.dev>
2021-08-24 00:27:01 -0400

    tmpwatch: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tmpwatch.rb
+++ Formula/tmpwatch.rb
@@ -4,7 +4,7 @@ class Tmpwatch < Formula
   url "https://releases.pagure.org/tmpwatch/tmpwatch-2.11.tar.bz2"
   sha256 "93168112b2515bc4c7117e8113b8d91e06b79550d2194d62a0c174fe6c2aa8d4"
   license "GPL-2.0-only"
-  head "https://pagure.io/tmpwatch.git"
+  head "https://pagure.io/tmpwatch.git", branch: "master"
 
   livecheck do
     url :head
