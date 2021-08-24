8520ac12736adf3a080e81bd472c2acc858224b0
Rui Chen <rui@chenrui.dev>
2021-08-23 23:54:39 -0400

    vapoursynth-imwri: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/vapoursynth-imwri.rb
+++ Formula/vapoursynth-imwri.rb
@@ -4,7 +4,7 @@ class VapoursynthImwri < Formula
   url "https://github.com/vapoursynth/vapoursynth/archive/R54.tar.gz"
   sha256 "ad0c446adcb3877c253dc8c1372a053ad35022bcf42600889b927d2797c5330b"
   license "LGPL-2.1-or-later"
-  head "https://github.com/vapoursynth/vapoursynth.git"
+  head "https://github.com/vapoursynth/vapoursynth.git", branch: "master"
 
   livecheck do
     formula "vapoursynth"
