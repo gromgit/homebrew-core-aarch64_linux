f8ea09be6129426d81f3e38c5cd0f90e2b077129
Rui Chen <rui@chenrui.dev>
2021-08-23 23:54:43 -0400

    virtualhost.sh: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/virtualhost.sh.rb
+++ Formula/virtualhost.sh.rb
@@ -3,7 +3,7 @@ class VirtualhostSh < Formula
   homepage "https://github.com/virtualhost/virtualhost.sh"
   url "https://github.com/virtualhost/virtualhost.sh/archive/1.35.tar.gz"
   sha256 "75d34b807e71acd253876c6a99cdbc11ce31ffb159155373c83a99af862fffcc"
-  head "https://github.com/virtualhost/virtualhost.sh.git"
+  head "https://github.com/virtualhost/virtualhost.sh.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "69fa9f84d063cdb79b54c2e198110a9ee786851f8ff9174e43a2565ccd5fbc7e"
