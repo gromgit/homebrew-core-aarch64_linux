d943fcccd55648330ffca05e771b0cde3119fc50
Rui Chen <rui@chenrui.dev>
2021-08-23 23:54:43 -0400

    visionmedia-watch: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/visionmedia-watch.rb
+++ Formula/visionmedia-watch.rb
@@ -3,7 +3,7 @@ class VisionmediaWatch < Formula
   homepage "https://github.com/visionmedia/watch"
   url "https://github.com/visionmedia/watch/archive/0.3.1.tar.gz"
   sha256 "769196a9f33d069b1d6c9c89e982e5fdae9cfccd1fd4000d8da85e9620faf5a6"
-  head "https://github.com/visionmedia/watch.git"
+  head "https://github.com/visionmedia/watch.git", branch: "master"
 
   bottle do
     rebuild 1
