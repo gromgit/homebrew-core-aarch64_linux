78aa29e3889dd08415c23932e7c8c8604c31b230
Rui Chen <rui@chenrui.dev>
2021-08-24 00:26:48 -0400

    t-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t-completion.rb
+++ Formula/t-completion.rb
@@ -4,7 +4,7 @@ class TCompletion < Formula
   url "https://github.com/sferik/t/archive/v3.1.0.tar.gz"
   sha256 "900ef6e3d6180b70bf2434503774ea5e1bf985b9110d4f051c44a191b08f6062"
   license "MIT"
-  head "https://github.com/sferik/t.git"
+  head "https://github.com/sferik/t.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "6735212e9fee5e04fce9adaa96e09c20cc7508b8d26e1d85658825b0c50e85c7"
