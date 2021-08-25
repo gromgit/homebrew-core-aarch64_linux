8f81b1b9b51b76589dfc3479a3845201f6f01ea3
fishtreesugar <fishtreesugar@gmail.com>
2021-08-25 23:27:09 +0800

    purescript 0.14.4
    * purescript 0.14.4
    * purescript: bump purescript-cst to 0.4.0.0
    
    Closes #83967.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -1,8 +1,8 @@
 class Purescript < Formula
   desc "Strongly typed programming language that compiles to JavaScript"
   homepage "https://www.purescript.org/"
-  url "https://hackage.haskell.org/package/purescript-0.14.3/purescript-0.14.3.tar.gz"
-  sha256 "2a82532d416d93d117ab942ab0b618642d788bf0dae09fc045bc1cc88ba71f3c"
+  url "https://hackage.haskell.org/package/purescript-0.14.4/purescript-0.14.4.tar.gz"
+  sha256 "730b0ef2d479c1655f4ed7b1515629fd76bfbad57563779bf45e6ce63d48aa61"
   license "BSD-3-Clause"
   head "https://github.com/purescript/purescript.git", branch: "master"
 
@@ -21,8 +21,8 @@ class Purescript < Formula
   uses_from_macos "zlib"
 
   resource "purescript-cst" do
-    url "https://hackage.haskell.org/package/purescript-cst-0.3.0.0/purescript-cst-0.3.0.0.tar.gz"
-    sha256 "c23ba1ef0714ff59c9e4bc7a74531f5e1422ebf616bdc3cbe9f6597cbb1bee95"
+    url "https://hackage.haskell.org/package/purescript-cst-0.4.0.0/purescript-cst-0.4.0.0.tar.gz"
+    sha256 "0f592230f528ce471a3d3ce44d85f4b96f2a08f5d6483edfe569679a322d6e64"
   end
 
   def install
