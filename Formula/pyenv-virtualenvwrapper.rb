91bebd9af351c4f35bcdb841edf3feb2a2ad3389
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:41 -0400

    pyenv-virtualenvwrapper: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pyenv-virtualenvwrapper.rb
+++ Formula/pyenv-virtualenvwrapper.rb
@@ -4,7 +4,7 @@ class PyenvVirtualenvwrapper < Formula
   url "https://github.com/pyenv/pyenv-virtualenvwrapper/archive/v20140609.tar.gz"
   sha256 "c1c812c4954394c58628952654ba745c4fb814d045adc076f7fb9e310bed03bf"
   license "MIT"
-  head "https://github.com/pyenv/pyenv-virtualenvwrapper.git"
+  head "https://github.com/pyenv/pyenv-virtualenvwrapper.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "c0c302d7010b5d22e5f452a8dacc52d6f899522d976d51763cbaa60c372add15"
