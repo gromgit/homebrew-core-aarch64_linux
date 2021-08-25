a1e10000240d06fbee2526f22c9b670c12e140c5
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:39 -0400

    rustc-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rustc-completion.rb
+++ Formula/rustc-completion.rb
@@ -2,7 +2,7 @@ class RustcCompletion < Formula
   desc "Bash completion for rustc"
   homepage "https://github.com/roshan/rust-bash-completion"
   license "MIT"
-  head "https://github.com/roshan/rust-bash-completion.git"
+  head "https://github.com/roshan/rust-bash-completion.git", branch: "master"
 
   stable do
     url "https://github.com/roshan/rust-bash-completion/archive/0.12.1.tar.gz"
