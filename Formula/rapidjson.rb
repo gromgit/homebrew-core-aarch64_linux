177509821fc2b87b39314c6a178ff7ebf8514ebe
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:27 -0400

    rapidjson: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rapidjson.rb
+++ Formula/rapidjson.rb
@@ -4,7 +4,7 @@ class Rapidjson < Formula
   url "https://github.com/miloyip/rapidjson/archive/v1.1.0.tar.gz"
   sha256 "bf7ced29704a1e696fbccf2a2b4ea068e7774fa37f6d7dd4039d0787f8bed98e"
   license "MIT"
-  head "https://github.com/miloyip/rapidjson.git"
+  head "https://github.com/miloyip/rapidjson.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "07acf5e89f30f9db3b69ad953bf4490ed02f6e5eec9d5f84987962132373aa6e"
