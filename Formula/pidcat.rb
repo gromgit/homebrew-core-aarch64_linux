171408f2bfb862ea9990b4ee2e42ddbce003e92c
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:29 -0400

    pidcat: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pidcat.rb
+++ Formula/pidcat.rb
@@ -4,7 +4,7 @@ class Pidcat < Formula
   url "https://github.com/JakeWharton/pidcat/archive/2.1.0.tar.gz"
   sha256 "e6f999ee0f23f0e9c9aee5ad21c6647fb1a1572063bdccd16a72464c8b522cb1"
   license "Apache-2.0"
-  head "https://github.com/JakeWharton/pidcat.git"
+  head "https://github.com/JakeWharton/pidcat.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "040e4e6968c1b152d7b25104e3b4cd27c86df790bc0d863f6ad7371c761d5386"
