7a9d09422a740e0976807abb0c4eb8ec2f382492
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:41 -0400

    pyenv-ccache: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pyenv-ccache.rb
+++ Formula/pyenv-ccache.rb
@@ -4,7 +4,7 @@ class PyenvCcache < Formula
   url "https://github.com/pyenv/pyenv-ccache/archive/v0.0.2.tar.gz"
   sha256 "ebfb8a5ed754df485b3f391078c5dc913f0587791a5e3815e61078f0db180b9e"
   license "MIT"
-  head "https://github.com/pyenv/pyenv-ccache.git"
+  head "https://github.com/pyenv/pyenv-ccache.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "8cb5b918ec485fd99f0af48056be2d00d2c1bf2055f7f5890dbf6335086be4b5"
