5d39b64b05a4b1ab3028b31d93bf2a963e812c21
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:29 -0400

    piknik: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/piknik.rb
+++ Formula/piknik.rb
@@ -4,7 +4,7 @@ class Piknik < Formula
   url "https://github.com/jedisct1/piknik/archive/0.10.1.tar.gz"
   sha256 "9172acb424d864ba3563bbdb0cd2307815129027eec1a6ca04aee17da7f936c2"
   license "BSD-2-Clause"
-  head "https://github.com/jedisct1/piknik.git"
+  head "https://github.com/jedisct1/piknik.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b503df3a16dfdf25219a598da5ae3c17676ee2367c0db0837403a6f728e4fcb4"
