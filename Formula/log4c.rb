18d516adab4819ea9965ed241ed1e87b35861977
Rui Chen <rui@chenrui.dev>
2021-08-25 14:10:50 -0400

    log4c: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/log4c.rb
+++ Formula/log4c.rb
@@ -4,7 +4,7 @@ class Log4c < Formula
   url "https://downloads.sourceforge.net/project/log4c/log4c/1.2.4/log4c-1.2.4.tar.gz"
   sha256 "5991020192f52cc40fa852fbf6bbf5bd5db5d5d00aa9905c67f6f0eadeed48ea"
   license "LGPL-2.1"
-  head "https://git.code.sf.net/p/log4c/log4c.git"
+  head "https://git.code.sf.net/p/log4c/log4c.git", branch: "master"
 
   livecheck do
     url :stable
