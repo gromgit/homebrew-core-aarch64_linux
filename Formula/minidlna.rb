94a1a0c4a39ee535bf0a482bddde6c10a6c0b735
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-25 06:14:23 +0000

    minidlna: update 1.3.0 bottle.

--- Formula/minidlna.rb
+++ Formula/minidlna.rb
@@ -6,10 +6,11 @@ class Minidlna < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e8123a9b2f7c100c538774cdb4114c0fdc44ea3a9a3a257e2f28db80a982c8ab"
-    sha256 cellar: :any, big_sur:       "3934b6e9bea9c3f74be9b163909eff0a6f697bdcd36c0fb5b8ac46793b7e362b"
-    sha256 cellar: :any, catalina:      "87538e0663825ec20c3d829db386fffeaf451df2f22f78845d92ff5dcad09a2e"
-    sha256 cellar: :any, mojave:        "f1dd29bb2e954ed3b842ad591f8673419b37c1d151add4dd91c05350cde0e51a"
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "3296fc2759c028ff9316c712504be6644eb5e1fb15155e2aea1f8c4bb609cc33"
+    sha256 cellar: :any, big_sur:       "92e3ced10e41d5528a06befe8124b0badde96b3542c094109c0d9b07315af0c4"
+    sha256 cellar: :any, catalina:      "e43528eb7559099f5dd38d278c75ac2555be3f28474c84a7005e5a16f920e528"
+    sha256 cellar: :any, mojave:        "fc24ceaaed86d9b4417349afa20ad6c86b765097314cc01bb2bb81d83a4c4639"
   end
 
   head do
