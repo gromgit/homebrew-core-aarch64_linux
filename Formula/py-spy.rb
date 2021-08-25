1d7eedaa6d53ec699446ab71b119a679c8b1f237
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:40 -0400

    py-spy: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/py-spy.rb
+++ Formula/py-spy.rb
@@ -4,7 +4,7 @@ class PySpy < Formula
   url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.8.tar.gz"
   sha256 "9dbfd0ea79ef31a2966891e86cf6238ed3831938cf562e71848e07b7009cf57d"
   license "MIT"
-  head "https://github.com/benfred/py-spy.git"
+  head "https://github.com/benfred/py-spy.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "22121c8eaec4a7cd913ae2add7287df36ad0e9a32f8f3a141faeef12401672cc"
