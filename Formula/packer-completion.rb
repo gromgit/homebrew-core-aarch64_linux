d0c6f35b15670f630568ca45008b6eb6fe1d56c3
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:20 -0400

    packer-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/packer-completion.rb
+++ Formula/packer-completion.rb
@@ -4,7 +4,7 @@ class PackerCompletion < Formula
   url "https://github.com/mrolli/packer-bash-completion/archive/1.4.3.tar.gz"
   sha256 "af7b3b49b29ffdb05b519dad2d83066f3d166dd8e29abd406ca0f3d480901df4"
   license "MIT"
-  head "https://github.com/mrolli/packer-bash-completion.git"
+  head "https://github.com/mrolli/packer-bash-completion.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e607f862efdc7c44bbf62da84a69d6986251af7e8a030809bcacc3b24804258c"
