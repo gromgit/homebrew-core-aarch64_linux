0d7c7e821835faa76b5737d9cabc30b78e1be88f
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:28 -0400

    rbenv-chefdk: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rbenv-chefdk.rb
+++ Formula/rbenv-chefdk.rb
@@ -5,7 +5,7 @@ class RbenvChefdk < Formula
   sha256 "79b48257f1a24085a680da18803ba6a94a1dd0cb25bd390629006a5fb67f3b69"
   license "MIT"
   revision 1
-  head "https://github.com/docwhat/rbenv-chefdk.git"
+  head "https://github.com/docwhat/rbenv-chefdk.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "43558c05b45ae34900651355579bbcac63771e75f98c501059233f4f1d832d5f"
