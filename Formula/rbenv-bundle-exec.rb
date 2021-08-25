a28aee090d7b88c4635faa4a0812faf2058b4013
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:28 -0400

    rbenv-bundle-exec: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rbenv-bundle-exec.rb
+++ Formula/rbenv-bundle-exec.rb
@@ -5,7 +5,7 @@ class RbenvBundleExec < Formula
   sha256 "2da08cbb1d8edecd1bcf68005d30e853f6f948c54ddb07bada67762032445cf3"
   license "MIT"
   revision 1
-  head "https://github.com/maljub01/rbenv-bundle-exec.git"
+  head "https://github.com/maljub01/rbenv-bundle-exec.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "025058645a7236199e0b2a4083ab8a1f540d48026ad69d3607e3f7abf07e61e9"
