c5d3b0673b53391b74a4e147263ef3c83ca8c547
Rui Chen <rui@chenrui.dev>
2021-08-25 00:37:10 -0400

    qpid-proton: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -5,7 +5,7 @@ class QpidProton < Formula
   mirror "https://archive.apache.org/dist/qpid/proton/0.35.0/qpid-proton-0.35.0.tar.gz"
   sha256 "a2d21cd9612dd79332ca18b794e372256e9b8eb12195cd8ffcb69043e32b5926"
   license "Apache-2.0"
-  head "https://gitbox.apache.org/repos/asf/qpid-proton.git"
+  head "https://gitbox.apache.org/repos/asf/qpid-proton.git", branch: "main"
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "5771a4a81c0ddaae9e6c8f1620c59c250d96af2eab7891989afa56117d2458d0"
