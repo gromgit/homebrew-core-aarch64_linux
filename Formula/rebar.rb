9846a7d36656086f49e192ff883fb9471feee40c
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:31 -0400

    rebar: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rebar.rb
+++ Formula/rebar.rb
@@ -4,7 +4,7 @@ class Rebar < Formula
   url "https://github.com/rebar/rebar/archive/2.6.4.tar.gz"
   sha256 "577246bafa2eb2b2c3f1d0c157408650446884555bf87901508ce71d5cc0bd07"
   license "Apache-2.0"
-  head "https://github.com/rebar/rebar.git"
+  head "https://github.com/rebar/rebar.git", branch: "master"
 
   bottle do
     rebuild 2
