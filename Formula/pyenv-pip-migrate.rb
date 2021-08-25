f6310b7b2ae36fcc8cca8a44eae7e15cde4268fe
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:41 -0400

    pyenv-pip-migrate: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pyenv-pip-migrate.rb
+++ Formula/pyenv-pip-migrate.rb
@@ -4,7 +4,7 @@ class PyenvPipMigrate < Formula
   url "https://github.com/pyenv/pyenv-pip-migrate/archive/v20181205.tar.gz"
   sha256 "c064c76b854fa905c40e71b5223699bacf18ca492547aad93cdde2b98ca4e58c"
   license "MIT"
-  head "https://github.com/pyenv/pyenv-pip-migrate.git"
+  head "https://github.com/pyenv/pyenv-pip-migrate.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a8b8a8f1c522ac7425a4ada017386a97558038c858fe209e60cd1392e6c0ea6c"
