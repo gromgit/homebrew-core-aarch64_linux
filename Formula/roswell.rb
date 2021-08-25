57c35b0a79312faf82547e9700b4bd421234439a
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:36 -0400

    roswell: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -4,7 +4,7 @@ class Roswell < Formula
   url "https://github.com/roswell/roswell/archive/v21.06.14.110.tar.gz"
   sha256 "4649a495e55e3e04e3dae5890d318a170525cb01714661c412429f9503aeefd5"
   license "MIT"
-  head "https://github.com/roswell/roswell.git"
+  head "https://github.com/roswell/roswell.git", branch: "master"
 
   bottle do
     sha256 arm64_big_sur: "a175b1f7c4f29305cd4ae65c2dc516d2a017e1afa6bf3111793b9ca46923e4bf"
