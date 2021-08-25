93c45f0765245b88994b2c9df92b541cba68efa1
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:35 -0400

    rmate: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rmate.rb
+++ Formula/rmate.rb
@@ -4,7 +4,7 @@ class Rmate < Formula
   url "https://github.com/textmate/rmate/archive/v1.5.8.tar.gz"
   sha256 "40be07ae251bfa47b408eb56395dd2385d8e9ea220a19efd5145593cd8cbd89c"
   license "MIT"
-  head "https://github.com/textmate/rmate.git"
+  head "https://github.com/textmate/rmate.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f17a839cae2678598895cdbd94ecceb0197ddd0c6e94e17768a5580c3cf9cc98"
