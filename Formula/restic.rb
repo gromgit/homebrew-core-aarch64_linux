96fb7abac6ba25775e90bf5a33c01234727b6c84
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:33 -0400

    restic: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/restic.rb
+++ Formula/restic.rb
@@ -4,7 +4,7 @@ class Restic < Formula
   url "https://github.com/restic/restic/archive/v0.12.1.tar.gz"
   sha256 "a9c88d5288ce04a6cc78afcda7590d3124966dab3daa9908de9b3e492e2925fb"
   license "BSD-2-Clause"
-  head "https://github.com/restic/restic.git"
+  head "https://github.com/restic/restic.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "c42f5dc067e1f3d2fc001bceb623564bff4b2bc692c486e2bc69911fc4db932e"
