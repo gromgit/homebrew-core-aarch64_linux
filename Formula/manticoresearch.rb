ea0839f4f8b6d1b8679e35f839c0d50e6051a836
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-25 06:14:27 +0000

    manticoresearch: update 3.6.0 bottle.

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -9,10 +9,11 @@ class Manticoresearch < Formula
   head "https://github.com/manticoresoftware/manticoresearch.git"
 
   bottle do
-    sha256 arm64_big_sur: "30452ef44c28fa988ed5b8a42b1ef319cad80449e1ee94761b97dd601529faf6"
-    sha256 big_sur:       "97f2d35c667bbf98e830104523659fa070163dca549a1a4437b2cf58d3d31c1d"
-    sha256 catalina:      "1f4bab27f0f29378e6b4edf3b989361d5b0169c4cc06490dafc9afec38b87a7c"
-    sha256 mojave:        "5a6d4ec64e5f6cd8a2a8041b7823a120ee1333a779a3cae11e13972edc89129f"
+    rebuild 1
+    sha256 arm64_big_sur: "e285345ff7b6a8a99c0cbac69232301a6ad97cd09abae93bfffce5d3885f6ac2"
+    sha256 big_sur:       "11dfe819517e1b8c379d761ed9391192bb585564588a0ee23f85880a4b7e2f8c"
+    sha256 catalina:      "77c342a7a0d4a7c786b2c777b92522ab8b9eae7806074b0f81112cd7890c4d91"
+    sha256 mojave:        "f20768ffb1961f40fe39b3bebd20491f31cd2d91f9c43c4b5897458a50354a06"
   end
 
   depends_on "boost" => :build
