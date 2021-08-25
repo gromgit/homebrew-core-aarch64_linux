5e31a17aa7f111f6ce280dcae4a2a52a2a234076
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:28 -0400

    rbenv-bundler-ruby-version: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rbenv-bundler-ruby-version.rb
+++ Formula/rbenv-bundler-ruby-version.rb
@@ -5,7 +5,7 @@ class RbenvBundlerRubyVersion < Formula
   sha256 "96c6b7eb191d436142fef0bb8c28071d54aca3e1a10ca01a525d1066699b03f2"
   license "Unlicense"
   revision 1
-  head "https://github.com/aripollak/rbenv-bundler-ruby-version.git"
+  head "https://github.com/aripollak/rbenv-bundler-ruby-version.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a08de996e3e539b9f48812d60fac7195ef5f65e20025ea5f7dec173ad8fe8ba3"
