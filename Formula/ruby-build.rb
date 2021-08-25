fc6a3d57efd37a9947ec53828f8686f5db3c8ae7
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:38 -0400

    ruby-build: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -4,7 +4,7 @@ class RubyBuild < Formula
   url "https://github.com/rbenv/ruby-build/archive/v20210825.tar.gz"
   sha256 "9bc4f44303ab2fe454d4e3a173929a5bd59b0fda75a829e00bafb321c1476d30"
   license "MIT"
-  head "https://github.com/rbenv/ruby-build.git"
+  head "https://github.com/rbenv/ruby-build.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "769348cb7868f53488c857bfd0b04e67564270249a7aa7eafe8335c24cdd4906"
