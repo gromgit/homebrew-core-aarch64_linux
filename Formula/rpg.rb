567c067c20ff282d00cb9ea7d985f08aa48154d6
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:37 -0400

    rpg: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rpg.rb
+++ Formula/rpg.rb
@@ -4,7 +4,7 @@ class Rpg < Formula
   url "https://github.com/downloads/rtomayko/rpg/rpg-0.3.0.tar.gz"
   sha256 "c350f64744fb602956a91a57c8920e69058ea42e4e36b0e74368e96954d9d0c7"
   license "MIT"
-  head "https://github.com/rtomayko/rpg.git"
+  head "https://github.com/rtomayko/rpg.git", branch: "master"
 
   bottle do
     rebuild 1
