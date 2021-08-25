07de907993d1ca4bfe5a3097bad1bd7324c19a55
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:28 -0400

    pgtune: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pgtune.rb
+++ Formula/pgtune.rb
@@ -8,7 +8,7 @@ class Pgtune < Formula
   license "BSD-3-Clause"
 
   # 0.9.3 does not have settings for PostgreSQL 9.x, but the trunk does
-  head "https://github.com/gregs1104/pgtune.git"
+  head "https://github.com/gregs1104/pgtune.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "99d46ab0880d22bb3a19faf759bde25d51dd0e4c5c1890d6bf0e253a9042e09f"
