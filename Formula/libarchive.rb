6356077f639d59815f42248bd05715eae76ab0fb
Caleb Xu <calebcenter@live.com>
2021-08-25 09:42:30 -0400

    libarchive 3.5.2
    Closes #83958.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -1,10 +1,9 @@
 class Libarchive < Formula
   desc "Multi-format archive and compression library"
   homepage "https://www.libarchive.org"
-  url "https://www.libarchive.org/downloads/libarchive-3.5.1.tar.xz"
-  sha256 "0e17d3a8d0b206018693b27f08029b598f6ef03600c2b5d10c94ce58692e299b"
+  url "https://www.libarchive.org/downloads/libarchive-3.5.2.tar.xz"
+  sha256 "f0b19ff39c3c9a5898a219497ababbadab99d8178acc980155c7e1271089b5a0"
   license "BSD-2-Clause"
-  revision 1
 
   livecheck do
     url "https://libarchive.org/downloads/"
