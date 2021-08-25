a10fb75e136648bd27fd5b9bcaf22e196be1a7f9
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:26 -0400

    r3: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r3.rb
+++ Formula/r3.rb
@@ -4,7 +4,7 @@ class R3 < Formula
   url "https://github.com/c9s/r3/archive/1.3.4.tar.gz"
   sha256 "db1fb91e51646e523e78b458643c0250231a2640488d5781109f95bd77c5eb82"
   license "MIT"
-  head "https://github.com/c9s/r3.git"
+  head "https://github.com/c9s/r3.git", branch: "master"
 
   livecheck do
     url :stable
