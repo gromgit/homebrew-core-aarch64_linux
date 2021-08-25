a9b97929f27f21bf72e8992a8da1b5c15519e40a
Rui Chen <rui@chenrui.dev>
2021-08-25 01:06:37 -0400

    psgrep: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/psgrep.rb
+++ Formula/psgrep.rb
@@ -4,7 +4,7 @@ class Psgrep < Formula
   url "https://github.com/jvz/psgrep/archive/1.0.9.tar.gz"
   sha256 "6408e4fc99414367ad08bfbeda6aa86400985efe1ccb1a1f00f294f86dd8b984"
   license "GPL-3.0"
-  head "https://github.com/jvz/psgrep.git"
+  head "https://github.com/jvz/psgrep.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "3ec88e4f8662da264f6312ba3d454887825f21285f39c926f9f2d80f5b035d02"
