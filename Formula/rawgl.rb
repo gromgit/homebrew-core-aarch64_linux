a1b102332eff5c07f8b137d99db4db2a206a7076
Rui Chen <rui@chenrui.dev>
2021-08-25 00:26:27 -0400

    rawgl: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rawgl.rb
+++ Formula/rawgl.rb
@@ -3,7 +3,7 @@ class Rawgl < Formula
   homepage "https://github.com/cyxx/rawgl"
   url "https://github.com/cyxx/rawgl/archive/rawgl-0.2.1.tar.gz"
   sha256 "da00d4abbc266e94c3755c2fd214953c48698826011b1d4afbebb99396240073"
-  head "https://github.com/cyxx/rawgl.git"
+  head "https://github.com/cyxx/rawgl.git", branch: "master"
 
   bottle do
     rebuild 1
