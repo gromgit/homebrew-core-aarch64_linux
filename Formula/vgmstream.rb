f4a31ca2fdd4170e9be8f008b069eb3693ce4da1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-26 03:13:29 +0000

    vgmstream: update r1050-3448-g77cc431b_2 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -17,10 +17,11 @@ class Vgmstream < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "f344401ea028c6fced781b98573acc97648380cbb3da37fccb614543528d58b3"
-    sha256 big_sur:       "a8b9e590e143c8a5820562376a4d8d6455b4aa3719d69134182ccdbe6e2bc940"
-    sha256 catalina:      "ea5a421a93602621a8bf2a62b2eca9affa50790f16d7153bd3f901ef3edd9d9a"
-    sha256 mojave:        "1641ceee1b1849446b3aa2c1ccd07241c1641c9546fff3f785ae5f842b695fcc"
+    sha256                               arm64_big_sur: "f344401ea028c6fced781b98573acc97648380cbb3da37fccb614543528d58b3"
+    sha256                               big_sur:       "a8b9e590e143c8a5820562376a4d8d6455b4aa3719d69134182ccdbe6e2bc940"
+    sha256                               catalina:      "ea5a421a93602621a8bf2a62b2eca9affa50790f16d7153bd3f901ef3edd9d9a"
+    sha256                               mojave:        "1641ceee1b1849446b3aa2c1ccd07241c1641c9546fff3f785ae5f842b695fcc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "198d6a243a89c0019a1beafbe060c5e8ac361af28eb6e3d396bc5d8ea71af7a5"
   end
 
   depends_on "cmake" => :build
