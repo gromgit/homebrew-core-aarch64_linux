b9ac4c8340779869cec0c1d84a0d521f3792ef42
Branch Vincent <branchevincent@gmail.com>
2021-08-26 19:03:30 -0400

    typescript 4.4.2
    Closes #84063.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Typescript < Formula
   desc "Language for application scale JavaScript development"
   homepage "https://www.typescriptlang.org/"
-  url "https://registry.npmjs.org/typescript/-/typescript-4.3.5.tgz"
-  sha256 "c7be550da858be7abfc73dd0b9060ab23ce835ae3b05931f4500a25c09766d45"
+  url "https://registry.npmjs.org/typescript/-/typescript-4.4.2.tgz"
+  sha256 "6de61b7a3a79680b0de4c77a22eecf0dad7dfab5e9301894bfea452c5b9b4b25"
   license "Apache-2.0"
   head "https://github.com/Microsoft/TypeScript.git", branch: "main"
 
