eae55bb295c7f3b18b4c7c99cce811ca36a645e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-26 22:07:22 +0000

    ssh-audit: update 2.5.0 bottle.

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -9,11 +9,11 @@ class SshAudit < Formula
   head "https://github.com/jtesta/ssh-audit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8bb5287d63d9ac17380fd712b1e263b13f88cff456685bb1f34a21812491f807"
-    sha256 cellar: :any_skip_relocation, big_sur:       "68ef1fb06d02ac84edd2409a4988353684279f717dc9678411ac3a6eec37b12e"
-    sha256 cellar: :any_skip_relocation, catalina:      "03faa5f6e250751b1247704617e78e7e4a27387d7c2c586a3f373576898cfc81"
-    sha256 cellar: :any_skip_relocation, mojave:        "f01d8bd0cb8b9d461c8b9f93b765def198756cacbe623a8345efabf93a9984f9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "926a05db7d305de243e55d97031b12c8f6f16f6249442743227ce9a89b445057"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5aa1b2e66cbfe400cefe9cec235accee9e70cf66f9816dffbca2ae483c4c40d4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4470233747b0de7046aad58f217f918b7020b3aa1b965bb572b0d5a72aadd35c"
+    sha256 cellar: :any_skip_relocation, catalina:      "4470233747b0de7046aad58f217f918b7020b3aa1b965bb572b0d5a72aadd35c"
+    sha256 cellar: :any_skip_relocation, mojave:        "4470233747b0de7046aad58f217f918b7020b3aa1b965bb572b0d5a72aadd35c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f6daec517b07e1a575ea7ae6f77f4f2ca6d5b50ae10b8013166682db38a514c"
   end
 
   depends_on "python@3.9"
