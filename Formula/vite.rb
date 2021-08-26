ce0081f2276b9ed883a4fcdd4422ce45963dd581
Rui Chen <rui@chenrui.dev>
2021-08-26 17:36:37 -0400

    vite 2.5.1 (new formula)
    ```
    vite:
      * Unexpected universal binaries were found.
        The offending files are:
          /usr/local/Cellar/vite/2.5.1/libexec/lib/node_modules/vite/node_modules/fsevents/fsevents.node
    ```
    
    Closes #84057.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/vite.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class Vite < Formula
+  desc "Next generation frontend tooling. It's fast!"
+  homepage "https://vitejs.dev/"
+  url "https://registry.npmjs.org/vite/-/vite-2.5.1.tgz"
+  sha256 "195adabc6953545ba0314f38467adc849ed09a1b7f709e78546bed5613dee411"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    deuniversalize_machos
+  end
+
+  test do
+    port = free_port
+    fork do
+      system bin/"vite", "preview", "--port", port
+    end
+    sleep 2
+    assert_match "Cannot GET /", shell_output("curl -s localhost:#{port}")
+  end
+end
