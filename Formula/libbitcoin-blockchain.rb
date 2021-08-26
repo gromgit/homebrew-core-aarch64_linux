aa6c1b2865f90a83d7edd1c781a49cfb6a5acfe3
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-08-26 16:55:24 +0000

    libbitcoin-blockchain: update 3.6.0_1 bottle.

--- Formula/libbitcoin-blockchain.rb
+++ Formula/libbitcoin-blockchain.rb
@@ -8,10 +8,11 @@ class LibbitcoinBlockchain < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "b6c7eba40fc345969eda3c9c0aee34b4f2b460cece3e9c1110a9cc9d3ed4ad72"
-    sha256 cellar: :any, big_sur:       "d68b78c0430f3610f43395bcf12633ab7fc037f3bc874b83fd456fa6a3cb38a6"
-    sha256 cellar: :any, catalina:      "40ef8e8935d75f1c894852305b586fc922425467b6955813c2327d0ac901005f"
-    sha256 cellar: :any, mojave:        "ce1a34b80aa5d9c404c27c2282bf1e654b48999e74e4facc4dff70bf8087a514"
+    sha256 cellar: :any,                 arm64_big_sur: "b6c7eba40fc345969eda3c9c0aee34b4f2b460cece3e9c1110a9cc9d3ed4ad72"
+    sha256 cellar: :any,                 big_sur:       "d68b78c0430f3610f43395bcf12633ab7fc037f3bc874b83fd456fa6a3cb38a6"
+    sha256 cellar: :any,                 catalina:      "40ef8e8935d75f1c894852305b586fc922425467b6955813c2327d0ac901005f"
+    sha256 cellar: :any,                 mojave:        "ce1a34b80aa5d9c404c27c2282bf1e654b48999e74e4facc4dff70bf8087a514"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "71dfd2b32aea36f35b7d57e6d166a23ef3f1eb383ab807705ab8eaa97dcbe121"
   end
 
   depends_on "autoconf" => :build
