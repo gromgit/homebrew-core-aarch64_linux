356dc6f78059f1706bc8c6c44545c262dca43c3e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-26 03:09:13 +0000

    mpv: update 0.33.1 bottle.

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -11,6 +11,7 @@ class Mpv < Formula
     sha256 big_sur:       "76b0fc9d207aee16f65b8b1782bc35dec5a870952ebba6ae7a74e6ede9bdd34a"
     sha256 catalina:      "8ab98fffc330dea03f2732fa17c7f53753601c49c3f9dec2a7d727bdc901c484"
     sha256 mojave:        "87df95e8f4f723a5b6fe163d5ac740fef9f5ffa9c318c82c9a6b0844aa7203b9"
+    sha256 x86_64_linux:  "6a037de38402f73783c5b403e6aada7388373eda6593b21e43844043cf6496d6"
   end
 
   depends_on "docutils" => :build
