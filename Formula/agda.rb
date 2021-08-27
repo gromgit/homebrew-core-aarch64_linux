b597f77dc9752ecfb55503816926b146218340ff
fishtreesugar <fishtreesugar@gmail.com>
2021-08-28 02:01:01 +0800

    agda: revision bump (ghc 8.10.7)

--- Formula/agda.rb
+++ Formula/agda.rb
@@ -2,7 +2,7 @@ class Agda < Formula
   desc "Dependently typed functional programming language"
   homepage "https://wiki.portal.chalmers.se/agda/"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   stable do
     url "https://hackage.haskell.org/package/Agda-2.6.2/Agda-2.6.2.tar.gz"
