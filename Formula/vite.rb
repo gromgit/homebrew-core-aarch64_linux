28877e535a888b40a54d545c6f4d753c93943790
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-08-27 03:37:49 +0000

    vite: add 2.5.1 bottle.

--- Formula/vite.rb
+++ Formula/vite.rb
@@ -7,6 +7,14 @@ class Vite < Formula
   sha256 "195adabc6953545ba0314f38467adc849ed09a1b7f709e78546bed5613dee411"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "47a9f96b9a3fc1ca21a5d3771578aad60b23fc269a291519e74a950e5a88d0aa"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ac643e4ae177c2e2f26bb8f9c54f44c7680d45b27de300644fbe3606f743de7b"
+    sha256 cellar: :any_skip_relocation, catalina:      "ac643e4ae177c2e2f26bb8f9c54f44c7680d45b27de300644fbe3606f743de7b"
+    sha256 cellar: :any_skip_relocation, mojave:        "ac643e4ae177c2e2f26bb8f9c54f44c7680d45b27de300644fbe3606f743de7b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b9ef8d4c447f99f0f3d829953a4d29e957ff25252ad5bc946d3aeb1349b6af5"
+  end
+
   depends_on "node"
 
   def install
