c450908417553c62a136f2b6600a195d8d7d7457
Branch Vincent <branchevincent@gmail.com>
2021-08-27 23:04:10 -0400

    libssh 0.9.6
    Closes #84124.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libssh.rb
+++ Formula/libssh.rb
@@ -1,10 +1,9 @@
 class Libssh < Formula
   desc "C library SSHv1/SSHv2 client and server protocols"
   homepage "https://www.libssh.org/"
-  url "https://www.libssh.org/files/0.9/libssh-0.9.5.tar.xz"
-  sha256 "acffef2da98e761fc1fd9c4fddde0f3af60ab44c4f5af05cd1b2d60a3fa08718"
+  url "https://www.libssh.org/files/0.9/libssh-0.9.6.tar.xz"
+  sha256 "86bcf885bd9b80466fe0e05453c58b877df61afa8ba947a58c356d7f0fab829b"
   license "LGPL-2.1-or-later"
-  revision 1
   head "https://git.libssh.org/projects/libssh.git"
 
   bottle do
