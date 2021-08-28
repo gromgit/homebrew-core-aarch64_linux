85ab5fa730cd7ca5f61afefd30d7162951ea468c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-28 04:07:36 +0000

    rancher-cli: update 2.4.12 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -12,11 +12,11 @@ class RancherCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd57269ac33e17ddbf907d18ef7ef177800689bf4790c48e83d56d6652d9104e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "edfa744550563d18207ef12d174131f7ba0486711bd7ccb252f12850efd2ae81"
-    sha256 cellar: :any_skip_relocation, catalina:      "bfb880e1663556643a7d6c687e2d0d51fefe1ed36163d66d42274046d80f1862"
-    sha256 cellar: :any_skip_relocation, mojave:        "ec162d2372ff7bbc244d2785e33b849fbd4773f37322b586bcc20f2aa9ad08a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c17d2305a33ff71e15d2bf513e875c124e09b6ec7aace4ef536104ee84a36ae8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "81f0d67c3cc237d67246ed1e0b9f74c25365e907e45f272fa642d012170657df"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6579774482291cf6fc8bb1bdae68335e248e4ce2a35c858645f1d7e86164731e"
+    sha256 cellar: :any_skip_relocation, catalina:      "02ad8e0cf289919361a10c7021d8b730d447753a2a2aa5ed2605da10047021ca"
+    sha256 cellar: :any_skip_relocation, mojave:        "3241f896a3d9d31be2053ccbdfccb1d6e817318c65a6c17008dd0042f37de2ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a7812410b2f76b8d5a9df9cefb53a8b0db2feb37de6fce3758416a6aab79d9e"
   end
 
   depends_on "go" => :build
