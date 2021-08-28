eeac6d343e9ff209df877d43b28e550eeffef44a
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-08-28 23:43:33 +0000

    gptfdisk: update 1.0.8 bottle.

--- Formula/gptfdisk.rb
+++ Formula/gptfdisk.rb
@@ -6,10 +6,11 @@ class Gptfdisk < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9ddfc62f39c786868b5bcafb0cc949a89977ece0bf27eac038a70dbcd7772b8f"
-    sha256 cellar: :any, big_sur:       "a16cd2748dcf4ce4a18caf1d09e04e077a456fe323553685ab07dc7b628567a7"
-    sha256 cellar: :any, catalina:      "e5c8a8a789a75e2ff5cd3120922c0fa205ef3e9aec23fd77558a04b349283aea"
-    sha256 cellar: :any, mojave:        "8ea2978e8d5612e21cef00d747ac24e0c5f44eeb5c9c2edcf926752bd389523a"
+    sha256 cellar: :any,                 arm64_big_sur: "9ddfc62f39c786868b5bcafb0cc949a89977ece0bf27eac038a70dbcd7772b8f"
+    sha256 cellar: :any,                 big_sur:       "a16cd2748dcf4ce4a18caf1d09e04e077a456fe323553685ab07dc7b628567a7"
+    sha256 cellar: :any,                 catalina:      "e5c8a8a789a75e2ff5cd3120922c0fa205ef3e9aec23fd77558a04b349283aea"
+    sha256 cellar: :any,                 mojave:        "8ea2978e8d5612e21cef00d747ac24e0c5f44eeb5c9c2edcf926752bd389523a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e93c2383a78abc4ea044c1d750630342118f7257358421c04471efd94c32cbce"
   end
 
   depends_on "popt"
