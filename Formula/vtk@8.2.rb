782d8c1fe6190fe7d93d50952342123e2f87ea6f
Michka Popoff <michkapopoff@gmail.com>
2021-08-28 22:52:03 +0200

    vtk@8.2: add linux dependencies
    Closes #84167.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vtk@8.2.rb
+++ Formula/vtk@8.2.rb
@@ -17,7 +17,7 @@ class VtkAT82 < Formula
 
   deprecate! date: "2020-05-14", because: :versioned_formula
 
-  depends_on "cmake" => :build
+  depends_on "cmake" => [:build, :test]
   depends_on "boost"
   depends_on "fontconfig"
   depends_on "hdf5"
@@ -29,14 +29,20 @@ class VtkAT82 < Formula
   depends_on "python@3.9"
   depends_on "qt@5"
 
+  uses_from_macos "expat"
+  uses_from_macos "libxml2"
   uses_from_macos "tcl-tk"
+  uses_from_macos "zlib"
 
   on_linux do
+    depends_on "gcc"
     depends_on "icu4c"
     depends_on "libxt"
     depends_on "szip"
     depends_on "mesa-glu"
   end
+
+  fails_with gcc: "5"
 
   # TODO: use diff
   # Fix compile issues on Mojave and later
@@ -57,9 +63,19 @@ class VtkAT82 < Formula
     sha256 "cb3b3a0e6978889a9cb95be35f3d4a6928397d3b843ab72ecaaf96554c6d4fc7"
   end
 
+  # GCC 11 support
+  patch do
+    url "https://gitlab.kitware.com/vtk/vtk/-/commit/c83b583cc06e375c320a4980d2104b8d1e7fbfde.diff"
+    sha256 "becf3a21c6f768dc378b598eb87a0e11258cdb8bdd69930784696823408498c6"
+  end
+
   def install
     # Do not record compiler path because it references the shim directory
     inreplace "Common/Core/vtkConfigure.h.in", "@CMAKE_CXX_COMPILER@", "clang++"
+
+    # Fix build with GCC 10 or newer
+    # Adapted from https://bugs.gentoo.org/attachment.cgi?id=641488&action=diff
+    on_linux { inreplace "CMake/VTKGenerateExportHeader.cmake", "[3-9]", "[1-9][0-9]" }
 
     pyver = Language::Python.major_minor_version "python3"
     args = std_cmake_args + %W[
@@ -115,28 +131,48 @@ class VtkAT82 < Formula
     # Prevent dependents from using fragile Cellar paths
     inreplace_cmake_modules = [
       lib/"cmake/vtk-#{version.major_minor}/VTKConfig.cmake",
-      lib/"cmake/vtk-#{version.major_minor}/VTKTargets-release.cmake",
       lib/"cmake/vtk-#{version.major_minor}/Modules/vtkPython.cmake",
     ]
+    on_macos { inreplace_cmake_modules << lib/"cmake/vtk-#{version.major_minor}/VTKTargets-release.cmake" }
+
     inreplace inreplace_cmake_modules, prefix, opt_prefix
   end
 
   test do
-    vtk_include = Dir[opt_include/"vtk-*"].first
-    major, minor = vtk_include.match(/.*-(.*)$/)[1].split(".")
+    (testpath/"CMakeLists.txt").write <<~EOS
+      set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
+      set(CMAKE_INSTALL_RPATH "#{Formula["vtk@8.2"].opt_lib}")
+      cmake_minimum_required(VERSION 3.3 FATAL_ERROR)
+      project(Distance2BetweenPoints LANGUAGES CXX)
+      find_package(VTK REQUIRED COMPONENTS vtkCommonCore CONFIG)
+      add_executable(Distance2BetweenPoints Distance2BetweenPoints.cxx)
+      target_link_libraries(Distance2BetweenPoints PRIVATE ${VTK_LIBRARIES})
+    EOS
 
-    (testpath/"version.cpp").write <<~EOS
-      #include <vtkVersion.h>
-      #include <assert.h>
-      int main(int, char *[]) {
-        assert (vtkVersion::GetVTKMajorVersion()==#{major});
-        assert (vtkVersion::GetVTKMinorVersion()==#{minor});
-        return EXIT_SUCCESS;
+    (testpath/"Distance2BetweenPoints.cxx").write <<~EOS
+      #include <cassert>
+      #include <vtkMath.h>
+      int main() {
+        double p0[3] = {0.0, 0.0, 0.0};
+        double p1[3] = {1.0, 1.0, 1.0};
+        assert(vtkMath::Distance2BetweenPoints(p0, p1) == 3.0);
+        return 0;
       }
     EOS
 
-    system ENV.cxx, "-std=c++11", "version.cpp", "-I#{vtk_include}"
-    system "./a.out"
-    system "#{bin}/vtkpython", "-c", "exit()"
+    vtk_dir = Dir[opt_lib/"cmake/vtk-*"].first
+    system "cmake", "-DCMAKE_BUILD_TYPE=Debug", "-DCMAKE_VERBOSE_MAKEFILE=ON",
+      "-DVTK_DIR=#{vtk_dir}", "."
+    system "make"
+    system "./Distance2BetweenPoints"
+
+    (testpath/"Distance2BetweenPoints.py").write <<~EOS
+      import vtk
+      p0 = (0, 0, 0)
+      p1 = (1, 1, 1)
+      assert vtk.vtkMath.Distance2BetweenPoints(p0, p1) == 3
+    EOS
+
+    system bin/"vtkpython", "Distance2BetweenPoints.py"
   end
 end
