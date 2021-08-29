3e3009a06482f3b80512a71a5edbf51b53905692
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-29 01:25:22 +0000

    poppler-qt5: update 21.08.0 bottle.

--- Formula/poppler-qt5.rb
+++ Formula/poppler-qt5.rb
@@ -15,6 +15,7 @@ class PopplerQt5 < Formula
     sha256 big_sur:       "b90868c632e319845acf6412e280fd9ba07fa25b986415886ccfa95ee69e065c"
     sha256 catalina:      "7cd81e1ff43fbad733203084ea386bc1d7ecb04a29d958dd873592142dd161be"
     sha256 mojave:        "73d297ab2e685cb9af25373bf6d19fda06d1ecd20f9bcdd2f73abbc6127b79d0"
+    sha256 x86_64_linux:  "a19e3989ba8f77516a80d71562b5718a1a42b876c413b4017471d54896fc56e4"
   end
 
   keg_only "it conflicts with poppler"
