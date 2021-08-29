0b3f456cea152486fd7cbe25b2ec1b58561bc7d6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-29 01:17:59 +0000

    pyqt@5: update 5.15.4_2 bottle.

--- Formula/pyqt@5.rb
+++ Formula/pyqt@5.rb
@@ -11,6 +11,7 @@ class PyqtAT5 < Formula
     sha256 cellar: :any, big_sur:       "57e1fd479cc069392daab557c213607bcb45fae6494fce0898803be3696bb24b"
     sha256 cellar: :any, catalina:      "70bc904d9efee5ec5a2e6eec8d5cf7daf1d96790f13b41fa4027d42f62ebd6d4"
     sha256 cellar: :any, mojave:        "d824441486b767137fcad5620117299a7e3dd1423726a31c637cde4b1fcf2d39"
+    sha256               x86_64_linux:  "7666898e0b34c8e2c204a9ee8a9d50c8daf0f8a82522d77a65e15d639468196f"
   end
 
   depends_on "pyqt-builder" => :build
