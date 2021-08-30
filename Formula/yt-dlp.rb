2c0ea29493f9e923e7decd455e3c0bfd7b0dbc51
Kid <44045911+kidonng@users.noreply.github.com>
2021-08-30 19:41:21 +0800

    yt-dlp: rebuild
    * yt-dlp: switch to pypi package
    * Fix revision position
    * Remove unnecessary revision
    
    Closes #84251.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yt-dlp.rb
+++ Formula/yt-dlp.rb
@@ -1,16 +1,12 @@
 class YtDlp < Formula
+  include Language::Python::Virtualenv
+
   desc "Fork of youtube-dl with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://github.com/yt-dlp/yt-dlp/archive/2021.08.10.tar.gz"
-  sha256 "d847e32bc18767be2f27183f5da2ba05d0142e194e95ac6b3e69494353ee366b"
+  url "https://files.pythonhosted.org/packages/ee/59/d763a51fa975639946c327ffbf85e109dbaa87f8aa5cae54a4ee5d09593c/yt-dlp-2021.8.10.tar.gz"
+  sha256 "8da1bf4dc4641d37d137443c4783109ee8393caad5e0d270d9d1d534e8f25240"
   license "Unlicense"
   head "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-    regex(%r{href=.*?/tag/v?(\d+(?:[.-]\d+)+)["' >]}i)
-  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "96d6684e34d20fa121b4bad4c0706f024bb1a406ece55db3a188e0e4bd8f08c5"
@@ -20,17 +16,14 @@ class YtDlp < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b6e65096ceac3c5270ad48b2e5f8381317dbbbe687052f1003b55d927fc6454"
   end
 
-  depends_on "pandoc" => :build
   depends_on "python@3.9"
-  uses_from_macos "zip" => :build
 
   def install
-    system "make </dev/null"
-    bin.install "yt-dlp"
-    bash_completion.install "completions/bash/yt-dlp"
-    zsh_completion.install "completions/zsh/_yt-dlp"
-    fish_completion.install "completions/fish/yt-dlp.fish"
-    man1.install "yt-dlp.1"
+    virtualenv_install_with_resources
+    man1.install_symlink libexec/"share/man/man1/yt-dlp.1"
+    bash_completion.install libexec/"share/bash-completion/completions/yt-dlp"
+    zsh_completion.install libexec/"share/zsh/site-functions/_yt-dlp"
+    fish_completion.install libexec/"share/fish/vendor_completions.d/yt-dlp.fish"
   end
 
   test do
