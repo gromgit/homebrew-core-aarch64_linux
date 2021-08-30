42d8288f0d8b0c95c17d39d1094b9d7e47871ae4
Sean Molenaar <smillerdev@me.com>
2021-08-30 15:00:37 +0200

    acl2: revision bump for sbcl 2.1.8
    Closes #84254.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/acl2.rb
+++ Formula/acl2.rb
@@ -4,7 +4,7 @@ class Acl2 < Formula
   url "https://github.com/acl2/acl2/archive/8.3.tar.gz"
   sha256 "45eedddb36b2eff889f0dba2b96fc7a9b1cf23992fcfdf909bc179f116f2c5ea"
   license "BSD-3-Clause"
-  revision 10
+  revision 11
 
   bottle do
     sha256 big_sur:      "f63fe946638a7ebd051c5594144ae4ea6248c360b9aa13d6f97a18406568f94e"
