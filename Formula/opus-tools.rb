8a7d163d151156a2f442beb70955822128328532
Michael Cho <20700669+cho-m@users.noreply.github.com>
2021-08-31 15:50:56 +0000

    opus-tools: update 0.2 bottle.

--- Formula/opus-tools.rb
+++ Formula/opus-tools.rb
@@ -7,11 +7,12 @@ class OpusTools < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "33c1b089709532f82978e0f0fade8bbbf0c56ef07c81d7d22f8e8b649d0c72ca"
-    sha256 cellar: :any, big_sur:       "437d89ccde33e47708506a235fd68e2a3745d7848498dd1ce72e8c77a1e74dcc"
-    sha256 cellar: :any, catalina:      "964b3493cffeb5e32411c7a34e1813e8e83e940419aca39f50be7db9b0c8fab2"
-    sha256 cellar: :any, mojave:        "c4059aca471c3b1ec384323c1c801b844a2d2b61f17434ebc43c0accffde9f9b"
-    sha256 cellar: :any, high_sierra:   "0f8828cf6044f2d7b0cac9c591295b420244e8f086dc0afae9ae5420e6be7cb9"
+    sha256 cellar: :any,                 arm64_big_sur: "33c1b089709532f82978e0f0fade8bbbf0c56ef07c81d7d22f8e8b649d0c72ca"
+    sha256 cellar: :any,                 big_sur:       "437d89ccde33e47708506a235fd68e2a3745d7848498dd1ce72e8c77a1e74dcc"
+    sha256 cellar: :any,                 catalina:      "964b3493cffeb5e32411c7a34e1813e8e83e940419aca39f50be7db9b0c8fab2"
+    sha256 cellar: :any,                 mojave:        "c4059aca471c3b1ec384323c1c801b844a2d2b61f17434ebc43c0accffde9f9b"
+    sha256 cellar: :any,                 high_sierra:   "0f8828cf6044f2d7b0cac9c591295b420244e8f086dc0afae9ae5420e6be7cb9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "163de268a10eb01b4de4047ce4f0df9c180077418b7fbecf79efe9e5f193ca81"
   end
 
   head do
