10f3d1a4c01c487fa055b3f12f7511d947ed7e51
Jonathan Chang <me@jonathanchang.org>
2021-08-31 16:46:42 -0700

    mrbayes 3.2.7 (new formula)
    Bayesian inference of phylogenies and evolutionary models.
    
    Closes #84357.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/mrbayes.rb
@@ -0,0 +1,32 @@
+class Mrbayes < Formula
+  desc "Bayesian inference of phylogenies and evolutionary models"
+  homepage "https://nbisweden.github.io/MrBayes/"
+  url "https://github.com/NBISweden/MrBayes/archive/v3.2.7a.tar.gz"
+  sha256 "3eed2e3b1d9e46f265b6067a502a89732b6f430585d258b886e008e846ecc5c6"
+  license "GPL-3.0-or-later"
+  head "https://github.com/NBISweden/MrBayes.git", branch: "develop"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "beagle"
+  depends_on "open-mpi"
+
+  def install
+    system "./configure", *std_configure_args, "--with-mpi=yes"
+    system "make", "install"
+
+    doc.install share/"examples/mrbayes" => "examples"
+  end
+
+  test do
+    cp doc/"examples/primates.nex", testpath
+    cmd = "mcmc ngen = 5000; sump; sumt;"
+    cmd = "set usebeagle=yes beagledevice=cpu;" + cmd
+    inreplace "primates.nex", "end;", cmd + "\n\nend;"
+    system bin/"mb", "primates.nex"
+  end
+end
