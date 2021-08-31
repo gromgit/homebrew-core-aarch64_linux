df543c1ff1d2d5935d06a835ea86205ebffe05b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-31 03:06:43 +0000

    terragrunt: update 0.31.8 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -6,11 +6,11 @@ class Terragrunt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e2963ea09f4078f4d6216f3526451c9b9cabc831b7f5eda092cbc2c41fdf0cba"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3d517d577f1026777c870f3bde364d4c3922c5d0b4515d2f2faf31ee478b496a"
-    sha256 cellar: :any_skip_relocation, catalina:      "c8560ce26c8fd5af8119e0abf73ccb72f3086926cd7fbf58dd2e0d582e93f943"
-    sha256 cellar: :any_skip_relocation, mojave:        "e6be2bb29c3cf8e261d6edd36656d3e507403c96133f25863d00a94514808c37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d522e56e82063fbab284c3b37832f2c9b908cf420829be53a365c40bfbb5fad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9e6cf2bf18a05acf4df6e6748d35a82e7c085918638c4abc9ad311901f05c21d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d54915f105fc5376214f7025a084065144851510bb15a7becc4d73661d805b45"
+    sha256 cellar: :any_skip_relocation, catalina:      "08b16bc2e39870d9ad122bd9e33632c3105d84d77bc2ad326b471ef9b5e29ad7"
+    sha256 cellar: :any_skip_relocation, mojave:        "f6351d2f916bc3a3062664da55e50bc52b12b1d53ccd19745e44106e3f7f1a4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6953b9795123adf5635dca9716b35fc687d08bccffb694f19cfd284508b65295"
   end
 
   depends_on "go" => :build
