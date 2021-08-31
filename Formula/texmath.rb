725e1ca5ae018dfd7ce7c5703726fd2a4d416701
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-31 09:14:06 +0000

    texmath: update 0.12.3.1 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -7,11 +7,11 @@ class Texmath < Formula
   head "https://github.com/jgm/texmath.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "820604565c309184e9c3627f2846f5a1715fdf1607fa6647f65e1b1f7bdbee38"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5103aa3afe5577ee258878a49075f887c10f704a3267e005b6be77518d27e871"
-    sha256 cellar: :any_skip_relocation, catalina:      "821d782521b756be6628037d9848a1080f3acd98cf75585241d3a73f70841dcb"
-    sha256 cellar: :any_skip_relocation, mojave:        "9f1fc7ee11583a51ccb981bd0ec238b19b963054e5b58bf47edd5e86098c78d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0898fbd78de26c2058503760b1c53c1c9d9d230f0f71fdf1ee3032a7e1642bc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "46ed72e0b0dfa667e734bef3a4d82c544d75e070fa553eb36fd226b4ce5081d3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f61a2add343ea013338911404df8c8430c35a6eb32ee30898b541619f8bdad78"
+    sha256 cellar: :any_skip_relocation, catalina:      "570f9bb16277e48262e8e35a9ed290ce9ff2b29801069d452d77128667e991bb"
+    sha256 cellar: :any_skip_relocation, mojave:        "db017fcc57b938920ddaadd5dc672812b24d855c5d23892a7bf0e5f93fb42924"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5646b903efee5029614c5672bf6e4bdbfb77470f68a9011994c139d80e381f7e"
   end
 
   depends_on "cabal-install" => :build
