d2b1adf5123bdef7103b67e9b9e930fa3724bdf3
Michael Cho <cho-m@tuta.io>
2021-08-30 22:32:07 -0700

    grails 4.0.12
    Closes #84306.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v4.0.11/grails-4.0.11.zip"
-  sha256 "1c7a9439b48024f14833bce73a971074cf0be76481e35b54048442c5ac52f528"
+  url "https://github.com/grails/grails-core/releases/download/v4.0.12/grails-4.0.12.zip"
+  sha256 "3f73d9f6b0943530e885fdc98c3cba18b8fcc07f08568e49ca60f8a6cb038685"
   license "Apache-2.0"
 
   livecheck do
@@ -19,8 +19,8 @@ class Grails < Formula
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     libexec.install Dir["*"]
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files libexec/"bin", JAVA_HOME: "${JAVA_HOME:-#{Formula["openjdk@11"].opt_prefix}}"
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env("11")
   end
 
   def caveats
