f423295939f560626ae2abe6d47f9cbfd28fcb75
rui <rui@chenrui.dev>
2021-08-31 14:12:00 +0000

    ansilove: add 4.1.5 bottle.

--- Formula/ansilove.rb
+++ Formula/ansilove.rb
@@ -5,6 +5,14 @@ class Ansilove < Formula
   sha256 "dcc6e82fab1587a0f556ee64a6cda9c91dcaaa37306cccc4a4d25f7c96b04d19"
   license "BSD-2-Clause"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_big_sur: "ce746bff57bf9c61f3dae89126257d741593d68587d740f7267ef5e1b2a3a111"
+    sha256 cellar: :any,                 big_sur:       "6b82fee266c64e4738ebef22c745f3a457dccad114a285bc85972651c2545c26"
+    sha256 cellar: :any,                 catalina:      "78487b299437decd72a5a2152caf81ba4997e9300016032ed449981cfc174641"
+    sha256 cellar: :any,                 mojave:        "d40386a9e4ab3b22d87f894f1a018bd481592c26792fe95ea86fc918e4f3c26f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f25e133bbae1b11f1422da43fd27da11bf42cd3d2caf68e92024143726d24d02"
+  end
+
   depends_on "cmake" => :build
   depends_on "gd"
 
