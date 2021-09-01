1f03094dee52c07d5877372b292d163c6a0450f4
Rui Chen <rui@chenrui.dev>
2021-08-31 20:50:28 -0400

    rollup 2.56.3 (new formula)
    Closes #84360.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/rollup.rb
@@ -0,0 +1,44 @@
+require "language/node"
+
+class Rollup < Formula
+  desc "Next-generation ES module bundler"
+  homepage "https://rollupjs.org/"
+  url "https://registry.npmjs.org/rollup/-/rollup-2.56.3.tgz"
+  sha256 "ff1b1ea315ec186efb3f70a643ef17e552e076c518bb54d4e84a22b3e9f48cda"
+  license all_of: ["ISC", "MIT"]
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    deuniversalize_machos
+  end
+
+  test do
+    (testpath/"test/main.js").write <<~EOS
+      import foo from './foo.js';
+      export default function () {
+        console.log(foo);
+      }
+    EOS
+
+    (testpath/"test/foo.js").write <<~EOS
+      export default 'hello world!';
+    EOS
+
+    expected = <<~EOS
+      'use strict';
+
+      var foo = 'hello world!';
+
+      function main () {
+        console.log(foo);
+      }
+
+      module.exports = main;
+    EOS
+
+    assert_equal expected, shell_output("#{bin}/rollup #{testpath}/test/main.js -f cjs")
+  end
+end
