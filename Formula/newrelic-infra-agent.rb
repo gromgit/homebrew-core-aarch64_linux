cfc933951b4837bac7aec30a226162e1d524d07a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 08:13:23 +0000

    newrelic-infra-agent: update 1.20.2 bottle.

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -7,10 +7,10 @@ class NewrelicInfraAgent < Formula
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "ac813242b7d7adee1382611d5ff0b25bf12c98ead33fd5fda0474a143b79fa29"
-    sha256 cellar: :any_skip_relocation, catalina:     "6f8b942d9f1b02383027bc790352b5bb24e8b500389555f05b7060cf820c3761"
-    sha256 cellar: :any_skip_relocation, mojave:       "be96279cf0596779f75172306d88fd6bf8138d40051190c775bb805bdf811924"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dfc811b08b78f09bfca30f7cc64f8590854219ddd6461eabb8db4b61a9ea0941"
+    sha256 cellar: :any_skip_relocation, big_sur:      "4106b38ed2037ceebb6e984b9e53b9e4c03353bd306a8db75c2122da6ef8cb41"
+    sha256 cellar: :any_skip_relocation, catalina:     "0db6721dc524dbbdfa5a7e27d51dca6691466e7b3be621f46909d55cf1c81fc1"
+    sha256 cellar: :any_skip_relocation, mojave:       "0ebf0c24dcf57bf8c872354d924b8076b83b4e771a351527629734d1c896f402"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "80a1f2d22913a89ec427d20a8e03579b6de3f74e23ecf7db99546d513eec35f6"
   end
 
   depends_on "go" => :build
