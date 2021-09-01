820f59e5bd071427074d4463314588c191f716bb
Dustin Rodrigues <dust.rod@gmail.com>
2021-08-31 20:56:02 -0400

    pyright 1.1.165
    Closes #84361.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyright.rb
+++ Formula/pyright.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Pyright < Formula
   desc "Static type checker for Python"
   homepage "https://github.com/microsoft/pyright"
-  url "https://registry.npmjs.org/pyright/-/pyright-1.1.164.tgz"
-  sha256 "eca2b6a0600461089ab49efd14af94ebdeb1b47e7b1e487b54fe7bc598f07163"
+  url "https://registry.npmjs.org/pyright/-/pyright-1.1.165.tgz"
+  sha256 "042484e6267bbf3d3e56099985d70320fae9805b44109067356474291eb586bc"
   license "MIT"
   head "https://github.com/microsoft/pyright.git", branch: "main"
 
