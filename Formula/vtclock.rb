d4627c4a2319110169a3f65a1b472783102d4765
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 18:12:24 +0000

    vtclock: update 0.0.20161228 bottle.

--- Formula/vtclock.rb
+++ Formula/vtclock.rb
@@ -13,14 +13,11 @@ class Vtclock < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "34bc3937dbc073c9f9a210beda09527ae97f49826a3ef32f8f997317481cdf72"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c545d476592374b924d2d05a07d37e02461868d9c8ab9ba83cc82b9d6c21ba15"
-    sha256 cellar: :any_skip_relocation, catalina:      "a9d8600f4ab8ef25a88f5778d4625ad04c7600b8770e08300193f2841451c6d4"
-    sha256 cellar: :any_skip_relocation, mojave:        "380055ab532f75e4cb03f91d5e0cbceb14371d4b57ddad9771d562cf572e5746"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "d72cbcb6862c0030433a2413fbf1d1773d6ed8207a5c681c9ffd206fa5798f45"
-    sha256 cellar: :any_skip_relocation, sierra:        "766e69763326b8a8c5cfdc636cbba9f6fcffde0739be56612c54a2904d95d456"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "f87c685e59533a0085b439c4153c2734d4091447f5a81c627ccc0d2e589ac65d"
-    sha256 cellar: :any_skip_relocation, yosemite:      "a72a8c176276c40a3e9b0c6083a61013efb55b5ea43cd786000dad3c4243dd96"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7ba3ce880fdedf82e166ed0f7f7a4124c57510680d5c591fd363ac14887618f5"
+    sha256 cellar: :any_skip_relocation, big_sur:       "747b98b591c6a732012006db26bc8cdd871509e6407f557a1b616c54d2c2079d"
+    sha256 cellar: :any_skip_relocation, catalina:      "77d6846683ebe827ad322e81cd281e79d28b5e93122aadf32c49a1566b1c9f2e"
+    sha256 cellar: :any_skip_relocation, mojave:        "1f4d80e05dcf1c214a317d54057674f496ff0a063d9acaedc3e845d212c627bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70959665bdcaa3a6909a6b3e8f13e2b81c50fd28ba845f69af0cbff34df1d209"
   end
 
   depends_on "pkg-config" => :build
