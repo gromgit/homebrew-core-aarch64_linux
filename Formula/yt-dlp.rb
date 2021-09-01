e86a52e399f3d88eea1d561e003cf10bba31eafb
Bevan Kay <email@bevankay.me>
2021-09-02 09:30:23 +1000

    yt-dlp 2021.9.1
    Closes #84432.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yt-dlp.rb
+++ Formula/yt-dlp.rb
@@ -3,8 +3,8 @@ class YtDlp < Formula
 
   desc "Fork of youtube-dl with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://files.pythonhosted.org/packages/ee/59/d763a51fa975639946c327ffbf85e109dbaa87f8aa5cae54a4ee5d09593c/yt-dlp-2021.8.10.tar.gz"
-  sha256 "8da1bf4dc4641d37d137443c4783109ee8393caad5e0d270d9d1d534e8f25240"
+  url "https://files.pythonhosted.org/packages/d0/7a/40aa2c5e8b25e4c0ee8e00963507acc104f68d03b540e6a3686f331b3bec/yt-dlp-2021.9.1.tar.gz"
+  sha256 "efefbec227a014aaecb7b3c5d4e1649de474802058598a9c0281ee020e0d1cb6"
   license "Unlicense"
   head "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
 
@@ -18,6 +18,21 @@ class YtDlp < Formula
   end
 
   depends_on "python@3.9"
+
+  resource "mutagen" do
+    url "https://files.pythonhosted.org/packages/f3/d9/2232a4cb9a98e2d2501f7e58d193bc49c956ef23756d7423ba1bd87e386d/mutagen-1.45.1.tar.gz"
+    sha256 "6397602efb3c2d7baebd2166ed85731ae1c1d475abca22090b7141ff5034b3e1"
+  end
+
+  resource "pycryptodome" do
+    url "https://files.pythonhosted.org/packages/88/7f/740b99ffb8173ba9d20eb890cc05187677df90219649645aca7e44eb8ff4/pycryptodome-3.10.1.tar.gz"
+    sha256 "3e2e3a06580c5f190df843cdb90ea28d61099cf4924334d5297a995de68e4673"
+  end
+
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/0d/bd/5262054455ab2067e51de331bfbc53a1dfa9071af7c424cf7c0793c4349a/websockets-9.1.tar.gz"
+    sha256 "276d2339ebf0df4f45df453923ebd2270b87900eda5dfd4a6b0cfa15f82111c3"
+  end
 
   def install
     virtualenv_install_with_resources
