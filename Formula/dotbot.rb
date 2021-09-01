276c4dcc077679f77ad0a76df5694c48f4d7a419
Rui Chen <rui@chenrui.dev>
2021-09-01 01:58:01 -0400

    dotbot 1.19.0 (new formula)
    Closes #84375.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/dotbot.rb
@@ -0,0 +1,33 @@
+class Dotbot < Formula
+  include Language::Python::Virtualenv
+
+  desc "Tool that bootstraps your dotfiles"
+  homepage "https://github.com/anishathalye/dotbot"
+  url "https://files.pythonhosted.org/packages/d3/67/733dbf0b444d41af473238537d5ef7bd5906870f35a69ef4f7dc64e74519/dotbot-1.19.0.tar.gz"
+  sha256 "29f4a461462a5ff3b1e9929849458e88d827a45d764f582c633237edd373f0af"
+  license "MIT"
+
+  depends_on "python@3.9"
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d/PyYAML-5.4.1.tar.gz"
+    sha256 "607774cbba28732bfa802b54baa7484215f530991055bb562efbed5b2f20a45e"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"install.conf.yaml").write <<~EOS
+      - create:
+        - brew
+        - .brew/test
+    EOS
+
+    output = shell_output("#{bin}/dotbot -c #{testpath}/install.conf.yaml")
+    assert_match "All tasks executed successfully", output
+    assert_predicate testpath/"brew", :exist?
+    assert_predicate testpath/".brew/test", :exist?
+  end
+end
