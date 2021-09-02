01658d8828cf746b0e9a72a999198d052e11a849
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-02 06:08:09 +0000

    clib: update 2.8.0 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -12,11 +12,11 @@ class Clib < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b77f5026ceceec9764651778eb88f536540faaa8aa3845f7cf6bc15bd78dc0f9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ba2d9b6112f7a183a28e06b00bdf01c3164dbdf05b6284b5a5d8c972c4a5ef59"
-    sha256 cellar: :any_skip_relocation, catalina:      "19a1b473f5c392ce7a1f8ae8209918926777f32395a31199047af6cd22ff01a5"
-    sha256 cellar: :any_skip_relocation, mojave:        "30f21023c47efeda8c7609f7ad09de5c866e903ab85d90f11227424836fbf248"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13bb925a61aec0575c5112f596752c71b48b42f43b37fd3da4ba6eb69f56ebc8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "44f5920711c2eb79e334cd7bc4b5972c9b3010a0345607b8c59ee71b1e2391c1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8a4af428b5c74fed903aca3337334bf0d418fd9765ea50d73ac2f308c60f7061"
+    sha256 cellar: :any_skip_relocation, catalina:      "b1d926918eab64e133117b5089f17c51f0a0888d7ed33448fd3247e05a15e919"
+    sha256 cellar: :any_skip_relocation, mojave:        "277176751578539b28b96e3d18f1ca9efa576b270e6ee6a42a7f4fdea317c061"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "732a47d87de00dc02478feb7dede8ba1a5941ce10ededd895cf96f5111e71f4e"
   end
 
   uses_from_macos "curl"
