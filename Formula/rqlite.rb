57f849a4547319a78a70c71bd5374e1c61cb485e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-02 16:12:12 +0000

    rqlite: update 6.4.2 bottle.

--- Formula/rqlite.rb
+++ Formula/rqlite.rb
@@ -7,11 +7,11 @@ class Rqlite < Formula
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "567d16d9731ddd548bb78830ad35ec950a809fbd55ec14cc372300083ef18b34"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e1d392d317fb1ceb951aa9f8c8020337ae07196fb448ecb98e1955f8d79a84cb"
-    sha256 cellar: :any_skip_relocation, catalina:      "4d80ddec2aa1a29276f8a7cd1f92a09ddeb04b5933fa58233f758d5bac7fee95"
-    sha256 cellar: :any_skip_relocation, mojave:        "651db07333db9faa5c42a8fb5fe4b677911688f94e44d4b3e16cfc2576b30f2c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8dd76d6f443c36b0cfb2b47fadece0ec81a1b78992fc1d342323a24c439c319f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d0c8767e06e2ece48938e64056255bd67fff1438637aa5fd95da533f1780bae3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "208dad1214feaf4c9adaaa5c225223f724bdcf493659362d0bada9ea79274cac"
+    sha256 cellar: :any_skip_relocation, catalina:      "41ab5616109bb8b678d41152833517d8c4ee582386f3263d4a038e8e9230119e"
+    sha256 cellar: :any_skip_relocation, mojave:        "649617599e8b890e60129bd990c59a35c9ab17d0f1e7b533d77e4d6c15b6ae78"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6c9d47f535d5ff720f2e86f1355ade7c0566a6317d1ef206e2affeac6d56862"
   end
 
   depends_on "go" => :build
