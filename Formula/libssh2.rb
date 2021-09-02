073c1b2f9d79dda3c5145782a12105bc086d221f
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-09-02 23:20:51 +0000

    libssh2: update 1.10.0 bottle.

--- Formula/libssh2.rb
+++ Formula/libssh2.rb
@@ -11,13 +11,11 @@ class Libssh2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2840147f112db1ef9e353e104e8aff64fc6572a9194b3ded9cc006deaa46560c"
-    sha256 cellar: :any,                 big_sur:       "f9a31faed068d80fff508f2a08d1c8b7213a626d9c38dde22db9a8173a85c6ee"
-    sha256 cellar: :any,                 catalina:      "2c4dcf8149663f9a133deac5bc42ce308d1ced90227cac391ca30b0ab2d381f9"
-    sha256 cellar: :any,                 mojave:        "327c56ad6a54894e5ef9aa3019d2444d32f1d0fba80925940100e517dd3109c9"
-    sha256 cellar: :any,                 high_sierra:   "ee29f44ef6fb59242fc7ee1747f02df2287722af4a45319289c9ee224367ba06"
-    sha256 cellar: :any,                 sierra:        "769fbbdc4e67b8de15c269f66a6efe86c5b0195df56e7e46b44377a572800efa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7b18dca8b0e6a33c68c33ecb8af854de88fc2df59c14d5f20fb95f1a9903f32"
+    sha256 cellar: :any,                 arm64_big_sur: "db07a7c502116b5a80ae01e82e7f5c54633a8ac7343d369af25af6cc2c7e5bbb"
+    sha256 cellar: :any,                 big_sur:       "56dd017876fd446d7283c7db7a6a0729eeebd34016094fdbf9f46b6711c0e26d"
+    sha256 cellar: :any,                 catalina:      "5b30fe11d2ced21be876b56787e5d6900cb991fdd7e6ad3a6058401aa59ee9d7"
+    sha256 cellar: :any,                 mojave:        "70c0928f2cb9034ad07c6242517ebc0e4cfb92b1ab74518f7b510a2ac36e81fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ff0fe65fb281d51dab44a53b15ef40ebeebf09a7f4d28e86dfc0cc18e49bbc1"
   end
 
   head do
