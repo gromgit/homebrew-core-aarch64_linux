5fceb7dd0a75349f4eb67384836cb2d0a330bcc4
Rui Chen <rui@chenrui.dev>
2021-09-03 07:25:47 -0400

    artillery 1.7.5 (new formula)
    fix audit failure
    
    Closes #84595.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/artillery.rb
@@ -0,0 +1,50 @@
+require "language/node"
+
+class Artillery < Formula
+  desc "Cloud-native performance & reliability testing for developers and SREs"
+  homepage "https://artillery.io/"
+  url "https://registry.npmjs.org/artillery/-/artillery-1.7.5.tgz"
+  sha256 "6cf667509b8bfd8570ea80df60f03430848d0a0b538ae860ba1061318c13f171"
+  license "MPL-2.0"
+
+  depends_on "node"
+
+  on_macos do
+    depends_on "macos-term-size"
+  end
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    term_size_vendor_dir = libexec/"lib/node_modules"/name/"node_modules/term-size/vendor"
+    term_size_vendor_dir.rmtree # remove pre-built binaries
+
+    on_macos do
+      macos_dir = term_size_vendor_dir/"macos"
+      macos_dir.mkpath
+      # Replace the vendored pre-built term-size with one we build ourselves
+      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
+    end
+  end
+
+  test do
+    system bin/"artillery", "dino", "-m", "let's run some tests!"
+
+    (testpath/"config.yml").write <<~EOS
+      config:
+        target: "http://httpbin.org"
+        phases:
+          - duration: 10
+            arrivalRate: 1
+      scenarios:
+        - flow:
+            - get:
+                url: "/headers"
+            - post:
+                url: "/response-headers"
+    EOS
+
+    assert_match "All virtual users finished", shell_output("#{bin}/artillery run #{testpath}/config.yml")
+  end
+end
