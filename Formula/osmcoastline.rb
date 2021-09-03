7b70a9b3a59c94c4a2ef2b9dc3f6dcfcca759366
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-03 01:26:19 +0000

    osmcoastline: update 2.3.1 bottle.

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -6,10 +6,10 @@ class Osmcoastline < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0306a9c8556638a4d128bc15d9adc68736a3022333a55beeb227af97eb30a9aa"
-    sha256 cellar: :any, big_sur:       "50cfe5960393f3787b938697d26f620ed4218e9a65a16d24e9b4b9ac82bfafa5"
-    sha256 cellar: :any, catalina:      "cf181ab2a5da968d965d6712e5756de4d30b950bbfe8eef8f54907be8aeffef6"
-    sha256 cellar: :any, mojave:        "b5329fbfabf3ed6f2754c4fafb24ef9f35680e9391c911887c4c0037c33fa430"
+    sha256 cellar: :any, arm64_big_sur: "e0820b1a6cd6e77dfdd742f29404c5f1b78f9b198a59c7019152e4e9d9608d72"
+    sha256 cellar: :any, big_sur:       "f4de31702d99101baff041d2b96bc4bd58f1bd728c07353f5c9f753551ba3d31"
+    sha256 cellar: :any, catalina:      "eb5bba76581312e9b070eb5f83b8d98e9862d0f33b13808f5a6f0a33ce3c0973"
+    sha256 cellar: :any, mojave:        "37f1b4c9d9fb60ed418eb69292c7f3b9de2cc3b7599f2ba8fed69a1b1ee88d13"
   end
 
   depends_on "cmake" => :build
