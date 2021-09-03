29d316fb938ebf1569b024fe5fe1ac5a8a648ec4
Luis Martinez <luis.martinez@disroot.org>
2021-09-03 18:15:23 -0500

    cpufetch 1.00 (new formula)
    Closes #84631.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cpufetch.rb
@@ -0,0 +1,28 @@
+class Cpufetch < Formula
+  desc "CPU architecture fetching tool"
+  homepage "https://github.com/Dr-Noob/cpufetch"
+  url "https://github.com/Dr-Noob/cpufetch/archive/v1.00.tar.gz"
+  sha256 "2254c2578435cc35c4d325b25fdff4c4b681de92cbce9a7a36e58ad58a3d9173"
+  license "MIT"
+  head "https://github.com/Dr-Noob/cpufetch.git", branch: "master"
+
+  def install
+    system "make"
+    bin.install "cpufetch"
+    man1.install "cpufetch.1"
+  end
+
+  test do
+    actual = shell_output("#{bin}/cpufetch -d").each_line.first.strip
+
+    expected = if OS.linux?
+      "cpufetch v#{version} (Linux #{Hardware::CPU.arch} build)"
+    elsif Hardware::CPU.arm?
+      "cpufetch v#{version} (macOS ARM build)"
+    else
+      "cpufetch is computing APIC IDs, please wait..."
+    end
+
+    assert_equal expected, actual
+  end
+end
