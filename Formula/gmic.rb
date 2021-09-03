234c00fcfa103153b552bf85d4f8d74ee6d22658
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-03 09:12:24 +0000

    gmic: update 2.9.9 bottle.

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -12,11 +12,11 @@ class Gmic < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b4ca938c13a0d48444a68b260556af081c92c7155f837ee967ab325687f97cd1"
-    sha256 cellar: :any,                 big_sur:       "aaf9c8289fca869eb689a3c8df531c88d06a7f20e66834476040b28bda1d8f27"
-    sha256 cellar: :any,                 catalina:      "2393fee531477a0737818b367e8a7fd1bf5b2d1834c9f71d0424d9fdfe35289d"
-    sha256 cellar: :any,                 mojave:        "06c829f95058ad32cd4c851fef13238008215252775cd35218428d440b8d3eb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a575f45f76d9652e38f78982d5a5edb053191f99a4125805971f7cc2441332c"
+    sha256 cellar: :any,                 arm64_big_sur: "1a578cfffc741545e5b49370a58ffba93d573173c19d81a883b7b0960dc07b73"
+    sha256 cellar: :any,                 big_sur:       "be87829332b36a812cdc929cefcc5d2fdc29d026f7b04fe9ab98d0fb1b17d1d5"
+    sha256 cellar: :any,                 catalina:      "da3502b0e4aa8306e4cd46fe3df9a3e0851d5d9e2bfb7a2ca8fa41f23e0b06c0"
+    sha256 cellar: :any,                 mojave:        "f91c71752cfb0d544dd70e5ef60ab40de2ca8f47a2ac0b648980220a180528e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "796e64c3eefd7d603556f391fcc284eaddb7cda7d0c15fe73d36fc04304ca5ca"
   end
 
   depends_on "cmake" => :build
