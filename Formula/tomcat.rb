738d31879b51258ce6228a43f802b0542dbbe2a4
Vojtech Knyttl <knyttl@goout.cz>
2021-09-03 14:30:44 +0200

    tomcat: Fix configuration directory

--- Formula/tomcat.rb
+++ Formula/tomcat.rb
@@ -5,6 +5,7 @@ class Tomcat < Formula
   mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.10/bin/apache-tomcat-10.0.10.tar.gz"
   sha256 "cd1e27216cd8dad1e9b7f9a0a4107c7148b748eb646f2aee45a11016d1268bb2"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "0cdfbbdd72060205ff5346d2046f8369779623221c09ed52c6e990a1c2107752"
@@ -18,13 +19,13 @@ class Tomcat < Formula
 
     # Install files
     prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
+
+    pkgetc.install Dir["conf/*"]
+    (buildpath/"conf").rmdir
+    libexec.install_symlink pkgetc => "conf"
+
     libexec.install Dir["*"]
     (bin/"catalina").write_env_script "#{libexec}/bin/catalina.sh", JAVA_HOME: Formula["openjdk"].opt_prefix
-  end
-
-  def post_install
-    pkgetc.mkpath
-    ln_s pkgetc, libexec/"config" unless (libexec/"config").exist?
   end
 
   def caveats
