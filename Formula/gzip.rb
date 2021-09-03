e91199a8a8575a47caf04357cf16e1f142724b0f
Alexander Ronald Altman <alexanderaltman@me.com>
2021-09-03 14:12:43 -0700

    gzip 1.11
    Closes #84628.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gzip.rb
+++ Formula/gzip.rb
@@ -1,9 +1,9 @@
 class Gzip < Formula
   desc "Popular GNU data compression program"
   homepage "https://www.gnu.org/software/gzip"
-  url "https://ftp.gnu.org/gnu/gzip/gzip-1.10.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gzip/gzip-1.10.tar.gz"
-  sha256 "c91f74430bf7bc20402e1f657d0b252cb80aa66ba333a25704512af346633c68"
+  url "https://ftp.gnu.org/gnu/gzip/gzip-1.11.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gzip/gzip-1.11.tar.gz"
+  sha256 "3e8a0e0c45bad3009341dce17d71536c4c655d9313039021ce7554a26cd50ed9"
   license "GPL-3.0-or-later"
 
   bottle do
