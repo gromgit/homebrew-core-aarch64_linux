4299322e622fc3a907cfab6df2254308aad6fe96
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-03 11:17:54 +0000

    kpcli: update 3.6_2 bottle.

--- Formula/kpcli.rb
+++ Formula/kpcli.rb
@@ -16,11 +16,11 @@ class Kpcli < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ada893ae300554a13c7b56fce7bc91716d45294078a0f2006175a10e581eddc0"
-    sha256 cellar: :any,                 big_sur:       "0918f51306694d5a21d117030f32ce49e08a4da4b47d0106d8542b826db03ce6"
-    sha256 cellar: :any,                 catalina:      "8b88064bbd450ba7b3a7e902a85ac926d2d57b38b74b8a79f4cb72cc9a43aee9"
-    sha256 cellar: :any,                 mojave:        "6cbffa6128e72f200e5216a6df39b07586048d165e69e45a8cb4821928b98a5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fbcd686944fd696d1a6cb010e9ffbcc106ea4e7f65a008e2b7f4d30573d6b953"
+    sha256 cellar: :any,                 arm64_big_sur: "793a7e8ce3bf2a0beae2b0a51b14e3915df7f8ba05f20b4bf26c5ed3dba11e98"
+    sha256 cellar: :any,                 big_sur:       "8431834a984e31a2bb567913b8bd82ec97e616879f058c10e1b0af77fc9528e8"
+    sha256 cellar: :any,                 catalina:      "ef0dcac2509419af4f99407cd40d7072a44d53472cb692afa49bae06bb91fb71"
+    sha256 cellar: :any,                 mojave:        "7635208158e7c7a7dd1e7a4e28d51d2c836d6e2bd9f6c329c265cebdf4b69939"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a340307dd0234be2d5db82bfb5a949a5547fd2a83b239766c535aef2237e195"
   end
 
   depends_on "readline"
