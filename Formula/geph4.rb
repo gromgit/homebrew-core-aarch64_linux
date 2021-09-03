e40d3c8b87123f347793aaf392d1482c8792636f
Rui Chen <rui@chenrui.dev>
2021-09-02 23:14:12 -0400

    geph4 4.4.16
    * geph4 4.4.15
    * update build
    * geph4 4.4.16
    
    Closes #84568.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/geph4.rb
+++ Formula/geph4.rb
@@ -1,9 +1,10 @@
 class Geph4 < Formula
   desc "Modular Internet censorship circumvention system to deal with national filtering"
   homepage "https://geph.io/"
-  url "https://github.com/geph-official/geph4/archive/v4.4.12.tar.gz"
-  sha256 "43bde1dda387a89131f5f33a9e75a5c4bc9b410cd3e5cd34fed0d0e9f942cf27"
+  url "https://github.com/geph-official/geph4/archive/v4.4.16.tar.gz"
+  sha256 "e16a98ed8332ea8edbad7eba7b12b663907cba0b10f525ebf6541212067ee097"
   license "GPL-3.0-only"
+  head "https://github.com/geph-official/geph4.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "14ea32e04cddf8f57948ac0713498feac1ff67a3a21444b086d310b2ac357581"
@@ -15,10 +16,8 @@ class Geph4 < Formula
   depends_on "rust" => :build
 
   def install
-    File.delete("Cross.toml")
-    remove_dir(".cargo")
-    Dir.chdir "geph4-client"
-    system "cargo", "install", "--bin", "geph4-client", *std_cargo_args
+    (buildpath/".cargo").rmtree
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
