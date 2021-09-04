e250346e3a4c5baea23e8c2f36653e8b4b16ccc9
rui <rui@chenrui.dev>
2021-09-04 07:22:00 +0000

    locust: add 2.2.1 bottle.

--- Formula/locust.rb
+++ Formula/locust.rb
@@ -7,6 +7,14 @@ class Locust < Formula
   sha256 "5d65c9b4894f2ac47ed1a4c6f330705f478ed9c8fbf9931be913390e638b3aa7"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "014ebff56b25b1fa3c6a1627f847a4b135046471bdeb6830ac58c1a55131b1a4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a4232840aa56769b58a4f5254851db3c5f74fc6069e4063c51d4e91ae730f79b"
+    sha256 cellar: :any_skip_relocation, catalina:      "c6bfbc56ed1b299defa5f3fb5b3f13d49596f096c7eeff073fe3014d1cb897b5"
+    sha256 cellar: :any_skip_relocation, mojave:        "1d98ac97b351cff24482e45ce68582b0d1eaa6e2b2b02e2a51a1145ce182779d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "17653c08ce5937dd6cbc90eea37894118484bfe405c6effdee3e4ff1c86cb4b2"
+  end
+
   depends_on "python@3.9"
   depends_on "six"
 
