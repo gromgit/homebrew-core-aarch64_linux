0470fc714ac18884ed663433e1fda6e59459997a
Jonathan Chang <me@jonathanchang.org>
2021-09-04 20:54:23 +0000

    nanoflann: add 1.3.2 bottle.

--- Formula/nanoflann.rb
+++ Formula/nanoflann.rb
@@ -11,6 +11,14 @@ class Nanoflann < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dd4506127f2bd65002f11b2deb0b2f6b636fd864bee4a4294fd6123b9c94b6b6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9ce420030884830f6b94c1fa7304d647ad4ceb44622a56781e94d2c3e8c5ca6c"
+    sha256 cellar: :any_skip_relocation, catalina:      "a0cba3b9ca8e124b841a62554ebbf71234253b6706e779c0e928a3a012bc2598"
+    sha256 cellar: :any_skip_relocation, mojave:        "0df3f7eb8bbea15676f63d57c96ac6ad7ebd74996a496ee94adff7845799a651"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a70a8dace8f5f823b993f32246c12b5cff47c551fe8326c4338871bfb2b874f"
+  end
+
   depends_on "cmake" => :build
 
   def install
