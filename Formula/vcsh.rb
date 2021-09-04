67810b68b7d29956dd6f6df7325bb50c6d0797db
Richard Hartmann <RichiH@users.noreply.github.com>
2021-09-04 20:57:11 +0200

    vcsh 2.0.2
    Signed-off-by: Richard Hartmann <richih@richih.org>
        Co-authored-by: Caleb Maclennan <caleb@alerque.com>
        Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    
    Closes #84672.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vcsh.rb
+++ Formula/vcsh.rb
@@ -1,9 +1,8 @@
 class Vcsh < Formula
   desc "Config manager based on git"
   homepage "https://github.com/RichiH/vcsh"
-  url "https://github.com/RichiH/vcsh/archive/refs/tags/v1.20190621-4.tar.gz"
-  version "1.20190621"
-  sha256 "178ddf6f7bba15bcc295a08247070665e5b799af64753e21c7fac68f72296ca8"
+  url "https://github.com/RichiH/vcsh/releases/download/v2.0.2/vcsh-2.0.2.tar.xz"
+  sha256 "3ffc0bbb43c76620c8234c98f4ae94d0a99d24bb240497aab730979a8d23ad61"
   license "GPL-2.0"
 
   bottle do
@@ -11,9 +10,13 @@ class Vcsh < Formula
   end
 
   def install
-    bin.install "vcsh"
-    man1.install "vcsh.1"
-    zsh_completion.install "_vcsh"
+    system "./configure", "--with-zsh-completion-dir=#{zsh_completion}",
+                          "--with-bash-completion-dir=#{bash_completion}",
+                          *std_configure_args
+    system "make", "install"
+
+    # Remove references to git shim
+    inreplace bin/"vcsh", %r{#{HOMEBREW_SHIMS_PATH}/[^/]+/super/git}o, "git"
   end
 
   test do
