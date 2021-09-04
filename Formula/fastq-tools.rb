110988e58b46c3e8a3e12612b21f5d6992ba71ef
Jonathan Chang <me@jonathanchang.org>
2021-09-04 21:02:10 +0000

    fastq-tools: add 0.8.3 bottle.

--- Formula/fastq-tools.rb
+++ Formula/fastq-tools.rb
@@ -10,6 +10,14 @@ class FastqTools < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_big_sur: "ac48791014e14979ad786e59178d0b468510d02f5d51a86608b388adad4405f1"
+    sha256 cellar: :any,                 big_sur:       "18f3e795ec5c2c182bfc995ce662816cf17ccbd719fef30937f5456d28bbccc5"
+    sha256 cellar: :any,                 catalina:      "20105d6a89abbf493ceeedfc29a956b9b4cc90bf2843cf73e544b03c0456b0ad"
+    sha256 cellar: :any,                 mojave:        "adf7e00692719889bdaee28870700857b9e1b0ec096b62f48e76eac5ac27f52c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "abd98e72c698e16a077443d29259f946f7ac36c6e5f5a6e172be5466c4cbf2cd"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
