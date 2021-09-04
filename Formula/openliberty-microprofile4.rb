c094cab64a7f8d8924a5b741042454bf6a3044a6
MoonFruit <dkmoonfruit@gmail.com>
2021-09-04 14:10:37 +0800

    openliberty-microprofile4 21.0.0.9
    Closes #84644.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openliberty-microprofile4.rb
+++ Formula/openliberty-microprofile4.rb
@@ -1,8 +1,8 @@
 class OpenlibertyMicroprofile4 < Formula
   desc "Lightweight open framework for Java (Micro Profile 4)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-07-27_1323/openliberty-microProfile4-21.0.0.8.zip"
-  sha256 "53a4d10b77fc6b21a6c387ced1829c16b975224597048a84697b16cd9f40ff44"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-08-24_2341/openliberty-microProfile4-21.0.0.9.zip"
+  sha256 "5f83b9aa8eebf60bd27a41790e5548d03ee15924bc64773fafa619a2d09dd5aa"
   license "EPL-1.0"
 
   livecheck do
@@ -43,6 +43,6 @@ class OpenlibertyMicroprofile4 < Formula
     end
 
     refute_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
-    assert_match "<feature>microProfile-4.0</feature>", (testpath/"servers/defaultServer/server.xml").read
+    assert_match "<feature>microProfile-4.1</feature>", (testpath/"servers/defaultServer/server.xml").read
   end
 end
