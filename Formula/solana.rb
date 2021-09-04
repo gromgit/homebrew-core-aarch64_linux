e3ca6fb198428d7bc9addbe85092e614ad05ee90
Richard Patel <richard@blockdaemon.com>
2021-09-04 10:33:53 +0200

    solana 1.7.11 (new formula)
    Closes #84647.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/solana.rb
@@ -0,0 +1,42 @@
+class Solana < Formula
+  desc "Web-Scale Blockchain for decentralized apps and marketplaces"
+  homepage "https://solana.com"
+  url "https://github.com/solana-labs/solana/archive/refs/tags/v1.7.11.tar.gz"
+  sha256 "ed70aaed16910eab637eff0499c6e4762350c4ad50340fcb1b4bf94d23f1f97a"
+  license "Apache-2.0"
+
+  depends_on "protobuf" => :build
+  depends_on "rust" => :build
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+
+    depends_on "openssl@1.1"
+    depends_on "systemd"
+  end
+
+  def install
+    %w[
+      cli
+      bench-streamer
+      faucet
+      keygen
+      log-analyzer
+      net-shaper
+      stake-accounts
+      sys-tuner
+      tokens
+      watchtower
+    ].each do |bin|
+      cd bin do
+        system "cargo", "install", "--no-default-features", *std_cargo_args
+      end
+    end
+  end
+
+  test do
+    assert_match(/pubkey: \w{44}/, shell_output("#{bin}/solana-keygen new --no-bip39-passphrase --no-outfile"))
+  end
+end
