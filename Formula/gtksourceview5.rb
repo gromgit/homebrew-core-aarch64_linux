17eff105276a3bee5f0e5a4f89f1ae5cef1bec6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-04 18:06:28 +0000

    gtksourceview5: update 5.2.0 bottle.

--- Formula/gtksourceview5.rb
+++ Formula/gtksourceview5.rb
@@ -11,10 +11,10 @@ class Gtksourceview5 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "195195221b285f8158d2a261d7e7b6b081a702aeac2f868e0ba7d33300589836"
-    sha256 big_sur:       "ed1b0232d384c9b9aca135cacb412a53f1ea08a6c964f0911dbc0af15481b5d1"
-    sha256 catalina:      "f2be3a8df55eebb30a6f873631bbb4142f51c0f99a5df55a9ba3c26ef0dcda07"
-    sha256 mojave:        "2c31df3348cd823f136f70a4bf6f57a16a3b95ad819a5113aea83450f83896d1"
+    sha256 arm64_big_sur: "0d1942117f008838775478880099e7947f4aa62a0edfc880deb7d75c23886003"
+    sha256 big_sur:       "707dccdc76932f3cc415aa2023e11a2da4954802e6e7ff78e02f34ed5232244f"
+    sha256 catalina:      "9de90cd3cfdd86fdb73fea825ee99e0d16ac2a3cd667eff86e8964a05bc33e56"
+    sha256 mojave:        "ac9537f6f10d0240d6eda7d94a755c7e5a62a1009c27ea894d310dd9d1109d45"
   end
 
   depends_on "gobject-introspection" => :build
