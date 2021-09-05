06977f77f4a794867894ee3bdf591ddf301dd1d1
Rui Chen <rui@chenrui.dev>
2021-09-05 11:47:04 -0400

    cdogs-sdl 1.0.2
    Closes #84725.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cdogs-sdl.rb
+++ Formula/cdogs-sdl.rb
@@ -1,8 +1,8 @@
 class CdogsSdl < Formula
   desc "Classic overhead run-and-gun game"
   homepage "https://cxong.github.io/cdogs-sdl/"
-  url "https://github.com/cxong/cdogs-sdl/archive/1.0.1.tar.gz"
-  sha256 "1a099de0fa4f8a85b28f1cc1bab7070042faebc33ce563b67fbfc639af75fcf5"
+  url "https://github.com/cxong/cdogs-sdl/archive/1.0.2.tar.gz"
+  sha256 "06e177d1a794f05d007bd5cf2c75e677b74384ac49242f86058d58d5001036e3"
   license "GPL-2.0-or-later"
   head "https://github.com/cxong/cdogs-sdl.git", branch: "master"
 
