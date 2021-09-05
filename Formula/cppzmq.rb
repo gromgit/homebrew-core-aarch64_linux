e706195fe18e1372fd5fd1844541e87cfb3e9da4
Andrew Kane <andrew@ankane.org>
2021-09-05 16:48:48 -0700

    cppzmq 4.8.0 (new formula)
    Closes #84772.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cppzmq.rb
@@ -0,0 +1,27 @@
+class Cppzmq < Formula
+  desc "Header-only C++ binding for libzmq"
+  homepage "https://www.zeromq.org"
+  url "https://github.com/zeromq/cppzmq/archive/v4.8.0.tar.gz"
+  sha256 "4c07600e3ef311e24d9cf1917122067b48bd447cb7de86c5d23a29826a204649"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "zeromq"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, "-DCPPZMQ_BUILD_TESTS=OFF"
+      system "make"
+      system "make", "install"
+    end
+    pkgshare.install "examples"
+  end
+
+  test do
+    cp pkgshare/"examples/hello_world.cpp", testpath
+    system ENV.cxx, "-std=c++11", "hello_world.cpp", "-I#{include}",
+                    "-L#{Formula["zeromq"].opt_lib}", "-lzmq", "-o", "test"
+    system "./test"
+  end
+end
