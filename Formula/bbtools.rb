3c46c7f05b3870681d773eaa614b93fa5d863bf3
Jonathan Chang <me@jonathanchang.org>
2021-09-06 12:20:03 -0700

    bbtools 38.92 (new formula)
    Closes #84767.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/bbtools.rb
@@ -0,0 +1,36 @@
+class Bbtools < Formula
+  desc "Brian Bushnell's tools for manipulating reads"
+  homepage "https://jgi.doe.gov/data-and-tools/bbtools/"
+  url "https://downloads.sourceforge.net/bbmap/BBMap_38.92.tar.gz"
+  sha256 "a5db3921684296dcbcabd0d36dc123597f587a6ac96855f4f901fee494718dc3"
+  license "BSD-3-Clause"
+
+  depends_on "openjdk"
+
+  def install
+    cd "jni" do
+      rm Dir["libbbtoolsjni.*", "*.o"]
+      system "make", "-f", OS.mac? ? "makefile.osx" : "makefile.linux"
+    end
+    libexec.install %w[current jni resources]
+    libexec.install Dir["*.sh"]
+    bin.install Dir[libexec/"*.sh"]
+    bin.env_script_all_files(libexec, Language::Java.overridable_java_home_env)
+    doc.install Dir["docs/*"]
+  end
+
+  test do
+    res = libexec/"resources"
+    args = %W[in=#{res}/sample1.fq.gz
+              in2=#{res}/sample2.fq.gz
+              out=R1.fastq.gz
+              out2=R2.fastq.gz
+              ref=#{res}/phix174_ill.ref.fa.gz
+              k=31
+              hdist=1]
+    system "#{bin}/bbduk.sh", *args
+    assert_match "bbushnell@lbl.gov", shell_output("#{bin}/bbmap.sh")
+    assert_match "maqb", shell_output("#{bin}/bbmap.sh --help 2>&1")
+    assert_match "minkmerhits", shell_output("#{bin}/bbduk.sh --help 2>&1")
+  end
+end
