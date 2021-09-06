a4491253379ca484a48dded02e3c577e0ebeca87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-06 17:11:23 +0000

    buildozer: update 4.2.0 bottle.

--- Formula/buildozer.rb
+++ Formula/buildozer.rb
@@ -7,11 +7,11 @@ class Buildozer < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6e6fbe3529f8024adcfb4a07a878d24290e0005bdb139a370f955750311470c6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a4d0c0665fff008552b629d97d0194a47e272e110fa01843fc95042163ae8d53"
-    sha256 cellar: :any_skip_relocation, catalina:      "9860c62aa97c12fdf060ec4046bddecd00235d420649ceccd23a290c251ef4bd"
-    sha256 cellar: :any_skip_relocation, mojave:        "8dd939f4e3e8575d5ad4f8a493f318a5f6659dbb9efc52a53fc3ca3821bd59e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66aedd3ebf3619bac0d626b0ca658f590c0f932043920c90039a9aaf0e9f2b8e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c1bedbb3305b3e492ac9b8f84ce653e7e9daa8efa21e82f96278f865849d4759"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c101f965a5430d204d3449f8389f42cc48cd1fdd751ff000aa353e044cf38030"
+    sha256 cellar: :any_skip_relocation, catalina:      "c101f965a5430d204d3449f8389f42cc48cd1fdd751ff000aa353e044cf38030"
+    sha256 cellar: :any_skip_relocation, mojave:        "c101f965a5430d204d3449f8389f42cc48cd1fdd751ff000aa353e044cf38030"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f3e9521c6adb3a5135b9ce749b57720463bc94bc9c51f7f5428b101e801e4f6"
   end
 
   depends_on "go" => :build
