3ad851c36c9c2acb8ee607e33e72ba3b837c8dd8
Rui Chen <rui@chenrui.dev>
2021-09-05 23:47:47 -0400

    cosign 1.1.0 (new formula)
    cosign: update the checkout
    
    Closes #84748.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cosign.rb
@@ -0,0 +1,32 @@
+class Cosign < Formula
+  desc "Container Signing"
+  homepage "https://github.com/sigstore/cosign"
+  url "https://github.com/sigstore/cosign.git",
+      tag:      "v1.1.0",
+      revision: "67934a685ddc83aa7b0b8a55c911e299117afac5"
+  license "Apache-2.0"
+  head "https://github.com/sigstore/cosign.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    pkg = "github.com/sigstore/cosign/cmd/cosign/cli"
+    ldflags = %W[
+      -s -w
+      -X #{pkg}.GitVersion=#{version}
+      -X #{pkg}.gitCommit=#{Utils.git_head}
+      -X #{pkg}.gitTreeState="clean"
+      -X #{pkg}.buildDate=#{time.iso8601}
+    ].join(" ")
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/cosign"
+  end
+
+  test do
+    assert_match "Private key written to cosign.key",
+      pipe_output("#{bin}/cosign generate-key-pair 2>&1", "foo\nfoo\n")
+    assert_predicate testpath/"cosign.pub", :exist?
+
+    assert_match version.to_s, shell_output(bin/"cosign version")
+  end
+end
