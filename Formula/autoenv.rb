e56c258f467c3c889ea5eee60e78229ebc71be72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-06 09:11:46 +0000

    autoenv: update 0.3.0 bottle.

--- Formula/autoenv.rb
+++ Formula/autoenv.rb
@@ -7,7 +7,7 @@ class Autoenv < Formula
   head "https://github.com/hyperupcall/autoenv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "657fdd3a35ac9967764be96cd948ad27ac9eb3160120ff16d77c24b7ab15cd09"
+    sha256 cellar: :any_skip_relocation, all: "879dc65b4f4740aed9cf1960c2b2da66f877f10c4ad774084373e36097d8efb0"
   end
 
   def install
