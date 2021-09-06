9f5b52314d25187af45a761dedb4d5fcdef833de
rui <rui@chenrui.dev>
2021-09-06 16:30:37 +0000

    flamebearer: add 1.1.3 bottle.

--- Formula/flamebearer.rb
+++ Formula/flamebearer.rb
@@ -7,6 +7,14 @@ class Flamebearer < Formula
   sha256 "e787b71204f546f79360fd103197bc7b68fb07dbe2de3a3632a3923428e2f5f1"
   license "ISC"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c58234912fa727a85aa1f072c14a7fd49aae68ff6023325baf045ddec3b7c3a1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "98988646062bd8230583e9bbb7cad33fadcde48cae70d92a6011580494cc84f6"
+    sha256 cellar: :any_skip_relocation, catalina:      "98988646062bd8230583e9bbb7cad33fadcde48cae70d92a6011580494cc84f6"
+    sha256 cellar: :any_skip_relocation, mojave:        "98988646062bd8230583e9bbb7cad33fadcde48cae70d92a6011580494cc84f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c58234912fa727a85aa1f072c14a7fd49aae68ff6023325baf045ddec3b7c3a1"
+  end
+
   depends_on "node"
 
   def install
