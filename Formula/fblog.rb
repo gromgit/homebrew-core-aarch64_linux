586be1b2f95f93758bab2bff8d29160710e17224
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-07 18:06:03 +0000

    fblog: update 3.0.2 bottle.

--- Formula/fblog.rb
+++ Formula/fblog.rb
@@ -7,12 +7,11 @@ class Fblog < Formula
   head "https://github.com/brocode/fblog.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0db7ff023c0be59803cb2d7e4bafd73e5b582b55b5c11a8bca7d24cd1697747f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1f7f7bc4090b60cef5e30c70e12b476e7664e7c4910f49dbb51255d4d81c4050"
-    sha256 cellar: :any_skip_relocation, catalina:      "18a0a07a3af24fa74b4590385f297441379005a0e4e3f6c79895b38c5cef6ccf"
-    sha256 cellar: :any_skip_relocation, mojave:        "b8da5a81c66d27b82d8e604444dd31807b4c567e73c8c80a84704df402e6ec56"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8e7bf6bf388b11ebcf78a0f6442b2dd2673ca7e99b93282f59a38103e2ee3293"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2672eb2839cade14d1702c5aaf2a5e4c3192b5294562d5ea43ed6ae3741a122"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f83b37b77e72adbbf90fe6679ec929d199e70de6e73d94bf9bf0707e9c952344"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ed009c9b8a0ec2ea2c321f386542a409790605ba93fb186ee003361c98f29d40"
+    sha256 cellar: :any_skip_relocation, catalina:      "d3118970c617f0b55a5512ea90cdc030809d7f9a1f4c8d06b13c5db013e344a6"
+    sha256 cellar: :any_skip_relocation, mojave:        "419b3983e3ae3bc752d458f2f6aea11f13f4dc1ac48cb334a4d8771c8ceec134"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7fa6e512521710cb26f1cc8f8caa362598110ac1e8fcb94fab560452a211eaa7"
   end
 
   depends_on "rust" => :build
