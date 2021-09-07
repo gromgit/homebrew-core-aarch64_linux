c4da9d27f4db658b348040428f00f25dfea1f546
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:05:00 +0100

    ed: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/ed.rb
+++ Formula/ed.rb
@@ -20,15 +20,13 @@ class Ed < Formula
     ENV.deparallelize
 
     args = ["--prefix=#{prefix}"]
-    on_macos do
-      args << "--program-prefix=g"
-    end
+    args << "--program-prefix=g" if OS.mac?
 
     system "./configure", *args
     system "make"
     system "make", "install"
 
-    on_macos do
+    if OS.mac?
       %w[ed red].each do |prog|
         (libexec/"gnubin").install_symlink bin/"g#{prog}" => prog
         (libexec/"gnuman/man1").install_symlink man1/"g#{prog}.1" => "#{prog}.1"
