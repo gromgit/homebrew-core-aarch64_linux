afe3cea6f3001ec848489899243e027f34b0cd38
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:04:31 +0100

    ampl-mp: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/ampl-mp.rb
+++ Formula/ampl-mp.rb
@@ -37,7 +37,7 @@ class AmplMp < Formula
   def install
     system "cmake", ".", *std_cmake_args, "-DBUILD_SHARED_LIBS=True"
     system "make", "all"
-    on_macos do
+    if OS.mac?
       MachO::Tools.change_install_name("bin/libasl.dylib", "@rpath/libmp.3.dylib",
                                        "#{opt_lib}/libmp.dylib")
     end
