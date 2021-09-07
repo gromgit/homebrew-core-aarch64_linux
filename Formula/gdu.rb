03baccdd9800a48a565790a727291ddebcac865c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-07 03:06:24 +0000

    gdu: update 5.7.0 bottle.

--- Formula/gdu.rb
+++ Formula/gdu.rb
@@ -6,11 +6,11 @@ class Gdu < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5d91d36d764a608ebf994a623e716c5d55a31b507424028d810e40c4c18ff0ee"
-    sha256 cellar: :any_skip_relocation, big_sur:       "780421d05f8b84d4d7bc8734f2760e264d213eb3988b57daa3832df9c9e7bd5b"
-    sha256 cellar: :any_skip_relocation, catalina:      "b28cd7f66c19e09244d37a68c79277b05e5c5af333066c47dc6b39de085db2af"
-    sha256 cellar: :any_skip_relocation, mojave:        "72c0898eb316be7a9b839fc33438ff6c50d240f2fd1032468df91b3842301416"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "116c79a59061a141b60def54121021bc704c09a7e04c46e6ae763e4c4fa006d3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1bb9a6c97258c9b580a529869ba6863b5fc0e6043112d85922afd12525840fd2"
+    sha256 cellar: :any_skip_relocation, big_sur:       "52dae1c25356b63aabba7b68ada6b1f5fcd964457aef1717800612ff994fa9b4"
+    sha256 cellar: :any_skip_relocation, catalina:      "3dbe69eb522789e0b88d7b6a35ff8574c45609e7d5d30484cf9b38c70533d4a5"
+    sha256 cellar: :any_skip_relocation, mojave:        "a96d1c6f56e188fc8728dc62c14985d769f39d552054feeec1fa3101960c2c0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16b277a774d69bc9d3899013943f204f3b89a3afb6671b7b96ecea65375d162f"
   end
 
   depends_on "go" => :build
