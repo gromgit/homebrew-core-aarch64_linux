09d1e518c92a3028e99781e71d5be8a6941cd77a
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:04:53 +0100

    dcmtk: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -31,14 +31,10 @@ class Dcmtk < Formula
       system "cmake", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args, ".."
       system "make", "install"
 
-      on_macos do
+      if OS.mac?
         inreplace lib/"cmake/dcmtk/DCMTKConfig.cmake", "#{HOMEBREW_SHIMS_PATH}/mac/super/", ""
-      end
-
-      on_linux do
-        if File.readlines(lib/"cmake/dcmtk/DCMTKConfig.cmake").grep(/#{HOMEBREW_SHIMS_PATH}/o).any?
-          inreplace lib/"cmake/dcmtk/DCMTKConfig.cmake", "#{HOMEBREW_SHIMS_PATH}/linux/super/", ""
-        end
+      elsif File.readlines(lib/"cmake/dcmtk/DCMTKConfig.cmake").grep(/#{HOMEBREW_SHIMS_PATH}/o).any?
+        inreplace lib/"cmake/dcmtk/DCMTKConfig.cmake", "#{HOMEBREW_SHIMS_PATH}/linux/super/", ""
       end
     end
   end
