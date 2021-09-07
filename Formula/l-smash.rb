3ad1cd828c631abed4cc5908351842612ad469cd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-07 09:19:52 +0800

    l-smash: remove `shallow: false`
    Continuation of #77981.

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -2,7 +2,6 @@ class LSmash < Formula
   desc "Tool for working with MP4 files"
   homepage "https://l-smash.github.io/l-smash/"
   url "https://github.com/l-smash/l-smash.git",
-      shallow:  false,
       tag:      "v2.9.1",
       revision: "4cea08d264933634db5bc06da9d8d88fb5ddae07"
   license "ISC"
