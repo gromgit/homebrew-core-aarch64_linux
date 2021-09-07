38703cd835a1bf802a28aabefe2e89c6a8dbf60d
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:04:36 +0100

    augustus: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/augustus.rb
+++ Formula/augustus.rb
@@ -48,7 +48,7 @@ class Augustus < Formula
     system "make", "clean"
 
     cd "src" do
-      on_macos do
+      if OS.mac?
         # Clang breaks proteinprofile on macOS. This issue has been first reported
         # to upstream in 2016 (see https://github.com/nextgenusfs/funannotate/issues/3).
         # See also https://github.com/Gaius-Augustus/Augustus/issues/64
@@ -56,8 +56,7 @@ class Augustus < Formula
         with_env("HOMEBREW_CC" => Formula["gcc"].opt_bin/"gcc-#{gcc_major_ver}") do
           system "make"
         end
-      end
-      on_linux do
+      else
         system "make"
       end
     end
