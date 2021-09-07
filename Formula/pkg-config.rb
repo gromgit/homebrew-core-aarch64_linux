35521e77d0393331f26cd7a52d16eb5f427162dd
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:06:41 +0100

    pkg-config: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -25,14 +25,14 @@ class PkgConfig < Formula
       #{HOMEBREW_PREFIX}/lib/pkgconfig
       #{HOMEBREW_PREFIX}/share/pkgconfig
     ]
-    on_macos do
+    pc_path << if OS.mac?
       pc_path << "/usr/local/lib/pkgconfig"
       pc_path << "/usr/lib/pkgconfig"
-      pc_path << "#{HOMEBREW_LIBRARY}/Homebrew/os/mac/pkgconfig/#{MacOS.version}"
+      "#{HOMEBREW_LIBRARY}/Homebrew/os/mac/pkgconfig/#{MacOS.version}"
+    else
+      "#{HOMEBREW_LIBRARY}/Homebrew/os/linux/pkgconfig"
     end
-    on_linux do
-      pc_path << "#{HOMEBREW_LIBRARY}/Homebrew/os/linux/pkgconfig"
-    end
+
     pc_path = pc_path.uniq.join(File::PATH_SEPARATOR)
 
     system "./configure", "--disable-debug",
