f25dc8bb78bd94430acea180f74829083ca15341
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:06:45 +0100

    postgresql@9.6: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/postgresql@9.6.rb
+++ Formula/postgresql@9.6.rb
@@ -60,7 +60,7 @@ class PostgresqlAT96 < Formula
       --with-perl
       --with-uuid=e2fs
     ]
-    on_macos do
+    if OS.mac?
       args += %w[
         --with-bonjour
         --with-tcl
@@ -84,7 +84,7 @@ class PostgresqlAT96 < Formula
     # Attempting to fix that by adding a dependency on `open-sp` doesn't
     # work and the build errors out on generating the documentation, so
     # for now let's simply omit it so we can package Postgresql for Mojave.
-    on_macos do
+    if OS.mac?
       if DevelopmentTools.clang_build_version >= 1000
         system "make", "all"
         system "make", "-C", "contrib", "install", "all", *dirs
@@ -92,8 +92,7 @@ class PostgresqlAT96 < Formula
       else
         system "make", "install-world", *dirs
       end
-    end
-    on_linux do
+    else
       system "make", "all"
       system "make", "-C", "contrib", "install", "all", *dirs
       system "make", "install", "all", *dirs
