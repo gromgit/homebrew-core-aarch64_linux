9d98eb36b8825ffb4a3c304efe1329580f691656
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:05:03 +0100

    erlang@23: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/erlang@23.rb
+++ Formula/erlang@23.rb
@@ -49,7 +49,7 @@ class ErlangAT23 < Formula
       --without-javac
     ]
 
-    on_macos do
+    if OS.mac?
       args << "--enable-darwin-64bit"
       args << "--enable-kernel-poll" if MacOS.version > :el_capitan
       args << "--with-dynamic-trace=dtrace" if MacOS::CLT.installed?
