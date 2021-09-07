ecff7515615eae7b049cd5a9bab525dcc373d4c7
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:06:19 +0100

    mysql@5.7: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/mysql@5.7.rb
+++ Formula/mysql@5.7.rb
@@ -38,7 +38,7 @@ class MysqlAT57 < Formula
   patch :DATA
 
   def install
-    on_linux do
+    if OS.linux?
       # Fix libmysqlgcs.a(gcs_logging.cc.o): relocation R_X86_64_32
       # against `_ZN17Gcs_debug_options12m_debug_noneB5cxx11E' can not be used when making
       # a shared object; recompile with -fPIC
