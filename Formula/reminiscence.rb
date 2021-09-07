20b2a5430a4fd6825bc8f8cd2a2c95dec32f76b6
Rui Chen <rui@chenrui.dev>
2021-09-07 00:07:18 -0400

    reminiscence 0.4.9
    Closes #84794.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/reminiscence.rb
+++ Formula/reminiscence.rb
@@ -1,8 +1,8 @@
 class Reminiscence < Formula
   desc "Flashback engine reimplementation"
   homepage "http://cyxdown.free.fr/reminiscence/"
-  url "http://cyxdown.free.fr/reminiscence/REminiscence-0.4.8.tar.bz2"
-  sha256 "1cb6ccb4ac9ec38fa80dd80dbdb24ccf16cf58d2de42a030776a3a7c691cabf8"
+  url "http://cyxdown.free.fr/reminiscence/REminiscence-0.4.9.tar.bz2"
+  sha256 "320463e629c38f2e3aaaa510febacc0c5d88a59f5e906b0500a1dcb9c7e1e935"
 
   livecheck do
     url :homepage
@@ -29,12 +29,22 @@ class Reminiscence < Formula
 
   uses_from_macos "zlib"
 
+  resource "stb_vorbis" do
+    url "https://raw.githubusercontent.com/nothings/stb/1ee679ca2ef753a528db5ba6801e1067b40481b8/stb_vorbis.c"
+    sha256 "4c7cb2ff1f7011e9d67950446b7eb9ca044f2e464d76bfbb0b84dd2e23e65636"
+    version "1.22"
+  end
+
   resource "tremor" do
     url "https://gitlab.xiph.org/xiph/tremor.git",
         revision: "7c30a66346199f3f09017a09567c6c8a3a0eedc8"
   end
 
   def install
+    resource("stb_vorbis").stage do
+      buildpath.install "stb_vorbis.c"
+    end
+
     resource("tremor").stage do
       system "./autogen.sh", "--disable-dependency-tracking",
                              "--disable-silent-rules",
