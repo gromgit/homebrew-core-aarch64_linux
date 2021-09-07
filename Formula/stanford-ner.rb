5356d473f42167ad3f7be4ca98f0e9381fa17494
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-06 23:12:07 -0400

    stanford-ner: add livecheck

--- Formula/stanford-ner.rb
+++ Formula/stanford-ner.rb
@@ -5,6 +5,11 @@ class StanfordNer < Formula
   sha256 "06dd9f827106359bad90049c6952137502bc59ed40b9c88b448831b32cf55b2a"
   license "GPL-2.0-or-later"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?stanford-ner[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f6aace4b941b03a278f815f240b6b8217a52b21ab48f4152a7e0e5b385e0f63f"
   end
