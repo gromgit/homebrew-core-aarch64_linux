1d3fb453c37cd6da1115c409bfcf8d115d318d2e
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:05:09 +0100

    firebase-cli: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -31,7 +31,7 @@ class FirebaseCli < Formula
     term_size_vendor_dir = libexec/"lib/node_modules/firebase-tools/node_modules/term-size/vendor"
     term_size_vendor_dir.rmtree # remove pre-built binaries
 
-    on_macos do
+    if OS.mac?
       macos_dir = term_size_vendor_dir/"macos"
       macos_dir.mkpath
       # Replace the vendored pre-built term-size with one we build ourselves
