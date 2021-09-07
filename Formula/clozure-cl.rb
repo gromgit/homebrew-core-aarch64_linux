96e5b49e49f9942134e92a7c74a9cc99accc8e8f
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:04:49 +0100

    clozure-cl: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/clozure-cl.rb
+++ Formula/clozure-cl.rb
@@ -40,15 +40,13 @@ class ClozureCl < Formula
     tmpdir = Pathname.new(Dir.mktmpdir)
     tmpdir.install resource("bootstrap")
 
-    on_macos do
+    if OS.mac?
       buildpath.install tmpdir/"dx86cl64.image"
       buildpath.install tmpdir/"darwin-x86-headers64"
       cd "lisp-kernel/darwinx8664" do
         system "make"
       end
-    end
-
-    on_linux do
+    else
       buildpath.install tmpdir/"lx86cl64"
       buildpath.install tmpdir/"lx86cl64.image"
       buildpath.install tmpdir/"x86-headers64"
@@ -56,16 +54,14 @@ class ClozureCl < Formula
 
     ENV["CCL_DEFAULT_DIRECTORY"] = buildpath
 
-    on_macos do
+    if OS.mac?
       system "./dx86cl64", "-n", "-l", "lib/x8664env.lisp",
             "-e", "(ccl:xload-level-0)",
             "-e", "(ccl:compile-ccl)",
             "-e", "(quit)"
       (buildpath/"image").write('(ccl:save-application "dx86cl64.image")\n(quit)\n')
       system "cat image | ./dx86cl64 -n --image-name x86-boot64.image"
-    end
-
-    on_linux do
+    else
       system "./lx86cl64", "-n", "-l", "lib/x8664env.lisp",
             "-e", "(ccl:rebuild-ccl :full t)",
             "-e", "(quit)"
