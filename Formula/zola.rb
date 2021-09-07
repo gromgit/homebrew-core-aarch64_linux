dfa45b3a40d3675c1eaf5d3db98ffaf5f5861888
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:07:26 +0100

    zola: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.
    
    Closes #84820

--- Formula/zola.rb
+++ Formula/zola.rb
@@ -21,9 +21,7 @@ class Zola < Formula
   end
 
   def install
-    on_linux do
-      ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
-    end
+    ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix if OS.linux?
     system "cargo", "install", *std_cargo_args
 
     bash_completion.install "completions/zola.bash"
