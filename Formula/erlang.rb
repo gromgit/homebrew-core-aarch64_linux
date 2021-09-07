a259d7cae545b4ec18527a8cc7b738b4574c713a
Mike McQuaid <mike@mikemcquaid.com>
2021-09-07 16:05:02 +0100

    erlang: fix brew style.
    Part of https://github.com/Homebrew/brew/pull/11955.

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -58,7 +58,7 @@ class Erlang < Formula
       --without-javac
     ]
 
-    on_macos do
+    if OS.mac?
       args << "--enable-darwin-64bit"
       args << "--enable-kernel-poll" if MacOS.version > :el_capitan
       args << "--with-dynamic-trace=dtrace" if MacOS::CLT.installed?
