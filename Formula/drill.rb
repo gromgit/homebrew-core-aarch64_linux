9f4777b8f28c9bf6533d2353d24e21e7acb928e5
Rui Chen <rui@chenrui.dev>
2021-09-07 00:12:47 -0400

    drill 0.7.2
    * drill 0.7.2
    * drill: add head build
    
    Closes #84800.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/drill.rb
+++ Formula/drill.rb
@@ -1,9 +1,10 @@
 class Drill < Formula
   desc "HTTP load testing application written in Rust"
   homepage "https://github.com/fcsonline/drill"
-  url "https://github.com/fcsonline/drill/archive/0.7.0.tar.gz"
-  sha256 "0041e4b8e8bae0bfc4dfc42b0d1c9a65a8a04dfb68766138d3ae85407e42b15f"
+  url "https://github.com/fcsonline/drill/archive/0.7.2.tar.gz"
+  sha256 "cc33f5e214cf8c9c975bd2b912b87541eab2ceb34689fdc1f4882b332ad4ee44"
   license "GPL-3.0-or-later"
+  head "https://github.com/fcsonline/drill.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "4332ccf85b364c2cda268ad8e932594a999dc6c6a911e77a7cf5fff2df5eb628"
