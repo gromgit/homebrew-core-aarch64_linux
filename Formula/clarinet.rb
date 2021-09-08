6888aefe96746dd747ffbf998c94c66243e5235a
Caleb Xu <calebcenter@live.com>
2021-09-08 10:23:25 -0400

    clarinet: fix build on Linux

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -20,6 +20,11 @@ class Clarinet < Formula
 
   depends_on "rust" => :build
 
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
