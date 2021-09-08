e5618b2ad1c5d0fc0920f554277f8d09ae90bf7c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-08 11:52:57 +0000

    notcurses: update 2.4.0 bottle.

--- Formula/notcurses.rb
+++ Formula/notcurses.rb
@@ -7,10 +7,10 @@ class Notcurses < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 arm64_big_sur: "3206f7d892bd1b7030aaad87456d38e792e7a4f1db6a1bf236812fb806fca966"
-    sha256 big_sur:       "c3d97b3bb9d3ad6c5c71ea9b8db6af548d2f26d621f63d2fcb3b4500623deb14"
-    sha256 catalina:      "631a30874cf16b202102fb0094b38e641f72554b9043277115b70ee8b449b7fe"
-    sha256 mojave:        "7e5ba57cea44e6377410f50d119eb372656944e874fb1391efbce66e01fb6802"
+    sha256 arm64_big_sur: "2214f51b0442322823fe21edc073aed06e993d95277a628fae2a1575f0a5d9a4"
+    sha256 big_sur:       "a2b0ccf2c2cf22964ec0678aa8dbc04d3ee326c8314fcb82408c6154fa6bd4e9"
+    sha256 catalina:      "b35722ef54ab47ac89a2a57bbcc8d9d28b817a6c751e360abf8a291614f712d0"
+    sha256 mojave:        "92f08c1c6e076a8b8cee89a326c2ecf8e31d47bc0d40cbfced7e802629959998"
   end
 
   depends_on "cmake" => :build
