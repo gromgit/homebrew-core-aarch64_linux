6ddf867d7fc3192480fb775e5fcb43d39fec037f
Mike McQuaid <mike@mikemcquaid.com>
2021-09-08 13:56:42 +0100

    petsc-complex: use Superenv.shims_path
    Added in https://github.com/Homebrew/brew/pull/11987.

--- Formula/petsc-complex.rb
+++ Formula/petsc-complex.rb
@@ -44,9 +44,9 @@ class PetscComplex < Formula
     rm_f lib/"petsc/conf/configure-hash"
 
     if OS.mac?
-      inreplace lib/"petsc/conf/petscvariables", "#{HOMEBREW_SHIMS_PATH}/mac/super/", ""
-    elsif File.readlines("#{lib}/petsc/conf/petscvariables").grep(/#{HOMEBREW_SHIMS_PATH}/o).any?
-      inreplace lib/"petsc/conf/petscvariables", "#{HOMEBREW_SHIMS_PATH}/linux/super/", ""
+      inreplace lib/"petsc/conf/petscvariables", Superenv.shims_path, ""
+    elsif File.readlines("#{lib}/petsc/conf/petscvariables").grep(Superenv.shims_path.to_s).any?
+      inreplace lib/"petsc/conf/petscvariables", Superenv.shims_path, ""
     end
   end
 
