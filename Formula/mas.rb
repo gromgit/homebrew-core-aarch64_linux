385900dc599afb930be82f1633fd8050dc15ae93
Chris Araman <chris.araman@gmail.com>
2021-09-08 15:47:40 -0700

    mas 1.8.3
    Closes #84888.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -2,8 +2,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.2",
-      revision: "2f2a43b425498f3cee50974116b8c9d27adbb7cb"
+      tag:      "v1.8.3",
+      revision: "aeeb1c508e98d657769ef4e368a113be7822d92e"
   license "MIT"
   head "https://github.com/mas-cli/mas.git"
 
