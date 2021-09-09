64d30f8b9371023109cd7bfb87e830eead4c6d81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-09 05:07:43 +0000

    tendermint: update 0.34.13 bottle.

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -7,10 +7,10 @@ class Tendermint < Formula
   head "https://github.com/tendermint/tendermint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d069288a129924e9c6c8c80af2ccaf78b71b724c794e1830ebbbba6669d09f0d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9473894f1f5e2d7e08817844045172af8a62b17db2ddd819fb019aa9014aa587"
-    sha256 cellar: :any_skip_relocation, catalina:      "9473894f1f5e2d7e08817844045172af8a62b17db2ddd819fb019aa9014aa587"
-    sha256 cellar: :any_skip_relocation, mojave:        "9473894f1f5e2d7e08817844045172af8a62b17db2ddd819fb019aa9014aa587"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b468b0925db42368fd268c0a21216ef36edf527ad86d6d6b39a2639b544985b7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "897e121110439f075ea2fc2e0cca767cba9f6b8e8df142d06e335e95be571452"
+    sha256 cellar: :any_skip_relocation, catalina:      "897e121110439f075ea2fc2e0cca767cba9f6b8e8df142d06e335e95be571452"
+    sha256 cellar: :any_skip_relocation, mojave:        "897e121110439f075ea2fc2e0cca767cba9f6b8e8df142d06e335e95be571452"
   end
 
   depends_on "go" => :build
