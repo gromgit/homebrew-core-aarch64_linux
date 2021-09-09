af5d4d8ea6b96d3216edc054dbc6acc656119bb6
Martin Polden <mpolden@yahoo-inc.com>
2021-09-09 08:44:10 +0200

    vespa-cli 7.462.20 (new formula)
    Closes #84908.
    
    Closes #84988.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/vespa-cli.rb
@@ -0,0 +1,32 @@
+class VespaCli < Formula
+  desc "Command-line tool for Vespa.ai"
+  homepage "https://vespa.ai"
+  url "https://github.com/vespa-engine/vespa/archive/vespa-7.462.20-1.tar.gz"
+  version "7.462.20"
+  sha256 "87e309fbdac0bef174ef3ac1bf094d551a54dd2da57ecc52e19880d0d81a9cda"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    cd "client/go" do
+      with_env(VERSION: version.to_s) do
+        system "make", "all", "manpages"
+      end
+      bin.install "bin/vespa"
+      man1.install Dir["share/man/man1/vespa*.1"]
+      (bash_completion/"vespa").write Utils.safe_popen_read(bin/"vespa", "completion", "bash")
+      (fish_completion/"vespa.fish").write Utils.safe_popen_read(bin/"vespa", "completion", "fish")
+      (zsh_completion/"_vespa").write Utils.safe_popen_read(bin/"vespa", "completion", "zsh")
+    end
+  end
+
+  test do
+    ENV["VESPA_CLI_HOME"] = testpath
+    assert_match "vespa version #{version}", shell_output("#{bin}/vespa version")
+    query = "yql=select * from sources * where title contains 'foo';"
+    assert_match "Error: Request failed", shell_output("#{bin}/vespa query -t local '#{query}' hits=5")
+    system "#{bin}/vespa", "config", "set", "target", "cloud"
+    assert_match "target = cloud", shell_output("#{bin}/vespa config get target")
+  end
+end
