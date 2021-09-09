578f71b3e1e806dafa36e4472f698da8c332f8a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-09 13:11:45 +0000

    py-spy: update 0.3.9 bottle.

--- Formula/py-spy.rb
+++ Formula/py-spy.rb
@@ -7,10 +7,10 @@ class PySpy < Formula
   head "https://github.com/benfred/py-spy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "22121c8eaec4a7cd913ae2add7287df36ad0e9a32f8f3a141faeef12401672cc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4dffb9d38caafb69e1bb3f3183a668102679b17824205a6a54e55966713cfcd5"
-    sha256 cellar: :any_skip_relocation, catalina:      "915ac43c4c40dbac5d0abf7c014a335264d0d34e1d36e6f5eefc02147a296375"
-    sha256 cellar: :any_skip_relocation, mojave:        "58015a69ab226cdbb2a2a28d9094cd9b90c23743aee394566e86c63f3cbc128c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6a2e9bd709864ff5c99ac64d761a8bc50afce0222717c78b43135da05519449c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4a3a7ff1984e412ec62dc5f00c4699cfd33eb30c6d1711055be9428617a72748"
+    sha256 cellar: :any_skip_relocation, catalina:      "8cd7695d35222cb75c59ffff972eac470acc332a815c63ce16cc2eb3cc4559af"
+    sha256 cellar: :any_skip_relocation, mojave:        "dfb3079b96434ce14dd438a69830b31a27d9a0b7e6875df861b47dfbd9f9c6ca"
   end
 
   depends_on "rust" => :build
