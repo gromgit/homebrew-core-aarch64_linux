013ca5784131e2bdf233d918bbcebc4d6ab471d5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-10 05:08:59 +0000

    simdjson: update 1.0.0 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -7,10 +7,11 @@ class Simdjson < Formula
   head "https://github.com/simdjson/simdjson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b3973e31d27a2bbf477ab7b1daa2fe9ae9302340a56f91c6dd2b991af5547ec2"
-    sha256 cellar: :any, big_sur:       "1544c6932b3ac405e24c7c50f5b32f6918c99615a575b56bae19d29295d47e90"
-    sha256 cellar: :any, catalina:      "1abef2e6ba683d83371fcd220578408b7d659b9d57c73bebe87b99281b03e31a"
-    sha256 cellar: :any, mojave:        "f6694a3a2e21b588d3909bd82095a461713021b11aae7d0b683da04c3de732d5"
+    sha256 cellar: :any,                 arm64_big_sur: "b3973e31d27a2bbf477ab7b1daa2fe9ae9302340a56f91c6dd2b991af5547ec2"
+    sha256 cellar: :any,                 big_sur:       "1544c6932b3ac405e24c7c50f5b32f6918c99615a575b56bae19d29295d47e90"
+    sha256 cellar: :any,                 catalina:      "1abef2e6ba683d83371fcd220578408b7d659b9d57c73bebe87b99281b03e31a"
+    sha256 cellar: :any,                 mojave:        "f6694a3a2e21b588d3909bd82095a461713021b11aae7d0b683da04c3de732d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4eca3b5b64d76b216c71d214b3e5556c62ab92bf3adbc5beb6e0ac0b1ac05926"
   end
 
   depends_on "cmake" => :build
