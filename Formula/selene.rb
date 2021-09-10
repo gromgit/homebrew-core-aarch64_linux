8124eda1c5d45c2584864065974c529ac57b5456
Charles B Johnson <mail@cbjohnson.info>
2021-09-09 22:18:23 -0500

    selene 0.14.0 (new formula)
    Closes #84979.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/selene.rb
@@ -0,0 +1,25 @@
+class Selene < Formula
+  desc "Blazing-fast modern Lua linter"
+  homepage "https://kampfkarren.github.io/selene"
+  url "https://github.com/Kampfkarren/selene/archive/0.14.0.tar.gz"
+  sha256 "7accab1f2e2c32af7ca3a28da91f0f3ff89a9437a4d6008a9514f13009912fe3"
+  license "MPL-2.0"
+
+  depends_on "rust" => :build
+  depends_on "openssl@1.1"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
+  def install
+    cd "selene" do
+      system "cargo", "install", "--bin", "selene", *std_cargo_args
+    end
+  end
+
+  test do
+    (testpath/"test.lua").write("print(1 / 0)")
+    assert_match "warning[divide_by_zero]", shell_output("#{bin}/selene #{testpath}/test.lua", 1)
+  end
+end
