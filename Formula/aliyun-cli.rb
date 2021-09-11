c14236445a1b407a977b4391e6660dba61c13385
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-11 14:01:13 +0800

    aliyun-cli: align arguments to `url`
    This makes formatting more consistent across formulae.

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -2,8 +2,8 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-    tag:      "v3.0.90",
-    revision: "53042da35bce68987fd093b58d40845586daa137"
+      tag:      "v3.0.90",
+      revision: "53042da35bce68987fd093b58d40845586daa137"
   license "Apache-2.0"
 
   bottle do
