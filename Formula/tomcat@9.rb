b1ce2698f23dd0671e447d86c3b4ba732d6879ec
MoonFruit <dkmoonfruit@gmail.com>
2021-09-11 11:05:53 +0800

    tomcat@9 9.0.53
    Closes #85018.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomcat@9.rb
+++ Formula/tomcat@9.rb
@@ -1,11 +1,10 @@
 class TomcatAT9 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.52/bin/apache-tomcat-9.0.52.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.52/bin/apache-tomcat-9.0.52.tar.gz"
-  sha256 "29909c659fda7a2e06be17684a1fadbbae9ed4cdecf8077c4ede1c34e4d4a631"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.53/bin/apache-tomcat-9.0.53.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.53/bin/apache-tomcat-9.0.53.tar.gz"
+  sha256 "7b3e456ed76b0b42d99767985dc3774b22e2388834994f8539272eb7c05ab6fd"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
