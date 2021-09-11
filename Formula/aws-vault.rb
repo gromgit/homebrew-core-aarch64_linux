a1ce176ea242929de07d7003804c748bb74bfdee
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-11 18:17:01 +0800

    aws-vault: use `OS.kernel_name`

--- Formula/aws-vault.rb
+++ Formula/aws-vault.rb
@@ -19,8 +19,8 @@ class AwsVault < Formula
     # Remove this line because we don't have a certificate to code sign with
     inreplace "Makefile",
       "codesign --options runtime --timestamp --sign \"$(CERT_ID)\" $(INSTALL_DIR)/aws-vault || true", ""
-    os = OS.mac? ? "darwin" : "linux"
-    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
 
     system "make", "aws-vault-#{os}-#{arch}", "VERSION=#{version}-#{tap.user}"
     system "make", "install", "INSTALL_DIR=#{bin}", "VERSION=#{version}-#{tap.user}"
