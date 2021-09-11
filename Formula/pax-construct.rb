b9d4e5598339ff4f137f6bd708e47ca19738c313
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-11 15:55:51 -0400

    pax-construct: add livecheck

--- Formula/pax-construct.rb
+++ Formula/pax-construct.rb
@@ -1,9 +1,14 @@
 class PaxConstruct < Formula
   desc "Tools to setup and develop OSGi projects quickly"
-  homepage "https://ops4j1.jira.com/wiki/display/paxconstruct/Pax+Construct"
+  homepage "https://ops4j1.jira.com/wiki/spaces/paxconstruct/overview"
   url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/construct/scripts/1.6.0/scripts-1.6.0.zip"
   sha256 "fc832b94a7d095d5ee26b1ce4b3db449261f0154e55b34a7bc430cb685d51064"
   license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/construct/scripts/maven-metadata.xml"
+    regex(%r{<version>\s*v?(\d+(?:\.\d+)+)\s*</version>}i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "7b033ca0a10e6011280fce831ec89b49717cf985137b4134caac26da3669c5a5"
