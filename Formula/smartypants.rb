8e92cba5789009dc77815fa64a554f460fcab209
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-11 16:21:34 -0400

    smartypants: add livecheck

--- Formula/smartypants.rb
+++ Formula/smartypants.rb
@@ -4,6 +4,11 @@ class Smartypants < Formula
   url "https://daringfireball.net/projects/downloads/SmartyPants_1.5.1.zip"
   sha256 "2813a12d8dd23f091399195edd7965e130103e439e2a14f298b75b253616d531"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?SmartyPants[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a1af00dbd1e4d6d42c6251fc9ca0cd36cce3370a9321f183e7f96a5bdebf8c6d"
   end
