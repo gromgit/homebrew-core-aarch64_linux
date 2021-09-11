a071b7346375587acb4050c3f09abe82eb471f76
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-11 14:01:17 +0800

    opensearch-dashboards: align arguments to `url`
    This makes formatting more consistent across formulae.

--- Formula/opensearch-dashboards.rb
+++ Formula/opensearch-dashboards.rb
@@ -2,8 +2,8 @@ class OpensearchDashboards < Formula
   desc "Open source visualization dashboards for OpenSearch"
   homepage "https://opensearch.org/docs/dashboards/index/"
   url "https://github.com/opensearch-project/OpenSearch-Dashboards.git",
-    tag:      "1.0.0",
-    revision: "b15ad258e6960003e609a70889cda5fb39f90947"
+      tag:      "1.0.0",
+      revision: "b15ad258e6960003e609a70889cda5fb39f90947"
   license "Apache-2.0"
 
   bottle do
