4dd651ca6289675edc0d7b0eaf9018c8d70e9761
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-11 18:40:13 -0400

    c-ares: update homepage, stable
    Closes #85079.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c-ares.rb
+++ Formula/c-ares.rb
@@ -1,8 +1,8 @@
 class CAres < Formula
   desc "Asynchronous DNS library"
-  homepage "https://c-ares.haxx.se/"
+  homepage "https://c-ares.org/"
   # Check whether patch for `node.rb` can be removed at version bump
-  url "https://c-ares.haxx.se/download/c-ares-1.17.2.tar.gz"
+  url "https://c-ares.org/download/c-ares-1.17.2.tar.gz"
   sha256 "4803c844ce20ce510ef0eb83f8ea41fa24ecaae9d280c468c582d2bb25b3913d"
   license "MIT"
   head "https://github.com/c-ares/c-ares.git", branch: "main"
