ff4a6aede5731999e2701772c79e0d5cb8e0eb48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-12 04:08:40 +0000

    mdds: update 2.0.0 bottle.

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -6,12 +6,7 @@ class Mdds < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f4c421f18efdd519f3ca12a78295c2b1c5e36f6726369736e68ed07870e40a33"
-    sha256 cellar: :any_skip_relocation, big_sur:       "59ebe66bdf74479076e8df76ba906f2bda539f819c778abaf608acbae04343f3"
-    sha256 cellar: :any_skip_relocation, catalina:      "a5e6a996bf112bca0d5c7e628fd15128977b9075938155a4185aaf5613d136bc"
-    sha256 cellar: :any_skip_relocation, mojave:        "5146b50529f63030c978dbdab3755ce7a7383d7c8049e03ae1186fa231f867c9"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "67f497efa10f695da64e4769d3ef5de6fb0e9d3d0d62026c2105b7c5148b91a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "22d6a3682982e13c35d52afee6e0b9432fd855c51662cc25cfb4531f4fcf3e79"
+    sha256 cellar: :any_skip_relocation, all: "602a95bcf70ac8445a3c964c67c423108b6a58ca5b743d9eb26f5a017776c63f"
   end
 
   head do
