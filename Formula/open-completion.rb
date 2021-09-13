edf4b91ab3af11bfab14bc8a898d9ab9a17f78e6
Rui Chen <rui@chenrui.dev>
2021-09-13 00:40:31 -0400

    open-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/open-completion.rb
+++ Formula/open-completion.rb
@@ -4,7 +4,7 @@ class OpenCompletion < Formula
   url "https://github.com/moshen/open-bash-completion/archive/v1.0.4.tar.gz"
   sha256 "23a8a30f9f65f5b3eb60aa6f2d1c60d7d0a858ea753a58f31ddb51d40e16b668"
   license "MIT"
-  head "https://github.com/moshen/open-bash-completion.git"
+  head "https://github.com/moshen/open-bash-completion.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "5ea91a3335f030fb57487c30aa307dee22587db31ff801bb7bc6a121775ba579"
