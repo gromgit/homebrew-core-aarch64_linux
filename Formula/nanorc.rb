88f41bc54e7c76f823a5b62d26111bf3d494ade5
Rui Chen <rui@chenrui.dev>
2021-09-13 10:10:17 -0400

    nanorc: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/nanorc.rb
+++ Formula/nanorc.rb
@@ -4,7 +4,7 @@ class Nanorc < Formula
   url "https://github.com/scopatz/nanorc/releases/download/2020.10.10/nanorc-2020.10.10.tar.gz"
   sha256 "cd674e9eb230e4ba306b418c22d1891d93a3d2ffdf22234748d3398da50dfe64"
   license "GPL-3.0"
-  head "https://github.com/scopatz/nanorc.git"
+  head "https://github.com/scopatz/nanorc.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "bf71c9da19d2ac1b05163bd9f92ea86adc475ca15d3e95101c91d788ff781625"
