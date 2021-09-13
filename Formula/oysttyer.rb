776087748b78d538965fb53eeb01ea13c6344c1d
Rui Chen <rui@chenrui.dev>
2021-09-13 00:40:34 -0400

    oysttyer: add `head` branch
    Closes #85116.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oysttyer.rb
+++ Formula/oysttyer.rb
@@ -3,7 +3,7 @@ class Oysttyer < Formula
   homepage "https://github.com/oysttyer/oysttyer"
   url "https://github.com/oysttyer/oysttyer/archive/2.10.0.tar.gz"
   sha256 "3c0ce1c7b112f2db496cc75a6e76c67f1cad956f9e7812819c6ae7a979b2baea"
-  head "https://github.com/oysttyer/oysttyer.git"
+  head "https://github.com/oysttyer/oysttyer.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a1700139674a1c4bdfb17dc7b5c7d513f835ac100fd419ac07672cdd6f78ec1b"
