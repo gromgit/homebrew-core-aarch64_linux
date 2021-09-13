40c0d1936aab32cf1e725349731075369d3dd2e0
Rui Chen <rui@chenrui.dev>
2021-09-13 10:10:17 -0400

    narwhal: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/narwhal.rb
+++ Formula/narwhal.rb
@@ -4,7 +4,7 @@ class Narwhal < Formula
   url "https://github.com/280north/narwhal/archive/v0.3.2.tar.gz"
   sha256 "a26ac20097839a5c7b5de665678fb76699371eea433d6e3b820d4d8de2ad4937"
   license "MIT"
-  head "https://github.com/280north/narwhal.git"
+  head "https://github.com/280north/narwhal.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "74667a2b115b6ee5a337b291882a06a3df9b4292a5a2b11fb5470e38abd228bc"
