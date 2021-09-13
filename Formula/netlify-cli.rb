0c522533d5597948ac152571d408f69a831cc7ba
Rui Chen <rui@chenrui.dev>
2021-09-13 10:10:19 -0400

    netlify-cli: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -6,7 +6,7 @@ class NetlifyCli < Formula
   url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-6.8.10.tgz"
   sha256 "bea250205a00c79b939ef50cdbcd5c2edf8b9cd23c0429f3b97ade5ab529fee6"
   license "MIT"
-  head "https://github.com/netlify/cli.git"
+  head "https://github.com/netlify/cli.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "1705353014474fab897cb1cf5c38bcb4998d079247cd9c94831a1a087290f8bc"
