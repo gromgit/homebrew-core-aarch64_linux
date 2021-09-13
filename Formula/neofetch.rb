0ee1b5f2ddf16f4b0184ac6d91a9257726b1fec5
Rui Chen <rui@chenrui.dev>
2021-09-13 10:10:18 -0400

    neofetch: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -4,7 +4,7 @@ class Neofetch < Formula
   url "https://github.com/dylanaraps/neofetch/archive/7.1.0.tar.gz"
   sha256 "58a95e6b714e41efc804eca389a223309169b2def35e57fa934482a6b47c27e7"
   license "MIT"
-  head "https://github.com/dylanaraps/neofetch.git"
+  head "https://github.com/dylanaraps/neofetch.git", branch: "master"
 
   bottle do
     rebuild 2
