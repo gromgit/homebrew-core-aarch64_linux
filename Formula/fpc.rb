59df9fa9bafc3045d9c80010ea921f6a902cfe7d
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-09-13 12:19:46 +0000

    fpc: update 3.2.2_1 bottle.

--- Formula/fpc.rb
+++ Formula/fpc.rb
@@ -15,10 +15,11 @@ class Fpc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b4efbb9f568afadfb27aab8ca80895b7f306f58c7ff8a0623f2bd8418338b745"
-    sha256 cellar: :any, big_sur:       "4c3a012398b6136776358206b0cac52ec1096484c27a08c142e7f51afc713956"
-    sha256 cellar: :any, catalina:      "1bbaa4c1b6a616f8a56554b30c69cae267d22849074eb628d77c23af2e911e6e"
-    sha256 cellar: :any, mojave:        "314265a7bff5c2f8a613d1c04db8856f6523d8d00d33435892260ef3fa9cc604"
+    sha256 cellar: :any,                 arm64_big_sur: "b4efbb9f568afadfb27aab8ca80895b7f306f58c7ff8a0623f2bd8418338b745"
+    sha256 cellar: :any,                 big_sur:       "4c3a012398b6136776358206b0cac52ec1096484c27a08c142e7f51afc713956"
+    sha256 cellar: :any,                 catalina:      "1bbaa4c1b6a616f8a56554b30c69cae267d22849074eb628d77c23af2e911e6e"
+    sha256 cellar: :any,                 mojave:        "314265a7bff5c2f8a613d1c04db8856f6523d8d00d33435892260ef3fa9cc604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3491933cdf5782d3c4b9b1188757cb3846b5d823a6db75c8fb56f13b23bc6747"
   end
 
   on_macos do
