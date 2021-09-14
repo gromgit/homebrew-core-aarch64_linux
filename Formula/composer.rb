8b6fe162c52c373fac341e53d2212eed046cc131
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-14 14:07:08 +0000

    composer: update 2.1.7 bottle.

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -11,11 +11,11 @@ class Composer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2b4fec192c3276fc11f9c4bb593e7a28d42e1139107b7517377998f4040591ee"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ded5ca31c045db4fa534d30a45ebea8b5aca70e31e32361b34565377b267ab14"
-    sha256 cellar: :any_skip_relocation, catalina:      "ded5ca31c045db4fa534d30a45ebea8b5aca70e31e32361b34565377b267ab14"
-    sha256 cellar: :any_skip_relocation, mojave:        "ded5ca31c045db4fa534d30a45ebea8b5aca70e31e32361b34565377b267ab14"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b4fec192c3276fc11f9c4bb593e7a28d42e1139107b7517377998f4040591ee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "222d26e07d276a0d17d264e12dbe657b959d8fb006a4365640f9149c66055160"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9b514659ab7804211b191eaf4f6d5ca274cc77711073c8b4eab305c0955a8fd9"
+    sha256 cellar: :any_skip_relocation, catalina:      "9b514659ab7804211b191eaf4f6d5ca274cc77711073c8b4eab305c0955a8fd9"
+    sha256 cellar: :any_skip_relocation, mojave:        "9b514659ab7804211b191eaf4f6d5ca274cc77711073c8b4eab305c0955a8fd9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "222d26e07d276a0d17d264e12dbe657b959d8fb006a4365640f9149c66055160"
   end
 
   uses_from_macos "php"
