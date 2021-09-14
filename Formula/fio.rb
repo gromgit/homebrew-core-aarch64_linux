3ed05e63820f2f449592afa72785fae3d5dc85c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-14 20:06:54 +0000

    fio: update 3.28 bottle.

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -6,11 +6,11 @@ class Fio < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c280b2552998b2149c68b50f979351d8c5d5cdd8ed958931737274a944b70cf9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bb9050168d21fefdc45f840013449816b36081531f378bad0476d7bfc98aead2"
-    sha256 cellar: :any_skip_relocation, catalina:      "67b18e9eaacd69787de1eaa32d978a11f507e47178121e3e1ca7a625240a4fdc"
-    sha256 cellar: :any_skip_relocation, mojave:        "c438ba9327ba63b5034b0a0e4004b3a97480fdd8568c6fcb8fe6f6bd7446421c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad5d53fc7bde5db63ec6e91596f14e51f6f91437a20c17a0be9606eae2c5e6e8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1d5d36dc0b2e846c1ce721c99a5182d32df19af91e123cd7aeb3d76ebdb91585"
+    sha256 cellar: :any_skip_relocation, big_sur:       "edbda2b51e97d04ac6d43ba0bfd78b87493419c5960ac57f6e7bb378c56139f2"
+    sha256 cellar: :any_skip_relocation, catalina:      "3b7487a936c73e0322e68f34ad4b01797524a1f94f2c623d486a7a858f310670"
+    sha256 cellar: :any_skip_relocation, mojave:        "0eab936b15e135fad3f2aab2f22504ecd3d8dd98d4b53970a70c24983f546a49"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11f6df69a83a953817b413dcaf89e1da13965c85b2fcdcdeb670303c8743b7e4"
   end
 
   uses_from_macos "zlib"
