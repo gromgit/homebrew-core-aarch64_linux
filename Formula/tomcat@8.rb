29b3b5188b9181f34b8981bca4e52022dfb2f8cc
MoonFruit <dkmoonfruit@gmail.com>
2021-09-14 10:20:45 +0800

    tomcat@8 8.5.71
    Closes #85144.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -1,11 +1,10 @@
 class TomcatAT8 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.70/bin/apache-tomcat-8.5.70.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.70/bin/apache-tomcat-8.5.70.tar.gz"
-  sha256 "eccb6aed7a768ff16c094c292af520219f2882950aa6a107f92e22ecd872d85f"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.71/bin/apache-tomcat-8.5.71.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.71/bin/apache-tomcat-8.5.71.tar.gz"
+  sha256 "063484948dd26f6c2ab6d9de62b10cfcbcb2ebc4feb735ce1af0784f2c7870a5"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
