af7fca187db4f0740b6df895248bcd94b3d9440f
Michael Cho <cho-m@tuta.io>
2021-09-14 19:59:42 -0700

    openmodelica 1.18.0
    Closes #85209.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openmodelica.rb
+++ Formula/openmodelica.rb
@@ -3,10 +3,9 @@ class Openmodelica < Formula
   homepage "https://openmodelica.org/"
   # GitHub's archives lack submodules, must pull:
   url "https://github.com/OpenModelica/OpenModelica.git",
-      tag:      "v1.16.5",
-      revision: "11fcab4f2d6895f2db073572b2bff1a43177313f"
+      tag:      "v1.18.0",
+      revision: "49be4faa5a625a18efbbd74cc2f5be86aeea37bb"
   license "GPL-3.0-only"
-  revision 1
   head "https://github.com/OpenModelica/OpenModelica.git", branch: "master"
 
   bottle do
@@ -19,7 +18,7 @@ class Openmodelica < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
-  depends_on "gcc@10" => :build # for gfortran
+  depends_on "gcc" => :build # for gfortran
   depends_on "gnu-sed" => :build
   depends_on "libtool" => :build
   depends_on "openjdk" => :build
@@ -38,18 +37,15 @@ class Openmodelica < Formula
 
   uses_from_macos "curl"
   uses_from_macos "expat"
-  uses_from_macos "libffi"
+  uses_from_macos "libffi", since: :catalina
   uses_from_macos "ncurses"
 
-  # Fix issues with CMake 3.20+
-  # https://github.com/OpenModelica/OpenModelica/pull/7445
-  patch do
-    url "https://github.com/OpenModelica/OpenModelica/commit/71aa2f871639041f3569fafe1b1cea25b84981ff.patch?full_index=1"
-    sha256 "0f794a01481227b4c58a4c57c3f37035962de3955b9878f78207d0d3ebfbce09"
-  end
-
   def install
-    ENV.append_to_cflags "-I#{MacOS.sdk_path_if_needed}/usr/include/ffi"
+    if MacOS.version >= :catalina
+      ENV.append_to_cflags "-I#{MacOS.sdk_path_if_needed}/usr/include/ffi"
+    else
+      ENV.append_to_cflags "-I#{Formula["libffi"].opt_include}"
+    end
     args = %W[
       --prefix=#{prefix}
       --disable-debug
