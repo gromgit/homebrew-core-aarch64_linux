16f8e8646cd8dda8c2988c8cc5e859eb85f0e603
PaperChalice <lgamma@163.com>
2021-09-15 20:54:01 +0800

    pkgconf 1.8.0 (new formula)
    Closes #85230.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/pkgconf.rb
@@ -0,0 +1,70 @@
+class Pkgconf < Formula
+  desc "Package compiler and linker metadata toolkit"
+  homepage "https://git.sr.ht/~kaniini/pkgconf"
+  url "https://distfiles.dereferenced.org/pkgconf/pkgconf-1.8.0.tar.xz"
+  sha256 "ef9c7e61822b7cb8356e6e9e1dca58d9556f3200d78acab35e4347e9d4c2bbaf"
+  license "ISC"
+
+  livecheck do
+    url "https://distfiles.dereferenced.org/pkgconf/"
+    regex(/href=.*?pkgconf[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  def install
+    pc_path = %W[
+      #{HOMEBREW_PREFIX}/lib/pkgconfig
+      #{HOMEBREW_PREFIX}/share/pkgconfig
+    ]
+    pc_path << if OS.mac?
+      pc_path << "/usr/local/lib/pkgconfig"
+      pc_path << "/usr/lib/pkgconfig"
+      "#{HOMEBREW_LIBRARY}/Homebrew/os/mac/pkgconfig/#{MacOS.version}"
+    else
+      "#{HOMEBREW_LIBRARY}/Homebrew/os/linux/pkgconfig"
+    end
+
+    pc_path = pc_path.uniq.join(File::PATH_SEPARATOR)
+
+    configure_args = std_configure_args + %W[
+      --with-pkg-config-dir=#{pc_path}
+    ]
+
+    system "./configure", *configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"foo.pc").write <<~EOS
+      prefix=/usr
+      exec_prefix=${prefix}
+      includedir=${prefix}/include
+      libdir=${exec_prefix}/lib
+
+      Name: foo
+      Description: The foo library
+      Version: 1.0.0
+      Cflags: -I${includedir}/foo
+      Libs: -L${libdir} -lfoo
+    EOS
+
+    ENV["PKG_CONFIG_LIBDIR"] = testpath
+    system bin/"pkgconf", "--validate", "foo"
+    assert_equal "1.0.0", shell_output("#{bin}/pkgconf --modversion foo").strip
+    assert_equal "-lfoo", shell_output("#{bin}/pkgconf --libs-only-l foo").strip
+    assert_equal "-I/usr/include/foo", shell_output("#{bin}/pkgconf --cflags foo").strip
+
+    (testpath/"test.c").write <<~EOS
+      #include <assert.h>
+      #include <libpkgconf/libpkgconf.h>
+
+      int main(void) {
+        assert(pkgconf_compare_version(LIBPKGCONF_VERSION_STR, LIBPKGCONF_VERSION_STR) == 0);
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}/pkgconf", "-L#{lib}", "-lpkgconf"
+    system "./a.out"
+  end
+end
