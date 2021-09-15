2aa42e5e5acdc92714baf72edb94c568049915aa
Rui Chen <rui@chenrui.dev>
2021-09-14 21:40:56 -0400

    svgo 2.6.0
    Closes #85185.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/svgo.rb
+++ Formula/svgo.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Svgo < Formula
   desc "Nodejs-based tool for optimizing SVG vector graphics files"
   homepage "https://github.com/svg/svgo"
-  url "https://github.com/svg/svgo/archive/v2.5.0.tar.gz"
-  sha256 "a3cb6e095c43a21ea2317f00e48b10b41ad729b96bf11c1846a3d3b25dc527e0"
+  url "https://github.com/svg/svgo/archive/v2.6.0.tar.gz"
+  sha256 "f4901a74396e16cf0c12533ff0b7be11112514c748fc115d870df6cc2791c0f6"
   license "MIT"
 
   bottle do
