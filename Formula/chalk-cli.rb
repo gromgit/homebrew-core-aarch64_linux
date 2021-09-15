f9477bb10848cdc30bef7e7ece921de371623b4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-15 21:09:34 +0000

    chalk-cli: update 5.0.0 bottle.

--- Formula/chalk-cli.rb
+++ Formula/chalk-cli.rb
@@ -8,12 +8,7 @@ class ChalkCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f16593ee30cc571295da9d7e954795d8eb7fb453728732ad8a0404f856eeb66e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "87e5d3b529b23dfc3fa215d531ad0f3a2f2578a9015a02ef95a7810d70c1a8b8"
-    sha256 cellar: :any_skip_relocation, catalina:      "3761c3a51544b65bfc8868207049ef0af1bf6d4c80f8fec674884c52f783007e"
-    sha256 cellar: :any_skip_relocation, mojave:        "f3ddf504d4a6d55285398c1fda8c5b9b7d8ff0c7e7594afcd0288fa8adf3ab81"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "a0966f5c0489b4edd61e8f733bd1571de8c80deec7c715cca6084f3fc72d2abf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92b331d66c6e1bd7f82217cb7ec4543cd10d61d2eaf8105c52a4999840f8fd1f"
+    sha256 cellar: :any_skip_relocation, all: "092f65c0a922bfb51cf21d1b3be5dfaddefb61338b06a8c71acb0bc6c15310c2"
   end
 
   depends_on "node"
