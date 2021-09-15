382d9c742998b40187a8a1e909187e166f99b556
Michihito Shigemura <i.am.shige@gmail.com>
2021-09-15 11:44:26 +0900

    awscli 2.2.38
    * awscli 2.2.38
    * botocore 2.0.0dev146
    
    Closes #85208.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -6,16 +6,16 @@ class Awscli < Formula
   license "Apache-2.0"
 
   stable do
-    url "https://github.com/aws/aws-cli/archive/2.2.37.tar.gz"
-    sha256 "3a71e4efd262c3c61e6991e3e0ddb8b180ddf3027e1d2ed56c4dedfac51fead3"
+    url "https://github.com/aws/aws-cli/archive/2.2.38.tar.gz"
+    sha256 "91b0a10039129e83f81db631c4c47be3a5728a0818893301216630ed9d973407"
 
     # Botocore v2 is not available on PyPI and version commits are not tagged. One way to update:
     # 1. Get `botocore` version at https://github.com/aws/aws-cli/blob/#{version}/setup.py
     # 2. Get commit matching version at https://github.com/boto/botocore/commits/v2
     resource "botocore" do
-      url "https://github.com/boto/botocore/archive/981292a6fe9826c0675ae61c025dd93039e30aa6.tar.gz"
-      sha256 "2d5ebbb7343d15f15f2174838ca99eee9f97266d75533def1c47a28d9ba25fcd"
-      version "2.0.0dev145"
+      url "https://github.com/boto/botocore/archive/8e0ebb91c12024a863503e54d4a3a9383e58da72.tar.gz"
+      sha256 "47e38d8edc118aca99f61faaab352882e7b8d4e8cdca973971c7a3695c798b70"
+      version "2.0.0dev146"
     end
   end
 
