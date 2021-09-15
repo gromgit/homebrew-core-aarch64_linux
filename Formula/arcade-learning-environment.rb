5a8377f7373bd6198f8b64d729b9a2fc81e7814b
Rui Chen <rui@chenrui.dev>
2021-09-15 07:59:44 -0400

    arcade-learning-environment 0.7.4
    * arcade-learning-environment 0.7.0
    * update license
    * arcade-learning-environment: build out-of-tree
    * arcade-learning-environment: build with sdl2
    * arcade-learning-environment: tag release for ALE python
    * arcade-learning-environment: use gcc on Linux for C++17
    * arcade-learning-environment: try to fix build failure
    * arcade-learning-environment 0.7.4
    * arcade-learning-environment: try to fix Catalina build
    
    Closes #85225.
    
    Co-authored-by: rui <chenrui333@gmail.com>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arcade-learning-environment.rb
+++ Formula/arcade-learning-environment.rb
@@ -1,10 +1,12 @@
 class ArcadeLearningEnvironment < Formula
+  include Language::Python::Virtualenv
+
   desc "Platform for AI research"
   homepage "https://github.com/mgbellemare/Arcade-Learning-Environment"
-  url "https://github.com/mgbellemare/Arcade-Learning-Environment/archive/v0.6.1.tar.gz"
-  sha256 "8059a4087680da03878c1648a8ceb0413a341032ecaa44bef4ef1f9f829b6dde"
-  license "GPL-2.0"
-  revision 2
+  url "https://github.com/mgbellemare/Arcade-Learning-Environment.git",
+      tag:      "v0.7.4",
+      revision: "069f8bd860b9da816cea58c5ade791025a51c105"
+  license "GPL-2.0-only"
   head "https://github.com/mgbellemare/Arcade-Learning-Environment.git", branch: "master"
 
   bottle do
@@ -19,27 +21,103 @@ class ArcadeLearningEnvironment < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on macos: :catalina # requires std::filesystem
   depends_on "numpy"
   depends_on "python@3.9"
-  depends_on "sdl"
+  depends_on "sdl2"
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "gcc" # for C++17
+  end
+
+  fails_with gcc: "5"
+
+  resource "homebrew-test-tetris.bin" do
+    url "https://raw.githubusercontent.com/mgbellemare/Arcade-Learning-Environment/v0.7.0/tests/resources/tetris.bin"
+    sha256 "36d5b5d3222f007ca8e3691cfc17f639801453b98438b1282dfd695ae44752f6"
+  end
+
+  resource "importlib-resources" do
+    url "https://files.pythonhosted.org/packages/b5/d8/51ace1c1ea6609c01c7f46ca2978e11821aa0efaaa7516002ef6df000731/importlib_resources-5.4.0.tar.gz"
+    sha256 "d756e2f85dd4de2ba89be0b21dba2a3bbec2e871a42a3a16719258a11f87506b"
+  end
+
+  # TODO: Check to see if `importlib-metadata` resource should be removed when switching to `python@3.10`
+  # https://github.com/mgbellemare/Arcade-Learning-Environment/blob/v#{version}/setup.cfg
+  resource "importlib-metadata" do
+    url "https://files.pythonhosted.org/packages/82/10/600b88188a4e94562cfdf202ef1aca6fedda0723acae8a47376350ec0d5d/importlib_metadata-4.11.1.tar.gz"
+    sha256 "175f4ee440a0317f6e8d81b7f8d4869f93316170a65ad2b007d2929186c8052c"
+  end
+
+  # TODO: Check to see if `zipp` resource should be removed when switching to `python@3.10`
+  # https://github.com/python/importlib_resources/blob/#{importlib-resources.version}/setup.cfg
+  resource "zipp" do
+    url "https://files.pythonhosted.org/packages/94/64/3115548d41cb001378099cb4fc6a6889c64ef43ac1b0e68c9e80b55884fa/zipp-3.7.0.tar.gz"
+    sha256 "9f50f446828eb9d45b267433fd3e9da8d801f614129124863f9c51ebceafb87d"
+  end
 
   def install
-    args = std_cmake_args + %W[
+    args = %W[
       -DCMAKE_INSTALL_NAME_DIR=#{opt_lib}
       -DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON
+      -DSDL_SUPPORT=ON
+      -DSDL_DYNLOAD=ON
     ]
-    system "cmake", ".", *args
-    system "make", "install"
-    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install resources.reject { |r| r.name == "homebrew-test-tetris.bin" }
+
+    # error: no member named 'signbit' in the global namespace
+    python_args = Language::Python.setup_install_args(libexec)
+    python_cmake_options = "--cmake-options=-DCMAKE_OSX_SYSROOT=#{MacOS.sdk_path}"
+    python_args.insert((python_args.index "install"), python_cmake_options) if OS.mac?
+
+    # `venv.pip_install_and_link buildpath` alternative to allow passing in arguments
+    bin_before = Dir[libexec/"bin/*"].to_set
+    system libexec/"bin/python3", *python_args
+    bin.install_symlink (Dir[libexec/"bin/*"].to_set - bin_before).to_a
+
+    site_packages = Language::Python.site_packages("python3")
+    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+    (prefix/site_packages/"homebrew-ale-py.pth").write pth_contents
   end
 
   test do
-    output = shell_output("#{bin}/ale 2>&1", 1).lines.last.chomp
-    assert_equal "No ROM File specified.", output
+    output = shell_output("#{bin}/ale-import-roms 2>&1", 2)
+    assert_match "one of the arguments --import-from-pkg romdir is required", output
+    output = shell_output("#{bin}/ale-import-roms .").lines.last.chomp
+    assert_equal "Imported 0 / 0 ROMs", output
+
+    testpath.install resource("homebrew-test-tetris.bin")
+    output = shell_output("#{bin}/ale-import-roms --dry-run .").lines.first.chomp
+    assert_match(/\[SUPPORTED\].*tetris\.bin/, output)
+
     (testpath/"test.py").write <<~EOS
-      from ale_python_interface import ALEInterface;
-      ale = ALEInterface();
+      from ale_py import ALEInterface, SDL_SUPPORT
+      assert SDL_SUPPORT
+
+      ale = ALEInterface()
+      ale.setInt("random_seed", 123)
+      ale.loadROM("tetris.bin")
+      assert len(ale.getLegalActionSet()) == 18
     EOS
-    assert_match "ale.cfg", shell_output("#{Formula["python@3.9"].opt_bin}/python3 test.py 2>&1")
+
+    output = shell_output("#{Formula["python@3.9"].opt_bin}/python3 test.py 2>&1")
+    assert_match <<~EOS, output
+      Game console created:
+        ROM file:  tetris.bin
+        Cart Name: Tetris 2600 (Colin Hughes)
+        Cart MD5:  b0e1ee07fbc73493eac5651a52f90f00
+    EOS
+    assert_match <<~EOS, output
+      Running ROM file...
+      Random seed is 123
+    EOS
   end
 end
