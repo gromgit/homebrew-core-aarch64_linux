5b0557cbce46ba281f435ea50848bdff6cf6b794
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-15 21:13:03 +0000

    ormolu: update 0.3.0.0 bottle.

--- Formula/ormolu.rb
+++ Formula/ormolu.rb
@@ -7,11 +7,11 @@ class Ormolu < Formula
   head "https://github.com/tweag/ormolu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d8fd89b32a6dbe77698d25b8d5b6a9bde490e1c9e05b73079d9146ea4ab5c032"
-    sha256 cellar: :any_skip_relocation, big_sur:       "151d4b1c54440eaf96b72a0b417894d3fe4ca2478eb94a242b3abe144498ea68"
-    sha256 cellar: :any_skip_relocation, catalina:      "cdf06624755fe56202d3f0aa504fe0402ada1189e44de0d60a53a8cd7901e83b"
-    sha256 cellar: :any_skip_relocation, mojave:        "3e54ed98fd90c029f523657d543bea53ca67d37918144452846dfa059c3d461d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8f12ec8c0d3dec366988d54a859832488996ca0f2d5986c21f4dd01e22ed3d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "20985d0d7f3ecdc40e4e446237dd00beee5549586c31b8e5397d30a63e57a1c0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "366b87413d6e300e9d089f35e550ca02db32156a837ebd7549bdf83d1188c170"
+    sha256 cellar: :any_skip_relocation, catalina:      "bd06fd5e6eb8b540a95bd9e857701f4b4eedd66239c780d9fe5aa01f83517dc4"
+    sha256 cellar: :any_skip_relocation, mojave:        "0963f25611342146a3435f4f65610c1a29441ba94ad52670a2a4baa47ff6da56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2c6c09139745c2b4b51db43a9d7798d67eefb3d128513b1bbce66897c6b1eb2"
   end
 
   depends_on "cabal-install" => :build
