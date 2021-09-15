0eb4c6f9e1d53695d3f78864d5566717a358fc1c
Dustin Rodrigues <dust.rod@gmail.com>
2021-09-15 11:15:22 -0400

    chalk-cli 5.0.0
    * chalk-cli 5.0.0
    * chalk-cli: update test
    
    Closes #85234.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chalk-cli.rb
+++ Formula/chalk-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ChalkCli < Formula
   desc "Terminal string styling done right"
   homepage "https://github.com/chalk/chalk-cli"
-  url "https://registry.npmjs.org/chalk-cli/-/chalk-cli-4.1.0.tgz"
-  sha256 "000d9fa6969d5da248fc3b415c48e76a999e2f3e04594f14c335baab058c7a7c"
+  url "https://registry.npmjs.org/chalk-cli/-/chalk-cli-5.0.0.tgz"
+  sha256 "0c0a4e8d93b923b23fea33cee5ebb6b3c9029d1adfac5d932b5c1c428e5c0782"
   license "MIT"
 
   bottle do
@@ -24,6 +24,6 @@ class ChalkCli < Formula
   end
 
   test do
-    assert_match "hello, world!", pipe_output("#{bin}/chalk bold cyan", "hello, world!")
+    assert_match "hello, world!", pipe_output("#{bin}/chalk bold cyan --stdin", "hello, world!")
   end
 end
