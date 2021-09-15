4e5aba9735fb501245d92e553faad8f9ef95e0f5
Michael Cho <cho-m@tuta.io>
2021-09-15 07:12:11 -0700

    serverless: fix Linux build by deleting unused CPython library (#83564)

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -6,6 +6,7 @@ class Serverless < Formula
   url "https://github.com/serverless/serverless/archive/v2.57.0.tar.gz"
   sha256 "80a4ce6a737a58fb23cc5095f14e2a147f72b7607b48c28709f085467f9377d1"
   license "MIT"
+  head "https://github.com/serverless/serverless.git", branch: "master"
 
   bottle do
     sha256 arm64_big_sur: "be114aa3dc7ed36e15b8ca08e7762ab44509b6ce23535e85d5cfa5e5f97fbd7f"
@@ -18,7 +19,14 @@ class Serverless < Formula
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink Dir[libexec/"bin/*"]
+
+    # Delete incompatible Linux CPython shared library included in dependency package.
+    # Raise an error if no longer found so that the unused logic can be removed.
+    (libexec/"lib/node_modules/serverless/node_modules/@serverless/dashboard-plugin")
+      .glob("sdk-py/serverless_sdk/vendor/wrapt/_wrappers.cpython-*-linux-gnu.so")
+      .map(&:unlink)
+      .empty? && raise("Unable to find wrapt shared library to delete.")
   end
 
   test do
