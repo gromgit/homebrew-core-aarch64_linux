2bfb3ad62a71c1e0609cdbf1b833e126dc28f6ab
Rui Chen <rui@chenrui.dev>
2021-09-16 10:58:07 -0400

    closure-compiler 20210907
    Closes #85307.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -1,8 +1,8 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://developers.google.com/closure/compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20210808/closure-compiler-v20210808.jar"
-  sha256 "7e4cdd4086f902ad917a278e7805c9eb2d61f880fa52432b31b7128aea5e71b3"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20210907/closure-compiler-v20210907.jar"
+  sha256 "2a591e64c540e367fa097f5c11602f3e2b8222253cf4f159b120aa32562d083d"
   license "Apache-2.0"
 
   livecheck do
