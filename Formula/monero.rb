bb7c283ca13acdc4d18296b979c6258d41c0cbc4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-17 00:58:19 +0800

    monero: revision bump (protobuf 3.19.1)

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -5,6 +5,7 @@ class Monero < Formula
       tag:      "v0.17.3.0",
       revision: "ab18fea3500841fc312630d49ed6840b3aedb34d"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url :stable
