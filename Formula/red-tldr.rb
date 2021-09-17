0e94c0f1b2cb3b95e65d70d51515e778cd353b89
Rvn0xsy <rvn0xsy@gmail.com>
2021-09-17 14:20:41 +0800

    red-tldr 0.4.2
    Closes #85351.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/red-tldr.rb
+++ Formula/red-tldr.rb
@@ -1,8 +1,8 @@
 class RedTldr < Formula
   desc "Used to help red team staff quickly find the commands and key points"
   homepage "https://payloads.online/red-tldr/"
-  url "https://github.com/Rvn0xsy/red-tldr/archive/v0.4.tar.gz"
-  sha256 "47894a194da21e676901985909ba60c616f8b832df7e74834ae3f2da44b64b02"
+  url "https://github.com/Rvn0xsy/red-tldr/archive/v0.4.2.tar.gz"
+  sha256 "94cc5f195fac8617873f30616d65eba4b44b2ce4c58d7b6a5f8e2bbceef569f4"
   license "MIT"
   head "https://github.com/Rvn0xsy/red-tldr.git", branch: "main"
 
@@ -21,6 +21,6 @@ class RedTldr < Formula
   end
 
   test do
-    assert_match "privilege", shell_output("#{bin}/red-tldr search mimikatz")
+    assert_match "privilege", shell_output("#{bin}/red-tldr mimikatz")
   end
 end
