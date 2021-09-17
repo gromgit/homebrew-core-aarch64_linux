e8b83f81af21628d7fb8fab49255a8c24be26858
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-17 16:58:32 -0400

    cubelib: add livecheck

--- Formula/cubelib.rb
+++ Formula/cubelib.rb
@@ -4,6 +4,11 @@ class Cubelib < Formula
   url "https://apps.fz-juelich.de/scalasca/releases/cube/4.6/dist/cubelib-4.6.tar.gz"
   sha256 "36eaffa7688db8b9304c9e48ca5dc4edc2cb66538aaf48657b9b5ccd7979385b"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?cubelib[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 arm64_big_sur: "6b647d7c7673d0b4b95c7240dc477ee13e0fdd5c9f8952bd9b8d80b0f3f55d4d"
     sha256 big_sur:       "2680056fa693e78f9f89fcaa55b67888c4aa2d947cc4a2c9d2167e3380782517"
