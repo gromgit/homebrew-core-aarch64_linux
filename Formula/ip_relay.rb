651d0eac1a9dd4a2fbdc3beaac95e444f55bb832
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-17 17:35:45 -0400

    ip_relay: add livecheck

--- Formula/ip_relay.rb
+++ Formula/ip_relay.rb
@@ -4,6 +4,11 @@ class IpRelay < Formula
   url "http://www.stewart.com.au/ip_relay/ip_relay-0.71.tgz"
   sha256 "0cf6c7db64344b84061c64e848e8b4f547b5576ad28f8f5e67163fc0382d9ed3"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?ip_relay[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "35934dd4047dc1c1966fca089d71bc870e23f5a6370368fad9861ff3e3f76064"
   end
