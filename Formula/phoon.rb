be7dfbb402cd8c59061c6d5d8ce31421b8dd92aa
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-17 09:53:30 -0400

    phoon: update version, add livecheck
    Closes #85359.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phoon.rb
+++ Formula/phoon.rb
@@ -2,8 +2,22 @@ class Phoon < Formula
   desc "Displays current or specified phase of the moon via ASCII art"
   homepage "https://www.acme.com/software/phoon/"
   url "https://www.acme.com/software/phoon/phoon_14Aug2014.tar.gz"
-  version "04A"
+  version "20140814"
   sha256 "bad9b5e37ccaf76a10391cc1fa4aff9654e54814be652b443853706db18ad7c1"
+  version_scheme 1
+
+  # We check the site using HTTP (rather than HTTPS) because this server
+  # produces the following cURL error on our Ubuntu CI:
+  #   curl: (56) GnuTLS recv error (-110): The TLS connection was non-properly
+  #   terminated.
+  # If/when this is resolved, we can update this to use `url :homepage`.
+  livecheck do
+    url "http://www.acme.com/software/phoon/"
+    regex(/href=.*?phoon[._-]v?(\d{1,2}[a-z]+\d{2,4})\.t/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| Date.parse(match.first)&.strftime("%Y%m%d") }
+    end
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "ee678f636d233dc19da162128f46e9a9d4e77fcb8c1eaee2ec5f435c3c616aae"
