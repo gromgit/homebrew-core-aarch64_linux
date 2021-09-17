84948230e28118fc0a91a28ce215852de11e6918
Dawid Dziurla <dawidd0811@gmail.com>
2021-09-17 21:59:30 +0200

    vala-language-server: bump revision
    Closes #85368.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vala-language-server.rb
+++ Formula/vala-language-server.rb
@@ -5,6 +5,7 @@ class ValaLanguageServer < Formula
   version "0.48.3"
   sha256 "6f3b34bcb4e049c299ae3d5433153e4b685b0bace0ea8d761ffea266714ce841"
   license "LGPL-2.1-only"
+  revision 1
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "66e6aeafb3cebc38b59abc884dc2b5f8a477f7be38f5af65da07221810c2344b"
