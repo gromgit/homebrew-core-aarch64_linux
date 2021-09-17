af3cc26440ea58aca23569b8808c0c5e60c21750
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-17 17:48:19 -0400

    cgvg: add livecheck

--- Formula/cgvg.rb
+++ Formula/cgvg.rb
@@ -5,6 +5,11 @@ class Cgvg < Formula
   sha256 "d879f541abcc988841a8d86f0c0781ded6e70498a63c9befdd52baf4649a12f3"
   license "GPL-2.0-or-later"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?cgvg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "db2e726fa0cc8e08dc89c030ea6aa2295b07a0559d7ec25d9ee159e5a917385e"
