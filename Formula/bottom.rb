6b9097fc95c8756df5092b3973eff87512967e4e
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-09-17 17:51:55 +0000

    bottom: add 0.6.4 bottle.

--- Formula/bottom.rb
+++ Formula/bottom.rb
@@ -6,6 +6,14 @@ class Bottom < Formula
   license "MIT"
   head "https://github.com/ClementTsang/bottom.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "858638f2ce93c5cd1f0f4b3fe5a34ad2978c1af1d1ff72fd6dd8aca954593505"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f047f6173fd9bbee11fb9ab8c3e74a57426115b41ef561cebff58890ec781b7b"
+    sha256 cellar: :any_skip_relocation, catalina:      "54c224a8df54fce11acbb473e5aa51e2a4b39b635e119c7dc49da0c7dbbba50a"
+    sha256 cellar: :any_skip_relocation, mojave:        "b4f68a0f65db30f870df86e1e2acd4eb32196f1da452c013d1e49f65aa3484a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45a3ad6c888122f5e082f8ab30854c0903316d7898917c16eda01804456db0de"
+  end
+
   depends_on "rust" => :build
 
   def install
