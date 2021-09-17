184b8fcd3643ed7f4ad93da383e709d3e19f3565
Branch Vincent <branchevincent@gmail.com>
2021-09-16 22:11:06 -0400

    gtop 1.1.1 (new formula)
    Closes #85344.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gtop.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Gtop < Formula
+  desc "System monitoring dashboard for terminal"
+  homepage "https://github.com/aksakalli/gtop"
+  url "https://registry.npmjs.org/gtop/-/gtop-1.1.1.tgz"
+  sha256 "79552e08552d9b785d416f001e0bc84d02add1619eb9bfd9244b3e27eda399df"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir[libexec/"bin/*"]
+  end
+
+  test do
+    assert_match "Error: Width must be multiple of 2", shell_output(bin/"gtop 2>&1", 1)
+  end
+end
