db6ad90aac8e2d0b1007ad365d4f310b2b39ad05
rui <rui@chenrui.dev>
2021-09-18 04:05:15 +0000

    pkg-config-wrapper: add 0.2.8 bottle.

--- Formula/pkg-config-wrapper.rb
+++ Formula/pkg-config-wrapper.rb
@@ -6,6 +6,14 @@ class PkgConfigWrapper < Formula
   license "MIT"
   head "https://github.com/influxdata/pkg-config.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "132d305934de84c9d98d82912312f0cbb7204dbf203277730fd1ef2238ee5621"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0c13ac5e30bafee95d4190ba733305bf481195299cc07d665bf25c7b34183f63"
+    sha256 cellar: :any_skip_relocation, catalina:      "164299afc7de07a790856a3a6c3aa05a129584e1d60b9d40d0d429775f5b2fff"
+    sha256 cellar: :any_skip_relocation, mojave:        "c6d86cb0821de7c39da2ee6f6b4ee30638f61d63a0a851bbd0b6ccc6ea811710"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c737a65676cc36f64a90c9ee8e2d8c3f9c1dc9d7c1d57a8f844473d7e0f64df"
+  end
+
   depends_on "go" => :build
   depends_on "pkg-config"
 
