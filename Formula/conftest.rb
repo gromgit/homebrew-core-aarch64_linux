12295360a15dba9b0cc9875b0aca8997a227c89f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-18 19:08:06 +0000

    conftest: update 0.28.0 bottle.

--- Formula/conftest.rb
+++ Formula/conftest.rb
@@ -7,11 +7,11 @@ class Conftest < Formula
   head "https://github.com/open-policy-agent/conftest.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "854edb50cb872782f0aba919f067158ae5a36c068c2ae55b70afd65cf7376988"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e7b3aa341bebd7501b6803177eceff8bf6dcfff38e156e3b8efee4a2723b414d"
-    sha256 cellar: :any_skip_relocation, catalina:      "03cb3c6779a1b379d600dea744895e9d497118db9ed766a342ed0de39953f222"
-    sha256 cellar: :any_skip_relocation, mojave:        "734478ed8f1dbca7fe4f2e2334b1c28625819fc747899dcf17def4fbe5278494"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0afd80a585ba50db81b22f8d9c42c1cde14e560a0ae640ee7a483f45e066a9e7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "33bcb948579d556f11f9c9baefd5b6c12aa902c8100f6d1513b4013ff3a8f7fb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8d05da0e8877beb89bb49d0d68a7defcdaf3c7726c0e4a403d487cffc609cf36"
+    sha256 cellar: :any_skip_relocation, catalina:      "0decbcdcc4b4df07a3c3d24f9cf6cfe597acc993d5aebd8aaa7c608c1a37da86"
+    sha256 cellar: :any_skip_relocation, mojave:        "b9a1eccf970f0e7568b8198ffecb46ea1bad39db70210b697d7e222d8f5a8dfc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8272e1be7cc13c765c9ab0c6f7fc3782c6d9ca1ad0dbb130acd120fc2c2f13d2"
   end
 
   depends_on "go" => :build
