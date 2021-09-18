234e21651f97eddb300d004b44ee4ea2b341960a
rui <rui@chenrui.dev>
2021-09-18 04:05:42 +0000

    gtop: add 1.1.1 bottle.

--- Formula/gtop.rb
+++ Formula/gtop.rb
@@ -7,6 +7,14 @@ class Gtop < Formula
   sha256 "79552e08552d9b785d416f001e0bc84d02add1619eb9bfd9244b3e27eda399df"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e5adce5d3f299b11b5a5278e89c8201eb5cc4f2617b1ef88f07020a33b1df655"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
+    sha256 cellar: :any_skip_relocation, catalina:      "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
+    sha256 cellar: :any_skip_relocation, mojave:        "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5adce5d3f299b11b5a5278e89c8201eb5cc4f2617b1ef88f07020a33b1df655"
+  end
+
   depends_on "node"
 
   def install
