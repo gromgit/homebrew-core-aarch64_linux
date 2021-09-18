c139858fc28c195e8c88be4ed56dc6f0df47ad82
Branch Vincent <branchevincent@gmail.com>
2021-09-18 02:02:43 -0400

    clisp 2.49.92
    Closes #85417.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clisp.rb
+++ Formula/clisp.rb
@@ -1,19 +1,18 @@
 class Clisp < Formula
   desc "GNU CLISP, a Common Lisp implementation"
   homepage "https://clisp.sourceforge.io/"
-  revision 2
+  license "GPL-2.0-or-later"
+  head "https://gitlab.com/gnu-clisp/clisp.git", branch: "master"
 
   stable do
-    url "https://ftp.gnu.org/gnu/clisp/release/2.49/clisp-2.49.tar.bz2"
-    mirror "https://ftpmirror.gnu.org/clisp/release/2.49/clisp-2.49.tar.bz2"
-    sha256 "8132ff353afaa70e6b19367a25ae3d5a43627279c25647c220641fed00f8e890"
+    url "https://alpha.gnu.org/gnu/clisp/clisp-2.49.92.tar.bz2"
+    sha256 "bd443a94aa9b02da4c4abbcecfc04ffff1919c0a8b0e7e35649b86198cd6bb89"
 
+    # Fix build on ARM
+    # Remove once https://gitlab.com/gnu-clisp/clisp/-/commit/39b68a14d9a1fcde8a357c088c7317b19ff598ad is released,
+    # which contains the necessary patch to the bundled gnulib
+    # https://git.savannah.gnu.org/gitweb/?p=gnulib.git;a=commit;h=00e688fc22c7bfb0bba2bd8a7b2a7d22d21d31ef
     patch :DATA
-
-    patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e2cc7c1/clisp/patch-src_lispbibl_d.diff"
-      sha256 "fd4e8a0327e04c224fb14ad6094741034d14cb45da5b56a2f3e7c930f84fd9a0"
-    end
   end
 
   livecheck do
@@ -30,58 +29,36 @@ class Clisp < Formula
     sha256               x86_64_linux: "46135189dd1acbfdcae88e81b78d90be6fce1d9b19e853d1c610cc9e146c8597"
   end
 
-  head do
-    url "https://gitlab.com/gnu-clisp/clisp.git"
-  end
-
   depends_on "libsigsegv"
   depends_on "readline"
 
   def install
-    ENV.deparallelize # This build isn't parallel safe.
-    ENV.O0 # Any optimization breaks the build
-
-    # Clisp requires to select word size explicitly this way,
-    # set it in CFLAGS won't work.
-    ENV["CC"] = "#{ENV.cc} -m64"
-
-    # Work around "configure: error: unrecognized option: `--elispdir"
-    # Upstream issue 16 Aug 2016 https://sourceforge.net/p/clisp/bugs/680/
-    inreplace "src/makemake.in", "${datarootdir}/emacs/site-lisp", elisp
-
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-readline=yes"
+    system "./configure", *std_configure_args,
+                          "--with-readline=yes",
+                          "--elispdir=#{elisp}"
 
     cd "src" do
-      # Multiple -O options will be in the generated Makefile,
-      # make Homebrew's the last such option so it's effective.
-      inreplace "Makefile" do |s|
-        s.change_make_var! "CFLAGS", "#{s.get_make_var("CFLAGS")} #{ENV["CFLAGS"]}"
-      end
-
-      # The ulimit must be set, otherwise `make` will fail and tell you to do so
-      system "ulimit -s 16384 && make"
-
+      system "make"
       system "make", "install"
     end
   end
 
   test do
-    system "#{bin}/clisp", "--version"
+    (testpath/"main.lisp").write <<~EOS
+      (format t "Hello, World!")
+    EOS
+    assert_equal "Hello, World!", shell_output(bin/"clisp main.lisp").chomp
   end
 end
 
 __END__
-diff --git a/src/stream.d b/src/stream.d
-index 5345ed6..cf14e29 100644
---- a/src/stream.d
-+++ b/src/stream.d
-@@ -3994,7 +3994,7 @@ global object iconv_range (object encoding, uintL start, uintL end, uintL maxint
- nonreturning_function(extern, error_unencodable, (object encoding, chart ch));
-
- /* Avoid annoying warning caused by a wrongly standardized iconv() prototype. */
--#ifdef GNU_LIBICONV
-+#if defined(GNU_LIBICONV) && !defined(__APPLE_CC__)
-   #undef iconv
-   #define iconv(cd,inbuf,inbytesleft,outbuf,outbytesleft) \
-     libiconv(cd,(ICONV_CONST char **)(inbuf),inbytesleft,outbuf,outbytesleft)
+--- a/src/gllib/vma-iter.c
++++ b/src/gllib/vma-iter.c
+@@ -1327,7 +1327,7 @@
+          In 64-bit processes, we could use vm_region_64 or mach_vm_region.
+          I choose vm_region_64 because it uses the same types as vm_region,
+          resulting in less conditional code.  */
+-# if defined __ppc64__ || defined __x86_64__
++# if defined __aarch64__ || defined __ppc64__ || defined __x86_64__
+       struct vm_region_basic_info_64 info;
+       mach_msg_type_number_t info_count = VM_REGION_BASIC_INFO_COUNT_64;
