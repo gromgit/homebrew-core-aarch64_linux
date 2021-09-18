3886f2f24a803134fa32f4f910be5df6d0545636
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-18 09:50:15 +0000

    libedit: update 20210910-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -12,11 +12,11 @@ class Libedit < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "13d4e88e23a6c82ca458d3ca287af597b27ac211d258ef825bc0a4ab3111a80b"
-    sha256 cellar: :any,                 big_sur:       "75d0b470e0478010fbaa2708db5eefba895d71d32efe79bc3eee389e26375bb7"
-    sha256 cellar: :any,                 catalina:      "b28c79aa687f372834237a8a13ff274ff91bbb378aaa7bdd055411a0817427a7"
-    sha256 cellar: :any,                 mojave:        "b011a5735eee4db2740abc9cc25703e9dcf89da5f79e5d210a3a1e8609f9205e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08fede13bbc6910dbb2cc33aac6d4846b933713118e19503e09490f2404d3994"
+    sha256 cellar: :any,                 arm64_big_sur: "87f49ccb584888cb95885c09a586ec0d97f7d3813cdc38f66e07690143226583"
+    sha256 cellar: :any,                 big_sur:       "f1c631b30d1daf17da2bdd3d1f59330d99439ff15dd573e52364bc014a664803"
+    sha256 cellar: :any,                 catalina:      "38e6ae9c4fd560bc93003106a786fac1e26749426b837e8f92fb38b547b0edb7"
+    sha256 cellar: :any,                 mojave:        "44e7ce6ef6d1f558f565d03e4287d6da98a36e120b736a083d5c751669fdb7c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c0bdbd74d04e366389621c1951d0a1de06a359c19da5c2c5b847605bca617753"
   end
 
   keg_only :provided_by_macos
