d29d28340ac76c3f68b5e49c643fe16aef7747e3
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-17 20:03:10 -0400

    xtitle: update urls, add license and livecheck (#85377)

--- Formula/xtitle.rb
+++ Formula/xtitle.rb
@@ -1,8 +1,14 @@
 class Xtitle < Formula
   desc "Set window title and icon for your X terminal"
-  homepage "https://www.cs.indiana.edu/~kinzler/xtitle/"
-  url "https://www.cs.indiana.edu/~kinzler/xtitle/xtitle-1.0.4.tgz"
+  homepage "https://kinzler.com/me/xtitle/"
+  url "https://kinzler.com/me/xtitle/xtitle-1.0.4.tgz"
   sha256 "cadddef1389ba1c5e1dc7dd861545a5fe11cb397a3f692cd63881671340fcc15"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?xtitle[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "c463ee3a0d4e7e17b1dbcdced8c6ed5eb50d2815a56cc28ac037848735fe1803"
