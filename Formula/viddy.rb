151451751d8ba2381ca390c9106faa531968585d
Takumasa Sakao <tsakao@zlab.co.jp>
2021-09-19 14:12:18 +0900

    viddy 0.3.0 (new formula)
    Closes #85521.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/viddy.rb
@@ -0,0 +1,30 @@
+class Viddy < Formula
+  desc "Modern watch command"
+  homepage "https://github.com/sachaos/viddy"
+  url "https://github.com/sachaos/viddy/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "46a7705b97d985ca3bc4ffbc47ab0bdfa14ec9f7907fee041014134b89983782"
+  license "MIT"
+  head "https://github.com/sachaos/viddy.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=v#{version}")
+  end
+
+  test do
+    on_linux do
+      # Errno::EIO: Input/output error @ io_fread - /dev/pts/0
+      return if ENV["HOMEBREW_GITHUB_ACTIONS"]
+    end
+
+    ENV["TERM"] = "xterm"
+    require "pty"
+    r, _, pid = PTY.spawn "#{bin}/viddy echo hello"
+    sleep 1
+    Process.kill "SIGINT", pid
+    assert_includes r.read, "Every"
+
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
