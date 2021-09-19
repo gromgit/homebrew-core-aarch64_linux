1fddf0b56aa5528be94bb4833f8eb860c6155bd3
Rui Chen <rui@chenrui.dev>
2021-09-18 23:45:47 -0400

    moreutils 0.66
    * moreutils 0.66
    * Update moreutils.rb
    
    Closes #85511.
    
    Co-authored-by: rui <chenrui333@gmail.com>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/moreutils.rb
+++ Formula/moreutils.rb
@@ -2,8 +2,8 @@ class Moreutils < Formula
   desc "Collection of tools that nobody wrote when UNIX was young"
   homepage "https://joeyh.name/code/moreutils/"
   url "https://git.joeyh.name/git/moreutils.git",
-      tag:      "0.65",
-      revision: "c3261f4190c3803177b6c50ec43fd302171dd48e"
+      tag:      "0.66",
+      revision: "f0642d6331e89ca5a6ced8c0f1744428983e1780"
   license all_of: [
     "GPL-2.0-or-later",
     { any_of: ["GPL-2.0-only", "Artistic-2.0"] },
