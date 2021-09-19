28be0e33c325731956907cc64f8cc36cd05dda5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 16:07:45 +0000

    rakudo: update 2021.09 bottle.

--- Formula/rakudo.rb
+++ Formula/rakudo.rb
@@ -7,10 +7,10 @@ class Rakudo < Formula
   license "Artistic-2.0"
 
   bottle do
-    sha256 arm64_big_sur: "399858c59537edc692da506005d517cc05374a9efe05ba7e47cd947b5c80d829"
-    sha256 big_sur:       "089cec956db676e699ab062d5eea7139916a045346a5475d1ac26c84259dcafe"
-    sha256 catalina:      "28320900a5c12a34fdf4ce0c36b521f592446cc9171e203a10fc0dadd5197488"
-    sha256 mojave:        "08c7f7bb0cc04019314232620ffef7920208e99340af5d2820322c5994f04f1d"
+    sha256 arm64_big_sur: "e8106f54cabef24a42f96bcd8caeeab7e394d40601695430fb3e090524334288"
+    sha256 big_sur:       "3cbd0401d0e26e69cea32379eb9f41c38924d157d0f85a4ebf7d3a149e916244"
+    sha256 catalina:      "7219a7ab1b4ab2679c69cdf8d6f2d27ff1f7b931c6fd1c8f32d1d66033023ffb"
+    sha256 mojave:        "6ed9b471f9c8b991ba8f33130a754212a914c87b55bf9e287e17049936cc76e3"
   end
 
   depends_on "nqp"
