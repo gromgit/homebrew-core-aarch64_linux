cc97d95e74b8d3af70e4f04f9c469fa78e6dcea4
Rui Chen <rui@chenrui.dev>
2021-09-18 22:34:26 -0400

    wallpaper 2.2.0
    * wallpaper 2.2.0
    * needs xcode 13+ for swift 5.5
    
    Closes #85484.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -1,10 +1,9 @@
 class Wallpaper < Formula
   desc "Manage the desktop wallpaper"
   homepage "https://github.com/sindresorhus/macos-wallpaper"
-  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.1.0.tar.gz"
-  sha256 "4925247524535c0cc128dcc4d87f5538a5ce3b5d3a3c211127fd646ee00252b6"
+  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.2.0.tar.gz"
+  sha256 "239ba18397a5886066fc0fbee4235e3282edcf7ea075109889eb2552a04f0d1f"
   license "MIT"
-  revision 1
   head "https://github.com/sindresorhus/macos-wallpaper.git", branch: "main"
 
   bottle do
@@ -15,7 +14,7 @@ class Wallpaper < Formula
     sha256 cellar: :any_skip_relocation, catalina:       "2a2ca640dde6aed8dab04983c680d60532eefcadc11f3c0b379e7754d4d9a662"
   end
 
-  depends_on xcode: ["11.4", :build]
+  depends_on xcode: ["13.0", :build]
   depends_on :macos
 
   def install
