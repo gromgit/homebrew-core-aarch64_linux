446f48849b8e11e13f28d66f50387926412300c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 23:07:50 +0000

    coffeescript: update 2.6.0 bottle.

--- Formula/coffeescript.rb
+++ Formula/coffeescript.rb
@@ -9,12 +9,7 @@ class Coffeescript < Formula
   head "https://github.com/jashkenas/coffeescript.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cfe10ba50bec4e20897d1d2ead0a21e8bfcd25841dae5e875e8c04a8cbfdd44f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "203638208a42d33debe48c9e5c4cea7adfec2fb30b0afeb4d0dea77bcb506863"
-    sha256 cellar: :any_skip_relocation, catalina:      "090fe2a4161fbcf4e7615cded97c8e9736f7939c1a70a863f37d83141f5cb118"
-    sha256 cellar: :any_skip_relocation, mojave:        "acd2a08cb5db976c36508582ac5ff82476ad9dc037ab065353d071cf46c211f5"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "4ee049a8e7bb8a0c67452cb0b912ef5fd4e402c4948cf1b4cb8a5022640df19e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74189e3849ff5df9c38e51a8c986a269c15974c7e24959141351b1fa93ec4a78"
+    sha256 cellar: :any_skip_relocation, all: "e58a5435e6c6321655e5b6991d363718b74913537bb92f25a78d7832ad0b27b9"
   end
 
   depends_on "node"
