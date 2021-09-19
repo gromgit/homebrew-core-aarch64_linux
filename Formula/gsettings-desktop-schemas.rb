4cf6b5cf77848e16652a2963a789b5f5ab8ef1a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 20:07:23 +0000

    gsettings-desktop-schemas: update 41.0 bottle.

--- Formula/gsettings-desktop-schemas.rb
+++ Formula/gsettings-desktop-schemas.rb
@@ -6,11 +6,11 @@ class GsettingsDesktopSchemas < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f98030c66d5023f9fd91a7700dff2372a9becd02514bd291139d56c32b2c06c1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a7f5b4fc71c5dfdd9418823c6252872b6e6ecc7bab2e5828dcdcf7d484223178"
-    sha256 cellar: :any_skip_relocation, catalina:      "24379b6ab7c18f2d682d59ef7c8f94eaf67a41d0ed3156deb5f0f9bc99919cd4"
-    sha256 cellar: :any_skip_relocation, mojave:        "1350e64db550d9db2f48be5495f4da335b324b9b6c87ce4f073cf52cdbe3f790"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8941cdf3c463cbca6ab9028533fd0da7f31ffb05fc0c563ea0ff7811c1fcf6f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d27eabf5d5e5f619418a2b6be7ca9b42de63572e595ab92e6e8960256ca176c4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, catalina:      "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, mojave:        "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08f6428fc391dda01e161b14a0bd183d340b78cf5cb85a9e66b4ba33be790fa8"
   end
 
   depends_on "gobject-introspection" => :build
