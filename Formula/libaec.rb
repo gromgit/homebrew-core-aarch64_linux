2c1a50ca58150fed23ba0a71f4271a2b78343e36
Rui Chen <rui@chenrui.dev>
2021-09-18 23:47:19 -0400

    libaec 1.0.6
    * libaec 1.0.6
    * no `make test`
    * libaec: fix `make test`
    
    Closes #85515.
    
    Co-authored-by: rui <chenrui333@gmail.com>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libaec.rb
+++ Formula/libaec.rb
@@ -1,8 +1,8 @@
 class Libaec < Formula
   desc "Adaptive Entropy Coding implementing Golomb-Rice algorithm"
   homepage "https://gitlab.dkrz.de/k202009/libaec"
-  url "https://gitlab.dkrz.de/k202009/libaec/-/archive/v1.0.5/libaec-v1.0.5.tar.bz2"
-  sha256 "49cc260c3321a358a8d854444584eec58382c26cda3c4bcd156b4ba396075ae7"
+  url "https://gitlab.dkrz.de/k202009/libaec/-/archive/v1.0.6/libaec-v1.0.6.tar.bz2"
+  sha256 "31fb65b31e835e1a0f3b682d64920957b6e4407ee5bbf42ca49549438795a288"
   license "BSD-2-Clause"
   head "https://gitlab.dkrz.de/k202009/libaec.git", branch: "master"
 
@@ -19,7 +19,8 @@ class Libaec < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      # We run `make test` for libraries
+      system "cmake", "..", *std_cmake_args, "-DBUILD_TESTING=ON"
       system "make", "install"
       system "make", "test"
     end
