d178ed731f31806cc5239161276b7de0a0e9ed05
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-19 10:45:52 +0800

    fftw: use `ENV.runtime_cpu_detection`
    Also, enable AVX2 on Intel.
    
    Closes #85473.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fftw.rb
+++ Formula/fftw.rb
@@ -27,6 +27,8 @@ class Fftw < Formula
   fails_with :clang
 
   def install
+    ENV.runtime_cpu_detection
+
     args = [
       "--enable-shared",
       "--disable-debug",
@@ -40,7 +42,7 @@ class Fftw < Formula
     # FFTW supports runtime detection of CPU capabilities, so it is safe to
     # use with --enable-avx and the code will still run on all CPUs
     simd_args = []
-    simd_args << "--enable-sse2" << "--enable-avx" if Hardware::CPU.intel?
+    simd_args += %w[--enable-sse2 --enable-avx --enable-avx2] if Hardware::CPU.intel?
 
     # single precision
     # enable-sse2, enable-avx and enable-avx2 work for both single and double precision
