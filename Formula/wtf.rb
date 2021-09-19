d95fd752d7905d7a0f3ba9e269bced245fb935eb
Rui Chen <rui@chenrui.dev>
2021-09-18 22:33:34 -0400

    wtf 20210916
    Closes #85482.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20210905.tar.gz"
-  sha256 "89334fe299188654cc416eeb29dbf7bcb7cfb02a6c9b27b30748e6082dd7491d"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20210916.tar.gz"
+  sha256 "e79abe5b0168d6d76148b338e517fcc77821716a6192182bb38d83469afe0187"
   license :public_domain
 
   livecheck do
