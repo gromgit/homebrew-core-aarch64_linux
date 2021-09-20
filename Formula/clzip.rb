9ab84c2543a3a0380963b20532095cd9ae21e62f
Caleb Xu <calebcenter@live.com>
2021-09-19 21:42:33 -0400

    clzip 1.12
    Closes #85552.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clzip.rb
+++ Formula/clzip.rb
@@ -1,9 +1,10 @@
 class Clzip < Formula
   desc "C language version of lzip"
   homepage "https://www.nongnu.org/lzip/clzip.html"
-  url "https://download.savannah.gnu.org/releases/lzip/clzip/clzip-1.11.tar.gz"
-  sha256 "d9d51212afa80371dc2546d278ef8ebbb3cd57c06fdd761b7b204497586d24c0"
-  license "GPL-2.0"
+  url "https://download.savannah.gnu.org/releases/lzip/clzip/clzip-1.12.tar.gz"
+  mirror "https://download-mirror.savannah.gnu.org/releases/lzip/clzip/clzip-1.12.tar.gz"
+  sha256 "fcc92b3006d87b7c4affa03fe9dcc4869a802253052653200c26f6ba718bfee8"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://download.savannah.gnu.org/releases/lzip/clzip/"
