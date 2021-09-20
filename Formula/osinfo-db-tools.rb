6da24fdda7834d21c39b43d0526db5798ee3308b
Nanda H Krishna <me@nandahkrishna.com>
2021-09-20 19:24:51 -0400

    osinfo-db-tools: revision bump (libsoup 3.0.0)

--- Formula/osinfo-db-tools.rb
+++ Formula/osinfo-db-tools.rb
@@ -4,6 +4,7 @@ class OsinfoDbTools < Formula
   url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.9.0.tar.xz"
   sha256 "255f1c878bacec70c3020ff5a9cb0f6bd861ca0009f24608df5ef6f62d5243c0"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://releases.pagure.org/libosinfo/?C=M&O=D"
@@ -26,7 +27,7 @@ class OsinfoDbTools < Formula
   depends_on "glib"
   depends_on "json-glib"
   depends_on "libarchive"
-  depends_on "libsoup"
+  depends_on "libsoup@2"
   depends_on "python@3.9"
 
   uses_from_macos "pod2man" => :build
