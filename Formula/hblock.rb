178587f7e27edca2213cc86bcee03a9ec8bce53c
Rui Chen <rui@chenrui.dev>
2021-09-20 18:59:25 -0400

    hblock 3.2.3
    Closes #85621.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -1,8 +1,8 @@
 class Hblock < Formula
   desc "Adblocker that creates a hosts file from multiple sources"
   homepage "https://hblock.molinero.dev/"
-  url "https://github.com/hectorm/hblock/archive/v3.2.2.tar.gz"
-  sha256 "151e5ce32403f65892b0a15df04310ee4d115e12da9d38a7289f13f20e753d92"
+  url "https://github.com/hectorm/hblock/archive/v3.2.3.tar.gz"
+  sha256 "1b8eb3c5cb074cbe7b0a8b5e040641c12b519bee21a4a879e1bdd328cd17aa60"
   license "MIT"
 
   bottle do
