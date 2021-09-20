24391e651f4833c57465f5fbc0ea67928472037c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-20 07:07:51 +0000

    ortp: update 5.0.23 bottle.

--- Formula/ortp.rb
+++ Formula/ortp.rb
@@ -7,10 +7,10 @@ class Ortp < Formula
   head "https://gitlab.linphone.org/BC/public/ortp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "a41f8a27984e61f7b90ce501b6ef4d7cf90a02816668396f58dbe11a4efe3aac"
-    sha256 cellar: :any, big_sur:       "710e7827d6808dc2cadd9a867c84b42cbb2dc856fb2ee0c1e79056164f4624a8"
-    sha256 cellar: :any, catalina:      "28b8267133f9d77be72851361454cb09e90a67bfa472aedaf0ace33f4862cdbd"
-    sha256 cellar: :any, mojave:        "98cbf54721d04bb914fe0235dc0eede809fe89b6fc085d36e3af9098607d8616"
+    sha256 cellar: :any, arm64_big_sur: "3d204745ea3534e21f5171d040bd58e565b398afc7e42c01e8da27301297fcd8"
+    sha256 cellar: :any, big_sur:       "79c20b778de10705e8b1e7af10763215d45c4aef83c5bd1c27e848af887a396c"
+    sha256 cellar: :any, catalina:      "10cadafe20724532016c7efcc7cce6df6ad4703b18dd05929bdabf32ec57aa25"
+    sha256 cellar: :any, mojave:        "912186418879b39eb6cb45441c05d6f56de9e7a409f486e87c04a25a832db0a9"
   end
 
   depends_on "cmake" => :build
