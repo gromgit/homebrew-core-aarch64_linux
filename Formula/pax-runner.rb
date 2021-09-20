e120626b7a1b8735b5f46b7fa00ed7bf5009cc03
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-20 14:09:07 -0400

    pax-runner 1.9.0
    Closes #85609.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pax-runner.rb
+++ Formula/pax-runner.rb
@@ -1,9 +1,14 @@
 class PaxRunner < Formula
   desc "Tool to provision OSGi bundles"
-  homepage "https://ops4j1.jira.com/"
-  url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/runner/pax-runner-assembly/1.8.6/pax-runner-assembly-1.8.6-jdk15.tar.gz"
-  version "1.8.6"
-  sha256 "42a650efdedcb48dca89f3e4272a9e2e1dcc6bc84570dbb176b5e578ca1ce2d4"
+  homepage "https://ops4j1.jira.com/wiki/spaces/paxrunner/overview"
+  url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/runner/pax-runner-assembly/1.9.0/pax-runner-assembly-1.9.0-jdk15.tar.gz"
+  version "1.9.0"
+  sha256 "b1ff2039dc1e73b6957653d967d6ee028f9c79d663b9031a6b77a49932352dc1"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/runner/pax-runner-assembly/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "cdaa1633b1cc5310e7e6d5edd558316c794d37869976494d6b1b4d465ce1129d"
