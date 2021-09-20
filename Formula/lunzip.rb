3c8e78a5070ea83e09c348dbea68f64b6565e3b9
Caleb Xu <calebcenter@live.com>
2021-09-20 11:19:39 -0400

    lunzip 1.12 (new formula)
    Closes #85578.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/lunzip.rb
@@ -0,0 +1,33 @@
+class Lunzip < Formula
+  desc "Decompressor for lzip files"
+  homepage "https://www.nongnu.org/lzip/lunzip.html"
+  url "https://download-mirror.savannah.gnu.org/releases/lzip/lunzip/lunzip-1.12.tar.gz"
+  sha256 "e55b4aaac5666b6ccad8459e52f8adb6869ce259f686e960129ae1e1bdf52ad2"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/lzip/lunzip/"
+    regex(/href=.*?lunzip[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "lzip" => :test
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    path = testpath/"data.txt"
+    original_contents = "." * 1000
+    path.write original_contents
+
+    # compress: data.txt -> data.txt.lz
+    system Formula["lzip"].opt_bin/"lzip", path
+    refute_predicate path, :exist?
+
+    # decompress: data.txt.lz -> data.txt
+    system bin/"lunzip", "#{path}.lz"
+    assert_equal original_contents, path.read
+  end
+end
