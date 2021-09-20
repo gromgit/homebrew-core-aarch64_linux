8c4c5b1d52285f4e9757bc94935036f8163f2ac8
Alexander Bayandin <a.bayandin@gmail.com>
2021-09-20 18:11:21 +0100

    libavif 0.9.2 (new formula)
    Closes #85593.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/libavif.rb
@@ -0,0 +1,46 @@
+class Libavif < Formula
+  desc "Library for encoding and decoding .avif files"
+  homepage "https://github.com/AOMediaCodec/libavif"
+  url "https://github.com/AOMediaCodec/libavif/archive/refs/tags/v0.9.2.tar.gz"
+  sha256 "d6607d654adc40a392da83daa72a4ff802cd750c045a68131c9305639c10fc5c"
+  license "BSD-2-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "nasm" => :build
+  depends_on "aom"
+  depends_on "jpeg"
+  depends_on "libpng"
+
+  uses_from_macos "zlib"
+
+  def install
+    args = %W[
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DAVIF_CODEC_AOM=ON
+      -DAVIF_BUILD_APPS=ON
+      -DAVIF_BUILD_EXAMPLES=OFF
+      -DAVIF_BUILD_TESTS=OFF
+    ] + std_cmake_args
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make"
+      system "make", "install"
+    end
+
+    pkgshare.install "examples"
+  end
+
+  test do
+    system bin/"avifenc", test_fixtures("test.png"), testpath/"test.avif"
+    assert_path_exists testpath/"test.avif"
+
+    system bin/"avifdec", testpath/"test.avif", testpath/"test.jpg"
+    assert_path_exists testpath/"test.jpg"
+
+    example = pkgshare/"examples/avif_example_decode_file.c"
+    system ENV.cc, example, "-I#{include}", "-L#{lib}", "-lavif", "-o", "avif_example_decode_file"
+    output = shell_output("#{testpath}/avif_example_decode_file #{testpath}/test.avif")
+    assert_match "Parsed AVIF: 8x8", output
+  end
+end
