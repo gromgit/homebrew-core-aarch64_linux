85844c1bb49195a1852f766ee918715e3acce80f
Branch Vincent <branchevincent@gmail.com>
2021-09-20 08:21:32 -0400

    kotlin 1.5.31
    Closes #85569.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.5.30/kotlin-compiler-1.5.30.zip"
-  sha256 "25eebdb9bb2e165e4ba8efbf8ff943c45d4fc39dbffc2c1caa059e8dd813675f"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.5.31/kotlin-compiler-1.5.31.zip"
+  sha256 "661111286f3e5ac06aaf3a9403d869d9a96a176b62b141814be626a47249fe9e"
   license "Apache-2.0"
 
   livecheck do
@@ -18,8 +18,8 @@ class Kotlin < Formula
 
   def install
     libexec.install "bin", "build.txt", "lib"
-    rm Dir["#{libexec}/bin/*.bat"]
-    bin.install Dir["#{libexec}/bin/*"]
+    rm Dir[libexec/"bin/*.bat"]
+    bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
     prefix.install "license"
   end
@@ -30,8 +30,8 @@ class Kotlin < Formula
         println("Hello World!")
       }
     EOS
-    system "#{bin}/kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
-    system "#{bin}/kotlinc-js", "test.kt", "-output", "test.js"
-    system "#{bin}/kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
+    system bin/"kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system bin/"kotlinc-js", "test.kt", "-output", "test.js"
+    system bin/"kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
   end
 end
