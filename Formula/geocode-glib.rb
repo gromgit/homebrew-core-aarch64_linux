19e5f6679ab5509c8a505f7bdd8cee78a8418b7a
Nanda H Krishna <me@nandahkrishna.com>
2021-09-20 19:24:47 -0400

    geocode-glib: revision bump (libsoup 3.0.0)

--- Formula/geocode-glib.rb
+++ Formula/geocode-glib.rb
@@ -4,7 +4,7 @@ class GeocodeGlib < Formula
   url "https://download.gnome.org/sources/geocode-glib/3.26/geocode-glib-3.26.2.tar.xz"
   sha256 "01fe84cfa0be50c6e401147a2bc5e2f1574326e2293b55c69879be3e82030fd1"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "878e80675652cec9dd995eb7d896681db3203a8567cce2b35577fbc952cb8be0"
@@ -22,9 +22,13 @@ class GeocodeGlib < Formula
   depends_on "pkg-config" => :build
   depends_on "gtk+3"
   depends_on "json-glib"
-  depends_on "libsoup"
+  depends_on "libsoup@2"
 
   def install
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libsoup@2"].opt_lib/"pkgconfig"
+    ENV.prepend_path "XDG_DATA_DIRS", Formula["libsoup@2"].opt_share
+    ENV.prepend_path "XDG_DATA_DIRS", HOMEBREW_PREFIX/"share"
+
     mkdir "build" do
       system "meson", *std_meson_args, "-Denable-installed-tests=false", "-Denable-gtk-doc=false", ".."
       system "ninja"
@@ -33,7 +37,7 @@ class GeocodeGlib < Formula
   end
 
   def post_install
-    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/gnome"
+    system Formula["gtk+3"].opt_bin/"gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/gnome"
   end
 
   test do
