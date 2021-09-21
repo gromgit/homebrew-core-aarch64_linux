6449be15a043c4971a1e0d0db5955b02ffef2d99
Dawid Dziurla <dawidd0811@gmail.com>
2021-09-21 17:46:06 +0200

    gssdp: depend on vala at build time
    Closes #85615.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -16,13 +16,10 @@ class Gssdp < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "vala" => :build
   depends_on "gettext"
   depends_on "glib"
   depends_on "libsoup"
-
-  on_linux do
-    depends_on "vala"
-  end
 
   def install
     mkdir "build" do
