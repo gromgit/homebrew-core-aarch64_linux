7bd375358289da55593ef5ea7eaaa3686e34a2b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-21 05:06:59 +0000

    berglas: update 0.6.1 bottle.

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -6,11 +6,11 @@ class Berglas < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b613bf1ac50406c91c9bc8c8f1b322d1afe4cd2f3e4ddce1560da00f929c1092"
-    sha256 cellar: :any_skip_relocation, big_sur:       "aaa3598f118b10a7dc122231860e4174cee65ff92cdedd5b0fa621146870e2cd"
-    sha256 cellar: :any_skip_relocation, catalina:      "87acc0322aa1d691763b631e3138d90d9d8ff0dc10e2383eb736506cfcb21b30"
-    sha256 cellar: :any_skip_relocation, mojave:        "4edd184d04304be53ddc3b45a78c7659e4367b8e7d87fc294c5218d6e341a18a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bae684b7cf750807d14327d34c65639a72eb1363572bc1d97fc5bf28e28dba75"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "efff48b9ecaaddbec4acb42e734cd9d20ac69ff99cdcee9da1038f4f528f5f70"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3ad3b597268182525c38548ace467d29e38a3a321792b20ad6549818c2b36dc6"
+    sha256 cellar: :any_skip_relocation, catalina:      "462ce1785b8978e6d3e40529895277ffaab10e0b6a6a4b73bfd73c59f59368d8"
+    sha256 cellar: :any_skip_relocation, mojave:        "e415365e97979ea042b08f192621b660200cb184100a9fd28b58c1b2d0317113"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7cdb430cc5aa32e9192a22b54bbc25bec925110be518cd9b87f8a70dda969ea3"
   end
 
   depends_on "go" => :build
