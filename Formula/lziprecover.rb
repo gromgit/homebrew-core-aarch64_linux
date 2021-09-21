fefd27fbc9c7d8e5f0037035157c5688a309cff2
Nanda H Krishna <me@nandahkrishna.com>
2021-09-21 00:48:13 +0000

    lziprecover: add 1.22 bottle.

--- Formula/lziprecover.rb
+++ Formula/lziprecover.rb
@@ -10,6 +10,14 @@ class Lziprecover < Formula
     regex(/href=.*?lziprecover[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1619cf04484327e4a674f51a54c18f69212908483f5aeeef3ce553997058dbfb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fcf5e375245c6fabe48f084846c055c66065af4575027f05a81a5e33ef931a57"
+    sha256 cellar: :any_skip_relocation, catalina:      "f99984733bd12d57d31bda428f03c9603a01f3b33aa1e83cfae464bf5db6fd0e"
+    sha256 cellar: :any_skip_relocation, mojave:        "7cd81f75c35b9ff4e27b60780bdf5503a6e986c94a3772c6493b9cd490dc6a92"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d39f36865f6a9f5c8c4e701534ea500d2b900a1d38b7a455ffb0fd977a23ce81"
+  end
+
   depends_on "lzip" => :test
 
   def install
