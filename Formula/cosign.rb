bdeecc9365a4012726b75541115f829a8f797be1
Rui Chen <rui@chenrui.dev>
2021-09-21 12:40:44 -0400

    cosign 1.2.1
    Closes #85667.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -2,8 +2,8 @@ class Cosign < Formula
   desc "Container Signing"
   homepage "https://github.com/sigstore/cosign"
   url "https://github.com/sigstore/cosign.git",
-      tag:      "v1.2.0",
-      revision: "aa5d23b86d09c2275dd3a7317b7c98cc30a8eee7"
+      tag:      "v1.2.1",
+      revision: "96d39a9f1d1861657c6d89aed9aa1903b42ea2f2"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
