ca19f521dccdab6820b971402b6662eceb1ffdcd
Bevan Kay <email@bevankay.me>
2021-09-22 09:55:08 +1000

    deno 1.14.1
    Closes #85693.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -1,8 +1,8 @@
 class Deno < Formula
   desc "Secure runtime for JavaScript and TypeScript"
   homepage "https://deno.land/"
-  url "https://github.com/denoland/deno/releases/download/v1.14.0/deno_src.tar.gz"
-  sha256 "56f370825f019e4bdb95f354a49576c057f90ef1cc07b7491566763b2147edc6"
+  url "https://github.com/denoland/deno/releases/download/v1.14.1/deno_src.tar.gz"
+  sha256 "330d2342c9e312e20a9e5b4f0f487957a6b0508d127c9848b95a59548cb5db5b"
   license "MIT"
 
   bottle do
