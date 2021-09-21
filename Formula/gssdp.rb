9f293e7b3bad720acd0b10daaf47a49f5055f0e2
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-09-21 16:59:58 +0000

    gssdp: update 1.4.0.1 bottle.

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -5,11 +5,12 @@ class Gssdp < Formula
   sha256 "8676849d57fb822b8728856dbadebf3867f89ee47a0ec47a20045d011f431582"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "f8d473345a33036b882bf4b5721940b17cdfbacc59c6f6c7bc21b354cebafc77"
-    sha256 cellar: :any, big_sur:       "7e3f746570d185163477dbc29d5d1b52bd9fb1655223d9428817e998c62c57df"
-    sha256 cellar: :any, catalina:      "7ca8d4f29d9d29cb3359116f07062a81410900cf0296d2d1f9e9d8d7a3930cb6"
-    sha256 cellar: :any, mojave:        "fc63d29a1b04887aa478eb7da62593e15ec23efb853533f8ab2e2c11db89a533"
-    sha256               x86_64_linux:  "375c1f636abd720cef920fb12e71fd4628f95eaee83fc8fbcbaf9ae86e1dd68a"
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "af3cfdeebe33dc2235b183b4b5db2dc89cc1746e5f9d1f66497540008ac139ec"
+    sha256 cellar: :any, big_sur:       "f1b7f55cf138a7b567c9b19f6a053a96e91c9cf1fe74d15d6d45918e80d0ae7b"
+    sha256 cellar: :any, catalina:      "0247e477790f650d86a7e093747b7a3d48fd59420e85cdac0700c24eea101fe0"
+    sha256 cellar: :any, mojave:        "037dfafa30476082ad6d7c31cdedcda2a45f899a1b2ddba1ab7291f17c59f405"
+    sha256               x86_64_linux:  "ef5baab8ee1c70435c33b983055869689384280ce341b6d27ffc67f8a64662d8"
   end
 
   depends_on "gobject-introspection" => :build
