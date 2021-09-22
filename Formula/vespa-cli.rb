90239a4a34f254c4fd5590d16264b9c84c7ad91b
Martin Polden <mpolden@yahoo-inc.com>
2021-09-22 10:50:02 +0200

    vespa-cli 7.469.18
    Closes #85717.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vespa-cli.rb
+++ Formula/vespa-cli.rb
@@ -1,9 +1,9 @@
 class VespaCli < Formula
   desc "Command-line tool for Vespa.ai"
   homepage "https://vespa.ai"
-  url "https://github.com/vespa-engine/vespa/archive/vespa-7.462.20-1.tar.gz"
-  version "7.462.20"
-  sha256 "87e309fbdac0bef174ef3ac1bf094d551a54dd2da57ecc52e19880d0d81a9cda"
+  url "https://github.com/vespa-engine/vespa/archive/vespa-7.469.18-1.tar.gz"
+  version "7.469.18"
+  sha256 "d718dc93491ea05304d576d3115f408acb149e568f884a5c01321b13780698a0"
   license "Apache-2.0"
 
   livecheck do
@@ -38,7 +38,7 @@ class VespaCli < Formula
     ENV["VESPA_CLI_HOME"] = testpath
     assert_match "vespa version #{version}", shell_output("#{bin}/vespa version")
     query = "yql=select * from sources * where title contains 'foo';"
-    assert_match "Error: Request failed", shell_output("#{bin}/vespa query -t local '#{query}' hits=5")
+    assert_match "Error: Services unavailable", shell_output("#{bin}/vespa query -t local '#{query}' hits=5", 1)
     system "#{bin}/vespa", "config", "set", "target", "cloud"
     assert_match "target = cloud", shell_output("#{bin}/vespa config get target")
   end
