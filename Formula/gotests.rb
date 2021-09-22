8931ce2df6a5bffed6ad073c08ffee29d036ebf6
MoonFruit <dkmoonfruit@gmail.com>
2021-09-22 11:48:52 +0800

    gotests 1.6.0 (new formula)
    Closes #85713.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gotests.rb
@@ -0,0 +1,51 @@
+class Gotests < Formula
+  desc "Automatically generate Go test boilerplate from your source code"
+  homepage "https://github.com/cweill/gotests"
+  url "https://github.com/cweill/gotests/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "f0236dbebd8a3fd19ec4260f490cb164240e1d518d3971b42872978f7a50c040"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./gotests"
+  end
+
+  test do
+    (testpath/"test.go").write <<~EOS
+      package main
+
+      func add(x int, y int) int {
+      	return x + y
+      }
+    EOS
+    expected = <<~EOS
+      Generated Test_add
+      package main
+
+      import "testing"
+
+      func Test_add(t *testing.T) {
+      	type args struct {
+      		x int
+      		y int
+      	}
+      	tests := []struct {
+      		name string
+      		args args
+      		want int
+      	}{
+      		// TODO: Add test cases.
+      	}
+      	for _, tt := range tests {
+      		t.Run(tt.name, func(t *testing.T) {
+      			if got := add(tt.args.x, tt.args.y); got != tt.want {
+      				t.Errorf("add() = %v, want %v", got, tt.want)
+      			}
+      		})
+      	}
+      }
+    EOS
+    assert_equal expected, shell_output("#{bin}/gotests -all test.go")
+  end
+end
