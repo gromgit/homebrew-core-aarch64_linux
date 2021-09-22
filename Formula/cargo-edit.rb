e01d88a4e367b48790a4087e915fd7729befb867
Branch Vincent <branchevincent@gmail.com>
2021-09-22 17:26:40 -0400

    cargo-edit 0.8.0
    Closes #85750.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-edit.rb
+++ Formula/cargo-edit.rb
@@ -1,10 +1,9 @@
 class CargoEdit < Formula
   desc "Utility for managing cargo dependencies from the command-line"
   homepage "https://killercup.github.io/cargo-edit/"
-  url "https://github.com/killercup/cargo-edit/archive/v0.7.0.tar.gz"
-  sha256 "56b51ef8d52d8b414b5c4001053fa196dc7710fea9b1140171a314bc527a2ea2"
+  url "https://github.com/killercup/cargo-edit/archive/v0.8.0.tar.gz"
+  sha256 "4a08e914c17204cb3ab303b62362ca30d44cf457b3b1d7bde117b8ab4cb2fa64"
   license "MIT"
-  revision 2
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "eb04c951480fec71b299c9cdda64fb0a3e7fc84a349fb185b7a193116d74ee4a"
@@ -13,9 +12,9 @@ class CargoEdit < Formula
     sha256 cellar: :any, mojave:        "30e4b1ed5ddf69752deebec4480aab5aad32fe9da351324b67e42c00024c413d"
   end
 
-  depends_on "rust" => [:build, :test]
   depends_on "libgit2"
   depends_on "openssl@1.1"
+  depends_on "rust" # uses `cargo` at runtime
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -24,16 +23,12 @@ class CargoEdit < Formula
   test do
     crate = testpath/"demo-crate"
     mkdir crate do
+      (crate/"src/main.rs").write "// Dummy file"
       (crate/"Cargo.toml").write <<~EOS
         [package]
         name = "demo-crate"
         version = "0.1.0"
       EOS
-
-      # Update the crates.io index. cargo-add doesn't currently handle this properly.
-      # https://github.com/killercup/cargo-edit/issues/420
-      # Remove this and the rust test dependency when this is fixed.
-      system "cargo", "search", "--limit=0"
 
       system bin/"cargo-add", "add", "clap@2", "serde"
       system bin/"cargo-add", "add", "-D", "just@0.8.3"
