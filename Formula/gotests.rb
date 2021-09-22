a42f861c56703b258255bee4369e428dd87ca285
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-22 07:49:38 +0000

    gotests: add 1.6.0 bottle.

--- Formula/gotests.rb
+++ Formula/gotests.rb
@@ -5,6 +5,14 @@ class Gotests < Formula
   sha256 "f0236dbebd8a3fd19ec4260f490cb164240e1d518d3971b42872978f7a50c040"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ca761cc187d08b5d211562b383610d614c67a4533de9c552f6e2b57a07b7ed5b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "222e2a0280ae3d085bbf76080b9bd30e4fe1e9942a0427dc2840d74337621f76"
+    sha256 cellar: :any_skip_relocation, catalina:      "717ea92ad6172ab6ca8987b618683c85f4a576161e78bc75c02000966ea3f9ac"
+    sha256 cellar: :any_skip_relocation, mojave:        "41417917289de92b26a9182dec51f9d43dc7bcf9c93eb2751143f5f70ecb0c80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2f0b6e37e88deaf7713ebf6a2e34baf767e2a7b6168244f31e909ef4b8f8100"
+  end
+
   depends_on "go" => :build
 
   def install
