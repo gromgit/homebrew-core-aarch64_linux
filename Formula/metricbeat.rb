c3e43e4cffb4149d72f08f29692510dfa7439710
Alexander Zigelski <crunchtime-ali@users.noreply.github.com>
2021-09-22 18:08:14 +0000

    metricbeat 7.15.0
    Closes #85737.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -2,8 +2,8 @@ class Metricbeat < Formula
   desc "Collect metrics from your systems and services"
   homepage "https://www.elastic.co/beats/metricbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v7.14.2",
-      revision: "574c21d25ddb65a63665ac26b54799f81a7e9706"
+      tag:      "v7.15.0",
+      revision: "9023152025ec6251bc6b6c38009b309157f10f17"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git"
 
