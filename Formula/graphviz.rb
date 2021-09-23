e70ae5cc66052a7470c9b3807e20ec568fc6ee40
Branch Vincent <branchevincent@gmail.com>
2021-09-22 23:03:26 -0400

    graphviz 2.49.1
    Closes #85765.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -2,8 +2,8 @@ class Graphviz < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://www.graphviz.org/"
   url "https://gitlab.com/graphviz/graphviz.git",
-      tag:      "2.49.0",
-      revision: "438731242936b0b4c481f3b0893f4cdb0c689e93"
+      tag:      "2.49.1",
+      revision: "b6c12d35981d206b113a0f81e6eccbb3a5b42049"
   license "EPL-1.0"
   version_scheme 1
   head "https://gitlab.com/graphviz/graphviz.git"
