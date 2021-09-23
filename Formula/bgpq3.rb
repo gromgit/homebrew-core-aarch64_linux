fb2dcf991cf6d9bcd0ad173232f472701e189c73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-23 14:07:56 +0000

    bgpq3: update 0.1.36 bottle.

--- Formula/bgpq3.rb
+++ Formula/bgpq3.rb
@@ -12,13 +12,11 @@ class Bgpq3 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8007ef3e5542b067d4684b88e1ac509d2bc8566ba85536235070081dba0994f1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4550c91dd5daeaf0320c7b678ffd68c5d5fc3d611857f5dcb4fe9b7130e3f439"
-    sha256 cellar: :any_skip_relocation, catalina:      "428a0dcb0af2876c03374236ee10b2385ab993dc54cc12e080198d7e552bbdea"
-    sha256 cellar: :any_skip_relocation, mojave:        "a16c482aa8e1a821d6747b2871174109ccbcf407e5799794f6307303fcffafcb"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "2bf730bec0bca51bd9a3db7a3cd5e4bf36199717a8190db270a5f4751bb1a5e1"
-    sha256 cellar: :any_skip_relocation, sierra:        "052fb1ae9a1546b13f865b25f4ff5879f4a7c77350d14720442fc6cd898d833d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e677c99a35fcedcf1022bbd06eac0f3868327d8bf8f16cf87c4d2cf867fb8f75"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "472157a36cb85bb858b6687a6fccb1dc32221b93545190459c6441f8644977ba"
+    sha256 cellar: :any_skip_relocation, big_sur:       "21400e8e71cc8647ec4acbf90d4dbe1ed01dc483bae6c5fd422ce7856c922bb0"
+    sha256 cellar: :any_skip_relocation, catalina:      "20323444446909aa9892ea16f3a2cbdc8ce6665be3270713dc01c3cb0163d35a"
+    sha256 cellar: :any_skip_relocation, mojave:        "19d8b27219ecd0988603200ad428df266bacfddea15a4570de15ddfd2a017190"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55ff963a23fb548d055ae5bea489108a00caeca54c3308db10eb399c400a049d"
   end
 
   # Makefile: upstream has been informed of the patch through email (multiple
