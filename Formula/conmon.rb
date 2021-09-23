cc06e5464326dfb99188c98d8081b9d2e58e46fd
Caleb Xu <calebcenter@live.com>
2021-09-23 07:56:01 -0400

    conmon 2.0.30
    * conmon 2.0.30
    * conmon: depend on libseccomp
    
    Closes #85783.
    
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/conmon.rb
+++ Formula/conmon.rb
@@ -1,8 +1,8 @@
 class Conmon < Formula
   desc "OCI container runtime monitor"
   homepage "https://github.com/containers/conmon"
-  url "https://github.com/containers/conmon/archive/refs/tags/v2.0.29.tar.gz"
-  sha256 "eb4d5e157671a61b88786e44094775194e30e1d0ad0b9d50035532ece78dbc28"
+  url "https://github.com/containers/conmon/archive/refs/tags/v2.0.30.tar.gz"
+  sha256 "4b0a98fbe8a63c42f60edac25c19aa6606caa7b1e4fe7846fc7f7de0b566ba25"
   license "Apache-2.0"
 
   bottle do
@@ -13,6 +13,7 @@ class Conmon < Formula
   depends_on "pkg-config" => :build
 
   depends_on "glib"
+  depends_on "libseccomp"
   depends_on :linux
   depends_on "systemd"
 
