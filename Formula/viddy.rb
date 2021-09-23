454a24c0d94b69e022526ee329894d0412ab3021
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-23 20:07:31 +0000

    viddy: update 0.3.1 bottle.

--- Formula/viddy.rb
+++ Formula/viddy.rb
@@ -7,11 +7,11 @@ class Viddy < Formula
   head "https://github.com/sachaos/viddy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6f6ac1e35cc8ac5cf4196a76a5b9d91eb3f9d619923126e3eeacd34faedd5fd3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2327bc60f9f90c450013699cb6a07d0ff65290a9d630b2ab90e4eda5baa98a2c"
-    sha256 cellar: :any_skip_relocation, catalina:      "e4881b0f77e8e9432540bb7343e3ab56ff8f4c4f2cbd1f2ad78eec7fb7fdb8ac"
-    sha256 cellar: :any_skip_relocation, mojave:        "152bdb2cd7eaee548d5a5113ed16c79f7053608cb976ae311e02e9f87dcb41e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "022a512261214b49ce8642cc6b7eb338beda3230b88908bbfebec6af44f3c8b0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1c4e5817f0684e356c2ab09828f76b0d05c0e034408018fa3ae0479b5f4b1849"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a41c2e1195882c2f7742d534b28f0bb43677b262e0e5a66ff8e556296a039f37"
+    sha256 cellar: :any_skip_relocation, catalina:      "1f7190c3d2f026fb06a7b12e1345be3b564de5265d29fadbe89b3d3995869882"
+    sha256 cellar: :any_skip_relocation, mojave:        "6f0d8ea45e9a44c3da562a7e0b7ca230517cc9fd4ab153fe6521e4e8988355b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48522eaf53c20f2b5d9fe96cb28843962039d27686c065e0c2eac301aff8fce1"
   end
 
   depends_on "go" => :build
