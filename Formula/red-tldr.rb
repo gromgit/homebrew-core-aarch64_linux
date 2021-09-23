cb54c8d32e02a693021af97a3da97f0b85524eef
Caleb Xu <calebcenter@live.com>
2021-09-23 08:50:04 -0400

    red-tldr 0.4.3
    Closes #85793.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/red-tldr.rb
+++ Formula/red-tldr.rb
@@ -1,8 +1,8 @@
 class RedTldr < Formula
   desc "Used to help red team staff quickly find the commands and key points"
   homepage "https://payloads.online/red-tldr/"
-  url "https://github.com/Rvn0xsy/red-tldr/archive/v0.4.2.tar.gz"
-  sha256 "94cc5f195fac8617873f30616d65eba4b44b2ce4c58d7b6a5f8e2bbceef569f4"
+  url "https://github.com/Rvn0xsy/red-tldr/archive/v0.4.3.tar.gz"
+  sha256 "3f32a438226287d80ae86509964d7767c2002952c95da03501beb882cae22d2d"
   license "MIT"
   head "https://github.com/Rvn0xsy/red-tldr.git", branch: "main"
 
