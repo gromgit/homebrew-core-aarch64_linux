df414259a6a157cf3aa820e98f9cf902de7f1b3a
Caleb Xu <calebcenter@live.com>
2021-09-23 08:04:55 -0400

    digdag 0.10.3
    Closes #85785.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -1,8 +1,8 @@
 class Digdag < Formula
   desc "Workload Automation System"
   homepage "https://www.digdag.io/"
-  url "https://dl.digdag.io/digdag-0.10.2.jar"
-  sha256 "3d85c052cc4a6d6bf545e1a64b02f8ac3830bc1b7910d7f2d798c297cd4b1c41"
+  url "https://dl.digdag.io/digdag-0.10.3.jar"
+  sha256 "200911b6a35a3d8b40e25f028753fb9652aeb19dbd05b8f950dc35ff69547c34"
   license "Apache-2.0"
 
   livecheck do
