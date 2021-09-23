8c0abefe1a7a29c8082f777ae41de6d5de6cee15
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2021-09-24 00:11:25 +0100

    cbmc: remove `cmake` compiler designation. (#85799)
    * cbmc: remove `cmake` compiler designation.
    
    We now have incorporated this as part of an upstream change to make
    compilation on osX more straightforward, so this is now redundant.
    
    * cbmc: clean up
    
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -28,11 +28,7 @@ class Cbmc < Formula
   fails_with gcc: "5"
 
   def install
-    args = []
-    # Workaround borrowed from https://github.com/diffblue/cbmc/issues/4956
-    args << "-DCMAKE_C_COMPILER=/usr/bin/clang" if OS.mac?
-
-    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
