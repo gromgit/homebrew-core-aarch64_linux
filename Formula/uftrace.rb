3d09dec17fc82a852af8cfb3d2dd57107164e1e6
Rui Chen <rui@chenrui.dev>
2021-09-23 20:33:28 -0400

    uftrace 0.11
    Closes #85848.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/uftrace.rb
+++ Formula/uftrace.rb
@@ -1,10 +1,9 @@
 class Uftrace < Formula
   desc "Function graph tracer for C/C++/Rust"
   homepage "https://uftrace.github.io/slide/"
-  url "https://github.com/namhyung/uftrace/archive/v0.10.tar.gz"
-  sha256 "b8b56d540ea95c3eafe56440d6a998e0a140d53ca2584916b6ca82702795bbd9"
+  url "https://github.com/namhyung/uftrace/archive/v0.11.tar.gz"
+  sha256 "101dbb13cb3320ee76525ec26426f2aa1de4e3ee5af74f79cb403ae4d2c6c871"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/namhyung/uftrace.git", branch: "master"
 
   bottle do
