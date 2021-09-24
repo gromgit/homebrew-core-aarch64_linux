930ec373094607cbf80dfa334e4b46645d0b5553
rui <rui@chenrui.dev>
2021-09-24 14:48:58 +0000

    kn: update 0.26.0 bottle.

--- Formula/kn.rb
+++ Formula/kn.rb
@@ -8,11 +8,11 @@ class Kn < Formula
   head "https://github.com/knative/client.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "153e6261572b440165f50fdff03a0e261e20ec3128a127ceac71940e95f0aa4e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ff799fabf564de8201bf797b990ec8045f5fb6d2257508910ffffe1ccbd9673a"
-    sha256 cellar: :any_skip_relocation, catalina:      "ff799fabf564de8201bf797b990ec8045f5fb6d2257508910ffffe1ccbd9673a"
-    sha256 cellar: :any_skip_relocation, mojave:        "ff799fabf564de8201bf797b990ec8045f5fb6d2257508910ffffe1ccbd9673a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5cd97c29b91774f1f83cfa05c11115f2e928dfc5f3738ded35cae3ef941b24c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "61c1e60bb5a297b50c19b29fc64420bdd2da3c1d9a104a5fd7c0f908d7b9465c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b02d96368c5d026876ecc9168b36b1c494e5b3ff9724853ce470a969f739c5a2"
+    sha256 cellar: :any_skip_relocation, catalina:      "b02d96368c5d026876ecc9168b36b1c494e5b3ff9724853ce470a969f739c5a2"
+    sha256 cellar: :any_skip_relocation, mojave:        "b02d96368c5d026876ecc9168b36b1c494e5b3ff9724853ce470a969f739c5a2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f4b4f870fe3845c801cb10108dbd69cbdf1029e1dbf1c24c3c607df0c9eeba1"
   end
 
   depends_on "go" => :build
