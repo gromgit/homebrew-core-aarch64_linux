91597cd8664b7e62e9f4dec96393169425bbc3ec
Rui Chen <rui@chenrui.dev>
2021-09-25 13:46:36 -0400

    pmd 6.39.0
    Closes #85941.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.38.0/pmd-bin-6.38.0.zip"
-  sha256 "bc316327421367fd33a9fe3655d10aea1486d01e744a0f9096e3bf7a869664e8"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.39.0/pmd-bin-6.39.0.zip"
+  sha256 "3831a51241c38285e4c0e375e844a2195c0dc5216d7247ffe12f2ab3fced64fd"
   license "BSD-4-Clause"
 
   bottle do
