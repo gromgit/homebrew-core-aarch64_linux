2d55fe5c070f4a897ec8d5d85d85cd8639e199bc
Rui Chen <rui@chenrui.dev>
2021-09-25 12:51:55 -0400

    functionalplus 0.2.16
    Closes #85921.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/functionalplus.rb
+++ Formula/functionalplus.rb
@@ -1,9 +1,9 @@
 class Functionalplus < Formula
   desc "Functional Programming Library for C++"
   homepage "https://github.com/Dobiasd/FunctionalPlus"
-  url "https://github.com/Dobiasd/FunctionalPlus/archive/v0.2.15-p0.tar.gz"
-  version "0.2.15"
-  sha256 "4c76104ec8f6da5e66ed768380bdf128e0ba01725056c40a3c1b850cf4b441ad"
+  url "https://github.com/Dobiasd/FunctionalPlus/archive/v0.2.16-p0.tar.gz"
+  version "0.2.16"
+  sha256 "6026e64260afbd6941aaf19559d6e5dc51cbb3e045ef8d8e158d96bcd8651ed6"
   license "BSL-1.0"
   head "https://github.com/Dobiasd/FunctionalPlus.git"
 
