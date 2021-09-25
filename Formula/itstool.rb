b421b3952b0fb2872ce970aebe66db7a36810193
Rui Chen <rui@chenrui.dev>
2021-09-25 12:53:49 -0400

    itstool 2.0.7
    Closes #85924.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -1,10 +1,9 @@
 class Itstool < Formula
   desc "Make XML documents translatable through PO files"
   homepage "http://itstool.org/"
-  url "https://github.com/itstool/itstool/archive/2.0.6.tar.gz"
-  sha256 "bda0b08e9a1db885c9d7d1545535e9814dd8931d5b8dd5ab4a47bd769d0130c6"
+  url "https://github.com/itstool/itstool/archive/2.0.7.tar.gz"
+  sha256 "fba78a37dc3535e4686c7f57407b97d03c676e3a57beac5fb2315162b0cc3176"
   license "GPL-3.0"
-  revision 2
   head "https://github.com/itstool/itstool.git"
 
   bottle do
