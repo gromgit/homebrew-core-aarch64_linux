3fd9bf5a057dc2e200efbfa5b39f0fc6896ea838
Sam Ford <1584702+samford@users.noreply.github.com>
2021-09-25 10:52:57 -0400

    avce00: add livecheck

--- Formula/avce00.rb
+++ Formula/avce00.rb
@@ -4,6 +4,11 @@ class Avce00 < Formula
   url "http://avce00.maptools.org/dl/avce00-2.0.0.tar.gz"
   sha256 "c0851f86b4cd414d6150a04820491024fb6248b52ca5c7bd1ca3d2a0f9946a40"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?avce00[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "9c4e10ddc6cad4b6a7001a697b8318513a09166e70fa6831ed5fdbede1e71d47"
     sha256 cellar: :any_skip_relocation, big_sur:       "1fdd45d6a401ca88019bbd58cb3afdda23dedf706c4556e87a7cc48b1a3e952a"
