cc0d437903da9da142a8df471c6a5f2d7f88734c
Rui Chen <rui@chenrui.dev>
2021-09-25 12:46:27 -0400

    bison 3.8.2
    Closes #85915.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -2,9 +2,9 @@ class Bison < Formula
   desc "Parser generator"
   homepage "https://www.gnu.org/software/bison/"
   # X.Y.9Z are beta releases that sometimes get accidentally uploaded to the release FTP
-  url "https://ftp.gnu.org/gnu/bison/bison-3.8.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/bison/bison-3.8.1.tar.xz"
-  sha256 "31fc602488aad6bdecf0ccc556e0fc72fc57cdc595cf92398f020e0cf4980f15"
+  url "https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/bison/bison-3.8.2.tar.xz"
+  sha256 "9bba0214ccf7f1079c5d59210045227bcf619519840ebfa80cd3849cff5a5bf2"
   license "GPL-3.0-or-later"
   version_scheme 1
 
