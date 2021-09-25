87d973f3ac2731383b86609df4e9c8fd73d944b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-25 21:07:44 +0000

    libvmaf: update 2.2.1 bottle.

--- Formula/libvmaf.rb
+++ Formula/libvmaf.rb
@@ -6,11 +6,11 @@ class Libvmaf < Formula
   license "BSD-2-Clause-Patent"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "f2adac0ebf6a1d228baa816132efa0477bbdf53e3fed586927c17eba0d73d7db"
-    sha256 cellar: :any,                 big_sur:       "903463941bf0fd5dc38c35c3893db9a9c97a587122ba49dd7db98d82e33164a2"
-    sha256 cellar: :any,                 catalina:      "be05df065d1a6b5402fe6d9a2a68e9fb9184f752b125b366b79bc9c5560c0199"
-    sha256 cellar: :any,                 mojave:        "9e6a47e5a4d145b5ed8e2be17de5eadca56425c27dee792310aee1baa9f51c27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5874760e03675b5f171c3707959ef1205d5c82742f4240a36906c59d175454de"
+    sha256 cellar: :any,                 arm64_big_sur: "fee5dad3a7de18f2e298a162730f2c12bf571ae46677f610a549676a932167fe"
+    sha256 cellar: :any,                 big_sur:       "14cbc4c13ca4638b1bfab15320451eaaad85a0d1f02850a7ea2cdeadcc996d5f"
+    sha256 cellar: :any,                 catalina:      "473bc63397de71d332e7bc2ce96ebaf7dd9aa79127b75b33fd1a344cac8d95ff"
+    sha256 cellar: :any,                 mojave:        "960cb12eb62f5a3eee4c7ff5bed852d58d340dd4bb0be98e2aadf470f85cdbfa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1bbf4bc9099b33a927cbf16548d40b5a9cf5e4adb42527ff60d84a063a11db10"
   end
 
   depends_on "meson" => :build
