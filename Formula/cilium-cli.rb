bb1a0c003c263ce60f7e9e77017fb4d729c15984
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-25 04:06:43 +0000

    cilium-cli: update 0.9.1 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -6,11 +6,11 @@ class CiliumCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "aa6e358d5e0ba7fc9d2911611457de56162e9c0277e1f94089be768d93f7c4fc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "eb01295e22d3ba7bc97b295aa2b5b044d5a138258fbe97cd8db49420a84e53ec"
-    sha256 cellar: :any_skip_relocation, catalina:      "857e9a3bd730c8a77ef84b2620d23037094fbc676fb84e7909cc33b1735da121"
-    sha256 cellar: :any_skip_relocation, mojave:        "8e034133cf7c28835c47f50006a07828b237aa53865972c9fcfc1945167724f7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3833821cc133c36fa0a98213d30983b3783dbba2d021f7ff48a0c8f51bbdd824"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d6f60f2746a8ac9611aebd2d6b16fb5d4b0594208d21d327a5f2e6fdab7795f8"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d1f58448204e2cc792c6e8864ae8033b1f19a7efadbfe8751280e973f6759843"
+    sha256 cellar: :any_skip_relocation, catalina:      "05a99a179f3287de0f53c84687c9f1919c0d05172682d86c122d471314c8533f"
+    sha256 cellar: :any_skip_relocation, mojave:        "24722bc7957c2b87783694ff4bf436d46fa1c4447f878e04635ded8b8535eb18"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ace8b86d74337b1badfbd14925363d5632de607bd08cd0ca3198636480edb818"
   end
 
   depends_on "go" => :build
