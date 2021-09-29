9c1da0016377f7d78b01c8dbaae9f41485bc5687
miccal <miccal.matthews@gmail.com>
2021-09-29 08:22:10 +0800

    bdw-gc 8.0.6
    * bdw-gc 8.0.6
    * bdw-gc: update patch for current release
    
    Closes #86092.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bdw-gc.rb
+++ Formula/bdw-gc.rb
@@ -2,17 +2,17 @@ class BdwGc < Formula
   desc "Garbage collector for C and C++"
   homepage "https://www.hboehm.info/gc/"
   license "MIT"
-  revision 2
 
+  # Remove stable block when patch is removed
   stable do
-    url "https://github.com/ivmai/bdwgc/releases/download/v8.0.4/gc-8.0.4.tar.gz"
-    sha256 "436a0ddc67b1ac0b0405b61a9675bca9e075c8156f4debd1d06f3a56c7cd289d"
+    url "https://github.com/ivmai/bdwgc/releases/download/v8.0.6/gc-8.0.6.tar.gz"
+    sha256 "3b4914abc9fa76593596773e4da671d7ed4d5390e3d46fbf2e5f155e121bea11"
 
-    # Extension to handle multithreading
+    # Extension to handle multithreading. Remove in v8.2.0.
     # https://github.com/ivmai/bdwgc/pull/277
     patch do
-      url "https://github.com/ivmai/bdwgc/commit/5668de71107022a316ee967162bc16c10754b9ce.patch?full_index=1"
-      sha256 "5c42d4b37cf4997bb6af3f9b00f5513644e1287c322607dc980a1955a09246e3"
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/f14c259aef209e5f5df302b834b2119381dd36d5/bdw-gc/crystal-mt.patch"
+      sha256 "18380da9c5451c9b7668ccf5e1f106f8cf8115992d9a403e32444fb487566c33"
     end
   end
 
@@ -25,7 +25,7 @@ class BdwGc < Formula
   end
 
   head do
-    url "https://github.com/ivmai/bdwgc.git"
+    url "https://github.com/ivmai/bdwgc.git", branch: "master"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool"  => :build
