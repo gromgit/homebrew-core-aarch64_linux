6c6e784d07f1bb40988ae94df8c7a64703529992
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-29 17:10:25 +0000

    vite: update 2.6.0 bottle.

--- Formula/vite.rb
+++ Formula/vite.rb
@@ -8,11 +8,11 @@ class Vite < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "03341a3e7c5a7304aa0f4f0054dfc9f28934589eb033397d0749e97697244e6f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "48ecbe0dbb2c7febdf33fbe9701a9466ff604e240df0c8e6e890936c9148336a"
-    sha256 cellar: :any_skip_relocation, catalina:      "48ecbe0dbb2c7febdf33fbe9701a9466ff604e240df0c8e6e890936c9148336a"
-    sha256 cellar: :any_skip_relocation, mojave:        "48ecbe0dbb2c7febdf33fbe9701a9466ff604e240df0c8e6e890936c9148336a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "72ad05f6be2ba3aa39cd753fbcf275f98d7d6d9ade827a284890ceb07cbabef3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "762abca9bfaee4ba59d906af77e24c81f9ca6cd0c931a5e6cfc7dcef0716d105"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9efcd67353fd6ad1cdb4c7557d302ad0b5bba039cc2eed9b5c8395e71fa0488a"
+    sha256 cellar: :any_skip_relocation, catalina:      "9efcd67353fd6ad1cdb4c7557d302ad0b5bba039cc2eed9b5c8395e71fa0488a"
+    sha256 cellar: :any_skip_relocation, mojave:        "9efcd67353fd6ad1cdb4c7557d302ad0b5bba039cc2eed9b5c8395e71fa0488a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "805b6fea064efd2fc425e3b5f8a7697208407453daafe438cb067e4930205e80"
   end
 
   depends_on "node"
