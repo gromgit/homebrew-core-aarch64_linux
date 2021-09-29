13774c95743cc7d4ac735d07f8a321caed241663
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-29 23:53:17 +0800

    gitless: revision bump (libgit2 1.3.0)
    Also, update patch for libgit2 1.3.0.

--- Formula/gitless.rb
+++ Formula/gitless.rb
@@ -6,7 +6,7 @@ class Gitless < Formula
   url "https://files.pythonhosted.org/packages/9c/2e/457ae38c636c5947d603c84fea1cf51b7fcd0c8a5e4a9f2899b5b71534a0/gitless-0.8.8.tar.gz"
   sha256 "590d9636d2ca743fdd972d9bf1f55027c1d7bc2ab1d5e877868807c3359b78ef"
   license "MIT"
-  revision 8
+  revision 9
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "a75cc978953796b096a5b4e1b70c9083f15a600575f86b7df630523dd3518d64"
@@ -54,11 +54,11 @@ class Gitless < Formula
     url "https://files.pythonhosted.org/packages/6b/23/a8c5b726a58282fe2cadcc63faaddd4be147c3c8e0bd38b233114adf98fd/pygit2-1.6.1.tar.gz"
     sha256 "c3303776f774d3e0115c1c4f6e1fc35470d15f113a7ae9401a0b90acfa1661ac"
 
-    # libgit2 1.2+ support
+    # libgit2 1.3 support
     # https://github.com/libgit2/pygit2/pull/1089
     patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/bf370d7782f7686a4a61d8d7d28068e78e28e48c/pygit2/libgit2-1.2.0.patch"
-      sha256 "b5acca41ff7752ea25adb050d7494939fc6d471f4f91d94a06f4afc9902f4117"
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/54d3a0d1f241fdd4e9229312ced0d8da85d964b1/pygit2/libgit2-1.3.0.patch"
+      sha256 "4d501c09d6642d50d89a1a4d691980e3a4a2ebcb6de7b45d22cce16a451b9839"
     end
   end
 
