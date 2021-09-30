4ae1234a1d2e1484698c15defd8f5d3f6b6b7410
Rui Chen <rui@chenrui.dev>
2021-09-29 21:44:37 -0400

    watchman 2021.09.27.00
    Closes #86145.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -1,8 +1,8 @@
 class Watchman < Formula
   desc "Watch files and take action when they change"
   homepage "https://github.com/facebook/watchman"
-  url "https://github.com/facebook/watchman/archive/v2021.09.13.00.tar.gz"
-  sha256 "90c417f3f96e31e7e18fc5ab0824f929ad081c81d03315e6e2288c73c12be602"
+  url "https://github.com/facebook/watchman/archive/v2021.09.27.00.tar.gz"
+  sha256 "e93a8915fbb8c2152fd32273006bd9e653550049453717f54bd887817b249608"
   license "MIT"
   head "https://github.com/facebook/watchman.git", branch: "main"
 
