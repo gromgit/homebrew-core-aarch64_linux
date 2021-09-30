6bd592112fa4822a3d4b85567e00c22d8f55495f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-30 04:07:41 +0000

    ko: update 0.9.3 bottle.

--- Formula/ko.rb
+++ Formula/ko.rb
@@ -6,11 +6,11 @@ class Ko < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c07b307a4a5a4a22501d099d331f222cf9b6d2ed1a21053c782872d21f1d936a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2f8e6fcfc3205e830387bff26c4f5ac01ac9d4e9afbc902a7140976dd30cf965"
-    sha256 cellar: :any_skip_relocation, catalina:      "bfd196ae4e4184bba5b4e5c6bad6b5dc4fab57af2f22b383d1ab00cb0090c030"
-    sha256 cellar: :any_skip_relocation, mojave:        "359497988b2c425ff3ba43ab22e8b6dae276c51372f390e5fe065f448fe74768"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b93b10a18c9e5c04e4d3409e7f109d84680545b945ee53c614a98f6f8673c55b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "05aee246743e81f1249b21b6972168756588d69e55ed6eb7c313527e1e7ef002"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2f7f3143a1033749e9ea454c920f8a8ced7fecb79ad7f51ba610ce03a2a5150e"
+    sha256 cellar: :any_skip_relocation, catalina:      "2ee78e315a67abd4852c5aed5518c34edb19807a42c19bcbd81d9c28063f038f"
+    sha256 cellar: :any_skip_relocation, mojave:        "2632cb6ac6ba4340008befa04b0b1a81415506a3c0b526006183a22636ce14e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2591ec7bdad23739ba49f2835c9b8d8de0b70fd1ae59ce36c0500fba482c1463"
   end
 
   depends_on "go" => :build
