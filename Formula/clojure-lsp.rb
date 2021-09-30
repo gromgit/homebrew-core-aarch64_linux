2cf3c0e1de393153368c540d196efaf7a0eb47ed
Rui Chen <rui@chenrui.dev>
2021-09-30 01:08:57 -0400

    clojure-lsp 20210913T222535
    Closes #86165.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -2,9 +2,9 @@ class ClojureLsp < Formula
   desc "Language Server (LSP) for Clojure"
   homepage "https://github.com/clojure-lsp/clojure-lsp"
   url "https://github.com/clojure-lsp/clojure-lsp.git",
-      tag:      "2021.08.16-19.02.30",
-      revision: "226fa911d10d332a2b1df9201cc8565a3002f642"
-  version "20210816T190230"
+      tag:      "2021.09.13-22.25.35",
+      revision: "d564f81e25c71cc370c33d745881af1187f97667"
+  version "20210913T222535"
   license "MIT"
   head "https://github.com/clojure-lsp/clojure-lsp.git", branch: "master"
 
