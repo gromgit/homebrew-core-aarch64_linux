bdc142bf8e8359617a9c168780a570c5f7b8ad1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-01 13:11:57 +0000

    gitlab-runner: update 14.3.2 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -13,11 +13,11 @@ class GitlabRunner < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "535ccbc734817b038e531bb90dbdc15ecb6f4414186b0b35fa8109fbbd350c11"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ba54124ebdb8ec521f7c745d23fe7bd24ed2762880c8c373b773b9b10e9225d3"
-    sha256 cellar: :any_skip_relocation, catalina:      "46b51fbd2859d1c43a0ba6063ab8546f9b66ba0c8d42e8762387d244aa41dc19"
-    sha256 cellar: :any_skip_relocation, mojave:        "e05aa52fff231b427327a4ee34d9b899a5192b21de234fb7a0467f3f81de75b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9bf64f9e2e8c0792ae9308052c3ebc16f5fc0678c848890b42835073b59ec58"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f23ba0ea62f49c9f93ec2881837955ca52a8aaf9cb8e7897ca38715a76a754e4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d3f75a4e9d61b9451b1813be0d74ab943ef2ac829eeef0fafb02068455fd43bc"
+    sha256 cellar: :any_skip_relocation, catalina:      "5b8db7df68728bd7e13310d8eb77ab75766519d7e3d001c21fc4c2cdd0f62fb1"
+    sha256 cellar: :any_skip_relocation, mojave:        "927546c4075cb8329beef754a90afe8d01cf422e3d58dad072c28f3cbd8e53d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a721f192fd1606f7c8666cc25d456bfeef02c79b7ed6323be4e62d2ab33c9bf8"
   end
 
   depends_on "go" => :build
