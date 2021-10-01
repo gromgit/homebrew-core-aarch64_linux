e3db41be6dd882e19b434df8b9cea60a7c2b01dd
Rui Chen <rui@chenrui.dev>
2021-10-01 18:06:36 -0400

    angular-cli 12.2.8
    Closes #86290.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-12.2.7.tgz"
-  sha256 "c6df21286bde837046a6d78eae46f80261fb73d482d33247e2e771acd5f8dc4e"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-12.2.8.tgz"
+  sha256 "e45766dc131ed5c645e60b50921031a16169e2e0a505f63a91cb4ad3dbafe882"
   license "MIT"
 
   bottle do
