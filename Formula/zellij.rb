4119ab758ae132acdae47e218d718ff273a47997
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-01 01:34:51 +0000

    zellij: update 0.18.1 bottle.

--- Formula/zellij.rb
+++ Formula/zellij.rb
@@ -6,11 +6,11 @@ class Zellij < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "17e1e84e6f7349b1bab42f14f8d7d1875154a6a9154ba709d27c12394969ce86"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ba3c7f10bbe57bde803cf3353dec300331e8e86eecc9a725b1101e08aa42e85d"
-    sha256 cellar: :any_skip_relocation, catalina:      "fdcc5c1f0d49ef82f2e13950d6cb73f169ee1675059f13f6b4d2e30b613375dd"
-    sha256 cellar: :any_skip_relocation, mojave:        "a2adc7c2e8bb6a9b29b6017bbcc5836edef626bcefbbf8f99c7459ed0b318c03"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95d55e44891dfd7ced8c9e1a86cda3925f433c5cf14801da5eea908e71d0caec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ffcefef361baf3ff64e3e7886b06651bf3a61ab7a3cb3c8f5defcc811893632d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8ad1b08cd95fa7c1d2b919ef4b0acc8789e7f6f6ce1cbea5909dc059a2681a70"
+    sha256 cellar: :any_skip_relocation, catalina:      "7e3ffa9ee3535ad5936a16385ff6b16f6fe9539f77b663f8953a1d319a312d3b"
+    sha256 cellar: :any_skip_relocation, mojave:        "d67154fc065bf0b2053edf8f3e35b867a26293848ebe19954bb223b9ab991586"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7f929541e8d238e703be5ae40715179929cc3a15938a87d2b30c8ac9e750eb3"
   end
 
   depends_on "rust" => :build
