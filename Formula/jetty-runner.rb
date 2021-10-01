870cb924df36f0c649881294e1762ac4146a9dac
Alexander Zigelski <crunchtime-ali@users.noreply.github.com>
2021-10-01 18:08:20 +0000

    jetty-runner 9.4.44.v20210927
    Closes #86280.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -1,9 +1,9 @@
 class JettyRunner < Formula
   desc "Use Jetty without an installed distribution"
   homepage "https://www.eclipse.org/jetty/"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.43.v20210629/jetty-runner-9.4.43.v20210629.jar"
-  version "9.4.43.v20210629"
-  sha256 "7f7246eb2520c91aa406f50aa2d0969c0816058d080e2fcb3e74fda9cceeca8b"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.44.v20210927/jetty-runner-9.4.44.v20210927.jar"
+  version "9.4.44.v20210927"
+  sha256 "efc948f654bbba3e8578acd8f6d5fd9691e0570abc2e80429e9a1f229a97feb0"
   license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
