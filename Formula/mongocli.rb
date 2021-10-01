7e53677f66007a1401381e0920df79362765b2ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-01 22:08:06 +0000

    mongocli: update 1.20.3 bottle.

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -6,11 +6,11 @@ class Mongocli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3d278ad35465e12300daad0e9f70162cbfbf36ecea95085bd62720250e8f9623"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7671eefec6ba2a72d9157d4fbeef109816ec10453edec02b77bf1dd74189b156"
-    sha256 cellar: :any_skip_relocation, catalina:      "d4a207e590dab7f5a79ede0a8390d86dccdc75faa40fe4a6ba04d2128e7295e9"
-    sha256 cellar: :any_skip_relocation, mojave:        "63080bb88ee9c1b8caa310f146ef398ecf4d7ee8479e20866db849c7f4b78f95"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc9f880ddec22a34522ebcbc4abede3376240ea55c89bbdf4e7fc3ece654e190"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eae4eeb5b6342d0e2140cc979779eeec630fb038518c788ce97a30ad256f0eb7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "14789d66690f3af076b074535be67ac28f0e34e8e23eeba2a83d97f8f340ea25"
+    sha256 cellar: :any_skip_relocation, catalina:      "024ec0fe3adda957c70f239e10aa8a285e9086eab367d2f34c70a8045ab99e1c"
+    sha256 cellar: :any_skip_relocation, mojave:        "4af81c5dea666f2445f74726f6816015027f8ce14b9ca4a962b6411c55f22c69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46cf06c31f763f92c4052fe5cee22515ffabef7962e03fcb2e8ccbcc80c55cf6"
   end
 
   depends_on "go" => :build
