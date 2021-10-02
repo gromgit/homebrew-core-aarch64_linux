569990b8d573adf467718c1925e40fecdbb7584a
Christian Clason <c.clason@uni-graz.at>
2021-10-02 17:00:28 +0200

    uncrustify 0.73.0
    * uncrustify 0.73.0
    * uncrustify: build with GCC on Linux
    
    Closes #86327.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -1,8 +1,8 @@
 class Uncrustify < Formula
   desc "Source code beautifier"
   homepage "https://uncrustify.sourceforge.io/"
-  url "https://github.com/uncrustify/uncrustify/archive/uncrustify-0.72.0.tar.gz"
-  sha256 "d6fff70bc7823fac4c77013055333b79a4839909094e8eee8a14ee8f1777374e"
+  url "https://github.com/uncrustify/uncrustify/archive/uncrustify-0.73.0.tar.gz"
+  sha256 "2df0326ba8c413d675b796e051d89a318b7c9cccebc993d66466e2e7fd970672"
   license "GPL-2.0-or-later"
   head "https://github.com/uncrustify/uncrustify.git", branch: "master"
 
@@ -17,7 +17,20 @@ class Uncrustify < Formula
 
   depends_on "cmake" => :build
 
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  # patches from https://github.com/uncrustify/uncrustify/pull/3178
+  # and https://github.com/uncrustify/uncrustify/pull/3179 that fix build failures
+  # can be removed for 0.74.0
+  patch :DATA
+
   def install
+    ENV.cxx11
+
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
@@ -41,3 +54,80 @@ class Uncrustify < Formula
     assert_equal expected, File.read("#{testpath}/t.c.uncrustify")
   end
 end
+
+__END__
+diff --git a/src/output.cpp b/src/output.cpp
+index fd60ebab61..eb22edce04 100644
+--- a/src/output.cpp
++++ b/src/output.cpp
+@@ -17,13 +17,10 @@
+ #include "unc_ctype.h"
+ #include "unicode.h"
+ 
++#include <map>
+ #include <regex>
+ #include <set>
+ 
+-#ifdef WIN32
+-#include <map>                    // to get std::map
+-#endif // WIN32
+-
+ 
+ constexpr static auto LCURRENT = LOUTPUT;
+ 
+diff --git a/src/tokenize.cpp b/src/tokenize.cpp
+index f0a4e2d95f..f412b6f138 100644
+--- a/src/tokenize.cpp
++++ b/src/tokenize.cpp
+@@ -16,10 +16,7 @@
+ #include "unc_ctype.h"
+ 
+ #include <regex>
+-
+-#ifdef WIN32
+-#include <stack>            // to get std::stack
+-#endif // WIN32
++#include <stack>
+ 
+ 
+ #define LE_COUNT(x)    cpd.le_counts[static_cast<size_t>(LE_ ## x)]
+
+diff --git a/src/backup.cpp b/src/backup.cpp
+index c2a9ae51b5..18d4f854f8 100644
+--- a/src/backup.cpp
++++ b/src/backup.cpp
+@@ -29,9 +29,7 @@
+ #include "md5.h"
+ #include "unc_ctype.h"
+ 
+-#ifdef __APPLE__
+-#include <cerrno>                // is needed under OSX
+-#endif
++#include <cerrno>
+ 
+ using namespace std;
+ 
+diff --git a/src/keywords.cpp b/src/keywords.cpp
+index 5e4be69103..56a8a69363 100644
+--- a/src/keywords.cpp
++++ b/src/keywords.cpp
+@@ -13,6 +13,7 @@
+ #include "args.h"
+ #include "prototypes.h"
+ 
++#include <cerrno>
+ #include <map>
+ 
+ 
+diff --git a/src/uncrustify.cpp b/src/uncrustify.cpp
+index bc01f53e23..7ad986cfa7 100644
+--- a/src/uncrustify.cpp
++++ b/src/uncrustify.cpp
+@@ -43,6 +43,7 @@
+ #include "universalindentgui.h"
+ #include "width.h"
+ 
++#include <cerrno>
+ #include <fcntl.h>
+ #include <map>
+ #ifdef HAVE_UNISTD_H
