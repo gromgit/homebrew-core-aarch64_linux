d2eb42a28117d9f22c3a742128ac4283f483e63f
EricFromCanada <enk3@outlook.com>
2021-10-02 12:04:12 -0400

    libcap 2.59
    Closes #86332.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libcap.rb
+++ Formula/libcap.rb
@@ -1,9 +1,14 @@
 class Libcap < Formula
   desc "User-space interfaces to POSIX 1003.1e capabilities"
   homepage "https://sites.google.com/site/fullycapable/"
-  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.27.tar.xz"
-  sha256 "dac1792d0118bee6aae6ba7fb93ff1602c6a9bda812fd63916eee1435b9c486a"
+  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.59.tar.xz"
+  sha256 "49f64ae40b113e53cbc161e22af7094e3bb4a0611fa33ef5a4257b011779f034"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
+
+  livecheck do
+    url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/"
+    regex(/href=.*?libcap[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "c32acc483373e3079418a9fd64e7a1826caa82184b51a5294038cb4feed79418"
