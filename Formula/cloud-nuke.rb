068af267e5603dbd4f4fecf19b6b12bc4b0837d4
Stephen <stephengroat@users.noreply.github.com>
2021-10-01 23:52:24 -0700

    cloud-nuke: remove caveats (#86317)
    no longer accurate, now support standard aws credentials https://github.com/gruntwork-io/cloud-nuke#aws-1

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -20,15 +20,6 @@ class CloudNuke < Formula
     system "go", "build", "-ldflags", "-s -w -X main.VERSION=v#{version}", *std_go_args
   end
 
-  def caveats
-    <<~EOS
-      Before you can use these tools, you must export some variables to your $SHELL.
-        export AWS_ACCESS_KEY="<Your AWS Access ID>"
-        export AWS_SECRET_KEY="<Your AWS Secret Key>"
-        export AWS_REGION="<Your AWS Region>"
-    EOS
-  end
-
   test do
     assert_match "A CLI tool to nuke (delete) cloud resources", shell_output("#{bin}/cloud-nuke --help 2>1&")
     assert_match "ec2", shell_output("#{bin}/cloud-nuke aws --list-resource-types")
