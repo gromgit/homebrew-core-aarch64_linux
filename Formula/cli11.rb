910249d6caad79de9b6fc70af5fd1172423db9be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-02 07:08:04 +0000

    cli11: update 2.1.1 bottle.

--- Formula/cli11.rb
+++ Formula/cli11.rb
@@ -7,7 +7,7 @@ class Cli11 < Formula
   head "https://github.com/CLIUtils/CLI11.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "14e40203b7a5a2a7aba74846647ed01780ba2ebf97fbaeb78f9a82ec2a12a543"
+    sha256 cellar: :any_skip_relocation, all: "9a31a1aad95d40ff011b1fcc1c5aa315fee958277ea1e8af31c5cdaa14febaa5"
   end
 
   depends_on "cmake" => :build
