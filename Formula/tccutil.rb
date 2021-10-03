87621e8d465caba3ac2ac27440bd87a1ce78aaa7
Jacob Salmela <me@jacobsalmela.com>
2021-10-03 10:14:16 -0500

    tccutil 1.2.11
    Closes #86387.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tccutil.rb
+++ Formula/tccutil.rb
@@ -1,8 +1,8 @@
 class Tccutil < Formula
   desc "Utility to modify the macOS Accessibility Database (TCC.db)"
   homepage "https://github.com/jacobsalmela/tccutil"
-  url "https://github.com/jacobsalmela/tccutil/archive/v1.2.10.tar.gz"
-  sha256 "8a6f9961b37322dee0f20a70b4299a23f16f3023a549147a4410e3f322dbfa22"
+  url "https://github.com/jacobsalmela/tccutil/archive/v1.2.11.tar.gz"
+  sha256 "efff442bc4d1b50ededa0798c9e3a6a881ac3d06310148cf438d5e531f9d6564"
   license "GPL-2.0-or-later"
   head "https://github.com/jacobsalmela/tccutil.git", branch: "main"
 
