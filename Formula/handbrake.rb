1d631d63d6282d4cb1844e552c0b0c0e3d47ded4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-03 20:07:53 +0000

    handbrake: update 1.4.2 bottle.

--- Formula/handbrake.rb
+++ Formula/handbrake.rb
@@ -7,11 +7,11 @@ class Handbrake < Formula
   head "https://github.com/HandBrake/HandBrake.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "037e8b1be2b8264f233aef92208ba264072a66cfdf26591dedd8a40ad44c5796"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6d8f7a87b12e402d23142a3b4940170126a9e85ed6e218fe9af93aaf57f8eba2"
-    sha256 cellar: :any_skip_relocation, catalina:      "3fe4097ce9a1f0ef6c22212eba9059ee3d181eb8a1875577994efff1d8be4d57"
-    sha256 cellar: :any_skip_relocation, mojave:        "e4cedfb355baeed4c7b9ad6017db1be6f6cf2c666f4120a1a8eb95066b23a88b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "268c727839cac0f0d7044355ebc90ca8ee86ad1e44cc41bcf00eb766f37337f6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ce915dd92e8ecea860c57f930fe059fbebb2f4282a64c68c77988d6e4e3ba090"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bdaf789e29ad765f12f4ee1d6173bc7c4254019c979646f87b5054be3dd9c785"
+    sha256 cellar: :any_skip_relocation, catalina:      "405a3c3c9d3511ddcf31b93c800e6db7fe6c1dbfacc68b98891ef846dec1d390"
+    sha256 cellar: :any_skip_relocation, mojave:        "cd6a2f04d56322bd3f51ca0b64f54e51a98d3709d9c0a62e42c6475658bcbe60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "036238b99cddfee4e7553d14a82aadf6e14293b8903126b620d88f0dc0d7a7b2"
   end
 
   depends_on "autoconf" => :build
