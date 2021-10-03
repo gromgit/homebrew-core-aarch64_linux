85c3c71423b2431a4ebf422f1bb6cf6dcc433ab2
EricFromCanada <enk3@outlook.com>
2021-10-02 22:52:22 -0400

    wayland-protocols 1.23
    Closes #86355.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -1,23 +1,25 @@
 class WaylandProtocols < Formula
   desc "Additional Wayland protocols"
   homepage "https://wayland.freedesktop.org"
-  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.22.tar.xz"
-  sha256 "96e7cf03524995a47028236c6d6141c874e693cb80c0be8dabe15455cdd5a5a7"
+  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.23.tar.xz"
+  sha256 "6c0af1915f96f615927a6270d025bd973ff1c58e521e4ca1fc9abfc914633f76"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "97dc1b8c59b0e36bc6a3f78863de15a5e675f6231d371f6ca98d2ee21ee6cf4e"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "wayland" => :build
   depends_on :linux
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}",
-                          "--localstatedir=#{var}",
-                          "--disable-silent-rules"
+    system "./autogen.sh", "--prefix=#{prefix}",
+                           "--sysconfdir=#{etc}",
+                           "--localstatedir=#{var}",
+                           "--disable-silent-rules"
     system "make"
     system "make", "install"
   end
