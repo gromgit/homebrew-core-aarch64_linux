c9580568857b18d95ea720eb01154f07fb375a76
Dawid Dziurla <dawidd0811@gmail.com>
2021-10-03 07:37:31 +0000

    zurl: update 1.11.0_3 bottle.

--- Formula/zurl.rb
+++ Formula/zurl.rb
@@ -7,10 +7,11 @@ class Zurl < Formula
   revision 3
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d4252a3968cce4e2dbb442c16e9ceac0f917ea44f6fe29746fb62cc7b7fdbd36"
-    sha256 cellar: :any, big_sur:       "ec815b28c14380cbc309c11fb2becb4e0421b3d933dfbe4f3b881941b97069c3"
-    sha256 cellar: :any, catalina:      "2d34fd92311ba6e171d3bc3a5c567daa4238a0d06e0cd078c79ce4c5368890a3"
-    sha256 cellar: :any, mojave:        "21b2977646141c7d191a9f835c42b70eff3e793b799228386043ac62ae44a34b"
+    sha256 cellar: :any,                 arm64_big_sur: "d4252a3968cce4e2dbb442c16e9ceac0f917ea44f6fe29746fb62cc7b7fdbd36"
+    sha256 cellar: :any,                 big_sur:       "ec815b28c14380cbc309c11fb2becb4e0421b3d933dfbe4f3b881941b97069c3"
+    sha256 cellar: :any,                 catalina:      "2d34fd92311ba6e171d3bc3a5c567daa4238a0d06e0cd078c79ce4c5368890a3"
+    sha256 cellar: :any,                 mojave:        "21b2977646141c7d191a9f835c42b70eff3e793b799228386043ac62ae44a34b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35c074c84d46a1310585b97afbb7f039fb427083d69182f959a07c279acd696a"
   end
 
   depends_on "pkg-config" => :build
