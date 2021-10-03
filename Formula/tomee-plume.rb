7ff170b1efc92a289ff38c7db1f19cfdf83d7f12
EricFromCanada <enk3@outlook.com>
2021-10-02 23:57:00 -0400

    tomee-plume 8.0.8
    Closes #86359.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomee-plume.rb
+++ Formula/tomee-plume.rb
@@ -1,9 +1,9 @@
 class TomeePlume < Formula
   desc "Apache TomEE Plume"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.4/apache-tomee-8.0.4-plume.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.4/apache-tomee-8.0.4-plume.tar.gz"
-  sha256 "fff4d98606f023b11c37bb7b4ceb74600552ff2953dc90fb484456236da34663"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.8/apache-tomee-8.0.8-plume.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.8/apache-tomee-8.0.8-plume.tar.gz"
+  sha256 "396e92760483ac871a446219fa7f35b01927e4bea41bc9d922d0211bb526913b"
   license "Apache-2.0"
 
   bottle do
@@ -21,8 +21,8 @@ class TomeePlume < Formula
     # Install files
     prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
     libexec.install Dir["*"]
-    (bin/"tomee-plume-startup").write_env_script "#{libexec}/bin/startup.sh",
-                                                 Language::Java.overridable_java_home_env
+    bin.install Dir["#{libexec}/bin/*.sh"]
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
   end
 
   def caveats
@@ -30,12 +30,11 @@ class TomeePlume < Formula
       The home of Apache TomEE Plume is:
         #{opt_libexec}
       To run Apache TomEE:
-        #{opt_libexec}/bin/tomee-plume-startup
+        #{opt_bin}/startup.sh
     EOS
   end
 
   test do
-    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
-    system "#{opt_libexec}/bin/configtest.sh"
+    system "#{opt_bin}/configtest.sh"
   end
 end
