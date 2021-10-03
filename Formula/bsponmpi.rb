5d87e5490da76b6501931abbc0598232c49afd34
Daniel Nachun <daniel.nachun@gmail.com>
2021-10-03 16:07:18 -0700

    bsponmpi: depend on macOS because of Python 2 (#86407)
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/bsponmpi.rb
+++ Formula/bsponmpi.rb
@@ -3,6 +3,7 @@ class Bsponmpi < Formula
   homepage "https://sourceforge.net/projects/bsponmpi/"
   url "https://downloads.sourceforge.net/project/bsponmpi/bsponmpi/0.3/bsponmpi-0.3.tar.gz"
   sha256 "bc90ca22155be9ff65aca4e964d8cd0bef5f0facef0a42bc1db8b9f822c92a90"
+  license "LGPL-2.1-or-later"
   revision 2
 
   bottle do
@@ -17,6 +18,7 @@ class Bsponmpi < Formula
   deprecate! date: "2021-08-08", because: :does_not_build
 
   depends_on "scons" => :build
+  depends_on :macos
   depends_on "open-mpi"
 
   def install
