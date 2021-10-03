fa486d1d3a634b3a189eac90e8fc4af2ecf66a9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-03 00:12:48 +0000

    gnunet: update 0.15.3 bottle.

--- Formula/gnunet.rb
+++ Formula/gnunet.rb
@@ -7,11 +7,11 @@ class Gnunet < Formula
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "913d3a808c3beaacaa7aa0307c200af943f539bc5203a4b2c62e6e6e9919eee8"
-    sha256 cellar: :any,                 big_sur:       "b97c192441e2b2836c0dfead9f2e85cfea8ae0889e34510436be4b3dc427b944"
-    sha256 cellar: :any,                 catalina:      "fa620c92930b29e01dc564f14867462126cf508692be8b9c3ca5c716419c6f7c"
-    sha256 cellar: :any,                 mojave:        "8f032fccf8877f461e5ccf87d059a9649ca056493b984a931ddc97317f45512a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "77d500a6ec053927f4df368c4a9c230844b50f30e787d14a268becf8d6c95355"
+    sha256 cellar: :any,                 arm64_big_sur: "e11fd43cdca5151094863572ecda637522293c97882a316ee637ff01d48668d5"
+    sha256 cellar: :any,                 big_sur:       "ee3b0b12547986dbea1806833d4a0845a4ac092b5653be75b4bb5a2ff257456e"
+    sha256 cellar: :any,                 catalina:      "2d6022911c05c414bb187c58077f3e27e4c7cf710a0c7470b1175e2e0ac74788"
+    sha256 cellar: :any,                 mojave:        "0990533dacaf2decb9eb3bc20f7d8e21d1d9999670a74c2de483cb893464f114"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6bb67039350368fce378f5738c23a80429cec01d873b892cce27cfbed8d83012"
   end
 
   depends_on "pkg-config" => :build
