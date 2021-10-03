c2ad7ef8b0de4da6c1e9f11257cd7ec985b54325
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-03 13:09:45 +0000

    pcsc-lite: update 1.9.4 bottle.

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -11,11 +11,11 @@ class PcscLite < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "9fe3e880c7dbad11250e2b9c9ec667b9f889acd93b3fcde0b58fbfe67c840e9e"
-    sha256 cellar: :any,                 big_sur:       "073696834600548ef1830d0087374bf1321a9b8a9bc3ad6d52551a2d66400c47"
-    sha256 cellar: :any,                 catalina:      "bdbf3a4a84b10227f6096ee0afa72dc57fb9b78ece3f6ed09a22bef02b0348cb"
-    sha256 cellar: :any,                 mojave:        "f3857ad6c5c1ac83a44d2c590a71f1c0c15dfcf1462763fa59d1225ce6c11d07"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4910f18bae9e0aec5366e078f199929cd299d6643b9cdcb1819007b40b4d634"
+    sha256 cellar: :any,                 arm64_big_sur: "2a2c65f8456b5f8a409a5ce90601539b23553ded657c0049c36311d52584dd87"
+    sha256 cellar: :any,                 big_sur:       "f5d733e1cccba6a07f87b29ec9a18886d210376ea13d0a42eeacdadb706c97a3"
+    sha256 cellar: :any,                 catalina:      "e97ed967e538b00673e7b0e46592adafd3dbc0f69bb67aea921fe24e51c376ff"
+    sha256 cellar: :any,                 mojave:        "4afd86caaf938458d6697b216f0c6760e4d1e7b8228995943e316eb6bf315476"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dbbcbdc0bab21c08e55d7f5d8e897cc5f17dd3e387a38c9e11480996c189f050"
   end
 
   keg_only :shadowed_by_macos, "macOS provides PCSC.framework"
