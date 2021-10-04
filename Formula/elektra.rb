90e99796ddf7e66ebfe83b087677bf6324409039
Mihael Pranjić <mpranj@limun.org>
2021-10-04 09:00:33 +0200

    elektra 0.9.8
    Closes #86421.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elektra.rb
+++ Formula/elektra.rb
@@ -1,8 +1,8 @@
 class Elektra < Formula
   desc "Framework to access config settings in a global key database"
   homepage "https://libelektra.org/"
-  url "https://www.libelektra.org/ftp/elektra/releases/elektra-0.9.7.tar.gz"
-  sha256 "12b7b046004db29317b7b937dc794abf719c400ba3115af8d41849127b562681"
+  url "https://www.libelektra.org/ftp/elektra/releases/elektra-0.9.8.tar.gz"
+  sha256 "b1e8908c138b84e788fdff25eab1c2b07e0b422a5fd1667814539ea02f151c58"
   license "BSD-3-Clause"
   head "https://github.com/ElektraInitiative/libelektra.git"
 
