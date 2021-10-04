2cef961d06de4622c78d873822f05accc773991f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-04 23:06:51 +0000

    docker: update 20.10.9 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -13,11 +13,11 @@ class Docker < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2f006963b0e5393fa808670e88c3b6c6bab963aca3a7968f5bfeb41c331217c1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1e6f9213259e150fcc57d05c55336018031f484277577a07b5740c3bbbc1cebb"
-    sha256 cellar: :any_skip_relocation, catalina:      "6f5af0ff463a59e5e11343f34a6a9f5d194cc8cde56cc9a3a5b70ecdf22e95fd"
-    sha256 cellar: :any_skip_relocation, mojave:        "e71bd32fcfb1071531869362af409ca7f9903d8831a8551c694fbca6962397e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "927529c05c83d23e433e4ac4dee94dce9b174101b80732a99726942e705ad35f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "06a4f27c2c0a84b0321ba20e6c6f8d37de44eddc66bddda737d6a725a9b8c07e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f0cf1c8352a7172bdbbc28d7c16f4b5f91f65dad382abe76b99a7622b0fd3659"
+    sha256 cellar: :any_skip_relocation, catalina:      "34d879c9c88c4df9c676d2cc6e1d5fa348f5530f1420cb5d35b15ea49fe7f87d"
+    sha256 cellar: :any_skip_relocation, mojave:        "5098c15dcc506cbbcc2916556ee2e034b0e5143139c7541da751aa194076e9ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "687d2a22221cd497810d4564a2abfb01e9de12019808e9a9f75a65d81d10b4f6"
   end
 
   depends_on "go" => :build
