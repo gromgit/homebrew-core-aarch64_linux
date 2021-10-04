f73cbe6ac3406efe5b7d1d720351556acbc62da7
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2021-10-04 02:03:45 +0000

    nacl: update 20110221 bottle.

--- Formula/nacl.rb
+++ Formula/nacl.rb
@@ -15,13 +15,14 @@ class Nacl < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any_skip_relocation, big_sur:     "89574694f733c8aa852e09e3828f10dd6ce2ece4219bd825e5f6c18253bddb28"
-    sha256 cellar: :any_skip_relocation, catalina:    "b9fb1445709388168d0e1c56964a7540e8ff4e6294d31eb23c62368ce56e6d1b"
-    sha256 cellar: :any_skip_relocation, mojave:      "bb0b22e1aa3a87657b064def3d19bcad419a4339889046f931a5eac7e5bc8bc1"
-    sha256 cellar: :any_skip_relocation, high_sierra: "43fffe959f6a95aacff4d5d4b7bfbb34f835a2487e8bff0645473d8ec1de83b6"
-    sha256 cellar: :any_skip_relocation, sierra:      "86e5ef1c0a14b029d1ed3f63df48fde9c302adbbc3e1dcacd1bb7617bf547615"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "e08c93b814989405fa3b7db9e3a9c4f149e36aaab32aba44e9a2f1659d2d3efd"
-    sha256 cellar: :any_skip_relocation, yosemite:    "1a1a7fffc6d41f2f7bcc393375f2907f63b5a13f9414fe0827daef96246301e7"
+    sha256 cellar: :any_skip_relocation, big_sur:      "89574694f733c8aa852e09e3828f10dd6ce2ece4219bd825e5f6c18253bddb28"
+    sha256 cellar: :any_skip_relocation, catalina:     "b9fb1445709388168d0e1c56964a7540e8ff4e6294d31eb23c62368ce56e6d1b"
+    sha256 cellar: :any_skip_relocation, mojave:       "bb0b22e1aa3a87657b064def3d19bcad419a4339889046f931a5eac7e5bc8bc1"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "43fffe959f6a95aacff4d5d4b7bfbb34f835a2487e8bff0645473d8ec1de83b6"
+    sha256 cellar: :any_skip_relocation, sierra:       "86e5ef1c0a14b029d1ed3f63df48fde9c302adbbc3e1dcacd1bb7617bf547615"
+    sha256 cellar: :any_skip_relocation, el_capitan:   "e08c93b814989405fa3b7db9e3a9c4f149e36aaab32aba44e9a2f1659d2d3efd"
+    sha256 cellar: :any_skip_relocation, yosemite:     "1a1a7fffc6d41f2f7bcc393375f2907f63b5a13f9414fe0827daef96246301e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "03cbc3623ab6388289a613b8499f4e120d19cda47b661bd494e0d0cdfc355288"
   end
 
   def install
