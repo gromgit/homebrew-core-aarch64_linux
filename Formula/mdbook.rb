5109d0a2967e70a1439a6d00cb7ff14979247c82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-04 13:13:50 +0000

    mdbook: update 0.4.13 bottle.

--- Formula/mdbook.rb
+++ Formula/mdbook.rb
@@ -7,11 +7,11 @@ class Mdbook < Formula
   head "https://github.com/rust-lang/mdBook.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b9f487d2c7f7f2fb65cc99ceced811a4170453d8bec0c7d60cec717a0f3b53ca"
-    sha256 cellar: :any_skip_relocation, big_sur:       "32743f10b38d4a421e87fbc61773abd08f8b286d35e77797c0fecb847f845896"
-    sha256 cellar: :any_skip_relocation, catalina:      "0e3250f1ba78dd8d4da7876be308b784df527befa88eb96e4fc900722d4a2723"
-    sha256 cellar: :any_skip_relocation, mojave:        "83def6375e138ef30407d0822f8af2f17c465be4159eb27fc508a7074aef9057"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8521b42392591d9ca3940f5984279eb33f6cf716b20475b141587cb864a2ac94"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9561cf4600591a7832591d906e4992e3d18d6f40d8b1672d103877af058cfbe5"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e0ca7966601af810e8bed831e3ab0729469f9ce3d525117dd2de663d531621fe"
+    sha256 cellar: :any_skip_relocation, catalina:      "55bfe16440b12b5bc9d1013aced30aa66358c0160d519083cd44d1bb679a6505"
+    sha256 cellar: :any_skip_relocation, mojave:        "aa9be33d9d55d5ef7a2f1f3457cd064f3b9dd782274e3a1a54ab584aee35b81d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0671a46eabf1310248f560d85cd3f84148fd362c08fcb95606181eb196263811"
   end
 
   depends_on "rust" => :build
