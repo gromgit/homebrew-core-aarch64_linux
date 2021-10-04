76444d331d9849d8664f31f9ee6532f299af8579
BJ Hargrave <bj@hargrave.dev>
2021-10-04 17:58:43 -0400

    bnd 6.0.0
    * bnd 6.0.0
    * bnd: add license
    
    Closes #86454.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bnd.rb
+++ Formula/bnd.rb
@@ -1,8 +1,9 @@
 class Bnd < Formula
   desc "Swiss Army Knife for OSGi bundles"
   homepage "https://bnd.bndtools.org/"
-  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/5.3.0/biz.aQute.bnd-5.3.0.jar"
-  sha256 "f02cd3406b054da1840795c3cecc11c4226a8563aa74a67acc05f3d9aeaa9b85"
+  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/6.0.0/biz.aQute.bnd-6.0.0.jar"
+  sha256 "6ca070c9d33fb7756aff64813cb49d258e4664a9c3d4953c797da394e2348112"
+  license any_of: ["Apache-2.0", "EPL-2.0"]
 
   livecheck do
     url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/maven-metadata.xml"
