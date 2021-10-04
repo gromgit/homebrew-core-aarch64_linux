331280c6a237c2b8b78abc58c23c9ee2eb88e844
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-04 16:08:31 +0000

    jfrog-cli: update 2.4.1 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -6,11 +6,11 @@ class JfrogCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "586f5b9ec6d605ea3f02ebad7f101372782397f9c6499ed0e62663c86c68eaaa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3fbb4f782454ba63e1a0ea2da29d0fa9af93497df6b890455193c5baca1bfea3"
-    sha256 cellar: :any_skip_relocation, catalina:      "f589bee68ca81546aca874b2dd33b03e8fe5ee157ad4765669dba60297359b24"
-    sha256 cellar: :any_skip_relocation, mojave:        "cbefda593b63b285ca1438bb252bc38c801237e59907342c1f36be560952bdca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d5fa601c4a50489cc281157a2b3ee2be8556b0078313b73e7d41c02f86c95b55"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c062b09d3694a62897950243dca460ac566bdd8de494d7218a754730fb8a07e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "397e90940d4d2708d10424d0bf7d7bdb827a539d569a5f8cf161b83be992f9d5"
+    sha256 cellar: :any_skip_relocation, catalina:      "6a1546569755ddaf58e04c25e57c5dfec11dc873798118c315f29a6c8362185f"
+    sha256 cellar: :any_skip_relocation, mojave:        "db4009d0c74598eeb2eeb3d3aca9b29becb4c62514e6590629f5299a628840f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f3ffa51b6b19c936001305667513475a55836832e37ac715afc7264dd19667e"
   end
 
   depends_on "go" => :build
