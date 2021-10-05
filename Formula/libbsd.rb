cc54b64f3ccc8b1a84fcde152b7d0c39ad48c4f3
Sam Ford <1584702+samford@users.noreply.github.com>
2021-10-05 16:44:18 -0400

    libbsd: add livecheck

--- Formula/libbsd.rb
+++ Formula/libbsd.rb
@@ -5,6 +5,11 @@ class Libbsd < Formula
   sha256 "ff95cf8184151dacae4247832f8d4ea8800fa127dbd15033ecfe839f285b42a1"
   license "BSD-3-Clause"
 
+  livecheck do
+    url "https://libbsd.freedesktop.org/releases/"
+    regex(/href=.*?libbsd[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "47ff83f8f952f71030f61444df19635a5eb3d98edc346be4ee3955efc8f7d4fa"
   end
