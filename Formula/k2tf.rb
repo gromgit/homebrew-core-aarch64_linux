619fa73e110ebec459ebc027ef6a1305cfc3ec26
Rui Chen <rui@chenrui.dev>
2021-10-04 20:30:03 -0400

    k2tf 0.6.2
    * k2tf 0.6.0
    * k2tf 0.6.2
    
    Closes #86463.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k2tf.rb
+++ Formula/k2tf.rb
@@ -1,8 +1,8 @@
 class K2tf < Formula
   desc "Kubernetes YAML to Terraform HCL converter"
   homepage "https://github.com/sl1pm4t/k2tf"
-  url "https://github.com/sl1pm4t/k2tf/archive/v0.5.0.tar.gz"
-  sha256 "94113ffb4874b9206148b7ea8bda56f30381b037547651e7c19af8547d845706"
+  url "https://github.com/sl1pm4t/k2tf/archive/v0.6.2.tar.gz"
+  sha256 "e8971e6e7783e8e96014939f67d81570042f147f027fb1eb4c2f524632a68403"
   license "MPL-2.0"
   head "https://github.com/sl1pm4t/k2tf.git", branch: "master"
 
