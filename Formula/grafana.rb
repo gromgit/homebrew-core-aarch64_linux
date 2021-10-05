5cebe8d389d7f4536e982ebf896542aa6e8d0bbb
Rui Chen <rui@chenrui.dev>
2021-10-05 11:12:46 -0400

    grafana 8.1.7
    * grafana 8.1.6
    * grafana 8.1.7
    
    Closes #86505.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v8.1.5.tar.gz"
-  sha256 "8b821910affa5ba3d2b72f77aff05a4c5150e9cccd1416d7621fbe2ea3aaa2c8"
+  url "https://github.com/grafana/grafana/archive/v8.1.7.tar.gz"
+  sha256 "08179d4cc19d689ee8e2ca2d903f26f768fe2092d7145912ead46c3335c8d90d"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
 
