29a6994d592b90b7d89f7a8d27248e4805a80904
Rui Chen <rui@chenrui.dev>
2021-10-05 11:17:01 -0400

    berglas 0.6.2
    Closes #86510.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -1,8 +1,8 @@
 class Berglas < Formula
   desc "Tool for managing secrets on Google Cloud"
   homepage "https://github.com/GoogleCloudPlatform/berglas"
-  url "https://github.com/GoogleCloudPlatform/berglas/archive/v0.6.1.tar.gz"
-  sha256 "25896fa483895d4f79c30314df95c5afcbb1af2f92355ccabb5d58edb8c71ca5"
+  url "https://github.com/GoogleCloudPlatform/berglas/archive/v0.6.2.tar.gz"
+  sha256 "03ee83b49598f864537aec48ef081d3a79a0cf0f32027d815c37755bf5d31376"
   license "Apache-2.0"
 
   bottle do
