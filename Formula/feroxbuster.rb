6ce12aa1ef97742f06699076e23a0f15d270f62c
Dominik Nakamura <dnaka91@gmail.com>
2021-10-05 17:55:08 +0900

    feroxbuster 2.3.3 (new formula)
    * feroxbuster 2.3.3 (new formula)
    * Remove caveats
    * Create a proper test for feroxbuster
    * Shut down miniserve properly after test
    
    Closes #86484.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/feroxbuster.rb
@@ -0,0 +1,39 @@
+class Feroxbuster < Formula
+  desc "Fast, simple, recursive content discovery tool written in Rust"
+  homepage "https://epi052.github.io/feroxbuster"
+  url "https://github.com/epi052/feroxbuster/archive/v2.3.3.tar.gz"
+  sha256 "5a244d1614fb9dc476e9be8bd1cdbb33e0b07a1fcb7285e911ae6c77a44c7327"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "miniserve" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"wordlist").write <<~EOS
+      a.txt
+      b.txt
+    EOS
+
+    (testpath/"web").mkpath
+    (testpath/"web/a.txt").write "a"
+    (testpath/"web/b.txt").write "b"
+
+    port = free_port
+    pid = fork do
+      exec "miniserve", testpath/"web", "-i", "127.0.0.1", "--port", port.to_s
+    end
+
+    sleep 1
+
+    begin
+      exec bin/"feroxbuster", "-q", "-w", testpath/"wordlist", "-u", "http://127.0.0.1:#{port}"
+    ensure
+      Process.kill("SIGINT", pid)
+      Process.wait(pid)
+    end
+  end
+end
