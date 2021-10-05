4dc3f1fdf28eb294e400694d721ec9a636417df7
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-05 03:31:47 +0000

    python-tk@3.10: add 3.10.0 bottle.

--- Formula/python-tk@3.10.rb
+++ Formula/python-tk@3.10.rb
@@ -10,6 +10,14 @@ class PythonTkAT310 < Formula
     formula "python@3.10"
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "1ee8f1de8afa700ac6e58788ac9bd0441cbc06fff7a7369df8a7381dbedf8c20"
+    sha256 cellar: :any, big_sur:       "4cc3cd0c18a7dae959fed8609b8acdce3b1e550336cef5e1be9f816b60e6e352"
+    sha256 cellar: :any, catalina:      "85d0c86f5600f8d5254a34fbc75fe0f8425cdaa51b501cbd2fb9645c4ad28ccb"
+    sha256 cellar: :any, mojave:        "47aff48291f42c9116239982196d36264afb9b6afbebca2e3f0e8f706960d569"
+    sha256               x86_64_linux:  "33e5057494f2cbbeba821221aa97de4c3729c825cf2f47e583081d5bff55267b"
+  end
+
   depends_on "python@3.10"
   depends_on "tcl-tk"
 
