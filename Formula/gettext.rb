3938c024eb583da1cd4e398bb550bd6c65ac57f0
Bo Anderson <mail@boanderson.me>
2021-10-05 14:44:02 +0100

    gettext: remove xz dependency

--- Formula/gettext.rb
+++ Formula/gettext.rb
@@ -1,10 +1,10 @@
 class Gettext < Formula
   desc "GNU internationalization (i18n) and localization (l10n) library"
   homepage "https://www.gnu.org/software/gettext/"
-  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.tar.xz"
-  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.xz"
-  sha256 "d20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192"
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
+  sha256 "c77d0da3102aec9c07f43671e60611ebff89a996ef159497ce8e59d075786b12"
   license "GPL-3.0-or-later"
 
   bottle do
