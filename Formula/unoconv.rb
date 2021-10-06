4b18940f6d69b0092f3759ecd8d1361f21d81f82
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:40:02 -0400

    unoconv: migrate to python@3.10
    Closes #86684.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/unoconv.rb
+++ Formula/unoconv.rb
@@ -6,7 +6,7 @@ class Unoconv < Formula
   url "https://files.pythonhosted.org/packages/ab/40/b4cab1140087f3f07b2f6d7cb9ca1c14b9bdbb525d2d83a3b29c924fe9ae/unoconv-0.9.0.tar.gz"
   sha256 "308ebfd98e67d898834876348b27caf41470cd853fbe2681cc7dacd8fd5e6031"
   license "GPL-2.0"
-  revision 2
+  revision 3
   head "https://github.com/unoconv/unoconv.git", branch: "master"
 
   bottle do
@@ -18,7 +18,7 @@ class Unoconv < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ece3a0399c4f7ea93b1d04e9cb01915b615f680116ab372bc33a27c20d0346a"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     rewrite_shebang detected_python_shebang, "unoconv"
