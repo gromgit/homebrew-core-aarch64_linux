6ce9670e08677dae8dd6237bec566cab89c37d50
Branch Vincent <branchevincent@gmail.com>
2021-10-05 23:55:07 -0400

    codespell: migrate to python@3.10
    Closes #86558.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -6,6 +6,7 @@ class Codespell < Formula
   url "https://files.pythonhosted.org/packages/26/37/c524f1750635cb8806240013af1fd4147a60019f9a80e788759e3d2fb644/codespell-2.1.0.tar.gz"
   sha256 "19d3fe5644fef3425777e66f225a8c82d39059dcfe9edb3349a8a2cf48383ee5"
   license "GPL-2.0-only"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "4d9b687cd4c5a54e31afc47b6c4e33e06f8408fda2d8a0ccc64172b82f83d1fe"
@@ -15,7 +16,7 @@ class Codespell < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "000d9a448e8009aab61532ef00642afbef2baa61a70271f49793627bb9cfc5e3"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
