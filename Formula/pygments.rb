46d322f16cac3e69723ff7d3175616d5096dc968
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:35:48 -0400

    pygments: migrate to python@3.10
    Closes #86639.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -6,6 +6,7 @@ class Pygments < Formula
   url "https://files.pythonhosted.org/packages/b7/b3/5cba26637fe43500d4568d0ee7b7362de1fb29c0e158d50b4b69e9a40422/Pygments-2.10.0.tar.gz"
   sha256 "f398865f7eb6874156579fdf36bc840a03cab64d1cde9e93d68f46a425ec52c6"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/pygments/pygments.git", branch: "master"
 
   bottle do
@@ -16,7 +17,7 @@ class Pygments < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "eb7f8e59cb96f9ecef4e262dcc90f8e550b267f88bfeb47d7d68b54746e221c8"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     bash_completion.install "external/pygments.bashcomp" => "pygmentize"
