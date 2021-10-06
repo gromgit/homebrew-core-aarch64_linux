7a8b8a6ef649f5a9803e15e5f4fbe479ea11ba38
Porkepix <porkepix@gmail.com>
2021-10-06 13:52:16 +0200

    openvpn 2.5.4
    Closes #86706.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openvpn.rb
+++ Formula/openvpn.rb
@@ -1,9 +1,9 @@
 class Openvpn < Formula
   desc "SSL/TLS VPN implementing OSI layer 2 or 3 secure network extension"
   homepage "https://openvpn.net/community/"
-  url "https://swupdate.openvpn.org/community/releases/openvpn-2.5.3.tar.xz"
-  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.5.3.tar.xz"
-  sha256 "fb6a9943c603a1951ca13e9267653f8dd650c02f84bccd2b9d20f06a4c9c9a7e"
+  url "https://swupdate.openvpn.org/community/releases/openvpn-2.5.4.tar.xz"
+  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.5.4.tar.xz"
+  sha256 "56c0dcd27ab938c4ad07469c86eb8b7408ef64c3e68f98497db8c03f11792436"
   license "GPL-2.0-only" => { with: "openvpn-openssl-exception" }
 
   livecheck do
