8890f85a2853299c1cc86b89b3e4f4ca94479ac9
Branch Vincent <branchevincent@gmail.com>
2021-10-05 23:53:23 -0400

    atdtool: migrate to python@3.10
    Closes #86540.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -6,7 +6,7 @@ class Atdtool < Formula
   url "https://files.pythonhosted.org/packages/83/d1/55150f2dd9afda92e2f0dcb697d6f555f8b1f578f1df4d685371e8b81089/atdtool-1.3.3.tar.gz"
   sha256 "a83f50e7705c65e7ba5bc339f1a0624151bba9f7cdec7fb1460bb23e9a02dab9"
   license "BSD-3-Clause"
-  revision 4
+  revision 5
 
   bottle do
     rebuild 1
@@ -19,7 +19,7 @@ class Atdtool < Formula
 
   deprecate! date: "2020-11-18", because: :repo_archived
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
