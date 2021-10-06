a5f71ab11cfe23581cc94810e94a32c0f49a962a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 12:10:17 +0000

    jc: update 1.17.0_1 bottle.

--- Formula/jc.rb
+++ Formula/jc.rb
@@ -9,11 +9,11 @@ class Jc < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6c06509b2c7765b3a0025225089826cff9ff1a342e3f91cabbecdead2345249c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0ec313f4322578ff7c2c258e140c13c485e2477ca787c42076321d8e4fc73f36"
-    sha256 cellar: :any_skip_relocation, catalina:      "3a70be5ca313847e69f53888286f3be7654ba0f85b2c3f9e2f291a4ae4ac6379"
-    sha256 cellar: :any_skip_relocation, mojave:        "68b142be7ebcf8b056eb1af1e54fd58432ada594abcaf1ba1a47fb3035e23e3e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28ebb8a0c31f23e0e582d5952976369ef0428365ab429ea86ca5db599cd165e2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "950d2f985d3d11721a15c5b0e750f13d0400c02fb5a9cff97dbb8c3df68f3123"
+    sha256 cellar: :any_skip_relocation, big_sur:       "13b96430280226ce2c59a63dc62db45b44b5ebb117d60c00f02527affbfc04c5"
+    sha256 cellar: :any_skip_relocation, catalina:      "9db641ca35ab45421e6d5f00c27092375d973c2cfbd3104112cc719b718143d6"
+    sha256 cellar: :any_skip_relocation, mojave:        "7b98e590f9094c97985ca3670047f5580a8a0c78559103e05c30dc32c7ccf935"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a5d407532c31f8049262f849864d91b3f1194ca6eaced271d7f39d3cbfdea281"
   end
 
   depends_on "python@3.10"
