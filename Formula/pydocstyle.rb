a6d5e10f2c4b565aac7a98ee26fed243d65d5dc6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 12:08:02 +0000

    pydocstyle: update 6.1.1_1 bottle.

--- Formula/pydocstyle.rb
+++ Formula/pydocstyle.rb
@@ -10,10 +10,10 @@ class Pydocstyle < Formula
   head "https://github.com/PyCQA/pydocstyle.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8de054460c03ef2ae090e16de96d5555673f615a0cc66926c23b71cd67b1f2c6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "271f7fbfd202df294424111730f3fa7b29b9e07602a13b06239f4c9216635a45"
-    sha256 cellar: :any_skip_relocation, catalina:      "3658ffaa56d1bc1b0da49974e889e3b37e09a069fca1a3facc789d5293fb457d"
-    sha256 cellar: :any_skip_relocation, mojave:        "66244942f557879c00ed156a7f9bd70a70230ece72b6e22d5f0ef9800e4a07e5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f484ef39a1a165ec192fafe7f6d76c1ebf5fa97d7499d729cf1de98c6444edef"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6a454e42222a31a0437d0d33197b068b952c5a48011fe107f0d0f14f707e36f1"
+    sha256 cellar: :any_skip_relocation, catalina:      "a4091b17676b23f645aec6684939a19a2a5c05f991b25e9d4ebbf09c90764084"
+    sha256 cellar: :any_skip_relocation, mojave:        "84a68e5f7328a5a2127b50254e52f13a3f68f8bc718dd8ef84ceae59461a3bbd"
   end
 
   depends_on "python@3.10"
