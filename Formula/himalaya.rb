55fde80497c110399b10f99c1fe16a4370c2e515
Caleb Xu <calebcenter@live.com>
2021-10-06 13:08:10 -0400

    himalaya: fix build for Linux

--- Formula/himalaya.rb
+++ Formula/himalaya.rb
@@ -14,6 +14,11 @@ class Himalaya < Formula
 
   depends_on "rust" => :build
 
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
