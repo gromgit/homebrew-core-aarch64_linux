5dd5f68f6f87ed4d7e10a282785b3a59df33f2b1
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:38:25 -0400

    sshuttle: migrate to python@3.10
    Closes #86667.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sshuttle.rb
+++ Formula/sshuttle.rb
@@ -6,6 +6,7 @@ class Sshuttle < Formula
   url "https://files.pythonhosted.org/packages/e9/4b/51d6aaa900a6a13efb380b0a084a327c41aad28a267d4c1f074cb2e41baa/sshuttle-1.0.5.tar.gz"
   sha256 "fd8c691aac2cb80933aae7f94d9d9e271a820efc5c48e73408f1a90da426a1bd"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://github.com/sshuttle/sshuttle.git", branch: "master"
 
   bottle do
@@ -16,7 +17,7 @@ class Sshuttle < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc5f1f7e0cff2cfbe0b5f33c7014e3b85e4f24ed040db8f5dc32744edae707a5"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "psutil" do
     url "https://files.pythonhosted.org/packages/e1/b0/7276de53321c12981717490516b7e612364f2cb372ee8901bd4a66a000d7/psutil-5.8.0.tar.gz"
