4057c8813da4f0c76c2495e130b22844ffac6d55
Branch Vincent <branchevincent@gmail.com>
2021-10-05 23:56:25 -0400

    docutils: migrate to python@3.10
    Closes #86572.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -5,7 +5,7 @@ class Docutils < Formula
   homepage "https://docutils.sourceforge.io"
   url "https://downloads.sourceforge.net/project/docutils/docutils/0.17.1/docutils-0.17.1.tar.gz"
   sha256 "686577d2e4c32380bb50cbb22f575ed742d58168cee37e99117a854bcd88f125"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b4f3eca7d4566cbb4ba7ecd31a55c6baf8bf3b6d3ff2574d0b91149e627a3b29"
@@ -15,7 +15,7 @@ class Docutils < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0317c9472b48cf0ce34c2d72dce67f4122579fec2f6e98c8caaf8b6942eb95e4"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
