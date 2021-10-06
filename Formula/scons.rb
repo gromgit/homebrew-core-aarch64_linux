c865763ed332985121e5ac10f9389187df6bbce7
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:37:23 -0400

    scons: migrate to python@3.10
    Closes #86656.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -6,6 +6,7 @@ class Scons < Formula
   url "https://files.pythonhosted.org/packages/5e/f1/82e5d9c0621f116415526181610adf3f9b07ffca419620f4edfc41ef5237/SCons-4.2.0.tar.gz"
   sha256 "691893b63f38ad14295f5104661d55cb738ec6514421c6261323351c25432b0a"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "13c816bd6916bd07503eef1b9b12788a9eb09469f6c54b9fb1a8e58ab084a82a"
@@ -15,7 +16,7 @@ class Scons < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "87be85481d3a954f2e7ac8a838fe0032dc5d0ff88bbf9db2a6f3b9c004957732"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
