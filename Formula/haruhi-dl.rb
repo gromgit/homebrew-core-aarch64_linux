c8bf8f206dce4ef16787a273e38c0eb85eb1b608
Branch Vincent <branchevincent@gmail.com>
2021-10-06 01:37:25 -0400

    haruhi-dl: migrate to python@3.10
    Closes #86607.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/haruhi-dl.rb
+++ Formula/haruhi-dl.rb
@@ -6,6 +6,7 @@ class HaruhiDl < Formula
   url "https://files.pythonhosted.org/packages/24/f2/a2d22274cfa8f09c849495e8a5106cf72365091b58d55a45c2c91d9f79b9/haruhi_dl-2021.8.1.tar.gz"
   sha256 "069dc4a5f82f98861a291c7edd8bb1ca01eb74602dd36220343a75cb7bb617a8"
   license "LGPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "ea81fa790015d19ff63a57e553ca9914399becb582c22aa0afb6c689849eb81b"
@@ -14,7 +15,7 @@ class HaruhiDl < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "8a12ef4839c85aaa8a9a895555e4e784d52b18eb285c67ff22632218381f2417"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
