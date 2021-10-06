8c03f9a6583ad8bcf79afbf58188a7d02682a594
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 00:15:47 +0000

    smimesign: update 0.2.0 bottle.

--- Formula/smimesign.rb
+++ Formula/smimesign.rb
@@ -6,10 +6,10 @@ class Smimesign < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "aac8e5874ac87a67c2160119794b058d60a0f05e3a84879b414b8a581d8d7960"
-    sha256 cellar: :any_skip_relocation, big_sur:       "eee6928c9a0910092cac7e40f898ac786680e665dc20eab1e9e6643c8bbefc24"
-    sha256 cellar: :any_skip_relocation, catalina:      "b63db200f9b9303940cde9c64a588e27475fda08bb478f49451316c41d48d71f"
-    sha256 cellar: :any_skip_relocation, mojave:        "b3b5ac68fde1afbb262eaad0cd10d21f8b78212de226a064f763a51444fbe5ba"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2adc095ef7135d81dde128352c105f36affe27882900960e5ea658b1acd2427f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9781b5ecad25be5a9ef95fb714caedae7512af4d6a31be300b30c57fd17d1fb9"
+    sha256 cellar: :any_skip_relocation, catalina:      "4a8f0b0a87417c22175a7cfa7c25583a3c71170b220d3cbc56b05786baa3227d"
+    sha256 cellar: :any_skip_relocation, mojave:        "146db9c5113009eb23612bdf240d47a8f539619be6e7d87025c43c5cde8eca82"
   end
 
   depends_on "go" => :build
