c324c1c4f31d8f3aa87c9f92c0ae0d231aea2157
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 12:07:50 +0000

    streamlink: update 2.4.0_1 bottle.

--- Formula/streamlink.rb
+++ Formula/streamlink.rb
@@ -10,11 +10,11 @@ class Streamlink < Formula
   head "https://github.com/streamlink/streamlink.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8a363177dbc915c789c474834f3564ade4c0227741958e840ed3fb845e063af6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b31dd9fb5fdd4881269256544379060b065a73688451869d4111c72140d02323"
-    sha256 cellar: :any_skip_relocation, catalina:      "b35e07318cc3da02f5d3784273d5f2931d27be4d173761910d0ecb8a6de8bfc0"
-    sha256 cellar: :any_skip_relocation, mojave:        "8986287356842960772a48322a8bdf43abb7dd98e1ffe186a9637e0a3e3262fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ec7cca4a5cef9fec5c04a48e053e4235f8acbe49f895a75e8fec8d632921eee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b544c7e3424b069304e83ec79bbb81dc6d69f336945f8bfd146c78f0f3bc9587"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ea631111fd5875bde840b98af75ae75d2d50cd1e7c047a7067c5b01889e3e0e4"
+    sha256 cellar: :any_skip_relocation, catalina:      "5bf04b65e40c4f0541129a0d09320407ef5a2158af166b38f665048af1922e3d"
+    sha256 cellar: :any_skip_relocation, mojave:        "c18ec882a33aa2c3c169a0da29be3dad0350e5c9e5706b9d6435d2d45728cb21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02f39d9f315037a7a32a209ec1e31541820d7afa1f8f8cc18ce8aad41b0ec68c"
   end
 
   depends_on "python@3.10"
