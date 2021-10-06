478ba3e229fcb92659b017d24a2297bdae2de33e
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:39:50 -0400

    twoping: migrate to python@3.10
    Closes #86682.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/twoping.rb
+++ Formula/twoping.rb
@@ -4,6 +4,7 @@ class Twoping < Formula
   url "https://www.finnie.org/software/2ping/2ping-4.5.1.tar.gz"
   sha256 "b56beb1b7da1ab23faa6d28462bcab9785021011b3df004d5d3c8a97ed7d70d8"
   license "MPL-2.0"
+  revision 1
   head "https://github.com/rfinnie/2ping.git", branch: "main"
 
   bottle do
@@ -11,7 +12,7 @@ class Twoping < Formula
     sha256 cellar: :any_skip_relocation, all: "5da1355d4aa2d6c8729362449bd49b8a726e1daa7d908e68c410818f7a439dfe"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     pyver = Language::Python.major_minor_version "python3"
