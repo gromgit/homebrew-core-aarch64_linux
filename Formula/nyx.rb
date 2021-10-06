5a0932da6e2fa79929e32af110522a52921cd670
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 12:08:22 +0000

    nyx: update 2.1.0_3 bottle.

--- Formula/nyx.rb
+++ Formula/nyx.rb
@@ -8,11 +8,10 @@ class Nyx < Formula
   revision 3
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0588b4c6b3df71714a500b85096f5b83d2773cff86bec2d77ae11139117385ed"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a78e5ce784aeefb44ab69069294cf4aa306e8d9c3ed03ba6bf6883729094e396"
-    sha256 cellar: :any_skip_relocation, catalina:      "23791dab3ee8f90133cb743c9bdc2e16ac65ffe2346bba10c14f1a0bf8d553c3"
-    sha256 cellar: :any_skip_relocation, mojave:        "4a219bd35ad035daf11653bc1f98c245fcb380c674f218598e6aeedc3271c4b6"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "50bf36b910c98d16a0bdee31c76ec4ba1a0ef4b8f6ad1cad7ebdb035f05fd286"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bae78955aa83e7e742c86ead94067128d34ee67e46d3df228782bb14e3ff1933"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e3361830a3dca5a02c43318bac9b699b6716ccf4b75fd3a2106a4b48bd4915dc"
+    sha256 cellar: :any_skip_relocation, catalina:      "e3361830a3dca5a02c43318bac9b699b6716ccf4b75fd3a2106a4b48bd4915dc"
+    sha256 cellar: :any_skip_relocation, mojave:        "e3361830a3dca5a02c43318bac9b699b6716ccf4b75fd3a2106a4b48bd4915dc"
   end
 
   depends_on "python@3.10"
