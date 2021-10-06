855715accf45349673b740999dbb42475eeeeb9e
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:39:56 -0400

    txt2tags: migrate to python@3.10
    Closes #86683.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/txt2tags.rb
+++ Formula/txt2tags.rb
@@ -5,7 +5,7 @@ class Txt2tags < Formula
   homepage "https://txt2tags.org/"
   url "https://files.pythonhosted.org/packages/0e/80/dc4215b549ddbe1d1251bc4cd47ad6f4a65e1f9803815997817ff297d22e/txt2tags-3.7.tar.gz"
   sha256 "27969387206d12b4e4a0eb13d0d5dd957d71dbb932451b0dceeab5e3dbb6178a"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "0b2ab997932f742fb905ab546a1f12c417d12212f553ecc2be9ddbd49daa3a34"
@@ -16,7 +16,7 @@ class Txt2tags < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "f33d4937c6b718e193653753656567ff62ebfebfa421238355538830b89760f4"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
