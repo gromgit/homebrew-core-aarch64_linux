6f9dbee16b2dde4dd0a2663ba4e246e9d70bc922
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 12:07:54 +0000

    sqlparse: update 0.4.2_1 bottle.

--- Formula/sqlparse.rb
+++ Formula/sqlparse.rb
@@ -9,11 +9,11 @@ class Sqlparse < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1a984ac437bd907f5eac3e62965dd8cf78859965304d74cab2c9c240d9c7d17a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "361c451cebe6f78c03e55929ed30d1d98781025e65f5bad09d2f41d55d15a52d"
-    sha256 cellar: :any_skip_relocation, catalina:      "361c451cebe6f78c03e55929ed30d1d98781025e65f5bad09d2f41d55d15a52d"
-    sha256 cellar: :any_skip_relocation, mojave:        "361c451cebe6f78c03e55929ed30d1d98781025e65f5bad09d2f41d55d15a52d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "180b65725243b7b0746ce80b98d4e4d3522ed720e38f6f02d0c6f98a849e9a33"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5917e87d404c93aa04ce10b94b1d036e1b7302b32e118931d86799022f22f671"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, catalina:      "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, mojave:        "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76b4d21dec27b2311986f15e9b279a8ac0cefe6bc5d91a94733ded3746cf3c90"
   end
 
   depends_on "python@3.10"
