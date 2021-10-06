cc3e2b37894a96336f56d9d4302424c85adfe1c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 07:07:22 +0000

    googler: update 4.3.2_1 bottle.

--- Formula/googler.rb
+++ Formula/googler.rb
@@ -10,11 +10,7 @@ class Googler < Formula
   head "https://github.com/jarun/googler.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "34d74100c542d7674e393c1eeb0ddfc807d2e9e46c130777b16b85ed6a519551"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cdb78a7ecc2656018a8ecaedf15230a9bb4e58eb5a9f405b99da297f1423f58d"
-    sha256 cellar: :any_skip_relocation, catalina:      "27e27a67d1bca7dd2a05cc8dfb080c89c8871f92df951b95054068667902fa95"
-    sha256 cellar: :any_skip_relocation, mojave:        "49f64f0b333b9f65e18d6bc0ba5171e8e52a3df809f8e568207f343cd6823029"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "198a84d6d6de97d35b45f726315cd8aaae307347539b42f793e0647b7d668acb"
+    sha256 cellar: :any_skip_relocation, all: "18cddbd924b8544fa84dce44221443c08053e1939f4b638538e0da04a696df03"
   end
 
   depends_on "python@3.10"
