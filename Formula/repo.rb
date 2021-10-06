ad6680121417547ca13f03094501b05461b7db3f
Branch Vincent <branchevincent@gmail.com>
2021-10-06 03:36:52 -0400

    repo: migrate to python@3.10
    Closes #86650.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -7,13 +7,14 @@ class Repo < Formula
       tag:      "v2.16.8",
       revision: "0ec2029833ffa85400b729dc3b7039661eb42619"
   license "Apache-2.0"
+  revision 1
   version_scheme 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "83e0d9547697d414580aec9fc2df6cac89d8699d36c73d86cdf9490bb7675e70"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     bin.install "repo"
