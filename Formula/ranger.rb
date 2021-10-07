43139863dc7e9ce90f7f18a919822fe0cb1e72dc
laggardkernel <laggardkernel@gmail.com>
2021-10-07 22:03:08 +0800

    ranger: add missing command 'rifle'
    Define an entry for command 'rifle'.
    
    Running the `rifle.py` in `#{libexec}/ranger/ext/rifle.py` doesn't work.
    Cause `ranger` is not really installed into the brewed python, but
    running as a python file by it. To make sure pkg `ranger` being found
    in `sys.path`. We must define a helper under "#{libexec}", which is
    the working directory and pkg source `ranger/` is stored there.
    
    Closes #86801.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ranger.rb
+++ Formula/ranger.rb
@@ -6,7 +6,7 @@ class Ranger < Formula
   url "https://ranger.github.io/ranger-1.9.3.tar.gz"
   sha256 "ce088a04c91c25263a9675dc5c43514b7ec1b38c8ea43d9a9d00923ff6cdd251"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
   head "https://github.com/ranger/ranger.git", branch: "master"
 
   bottle do
@@ -24,6 +24,18 @@ class Ranger < Formula
     libexec.install "ranger.py", "ranger"
     rewrite_shebang detected_python_shebang, libexec/"ranger.py"
     bin.install_symlink libexec/"ranger.py" => "ranger"
+
+    (buildpath/"rifle.py").write <<~EOS
+      #!/usr/bin/python -O
+      import sys
+      from ranger.ext import rifle
+      sys.exit(rifle.main())
+    EOS
+    chmod 0700, buildpath/"rifle.py"
+    libexec.install "rifle.py"
+    rewrite_shebang detected_python_shebang, libexec/"rifle.py"
+    bin.install_symlink libexec/"rifle.py" => "rifle"
+
     doc.install "examples"
   end
 
