8eee45dde2594b604d4da25ac29f6284d42cb984
rui <rui@chenrui.dev>
2021-10-07 05:45:51 +0000

    fselect: update 0.7.7 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -6,11 +6,11 @@ class Fselect < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "57571c72929bf97d6992c7c8f7826d6ed1574bc29ace961429ff563524ef8452"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a662d57fb34649294ece8d5e75332f73bce6394bf139d31167c4a2a83b3d59e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "3a0154264d20257b7376d49be91a9d03f24624fa1e57281cbb79640038e9b1ae"
-    sha256 cellar: :any_skip_relocation, mojave:        "16259c0ce60aeba0646452b3f7081a1ad9a7233bf30b3981da24624b781b9790"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ffdc104dcdd3060fdc227a132be59dc9f7cb9e28ddb3d4e9bfea561fdb85e030"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d8180a55468735611fa9bee46807e48e9edac0be08dd4151c264fad845bcfa0a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "59263d1707e8939cd18ded9e607895abf7e8a258181ed7612328670bb0f8c60e"
+    sha256 cellar: :any_skip_relocation, catalina:      "0e56d75cc6ca7214644a908565b7ae1acd48cb3bf53011813f64073b4b05b1f9"
+    sha256 cellar: :any_skip_relocation, mojave:        "62f42c61ba58c8838819704e1e9de8dff131e85feb8858b483129642913498db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f215a43c84cb0e94250b4abe4ec2af993c0b633c65198121bb0382ab5e57d2f8"
   end
 
   depends_on "rust" => :build
