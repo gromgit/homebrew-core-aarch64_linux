e64154f7f508da74d5b33cf9140ba31ad86a7785
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-07 02:19:39 +0000

    angle-grinder: update 0.18.0 bottle.

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -6,11 +6,11 @@ class AngleGrinder < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "782939f8ddc8b9c0c15e0a45b0f36d546a6771a27f6273cfb7fbbe580d6e8149"
-    sha256 cellar: :any_skip_relocation, big_sur:       "74c4ae3251570186fe78ad4f55355e006a268d9eeab81405b61cd719aa931316"
-    sha256 cellar: :any_skip_relocation, catalina:      "fd9b197c1537ef416c48ff759fb7889f4061aaca877b40f1b424c872e63e4dc8"
-    sha256 cellar: :any_skip_relocation, mojave:        "29b8a96121053b1b604a21f93819c4ee9e6bfeafba7de24b7798d2367ab69b30"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10e7831dee92f88d81165b878011b62c50c7cbab1d8a8f5e79cf389cfe6bec10"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "116d320e843719451ec991b97c2bb8c1aa2dea3623f387c866f12a718b512993"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d94050232f3ce01eb97c1c36f79aa5d2734b9e4e25ede7d81e32eaaeca4a4db7"
+    sha256 cellar: :any_skip_relocation, catalina:      "f41ebbb8078940ecc259ffac8451f70dc49df53a8d8f1fcafe4b02bc6723dcbc"
+    sha256 cellar: :any_skip_relocation, mojave:        "73c19bc8e8e2697797e46ba6e6dd8a24deaa77a8059f0546bf29bffba5e0c8ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afe0edc8a8c223a9bfdbd0c515701e5103a4b4b9b168da747d991eb487dec25a"
   end
 
   depends_on "rust" => :build
