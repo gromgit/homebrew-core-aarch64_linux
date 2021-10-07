e4c3eff8983dbd09d5245a88d22e25312fc8b56f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-07 15:12:30 +0000

    wireshark: update 3.4.9 bottle.

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -13,11 +13,11 @@ class Wireshark < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5444a0d45239444a147c992dfdd8ded45f55872e1763dc64b86d530db13f00eb"
-    sha256 big_sur:       "5e6757e853f43b3e70dedbd1ada0f68f3dbdebfeb5b43bd5b23f4c4ec611d9c6"
-    sha256 catalina:      "88205408edddb908c29462f40be51b5b5a88112f403da28885b048ea2d84b964"
-    sha256 mojave:        "c716a62b1e2cfb8cd86eabc8a8297d66768aca0117da298ac2a27dfecbc7683c"
-    sha256 x86_64_linux:  "1389563805ee8511e4593363525d0e0ee14e24c0856e58a5911e5fc3fc8c75e4"
+    sha256 arm64_big_sur: "1ea892a48ea622868e01f59f5c31862e80785d4d1c276fedc17465512dce9dfd"
+    sha256 big_sur:       "6885514683558139b01f9cb8820cf42d1474fb0b72b381996327ae64d7cb694c"
+    sha256 catalina:      "c9e2893065d4b33debbb40453093c72781c6835a3df502713786aa9de69136cd"
+    sha256 mojave:        "b0acdf1efbb775f4e9de3057ed00dcc73c438c532099d3b43e1dba20517fe0e4"
+    sha256 x86_64_linux:  "518874dd9f83f422b433fd8211387db4d1385e6ce890cc4024aa651b4e1e0ee7"
   end
 
   depends_on "cmake" => :build
