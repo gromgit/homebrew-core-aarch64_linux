981f7072fd00d623f2d9eef4093d35bf759c23e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-08 09:08:56 +0000

    skaffold: update 1.33.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -8,11 +8,11 @@ class Skaffold < Formula
   head "https://github.com/GoogleContainerTools/skaffold.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f40b1246c5c65e4590525e8e6b43b307fd96290ce809b7c0b8ee7c6c8eea1da2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "472c853214015ca234f9142f8fe3718b142f099867ed3fe0e82532eb2d16f778"
-    sha256 cellar: :any_skip_relocation, catalina:      "29cbefa29462dbd923d4945669ce2843947d3a91ebe41d2c431efb1be9e439e8"
-    sha256 cellar: :any_skip_relocation, mojave:        "3f6a6d0eedf93ddaa16af6bb574a680da130d2e43a376683408487d16b7eeefe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d92c3d79b913ed251df058cf789826573b95e5c384ffda6970ae324de6d159c0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2f2455f7f099dae1bf83a163b7e856f257b26a74d5c2b13044b082c702161288"
+    sha256 cellar: :any_skip_relocation, big_sur:       "1d9b4729d15d2fab7c71c7849cf08ec7e4ff0ad3a731a89abff659b217ae271f"
+    sha256 cellar: :any_skip_relocation, catalina:      "73fb0cceba81f9a6a0661e3212f4612eec246314c295d72da7b50a3f09fafec3"
+    sha256 cellar: :any_skip_relocation, mojave:        "bed6d3a0c299e93436cc8aaa93da15484e76a1514bb555fe9eab46ebf51af604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "123d9e087ce93d4f6287ad33684d70e9bfa6d0ceecab189d49bcfa4ff9ec3376"
   end
 
   depends_on "go" => :build
