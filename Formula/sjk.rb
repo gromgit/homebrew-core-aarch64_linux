79179396aca5a70ccd34c44f5b9623fcf96688b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-08 18:07:40 +0000

    sjk: update 0.20 bottle.

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -6,7 +6,11 @@ class Sjk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2bc1c9dadeff20643d36e42a6f018a80c6df288fa5e3bbaf557b611007142b14"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, big_sur:       "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, catalina:      "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, mojave:        "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51d29b3fd8b210d7dbd7d881ab0dda3aa40b99c99362f5ed3aa8c2a1de5b7d54"
   end
 
   depends_on "openjdk"
