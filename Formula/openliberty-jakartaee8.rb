6472434042c8c7b7103818a66ce688b99a8dada4
MoonFruit <dkmoonfruit@gmail.com>
2021-10-08 11:32:21 +0800

    openliberty-jakartaee8 21.0.0.10
    Closes #86846.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openliberty-jakartaee8.rb
+++ Formula/openliberty-jakartaee8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyJakartaee8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-08-24_2341/openliberty-javaee8-21.0.0.9.zip"
-  sha256 "a8b47fa7a195908120f8c48a8cebbd80222f61692d1cb1dc704b143dab5dc690"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2021-09-20_1900/openliberty-javaee8-21.0.0.10.zip"
+  sha256 "caedb8cc99759e86fe70f2e0cfa32ca25ec5d098350241a02101de747e5a61a1"
   license "EPL-1.0"
 
   livecheck do
