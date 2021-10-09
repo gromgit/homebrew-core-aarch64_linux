8a95e302fc279e672c0f316a15e2e7fe9e1de2a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-09 13:13:23 +0000

    log4cxx: update 0.12.1 bottle.

--- Formula/log4cxx.rb
+++ Formula/log4cxx.rb
@@ -7,11 +7,11 @@ class Log4cxx < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "5654067019235ef0ea4d7d2dda99116a5f59eb9de503d02c2831c1f54e971d88"
-    sha256 cellar: :any,                 big_sur:       "c7c19a708049c810cea3514308e52316d25c02f8ffc1fd2eb3f80485d34bb916"
-    sha256 cellar: :any,                 catalina:      "6cfbf907bb67c4ffb62c71e91343d894e0ead3534856933801df942f60ffc3a5"
-    sha256 cellar: :any,                 mojave:        "1c56033e73bf61b3c5742d7b9f64f65c3d2e4223edbb30a3fef45287f3efe883"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb8304107efd616f1beef853a933c31dd0839321266f9b2dda1ebdb632304f8f"
+    sha256 cellar: :any,                 arm64_big_sur: "56c60d9be7f933a782852a55dd1feb6975a0063a73b14670d27a6c375ece7e49"
+    sha256 cellar: :any,                 big_sur:       "6d1b6f87ea1ffcc38069798e5150ad6523e7d64d2682b1c4afc2e4bd1c5e9294"
+    sha256 cellar: :any,                 catalina:      "8f33426b24d1d711a72ce4ee1328a5c73607d5f1755c8055361aa9f74dbbebae"
+    sha256 cellar: :any,                 mojave:        "14367506f0070f9142b47ca2de95101440ff0b9a6a1ea00dc6095d338c0f2b04"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e640dce6a616b1e0f083069b51f77da795916fde0e17da4a9d1a5a16bbb652df"
   end
 
   depends_on "cmake" => :build
