fd7e970016bd34409019b1c74990bdf6ab84a669
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-09 13:13:35 +0000

    libvpx: update 1.11.0 bottle.

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -7,12 +7,11 @@ class Libvpx < Formula
   head "https://chromium.googlesource.com/webm/libvpx.git"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "23d8b0c0b6acfba7df69ac222251797efd5db0d5a143494f90ad52d5ff6f4083"
-    sha256 cellar: :any,                 big_sur:       "a5c097e3c902b9c1bdfaf6c1100e01366d6537b1983f0bc4075e3a642c8ffab9"
-    sha256 cellar: :any,                 catalina:      "ee2c55fcd7d7143a74171c42d227e63c383f46bfe78d2d8fac4dc45cc6a1e8e0"
-    sha256 cellar: :any,                 mojave:        "3d578a1b4e7a4cdb73c07b3529b9f90c4cfa6583386035400be5cf85448e625d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8597613378a9e3e33d79dc63db9029edf7376780152eaf55e9ed35581670f2e6"
+    sha256 cellar: :any,                 arm64_big_sur: "2edfb6a133be8947da27719975dbd19dd6233e12f21d221bea7f6008cfbc51a2"
+    sha256 cellar: :any,                 big_sur:       "73d6365843bd6b8c868b3bf020152225d06304117c23c2a1e579ce347d3ab4e1"
+    sha256 cellar: :any,                 catalina:      "28194ea0a917dcfecbfdcd51cb37da4ae1697238e3c2dedc769c502b435124c7"
+    sha256 cellar: :any,                 mojave:        "378b9f6680ea8a99c064d8acd0cfbfbf2a1c142d8c4f27cf935dcfed3342cc61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e79ec2f50112da2b5adfc83b46604af182af8364410c9ec686c6c519c133229d"
   end
 
   depends_on "yasm" => :build
