5c124840e07b18d8b65fbdfa1231b2c4438c765c
Michael Cho <cho-m@tuta.io>
2021-10-08 23:29:33 -0700

    akamai: update url with renamed tag
    Closes #86915.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -1,7 +1,7 @@
 class Akamai < Formula
   desc "CLI toolkit for working with Akamai's APIs"
   homepage "https://github.com/akamai/cli"
-  url "https://github.com/akamai/cli/archive/refs/tags/v1.3.0.tar.gz"
+  url "https://github.com/akamai/cli/archive/1.3.0.tar.gz"
   sha256 "338d4be3e5878d52c764bb2e2e5faf5ecaf0510a37c582b3644615df39718141"
   license "Apache-2.0"
 
