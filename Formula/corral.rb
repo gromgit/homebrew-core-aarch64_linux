af1df7afe87370a4a01805bd284a96bdc12d3209
Rui Chen <rui@chenrui.dev>
2021-10-09 17:31:22 -0400

    corral 0.5.4
    Closes #86942.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -1,8 +1,8 @@
 class Corral < Formula
   desc "Dependency manager for the Pony language"
   homepage "https://github.com/ponylang/corral"
-  url "https://github.com/ponylang/corral/archive/0.5.3.tar.gz"
-  sha256 "caee35ca820201c13b87e11224ede472d1ed9798985d17eee8c46b14711f7d07"
+  url "https://github.com/ponylang/corral/archive/0.5.4.tar.gz"
+  sha256 "ca2fff8568fd14404d25de402dec2cae16cad39edb7937f829f88a23a66da2d1"
   license "BSD-2-Clause"
   head "https://github.com/ponylang/corral.git", branch: "main"
 
