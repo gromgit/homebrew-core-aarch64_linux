e01c3e852029a7cde0e21dfbc7403f971ca04fcb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-09 19:06:47 +0000

    pywhat: update 4.3.0 bottle.

--- Formula/pywhat.rb
+++ Formula/pywhat.rb
@@ -9,11 +9,11 @@ class Pywhat < Formula
   head "https://github.com/bee-san/pyWhat.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7b7006c6e82e74250a098435e9902ff93895d941a834538c41d82b2a486a689e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6a344106236e95e813de7962703b59d247da1f5237fed1313d53a88d068b755d"
-    sha256 cellar: :any_skip_relocation, catalina:      "45183033f9f7e0eec15e60106be30048e77f8d0552b2094593dc263d938ff8cc"
-    sha256 cellar: :any_skip_relocation, mojave:        "d68c6af2966de7845bad2a1acb567badd625b206c1bb52956afe6837e58bb8bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "610f1001e81c862d708b9a7cb0ba8cb34e269af1f623d0cd5804cd9b2217ede4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c8b90b0ea0a7d0dde5d6b83a75fab5cce35541069ee1fee59237c1d2cb0a6fbc"
+    sha256 cellar: :any_skip_relocation, big_sur:       "61590ef5f88101a49a0ad8949c8802859c0c039e916360c8bd0276f7304dd3e8"
+    sha256 cellar: :any_skip_relocation, catalina:      "8301e11f7a275af316ecc87bdc40fc498f9c1322a9fe3a5544e515bf74d741a5"
+    sha256 cellar: :any_skip_relocation, mojave:        "c5d70a060f16418e613fd763b8d28b22c87a3e503b37429b6099c7a773a62736"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78e9d48549f7cd9a01c35f7583b49e1ba94dc55b2945c48c465275dbde8b91c5"
   end
 
   depends_on "python@3.9"
