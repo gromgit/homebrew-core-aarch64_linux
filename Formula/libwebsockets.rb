ebd83117b03a7ddc0423810ea1ddc6c05f163d66
Rui Chen <rui@chenrui.dev>
2021-10-09 18:13:03 -0400

    libwebsockets 4.3.0
    * libwebsockets 4.3.0
    * fix tag
    
    Co-authored-by: rui <rui@chenrui.dev>

--- Formula/libwebsockets.rb
+++ Formula/libwebsockets.rb
@@ -2,8 +2,8 @@ class Libwebsockets < Formula
   desc "C websockets server library"
   homepage "https://libwebsockets.org"
   url "https://github.com/warmcat/libwebsockets.git",
-      tag:      "v4.2.2",
-      revision: "8d605f0649ed1ab6d27a443c7688598ea21fdb75"
+      tag:      "v4.3.0",
+      revision: "a5aae049b2a386712e1be3b417915c0d44c7e675"
   license "MIT"
   head "https://github.com/warmcat/libwebsockets.git", branch: "main"
 
