f1e6c41d89cbbf0254dd5941b433d2ab86d5670b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-10 20:53:29 +0800

    i2c-tools: migrate to python 3.10
    Also, fix test style and remove some unneeded versioned Python
    references.
    
    Closes #86889.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i2c-tools.rb
+++ Formula/i2c-tools.rb
@@ -4,6 +4,7 @@ class I2cTools < Formula
   url "https://mirrors.edge.kernel.org/pub/software/utils/i2c-tools/i2c-tools-4.3.tar.xz"
   sha256 "1f899e43603184fac32f34d72498fc737952dbc9c97a8dd9467fadfdf4600cf9"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 1
 
   livecheck do
     url "https://mirrors.edge.kernel.org/pub/software/utils/i2c-tools/"
@@ -14,19 +15,19 @@ class I2cTools < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "59e657b8e5a828460b17193ea440bef1229d9e74d73d193a66045526b738e95c"
   end
 
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.10" => [:build, :test]
   depends_on :linux
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "EXTRA=eeprog"
     cd "py-smbus" do
-      system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+      system "python3", *Language::Python.setup_install_args(prefix)
     end
   end
 
   test do
-    system Formula["python@3.9"].opt_bin/"python3", "-c", "import smbus"
-    assert_equal "", shell_output("#{sbin}/i2cdetect -l 2>&1").strip
+    system Formula["python@3.10"].opt_bin/"python3", "-c", "import smbus"
+    assert_empty shell_output("#{sbin}/i2cdetect -l 2>&1").strip
     assert_match "/dev/i2c/0': No such file or directory", shell_output("#{sbin}/i2cget -y 0 0x08 2>&1", 1)
     assert_match "No EEPROM found", shell_output("#{bin}/decode-dimms 2>&1")
   end
