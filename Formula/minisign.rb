e72796ed96b4c3977a853ee62d1053411565357b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 01:30:44 +0000

    minisign: update 0.10 bottle.

--- Formula/minisign.rb
+++ Formula/minisign.rb
@@ -6,12 +6,11 @@ class Minisign < Formula
   license "ISC"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "5e915e427db3117320aff459d67438ac7bf94cf3e37291ca02a1d6b83f2fa827"
-    sha256 cellar: :any,                 big_sur:       "9c86c4043d4ebadd47e6ba606b80dcf0611f302dd11d6f8f56b9c4c1f1d3bfe9"
-    sha256 cellar: :any,                 catalina:      "42a044324786df52bcb3334eb3c07b3c3ac65414af72c17c73ccb0fb081507ab"
-    sha256 cellar: :any,                 mojave:        "fc5d9762d710f500978ac0d09b0fbf6fcf3745c4b018313fe9d3ae5679e1f37e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "233098345606342ce2d4028c0f7814b211e84dc6952a4db4cef1ee257caae892"
+    sha256 cellar: :any,                 arm64_big_sur: "50bdf209ebcc5e9079223dc1b47c488423655d154eca277a25269aa8d338dc2a"
+    sha256 cellar: :any,                 big_sur:       "73272eb26875a7b1c219279f91c1e97c24f5343bc28698e0adf190cdfbb611f5"
+    sha256 cellar: :any,                 catalina:      "6ef7ba174b48584911276e6491dea599b5e84f3747d2f25753c7b36b55ce3b3c"
+    sha256 cellar: :any,                 mojave:        "9340a13b895807ec4dbfced552ec3a612a5dbcc88b64b363aef876ec57a4ae4a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "932f5e7be0cb17788c687f8c474363a2acdbfb1b33041ba8c7d9781dde1d2353"
   end
 
   depends_on "cmake" => :build
