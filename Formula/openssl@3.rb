51244380d2f3330133000e8d416e142db48e82dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 16:06:48 +0000

    openssl@3: update 3.0.0_1 bottle.

--- Formula/openssl@3.rb
+++ Formula/openssl@3.rb
@@ -13,11 +13,11 @@ class OpensslAT3 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "f676e5bd999369a98b2e89e4a0fffeac8b5961ac4eba3ad45ef3c5c5e07aa217"
-    sha256 big_sur:       "f279ab1fb5880c36c8b5b7557ab44fd8c6e4b33ed03716bed4d3d69b96097a74"
-    sha256 catalina:      "ec819e57038f4ae11e3a3e083ec1d37c174e44fce08830bda22e7f6d77f063df"
-    sha256 mojave:        "2d6c5b6a1d828d26c4514a557496c64bd445126ea6c10bf81a5660c7690405f6"
-    sha256 x86_64_linux:  "e1ef778855bf72986f5b6acc08afc3e29735a73541cbb92e917cf38431904174"
+    sha256 arm64_big_sur: "f62a52f5321f601e49411a9b80c079217cb28e606360a559da448e2673085d44"
+    sha256 big_sur:       "b5a51da4059ef651824d284210f8c6bae62c58d53bedff020f51c24384b6a23a"
+    sha256 catalina:      "c6ccff3db9265ab98f190357d8d324ca88a4126e47f5cfebea876a712a1adfba"
+    sha256 mojave:        "60eea9f4dd2dc264490e18980ba1cd881877bd194e2cda3564e9fe56e6bdadf4"
+    sha256 x86_64_linux:  "6bdb460fc9a6d1b5436a21538866ce56c67956d8647c0efcda10e136dc8f77b9"
   end
 
   keg_only :shadowed_by_macos, "macOS provides LibreSSL"
