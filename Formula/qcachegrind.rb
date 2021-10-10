850d15f4686f45a88f6d2a82cbd0da679fb5000f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 02:18:41 +0000

    qcachegrind: update 21.08.2 bottle.

--- Formula/qcachegrind.rb
+++ Formula/qcachegrind.rb
@@ -13,11 +13,11 @@ class Qcachegrind < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "e463fbfe5a4ef00611f34c0a0e049f96df312a1eddc779a0834741f23da1efdb"
-    sha256 cellar: :any,                 big_sur:       "c97be1624d3b85d6d2b7b947d28571459026d0aa222224e438d4b6a730d2016b"
-    sha256 cellar: :any,                 catalina:      "8988bd786e8d2d4c0ac589fd747ccb388ae343ddb262742c2231f94c88c39943"
-    sha256 cellar: :any,                 mojave:        "996472038ca71170dd567f04a37fc44d1c08dfda365f3ea986e057dda1d4c00c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "459804a72f566b4ae5ed724416c741dc301af09029bda5a5e4647c7ee6092327"
+    sha256 cellar: :any,                 arm64_big_sur: "56367beb5e041fc7c212223572414a35913b61cab87cd4e40948f8c7a26b6631"
+    sha256 cellar: :any,                 big_sur:       "b8b09416ce6dd7829da4c04044ed84dade46f3b652ff10bb9c1d2fab80a08691"
+    sha256 cellar: :any,                 catalina:      "d6d92d76cb35a33f305e67810f5f6443c5cf1d56eab7e9de11d4f0df345141a2"
+    sha256 cellar: :any,                 mojave:        "925fbdcc28c0b4f200414e3d957450619f9e1855ed87f60d2f56cd71fcded8d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5015186bd3630d3dad1eca45438ceef8a7068327fa2405a8020a315f506916e0"
   end
 
   depends_on "graphviz"
