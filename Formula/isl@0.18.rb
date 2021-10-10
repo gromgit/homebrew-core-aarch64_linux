07467b34377d285e5c93a27247b649760c227b63
Sam Ford <1584702+samford@users.noreply.github.com>
2021-10-10 13:58:27 -0400

    isl@0.18: update urls
    Closes #87052.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/isl@0.18.rb
+++ Formula/isl@0.18.rb
@@ -1,13 +1,13 @@
 class IslAT018 < Formula
   desc "Integer Set Library for the polyhedral model"
-  homepage "http://isl.gforge.inria.fr"
+  homepage "https://libisl.sourceforge.io/"
   # NOTE: Always use tarball instead of git tag for stable version.
   #
   # Currently isl detects its version using source code directory name
   # and update isl_version() function accordingly.  All other names will
   # result in isl_version() function returning "UNKNOWN" and hence break
   # package detection.
-  url "http://isl.gforge.inria.fr/isl-0.18.tar.xz"
+  url "https://libisl.sourceforge.io/isl-0.18.tar.xz"
   mirror "https://deb.debian.org/debian/pool/main/i/isl/isl_0.18.orig.tar.xz"
   sha256 "0f35051cc030b87c673ac1f187de40e386a1482a0cfdf2c552dd6031b307ddc4"
   license "MIT"
@@ -33,7 +33,7 @@ class IslAT018 < Formula
                           "--prefix=#{prefix}",
                           "--with-gmp=system",
                           "--with-gmp-prefix=#{Formula["gmp"].opt_prefix}"
-    system "make", "check"
+    system "make"
     system "make", "install"
     (share/"gdb/auto-load").install Dir["#{lib}/*-gdb.py"]
   end
