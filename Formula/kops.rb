5480de7db94f6ebf01bad59e6afeb4afde2572cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 01:31:41 +0000

    kops: update 1.21.2 bottle.

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -12,11 +12,11 @@ class Kops < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ab18ed01fa59ff645ea08f83b36ac0e5fcdb5a59a8ccea1d845d674193d8f1d2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "df0f4405b78b6b34277d6543d0e86aa47af07c5b0b58418a3388577c9d5b280e"
-    sha256 cellar: :any_skip_relocation, catalina:      "4282f738df8fc2bfcc92c4c273063372d92a1060e47896fb9141dea6c2dd580d"
-    sha256 cellar: :any_skip_relocation, mojave:        "01a39ba6440881bb7513b6b332f8f89f2861f3ff252e460c00c596b24571a2d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1642575ac0a6191f5bc75c7580729bc971e4f77a20db16680e62487e1c6755f6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a6f75f344b5bfa38ff54125b3e46a16aca526d70ecb81858be94b90706582208"
+    sha256 cellar: :any_skip_relocation, big_sur:       "dcab23a9556ff0de9ec455854d11ec14fe5f888d2cb9d8ceeb4a75a12ddc4281"
+    sha256 cellar: :any_skip_relocation, catalina:      "3dd1c755bd7384305ba0fddc1fad921bb418d209149961bc4b891d00c8570e86"
+    sha256 cellar: :any_skip_relocation, mojave:        "033edc03203c95141063b284dd757b15e13971ef482249b7bba702d34428fc8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "071abf3a8ea3ebc1840fefa5ebc3c373aacb23931c38010672b5914345ac5ba7"
   end
 
   depends_on "go" => :build
