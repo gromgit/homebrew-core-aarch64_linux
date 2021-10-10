9c4f260cd866fb376d624aba1b2a4d0a090965fa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-10 12:58:38 +0000

    go: update 1.17.2 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -13,11 +13,11 @@ class Go < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "d0108e9df0041587d852d63e5811e4b0a4ae498309cf7f9dd95463ddaa581d48"
-    sha256 big_sur:       "d1db2f29f84397225afccd674eef9b6c9e5a591a9372aab195b5c463d8ad6421"
-    sha256 catalina:      "15f1890939c34f3eeb841c09c6b74c23c43450337d50a3f0963493a3a861b7f9"
-    sha256 mojave:        "0299a79c3c05259256f5b4f10e532a3b2057423a93024f87ac99a174ec2e9d9f"
-    sha256 x86_64_linux:  "65e57b46322ebb9957754293cc66012579d93a7795b286bd2f267758f8006d7b"
+    sha256 arm64_big_sur: "b0a1686c5c5ba668e78a97d66567e9d007f7d2a0c9b1a53e79841e3736d729e6"
+    sha256 big_sur:       "01f50ff6dbea579b3b3e792751ec14ba90de1ac5fe09db03adf438558444ddfa"
+    sha256 catalina:      "cc6b0bff088b30b80afad3be3519af192e515d83c8928262114ca07e9bf34bd8"
+    sha256 mojave:        "a98bd1c5f1cae4907712126d9ea040680084e5fb9743f1d4160744fe3fe8861a"
+    sha256 x86_64_linux:  "aa5fbcd2c12be4e08d5edbcbb5eb8a8859c48ecd036c697d2d83b62d410dbd8c"
   end
 
   # Don't update this unless this version cannot bootstrap the new version.
