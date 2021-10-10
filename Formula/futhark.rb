c5a12d5c0baaf579d54514cf16dd20793c5abbed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 04:11:02 +0000

    futhark: update 0.20.4 bottle.

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -7,11 +7,11 @@ class Futhark < Formula
   head "https://github.com/diku-dk/futhark.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "45b9520e676f20f8048d7b7ce812db45ad2895625b70e6d4ae70b69a372a2104"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ad72e44ea0fc9db137ebe8ad1db5eec6d2a1dee8696b53c84d25fa918531b04b"
-    sha256 cellar: :any_skip_relocation, catalina:      "297d35208978002ceee4f0ddde9ee74b985c9b58165a18ba34f34ce6d65aa9af"
-    sha256 cellar: :any_skip_relocation, mojave:        "ada26ecee2c0deb32791d04226a52c8025e0e21d61878c163500b7f2fd3b90c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d6e8594e03271cb0eb9e16ed04dee29cac345f14c92ab7f9efc95bcf81a2e2b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "217e1f392f74ae06af2912fb864c73ab0e3c5632a8fc9511f10437c4ae1efaeb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0a1e263e927643bd26d69831c8f5590186c581e921204d480e2fed29145d52b1"
+    sha256 cellar: :any_skip_relocation, catalina:      "b6fad661d6e5d8296128b256cc4fa5031584ff21f0827b318e23de5b2a83616a"
+    sha256 cellar: :any_skip_relocation, mojave:        "97c2af42b567929c2e5a982f4bca6cb4509abd049561ef9aac067b7dc4b215d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0655ac5c57a56c3f66efa05727d97290f40c842a1db7e22e03b56ff67245a1d2"
   end
 
   depends_on "cabal-install" => :build
