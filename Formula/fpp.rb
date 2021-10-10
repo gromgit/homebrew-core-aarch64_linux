df2ec5a643d12e1411dca745aa4b223217ebc7f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 17:08:40 +0000

    fpp: update 0.9.2_3 bottle.

--- Formula/fpp.rb
+++ Formula/fpp.rb
@@ -8,7 +8,11 @@ class Fpp < Formula
   head "https://github.com/facebook/pathpicker.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2a149cfef3d217abcd20e567872aa5930c47b959b1e9647ec7bb48c2041e5a07"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
+    sha256 cellar: :any_skip_relocation, catalina:      "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
+    sha256 cellar: :any_skip_relocation, mojave:        "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f51509aa7ff591a83d65bf092b43f19ecb446c6dd08d55f33f52725b4618fe52"
   end
 
   depends_on "python@3.10"
