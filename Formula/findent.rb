1300edd6b8ee1eeb566a3bd5e72c3f0e78b86a12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 04:11:46 +0000

    findent: update 4.1.2 bottle.

--- Formula/findent.rb
+++ Formula/findent.rb
@@ -11,10 +11,10 @@ class Findent < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "4dfb18b69c58a7fa48d54072829f63f1588a43e0e95d5fc970fd9a37397a2d92"
-    sha256 cellar: :any_skip_relocation, catalina:     "0baa0d330bd9c5bf4ce35263736819541f31a8af1d1289472bf0e3a364a20446"
-    sha256 cellar: :any_skip_relocation, mojave:       "d8e2ff05dfc1f72761614710a28ddbb93bbcb550a79452eb0aed35bd9e2000ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ab21e74bb78bd44418ad39b5a630626ece59e07a9eaf5fcbb069e93661699b87"
+    sha256 cellar: :any_skip_relocation, big_sur:      "0ee5a1228898bc222f04076546701d067c67c02ae2c22b543838cd10d4b677c7"
+    sha256 cellar: :any_skip_relocation, catalina:     "9ae723ffe700b49e20935ba897d62def51e24cf459ee00a1e770d374bd31cb06"
+    sha256 cellar: :any_skip_relocation, mojave:       "f9a2502784da94f40412d65e488d9e7f0410a1da163b387ced45e030effc4061"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "df6b341648495a84bb298258aacb816d5afb86ddc3058417ecdabe58d805fc49"
   end
 
   def install
