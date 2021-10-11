6e17fb37b4f3d806d2608cb1666484f1aa9690df
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-11 02:15:15 +0000

    mmtabbarview: add 1.4.2 bottle.

--- Formula/mmtabbarview.rb
+++ Formula/mmtabbarview.rb
@@ -5,6 +5,13 @@ class Mmtabbarview < Formula
   sha256 "a5b79f1b50f6cabe97558f4c24a6317c448c534f15655309b6b29a532590e976"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "10a139efa381ffffb4b38609246914c123559b80ceaf16baa96135ae4687ba5b"
+    sha256 cellar: :any, big_sur:       "a16676e466f896888d2e90cc703dd95919b242bcff90ae84d4c5be05eee3b881"
+    sha256 cellar: :any, catalina:      "3ef5d2b3664b7ba3def8ba27c4b3c2e5d94af4f5da6aee0400fd148b091e955c"
+    sha256 cellar: :any, mojave:        "6ce9ac264e1e62f9ee98dd08e6837def238b99c3a5506cc0c470be4c5442ba3e"
+  end
+
   depends_on xcode: :build
   depends_on :macos
 
