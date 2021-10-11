1279b3a76976db81feb779b26f8f3af8c31e89c5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-11 17:18:39 +0000

    cpufetch: add 1.00 bottle.

--- Formula/cpufetch.rb
+++ Formula/cpufetch.rb
@@ -6,6 +6,14 @@ class Cpufetch < Formula
   license "MIT"
   head "https://github.com/Dr-Noob/cpufetch.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a9261597d5753e1946399243d1a678ad734d04583769a7c44471f7ce9618cc82"
+    sha256 cellar: :any_skip_relocation, big_sur:       "79a1430cf484b5af27898f13a6cbfa50c45c704b114dc1422f033f540b9c9fdf"
+    sha256 cellar: :any_skip_relocation, catalina:      "38e7cd730de97b753d3c1cbf342d132c62dbc914e9ec17f775e55c2e8d78ad1a"
+    sha256 cellar: :any_skip_relocation, mojave:        "790d979cab962161c6b4e372f67f11c756f9f2f1404f39f61997d64a5dd1215e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88d4ed1abfb99807493c94f56ed01f1274763c3db9e747c4cdc2bf383e5c40a1"
+  end
+
   def install
     system "make"
     bin.install "cpufetch"
