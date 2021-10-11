084d2a5cd523c6df3e403d943bb52c5b0e74a21b
Rui Chen <rui@chenrui.dev>
2021-10-11 09:15:19 -0400

    whistle 2.7.28
    Closes #87084.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.7.26.tgz"
-  sha256 "2db94cad759837e9ab2fb3796efbce099ee24c1c787a8a0d15f8e09355e96133"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.7.28.tgz"
+  sha256 "11b81acb542c2810867947c0925b5045c01396a2620eaa576db09d620f783ec0"
   license "MIT"
 
   bottle do
