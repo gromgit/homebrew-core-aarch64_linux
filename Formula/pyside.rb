f6b0dfd58574eb2ead0b6f9e389af10d3faac667
PaperChalice <lgamma@163.com>
2021-10-11 09:11:41 +0800

    pyside 6.2.1
    Closes #89084.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -1,8 +1,8 @@
 class Pyside < Formula
   desc "Official Python bindings for Qt"
   homepage "https://wiki.qt.io/Qt_for_Python"
-  url "https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-6.0.4-src/pyside-setup-opensource-src-6.0.4.tar.xz"
-  sha256 "0a076dd9f28aabe947739986a47431fa5bece1dccfd8ea90d2c9048ddede6303"
+  url "https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-6.2.1-src/pyside-setup-opensource-src-6.2.1.tar.xz"
+  sha256 "e0df6f42ed92e039d44ae9bf7d23cc4ee2fc4722c87adddbeafc6376074c4cd4"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-3.0-only"]
 
   livecheck do
@@ -19,46 +19,64 @@ class Pyside < Formula
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
+  depends_on xcode: :build
   depends_on "llvm"
   depends_on "python@3.9"
   depends_on "qt"
 
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+
   def install
-    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    # upstream issue: https://bugreports.qt.io/browse/PYSIDE-1684
+    inreplace "sources/pyside6/cmake/Macros/PySideModules.cmake",
+              "${shiboken_include_dirs}",
+              "${shiboken_include_dirs}:#{Formula["qt"].opt_include}"
 
-    args = std_cmake_args + %W[
-      -GNinja
-      -DPYTHON_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python#{xy}
-      -DCMAKE_INSTALL_RPATH=#{lib}
+    qt = Formula["qt"]
+    site_packages = prefix/Language::Python.site_packages("python3")
+    site_pyside = site_packages/"PySide6"
+    pyside_args = %w[
+      --no-examples
+      --no-qt-tools
+      --rpath @loader_path/../shiboken6
+      --shorter-paths
+      --skip-docs
     ]
+    system "python3", *Language::Python.setup_install_args(prefix), *pyside_args
 
-    mkdir "build" do
-      system "cmake", *args, ".."
-      system "ninja", "install"
+    # install tools symlinks
+    %w[lupdate lrelease].each { |x| ln_s (qt.opt_bin/x).relative_path_from(site_pyside), site_pyside }
+    mkdir_p site_pyside/"Qt/libexec"
+    %w[uic rcc].each do |x|
+      ln_s (qt.opt_pkgshare/"libexec"/x).relative_path_from(site_pyside/"Qt/libexec"), site_pyside/"Qt/libexec"
     end
+    %w[assistant linguist].each { |x| ln_s (qt.opt_bin/x).relative_path_from(site_pyside), site_pyside }
+    ln_s (qt.opt_libexec/"Designer.app").relative_path_from(site_pyside), site_pyside
   end
 
   test do
     system Formula["python@3.9"].opt_bin/"python3", "-c", "import PySide6"
     system Formula["python@3.9"].opt_bin/"python3", "-c", "import shiboken6"
 
-    # TODO: add modules `Position`, `Multimedia`and `WebEngineWidgets` when qt6.2 is released
-    # arm support will finish in qt6.1
     modules = %w[
       Core
       Gui
       Network
+      Positioning
       Quick
       Svg
       Widgets
       Xml
     ]
+    modules << "WebEngineCore" if OS.mac? && (DevelopmentTools.clang_build_version > 1200)
 
     modules.each { |mod| system Formula["python@3.9"].opt_bin/"python3", "-c", "import PySide6.Qt#{mod}" }
 
     pyincludes = shell_output("#{Formula["python@3.9"].opt_bin}/python3-config --includes").chomp.split
     pylib = shell_output("#{Formula["python@3.9"].opt_bin}/python3-config --ldflags --embed").chomp.split
     pyver = Language::Python.major_minor_version(Formula["python@3.9"].opt_bin/"python3").to_s.delete(".")
+    site_packages = prefix/Language::Python.site_packages("python3")
 
     (testpath/"test.cpp").write <<~EOS
       #include <shiboken.h>
@@ -70,8 +88,9 @@ class Pyside < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "-std=c++11", "test.cpp",
-           "-I#{include}/shiboken6", "-L#{lib}", "-lshiboken6.cpython-#{pyver}-darwin",
+    system ENV.cxx, "-std=c++17", "test.cpp",
+           "-I#{site_packages}/shiboken6_generator/include", "-L#{site_packages}/shiboken6",
+           "-lshiboken6.cpython-#{pyver}-darwin.#{version.major_minor}",
            *pyincludes, *pylib, "-o", "test"
     system "./test"
   end
