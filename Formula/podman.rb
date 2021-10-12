8775c52f24abbfa9d5438adcb980a7ff5ba9251f
Guillaume Rose <gurose@redhat.com>
2021-10-12 10:46:07 +0200

    podman: revision
    * podman: bump gvproxy component
    * podman: use gvproxy from git when installing head
      Also only apply the patch on stable
    
    Closes #87116.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/podman.rb
+++ Formula/podman.rb
@@ -1,11 +1,23 @@
 class Podman < Formula
   desc "Tool for managing OCI containers and pods"
   homepage "https://podman.io/"
-  url "https://github.com/containers/podman/archive/v3.4.0.tar.gz"
-  sha256 "558dcc8fbf72095aa1ec8abeb84ca2093dd0d51b77f0115ef855e640e2f03146"
   license "Apache-2.0"
-  revision 1
-  head "https://github.com/containers/podman.git", branch: "main"
+  revision 2
+
+  stable do
+    url "https://github.com/containers/podman/archive/v3.4.0.tar.gz"
+    sha256 "558dcc8fbf72095aa1ec8abeb84ca2093dd0d51b77f0115ef855e640e2f03146"
+
+    patch do
+      url "https://github.com/containers/podman/commit/cd4e10fdf93009f8ecba5f0c82c1c2a4a46f3e4f.patch?full_index=1"
+      sha256 "d173f27ff530022244cc6895bfd08fbb7546e1457b2edee0854732200aabfde5"
+    end
+
+    resource "gvproxy" do
+      url "https://github.com/containers/gvisor-tap-vsock/archive/v0.2.0.tar.gz"
+      sha256 "a54da74d6ad129a1c8fed3802ba8651cce37b123ee0e771b0d35889dae4751fc"
+    end
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "03f38f5d89276c5b448812e4add56822e9e85c912d8fd078d87c0606b781cc5b"
@@ -14,19 +26,17 @@ class Podman < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "e56623493f090b800054d70a611b087528576b3ba3cb9efd59bcd549497beebe"
   end
 
+  head do
+    url "https://github.com/containers/podman.git", branch: "main"
+
+    resource "gvproxy" do
+      url "https://github.com/containers/gvisor-tap-vsock.git", branch: "main"
+    end
+  end
+
   depends_on "go" => :build
   depends_on "go-md2man" => :build
   depends_on "qemu"
-
-  resource "gvproxy" do
-    url "https://github.com/containers/gvisor-tap-vsock/archive/v0.1.0.tar.gz"
-    sha256 "e1e1bec2fc42039da1ae68d382d4560a27c04bbe2aae535837294dd6773e88e0"
-  end
-
-  patch do
-    url "https://github.com/containers/podman/commit/cd4e10fdf93009f8ecba5f0c82c1c2a4a46f3e4f.patch?full_index=1"
-    sha256 "d173f27ff530022244cc6895bfd08fbb7546e1457b2edee0854732200aabfde5"
-  end
 
   def install
     os = if OS.mac?
@@ -44,7 +54,7 @@ class Podman < Formula
     end
 
     resource("gvproxy").stage do
-      system "make"
+      system "make", "gvproxy"
       libexec.install "bin/gvproxy"
     end
 
