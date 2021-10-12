e18c9df8504447efe49f6adf8db3c094636fd64a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-12 05:03:17 +0000

    teleport 7.3.0
    Closes #87112.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v7.2.1.tar.gz"
-  sha256 "8087cb4c6a6725038e4b31f2ec6accd1ba5122b0678db2583d24efe712f497ec"
+  url "https://github.com/gravitational/teleport/archive/v7.3.0.tar.gz"
+  sha256 "0ebda4ebc5482a3e23302df696edf8ad60fb68466e41a4deecc05acf1e76c771"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
