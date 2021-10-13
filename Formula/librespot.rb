141d167497ee933d9c8e0cc633c4bf9e2e3433f6
Rui Chen <rui@chenrui.dev>
2021-10-13 16:53:13 -0700

    librespot 0.3.0
    Closes #87218.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/librespot.rb
+++ Formula/librespot.rb
@@ -1,8 +1,8 @@
 class Librespot < Formula
   desc "Open Source Spotify client library"
   homepage "https://github.com/librespot-org/librespot"
-  url "https://github.com/librespot-org/librespot/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "cd6e7ae63583584598dead53b5e8e4077ab37df6e3b59c2a7416459b9a649fc3"
+  url "https://github.com/librespot-org/librespot/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "3ae67ac39606d049a26ee9e4b59f932222aac0098845ce3e1ef33c1b98080454"
   license "MIT"
   head "https://github.com/librespot-org/librespot.git", branch: "dev"
 
