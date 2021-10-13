75a8302115027f7c75811f74de3c287f309bb6d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-13 08:09:21 +0000

    libmobi: update 0.8 bottle.

--- Formula/libmobi.rb
+++ Formula/libmobi.rb
@@ -6,10 +6,11 @@ class Libmobi < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "43d37ed55ec4c3c5c376456d0caf9d28092e2781d0dbd4301fa58a456402311a"
-    sha256 cellar: :any, big_sur:       "321971da1ec7a654f2c2d1422502b6accae3f392e1cfd6af50b633d6a058d554"
-    sha256 cellar: :any, catalina:      "88ac957feb1b298437656e370c79ec851b8e8c4144e2de616f180669452f7423"
-    sha256 cellar: :any, mojave:        "4fcf8e0c1355976f7f1bd44d2395e3e67c167b3dca04a9d6a8194c38ea6aa9f8"
+    sha256 cellar: :any,                 arm64_big_sur: "c457b6ace79b52c67aa29a2fa6a95efa542104bc263bca7a1d79f519fb34ddcd"
+    sha256 cellar: :any,                 big_sur:       "03942b017ce950187f2950fd8612e7d5c3ddb4541e4a48344f8dd7f9adb306dc"
+    sha256 cellar: :any,                 catalina:      "eea4fc062b119d7a420e1e511cb000927976fc56cc3cfda77e95f2224cc71b57"
+    sha256 cellar: :any,                 mojave:        "33ad38a4a40187f9aa20dec2e9f153102f3ac961cf619686698bf7d23d9142e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3adb5a02f5c65985cd7f3341a4068e178513c4bd1038ecd76b1a0141210de13a"
   end
 
   uses_from_macos "libxml2"
