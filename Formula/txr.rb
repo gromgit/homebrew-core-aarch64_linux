4ffd50611b94aacdd54a0a8c06dc7511ebf502dd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-13 11:43:30 +0800

    txr: update 271_1 bottle.

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -12,10 +12,10 @@ class Txr < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ac7923ccf5ffde9633e50594fa2ae0115dbde704bb14897bb3e1f268aca59cd2"
-    sha256 cellar: :any, big_sur:       "8a7c80abc84909a7b5e3a9f425cf8d44b91da43cf0de224d8ac33fbbbd6ac3d9"
-    sha256 cellar: :any, catalina:      "723f72aa2392fe9b0ba78592269363d288846357be57524a3fbacaf9379a25b2"
-    sha256 cellar: :any, mojave:        "43089c64ba79f3ec5dfc095ddaac58cb8832554be3e7fbf6ed9ccebcaa9a14df"
+    sha256 cellar: :any, arm64_big_sur: "075aef49b3405bd3e5eb1198cc4cc855e8869cece96a3e3c70449091bd6b4f95"
+    sha256 cellar: :any, big_sur:       "26bfd1e76168d6f2ad2c830ffd02e279c7748f928455bc18f25a4288dce683f2"
+    sha256 cellar: :any, catalina:      "d995de85c281bf8af8cb32ac2f7d922174c3e0fa241c322b0f274208123eab93"
+    sha256 cellar: :any, mojave:        "793139738a803ad1a1b62dee660aa5de084d69510b4fa8adbe04c4fa258cc14f"
   end
 
   depends_on "libffi"
