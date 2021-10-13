9e3b8cb6ae9b9dc64c16bb638f85339830518045
Caleb Xu <calebcenter@live.com>
2021-10-13 01:22:18 -0400

    spotbugs 4.4.2
    Closes #87170.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/spotbugs.rb
+++ Formula/spotbugs.rb
@@ -1,8 +1,8 @@
 class Spotbugs < Formula
   desc "Tool for Java static analysis (FindBugs's successor)"
   homepage "https://spotbugs.github.io/"
-  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.4.1/spotbugs-4.4.1.tgz"
-  sha256 "341873c7c4a73508aca6f32f03339aad38c926703accb2e799b5b632b0832bd9"
+  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.4.2/spotbugs-4.4.2.tgz"
+  sha256 "459c3b245718338c010c4e2444a5c40900e8455b1127280a7e2515e60212dea1"
   license "LGPL-2.1-or-later"
 
   bottle do
