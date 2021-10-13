8fc11439ad7b61f75f9b211076b17d46bc8fcb00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-13 01:29:09 +0000

    himalaya: update 0.5.0 bottle.

--- Formula/himalaya.rb
+++ Formula/himalaya.rb
@@ -6,11 +6,11 @@ class Himalaya < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b9f1a4782b7eccff169258d151394248048c3d87383091464ee2f81c6f95b82e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b312c3288de354c72d4fdeaffe0cad976b4e0b05fd8c07232b5b39bd2eeeff3a"
-    sha256 cellar: :any_skip_relocation, catalina:      "32e04cd25c7c15f041ffbbb835d789b4d90aa1368d26478011d91d79e6303d7d"
-    sha256 cellar: :any_skip_relocation, mojave:        "155a6e3097c420a9b48a6136778fc35b1deadec83047da7c1f5b4d92a81f32f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3f38e475139deecaca7a218e4d38f8864dc65d6ef293379b08b9e707c0ffa9b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f4d5f0bfbea959ce2c1ec28eb7390d6aaeb10142b49b5d6c6fbd924a5e2d4b8b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "add65bf60e94df3070afbfc47762c124110105d46bce5b623addb50c48b761ce"
+    sha256 cellar: :any_skip_relocation, catalina:      "dc9d1dbdf992d95748c7a1e968adab904b0d347bb9d9a3c3c74e91c2f8830bf9"
+    sha256 cellar: :any_skip_relocation, mojave:        "a3026d947c691e9df9559fd31a75dc0f5d7ed5b8c578556505b3cdffd0e3766d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b0612ac59172e8b4884ab9c3038d9a0d1aaf1e2807f4a9892654d1bb02286b83"
   end
 
   depends_on "rust" => :build
