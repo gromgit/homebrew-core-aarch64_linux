f0ca5f521831c2ebae2debe7080a8086ee2aa420
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-13 11:43:33 +0800

    p11-kit: update 0.24.0_1 bottle.

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -7,11 +7,11 @@ class P11Kit < Formula
   revision 1
 
   bottle do
-    sha256 arm64_big_sur: "08f5b20fdf61c9b8fae57e972c9970aa28f5ef2daa7ea5990e20b02989c49095"
-    sha256 big_sur:       "d72ad488a7efacfbd178e3c95fbc2315661cf27071500c05ff267abd196e2970"
-    sha256 catalina:      "438735a16afded002578fd553d96047e53fda95de84928e696bd3dda08aee0de"
-    sha256 mojave:        "1948f25f27aa1a6978246c644c5c356ea5b4371bf2eabe5b96c378251d5bbeaf"
-    sha256 x86_64_linux:  "795988f18b0a393f4e5769c3b55646f7ee236b865489f547c6853ce111073903"
+    sha256 arm64_big_sur: "74df2e80935cbf3fd9f434d39787fab5f89ca1f4dc210c3dc002d2e8445c155d"
+    sha256 big_sur:       "afd53d336262a0bb9983276eb6ea6b9c602a534d021dc9abc1f3ddfa20ec9869"
+    sha256 catalina:      "37ee66ec71206a3077deef9d2f2452d0ea43a60d66a1d257a22947a189e115ea"
+    sha256 mojave:        "8d25a20a23fbd9e5787369b86b2af53d1cb558e69de5f7f8ab25069ae7beb390"
+    sha256 x86_64_linux:  "a8d4ad8b26dd3742a23afacba624ce409425e83b49b118a056f28d1e6fb96637"
   end
 
   head do
