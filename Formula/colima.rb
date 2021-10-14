318a03a23a6f1c2b98c612f6d0e3b24f595512bb
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2021-10-14 11:17:37 +0300

    colima 0.2.2 (new formula)
    * colima 0.2.1 (new formula)
      This adds a formula for colima - tool for bootstrapping
      docker/containerd environment in Mac OS.
    * Address CR
    * Update Formula/colima.rb
    * Address CR
    * Mark as Mac OS-only package
      Also bump version to 0.2.2
    
    Closes #87237.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/colima.rb
@@ -0,0 +1,31 @@
+class Colima < Formula
+  desc "Container runtimes on MacOS with minimal setup"
+  homepage "https://github.com/abiosoft/colima/blob/main/README.md"
+  url "https://github.com/abiosoft/colima.git",
+      tag:      "v0.2.2",
+      revision: "b2c7697bee2d73e995f156fe8e9870eb246c07e6"
+  license "MIT"
+  head "https://github.com/abiosoft/colima.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "lima"
+  depends_on :macos
+
+  def install
+    project = "github.com/abiosoft/colima"
+    ldflags = %W[
+      -X #{project}/config.appVersion=#{version}
+      -X #{project}/config.revision=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/colima"
+
+    (bash_completion/"colima").write Utils.safe_popen_read(bin/"colima", "completion", "bash")
+    (zsh_completion/"_colima").write Utils.safe_popen_read(bin/"colima", "completion", "zsh")
+    (fish_completion/"colima.fish").write Utils.safe_popen_read(bin/"colima", "completion", "fish")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/colima version 2>&1")
+    assert_match "colima is not running", shell_output("#{bin}/colima status 2>&1", 1)
+  end
+end
