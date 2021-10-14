46ceec7b4b84b21ab8a5cbcc4ab79b51932cd5d1
Caleb Xu <calebcenter@live.com>
2021-10-14 00:00:03 -0400

    mvtools: fix test for new Vapoursynth API

--- Formula/mvtools.rb
+++ Formula/mvtools.rb
@@ -34,15 +34,14 @@ class Mvtools < Formula
       MVTools will not be autoloaded in your VapourSynth scripts. To use it
       use the following code in your scripts:
 
-        core.std.LoadPlugin(path="#{HOMEBREW_PREFIX}/lib/#{shared_library("libmvtools")}")
+        vs.core.std.LoadPlugin(path="#{HOMEBREW_PREFIX}/lib/#{shared_library("libmvtools")}")
     EOS
   end
 
   test do
     script = <<~EOS.split("\n").join(";")
       import vapoursynth as vs
-      core = vs.get_core()
-      core.std.LoadPlugin(path="#{lib}/#{shared_library("libmvtools")}")
+      vs.core.std.LoadPlugin(path="#{lib/shared_library("libmvtools")}")
     EOS
 
     system Formula["python@3.9"].opt_bin/"python3", "-c", script
