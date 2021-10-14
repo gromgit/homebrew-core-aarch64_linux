6d6a4aa9ba0d0f2688b07ea0df3d90a9a5f40f81
Sam Ford <1584702+samford@users.noreply.github.com>
2021-10-14 11:51:01 -0400

    libnfnetlink: add livecheck

--- Formula/libnfnetlink.rb
+++ Formula/libnfnetlink.rb
@@ -5,6 +5,11 @@ class Libnfnetlink < Formula
   sha256 "f270e19de9127642d2a11589ef2ec97ef90a649a74f56cf9a96306b04817b51a"
   license "LGPL-2.1-or-later"
 
+  livecheck do
+    url "https://www.netfilter.org/projects/libnfnetlink/downloads.html"
+    regex(/href=.*?libnfnetlink[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4cc0401b5074648aba2b0140000ad7728fafd440f8d100d6c868b6f5a9f524aa"
   end
