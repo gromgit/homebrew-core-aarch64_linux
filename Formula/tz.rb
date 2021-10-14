0d14c17f9abf09d43ae40a445ec53b4aa4741254
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-14 15:12:30 +0000

    tz: update 0.6.1 bottle.

--- Formula/tz.rb
+++ Formula/tz.rb
@@ -7,10 +7,10 @@ class Tz < Formula
   head "https://github.com/oz/tz.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "095758a0ce88d49b3df127854862d39359ab5449543dc9525609000eb673133f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a2bfa56237ba4a0165ecdf1911cb1afcf68d82357c9bf71afd7c68ae9721c1e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "a2bfa56237ba4a0165ecdf1911cb1afcf68d82357c9bf71afd7c68ae9721c1e6"
-    sha256 cellar: :any_skip_relocation, mojave:        "a2bfa56237ba4a0165ecdf1911cb1afcf68d82357c9bf71afd7c68ae9721c1e6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ffcc6e47c89080907b571031d530046fe710d4c74c45183a8981c6c67ed3515a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2ce8046b150966b4e05f0f13255f17d6589650331578256f6f6f41ec25823f81"
+    sha256 cellar: :any_skip_relocation, catalina:      "2ce8046b150966b4e05f0f13255f17d6589650331578256f6f6f41ec25823f81"
+    sha256 cellar: :any_skip_relocation, mojave:        "2ce8046b150966b4e05f0f13255f17d6589650331578256f6f6f41ec25823f81"
   end
 
   depends_on "go" => :build
