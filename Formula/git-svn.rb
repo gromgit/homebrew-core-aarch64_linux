36344df967c32847173015acd901e7f625acebd4
MoonFruit <dkmoonfruit@gmail.com>
2021-10-14 19:52:11 +0800

    git-svn 2.33.1 (new formula)
    Closes #87241.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/git-svn.rb
@@ -0,0 +1,68 @@
+class GitSvn < Formula
+  desc "Bidirectional operation between a Subversion repository and Git"
+  homepage "https://git-scm.com"
+  # NOTE: Please keep these values in sync with git.rb when updating.
+  url "https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.33.1.tar.xz"
+  sha256 "e054a6e6c2b088bd1bff5f61ed9ba5aa91c9a3cd509539a4b41c5ddf02201f2f"
+  license "GPL-2.0-only"
+  head "https://github.com/git/git.git", branch: "master"
+
+  livecheck do
+    formula "git"
+  end
+
+  depends_on "git"
+  depends_on "subversion"
+
+  uses_from_macos "perl"
+
+  def install
+    perl = DevelopmentTools.locate("perl")
+    perl_version, perl_short_version = Utils.safe_popen_read(perl, "-e", "print $^V")
+                                            .match(/v((\d+\.\d+)(?:\.\d+)?)/).captures
+
+    ENV["PERL_PATH"] = perl
+    ENV["PERLLIB_EXTRA"] = Formula["subversion"].opt_lib/"perl5/site_perl"/perl_version/"darwin-thread-multi-2level"
+    if OS.mac?
+      ENV["PERLLIB_EXTRA"] += ":" + %W[
+        #{MacOS.active_developer_dir}
+        /Library/Developer/CommandLineTools
+        /Applications/Xcode.app/Contents/Developer
+      ].uniq.map do |p|
+        "#{p}/Library/Perl/#{perl_short_version}/darwin-thread-multi-2level"
+      end.join(":")
+    end
+
+    args = %W[
+      prefix=#{prefix}
+      perllibdir=#{Formula["git"].opt_share}/perl5
+      SCRIPT_PERL=git-svn.perl
+    ]
+
+    mkdir libexec/"git-core"
+    system "make", "install-perl-script", *args
+
+    bin.install_symlink libexec/"git-core/git-svn"
+  end
+
+  test do
+    system "svnadmin", "create", "repo"
+
+    url = "file://#{testpath}/repo"
+    text = "I am the text."
+    log = "Initial commit"
+
+    system "svn", "checkout", url, "svn-work"
+    (testpath/"svn-work").cd do |current|
+      (current/"text").write text
+      system "svn", "add", "text"
+      system "svn", "commit", "-m", log
+    end
+
+    system "git", "svn", "clone", url, "git-work"
+    (testpath/"git-work").cd do |current|
+      assert_equal text, (current/"text").read
+      assert_match log, pipe_output("git log --oneline")
+    end
+  end
+end
