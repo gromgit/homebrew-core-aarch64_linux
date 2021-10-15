76b0e19cba8082a4a963818a029590d9e7f3b356
rui <rui@chenrui.dev>
2021-10-15 18:01:49 +0000

    liqoctl: add 0.3.0 bottle.

--- Formula/liqoctl.rb
+++ Formula/liqoctl.rb
@@ -5,6 +5,14 @@ class Liqoctl < Formula
   sha256 "1f556c5e0aca3667d129c65d19959f8d62919639dc5f3c85e6ab63e3c5e10d27"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4f60e00ba9c591f7b4628dbae97eed5e4015ee145f614bd91ccea3f1f60ad0f1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0e3ff868e626e694e7a99cb019fa4c8168ce63707d9507794e414e6d64c87b65"
+    sha256 cellar: :any_skip_relocation, catalina:      "0e3ff868e626e694e7a99cb019fa4c8168ce63707d9507794e414e6d64c87b65"
+    sha256 cellar: :any_skip_relocation, mojave:        "0e3ff868e626e694e7a99cb019fa4c8168ce63707d9507794e414e6d64c87b65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58aa8f09f2e9eeab4292829d8a09c2f33b16482864a8676ea7f4536e75138416"
+  end
+
   depends_on "go" => :build
 
   def install
