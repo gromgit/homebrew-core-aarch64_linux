1c498943377b83e090d99163ba2d9427493bceb9
Ferhat Yildiz <ferhat.yildiz@turingts.com>
2021-10-15 11:04:46 +0200

    liqoctl 0.3.0 (new formula)
    Closes #87285.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/liqoctl.rb
@@ -0,0 +1,34 @@
+class Liqoctl < Formula
+  desc "Is a CLI tool to install and manage Liqo-enabled clusters"
+  homepage "https://liqo.io"
+  url "https://github.com/liqotech/liqo/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "1f556c5e0aca3667d129c65d19959f8d62919639dc5f3c85e6ab63e3c5e10d27"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    ldflags = %W[
+      -s -w
+      -X github.com/liqotech/liqo/pkg/liqoctl/version.liqoctlVersion=v#{version}
+    ].join(" ")
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/liqoctl"
+
+    output = Utils.safe_popen_read(bin/"liqoctl", "completion", "bash")
+    (bash_completion/"liqoctl").write output
+    output = Utils.safe_popen_read(bin/"liqoctl", "completion", "zsh")
+    (zsh_completion/"_liqoctl").write output
+    output = Utils.safe_popen_read(bin/"liqoctl", "completion", "fish")
+    (fish_completion/"liqoctl").write output
+  end
+
+  test do
+    run_output = shell_output("#{bin}/liqoctl 2>&1")
+    assert_match "liqoctl is a CLI tool to install and manage Liqo-enabled clusters.", run_output
+    assert_empty shell_output("#{bin}/liqoctl install kind 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/liqoctl version")
+  end
+end
