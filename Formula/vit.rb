cbeccffbea6b21182977012eab162b88d2b020b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-15 02:19:01 +0000

    vit: update 2.1.0_1 bottle.

--- Formula/vit.rb
+++ Formula/vit.rb
@@ -10,11 +10,11 @@ class Vit < Formula
   head "https://github.com/vit-project/vit.git", branch: "2.x"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3fb8beae350a46c57b5e40940f5223043eb171dd7a1bcc4b9ef40320c2202571"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5fedd66cd3ea0e7b6f5cbb4e285b65c085a6b041190d42afa3ddbc4a1aa18f13"
-    sha256 cellar: :any_skip_relocation, catalina:      "17221b4deacb1ca0e63fc7949a06298dfe9d64c9a672a9974f63f4dc15473404"
-    sha256 cellar: :any_skip_relocation, mojave:        "d65ce3abf2f776a8baee233c618b543c2f806e48cfd343cc4d3febc9947f71b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f071f296dda7703215badc37478d56c9cec45b9e592293e9b71741f5ee36c844"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1c1e4368f8918aa08a3571bcdfd2f96e0acab05e474040ed5e9f09e9dc809f64"
+    sha256 cellar: :any_skip_relocation, big_sur:       "cc1f63918e6786ff2a803aff29b4b0d5bf7d4790ee971a693ff4145f78eeb3ee"
+    sha256 cellar: :any_skip_relocation, catalina:      "fa97ec6434ae23a90c70ac42e4361b92fe7dbfc0597a5cbb3bddacdb7a95b7c5"
+    sha256 cellar: :any_skip_relocation, mojave:        "7e08b872f1007fb5b47545831cdbcd301417ad0c9a6a85e686f350fa272ac502"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7e13e260c35eb8fc957abac0aec2b2186df75dcc76eb49b9193210f749d61c2"
   end
 
   depends_on "python@3.10"
