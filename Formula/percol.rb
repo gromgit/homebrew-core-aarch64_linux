3ddbe84aaae2fb89895688ebe8471ad49b364535
Michael Cho <cho-m@tuta.io>
2021-10-14 18:53:41 -0700

    percol: migrate to python@3.10
    Closes #87266.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/percol.rb
+++ Formula/percol.rb
@@ -6,7 +6,7 @@ class Percol < Formula
   url "https://files.pythonhosted.org/packages/50/ea/282b2df42d6be8d4292206ea9169742951c39374af43ae0d6f9fff0af599/percol-0.2.1.tar.gz"
   sha256 "7a649c6fae61635519d12a6bcacc742241aad1bff3230baef2cedd693ed9cfe8"
   license "MIT"
-  revision 3
+  revision 4
   head "https://github.com/mooz/percol.git", branch: "master"
 
   bottle do
@@ -17,17 +17,14 @@ class Percol < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "5bb5d60ccdc9296979b17f056377fc73c9d970580654e342e89b0f7e2a23154c"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
+  depends_on "six"
+
   uses_from_macos "expect" => :test
 
   resource "cmigemo" do
     url "https://files.pythonhosted.org/packages/2f/e4/374df50b655e36139334046f898469bf5e2d7600e1e638f29baf05b14b72/cmigemo-0.1.6.tar.gz"
     sha256 "7313aa3007f67600b066e04a4805e444563d151341deb330135b4dcdf6444626"
-  end
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
-    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
   end
 
   def install
@@ -42,6 +39,6 @@ class Percol < Formula
       spawn #{bin}/percol --query=Homebrew textfile
       expect "QUERY> Homebrew"
     EOS
-    assert_match "Homebrew", shell_output("/usr/bin/expect -f expect-script")
+    assert_match "Homebrew", shell_output("expect -f expect-script")
   end
 end
