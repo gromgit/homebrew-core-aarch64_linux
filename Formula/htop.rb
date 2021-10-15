b084743969e99cfb22b26acbaeac336724dd8db1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-15 03:08:14 +0000

    htop: update 3.1.1 bottle.

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -12,11 +12,11 @@ class Htop < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "be94287535d61d1c180bace2ec77492a8de092539ace76b770d53c5234704e69"
-    sha256 cellar: :any,                 big_sur:       "f651173535b859cecca10d2dfab28ff78c184cf1e16455ec296fae2d509d2aad"
-    sha256 cellar: :any,                 catalina:      "2601b6b120df50c7790d5b2f8dcf06848ddae0b35315affec641dec17271fa46"
-    sha256 cellar: :any,                 mojave:        "7c9b9ac633b0339fead96c6e611b87bcc6df9f7fe0947dac76908340290707c2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bb5ac6bbfb4109cc5824ae40753bcab7fb08c9dd5b87ff0f41ea76e369d92545"
+    sha256 cellar: :any,                 arm64_big_sur: "bc5dfcfd0b154377c4436bc3f2c35a43d618319c43e9184d13832e06fdd79820"
+    sha256 cellar: :any,                 big_sur:       "301c8306cbfe6584335a8e7db71e2436e336357b0371f5cf29b94b1a557c6652"
+    sha256 cellar: :any,                 catalina:      "7ddeb74a5a6873b69f705fb090e6b477dec8261dbb221a5b8dbc6bcc810af804"
+    sha256 cellar: :any,                 mojave:        "c5a1729400dfec62ee63ab53e5667f06d4709ee9021e3573c8ec1fd6ae4fcebc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a33628783ecc09a777afce0eb819e877a9379e71f9f22a074316ebf7ef2f47f4"
   end
 
   depends_on "autoconf" => :build
