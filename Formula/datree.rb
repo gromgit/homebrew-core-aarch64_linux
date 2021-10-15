71fb1b7538e269b4cddf57df4d0def1da3f31532
Rui Chen <rui@chenrui.dev>
2021-10-15 10:24:12 -0700

    datree 0.14.2
    Closes #87326.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/datree.rb
+++ Formula/datree.rb
@@ -1,8 +1,8 @@
 class Datree < Formula
   desc "CLI tool to run policies against Kubernetes manifests YAML files or Helm charts"
   homepage "https://www.datree.io/"
-  url "https://github.com/datreeio/datree/archive/0.13.7.tar.gz"
-  sha256 "ba69135e4c3f4931f3195e53d19761fd30b48575a9ee14b305cf1083afe61d17"
+  url "https://github.com/datreeio/datree/archive/0.14.2.tar.gz"
+  sha256 "c8bb07bbf757fbe0069a2f80af1af16350e3aa194dd46feed18ca79db16fc055"
   license "Apache-2.0"
   head "https://github.com/datreeio/datree.git", branch: "staging"
 
