cab376255520cf1f94e378edd701bf03ace145b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-15 01:30:26 +0000

    semgrep: update 0.69.1 bottle.

--- Formula/semgrep.rb
+++ Formula/semgrep.rb
@@ -15,11 +15,11 @@ class Semgrep < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e7e9a40ea4d0c6531b17c05f129697c3cd8af0e1889ba57e453802836a804518"
-    sha256 cellar: :any, big_sur:       "bd5a7ede9227f09f12af3b50b191358b2be82796e41e780c121980a0ba4e5726"
-    sha256 cellar: :any, catalina:      "c20acd708b991bea422d13b3e8e0daa654fbca6dfde0c9f96bc12e05acaa7a8a"
-    sha256 cellar: :any, mojave:        "6a239e42a0bf082bda1fceed9a0d89f0625cdc96aab3990412e8edb661777b1c"
-    sha256               x86_64_linux:  "17f169019860d015df338439141ae3f01e5280ece0344bed579d0efd4e4647c3"
+    sha256 cellar: :any, arm64_big_sur: "adab67140fe7da66df6ebb4aca63e4ff7882897955fd971c0e813f9f75cff23f"
+    sha256 cellar: :any, big_sur:       "1c4b95a0ae54be906bfc8629efaddc390406e65cffde8531de1aecf9eb96d732"
+    sha256 cellar: :any, catalina:      "f56e2bc229f6c9d60ba11169f49bd6f0a5b14a093cce6ad53d97de7951ed75bb"
+    sha256 cellar: :any, mojave:        "c5cfbd98685a2757c0c728da88e41c26c5f74f16eb919ec0ee3768a95f0ccd5f"
+    sha256               x86_64_linux:  "323577a9e572709a2b7fad9fb649fc9d2c73e3d195ebe6cd1c2552eacbbf0c36"
   end
 
   depends_on "cmake" => :build
