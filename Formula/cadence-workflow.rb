f1282e63ee4ff94d2a0159c8573906f769db9fb6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-16 02:22:05 +0000

    cadence-workflow: update 0.22.3 bottle.

--- Formula/cadence-workflow.rb
+++ Formula/cadence-workflow.rb
@@ -8,11 +8,11 @@ class CadenceWorkflow < Formula
   head "https://github.com/uber/cadence.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eb11b10bda98b3a84ae70c022fbdd5b21fed571a5e604d33071f92211b407977"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2e086ba5ae78d9bbbeb1b7b1498cfa6eadf3a87c7d2f0bb2cdbf6c5e33cce087"
-    sha256 cellar: :any_skip_relocation, catalina:      "72006c3ae1010c72e2d429a51f7548a655ad4c5bb5d69560e1f0d278eaf47057"
-    sha256 cellar: :any_skip_relocation, mojave:        "ead79d3141d2d7fe840b87350373dc4f030e0d5a2f5878eb07eb1a884e83b9b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8c3240980dcd953ac09934a87bc90d0dcf81148cda854c6a08301659f026e7e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "16ff455391768f46b87b3dd99b4728af4ba57595f9bfce7231135899b6fc715c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "be60b97549e05e29a88a707dd70b2be9ad33d237e0ba934ae1beeeacf4101822"
+    sha256 cellar: :any_skip_relocation, catalina:      "be81affc31ea7e805d349b2b79490f71635185327be2a2fb35face1a49d68529"
+    sha256 cellar: :any_skip_relocation, mojave:        "37317f58ab1ee1ea6202646df143c663c83c19774d8e30fca40f621949856d51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e43c44c10b20e1b68dddd0cf61039ad16a7da3e59e9775642deeb4336a5672cb"
   end
 
   depends_on "go" => :build
