888acc715d7f16662f5ddbfecff897f54bad9550
Rui Chen <rui@chenrui.dev>
2021-10-16 08:44:26 -0700

    yarn 1.22.17
    * yarn 1.22.17
    * yarn: update `NPM_CONFIG_PYTHON`
      `node` now depends on Homebrew Python 3, so let's make sure that's
      reflected in the config scripts.
    
      While we're here, let's remove unnecessary interpolation, avoid
      hardcoding 'homebrew', and clean up the style a little.
    
    Closes #87355.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,8 +1,8 @@
 class Yarn < Formula
   desc "JavaScript package manager"
   homepage "https://yarnpkg.com/"
-  url "https://yarnpkg.com/downloads/1.22.15/yarn-v1.22.15.tar.gz"
-  sha256 "0c2841b9423f0fb9657ae6b18873f39551396ec242bfb882b11bed9e4648235e"
+  url "https://yarnpkg.com/downloads/1.22.17/yarn-v1.22.17.tar.gz"
+  sha256 "267982c61119a055ba2b23d9cf90b02d3d16c202c03cb0c3a53b9633eae37249"
   license "BSD-2-Clause"
 
   livecheck do
@@ -18,15 +18,16 @@ class Yarn < Formula
   conflicts_with "hadoop", because: "both install `yarn` binaries"
 
   def install
-    libexec.install Dir["*"]
-    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js",
-      PREFIX:            HOMEBREW_PREFIX,
-      NPM_CONFIG_PYTHON: "/usr/bin/python"
-    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js",
-      PREFIX:            HOMEBREW_PREFIX,
-      NPM_CONFIG_PYTHON: "/usr/bin/python"
-    inreplace "#{libexec}/lib/cli.js", "/usr/local", HOMEBREW_PREFIX
-    inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
+    libexec.install buildpath.glob("*")
+    (bin/"yarn").write_env_script libexec/"bin/yarn.js",
+                                  PREFIX:            HOMEBREW_PREFIX,
+                                  NPM_CONFIG_PYTHON: which("python3")
+    (bin/"yarnpkg").write_env_script libexec/"bin/yarn.js",
+                                      PREFIX:            HOMEBREW_PREFIX,
+                                      NPM_CONFIG_PYTHON: which("python3")
+    inreplace libexec/"lib/cli.js", "/usr/local", HOMEBREW_PREFIX
+    inreplace libexec/"package.json", '"installationMethod": "tar"',
+                                      "\"installationMethod\": \"#{tap.user.downcase}\""
   end
 
   test do
