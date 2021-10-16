a7c2d9a7802ead568567f39526ade318410c9a4f
Caleb Xu <calebcenter@live.com>
2021-10-16 07:16:33 -0400

    igv 2.11.2
    Closes #87350.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.11/IGV_2.11.1.zip"
-  sha256 "71e9dc22d161b1747262a77c5053a224e2d279b2bbe4a0bdd64ab3a095fc88c7"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.11/IGV_2.11.2.zip"
+  sha256 "d036cdde3a99d1d415b78ed03c9aedf7b2b9fbb2da61e12dade0110956d33382"
   license "MIT"
 
   livecheck do
