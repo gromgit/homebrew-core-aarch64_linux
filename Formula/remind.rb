f1f6ec5dc0a2485640ba3444df1219c8565876fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-17 08:09:08 +0000

    remind: update 03.03.09 bottle.

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -12,10 +12,10 @@ class Remind < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "afca5089ee626813ac35ada41686bf93a3bbd91a816e74c08f8f541e683e8b3c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e89d4e7bbb52603fa5d3685024e6d224058323ddebb3e45ced59aff454758f8c"
-    sha256 cellar: :any_skip_relocation, catalina:      "5a4f3ee32e49c6190d4801b2946926a233f473bd25a78f495781ac24213c6d3c"
-    sha256 cellar: :any_skip_relocation, mojave:        "0f79a1688aa275201660ad40b624ba5bd10cd857d021779a182dfb1ea81b89b8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "23316bd202a1d1e14eaa0d65457e9aa67381f978ecef569c6168f25d6f9bff87"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c4ac21abe4902371e78e3535f474f75b9226a26d41a6b0d2c76fcb60d7cf7e17"
+    sha256 cellar: :any_skip_relocation, catalina:      "dd07a739df85b3fc68f68a74b5ac2b441dbb79adc9f8be4157bcb18ce9f93392"
+    sha256 cellar: :any_skip_relocation, mojave:        "25d7f636960dd34264dfc3f35f5b79105eeff5512cf9b84b274f02203af7d0ca"
   end
 
   conflicts_with "rem", because: "both install `rem` binaries"
