31771ea455a9328064a481f42868bab2329daa62
Rui Chen <rui@chenrui.dev>
2021-10-17 01:55:41 -0400

    picard-tools 2.26.3
    Closes #87441.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.26.2/picard.jar"
-  sha256 "99fab1699a735fd048a05975a243774f1cc99e87a9b28311d4aa872d06390474"
+  url "https://github.com/broadinstitute/picard/releases/download/2.26.3/picard.jar"
+  sha256 "1fb5600fbe6d98011dd353e3d68e81353d1069785dd69508a4016628ce8e525a"
   license "MIT"
 
   livecheck do
