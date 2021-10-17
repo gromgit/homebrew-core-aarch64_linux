967df946a64941f4d7a2ce792c94bc8551fa3c9b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-17 08:08:36 +0000

    pidgin: update 2.14.8 bottle.

--- Formula/pidgin.rb
+++ Formula/pidgin.rb
@@ -12,10 +12,10 @@ class Pidgin < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5a4a12719ba5f0ecf8760a4b924421e53436894c9768aef39ccb746556dea921"
-    sha256 big_sur:       "1818362c8ad056d5527ab78423b8d9439bb1e8cd5008dc0cea99d296e37ed482"
-    sha256 catalina:      "454cf51fead6fb7b37b226b9ffe45ab7af64b327f6a3022fd3c6b89cce93a060"
-    sha256 mojave:        "2940e978897485867020f76745c82069617eac27632421c3e0063e9d6a61ee90"
+    sha256 arm64_big_sur: "c848ea93d573f4104a7a7edf23c41d159ac5070e36f08d84e14d65af4a5a3bef"
+    sha256 big_sur:       "309a8ac7ee4e09f884ad79985d23895d77ddec13197a71ed764f73f9984d9175"
+    sha256 catalina:      "fbea5ba3a0051d76282289dadc36ec61d0c466aa1dda6358ccae7a8f12f48b42"
+    sha256 mojave:        "10159d6af23b8603b31160d0eba5dd689693e1cf8a6486e032ee0fc34dbbc703"
   end
 
   depends_on "intltool" => :build
