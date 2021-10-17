4dcedc7640fe0e145975e34fda95179c74a2ab39
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-10-17 13:05:45 +0100

    glibmm 2.70.0
    Closes #87456.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -1,8 +1,8 @@
 class Glibmm < Formula
   desc "C++ interface to glib"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/glibmm/2.68/glibmm-2.68.1.tar.xz"
-  sha256 "6664e27c9a9cca81c29e35687f49f2e0d173a2fc9e98c3428311f707db532f8c"
+  url "https://download.gnome.org/sources/glibmm/2.70/glibmm-2.70.0.tar.xz"
+  sha256 "8008fd8aeddcc867a3f97f113de625f6e96ef98cf7860379813a9c0feffdb520"
   license "LGPL-2.1-or-later"
 
   bottle do
