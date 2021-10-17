ceef588bdcace23ade107706e5867a71b5b98d3b
Rui Chen <rui@chenrui.dev>
2021-10-17 14:31:10 -0400

    asciidoc 10.0.0
    * asciidoc 10.0.0
    * asciidoc: fix livecheck
    
    Closes #88060.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -1,16 +1,15 @@
 class Asciidoc < Formula
-  include Language::Python::Shebang
+  include Language::Python::Virtualenv
 
-  desc "Formatter/translator for text files to numerous formats. Includes a2x"
+  desc "Formatter/translator for text files to numerous formats"
   homepage "https://asciidoc-py.github.io/"
-  url "https://github.com/asciidoc-py/asciidoc-py/archive/9.1.1.tar.gz"
-  sha256 "914dfc1542c30bd47faa0aaaae0985cb57d0ca584015729ccd1b94d90da3a616"
+  url "https://files.pythonhosted.org/packages/6b/26/d2828867b366b73fc55b5ed51d7ad369195cc51d6293875ac152aa4b1742/asciidoc-10.0.0.tar.gz"
+  sha256 "709e46e5fe528f9cf52272ec782a571e59c2f3776f6f67d49dd727351f669fdd"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/asciidoc-py/asciidoc-py.git", branch: "main"
 
   livecheck do
-    url :stable
+    url :head
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
@@ -18,35 +17,12 @@ class Asciidoc < Formula
     sha256 cellar: :any_skip_relocation, all: "c9ddf877047f2b026bc790eb8f7414ea909dbd213c92b50fba336da80e1d8286"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "docbook-xsl" => :build
   depends_on "docbook"
   depends_on "python@3.10"
   depends_on "source-highlight"
 
-  uses_from_macos "libxml2" => :build
-  uses_from_macos "libxslt" => :build
-
-  on_linux do
-    depends_on "xmlto" => :build
-  end
-
   def install
-    ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
-
-    system "autoconf"
-    system "./configure", "--prefix=#{prefix}"
-
-    %w[
-      a2x.py asciidoc.py filters/code/code-filter.py
-      filters/graphviz/graphviz2png.py filters/latex/latex2img.py
-      filters/music/music2png.py filters/unwraplatex.py
-    ].map { |f| rewrite_shebang detected_python_shebang, f }
-
-    # otherwise macOS's xmllint bails out
-    inreplace "Makefile", "-f manpage", "-f manpage -L"
-    system "make", "install"
-    system "make", "docs"
+    virtualenv_install_with_resources
   end
 
   def caveats
@@ -66,7 +42,7 @@ class Asciidoc < Formula
 
   test do
     (testpath/"test.txt").write("== Hello World!")
-    system "#{bin}/asciidoc", "-b", "html5", "-o", "test.html", "test.txt"
-    assert_match %r{<h2 id="_hello_world">Hello World!</h2>}, File.read("test.html")
+    system "#{bin}/asciidoc", "-b", "html5", "-o", testpath/"test.html", testpath/"test.txt"
+    assert_match %r{<h2 id="_hello_world">Hello World!</h2>}, File.read(testpath/"test.html")
   end
 end
