e24f6b306cac5929ecc0d938fdd68e234d566a64
Ryan Mast <mast.ryan@gmail.com>
2021-10-16 19:02:35 -0700

    toml11 3.7.0 (new formula)
    toml1 3.7.0 (new formula)
    
    Closes #87394.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/toml11.rb
@@ -0,0 +1,36 @@
+class Toml11 < Formula
+  desc "TOML for Modern C++"
+  homepage "https://github.com/ToruNiina/toml11"
+  url "https://github.com/ToruNiina/toml11/archive/refs/tags/v3.7.0.tar.gz"
+  sha256 "a0b6bec77c0e418eea7d270a4437510884f2fe8f61e7ab121729624f04c4b58e"
+  license "MIT"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.toml").write <<~EOS
+      test_str = "a test string"
+    EOS
+
+    (testpath/"test.cpp").write <<~EOS
+      #include "toml.hpp"
+      #include <iostream>
+
+      int main(int argc, char** argv) {
+          const auto data = toml::parse("test.toml");
+          const auto test_str = toml::find<std::string>(data, "test_str");
+          std::cout << "test_str = " << test_str << std::endl;
+          return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}"
+    assert_equal "test_str = a test string\n", shell_output("./test")
+  end
+end
