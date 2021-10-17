76265bb8a69613620d12ac4cb5cdbce27d3b2e27
Rui Chen <rui@chenrui.dev>
2021-10-17 01:09:36 -0400

    feroxbuster 2.4.0
    Closes #87408.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/feroxbuster.rb
+++ Formula/feroxbuster.rb
@@ -1,8 +1,8 @@
 class Feroxbuster < Formula
   desc "Fast, simple, recursive content discovery tool written in Rust"
   homepage "https://epi052.github.io/feroxbuster"
-  url "https://github.com/epi052/feroxbuster/archive/v2.3.3.tar.gz"
-  sha256 "5a244d1614fb9dc476e9be8bd1cdbb33e0b07a1fcb7285e911ae6c77a44c7327"
+  url "https://github.com/epi052/feroxbuster/archive/v2.4.0.tar.gz"
+  sha256 "077dec4b9e043b6a05582ea5598c35f4ea70beb45c8e516fee8279257ddf6c67"
   license "MIT"
 
   bottle do
