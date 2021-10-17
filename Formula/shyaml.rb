be77a982f21971851f0739f9f3cd88d9b1baa0c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-17 06:09:29 +0000

    shyaml: update 0.6.2_1 bottle.

--- Formula/shyaml.rb
+++ Formula/shyaml.rb
@@ -10,11 +10,11 @@ class Shyaml < Formula
   head "https://github.com/0k/shyaml.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ff1ab13915b1148905c77a4bac3ff65db4bc496d3b2fcdf031f30678f781c9f1"
-    sha256 cellar: :any,                 big_sur:       "bdfdbc881e2ef1cc0ae52ad5cafae3715bdfaf76a1ac905a6e3aa3a7fc1736a2"
-    sha256 cellar: :any,                 catalina:      "c95f616993a2fd70d3ed9fbb7556b582ef2aca26fbda97b36898bcfb9efbcee1"
-    sha256 cellar: :any,                 mojave:        "1f157591a67c0165af8492b3cb1b6049a7e151b0770aaed4c176c15fe3050f68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f3000c7847a03060e544f9eb06530071d2e38dbc561b817afc5ce3bf62bdb59"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9758d2528c7868f37da2288575d41fedd68ef58a3b94e67ef1d5f0d4887d64f0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
+    sha256 cellar: :any_skip_relocation, catalina:      "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
+    sha256 cellar: :any_skip_relocation, mojave:        "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f72387bb319b260214648632cd847356ca753e0193d8c7a0ce3f53ac5eed3f86"
   end
 
   depends_on "libyaml"
