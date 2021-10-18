a3e94fc87ac7022e602809e2e14ff08cf1d9ae53
Bevan Kay <email@bevankay.me>
2021-10-19 09:39:34 +1100

    libmagic 5.41

--- Formula/libmagic.rb
+++ Formula/libmagic.rb
@@ -1,8 +1,8 @@
 class Libmagic < Formula
   desc "Implementation of the file(1) command"
   homepage "https://www.darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.40.tar.gz"
-  sha256 "167321f43c148a553f68a0ea7f579821ef3b11c27b8cbe158e4df897e4a5dd57"
+  url "https://astron.com/pub/file/file-5.41.tar.gz"
+  sha256 "13e532c7b364f7d57e23dfeea3147103150cb90593a57af86c10e4f6e411603f"
   # libmagic has a BSD-2-Clause-like license
   license :cannot_represent
 
