b188341c2af893edb0ce85a69aeed7236e36d4a0
Alexander Bayandin <a.bayandin@gmail.com>
2021-10-18 11:03:37 +0100

    selenium-server: renamed from selenium-server-standalone
    Closes #87489.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/selenium-server.rb
+++ Formula/selenium-server.rb
@@ -1,4 +1,4 @@
-class SeleniumServerStandalone < Formula
+class SeleniumServer < Formula
   desc "Browser automation for testing purposes"
   homepage "https://www.selenium.dev/"
   url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.0.0/selenium-server-4.0.0.jar"
@@ -10,16 +10,13 @@ class SeleniumServerStandalone < Formula
     regex(/href=.*?selenium-server[._-]v?(\d+(?:\.\d+)+)\.jar/i)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4b65fc10655486a4b59a0a1f5e850b1920a360463dbd5228a859f803a0e96eea"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4b65fc10655486a4b59a0a1f5e850b1920a360463dbd5228a859f803a0e96eea"
-    sha256 cellar: :any_skip_relocation, catalina:      "4b65fc10655486a4b59a0a1f5e850b1920a360463dbd5228a859f803a0e96eea"
-    sha256 cellar: :any_skip_relocation, mojave:        "4b65fc10655486a4b59a0a1f5e850b1920a360463dbd5228a859f803a0e96eea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8fef5d9217b4be71f5856639e23b5e8dbb8ac4b36fbe883c6f53fbcd2332e872"
-  end
-
-  depends_on "geckodriver" => :test
   depends_on "openjdk"
+
+  on_linux do
+    # We need to have any webdriver installed for testing,
+    # macOS comes with safaridriver, let's use geckodriver for linux
+    depends_on "geckodriver" => :test
+  end
 
   def install
     libexec.install "selenium-server-#{version}.jar"
