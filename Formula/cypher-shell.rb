d69167b1390cdf5ae27a54eb996323c5d0ac9866
Sean O'Neil <oneilse14@gmail.com>
2021-10-19 06:47:58 -0700

    cypher-shell 4.3.0
    * cypher-shell 4.3.0
    * cypher-shell: update livecheck
    
    Closes #87586.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cypher-shell.rb
+++ Formula/cypher-shell.rb
@@ -1,10 +1,15 @@
 class CypherShell < Formula
   desc "Command-line shell where you can execute Cypher against Neo4j"
-  homepage "https://github.com/neo4j/cypher-shell"
-  url "https://github.com/neo4j/cypher-shell/releases/download/4.2.2/cypher-shell.zip"
-  sha256 "b57623b045a252e3b46f4bb0c7db4bc5ddb080f248bb866fc27023a7c9118b91"
-  license "GPL-3.0"
+  homepage "https://neo4j.com"
+  url "https://dist.neo4j.org/cypher-shell/cypher-shell-4.3.0.zip"
+  sha256 "bd61dd4cffcfc8935bc3cf06b4d3591eda46d56d43dd0b88e494ccd518d105d1"
+  license "GPL-3.0-only"
   version_scheme 1
+
+  livecheck do
+    url "https://neo4j.com/download-center/"
+    regex(/href=.*?cypher-shell[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "d0feb61b9ddf8a28c96f029727b1ce694fe6cfce030e9ba2be96da616c001bb9"
