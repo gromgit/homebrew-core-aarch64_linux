da59e10842500871db70e49043ad706950c62f9c
Rui Chen <rui@chenrui.dev>
2021-10-18 21:08:51 -0400

    ortp 5.0.36
    * ortp 5.0.36
    * bctoolbox 5.0.36
    
    Closes #87542.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ortp.rb
+++ Formula/ortp.rb
@@ -1,8 +1,8 @@
 class Ortp < Formula
   desc "Real-time transport protocol (RTP, RFC3550) library"
   homepage "https://www.linphone.org/technical-corner/ortp"
-  url "https://gitlab.linphone.org/BC/public/ortp/-/archive/5.0.32/ortp-5.0.32.tar.bz2"
-  sha256 "1fd47e5e005bd4ad7ec73c29536a855868999b72899d6e40548ff7b2d4ae24ea"
+  url "https://gitlab.linphone.org/BC/public/ortp/-/archive/5.0.36/ortp-5.0.36.tar.bz2"
+  sha256 "322e6921d882e554eecb34a0ca11ec5b7065aa74453ef07cdb480a26b3c5b650"
   license "GPL-3.0-or-later"
   head "https://gitlab.linphone.org/BC/public/ortp.git", branch: "master"
 
@@ -20,8 +20,8 @@ class Ortp < Formula
   # bctoolbox appears to follow ortp's version. This can be verified at the GitHub mirror:
   # https://github.com/BelledonneCommunications/bctoolbox
   resource "bctoolbox" do
-    url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.0.32/bctoolbox-5.0.32.tar.bz2"
-    sha256 "a4090af26c50a2ace5409ae1af4f1dbb0b68e2dd89deffa9af844bb15160eb7e"
+    url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.0.36/bctoolbox-5.0.36.tar.bz2"
+    sha256 "766ab4f804ced6af56bb3dac49aee717a91e2ec6d4c402c26bf35958995b8a0e"
   end
 
   def install
