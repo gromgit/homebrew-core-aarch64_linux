6f79b1998b1f64877e0e1e05b080ea1fc4aa9b14
curoky <cccuroky@gmail.com>
2021-10-19 11:23:26 +0800

    iproute2 5.14.0 (new formula)
    * iproute2 5.11.0 (new formula)
    * iproute2: modify the parameter order
    * iproute: update license
    * iproute2: update homepage/url/head base on readme
    
    Closes #87554.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/iproute2.rb
@@ -0,0 +1,29 @@
+class Iproute2 < Formula
+  desc "Linux routing utilities"
+  homepage "https://wiki.linuxfoundation.org/networking/iproute2"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.14.0.tar.xz"
+  sha256 "210fa785a52f3763c4287fd5ae63e246f6311bfaa48c424baab6d383bb7591d4"
+  license "GPL-2.0-only"
+  head "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git", branch: "main"
+
+  depends_on "bison" => :build
+  depends_on "flex" => :build
+  depends_on :linux
+
+  def install
+    system "make", "install",
+           "PREFIX=#{prefix}",
+           "SBINDIR=#{sbin}",
+           "CONFDIR=#{etc}/iproute2",
+           "NETNS_RUN_DIR=#{var}/run/netns",
+           "NETNS_ETC_DIR=#{etc}/netns",
+           "ARPDDIR=#{var}/lib/arpd",
+           "KERNEL_INCLUDE=#{include}",
+           "DBM_INCLUDE=#{include}"
+  end
+
+  test do
+    output = shell_output("#{sbin}/ip addr").strip
+    assert_match "link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", output
+  end
+end
