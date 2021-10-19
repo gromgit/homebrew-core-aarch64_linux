f095c46b50fdede2614de2bfd38f9c56f7bc4881
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-19 06:08:35 +0000

    stgit: update 1.3_1 bottle.

--- Formula/stgit.rb
+++ Formula/stgit.rb
@@ -8,11 +8,11 @@ class Stgit < Formula
   head "https://github.com/stacked-git/stgit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e3bdca3fa2b8dfd10dd3e89dd08aa7c5760d7fb9159c1abecbb56b53c9241731"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ae08af52ebd73ea02f596294454b6bda5c9efc360e2b03c9806d0f508f4a5282"
-    sha256 cellar: :any_skip_relocation, catalina:      "ae08af52ebd73ea02f596294454b6bda5c9efc360e2b03c9806d0f508f4a5282"
-    sha256 cellar: :any_skip_relocation, mojave:        "ae08af52ebd73ea02f596294454b6bda5c9efc360e2b03c9806d0f508f4a5282"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3bdca3fa2b8dfd10dd3e89dd08aa7c5760d7fb9159c1abecbb56b53c9241731"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cb37491ac4052c6153e35a1809ea7e54b3a31385112394dde1e56b40568bd720"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9232c43d2b1b93f7a39a292e2fa8c7f4dca5cac9516f1af5b4920728839bfcdb"
+    sha256 cellar: :any_skip_relocation, catalina:      "9232c43d2b1b93f7a39a292e2fa8c7f4dca5cac9516f1af5b4920728839bfcdb"
+    sha256 cellar: :any_skip_relocation, mojave:        "9232c43d2b1b93f7a39a292e2fa8c7f4dca5cac9516f1af5b4920728839bfcdb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb37491ac4052c6153e35a1809ea7e54b3a31385112394dde1e56b40568bd720"
   end
 
   depends_on "asciidoc" => :build
