6ac761cc32ac07f7a7132fc1cd9a0cc4f1cfc58f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-20 01:30:46 +0000

    locust: update 2.4.1 bottle.

--- Formula/locust.rb
+++ Formula/locust.rb
@@ -8,11 +8,11 @@ class Locust < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8363dfb4490d485d15cbd2df2d511c42c0b41f5dbd8933d33f1a0afa7cd279aa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9b436bd3fbfe79ea366f5e780d55594141f9c1abff34eb209917f9aaa2fff080"
-    sha256 cellar: :any_skip_relocation, catalina:      "aefc34751a171cad193e55ee2491d1ae6b2088457cf54931f9f2d917afe46a27"
-    sha256 cellar: :any_skip_relocation, mojave:        "8030d665608a03236aa6aa34d3a81491ff15f5d885b0c0f395b2b58103fbbebe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "43454fbf053b738b9b3bf97beaff456678d3eb5d7daede16867004674d85ed09"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ac57c1799afb0c41f6df232eb2e82cb8a9db3c3f258773ad02c0cb076f92b13c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "91edaa66f7dbdd9e57c6e5540a04bc4e774c207d8723e8437fbef437b0951724"
+    sha256 cellar: :any_skip_relocation, catalina:      "f557b36490eee9511456fab780281324f08824e996755d0b5afc7417a7a54cbb"
+    sha256 cellar: :any_skip_relocation, mojave:        "79067b00653f3e3787b7161c2bc9f972d20f29dcd3749233acb342175acb3979"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c6389671753427165647a1f4f0394472f50d5ca11e81476f8fbd1f14d79796d"
   end
 
   depends_on "python@3.10"
