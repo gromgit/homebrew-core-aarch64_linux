46ce3a94a013019aa09a3f50d61bf6a8791e785b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-20 01:30:44 +0000

    gopls: update 0.7.3 bottle.

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -12,11 +12,11 @@ class Gopls < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d3da9c6aedca9fd37c9022c8a7545a00e4d63ff33a9aaf6806bbd30ba4fc4724"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7217383324ca5d77ade1972e8357377bc2526913f7b2da3fd5a7021b649bb799"
-    sha256 cellar: :any_skip_relocation, catalina:      "9d61076699f6cdf086bcc8104fc355f87158799261739956d85960994c6b028d"
-    sha256 cellar: :any_skip_relocation, mojave:        "1e771496411197bac6386a0e6977e2cdbb6d5655a62e5e7126a0c3e6bd1b6496"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dcf8e757e34afba03062e8d719576ec14fb5f4a0a19fbcd868babb130d429656"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "13d75c3c170341c89c5d4f20ff2529142930aa8b4b8b2ec15e68aa3ad9d686ad"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ab84294c3148d2f0ebb6d3e5d06673e19303041f9c5d8f3866e4c06d1167f9f7"
+    sha256 cellar: :any_skip_relocation, catalina:      "8379dd0c00228c617f6dac6de60feb0a2962d1bfffd06d9f03c4ef66e191cce9"
+    sha256 cellar: :any_skip_relocation, mojave:        "e1b4b5f5688554660711ebb64f2d98fda1ffd9a1f6eee86a14e6472eaea53101"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02392419baea5991aa5a139c1f6f59f8609ea4095495b0a026deea6aba65a40f"
   end
 
   depends_on "go" => :build
