59618c9027b6a4908e1f88662954670708d6abce
Jesper Broge Jørgensen <jbj@cego.dk>
2021-10-20 13:24:09 +0200

    gitlab-ci-local 4.25.1 (new formula)
    * gitlab-ci-local 4.25.1
    * gitlab-ci-local assert test output
    
    Closes #87640.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gitlab-ci-local.rb
@@ -0,0 +1,69 @@
+require "language/node"
+
+class GitlabCiLocal < Formula
+  desc "Run gitlab pipelines locally as shell executor or docker executor"
+  homepage "https://github.com/firecow/gitlab-ci-local"
+  url "https://registry.npmjs.org/gitlab-ci-local/-/gitlab-ci-local-4.25.1.tgz"
+  sha256 "c920b068ab98eb3693133e418ab707f21c9b6af074b9817245ddf64d391f01ef"
+  license "MIT"
+  head "https://github.com/firecow/gitlab-ci-local.git", branch: "master"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/".gitlab-ci.yml").write <<~YML
+      ---
+      stages:
+        - build
+        - tag
+      variables:
+        HELLO: world
+      build:
+        stage: build
+        needs: []
+        tags:
+          - shared-docker
+        script:
+          - echo "HELLO"
+      tag-docker-image:
+        stage: tag
+        needs: [ build ]
+        tags:
+          - shared-docker
+        script:
+          - echo $HELLO
+    YML
+
+    system "git", "init"
+    system "git", "add", ".gitlab-ci.yml"
+    system "git", "commit", "-m", "'some message'"
+    rm ".git/config"
+
+    (testpath/".git/config").write <<~EOS
+      [core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+        ignorecase = true
+        precomposeunicode = true
+      [remote "origin"]
+        url = git@github.com:firecow/gitlab-ci-local.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+      [branch "master"]
+        remote = origin
+        merge = refs/heads/master
+    EOS
+
+    assert_equal shell_output("#{bin}/gitlab-ci-local --list"), <<~OUTPUT
+      name              description  stage  when        allow_failure  needs
+      build                          build  on_success  false          []
+      tag-docker-image               tag    on_success  false          [build]
+    OUTPUT
+  end
+end
