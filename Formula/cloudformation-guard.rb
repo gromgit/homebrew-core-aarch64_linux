679a23a18b50a155e3ac8b78b2fe08f8950a4f50
rui <rui@chenrui.dev>
2021-10-21 22:35:12 +0000

    cloudformation-guard: update 2.0.3 bottle.

--- Formula/cloudformation-guard.rb
+++ Formula/cloudformation-guard.rb
@@ -6,11 +6,12 @@ class CloudformationGuard < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "68ceb64498c4b499e1c5b9d8855a884a0ac4f389476d5bf6da0f151378c1ad4c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f8a125c4de3fc9e921df68c0ac957b785de73ea8e815d1fc0ea2cced5dfef881"
-    sha256 cellar: :any_skip_relocation, catalina:      "36b146ca21768e8aaef9c1b10d291488f5a776dabdb02a690107ab1d8383ce67"
-    sha256 cellar: :any_skip_relocation, mojave:        "c7e5b37de4696d225b5c550d2a82e9f48f1f726fce5d7af531a2d38f55eaad3e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aceecc8eed7f44f26787420ded0984f8cd18ce01a90ce15b3878c00ba4af71f0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "896fe48c051f1d132121090c0058cef528ee3e888e1f2baeaa673be23a123b7f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68ceb64498c4b499e1c5b9d8855a884a0ac4f389476d5bf6da0f151378c1ad4c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f8a125c4de3fc9e921df68c0ac957b785de73ea8e815d1fc0ea2cced5dfef881"
+    sha256 cellar: :any_skip_relocation, catalina:       "36b146ca21768e8aaef9c1b10d291488f5a776dabdb02a690107ab1d8383ce67"
+    sha256 cellar: :any_skip_relocation, mojave:         "c7e5b37de4696d225b5c550d2a82e9f48f1f726fce5d7af531a2d38f55eaad3e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aceecc8eed7f44f26787420ded0984f8cd18ce01a90ce15b3878c00ba4af71f0"
   end
 
   depends_on "rust" => :build
