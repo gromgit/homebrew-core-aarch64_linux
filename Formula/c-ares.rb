8ffdb643674c0e023d265f884e88c16b874727e6
Bo Anderson <mail@boanderson.me>
2021-10-21 20:21:50 +0000

    c-ares: update 1.17.2 bottle.

--- Formula/c-ares.rb
+++ Formula/c-ares.rb
@@ -16,11 +16,12 @@ class CAres < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "faf4361fe875f4b4d9fa521c3aed53ae6ad1935a859dec0b7cfd4638c6841a82"
-    sha256 cellar: :any,                 big_sur:       "999647263cf8819d6fd324ce9bf48ea5eaa94b34f7796c1fe3d772572f361459"
-    sha256 cellar: :any,                 catalina:      "8cf15891ac55f5d9d7a28a5122e7c5ee6c9585c82643b029ee5c295bfd408209"
-    sha256 cellar: :any,                 mojave:        "60adc74ad87d834ff201feef8a25c7e27b8ae8e3d1d09f71b08f41384cf994e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c23f4ffa78d6eb7595ffd15c22067ef5ecb8370fbbdeb4cef0f7e178e6a34e3b"
+    sha256 cellar: :any,                 arm64_monterey: "7781f0e1ba65cf1876352302a4c609cf09a00f0cdc65560b6306e52ade04ce43"
+    sha256 cellar: :any,                 arm64_big_sur:  "faf4361fe875f4b4d9fa521c3aed53ae6ad1935a859dec0b7cfd4638c6841a82"
+    sha256 cellar: :any,                 big_sur:        "999647263cf8819d6fd324ce9bf48ea5eaa94b34f7796c1fe3d772572f361459"
+    sha256 cellar: :any,                 catalina:       "8cf15891ac55f5d9d7a28a5122e7c5ee6c9585c82643b029ee5c295bfd408209"
+    sha256 cellar: :any,                 mojave:         "60adc74ad87d834ff201feef8a25c7e27b8ae8e3d1d09f71b08f41384cf994e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c23f4ffa78d6eb7595ffd15c22067ef5ecb8370fbbdeb4cef0f7e178e6a34e3b"
   end
 
   depends_on "cmake" => :build
