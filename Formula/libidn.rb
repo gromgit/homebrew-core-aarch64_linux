1c174c62f1cb6e297454227af31595a858129af3
Bo Anderson <mail@boanderson.me>
2021-10-21 21:17:43 +0000

    libidn: update 1.38 bottle.

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -9,6 +9,7 @@ class Libidn < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "b36c5a702e23a79b713fa8ef1cde2f45f3547507146928d25649e34704b89810"
     sha256 cellar: :any, arm64_big_sur:  "7b3adb7115f8e786852ce270f633f726525f0017ce2f44b99b1b05c42c672c25"
+    sha256 cellar: :any, monterey:       "97ae6e40039b1abaa15ea01851a407c6330ece0687f436fcac14f2ed1ed7bfee"
     sha256 cellar: :any, big_sur:        "edf0fcd338457708fb6ee986f85c380b744d7ea7b9e03df1bec892959d2a9e0d"
     sha256 cellar: :any, catalina:       "ce069c2b604d6dd0194541bc510a43d0e1eecacd816dfa4d2f537dde7de3cd5f"
     sha256 cellar: :any, mojave:         "1e5c4afc066727df497031e2cefbd012fc7b5b4e4cee793cd4b84f68bf9ff3de"
