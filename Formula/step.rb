66ff9e24adec43569c1eb7658115f97930356dcb
rui <rui@chenrui.dev>
2021-10-21 18:32:05 +0000

    step: update 0.17.7 bottle.

--- Formula/step.rb
+++ Formula/step.rb
@@ -6,11 +6,11 @@ class Step < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7749ded5fdbff3ddd3587611f8222fab4410ef85ed785e6e0bdde1dae13658a3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5f824b0604b7b3abf7ff503b95b39779078209e6d1fcd57b7f5646c1ecc28847"
-    sha256 cellar: :any_skip_relocation, catalina:      "41545598502ad9e2cb27eb8b5226f835cefc8ab1d3f60d403fd2dd00394e335e"
-    sha256 cellar: :any_skip_relocation, mojave:        "08bf1064bc52177082c49d2e16ec220ce626759e4f3614368f229b1983cc6f80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "937441b8dcd95b2965f12912997291d3097df8fca0723e1d9976a5ead6af03d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "96d78821f7357d3fa2e20559bfc60720610187dad37d36a34fd034cf5b3486fd"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f72fe0d16a703212f9005a8f3c0b1657f25d49c912e751b621cbbc5e3c2e8720"
+    sha256 cellar: :any_skip_relocation, catalina:      "b5f6993aa83927c294ed34b9062b580420c924d06b4fb1efbb7e7a1b7913087b"
+    sha256 cellar: :any_skip_relocation, mojave:        "2779cca9f4814178321e75769bfcbc8a3ff4ead2253c1a25f0588e6552ff76c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05ec347965de986ce07bc941ef37c35440564f1906ce23a94609058565dee463"
   end
 
   depends_on "go" => :build
