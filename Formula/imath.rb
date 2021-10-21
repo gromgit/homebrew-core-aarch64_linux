d160556d6b70ab458f37faffa474177256026737
Bo Anderson <mail@boanderson.me>
2021-10-21 21:05:49 +0000

    imath: update 3.1.3 bottle.

--- Formula/imath.rb
+++ Formula/imath.rb
@@ -8,6 +8,7 @@ class Imath < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "8932b50d2bd8e5e650b8b4076f62840b640f0440462f05f8068a2aa52333f2d0"
     sha256 cellar: :any,                 arm64_big_sur:  "2a2e81873d2de90b295234a6d639d8c61c375253c739328f54856477dad5193e"
+    sha256 cellar: :any,                 monterey:       "5a09244b12f24ded2ab4056d302ecf30dacb0b06a0f45c2817cf56ed70da64ec"
     sha256 cellar: :any,                 big_sur:        "0a0b0dfa316ca1c7b122de50f6a3bb8d5f2827056c2981d8b69f49d63bc6fe47"
     sha256 cellar: :any,                 catalina:       "485012850077bef28aa6116a27868877c5c4e2b133081cae290804efd0bb7667"
     sha256 cellar: :any,                 mojave:         "ae767801f200cee72351c40a60bcb48a9441870c5d2d7c4da844b6da44057d28"
