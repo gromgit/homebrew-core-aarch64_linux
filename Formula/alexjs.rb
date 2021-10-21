0f997021962ad45be56d6529b29a693e8a38f0ec
rui <rui@chenrui.dev>
2021-10-21 05:15:30 +0000

    alexjs: update 10.0.0 bottle.

--- Formula/alexjs.rb
+++ Formula/alexjs.rb
@@ -8,12 +8,11 @@ class Alexjs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1869fa035c9ce8179295d3cd697c8abfbc72a5fefc81d12ce0c047bf4db64eae"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1102acddac4146addf4d4b59ac66bf5d5d57b2badabfe9038c286ab5660c63f6"
-    sha256 cellar: :any_skip_relocation, catalina:      "e37814ad3315475e5484cc5c334089213a023ea443ccb2207702b8667f3e1a0e"
-    sha256 cellar: :any_skip_relocation, mojave:        "7c2d9fedf79264bd59e73b7f062512e03afdd1e6a66152c233996471636549f1"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "462578faccc1fd6ba5df072a50c90c28522067e6cbba84a3b6a6dfa29c7a2eaf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f68677cdd5368c5da400dd06c99652dd90a30a0973d0cb4fc80fdadc994a5ea7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "970a9f5d0d22e0934ebe287c7d64c0e79a745a139dcbaf7ce6e643ee84184b43"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a616902bfd0d0d1205eca973d30ad3657246e2d88dff8a6f5c7e98c70375b60d"
+    sha256 cellar: :any_skip_relocation, catalina:      "a616902bfd0d0d1205eca973d30ad3657246e2d88dff8a6f5c7e98c70375b60d"
+    sha256 cellar: :any_skip_relocation, mojave:        "a616902bfd0d0d1205eca973d30ad3657246e2d88dff8a6f5c7e98c70375b60d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "970a9f5d0d22e0934ebe287c7d64c0e79a745a139dcbaf7ce6e643ee84184b43"
   end
 
   depends_on "node"
