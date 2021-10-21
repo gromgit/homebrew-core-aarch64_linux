3e9f9ca0ff41cd3bf4ab6798e9ccfaccdd2e054c
Bo Anderson <mail@boanderson.me>
2021-10-21 20:10:06 +0100

    xz: update 5.2.5 bottle.

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -16,6 +16,7 @@ class Xz < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "fcda3e81efe284f7e07effcb4ba03a87c8d828833351ac3f41e1e808e7753b0a"
     sha256 cellar: :any, arm64_big_sur:  "c84206005787304416ed81094bd3a0cdd2ae8eb62649db5a3a44fa14b276d09f"
+    sha256 cellar: :any, monterey:       "099055bb0afb3dfd454e72ce2228a0fad54e90ef63577e33d18b9c92a444317a"
     sha256 cellar: :any, big_sur:        "4fbd4a9e3eb49c27e83bd125b0e76d386c0e12ae1139d4dc9e31841fb8880a35"
     sha256 cellar: :any, catalina:       "2dcc8e0121c934d1e34ffdb37fcd70f0f7b5c2f4755f2f7cbcf360e9e54cb43b"
     sha256 cellar: :any, mojave:         "44483961b5d2b535b0ece1936c9d40b4bc7d9c7281646cca0fb476291ab9d4dc"
