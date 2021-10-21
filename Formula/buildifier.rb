646c65bf4194cb8bee4196ba573bb4f2bd06cfff
Bo Anderson <mail@boanderson.me>
2021-10-21 23:53:43 +0100

    buildifier: update 4.2.2 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -9,6 +9,7 @@ class Buildifier < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6bc3fa86c88b0d80a1539ae4f99f5e77b50fa6b4093dfea55ebdd7c3a5ed5b5f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30cc17e7dbbdb450b5b7c914bbe77635c99c6bd0cc106b2796233c085787c525"
+    sha256 cellar: :any_skip_relocation, monterey:       "3484eb0afc3b1dfc9cfe44a44c1019c6726198dc822cb9d1b93c73f137b0b734"
     sha256 cellar: :any_skip_relocation, big_sur:        "9f6f2318368bb64129a35dbdb151b215f6e426fedcca5c17d02f633dd147b45a"
     sha256 cellar: :any_skip_relocation, catalina:       "9f6f2318368bb64129a35dbdb151b215f6e426fedcca5c17d02f633dd147b45a"
     sha256 cellar: :any_skip_relocation, mojave:         "9f6f2318368bb64129a35dbdb151b215f6e426fedcca5c17d02f633dd147b45a"
