9ddf819c65a29f791521d0b2c068cc19918a7208
Bo Anderson <mail@boanderson.me>
2021-10-21 20:56:10 +0000

    fribidi: update 1.0.11 bottle.

--- Formula/fribidi.rb
+++ Formula/fribidi.rb
@@ -6,11 +6,12 @@ class Fribidi < Formula
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d80b767910f47f85b6fefc38a2742d520396f148b8290614b16328704f7c3bf4"
-    sha256 cellar: :any,                 big_sur:       "434c488a27dca39fa4fab0644cb9b2f495ea4f839eef63587d0de715a93a6f12"
-    sha256 cellar: :any,                 catalina:      "9ae1580fef75c9d665f5723200d7987b07674c452e2c236dae33e12a8cf16324"
-    sha256 cellar: :any,                 mojave:        "3ef3b5f32b31fad3fb8dc39a559b24abb04cf46c9b29303285eb160d1f4ed19c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f82a49f101fe666c5085302138ec5899e51c9346733149f4d872a9c468b7dcbe"
+    sha256 cellar: :any,                 arm64_monterey: "e7c104d645d36d758001c381266aaa20ac40e0fc1591c02ae8d2310e8ca21109"
+    sha256 cellar: :any,                 arm64_big_sur:  "d80b767910f47f85b6fefc38a2742d520396f148b8290614b16328704f7c3bf4"
+    sha256 cellar: :any,                 big_sur:        "434c488a27dca39fa4fab0644cb9b2f495ea4f839eef63587d0de715a93a6f12"
+    sha256 cellar: :any,                 catalina:       "9ae1580fef75c9d665f5723200d7987b07674c452e2c236dae33e12a8cf16324"
+    sha256 cellar: :any,                 mojave:         "3ef3b5f32b31fad3fb8dc39a559b24abb04cf46c9b29303285eb160d1f4ed19c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f82a49f101fe666c5085302138ec5899e51c9346733149f4d872a9c468b7dcbe"
   end
 
   def install
