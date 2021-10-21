d1328461dabfec8f96450c6cd7c5fe49f1289ec1
rui <rui@chenrui.dev>
2021-10-21 22:08:52 +0000

    terragrunt: update 0.35.4 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -6,11 +6,12 @@ class Terragrunt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f37f43ba483f6d88a00b3542c425c685bafbb0976ad333ccaa261e3ca1a65700"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e65387285dc98bd882d288a743d49246300f9c42afc2562d1c88bee0c11f27ed"
-    sha256 cellar: :any_skip_relocation, catalina:      "afad6878ef5cc58bc106196850ac972aefc0397f72685bcdedfe83ea460083f0"
-    sha256 cellar: :any_skip_relocation, mojave:        "9144da54ff3014698c494951beab6dfd3d7508af9b4653aaaf559239cc7eab30"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "600227492ecbd37e0e6d5f6780b64cd5f7e5da607fd3f69b56d8227c71b42fd2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5f70a735317b84e80544db228a0c21dab580e79b765d097493063abef2b2a97d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f37f43ba483f6d88a00b3542c425c685bafbb0976ad333ccaa261e3ca1a65700"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e65387285dc98bd882d288a743d49246300f9c42afc2562d1c88bee0c11f27ed"
+    sha256 cellar: :any_skip_relocation, catalina:       "afad6878ef5cc58bc106196850ac972aefc0397f72685bcdedfe83ea460083f0"
+    sha256 cellar: :any_skip_relocation, mojave:         "9144da54ff3014698c494951beab6dfd3d7508af9b4653aaaf559239cc7eab30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "600227492ecbd37e0e6d5f6780b64cd5f7e5da607fd3f69b56d8227c71b42fd2"
   end
 
   depends_on "go" => :build
