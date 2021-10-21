aa82480f012ea823631abeb74b2637ba84771e0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-21 16:19:33 +0000

    whatmp3: update 3.8_5 bottle.

--- Formula/whatmp3.rb
+++ Formula/whatmp3.rb
@@ -10,12 +10,7 @@ class Whatmp3 < Formula
   head "https://github.com/RecursiveForest/whatmp3.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2508dc07fce36d65efad6e315157faf498789a43e0d8f9eb595ea8122250d6bb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "68a5f39de733c7d95679ac3e9c91e9edd543eb9450592a517b2491dd7089641b"
-    sha256 cellar: :any_skip_relocation, catalina:      "d449a8bb1339bbc02f27b930a08c21f2acfdbbb49f45dd43ea4015054607244f"
-    sha256 cellar: :any_skip_relocation, mojave:        "73e35194bc0eed4cab900ae2436ca67283bddc942d73a77f84e3aa02cf2e518a"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "87e78b789996ff11ea55d9224896d581d0aff8a6ad10e14e456588903cf592cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d96b8eef9263818c2f0c41793b96f38828a2837390c90cef40f5d8845a65194c"
+    sha256 cellar: :any_skip_relocation, all: "e2d016997747d101763e515bb9a223090672b2914340c05bd4fb71618af491cc"
   end
 
   depends_on "flac"
