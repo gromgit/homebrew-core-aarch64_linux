f8f52f173ffa371626a5ab48566525e56a52a5a3
Bevan Kay <email@bevankay.me>
2021-10-21 11:14:10 +1100

    geos 3.10.0
    * geos 3.10.0
    * geos: improve test
    
    Closes #87669.
    
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/geos.rb
+++ Formula/geos.rb
@@ -1,9 +1,9 @@
 class Geos < Formula
   desc "Geometry Engine"
   homepage "https://trac.osgeo.org/geos"
-  url "https://download.osgeo.org/geos/geos-3.9.1.tar.bz2"
-  sha256 "7e630507dcac9dc07565d249a26f06a15c9f5b0c52dd29129a0e3d381d7e382a"
-  license "LGPL-2.1"
+  url "https://download.osgeo.org/geos/geos-3.10.0.tar.bz2"
+  sha256 "097d70e3c8f688e59633ceb8d38ad5c9b0d7ead5729adeb925dbc489437abe13"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url "https://download.osgeo.org/geos/"
@@ -18,23 +18,43 @@ class Geos < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "88c1238afa2af41abed21be1069ce32fb5732d3b64317cb96087bdf4c23cbf15"
   end
 
-  depends_on "swig" => :build
-  depends_on "python@3.9"
+  depends_on "cmake" => :build
 
   def install
-    args = %W[
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --enable-python
-      PYTHON=#{Formula["python@3.9"].opt_bin}/python3
-    ]
-    args << "--disable-inline" if Hardware::CPU.arm?
-
-    system "./configure", *args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    system "#{bin}/geos-config", "--libs"
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdarg.h>
+      #include <geos_c.h>
+      static void geos_message_handler(const char* fmt, ...) {
+          va_list ap;
+          va_start(ap, fmt);
+          vprintf (fmt, ap);
+          va_end(ap);
+      }
+      int main() {
+          initGEOS(geos_message_handler, geos_message_handler);
+          const char* wkt_a = "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))";
+          const char* wkt_b = "POLYGON((5 5, 15 5, 15 15, 5 15, 5 5))";
+          GEOSWKTReader* reader = GEOSWKTReader_create();
+          GEOSGeometry* geom_a = GEOSWKTReader_read(reader, wkt_a);
+          GEOSGeometry* geom_b = GEOSWKTReader_read(reader, wkt_b);
+          GEOSGeometry* inter = GEOSIntersection(geom_a, geom_b);
+          GEOSWKTWriter* writer = GEOSWKTWriter_create();
+          GEOSWKTWriter_setTrim(writer, 1);
+          char* wkt_inter = GEOSWKTWriter_write(writer, inter);
+          printf("Intersection(A, B): %s\\n", wkt_inter);
+          return 0;
+      }
+    EOS
+    cflags = shell_output("#{bin}/geos-config --cflags").split
+    libs = shell_output("#{bin}/geos-config --clibs").split
+    system ENV.cc, *cflags, "test.c", *libs
+    assert_match "POLYGON ((10 10, 10 5, 5 5, 5 10, 10 10))", shell_output("./a.out")
   end
 end
