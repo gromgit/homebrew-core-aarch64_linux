b0697e23e8c9510453745694cde828a4967fabd9
rui <rui@chenrui.dev>
2021-10-21 22:11:17 +0000

    tfsec: update 0.58.14 bottle.

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -12,11 +12,12 @@ class Tfsec < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4ce616515955602888c219c8393571e1a3f353ee728d84292edf050198bd9786"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fd870c2295481d4185e20302da68bb9ba7a210fba9fb1583f94759ad42b864f7"
-    sha256 cellar: :any_skip_relocation, catalina:      "520309c5407048736ef13bf7abad8a6b46c6ee462e0b417ab48436290ab3afe4"
-    sha256 cellar: :any_skip_relocation, mojave:        "3bc49accf1843bf7ec4a2837c8a45bc2891d9a85b0b0b2a549dfbeffa178f656"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf6d12881ff0dd427a43ea0aaa9a6547c6f6c6a857f4e8649fdc9bae724a3808"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36a7b5eed63bdf40d87e89043a7ef2523430928850ce869124f387fefae7c1de"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ce616515955602888c219c8393571e1a3f353ee728d84292edf050198bd9786"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd870c2295481d4185e20302da68bb9ba7a210fba9fb1583f94759ad42b864f7"
+    sha256 cellar: :any_skip_relocation, catalina:       "520309c5407048736ef13bf7abad8a6b46c6ee462e0b417ab48436290ab3afe4"
+    sha256 cellar: :any_skip_relocation, mojave:         "3bc49accf1843bf7ec4a2837c8a45bc2891d9a85b0b0b2a549dfbeffa178f656"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf6d12881ff0dd427a43ea0aaa9a6547c6f6c6a857f4e8649fdc9bae724a3808"
   end
 
   depends_on "go" => :build
