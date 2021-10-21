39e4769f1797b4b61e6978d84edeed86204f62df
Bo Anderson <mail@boanderson.me>
2021-10-21 19:36:05 +0000

    jpeg: update 9d bottle.

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -14,6 +14,7 @@ class Jpeg < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "ea3b46eb66c711e99ccbab7f9b821efeecbc737ab07e1104e37eafc0bdba8a5f"
     sha256 cellar: :any,                 arm64_big_sur:  "e511143cd72a76572dabe52cef0034996b2ed24334f1b3466ba339746230e37b"
+    sha256 cellar: :any,                 monterey:       "1a67c415e4636c77057bf80bf37ea3978e4888064a8b7fd04c0cf71a24f32cee"
     sha256 cellar: :any,                 big_sur:        "c565929a4901365a3408b57275802f943625c1e29e1b48a186edd2e97d8c0bdb"
     sha256 cellar: :any,                 catalina:       "8f7b82a952fb3937889c7f22da1403e5338cd320495917eb26b0c5b2e614791c"
     sha256 cellar: :any,                 mojave:         "b931e7725c83275c56f962b51b83c10f31a01ac8d823c6722edaf16d893970b1"
