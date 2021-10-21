960c430999987df88dca3fedc5d0d9c2707ca2eb
Rui Chen <rui@chenrui.dev>
2021-10-21 15:10:06 -0400

    govc 0.27.1
    Closes #87717.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -1,8 +1,8 @@
 class Govc < Formula
   desc "Command-line tool for VMware vSphere"
   homepage "https://github.com/vmware/govmomi/tree/master/govc"
-  url "https://github.com/vmware/govmomi/archive/v0.27.0.tar.gz"
-  sha256 "0d2c7764ce924860c326eb92f58ca0fc48f1e39a9c71315a37187bdd0550517b"
+  url "https://github.com/vmware/govmomi/archive/v0.27.1.tar.gz"
+  sha256 "8b5f0748b6bbbd1088950769bb82c44431478a9e80f729ab791fff9862d89a09"
   license "Apache-2.0"
 
   bottle do
