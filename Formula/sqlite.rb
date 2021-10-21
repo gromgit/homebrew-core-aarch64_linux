593fc3e38428aa45c61302e5fdefed4a08e137a7
Bo Anderson <mail@boanderson.me>
2021-10-21 19:55:46 +0000

    sqlite: update 3.36.0 bottle.

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -15,11 +15,12 @@ class Sqlite < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b7d98dc89e39bfd68676ce9df2e58d78e790a2591ef204800b27a454c019024a"
-    sha256 cellar: :any,                 big_sur:       "2c9c5f05c16c1fa972fc49c4b2705d7877b99640b6bb0b3908333e17f63dd71b"
-    sha256 cellar: :any,                 catalina:      "3665b75356219a7823309efad0304b40bbccc369576685e8faef280a539e8600"
-    sha256 cellar: :any,                 mojave:        "f96b12f3fe9b69283cb786aee0d034370cb2407a076f58f20e19de08745d58c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b8faf56c6f089b2538a86678652af07da8eb0db8000e684e875c1ef12be011e"
+    sha256 cellar: :any,                 arm64_monterey: "87acc578c27c3e9b21013e1186cf95e9de830886f8dbbb34bf22ca1fe3ec460a"
+    sha256 cellar: :any,                 arm64_big_sur:  "b7d98dc89e39bfd68676ce9df2e58d78e790a2591ef204800b27a454c019024a"
+    sha256 cellar: :any,                 big_sur:        "2c9c5f05c16c1fa972fc49c4b2705d7877b99640b6bb0b3908333e17f63dd71b"
+    sha256 cellar: :any,                 catalina:       "3665b75356219a7823309efad0304b40bbccc369576685e8faef280a539e8600"
+    sha256 cellar: :any,                 mojave:         "f96b12f3fe9b69283cb786aee0d034370cb2407a076f58f20e19de08745d58c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1b8faf56c6f089b2538a86678652af07da8eb0db8000e684e875c1ef12be011e"
   end
 
   keg_only :provided_by_macos
