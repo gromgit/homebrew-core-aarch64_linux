d2fe160d0499d38d7cb55b33e9939fa564702602
Bo Anderson <mail@boanderson.me>
2021-10-21 21:06:11 +0000

    openexr: update 3.1.2 bottle.

--- Formula/openexr.rb
+++ Formula/openexr.rb
@@ -7,11 +7,12 @@ class Openexr < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ab723009a94026ae725e87542aebd00bc69992ac3f672ff6960930eb158819a1"
-    sha256 cellar: :any,                 big_sur:       "922846fff4a126532cbba994521b8768f3ad9c6e4d11c175df4a51f333b8da87"
-    sha256 cellar: :any,                 catalina:      "f9bf9db8861ba9c96ca5b76a03e986a20c4545528c7770dc1915044b7ef3c25e"
-    sha256 cellar: :any,                 mojave:        "e2ab720e00d56143302a0c3af337e7605c8e2768cce9d5dadba3d28dfa28b904"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1472cf78e5388c21d9ecb77f5f3998b66ebf701b1b8fea8119072510ce0fb7ce"
+    sha256 cellar: :any,                 arm64_monterey: "fced64362706423a154f34267febde96ae7b9400f2461928572e36bbc0a32ba4"
+    sha256 cellar: :any,                 arm64_big_sur:  "ab723009a94026ae725e87542aebd00bc69992ac3f672ff6960930eb158819a1"
+    sha256 cellar: :any,                 big_sur:        "922846fff4a126532cbba994521b8768f3ad9c6e4d11c175df4a51f333b8da87"
+    sha256 cellar: :any,                 catalina:       "f9bf9db8861ba9c96ca5b76a03e986a20c4545528c7770dc1915044b7ef3c25e"
+    sha256 cellar: :any,                 mojave:         "e2ab720e00d56143302a0c3af337e7605c8e2768cce9d5dadba3d28dfa28b904"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1472cf78e5388c21d9ecb77f5f3998b66ebf701b1b8fea8119072510ce0fb7ce"
   end
 
   depends_on "cmake" => :build
