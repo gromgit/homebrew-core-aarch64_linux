e7392a55f33d02f80b60bd2110cfb2d730da900a
Rui Chen <rui@chenrui.dev>
2021-10-21 15:40:57 -0400

    waypoint 0.6.1
    Closes #87726.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -1,8 +1,8 @@
 class Waypoint < Formula
   desc "Tool to build, deploy, and release any application on any platform"
   homepage "https://www.waypointproject.io/"
-  url "https://github.com/hashicorp/waypoint/archive/v0.6.0.tar.gz"
-  sha256 "c8c5f2bfe02153a3ebd5e31b8ea0b454c66a8cc27aee135d6ecc238197d44f38"
+  url "https://github.com/hashicorp/waypoint/archive/v0.6.1.tar.gz"
+  sha256 "e4b75b45511a284e02b8809a6637a56ffc8188ebb786f29c34f6a6bd7cf69165"
   license "MPL-2.0"
   head "https://github.com/hashicorp/waypoint.git", branch: "main"
 
