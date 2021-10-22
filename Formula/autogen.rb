efa459ab8d10a7b34290117aff204298dbcc3808
Bo Anderson <mail@boanderson.me>
2021-10-22 17:16:57 +0000

    autogen: update 5.18.16_1 bottle.

--- Formula/autogen.rb
+++ Formula/autogen.rb
@@ -12,12 +12,13 @@ class Autogen < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5058d3eb0e5520f98914d8d3cc37d941ff260e36d7ccb2733b2b0dd8d7026ad8"
-    sha256 big_sur:       "f648b54769e2022a5801ba90716855fee7c1266b906b8f768934bde0063c05ea"
-    sha256 catalina:      "fa3818d518a214d9798a514e90c461d3a6be2c6fc0758c85ad4ad6b134a28851"
-    sha256 mojave:        "76df021218eb1d338cb8ee2a18c04e1d120166991c94ba64055537beac0e68fb"
-    sha256 high_sierra:   "45fb9e222b8c21729659821aa5565010df9c3f347fae4bc2f0e5fc01680a2c1a"
-    sha256 x86_64_linux:  "459c36573772600aab0085300e551ecbbe224a8b036bc10c15d48db1719a5a52"
+    sha256 arm64_monterey: "dc64368c53d0eb66c1b093ccbba95d0fddaf2e9fc3053cee87a945309c0641af"
+    sha256 arm64_big_sur:  "5058d3eb0e5520f98914d8d3cc37d941ff260e36d7ccb2733b2b0dd8d7026ad8"
+    sha256 big_sur:        "f648b54769e2022a5801ba90716855fee7c1266b906b8f768934bde0063c05ea"
+    sha256 catalina:       "fa3818d518a214d9798a514e90c461d3a6be2c6fc0758c85ad4ad6b134a28851"
+    sha256 mojave:         "76df021218eb1d338cb8ee2a18c04e1d120166991c94ba64055537beac0e68fb"
+    sha256 high_sierra:    "45fb9e222b8c21729659821aa5565010df9c3f347fae4bc2f0e5fc01680a2c1a"
+    sha256 x86_64_linux:   "459c36573772600aab0085300e551ecbbe224a8b036bc10c15d48db1719a5a52"
   end
 
   depends_on "coreutils" => :build
