76e632927cc29bec74f47dd711b875e3574d0446
Bo Anderson <mail@boanderson.me>
2021-10-22 00:55:59 +0000

    ruby@2.7: update 2.7.4 bottle.

--- Formula/ruby@2.7.rb
+++ Formula/ruby@2.7.rb
@@ -11,11 +11,12 @@ class RubyAT27 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "c69d8b9a90e4ec60cbefc2dfea8bda47e4eb02102f00b527df040a1f822f2f6b"
-    sha256 big_sur:       "81b7afe251434a03c0da4654afc07bd0c2f36ecc16bc6c34847564b124167724"
-    sha256 catalina:      "f55efd7734e36bb91b3fda9d30819f9d6e9bd731ad32ea1bc775586723e1e201"
-    sha256 mojave:        "b297b0ae4295e6757efb79a9c6634a80c8fff68f5399bf45e5d179b1c25cbe8e"
-    sha256 x86_64_linux:  "4114feae001b41fe15163cfe307b241f1a9034988d745f49fb55380eb188f233"
+    sha256 arm64_monterey: "09fc265929adbd3edeb8c6ffd03c4977fe821b882348aef53516199334a90a85"
+    sha256 arm64_big_sur:  "c69d8b9a90e4ec60cbefc2dfea8bda47e4eb02102f00b527df040a1f822f2f6b"
+    sha256 big_sur:        "81b7afe251434a03c0da4654afc07bd0c2f36ecc16bc6c34847564b124167724"
+    sha256 catalina:       "f55efd7734e36bb91b3fda9d30819f9d6e9bd731ad32ea1bc775586723e1e201"
+    sha256 mojave:         "b297b0ae4295e6757efb79a9c6634a80c8fff68f5399bf45e5d179b1c25cbe8e"
+    sha256 x86_64_linux:   "4114feae001b41fe15163cfe307b241f1a9034988d745f49fb55380eb188f233"
   end
 
   keg_only :versioned_formula
