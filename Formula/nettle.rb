f4fe82d894a10aaa04fca66976b658ca26603674
Bo Anderson <mail@boanderson.me>
2021-10-22 14:05:54 +0000

    nettle: update 3.7.3 bottle.

--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -9,6 +9,7 @@ class Nettle < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "c47b2e4721e5c8347194e3dad066bf00fc6b5d28cb4082f53535dba76a1ed658"
     sha256 cellar: :any,                 arm64_big_sur:  "3cd41002e6358b07ca506ed09fb07473d61c1c2717b630f8d708c74ab5d06704"
+    sha256 cellar: :any,                 monterey:       "dccc8b4c4e02bcd485c1558ec1f01ca4c591a502b464629dc2e514add894b64b"
     sha256 cellar: :any,                 big_sur:        "80fa0a047c3d08ccb47c8820a566d187365e8ea75e0cdf903ef0653d3aa3bb67"
     sha256 cellar: :any,                 catalina:       "f1f1c41bf3dadabc748a34bba26b8771e4e36ae0815be4a83d1d317d90fa3c2e"
     sha256 cellar: :any,                 mojave:         "f7900666b8d57164a5e770008390ea3fe2c519941e3f28daba9ef91dad4e5e69"
