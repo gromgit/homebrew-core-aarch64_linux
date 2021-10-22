9a9c5353f7f73781cba12388ca5b60c63c535bf4
Bo Anderson <mail@boanderson.me>
2021-10-22 02:23:45 +0000

    gnu-tar: update 1.34 bottle.

--- Formula/gnu-tar.rb
+++ Formula/gnu-tar.rb
@@ -7,11 +7,12 @@ class GnuTar < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "660e573b78965f1d3fa9f8f7f78a72d38f7f26f77ca66e9f72fec26fe9be6c3a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a6ab3eb4a49d609f5f1dde43710b847fd827ebc03195aee052c7aeb528aa9bcc"
-    sha256 cellar: :any_skip_relocation, catalina:      "53b9fc4011ca3ca3e669aa96a95a5394ef45138b9b2d52c76c3a17fceb432229"
-    sha256 cellar: :any_skip_relocation, mojave:        "c4f9fcc7bdbb2bc5591a6650cf3bbfc1aa791e85f6d299f165a9466c235c83ae"
-    sha256                               x86_64_linux:  "f4206b84b3b5d4a8244b6cae99226877cf3e4927b149465ab44cbb1edce8b382"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "be51eda1c4fe90214822f47ff0e49b3e6cf87791890cd69bb198ef6fb9ac082d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "660e573b78965f1d3fa9f8f7f78a72d38f7f26f77ca66e9f72fec26fe9be6c3a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a6ab3eb4a49d609f5f1dde43710b847fd827ebc03195aee052c7aeb528aa9bcc"
+    sha256 cellar: :any_skip_relocation, catalina:       "53b9fc4011ca3ca3e669aa96a95a5394ef45138b9b2d52c76c3a17fceb432229"
+    sha256 cellar: :any_skip_relocation, mojave:         "c4f9fcc7bdbb2bc5591a6650cf3bbfc1aa791e85f6d299f165a9466c235c83ae"
+    sha256                               x86_64_linux:   "f4206b84b3b5d4a8244b6cae99226877cf3e4927b149465ab44cbb1edce8b382"
   end
 
   head do
