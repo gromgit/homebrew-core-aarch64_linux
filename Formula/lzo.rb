ef57da5563056845221f95cfcd3017d29669091c
Bo Anderson <mail@boanderson.me>
2021-10-22 02:04:11 +0000

    lzo: update 2.10 bottle.

--- Formula/lzo.rb
+++ Formula/lzo.rb
@@ -13,6 +13,7 @@ class Lzo < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "e16072e8ef7a8810284ccea232a7333a2b620367814b133a455217d22e89ae8e"
     sha256 cellar: :any,                 arm64_big_sur:  "76d0933f626d8a1645b559b1709396a2a6fd57dbd556d2f1f1848b5fddfcd327"
+    sha256 cellar: :any,                 monterey:       "0a20a578e6a31ebbe3c5d708af38b1c3ca5ba503612ed28a197cd326505d31dd"
     sha256 cellar: :any,                 big_sur:        "fcd3c9f7042104ca13be96fd0ec53acdc7da1480c16140441b2e66d4e7c5eb78"
     sha256 cellar: :any,                 catalina:       "c8f55ba0de85273c1851136f47b89f43ba3cce9cbf0ba9f2bba7db311544a000"
     sha256 cellar: :any,                 mojave:         "84f4e3223c03375b0be93bd87be98f512e092621b4f6b4216e3da7210c56ddad"
