b6ddfb94c6168ecc996410e8fe5d012829cc24c8
Bo Anderson <mail@boanderson.me>
2021-10-22 03:03:48 +0000

    libvidstab: update 1.1.0 bottle.

--- Formula/libvidstab.rb
+++ Formula/libvidstab.rb
@@ -9,6 +9,7 @@ class Libvidstab < Formula
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "ece699a3cc725790bad5d1153f0203e06bfd64427f2b4915e4f4778a75d59635"
     sha256 cellar: :any,                 arm64_big_sur:  "b98be46d2375a1e6b30947b31c981009785a7c0e97c31ca0c64a52228b0d1576"
+    sha256 cellar: :any,                 monterey:       "8ca80c30e8cbd76cf6aa593e39da3f0579ce60edbbb5bd4039b34b3cc00f493c"
     sha256 cellar: :any,                 big_sur:        "b4c67e80b92e95aa19520b0b130a60cc3949db7899d9d02520d32d9fc62ec837"
     sha256 cellar: :any,                 catalina:       "df23e5e7933b6535f34c429ee8286e4d9dec6d0a2349cf3256f44ec687e7968f"
     sha256 cellar: :any,                 mojave:         "783224577a1cc7a57de76eac74b00aac69e7fe15c920d26454e58a369854974f"
