89bc56e15cdc256fee84e95918d7b27b13e3a7f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-22 01:32:04 +0000

    govc: update 0.27.1 bottle.

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -6,11 +6,10 @@ class Govc < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "deae249c0ccae92bb2e4c8caa38f248566e0824e423efa7f3ed473ea321937a1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5d7d5c4131269376248af16da8be3e59398f775e24163d8d8c0efb22b62fc358"
-    sha256 cellar: :any_skip_relocation, catalina:      "d8fb826951dfaaf31592c306e34dbd2c60341e49d421ea602e8a4b5a508d48a2"
-    sha256 cellar: :any_skip_relocation, mojave:        "5cc9eb6d31e356d637115e9a861cd89d633e10783113f36f0ca357bea67cf250"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1fbc575b3812e430e176bd8d035b1ff276d3232019a0ffd446342d7832c7fa1b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c6a5e9b3a2c9ea651a0b27a717ab459745b2a4694c9662cbd516e7716b70576f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ca9122def6ce7b60f470ff8dccb6f9df0bee40e924d79691715ab796d6ffedee"
+    sha256 cellar: :any_skip_relocation, catalina:      "a7e040bab3cc29e23eab63e23ff1e4dfdf99dc4bbdbbfd75652bfcf3fdbfd840"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4381ea4c4678fb2fbced93802bb9820993872fc741bcf10955e5aa9233824b7f"
   end
 
   depends_on "go" => :build
