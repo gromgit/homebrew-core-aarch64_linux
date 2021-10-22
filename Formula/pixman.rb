8100e7aa87f2e8becc8499e47af257a6bff049b5
Bo Anderson <mail@boanderson.me>
2021-10-22 02:01:15 +0000

    pixman: update 0.40.0 bottle.

--- Formula/pixman.rb
+++ Formula/pixman.rb
@@ -13,6 +13,7 @@ class Pixman < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "3dbb0582d3c6fcb87fc1d99a42064e8b81d409951ba7b863c2482957792f837b"
     sha256 cellar: :any,                 arm64_big_sur:  "da951aa8e872276034458036321dfa78e7c8b5c89b9de3844d3b546ff955c4c3"
+    sha256 cellar: :any,                 monterey:       "300fc41cc99dfc7ba11862149f9cb88ab9976200bf88b5b944ff09796ed05f40"
     sha256 cellar: :any,                 big_sur:        "0114710dd922d5e4839c9dea3b72cd5fbe6f00157dd63457c99ca15554cf8d7f"
     sha256 cellar: :any,                 catalina:       "1862e6826a4bedb97af8dcb9ab849c69754226ed92e5ee19267fa33ee96f94f8"
     sha256 cellar: :any,                 mojave:         "70a476e6b14fdfa42188d3df2797f8c13f25bd633528164b0d42c5fb70dfb431"
