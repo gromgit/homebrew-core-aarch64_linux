a47b7b7183a09d1d352919b1c29c1323ecac855e
Bo Anderson <mail@boanderson.me>
2021-10-22 00:57:41 +0000

    srt: update 1.4.4 bottle.

--- Formula/srt.rb
+++ Formula/srt.rb
@@ -7,11 +7,12 @@ class Srt < Formula
   head "https://github.com/Haivision/srt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "7f19f11cb0131e7fe95bf612906b40e00fc8c941f80d294eac2f80850c88ced9"
-    sha256 cellar: :any,                 big_sur:       "1feec9a3449db0914403420eddd731dc17bd2df8fce2b0471227c79e8e7688f9"
-    sha256 cellar: :any,                 catalina:      "078b246649eed71dc3eed9d8d3e2d71b2ce025ecf5933fdc33956f6582251b96"
-    sha256 cellar: :any,                 mojave:        "7308578774fd1cdfed6c250044bf013e2b188c19d866ba114fe02c505ae92303"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84d86cd1b27fe5f6dd097e29ceb63d83aec156c2358e2b21cf00c48435d96a96"
+    sha256 cellar: :any,                 arm64_monterey: "c4abdf93a43db5fc8be19f44e921dd28c249bdc560fcd9321b41616de8c20617"
+    sha256 cellar: :any,                 arm64_big_sur:  "7f19f11cb0131e7fe95bf612906b40e00fc8c941f80d294eac2f80850c88ced9"
+    sha256 cellar: :any,                 big_sur:        "1feec9a3449db0914403420eddd731dc17bd2df8fce2b0471227c79e8e7688f9"
+    sha256 cellar: :any,                 catalina:       "078b246649eed71dc3eed9d8d3e2d71b2ce025ecf5933fdc33956f6582251b96"
+    sha256 cellar: :any,                 mojave:         "7308578774fd1cdfed6c250044bf013e2b188c19d866ba114fe02c505ae92303"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84d86cd1b27fe5f6dd097e29ceb63d83aec156c2358e2b21cf00c48435d96a96"
   end
 
   depends_on "cmake" => :build
