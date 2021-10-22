b4371e51b7651831997b6ff22ae29f126ffe956b
Bo Anderson <mail@boanderson.me>
2021-10-22 14:25:18 +0000

    nasm: update 2.15.05 bottle.

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -11,12 +11,13 @@ class Nasm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8e3fb019ffdfcd4ce6b418adbe3d1d1d0e1e232a7de3b62fc4872c2401543cb3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cc45793ac9f3fedd01dd08bbbf766137a40bf22e18c43742498b0a542aa319e8"
-    sha256 cellar: :any_skip_relocation, catalina:      "1875e67160bac1675dd505d66a6b78469767d2dfe8baab2652409f91ac0549ef"
-    sha256 cellar: :any_skip_relocation, mojave:        "4b3614f857264edfa9aeab961c523b3910cdef0ceccaf9957888b477c1c512f4"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8f8c181994b1f05bf425a4034f76d6973c9e1a85ecb64af7f67d47556f23a0d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21fd7041a27b500983d56bf9b24739790f5de243da14a4a549a2afb3ed23deb4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b890027bb7fef2cf20c69f3fd38ed0c19c373bce79c949ab6d123601617ae949"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e3fb019ffdfcd4ce6b418adbe3d1d1d0e1e232a7de3b62fc4872c2401543cb3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cc45793ac9f3fedd01dd08bbbf766137a40bf22e18c43742498b0a542aa319e8"
+    sha256 cellar: :any_skip_relocation, catalina:       "1875e67160bac1675dd505d66a6b78469767d2dfe8baab2652409f91ac0549ef"
+    sha256 cellar: :any_skip_relocation, mojave:         "4b3614f857264edfa9aeab961c523b3910cdef0ceccaf9957888b477c1c512f4"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "8f8c181994b1f05bf425a4034f76d6973c9e1a85ecb64af7f67d47556f23a0d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21fd7041a27b500983d56bf9b24739790f5de243da14a4a549a2afb3ed23deb4"
   end
 
   head do
