c672e7ab40ea8945adf928db78aed41a364460ff
Bo Anderson <mail@boanderson.me>
2021-10-22 17:28:01 +0000

    xvid: update 1.3.7 bottle.

--- Formula/xvid.rb
+++ Formula/xvid.rb
@@ -13,6 +13,7 @@ class Xvid < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "ccf0c5b732d140dce8c595ff6bad287ed5be49a2c6a05912a1dbfdedfcb232bf"
     sha256 cellar: :any,                 arm64_big_sur:  "8974d7b8f816f7d5e8d9ae967b94922e0ed212f22f6475b7fa4c80c7a95d6582"
+    sha256 cellar: :any,                 monterey:       "57aae7b7565705fdd83b0c2996cf0d2e3569546e9691197d175431b89a9599b9"
     sha256 cellar: :any,                 big_sur:        "feabfa1a3df3b916654ba5eef30193b65cdba70a7a49cca6406ec0c214b50338"
     sha256 cellar: :any,                 catalina:       "ace5fea6272f3594b5c8fca6f1fe03c41c50a14af8599751571c5e44a49a5a53"
     sha256 cellar: :any,                 mojave:         "4e119534a1351c85799944eb35f6f5675192e67e077fb3452f73f210a57eabe3"
