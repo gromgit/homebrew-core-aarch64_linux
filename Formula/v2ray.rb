df30f8a109ceec30c0b1ce6d49a29e0db0ea164e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-22 10:11:50 +0000

    v2ray: update 4.43.0 bottle.

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -12,11 +12,10 @@ class V2ray < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "58a341d97e01eade50de245608cf61069b372917e447c6fc5d478e38d76af786"
-    sha256 cellar: :any_skip_relocation, big_sur:       "58880e010e81991c51c3f1bf9347fab816a556b370481b048f28789c56e32b1d"
-    sha256 cellar: :any_skip_relocation, catalina:      "a6e0a954b4cec4600212e706d56bda7ae8d5b89467bacb5f73ee3f320a0fee5e"
-    sha256 cellar: :any_skip_relocation, mojave:        "7fbf1fc09f1b7ab1c6952c3d2a3e3042d36920c22558d7a42b5c2f6f06d27380"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19fe8081c43b3b2b2419310a5966c6d016e2aaf7e42fc29af58b5b5ef6ff5259"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5f2e213e1f2dbcd8bc8d8101f452d12b4d16eba09bb9cbafae7cec44754579d3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f33265071c5573d0c129ec320dd225805518083feadab6561acedbc06045aa47"
+    sha256 cellar: :any_skip_relocation, catalina:      "b6b2e1efcbaf69ecc084b0f6856f770a05c69d4f651c37a505b8f34fb69c3c4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86453bcba9ac3b90ed862c158a5cfe17138ade209a3fd9965e8650b0d40d9078"
   end
 
   depends_on "go" => :build
