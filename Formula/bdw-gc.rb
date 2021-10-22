6de5871969e786fa48428be306d4026d7c050b2a
Bo Anderson <mail@boanderson.me>
2021-10-22 14:39:28 +0000

    bdw-gc: update 8.0.6 bottle.

--- Formula/bdw-gc.rb
+++ Formula/bdw-gc.rb
@@ -24,6 +24,7 @@ class BdwGc < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "55bdbcc825a5f4657ca307ed0a002e8cd07bb1635148962ff9187aca4b7dcb9c"
     sha256 cellar: :any,                 arm64_big_sur:  "788d86cb322a9409fb6e8117fc6ee48e57e3258a18b77a39f8682730caf2d239"
+    sha256 cellar: :any,                 monterey:       "bdc97f40152e69b1b462021b1aa85cb97a134a6760bbdeba41a1d729a87c13d6"
     sha256 cellar: :any,                 big_sur:        "e1657498c65d958779349a5b3b2283e52e5c48cde79e26f761340bacfc3627e9"
     sha256 cellar: :any,                 catalina:       "0473379cc9c7dd97c1d28386e333702af1c2b98ae86b1564e5ef72fda94d395c"
     sha256 cellar: :any,                 mojave:         "45add006a7a827ea1b1828d2e5edb45b991cbb31906370d8270f511b781acfd6"
