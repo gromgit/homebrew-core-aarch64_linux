f36b35d16a7265d0ef1eb1226262125976b8910b
Bo Anderson <mail@boanderson.me>
2021-10-22 18:40:24 +0000

    python-tk@3.10: update 3.10.0_1 bottle.

--- Formula/python-tk@3.10.rb
+++ Formula/python-tk@3.10.rb
@@ -14,6 +14,7 @@ class PythonTkAT310 < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "5c5ca802727c9bb7cad99b3335db43efcc0ab3d0b47035884ff804dd764cbad2"
     sha256 cellar: :any, arm64_big_sur:  "6495ba7d17711b36a7cec076db2458da038d4b3adb83b6d70404c7bc39eb5ec8"
+    sha256 cellar: :any, monterey:       "1a4ddbb28f7fd41be9864a80234d41d53f49056878fb8fc7a5b5fc6f73682c10"
     sha256 cellar: :any, big_sur:        "9a840114d16d6f0e164e4b7af1b20d95196431bc128473f866fffd69f028eef8"
     sha256 cellar: :any, catalina:       "bd83cb8f690893fb79ec7f4ba8414455bc175f246c764e48ec2991b1a94fb35d"
     sha256 cellar: :any, mojave:         "fde0835103328932bf87517e9737cca4d9bbf0b7b6a17eba99fc7eaa9647c2c8"
