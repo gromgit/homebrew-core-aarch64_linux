c451ba4a2e9378883b5a717449816167b15a155a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-22 12:09:52 +0000

    docker-completion: update 20.10.9 bottle.

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -11,7 +11,7 @@ class DockerCompletion < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "90ed4550064c756f9cc165649143b5daf88a32af7c254ee20410cb1b26e7c064"
+    sha256 cellar: :any_skip_relocation, all: "8724f29af863a3c8cc8ad5ba8775cf6c120d382543d10c18dffbac53446fa2c7"
   end
 
   conflicts_with "docker",
