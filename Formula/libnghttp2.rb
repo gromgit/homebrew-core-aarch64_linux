c7363741451746d6b64a7eced2a394955662bef6
Bo Anderson <mail@boanderson.me>
2021-10-22 13:30:42 +0000

    libnghttp2: update 1.46.0 bottle.

--- Formula/libnghttp2.rb
+++ Formula/libnghttp2.rb
@@ -13,11 +13,12 @@ class Libnghttp2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "f73eb0a7cec33617af3678052a930246107b6fa0d00fc480678718227fe661a4"
-    sha256 cellar: :any,                 big_sur:       "14061ffcaca71329e5f77f09bdf3d90aa06e406fa7f732c456209e9ff640a007"
-    sha256 cellar: :any,                 catalina:      "35666c878a2866c108581b95caacb6c4854e9f5778f2102723c4c09c625c8141"
-    sha256 cellar: :any,                 mojave:        "67ad1b85114a52237011f740be10a36e7906b6aa0efbc7056ad8f97df52d28d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "20e19d7c51bdd7eed7ccb15169c10b02dedfae921c12e9832e423094ed0549eb"
+    sha256 cellar: :any,                 arm64_monterey: "7676ac3fa660ac5c0f08715d77edb7bbe68bfbe2853697271fcb9a32fb9caad7"
+    sha256 cellar: :any,                 arm64_big_sur:  "f73eb0a7cec33617af3678052a930246107b6fa0d00fc480678718227fe661a4"
+    sha256 cellar: :any,                 big_sur:        "14061ffcaca71329e5f77f09bdf3d90aa06e406fa7f732c456209e9ff640a007"
+    sha256 cellar: :any,                 catalina:       "35666c878a2866c108581b95caacb6c4854e9f5778f2102723c4c09c625c8141"
+    sha256 cellar: :any,                 mojave:         "67ad1b85114a52237011f740be10a36e7906b6aa0efbc7056ad8f97df52d28d0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20e19d7c51bdd7eed7ccb15169c10b02dedfae921c12e9832e423094ed0549eb"
   end
 
   head do
