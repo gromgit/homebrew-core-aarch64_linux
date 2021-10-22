8c809f6e0717f04d455874de01c9ef8bcc1974e9
Bo Anderson <mail@boanderson.me>
2021-10-22 13:57:42 +0000

    wget: update 1.21.2 bottle.

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -6,11 +6,12 @@ class Wget < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "4f8b66c5f181f01064522a80bfda72eabddd47299a8b88bc7d0022c457e72594"
-    sha256 big_sur:       "7a8e6512e0890076b9ebc4f8db6165d70b4bd05e04dfc0491519ba3c91a5c21e"
-    sha256 catalina:      "3b191bb28b5011e7a105ae76427f6dd21a1e12c33da2273b7e01ef2110f0f375"
-    sha256 mojave:        "e0d4b68c9e5abeaa6395241c43307c4bbd26133cd63d136321974535788c37e9"
-    sha256 x86_64_linux:  "1c102dc1129e508f7788824ea6ef4db4656fbab2a6a4b54419689925a5ed6855"
+    sha256 arm64_monterey: "571ef7b59ebab2aa947485aa33bf612d001d51f5bbc89b59d00ac39712b846c8"
+    sha256 arm64_big_sur:  "4f8b66c5f181f01064522a80bfda72eabddd47299a8b88bc7d0022c457e72594"
+    sha256 big_sur:        "7a8e6512e0890076b9ebc4f8db6165d70b4bd05e04dfc0491519ba3c91a5c21e"
+    sha256 catalina:       "3b191bb28b5011e7a105ae76427f6dd21a1e12c33da2273b7e01ef2110f0f375"
+    sha256 mojave:         "e0d4b68c9e5abeaa6395241c43307c4bbd26133cd63d136321974535788c37e9"
+    sha256 x86_64_linux:   "1c102dc1129e508f7788824ea6ef4db4656fbab2a6a4b54419689925a5ed6855"
   end
 
   head do
