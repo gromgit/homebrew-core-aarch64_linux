3234ec48c219d826c750e7884ef2206aa36d5435
Bo Anderson <mail@boanderson.me>
2021-10-22 02:07:53 +0000

    fmt: update 8.0.1 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -6,11 +6,12 @@ class Fmt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "f9b84ce76a3226dc6f7e892b844de4238b1ea278bd77949839afbbc4b0bdce57"
-    sha256 cellar: :any,                 big_sur:       "20e10af8ee859764717466c355812a03b4ed4e23b7e97f155b1bc0b391dfe2ba"
-    sha256 cellar: :any,                 catalina:      "08d31cecf8c989a946b205aec6e5cb305f2982a59ea345ba343a4349cef998b7"
-    sha256 cellar: :any,                 mojave:        "f0d905746ce5dc69067e240b33c2e37268c9f88889c65578fb96b7c373cc0e80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4edba06a41ddb0fcea04cfb3b71c95dd945df125aee3d1de2f22abfa2b65fd6b"
+    sha256 cellar: :any,                 arm64_monterey: "73db4f891010951987fe242ce0cdba2f81e0ac92ddbea69e58d7277a903da231"
+    sha256 cellar: :any,                 arm64_big_sur:  "f9b84ce76a3226dc6f7e892b844de4238b1ea278bd77949839afbbc4b0bdce57"
+    sha256 cellar: :any,                 big_sur:        "20e10af8ee859764717466c355812a03b4ed4e23b7e97f155b1bc0b391dfe2ba"
+    sha256 cellar: :any,                 catalina:       "08d31cecf8c989a946b205aec6e5cb305f2982a59ea345ba343a4349cef998b7"
+    sha256 cellar: :any,                 mojave:         "f0d905746ce5dc69067e240b33c2e37268c9f88889c65578fb96b7c373cc0e80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4edba06a41ddb0fcea04cfb3b71c95dd945df125aee3d1de2f22abfa2b65fd6b"
   end
 
   depends_on "cmake" => :build
