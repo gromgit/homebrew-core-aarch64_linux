906d05279ad3c7b5ba67e2b1576bf2a7aad2365d
Bo Anderson <mail@boanderson.me>
2021-10-22 16:25:39 +0000

    htop: update 3.1.1 bottle.

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -12,11 +12,12 @@ class Htop < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "bc5dfcfd0b154377c4436bc3f2c35a43d618319c43e9184d13832e06fdd79820"
-    sha256 cellar: :any,                 big_sur:       "301c8306cbfe6584335a8e7db71e2436e336357b0371f5cf29b94b1a557c6652"
-    sha256 cellar: :any,                 catalina:      "7ddeb74a5a6873b69f705fb090e6b477dec8261dbb221a5b8dbc6bcc810af804"
-    sha256 cellar: :any,                 mojave:        "c5a1729400dfec62ee63ab53e5667f06d4709ee9021e3573c8ec1fd6ae4fcebc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a33628783ecc09a777afce0eb819e877a9379e71f9f22a074316ebf7ef2f47f4"
+    sha256 cellar: :any,                 arm64_monterey: "a750757241fe07c332f12157fefd3122d223ad1356e1a85e70c78e8ab69aebc2"
+    sha256 cellar: :any,                 arm64_big_sur:  "bc5dfcfd0b154377c4436bc3f2c35a43d618319c43e9184d13832e06fdd79820"
+    sha256 cellar: :any,                 big_sur:        "301c8306cbfe6584335a8e7db71e2436e336357b0371f5cf29b94b1a557c6652"
+    sha256 cellar: :any,                 catalina:       "7ddeb74a5a6873b69f705fb090e6b477dec8261dbb221a5b8dbc6bcc810af804"
+    sha256 cellar: :any,                 mojave:         "c5a1729400dfec62ee63ab53e5667f06d4709ee9021e3573c8ec1fd6ae4fcebc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a33628783ecc09a777afce0eb819e877a9379e71f9f22a074316ebf7ef2f47f4"
   end
 
   depends_on "autoconf" => :build
