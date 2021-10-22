e9a52ed457d2377d7020905655f7cbe9da96b909
Bo Anderson <mail@boanderson.me>
2021-10-22 02:48:58 +0100

    libev: patch incorrect -flat_namespace usage

--- Formula/libev.rb
+++ Formula/libev.rb
@@ -19,6 +19,12 @@ class Libev < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a22fcf5d3733f1cd5814c5ae2c5a46c7c408195d408d3666b42696a0127f8bb5"
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
