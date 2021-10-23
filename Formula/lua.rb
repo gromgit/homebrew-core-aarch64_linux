53495588ce42a9e7800a2d6a0430b610878b4a56
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 07:31:57 +0000

    lua: update 5.4.3 bottle.

--- Formula/lua.rb
+++ Formula/lua.rb
@@ -12,11 +12,12 @@ class Lua < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any,                 arm64_big_sur: "2a9741ed654f1062394c3431072711bd3aaa39c1fb45ccf3468ace915a7cd843"
-    sha256 cellar: :any,                 big_sur:       "b47b9174126bc9bdabb694db4c61cc4d705b06cf7b6f5c19771f447992863bb4"
-    sha256 cellar: :any,                 catalina:      "8503086f7311c0c05a12fbad5c49561d066efb8abef9ed3b66c8b35f17e6a5a0"
-    sha256 cellar: :any,                 mojave:        "e075a5333160b570cb0532f7124061c44ae58fe33cad382ad2dbbf9f87675712"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ed70cbfb305766e0b80ed8fb9a8ac077246908194342edf8cfd108fb0f3d672"
+    sha256 cellar: :any,                 arm64_monterey: "5f129954a5f5f6e33131f9f9d54c035aab8bd6a4bcd2d202b744ed36f9496796"
+    sha256 cellar: :any,                 arm64_big_sur:  "2a9741ed654f1062394c3431072711bd3aaa39c1fb45ccf3468ace915a7cd843"
+    sha256 cellar: :any,                 big_sur:        "b47b9174126bc9bdabb694db4c61cc4d705b06cf7b6f5c19771f447992863bb4"
+    sha256 cellar: :any,                 catalina:       "8503086f7311c0c05a12fbad5c49561d066efb8abef9ed3b66c8b35f17e6a5a0"
+    sha256 cellar: :any,                 mojave:         "e075a5333160b570cb0532f7124061c44ae58fe33cad382ad2dbbf9f87675712"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ed70cbfb305766e0b80ed8fb9a8ac077246908194342edf8cfd108fb0f3d672"
   end
 
   uses_from_macos "unzip" => :build
