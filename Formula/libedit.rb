a0df1cf33d04f66dcfb5b087ba8f4f5ad164f90e
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:51:49 +0000

    libedit: update 20210910-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -13,6 +13,7 @@ class Libedit < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "87f49ccb584888cb95885c09a586ec0d97f7d3813cdc38f66e07690143226583"
+    sha256 cellar: :any,                 monterey:      "95938022416b3a19c67763eef069c3b98922f58d87a5c2c0d6a654a9a9a08323"
     sha256 cellar: :any,                 big_sur:       "f1c631b30d1daf17da2bdd3d1f59330d99439ff15dd573e52364bc014a664803"
     sha256 cellar: :any,                 catalina:      "38e6ae9c4fd560bc93003106a786fac1e26749426b837e8f92fb38b547b0edb7"
     sha256 cellar: :any,                 mojave:        "44e7ce6ef6d1f558f565d03e4287d6da98a36e120b736a083d5c751669fdb7c1"
