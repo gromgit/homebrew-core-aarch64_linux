3b2fa1cd67be079f1c087de792a3b348f211f3c4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 10:43:56 +0000

    libcerf: update 1.17 bottle.

--- Formula/libcerf.rb
+++ Formula/libcerf.rb
@@ -13,12 +13,13 @@ class Libcerf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b556c15568b822024dcb8bb43f348ce96190dadb06a92daf4a7bdc6d2bb952c7"
-    sha256 cellar: :any,                 monterey:      "67d445f7fe88fd85b9dc07e772cc5c7e802d5032e1885b14178c8c79399aabcd"
-    sha256 cellar: :any,                 big_sur:       "3c23b5b1d35053a0378674aa6d6c5668a4b36be61382b7974d284dcfa17cdde7"
-    sha256 cellar: :any,                 catalina:      "9d0409617477f3c400ef771c2b8c9b3eb46faeda7608f2a3200dcf7a69eb8c80"
-    sha256 cellar: :any,                 mojave:        "7dc6c5eff9c002bda2fc9790cba55fa81965da5298f5666542fdd3717b751d1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8143e570f28eb8f2d413dc4269992663f762509b4f0f3a5fa3064260baec2660"
+    sha256 cellar: :any,                 arm64_monterey: "fb7e498e7fa1763ff17623233f6fa0a99034fcf47f47c4de073de42c00067299"
+    sha256 cellar: :any,                 arm64_big_sur:  "b556c15568b822024dcb8bb43f348ce96190dadb06a92daf4a7bdc6d2bb952c7"
+    sha256 cellar: :any,                 monterey:       "67d445f7fe88fd85b9dc07e772cc5c7e802d5032e1885b14178c8c79399aabcd"
+    sha256 cellar: :any,                 big_sur:        "3c23b5b1d35053a0378674aa6d6c5668a4b36be61382b7974d284dcfa17cdde7"
+    sha256 cellar: :any,                 catalina:       "9d0409617477f3c400ef771c2b8c9b3eb46faeda7608f2a3200dcf7a69eb8c80"
+    sha256 cellar: :any,                 mojave:         "7dc6c5eff9c002bda2fc9790cba55fa81965da5298f5666542fdd3717b751d1b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8143e570f28eb8f2d413dc4269992663f762509b4f0f3a5fa3064260baec2660"
   end
 
   depends_on "cmake" => :build
