9f06e92e0e9e0606d13ba144c9c1fb071c70d0e0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 11:04:15 +0000

    swiftlint: update 0.45.0 bottle.

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -10,6 +10,7 @@ class Swiftlint < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5acfc58c5c401447183ac281e264c3f7724eeda5a708d49cb20a1618a311184b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85a35161284914d468563c898babfefcb4010d8de96b843a7f36ad8b55bf996a"
+    sha256 cellar: :any_skip_relocation, monterey:       "52444cc37d10ed0db9edeb4e3500617492b08d768b13198ddb1c6b200f96bc29"
     sha256 cellar: :any_skip_relocation, big_sur:        "e7213df813a99fa4a310a15f7c4212bf3aad8955adadb2621c86ffdacda00a99"
   end
 
