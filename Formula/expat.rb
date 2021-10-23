9d87dc927114b9f6eb758c7d39630a214a249385
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 10:41:58 +0000

    expat: update 2.4.1 bottle.

--- Formula/expat.rb
+++ Formula/expat.rb
@@ -12,12 +12,13 @@ class Expat < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "fac282dc87d050030a34ad8b6fdae89dd5ecc1c08d03f0d9efc49c9396daaed8"
-    sha256 cellar: :any,                 monterey:      "3a0842f8aaf57a65a2e9a8d214f984f578d582b2e194982d3064e892bd985141"
-    sha256 cellar: :any,                 big_sur:       "18ebdde1356dd48de8c575eb2479dbb6556f7bef7a2b48a64c0f018dd79af1dc"
-    sha256 cellar: :any,                 catalina:      "b679f0f8807216636b660b01b3b8f5506e245e4f6160450f408f6446d3017e48"
-    sha256 cellar: :any,                 mojave:        "45004cc0adb2a75d9d0f965a0fc3f087059907aeed9c5f8b36bbb3babce4a6df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6cb9d218fe6fbfb86d5aa69f6ac7087b6a082e9b6739a51a701c1a7f3d7d3642"
+    sha256 cellar: :any,                 arm64_monterey: "5707bf186fa91f8196316967ad00e629e33b4cb98c0ae27503a57caa3dbc6216"
+    sha256 cellar: :any,                 arm64_big_sur:  "fac282dc87d050030a34ad8b6fdae89dd5ecc1c08d03f0d9efc49c9396daaed8"
+    sha256 cellar: :any,                 monterey:       "3a0842f8aaf57a65a2e9a8d214f984f578d582b2e194982d3064e892bd985141"
+    sha256 cellar: :any,                 big_sur:        "18ebdde1356dd48de8c575eb2479dbb6556f7bef7a2b48a64c0f018dd79af1dc"
+    sha256 cellar: :any,                 catalina:       "b679f0f8807216636b660b01b3b8f5506e245e4f6160450f408f6446d3017e48"
+    sha256 cellar: :any,                 mojave:         "45004cc0adb2a75d9d0f965a0fc3f087059907aeed9c5f8b36bbb3babce4a6df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6cb9d218fe6fbfb86d5aa69f6ac7087b6a082e9b6739a51a701c1a7f3d7d3642"
   end
 
   head do
