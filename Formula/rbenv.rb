1e16433ab56a2306c943b64b8f25ed343b7ee3fb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 07:45:02 +0000

    rbenv: update 1.2.0 bottle.

--- Formula/rbenv.rb
+++ Formula/rbenv.rb
@@ -9,6 +9,7 @@ class Rbenv < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "dede9454bc8a665ac2b1858a0522fb77d95deebb5db7437918cfb056ff119b16"
     sha256 cellar: :any,                 arm64_big_sur:  "d5e6168ad6ab8843946273319fc6949b322c80f2d666a6bdda62466e256e6746"
+    sha256 cellar: :any,                 monterey:       "42657e04e2d1e8bf9abb9c5f0ba50e567df95f93a2a212491f005e4bd0ad9cee"
     sha256 cellar: :any,                 big_sur:        "8a1b159909d472cc461d0a9b85a192a31ab58860e34f022fcbb33175732d24aa"
     sha256 cellar: :any,                 catalina:       "a2ca52c4fe3b7000d9f84f81836ddcb9b3aea9c20ee092dd71c1e10cf3a6a19a"
     sha256 cellar: :any,                 mojave:         "87ca53a9f4f84aff56ccbf2f823f903d20bc6669dde548018892857cc8871936"
