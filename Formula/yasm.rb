167df454fda03d1cfb9c24ed88f8e522e35cd6b9
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:16:46 +0000

    yasm: update 1.3.0_2 bottle.

--- Formula/yasm.rb
+++ Formula/yasm.rb
@@ -8,13 +8,14 @@ class Yasm < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "398b7f3d8a22e43b5af2335fe2d39448a3e9cc7a769ef1faf60c25fda0873d50"
-    sha256 cellar: :any_skip_relocation, monterey:      "8348a13c38c499aa114f71e4d46f311105b68dbafbf0e92f6c19d5b492eed569"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ca95cb3c02508796ff4e60d54146b03016b93e80837916359912ebf737a37562"
-    sha256 cellar: :any_skip_relocation, catalina:      "9aa61930f25fe305dc5364e72f539b0a225702b5f1dc222a9dde1216e901f7ab"
-    sha256 cellar: :any_skip_relocation, mojave:        "0dc797b72ee3bad9c6a52276c871ac745207b5626722e805fa642d7a872847fc"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "7f31deeff91c5929f2cd52eca6b636669f9c8966f6d4777e89fa4b04e541ad85"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6d46adb6213bba936b7d62ef9564f752cc5b4268e19e91f0b67e136408ab30e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6d1a844ce9a26db6d2a5c72dbced52b7fbfc8491bfde95a2f026eaa1e46433be"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "398b7f3d8a22e43b5af2335fe2d39448a3e9cc7a769ef1faf60c25fda0873d50"
+    sha256 cellar: :any_skip_relocation, monterey:       "8348a13c38c499aa114f71e4d46f311105b68dbafbf0e92f6c19d5b492eed569"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ca95cb3c02508796ff4e60d54146b03016b93e80837916359912ebf737a37562"
+    sha256 cellar: :any_skip_relocation, catalina:       "9aa61930f25fe305dc5364e72f539b0a225702b5f1dc222a9dde1216e901f7ab"
+    sha256 cellar: :any_skip_relocation, mojave:         "0dc797b72ee3bad9c6a52276c871ac745207b5626722e805fa642d7a872847fc"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "7f31deeff91c5929f2cd52eca6b636669f9c8966f6d4777e89fa4b04e541ad85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6d46adb6213bba936b7d62ef9564f752cc5b4268e19e91f0b67e136408ab30e"
   end
 
   head do
