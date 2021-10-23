aaaf913e54c65aa27f2169f2ed87f45081d6e0bf
Bo Anderson <mail@boanderson.me>
2021-10-23 18:55:29 +0000

    pugixml: update 1.11.4_2 bottle.

--- Formula/pugixml.rb
+++ Formula/pugixml.rb
@@ -14,6 +14,7 @@ class Pugixml < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "a5284ea4e8d6fa76cd41505294e23a4c448b54d888de993b3cf57308ccbb5cd7"
     sha256 cellar: :any,                 arm64_big_sur:  "72487f35e935099779ea88bbbf6dd205406710df262674e6295972edff2a9ac1"
+    sha256 cellar: :any,                 monterey:       "83d019d9a46aaa30c40a9acbebfeb19115b09f8534262a8d5ef156bdaa0c3e0b"
     sha256 cellar: :any,                 big_sur:        "450e11f4eafe21828d3987620406eb42695a60c15086b7741898d483bb05fa8d"
     sha256 cellar: :any,                 catalina:       "112dda2780766cf7403426252180cea172cd396f7b52aee42a690aa7539c933b"
     sha256 cellar: :any,                 mojave:         "60d558fea876933be7a5322267ca58c0850eb23ec05d71b3d2c3876793b01367"
