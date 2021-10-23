485b6f0ae1b8a9b55a5d1215c0731171dde04796
Bo Anderson <mail@boanderson.me>
2021-10-23 20:01:11 +0000

    golangci-lint: update 1.42.1 bottle.

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -8,11 +8,12 @@ class GolangciLint < Formula
   head "https://github.com/golangci/golangci-lint.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0e7f13fd8b6e7eb315a66d810ee22d53e5337a40a4855f0befe3a7cb1fc511c0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bcbdd5583eeacfd97dde10cd5db41dff4a473f14ebf53d0cb7aa99ae9a241dbc"
-    sha256 cellar: :any_skip_relocation, catalina:      "218b2b3110ab2a1fe46b7eae2f8ebab0c320f92b44b3b3b00796e8bd9ce01256"
-    sha256 cellar: :any_skip_relocation, mojave:        "eb9c9bac43be153ec8a69329409fc0786744a8c8992fa93403797a4aa663f150"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "59b45d6e936c53b98f7e2420b2726bd8dc5ffa2d73bb3dbc2af6b7a2361066a1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3264cebec318f099ad435e4807a63bb8f740e3b63d84596ca93af30df43bc1ec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0e7f13fd8b6e7eb315a66d810ee22d53e5337a40a4855f0befe3a7cb1fc511c0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bcbdd5583eeacfd97dde10cd5db41dff4a473f14ebf53d0cb7aa99ae9a241dbc"
+    sha256 cellar: :any_skip_relocation, catalina:       "218b2b3110ab2a1fe46b7eae2f8ebab0c320f92b44b3b3b00796e8bd9ce01256"
+    sha256 cellar: :any_skip_relocation, mojave:         "eb9c9bac43be153ec8a69329409fc0786744a8c8992fa93403797a4aa663f150"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59b45d6e936c53b98f7e2420b2726bd8dc5ffa2d73bb3dbc2af6b7a2361066a1"
   end
 
   depends_on "go"
