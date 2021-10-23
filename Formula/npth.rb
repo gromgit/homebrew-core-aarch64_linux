247ed8da2be9d3f0cee305c4174bcd4b2767bc4d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 16:13:17 +0800

    npth: patch incorrect -flat_namespace usage
    This is needed for bottling on Monterey.
    
    Closes #87823.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/npth.rb
+++ Formula/npth.rb
@@ -22,6 +22,12 @@ class Npth < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "04d83373d7eb4a417d127c7e341e0de0cdd154b876097c124dab3b83ada2fc9e"
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
