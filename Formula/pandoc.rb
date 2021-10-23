849640e3a0db9942ac1b24a2b1c347cf7941b896
Bo Anderson <mail@boanderson.me>
2021-10-23 20:35:41 +0000

    pandoc: update 2.14.2 bottle.

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -9,6 +9,7 @@ class Pandoc < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fc73fff9e7b8835ae86eb31ee3bec218f6b15c5054d062359f219b6e0d3dde87"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "817369a3214c250c65b9d6fd6bd206520c333ea55832e1e74b8d7b49002a7bed"
+    sha256 cellar: :any_skip_relocation, monterey:       "eba0a237c7f86571c80c6bda88f574747ec3f784d32abdf8fd42a62282de98d3"
     sha256 cellar: :any_skip_relocation, big_sur:        "2b5eb56d4c06ff7350259d7b8dfc67291a389913b935c7cbfa0cd34b735fa728"
     sha256 cellar: :any_skip_relocation, catalina:       "86246ec052a743cec4b83cb339fbfbe8016a13b22c8f69a7dfe2d5d12341ecbe"
     sha256 cellar: :any_skip_relocation, mojave:         "0bda1c5a703f8cc92861fde63cfb6d54f8d6bb363f4d3d096f95237147b38d54"
