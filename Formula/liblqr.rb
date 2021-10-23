15c9611a825c9dd9626b5bf2cb6d683f85536b26
Bo Anderson <mail@boanderson.me>
2021-10-23 19:26:56 +0000

    liblqr: update 0.4.2_1 bottle.

--- Formula/liblqr.rb
+++ Formula/liblqr.rb
@@ -20,6 +20,7 @@ class Liblqr < Formula
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "00c8af9f4f4818defb464b1543cdc820cf8c10c5b7ccf30a9b306ee96519ce66"
     sha256 cellar: :any,                 arm64_big_sur:  "5b55b5517f358ea17c882c7afcb02ef538fe032854a6a9e1f54785a35862adde"
+    sha256 cellar: :any,                 monterey:       "491fd59fd84a8bc28963dc72b04c4f98de9d835379ae5712af83ff66a6331180"
     sha256 cellar: :any,                 big_sur:        "94977eaf2a6b9c9d52f178267ba034bb2515cb2ba0a643006c10f83ab6a532b9"
     sha256 cellar: :any,                 catalina:       "18803ed552ae07c1998c87ba6c4ebaee1ec5eaab843c2cfa2cc3775f0b55da23"
     sha256 cellar: :any,                 mojave:         "83054ddb4fffb94ea12f609a90082220a451bfdc793284d104f1fdeaf4aa8fd6"
