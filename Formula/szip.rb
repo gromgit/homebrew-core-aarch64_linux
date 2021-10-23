27262580b5760776f3cb70b3d475c2b907094362
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 20:30:37 +0800

    szip: patch incorrect -flat_namespace usage
    This is needed for bottling on Monterey.
    
    Closes #87828.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/szip.rb
+++ Formula/szip.rb
@@ -23,6 +23,12 @@ class Szip < Formula
 
   conflicts_with "libaec", because: "libaec provides a replacement for szip"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
