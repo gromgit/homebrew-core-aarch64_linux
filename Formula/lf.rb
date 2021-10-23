1cc0558d853a19a3e950f6c6481c1714ccc3243c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-23 05:41:16 +0000

    lf: update 26 bottle.

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -6,11 +6,10 @@ class Lf < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "013b12e45ce6d3a25d952adef77f8c3ef8f8a7ff7464accb2c42b79ab509c004"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0e742ca0c46012ce472565999d8d6d2725c87ec6b59ef4007e16cadfdacc0670"
-    sha256 cellar: :any_skip_relocation, catalina:      "30c756ad4361ae806e7f2c5c17cdf96fde5c482aedac1cb1d0d2cc60b7304492"
-    sha256 cellar: :any_skip_relocation, mojave:        "388215d3cff682c04d6a9d7e4ac2e66170702fd4622b9ea1a061141b085a9425"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "502a37242a51501083104f019c551bfa6135d791badcedf6752100729de9ca4d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f16e27a50a509db35453fcd2de41c4973da8f1b37131c7de5ef2a8097ea9219b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e76f4582df73496e7957136782b809d450ccac55337b7040776dd6ad48c51d6b"
+    sha256 cellar: :any_skip_relocation, catalina:      "a9ed31146b0ebbd0662b78e58bed48344774a485302ddf080ca1f1aea7cdc7bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6cc327c457adb1387fbecb627c2adef0de39c18cbcbf0848e0b6b8869a39280d"
   end
 
   depends_on "go" => :build
