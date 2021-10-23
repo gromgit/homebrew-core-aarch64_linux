835084983b152a424f0324d2cc5f8bababe08747
Bo Anderson <mail@boanderson.me>
2021-10-23 14:38:37 +0000

    llvm: update 13.0.0_1 bottle.

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -16,6 +16,7 @@ class Llvm < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "124b04767b224f74be9df06181f1e20122b39108e6926e621a7a64702c9b2796"
     sha256 cellar: :any,                 arm64_big_sur:  "1213e5b2e63bc8d8440320cccd316a04b9824eba563534e4b5cfca000c5acf29"
+    sha256 cellar: :any,                 monterey:       "2d2f5c57e8cb8040031b26a81eb7355068b590a17a46034ca7ac339463eca14a"
     sha256 cellar: :any,                 big_sur:        "dc0ac71f6ce7183b0fa4745fd94e08c55770dd1ee6d38d63f0c503d644937bb6"
     sha256 cellar: :any,                 catalina:       "dbcd8e6f48bf03b4730b92f4fe6351af97addc2db3eefde3ba85402b120368f5"
     sha256 cellar: :any,                 mojave:         "d2e8294ee877ecb1cf6b3d36f1f8bbc5f7749306b3b25ef988886600e931b7b4"
