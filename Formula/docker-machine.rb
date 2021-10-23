8557b58b9f89494cce67e6083de1b2fee077a63f
Bo Anderson <mail@boanderson.me>
2021-10-23 19:11:51 +0000

    docker-machine: update 0.16.2 bottle.

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -8,11 +8,12 @@ class DockerMachine < Formula
   head "https://github.com/docker/machine.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8ed6a73a1d30c911811e8f6fb0e61e41bc3be4aea62bc2b77f7b6dca50b517a9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "720ea8bbbfdc6b9d0701f02014e09f6a46e6785bcbdb36ebe3e95bddd0849dfa"
-    sha256 cellar: :any_skip_relocation, catalina:      "e27501077ccc67fc468ca8e2881366a9fc23260296ed93a3f436b4d12f41ec43"
-    sha256 cellar: :any_skip_relocation, mojave:        "0cfe7d344bd6c2b3bc0d1c1de472c430162a45dd54454b268e82750094b9cf9f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f08e7ba29eb793a79b8126631485ee4100cf07b9e1e5654a7c4db8c2d229d5af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c941d688b50d6eae302320aa5e702d5da26e4e38ceac2f925a24b6efe6c589db"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ed6a73a1d30c911811e8f6fb0e61e41bc3be4aea62bc2b77f7b6dca50b517a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "720ea8bbbfdc6b9d0701f02014e09f6a46e6785bcbdb36ebe3e95bddd0849dfa"
+    sha256 cellar: :any_skip_relocation, catalina:       "e27501077ccc67fc468ca8e2881366a9fc23260296ed93a3f436b4d12f41ec43"
+    sha256 cellar: :any_skip_relocation, mojave:         "0cfe7d344bd6c2b3bc0d1c1de472c430162a45dd54454b268e82750094b9cf9f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f08e7ba29eb793a79b8126631485ee4100cf07b9e1e5654a7c4db8c2d229d5af"
   end
 
   deprecate! date: "2021-09-30", because: :repo_archived
