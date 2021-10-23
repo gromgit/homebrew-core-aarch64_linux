b391b77016f6c7d95e0d08245795b3b1526ed26a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-23 05:41:48 +0000

    q: update 2.0.20 bottle.

--- Formula/q.rb
+++ Formula/q.rb
@@ -9,11 +9,10 @@ class Q < Formula
   head "https://github.com/harelba/q.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b3d4d40c7c4eacc8abe3ea9d3ab9e34ea0019d93b696a8660d092507c16bfa07"
-    sha256 cellar: :any_skip_relocation, big_sur:       "df0790f33e2fc35de6454f038789eeeb51db774ff1c96f1fdf73da2291b13d64"
-    sha256 cellar: :any_skip_relocation, catalina:      "df0790f33e2fc35de6454f038789eeeb51db774ff1c96f1fdf73da2291b13d64"
-    sha256 cellar: :any_skip_relocation, mojave:        "df0790f33e2fc35de6454f038789eeeb51db774ff1c96f1fdf73da2291b13d64"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26c5d5f054b1399bb5cdf22aab604873d00a11e5020ffd2427beec08da39fbb0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c72ca06a7c9dbe3b3eaee1b8db72811edbf7e64fbee5b694bcf2ed7e8d877d50"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4511c183df36704ec7cb497b4a319409875ea2ef6068255ae2a2e0a2d7293e29"
+    sha256 cellar: :any_skip_relocation, catalina:      "4511c183df36704ec7cb497b4a319409875ea2ef6068255ae2a2e0a2d7293e29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bed14a331133ff96b85fa37e0729ca695bd273f78ee82e792185d137edf9917a"
   end
 
   depends_on "ronn" => :build
