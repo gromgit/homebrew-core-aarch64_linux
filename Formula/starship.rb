040f4671fda679bda3aaccddbe7ccee3139f202f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 08:53:08 +0000

    starship: update 0.58.0 bottle.

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -7,11 +7,12 @@ class Starship < Formula
   head "https://github.com/starship/starship.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b5cdf15b3acc6d79796801ccecec7abbec07a80cd0930d8b8eb0ea0eca5c4234"
-    sha256 cellar: :any_skip_relocation, big_sur:       "497ab79648951d3c78c19ad71e58059e1a810cf6acd70bac6e82369c61d7afd6"
-    sha256 cellar: :any_skip_relocation, catalina:      "cf3eece649108cd3e0f0a06d60917798baa94df0b9e6cc5b47a9f74946bc5f71"
-    sha256 cellar: :any_skip_relocation, mojave:        "b81cf067b32c42429b1509a9349cfd931e6b738429b52e221411c8d00af89098"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c587f2f64efd31838b3c23ba82f13a0301d58e21a10b1795cf0c253ee55a7dae"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a60faabb96b0f5018d9eca0411cc0e62d426104393363aef5cf790b037f20bf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b5cdf15b3acc6d79796801ccecec7abbec07a80cd0930d8b8eb0ea0eca5c4234"
+    sha256 cellar: :any_skip_relocation, big_sur:        "497ab79648951d3c78c19ad71e58059e1a810cf6acd70bac6e82369c61d7afd6"
+    sha256 cellar: :any_skip_relocation, catalina:       "cf3eece649108cd3e0f0a06d60917798baa94df0b9e6cc5b47a9f74946bc5f71"
+    sha256 cellar: :any_skip_relocation, mojave:         "b81cf067b32c42429b1509a9349cfd931e6b738429b52e221411c8d00af89098"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c587f2f64efd31838b3c23ba82f13a0301d58e21a10b1795cf0c253ee55a7dae"
   end
 
   depends_on "rust" => :build
