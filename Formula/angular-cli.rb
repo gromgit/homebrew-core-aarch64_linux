0421e5032eb4573c62c9b0339bdd7cbd6cc1526c
Bo Anderson <mail@boanderson.me>
2021-10-23 23:38:58 +0000

    angular-cli: update 12.2.11 bottle.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -8,11 +8,12 @@ class AngularCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e962f3da366cc0286deee067f6cfc940f4069d585caa42521f100e8931eee5d4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9e286a646754a005aba802ca36b97cb860e2ac0462928ab510fa16ef4bdeff8d"
-    sha256 cellar: :any_skip_relocation, catalina:      "9e286a646754a005aba802ca36b97cb860e2ac0462928ab510fa16ef4bdeff8d"
-    sha256 cellar: :any_skip_relocation, mojave:        "9e286a646754a005aba802ca36b97cb860e2ac0462928ab510fa16ef4bdeff8d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e962f3da366cc0286deee067f6cfc940f4069d585caa42521f100e8931eee5d4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e962f3da366cc0286deee067f6cfc940f4069d585caa42521f100e8931eee5d4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e962f3da366cc0286deee067f6cfc940f4069d585caa42521f100e8931eee5d4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9e286a646754a005aba802ca36b97cb860e2ac0462928ab510fa16ef4bdeff8d"
+    sha256 cellar: :any_skip_relocation, catalina:       "9e286a646754a005aba802ca36b97cb860e2ac0462928ab510fa16ef4bdeff8d"
+    sha256 cellar: :any_skip_relocation, mojave:         "9e286a646754a005aba802ca36b97cb860e2ac0462928ab510fa16ef4bdeff8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e962f3da366cc0286deee067f6cfc940f4069d585caa42521f100e8931eee5d4"
   end
 
   depends_on "node"
