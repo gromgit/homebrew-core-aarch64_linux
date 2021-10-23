66d790fe1a1e0f0265fbea3e9dbc47be7bdbb009
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:58:42 +0000

    dnscrypt-proxy: update 2.1.1 bottle.

--- Formula/dnscrypt-proxy.rb
+++ Formula/dnscrypt-proxy.rb
@@ -12,12 +12,13 @@ class DnscryptProxy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5db483c47f16ed99a01af92ea89aca4270c8146e33ca0aee4e3172b9960ef7f2"
-    sha256 cellar: :any_skip_relocation, monterey:      "aece7d8508b5dcda8746beb38e9d6e854d82f3a5a1231af77479df17e3063beb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "66deed2c2da425b2443ac734e61b83cb55d7298179390b6ea3292fdfdca6f703"
-    sha256 cellar: :any_skip_relocation, catalina:      "fcc6afc722e5b3dc444c99f253d2424f82e3d40daf27524de03bcee09ec69c8e"
-    sha256 cellar: :any_skip_relocation, mojave:        "b83c086949a3fbbe51c4030db4ad3dccc0f4e48ad502eacbe26e4fe4286f4aef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "974f2bf8c5dae80037aa68da36bb66fcea9571a0fa6ebb586cc262df705e28ec"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1c5f214e81da7b5f339e8abbe78e386ece9f42c72baf426d13bd08a40cad949c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5db483c47f16ed99a01af92ea89aca4270c8146e33ca0aee4e3172b9960ef7f2"
+    sha256 cellar: :any_skip_relocation, monterey:       "aece7d8508b5dcda8746beb38e9d6e854d82f3a5a1231af77479df17e3063beb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "66deed2c2da425b2443ac734e61b83cb55d7298179390b6ea3292fdfdca6f703"
+    sha256 cellar: :any_skip_relocation, catalina:       "fcc6afc722e5b3dc444c99f253d2424f82e3d40daf27524de03bcee09ec69c8e"
+    sha256 cellar: :any_skip_relocation, mojave:         "b83c086949a3fbbe51c4030db4ad3dccc0f4e48ad502eacbe26e4fe4286f4aef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "974f2bf8c5dae80037aa68da36bb66fcea9571a0fa6ebb586cc262df705e28ec"
   end
 
   depends_on "go" => :build
