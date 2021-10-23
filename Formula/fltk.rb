705adb85760be77bc2f4ed1383b771d83157f7e2
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 01:51:10 +0000

    fltk: update 1.3.7 bottle.

--- Formula/fltk.rb
+++ Formula/fltk.rb
@@ -12,6 +12,7 @@ class Fltk < Formula
 
   bottle do
     sha256 arm64_big_sur: "ef093c6e98b952c2e3393e124e325ad67a038a08f0ee3d88ca7c6a7b07480567"
+    sha256 monterey:      "745b3f942ec06b482727b81a15da86e938eb82ed6bec27c03055c51b26db8eba"
     sha256 big_sur:       "d95e5a1b0dcc2537f1ac2964a348728e4a66e774f1aed1f0f5dc84a2f5564430"
     sha256 catalina:      "691edcc9f9c48a7c5ebaba813719ef4a9bb66dae228be3a2f4ed6d29acf17dea"
     sha256 mojave:        "550f4a444fd0b0682c154745c5a1b780ce003a3477afaa9ae25a8a846fa05900"
