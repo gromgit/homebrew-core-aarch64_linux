5c12a68bce698ac5334fa0dc9b82c4052b12c25c
Bo Anderson <mail@boanderson.me>
2021-10-23 18:46:24 +0000

    protobuf-c: update 1.4.0 bottle.

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -8,6 +8,7 @@ class ProtobufC < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "26abd73290938018b40214fbf1d76dba12f9fa5d96bcd51aa719e1793eb0c366"
     sha256 cellar: :any,                 arm64_big_sur:  "1ca90f4286b3c7210aeb15ba9ce34c09972806c1477f40dbd2e7b3bafcdbb275"
+    sha256 cellar: :any,                 monterey:       "5948d96ab2e7476bfab2ca0fe18ae8bdb876ef06cff93664f6faae8735b250c0"
     sha256 cellar: :any,                 big_sur:        "a79fd80a8a0fb8dd05a014cc34ac7281441c167659c0bf1ea36df7be8db3084b"
     sha256 cellar: :any,                 catalina:       "4caae0df2e6727218460e8ef1a0cf18aa0bee6fd14841e6f3456fa325faf4326"
     sha256 cellar: :any,                 mojave:         "96cf2cab8b8b7e5e8228fb5c6bf9bfdd34c7b54ab208eda056ace3648600777a"
