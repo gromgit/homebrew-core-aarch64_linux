ff273e34bae1235f1451882278bf2fea7a1df112
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:18:23 +0000

    diffutils: update 3.8 bottle.

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -8,6 +8,7 @@ class Diffutils < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "239aaf4a4b3e63ade873472f3442e6a910130f1f999b494ee02fa6acffa11c0b"
+    sha256 cellar: :any_skip_relocation, monterey:      "64ddb0cccbc7163969ac093ca0251053991af34393188d3457695a358a0f3034"
     sha256 cellar: :any_skip_relocation, big_sur:       "43ed975b1f8cd9c8aedc16848691972950c2c95405395bc646650fbf8e3d60c5"
     sha256 cellar: :any_skip_relocation, catalina:      "c0a2132f021243dc25d19e6638eea2a423e09957d2c6c11582fc134301fffefd"
     sha256 cellar: :any_skip_relocation, mojave:        "20cf9f34754b7c6c84ff790fe2240e072705a074a2af81f1ca25796801de2780"
