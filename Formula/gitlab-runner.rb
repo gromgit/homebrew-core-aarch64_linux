9599d965525709c8f7d9f8cd0e09f339041c0988
Bo Anderson <mail@boanderson.me>
2021-10-23 23:46:33 +0000

    gitlab-runner: update 14.3.2 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -15,6 +15,7 @@ class GitlabRunner < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c2517f77c58afad8723aa006333df1c933f3dbd44ae6144550fcd3d7d49dbecb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f23ba0ea62f49c9f93ec2881837955ca52a8aaf9cb8e7897ca38715a76a754e4"
+    sha256 cellar: :any_skip_relocation, monterey:       "7e40d5758455c6a45f146c69f6c18ceffe87c59e2991ebc18230256d4ce6c03b"
     sha256 cellar: :any_skip_relocation, big_sur:        "d3f75a4e9d61b9451b1813be0d74ab943ef2ac829eeef0fafb02068455fd43bc"
     sha256 cellar: :any_skip_relocation, catalina:       "5b8db7df68728bd7e13310d8eb77ab75766519d7e3d001c21fc4c2cdd0f62fb1"
     sha256 cellar: :any_skip_relocation, mojave:         "927546c4075cb8329beef754a90afe8d01cf422e3d58dad072c28f3cbd8e53d7"
