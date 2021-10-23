ccdbc8fb6c25cbe8f589af49aceee4994eb663b3
Bo Anderson <mail@boanderson.me>
2021-10-23 23:30:58 +0000

    libraqm: update 0.7.2 bottle.

--- Formula/libraqm.rb
+++ Formula/libraqm.rb
@@ -6,11 +6,12 @@ class Libraqm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "05c31c92e29b7202d42cb892a327ed2c5ca4e7f211020dcf4b54d1d08937479a"
-    sha256 cellar: :any,                 big_sur:       "66f14af1e2afb8aa3f89cb6b2bc0e0274a260bf832250364ec691e398296fe8b"
-    sha256 cellar: :any,                 catalina:      "d619aee4f8e198220d88df6c68ab19ad4b4dbb1a16a4c910ffe923281461d4a5"
-    sha256 cellar: :any,                 mojave:        "cd4063f2520edce76e7a53a676508410ba42bdfe34f3a49c58414f94ea2ab647"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9435ed91d1d18865b406874aedd92b2b7c34e211970d19aaccf550b20a68303a"
+    sha256 cellar: :any,                 arm64_monterey: "31633bafe07acbb111d61d6903d1d31fdfa1442017083a656f45cec235b126a2"
+    sha256 cellar: :any,                 arm64_big_sur:  "05c31c92e29b7202d42cb892a327ed2c5ca4e7f211020dcf4b54d1d08937479a"
+    sha256 cellar: :any,                 big_sur:        "66f14af1e2afb8aa3f89cb6b2bc0e0274a260bf832250364ec691e398296fe8b"
+    sha256 cellar: :any,                 catalina:       "d619aee4f8e198220d88df6c68ab19ad4b4dbb1a16a4c910ffe923281461d4a5"
+    sha256 cellar: :any,                 mojave:         "cd4063f2520edce76e7a53a676508410ba42bdfe34f3a49c58414f94ea2ab647"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9435ed91d1d18865b406874aedd92b2b7c34e211970d19aaccf550b20a68303a"
   end
 
   depends_on "autoconf" => :build
