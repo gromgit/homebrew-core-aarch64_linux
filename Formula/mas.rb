a277fcd0e99d7ce3aae31cd576179a7473d437b0
Bo Anderson <mail@boanderson.me>
2021-10-23 18:08:23 +0000

    mas: update 1.8.3 bottle.

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -8,9 +8,10 @@ class Mas < Formula
   head "https://github.com/mas-cli/mas.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6498917d8d2106fa582aa0fd0714cd87ad7d091023c7b3c6b2db3a6051a2fb5b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0116c8f800780c890dc819ff3479640f3b33ef235af9fe0bc7a53202b35c9b82"
-    sha256 cellar: :any_skip_relocation, catalina:      "015c0e53aee08c429ff468fc2a9c8b0bc973df084c0e6366d92905714f20248b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a6f0cd804841db315c3d253d922aecc23fc3bbcdc530b1bcf4da71998052d4a8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6498917d8d2106fa582aa0fd0714cd87ad7d091023c7b3c6b2db3a6051a2fb5b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0116c8f800780c890dc819ff3479640f3b33ef235af9fe0bc7a53202b35c9b82"
+    sha256 cellar: :any_skip_relocation, catalina:       "015c0e53aee08c429ff468fc2a9c8b0bc973df084c0e6366d92905714f20248b"
   end
 
   depends_on :macos
