fc3ba0c4e768aac736bd482afde84c13e5b6a958
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 15:48:55 +0000

    atk: update 2.36.0 bottle.

--- Formula/atk.rb
+++ Formula/atk.rb
@@ -6,12 +6,13 @@ class Atk < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e7d40dbacc2c965c8b23224a5e1cd2a90d6c54758b957dcf3d66c2238feec518"
-    sha256 cellar: :any, big_sur:       "8321e0ee7364e1de1a3667c50954b4b4f629cba7c2d8077114c4a5bc38a24655"
-    sha256 cellar: :any, catalina:      "1065293046ab2984940dfa0b9c9e724439838e63f685c932d508ccd74bcf921b"
-    sha256 cellar: :any, mojave:        "68c7b621339c03964036877987db69806f663612ba275e68554a97d218a2b5b4"
-    sha256 cellar: :any, high_sierra:   "fa8f525bfeacab676f795bac37f622fc100e63c9e9661fbd6ddd3e1725ebd097"
-    sha256               x86_64_linux:  "22fce3b8041576c4ab184bef170f8902c73ad9b79809b2aa0ef79049f5c5ac43"
+    sha256 cellar: :any, arm64_monterey: "2bbb8f77fc0b5cccd3a01e0d1f18e4f7c26a085f8224430b2e5ea094357e91d6"
+    sha256 cellar: :any, arm64_big_sur:  "e7d40dbacc2c965c8b23224a5e1cd2a90d6c54758b957dcf3d66c2238feec518"
+    sha256 cellar: :any, big_sur:        "8321e0ee7364e1de1a3667c50954b4b4f629cba7c2d8077114c4a5bc38a24655"
+    sha256 cellar: :any, catalina:       "1065293046ab2984940dfa0b9c9e724439838e63f685c932d508ccd74bcf921b"
+    sha256 cellar: :any, mojave:         "68c7b621339c03964036877987db69806f663612ba275e68554a97d218a2b5b4"
+    sha256 cellar: :any, high_sierra:    "fa8f525bfeacab676f795bac37f622fc100e63c9e9661fbd6ddd3e1725ebd097"
+    sha256               x86_64_linux:   "22fce3b8041576c4ab184bef170f8902c73ad9b79809b2aa0ef79049f5c5ac43"
   end
 
   depends_on "gobject-introspection" => :build
