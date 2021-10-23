f777ab929d128bff332b07c2bb87b17585ca6b2b
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:43:21 +0000

    pigz: update 2.6 bottle.

--- Formula/pigz.rb
+++ Formula/pigz.rb
@@ -12,6 +12,7 @@ class Pigz < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "487e4803119cf79952f0604f7c22f400299412210e771d1a5312f75b19903751"
+    sha256 cellar: :any_skip_relocation, monterey:      "2276df8aa169d851c1e063447b977036b227104df789a0221e7a28cbb11746dd"
     sha256 cellar: :any_skip_relocation, big_sur:       "6ed76215af752e06d11b176c3c169a55f76cf2010c01e67e62f7543081137b5e"
     sha256 cellar: :any_skip_relocation, catalina:      "c1e5284c99ef13e0401035597901b5b63d19bbb642fd7206d648193c89d0e6e7"
     sha256 cellar: :any_skip_relocation, mojave:        "0d382426d9bf386f21f8412317b58d05fcaf6ada17b260a1b6e95e8f2e955cf4"
