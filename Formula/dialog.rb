f48eb3615c5872eeb20a336dfc1c99654366c5b7
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 01:51:15 +0000

    dialog: update 1.3-20210621 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -12,6 +12,7 @@ class Dialog < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b46e417d73e065176006d358a36cd293d3f9d881b7b6aea5b2be3fa0a381e721"
+    sha256 cellar: :any_skip_relocation, monterey:      "86185b79fa04eb2379c548a490b747a9969b8e6a1509ff9ed2c1e606bd31e2d9"
     sha256 cellar: :any_skip_relocation, big_sur:       "990bbe5e7a36febc23c79bdbbc454075d695a54dd730e4dab8465d7a66ecab0d"
     sha256 cellar: :any_skip_relocation, catalina:      "87d270268a38de209e9b6fde7c963e4467554d7f0777425f2aa55a3701f25559"
     sha256 cellar: :any_skip_relocation, mojave:        "c05946dd7b57aa11f42ccccd0aab7c9b1506e8093c9dcc60588046aadd4b15dd"
