5589a41fdb11b5a2af1d7d56f5030f71c9ea7332
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:50:58 +0000

    azcopy: update 10.12.2 bottle.

--- Formula/azcopy.rb
+++ Formula/azcopy.rb
@@ -6,12 +6,13 @@ class Azcopy < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1b5695aa8fb6ce52898782c407ee6395fb65532a65c5030d0ab438f6ae40580f"
-    sha256 cellar: :any_skip_relocation, monterey:      "87c0c978170fbecf7b435caa5290e26582f9eed14eb69d08e23794670350c0dd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "895a0df8375076afdd39649821374370c0771a3322cbcbd7f0e052c2157825be"
-    sha256 cellar: :any_skip_relocation, catalina:      "e90ca6c0354d38ab4efff87d95a9859b14d065f4cdc1c76d5a8df80300f1c5b8"
-    sha256 cellar: :any_skip_relocation, mojave:        "cd976cce204fedeca62755455406045d4d3d6303ca625407105c4b61d2fd4262"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "467fbdd5993292c56a2bc3776b2954093f9db2516d0176b98b80756ed3634766"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b69f208199e9c0961a0fd0bf131666d5205e1893a8b0c725267195f6ce5a992"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b5695aa8fb6ce52898782c407ee6395fb65532a65c5030d0ab438f6ae40580f"
+    sha256 cellar: :any_skip_relocation, monterey:       "87c0c978170fbecf7b435caa5290e26582f9eed14eb69d08e23794670350c0dd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "895a0df8375076afdd39649821374370c0771a3322cbcbd7f0e052c2157825be"
+    sha256 cellar: :any_skip_relocation, catalina:       "e90ca6c0354d38ab4efff87d95a9859b14d065f4cdc1c76d5a8df80300f1c5b8"
+    sha256 cellar: :any_skip_relocation, mojave:         "cd976cce204fedeca62755455406045d4d3d6303ca625407105c4b61d2fd4262"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "467fbdd5993292c56a2bc3776b2954093f9db2516d0176b98b80756ed3634766"
   end
 
   depends_on "go" => :build
