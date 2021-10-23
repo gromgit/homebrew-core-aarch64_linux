349e74f0ba32b81939bd940b8e161ee91344e02b
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 03:09:09 +0000

    libkeccak: update 1.2.2 bottle.

--- Formula/libkeccak.rb
+++ Formula/libkeccak.rb
@@ -7,6 +7,7 @@ class Libkeccak < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "bce1f455cd7594f5f8c9681b54b06fe169ee72acc7825726bbe536405c4eef4b"
+    sha256 cellar: :any,                 monterey:      "cbb00c8a160918c4be3dc9d4bd6d73625e58ef3b9215583d59a7f8eef4fc39bf"
     sha256 cellar: :any,                 big_sur:       "57c3066f8d5fd20faff6aa81dfbd926cb60fbdfeacc26770ddfae0f46b6872da"
     sha256 cellar: :any,                 catalina:      "9e1a251941325262eeeb42b3f30404150d794e7a3f223bba1b7557a1458b99aa"
     sha256 cellar: :any,                 mojave:        "bbacd3d46362bb80a2ead391e7f5f614b5be9c89ef3967b1aa287d00e48deba3"
