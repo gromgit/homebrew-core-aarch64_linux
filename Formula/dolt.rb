37d9397f97bcf8f5ca76bf7f6f42bd626de36bc4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 10:12:14 +0000

    dolt: update 0.32.0 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -11,10 +11,11 @@ class Dolt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d323da1ecdfad79e429726736da487c574e997a0ab6602cecbf2fdcd9f4d6bb2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3e0201f4fd6f713c6c7aefdfe951b2942c2752ec7229b3902920b2e2bb2744df"
-    sha256 cellar: :any_skip_relocation, catalina:      "71e3f8a422076b817a07bd3f5fb90f17d640d3c6d1c57fccaa8585c65ed27a40"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d9b7c0b442a86fd62f2853bd5cba55c8aeba74f04c6bb9c64bf8d411c8de528"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "448b4d4e9c50dfb4de8a445a0896c76fbf857e37bdd7d83b86d42b06b7c555ae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d323da1ecdfad79e429726736da487c574e997a0ab6602cecbf2fdcd9f4d6bb2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3e0201f4fd6f713c6c7aefdfe951b2942c2752ec7229b3902920b2e2bb2744df"
+    sha256 cellar: :any_skip_relocation, catalina:       "71e3f8a422076b817a07bd3f5fb90f17d640d3c6d1c57fccaa8585c65ed27a40"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3d9b7c0b442a86fd62f2853bd5cba55c8aeba74f04c6bb9c64bf8d411c8de528"
   end
 
   depends_on "go" => :build
