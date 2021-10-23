2fdd20b0303f7b3114532137791adb043c84fcc3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 08:30:13 +0000

    libslirp: update 4.6.1 bottle.

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -6,11 +6,12 @@ class Libslirp < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "93fca2e4a0c689c366680a9b56ca0c90cb424eb8fb00136a170cea67e7919d67"
-    sha256 cellar: :any, big_sur:       "120eb1362ba0645a96ace10153fb41b5669f0a32669947cca96b2e1b3108edd3"
-    sha256 cellar: :any, catalina:      "fc267a6871f5459a38e174ffd519a3016c849b6c99c2bce2ca714c20b71ae1b6"
-    sha256 cellar: :any, mojave:        "3a19812499b688c6698c0e50b1b0a567727cf9a5af26ed71ff99a412a98f1c44"
-    sha256               x86_64_linux:  "e49c9b8b88c0f9cb434cac8b488d38450bc2b148f71ba934c7562437a5b46c58"
+    sha256 cellar: :any, arm64_monterey: "475c2d9ad2e1a2b44c52e9ea1cd5627d392adb216b8e9969d059a5e17d9dafc5"
+    sha256 cellar: :any, arm64_big_sur:  "93fca2e4a0c689c366680a9b56ca0c90cb424eb8fb00136a170cea67e7919d67"
+    sha256 cellar: :any, big_sur:        "120eb1362ba0645a96ace10153fb41b5669f0a32669947cca96b2e1b3108edd3"
+    sha256 cellar: :any, catalina:       "fc267a6871f5459a38e174ffd519a3016c849b6c99c2bce2ca714c20b71ae1b6"
+    sha256 cellar: :any, mojave:         "3a19812499b688c6698c0e50b1b0a567727cf9a5af26ed71ff99a412a98f1c44"
+    sha256               x86_64_linux:   "e49c9b8b88c0f9cb434cac8b488d38450bc2b148f71ba934c7562437a5b46c58"
   end
 
   depends_on "meson" => :build
