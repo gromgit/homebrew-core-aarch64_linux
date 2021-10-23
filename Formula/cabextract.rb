d6b5b4a39c3d3329d8495a98e37484c0a62ebfdd
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:06:22 +0000

    cabextract: update 1.9.1 bottle.

--- Formula/cabextract.rb
+++ Formula/cabextract.rb
@@ -12,6 +12,7 @@ class Cabextract < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "f421b4d93548f37080a4041a1ae83cffa6e0407489913ea09525e3e3f482d39e"
+    sha256 cellar: :any_skip_relocation, monterey:      "293895188547de1614324e2830e80f7605ee63b0c8019961e413a8c11c4e2fb7"
     sha256 cellar: :any_skip_relocation, big_sur:       "1ddac23f5e64926d1f2cd400e6fa0739db93dc0517712965b2b0ca1b3e74eabd"
     sha256 cellar: :any_skip_relocation, catalina:      "d60179c028ac5fb69580f2f01cd9f59c1d1544c8f6d84a230a7dd3587f3c27e0"
     sha256 cellar: :any_skip_relocation, mojave:        "cd27b939a0191d4dfff8ae13300b260b5ae01c563a21613718160012a982d5e8"
