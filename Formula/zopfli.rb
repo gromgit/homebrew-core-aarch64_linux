8cd007e2740c3849b0eb10ab52705ca278fbbb14
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 03:02:24 +0000

    zopfli: update 1.0.3 bottle.

--- Formula/zopfli.rb
+++ Formula/zopfli.rb
@@ -8,6 +8,7 @@ class Zopfli < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "8713f7f74eeab80dad2dfe679e985c11479f86385e0d4de4673365d47c7683ff"
+    sha256 cellar: :any_skip_relocation, monterey:      "12d9e6837065e018b0562beba8abf42a485163a712e954efc9cda5181db92179"
     sha256 cellar: :any_skip_relocation, big_sur:       "1d60005d0944419b4f6c570c66b906701411d380030f7b6f148347b3b9fbff04"
     sha256 cellar: :any_skip_relocation, catalina:      "aa44f1667254347800d442b6347d6df658e3ee24386d2284cce8e88a27e87d6d"
     sha256 cellar: :any_skip_relocation, mojave:        "521a5185b6881c878be60af7df7c673f5845255f957b88d01307eb9220407a52"
