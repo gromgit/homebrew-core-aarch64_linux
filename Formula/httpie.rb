2e2ec9c7d688a214e91cd244622718edbc3deac7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 08:11:48 +0000

    httpie: update 2.6.0 bottle.

--- Formula/httpie.rb
+++ Formula/httpie.rb
@@ -9,11 +9,12 @@ class Httpie < Formula
   head "https://github.com/httpie/httpie.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3c3a5c2458d0658e14b663495e115297c573aa3466d292f12d02c3ec13a24bdf"
-    sha256 cellar: :any_skip_relocation, big_sur:       "377b0643aa1f6d310ba4cfc70d66a94cc458213db8d134940d3b10a32defacf1"
-    sha256 cellar: :any_skip_relocation, catalina:      "6d306c30f6f1d7a551d88415efe12b7c3f25d0602f3579dc632771a463f78fa5"
-    sha256 cellar: :any_skip_relocation, mojave:        "f66b8cdff9cb7b44a84197c3e3d81d810f7ff8f2188998b977ccadfc7e2ec893"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "53f036b0114814c28982e8c022dcf494e7024de088641d7076fd73d12a45a0e9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "83aab05ffbcd4c3baa6de6158d57ebdaa67c148bef8c872527d90bdaebff0504"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c3a5c2458d0658e14b663495e115297c573aa3466d292f12d02c3ec13a24bdf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "377b0643aa1f6d310ba4cfc70d66a94cc458213db8d134940d3b10a32defacf1"
+    sha256 cellar: :any_skip_relocation, catalina:       "6d306c30f6f1d7a551d88415efe12b7c3f25d0602f3579dc632771a463f78fa5"
+    sha256 cellar: :any_skip_relocation, mojave:         "f66b8cdff9cb7b44a84197c3e3d81d810f7ff8f2188998b977ccadfc7e2ec893"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53f036b0114814c28982e8c022dcf494e7024de088641d7076fd73d12a45a0e9"
   end
 
   depends_on "python@3.10"
