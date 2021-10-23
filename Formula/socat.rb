4ab5f2954448e12b9251352ee1da75d1484ae281
Bo Anderson <mail@boanderson.me>
2021-10-23 23:46:02 +0000

    socat: update 1.7.4.1 bottle.

--- Formula/socat.rb
+++ Formula/socat.rb
@@ -13,6 +13,7 @@ class Socat < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "a1d69b34c205acecf9cd848a83884675e730920749ed3d5b2cb3d2770d4566bd"
     sha256 cellar: :any,                 arm64_big_sur:  "1d355658a55eb44cb6ffe1fa8dc140883359467080e13be0d4237cf181c05dc0"
+    sha256 cellar: :any,                 monterey:       "b6fb68ad1092c12e0ccac75526ab19bb3714adc1605eb4a3757c238a2430c71a"
     sha256 cellar: :any,                 big_sur:        "2249d3b3852d95fc683e27292e26967b0e3a13d60e59a99181445f941a343a32"
     sha256 cellar: :any,                 catalina:       "f2a0d0d0bca542cb0f4b700d42dc244e82b8da9be2d5aff8d98b8a7fef77c9fe"
     sha256 cellar: :any,                 mojave:         "531f3ea55671c8d01165c3a314b24cef873c51442a1729fe2e9ce14ff908aebb"
