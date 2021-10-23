f40a459c047f2831743dac5d779658e7740a500c
Bo Anderson <mail@boanderson.me>
2021-10-23 18:39:31 +0000

    aws-iam-authenticator: update 0.5.3 bottle.

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -10,6 +10,7 @@ class AwsIamAuthenticator < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e82311b2f11745925a8ce73a05c24e38c8122063b6ce070a4610c8d54f5fda76"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30639bda3ecb72388aeabc0f3e6587f58bc9867dc6b3a9f3046e9d0ef659f122"
+    sha256 cellar: :any_skip_relocation, monterey:       "e3718d03e98702a4893b480a897779e7ae8ca52b39f879d02aa6fbf0a1220f8d"
     sha256 cellar: :any_skip_relocation, big_sur:        "a161d4ea3ef00ab85c8f0cea198e0dafde39cd3a76359b75ea22521c35eac7d6"
     sha256 cellar: :any_skip_relocation, catalina:       "192ace97a0c76ed0aceda060290db6abcde5af8ad7dfc30faaf5f4ed4ae92b6a"
     sha256 cellar: :any_skip_relocation, mojave:         "c7f23ccd6bdd852a7c1928df437381204aa9da22a2f41eb3a5776864838f39db"
