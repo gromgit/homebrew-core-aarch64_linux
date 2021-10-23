4b908c79594ab6a7da7670d0b731f31c7124092a
Bo Anderson <mail@boanderson.me>
2021-10-23 23:35:07 +0000

    rclone: update 1.56.2 bottle.

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -7,11 +7,12 @@ class Rclone < Formula
   head "https://github.com/rclone/rclone.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "957e720c43d34dab552b432aa5b7e1c1f8248fcc710636bb7d0f9ffc04865539"
-    sha256 cellar: :any_skip_relocation, big_sur:       "33790b4ad68970d136d97859a771bd12ea17eba644c70a64ac8f848e7adbefa2"
-    sha256 cellar: :any_skip_relocation, catalina:      "89eba48543a11570324e2a96e56b0962486969fd34ea07ff41fb278769dfe2f8"
-    sha256 cellar: :any_skip_relocation, mojave:        "4d4d8134ca7c9cd27cf242fafac39c1ab90fa51ac592d47d02c1a87b9e0daf26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e53ec3ae829bd6f86f8d83d332ecdedd53e1bc16646e3b815cc85b3fc8002c5c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1cffb95b9ddb395d2b2aff4d046ae5f9de8ad10e4fdb38261ccd763e1fd2629b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "957e720c43d34dab552b432aa5b7e1c1f8248fcc710636bb7d0f9ffc04865539"
+    sha256 cellar: :any_skip_relocation, big_sur:        "33790b4ad68970d136d97859a771bd12ea17eba644c70a64ac8f848e7adbefa2"
+    sha256 cellar: :any_skip_relocation, catalina:       "89eba48543a11570324e2a96e56b0962486969fd34ea07ff41fb278769dfe2f8"
+    sha256 cellar: :any_skip_relocation, mojave:         "4d4d8134ca7c9cd27cf242fafac39c1ab90fa51ac592d47d02c1a87b9e0daf26"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e53ec3ae829bd6f86f8d83d332ecdedd53e1bc16646e3b815cc85b3fc8002c5c"
   end
 
   depends_on "go" => :build
