d12bf848f0265d63bb50406437d3df70463dbf4a
Bo Anderson <mail@boanderson.me>
2021-10-23 20:27:23 +0000

    vde: update 2.3.2_1 bottle.

--- Formula/vde.rb
+++ Formula/vde.rb
@@ -14,6 +14,7 @@ class Vde < Formula
   bottle do
     sha256 arm64_monterey: "e4d5fbb28025cb50acf1a1c8e11a2aeb33e1324b42b49d7f3709fab81a708c55"
     sha256 arm64_big_sur:  "d504166629275fb173304ee78b134a6c5b5eabba65c054f2fede1949204382dd"
+    sha256 monterey:       "c043ada3aefd2f0a9eeb6f60db1003cc6b340da282d7fb93d940be47aac9fc6b"
     sha256 big_sur:        "f634d3558c44876138a229f06554ab603b31e412a03c049d96f6c3616e579729"
     sha256 catalina:       "711f5b171e033b92505178b35a324a5c21e806ed5054a92ef02f26b3a38a760e"
     sha256 mojave:         "4f880ec345fe86fdfcfc53468c7c24d160261a17ee71a289ea3357a47b71416c"
