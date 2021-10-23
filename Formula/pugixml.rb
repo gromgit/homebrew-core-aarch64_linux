d5c61f465cb12ff9df8d897ed57638a3be404ceb
Bo Anderson <mail@boanderson.me>
2021-10-23 18:44:06 +0000

    pugixml: update 1.11.4_2 bottle.

--- Formula/pugixml.rb
+++ Formula/pugixml.rb
@@ -12,11 +12,12 @@ class Pugixml < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "72487f35e935099779ea88bbbf6dd205406710df262674e6295972edff2a9ac1"
-    sha256 cellar: :any,                 big_sur:       "450e11f4eafe21828d3987620406eb42695a60c15086b7741898d483bb05fa8d"
-    sha256 cellar: :any,                 catalina:      "112dda2780766cf7403426252180cea172cd396f7b52aee42a690aa7539c933b"
-    sha256 cellar: :any,                 mojave:        "60d558fea876933be7a5322267ca58c0850eb23ec05d71b3d2c3876793b01367"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c6e3c671984d4a3c53e58f79ecf45be27b3c57d1073223aa2c0c4dde91aa80e"
+    sha256 cellar: :any,                 arm64_monterey: "a5284ea4e8d6fa76cd41505294e23a4c448b54d888de993b3cf57308ccbb5cd7"
+    sha256 cellar: :any,                 arm64_big_sur:  "72487f35e935099779ea88bbbf6dd205406710df262674e6295972edff2a9ac1"
+    sha256 cellar: :any,                 big_sur:        "450e11f4eafe21828d3987620406eb42695a60c15086b7741898d483bb05fa8d"
+    sha256 cellar: :any,                 catalina:       "112dda2780766cf7403426252180cea172cd396f7b52aee42a690aa7539c933b"
+    sha256 cellar: :any,                 mojave:         "60d558fea876933be7a5322267ca58c0850eb23ec05d71b3d2c3876793b01367"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c6e3c671984d4a3c53e58f79ecf45be27b3c57d1073223aa2c0c4dde91aa80e"
   end
 
   depends_on "cmake" => :build
