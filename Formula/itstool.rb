ba4c4f1972cffc29a2652b00480cbe169f3af003
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 15:04:41 +0000

    itstool: update 2.0.7 bottle.

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -7,11 +7,12 @@ class Itstool < Formula
   head "https://github.com/itstool/itstool.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, catalina:      "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, mojave:        "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
+    sha256 cellar: :any_skip_relocation, catalina:       "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
+    sha256 cellar: :any_skip_relocation, mojave:         "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
   end
 
   depends_on "autoconf" => :build
