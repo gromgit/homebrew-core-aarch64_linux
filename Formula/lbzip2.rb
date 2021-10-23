a5a2efda0adba522f9bb826478b1fe5e5d83df06
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 03:30:32 +0000

    lbzip2: update 2.5_1 bottle.

--- Formula/lbzip2.rb
+++ Formula/lbzip2.rb
@@ -8,13 +8,14 @@ class Lbzip2 < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e9ecc58d178f18ab33d500ed768c156058589fed3132bf804314e76715730333"
-    sha256 cellar: :any_skip_relocation, monterey:      "b9882075d6ce9ed47c0e18cc27c9b23706a8ceedc8ff42977b614f8fc92cdb57"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4bb02d26e53336134329f3aaacf2ce045375b926b283520788ecdf2ae4d778e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "6643ba1c0f17a13e742383c69112df62c1d6bce80e6833d717df4e112922deb5"
-    sha256 cellar: :any_skip_relocation, mojave:        "5f7f053aac95586cdcacb2528fe4540bd16522707e9d7bbbf8e6d38012378e06"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "3d4e0de242b81f83ba2addd163688647288fb17f3a3ae3ccd37a2e62f20871d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88137d3cccfc08f362d926aeaad73312aa4d5265dd89fc92c881b1da20097f6f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9815b21683195fdeda12fe2ec7b2f4336d34e7a4b44a4a318c67efed6f9e035e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e9ecc58d178f18ab33d500ed768c156058589fed3132bf804314e76715730333"
+    sha256 cellar: :any_skip_relocation, monterey:       "b9882075d6ce9ed47c0e18cc27c9b23706a8ceedc8ff42977b614f8fc92cdb57"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4bb02d26e53336134329f3aaacf2ce045375b926b283520788ecdf2ae4d778e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "6643ba1c0f17a13e742383c69112df62c1d6bce80e6833d717df4e112922deb5"
+    sha256 cellar: :any_skip_relocation, mojave:         "5f7f053aac95586cdcacb2528fe4540bd16522707e9d7bbbf8e6d38012378e06"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "3d4e0de242b81f83ba2addd163688647288fb17f3a3ae3ccd37a2e62f20871d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88137d3cccfc08f362d926aeaad73312aa4d5265dd89fc92c881b1da20097f6f"
   end
 
   # Fix crash on macOS >= 10.13.
