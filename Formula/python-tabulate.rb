0fde2435d4a58b4723089b5c49b219bd39e079fe
Bo Anderson <mail@boanderson.me>
2021-10-23 20:10:25 +0000

    python-tabulate: update 0.8.9 bottle.

--- Formula/python-tabulate.rb
+++ Formula/python-tabulate.rb
@@ -9,6 +9,7 @@ class PythonTabulate < Formula
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f5bd465f5fd9a0e3eb107c4cdd244b63090a2ce102346f7d7fecfcdbabc1812a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2beca641f9753d21b695dd61d57f2557dee3231c78dad9d7e5c456554b792f44"
+    sha256 cellar: :any_skip_relocation, monterey:       "f5bd465f5fd9a0e3eb107c4cdd244b63090a2ce102346f7d7fecfcdbabc1812a"
     sha256 cellar: :any_skip_relocation, big_sur:        "c6dc3b60616b7fc6d810a17471bdd2ca872224560a5283039eae5dd618b8c65d"
     sha256 cellar: :any_skip_relocation, catalina:       "9e8e0c24b82a80db0e474a1cbb4cfd4e8628513fc8d70dfe6dde0c49b197df17"
     sha256 cellar: :any_skip_relocation, mojave:         "3d9e98f92133a5c058618584b0178645eafb0230531fbef665d3ed2546f63f0f"
