190269384f41bf62f587c29c220538b948163584
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 08:36:48 +0000

    watchman: update 2021.10.18.00 bottle.

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -9,6 +9,7 @@ class Watchman < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "a0827f828c341593562e222d7ba86993360cd5e075983b5997ffb578343708b8"
     sha256 cellar: :any, arm64_big_sur:  "c9e25436ddb021caa98115bd1b713286e91dd40e59de8797f0c00bea18f478f2"
+    sha256 cellar: :any, monterey:       "f348042c89870ac25dc608f475ae14ce129737030733c34a27ee7de3d5181dbd"
     sha256 cellar: :any, big_sur:        "3d952cb346ed6f9318aeb68122d6b81b3dc9b0fbeab0290993f21f020d11680f"
     sha256 cellar: :any, catalina:       "3cd12de321b36671f0f6bb0fbc3dced352ecaa291d6bac5920583ee48bd05b8e"
     sha256 cellar: :any, mojave:         "23ded350dccb7e1d00412c58f8b99add97a1ce0ba1e5ec11e683b8eaf26e1140"
