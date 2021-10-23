c44774669758806c09cd4ad80916a112bff8fcc3
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:40:54 +0000

    ucl: update 1.03 bottle.

--- Formula/ucl.rb
+++ Formula/ucl.rb
@@ -12,12 +12,13 @@ class Ucl < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ef63a86669c63e486c3682494a93c9db453a33089a2a71398efb8e5e26cf5e44"
-    sha256 cellar: :any_skip_relocation, monterey:      "4317885999b8297a1919d5d65a9246efdd7bc1807fd2df4a9268a202fbf3a97c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "91ce0597dc8e648e4ee0d0caaa30bceb5f569acc90634d88fa5e7859f2ae682a"
-    sha256 cellar: :any_skip_relocation, catalina:      "116db1f8157bf88831fece730fb3e6fa82420d53c29b032afd63b979df42b386"
-    sha256 cellar: :any_skip_relocation, mojave:        "89c37d38b41d5107f85c0880eb1599c885dafc2a7150a378c645b3fbe1f0e5ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f1733761c7ce4452219f15055b4d72ca6e287c9c18691d9aa66c4aae0349d28c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4c7f82e2d2c969d71a2dbaca2cc6c0f2577c422a16281a981e3193535263803c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ef63a86669c63e486c3682494a93c9db453a33089a2a71398efb8e5e26cf5e44"
+    sha256 cellar: :any_skip_relocation, monterey:       "4317885999b8297a1919d5d65a9246efdd7bc1807fd2df4a9268a202fbf3a97c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "91ce0597dc8e648e4ee0d0caaa30bceb5f569acc90634d88fa5e7859f2ae682a"
+    sha256 cellar: :any_skip_relocation, catalina:       "116db1f8157bf88831fece730fb3e6fa82420d53c29b032afd63b979df42b386"
+    sha256 cellar: :any_skip_relocation, mojave:         "89c37d38b41d5107f85c0880eb1599c885dafc2a7150a378c645b3fbe1f0e5ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f1733761c7ce4452219f15055b4d72ca6e287c9c18691d9aa66c4aae0349d28c"
   end
 
   depends_on "automake" => :build
