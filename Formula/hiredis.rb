d571f5bcb6599166788480e2df8e9f7432134e26
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 10:27:18 +0000

    hiredis: update 1.0.2 bottle.

--- Formula/hiredis.rb
+++ Formula/hiredis.rb
@@ -9,6 +9,7 @@ class Hiredis < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "e499ad54baf866fd665c1160db82f235cd9cb84f8ae369f0be4a14a52e81eae0"
     sha256 cellar: :any,                 arm64_big_sur:  "95224205d629a4e8715c1e66d722cb580ccd4c78ce5e943f70cd8bde0021e641"
+    sha256 cellar: :any,                 monterey:       "61d0cc662645452391544eaed8720a8043d65ed742a6119166d6b6d98ea62a3f"
     sha256 cellar: :any,                 big_sur:        "5cbf446863123927636711ced21b342fa66568de9b25ec793fd54d3b3b53ca41"
     sha256 cellar: :any,                 catalina:       "14ae7b3adb354b673a3744e9d849d6698846d5162d3d5f0eb8f9d8837c858e75"
     sha256 cellar: :any,                 mojave:         "050805a747642516f0b8a9573f2b2935de26089b7f0380b84f6f8a6e4ab41b50"
