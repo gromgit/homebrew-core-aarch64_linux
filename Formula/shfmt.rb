e81635ea2094e482c674804dccd3765f839b2315
Bo Anderson <mail@boanderson.me>
2021-10-23 22:16:23 +0000

    shfmt: update 3.4.0 bottle.

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -9,6 +9,7 @@ class Shfmt < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "972fa05139ca8b1111455370a6e8be19a050067ebe9d176c1c05e3d1ee3d658c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "38f0107e79a8462e0813785ac59fad4cb6a27ad71f0e7c97d967b228a00604a9"
+    sha256 cellar: :any_skip_relocation, monterey:       "8d2a8ce09acee0fb1e652db47f805fd06992490fb856d95c778911dd0d8ddc34"
     sha256 cellar: :any_skip_relocation, big_sur:        "8200fcf756b23f3e06fd07437b332d813280a3bb317e41a1d2b02e1f442a1966"
     sha256 cellar: :any_skip_relocation, catalina:       "73afc11e5d8f378dd818ebb0f7b16ffe1444742b06189a6ae344da867f3eec02"
     sha256 cellar: :any_skip_relocation, mojave:         "a5bd6ad427afcf75b6fe3aea673377d6664f295cc6c5b09bb49825fb1957df40"
