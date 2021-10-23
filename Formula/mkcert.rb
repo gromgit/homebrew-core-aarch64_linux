9239cdb1f75fcf83068a4515f44da9dc6ebbb716
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 01:51:11 +0000

    mkcert: update 1.4.3 bottle.

--- Formula/mkcert.rb
+++ Formula/mkcert.rb
@@ -8,6 +8,7 @@ class Mkcert < Formula
   bottle do
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "053f02796ab0165faaabc470cc161559d3ba5062b5e56f6df1bbd46a828f4991"
+    sha256 cellar: :any_skip_relocation, monterey:      "5cf785059a301673a09850b1349afc0a799828e57eaec2e6423dbba75606da51"
     sha256 cellar: :any_skip_relocation, big_sur:       "4dc2370651718c72f2484c81a6dd5813cb7fcf6a5ec6bb1bee94e1720d23d412"
     sha256 cellar: :any_skip_relocation, catalina:      "92ac9e87e65741d1cadb0372b259291dcd726fe1048715cfc993053cb62273e1"
     sha256 cellar: :any_skip_relocation, mojave:        "49c14e8620ffb1dc44d587eea2a6c329bac516f24d209d08b656b0c21af4e3ac"
