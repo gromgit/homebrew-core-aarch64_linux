1dc96921a8179797233f5496da95ee9a3e14ad89
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 03:25:45 +0000

    opencc: update 1.1.3 bottle.

--- Formula/opencc.rb
+++ Formula/opencc.rb
@@ -6,12 +6,13 @@ class Opencc < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 arm64_big_sur: "c511e94c8ede779f36276e9149f503e88bf9241ed06c15b5f1b667eb66b6a93d"
-    sha256 monterey:      "7c64d8b156a92d9f94b6e10dcfbb065d4cc20024215d348a161970db05bad282"
-    sha256 big_sur:       "f03bc24b794a0be72ffeec4b97ad2a7ef350cfbcce48c27480720bfa2b5ddbbe"
-    sha256 catalina:      "71a2e9d6df44f77c60ee8bfb22f355dc7cd073ce58a2e990a7e1a9c54039a9a0"
-    sha256 mojave:        "d35684ce9298dca475a9f30318e86f8209aef8df8e06a0a930b3d2d500f7bb2f"
-    sha256 x86_64_linux:  "298d3cd7f7918b43d6e5c5f0d533b609b538d35bebf7f1af48f943d13278971c"
+    sha256 arm64_monterey: "858a21a759d237f29b6e882a969eea5c215377a8d8289fae04a081f78c5e2821"
+    sha256 arm64_big_sur:  "c511e94c8ede779f36276e9149f503e88bf9241ed06c15b5f1b667eb66b6a93d"
+    sha256 monterey:       "7c64d8b156a92d9f94b6e10dcfbb065d4cc20024215d348a161970db05bad282"
+    sha256 big_sur:        "f03bc24b794a0be72ffeec4b97ad2a7ef350cfbcce48c27480720bfa2b5ddbbe"
+    sha256 catalina:       "71a2e9d6df44f77c60ee8bfb22f355dc7cd073ce58a2e990a7e1a9c54039a9a0"
+    sha256 mojave:         "d35684ce9298dca475a9f30318e86f8209aef8df8e06a0a930b3d2d500f7bb2f"
+    sha256 x86_64_linux:   "298d3cd7f7918b43d6e5c5f0d533b609b538d35bebf7f1af48f943d13278971c"
   end
 
   depends_on "cmake" => :build
