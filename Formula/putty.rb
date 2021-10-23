fd603bb097dce3725169cba0a327f09cf7c6d2e7
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 01:10:12 +0000

    putty: update 0.76 bottle.

--- Formula/putty.rb
+++ Formula/putty.rb
@@ -6,12 +6,13 @@ class Putty < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "adb57691d42b70e51a4336dc37126996c782f2bf66db5d0f05813ebd04178ebf"
-    sha256 cellar: :any_skip_relocation, monterey:      "ab3cc934eace1bfe68e8a8896888b1c1213a68d18c4591f59df3f9312e9fbb0a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "75e23ad8100002d5ade0acf3745f4f40a9add28a25ea4814caafd0cb37be7cb8"
-    sha256 cellar: :any_skip_relocation, catalina:      "ab58a1de02894bd5364c31e4a5b864acb81cbc0160814d048f98077bfe01a4b1"
-    sha256 cellar: :any_skip_relocation, mojave:        "4aab22d3d6867678be1d1be95edb5fe41fcd7d807892b00b83d0280b6f356f46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51689b0cb6c740349175dad1786ac7786b639e78f5e5b3765272ae83162009d4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d4ca114d39cb55f75ef0b71f496bb96acf2e3f97e10e66d3ea7de8a4c87dc51b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "adb57691d42b70e51a4336dc37126996c782f2bf66db5d0f05813ebd04178ebf"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab3cc934eace1bfe68e8a8896888b1c1213a68d18c4591f59df3f9312e9fbb0a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "75e23ad8100002d5ade0acf3745f4f40a9add28a25ea4814caafd0cb37be7cb8"
+    sha256 cellar: :any_skip_relocation, catalina:       "ab58a1de02894bd5364c31e4a5b864acb81cbc0160814d048f98077bfe01a4b1"
+    sha256 cellar: :any_skip_relocation, mojave:         "4aab22d3d6867678be1d1be95edb5fe41fcd7d807892b00b83d0280b6f356f46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51689b0cb6c740349175dad1786ac7786b639e78f5e5b3765272ae83162009d4"
   end
 
   head do
