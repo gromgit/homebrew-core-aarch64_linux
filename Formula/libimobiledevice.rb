3af711113613c078c5238271bb3971896bcfebbc
Bo Anderson <mail@boanderson.me>
2021-10-23 18:54:44 +0000

    libimobiledevice: update 1.3.0 bottle.

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -8,6 +8,7 @@ class Libimobiledevice < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "f3c97e567f59c4a8ab79f8a3d66a32d109fc9a7c22891589b998edb6a4e5ba28"
     sha256 cellar: :any,                 arm64_big_sur:  "41a64c9856f7845bb4c21bba4f42eb55c640301b59c032eb4db416db19ecf97d"
+    sha256 cellar: :any,                 monterey:       "2cde67c8eef4e971ce74428a9162e9680d7a9ab542571f438602efe431d3a121"
     sha256 cellar: :any,                 big_sur:        "0fe21433f470130b972354d411d05f43ab37d82198565bb6b947734a95e98c5d"
     sha256 cellar: :any,                 catalina:       "eb7f28d86797461d5ef859d00629176e1ce3234790ef17b9ee3f9c9990a664e2"
     sha256 cellar: :any,                 mojave:         "5143eaf34011a22dd1951f10495a7568e77a2e862fb9f4dbae9bab2f784f926e"
