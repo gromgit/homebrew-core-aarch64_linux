7e1420aea98015c87995885ce7afb12d77468050
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:16:39 +0000

    moreutils: update 0.66 bottle.

--- Formula/moreutils.rb
+++ Formula/moreutils.rb
@@ -11,12 +11,13 @@ class Moreutils < Formula
   head "https://git.joeyh.name/git/moreutils.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b7bb6a5498e898c303cbbb526f83a12e690315c43260ebab1cf348ecb2a712bb"
-    sha256 cellar: :any_skip_relocation, monterey:      "4d5c2460a0d242df46376e618207038722ffc65a9e3e6379fdd229ce821f4d52"
-    sha256 cellar: :any_skip_relocation, big_sur:       "98e36f509fe2556660dec088c1dcf380cf1ce60167fcb4289782614feb381821"
-    sha256 cellar: :any_skip_relocation, catalina:      "e7774183139434a9f4707a5931793fb2aed0678fd0257a5860571f320f507c8e"
-    sha256 cellar: :any_skip_relocation, mojave:        "099c5b0cc96ae204c211da65446db79bf83469ed0bcbc40d81e5a9e95b5a678c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0890cc88f2e311ec01cc0439cbfa6a7a43166ebaeb54a8bfa9d22fc21fba4bb4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c93055c0a353c93a20b0421a85eb055cc54340d1793cf12dd0fad275be123b71"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7bb6a5498e898c303cbbb526f83a12e690315c43260ebab1cf348ecb2a712bb"
+    sha256 cellar: :any_skip_relocation, monterey:       "4d5c2460a0d242df46376e618207038722ffc65a9e3e6379fdd229ce821f4d52"
+    sha256 cellar: :any_skip_relocation, big_sur:        "98e36f509fe2556660dec088c1dcf380cf1ce60167fcb4289782614feb381821"
+    sha256 cellar: :any_skip_relocation, catalina:       "e7774183139434a9f4707a5931793fb2aed0678fd0257a5860571f320f507c8e"
+    sha256 cellar: :any_skip_relocation, mojave:         "099c5b0cc96ae204c211da65446db79bf83469ed0bcbc40d81e5a9e95b5a678c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0890cc88f2e311ec01cc0439cbfa6a7a43166ebaeb54a8bfa9d22fc21fba4bb4"
   end
 
   depends_on "docbook-xsl" => :build
