d76db57edd435ca5056b02bac7dd4963117fc6ff
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-23 02:30:48 +0000

    re2c: update 2.2 bottle.

--- Formula/re2c.rb
+++ Formula/re2c.rb
@@ -6,12 +6,13 @@ class Re2c < Formula
   license :public_domain
 
   bottle do
-    sha256 arm64_big_sur: "9f32566ee362b6ebf98674a7bc9d642c03a120c105b32b28efd16ec1fdfe1dbc"
-    sha256 monterey:      "5ae23f0895217f59237686bdbfc4c2e4c40d4d0ac1e6e45f5032e874c9f9f3ba"
-    sha256 big_sur:       "622c04bf38ad6f7ff7f9ca272cf9dcbde186f0b16e3e51eed1ffc108db56adb7"
-    sha256 catalina:      "8dd19c7d8532026141d42d6893faf18e86b97053fd113f9f30a1271662108e25"
-    sha256 mojave:        "7092db76fdd568e0c162ffe2066e8318ec4102aab85b361098908ca49f535a0e"
-    sha256 x86_64_linux:  "f8ae38af6e0ef9a4131174fc66750498c1a3cca66e896ef2392632ef16a0e9a4"
+    sha256 arm64_monterey: "4a95c3cdce967731404c853fe7c56f2e37c9602710af795987780acf5e30c383"
+    sha256 arm64_big_sur:  "9f32566ee362b6ebf98674a7bc9d642c03a120c105b32b28efd16ec1fdfe1dbc"
+    sha256 monterey:       "5ae23f0895217f59237686bdbfc4c2e4c40d4d0ac1e6e45f5032e874c9f9f3ba"
+    sha256 big_sur:        "622c04bf38ad6f7ff7f9ca272cf9dcbde186f0b16e3e51eed1ffc108db56adb7"
+    sha256 catalina:       "8dd19c7d8532026141d42d6893faf18e86b97053fd113f9f30a1271662108e25"
+    sha256 mojave:         "7092db76fdd568e0c162ffe2066e8318ec4102aab85b361098908ca49f535a0e"
+    sha256 x86_64_linux:   "f8ae38af6e0ef9a4131174fc66750498c1a3cca66e896ef2392632ef16a0e9a4"
   end
 
   def install
