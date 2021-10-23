8ba1b450196ada03e2611396f10201f8dc8297b5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-23 15:42:20 +0000

    uchardet: update 0.0.7 bottle.

--- Formula/uchardet.rb
+++ Formula/uchardet.rb
@@ -8,6 +8,7 @@ class Uchardet < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "6f144a202080194afbba458511b7250a27a5b0ff5f0baa4f81a4ab4148906aa5"
     sha256 cellar: :any,                 arm64_big_sur:  "33989aa08f1ffc6f71f536c2a3cb28aabcc774e90da2aa40db110048f4b38238"
+    sha256 cellar: :any,                 monterey:       "2b32584a9ef94d89025ed7b2d027608013571f12e30b363fa46bfddd5aab2893"
     sha256 cellar: :any,                 big_sur:        "f5125cd193034470e278536d8b5d2498e99ddbd5c2853e214fe24fe845cc56d7"
     sha256 cellar: :any,                 catalina:       "34bd4791834a762da961de136a3cad253800a98deea2d1dededf8efb465f215d"
     sha256 cellar: :any,                 mojave:         "a254139ee777de77d5907dfeb47d4306f2e5e5f0b3775edfedcbf1fcf217fdbe"
