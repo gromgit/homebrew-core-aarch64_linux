71f98c6df67feffd1018978b85a4abe725017909
Bo Anderson <mail@boanderson.me>
2021-10-23 19:32:58 +0000

    libexif: update 0.6.23 bottle.

--- Formula/libexif.rb
+++ Formula/libexif.rb
@@ -6,11 +6,12 @@ class Libexif < Formula
   license "LGPL-2.1"
 
   bottle do
-    sha256 arm64_big_sur: "f2a133f663a82d1d39ba238692f6c56de3c6bae12add2107204a75af2f6d923a"
-    sha256 big_sur:       "17a348763e827089c2d2fb2b2c631f38df87d99234d81f2bbbe1c7219577522c"
-    sha256 catalina:      "5b2a8266f5236b9fe1921040976c07d9eda3cc178a2c2d82824254de82df0e2e"
-    sha256 mojave:        "85e039d08a8668365cd7f71a9012c587028e4e6d6dfe0a9cc082bcc99ca7b643"
-    sha256 x86_64_linux:  "6b20530c09fffe2d4dcda90dfd9b8c1869e1ff58fe94eb9e74763d2f705f1a9e"
+    sha256 arm64_monterey: "07f9ad919f5bdbb2857580549cba37da0aee5c1fb88cd5eb410ac3ec857de11d"
+    sha256 arm64_big_sur:  "f2a133f663a82d1d39ba238692f6c56de3c6bae12add2107204a75af2f6d923a"
+    sha256 big_sur:        "17a348763e827089c2d2fb2b2c631f38df87d99234d81f2bbbe1c7219577522c"
+    sha256 catalina:       "5b2a8266f5236b9fe1921040976c07d9eda3cc178a2c2d82824254de82df0e2e"
+    sha256 mojave:         "85e039d08a8668365cd7f71a9012c587028e4e6d6dfe0a9cc082bcc99ca7b643"
+    sha256 x86_64_linux:   "6b20530c09fffe2d4dcda90dfd9b8c1869e1ff58fe94eb9e74763d2f705f1a9e"
   end
 
   depends_on "autoconf" => :build
