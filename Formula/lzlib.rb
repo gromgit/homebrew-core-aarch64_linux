9f8e7dda81dcfb7aa313cc6fc2fb92ef66777cd7
Bo Anderson <mail@boanderson.me>
2021-10-23 23:49:36 +0000

    lzlib: update 1.12 bottle.

--- Formula/lzlib.rb
+++ Formula/lzlib.rb
@@ -13,6 +13,7 @@ class Lzlib < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ad165df26fa141dc21c58b0fd9adf2427aebc23ef3f7e43b5e64a59582fa371c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06e131d4c75dc18d8aa6571d4b80ff302c9e0b4c5487d661c9d9d0cd34d93100"
+    sha256 cellar: :any_skip_relocation, monterey:       "319d882b6cc4a10c3eb64c43467f58bbabceba3130b797f0df46dfd75ee17061"
     sha256 cellar: :any_skip_relocation, big_sur:        "ebdc6759b366c12f7884ab27af11752c4d0348b91c3dfdd23f29d3e470dc94fa"
     sha256 cellar: :any_skip_relocation, catalina:       "bd80163e5b149eff8a652ee79b8551af09bba6410d9549c481d680ffb6c5dcb9"
     sha256 cellar: :any_skip_relocation, mojave:         "d351d2530ffa16a75df2afaf35cad291acca3a8a9374ab0b7b3edfcf1bd4f64d"
