0a9302ee10053ce7af880a87267abf61bbe7bc2a
Bo Anderson <mail@boanderson.me>
2021-10-23 20:08:41 +0000

    tig: update 2.5.4 bottle.

--- Formula/tig.rb
+++ Formula/tig.rb
@@ -8,6 +8,7 @@ class Tig < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "75fec92f280daf4789ccd06a2ad809185032cbd98097baffeb6b0b283b9212ef"
     sha256 cellar: :any,                 arm64_big_sur:  "ca3273a9bb53f6b060ce4ccbffaf82308d90d833a7120505a55f877b6f197592"
+    sha256 cellar: :any,                 monterey:       "3ff950a8d1588789c3b4bb86f998464f3b69ebbf37aca1403a43b8499f93f244"
     sha256 cellar: :any,                 big_sur:        "506b2d47105b97ba9d04f3b91a45e9a164dd409fc1fd58655bf3015bb1403e0b"
     sha256 cellar: :any,                 catalina:       "5afa0c1a9dcb4d851f88db05b0f911e70014abb6d64b43d1b1e56f12452d233a"
     sha256 cellar: :any,                 mojave:         "c2dff759957d7b62f5ab947e6637c428c9352c244faeb6f8e02918f8c9587771"
