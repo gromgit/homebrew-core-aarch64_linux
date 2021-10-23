e0e63faaad4c7106c5b984d8cf929c717d9a159f
Bo Anderson <mail@boanderson.me>
2021-10-23 00:25:37 +0000

    git: update 2.33.1_1 bottle.

--- Formula/git.rb
+++ Formula/git.rb
@@ -17,6 +17,7 @@ class Git < Formula
   bottle do
     sha256 arm64_monterey: "01e1dc0704d6606839599ef423d34767997725d8e25110ce848e20434681efc5"
     sha256 arm64_big_sur:  "1adc0340427aeef3afbb0980b6fcea4edcfa6127ea44846615065b68a27544a8"
+    sha256 monterey:       "ac8dd353f5d735a7c67317d612bdf77112b596b541248472f39cd907cb3eb2f0"
     sha256 big_sur:        "6635c9eac78d922dab5d49229ac667147d8b38e3535142afe647647dedcee260"
     sha256 catalina:       "b7bab47a69503d9f6a949c7b27db4e4545d2902b0ca7a5a4fd02071b7ad2cb9b"
     sha256 mojave:         "7d17fffd0920dbfdd79c68c38f375fae4da7dbaf4965f8d6a2876bbb9bc87e84"
