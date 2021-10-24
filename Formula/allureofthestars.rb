23066ed808919e6ce43bb29dad8e98d02b4d0a32
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 08:54:34 +0000

    allureofthestars: update 0.10.2.0_4 bottle.

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -8,11 +8,12 @@ class Allureofthestars < Formula
   head "https://github.com/AllureOfTheStars/Allure.git", branch: "master"
 
   bottle do
-    sha256 arm64_big_sur: "35f81924e44e1dbb5b3afdb9447117622f6fa31388722f3ba4326641e3399eb7"
-    sha256 big_sur:       "07130a7da685c1df7dfc4231696b3819edac6ef3f4626796a4b7a01c9d8e1bba"
-    sha256 catalina:      "522ebd08dd14793bcf72c9cc260d2d1649a394c35941db6476baec2c7c441a85"
-    sha256 mojave:        "7f3af277723076c9b3dae13fbf597a24d92069cd2aca02e9628de915036a3f04"
-    sha256 x86_64_linux:  "96891cb7896bc936721f38f9d0ce62f3dc9675d245345d5e2ecf3bb68396763b"
+    sha256 arm64_monterey: "cffe10d641e44846b61f44783d57d65bfdf38143a7d345899a4550398a3c11aa"
+    sha256 arm64_big_sur:  "35f81924e44e1dbb5b3afdb9447117622f6fa31388722f3ba4326641e3399eb7"
+    sha256 big_sur:        "07130a7da685c1df7dfc4231696b3819edac6ef3f4626796a4b7a01c9d8e1bba"
+    sha256 catalina:       "522ebd08dd14793bcf72c9cc260d2d1649a394c35941db6476baec2c7c441a85"
+    sha256 mojave:         "7f3af277723076c9b3dae13fbf597a24d92069cd2aca02e9628de915036a3f04"
+    sha256 x86_64_linux:   "96891cb7896bc936721f38f9d0ce62f3dc9675d245345d5e2ecf3bb68396763b"
   end
 
   depends_on "cabal-install" => :build
