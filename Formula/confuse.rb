9572dfa10b4e27dc836ddeb85374460f66ee1a18
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 11:56:51 +0000

    confuse: update 3.3 bottle.

--- Formula/confuse.rb
+++ Formula/confuse.rb
@@ -6,12 +6,13 @@ class Confuse < Formula
   license "ISC"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1eeec2cb7b54cf11c1e13448f191ed97d4f2477c215130b6402256678019f36e"
-    sha256 cellar: :any, big_sur:       "370cd5df07249d44cbf0a848001be19d41341f404d229dcdcb3b5ae6ead4300c"
-    sha256 cellar: :any, catalina:      "13ad01ca606e746ab7f6bcd42b0da08abdcc29ccaaa9e8106f9d28bfe96bffd7"
-    sha256 cellar: :any, mojave:        "d6038fe2a7fcfea4ba6e3c29174cb6201ce7d05e22ef4c76b881b9f12dabcff6"
-    sha256 cellar: :any, high_sierra:   "371f699488d7e4459251c55e4ef4d9087b08e07b4fedfc553476bc30070ca9c1"
-    sha256               x86_64_linux:  "a5fbe815c75f10344684dab03501ecab39cec4b157e46d955f6e2c70062d120b"
+    sha256 cellar: :any, arm64_monterey: "633330ab55c7992ac1a9dcb3d990029d1445aab0d3e5c3a8c5759af2554b33d4"
+    sha256 cellar: :any, arm64_big_sur:  "1eeec2cb7b54cf11c1e13448f191ed97d4f2477c215130b6402256678019f36e"
+    sha256 cellar: :any, big_sur:        "370cd5df07249d44cbf0a848001be19d41341f404d229dcdcb3b5ae6ead4300c"
+    sha256 cellar: :any, catalina:       "13ad01ca606e746ab7f6bcd42b0da08abdcc29ccaaa9e8106f9d28bfe96bffd7"
+    sha256 cellar: :any, mojave:         "d6038fe2a7fcfea4ba6e3c29174cb6201ce7d05e22ef4c76b881b9f12dabcff6"
+    sha256 cellar: :any, high_sierra:    "371f699488d7e4459251c55e4ef4d9087b08e07b4fedfc553476bc30070ca9c1"
+    sha256               x86_64_linux:   "a5fbe815c75f10344684dab03501ecab39cec4b157e46d955f6e2c70062d120b"
   end
 
   depends_on "pkg-config" => :build
