bd2ff92e1ee1cf56b96f27322f757a500de47e49
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:49:28 +0000

    dhall-bash: update 1.0.38 bottle.

--- Formula/dhall-bash.rb
+++ Formula/dhall-bash.rb
@@ -7,10 +7,11 @@ class DhallBash < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a024b04b527c5f6863ace794e5f83afe92b1b16203acd23f1e95e3e27901cbcf"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f5063002c22244dd97c80f34b28b9b185fd46d64b11ece003eace16ea54d9a65"
-    sha256 cellar: :any_skip_relocation, catalina:      "49c267f3236157660b58386c17d9f158c1c4df110dc3f112fdc20b565f90334e"
-    sha256 cellar: :any_skip_relocation, mojave:        "ced176f284777f2578f2432fd2943a5e872fbaa0486cde7790e831ad1c116dc8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b815cab1ba135f2682f49756077b7712938e9d84b9487b77498f1158d6e0fbb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a024b04b527c5f6863ace794e5f83afe92b1b16203acd23f1e95e3e27901cbcf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f5063002c22244dd97c80f34b28b9b185fd46d64b11ece003eace16ea54d9a65"
+    sha256 cellar: :any_skip_relocation, catalina:       "49c267f3236157660b58386c17d9f158c1c4df110dc3f112fdc20b565f90334e"
+    sha256 cellar: :any_skip_relocation, mojave:         "ced176f284777f2578f2432fd2943a5e872fbaa0486cde7790e831ad1c116dc8"
   end
 
   depends_on "cabal-install" => :build
