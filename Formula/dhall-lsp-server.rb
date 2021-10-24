3308e499793c36e1d17733cb37f64915888989d5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:23:28 +0000

    dhall-lsp-server: update 1.0.16 bottle.

--- Formula/dhall-lsp-server.rb
+++ Formula/dhall-lsp-server.rb
@@ -7,10 +7,11 @@ class DhallLspServer < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "040c4961a15ce28cbfb896a0057b26d5b6ad155df4543d9efc9edcf518343aa1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8fd9acfed2ce6997a2a16435372b7d012a67fcb427a031cf24595c153d13c63a"
-    sha256 cellar: :any_skip_relocation, catalina:      "2ef242fd1a6d231fcfa4cb37df3e58004da2d8e1a98452b0e5e82e3708f424bf"
-    sha256 cellar: :any_skip_relocation, mojave:        "b8d9503c6462a3e0739001212ae5bebaf80d653df9de6b10f788f3f1eb623115"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "79e2124c41514651a7eb1da835e35957ab9aee3958341361f92f9bc258fef064"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "040c4961a15ce28cbfb896a0057b26d5b6ad155df4543d9efc9edcf518343aa1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8fd9acfed2ce6997a2a16435372b7d012a67fcb427a031cf24595c153d13c63a"
+    sha256 cellar: :any_skip_relocation, catalina:       "2ef242fd1a6d231fcfa4cb37df3e58004da2d8e1a98452b0e5e82e3708f424bf"
+    sha256 cellar: :any_skip_relocation, mojave:         "b8d9503c6462a3e0739001212ae5bebaf80d653df9de6b10f788f3f1eb623115"
   end
 
   depends_on "cabal-install" => :build
