1feb6b19ec1c217b2eb345aeeb725317c164a461
Bo Anderson <mail@boanderson.me>
2021-10-24 03:21:53 +0000

    dfu-util: update 0.11 bottle.

--- Formula/dfu-util.rb
+++ Formula/dfu-util.rb
@@ -6,11 +6,12 @@ class DfuUtil < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "c7dd53f422003b99c57f565aad8371e8cef1aa3de825f36cd927cd61ed64249d"
-    sha256 cellar: :any,                 big_sur:       "b970a649e90f3e080af2143e8479e0616959e35650defea16b96288c4af011dc"
-    sha256 cellar: :any,                 catalina:      "5a5d86794a00b9559ffc819715c297da4f477296d20a92c804aefc426795d0b0"
-    sha256 cellar: :any,                 mojave:        "1ded847895f4d2a86a4a7754fa711014d09c334044ccc03aa97d89059ae58604"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13c26d1ebc67dd60446845d51e3ff92bcded0ae223852dc104e897c8c3423f71"
+    sha256 cellar: :any,                 arm64_monterey: "7d09c40c797df76fdea2862b205111fa9c14d44b09c27a0b00e083fcc827bee9"
+    sha256 cellar: :any,                 arm64_big_sur:  "c7dd53f422003b99c57f565aad8371e8cef1aa3de825f36cd927cd61ed64249d"
+    sha256 cellar: :any,                 big_sur:        "b970a649e90f3e080af2143e8479e0616959e35650defea16b96288c4af011dc"
+    sha256 cellar: :any,                 catalina:       "5a5d86794a00b9559ffc819715c297da4f477296d20a92c804aefc426795d0b0"
+    sha256 cellar: :any,                 mojave:         "1ded847895f4d2a86a4a7754fa711014d09c334044ccc03aa97d89059ae58604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13c26d1ebc67dd60446845d51e3ff92bcded0ae223852dc104e897c8c3423f71"
   end
 
   head do
