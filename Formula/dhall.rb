f1c14cda8c3590756500092a3f0040b99eae0f8b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 10:24:08 +0000

    dhall: update 1.40.1 bottle.

--- Formula/dhall.rb
+++ Formula/dhall.rb
@@ -8,6 +8,7 @@ class Dhall < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1eaee9b0608abaa3b1c07fdba1d17a38d865bee1b13c07d983fbd4d6f7a8f947"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d5813e732f904689c01bd850dacfbadefc7cc7eabc9de698eae354f6da536487"
+    sha256 cellar: :any_skip_relocation, monterey:       "8afcf38a7140334e030a4a28dc163a3efd7da193cbdbf9f85296f5a6a2169f84"
     sha256 cellar: :any_skip_relocation, big_sur:        "7419b77dbe5d0c193fee5fcc025b4498093827c49b7bb72cd5a1ea9049df2ace"
     sha256 cellar: :any_skip_relocation, catalina:       "5237942d82f12c997ed07086c1be7610ce4ba998dc87b24a3dec2d88bd382d71"
     sha256 cellar: :any_skip_relocation, mojave:         "dad39b07677cd3dc786e269a9160e938f3cdb1cdd7c35d7856293d61c7aad741"
