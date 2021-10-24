85c6959a8dac8161bb89315bfec0627153f90fe4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:28:40 +0000

    shelltestrunner: update 1.9_1 bottle.

--- Formula/shelltestrunner.rb
+++ Formula/shelltestrunner.rb
@@ -8,11 +8,12 @@ class Shelltestrunner < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "58f03249e05d5808baff4da11f784b37bd818f35c3243855bdc467562526ff76"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e42bf02d5f462aca20fe7a9b80ba6e2431b15940b81f5304ff1611665a29bac0"
-    sha256 cellar: :any_skip_relocation, catalina:      "d425959bf27c059fb22a6e2f916f839645c0e79f180903b2c99c189ba52f4d96"
-    sha256 cellar: :any_skip_relocation, mojave:        "15ccce4fe40fe20c6f9c97442fb37079a6925351f725d4e6840541004375520c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "380d1768a50c99b9ececeea2044324a869ccb4248609ccee61adc5f6c34ef363"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6a8727bdd254b5af7101182d68e536f23421e6a08d880c4adf4ac949daed5628"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "58f03249e05d5808baff4da11f784b37bd818f35c3243855bdc467562526ff76"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e42bf02d5f462aca20fe7a9b80ba6e2431b15940b81f5304ff1611665a29bac0"
+    sha256 cellar: :any_skip_relocation, catalina:       "d425959bf27c059fb22a6e2f916f839645c0e79f180903b2c99c189ba52f4d96"
+    sha256 cellar: :any_skip_relocation, mojave:         "15ccce4fe40fe20c6f9c97442fb37079a6925351f725d4e6840541004375520c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "380d1768a50c99b9ececeea2044324a869ccb4248609ccee61adc5f6c34ef363"
   end
 
   depends_on "cabal-install" => :build
