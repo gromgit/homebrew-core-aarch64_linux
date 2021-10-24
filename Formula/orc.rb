c7b6f2331ee3d19d3ea40bee503f528592e85404
Bo Anderson <mail@boanderson.me>
2021-10-24 00:25:05 +0000

    orc: update 0.4.32 bottle.

--- Formula/orc.rb
+++ Formula/orc.rb
@@ -12,6 +12,7 @@ class Orc < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "159d7effc8703f289417c7f183c73a2d081dc0ad20cdbc6b7da2cced48f11cf3"
     sha256 cellar: :any,                 arm64_big_sur:  "657bdaa5b945e5071ae862066b4f703750890b106facb3528c9e649beada7c30"
+    sha256 cellar: :any,                 monterey:       "0f13d05920a43d5704056207f2dca51ca2bcbed74d141cec55be30d9f0d8f243"
     sha256 cellar: :any,                 big_sur:        "7c5d9d6caf789828006ff7d604eb81a247c8d9ff6004c524978452a252dd8162"
     sha256 cellar: :any,                 catalina:       "df8ed6c8e2d13c9425174f38a6ff60d291cc3febababa670ebe74b21fe56ceab"
     sha256 cellar: :any,                 mojave:         "8b11d82f1c1e51fe41d4b7cf1a905f72bdb977ce2acc0eafcf2ba6b89f06d58c"
