fa0408df46e64553530cc16dfc45cd3655c37a5e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 10:02:33 +0000

    dhall-lsp-server: update 1.0.16 bottle.

--- Formula/dhall-lsp-server.rb
+++ Formula/dhall-lsp-server.rb
@@ -9,6 +9,7 @@ class DhallLspServer < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "79e2124c41514651a7eb1da835e35957ab9aee3958341361f92f9bc258fef064"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "040c4961a15ce28cbfb896a0057b26d5b6ad155df4543d9efc9edcf518343aa1"
+    sha256 cellar: :any_skip_relocation, monterey:       "198b8fc3aaf6a535057e52c075bc3269d60cdf8dfd83276d24f04b4db8875dee"
     sha256 cellar: :any_skip_relocation, big_sur:        "8fd9acfed2ce6997a2a16435372b7d012a67fcb427a031cf24595c153d13c63a"
     sha256 cellar: :any_skip_relocation, catalina:       "2ef242fd1a6d231fcfa4cb37df3e58004da2d8e1a98452b0e5e82e3708f424bf"
     sha256 cellar: :any_skip_relocation, mojave:         "b8d9503c6462a3e0739001212ae5bebaf80d653df9de6b10f788f3f1eb623115"
