b2ada609c84ca9630baea17f372ba2242f6c80bc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:32:59 +0000

    hpack: update 0.34.5 bottle.

--- Formula/hpack.rb
+++ Formula/hpack.rb
@@ -7,11 +7,12 @@ class Hpack < Formula
   head "https://github.com/sol/hpack.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9c1aaf762db8ae437de6483a6614925c1ca4412df71b220aef4d54afb0d9f1b4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "807f4c5ce6363b4051ae08c4a00089fd91c4bb7cab4e3247af0e9cd8c22ece37"
-    sha256 cellar: :any_skip_relocation, catalina:      "9fbf23b07e7d3b61ff29a3c95f487456584db41011f3258a3b2ee3305b9f79a9"
-    sha256 cellar: :any_skip_relocation, mojave:        "09ee5036e2bae87c171460857ed5fbb0abf83d4f3dee6605c1e3df7464aa02e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aac2909793335d9b37761f540fa49816d9613f0ebf94eb740bb2a7211f5f8220"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "60566418c082e7da4478563a456375b5b40695a2311379109ab7d21f75434da1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c1aaf762db8ae437de6483a6614925c1ca4412df71b220aef4d54afb0d9f1b4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "807f4c5ce6363b4051ae08c4a00089fd91c4bb7cab4e3247af0e9cd8c22ece37"
+    sha256 cellar: :any_skip_relocation, catalina:       "9fbf23b07e7d3b61ff29a3c95f487456584db41011f3258a3b2ee3305b9f79a9"
+    sha256 cellar: :any_skip_relocation, mojave:         "09ee5036e2bae87c171460857ed5fbb0abf83d4f3dee6605c1e3df7464aa02e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aac2909793335d9b37761f540fa49816d9613f0ebf94eb740bb2a7211f5f8220"
   end
 
   depends_on "cabal-install" => :build
