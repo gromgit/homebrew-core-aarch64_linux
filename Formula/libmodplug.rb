4aaaffa20227549b03c1a642963c5b9581af1b81
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:24:35 +0800

    libmodplug: patch incorrect -flat_namespace usage (#87845)
    This is needed for bottling on Monterey.

--- Formula/libmodplug.rb
+++ Formula/libmodplug.rb
@@ -28,6 +28,12 @@ class Libmodplug < Formula
     sha256 "f80735b77123cc7e02c4dad6ce8197bfefcb8748b164a66ffecd206cc4b63d97"
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
