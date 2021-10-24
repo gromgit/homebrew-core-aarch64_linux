8f84d753d4b3c94b1b49a199fc3f7778d1ba3765
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 06:39:06 +0000

    librttopo: update 1.1.0 bottle.

--- Formula/librttopo.rb
+++ Formula/librttopo.rb
@@ -14,6 +14,7 @@ class Librttopo < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "9eee7c1b207b85c576f61e416c507d7b58d657f4d9e314e26ff3a964066965a3"
     sha256 cellar: :any,                 arm64_big_sur:  "531ac2ca2e4247da1ebdacac77cbd68faaf5ea5935608de9ff842ae21aa18ce0"
+    sha256 cellar: :any,                 monterey:       "2eb9b1d4133a764edab33bd122c2d7326b95f983edbbb4cea9bec3888be3885f"
     sha256 cellar: :any,                 big_sur:        "59068843a454371abc25ad9421771eb2770febfaa00d41e1527476f4cbfdb05b"
     sha256 cellar: :any,                 catalina:       "9512f32068f310fc02c082828e4ebac85a698ef69f370243aa00a5b873569319"
     sha256 cellar: :any,                 mojave:         "d6bc9674875a3eeb44cec544f6cc9ac9ce6435f7fd951f446801a8aadcb1a323"
