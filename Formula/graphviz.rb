a5c1663a8655c938cb9dfbe548a5cfc9b43d9650
Bevan Kay <email@bevankay.me>
2021-10-25 10:11:10 +1100

    graphviz 2.49.3
    Closes #87915.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -2,8 +2,8 @@ class Graphviz < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://www.graphviz.org/"
   url "https://gitlab.com/graphviz/graphviz.git",
-      tag:      "2.49.2",
-      revision: "cf96d517357e42b00279cacc042ec0c0bd2a5e89"
+      tag:      "2.49.3",
+      revision: "3425dae078262591d04fec107ec71ab010651852"
   license "EPL-1.0"
   version_scheme 1
   head "https://gitlab.com/graphviz/graphviz.git"
