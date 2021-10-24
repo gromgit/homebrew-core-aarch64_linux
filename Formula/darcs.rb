d5637990ce55ad362d086490d31699f66b6692b8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:16:32 +0000

    darcs: update 2.16.4 bottle.

--- Formula/darcs.rb
+++ Formula/darcs.rb
@@ -7,11 +7,12 @@ class Darcs < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "26d3cc477f82644b7c0c37fafd113d9f78e54dbabf8693c50227b4423e5d193d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e320fccaf6483a2acad3853a76e812f797dd5c9f65c6dd314fb5a8a8f1c174a0"
-    sha256 cellar: :any_skip_relocation, catalina:      "312a9c5d7e0c4849fdccefc6c949ebbfb7d5aede071369225ac97f5699a32691"
-    sha256 cellar: :any_skip_relocation, mojave:        "e27c0d7e3a7999aabbf51fab11a27558e942848058e4bcffd2dd40a75b157e60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07bd38a471302315cec54794883dc078db6c37fb3b37885bcc879514759b2cfd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7236518c4635bc2a77cefdc34a47af8e11f687c680b082314fe68881432eeb6d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "26d3cc477f82644b7c0c37fafd113d9f78e54dbabf8693c50227b4423e5d193d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e320fccaf6483a2acad3853a76e812f797dd5c9f65c6dd314fb5a8a8f1c174a0"
+    sha256 cellar: :any_skip_relocation, catalina:       "312a9c5d7e0c4849fdccefc6c949ebbfb7d5aede071369225ac97f5699a32691"
+    sha256 cellar: :any_skip_relocation, mojave:         "e27c0d7e3a7999aabbf51fab11a27558e942848058e4bcffd2dd40a75b157e60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "07bd38a471302315cec54794883dc078db6c37fb3b37885bcc879514759b2cfd"
   end
 
   depends_on "cabal-install" => :build
