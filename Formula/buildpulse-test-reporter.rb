9048dee14828a537543fabf4eaf4f6edafc5dff7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 02:41:00 +0000

    buildpulse-test-reporter: update 0.21.0 bottle.

--- Formula/buildpulse-test-reporter.rb
+++ Formula/buildpulse-test-reporter.rb
@@ -7,10 +7,11 @@ class BuildpulseTestReporter < Formula
   head "https://github.com/buildpulse/test-reporter.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cc5b7d41d3e0f9ed4d2f9fd73be416d59eff7002b00a5ac427cb3f987fcc76ba"
-    sha256 cellar: :any_skip_relocation, big_sur:       "57d2559e7c0e34e593c657d55ce05ab7e9773064923f7fce45e7b5e8ff391254"
-    sha256 cellar: :any_skip_relocation, catalina:      "6a695e176c6ff0f5d39fe7a9e6967b0b848fbaea26159a28301af612f111080b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb918a49ec30f8a00bb36bd70176621cf6e21307bc0437ef95b8c09263ef94c1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "632ad07ab1c55c93c4d306abfa7ac5f42eaafd3cfcc0bbc18fe94f5a84550c3c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cc5b7d41d3e0f9ed4d2f9fd73be416d59eff7002b00a5ac427cb3f987fcc76ba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "57d2559e7c0e34e593c657d55ce05ab7e9773064923f7fce45e7b5e8ff391254"
+    sha256 cellar: :any_skip_relocation, catalina:       "6a695e176c6ff0f5d39fe7a9e6967b0b848fbaea26159a28301af612f111080b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb918a49ec30f8a00bb36bd70176621cf6e21307bc0437ef95b8c09263ef94c1"
   end
 
   depends_on "go" => :build
