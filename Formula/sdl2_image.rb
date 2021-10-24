dc1d868cb2a9e046fc72a5deb746bbf7bdbaf218
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 16:24:12 +0000

    sdl2_image: update 2.0.5 bottle.

--- Formula/sdl2_image.rb
+++ Formula/sdl2_image.rb
@@ -11,13 +11,14 @@ class Sdl2Image < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2a4cd10f3598553180559df330435f359703eaa02a77c12bf9667d953fa2b5a3"
-    sha256 cellar: :any,                 big_sur:       "d106f96771895c1b6faa9864e3605d301cdbe658672900108605c521616a8bf6"
-    sha256 cellar: :any,                 catalina:      "691d5407fef2bc374ac3b7c2fafbe46a6bc0f9ed609f98812b24fec33ab9bd27"
-    sha256 cellar: :any,                 mojave:        "1b3a464579d9ef25b3bdd9276119efffd0134fda5c5dc27051a35f1b21c00cfd"
-    sha256 cellar: :any,                 high_sierra:   "55c1f996fb523c2727d2b103f0a5ecfd7a073f55ff9a7230bb609d22bbf5a576"
-    sha256 cellar: :any,                 sierra:        "e3c9cf45d97099e818c667d23af8352e6d1bba0e3b609cdddee654f2a9da80cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "862dee72c17699cb5d0014116b98ae4de3998eead5bed93063d84b2b430147a5"
+    sha256 cellar: :any,                 arm64_monterey: "180b64296c90fe9587a88efa1a607d939bb4dbb127f31a38193d894c5da9bfeb"
+    sha256 cellar: :any,                 arm64_big_sur:  "2a4cd10f3598553180559df330435f359703eaa02a77c12bf9667d953fa2b5a3"
+    sha256 cellar: :any,                 big_sur:        "d106f96771895c1b6faa9864e3605d301cdbe658672900108605c521616a8bf6"
+    sha256 cellar: :any,                 catalina:       "691d5407fef2bc374ac3b7c2fafbe46a6bc0f9ed609f98812b24fec33ab9bd27"
+    sha256 cellar: :any,                 mojave:         "1b3a464579d9ef25b3bdd9276119efffd0134fda5c5dc27051a35f1b21c00cfd"
+    sha256 cellar: :any,                 high_sierra:    "55c1f996fb523c2727d2b103f0a5ecfd7a073f55ff9a7230bb609d22bbf5a576"
+    sha256 cellar: :any,                 sierra:         "e3c9cf45d97099e818c667d23af8352e6d1bba0e3b609cdddee654f2a9da80cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "862dee72c17699cb5d0014116b98ae4de3998eead5bed93063d84b2b430147a5"
   end
 
   head do
