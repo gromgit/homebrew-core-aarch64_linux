7e03e35f2eb2189235b1e33a7bac456b5023cc3d
Bo Anderson <mail@boanderson.me>
2021-10-24 00:23:09 +0000

    mtr: update 0.94 bottle.

--- Formula/mtr.rb
+++ Formula/mtr.rb
@@ -9,6 +9,7 @@ class Mtr < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "36ce33f2a57a1ae2d38579526ef8263b29d3bb1976d33cbebf59c3c38b8a47ef"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2e6e4509d128d1f3bc7e0594c8fb12b60971780e070523a2e4d8b896507cef57"
+    sha256 cellar: :any_skip_relocation, monterey:       "a6969ff4d1bd4b6b098cb220baa5df6a0157bfdb07b2a1e436c73fe4faad1bbc"
     sha256 cellar: :any_skip_relocation, big_sur:        "3625ac3eeb2409adfb156194c8bda385b98ebb7afa59229e302e9e61bb897004"
     sha256 cellar: :any_skip_relocation, catalina:       "6ec962809c1c2381b7723647e16c2283dbef8042ae04af14ad675fa63c38a859"
     sha256 cellar: :any_skip_relocation, mojave:         "6a768b9cd07026aec0276742fd3fb6723c0f545a8498dff1ab3bb5b9e23e85e0"
