da169d7cbc86b2557bace8efa89f2d02914b7749
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 14:23:23 +0000

    openjpeg: update 2.4.0 bottle.

--- Formula/openjpeg.rb
+++ Formula/openjpeg.rb
@@ -9,6 +9,7 @@ class Openjpeg < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "13b02f1e67d92a72a712a4cbeca0613c9467acef5e0a6c3ef45343988353c477"
     sha256 cellar: :any,                 arm64_big_sur:  "b57a02c3bc4ee8a43e47df5015e6e40a04d7149e172806157e279b1b03c715ef"
+    sha256 cellar: :any,                 monterey:       "2dcf97a3d9df6686f006e9e9d471a71612da54a81de169dfc8b2dae83cfa2b79"
     sha256 cellar: :any,                 big_sur:        "43c37565eb2eec2b41dee3f1cc26e3324a42a368cb88092fe1b0dbc941f7678f"
     sha256 cellar: :any,                 catalina:       "80426609c75b98ee0ee394e9017bb621dc73dd2d6f60d0c851f6940d0b268676"
     sha256 cellar: :any,                 mojave:         "e26d092b6177ee282d3724dea5ea4cb76af3645472791c3fefb002e2638588b0"
