20625c6b4d67f2107a907e319dc873aed6e93aad
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 06:45:08 +0000

    jfrog-cli: update 2.5.0 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -8,6 +8,7 @@ class JfrogCli < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2885fbb1b5c3a4c1daf86f1c32aed03cf62cfb0376148c9245862a3db68d20e9"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "342ad46e482e95c4b9335c0de8f4097a18c5c4757c23dd1542678c9a61b44a33"
+    sha256 cellar: :any_skip_relocation, monterey:       "99d8b968c6f1681278bf4b3836723d5f9f518d7cb92875ebaaca39209af64db1"
     sha256 cellar: :any_skip_relocation, big_sur:        "bf00236ba19abe9614c3e69870b7450fa8b09e7ed6595b42d54cbb5ce5f77229"
     sha256 cellar: :any_skip_relocation, catalina:       "223adb77126c1d39ee7ae37ca650bc6cbae2946c6c44b1a5a08a75569ca677d0"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5167b04cedb616915eff0e229b3942465d97425868f189e76f6aee5fd6ccac32"
