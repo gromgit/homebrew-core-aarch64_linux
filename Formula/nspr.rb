05d54fccdb48059bc7d52d4c40cdaa9e62af31ef
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 02:27:54 +0000

    nspr: update 4.32 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -13,6 +13,7 @@ class Nspr < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "45df53b5a421215b987071da52791d105137df0202a374a0e8c7e209c60f880f"
     sha256 cellar: :any,                 arm64_big_sur:  "69c39b2f0a2d93ea5969a9906d0e5063caa34ad495cd56cef7e22b8b8628a67a"
+    sha256 cellar: :any,                 monterey:       "2b6b4bde77314607f5c19b327a9fc854b32053655848a60b802e36bebd07124c"
     sha256 cellar: :any,                 big_sur:        "fa4089b067d319a827833747ed64c0e9ed9ce1c95aa54d77fb1dabffd52436ed"
     sha256 cellar: :any,                 catalina:       "f5a4e62a6d63a398ce0d14af9aa25ad1c33c8eb9ff64de988551ee0791aae69b"
     sha256 cellar: :any,                 mojave:         "fd4bc5953ceff865f0a3e049fbb06ccb07015c4369a7aa4c67e820af6b7cc53b"
