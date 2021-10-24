a7d3bfd0fcd2bcdcc0294ca16c14898991c5205c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 11:42:46 +0000

    pandoc-crossref: update 0.3.12.0 bottle.

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -6,11 +6,12 @@ class PandocCrossref < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3b525d5593fc8e35259c94fe0352ac89ae51564d2681a69e08e66915f8c0dd7d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "777c6def219f6ce20fc702ff5b9da4cf404c083b4376947c46a98da6cfeae046"
-    sha256 cellar: :any_skip_relocation, catalina:      "57d4c7b20861051984b105ffce52e3e7716ddd3484dd34b2068da1851ad35547"
-    sha256 cellar: :any_skip_relocation, mojave:        "f5b01586f853381399a67e203a95e404184916cc6fe81edc5c3a0844b6762337"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae4f8dbc8a165ebc5adb838c4fe21acd1d674fa8b614837962c200aa93bbd027"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1f18824d86b714d26c67c71875f7010c7cc541588ad8c94ffcbdf585cbdf4762"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b525d5593fc8e35259c94fe0352ac89ae51564d2681a69e08e66915f8c0dd7d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "777c6def219f6ce20fc702ff5b9da4cf404c083b4376947c46a98da6cfeae046"
+    sha256 cellar: :any_skip_relocation, catalina:       "57d4c7b20861051984b105ffce52e3e7716ddd3484dd34b2068da1851ad35547"
+    sha256 cellar: :any_skip_relocation, mojave:         "f5b01586f853381399a67e203a95e404184916cc6fe81edc5c3a0844b6762337"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae4f8dbc8a165ebc5adb838c4fe21acd1d674fa8b614837962c200aa93bbd027"
   end
 
   depends_on "cabal-install" => :build
