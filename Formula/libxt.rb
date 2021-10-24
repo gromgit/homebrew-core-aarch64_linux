c3a78354f4d48b38fd61ad062f4e8c8b06ea532f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 01:03:05 +0000

    libxt: update 1.2.1 bottle.

--- Formula/libxt.rb
+++ Formula/libxt.rb
@@ -8,6 +8,7 @@ class Libxt < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "7e817f2cb5cf9b4708246377c23745307d8cb7d0ee03d77b3a6e3b4fb16708ff"
     sha256 cellar: :any,                 arm64_big_sur:  "0fd900ad5097946ee343ba7c15311a3b85540dcd058233e05f198c15405b9da0"
+    sha256 cellar: :any,                 monterey:       "f2dacb815eb7792b97b5e163bf6f8eef12b5d2283409322664ab937eb9954545"
     sha256 cellar: :any,                 big_sur:        "db76d4efdf96e00af7b4230245b81c26539b4ec20e93c1d379a3c92b39217885"
     sha256 cellar: :any,                 catalina:       "a1bcc92d37e1602ef760fecb79f7729db7e38aee2835879689596d478480217b"
     sha256 cellar: :any,                 mojave:         "4bd6052344cc12c674d40f44c31083264f5ce097ec0d2f6111e726862a8a2b04"
