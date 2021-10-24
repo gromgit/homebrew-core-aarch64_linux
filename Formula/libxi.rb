908ec8d822c8b73858a3591d8fb67f402f68b444
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 16:26:20 +0000

    libxi: update 1.8 bottle.

--- Formula/libxi.rb
+++ Formula/libxi.rb
@@ -6,11 +6,12 @@ class Libxi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "44a124946d8d522eb9ef39f5ec913354e7fba982b31e46743e87fe4a9a0984ed"
-    sha256 cellar: :any,                 big_sur:       "29a8ae9f7812ea8ec985367dd8c3bf061bd18a689e3b3c4ff0503773263ac923"
-    sha256 cellar: :any,                 catalina:      "63cc1eba03c04eda56ed89aea690805f6dbcaaa7ea82166b64d7ec5d7d7f2bc1"
-    sha256 cellar: :any,                 mojave:        "11c296a17b8842fd23ca1ac58c17b3eaca8e91ecea322fc5b19eab91233ca891"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1026c8936f13ff85a1c93091cf37e488510361bdfb69d0c0ec15234a7d281ed5"
+    sha256 cellar: :any,                 arm64_monterey: "a65d47579981145f9c7b4225136c6e9ad7eed5268687abe7f7a8941a3a608b6c"
+    sha256 cellar: :any,                 arm64_big_sur:  "44a124946d8d522eb9ef39f5ec913354e7fba982b31e46743e87fe4a9a0984ed"
+    sha256 cellar: :any,                 big_sur:        "29a8ae9f7812ea8ec985367dd8c3bf061bd18a689e3b3c4ff0503773263ac923"
+    sha256 cellar: :any,                 catalina:       "63cc1eba03c04eda56ed89aea690805f6dbcaaa7ea82166b64d7ec5d7d7f2bc1"
+    sha256 cellar: :any,                 mojave:         "11c296a17b8842fd23ca1ac58c17b3eaca8e91ecea322fc5b19eab91233ca891"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1026c8936f13ff85a1c93091cf37e488510361bdfb69d0c0ec15234a7d281ed5"
   end
 
   depends_on "pkg-config" => :build
