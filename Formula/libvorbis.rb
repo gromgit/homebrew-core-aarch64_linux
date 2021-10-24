ae42e79a1c2b5db0d51cd6a870ef3944826138b3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 02:26:25 +0000

    libvorbis: update 1.3.7 bottle.

--- Formula/libvorbis.rb
+++ Formula/libvorbis.rb
@@ -12,12 +12,13 @@ class Libvorbis < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "07ab1118fc6d389a8b0506d0b74a3cfc12026a837c8f2609b2133318c8818c81"
-    sha256 cellar: :any,                 big_sur:       "05e639c274f52924cbf31fb4337888ab51554a66597486aeed8e5942d267c586"
-    sha256 cellar: :any,                 catalina:      "432eb21045d9dfac3ef879648d845d894cc828862f5498448fe98c0141ef5cd0"
-    sha256 cellar: :any,                 mojave:        "59509a351e88352f01512b54cc5cb849c2551623f7d6dcd6679d38b5e96032ed"
-    sha256 cellar: :any,                 high_sierra:   "3e6609520d0ffd7179f721c23c1291f2735b70384d56d1c1dd10185ae355c4b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c205523df9d4dd5f8ee71b26018419b9cf5bbca73c64eec36f5cbe5f2db6bbbd"
+    sha256 cellar: :any,                 arm64_monterey: "521810a7d5d4d0779cfc22a7d8ba37bb452fea06a3ab8205d882961d1eeb8ff7"
+    sha256 cellar: :any,                 arm64_big_sur:  "07ab1118fc6d389a8b0506d0b74a3cfc12026a837c8f2609b2133318c8818c81"
+    sha256 cellar: :any,                 big_sur:        "05e639c274f52924cbf31fb4337888ab51554a66597486aeed8e5942d267c586"
+    sha256 cellar: :any,                 catalina:       "432eb21045d9dfac3ef879648d845d894cc828862f5498448fe98c0141ef5cd0"
+    sha256 cellar: :any,                 mojave:         "59509a351e88352f01512b54cc5cb849c2551623f7d6dcd6679d38b5e96032ed"
+    sha256 cellar: :any,                 high_sierra:    "3e6609520d0ffd7179f721c23c1291f2735b70384d56d1c1dd10185ae355c4b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c205523df9d4dd5f8ee71b26018419b9cf5bbca73c64eec36f5cbe5f2db6bbbd"
   end
 
   head do
