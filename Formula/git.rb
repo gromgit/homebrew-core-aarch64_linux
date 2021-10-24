2a5fac844ebfbb2adaed2b67c6f42f72933bf589
Alexander Bayandin <a.bayandin@gmail.com>
2021-10-24 14:03:53 +0100

    git: remove comment about synced version

--- Formula/git.rb
+++ Formula/git.rb
@@ -1,8 +1,6 @@
 class Git < Formula
   desc "Distributed revision control system"
   homepage "https://git-scm.com"
-  # NOTE: Please keep these values in sync with git-gui.rb, git-svn.rb and
-  #       git-credential-libsecret.rb when updating.
   url "https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.33.1.tar.xz"
   sha256 "e054a6e6c2b088bd1bff5f61ed9ba5aa91c9a3cd509539a4b41c5ddf02201f2f"
   license "GPL-2.0-only"
