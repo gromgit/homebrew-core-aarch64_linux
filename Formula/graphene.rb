f50247d6befdaee7cf76a2ff30012a423f4fefee
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 06:43:05 +0000

    graphene: update 1.10.6 bottle.

--- Formula/graphene.rb
+++ Formula/graphene.rb
@@ -8,6 +8,7 @@ class Graphene < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "75db915e9b0cb0c4c567912e1d8cf2e78655f136f672429c0eabe56dbd23e147"
     sha256 cellar: :any,                 arm64_big_sur:  "64f77c528bc4e693aef3a747f76eb2c63a1a25f37e730d46fda139c002271301"
+    sha256 cellar: :any,                 monterey:       "02d923cb6c5358ae637c0f455b850b8ad7a2d97f2d8993cba979171013f942c6"
     sha256 cellar: :any,                 big_sur:        "20b41dfc4c7bf01973d14f33129db71d7968509e8dc0761f640e36400ae8127e"
     sha256 cellar: :any,                 catalina:       "9a39689fd7d593fc8d5b86b077d153d863c51b470703e87dfd8cfd1ee157d742"
     sha256 cellar: :any,                 mojave:         "d6e6d695f0b7c04b6b8b0b09a18a9cd39bd25ba0e64d5843fa12c0a56100ea06"
