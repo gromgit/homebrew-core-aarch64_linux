55a39c7605eb3b60d4300a647948612ea5daa731
Rui Chen <rui@chenrui.dev>
2021-10-23 21:26:23 -0400

    inxi 3.3.08-1
    Closes #87850.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -1,8 +1,8 @@
 class Inxi < Formula
   desc "Full featured CLI system information tool"
   homepage "https://smxi.org/docs/inxi.htm"
-  url "https://github.com/smxi/inxi/archive/3.3.07-1.tar.gz"
-  sha256 "d470c25df84bf1566d04a60842bc006ec6baf62ac700672790d1c6c183dafcef"
+  url "https://github.com/smxi/inxi/archive/3.3.08-1.tar.gz"
+  sha256 "44008d9e77dc82855fd91d634f5f817813eb4653e4df7106e56a1c9986ab8abd"
   license "GPL-3.0-or-later"
   head "https://github.com/smxi/inxi.git"
 
