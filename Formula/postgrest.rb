899badd7fb2c06818e5f371b5af5045999233c4d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 10:20:49 +0000

    postgrest: update 8.0.0_1 bottle.

--- Formula/postgrest.rb
+++ Formula/postgrest.rb
@@ -8,11 +8,12 @@ class Postgrest < Formula
   head "https://github.com/PostgREST/postgrest.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a505a99c6f164a72936e18584d9a172204be89694934adc2a4d168c33ae8f91a"
-    sha256 cellar: :any,                 big_sur:       "9cbf169535605656b931ae0065af5a57c2ff511a8b50529e9ded33c560306c29"
-    sha256 cellar: :any,                 catalina:      "af90758e13856d719d8fc68f770cd59356081321a90e68c52dd8d897fdcc8f4b"
-    sha256 cellar: :any,                 mojave:        "e2a7fa323490cbd817ec9a74729de4a7692d64f2c59c4bb73e188c9235340400"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02c47b193a23f58ccb2df1978aad6df9632428f9c7cccbcbc6fb6eae69b6df87"
+    sha256 cellar: :any,                 arm64_monterey: "bd989403d46a8f5fa86ad41e813fb48e06ec60d6b4052f6bc5a3168c22671ba6"
+    sha256 cellar: :any,                 arm64_big_sur:  "a505a99c6f164a72936e18584d9a172204be89694934adc2a4d168c33ae8f91a"
+    sha256 cellar: :any,                 big_sur:        "9cbf169535605656b931ae0065af5a57c2ff511a8b50529e9ded33c560306c29"
+    sha256 cellar: :any,                 catalina:       "af90758e13856d719d8fc68f770cd59356081321a90e68c52dd8d897fdcc8f4b"
+    sha256 cellar: :any,                 mojave:         "e2a7fa323490cbd817ec9a74729de4a7692d64f2c59c4bb73e188c9235340400"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02c47b193a23f58ccb2df1978aad6df9632428f9c7cccbcbc6fb6eae69b6df87"
   end
 
   depends_on "cabal-install" => :build
