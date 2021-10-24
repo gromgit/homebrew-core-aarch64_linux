b46d6ea0efbc956ca365e4598ae28fe424a52888
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 02:45:33 +0000

    c-blosc: update 1.21.1 bottle.

--- Formula/c-blosc.rb
+++ Formula/c-blosc.rb
@@ -8,6 +8,7 @@ class CBlosc < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "7baccb768cf8ab1252769c5a88fb40d3a4833fb3b110cdbb9e549b5b699f3332"
     sha256 cellar: :any,                 arm64_big_sur:  "1330f5de4f7c9529effbadf7798b73346847d629bf24c8bf90887ed3a6419ab9"
+    sha256 cellar: :any,                 monterey:       "7c982a5118aaffc9565a404da9823789111a3eca0300a20aece2101d29626f56"
     sha256 cellar: :any,                 big_sur:        "e7ef0adf43ef181d7645dfd7f6ab74b5c11c07c0b4cebd473a1096bce406b6ac"
     sha256 cellar: :any,                 catalina:       "feaf08d27a7f27259382d7a67d852b402255e3997daee3dfac0829bd1fc47fd0"
     sha256 cellar: :any,                 mojave:         "e036c972febeb96fbd84b4d40578e5e49c058e43e789946690d8547bb7358c05"
