a5626196027e661fcfb60ce519285487c24c1997
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 14:16:56 +0000

    jpeg-xl: update 0.5_1 bottle.

--- Formula/jpeg-xl.rb
+++ Formula/jpeg-xl.rb
@@ -7,10 +7,11 @@ class JpegXl < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "37a9eb58bb2e1d55f9691161544bdcd6c615787204f7426436087af5037749f8"
-    sha256 cellar: :any, big_sur:       "017bbfa9af1adf69981c1ff8a7f18f3c7b4c8ac1de87168e9f4610825dcfe861"
-    sha256 cellar: :any, catalina:      "b3e21b0631fca6edf8e534f1bdc23a017e766b9dd63afc87e7c16962da5f334c"
-    sha256 cellar: :any, mojave:        "b7e4f3c64f8fe63c0ed2771f8e94ab69c58a3bbc9e716cfda6278682a51b450c"
+    sha256 cellar: :any, arm64_monterey: "f9e47df2a8feecf79234e815761ec21619370807db3ff431f5aba99bbb628f22"
+    sha256 cellar: :any, arm64_big_sur:  "37a9eb58bb2e1d55f9691161544bdcd6c615787204f7426436087af5037749f8"
+    sha256 cellar: :any, big_sur:        "017bbfa9af1adf69981c1ff8a7f18f3c7b4c8ac1de87168e9f4610825dcfe861"
+    sha256 cellar: :any, catalina:       "b3e21b0631fca6edf8e534f1bdc23a017e766b9dd63afc87e7c16962da5f334c"
+    sha256 cellar: :any, mojave:         "b7e4f3c64f8fe63c0ed2771f8e94ab69c58a3bbc9e716cfda6278682a51b450c"
   end
 
   depends_on "asciidoc" => :build
