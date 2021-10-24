8c2a203c5d9c8efc61e3e7cceba87bed90533cbe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 11:56:49 +0000

    gperf: update 3.1 bottle.

--- Formula/gperf.rb
+++ Formula/gperf.rb
@@ -7,15 +7,16 @@ class Gperf < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "311e481114105723752e9b8d0f99dda4ad2bbbfd0cf9c75498384c2279d6b94a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "00685e086c83d1d93a856f35ae56106ac5c19cc4541174040d6d6d71e51bbe75"
-    sha256 cellar: :any_skip_relocation, catalina:      "fc18781c090c4b8b7bb7305a864eeb4e6f3f458d8daa2fff96da3bda061fa8bb"
-    sha256 cellar: :any_skip_relocation, mojave:        "85c9bd450b0a0d7453584c343fe6770c94f8f3941aaa6f95d735f1923209b6ed"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "d71157cd1baddf951e91477b85b533ade99dfe97a5876bb993fe7f6e8336f780"
-    sha256 cellar: :any_skip_relocation, sierra:        "3cbaa18692ac53ce98a754d46e07e89d6dddca4bef3bbb312e762abf5a30093d"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "27f661ef9546ff113279654e92c08bb8d8ab837f7dc8b308c1a2beeafdcebc76"
-    sha256 cellar: :any_skip_relocation, yosemite:      "263440c302dddec69c2140e8df2e4c00a76b76137243e712e8e8756140e0eaf5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a91b89f648c21ae225074e0a9f4e54154b4f2744cc0a37e8421e84ee7ac61a95"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "af16e90596878170e8235696dd8093a4953b2b5948b054e76ccd1741ce0e47d3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "311e481114105723752e9b8d0f99dda4ad2bbbfd0cf9c75498384c2279d6b94a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "00685e086c83d1d93a856f35ae56106ac5c19cc4541174040d6d6d71e51bbe75"
+    sha256 cellar: :any_skip_relocation, catalina:       "fc18781c090c4b8b7bb7305a864eeb4e6f3f458d8daa2fff96da3bda061fa8bb"
+    sha256 cellar: :any_skip_relocation, mojave:         "85c9bd450b0a0d7453584c343fe6770c94f8f3941aaa6f95d735f1923209b6ed"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "d71157cd1baddf951e91477b85b533ade99dfe97a5876bb993fe7f6e8336f780"
+    sha256 cellar: :any_skip_relocation, sierra:         "3cbaa18692ac53ce98a754d46e07e89d6dddca4bef3bbb312e762abf5a30093d"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "27f661ef9546ff113279654e92c08bb8d8ab837f7dc8b308c1a2beeafdcebc76"
+    sha256 cellar: :any_skip_relocation, yosemite:       "263440c302dddec69c2140e8df2e4c00a76b76137243e712e8e8756140e0eaf5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a91b89f648c21ae225074e0a9f4e54154b4f2744cc0a37e8421e84ee7ac61a95"
   end
 
   def install
