4764371804a0536c88a33391dbe8881d5c20bc15
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 07:05:30 +0000

    libpcap: update 1.10.1 bottle.

--- Formula/libpcap.rb
+++ Formula/libpcap.rb
@@ -14,6 +14,7 @@ class Libpcap < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "806a586f73f832dcc33b9c6019c8f8fd5e38c229abe5a2e84335bf6250e785bb"
     sha256 cellar: :any,                 arm64_big_sur:  "56f83e298a7906e44b44ac6d0c99f9a02d0be2ea4726089171a045543368ea9c"
+    sha256 cellar: :any,                 monterey:       "3a77261badf57e93273aa3190348d34a27231e728cb0cd4d4c501e874bd7b2c6"
     sha256 cellar: :any,                 big_sur:        "0aee8b23fc3ede63cbd2cde3bbbaade84ffd6f2ac90315f9738fb872236a8c2f"
     sha256 cellar: :any,                 catalina:       "8d57e7519f14cfec3a1104237e52b56e838faa33b07cdbac05572533e5ae7e07"
     sha256 cellar: :any,                 mojave:         "a31646c901c2fdd953e648e5c5715ec2cdcf49c9e661725a8e56d9f6c38890dc"
