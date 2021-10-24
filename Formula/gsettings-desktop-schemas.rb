190cb020fdf92a990aeb0f008b63d43ca98feb69
Bo Anderson <mail@boanderson.me>
2021-10-24 01:31:39 +0000

    gsettings-desktop-schemas: update 41.0 bottle.

--- Formula/gsettings-desktop-schemas.rb
+++ Formula/gsettings-desktop-schemas.rb
@@ -6,11 +6,12 @@ class GsettingsDesktopSchemas < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d27eabf5d5e5f619418a2b6be7ca9b42de63572e595ab92e6e8960256ca176c4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
-    sha256 cellar: :any_skip_relocation, catalina:      "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
-    sha256 cellar: :any_skip_relocation, mojave:        "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08f6428fc391dda01e161b14a0bd183d340b78cf5cb85a9e66b4ba33be790fa8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d27eabf5d5e5f619418a2b6be7ca9b42de63572e595ab92e6e8960256ca176c4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d27eabf5d5e5f619418a2b6be7ca9b42de63572e595ab92e6e8960256ca176c4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, catalina:       "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, mojave:         "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "08f6428fc391dda01e161b14a0bd183d340b78cf5cb85a9e66b4ba33be790fa8"
   end
 
   depends_on "gobject-introspection" => :build
