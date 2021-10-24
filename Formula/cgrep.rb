dcffc614d44eb44a34f8d39fbc79b2da4914a219
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 09:08:52 +0000

    cgrep: update 6.7.0 bottle.

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -7,11 +7,12 @@ class Cgrep < Formula
   head "https://github.com/awgn/cgrep.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "25858aa7c4f6851083589571653bf47b278d304276596e475319f7165b28b18e"
-    sha256 cellar: :any,                 big_sur:       "c802cbb5bae4c207cf1aa50add2c115621a20632a05129a386589c4b73ff036b"
-    sha256 cellar: :any,                 catalina:      "1cde998562c4510e121298c30c12ccdc3b30f875950c6748103ab1d12469f740"
-    sha256 cellar: :any,                 mojave:        "89bf1c8dacd4e56fd902ac26b5d0f89bac3fd314220fdb4c57f1fbb996d4db1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50bad499d6659e8c7d123f53cadc6e68711606cd97401495b3c2967f353a7a23"
+    sha256 cellar: :any,                 arm64_monterey: "c1d52cc310637e53841cda7ad606752523595fcf5cde0fdd225f69b053990622"
+    sha256 cellar: :any,                 arm64_big_sur:  "25858aa7c4f6851083589571653bf47b278d304276596e475319f7165b28b18e"
+    sha256 cellar: :any,                 big_sur:        "c802cbb5bae4c207cf1aa50add2c115621a20632a05129a386589c4b73ff036b"
+    sha256 cellar: :any,                 catalina:       "1cde998562c4510e121298c30c12ccdc3b30f875950c6748103ab1d12469f740"
+    sha256 cellar: :any,                 mojave:         "89bf1c8dacd4e56fd902ac26b5d0f89bac3fd314220fdb4c57f1fbb996d4db1c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50bad499d6659e8c7d123f53cadc6e68711606cd97401495b3c2967f353a7a23"
   end
 
   depends_on "cabal-install" => :build
