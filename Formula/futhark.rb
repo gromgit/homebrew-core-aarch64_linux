5fbb5bcf8d964d8529e9cb8e627c23e3429124cd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-24 10:25:43 +0000

    futhark: update 0.20.4 bottle.

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -9,6 +9,7 @@ class Futhark < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7d7845bdb2a7fc7c0bd306793ad91eee0c94d543c350e4f3666697c2421ca419"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "217e1f392f74ae06af2912fb864c73ab0e3c5632a8fc9511f10437c4ae1efaeb"
+    sha256 cellar: :any_skip_relocation, monterey:       "db11dee0a5cef876e0b43c3caa7ec3be3dfedec86999fd039db52348da0939d4"
     sha256 cellar: :any_skip_relocation, big_sur:        "0a1e263e927643bd26d69831c8f5590186c581e921204d480e2fed29145d52b1"
     sha256 cellar: :any_skip_relocation, catalina:       "b6fad661d6e5d8296128b256cc4fa5031584ff21f0827b318e23de5b2a83616a"
     sha256 cellar: :any_skip_relocation, mojave:         "97c2af42b567929c2e5a982f4bca6cb4509abd049561ef9aac067b7dc4b215d6"
