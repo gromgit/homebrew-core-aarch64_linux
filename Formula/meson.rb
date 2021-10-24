7f4fe87d18c84762412dbfc9860ed07d1bbfeab9
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-10-24 18:55:54 +0300

    meson 0.60.0
    Closes #87883.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -1,8 +1,8 @@
 class Meson < Formula
   desc "Fast and user friendly build system"
   homepage "https://mesonbuild.com/"
-  url "https://github.com/mesonbuild/meson/releases/download/0.59.2/meson-0.59.2.tar.gz"
-  sha256 "13dee549a7ba758b7e33ce7719f28d1d337a98d10d378a4779ccc996f5a2fc49"
+  url "https://github.com/mesonbuild/meson/releases/download/0.60.0/meson-0.60.0.tar.gz"
+  sha256 "080d68b685e9a0d9c9bb475457e097b49e1d1a6f750abc971428a8d2e1b12d47"
   license "Apache-2.0"
   head "https://github.com/mesonbuild/meson.git"
 
