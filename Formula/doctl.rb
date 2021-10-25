f5bdd68026d934eeeaedd6830d8347b14999590e
Bo Anderson <mail@boanderson.me>
2021-10-25 03:48:18 +0000

    doctl: update 1.65.0 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -7,11 +7,13 @@ class Doctl < Formula
   head "https://github.com/digitalocean/doctl.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c442259ca09852b1c19fd6217c4c7f2f6128d225d004e6d2ab496c0da2a7317c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e86f423a95e66ac52f95260c6ee5f31de2d14407aa7228e60d673bef93ce64f9"
-    sha256 cellar: :any_skip_relocation, catalina:      "367d604509d4821204ffc997a09f04a24bd646446d25486374cb156c41deabd1"
-    sha256 cellar: :any_skip_relocation, mojave:        "7bdd20fb58a102d1035ea5ccff9514b5f32e3281ac8ae76caf895b5ddd6e9a0d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f085be99de8bb758f92368c5829e1c515e89e0186d0f55186fee4310f9409f53"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5782dff2043e6bfc360d5693cfa27da596d524486c7318293bc257479b7320c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c442259ca09852b1c19fd6217c4c7f2f6128d225d004e6d2ab496c0da2a7317c"
+    sha256 cellar: :any_skip_relocation, monterey:       "43d4914f950bcd70e5ea09e4bdb890e1075b5a8edec30a7d9e36e7a94fb80b71"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e86f423a95e66ac52f95260c6ee5f31de2d14407aa7228e60d673bef93ce64f9"
+    sha256 cellar: :any_skip_relocation, catalina:       "367d604509d4821204ffc997a09f04a24bd646446d25486374cb156c41deabd1"
+    sha256 cellar: :any_skip_relocation, mojave:         "7bdd20fb58a102d1035ea5ccff9514b5f32e3281ac8ae76caf895b5ddd6e9a0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f085be99de8bb758f92368c5829e1c515e89e0186d0f55186fee4310f9409f53"
   end
 
   depends_on "go" => :build
