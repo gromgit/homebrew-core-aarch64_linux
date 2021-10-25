b66ee075de6312947f90545c80eae126a6df0cbd
Bo Anderson <mail@boanderson.me>
2021-10-25 17:41:44 +0000

    node_exporter: update 1.2.2 bottle.

--- Formula/node_exporter.rb
+++ Formula/node_exporter.rb
@@ -13,11 +13,13 @@ class NodeExporter < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "08defe8d74459977bc0bf5b5da1e0c09bada5943fd6bbb78d053deb0f6ad1505"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bbac191f8d01fe6a3cd41a389d02ac800e6b823ddb0a5bb9fc60f4d7e59da41c"
-    sha256 cellar: :any_skip_relocation, catalina:      "71aa1e6052258257c504b3968027c43238e62ab583efaf1937670ba25defce19"
-    sha256 cellar: :any_skip_relocation, mojave:        "d79d282260ddd651b834688c74b5255d74ccd0c67d9c504efe71882503ebd6a5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5937f64800a35b0205c36e679d7d7f83042092e8e9cb5ef71fb145b22e4e296c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "24542f9ea4c259bcc715a11a2be45cf63102ae3a1e51a7f9d1df1774229427c2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "08defe8d74459977bc0bf5b5da1e0c09bada5943fd6bbb78d053deb0f6ad1505"
+    sha256 cellar: :any_skip_relocation, monterey:       "4534d77e58bb1d29981f68562225aa48dcb7aaffd907276ccbb2e99faf8a2d17"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bbac191f8d01fe6a3cd41a389d02ac800e6b823ddb0a5bb9fc60f4d7e59da41c"
+    sha256 cellar: :any_skip_relocation, catalina:       "71aa1e6052258257c504b3968027c43238e62ab583efaf1937670ba25defce19"
+    sha256 cellar: :any_skip_relocation, mojave:         "d79d282260ddd651b834688c74b5255d74ccd0c67d9c504efe71882503ebd6a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5937f64800a35b0205c36e679d7d7f83042092e8e9cb5ef71fb145b22e4e296c"
   end
 
   depends_on "go" => :build
