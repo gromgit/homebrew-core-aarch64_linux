6a72803f29ec6c901e4508a526d1a58c29ba70cd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-25 08:03:05 +0800

    glew: update 2.2.0_1 bottle.

--- Formula/glew.rb
+++ Formula/glew.rb
@@ -11,6 +11,7 @@ class Glew < Formula
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "17420ea2fddc60d424c2113aa9f452603763cacabcf4447457781c9dc974bf78"
     sha256 cellar: :any,                 arm64_big_sur:  "ff3928527b5eca567f758adaec4674495baf760b974e624d06a6e4f9f0540db1"
+    sha256 cellar: :any,                 monterey:       "17e8c8fcc77d132b1d47c47ba5bb3a22a84088aa6c06ec890bf8a8463f363ffc"
     sha256 cellar: :any,                 big_sur:        "c96cbd58749e9e19359058823ef06c2b4b3b621e4751e4970dbc649d6e0f6bae"
     sha256 cellar: :any,                 catalina:       "4afe7a78fbe20c553a42d30e6b14f7696c3636bab810d2907b8843d583c115f2"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "1bad4a1bacddec065a38c9ccb6c87dd5423c554aed6ac1b187fe8164b071267e"
