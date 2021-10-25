e32751260938e402888cfbf4af7bcab60a78a874
Bo Anderson <mail@boanderson.me>
2021-10-25 04:02:33 +0000

    pkcs11-helper: update 1.27.0 bottle.

--- Formula/pkcs11-helper.rb
+++ Formula/pkcs11-helper.rb
@@ -13,11 +13,13 @@ class Pkcs11Helper < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "79eaec51f13a0bda941703a652f790a2306233428878fd9c2beaca7fcbdb9422"
-    sha256 cellar: :any,                 big_sur:       "84c49ac08cc1c9f222742d7aa3bd628b32673d2376efbe7059fc8d355ff540ad"
-    sha256 cellar: :any,                 catalina:      "5cdee7e99d40242d5026b2fbb448f7390e272bb610f8f7a125ab599941c73a06"
-    sha256 cellar: :any,                 mojave:        "3bc3ca9909c0cc67a51ab579ed498dbc9c9dc2842d572b5adc4c715405f78ada"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6887bf48e4dcb35860dcc487b8ed57f883f2c5ff8c06722ac43b006be529b73c"
+    sha256 cellar: :any,                 arm64_monterey: "400fcc51aa64cff0488cf67473f4854360dbd148ce7c00aa0c6d7ab48b60d473"
+    sha256 cellar: :any,                 arm64_big_sur:  "79eaec51f13a0bda941703a652f790a2306233428878fd9c2beaca7fcbdb9422"
+    sha256 cellar: :any,                 monterey:       "81f13fb6bdc7b87d4c942b76b627e57a1dcc15ef78e77451c85ed23db5f4fa39"
+    sha256 cellar: :any,                 big_sur:        "84c49ac08cc1c9f222742d7aa3bd628b32673d2376efbe7059fc8d355ff540ad"
+    sha256 cellar: :any,                 catalina:       "5cdee7e99d40242d5026b2fbb448f7390e272bb610f8f7a125ab599941c73a06"
+    sha256 cellar: :any,                 mojave:         "3bc3ca9909c0cc67a51ab579ed498dbc9c9dc2842d572b5adc4c715405f78ada"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6887bf48e4dcb35860dcc487b8ed57f883f2c5ff8c06722ac43b006be529b73c"
   end
 
   depends_on "autoconf" => :build
