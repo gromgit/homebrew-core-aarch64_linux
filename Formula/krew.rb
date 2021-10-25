8751098925c48b3468f1c437071d05626c9d3f0e
Bo Anderson <mail@boanderson.me>
2021-10-25 06:57:33 +0000

    krew: update 0.4.2 bottle.

--- Formula/krew.rb
+++ Formula/krew.rb
@@ -8,11 +8,13 @@ class Krew < Formula
   head "https://github.com/kubernetes-sigs/krew.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9bd45bf4ce538f251c393ba6279d18c7599866abb46e556fb9fa26e2e7473249"
-    sha256 cellar: :any_skip_relocation, big_sur:       "17c8fd8f71c8a92ede7c9f9d56382b1c6ffc962d4a7f6e9719e7e9a9d9f1755a"
-    sha256 cellar: :any_skip_relocation, catalina:      "3405b2bbbedd29dad0cec63a9a74d959ff9f8ef1ee1ce8c9d540156a225492ed"
-    sha256 cellar: :any_skip_relocation, mojave:        "500cfe13d69c132e42b689b2c575317dbdb1d4d1364d0edaeb53a3db07ebf055"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "542f5e62f150e93e2dcc5fc91493301ad8c96f2e014735b90b47f500a0a68c3a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "01c440b830f40fd4f1aaec78a00f8ab2ab1602bba6dc1be7a367d44bd6941edf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9bd45bf4ce538f251c393ba6279d18c7599866abb46e556fb9fa26e2e7473249"
+    sha256 cellar: :any_skip_relocation, monterey:       "532c892b298dcb35aec75424e3f102f4223b4c068c9999e67775ecb6515a6b96"
+    sha256 cellar: :any_skip_relocation, big_sur:        "17c8fd8f71c8a92ede7c9f9d56382b1c6ffc962d4a7f6e9719e7e9a9d9f1755a"
+    sha256 cellar: :any_skip_relocation, catalina:       "3405b2bbbedd29dad0cec63a9a74d959ff9f8ef1ee1ce8c9d540156a225492ed"
+    sha256 cellar: :any_skip_relocation, mojave:         "500cfe13d69c132e42b689b2c575317dbdb1d4d1364d0edaeb53a3db07ebf055"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "542f5e62f150e93e2dcc5fc91493301ad8c96f2e014735b90b47f500a0a68c3a"
   end
 
   depends_on "go" => :build
