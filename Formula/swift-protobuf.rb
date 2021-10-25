50190f973972a130d1fc95fc87b1cb9a6c1285e0
Bo Anderson <mail@boanderson.me>
2021-10-25 17:02:23 +0000

    swift-protobuf: update 1.18.0_1 bottle.

--- Formula/swift-protobuf.rb
+++ Formula/swift-protobuf.rb
@@ -8,10 +8,12 @@ class SwiftProtobuf < Formula
   head "https://github.com/apple/swift-protobuf.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5a1c709bf25fa4c7d02cdbcb5ff481fa8d0ba37897a03c6f1a58b1d38248bc32"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ae0390c500b0ca6e9665f23c8e4a0224f790ed232184d530bf9c8c772f068f89"
-    sha256 cellar: :any_skip_relocation, catalina:      "172f3e61a551548eeb7d93ae2fb001b0b99c38cc440c905aa72d907037730a4e"
-    sha256                               x86_64_linux:  "0b7c5288ec35491c7a435cd5cae5deb553221c6ee1d50946af12271f9f264fbe"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a5c84ddd827e0126c615d5dd274beda8d257d1bf63b27ceb3c615bd12eaa0ef"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a1c709bf25fa4c7d02cdbcb5ff481fa8d0ba37897a03c6f1a58b1d38248bc32"
+    sha256 cellar: :any_skip_relocation, monterey:       "81496b6246fa89194d7ffa490ae303332ffc6dcd2f2efd7786a25a3a82202651"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ae0390c500b0ca6e9665f23c8e4a0224f790ed232184d530bf9c8c772f068f89"
+    sha256 cellar: :any_skip_relocation, catalina:       "172f3e61a551548eeb7d93ae2fb001b0b99c38cc440c905aa72d907037730a4e"
+    sha256                               x86_64_linux:   "0b7c5288ec35491c7a435cd5cae5deb553221c6ee1d50946af12271f9f264fbe"
   end
 
   depends_on xcode: ["8.3", :build]
