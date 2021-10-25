2cf44c735e241a1ec1ab3b54dec538a86953da0b
Bo Anderson <mail@boanderson.me>
2021-10-25 18:06:47 +0000

    poco: update 1.11.0 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -12,11 +12,13 @@ class Poco < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a9b4b2d1c1c24b81acea5947325bffd2af88426291857c3c23592659ce3957ca"
-    sha256 cellar: :any,                 big_sur:       "ec8ca74c0085ccb363451475afd0913463730f5129205ff1e76edf43bcafae1d"
-    sha256 cellar: :any,                 catalina:      "15372ab13fe7e6269b7038532c0a05675b12f71df5f5a69c4930d57a16a598c1"
-    sha256 cellar: :any,                 mojave:        "2bb42c67ece76fde31d9dd8d79fad23fecbf9ae33231f1601a335829b1f6f3c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "52fd3511305d75b4c25b2a562a6c3f741cce5abe050f3107a8b9fa2310f9c8de"
+    sha256 cellar: :any,                 arm64_monterey: "bd7baa8b2dc785ce03e5c20dc658bbd256820fdc715e6a5dbf667d0ad64acdb1"
+    sha256 cellar: :any,                 arm64_big_sur:  "a9b4b2d1c1c24b81acea5947325bffd2af88426291857c3c23592659ce3957ca"
+    sha256 cellar: :any,                 monterey:       "3decf0c15163daf9c2e384499567ebf20a6385fc5f449e4e1c4a669aacc28ba1"
+    sha256 cellar: :any,                 big_sur:        "ec8ca74c0085ccb363451475afd0913463730f5129205ff1e76edf43bcafae1d"
+    sha256 cellar: :any,                 catalina:       "15372ab13fe7e6269b7038532c0a05675b12f71df5f5a69c4930d57a16a598c1"
+    sha256 cellar: :any,                 mojave:         "2bb42c67ece76fde31d9dd8d79fad23fecbf9ae33231f1601a335829b1f6f3c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52fd3511305d75b4c25b2a562a6c3f741cce5abe050f3107a8b9fa2310f9c8de"
   end
 
   depends_on "cmake" => :build
