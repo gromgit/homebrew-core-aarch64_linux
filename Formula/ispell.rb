1d24d36c1eb0092512550c265749efa38c3798f9
Bo Anderson <mail@boanderson.me>
2021-10-25 18:51:35 +0000

    ispell: update 3.4.04 bottle.

--- Formula/ispell.rb
+++ Formula/ispell.rb
@@ -11,11 +11,13 @@ class Ispell < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "f48b72490babfc3d3debf6eeeb34fb27516dd7a360322e05217f75f9a060642d"
-    sha256 big_sur:       "ed399105823ef8d4c978b9f29675fe52766cefb3c33e969a36fc0ff0e33dba13"
-    sha256 catalina:      "bd78389107e1457352b3c9eb88a73d434cffae5764455ea461e4c201f5180b85"
-    sha256 mojave:        "85bb0fb8d13a18cffdc0ddd2e9ed1d4d007b884967d0952a7a174135ea5cc416"
-    sha256 x86_64_linux:  "83022ccd0a50207196c2d29a2b3c08003157b1e77bb48ed796baa4e9c3e9092f"
+    sha256 arm64_monterey: "62553e9203c1553d699d0014224a6ff9f934844a3377b90362e2d33ed079f2dd"
+    sha256 arm64_big_sur:  "f48b72490babfc3d3debf6eeeb34fb27516dd7a360322e05217f75f9a060642d"
+    sha256 monterey:       "4cb7e05fb71b9db13a497fd2ef4df875187e5ba1976c79e8776aa9fbe2f6da63"
+    sha256 big_sur:        "ed399105823ef8d4c978b9f29675fe52766cefb3c33e969a36fc0ff0e33dba13"
+    sha256 catalina:       "bd78389107e1457352b3c9eb88a73d434cffae5764455ea461e4c201f5180b85"
+    sha256 mojave:         "85bb0fb8d13a18cffdc0ddd2e9ed1d4d007b884967d0952a7a174135ea5cc416"
+    sha256 x86_64_linux:   "83022ccd0a50207196c2d29a2b3c08003157b1e77bb48ed796baa4e9c3e9092f"
   end
 
   uses_from_macos "bison" => :build
