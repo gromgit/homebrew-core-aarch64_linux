3f9b9e1ece70ca57d8ccf6ec2000ca368e6dafc7
Bo Anderson <mail@boanderson.me>
2021-10-25 06:18:55 +0000

    highlight: update 4.1 bottle.

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -12,11 +12,13 @@ class Highlight < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "90c97137575b00aaa9050fb14f049e6b1dcfe236ae4266d7e684706f09a58f0f"
-    sha256 big_sur:       "e364d145b581f0622acf1cc9476142b8e318efa60495ca6e9c1f44ed560a2d66"
-    sha256 catalina:      "4238b72def77283c7cedf385639bd9b28b5d0e3fb5d5c0bc6ec6fe6d06ae7bcf"
-    sha256 mojave:        "81057a5898f59793eda32340275cb8bf87da61e6330bb5d0e925690c678b606b"
-    sha256 x86_64_linux:  "f6f656122700a511ed946ca5a3d4db0e05db5c6f4f988567e866dfc50938a406"
+    sha256 arm64_monterey: "6727bd0348ec08e9855fd4574cf8a564d94cba5c3195f1bb9852f9733c351f08"
+    sha256 arm64_big_sur:  "90c97137575b00aaa9050fb14f049e6b1dcfe236ae4266d7e684706f09a58f0f"
+    sha256 monterey:       "4186adee8552304558aa2fd7af70db3355b92650564324b6cd02770bd22d4887"
+    sha256 big_sur:        "e364d145b581f0622acf1cc9476142b8e318efa60495ca6e9c1f44ed560a2d66"
+    sha256 catalina:       "4238b72def77283c7cedf385639bd9b28b5d0e3fb5d5c0bc6ec6fe6d06ae7bcf"
+    sha256 mojave:         "81057a5898f59793eda32340275cb8bf87da61e6330bb5d0e925690c678b606b"
+    sha256 x86_64_linux:   "f6f656122700a511ed946ca5a3d4db0e05db5c6f4f988567e866dfc50938a406"
   end
 
   depends_on "boost" => :build
