bd4eade95ac5f55851715e2a7823a00415c089fe
Rui Chen <rui@chenrui.dev>
2021-10-25 00:21:47 -0400

    vcsh 2.0.3
    * vcsh 2.0.3
    * vcsh: fix build failure
      Also, remove `SED` flag, as this is no longer necessary.
    
      The build failure has been reported at RichiH/vcsh#321.
    
    Closes #87971.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vcsh.rb
+++ Formula/vcsh.rb
@@ -1,8 +1,8 @@
 class Vcsh < Formula
   desc "Config manager based on git"
   homepage "https://github.com/RichiH/vcsh"
-  url "https://github.com/RichiH/vcsh/releases/download/v2.0.2/vcsh-2.0.2.tar.xz"
-  sha256 "3ffc0bbb43c76620c8234c98f4ae94d0a99d24bb240497aab730979a8d23ad61"
+  url "https://github.com/RichiH/vcsh/releases/download/v2.0.3/vcsh-2.0.3.tar.xz"
+  sha256 "e772596111fb26750bc688d9c836fcd73770b1f24bad08b5ad23189666736204"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -10,12 +10,16 @@ class Vcsh < Formula
     sha256 cellar: :any_skip_relocation, all: "40f77813b0e23090862c769daaa02a99ddc179ca1dae0e9834827dc02e16ce5b"
   end
 
+  # Fix build failure with BSD `install`. Reported upstream at
+  # https://github.com/RichiH/vcsh/issues/321
+  patch :DATA
+
   def install
-    # Set GIT, SED, and GREP to prevent
+    # Set GIT, and GREP to prevent
     # hardcoding shim references and absolute paths
     system "./configure", "--with-zsh-completion-dir=#{zsh_completion}",
                           "--with-bash-completion-dir=#{bash_completion}",
-                          "GIT=git", "SED=sed", "GREP=grep",
+                          "GIT=git", "GREP=grep",
                           *std_configure_args
     system "make", "install"
 
@@ -27,3 +31,24 @@ class Vcsh < Formula
     assert_match "Initialized empty", shell_output("#{bin}/vcsh init test").strip
   end
 end
+
+__END__
+diff --git a/Makefile.in b/Makefile.in
+index a58e41e..824b0f1 100644
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -848,10 +848,12 @@ uninstall-man: uninstall-man1
+ @IS_SDIST_FALSE@	$(RONN) < $< > $@
+ 
+ completions/$(TRANSFORMED_PACKAGE_NAME): completions/vcsh.bash
+-	install -D $< $@
++	mkdir -p $(dir $@)
++	install $< $@
+ 
+ completions/_$(TRANSFORMED_PACKAGE_NAME): completions/vcsh.zsh
+-	install -D $< $@
++	mkdir -p $(dir $@)
++	install $< $@
+ 
+ .version: $(shell $(AWK) '{print ".git/" $$2}' .git/HEAD 2>/dev/null ||:)
+ 	[ -e "$@" ] && mv "$@" "$@-prev" || $(if $<,touch,cp "$(srcdir)/.tarball-version") "$@-prev"
