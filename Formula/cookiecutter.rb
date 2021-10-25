2cbd4bdda974b04038413db050452715bd0ec9dc
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-25 01:51:27 +0000

    cookiecutter: update 1.7.3_1 bottle.

--- Formula/cookiecutter.rb
+++ Formula/cookiecutter.rb
@@ -10,12 +10,13 @@ class Cookiecutter < Formula
   head "https://github.com/cookiecutter/cookiecutter.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c47bd026d10ec8f3c6267c689189abec6cdf90feb8879ddc498207d282219000"
-    sha256 cellar: :any_skip_relocation, monterey:      "c48e22fb646e36938d0d5f7fe2679e9dde7a8edb8e92537f839e49c21e4f9bee"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9db1b7554b40b410f51c337f60f6584cfe63f9d13764d6190976a7f83446aea9"
-    sha256 cellar: :any_skip_relocation, catalina:      "ccaafe5436a8eb27c99c924b676de2601ca87413f9c980b6697e6144e32e5392"
-    sha256 cellar: :any_skip_relocation, mojave:        "ffbc7736257952c954b380cc13a56c746cacb3991778d2a424eb0ab39129dc3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "295c5c1f199c24086400af1e87559a8affa98631a2d847883c3edebca2c47348"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a4ec359ec5071b56b351672f75a833d664952e6cb91fd2fd7af9eaa820bb4cc0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c47bd026d10ec8f3c6267c689189abec6cdf90feb8879ddc498207d282219000"
+    sha256 cellar: :any_skip_relocation, monterey:       "c48e22fb646e36938d0d5f7fe2679e9dde7a8edb8e92537f839e49c21e4f9bee"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9db1b7554b40b410f51c337f60f6584cfe63f9d13764d6190976a7f83446aea9"
+    sha256 cellar: :any_skip_relocation, catalina:       "ccaafe5436a8eb27c99c924b676de2601ca87413f9c980b6697e6144e32e5392"
+    sha256 cellar: :any_skip_relocation, mojave:         "ffbc7736257952c954b380cc13a56c746cacb3991778d2a424eb0ab39129dc3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "295c5c1f199c24086400af1e87559a8affa98631a2d847883c3edebca2c47348"
   end
 
   depends_on "python@3.10"
