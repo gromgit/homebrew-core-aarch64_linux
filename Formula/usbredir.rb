4d947ac676055cfdc5295a4e3b759dd610b4900f
Bo Anderson <mail@boanderson.me>
2021-10-25 05:47:41 +0000

    usbredir: update 0.11.0 bottle.

--- Formula/usbredir.rb
+++ Formula/usbredir.rb
@@ -11,11 +11,13 @@ class Usbredir < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "301a06eaf3c9f51d349f92b75d898857aeebb882ec2a1a83cbe1101cc2c3a58f"
-    sha256 cellar: :any, big_sur:       "1d29697803e3eec97d1d8ebe00d9dba354b1ad51122913414752380c0eedacd7"
-    sha256 cellar: :any, catalina:      "ac6f36424efd438cd495ac1f2309b3d058541a43a71cdd0477cd5d4f602c130e"
-    sha256 cellar: :any, mojave:        "16c79281da519d94bf5f10c1521e84f38e1465bc4f2835fa8ab9eb576af479cc"
-    sha256               x86_64_linux:  "73ba4b4154c70fb9e65a95c36e6cf6e2eb7cd09c90c8aa5f2d844bcfb8502694"
+    sha256 cellar: :any, arm64_monterey: "eafd6296436a1e77dca32836fadea6fc70a4f32c58f3f780fd9da91fec06cebf"
+    sha256 cellar: :any, arm64_big_sur:  "301a06eaf3c9f51d349f92b75d898857aeebb882ec2a1a83cbe1101cc2c3a58f"
+    sha256 cellar: :any, monterey:       "e26c68bb09c3584216b8427f8297b2d91a10b12299ecd207a96c3cb5d7a924a4"
+    sha256 cellar: :any, big_sur:        "1d29697803e3eec97d1d8ebe00d9dba354b1ad51122913414752380c0eedacd7"
+    sha256 cellar: :any, catalina:       "ac6f36424efd438cd495ac1f2309b3d058541a43a71cdd0477cd5d4f602c130e"
+    sha256 cellar: :any, mojave:         "16c79281da519d94bf5f10c1521e84f38e1465bc4f2835fa8ab9eb576af479cc"
+    sha256               x86_64_linux:   "73ba4b4154c70fb9e65a95c36e6cf6e2eb7cd09c90c8aa5f2d844bcfb8502694"
   end
 
   depends_on "meson" => :build
