8f493a0667b4f2a9685e83d6662d425dd25df9cc
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-25 00:36:55 +0000

    flake8: update 4.0.1 bottle.

--- Formula/flake8.rb
+++ Formula/flake8.rb
@@ -10,6 +10,7 @@ class Flake8 < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "0df982d4a3ed80b694876262243dc7356b3f21bec95e016843964588ea268ef2"
+    sha256 cellar: :any_skip_relocation, monterey:      "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
     sha256 cellar: :any_skip_relocation, big_sur:       "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
     sha256 cellar: :any_skip_relocation, catalina:      "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
     sha256 cellar: :any_skip_relocation, mojave:        "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
