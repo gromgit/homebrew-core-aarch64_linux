dbdbef7373a58500dd74854b5ce747a152568078
Bo Anderson <mail@boanderson.me>
2021-10-25 15:57:54 +0000

    less: update 590 bottle.

--- Formula/less.rb
+++ Formula/less.rb
@@ -11,11 +11,13 @@ class Less < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "bc5f182ccbe6676c1647939b2dd1b66e4f66eb920bb865f94d041fccdb2bd493"
-    sha256 cellar: :any,                 big_sur:       "7b8ea7c58b438ef80d6b13fd988061543ab3413a40113cd30644cb22fa6f1081"
-    sha256 cellar: :any,                 catalina:      "ccbcf747eac1e0a8338be43a6be0e4f3fb241394a6bc0c921b6e51b4ca32c042"
-    sha256 cellar: :any,                 mojave:        "916e88216d17654f290affa519d85ad295696dc6c753d3311ed71fb4cc2f9268"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d3b89016d59864389e96b2c234f9f8e98254374c1b1d20847a03a8fa8bce6d2"
+    sha256 cellar: :any,                 arm64_monterey: "6342e6c36bbb6fb617994ff5904e389d3a3a15826f25097e6d60450a30b3c89d"
+    sha256 cellar: :any,                 arm64_big_sur:  "bc5f182ccbe6676c1647939b2dd1b66e4f66eb920bb865f94d041fccdb2bd493"
+    sha256 cellar: :any,                 monterey:       "c619cd7da95e0b54f9b75c8737e4745df5b83d918a8ee7d09a8b94e1a3f635b0"
+    sha256 cellar: :any,                 big_sur:        "7b8ea7c58b438ef80d6b13fd988061543ab3413a40113cd30644cb22fa6f1081"
+    sha256 cellar: :any,                 catalina:       "ccbcf747eac1e0a8338be43a6be0e4f3fb241394a6bc0c921b6e51b4ca32c042"
+    sha256 cellar: :any,                 mojave:         "916e88216d17654f290affa519d85ad295696dc6c753d3311ed71fb4cc2f9268"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d3b89016d59864389e96b2c234f9f8e98254374c1b1d20847a03a8fa8bce6d2"
   end
 
   head do
