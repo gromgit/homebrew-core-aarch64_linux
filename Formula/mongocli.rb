87f0428e2c95d62660c20a9f4dd389b92289e538
Bo Anderson <mail@boanderson.me>
2021-10-25 05:40:28 +0000

    mongocli: update 1.20.4 bottle.

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -6,11 +6,13 @@ class Mongocli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d231136f5c09c45f88d2f6489c2ae7779c64c6e96875b7ef28a2f179b562f229"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ce9f5a648f7f52cce7f974dcaef59237a7c21ef42c4bb9f8f765edb31902fd65"
-    sha256 cellar: :any_skip_relocation, catalina:      "690f22d0ff9cca92e48fae6e6681284405ea513915f1d22703451eb8e10e13c5"
-    sha256 cellar: :any_skip_relocation, mojave:        "75cbbb2f6aba7a22ae2d878c29f30cc784690bce855a7c4e013c0ddfddacc601"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e24a77c82c0a7db4a4693bbe5c932d7aaf4c9ff866e0851573a8e76c42eccb31"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "25a32b44606022f20283b2fd9cff5de1b2d49fe32222b4d6dd8ae548ce75f7cb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d231136f5c09c45f88d2f6489c2ae7779c64c6e96875b7ef28a2f179b562f229"
+    sha256 cellar: :any_skip_relocation, monterey:       "4040b0c8723ca498573419ba5af8152f8dba63954dccd51f7a307e24ef5680aa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce9f5a648f7f52cce7f974dcaef59237a7c21ef42c4bb9f8f765edb31902fd65"
+    sha256 cellar: :any_skip_relocation, catalina:       "690f22d0ff9cca92e48fae6e6681284405ea513915f1d22703451eb8e10e13c5"
+    sha256 cellar: :any_skip_relocation, mojave:         "75cbbb2f6aba7a22ae2d878c29f30cc784690bce855a7c4e013c0ddfddacc601"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e24a77c82c0a7db4a4693bbe5c932d7aaf4c9ff866e0851573a8e76c42eccb31"
   end
 
   depends_on "go" => :build
