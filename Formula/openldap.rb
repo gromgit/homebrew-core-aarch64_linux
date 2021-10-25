983ee2a2ffe4048e7ee5e44f62014c222e402d5f
Bevan Kay <email@bevankay.me>
2021-10-26 09:22:50 +1100

    openldap 2.6.0

--- Formula/openldap.rb
+++ Formula/openldap.rb
@@ -1,10 +1,10 @@
 class Openldap < Formula
   desc "Open source suite of directory software"
   homepage "https://www.openldap.org/software/"
-  url "https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.5.8.tgz"
-  mirror "http://fresh-center.net/linux/misc/openldap-2.5.8.tgz"
-  mirror "http://fresh-center.net/linux/misc/legacy/openldap-2.5.8.tgz"
-  sha256 "366ea1c3b24202de4481978b632128c0cfe4148d4ae13cabf93a1f38c56472dc"
+  url "https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.6.0.tgz"
+  mirror "http://fresh-center.net/linux/misc/openldap-2.6.0.tgz"
+  mirror "http://fresh-center.net/linux/misc/legacy/openldap-2.6.0.tgz"
+  sha256 "b71c580eac573e9aba15d95f33dd4dd08f2ed4f0d7fc09e08ad4be7ed1e41a4f"
   license "OLDAP-2.8"
 
   livecheck do
@@ -34,6 +34,12 @@ class Openldap < Formula
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
     sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  # Fix https://bugs.openldap.org/show_bug.cgi?id=9733, remove in next release
+  patch do
+    url "https://git.openldap.org/openldap/openldap/-/commit/eb989be4081cf996bd7e7eb6a529bbc1dc483a59.patch"
+    sha256 "d083c2ca7c0ec5c211df53a98ffb02e1ba926abf108bbe4d88550fa6064536a4"
   end
 
   def install
