d375b1941d3e2968c0bdd55cfc0afbe5c7ee6a47
Bo Anderson <mail@boanderson.me>
2021-10-25 17:51:07 +0000

    sourcery: update 1.6.0 bottle.

--- Formula/sourcery.rb
+++ Formula/sourcery.rb
@@ -7,8 +7,10 @@ class Sourcery < Formula
   head "https://github.com/krzysztofzablocki/Sourcery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "17f5cb2a8f9037b2b3a106f1d11783271174a68ab582782b08118b3c51226b07"
-    sha256 cellar: :any, big_sur:       "1d91abe4f760e2a3d232ac1ee3eb24b636a8948049f8f4c4bbb13be4bc2ce072"
+    sha256 cellar: :any, arm64_monterey: "010fd4876fb047ca2bd14feeda78d755987c71ebbd5320d784e585bb37b5779c"
+    sha256 cellar: :any, arm64_big_sur:  "17f5cb2a8f9037b2b3a106f1d11783271174a68ab582782b08118b3c51226b07"
+    sha256 cellar: :any, monterey:       "0cd3dad2ea6d14c4a50706f7d0640265982dda7aa67cdf8f3ad4d011773bc3bc"
+    sha256 cellar: :any, big_sur:        "1d91abe4f760e2a3d232ac1ee3eb24b636a8948049f8f4c4bbb13be4bc2ce072"
   end
 
   depends_on xcode: "13.0"
