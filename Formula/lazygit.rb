d16b25b945161892ab57d4712e6db4c380dc4c5e
Bo Anderson <mail@boanderson.me>
2021-10-25 05:46:06 +0000

    lazygit: update 0.30 bottle.

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -6,10 +6,12 @@ class Lazygit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9bc3703fa3c8817ee39a6e3bbccdac73d908796d7ef942035b0c0261af1e8362"
-    sha256 cellar: :any_skip_relocation, big_sur:       "478558d51847a2340d633326a8d7cc6be8480ae1817f7041bf99b14ab91225dd"
-    sha256 cellar: :any_skip_relocation, catalina:      "82b1dbb31e577d4bfef39d685b75a6a3088a8a65d0079868a94ed3d48348e7d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "215ad0bf0b5e3ae5b928c384e2ee0f5995871e836e34439c29b3559be27b6e4f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "393598282307b54f828ac8d9b7c9ec71cbab6db3e42f7a6d63708e54e31af379"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9bc3703fa3c8817ee39a6e3bbccdac73d908796d7ef942035b0c0261af1e8362"
+    sha256 cellar: :any_skip_relocation, monterey:       "ed8f39c51540da4415a83374a5a97983f963aaed7cae26c0a2eb1491b1b25ea8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "478558d51847a2340d633326a8d7cc6be8480ae1817f7041bf99b14ab91225dd"
+    sha256 cellar: :any_skip_relocation, catalina:       "82b1dbb31e577d4bfef39d685b75a6a3088a8a65d0079868a94ed3d48348e7d0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "215ad0bf0b5e3ae5b928c384e2ee0f5995871e836e34439c29b3559be27b6e4f"
   end
 
   depends_on "go" => :build
