83b4c8be6d773a0cbdfb065c5916451bde84e2be
Bo Anderson <mail@boanderson.me>
2021-10-25 03:34:25 +0000

    netpbm: update 10.86.25 bottle.

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -16,10 +16,12 @@ class Netpbm < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "e79a279c0297fa7dec50b94e507b1c6a9ca1897f4227e21ed191d20b07066514"
-    sha256 cellar: :any,                 big_sur:       "3d97e71c8f9dfca480d84aae7c48cf78c41b9d0c0900714eb77a19ea85b4d7d3"
-    sha256 cellar: :any,                 catalina:      "a7222d95a418067c509245a96723c847a627f931904e843067454f91d95a4e6c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ef7fc652aa8157e601a56fd3c49077b58d24603ef771288f26f7122ec4cf03c"
+    sha256 cellar: :any,                 arm64_monterey: "c8ece85fb0c78ef1ae3282f8c92446f10bb01bdf3ad40a558b6702397e46979b"
+    sha256 cellar: :any,                 arm64_big_sur:  "e79a279c0297fa7dec50b94e507b1c6a9ca1897f4227e21ed191d20b07066514"
+    sha256 cellar: :any,                 monterey:       "e209ccde19aea51b6a1c517ea72978d44417ad53632af5522488f382b866afda"
+    sha256 cellar: :any,                 big_sur:        "3d97e71c8f9dfca480d84aae7c48cf78c41b9d0c0900714eb77a19ea85b4d7d3"
+    sha256 cellar: :any,                 catalina:       "a7222d95a418067c509245a96723c847a627f931904e843067454f91d95a4e6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ef7fc652aa8157e601a56fd3c49077b58d24603ef771288f26f7122ec4cf03c"
   end
 
   depends_on "jasper"
