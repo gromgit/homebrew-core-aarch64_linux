e371915be51b0b617fe5d2b1f388ca088e2cce99
Bo Anderson <mail@boanderson.me>
2021-10-25 05:08:28 +0000

    siege: update 4.1.1 bottle.

--- Formula/siege.rb
+++ Formula/siege.rb
@@ -11,11 +11,13 @@ class Siege < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "14c77189b3ea80705dd31c790cd65aca11d8752a5ff60f9f56abc190fb5dd860"
-    sha256 big_sur:       "3a0700e1b41361e960dd0266bde486ab89afb27a706de7d970325889ae39bc7c"
-    sha256 catalina:      "45ac6999d1446257c046416c6be4ebea0e8aae69ca49ef6ac2aba2844fde3281"
-    sha256 mojave:        "650238923f354fcec454b5bd6219ab26bbb3680d440420216446a3878b382b92"
-    sha256 x86_64_linux:  "14d0a4594a88c19185f06de0bc12a626b2dde2740868250bc58312e57dabbc74"
+    sha256 arm64_monterey: "c57f85c927a9a67ab09980855cf2498335ed2a9849bae6eb8eb58ce58bc7db89"
+    sha256 arm64_big_sur:  "14c77189b3ea80705dd31c790cd65aca11d8752a5ff60f9f56abc190fb5dd860"
+    sha256 monterey:       "01dc1bb6290f7ada1158c978971ebae843e1c58e74aa59716a770286a159baf9"
+    sha256 big_sur:        "3a0700e1b41361e960dd0266bde486ab89afb27a706de7d970325889ae39bc7c"
+    sha256 catalina:       "45ac6999d1446257c046416c6be4ebea0e8aae69ca49ef6ac2aba2844fde3281"
+    sha256 mojave:         "650238923f354fcec454b5bd6219ab26bbb3680d440420216446a3878b382b92"
+    sha256 x86_64_linux:   "14d0a4594a88c19185f06de0bc12a626b2dde2740868250bc58312e57dabbc74"
   end
 
   depends_on "openssl@1.1"
