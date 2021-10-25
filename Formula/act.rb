3ab2604b1e630d4eccab40d0e78f29bd912a72b8
Bo Anderson <mail@boanderson.me>
2021-10-25 03:53:03 +0000

    act: update 0.2.24 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -7,11 +7,13 @@ class Act < Formula
   head "https://github.com/nektos/act.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a20e74be00a60813dda3b8cf2ff6b15ee16127989b9b5e316796b311391997c9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "dda88098e1615b3d69ea03a8edefde476ea2128427ee12915ef5913b3adde8c4"
-    sha256 cellar: :any_skip_relocation, catalina:      "91b2522b430fc0a8045ef844633903b651432a4f355ce2e1b142ab35e55254bf"
-    sha256 cellar: :any_skip_relocation, mojave:        "20e5acc8fe34351772ffee1367febe509045cf7abea85e24112040043b9399c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f09351f33ce63078e1b2e195fc7b12405b15bc90eff9065061972827aaa910ee"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e22428d1c18551bebecb98493eb4be72782e259cf15a0707ba0e92fcb808b553"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a20e74be00a60813dda3b8cf2ff6b15ee16127989b9b5e316796b311391997c9"
+    sha256 cellar: :any_skip_relocation, monterey:       "90dac515cdb91ff6fa0b342de64efb8c24fe9691f6312976ebfd62765cd44d68"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dda88098e1615b3d69ea03a8edefde476ea2128427ee12915ef5913b3adde8c4"
+    sha256 cellar: :any_skip_relocation, catalina:       "91b2522b430fc0a8045ef844633903b651432a4f355ce2e1b142ab35e55254bf"
+    sha256 cellar: :any_skip_relocation, mojave:         "20e5acc8fe34351772ffee1367febe509045cf7abea85e24112040043b9399c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f09351f33ce63078e1b2e195fc7b12405b15bc90eff9065061972827aaa910ee"
   end
 
   depends_on "go" => :build
