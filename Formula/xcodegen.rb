10d86409049cfc335a9a561aff27eb48168873a2
Bo Anderson <mail@boanderson.me>
2021-10-25 04:02:42 +0000

    xcodegen: update 2.25.0 bottle.

--- Formula/xcodegen.rb
+++ Formula/xcodegen.rb
@@ -7,9 +7,11 @@ class Xcodegen < Formula
   head "https://github.com/yonaskolb/XcodeGen.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "558d80be972d11ef397faf078060b06eec90a8c0e8371202ec5691660c27c3af"
-    sha256 cellar: :any_skip_relocation, big_sur:       "88f2036496a41621c151480d5841a7f7bed90157a292e7cd48efb121a082a29b"
-    sha256 cellar: :any_skip_relocation, catalina:      "366ce43fa5f03f0fa975bb4f1433b4aa8a8ba3a745148cf4800b7e25266aa800"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c75414fd0917557984b1f4157ffa8b604c41c3de094e503e68500e877917a4a8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "558d80be972d11ef397faf078060b06eec90a8c0e8371202ec5691660c27c3af"
+    sha256 cellar: :any_skip_relocation, monterey:       "45edd1789a68c07909c29dad1eb25cda02a8fe50e5d44e62660949170c84964d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "88f2036496a41621c151480d5841a7f7bed90157a292e7cd48efb121a082a29b"
+    sha256 cellar: :any_skip_relocation, catalina:       "366ce43fa5f03f0fa975bb4f1433b4aa8a8ba3a745148cf4800b7e25266aa800"
   end
 
   depends_on xcode: ["10.2", :build]
