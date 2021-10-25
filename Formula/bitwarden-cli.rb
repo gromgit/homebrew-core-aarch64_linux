3f4e4a5f9403a30ee7a7aa970f8409d09cc7b13d
Bo Anderson <mail@boanderson.me>
2021-10-25 05:00:11 +0000

    bitwarden-cli: update 1.18.1 bottle.

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -8,11 +8,13 @@ class BitwardenCli < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "23eba2bfa483bcc6a1b94cadb8e635bcfb2905bb32601eb12bf45b6a583e8298"
-    sha256 cellar: :any_skip_relocation, big_sur:       "147a06ea8c66d129e89ae300ec1516be284ebe5ab5091a0146393ad6b3ee1ada"
-    sha256 cellar: :any_skip_relocation, catalina:      "147a06ea8c66d129e89ae300ec1516be284ebe5ab5091a0146393ad6b3ee1ada"
-    sha256 cellar: :any_skip_relocation, mojave:        "147a06ea8c66d129e89ae300ec1516be284ebe5ab5091a0146393ad6b3ee1ada"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23eba2bfa483bcc6a1b94cadb8e635bcfb2905bb32601eb12bf45b6a583e8298"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ec4e52725887191860f53231a6b5ec374577f53635ad667dadff6153d3b00fee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23eba2bfa483bcc6a1b94cadb8e635bcfb2905bb32601eb12bf45b6a583e8298"
+    sha256 cellar: :any_skip_relocation, monterey:       "f6962975766dc80f0c32ae8ab2b09b258328784cfde090c29e48b33ba9609fde"
+    sha256 cellar: :any_skip_relocation, big_sur:        "147a06ea8c66d129e89ae300ec1516be284ebe5ab5091a0146393ad6b3ee1ada"
+    sha256 cellar: :any_skip_relocation, catalina:       "147a06ea8c66d129e89ae300ec1516be284ebe5ab5091a0146393ad6b3ee1ada"
+    sha256 cellar: :any_skip_relocation, mojave:         "147a06ea8c66d129e89ae300ec1516be284ebe5ab5091a0146393ad6b3ee1ada"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23eba2bfa483bcc6a1b94cadb8e635bcfb2905bb32601eb12bf45b6a583e8298"
   end
 
   depends_on "node"
