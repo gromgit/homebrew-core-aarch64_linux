396d11b4d80049ff6e167adb66529887a6232cd0
Bo Anderson <mail@boanderson.me>
2021-10-25 17:08:42 +0000

    universal-ctags: update p5.9.20211024.0 bottle.

--- Formula/universal-ctags.rb
+++ Formula/universal-ctags.rb
@@ -13,10 +13,12 @@ class UniversalCtags < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2037532a03d52b0328f18ac2bb4f1abd61093689a8dd940091ea2a4d4829d3b7"
-    sha256 cellar: :any,                 big_sur:       "e76b6f3ebdde0698fda219daa9180190e66e8f416688302a9ecbbb400aeabc84"
-    sha256 cellar: :any,                 catalina:      "a636f0c8b28a0782ecd98156e43f5975e574e27ac4fa5f89181c8d4d70eb5abb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ee7849598efe4d73a661c920450e8624bd887466d0303fb7c1fbbfbdced26b3"
+    sha256 cellar: :any,                 arm64_monterey: "27717e170d64bc0477e27387c8d09d601d908d5743d7309d55ce20f6e5b36883"
+    sha256 cellar: :any,                 arm64_big_sur:  "2037532a03d52b0328f18ac2bb4f1abd61093689a8dd940091ea2a4d4829d3b7"
+    sha256 cellar: :any,                 monterey:       "c2e3be2b2a7d1ae66c3e266bdcd8b379167000dd54f1fddd65171bbcc9430586"
+    sha256 cellar: :any,                 big_sur:        "e76b6f3ebdde0698fda219daa9180190e66e8f416688302a9ecbbb400aeabc84"
+    sha256 cellar: :any,                 catalina:       "a636f0c8b28a0782ecd98156e43f5975e574e27ac4fa5f89181c8d4d70eb5abb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ee7849598efe4d73a661c920450e8624bd887466d0303fb7c1fbbfbdced26b3"
   end
 
   depends_on "autoconf" => :build
