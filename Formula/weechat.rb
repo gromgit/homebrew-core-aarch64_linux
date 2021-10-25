9a55c710bac30c031417f9f3e61083697426874d
Bo Anderson <mail@boanderson.me>
2021-10-25 06:32:26 +0000

    weechat: update 3.3 bottle.

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -7,11 +7,13 @@ class Weechat < Formula
   head "https://github.com/weechat/weechat.git", branch: "master"
 
   bottle do
-    sha256 arm64_big_sur: "e4f9d439f3a83efdf7c0f88fcc7d638c1b4b8a1bff55e6c6215c174ac3cddf16"
-    sha256 big_sur:       "f176eda50d9ef8ab5858b35a1a572c0bd50fd531f912f33210afdebb3e200677"
-    sha256 catalina:      "ccfa0fff385966e6ff08de5e280f49defac48eafc0d649c3ebe983e0475631ff"
-    sha256 mojave:        "e68941b11b7b86d8603c809420ac6fba6761ad095bcf5c6087d0f560dd542114"
-    sha256 x86_64_linux:  "d7d367abe8fe2a22ac47b929332176d22215fa230f886d52be5f5f78e1c4c940"
+    sha256 arm64_monterey: "4a1e008cf76feb26be4037753389ddd5259768e40478dce3e43089cb576ff12e"
+    sha256 arm64_big_sur:  "e4f9d439f3a83efdf7c0f88fcc7d638c1b4b8a1bff55e6c6215c174ac3cddf16"
+    sha256 monterey:       "c8c53f4df53f45f666515d0e012b54c05625a8fd9e155bcf964131bd96298c12"
+    sha256 big_sur:        "f176eda50d9ef8ab5858b35a1a572c0bd50fd531f912f33210afdebb3e200677"
+    sha256 catalina:       "ccfa0fff385966e6ff08de5e280f49defac48eafc0d649c3ebe983e0475631ff"
+    sha256 mojave:         "e68941b11b7b86d8603c809420ac6fba6761ad095bcf5c6087d0f560dd542114"
+    sha256 x86_64_linux:   "d7d367abe8fe2a22ac47b929332176d22215fa230f886d52be5f5f78e1c4c940"
   end
 
   depends_on "asciidoctor" => :build
