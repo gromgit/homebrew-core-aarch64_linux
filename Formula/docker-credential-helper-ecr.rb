45725c8ec49a1d78e1e53ea099c70582224c92bb
Bo Anderson <mail@boanderson.me>
2021-10-25 06:23:33 +0000

    docker-credential-helper-ecr: update 0.5.0 bottle.

--- Formula/docker-credential-helper-ecr.rb
+++ Formula/docker-credential-helper-ecr.rb
@@ -12,11 +12,13 @@ class DockerCredentialHelperEcr < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0f76feceea1f591784fb2bb273950336675228c1d9e04d91af48d9bc6b700ddb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "822e7f36e21c109c0228fd53858781f872a4176a712b591a775c38769edc5189"
-    sha256 cellar: :any_skip_relocation, catalina:      "8506b1954205b0ccc6622738219606deb595da298797455442f5e7590886a1d8"
-    sha256 cellar: :any_skip_relocation, mojave:        "ade7af932bef1787cd560cb2befcd2ce9dcaa4ee694cac37dbf4a86265186667"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "643485656fa1b61484de16eb6ff48f0193a6eec6bd7e85b6c5d210e984a70085"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "19a3532dcc14111ee60f610c8bda076d4bd3662f519ef04673db3e0042b20f8f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0f76feceea1f591784fb2bb273950336675228c1d9e04d91af48d9bc6b700ddb"
+    sha256 cellar: :any_skip_relocation, monterey:       "3960a6b658b4ebd52ea4af9fc7704043029a673a0a6d97859def967b4dca536e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "822e7f36e21c109c0228fd53858781f872a4176a712b591a775c38769edc5189"
+    sha256 cellar: :any_skip_relocation, catalina:       "8506b1954205b0ccc6622738219606deb595da298797455442f5e7590886a1d8"
+    sha256 cellar: :any_skip_relocation, mojave:         "ade7af932bef1787cd560cb2befcd2ce9dcaa4ee694cac37dbf4a86265186667"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "643485656fa1b61484de16eb6ff48f0193a6eec6bd7e85b6c5d210e984a70085"
   end
 
   depends_on "go" => :build
