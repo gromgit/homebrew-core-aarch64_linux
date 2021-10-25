f165956049c84ca96a2dc7d7af0ff113b39f2457
Bo Anderson <mail@boanderson.me>
2021-10-25 15:58:55 +0000

    brew-pip: update 0.4.1 bottle.

--- Formula/brew-pip.rb
+++ Formula/brew-pip.rb
@@ -6,10 +6,12 @@ class BrewPip < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3231885b29ff349dfcee4f83a375f5d0ce4643750ea61d40a64a9ca0373ab71b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
-    sha256 cellar: :any_skip_relocation, catalina:      "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
-    sha256 cellar: :any_skip_relocation, mojave:        "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3231885b29ff349dfcee4f83a375f5d0ce4643750ea61d40a64a9ca0373ab71b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3231885b29ff349dfcee4f83a375f5d0ce4643750ea61d40a64a9ca0373ab71b"
+    sha256 cellar: :any_skip_relocation, monterey:       "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
+    sha256 cellar: :any_skip_relocation, catalina:       "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
+    sha256 cellar: :any_skip_relocation, mojave:         "d2cc472aef77f711ebb4890a1f669e9d7bf1668c5e024ef97b8e09fb8e6565aa"
   end
 
   def install
