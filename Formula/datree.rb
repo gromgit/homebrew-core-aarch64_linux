48bf147fa483d2d1178c9aec1f2ffca413feb43b
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-25 23:52:49 +0000

    datree: update 0.14.6 bottle.

--- Formula/datree.rb
+++ Formula/datree.rb
@@ -12,10 +12,12 @@ class Datree < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8411d0090ccb7688425ffe96c2c8f981938a442534cf94c93b570249aca5aaf6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c4734fd84c45e820389dbc52f632d117540b696b051ad835c16776d7de2db257"
-    sha256 cellar: :any_skip_relocation, catalina:      "2a4b463d7e08903822e772cb1af5d84841c4a8b34c75e62af3e9e8511f55cf1f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "768c1cc65dfc05adbd3b6a2400f53aa3e9cc72056f3c2348c24562a027e3f4ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "03d501914a90aafb1420a04d9d84dd69b27ec23f259e6cc8cf6c3eb04a6aa75d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8411d0090ccb7688425ffe96c2c8f981938a442534cf94c93b570249aca5aaf6"
+    sha256 cellar: :any_skip_relocation, monterey:       "67d41c5ce0a5cdabedd1526e8685326067c59229245011d56c75652181e620f1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c4734fd84c45e820389dbc52f632d117540b696b051ad835c16776d7de2db257"
+    sha256 cellar: :any_skip_relocation, catalina:       "2a4b463d7e08903822e772cb1af5d84841c4a8b34c75e62af3e9e8511f55cf1f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "768c1cc65dfc05adbd3b6a2400f53aa3e9cc72056f3c2348c24562a027e3f4ac"
   end
 
   depends_on "go" => :build
