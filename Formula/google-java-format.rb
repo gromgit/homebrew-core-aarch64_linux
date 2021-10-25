a322772d38aa4239d46958f9d3a49a57b6f34f9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-25 15:12:58 +0000

    google-java-format: update 1.12.0 bottle.

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -6,7 +6,10 @@ class GoogleJavaFormat < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3fc42b7dadb269a82002fe663bbac560e87c9e9500a92e8b1a3a9053582161cb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7dccb80af3364f807fd943e8315cd53c4eb6413744245885dd171d820937cd36"
+    sha256 cellar: :any_skip_relocation, big_sur:       "7dccb80af3364f807fd943e8315cd53c4eb6413744245885dd171d820937cd36"
+    sha256 cellar: :any_skip_relocation, catalina:      "7dccb80af3364f807fd943e8315cd53c4eb6413744245885dd171d820937cd36"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8e32c09781e5e93774ffeb6dd74e05512af27a609989db0bfff26a47eef279a"
   end
 
   depends_on "openjdk"
