aed58be4f2a9db5ec69ff4ca773c7bb212b99ab6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-25 15:13:33 +0000

    micronaut: update 3.1.1 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -11,11 +11,10 @@ class Micronaut < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "066e6d6eaf99070ff22d278d649db2e4bd7568e65e0f61dfc9ba12591626b086"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5a035be33a6a864a6f4ef8d06e62435b52c1968537f56aff9489f0bfd30d08ce"
-    sha256 cellar: :any_skip_relocation, catalina:      "c49b17c932385f0cc93d05176d37912cd74f1fcf16318047b1f8caa36a03a3e9"
-    sha256 cellar: :any_skip_relocation, mojave:        "0e772ebdec5f685f80dcb8dd47419f099d322a379f2fc8bd389c4ae1155bc6dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6aee4b194e4547e8ef8d39d6488005b5d432a66007fd0bb94dc2b91b07881a1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "46d9741e3c9c955dfb225a726ee4002b7ac234d6b095ac8a1b4f05aeeb44756f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "7a40d724c8dd2411faee8cf998d3d48dacb322fddbf9c6eaf33cb2267c69ceaa"
+    sha256 cellar: :any_skip_relocation, catalina:      "c6e9a842bbd54d3abbf259f0a23ab334f9bcf3c44870132f7b4a16e5d09a43fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5aa7a59769a9f2f4bd31bca32a73897a8f14d85fc4b68686431f46ef3c97ae52"
   end
 
   # Uses a hardcoded list of supported JDKs. Try switching to `openjdk` on update.
