bd98ab41fcc4a345e3b88b17c5f1cd6ec71365e4
Bo Anderson <mail@boanderson.me>
2021-10-25 03:38:23 +0000

    midnight-commander: update 4.8.27 bottle.

--- Formula/midnight-commander.rb
+++ Formula/midnight-commander.rb
@@ -12,11 +12,13 @@ class MidnightCommander < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "3f45e1b92e6f263924e4544f0c935b4d18c1e37fdfcf0b7f7d5be369e05910b9"
-    sha256 big_sur:       "31c1399b014432a36b0dcbb7b3834c50f7c5ac0809a8d1ae7bf8df8afbe838c1"
-    sha256 catalina:      "1b39f54060789701af81163180ae7dab3fffcee18cc07bc6255f3f712504a3a3"
-    sha256 mojave:        "dc2578f9825aa95824489fe52bfde70a130dadcd1c232c4fb07d538f1d9b19d1"
-    sha256 x86_64_linux:  "2c3ec573057f385886b1c20515a8788e3c0b0d9767829dd3a93c74b5973a5cb3"
+    sha256 arm64_monterey: "24f29d448f363b0c3cf793dbdf88fbf850b66a3fd550eb6dd01fd64f655f95e2"
+    sha256 arm64_big_sur:  "3f45e1b92e6f263924e4544f0c935b4d18c1e37fdfcf0b7f7d5be369e05910b9"
+    sha256 monterey:       "da16e0137f6702c18965c063cf6194e818dbb1a5e7f4db93ebc2d2606644469d"
+    sha256 big_sur:        "31c1399b014432a36b0dcbb7b3834c50f7c5ac0809a8d1ae7bf8df8afbe838c1"
+    sha256 catalina:       "1b39f54060789701af81163180ae7dab3fffcee18cc07bc6255f3f712504a3a3"
+    sha256 mojave:         "dc2578f9825aa95824489fe52bfde70a130dadcd1c232c4fb07d538f1d9b19d1"
+    sha256 x86_64_linux:   "2c3ec573057f385886b1c20515a8788e3c0b0d9767829dd3a93c74b5973a5cb3"
   end
 
   head do
