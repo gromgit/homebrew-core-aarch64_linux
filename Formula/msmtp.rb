f3d92b40401096642a1a67ba4c61974063126b59
Bo Anderson <mail@boanderson.me>
2021-10-25 18:43:31 +0000

    msmtp: update 1.8.18 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -11,10 +11,12 @@ class Msmtp < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "b808aa8faa33fd35f611594e650a9b5ec993aeb579d1bae81dc1d72bca9ffff3"
-    sha256 big_sur:       "6ee5aa8e7d52373b8249f9d1e34203beead0cc86163d96dac1854bc6648945ec"
-    sha256 catalina:      "28a120b2fda2c50a4efe60367b5fdd9a4e936f46b1adffb422499c3e610d17ac"
-    sha256 x86_64_linux:  "8337630534e09069a6ca50f4006f564058424f37ea3a4e4977c2a7b61dad184c"
+    sha256 arm64_monterey: "4bbe3d7e3d0517d3773a511854d0132793a83744f03add396641703b5a0b70d0"
+    sha256 arm64_big_sur:  "b808aa8faa33fd35f611594e650a9b5ec993aeb579d1bae81dc1d72bca9ffff3"
+    sha256 monterey:       "2fdd667a38138fa440d9aab3357676865fa9f15ba91e3cbe030eac754d100433"
+    sha256 big_sur:        "6ee5aa8e7d52373b8249f9d1e34203beead0cc86163d96dac1854bc6648945ec"
+    sha256 catalina:       "28a120b2fda2c50a4efe60367b5fdd9a4e936f46b1adffb422499c3e610d17ac"
+    sha256 x86_64_linux:   "8337630534e09069a6ca50f4006f564058424f37ea3a4e4977c2a7b61dad184c"
   end
 
   depends_on "pkg-config" => :build
