2298092505f658f79f101da640eae9e888d02431
Bo Anderson <mail@boanderson.me>
2021-10-25 05:21:16 +0000

    pnpm: update 6.19.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,10 +13,12 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "66cbd78fb3559f26e9ca247003905ac6286dc0f138a3ae01fc4dc3a94311b1ca"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b2f8119013eccc3c3a7ae734dc7c1fc14641bde57feb18721ac335baa2bf94e0"
-    sha256 cellar: :any_skip_relocation, catalina:      "b2f8119013eccc3c3a7ae734dc7c1fc14641bde57feb18721ac335baa2bf94e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66cbd78fb3559f26e9ca247003905ac6286dc0f138a3ae01fc4dc3a94311b1ca"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "66cbd78fb3559f26e9ca247003905ac6286dc0f138a3ae01fc4dc3a94311b1ca"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66cbd78fb3559f26e9ca247003905ac6286dc0f138a3ae01fc4dc3a94311b1ca"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b17349746ce11cbd5c52065bbc242dc1241a78e2521ca75bece9a13290f8d65"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b2f8119013eccc3c3a7ae734dc7c1fc14641bde57feb18721ac335baa2bf94e0"
+    sha256 cellar: :any_skip_relocation, catalina:       "b2f8119013eccc3c3a7ae734dc7c1fc14641bde57feb18721ac335baa2bf94e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "66cbd78fb3559f26e9ca247003905ac6286dc0f138a3ae01fc4dc3a94311b1ca"
   end
 
   depends_on "node"
