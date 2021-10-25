53eea986308e67d5510c3432f6a867368e5aa758
Rui Chen <rui@chenrui.dev>
2021-10-25 00:13:43 -0400

    pegtl 3.2.2
    * pegtl 3.2.2
    * pegtl: update license
    * update build
    
    Closes #87958.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,9 +1,9 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/3.2.1.tar.gz"
-  sha256 "af33818352bc0b73c66aa9bc037735ce43d471533f1078ee828ba7e09962847d"
-  license "MIT"
+  url "https://github.com/taocpp/PEGTL/archive/3.2.2.tar.gz"
+  sha256 "c6616275e78c618c016b79054eed0a0bdf4c1934f830d3ab33d3c3dac7320b03"
+  license "BSL-1.0"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "4751694ed5e233b46d8cf428145a506f488b9544e0f46b791ea2e2e660edae92"
@@ -11,12 +11,11 @@ class Pegtl < Formula
 
   depends_on "cmake" => :build
 
-  if MacOS.version <= :mojave
+  on_linux do
     depends_on "gcc"
-    fails_with :clang do
-      cause "'path' is unavailable in c++ < 17: introduced in macOS 10.15"
-    end
   end
+
+  fails_with gcc: "5"
 
   def install
     mkdir "build" do
