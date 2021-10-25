c2416b018f662ddfa1b850619c258e9e134add1d
Rui Chen <rui@chenrui.dev>
2021-10-25 00:13:13 -0400

    payara 5.2021.8
    * payara 5.2021.8
    * payara: depend on macOS
      The test fails on Linux.
    
    Closes #87957.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/payara.rb
+++ Formula/payara.rb
@@ -1,8 +1,8 @@
 class Payara < Formula
   desc "Java EE application server forked from GlassFish"
   homepage "https://www.payara.fish"
-  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/5.2021.7/payara-5.2021.7.zip"
-  sha256 "13db90d934f64da2adc8ef19bea68850e45879b7587f8dc7f30fb08f078c9f75"
+  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/5.2021.8/payara-5.2021.8.zip"
+  sha256 "64ebb1ac3507afd4fa6daa2c3735e964889faee816f10c8775ba09e8166c9ce1"
   license any_of: [
     "CDDL-1.1",
     { "GPL-2.0-only" => { with: "Classpath-exception-2.0" } },
@@ -17,6 +17,7 @@ class Payara < Formula
     sha256 cellar: :any_skip_relocation, all: "5600ce69eea35a21a58aa4a713c66b74c2f1d700dbed24b95dbe518d2a478db3"
   end
 
+  depends_on :macos # The test fails on Linux.
   depends_on "openjdk@11"
 
   conflicts_with "glassfish", because: "both install the same scripts"
