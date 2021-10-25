036769584fc774e6640d8ff2f5ff7d03c8476a1c
Bo Anderson <mail@boanderson.me>
2021-10-25 16:28:23 +0000

    nlopt: update 2.7.0 bottle.

--- Formula/nlopt.rb
+++ Formula/nlopt.rb
@@ -7,11 +7,13 @@ class Nlopt < Formula
   head "https://github.com/stevengj/nlopt.git", branch: "master"
 
   bottle do
-    sha256                               arm64_big_sur: "ff00418c72bb7cd562a176f90eeb9a4a4a4a98ea070e10c5ff83515f57a55e58"
-    sha256                               big_sur:       "255f283f65d458191cfa27b68a827074eaa216f7e7b8159d56921575b0cad1d9"
-    sha256                               catalina:      "3bd9fb4ba224bb9167c0412ae4ebc77de6a050427db58fa61c1f9ed2cff0cccd"
-    sha256                               mojave:        "03a4858507f225ff2df60358206618ce4cd0cb45a047dbe382b3a1429a6b9bb0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c16fa7988b0549abf8385ff9ab05f13436d5676c08dd4b0b5011285aaf22baca"
+    sha256 cellar: :any,                 arm64_monterey: "6e26a8d3eb8f7f92de071ebbeb3a4f646363dc84c5005deb7a495789d7c7d6f1"
+    sha256                               arm64_big_sur:  "ff00418c72bb7cd562a176f90eeb9a4a4a4a98ea070e10c5ff83515f57a55e58"
+    sha256 cellar: :any,                 monterey:       "c4f633e590b44416e0b3b3a270e786cb01a9714899651938796ee358b1cc083a"
+    sha256                               big_sur:        "255f283f65d458191cfa27b68a827074eaa216f7e7b8159d56921575b0cad1d9"
+    sha256                               catalina:       "3bd9fb4ba224bb9167c0412ae4ebc77de6a050427db58fa61c1f9ed2cff0cccd"
+    sha256                               mojave:         "03a4858507f225ff2df60358206618ce4cd0cb45a047dbe382b3a1429a6b9bb0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c16fa7988b0549abf8385ff9ab05f13436d5676c08dd4b0b5011285aaf22baca"
   end
 
   depends_on "cmake" => [:build, :test]
