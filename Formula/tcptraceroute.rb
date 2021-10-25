b4a81b814e5a4f998ee3c565b2feeb6231cfbe7a
Bo Anderson <mail@boanderson.me>
2021-10-25 18:13:10 +0000

    tcptraceroute: update 1.5beta7_2 bottle.

--- Formula/tcptraceroute.rb
+++ Formula/tcptraceroute.rb
@@ -26,11 +26,13 @@ class Tcptraceroute < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "4e2b9c6ebec4fbbe3918044164a2ea7bfff0970e06e0c720bef8143d322ab3e2"
-    sha256 cellar: :any,                 big_sur:       "f0e063340080998a098d428af420778bf27b0d5b772943b482152ad9e2793db2"
-    sha256 cellar: :any,                 catalina:      "32a7e7e680f6e481353c0ab25fbfebb1f79f48bce4d2215d4765211e3494d450"
-    sha256 cellar: :any,                 mojave:        "99c51ddf23c5a4c9ac6d853c39a03513b340e60aa2d57211a46ea58bbad7290d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97750459321657901904cd492047c4d011d7e7b705d01ce37d82fe5622dec168"
+    sha256 cellar: :any,                 arm64_monterey: "ba3030414b8fd5d2a556652a1bf5d7be188dddb8ad6e221e7d6662e295a00102"
+    sha256 cellar: :any,                 arm64_big_sur:  "4e2b9c6ebec4fbbe3918044164a2ea7bfff0970e06e0c720bef8143d322ab3e2"
+    sha256 cellar: :any,                 monterey:       "b9308ddeb907678411916f047d9119350c5054afd5e995e113cf5ad287156e9c"
+    sha256 cellar: :any,                 big_sur:        "f0e063340080998a098d428af420778bf27b0d5b772943b482152ad9e2793db2"
+    sha256 cellar: :any,                 catalina:       "32a7e7e680f6e481353c0ab25fbfebb1f79f48bce4d2215d4765211e3494d450"
+    sha256 cellar: :any,                 mojave:         "99c51ddf23c5a4c9ac6d853c39a03513b340e60aa2d57211a46ea58bbad7290d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97750459321657901904cd492047c4d011d7e7b705d01ce37d82fe5622dec168"
   end
 
   depends_on "autoconf" => :build
