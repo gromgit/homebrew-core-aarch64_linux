e911188a236510a51ac52ce3cece663ff1ae2d4d
Bo Anderson <mail@boanderson.me>
2021-10-25 03:43:31 +0000

    libserdes: update 7.0.0 bottle.

--- Formula/libserdes.rb
+++ Formula/libserdes.rb
@@ -8,11 +8,13 @@ class Libserdes < Formula
   head "https://github.com/confluentinc/libserdes.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ae93f71843a6b09dee0ec5921e12ecea336f1dab672509c3780868edd674f2b4"
-    sha256 cellar: :any,                 big_sur:       "8dc8f1c91e214a86d2d2b9b3c9e093f11f8410327f0834581be528cb31ef0e2f"
-    sha256 cellar: :any,                 catalina:      "6e787eee3da70740262d668ba474c849fb060d807f5896fb5fa741f87757c379"
-    sha256 cellar: :any,                 mojave:        "bafb84c4afe99d9852283bbc86370294aa973569eb4c85f600f0c8993428de56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3129deb96b23fc8518456f9e1673b36f1cf74e7b55e1f5f7f72fe840573c8331"
+    sha256 cellar: :any,                 arm64_monterey: "7c15431602aecdd6cb2e108d7d84e4f7468a17235cdf0289dd619a3cf951251c"
+    sha256 cellar: :any,                 arm64_big_sur:  "ae93f71843a6b09dee0ec5921e12ecea336f1dab672509c3780868edd674f2b4"
+    sha256 cellar: :any,                 monterey:       "49d5c408b5f11670190146a8aa78db5499d5c84d52ced60df72b21437ce39ffa"
+    sha256 cellar: :any,                 big_sur:        "8dc8f1c91e214a86d2d2b9b3c9e093f11f8410327f0834581be528cb31ef0e2f"
+    sha256 cellar: :any,                 catalina:       "6e787eee3da70740262d668ba474c849fb060d807f5896fb5fa741f87757c379"
+    sha256 cellar: :any,                 mojave:         "bafb84c4afe99d9852283bbc86370294aa973569eb4c85f600f0c8993428de56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3129deb96b23fc8518456f9e1673b36f1cf74e7b55e1f5f7f72fe840573c8331"
   end
 
   depends_on "avro-c"
