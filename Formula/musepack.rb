27331d4aa5732fedb02ec310b4d6c6953aa2f326
Bo Anderson <mail@boanderson.me>
2021-10-25 15:09:41 +0000

    musepack: update r475 bottle.

--- Formula/musepack.rb
+++ Formula/musepack.rb
@@ -12,11 +12,13 @@ class Musepack < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "ce4329e0fc5d5d1a1e518e7c1f471ef300ff96ddaea0219e70e4408924f75ff6"
-    sha256 cellar: :any, big_sur:       "e9221e9612e1c0134554fbf380639cd995a5970ad34702bea371633f121ffb3e"
-    sha256 cellar: :any, catalina:      "847cacb946b6289a5fbfbfe4e1a38f1ec5b7f1e32d6c12145aaf1044317e4ce0"
-    sha256 cellar: :any, mojave:        "5efee306aff13a0c0b8f98371e3cbe3eab6b73b0e92bdd59237d7db608a17708"
-    sha256 cellar: :any, high_sierra:   "e1b6641d11a5338d395de8f5573464beddb81dc3dce16998e53361b43502844b"
+    sha256 cellar: :any, arm64_monterey: "76db5599c7e47ab4317292c040ef16095f658ee3b524d89c0296a574380a4570"
+    sha256 cellar: :any, arm64_big_sur:  "ce4329e0fc5d5d1a1e518e7c1f471ef300ff96ddaea0219e70e4408924f75ff6"
+    sha256 cellar: :any, monterey:       "9f74dcc9567656a42082125ce4d758c3318a452dbd940b6db39b72c4981768da"
+    sha256 cellar: :any, big_sur:        "e9221e9612e1c0134554fbf380639cd995a5970ad34702bea371633f121ffb3e"
+    sha256 cellar: :any, catalina:       "847cacb946b6289a5fbfbfe4e1a38f1ec5b7f1e32d6c12145aaf1044317e4ce0"
+    sha256 cellar: :any, mojave:         "5efee306aff13a0c0b8f98371e3cbe3eab6b73b0e92bdd59237d7db608a17708"
+    sha256 cellar: :any, high_sierra:    "e1b6641d11a5338d395de8f5573464beddb81dc3dce16998e53361b43502844b"
   end
 
   depends_on "cmake" => :build
