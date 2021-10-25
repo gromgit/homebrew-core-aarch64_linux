51af24aec7979359e49098c3d2901b5b034de464
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-25 07:15:24 +0000

    sdl2_mixer: update 2.0.4_2 bottle.

--- Formula/sdl2_mixer.rb
+++ Formula/sdl2_mixer.rb
@@ -12,11 +12,13 @@ class Sdl2Mixer < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "f3c822ba6f80353d0fee40ea106025d4519e1dbb141d097fef092393e95fb0b7"
-    sha256 cellar: :any,                 big_sur:       "1529a00916c4066d8adc0987b627e2bc7cf66aca063562cb3af64f8fa5f231f7"
-    sha256 cellar: :any,                 catalina:      "9779416544a0d71a8206b45895a3060baca2bf0877441017aaa6b1d6136654a2"
-    sha256 cellar: :any,                 mojave:        "9c13dd597aca2e0d5f53f2a7b4a1ea4e5a724c08796ba0eaf71a54f9cc714fbc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "412de036ef39cab5f7e1907711c2273a3d671e995ddeda320cad9cfedaa72a28"
+    sha256 cellar: :any,                 arm64_monterey: "375fbfb9b64fdfc631a7446aa1e21c74f3fc1b5a2e732cbe9db711078fe9c805"
+    sha256 cellar: :any,                 arm64_big_sur:  "f3c822ba6f80353d0fee40ea106025d4519e1dbb141d097fef092393e95fb0b7"
+    sha256 cellar: :any,                 monterey:       "4784c901a5fe2da26d58b06d1c888cb861874cb16d1211705066dcd7a61027ab"
+    sha256 cellar: :any,                 big_sur:        "1529a00916c4066d8adc0987b627e2bc7cf66aca063562cb3af64f8fa5f231f7"
+    sha256 cellar: :any,                 catalina:       "9779416544a0d71a8206b45895a3060baca2bf0877441017aaa6b1d6136654a2"
+    sha256 cellar: :any,                 mojave:         "9c13dd597aca2e0d5f53f2a7b4a1ea4e5a724c08796ba0eaf71a54f9cc714fbc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "412de036ef39cab5f7e1907711c2273a3d671e995ddeda320cad9cfedaa72a28"
   end
 
   head do
