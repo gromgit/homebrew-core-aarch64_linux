b9d6aa7dad14326007365aa8e1e740aeaf016f3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-25 11:09:03 +0000

    n: update 8.0.0 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -7,13 +7,10 @@ class N < Formula
   head "https://github.com/tj/n.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "92c3adc465225cedba159c9c7218cf441ef47ba373bea23c2d4aba60426e51f8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "92c3adc465225cedba159c9c7218cf441ef47ba373bea23c2d4aba60426e51f8"
-    sha256 cellar: :any_skip_relocation, monterey:       "b981f163cebf4eb714b40491fa218d46cb824cfd50fe5fdffd091a38bdf32977"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b981f163cebf4eb714b40491fa218d46cb824cfd50fe5fdffd091a38bdf32977"
-    sha256 cellar: :any_skip_relocation, catalina:       "b981f163cebf4eb714b40491fa218d46cb824cfd50fe5fdffd091a38bdf32977"
-    sha256 cellar: :any_skip_relocation, mojave:         "b981f163cebf4eb714b40491fa218d46cb824cfd50fe5fdffd091a38bdf32977"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "92c3adc465225cedba159c9c7218cf441ef47ba373bea23c2d4aba60426e51f8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9fa15a18556ed914c2a8ff7505ea084e4685e8da5d166c96a98e3297ddefc000"
+    sha256 cellar: :any_skip_relocation, big_sur:       "43dbc0e42d23a9c30454f177dbe2ee6b65db1d967b7b0a8c15552087bf2f8dd3"
+    sha256 cellar: :any_skip_relocation, catalina:      "43dbc0e42d23a9c30454f177dbe2ee6b65db1d967b7b0a8c15552087bf2f8dd3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9fa15a18556ed914c2a8ff7505ea084e4685e8da5d166c96a98e3297ddefc000"
   end
 
   def install
