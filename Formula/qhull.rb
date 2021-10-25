ef1f19a46a24778c9e053c55d1118afea5e64839
Bo Anderson <mail@boanderson.me>
2021-10-25 03:55:02 +0000

    qhull: update 2020.2 bottle.

--- Formula/qhull.rb
+++ Formula/qhull.rb
@@ -15,11 +15,13 @@ class Qhull < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d54263b22f2c4effc10ab2dbab54ec0b7f2592d07cdad43c20ddfffff149aad0"
-    sha256 cellar: :any,                 big_sur:       "1c0b6ed4613b8319859b7c0c15b174bb1e89178c79e060ccc400220beb079d46"
-    sha256 cellar: :any,                 catalina:      "b48c342482e1e50857c444f8eb39f71c36a522a9f0692bd479b93d2088672d2f"
-    sha256 cellar: :any,                 mojave:        "6bec66662d9b4d1942a959505442790cfafd482660a2c8785a45175714fe1ae6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97152aca48ede908990b7d3935bf3305b559833074bb4c92fc6fdab68f95fd23"
+    sha256 cellar: :any,                 arm64_monterey: "149d647d68ab1386f21996abde29a0c158cb11d740318c8c61112e77e3419170"
+    sha256 cellar: :any,                 arm64_big_sur:  "d54263b22f2c4effc10ab2dbab54ec0b7f2592d07cdad43c20ddfffff149aad0"
+    sha256 cellar: :any,                 monterey:       "91c5c29003c0f86c85b4e597b2aae623012517c8cf1696686a6d3f97b0c507f3"
+    sha256 cellar: :any,                 big_sur:        "1c0b6ed4613b8319859b7c0c15b174bb1e89178c79e060ccc400220beb079d46"
+    sha256 cellar: :any,                 catalina:       "b48c342482e1e50857c444f8eb39f71c36a522a9f0692bd479b93d2088672d2f"
+    sha256 cellar: :any,                 mojave:         "6bec66662d9b4d1942a959505442790cfafd482660a2c8785a45175714fe1ae6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97152aca48ede908990b7d3935bf3305b559833074bb4c92fc6fdab68f95fd23"
   end
 
   depends_on "cmake" => :build
