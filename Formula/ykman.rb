c82f1f25e16d3d834a8f4aac4c1b232c7d43027d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-25 16:13:20 +0000

    ykman: update 4.0.7_1 bottle.

--- Formula/ykman.rb
+++ Formula/ykman.rb
@@ -10,11 +10,10 @@ class Ykman < Formula
   head "https://github.com/Yubico/yubikey-manager.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "bf1c6a2abc542c17e855310faa3b7556af3a00b2102e1e4441775698e2334c12"
-    sha256 cellar: :any,                 big_sur:       "3951c41b15282af752b2b2cdb13fd3553b39c4e690ebfc52b5b4c0e5ff727db8"
-    sha256 cellar: :any,                 catalina:      "cb1d3418303dbdf5e4c652bf452c3c940eca9cf894f46438875ccf8afb9fe2b9"
-    sha256 cellar: :any,                 mojave:        "d979a0fd0c625fcd30b82de7979cf11b96c41c3cc94e7183e527006c3dd081be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57eb7a870aa161ce802c05e2eed6d2dee208556abde5685e96febb38cebc63d2"
+    sha256 cellar: :any,                 arm64_big_sur: "fd85250ee051dd400d046ff2d950cc08fa13e62e4ed510561130f109e67a381f"
+    sha256 cellar: :any,                 big_sur:       "b9ae9a5530e4ae78cfd3ed7312f0328b3f3e9e37373e410160612fa41bffca07"
+    sha256 cellar: :any,                 catalina:      "8ca23d5defacb45cddc59075950aa75f7e25127d69e297001e6fe6c3e0411609"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9bc3d373b749caa00e6f79f141f0b87cd335b8190579b6736ca811d2dda6b744"
   end
 
   depends_on "rust" => :build
