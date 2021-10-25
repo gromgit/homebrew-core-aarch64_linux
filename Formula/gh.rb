359f9f161710362efdf3e9191e052ac40e8dad9e
Bo Anderson <mail@boanderson.me>
2021-10-25 22:18:31 +0000

    gh: update 2.2.0 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -13,10 +13,12 @@ class Gh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f4bb54f147bb3f15abed9296141b9566f8215e11a02e147bd201003cec0a8055"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fbb6999034c88aa685f191cf3bef89f64f7d12ab8a30210d0f784115f4da63cf"
-    sha256 cellar: :any_skip_relocation, catalina:      "20c50a415ebe0678bc8b51738282a5bc86fb6df4927462466b03b2e8c256fb94"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "94e86f5db6d884e1f88741245c60587a4503c4326b0163ce2eefac0ffb4f80d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f21b45d9ecc03948053f1d0e50114a13f4439e669ac72358d419888a4feacecc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4bb54f147bb3f15abed9296141b9566f8215e11a02e147bd201003cec0a8055"
+    sha256 cellar: :any_skip_relocation, monterey:       "c545de63ab831cf9e27e4118143e88249c324856f30d80c2b6b85d636145a79b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fbb6999034c88aa685f191cf3bef89f64f7d12ab8a30210d0f784115f4da63cf"
+    sha256 cellar: :any_skip_relocation, catalina:       "20c50a415ebe0678bc8b51738282a5bc86fb6df4927462466b03b2e8c256fb94"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94e86f5db6d884e1f88741245c60587a4503c4326b0163ce2eefac0ffb4f80d6"
   end
 
   depends_on "go" => :build
