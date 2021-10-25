e9ad465a6127999a5d74c360a15c64bcb0e4d77a
Bo Anderson <mail@boanderson.me>
2021-10-25 21:37:04 +0000

    gitlab-runner: update 14.4.0 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -13,10 +13,12 @@ class GitlabRunner < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1313bb413961d78b9fde57a3a14dc8642dee3f7d3ddf4fefb354dc95fe49ea76"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d20050b6390ae734ba30c7dce568323ea9714b9b0c9eff7154268a1028a73aa7"
-    sha256 cellar: :any_skip_relocation, catalina:      "e5302f85731f53752c309fcc8f1157ea58e7aac8453fcd4a331a9e35e5a18b4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a565ac79ccb691a2af326e15c3d10ae92c4355cbcfa8687b1b88a176dcf815f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff7fcb67c66534a36b32d043c573005421ebc26fe87fe09937cdc9164708b900"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1313bb413961d78b9fde57a3a14dc8642dee3f7d3ddf4fefb354dc95fe49ea76"
+    sha256 cellar: :any_skip_relocation, monterey:       "4f997780c8add77429da13d9bf3cbdfb51f44c042f8e27cbbf3a4a88b3f684fc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d20050b6390ae734ba30c7dce568323ea9714b9b0c9eff7154268a1028a73aa7"
+    sha256 cellar: :any_skip_relocation, catalina:       "e5302f85731f53752c309fcc8f1157ea58e7aac8453fcd4a331a9e35e5a18b4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a565ac79ccb691a2af326e15c3d10ae92c4355cbcfa8687b1b88a176dcf815f"
   end
 
   depends_on "go" => :build
