5b248ffa739274bad79503f8b533320db29005ca
Bo Anderson <mail@boanderson.me>
2021-10-25 06:00:57 +0000

    lnav: update 0.10.0_1 bottle.

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -12,11 +12,13 @@ class Lnav < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "689f5a6514b114b7bc08e41b92dd146cf37db4d8e129bd08ad3002c93d739402"
-    sha256 cellar: :any,                 big_sur:       "a3b4186894e82c09c06c49f1e86f43fa829c6aebda2abd125a2c90e4202b02b6"
-    sha256 cellar: :any,                 catalina:      "87b9c1b85678d964076f0d2a622330a121b15058751de52d4d3802118983165f"
-    sha256 cellar: :any,                 mojave:        "f40a8164360104c3078d17b78ab5ad109437fbb84e7db487a68ad4ed3ac8391f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f5d6c89163a52768fd9ac42d9c3a45737cc87b7a0beb80cc1e4193ad95c0158"
+    sha256 cellar: :any,                 arm64_monterey: "601fe3baef067fde3edab704f4277ceea2d23a2a385e8b32e0e71b903c89db3a"
+    sha256 cellar: :any,                 arm64_big_sur:  "689f5a6514b114b7bc08e41b92dd146cf37db4d8e129bd08ad3002c93d739402"
+    sha256 cellar: :any,                 monterey:       "c16dcf1756ca33128ddb5d5300e0cea1eae6664c12901ef7ad42f5a2969b472e"
+    sha256 cellar: :any,                 big_sur:        "a3b4186894e82c09c06c49f1e86f43fa829c6aebda2abd125a2c90e4202b02b6"
+    sha256 cellar: :any,                 catalina:       "87b9c1b85678d964076f0d2a622330a121b15058751de52d4d3802118983165f"
+    sha256 cellar: :any,                 mojave:         "f40a8164360104c3078d17b78ab5ad109437fbb84e7db487a68ad4ed3ac8391f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f5d6c89163a52768fd9ac42d9c3a45737cc87b7a0beb80cc1e4193ad95c0158"
   end
 
   head do
