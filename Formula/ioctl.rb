9b191a25e6723b4608da3f32b360b9722cc85b6b
rui <rui@chenrui.dev>
2021-10-25 17:22:10 +0000

    ioctl: update 1.5.1 bottle.

--- Formula/ioctl.rb
+++ Formula/ioctl.rb
@@ -6,9 +6,11 @@ class Ioctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "90d4cf8be3e079a14eb2d52828d3f1aec5ff1829459a88853129baff7f37eef5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "453336d2d6c20f8d74743482891bd3acb6e171ba33bcd7037f4ffd6c1e4f7678"
-    sha256 cellar: :any_skip_relocation, catalina:      "a90584206c438b93886642314f27731f25a1ec338c7b6ca944314c5b3ffb24c9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "729a69f059587797efde932833f358d3dfd6fa9df019d5c2d701cf0c51fbda27"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "90d4cf8be3e079a14eb2d52828d3f1aec5ff1829459a88853129baff7f37eef5"
+    sha256 cellar: :any_skip_relocation, monterey:       "cd44935fdea7f0747648e2363a8799ec94d0ccb8ab4674bf544cf906af8518e3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "453336d2d6c20f8d74743482891bd3acb6e171ba33bcd7037f4ffd6c1e4f7678"
+    sha256 cellar: :any_skip_relocation, catalina:       "a90584206c438b93886642314f27731f25a1ec338c7b6ca944314c5b3ffb24c9"
   end
 
   depends_on "go" => :build
