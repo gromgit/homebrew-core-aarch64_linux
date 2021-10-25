2d1c3bd2b75e9d9bcd7f6d167c6996bb5f589923
Bo Anderson <mail@boanderson.me>
2021-10-25 07:02:56 +0000

    libzzip: update 0.13.72_1 bottle.

--- Formula/libzzip.rb
+++ Formula/libzzip.rb
@@ -8,11 +8,13 @@ class Libzzip < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "43fbba2b7e506170bf0f03a8c281c142b04cd1b95365392d36dcc014e5f24743"
-    sha256 cellar: :any,                 big_sur:       "f4471c0801590824b9fa2de9a5f25c14fc42dc8d87a5efcdf16144a116d5b997"
-    sha256 cellar: :any,                 catalina:      "0d0827679b5108d79b6bcbf8a3f1ede078d547bb1986d4b7808d6cdb77104023"
-    sha256 cellar: :any,                 mojave:        "f165f79a37ac61eeb25c2f9b4756848f4c3a9ddcb7250b9de0e6cc5640b00598"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8360520c0127acc397b30f0aac2213f717d37c023a2aa9a96d265a0d7b77d4ad"
+    sha256 cellar: :any,                 arm64_monterey: "460bbbc8e5d56c82fd67e731e7abba29244400566292bbeaed1d67e4bb2a092e"
+    sha256 cellar: :any,                 arm64_big_sur:  "43fbba2b7e506170bf0f03a8c281c142b04cd1b95365392d36dcc014e5f24743"
+    sha256 cellar: :any,                 monterey:       "6866b7cf2364fada4775bb915c9ecd761ea6d909a9bc744e6b93a81796f6957f"
+    sha256 cellar: :any,                 big_sur:        "f4471c0801590824b9fa2de9a5f25c14fc42dc8d87a5efcdf16144a116d5b997"
+    sha256 cellar: :any,                 catalina:       "0d0827679b5108d79b6bcbf8a3f1ede078d547bb1986d4b7808d6cdb77104023"
+    sha256 cellar: :any,                 mojave:         "f165f79a37ac61eeb25c2f9b4756848f4c3a9ddcb7250b9de0e6cc5640b00598"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8360520c0127acc397b30f0aac2213f717d37c023a2aa9a96d265a0d7b77d4ad"
   end
 
   depends_on "cmake" => :build
