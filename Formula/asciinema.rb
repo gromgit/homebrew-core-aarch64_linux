e1b7c9eb2ae1428e3f2831db44ac8706c5e692bf
Bo Anderson <mail@boanderson.me>
2021-10-25 04:08:00 +0000

    asciinema: update 2.1.0 bottle.

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -9,11 +9,13 @@ class Asciinema < Formula
   head "https://github.com/asciinema/asciinema.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1fea4ae9e201966f38b7b1d5a5edd46f047b8ab80ca382e5a4d218081ae5c8d5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, catalina:      "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, mojave:        "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "388d5d894e1150768c822e978ec7c86cd62a1e730d98eb5055823dbbd728d7a8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1fea4ae9e201966f38b7b1d5a5edd46f047b8ab80ca382e5a4d218081ae5c8d5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1fea4ae9e201966f38b7b1d5a5edd46f047b8ab80ca382e5a4d218081ae5c8d5"
+    sha256 cellar: :any_skip_relocation, monterey:       "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
+    sha256 cellar: :any_skip_relocation, catalina:       "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
+    sha256 cellar: :any_skip_relocation, mojave:         "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "388d5d894e1150768c822e978ec7c86cd62a1e730d98eb5055823dbbd728d7a8"
   end
 
   depends_on "python@3.10"
