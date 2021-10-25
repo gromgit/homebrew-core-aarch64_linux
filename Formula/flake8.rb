503f0ee70192ee5b84e1d644cc1ed5f17732080b
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-25 00:45:08 +0000

    flake8: update 4.0.1 bottle.

--- Formula/flake8.rb
+++ Formula/flake8.rb
@@ -9,12 +9,13 @@ class Flake8 < Formula
   head "https://gitlab.com/PyCQA/flake8.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0df982d4a3ed80b694876262243dc7356b3f21bec95e016843964588ea268ef2"
-    sha256 cellar: :any_skip_relocation, monterey:      "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
-    sha256 cellar: :any_skip_relocation, big_sur:       "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
-    sha256 cellar: :any_skip_relocation, catalina:      "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
-    sha256 cellar: :any_skip_relocation, mojave:        "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e1fd8816e95438fa558a91432cdc848760649a020dcc067eb801ee8123e88ff"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0df982d4a3ed80b694876262243dc7356b3f21bec95e016843964588ea268ef2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0df982d4a3ed80b694876262243dc7356b3f21bec95e016843964588ea268ef2"
+    sha256 cellar: :any_skip_relocation, monterey:       "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
+    sha256 cellar: :any_skip_relocation, big_sur:        "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
+    sha256 cellar: :any_skip_relocation, catalina:       "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
+    sha256 cellar: :any_skip_relocation, mojave:         "268cb65b355888176c6bbfdfb74547f79ad8faa8b207483bc03766bc33776401"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e1fd8816e95438fa558a91432cdc848760649a020dcc067eb801ee8123e88ff"
   end
 
   depends_on "python@3.10"
