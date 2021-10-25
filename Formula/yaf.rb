3479cf75ca6a47ef26022290aaed01f512deefe2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-25 13:25:34 +0000

    yaf: update 2.12.2 bottle.

--- Formula/yaf.rb
+++ Formula/yaf.rb
@@ -11,11 +11,10 @@ class Yaf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "76f4110e69d43358fe777fb8592a1b70a7d559f5907f7e8a49498ceffaefb58a"
-    sha256 cellar: :any,                 big_sur:       "c1b74fbe1059282892a5b6ffaa20ab78a2a826e012dd9660ca3d47620cf5c5c4"
-    sha256 cellar: :any,                 catalina:      "916e896ce18eac857a04e804dd1e71b5db9d35bb140c07dbf6da3db392022ca9"
-    sha256 cellar: :any,                 mojave:        "a3cc7c4da5056644725dfe0316140886199c6151bf868e62765ba4c2fe07d0e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ce2ba016231be6eaf8916076815ac8c0d5b3cc9c75eb2d2b06580c8153f8cd2e"
+    sha256 cellar: :any,                 arm64_big_sur: "554d7265d1648b6aeb930e7d70ea7a9c8e67bf259049bc17c8a2b4c493534ef1"
+    sha256 cellar: :any,                 big_sur:       "e9a598eb315228ca4e968cbf074f2fb819311e4666559acc382c784f87720faf"
+    sha256 cellar: :any,                 catalina:      "26ce51e3201562138d3ec6f04ccaca48799766196b04e390ac2989552b507c64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a28bfd8a9a02046495ff8ac90f3487de50adf772672d183e5670bd3f5cb4d6b4"
   end
 
   depends_on "pkg-config" => :build
