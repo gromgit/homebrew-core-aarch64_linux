52b6055d0e965c302a722ad835baefe1d7a090a3
Bo Anderson <mail@boanderson.me>
2021-10-25 06:51:32 +0000

    console_bridge: update 1.0.1 bottle.

--- Formula/console_bridge.rb
+++ Formula/console_bridge.rb
@@ -6,12 +6,14 @@ class ConsoleBridge < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2de26293b962e5fd45cf7d33e3d867ec5f3e44248736594f668993cab66cd642"
-    sha256 cellar: :any,                 big_sur:       "c9f53becfc647527e7044c1419bf992936c825920bd27637b8d4cbe07a675f9c"
-    sha256 cellar: :any,                 catalina:      "0b499b94d9a1f14aebdbcdbfb21d95ce06a9c2a9160ecf317541bef7c76a1324"
-    sha256 cellar: :any,                 mojave:        "a8fe261af6240d0a5128c4a9d214457bc0f0d20b198172c8a84e95d9ffa3fbd9"
-    sha256 cellar: :any,                 high_sierra:   "0be0e4bb4c6a14c7f8725dc44443b5772e4b4a5b9da1df521cfb4d18d88c9437"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46bbb8159d744a618988624b1eb93b012852230b8347cafabb2f2c14c4ad8caa"
+    sha256 cellar: :any,                 arm64_monterey: "def9d47faffa3ced59e086013fd3677c765eddccecae200df4cd8b44911aeda0"
+    sha256 cellar: :any,                 arm64_big_sur:  "2de26293b962e5fd45cf7d33e3d867ec5f3e44248736594f668993cab66cd642"
+    sha256 cellar: :any,                 monterey:       "a585e971c0393f5460f204f03f31e6fef9f4068cd075503a402c8e6c7f513399"
+    sha256 cellar: :any,                 big_sur:        "c9f53becfc647527e7044c1419bf992936c825920bd27637b8d4cbe07a675f9c"
+    sha256 cellar: :any,                 catalina:       "0b499b94d9a1f14aebdbcdbfb21d95ce06a9c2a9160ecf317541bef7c76a1324"
+    sha256 cellar: :any,                 mojave:         "a8fe261af6240d0a5128c4a9d214457bc0f0d20b198172c8a84e95d9ffa3fbd9"
+    sha256 cellar: :any,                 high_sierra:    "0be0e4bb4c6a14c7f8725dc44443b5772e4b4a5b9da1df521cfb4d18d88c9437"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46bbb8159d744a618988624b1eb93b012852230b8347cafabb2f2c14c4ad8caa"
   end
 
   depends_on "cmake" => :build
