82884ea1950795f15c5ea19b16c9e96565f365a3
jeffreytse <jeffreytse.mail@gmail.com>
2021-10-25 11:19:06 +0800

    zsh-vi-mode 0.8.5
    * feat(zsh-vi-mode): update url to v0.8.5
    * zsh-vi-mode: declare a zsh dependency on linux
    * zsh-vi-mode: cleanup whitespace
    
    Closes #87933.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zsh-vi-mode.rb
+++ Formula/zsh-vi-mode.rb
@@ -1,14 +1,16 @@
 class ZshViMode < Formula
   desc "Better and friendly vi(vim) mode plugin for ZSH"
   homepage "https://github.com/jeffreytse/zsh-vi-mode"
-  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.8.4.tar.gz"
-  sha256 "d16814693099dcc6f47afe3e5575af539a7aee1071f3f532c233800918722398"
+  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.8.5.tar.gz"
+  sha256 "98ae59b83ee1886929d5c2af5e5b8a2512828cc312815bdfb34db74f2dc0476a"
   license "MIT"
   head "https://github.com/jeffreytse/zsh-vi-mode.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "75cb847e1c112353962727000722ad1bb21184ab954a9a6931caeb8cd26401c4"
   end
+
+  uses_from_macos "zsh"
 
   def install
     pkgshare.install "zsh-vi-mode.zsh"
