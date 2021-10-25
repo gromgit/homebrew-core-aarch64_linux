ff3eee92476fca13137e78f2fa52c41ea9e07033
Rui Chen <rui@chenrui.dev>
2021-10-24 23:49:58 -0400

    libcap 2.60
    Closes #87940.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libcap.rb
+++ Formula/libcap.rb
@@ -1,8 +1,8 @@
 class Libcap < Formula
   desc "User-space interfaces to POSIX 1003.1e capabilities"
   homepage "https://sites.google.com/site/fullycapable/"
-  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.59.tar.xz"
-  sha256 "49f64ae40b113e53cbc161e22af7094e3bb4a0611fa33ef5a4257b011779f034"
+  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.60.tar.xz"
+  sha256 "06a92076ce39a78bd28089e32085f1bde7f3bfa448fad37d895c2358f760b2eb"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
 
   livecheck do
