8162423285f53c880e7c0b859a45be22e43e471b
Bo Anderson <mail@boanderson.me>
2021-10-25 17:51:04 +0000

    xrootd: update 5.3.1 bottle.

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -12,11 +12,13 @@ class Xrootd < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "362424b968ff53f18f064954e2c5ced9565d7c2401e86b921efd645e66bfe0c9"
-    sha256 cellar: :any,                 big_sur:       "c3a2b3d0e6078523c33a8256461615bde15c5444ce44da8ffba50ce6184db70e"
-    sha256 cellar: :any,                 catalina:      "60141a86efe1880508a94a9aa935d7eeea4708d710fb1d32c39b84b4a3cac779"
-    sha256 cellar: :any,                 mojave:        "402b1697b621101b2952acbbc9aae36d0b953b3a20acb1dbabc2ef9910e4734c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e05c2784cb3c302cdc0951d1755790cea3b63d3e05c78d3f672204a5f70b9e8"
+    sha256 cellar: :any,                 arm64_monterey: "06ebc502b6824247a757cfb8a9a5e1cb9a2053d4acd6b7683d8799cdf051572b"
+    sha256 cellar: :any,                 arm64_big_sur:  "362424b968ff53f18f064954e2c5ced9565d7c2401e86b921efd645e66bfe0c9"
+    sha256 cellar: :any,                 monterey:       "da82633e5ed250f32444e94e57d9ac566860544fdb7862dc3becdcb129cff89f"
+    sha256 cellar: :any,                 big_sur:        "c3a2b3d0e6078523c33a8256461615bde15c5444ce44da8ffba50ce6184db70e"
+    sha256 cellar: :any,                 catalina:       "60141a86efe1880508a94a9aa935d7eeea4708d710fb1d32c39b84b4a3cac779"
+    sha256 cellar: :any,                 mojave:         "402b1697b621101b2952acbbc9aae36d0b953b3a20acb1dbabc2ef9910e4734c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e05c2784cb3c302cdc0951d1755790cea3b63d3e05c78d3f672204a5f70b9e8"
   end
 
   depends_on "cmake" => :build
