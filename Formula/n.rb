a30317d81a8c3c4a4629ed05e65c6f86f2dfd021
Rui Chen <rui@chenrui.dev>
2021-10-25 00:10:27 -0400

    n 8.0.0
    Closes #87952.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/n.rb
+++ Formula/n.rb
@@ -1,8 +1,8 @@
 class N < Formula
   desc "Node version management"
   homepage "https://github.com/tj/n"
-  url "https://github.com/tj/n/archive/v7.5.0.tar.gz"
-  sha256 "d4da7ea91f680de0c9b5876e097e2a793e8234fcd0f7ca87a0599b925be087a3"
+  url "https://github.com/tj/n/archive/v8.0.0.tar.gz"
+  sha256 "9e8879dc4f1c4c0fe4e08a108ed6c23046419b6865fe922ca5176ff7998ae6ff"
   license "MIT"
   head "https://github.com/tj/n.git", branch: "master"
 
