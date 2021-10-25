1422290fb625d6aee36098d7cfe8a55d1c156a63
Bo Anderson <mail@boanderson.me>
2021-10-25 04:37:17 +0000

    cscope: update 15.9 bottle.

--- Formula/cscope.rb
+++ Formula/cscope.rb
@@ -10,13 +10,15 @@ class Cscope < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "149cb1ebe576bca4c8c928ed98005054e9ac2b8822c80ce9582d500453bce0ad"
-    sha256 cellar: :any_skip_relocation, big_sur:       "41553bf0bbc2ce6e41712381fcbc2d86eca0dd4618d138ca70037df8a1bf4e01"
-    sha256 cellar: :any_skip_relocation, catalina:      "212b5f945f2a2eae2d07893bb08c490098f4f3e58ec8865499bec550882de29e"
-    sha256 cellar: :any_skip_relocation, mojave:        "0a8c76e372e2c965e654b5024cbf872931e6204b7e2ba79623d5d7d002cd3c2f"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "ae7b5f716debeb937c3472add41f69c7176e9c4a9a0668090afd63313eabbe86"
-    sha256 cellar: :any_skip_relocation, sierra:        "7eef899511b0d7eb0d6a35acf677d9b19f89528aae0272d5c414bbafbe5daaaf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aed0460f7dc9a9355d4fb019760284424d56012224f12df098c8fe9912c904c2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b85e2ad5e9ddcdb57a2cca430520e64f70cefd2b182bae70c216360e72757611"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "149cb1ebe576bca4c8c928ed98005054e9ac2b8822c80ce9582d500453bce0ad"
+    sha256 cellar: :any_skip_relocation, monterey:       "0baacd94730059e4a03e8980f1332ca38ed48f98dc70950103a71f4435347f2e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "41553bf0bbc2ce6e41712381fcbc2d86eca0dd4618d138ca70037df8a1bf4e01"
+    sha256 cellar: :any_skip_relocation, catalina:       "212b5f945f2a2eae2d07893bb08c490098f4f3e58ec8865499bec550882de29e"
+    sha256 cellar: :any_skip_relocation, mojave:         "0a8c76e372e2c965e654b5024cbf872931e6204b7e2ba79623d5d7d002cd3c2f"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "ae7b5f716debeb937c3472add41f69c7176e9c4a9a0668090afd63313eabbe86"
+    sha256 cellar: :any_skip_relocation, sierra:         "7eef899511b0d7eb0d6a35acf677d9b19f89528aae0272d5c414bbafbe5daaaf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aed0460f7dc9a9355d4fb019760284424d56012224f12df098c8fe9912c904c2"
   end
 
   uses_from_macos "ncurses"
