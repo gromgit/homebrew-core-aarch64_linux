ac2528e0e2b47c11f94074f7b032047e09a5f70d
Bo Anderson <mail@boanderson.me>
2021-10-25 15:52:30 +0000

    pssh: update 2.3.1_5 bottle.

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -9,11 +9,13 @@ class Pssh < Formula
   revision 5
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "59a80ba06f64db6ac564b05a91b33cd9dab0d7d9c1a9cd7e932b9afb2b2012c3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
-    sha256 cellar: :any_skip_relocation, catalina:      "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
-    sha256 cellar: :any_skip_relocation, mojave:        "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "db241389e508b5db1cf14b6edf01b09b185f95326b8301ba7c04ee38478f7e39"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "59a80ba06f64db6ac564b05a91b33cd9dab0d7d9c1a9cd7e932b9afb2b2012c3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59a80ba06f64db6ac564b05a91b33cd9dab0d7d9c1a9cd7e932b9afb2b2012c3"
+    sha256 cellar: :any_skip_relocation, monterey:       "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
+    sha256 cellar: :any_skip_relocation, catalina:       "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
+    sha256 cellar: :any_skip_relocation, mojave:         "d1c1837c888b3c60e7e5cf3db91660c35b14c39e72d7418317eba99fe30bee0c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db241389e508b5db1cf14b6edf01b09b185f95326b8301ba7c04ee38478f7e39"
   end
 
   depends_on "python@3.10"
