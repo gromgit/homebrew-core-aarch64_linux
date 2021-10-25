77bcffb77bba7444baaa4bf3f90acef517d2e4af
Bo Anderson <mail@boanderson.me>
2021-10-25 05:51:39 +0000

    gperftools: update 2.9.1 bottle.

--- Formula/gperftools.rb
+++ Formula/gperftools.rb
@@ -12,11 +12,13 @@ class Gperftools < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a8711aa1a9496a1c801b10bcfc3572fc204be3af52f2455a0a3d9e1b2d924aae"
-    sha256 cellar: :any,                 big_sur:       "db13bfa856a699c5e74e95ee81722cc76b38bb9dcca1d10cebe2eed17888ff68"
-    sha256 cellar: :any,                 catalina:      "df9901c12be430101b403c8024a4dc5b5f5d0f718e4ace970f52bc68b17a3659"
-    sha256 cellar: :any,                 mojave:        "9976b82f86958d3ad6924d138d138d5bddbc2bcc6eb16ea44c4255ed9cb889b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35bdc71ea25b996acec0752c3dbb6a68b1f56529f577f08f1a32556893ce70ca"
+    sha256 cellar: :any,                 arm64_monterey: "225e48e3d56d64dc9161d286a142a6f410a0ebc5572127cc3c1e51e426352fbd"
+    sha256 cellar: :any,                 arm64_big_sur:  "a8711aa1a9496a1c801b10bcfc3572fc204be3af52f2455a0a3d9e1b2d924aae"
+    sha256 cellar: :any,                 monterey:       "43ee00f578719526821be22dfa6c6ee8637a65206f9862056bd6a7d5c0d6f6df"
+    sha256 cellar: :any,                 big_sur:        "db13bfa856a699c5e74e95ee81722cc76b38bb9dcca1d10cebe2eed17888ff68"
+    sha256 cellar: :any,                 catalina:       "df9901c12be430101b403c8024a4dc5b5f5d0f718e4ace970f52bc68b17a3659"
+    sha256 cellar: :any,                 mojave:         "9976b82f86958d3ad6924d138d138d5bddbc2bcc6eb16ea44c4255ed9cb889b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "35bdc71ea25b996acec0752c3dbb6a68b1f56529f577f08f1a32556893ce70ca"
   end
 
   head do
