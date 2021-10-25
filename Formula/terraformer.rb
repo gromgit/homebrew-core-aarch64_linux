eb66dacadfceeeab2e7c870705a346f048a233c2
Bo Anderson <mail@boanderson.me>
2021-10-25 06:33:45 +0000

    terraformer: update 0.8.18 bottle.

--- Formula/terraformer.rb
+++ Formula/terraformer.rb
@@ -7,10 +7,12 @@ class Terraformer < Formula
   head "https://github.com/GoogleCloudPlatform/terraformer.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "af33c9e922c7b28762fa3c6140978bd6ee8382c69d8923920eb9a5c9110d49d2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1541fa358639322c5ffd323e294fce0d1fde0ea623f39b6eaedd6fa60995a47a"
-    sha256 cellar: :any_skip_relocation, catalina:      "58727f550e40191baf766b3e7141c038ac42e9025b2423392932e687a5738fb5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4405e1fb31770e11c684c8cd1d3ef4ad1945326d54c83254198aa77e79131d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5072433e56f6733a7b5e155d5d96462ac65affad4797630299ad12afc21ff04c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af33c9e922c7b28762fa3c6140978bd6ee8382c69d8923920eb9a5c9110d49d2"
+    sha256 cellar: :any_skip_relocation, monterey:       "58aae7baefdc9a45ea133e219c9cf05e53793bf1b872255e720e72f7fe3fc868"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1541fa358639322c5ffd323e294fce0d1fde0ea623f39b6eaedd6fa60995a47a"
+    sha256 cellar: :any_skip_relocation, catalina:       "58727f550e40191baf766b3e7141c038ac42e9025b2423392932e687a5738fb5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4405e1fb31770e11c684c8cd1d3ef4ad1945326d54c83254198aa77e79131d3"
   end
 
   depends_on "go" => :build
