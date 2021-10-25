81d00bd598cbfabb5e498e7d3636bf635dea1c11
Bo Anderson <mail@boanderson.me>
2021-10-25 17:52:19 +0000

    htslib: update 1.14 bottle.

--- Formula/htslib.rb
+++ Formula/htslib.rb
@@ -11,10 +11,12 @@ class Htslib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "01178faca1c4825644c4f45d719c547d97eea893ece6eaff9aba9ba3e06155c3"
-    sha256 cellar: :any,                 big_sur:       "cf7d3aa06fb89dca0f35f9184d2f431897fc290ed20b90c838cea235c4de814c"
-    sha256 cellar: :any,                 catalina:      "89e858252df0b8fec7e07dc82159bb4a9cb629ff54eb5848b319baef41197e26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "90d770d787efcc137b83a491385683949861a0c5a9084e8eae66ff4ff47ce432"
+    sha256 cellar: :any,                 arm64_monterey: "75ad52abf7902d356ecd260cdc922220879284462d3178194e14e57b87b21cdd"
+    sha256 cellar: :any,                 arm64_big_sur:  "01178faca1c4825644c4f45d719c547d97eea893ece6eaff9aba9ba3e06155c3"
+    sha256 cellar: :any,                 monterey:       "fdd1fb62df77a796745eb09f9b348c78176703f4deeb593a8f881845c4b426d5"
+    sha256 cellar: :any,                 big_sur:        "cf7d3aa06fb89dca0f35f9184d2f431897fc290ed20b90c838cea235c4de814c"
+    sha256 cellar: :any,                 catalina:       "89e858252df0b8fec7e07dc82159bb4a9cb629ff54eb5848b319baef41197e26"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "90d770d787efcc137b83a491385683949861a0c5a9084e8eae66ff4ff47ce432"
   end
 
   depends_on "xz"
