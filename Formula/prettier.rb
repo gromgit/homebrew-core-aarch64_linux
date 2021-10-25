3b98fcbc78168d7c7e14b3e28bfab5071504341d
Bo Anderson <mail@boanderson.me>
2021-10-25 05:30:34 +0000

    prettier: update 2.4.1 bottle.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -9,11 +9,13 @@ class Prettier < Formula
   head "https://github.com/prettier/prettier.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8ff333e5a85072c16ffc3e45c547ee03adbd2db1ccfcad6530e676cdde8829f9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
-    sha256 cellar: :any_skip_relocation, catalina:      "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
-    sha256 cellar: :any_skip_relocation, mojave:        "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ff333e5a85072c16ffc3e45c547ee03adbd2db1ccfcad6530e676cdde8829f9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8ff333e5a85072c16ffc3e45c547ee03adbd2db1ccfcad6530e676cdde8829f9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ff333e5a85072c16ffc3e45c547ee03adbd2db1ccfcad6530e676cdde8829f9"
+    sha256 cellar: :any_skip_relocation, monterey:       "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
+    sha256 cellar: :any_skip_relocation, catalina:       "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
+    sha256 cellar: :any_skip_relocation, mojave:         "bf7b106b0628675290c36da366cc9c4caf1d1883f66634410520f2fefd58ac82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ff333e5a85072c16ffc3e45c547ee03adbd2db1ccfcad6530e676cdde8829f9"
   end
 
   depends_on "node"
