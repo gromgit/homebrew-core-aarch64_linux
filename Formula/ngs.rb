9479e2260bf4132158ea4dade8959c88d6a5553c
Michka Popoff <michkapopoff@gmail.com>
2021-10-25 08:12:54 +0200

    ngs: bump revision for libffi on Linux
    Closes #87976.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ngs.rb
+++ Formula/ngs.rb
@@ -4,6 +4,7 @@ class Ngs < Formula
   url "https://github.com/ngs-lang/ngs/archive/v0.2.12.tar.gz"
   sha256 "bd3f3b7cca4a36150405f26bb9bcc2fb41d0149388d3051472f159072485f962"
   license "GPL-3.0-only"
+  revision 1
   head "https://github.com/ngs-lang/ngs.git", branch: "master"
 
   bottle do
