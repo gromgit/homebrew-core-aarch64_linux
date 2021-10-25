5218b6c2232dc7cc57c36fb03bfa0ce533dc338c
Bo Anderson <mail@boanderson.me>
2021-10-25 17:13:39 +0000

    gitmoji: update 4.7.0 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -8,11 +8,13 @@ class Gitmoji < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8bcb44e8427347f0c761adb99eaca5344fc886dfc288a51098a21277aa96f3d5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "07195afc3abf49df75264de636d55ec69b6aa305a03e41c30a435fe366779d61"
-    sha256 cellar: :any_skip_relocation, catalina:      "07195afc3abf49df75264de636d55ec69b6aa305a03e41c30a435fe366779d61"
-    sha256 cellar: :any_skip_relocation, mojave:        "07195afc3abf49df75264de636d55ec69b6aa305a03e41c30a435fe366779d61"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8bcb44e8427347f0c761adb99eaca5344fc886dfc288a51098a21277aa96f3d5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "33877fad24b1ca32539f500fe3bd35fa26cc9c92aa78fba352a01082c435f493"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8bcb44e8427347f0c761adb99eaca5344fc886dfc288a51098a21277aa96f3d5"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab0221dc3571e720f5b1a376b8d415fdaeafe0ff54c9a9abfaa53587aae7c1b0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "07195afc3abf49df75264de636d55ec69b6aa305a03e41c30a435fe366779d61"
+    sha256 cellar: :any_skip_relocation, catalina:       "07195afc3abf49df75264de636d55ec69b6aa305a03e41c30a435fe366779d61"
+    sha256 cellar: :any_skip_relocation, mojave:         "07195afc3abf49df75264de636d55ec69b6aa305a03e41c30a435fe366779d61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bcb44e8427347f0c761adb99eaca5344fc886dfc288a51098a21277aa96f3d5"
   end
 
   depends_on "node"
