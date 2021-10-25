1756f64d80f9cf443db0b746c3d3f7612fcca134
Bo Anderson <mail@boanderson.me>
2021-10-25 06:03:18 +0000

    pygments: update 2.10.0_1 bottle.

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -10,11 +10,13 @@ class Pygments < Formula
   head "https://github.com/pygments/pygments.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c625b002e4402b4c7fdecded981fa7ff87e1514d284e4057011d041d12ad093e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, catalina:      "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, mojave:        "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd675d1a3d161a05a82e0a4819eeb4464219e9eba491b6af816bff8820ce3e20"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c625b002e4402b4c7fdecded981fa7ff87e1514d284e4057011d041d12ad093e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c625b002e4402b4c7fdecded981fa7ff87e1514d284e4057011d041d12ad093e"
+    sha256 cellar: :any_skip_relocation, monterey:       "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
+    sha256 cellar: :any_skip_relocation, catalina:       "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
+    sha256 cellar: :any_skip_relocation, mojave:         "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd675d1a3d161a05a82e0a4819eeb4464219e9eba491b6af816bff8820ce3e20"
   end
 
   depends_on "python@3.10"
