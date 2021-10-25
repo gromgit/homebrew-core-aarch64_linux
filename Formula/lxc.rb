77ad74ae176b5021f90d097e908ea26cfda969dc
Bo Anderson <mail@boanderson.me>
2021-10-25 07:05:19 +0000

    lxc: update 4.19 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -11,11 +11,13 @@ class Lxc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cbbef2f6c2fc729ef7c9d4b5602715aa3045596bae6f259fcf90fb5c10cfaf1d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9ba5f63e307465a3a961287559ce3473635ab2fcc2f73fe40fb5376f44b97881"
-    sha256 cellar: :any_skip_relocation, catalina:      "725720b4449d358517318a8d8a29df5dc9f476369ee8ba9c43b3ae0c53624b42"
-    sha256 cellar: :any_skip_relocation, mojave:        "5b52391136e53d428740b1eca815d61b9aa7c4f31c43924833ed9e2cb2db6323"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9f4ea04b46a4aa396f9510e7f95f816e74c889621f0a01e5f191a311526ff871"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ed9aac80ad29cd09778bdab57cc4bbcfbcb054d10d0cabe7ecd057e019d2d298"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cbbef2f6c2fc729ef7c9d4b5602715aa3045596bae6f259fcf90fb5c10cfaf1d"
+    sha256 cellar: :any_skip_relocation, monterey:       "0681cf660920b66674c24466a717e64ec1f8332c5520b7e380ce57e538052731"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9ba5f63e307465a3a961287559ce3473635ab2fcc2f73fe40fb5376f44b97881"
+    sha256 cellar: :any_skip_relocation, catalina:       "725720b4449d358517318a8d8a29df5dc9f476369ee8ba9c43b3ae0c53624b42"
+    sha256 cellar: :any_skip_relocation, mojave:         "5b52391136e53d428740b1eca815d61b9aa7c4f31c43924833ed9e2cb2db6323"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f4ea04b46a4aa396f9510e7f95f816e74c889621f0a01e5f191a311526ff871"
   end
 
   depends_on "go" => :build
