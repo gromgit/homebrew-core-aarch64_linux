e585e8ad9cc518ab10c110183527e65c919c253c
Bo Anderson <mail@boanderson.me>
2021-10-25 05:36:53 +0000

    hashcat: update 6.1.1 bottle.

--- Formula/hashcat.rb
+++ Formula/hashcat.rb
@@ -11,11 +11,13 @@ class Hashcat < Formula
   head "https://github.com/hashcat/hashcat.git"
 
   bottle do
-    sha256 arm64_big_sur: "0b7ed83aa1cad6c2b763dee4495d2f56cf5d045dfc8df730a81c4ec35cd453a0"
-    sha256 big_sur:       "b0d2f4ed9f9d8450f1d79285869b69909d2a08f9393ec2d8b39cb05dcefb52b9"
-    sha256 catalina:      "dd7e0ba6e2441b0517b55ef243efc8f8be3dfd1550b83033e264057a677e8fa2"
-    sha256 mojave:        "ad145c3ffb0ffb779dbdde3e0c1c0329b2b45d491e76ba0b92c48c02028e51c1"
-    sha256 high_sierra:   "2e1cbf12a3f5ed5fceb622e5f773d102edfaff06a847df925456e92dd6ca91ba"
+    sha256 arm64_monterey: "88e25be640507954e9e03f701c8fc81bd4fc7f3545218b73088f02bb9672e885"
+    sha256 arm64_big_sur:  "0b7ed83aa1cad6c2b763dee4495d2f56cf5d045dfc8df730a81c4ec35cd453a0"
+    sha256 monterey:       "42cc0e8b6cc15bdcaaea34809628108df32ea70bb0f0b1a384623bd39711e81c"
+    sha256 big_sur:        "b0d2f4ed9f9d8450f1d79285869b69909d2a08f9393ec2d8b39cb05dcefb52b9"
+    sha256 catalina:       "dd7e0ba6e2441b0517b55ef243efc8f8be3dfd1550b83033e264057a677e8fa2"
+    sha256 mojave:         "ad145c3ffb0ffb779dbdde3e0c1c0329b2b45d491e76ba0b92c48c02028e51c1"
+    sha256 high_sierra:    "2e1cbf12a3f5ed5fceb622e5f773d102edfaff06a847df925456e92dd6ca91ba"
   end
 
   depends_on "gnu-sed" => :build
