ee4625e7b5805ee11d033b8303411f7c33ca2d1b
Bo Anderson <mail@boanderson.me>
2021-10-25 18:54:09 +0000

    psqlodbc: update 13.02.0000 bottle.

--- Formula/psqlodbc.rb
+++ Formula/psqlodbc.rb
@@ -10,11 +10,13 @@ class Psqlodbc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "45bf96f7600543acf57ff529ac884ce9da5d84ab3df2fba8c799c318988b17a8"
-    sha256 cellar: :any,                 big_sur:       "8109e135efa71d1e0369b2433181819ef275bec48934a5b3107f99fcdce73efb"
-    sha256 cellar: :any,                 catalina:      "6da4ec9d0ec4ce763dc117b4e8c465289a90ea05c5c4d8bc954789334ea8021c"
-    sha256 cellar: :any,                 mojave:        "c766f5701dba28974ac654fcba040715fcef30a6fc9974802d7b61a5d63e0584"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de683e7933da22982cc7d6804a8d5ebb0333c87c8cff49aa3ec23c8e2119ea8b"
+    sha256 cellar: :any,                 arm64_monterey: "ed1b6837baffae5f6c56b867601f01a2e3afe17706a0807181f313bb433689d5"
+    sha256 cellar: :any,                 arm64_big_sur:  "45bf96f7600543acf57ff529ac884ce9da5d84ab3df2fba8c799c318988b17a8"
+    sha256 cellar: :any,                 monterey:       "9acc450ccbb8c63aebbc866a17f2b71b75297740ac8f6c9ea6bab5883f74e4a6"
+    sha256 cellar: :any,                 big_sur:        "8109e135efa71d1e0369b2433181819ef275bec48934a5b3107f99fcdce73efb"
+    sha256 cellar: :any,                 catalina:       "6da4ec9d0ec4ce763dc117b4e8c465289a90ea05c5c4d8bc954789334ea8021c"
+    sha256 cellar: :any,                 mojave:         "c766f5701dba28974ac654fcba040715fcef30a6fc9974802d7b61a5d63e0584"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "de683e7933da22982cc7d6804a8d5ebb0333c87c8cff49aa3ec23c8e2119ea8b"
   end
 
   head do
