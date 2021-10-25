5b99ed2da125ca18123ac3b597d079edf2996191
Bo Anderson <mail@boanderson.me>
2021-10-25 05:50:37 +0000

    fluid-synth: update 2.2.3 bottle.

--- Formula/fluid-synth.rb
+++ Formula/fluid-synth.rb
@@ -7,11 +7,13 @@ class FluidSynth < Formula
   head "https://github.com/FluidSynth/fluidsynth.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "57b1c20d591b10710d8925d9dc6ec36c59de7cc29f38fbbcf3227781647298a3"
-    sha256 cellar: :any,                 big_sur:       "df4608a36dba661b05360b0ce0f7b83c6a1d520a40f7d405f72a416517a01551"
-    sha256 cellar: :any,                 catalina:      "0ba416ec67f720ec5c3e2542699b93d53c7ea104dd101e1e794c9066828c00b1"
-    sha256 cellar: :any,                 mojave:        "9b26d7238abf5cf5fa3215ffc5c53da254cc3c9b020fab2922b3e7065ecb2384"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84ef1dae7d024c05e9e268da2353fbf04de322d38a33f9f8e45c1d565cb1f7ea"
+    sha256 cellar: :any,                 arm64_monterey: "a7a3375ad94fc3536c8a8e73763a4f6a5b239da1bde4847ee9766afdbc5073da"
+    sha256 cellar: :any,                 arm64_big_sur:  "57b1c20d591b10710d8925d9dc6ec36c59de7cc29f38fbbcf3227781647298a3"
+    sha256 cellar: :any,                 monterey:       "dc1c93485c360861c9ae0846fed81ef72d85349ed128dc2a26a93f99f000709e"
+    sha256 cellar: :any,                 big_sur:        "df4608a36dba661b05360b0ce0f7b83c6a1d520a40f7d405f72a416517a01551"
+    sha256 cellar: :any,                 catalina:       "0ba416ec67f720ec5c3e2542699b93d53c7ea104dd101e1e794c9066828c00b1"
+    sha256 cellar: :any,                 mojave:         "9b26d7238abf5cf5fa3215ffc5c53da254cc3c9b020fab2922b3e7065ecb2384"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84ef1dae7d024c05e9e268da2353fbf04de322d38a33f9f8e45c1d565cb1f7ea"
   end
 
   depends_on "cmake" => :build
