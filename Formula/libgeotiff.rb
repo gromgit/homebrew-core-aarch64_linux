06c07c39fca4811d8e88a133788b872bad040888
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-25 13:48:54 +0000

    libgeotiff: update 1.7.0 bottle.

--- Formula/libgeotiff.rb
+++ Formula/libgeotiff.rb
@@ -20,11 +20,13 @@ class Libgeotiff < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "0f191bda555533ac59d14b262d66a7a874ed55f77a6dc96785150eed1e84f7fe"
-    sha256 cellar: :any,                 big_sur:       "0e37a3add2a8840aebaf25f5e3e365eaca77c0943722366f760d4bebbcbdfe95"
-    sha256 cellar: :any,                 catalina:      "20f4e9268c9c5154858452b5d4d1764c042de0e53f5ee7ee45fb889b3754e7ec"
-    sha256 cellar: :any,                 mojave:        "ae97cbd99b52140c77705d0acd1f2b1affd7ffd907e00d6f00f72eae2e61c292"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8731b70f84b1e9daa9ac6c8747232d87c760bcb2be7eb83643cc6582e1163cc"
+    sha256 cellar: :any,                 arm64_monterey: "3d173e746cacebaf4a9be10c49e9f2a0fa94808993d75e44ba9465e5739d2100"
+    sha256 cellar: :any,                 arm64_big_sur:  "0f191bda555533ac59d14b262d66a7a874ed55f77a6dc96785150eed1e84f7fe"
+    sha256 cellar: :any,                 monterey:       "227714a8d1abd87e7f1cf3905116501838da12f3dc5a160b90e8b149d32cfec9"
+    sha256 cellar: :any,                 big_sur:        "0e37a3add2a8840aebaf25f5e3e365eaca77c0943722366f760d4bebbcbdfe95"
+    sha256 cellar: :any,                 catalina:       "20f4e9268c9c5154858452b5d4d1764c042de0e53f5ee7ee45fb889b3754e7ec"
+    sha256 cellar: :any,                 mojave:         "ae97cbd99b52140c77705d0acd1f2b1affd7ffd907e00d6f00f72eae2e61c292"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f8731b70f84b1e9daa9ac6c8747232d87c760bcb2be7eb83643cc6582e1163cc"
   end
 
   head do
