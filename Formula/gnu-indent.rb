5f7fc3363450ec7746ff017cddd7f194805fb104
Bo Anderson <mail@boanderson.me>
2021-10-25 17:49:44 +0000

    gnu-indent: update 2.2.12_1 bottle.

--- Formula/gnu-indent.rb
+++ Formula/gnu-indent.rb
@@ -8,13 +8,15 @@ class GnuIndent < Formula
   revision 1
 
   bottle do
-    sha256 arm64_big_sur: "bf082593202d39ea4c2929b333d544c72ef23d16fed04f570e1f4227098ebf6e"
-    sha256 big_sur:       "af4b5212440cdbb8c1c80bef3a13ca33bbdbd49918d24588af3a9eb44e484dab"
-    sha256 catalina:      "82a12279be8834591a2104253ac562978b557c26b262dd8d5bfbf6e7b1103dd1"
-    sha256 mojave:        "e960e3f35f6a77daef487f54158953522f58a27caf27e39e0c17702754718ee1"
-    sha256 high_sierra:   "3280e6e9fc0c5cc895367291fc328dccae5f2e36606dd503b5721d449bc33eb8"
-    sha256 sierra:        "98bcdee2e49d7e165a07ce6468d2c1a3030db7205472d015ba516e43f5a1e0fd"
-    sha256 x86_64_linux:  "e3ef74b022310ffe3742480791e140ee05b61b315cfcb7de9fa6ebdac77a7b92"
+    sha256 arm64_monterey: "2311fb51012e426bd4cf01047a98cbcf70a5bf343aa089dc706d1e3c84b05964"
+    sha256 arm64_big_sur:  "bf082593202d39ea4c2929b333d544c72ef23d16fed04f570e1f4227098ebf6e"
+    sha256 monterey:       "f23364d4a472c8c1430967e887da54e319bf94687cdeb718988dde87459691cd"
+    sha256 big_sur:        "af4b5212440cdbb8c1c80bef3a13ca33bbdbd49918d24588af3a9eb44e484dab"
+    sha256 catalina:       "82a12279be8834591a2104253ac562978b557c26b262dd8d5bfbf6e7b1103dd1"
+    sha256 mojave:         "e960e3f35f6a77daef487f54158953522f58a27caf27e39e0c17702754718ee1"
+    sha256 high_sierra:    "3280e6e9fc0c5cc895367291fc328dccae5f2e36606dd503b5721d449bc33eb8"
+    sha256 sierra:         "98bcdee2e49d7e165a07ce6468d2c1a3030db7205472d015ba516e43f5a1e0fd"
+    sha256 x86_64_linux:   "e3ef74b022310ffe3742480791e140ee05b61b315cfcb7de9fa6ebdac77a7b92"
   end
 
   depends_on "gettext"
