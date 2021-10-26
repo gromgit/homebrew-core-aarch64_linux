e91d497a7a34fb633814617dc0f33eced3ed3cde
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:16:48 +0000

    draco: update 1.4.3 bottle.

--- Formula/draco.rb
+++ Formula/draco.rb
@@ -6,11 +6,13 @@ class Draco < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3b133142f86e93c4da293e5313d225235078de8f3e08baddecb3eb3b5675e6f2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1290a57ebb6ba3bd53664969513f457c8f85b7c2f543211f08fb72e196abe819"
-    sha256 cellar: :any_skip_relocation, catalina:      "5d8c5f99fa4e605adc5268058c31ef8cd109e763e548679e63d6efbb63412941"
-    sha256 cellar: :any_skip_relocation, mojave:        "76c996cfd7d4ae8de4fcf9693cc7d1c7e61ea35cb9e730e7e5d005d2b09aa527"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fcd04c6ce5607217f0bae7918c246d7b8482906676067320ecf4592c17ff3260"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7780c806200754f059d5b0308c05e754f8178732094db56778e5dd84e6e3be1b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b133142f86e93c4da293e5313d225235078de8f3e08baddecb3eb3b5675e6f2"
+    sha256 cellar: :any_skip_relocation, monterey:       "dafc34d46e7011e870a0be988b5f95622dc2d39d35da161137b2571ae3c48f30"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1290a57ebb6ba3bd53664969513f457c8f85b7c2f543211f08fb72e196abe819"
+    sha256 cellar: :any_skip_relocation, catalina:       "5d8c5f99fa4e605adc5268058c31ef8cd109e763e548679e63d6efbb63412941"
+    sha256 cellar: :any_skip_relocation, mojave:         "76c996cfd7d4ae8de4fcf9693cc7d1c7e61ea35cb9e730e7e5d005d2b09aa527"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fcd04c6ce5607217f0bae7918c246d7b8482906676067320ecf4592c17ff3260"
   end
 
   depends_on "cmake" => :build
