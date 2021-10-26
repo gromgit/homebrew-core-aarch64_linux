2da003a1c6409f919674324d3f4ddc3dfe0eb506
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-10-26 14:13:32 +0000

    ld-find-code-refs: add 2.2.3 bottle.

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -6,6 +6,13 @@ class LdFindCodeRefs < Formula
   license "Apache-2.0"
   head "https://github.com/launchdarkly/ld-find-code-refs.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "726e6e93834ae526104c86d97499650e8258a569dc8ab2876497c447fba1e2ba"
+    sha256 cellar: :any_skip_relocation, big_sur:       "50a65a662b9fa349254f2fa6b093fe8d94ebb6308538252dcdbdb92c51cb7ce0"
+    sha256 cellar: :any_skip_relocation, catalina:      "b5ae0b0c2b01e6e2a846788d329e71d6e27210a7c14d8da465841f755fa13445"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33eba7847e1671d9edc0196b227b7b73b07446e297e284e8fb6814353eb0906d"
+  end
+
   depends_on "go" => :build
 
   def install
