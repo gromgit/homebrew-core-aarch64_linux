2b97d295aff9a80345fbe1d43548b63aa539c176
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 06:58:26 +0000

    vpn-slice: update 0.15_1 bottle.

--- Formula/vpn-slice.rb
+++ Formula/vpn-slice.rb
@@ -10,11 +10,13 @@ class VpnSlice < Formula
   head "https://github.com/dlenski/vpn-slice.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4d573308656014686896eb8f1089b34c80681dcd9f9f236830cc4047fc7b2a95"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1c88c94f36629761206f33c6c15fedf999abb119e7cec586c01f1d2fe188ae70"
-    sha256 cellar: :any_skip_relocation, catalina:      "d163cd9ca3fe156de8cdc2b77a36e357cdff7317a145115157b545f0703520c4"
-    sha256 cellar: :any_skip_relocation, mojave:        "4d16aa1912e1c56a359242288d82c5bf0246c7e9cb0d507d1c267b35f176eee4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c1e691a28af9d74ef1453b1b4b2e782cd5c8271ea1a991694daf3fab7c4dc3a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c7e8122a3a4c573ce3f56a4ea590d8dd624e3d9a77a6ef5c6cc7920264a8c470"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d573308656014686896eb8f1089b34c80681dcd9f9f236830cc4047fc7b2a95"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd8425b424c9a0ab4f20d520a5161ea6fd4df98dd2abae8167fce85a037f8650"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1c88c94f36629761206f33c6c15fedf999abb119e7cec586c01f1d2fe188ae70"
+    sha256 cellar: :any_skip_relocation, catalina:       "d163cd9ca3fe156de8cdc2b77a36e357cdff7317a145115157b545f0703520c4"
+    sha256 cellar: :any_skip_relocation, mojave:         "4d16aa1912e1c56a359242288d82c5bf0246c7e9cb0d507d1c267b35f176eee4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c1e691a28af9d74ef1453b1b4b2e782cd5c8271ea1a991694daf3fab7c4dc3a"
   end
 
   depends_on "python@3.10"
