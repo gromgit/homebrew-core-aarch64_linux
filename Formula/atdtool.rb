5b344a6bf5d3a5ba26ce1747143fd31056bdce1d
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 05:21:53 +0000

    atdtool: update 1.3.3_5 bottle.

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -9,11 +9,13 @@ class Atdtool < Formula
   revision 5
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "00860b394d9167bc00b8a31a3edf07aa02ecca3434ad379d2b48e1914f0be191"
-    sha256 cellar: :any_skip_relocation, big_sur:       "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
-    sha256 cellar: :any_skip_relocation, catalina:      "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
-    sha256 cellar: :any_skip_relocation, mojave:        "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e982a3e891104e18964da29892efef84c79da560c3c2525467fcaa48bb28819f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "00860b394d9167bc00b8a31a3edf07aa02ecca3434ad379d2b48e1914f0be191"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "00860b394d9167bc00b8a31a3edf07aa02ecca3434ad379d2b48e1914f0be191"
+    sha256 cellar: :any_skip_relocation, monterey:       "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
+    sha256 cellar: :any_skip_relocation, catalina:       "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
+    sha256 cellar: :any_skip_relocation, mojave:         "32171ec53c6f52a2e877ba0b1506be093e42f4d594418a2727675a327daef1c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e982a3e891104e18964da29892efef84c79da560c3c2525467fcaa48bb28819f"
   end
 
   deprecate! date: "2020-11-18", because: :repo_archived
