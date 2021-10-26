94d483882ba03abb0e6fa1395059e3e663f8708b
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 05:33:14 +0000

    cpplint: update 1.5.5_1 bottle.

--- Formula/cpplint.rb
+++ Formula/cpplint.rb
@@ -9,11 +9,13 @@ class Cpplint < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "52d1995aad005c3f5b2bf942e97ae8614aa32b0cdbc42b89366d9bdd02cfdca8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
-    sha256 cellar: :any_skip_relocation, catalina:      "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
-    sha256 cellar: :any_skip_relocation, mojave:        "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f37dd7a7c63a626a990f6703348e9cafec1e4928b3bd4eee470062fc45a77b0e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "52d1995aad005c3f5b2bf942e97ae8614aa32b0cdbc42b89366d9bdd02cfdca8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "52d1995aad005c3f5b2bf942e97ae8614aa32b0cdbc42b89366d9bdd02cfdca8"
+    sha256 cellar: :any_skip_relocation, monterey:       "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
+    sha256 cellar: :any_skip_relocation, catalina:       "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
+    sha256 cellar: :any_skip_relocation, mojave:         "8f6ae4eafa2e16ecbc84c7f36c9f37376a3e20b4469d3d7a8f53b13e9b9a9d56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f37dd7a7c63a626a990f6703348e9cafec1e4928b3bd4eee470062fc45a77b0e"
   end
 
   depends_on "python@3.10"
