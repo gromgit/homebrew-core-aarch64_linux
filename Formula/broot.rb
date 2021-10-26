5ff98b1d41e75604c1c80687e10eac4d6eca8c47
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 08:19:59 +0000

    broot: update 1.6.6 bottle.

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -7,10 +7,12 @@ class Broot < Formula
   head "https://github.com/Canop/broot.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e131378e8c88e1401108675a82cf04baeeedd2c4bea9d5d10fd876064d8756fa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3389152727e5ae6b70eed971859c31950f13b52b923a87f7b15617645992d8e3"
-    sha256 cellar: :any_skip_relocation, catalina:      "8970f833a720ab59aced6f44a2adec0bef98aaeacdbf0e37752447d333af08e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2df48d71e4bf9efcc8a06e11f46f612ea6010924fe5d8e4ebe6c8b7a8dd0eb8f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b4b2d6e32ef0b25158f02d37e13e2256780cac049e5b3cb02f930e73ea30509b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e131378e8c88e1401108675a82cf04baeeedd2c4bea9d5d10fd876064d8756fa"
+    sha256 cellar: :any_skip_relocation, monterey:       "cb022d6612b55839918433355164b992438f14f7c4d568a49e1437f55783c550"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3389152727e5ae6b70eed971859c31950f13b52b923a87f7b15617645992d8e3"
+    sha256 cellar: :any_skip_relocation, catalina:       "8970f833a720ab59aced6f44a2adec0bef98aaeacdbf0e37752447d333af08e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2df48d71e4bf9efcc8a06e11f46f612ea6010924fe5d8e4ebe6c8b7a8dd0eb8f"
   end
 
   depends_on "rust" => :build
