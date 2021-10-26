11e0f6f812303821aa45af1a783ad3960a7ec896
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 00:36:18 +0000

    zig: update 0.8.1_1 bottle.

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -12,10 +12,12 @@ class Zig < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "4b06b5abf72a311162138a852dcadb863974d4c69573dfa13d9857b312010165"
-    sha256 cellar: :any,                 big_sur:       "18e6b29f749642d8a01ce2d9657e206c9b7d3f130264f39f7afde9b9b072e03b"
-    sha256 cellar: :any,                 catalina:      "80fa8f4df5ccc5e0f8b190a8ec86eabd223ec56c756fa5e1821ef8d75d93934f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "effb69c4ee99ffa28c7a927c786565c03f56f99da437026f3788218d962c3ab8"
+    sha256 cellar: :any,                 arm64_monterey: "018b275bd4e61525d19dc95f065622cac2deeb83a562d6d779414b056db989ce"
+    sha256 cellar: :any,                 arm64_big_sur:  "4b06b5abf72a311162138a852dcadb863974d4c69573dfa13d9857b312010165"
+    sha256 cellar: :any,                 monterey:       "d882ab923be5da7adf934d5ad34f3b6c90907411c896ad78f87841f513afcbfc"
+    sha256 cellar: :any,                 big_sur:        "18e6b29f749642d8a01ce2d9657e206c9b7d3f130264f39f7afde9b9b072e03b"
+    sha256 cellar: :any,                 catalina:       "80fa8f4df5ccc5e0f8b190a8ec86eabd223ec56c756fa5e1821ef8d75d93934f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "effb69c4ee99ffa28c7a927c786565c03f56f99da437026f3788218d962c3ab8"
   end
 
   head do
