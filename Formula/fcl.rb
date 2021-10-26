67b5f657adec32347d8b25a172c8a4346f081eb2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 14:22:11 +0000

    fcl: update 0.7.0 bottle.

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -7,11 +7,13 @@ class Fcl < Formula
   head "https://github.com/flexible-collision-library/fcl.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d2054fa4adf473a736ee945e0a1817772a77b423c1fbd314f7ced016ad616760"
-    sha256 cellar: :any,                 big_sur:       "211e57fdf880e25e2441be78170dd1433d746aee1bb9a98990479abc2037a546"
-    sha256 cellar: :any,                 catalina:      "2bac88b84304ad8de56bbf604f7466ee147c31e04484df02ff45ef4aa9c966cc"
-    sha256 cellar: :any,                 mojave:        "095c6887a9007ec034751d8326fa7f5052180b78605d5df92223e081a17cad3c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7b0a2f8116b46c0f78f86c850ca6a808282bbc8dc6a7768d716faef891ce64bf"
+    sha256 cellar: :any,                 arm64_monterey: "15b9e1b7c5166ad222c94e848259726896f59187b4a31c90671f4bdd00547591"
+    sha256 cellar: :any,                 arm64_big_sur:  "d2054fa4adf473a736ee945e0a1817772a77b423c1fbd314f7ced016ad616760"
+    sha256 cellar: :any,                 monterey:       "b79ff81383e9aa9de0a692f11d287365751a9610395e99b0acaff1eae9a8f4c8"
+    sha256 cellar: :any,                 big_sur:        "211e57fdf880e25e2441be78170dd1433d746aee1bb9a98990479abc2037a546"
+    sha256 cellar: :any,                 catalina:       "2bac88b84304ad8de56bbf604f7466ee147c31e04484df02ff45ef4aa9c966cc"
+    sha256 cellar: :any,                 mojave:         "095c6887a9007ec034751d8326fa7f5052180b78605d5df92223e081a17cad3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b0a2f8116b46c0f78f86c850ca6a808282bbc8dc6a7768d716faef891ce64bf"
   end
 
   depends_on "cmake" => :build
