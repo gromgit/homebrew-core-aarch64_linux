cc62197c88d2e9486c34f46124deae9861604138
rui <rui@chenrui.dev>
2021-10-26 15:31:04 +0000

    amp: update 0.6.2_1 bottle.

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -8,11 +8,13 @@ class Amp < Formula
   head "https://github.com/jmacdonald/amp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f001a886b5ea456bc925ae37ef45c6f5ae70ef8506ae576fe3f831e78f2ecbcb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "db4b6bdf3468476c2f2b6696f755c05b2471f6d09bc47bf783fbd6c65b1b1aac"
-    sha256 cellar: :any_skip_relocation, catalina:      "6b886491460ff6245f9f6ecd22d0a856f51afbd06ec7adf13e2c8be974693656"
-    sha256 cellar: :any_skip_relocation, mojave:        "8d11d70c1a7ae6bb4cf3c4460ad93a303a7ec4bdc63166ce0796f4025c05d517"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c6bbda9b197a6d0e83923d15d82cee7e0f72cd5d7416dc32dd3bd993c8a8fc4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a16ac20a15f1ff7716824be467f4980b0e3ab1f99326539d9a8b086eece946bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f001a886b5ea456bc925ae37ef45c6f5ae70ef8506ae576fe3f831e78f2ecbcb"
+    sha256 cellar: :any_skip_relocation, monterey:       "08fbf3e6231fec7cafca9fcaa85e498276b12a5866bb4f918384b551a704599e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "db4b6bdf3468476c2f2b6696f755c05b2471f6d09bc47bf783fbd6c65b1b1aac"
+    sha256 cellar: :any_skip_relocation, catalina:       "6b886491460ff6245f9f6ecd22d0a856f51afbd06ec7adf13e2c8be974693656"
+    sha256 cellar: :any_skip_relocation, mojave:         "8d11d70c1a7ae6bb4cf3c4460ad93a303a7ec4bdc63166ce0796f4025c05d517"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c6bbda9b197a6d0e83923d15d82cee7e0f72cd5d7416dc32dd3bd993c8a8fc4"
   end
 
   depends_on "cmake" => :build
