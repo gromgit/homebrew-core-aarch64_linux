46b82830c2fac3bc33007eeff8ec521f11f999d5
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:40:24 +0000

    gsar: update 1.51 bottle.

--- Formula/gsar.rb
+++ Formula/gsar.rb
@@ -15,11 +15,13 @@ class Gsar < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "86de578de3d7754f9f3b6f3f092a262e32e2621f240105a8e983ce2b2afe4f7c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8bd2f9e7a24ec476fd8ae541dbeb60d1a5b93cc48790d28059d43fad27295581"
-    sha256 cellar: :any_skip_relocation, catalina:      "6bba5adb80da7941b6a6a62015eae1ad8d6e27a11678a2d966ca0699ac05eae2"
-    sha256 cellar: :any_skip_relocation, mojave:        "c3bf2fcc08cefb75fb0c8cae257f0b70f85172093649e065860324db6338f43a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "160d27dd1c5497302c7249adb93a43cdb81669cdf5d01a23405adf42d5af16f4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "68051084dfbefccd95074b0e7cf9316fe916bd23e452a8c018b83c2aa185c0e7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "86de578de3d7754f9f3b6f3f092a262e32e2621f240105a8e983ce2b2afe4f7c"
+    sha256 cellar: :any_skip_relocation, monterey:       "7b479c3f3d2d9ab12bacd67ae0792472f803f1c59ea65b1c180bba9c9e8cdbeb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8bd2f9e7a24ec476fd8ae541dbeb60d1a5b93cc48790d28059d43fad27295581"
+    sha256 cellar: :any_skip_relocation, catalina:       "6bba5adb80da7941b6a6a62015eae1ad8d6e27a11678a2d966ca0699ac05eae2"
+    sha256 cellar: :any_skip_relocation, mojave:         "c3bf2fcc08cefb75fb0c8cae257f0b70f85172093649e065860324db6338f43a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "160d27dd1c5497302c7249adb93a43cdb81669cdf5d01a23405adf42d5af16f4"
   end
 
   def install
