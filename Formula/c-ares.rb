e9305a0886e04492c08f35d0042ab0797e2ae9b4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 07:05:16 +0000

    c-ares: update 1.18.0 bottle.

--- Formula/c-ares.rb
+++ Formula/c-ares.rb
@@ -16,10 +16,12 @@ class CAres < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "4b3f66b638dbcbe79f4f4501ea18d8ab8ece1c456dd4224abc832c840b58d25c"
-    sha256 cellar: :any,                 big_sur:       "18830bd78ca245d689bab0c1e339565d285135172524142a316eda0ba278bf52"
-    sha256 cellar: :any,                 catalina:      "50099cd3d317f25932825dec6683b539ceec5c73078b9933819cc9e005dd9952"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68029f1953a85a50394d2a1501aeb07f744281cfb669feaf99b0d0a5b233fa7e"
+    sha256 cellar: :any,                 arm64_monterey: "9795fec1a0b9c71ca86b762fea8a2f8fa85a1633da2017786150e67c50dc0b7d"
+    sha256 cellar: :any,                 arm64_big_sur:  "4b3f66b638dbcbe79f4f4501ea18d8ab8ece1c456dd4224abc832c840b58d25c"
+    sha256 cellar: :any,                 monterey:       "40768dc10237c172151af1215e8081d61238ff82d57438a193088c3060338327"
+    sha256 cellar: :any,                 big_sur:        "18830bd78ca245d689bab0c1e339565d285135172524142a316eda0ba278bf52"
+    sha256 cellar: :any,                 catalina:       "50099cd3d317f25932825dec6683b539ceec5c73078b9933819cc9e005dd9952"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68029f1953a85a50394d2a1501aeb07f744281cfb669feaf99b0d0a5b233fa7e"
   end
 
   depends_on "cmake" => :build
