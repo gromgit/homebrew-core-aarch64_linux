25892ed4c4c6806c0080f4bcdde3a296d3dc6c48
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:20:35 +0000

    hcl2json: update 0.3.3 bottle.

--- Formula/hcl2json.rb
+++ Formula/hcl2json.rb
@@ -7,11 +7,13 @@ class Hcl2json < Formula
   head "https://github.com/tmccombs/hcl2json.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "357414d0b2d4cfd38220d4e9d52b556fc431334f0fcd6d62d5fd3ff97b1b2f14"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
-    sha256 cellar: :any_skip_relocation, catalina:      "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
-    sha256 cellar: :any_skip_relocation, mojave:        "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b1b2da82eecafa95be718e656620b460e6a36bbb5acd8259bc66e41341b32ae0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "188a9a985e274d51d656f19774c7aef180d51c2baa89b6a8cfcf7a1653074ba8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "357414d0b2d4cfd38220d4e9d52b556fc431334f0fcd6d62d5fd3ff97b1b2f14"
+    sha256 cellar: :any_skip_relocation, monterey:       "2bafed673735a5bc368725750c957e03e32096cf941d61e7be22e080c64c6d91"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
+    sha256 cellar: :any_skip_relocation, catalina:       "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
+    sha256 cellar: :any_skip_relocation, mojave:         "9b5fcea7928871e1f18fc048f103fc8be8e3564856c869155d16d41160a1515d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1b2da82eecafa95be718e656620b460e6a36bbb5acd8259bc66e41341b32ae0"
   end
 
   depends_on "go" => :build
