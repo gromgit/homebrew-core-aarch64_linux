63684abf706c236c40c1dff8b14a5ed7ece713e5
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:28:51 +0000

    git-gerrit: update 0.3.0 bottle.

--- Formula/git-gerrit.rb
+++ Formula/git-gerrit.rb
@@ -7,10 +7,12 @@ class GitGerrit < Formula
   head "https://github.com/fbzhong/git-gerrit.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
-    sha256 cellar: :any_skip_relocation, big_sur:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
-    sha256 cellar: :any_skip_relocation, catalina:      "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
-    sha256 cellar: :any_skip_relocation, mojave:        "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
+    sha256 cellar: :any_skip_relocation, monterey:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+    sha256 cellar: :any_skip_relocation, catalina:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+    sha256 cellar: :any_skip_relocation, mojave:         "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
   end
 
   def install
