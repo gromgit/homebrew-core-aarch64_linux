3f8d917ac1789fd68da93e20cc60f6de71ae6a22
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 03:26:58 +0000

    terraform-rover: update 0.2.2 bottle.

--- Formula/terraform-rover.rb
+++ Formula/terraform-rover.rb
@@ -7,11 +7,13 @@ class TerraformRover < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a074f14618b6eb3f4fa1f3538508322aeff9a4aafb6e13176a3ceaab56dcd8ea"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cb2dc4dab8d77371813992ead874a94223269ee42f489d95846a0be627adc9ea"
-    sha256 cellar: :any_skip_relocation, catalina:      "13de4d7c616eb75498f5f633025a6db46cd436d37386a5f94a97d120564fcb72"
-    sha256 cellar: :any_skip_relocation, mojave:        "865c313e1f50c9da29fc570cc602727cf222050b337ab8e61b35ed544be145bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64b4259285d662de26aec694874b0503ec05edc271915f8535c3b1b2afd35080"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5367581968ec927f8b2d8a5c576c5b9a381f45da7e135a03093772cc4d54c1f6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a074f14618b6eb3f4fa1f3538508322aeff9a4aafb6e13176a3ceaab56dcd8ea"
+    sha256 cellar: :any_skip_relocation, monterey:       "052fed203465498fdacfd6d1ee2fe0d14ae4393eedf73cea25496357a6a169a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb2dc4dab8d77371813992ead874a94223269ee42f489d95846a0be627adc9ea"
+    sha256 cellar: :any_skip_relocation, catalina:       "13de4d7c616eb75498f5f633025a6db46cd436d37386a5f94a97d120564fcb72"
+    sha256 cellar: :any_skip_relocation, mojave:         "865c313e1f50c9da29fc570cc602727cf222050b337ab8e61b35ed544be145bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "64b4259285d662de26aec694874b0503ec05edc271915f8535c3b1b2afd35080"
   end
 
   depends_on "go" => :build
