b64c08175dcce46d71f055cab6e4753ce78a24a9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 14:50:58 +0000

    oath-toolkit: update 2.6.6 bottle.

--- Formula/oath-toolkit.rb
+++ Formula/oath-toolkit.rb
@@ -12,11 +12,13 @@ class OathToolkit < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "3e54014feda461a1aa6f68f71570c8be14076aac4a6823345b76b10feba0cf94"
-    sha256 big_sur:       "ed6ceb54edc0b0bea449a75c756b604c6204f6fd80c9e280ce57b1e3d7140ac7"
-    sha256 catalina:      "04c85d25d9c1e8cac2164a4b538344f95181346fd3170e65e43173aca6770b6d"
-    sha256 mojave:        "136fc9c533486f31645fdd6594d96fc8f17487439248b78a8c42a868ce7aaacb"
-    sha256 x86_64_linux:  "3ea5398bb38297c062a54cce0ee803487211cf9f16c7697981d67a3edebd94e6"
+    sha256 cellar: :any, arm64_monterey: "d12d5f53c630491e0db01dd5955e9dd23baeae9080df3f24eb726ccf359aaa97"
+    sha256               arm64_big_sur:  "3e54014feda461a1aa6f68f71570c8be14076aac4a6823345b76b10feba0cf94"
+    sha256               monterey:       "a3b5fca2b9fbe382935b54efa49d56f07bb1f637cc1c17d90418a33ce265e92b"
+    sha256               big_sur:        "ed6ceb54edc0b0bea449a75c756b604c6204f6fd80c9e280ce57b1e3d7140ac7"
+    sha256               catalina:       "04c85d25d9c1e8cac2164a4b538344f95181346fd3170e65e43173aca6770b6d"
+    sha256               mojave:         "136fc9c533486f31645fdd6594d96fc8f17487439248b78a8c42a868ce7aaacb"
+    sha256               x86_64_linux:   "3ea5398bb38297c062a54cce0ee803487211cf9f16c7697981d67a3edebd94e6"
   end
 
   depends_on "pkg-config" => :build
