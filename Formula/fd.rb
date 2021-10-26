f918cd31b3bfaba436294a684d6cfeedb3bfe7bb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 11:11:10 +0000

    fd: update 8.2.1 bottle.

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -7,11 +7,13 @@ class Fd < Formula
   head "https://github.com/sharkdp/fd.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b50a503fc0bddc9c82d6ebc42198071160426ee6247c122f8fb81b1f9ecc4aeb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "378bf3b71edf7c09a80cd8815bd068f6c2b8abaf2df149fc23f33f52acecc817"
-    sha256 cellar: :any_skip_relocation, catalina:      "1fef32a7cd0c80f62343b4caf6a0979f89bacfa7434ed54ffede6adb85ace329"
-    sha256 cellar: :any_skip_relocation, mojave:        "160cdfc22b5d0ac9694ce8dd95f7e22a7bdc95f6d376344d15f924f9ef67149b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d2387b3ee1027e1e6e06050edfd302d7dda1d13d3f4c646408ee57d968f07f5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "91f363707c516476edb8feb37aa9001e044fce571929de9651f7a5bb66f46cb0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b50a503fc0bddc9c82d6ebc42198071160426ee6247c122f8fb81b1f9ecc4aeb"
+    sha256 cellar: :any_skip_relocation, monterey:       "217a04685b72a167001a57a28dfb8270d4d625f90e4c8e9d62492dc616a9713e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "378bf3b71edf7c09a80cd8815bd068f6c2b8abaf2df149fc23f33f52acecc817"
+    sha256 cellar: :any_skip_relocation, catalina:       "1fef32a7cd0c80f62343b4caf6a0979f89bacfa7434ed54ffede6adb85ace329"
+    sha256 cellar: :any_skip_relocation, mojave:         "160cdfc22b5d0ac9694ce8dd95f7e22a7bdc95f6d376344d15f924f9ef67149b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d2387b3ee1027e1e6e06050edfd302d7dda1d13d3f4c646408ee57d968f07f5"
   end
 
   depends_on "rust" => :build
