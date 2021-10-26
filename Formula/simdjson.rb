177a6fb475de1965ac4be73bcc15b1af145c8427
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:12:25 +0000

    simdjson: update 1.0.1 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -7,10 +7,12 @@ class Simdjson < Formula
   head "https://github.com/simdjson/simdjson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b14f67b6f82b861329cfbcb28585f889a73285a9a0f3851e5e51c3c180e73908"
-    sha256 cellar: :any,                 big_sur:       "54906dad9a15dbe6cfefe33a28c167b0964b64b2413126bcf18ed0c0fc40a733"
-    sha256 cellar: :any,                 catalina:      "4017b4e21af561ee3f4929e12baa338a947cdb55767a00194c00fb74a31f5277"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc6f207f238e8f57504bf9e4299d931bdcdcb72301e27df41d525879e936a27b"
+    sha256 cellar: :any,                 arm64_monterey: "695a571b0a3006b806f0bf5713fd6b753a10c0e764f3babebbb5b3fc23338e80"
+    sha256 cellar: :any,                 arm64_big_sur:  "b14f67b6f82b861329cfbcb28585f889a73285a9a0f3851e5e51c3c180e73908"
+    sha256 cellar: :any,                 monterey:       "584b19f49f0cbf90174aba32a015895d91aedcc1472e6d53a93ef63465c10afd"
+    sha256 cellar: :any,                 big_sur:        "54906dad9a15dbe6cfefe33a28c167b0964b64b2413126bcf18ed0c0fc40a733"
+    sha256 cellar: :any,                 catalina:       "4017b4e21af561ee3f4929e12baa338a947cdb55767a00194c00fb74a31f5277"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc6f207f238e8f57504bf9e4299d931bdcdcb72301e27df41d525879e936a27b"
   end
 
   depends_on "cmake" => :build
