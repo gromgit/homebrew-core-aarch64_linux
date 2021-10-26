707ed1f95e13f45227af351f02e7652d3767310f
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 06:35:19 +0000

    sqlparse: update 0.4.2_1 bottle.

--- Formula/sqlparse.rb
+++ Formula/sqlparse.rb
@@ -9,11 +9,13 @@ class Sqlparse < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5917e87d404c93aa04ce10b94b1d036e1b7302b32e118931d86799022f22f671"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
-    sha256 cellar: :any_skip_relocation, catalina:      "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
-    sha256 cellar: :any_skip_relocation, mojave:        "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76b4d21dec27b2311986f15e9b279a8ac0cefe6bc5d91a94733ded3746cf3c90"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5917e87d404c93aa04ce10b94b1d036e1b7302b32e118931d86799022f22f671"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5917e87d404c93aa04ce10b94b1d036e1b7302b32e118931d86799022f22f671"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, catalina:       "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, mojave:         "2b655782b25db3c6de626342cc640acdf2226540935546a1e947c4bc064c969c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76b4d21dec27b2311986f15e9b279a8ac0cefe6bc5d91a94733ded3746cf3c90"
   end
 
   depends_on "python@3.10"
