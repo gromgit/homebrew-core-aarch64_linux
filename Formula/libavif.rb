346528e9a640f39fde01cdeb5a9eb29141dfc483
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:36:44 +0000

    libavif: update 0.9.3 bottle.

--- Formula/libavif.rb
+++ Formula/libavif.rb
@@ -6,10 +6,12 @@ class Libavif < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "0e64657e9f2d5ea82d0c66aaf761cfbb7d39e245590fbdcc603743f5280313c3"
-    sha256 cellar: :any,                 big_sur:       "9d11f6321b3889671d683e86ebb6db03716142cdc16f0a95ce5e761ba31ab258"
-    sha256 cellar: :any,                 catalina:      "f9611aacd9b0decd5ef0fbbccc67119cea5603eadfd4430ceac556f54723945e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84b065490127c11a9b8e5fe9263d270ca9751e9cb7962bb5a5ccf6ff2f3c4c08"
+    sha256 cellar: :any,                 arm64_monterey: "89ae2754b605c1a5dd35135ebfbf8e0d0970029fb617c8019c9566ad3da600ed"
+    sha256 cellar: :any,                 arm64_big_sur:  "0e64657e9f2d5ea82d0c66aaf761cfbb7d39e245590fbdcc603743f5280313c3"
+    sha256 cellar: :any,                 monterey:       "8c48b4c95b0df48df71fc8de3e924cfad0dd06c886cd9a69419a6affd7a76e7a"
+    sha256 cellar: :any,                 big_sur:        "9d11f6321b3889671d683e86ebb6db03716142cdc16f0a95ce5e761ba31ab258"
+    sha256 cellar: :any,                 catalina:       "f9611aacd9b0decd5ef0fbbccc67119cea5603eadfd4430ceac556f54723945e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84b065490127c11a9b8e5fe9263d270ca9751e9cb7962bb5a5ccf6ff2f3c4c08"
   end
 
   depends_on "cmake" => :build
