bc7d37f50bb4a0db7bfcc0ecdc4ad8f36de3d457
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:46:07 +0000

    fanyi: update 5.1.1 bottle.

--- Formula/fanyi.rb
+++ Formula/fanyi.rb
@@ -8,10 +8,12 @@ class Fanyi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bdb5e4c6f216c41688c1e6935433006195eab8846bed9024a332537e73ca2dd6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5548268ec8a85f156de8612bdd8e236d7495fc8e5e3f7f9c96e05b4d54305eff"
-    sha256 cellar: :any_skip_relocation, catalina:      "5548268ec8a85f156de8612bdd8e236d7495fc8e5e3f7f9c96e05b4d54305eff"
-    sha256 cellar: :any_skip_relocation, mojave:        "5548268ec8a85f156de8612bdd8e236d7495fc8e5e3f7f9c96e05b4d54305eff"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5b79858ee2720463b8165a60cecf7544dbfbb64a92d7af18dec4526fc69b905c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bdb5e4c6f216c41688c1e6935433006195eab8846bed9024a332537e73ca2dd6"
+    sha256 cellar: :any_skip_relocation, monterey:       "2ed67703f3155a7807dcb2c3247fb6e4b81fb154c7c19f3c04a8f5ea83727407"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5548268ec8a85f156de8612bdd8e236d7495fc8e5e3f7f9c96e05b4d54305eff"
+    sha256 cellar: :any_skip_relocation, catalina:       "5548268ec8a85f156de8612bdd8e236d7495fc8e5e3f7f9c96e05b4d54305eff"
+    sha256 cellar: :any_skip_relocation, mojave:         "5548268ec8a85f156de8612bdd8e236d7495fc8e5e3f7f9c96e05b4d54305eff"
   end
 
   depends_on "node"
