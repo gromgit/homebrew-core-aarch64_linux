cd7d28597b1775a8159befb44428ce5cad84cabd
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:22:04 +0000

    csvtk: update 0.23.0 bottle.

--- Formula/csvtk.rb
+++ Formula/csvtk.rb
@@ -6,10 +6,12 @@ class Csvtk < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "85d884da7d8fd0d6ed0d53eabeccdb900baeec93522b3f817f3dfb1c8a9e2d18"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f2869fb31ea0a291dff303db08ee8387cfab96ee7e77fe98c82511c6b5805613"
-    sha256 cellar: :any_skip_relocation, catalina:      "1d9d5d3ecf0c81b1162f88813f871265ea174ae2535624ad345c1e52364ca692"
-    sha256 cellar: :any_skip_relocation, mojave:        "0e0b648de5a24b410a9d56d663668741cb7429e1295ed1cc120014aaa516c961"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb893a0139d975c854d38b7002391a413f7e8437e2e077a6ddbe7f0860f5c756"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85d884da7d8fd0d6ed0d53eabeccdb900baeec93522b3f817f3dfb1c8a9e2d18"
+    sha256 cellar: :any_skip_relocation, monterey:       "869f5b931d4e0c089dc4a05c1dde8db0dce77004b41ff115e0db3dd613d3a545"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f2869fb31ea0a291dff303db08ee8387cfab96ee7e77fe98c82511c6b5805613"
+    sha256 cellar: :any_skip_relocation, catalina:       "1d9d5d3ecf0c81b1162f88813f871265ea174ae2535624ad345c1e52364ca692"
+    sha256 cellar: :any_skip_relocation, mojave:         "0e0b648de5a24b410a9d56d663668741cb7429e1295ed1cc120014aaa516c961"
   end
 
   depends_on "go" => :build
