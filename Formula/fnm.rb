3060e093cee3ad76a4d54b810ecd726a2117005d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 14:18:47 +0000

    fnm: update 1.27.0 bottle.

--- Formula/fnm.rb
+++ Formula/fnm.rb
@@ -12,11 +12,13 @@ class Fnm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8a7b6d36aa0cf64fb42f99a57686c7e771690b6b97eb19f6e65486365b9e262d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1ed1913f1c9f70bcc703bac59e9e46540cb2030c5e42e0fb8cfe42eb1754ee75"
-    sha256 cellar: :any_skip_relocation, catalina:      "3195625f6b556c8eeee0894ba516b7a91a7fddd1fd31090867b942bcb583e917"
-    sha256 cellar: :any_skip_relocation, mojave:        "9921bb97be053afe96cb9a18bdcfd95fb34faa9a00dee89d0735938b52ea0a40"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a237dc5807cb312d8cde5f0d937046cc10a4c58d3d941d82064fe6fa49f0b96"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "86b8692f33637fe310b36744e43477fe968631fb3e06b22c2d40399277b1e18e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a7b6d36aa0cf64fb42f99a57686c7e771690b6b97eb19f6e65486365b9e262d"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0473cfa5f4dd2f56ca25cbd06a80af862d5641b5b8481c26012aca73f619659"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1ed1913f1c9f70bcc703bac59e9e46540cb2030c5e42e0fb8cfe42eb1754ee75"
+    sha256 cellar: :any_skip_relocation, catalina:       "3195625f6b556c8eeee0894ba516b7a91a7fddd1fd31090867b942bcb583e917"
+    sha256 cellar: :any_skip_relocation, mojave:         "9921bb97be053afe96cb9a18bdcfd95fb34faa9a00dee89d0735938b52ea0a40"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a237dc5807cb312d8cde5f0d937046cc10a4c58d3d941d82064fe6fa49f0b96"
   end
 
   depends_on "rust" => :build
