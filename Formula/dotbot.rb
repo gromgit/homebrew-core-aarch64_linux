cfeddaf44ab5b678660645689125b5bd352dd43d
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 05:43:25 +0000

    dotbot: update 1.19.0_1 bottle.

--- Formula/dotbot.rb
+++ Formula/dotbot.rb
@@ -9,11 +9,13 @@ class Dotbot < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1e5418a009f3e05c563ada0705c6e077d56e4df49ab42f1496ef43e097dc6f1d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4eed817693537664519d582af946fa870d40c8c584b9706d9c4742ebe13ba8e4"
-    sha256 cellar: :any_skip_relocation, catalina:      "1580a56ae5c22273b996866519a938da04c8fec7adc805f435399b954bebb9df"
-    sha256 cellar: :any_skip_relocation, mojave:        "fdb6be14bd6a4ad73c2ca13c3f5a62cc74bb64ff507278a99d4e0b944e09dd75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b0e8e2840e9c40ac40da72cf275aebbc2365a21a4874b8c040109667e1c004b6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8e4681de4209b0622d9523124931dc7f8ead206d8768e8b934af4bce1149ee1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e5418a009f3e05c563ada0705c6e077d56e4df49ab42f1496ef43e097dc6f1d"
+    sha256 cellar: :any_skip_relocation, monterey:       "b7cf1d41236d1358f46b52827a24c323311e151839391a808472032ef6c3e1b7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4eed817693537664519d582af946fa870d40c8c584b9706d9c4742ebe13ba8e4"
+    sha256 cellar: :any_skip_relocation, catalina:       "1580a56ae5c22273b996866519a938da04c8fec7adc805f435399b954bebb9df"
+    sha256 cellar: :any_skip_relocation, mojave:         "fdb6be14bd6a4ad73c2ca13c3f5a62cc74bb64ff507278a99d4e0b944e09dd75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0e8e2840e9c40ac40da72cf275aebbc2365a21a4874b8c040109667e1c004b6"
   end
 
   depends_on "python@3.10"
