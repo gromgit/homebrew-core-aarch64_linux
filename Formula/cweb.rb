a157fd42c34c4bdc853ed33f852c0fb224809cac
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:28:21 +0000

    cweb: update 3.64c bottle.

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -6,11 +6,13 @@ class Cweb < Formula
   sha256 "efbd6fbeca9b3e75629b69e9565ac6a0e4067f55bda6a0a3b7b6f9449d9ed81f"
 
   bottle do
-    sha256 arm64_big_sur: "9a1237298f33f69283f923525eb5668911226d48043024bbbd44d337ad1bcdbe"
-    sha256 big_sur:       "2ee6617cf9da76e1e7cafbd49ed6e53fee15339c82df053eab55a398fb96f50b"
-    sha256 catalina:      "e100640669a1d066177514aae1c813f7c18b530c4cae5744d0431d850933648c"
-    sha256 mojave:        "410376faad622cd11745ca94c877135c1b4837ccdc8c7cab43bf12b4b849a3b9"
-    sha256 x86_64_linux:  "62cac91ee39b5981ac30e4797a75b76572b4ae04c539796d120f4fbe36fc9716"
+    sha256 arm64_monterey: "bf4fa48bb4855f8424de024bfdafccfd3ed28fade1485a8526ee5e6cfb80b509"
+    sha256 arm64_big_sur:  "9a1237298f33f69283f923525eb5668911226d48043024bbbd44d337ad1bcdbe"
+    sha256 monterey:       "de7c0543066179c97ebb5316d6bcfa8d6ecb7ca00e62507fda2d160cb019ed71"
+    sha256 big_sur:        "2ee6617cf9da76e1e7cafbd49ed6e53fee15339c82df053eab55a398fb96f50b"
+    sha256 catalina:       "e100640669a1d066177514aae1c813f7c18b530c4cae5744d0431d850933648c"
+    sha256 mojave:         "410376faad622cd11745ca94c877135c1b4837ccdc8c7cab43bf12b4b849a3b9"
+    sha256 x86_64_linux:   "62cac91ee39b5981ac30e4797a75b76572b4ae04c539796d120f4fbe36fc9716"
   end
 
   def install
