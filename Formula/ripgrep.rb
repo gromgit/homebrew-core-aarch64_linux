74439f5f9009d61838a6dc1e908d4219d04a95f6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 07:14:37 +0000

    ripgrep: update 13.0.0 bottle.

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -14,6 +14,7 @@ class Ripgrep < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "ea67bdf39c1727b2b6b5b5f5110d1736b42b3eb0ec2068aa99903b670545b015"
     sha256 cellar: :any,                 arm64_big_sur:  "d3e0ae859dc1e66ebecbc66a8ad1ec2abac59bc707d2305dde66212e71406d36"
+    sha256 cellar: :any,                 monterey:       "db0260c6be9656c54575a4d82c720fb24141be38a17350d6cddba7be14f104e6"
     sha256 cellar: :any,                 big_sur:        "a8f2bd6586de9f7aa36eaaefd36777309f9b5d57f01bf33bf022d715fd3dbb89"
     sha256 cellar: :any,                 catalina:       "0edcffa1251002e2747020d62a16ae077bd7aa5fb289d351622e0065c9686c40"
     sha256 cellar: :any,                 mojave:         "b57024c0d221249a1f5eaef1069ac90d44e54afdadb146acd117ae23b7de98c6"
