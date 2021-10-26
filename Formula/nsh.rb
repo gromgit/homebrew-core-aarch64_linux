f0f54d54e51e48be74bda16609e836f826b1756f
rui <rui@chenrui.dev>
2021-10-26 00:08:57 +0000

    nsh: add 0.4.2 bottle.

--- Formula/nsh.rb
+++ Formula/nsh.rb
@@ -6,6 +6,13 @@ class Nsh < Formula
   license any_of: ["CC0-1.0", "MIT"]
   head "https://github.com/nuta/nsh.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9025b069d64133a40f5e1c5f1ad35178ef47a601d321da10749323dc54f48f3c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8454140462f3968d3c29b64ab2248faa2fb544a491c3e3f2291da24f60801dac"
+    sha256 cellar: :any_skip_relocation, catalina:      "04010c69722c2060dd83c66a1b30281014126248a4586b5c0959ff5bbfdc00e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "131188c37bdb5c53e7cdad97e1c24ae13e0ae361636a883698521bee40c699d3"
+  end
+
   depends_on "rust" => :build
 
   def install
