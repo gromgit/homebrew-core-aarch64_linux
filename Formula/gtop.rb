04c63b5edb53ce233803846b29378e97d8c31164
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 03:49:29 +0000

    gtop: update 1.1.1 bottle.

--- Formula/gtop.rb
+++ Formula/gtop.rb
@@ -8,11 +8,13 @@ class Gtop < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e5adce5d3f299b11b5a5278e89c8201eb5cc4f2617b1ef88f07020a33b1df655"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
-    sha256 cellar: :any_skip_relocation, catalina:      "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
-    sha256 cellar: :any_skip_relocation, mojave:        "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5adce5d3f299b11b5a5278e89c8201eb5cc4f2617b1ef88f07020a33b1df655"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d8dd667ec7922211b271a4392d7b4040748b9741606d89d74e5ef62a201a1b4a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e5adce5d3f299b11b5a5278e89c8201eb5cc4f2617b1ef88f07020a33b1df655"
+    sha256 cellar: :any_skip_relocation, monterey:       "e91e34957b1c18263e1ca702de14758014ef3c81196c5225c220ffd6b8f9bb95"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
+    sha256 cellar: :any_skip_relocation, catalina:       "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
+    sha256 cellar: :any_skip_relocation, mojave:         "d155b9fe2f75deabbd70c7cb6f07ac09cb69158a770b6ed8cca07d6d12cc7e60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e5adce5d3f299b11b5a5278e89c8201eb5cc4f2617b1ef88f07020a33b1df655"
   end
 
   depends_on "node"
