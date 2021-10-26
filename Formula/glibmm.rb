93e21ec27d67725abad688827ae7cc6b257ee126
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-26 14:12:27 +0000

    glibmm: update 2.70.0 bottle.

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -6,11 +6,13 @@ class Glibmm < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "85cf4f30e430474bb31b4d40331658d4abbbd6656b0fd545e83757289863545c"
-    sha256 cellar: :any, big_sur:       "edf7767c19d7af3fa0029a77b900b160856bfc1d65b789e58ad9001c40cd6552"
-    sha256 cellar: :any, catalina:      "371c5642bb40285423561696728f0bb760197d5971961819e82846f452d0659b"
-    sha256 cellar: :any, mojave:        "6b17198a3468451bf0019b02dbeffe9fa0907876964b63981cebaf53966dcbf8"
-    sha256               x86_64_linux:  "f000a536bbbfc6100c9fd63f8b7ec6e674867ab75e86e8132da1ef2b3c409988"
+    sha256 cellar: :any, arm64_monterey: "1ebb6889448d05a7fa6772588946d090464ba3acca808692d72ab32d2e6304cf"
+    sha256 cellar: :any, arm64_big_sur:  "85cf4f30e430474bb31b4d40331658d4abbbd6656b0fd545e83757289863545c"
+    sha256 cellar: :any, monterey:       "0a8cf3b36eef323ccfee7b4489c4783ecf4afb573ee8a74b692c36eb82801152"
+    sha256 cellar: :any, big_sur:        "edf7767c19d7af3fa0029a77b900b160856bfc1d65b789e58ad9001c40cd6552"
+    sha256 cellar: :any, catalina:       "371c5642bb40285423561696728f0bb760197d5971961819e82846f452d0659b"
+    sha256 cellar: :any, mojave:         "6b17198a3468451bf0019b02dbeffe9fa0907876964b63981cebaf53966dcbf8"
+    sha256               x86_64_linux:   "f000a536bbbfc6100c9fd63f8b7ec6e674867ab75e86e8132da1ef2b3c409988"
   end
 
   depends_on "meson" => :build
