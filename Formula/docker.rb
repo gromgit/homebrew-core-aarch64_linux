d2eb86a5f7099e7532d5cac9ef215132666fbfc2
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 00:08:05 +0000

    docker: update 20.10.10 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -13,10 +13,12 @@ class Docker < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "39595e1525b94f6014e4f691a909ce3ef3a038970624ed1acec9ce81d95d8b59"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9a35f8e87a2e6c4ad4d6ed8d423b825274ca85fd048613a6bc3277bdfcd158aa"
-    sha256 cellar: :any_skip_relocation, catalina:      "ac90e89757152b6a0ec0fe1d6d69cfde14be882a1d9758a78de0cec1e9576c65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6192c27351b82e5acf0652d88c30bea8e48d93046082b2743e6be98393f443c8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d7fa41aea6c5d1b0f0f38bcc3bcc6b4107cba0b4e5a0b091200905232ed4c3a4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39595e1525b94f6014e4f691a909ce3ef3a038970624ed1acec9ce81d95d8b59"
+    sha256 cellar: :any_skip_relocation, monterey:       "c6ce1c4a66a9856862d791aaf27512b81c6b312c44ef434f56edcfc842ccd8a5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9a35f8e87a2e6c4ad4d6ed8d423b825274ca85fd048613a6bc3277bdfcd158aa"
+    sha256 cellar: :any_skip_relocation, catalina:       "ac90e89757152b6a0ec0fe1d6d69cfde14be882a1d9758a78de0cec1e9576c65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6192c27351b82e5acf0652d88c30bea8e48d93046082b2743e6be98393f443c8"
   end
 
   depends_on "go" => :build
