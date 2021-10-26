3a830642d7ecf356d50271fecef5329b6774ba48
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-26 15:15:02 +0000

    autodiff: update 0.6.4 bottle.

--- Formula/autodiff.rb
+++ Formula/autodiff.rb
@@ -7,10 +7,12 @@ class Autodiff < Formula
   head "https://github.com/autodiff/autodiff.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cb8dd848ff068721e8c8d0d58e7b62e031ad0eebacfea36d693bc7528264d5be"
-    sha256 cellar: :any_skip_relocation, big_sur:       "12bab4a6f997bd961b1b01c0d37a68f49b50ed5419940dec018dae4837bb5f31"
-    sha256 cellar: :any_skip_relocation, catalina:      "109cfe02558a837c81ec22bd9f77a9a7a9d63fa0f61d4dd21d2a3a8622161f74"
-    sha256 cellar: :any_skip_relocation, mojave:        "6485a71ec52617ecf4ecca6f435376354877896ba21a6b5d5d3514e6da996d97"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5105ff070861435bbcb5d9d0d7a60dd1152cd53e4a81073d8715586513fc2da5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cb8dd848ff068721e8c8d0d58e7b62e031ad0eebacfea36d693bc7528264d5be"
+    sha256 cellar: :any_skip_relocation, monterey:       "583df3c20e81bf85eea85e60554b50d1d9253358dda911fd10c5e3c891de9690"
+    sha256 cellar: :any_skip_relocation, big_sur:        "12bab4a6f997bd961b1b01c0d37a68f49b50ed5419940dec018dae4837bb5f31"
+    sha256 cellar: :any_skip_relocation, catalina:       "109cfe02558a837c81ec22bd9f77a9a7a9d63fa0f61d4dd21d2a3a8622161f74"
+    sha256 cellar: :any_skip_relocation, mojave:         "6485a71ec52617ecf4ecca6f435376354877896ba21a6b5d5d3514e6da996d97"
   end
 
   depends_on "cmake" => :build
