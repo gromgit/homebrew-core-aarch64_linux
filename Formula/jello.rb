f2e52f465e340bfc7d4eb6411498f469353f49c8
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 06:08:00 +0000

    jello: update 1.4.4_1 bottle.

--- Formula/jello.rb
+++ Formula/jello.rb
@@ -9,10 +9,12 @@ class Jello < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2cf89b26555543aa7ee63d93e02df14d260fc06ba1dffb9ece2c070fadaaa6f4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
-    sha256 cellar: :any_skip_relocation, catalina:      "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
-    sha256 cellar: :any_skip_relocation, mojave:        "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2cf89b26555543aa7ee63d93e02df14d260fc06ba1dffb9ece2c070fadaaa6f4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2cf89b26555543aa7ee63d93e02df14d260fc06ba1dffb9ece2c070fadaaa6f4"
+    sha256 cellar: :any_skip_relocation, monterey:       "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
+    sha256 cellar: :any_skip_relocation, catalina:       "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
+    sha256 cellar: :any_skip_relocation, mojave:         "e1c991340147788989124b846af28bfa283dccf1af47fcdf9c03f2fd2bd0299c"
   end
 
   depends_on "python@3.10"
