dadc5786ec0275d653e3bf648b8c4d00dbea81e9
rui <rui@chenrui.dev>
2021-10-26 15:33:49 +0000

    angle-grinder: update 0.18.0 bottle.

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -6,11 +6,13 @@ class AngleGrinder < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "116d320e843719451ec991b97c2bb8c1aa2dea3623f387c866f12a718b512993"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d94050232f3ce01eb97c1c36f79aa5d2734b9e4e25ede7d81e32eaaeca4a4db7"
-    sha256 cellar: :any_skip_relocation, catalina:      "f41ebbb8078940ecc259ffac8451f70dc49df53a8d8f1fcafe4b02bc6723dcbc"
-    sha256 cellar: :any_skip_relocation, mojave:        "73c19bc8e8e2697797e46ba6e6dd8a24deaa77a8059f0546bf29bffba5e0c8ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afe0edc8a8c223a9bfdbd0c515701e5103a4b4b9b168da747d991eb487dec25a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aac94d72d3e7b33100dec2efdcf5e77678f81f424ab8d408ae2f044214c59f7f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "116d320e843719451ec991b97c2bb8c1aa2dea3623f387c866f12a718b512993"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b1e7b6bfef8586378fb75138e655a70717ccc2be44c9023421e7775a83e9bd2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d94050232f3ce01eb97c1c36f79aa5d2734b9e4e25ede7d81e32eaaeca4a4db7"
+    sha256 cellar: :any_skip_relocation, catalina:       "f41ebbb8078940ecc259ffac8451f70dc49df53a8d8f1fcafe4b02bc6723dcbc"
+    sha256 cellar: :any_skip_relocation, mojave:         "73c19bc8e8e2697797e46ba6e6dd8a24deaa77a8059f0546bf29bffba5e0c8ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "afe0edc8a8c223a9bfdbd0c515701e5103a4b4b9b168da747d991eb487dec25a"
   end
 
   depends_on "rust" => :build
