67127bac958df38bc5ad978d4b9e910a8a20cc9d
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-26 05:44:17 +0000

    epr: update 2.4.11_1 bottle.

--- Formula/epr.rb
+++ Formula/epr.rb
@@ -10,10 +10,12 @@ class Epr < Formula
   head "https://github.com/wustho/epr.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "99d91cc2d45112cc563aa030bbb735bf2fa55b0bd3fc81f664ea6bb842fda5c9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
-    sha256 cellar: :any_skip_relocation, catalina:      "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
-    sha256 cellar: :any_skip_relocation, mojave:        "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "99d91cc2d45112cc563aa030bbb735bf2fa55b0bd3fc81f664ea6bb842fda5c9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99d91cc2d45112cc563aa030bbb735bf2fa55b0bd3fc81f664ea6bb842fda5c9"
+    sha256 cellar: :any_skip_relocation, monterey:       "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
+    sha256 cellar: :any_skip_relocation, catalina:       "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
+    sha256 cellar: :any_skip_relocation, mojave:         "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
   end
 
   depends_on "python@3.10"
