bf53bf7361580b37c91d5ab4b506e5e54db2d806
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 15:18:38 +0000

    gromacs: update 2021.3 bottle.

--- Formula/gromacs.rb
+++ Formula/gromacs.rb
@@ -13,6 +13,7 @@ class Gromacs < Formula
   bottle do
     rebuild 1
     sha256 arm64_big_sur: "7eb394d589b30f8842352abe3b0fea9916c4b8105a44e8f1fbfb3936b583f7fc"
+    sha256 monterey:      "71f8e1c418794542f18c1d56af3f3a008a1fc5cf59c00532596448609cd1b69c"
     sha256 big_sur:       "fdc9c850650ffe6509c8e7e28d6ed4e92de5b967f3ac4ccc3ec5a2feb4aea8fd"
     sha256 catalina:      "6af6a5ec7c57941799723a3b8777619932105e486b9ffa75920d996123ae9b61"
     sha256 mojave:        "a01c61b1eb54a9d11d056dce98b60e048dcf4fb744d5c681f3b517f2c5bcb71b"
