3b45bc5bb95e606c3f9220b7870858cb5713ba5a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 18:13:46 +0000

    flow-cli: update 0.28.3 bottle.

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -12,11 +12,13 @@ class FlowCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b7065ee1fdbf6a485f4f6f8cad0d085b59b4106828cdf342605c4143180bbb1a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b45fa75b7e3172179887e9f1e3531a4c62fc0bad81104ee264647a3d16202fcb"
-    sha256 cellar: :any_skip_relocation, catalina:      "fcc81ea6b689f6e574d56b43167bb94491bc64605e38291ae605e1edad2996c4"
-    sha256 cellar: :any_skip_relocation, mojave:        "ab1360c77c909e3952e33c1c62083ae7df639964cdaafa5ad4bb04810540de26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "deda493010c94063da14e54251bb5842fd0e3cefee638bf24e0d0aa58931265f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "366d252e7ed8620b706977c02538f7f3bee931265a18aa16eabeba7e0be7734c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7065ee1fdbf6a485f4f6f8cad0d085b59b4106828cdf342605c4143180bbb1a"
+    sha256 cellar: :any_skip_relocation, monterey:       "96b0e0222cb752833f08f40df9329309c26283b42ec6a56f19639fefcfb3962b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b45fa75b7e3172179887e9f1e3531a4c62fc0bad81104ee264647a3d16202fcb"
+    sha256 cellar: :any_skip_relocation, catalina:       "fcc81ea6b689f6e574d56b43167bb94491bc64605e38291ae605e1edad2996c4"
+    sha256 cellar: :any_skip_relocation, mojave:         "ab1360c77c909e3952e33c1c62083ae7df639964cdaafa5ad4bb04810540de26"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "deda493010c94063da14e54251bb5842fd0e3cefee638bf24e0d0aa58931265f"
   end
 
   depends_on "go" => :build
