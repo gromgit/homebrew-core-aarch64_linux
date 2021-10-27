e98a167a9f57be937a044c486e8f07c8f2711b42
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:02:44 +0000

    gopass-jsonapi: update 1.11.1 bottle.

--- Formula/gopass-jsonapi.rb
+++ Formula/gopass-jsonapi.rb
@@ -6,10 +6,12 @@ class GopassJsonapi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "866c68f3fd44e4245febcee728c110d2fd2651514890eb7b4a31e6c5e3d429dc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "57b58bc0327828830342b5268360f7f8b9c4980e3a07f1adac51d380f8241677"
-    sha256 cellar: :any_skip_relocation, catalina:      "d8d10b1fb7fa79a18f6fa2949ce1bccc2582bd9628e46e43da34e56a5eedc4a1"
-    sha256 cellar: :any_skip_relocation, mojave:        "9d9a8953da2f0777cef157a69c2784f46f3c5873fbb14f61dedffabce1074153"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5b029bea2be7e8ac0019e06a5483a3593e6b741c0b85b69e99f3625bbaa1f10e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "866c68f3fd44e4245febcee728c110d2fd2651514890eb7b4a31e6c5e3d429dc"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ac1be40eec6bd6436c1f0ffc38fca10fdc1d4e57619d79dfbd6c395e8f2e44e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "57b58bc0327828830342b5268360f7f8b9c4980e3a07f1adac51d380f8241677"
+    sha256 cellar: :any_skip_relocation, catalina:       "d8d10b1fb7fa79a18f6fa2949ce1bccc2582bd9628e46e43da34e56a5eedc4a1"
+    sha256 cellar: :any_skip_relocation, mojave:         "9d9a8953da2f0777cef157a69c2784f46f3c5873fbb14f61dedffabce1074153"
   end
 
   depends_on "go" => :build
