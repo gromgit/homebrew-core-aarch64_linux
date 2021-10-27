46cc52b43cdc980d6078b2b6da72d595effe1ea7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:32:38 +0000

    moar: update 1.8.3 bottle.

--- Formula/moar.rb
+++ Formula/moar.rb
@@ -6,11 +6,13 @@ class Moar < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ce50007be900baf5bed2cf7891350b6f855ae6072ccc98b4c195503cc6a2fb2d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
-    sha256 cellar: :any_skip_relocation, catalina:      "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
-    sha256 cellar: :any_skip_relocation, mojave:        "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26fe3f837b290f636ad3c11c3e47f17b238c5761857e98967e7aff7fac9850a0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce50007be900baf5bed2cf7891350b6f855ae6072ccc98b4c195503cc6a2fb2d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ce50007be900baf5bed2cf7891350b6f855ae6072ccc98b4c195503cc6a2fb2d"
+    sha256 cellar: :any_skip_relocation, monterey:       "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
+    sha256 cellar: :any_skip_relocation, catalina:       "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
+    sha256 cellar: :any_skip_relocation, mojave:         "bed1030e76010d17f5279388b91808ec5b26e6fb58e85231c35d1bb4a98841df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26fe3f837b290f636ad3c11c3e47f17b238c5761857e98967e7aff7fac9850a0"
   end
 
   depends_on "go" => :build
