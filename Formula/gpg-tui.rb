ca653af64c27c6abdc99eb1804efa03eb4d23aec
rui <rui@chenrui.dev>
2021-10-27 05:53:26 +0000

    gpg-tui: update 0.8.1 bottle.

--- Formula/gpg-tui.rb
+++ Formula/gpg-tui.rb
@@ -6,10 +6,12 @@ class GpgTui < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "76f4694e7e26315597a7b5ca3f7c5d115128f859d2693eded12b732f4b61e99f"
-    sha256 cellar: :any, big_sur:       "db37af3c36e7b10f326d2ed5c8d5bddafc07203e1c846e2706f43356a22cfaab"
-    sha256 cellar: :any, catalina:      "c95fbfb237ea576e25e5ddf56efa1044c994279d9cbf53460153b551e8eef1ed"
-    sha256 cellar: :any, mojave:        "1dc1aa3b1a3a7f13c431ff73d06a4bb98ada95e58c3f8db908202932e269274e"
+    sha256 cellar: :any, arm64_monterey: "89e9f24b67d35358860b93d0ca33b1a78a3d3fcfb18a431306e4a196740a4ccd"
+    sha256 cellar: :any, arm64_big_sur:  "76f4694e7e26315597a7b5ca3f7c5d115128f859d2693eded12b732f4b61e99f"
+    sha256 cellar: :any, monterey:       "a048df27a97c9dfed8cb2876baeb0b4da1be5561485bf0a03ec1a290cce07751"
+    sha256 cellar: :any, big_sur:        "db37af3c36e7b10f326d2ed5c8d5bddafc07203e1c846e2706f43356a22cfaab"
+    sha256 cellar: :any, catalina:       "c95fbfb237ea576e25e5ddf56efa1044c994279d9cbf53460153b551e8eef1ed"
+    sha256 cellar: :any, mojave:         "1dc1aa3b1a3a7f13c431ff73d06a4bb98ada95e58c3f8db908202932e269274e"
   end
 
   depends_on "rust" => :build
