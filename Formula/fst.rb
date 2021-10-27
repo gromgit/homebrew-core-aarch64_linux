5b795c3f8293b054688422ed43b6cfdb7254ebdc
rui <rui@chenrui.dev>
2021-10-27 05:23:17 +0000

    fst: update 0.4.2 bottle.

--- Formula/fst.rb
+++ Formula/fst.rb
@@ -12,10 +12,12 @@ class Fst < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "04671c602275ab8d9bcf8525b94653f1953485cbb0b8a0da5fd9710b1e8125e3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "94a9f50588d2e816c45bcb162f03af288b86cc685eeffed746ae1fa3270c8ee2"
-    sha256 cellar: :any_skip_relocation, catalina:      "ea8b25fc75d04973f1b9d19fbc667ddef74c452321a17bf72e705a401225956c"
-    sha256 cellar: :any_skip_relocation, mojave:        "7724e05645b4567b4e464c14d1631e5bd99381602d8a24442beeb72db0483732"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4702973771c36c5600662a537e317c6bc33f3e968df217799273ec8930e662f1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "04671c602275ab8d9bcf8525b94653f1953485cbb0b8a0da5fd9710b1e8125e3"
+    sha256 cellar: :any_skip_relocation, monterey:       "03660a8c66110b0e2216dce9e28b15a0f668de09644bd273426fd34635055d8c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "94a9f50588d2e816c45bcb162f03af288b86cc685eeffed746ae1fa3270c8ee2"
+    sha256 cellar: :any_skip_relocation, catalina:       "ea8b25fc75d04973f1b9d19fbc667ddef74c452321a17bf72e705a401225956c"
+    sha256 cellar: :any_skip_relocation, mojave:         "7724e05645b4567b4e464c14d1631e5bd99381602d8a24442beeb72db0483732"
   end
 
   depends_on "rust" => :build
