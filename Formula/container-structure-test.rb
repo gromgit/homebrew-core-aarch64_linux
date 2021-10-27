1fca5b7307408cf4f2c8c29d75343f77476efe50
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:42:39 +0000

    container-structure-test: update 1.10.0 bottle.

--- Formula/container-structure-test.rb
+++ Formula/container-structure-test.rb
@@ -7,11 +7,13 @@ class ContainerStructureTest < Formula
   head "https://github.com/GoogleContainerTools/container-structure-test.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91fcb7cb88abaa849aa25cbf4a6f1d4d7115fd214f80f16e4a0cdb8e38c04e23"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fe1c78a41314a909b9251ac55e09a109dcfff01315bde3d515dd2344038183df"
-    sha256 cellar: :any_skip_relocation, catalina:      "62fe8a2e6ee7931c9755dcede739cc54f27173362e1767c675f2fa244dbebf42"
-    sha256 cellar: :any_skip_relocation, mojave:        "11d19adb68076f69c4fc9a27bdfd858cdb9260406e5ec72b15f04b08d795650c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4fc26d1a6f06f9f657f49b72d2ae8554af944ef36d7cbc45064638b24905e548"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e06c1977e1accc08b5b73ae7804f3d2c230a3188e18876fd57b1e7876a3ff20b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91fcb7cb88abaa849aa25cbf4a6f1d4d7115fd214f80f16e4a0cdb8e38c04e23"
+    sha256 cellar: :any_skip_relocation, monterey:       "fe8c36435a7253b707023ac7ed72ed73f824e088a0e5d429de02ac2123cae7ca"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fe1c78a41314a909b9251ac55e09a109dcfff01315bde3d515dd2344038183df"
+    sha256 cellar: :any_skip_relocation, catalina:       "62fe8a2e6ee7931c9755dcede739cc54f27173362e1767c675f2fa244dbebf42"
+    sha256 cellar: :any_skip_relocation, mojave:         "11d19adb68076f69c4fc9a27bdfd858cdb9260406e5ec72b15f04b08d795650c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4fc26d1a6f06f9f657f49b72d2ae8554af944ef36d7cbc45064638b24905e548"
   end
 
   depends_on "go" => :build
