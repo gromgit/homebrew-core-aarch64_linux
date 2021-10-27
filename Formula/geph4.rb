0a7ec5510122a0d146007721ea3370db024d7c50
rui <rui@chenrui.dev>
2021-10-27 05:38:34 +0000

    geph4: update 4.4.20 bottle.

--- Formula/geph4.rb
+++ Formula/geph4.rb
@@ -7,11 +7,13 @@ class Geph4 < Formula
   head "https://github.com/geph-official/geph4.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ccb2f2f58553241167e16ac1a537573a155255a1f529dda55163f5df88eaea08"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d78aa96fe152ffbaf59b262fe6ed5fdc78bc71215fc55b3e5893b82e100865f3"
-    sha256 cellar: :any_skip_relocation, catalina:      "0d158c47acaa8221ee4db67333eca2d06a9cf6ea58e8dd1a72be463705951713"
-    sha256 cellar: :any_skip_relocation, mojave:        "d5de96622d23bc8c574bd662941a415d85a8f0d71823b4014c1074a7606e7d80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00f042f89e96bbd233b037423c0e5a8c77c6a9f3847224ccb0041d5be7127118"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9bdd3d2dbf4537e2f5f452585092a1fa12152a53d0d30072790d4af9ac049376"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ccb2f2f58553241167e16ac1a537573a155255a1f529dda55163f5df88eaea08"
+    sha256 cellar: :any_skip_relocation, monterey:       "206c4b7abdd1cfd6ca73a0af4355f3197f7005411e0dddf144465f88ba997b68"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d78aa96fe152ffbaf59b262fe6ed5fdc78bc71215fc55b3e5893b82e100865f3"
+    sha256 cellar: :any_skip_relocation, catalina:       "0d158c47acaa8221ee4db67333eca2d06a9cf6ea58e8dd1a72be463705951713"
+    sha256 cellar: :any_skip_relocation, mojave:         "d5de96622d23bc8c574bd662941a415d85a8f0d71823b4014c1074a7606e7d80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "00f042f89e96bbd233b037423c0e5a8c77c6a9f3847224ccb0041d5be7127118"
   end
 
   depends_on "rust" => :build
