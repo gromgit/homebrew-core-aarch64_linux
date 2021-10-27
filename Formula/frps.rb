3688635c6022157b0dbf530ebc938049cab26224
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 18:23:45 +0000

    frps: update 0.38.0 bottle.

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -7,10 +7,12 @@ class Frps < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "30fb51a48235fc0c4248de378f893ccbc5773b4ecc23e43eeafe8bc8f94121b6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b9919a54bb73f487774748f3a63b9c67d8f1dccaaca3a796eea1591744cd5498"
-    sha256 cellar: :any_skip_relocation, catalina:      "b9919a54bb73f487774748f3a63b9c67d8f1dccaaca3a796eea1591744cd5498"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "efc764b05e9a8cac8e2a09a19115b1f9a5bea6afe2e7c503f5471bbacac00a9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "30fb51a48235fc0c4248de378f893ccbc5773b4ecc23e43eeafe8bc8f94121b6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30fb51a48235fc0c4248de378f893ccbc5773b4ecc23e43eeafe8bc8f94121b6"
+    sha256 cellar: :any_skip_relocation, monterey:       "b9919a54bb73f487774748f3a63b9c67d8f1dccaaca3a796eea1591744cd5498"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b9919a54bb73f487774748f3a63b9c67d8f1dccaaca3a796eea1591744cd5498"
+    sha256 cellar: :any_skip_relocation, catalina:       "b9919a54bb73f487774748f3a63b9c67d8f1dccaaca3a796eea1591744cd5498"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "efc764b05e9a8cac8e2a09a19115b1f9a5bea6afe2e7c503f5471bbacac00a9b"
   end
 
   depends_on "go" => :build
