a9f0826ca7b4e7df4b84180043f4ef2727490920
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 19:53:36 +0000

    open-ocd: update 0.11.0 bottle.

--- Formula/open-ocd.rb
+++ Formula/open-ocd.rb
@@ -11,11 +11,13 @@ class OpenOcd < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "c95313b03e7050963f4a35a28e88743cd01d1a5e198f809e60ac9e3374799995"
-    sha256 big_sur:       "8c776e777a2587d45f7abc2ab9cb3d682a9e1d0c186c16d3beeec4b5dddcc637"
-    sha256 catalina:      "70eeaab9796e8356a5ad08c8f69e9fc7a86f9ef3f7060248ab49722dfeb95794"
-    sha256 mojave:        "31773a9703e8b217b5d6dc58dc670a8f8afd2b51e7735bbd499eef2daad357dd"
-    sha256 x86_64_linux:  "7a1e636fc79b084859ad0a60a6a8bcd358f190b7381e9bb8fc3c8c8823978fb8"
+    sha256 arm64_monterey: "c9d4a7541767a76b5f588b30a188422c546d731d2786369d53a9458661879343"
+    sha256 arm64_big_sur:  "c95313b03e7050963f4a35a28e88743cd01d1a5e198f809e60ac9e3374799995"
+    sha256 monterey:       "dfb1540fd17932f3f081c6c10b320a9794ee2ab498b4e869d211092cb594c820"
+    sha256 big_sur:        "8c776e777a2587d45f7abc2ab9cb3d682a9e1d0c186c16d3beeec4b5dddcc637"
+    sha256 catalina:       "70eeaab9796e8356a5ad08c8f69e9fc7a86f9ef3f7060248ab49722dfeb95794"
+    sha256 mojave:         "31773a9703e8b217b5d6dc58dc670a8f8afd2b51e7735bbd499eef2daad357dd"
+    sha256 x86_64_linux:   "7a1e636fc79b084859ad0a60a6a8bcd358f190b7381e9bb8fc3c8c8823978fb8"
   end
 
   head do
