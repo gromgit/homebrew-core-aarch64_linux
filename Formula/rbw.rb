a2b86f7e91a671f42ea1a64d60bb924482147907
rui <rui@chenrui.dev>
2021-10-27 07:48:57 +0000

    rbw: update 1.3.0 bottle.

--- Formula/rbw.rb
+++ Formula/rbw.rb
@@ -7,11 +7,13 @@ class Rbw < Formula
   head "https://github.com/doy/rbw.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0e0ba9f71889ea47c3a6b12df6723ed378b2a35be281d3badb4e7a87ac81e057"
-    sha256 cellar: :any_skip_relocation, big_sur:       "916a7562c8c63eb6da7745f1698a7a685bc8d56478f98793f9c7a1e05e35f4de"
-    sha256 cellar: :any_skip_relocation, catalina:      "dc4ef8419b27659d6d6b937bf21c2357b3fdea7ca0befb7060c224f8f7f36835"
-    sha256 cellar: :any_skip_relocation, mojave:        "762e2ee5b53c5c64c02d0be5bbc248d84b187e4e641c2aca4d19e7517f71ea2d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd02ace12c281792045f496308cb037003e3727f2c79d1f984de7a77b882bc66"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c87111e66d9c3588238918493fcff608885ffe3ab7a17d8af7e34f5d44eaca50"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0e0ba9f71889ea47c3a6b12df6723ed378b2a35be281d3badb4e7a87ac81e057"
+    sha256 cellar: :any_skip_relocation, monterey:       "59299d69cdf5cb461d0802eb7725a2d6db58dac534337bb54612a8e7833c59c2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "916a7562c8c63eb6da7745f1698a7a685bc8d56478f98793f9c7a1e05e35f4de"
+    sha256 cellar: :any_skip_relocation, catalina:       "dc4ef8419b27659d6d6b937bf21c2357b3fdea7ca0befb7060c224f8f7f36835"
+    sha256 cellar: :any_skip_relocation, mojave:         "762e2ee5b53c5c64c02d0be5bbc248d84b187e4e641c2aca4d19e7517f71ea2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd02ace12c281792045f496308cb037003e3727f2c79d1f984de7a77b882bc66"
   end
 
   depends_on "rust" => :build
