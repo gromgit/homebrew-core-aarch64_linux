127990cbb3b20a711c9f6ba8198060f1a1f8353e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:47:50 +0000

    reposurgeon: update 4.29 bottle.

--- Formula/reposurgeon.rb
+++ Formula/reposurgeon.rb
@@ -8,10 +8,12 @@ class Reposurgeon < Formula
   head "https://gitlab.com/esr/reposurgeon.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c7f30cf4589429feb7bd480ebe7f57c627e2ac9e16c5946ad3939e739c5e4ae8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "60748bd31b737f5da19048f67d8db22113a912b937fc013272af329155f86b8e"
-    sha256 cellar: :any_skip_relocation, catalina:      "45bee33e5c8e35fe15257333aef0ac31fbfff7c868b0fcaf97232155ab087822"
-    sha256 cellar: :any_skip_relocation, mojave:        "85988d5f6ffd08dcedeb706a566d8123869840ab61f0eb0d7fe8ce1ae0207b41"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3b98cc8b69dc7952fc935fe26d7dbbbd2dad705207181a86b3423e108fc317de"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c7f30cf4589429feb7bd480ebe7f57c627e2ac9e16c5946ad3939e739c5e4ae8"
+    sha256 cellar: :any_skip_relocation, monterey:       "bda19f7c15e15ef6766e80a7e9e2dc580dbe822796a183e5376d4bff98720f71"
+    sha256 cellar: :any_skip_relocation, big_sur:        "60748bd31b737f5da19048f67d8db22113a912b937fc013272af329155f86b8e"
+    sha256 cellar: :any_skip_relocation, catalina:       "45bee33e5c8e35fe15257333aef0ac31fbfff7c868b0fcaf97232155ab087822"
+    sha256 cellar: :any_skip_relocation, mojave:         "85988d5f6ffd08dcedeb706a566d8123869840ab61f0eb0d7fe8ce1ae0207b41"
   end
 
   depends_on "asciidoctor" => :build
