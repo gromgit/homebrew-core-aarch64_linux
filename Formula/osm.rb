357d271cbb85d76996ca861b0d2d42914545fcfb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:53:20 +0000

    osm: update 0.11.1 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -7,10 +7,12 @@ class Osm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "339a133e39c101135a735c6d6a42210fbe82be3fa022b602913bbef7d51c6cba"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6c364faa784fdcc6459263149144ba6ce0c153e53afb5f6aa638a8c8508282de"
-    sha256 cellar: :any_skip_relocation, catalina:      "691b86178c205485e2dcf9e904b09904418abe2371cbb9c930cfd56c0220ed37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8179406aa361fa495fc40f24d778cac43af2940b77abd135ea1d4b0edda7f6f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c83f900219fa1b556c68a1eb7761f8d0bb0ad3dc6072977ad36eb6fadc7e4eec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "339a133e39c101135a735c6d6a42210fbe82be3fa022b602913bbef7d51c6cba"
+    sha256 cellar: :any_skip_relocation, monterey:       "f486ee23471840560a9efb385661693e4215c3a7c4e05d7974e8b4f980e4e958"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6c364faa784fdcc6459263149144ba6ce0c153e53afb5f6aa638a8c8508282de"
+    sha256 cellar: :any_skip_relocation, catalina:       "691b86178c205485e2dcf9e904b09904418abe2371cbb9c930cfd56c0220ed37"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8179406aa361fa495fc40f24d778cac43af2940b77abd135ea1d4b0edda7f6f"
   end
 
   depends_on "go" => :build
