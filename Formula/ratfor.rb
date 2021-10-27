e019ad0953b608e523ec9d11dea649092764ecce
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:02:56 +0000

    ratfor: update 1.05 bottle.

--- Formula/ratfor.rb
+++ Formula/ratfor.rb
@@ -11,6 +11,7 @@ class Ratfor < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "86d1de3e075edcc1e493b46fc7186bd21906644ba69a7032f3bc827487eb9449"
+    sha256 cellar: :any_skip_relocation, monterey:      "fc0242f98c14b752d27ed3bf1ce4c92827474918c87e38f541fc95c8cd95a267"
     sha256 cellar: :any_skip_relocation, big_sur:       "0d9bfcd885197bf8bbfbd38469cd831e16f2dceb6cb6155acf075f0dfebcf095"
     sha256 cellar: :any_skip_relocation, catalina:      "053917ccdf191b7cb15adb1c207cb3f18553def7d4cc9584b09222be07754660"
     sha256 cellar: :any_skip_relocation, mojave:        "054cb6d92e13050233c54a5bbfdd1dc9fbaed09d63937b8426d543d9569ee07b"
