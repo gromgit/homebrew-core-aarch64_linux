bd6a4966da2b2161789f4b4597e48e4c7564b411
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 01:34:29 +0000

    switchaudio-osx: update 1.1.0 bottle.

--- Formula/switchaudio-osx.rb
+++ Formula/switchaudio-osx.rb
@@ -7,10 +7,12 @@ class SwitchaudioOsx < Formula
   head "https://github.com/deweller/switchaudio-osx.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "631619e7f83da181c8287e6795de8d54c03cab15bf29ba4ce5b5c0fb1f3aafcb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4e6b47d1a51d71706f160ed4f19cc6fe6cacd1370eab298c029976ae6c32484e"
-    sha256 cellar: :any_skip_relocation, catalina:      "59e92aaf09b86e49b6bd5f4300db02cf0812118e08146eaa2c540cb8e34b1a6b"
-    sha256 cellar: :any_skip_relocation, mojave:        "e48cf16dc12a923093b4ddf5467f8234d129e5bdc15f1df1fdad30ec251e7f35"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a6bf6a7e8873d101da1d5f24be8a8c3470af13f56ed21de847685ebf9e6efad8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "631619e7f83da181c8287e6795de8d54c03cab15bf29ba4ce5b5c0fb1f3aafcb"
+    sha256 cellar: :any_skip_relocation, monterey:       "7d94bc90f9d9d0362c8fc36634170bd3e433dfd5a61b8bad3243205f4af81bec"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4e6b47d1a51d71706f160ed4f19cc6fe6cacd1370eab298c029976ae6c32484e"
+    sha256 cellar: :any_skip_relocation, catalina:       "59e92aaf09b86e49b6bd5f4300db02cf0812118e08146eaa2c540cb8e34b1a6b"
+    sha256 cellar: :any_skip_relocation, mojave:         "e48cf16dc12a923093b4ddf5467f8234d129e5bdc15f1df1fdad30ec251e7f35"
   end
 
   depends_on xcode: :build
