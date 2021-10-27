f694ae835030577812206edd5289c9155f4aeb85
rui <rui@chenrui.dev>
2021-10-27 08:33:33 +0000

    spotifyd: update 0.3.2 bottle.

--- Formula/spotifyd.rb
+++ Formula/spotifyd.rb
@@ -13,11 +13,13 @@ class Spotifyd < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any,                 arm64_big_sur: "fe8f92ca3a00fc2b8dc28a6c6d868c49f0febbe26ad818755045af763102e04f"
-    sha256 cellar: :any,                 big_sur:       "027e2994c8471dcde0b06ceda61c07166fa9083d3a08f4056ba986be37f21db0"
-    sha256 cellar: :any,                 catalina:      "b2a8c0dffe45b557509e6a70a47d9cd96c6222cdd2ab2d44c7366806ba3d7721"
-    sha256 cellar: :any,                 mojave:        "2c047d9f19710edd8795e14351e36aac051c5f9397e262f4199cf9beffe1483b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "804c7e8e5855f5082734c558606c650c6004ea4e9cfc11d342f5d802cca0c1b4"
+    sha256 cellar: :any,                 arm64_monterey: "1108e828b2ed18a151143e3dc3dacdc0460efce76f56eba5d50fdf60bfbe06ae"
+    sha256 cellar: :any,                 arm64_big_sur:  "fe8f92ca3a00fc2b8dc28a6c6d868c49f0febbe26ad818755045af763102e04f"
+    sha256 cellar: :any,                 monterey:       "0666848f95b581365fa2563736f2226ae55e6c169d4efd446c3bcebc2028525e"
+    sha256 cellar: :any,                 big_sur:        "027e2994c8471dcde0b06ceda61c07166fa9083d3a08f4056ba986be37f21db0"
+    sha256 cellar: :any,                 catalina:       "b2a8c0dffe45b557509e6a70a47d9cd96c6222cdd2ab2d44c7366806ba3d7721"
+    sha256 cellar: :any,                 mojave:         "2c047d9f19710edd8795e14351e36aac051c5f9397e262f4199cf9beffe1483b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "804c7e8e5855f5082734c558606c650c6004ea4e9cfc11d342f5d802cca0c1b4"
   end
 
   depends_on "pkg-config" => :build
