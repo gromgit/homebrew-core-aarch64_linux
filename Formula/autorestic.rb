a1b21cd299d58a8d609083eef3b5beeb09e691b8
rui <rui@chenrui.dev>
2021-10-27 02:58:13 +0000

    autorestic: update 1.3.0 bottle.

--- Formula/autorestic.rb
+++ Formula/autorestic.rb
@@ -7,10 +7,12 @@ class Autorestic < Formula
   head "https://github.com/cupcakearmy/autorestic.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "984e7b1cacb3d3681542c456dfadd636e97ce6c9e6c7fb57c17ddc572181f279"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b71c3459ec007b6a847ba4fd731edce74665d55b7c0f72d0a7c60ddbc7a9a9e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "6aa4994d633514cec4eba16f528c5e7aae4bbca11e1d2b821715776fa2873dc4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58577a7c45501bdfeb6097845ebc452297af2da7734b285258b63227cc57b82d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "444363638492e061fe8a5f7c025c1bdc380234de77dfe4e198715c9339e40ae4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "984e7b1cacb3d3681542c456dfadd636e97ce6c9e6c7fb57c17ddc572181f279"
+    sha256 cellar: :any_skip_relocation, monterey:       "163fe3de2a115117b49744cfe853ab668df153c8511a3c43ba023e32fc46724b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b71c3459ec007b6a847ba4fd731edce74665d55b7c0f72d0a7c60ddbc7a9a9e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "6aa4994d633514cec4eba16f528c5e7aae4bbca11e1d2b821715776fa2873dc4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58577a7c45501bdfeb6097845ebc452297af2da7734b285258b63227cc57b82d"
   end
 
   depends_on "go" => :build
