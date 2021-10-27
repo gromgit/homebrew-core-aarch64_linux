6e681a487a7a1ee6f96235581d02f466b5dfb9d2
rui <rui@chenrui.dev>
2021-10-27 05:43:52 +0000

    gifski: update 1.5.1 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -6,11 +6,13 @@ class Gifski < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "4e4f752ba34b14b134bc80a67ee0f6f36d74f5cbdf95b88ec306bfade5a075cf"
-    sha256 cellar: :any,                 big_sur:       "e71fc42f451a7d7e5d9d7fe61133855e2cab497c96c854dd738f24da3d3818c8"
-    sha256 cellar: :any,                 catalina:      "e4c94f149462465d73b0cbf4acae91a6dd9e4566b8c3852a7c96b319e831ca1f"
-    sha256 cellar: :any,                 mojave:        "f19277f478fde77637dacca7a4a080294f7ad8109de4568b32acbec00d647ce8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c56085cbe3eca95c3fc1958a38924283bff2b54c9d5d011d1132cb9c181fce40"
+    sha256 cellar: :any,                 arm64_monterey: "a587ba695eafdf36d0d552490d75f904be8357de3367f7d6a3e24425c32689ad"
+    sha256 cellar: :any,                 arm64_big_sur:  "4e4f752ba34b14b134bc80a67ee0f6f36d74f5cbdf95b88ec306bfade5a075cf"
+    sha256 cellar: :any,                 monterey:       "45e66b70557e5754ead07f6cfe259ac8c0d59819c96bdadf5002d1f4fa5f8e29"
+    sha256 cellar: :any,                 big_sur:        "e71fc42f451a7d7e5d9d7fe61133855e2cab497c96c854dd738f24da3d3818c8"
+    sha256 cellar: :any,                 catalina:       "e4c94f149462465d73b0cbf4acae91a6dd9e4566b8c3852a7c96b319e831ca1f"
+    sha256 cellar: :any,                 mojave:         "f19277f478fde77637dacca7a4a080294f7ad8109de4568b32acbec00d647ce8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c56085cbe3eca95c3fc1958a38924283bff2b54c9d5d011d1132cb9c181fce40"
   end
 
   depends_on "pkg-config" => :build
