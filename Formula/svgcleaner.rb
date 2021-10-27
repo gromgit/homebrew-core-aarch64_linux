d674b57cb4a994d4195f48fdc217f18e7ced8cc3
rui <rui@chenrui.dev>
2021-10-27 08:40:47 +0000

    svgcleaner: update 0.9.5 bottle.

--- Formula/svgcleaner.rb
+++ Formula/svgcleaner.rb
@@ -8,12 +8,14 @@ class Svgcleaner < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d4e1232a8f5f904166c97f537df03596bf133ea9d68a1ab08df2f4b0dfbbdc1b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "69290442826481651a7e314ec175f7fc980640c53e7908aa5fd6e9cbff03a1dc"
-    sha256 cellar: :any_skip_relocation, catalina:      "43533727baf2ed09cdce9fe64357c1bc1f70fed57d70f37cfd824b664ab1266f"
-    sha256 cellar: :any_skip_relocation, mojave:        "bf18c353316b7a46ed2cecad188a638e359ce77acdcf501f578e5f96149ed667"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "7e6df86bb8f994b157ff6de9bb7f43605b813a6a476f6f2d3af4d3483c1b6483"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "faae5c691c1bbb241aa001e912458be50a1aaf6eea241591b6a34148d3d93228"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "526c99b79cb8de90e2d43a2723dedc23786f16a1e3992669d071d478cffb7e9d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d4e1232a8f5f904166c97f537df03596bf133ea9d68a1ab08df2f4b0dfbbdc1b"
+    sha256 cellar: :any_skip_relocation, monterey:       "b0f023be007c4b8a5cb02b91de3369ac0cb49b4bfccc8917ed5ee81ee1f7ea76"
+    sha256 cellar: :any_skip_relocation, big_sur:        "69290442826481651a7e314ec175f7fc980640c53e7908aa5fd6e9cbff03a1dc"
+    sha256 cellar: :any_skip_relocation, catalina:       "43533727baf2ed09cdce9fe64357c1bc1f70fed57d70f37cfd824b664ab1266f"
+    sha256 cellar: :any_skip_relocation, mojave:         "bf18c353316b7a46ed2cecad188a638e359ce77acdcf501f578e5f96149ed667"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "7e6df86bb8f994b157ff6de9bb7f43605b813a6a476f6f2d3af4d3483c1b6483"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "faae5c691c1bbb241aa001e912458be50a1aaf6eea241591b6a34148d3d93228"
   end
 
   depends_on "rust" => :build
