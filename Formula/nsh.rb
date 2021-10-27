edc0557aaf4adb8336e60c36b51f79c0c0b71952
rui <rui@chenrui.dev>
2021-10-27 06:58:39 +0000

    nsh: update 0.4.2 bottle.

--- Formula/nsh.rb
+++ Formula/nsh.rb
@@ -7,10 +7,12 @@ class Nsh < Formula
   head "https://github.com/nuta/nsh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9025b069d64133a40f5e1c5f1ad35178ef47a601d321da10749323dc54f48f3c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8454140462f3968d3c29b64ab2248faa2fb544a491c3e3f2291da24f60801dac"
-    sha256 cellar: :any_skip_relocation, catalina:      "04010c69722c2060dd83c66a1b30281014126248a4586b5c0959ff5bbfdc00e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "131188c37bdb5c53e7cdad97e1c24ae13e0ae361636a883698521bee40c699d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1d4aaa96c329882423ba9bef1a834e9d0e7f12eebd786e7a229461113a7de99"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9025b069d64133a40f5e1c5f1ad35178ef47a601d321da10749323dc54f48f3c"
+    sha256 cellar: :any_skip_relocation, monterey:       "6deefb05e0d22f1597101e1703aadfa5a27e1ec3175037b84a5b5584b3310181"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8454140462f3968d3c29b64ab2248faa2fb544a491c3e3f2291da24f60801dac"
+    sha256 cellar: :any_skip_relocation, catalina:       "04010c69722c2060dd83c66a1b30281014126248a4586b5c0959ff5bbfdc00e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "131188c37bdb5c53e7cdad97e1c24ae13e0ae361636a883698521bee40c699d3"
   end
 
   depends_on "rust" => :build
