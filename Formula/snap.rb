166788c86138b08ba7ca5ba9d9821c60ab88eec0
rui <rui@chenrui.dev>
2021-10-27 08:13:25 +0000

    snap: update 2.46 bottle.

--- Formula/snap.rb
+++ Formula/snap.rb
@@ -13,11 +13,13 @@ class Snap < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "736643f2e11f651d081b6515f04127c3bca06afb4b86734436d45534abead197"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9cb1d7db5a7f7854fecedf029d130977238a0ed9a8a32a4454225712d3542878"
-    sha256 cellar: :any_skip_relocation, catalina:      "f60a56adf86fdc4c86b5d38b47f21c52e0459612c9ed7ae15905a4c838e51787"
-    sha256 cellar: :any_skip_relocation, mojave:        "febbdc8548096fb9d0159c8b7cbaa4281ba8b868625264b254aab3ce4d2b7924"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ed92bc9ab2a0e2fe25f5b71d7d240f617a6e2e91e5bc191bb8fa3f49a9cbf5e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6df212521ccfc1a97ea438858f538652a15fda29869cac1ba674bf8d1908b6f4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "736643f2e11f651d081b6515f04127c3bca06afb4b86734436d45534abead197"
+    sha256 cellar: :any_skip_relocation, monterey:       "623240f79ed648bcf79aa9c82925fa97a5d448ada14b83629f9c37e1ddb29089"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9cb1d7db5a7f7854fecedf029d130977238a0ed9a8a32a4454225712d3542878"
+    sha256 cellar: :any_skip_relocation, catalina:       "f60a56adf86fdc4c86b5d38b47f21c52e0459612c9ed7ae15905a4c838e51787"
+    sha256 cellar: :any_skip_relocation, mojave:         "febbdc8548096fb9d0159c8b7cbaa4281ba8b868625264b254aab3ce4d2b7924"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ed92bc9ab2a0e2fe25f5b71d7d240f617a6e2e91e5bc191bb8fa3f49a9cbf5e"
   end
 
   depends_on "go" => :build
