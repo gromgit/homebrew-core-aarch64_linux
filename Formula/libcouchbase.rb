58fb71d4be8d3046b2b572b2825465407c532aa8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:25:17 +0000

    libcouchbase: update 3.2.3 bottle.

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -7,11 +7,13 @@ class Libcouchbase < Formula
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
-    sha256 arm64_big_sur: "b34f3cc0a623995a4030e8013029aa9e5ffc550bf6548da3c6498fc598243284"
-    sha256 big_sur:       "4612c9639e2f6438b631b40f72fb7a005de0381266698ef617cfc98cc5113ebb"
-    sha256 catalina:      "849c52aec8ef236bbcbea83e3eb6d12773124beecc51344c63424da7e0a579c7"
-    sha256 mojave:        "2dd828a51b990553186fd1bcdd26dd7f7367e02a6eeb092e6f1ef8db134ef845"
-    sha256 x86_64_linux:  "e2d7602e6338db5a0b576566693972e3abd616d65079f8d751076653653ce651"
+    sha256 arm64_monterey: "b866f2edb54f3b20cca64adc565ce7bc4a71f871d14e917b9bb59ebd39c4158a"
+    sha256 arm64_big_sur:  "b34f3cc0a623995a4030e8013029aa9e5ffc550bf6548da3c6498fc598243284"
+    sha256 monterey:       "ba4296240fb178e72b43b3073608e689f1893f14e3b664369e3672c4621d809b"
+    sha256 big_sur:        "4612c9639e2f6438b631b40f72fb7a005de0381266698ef617cfc98cc5113ebb"
+    sha256 catalina:       "849c52aec8ef236bbcbea83e3eb6d12773124beecc51344c63424da7e0a579c7"
+    sha256 mojave:         "2dd828a51b990553186fd1bcdd26dd7f7367e02a6eeb092e6f1ef8db134ef845"
+    sha256 x86_64_linux:   "e2d7602e6338db5a0b576566693972e3abd616d65079f8d751076653653ce651"
   end
 
   depends_on "cmake" => :build
