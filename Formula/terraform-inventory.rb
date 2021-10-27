abde6769cbf29a6302a24d6a8d889d0253123587
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:21:27 +0000

    terraform-inventory: update 0.10 bottle.

--- Formula/terraform-inventory.rb
+++ Formula/terraform-inventory.rb
@@ -7,11 +7,13 @@ class TerraformInventory < Formula
   head "https://github.com/adammck/terraform-inventory.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "df26181ba3116beae2b5582eb6725c2c8d9ddad018be827f6d07d252cfc019c0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
-    sha256 cellar: :any_skip_relocation, catalina:      "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
-    sha256 cellar: :any_skip_relocation, mojave:        "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "853cf36916d4bb9834f0f6b491e347a109a2930d350a1a31b8e7b8233720c20b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0a1c36c1ddf616cee16ffaa7686fee0dc5043142c5aac0b95698b0caafe67c50"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "df26181ba3116beae2b5582eb6725c2c8d9ddad018be827f6d07d252cfc019c0"
+    sha256 cellar: :any_skip_relocation, monterey:       "7444adf6b3ea13567454c7ef34feebc5d5fddedeaefe5be07382544ebf67d79b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
+    sha256 cellar: :any_skip_relocation, catalina:       "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
+    sha256 cellar: :any_skip_relocation, mojave:         "ab56b7c132afc5508e5e10cfa21b784aa2f84fa8a23d9985b7b45eb04c8bdae1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "853cf36916d4bb9834f0f6b491e347a109a2930d350a1a31b8e7b8233720c20b"
   end
 
   depends_on "go" => :build
