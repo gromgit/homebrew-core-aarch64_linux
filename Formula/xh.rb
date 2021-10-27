7c476b5b5f3d049dfc9b7305940c1ec1b4d11439
rui <rui@chenrui.dev>
2021-10-27 09:35:17 +0000

    xh: update 0.13.0 bottle.

--- Formula/xh.rb
+++ Formula/xh.rb
@@ -7,11 +7,13 @@ class Xh < Formula
   head "https://github.com/ducaale/xh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "26292428e0b0ba0727e38b9693782e0c1a7f795ffb43c58d1eada081ef0aea44"
-    sha256 cellar: :any_skip_relocation, big_sur:       "daf8cdcc24b0ac8a3b25abb4f42871c7af52dd2690bdaf94576433fa78e998cf"
-    sha256 cellar: :any_skip_relocation, catalina:      "ea2de87328ac70becf698b04ea9ea8c8d4f04434cae2c42444dedcd02858e90a"
-    sha256 cellar: :any_skip_relocation, mojave:        "a3824630363bb66568441a1197133ae8b98534bf033fbbbf6851aa0176fb5e41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f90037394c877924369d65e167bdc1c22364928a57a02c81aaa488c08be17cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "835f689cd08741fbea5b0661fb86eb47fa9b11761f4a8aad12effc96daf7b932"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "26292428e0b0ba0727e38b9693782e0c1a7f795ffb43c58d1eada081ef0aea44"
+    sha256 cellar: :any_skip_relocation, monterey:       "a3868e6357c4cc79d178236abc237d341c5d08da28d7459a4ce1f423e6e1935b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "daf8cdcc24b0ac8a3b25abb4f42871c7af52dd2690bdaf94576433fa78e998cf"
+    sha256 cellar: :any_skip_relocation, catalina:       "ea2de87328ac70becf698b04ea9ea8c8d4f04434cae2c42444dedcd02858e90a"
+    sha256 cellar: :any_skip_relocation, mojave:         "a3824630363bb66568441a1197133ae8b98534bf033fbbbf6851aa0176fb5e41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2f90037394c877924369d65e167bdc1c22364928a57a02c81aaa488c08be17cc"
   end
 
   depends_on "rust" => :build
