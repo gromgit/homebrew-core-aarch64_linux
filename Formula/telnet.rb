d3f3163de580207df4169e52dd043b104bcbc5b5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 01:01:33 +0000

    telnet: update 63 bottle.

--- Formula/telnet.rb
+++ Formula/telnet.rb
@@ -11,11 +11,13 @@ class Telnet < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "563fcbef08590e48a07079f00877e14f22f54849db11ee02ba7c935499a7ccd1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e6fb7de53e703755a72e227752f81023c2935567d935af638959e986da910b3e"
-    sha256 cellar: :any_skip_relocation, catalina:      "7435a9fd2515158762a85197a4ad7141e430383e185e002da169dbbb638c952f"
-    sha256 cellar: :any_skip_relocation, mojave:        "d5009f496dc6cf0c13b936996f98b91b0f12733ea9462843b56a39fc53b20fe0"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "af38f3c6dd4ff5eda2248671958e66595b39e74cdeecca52af4efb495bc659a7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "83fb633f08504f7f2e89c866530c4ee83798e043eeb5a1974919311651c99c37"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "563fcbef08590e48a07079f00877e14f22f54849db11ee02ba7c935499a7ccd1"
+    sha256 cellar: :any_skip_relocation, monterey:       "f0b297685d31e574035700bd31e2003e81cad22c6d5b5a0f746d7b9b0be20f90"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e6fb7de53e703755a72e227752f81023c2935567d935af638959e986da910b3e"
+    sha256 cellar: :any_skip_relocation, catalina:       "7435a9fd2515158762a85197a4ad7141e430383e185e002da169dbbb638c952f"
+    sha256 cellar: :any_skip_relocation, mojave:         "d5009f496dc6cf0c13b936996f98b91b0f12733ea9462843b56a39fc53b20fe0"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "af38f3c6dd4ff5eda2248671958e66595b39e74cdeecca52af4efb495bc659a7"
   end
 
   depends_on xcode: :build
