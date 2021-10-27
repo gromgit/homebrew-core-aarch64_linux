185167a9e1a9bbe3757bfe1e92612650cd6588bb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 01:29:54 +0000

    gst-plugins-bad: update 1.18.4 bottle.

--- Formula/gst-plugins-bad.rb
+++ Formula/gst-plugins-bad.rb
@@ -12,10 +12,12 @@ class GstPluginsBad < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "6c0ada7dac8defafc4e6d099870d22efd9cae93ef4c7afce32684e2d62dbb674"
-    sha256 big_sur:       "c4c7f8121511f8981dd7478d81ef8a773fab0c714b820ecbbfdfb56d71ccf737"
-    sha256 catalina:      "bb4a58be9dd95562605a8a7537eedcdb76ec4671eaebe8fdda4416b1a67b5621"
-    sha256 mojave:        "fa30f63fdccd9d97dd43612582b5fb32952f33ae1f7eac26b506fefe64a2f39b"
+    sha256 arm64_monterey: "ca8586eaf948a1b1d5c3e56fa258fd3098764f81d5bef1c622324aaedec53695"
+    sha256 arm64_big_sur:  "6c0ada7dac8defafc4e6d099870d22efd9cae93ef4c7afce32684e2d62dbb674"
+    sha256 monterey:       "c591057877a706e583a293a858553cf26ed3a8346dd37bfeaf2bab39b9d11daa"
+    sha256 big_sur:        "c4c7f8121511f8981dd7478d81ef8a773fab0c714b820ecbbfdfb56d71ccf737"
+    sha256 catalina:       "bb4a58be9dd95562605a8a7537eedcdb76ec4671eaebe8fdda4416b1a67b5621"
+    sha256 mojave:         "fa30f63fdccd9d97dd43612582b5fb32952f33ae1f7eac26b506fefe64a2f39b"
   end
 
   depends_on "gobject-introspection" => :build
