4dca4adce2f506e8f901b09c63acb539b418285a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 01:59:16 +0000

    squid: update 4.17 bottle.

--- Formula/squid.rb
+++ Formula/squid.rb
@@ -11,11 +11,13 @@ class Squid < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "ab7f9bcb273ce10621b219b3fa36306039a025d0a61745e061ff7135c2c5ce02"
-    sha256 big_sur:       "cd2c5c2dc8843d49adf1cf7b7c0b51567a0ad6d8293d3779006906695348e4ec"
-    sha256 catalina:      "4e4cb257f1a82777ba111d1c4a80de8398b7fa9e330cd2506f698e85a6057be9"
-    sha256 mojave:        "14b611332f6cb8001e15e875f02192b9ce2ca600a2a19f125cd51402032d102e"
-    sha256 x86_64_linux:  "ff99b1569d2fc90eea26ff2aadff55670eeacf2469fca3e713fb00ee6e77d10b"
+    sha256 arm64_monterey: "4c922c513876e0eac5e5eea248e75f5d7b15ac934defbb29531b547211ca2ed5"
+    sha256 arm64_big_sur:  "ab7f9bcb273ce10621b219b3fa36306039a025d0a61745e061ff7135c2c5ce02"
+    sha256 monterey:       "6c60aef5ac07e2350e67ed8c190bb023152e3fab0ab676b95e77cb3b626f0c24"
+    sha256 big_sur:        "cd2c5c2dc8843d49adf1cf7b7c0b51567a0ad6d8293d3779006906695348e4ec"
+    sha256 catalina:       "4e4cb257f1a82777ba111d1c4a80de8398b7fa9e330cd2506f698e85a6057be9"
+    sha256 mojave:         "14b611332f6cb8001e15e875f02192b9ce2ca600a2a19f125cd51402032d102e"
+    sha256 x86_64_linux:   "ff99b1569d2fc90eea26ff2aadff55670eeacf2469fca3e713fb00ee6e77d10b"
   end
 
   head do
