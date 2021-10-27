3e910c8f4db9d4b405fd40634aea865123d778ef
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:48:25 +0000

    s-search: update 0.6.0 bottle.

--- Formula/s-search.rb
+++ Formula/s-search.rb
@@ -7,11 +7,13 @@ class SSearch < Formula
   head "https://github.com/zquestz/s.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f11b7632fb8b306788d486d54b53feed63f7dec7c4f149fc2a6bf716cbe18837"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e37573825550c63f264efaebf0f9331034fa722cbcff2f41480e3f36663b6a70"
-    sha256 cellar: :any_skip_relocation, catalina:      "71ec2554f4cb3b04e2cb4bb871f91b80c102e0d08ea42cc9d9c77c7828b8c20e"
-    sha256 cellar: :any_skip_relocation, mojave:        "437a7566955f6c381b81458428abdae3ae01dd124543cf08212e1617dbbafd65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a06b8ed3430c05ce0cd462284da8fdcbdea88f023aeb71f965804bd3d97f95c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "86eb423733972f1501502155b72e22661a8266fa433ade90f27e045dfb0c6de3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f11b7632fb8b306788d486d54b53feed63f7dec7c4f149fc2a6bf716cbe18837"
+    sha256 cellar: :any_skip_relocation, monterey:       "382b04e8468aa53b3262bdc505cf93d85c8bb0df45ffdc6cf877c76a9c6c1727"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e37573825550c63f264efaebf0f9331034fa722cbcff2f41480e3f36663b6a70"
+    sha256 cellar: :any_skip_relocation, catalina:       "71ec2554f4cb3b04e2cb4bb871f91b80c102e0d08ea42cc9d9c77c7828b8c20e"
+    sha256 cellar: :any_skip_relocation, mojave:         "437a7566955f6c381b81458428abdae3ae01dd124543cf08212e1617dbbafd65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a06b8ed3430c05ce0cd462284da8fdcbdea88f023aeb71f965804bd3d97f95c"
   end
 
   depends_on "go" => :build
