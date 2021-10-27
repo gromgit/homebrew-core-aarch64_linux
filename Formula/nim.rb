a45d167ca68b586cef4e3fb0ec09e462381ec5ef
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 01:45:58 +0000

    nim: update 1.6.0 bottle.

--- Formula/nim.rb
+++ Formula/nim.rb
@@ -7,11 +7,13 @@ class Nim < Formula
   head "https://github.com/nim-lang/Nim.git", branch: "devel"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "76f0e1b7db990f867bdbf97db92fcdb9a96de73733eb29a33878f8935a27214f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b44d54cb206ab367dc6af6bfaf2b88a093369b95344f068bc8a71c8c99ba2406"
-    sha256 cellar: :any_skip_relocation, catalina:      "2662f8654a5bba61c293513cccecf72ce54e9a5a17aff9373a516087c999630e"
-    sha256 cellar: :any_skip_relocation, mojave:        "3cad9495839a01078ff66b0f8db540ca038c5ebf11b9ecd070a9d297d2193616"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "db63f1cd066553a0bd0c7b8d38cd171876bef75da13b4783fc3927bf323846b4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2752c65069f4a92c529d85dd60b5aa21d557b7753d25b875d01e0931238b7747"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "76f0e1b7db990f867bdbf97db92fcdb9a96de73733eb29a33878f8935a27214f"
+    sha256 cellar: :any_skip_relocation, monterey:       "6bed54ef25269f67f8fb04a0339b726ac74b5dffa698ef2e0041dc76fea33063"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b44d54cb206ab367dc6af6bfaf2b88a093369b95344f068bc8a71c8c99ba2406"
+    sha256 cellar: :any_skip_relocation, catalina:       "2662f8654a5bba61c293513cccecf72ce54e9a5a17aff9373a516087c999630e"
+    sha256 cellar: :any_skip_relocation, mojave:         "3cad9495839a01078ff66b0f8db540ca038c5ebf11b9ecd070a9d297d2193616"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db63f1cd066553a0bd0c7b8d38cd171876bef75da13b4783fc3927bf323846b4"
   end
 
   depends_on "help2man" => :build
