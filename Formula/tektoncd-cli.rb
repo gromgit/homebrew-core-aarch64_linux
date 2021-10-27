a4dda6b8c1d2a1eca6bc882871eb743ef66095ad
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:11:47 +0000

    tektoncd-cli: update 0.21.0 bottle.

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -11,11 +11,13 @@ class TektoncdCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9ce190179c039a659fdd56db1dfd61ad653ec6f9de6c6aa9950b9a068e5a22ea"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2340e7366bb268236ee9e2e8c3172712534e547f066ea1b13c4882c7b8014af0"
-    sha256 cellar: :any_skip_relocation, catalina:      "f3bc8b676c7055a244760a82d1b64631327dc4f66e04d5c254c8669250a650c7"
-    sha256 cellar: :any_skip_relocation, mojave:        "19b2eb7fc9058bb500c33b1f9e60f740627d7740ce6201bc9408a3bd6658b834"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46c546dd97f355278764d8a2c9566c2eebe33b7d5890eda1cba903aaf6a03bf0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5b74d7582881b8bade5bd62b7767171a8f55de6abdad628abece94e5bc95234"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ce190179c039a659fdd56db1dfd61ad653ec6f9de6c6aa9950b9a068e5a22ea"
+    sha256 cellar: :any_skip_relocation, monterey:       "effd1801c642b1c27a392d906958103f8302aa22c9d118c52e9355845bc78429"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2340e7366bb268236ee9e2e8c3172712534e547f066ea1b13c4882c7b8014af0"
+    sha256 cellar: :any_skip_relocation, catalina:       "f3bc8b676c7055a244760a82d1b64631327dc4f66e04d5c254c8669250a650c7"
+    sha256 cellar: :any_skip_relocation, mojave:         "19b2eb7fc9058bb500c33b1f9e60f740627d7740ce6201bc9408a3bd6658b834"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46c546dd97f355278764d8a2c9566c2eebe33b7d5890eda1cba903aaf6a03bf0"
   end
 
   depends_on "go" => :build
