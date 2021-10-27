86a3e2a61e8fd6ee95bf48ff258048bc3e021e24
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:47:32 +0000

    oauth2_proxy: update 7.2.0 bottle.

--- Formula/oauth2_proxy.rb
+++ Formula/oauth2_proxy.rb
@@ -7,10 +7,12 @@ class Oauth2Proxy < Formula
   head "https://github.com/oauth2-proxy/oauth2-proxy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4711f05aad5403a07683f1506548c2dcbd120de9fe7d7dbd272633e2a46e97b2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b6a4c2f5dc2196c0b2a110bef21e717b5073d258409e52ef5f98271c8728d44d"
-    sha256 cellar: :any_skip_relocation, catalina:      "0d98e96bbbd926774ae07441f115732755d2d871897d371af0e1a1df9e6c6a28"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "03d8171193bae05af58e33086db444f92ec829038a3c3750a7d5a7dea211fc92"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2d80ccdedcd39fef312774b9fb03a38f1620a5e8785c52e996a285ca38b2bf5c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4711f05aad5403a07683f1506548c2dcbd120de9fe7d7dbd272633e2a46e97b2"
+    sha256 cellar: :any_skip_relocation, monterey:       "4fad35aa9831749fa67d830806d4970023cd2efe916657a85422e8dcd41d53ee"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b6a4c2f5dc2196c0b2a110bef21e717b5073d258409e52ef5f98271c8728d44d"
+    sha256 cellar: :any_skip_relocation, catalina:       "0d98e96bbbd926774ae07441f115732755d2d871897d371af0e1a1df9e6c6a28"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03d8171193bae05af58e33086db444f92ec829038a3c3750a7d5a7dea211fc92"
   end
 
   depends_on "go" => :build
