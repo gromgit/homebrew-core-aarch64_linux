cd6a21aeb078d7e6d257bc1bc5ef75803289e89d
rui <rui@chenrui.dev>
2021-10-27 09:44:48 +0000

    apidoc: update 0.50.1 bottle.

--- Formula/apidoc.rb
+++ Formula/apidoc.rb
@@ -8,10 +8,12 @@ class Apidoc < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7d5a43ec7bf060ebd82429bbb9653f8450ebaaed5a31aaec0c3c9ccec57e65cd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3d30068d8e095bb6fd05505a69cdfe6272e032ad157dc07e304195317ea24664"
-    sha256 cellar: :any_skip_relocation, catalina:      "3d30068d8e095bb6fd05505a69cdfe6272e032ad157dc07e304195317ea24664"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33af4cb13226ef09da336332ae043fbf3628df1fbc740b3f0e87fd53b08d4039"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "98d026c39e2734a6efadad3105e6007a1bef42c36d3f3ab10466cd6ad5a27df8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d5a43ec7bf060ebd82429bbb9653f8450ebaaed5a31aaec0c3c9ccec57e65cd"
+    sha256 cellar: :any_skip_relocation, monterey:       "3d30068d8e095bb6fd05505a69cdfe6272e032ad157dc07e304195317ea24664"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3d30068d8e095bb6fd05505a69cdfe6272e032ad157dc07e304195317ea24664"
+    sha256 cellar: :any_skip_relocation, catalina:       "3d30068d8e095bb6fd05505a69cdfe6272e032ad157dc07e304195317ea24664"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33af4cb13226ef09da336332ae043fbf3628df1fbc740b3f0e87fd53b08d4039"
   end
 
   depends_on "node"
