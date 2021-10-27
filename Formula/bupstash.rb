b591122946a96b322e95d3608f278b4df31a3f60
rui <rui@chenrui.dev>
2021-10-27 02:44:51 +0000

    bupstash: update 0.10.2 bottle.

--- Formula/bupstash.rb
+++ Formula/bupstash.rb
@@ -6,11 +6,13 @@ class Bupstash < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ed18dd9a09063de9d3be7739ab048f977dc89b30d4f17ca6e625dfca7bfd4e6a"
-    sha256 cellar: :any,                 big_sur:       "f30a220780b8d94cbbeec8faf0dc13464a38f7029116ee587ea39279c4e3e1cb"
-    sha256 cellar: :any,                 catalina:      "0488ba4d72c967d3d2ba923930f248a0bb60122f773f98f4438cfa8b2097084d"
-    sha256 cellar: :any,                 mojave:        "b637f71d3cff79b2621c436f597e313cb76d1eef015ed501269b73e0832cc5d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c4123bcb3b3c06fe08f42913d172f0cf9659b0b414c06e59515fd7a52722769"
+    sha256 cellar: :any,                 arm64_monterey: "cad066197a7c2256ba0b083cfc21b426ba1e5e5fb3e74d2f453e595888cb1931"
+    sha256 cellar: :any,                 arm64_big_sur:  "ed18dd9a09063de9d3be7739ab048f977dc89b30d4f17ca6e625dfca7bfd4e6a"
+    sha256 cellar: :any,                 monterey:       "500e9045af3a7c3dede9244f74c21e5701a649a183001b01985153f2e47af87c"
+    sha256 cellar: :any,                 big_sur:        "f30a220780b8d94cbbeec8faf0dc13464a38f7029116ee587ea39279c4e3e1cb"
+    sha256 cellar: :any,                 catalina:       "0488ba4d72c967d3d2ba923930f248a0bb60122f773f98f4438cfa8b2097084d"
+    sha256 cellar: :any,                 mojave:         "b637f71d3cff79b2621c436f597e313cb76d1eef015ed501269b73e0832cc5d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c4123bcb3b3c06fe08f42913d172f0cf9659b0b414c06e59515fd7a52722769"
   end
 
   depends_on "pkg-config" => :build
