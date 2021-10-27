b3121d6fd593edd666d7e01724a2be0eb0b29945
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:16:17 +0000

    terrahelp: update 0.7.5 bottle.

--- Formula/terrahelp.rb
+++ Formula/terrahelp.rb
@@ -7,11 +7,13 @@ class Terrahelp < Formula
   head "https://github.com/opencredo/terrahelp.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a416eab4b11b794fd807c6a6ce9d1fd87ebf8a4bfedeaa6ac45eeb9f6c092d52"
-    sha256 cellar: :any_skip_relocation, big_sur:       "da129e3fa2f21f00fe0b054d5510509d39d6d26cbd58efa7d06297363254fcc7"
-    sha256 cellar: :any_skip_relocation, catalina:      "8db95e8da4909b68eaa18a9fab2e38769fcfa79426b3c1a53a4ac9d5315092fd"
-    sha256 cellar: :any_skip_relocation, mojave:        "10cfea117c1dd3d1e1f5c7b609b3c299c4544b46c87d996836971d1185e77004"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6fdaed3d9218418ada94b13cbdfc0bd156ac0b5b44294a95674df07a3e66147a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1de2d581302493095a12fc646062b1ea074aa792a36e81d37827438e832599a3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a416eab4b11b794fd807c6a6ce9d1fd87ebf8a4bfedeaa6ac45eeb9f6c092d52"
+    sha256 cellar: :any_skip_relocation, monterey:       "e04b7ade448da1809858ef7cefea05f34b5670d4b159b3f67d8700c0572201d7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "da129e3fa2f21f00fe0b054d5510509d39d6d26cbd58efa7d06297363254fcc7"
+    sha256 cellar: :any_skip_relocation, catalina:       "8db95e8da4909b68eaa18a9fab2e38769fcfa79426b3c1a53a4ac9d5315092fd"
+    sha256 cellar: :any_skip_relocation, mojave:         "10cfea117c1dd3d1e1f5c7b609b3c299c4544b46c87d996836971d1185e77004"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6fdaed3d9218418ada94b13cbdfc0bd156ac0b5b44294a95674df07a3e66147a"
   end
 
   depends_on "go" => :build
