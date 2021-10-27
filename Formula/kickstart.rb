ba2b08919b3de376963892cbc15c0255ced96d28
rui <rui@chenrui.dev>
2021-10-27 06:12:05 +0000

    kickstart: update 0.3.0 bottle.

--- Formula/kickstart.rb
+++ Formula/kickstart.rb
@@ -6,10 +6,12 @@ class Kickstart < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "aa5e270b58a74bf343c3b4482c6f4fcb5badca3d552d50c37fbb2f51447dcea1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "312faa72771af3a139b1de745f164e7a95ac8705ab3db7f2bf6a7ddc451f372f"
-    sha256 cellar: :any_skip_relocation, catalina:      "6dbb7cc02996cbde1ead056a058e26adc420f6d7d8e0892ed072659c72e6d141"
-    sha256 cellar: :any_skip_relocation, mojave:        "ba2d8ea34e7bf6aa226c2c6dd5ffeea606a2046113250fdb85a3d92655456bbc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6cae28a7a5048e193e6c901b0e68645d7f9858fdbce23b0d94fc9ccfec42f154"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa5e270b58a74bf343c3b4482c6f4fcb5badca3d552d50c37fbb2f51447dcea1"
+    sha256 cellar: :any_skip_relocation, monterey:       "67ab36ce6bc37d9092b3a6959e074975d7aa5532337087595d7df5ef2235cb9d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "312faa72771af3a139b1de745f164e7a95ac8705ab3db7f2bf6a7ddc451f372f"
+    sha256 cellar: :any_skip_relocation, catalina:       "6dbb7cc02996cbde1ead056a058e26adc420f6d7d8e0892ed072659c72e6d141"
+    sha256 cellar: :any_skip_relocation, mojave:         "ba2d8ea34e7bf6aa226c2c6dd5ffeea606a2046113250fdb85a3d92655456bbc"
   end
 
   depends_on "rust" => :build
