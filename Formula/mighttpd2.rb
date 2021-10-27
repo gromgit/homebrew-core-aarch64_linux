d938c54bbdda1d70f9a8f3134f556b5d7eee595d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 20:17:07 +0000

    mighttpd2: update 4.0.0 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -6,10 +6,12 @@ class Mighttpd2 < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3a3795338aecf816f75d65a1b94306a7ad10ce00ce06f542ba4ba70c9d6633b3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b47d66f64181fb396631c5d7811a45af16b15fb709b04f877beb1dd53defe2bf"
-    sha256 cellar: :any_skip_relocation, catalina:      "ccf3d9edc4c2f4816fb03a3de973d28d8e19798c696d57eab63cbbebe9064af3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "233acd9876e3aff534a0aadd6f78d6f0f187af11d56a676cc3f15bf3d37c02ec"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cb4f3d679f45eb1cafeef1382405c56ff3a12163557921f522d04d8515072556"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a3795338aecf816f75d65a1b94306a7ad10ce00ce06f542ba4ba70c9d6633b3"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b40c73cbea84c5a873d4fb2a42434e21fc78ee605fb034e74be85c1a845cf55"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b47d66f64181fb396631c5d7811a45af16b15fb709b04f877beb1dd53defe2bf"
+    sha256 cellar: :any_skip_relocation, catalina:       "ccf3d9edc4c2f4816fb03a3de973d28d8e19798c696d57eab63cbbebe9064af3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "233acd9876e3aff534a0aadd6f78d6f0f187af11d56a676cc3f15bf3d37c02ec"
   end
 
   depends_on "cabal-install" => :build
