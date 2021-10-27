cbbb0f9e3f878e07573f31d67861af9b390c7800
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:24:06 +0000

    tile38: update 1.26.4 bottle.

--- Formula/tile38.rb
+++ Formula/tile38.rb
@@ -8,10 +8,12 @@ class Tile38 < Formula
   head "https://github.com/tidwall/tile38.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "286538bcce0ce8fcc41120a3bcc945d44aac13c58e997e0dfe650db97889c14b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e176bbffe3d06c909d3259a25c2c184987e0f9d3b80e860df341ab6f9c74520a"
-    sha256 cellar: :any_skip_relocation, catalina:      "f0b427df3a46e118f78de35a56a7e8baa97b9f20cb324da79d61f5d1309cd5c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d9a994ccbfa467bef9c94b93e70366629d642921d6c903892e32077bf6109563"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dafe973e532fb1bff31914de94ed01c385028aef564f00cf9f86129da30a910f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "286538bcce0ce8fcc41120a3bcc945d44aac13c58e997e0dfe650db97889c14b"
+    sha256 cellar: :any_skip_relocation, monterey:       "d89cbd7c487486daa2a7dab89e0e7c687ea82c1f37b34196e29d8876c177bcf3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e176bbffe3d06c909d3259a25c2c184987e0f9d3b80e860df341ab6f9c74520a"
+    sha256 cellar: :any_skip_relocation, catalina:       "f0b427df3a46e118f78de35a56a7e8baa97b9f20cb324da79d61f5d1309cd5c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9a994ccbfa467bef9c94b93e70366629d642921d6c903892e32077bf6109563"
   end
 
   depends_on "go" => :build
