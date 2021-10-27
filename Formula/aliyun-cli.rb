3ab294d4f06ba3c56f409a428fdeccbb8f4f3ec1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:44:45 +0000

    aliyun-cli: update 3.0.96 bottle.

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -7,10 +7,12 @@ class AliyunCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9f0ccc33ac63a19ea74b3f4f33126fce2e3ca18b2a27c82875ee729e31d06c5b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b16903d1164d39420bca409558c7060dd19d214b64714d235b19d3400309b4a6"
-    sha256 cellar: :any_skip_relocation, catalina:      "aa9e911d274c67a5d21b430e9df0f1a2f0a77751ff208ab5e28f90b53d19170d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e22676203dc94429052fe76b9773452e4843c112c1e171f2ff9593a8b28e8c9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "063212522142db5ce61bf8818b31b173627f5958899ad66618ebefeb0b345dc5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f0ccc33ac63a19ea74b3f4f33126fce2e3ca18b2a27c82875ee729e31d06c5b"
+    sha256 cellar: :any_skip_relocation, monterey:       "67ee72f86f85720fc2db1aa9714c5167b863af1e4e915dac2c205c4a447b2fc7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b16903d1164d39420bca409558c7060dd19d214b64714d235b19d3400309b4a6"
+    sha256 cellar: :any_skip_relocation, catalina:       "aa9e911d274c67a5d21b430e9df0f1a2f0a77751ff208ab5e28f90b53d19170d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e22676203dc94429052fe76b9773452e4843c112c1e171f2ff9593a8b28e8c9"
   end
 
   depends_on "go" => :build
