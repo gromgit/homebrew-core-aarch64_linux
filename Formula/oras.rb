2b1472c7c5ed289cc88bdaac8aa76db82b373f73
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 03:43:46 +0800

    oras: update 0.12.0 bottle.

--- Formula/oras.rb
+++ Formula/oras.rb
@@ -6,11 +6,12 @@ class Oras < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0b3b53df6013d9b3ad1ab832a8d0daad2ae4fbb017adf387b7f6d13e5e2d76bd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "18d86fcc6965357bb00cc945ef6ee888f874eb487bb341a1389cf8f9cac626d1"
-    sha256 cellar: :any_skip_relocation, catalina:      "d08f9849ddb604f0fd61574afd8f2dad30fc38022b14b547c2abd50bb6ef97d9"
-    sha256 cellar: :any_skip_relocation, mojave:        "ef76f4634fe95c6c000f90c3ddb323b8edf694f60e5e6eed4e416bef3b874f15"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64c8e274d78dad6bf79c17b8cdc2d5e31738ff761704bef3e7be4f550305ad9c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c6d96bff8a617c66e247fdeff34859df2969d6d4ea17f9ce8bde54e32f688d76"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b3b53df6013d9b3ad1ab832a8d0daad2ae4fbb017adf387b7f6d13e5e2d76bd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "18d86fcc6965357bb00cc945ef6ee888f874eb487bb341a1389cf8f9cac626d1"
+    sha256 cellar: :any_skip_relocation, catalina:       "d08f9849ddb604f0fd61574afd8f2dad30fc38022b14b547c2abd50bb6ef97d9"
+    sha256 cellar: :any_skip_relocation, mojave:         "ef76f4634fe95c6c000f90c3ddb323b8edf694f60e5e6eed4e416bef3b874f15"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "64c8e274d78dad6bf79c17b8cdc2d5e31738ff761704bef3e7be4f550305ad9c"
   end
 
   depends_on "go" => :build
