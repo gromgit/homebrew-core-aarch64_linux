ed06f774302011002c4e1ffc27fa39cbfeaa18d5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:46:44 +0000

    anycable-go: update 1.1.3 bottle.

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -12,11 +12,13 @@ class AnycableGo < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1cc2c4cf90b5b993502fc9199deed645c82034fcf0c89cc32abd90499583e58a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9cfcd34dd576979ac4b61479be2e6cb0768a8f91220ed1186545dec372718258"
-    sha256 cellar: :any_skip_relocation, catalina:      "f1d879be44533a9bfbee54b30b0a5418299544ffd92a124176f62e1ec28489e5"
-    sha256 cellar: :any_skip_relocation, mojave:        "b537ae6c62cd1ed8fad1bb6c7b2238423df93158e6e11c8c2e8cddb2197aeba2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f56dadd5ec3a0fcf20afbec6164a8653b3252bd223f394a72580101b1fa98e2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8bbb800af6e5a00019fc330f7b63f9135eacb2f8601619df0db92f0d2be5801f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1cc2c4cf90b5b993502fc9199deed645c82034fcf0c89cc32abd90499583e58a"
+    sha256 cellar: :any_skip_relocation, monterey:       "f9d9bff3bd259616d59c9be97792e9a5edfcb7907c8828e81e8c3cfe6269b3db"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9cfcd34dd576979ac4b61479be2e6cb0768a8f91220ed1186545dec372718258"
+    sha256 cellar: :any_skip_relocation, catalina:       "f1d879be44533a9bfbee54b30b0a5418299544ffd92a124176f62e1ec28489e5"
+    sha256 cellar: :any_skip_relocation, mojave:         "b537ae6c62cd1ed8fad1bb6c7b2238423df93158e6e11c8c2e8cddb2197aeba2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f56dadd5ec3a0fcf20afbec6164a8653b3252bd223f394a72580101b1fa98e2"
   end
 
   depends_on "go" => :build
