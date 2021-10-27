64ba6be2eaa84b961e66857832569d63791f12e4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:27:15 +0000

    plow: update 1.1.0 bottle.

--- Formula/plow.rb
+++ Formula/plow.rb
@@ -6,10 +6,12 @@ class Plow < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eb13e15c3cb95c4e9ae36c498f5eafbbe2401bba7846e3eef7764e56927bafb2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "35efcee910b6391bcb00a74e174bd2c7961742c75b7ce5d81cf739f5f8907e41"
-    sha256 cellar: :any_skip_relocation, catalina:      "d30cae49b41472ed81c454b2e5ef2031e1e46307f8b838b4892fedd80c41d318"
-    sha256 cellar: :any_skip_relocation, mojave:        "beb6ba83885124eb4ba8e2e4bb5bcaf8951bb574ef8d9b94c2c48307b92caff6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "efe290ac3d86bec107df0e22c15f4cbdf960dc88df929b4a7b578440b3412389"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb13e15c3cb95c4e9ae36c498f5eafbbe2401bba7846e3eef7764e56927bafb2"
+    sha256 cellar: :any_skip_relocation, monterey:       "f42f7ff80afff18956562d7c1d98a09571efb50951fe88e8363d2a16caa74dd0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "35efcee910b6391bcb00a74e174bd2c7961742c75b7ce5d81cf739f5f8907e41"
+    sha256 cellar: :any_skip_relocation, catalina:       "d30cae49b41472ed81c454b2e5ef2031e1e46307f8b838b4892fedd80c41d318"
+    sha256 cellar: :any_skip_relocation, mojave:         "beb6ba83885124eb4ba8e2e4bb5bcaf8951bb574ef8d9b94c2c48307b92caff6"
   end
 
   depends_on "go" => :build
