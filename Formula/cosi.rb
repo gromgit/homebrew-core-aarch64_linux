c4914c3693d5ec9fc6c9da112cae98cfebde96e6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:47:47 +0000

    cosi: update 0.8.6 bottle.

--- Formula/cosi.rb
+++ Formula/cosi.rb
@@ -8,12 +8,14 @@ class Cosi < Formula
   license "AGPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5b529b010f6c26cb971f84630f3fdf1fcf185991d7606e5eeee985d086cc7e33"
-    sha256 cellar: :any_skip_relocation, big_sur:       "80422d33e38e99d9d70958a4b8edfcae2e740e5392b4b5d32ead4a4d2abf1b99"
-    sha256 cellar: :any_skip_relocation, catalina:      "30bbb457c0fb67ee264331e434068a4a747ece4cbc536cb75d289a06e93988e2"
-    sha256 cellar: :any_skip_relocation, mojave:        "2ddd695441977b1cd435fbae28d9aa864d48b7a90ec24971348d91b5d0e551df"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "00663999a04ee29f52e334022cc828d7ebe89a442f1e713afb2167112f4ebf75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b0af35c14fbdff8b9ddf46a325903d87fd3d5e7c4d7ea6d26ba2d4c8c4ac8201"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "80018e20758565f4559b3b6b1477e7b4d8f6654206056d727dcee2d3196b69ae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5b529b010f6c26cb971f84630f3fdf1fcf185991d7606e5eeee985d086cc7e33"
+    sha256 cellar: :any_skip_relocation, monterey:       "545693cdefb146ce05e92a289ab373fca3eccaaf064f078d842b1c0db87a19e7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "80422d33e38e99d9d70958a4b8edfcae2e740e5392b4b5d32ead4a4d2abf1b99"
+    sha256 cellar: :any_skip_relocation, catalina:       "30bbb457c0fb67ee264331e434068a4a747ece4cbc536cb75d289a06e93988e2"
+    sha256 cellar: :any_skip_relocation, mojave:         "2ddd695441977b1cd435fbae28d9aa864d48b7a90ec24971348d91b5d0e551df"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "00663999a04ee29f52e334022cc828d7ebe89a442f1e713afb2167112f4ebf75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0af35c14fbdff8b9ddf46a325903d87fd3d5e7c4d7ea6d26ba2d4c8c4ac8201"
   end
 
   # Deprecated in favor of the Cothority `blcosi` package.
