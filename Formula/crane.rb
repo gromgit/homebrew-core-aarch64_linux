c3072e84b4041c7e884104f320327aacf4103b8e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:42:55 +0000

    crane: update 0.6.0 bottle.

--- Formula/crane.rb
+++ Formula/crane.rb
@@ -6,11 +6,13 @@ class Crane < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1ee6c8238a5e4a08025a0b89f7deb81d1b92dbad227c718b8188cc5524769aef"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b95a968ba856bdb163b8a55bfe5d88308c3fc5f68e0f732e12fba0a947fd7496"
-    sha256 cellar: :any_skip_relocation, catalina:      "622eb564e0cf74344e570722f144032caf795b0f65be6e897046add7abcf4ccf"
-    sha256 cellar: :any_skip_relocation, mojave:        "41a0966f974b546da05413b9d16632e32852d8109a247d4cccfefe8c6bf03b72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63c3a660eeac1a72c066eb8f9cc4dd40b542686f247d031b4cda84830411b3bd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3be0fec76833c21b2fdddd3c6003a96e5578aad99638416a34c6285f91621db4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1ee6c8238a5e4a08025a0b89f7deb81d1b92dbad227c718b8188cc5524769aef"
+    sha256 cellar: :any_skip_relocation, monterey:       "472727f4fe3ab1afc3abada8d197e8310648c240216e4e80031cff41a7ea6f10"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b95a968ba856bdb163b8a55bfe5d88308c3fc5f68e0f732e12fba0a947fd7496"
+    sha256 cellar: :any_skip_relocation, catalina:       "622eb564e0cf74344e570722f144032caf795b0f65be6e897046add7abcf4ccf"
+    sha256 cellar: :any_skip_relocation, mojave:         "41a0966f974b546da05413b9d16632e32852d8109a247d4cccfefe8c6bf03b72"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63c3a660eeac1a72c066eb8f9cc4dd40b542686f247d031b4cda84830411b3bd"
   end
 
   depends_on "go" => :build
