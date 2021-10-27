f7b912c67c1cb3369e2227a160a701abdb9c6352
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:00:10 +0000

    periscope: update 0.3.1 bottle.

--- Formula/periscope.rb
+++ Formula/periscope.rb
@@ -8,11 +8,13 @@ class Periscope < Formula
   head "https://github.com/anishathalye/periscope.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a316061e8574f8306a0db428030a80b1d881fa01f44d3fb5e11fbf262eb005dd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ce293d18056b44958098f6950986676ba6747df28fda897ef6f7f9e83c19b724"
-    sha256 cellar: :any_skip_relocation, catalina:      "b27894c43a915698a3667d5c77ee120e097195bb42f039ad12fc8aabb684f168"
-    sha256 cellar: :any_skip_relocation, mojave:        "175b7fa2671aa807ae3574326c65efec9e2ec7599ce4645ffcbb4ee4b3b14056"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2998259acfde7a53687bf3f46df9c7258cefe94ee1a7a6e5bfca3ed187f969c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee724c8be393c6f09fb3d4d68091cfdc51ff1a1647391a9a53c901ee9a0f050b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a316061e8574f8306a0db428030a80b1d881fa01f44d3fb5e11fbf262eb005dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "9a1e52d7026e676bb53e8c8f11d4a93523d8b8a698a3225f33dcc71de215f02e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce293d18056b44958098f6950986676ba6747df28fda897ef6f7f9e83c19b724"
+    sha256 cellar: :any_skip_relocation, catalina:       "b27894c43a915698a3667d5c77ee120e097195bb42f039ad12fc8aabb684f168"
+    sha256 cellar: :any_skip_relocation, mojave:         "175b7fa2671aa807ae3574326c65efec9e2ec7599ce4645ffcbb4ee4b3b14056"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2998259acfde7a53687bf3f46df9c7258cefe94ee1a7a6e5bfca3ed187f969c"
   end
 
   depends_on "go" => :build
