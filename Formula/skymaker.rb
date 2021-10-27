13f0450d42fdebc549b0d012083d0276ba299626
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:18:35 +0000

    skymaker: update 3.10.5 bottle.

--- Formula/skymaker.rb
+++ Formula/skymaker.rb
@@ -9,6 +9,7 @@ class Skymaker < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "44e3eadc4f4a2984ffff332f93e9aa7a383b7a395ccd9fd6714a237a48ed72d0"
+    sha256 cellar: :any,                 monterey:      "9c6b33e5127676aaec7d1d8c0232fb78be8a0bb8a5ee4fc429e0516b45e0846c"
     sha256 cellar: :any,                 big_sur:       "2262e9a1f11f41c75fc8ccb47717f779bbb62bf86ddbabd42975fe684278efb9"
     sha256 cellar: :any,                 catalina:      "af78e7af9c84517e8f7db071ef3718a34eafc39d6eac3357d77ee183d4fe2cdf"
     sha256 cellar: :any,                 mojave:        "ef2182885eb6952289057ce2756ac56ec9a88397e746b694529a937eaa28b943"
