58a322595b3f8482c3f1321bdd80ed01a995f8f3
rui <rui@chenrui.dev>
2021-10-27 06:02:20 +0000

    inko: update 0.9.0_1 bottle.

--- Formula/inko.rb
+++ Formula/inko.rb
@@ -8,11 +8,13 @@ class Inko < Formula
   head "https://gitlab.com/inko-lang/inko.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a7f73e96ff5076466770d434090aa1c49316faa2092dda709554a642bab3e292"
-    sha256 cellar: :any,                 big_sur:       "ebceafed0b8fb72511c5788fe525ad83e204b9fce30116a76acd560ce6c36ba8"
-    sha256 cellar: :any,                 catalina:      "fa964ccada840c98ea19efad5e62cda6f73df789b4b820148c0af2ff3793347e"
-    sha256 cellar: :any,                 mojave:        "536ac0253c59601ac2f717af644997e37a6a801879904f99275c4112fb18c83c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4092e51e09e093d158314a50094730c3bbc4f794fb157ddd4812d790b8d2d51e"
+    sha256 cellar: :any,                 arm64_monterey: "962edbc75373209c6aa77517acd6b150cb4ddb799a3cf513485b6566aca73a65"
+    sha256 cellar: :any,                 arm64_big_sur:  "a7f73e96ff5076466770d434090aa1c49316faa2092dda709554a642bab3e292"
+    sha256 cellar: :any,                 monterey:       "0c4ad9650cf140ed89a8065f9efc257f549edb9af17a744798da40f887c7ea08"
+    sha256 cellar: :any,                 big_sur:        "ebceafed0b8fb72511c5788fe525ad83e204b9fce30116a76acd560ce6c36ba8"
+    sha256 cellar: :any,                 catalina:       "fa964ccada840c98ea19efad5e62cda6f73df789b4b820148c0af2ff3793347e"
+    sha256 cellar: :any,                 mojave:         "536ac0253c59601ac2f717af644997e37a6a801879904f99275c4112fb18c83c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4092e51e09e093d158314a50094730c3bbc4f794fb157ddd4812d790b8d2d51e"
   end
 
   depends_on "coreutils" => :build
