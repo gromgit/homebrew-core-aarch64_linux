6b3d8f93f7d42c0cc7221c975e761bddbd438bfe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:21:24 +0000

    the_platinum_searcher: update 2.2.0 bottle.

--- Formula/the_platinum_searcher.rb
+++ Formula/the_platinum_searcher.rb
@@ -8,11 +8,13 @@ class ThePlatinumSearcher < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0f3097b2d88f4b8479ecb3e3439f6688656fc1c5e20f18a4c300edf9ea953874"
-    sha256 cellar: :any_skip_relocation, big_sur:       "63cc973af4c1fc612acb86c7a928f1680f84db7edfae52f374b95925c00761dc"
-    sha256 cellar: :any_skip_relocation, catalina:      "79066cac44fd6cd21b8feadc9737045f98846832f15bd2a2e1fdaae3a8165e6d"
-    sha256 cellar: :any_skip_relocation, mojave:        "6b7fb2ff2ca2b5a0d264a7733a59eb0e1b68e211d15a261f6bbcab5664bb6ff7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6ba21397a0538c990656c24ba157c5650ea62a26da823afdba238da37226d3b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "920eca3416a24f405f47f9422797e405768da23008de8ec12f3beb292e8b6be2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0f3097b2d88f4b8479ecb3e3439f6688656fc1c5e20f18a4c300edf9ea953874"
+    sha256 cellar: :any_skip_relocation, monterey:       "b9f8d4628e265fe8cee61b17d40a0695316940d4805d84ee98de11abc6dc54fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "63cc973af4c1fc612acb86c7a928f1680f84db7edfae52f374b95925c00761dc"
+    sha256 cellar: :any_skip_relocation, catalina:       "79066cac44fd6cd21b8feadc9737045f98846832f15bd2a2e1fdaae3a8165e6d"
+    sha256 cellar: :any_skip_relocation, mojave:         "6b7fb2ff2ca2b5a0d264a7733a59eb0e1b68e211d15a261f6bbcab5664bb6ff7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6ba21397a0538c990656c24ba157c5650ea62a26da823afdba238da37226d3b"
   end
 
   depends_on "go" => :build
