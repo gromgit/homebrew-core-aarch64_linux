a2ba679d04988deebeaede2cc0b0e766a34fd689
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 10:03:16 +0000

    tor: update 0.4.6.8 bottle.

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -19,10 +19,12 @@ class Tor < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "efc314a687bb144fd4a655c7c9555d7688071749c2ceacb758c103cc30125b49"
-    sha256 big_sur:       "d075f702a798d00bed7bdfeead10ec0e6ef2be5953f7e62382840cd0eefab7d5"
-    sha256 catalina:      "d797c5390176cac9edbc7df042fa3421dc93dca3f291fa2340d37c97292c6d00"
-    sha256 x86_64_linux:  "34220f2e859a6d3d22f47a59fb4134d28ffe3c0b50b5d0badb8d217e70bbd167"
+    sha256 arm64_monterey: "c3762e94997d6b6f2ecb16aacc3764737b766e29f919803d3e53922d8742bbcb"
+    sha256 arm64_big_sur:  "efc314a687bb144fd4a655c7c9555d7688071749c2ceacb758c103cc30125b49"
+    sha256 monterey:       "90e8e0f7e240541020b1d425ba7ea312ca7f5c7721813396fcb151c57777d1d0"
+    sha256 big_sur:        "d075f702a798d00bed7bdfeead10ec0e6ef2be5953f7e62382840cd0eefab7d5"
+    sha256 catalina:       "d797c5390176cac9edbc7df042fa3421dc93dca3f291fa2340d37c97292c6d00"
+    sha256 x86_64_linux:   "34220f2e859a6d3d22f47a59fb4134d28ffe3c0b50b5d0badb8d217e70bbd167"
   end
 
   depends_on "pkg-config" => :build
