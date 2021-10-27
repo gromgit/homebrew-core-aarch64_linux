02898b4da45075d40ac719024c45c4824dbd5556
rui <rui@chenrui.dev>
2021-10-27 06:42:42 +0000

    monolith: update 2.6.1 bottle.

--- Formula/monolith.rb
+++ Formula/monolith.rb
@@ -6,11 +6,13 @@ class Monolith < Formula
   license "CC0-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f8d25cc101ab57a7b2392d748815de0d2dbf36654a4ef592b5eedf992846f17a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f665108ed7bbba69ffd99220d6d23d6baeb380fee9ce569eaef11b510c8affec"
-    sha256 cellar: :any_skip_relocation, catalina:      "03eead42ea9e5f888b968147d1975ffaa04ad470747b985c3920dfe5724f9c62"
-    sha256 cellar: :any_skip_relocation, mojave:        "3794386713e0d813abbcbc6377c943421efb94b8dde4ce01729bd8a88d206af2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e39b8d3264fb577e4afb9464c9ca88d0da5646aab9c0f0b09f0f9b48d00ba03"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e744be2aadfc192445260fc508eb69b767b353d47d732f9d3829d5da4dbcd1aa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f8d25cc101ab57a7b2392d748815de0d2dbf36654a4ef592b5eedf992846f17a"
+    sha256 cellar: :any_skip_relocation, monterey:       "c44f5f556c6e8190bf26365878d6d592dce98a42c2c535b48b33568555f041b6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f665108ed7bbba69ffd99220d6d23d6baeb380fee9ce569eaef11b510c8affec"
+    sha256 cellar: :any_skip_relocation, catalina:       "03eead42ea9e5f888b968147d1975ffaa04ad470747b985c3920dfe5724f9c62"
+    sha256 cellar: :any_skip_relocation, mojave:         "3794386713e0d813abbcbc6377c943421efb94b8dde4ce01729bd8a88d206af2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e39b8d3264fb577e4afb9464c9ca88d0da5646aab9c0f0b09f0f9b48d00ba03"
   end
 
   depends_on "pkg-config" => :build
