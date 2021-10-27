f9c21939f4ec401c748474988992d96a6ccef71b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:50:12 +0000

    okteto: update 1.14.1 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -7,11 +7,13 @@ class Okteto < Formula
   head "https://github.com/okteto/okteto.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7d9d48103383c927c43f1f70a0331cbc4bcd3b1050dea91535db1c16140c6df7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "17ae7f6ea66cdedd219d882cad965f4daacc42c134e25f1dfd3462b27d00b862"
-    sha256 cellar: :any_skip_relocation, catalina:      "5ee0985dbfbc0732806db3b0d7711fb310a8c0e78a73225f7ae0df6d24b63657"
-    sha256 cellar: :any_skip_relocation, mojave:        "69555ba18eda1cf5fe1cbdade65f8424d2128da01ed4739768b170565b5749e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05aa54d06560b4d33f1d3657550c68d2675aafc491f7c68fdac5f03df105eef7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "674773d0dc0384fb28a6336e4cff185e55ecb6709f245e1f613b1f80f2bad841"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d9d48103383c927c43f1f70a0331cbc4bcd3b1050dea91535db1c16140c6df7"
+    sha256 cellar: :any_skip_relocation, monterey:       "9be8c86b3d86f5aba8db63ff0e2713e10cc2f4483b346d2fe5d9e3c99283ab49"
+    sha256 cellar: :any_skip_relocation, big_sur:        "17ae7f6ea66cdedd219d882cad965f4daacc42c134e25f1dfd3462b27d00b862"
+    sha256 cellar: :any_skip_relocation, catalina:       "5ee0985dbfbc0732806db3b0d7711fb310a8c0e78a73225f7ae0df6d24b63657"
+    sha256 cellar: :any_skip_relocation, mojave:         "69555ba18eda1cf5fe1cbdade65f8424d2128da01ed4739768b170565b5749e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05aa54d06560b4d33f1d3657550c68d2675aafc491f7c68fdac5f03df105eef7"
   end
 
   depends_on "go" => :build
