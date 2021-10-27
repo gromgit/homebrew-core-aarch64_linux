f4860d707cad4a5a5df4d2f3f8857f386a95eabf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:37:56 +0000

    promtail: update 2.3.0 bottle.

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -10,10 +10,12 @@ class Promtail < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "61a6d1f9f6f4ffc0cde6bf6fe908ef94fbe2d0a583eb4ff7f99255e02ac3fdc3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2ac1bf4de911eaf2e6a0711dd22d87153e2503f50306bf768f12eb89b65a6be3"
-    sha256 cellar: :any_skip_relocation, catalina:      "86f751b4b098921337fd4e2bf4cd7c38b81d795d2cca740a3a0713f860f099fa"
-    sha256 cellar: :any_skip_relocation, mojave:        "450359cf7d3e027d4f13f17f9bdb80d35e80f8ad03ff5837c91f5120a84ef9b5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc05072ea8bf10d097a7fa84a87c0c162eecc44117578d4c21bfad77f8f51560"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "61a6d1f9f6f4ffc0cde6bf6fe908ef94fbe2d0a583eb4ff7f99255e02ac3fdc3"
+    sha256 cellar: :any_skip_relocation, monterey:       "22e0558412e1918d5eaf2a21961d38242923ddf5bd83e5752e778d89897dbc25"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2ac1bf4de911eaf2e6a0711dd22d87153e2503f50306bf768f12eb89b65a6be3"
+    sha256 cellar: :any_skip_relocation, catalina:       "86f751b4b098921337fd4e2bf4cd7c38b81d795d2cca740a3a0713f860f099fa"
+    sha256 cellar: :any_skip_relocation, mojave:         "450359cf7d3e027d4f13f17f9bdb80d35e80f8ad03ff5837c91f5120a84ef9b5"
   end
 
   depends_on "go" => :build
