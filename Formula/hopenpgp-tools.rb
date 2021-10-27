3fb1095d00464a44dd1bdf2ae4d0b11ece7a2cd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-27 07:10:20 +0000

    hopenpgp-tools: update 0.23.6 bottle.

--- Formula/hopenpgp-tools.rb
+++ Formula/hopenpgp-tools.rb
@@ -7,12 +7,11 @@ class HopenpgpTools < Formula
   head "https://salsa.debian.org/clint/hOpenPGP.git"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "17ba6c71d6b2c1841bb4ea381a128e8a9cb590a751214a15b646602eab496e10"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b6bbc4e108cb054f5723c0196bfffbef88ccd30242f01ea223525ed30efd83a9"
-    sha256 cellar: :any_skip_relocation, catalina:      "11ee2dc0b5f625cef483385dea031640b8762367281f7382989d999ac4719655"
-    sha256 cellar: :any_skip_relocation, mojave:        "0bd4820b9761e5e89d6751ade75a1d2ff6d7b192b71189294a78fe7f8215d95b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25189440b68e211dcb2ab3cd04349a761e461462e21691305b6c0217e6984f96"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "28a2c5d4842abe810b28d5e6f4433e8b23308b68ca6c19c8b097fb27c58b5f88"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e8ca61264f0399f95be40c4583b36604b4c64c9cf6ba761d209e4e722d4178e1"
+    sha256 cellar: :any_skip_relocation, catalina:      "6b44cea08ca2095f1dd4a997ff852f862a58c0a18f56e74509061cb173405e5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3fac0a93a10aa5a26607c224db66c19ab6809776791200d14fb254991af88864"
   end
 
   depends_on "cabal-install" => :build
