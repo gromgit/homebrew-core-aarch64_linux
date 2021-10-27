c551f57d23cd2f97a75b715dff8eacdac94c4b23
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:00:54 +0000

    goreman: update 0.3.8 bottle.

--- Formula/goreman.rb
+++ Formula/goreman.rb
@@ -12,11 +12,13 @@ class Goreman < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2f01e123072da262770e58be58d02cd4420109afc60ad4a9b3e40736f252ca91"
-    sha256 cellar: :any_skip_relocation, big_sur:       "379ff164b5c3f6cd46a38fcbee2763bf5720f968999752aa361eb12c9fc280fa"
-    sha256 cellar: :any_skip_relocation, catalina:      "9659197993188b2456b33a701f636bcbde07f8585a8bb3409c13ed85296fe687"
-    sha256 cellar: :any_skip_relocation, mojave:        "81050a32c568ca72e553e74e1758b9767cfe938252c94bffd9c87b7151a6c29a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2a5eb0ed131894cccafe025849ac67541c18658e4658fb3a33cddc0bb0a71c7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c848d97a3dd01b0766e9e150f1ec348bc678e09023f52beaa01fd37376a844ce"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f01e123072da262770e58be58d02cd4420109afc60ad4a9b3e40736f252ca91"
+    sha256 cellar: :any_skip_relocation, monterey:       "a4b7a2a1678d86ee83ead87697ab42c15493a80d367a642d9aec30359b3e59b8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "379ff164b5c3f6cd46a38fcbee2763bf5720f968999752aa361eb12c9fc280fa"
+    sha256 cellar: :any_skip_relocation, catalina:       "9659197993188b2456b33a701f636bcbde07f8585a8bb3409c13ed85296fe687"
+    sha256 cellar: :any_skip_relocation, mojave:         "81050a32c568ca72e553e74e1758b9767cfe938252c94bffd9c87b7151a6c29a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f2a5eb0ed131894cccafe025849ac67541c18658e4658fb3a33cddc0bb0a71c7"
   end
 
   depends_on "go" => :build
