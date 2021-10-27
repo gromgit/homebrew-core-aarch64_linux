d71f33d618abf4f3600baa1c39eef8229f4ef5f3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 18:11:46 +0000

    elvish: update 0.16.3 bottle.

--- Formula/elvish.rb
+++ Formula/elvish.rb
@@ -7,11 +7,13 @@ class Elvish < Formula
   head "https://github.com/elves/elvish.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "22868343fa4e1cf95ff4c59c5a7d88216fd497e3c1ef4bce63ebd80919725cb2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "baca586af00fca196d69e0e8b1b3309df1c0b8f7d72e4089fc20f8e1a2a1bbb4"
-    sha256 cellar: :any_skip_relocation, catalina:      "86453d36b3aed50cab567de1a763204f0162eb8b41ac039c7061c94cdaca630d"
-    sha256 cellar: :any_skip_relocation, mojave:        "c345a3ec54ff058d8fbd86a97948412e811785ccefe097b3defeb0061eaf0946"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7397b282b7170800c11579efe0bce701861703966dccc32113dc870345ca43e8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0d622f7dbe863693b07eb3a03335d6633d952f924d2af276bc2cd3c915961607"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22868343fa4e1cf95ff4c59c5a7d88216fd497e3c1ef4bce63ebd80919725cb2"
+    sha256 cellar: :any_skip_relocation, monterey:       "408a773e781c66875487fbc1a9eb97cdd9f6d42f29301ffbc199112a17e84747"
+    sha256 cellar: :any_skip_relocation, big_sur:        "baca586af00fca196d69e0e8b1b3309df1c0b8f7d72e4089fc20f8e1a2a1bbb4"
+    sha256 cellar: :any_skip_relocation, catalina:       "86453d36b3aed50cab567de1a763204f0162eb8b41ac039c7061c94cdaca630d"
+    sha256 cellar: :any_skip_relocation, mojave:         "c345a3ec54ff058d8fbd86a97948412e811785ccefe097b3defeb0061eaf0946"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7397b282b7170800c11579efe0bce701861703966dccc32113dc870345ca43e8"
   end
 
   depends_on "go" => :build
