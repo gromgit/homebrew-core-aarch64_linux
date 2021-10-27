911ff175f04bc090a521c3dd28b4cdd3178cf455
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:09:58 +0000

    tctl: update 1.13.0 bottle.

--- Formula/tctl.rb
+++ Formula/tctl.rb
@@ -6,10 +6,12 @@ class Tctl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eca11d4f54073f3f0ae364c34536f21f4f35cd7f3291d212ba13fbfe109db226"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bf0403eaf30235ef8e05cb8176ebe409d065241df9646e6990666d955d3ccfb2"
-    sha256 cellar: :any_skip_relocation, catalina:      "26d9725f94c88a64f66880b89a3b82b6532617ec94a2d9bf4eff10b030bde97f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8bed6d82ab4fb516cf1c4ac8cc715f30b32c29b5e74fe6683d6be77047da15c6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2361eec4b72cd5c7918e2b8a280ec93a608e2778095f03a2831e109f49c4fd4c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eca11d4f54073f3f0ae364c34536f21f4f35cd7f3291d212ba13fbfe109db226"
+    sha256 cellar: :any_skip_relocation, monterey:       "25b19bf81b4d1ff167322860011edcef76e77b717e5862e85de41f313611c769"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bf0403eaf30235ef8e05cb8176ebe409d065241df9646e6990666d955d3ccfb2"
+    sha256 cellar: :any_skip_relocation, catalina:       "26d9725f94c88a64f66880b89a3b82b6532617ec94a2d9bf4eff10b030bde97f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bed6d82ab4fb516cf1c4ac8cc715f30b32c29b5e74fe6683d6be77047da15c6"
   end
 
   depends_on "go" => :build
