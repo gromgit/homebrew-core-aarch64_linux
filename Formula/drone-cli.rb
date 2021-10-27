114c24a09d159027190eddb6f70da74897718fc7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 13:11:52 +0000

    drone-cli: update 1.4.0 bottle.

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -7,11 +7,13 @@ class DroneCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2506e576a09c124cfe6f6a5eb6906c49b04ce0090b3e05db980df3adb6d06c1c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7a76db340f0fe6269331b081004d40d152dddf8027514cbbbc6c075909bc0669"
-    sha256 cellar: :any_skip_relocation, catalina:      "7e5bfff2d039507cf02fd17ba86b3abd254b531eae177ff40ef4bb998e7e9473"
-    sha256 cellar: :any_skip_relocation, mojave:        "e0545bddd3c764979c708a80e6904c7f1ac5d0e8634da25f70f623098c6c0104"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13b1ae94776fc650d43968654ad1fceb0f5da1edf98f1af33e1331956be50f14"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "719153ef6d32b6fa46aa19b177ed7f2b0d52285157acc687c8cbf2dd2c2884a9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2506e576a09c124cfe6f6a5eb6906c49b04ce0090b3e05db980df3adb6d06c1c"
+    sha256 cellar: :any_skip_relocation, monterey:       "3c598f53e33a0c350244e0e6030eebee5ee89729b92f1ea249ca1ca7b39255e2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7a76db340f0fe6269331b081004d40d152dddf8027514cbbbc6c075909bc0669"
+    sha256 cellar: :any_skip_relocation, catalina:       "7e5bfff2d039507cf02fd17ba86b3abd254b531eae177ff40ef4bb998e7e9473"
+    sha256 cellar: :any_skip_relocation, mojave:         "e0545bddd3c764979c708a80e6904c7f1ac5d0e8634da25f70f623098c6c0104"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13b1ae94776fc650d43968654ad1fceb0f5da1edf98f1af33e1331956be50f14"
   end
 
   depends_on "go" => :build
