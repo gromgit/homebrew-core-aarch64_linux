e95b7780d602a4c3d0a142b1bf1ba337449dfdb0
rui <rui@chenrui.dev>
2021-10-27 08:25:49 +0000

    ssh-permit-a38: update 0.2.0_1 bottle.

--- Formula/ssh-permit-a38.rb
+++ Formula/ssh-permit-a38.rb
@@ -8,12 +8,14 @@ class SshPermitA38 < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "68df2c83ee0648226ee00df59ae6837dc97cce0c105487f5307be28615b2c3f4"
-    sha256 cellar: :any,                 big_sur:       "ed50251803a0e7fc976589be8b84f2cb7a149e871e1241b3f9819d5219413e99"
-    sha256 cellar: :any,                 catalina:      "3eefd64fbbe3e4d500a69352091da85ca685a435094facc30e6942d9d5e89a1d"
-    sha256 cellar: :any,                 mojave:        "683ebbe9a6a845802f825f1775e6d861387be41fd520b648275f97a580e92398"
-    sha256 cellar: :any,                 high_sierra:   "7d82d59932bb6d721a31726efc231d043d54d180995d0119d8f8bf9fc37f3e9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f45aa317ce341e24ffc8ba21c39bc3f46935a2d7ef4e74d299dba3ed6a58f11b"
+    sha256 cellar: :any,                 arm64_monterey: "ef2eb0850cf670b3b15074cf03e601be8e4d9dacbad032044b4352b5a5827c5d"
+    sha256 cellar: :any,                 arm64_big_sur:  "68df2c83ee0648226ee00df59ae6837dc97cce0c105487f5307be28615b2c3f4"
+    sha256 cellar: :any,                 monterey:       "e81cc7c78138751c3563ad1efac04789fddb2c938888edcc66c5714a43447782"
+    sha256 cellar: :any,                 big_sur:        "ed50251803a0e7fc976589be8b84f2cb7a149e871e1241b3f9819d5219413e99"
+    sha256 cellar: :any,                 catalina:       "3eefd64fbbe3e4d500a69352091da85ca685a435094facc30e6942d9d5e89a1d"
+    sha256 cellar: :any,                 mojave:         "683ebbe9a6a845802f825f1775e6d861387be41fd520b648275f97a580e92398"
+    sha256 cellar: :any,                 high_sierra:    "7d82d59932bb6d721a31726efc231d043d54d180995d0119d8f8bf9fc37f3e9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f45aa317ce341e24ffc8ba21c39bc3f46935a2d7ef4e74d299dba3ed6a58f11b"
   end
 
   depends_on "cmake" => :build
