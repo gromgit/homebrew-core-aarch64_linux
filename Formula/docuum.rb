7cd8a8864c3937cc98c9cbbc41cdccbe7d5f77e8
rui <rui@chenrui.dev>
2021-10-27 04:35:26 +0000

    docuum: update 0.20.3 bottle.

--- Formula/docuum.rb
+++ Formula/docuum.rb
@@ -6,11 +6,13 @@ class Docuum < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fd66c12ac9e8c81c8d4899b5882d05175cbb5a70bdd10cfbbd3a58a77247a35e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fc871eccda08dd9bdd4cfb5bfc5665decd1881d63aa8aa7c30188ab4e7cc6658"
-    sha256 cellar: :any_skip_relocation, catalina:      "4480c15b9438c3d94006156fd48d6bd3f563b906ab4c4da7081d62e131b79a48"
-    sha256 cellar: :any_skip_relocation, mojave:        "d91f0318adf9203cb53e478089d657079b060f93d799948a46c73e72e2257805"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2e080677044824ea663785d3d19ef43ad48e30160b054f8a4cf053afc5dcd7b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bd0292dfe7161cd19af3258d502b34fe177db695e7d0db47432fa11ad73d2337"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd66c12ac9e8c81c8d4899b5882d05175cbb5a70bdd10cfbbd3a58a77247a35e"
+    sha256 cellar: :any_skip_relocation, monterey:       "0a716d6bce640b9ccfe9ab7b9dd0151f57065fd3ccfa518fca48aeb8e9b5f929"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fc871eccda08dd9bdd4cfb5bfc5665decd1881d63aa8aa7c30188ab4e7cc6658"
+    sha256 cellar: :any_skip_relocation, catalina:       "4480c15b9438c3d94006156fd48d6bd3f563b906ab4c4da7081d62e131b79a48"
+    sha256 cellar: :any_skip_relocation, mojave:         "d91f0318adf9203cb53e478089d657079b060f93d799948a46c73e72e2257805"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2e080677044824ea663785d3d19ef43ad48e30160b054f8a4cf053afc5dcd7b"
   end
 
   depends_on "rust" => :build
