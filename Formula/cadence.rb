5d4ea3f99fdef2aefbe08898ab940f9270785aa2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:18:46 +0000

    cadence: update 0.19.1 bottle.

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -12,11 +12,13 @@ class Cadence < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6b69859be3fd982f728c326221c31753a0455f694763bdee2e731cafcd086001"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4286ae1d8fb901fd6837027aeff331cf78a48859ed2b1dda8b9821ee0d86ea85"
-    sha256 cellar: :any_skip_relocation, catalina:      "b7e12424470850706159d244a6e315fa1a6204f87840938061dca0343d0e4019"
-    sha256 cellar: :any_skip_relocation, mojave:        "08362554c9cb4cdcd762bfe2c5843aa6a23018bf14b32a32b15506ec650ddcb3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e26504111649b1f17c167864b7e3065c6b76b411e6dcf3bffad1daf98816691"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1cbfbdc586c3a5dcd925124227dc454c2438d381ce355df9c8d147cb1f187ec6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6b69859be3fd982f728c326221c31753a0455f694763bdee2e731cafcd086001"
+    sha256 cellar: :any_skip_relocation, monterey:       "e174d47ceb7aa7416f35c574671c5271b9cbe08a3991c0b2d05c552dd8f56d1a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4286ae1d8fb901fd6837027aeff331cf78a48859ed2b1dda8b9821ee0d86ea85"
+    sha256 cellar: :any_skip_relocation, catalina:       "b7e12424470850706159d244a6e315fa1a6204f87840938061dca0343d0e4019"
+    sha256 cellar: :any_skip_relocation, mojave:         "08362554c9cb4cdcd762bfe2c5843aa6a23018bf14b32a32b15506ec650ddcb3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1e26504111649b1f17c167864b7e3065c6b76b411e6dcf3bffad1daf98816691"
   end
 
   depends_on "go" => :build
