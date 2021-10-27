cb88eee426fcb8e9ff38be886089f743c0a2af9d
rui <rui@chenrui.dev>
2021-10-27 06:35:55 +0000

    miniserve: update 0.18.0 bottle.

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -6,10 +6,12 @@ class Miniserve < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f6a7a5e19a71d96a4f5286a7c234307c83b9d0f8ec33b7853ae1c08535e7e33a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "124295b36fc81247fd5d80c507d8144919dc5e19b53c248e8a6768404538635e"
-    sha256 cellar: :any_skip_relocation, catalina:      "c98b304e5655acf5671d771286f03976ddf7dabdd11705f528fdf1d2daa8037d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bcfa53f25624a56434a33925c98de0ead2331b990444ab37d0f70fa0d66c7188"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "997a1a800deef687042a2008be644e4ab44d98527f5d451ddb1b205f476e4cbb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6a7a5e19a71d96a4f5286a7c234307c83b9d0f8ec33b7853ae1c08535e7e33a"
+    sha256 cellar: :any_skip_relocation, monterey:       "11199e4e53bf315e00fbd0f101d96ad0c0491d5e9ea17b8d2c8537a72b09474f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "124295b36fc81247fd5d80c507d8144919dc5e19b53c248e8a6768404538635e"
+    sha256 cellar: :any_skip_relocation, catalina:       "c98b304e5655acf5671d771286f03976ddf7dabdd11705f528fdf1d2daa8037d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bcfa53f25624a56434a33925c98de0ead2331b990444ab37d0f70fa0d66c7188"
   end
 
   depends_on "rust" => :build
