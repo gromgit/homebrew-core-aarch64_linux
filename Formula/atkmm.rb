657af3eb3df735c63d3566614d3436ebe4630089
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 01:37:54 +0000

    atkmm: update 2.36.1 bottle.

--- Formula/atkmm.rb
+++ Formula/atkmm.rb
@@ -6,11 +6,13 @@ class Atkmm < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "82e89f908e2a3cc349c8478eef60d7b755f968d3523f037b8b78ea4edc4031ba"
-    sha256 cellar: :any,                 big_sur:       "4b147e97566fe8f13c6e72e1d350da5c8bbabe3abb6f4966eec44f227c9fa807"
-    sha256 cellar: :any,                 catalina:      "09b0e72f5d9fd8e2a3328202d8c2b8fc7f896cdd208ab51fa75bb87c79eee53d"
-    sha256 cellar: :any,                 mojave:        "a3ed94c51be9ae22f3a3c1d6650ade4037748f2c8602db9b647effd71b4d5108"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a2e8f09c113ecfd68cac31cc2e96daecc200a4260a076ef5c58bf852774fbdc1"
+    sha256 cellar: :any,                 arm64_monterey: "62a52bf13cb3e8ae0f3de2c22fe5eb99c82e2bfcf70562e7299b81e3b0eb46ab"
+    sha256 cellar: :any,                 arm64_big_sur:  "82e89f908e2a3cc349c8478eef60d7b755f968d3523f037b8b78ea4edc4031ba"
+    sha256 cellar: :any,                 monterey:       "8d00072e06cb6fa4748cad51217427499ace42f1177e0f1d6bfbb00363329378"
+    sha256 cellar: :any,                 big_sur:        "4b147e97566fe8f13c6e72e1d350da5c8bbabe3abb6f4966eec44f227c9fa807"
+    sha256 cellar: :any,                 catalina:       "09b0e72f5d9fd8e2a3328202d8c2b8fc7f896cdd208ab51fa75bb87c79eee53d"
+    sha256 cellar: :any,                 mojave:         "a3ed94c51be9ae22f3a3c1d6650ade4037748f2c8602db9b647effd71b4d5108"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2e8f09c113ecfd68cac31cc2e96daecc200a4260a076ef5c58bf852774fbdc1"
   end
 
   depends_on "meson" => :build
