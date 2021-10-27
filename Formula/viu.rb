0518ad17ce1ec88818e4728a356544ee16e1bea0
rui <rui@chenrui.dev>
2021-10-27 09:13:08 +0000

    viu: update 1.3.0 bottle.

--- Formula/viu.rb
+++ Formula/viu.rb
@@ -7,11 +7,13 @@ class Viu < Formula
   head "https://github.com/atanunq/viu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2522e8aca392e56f1517b092a8091bd3ba5c4ed45220fd92e4daa79240ee4182"
-    sha256 cellar: :any_skip_relocation, big_sur:       "572858e897b2e307b45e02d6814b4e562682554bfaab4c1a827cbb07b679ecf7"
-    sha256 cellar: :any_skip_relocation, catalina:      "0029a7093ea3cf9e65b8eaa168ace9a0c424dced8ca233bcb74a67139be10277"
-    sha256 cellar: :any_skip_relocation, mojave:        "051cee6ee32390448b48ba56e2e9d60be22c6d9b43687f3713482d9e3dfe6017"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06d513576fa431015bbc55fd6d118aff7be75a6fa468f5a42ee567439442cb16"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2269576ed41f39754a5a19c14d683d6cb49a3f542ae2c01f5ed4bdd407f4e0dc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2522e8aca392e56f1517b092a8091bd3ba5c4ed45220fd92e4daa79240ee4182"
+    sha256 cellar: :any_skip_relocation, monterey:       "3a6fa60f802de0598b653d7625e94a4208e9f66820a31749a02b45f376e95106"
+    sha256 cellar: :any_skip_relocation, big_sur:        "572858e897b2e307b45e02d6814b4e562682554bfaab4c1a827cbb07b679ecf7"
+    sha256 cellar: :any_skip_relocation, catalina:       "0029a7093ea3cf9e65b8eaa168ace9a0c424dced8ca233bcb74a67139be10277"
+    sha256 cellar: :any_skip_relocation, mojave:         "051cee6ee32390448b48ba56e2e9d60be22c6d9b43687f3713482d9e3dfe6017"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06d513576fa431015bbc55fd6d118aff7be75a6fa468f5a42ee567439442cb16"
   end
 
   depends_on "rust" => :build
