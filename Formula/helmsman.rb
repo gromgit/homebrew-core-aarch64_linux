16b67bae98fc8dd6331df4cb678a790158d3d865
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:01:58 +0000

    helmsman: update 3.7.7 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -8,10 +8,12 @@ class Helmsman < Formula
   head "https://github.com/Praqma/helmsman.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "528848b28684e57bded1532a99813d264b3f075e1892b2ae726305bbf5cf6a14"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4e72b6f0e6417b9a507a1ae5494cc9761196bc5fa807258ad2b4a1ba24add33c"
-    sha256 cellar: :any_skip_relocation, catalina:      "434a40e852b61b4b8fc817bde900b8fef6e5ec68ee19455877fb3fdef7cd7ae2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df72db51d4c47a769f4906605f6efeaed7f88572696f9c8610ec264b7648b2d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "442de067a914f57dd6cb9c42b4076866a3aa10ccff22f0e58809168d67aee5cb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "528848b28684e57bded1532a99813d264b3f075e1892b2ae726305bbf5cf6a14"
+    sha256 cellar: :any_skip_relocation, monterey:       "ff0b1a808cdbeab15d7b08a185ce477e38b2e958d31bed0169ea1b296fdeabdb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4e72b6f0e6417b9a507a1ae5494cc9761196bc5fa807258ad2b4a1ba24add33c"
+    sha256 cellar: :any_skip_relocation, catalina:       "434a40e852b61b4b8fc817bde900b8fef6e5ec68ee19455877fb3fdef7cd7ae2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "df72db51d4c47a769f4906605f6efeaed7f88572696f9c8610ec264b7648b2d6"
   end
 
   depends_on "go" => :build
