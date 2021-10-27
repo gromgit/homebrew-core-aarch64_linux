52eab75384539571294b4d0f87571050d6fb304b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 13:04:53 +0000

    binaryen: update 102 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -7,11 +7,13 @@ class Binaryen < Formula
   head "https://github.com/WebAssembly/binaryen.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "e00fd13945ddfa707b35a2fd6b7bd20e5c1fa99fb4574ad7b52607160ead74f7"
-    sha256 cellar: :any,                 big_sur:       "580bcfb61bdf0ef6fea0b92af3428ee3484b47a87eb2bf9cee4602a5aec5b3c8"
-    sha256 cellar: :any,                 catalina:      "8dc80e3e1d1a6de47acfd1d107cdea836d2a81ea0bee7fe117423052eacc4558"
-    sha256 cellar: :any,                 mojave:        "3fbf167d4e89a81d052375286fd46f8e8604ef5875f96336c9ed13fd4a8737a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c03a84244f36a2760d5789240aed5a05c121411d2b85cc933d7ce4aa6c947295"
+    sha256 cellar: :any,                 arm64_monterey: "9b92858870c251ea32675c694f09f5329fd56843ce23c483c5de62e1456f6f1d"
+    sha256 cellar: :any,                 arm64_big_sur:  "e00fd13945ddfa707b35a2fd6b7bd20e5c1fa99fb4574ad7b52607160ead74f7"
+    sha256 cellar: :any,                 monterey:       "7b33c63d98d073301bd875ede941e05ab3b269d2fbcbe527f95fe41390cd44d2"
+    sha256 cellar: :any,                 big_sur:        "580bcfb61bdf0ef6fea0b92af3428ee3484b47a87eb2bf9cee4602a5aec5b3c8"
+    sha256 cellar: :any,                 catalina:       "8dc80e3e1d1a6de47acfd1d107cdea836d2a81ea0bee7fe117423052eacc4558"
+    sha256 cellar: :any,                 mojave:         "3fbf167d4e89a81d052375286fd46f8e8604ef5875f96336c9ed13fd4a8737a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c03a84244f36a2760d5789240aed5a05c121411d2b85cc933d7ce4aa6c947295"
   end
 
   depends_on "cmake" => :build
