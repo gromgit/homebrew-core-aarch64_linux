3ac15637d522733064625f6ab9889bfde25da548
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:46:00 +0000

    qrcp: update 0.8.4 bottle.

--- Formula/qrcp.rb
+++ Formula/qrcp.rb
@@ -6,11 +6,13 @@ class Qrcp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8286ab8384850578862e84cceebb7aa8efc5d2bafd1d372a0e9e1f6930a7a726"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b90c0f156ce21a2ec39f23405fd1213008d96a8e1d62f17cd0d5c83c17b055ea"
-    sha256 cellar: :any_skip_relocation, catalina:      "954c095b0cd034464bd929a3b28855f932d09e0cb59a741939009221f6fdac94"
-    sha256 cellar: :any_skip_relocation, mojave:        "a319771dea04ac593d2928e8e937ce1fd5a7c55c67999e9505b258cbddbe226c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "564e4415f63d446ce140cbb3baccae5902ff4be08b041ceb8bc028c2c90376e8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a17126d0da69e86af27da5a5f519074c57fba00c3587895b802d2b862022123b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8286ab8384850578862e84cceebb7aa8efc5d2bafd1d372a0e9e1f6930a7a726"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e3a7ccb0226a080b8d0ef18e85d5e05c9aac9de20ace9c2b409624d475df7cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b90c0f156ce21a2ec39f23405fd1213008d96a8e1d62f17cd0d5c83c17b055ea"
+    sha256 cellar: :any_skip_relocation, catalina:       "954c095b0cd034464bd929a3b28855f932d09e0cb59a741939009221f6fdac94"
+    sha256 cellar: :any_skip_relocation, mojave:         "a319771dea04ac593d2928e8e937ce1fd5a7c55c67999e9505b258cbddbe226c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "564e4415f63d446ce140cbb3baccae5902ff4be08b041ceb8bc028c2c90376e8"
   end
 
   depends_on "go" => :build
