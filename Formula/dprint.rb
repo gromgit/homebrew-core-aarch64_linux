3365779d24ff473abf008d9a716285170a7210bb
rui <rui@chenrui.dev>
2021-10-27 04:44:09 +0000

    dprint: update 0.18.1 bottle.

--- Formula/dprint.rb
+++ Formula/dprint.rb
@@ -7,11 +7,13 @@ class Dprint < Formula
   head "https://github.com/dprint/dprint.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1a8aea65b37d424a35842688be35a1203ff8fb237c0baefd7967c3f4e322e310"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b6400b334b244ea481a552cdc56b9590c9f7838e80a296d5a3af8a6036d69595"
-    sha256 cellar: :any_skip_relocation, catalina:      "cb1411456754a8d0d39f5bc39151427dc85dfab027f5a06047d7881d05db3758"
-    sha256 cellar: :any_skip_relocation, mojave:        "66763d9090aafe122099e27bd8908b6433ed700743dc301afc506575eb2e22b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2379ebeb45e67f110b429eeb729510c8835af120603776f6b0d2dc579f31d5d8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "76ad8e0b6d5974264d84d929c2bf4aaa61585c84f21ef3b2798831d779ce1947"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a8aea65b37d424a35842688be35a1203ff8fb237c0baefd7967c3f4e322e310"
+    sha256 cellar: :any_skip_relocation, monterey:       "a4768ce919ed0d2f88f37080579189db8948bbf9f4d0561596405d4705e5c848"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b6400b334b244ea481a552cdc56b9590c9f7838e80a296d5a3af8a6036d69595"
+    sha256 cellar: :any_skip_relocation, catalina:       "cb1411456754a8d0d39f5bc39151427dc85dfab027f5a06047d7881d05db3758"
+    sha256 cellar: :any_skip_relocation, mojave:         "66763d9090aafe122099e27bd8908b6433ed700743dc301afc506575eb2e22b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2379ebeb45e67f110b429eeb729510c8835af120603776f6b0d2dc579f31d5d8"
   end
 
   depends_on "rust" => :build
