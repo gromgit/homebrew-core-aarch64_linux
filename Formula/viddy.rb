db252a628750165bc97cc3b8b459bcd428baeff5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 22:51:40 +0000

    viddy: update 0.3.1 bottle.

--- Formula/viddy.rb
+++ Formula/viddy.rb
@@ -7,11 +7,13 @@ class Viddy < Formula
   head "https://github.com/sachaos/viddy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1c4e5817f0684e356c2ab09828f76b0d05c0e034408018fa3ae0479b5f4b1849"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a41c2e1195882c2f7742d534b28f0bb43677b262e0e5a66ff8e556296a039f37"
-    sha256 cellar: :any_skip_relocation, catalina:      "1f7190c3d2f026fb06a7b12e1345be3b564de5265d29fadbe89b3d3995869882"
-    sha256 cellar: :any_skip_relocation, mojave:        "6f0d8ea45e9a44c3da562a7e0b7ca230517cc9fd4ab153fe6521e4e8988355b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48522eaf53c20f2b5d9fe96cb28843962039d27686c065e0c2eac301aff8fce1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d7f8256c6bce33976a1bed52e62b8da0dac15391dffd89631b31ce0300ec870a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c4e5817f0684e356c2ab09828f76b0d05c0e034408018fa3ae0479b5f4b1849"
+    sha256 cellar: :any_skip_relocation, monterey:       "a6806771097480c1b1852274746546dc3f35b6fe01abd914195e76df6f9a7a30"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a41c2e1195882c2f7742d534b28f0bb43677b262e0e5a66ff8e556296a039f37"
+    sha256 cellar: :any_skip_relocation, catalina:       "1f7190c3d2f026fb06a7b12e1345be3b564de5265d29fadbe89b3d3995869882"
+    sha256 cellar: :any_skip_relocation, mojave:         "6f0d8ea45e9a44c3da562a7e0b7ca230517cc9fd4ab153fe6521e4e8988355b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48522eaf53c20f2b5d9fe96cb28843962039d27686c065e0c2eac301aff8fce1"
   end
 
   depends_on "go" => :build
