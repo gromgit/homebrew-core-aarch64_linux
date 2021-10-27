bd01eae70e8db9251f40265ff608f82191d7df90
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 13:22:34 +0000

    torsocks: update 2.3.0 bottle.

--- Formula/torsocks.rb
+++ Formula/torsocks.rb
@@ -8,11 +8,13 @@ class Torsocks < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "3882e64c21d610b6e0fdb6e3bceb41a4ae9494849ced24901f0b50946b34a296"
-    sha256 big_sur:       "2e7d1f1ed60e53086456447d408f577a8ee8d10c73ea8e01b94f7fdbaf6cd141"
-    sha256 catalina:      "0fe4e287d086c2249645781e187dd12e45526ab6ed45b32051aac2ddb840dc92"
-    sha256 mojave:        "8f07df71ce2b8eee8ade8ac9c4de7c5e59acebb880b369dd3bd2d5caf5a93e8e"
-    sha256 x86_64_linux:  "439ed3da7de466d90191d08110cf96e5f435331e2d2e439accfdf8c89357799b"
+    sha256 arm64_monterey: "9d03fd12643e5ee1f023e316b5e8fca4837d4021c47f3ccbc3e12000c00f663f"
+    sha256 arm64_big_sur:  "3882e64c21d610b6e0fdb6e3bceb41a4ae9494849ced24901f0b50946b34a296"
+    sha256 monterey:       "88ddbac1ee6a4197bece3921800234395129d11733d3a1713519b57f449f9335"
+    sha256 big_sur:        "2e7d1f1ed60e53086456447d408f577a8ee8d10c73ea8e01b94f7fdbaf6cd141"
+    sha256 catalina:       "0fe4e287d086c2249645781e187dd12e45526ab6ed45b32051aac2ddb840dc92"
+    sha256 mojave:         "8f07df71ce2b8eee8ade8ac9c4de7c5e59acebb880b369dd3bd2d5caf5a93e8e"
+    sha256 x86_64_linux:   "439ed3da7de466d90191d08110cf96e5f435331e2d2e439accfdf8c89357799b"
   end
 
   depends_on "autoconf" => :build
