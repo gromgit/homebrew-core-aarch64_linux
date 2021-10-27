d4aff5e380718f1807cd8c6a7b80e664ad7b3ea7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 13:11:33 +0000

    ghz: update 0.105.0 bottle.

--- Formula/ghz.rb
+++ Formula/ghz.rb
@@ -11,11 +11,13 @@ class Ghz < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c997364f2d9040d210dd3b1ba1c88b81c687e425e4fa2a576441016ae8ce8c13"
-    sha256 cellar: :any_skip_relocation, big_sur:       "539bbe9d9681221ecfb9e740b5495b587ba5c887297fb3f91c49c27fe53ce9e2"
-    sha256 cellar: :any_skip_relocation, catalina:      "3ed01d5e3438b04d34b421a8cd3ddf7e8b40467c85b5e7a182a869a11832af77"
-    sha256 cellar: :any_skip_relocation, mojave:        "bbe6cd3a20c9591857ee05d2e61fba97ef5dbbf83476d8b0c47687397ba8bb31"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf02572c989e96394a223899c0156e8781474e7faa644a8dd5095d87882e5c41"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fdfbaf436fbab1e56fcc1bfa1db33f40a486146343a9a9f6f4cafa9bfde63bcb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c997364f2d9040d210dd3b1ba1c88b81c687e425e4fa2a576441016ae8ce8c13"
+    sha256 cellar: :any_skip_relocation, monterey:       "69ebeccb409db9a850abd6a649266fab78611d33def0a68bec2baf9ea922901d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "539bbe9d9681221ecfb9e740b5495b587ba5c887297fb3f91c49c27fe53ce9e2"
+    sha256 cellar: :any_skip_relocation, catalina:       "3ed01d5e3438b04d34b421a8cd3ddf7e8b40467c85b5e7a182a869a11832af77"
+    sha256 cellar: :any_skip_relocation, mojave:         "bbe6cd3a20c9591857ee05d2e61fba97ef5dbbf83476d8b0c47687397ba8bb31"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf02572c989e96394a223899c0156e8781474e7faa644a8dd5095d87882e5c41"
   end
 
   depends_on "go" => :build
