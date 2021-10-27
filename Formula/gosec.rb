5683e9c8382b28abdd304c46b64e80f66eade08d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:03:45 +0000

    gosec: update 2.9.1 bottle.

--- Formula/gosec.rb
+++ Formula/gosec.rb
@@ -7,11 +7,13 @@ class Gosec < Formula
   head "https://github.com/securego/gosec.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e96cbfd827be9a9b6d0e1adb9caee8e3160be0d95c25baf6bf91f29ccdf9a1c1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "dfd977f6bfff4c738f6ce0082bb7551d4b5763c64d8110ba3fa2d14fc3b49a11"
-    sha256 cellar: :any_skip_relocation, catalina:      "7913019c62267e97f3c4bfad104b83ab94e718b6ec89dac921d6b7140e520b29"
-    sha256 cellar: :any_skip_relocation, mojave:        "718f7c387a51ef6f32887a6c27a62acb4d4d1831b3c4e8bc7bd7b6edcbeedd66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afc27466cd8224c79a053eab34454826c00ff7b70dd7c1b75459f6e56fdd5b88"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7a1459b48766b6473ec41c909afacfed78a5ed38223caccf0957743ffd4b559c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e96cbfd827be9a9b6d0e1adb9caee8e3160be0d95c25baf6bf91f29ccdf9a1c1"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e9cf8e6c1134d556807fa5ae8e904e6d3b6e65491ca22ceda8c18561d2ebf8f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dfd977f6bfff4c738f6ce0082bb7551d4b5763c64d8110ba3fa2d14fc3b49a11"
+    sha256 cellar: :any_skip_relocation, catalina:       "7913019c62267e97f3c4bfad104b83ab94e718b6ec89dac921d6b7140e520b29"
+    sha256 cellar: :any_skip_relocation, mojave:         "718f7c387a51ef6f32887a6c27a62acb4d4d1831b3c4e8bc7bd7b6edcbeedd66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "afc27466cd8224c79a053eab34454826c00ff7b70dd7c1b75459f6e56fdd5b88"
   end
 
   depends_on "go"
