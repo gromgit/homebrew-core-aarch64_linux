0e356ad13da48ba0007763fdc7d1d6baef2bd90e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:58:35 +0000

    docker-machine-driver-vmware: update 0.1.3 bottle.

--- Formula/docker-machine-driver-vmware.rb
+++ Formula/docker-machine-driver-vmware.rb
@@ -7,11 +7,13 @@ class DockerMachineDriverVmware < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "700ee8e5f337ec89182e4585a42929d52a9a56417dd7ee6a5a09bbbc1f38d24b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5028fce4f23eba9047fa977e0db88861d99bcc0dc2b79b0c2920ea68569b970f"
-    sha256 cellar: :any_skip_relocation, catalina:      "d0ce7d4804d39392cfede2ecc19fbe763d39b4684306d0cb36efb19359668c53"
-    sha256 cellar: :any_skip_relocation, mojave:        "515e8951062268846b9c3ec85aa2b29c6f093cba35eed91a3503344fd5aa288a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cfe899d44e93a330c23fb56f24df7b98decb3aa4829f2f69eee63fac804ef8dd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d2335bba1da650fe3c7092d72a60f1cd64f7596d05b0c00f9a58c7be4a89dfc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "700ee8e5f337ec89182e4585a42929d52a9a56417dd7ee6a5a09bbbc1f38d24b"
+    sha256 cellar: :any_skip_relocation, monterey:       "d332e106b2dfc44145860eeb0833b7de58336f97f7b2ff746de9e552aead7f0b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5028fce4f23eba9047fa977e0db88861d99bcc0dc2b79b0c2920ea68569b970f"
+    sha256 cellar: :any_skip_relocation, catalina:       "d0ce7d4804d39392cfede2ecc19fbe763d39b4684306d0cb36efb19359668c53"
+    sha256 cellar: :any_skip_relocation, mojave:         "515e8951062268846b9c3ec85aa2b29c6f093cba35eed91a3503344fd5aa288a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cfe899d44e93a330c23fb56f24df7b98decb3aa4829f2f69eee63fac804ef8dd"
   end
 
   depends_on "go" => :build
