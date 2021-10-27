bf1d007c61e05838173f5fd89a7b12a80339da45
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:31:21 +0000

    glab: update 1.21.1 bottle.

--- Formula/glab.rb
+++ Formula/glab.rb
@@ -7,11 +7,13 @@ class Glab < Formula
   head "https://github.com/profclems/glab.git", branch: "trunk"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c1a989fc60ce6bf9071350d9ce9d48d59858e490d92cb23fed6c979ea3a4dfc8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "82a903f4d6f4866fa55c93250d63467c8f83003389343cec63e8bc70e0e9dc5e"
-    sha256 cellar: :any_skip_relocation, catalina:      "766192bb22eba3e0219aae40b4898d4e0993788154a5885d865ae1e6074ac722"
-    sha256 cellar: :any_skip_relocation, mojave:        "60e7f1b0149b2c767b3324bed1719b573db47db3120250c9c596a372a639a06a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c2494a7790d8b3a0391842bc0b70b645a459630a264501becb96d893420768e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f30115f346654210a5580268de26a0a957193e1296d59db442d52b7854738404"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c1a989fc60ce6bf9071350d9ce9d48d59858e490d92cb23fed6c979ea3a4dfc8"
+    sha256 cellar: :any_skip_relocation, monterey:       "a33a50438b8d38c0ed7da1e040d820652caf79c60177df1755d5ca4d00d7962a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "82a903f4d6f4866fa55c93250d63467c8f83003389343cec63e8bc70e0e9dc5e"
+    sha256 cellar: :any_skip_relocation, catalina:       "766192bb22eba3e0219aae40b4898d4e0993788154a5885d865ae1e6074ac722"
+    sha256 cellar: :any_skip_relocation, mojave:         "60e7f1b0149b2c767b3324bed1719b573db47db3120250c9c596a372a639a06a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c2494a7790d8b3a0391842bc0b70b645a459630a264501becb96d893420768e"
   end
 
   depends_on "go" => :build
