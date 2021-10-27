e202d0ef191dd0e877c6bd49c1ec5675011d864b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 19:44:43 +0000

    libcroco: update 0.6.13_1 bottle.

--- Formula/libcroco.rb
+++ Formula/libcroco.rb
@@ -6,13 +6,15 @@ class Libcroco < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d6cced1a48822aac65fbb995159f26ed0552217d125969bcae4bd61bdf223407"
-    sha256 cellar: :any,                 big_sur:       "001998f7977aa0e07aa26ab431422e56b2de76dcb7b75dee392f0d0f3674197a"
-    sha256 cellar: :any,                 catalina:      "bc64de8725726ae0188ec23dc9946759565f06e45d3eb10e510d5d42d0888e28"
-    sha256 cellar: :any,                 mojave:        "edf97f493296bfe01b2a8cfe156f1e8052e181bed6ea34cabaf18ed59ef28b17"
-    sha256 cellar: :any,                 high_sierra:   "f6e7d7d608dfcf6e57eaad77eef3cca27c15db0746e102f6dc33cccdd5a8a7bc"
-    sha256 cellar: :any,                 sierra:        "a95e3733bd72b789cc9a3cb9dfc9a92153939b984c4d1d47b8aa806e99e99552"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0b5c4b0de56d524a572a2bd8c93f65ed827c80093776bfd7681fd6351df6e13"
+    sha256 cellar: :any,                 arm64_monterey: "0c7ea7611c087cead41eabbc6e7680a7d47c8c3fd6736d7f97742f1311f61eaf"
+    sha256 cellar: :any,                 arm64_big_sur:  "d6cced1a48822aac65fbb995159f26ed0552217d125969bcae4bd61bdf223407"
+    sha256 cellar: :any,                 monterey:       "c8404e365ec027cb735e158ea042d5c338f6d0f6594e8a7196722801059df193"
+    sha256 cellar: :any,                 big_sur:        "001998f7977aa0e07aa26ab431422e56b2de76dcb7b75dee392f0d0f3674197a"
+    sha256 cellar: :any,                 catalina:       "bc64de8725726ae0188ec23dc9946759565f06e45d3eb10e510d5d42d0888e28"
+    sha256 cellar: :any,                 mojave:         "edf97f493296bfe01b2a8cfe156f1e8052e181bed6ea34cabaf18ed59ef28b17"
+    sha256 cellar: :any,                 high_sierra:    "f6e7d7d608dfcf6e57eaad77eef3cca27c15db0746e102f6dc33cccdd5a8a7bc"
+    sha256 cellar: :any,                 sierra:         "a95e3733bd72b789cc9a3cb9dfc9a92153939b984c4d1d47b8aa806e99e99552"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0b5c4b0de56d524a572a2bd8c93f65ed827c80093776bfd7681fd6351df6e13"
   end
 
   depends_on "intltool" => :build
