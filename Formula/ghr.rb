da1e662092a2178deda2e603a1557f0bad36efd3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 20:21:48 +0000

    ghr: update 0.14.0 bottle.

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -6,11 +6,13 @@ class Ghr < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "04afdc255e1db1bd569f7ca0b8a7b38b1bc5394050c12ab9ab31d1ebde5d1726"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9daff45d2137be191f4906d69385b64afe3f049dc656475585d1c7589a4dcfc1"
-    sha256 cellar: :any_skip_relocation, catalina:      "af8668bdf5cf37170d1476fdfeb3df0a81c8dcc83e63f22914d037560a096a37"
-    sha256 cellar: :any_skip_relocation, mojave:        "91b37053dae7ad067b16ac4e54a64ce43d71c1c7552b42635012c21a770f4f69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63ac6cdfcd910505644cfc262a1462fa10426c1460a0889924887e85fec69e36"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ef5d956b276719ddd21fba7bc706aca4a2c339766f035a483b65f4706caa2a96"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "04afdc255e1db1bd569f7ca0b8a7b38b1bc5394050c12ab9ab31d1ebde5d1726"
+    sha256 cellar: :any_skip_relocation, monterey:       "23f165ccc58dc30062c587043cca2b9044dba8a278f95516711c697451979983"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9daff45d2137be191f4906d69385b64afe3f049dc656475585d1c7589a4dcfc1"
+    sha256 cellar: :any_skip_relocation, catalina:       "af8668bdf5cf37170d1476fdfeb3df0a81c8dcc83e63f22914d037560a096a37"
+    sha256 cellar: :any_skip_relocation, mojave:         "91b37053dae7ad067b16ac4e54a64ce43d71c1c7552b42635012c21a770f4f69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63ac6cdfcd910505644cfc262a1462fa10426c1460a0889924887e85fec69e36"
   end
 
   depends_on "go" => :build
