ce900a68ca829206d8c66b992102b39a39cc2383
rui <rui@chenrui.dev>
2021-10-27 06:05:06 +0000

    jrsonnet: update 0.4.2 bottle.

--- Formula/jrsonnet.rb
+++ Formula/jrsonnet.rb
@@ -7,11 +7,13 @@ class Jrsonnet < Formula
   head "https://github.com/CertainLach/jrsonnet.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "583e666fb6a076a89b1d447ad9220d30409cc62e931c1f4c9dd99cfee9291252"
-    sha256 cellar: :any_skip_relocation, big_sur:       "419f0a50acd6a0cce0abbfe2131d18bdf964fe0d09a095c2c92e10b8b1fb04af"
-    sha256 cellar: :any_skip_relocation, catalina:      "08a4bac487db7433903275f05ba23d11a8a8865590a23a868aa105f49416b650"
-    sha256 cellar: :any_skip_relocation, mojave:        "d927e34108112f33bfa9bb6d004c3540d983ff789d6e5969a4fc9aa8cba92a99"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed7a1bfdfe53b056dbd4cb5433fc26a47b82dde02a913a34870fc6c365a8885a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fcb5b5d99df55c43c70b475ea291ee1d614b76e705be867d688f17bb22ac9842"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "583e666fb6a076a89b1d447ad9220d30409cc62e931c1f4c9dd99cfee9291252"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e79d3068b3c2ca7442c13fc57d8ddd85b0cbb8844c78b8a3c971f5873e3a3a6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "419f0a50acd6a0cce0abbfe2131d18bdf964fe0d09a095c2c92e10b8b1fb04af"
+    sha256 cellar: :any_skip_relocation, catalina:       "08a4bac487db7433903275f05ba23d11a8a8865590a23a868aa105f49416b650"
+    sha256 cellar: :any_skip_relocation, mojave:         "d927e34108112f33bfa9bb6d004c3540d983ff789d6e5969a4fc9aa8cba92a99"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed7a1bfdfe53b056dbd4cb5433fc26a47b82dde02a913a34870fc6c365a8885a"
   end
 
   depends_on "rust" => :build
