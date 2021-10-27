eb11c175385a9b5975585d4fc6560796da4d886a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:37:14 +0000

    copilot: update 1.10.1 bottle.

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -15,11 +15,13 @@ class Copilot < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8993199dfdef70c96b0bb754f96cbfdc5ac4f8512dc9afc606cfd0b9480c1fb9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "672366ea1b8ab2397a2a6ad25da162492b69d5869dcad2556ba7e0bd4032fbed"
-    sha256 cellar: :any_skip_relocation, catalina:      "5219ef7c23e3ba9af9aa39e34797658f08ac777666f0f59d07189e3b781833ae"
-    sha256 cellar: :any_skip_relocation, mojave:        "e65079bcbcd01f771c77c18699e9f44a000c8d0bfc64c21b07d4288b80618a8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1ad704b8d46786d0d9fed1dafb517d6fadec898ba7e13135167fcaf0c976c72"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2caa2455daf60d7aab24f5854971eb063eb6a4742356f0a5fec6c3d0f0ebff31"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8993199dfdef70c96b0bb754f96cbfdc5ac4f8512dc9afc606cfd0b9480c1fb9"
+    sha256 cellar: :any_skip_relocation, monterey:       "b2295f3f52cdfe07318ebefc95a085d65951055f4d6f772cf89f4b0995771518"
+    sha256 cellar: :any_skip_relocation, big_sur:        "672366ea1b8ab2397a2a6ad25da162492b69d5869dcad2556ba7e0bd4032fbed"
+    sha256 cellar: :any_skip_relocation, catalina:       "5219ef7c23e3ba9af9aa39e34797658f08ac777666f0f59d07189e3b781833ae"
+    sha256 cellar: :any_skip_relocation, mojave:         "e65079bcbcd01f771c77c18699e9f44a000c8d0bfc64c21b07d4288b80618a8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1ad704b8d46786d0d9fed1dafb517d6fadec898ba7e13135167fcaf0c976c72"
   end
 
   depends_on "go" => :build
