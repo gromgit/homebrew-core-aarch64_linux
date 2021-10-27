727061e17d2455654e47c7c28ae51202de625e99
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 16:28:35 +0000

    dasel: update 1.21.2 bottle.

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -7,11 +7,13 @@ class Dasel < Formula
   head "https://github.com/TomWright/dasel.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "07023f5c64b771c14ee66347e5c4857c40aef480f629b97e33d475dc1b0dcea0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4c588d2dc6f079c3964e4c93d46d0574110480048888204c9eb0937e4cea3e9e"
-    sha256 cellar: :any_skip_relocation, catalina:      "5e99d16b41132905368fb9c2df25623e60e9640a8a533466d90c52886677ea5c"
-    sha256 cellar: :any_skip_relocation, mojave:        "4f156e178ee7ad520b2d9b8fbdfea3c15a0412a9c9c4dba67c3c36b2ae66a8de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b5838ab6222d6b564d1b9029a0d9b5d7d6856f47488f31794f87e93b7ccba9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "677df4ef54a8e4388c651bd49313e1e040dd4d3a8e6b4769dc936e3deac42f9e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "07023f5c64b771c14ee66347e5c4857c40aef480f629b97e33d475dc1b0dcea0"
+    sha256 cellar: :any_skip_relocation, monterey:       "a5561eed4936476c4a7bfea2f77045dd7f493695eabcb8950c818898cf5ed952"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4c588d2dc6f079c3964e4c93d46d0574110480048888204c9eb0937e4cea3e9e"
+    sha256 cellar: :any_skip_relocation, catalina:       "5e99d16b41132905368fb9c2df25623e60e9640a8a533466d90c52886677ea5c"
+    sha256 cellar: :any_skip_relocation, mojave:         "4f156e178ee7ad520b2d9b8fbdfea3c15a0412a9c9c4dba67c3c36b2ae66a8de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b5838ab6222d6b564d1b9029a0d9b5d7d6856f47488f31794f87e93b7ccba9b"
   end
 
   depends_on "go" => :build
