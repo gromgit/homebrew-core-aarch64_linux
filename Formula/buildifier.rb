a7daa0a36a0eac09f5575a13fe07ad6b0f2d2a3d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 10:00:22 +0000

    buildifier: update 4.2.3 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -7,10 +7,12 @@ class Buildifier < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2ec784a3f54583a43dfc82e6b77cc3ec83c887d37e4b6b2414b1d533306c5ea7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3f12f2c43a50ddd88d020bbc84b25008bfc81d200a74f14ea7c78c40b8f69782"
-    sha256 cellar: :any_skip_relocation, catalina:      "3f12f2c43a50ddd88d020bbc84b25008bfc81d200a74f14ea7c78c40b8f69782"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b13b6469377ebb2f1952004ea11720d83ddc0e2adbe6e822b585fd6f95a6f47"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ec784a3f54583a43dfc82e6b77cc3ec83c887d37e4b6b2414b1d533306c5ea7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2ec784a3f54583a43dfc82e6b77cc3ec83c887d37e4b6b2414b1d533306c5ea7"
+    sha256 cellar: :any_skip_relocation, monterey:       "3f12f2c43a50ddd88d020bbc84b25008bfc81d200a74f14ea7c78c40b8f69782"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3f12f2c43a50ddd88d020bbc84b25008bfc81d200a74f14ea7c78c40b8f69782"
+    sha256 cellar: :any_skip_relocation, catalina:       "3f12f2c43a50ddd88d020bbc84b25008bfc81d200a74f14ea7c78c40b8f69782"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b13b6469377ebb2f1952004ea11720d83ddc0e2adbe6e822b585fd6f95a6f47"
   end
 
   depends_on "go" => :build
