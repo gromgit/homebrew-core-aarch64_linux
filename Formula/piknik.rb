3f760ed380490395620eb72dcee003783dd62bd1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:29:37 +0000

    piknik: update 0.10.1 bottle.

--- Formula/piknik.rb
+++ Formula/piknik.rb
@@ -7,11 +7,13 @@ class Piknik < Formula
   head "https://github.com/jedisct1/piknik.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b503df3a16dfdf25219a598da5ae3c17676ee2367c0db0837403a6f728e4fcb4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "48b98419184b858ff308f4ed96f0ff001f757524c38705337a25adfb960a85ea"
-    sha256 cellar: :any_skip_relocation, catalina:      "d454877b9f650eaa1fcd22ccad12c62a69d2ab21b48a16481d4be17067236233"
-    sha256 cellar: :any_skip_relocation, mojave:        "8afe990d9ff9828b6148928d27c9535fd31b7f8082db341cf962dfbf1e895b96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d9a86776e5339217206d091bc5b37921db0c11c82b8a068108e41ef76c23fde"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3b11a027946eda7b8937861f4458d6d0a8e320fb297fa44ea8a84883e5614b9f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b503df3a16dfdf25219a598da5ae3c17676ee2367c0db0837403a6f728e4fcb4"
+    sha256 cellar: :any_skip_relocation, monterey:       "28f6e0672fa5745285ec63560821f35c6bf604fa42ad80828bd2ca70eb5d94df"
+    sha256 cellar: :any_skip_relocation, big_sur:        "48b98419184b858ff308f4ed96f0ff001f757524c38705337a25adfb960a85ea"
+    sha256 cellar: :any_skip_relocation, catalina:       "d454877b9f650eaa1fcd22ccad12c62a69d2ab21b48a16481d4be17067236233"
+    sha256 cellar: :any_skip_relocation, mojave:         "8afe990d9ff9828b6148928d27c9535fd31b7f8082db341cf962dfbf1e895b96"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d9a86776e5339217206d091bc5b37921db0c11c82b8a068108e41ef76c23fde"
   end
 
   depends_on "go" => :build
