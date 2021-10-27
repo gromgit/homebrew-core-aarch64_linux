db3b0a26b93f10c9e70d88dea3fd0bd3ff243482
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 17:27:01 +0000

    clusterctl: update 1.0.0 bottle.

--- Formula/clusterctl.rb
+++ Formula/clusterctl.rb
@@ -19,11 +19,13 @@ class Clusterctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cf44da229fbd2190227c32fc830a13fbfd4c2ab218b3e404f058224a13332aea"
-    sha256 cellar: :any_skip_relocation, big_sur:       "34607708549a05bff8c4144f2a1fd91cc0f6461322b2c299942e6b25a09ad456"
-    sha256 cellar: :any_skip_relocation, catalina:      "25e082158e2f7ec8f5c248e4e409ff9c78e79f1753ce0ee9f30b698e638b5aa4"
-    sha256 cellar: :any_skip_relocation, mojave:        "4d6a2d3e93b26cc928599c3c906a1c9418672e42d2199b12c8cb587662b603f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "096e35d1cc0e3180a4837badada836925041a5d7d2b731d86a04ae7c96618fb4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8001e8c4fcf8f66a38956301607ddfc546b08055d72f191c5ce9af4e39da2a6c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cf44da229fbd2190227c32fc830a13fbfd4c2ab218b3e404f058224a13332aea"
+    sha256 cellar: :any_skip_relocation, monterey:       "035d903b58f4b7b1df0a01a17ae13772d363240d119b84bfbc5fe1fc715a1b3e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "34607708549a05bff8c4144f2a1fd91cc0f6461322b2c299942e6b25a09ad456"
+    sha256 cellar: :any_skip_relocation, catalina:       "25e082158e2f7ec8f5c248e4e409ff9c78e79f1753ce0ee9f30b698e638b5aa4"
+    sha256 cellar: :any_skip_relocation, mojave:         "4d6a2d3e93b26cc928599c3c906a1c9418672e42d2199b12c8cb587662b603f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "096e35d1cc0e3180a4837badada836925041a5d7d2b731d86a04ae7c96618fb4"
   end
 
   depends_on "go" => :build
