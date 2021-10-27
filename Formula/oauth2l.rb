f51c903e94be978ff90e52ce6b2506f79f6173b4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 21:45:13 +0000

    oauth2l: update 1.2.2 bottle.

--- Formula/oauth2l.rb
+++ Formula/oauth2l.rb
@@ -7,11 +7,13 @@ class Oauth2l < Formula
   head "https://github.com/google/oauth2l.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "97afa02857ded76e58750c9258dcdf6696cd341f4ef5da3fcef1b506d4af5d60"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a3997e4c2392de8d3ee2e7b4e6e6bdcdf6236e9d75fe1007afdf9b4aa4228913"
-    sha256 cellar: :any_skip_relocation, catalina:      "a1364fb71118addb9dfd3ad4c30a4ffcc337eb5115c7ae40a7fe6825492116c3"
-    sha256 cellar: :any_skip_relocation, mojave:        "5dfc5cfee1683e24ff6740c823920f629b13d834de40dc7832819e37428f9b23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "885ca033df96d0bc0e24402fd9cba8cba1ffee1b52498c41ff91b1a2c2be2b3d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "881f2ba9c36fcabcaacd881e99265b41622f8aaa437ea769eaeba1a6d187bf99"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "97afa02857ded76e58750c9258dcdf6696cd341f4ef5da3fcef1b506d4af5d60"
+    sha256 cellar: :any_skip_relocation, monterey:       "ff2f5238d702f84f01aa24b845227d5ef7fb3edc4a60099ce9a7cde92b46ca3f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a3997e4c2392de8d3ee2e7b4e6e6bdcdf6236e9d75fe1007afdf9b4aa4228913"
+    sha256 cellar: :any_skip_relocation, catalina:       "a1364fb71118addb9dfd3ad4c30a4ffcc337eb5115c7ae40a7fe6825492116c3"
+    sha256 cellar: :any_skip_relocation, mojave:         "5dfc5cfee1683e24ff6740c823920f629b13d834de40dc7832819e37428f9b23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "885ca033df96d0bc0e24402fd9cba8cba1ffee1b52498c41ff91b1a2c2be2b3d"
   end
 
   depends_on "go" => :build
