d71769543d660591bae2346909974c17a9890dba
rui <rui@chenrui.dev>
2021-10-27 03:00:04 +0000

    cargo-watch: update 8.1.1 bottle.

--- Formula/cargo-watch.rb
+++ Formula/cargo-watch.rb
@@ -7,11 +7,13 @@ class CargoWatch < Formula
   head "https://github.com/passcod/cargo-watch.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7f6aed6adaad6325af56d4a22d88f79e6e440e92c7494727d61a8af9ecfb20a2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "56af6500879866846fcbfb55c046c88558348d40c07d2a84c67dead1e01318e1"
-    sha256 cellar: :any_skip_relocation, catalina:      "8a5d75db0356f7953d61108bcfad80a95a068dc373c92437a24e0a5a68d1d560"
-    sha256 cellar: :any_skip_relocation, mojave:        "1d92202101624d505bbabb473937bccc69a41c112e0557254ddde85e84a78460"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e1a20bf39e3d0c4d93f4092ec988f0dc39ba75584a003f83e47e1490e5512661"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cc3932f1c0c7eab51c80331d746395285f0c952ec57f4330405173924dd1558f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f6aed6adaad6325af56d4a22d88f79e6e440e92c7494727d61a8af9ecfb20a2"
+    sha256 cellar: :any_skip_relocation, monterey:       "304c19de6669c54f510ba44516e486c260835d76581cef22aff8b3edc4250684"
+    sha256 cellar: :any_skip_relocation, big_sur:        "56af6500879866846fcbfb55c046c88558348d40c07d2a84c67dead1e01318e1"
+    sha256 cellar: :any_skip_relocation, catalina:       "8a5d75db0356f7953d61108bcfad80a95a068dc373c92437a24e0a5a68d1d560"
+    sha256 cellar: :any_skip_relocation, mojave:         "1d92202101624d505bbabb473937bccc69a41c112e0557254ddde85e84a78460"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1a20bf39e3d0c4d93f4092ec988f0dc39ba75584a003f83e47e1490e5512661"
   end
 
   depends_on "rust" => [:build, :test]
