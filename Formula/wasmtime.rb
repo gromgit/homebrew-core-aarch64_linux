5c2d567811f4f4d31f7b79a524c821d6f34ed463
rui <rui@chenrui.dev>
2021-10-27 09:35:55 +0000

    wasmtime: update 0.30.0 bottle.

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -13,11 +13,13 @@ class Wasmtime < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1e54231a80dcdd8a3ef12d4e23472cd18e609c6c758bc74868ca027cc9a4de50"
-    sha256 cellar: :any_skip_relocation, big_sur:       "696828c1a710e24d80fbb3d611299fe4b72c51a7c897879967e3d8d8acf9af65"
-    sha256 cellar: :any_skip_relocation, catalina:      "462ff64295b8d8287fef10079b38ba14e1884c2e75ef9151d4f7a09077a8b109"
-    sha256 cellar: :any_skip_relocation, mojave:        "af8587541c55c3b06e28dcb727f06364139b39b1b1580ac37f1bc4ab686093c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8cd563ee6bac25f91d109bc7813cdda266ba8ef958a910ab281d7057b094895b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "88f01df85384941d389feea6b4df3a8246bb3b6b3d309ff2d00b7f712d1fd778"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e54231a80dcdd8a3ef12d4e23472cd18e609c6c758bc74868ca027cc9a4de50"
+    sha256 cellar: :any_skip_relocation, monterey:       "b6ddfb4d074b5042e2e90b2adaf25cb86b43929b245021ff983ea9a7563cb0c8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "696828c1a710e24d80fbb3d611299fe4b72c51a7c897879967e3d8d8acf9af65"
+    sha256 cellar: :any_skip_relocation, catalina:       "462ff64295b8d8287fef10079b38ba14e1884c2e75ef9151d4f7a09077a8b109"
+    sha256 cellar: :any_skip_relocation, mojave:         "af8587541c55c3b06e28dcb727f06364139b39b1b1580ac37f1bc4ab686093c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8cd563ee6bac25f91d109bc7813cdda266ba8ef958a910ab281d7057b094895b"
   end
 
   depends_on "rust" => :build
