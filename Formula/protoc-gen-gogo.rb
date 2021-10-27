b8ea8d4e72fddd6a3a3406144b924ea65e10107b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:40:51 +0000

    protoc-gen-gogo: update 1.3.2_2 bottle.

--- Formula/protoc-gen-gogo.rb
+++ Formula/protoc-gen-gogo.rb
@@ -8,11 +8,13 @@ class ProtocGenGogo < Formula
   head "https://github.com/gogo/protobuf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9ba5d8b674ef61368f7dfdc0f072190ec0927fc3c42fa846535ccd2dfd3030f9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fd8a46c27fe74626f154ec7ea64183b1dfcfaea1cba6f35ebd91da51383c4122"
-    sha256 cellar: :any_skip_relocation, catalina:      "fd8a46c27fe74626f154ec7ea64183b1dfcfaea1cba6f35ebd91da51383c4122"
-    sha256 cellar: :any_skip_relocation, mojave:        "fd8a46c27fe74626f154ec7ea64183b1dfcfaea1cba6f35ebd91da51383c4122"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1cd0e2d85d2acb9aeaea2405f7ca8e9a734970758027ef098d0ffd624b11c895"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f806d12b79c1d49407fcad5fa50a8cf47303ee5f3e5f0403c2d1993387bf6a62"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ba5d8b674ef61368f7dfdc0f072190ec0927fc3c42fa846535ccd2dfd3030f9"
+    sha256 cellar: :any_skip_relocation, monterey:       "a0e11a313199c7912fbeca42f17d9c24bff4031a6d0a994ced6e6d7b4e25dc6a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd8a46c27fe74626f154ec7ea64183b1dfcfaea1cba6f35ebd91da51383c4122"
+    sha256 cellar: :any_skip_relocation, catalina:       "fd8a46c27fe74626f154ec7ea64183b1dfcfaea1cba6f35ebd91da51383c4122"
+    sha256 cellar: :any_skip_relocation, mojave:         "fd8a46c27fe74626f154ec7ea64183b1dfcfaea1cba6f35ebd91da51383c4122"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1cd0e2d85d2acb9aeaea2405f7ca8e9a734970758027ef098d0ffd624b11c895"
   end
 
   depends_on "go" => :build
