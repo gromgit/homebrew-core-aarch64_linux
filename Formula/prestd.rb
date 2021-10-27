186cfb19985debd5d8690b915a955eb0069fede0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:30:09 +0000

    prestd: update 1.0.10 bottle.

--- Formula/prestd.rb
+++ Formula/prestd.rb
@@ -12,11 +12,13 @@ class Prestd < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6f7e525ce2f762739c8e3f33af949046790099d2c81eb8dee6c26483b8b96c04"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9ddeb2d90e3f0376637547d74dccbb46a23cba6c6b579455a0fd477854443229"
-    sha256 cellar: :any_skip_relocation, catalina:      "55f91de387918a2d1166c50435c0a9c56c79b23f32a844ad105228f8fea2e57e"
-    sha256 cellar: :any_skip_relocation, mojave:        "0ac506e43d96a7cd6b841337d9ddf80167a96da34e177a2ffed460efbf15839e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "555d1beda6082e133ae4c3e8f832cac87e0799f6d5baaaee983214dab86a2236"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5b89f52d239d98f4da7751a1861524b482a803fa67a82f736577b86eebe88bf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f7e525ce2f762739c8e3f33af949046790099d2c81eb8dee6c26483b8b96c04"
+    sha256 cellar: :any_skip_relocation, monterey:       "42d34b993a2467cf32b87d4c7efc72526c97fdc54020f8187d4cbfa938320dbb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9ddeb2d90e3f0376637547d74dccbb46a23cba6c6b579455a0fd477854443229"
+    sha256 cellar: :any_skip_relocation, catalina:       "55f91de387918a2d1166c50435c0a9c56c79b23f32a844ad105228f8fea2e57e"
+    sha256 cellar: :any_skip_relocation, mojave:         "0ac506e43d96a7cd6b841337d9ddf80167a96da34e177a2ffed460efbf15839e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "555d1beda6082e133ae4c3e8f832cac87e0799f6d5baaaee983214dab86a2236"
   end
 
   depends_on "go" => :build
