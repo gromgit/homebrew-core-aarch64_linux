e425e7b6a95fd74493d957daccd32f0b2d194120
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 18:24:27 +0000

    frugal: update 3.14.9 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -6,11 +6,13 @@ class Frugal < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ddf0882cccedf077a47c9d6f19c1149d2deb2f41b9291877b3c74a9021afde64"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2010feb5c9e7ee315cdb22c94e1ac95336a56a868f553c346f400d297b580db5"
-    sha256 cellar: :any_skip_relocation, catalina:      "2010feb5c9e7ee315cdb22c94e1ac95336a56a868f553c346f400d297b580db5"
-    sha256 cellar: :any_skip_relocation, mojave:        "2010feb5c9e7ee315cdb22c94e1ac95336a56a868f553c346f400d297b580db5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19674e735af1211a6f7096df85eb6394c950888496df9d3d035304e87fcf52eb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "289768163cd1ed0bc55a755983db8fc94d15a3fc568154b95f8df7f757670170"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ddf0882cccedf077a47c9d6f19c1149d2deb2f41b9291877b3c74a9021afde64"
+    sha256 cellar: :any_skip_relocation, monterey:       "bd003a58e48331c91c9f8798965c3ea2e906d17e9fda70af882a78cda4093c1e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2010feb5c9e7ee315cdb22c94e1ac95336a56a868f553c346f400d297b580db5"
+    sha256 cellar: :any_skip_relocation, catalina:       "2010feb5c9e7ee315cdb22c94e1ac95336a56a868f553c346f400d297b580db5"
+    sha256 cellar: :any_skip_relocation, mojave:         "2010feb5c9e7ee315cdb22c94e1ac95336a56a868f553c346f400d297b580db5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19674e735af1211a6f7096df85eb6394c950888496df9d3d035304e87fcf52eb"
   end
 
   depends_on "go" => :build
