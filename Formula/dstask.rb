8771946a041458970d8437fd829de25af8d2eb67
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 18:02:25 +0000

    dstask: update 0.25 bottle.

--- Formula/dstask.rb
+++ Formula/dstask.rb
@@ -7,11 +7,13 @@ class Dstask < Formula
   head "https://github.com/naggie/dstask.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c4b84546f5c35f4bf4bed658a73ad1dd562cd4fa2340e9bf48cb6af55f2d8b55"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b1976a39a056f8bef4d354afefcc427c4612527fb578b56734fbfa8432cc6e25"
-    sha256 cellar: :any_skip_relocation, catalina:      "6db5f204f382f928f3890e5983f4c8531172080f3a90068a0b521435c7860e0b"
-    sha256 cellar: :any_skip_relocation, mojave:        "fbcb9d73ddf2619094cd64478112c73eb384a8d839a72aae3f16833087d3973c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e145f6d8d167f7024c4061a4ba5e9f523fd559f603310e03de1693ec99b1c56"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e297da28f9916641f3b3fafd8c3581db2939dc1ce96e6d784d8a99f00081bac7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c4b84546f5c35f4bf4bed658a73ad1dd562cd4fa2340e9bf48cb6af55f2d8b55"
+    sha256 cellar: :any_skip_relocation, monterey:       "af6523a42ebc28c44162f48f2f191c12ce2b73ff534c4ad55f5c88859e4a13a3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b1976a39a056f8bef4d354afefcc427c4612527fb578b56734fbfa8432cc6e25"
+    sha256 cellar: :any_skip_relocation, catalina:       "6db5f204f382f928f3890e5983f4c8531172080f3a90068a0b521435c7860e0b"
+    sha256 cellar: :any_skip_relocation, mojave:         "fbcb9d73ddf2619094cd64478112c73eb384a8d839a72aae3f16833087d3973c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e145f6d8d167f7024c4061a4ba5e9f523fd559f603310e03de1693ec99b1c56"
   end
 
   depends_on "go" => :build
