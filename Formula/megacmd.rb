a1543ead8f070b435138c05924e8b5df3bcabea6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 23:24:29 +0000

    megacmd: update 0.016 bottle.

--- Formula/megacmd.rb
+++ Formula/megacmd.rb
@@ -8,11 +8,13 @@ class Megacmd < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b0cc423d7d44f74416233d1890e003d8d1a92b32c4f281885e89dbda52031218"
-    sha256 cellar: :any_skip_relocation, big_sur:       "005012522f9d83387047d28fc1f4870b27090496d333a5ace382fd3b2b380850"
-    sha256 cellar: :any_skip_relocation, catalina:      "5e3e9a0dcacef7fcac245b621b8eee36cc9dc974b46ba1006769f1dbf781b01c"
-    sha256 cellar: :any_skip_relocation, mojave:        "a24988b1613d43a55748a6516f3d0ac15b13a533b92c201200d0c0998c4dbeb1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9158470703a89b5a963775d8a0470dd3a9e934e8815c1ec171f9574d77fb3c32"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f392f9d6d1ba3acece2b86882a1ae6f5f396e37b814460764655d89704e9d5b3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b0cc423d7d44f74416233d1890e003d8d1a92b32c4f281885e89dbda52031218"
+    sha256 cellar: :any_skip_relocation, monterey:       "82090686813f52e06f2a9f93c79ea0fc856963575da7d912513a1679ae0b425a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "005012522f9d83387047d28fc1f4870b27090496d333a5ace382fd3b2b380850"
+    sha256 cellar: :any_skip_relocation, catalina:       "5e3e9a0dcacef7fcac245b621b8eee36cc9dc974b46ba1006769f1dbf781b01c"
+    sha256 cellar: :any_skip_relocation, mojave:         "a24988b1613d43a55748a6516f3d0ac15b13a533b92c201200d0c0998c4dbeb1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9158470703a89b5a963775d8a0470dd3a9e934e8815c1ec171f9574d77fb3c32"
   end
 
   depends_on "go" => :build
