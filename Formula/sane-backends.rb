3bb14cfdef0f95b77498379b25602d4b946f645e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-27 13:28:26 +0000

    sane-backends: update 1.0.32 bottle.

--- Formula/sane-backends.rb
+++ Formula/sane-backends.rb
@@ -20,11 +20,13 @@ class SaneBackends < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "1ebc21e81f57ecb00c64799e448dff7606c8bea5a66123169af37bed634083ac"
-    sha256 big_sur:       "bcdbaa5208359537721be14fbf2420ff07c573d62a480cb0fbafd5cb0be4334b"
-    sha256 catalina:      "077644bb297e1e9e232d67ade77ef46bb8df7745a48444129e0b996d8fa2bec9"
-    sha256 mojave:        "d7a6d9cb0ef356bef081454e6ee551d0975be4444ce77d048b625b9f44460ed2"
-    sha256 x86_64_linux:  "51eaa1c638201959e754812033d8078f69af06a0b2746a13aed98aa2670e98fc"
+    sha256 arm64_monterey: "63e8564065939a6a8dd41b20293d20e35b94ff75ebaec99181d286269624a3ec"
+    sha256 arm64_big_sur:  "1ebc21e81f57ecb00c64799e448dff7606c8bea5a66123169af37bed634083ac"
+    sha256 monterey:       "ca18d70d2c990dca6f77b72c8d6fe5e04f66bb7a314a35fe4f9a0452e57e9eeb"
+    sha256 big_sur:        "bcdbaa5208359537721be14fbf2420ff07c573d62a480cb0fbafd5cb0be4334b"
+    sha256 catalina:       "077644bb297e1e9e232d67ade77ef46bb8df7745a48444129e0b996d8fa2bec9"
+    sha256 mojave:         "d7a6d9cb0ef356bef081454e6ee551d0975be4444ce77d048b625b9f44460ed2"
+    sha256 x86_64_linux:   "51eaa1c638201959e754812033d8078f69af06a0b2746a13aed98aa2670e98fc"
   end
 
   head do
