ac8c514a6b821c5e00a1ecf74d92101fc1567f7f
rui <rui@chenrui.dev>
2021-10-27 05:38:18 +0000

    git-interactive-rebase-tool: update 2.1.0 bottle.

--- Formula/git-interactive-rebase-tool.rb
+++ Formula/git-interactive-rebase-tool.rb
@@ -11,11 +11,13 @@ class GitInteractiveRebaseTool < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fd082607eef75ac3382d280c358cf19db42ad11d464ded8a2845734de378937c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4480f59a021edb031a3ef02b5cccae62719dad12f6ae7fc2413b75088be466ac"
-    sha256 cellar: :any_skip_relocation, catalina:      "9223a3f962e2af3897cefb9e221a529be7701e964005d55436f7de9d97d1b573"
-    sha256 cellar: :any_skip_relocation, mojave:        "bfab245c62dd4a37da0c275d8b4642ee86b5912153465da1e312a510fce60b80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c37316d7e0b5bc7d99d9979a001db0fbb348f6c6229ce5abfa7b50be39406c1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "84d875534c0464bf6f3dfef84835d10f8870fb23b3d9fefa51c75efada02037e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd082607eef75ac3382d280c358cf19db42ad11d464ded8a2845734de378937c"
+    sha256 cellar: :any_skip_relocation, monterey:       "cc4be634c05adea3dba46604cbfad74eaa3668b102d08b55e27d181f9e25fd0f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4480f59a021edb031a3ef02b5cccae62719dad12f6ae7fc2413b75088be466ac"
+    sha256 cellar: :any_skip_relocation, catalina:       "9223a3f962e2af3897cefb9e221a529be7701e964005d55436f7de9d97d1b573"
+    sha256 cellar: :any_skip_relocation, mojave:         "bfab245c62dd4a37da0c275d8b4642ee86b5912153465da1e312a510fce60b80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c37316d7e0b5bc7d99d9979a001db0fbb348f6c6229ce5abfa7b50be39406c1"
   end
 
   depends_on "rust" => :build
