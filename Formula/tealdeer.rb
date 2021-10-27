0ecfed07b9bb4af4b131b68590427faf3f105a99
rui <rui@chenrui.dev>
2021-10-27 08:52:24 +0000

    tealdeer: update 1.4.1 bottle.

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -6,12 +6,14 @@ class Tealdeer < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4007bbd16184eea9bb652cfcec1b7456551400f85de61cf080a98046cf79c5dd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5f323f75b7d481e86187ac45f9931ad114e1a9d4e7035f04a368d690de77e9e0"
-    sha256 cellar: :any_skip_relocation, catalina:      "db4a2fc7dceef4f4f914aaf7b655741e960664b46c61ab2c163f015a74949533"
-    sha256 cellar: :any_skip_relocation, mojave:        "9ad3e3ea878b05fd2764cc8e534888b8bf810f88cc4d986de158ed6b33633b42"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "80a29641c9b29a3cda69adf5afd2c36b27f84fb8f89a555dbf2a676dddf03b70"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a01f8be8f2e5ba17e70706875d0322c8ddfed2c3b3e7847c642d49f71629f94"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a88d1a7e6cbbd536fc3fa7f4746ddc3191664943207941f2163eaabc34525a44"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4007bbd16184eea9bb652cfcec1b7456551400f85de61cf080a98046cf79c5dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "4e85ccac7ae235ec3be341399f1a3eb6ff7e37b61652d87d85b111b6f0465f17"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5f323f75b7d481e86187ac45f9931ad114e1a9d4e7035f04a368d690de77e9e0"
+    sha256 cellar: :any_skip_relocation, catalina:       "db4a2fc7dceef4f4f914aaf7b655741e960664b46c61ab2c163f015a74949533"
+    sha256 cellar: :any_skip_relocation, mojave:         "9ad3e3ea878b05fd2764cc8e534888b8bf810f88cc4d986de158ed6b33633b42"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "80a29641c9b29a3cda69adf5afd2c36b27f84fb8f89a555dbf2a676dddf03b70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a01f8be8f2e5ba17e70706875d0322c8ddfed2c3b3e7847c642d49f71629f94"
   end
 
   depends_on "rust" => :build
