b4581a94f359df72e1c26df11f237f0ada6a0107
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-27 16:32:14 +0000

    obfs4proxy: update 0.0.11 bottle.

--- Formula/obfs4proxy.rb
+++ Formula/obfs4proxy.rb
@@ -11,11 +11,13 @@ class Obfs4proxy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4e7ffbf5b299d80b469fc0f4edbf44dcac2c698a7b5ac84f4d78632cc8888ce7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5918cdc41743f14ee4110b132361abe21b20445fb7afd4a60859b4b1e594462a"
-    sha256 cellar: :any_skip_relocation, catalina:      "4d22c53ebd7beaacc0c96b2af4fe3179f5b8a63f2f9bd624dc6d0d1d2867d40f"
-    sha256 cellar: :any_skip_relocation, mojave:        "75715a8bfc51f3ab2a3dd4e56077c27626ffd802a83f18da300d394091fb79cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4406d6bb3e815416d79ccdec2df0217aadc212387dce0adf64ad2cb9f6e56d09"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d7b477385d289553a2b0e4f24d388f4b5251050945b734c1bbf984352b6ab47"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e7ffbf5b299d80b469fc0f4edbf44dcac2c698a7b5ac84f4d78632cc8888ce7"
+    sha256 cellar: :any_skip_relocation, monterey:       "a460c5414796f401619ddca98f9bb5c807dff41071099f0d3ae5fe0d85536538"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5918cdc41743f14ee4110b132361abe21b20445fb7afd4a60859b4b1e594462a"
+    sha256 cellar: :any_skip_relocation, catalina:       "4d22c53ebd7beaacc0c96b2af4fe3179f5b8a63f2f9bd624dc6d0d1d2867d40f"
+    sha256 cellar: :any_skip_relocation, mojave:         "75715a8bfc51f3ab2a3dd4e56077c27626ffd802a83f18da300d394091fb79cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4406d6bb3e815416d79ccdec2df0217aadc212387dce0adf64ad2cb9f6e56d09"
   end
 
   depends_on "go" => :build
