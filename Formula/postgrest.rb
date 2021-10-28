846740574276737a6bb4ae1c8887b23868ce58f3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 06:15:38 +0000

    postgrest: update 8.0.0_1 bottle.

--- Formula/postgrest.rb
+++ Formula/postgrest.rb
@@ -10,6 +10,7 @@ class Postgrest < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "bd989403d46a8f5fa86ad41e813fb48e06ec60d6b4052f6bc5a3168c22671ba6"
     sha256 cellar: :any,                 arm64_big_sur:  "a505a99c6f164a72936e18584d9a172204be89694934adc2a4d168c33ae8f91a"
+    sha256 cellar: :any,                 monterey:       "c6d67ce89f433586f9828ebd36be1745161366c42750bfb896ddf939e3083d90"
     sha256 cellar: :any,                 big_sur:        "9cbf169535605656b931ae0065af5a57c2ff511a8b50529e9ded33c560306c29"
     sha256 cellar: :any,                 catalina:       "af90758e13856d719d8fc68f770cd59356081321a90e68c52dd8d897fdcc8f4b"
     sha256 cellar: :any,                 mojave:         "e2a7fa323490cbd817ec9a74729de4a7692d64f2c59c4bb73e188c9235340400"
