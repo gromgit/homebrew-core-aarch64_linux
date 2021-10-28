31df745082d20fe8d33b16b2603e3011019494a0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 00:22:46 +0000

    sonobuoy: update 0.54.0 bottle.

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -6,11 +6,13 @@ class Sonobuoy < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bdf70c480abc642bf1a788771f948c78522b3cb1db733c87216c3cedef5cec97"
-    sha256 cellar: :any_skip_relocation, big_sur:       "02e5f5504e71c6e069bd4a67032609ae2a3609ec187960ccbcdd921588b58d62"
-    sha256 cellar: :any_skip_relocation, catalina:      "48666d55e9f07e9c2a455651619803a97ba1d73f4e950209f9f1e41c561f1b67"
-    sha256 cellar: :any_skip_relocation, mojave:        "292668d4da3ddb77e594156fc790c9941c080ea291faf7b97b195f780d978a1e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0878be725dcc42c8c9cadad08d6659dd555990b4712751d6cd304c0340b6503e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "758e5d67fa71f074ee63cc9304823781c54dfabfffe86576966b87182e603d70"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bdf70c480abc642bf1a788771f948c78522b3cb1db733c87216c3cedef5cec97"
+    sha256 cellar: :any_skip_relocation, monterey:       "da8bf9fbcd32c9d9ee5b20e6f6cc02893b16f7be84d510b6a62dbcede4320ede"
+    sha256 cellar: :any_skip_relocation, big_sur:        "02e5f5504e71c6e069bd4a67032609ae2a3609ec187960ccbcdd921588b58d62"
+    sha256 cellar: :any_skip_relocation, catalina:       "48666d55e9f07e9c2a455651619803a97ba1d73f4e950209f9f1e41c561f1b67"
+    sha256 cellar: :any_skip_relocation, mojave:         "292668d4da3ddb77e594156fc790c9941c080ea291faf7b97b195f780d978a1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0878be725dcc42c8c9cadad08d6659dd555990b4712751d6cd304c0340b6503e"
   end
 
   depends_on "go" => :build
