395bb5c3d8c79b4dda8707818f51f6be28218243
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 02:44:06 +0000

    ponysay: update 3.0.3_7 bottle.

--- Formula/ponysay.rb
+++ Formula/ponysay.rb
@@ -17,11 +17,13 @@ class Ponysay < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "15d31809c411d0af2a1aa8d1d2590e061ab4cb9b9de0397e97be2ebbb01e1fc8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bde757a6e76ce24cf5f8b163e3c6046032ea486c25b707140c8eb0e117fe2148"
-    sha256 cellar: :any_skip_relocation, catalina:      "599a4e15aa4b45a57a2a49a1dd961ab92ea763fd176552ffa278a7f0f4908a92"
-    sha256 cellar: :any_skip_relocation, mojave:        "6c163cdc7026773234e757c83135d848ca2447e2e160d5d2fa12761e64bdda5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d7c805cc2d40f173cd617bef3d846c6858564e398b595271f23436608ad6e36"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf1695638d599e7bca5b59a6a1d15e4a9555fef2a64a8b57a680af46a578fbdd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "15d31809c411d0af2a1aa8d1d2590e061ab4cb9b9de0397e97be2ebbb01e1fc8"
+    sha256 cellar: :any_skip_relocation, monterey:       "919db1ea7b08a3bf90d2950e428bb845b0a490803b336c15aa55ee7470405897"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bde757a6e76ce24cf5f8b163e3c6046032ea486c25b707140c8eb0e117fe2148"
+    sha256 cellar: :any_skip_relocation, catalina:       "599a4e15aa4b45a57a2a49a1dd961ab92ea763fd176552ffa278a7f0f4908a92"
+    sha256 cellar: :any_skip_relocation, mojave:         "6c163cdc7026773234e757c83135d848ca2447e2e160d5d2fa12761e64bdda5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d7c805cc2d40f173cd617bef3d846c6858564e398b595271f23436608ad6e36"
   end
 
   depends_on "gzip" => :build
