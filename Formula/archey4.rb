b5e837f2136036f6ebd5614a38b12aa6316b05ea
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 00:55:29 +0000

    archey4: update 4.13.2 bottle.

--- Formula/archey4.rb
+++ Formula/archey4.rb
@@ -8,11 +8,13 @@ class Archey4 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1e824be83193a74a0d3b5ebed70583a941ee470514834a70c4ae0287d5dd6893"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e848273ecff0d37d6902db78fd668121e3fc6d474772b7b7ee0da3592171dc10"
-    sha256 cellar: :any_skip_relocation, catalina:      "317f1ef273ecca9613f395ff13e64351d63f7f4626fd0b638dd2ed66bec831ba"
-    sha256 cellar: :any_skip_relocation, mojave:        "8f277515b7d8d121480d26afa4cf671174e094f17aebd9babc06df1642577158"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "016fd46d432126417712bc146acb31a2087dfb96919ae536426afd0c9d361c05"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea4103fc5fefb64374aa0b0a298b4c6cfd4a3be3d17cf54222d0c77a10c15a56"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e824be83193a74a0d3b5ebed70583a941ee470514834a70c4ae0287d5dd6893"
+    sha256 cellar: :any_skip_relocation, monterey:       "8caed1e145509feaf3da6fbc74452374dd88109a665a61ef17d7e14007fd030c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e848273ecff0d37d6902db78fd668121e3fc6d474772b7b7ee0da3592171dc10"
+    sha256 cellar: :any_skip_relocation, catalina:       "317f1ef273ecca9613f395ff13e64351d63f7f4626fd0b638dd2ed66bec831ba"
+    sha256 cellar: :any_skip_relocation, mojave:         "8f277515b7d8d121480d26afa4cf671174e094f17aebd9babc06df1642577158"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "016fd46d432126417712bc146acb31a2087dfb96919ae536426afd0c9d361c05"
   end
 
   depends_on "python@3.10"
