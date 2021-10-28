915f5ef4a86f34ef472f2f7cd9daa7fb019ba6a9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 09:27:14 +0000

    erlang@23: update 23.3.4.7 bottle.

--- Formula/erlang@23.rb
+++ Formula/erlang@23.rb
@@ -12,11 +12,13 @@ class ErlangAT23 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "760de405f9142606ff2fa1f5908dc7dd320563692ff4c40791fc77359960aee6"
-    sha256 cellar: :any,                 big_sur:       "28e575220e5c763828581176919d8dede228f91f3d83cacacdc0dda96e02f008"
-    sha256 cellar: :any,                 catalina:      "d3d28042641439ff3ea27bb47c777d96563d457791581ccb1e5ed148acbca14f"
-    sha256 cellar: :any,                 mojave:        "007118b6f7f387239c1743589f6040d2c035171fbe27db0c477e2a0cbc25bb80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "180a9961c67edef2f6955d646c4791134528c4dc40df79b513e8b2300b371f4d"
+    sha256 cellar: :any,                 arm64_monterey: "61e84b73599f895586876457e55c9c29535fd340ac8c845054eb66e640b3adfe"
+    sha256 cellar: :any,                 arm64_big_sur:  "760de405f9142606ff2fa1f5908dc7dd320563692ff4c40791fc77359960aee6"
+    sha256 cellar: :any,                 monterey:       "7f60c896e594269ab814ae8cdda0fb83b9a5ed0839f762c2ec54c94ef57efe18"
+    sha256 cellar: :any,                 big_sur:        "28e575220e5c763828581176919d8dede228f91f3d83cacacdc0dda96e02f008"
+    sha256 cellar: :any,                 catalina:       "d3d28042641439ff3ea27bb47c777d96563d457791581ccb1e5ed148acbca14f"
+    sha256 cellar: :any,                 mojave:         "007118b6f7f387239c1743589f6040d2c035171fbe27db0c477e2a0cbc25bb80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "180a9961c67edef2f6955d646c4791134528c4dc40df79b513e8b2300b371f4d"
   end
 
   keg_only :versioned_formula
