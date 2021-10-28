082fe0d20516fd75c8d928e2e62d48d4db5ea1f8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 04:22:55 +0000

    verilator: update 4.200_1 bottle.

--- Formula/verilator.rb
+++ Formula/verilator.rb
@@ -12,10 +12,12 @@ class Verilator < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "b4c783a956765b7cca12010b793fe3d8728942897107d7a37d805b53bb6b446f"
-    sha256 big_sur:       "2e1d4b7478d0a85ebf90aff2dfcb506ab7950a0b9469f9bd306160682c7014ee"
-    sha256 catalina:      "15d0d1d9a1b335fd8bc14b35dba77e668ad723155d91e51943a11792e243f40e"
-    sha256 x86_64_linux:  "402ad2a601e053d5d888d22a936013693c2c5e97df14b94febfd5203abf7a404"
+    sha256 arm64_monterey: "745234546ac4e29afcc4ed3ec60396b66a52d5f3ee1d5c50bff0b9f7adb9e6e0"
+    sha256 arm64_big_sur:  "b4c783a956765b7cca12010b793fe3d8728942897107d7a37d805b53bb6b446f"
+    sha256 monterey:       "b8c590f31ab771bce5fe4dbf22c95cea8ee7b9bf12cb6362016e2806f7e286c0"
+    sha256 big_sur:        "2e1d4b7478d0a85ebf90aff2dfcb506ab7950a0b9469f9bd306160682c7014ee"
+    sha256 catalina:       "15d0d1d9a1b335fd8bc14b35dba77e668ad723155d91e51943a11792e243f40e"
+    sha256 x86_64_linux:   "402ad2a601e053d5d888d22a936013693c2c5e97df14b94febfd5203abf7a404"
   end
 
   head do
