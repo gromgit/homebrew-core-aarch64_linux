69d9aef8fbee2c80898044c3993469caf6e6afa2
Diffblue CI CProver <69641018+db-ci-cprover@users.noreply.github.com>
2021-10-28 13:03:37 +0000

    cbmc 5.43.0
    Closes #88194.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -2,8 +2,8 @@ class Cbmc < Formula
   desc "C Bounded Model Checker"
   homepage "https://www.cprover.org/cbmc/"
   url "https://github.com/diffblue/cbmc.git",
-      tag:      "cbmc-5.42.0",
-      revision: "7727f4530d59198381a42d250815305b1872a3ae"
+      tag:      "cbmc-5.43.0",
+      revision: "07895efefd6780560dffdae10d83857714ae6b8e"
   license "BSD-4-Clause"
 
   bottle do
