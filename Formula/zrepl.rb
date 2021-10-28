15103d3ab65d3c8aed941644b86e739d905c925f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 00:47:36 +0000

    zrepl: update 0.4.0 bottle.

--- Formula/zrepl.rb
+++ Formula/zrepl.rb
@@ -8,11 +8,13 @@ class Zrepl < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5f7aabb2340c6245bd76de2658f2d85eefa4892787a51de411d1c5fa0e273a70"
-    sha256 cellar: :any_skip_relocation, big_sur:       "21706026893bdb3aef1e8b66237d500fefc92519538491d212cea68616b01e1d"
-    sha256 cellar: :any_skip_relocation, catalina:      "2725ffa8a53c33564c61e6906bfe93a0c5e510b919757822b66ccec025b251d5"
-    sha256 cellar: :any_skip_relocation, mojave:        "0341ef3fdd925a2507f7d265dee147dc3a4a7249e5da86312abec05281b500fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa323fd542fdda26c040680679bcaf452306e653a91aeac5a20ca0569ff6d3fc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "303024f00388a2fced254a52a56017c6d65b75abf2a49868a23bf1d6110d5a9f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f7aabb2340c6245bd76de2658f2d85eefa4892787a51de411d1c5fa0e273a70"
+    sha256 cellar: :any_skip_relocation, monterey:       "7b7b42323528cfa48114e2e7bfe0e18d78ff76708099e822c25ee91d4696fc74"
+    sha256 cellar: :any_skip_relocation, big_sur:        "21706026893bdb3aef1e8b66237d500fefc92519538491d212cea68616b01e1d"
+    sha256 cellar: :any_skip_relocation, catalina:       "2725ffa8a53c33564c61e6906bfe93a0c5e510b919757822b66ccec025b251d5"
+    sha256 cellar: :any_skip_relocation, mojave:         "0341ef3fdd925a2507f7d265dee147dc3a4a7249e5da86312abec05281b500fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa323fd542fdda26c040680679bcaf452306e653a91aeac5a20ca0569ff6d3fc"
   end
 
   depends_on "go" => :build
