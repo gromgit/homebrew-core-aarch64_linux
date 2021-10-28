21f4d39f9d146702efb90a1beaa0e9b4606015a6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 00:24:08 +0000

    volt: update 0.3.7 bottle.

--- Formula/volt.rb
+++ Formula/volt.rb
@@ -7,13 +7,15 @@ class Volt < Formula
   head "https://github.com/vim-volt/volt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "836f10188a9b461531bac4b6eb803e4f86057342e443df99c0c466e224af18b6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f08427b7e8f71b984417f65a5154dde9883610fb683891e16e267928c578bd59"
-    sha256 cellar: :any_skip_relocation, catalina:      "60210297f62f908ef4090a7f69631ad02cb4fe2ce8472e953f67ad91caa9461c"
-    sha256 cellar: :any_skip_relocation, mojave:        "9db9e940c124e8e655cdd84b7d143f526535c588ebd6503acb3960143d08f905"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "7fd8887efcdc3a9816b2dea510c2e3ba218e0e719390841d3b0b416fde53378e"
-    sha256 cellar: :any_skip_relocation, sierra:        "4edc3f1130757ddbf0a7b3c018825f68f2ecb24417f3afc3fd54b532e8c72c46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe7d78905a357fa59f18330aab57269cce1fda6af521552918d54911fd075035"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "87cfdbc43edeb2cedc60ddda401062cad644f0fa6d799d7ef112800984a10da7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "836f10188a9b461531bac4b6eb803e4f86057342e443df99c0c466e224af18b6"
+    sha256 cellar: :any_skip_relocation, monterey:       "d7d07259218a768843d6c9131e6e9f616e242b50f01aacbbdb9f539960cbcf77"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f08427b7e8f71b984417f65a5154dde9883610fb683891e16e267928c578bd59"
+    sha256 cellar: :any_skip_relocation, catalina:       "60210297f62f908ef4090a7f69631ad02cb4fe2ce8472e953f67ad91caa9461c"
+    sha256 cellar: :any_skip_relocation, mojave:         "9db9e940c124e8e655cdd84b7d143f526535c588ebd6503acb3960143d08f905"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "7fd8887efcdc3a9816b2dea510c2e3ba218e0e719390841d3b0b416fde53378e"
+    sha256 cellar: :any_skip_relocation, sierra:         "4edc3f1130757ddbf0a7b3c018825f68f2ecb24417f3afc3fd54b532e8c72c46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe7d78905a357fa59f18330aab57269cce1fda6af521552918d54911fd075035"
   end
 
   depends_on "go" => :build
