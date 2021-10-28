9b54391be57116753a6fd3f1cceb2539ff7ef78c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 11:47:32 +0000

    ncftp: update 3.2.6 bottle.

--- Formula/ncftp.rb
+++ Formula/ncftp.rb
@@ -13,11 +13,13 @@ class Ncftp < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "1fc3f5a43b5e4e23f2bac0046acaf8f746d5d07e0eb6cf60d593830fb3acbf13"
-    sha256 big_sur:       "be10854d86393b58f542fbe778bf650c90a9635fbd14eff8149459838c4455c6"
-    sha256 catalina:      "c5759f94e328047e35a3f22c63ecf7bf7051edbc68432b3eb9db6c61d8f84bb2"
-    sha256 mojave:        "381492aa09e004859600ffc441ebd4dfe1c75685099debf5a7c283c15785a26c"
-    sha256 x86_64_linux:  "4e3ce2c25f180954ba28f3b4c630ede2dd4c798df59f5f5aa13fbfa2cd4f6acc"
+    sha256 arm64_monterey: "0d10c2e5a5cd32d495bb7cf1b16de23c8335658097ee209397170ccbec21e164"
+    sha256 arm64_big_sur:  "1fc3f5a43b5e4e23f2bac0046acaf8f746d5d07e0eb6cf60d593830fb3acbf13"
+    sha256 monterey:       "f8a7be7e00a9ed10c22c5396e8c67f1f8697cfe117c003a4e5a62f3a9f13f33e"
+    sha256 big_sur:        "be10854d86393b58f542fbe778bf650c90a9635fbd14eff8149459838c4455c6"
+    sha256 catalina:       "c5759f94e328047e35a3f22c63ecf7bf7051edbc68432b3eb9db6c61d8f84bb2"
+    sha256 mojave:         "381492aa09e004859600ffc441ebd4dfe1c75685099debf5a7c283c15785a26c"
+    sha256 x86_64_linux:   "4e3ce2c25f180954ba28f3b4c630ede2dd4c798df59f5f5aa13fbfa2cd4f6acc"
   end
 
   uses_from_macos "ncurses"
