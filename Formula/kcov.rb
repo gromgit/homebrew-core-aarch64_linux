770bff32123dc7ba0b9c301d7b1edf1e3e800d09
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 02:10:52 +0000

    kcov: update 38_1 bottle.

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -16,11 +16,13 @@ class Kcov < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "32aab6003ab8ca0cf45e55415ae5b78e6fc59f5e02fa1d84397df1be17261230"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e8181653c02129bd63fd438d3e5965cd25d5408ebe7c6699da2ab86b9a9f4340"
-    sha256 cellar: :any_skip_relocation, catalina:      "becb863fd482145cd67dab0b25df128b5deba598fa89a217cd6ff63ba79edbc5"
-    sha256 cellar: :any_skip_relocation, mojave:        "dd72670ef6f9abd9b44bf70b8fcb64faffe1ba4edfd4704c70de395d3594dc89"
-    sha256                               x86_64_linux:  "d06adbd0e486fd113be4eeb75322e8cad6b63091c39d4bb85562278bde569e76"
+    sha256 cellar: :any,                 arm64_monterey: "0c6ecf2b03d6aebbf4d4a3a54fee80a7293050ae7bb596a96275f804a3a33103"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "32aab6003ab8ca0cf45e55415ae5b78e6fc59f5e02fa1d84397df1be17261230"
+    sha256 cellar: :any,                 monterey:       "fa475a23f3e008465d4d4b96fa29b1ba7531f75efed0b670ab596a9099202738"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e8181653c02129bd63fd438d3e5965cd25d5408ebe7c6699da2ab86b9a9f4340"
+    sha256 cellar: :any_skip_relocation, catalina:       "becb863fd482145cd67dab0b25df128b5deba598fa89a217cd6ff63ba79edbc5"
+    sha256 cellar: :any_skip_relocation, mojave:         "dd72670ef6f9abd9b44bf70b8fcb64faffe1ba4edfd4704c70de395d3594dc89"
+    sha256                               x86_64_linux:   "d06adbd0e486fd113be4eeb75322e8cad6b63091c39d4bb85562278bde569e76"
   end
 
   depends_on "cmake" => :build
