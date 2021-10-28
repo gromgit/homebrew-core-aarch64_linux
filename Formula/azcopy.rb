aa87c1677961a65a378098009608db5e7bc44dc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-28 12:08:28 +0000

    azcopy: update 10.13.0 bottle.

--- Formula/azcopy.rb
+++ Formula/azcopy.rb
@@ -6,13 +6,10 @@ class Azcopy < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b69f208199e9c0961a0fd0bf131666d5205e1893a8b0c725267195f6ce5a992"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b5695aa8fb6ce52898782c407ee6395fb65532a65c5030d0ab438f6ae40580f"
-    sha256 cellar: :any_skip_relocation, monterey:       "87c0c978170fbecf7b435caa5290e26582f9eed14eb69d08e23794670350c0dd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "895a0df8375076afdd39649821374370c0771a3322cbcbd7f0e052c2157825be"
-    sha256 cellar: :any_skip_relocation, catalina:       "e90ca6c0354d38ab4efff87d95a9859b14d065f4cdc1c76d5a8df80300f1c5b8"
-    sha256 cellar: :any_skip_relocation, mojave:         "cd976cce204fedeca62755455406045d4d3d6303ca625407105c4b61d2fd4262"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "467fbdd5993292c56a2bc3776b2954093f9db2516d0176b98b80756ed3634766"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a6ef402590c4a392cecee26f41e84dff712308fd9ff12a1ddc3907e1a3849aa3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fd18912ddee9a577a33f12d175678e9245f4c09b77a7ba148ddb6241c0b8add9"
+    sha256 cellar: :any_skip_relocation, catalina:      "4f7bc9444f9b1dfa52b58d5d4400eaf702a56e420a52f18d423cc8a445ee8e5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c713bcc655c81265b62b27e6a97d8d3b526a62e5ba22494345e77b406cb67759"
   end
 
   depends_on "go" => :build
