e6c11ece8434dc950187e777f537245729fe0131
Sam Ford <1584702+samford@users.noreply.github.com>
2021-10-28 10:52:07 -0400

    bchunk: add livecheck

--- Formula/bchunk.rb
+++ Formula/bchunk.rb
@@ -6,6 +6,11 @@ class Bchunk < Formula
   license "GPL-2.0"
   head "https://github.com/hessu/bchunk.git", branch: "master"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?bchunk[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c361f431e4d301b9a0805db04d500f73c4247d06067aba3ec74944525bc7855c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "94279b4e400c05770ec6c5cce6fe7ef50a062835508add3e981942944bb3eecc"
