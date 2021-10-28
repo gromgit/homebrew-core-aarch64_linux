2463d754aee480abdb3ea314e3dcd2ab45d7fc0e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 11:41:40 +0000

    flatbuffers: update 2.0.0 bottle.

--- Formula/flatbuffers.rb
+++ Formula/flatbuffers.rb
@@ -7,11 +7,13 @@ class Flatbuffers < Formula
   head "https://github.com/google/flatbuffers.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd07b2efac10b35609da3ab52c9549d360c6db6b6193d5b916dc5eb98c27267b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c5a82ba1d1ce4c4b77053bc294af6a5e6094c5b7e59e4b4c946b4301dd71b6b3"
-    sha256 cellar: :any_skip_relocation, catalina:      "149eda4aca1b555fd228e9edc2bf4987eee46e0445ae1bff79404177149e6f72"
-    sha256 cellar: :any_skip_relocation, mojave:        "3827dfa116cdaf777f7bb5023b3eaecb2820265bcc57dce833724f84d2c17065"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cfcaa60b345226421198d8413ba4973db4d9f56ce471c6e95a7d50e0cabee3f9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a127e4a829de86f314a3990b9d85bf14d854bb682e8f8e32272990095b2b654a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd07b2efac10b35609da3ab52c9549d360c6db6b6193d5b916dc5eb98c27267b"
+    sha256 cellar: :any_skip_relocation, monterey:       "ccc0b4894dcef07cc2f7ab8b08822b0d32b418e51caf1f091dc58260f73871c3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c5a82ba1d1ce4c4b77053bc294af6a5e6094c5b7e59e4b4c946b4301dd71b6b3"
+    sha256 cellar: :any_skip_relocation, catalina:       "149eda4aca1b555fd228e9edc2bf4987eee46e0445ae1bff79404177149e6f72"
+    sha256 cellar: :any_skip_relocation, mojave:         "3827dfa116cdaf777f7bb5023b3eaecb2820265bcc57dce833724f84d2c17065"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cfcaa60b345226421198d8413ba4973db4d9f56ce471c6e95a7d50e0cabee3f9"
   end
 
   depends_on "cmake" => :build
