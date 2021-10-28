edf8fcd8baac746a4f9ec2f4ee4555aaa1412b02
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 00:26:09 +0000

    stolon: update 0.17.0 bottle.

--- Formula/stolon.rb
+++ Formula/stolon.rb
@@ -7,10 +7,12 @@ class Stolon < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "b0a56f3249029127bbee0714cabddf2aa1bd6fd8f8ddfa3d930318be36914c06"
-    sha256 cellar: :any_skip_relocation, catalina:     "ced3403c83e7d19c21117acb58056756538c9c76dd76c8cf28330c0c4c261ee9"
-    sha256 cellar: :any_skip_relocation, mojave:       "544b80f00ebb9447d95a1cb981147b95dbbe668abb0cf6037e5307460602d563"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e7b19327fb4e9f472bd1c830a8c4f33a121a1c0a589f59a64dd6b322cabf87b2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "da91318804a2a44ef42d195448013fe9696c5d8a6634d28f48c42d12af20e27a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4ef48e603eb0ea38a2b67ae20b6bcb3553e1f208db93b60f2a16b39d63f6d6f0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b0a56f3249029127bbee0714cabddf2aa1bd6fd8f8ddfa3d930318be36914c06"
+    sha256 cellar: :any_skip_relocation, catalina:       "ced3403c83e7d19c21117acb58056756538c9c76dd76c8cf28330c0c4c261ee9"
+    sha256 cellar: :any_skip_relocation, mojave:         "544b80f00ebb9447d95a1cb981147b95dbbe668abb0cf6037e5307460602d563"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7b19327fb4e9f472bd1c830a8c4f33a121a1c0a589f59a64dd6b322cabf87b2"
   end
 
   depends_on "go" => :build
