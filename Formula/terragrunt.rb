4da09b94811d74cf4f9b09a4284eda00d7dd3dd7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 16:38:10 +0000

    terragrunt: update 0.35.5 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -6,10 +6,12 @@ class Terragrunt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "115c3db7a8e3c216eb4007f71e2518c98995290add7b84b7482e32e95a5298ce"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3664b39da2d7c8903495f02163bc87496d914b6e74994e717af28b74901cfadd"
-    sha256 cellar: :any_skip_relocation, catalina:      "2187c9bc314a86075102cd3838d8a2a7cda5a8e75d7f36ca41db57f5abb5e5bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "96294e0d6437baebbe94d740d03c72974df4edb29ef5851bd1d8edef2ec4c09a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd980aa082ae8a35fa3e103d85a5ae76aa0a849e243cba2686332415e86aa2fe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "115c3db7a8e3c216eb4007f71e2518c98995290add7b84b7482e32e95a5298ce"
+    sha256 cellar: :any_skip_relocation, monterey:       "72abda93d52ef754118a89553b92af30c95d480c7cbb0bd1f28e775688f01a53"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3664b39da2d7c8903495f02163bc87496d914b6e74994e717af28b74901cfadd"
+    sha256 cellar: :any_skip_relocation, catalina:       "2187c9bc314a86075102cd3838d8a2a7cda5a8e75d7f36ca41db57f5abb5e5bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "96294e0d6437baebbe94d740d03c72974df4edb29ef5851bd1d8edef2ec4c09a"
   end
 
   depends_on "go" => :build
