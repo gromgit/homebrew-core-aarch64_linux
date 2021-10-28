841f7a33ca3cd92ecdce63157664011a8e03ded1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 11:51:36 +0000

    argp-standalone: update 1.3 bottle.

--- Formula/argp-standalone.rb
+++ Formula/argp-standalone.rb
@@ -11,14 +11,16 @@ class ArgpStandalone < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "88d3f4bfb3ab27ec4105a6853fa34b94342ab2ad0d7b79248461b3bea52649a6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "65a3586ad4399576aeb46e497b4ec08577f84764b7a28448ae6882a2c422068d"
-    sha256 cellar: :any_skip_relocation, catalina:      "560e1829dce7113479d755380e0e65318a996f0d04d8e0761f24810e9e019e7d"
-    sha256 cellar: :any_skip_relocation, mojave:        "fb60d10ba68efda61d1dfdb161bcf9bfa0474c82b03e0579517cb10608aa9aea"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "92532fafd8c2cc86b33de0f347496746d8049bb4d1a6ce0948148e0f3c4bca5a"
-    sha256 cellar: :any_skip_relocation, sierra:        "10627e72c0e0eb66cbd03a2beb767c06b8edad4bef01914de7f7c6c1be33a356"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "798e6ddb78957f9ad33662287b5971aaf3a43f3646e84691d56b3b85ca06d47f"
-    sha256 cellar: :any_skip_relocation, yosemite:      "c926ac0ad3b8dbb8c3e08299ade556470f81d3a88eb51dc60e7cfe107da533e8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d538d7be595c57c757670fbcd922b9335f9a4a47ef16662233e84b2260eb387b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "88d3f4bfb3ab27ec4105a6853fa34b94342ab2ad0d7b79248461b3bea52649a6"
+    sha256 cellar: :any_skip_relocation, monterey:       "f0ed596672be7b1b893ea07d341bce448470b3448b55e0b5d90f68e7895de7b2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "65a3586ad4399576aeb46e497b4ec08577f84764b7a28448ae6882a2c422068d"
+    sha256 cellar: :any_skip_relocation, catalina:       "560e1829dce7113479d755380e0e65318a996f0d04d8e0761f24810e9e019e7d"
+    sha256 cellar: :any_skip_relocation, mojave:         "fb60d10ba68efda61d1dfdb161bcf9bfa0474c82b03e0579517cb10608aa9aea"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "92532fafd8c2cc86b33de0f347496746d8049bb4d1a6ce0948148e0f3c4bca5a"
+    sha256 cellar: :any_skip_relocation, sierra:         "10627e72c0e0eb66cbd03a2beb767c06b8edad4bef01914de7f7c6c1be33a356"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "798e6ddb78957f9ad33662287b5971aaf3a43f3646e84691d56b3b85ca06d47f"
+    sha256 cellar: :any_skip_relocation, yosemite:       "c926ac0ad3b8dbb8c3e08299ade556470f81d3a88eb51dc60e7cfe107da533e8"
   end
 
   # This patch fixes compilation with Clang.
