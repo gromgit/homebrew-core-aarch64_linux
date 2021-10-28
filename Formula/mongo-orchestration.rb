a3bf08e14e24cc742618d477ad4d6ce97e916e6e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 05:01:19 +0000

    mongo-orchestration: update 0.7.0_1 bottle.

--- Formula/mongo-orchestration.rb
+++ Formula/mongo-orchestration.rb
@@ -10,11 +10,13 @@ class MongoOrchestration < Formula
   head "https://github.com/10gen/mongo-orchestration.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3be3d4d3361c13dd72c358ec25226486237b0669b3518dc27094ebb9b5cde0b7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d2042e1ea1f997f23c01dc88bc5e8cecb2fa08c58e5c641035d8d58c2d013be5"
-    sha256 cellar: :any_skip_relocation, catalina:      "24d106d1c9511112ce1a7c91664ea001d9d8dd074e303afd71b139fdfca6b14d"
-    sha256 cellar: :any_skip_relocation, mojave:        "80b35759af83e1a7effb496027d247cc856363fdad9d34238bf7c58f49b32ff9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02eb4bd1c6fcd120628f289d72e5caf2e3f7efb20611f44cecb0059eb847b7ef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd965e6c5f77a0d14a7eb917db11b60268677ce338fa303c50f69eda3cd0bab3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3be3d4d3361c13dd72c358ec25226486237b0669b3518dc27094ebb9b5cde0b7"
+    sha256 cellar: :any_skip_relocation, monterey:       "0818aea805f4196c41de20ecc0edbb222cd740229c673e7599e8203d6a0cce92"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d2042e1ea1f997f23c01dc88bc5e8cecb2fa08c58e5c641035d8d58c2d013be5"
+    sha256 cellar: :any_skip_relocation, catalina:       "24d106d1c9511112ce1a7c91664ea001d9d8dd074e303afd71b139fdfca6b14d"
+    sha256 cellar: :any_skip_relocation, mojave:         "80b35759af83e1a7effb496027d247cc856363fdad9d34238bf7c58f49b32ff9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02eb4bd1c6fcd120628f289d72e5caf2e3f7efb20611f44cecb0059eb847b7ef"
   end
 
   depends_on "python@3.10"
