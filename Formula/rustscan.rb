3933984a917a5b95b86f7f6b9aa301ff816d68c2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 10:19:55 +0000

    rustscan: update 2.0.1 bottle.

--- Formula/rustscan.rb
+++ Formula/rustscan.rb
@@ -6,6 +6,7 @@ class Rustscan < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:     "3b9fda4219ae6932bc7725b11709405731ccc2ca3c04df7d4abe4ed00219ef8d"
     sha256 cellar: :any_skip_relocation, big_sur:      "83c8a3e9b4a71e21590bd377b1db9518cb32794d341d161519e974dd3e837f2e"
     sha256 cellar: :any_skip_relocation, catalina:     "21f4b50689613475ca9194ecae3b062e874d49bb987b1949f453885360715200"
     sha256 cellar: :any_skip_relocation, mojave:       "135ddda8cfa5a670c0c49e3730148e8f50dfa1b597d9bdf2c35b5fc4d5a8fd9e"
