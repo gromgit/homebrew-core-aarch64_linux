f0c031176516c3b110398f3ec955873b6366f2e4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 02:06:01 +0000

    ispc: update 1.16.1_1 bottle.

--- Formula/ispc.rb
+++ Formula/ispc.rb
@@ -7,10 +7,12 @@ class Ispc < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "816feef4722edd8866c394110c503338eaba0bb373c87ddb9a898dc56b1adac7"
-    sha256 cellar: :any, big_sur:       "a82168d4f3a51a8078eb3603a9e3810f5a81e32e131fce032f4b505d6d0147e7"
-    sha256 cellar: :any, catalina:      "168dcec41346433e47d5478793216c86b96c4e9c2a31673e21de60b5f6a95427"
-    sha256 cellar: :any, mojave:        "d19b58b941d33a38e574e351d85445c5f8aee6c9ca6a164db874a2753138ae1c"
+    sha256 cellar: :any, arm64_monterey: "a9e533da0a8a1ad0327cbe13cf4e73712fd83d40eeff92286e9b2b404a6be58b"
+    sha256 cellar: :any, arm64_big_sur:  "816feef4722edd8866c394110c503338eaba0bb373c87ddb9a898dc56b1adac7"
+    sha256 cellar: :any, monterey:       "b0d22d24b1a89933c5ccc6b73968641c3ebc99839cb5e7fdbe2135cc9ce4673d"
+    sha256 cellar: :any, big_sur:        "a82168d4f3a51a8078eb3603a9e3810f5a81e32e131fce032f4b505d6d0147e7"
+    sha256 cellar: :any, catalina:       "168dcec41346433e47d5478793216c86b96c4e9c2a31673e21de60b5f6a95427"
+    sha256 cellar: :any, mojave:         "d19b58b941d33a38e574e351d85445c5f8aee6c9ca6a164db874a2753138ae1c"
   end
 
   depends_on "bison" => :build
