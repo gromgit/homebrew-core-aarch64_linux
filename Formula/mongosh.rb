b23d26f3f7887ab321da7f318172d8ac2a9a0ded
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 16:40:31 +0000

    mongosh: update 1.1.1 bottle.

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -8,10 +8,12 @@ class Mongosh < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256                               arm64_big_sur: "6aa3681f5f8f00e6f729cf13b1f4c0441c581dd4afd04b2f83c13df8dc804fc0"
-    sha256                               big_sur:       "3b052e833a4fb465ea52ff8cb13dd0b28e5d5b4c361c31b29aff6b8fbf7d80db"
-    sha256                               catalina:      "99a8a9c20ed9576dc3bf55704ac488482227d5d43dfd4d585ed2cb15afad0996"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a47aab9005c35cee091bd0d0e84d0fb688b469bb9a247a0944181e43db2bb37"
+    sha256                               arm64_monterey: "6b4e08c8fcec4cca588002bac497eaa595790894b0ce70c3e4706ab1a2ab3ddc"
+    sha256                               arm64_big_sur:  "6aa3681f5f8f00e6f729cf13b1f4c0441c581dd4afd04b2f83c13df8dc804fc0"
+    sha256                               monterey:       "88656f02ce2f58a421ef1d74dcccfd1f2813048fe13eeb51bcb26134e56f8d0b"
+    sha256                               big_sur:        "3b052e833a4fb465ea52ff8cb13dd0b28e5d5b4c361c31b29aff6b8fbf7d80db"
+    sha256                               catalina:       "99a8a9c20ed9576dc3bf55704ac488482227d5d43dfd4d585ed2cb15afad0996"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a47aab9005c35cee091bd0d0e84d0fb688b469bb9a247a0944181e43db2bb37"
   end
 
   depends_on "node@14"
