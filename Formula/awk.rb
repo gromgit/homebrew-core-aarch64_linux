c1c01b10bacb998ed70b9f09d90d2bb81563fe86
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 11:49:09 +0000

    awk: update 20180827 bottle.

--- Formula/awk.rb
+++ Formula/awk.rb
@@ -9,11 +9,13 @@ class Awk < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "09cf1fe371a924c163732e19310f635953ae3f4909a9c3baa9db4ea4d850d905"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a73059712fb0cc9a57389f72596ca5c52978ad54bf9dce04baedbf653b154022"
-    sha256 cellar: :any_skip_relocation, catalina:      "681acac2c4bac4b8f275f640abf46e5391216d800ff34ad8c57c8d116674fae6"
-    sha256 cellar: :any_skip_relocation, mojave:        "202e81f1562d8b46d4b932f91c64ab58fc6017f6cdd2cc3f2a636038abc76fc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a6d47998c2945313c463fb2736c6381f2bb6bfa725d902e17b7dded8f3025bb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "41e60320408494ad391cefeedf05d31d0692fdf29461aee70a75f110796efe4e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "09cf1fe371a924c163732e19310f635953ae3f4909a9c3baa9db4ea4d850d905"
+    sha256 cellar: :any_skip_relocation, monterey:       "aecf06d8731df59c04db7b67b4d4e8e4acfbf3a455e9eebafc082acc91e8385f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a73059712fb0cc9a57389f72596ca5c52978ad54bf9dce04baedbf653b154022"
+    sha256 cellar: :any_skip_relocation, catalina:       "681acac2c4bac4b8f275f640abf46e5391216d800ff34ad8c57c8d116674fae6"
+    sha256 cellar: :any_skip_relocation, mojave:         "202e81f1562d8b46d4b932f91c64ab58fc6017f6cdd2cc3f2a636038abc76fc0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a6d47998c2945313c463fb2736c6381f2bb6bfa725d902e17b7dded8f3025bb"
   end
 
   uses_from_macos "bison"
