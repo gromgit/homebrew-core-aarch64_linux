2cd1ff727ca527663e14e999155e4da714f7faa6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 07:56:52 +0000

    lldpd: update 1.0.12 bottle.

--- Formula/lldpd.rb
+++ Formula/lldpd.rb
@@ -10,11 +10,13 @@ class Lldpd < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "8984826e95e004de9684e27686016a83ea79dd330dbfeb986ae1da382dfe9741"
-    sha256 big_sur:       "1875a47b0f6c506e5e1589e61b939d6e6bb8849ba4e3da27f7e50951f12b802a"
-    sha256 catalina:      "f7a97457480a6729c8fbe2360d0d8b3a3f8dbe55cbe05ae9e7ced15e5bd99d26"
-    sha256 mojave:        "7727979d19df29e689f5ffea1921c5e8bcc9cecb86fd4623cd29d3c1d4912910"
-    sha256 x86_64_linux:  "ba438cefa8414d35140c5eaebe36f6fa7abe8bdf3fa77245d48e8ab412f33c93"
+    sha256 arm64_monterey: "5133b1d8731c60926daeaa7d2c7fb75426af48afa094f02ee6473896f4f1ee5b"
+    sha256 arm64_big_sur:  "8984826e95e004de9684e27686016a83ea79dd330dbfeb986ae1da382dfe9741"
+    sha256 monterey:       "08fd1196e68026eee7cab4b1594f5d17dfc0796e3bab4ed21e2077f4ed23864a"
+    sha256 big_sur:        "1875a47b0f6c506e5e1589e61b939d6e6bb8849ba4e3da27f7e50951f12b802a"
+    sha256 catalina:       "f7a97457480a6729c8fbe2360d0d8b3a3f8dbe55cbe05ae9e7ced15e5bd99d26"
+    sha256 mojave:         "7727979d19df29e689f5ffea1921c5e8bcc9cecb86fd4623cd29d3c1d4912910"
+    sha256 x86_64_linux:   "ba438cefa8414d35140c5eaebe36f6fa7abe8bdf3fa77245d48e8ab412f33c93"
   end
 
   depends_on "pkg-config" => :build
