18f71371c2a79f99e6daf96a9ef2fbd64ee570b4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 10:38:03 +0000

    librespot: update 0.3.1 bottle.

--- Formula/librespot.rb
+++ Formula/librespot.rb
@@ -9,6 +9,7 @@ class Librespot < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "881400cd44d2aeb0af3e0fed1dcce1855bed0e60b60a78720c2b020fd8b073bf"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6127d387d29a582a1fbb6944c9b5fd18c4febbc483a3711e4248213fa4f01111"
+    sha256 cellar: :any_skip_relocation, monterey:       "ac7a661b51abbc6324d597e35163862cc03b37583544152caaadc06a9cb69f4b"
     sha256 cellar: :any_skip_relocation, big_sur:        "8823b6d1718e2f8d6898c9306501a29ca36204159fd325725e02a187be77e325"
     sha256 cellar: :any_skip_relocation, catalina:       "bc83caa0485d4dd036f5bdc0643e9b10659799a3f0961442b2dcad349a9f3910"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "8a65c6190910c07e93e5151813c1c868b7b7f9d97b0dcb582b9bda88226c87a7"
