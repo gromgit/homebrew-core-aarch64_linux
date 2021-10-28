75a56349236c3d1938f7c6a5d4be8f63d610de05
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 03:50:12 +0000

    lensfun: update 0.3.95_4 bottle.

--- Formula/lensfun.rb
+++ Formula/lensfun.rb
@@ -14,11 +14,13 @@ class Lensfun < Formula
   revision 4
 
   bottle do
-    sha256 arm64_big_sur: "976711172998eae467ddaba1feb590e0229cc0b41f11ac58e1db2d833a57c99c"
-    sha256 big_sur:       "48cd331c4214979daa6c122e2b776000af76208cb051562e27f4cef4f3aa3b93"
-    sha256 catalina:      "b0d8cdbcf20af0b1d577626e04643687955030785f57911e9d0a708a7ef95997"
-    sha256 mojave:        "526b6752883c94e7e2807fa06e6803e9dc45060189be102be5ed79c24b187af6"
-    sha256 x86_64_linux:  "d5758ba26c4bb2d4134bc733a302a30b6534f7b5e64dbd25ec519c39f5234c7a"
+    sha256 arm64_monterey: "530ebafb7cb54daaa3095f543ba8f05e331fd8a36265fbb2cfbe482e5822a223"
+    sha256 arm64_big_sur:  "976711172998eae467ddaba1feb590e0229cc0b41f11ac58e1db2d833a57c99c"
+    sha256 monterey:       "08fec3eeb7b95d1c468b2525e2b92a7df9c34f1b6c7f4003d2c0cdaeb72f983f"
+    sha256 big_sur:        "48cd331c4214979daa6c122e2b776000af76208cb051562e27f4cef4f3aa3b93"
+    sha256 catalina:       "b0d8cdbcf20af0b1d577626e04643687955030785f57911e9d0a708a7ef95997"
+    sha256 mojave:         "526b6752883c94e7e2807fa06e6803e9dc45060189be102be5ed79c24b187af6"
+    sha256 x86_64_linux:   "d5758ba26c4bb2d4134bc733a302a30b6534f7b5e64dbd25ec519c39f5234c7a"
   end
 
   depends_on "cmake" => :build
