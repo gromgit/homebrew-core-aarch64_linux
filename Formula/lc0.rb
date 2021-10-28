fb03ae3d8d1fa025d65fb8f8da7f55457ed8e2f3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 03:45:13 +0000

    lc0: update 0.28.0 bottle.

--- Formula/lc0.rb
+++ Formula/lc0.rb
@@ -7,11 +7,13 @@ class Lc0 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "36aa6aad76aee88ce231dc5a0aac948f097055351813351876acd08dfc450a03"
-    sha256 cellar: :any_skip_relocation, big_sur:       "994f3b0045579eccda6c5eb1241a4484c202bb9c8528184ba2e4eee770a7e2c5"
-    sha256 cellar: :any_skip_relocation, catalina:      "e37cf24d564b5649d873692e5162a90e7e1840e2346326be86ef0ba25c8b7737"
-    sha256 cellar: :any_skip_relocation, mojave:        "650ff1cdf5e4aad508572322e89010722c92bf30b89ed07f95cdec6dd1fd0140"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d89547675b9eb3c62073100047d9f9d91e51f9d3063655cf38e7f836dd28063"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "392f9168c6f244ddff5ae49eeaa336fa96c4c76e29f6cfa8932c40defca12889"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "36aa6aad76aee88ce231dc5a0aac948f097055351813351876acd08dfc450a03"
+    sha256 cellar: :any_skip_relocation, monterey:       "d109979fed723569ff979e1cc24db86f1df7c5c8bf8b8bfcf9be6eb971441e28"
+    sha256 cellar: :any_skip_relocation, big_sur:        "994f3b0045579eccda6c5eb1241a4484c202bb9c8528184ba2e4eee770a7e2c5"
+    sha256 cellar: :any_skip_relocation, catalina:       "e37cf24d564b5649d873692e5162a90e7e1840e2346326be86ef0ba25c8b7737"
+    sha256 cellar: :any_skip_relocation, mojave:         "650ff1cdf5e4aad508572322e89010722c92bf30b89ed07f95cdec6dd1fd0140"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d89547675b9eb3c62073100047d9f9d91e51f9d3063655cf38e7f836dd28063"
   end
 
   depends_on "cmake" => :build
