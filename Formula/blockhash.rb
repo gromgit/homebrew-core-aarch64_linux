5af98237993e43f996d72f4b9be9849911ca318b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 01:05:40 +0000

    blockhash: update 0.3.1_4 bottle.

--- Formula/blockhash.rb
+++ Formula/blockhash.rb
@@ -8,10 +8,12 @@ class Blockhash < Formula
   head "https://github.com/commonsmachinery/blockhash.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1263f1d1a652f85a0cd026b5f9d1e1df6f1f467aee9a4336dfcacd02d590253b"
-    sha256 cellar: :any, big_sur:       "e7dad94beb23855c8bb9b9145da15b74b29f11f5125c3c9cb495d9894a747a3a"
-    sha256 cellar: :any, catalina:      "16fa35be4bfea3e7e71ee26879f2e8508062b3f9684c178bc0204c0cec1e0284"
-    sha256 cellar: :any, mojave:        "b6fd37575c7c00ab516240c916d7af8576655188c4fa830bf04833859d804b7f"
+    sha256 cellar: :any, arm64_monterey: "bb25e5ae4964868f4d3ce33bd622f5639d9dfcde90efdc444cfe1b6701ab283f"
+    sha256 cellar: :any, arm64_big_sur:  "1263f1d1a652f85a0cd026b5f9d1e1df6f1f467aee9a4336dfcacd02d590253b"
+    sha256 cellar: :any, monterey:       "16715363dffbf20fcd63777ee39f46998414fac808c841c5b4e82398ba1df18c"
+    sha256 cellar: :any, big_sur:        "e7dad94beb23855c8bb9b9145da15b74b29f11f5125c3c9cb495d9894a747a3a"
+    sha256 cellar: :any, catalina:       "16fa35be4bfea3e7e71ee26879f2e8508062b3f9684c178bc0204c0cec1e0284"
+    sha256 cellar: :any, mojave:         "b6fd37575c7c00ab516240c916d7af8576655188c4fa830bf04833859d804b7f"
   end
 
   depends_on "pkg-config" => :build
