193ce349e37ba45ddbf3dad1906d4b467eac3346
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-28 12:08:36 +0000

    infracost: update 0.9.12 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -7,13 +7,10 @@ class Infracost < Formula
   head "https://github.com/infracost/infracost.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fa4f870b1e09495e7adcd9c639bea55e8aeeb0da7c2365a8dc0ca9fa4b81e2f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa4f870b1e09495e7adcd9c639bea55e8aeeb0da7c2365a8dc0ca9fa4b81e2f9"
-    sha256 cellar: :any_skip_relocation, monterey:       "d8bad92116b4a7ffb60b1884d9d37b3c55afdbe97bc53b0dc1fe94591c8dbc3f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d8bad92116b4a7ffb60b1884d9d37b3c55afdbe97bc53b0dc1fe94591c8dbc3f"
-    sha256 cellar: :any_skip_relocation, catalina:       "d8bad92116b4a7ffb60b1884d9d37b3c55afdbe97bc53b0dc1fe94591c8dbc3f"
-    sha256 cellar: :any_skip_relocation, mojave:         "d8bad92116b4a7ffb60b1884d9d37b3c55afdbe97bc53b0dc1fe94591c8dbc3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "170fad23d165922751b0132b9f00d396b8010b191bec8448343f2935ce07f9b1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3441e092e5ef9da70e2b9cc0e5d66eb456d5dc6e33cbe141ebe70892e07031bf"
+    sha256 cellar: :any_skip_relocation, big_sur:       "23cb46ad7b96ed5ee8f1e696986d94976f808f89c444eb1dfca7454524fddb6c"
+    sha256 cellar: :any_skip_relocation, catalina:      "23cb46ad7b96ed5ee8f1e696986d94976f808f89c444eb1dfca7454524fddb6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99a890bd840332214f4555c93674476dbef4cc69726e24858355ecaf9f0f0df8"
   end
 
   depends_on "go" => :build
