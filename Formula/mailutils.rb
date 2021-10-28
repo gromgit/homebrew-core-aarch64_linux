70c9adb77a934269d9514fb37d338ddf4ea4a952
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 05:17:29 +0000

    mailutils: update 3.13 bottle.

--- Formula/mailutils.rb
+++ Formula/mailutils.rb
@@ -7,11 +7,13 @@ class Mailutils < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "30207f5919cdd31f2fb409d3c1ce2ed7b3f80dd1919c6cfe5fe17b92ae70000a"
-    sha256 big_sur:       "1182bc30b75d80f84753b5ddf53c74cb7bbeae001286747d402fc8e046c9cfa0"
-    sha256 catalina:      "fe4db7c7eb259f4cd9a657c8f876d045baa4026b7fdb3a5e1ee6bb46e5105973"
-    sha256 mojave:        "511990696c674dab6a9b1d180f987bb53a214e1bc889387d09dea134f6669216"
-    sha256 x86_64_linux:  "dd024392296a37d55a74566cc7fd5299597ddc4a69347991c0bc7e200bf1d1c4"
+    sha256 arm64_monterey: "f3d0df511ccf76e83d4ff1b290af40a82b5e8982b45a52fbccd5cfa5c8cacb7a"
+    sha256 arm64_big_sur:  "30207f5919cdd31f2fb409d3c1ce2ed7b3f80dd1919c6cfe5fe17b92ae70000a"
+    sha256 monterey:       "af82f7aff57b46968e30ef83e5db0a37082d748a1236eddb6abe9a7a0f845f7e"
+    sha256 big_sur:        "1182bc30b75d80f84753b5ddf53c74cb7bbeae001286747d402fc8e046c9cfa0"
+    sha256 catalina:       "fe4db7c7eb259f4cd9a657c8f876d045baa4026b7fdb3a5e1ee6bb46e5105973"
+    sha256 mojave:         "511990696c674dab6a9b1d180f987bb53a214e1bc889387d09dea134f6669216"
+    sha256 x86_64_linux:   "dd024392296a37d55a74566cc7fd5299597ddc4a69347991c0bc7e200bf1d1c4"
   end
 
   depends_on "gnutls"
