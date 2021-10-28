76e682504f542576225220c85b99aeec52a39ed0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 03:57:23 +0000

    libtcod: update 1.18.1 bottle.

--- Formula/libtcod.rb
+++ Formula/libtcod.rb
@@ -6,10 +6,12 @@ class Libtcod < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1fab6b1ff1ee7f9ab0ded5b89867f4250b882445ffef5c73b20348227abdd00f"
-    sha256 cellar: :any, big_sur:       "0110681440d6825ef184dea27219a190126ab176d5e278c5d27c2e38c33444f5"
-    sha256 cellar: :any, catalina:      "55e5e93eb87f3e7b7df2fdaa497b8888bf899139b4cad0cb1c1790eeed77bb6b"
-    sha256 cellar: :any, mojave:        "b9b4bd6b64f52cac8e09e7263caed4bad328eae3a58e3e43cba7b50a957c1371"
+    sha256 cellar: :any, arm64_monterey: "eb25fc002f492d38ed3a8acefa9b2c8af6b538b0c61e8d7ba76456c161a0902b"
+    sha256 cellar: :any, arm64_big_sur:  "1fab6b1ff1ee7f9ab0ded5b89867f4250b882445ffef5c73b20348227abdd00f"
+    sha256 cellar: :any, monterey:       "d7cb93d38458e8d83b7b61b6dfb5c65e16965572cb40f0e60d9b3809a8bfabde"
+    sha256 cellar: :any, big_sur:        "0110681440d6825ef184dea27219a190126ab176d5e278c5d27c2e38c33444f5"
+    sha256 cellar: :any, catalina:       "55e5e93eb87f3e7b7df2fdaa497b8888bf899139b4cad0cb1c1790eeed77bb6b"
+    sha256 cellar: :any, mojave:         "b9b4bd6b64f52cac8e09e7263caed4bad328eae3a58e3e43cba7b50a957c1371"
   end
 
   depends_on "autoconf" => :build
