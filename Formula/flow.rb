5e738ff44a466ccc027c86f57df8620d93a4c485
rui <rui@chenrui.dev>
2021-10-28 18:59:42 +0000

    flow: update 0.163.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -7,10 +7,12 @@ class Flow < Formula
   head "https://github.com/facebook/flow.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "44e53b454e64c744a661c2f78f639f30df8a422298d01a51687685cd96853825"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e2b79e1de9758410916ba2264ed728ae664276975de01042b4d1323f9ad3b220"
-    sha256 cellar: :any_skip_relocation, catalina:      "7a7f84270a50887307c8168ed5bd98817e85ec27f18a95538452f8a8636491bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f815cd4cf56e8dc7cc09fa2fc959bd8d430be8c6a9bcd12cdc558980c6e76dba"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f7dff924512c0d5e8d69988aa7a1c6d719def7512751df49edc5c8342fbcde4c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "44e53b454e64c744a661c2f78f639f30df8a422298d01a51687685cd96853825"
+    sha256 cellar: :any_skip_relocation, monterey:       "e20dbb70eae0b120f507b06c401939eb63cb879a8c9ce71c240148b73820f5fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e2b79e1de9758410916ba2264ed728ae664276975de01042b4d1323f9ad3b220"
+    sha256 cellar: :any_skip_relocation, catalina:       "7a7f84270a50887307c8168ed5bd98817e85ec27f18a95538452f8a8636491bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f815cd4cf56e8dc7cc09fa2fc959bd8d430be8c6a9bcd12cdc558980c6e76dba"
   end
 
   depends_on "ocaml" => :build
