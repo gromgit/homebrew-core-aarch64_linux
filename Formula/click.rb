714c1592f7a037cec6ec88d8bc7090c914e82ebb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 09:59:25 +0000

    click: update 0.5.4 bottle.

--- Formula/click.rb
+++ Formula/click.rb
@@ -7,10 +7,11 @@ class Click < Formula
   head "https://github.com/databricks/click.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "aa74cec2f0d6854791b46d54adbdb96bff085b67278629695f0ac266eef54717"
-    sha256 cellar: :any_skip_relocation, catalina:     "59c72a4b00a3bf477cd3d65573175d6009112273b715b30d49be7bc694fdcf80"
-    sha256 cellar: :any_skip_relocation, mojave:       "b7f4b485ff0eb502694555d0f85096880e6a0355b7e69adf2bb5075d2396ade1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "553af140b3e4382834691f12fba51f61640eee9aa35ed2db8a20732a6ccd9488"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "08fffc5fa5f08e292d5ebb07ccbd3d8cfe2b7ec70d06420377c94e849b4bc3c3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "aa74cec2f0d6854791b46d54adbdb96bff085b67278629695f0ac266eef54717"
+    sha256 cellar: :any_skip_relocation, catalina:       "59c72a4b00a3bf477cd3d65573175d6009112273b715b30d49be7bc694fdcf80"
+    sha256 cellar: :any_skip_relocation, mojave:         "b7f4b485ff0eb502694555d0f85096880e6a0355b7e69adf2bb5075d2396ade1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "553af140b3e4382834691f12fba51f61640eee9aa35ed2db8a20732a6ccd9488"
   end
 
   depends_on "rust" => :build
