d246518fb58fd57809c392d815655390ccb2009f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 11:33:08 +0000

    shadowsocks-libev: update 3.3.5_3 bottle.

--- Formula/shadowsocks-libev.rb
+++ Formula/shadowsocks-libev.rb
@@ -8,11 +8,13 @@ class ShadowsocksLibev < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "b1e8d388ea9ad7619ac942468e64697c4ee10f1d969b0ad6d779a7be95c84e08"
-    sha256 cellar: :any,                 big_sur:       "66f3fc332d7acd21599736d33995fe421d95cc89b6d7ce9057bc72a3713ec8d0"
-    sha256 cellar: :any,                 catalina:      "bbb14b97724efb4acf511b122f414a8f3f02810c7ec207413bb413063ef693ee"
-    sha256 cellar: :any,                 mojave:        "292cca782f44f284592f3538dd24dd5ebdb0d1e9a978f9f507cc78be3a8e471a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5bd589c0338a9991d42dd08f5b470e74f3aab6843c09036d13f6b074c73a81ed"
+    sha256 cellar: :any,                 arm64_monterey: "b6199950e5372876827efab88fb67c9c13f8dd7d9e76fef2645bbe86e7153bb6"
+    sha256 cellar: :any,                 arm64_big_sur:  "b1e8d388ea9ad7619ac942468e64697c4ee10f1d969b0ad6d779a7be95c84e08"
+    sha256 cellar: :any,                 monterey:       "139a812882a57f35a1338e875015b5ce57fead64d3cf3174f71c495dcf365183"
+    sha256 cellar: :any,                 big_sur:        "66f3fc332d7acd21599736d33995fe421d95cc89b6d7ce9057bc72a3713ec8d0"
+    sha256 cellar: :any,                 catalina:       "bbb14b97724efb4acf511b122f414a8f3f02810c7ec207413bb413063ef693ee"
+    sha256 cellar: :any,                 mojave:         "292cca782f44f284592f3538dd24dd5ebdb0d1e9a978f9f507cc78be3a8e471a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5bd589c0338a9991d42dd08f5b470e74f3aab6843c09036d13f6b074c73a81ed"
   end
 
   head do
