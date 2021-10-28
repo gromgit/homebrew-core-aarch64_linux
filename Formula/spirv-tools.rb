e79731f7a5c395ee78b2ac38b6451716d2589914
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 03:22:52 +0000

    spirv-tools: update 2021.3 bottle.

--- Formula/spirv-tools.rb
+++ Formula/spirv-tools.rb
@@ -6,11 +6,13 @@ class SpirvTools < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2a6a8dddfea9c6ec0e164d3bc91cd0e07dc2ce64a15f59e6ba2a5f851f34d373"
-    sha256 cellar: :any,                 big_sur:       "2c0679ffd907c528e88fea56a965bdba726327c40c5638080b185f79a0eafcb6"
-    sha256 cellar: :any,                 catalina:      "fe79aa02c8e46822aa429be46b842dc28876602a772a75c4cce8aa4d2bf8c32f"
-    sha256 cellar: :any,                 mojave:        "3075c62d7893812454a63ccf16f88c5db3b26569a33fcfed786c7a0d15c97d80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3f62ac74873e3278aded791b108256da0b90b9e1543ec7e5e580261dec5c16c"
+    sha256 cellar: :any,                 arm64_monterey: "6121a7b2c5304c3c88c9626176da97476c603812e16c75242a49de79ee4ee5d8"
+    sha256 cellar: :any,                 arm64_big_sur:  "2a6a8dddfea9c6ec0e164d3bc91cd0e07dc2ce64a15f59e6ba2a5f851f34d373"
+    sha256 cellar: :any,                 monterey:       "5280741a887af4e753578c35a62dfa0e4e0508d3a4bd1a517b2ab3c77444e3ef"
+    sha256 cellar: :any,                 big_sur:        "2c0679ffd907c528e88fea56a965bdba726327c40c5638080b185f79a0eafcb6"
+    sha256 cellar: :any,                 catalina:       "fe79aa02c8e46822aa429be46b842dc28876602a772a75c4cce8aa4d2bf8c32f"
+    sha256 cellar: :any,                 mojave:         "3075c62d7893812454a63ccf16f88c5db3b26569a33fcfed786c7a0d15c97d80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c3f62ac74873e3278aded791b108256da0b90b9e1543ec7e5e580261dec5c16c"
   end
 
   depends_on "cmake" => :build
