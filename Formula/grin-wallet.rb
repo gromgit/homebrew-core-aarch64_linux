183dbfef860c668fe7b1d6aad0122a19de91e726
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 10:28:17 +0000

    grin-wallet: update 5.0.3 bottle.

--- Formula/grin-wallet.rb
+++ Formula/grin-wallet.rb
@@ -6,6 +6,7 @@ class GrinWallet < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:     "1720cdcdb2a585aa588e3d20eafe0bde5924e844b14bc59fdbddeccdc8f42ec8"
     sha256 cellar: :any_skip_relocation, big_sur:      "a0dfda70b5c6cf1ed883e5af6b61c01aea7ddf18f6f22038aab71e06b5b9801a"
     sha256 cellar: :any_skip_relocation, catalina:     "4b43f745f1b82d9390cdaf4055c9d65e6bea850fe9ad7ebd2818789c33c45dbc"
     sha256 cellar: :any_skip_relocation, mojave:       "3ce2c42866c3d02c527fac13c2393af01eabb9c9591ef25d5503aca073863f50"
