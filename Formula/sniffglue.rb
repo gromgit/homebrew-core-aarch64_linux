404ce7f2666593c7378ca8a105554070be9acb10
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 13:00:16 +0000

    sniffglue: update 0.14.0 bottle.

--- Formula/sniffglue.rb
+++ Formula/sniffglue.rb
@@ -6,10 +6,12 @@ class Sniffglue < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b9dff099e2cbd6d5314cde13f133331d590f42d40743ba69981c269957e5e95e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "572f8ca8ce788d8b12b6585ff01ea28c7c88f919b98d9aada7497afe20c9ce58"
-    sha256 cellar: :any_skip_relocation, catalina:      "ff12c1cf459118bac72b2dc0e27a46acdaeaaa2177d9c87ffb0b4950d6a9c5b8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a72e4880dc7c5a4017b7c5b05167a88b39ac24d08b286ce6755b92f851a0bcc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "71792cbed38923a976cc8efea8dd687cda67d6f784f4ef3c593c7a36cc733fa2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b9dff099e2cbd6d5314cde13f133331d590f42d40743ba69981c269957e5e95e"
+    sha256 cellar: :any_skip_relocation, monterey:       "739d4c1901fee3972adb91af6876e24f926a9cd6e5700a5034869c2c4e0d3f6d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "572f8ca8ce788d8b12b6585ff01ea28c7c88f919b98d9aada7497afe20c9ce58"
+    sha256 cellar: :any_skip_relocation, catalina:       "ff12c1cf459118bac72b2dc0e27a46acdaeaaa2177d9c87ffb0b4950d6a9c5b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a72e4880dc7c5a4017b7c5b05167a88b39ac24d08b286ce6755b92f851a0bcc"
   end
 
   depends_on "rust" => :build
