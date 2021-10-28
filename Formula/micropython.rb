3447319c74199a023e168befb0a11a3c9ca4b77d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 04:15:18 +0000

    micropython: update 1.17_2 bottle.

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -8,11 +8,13 @@ class Micropython < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "677377aa0c47310b587970b0daff9ba29b987ebd4b8472ab5285d8e8cb15e7ff"
-    sha256 cellar: :any,                 big_sur:       "395c3694341235f8e3eeae947a9bd2692bbebfb7043d4b07f52d3dd211e1da17"
-    sha256 cellar: :any,                 catalina:      "ff82df0fd5a96052d8bfc7a0f114fb1f454c742ce6b4aedaebcba62cc24d97a6"
-    sha256 cellar: :any,                 mojave:        "8dc695ae73f1c6b8d508ae63af06def4c706d14ca9e007c5b3fce787c7f17ee3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8327f00b5e66af095f7299ae1fa34e9679dc9ae32426b052efa99e080d5db490"
+    sha256 cellar: :any,                 arm64_monterey: "958c9afb00970ba7480f7463ea9632ea2d8ea0980f2abb431371299ec36cdd83"
+    sha256 cellar: :any,                 arm64_big_sur:  "677377aa0c47310b587970b0daff9ba29b987ebd4b8472ab5285d8e8cb15e7ff"
+    sha256 cellar: :any,                 monterey:       "17ff3dd17107b974807eaa8ad14194063fb2069a32038a15a23e1a9cec7c77d6"
+    sha256 cellar: :any,                 big_sur:        "395c3694341235f8e3eeae947a9bd2692bbebfb7043d4b07f52d3dd211e1da17"
+    sha256 cellar: :any,                 catalina:       "ff82df0fd5a96052d8bfc7a0f114fb1f454c742ce6b4aedaebcba62cc24d97a6"
+    sha256 cellar: :any,                 mojave:         "8dc695ae73f1c6b8d508ae63af06def4c706d14ca9e007c5b3fce787c7f17ee3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8327f00b5e66af095f7299ae1fa34e9679dc9ae32426b052efa99e080d5db490"
   end
 
   depends_on "pkg-config" => :build
