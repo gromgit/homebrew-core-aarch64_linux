2982cc52bd881457a62c4da9527b75e932ddd68a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 06:02:14 +0000

    rav1e: update 0.4.1_1 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -15,6 +15,7 @@ class Rav1e < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "37f51962f8a33db3ceb63e808cdc64ef8c88e6fba4ee5bcbdcf9c09bd3019383"
     sha256 cellar: :any,                 arm64_big_sur:  "80c76875e03241980641acdfa1c25c87e437e7950424fce2304841ef9bd5957b"
+    sha256 cellar: :any,                 monterey:       "5859c2a9119b3d7ecfcd9622fc4ff5cddc16847cd6e1351404b960b600472137"
     sha256 cellar: :any,                 big_sur:        "45b2b8adc65f38c42d453665f302385df54b251846355d002b753365a0701a3e"
     sha256 cellar: :any,                 catalina:       "e08ce645cc56ed8258cc84c6aedaa7e78ce02ce4fed0ad7032845a2c0052aed0"
     sha256 cellar: :any,                 mojave:         "8ed66c0b3180c82355a8d9201ace97ac281b8dab4bc152ad9e58a855b01429ba"
