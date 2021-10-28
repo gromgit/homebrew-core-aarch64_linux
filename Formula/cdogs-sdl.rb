cba3bc613feb997d4ebc85daf87b946338bb8ad9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-28 01:16:07 +0000

    cdogs-sdl: update 1.1.1 bottle.

--- Formula/cdogs-sdl.rb
+++ Formula/cdogs-sdl.rb
@@ -12,11 +12,13 @@ class CdogsSdl < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "9db72aa1904869a7bb28afc1c550ce26939efa6981f783dfbc5ff0ff60ae7920"
-    sha256 big_sur:       "47ac08b78f35ae9ec4ba966f16c824fa42163e1020eb3ec2d144ac9fca4e057c"
-    sha256 catalina:      "10d48265e4705b1bda33270ff02beed252093aef168e1ce85ecd3e95d145ef3b"
-    sha256 mojave:        "4973b9b193c93618a7e7cafd8223ecd9bda40c62e0ff0438497a829469e4557d"
-    sha256 x86_64_linux:  "dc3a2562c440f36b419192cce13c3ca42e3397777d414286e230793360b7d554"
+    sha256 arm64_monterey: "1eeb228fb876c92c3809c3b1c4c3818b8ecb4917aa779a807eaf5b8a41763811"
+    sha256 arm64_big_sur:  "9db72aa1904869a7bb28afc1c550ce26939efa6981f783dfbc5ff0ff60ae7920"
+    sha256 monterey:       "e48b864e67aed9b18433a983f4aef44b374b61276c1421e7763ebebab9dbf12e"
+    sha256 big_sur:        "47ac08b78f35ae9ec4ba966f16c824fa42163e1020eb3ec2d144ac9fca4e057c"
+    sha256 catalina:       "10d48265e4705b1bda33270ff02beed252093aef168e1ce85ecd3e95d145ef3b"
+    sha256 mojave:         "4973b9b193c93618a7e7cafd8223ecd9bda40c62e0ff0438497a829469e4557d"
+    sha256 x86_64_linux:   "dc3a2562c440f36b419192cce13c3ca42e3397777d414286e230793360b7d554"
   end
 
   depends_on "cmake" => :build
