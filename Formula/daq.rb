bfd8f54f637d81aa9cd3182ff456cc7ab04a6a2b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:33:27 +0000

    daq: update 3.0.5 bottle.

--- Formula/daq.rb
+++ Formula/daq.rb
@@ -8,11 +8,13 @@ class Daq < Formula
   head "https://github.com/snort3/libdaq.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a381d5d506e129d22823f0993521b8f83f1bbba444d0eff6e42898830275959d"
-    sha256 cellar: :any,                 big_sur:       "d9ce6e9dbbcdac7b64575ec19ede1197a23451fbb8c3da5aabfce2c96d14820d"
-    sha256 cellar: :any,                 catalina:      "5207b98bd7d7e2954a5600409bbd862c72e1f246363f1e81359fa9a3530fba22"
-    sha256 cellar: :any,                 mojave:        "4f9c630d968eef80ef96102a1f12ea0ed12f5e1c74bb1dfb484687cd8a3a2aa0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "15d0fd96bdd3973960ee0a6781bb7576db9b452762beb57029f7c001e096944b"
+    sha256 cellar: :any,                 arm64_monterey: "0d3e31e4c3f477da99af902e3d94041e525161ecb62ebf7035a6f0849df15385"
+    sha256 cellar: :any,                 arm64_big_sur:  "a381d5d506e129d22823f0993521b8f83f1bbba444d0eff6e42898830275959d"
+    sha256 cellar: :any,                 monterey:       "9bfe0e35170d0e414e2b71fca7bdd39620a91d3ffc2f7a108ee3d3f7e853bb45"
+    sha256 cellar: :any,                 big_sur:        "d9ce6e9dbbcdac7b64575ec19ede1197a23451fbb8c3da5aabfce2c96d14820d"
+    sha256 cellar: :any,                 catalina:       "5207b98bd7d7e2954a5600409bbd862c72e1f246363f1e81359fa9a3530fba22"
+    sha256 cellar: :any,                 mojave:         "4f9c630d968eef80ef96102a1f12ea0ed12f5e1c74bb1dfb484687cd8a3a2aa0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15d0fd96bdd3973960ee0a6781bb7576db9b452762beb57029f7c001e096944b"
   end
 
   depends_on "autoconf" => :build
