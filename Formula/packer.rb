cfeb8342f17e9823858f586bd5a1501dbf6d4cf2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 04:18:35 +0000

    packer: update 1.7.8 bottle.

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -12,10 +12,12 @@ class Packer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c2a2ab4fb8e90aee4a29834194a3ebcf54ccc2444f80be96d13f98c622670f85"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ec225731830c3e3bdb3e2ec0a4a9d53934e6af1f7b90790f028f1d249ee79f7a"
-    sha256 cellar: :any_skip_relocation, catalina:      "063ae76c6a7d9eb1bbc55dea0066eac438230d30201a6947b4274b4e7eea7a55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f283572207e1e7cc001b93a0849dafc1154f29772b2087b68ba847d0330ddcd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "142c8b5b4808d2350b01b3116345e96e347e492a05b6682d165c09c26c2b7b84"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c2a2ab4fb8e90aee4a29834194a3ebcf54ccc2444f80be96d13f98c622670f85"
+    sha256 cellar: :any_skip_relocation, monterey:       "36b9e8754b3c79d8a4746cdc545ec2eaf9f699779c96b12a49326c4d9a55567d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ec225731830c3e3bdb3e2ec0a4a9d53934e6af1f7b90790f028f1d249ee79f7a"
+    sha256 cellar: :any_skip_relocation, catalina:       "063ae76c6a7d9eb1bbc55dea0066eac438230d30201a6947b4274b4e7eea7a55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f283572207e1e7cc001b93a0849dafc1154f29772b2087b68ba847d0330ddcd"
   end
 
   depends_on "go" => :build
