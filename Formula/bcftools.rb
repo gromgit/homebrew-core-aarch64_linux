fc1f5c54b5844bd31cbe2fbd28291c4d2ac293b7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:46:28 +0000

    bcftools: update 1.14 bottle.

--- Formula/bcftools.rb
+++ Formula/bcftools.rb
@@ -13,10 +13,12 @@ class Bcftools < Formula
   end
 
   bottle do
-    sha256                               arm64_big_sur: "13bc31d2086972697f374bcf68a24deaa389a57ed2adb4a3d432fd72a60ffcda"
-    sha256                               big_sur:       "e4cd74edeaa7c41ae71263732822b732b8d1e7cde98ce813dcd1fc1d2bf0529e"
-    sha256                               catalina:      "ddf5d0fe3d61c1386a383469a3bc7bb6bacc0ae11e6eaa3ef21ca3d38968e6a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8db6b53f0fe4b88ec4d1e41297a1487429b937e0579c0ba8ee2c67a5729216c3"
+    sha256                               arm64_monterey: "744a97a27e3bad5c82d483d388fb49af8c13b67c3186da29c57da2fb5fda0c41"
+    sha256                               arm64_big_sur:  "13bc31d2086972697f374bcf68a24deaa389a57ed2adb4a3d432fd72a60ffcda"
+    sha256                               monterey:       "0f9241650659b12f4196cc1401714cee3688f6e72021c654c93f43eff066de1d"
+    sha256                               big_sur:        "e4cd74edeaa7c41ae71263732822b732b8d1e7cde98ce813dcd1fc1d2bf0529e"
+    sha256                               catalina:       "ddf5d0fe3d61c1386a383469a3bc7bb6bacc0ae11e6eaa3ef21ca3d38968e6a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8db6b53f0fe4b88ec4d1e41297a1487429b937e0579c0ba8ee2c67a5729216c3"
   end
 
   depends_on "gsl"
