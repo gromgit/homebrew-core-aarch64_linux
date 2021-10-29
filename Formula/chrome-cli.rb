526bb3b7a7d0e21560417b2f657038301ae2aecf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 11:10:42 +0000

    chrome-cli: update 1.7.1 bottle.

--- Formula/chrome-cli.rb
+++ Formula/chrome-cli.rb
@@ -7,10 +7,12 @@ class ChromeCli < Formula
   head "https://github.com/prasmussen/chrome-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "835b7a9995ff61f7623ded6ce772d5ae712651aa690102c79afa0f989a6f5285"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2ce2275e0b6f7d189ee64884027807b6dd9305b89e4c382dcf6da4425bea2482"
-    sha256 cellar: :any_skip_relocation, catalina:      "8f263e2bc13457f68605d83e6aaf0dd26eda374e8342c955d97c497bd6b53a4b"
-    sha256 cellar: :any_skip_relocation, mojave:        "0ab4cf9ab507b5403c98493840d126a3d0062d9a19f982c8ee58b0d3a73748b3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2811e816a8994328fe3f6ea28935656ba0ce87de8e5a5e38a2a0c209d78fc279"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "835b7a9995ff61f7623ded6ce772d5ae712651aa690102c79afa0f989a6f5285"
+    sha256 cellar: :any_skip_relocation, monterey:       "04c69afc601992c0415706c92b0a07774be82b4f5f34a4d2a9d6a4c405de78c7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2ce2275e0b6f7d189ee64884027807b6dd9305b89e4c382dcf6da4425bea2482"
+    sha256 cellar: :any_skip_relocation, catalina:       "8f263e2bc13457f68605d83e6aaf0dd26eda374e8342c955d97c497bd6b53a4b"
+    sha256 cellar: :any_skip_relocation, mojave:         "0ab4cf9ab507b5403c98493840d126a3d0062d9a19f982c8ee58b0d3a73748b3"
   end
 
   depends_on xcode: :build
