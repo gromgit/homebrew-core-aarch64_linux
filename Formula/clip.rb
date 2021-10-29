c1b713fe6c69bff839332838862e090eecc352b7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 18:00:07 +0000

    clip: update 0.7_1 bottle.

--- Formula/clip.rb
+++ Formula/clip.rb
@@ -7,11 +7,13 @@ class Clip < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "20bb528940048dacd9f0eb67b3a1fe849e9e54d1c7e9a7c58da1fc9814beb1c8"
-    sha256 cellar: :any,                 big_sur:       "cb33d312ff97b10b5761ceb105e3a5d63ab11c48db072c199108167cecf47854"
-    sha256 cellar: :any,                 catalina:      "c6e0fc40838d76e9632e21e1a781603a1a75d584a3206300fe0a9f7272d9b702"
-    sha256 cellar: :any,                 mojave:        "72f17c87e0c824cea89ba5fa383cf22d013751997a916a408eaa8408544292fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "adbd6fc3d72a07a847e887fa050e91598e3045fa9d9dc251cec0953a6f6626d5"
+    sha256 cellar: :any,                 arm64_monterey: "02d8da157f889f384abb2900d10ead88cc6bfaba984165e78a92f6fbce100f6e"
+    sha256 cellar: :any,                 arm64_big_sur:  "20bb528940048dacd9f0eb67b3a1fe849e9e54d1c7e9a7c58da1fc9814beb1c8"
+    sha256 cellar: :any,                 monterey:       "14853fce6b1e8ccd447df646d60b9277a23e72279dd74c40677b823451801995"
+    sha256 cellar: :any,                 big_sur:        "cb33d312ff97b10b5761ceb105e3a5d63ab11c48db072c199108167cecf47854"
+    sha256 cellar: :any,                 catalina:       "c6e0fc40838d76e9632e21e1a781603a1a75d584a3206300fe0a9f7272d9b702"
+    sha256 cellar: :any,                 mojave:         "72f17c87e0c824cea89ba5fa383cf22d013751997a916a408eaa8408544292fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "adbd6fc3d72a07a847e887fa050e91598e3045fa9d9dc251cec0953a6f6626d5"
   end
 
   depends_on "cmake" => :build
