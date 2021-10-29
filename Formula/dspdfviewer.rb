46077947b61b07f254af57a74685252944749c51
miccal <miccal.matthews@gmail.com>
2021-10-29 11:40:02 +0800

    dspdfviewer: revision bump
    Closes #88217.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dspdfviewer.rb
+++ Formula/dspdfviewer.rb
@@ -4,7 +4,7 @@ class Dspdfviewer < Formula
   url "https://github.com/dannyedel/dspdfviewer/archive/v1.15.1.tar.gz"
   sha256 "c5b6f8c93d732e65a27810286d49a4b1c6f777d725e26a207b14f6b792307b03"
   license "GPL-2.0-or-later"
-  revision 10
+  revision 11
   head "https://github.com/dannyedel/dspdfviewer.git"
 
   bottle do
