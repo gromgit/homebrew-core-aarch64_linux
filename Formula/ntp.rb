2f19c04f2335ead4aea31fe84b525face7bc0099
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 07:19:17 +0000

    ntp: update 4.2.8p15 bottle.

--- Formula/ntp.rb
+++ Formula/ntp.rb
@@ -11,11 +11,13 @@ class Ntp < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "5aaceeca360143de4591c0294ed75755f50c14cfcdb908b7c1622fa5caa22cf4"
-    sha256 cellar: :any, big_sur:       "cee6250b029cdb17a3e7c8f68ec6bf16a3a3751bea52a758bca885932e5a0de4"
-    sha256 cellar: :any, catalina:      "3c6a8893b0e76b8af1a4fd19ab664279b5409c1129062bf1feee4643318236b3"
-    sha256 cellar: :any, mojave:        "b69ded37b2c8304157c3f46e4484af9099b4fd1e077929c35bb630903d059856"
-    sha256 cellar: :any, high_sierra:   "9f7ce9c3ff545ff738fcf4049445923c968ec807cf1ecde451be76412442e6f1"
+    sha256 cellar: :any, arm64_monterey: "56e2b28056f17d7650a305bfae1725adcff788bdfe53abde908c163fcc02ddd4"
+    sha256 cellar: :any, arm64_big_sur:  "5aaceeca360143de4591c0294ed75755f50c14cfcdb908b7c1622fa5caa22cf4"
+    sha256 cellar: :any, monterey:       "dc79526c62d10033e35cc0d16bb2f138c05577dd8f5de7289836a6e31f75d6a3"
+    sha256 cellar: :any, big_sur:        "cee6250b029cdb17a3e7c8f68ec6bf16a3a3751bea52a758bca885932e5a0de4"
+    sha256 cellar: :any, catalina:       "3c6a8893b0e76b8af1a4fd19ab664279b5409c1129062bf1feee4643318236b3"
+    sha256 cellar: :any, mojave:         "b69ded37b2c8304157c3f46e4484af9099b4fd1e077929c35bb630903d059856"
+    sha256 cellar: :any, high_sierra:    "9f7ce9c3ff545ff738fcf4049445923c968ec807cf1ecde451be76412442e6f1"
   end
 
   depends_on "openssl@1.1"
