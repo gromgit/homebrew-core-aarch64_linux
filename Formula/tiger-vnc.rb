0d8d080e5afdb4ea728d5400d29ba50df36774dd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:31:32 +0000

    tiger-vnc: update 1.11.0 bottle.

--- Formula/tiger-vnc.rb
+++ Formula/tiger-vnc.rb
@@ -6,12 +6,14 @@ class TigerVnc < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "d4e19469518f0167a6abf40625c3c21df88bff1b2ca19bb6feac1c4a5a077cd2"
-    sha256 big_sur:       "ae46d4c867f0f761368e22c80daa9e0805c15f7fc5855bf32d37575c36168367"
-    sha256 catalina:      "b9a09483c45610c81dd29fc20a41b4fa8120e1353f736bb637732d4788e4bb28"
-    sha256 mojave:        "c90bdf1ac012129c5d4caecd3e5acf2d110ca8cd68a8bcff6de07373149424db"
-    sha256 high_sierra:   "2370d829c67ca1df886e47aca162c68034e138a10a93b846c31f1c927d84c435"
-    sha256 x86_64_linux:  "a1aa65f57be2296d34ddf09f7f0e84d0357b1c9391296bcecdd2e28ff3fda483"
+    sha256 arm64_monterey: "1746ae1a34004bdf64450a397efdf0afa5ea3a5248bef4ed42d11320c5301a3c"
+    sha256 arm64_big_sur:  "d4e19469518f0167a6abf40625c3c21df88bff1b2ca19bb6feac1c4a5a077cd2"
+    sha256 monterey:       "dd45f81d38c8dd9eb9249f10df1a89b2d14a3df35e0dc394e5fb1132222db964"
+    sha256 big_sur:        "ae46d4c867f0f761368e22c80daa9e0805c15f7fc5855bf32d37575c36168367"
+    sha256 catalina:       "b9a09483c45610c81dd29fc20a41b4fa8120e1353f736bb637732d4788e4bb28"
+    sha256 mojave:         "c90bdf1ac012129c5d4caecd3e5acf2d110ca8cd68a8bcff6de07373149424db"
+    sha256 high_sierra:    "2370d829c67ca1df886e47aca162c68034e138a10a93b846c31f1c927d84c435"
+    sha256 x86_64_linux:   "a1aa65f57be2296d34ddf09f7f0e84d0357b1c9391296bcecdd2e28ff3fda483"
   end
 
   depends_on "cmake" => :build
