06d3c92603a40c9199bf26e0793abdc3ff0c67da
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-29 02:30:18 +0000

    wdc: update 1.1.5 bottle.

--- Formula/wdc.rb
+++ Formula/wdc.rb
@@ -5,12 +5,14 @@ class Wdc < Formula
   sha256 "3c45341521da9c68328c5fa8909d838915e8a768e7652ff1bcc2fbbd46ab9f64"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "be11e8b817c20749111b09db86ff335c6c97bad37575f715e9a7e7ec7ca0335a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c0d2e5d13ef2dca786d050eb726cdd240a8c7da8f56868f2a85aae67ee99ee8c"
-    sha256 cellar: :any_skip_relocation, catalina:      "18365f76dafd05a312e9a7862f2fa747caa8c63e881469719a8ef45d07dce3c6"
-    sha256 cellar: :any_skip_relocation, mojave:        "fbcaccbaa2440ac38f9efa41a342eef4d883e522fa5df7d642aaa1563d38f28b"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "92dcb68d02f64ff51446052bf5c41fa178cc48ade406a9533199461476f7c849"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "540ee4f07372f9080c4cf88d17a067379fbef4af4f69240cfced99f4944c07df"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "62495852d3c1071bf9d1f3121e330edae3fdbc9e9c63fb65e766be3f38c2a885"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "be11e8b817c20749111b09db86ff335c6c97bad37575f715e9a7e7ec7ca0335a"
+    sha256 cellar: :any_skip_relocation, monterey:       "c039308564cb3c12b33cd01344b939187d0172426fc8d3beb277bf4a55ce8843"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c0d2e5d13ef2dca786d050eb726cdd240a8c7da8f56868f2a85aae67ee99ee8c"
+    sha256 cellar: :any_skip_relocation, catalina:       "18365f76dafd05a312e9a7862f2fa747caa8c63e881469719a8ef45d07dce3c6"
+    sha256 cellar: :any_skip_relocation, mojave:         "fbcaccbaa2440ac38f9efa41a342eef4d883e522fa5df7d642aaa1563d38f28b"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "92dcb68d02f64ff51446052bf5c41fa178cc48ade406a9533199461476f7c849"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "540ee4f07372f9080c4cf88d17a067379fbef4af4f69240cfced99f4944c07df"
   end
 
   depends_on "cmake" => :build
