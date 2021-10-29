019524bee85f57e9e8b9af1eb7cbf17abb67141c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 22:54:05 +0000

    crispy-doom: update 5.10.3 bottle.

--- Formula/crispy-doom.rb
+++ Formula/crispy-doom.rb
@@ -7,11 +7,13 @@ class CrispyDoom < Formula
   head "https://github.com/fabiangreffrath/crispy-doom.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "6d9000bd5ee7e800eecf8748c419ea4f8d42dc1e5c78ec5e49e91ab095e34895"
-    sha256 cellar: :any,                 big_sur:       "cbd514df66ecb0eb170169ce1f099fedefafa17f79cdeb2dfe00120d7e2fa03a"
-    sha256 cellar: :any,                 catalina:      "07ab97bfe589f66a63007f32ab6f5a937aec221fb3528ab33259fc3c6652bb60"
-    sha256 cellar: :any,                 mojave:        "ae4f7e635129aa417d47826af76123678ae786c0889b4baad08c59b3c730c36a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ef1289f3ed451a8f7c301f9e56446139d349e9664f4be42c0858d082e148f935"
+    sha256 cellar: :any,                 arm64_monterey: "06236817abc6d69fec168a719edafd7196908f1610359228bf74894fae7b86f7"
+    sha256 cellar: :any,                 arm64_big_sur:  "6d9000bd5ee7e800eecf8748c419ea4f8d42dc1e5c78ec5e49e91ab095e34895"
+    sha256 cellar: :any,                 monterey:       "ae6437bd7678cdfb5a4ac4f318126d1f089f9a74105efa267b008a2451bc7b5b"
+    sha256 cellar: :any,                 big_sur:        "cbd514df66ecb0eb170169ce1f099fedefafa17f79cdeb2dfe00120d7e2fa03a"
+    sha256 cellar: :any,                 catalina:       "07ab97bfe589f66a63007f32ab6f5a937aec221fb3528ab33259fc3c6652bb60"
+    sha256 cellar: :any,                 mojave:         "ae4f7e635129aa417d47826af76123678ae786c0889b4baad08c59b3c730c36a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ef1289f3ed451a8f7c301f9e56446139d349e9664f4be42c0858d082e148f935"
   end
 
   depends_on "autoconf" => :build
