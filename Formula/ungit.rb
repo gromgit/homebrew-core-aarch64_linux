3e6d5a85807b4eff303d54f7208e8d39671c9f9c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 23:33:23 +0000

    ungit: update 1.5.18 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -8,11 +8,13 @@ class Ungit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "54ab4e9703e79db7145005c5f4e8a2b09001bc6a6a4c0ca0e074d20cb71b252a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
-    sha256 cellar: :any_skip_relocation, catalina:      "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
-    sha256 cellar: :any_skip_relocation, mojave:        "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5fed1c88fef55106397629b390d0c369cb1b7f179922b9b938e9e5b9a02f0698"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "26ee350bc93b45aca15ea4ddfecad513b4fe07f2ca143ba215241387572a5afb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "54ab4e9703e79db7145005c5f4e8a2b09001bc6a6a4c0ca0e074d20cb71b252a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4fa377f0d9fc242605e26308f4ddd90b317703bad6d48bd25f1175de5d1ee7ae"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
+    sha256 cellar: :any_skip_relocation, catalina:       "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
+    sha256 cellar: :any_skip_relocation, mojave:         "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fed1c88fef55106397629b390d0c369cb1b7f179922b9b938e9e5b9a02f0698"
   end
 
   depends_on "node"
