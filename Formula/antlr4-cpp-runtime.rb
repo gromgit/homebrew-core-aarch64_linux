fb99c7469b1f00c3334ca254b50403db2d7aa6a1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 08:56:13 +0000

    antlr4-cpp-runtime: update 4.9.2 bottle.

--- Formula/antlr4-cpp-runtime.rb
+++ Formula/antlr4-cpp-runtime.rb
@@ -11,11 +11,13 @@ class Antlr4CppRuntime < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2675f194768a527b27fb7423a2cd13d449d7102f87a84648dc3dee4c9a5a2ef1"
-    sha256 cellar: :any,                 big_sur:       "1a0ecd0f7f72c1ec539b5e827d4249d163a648678fe236697a78a4acb05e3766"
-    sha256 cellar: :any,                 catalina:      "8d4d96b21b91529016470d651680f6c90f02854e7b0fa1569570c9c830da0c6b"
-    sha256 cellar: :any,                 mojave:        "e9c6ac2f0d41c5e4c69894e6c4fdfb079693eaa0f297a013a66487339439c164"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3c5b79c485d2af18036f370291f4ca452f398ad85556ff1260d91b5eadaa0a8"
+    sha256 cellar: :any,                 arm64_monterey: "14b51ea90839f289030f16719ae4284045c85d9e8a485bd69a52658dfd37c484"
+    sha256 cellar: :any,                 arm64_big_sur:  "2675f194768a527b27fb7423a2cd13d449d7102f87a84648dc3dee4c9a5a2ef1"
+    sha256 cellar: :any,                 monterey:       "41aad9541b5af277d37bf285d4b345f3a82721afd0eeeec8539cc0ae203426bd"
+    sha256 cellar: :any,                 big_sur:        "1a0ecd0f7f72c1ec539b5e827d4249d163a648678fe236697a78a4acb05e3766"
+    sha256 cellar: :any,                 catalina:       "8d4d96b21b91529016470d651680f6c90f02854e7b0fa1569570c9c830da0c6b"
+    sha256 cellar: :any,                 mojave:         "e9c6ac2f0d41c5e4c69894e6c4fdfb079693eaa0f297a013a66487339439c164"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f3c5b79c485d2af18036f370291f4ca452f398ad85556ff1260d91b5eadaa0a8"
   end
 
   depends_on "cmake" => :build
