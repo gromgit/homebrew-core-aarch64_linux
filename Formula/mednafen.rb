c7df07a658c1e013396db4505222bb11fa58190a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 23:14:03 +0000

    mednafen: update 1.27.1 bottle.

--- Formula/mednafen.rb
+++ Formula/mednafen.rb
@@ -11,11 +11,13 @@ class Mednafen < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "89eb1006849d1d949b425d2937a7ca6e00c703a1edae563075dba88ccc817a0c"
-    sha256 big_sur:       "5d671db565de9ce937475c19880caf88d38faa2b2b8a42888230a0be27f32615"
-    sha256 catalina:      "beda51be33761b5b9e9764093e313b567d1b1bcd58aab91a64d3f7a4099d2c93"
-    sha256 mojave:        "62500c988c009c14e45f80de2f69d3b9a352946a36888adfe94b4eda14e6fc9f"
-    sha256 x86_64_linux:  "9385785347f0e28b221bc0d8dc0a6afbcab5eeda7664bbfc749cbafc8a8d75b6"
+    sha256 arm64_monterey: "bb336129af921db2f951e911115094e20b8296051633cc80cd70e8fafbede993"
+    sha256 arm64_big_sur:  "89eb1006849d1d949b425d2937a7ca6e00c703a1edae563075dba88ccc817a0c"
+    sha256 monterey:       "e79b486c330c248c8e537aa3dfea231b5087de4f75d3db391cce82e144c7cb08"
+    sha256 big_sur:        "5d671db565de9ce937475c19880caf88d38faa2b2b8a42888230a0be27f32615"
+    sha256 catalina:       "beda51be33761b5b9e9764093e313b567d1b1bcd58aab91a64d3f7a4099d2c93"
+    sha256 mojave:         "62500c988c009c14e45f80de2f69d3b9a352946a36888adfe94b4eda14e6fc9f"
+    sha256 x86_64_linux:   "9385785347f0e28b221bc0d8dc0a6afbcab5eeda7664bbfc749cbafc8a8d75b6"
   end
 
   depends_on "pkg-config" => :build
