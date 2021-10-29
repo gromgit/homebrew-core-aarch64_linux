be2b57fd3b7567f1d00dfb83bccd19d1628e12d0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 08:53:24 +0000

    getdns: update 1.7.0_1 bottle.

--- Formula/getdns.rb
+++ Formula/getdns.rb
@@ -16,11 +16,13 @@ class Getdns < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "6418fbe20eac5674ac1492eccba8941c57ab6111fca7a174144a0f7732c037c1"
-    sha256 cellar: :any,                 big_sur:       "d98dc03e72b3e2e6814f47c8d947ff7667d65b79cf4b11f3b1913d2e17f51e84"
-    sha256 cellar: :any,                 catalina:      "3481bfeb92cb2b50ad019c3cc816fe73c3f26dec20e80b4b52536e83c8ff9752"
-    sha256 cellar: :any,                 mojave:        "fabcba2c64119aa84e892266a93260d6c8a6d650e754568b6f5767c9223de001"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f445d5e97136b0171dac28b641e30d24461300978697259ae9272313695765c"
+    sha256 cellar: :any,                 arm64_monterey: "10850358ef91882e727c103bfabbd4fa6c096ddbb02f6708f1cc91fc46ae16d2"
+    sha256 cellar: :any,                 arm64_big_sur:  "6418fbe20eac5674ac1492eccba8941c57ab6111fca7a174144a0f7732c037c1"
+    sha256 cellar: :any,                 monterey:       "9a8a08d251a2a8284b4535d6afef601b8a91162b0005eff783d62b726579160f"
+    sha256 cellar: :any,                 big_sur:        "d98dc03e72b3e2e6814f47c8d947ff7667d65b79cf4b11f3b1913d2e17f51e84"
+    sha256 cellar: :any,                 catalina:       "3481bfeb92cb2b50ad019c3cc816fe73c3f26dec20e80b4b52536e83c8ff9752"
+    sha256 cellar: :any,                 mojave:         "fabcba2c64119aa84e892266a93260d6c8a6d650e754568b6f5767c9223de001"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f445d5e97136b0171dac28b641e30d24461300978697259ae9272313695765c"
   end
 
   depends_on "cmake" => :build
