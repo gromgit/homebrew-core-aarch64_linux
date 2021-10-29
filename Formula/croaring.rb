9239a5af03a9883025d4eac9086a6efc39bdc38b
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2021-10-29 04:37:19 +0000

    croaring: update 0.4.0 bottle.

--- Formula/croaring.rb
+++ Formula/croaring.rb
@@ -7,11 +7,13 @@ class Croaring < Formula
   head "https://github.com/RoaringBitmap/CRoaring.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7e7a0633d5789216b47ed5236431520ae37720cad3818cb96b0a050239e54656"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bc4cd03c1f0849065c0ff6e8a55a14fba01fafa1200338945f74f5273e8c05c2"
-    sha256 cellar: :any_skip_relocation, catalina:      "0f52b826c8c951829273c69a792007780363c9e0ff874f5e8693e0108786ea94"
-    sha256 cellar: :any_skip_relocation, mojave:        "9d81e20356089da75f20d83ff482999f48840a4899bb239de24142fb9e1e1f98"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b93bab19f46526216e3b1d0cf27bde0b9f10811b1de22c17b4bf56df12456ccc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6148b7ee48a3a5723524e4f918d7da0205df5bfd4025bafe92ecb0a8e78f3e09"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7e7a0633d5789216b47ed5236431520ae37720cad3818cb96b0a050239e54656"
+    sha256 cellar: :any_skip_relocation, monterey:       "7276b52c9cc62874b1319bf0027205ff853c2884ed80956557877a3bbc2464e9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bc4cd03c1f0849065c0ff6e8a55a14fba01fafa1200338945f74f5273e8c05c2"
+    sha256 cellar: :any_skip_relocation, catalina:       "0f52b826c8c951829273c69a792007780363c9e0ff874f5e8693e0108786ea94"
+    sha256 cellar: :any_skip_relocation, mojave:         "9d81e20356089da75f20d83ff482999f48840a4899bb239de24142fb9e1e1f98"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b93bab19f46526216e3b1d0cf27bde0b9f10811b1de22c17b4bf56df12456ccc"
   end
 
   depends_on "cmake" => :build
