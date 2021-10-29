a7e04dda08fd4d4ebba4b5b843ece9d6b8fcb34b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 10:29:50 +0000

    taskwarrior-tui: update 0.14.0 bottle.

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -12,10 +12,12 @@ class TaskwarriorTui < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "261ee2f297a434545e0c7c6acb5d0d5f7ecc8746fde0c3fa7a13832497efcc32"
-    sha256 cellar: :any_skip_relocation, big_sur:       "391eb75971364f11decae54a95ab3081d38581af7f01b19aa19401abcb1b5375"
-    sha256 cellar: :any_skip_relocation, catalina:      "bfb8d93a153c2350dfec908944bc69590ab186480d9f637398775e6f2191fcb5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4e31d4a9e8afb257ba24ab651513d107688476cbd4362ebd65130b088f3b6bd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0de2d46721ea72f15302149c1b1825b13701e83283490e0cadcc2e9554117449"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "261ee2f297a434545e0c7c6acb5d0d5f7ecc8746fde0c3fa7a13832497efcc32"
+    sha256 cellar: :any_skip_relocation, monterey:       "4a5f0b5c1b54b15739fbdc2c4e904e0f38ce56aec68a24feae3a5e4a9fdbe432"
+    sha256 cellar: :any_skip_relocation, big_sur:        "391eb75971364f11decae54a95ab3081d38581af7f01b19aa19401abcb1b5375"
+    sha256 cellar: :any_skip_relocation, catalina:       "bfb8d93a153c2350dfec908944bc69590ab186480d9f637398775e6f2191fcb5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4e31d4a9e8afb257ba24ab651513d107688476cbd4362ebd65130b088f3b6bd"
   end
 
   depends_on "pandoc" => :build
