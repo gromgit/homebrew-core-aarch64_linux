3c95406ef2a8ff4c01d7b2e2d20e9ff37b148f7d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 06:01:25 +0000

    zenity: update 3.32.0_1 bottle.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -6,13 +6,15 @@ class Zenity < Formula
   revision 1
 
   bottle do
-    sha256 arm64_big_sur: "794e362dfcf2ff4bc5138b4db391df744865b150086fef376c245c6e9b3d9669"
-    sha256 big_sur:       "bf8f705cbdadd17fca250b94df00f7a26c345c7476234a312819add6d0534e3d"
-    sha256 catalina:      "6bbf833a5a7d71346a5391fae436c2c46f530f0f5b9ac5d57330601b3456db49"
-    sha256 mojave:        "cef54fcd5601eb5dd3b563d1a09a6cd83654a2fa46e4a83a3d3c6e6a356fe29a"
-    sha256 high_sierra:   "36cf68d4838890e8d9122109464548a4630da0b06dcf6d4f0976ccf58b99dde2"
-    sha256 sierra:        "8b06d6cfec84ff39a95aeb4b466c1eb62584ff019ed90331334d243501cc8398"
-    sha256 x86_64_linux:  "08e7ce56caf7d471aa45e871e4a64bbd20a827aecf950670e5648ac1d1e21191"
+    sha256 arm64_monterey: "9410eaf14b7b10fc44ca257a2619860456b6d4da683ba0e5965427f49debbd7c"
+    sha256 arm64_big_sur:  "794e362dfcf2ff4bc5138b4db391df744865b150086fef376c245c6e9b3d9669"
+    sha256 monterey:       "96b0a805618f3468c2a6602ed2902f7961a13f4dbdcf6675cc22c04b1d0ffcc0"
+    sha256 big_sur:        "bf8f705cbdadd17fca250b94df00f7a26c345c7476234a312819add6d0534e3d"
+    sha256 catalina:       "6bbf833a5a7d71346a5391fae436c2c46f530f0f5b9ac5d57330601b3456db49"
+    sha256 mojave:         "cef54fcd5601eb5dd3b563d1a09a6cd83654a2fa46e4a83a3d3c6e6a356fe29a"
+    sha256 high_sierra:    "36cf68d4838890e8d9122109464548a4630da0b06dcf6d4f0976ccf58b99dde2"
+    sha256 sierra:         "8b06d6cfec84ff39a95aeb4b466c1eb62584ff019ed90331334d243501cc8398"
+    sha256 x86_64_linux:   "08e7ce56caf7d471aa45e871e4a64bbd20a827aecf950670e5648ac1d1e21191"
   end
 
   depends_on "itstool" => :build
