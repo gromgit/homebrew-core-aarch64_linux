1695c8d050b6619910320b3b39c031acab65415e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 09:07:20 +0000

    flashrom: update 1.2 bottle.

--- Formula/flashrom.rb
+++ Formula/flashrom.rb
@@ -13,12 +13,14 @@ class Flashrom < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "644f1d260c436590fbab9fbd11f946e57a9c56ee43911c8e1d6ea47f3775a1a0"
-    sha256 cellar: :any,                 big_sur:       "aa5c0856318732adf6a2bb4b980cef8a21829bd6a606beb357cae3ca71561217"
-    sha256 cellar: :any,                 catalina:      "301d0aafe8b31a53e6ee77217ce2280d1e998ceb7c8bc1a54a85c88afa940a33"
-    sha256 cellar: :any,                 mojave:        "69131a69023cd0336b8c9c9f1a56cafb28509f1e8eb5ada0bd45ff48357df38c"
-    sha256 cellar: :any,                 high_sierra:   "08d74d59cb4a56347de27465cc289b6494199951e2d251fafc328b4dc2f3e1e3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a894f6e3ec7f6eaaf960b53ec6539b6bdd5c778acd9de461e8b787c9f3d4670"
+    sha256 cellar: :any,                 arm64_monterey: "7c862cfd8fe14cccc7ab27cf41c6a0f97ce9ff536209b348014391ad93d45fc9"
+    sha256 cellar: :any,                 arm64_big_sur:  "644f1d260c436590fbab9fbd11f946e57a9c56ee43911c8e1d6ea47f3775a1a0"
+    sha256 cellar: :any,                 monterey:       "3a2da3a282bcd81282d21cb06574ef8ba8b3c258b17c9311363fb0d5434c16a5"
+    sha256 cellar: :any,                 big_sur:        "aa5c0856318732adf6a2bb4b980cef8a21829bd6a606beb357cae3ca71561217"
+    sha256 cellar: :any,                 catalina:       "301d0aafe8b31a53e6ee77217ce2280d1e998ceb7c8bc1a54a85c88afa940a33"
+    sha256 cellar: :any,                 mojave:         "69131a69023cd0336b8c9c9f1a56cafb28509f1e8eb5ada0bd45ff48357df38c"
+    sha256 cellar: :any,                 high_sierra:    "08d74d59cb4a56347de27465cc289b6494199951e2d251fafc328b4dc2f3e1e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a894f6e3ec7f6eaaf960b53ec6539b6bdd5c778acd9de461e8b787c9f3d4670"
   end
 
   depends_on "pkg-config" => :build
