bcfc601de670f8a95e6f7ae30269b74289c9bf06
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 10:00:02 +0000

    minisign: update 0.10 bottle.

--- Formula/minisign.rb
+++ Formula/minisign.rb
@@ -6,11 +6,13 @@ class Minisign < Formula
   license "ISC"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "50bdf209ebcc5e9079223dc1b47c488423655d154eca277a25269aa8d338dc2a"
-    sha256 cellar: :any,                 big_sur:       "73272eb26875a7b1c219279f91c1e97c24f5343bc28698e0adf190cdfbb611f5"
-    sha256 cellar: :any,                 catalina:      "6ef7ba174b48584911276e6491dea599b5e84f3747d2f25753c7b36b55ce3b3c"
-    sha256 cellar: :any,                 mojave:        "9340a13b895807ec4dbfced552ec3a612a5dbcc88b64b363aef876ec57a4ae4a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "932f5e7be0cb17788c687f8c474363a2acdbfb1b33041ba8c7d9781dde1d2353"
+    sha256 cellar: :any,                 arm64_monterey: "8d3f924d7180fe4b04d9e149691eaa4e08f3abe3c49ad817a551199caf274394"
+    sha256 cellar: :any,                 arm64_big_sur:  "50bdf209ebcc5e9079223dc1b47c488423655d154eca277a25269aa8d338dc2a"
+    sha256 cellar: :any,                 monterey:       "e9ed09ebebb07156ae2d4872bc6f77ee4ea8b92145354fc83ddbbae028af1da5"
+    sha256 cellar: :any,                 big_sur:        "73272eb26875a7b1c219279f91c1e97c24f5343bc28698e0adf190cdfbb611f5"
+    sha256 cellar: :any,                 catalina:       "6ef7ba174b48584911276e6491dea599b5e84f3747d2f25753c7b36b55ce3b3c"
+    sha256 cellar: :any,                 mojave:         "9340a13b895807ec4dbfced552ec3a612a5dbcc88b64b363aef876ec57a4ae4a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "932f5e7be0cb17788c687f8c474363a2acdbfb1b33041ba8c7d9781dde1d2353"
   end
 
   depends_on "cmake" => :build
