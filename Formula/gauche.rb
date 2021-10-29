bc47c4e5ee6c026cc96284d9157a3db2fd8856d7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 19:51:00 +0000

    gauche: update 0.9.10_3 bottle.

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -13,11 +13,13 @@ class Gauche < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "0fac8847033124e7ca5108de62cb3d08a573d8dc33715c911af85a668c50dfef"
-    sha256 big_sur:       "6edbe6e3edd503033d3feb3ff8a0bd1cfb4c16abbfa15328e2ccdd309656b017"
-    sha256 catalina:      "dc953fd8f622b64409d2dc6808d5cfd3828c3a36e5fd4fdbccde6db8529800e1"
-    sha256 mojave:        "06bcbbd5523d45e098d3e9f9c3c59d8e4858d66ad63f1cdb08bba8a804a08114"
-    sha256 x86_64_linux:  "6078ffcd2dea9f757643dec70b13ac66536c0dfd7f96a8ae1e86b0170c5aea53"
+    sha256 arm64_monterey: "f45d98a92e9ccf661db53a84fd768580848fa8e93733ca2934b426058b0a9589"
+    sha256 arm64_big_sur:  "0fac8847033124e7ca5108de62cb3d08a573d8dc33715c911af85a668c50dfef"
+    sha256 monterey:       "3173cffb21e9c6929a09fe5623b919241af7c9c6c1d5c606460a5f2dc3540f06"
+    sha256 big_sur:        "6edbe6e3edd503033d3feb3ff8a0bd1cfb4c16abbfa15328e2ccdd309656b017"
+    sha256 catalina:       "dc953fd8f622b64409d2dc6808d5cfd3828c3a36e5fd4fdbccde6db8529800e1"
+    sha256 mojave:         "06bcbbd5523d45e098d3e9f9c3c59d8e4858d66ad63f1cdb08bba8a804a08114"
+    sha256 x86_64_linux:   "6078ffcd2dea9f757643dec70b13ac66536c0dfd7f96a8ae1e86b0170c5aea53"
   end
 
   depends_on "mbedtls@2"
