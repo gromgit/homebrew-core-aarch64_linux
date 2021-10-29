bcf25dfd4eb61d33902a8a88cdc8ec47e2ae895e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 19:25:01 +0000

    ike-scan: update 1.9.5 bottle.

--- Formula/ike-scan.rb
+++ Formula/ike-scan.rb
@@ -8,11 +8,13 @@ class IkeScan < Formula
   head "https://github.com/royhills/ike-scan.git"
 
   bottle do
-    sha256 arm64_big_sur: "658bb4d7106e572a7d9a0dae2d6cc9abd4e1007d73ef4ab0ab0ec6f2cbaafd1c"
-    sha256 big_sur:       "debe304378fb8939b606d81a1658f95cb71b9edf538f9e2778385341d66bb3e9"
-    sha256 catalina:      "a02fd76ac113a9dc7cb1ed267d221af790abf4f5598a512c1993ea207ad7f6cf"
-    sha256 mojave:        "29f357b9b8a36f43410495f79a87e50fcf45507aee242f670182ea7db6630c1c"
-    sha256 x86_64_linux:  "16769378c139d0abb2a6c4a05fae1cc1929486d8035c3260391cde66b533b089"
+    sha256 arm64_monterey: "e2d29c13c00ac946fa360ecca65524a151e342f876fe4515e0bc2abdecccf7fa"
+    sha256 arm64_big_sur:  "658bb4d7106e572a7d9a0dae2d6cc9abd4e1007d73ef4ab0ab0ec6f2cbaafd1c"
+    sha256 monterey:       "5b806fa9c0134edb2710135e7bcf0ac8d5ac48debfd6286172e6abf687f52401"
+    sha256 big_sur:        "debe304378fb8939b606d81a1658f95cb71b9edf538f9e2778385341d66bb3e9"
+    sha256 catalina:       "a02fd76ac113a9dc7cb1ed267d221af790abf4f5598a512c1993ea207ad7f6cf"
+    sha256 mojave:         "29f357b9b8a36f43410495f79a87e50fcf45507aee242f670182ea7db6630c1c"
+    sha256 x86_64_linux:   "16769378c139d0abb2a6c4a05fae1cc1929486d8035c3260391cde66b533b089"
   end
 
   depends_on "autoconf" => :build
