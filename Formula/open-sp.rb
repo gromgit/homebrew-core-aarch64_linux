361fd7731e1013a2e2b485af1543b55b41ee2127
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 09:00:25 +0000

    open-sp: update 1.5.2 bottle.

--- Formula/open-sp.rb
+++ Formula/open-sp.rb
@@ -6,11 +6,13 @@ class OpenSp < Formula
 
   bottle do
     rebuild 5
-    sha256 cellar: :any,                 arm64_big_sur: "032676f1cd5c4bc0c1368cdf08bfe9a8b6df8f2c26ee4367c4a1285ab4fadc3a"
-    sha256 cellar: :any,                 big_sur:       "50109cdb514313693454259ba30f90f550618d48a1cc71df55ed04343d0cf641"
-    sha256 cellar: :any,                 catalina:      "1b2c18d6cdcd99d387770eaa14a773bb3edec5b22984ac75f3b07a181916f18f"
-    sha256 cellar: :any,                 mojave:        "47a3595b023164a54f73009f5d0a1bd092355f7c5b357cb86e1ec781b101bcb8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11f6bb56e019bea8f5fb9e2d38e62102230278ddc8fce115755a1cf6a6cbda54"
+    sha256 cellar: :any,                 arm64_monterey: "803db865811e2af00d1ea784c7bf0ed5d8b837f9bd5afff47bca13a5b97e8955"
+    sha256 cellar: :any,                 arm64_big_sur:  "032676f1cd5c4bc0c1368cdf08bfe9a8b6df8f2c26ee4367c4a1285ab4fadc3a"
+    sha256 cellar: :any,                 monterey:       "5c869b71025c07d7b86088189985d9a22c0f0c9fb719b775fb2388f5a0cbb16f"
+    sha256 cellar: :any,                 big_sur:        "50109cdb514313693454259ba30f90f550618d48a1cc71df55ed04343d0cf641"
+    sha256 cellar: :any,                 catalina:       "1b2c18d6cdcd99d387770eaa14a773bb3edec5b22984ac75f3b07a181916f18f"
+    sha256 cellar: :any,                 mojave:         "47a3595b023164a54f73009f5d0a1bd092355f7c5b357cb86e1ec781b101bcb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "11f6bb56e019bea8f5fb9e2d38e62102230278ddc8fce115755a1cf6a6cbda54"
   end
 
   depends_on "autoconf" => :build
