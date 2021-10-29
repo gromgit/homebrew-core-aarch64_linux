4700a7e02c8448f9e68c9a740b184ed401c46184
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 18:24:36 +0000

    quantlib: update 1.24 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -6,11 +6,13 @@ class Quantlib < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "4af724f1b39ac9d52d7e485b85733f083eb14bfccd8603822046506ab7956e96"
-    sha256 cellar: :any,                 big_sur:       "3c9fda9693a86afa5fbbc1b08ab3d964b99b6779a038b561358caabb19fd1813"
-    sha256 cellar: :any,                 catalina:      "2b742b1d55890ee8dbea13e12f6b2377dfa936c6ebf6a4983992d5546a72f436"
-    sha256 cellar: :any,                 mojave:        "3f6426241bdeec35cb2029054b9830301f1bd84037f12e07906d5b7afd0d99a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8ab1b70d643ad45ddd67b16d5071e57b08964cfa440abcb426b6f1fb07c5601"
+    sha256 cellar: :any,                 arm64_monterey: "5d7eb5ba677edbbe19268c4dd5b12b9bf6e7c5d2776ccf350e0f73733bf4bdd2"
+    sha256 cellar: :any,                 arm64_big_sur:  "4af724f1b39ac9d52d7e485b85733f083eb14bfccd8603822046506ab7956e96"
+    sha256 cellar: :any,                 monterey:       "b4e25a60744bceeb2624591c2f4bc396375c74513d0abec1496fee68fd27f685"
+    sha256 cellar: :any,                 big_sur:        "3c9fda9693a86afa5fbbc1b08ab3d964b99b6779a038b561358caabb19fd1813"
+    sha256 cellar: :any,                 catalina:       "2b742b1d55890ee8dbea13e12f6b2377dfa936c6ebf6a4983992d5546a72f436"
+    sha256 cellar: :any,                 mojave:         "3f6426241bdeec35cb2029054b9830301f1bd84037f12e07906d5b7afd0d99a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8ab1b70d643ad45ddd67b16d5071e57b08964cfa440abcb426b6f1fb07c5601"
   end
 
   head do
