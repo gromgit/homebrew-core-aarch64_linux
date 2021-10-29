b56ba3f0c3fc81fa377e713e4b04953fb6b6d9b0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:55:23 +0000

    knock: update 0.8 bottle.

--- Formula/knock.rb
+++ Formula/knock.rb
@@ -11,11 +11,13 @@ class Knock < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "81bcc7e4f700f88fd52678bcb97fe0eddf0a562e28bd406268ae263e286903dd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b8d423345658b70c35b16a032ace493f1da244144dbfe0f4c4b0ed79ce0ac560"
-    sha256 cellar: :any_skip_relocation, catalina:      "2c9a3167f4b08e9b4ed890f6cc165eda8e813da9c911e741fd9cdb5d3742de31"
-    sha256 cellar: :any_skip_relocation, mojave:        "5af91e5dcf61f216105c1562c032f2d06decbf6f6653171793d6af2101e27f0b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92ee3bfd7171b2fd896bda24b35ccb710365acf17e8c87b01d6a119a5133d996"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4f84095e2b3b16af1f3d2263b79dd3432f9dbc9db27654377d520fdb54a0c520"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "81bcc7e4f700f88fd52678bcb97fe0eddf0a562e28bd406268ae263e286903dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "964a0505b7d815efb5be3a9ecf3e935d19e07e8fb9245fc292dda30501d089cd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b8d423345658b70c35b16a032ace493f1da244144dbfe0f4c4b0ed79ce0ac560"
+    sha256 cellar: :any_skip_relocation, catalina:       "2c9a3167f4b08e9b4ed890f6cc165eda8e813da9c911e741fd9cdb5d3742de31"
+    sha256 cellar: :any_skip_relocation, mojave:         "5af91e5dcf61f216105c1562c032f2d06decbf6f6653171793d6af2101e27f0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "92ee3bfd7171b2fd896bda24b35ccb710365acf17e8c87b01d6a119a5133d996"
   end
 
   head do
