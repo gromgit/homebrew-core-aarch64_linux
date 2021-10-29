34347645acc6374abc4413a014c46d2c6a913b60
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:29:08 +0000

    riemann-client: update 1.10.5 bottle.

--- Formula/riemann-client.rb
+++ Formula/riemann-client.rb
@@ -7,11 +7,13 @@ class RiemannClient < Formula
   head "https://github.com/algernon/riemann-c-client.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "6ad0cf8ef43fc01df80a583cacb417077e6642df5580d7fa5cdcee6058dc457b"
-    sha256 cellar: :any,                 big_sur:       "2f1d22bea2043622bb483dcdc8232e4e848bc440705f78f5a093dcf5c1cb7293"
-    sha256 cellar: :any,                 catalina:      "9b6a719337b59560368471b724d9e059b7b625e25ee9008cc69fe33fecb5f474"
-    sha256 cellar: :any,                 mojave:        "0b2e63d5c1aa7a75fb6327e02e9dd0a4664e1d127ba46249da53361d4fe5f298"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "766018c13d715c98a6f726191508bb498c8b3056d584a305ce3c2cae2661417d"
+    sha256 cellar: :any,                 arm64_monterey: "1646e7a5beffa940b68eea346c32ccfcb770b611d7fc2843da6f15616c2cc690"
+    sha256 cellar: :any,                 arm64_big_sur:  "6ad0cf8ef43fc01df80a583cacb417077e6642df5580d7fa5cdcee6058dc457b"
+    sha256 cellar: :any,                 monterey:       "64bce13b27ffd64cf9fc61b0452089c5287c7f25e897eb98af716fb8835d9698"
+    sha256 cellar: :any,                 big_sur:        "2f1d22bea2043622bb483dcdc8232e4e848bc440705f78f5a093dcf5c1cb7293"
+    sha256 cellar: :any,                 catalina:       "9b6a719337b59560368471b724d9e059b7b625e25ee9008cc69fe33fecb5f474"
+    sha256 cellar: :any,                 mojave:         "0b2e63d5c1aa7a75fb6327e02e9dd0a4664e1d127ba46249da53361d4fe5f298"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "766018c13d715c98a6f726191508bb498c8b3056d584a305ce3c2cae2661417d"
   end
 
   depends_on "autoconf" => :build
