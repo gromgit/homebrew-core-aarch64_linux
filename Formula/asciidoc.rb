3741a3bd8a483e1511184897b1953c0620767d5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-29 04:09:14 +0000

    asciidoc: update 10.0.1 bottle.

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -14,12 +14,10 @@ class Asciidoc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0a2d42fa8688dde1c94c9650ed46fdefc2b7877bc7ccd55271022d7f89820aa3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0a2d42fa8688dde1c94c9650ed46fdefc2b7877bc7ccd55271022d7f89820aa3"
-    sha256 cellar: :any_skip_relocation, monterey:       "e888d83cc14f9a6cda0c71bed44ed1578b116b9b8c3f76f6948c73acb9d1ea7d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e888d83cc14f9a6cda0c71bed44ed1578b116b9b8c3f76f6948c73acb9d1ea7d"
-    sha256 cellar: :any_skip_relocation, catalina:       "e888d83cc14f9a6cda0c71bed44ed1578b116b9b8c3f76f6948c73acb9d1ea7d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a265c232637f028fd9bca2f2e993bce80261a2454b57169b10727cc100cb9363"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1fdfa8e89388fd98cc6b95e4e3aa982af82828361b52adc093c49d8f5d93f215"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fd1dae6a59a718a230623c4ced9a8dd9363471f2222b1992a5db617a3e3d8614"
+    sha256 cellar: :any_skip_relocation, catalina:      "fd1dae6a59a718a230623c4ced9a8dd9363471f2222b1992a5db617a3e3d8614"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "057a90f2c53ade6b7513e509a7b5d540158bb22a679533627f12b92c8bd46514"
   end
 
   depends_on "docbook"
