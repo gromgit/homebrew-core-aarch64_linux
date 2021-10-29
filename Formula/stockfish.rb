60c4a6884ee811b02bf24315a7a43615c22ad26f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:15:16 +0000

    stockfish: update 14 bottle.

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -12,11 +12,13 @@ class Stockfish < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "81b4c0a9df6cc42255c8795430e990ddf47a42831c5e54b1273e7b3a9221e316"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c5483261810c53e7600cf7739e046253951413b22bfe480d522ad63bd796d4e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "31819d40841821593f27ab92a021ae03458b251e91c80bd0d099af2062fa6a51"
-    sha256 cellar: :any_skip_relocation, mojave:        "684d804597360a5a7bc70b9392ef51b54627fdd864148eaaa10d6a3ddcbc5f8d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de53819aaf7d1f8679c0681ea2bc57c05d7040f8da1c6372bfa20616cc59492f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ed762f6cedfdd69e4d2068b956b6abb4eb86290c9254aa1cf75edf0ee010026b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "81b4c0a9df6cc42255c8795430e990ddf47a42831c5e54b1273e7b3a9221e316"
+    sha256 cellar: :any_skip_relocation, monterey:       "cc49bf2f3301735a7d8f22db0e8c9b6740314abcf7d388f9e59608077ce78aab"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c5483261810c53e7600cf7739e046253951413b22bfe480d522ad63bd796d4e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "31819d40841821593f27ab92a021ae03458b251e91c80bd0d099af2062fa6a51"
+    sha256 cellar: :any_skip_relocation, mojave:         "684d804597360a5a7bc70b9392ef51b54627fdd864148eaaa10d6a3ddcbc5f8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "de53819aaf7d1f8679c0681ea2bc57c05d7040f8da1c6372bfa20616cc59492f"
   end
 
   on_linux do
