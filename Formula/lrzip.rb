e77f03d0f7af0fbac3a7587d30c09bfbdba38cf9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 18:44:01 +0000

    lrzip: update 0.641 bottle.

--- Formula/lrzip.rb
+++ Formula/lrzip.rb
@@ -11,11 +11,13 @@ class Lrzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d9e67f4c880ecfe1e59f0714073f1dab6ce921a0585e763ab009bac21d545335"
-    sha256 cellar: :any,                 big_sur:       "33d561fad2bba643625d358fc65cfa2d8f37ae51d3329887da76e884d43b1515"
-    sha256 cellar: :any,                 catalina:      "701705808812d442dbd211235510a039a53cd4de9a4b28c014da5ad8a000014d"
-    sha256 cellar: :any,                 mojave:        "a3230ecfa68e08deb5f1414cb67736cffcde179ba34748df8e0fcdcb0d2c1ef7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5515b974789a0665b67ceb99c09d7c2b4edae560c5d7e4d7aee765fe95a563e0"
+    sha256 cellar: :any,                 arm64_monterey: "187914857fc2edba04f069d8fbd4d69bb8d140a9194b77b5bf45dd0295682c96"
+    sha256 cellar: :any,                 arm64_big_sur:  "d9e67f4c880ecfe1e59f0714073f1dab6ce921a0585e763ab009bac21d545335"
+    sha256 cellar: :any,                 monterey:       "d270d7085f30f8d07e32f746e8e9a10a09729ef6e60d0fbad9a5af85b1193522"
+    sha256 cellar: :any,                 big_sur:        "33d561fad2bba643625d358fc65cfa2d8f37ae51d3329887da76e884d43b1515"
+    sha256 cellar: :any,                 catalina:       "701705808812d442dbd211235510a039a53cd4de9a4b28c014da5ad8a000014d"
+    sha256 cellar: :any,                 mojave:         "a3230ecfa68e08deb5f1414cb67736cffcde179ba34748df8e0fcdcb0d2c1ef7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5515b974789a0665b67ceb99c09d7c2b4edae560c5d7e4d7aee765fe95a563e0"
   end
 
   depends_on "autoconf" => :build
