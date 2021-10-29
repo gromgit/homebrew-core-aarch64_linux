b21637e735941ecb9df3c42ab0730efc15c592b1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 18:53:22 +0000

    zint: update 2.10.0 bottle.

--- Formula/zint.rb
+++ Formula/zint.rb
@@ -12,11 +12,13 @@ class Zint < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "7e0a1464ff78c64c1be29694571dcfc6b5812ac7202706662a05fce7ba48717d"
-    sha256 cellar: :any,                 big_sur:       "aacdc432956a661f84eb7b1b31663b46f97087929a9d1f6aea3ae4a4441da56b"
-    sha256 cellar: :any,                 catalina:      "d23cc6ad59e245520ee185e971f9dab0f62630fc3b546d711716e32168967cca"
-    sha256 cellar: :any,                 mojave:        "9da4c9094f97924a5093044f6f3c59db2ccf2f6b0ff3d2c7bd75db1eac8b254d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64e38848f0b02b9eb42651b0693ab8bd0abe6b8bd69fefb8fa8fd555d1aaf265"
+    sha256 cellar: :any,                 arm64_monterey: "77d13463b6479df20523bb8a2bd09214a1a1ad5c6186b8284c642031d4a460e3"
+    sha256 cellar: :any,                 arm64_big_sur:  "7e0a1464ff78c64c1be29694571dcfc6b5812ac7202706662a05fce7ba48717d"
+    sha256 cellar: :any,                 monterey:       "40a3961b5875fff1e51ae1dba0f2b7a65eabb914261b79a315fa747123c08b76"
+    sha256 cellar: :any,                 big_sur:        "aacdc432956a661f84eb7b1b31663b46f97087929a9d1f6aea3ae4a4441da56b"
+    sha256 cellar: :any,                 catalina:       "d23cc6ad59e245520ee185e971f9dab0f62630fc3b546d711716e32168967cca"
+    sha256 cellar: :any,                 mojave:         "9da4c9094f97924a5093044f6f3c59db2ccf2f6b0ff3d2c7bd75db1eac8b254d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "64e38848f0b02b9eb42651b0693ab8bd0abe6b8bd69fefb8fa8fd555d1aaf265"
   end
 
   depends_on "cmake" => :build
