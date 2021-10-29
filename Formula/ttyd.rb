147fb636af623557490fb433b72e0f58de69b6f1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:50:23 +0000

    ttyd: update 1.6.3_4 bottle.

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -8,11 +8,13 @@ class Ttyd < Formula
   head "https://github.com/tsl0922/ttyd.git", branch: "main"
 
   bottle do
-    sha256 arm64_big_sur: "c447da2fb5319fb4f14156e6df03aced1a4eff128538c05674a3872a3843a5e0"
-    sha256 big_sur:       "1d05dbb4c06fdf46c47195d4cbd32939e484fbff6b973ce111166a6fead65b5c"
-    sha256 catalina:      "201f490f7ccbb978085190d01585deda06430aada52271cc4ea8f434d16492c6"
-    sha256 mojave:        "555135cdd2ff20f6e0c7d95e756e1be4561c458b2f2411c6478b574e726b8b5d"
-    sha256 x86_64_linux:  "00890703377dc26256a8209f0ba0f33cbab1f43e563b4cb3b28b096cdf25ee00"
+    sha256 arm64_monterey: "97d637edb8f541ca79760c6f95ac1f64ec418e7204fd6a4cce50fde4c14be8b1"
+    sha256 arm64_big_sur:  "c447da2fb5319fb4f14156e6df03aced1a4eff128538c05674a3872a3843a5e0"
+    sha256 monterey:       "28a8e002ddb777a9abe3eebe67795b9d9dc5008c31d1451aa6387fdd43705ced"
+    sha256 big_sur:        "1d05dbb4c06fdf46c47195d4cbd32939e484fbff6b973ce111166a6fead65b5c"
+    sha256 catalina:       "201f490f7ccbb978085190d01585deda06430aada52271cc4ea8f434d16492c6"
+    sha256 mojave:         "555135cdd2ff20f6e0c7d95e756e1be4561c458b2f2411c6478b574e726b8b5d"
+    sha256 x86_64_linux:   "00890703377dc26256a8209f0ba0f33cbab1f43e563b4cb3b28b096cdf25ee00"
   end
 
   depends_on "cmake" => :build
