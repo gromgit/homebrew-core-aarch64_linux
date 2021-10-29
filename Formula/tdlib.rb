3c8c71822c1359f74039e8879b32dedc53e14764
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 21:55:07 +0000

    tdlib: update 1.7.0 bottle.

--- Formula/tdlib.rb
+++ Formula/tdlib.rb
@@ -7,11 +7,13 @@ class Tdlib < Formula
   head "https://github.com/tdlib/td.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "15d07ea3abe99c9c65e1e74fa43aa6c2be758e84dc5f8657ef68fc47d8540a36"
-    sha256 cellar: :any,                 big_sur:       "79dc39f41a2ad6d8272887c0564f043e9c362b1073ba2ceeb338f50e717c97dc"
-    sha256 cellar: :any,                 catalina:      "fc606ff0b78fd6ad52f0449dfd1380e646b4de63ff36756546838b783a088ca2"
-    sha256 cellar: :any,                 mojave:        "007b08aced0aa457830daaade4299c979ee97db6b420bdfe5d0e6bdd416925c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b31b608f51cf0bf440570fe2eaf172a55816c378a8bfedc69e7625b55f2004b8"
+    sha256 cellar: :any,                 arm64_monterey: "65a4a0f8e819d3098b6d27a25094cbdd8f0b52b8b5634d61bc56cab322c69808"
+    sha256 cellar: :any,                 arm64_big_sur:  "15d07ea3abe99c9c65e1e74fa43aa6c2be758e84dc5f8657ef68fc47d8540a36"
+    sha256 cellar: :any,                 monterey:       "0d8bf2d9dc9fe1cbf2e14e09edad4b925e3c9364dee634a06c91665e9a9885a5"
+    sha256 cellar: :any,                 big_sur:        "79dc39f41a2ad6d8272887c0564f043e9c362b1073ba2ceeb338f50e717c97dc"
+    sha256 cellar: :any,                 catalina:       "fc606ff0b78fd6ad52f0449dfd1380e646b4de63ff36756546838b783a088ca2"
+    sha256 cellar: :any,                 mojave:         "007b08aced0aa457830daaade4299c979ee97db6b420bdfe5d0e6bdd416925c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b31b608f51cf0bf440570fe2eaf172a55816c378a8bfedc69e7625b55f2004b8"
   end
 
   depends_on "cmake" => :build
