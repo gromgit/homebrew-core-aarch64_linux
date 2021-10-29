9b257a75abd817fabc0296ed733b53754d843a84
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 19:19:46 +0000

    gputils: update 1.5.0-1 bottle.

--- Formula/gputils.rb
+++ Formula/gputils.rb
@@ -11,15 +11,17 @@ class Gputils < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "b4ac5565769da3d905f0c1da30257d24b94dac82c0cf7e753e96a2234d298eef"
-    sha256 big_sur:       "3d2dfbf25fd0d678b1e21572c6bc8b78e52c898f73ca3cb1e36c35d4d34ada73"
-    sha256 catalina:      "66500b5fe160a363d47dd326ff7e983e33f81d25a457f0304ada80de72d61ac8"
-    sha256 mojave:        "94ddaac79ac5d6cfdddbd588fbb2ccd8be3f5a62662fe64d19a63828d8b6d305"
-    sha256 high_sierra:   "c5ed95fc323471f635edbc08e81394f66486c4c81953a0881c5b26791d1176bb"
-    sha256 sierra:        "aba5cb544582e26bdb212f9782f911b0e9d36c5049d0aed3928c48ae9b74a6e9"
-    sha256 el_capitan:    "ad3a6688ca0dac1da0a10db36d9119d9bedcd8d0f389920a45832cc0676c67a3"
-    sha256 yosemite:      "dd69717c349e405ed04eebc07fc86dcc09d3763f892488514528b328a725a3f9"
-    sha256 x86_64_linux:  "040d2b02516d9469d2e90583e1d665dc1aacaaf4da5b153bad05797f24a4cb5d"
+    sha256 arm64_monterey: "e4ecaee9875f27a1b2a031c1da1a3e4a8ab57ae45057e7d12cf0171335122e27"
+    sha256 arm64_big_sur:  "b4ac5565769da3d905f0c1da30257d24b94dac82c0cf7e753e96a2234d298eef"
+    sha256 monterey:       "b3753904988b8a9581c68c2c724d7b60dd6d2fff331f549693d78ed486b26a49"
+    sha256 big_sur:        "3d2dfbf25fd0d678b1e21572c6bc8b78e52c898f73ca3cb1e36c35d4d34ada73"
+    sha256 catalina:       "66500b5fe160a363d47dd326ff7e983e33f81d25a457f0304ada80de72d61ac8"
+    sha256 mojave:         "94ddaac79ac5d6cfdddbd588fbb2ccd8be3f5a62662fe64d19a63828d8b6d305"
+    sha256 high_sierra:    "c5ed95fc323471f635edbc08e81394f66486c4c81953a0881c5b26791d1176bb"
+    sha256 sierra:         "aba5cb544582e26bdb212f9782f911b0e9d36c5049d0aed3928c48ae9b74a6e9"
+    sha256 el_capitan:     "ad3a6688ca0dac1da0a10db36d9119d9bedcd8d0f389920a45832cc0676c67a3"
+    sha256 yosemite:       "dd69717c349e405ed04eebc07fc86dcc09d3763f892488514528b328a725a3f9"
+    sha256 x86_64_linux:   "040d2b02516d9469d2e90583e1d665dc1aacaaf4da5b153bad05797f24a4cb5d"
   end
 
   def install
