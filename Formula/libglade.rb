cd79e2f840a70f967fe3e313cf4552c89bb0f81f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 16:46:54 +0000

    libglade: update 2.6.4_4 bottle.

--- Formula/libglade.rb
+++ Formula/libglade.rb
@@ -7,13 +7,15 @@ class Libglade < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "d4590501ed823f6ba28905f2a7ab3e5d64b6497393b11829fb753c7ed56cc1d3"
-    sha256 big_sur:       "7408ed79b9c5c118628b566cda02c6fec57cb8cbdbad4db83759f41324d5171f"
-    sha256 catalina:      "f87fe8b63946d78fd43586ef25fbd108d9f81fda2089a66f40cbdc0216601f8e"
-    sha256 mojave:        "3fdb8055e888e22f7054432b185aad35a20c0d48b3c07c97429cab2b7a0bd3cc"
-    sha256 high_sierra:   "fd198334f49180de53d5bde9406e17aa4e3051ee5c421defdab9dbb0f3a1e681"
-    sha256 sierra:        "019f499d6ca86f279d5bfec74bf71ffe11a89bb6bc70f6901b7074e14885132c"
-    sha256 x86_64_linux:  "bcdaaabe9019e832d451948a90c49f999d28b576610d4c46ea6bf2594b1fbc66"
+    sha256 arm64_monterey: "72596bd26b4e2fe8f08f3355afedccd2c8f9c9d6631794317c635e43d784fe2e"
+    sha256 arm64_big_sur:  "d4590501ed823f6ba28905f2a7ab3e5d64b6497393b11829fb753c7ed56cc1d3"
+    sha256 monterey:       "339fbb07749127c633652418e75148dbaed80c073f24aaee0982cd9ff2cc5ff0"
+    sha256 big_sur:        "7408ed79b9c5c118628b566cda02c6fec57cb8cbdbad4db83759f41324d5171f"
+    sha256 catalina:       "f87fe8b63946d78fd43586ef25fbd108d9f81fda2089a66f40cbdc0216601f8e"
+    sha256 mojave:         "3fdb8055e888e22f7054432b185aad35a20c0d48b3c07c97429cab2b7a0bd3cc"
+    sha256 high_sierra:    "fd198334f49180de53d5bde9406e17aa4e3051ee5c421defdab9dbb0f3a1e681"
+    sha256 sierra:         "019f499d6ca86f279d5bfec74bf71ffe11a89bb6bc70f6901b7074e14885132c"
+    sha256 x86_64_linux:   "bcdaaabe9019e832d451948a90c49f999d28b576610d4c46ea6bf2594b1fbc66"
   end
 
   depends_on "pkg-config" => :build
