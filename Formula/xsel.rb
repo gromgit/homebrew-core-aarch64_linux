25e81f2c1bdc5febdd74862f480f90afc86182c5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 20:14:47 +0000

    xsel: update 1.2.0 bottle.

--- Formula/xsel.rb
+++ Formula/xsel.rb
@@ -51,11 +51,13 @@ class Xsel < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b32e829803d81ea7d09bb0911b31a20ac6c75fdbc67fa3c4c9184458e3d3ecb3"
-    sha256 cellar: :any,                 big_sur:       "f596d08cffadf2bff12804cc18fc61ddb4cdf47599f869d938c7ebd860e1950d"
-    sha256 cellar: :any,                 catalina:      "18f72e215611df386415475668dd769e37c6b715715e477ba39866f17a95c1f2"
-    sha256 cellar: :any,                 mojave:        "8fd34073cf958d18b31d51f0e3d05f7a93e0a71b00cc696bdf4d4018409b3c46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "81062fbb7f2a56e6510ab0103c795e5f0091dafa810100d03a83b3295b3f577f"
+    sha256 cellar: :any,                 arm64_monterey: "de069e6eb3b7f2ab28641b1be7c7df88a0ea59587e01e59b79ddc680e0226194"
+    sha256 cellar: :any,                 arm64_big_sur:  "b32e829803d81ea7d09bb0911b31a20ac6c75fdbc67fa3c4c9184458e3d3ecb3"
+    sha256 cellar: :any,                 monterey:       "834b49b7669f077df30e1e791037b5ebabc657aef2d9b1c6c4c8425b8c401754"
+    sha256 cellar: :any,                 big_sur:        "f596d08cffadf2bff12804cc18fc61ddb4cdf47599f869d938c7ebd860e1950d"
+    sha256 cellar: :any,                 catalina:       "18f72e215611df386415475668dd769e37c6b715715e477ba39866f17a95c1f2"
+    sha256 cellar: :any,                 mojave:         "8fd34073cf958d18b31d51f0e3d05f7a93e0a71b00cc696bdf4d4018409b3c46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "81062fbb7f2a56e6510ab0103c795e5f0091dafa810100d03a83b3295b3f577f"
   end
 
   head do
