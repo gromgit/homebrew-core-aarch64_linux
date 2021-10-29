485a6d0434ddf75d0743b22eb19f7c0149e909f1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 19:01:59 +0000

    monero: update 0.17.2.3 bottle.

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -12,11 +12,13 @@ class Monero < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "13245ecf80fad0038bef6dedd713f9e97ef64c7d3985bc68dd4a8597646b4059"
-    sha256 cellar: :any,                 big_sur:       "bad7e328cceef655c092f5555e16a92c4c8841fc93ad8152058327be3bf8625d"
-    sha256 cellar: :any,                 catalina:      "b416b9387fd77c4b8041864c590b233d1dc6645e3a6cca6c23ba7bf233b16d3c"
-    sha256 cellar: :any,                 mojave:        "64fa20b4cf46cc810fd8aae3b28adaa346ba0f223a196489311eab03870df2fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d43bb2f040234a457f756d77ed4f6f596115fadd1189071e2a32f570d14ac233"
+    sha256 cellar: :any,                 arm64_monterey: "277c948b74a235441cfe17bf401a3c1867e464063ec5632b145f1863098e3dc4"
+    sha256 cellar: :any,                 arm64_big_sur:  "13245ecf80fad0038bef6dedd713f9e97ef64c7d3985bc68dd4a8597646b4059"
+    sha256 cellar: :any,                 monterey:       "f7491c58eb20dda7631ab70fa4770df2a0aa0318692ab11f9cff527af46f079d"
+    sha256 cellar: :any,                 big_sur:        "bad7e328cceef655c092f5555e16a92c4c8841fc93ad8152058327be3bf8625d"
+    sha256 cellar: :any,                 catalina:       "b416b9387fd77c4b8041864c590b233d1dc6645e3a6cca6c23ba7bf233b16d3c"
+    sha256 cellar: :any,                 mojave:         "64fa20b4cf46cc810fd8aae3b28adaa346ba0f223a196489311eab03870df2fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d43bb2f040234a457f756d77ed4f6f596115fadd1189071e2a32f570d14ac233"
   end
 
   depends_on "cmake" => :build
