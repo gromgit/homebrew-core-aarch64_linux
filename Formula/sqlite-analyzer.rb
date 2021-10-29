2e90bfa479794e013da9a23f8b5d18bf9a2c3ad5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 21:12:10 +0000

    sqlite-analyzer: update 3.36.0 bottle.

--- Formula/sqlite-analyzer.rb
+++ Formula/sqlite-analyzer.rb
@@ -11,10 +11,12 @@ class SqliteAnalyzer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "279951170e98f9f3b4b23fbd34cd210c6a3ad3c8b564f7a6dd41366f98610cf9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8e912eaec069e12f498c2ba3bc09217cd2cf924ff8e26bfa966680b98d53f95c"
-    sha256 cellar: :any_skip_relocation, catalina:      "927cb8b16c0c5c4fefc9169c1d51346c6db3daaba27219a194b353d0e1a4b08f"
-    sha256 cellar: :any_skip_relocation, mojave:        "039205cb4d999259011589f99db4dea8612a0a743cca17265c06734342820489"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e78e1c2c8eb006ec96b3ca1865e3dc79ea29a0e557c5eb55316903fa0c6dd352"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "279951170e98f9f3b4b23fbd34cd210c6a3ad3c8b564f7a6dd41366f98610cf9"
+    sha256 cellar: :any_skip_relocation, monterey:       "80e4e43e9de899dcd29baef59790315135305788439e1abad77e7871816954f9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8e912eaec069e12f498c2ba3bc09217cd2cf924ff8e26bfa966680b98d53f95c"
+    sha256 cellar: :any_skip_relocation, catalina:       "927cb8b16c0c5c4fefc9169c1d51346c6db3daaba27219a194b353d0e1a4b08f"
+    sha256 cellar: :any_skip_relocation, mojave:         "039205cb4d999259011589f99db4dea8612a0a743cca17265c06734342820489"
   end
 
   def install
