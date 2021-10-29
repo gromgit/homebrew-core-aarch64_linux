e4d67f0e86e16a7faece5a3f0e939aca6808dec0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 09:24:52 +0000

    pgbouncer: update 1.16.0 bottle.

--- Formula/pgbouncer.rb
+++ Formula/pgbouncer.rb
@@ -9,11 +9,13 @@ class Pgbouncer < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "296a29f3cbfc6913fc9fdd0ebfd3a0f5475b74a781192b869fcb6757e919c879"
-    sha256 cellar: :any,                 big_sur:       "7d61dac174635767840b8dde46b2d9b4b25f6a36fc5ac950beb0868366fa87b8"
-    sha256 cellar: :any,                 catalina:      "e9bcdece7ed67566382ed196d34f6550fd093519e0283cb5e6170bd66645355f"
-    sha256 cellar: :any,                 mojave:        "d6e080c159196a61edd69c73ea234ce34bc91baeaf25e814633d2b35cabfb496"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26f5a2974400ec50e0727ce1b3054ceacd83e2e10e6b65e0e92296f412c65bfb"
+    sha256 cellar: :any,                 arm64_monterey: "ed28e5a42d5212fa39c4f5906b93bea54eed6ed3a7f0df89dd7596ce4190afa3"
+    sha256 cellar: :any,                 arm64_big_sur:  "296a29f3cbfc6913fc9fdd0ebfd3a0f5475b74a781192b869fcb6757e919c879"
+    sha256 cellar: :any,                 monterey:       "c253e4a238780e44ac2aff1873ed9750a671ef9960014c078627ab45ed8cdb7d"
+    sha256 cellar: :any,                 big_sur:        "7d61dac174635767840b8dde46b2d9b4b25f6a36fc5ac950beb0868366fa87b8"
+    sha256 cellar: :any,                 catalina:       "e9bcdece7ed67566382ed196d34f6550fd093519e0283cb5e6170bd66645355f"
+    sha256 cellar: :any,                 mojave:         "d6e080c159196a61edd69c73ea234ce34bc91baeaf25e814633d2b35cabfb496"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26f5a2974400ec50e0727ce1b3054ceacd83e2e10e6b65e0e92296f412c65bfb"
   end
 
   depends_on "pkg-config" => :build
