f370e186630117bab31bccc8d27925587fe5a9e9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 20:37:25 +0000

    orocos-kdl: update 1.5.1 bottle.

--- Formula/orocos-kdl.rb
+++ Formula/orocos-kdl.rb
@@ -6,11 +6,13 @@ class OrocosKdl < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "70f546d7391dea4d7005d93f3720cde06f14e53b3cc7c514d67376ea05ac6493"
-    sha256 cellar: :any,                 big_sur:       "1c7f7a1fc384bd107acd0d3b8d302beed12c89198617f19b2c74d89e437e3ee8"
-    sha256 cellar: :any,                 catalina:      "4318687278abffc43d727c33539957387dbbab41f7ec9c08a4f5ff7a6061abd1"
-    sha256 cellar: :any,                 mojave:        "be089171fe396309561089d802e13ec101e56e91a5d2141207d588c1f703f52b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76ca5a86d809b86783da8111f4d3aa54f0f3cf6e2eb09dc499e0b5de4e6257f9"
+    sha256 cellar: :any,                 arm64_monterey: "d500d33947bf10c203f853e3476cd4c2f86eb576c24cbd6b36e129a27a92846b"
+    sha256 cellar: :any,                 arm64_big_sur:  "70f546d7391dea4d7005d93f3720cde06f14e53b3cc7c514d67376ea05ac6493"
+    sha256 cellar: :any,                 monterey:       "97af713602639e5fd50b3027a97d8c6d7aa7261a90fe474ac35325b48032319e"
+    sha256 cellar: :any,                 big_sur:        "1c7f7a1fc384bd107acd0d3b8d302beed12c89198617f19b2c74d89e437e3ee8"
+    sha256 cellar: :any,                 catalina:       "4318687278abffc43d727c33539957387dbbab41f7ec9c08a4f5ff7a6061abd1"
+    sha256 cellar: :any,                 mojave:         "be089171fe396309561089d802e13ec101e56e91a5d2141207d588c1f703f52b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76ca5a86d809b86783da8111f4d3aa54f0f3cf6e2eb09dc499e0b5de4e6257f9"
   end
 
   depends_on "cmake" => :build
