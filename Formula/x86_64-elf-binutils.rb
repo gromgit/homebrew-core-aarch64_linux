a85c407ac16cd12a1bd7a5f9e18f1b9393b23c2f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 06:35:14 +0000

    x86_64-elf-binutils: update 2.37 bottle.

--- Formula/x86_64-elf-binutils.rb
+++ Formula/x86_64-elf-binutils.rb
@@ -11,11 +11,13 @@ class X8664ElfBinutils < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "c1cc0eeef6981f55e0df57f0e8115956ab5b4d94d043daf805c8833ed843dc1d"
-    sha256 big_sur:       "085e8d5b89905e85c00951d3f12a6b36ea2e085b07e6af28b4138956b9c53384"
-    sha256 catalina:      "8853d7a257c9cbe3243e9cb48837222768b6a211c0a0b7733cd40effdb56cb72"
-    sha256 mojave:        "efb90bb7f3e96087317c35a824fbe46e4dda1ccaaa6cd2fc26f889b6210b4d5a"
-    sha256 x86_64_linux:  "94bb83f893103b9be998affaec0cc210691fa4b71c27d9491a7598c2c9a30576"
+    sha256 arm64_monterey: "cc2b264def9baf925812bf75622f1132b16de47a940e4e999680ced6ce17580a"
+    sha256 arm64_big_sur:  "c1cc0eeef6981f55e0df57f0e8115956ab5b4d94d043daf805c8833ed843dc1d"
+    sha256 monterey:       "587fe762419c190b723e48506d6ea0a3ee571d0c0d479977c02225fdcba60393"
+    sha256 big_sur:        "085e8d5b89905e85c00951d3f12a6b36ea2e085b07e6af28b4138956b9c53384"
+    sha256 catalina:       "8853d7a257c9cbe3243e9cb48837222768b6a211c0a0b7733cd40effdb56cb72"
+    sha256 mojave:         "efb90bb7f3e96087317c35a824fbe46e4dda1ccaaa6cd2fc26f889b6210b4d5a"
+    sha256 x86_64_linux:   "94bb83f893103b9be998affaec0cc210691fa4b71c27d9491a7598c2c9a30576"
   end
 
   uses_from_macos "texinfo"
