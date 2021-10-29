bdb60e7ffc3655f7d6bbf4115295aaa7b9dbe651
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 17:19:21 +0800

    enca: patch incorrect -flat_namespace usage (#88242)
    This is needed for bottling on Monterey.

--- Formula/enca.rb
+++ Formula/enca.rb
@@ -18,6 +18,12 @@ class Enca < Formula
     sha256 x86_64_linux:  "c434ad486d2bc894f2562f7a02257bfa726a8623287e5f665cc4d20f7ea42c25"
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
