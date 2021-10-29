2edf3e33be388c77a9fdad67a2d4fc15e2955e78
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 22:52:48 +0000

    hercules: update 3.13 bottle.

--- Formula/hercules.rb
+++ Formula/hercules.rb
@@ -12,10 +12,12 @@ class Hercules < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "ba4b3fa347d63601909127c94c0a2b1e42d81bbcc154970a18d7dd4ad8b15bba"
-    sha256 big_sur:       "c85d96adaa0f5cc5a17d5927d4cd1b44f42003baba3e59ff11cee5ce444512fc"
-    sha256 catalina:      "aae09d5616cf146c74bf3bfae69c1490cf920404d75d43c7d8c28ac1aab176b8"
-    sha256 mojave:        "3c7535fa1d1e9385c9f2525e40445b931c3768ab611db4e6a2019c7910538c41"
+    sha256 arm64_monterey: "989fec41881653b8d1f7372d4de8447703fba4b2b6880194e5974cdad50b58d9"
+    sha256 arm64_big_sur:  "ba4b3fa347d63601909127c94c0a2b1e42d81bbcc154970a18d7dd4ad8b15bba"
+    sha256 monterey:       "00df43bff8324b015c01c6cae809d69b911e2c2ba45b0a07a4d3be440daf672b"
+    sha256 big_sur:        "c85d96adaa0f5cc5a17d5927d4cd1b44f42003baba3e59ff11cee5ce444512fc"
+    sha256 catalina:       "aae09d5616cf146c74bf3bfae69c1490cf920404d75d43c7d8c28ac1aab176b8"
+    sha256 mojave:         "3c7535fa1d1e9385c9f2525e40445b931c3768ab611db4e6a2019c7910538c41"
   end
 
   head do
