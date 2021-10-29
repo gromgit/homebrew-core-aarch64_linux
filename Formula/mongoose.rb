5a859558f85df671d7411721ee4483092766858f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:34:20 +0000

    mongoose: update 7.4 bottle.

--- Formula/mongoose.rb
+++ Formula/mongoose.rb
@@ -6,11 +6,13 @@ class Mongoose < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a9b43175f92e8b23abbd752fd0ed21879d89595caaec340222775da33cac7d5f"
-    sha256 cellar: :any,                 big_sur:       "f983ab7ccf9331fcb3955ce1a12974345016b95e68e221e206a24b1008efd3a0"
-    sha256 cellar: :any,                 catalina:      "633718da5b3cd80405580bdb7e8e1fb49cfcd0750341ea4c3283613a212c1f2d"
-    sha256 cellar: :any,                 mojave:        "6af8c2a0736cbf2ea1c69266c6af4bde1f2bc3005e0550f2875ec3acd7b155d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d03f8869ca3d67dc687cda9e61bfff91aaa9c82f5437dcb0191e9f062890d453"
+    sha256 cellar: :any,                 arm64_monterey: "0c005a2ff1a9631ba39b062b1785c8ec9beeb1c7d9aad70d7e569f50114c28b9"
+    sha256 cellar: :any,                 arm64_big_sur:  "a9b43175f92e8b23abbd752fd0ed21879d89595caaec340222775da33cac7d5f"
+    sha256 cellar: :any,                 monterey:       "e00fee67719e59495dbd7ff71c46636d3ccd636240b5b4155b24a38eecdbf4f8"
+    sha256 cellar: :any,                 big_sur:        "f983ab7ccf9331fcb3955ce1a12974345016b95e68e221e206a24b1008efd3a0"
+    sha256 cellar: :any,                 catalina:       "633718da5b3cd80405580bdb7e8e1fb49cfcd0750341ea4c3283613a212c1f2d"
+    sha256 cellar: :any,                 mojave:         "6af8c2a0736cbf2ea1c69266c6af4bde1f2bc3005e0550f2875ec3acd7b155d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d03f8869ca3d67dc687cda9e61bfff91aaa9c82f5437dcb0191e9f062890d453"
   end
 
   depends_on "openssl@1.1"
