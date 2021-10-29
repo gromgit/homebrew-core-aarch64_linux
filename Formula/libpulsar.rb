071f7cb76b4535cd83581774c337c28ba3c57546
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 06:44:20 +0000

    libpulsar: update 2.8.1 bottle.

--- Formula/libpulsar.rb
+++ Formula/libpulsar.rb
@@ -7,11 +7,13 @@ class Libpulsar < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "f44531771bffaa687d9061132bd83acafd1288cd82b526028366c88ba52a027a"
-    sha256 cellar: :any,                 big_sur:       "3dd3fd3c00956ed62b9b2fbb91795889fb06b264e1df331fcb58a445cfa0784d"
-    sha256 cellar: :any,                 catalina:      "8538b133cd33c189390d1bcdcaa8d3c41dbac40631697f168f74c415929edf8a"
-    sha256 cellar: :any,                 mojave:        "743de257b0996bcf769a74054fcbee90c24a2a3f77584c34960ba3ffdfa153f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8acd3ad68d151b0e698b811d60db8e6eb00d7465c56fe9ac913546c45196dbeb"
+    sha256 cellar: :any,                 arm64_monterey: "d66af4667d00250e795e34577700b927b3fbeb73d0ac629d466305333ae2b631"
+    sha256 cellar: :any,                 arm64_big_sur:  "f44531771bffaa687d9061132bd83acafd1288cd82b526028366c88ba52a027a"
+    sha256 cellar: :any,                 monterey:       "4b8fca76b83c8d16b07c7d7d2cf3585ecc87f6ec3f317e33b9feda96eaed9759"
+    sha256 cellar: :any,                 big_sur:        "3dd3fd3c00956ed62b9b2fbb91795889fb06b264e1df331fcb58a445cfa0784d"
+    sha256 cellar: :any,                 catalina:       "8538b133cd33c189390d1bcdcaa8d3c41dbac40631697f168f74c415929edf8a"
+    sha256 cellar: :any,                 mojave:         "743de257b0996bcf769a74054fcbee90c24a2a3f77584c34960ba3ffdfa153f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8acd3ad68d151b0e698b811d60db8e6eb00d7465c56fe9ac913546c45196dbeb"
   end
 
   depends_on "cmake" => :build
