461ae5bba6453d84b1fa23736948738aebe82ccb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 07:21:19 +0000

    faust: update 2.33.1_1 bottle.

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -7,10 +7,12 @@ class Faust < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4bd42dda70facd749d115327250fc08907637c3d0afbf7baa66a1b4ad4c83718"
-    sha256 cellar: :any, big_sur:       "fb9c0feafa370d43b0f6ea3e846a11b100def498a160ef5556005e248caa8b0f"
-    sha256 cellar: :any, catalina:      "b648b47e1f52d5a3c810a36137261c914b7420c325c7ea22909588838e440712"
-    sha256 cellar: :any, mojave:        "07f13fd3379b293917ec177cda749c7b9a542c2ae5816496bac74b8a8b790f56"
+    sha256 cellar: :any, arm64_monterey: "12b8c83359c815e0a56662f53bdf75a7b6c20feac9e29cc72aad4d1f821f2002"
+    sha256 cellar: :any, arm64_big_sur:  "4bd42dda70facd749d115327250fc08907637c3d0afbf7baa66a1b4ad4c83718"
+    sha256 cellar: :any, monterey:       "67df30f6d5def23ac17526385e9ea6b5baabb67a80af29918075db0caa3c6468"
+    sha256 cellar: :any, big_sur:        "fb9c0feafa370d43b0f6ea3e846a11b100def498a160ef5556005e248caa8b0f"
+    sha256 cellar: :any, catalina:       "b648b47e1f52d5a3c810a36137261c914b7420c325c7ea22909588838e440712"
+    sha256 cellar: :any, mojave:         "07f13fd3379b293917ec177cda749c7b9a542c2ae5816496bac74b8a8b790f56"
   end
 
   depends_on "cmake" => :build
