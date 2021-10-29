6f90972f99bbcc7bf120600743d07cdb40cb2c23
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 20:21:52 +0000

    bibutils: update 7.1 bottle.

--- Formula/bibutils.rb
+++ Formula/bibutils.rb
@@ -11,11 +11,13 @@ class Bibutils < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fe797d92065d5b0ef0f16511bfea5ba2567d2f877038109cb5e30f0d796a0f57"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a8989477b91d3423deea04a5833b83cea7eee836ad197dfb30db9ce9599788b2"
-    sha256 cellar: :any_skip_relocation, catalina:      "4f3cd7ce2f12a53e6afd35f92990968e6463a674dc4082315709c295e4869bb6"
-    sha256 cellar: :any_skip_relocation, mojave:        "866724d8735e42aa3c62cb4b47e8be77bb41490d23ca2d83f0096ee2b9b4e5fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf0f79873c4ce317aed31c0c1516552e937ccddc191f951d3acf8cc3f2e9ca06"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac5a87386eb76043970aa522dd774abf6f60d807646f361a8039c64e1dc5472b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fe797d92065d5b0ef0f16511bfea5ba2567d2f877038109cb5e30f0d796a0f57"
+    sha256 cellar: :any_skip_relocation, monterey:       "dab6027f372c7969b4860d6e6a8d85bafbfe5953f773f2e7185cba6db3f0dc0c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a8989477b91d3423deea04a5833b83cea7eee836ad197dfb30db9ce9599788b2"
+    sha256 cellar: :any_skip_relocation, catalina:       "4f3cd7ce2f12a53e6afd35f92990968e6463a674dc4082315709c295e4869bb6"
+    sha256 cellar: :any_skip_relocation, mojave:         "866724d8735e42aa3c62cb4b47e8be77bb41490d23ca2d83f0096ee2b9b4e5fd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf0f79873c4ce317aed31c0c1516552e937ccddc191f951d3acf8cc3f2e9ca06"
   end
 
   def install
