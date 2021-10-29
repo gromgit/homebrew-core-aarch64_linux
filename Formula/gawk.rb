6cc79e86862d56d37e6731af6da686861e8db33b
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-29 04:21:39 +0000

    gawk: update 5.1.1 bottle.

--- Formula/gawk.rb
+++ Formula/gawk.rb
@@ -7,10 +7,12 @@ class Gawk < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "efc88dd4e2c2d87eaddb7aed2487eb17128e056ce47ce117f234a287e0e7160e"
-    sha256 big_sur:       "f909760ee429b1b41478900af7245c57cffe0a31f76b651c86e08cd1b6bcbc4d"
-    sha256 catalina:      "1bfbe650e0ef014e64d5380558c21a450aff7196f06596ff89484b185777dc90"
-    sha256 x86_64_linux:  "e62547695b5737c52a3174813091354f5ef1ef6e1369401f5f72367278caf578"
+    sha256 arm64_monterey: "093465f34b94ec8ddeb4ff8dab2a02dafbccf8ec05f6ef0391673b7c4fd0a91f"
+    sha256 arm64_big_sur:  "efc88dd4e2c2d87eaddb7aed2487eb17128e056ce47ce117f234a287e0e7160e"
+    sha256 monterey:       "2dcae063cbf93dd82b36be9d9aaf08644831a5f9efd304af768b3b59f7db5192"
+    sha256 big_sur:        "f909760ee429b1b41478900af7245c57cffe0a31f76b651c86e08cd1b6bcbc4d"
+    sha256 catalina:       "1bfbe650e0ef014e64d5380558c21a450aff7196f06596ff89484b185777dc90"
+    sha256 x86_64_linux:   "e62547695b5737c52a3174813091354f5ef1ef6e1369401f5f72367278caf578"
   end
 
   depends_on "gettext"
