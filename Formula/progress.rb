049e2e6cca16d868c97dfbcd1554a13dc760ba06
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 06:47:59 +0000

    progress: update 0.16 bottle.

--- Formula/progress.rb
+++ Formula/progress.rb
@@ -7,11 +7,13 @@ class Progress < Formula
   head "https://github.com/Xfennec/progress.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "97407ff56c65c8f77371ce073478868eb507742db82c11e107a43413bce646c3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "21a1663abf8e1a60baf99a3ae46d41883cd5136fabbe77dd07987e0730b08ec0"
-    sha256 cellar: :any_skip_relocation, catalina:      "804256c3be440464694fe8f0e9b94f323860f9fa2304c8984dc0e8cbbce43fab"
-    sha256 cellar: :any_skip_relocation, mojave:        "5c834228aa86062624e1d2f6f3c386607d3151842218eb9c70529e1485ee1e5b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31559faf7e06989812f6ea8069a8baf8de1b1abcd68c41dba11f5f2004595f65"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "95538fd7595dfdb2d046961264df824fd0ad908afaf732b6bbd2e5ddf13af2ea"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "97407ff56c65c8f77371ce073478868eb507742db82c11e107a43413bce646c3"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a8e247b7fd7ba0c6bd3556739b283d4609ee32ce82b986637a6d1d823a9cf31"
+    sha256 cellar: :any_skip_relocation, big_sur:        "21a1663abf8e1a60baf99a3ae46d41883cd5136fabbe77dd07987e0730b08ec0"
+    sha256 cellar: :any_skip_relocation, catalina:       "804256c3be440464694fe8f0e9b94f323860f9fa2304c8984dc0e8cbbce43fab"
+    sha256 cellar: :any_skip_relocation, mojave:         "5c834228aa86062624e1d2f6f3c386607d3151842218eb9c70529e1485ee1e5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "31559faf7e06989812f6ea8069a8baf8de1b1abcd68c41dba11f5f2004595f65"
   end
 
   uses_from_macos "ncurses"
