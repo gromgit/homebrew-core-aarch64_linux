ffaf93f4f370d30d0aa02adb6743b06b0dcc237f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 07:36:29 +0000

    libnfc: update 1.8.0 bottle.

--- Formula/libnfc.rb
+++ Formula/libnfc.rb
@@ -6,12 +6,14 @@ class Libnfc < Formula
   license "LGPL-3.0"
 
   bottle do
-    sha256 arm64_big_sur: "6e97d8892b2129437513be8a21fccf7e3c6a23b14dd28e3d43aea1fce9b97ed7"
-    sha256 big_sur:       "566a81b623abfb5d68480274b59b13c44fc098cd1d8cbf59dc112295a58a363c"
-    sha256 catalina:      "6659f67e40774cdb8e95548c03542bbc123ccabc0f4a6160504c03e43fa43c26"
-    sha256 mojave:        "9bc90c84f89408a8960289a668af7ad9b7b17d34a02996b83ec960c5cbefafeb"
-    sha256 high_sierra:   "8e6abd4d61ef9aff76ef25b092806b95614c07a9e46a0e13ca6e915271454a92"
-    sha256 x86_64_linux:  "db84cf74f8217a9cb32aa5c804cf20c9b74464ce21ea2a87805b0c8de5abdfe6"
+    sha256 arm64_monterey: "24d476cf0560256e53b5efb4f915ead0e5a5bf336da89395a3b8a5c0903f1caf"
+    sha256 arm64_big_sur:  "6e97d8892b2129437513be8a21fccf7e3c6a23b14dd28e3d43aea1fce9b97ed7"
+    sha256 monterey:       "a42411e1b19e52e85c138f4566613bc87570851403d148315d384d953d2a82b5"
+    sha256 big_sur:        "566a81b623abfb5d68480274b59b13c44fc098cd1d8cbf59dc112295a58a363c"
+    sha256 catalina:       "6659f67e40774cdb8e95548c03542bbc123ccabc0f4a6160504c03e43fa43c26"
+    sha256 mojave:         "9bc90c84f89408a8960289a668af7ad9b7b17d34a02996b83ec960c5cbefafeb"
+    sha256 high_sierra:    "8e6abd4d61ef9aff76ef25b092806b95614c07a9e46a0e13ca6e915271454a92"
+    sha256 x86_64_linux:   "db84cf74f8217a9cb32aa5c804cf20c9b74464ce21ea2a87805b0c8de5abdfe6"
   end
 
   head do
