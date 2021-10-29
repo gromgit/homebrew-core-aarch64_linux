c6a66a637a3268aa9adfa4c727dab685b87c743a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 09:18:45 +0000

    inxi: update 3.3.08-1 bottle.

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -7,9 +7,11 @@ class Inxi < Formula
   head "https://github.com/smxi/inxi.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3020e98c06282771e24069218af450f06a34be75e872de5ede0785f474881b2e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ead85103f91c94113b2970a4758f14bba35b5ec8508e0107268a38bee719c624"
-    sha256 cellar: :any_skip_relocation, catalina:      "ead85103f91c94113b2970a4758f14bba35b5ec8508e0107268a38bee719c624"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3020e98c06282771e24069218af450f06a34be75e872de5ede0785f474881b2e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3020e98c06282771e24069218af450f06a34be75e872de5ede0785f474881b2e"
+    sha256 cellar: :any_skip_relocation, monterey:       "ead85103f91c94113b2970a4758f14bba35b5ec8508e0107268a38bee719c624"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ead85103f91c94113b2970a4758f14bba35b5ec8508e0107268a38bee719c624"
+    sha256 cellar: :any_skip_relocation, catalina:       "ead85103f91c94113b2970a4758f14bba35b5ec8508e0107268a38bee719c624"
   end
 
   def install
