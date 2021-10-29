b6dd7a868a9db43e59f5d6529e9004f536560025
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 20:41:43 +0000

    defaultbrowser: update 1.1 bottle.

--- Formula/defaultbrowser.rb
+++ Formula/defaultbrowser.rb
@@ -6,13 +6,15 @@ class Defaultbrowser < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "19e88d5316731019db1bafd3361af53fe3a4dd40348ac5a30bf34754b250f992"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e796471951ee1290e11172aea1fff0b59c70cdbbfef43303bf11a3178e676a7f"
-    sha256 cellar: :any_skip_relocation, catalina:      "e03bfa37fde424b0d7e76e6d2f99a26bad458e9d2bdf912db06d83d64bfe5a17"
-    sha256 cellar: :any_skip_relocation, mojave:        "3a02a1fd0321f5070fa4ec9088a4a58a28ddb561bef5b94a0ccc31fdb896efa6"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "d07cc35e06f440584d698cde7ae5f27b09acfeafacc1499d331aa8553e945961"
-    sha256 cellar: :any_skip_relocation, sierra:        "d0279f8c05c0c7828c534517897346fc231864ea8534f0ae04878ecfd51ca72d"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "f0ccf84abbd31469f80c4d232292dd280a978d3f04a1a6db46079902d9821d1e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fdbee46ea9cdc0bf6adbea67eed257663c77d745af838f4a2e71f69987242d6c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "19e88d5316731019db1bafd3361af53fe3a4dd40348ac5a30bf34754b250f992"
+    sha256 cellar: :any_skip_relocation, monterey:       "cb836a15dc466342c11bb7cdf35370b261b77723d81438664f89e84513a06f8f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e796471951ee1290e11172aea1fff0b59c70cdbbfef43303bf11a3178e676a7f"
+    sha256 cellar: :any_skip_relocation, catalina:       "e03bfa37fde424b0d7e76e6d2f99a26bad458e9d2bdf912db06d83d64bfe5a17"
+    sha256 cellar: :any_skip_relocation, mojave:         "3a02a1fd0321f5070fa4ec9088a4a58a28ddb561bef5b94a0ccc31fdb896efa6"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "d07cc35e06f440584d698cde7ae5f27b09acfeafacc1499d331aa8553e945961"
+    sha256 cellar: :any_skip_relocation, sierra:         "d0279f8c05c0c7828c534517897346fc231864ea8534f0ae04878ecfd51ca72d"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "f0ccf84abbd31469f80c4d232292dd280a978d3f04a1a6db46079902d9821d1e"
   end
 
   depends_on :macos
