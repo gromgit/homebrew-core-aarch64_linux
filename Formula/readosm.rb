a3c3c3e1e9d803e92601e10074dbcc83354f06e9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 17:02:22 +0000

    readosm: update 1.1.0a bottle.

--- Formula/readosm.rb
+++ Formula/readosm.rb
@@ -11,10 +11,12 @@ class Readosm < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bd41553e655ddd0efb25350087b8247102f308e40e20de46274a703beee4a1de"
-    sha256 cellar: :any, big_sur:       "6f0a6b5f33f57429ed7d4608cf6819d85b829468abd7c954c381a599c8c73647"
-    sha256 cellar: :any, catalina:      "2ea6c35bdfab9c28d9a5bc8a87e5306cbec6be17c26b1ad6f63ca70207a332a5"
-    sha256 cellar: :any, mojave:        "fcc1af52f7c13bfe4b3df0e1ca559ab79cee172c8941f51a335fb0fbb505027f"
+    sha256 cellar: :any, arm64_monterey: "938d6d422d3eb547b702f3bbeb547b1a6879f37782d72c705475a61fede5c780"
+    sha256 cellar: :any, arm64_big_sur:  "bd41553e655ddd0efb25350087b8247102f308e40e20de46274a703beee4a1de"
+    sha256 cellar: :any, monterey:       "37ac4df09be8730582ff8b11bf3480145eabdd697bf3335f80e7c6629b7bc74e"
+    sha256 cellar: :any, big_sur:        "6f0a6b5f33f57429ed7d4608cf6819d85b829468abd7c954c381a599c8c73647"
+    sha256 cellar: :any, catalina:       "2ea6c35bdfab9c28d9a5bc8a87e5306cbec6be17c26b1ad6f63ca70207a332a5"
+    sha256 cellar: :any, mojave:         "fcc1af52f7c13bfe4b3df0e1ca559ab79cee172c8941f51a335fb0fbb505027f"
   end
 
   def install
