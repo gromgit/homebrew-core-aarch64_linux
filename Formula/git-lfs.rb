abb9a970a95965ca77f4f87a8857f0d068d88523
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-29 04:21:42 +0000

    git-lfs: update 3.0.2 bottle.

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -6,10 +6,12 @@ class GitLfs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "064d030a26d7928685602c3fbd6daeb30e4d81b3a94f86aa3ddd2ffe1bd8396c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a2cac31dfa6ded45a6633c730816551bd3e4810a2b7fe3cdc414a830a02e7c10"
-    sha256 cellar: :any_skip_relocation, catalina:      "6f86300bf487d24aace589b4c78521b6dce3f834e660b926d81dd28544d561e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c8824cdcda78651cfd106352af9ae9834b8c4996081a9cb35f564afd1139689"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c29212adc03440123d3732cecc321bdf8e7e946ee13580e00882c8f2e415c567"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "064d030a26d7928685602c3fbd6daeb30e4d81b3a94f86aa3ddd2ffe1bd8396c"
+    sha256 cellar: :any_skip_relocation, monterey:       "79dc4e28ac75d8ee526215b10c16d17873009a9cb8fcfd50c92069868a01e2ef"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a2cac31dfa6ded45a6633c730816551bd3e4810a2b7fe3cdc414a830a02e7c10"
+    sha256 cellar: :any_skip_relocation, catalina:       "6f86300bf487d24aace589b4c78521b6dce3f834e660b926d81dd28544d561e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c8824cdcda78651cfd106352af9ae9834b8c4996081a9cb35f564afd1139689"
   end
 
   depends_on "go" => :build
