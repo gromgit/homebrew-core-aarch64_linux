86578512c6ade108e942f79e7013ee5845814caf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 06:47:17 +0000

    ugrep: update 3.3.8 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -6,10 +6,12 @@ class Ugrep < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_big_sur: "f8f26c21ec1426657aa979e17e06c2cb48e99c24d1a8fa1acf6652909d946d2c"
-    sha256 big_sur:       "44c0d50709b4471ce59b7c09c2e3382b6f812663495bfc21451229cea83cf843"
-    sha256 catalina:      "2b652b54a6f73d89acb9f35198fa02c6fff4ade61acd9bbb6bcb9e2d3be44a9b"
-    sha256 x86_64_linux:  "50cfc84f06777701ad8e204018d59352b9f6a7c30c2929d9c0c65b2afc4c28ff"
+    sha256 arm64_monterey: "d5385c90812d894a44c42fdaec04b8c82092cb350286cb4bea915230c9cdcddb"
+    sha256 arm64_big_sur:  "f8f26c21ec1426657aa979e17e06c2cb48e99c24d1a8fa1acf6652909d946d2c"
+    sha256 monterey:       "031a53dcbc5765babe25399b261057c7db3046f2229251929fce3d959b81b8a4"
+    sha256 big_sur:        "44c0d50709b4471ce59b7c09c2e3382b6f812663495bfc21451229cea83cf843"
+    sha256 catalina:       "2b652b54a6f73d89acb9f35198fa02c6fff4ade61acd9bbb6bcb9e2d3be44a9b"
+    sha256 x86_64_linux:   "50cfc84f06777701ad8e204018d59352b9f6a7c30c2929d9c0c65b2afc4c28ff"
   end
 
   depends_on "pcre2"
