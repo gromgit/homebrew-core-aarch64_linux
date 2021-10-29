30997caf9816cbe1998265d3d9344665e9b06e20
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 20:43:23 +0000

    html-xml-utils: update 8.0 bottle.

--- Formula/html-xml-utils.rb
+++ Formula/html-xml-utils.rb
@@ -11,11 +11,13 @@ class HtmlXmlUtils < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c16a3cfc6c495dc7dcfd4c91d783468ab0b2d06080a50ff9d2ee22a005a549af"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d091c52a06b99739740d1d3c293bb76cf648a892f973b51d05dbba8224981637"
-    sha256 cellar: :any_skip_relocation, catalina:      "8963544c153ee5d40ea25a7f28ecee4f6fcf7e631e7ea294b5b92c551e0848da"
-    sha256 cellar: :any_skip_relocation, mojave:        "528fa00404d4a9b5b14afa4df46ea6ae9bd74b984a6f21b747bcbfb4b3773360"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38dd92ab8c435790ffbd448a74a89562050fbcfa0f2af0dad4cf1aa34ca7a541"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3c9cb3059c92b42a37509759221e9b0b9fe06f2e2a55efdfdd1846c2ab56a0fc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c16a3cfc6c495dc7dcfd4c91d783468ab0b2d06080a50ff9d2ee22a005a549af"
+    sha256 cellar: :any_skip_relocation, monterey:       "3958e53da4cd35845b47355772423daee28f2f52243f2381371fe770241d9994"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d091c52a06b99739740d1d3c293bb76cf648a892f973b51d05dbba8224981637"
+    sha256 cellar: :any_skip_relocation, catalina:       "8963544c153ee5d40ea25a7f28ecee4f6fcf7e631e7ea294b5b92c551e0848da"
+    sha256 cellar: :any_skip_relocation, mojave:         "528fa00404d4a9b5b14afa4df46ea6ae9bd74b984a6f21b747bcbfb4b3773360"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38dd92ab8c435790ffbd448a74a89562050fbcfa0f2af0dad4cf1aa34ca7a541"
   end
 
   def install
