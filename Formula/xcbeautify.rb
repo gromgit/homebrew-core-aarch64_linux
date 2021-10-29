777ff88827375ae059784669d62d04f83bcce760
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 10:03:52 +0000

    xcbeautify: update 0.9.1 bottle.

--- Formula/xcbeautify.rb
+++ Formula/xcbeautify.rb
@@ -8,9 +8,11 @@ class Xcbeautify < Formula
   head "https://github.com/thii/xcbeautify.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "07f7226da16337b9947871edf3e6112e042d9371872eea692877cd83e3f4e18c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1b95942893695e6ea3d30c17064939161f6e573243cc92035e930b4e1a39deef"
-    sha256 cellar: :any_skip_relocation, catalina:      "5b053728e2ee73f3542cf2b7178af1a5d309970f5374fd1342da93e6dfcb57af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5572f364b24076c89fe78d7128d8abb198a5041561b7dab0312c2050e3390624"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "07f7226da16337b9947871edf3e6112e042d9371872eea692877cd83e3f4e18c"
+    sha256 cellar: :any_skip_relocation, monterey:       "6b181df5d9351b281176906c09009e991b014baf16f0f88d4680012d7715a52d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1b95942893695e6ea3d30c17064939161f6e573243cc92035e930b4e1a39deef"
+    sha256 cellar: :any_skip_relocation, catalina:       "5b053728e2ee73f3542cf2b7178af1a5d309970f5374fd1342da93e6dfcb57af"
   end
 
   depends_on xcode: ["11.4", :build]
