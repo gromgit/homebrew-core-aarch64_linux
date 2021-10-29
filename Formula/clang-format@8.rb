78cb451e8623378004ad0b26c38aa43532366dc6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 22:28:06 +0000

    clang-format@8: update 8.0.1_1 bottle.

--- Formula/clang-format@8.rb
+++ Formula/clang-format@8.rb
@@ -7,10 +7,12 @@ class ClangFormatAT8 < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7f7277da699e1b3c7f20c256c25093b5833e164b52134d2697979b6c29fb7757"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c019f2355036a45a2e084fe1c5a7253225c2abb1c0c330f46e41d05f619106fe"
-    sha256 cellar: :any_skip_relocation, catalina:      "30b5274aa2f2fc590ac2bd4e7152c40c5fd76ec779703cd3898b5e2d46d563f8"
-    sha256 cellar: :any_skip_relocation, mojave:        "d1fa0fa103bb53196f1289b2587e35e615bf3d4bf0b1c71f32a5f66effd3726a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "487c96e15eb0d1460df9453b4ebf3dc2332601f39fd476df9d182cb006b7c16d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f7277da699e1b3c7f20c256c25093b5833e164b52134d2697979b6c29fb7757"
+    sha256 cellar: :any_skip_relocation, monterey:       "d529cbffdef8a529c7c7f23b3be5aba8e0da29fc5b8aa2ffacddc29fa99b18d5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c019f2355036a45a2e084fe1c5a7253225c2abb1c0c330f46e41d05f619106fe"
+    sha256 cellar: :any_skip_relocation, catalina:       "30b5274aa2f2fc590ac2bd4e7152c40c5fd76ec779703cd3898b5e2d46d563f8"
+    sha256 cellar: :any_skip_relocation, mojave:         "d1fa0fa103bb53196f1289b2587e35e615bf3d4bf0b1c71f32a5f66effd3726a"
   end
 
   depends_on "cmake" => :build
