6df03cb562bebfbdd5b2c67ad7d48721752b989c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 05:05:24 +0000

    libnotify: update 0.7.9 bottle.

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -14,12 +14,14 @@ class Libnotify < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "49e795c1869eb3f544ef5710861ec3ef0f829f6b8db64b3c44722025e2c4ba97"
-    sha256 cellar: :any, big_sur:       "415ef3754d6910255fc161e352b0b5a7006efe3aa5684fbf8abeb98514358562"
-    sha256 cellar: :any, catalina:      "367a8d51cb565452392b9bc92c753ca641c23f91fc4ff93fb6166b63f2beafda"
-    sha256 cellar: :any, mojave:        "e6d5a6a87f885bf421e6a70c9cef1c6aaf89db46a98216af6c06754246a8f896"
-    sha256 cellar: :any, high_sierra:   "0560e601843a3e42a4823904dd5534212efd823292444a9588f1cf99ea8bc8f5"
-    sha256               x86_64_linux:  "3be2be401ff1fda07663fdeaf8929ca12b92fa136acfedc2a273b1abcd9438bd"
+    sha256 cellar: :any, arm64_monterey: "050ec9423bfe7bfaef51c4f173692d55758a9e52a6df70dff96a667e3ed4bc75"
+    sha256 cellar: :any, arm64_big_sur:  "49e795c1869eb3f544ef5710861ec3ef0f829f6b8db64b3c44722025e2c4ba97"
+    sha256 cellar: :any, monterey:       "6c4586e332260bdf0db438be5ec2194d9831aaef30a53ce661352bb05f361ee4"
+    sha256 cellar: :any, big_sur:        "415ef3754d6910255fc161e352b0b5a7006efe3aa5684fbf8abeb98514358562"
+    sha256 cellar: :any, catalina:       "367a8d51cb565452392b9bc92c753ca641c23f91fc4ff93fb6166b63f2beafda"
+    sha256 cellar: :any, mojave:         "e6d5a6a87f885bf421e6a70c9cef1c6aaf89db46a98216af6c06754246a8f896"
+    sha256 cellar: :any, high_sierra:    "0560e601843a3e42a4823904dd5534212efd823292444a9588f1cf99ea8bc8f5"
+    sha256               x86_64_linux:   "3be2be401ff1fda07663fdeaf8929ca12b92fa136acfedc2a273b1abcd9438bd"
   end
 
   depends_on "docbook-xsl" => :build
