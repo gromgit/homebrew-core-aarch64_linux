b3efbc8d5817529243a6b4c6f0a9cc4a9b68f788
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 19:43:25 +0000

    hfstospell: update 0.5.2_2 bottle.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -12,11 +12,13 @@ class Hfstospell < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "1a5437ebb7e8abeae096734d53edbbc8cf154f6635f8e15ac3a1cfa038782e85"
-    sha256 cellar: :any,                 big_sur:       "6fb2851153c12aa38ed01a7335781df78be3490380e6713b2a9c642f88e737d0"
-    sha256 cellar: :any,                 catalina:      "0651d2057fcf3c0242bcd277b0ddafb247c0f00fc78d2652e9eae9c82776f923"
-    sha256 cellar: :any,                 mojave:        "25a4f7bfe15fae7efd0ce6cf1ccedb150571935de5e0266cbf7fa472290bbf6d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "558374d12fffddfe01a121b42e9e4edaaceeea1afdd6fdb6283672488b70eb96"
+    sha256 cellar: :any,                 arm64_monterey: "fc339fb27f8405230d88addd9ccf93b1ebdfa00fcc013fb38e3e327061b56b8b"
+    sha256 cellar: :any,                 arm64_big_sur:  "1a5437ebb7e8abeae096734d53edbbc8cf154f6635f8e15ac3a1cfa038782e85"
+    sha256 cellar: :any,                 monterey:       "49d6d65cff8b16516e2d0b570af2d37ec6fe592b6221504f0a28055aafe3a50a"
+    sha256 cellar: :any,                 big_sur:        "6fb2851153c12aa38ed01a7335781df78be3490380e6713b2a9c642f88e737d0"
+    sha256 cellar: :any,                 catalina:       "0651d2057fcf3c0242bcd277b0ddafb247c0f00fc78d2652e9eae9c82776f923"
+    sha256 cellar: :any,                 mojave:         "25a4f7bfe15fae7efd0ce6cf1ccedb150571935de5e0266cbf7fa472290bbf6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "558374d12fffddfe01a121b42e9e4edaaceeea1afdd6fdb6283672488b70eb96"
   end
 
   depends_on "autoconf" => :build
