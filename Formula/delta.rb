cb6d612f1cdee2a48790a86c1b795edd89eb6235
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 06:03:03 +0000

    delta: update 2006.08.03 bottle.

--- Formula/delta.rb
+++ Formula/delta.rb
@@ -6,14 +6,16 @@ class Delta < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3cf5598409d5234d1c9c810a36cb3559b8b8ccdeb3593071435c3f6e7af7f64c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "92e77d238cf7180260e1c176e7716760d3438a2b3dc713a64b2873c2a24ecfdd"
-    sha256 cellar: :any_skip_relocation, catalina:      "244dfd6407c2b65ad33ca707b8642f51d5f63c8056ddd45baf5bc3734dc545ec"
-    sha256 cellar: :any_skip_relocation, mojave:        "a6116fb7212cb2271b5c73c1bb53f51aeb33bfcc734e77bd42396a968744a42c"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "46734f3eb952455ecd9237ce455aebb3e66be791bbf190021d894dae39d55b66"
-    sha256 cellar: :any_skip_relocation, sierra:        "07e775a1054966ad2924512386643bc8cb4ef3ad7e12ce9a140015c82fba3072"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "202409012500969cfd034c9d44c441a809445a3b367d514357346438aa850f14"
-    sha256 cellar: :any_skip_relocation, yosemite:      "d3374cc3e84c93bb84615b1669503ea8b708ab65baf629ee0be9a728b12b10bc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a929cf7113873c9fb9d4f9d05fa8e522299a617586c3c4f7bc756629b4eb77d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3cf5598409d5234d1c9c810a36cb3559b8b8ccdeb3593071435c3f6e7af7f64c"
+    sha256 cellar: :any_skip_relocation, monterey:       "bab82bd6e0d4cd2b4815a06cc7854c38d7d957fbad255168c29c0deb5022ec40"
+    sha256 cellar: :any_skip_relocation, big_sur:        "92e77d238cf7180260e1c176e7716760d3438a2b3dc713a64b2873c2a24ecfdd"
+    sha256 cellar: :any_skip_relocation, catalina:       "244dfd6407c2b65ad33ca707b8642f51d5f63c8056ddd45baf5bc3734dc545ec"
+    sha256 cellar: :any_skip_relocation, mojave:         "a6116fb7212cb2271b5c73c1bb53f51aeb33bfcc734e77bd42396a968744a42c"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "46734f3eb952455ecd9237ce455aebb3e66be791bbf190021d894dae39d55b66"
+    sha256 cellar: :any_skip_relocation, sierra:         "07e775a1054966ad2924512386643bc8cb4ef3ad7e12ce9a140015c82fba3072"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "202409012500969cfd034c9d44c441a809445a3b367d514357346438aa850f14"
+    sha256 cellar: :any_skip_relocation, yosemite:       "d3374cc3e84c93bb84615b1669503ea8b708ab65baf629ee0be9a728b12b10bc"
   end
 
   conflicts_with "git-delta", because: "both install a `delta` binary"
