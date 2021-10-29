91f2d146e40ad50515d090c7bac6b1d25bb1b02b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 08:44:18 +0000

    gnu-cobol: update 3.1.2 bottle.

--- Formula/gnu-cobol.rb
+++ Formula/gnu-cobol.rb
@@ -12,10 +12,12 @@ class GnuCobol < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "56a9a4dedd7cac8608aa2c570d6e3c77647cc5a15235413eef2fc5ff7f4c698e"
-    sha256 big_sur:       "bc73094fd113c6dc58c3cc475c78c8ec4dac1d9459895ab8ba23ff8f1974df34"
-    sha256 catalina:      "ed671ad5c7cabc4992d399cdc02a5bdda5ead3d273d307dcff68eaa9204f3447"
-    sha256 mojave:        "d0c71a8b125011452f7e47411ba743021a6d0edeb477a267fc905abd81b1a561"
+    sha256 arm64_monterey: "8486284406cb96f0b17a7a61226db7fc57726711852737c4c500e1b1b089e279"
+    sha256 arm64_big_sur:  "56a9a4dedd7cac8608aa2c570d6e3c77647cc5a15235413eef2fc5ff7f4c698e"
+    sha256 monterey:       "10d0dd94d0f10d121fb3910bfe7fbdfd006bf4a4f22f8f8ece72ae6c5371d00f"
+    sha256 big_sur:        "bc73094fd113c6dc58c3cc475c78c8ec4dac1d9459895ab8ba23ff8f1974df34"
+    sha256 catalina:       "ed671ad5c7cabc4992d399cdc02a5bdda5ead3d273d307dcff68eaa9204f3447"
+    sha256 mojave:         "d0c71a8b125011452f7e47411ba743021a6d0edeb477a267fc905abd81b1a561"
   end
 
   depends_on "berkeley-db"
