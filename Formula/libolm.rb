5a5064f21147eb0d12536142908791099206f1ab
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 17:21:38 +0000

    libolm: update 3.2.6 bottle.

--- Formula/libolm.rb
+++ Formula/libolm.rb
@@ -6,11 +6,13 @@ class Libolm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a0d40c19dde560b6c429ede66a7c549f46c36f58872a055e6fcacb24b565d72f"
-    sha256 cellar: :any,                 big_sur:       "9b0fa6f2b8bcf26a480b43cf8fd38bdd474f2296d1a13a378228468811ba3876"
-    sha256 cellar: :any,                 catalina:      "dbc06704bf32fedbfd2c8aad88ba9d504e44d055c553d22677298ec877f40c8a"
-    sha256 cellar: :any,                 mojave:        "be9653954751491a58c54ea5f9c0e6284af4975a8d09f0c4248d14ec735e0d5b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3fcdb358514800ef0cf2248b20b36e57ee1faf258936514a05b2d821e5322bfd"
+    sha256 cellar: :any,                 arm64_monterey: "245688a835df1a73a40e477c19cc8874f52b36e56731af9381f3513f7e71ea97"
+    sha256 cellar: :any,                 arm64_big_sur:  "a0d40c19dde560b6c429ede66a7c549f46c36f58872a055e6fcacb24b565d72f"
+    sha256 cellar: :any,                 monterey:       "5dc746624fa4f5cc0613bca33f65ded6dfae709e0c987278cd326104a31947d1"
+    sha256 cellar: :any,                 big_sur:        "9b0fa6f2b8bcf26a480b43cf8fd38bdd474f2296d1a13a378228468811ba3876"
+    sha256 cellar: :any,                 catalina:       "dbc06704bf32fedbfd2c8aad88ba9d504e44d055c553d22677298ec877f40c8a"
+    sha256 cellar: :any,                 mojave:         "be9653954751491a58c54ea5f9c0e6284af4975a8d09f0c4248d14ec735e0d5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3fcdb358514800ef0cf2248b20b36e57ee1faf258936514a05b2d821e5322bfd"
   end
 
   depends_on "cmake" => :build
