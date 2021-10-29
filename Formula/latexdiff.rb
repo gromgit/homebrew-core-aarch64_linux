cd2184889e39589da42a189a20d12d439178cfa8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 17:17:51 +0000

    latexdiff: update 1.3.1.1 bottle.

--- Formula/latexdiff.rb
+++ Formula/latexdiff.rb
@@ -6,12 +6,14 @@ class Latexdiff < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "90e14ec7fe78b06d74521a9d8ff102589d63935e70f40cfcb7af63d7dbdd0d43"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9ea5219166059a04ab1f510d92339e2443f883a114384d5f8af30b2631ce4921"
-    sha256 cellar: :any_skip_relocation, catalina:      "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
-    sha256 cellar: :any_skip_relocation, mojave:        "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5fd8db27743a46025e5dbd9ab31181fbdff44648e1b12e850e4c88878e466391"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5fd8db27743a46025e5dbd9ab31181fbdff44648e1b12e850e4c88878e466391"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "90e14ec7fe78b06d74521a9d8ff102589d63935e70f40cfcb7af63d7dbdd0d43"
+    sha256 cellar: :any_skip_relocation, monterey:       "d47104ab0a06e597cea157f9a902d5fa2df1123e195bc4778d611cb1916119fe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9ea5219166059a04ab1f510d92339e2443f883a114384d5f8af30b2631ce4921"
+    sha256 cellar: :any_skip_relocation, catalina:       "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
+    sha256 cellar: :any_skip_relocation, mojave:         "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fd8db27743a46025e5dbd9ab31181fbdff44648e1b12e850e4c88878e466391"
   end
 
   # osx default perl cause compilation error
