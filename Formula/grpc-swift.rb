5226a338811a91292025f64c6afc8dcd8665dcb4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 20:50:25 +0000

    grpc-swift: update 1.5.0 bottle.

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -7,10 +7,12 @@ class GrpcSwift < Formula
   head "https://github.com/grpc/grpc-swift.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "442e5ec9bbab09b0d5486f9d241cd88369f79a188831e6e2450d885fd213231d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3e545a1d2751ad6b2dea6369b9da778364b879eb54796044423fb4a2c2d2b245"
-    sha256 cellar: :any_skip_relocation, catalina:      "7c6b804c466e2f8d5245e9efd2749c57ece2a4f1dc8f30c2fd88774eaa9dace8"
-    sha256                               x86_64_linux:  "24dd2bb28f194eae0629720b07ef4e8fd2f5ad31fd32ba854ab8cbcc63a3ac42"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3fec127f5e67589e99c1fb4aa146632512676bfc2de3fa82ddca739ac4cb5e40"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "442e5ec9bbab09b0d5486f9d241cd88369f79a188831e6e2450d885fd213231d"
+    sha256 cellar: :any_skip_relocation, monterey:       "b3ef832720781eec5ef6f5fe13f9a8bce3d048d533c4ebaff1361124a312e45a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3e545a1d2751ad6b2dea6369b9da778364b879eb54796044423fb4a2c2d2b245"
+    sha256 cellar: :any_skip_relocation, catalina:       "7c6b804c466e2f8d5245e9efd2749c57ece2a4f1dc8f30c2fd88774eaa9dace8"
+    sha256                               x86_64_linux:   "24dd2bb28f194eae0629720b07ef4e8fd2f5ad31fd32ba854ab8cbcc63a3ac42"
   end
 
   depends_on xcode: ["12.0", :build]
