fa0baed1d6f27aed30814a9691934cddecd9ef6d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 18:48:38 +0000

    a2ps: update 4.14 bottle.

--- Formula/a2ps.rb
+++ Formula/a2ps.rb
@@ -8,11 +8,13 @@ class A2ps < Formula
 
   bottle do
     rebuild 4
-    sha256 arm64_big_sur: "8ac02041dbec3966b6a695dfc4215b90b9e331ae6eb8c6698cbbfa0175154c9f"
-    sha256 big_sur:       "e87da2b47386fc7e3c6f20b3ff90c4bbe37b9e0aaa884440ffa216492dbc150b"
-    sha256 catalina:      "82e64b2008971430d160a3f564e32593e98fb55c43d7748c7deb9d6f546e1102"
-    sha256 mojave:        "8ca49b4797277f79e87e48ab4c6794601b64d1dde35b9eac556d4153b8237a51"
-    sha256 x86_64_linux:  "063b4b31a62c4d5bd905bc4faab09ac2a50c77291de52ab216fc6a7a56f8e406"
+    sha256 arm64_monterey: "b92375f7cc49a7440b431d2248cad0d97c96fcca127dace6efdeb0b2f3faa08c"
+    sha256 arm64_big_sur:  "8ac02041dbec3966b6a695dfc4215b90b9e331ae6eb8c6698cbbfa0175154c9f"
+    sha256 monterey:       "c0347849efe7486dfa2c5cfd35fae4c87e194fdcd9a10c6ce8758c99e8cf144c"
+    sha256 big_sur:        "e87da2b47386fc7e3c6f20b3ff90c4bbe37b9e0aaa884440ffa216492dbc150b"
+    sha256 catalina:       "82e64b2008971430d160a3f564e32593e98fb55c43d7748c7deb9d6f546e1102"
+    sha256 mojave:         "8ca49b4797277f79e87e48ab4c6794601b64d1dde35b9eac556d4153b8237a51"
+    sha256 x86_64_linux:   "063b4b31a62c4d5bd905bc4faab09ac2a50c77291de52ab216fc6a7a56f8e406"
   end
 
   uses_from_macos "gperf"
