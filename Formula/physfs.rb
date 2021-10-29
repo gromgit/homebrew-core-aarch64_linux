b1ad7ce6b7fb143b90f5f5dc451cf17712b6c816
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 17:42:52 +0000

    physfs: update 3.0.2 bottle.

--- Formula/physfs.rb
+++ Formula/physfs.rb
@@ -13,11 +13,13 @@ class Physfs < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any,                 arm64_big_sur: "065d120b86dd681aa4fb20c874456b1fbbae3b8428e2051cea9f49b9da01dceb"
-    sha256 cellar: :any,                 big_sur:       "f2348a828a9f32b6fdb78278c5ecd86c7f7bb4abf27032478b44cd4db6338b0c"
-    sha256 cellar: :any,                 catalina:      "be794e8986be384f98e3d4d14a4fe3830428084febea0caff4bba5c363e890c6"
-    sha256 cellar: :any,                 mojave:        "03f4a5a5ed440e3b39e91af11ac4470f07ce742f844d188bca3e58becfd24f3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9be8022f4ef1cc79e8bbf71a4df0c7589ace1162f316b6c481f49ce8c67d3dc"
+    sha256 cellar: :any,                 arm64_monterey: "cab1caaa3f55144dbadb72e9a959862b33acd2901b2adb7231818e293a0b5d28"
+    sha256 cellar: :any,                 arm64_big_sur:  "065d120b86dd681aa4fb20c874456b1fbbae3b8428e2051cea9f49b9da01dceb"
+    sha256 cellar: :any,                 monterey:       "4d466b0a2b62169961a13f0f88e0392dd5662ecbfed5e9efa39ef5375b22f284"
+    sha256 cellar: :any,                 big_sur:        "f2348a828a9f32b6fdb78278c5ecd86c7f7bb4abf27032478b44cd4db6338b0c"
+    sha256 cellar: :any,                 catalina:       "be794e8986be384f98e3d4d14a4fe3830428084febea0caff4bba5c363e890c6"
+    sha256 cellar: :any,                 mojave:         "03f4a5a5ed440e3b39e91af11ac4470f07ce742f844d188bca3e58becfd24f3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9be8022f4ef1cc79e8bbf71a4df0c7589ace1162f316b6c481f49ce8c67d3dc"
   end
 
   depends_on "cmake" => :build
