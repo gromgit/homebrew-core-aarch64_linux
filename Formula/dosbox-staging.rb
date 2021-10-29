2765fb69e383d4973d338945191f8c41821f659b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 22:05:22 +0000

    dosbox-staging: update 0.77.1 bottle.

--- Formula/dosbox-staging.rb
+++ Formula/dosbox-staging.rb
@@ -7,10 +7,12 @@ class DosboxStaging < Formula
   head "https://github.com/dosbox-staging/dosbox-staging.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7dd4f286588301620a44495e5810974739ab4fd32cccd5b5f2396bdab0553a23"
-    sha256 cellar: :any, big_sur:       "e2e63248f71bed815d467f3afecf49983470a3579490dd7ff60afbae1383625d"
-    sha256 cellar: :any, catalina:      "01c1cf45fb8d75aeb5b81174bc02693093c047ca138ae3cf1a880c5ad1ca35eb"
-    sha256 cellar: :any, mojave:        "e56e6203ef35b2402396029baba105d93127531dcc92acf5182edf06ee747f2c"
+    sha256 cellar: :any, arm64_monterey: "0a398c0d996c59dfd8fc0f15f431ae0cfb81c8cfaefa652321f3e81a7e2afae3"
+    sha256 cellar: :any, arm64_big_sur:  "7dd4f286588301620a44495e5810974739ab4fd32cccd5b5f2396bdab0553a23"
+    sha256 cellar: :any, monterey:       "b0f8a49984c9cff4a3dcc5f1314d62b8ea1faab5b5cfb74fdfc606bc2a6ab837"
+    sha256 cellar: :any, big_sur:        "e2e63248f71bed815d467f3afecf49983470a3579490dd7ff60afbae1383625d"
+    sha256 cellar: :any, catalina:       "01c1cf45fb8d75aeb5b81174bc02693093c047ca138ae3cf1a880c5ad1ca35eb"
+    sha256 cellar: :any, mojave:         "e56e6203ef35b2402396029baba105d93127531dcc92acf5182edf06ee747f2c"
   end
 
   depends_on "meson" => :build
