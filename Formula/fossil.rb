0a6f2ff4f150613ee6446ff3daaff7c35f1c386a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-29 08:27:07 +0000

    fossil: update 2.17 bottle.

--- Formula/fossil.rb
+++ Formula/fossil.rb
@@ -12,11 +12,13 @@ class Fossil < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "465292116e1bb204aefa96b56c1350c08af43c2963975ef7a06a71f278637858"
-    sha256 cellar: :any,                 big_sur:       "e4ab11e3b0739b51fa4ce5ef8b020df84704c2050a52b1a89a804008ad9fb9d8"
-    sha256 cellar: :any,                 catalina:      "c2464646ca8502f36e67f1a0730d93192e6ddf0eb1b113690c1a64689f4a8d4e"
-    sha256 cellar: :any,                 mojave:        "a0b10c2b031c7c54b51bf919fcd1d79c0f7047804851192af34b4d2c1e377bc8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8b5b3ad3e442b166240b72299c66eb22b989d47b5e06ca96a13e2527be1dc48"
+    sha256 cellar: :any,                 arm64_monterey: "0d253775540cedf23dfb2d3135f7a147be6aafcef658ae9567eec0f702254fe4"
+    sha256 cellar: :any,                 arm64_big_sur:  "465292116e1bb204aefa96b56c1350c08af43c2963975ef7a06a71f278637858"
+    sha256 cellar: :any,                 monterey:       "22187e1f35814920ea0df4139f2c5f0e60a5b249f7f186345b1791a9a8b5af64"
+    sha256 cellar: :any,                 big_sur:        "e4ab11e3b0739b51fa4ce5ef8b020df84704c2050a52b1a89a804008ad9fb9d8"
+    sha256 cellar: :any,                 catalina:       "c2464646ca8502f36e67f1a0730d93192e6ddf0eb1b113690c1a64689f4a8d4e"
+    sha256 cellar: :any,                 mojave:         "a0b10c2b031c7c54b51bf919fcd1d79c0f7047804851192af34b4d2c1e377bc8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b8b5b3ad3e442b166240b72299c66eb22b989d47b5e06ca96a13e2527be1dc48"
   end
 
   depends_on "openssl@1.1"
