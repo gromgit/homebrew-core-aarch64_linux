16f17367709f643e3b2d9618c8ed45ce0b8c89aa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:16:29 +0000

    quill: update 1.6.3 bottle.

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -7,11 +7,13 @@ class Quill < Formula
   head "https://github.com/odygrd/quill.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "20314cbc7129a86e7431687ba26d26ba0add0e115e80536b02dfcf5398217097"
-    sha256 cellar: :any_skip_relocation, big_sur:       "22f64135866f9600feb5495d4a8b58b29ef1f7c02de8edafe50960b2595910aa"
-    sha256 cellar: :any_skip_relocation, catalina:      "2112f5965f599db294f9e2c2cfb86acf39b8482120c181cf841ee2a569e7c30e"
-    sha256 cellar: :any_skip_relocation, mojave:        "d8a4190b8461b8860363d96a0623e4ad75a1da416a21bcc48a00340f50ce41ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3daaf537147bfa374930cb088a271cb735d42a12f208727680e578eef62a0f38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d43aba895c3307a5965b4cdb34124967af8f748c5da80d7d9b0842c88d979eed"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "20314cbc7129a86e7431687ba26d26ba0add0e115e80536b02dfcf5398217097"
+    sha256 cellar: :any_skip_relocation, monterey:       "94577e70e360724ae8e3fa750d6f210e4b14ebcf5eb946caf051dafda0276cb5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "22f64135866f9600feb5495d4a8b58b29ef1f7c02de8edafe50960b2595910aa"
+    sha256 cellar: :any_skip_relocation, catalina:       "2112f5965f599db294f9e2c2cfb86acf39b8482120c181cf841ee2a569e7c30e"
+    sha256 cellar: :any_skip_relocation, mojave:         "d8a4190b8461b8860363d96a0623e4ad75a1da416a21bcc48a00340f50ce41ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3daaf537147bfa374930cb088a271cb735d42a12f208727680e578eef62a0f38"
   end
 
   depends_on "cmake" => :build
