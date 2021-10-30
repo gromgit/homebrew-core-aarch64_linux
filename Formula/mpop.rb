b34363e01fdfb9bf20126b9093a67abf1656e30e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:52:09 +0000

    mpop: update 1.4.16 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -11,10 +11,12 @@ class Mpop < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "fb83315e0e06e5586ef99f2dcbabc7cf248744cd72c736615c08c6c93f5ac809"
-    sha256 big_sur:       "9b42850fdeb9d94c15228333ec8537b0f39ce90f5ad799729706113b97d59ee3"
-    sha256 catalina:      "16273c6c9296a2aeb6724a6c0c023c2c83ae8304f90c78cd8efbd9dacd3475c6"
-    sha256 x86_64_linux:  "a14a672f256617064258a4d0b1dbe96b30e39fb13aef5c1c6397873463766a26"
+    sha256 arm64_monterey: "762d29a253ab63afea52f49ef0380a6530e6261e3050361206e9122e5a4e3c49"
+    sha256 arm64_big_sur:  "fb83315e0e06e5586ef99f2dcbabc7cf248744cd72c736615c08c6c93f5ac809"
+    sha256 monterey:       "ddef337f0853edbcbfe4fb7d408eb5cfd3ccd2f459b645ef510a21e49093a6a7"
+    sha256 big_sur:        "9b42850fdeb9d94c15228333ec8537b0f39ce90f5ad799729706113b97d59ee3"
+    sha256 catalina:       "16273c6c9296a2aeb6724a6c0c023c2c83ae8304f90c78cd8efbd9dacd3475c6"
+    sha256 x86_64_linux:   "a14a672f256617064258a4d0b1dbe96b30e39fb13aef5c1c6397873463766a26"
   end
 
   depends_on "pkg-config" => :build
