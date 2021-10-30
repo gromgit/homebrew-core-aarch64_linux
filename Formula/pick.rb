3615ef0c5c47154618128b89f62a40b72cd0a07b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 14:33:52 +0000

    pick: update 4.0.0 bottle.

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -7,11 +7,13 @@ class Pick < Formula
   head "https://github.com/mptre/pick.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "032d06aa754000e281f773bb857266efc79e1762e8f689617778a19e17505688"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c8da7b41b502c8c72b90fd41bf1570e840198fa6678cc5efca8a1c26a8d5557f"
-    sha256 cellar: :any_skip_relocation, catalina:      "754879e53b48743051bb1571bb4b6180a415ac36af8deaf335f5c193326d232f"
-    sha256 cellar: :any_skip_relocation, mojave:        "55596e8ab28fd4fc36d064f6395c38ce51314bcc0d2f2f3862515a683bc92182"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "0fc521881c760d4f9e4f8625795716e0e1c0e1ed1522ccb5efd055313b2729bc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3978f455a6bf9cba97b215e8a71a4f314eb48bfd0a920ef307e46c5c3bf3e186"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "032d06aa754000e281f773bb857266efc79e1762e8f689617778a19e17505688"
+    sha256 cellar: :any_skip_relocation, monterey:       "d9bfbabddcd3e479420308af573f56ac530386d110c95044a65e64f4ef7c8f28"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c8da7b41b502c8c72b90fd41bf1570e840198fa6678cc5efca8a1c26a8d5557f"
+    sha256 cellar: :any_skip_relocation, catalina:       "754879e53b48743051bb1571bb4b6180a415ac36af8deaf335f5c193326d232f"
+    sha256 cellar: :any_skip_relocation, mojave:         "55596e8ab28fd4fc36d064f6395c38ce51314bcc0d2f2f3862515a683bc92182"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "0fc521881c760d4f9e4f8625795716e0e1c0e1ed1522ccb5efd055313b2729bc"
   end
 
   uses_from_macos "ncurses"
