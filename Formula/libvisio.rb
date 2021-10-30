3662fb33eb0c849532b367b348515efc40ef3bec
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 19:43:13 +0000

    libvisio: update 0.1.7_4 bottle.

--- Formula/libvisio.rb
+++ Formula/libvisio.rb
@@ -11,10 +11,12 @@ class Libvisio < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9dbc60f38f0b2cf07e5df55fcc4a67d9d31a8698cfdc8f411b269d1e3daae0bf"
-    sha256 cellar: :any, big_sur:       "09a9e29742cf5667ad9f935a7d686641b54383c1ff8d5581c1a7c145a966760d"
-    sha256 cellar: :any, catalina:      "e431e0e6e96f2ac918d37e4e10f264cf23b7af0dbc54d3f82ab80b309efbd079"
-    sha256 cellar: :any, mojave:        "af67f5b493aebbe8c072fd0254e47bd8b205a00eb29049ea38046a9065fe8876"
+    sha256 cellar: :any, arm64_monterey: "eb2af5e8a4c4c1289e4b8e1db77de18ff78c99b0f6ed68ffed3d6c3a2cb0abc9"
+    sha256 cellar: :any, arm64_big_sur:  "9dbc60f38f0b2cf07e5df55fcc4a67d9d31a8698cfdc8f411b269d1e3daae0bf"
+    sha256 cellar: :any, monterey:       "131e5afcfde4ba63620ca6820c657209e1f4c51dca3b95cc3db4bae23bdddb0a"
+    sha256 cellar: :any, big_sur:        "09a9e29742cf5667ad9f935a7d686641b54383c1ff8d5581c1a7c145a966760d"
+    sha256 cellar: :any, catalina:       "e431e0e6e96f2ac918d37e4e10f264cf23b7af0dbc54d3f82ab80b309efbd079"
+    sha256 cellar: :any, mojave:         "af67f5b493aebbe8c072fd0254e47bd8b205a00eb29049ea38046a9065fe8876"
   end
 
   depends_on "cppunit" => :build
