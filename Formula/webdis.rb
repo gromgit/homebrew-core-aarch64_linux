065f24fd22c637648fa0037936b9245a9721023d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 17:53:18 +0000

    webdis: update 0.1.18 bottle.

--- Formula/webdis.rb
+++ Formula/webdis.rb
@@ -6,10 +6,12 @@ class Webdis < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "7b6fb29239bd1ecc823c627b2aed17b17a3a747369900883c298dda7658266c9"
-    sha256 cellar: :any,                 big_sur:       "3b514fbabc1805463147d6e57e8714a61beea7c562a4543489f62a9f0eb29b34"
-    sha256 cellar: :any,                 catalina:      "42647e2db028c4e74ab5a8ec031e267c724bc1d876ff5549ac1ac764fdd746ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e1c131f0f7b34e1108fc2bcdacb752e152dda92f5bf6022d71083e4665d53b5"
+    sha256 cellar: :any,                 arm64_monterey: "f8c83408882f42a23810229c011a1b2c42a855ca8d115802645cbdf02af5e376"
+    sha256 cellar: :any,                 arm64_big_sur:  "7b6fb29239bd1ecc823c627b2aed17b17a3a747369900883c298dda7658266c9"
+    sha256 cellar: :any,                 monterey:       "87f7ff9e6292332d39e2861c4755a839e35f70ed100a1c2055ff351dea7703bb"
+    sha256 cellar: :any,                 big_sur:        "3b514fbabc1805463147d6e57e8714a61beea7c562a4543489f62a9f0eb29b34"
+    sha256 cellar: :any,                 catalina:       "42647e2db028c4e74ab5a8ec031e267c724bc1d876ff5549ac1ac764fdd746ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e1c131f0f7b34e1108fc2bcdacb752e152dda92f5bf6022d71083e4665d53b5"
   end
 
   depends_on "libevent"
