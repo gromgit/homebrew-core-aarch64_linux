fc6dfd0e81bcea44cc8f4759d2872e65b45391aa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:37:43 +0000

    liblcf: update 0.6.2_3 bottle.

--- Formula/liblcf.rb
+++ Formula/liblcf.rb
@@ -8,11 +8,13 @@ class Liblcf < Formula
   head "https://github.com/EasyRPG/liblcf.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ac4128d58f95e92dbb494d02fd3a9c75f41f024c37bf5225e5b4dc551bbd207b"
-    sha256 cellar: :any,                 big_sur:       "4324dce9a80a86cbd12fa12f73719cf5a9710f42d7b5d71e29d87fd4179f685c"
-    sha256 cellar: :any,                 catalina:      "928d1095b1b008b0416636501459f0ff7bd22d8b69eef75ea9e4c151dafbe703"
-    sha256 cellar: :any,                 mojave:        "0391e77bd5cefbdfdda6ba603a01e8c206b21acb662649f2e602d66e6f9401ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34b0eea36554d5c51a76a48fbb100a302a0986fc38d8cee84df55864c2e57775"
+    sha256 cellar: :any,                 arm64_monterey: "595a24c5830a441b172f1b4df67e4ea738fd056a1104df95a606aaa88ace14bd"
+    sha256 cellar: :any,                 arm64_big_sur:  "ac4128d58f95e92dbb494d02fd3a9c75f41f024c37bf5225e5b4dc551bbd207b"
+    sha256 cellar: :any,                 monterey:       "99eff2d4ab4c5bf8b1bc1fb0838336119cfa63d9e056d99f2ef30674fa1e0f74"
+    sha256 cellar: :any,                 big_sur:        "4324dce9a80a86cbd12fa12f73719cf5a9710f42d7b5d71e29d87fd4179f685c"
+    sha256 cellar: :any,                 catalina:       "928d1095b1b008b0416636501459f0ff7bd22d8b69eef75ea9e4c151dafbe703"
+    sha256 cellar: :any,                 mojave:         "0391e77bd5cefbdfdda6ba603a01e8c206b21acb662649f2e602d66e6f9401ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "34b0eea36554d5c51a76a48fbb100a302a0986fc38d8cee84df55864c2e57775"
   end
 
   depends_on "pkg-config" => :build
