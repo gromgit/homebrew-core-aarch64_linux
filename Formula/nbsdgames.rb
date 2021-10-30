78f26245ff42429fdba44e87e6c3d5435e21ce65
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 13:45:46 +0000

    nbsdgames: update 4.1.2 bottle.

--- Formula/nbsdgames.rb
+++ Formula/nbsdgames.rb
@@ -7,11 +7,13 @@ class Nbsdgames < Formula
   head "https://github.com/abakh/nbsdgames.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4281d8274d3f05106a5e34a31db7498615e2b4ac89e52528a864d8d4c2fa0999"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d30a3395191ecada03c1c7c18b0c82e6833a35fdf47f5a4c98fb708e89eede2a"
-    sha256 cellar: :any_skip_relocation, catalina:      "6fc7bda5fa3519d9a4c241396a0ea512e252dc43e06289059b52e8017adef885"
-    sha256 cellar: :any_skip_relocation, mojave:        "88cebe1f55579632df4606eb0f9e86f4488dee8cc2b108e97a0f3bd90a6c3f90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0323c032e3636a122be2af5faca46694b70a6072e334a42996068abccde6fb7c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f27112c26f752624c594d5ab67af18de7e9138b985cfc4113ce293b7e9b91c58"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4281d8274d3f05106a5e34a31db7498615e2b4ac89e52528a864d8d4c2fa0999"
+    sha256 cellar: :any_skip_relocation, monterey:       "bbf1f0908c51d75fd78cbc5cd301c8f586e11d3357ff552dbfe61ab802cf8bdb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d30a3395191ecada03c1c7c18b0c82e6833a35fdf47f5a4c98fb708e89eede2a"
+    sha256 cellar: :any_skip_relocation, catalina:       "6fc7bda5fa3519d9a4c241396a0ea512e252dc43e06289059b52e8017adef885"
+    sha256 cellar: :any_skip_relocation, mojave:         "88cebe1f55579632df4606eb0f9e86f4488dee8cc2b108e97a0f3bd90a6c3f90"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0323c032e3636a122be2af5faca46694b70a6072e334a42996068abccde6fb7c"
   end
 
   uses_from_macos "ncurses"
