8089c0e3026062972aaf4d008d4c9c8857bae3b4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 17:33:33 +0000

    plzip: update 1.9 bottle.

--- Formula/plzip.rb
+++ Formula/plzip.rb
@@ -12,11 +12,13 @@ class Plzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9d197045d0de19677e6ce0079afb3cfb94e54360aa2c00cda3bf5a74031e6b40"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6c79b456b6b1ea19d12efe0a1087f4b36092787a94cedacdfeb1186294e1f72b"
-    sha256 cellar: :any_skip_relocation, catalina:      "9f278923d8c1d12bebbfa37b52816b85b8f4cc49e67ef7dc37de7ff1af9ea4ab"
-    sha256 cellar: :any_skip_relocation, mojave:        "fe9addff91b4833212aa8107a92f1775af9cc7d5d96de1466917c3f0a51ee912"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b54241076d7ac3e03f00823d18c069e9d52dbfaab3a0da8926e7ac955b055663"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "00894ac8632cd219e49242c93d4f0405d36358f084b74f64589e4be15e612e59"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d197045d0de19677e6ce0079afb3cfb94e54360aa2c00cda3bf5a74031e6b40"
+    sha256 cellar: :any_skip_relocation, monterey:       "4ebdd421bc438eea1ffbd5e26a69418cde8483a164eaaf0698ae827bdde5c31a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6c79b456b6b1ea19d12efe0a1087f4b36092787a94cedacdfeb1186294e1f72b"
+    sha256 cellar: :any_skip_relocation, catalina:       "9f278923d8c1d12bebbfa37b52816b85b8f4cc49e67ef7dc37de7ff1af9ea4ab"
+    sha256 cellar: :any_skip_relocation, mojave:         "fe9addff91b4833212aa8107a92f1775af9cc7d5d96de1466917c3f0a51ee912"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b54241076d7ac3e03f00823d18c069e9d52dbfaab3a0da8926e7ac955b055663"
   end
 
   depends_on "lzlib"
