4bb95f5c66e3a6e294d1a7cad3cf20704b80267b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 01:01:37 +0000

    bsdmake: update 24 bottle.

--- Formula/bsdmake.rb
+++ Formula/bsdmake.rb
@@ -12,14 +12,16 @@ class Bsdmake < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "e844047b809f9c3d3c297baebfcaaab0e5e0ee4cc993d4a8bb81c1db3ad372df"
-    sha256 big_sur:       "a7540422211c370618f938241419971aa6542298a0735a8e369b612c1f639866"
-    sha256 catalina:      "85bdfdf2ca2e5761195c4781e52232e4fb1258c99bf79f46cf82f66338197df3"
-    sha256 mojave:        "acee008d57c2ebe6ad2ee5932d1521a254e16453c61cdd517da2c675f60c1eb4"
-    sha256 high_sierra:   "fa009c31c9fa5fc71f774cfe146f1338ca856158a606b796c3a1e7dbd64f3895"
-    sha256 sierra:        "3d5b8c21cf86cd6bb9eb28d1e8cbec434b370aa15e19540e366d045ea807c8c8"
-    sha256 el_capitan:    "b4052277ac6cf79ed579107fb73da96954c350d7bf29a124c55d87a0df8940b0"
-    sha256 yosemite:      "18d7cb56f14eb2e404cf3abb163a354f57c5e45b72991efdd6566a15fcffe90f"
+    sha256 arm64_monterey: "8471df063ebf08e34ecc83482f48ac0758e62535f1046a83659ad83e0aecdedc"
+    sha256 arm64_big_sur:  "e844047b809f9c3d3c297baebfcaaab0e5e0ee4cc993d4a8bb81c1db3ad372df"
+    sha256 monterey:       "7ac963918bb1d8bf0a6be631304c0962179dc98348a94480e39c3024502abcb0"
+    sha256 big_sur:        "a7540422211c370618f938241419971aa6542298a0735a8e369b612c1f639866"
+    sha256 catalina:       "85bdfdf2ca2e5761195c4781e52232e4fb1258c99bf79f46cf82f66338197df3"
+    sha256 mojave:         "acee008d57c2ebe6ad2ee5932d1521a254e16453c61cdd517da2c675f60c1eb4"
+    sha256 high_sierra:    "fa009c31c9fa5fc71f774cfe146f1338ca856158a606b796c3a1e7dbd64f3895"
+    sha256 sierra:         "3d5b8c21cf86cd6bb9eb28d1e8cbec434b370aa15e19540e366d045ea807c8c8"
+    sha256 el_capitan:     "b4052277ac6cf79ed579107fb73da96954c350d7bf29a124c55d87a0df8940b0"
+    sha256 yosemite:       "18d7cb56f14eb2e404cf3abb163a354f57c5e45b72991efdd6566a15fcffe90f"
   end
 
   # MacPorts patches to make bsdmake play nice with our prefix system
