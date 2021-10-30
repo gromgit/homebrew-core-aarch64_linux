a2edc04c8037d4b15b96f731c36e86339f1f929d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:49:36 +0000

    vamp-plugin-sdk: update 2.10.0 bottle.

--- Formula/vamp-plugin-sdk.rb
+++ Formula/vamp-plugin-sdk.rb
@@ -17,12 +17,14 @@ class VampPluginSdk < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "aa6184c469e855de77725477097a0c6998a04d4753bc852aa756123edaac446c"
-    sha256 cellar: :any,                 big_sur:       "21e590739905e6794c11e4f7037adfa6fa83da4d7c2ab2b083c43563449d8a45"
-    sha256 cellar: :any,                 catalina:      "b31926ceedbd7f79dc9783da8092b543c549d800705d9d8e8d8d0fd451d093de"
-    sha256 cellar: :any,                 mojave:        "ee8d69d0b8c72e3e9ed1c79bfa7ca6650d10e36a2b110215b3d803f841ae2ec0"
-    sha256 cellar: :any,                 high_sierra:   "834812edc745c782511f1397fb5e3e6995b9fd25b42426ec784cd5610dbc9eb4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d07d32893d2d362347c137dfb133d7d2bd0ef0c5815b618f5cf2780de28f40d"
+    sha256 cellar: :any,                 arm64_monterey: "e10a03b4cbf89602757ef2f2bd43757bc7a734d3c200ca2a4258b304c05e9351"
+    sha256 cellar: :any,                 arm64_big_sur:  "aa6184c469e855de77725477097a0c6998a04d4753bc852aa756123edaac446c"
+    sha256 cellar: :any,                 monterey:       "2e24031117d9bed00ea07b4bb2b3ca4c09305a001647a2a256928f2e29e2a999"
+    sha256 cellar: :any,                 big_sur:        "21e590739905e6794c11e4f7037adfa6fa83da4d7c2ab2b083c43563449d8a45"
+    sha256 cellar: :any,                 catalina:       "b31926ceedbd7f79dc9783da8092b543c549d800705d9d8e8d8d0fd451d093de"
+    sha256 cellar: :any,                 mojave:         "ee8d69d0b8c72e3e9ed1c79bfa7ca6650d10e36a2b110215b3d803f841ae2ec0"
+    sha256 cellar: :any,                 high_sierra:    "834812edc745c782511f1397fb5e3e6995b9fd25b42426ec784cd5610dbc9eb4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d07d32893d2d362347c137dfb133d7d2bd0ef0c5815b618f5cf2780de28f40d"
   end
 
   depends_on "automake" => :build
