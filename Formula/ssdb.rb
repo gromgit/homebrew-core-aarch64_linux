b9d0a199bb10c97023ba2b9e3fa68730a9e4a2b6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:54:48 +0000

    ssdb: update 1.9.9 bottle.

--- Formula/ssdb.rb
+++ Formula/ssdb.rb
@@ -8,6 +8,7 @@ class Ssdb < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey:     "5251016f1ba03a184424fc041e61501398577ca0c1aceac23748e22bc8883f5f"
     sha256 cellar: :any_skip_relocation, big_sur:      "f92e221d20ca1a85c7ae555acd1417bba60b208a56091eb3a25d98fc788f25a3"
     sha256 cellar: :any_skip_relocation, catalina:     "4253e51c8e447b5d4e0ec5f064ee2fcc3ef57b30734df70f3b24d6399abb9363"
     sha256 cellar: :any_skip_relocation, mojave:       "a10edecc28880cd37e02e75fdc318392ba6bda016f624181a9f4ff10982b211f"
