1f40b5774b78845be5f62aed26e4c84019c0de3d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:43:17 +0000

    nvc: update 1.5.2 bottle.

--- Formula/nvc.rb
+++ Formula/nvc.rb
@@ -6,10 +6,12 @@ class Nvc < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "ed81b83101417314e47d4c9e27c1efc2cd5aa8b2f6d360d16ff4ff4a39693dd7"
-    sha256 big_sur:       "878fc444b7f6694cbb12ec4d1f6e644d1c7f853d109915ab49c5e77b151100ee"
-    sha256 catalina:      "fe88883631d30f010b06fc8c7ff7f5ae7cd8a67cdcde8e28f262ad4f1fcb3e29"
-    sha256 mojave:        "5d42447afee153063a563b0b32d8d2a98c955aff3860bbbe30d994d8c82f4dac"
+    sha256 arm64_monterey: "68d9ff0d508f2090a14e94985c8930dbf5433225e50f0f79bcc1c9ca6b5f18e5"
+    sha256 arm64_big_sur:  "ed81b83101417314e47d4c9e27c1efc2cd5aa8b2f6d360d16ff4ff4a39693dd7"
+    sha256 monterey:       "052b1a76869e0f8be567dee7d1244097542df07e2a6fcff4d43a3ff0cfb62718"
+    sha256 big_sur:        "878fc444b7f6694cbb12ec4d1f6e644d1c7f853d109915ab49c5e77b151100ee"
+    sha256 catalina:       "fe88883631d30f010b06fc8c7ff7f5ae7cd8a67cdcde8e28f262ad4f1fcb3e29"
+    sha256 mojave:         "5d42447afee153063a563b0b32d8d2a98c955aff3860bbbe30d994d8c82f4dac"
   end
 
   head do
