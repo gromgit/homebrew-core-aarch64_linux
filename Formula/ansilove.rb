f54caebc3142188aa5fd3328c3e4825f8143f6b9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 17:44:14 +0000

    ansilove: update 4.1.5 bottle.

--- Formula/ansilove.rb
+++ Formula/ansilove.rb
@@ -6,11 +6,13 @@ class Ansilove < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ce746bff57bf9c61f3dae89126257d741593d68587d740f7267ef5e1b2a3a111"
-    sha256 cellar: :any,                 big_sur:       "6b82fee266c64e4738ebef22c745f3a457dccad114a285bc85972651c2545c26"
-    sha256 cellar: :any,                 catalina:      "78487b299437decd72a5a2152caf81ba4997e9300016032ed449981cfc174641"
-    sha256 cellar: :any,                 mojave:        "d40386a9e4ab3b22d87f894f1a018bd481592c26792fe95ea86fc918e4f3c26f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f25e133bbae1b11f1422da43fd27da11bf42cd3d2caf68e92024143726d24d02"
+    sha256 cellar: :any,                 arm64_monterey: "8153bad43ca6f245a5266ada677be1b53a6aa3436113849360ea7f28eefd723a"
+    sha256 cellar: :any,                 arm64_big_sur:  "ce746bff57bf9c61f3dae89126257d741593d68587d740f7267ef5e1b2a3a111"
+    sha256 cellar: :any,                 monterey:       "95fbd757366e5ec61fb261941e109d8e5462d82c84dd9e174da73ff7bc83e074"
+    sha256 cellar: :any,                 big_sur:        "6b82fee266c64e4738ebef22c745f3a457dccad114a285bc85972651c2545c26"
+    sha256 cellar: :any,                 catalina:       "78487b299437decd72a5a2152caf81ba4997e9300016032ed449981cfc174641"
+    sha256 cellar: :any,                 mojave:         "d40386a9e4ab3b22d87f894f1a018bd481592c26792fe95ea86fc918e4f3c26f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f25e133bbae1b11f1422da43fd27da11bf42cd3d2caf68e92024143726d24d02"
   end
 
   depends_on "cmake" => :build
