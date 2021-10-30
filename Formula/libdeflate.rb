efac2a4f7f3c6882dae13c38e096b198b1d75a4d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 01:06:34 +0000

    libdeflate: update 1.8 bottle.

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -6,11 +6,13 @@ class Libdeflate < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "663669870ff0bde818c9d35e24d6dbb4c0601d61b81576e34e18931b1f4badfe"
-    sha256 cellar: :any,                 big_sur:       "c984576da43b8e60e2f263cbdc13c176a42958b1c1e168ca8bd49505042b67d0"
-    sha256 cellar: :any,                 catalina:      "083f6b73565408c7a3fb01d92dd917364ab0345125741d8e811963ff74949ade"
-    sha256 cellar: :any,                 mojave:        "7c9e2f9ef003ee75d5639b04ca25b7abe97d3d7548bf9f1d60b13e7071cbf1c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b307828767aa51938d7f552e7c6ba69473338907bce68e6cf561e9302a1c36e"
+    sha256 cellar: :any,                 arm64_monterey: "753a9e821ec87092fd731b04b073937a126a41776eeb638c1d7858c448575e99"
+    sha256 cellar: :any,                 arm64_big_sur:  "663669870ff0bde818c9d35e24d6dbb4c0601d61b81576e34e18931b1f4badfe"
+    sha256 cellar: :any,                 monterey:       "885468709de7f330635973aba560e918fd50c73321f652d060e4ecc828789782"
+    sha256 cellar: :any,                 big_sur:        "c984576da43b8e60e2f263cbdc13c176a42958b1c1e168ca8bd49505042b67d0"
+    sha256 cellar: :any,                 catalina:       "083f6b73565408c7a3fb01d92dd917364ab0345125741d8e811963ff74949ade"
+    sha256 cellar: :any,                 mojave:         "7c9e2f9ef003ee75d5639b04ca25b7abe97d3d7548bf9f1d60b13e7071cbf1c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9b307828767aa51938d7f552e7c6ba69473338907bce68e6cf561e9302a1c36e"
   end
 
   def install
