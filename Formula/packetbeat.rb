25511604b9eb78b0935c1ffd66c4cadde968e95b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 00:11:28 +0000

    packetbeat: update 7.15.1 bottle.

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -8,11 +8,13 @@ class Packetbeat < Formula
   head "https://github.com/elastic/beats.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fcd9f4ba3761b81d7d075658b85705cac9ba0d56cbded58ff4c309a0481e80f0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0878d3d9e02b2e1fc6d5ffb9676c2750561f58049733cdfad21eb244c46fb103"
-    sha256 cellar: :any_skip_relocation, catalina:      "d47797ee81211e73dac7a616e55800a75738852b6a4522213d15a0c7d60e8169"
-    sha256 cellar: :any_skip_relocation, mojave:        "342ed845e196eb6975bd40add28c79cd41864d2ab6b75595a18fa293b12c49f7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aece0f09b8386966a81f835c6792068a6d38ce0386ee53c39973826a194e98e0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fa49c7f261d2b66e744c67f48ab597dc8063fd75928513dd339295cac92292c3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fcd9f4ba3761b81d7d075658b85705cac9ba0d56cbded58ff4c309a0481e80f0"
+    sha256 cellar: :any_skip_relocation, monterey:       "d2b00c39c0a46517075efd1d6983eada77dd4cd7d57ba1c2009a906975ad7b78"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0878d3d9e02b2e1fc6d5ffb9676c2750561f58049733cdfad21eb244c46fb103"
+    sha256 cellar: :any_skip_relocation, catalina:       "d47797ee81211e73dac7a616e55800a75738852b6a4522213d15a0c7d60e8169"
+    sha256 cellar: :any_skip_relocation, mojave:         "342ed845e196eb6975bd40add28c79cd41864d2ab6b75595a18fa293b12c49f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aece0f09b8386966a81f835c6792068a6d38ce0386ee53c39973826a194e98e0"
   end
 
   depends_on "go" => :build
