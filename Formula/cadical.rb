f337668452d26f287dda31b853c1dcd6f1c6fdbb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 00:46:41 +0000

    cadical: update 1.4.1 bottle.

--- Formula/cadical.rb
+++ Formula/cadical.rb
@@ -11,11 +11,13 @@ class Cadical < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ec7b790e83fbf0a79bdb56d42bd60b1bd84a0956628d6c6de9c41f692f99e5ae"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fc095f38bfc3402adecdb2e33f76353ef95248f8bf25cd1731fc89c93070f7f1"
-    sha256 cellar: :any_skip_relocation, catalina:      "1a5c3d28eeaf54b30f08b4ab5f27bf9c0ac366d4cb4f5003fe04d71ef11a26e2"
-    sha256 cellar: :any_skip_relocation, mojave:        "c88a8df9110d4996b3f5bd00f3227e6031aeaf89c8ad652d5b8412f7ef441dd5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a316fb060adcad424dfa1f09b5c7d4ca40b5ead8aec27422478e9a26b6216069"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "443f4f79c5f7139f30c257db3cfedb2767ea94e5ee70ab3ac8e3d6ac28f398a4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ec7b790e83fbf0a79bdb56d42bd60b1bd84a0956628d6c6de9c41f692f99e5ae"
+    sha256 cellar: :any_skip_relocation, monterey:       "4b02db022aa66f0a024dc5d8812c6ac17c0d04d537f77b52424b3ca02a4fefa1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fc095f38bfc3402adecdb2e33f76353ef95248f8bf25cd1731fc89c93070f7f1"
+    sha256 cellar: :any_skip_relocation, catalina:       "1a5c3d28eeaf54b30f08b4ab5f27bf9c0ac366d4cb4f5003fe04d71ef11a26e2"
+    sha256 cellar: :any_skip_relocation, mojave:         "c88a8df9110d4996b3f5bd00f3227e6031aeaf89c8ad652d5b8412f7ef441dd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a316fb060adcad424dfa1f09b5c7d4ca40b5ead8aec27422478e9a26b6216069"
   end
 
   def install
