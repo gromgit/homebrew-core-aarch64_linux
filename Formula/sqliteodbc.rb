99e23c84d3c75024747a471f7ce518dcf4bee24c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 08:00:11 +0000

    sqliteodbc: update 0.9998 bottle.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -10,6 +10,7 @@ class Sqliteodbc < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 monterey:     "df3e249a7780ffe6178fd754655a6313c4526c9f6eb06cf169c5776a669d749b"
     sha256 cellar: :any,                 big_sur:      "5f98876aef9733997e750451ee0e3db30cc2bd1f371aa690f08d7e4038f11958"
     sha256 cellar: :any,                 catalina:     "d0105cc73d44561e636923adb520710cdd7e0db835c6b31f151fe8a66a1b4fcc"
     sha256 cellar: :any,                 mojave:       "6499af774d13212bf19dfdbd14c18feadf516a5d6afbd2ebe7718d99db1723eb"
