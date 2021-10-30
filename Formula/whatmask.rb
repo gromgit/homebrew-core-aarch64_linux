9db8f925fbd01e503a441faca3216d671fc7790c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 17:25:50 +0000

    whatmask: update 1.2 bottle.

--- Formula/whatmask.rb
+++ Formula/whatmask.rb
@@ -7,11 +7,13 @@ class Whatmask < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a5bf6f569bef04d197a6eb0c097450e65dcb5082b65ecc82201e15eb873ae755"
-    sha256 cellar: :any_skip_relocation, big_sur:       "55789adc6a9326b814965c6c0fcf41f912638f2e7d55d4167cbe404ec1a6938d"
-    sha256 cellar: :any_skip_relocation, catalina:      "89a44972f8d27003b4c91f04a294f0be9a0d00628fb8db21faf46a55a0720cb2"
-    sha256 cellar: :any_skip_relocation, mojave:        "a3a5a8887d1c7d43f83bf99c2f81f8900af0d83091978f5aac28447d0f093785"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8bb2ab87b646751414698bd1bae28cbea1eecf7c147ae6a38fd02e9f3857a1c9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "93e5d651f3376c9194a94a3afa5dc7860bbe55ac675339e6c2d9951f57d1b075"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5bf6f569bef04d197a6eb0c097450e65dcb5082b65ecc82201e15eb873ae755"
+    sha256 cellar: :any_skip_relocation, monterey:       "dedc8e95cb750f18d6b8f03505387a17cc88753810f4e052b30b33126d9a8b8b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "55789adc6a9326b814965c6c0fcf41f912638f2e7d55d4167cbe404ec1a6938d"
+    sha256 cellar: :any_skip_relocation, catalina:       "89a44972f8d27003b4c91f04a294f0be9a0d00628fb8db21faf46a55a0720cb2"
+    sha256 cellar: :any_skip_relocation, mojave:         "a3a5a8887d1c7d43f83bf99c2f81f8900af0d83091978f5aac28447d0f093785"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bb2ab87b646751414698bd1bae28cbea1eecf7c147ae6a38fd02e9f3857a1c9"
   end
 
   depends_on "autoconf" => :build
