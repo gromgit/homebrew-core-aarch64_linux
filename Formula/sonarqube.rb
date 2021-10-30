2586fe65139936e9e2fc727e009ee0de1f9a298b
VonUniGE <VonUniGE@github.com>
2021-10-31 00:05:57 +0200

    sonarqube: rebuild
    * sonarqube: allow to run on Apple Silicon
    * Use Java Service Wrapper 3.5.46
    * Use binaries from Homebrew built java-service-wrapper
    * Fix brew audit
    * Use Java Service Wrapper 3.5.46 for all platforms
    * Order dependencies correctly
    * Link needed files from java-service-wrapper
      Java Service Wrapper doesn't support the  wrapper binary to be symlinked, so it's copied
    
    Closes #88380.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sonarqube.rb
+++ Formula/sonarqube.rb
@@ -16,13 +16,27 @@ class Sonarqube < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5bd7b5e142d30a1caf30979e5e077f462e076540c514505d354df4625e2be67d"
   end
 
-  # sonarqube ships pre-built x86_64 binaries
-  depends_on arch: :x86_64
+  depends_on "java-service-wrapper"
   depends_on "openjdk@11"
 
   conflicts_with "sonarqube-lts", because: "both install the same binaries"
 
   def install
+    # Use Java Service Wrapper 3.5.46 which is Apple Silicon compatible
+    # Java Service Wrapper doesn't support the  wrapper binary to be symlinked, so it's copied
+    jsw_libexec = Formula["java-service-wrapper"].opt_libexec
+    ln_s jsw_libexec/"lib/wrapper.jar", "#{buildpath}/lib/jsw/wrapper-3.5.46.jar"
+    ln_s jsw_libexec/"lib/libwrapper.dylib", "#{buildpath}/bin/macosx-universal-64/lib/"
+    cp jsw_libexec/"bin/wrapper", "#{buildpath}/bin/macosx-universal-64/"
+    cp jsw_libexec/"scripts/App.sh.in", "#{buildpath}/bin/macosx-universal-64/sonar.sh"
+    sonar_sh_file = "bin/macosx-universal-64/sonar.sh"
+    inreplace sonar_sh_file, "@app.name@", "SonarQube"
+    inreplace sonar_sh_file, "@app.long.name@", "SonarQube"
+    inreplace sonar_sh_file, "../conf/wrapper.conf", "../../conf/wrapper.conf"
+    inreplace "conf/wrapper.conf", "wrapper-3.2.3.jar", "wrapper-3.5.46.jar"
+    rm "lib/jsw/wrapper-3.2.3.jar"
+    rm "bin/macosx-universal-64/lib/libwrapper.jnilib"
+
     # Delete native bin directories for other systems
     remove, keep = if OS.mac?
       ["linux", "macosx-universal"]
