da275f8d3d943ab6f90c187aa67d00da2013ee4c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 19:39:13 +0000

    ecasound: update 2.9.3 bottle.

--- Formula/ecasound.rb
+++ Formula/ecasound.rb
@@ -10,11 +10,13 @@ class Ecasound < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "3032ed920ff4b40250c2c1efcd19a591e0df1b2714159aed9fd6ea4db094af4c"
-    sha256 big_sur:       "d3a693686266e5570afbd54ecaede7930145c6a69461e7839c97857b373c63f6"
-    sha256 catalina:      "f6fede56fea73bdfd32cebd514448b50dec47542ff7d76342f950a61160a9fff"
-    sha256 mojave:        "38869046308b12e2d722f1bcb5e9a7085ffab93448e0490b161a6d18fc2fbd09"
-    sha256 high_sierra:   "9dd2864d7b5a66bf3a7fc674b64a11d3cb1abeaf9fc4c65dd8898a2724c1a5a8"
+    sha256 arm64_monterey: "73ca5b440adf4626dea12d21da27ecc13d76829efee851e8160dedb78709023d"
+    sha256 arm64_big_sur:  "3032ed920ff4b40250c2c1efcd19a591e0df1b2714159aed9fd6ea4db094af4c"
+    sha256 monterey:       "34a6f3f1ac115bd659329799ee74b5d3a04fdf5a2bb5c8b99d6786c5a81d1283"
+    sha256 big_sur:        "d3a693686266e5570afbd54ecaede7930145c6a69461e7839c97857b373c63f6"
+    sha256 catalina:       "f6fede56fea73bdfd32cebd514448b50dec47542ff7d76342f950a61160a9fff"
+    sha256 mojave:         "38869046308b12e2d722f1bcb5e9a7085ffab93448e0490b161a6d18fc2fbd09"
+    sha256 high_sierra:    "9dd2864d7b5a66bf3a7fc674b64a11d3cb1abeaf9fc4c65dd8898a2724c1a5a8"
   end
 
   depends_on "jack"
