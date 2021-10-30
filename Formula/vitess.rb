0a11b39bea60fedee00d17aed70fb127024fa40c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 19:16:26 +0000

    vitess: update 10.0.2 bottle.

--- Formula/vitess.rb
+++ Formula/vitess.rb
@@ -7,6 +7,7 @@ class Vitess < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3d7de1a40fc1fa9e6e77e3ae7a795e0155cd82e6e4b00c579813142eb70066fa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4aa971754b4a25ba92ee044864d45ed528a7247625c4ee02f0593d5be05bf0f2"
     sha256 cellar: :any_skip_relocation, monterey:       "c4d4555d17a7746fef01ef12af90b591be43d83631ce882e5bf5923ab7e2223d"
     sha256 cellar: :any_skip_relocation, big_sur:        "223ec20c9812977bfa7b27f4c8bc96543151539d0e290c79e4a77cc5194cb886"
     sha256 cellar: :any_skip_relocation, catalina:       "fd69b5532c0a3621f749415cc6123b26184393177207d22dbb926f57cda306b2"
