9db2ed28bcde798ab12eb13bcc457c93730a4cc0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 01:09:08 +0000

    mtoc: update 949.0.1 bottle.

--- Formula/mtoc.rb
+++ Formula/mtoc.rb
@@ -10,11 +10,13 @@ class Mtoc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "935bfd119379ec4d223830db858a2b2279152709a6e78eba895af5859110d22f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "605abc57733add4e0643d6ffa0186df37e1b4adb5461b9fcdd92d1bfb688f649"
-    sha256 cellar: :any_skip_relocation, catalina:      "2f60b3731066cf662f3d8e9451ce0f94954980100780c9e79b6e8ea066ad8def"
-    sha256 cellar: :any_skip_relocation, mojave:        "c9cba74c5669816e90ae2fa9110be8c9b6b9d1a90ec7d1f246687a3f512e08ab"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "62587e723f38c2a51d3a951dca42df10b9aa1ac67c88d8e286b27e6957edd985"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ebb0ffb0ba60ca6d02f0df9919427bf8a2579632e585a2a6ae851c5fbe858cc5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "935bfd119379ec4d223830db858a2b2279152709a6e78eba895af5859110d22f"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd72c247a0ea992a4ab1a645e3a51007331a1bd15fc693e93fc1bd0267f38273"
+    sha256 cellar: :any_skip_relocation, big_sur:        "605abc57733add4e0643d6ffa0186df37e1b4adb5461b9fcdd92d1bfb688f649"
+    sha256 cellar: :any_skip_relocation, catalina:       "2f60b3731066cf662f3d8e9451ce0f94954980100780c9e79b6e8ea066ad8def"
+    sha256 cellar: :any_skip_relocation, mojave:         "c9cba74c5669816e90ae2fa9110be8c9b6b9d1a90ec7d1f246687a3f512e08ab"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "62587e723f38c2a51d3a951dca42df10b9aa1ac67c88d8e286b27e6957edd985"
   end
 
   depends_on "llvm" => :build
