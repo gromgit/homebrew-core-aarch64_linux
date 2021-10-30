ab425c71054bdc9b5d93840dcf56fa7e9330cf5c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 21:10:51 +0000

    ser2net: update 4.2.3 bottle.

--- Formula/ser2net.rb
+++ Formula/ser2net.rb
@@ -12,11 +12,13 @@ class Ser2net < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "1845d6644d7cd518e8b1c7f7abd4ab1d6515eb482690a062b8e17ddbc6eb7c4c"
-    sha256 cellar: :any,                 big_sur:       "03feeb3d9e3049231fccb29a8ca8bc7d79c0e4839381d1340c45cf932ddaea68"
-    sha256 cellar: :any,                 catalina:      "21a9c4c0a980b74a864c7db0782f71786e133cc2e3d62411fa9ea0f99c38e0b2"
-    sha256 cellar: :any,                 mojave:        "a2abbe3f823de5e70a863f521e63fcb079801b22d653f4bdb26a775c97fa4289"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c4726875c7f661b925b6fbd7d82eeaf89fd4b91a28ba34377860c8ebbb3f999"
+    sha256 cellar: :any,                 arm64_monterey: "be5488df8e6b9e4fd6750ff8b0156ff74bf77e1c65789bab0466fefd03c1f391"
+    sha256 cellar: :any,                 arm64_big_sur:  "1845d6644d7cd518e8b1c7f7abd4ab1d6515eb482690a062b8e17ddbc6eb7c4c"
+    sha256 cellar: :any,                 monterey:       "f6cac316c6081bfef01bcdd90eb4f7ac4f1b90232400b067d048e890facb992a"
+    sha256 cellar: :any,                 big_sur:        "03feeb3d9e3049231fccb29a8ca8bc7d79c0e4839381d1340c45cf932ddaea68"
+    sha256 cellar: :any,                 catalina:       "21a9c4c0a980b74a864c7db0782f71786e133cc2e3d62411fa9ea0f99c38e0b2"
+    sha256 cellar: :any,                 mojave:         "a2abbe3f823de5e70a863f521e63fcb079801b22d653f4bdb26a775c97fa4289"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2c4726875c7f661b925b6fbd7d82eeaf89fd4b91a28ba34377860c8ebbb3f999"
   end
 
   depends_on "libyaml"
