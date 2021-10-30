ea55a557711990f17fca5cf07e7ab8ac83d2c8de
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:42:18 +0000

    pgpool-ii: update 4.2.5 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -10,11 +10,13 @@ class PgpoolIi < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5828372d6ca3200174cff7f8ffdd248de570f0d378c60db47ecd35c4a46ecab5"
-    sha256 big_sur:       "59740c49588ae2a35b2b15d9f88a7654435d70c05ccf14792054eb9bdd1a2b56"
-    sha256 catalina:      "cce39ff3a542fe53fcb46a1a224f65a1b7f791d3a3ba0d85e452cbccba56931b"
-    sha256 mojave:        "907f52bf7604e8dc88866f65d12814bf33cd853ebdb4a6d013921e3e499f9ecf"
-    sha256 x86_64_linux:  "27cff1b485ad2f62e617b8bf14647f3ea2b43447911a91aa0d97b80668a6a5c1"
+    sha256 arm64_monterey: "2596afdc5a5a6c3a1026317df1d655aad1e7f3b31d55106bf3fa58f50dc35ffa"
+    sha256 arm64_big_sur:  "5828372d6ca3200174cff7f8ffdd248de570f0d378c60db47ecd35c4a46ecab5"
+    sha256 monterey:       "ed50605af924aacd0dc69cc2e82515a7eb0b339c0fd3a4265a0530a6ab3a7cf1"
+    sha256 big_sur:        "59740c49588ae2a35b2b15d9f88a7654435d70c05ccf14792054eb9bdd1a2b56"
+    sha256 catalina:       "cce39ff3a542fe53fcb46a1a224f65a1b7f791d3a3ba0d85e452cbccba56931b"
+    sha256 mojave:         "907f52bf7604e8dc88866f65d12814bf33cd853ebdb4a6d013921e3e499f9ecf"
+    sha256 x86_64_linux:   "27cff1b485ad2f62e617b8bf14647f3ea2b43447911a91aa0d97b80668a6a5c1"
   end
 
   depends_on "postgresql"
