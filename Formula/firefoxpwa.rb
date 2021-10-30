e129e2ed9a25d6b605b0c0567c247c0e8fee944e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 01:42:22 +0000

    firefoxpwa: update 1.2.1 bottle.

--- Formula/firefoxpwa.rb
+++ Formula/firefoxpwa.rb
@@ -7,10 +7,12 @@ class Firefoxpwa < Formula
   head "https://github.com/filips123/FirefoxPWA.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0bda2f63e00c22d6a8fb86ba284625b84dcf94a33d3f5d6c68a6691314b69e40"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3d1d17cf2ddc34e185017c175c55285b89ae70ebc4f367c34af043a7a5051849"
-    sha256 cellar: :any_skip_relocation, catalina:      "6f0e4d628b7d612b6c651b72b9df8f0f54f60364d5f36d67aa9849bafd6a43b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ee74182cd7e9cc782ff2b67cefe7f09c29c48d1fe0e74e7814e1c5c7acb8709b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "322606d678649e3b358613064efb3242dfcf8fdfe6e2f7fdea126f75e54f7547"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0bda2f63e00c22d6a8fb86ba284625b84dcf94a33d3f5d6c68a6691314b69e40"
+    sha256 cellar: :any_skip_relocation, monterey:       "d917d9321b83aa6e31a5ad65f656878a316b5df6e95af85ff383dfebb00e0122"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3d1d17cf2ddc34e185017c175c55285b89ae70ebc4f367c34af043a7a5051849"
+    sha256 cellar: :any_skip_relocation, catalina:       "6f0e4d628b7d612b6c651b72b9df8f0f54f60364d5f36d67aa9849bafd6a43b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee74182cd7e9cc782ff2b67cefe7f09c29c48d1fe0e74e7814e1c5c7acb8709b"
   end
 
   depends_on "rust" => :build
