7d3de656dc09af4d27894db9442924830a07ce99
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 14:38:13 +0000

    cartridge-cli: update 2.10.0 bottle.

--- Formula/cartridge-cli.rb
+++ Formula/cartridge-cli.rb
@@ -8,11 +8,13 @@ class CartridgeCli < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4377b98d0ad6eb4505851335a1f0f28efaee3c08038e4e6b01e2b45a30746236"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3a7029c4c492dcb0dc25880ab262e8990494ac59839906459974d39f3c18a684"
-    sha256 cellar: :any_skip_relocation, catalina:      "f6a520934a4deb9ced42ce061c5d5c3b8742afb25835fb01a6db0be84a3ce315"
-    sha256 cellar: :any_skip_relocation, mojave:        "08fdfac9a58c68546ffc0c6333997bb5de778d9ac19406ec679173e197dbb4ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50793e3b6cc34d7e8e1e9f3f815db7e26a8caa7b709f2fb939b6c683b9077506"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "56bd1168aa1ee62c674d32226f15b0b92f7e90bc131420f70f8f5dd405f45466"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4377b98d0ad6eb4505851335a1f0f28efaee3c08038e4e6b01e2b45a30746236"
+    sha256 cellar: :any_skip_relocation, monterey:       "0df91da76c60a8e3b170411516c4d3fe051684dcf685454aa7054ea8814f079d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3a7029c4c492dcb0dc25880ab262e8990494ac59839906459974d39f3c18a684"
+    sha256 cellar: :any_skip_relocation, catalina:       "f6a520934a4deb9ced42ce061c5d5c3b8742afb25835fb01a6db0be84a3ce315"
+    sha256 cellar: :any_skip_relocation, mojave:         "08fdfac9a58c68546ffc0c6333997bb5de778d9ac19406ec679173e197dbb4ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50793e3b6cc34d7e8e1e9f3f815db7e26a8caa7b709f2fb939b6c683b9077506"
   end
 
   depends_on "go" => :build
