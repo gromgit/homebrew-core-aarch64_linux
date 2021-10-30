94fbdd207b830d53699b6e81ad674cc210a4b52d
Erik Sp <scmmail@aschwa.com>
2021-10-30 10:40:47 -0700

    lnav 0.10.1
    bump to 0.10.1
    removing 'revision' line on version bump
    
    Closes #88366.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -1,10 +1,9 @@
 class Lnav < Formula
   desc "Curses-based tool for viewing and analyzing log files"
   homepage "https://lnav.org/"
-  url "https://github.com/tstack/lnav/releases/download/v0.10.0/lnav-0.10.0.tar.gz"
-  sha256 "05caf14d410a3912ef9093773aec321e0f4718a29476005c05dd53fcd6de1531"
+  url "https://github.com/tstack/lnav/releases/download/v0.10.1/lnav-0.10.1.tar.gz"
+  sha256 "a1fd65916bf06e5f01f07aca73ff9cca783f0562465facdf28fa24e9cf568209"
   license "BSD-2-Clause"
-  revision 1
 
   livecheck do
     url :stable
