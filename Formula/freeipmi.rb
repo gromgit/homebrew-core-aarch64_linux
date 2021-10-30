c492bdbf3ca579be3082223260cec41412d21bf9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 21:08:51 +0000

    freeipmi: update 1.6.8 bottle.

--- Formula/freeipmi.rb
+++ Formula/freeipmi.rb
@@ -7,10 +7,12 @@ class Freeipmi < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "961064dca3e312d23150cb3b1fee87ef5903a3ea863404b04b90ae6fc3282d9a"
-    sha256 big_sur:       "94211efde5f73e6eca23983c67514ca977be9ce821e4e6266a95c4423531348d"
-    sha256 catalina:      "5b6dd5d1c757fc344be9401e63ee1774658efc1eeb5486171161acafb60a5b89"
-    sha256 mojave:        "96bd9a3bde2379bfaf2b88b2de7e0e8c185c93a71f0b9cd864afd1d9d693bb77"
+    sha256 arm64_monterey: "92acf4bfe30602ff62420cb4b2c6824066553410bf2fd4fae5986f5e28bf178c"
+    sha256 arm64_big_sur:  "961064dca3e312d23150cb3b1fee87ef5903a3ea863404b04b90ae6fc3282d9a"
+    sha256 monterey:       "ce2d911b60b36dd247441befcc18553e322428c04222be0d4c1166368eb253a2"
+    sha256 big_sur:        "94211efde5f73e6eca23983c67514ca977be9ce821e4e6266a95c4423531348d"
+    sha256 catalina:       "5b6dd5d1c757fc344be9401e63ee1774658efc1eeb5486171161acafb60a5b89"
+    sha256 mojave:         "96bd9a3bde2379bfaf2b88b2de7e0e8c185c93a71f0b9cd864afd1d9d693bb77"
   end
 
   depends_on "argp-standalone"
