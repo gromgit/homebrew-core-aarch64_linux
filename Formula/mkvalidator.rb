ed0af52cf5dffb1995c4149ccab7165f69e9cdf3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:24:08 +0000

    mkvalidator: update 0.6.0 bottle.

--- Formula/mkvalidator.rb
+++ Formula/mkvalidator.rb
@@ -11,11 +11,13 @@ class Mkvalidator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "132951513f0022d9a2f8e0b0f81a5d76668b292873df6d6c67b81c4cd304ac31"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cef2881fd23f1e0b7c465080379f8564b00da6db94cf28f5da272ec19f565014"
-    sha256 cellar: :any_skip_relocation, catalina:      "98fa360ee6e7ebc233784d62c599bc07bd92131159bdbc64d233ad99f883a99e"
-    sha256 cellar: :any_skip_relocation, mojave:        "4e2d50be71341f0a47591587ce3cc428da8493af87ea2bcdcca64c86cacd44f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83b78fb761e9d2bccbc88b9893d6760f445eef59c2d122dace22541ef1e04791"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2d72352ffb6de0c6ce0a925ca48e8e3276a99b4c15e503360a46dfc19f4b12dc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "132951513f0022d9a2f8e0b0f81a5d76668b292873df6d6c67b81c4cd304ac31"
+    sha256 cellar: :any_skip_relocation, monterey:       "3ecaed3130b7884171aac431fffd4b6be19ad322cc990a4659a26407ffb799fd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cef2881fd23f1e0b7c465080379f8564b00da6db94cf28f5da272ec19f565014"
+    sha256 cellar: :any_skip_relocation, catalina:       "98fa360ee6e7ebc233784d62c599bc07bd92131159bdbc64d233ad99f883a99e"
+    sha256 cellar: :any_skip_relocation, mojave:         "4e2d50be71341f0a47591587ce3cc428da8493af87ea2bcdcca64c86cacd44f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83b78fb761e9d2bccbc88b9893d6760f445eef59c2d122dace22541ef1e04791"
   end
 
   depends_on "cmake" => :build
