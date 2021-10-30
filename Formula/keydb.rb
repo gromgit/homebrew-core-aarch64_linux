6b98f7cb3ea579c6167b0a32e903b7b599e00ec9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 19:08:10 +0000

    keydb: update 6.2.0 bottle.

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -7,6 +7,7 @@ class Keydb < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2f7e445ed9f9f4707e12e2dfebfbb339abc24e3abe2380101f960ac730f5a7e9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6dfcf7a5b5ff5541ef7ddefe4f5571cd9e0e52a6662a001d21b718bee6f4d48d"
     sha256 cellar: :any_skip_relocation, monterey:       "edfdfc387c388f01925310c85d71aff3cb912e793943ca3b2bb650f1d844e63f"
     sha256 cellar: :any_skip_relocation, big_sur:        "6a23ffe855a1778880d8dda557da1b993899a83472cfeae29822504812ded307"
     sha256 cellar: :any_skip_relocation, catalina:       "f14d248e73516c85b53a72285301dac6a43d78fa39606787a127d7e21bda8a0f"
