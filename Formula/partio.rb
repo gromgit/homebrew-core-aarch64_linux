9fbbdae0d20e4407dcf4a550101eac7d402ebafd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 19:11:06 +0000

    partio: update 1.14.6 bottle.

--- Formula/partio.rb
+++ Formula/partio.rb
@@ -7,6 +7,7 @@ class Partio < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3ec005cfbaab7733356f7cc6f6682ee9f1cfb44b03a242e6a63c0678c7498345"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b5c63a7b137952e0e8e30941a6e493a30c08cacfe7a464573fe37b7f67a319cf"
     sha256 cellar: :any_skip_relocation, monterey:       "60c0b26a8c07ab1471d4fcd871432d8dfff1c74e96fb812046319aca23f02e15"
     sha256 cellar: :any_skip_relocation, big_sur:        "c5edd20c87a7b31af0632e12bb7c69ebe51e08530d33a292892cb5757b503b5e"
     sha256 cellar: :any_skip_relocation, catalina:       "513c77edf0748cfdd80dd8806add9b0166e2fc947de7fc89dc0a86e68505aece"
