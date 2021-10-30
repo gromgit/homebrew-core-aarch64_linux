c37a54e5cdfe67e4f7549f897dd25b45f0f0736c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 16:00:27 +0000

    wasm3: update 0.5.0 bottle.

--- Formula/wasm3.rb
+++ Formula/wasm3.rb
@@ -6,11 +6,13 @@ class Wasm3 < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "71dd2cacce7a57fca4255f6aa59bea0b03449d13334a2c98ba817401dc41da95"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e282401723657985765d781b1fc6b23ff47ca669fe12d7aba5efe4d5a5f75bab"
-    sha256 cellar: :any_skip_relocation, catalina:      "bd63b2e2268796e20ef1a3b12fa8460bea3e37c954fc7ca1abd8d756d39361ed"
-    sha256 cellar: :any_skip_relocation, mojave:        "43e49af5bf99efa53964ccfddffd2e8061ce3b1aac3707ea389ee1f19dd80fd7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1e99649702a2d0db3cf07442af0b960d04a194c62e4062e6a72012f41b5f81d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cb3038ca004157e4e7275ecfb3bce34d430651fda20dfe6044658bdb3c2b3afe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "71dd2cacce7a57fca4255f6aa59bea0b03449d13334a2c98ba817401dc41da95"
+    sha256 cellar: :any_skip_relocation, monterey:       "1cf28d959d6624a6e63b26178e45df73bca24ce18647ff034fbd7ab72c46aafd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e282401723657985765d781b1fc6b23ff47ca669fe12d7aba5efe4d5a5f75bab"
+    sha256 cellar: :any_skip_relocation, catalina:       "bd63b2e2268796e20ef1a3b12fa8460bea3e37c954fc7ca1abd8d756d39361ed"
+    sha256 cellar: :any_skip_relocation, mojave:         "43e49af5bf99efa53964ccfddffd2e8061ce3b1aac3707ea389ee1f19dd80fd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1e99649702a2d0db3cf07442af0b960d04a194c62e4062e6a72012f41b5f81d"
   end
 
   depends_on "cmake" => :build
