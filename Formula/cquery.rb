9b8f4b8fb4a5ba09824813c99cab8fdfce27b90c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 01:46:10 +0000

    cquery: update 20180718 bottle.

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -10,6 +10,7 @@ class Cquery < Formula
 
   bottle do
     rebuild 1
+    sha256 monterey:     "325a5c3bfe7522d659813c0a3ced06aef240dae2876d815bf8f1341d08a69342"
     sha256 big_sur:      "92e25635f16f15c7c34222616b2c4f33ba5605e16c010fb3ee9be24a7c8adab9"
     sha256 catalina:     "fc7c73b7d9132c879399fc65a3556d9382d9617dd5e59bc10a51eaed228be5f8"
     sha256 mojave:       "89a896183bd8e6635146263dc09c0f26464845f91637e5ca9557558aaa034139"
