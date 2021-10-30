4a92a5c3165f375bf251803f94ae088eaeea1d7e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 17:25:52 +0000

    pioneer: update 20210723 bottle.

--- Formula/pioneer.rb
+++ Formula/pioneer.rb
@@ -7,10 +7,12 @@ class Pioneer < Formula
   head "https://github.com/pioneerspacesim/pioneer.git", branch: "master"
 
   bottle do
-    sha256 arm64_big_sur: "f81b47bad4f1c6cef0655b91da3c805735c75c3111495eba83bda3bc917ce3bb"
-    sha256 big_sur:       "dedb56a9bf9ed68885724b45c5ff371247d9e42acbf1376bdf283e1b2bb2f301"
-    sha256 catalina:      "caf101b903a64522ee78327951b73b76499a110a9a32cea3ca1de699ff1e2574"
-    sha256 mojave:        "713fe514265cdbb7ed9f4c21fea203ba479aea835c75ac34be191f901c06eb12"
+    sha256 arm64_monterey: "cf660f08f7b18489c4a7fc5e5ff0f800f846886b48a0dcd78f6d7b3e6216a47a"
+    sha256 arm64_big_sur:  "f81b47bad4f1c6cef0655b91da3c805735c75c3111495eba83bda3bc917ce3bb"
+    sha256 monterey:       "dbb827cc837f5a275df362739ff2c2131050507f73c8026db06720fa4ea0d9d1"
+    sha256 big_sur:        "dedb56a9bf9ed68885724b45c5ff371247d9e42acbf1376bdf283e1b2bb2f301"
+    sha256 catalina:       "caf101b903a64522ee78327951b73b76499a110a9a32cea3ca1de699ff1e2574"
+    sha256 mojave:         "713fe514265cdbb7ed9f4c21fea203ba479aea835c75ac34be191f901c06eb12"
   end
 
   depends_on "cmake" => :build
