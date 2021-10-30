5764833141839d50ab03b728e9c51c9696bc8296
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:27:22 +0000

    instead: update 3.4.1 bottle.

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -6,10 +6,12 @@ class Instead < Formula
   license "MIT"
 
   bottle do
-    sha256 arm64_big_sur: "49487dd18206b1be817bcf05aa09c3dce2b17f1957b99d7a67bb05479bb8a998"
-    sha256 big_sur:       "93cdfed079bf614ca0d4579ed8e7023ed26b65d4897f3889dcd7feba2a9442b6"
-    sha256 catalina:      "aabf4a3da9ba905f5b565cd65fa9563de50df3dac77f671d58e7ee112f24821b"
-    sha256 mojave:        "70c78465c95c5b5e2a22852af9d0ae7aa15f377dd8ef8a04f23ab0ec26fdeed1"
+    sha256 arm64_monterey: "ecb4c6f8eeb0c748f184fbf5ff989f54b6ca8942e4518353fd9864296349ef6c"
+    sha256 arm64_big_sur:  "49487dd18206b1be817bcf05aa09c3dce2b17f1957b99d7a67bb05479bb8a998"
+    sha256 monterey:       "edbb317d7f0f77e8250648f311e86a112be6e0fd429659fb708cf9e4f8ca23a9"
+    sha256 big_sur:        "93cdfed079bf614ca0d4579ed8e7023ed26b65d4897f3889dcd7feba2a9442b6"
+    sha256 catalina:       "aabf4a3da9ba905f5b565cd65fa9563de50df3dac77f671d58e7ee112f24821b"
+    sha256 mojave:         "70c78465c95c5b5e2a22852af9d0ae7aa15f377dd8ef8a04f23ab0ec26fdeed1"
   end
 
   depends_on "cmake" => :build
