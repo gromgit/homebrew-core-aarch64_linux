37d56d5f59a67cfc9cddfba0da13230a5683c206
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:20:15 +0000

    zsync: update 0.6.2 bottle.

--- Formula/zsync.rb
+++ Formula/zsync.rb
@@ -10,15 +10,17 @@ class Zsync < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0ee85fb722fa125e4323e14732d4de448f3751e9445e2ec6933fce0ee38d5a90"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1be9e390c02555dbce349a76e0beb63231bc327f4326580b18679ff0307db446"
-    sha256 cellar: :any_skip_relocation, catalina:      "333d4b2be5c1b6621bf7e7ac87199da1c5ec24a3cdb408c97ed733b6fafb89a1"
-    sha256 cellar: :any_skip_relocation, mojave:        "9fa9f958c45a87c1a4e9b2ccdc95e732bb8ab248843ec3f0554e5b412d7f1ae5"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "b766bfc58f753376213e234d8e0e4238af1be39f77f239370583464040758fd6"
-    sha256 cellar: :any_skip_relocation, sierra:        "8d6e7eade289c62689e752151021e7bccac7900a5e7217e8885f2c38aec42c2c"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "9bbe0e102ca6a2b7ca57af6b2b29984f7da59ce97d15ce550bbbb206f1ad1815"
-    sha256 cellar: :any_skip_relocation, yosemite:      "b7436466e25e1fe44e2169059d613d9df279a69c31183f6cacce953fc6a47e8b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1961843c2195ae143b2f2ece7e26f91aa4c5a0acc67721441c221b5ae3404150"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d9c561ebe0167e590847ed7993ff01e098eed20ba1ab158ffc3fc6a1295d220"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0ee85fb722fa125e4323e14732d4de448f3751e9445e2ec6933fce0ee38d5a90"
+    sha256 cellar: :any_skip_relocation, monterey:       "257f153c9f34b33cfcbcb08aeaab17a7bdf5c5a0538edf96c1a9a6f8074dd212"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1be9e390c02555dbce349a76e0beb63231bc327f4326580b18679ff0307db446"
+    sha256 cellar: :any_skip_relocation, catalina:       "333d4b2be5c1b6621bf7e7ac87199da1c5ec24a3cdb408c97ed733b6fafb89a1"
+    sha256 cellar: :any_skip_relocation, mojave:         "9fa9f958c45a87c1a4e9b2ccdc95e732bb8ab248843ec3f0554e5b412d7f1ae5"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "b766bfc58f753376213e234d8e0e4238af1be39f77f239370583464040758fd6"
+    sha256 cellar: :any_skip_relocation, sierra:         "8d6e7eade289c62689e752151021e7bccac7900a5e7217e8885f2c38aec42c2c"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "9bbe0e102ca6a2b7ca57af6b2b29984f7da59ce97d15ce550bbbb206f1ad1815"
+    sha256 cellar: :any_skip_relocation, yosemite:       "b7436466e25e1fe44e2169059d613d9df279a69c31183f6cacce953fc6a47e8b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1961843c2195ae143b2f2ece7e26f91aa4c5a0acc67721441c221b5ae3404150"
   end
 
   def install
