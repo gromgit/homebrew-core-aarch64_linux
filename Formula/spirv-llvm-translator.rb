9a625346394dda9f8c54906e885592d2fe488adc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:29:01 +0000

    spirv-llvm-translator: update 13.0.0 bottle.

--- Formula/spirv-llvm-translator.rb
+++ Formula/spirv-llvm-translator.rb
@@ -6,11 +6,13 @@ class SpirvLlvmTranslator < Formula
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "92e5cd447c4697efb1426b15971cee928c5352e0ba7c3ba40c0e258290e2eeec"
-    sha256 cellar: :any,                 big_sur:       "7c1833d0f5f8160fdb143eb2ef45a8e501f5c4558e4adf99ed5c05dff778d751"
-    sha256 cellar: :any,                 catalina:      "3f6aadbe72ef0487be0bcbe21612e2c50f5b91eb17ae0a4364ebcd0e86864e97"
-    sha256 cellar: :any,                 mojave:        "879aa8f4b115e9f854f08b0bca109c733c5cef1a05c3af151b295a02a759fd49"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "474b5dd87cb87ec81fea6028e8ec6736626e7a70e7440cd647073c785374c709"
+    sha256 cellar: :any,                 arm64_monterey: "1d456f3222c431416e1b44700c2d82c7292aa678326506e7b351706985d9ee2f"
+    sha256 cellar: :any,                 arm64_big_sur:  "92e5cd447c4697efb1426b15971cee928c5352e0ba7c3ba40c0e258290e2eeec"
+    sha256 cellar: :any,                 monterey:       "cbf2377048c5b50dbab41c60baea31604c9bae41d3d350ee5ae66ede16c46ea8"
+    sha256 cellar: :any,                 big_sur:        "7c1833d0f5f8160fdb143eb2ef45a8e501f5c4558e4adf99ed5c05dff778d751"
+    sha256 cellar: :any,                 catalina:       "3f6aadbe72ef0487be0bcbe21612e2c50f5b91eb17ae0a4364ebcd0e86864e97"
+    sha256 cellar: :any,                 mojave:         "879aa8f4b115e9f854f08b0bca109c733c5cef1a05c3af151b295a02a759fd49"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "474b5dd87cb87ec81fea6028e8ec6736626e7a70e7440cd647073c785374c709"
   end
 
   depends_on "cmake" => :build
