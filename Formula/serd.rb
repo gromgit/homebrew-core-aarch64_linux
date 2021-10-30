480f345ba763aade0f48191a91e9f21f24dce88b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 10:28:16 +0800

    serd: update 0.30.10 bottle.

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -11,11 +11,12 @@ class Serd < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "10155eb151924f5fea4d02b197a5e08c5536340d9335329ce9d3dd6af3431a11"
-    sha256 cellar: :any,                 big_sur:       "e28a02ac86a643e66e2d98fb2e22089f58071bd27faba353494ddc92f6823bfb"
-    sha256 cellar: :any,                 catalina:      "b9b49fee3a281d23119785510ce19337a2eb9fea637583291651a44b27b4f15f"
-    sha256 cellar: :any,                 mojave:        "b780e951258475391de8618edc7915c5f10ea8286769537743d703ed51318778"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3845afc35cb30e10766ad72c7ebda66439f4a3a14de11b7441c5926930caa18e"
+    sha256 cellar: :any,                 arm64_monterey: "b26e4ec50043872551439df4b6a1ce3ed3aa8c7341abc7dfa7d16bb63e0cd691"
+    sha256 cellar: :any,                 arm64_big_sur:  "10155eb151924f5fea4d02b197a5e08c5536340d9335329ce9d3dd6af3431a11"
+    sha256 cellar: :any,                 big_sur:        "e28a02ac86a643e66e2d98fb2e22089f58071bd27faba353494ddc92f6823bfb"
+    sha256 cellar: :any,                 catalina:       "b9b49fee3a281d23119785510ce19337a2eb9fea637583291651a44b27b4f15f"
+    sha256 cellar: :any,                 mojave:         "b780e951258475391de8618edc7915c5f10ea8286769537743d703ed51318778"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3845afc35cb30e10766ad72c7ebda66439f4a3a14de11b7441c5926930caa18e"
   end
 
   depends_on "pkg-config" => :build
