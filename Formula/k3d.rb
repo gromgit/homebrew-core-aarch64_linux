b476e6e0b80098d383bcd3dc527ff37e84d5b885
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 08:22:32 +0000

    k3d: update 5.0.3 bottle.

--- Formula/k3d.rb
+++ Formula/k3d.rb
@@ -11,10 +11,12 @@ class K3d < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e94b60e00ef99c93f8a684dd9de14c6188e301e471113f3156c8ab621270d911"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a1d150c6cf260a17b16a155d305f20a97a80e6d7ae5e35671db8dc4a26c4885e"
-    sha256 cellar: :any_skip_relocation, catalina:      "d7777725eba873299f2344508b907d833973744640973a1201b053a18388d1f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e117dd2b6ebf56a0def38e790920f5c9ac9bbdbfe1abb617c933f46db8770f40"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd7c8e4270996845164dfcbbd58995c6536160cb94cc107af533fcc2d463115b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e94b60e00ef99c93f8a684dd9de14c6188e301e471113f3156c8ab621270d911"
+    sha256 cellar: :any_skip_relocation, monterey:       "8301adcc1ee53135b47891713adee35131b83cf466f2ce5c21814d085ac5e231"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a1d150c6cf260a17b16a155d305f20a97a80e6d7ae5e35671db8dc4a26c4885e"
+    sha256 cellar: :any_skip_relocation, catalina:       "d7777725eba873299f2344508b907d833973744640973a1201b053a18388d1f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e117dd2b6ebf56a0def38e790920f5c9ac9bbdbfe1abb617c933f46db8770f40"
   end
 
   depends_on "go" => :build
