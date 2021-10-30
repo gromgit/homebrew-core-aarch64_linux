0dedecc3c899e4ee48e3ee2e1b6c5a870f3cd04b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 14:27:39 +0000

    libwebm: update 1.0.0.28 bottle.

--- Formula/libwebm.rb
+++ Formula/libwebm.rb
@@ -6,11 +6,13 @@ class Libwebm < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1b142108d31d44af8d338ae68873c2b215860c592c6bf14ae2c074c6f1624737"
-    sha256 cellar: :any_skip_relocation, big_sur:       "80dad8a52ceff398465dc0aa2c0a1cf4fd045cd0d82096c75e064d8fe2b947bc"
-    sha256 cellar: :any_skip_relocation, catalina:      "29cb3c088a8b5ae9c8e6e0c24b57709245e17bafb7d3413f7869f3bdca914c99"
-    sha256 cellar: :any_skip_relocation, mojave:        "d3bc6cbe15a957e620353e165589f81e1ae8065600bc696206efe6bc129cf8ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f170f40ba5a030c9ac2e379a59f4a76020aa9a266f9a094deaa7d1c631180e5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f20ca71a41b3c3a3c648e4d3600e91ee87b87c66ef89ee33b4c04966cb42b4fd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b142108d31d44af8d338ae68873c2b215860c592c6bf14ae2c074c6f1624737"
+    sha256 cellar: :any_skip_relocation, monterey:       "9acafb56282cd618716bb4b2d262c2c65fbb98e4efd713c130b4b4fadb31a31b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "80dad8a52ceff398465dc0aa2c0a1cf4fd045cd0d82096c75e064d8fe2b947bc"
+    sha256 cellar: :any_skip_relocation, catalina:       "29cb3c088a8b5ae9c8e6e0c24b57709245e17bafb7d3413f7869f3bdca914c99"
+    sha256 cellar: :any_skip_relocation, mojave:         "d3bc6cbe15a957e620353e165589f81e1ae8065600bc696206efe6bc129cf8ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f170f40ba5a030c9ac2e379a59f4a76020aa9a266f9a094deaa7d1c631180e5"
   end
 
   depends_on "cmake" => :build
