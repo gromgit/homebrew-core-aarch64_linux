2a86693e68e892d10d1d2886e1e8f3149d48298e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 19:25:33 +0000

    epic5: update 2.1.6 bottle.

--- Formula/epic5.rb
+++ Formula/epic5.rb
@@ -13,10 +13,12 @@ class Epic5 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5139ccc0ee27332891de4634edccab4b74d39c0a65b9bac8034c4f76ae8dc8be"
-    sha256 big_sur:       "e34de29881ed577de6ffbc5fdec471f59707c4e6a91bce1f453656d93612c37f"
-    sha256 catalina:      "882c365f9c32d24c729464e83b399a35d4620944bffc5732cc21bff52751836f"
-    sha256 mojave:        "1ddec44e5b0af07dd5a434ebd913c7139a3270cb8bf6a0807eafca9795aa5dc0"
+    sha256 arm64_monterey: "a840f83399674d45b45c29bd41cbaf6a901779e33226c83cc4d71e08cbc93ec6"
+    sha256 arm64_big_sur:  "5139ccc0ee27332891de4634edccab4b74d39c0a65b9bac8034c4f76ae8dc8be"
+    sha256 monterey:       "e91fcbf5d7cc3af521719f0172836dab56634f0f7a8c52854343d38151e11f04"
+    sha256 big_sur:        "e34de29881ed577de6ffbc5fdec471f59707c4e6a91bce1f453656d93612c37f"
+    sha256 catalina:       "882c365f9c32d24c729464e83b399a35d4620944bffc5732cc21bff52751836f"
+    sha256 mojave:         "1ddec44e5b0af07dd5a434ebd913c7139a3270cb8bf6a0807eafca9795aa5dc0"
   end
 
   depends_on "openssl@1.1"
