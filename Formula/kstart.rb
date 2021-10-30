424aee2128849e8a0377d2042da091e016570433
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:14:39 +0000

    kstart: update 4.3 bottle.

--- Formula/kstart.rb
+++ Formula/kstart.rb
@@ -10,10 +10,12 @@ class Kstart < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dd2e41cf3cd5cf097f960909259741728d215c4d2ed563a4657a4c209d94bb11"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d34ef88ac1505c9590dd1848fd62688af3fdfef8fae4975025cae75684879c99"
-    sha256 cellar: :any_skip_relocation, catalina:      "a4fe5ae0036a4ace4191f41553d3d85ba6278e933f3792eb45ad234e10046e2c"
-    sha256 cellar: :any_skip_relocation, mojave:        "c024af687ae576958110ee27c4f8437c6893f4c25cec966e9f5a6e13f2aea8e9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "95e851996c1269e8e1bff896243972ccfb236db690f448aad6b2369b84f366de"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dd2e41cf3cd5cf097f960909259741728d215c4d2ed563a4657a4c209d94bb11"
+    sha256 cellar: :any_skip_relocation, monterey:       "69dc607481b782c4e341764acacd28362ce93a64c4769ab9bbaa70f9b9e827f5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d34ef88ac1505c9590dd1848fd62688af3fdfef8fae4975025cae75684879c99"
+    sha256 cellar: :any_skip_relocation, catalina:       "a4fe5ae0036a4ace4191f41553d3d85ba6278e933f3792eb45ad234e10046e2c"
+    sha256 cellar: :any_skip_relocation, mojave:         "c024af687ae576958110ee27c4f8437c6893f4c25cec966e9f5a6e13f2aea8e9"
   end
 
   def install
