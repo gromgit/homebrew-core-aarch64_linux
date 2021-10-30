2bac88a82744718bcf85b6af348c6437c4ab75f2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 07:59:34 +0000

    mogenerator: update 1.32 bottle.

--- Formula/mogenerator.rb
+++ Formula/mogenerator.rb
@@ -8,10 +8,12 @@ class Mogenerator < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "51aec3a49207ae357af26a5407494bc88d98027ba06293736b2888ece7b1d71c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "415e0e160574b7b16dff3d0395a7e156894675191c911d09cddf59e1d916571b"
-    sha256 cellar: :any_skip_relocation, catalina:      "d62cad0cc94a7b05286fb2a8a2f8e4a4cc3a9b46efa9a391aa9fcb00c381e85e"
-    sha256 cellar: :any_skip_relocation, mojave:        "dcb658659b696e44f13e382f553c92199a7ab0be48ff69f33a35ef98ee8a09ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "29159fa7d208a108c0a36a222f7a300151241810eccfef04059a86611dfe41d4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51aec3a49207ae357af26a5407494bc88d98027ba06293736b2888ece7b1d71c"
+    sha256 cellar: :any_skip_relocation, monterey:       "a9907203474f336c731912e28fad5ec2e912a1e7378d5ba527a7bb3d3b160134"
+    sha256 cellar: :any_skip_relocation, big_sur:        "415e0e160574b7b16dff3d0395a7e156894675191c911d09cddf59e1d916571b"
+    sha256 cellar: :any_skip_relocation, catalina:       "d62cad0cc94a7b05286fb2a8a2f8e4a4cc3a9b46efa9a391aa9fcb00c381e85e"
+    sha256 cellar: :any_skip_relocation, mojave:         "dcb658659b696e44f13e382f553c92199a7ab0be48ff69f33a35ef98ee8a09ac"
   end
 
   depends_on xcode: :build
