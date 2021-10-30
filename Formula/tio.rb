74e3f47473607c163bac8879a0c6e36cc9547238
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 00:21:03 +0000

    tio: update 1.32 bottle.

--- Formula/tio.rb
+++ Formula/tio.rb
@@ -6,12 +6,14 @@ class Tio < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cd68cb38333ea9bf99d8e0cdd28cf73ce8517b834213b2f786f29c4d58ca0dd8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "257626785fcbbab8298a98f912c7831b1c9565536ff6425c438424fca3163d90"
-    sha256 cellar: :any_skip_relocation, catalina:      "a630b860983adbd4c2691538739850ef934aeafcfa33c5561a00e3db2b355e88"
-    sha256 cellar: :any_skip_relocation, mojave:        "f33b4bc0d653c0f2111f0c30865395d2cadfe524f33ab1c84c843e54ec432ed9"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "1241b11c102b527fd43225a3283290fe5488889a9e0919e7b4b536ddcb4a4d83"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fa7b1f65d234ea6358e451925ce7b1759ce08f35a919ae89ea5dd81182610b6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd0fba8d9f5030bf11b0507832b4c51b6352630c0bc5c3d9629ffd682e087ce2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cd68cb38333ea9bf99d8e0cdd28cf73ce8517b834213b2f786f29c4d58ca0dd8"
+    sha256 cellar: :any_skip_relocation, monterey:       "2c4d432c3826cf3c8a235c90ffb4f8f543a5f77dcbd80f609b16ce46394f2d2b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "257626785fcbbab8298a98f912c7831b1c9565536ff6425c438424fca3163d90"
+    sha256 cellar: :any_skip_relocation, catalina:       "a630b860983adbd4c2691538739850ef934aeafcfa33c5561a00e3db2b355e88"
+    sha256 cellar: :any_skip_relocation, mojave:         "f33b4bc0d653c0f2111f0c30865395d2cadfe524f33ab1c84c843e54ec432ed9"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "1241b11c102b527fd43225a3283290fe5488889a9e0919e7b4b536ddcb4a4d83"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0fa7b1f65d234ea6358e451925ce7b1759ce08f35a919ae89ea5dd81182610b6"
   end
 
   def install
