4439c09a0bf3fb96c0be3bd782dde6619ea17ef7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:28:01 +0000

    open-mesh: update 8.1 bottle.

--- Formula/open-mesh.rb
+++ Formula/open-mesh.rb
@@ -6,12 +6,14 @@ class OpenMesh < Formula
   head "https://www.graphics.rwth-aachen.de:9000/OpenMesh/OpenMesh.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "4602ad1fc42a6aa33f4198ad3a3ea165e65303936d16444a5002821d013463cb"
-    sha256 cellar: :any,                 big_sur:       "1162cfaad47a077402ef01854605a5fe5ab7e696ea56ab9352f753c25df231d6"
-    sha256 cellar: :any,                 catalina:      "40eabd6160d88b74bb3298b42dfce249c327bee9a596b5911a4015462b457dfb"
-    sha256 cellar: :any,                 mojave:        "3c523efbed147ef236ba22b7fdfc8fddae883b4ce7b9f03e970af199416adbe5"
-    sha256 cellar: :any,                 high_sierra:   "a1b6514505ea011f01e8a61fd20dec9f31b900a42e8581e24a23beca738dc5f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0df53a37289dcca304749919744bfea1cb028291638b668dff65b804c9094dd1"
+    sha256 cellar: :any,                 arm64_monterey: "e066cb3914efa547fa06a4dfdbb4f5d4ecaf54c4eeaeecd4d41d4a0f3d16d23a"
+    sha256 cellar: :any,                 arm64_big_sur:  "4602ad1fc42a6aa33f4198ad3a3ea165e65303936d16444a5002821d013463cb"
+    sha256 cellar: :any,                 monterey:       "33029db8032f79b73a8e49a5f6cf79e95f9ea74615c09c60c49b443867000341"
+    sha256 cellar: :any,                 big_sur:        "1162cfaad47a077402ef01854605a5fe5ab7e696ea56ab9352f753c25df231d6"
+    sha256 cellar: :any,                 catalina:       "40eabd6160d88b74bb3298b42dfce249c327bee9a596b5911a4015462b457dfb"
+    sha256 cellar: :any,                 mojave:         "3c523efbed147ef236ba22b7fdfc8fddae883b4ce7b9f03e970af199416adbe5"
+    sha256 cellar: :any,                 high_sierra:    "a1b6514505ea011f01e8a61fd20dec9f31b900a42e8581e24a23beca738dc5f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0df53a37289dcca304749919744bfea1cb028291638b668dff65b804c9094dd1"
   end
 
   depends_on "cmake" => :build
