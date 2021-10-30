1592f4f6735b70ddd357dc5df4dbf0debee55e18
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:49:43 +0800

    ppl: patch incorrect -flat_namespace usage (#88302)
    This is needed for bottling on Monterey.

--- Formula/ppl.rb
+++ Formula/ppl.rb
@@ -34,6 +34,12 @@ class Ppl < Formula
   # patch reference, https://github.com/macports/macports-ports/commit/e5de9cc65a8e91fcbb9a3d90911569169f0ccf88
   patch :DATA
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
