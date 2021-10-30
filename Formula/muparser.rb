dbb05602112edaaf4566ba694725150a0024cb69
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:23:46 +0000

    muparser: update 2.3.2_2 bottle.

--- Formula/muparser.rb
+++ Formula/muparser.rb
@@ -8,12 +8,14 @@ class Muparser < Formula
   head "https://github.com/beltoforion/muparser.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "aaeffc1af6b24928270c796679326095b390f3d998f91962b58aa3f7b3581f87"
-    sha256 cellar: :any,                 big_sur:       "b7c21016c81037618b2e0148b52567c13f2d6f955d39913770e840a07dabecbd"
-    sha256 cellar: :any,                 catalina:      "0a1a8ee3560af0487a46b7c524cdf938b1d6e159e6c4d9689968225cd6311713"
-    sha256 cellar: :any,                 mojave:        "3094837032e20cbbd5e74531a20450af6986bfd5ac83ea4df4884a538a552c85"
-    sha256 cellar: :any,                 high_sierra:   "ca242a645a77e528c16cced97cf06bc796071c549a8d81f22bd4d9bd547828fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d327476ca7ff9b992284dc6ce9d31ec387a8bc2f0c7ad14078393a1e463ae86"
+    sha256 cellar: :any,                 arm64_monterey: "247811d2beb59f456963c712936a90cc3a9364cfea31ce4f66f23d6f45faded0"
+    sha256 cellar: :any,                 arm64_big_sur:  "aaeffc1af6b24928270c796679326095b390f3d998f91962b58aa3f7b3581f87"
+    sha256 cellar: :any,                 monterey:       "c309a018ca89e6650685397f9218d0871ee6adefc6a3a8efff94a455f855a02f"
+    sha256 cellar: :any,                 big_sur:        "b7c21016c81037618b2e0148b52567c13f2d6f955d39913770e840a07dabecbd"
+    sha256 cellar: :any,                 catalina:       "0a1a8ee3560af0487a46b7c524cdf938b1d6e159e6c4d9689968225cd6311713"
+    sha256 cellar: :any,                 mojave:         "3094837032e20cbbd5e74531a20450af6986bfd5ac83ea4df4884a538a552c85"
+    sha256 cellar: :any,                 high_sierra:    "ca242a645a77e528c16cced97cf06bc796071c549a8d81f22bd4d9bd547828fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d327476ca7ff9b992284dc6ce9d31ec387a8bc2f0c7ad14078393a1e463ae86"
   end
 
   depends_on "cmake" => :build
