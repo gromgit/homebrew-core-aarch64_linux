9bf645b3ed6e968880e8ec27ab632d805a040e0d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 09:05:01 +0000

    uuu: update 1.4.165 bottle.

--- Formula/uuu.rb
+++ Formula/uuu.rb
@@ -7,10 +7,12 @@ class Uuu < Formula
   head "https://github.com/NXPmicro/mfgtools.git", branch: "master"
 
   bottle do
-    sha256 arm64_big_sur: "3155b7adc7452904671c7da18cbaa59766dfdd567e85a2b242c24cd8dfede40a"
-    sha256 big_sur:       "90095bd9997651cf9ae3a7b50fd4766968913f2652f2ce47a272ce84e3ce4277"
-    sha256 catalina:      "a66ab4d8c2f44e2ed70106f45765591192b74726f6a1bb2e9e2475c3b9741a2d"
-    sha256 x86_64_linux:  "174d6f56a091fc444337cf21fe0b69595ee87d6bf7da58da597f045bb053de3e"
+    sha256 arm64_monterey: "56a412dd091e6e8f16eece2399fe09a6bb60f17f27b3cc9e7ba3a2fa831a1f32"
+    sha256 arm64_big_sur:  "3155b7adc7452904671c7da18cbaa59766dfdd567e85a2b242c24cd8dfede40a"
+    sha256 monterey:       "e05933d505e1ff45f462b6a45621166895816388efc4fed9a18a4b99f1d49081"
+    sha256 big_sur:        "90095bd9997651cf9ae3a7b50fd4766968913f2652f2ce47a272ce84e3ce4277"
+    sha256 catalina:       "a66ab4d8c2f44e2ed70106f45765591192b74726f6a1bb2e9e2475c3b9741a2d"
+    sha256 x86_64_linux:   "174d6f56a091fc444337cf21fe0b69595ee87d6bf7da58da597f045bb053de3e"
   end
 
   depends_on "cmake" => :build
