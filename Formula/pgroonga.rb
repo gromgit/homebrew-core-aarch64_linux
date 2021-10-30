4d1e1a7d9aedd7dd205d0b45ea73921d7d52bc74
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 14:40:27 +0000

    pgroonga: update 2.3.2 bottle.

--- Formula/pgroonga.rb
+++ Formula/pgroonga.rb
@@ -11,10 +11,12 @@ class Pgroonga < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "31e87e5383f745130c72470927ce55d33a0a431db59e319b342e56a4135e554e"
-    sha256 cellar: :any, big_sur:       "19b6bad62b3ede9ecef955ecc833cd0d22607b4686790814e0dc9262deb9cef0"
-    sha256 cellar: :any, catalina:      "de178482d102009c256d573adfab721d8154644c66fde6616823c46831dc2079"
-    sha256 cellar: :any, mojave:        "234fcf475e296d1b74a5973586cb2a848e1b89c958c928f86e8ab56c8c357630"
+    sha256 cellar: :any, arm64_monterey: "fce5a76d85b0121371047c86ed1d2d1609164222516a13600c347ab7e2354840"
+    sha256 cellar: :any, arm64_big_sur:  "31e87e5383f745130c72470927ce55d33a0a431db59e319b342e56a4135e554e"
+    sha256 cellar: :any, monterey:       "52419f1d6e5605b0faa2dffbf0ab8732b8b837cc88b67f706affd53dd53a51e8"
+    sha256 cellar: :any, big_sur:        "19b6bad62b3ede9ecef955ecc833cd0d22607b4686790814e0dc9262deb9cef0"
+    sha256 cellar: :any, catalina:       "de178482d102009c256d573adfab721d8154644c66fde6616823c46831dc2079"
+    sha256 cellar: :any, mojave:         "234fcf475e296d1b74a5973586cb2a848e1b89c958c928f86e8ab56c8c357630"
   end
 
   depends_on "pkg-config" => :build
