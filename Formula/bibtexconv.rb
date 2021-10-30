ef3a0dd29af2ce33105e053a286236110d6f638f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:03:07 +0000

    bibtexconv: update 1.3.0 bottle.

--- Formula/bibtexconv.rb
+++ Formula/bibtexconv.rb
@@ -7,11 +7,13 @@ class Bibtexconv < Formula
   head "https://github.com/dreibh/bibtexconv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "75b1b0a5166222d906d2fa962f5617ee8e2bcb67e806bc90213fc618a8107498"
-    sha256 cellar: :any,                 big_sur:       "483f1ea3375504b76437e54d3b20aa867a0153c0e273646494aad63b9aae1680"
-    sha256 cellar: :any,                 catalina:      "d14665d2cfd8c82a44dd80fdabe428c2af7c2a8ec686e7d9122e1590de7accc9"
-    sha256 cellar: :any,                 mojave:        "b086275062e61738d48b34c87a15896ff0d32db7f89819a137cd2b0a91079f8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f9d4aeb501f8a5e0bb54ab68f2907503df017a4b18b68f63a97099cbafe591c"
+    sha256 cellar: :any,                 arm64_monterey: "a3d356714a654ae74a4835e763ee47d0b84c1220d6b18c1103ed6610ab73f3b4"
+    sha256 cellar: :any,                 arm64_big_sur:  "75b1b0a5166222d906d2fa962f5617ee8e2bcb67e806bc90213fc618a8107498"
+    sha256 cellar: :any,                 monterey:       "dde5bf5093d0a83c2ec3e27c33a5aa92460d110630bb1c21fa319011f59572ad"
+    sha256 cellar: :any,                 big_sur:        "483f1ea3375504b76437e54d3b20aa867a0153c0e273646494aad63b9aae1680"
+    sha256 cellar: :any,                 catalina:       "d14665d2cfd8c82a44dd80fdabe428c2af7c2a8ec686e7d9122e1590de7accc9"
+    sha256 cellar: :any,                 mojave:         "b086275062e61738d48b34c87a15896ff0d32db7f89819a137cd2b0a91079f8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f9d4aeb501f8a5e0bb54ab68f2907503df017a4b18b68f63a97099cbafe591c"
   end
 
   depends_on "bison" => :build
