895cb4ace1e52690f3290b11e83cb2d1000a449e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 17:48:03 +0000

    cppad: update 20210000.8 bottle.

--- Formula/cppad.rb
+++ Formula/cppad.rb
@@ -14,11 +14,13 @@ class Cppad < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "f1e290d82781bd8a6eb2d834141f9c02bb7006ae26113bedf3ff518eaf6c93bf"
-    sha256 cellar: :any,                 big_sur:       "52998109aba462d55b4dc2dcf3a1d3bde03ed13b0f736985d8725fff163f6b01"
-    sha256 cellar: :any,                 catalina:      "9cedc9b79c879b310618c4f423ab4ec40d5e44b668c95e83c701e0fcc63ecb47"
-    sha256 cellar: :any,                 mojave:        "531ea9476520b4ebaa94a2cdc73d5e668d47ea042d6130f745f9dc8323d67c52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a265722ea9479807714b7cc6aa415e2a5562ae8b61ff7e149f3f9441359f9b7a"
+    sha256 cellar: :any,                 arm64_monterey: "fede696ea9899a66579f17c71657bd29ea1b0b93ec32e110da62abebbb2c4c15"
+    sha256 cellar: :any,                 arm64_big_sur:  "f1e290d82781bd8a6eb2d834141f9c02bb7006ae26113bedf3ff518eaf6c93bf"
+    sha256 cellar: :any,                 monterey:       "b52e0d8f8dc2c9c71f4f32ee74efcce38536b7802fd03c4c14c70eff61742d64"
+    sha256 cellar: :any,                 big_sur:        "52998109aba462d55b4dc2dcf3a1d3bde03ed13b0f736985d8725fff163f6b01"
+    sha256 cellar: :any,                 catalina:       "9cedc9b79c879b310618c4f423ab4ec40d5e44b668c95e83c701e0fcc63ecb47"
+    sha256 cellar: :any,                 mojave:         "531ea9476520b4ebaa94a2cdc73d5e668d47ea042d6130f745f9dc8323d67c52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a265722ea9479807714b7cc6aa415e2a5562ae8b61ff7e149f3f9441359f9b7a"
   end
 
   depends_on "cmake" => :build
