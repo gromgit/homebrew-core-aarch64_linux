28249e9e9b32395ffab88d86bcd553faa967f0d1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:56:06 +0000

    bcoin: update 2.1.2_5 bottle.

--- Formula/bcoin.rb
+++ Formula/bcoin.rb
@@ -10,10 +10,12 @@ class Bcoin < Formula
   head "https://github.com/bcoin-org/bcoin.git", branch: "master"
 
   bottle do
-    sha256                               arm64_big_sur: "728c2a8cb144edbcd2e0174a30f13eb62be21217c7f5a12a1e8d9a9ca30228da"
-    sha256                               big_sur:       "93b20a88905a0618fdcaa7e4802a52112297e5352a893b086ecd582a9b4f4cfe"
-    sha256                               catalina:      "e84084b15935bd9c85fee85b902cbbedb3a57e71cf80307878e7fc47964f74f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40c658b3c5f910037d531193d08cef916272f71fe6193543b05497db804f24b6"
+    sha256                               arm64_monterey: "7a23feaf3cf978ec978cd78b1821ceb1fd6e4701cc8d2ca6e6fcdfb9e6c9ca67"
+    sha256                               arm64_big_sur:  "728c2a8cb144edbcd2e0174a30f13eb62be21217c7f5a12a1e8d9a9ca30228da"
+    sha256                               monterey:       "cd6af05338a97aef7db95d59566a4073d443806ed519013829076fa7ed3885cb"
+    sha256                               big_sur:        "93b20a88905a0618fdcaa7e4802a52112297e5352a893b086ecd582a9b4f4cfe"
+    sha256                               catalina:       "e84084b15935bd9c85fee85b902cbbedb3a57e71cf80307878e7fc47964f74f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40c658b3c5f910037d531193d08cef916272f71fe6193543b05497db804f24b6"
   end
 
   depends_on "python@3.10" => :build
