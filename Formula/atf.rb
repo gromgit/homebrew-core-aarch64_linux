3ce25767e0a9a7b9fe86ba48f4d500cac35bf85e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:41:28 +0000

    atf: update 0.21 bottle.

--- Formula/atf.rb
+++ Formula/atf.rb
@@ -6,15 +6,17 @@ class Atf < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 arm64_big_sur: "67cc581f8b6a72d2ebe4a5d9210ff0b39c247f76852afa50df699988c3617783"
-    sha256 big_sur:       "fff75eabcd7eb2a52aca286d42f82f4488b5a28fc2c7dc154fd0b34d62366272"
-    sha256 catalina:      "39570850845a8c01f2ce167fec23284fc6172c816a9d5806b9c9034448d5a0a3"
-    sha256 mojave:        "c8e2c7b3d06d8c84409ef21b12201803113244d668eb092decf073fc5066fdab"
-    sha256 high_sierra:   "034a9f29ce63bd5cd019b957bc544a3129df7ec3872453f57f24914dce1f2da8"
-    sha256 sierra:        "a58333135e72fa1817c0411f3801615780c4346347d73d25ddec6eca6b213c41"
-    sha256 el_capitan:    "74493d4b4868628a7a84338eb28ecfce8afdd896962f3ba632b1e785def48737"
-    sha256 yosemite:      "e4caa0498d0caf2e99e0eea9ae9269240e4f4a49a41f712cf6a730fab8d72672"
-    sha256 x86_64_linux:  "c9a94b838e115887902fd1e12ef77cce606f475772668278908382fb161d1ca6"
+    sha256 arm64_monterey: "650c374c9509cd3634ef36fd0d61bd7c852536778be5ffa2c0c9b4590fc92d66"
+    sha256 arm64_big_sur:  "67cc581f8b6a72d2ebe4a5d9210ff0b39c247f76852afa50df699988c3617783"
+    sha256 monterey:       "4c9f336e433ec164c422e391de58eb6a68539b58463e0bc8eeefc151dd8767c2"
+    sha256 big_sur:        "fff75eabcd7eb2a52aca286d42f82f4488b5a28fc2c7dc154fd0b34d62366272"
+    sha256 catalina:       "39570850845a8c01f2ce167fec23284fc6172c816a9d5806b9c9034448d5a0a3"
+    sha256 mojave:         "c8e2c7b3d06d8c84409ef21b12201803113244d668eb092decf073fc5066fdab"
+    sha256 high_sierra:    "034a9f29ce63bd5cd019b957bc544a3129df7ec3872453f57f24914dce1f2da8"
+    sha256 sierra:         "a58333135e72fa1817c0411f3801615780c4346347d73d25ddec6eca6b213c41"
+    sha256 el_capitan:     "74493d4b4868628a7a84338eb28ecfce8afdd896962f3ba632b1e785def48737"
+    sha256 yosemite:       "e4caa0498d0caf2e99e0eea9ae9269240e4f4a49a41f712cf6a730fab8d72672"
+    sha256 x86_64_linux:   "c9a94b838e115887902fd1e12ef77cce606f475772668278908382fb161d1ca6"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
