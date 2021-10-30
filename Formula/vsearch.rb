ca9767d6ab50572106cc15bcd9341bf4314c3f29
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 18:05:50 +0000

    vsearch: update 2.18.0 bottle.

--- Formula/vsearch.rb
+++ Formula/vsearch.rb
@@ -6,11 +6,13 @@ class Vsearch < Formula
   license any_of: ["BSD-2-Clause", "GPL-3.0-or-later"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dc8381c80eec137ec9b59ff781c603d0f70ba2fa97a1cce6f51a0e452e7bdc8f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3865c00e1968566ee8b48f860d3412cb3a9137fcab1a970cc3e519a206f63bf9"
-    sha256 cellar: :any_skip_relocation, catalina:      "3003f6c4460a07260687b855ceb6f048a873afb0ccb9cdee1c5fcbc5d5b82bd5"
-    sha256 cellar: :any_skip_relocation, mojave:        "622bf6c177ed7493cedb01fd8fb65868405500a20c9772ea957ca0bf59782986"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "180e70bfad482076a2d8f5bb78116421ce6b50cac6d161c4ede84cf447f89bd6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "66083739731ce00fb146b06efb1eb8f7007dbf3e24c8ddd13a853ae0f9c42dd9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc8381c80eec137ec9b59ff781c603d0f70ba2fa97a1cce6f51a0e452e7bdc8f"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd4900ba58550a865f7a5929ebd39c073a7b7e51aa2a3fa23e98f5d0a3fcc1fd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3865c00e1968566ee8b48f860d3412cb3a9137fcab1a970cc3e519a206f63bf9"
+    sha256 cellar: :any_skip_relocation, catalina:       "3003f6c4460a07260687b855ceb6f048a873afb0ccb9cdee1c5fcbc5d5b82bd5"
+    sha256 cellar: :any_skip_relocation, mojave:         "622bf6c177ed7493cedb01fd8fb65868405500a20c9772ea957ca0bf59782986"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "180e70bfad482076a2d8f5bb78116421ce6b50cac6d161c4ede84cf447f89bd6"
   end
 
   depends_on "autoconf" => :build
