c53248716b9ca164a65e35d6526114359513dad1
Branch Vincent <branchevincent@gmail.com>
2021-10-30 10:21:29 -0400

    yq 4.14.1
    Closes #88344.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -1,8 +1,8 @@
 class Yq < Formula
   desc "Process YAML documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/v4.13.5.tar.gz"
-  sha256 "c0d637e7d7d5f370960af713e0f7e769e1b0876f71a844373d0307cbba68c4b2"
+  url "https://github.com/mikefarah/yq/archive/v4.14.1.tar.gz"
+  sha256 "294300a8c182c3f8e1f537ad2feebb6d0651f61330f33504bdc502f48992bf84"
   license "MIT"
   head "https://github.com/mikefarah/yq.git", branch: "master"
 
@@ -21,15 +21,22 @@ class Yq < Formula
   end
 
   depends_on "go" => :build
+  depends_on "pandoc" => :build
 
   conflicts_with "python-yq", because: "both install `yq` executables"
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
+    # Install shell completions
     (bash_completion/"yq").write Utils.safe_popen_read(bin/"yq", "shell-completion", "bash")
     (zsh_completion/"_yq").write Utils.safe_popen_read(bin/"yq", "shell-completion", "zsh")
     (fish_completion/"yq.fish").write Utils.safe_popen_read(bin/"yq", "shell-completion", "fish")
+
+    # Install man pages
+    system "./scripts/generate-man-page-md.sh"
+    system "./scripts/generate-man-page.sh"
+    man1.install "yq.1"
   end
 
   test do
