3427c89162dc7205258158d435a73d4dbcc2b5bd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 15:15:46 +0000

    open-zwave: update 1.6.1914 bottle.

--- Formula/open-zwave.rb
+++ Formula/open-zwave.rb
@@ -11,10 +11,12 @@ class OpenZwave < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "946d78311179280c3460097a1b60331daa782d916b10e819b97fa80a06037c3f"
-    sha256 big_sur:       "e3c9055c54562fc0fc8879f094359263626bb0cbb0b67a1c48999420f2f223c4"
-    sha256 catalina:      "af0ac45b4c07da453526cc464cf777d17cdbb3760c34ddefcfb3435977139d91"
-    sha256 mojave:        "9680488853f6ee6db1f0e299ff1f00597e8652c095ecb411e322a99b8b43caad"
+    sha256 arm64_monterey: "46059e0f107fa894491dcca4afbc27487374077ac10d0c9e0466b70a21b98bdf"
+    sha256 arm64_big_sur:  "946d78311179280c3460097a1b60331daa782d916b10e819b97fa80a06037c3f"
+    sha256 monterey:       "510ea3942d2bac0c420ce6f096c55d00158cb9d68eef036e893bb66c135a4246"
+    sha256 big_sur:        "e3c9055c54562fc0fc8879f094359263626bb0cbb0b67a1c48999420f2f223c4"
+    sha256 catalina:       "af0ac45b4c07da453526cc464cf777d17cdbb3760c34ddefcfb3435977139d91"
+    sha256 mojave:         "9680488853f6ee6db1f0e299ff1f00597e8652c095ecb411e322a99b8b43caad"
   end
 
   depends_on "doxygen" => :build
