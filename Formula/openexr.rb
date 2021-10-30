1648a5fb43c9442dea01eb5e3b494a2aacc429ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-30 19:06:50 +0000

    openexr: update 3.1.3 bottle.

--- Formula/openexr.rb
+++ Formula/openexr.rb
@@ -7,13 +7,10 @@ class Openexr < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "fced64362706423a154f34267febde96ae7b9400f2461928572e36bbc0a32ba4"
-    sha256 cellar: :any,                 arm64_big_sur:  "ab723009a94026ae725e87542aebd00bc69992ac3f672ff6960930eb158819a1"
-    sha256 cellar: :any,                 monterey:       "4e855c669b718fcec2c8755d80fea24a3d600475dc10445870e1aa33e4dfd112"
-    sha256 cellar: :any,                 big_sur:        "922846fff4a126532cbba994521b8768f3ad9c6e4d11c175df4a51f333b8da87"
-    sha256 cellar: :any,                 catalina:       "f9bf9db8861ba9c96ca5b76a03e986a20c4545528c7770dc1915044b7ef3c25e"
-    sha256 cellar: :any,                 mojave:         "e2ab720e00d56143302a0c3af337e7605c8e2768cce9d5dadba3d28dfa28b904"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1472cf78e5388c21d9ecb77f5f3998b66ebf701b1b8fea8119072510ce0fb7ce"
+    sha256 cellar: :any,                 arm64_big_sur: "6b2bb42d4a828b364c664883e49680d6867aa76071c87d4e3f71fa43ce295203"
+    sha256 cellar: :any,                 big_sur:       "d48e166e020597151ad259fc62f321ec128777dc6353e951dee014929b47dda4"
+    sha256 cellar: :any,                 catalina:      "6c43a1e10c1def8c49cc63b4cd50c705be7187ecc7d309309d555104ad886c92"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08dee6134b6115466678e17b91da4e450891b4f0534d3438065594fecdc0dd62"
   end
 
   depends_on "cmake" => :build
