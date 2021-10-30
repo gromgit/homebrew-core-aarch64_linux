3356ac8b1e16cfc4f866663197e87955ad8081e0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:11:11 +0000

    nu-smv: update 2.6.0 bottle.

--- Formula/nu-smv.rb
+++ Formula/nu-smv.rb
@@ -12,11 +12,13 @@ class NuSmv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "93b0188158e38160632863ca61c3e6809e2524fc46f93dacbe6d8c07a1693432"
-    sha256 cellar: :any_skip_relocation, catalina:    "90dad1b30d80ee7ddba984d6ad2536fff08896e79cf1a26a083a5e9990fc3c43"
-    sha256 cellar: :any_skip_relocation, mojave:      "c2cc207758d6f315db1116e0e162be72edc0356312c460cd3359dca8c7de597e"
-    sha256 cellar: :any_skip_relocation, high_sierra: "f2e93143e60b64244fd25958a88480acee332fd4109a6bd356719dc6259efc36"
-    sha256 cellar: :any_skip_relocation, sierra:      "64f825eac53c6c16c9b3db4b505d37a6de9f1f3471863b39081b5a98d517fb3e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fe65bcdedc024ee8cb5c94fcd1cb6487a5ba85bd76e82f3f3d84dfbe18aa19e6"
+    sha256 cellar: :any_skip_relocation, monterey:       "078139325d2258530e56211e7d2104dddc48e8640675b68b8c8548b1b8b90149"
+    sha256 cellar: :any_skip_relocation, big_sur:        "93b0188158e38160632863ca61c3e6809e2524fc46f93dacbe6d8c07a1693432"
+    sha256 cellar: :any_skip_relocation, catalina:       "90dad1b30d80ee7ddba984d6ad2536fff08896e79cf1a26a083a5e9990fc3c43"
+    sha256 cellar: :any_skip_relocation, mojave:         "c2cc207758d6f315db1116e0e162be72edc0356312c460cd3359dca8c7de597e"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "f2e93143e60b64244fd25958a88480acee332fd4109a6bd356719dc6259efc36"
+    sha256 cellar: :any_skip_relocation, sierra:         "64f825eac53c6c16c9b3db4b505d37a6de9f1f3471863b39081b5a98d517fb3e"
   end
 
   depends_on "cmake" => :build
