a12767e6b0508f19d9fbe9a52ccd9252d49220b8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 16:23:01 +0000

    grokj2k: update 9.2.0 bottle.

--- Formula/grokj2k.rb
+++ Formula/grokj2k.rb
@@ -12,10 +12,12 @@ class Grokj2k < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0ff80b270e775a3f3e8e5711358293e1360430a3277b0a69b20eb6522e61e0a0"
-    sha256 cellar: :any, big_sur:       "bb8a5620f4b6102088251cdaa9aa991d0fec06c9b9482fd2aa4a8232eb5afd95"
-    sha256 cellar: :any, catalina:      "32efd38b670868596e67fe8c8b0649318acc83644d55452a088687e9676a5c7d"
-    sha256 cellar: :any, mojave:        "028c9c864189cb90f43c422ba74f60c2297bb427f238713780c20a88adffc5c3"
+    sha256 cellar: :any, arm64_monterey: "262c7341aff1b3bcd6e5ca9fd373ed43f23926f6e606aca97bebc09485a126f5"
+    sha256 cellar: :any, arm64_big_sur:  "0ff80b270e775a3f3e8e5711358293e1360430a3277b0a69b20eb6522e61e0a0"
+    sha256 cellar: :any, monterey:       "0e7d0d7a2c08b753004ed5ea898dd6cba88212f12a15c7a2bfba5d8cfd403e5c"
+    sha256 cellar: :any, big_sur:        "bb8a5620f4b6102088251cdaa9aa991d0fec06c9b9482fd2aa4a8232eb5afd95"
+    sha256 cellar: :any, catalina:       "32efd38b670868596e67fe8c8b0649318acc83644d55452a088687e9676a5c7d"
+    sha256 cellar: :any, mojave:         "028c9c864189cb90f43c422ba74f60c2297bb427f238713780c20a88adffc5c3"
   end
 
   depends_on "cmake" => :build
