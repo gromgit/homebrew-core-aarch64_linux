e7dc95556383c046547e4425f17576ac7ae7a922
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 20:57:18 +0000

    yq: update 4.14.1 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -12,10 +12,12 @@ class Yq < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3925710394d329be6e7a7639b81389b742393f3e18dcd4bfb9a7b6a08acc7550"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b2a186db2d97380888eee97684be41ec832115057dd12d58107159b0ab6f3a09"
-    sha256 cellar: :any_skip_relocation, catalina:      "0a1091b3e54b4f704fbc77094db805110a3f65999224f998956a9f919d26bad0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ca308e471e524c8f9d7ef15827474dc688ddb4352296bb9b109485aac1a1a05"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f9b4f63f3e0e9125a0c547bb7de90dd4f79ed3cc5c976732347f3054663cb4d8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3925710394d329be6e7a7639b81389b742393f3e18dcd4bfb9a7b6a08acc7550"
+    sha256 cellar: :any_skip_relocation, monterey:       "b732a0258096abb50bae00b13332c84586d9370f5b68328ea0bd608c72d2f7d3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b2a186db2d97380888eee97684be41ec832115057dd12d58107159b0ab6f3a09"
+    sha256 cellar: :any_skip_relocation, catalina:       "0a1091b3e54b4f704fbc77094db805110a3f65999224f998956a9f919d26bad0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ca308e471e524c8f9d7ef15827474dc688ddb4352296bb9b109485aac1a1a05"
   end
 
   depends_on "go" => :build
