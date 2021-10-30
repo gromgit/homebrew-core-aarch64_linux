1d44d2dbd1a1882010020074bb0bcd79533a9c78
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 14:22:15 +0000

    brainfuck: update 2.7.1 bottle.

--- Formula/brainfuck.rb
+++ Formula/brainfuck.rb
@@ -18,11 +18,13 @@ class Brainfuck < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "c7c0b45b6d596c3fd5e83156331b7595521249b34c443518d5a757a937551046"
-    sha256 cellar: :any,                 big_sur:       "7b128f991009e1e9b4e5ce31b451f49d7aaab01b4a7867ce8709483dee4e8bea"
-    sha256 cellar: :any,                 catalina:      "e6df5d077a5a75d2f350064f7d8aab3ac109759ca330753f974c9bd23043a917"
-    sha256 cellar: :any,                 mojave:        "ede2edc346ff8bfff8829ced2ec99ef0df74edf6978fb2541bbaa7daa53f8d3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38510b863336aac8bf1c37a49c74a41451c7cfa7f3cd78de7bc7be27f953f242"
+    sha256 cellar: :any,                 arm64_monterey: "f52d2a6b20c727834e0aa1dd54180d13e9c1ef27e3aa2c7112f0bbb7b3c50151"
+    sha256 cellar: :any,                 arm64_big_sur:  "c7c0b45b6d596c3fd5e83156331b7595521249b34c443518d5a757a937551046"
+    sha256 cellar: :any,                 monterey:       "3bc30ee4c21acacd11d589ce31449979c322f4ac3a407abaf9a39a1a6946c543"
+    sha256 cellar: :any,                 big_sur:        "7b128f991009e1e9b4e5ce31b451f49d7aaab01b4a7867ce8709483dee4e8bea"
+    sha256 cellar: :any,                 catalina:       "e6df5d077a5a75d2f350064f7d8aab3ac109759ca330753f974c9bd23043a917"
+    sha256 cellar: :any,                 mojave:         "ede2edc346ff8bfff8829ced2ec99ef0df74edf6978fb2541bbaa7daa53f8d3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38510b863336aac8bf1c37a49c74a41451c7cfa7f3cd78de7bc7be27f953f242"
   end
 
   depends_on "cmake" => :build
