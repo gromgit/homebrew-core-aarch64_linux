b6259948b26e1956fb0a18eb9d6b9eea9929fc73
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-30 16:22:24 +0000

    darkstat: update 3.0.719 bottle.

--- Formula/darkstat.rb
+++ Formula/darkstat.rb
@@ -11,13 +11,15 @@ class Darkstat < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f118133e9435512691870171effd65220c2340328860a7b22ae32ad1ba3c369e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "384197a3882c62ebb9f346743561c6e747f9504e681e02ba57c4942439f6e459"
-    sha256 cellar: :any_skip_relocation, catalina:      "381e67870ea4f9f980421aa6f51580eedf98a916a09de042022744de1905798e"
-    sha256 cellar: :any_skip_relocation, mojave:        "edef4012fbd6b7626fbbf324ae92c34afcaba39f09a8ac7c7e80a3e25cef71c7"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "70eae96655d9872851ea02e44709155a6a9df509210fd2b49b0b72de334248f3"
-    sha256 cellar: :any_skip_relocation, sierra:        "680c9a1143b9a95990d246d7ba8357baa0ec1142545252d89734d98b8046337a"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "4e67244fc36d17dbdbe9ae33cc38bd79d2e016eeed0139c164d323e89b15c15e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d9d44eba247c8894ebdfe245d9d1c138ed8fde0044a3e76f0013f34ef6613e4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f118133e9435512691870171effd65220c2340328860a7b22ae32ad1ba3c369e"
+    sha256 cellar: :any_skip_relocation, monterey:       "0d9440ee8fc09322f360a818e5566eee64118cbedfad570f066acb6309cde07c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "384197a3882c62ebb9f346743561c6e747f9504e681e02ba57c4942439f6e459"
+    sha256 cellar: :any_skip_relocation, catalina:       "381e67870ea4f9f980421aa6f51580eedf98a916a09de042022744de1905798e"
+    sha256 cellar: :any_skip_relocation, mojave:         "edef4012fbd6b7626fbbf324ae92c34afcaba39f09a8ac7c7e80a3e25cef71c7"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "70eae96655d9872851ea02e44709155a6a9df509210fd2b49b0b72de334248f3"
+    sha256 cellar: :any_skip_relocation, sierra:         "680c9a1143b9a95990d246d7ba8357baa0ec1142545252d89734d98b8046337a"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "4e67244fc36d17dbdbe9ae33cc38bd79d2e016eeed0139c164d323e89b15c15e"
   end
 
   head do
