46b82152b093c60fc44313c912b6eb224ae18fe0
Bo Anderson <mail@boanderson.me>
2021-10-31 00:08:08 +0000

    scamper: update 20210324 bottle.

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -11,11 +11,13 @@ class Scamper < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "855e3ee08cbf5f3faf38ce9882a50726206ae937b5aaea7e05a0428ebd5a3a98"
-    sha256 cellar: :any,                 big_sur:       "09a4f52a2be595e32ca3fc36a34382c99b81b3dcd100100c2044f7a062fa26f8"
-    sha256 cellar: :any,                 catalina:      "c33b1518d8c66b25952e32d0b52ce8e44b060818056e0d09f5a594bd349fef52"
-    sha256 cellar: :any,                 mojave:        "0ad15790baa3e9045a7d82ecf4a8e40c35f50006c50bd6229ca8b4485ca35071"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3495fd46b0aa2ad9a540d8143f3d0c4b022f3df80e1874016e4edff219099e29"
+    sha256 cellar: :any,                 arm64_monterey: "172a57d0927be1c8f7a2fc7f6f50e7b6ab964582c6b4becbbf55dc8bb7b9b15e"
+    sha256 cellar: :any,                 arm64_big_sur:  "855e3ee08cbf5f3faf38ce9882a50726206ae937b5aaea7e05a0428ebd5a3a98"
+    sha256 cellar: :any,                 monterey:       "84a88c7b1f30397f915bb08ad216e80c5f128c4a771a7393dd0197c6f9aace74"
+    sha256 cellar: :any,                 big_sur:        "09a4f52a2be595e32ca3fc36a34382c99b81b3dcd100100c2044f7a062fa26f8"
+    sha256 cellar: :any,                 catalina:       "c33b1518d8c66b25952e32d0b52ce8e44b060818056e0d09f5a594bd349fef52"
+    sha256 cellar: :any,                 mojave:         "0ad15790baa3e9045a7d82ecf4a8e40c35f50006c50bd6229ca8b4485ca35071"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3495fd46b0aa2ad9a540d8143f3d0c4b022f3df80e1874016e4edff219099e29"
   end
 
   depends_on "pkg-config" => :build
