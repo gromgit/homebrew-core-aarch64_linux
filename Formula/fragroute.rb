f8bb6438c15dc337d917c06e3e3b06e979854120
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 04:28:26 +0000

    fragroute: update 1.2_2 bottle.

--- Formula/fragroute.rb
+++ Formula/fragroute.rb
@@ -14,10 +14,12 @@ class Fragroute < Formula
 
   bottle do
     rebuild 2
-    sha256 arm64_big_sur: "35adad42ecbe16056a06708e7d0a3af1b9611aa3cfc1b1dc8cede40ee6f3f69d"
-    sha256 big_sur:       "6d9bc388969f3798ca6ff4bc6e4cf5ecbc03f995b5f21268ae57fd49a69ec1c2"
-    sha256 catalina:      "1427f299e84d0b1662a3492dc9c69cd46776265dc8b76488752b19eee1126ba6"
-    sha256 mojave:        "2e4c49a602719693ed6a285aab60158a489d0f6592920b37a41e7ee933959ea6"
+    sha256 cellar: :any, arm64_monterey: "7a01636214817acbaffacc3eb4f5c38b5a44c3b63d0239e548c923cc22e17381"
+    sha256               arm64_big_sur:  "35adad42ecbe16056a06708e7d0a3af1b9611aa3cfc1b1dc8cede40ee6f3f69d"
+    sha256               monterey:       "de505dc5218cbde66b8d8dc1538be12fa87ab717c35ea3002c3e8dd017c50fe1"
+    sha256               big_sur:        "6d9bc388969f3798ca6ff4bc6e4cf5ecbc03f995b5f21268ae57fd49a69ec1c2"
+    sha256               catalina:       "1427f299e84d0b1662a3492dc9c69cd46776265dc8b76488752b19eee1126ba6"
+    sha256               mojave:         "2e4c49a602719693ed6a285aab60158a489d0f6592920b37a41e7ee933959ea6"
   end
 
   depends_on "libdnet"
