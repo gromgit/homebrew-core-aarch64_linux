6f5f2878bfd4b30673b5a76cdd88e9f114dcfb6a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 10:23:49 +0000

    hspell: update 1.4 bottle.

--- Formula/hspell.rb
+++ Formula/hspell.rb
@@ -12,11 +12,13 @@ class Hspell < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "421fdc3ab5d0ebde258ce7bdb235d2b50144966a27a74cbbe5c607dff0984c7f"
-    sha256 big_sur:       "426c87d91350f33392c862296b5d1b0081bc953adae5c04a9769ebb2a626213f"
-    sha256 catalina:      "a0406d5a4d5adefa40b5e820510a9b7f461fcea6a61112103c112775fff49ae8"
-    sha256 mojave:        "32e8037e9d494241b975c7558635456991285d53c9bbc89005cd6c86744f30e3"
-    sha256 x86_64_linux:  "fd7cae8024a97aadce0f713008dba1f27e7254969f689a21c9501d42be84fcdb"
+    sha256 arm64_monterey: "743ad6762f4452a62702961c030950a6d70d36877140dd8728ce45f8d06411c3"
+    sha256 arm64_big_sur:  "421fdc3ab5d0ebde258ce7bdb235d2b50144966a27a74cbbe5c607dff0984c7f"
+    sha256 monterey:       "21abb651324e2e46eae76ae915efe203f198a7a74f7c9144b3d21060fc5a2dfd"
+    sha256 big_sur:        "426c87d91350f33392c862296b5d1b0081bc953adae5c04a9769ebb2a626213f"
+    sha256 catalina:       "a0406d5a4d5adefa40b5e820510a9b7f461fcea6a61112103c112775fff49ae8"
+    sha256 mojave:         "32e8037e9d494241b975c7558635456991285d53c9bbc89005cd6c86744f30e3"
+    sha256 x86_64_linux:   "fd7cae8024a97aadce0f713008dba1f27e7254969f689a21c9501d42be84fcdb"
   end
 
   depends_on "autoconf" => :build
