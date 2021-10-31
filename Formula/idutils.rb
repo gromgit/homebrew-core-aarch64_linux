e2fcf42c7ad0b60ba7588747957f1681a14d6157
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 05:12:52 +0000

    idutils: update 4.6_1 bottle.

--- Formula/idutils.rb
+++ Formula/idutils.rb
@@ -9,11 +9,13 @@ class Idutils < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "226783d59f2f9d0d57462e16af1985c475af17ade456463c3c576804646adfe9"
-    sha256 big_sur:       "566c400425874363c736ef591555cadffe7a09875ae5d1e07cbd1c224effbd4d"
-    sha256 catalina:      "5df54c76ae786e54f6994c1c65821adaa746c8a6b1aecbafbe3cd9f4f77f7c62"
-    sha256 mojave:        "b48a4caf24a1eba916f1932c85970294e56a0559603a8289fe732c124fbf0811"
-    sha256 high_sierra:   "95f118aa56026de98d148bccc5a807d609a2bfc54749e1d9051a5dce80f603ef"
+    sha256 arm64_monterey: "61f32269c9b9d859cb3f0951b7bcdb433e68d0a199335631ff94619ffdbe29e3"
+    sha256 arm64_big_sur:  "226783d59f2f9d0d57462e16af1985c475af17ade456463c3c576804646adfe9"
+    sha256 monterey:       "634de9338f14b22d4f5edc452dd828c11fed4b0753bf03fc42635f55d07d35b6"
+    sha256 big_sur:        "566c400425874363c736ef591555cadffe7a09875ae5d1e07cbd1c224effbd4d"
+    sha256 catalina:       "5df54c76ae786e54f6994c1c65821adaa746c8a6b1aecbafbe3cd9f4f77f7c62"
+    sha256 mojave:         "b48a4caf24a1eba916f1932c85970294e56a0559603a8289fe732c124fbf0811"
+    sha256 high_sierra:    "95f118aa56026de98d148bccc5a807d609a2bfc54749e1d9051a5dce80f603ef"
   end
 
   conflicts_with "coreutils", because: "both install `gid` and `gid.1`"
