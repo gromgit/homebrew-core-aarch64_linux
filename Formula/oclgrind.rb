0494f854ee67f67d08972f59a6d84e570b81cb3b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 08:49:06 +0000

    oclgrind: update 21.10 bottle.

--- Formula/oclgrind.rb
+++ Formula/oclgrind.rb
@@ -11,10 +11,12 @@ class Oclgrind < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7eb82923ede6b708c46585ea8277f4f73ed521f5a9411709f27de4daa9f48bb4"
-    sha256 cellar: :any, big_sur:       "d7e20e1c27a6716a448f9e878f60706490751edfde5296a676776c2ec3d2ef2c"
-    sha256 cellar: :any, catalina:      "3b7f3865bc3ed7ea3f9d4436b9e4a003b8759262347c7a27edf68ae291e804a2"
-    sha256 cellar: :any, mojave:        "f92d8a342ad7d878f2164345e2f8982d8bd6c750ef625e1761f80cee666627fb"
+    sha256 cellar: :any, arm64_monterey: "c28fb5bc8dd0fa6897dfeae3dcd1b04c7d2494d6e69f2fdad110dfe0fd53210b"
+    sha256 cellar: :any, arm64_big_sur:  "7eb82923ede6b708c46585ea8277f4f73ed521f5a9411709f27de4daa9f48bb4"
+    sha256 cellar: :any, monterey:       "456d3363136042e98b217cd9fad9165fa0038b97c68279911ff7cd93cfc4889e"
+    sha256 cellar: :any, big_sur:        "d7e20e1c27a6716a448f9e878f60706490751edfde5296a676776c2ec3d2ef2c"
+    sha256 cellar: :any, catalina:       "3b7f3865bc3ed7ea3f9d4436b9e4a003b8759262347c7a27edf68ae291e804a2"
+    sha256 cellar: :any, mojave:         "f92d8a342ad7d878f2164345e2f8982d8bd6c750ef625e1761f80cee666627fb"
   end
 
   depends_on "cmake" => :build
