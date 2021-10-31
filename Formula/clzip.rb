ca224c47d40992d18147d476a202d1596a991f1b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 05:28:14 +0000

    clzip: update 1.12 bottle.

--- Formula/clzip.rb
+++ Formula/clzip.rb
@@ -12,11 +12,13 @@ class Clzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5ae403fecae4e6688b40662a5160f7fbc55e5636ce1a9447dba83c14c60e825f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e82b38513ecfa60b53ea584aa573682fd4b7df5db3e45ccbe633840258f547ee"
-    sha256 cellar: :any_skip_relocation, catalina:      "dc96f1ee14b4b904dd1ecfef38cb3fae30cefd7b490eb31b78e57942df72c2b9"
-    sha256 cellar: :any_skip_relocation, mojave:        "d4cbf4b08330488cf80bbdf6c16d740038c72028760e1bba043122f217e00721"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a1ea519aedf9b7ad116ad71caf65ca8fa08f2b0f444945f8127c6faf05fa6c6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3907dfa594ee7d6d237ae118fade57365a6c1318379da8ea709845ffca68af76"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5ae403fecae4e6688b40662a5160f7fbc55e5636ce1a9447dba83c14c60e825f"
+    sha256 cellar: :any_skip_relocation, monterey:       "23691b3d00a35a10480592393ab1c3442950e663768369d64f09df09a3a63b4b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e82b38513ecfa60b53ea584aa573682fd4b7df5db3e45ccbe633840258f547ee"
+    sha256 cellar: :any_skip_relocation, catalina:       "dc96f1ee14b4b904dd1ecfef38cb3fae30cefd7b490eb31b78e57942df72c2b9"
+    sha256 cellar: :any_skip_relocation, mojave:         "d4cbf4b08330488cf80bbdf6c16d740038c72028760e1bba043122f217e00721"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a1ea519aedf9b7ad116ad71caf65ca8fa08f2b0f444945f8127c6faf05fa6c6"
   end
 
   def install
