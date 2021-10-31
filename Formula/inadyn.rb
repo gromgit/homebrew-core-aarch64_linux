63f37dfa6bb5fa3c889c0b4e99633e78a55ffb36
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 08:41:13 +0000

    inadyn: update 2.8.1 bottle.

--- Formula/inadyn.rb
+++ Formula/inadyn.rb
@@ -6,11 +6,13 @@ class Inadyn < Formula
   license all_of: ["GPL-2.0-or-later", "ISC", "MIT"]
 
   bottle do
-    sha256 arm64_big_sur: "3459e2119123af5cceab755c044952c6399e80bf0efb67ae2c5a5c268b618341"
-    sha256 big_sur:       "5046bd4f55ab963fdfa8bb8edd46454b2bf5b8e0e5f7ae8b1ba6f6d4b81251ee"
-    sha256 catalina:      "e0a601e59fecb91b7932eabfaf7b276a45cadb2233ca2fbde1e71e17240a96a4"
-    sha256 mojave:        "5f26612509b04128a4cc7d73c4a47520a7c72e3575de255f9b579c6a94255f0b"
-    sha256 x86_64_linux:  "56cc59f1fa37c788df4d98bfe8841debe79207d018122eedbcb0cc7954d8e786"
+    sha256 arm64_monterey: "51570d800785bf2c97531b5d80aa4eb07ed51e0049beb28f5efde4b23923983c"
+    sha256 arm64_big_sur:  "3459e2119123af5cceab755c044952c6399e80bf0efb67ae2c5a5c268b618341"
+    sha256 monterey:       "fe3cfb83cfbf077c5af30175643f0a89500402637e4a288ce27f96fa35dd1e06"
+    sha256 big_sur:        "5046bd4f55ab963fdfa8bb8edd46454b2bf5b8e0e5f7ae8b1ba6f6d4b81251ee"
+    sha256 catalina:       "e0a601e59fecb91b7932eabfaf7b276a45cadb2233ca2fbde1e71e17240a96a4"
+    sha256 mojave:         "5f26612509b04128a4cc7d73c4a47520a7c72e3575de255f9b579c6a94255f0b"
+    sha256 x86_64_linux:   "56cc59f1fa37c788df4d98bfe8841debe79207d018122eedbcb0cc7954d8e786"
   end
 
   depends_on "autoconf" => :build
