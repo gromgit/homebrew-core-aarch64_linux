75a5edda1b79498c2a52139de11b769df091dce9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 15:45:52 +0000

    itex2mml: update 1.6.1 bottle.

--- Formula/itex2mml.rb
+++ Formula/itex2mml.rb
@@ -17,10 +17,12 @@ class Itex2mml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2a13992add208a7ab179fab850b3aba9a18a672dd803247ccde9c225103edf01"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3cf7d88d4e102acb646f5e23a4bc168a50c19ce8bda26011bd25c7d8208dbb86"
-    sha256 cellar: :any_skip_relocation, catalina:      "a4a3f1a4d8ff096ed6a4e1eb6ac2883d916de6504496cd8da929081484ab65c4"
-    sha256 cellar: :any_skip_relocation, mojave:        "ca96d27550adc14145a18df3a31ed79dfd12d082f7e4dbccce73e8eabe4ae69e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f08478f4813d052ae7d98339582ca05b95674d7b08a254305bf8e4e6575b3327"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2a13992add208a7ab179fab850b3aba9a18a672dd803247ccde9c225103edf01"
+    sha256 cellar: :any_skip_relocation, monterey:       "fa3e744eb8281aba061785ebb783c1a55d7f4a85c00787052a309411af702583"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3cf7d88d4e102acb646f5e23a4bc168a50c19ce8bda26011bd25c7d8208dbb86"
+    sha256 cellar: :any_skip_relocation, catalina:       "a4a3f1a4d8ff096ed6a4e1eb6ac2883d916de6504496cd8da929081484ab65c4"
+    sha256 cellar: :any_skip_relocation, mojave:         "ca96d27550adc14145a18df3a31ed79dfd12d082f7e4dbccce73e8eabe4ae69e"
   end
 
   def install
