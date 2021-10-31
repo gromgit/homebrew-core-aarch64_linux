1c1d29695564fa21d514116386816510dbe496f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-31 13:09:11 +0000

    asn1c: update 0.9.28 bottle.

--- Formula/asn1c.rb
+++ Formula/asn1c.rb
@@ -6,15 +6,11 @@ class Asn1c < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 arm64_big_sur: "fb9e9bcf79e03d772682d793cae01dcf56327295ec42907541f5a1467186992d"
-    sha256 big_sur:       "5d7a47c30cba9b47d1cdb6a59ba824b60e9435bc977db2718e9cda8e023e3932"
-    sha256 catalina:      "3a3f6f82a118d66043522ec62a31b34c9074193e43b7cda483c13b98aab35d2a"
-    sha256 mojave:        "ca164013b7756e3e0362ed4aae3a7e3cc541e2963354ddfeb1f08d6a754b4a68"
-    sha256 high_sierra:   "8b26526bf103e9b11b07b401a68bc86fb35d4d1ce6e62f6a1568dbc80bd86613"
-    sha256 sierra:        "432aa83cf3a3f9db86435fe75330902b556885605446a477a3f55e9e0ac13806"
-    sha256 el_capitan:    "be0a7e18cf6c2a3cf04831fb854247b26b6da96d3b56b3f8b3088fe3bd5c7668"
-    sha256 yosemite:      "c9f31dc8f2f27a99370713ad9d5dfc1969ced862045bfa3680b48e85080277b5"
-    sha256 x86_64_linux:  "ed036934421098b70439fb08246d7cb2c5cad95c2a97de80c13571e19b21c596"
+    rebuild 1
+    sha256 arm64_big_sur: "25ad95ded32395974dee8fdf8d0e5f7e2dc7cebd38ff0082a13cd3e52677f329"
+    sha256 big_sur:       "d3db341a38f139efbea8f9d2f70912af6e80d4f9cd0b472f2f6202bcd31431b3"
+    sha256 catalina:      "a7688d139182258a7377b3a30cf57ef3ff95c184940bcb171d0968c2c152f65f"
+    sha256 x86_64_linux:  "fe7fa5f68ab94a7d748a2af7451d496192c7bc543bd9dc9c660673cb8026bda4"
   end
 
   head do
