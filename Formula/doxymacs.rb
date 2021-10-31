25cb87b5f53883c7178c4bfe20578ece7a73e41c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 05:52:46 +0000

    doxymacs: update 1.8.0 bottle.

--- Formula/doxymacs.rb
+++ Formula/doxymacs.rb
@@ -6,15 +6,17 @@ class Doxymacs < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4ffe57742c559c3ef80b3bf338d2903c7fc0137d4d9cc96f2b23bea2e0cab832"
-    sha256 cellar: :any_skip_relocation, big_sur:       "761f34a12276f673ad5914b0b9caa8891eaab8fb213292a897e1000375a0370a"
-    sha256 cellar: :any_skip_relocation, catalina:      "060a755f85149143e0aea876b488f98e685e320c7ced43d3ae87dfcbd4931f14"
-    sha256 cellar: :any_skip_relocation, mojave:        "48298f0f0b797c18f3af78a77a0f09f9db3880dc9d85771794894da348aedf1c"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "29a4865170b12a2194c238c35ec5e0902b8e637e378f9013b7aef64fa21eb0fc"
-    sha256 cellar: :any_skip_relocation, sierra:        "2fd3dc59a8c0c8fdccf8195265d320aaa7b5d67e9a81b5a085f27cc287e7370e"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "fb892db831aed57dbdcb2d3a81d78bd05c5b689376d4b7f14bffc56826205ce9"
-    sha256 cellar: :any_skip_relocation, yosemite:      "09eb19921c2ecce5bb02b185c1040caef07d18706866006bdd5fa428bf6b8560"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00c2ebbdf243dded3a23783b3a22ee3705a53dd0ba50c292dcb190bb5bcebc9a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "32c79209e9d8c2f8e47a4e6e28993954250060f74717a749e48ea04b381b63a8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ffe57742c559c3ef80b3bf338d2903c7fc0137d4d9cc96f2b23bea2e0cab832"
+    sha256 cellar: :any_skip_relocation, monterey:       "dba8d6a64b38ed2b2912d6ecc9fa0e895bfbeffb06255a183676f6be56c55c63"
+    sha256 cellar: :any_skip_relocation, big_sur:        "761f34a12276f673ad5914b0b9caa8891eaab8fb213292a897e1000375a0370a"
+    sha256 cellar: :any_skip_relocation, catalina:       "060a755f85149143e0aea876b488f98e685e320c7ced43d3ae87dfcbd4931f14"
+    sha256 cellar: :any_skip_relocation, mojave:         "48298f0f0b797c18f3af78a77a0f09f9db3880dc9d85771794894da348aedf1c"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "29a4865170b12a2194c238c35ec5e0902b8e637e378f9013b7aef64fa21eb0fc"
+    sha256 cellar: :any_skip_relocation, sierra:         "2fd3dc59a8c0c8fdccf8195265d320aaa7b5d67e9a81b5a085f27cc287e7370e"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "fb892db831aed57dbdcb2d3a81d78bd05c5b689376d4b7f14bffc56826205ce9"
+    sha256 cellar: :any_skip_relocation, yosemite:       "09eb19921c2ecce5bb02b185c1040caef07d18706866006bdd5fa428bf6b8560"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "00c2ebbdf243dded3a23783b3a22ee3705a53dd0ba50c292dcb190bb5bcebc9a"
   end
 
   head do
