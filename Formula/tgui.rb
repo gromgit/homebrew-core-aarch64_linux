fea8e5f1c9826344e92557d955e23b51b0a016dc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 10:37:36 +0000

    tgui: update 0.9.1 bottle.

--- Formula/tgui.rb
+++ Formula/tgui.rb
@@ -6,10 +6,12 @@ class Tgui < Formula
   license "Zlib"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b58a6d6ff3990f2afb85ea39073e00ce31d91c16d7c88c6c6f1c4f22c4f4f674"
-    sha256 cellar: :any, big_sur:       "43215426baf657e7f73aa0751c3c952d35c489025065ed530223353d254c6556"
-    sha256 cellar: :any, catalina:      "7e8a817332ba8a35526ee7dd0a499d4136780f54d905f4abd10fea287a185662"
-    sha256 cellar: :any, mojave:        "4f003c5fc1520d427c657af818d88168cba1eeda04652a8cc016d10ef3214b23"
+    sha256 cellar: :any, arm64_monterey: "8b941cab3a40750fc3764adb2d59eb09084e1bfb05f93a535f79554f72e295db"
+    sha256 cellar: :any, arm64_big_sur:  "b58a6d6ff3990f2afb85ea39073e00ce31d91c16d7c88c6c6f1c4f22c4f4f674"
+    sha256 cellar: :any, monterey:       "5e3d966222709fa8d3e5b816c9c4b7e58c8efdd4edb35a7f93404f85cf4da8d1"
+    sha256 cellar: :any, big_sur:        "43215426baf657e7f73aa0751c3c952d35c489025065ed530223353d254c6556"
+    sha256 cellar: :any, catalina:       "7e8a817332ba8a35526ee7dd0a499d4136780f54d905f4abd10fea287a185662"
+    sha256 cellar: :any, mojave:         "4f003c5fc1520d427c657af818d88168cba1eeda04652a8cc016d10ef3214b23"
   end
 
   depends_on "cmake" => :build
