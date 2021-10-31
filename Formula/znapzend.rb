6041be4fcfc5e666a24d229a0807985efa7c7909
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 10:10:08 +0000

    znapzend: update 0.21.0 bottle.

--- Formula/znapzend.rb
+++ Formula/znapzend.rb
@@ -7,11 +7,13 @@ class Znapzend < Formula
   head "https://github.com/oetiker/znapzend.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "712e06398c1865796cc896a7d8c0d6c0baf4c10a52ac5006cc4c8d69b6359fcb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "03e855b5e75b84add0bd121a3f729112c7874ee09c74c2408479e1df095bc96c"
-    sha256 cellar: :any_skip_relocation, catalina:      "d4a36c0209fcb921c565bc9430ecf7a13ef3cfd226e32090c67af215c67b28ca"
-    sha256 cellar: :any_skip_relocation, mojave:        "2435d387ff28213402b816f40fdbf34bb733db8216ca72e66459b188d9e25ae5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d434f71ee8faa538bbf39b2cc6c328274d9f45873098f03bd7f4b2b33b05da40"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "851d5b6216c7083c7d4d3e164f1bd60ec2b2fdaf28a8f1ef186d2a61666a95e3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "712e06398c1865796cc896a7d8c0d6c0baf4c10a52ac5006cc4c8d69b6359fcb"
+    sha256 cellar: :any_skip_relocation, monterey:       "a07b9bdb9006c9e2ef7ca1eec263c177fd6347e864a92f46df781f7d70d94353"
+    sha256 cellar: :any_skip_relocation, big_sur:        "03e855b5e75b84add0bd121a3f729112c7874ee09c74c2408479e1df095bc96c"
+    sha256 cellar: :any_skip_relocation, catalina:       "d4a36c0209fcb921c565bc9430ecf7a13ef3cfd226e32090c67af215c67b28ca"
+    sha256 cellar: :any_skip_relocation, mojave:         "2435d387ff28213402b816f40fdbf34bb733db8216ca72e66459b188d9e25ae5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d434f71ee8faa538bbf39b2cc6c328274d9f45873098f03bd7f4b2b33b05da40"
   end
 
   uses_from_macos "perl"
