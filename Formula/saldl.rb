75be9f2d766664727deaf64edc22cd36c70ae11d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 13:47:58 +0000

    saldl: update 41 bottle.

--- Formula/saldl.rb
+++ Formula/saldl.rb
@@ -7,10 +7,12 @@ class Saldl < Formula
   head "https://github.com/saldl/saldl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2b965040a5e53c33801a86f8090a3339b8967903b1a11c3cc5b8bfca9e9de33c"
-    sha256 cellar: :any, big_sur:       "787ac1c4f96155b215ebc0eb06b6fbb404ac4dcca1cd88670c127ec1d504d709"
-    sha256 cellar: :any, catalina:      "9ac9aaa7e2882bcdb821c33c5bf7013b2ba1221242c1ad7d3da4e85d8febb324"
-    sha256 cellar: :any, mojave:        "3384921f2669ccb3e6021260c81b9494165ddf32f8f69dc0d4196910d4afe6be"
+    sha256 cellar: :any, arm64_monterey: "7d946fb405f3e50a3bd407a5deb8d1dc1c71094cd5092d9a314385e0165d446b"
+    sha256 cellar: :any, arm64_big_sur:  "2b965040a5e53c33801a86f8090a3339b8967903b1a11c3cc5b8bfca9e9de33c"
+    sha256 cellar: :any, monterey:       "bf33da07c23b69e41cdc44c0bcef25625128da44e134ac0f3417bd60fe67801e"
+    sha256 cellar: :any, big_sur:        "787ac1c4f96155b215ebc0eb06b6fbb404ac4dcca1cd88670c127ec1d504d709"
+    sha256 cellar: :any, catalina:       "9ac9aaa7e2882bcdb821c33c5bf7013b2ba1221242c1ad7d3da4e85d8febb324"
+    sha256 cellar: :any, mojave:         "3384921f2669ccb3e6021260c81b9494165ddf32f8f69dc0d4196910d4afe6be"
   end
 
   depends_on "asciidoc" => :build
