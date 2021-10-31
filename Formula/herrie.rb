851d026a5331dc41c081bcdad7a26a52b4977c94
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 17:02:00 +0000

    herrie: update 2.2_1 bottle.

--- Formula/herrie.rb
+++ Formula/herrie.rb
@@ -8,10 +8,12 @@ class Herrie < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "2e6dfaa27fef44c8bc0b4caf3250fed6dfcfca8cbdb5f8104a0ba1dc781371c1"
-    sha256 big_sur:       "41be543b395d9650113133b77d4d03d37febc9ecba23cb5f85abe8deb681f011"
-    sha256 catalina:      "8f1d86edb139e3385ac225852623f9c5018779a81b10c307a3ae872c1e64392d"
-    sha256 mojave:        "4e3258f904116ff2eebbe9436ff3ea3bae676fe15bd0ea193d1d934353e2ffe3"
+    sha256 arm64_monterey: "6432c7e843bd2580b1b736d4df4cdc37a8e80133ec46e7707cd379e662f5f2ed"
+    sha256 arm64_big_sur:  "2e6dfaa27fef44c8bc0b4caf3250fed6dfcfca8cbdb5f8104a0ba1dc781371c1"
+    sha256 monterey:       "22afb3f4e7d5021cfd0dea01a4f64b8259134ce05b425882f118d2b40f9764ca"
+    sha256 big_sur:        "41be543b395d9650113133b77d4d03d37febc9ecba23cb5f85abe8deb681f011"
+    sha256 catalina:       "8f1d86edb139e3385ac225852623f9c5018779a81b10c307a3ae872c1e64392d"
+    sha256 mojave:         "4e3258f904116ff2eebbe9436ff3ea3bae676fe15bd0ea193d1d934353e2ffe3"
   end
 
   deprecate! date: "2021-02-09", because: :repo_archived
