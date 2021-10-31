b0175f3e93b4fb67a2d6e8544922c868e9367622
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 17:10:06 +0000

    bcpp: update 20210108 bottle.

--- Formula/bcpp.rb
+++ Formula/bcpp.rb
@@ -11,10 +11,12 @@ class Bcpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1c7332e45d68c7c34e04b36935495e8e189944442650379f2c920757f2b210b7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "447070d7c227cdb2e5c8df360c8ea31c8f9fa89b39e2092a3a888a40caedb523"
-    sha256 cellar: :any_skip_relocation, catalina:      "1f2a9da46190bde2855e3bdc5d430302c831e3ff0eb3e3c34f8754bbe73744da"
-    sha256 cellar: :any_skip_relocation, mojave:        "1872e08cd8d7addb8459865d451622d05ed4f4fc2f91e3a6f144ba1fe483b27a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1ef362f6583a9a5779f8fb0a5237cc554615161d4f7f9fd5b054e0a59b51d917"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c7332e45d68c7c34e04b36935495e8e189944442650379f2c920757f2b210b7"
+    sha256 cellar: :any_skip_relocation, monterey:       "43a5d452995b4befa7498697435d30594bf4aa619750ef9f60d8192a35332ee0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "447070d7c227cdb2e5c8df360c8ea31c8f9fa89b39e2092a3a888a40caedb523"
+    sha256 cellar: :any_skip_relocation, catalina:       "1f2a9da46190bde2855e3bdc5d430302c831e3ff0eb3e3c34f8754bbe73744da"
+    sha256 cellar: :any_skip_relocation, mojave:         "1872e08cd8d7addb8459865d451622d05ed4f4fc2f91e3a6f144ba1fe483b27a"
   end
 
   def install
