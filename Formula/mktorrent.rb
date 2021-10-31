c6792889ccd35d7a8dce8566414949e5215d8e90
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 11:09:57 +0800

    mktorrent: update 1.1_1 bottle.

--- Formula/mktorrent.rb
+++ Formula/mktorrent.rb
@@ -9,6 +9,7 @@ class Mktorrent < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "a3b1b1184becd346de3398d0f32c7f2bfc5b72700414760f2bfbb787f7ef4453"
     sha256 cellar: :any,                 arm64_big_sur:  "52e68c18ada643d382daa660c5bc697a6a6559abeba4138e87b722054668edf8"
+    sha256 cellar: :any,                 monterey:       "08dbd0acde38acd472a4411cac8cb6d1e2c9a4bc2d1e609eaa8185a2ce6c3f03"
     sha256 cellar: :any,                 big_sur:        "d4f6a644ffc64e3ffba7559569abc381de67f4cbc64245d6c1548ebb1cb5262d"
     sha256 cellar: :any,                 catalina:       "3c9a180d450b8e49d1c4a6fc967df8599f602f955b7b27f8589b2052e0d77a91"
     sha256 cellar: :any,                 mojave:         "22bc8649ce5fea25549610eec4110d45f3fa1d05335cfc982df82806ff34d71b"
