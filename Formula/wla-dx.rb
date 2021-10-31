875fd1c9a2b6f2322cd378117d5c0f800df9af13
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 11:01:56 +0000

    wla-dx: update 10.0 bottle.

--- Formula/wla-dx.rb
+++ Formula/wla-dx.rb
@@ -12,11 +12,13 @@ class WlaDx < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8d3ae6b8ecb78d43c5d475c3e12a2ab139608682ac50205541aa2792738121a8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b5f923d5b2868582fb46f23ed340246d921fd6f58e4119cb65172eb76f5156f7"
-    sha256 cellar: :any_skip_relocation, catalina:      "5158594f32b15295a50814c6108800d7f689371e39a5a363e035f3be49e18be9"
-    sha256 cellar: :any_skip_relocation, mojave:        "a01d1aab6f7380a4fa44facf9aeb31a9578d25acc093c2cb8743cbec7ecacf10"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0dc2d2cc7aaa6f84b5795290bb7404ed0e95786d2ad89a58230dfe7bed705d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "723085318854f1bdbb5c58446a858841d1e276e3f6179584643a21a0d3abce9d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8d3ae6b8ecb78d43c5d475c3e12a2ab139608682ac50205541aa2792738121a8"
+    sha256 cellar: :any_skip_relocation, monterey:       "8528e631fd44a87cf339284fbd73232772d5ceeaa26682e3be4ce1c7d06d4fc2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b5f923d5b2868582fb46f23ed340246d921fd6f58e4119cb65172eb76f5156f7"
+    sha256 cellar: :any_skip_relocation, catalina:       "5158594f32b15295a50814c6108800d7f689371e39a5a363e035f3be49e18be9"
+    sha256 cellar: :any_skip_relocation, mojave:         "a01d1aab6f7380a4fa44facf9aeb31a9578d25acc093c2cb8743cbec7ecacf10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0dc2d2cc7aaa6f84b5795290bb7404ed0e95786d2ad89a58230dfe7bed705d6"
   end
 
   depends_on "cmake" => :build
