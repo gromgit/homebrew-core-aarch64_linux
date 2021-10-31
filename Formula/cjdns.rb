7deb7cc148c9b6aeb17d6f06fc34ea2bc9bb78f4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 07:42:16 +0000

    cjdns: update 21.1 bottle.

--- Formula/cjdns.rb
+++ Formula/cjdns.rb
@@ -7,6 +7,7 @@ class Cjdns < Formula
   head "https://github.com/cjdelisle/cjdns.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey: "5ba943bc0a44c52c2f23bae683e9fb28b7d1c11e76e5c6cfe7d97f5fbc65c1b1"
     sha256 cellar: :any_skip_relocation, big_sur:  "b57d1c38866eab0c671732fdf70890fde76e7b03a2fc6a3dc59d5dea29e9fbaa"
     sha256 cellar: :any_skip_relocation, catalina: "dc8c73f740f3bfbde7db45dde12cbc57bc34c925b88a15be55e4ba47b73eb1d4"
     sha256 cellar: :any_skip_relocation, mojave:   "6f5536caa2f432a96027fa223e20b12d54baa78f96a7d5cabb454ad380faf523"
