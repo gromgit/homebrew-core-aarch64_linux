787763bd5e042ab1a5d7eb62f809c62da9d2d387
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 13:36:00 +0000

    halibut: update 1.2 bottle.

--- Formula/halibut.rb
+++ Formula/halibut.rb
@@ -7,12 +7,14 @@ class Halibut < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bbe2900f2e05ece4838ae138e287192fce7c97ed648994f15b19c91304e26154"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2f3dc434449de2f5d18bb5019dc62a966cba2981eb88d59d77d34b2049e44f2d"
-    sha256 cellar: :any_skip_relocation, catalina:      "e9bd74c1ab130f4abc824906bf1f73f910032a4c7c0938798f7fbab2f1346020"
-    sha256 cellar: :any_skip_relocation, mojave:        "05f0236c180aeab690979615812fb72642e7cdeaccb35ebb865a53aadb35e7c6"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "fe74b9670ae0d996a17de4a70a140365d057a83a643125dcbd16b33dacad9f6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f37d878d7f5d8a0869220fc00abdad12f298da1cdb1d2541b3f66793f749e9b4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "edd2a0fb184a9f0b782886d86d149dbc400fc3c32488fe089603e111147a3f88"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bbe2900f2e05ece4838ae138e287192fce7c97ed648994f15b19c91304e26154"
+    sha256 cellar: :any_skip_relocation, monterey:       "7274b17081b5c45aa8986ed7d1d02e1b0f8e95c519e8a5cf5a04bbc1798450dc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2f3dc434449de2f5d18bb5019dc62a966cba2981eb88d59d77d34b2049e44f2d"
+    sha256 cellar: :any_skip_relocation, catalina:       "e9bd74c1ab130f4abc824906bf1f73f910032a4c7c0938798f7fbab2f1346020"
+    sha256 cellar: :any_skip_relocation, mojave:         "05f0236c180aeab690979615812fb72642e7cdeaccb35ebb865a53aadb35e7c6"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "fe74b9670ae0d996a17de4a70a140365d057a83a643125dcbd16b33dacad9f6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f37d878d7f5d8a0869220fc00abdad12f298da1cdb1d2541b3f66793f749e9b4"
   end
 
   def install
