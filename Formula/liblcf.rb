5b9184c4f34a50da4c7758421da5970da9d382de
Bo Anderson <mail@boanderson.me>
2021-10-31 01:51:17 +0100

    liblcf: revision
    * liblcf: revision bump for icu4c
    * liblcf: update 0.7.0_1 bottle.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/liblcf.rb
+++ Formula/liblcf.rb
@@ -4,15 +4,16 @@ class Liblcf < Formula
   url "https://easyrpg.org/downloads/player/0.7.0/liblcf-0.7.0.tar.xz"
   sha256 "ed76501bf973bf2f5bd7240ab32a8ae3824dce387ef7bb3db8f6c073f0bc7a6a"
   license "MIT"
+  revision 1
   head "https://github.com/EasyRPG/liblcf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7f1164e942934fc234d035cfc9eab11330831244bc69a96061fb3cefc00dd03b"
-    sha256 cellar: :any,                 arm64_big_sur:  "be2a943fe7db52a0a29c910e04f981464a58e0cab77b65b3f0c39fd56490635b"
-    sha256 cellar: :any,                 monterey:       "5520823380b4ff8f68aeaacef327c264e6edd9208e30acd717d792d527aa3763"
-    sha256 cellar: :any,                 big_sur:        "7ef6b01e609ba9d7f4d57d28b97e396122b6f09245954a6229493d86ea3aa879"
-    sha256 cellar: :any,                 catalina:       "f73afcd81ca34da7633475f9ee5cb470b12fd5e485199bc5073d6d30dacbb77f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b640ca618fe564b9377ebdb7d7f3b7adcdcbef7690ae2e4a37b473d21c0e255a"
+    sha256 cellar: :any,                 arm64_monterey: "35a263d892d4bbde6669efaa33eef17676a038add327c38f230dea635e294a8e"
+    sha256 cellar: :any,                 arm64_big_sur:  "2bc63c7a01e2dca4b6e701be0f696c2aff0da5e451e0ffed456a37ffd9507129"
+    sha256 cellar: :any,                 monterey:       "b1824bb8df43c238e9ea5717b3cf34a4083ac4104565ede72b61313c32c05821"
+    sha256 cellar: :any,                 big_sur:        "86d3d392e551484d23cbba9ada5069984f4213ca486ca4980f65375f8e03faa9"
+    sha256 cellar: :any,                 catalina:       "9fb8b516f6420faf872102d14c6e6085a14f382b1aef5de71952b88650bf66ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a10abcb630c5b44108dff330404ed34b57a5af31ebda4d6a2582e391b8dce4e"
   end
 
   depends_on "cmake" => :build
