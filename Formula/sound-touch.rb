f000108e56aa4e118af85d6379aaf0b58c0bf3e4
Thierry Moisan <thierry.moisan@gmail.com>
2021-10-31 19:22:27 +0000

    sound-touch: update 2.3.1 bottle.

--- Formula/sound-touch.rb
+++ Formula/sound-touch.rb
@@ -7,10 +7,12 @@ class SoundTouch < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "1ccbe6750451a745654c3b00c8f14e1706381074254136278bb4cc1f7b88e009"
-    sha256 cellar: :any,                 big_sur:       "cbc1dd06e73d9712e1b1fa21c4767967f122a40e2cd92097c912084f002cc4d5"
-    sha256 cellar: :any,                 catalina:      "650c953db2306d78bd309a24206c014037584c08a490e05a89ac5294cef36df4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af9833833154ab73181849ccdb563b1f4146848f69fd3cc4b386c9e46a31d8b8"
+    sha256 cellar: :any,                 arm64_monterey: "0d340cfb0bf1d17b2eea238b756e58cd0559a4e394394b0f5e2031114e75506e"
+    sha256 cellar: :any,                 arm64_big_sur:  "1ccbe6750451a745654c3b00c8f14e1706381074254136278bb4cc1f7b88e009"
+    sha256 cellar: :any,                 monterey:       "ecf1be5a1ff7d41f7346f32c238ae3f30f965e58252e994082c4f2c7dd7d2784"
+    sha256 cellar: :any,                 big_sur:        "cbc1dd06e73d9712e1b1fa21c4767967f122a40e2cd92097c912084f002cc4d5"
+    sha256 cellar: :any,                 catalina:       "650c953db2306d78bd309a24206c014037584c08a490e05a89ac5294cef36df4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af9833833154ab73181849ccdb563b1f4146848f69fd3cc4b386c9e46a31d8b8"
   end
 
   depends_on "autoconf" => :build
