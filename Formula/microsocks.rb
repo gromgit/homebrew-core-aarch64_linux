ea0c86bb98c8c4e938650923477c46268e289fc8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 10:49:52 +0000

    microsocks: update 1.0.2 bottle.

--- Formula/microsocks.rb
+++ Formula/microsocks.rb
@@ -7,11 +7,13 @@ class Microsocks < Formula
   head "https://github.com/rofl0r/microsocks.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ac28cf21d02ba3d7d48950bfab977718e5aaef2eac6e19f0885a5e54cc5bdd92"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f80592c439fb03b85318e2356ff0c9481b0dc6643b4224697f359fcbb9d585ce"
-    sha256 cellar: :any_skip_relocation, catalina:      "95c80ff1e1fe1f25efa6c5bd2498c969575978c0bac2935b293ae1dc6a0cfef5"
-    sha256 cellar: :any_skip_relocation, mojave:        "007187db61ac04906954220f606b66d23d00d04457ce94667b0f59f82ac1bfcc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4356d57b9e923021407147fb19c46d36223daffc5ed6c2475eae751ca70da3a5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1281153db801e84fd1db562c877cf3537412cd86e029de0e5ea1846eec8c7dd9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ac28cf21d02ba3d7d48950bfab977718e5aaef2eac6e19f0885a5e54cc5bdd92"
+    sha256 cellar: :any_skip_relocation, monterey:       "ce98ce63848e856cea90e0e75eb435a7aea76cce34c8c8c5b4ef8cbefe09e669"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f80592c439fb03b85318e2356ff0c9481b0dc6643b4224697f359fcbb9d585ce"
+    sha256 cellar: :any_skip_relocation, catalina:       "95c80ff1e1fe1f25efa6c5bd2498c969575978c0bac2935b293ae1dc6a0cfef5"
+    sha256 cellar: :any_skip_relocation, mojave:         "007187db61ac04906954220f606b66d23d00d04457ce94667b0f59f82ac1bfcc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4356d57b9e923021407147fb19c46d36223daffc5ed6c2475eae751ca70da3a5"
   end
 
   def install
