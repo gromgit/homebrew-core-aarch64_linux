7ee06db74ee93025443970b4153562f8dacfe13c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 10:28:49 +0000

    libcds: update 2.3.3_1 bottle.

--- Formula/libcds.rb
+++ Formula/libcds.rb
@@ -7,11 +7,13 @@ class Libcds < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "52e6bee0d1b0f1dfea3ae69573b6a3a92c0ff42ddb41634c464fc35672e94bf4"
-    sha256 cellar: :any,                 big_sur:       "029e18020211d4f155d07a9716303309c1b3f8d685cbd167d87f476dfe8f77a1"
-    sha256 cellar: :any,                 catalina:      "9962a58f2df627f74d0c248397cc8bb8a501f0380d99bfc3a84365070ab902fc"
-    sha256 cellar: :any,                 mojave:        "1667d75383b82cd2365808502c9468ca1e47aad4d6c5943b02e1aa258cad3fe4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "403e15797cf8fd6c2caea7427e964fe4e3392eb1685d7ff9203a0d72cde26aac"
+    sha256 cellar: :any,                 arm64_monterey: "e7edddfac2c3ecf31d6a7acfd6d261019c47bf67a603dcbcdd8cbea524c632fc"
+    sha256 cellar: :any,                 arm64_big_sur:  "52e6bee0d1b0f1dfea3ae69573b6a3a92c0ff42ddb41634c464fc35672e94bf4"
+    sha256 cellar: :any,                 monterey:       "c5308e1c184a4a60063671305eadb56462fd60af510e437491640d9faee0f95d"
+    sha256 cellar: :any,                 big_sur:        "029e18020211d4f155d07a9716303309c1b3f8d685cbd167d87f476dfe8f77a1"
+    sha256 cellar: :any,                 catalina:       "9962a58f2df627f74d0c248397cc8bb8a501f0380d99bfc3a84365070ab902fc"
+    sha256 cellar: :any,                 mojave:         "1667d75383b82cd2365808502c9468ca1e47aad4d6c5943b02e1aa258cad3fe4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "403e15797cf8fd6c2caea7427e964fe4e3392eb1685d7ff9203a0d72cde26aac"
   end
 
   depends_on "cmake" => :build
