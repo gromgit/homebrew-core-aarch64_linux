0e9fa01436d78f28ae19c689f67881394d86d6fa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 08:55:46 +0000

    collector-sidecar: update 1.1.0 bottle.

--- Formula/collector-sidecar.rb
+++ Formula/collector-sidecar.rb
@@ -7,11 +7,13 @@ class CollectorSidecar < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "24494acc03245693ab94d61f3aa8903b1006243713fa3fac91e499c7de885b44"
-    sha256 cellar: :any_skip_relocation, big_sur:       "689ede327bbf176b9c4f3d544f90f5940b1d026151b91c50a0100edd268c48ec"
-    sha256 cellar: :any_skip_relocation, catalina:      "30f35fc66f276071c2126fbe77f7bb7b3a7852a1c45adc2d5532a8340b421902"
-    sha256 cellar: :any_skip_relocation, mojave:        "193227a80963772b7a480713f3d104fe9e3a1dc7e1c7122a26292fd5bf5c0708"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21646bc7c252e5052a04341e4305fdd3821501206a7052f9ce1a83e8f8816de6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "71aa58d8b75b684ecb4f4aaa44ba2e671ae1cf55c2bf5d0085154332f4a81ff1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24494acc03245693ab94d61f3aa8903b1006243713fa3fac91e499c7de885b44"
+    sha256 cellar: :any_skip_relocation, monterey:       "3f943a1a0a1a6035c6e4c234f128ed24bc9d4cc7bb565769963a961e91b28bd4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "689ede327bbf176b9c4f3d544f90f5940b1d026151b91c50a0100edd268c48ec"
+    sha256 cellar: :any_skip_relocation, catalina:       "30f35fc66f276071c2126fbe77f7bb7b3a7852a1c45adc2d5532a8340b421902"
+    sha256 cellar: :any_skip_relocation, mojave:         "193227a80963772b7a480713f3d104fe9e3a1dc7e1c7122a26292fd5bf5c0708"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21646bc7c252e5052a04341e4305fdd3821501206a7052f9ce1a83e8f8816de6"
   end
 
   depends_on "go" => :build
