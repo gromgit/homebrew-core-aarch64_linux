eef3d15b74448c760228697a007e8ef91a745aa4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 14:05:55 +0000

    chipmunk: update 7.0.3 bottle.

--- Formula/chipmunk.rb
+++ Formula/chipmunk.rb
@@ -8,13 +8,15 @@ class Chipmunk < Formula
   head "https://github.com/slembcke/Chipmunk2D.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "53a1d8968efd45940eda303182f7a68be0e31221295a85e803f92f3c968c45ad"
-    sha256 cellar: :any,                 big_sur:       "6f68cb2c7dfecb8ef8b4572257ce7dd86d3de49af8c6073b173996020132902f"
-    sha256 cellar: :any,                 catalina:      "b71191c2c1e4859cb9d5e77b8684612dec1c191780a0b1d56afc04ada66da036"
-    sha256 cellar: :any,                 mojave:        "16292e5518bae60c6990a6f1565e1416f91ffe1c878ab43b58465bb2a24d3d11"
-    sha256 cellar: :any,                 high_sierra:   "5370b9d8db489d6b8944c23fd4906768c84d87e22f054ca3381c7ee527233f4d"
-    sha256 cellar: :any,                 sierra:        "c92a9c1134a272244ca3936b2c94431df7ed7002a9eec99f6914fe1128adae12"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe06ebff94b32562d036af4c9e34edb95a32fc4bdae48b846b1f21702441dfe2"
+    sha256 cellar: :any,                 arm64_monterey: "544c8185e3366a6b067ad4cafb6272610014f0b5787b0c35a735b2c6bc3c7588"
+    sha256 cellar: :any,                 arm64_big_sur:  "53a1d8968efd45940eda303182f7a68be0e31221295a85e803f92f3c968c45ad"
+    sha256 cellar: :any,                 monterey:       "a404cce4a0aa17f7f4a0645c38ecc7b318a8fa1576d1168b5e4db40a51de9c12"
+    sha256 cellar: :any,                 big_sur:        "6f68cb2c7dfecb8ef8b4572257ce7dd86d3de49af8c6073b173996020132902f"
+    sha256 cellar: :any,                 catalina:       "b71191c2c1e4859cb9d5e77b8684612dec1c191780a0b1d56afc04ada66da036"
+    sha256 cellar: :any,                 mojave:         "16292e5518bae60c6990a6f1565e1416f91ffe1c878ab43b58465bb2a24d3d11"
+    sha256 cellar: :any,                 high_sierra:    "5370b9d8db489d6b8944c23fd4906768c84d87e22f054ca3381c7ee527233f4d"
+    sha256 cellar: :any,                 sierra:         "c92a9c1134a272244ca3936b2c94431df7ed7002a9eec99f6914fe1128adae12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe06ebff94b32562d036af4c9e34edb95a32fc4bdae48b846b1f21702441dfe2"
   end
 
   depends_on "cmake" => :build
