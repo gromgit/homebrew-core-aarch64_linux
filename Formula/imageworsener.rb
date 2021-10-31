ad070fee606b03c50ff904716b1dee69cdb81de5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 15:41:37 +0800

    imageworsener: patch incorrect -flat_namespace usage (#88410)
    This is needed for bottling on Monterey.

--- Formula/imageworsener.rb
+++ Formula/imageworsener.rb
@@ -1,9 +1,18 @@
 class Imageworsener < Formula
   desc "Utility and library for image scaling and processing"
   homepage "https://entropymine.com/imageworsener/"
-  url "https://entropymine.com/imageworsener/imageworsener-1.3.4.tar.gz"
-  sha256 "bae0b2bb35e565133dd804a6f4af303992527f53068cd67b03e5d9961d8512b6"
   license "MIT"
+
+  stable do
+    url "https://entropymine.com/imageworsener/imageworsener-1.3.4.tar.gz"
+    sha256 "bae0b2bb35e565133dd804a6f4af303992527f53068cd67b03e5d9961d8512b6"
+
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    end
+  end
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "0ee5c5f12bf988c164ce3ea06ce3c6a22af96427edaf241fb68f91c3e951d2de"
