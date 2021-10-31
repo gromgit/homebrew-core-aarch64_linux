ceedac40c33e243b985044aa01272ccc383cb2a2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 16:50:38 +0000

    moe: update 1.11 bottle.

--- Formula/moe.rb
+++ Formula/moe.rb
@@ -7,11 +7,13 @@ class Moe < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "ecf7d889fc677d4fbd201086dc195d5d072dfdbc78fc0c506104a8a1e5216365"
-    sha256 big_sur:       "fd26036b9c0e0c72963f91b99f1a0787109af0a519df1d33d0f04d0d0cc12ebe"
-    sha256 catalina:      "38b7920c9d82ba731f98bd1a56932b0d0ebe675d6d9006848a48e392013aad5a"
-    sha256 mojave:        "688fc7c768e785581675079dd436c9cf3fef36094ea1aa078a8c3fc221d00fbc"
-    sha256 x86_64_linux:  "b54eb559d79b40ec9054ac5cea562fbd562c963c9965a126b99ce082e7c859d0"
+    sha256 arm64_monterey: "55c6298deb23524518371ef927aae3e2a4f9c6e2dc52a9318623e0b8f1c41bc7"
+    sha256 arm64_big_sur:  "ecf7d889fc677d4fbd201086dc195d5d072dfdbc78fc0c506104a8a1e5216365"
+    sha256 monterey:       "c6defb99e4c9c3743c5f1ff438d543a5ce6390d8fc3f96e5594fc22dcff50a9b"
+    sha256 big_sur:        "fd26036b9c0e0c72963f91b99f1a0787109af0a519df1d33d0f04d0d0cc12ebe"
+    sha256 catalina:       "38b7920c9d82ba731f98bd1a56932b0d0ebe675d6d9006848a48e392013aad5a"
+    sha256 mojave:         "688fc7c768e785581675079dd436c9cf3fef36094ea1aa078a8c3fc221d00fbc"
+    sha256 x86_64_linux:   "b54eb559d79b40ec9054ac5cea562fbd562c963c9965a126b99ce082e7c859d0"
   end
 
   uses_from_macos "ncurses"
