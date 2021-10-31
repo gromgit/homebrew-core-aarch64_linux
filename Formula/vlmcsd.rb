2cb6020b46c80d02b27ffd6cce4d1cc5c9557582
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 16:16:00 +0000

    vlmcsd: update svn1113 bottle.

--- Formula/vlmcsd.rb
+++ Formula/vlmcsd.rb
@@ -14,11 +14,13 @@ class Vlmcsd < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7b3abfda639485474805f9d4d93f2c6e47efacd9e8affbed3aca44bda55c1964"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4e7ff7a7b2b24f12671783aba5e87a444576418ec0220d037dbe25d5f1e2ff71"
-    sha256 cellar: :any_skip_relocation, catalina:      "1b6375150a6cbd27eb386f0fae0bcbbccdfc9b3079dc6cfb5a9ce633029d5484"
-    sha256 cellar: :any_skip_relocation, mojave:        "d2b0cccd86ab053118aebc1885b362130b7c7e0f73f3b60c768e4907532254cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8bacd07ef0cda2ea4ad4c5d7274b97bf8b067b69fd4412d20272b0d1c8828d1c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "20f3ba285635e158a02b7cb528e25eda9fa45b6a832f5893536e88b6e965a332"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7b3abfda639485474805f9d4d93f2c6e47efacd9e8affbed3aca44bda55c1964"
+    sha256 cellar: :any_skip_relocation, monterey:       "46d9330798889d87f2e2013b99fed4416124fa119d59591aadc7ebd80197c024"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4e7ff7a7b2b24f12671783aba5e87a444576418ec0220d037dbe25d5f1e2ff71"
+    sha256 cellar: :any_skip_relocation, catalina:       "1b6375150a6cbd27eb386f0fae0bcbbccdfc9b3079dc6cfb5a9ce633029d5484"
+    sha256 cellar: :any_skip_relocation, mojave:         "d2b0cccd86ab053118aebc1885b362130b7c7e0f73f3b60c768e4907532254cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bacd07ef0cda2ea4ad4c5d7274b97bf8b067b69fd4412d20272b0d1c8828d1c"
   end
 
   uses_from_macos "llvm" => :build
