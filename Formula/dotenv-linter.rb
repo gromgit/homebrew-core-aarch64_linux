d199d748659d6cf13ba5f3bb19d663bfb2698c5b
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-10-31 23:19:28 +0000

    dotenv-linter: update 3.1.1 bottle.

--- Formula/dotenv-linter.rb
+++ Formula/dotenv-linter.rb
@@ -7,11 +7,13 @@ class DotenvLinter < Formula
   head "https://github.com/dotenv-linter/dotenv-linter.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bcfbdff2f21f9d7fa1b28bf5edb68c695721eec092b4e240ec68d5102c565050"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5b6fd178bf335818ffc7e35a7f333bb47bb451829c24671e70f4140e37e0e080"
-    sha256 cellar: :any_skip_relocation, catalina:      "2f6ccdd1a2c589bbea2deefdbf5f017a9c0380cc1ee4e6e16f45b0160fe1faa6"
-    sha256 cellar: :any_skip_relocation, mojave:        "ca344c71611590a50f18c25c7dc4a1dc945d018e9de8fb77fb4b67db516761f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39b46bf6ca08601e0af98d5cd1fecb715d1789c1df8e2c358255a2fd93eda0d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c04c036c9bcece657db245e527c7fb89d201c7a9dda9033745fa040f2639d56a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bcfbdff2f21f9d7fa1b28bf5edb68c695721eec092b4e240ec68d5102c565050"
+    sha256 cellar: :any_skip_relocation, monterey:       "8dcff983c616efd4e2b0ec0acfd3228fb989627ea85e2592c58df7352ed92ce0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5b6fd178bf335818ffc7e35a7f333bb47bb451829c24671e70f4140e37e0e080"
+    sha256 cellar: :any_skip_relocation, catalina:       "2f6ccdd1a2c589bbea2deefdbf5f017a9c0380cc1ee4e6e16f45b0160fe1faa6"
+    sha256 cellar: :any_skip_relocation, mojave:         "ca344c71611590a50f18c25c7dc4a1dc945d018e9de8fb77fb4b67db516761f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39b46bf6ca08601e0af98d5cd1fecb715d1789c1df8e2c358255a2fd93eda0d6"
   end
 
   depends_on "rust" => :build
