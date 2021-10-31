3b683218443422e511867c9f406997505282b3fe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 04:54:51 +0000

    ezstream: update 1.0.2 bottle.

--- Formula/ezstream.rb
+++ Formula/ezstream.rb
@@ -12,11 +12,13 @@ class Ezstream < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "188838a38d3573fc77ffd5684e0e7759b24d550ffdd895243425e13c29e038c2"
-    sha256 cellar: :any,                 big_sur:       "fbfe1082559a1313ee3ff071ad35866fb20d5fb360fbfc634fbf85ac48c3e94d"
-    sha256 cellar: :any,                 catalina:      "2854c21def8d7e97747aeca5e856833d17780698739e581a192059c58f50ffa2"
-    sha256 cellar: :any,                 mojave:        "cfc4088a51cdcb0a586ee2a796d5a515d89007bebfae0f7bfd6b2a4c7a2c13f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0c88f43345c0ebb3eca025869b579fa86820e12268fdb0e79d77fa2ee16a296"
+    sha256 cellar: :any,                 arm64_monterey: "22efe55635691409ca6c53eda5be73a7667c8ca59f0076b46380b67e663f5283"
+    sha256 cellar: :any,                 arm64_big_sur:  "188838a38d3573fc77ffd5684e0e7759b24d550ffdd895243425e13c29e038c2"
+    sha256 cellar: :any,                 monterey:       "07ec03e5e37aee0593f5d9121b9bc0ccb071df6c5ee520fc619991820ca90f31"
+    sha256 cellar: :any,                 big_sur:        "fbfe1082559a1313ee3ff071ad35866fb20d5fb360fbfc634fbf85ac48c3e94d"
+    sha256 cellar: :any,                 catalina:       "2854c21def8d7e97747aeca5e856833d17780698739e581a192059c58f50ffa2"
+    sha256 cellar: :any,                 mojave:         "cfc4088a51cdcb0a586ee2a796d5a515d89007bebfae0f7bfd6b2a4c7a2c13f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f0c88f43345c0ebb3eca025869b579fa86820e12268fdb0e79d77fa2ee16a296"
   end
 
   head do
