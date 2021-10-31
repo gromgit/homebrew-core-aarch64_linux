c9c49e63ffa860f66d2985f8b430235b9eadf381
rui <rui@chenrui.dev>
2021-10-31 16:24:17 +0000

    fnlfmt: add 0.2.2 bottle.

--- Formula/fnlfmt.rb
+++ Formula/fnlfmt.rb
@@ -5,6 +5,10 @@ class Fnlfmt < Formula
   sha256 "9e14d6750495c78daa7a474ff2d44bccee260e3d39e55dd61d86986e75bd9def"
   license "LGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "18b456dcb6cc242942c5677c9b8ab8e74274426f5eba01038819dd5b71f9db79"
+  end
+
   depends_on "lua"
 
   def install
