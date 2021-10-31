5759787bd72424d20c6950299bb1e45bac4b582a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 04:32:40 +0000

    vc: update 1.4.2 bottle.

--- Formula/vc.rb
+++ Formula/vc.rb
@@ -6,10 +6,12 @@ class Vc < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "67e2a123067f4885b3779f9a8f005988ae16926c34298fc069d5f8c2f53f60e5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8850a8e86a3ff2810f491ce25af976ec85e49601ba0b094a6543e3c0b665540b"
-    sha256 cellar: :any_skip_relocation, catalina:      "b1f8a4e74cae6267405569a0e4c774c8c68cd258cb61e56e50208f4a32d65d2a"
-    sha256 cellar: :any_skip_relocation, mojave:        "b2b19a6798b4dd6db4355ab6d069e4b645dec1790c231a18c09e6a2a9ecf0a3f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3c8d1af6073bdeb469cc870f57786abd0293cebeb82f32122157428ab7ae7cc9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "67e2a123067f4885b3779f9a8f005988ae16926c34298fc069d5f8c2f53f60e5"
+    sha256 cellar: :any_skip_relocation, monterey:       "1a4687a8cea3e48b3047a577f07fd6a12742e036ed0d91a5790b4dde878dc9b0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8850a8e86a3ff2810f491ce25af976ec85e49601ba0b094a6543e3c0b665540b"
+    sha256 cellar: :any_skip_relocation, catalina:       "b1f8a4e74cae6267405569a0e4c774c8c68cd258cb61e56e50208f4a32d65d2a"
+    sha256 cellar: :any_skip_relocation, mojave:         "b2b19a6798b4dd6db4355ab6d069e4b645dec1790c231a18c09e6a2a9ecf0a3f"
   end
 
   depends_on "cmake" => :build
