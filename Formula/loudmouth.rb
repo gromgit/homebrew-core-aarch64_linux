ef572488b7b7268fbb09e300d9e724452d8e9e03
Bo Anderson <mail@boanderson.me>
2021-10-31 00:11:11 +0000

    loudmouth: update 1.5.4 bottle.

--- Formula/loudmouth.rb
+++ Formula/loudmouth.rb
@@ -15,11 +15,13 @@ class Loudmouth < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "0b60046b8a592ab656ed824b75774f2e9e8f9749b0a5edb024190019c36da766"
-    sha256 cellar: :any,                 big_sur:       "d770f0cd1a81375c306d0bc6fdd81610d27bc844fd5086518aaa7f8fa6252a14"
-    sha256 cellar: :any,                 catalina:      "b83be4ad6fce30f484015b344d21e3e425860b3c8a2cb6a609e059611d03caf9"
-    sha256 cellar: :any,                 mojave:        "681944a95c5642a4651110e5d91d88acf335176b34d85f0f159aef291f07b38d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6052693231034f7a87a85eb4e56755786089171b9ce5ad1e0babe0a891af55d9"
+    sha256 cellar: :any,                 arm64_monterey: "56ce5d15bd9625075e7160268936d0197a1116c1b3a095ebbbf190d2ac9becc7"
+    sha256 cellar: :any,                 arm64_big_sur:  "0b60046b8a592ab656ed824b75774f2e9e8f9749b0a5edb024190019c36da766"
+    sha256 cellar: :any,                 monterey:       "890ebdd35bf9275e771984de61b38557ff279e61d15e8c9d11813def0b65689a"
+    sha256 cellar: :any,                 big_sur:        "d770f0cd1a81375c306d0bc6fdd81610d27bc844fd5086518aaa7f8fa6252a14"
+    sha256 cellar: :any,                 catalina:       "b83be4ad6fce30f484015b344d21e3e425860b3c8a2cb6a609e059611d03caf9"
+    sha256 cellar: :any,                 mojave:         "681944a95c5642a4651110e5d91d88acf335176b34d85f0f159aef291f07b38d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6052693231034f7a87a85eb4e56755786089171b9ce5ad1e0babe0a891af55d9"
   end
 
   head do
