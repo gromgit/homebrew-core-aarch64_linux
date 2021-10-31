2ba51e85db7d7db0f193ea91b9466597792556bd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 13:44:58 +0000

    tmx: update 1.4.0 bottle.

--- Formula/tmx.rb
+++ Formula/tmx.rb
@@ -6,11 +6,13 @@ class Tmx < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "770cdb601ea6b496a29832960cb5fd79626a99f55f01c635985aa921f3e5f31d"
-    sha256 cellar: :any,                 big_sur:       "91e9846b6d59e0694918753e357736c229c2a70d8021fdbaa2eb506e5be746c2"
-    sha256 cellar: :any,                 catalina:      "1013715fdb263f6d6985c9145a5dbc05d2e41ba6c4aa28af766f0bc82a87f2c5"
-    sha256 cellar: :any,                 mojave:        "060eab2a5090afed9dfbf6ca716a2867b956be2222e6a623a5b98774bf06ef6e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ab5a06f59f128524607cfc604d1f7a5b40875572bd4516524fd61bb5db4ba13"
+    sha256 cellar: :any,                 arm64_monterey: "1741909448819ba29272e002c9d25fdb8cc315e0ea70991799e2bbfa4bcdb88f"
+    sha256 cellar: :any,                 arm64_big_sur:  "770cdb601ea6b496a29832960cb5fd79626a99f55f01c635985aa921f3e5f31d"
+    sha256 cellar: :any,                 monterey:       "fd62803bd77e4f17e11137d591ee0f916eea138b4e9a076355fea04f5a01d67c"
+    sha256 cellar: :any,                 big_sur:        "91e9846b6d59e0694918753e357736c229c2a70d8021fdbaa2eb506e5be746c2"
+    sha256 cellar: :any,                 catalina:       "1013715fdb263f6d6985c9145a5dbc05d2e41ba6c4aa28af766f0bc82a87f2c5"
+    sha256 cellar: :any,                 mojave:         "060eab2a5090afed9dfbf6ca716a2867b956be2222e6a623a5b98774bf06ef6e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ab5a06f59f128524607cfc604d1f7a5b40875572bd4516524fd61bb5db4ba13"
   end
 
   depends_on "cmake" => :build
