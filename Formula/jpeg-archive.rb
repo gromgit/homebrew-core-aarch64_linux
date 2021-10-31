8645ca00c77a704dfbf54ca1e8a99c81c4324e58
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 11:24:32 +0000

    jpeg-archive: update 2.2.0_1 bottle.

--- Formula/jpeg-archive.rb
+++ Formula/jpeg-archive.rb
@@ -7,6 +7,7 @@ class JpegArchive < Formula
   revision 1
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:    "3461975fc932a94798f2d7c6cad3f030081a29a24a31bc391e2344c6aa6ed177"
     sha256 cellar: :any_skip_relocation, big_sur:     "41ac0d9c5bd290d77e7e5548a2257c6455f9f87265b06b5dc4e02ac7836dfc22"
     sha256 cellar: :any_skip_relocation, catalina:    "222d7258f63f000794693bc5912c88ce42d0a33473a8acbbc585821655c9b8dd"
     sha256 cellar: :any_skip_relocation, mojave:      "2df1b3a007b7553addc977582d0c38d5007892f9e8a866a4fc9cda9b8f3b2af2"
