90823d22c435ae8f1796fe3e23febee293c90d81
Bo Anderson <mail@boanderson.me>
2021-10-31 01:22:30 +0000

    znc: revision
    * znc: revision bump for icu4c
    * znc: update 1.8.2_5 bottle.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/znc.rb
+++ Formula/znc.rb
@@ -4,16 +4,15 @@ class Znc < Formula
   url "https://znc.in/releases/archive/znc-1.8.2.tar.gz"
   sha256 "ff238aae3f2ae0e44e683c4aee17dc8e4fdd261ca9379d83b48a7d422488de0d"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   bottle do
-    sha256 arm64_monterey: "48bbc131756030fa8073447e12ac4d17dd02ef000510bb60d423bfbdee8618f0"
-    sha256 arm64_big_sur:  "f6976cbbb65b4261db7ca3ab26c4f442096dde743a9fd63afe282d551faa07b8"
-    sha256 monterey:       "557bd2025ab422e1f45779d35ffc7448f2f3be13ec5bdf686f5416c50fe67f46"
-    sha256 big_sur:        "5ead56d8a9fd75e5c76cf96ffc0351c4a7a6cb91d6e6ae1dc35e55c8c410e734"
-    sha256 catalina:       "6f51d8c1b693434a4faf554c20bcf05ba98cb147058fd1ba35a666839329dda6"
-    sha256 mojave:         "d8dc6d3095a4fc27d77baa19cf870922374b701a41cf503170f7f99012d44acf"
-    sha256 x86_64_linux:   "38164d05dbeec581b58630e44faec2766290d7e8c50959e8559a5fc317b5bb65"
+    sha256 arm64_monterey: "a5cefafd6afb35a8088d63c38c7eaba3ea1f6bc03b32826c2e6c046bc3213b18"
+    sha256 arm64_big_sur:  "9d363de93b8feb7187615978a6aca7630666a6b86fa3c2f5016bce5f48adf1e0"
+    sha256 monterey:       "56b9619483c6d8214c9ae0647571440b86e9cd8a0f0074ec199f1217c848ceb0"
+    sha256 big_sur:        "18eb1fb1b9e70de8db85ae790909ba5cdb61ebdea6d90b42c82df0372d46db19"
+    sha256 catalina:       "da3ec87a956fa3141d7cb7051a7b9926d84947271e84ce664d130a8849e3d8e2"
+    sha256 x86_64_linux:   "5e20a56f59e8da43b0f7e1b1f55ead1c82705ce6d8ef54be36b83b76482760c2"
   end
 
   head do
