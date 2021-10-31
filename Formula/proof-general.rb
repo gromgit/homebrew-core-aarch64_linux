a83c1ffd68045a9644ac7cdc56378ec1b257fabb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 05:05:59 +0000

    proof-general: update 4.4_2 bottle.

--- Formula/proof-general.rb
+++ Formula/proof-general.rb
@@ -9,12 +9,14 @@ class ProofGeneral < Formula
 
   bottle do
     rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "89bdd53f3c8afd028343bbb03ce2311cdeeab93924b96817b1b408117adc60fd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4fbcb27cb3b5bbdc7fba3cf07ca70953ab66cdd7749c85b06097d272cc31a630"
-    sha256 cellar: :any_skip_relocation, catalina:      "ccc115760830d046c9e53277a571f451eb251c9b10c09589c627f146f4a9a4dd"
-    sha256 cellar: :any_skip_relocation, mojave:        "ccc115760830d046c9e53277a571f451eb251c9b10c09589c627f146f4a9a4dd"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "ccc115760830d046c9e53277a571f451eb251c9b10c09589c627f146f4a9a4dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "277a264e49e065837b17eb67754b58e1447cf1e44c3b8b1ef65075d914b7e9f7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0953fbad4ad099431d4c4324794acd629180713c1f6c0359fed0b85a8d008672"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89bdd53f3c8afd028343bbb03ce2311cdeeab93924b96817b1b408117adc60fd"
+    sha256 cellar: :any_skip_relocation, monterey:       "7132e6a9b85cf89a4c356db0bbdcb5f3505c0536223120321e6fa55b63e4da46"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4fbcb27cb3b5bbdc7fba3cf07ca70953ab66cdd7749c85b06097d272cc31a630"
+    sha256 cellar: :any_skip_relocation, catalina:       "ccc115760830d046c9e53277a571f451eb251c9b10c09589c627f146f4a9a4dd"
+    sha256 cellar: :any_skip_relocation, mojave:         "ccc115760830d046c9e53277a571f451eb251c9b10c09589c627f146f4a9a4dd"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "ccc115760830d046c9e53277a571f451eb251c9b10c09589c627f146f4a9a4dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "277a264e49e065837b17eb67754b58e1447cf1e44c3b8b1ef65075d914b7e9f7"
   end
 
   depends_on "texi2html" => :build
