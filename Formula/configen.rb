09b47c7aa9cedb66680063ea700e5722d8ab5489
Bo Anderson <mail@boanderson.me>
2021-10-31 00:15:29 +0000

    configen: update 1.1.2 bottle.

--- Formula/configen.rb
+++ Formula/configen.rb
@@ -7,10 +7,12 @@ class Configen < Formula
   head "https://github.com/theappbusiness/ConfigGenerator.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d7aa87ea082759093c1192bf7e0ee69c1146ef4626534731ff506a83ec682641"
-    sha256 cellar: :any_skip_relocation, big_sur:       "78a7c0604f2a98b2f488b2bfefebff47e08342e69d5f47b7123f15f71bcb9653"
-    sha256 cellar: :any_skip_relocation, catalina:      "9bdb2988618d5a1e9291a8579207d9dad1092f377d29d13af68cf6ef5afcb202"
-    sha256 cellar: :any_skip_relocation, mojave:        "befb8801be997ff110c9ca0b817fed82b4e233842f5afe05e7ae372a10c4007f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a677709fb477a2e97d7791393044062418f5143928cf69d2d373e075c1209cc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7aa87ea082759093c1192bf7e0ee69c1146ef4626534731ff506a83ec682641"
+    sha256 cellar: :any_skip_relocation, monterey:       "3fde28a899b11fd837c3d30b27b26485a1e33f7bf951fe469d7fe8ea6ec41e0b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "78a7c0604f2a98b2f488b2bfefebff47e08342e69d5f47b7123f15f71bcb9653"
+    sha256 cellar: :any_skip_relocation, catalina:       "9bdb2988618d5a1e9291a8579207d9dad1092f377d29d13af68cf6ef5afcb202"
+    sha256 cellar: :any_skip_relocation, mojave:         "befb8801be997ff110c9ca0b817fed82b4e233842f5afe05e7ae372a10c4007f"
   end
 
   depends_on xcode: ["10.2", :build]
