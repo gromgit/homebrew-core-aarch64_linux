001b5e8dd3aceb77236b3f30a6ad3e37d8620ea5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 14:37:02 +0000

    cppcms: update 1.2.1 bottle.

--- Formula/cppcms.rb
+++ Formula/cppcms.rb
@@ -11,12 +11,14 @@ class Cppcms < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "a76448718798b05d84c1b9a54afa83c35afc924ed5bb5a8ad592d39cff90eaee"
-    sha256 cellar: :any, big_sur:       "ebe54531c492cd6771e3eab7cfee4d4a858c5b13a91e061c9d5bb2cb75f310dc"
-    sha256 cellar: :any, catalina:      "14a71b7ff0bbcbd0def75bd0a5e4552d5bfeccd24b7de17d38dcb676c37a71cf"
-    sha256 cellar: :any, mojave:        "aa587cdc614e7450100ee7c9aef5259893db98db66b9aa3fce8bc928fe080de7"
-    sha256 cellar: :any, high_sierra:   "3339592fd6caed70941abe444cf34c1621dd65878eea1acbd07e798d4bb5c9b4"
-    sha256 cellar: :any, sierra:        "9f21d55044af09d3eced9664c2d570657f0b3221c9f3051a5311f6f197bd2a28"
+    sha256 cellar: :any, arm64_monterey: "c6a5fe13878c0c37e6b326f957fc7d6dda3a0987c2fb09f5c8c54f169a76012b"
+    sha256 cellar: :any, arm64_big_sur:  "a76448718798b05d84c1b9a54afa83c35afc924ed5bb5a8ad592d39cff90eaee"
+    sha256 cellar: :any, monterey:       "34eccbf3637e2d5193070b75247f7d6e601b83166d8023aa87077dbb4e871e0c"
+    sha256 cellar: :any, big_sur:        "ebe54531c492cd6771e3eab7cfee4d4a858c5b13a91e061c9d5bb2cb75f310dc"
+    sha256 cellar: :any, catalina:       "14a71b7ff0bbcbd0def75bd0a5e4552d5bfeccd24b7de17d38dcb676c37a71cf"
+    sha256 cellar: :any, mojave:         "aa587cdc614e7450100ee7c9aef5259893db98db66b9aa3fce8bc928fe080de7"
+    sha256 cellar: :any, high_sierra:    "3339592fd6caed70941abe444cf34c1621dd65878eea1acbd07e798d4bb5c9b4"
+    sha256 cellar: :any, sierra:         "9f21d55044af09d3eced9664c2d570657f0b3221c9f3051a5311f6f197bd2a28"
   end
 
   depends_on "cmake" => :build
