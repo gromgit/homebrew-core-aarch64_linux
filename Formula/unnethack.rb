a7c89d4517733a86b8d3e1e6d4c2c73adf00707e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 05:08:01 +0000

    unnethack: update 5.3.2 bottle.

--- Formula/unnethack.rb
+++ Formula/unnethack.rb
@@ -11,11 +11,13 @@ class Unnethack < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5b4386eee78f20075e693b6ad437df496c8c914518161d8901991c1c4a6ee1f9"
-    sha256 big_sur:       "45d58053580ccdf9b65510768136206b71453b3457f23240a6dc592f817a6145"
-    sha256 catalina:      "5a1aea5f715d4c8892be4a5e76d60157da6637559a0055c41ea8024284807e91"
-    sha256 mojave:        "84267cd44f073a41058516e7a8937da6b8b0f16e3500b0fd10ab0fedad77a5ce"
-    sha256 high_sierra:   "47228cb416afe4d7e9ab31a2b85914e6b27f77e88340f7ef174bb2d9dd3ea2bb"
+    sha256 arm64_monterey: "05c4befbdb39343bd07d991ea4d3b048215098aea8af4239e0c6ecef27deb330"
+    sha256 arm64_big_sur:  "5b4386eee78f20075e693b6ad437df496c8c914518161d8901991c1c4a6ee1f9"
+    sha256 monterey:       "c93c7e1e75f40ea747049d51072aefee9604e92c2643921aaa251ca35a08b2fc"
+    sha256 big_sur:        "45d58053580ccdf9b65510768136206b71453b3457f23240a6dc592f817a6145"
+    sha256 catalina:       "5a1aea5f715d4c8892be4a5e76d60157da6637559a0055c41ea8024284807e91"
+    sha256 mojave:         "84267cd44f073a41058516e7a8937da6b8b0f16e3500b0fd10ab0fedad77a5ce"
+    sha256 high_sierra:    "47228cb416afe4d7e9ab31a2b85914e6b27f77e88340f7ef174bb2d9dd3ea2bb"
   end
 
   # directory for temporary level data of running games
