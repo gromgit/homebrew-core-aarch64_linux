ad3a993e860b88b9cec25509bea9616ceafd3eae
Thierry Moisan <thierry.moisan@gmail.com>
2021-10-31 19:10:59 +0000

    tunnel: update 0.5.15 bottle.

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -7,10 +7,12 @@ class Tunnel < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "57607af5d1084a837f6dac59d268a9c5ed879acddac12840a1a4e8f375a070c6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cb4ebc8c76b4866aeaa4da672e81e7027ffad1f4d78a7867aa1b17511827b3bd"
-    sha256 cellar: :any_skip_relocation, catalina:      "8119646cdda9c9578230f230b8b9156159d9adecbf30b1c8d6fed8ddbdce54bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9a079e012dffd514c536355269716695a150db0dc30cd3ea2c27aee789615805"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc5f7baf6232a8be13a17350b3ac13d264c530e40c93cdc5aacbe9af856fb060"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57607af5d1084a837f6dac59d268a9c5ed879acddac12840a1a4e8f375a070c6"
+    sha256 cellar: :any_skip_relocation, monterey:       "b63c0576e0a46d3177fcb0574dc63bc252885ab52840f3454eb3660f31fcb744"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb4ebc8c76b4866aeaa4da672e81e7027ffad1f4d78a7867aa1b17511827b3bd"
+    sha256 cellar: :any_skip_relocation, catalina:       "8119646cdda9c9578230f230b8b9156159d9adecbf30b1c8d6fed8ddbdce54bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a079e012dffd514c536355269716695a150db0dc30cd3ea2c27aee789615805"
   end
 
   depends_on "go" => :build
