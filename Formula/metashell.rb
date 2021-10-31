04e706c7f666e1d63703def4954e29447ad7b45c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 16:43:37 +0000

    metashell: update 4.0.0 bottle.

--- Formula/metashell.rb
+++ Formula/metashell.rb
@@ -8,6 +8,7 @@ class Metashell < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "72ac6ed1ef416a844bd9794ef1810bebb3bd24397e7ed4f1aac754e8842b0600"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c7cbb01fe420183e802a5a974d5eea39f38333469674f8128d74db49376a726"
+    sha256 cellar: :any_skip_relocation, monterey:       "473cc7a5c1ea7ef3bdd0e196022fc3f8941424f0cd6e80af44ecfe64c92d4952"
     sha256 cellar: :any_skip_relocation, big_sur:        "a1fc773f5452ccb165e28e9ec0a79616c14ababc66ed3614a213bc86bbfcda84"
     sha256 cellar: :any_skip_relocation, catalina:       "792f1b46b5f17933b21ec7adb62cf0b6add03ef94e8a73e5e691e12e9aa85049"
     sha256 cellar: :any_skip_relocation, mojave:         "4629398ca4b1bf5cf7779b8d5c9e6f066ea5e96f66063c265f0b13e106a0cba0"
