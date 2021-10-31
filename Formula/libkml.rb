9dbe552d4b2e4391cc8e7ba156bffefbd26ce4b8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 07:32:11 +0000

    libkml: update 1.2.0 bottle.

--- Formula/libkml.rb
+++ Formula/libkml.rb
@@ -6,14 +6,16 @@ class Libkml < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "589b147706bc4aec04f96039cb2e61e80d85bcbedf0e919ebcca29fe09d81e26"
-    sha256 cellar: :any, big_sur:       "a0694686c535fa33f6222e11dde9858881e6a5eaa12c6b11c5ef310a32635087"
-    sha256 cellar: :any, catalina:      "2b73d6ea2eacd6e11229a0a9747444c28a455bb24943108b0351f689d17eb3d9"
-    sha256 cellar: :any, mojave:        "c79c73e048728d0497b7f91c0e174bd97e27f65ff471e00324483a3557b6a13f"
-    sha256 cellar: :any, high_sierra:   "31d567e2e0d87794adea3507cb34ace0483309de7ba5b32fc98bc1ca59a461c5"
-    sha256 cellar: :any, sierra:        "860294d677de2f8a4c18e4d750d59aeafa2b38801b12eb76b5e951a23a8ec108"
-    sha256 cellar: :any, el_capitan:    "57b9693cdf9a6abaeeea9648cd84a81d17ba0f056bd8d8e8442e68d97dbc7828"
-    sha256 cellar: :any, yosemite:      "a3cdfca3ed0acbc93949683a8bb2862c36ec8bf06f20b9fe3752ac624667f455"
+    sha256 cellar: :any, arm64_monterey: "93812e697f75c3ddebdb3f15b8b7e98773c49c69e929a244439c66076f81ff76"
+    sha256 cellar: :any, arm64_big_sur:  "589b147706bc4aec04f96039cb2e61e80d85bcbedf0e919ebcca29fe09d81e26"
+    sha256 cellar: :any, monterey:       "a217fb8977e5923462c00746cd607f510e76a9a20be9c53db554f5c72ffdfeef"
+    sha256 cellar: :any, big_sur:        "a0694686c535fa33f6222e11dde9858881e6a5eaa12c6b11c5ef310a32635087"
+    sha256 cellar: :any, catalina:       "2b73d6ea2eacd6e11229a0a9747444c28a455bb24943108b0351f689d17eb3d9"
+    sha256 cellar: :any, mojave:         "c79c73e048728d0497b7f91c0e174bd97e27f65ff471e00324483a3557b6a13f"
+    sha256 cellar: :any, high_sierra:    "31d567e2e0d87794adea3507cb34ace0483309de7ba5b32fc98bc1ca59a461c5"
+    sha256 cellar: :any, sierra:         "860294d677de2f8a4c18e4d750d59aeafa2b38801b12eb76b5e951a23a8ec108"
+    sha256 cellar: :any, el_capitan:     "57b9693cdf9a6abaeeea9648cd84a81d17ba0f056bd8d8e8442e68d97dbc7828"
+    sha256 cellar: :any, yosemite:       "a3cdfca3ed0acbc93949683a8bb2862c36ec8bf06f20b9fe3752ac624667f455"
   end
 
   conflicts_with "uriparser", because: "both install `liburiparser.dylib`"
