9b75707ab5856c109937a6fef43dd7b8f0328772
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 09:39:15 +0000

    mrboom: update 5.2 bottle.

--- Formula/mrboom.rb
+++ Formula/mrboom.rb
@@ -7,11 +7,13 @@ class Mrboom < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "90484ee7a62a29aa82242664b917340def45c3b999a7d21197ac10e020617194"
-    sha256 cellar: :any,                 big_sur:       "904cd506e99c6269809fe4c593263de7cc1f0746fe0c5b5180aa63ef522ca212"
-    sha256 cellar: :any,                 catalina:      "7fc60e5a37d093f2311b797c5822dbeb098cdf47c038c808496973d29f563f2c"
-    sha256 cellar: :any,                 mojave:        "262fab23ed3b5a3b80948ae4fb4eca1c0c0cad04220a031a731905d812aebaae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bb5d8f528c43dfa0eb5970a5ba8e0b98d4db662f903ec2d62751d98ef013f780"
+    sha256 cellar: :any,                 arm64_monterey: "cf2c1633672145fe98dcc7ed89e75b1282081941d8def655b84967c5d2a80ea8"
+    sha256 cellar: :any,                 arm64_big_sur:  "90484ee7a62a29aa82242664b917340def45c3b999a7d21197ac10e020617194"
+    sha256 cellar: :any,                 monterey:       "fd4bf9a47d15da1e296433860388dd78cbc6d46d036a0145eb36c651693fce25"
+    sha256 cellar: :any,                 big_sur:        "904cd506e99c6269809fe4c593263de7cc1f0746fe0c5b5180aa63ef522ca212"
+    sha256 cellar: :any,                 catalina:       "7fc60e5a37d093f2311b797c5822dbeb098cdf47c038c808496973d29f563f2c"
+    sha256 cellar: :any,                 mojave:         "262fab23ed3b5a3b80948ae4fb4eca1c0c0cad04220a031a731905d812aebaae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb5d8f528c43dfa0eb5970a5ba8e0b98d4db662f903ec2d62751d98ef013f780"
   end
 
   depends_on "cmake" => :build
