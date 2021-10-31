d5cd698a4c8cb00b1a770c8f1a5275664873d888
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 10:47:13 +0000

    dbhash: update 3.36.0 bottle.

--- Formula/dbhash.rb
+++ Formula/dbhash.rb
@@ -11,11 +11,13 @@ class Dbhash < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "93a11d1334f5d87a45445373afe1de1fd99ed87f8dd12a9b02dc92bdcf01735e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "23eccd9d6af0e7e0e23e708bd4697cb3334299a5d7127d763fe53a90aab6d064"
-    sha256 cellar: :any_skip_relocation, catalina:      "f7e686231f1748f5655b27cf334fe972a7be8d029a284f291ea2daa8f94c51af"
-    sha256 cellar: :any_skip_relocation, mojave:        "56404056dd0a9b8ece371576e64a1b3ad8c9b19f129dd500c38edfbe58c5cccf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "036505f744fca371174119931c174fc1bfd73d4772a46983fe40e8cfb7b4d90e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c8fdda6f47ff0d2f6f4f367f1ae8fdfafd8f115090c6d07a610790aae885adf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93a11d1334f5d87a45445373afe1de1fd99ed87f8dd12a9b02dc92bdcf01735e"
+    sha256 cellar: :any_skip_relocation, monterey:       "6b4121bfc0281460baa8e0703578140a73cc304814b8786b7a548c282c7c3c4e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "23eccd9d6af0e7e0e23e708bd4697cb3334299a5d7127d763fe53a90aab6d064"
+    sha256 cellar: :any_skip_relocation, catalina:       "f7e686231f1748f5655b27cf334fe972a7be8d029a284f291ea2daa8f94c51af"
+    sha256 cellar: :any_skip_relocation, mojave:         "56404056dd0a9b8ece371576e64a1b3ad8c9b19f129dd500c38edfbe58c5cccf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "036505f744fca371174119931c174fc1bfd73d4772a46983fe40e8cfb7b4d90e"
   end
 
   uses_from_macos "tcl-tk" => :build
