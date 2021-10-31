8ff91110491877ba8fd1a617737fadaecafadf08
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 00:57:32 +0800

    xmake: update 2.5.9 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -7,10 +7,11 @@ class Xmake < Formula
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e8bc227e357e5438787a1d3c7ed15a65d7ad020fe48036dcbaba0d8851e25c86"
-    sha256 cellar: :any_skip_relocation, big_sur:       "dbe0397acf297160003a8f8f73906e44afc746033d4bf7591cd09898caa77dbd"
-    sha256 cellar: :any_skip_relocation, catalina:      "12153f7e13ef7d654bba10bb908f368c9092619e39a248f1398c033671a261d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e72219ec96d022d8cb10aa13fcdf60dcb1de47d4fa674bb086289827adbfd03"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36cd5067ca7998d06b6266b6233b9ebd9395896b81fbdb1bb585fec6b39d8482"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8bc227e357e5438787a1d3c7ed15a65d7ad020fe48036dcbaba0d8851e25c86"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dbe0397acf297160003a8f8f73906e44afc746033d4bf7591cd09898caa77dbd"
+    sha256 cellar: :any_skip_relocation, catalina:       "12153f7e13ef7d654bba10bb908f368c9092619e39a248f1398c033671a261d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3e72219ec96d022d8cb10aa13fcdf60dcb1de47d4fa674bb086289827adbfd03"
   end
 
   on_linux do
