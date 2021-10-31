e33e830367b6d21b5b62c2910553974da438c9c0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 07:31:54 +0000

    wiredtiger: update 10.0.0 bottle.

--- Formula/wiredtiger.rb
+++ Formula/wiredtiger.rb
@@ -11,11 +11,13 @@ class Wiredtiger < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "2e2b170afa925805d7f94e127dc6c66f7ae5d042a37860e736e9c6cbf1696acb"
-    sha256 cellar: :any,                 big_sur:       "73dec56cf3779376bb1e111c6d96900bfd73e5df072dfce752defaf06d98b167"
-    sha256 cellar: :any,                 catalina:      "16d0323167834b745163edf87b88693a7b49ace3f901042c0d78fbfbe5afa8a8"
-    sha256 cellar: :any,                 mojave:        "0c2bbb142e29427648f66455b69028d1650b3d420700d31e952a70e58cc361f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e89496c6a6af975b83b16006c84ef7c69d7df3b0e127e2f58a3b2ff6cfc860f"
+    sha256 cellar: :any,                 arm64_monterey: "4b72a7ac0c4f99e3fa6cdd8395f03bf0c819ccbcd90e9e4ce373de6e016180f4"
+    sha256 cellar: :any,                 arm64_big_sur:  "2e2b170afa925805d7f94e127dc6c66f7ae5d042a37860e736e9c6cbf1696acb"
+    sha256 cellar: :any,                 monterey:       "2d693bed27b7602f8645f861bd063f98ef9e9653294e238550a57c4fbb762924"
+    sha256 cellar: :any,                 big_sur:        "73dec56cf3779376bb1e111c6d96900bfd73e5df072dfce752defaf06d98b167"
+    sha256 cellar: :any,                 catalina:       "16d0323167834b745163edf87b88693a7b49ace3f901042c0d78fbfbe5afa8a8"
+    sha256 cellar: :any,                 mojave:         "0c2bbb142e29427648f66455b69028d1650b3d420700d31e952a70e58cc361f8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2e89496c6a6af975b83b16006c84ef7c69d7df3b0e127e2f58a3b2ff6cfc860f"
   end
 
   depends_on "snappy"
