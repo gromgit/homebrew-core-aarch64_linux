ceaf35f4c7ae417c9a51dc1a585587675c9b2489
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-31 05:23:15 +0000

    openfpgaloader: update 0.5.0 bottle.

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -7,11 +7,13 @@ class Openfpgaloader < Formula
   head "https://github.com/trabucayre/openFPGALoader.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "77483712fb4bc07885f71696245d13d3bbb34255db2e94188588345b52f81866"
-    sha256 cellar: :any_skip_relocation, big_sur:       "33f9a2c0324595ab30136b1f83e5b97db844c362f7b5543fa009796145f23f42"
-    sha256 cellar: :any_skip_relocation, catalina:      "01322e5f63e499776d44a32f7752b71c7238f633e51ef46d46a5b8aaebc73929"
-    sha256 cellar: :any_skip_relocation, mojave:        "759abeb1d7b64b34f216518ffebc9584bccd6eef7003c29eb11740cd49c38549"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d30a0f2f57e76b8e667eecdf85281fc619dcddb9d8e6f3b2ba7491bb01ca8fb8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "648e0b46b3111791b7b30de236fdc8edcb0118e122d87cef7017b64751db5e64"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "77483712fb4bc07885f71696245d13d3bbb34255db2e94188588345b52f81866"
+    sha256 cellar: :any_skip_relocation, monterey:       "6a71a65727b492748b5cb8ded309a519cc0f332eeb43a609b494812266c8ece6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "33f9a2c0324595ab30136b1f83e5b97db844c362f7b5543fa009796145f23f42"
+    sha256 cellar: :any_skip_relocation, catalina:       "01322e5f63e499776d44a32f7752b71c7238f633e51ef46d46a5b8aaebc73929"
+    sha256 cellar: :any_skip_relocation, mojave:         "759abeb1d7b64b34f216518ffebc9584bccd6eef7003c29eb11740cd49c38549"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d30a0f2f57e76b8e667eecdf85281fc619dcddb9d8e6f3b2ba7491bb01ca8fb8"
   end
 
   depends_on "cmake" => :build
