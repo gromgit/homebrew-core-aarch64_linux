a8f51939dbcc370c8251e09001b0d7a37236a22e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 15:28:05 +0000

    dnsperf: update 2.7.1_2 bottle.

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -12,11 +12,12 @@ class Dnsperf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "9058dc5367702b9bb5ceb1fc417a34f60344eea124384beca9c75b333351265b"
-    sha256 cellar: :any,                 big_sur:       "33f81adab77f7d988a927144bdf77f5090d3489e0d3ef812f3b273c74cd07e25"
-    sha256 cellar: :any,                 catalina:      "88b0ae4ee57e39b8e87f97d5b1aac1c12b5f44e744aad1316b45619781e25b34"
-    sha256 cellar: :any,                 mojave:        "4d9debb3fb6b0a43620a205abf27d083d6b731e355afcb8c55f23812347f7aa2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cae9a4ea9e132cc17ab2c378c5e12b7c1159edf536dc839438afdbffb43bb858"
+    sha256 cellar: :any,                 arm64_monterey: "ed9838c9f524357a9c35a91df072098140af8f20b5b55b818cc91350e1537d63"
+    sha256 cellar: :any,                 arm64_big_sur:  "9058dc5367702b9bb5ceb1fc417a34f60344eea124384beca9c75b333351265b"
+    sha256 cellar: :any,                 big_sur:        "33f81adab77f7d988a927144bdf77f5090d3489e0d3ef812f3b273c74cd07e25"
+    sha256 cellar: :any,                 catalina:       "88b0ae4ee57e39b8e87f97d5b1aac1c12b5f44e744aad1316b45619781e25b34"
+    sha256 cellar: :any,                 mojave:         "4d9debb3fb6b0a43620a205abf27d083d6b731e355afcb8c55f23812347f7aa2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cae9a4ea9e132cc17ab2c378c5e12b7c1159edf536dc839438afdbffb43bb858"
   end
 
   depends_on "pkg-config" => :build
