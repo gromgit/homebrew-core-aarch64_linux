fc86412a353fefcbaa4fa09bdf0ca229e3644057
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 06:34:28 +0000

    nift: update 2.4.12 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -6,6 +6,7 @@ class Nift < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any,                 monterey:     "0594705207068ce496955651d2aca3e9430d76dfddb6ae038568bd1cb7672848"
     sha256 cellar: :any,                 big_sur:      "643dbd50106a96d8af8e1071c49fb6c41522f7d3384d0e438810d48e5503c7ab"
     sha256 cellar: :any,                 catalina:     "dbf48067fac536bfe804c35c19c6198bae0c0d29107be3e4512d31a37485fd96"
     sha256 cellar: :any,                 mojave:       "d51812440b4e4b8df56ee07b377e2d2a4cee7d84233377218e9ed3fc5e9e68e1"
