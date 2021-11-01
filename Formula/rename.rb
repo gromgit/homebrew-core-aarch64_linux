719ec26f1d388076e14884c69c0ef8419c04b8e4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 14:55:29 +0000

    rename: update 1.601 bottle.

--- Formula/rename.rb
+++ Formula/rename.rb
@@ -7,11 +7,12 @@ class Rename < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "308b9f76cf8386eb9c5835204233f0869cc566d9995b383a5215649e8b1c7a48"
-    sha256 cellar: :any_skip_relocation, big_sur:       "308b9f76cf8386eb9c5835204233f0869cc566d9995b383a5215649e8b1c7a48"
-    sha256 cellar: :any_skip_relocation, catalina:      "2f1c70cacb289e2286bc6ec1e47319d197c2f0d74f8474b303aa2cb9aad8bb0e"
-    sha256 cellar: :any_skip_relocation, mojave:        "2f1c70cacb289e2286bc6ec1e47319d197c2f0d74f8474b303aa2cb9aad8bb0e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a88a0c2cd57632633f5ab3e9078f22903b3c12f0520276c3e408570ff10dee14"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1d7f81a8f319841108fb8082ea6cd5cf591224964e6f34bb0135cf851b7f951f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "308b9f76cf8386eb9c5835204233f0869cc566d9995b383a5215649e8b1c7a48"
+    sha256 cellar: :any_skip_relocation, big_sur:        "308b9f76cf8386eb9c5835204233f0869cc566d9995b383a5215649e8b1c7a48"
+    sha256 cellar: :any_skip_relocation, catalina:       "2f1c70cacb289e2286bc6ec1e47319d197c2f0d74f8474b303aa2cb9aad8bb0e"
+    sha256 cellar: :any_skip_relocation, mojave:         "2f1c70cacb289e2286bc6ec1e47319d197c2f0d74f8474b303aa2cb9aad8bb0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a88a0c2cd57632633f5ab3e9078f22903b3c12f0520276c3e408570ff10dee14"
   end
 
   depends_on "pod2man" => :build
