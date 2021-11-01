9aa777a1f4a6c5fe123dc06053d90548e9ed3817
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 17:31:42 +0000

    libmonome: update 1.4.4 bottle.

--- Formula/libmonome.rb
+++ Formula/libmonome.rb
@@ -7,10 +7,11 @@ class Libmonome < Formula
   head "https://github.com/monome/libmonome.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "745e10cf5a29c44c9837fa7b822407eca0993ea00c414d99b7ad2a98fe7f059c"
-    sha256 cellar: :any, big_sur:       "ba70b11a69fab981f80172fcdf8cd78cb88b64386f0d86cfcdf0089d54916dbe"
-    sha256 cellar: :any, catalina:      "7aacc1a7d070958460b380f182ccdb7cc073ccef592eb018a69d61d30c07e0f6"
-    sha256 cellar: :any, mojave:        "a75784a9297378f3c477ee251f9e729f3e98230113908af03078a92cd4c7d076"
+    sha256 cellar: :any, arm64_monterey: "415d02f115ed624be7e04e5f71dc56b57b313b8c0b35090fc8f73da367f8f5f9"
+    sha256 cellar: :any, arm64_big_sur:  "745e10cf5a29c44c9837fa7b822407eca0993ea00c414d99b7ad2a98fe7f059c"
+    sha256 cellar: :any, big_sur:        "ba70b11a69fab981f80172fcdf8cd78cb88b64386f0d86cfcdf0089d54916dbe"
+    sha256 cellar: :any, catalina:       "7aacc1a7d070958460b380f182ccdb7cc073ccef592eb018a69d61d30c07e0f6"
+    sha256 cellar: :any, mojave:         "a75784a9297378f3c477ee251f9e729f3e98230113908af03078a92cd4c7d076"
   end
 
   depends_on "liblo"
