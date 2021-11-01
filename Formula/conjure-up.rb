12400a3c058cf8be7f23628930640d99ab3d5f92
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 20:36:15 +0000

    conjure-up: update 2.6.14_4 bottle.

--- Formula/conjure-up.rb
+++ Formula/conjure-up.rb
@@ -11,6 +11,7 @@ class ConjureUp < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "e4da8a68d5c475d4432d9706c22b5d8ab8b69cf20e7ced2c26d7686c273d2682"
     sha256 cellar: :any,                 arm64_big_sur:  "0d392e7fe4c46dcb50da2344dec0a11dc2141bca36c7342b311e813bbd021b03"
+    sha256 cellar: :any,                 monterey:       "178493694c306b5aed3aa4f816fd94f82a90d0f28795c3f28497a5e9b4f97501"
     sha256 cellar: :any,                 big_sur:        "4aa49ccf202ed4bf0ecbb987b04fa30ee28bdc3e4c4bf37c396668269b23537e"
     sha256 cellar: :any,                 catalina:       "b9c8766c411e1d736d52714678420065071866d795360a5a8e21045d559714f7"
     sha256 cellar: :any,                 mojave:         "1e8d73fbff45339f18bb6fbbc1b00bd7bba4590feeb6a2980b8c68ddefb56c98"
