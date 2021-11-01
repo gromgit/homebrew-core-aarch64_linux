7a3700a59d2bc37bf2e81f0c7fd2054999e72041
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 19:44:22 +0000

    minipro: update 0.5 bottle.

--- Formula/minipro.rb
+++ Formula/minipro.rb
@@ -9,6 +9,7 @@ class Minipro < Formula
     rebuild 1
     sha256 arm64_monterey: "569f71476e42356da2d010cb2d4bdd3316da94b56ae5888b33e7c6b72dae105c"
     sha256 arm64_big_sur:  "3ba41fe3390795a6afd327ee42c709ca59cb6f7b4cfa2bf9bc150ddc9608b72f"
+    sha256 monterey:       "b61dcb6a0b6b5c8308ddd925febade67c2533561e80daf62eb0313aaec7daace"
     sha256 big_sur:        "0590a42c14ad858823a7839a745a812b921d0e458b64568a81ca17c6f0a5bb45"
     sha256 catalina:       "c949912791a3b9c40f049a8c10f289062bbd9ede15da2f37a597c6c40c4d5f43"
     sha256 mojave:         "d54e9413a92b7bd5118b1938215d61152ea06055f0860ddcb167d74924a828f9"
