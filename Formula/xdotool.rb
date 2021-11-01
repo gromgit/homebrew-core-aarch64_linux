b57fa0369eeedfc9bd51f05426b9423ec16f5715
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 15:07:00 +0000

    xdotool: update 3.20211022.1 bottle.

--- Formula/xdotool.rb
+++ Formula/xdotool.rb
@@ -7,10 +7,11 @@ class Xdotool < Formula
   head "https://github.com/jordansissel/xdotool.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "cdf3234a474044e88dcf18b5cb5e8da2c2af6da4d85eb04e8be737802baeae16"
-    sha256 cellar: :any,                 big_sur:       "f33aa5be05e49f700d166a13a36ecf5a1f8da3059e36f67e0cc9d7f26c3bf088"
-    sha256 cellar: :any,                 catalina:      "21276c0386840d584e70f5425578b5184e56ef7649e6992f191a6c7a3cf8a30e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ffe41af3fe21135efdee19b3fabf9f459d850946dd592858a50b4cd46035f35e"
+    sha256 cellar: :any,                 arm64_monterey: "2e59d046b4cb7d97f989a022c0775cbf3ab9f5929cd05650d50a9eed62b162c2"
+    sha256 cellar: :any,                 arm64_big_sur:  "cdf3234a474044e88dcf18b5cb5e8da2c2af6da4d85eb04e8be737802baeae16"
+    sha256 cellar: :any,                 big_sur:        "f33aa5be05e49f700d166a13a36ecf5a1f8da3059e36f67e0cc9d7f26c3bf088"
+    sha256 cellar: :any,                 catalina:       "21276c0386840d584e70f5425578b5184e56ef7649e6992f191a6c7a3cf8a30e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ffe41af3fe21135efdee19b3fabf9f459d850946dd592858a50b4cd46035f35e"
   end
 
   depends_on "pkg-config" => :build
