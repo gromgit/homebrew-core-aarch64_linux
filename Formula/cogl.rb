c309705143290fdcb501b10f1ffd6768c9e0222b
Thierry Moisan <thierry.moisan@gmail.com>
2021-10-31 23:45:33 -0400

    cogl: update homepage and head branch

--- Formula/cogl.rb
+++ Formula/cogl.rb
@@ -1,9 +1,10 @@
 class Cogl < Formula
   desc "Low level OpenGL abstraction library developed for Clutter"
-  homepage "https://developer.gnome.org/cogl/"
+  homepage "https://gitlab.gnome.org/GNOME/cogl"
   url "https://download.gnome.org/sources/cogl/1.22/cogl-1.22.8.tar.xz"
   sha256 "a805b2b019184710ff53d0496f9f0ce6dcca420c141a0f4f6fcc02131581d759"
   license all_of: ["MIT", "SGI-B-2.0", "BSD-3-Clause", :public_domain]
+  head "https://gitlab.gnome.org/GNOME/cogl.git", branch: "cogl-1.22"
 
   bottle do
     sha256 arm64_big_sur: "9a487a4bf7fbe5fdec29d902ba668fe20cbbc05e66864cb8d9c5fe564373e586"
@@ -12,10 +13,6 @@ class Cogl < Formula
     sha256 mojave:        "eb37baaa178631afac43c8bb1c93cdf9b78dd7d44862c63dec598d54a51b201e"
     sha256 high_sierra:   "46de52386a1123e828d94598279a99a88e3819d8f1dac1a51f39850a321ff7f2"
     sha256 x86_64_linux:  "2e1af63ab7d9ffcfca78804cdcab02d3b60969c52c038d89a7a7caa42afd4c00"
-  end
-
-  head do
-    url "https://gitlab.gnome.org/GNOME/cogl.git"
   end
 
   depends_on "gobject-introspection" => :build
