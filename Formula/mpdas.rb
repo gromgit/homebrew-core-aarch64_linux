e83caa7b1b78ba7588d2c00e829c0aaf502943cd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 21:17:28 +0000

    mpdas: update 0.4.5 bottle.

--- Formula/mpdas.rb
+++ Formula/mpdas.rb
@@ -10,6 +10,7 @@ class Mpdas < Formula
     rebuild 1
     sha256 arm64_monterey: "89b5e45abcb88c8df236dea703162ed9d7f372c0872c3af23630c56e18e31c8e"
     sha256 arm64_big_sur:  "a7fd32d05844b78fb8eede1ff4a1285e531250919e296166f0eb74d6a9f0cd39"
+    sha256 monterey:       "25dd98ffb770fa018112f3014ae2574ab346b2d54d542b4feab516f264f30ced"
     sha256 big_sur:        "1f41b8a297270de980e814104f1a74d9437777caa9443686fb251cac6625494b"
     sha256 catalina:       "ae3eaddea864370fbcb2ad3f815165ba4f79f57470cbf0b3d81c781f0aaccc37"
     sha256 mojave:         "4b6a70ab4c6599598c88dc0da91b3646a5d36bc7db9c174ed1cad387861f4370"
