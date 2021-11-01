355ce66432c3c655f09b34564f6b723631f178cd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 18:50:05 +0000

    mpc: update 0.33 bottle.

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -13,6 +13,7 @@ class Mpc < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "c86764449bddde59112a1627ff4c6676100afc87a2d39346c2f4a13677bc5bfb"
     sha256 cellar: :any, arm64_big_sur:  "8062f7af33fe3603b4c625509f4c80570d790b15922287025ef7333748dae279"
+    sha256 cellar: :any, monterey:       "f00ccbb53d1ed598c3de7b2723355debfd5d9bc29e105469150a7ec7f7a3ccfc"
     sha256 cellar: :any, big_sur:        "cc01508026db65016ee6e2ec1f6663921a5c5ace5c76ea51ffbd1b0b7ade5e63"
     sha256 cellar: :any, catalina:       "341a4c3cef23004a47f37fa299047e63baedceb07405813d6fc112c9ad7d4ff2"
     sha256 cellar: :any, mojave:         "29742180fafe0fffeba3fc09c3d355395084ef3d063004347a96bc37c72682db"
