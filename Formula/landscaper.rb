7b5feb877a9e5a697b431bcf5f46e2d05db9177d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 22:03:48 +0000

    landscaper: update 1.0.24_1 bottle.

--- Formula/landscaper.rb
+++ Formula/landscaper.rb
@@ -11,6 +11,7 @@ class Landscaper < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "835b3c32293b1725d2065652ecd2a0017ba2387351f586936230ff96030f79cb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d6d705dc7d36d5fd8f0f6abd093bd86398c799929069fa47f117deb25f5bbe0f"
+    sha256 cellar: :any_skip_relocation, monterey:       "13cf71ffacb84e95c4346beec92e389893755f998f6f421b930b9adb5ccfdc5d"
     sha256 cellar: :any_skip_relocation, big_sur:        "bad7cf082826c5d92dd8c09a79b682e1582fcfc3f4e471dde4112393ec7095ce"
     sha256 cellar: :any_skip_relocation, catalina:       "74decffaf180e0e0dd9bfa2312877da01443a3418afe0f485c1b655c4af1da41"
     sha256 cellar: :any_skip_relocation, mojave:         "ff82cdb7be6329f9a4a5ce34bcbb04bc9356ab46fa3ecd30b830cf35df268529"
