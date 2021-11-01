24d93b09a7634ec4f2e96a518c0951d05dff2996
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 16:05:59 +0000

    dwm: update 6.2_2 bottle.

--- Formula/dwm.rb
+++ Formula/dwm.rb
@@ -13,11 +13,12 @@ class Dwm < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7fd3a01a1f29927ca94c2c5ea32b4ee0c9f31d9ea39adc04e76ab40517663149"
-    sha256 cellar: :any, big_sur:       "afd787afd9c6ea4cc81c100f324d2b8aa4c65c2a06e43ca87d54135425b347cf"
-    sha256 cellar: :any, catalina:      "d872be09d1f5c11c9fb4d34002cc5f4622fbc259691800e1742354573b9effb0"
-    sha256 cellar: :any, mojave:        "e4ec85368754c0594847dad5272770a36e69876ed433fdd390d73a7d05c43263"
-    sha256 cellar: :any, high_sierra:   "b22ec01678edc39f1b82837087bb69ac311bce937eb10cb096fc8b1002f97701"
+    sha256 cellar: :any, arm64_monterey: "3971098bdbc2dee1f4acac3020a38052d6be49f777543c422e93e2c2f361e3bf"
+    sha256 cellar: :any, arm64_big_sur:  "7fd3a01a1f29927ca94c2c5ea32b4ee0c9f31d9ea39adc04e76ab40517663149"
+    sha256 cellar: :any, big_sur:        "afd787afd9c6ea4cc81c100f324d2b8aa4c65c2a06e43ca87d54135425b347cf"
+    sha256 cellar: :any, catalina:       "d872be09d1f5c11c9fb4d34002cc5f4622fbc259691800e1742354573b9effb0"
+    sha256 cellar: :any, mojave:         "e4ec85368754c0594847dad5272770a36e69876ed433fdd390d73a7d05c43263"
+    sha256 cellar: :any, high_sierra:    "b22ec01678edc39f1b82837087bb69ac311bce937eb10cb096fc8b1002f97701"
   end
 
   depends_on "dmenu"
