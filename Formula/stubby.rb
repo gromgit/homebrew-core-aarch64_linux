7ddfc1b1dc6893e12e7c5cf1bab275f1872341bd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 17:41:37 +0000

    stubby: update 0.4.0 bottle.

--- Formula/stubby.rb
+++ Formula/stubby.rb
@@ -10,6 +10,7 @@ class Stubby < Formula
     rebuild 1
     sha256 arm64_monterey: "98de227b643cc50357f442dda39ba673c9bdba8f6b5977c6ee0f519ce0560036"
     sha256 arm64_big_sur:  "aacda92701ecc4c275bfe6eb5ede29a6f07f6d0d85701f293146880437f448f8"
+    sha256 monterey:       "e3f52352894e8ed61a7166b2e3fd6fbf184264326f07615f32b450df8f162a94"
     sha256 big_sur:        "435174729967fbf5bb4dc87a8e2ef440f6cec7e56c46a5373dfe6f5a6a6ec96c"
     sha256 catalina:       "df3b7e64116093724ab01d7a6f3abee725e9ffebfd030a1255d9f6c8467101f2"
     sha256 mojave:         "21530780a842976f9dbd45777c85900b841e15a063ab522d5c8d30f4bba74eec"
