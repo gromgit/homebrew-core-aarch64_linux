c91dd98b3358fae9117d2fd1064983eee8355a13
Rui Chen <rui@chenrui.dev>
2021-11-01 12:24:25 -0400

    duckscript 0.8.9
    Closes #88529.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/duckscript.rb
+++ Formula/duckscript.rb
@@ -1,8 +1,8 @@
 class Duckscript < Formula
   desc "Simple, extendable and embeddable scripting language"
   homepage "https://sagiegurari.github.io/duckscript"
-  url "https://github.com/sagiegurari/duckscript/archive/0.8.7.tar.gz"
-  sha256 "b2143f0f4660eb61b43da440fbf1c43e2bdefefc657435187e031b2fe671fa22"
+  url "https://github.com/sagiegurari/duckscript/archive/0.8.9.tar.gz"
+  sha256 "b26ef19d50367352af3d0ba79946838202e7418a9e53e82fbb96f05c87dd389a"
   license "Apache-2.0"
   head "https://github.com/sagiegurari/duckscript.git"
 
