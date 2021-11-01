f0938d1fe14ad9ace4d6a1f0f82d85ca7cd5ae40
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 06:04:48 +0000

    dmagnetic: update 0.32 bottle.

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -11,11 +11,13 @@ class Dmagnetic < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e8780abff0b126cca9e22f89a1e84c6cb9187c5638743764329b84df8063098a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bd532ceab76672bdbcb72e04c88eda4095c60f1c14bea92ca44055a175e886da"
-    sha256 cellar: :any_skip_relocation, catalina:      "785b5fdcba51200e389341550837690f9796139e50770831d8d08a36a2acfafb"
-    sha256 cellar: :any_skip_relocation, mojave:        "31873834919bd8ddd75ce8c608f8866894c369c1bcc1fa46e4778bb6531199ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc01bdeff82b14153f599b72b7d92de4eb5d037c0f4b8cdcbd90e3fe0b0a5b2d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cce9bb5d4d70d513016c7f2d4f3493a659ac4a81b01fadff20b93e0df2ce69e0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8780abff0b126cca9e22f89a1e84c6cb9187c5638743764329b84df8063098a"
+    sha256 cellar: :any_skip_relocation, monterey:       "8c6e154812b219b37d2764643d28b8ad97f8e91215a155f36df93f1997c2c2a0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bd532ceab76672bdbcb72e04c88eda4095c60f1c14bea92ca44055a175e886da"
+    sha256 cellar: :any_skip_relocation, catalina:       "785b5fdcba51200e389341550837690f9796139e50770831d8d08a36a2acfafb"
+    sha256 cellar: :any_skip_relocation, mojave:         "31873834919bd8ddd75ce8c608f8866894c369c1bcc1fa46e4778bb6531199ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cc01bdeff82b14153f599b72b7d92de4eb5d037c0f4b8cdcbd90e3fe0b0a5b2d"
   end
 
   def install
