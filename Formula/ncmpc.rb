69d1f960140c1cdee730a5284b6c7836f4abd518
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 17:30:45 +0000

    ncmpc: update 0.45 bottle.

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -12,11 +12,12 @@ class Ncmpc < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "a64c5c10fe80ddd0ed8b2be3c18b9e372d9ed4d67a2d80bd28f6190721c92b89"
-    sha256 cellar: :any, big_sur:       "fa933f289d06e2e37bf1b94d897fe86b20f294e5910f5782fc5a2bd0be20e75c"
-    sha256 cellar: :any, catalina:      "70b5ffcaebc6ff5cf99f6a3b2ef2c4533457fe43b62e9107be991f8367be6785"
-    sha256 cellar: :any, mojave:        "ae2e4bb23568d6a807defe05f4d5256ca265c9aa7657b8ad073dc4f71e1c62ca"
-    sha256               x86_64_linux:  "a2078dbe3f31bc48f25389018cb28fadb64b303d4671dedea214e98a37c34dc7"
+    sha256 cellar: :any, arm64_monterey: "81ba07b2e95cef84c71eb8a39d735580775bb67875f4075c824a7347fc6385c2"
+    sha256 cellar: :any, arm64_big_sur:  "a64c5c10fe80ddd0ed8b2be3c18b9e372d9ed4d67a2d80bd28f6190721c92b89"
+    sha256 cellar: :any, big_sur:        "fa933f289d06e2e37bf1b94d897fe86b20f294e5910f5782fc5a2bd0be20e75c"
+    sha256 cellar: :any, catalina:       "70b5ffcaebc6ff5cf99f6a3b2ef2c4533457fe43b62e9107be991f8367be6785"
+    sha256 cellar: :any, mojave:         "ae2e4bb23568d6a807defe05f4d5256ca265c9aa7657b8ad073dc4f71e1c62ca"
+    sha256               x86_64_linux:   "a2078dbe3f31bc48f25389018cb28fadb64b303d4671dedea214e98a37c34dc7"
   end
 
   depends_on "boost" => :build
