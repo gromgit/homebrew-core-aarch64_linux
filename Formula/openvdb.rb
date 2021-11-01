710820491b60740766069182e99cb058dbba251a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 16:06:14 +0000

    openvdb: update 8.1.0 bottle.

--- Formula/openvdb.rb
+++ Formula/openvdb.rb
@@ -10,11 +10,12 @@ class Openvdb < Formula
   head "https://github.com/AcademySoftwareFoundation/openvdb.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "3b009e6f335c6dd6264c391ede13d7dcda7731851f8e6bb8d7f1395d1baa1338"
-    sha256 cellar: :any,                 big_sur:       "09b92c96f974aa12123a31b92c5cda3fec0678d6491c5e9895d7cdd8dbfdde50"
-    sha256 cellar: :any,                 catalina:      "55ec23082cdec8e584dbacc3b566430a6d83f847b7fbaf58fcc817e05194b255"
-    sha256 cellar: :any,                 mojave:        "2a82056566ede58322204b6881cd00600b210d8fd9a781fc46499a39d254830a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8c1520e74d9762f6aa607dae1a6d1a111e83808f3f7458c986b3e498fd3487d"
+    sha256 cellar: :any,                 arm64_monterey: "31063bc8947d87d94954752809d6ab0a0e84315ba147f9a653e6b6d11200f605"
+    sha256 cellar: :any,                 arm64_big_sur:  "3b009e6f335c6dd6264c391ede13d7dcda7731851f8e6bb8d7f1395d1baa1338"
+    sha256 cellar: :any,                 big_sur:        "09b92c96f974aa12123a31b92c5cda3fec0678d6491c5e9895d7cdd8dbfdde50"
+    sha256 cellar: :any,                 catalina:       "55ec23082cdec8e584dbacc3b566430a6d83f847b7fbaf58fcc817e05194b255"
+    sha256 cellar: :any,                 mojave:         "2a82056566ede58322204b6881cd00600b210d8fd9a781fc46499a39d254830a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8c1520e74d9762f6aa607dae1a6d1a111e83808f3f7458c986b3e498fd3487d"
   end
 
   depends_on "cmake" => :build
