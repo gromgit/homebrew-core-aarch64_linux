7508f89330de59706072fd9b7274a8140f139377
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-01 10:17:27 +0000

    cweb: update 4.5 bottle.

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -5,13 +5,10 @@ class Cweb < Formula
   sha256 "5afa2bad211b60e7a3e33cf72b1ea0873b66427d24c17ec12e211b20bd1ad4aa"
 
   bottle do
-    sha256 arm64_monterey: "bf4fa48bb4855f8424de024bfdafccfd3ed28fade1485a8526ee5e6cfb80b509"
-    sha256 arm64_big_sur:  "9a1237298f33f69283f923525eb5668911226d48043024bbbd44d337ad1bcdbe"
-    sha256 monterey:       "de7c0543066179c97ebb5316d6bcfa8d6ecb7ca00e62507fda2d160cb019ed71"
-    sha256 big_sur:        "2ee6617cf9da76e1e7cafbd49ed6e53fee15339c82df053eab55a398fb96f50b"
-    sha256 catalina:       "e100640669a1d066177514aae1c813f7c18b530c4cae5744d0431d850933648c"
-    sha256 mojave:         "410376faad622cd11745ca94c877135c1b4837ccdc8c7cab43bf12b4b849a3b9"
-    sha256 x86_64_linux:   "62cac91ee39b5981ac30e4797a75b76572b4ae04c539796d120f4fbe36fc9716"
+    sha256                               arm64_big_sur: "90e531e4c475de1c1630fe17b0a41856abbe0aaff3f6edebdbc94deccc4b8bff"
+    sha256                               big_sur:       "d289cbfd452407b42ed39ce37127e7c7d5187752ad8b286d5c06692460336bf3"
+    sha256                               catalina:      "65606687d8bfe11b2a441613123fa2fac30f9362e822f13695c1f168bc01b62f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d164644aba2adcb257e9316d4627b94239ecfc90cc477e8715ca6284ac416363"
   end
 
   conflicts_with "texlive", because: "both install `cweb` binaries"
