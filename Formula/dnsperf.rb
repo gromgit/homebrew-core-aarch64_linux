eb88952805c427039e1ce1d9ad5429db7de17bf7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 18:58:06 +0000

    dnsperf: update 2.7.1_2 bottle.

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -14,6 +14,7 @@ class Dnsperf < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "ed9838c9f524357a9c35a91df072098140af8f20b5b55b818cc91350e1537d63"
     sha256 cellar: :any,                 arm64_big_sur:  "9058dc5367702b9bb5ceb1fc417a34f60344eea124384beca9c75b333351265b"
+    sha256 cellar: :any,                 monterey:       "618fbfd1a0c7491c2c50910d4c0cccedbf1ea38d22afff30c0d881b0ee004332"
     sha256 cellar: :any,                 big_sur:        "33f81adab77f7d988a927144bdf77f5090d3489e0d3ef812f3b273c74cd07e25"
     sha256 cellar: :any,                 catalina:       "88b0ae4ee57e39b8e87f97d5b1aac1c12b5f44e744aad1316b45619781e25b34"
     sha256 cellar: :any,                 mojave:         "4d9debb3fb6b0a43620a205abf27d083d6b731e355afcb8c55f23812347f7aa2"
