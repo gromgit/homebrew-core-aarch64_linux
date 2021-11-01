1c4ca93908258cf6d9d1007b32c661f0c319a98a
Rui Chen <rui@chenrui.dev>
2021-10-31 20:52:05 -0400

    kamel 1.6.1
    Closes #88480.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kamel.rb
+++ Formula/kamel.rb
@@ -2,8 +2,8 @@ class Kamel < Formula
   desc "Apache Camel K CLI"
   homepage "https://camel.apache.org/"
   url "https://github.com/apache/camel-k.git",
-      tag:      "v1.6.0",
-      revision: "e929db111d6d8ac000f9262342cb3d2eed157aad"
+      tag:      "v1.6.1",
+      revision: "7f1a34f473275b3c40eb0e1c369901c030a71c93"
   license "Apache-2.0"
   head "https://github.com/apache/camel-k.git"
 
