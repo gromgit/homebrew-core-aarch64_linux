4e3f6ed77b65a9165e94bb2ca63dcb512e5df0f2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 16:46:36 +0000

    claws-mail: update 3.18.0 bottle.

--- Formula/claws-mail.rb
+++ Formula/claws-mail.rb
@@ -11,10 +11,11 @@ class ClawsMail < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "1e94b0baa4142502cd2f18e4bbdeafe3d73b7d0a5a6c13c16b5a74b6e49a908d"
-    sha256 big_sur:       "2c94ba339b902091781d9201c6cd71a2b9dfd401224317ac1f1368d17e44ee03"
-    sha256 catalina:      "e1691b2e1a632759a0c4aef4167265af4edde36d777eb5bfce0310c3235f2b1d"
-    sha256 mojave:        "b279ad0a5d167795deed2bdadafeea2f7fff4dca2b26c580a91c28cdaaa4b1c5"
+    sha256 arm64_monterey: "5f1a0bc9c1950c7bf5169f927e7f0cc6895e43e6188dafc38a2cf1cbc029f9e4"
+    sha256 arm64_big_sur:  "1e94b0baa4142502cd2f18e4bbdeafe3d73b7d0a5a6c13c16b5a74b6e49a908d"
+    sha256 big_sur:        "2c94ba339b902091781d9201c6cd71a2b9dfd401224317ac1f1368d17e44ee03"
+    sha256 catalina:       "e1691b2e1a632759a0c4aef4167265af4edde36d777eb5bfce0310c3235f2b1d"
+    sha256 mojave:         "b279ad0a5d167795deed2bdadafeea2f7fff4dca2b26c580a91c28cdaaa4b1c5"
   end
 
   depends_on "pkg-config" => :build
