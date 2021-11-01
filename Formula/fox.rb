f9e6ee85a4c1d9d393fcd1e86d8161e1ed68c962
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 23:42:39 +0800

    fox: patch incorrect -flat_namespace usage
    This is needed for bottling on Monterey.
    
    Closes #88525.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fox.rb
+++ Formula/fox.rb
@@ -36,6 +36,12 @@ class Fox < Formula
   depends_on "mesa"
   depends_on "mesa-glu"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
     # Needed for libxft to find ftbuild2.h provided by freetype
     ENV.append "CPPFLAGS", "-I#{Formula["freetype"].opt_include}/freetype2"
