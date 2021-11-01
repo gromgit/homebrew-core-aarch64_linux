70ab8ee6de0100dd3410641e7fb567e42977711c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 20:54:48 +0000

    librem: update 0.6.0 bottle.

--- Formula/librem.rb
+++ Formula/librem.rb
@@ -8,6 +8,7 @@ class Librem < Formula
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "2762f50f0dce284c02dbdbbe00ae376fbc143ae74aa028dffb12f3577c720eb0"
     sha256 cellar: :any,                 arm64_big_sur:  "f89cd157675ca8fe3154b27cfeccf20846e40911c74820ccf61d2fb83abd6bfd"
+    sha256 cellar: :any,                 monterey:       "eac3ed6596fbb6296164a74c67b2a7495cdb215a79567b90c6e3a259d50d693b"
     sha256 cellar: :any,                 big_sur:        "b571b06f4fc31e68adc40869c1e5b415eda6aed48f9093ee844f53f17baf5f44"
     sha256 cellar: :any,                 catalina:       "cfb5f86357e8176a51e80b2ec726e8e38fc002a7dfea8f36256328747df01d9a"
     sha256 cellar: :any,                 mojave:         "5fc70f673166bfc2794c40b440de29cc544cb3d46cccfb538ba868362e6e813a"
