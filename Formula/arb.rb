67dae81af353949ef333fbe8e32f7900dd3c6811
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 19:49:37 +0000

    arb: update 2.21.1 bottle.

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -9,6 +9,7 @@ class Arb < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "41583ad89df5a7377eed7820a2362b1c541d71b946a8984c3de80deef25b8c87"
     sha256 cellar: :any,                 arm64_big_sur:  "90a180e2d81802642eebee8b799be87fee105fe21ee96a387d0d0fa0e6e6bb6f"
+    sha256 cellar: :any,                 monterey:       "dca91f023aa5bb528ab8f43a5973180d014385818dc2fdc8108be7de60f3d02c"
     sha256 cellar: :any,                 big_sur:        "43d1508bf8b2902ad5dc670db8827f3183df9ce1069fb15072c2fb578c4c2ebb"
     sha256 cellar: :any,                 catalina:       "4f9d704d41d0b413bfd7523c7217cc7b0b31accf060565dc188424d1ce3bf44d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "39945070871bece010b60f8af6d4ed6b79633bd6cf5dbde78a52faebd8cbfea4"
