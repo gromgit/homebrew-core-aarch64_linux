5dec1bfc24083754bd6b4d7be0ba55353e1ba3c8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 22:16:25 +0000

    zydis: update 3.2.0 bottle.

--- Formula/zydis.rb
+++ Formula/zydis.rb
@@ -9,6 +9,7 @@ class Zydis < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "49b61da77b2bc62fd900cb335e3d0c36093c5abf5dde65814f4b83a9a419856a"
+    sha256 cellar: :any_skip_relocation, monterey:      "f7d4bd77d3304b11d0c5bb6c1cafb7676354c70219e26adc6b1b4bc4f9250dd6"
     sha256 cellar: :any_skip_relocation, big_sur:       "caf94bcb4ccdf9cbeb849a614837264e8bcb5413ef1353ad9da20ebcef7d51d9"
     sha256 cellar: :any_skip_relocation, catalina:      "f4b7cdfca8bf8d59843b75d493401b04a917a372658fec079db52e0ca1485a69"
     sha256 cellar: :any_skip_relocation, mojave:        "9cbfdefca894dd7822f0a4d2d7639cf80ca24c0684d99338b84de04e317f3de7"
