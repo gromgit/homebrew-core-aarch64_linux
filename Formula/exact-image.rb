fe7af0306b28e6e063a2b4e39db11b2b7b687de3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 20:25:14 +0000

    exact-image: update 1.0.2 bottle.

--- Formula/exact-image.rb
+++ Formula/exact-image.rb
@@ -13,6 +13,7 @@ class ExactImage < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "a11e0789738c798fc3cf6785353e023e4e27b0328aa7ccb5dd63c39b988a0691"
     sha256                               arm64_big_sur:  "47aa8c7861a759d66f553bc8960ba09c14a3b5acf86e2c0f22779379716cac5f"
+    sha256 cellar: :any,                 monterey:       "39352ff44276b4d207a58999302b32fa3e6a1918a955c6c3e1d3235ee4654634"
     sha256                               big_sur:        "9b3619df825bd01981c7a7b6fd1b6f88346d7d0fbbb7f9ed8fc30f9fef41cab0"
     sha256                               catalina:       "78a802b0edd2c27640aa2e6be381c146a7fa05bd6dd584ace90b1dfa0e426291"
     sha256                               mojave:         "942bfd38bf5fd52613c936077eee5d5f71530325c7337e9db84e44e0b6c643a0"
