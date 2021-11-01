ec32b49dfa0e57b1b4f7ed5c4c61258ac4a3a595
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 17:32:48 +0000

    x11vnc: update 0.9.16 bottle.

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -8,6 +8,7 @@ class X11vnc < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "056f5811d473464e1642e707b0e9f970ab8a4d21a39643e417e65795a36cbc5f"
     sha256 cellar: :any,                 arm64_big_sur:  "f066915a77e9635f0f1394566b2b825bcb0a02207e6393e3c0c5a62b7f8a03ae"
+    sha256 cellar: :any,                 monterey:       "8e50851314ec0ed46b747c489a54b56d2ec2af9e74be8458022612f0a7e236b1"
     sha256 cellar: :any,                 big_sur:        "50270ae1fd7681db301b3449748f46108d5eb93df535be7085ef6498f936555d"
     sha256 cellar: :any,                 catalina:       "66ddb190e2e2a183ba662d4c7ac2de508b6ebe3c3c827078a5eec5b550477e5e"
     sha256 cellar: :any,                 mojave:         "cd3d5d0047a8fb2e7b66ac94baf08c2da16aa8e135b8180acacce2d1bf366e58"
