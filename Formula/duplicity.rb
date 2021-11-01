e46ab1ac45717893e6f24808bb52a439f138c2e0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 16:09:58 +0000

    duplicity: update 0.8.20_1 bottle.

--- Formula/duplicity.rb
+++ Formula/duplicity.rb
@@ -9,11 +9,12 @@ class Duplicity < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "6c6b66266734d719cf06275cc51da33f1250fbdfdecfe925b2ea9cbbbc43075a"
-    sha256 cellar: :any,                 big_sur:       "6a0149ec7cee5ae2b9e94ba3abda42976c1c3930840405d5c4935d294148a9bc"
-    sha256 cellar: :any,                 catalina:      "2c1188acf3647c4d69cd838ef872c5510d2a4dde419930e8c9407b8684ef1cf6"
-    sha256 cellar: :any,                 mojave:        "9ce160d22e8f610640b7966d06eeec924c15601ee9604f4d72f6c47f420e6508"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "667182e0c461b0fb907b3587d332506d3a4db4ee0d72c46bd43f4c1990fcc58b"
+    sha256 cellar: :any,                 arm64_monterey: "4d4a669e32edb2f371d1d378cfcea8c4afd0a14c2902481ab130d5b54d93a93b"
+    sha256 cellar: :any,                 arm64_big_sur:  "6c6b66266734d719cf06275cc51da33f1250fbdfdecfe925b2ea9cbbbc43075a"
+    sha256 cellar: :any,                 big_sur:        "6a0149ec7cee5ae2b9e94ba3abda42976c1c3930840405d5c4935d294148a9bc"
+    sha256 cellar: :any,                 catalina:       "2c1188acf3647c4d69cd838ef872c5510d2a4dde419930e8c9407b8684ef1cf6"
+    sha256 cellar: :any,                 mojave:         "9ce160d22e8f610640b7966d06eeec924c15601ee9604f4d72f6c47f420e6508"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "667182e0c461b0fb907b3587d332506d3a4db4ee0d72c46bd43f4c1990fcc58b"
   end
 
   depends_on "rust" => :build # for cryptography
