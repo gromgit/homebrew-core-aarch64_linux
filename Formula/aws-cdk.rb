9c6f01e57964e9f0551496576d8d2ca4707a2d5c
rui <rui@chenrui.dev>
2021-11-01 00:53:13 +0000

    aws-cdk: update 1.130.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -8,10 +8,12 @@ class AwsCdk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "346e91052f0f93cbfa5d4ed03f900d818e781ac2335446e18f41325bf9754201"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bd272042e0c40ec6b1d3c78df91c2be7b7fd3eb3996ecc55e867c82f6550c41b"
-    sha256 cellar: :any_skip_relocation, catalina:      "bd272042e0c40ec6b1d3c78df91c2be7b7fd3eb3996ecc55e867c82f6550c41b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "346e91052f0f93cbfa5d4ed03f900d818e781ac2335446e18f41325bf9754201"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "346e91052f0f93cbfa5d4ed03f900d818e781ac2335446e18f41325bf9754201"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "346e91052f0f93cbfa5d4ed03f900d818e781ac2335446e18f41325bf9754201"
+    sha256 cellar: :any_skip_relocation, monterey:       "bd272042e0c40ec6b1d3c78df91c2be7b7fd3eb3996ecc55e867c82f6550c41b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bd272042e0c40ec6b1d3c78df91c2be7b7fd3eb3996ecc55e867c82f6550c41b"
+    sha256 cellar: :any_skip_relocation, catalina:       "bd272042e0c40ec6b1d3c78df91c2be7b7fd3eb3996ecc55e867c82f6550c41b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "346e91052f0f93cbfa5d4ed03f900d818e781ac2335446e18f41325bf9754201"
   end
 
   depends_on "node"
