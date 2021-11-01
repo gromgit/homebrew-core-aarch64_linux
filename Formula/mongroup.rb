1ed928d2aa362588ee3dd6050b5067cfbd0f4890
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 22:09:01 +0000

    mongroup: update 0.4.1 bottle.

--- Formula/mongroup.rb
+++ Formula/mongroup.rb
@@ -8,6 +8,7 @@ class Mongroup < Formula
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_monterey: "16deb20baa844034c6a1c8bfcb61971a71f576fbebd0dd6170321aeed2866ee7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc40264f75aafc4f194f10d2b7472e152a4c9f58ec104db6ebeac3dc346c4370"
+    sha256 cellar: :any_skip_relocation, monterey:       "16deb20baa844034c6a1c8bfcb61971a71f576fbebd0dd6170321aeed2866ee7"
     sha256 cellar: :any_skip_relocation, big_sur:        "d2112e201508ee22715b2e318b2fd97ae0cf4044e1d74d0893abf12598fb72bc"
     sha256 cellar: :any_skip_relocation, catalina:       "57c107a2746fae7e9db832f54df3d5170449defc30334094939794288659f026"
     sha256 cellar: :any_skip_relocation, mojave:         "4c11751013bae001ff2dcf55c3566613e83fe0d9257e0691c9da7b2aec298918"
