a5c2313e1b3b70bac3372cf39eb5bd6c736962d6
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-11-01 04:23:57 +0000

    kubevela: update 1.1.7 bottle.

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -7,10 +7,12 @@ class Kubevela < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "63897a419d79ef4b66afa0f635507766ed63b90660b8ae2e4ac4d3eec07da080"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4dcb0717494a844f63a55d7e26413197b694169be98bb0b6459f38c703fc50b2"
-    sha256 cellar: :any_skip_relocation, catalina:      "905bc8864bdd727fb9a223490dd2e47af8d58a8f00e6a8fa3b32b0dd323ff409"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b48f6466de06dac30df14811aa01e815ec9c1c8e5ad48bb58a42880fc1c36f6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "89a45a3ddeb349b19b00e13dd10c330a16b5cfaee1c59b5ff2fc0f2e9845a172"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63897a419d79ef4b66afa0f635507766ed63b90660b8ae2e4ac4d3eec07da080"
+    sha256 cellar: :any_skip_relocation, monterey:       "ac2c3e50533d537c73dda62b283bcd93ec8e82a8c533903dcdcba2b7032e7d6f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4dcb0717494a844f63a55d7e26413197b694169be98bb0b6459f38c703fc50b2"
+    sha256 cellar: :any_skip_relocation, catalina:       "905bc8864bdd727fb9a223490dd2e47af8d58a8f00e6a8fa3b32b0dd323ff409"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b48f6466de06dac30df14811aa01e815ec9c1c8e5ad48bb58a42880fc1c36f6"
   end
 
   depends_on "go" => :build
