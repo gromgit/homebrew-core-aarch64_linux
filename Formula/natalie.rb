3740ef1be905f753b986ec19901f3ec6ee5790b7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 05:20:03 +0000

    natalie: update 0.7.0_1 bottle.

--- Formula/natalie.rb
+++ Formula/natalie.rb
@@ -8,11 +8,13 @@ class Natalie < Formula
   head "https://github.com/krzyzanowskim/Natalie.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "df7c39d8bcb8d60b095d9b9b90207042791d994f22a53c97e709aa16768a9b28"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6e745ce1d0a34e0887319f32d39ae10e7ae34475fcb9a54916569f7672074b22"
-    sha256 cellar: :any_skip_relocation, catalina:      "218ec8bb0ac3ac4de7a6fa8489f3ad7013b1beb051a7c0e74a6e37ade79eee6c"
-    sha256 cellar: :any_skip_relocation, mojave:        "9dcc093fc648175eb165aec20413246ace7427d0d3c4a9884d37cfad9a851dca"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "dd51e00a1969ffdd478e954bed48bedd1c5a9813b67931aa146711f49cb58223"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "149e46b9c1fd6dc56e855583824fc194f68f6c910b671bebe4fa68ac43915041"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "df7c39d8bcb8d60b095d9b9b90207042791d994f22a53c97e709aa16768a9b28"
+    sha256 cellar: :any_skip_relocation, monterey:       "5bb0e910fe6f93ec2a190e3a36e51baa2550bd08c64a83ebd6f8d9ca78296510"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6e745ce1d0a34e0887319f32d39ae10e7ae34475fcb9a54916569f7672074b22"
+    sha256 cellar: :any_skip_relocation, catalina:       "218ec8bb0ac3ac4de7a6fa8489f3ad7013b1beb051a7c0e74a6e37ade79eee6c"
+    sha256 cellar: :any_skip_relocation, mojave:         "9dcc093fc648175eb165aec20413246ace7427d0d3c4a9884d37cfad9a851dca"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "dd51e00a1969ffdd478e954bed48bedd1c5a9813b67931aa146711f49cb58223"
   end
 
   deprecate! date: "2020-09-08", because: :repo_archived
