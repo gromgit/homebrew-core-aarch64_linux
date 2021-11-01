63e9d58935371348946636b545c6377b3ef9522e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 05:26:03 +0000

    sql-translator: update 1.62 bottle.

--- Formula/sql-translator.rb
+++ Formula/sql-translator.rb
@@ -10,12 +10,14 @@ class SqlTranslator < Formula
   license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd430de2cdd05f6766fad897788bcba2405eb93f0c35b56952df1b0c062d739f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7389609bcf5844ae648f0c4f8cf7e9cc6776ac085f3826378e3dd54421d5627e"
-    sha256 cellar: :any_skip_relocation, catalina:      "3d141958909ac51a8ba45a075db30100a447230d2a6e1bf08e44a8a677425afd"
-    sha256 cellar: :any_skip_relocation, mojave:        "9293157476e7ed616067b1c378c811c9e05c5ec17d9c0ad14d1d785428945d80"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "d9247d5d149363071ceecea045453779ebb5c0df249e0ef3b679b6c684acb453"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "950bde11b0c016ec277dab61ce3aea1f61aba2e7a069da3abce3ba3ba733bf08"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "87a368a2a4ac14068f5af0552cb54c8cd7eaa773946ebee0f35ed7a4b2afb516"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd430de2cdd05f6766fad897788bcba2405eb93f0c35b56952df1b0c062d739f"
+    sha256 cellar: :any_skip_relocation, monterey:       "f987d8c6e63c50e590066fe49e53b6f24472bd2ae05fb8a6fec93e87874b883e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7389609bcf5844ae648f0c4f8cf7e9cc6776ac085f3826378e3dd54421d5627e"
+    sha256 cellar: :any_skip_relocation, catalina:       "3d141958909ac51a8ba45a075db30100a447230d2a6e1bf08e44a8a677425afd"
+    sha256 cellar: :any_skip_relocation, mojave:         "9293157476e7ed616067b1c378c811c9e05c5ec17d9c0ad14d1d785428945d80"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "d9247d5d149363071ceecea045453779ebb5c0df249e0ef3b679b6c684acb453"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "950bde11b0c016ec277dab61ce3aea1f61aba2e7a069da3abce3ba3ba733bf08"
   end
 
   uses_from_macos "perl"
