d97dc29ca47214d1e9b4d1a72babcd06b000ec9d
rui <rui@chenrui.dev>
2021-11-01 00:58:16 +0000

    esbuild: update 0.13.12 bottle.

--- Formula/esbuild.rb
+++ Formula/esbuild.rb
@@ -8,10 +8,12 @@ class Esbuild < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "526cdf12f8622ca6798fa64f1a1e7f47afd2826debff33bb515e784b49be49e2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a9137b4121619684da117b0f0ac9664494d9531ea52621465b80378a1c9893bf"
-    sha256 cellar: :any_skip_relocation, catalina:      "a9137b4121619684da117b0f0ac9664494d9531ea52621465b80378a1c9893bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "165394a8f78fda0df2e0943b157a4f4afdcc16cbfb2e0a64dda0b1e2228804e7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "526cdf12f8622ca6798fa64f1a1e7f47afd2826debff33bb515e784b49be49e2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "526cdf12f8622ca6798fa64f1a1e7f47afd2826debff33bb515e784b49be49e2"
+    sha256 cellar: :any_skip_relocation, monterey:       "a9137b4121619684da117b0f0ac9664494d9531ea52621465b80378a1c9893bf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a9137b4121619684da117b0f0ac9664494d9531ea52621465b80378a1c9893bf"
+    sha256 cellar: :any_skip_relocation, catalina:       "a9137b4121619684da117b0f0ac9664494d9531ea52621465b80378a1c9893bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "165394a8f78fda0df2e0943b157a4f4afdcc16cbfb2e0a64dda0b1e2228804e7"
   end
 
   depends_on "node"
