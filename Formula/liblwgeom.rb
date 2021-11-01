53ca2a0050a3f314c079e9fd459125a2950e7c71
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 18:59:05 +0000

    liblwgeom: update 2.5.4_1 bottle.

--- Formula/liblwgeom.rb
+++ Formula/liblwgeom.rb
@@ -11,6 +11,7 @@ class Liblwgeom < Formula
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "c6e2f39f90827be84a17c7a1bfd7c0043bf3f0a433bfc1e38e2d9094028caad1"
     sha256 cellar: :any,                 arm64_big_sur:  "4f8f0403e973d5e2eafb1b3d49deae54a3cb95a80dc42c50f1f28edcc73da0d8"
+    sha256 cellar: :any,                 monterey:       "fe6ef90e0a9026b59e89684f49ad9ddcb25a06fff022168b0b8f1f8d10a92bb7"
     sha256 cellar: :any,                 big_sur:        "e28a391dfb1ccf34656e8169d5eda63bb96c7693508429f7c22b47add8a8bd47"
     sha256 cellar: :any,                 catalina:       "cd5a31ea1b30721f36fcd64285b3150667c4cf30a148ffafa88d4e5c81456f45"
     sha256 cellar: :any,                 mojave:         "79247efadb38c42e631ceeb750a8379fd68a2a5c720ec265f8f11502764be46b"
