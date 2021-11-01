29e3173891df93bdd169940995e36015952c81ea
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 06:02:13 +0000

    density: update 0.14.2 bottle.

--- Formula/density.rb
+++ Formula/density.rb
@@ -6,13 +6,15 @@ class Density < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1a4efa81a981da64d95dd3c323e0e7ee8fd78c80f7bbcc103e301772e2efb1d7"
-    sha256 cellar: :any, big_sur:       "c262dbf150942de16d4054010d40622491eeba19c7f273d64a5675de9bf50024"
-    sha256 cellar: :any, catalina:      "1d7dee6e22957cd5d755b11628e9a64d2233ef4abcb27e6aea15cbc9313b057e"
-    sha256 cellar: :any, mojave:        "a123e229e87c6acb5e38292e35671f71e49d0cd0afbb1a315d941a49ab3ec8e4"
-    sha256 cellar: :any, high_sierra:   "798b64ce99c516e735c76aea05944869124634488c82af5ba3f2930f60d83de3"
-    sha256 cellar: :any, sierra:        "7740a6a2ba8aa701025c01553c57051e72b49e2f8f0774f064ca98832e1e58e9"
-    sha256 cellar: :any, el_capitan:    "6853e909ed8057817becb03cbb5dbfdd3d4e2ea35348bf562e86dc51aed11a78"
+    sha256 cellar: :any, arm64_monterey: "3c7879e53bde3927486a7ce07426c32a43057341ac351ef7af58c983b93a1be9"
+    sha256 cellar: :any, arm64_big_sur:  "1a4efa81a981da64d95dd3c323e0e7ee8fd78c80f7bbcc103e301772e2efb1d7"
+    sha256 cellar: :any, monterey:       "a37ef9a77b7e322d50e86cd94f10a7d1513b4f103b14a9beb03435ae49603af7"
+    sha256 cellar: :any, big_sur:        "c262dbf150942de16d4054010d40622491eeba19c7f273d64a5675de9bf50024"
+    sha256 cellar: :any, catalina:       "1d7dee6e22957cd5d755b11628e9a64d2233ef4abcb27e6aea15cbc9313b057e"
+    sha256 cellar: :any, mojave:         "a123e229e87c6acb5e38292e35671f71e49d0cd0afbb1a315d941a49ab3ec8e4"
+    sha256 cellar: :any, high_sierra:    "798b64ce99c516e735c76aea05944869124634488c82af5ba3f2930f60d83de3"
+    sha256 cellar: :any, sierra:         "7740a6a2ba8aa701025c01553c57051e72b49e2f8f0774f064ca98832e1e58e9"
+    sha256 cellar: :any, el_capitan:     "6853e909ed8057817becb03cbb5dbfdd3d4e2ea35348bf562e86dc51aed11a78"
   end
 
   resource "cputime" do
