fab3cc9d4fc304fcdee4e5f982acb46a04ac2393
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 16:09:50 +0000

    lbdb: update 0.49.1 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -11,11 +11,12 @@ class Lbdb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6c8daad11d118ab04bb0209f95c9c828392a5b260eaee63dbe73a7b5f30b8ba8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d95cc4ac68303be926a5c9ca8a4a17bbc545f3ce02ce345ecebe95a613ed5484"
-    sha256 cellar: :any_skip_relocation, catalina:      "3eafefa5553d8660c6d315f15f6bb963b7e21673b485ad681a2b8e2c3c9aeb13"
-    sha256 cellar: :any_skip_relocation, mojave:        "d8292aef28eecf34b970aba54f473dfa88cd5c64b9415002e1a62ae1e2dc393c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "482715f9ab4a754a6425a34380b0388f51eff44c73b45ed1bec356566173050a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e087de084cff07eae814fc954fa74a74d73b0f06d0f91129977112d6558ecbac"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c8daad11d118ab04bb0209f95c9c828392a5b260eaee63dbe73a7b5f30b8ba8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d95cc4ac68303be926a5c9ca8a4a17bbc545f3ce02ce345ecebe95a613ed5484"
+    sha256 cellar: :any_skip_relocation, catalina:       "3eafefa5553d8660c6d315f15f6bb963b7e21673b485ad681a2b8e2c3c9aeb13"
+    sha256 cellar: :any_skip_relocation, mojave:         "d8292aef28eecf34b970aba54f473dfa88cd5c64b9415002e1a62ae1e2dc393c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "482715f9ab4a754a6425a34380b0388f51eff44c73b45ed1bec356566173050a"
   end
 
   depends_on "abook"
