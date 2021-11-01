17b75512a39e30aaa0ee95922a0f2df60278a4f9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 17:47:52 +0000

    gerbil-scheme: update 0.16 bottle.

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -11,11 +11,12 @@ class GerbilScheme < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "2961aec56f4dc30a10a944ffb2f7c34d2c725c4e53b3eb5486eaf5d1a2bd92dc"
-    sha256 big_sur:       "c116f383adefb6954e99e3fc318244f02c30ec0f53e3a7bac135c87e02a90dbc"
-    sha256 catalina:      "2e58cb74f57dd09a84bfed13aa1d44a1f1f8f5c057c59d2a27a338950c53ce82"
-    sha256 mojave:        "119570c624e9fb56e17df10263434eb0ba94ba3cdbcf5942c10cac9b26161318"
-    sha256 high_sierra:   "adb9592baedab1841b1a6603ff8cdc30694b01ecd5444d7c637a987f98356a44"
+    sha256 arm64_monterey: "e77cbea05b9d1991e02abd6d1ee6a59e623e3a27dd777cde903ef61ad16d195e"
+    sha256 arm64_big_sur:  "2961aec56f4dc30a10a944ffb2f7c34d2c725c4e53b3eb5486eaf5d1a2bd92dc"
+    sha256 big_sur:        "c116f383adefb6954e99e3fc318244f02c30ec0f53e3a7bac135c87e02a90dbc"
+    sha256 catalina:       "2e58cb74f57dd09a84bfed13aa1d44a1f1f8f5c057c59d2a27a338950c53ce82"
+    sha256 mojave:         "119570c624e9fb56e17df10263434eb0ba94ba3cdbcf5942c10cac9b26161318"
+    sha256 high_sierra:    "adb9592baedab1841b1a6603ff8cdc30694b01ecd5444d7c637a987f98356a44"
   end
 
   depends_on "gambit-scheme"
