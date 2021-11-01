47f1c1ae29baed3a80eade6e7442979f766a3088
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 20:02:26 +0000

    ttygif: update 1.5.0 bottle.

--- Formula/ttygif.rb
+++ Formula/ttygif.rb
@@ -8,6 +8,7 @@ class Ttygif < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c2c31ef2caaf2c5dcb9ee96fd19267f93c4a3e8376e9d44e21686bdede507d85"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c24b1c9714124e4e36eb799ef832ed4e1cd1264dd92c131d235df7cb47dbf10b"
+    sha256 cellar: :any_skip_relocation, monterey:       "4efc5dee59ad2f3dcdde5e6397272f5799a132691a2c3cfddc801963546cb61a"
     sha256 cellar: :any_skip_relocation, big_sur:        "10887e22e06da0d22817453166f442d619e28058e505d981b159dcf70b20e74a"
     sha256 cellar: :any_skip_relocation, catalina:       "61f7135b9f03465ac86f26e7b7cad7ca09ec35495841ee868b76f001faefd040"
     sha256 cellar: :any_skip_relocation, mojave:         "34060f2f53d6388461ca29a81938490bb1768aa9f44303c7cce717c2f8ad6246"
