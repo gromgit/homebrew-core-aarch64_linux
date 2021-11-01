e94a24cb79c8a1f90acddf377cf829193323d604
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 17:45:57 +0000

    djbdns: update 1.05 bottle.

--- Formula/djbdns.rb
+++ Formula/djbdns.rb
@@ -11,13 +11,14 @@ class Djbdns < Formula
 
   bottle do
     rebuild 3
-    sha256 arm64_big_sur: "62ab5e22e0c15787a98c84f23905dd569067cd4376dc8c472509ac5ee5d24955"
-    sha256 big_sur:       "1231622a14007c9ec76ef137a5e1a42a30ce4192b0fbba0cf768f981090059ce"
-    sha256 catalina:      "5b473b664d7370f2e838bd496555841e20a8ef13aaeee6b312fc6501911b7fe0"
-    sha256 mojave:        "b57557c57ac07e053f78b2e73aed4cc9ec72a0c89d68e4ca8bc1dd3b2b9cddba"
-    sha256 high_sierra:   "f6555710c361d47fabfeeb6d8148b84c3a7e973ba4407def4f0a37e327ac3a5b"
-    sha256 sierra:        "ce72334aa541af3a486f90e32b2162ba8b5c86825f0a52f1b6de9cb33640eeff"
-    sha256 el_capitan:    "9bbf4356e0bb4e25827fdf02d4efa0fc3763600456ad76e63f662dae6e1fb4ce"
+    sha256 arm64_monterey: "eb8f1b169c2ef3b24defe00ef952b8dab42b45d42517bce471aa6e9016c7b4b6"
+    sha256 arm64_big_sur:  "62ab5e22e0c15787a98c84f23905dd569067cd4376dc8c472509ac5ee5d24955"
+    sha256 big_sur:        "1231622a14007c9ec76ef137a5e1a42a30ce4192b0fbba0cf768f981090059ce"
+    sha256 catalina:       "5b473b664d7370f2e838bd496555841e20a8ef13aaeee6b312fc6501911b7fe0"
+    sha256 mojave:         "b57557c57ac07e053f78b2e73aed4cc9ec72a0c89d68e4ca8bc1dd3b2b9cddba"
+    sha256 high_sierra:    "f6555710c361d47fabfeeb6d8148b84c3a7e973ba4407def4f0a37e327ac3a5b"
+    sha256 sierra:         "ce72334aa541af3a486f90e32b2162ba8b5c86825f0a52f1b6de9cb33640eeff"
+    sha256 el_capitan:     "9bbf4356e0bb4e25827fdf02d4efa0fc3763600456ad76e63f662dae6e1fb4ce"
   end
 
   depends_on "daemontools"
