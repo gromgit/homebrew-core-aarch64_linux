fbbc0b16fc261ce51469cb467cf251de42b1ac25
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 15:44:33 +0000

    dub: update 1.27.0 bottle.

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -13,11 +13,12 @@ class Dub < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "20c67d16147775d49407fae421cef74e323cef53dab0a663b562e9710cde031f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "522469719933702ba030cce31f83a4f19638e17945efce2786eaa6ab29b38d4b"
-    sha256 cellar: :any_skip_relocation, catalina:      "a708c2b342b00718471ec8427e56f7d36f91d1029939469110ca830d0a961748"
-    sha256 cellar: :any_skip_relocation, mojave:        "5da8b64546f9436fcc1210c7acf63a9f95a2c300b7eec8192610e3ec25d7c124"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f60af34e8faf094bd0cfefedcad4cb7f5f7fa71634c9dad21229d44c693f7486"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a74cb46fb2e1b14261d877c8c1c3c492f5ca9cb333f1be78412119e957d7f81"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "20c67d16147775d49407fae421cef74e323cef53dab0a663b562e9710cde031f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "522469719933702ba030cce31f83a4f19638e17945efce2786eaa6ab29b38d4b"
+    sha256 cellar: :any_skip_relocation, catalina:       "a708c2b342b00718471ec8427e56f7d36f91d1029939469110ca830d0a961748"
+    sha256 cellar: :any_skip_relocation, mojave:         "5da8b64546f9436fcc1210c7acf63a9f95a2c300b7eec8192610e3ec25d7c124"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f60af34e8faf094bd0cfefedcad4cb7f5f7fa71634c9dad21229d44c693f7486"
   end
 
   depends_on "ldc" => :build
