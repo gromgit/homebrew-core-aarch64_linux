2880cb76129514cdbd33cbf97eef47c11b8c2ca3
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-11-01 04:22:33 +0000

    opa: update 0.34.0 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -7,10 +7,12 @@ class Opa < Formula
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6c33ea5a7b0805d904d6e9ef6685241c65fc8cd2a270e6eb8cf0a7dd7817203e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "37f10c5123a63a75428cfa1f51fb562f2759fa8f524777cb05a013110c30e943"
-    sha256 cellar: :any_skip_relocation, catalina:      "2fba8bde97d0b20df9dbc7c86428dbec96f7ab13fd0622624cae5b87c7bae054"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cad14d24cd671b0b58b9280789fa83a9db6532a160b492c0ad263f39c207cfc5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ec9588f2ef21af1728b185a8153247ed3cf7601a0d4bdf9de1e6faebdf95565"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c33ea5a7b0805d904d6e9ef6685241c65fc8cd2a270e6eb8cf0a7dd7817203e"
+    sha256 cellar: :any_skip_relocation, monterey:       "9a1b41bfc717b663aaf7a42582d55ca6d5a88e0e5757e3b49e52db08304df94e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "37f10c5123a63a75428cfa1f51fb562f2759fa8f524777cb05a013110c30e943"
+    sha256 cellar: :any_skip_relocation, catalina:       "2fba8bde97d0b20df9dbc7c86428dbec96f7ab13fd0622624cae5b87c7bae054"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cad14d24cd671b0b58b9280789fa83a9db6532a160b492c0ad263f39c207cfc5"
   end
 
   depends_on "go" => :build
