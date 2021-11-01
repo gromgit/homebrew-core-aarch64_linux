beac92a250356dbb4bc1f86e355d70634e8af219
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-11-01 03:59:45 +0000

    cogl: update 1.22.8 bottle.

--- Formula/cogl.rb
+++ Formula/cogl.rb
@@ -7,12 +7,14 @@ class Cogl < Formula
   head "https://gitlab.gnome.org/GNOME/cogl.git", branch: "cogl-1.22"
 
   bottle do
-    sha256 arm64_big_sur: "9a487a4bf7fbe5fdec29d902ba668fe20cbbc05e66864cb8d9c5fe564373e586"
-    sha256 big_sur:       "ec1ef03d2e1e855ae5277a2f599fb7ed83c221f0ae29d8c8a5f45277be96d869"
-    sha256 catalina:      "37fdd46a2845adf0e8f4ce85d5a80384ea235e435ef5f42167622f5224e4e51f"
-    sha256 mojave:        "eb37baaa178631afac43c8bb1c93cdf9b78dd7d44862c63dec598d54a51b201e"
-    sha256 high_sierra:   "46de52386a1123e828d94598279a99a88e3819d8f1dac1a51f39850a321ff7f2"
-    sha256 x86_64_linux:  "2e1af63ab7d9ffcfca78804cdcab02d3b60969c52c038d89a7a7caa42afd4c00"
+    sha256 arm64_monterey: "619709d99ff34b8468b4036dad1a23a355e10672ccffc109cf093d57183c3ae8"
+    sha256 arm64_big_sur:  "9a487a4bf7fbe5fdec29d902ba668fe20cbbc05e66864cb8d9c5fe564373e586"
+    sha256 monterey:       "b1770215a0378766b70f4337b6ca2608c54fbe78568cff08363dea00dfbc1b23"
+    sha256 big_sur:        "ec1ef03d2e1e855ae5277a2f599fb7ed83c221f0ae29d8c8a5f45277be96d869"
+    sha256 catalina:       "37fdd46a2845adf0e8f4ce85d5a80384ea235e435ef5f42167622f5224e4e51f"
+    sha256 mojave:         "eb37baaa178631afac43c8bb1c93cdf9b78dd7d44862c63dec598d54a51b201e"
+    sha256 high_sierra:    "46de52386a1123e828d94598279a99a88e3819d8f1dac1a51f39850a321ff7f2"
+    sha256 x86_64_linux:   "2e1af63ab7d9ffcfca78804cdcab02d3b60969c52c038d89a7a7caa42afd4c00"
   end
 
   depends_on "gobject-introspection" => :build
