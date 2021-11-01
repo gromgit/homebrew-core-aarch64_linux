4e9f1e4a04d47cb83b84f7bf46a93d9ce7115f53
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 16:42:29 +0000

    burp: update 2.2.18_1 bottle.

--- Formula/burp.rb
+++ Formula/burp.rb
@@ -20,13 +20,14 @@ class Burp < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "c577ab1379ef9343a399ddf347f52ceb5949eb86f68aa24f79aa8f566fbd8e70"
-    sha256 big_sur:       "2b7114e8a7c736749bf2a073c2cd34bd269ce2129c16035ee9d4df4c7faacfef"
-    sha256 catalina:      "a028ea604ba4bbb5abe2d9985e94ece9f673cf33e35191063eb91e356923e982"
-    sha256 mojave:        "f45062f56a6cc3bc9ba09b84d9f44e599015387d6d31b0ae8a289fa74a904021"
-    sha256 high_sierra:   "1855c5623a4d7ec1ed397f2646772d807a127f80f196c41dcae0efe7615afd8d"
-    sha256 sierra:        "157aa6cc33291ec50b8597b3bd97b08e0a92f79e634ec122eb0911e86bc395c9"
-    sha256 x86_64_linux:  "938917e5c0bc5901f546caee65cb0a0f37284a00b5a6d0a9e2df78d0fd3161b5"
+    sha256 arm64_monterey: "83b2b4ab24707d1ce623e5b17fc41c28c531efe201082732875155df0bdcd56e"
+    sha256 arm64_big_sur:  "c577ab1379ef9343a399ddf347f52ceb5949eb86f68aa24f79aa8f566fbd8e70"
+    sha256 big_sur:        "2b7114e8a7c736749bf2a073c2cd34bd269ce2129c16035ee9d4df4c7faacfef"
+    sha256 catalina:       "a028ea604ba4bbb5abe2d9985e94ece9f673cf33e35191063eb91e356923e982"
+    sha256 mojave:         "f45062f56a6cc3bc9ba09b84d9f44e599015387d6d31b0ae8a289fa74a904021"
+    sha256 high_sierra:    "1855c5623a4d7ec1ed397f2646772d807a127f80f196c41dcae0efe7615afd8d"
+    sha256 sierra:         "157aa6cc33291ec50b8597b3bd97b08e0a92f79e634ec122eb0911e86bc395c9"
+    sha256 x86_64_linux:   "938917e5c0bc5901f546caee65cb0a0f37284a00b5a6d0a9e2df78d0fd3161b5"
   end
 
   head do
