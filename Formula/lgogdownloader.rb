97ede12c09db1032035f5bfcf1d1226387c29d1f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 20:50:00 +0000

    lgogdownloader: update 3.8 bottle.

--- Formula/lgogdownloader.rb
+++ Formula/lgogdownloader.rb
@@ -14,6 +14,7 @@ class Lgogdownloader < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "4491f2c9b0fab031877c090f1880cad45348b6fb9636b9eec834fbcb16472a56"
     sha256 cellar: :any, arm64_big_sur:  "369c84e75a36b791769eb7cb84b888fd050a5ad16f9cb010e85c4bd28a17463c"
+    sha256 cellar: :any, monterey:       "9994c57133352e6eec3a7d3766745b41e07ab272441d8c705ae2a7b7ad1ee789"
     sha256 cellar: :any, big_sur:        "2fa1804d59145b057b02b32e358ba179246248d35ea69d53873e756d88ab95ff"
     sha256 cellar: :any, catalina:       "850adc82be488503d799eb2211311ab455839ca65b1e9d65b94c3ce9a8f3ec97"
     sha256 cellar: :any, mojave:         "3be4696256c82a16e5f30caaf0c9c2b7e99d465b3b96df2ff4e937ee7d3c78d7"
