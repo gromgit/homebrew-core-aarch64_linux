8fe9f945049dec437d466b050cd681b6df3ef801
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-01 18:29:40 +0000

    lbdb: update 0.49.1 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -13,6 +13,7 @@ class Lbdb < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e087de084cff07eae814fc954fa74a74d73b0f06d0f91129977112d6558ecbac"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c8daad11d118ab04bb0209f95c9c828392a5b260eaee63dbe73a7b5f30b8ba8"
+    sha256 cellar: :any_skip_relocation, monterey:       "41cdd85f7474a49f24cee7c69a7d5108c4ea24fb766674112415eb1b42659a0a"
     sha256 cellar: :any_skip_relocation, big_sur:        "d95cc4ac68303be926a5c9ca8a4a17bbc545f3ce02ce345ecebe95a613ed5484"
     sha256 cellar: :any_skip_relocation, catalina:       "3eafefa5553d8660c6d315f15f6bb963b7e21673b485ad681a2b8e2c3c9aeb13"
     sha256 cellar: :any_skip_relocation, mojave:         "d8292aef28eecf34b970aba54f473dfa88cd5c64b9415002e1a62ae1e2dc393c"
