13bb384118f7137fc2135b8bcc49e741105f85d1
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-11-01 03:54:33 +0000

    lychee: update 0.8.0 bottle.

--- Formula/lychee.rb
+++ Formula/lychee.rb
@@ -7,10 +7,12 @@ class Lychee < Formula
   head "https://github.com/lycheeverse/lychee.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "ca051953818fc0225798c9da1386ca123fe703072e663eb536742a16e9a3c8d4"
-    sha256 cellar: :any,                 big_sur:       "9cf953e7150a68d289afc6dc2c217a7fde47beba38f36923c56523fed33437de"
-    sha256 cellar: :any,                 catalina:      "1e86fdf493ac7a145f1a1b62eec6534c1912342fae1d3916fe7eee18e4a22fcf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "87922c0bfd94491dfb41c9ee4f4f71145b6f4e56223b4a288bbfc6122c231165"
+    sha256 cellar: :any,                 arm64_monterey: "fa5fc60ed01176f34aa9e83eda35fcaa099f338b1dcaf27fbd57fff4b4b0977f"
+    sha256 cellar: :any,                 arm64_big_sur:  "ca051953818fc0225798c9da1386ca123fe703072e663eb536742a16e9a3c8d4"
+    sha256 cellar: :any,                 monterey:       "3a7fefb067f893b85f5a8a41b0eadc9963b2f86763c0eb0732d174f2f7459cf6"
+    sha256 cellar: :any,                 big_sur:        "9cf953e7150a68d289afc6dc2c217a7fde47beba38f36923c56523fed33437de"
+    sha256 cellar: :any,                 catalina:       "1e86fdf493ac7a145f1a1b62eec6534c1912342fae1d3916fe7eee18e4a22fcf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87922c0bfd94491dfb41c9ee4f4f71145b6f4e56223b4a288bbfc6122c231165"
   end
 
   depends_on "rust" => :build
