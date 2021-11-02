5e13841974b01cbc0cb852c3522497f2c2e0fe2f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 06:49:07 +0000

    proteinortho: update 6.0.31 bottle.

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -7,6 +7,7 @@ class Proteinortho < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "765899e2a19ba1d3db3aca66172946123ebfde61e1d33982f15af260b15792df"
+    sha256 cellar: :any,                 monterey:      "a646a16c3c4127fb72ce2fae91fc774dd9462e22dec105464ace576c2eb046b7"
     sha256 cellar: :any,                 big_sur:       "c75afcf372564f4b5f22bbed0bcce9cdb7ec6fef91cd0fcf7e1aba153ecaaf32"
     sha256 cellar: :any,                 catalina:      "090b08218718cb955e4ee1044fc337394d9dc8f745eb83af5dbb4beb7615c153"
     sha256 cellar: :any,                 mojave:        "3861fa61d70fac1b647864001254a196fcd135f435af0d816a87c2605bb538e7"
