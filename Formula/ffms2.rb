f9c97c2a15948bf778ecd78ca820fed00b2fedfb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 07:32:26 +0000

    ffms2: update 2.40_1 bottle.

--- Formula/ffms2.rb
+++ Formula/ffms2.rb
@@ -11,11 +11,13 @@ class Ffms2 < Formula
   head "https://github.com/FFMS/ffms2.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "221a3acdb567fd7414a8c8c7a452a878a941962c9c88fb238e6242ae5d7fc1b3"
-    sha256 cellar: :any,                 big_sur:       "d3933ecde477f9ad7156ab174af028a409cf1a9e9def84f775036704a413101e"
-    sha256 cellar: :any,                 catalina:      "978c5addaa61cde403d5f5cf51448d6b9512e68c08570385b3ef645e39813d8b"
-    sha256 cellar: :any,                 mojave:        "4c9b2aa7932969e43df33c69c559df7bbc3212011e94a073cfb3024e334f7fee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86d18d8d718e46acabd4fa3a305667cdae70d9e82bc2905d2857f06c600ae049"
+    sha256 cellar: :any,                 arm64_monterey: "320b01b2ff9f7e02f2c9084ff3f5eecfa82ac965be08bb772ab93389e734db87"
+    sha256 cellar: :any,                 arm64_big_sur:  "221a3acdb567fd7414a8c8c7a452a878a941962c9c88fb238e6242ae5d7fc1b3"
+    sha256 cellar: :any,                 monterey:       "ccab0f325a1aba510fb1f1ae8078243126d43344c1cac035aaf9d1b2829708f6"
+    sha256 cellar: :any,                 big_sur:        "d3933ecde477f9ad7156ab174af028a409cf1a9e9def84f775036704a413101e"
+    sha256 cellar: :any,                 catalina:       "978c5addaa61cde403d5f5cf51448d6b9512e68c08570385b3ef645e39813d8b"
+    sha256 cellar: :any,                 mojave:         "4c9b2aa7932969e43df33c69c559df7bbc3212011e94a073cfb3024e334f7fee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "86d18d8d718e46acabd4fa3a305667cdae70d9e82bc2905d2857f06c600ae049"
   end
 
   depends_on "autoconf" => :build
