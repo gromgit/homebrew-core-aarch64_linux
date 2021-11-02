78051fdb39754696ab4c0ac1ff33ba3af897370b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 13:38:13 +0000

    elan-init: update 1.3.0 bottle.

--- Formula/elan-init.rb
+++ Formula/elan-init.rb
@@ -7,6 +7,7 @@ class ElanInit < Formula
   head "https://github.com/leanprover/elan.git"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:     "8e4ec81d30fc503b9e584012962ae689b97c7b39ad2a3f08be03162d1b8fd7b8"
     sha256 cellar: :any_skip_relocation, big_sur:      "72d7b850d01d2203c9c69c03466312085c92f9841aa6c5e94c760b21cbaf355b"
     sha256 cellar: :any_skip_relocation, catalina:     "242382ab38d52c7454779903b85f304dc0e4471dabc44769427faf7ed8ffcc5b"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "c3de4ae75a6278bab2efe45b05d0423f3dce38c1aa42a0e11e7b6d86ffeac357"
