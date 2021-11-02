f44a8528f8617511b5223de587364c4769d289bf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 18:26:42 +0000

    libstrophe: update 0.11.0 bottle.

--- Formula/libstrophe.rb
+++ Formula/libstrophe.rb
@@ -7,10 +7,12 @@ class Libstrophe < Formula
   head "https://github.com/strophe/libstrophe.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "db4c52347be6860ebc237f49996a177b1706a89653d4891db5041cf6b9cc9951"
-    sha256 cellar: :any,                 big_sur:       "9e0cf46077fc1ef4e30377e9a1d6047f542f86796b7f0e30bd6f9be307c20a6b"
-    sha256 cellar: :any,                 catalina:      "39d5bee8000fa0d133cb622b007cd050b2f1e7556a3f5e93d93ff2688abd6a1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b74000aa583bff02c47f079b4c75940ea81803761256eda098132c0b74d369b"
+    sha256 cellar: :any,                 arm64_monterey: "13e67f5e28df838ffbf4bdcaded2308ae38abb6665cafa6bd36deba5c6ed2cb9"
+    sha256 cellar: :any,                 arm64_big_sur:  "db4c52347be6860ebc237f49996a177b1706a89653d4891db5041cf6b9cc9951"
+    sha256 cellar: :any,                 monterey:       "571d271187eb094a7b8ddee9e93fec77140042e279860b2ce6293d38bdaa1898"
+    sha256 cellar: :any,                 big_sur:        "9e0cf46077fc1ef4e30377e9a1d6047f542f86796b7f0e30bd6f9be307c20a6b"
+    sha256 cellar: :any,                 catalina:       "39d5bee8000fa0d133cb622b007cd050b2f1e7556a3f5e93d93ff2688abd6a1b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1b74000aa583bff02c47f079b4c75940ea81803761256eda098132c0b74d369b"
   end
 
   depends_on "autoconf" => :build
