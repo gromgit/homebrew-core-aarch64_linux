99788fe517d5abdeef89692c8acd2c135e31b6ca
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 10:36:55 +0000

    links: update 2.25 bottle.

--- Formula/links.rb
+++ Formula/links.rb
@@ -12,6 +12,7 @@ class Links < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "b3e9ad1d3617acdbbc86edbc90f429083130a51eb6ea26b809774b8eaf39fa53"
+    sha256 cellar: :any,                 monterey:      "77261cf3aeedfe6b058734536d55b744b80bb52c1cf777a2d78497bd1f4c69e2"
     sha256 cellar: :any,                 big_sur:       "9d7178901279a9ee80768056fa4f2b04676777d5ef2063c9f8a8c2b9f98a8906"
     sha256 cellar: :any,                 catalina:      "1eaa9411f73c61b7a82ee8d16b94bd7c8c0dde86e1eec953c5ce5872394ee073"
     sha256 cellar: :any,                 mojave:        "c12924230d58173c263dcdd827ecb4d9f44bb1e654a191ff5882a46330125cdf"
