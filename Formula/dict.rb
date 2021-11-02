fac1d9164df23b79b61e0907189559848d89bd94
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 15:38:10 +0000

    dict: update 1.13.0 bottle.

--- Formula/dict.rb
+++ Formula/dict.rb
@@ -7,10 +7,12 @@ class Dict < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "8e18fd5f8791cb97d71d38bce9e2190fab100fe578c70d2d8be17e2726e46931"
-    sha256 big_sur:       "e6067e6141c67f672e798f885b2ae679e3e6246864891498d3146ae95935e04d"
-    sha256 catalina:      "51af859a4f115e88b032dfdedb3c1a854af3c5daa19a97d4e796251dfa60ed4f"
-    sha256 mojave:        "707b10a71cd00d48d591d44654147ac419a217e59c32702593fa6d8a27374f6b"
+    sha256 arm64_monterey: "23929a3cb0f10cbae3e5c1e9222cbc1422605739961331c3a6809c2fe5d6520d"
+    sha256 arm64_big_sur:  "8e18fd5f8791cb97d71d38bce9e2190fab100fe578c70d2d8be17e2726e46931"
+    sha256 monterey:       "fcaac6a257868b92ca7e205efa4aa03a4b2310a1dc6b4d4e484d3efe0296df66"
+    sha256 big_sur:        "e6067e6141c67f672e798f885b2ae679e3e6246864891498d3146ae95935e04d"
+    sha256 catalina:       "51af859a4f115e88b032dfdedb3c1a854af3c5daa19a97d4e796251dfa60ed4f"
+    sha256 mojave:         "707b10a71cd00d48d591d44654147ac419a217e59c32702593fa6d8a27374f6b"
   end
 
   depends_on "libtool" => :build
