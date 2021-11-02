b0c092e61f09e2b49949a34167b5a3e53a5ec786
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 12:52:23 +0000

    telegraf: update 1.20.3 bottle.

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -12,10 +12,12 @@ class Telegraf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f4bcd0c5cd11ba61ed4ee73ed884d4f026ef4b659600fb832e299f711da5324b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "09d0bed283ffc66e647027ec0f532ad575f21ea4197d1a825d51653306e47feb"
-    sha256 cellar: :any_skip_relocation, catalina:      "8d41a6f1a4dd4411b711e0af072a403a291fdac187bab206fc2bac24bb13a085"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "872f328452ee4d58df51da0bfc68d87201804e5672fcf9f093ac3f34ca2d3b6b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "55c237fc13ed19f1760333f014f463a7fb1372a49051ea94302ad4d7b8f29bf0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4bcd0c5cd11ba61ed4ee73ed884d4f026ef4b659600fb832e299f711da5324b"
+    sha256 cellar: :any_skip_relocation, monterey:       "8e82d87ec5323106dcb1596d973be545ad940028006f9ad81fd717f838093346"
+    sha256 cellar: :any_skip_relocation, big_sur:        "09d0bed283ffc66e647027ec0f532ad575f21ea4197d1a825d51653306e47feb"
+    sha256 cellar: :any_skip_relocation, catalina:       "8d41a6f1a4dd4411b711e0af072a403a291fdac187bab206fc2bac24bb13a085"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "872f328452ee4d58df51da0bfc68d87201804e5672fcf9f093ac3f34ca2d3b6b"
   end
 
   depends_on "go" => :build
