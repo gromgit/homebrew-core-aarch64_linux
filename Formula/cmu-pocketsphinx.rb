0048b5aa3c0793e9292fbe0777cb54ab72a32ee7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 13:14:21 +0000

    cmu-pocketsphinx: update 0.8 bottle.

--- Formula/cmu-pocketsphinx.rb
+++ Formula/cmu-pocketsphinx.rb
@@ -20,14 +20,16 @@ class CmuPocketsphinx < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "4c3fc6b03df2530653a4ff38b270e9a37dd968748b38a011cae6500ab38d8084"
-    sha256 big_sur:       "25ce90366420de438c34e8f037f1da3c7ded680375fcc69127cdec7695f84fed"
-    sha256 catalina:      "f2fc23a67634f26befdd128d21e886d7b3789484a14d498a40949e0e100d8afa"
-    sha256 mojave:        "cc655d82bfce35b2976e4dd0867fceb02b233363e7e101c62b09e60e2be9f8bb"
-    sha256 high_sierra:   "628d162751962337c769090867c3f9921d10b09f704f8f208b63abbefef205eb"
-    sha256 sierra:        "12abc8b527906e7ed0d2f6f0a6b6cb5c00f548fe94fcce995bdc80f43b4cf17b"
-    sha256 el_capitan:    "2f1f4738dbcf7641a530b82c4dc6447ecadb5f9b60cd2484c33c379efb5c46e5"
-    sha256 yosemite:      "dea4e6a8e131f68c94a6b9fb783a0445476354a90629001d5007fe3b4e5247bd"
+    sha256 arm64_monterey: "314f80412a27862cdb5b857b3ae8273e28e29f81dcb4279d60228b149df9b97f"
+    sha256 arm64_big_sur:  "4c3fc6b03df2530653a4ff38b270e9a37dd968748b38a011cae6500ab38d8084"
+    sha256 monterey:       "263434383a2856a30d3119a4683a7c61a52521f08b8ee7bd08f02d8b1efa2de7"
+    sha256 big_sur:        "25ce90366420de438c34e8f037f1da3c7ded680375fcc69127cdec7695f84fed"
+    sha256 catalina:       "f2fc23a67634f26befdd128d21e886d7b3789484a14d498a40949e0e100d8afa"
+    sha256 mojave:         "cc655d82bfce35b2976e4dd0867fceb02b233363e7e101c62b09e60e2be9f8bb"
+    sha256 high_sierra:    "628d162751962337c769090867c3f9921d10b09f704f8f208b63abbefef205eb"
+    sha256 sierra:         "12abc8b527906e7ed0d2f6f0a6b6cb5c00f548fe94fcce995bdc80f43b4cf17b"
+    sha256 el_capitan:     "2f1f4738dbcf7641a530b82c4dc6447ecadb5f9b60cd2484c33c379efb5c46e5"
+    sha256 yosemite:       "dea4e6a8e131f68c94a6b9fb783a0445476354a90629001d5007fe3b4e5247bd"
   end
 
   head do
