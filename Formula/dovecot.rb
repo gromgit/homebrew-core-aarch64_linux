05ccde94a622a74439493185c3effc16bfe63519
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 22:21:02 +0800

    dovecot: use Homebrew `curl`, patch out `-flat_namespace` (#88330)
    Downloading the `pigeonhole` resource fails with system `curl`. I'm not
    sure why this is needed for the resource only and not also the tarball,
    but here we are.
    
    We also apply one of our usual patches for the `-flat_namespace` bug.
    
    This enables bottling on Monterey.

--- Formula/dovecot.rb
+++ Formula/dovecot.rb
@@ -28,8 +28,22 @@ class Dovecot < Formula
   end
 
   resource "pigeonhole" do
-    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.16.tar.gz"
+    # Syystem curl errors with:
+    # curl: (35) error:1400442E:SSL routines:CONNECT_CR_SRVR_HELLO:tlsv1 alert protocol version
+    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.16.tar.gz", using: :homebrew_curl
     sha256 "5ca36780e23b99e6206440f1b3fe3c6598eda5b699b99cebb15d418ba3c6e938"
+
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    end
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
   end
 
   def install
