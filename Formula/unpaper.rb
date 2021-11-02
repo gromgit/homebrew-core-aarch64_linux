6d248eee7df61e39b57c2d18ae146072937f4e81
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 08:08:44 +0000

    unpaper: update 6.1_7 bottle.

--- Formula/unpaper.rb
+++ Formula/unpaper.rb
@@ -7,11 +7,13 @@ class Unpaper < Formula
   revision 7
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "8aadd07d5712465893b6c3625e7da966c8bfdee572c7ba660cedaa3b0cdff034"
-    sha256 cellar: :any,                 big_sur:       "a9841d58884ee1a0616a2a115c21c593eab613c0e040bf2b21d02ddeec682765"
-    sha256 cellar: :any,                 catalina:      "63a30f9ac771386a0f7d7302c31abe60855b4c4028458cbf9371270f42ee49e6"
-    sha256 cellar: :any,                 mojave:        "15d95668bd014ac329b703502832f020efcdb9011558ab8ba86ee0c8a458046d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5719e8ac973a33236779dfc6f7cf56ee3d93987510d1b6edae9da1d091f162a3"
+    sha256 cellar: :any,                 arm64_monterey: "294f7cece1a54c389382ed61101719710e0ec7c5f26dd679cd9fa9ce68e919f4"
+    sha256 cellar: :any,                 arm64_big_sur:  "8aadd07d5712465893b6c3625e7da966c8bfdee572c7ba660cedaa3b0cdff034"
+    sha256 cellar: :any,                 monterey:       "dca1c952850c1e9f3496cf2facf1b570d50c90fde110d9297360db337c2d4906"
+    sha256 cellar: :any,                 big_sur:        "a9841d58884ee1a0616a2a115c21c593eab613c0e040bf2b21d02ddeec682765"
+    sha256 cellar: :any,                 catalina:       "63a30f9ac771386a0f7d7302c31abe60855b4c4028458cbf9371270f42ee49e6"
+    sha256 cellar: :any,                 mojave:         "15d95668bd014ac329b703502832f020efcdb9011558ab8ba86ee0c8a458046d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5719e8ac973a33236779dfc6f7cf56ee3d93987510d1b6edae9da1d091f162a3"
   end
 
   head do
