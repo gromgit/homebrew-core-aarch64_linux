2be684caf0ed785935518cdfafca19ee2ec916c7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 11:09:51 +0000

    i386-elf-gdb: update 10.2_1 bottle.

--- Formula/i386-elf-gdb.rb
+++ Formula/i386-elf-gdb.rb
@@ -14,12 +14,13 @@ class I386ElfGdb < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "b4c91d248b5ba7d765c277903ac03f1f3d35a77079ae3acbdba8768a9dcb4c55"
-    sha256 monterey:      "21b133542f15adac54a0b2b55e7da2ffd584396b1fcd771864742f3811db784a"
-    sha256 big_sur:       "dbf60ac8e71d01328d134cb1eaa47cd734dd612cd67cc7b730d56afc138ea969"
-    sha256 catalina:      "4ca5521aab0566367e9a72767225d08667efb40e609c0b8e9e4ff7464f755052"
-    sha256 mojave:        "85a64a23e61b011e32cff2c56e7915f32a8d8d669be09e4698b431412e8ea7b0"
-    sha256 x86_64_linux:  "919e6534fce532ad0395fe4fd756004e3ebe25fd653aff1df825455b68643f7c"
+    sha256 arm64_monterey: "5bbec8b43413d033bf75a123959476b515bf13f1a66af58032634aba5d5b2757"
+    sha256 arm64_big_sur:  "b4c91d248b5ba7d765c277903ac03f1f3d35a77079ae3acbdba8768a9dcb4c55"
+    sha256 monterey:       "21b133542f15adac54a0b2b55e7da2ffd584396b1fcd771864742f3811db784a"
+    sha256 big_sur:        "dbf60ac8e71d01328d134cb1eaa47cd734dd612cd67cc7b730d56afc138ea969"
+    sha256 catalina:       "4ca5521aab0566367e9a72767225d08667efb40e609c0b8e9e4ff7464f755052"
+    sha256 mojave:         "85a64a23e61b011e32cff2c56e7915f32a8d8d669be09e4698b431412e8ea7b0"
+    sha256 x86_64_linux:   "919e6534fce532ad0395fe4fd756004e3ebe25fd653aff1df825455b68643f7c"
   end
 
   depends_on "i686-elf-gcc" => :test
