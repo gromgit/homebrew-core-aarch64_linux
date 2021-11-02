7360c2880010d02906c4fbb6ebc181f8254b313d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-02 05:02:13 +0000

    chezmoi 2.7.4
    Closes #88596.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -2,8 +2,8 @@ class Chezmoi < Formula
   desc "Manage your dotfiles across multiple diverse machines, securely"
   homepage "https://chezmoi.io/"
   url "https://github.com/twpayne/chezmoi.git",
-      tag:      "v2.7.3",
-      revision: "32a15ac909ee8108d0eaeebcdbbac2305e32a9ba"
+      tag:      "v2.7.4",
+      revision: "785bd8464fc6fc6503ada0581f03053f3fbdf8b0"
   license "MIT"
   head "https://github.com/twpayne/chezmoi.git", branch: "master"
 
