813581dedfa70b62b00570f746d5017a471451d5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 05:38:17 +0000

    zsxd: update 1.12.2 bottle.

--- Formula/zsxd.rb
+++ Formula/zsxd.rb
@@ -7,10 +7,12 @@ class Zsxd < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:     "ccae47d22a42e29f9d5d37fdd0be7cfa8f451d3b0f42d2db2b933f6a7ae1d129"
-    sha256 cellar: :any_skip_relocation, catalina:    "aabcc393aae8f00a45ffa24d959ff57a6023caace90a815f8107c579e113b87e"
-    sha256 cellar: :any_skip_relocation, mojave:      "8b6e336bd61f16c620ab8323ccd15dfc35cf1665c71799a838c4436fefd561b0"
-    sha256 cellar: :any_skip_relocation, high_sierra: "fa0726547d624647bd7453100b6e2221ce0ec9174e0cd43275844b09aefb6c0d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee42ed381fd488a0e538e30eeff539346f71c0822fcc899907792fc4acc5988a"
+    sha256 cellar: :any_skip_relocation, monterey:       "5641638453928b8d7dc434051961417e5c3e768c06fc35f9879737640027ece3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ccae47d22a42e29f9d5d37fdd0be7cfa8f451d3b0f42d2db2b933f6a7ae1d129"
+    sha256 cellar: :any_skip_relocation, catalina:       "aabcc393aae8f00a45ffa24d959ff57a6023caace90a815f8107c579e113b87e"
+    sha256 cellar: :any_skip_relocation, mojave:         "8b6e336bd61f16c620ab8323ccd15dfc35cf1665c71799a838c4436fefd561b0"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "fa0726547d624647bd7453100b6e2221ce0ec9174e0cd43275844b09aefb6c0d"
   end
 
   depends_on "cmake" => :build
