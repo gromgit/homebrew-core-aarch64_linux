0ea37f04457b1aade67266d172af9976ed08d598
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 13:05:48 +0000

    juju: update 2.9.17 bottle.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -14,10 +14,12 @@ class Juju < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1c4fcfe3e56a616915e64645022f536a5bbf0a7286a4f1e0867ef97877f70f7d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1bb2c98567e9cab8266c584eb31b91dd1a720081b320b80dd2e8bb408e3deb9f"
-    sha256 cellar: :any_skip_relocation, catalina:      "4db9c75f82acc1c4f13d9899c20188d2c9e97d6fe53b36a9854ad9694e6460d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "22c94697bb4875df448e48210b3b358ecddcaa3c3c7ed1cc9b06bd5cf574ebf4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "233ed0721e3235e85d0c54cd2f72f4de533c3bbb0087a53cc598de648e1d291c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c4fcfe3e56a616915e64645022f536a5bbf0a7286a4f1e0867ef97877f70f7d"
+    sha256 cellar: :any_skip_relocation, monterey:       "c5a96d89c3b749d08cc338f513af66b38325380c47efd2d2aa876351bd597442"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1bb2c98567e9cab8266c584eb31b91dd1a720081b320b80dd2e8bb408e3deb9f"
+    sha256 cellar: :any_skip_relocation, catalina:       "4db9c75f82acc1c4f13d9899c20188d2c9e97d6fe53b36a9854ad9694e6460d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "22c94697bb4875df448e48210b3b358ecddcaa3c3c7ed1cc9b06bd5cf574ebf4"
   end
 
   depends_on "go" => :build
