f7fedc0947a13cc4cda290fbe890b2b0ae85248d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 13:07:47 +0000

    datree: update 0.14.29 bottle.

--- Formula/datree.rb
+++ Formula/datree.rb
@@ -12,10 +12,12 @@ class Datree < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "285b980a9075a6a5f9fb21836d8f685a59efdc0eccc3c30ffd1104421633c801"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f85d63bc21fce5af966eec2d52feae7fc1a21c5aa7514010bf3f522386f412d2"
-    sha256 cellar: :any_skip_relocation, catalina:      "085fc00d3a958d91c2302b06ad2fcf813e243a90ec00200d9870dad81253f9ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "266f81a2eef2a1b803ab6c74e76db98925145f7b80a267671698a38830f61a97"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "74bd3ca79b0521208cabb778ec256bd85de7080ac458ccb07528bdbaba239d46"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "285b980a9075a6a5f9fb21836d8f685a59efdc0eccc3c30ffd1104421633c801"
+    sha256 cellar: :any_skip_relocation, monterey:       "881b246100bd7a926e88c8b4cb53fbf1b1062433956315cd72705ce707dd15a2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f85d63bc21fce5af966eec2d52feae7fc1a21c5aa7514010bf3f522386f412d2"
+    sha256 cellar: :any_skip_relocation, catalina:       "085fc00d3a958d91c2302b06ad2fcf813e243a90ec00200d9870dad81253f9ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "266f81a2eef2a1b803ab6c74e76db98925145f7b80a267671698a38830f61a97"
   end
 
   depends_on "go" => :build
