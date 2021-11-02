0fea88f8b0d86279044c6fe46b1239d970534373
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 16:10:44 +0000

    profanity: update 0.11.1 bottle.

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -6,11 +6,13 @@ class Profanity < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 monterey:     "87106dc7e112e37b7307f6a3ff7ca4565c0e269a37c0486b5778648614907e19"
-    sha256 big_sur:      "01f299b832782d3ed87ad78fc432c1a20799b3804140c017671ddde7c5b4fb42"
-    sha256 catalina:     "1096a2f8ed2d6bedc3dd67f6247dee1a35ceb17c6af052161c026865fb318c4d"
-    sha256 mojave:       "3187f12ce661fbf90021b11e313d9a98b5b65da311d04807283f0722539e4f7b"
-    sha256 x86_64_linux: "1e0abc2724db615d5997766a64fd0b22be03b25fc1e47c4fd07dd509633024ee"
+    sha256 arm64_monterey: "f2f1940db34f5f4dbd8e98417e591ae1a3afcaa5270a7993c09a2278cc5dbd88"
+    sha256 arm64_big_sur:  "f25f8e773bc05b798776a12b213bcf96edd7eb59d5b30e933ac1231e6a89fe7f"
+    sha256 monterey:       "87106dc7e112e37b7307f6a3ff7ca4565c0e269a37c0486b5778648614907e19"
+    sha256 big_sur:        "01f299b832782d3ed87ad78fc432c1a20799b3804140c017671ddde7c5b4fb42"
+    sha256 catalina:       "1096a2f8ed2d6bedc3dd67f6247dee1a35ceb17c6af052161c026865fb318c4d"
+    sha256 mojave:         "3187f12ce661fbf90021b11e313d9a98b5b65da311d04807283f0722539e4f7b"
+    sha256 x86_64_linux:   "1e0abc2724db615d5997766a64fd0b22be03b25fc1e47c4fd07dd509633024ee"
   end
 
   head do
