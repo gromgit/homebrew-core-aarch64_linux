c39b201f2f04efbd5101c8a20db33b2faf2404fe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 10:51:49 +0000

    gdbgui: update 0.15.0.1 bottle.

--- Formula/gdbgui.rb
+++ Formula/gdbgui.rb
@@ -8,6 +8,7 @@ class Gdbgui < Formula
   license "GPL-3.0-only"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:     "66ab0fe289baa0e06dca98ce2c3fbb6179093da983faab5119967ca33554e372"
     sha256 cellar: :any_skip_relocation, big_sur:      "62870c658d7305914e55b06977f1bdd3599cdb5678b0a9b92cb8998f561bddea"
     sha256 cellar: :any_skip_relocation, catalina:     "2f3943c95fd3a2345b6e0da211d4aaaa701dd295ff73c5b0e6ca576e357acc34"
     sha256 cellar: :any_skip_relocation, mojave:       "419065456561b0783e97f2a556d937b9d171b2869db0a34a5c2216363aee2b48"
