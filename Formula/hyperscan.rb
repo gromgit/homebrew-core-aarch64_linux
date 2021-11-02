32ce29ef6ed4696e2082b8da7b0334dc934d8f95
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 05:58:43 +0000

    hyperscan: update 5.4.0 bottle.

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -6,6 +6,7 @@ class Hyperscan < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 cellar: :any,                 monterey:     "d042b40534ac02d2cc6ad57621802787340c7b76ee2bb7abfed79442b2f61d7e"
     sha256 cellar: :any,                 big_sur:      "1e75b4699ac1040d24cbe81ddae60149be7179e09f450840bdcbe5fd0e4582dc"
     sha256 cellar: :any,                 catalina:     "2c5afe9775aad01d1bfb577cb80218bdf241c48d5b567ad85fe6bba68241c8d3"
     sha256 cellar: :any,                 mojave:       "0564db4adcb7022d1691f482d12fdf3a2c0ea71079a749c90f2233340aebb98e"
