fbeacc4917db945e75790d08fe05d772a30f0276
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-02 22:08:40 +0000

    poppler-qt5: update 21.11.0 bottle.

--- Formula/poppler-qt5.rb
+++ Formula/poppler-qt5.rb
@@ -11,11 +11,10 @@ class PopplerQt5 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "b04bd910e016cf419a9f4cd26426225194819043fbb03287ce05b5fe1e5738f5"
-    sha256 big_sur:       "9c3edf61ef29b2908c8e476c67163a6c0c8b689d834d93323431a32a86b44026"
-    sha256 catalina:      "dd442783223a090fc558d6515b8dac8d4fac6a522c1c9878a827e28638a40ea6"
-    sha256 mojave:        "0b3aa91d6df18c74425f17f4f0d194e91b62dacccd42e7caadb9e6342a77bca4"
-    sha256 x86_64_linux:  "806a60be7dbaeca537e075f4eb5f2eeb62641c2a0ebaf0a401bdb57d33308c39"
+    sha256 arm64_big_sur: "f18ed9acb1380c09a482af222e26e3bfd6cef24bb50635d602e20338586d423b"
+    sha256 big_sur:       "10d7eaa9579959aaff146a82c780d0dea4e7a966592760b0bc9c8cb1fc8e5244"
+    sha256 catalina:      "56f321088f630328174561c41f64aadf5bd3b560851bbded7db905d17b82205f"
+    sha256 x86_64_linux:  "b60f0d6999b4527e67ac0b989aea8c89c80e7576f0648ea2776684a92fc3ed14"
   end
 
   keg_only "it conflicts with poppler"
