139ae312dba656d760af5377d9617f21759ee099
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 12:53:23 +0000

    ory-hydra: update 1.10.7 bottle.

--- Formula/ory-hydra.rb
+++ Formula/ory-hydra.rb
@@ -12,10 +12,12 @@ class OryHydra < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0af8b637ccc81a1f775e2f38c3e1dc1d60a08c64b916ac02d26fe6c1472769af"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9e3fc8e30be3ec3ce32373848d02350b73fe33cc12d01b88aada7a4292e3dc25"
-    sha256 cellar: :any_skip_relocation, catalina:      "497d6dda3cb26b26da700bc6887b0edb315a93f6583a98a0b84c63dd19e401a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bc854e8748ed1fafca5c9d433a6a53e3fca5a892ad76798aa6f92a1f927836c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "015935d382ef65596a96f0d36527ba6ce51576b4caf8ac3000f77d740c376fc6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0af8b637ccc81a1f775e2f38c3e1dc1d60a08c64b916ac02d26fe6c1472769af"
+    sha256 cellar: :any_skip_relocation, monterey:       "129ed0044c86ccece0985165ec1158cdb5b7fcdfd84e6e25a9b3c12132c042d4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9e3fc8e30be3ec3ce32373848d02350b73fe33cc12d01b88aada7a4292e3dc25"
+    sha256 cellar: :any_skip_relocation, catalina:       "497d6dda3cb26b26da700bc6887b0edb315a93f6583a98a0b84c63dd19e401a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0bc854e8748ed1fafca5c9d433a6a53e3fca5a892ad76798aa6f92a1f927836c"
   end
 
   depends_on "go" => :build
