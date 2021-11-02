58b5670c01d804f3284b9b20053c3bb0d85a1c55
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 14:04:40 +0000

    gdbm: update 1.22 bottle.

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -7,10 +7,12 @@ class Gdbm < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ee9c72472b2e910435fcd2410c299cf784471132decba1e6945a68e29b0c5ddf"
-    sha256 cellar: :any, big_sur:       "7e9737ec99942ede2bb0c522f0e0c4f7c22a31aa94afa9fbab9c8bc81d2ea9d0"
-    sha256 cellar: :any, catalina:      "f7e29005a4a9232965f67ef89364193e3ab95b249b169164be10db9e56f7f22e"
-    sha256               x86_64_linux:  "5942379d8543bf2780fc6ac1ddd96ea5eff267ce1a4af541c85b70efb3b2721c"
+    sha256 cellar: :any, arm64_monterey: "25cfaedb0fd8f973835f30754e0838747b01da48071d6cbb9c94add907287fea"
+    sha256 cellar: :any, arm64_big_sur:  "ee9c72472b2e910435fcd2410c299cf784471132decba1e6945a68e29b0c5ddf"
+    sha256 cellar: :any, monterey:       "b5f3343eef068c75c152fa0c2a4e8fdec033dc90f072f686bcff0298c6c4857b"
+    sha256 cellar: :any, big_sur:        "7e9737ec99942ede2bb0c522f0e0c4f7c22a31aa94afa9fbab9c8bc81d2ea9d0"
+    sha256 cellar: :any, catalina:       "f7e29005a4a9232965f67ef89364193e3ab95b249b169164be10db9e56f7f22e"
+    sha256               x86_64_linux:   "5942379d8543bf2780fc6ac1ddd96ea5eff267ce1a4af541c85b70efb3b2721c"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
