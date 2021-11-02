30abf3cbcb4dc80c73fb9c1d00da95a860e7859e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-02 05:38:23 +0000

    zelda-roth-se: update 1.2.1 bottle.

--- Formula/zelda-roth-se.rb
+++ Formula/zelda-roth-se.rb
@@ -7,10 +7,12 @@ class ZeldaRothSe < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:     "28b1bd5308092389db177a9b277a29f1da892c1a4a71dd9b12e483a045e52808"
-    sha256 cellar: :any_skip_relocation, catalina:    "1531cd6fc89cca4cc08287e569cdd8b86e41a52bb8c66fb10f6a74bb5006bc24"
-    sha256 cellar: :any_skip_relocation, mojave:      "b0451d1eb512280f9dcb2c6057188cbe02e9b2c71fbf337ac463a4e284ba1987"
-    sha256 cellar: :any_skip_relocation, high_sierra: "dcf7800dd6c2e8798abb867733a79acda20e3ce7745b7d489eeac3050a7bf829"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f1f9b1fadb798f87d38698f31a0bc8cfb36c21c272087a25fc50256de704d379"
+    sha256 cellar: :any_skip_relocation, monterey:       "bd2099609e574fa6b5703c40455f7d7187eda55f938d7418bf4eb2dd68a1dd66"
+    sha256 cellar: :any_skip_relocation, big_sur:        "28b1bd5308092389db177a9b277a29f1da892c1a4a71dd9b12e483a045e52808"
+    sha256 cellar: :any_skip_relocation, catalina:       "1531cd6fc89cca4cc08287e569cdd8b86e41a52bb8c66fb10f6a74bb5006bc24"
+    sha256 cellar: :any_skip_relocation, mojave:         "b0451d1eb512280f9dcb2c6057188cbe02e9b2c71fbf337ac463a4e284ba1987"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "dcf7800dd6c2e8798abb867733a79acda20e3ce7745b7d489eeac3050a7bf829"
   end
 
   depends_on "cmake" => :build
