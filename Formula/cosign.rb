885c9c186bfffaabd7b0cd7ebdb4beecc7524446
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 10:00:30 +0000

    cosign: update 1.3.0 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -8,10 +8,12 @@ class Cosign < Formula
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1d2fc8933b09095683fad9c6ccf3fe30b42c174fb20379ec233ab3e71c97095a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3aa02b3c736440f849a1fbfa2f41f5008815f0ca65b03b356eebc58d69fd2924"
-    sha256 cellar: :any_skip_relocation, catalina:      "bda996e03a78e48877a24ec67a7ee1f2c0bd6bacd1e594ec4bae89363a566993"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32d9d5c810fd20e6520a5cb05c78194410c0a31b35e15fef2fdf9bba84ae598e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a294732dd772988a02771cecf8d6c2796af18abe93670b9ac844c9f8e94a408e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d2fc8933b09095683fad9c6ccf3fe30b42c174fb20379ec233ab3e71c97095a"
+    sha256 cellar: :any_skip_relocation, monterey:       "1a2f5792632bb82870bb0900e9cbd33bbb40366492740b33966a82971d7e516c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3aa02b3c736440f849a1fbfa2f41f5008815f0ca65b03b356eebc58d69fd2924"
+    sha256 cellar: :any_skip_relocation, catalina:       "bda996e03a78e48877a24ec67a7ee1f2c0bd6bacd1e594ec4bae89363a566993"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32d9d5c810fd20e6520a5cb05c78194410c0a31b35e15fef2fdf9bba84ae598e"
   end
 
   depends_on "go" => :build
