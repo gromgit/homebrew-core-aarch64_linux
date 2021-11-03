7f3e45605b888aa4e021b78955ff2f3049cc4bbe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 18:55:13 +0000

    template-glib: update 3.34.0_2 bottle.

--- Formula/template-glib.rb
+++ Formula/template-glib.rb
@@ -6,11 +6,13 @@ class TemplateGlib < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2340864768d827293d86853d8d368d78ed1bb82b87311c3cd3e7c5b315bb3e47"
-    sha256 cellar: :any, big_sur:       "4ebcd5e2a6aa072fadc6f4ce54e78aba96cac605fac1ba312bb9d798add9d60d"
-    sha256 cellar: :any, catalina:      "9076cc6161b090edf56b7ffdb0dcb31f3590c5b359b3e74fb78c1c0119b2c256"
-    sha256 cellar: :any, mojave:        "b5cbd61d31bcf899a1940b0e0c00b2a788a6dc1316d90847a0668973525a6048"
-    sha256 cellar: :any, high_sierra:   "4e0560a1eb5ac91fdd4ea3dc89086f0b50cc65d68c32c3c8bb4fa49e0d05454d"
+    sha256 cellar: :any, arm64_monterey: "08a46c016c0262bb1c044085e2259f7506e7df1dba0e8eab51561756de1cfe49"
+    sha256 cellar: :any, arm64_big_sur:  "2340864768d827293d86853d8d368d78ed1bb82b87311c3cd3e7c5b315bb3e47"
+    sha256 cellar: :any, monterey:       "fda2867fd1220b9bbd3a96d5665508c4c1416ee922f9bc8909cb0128f59149e2"
+    sha256 cellar: :any, big_sur:        "4ebcd5e2a6aa072fadc6f4ce54e78aba96cac605fac1ba312bb9d798add9d60d"
+    sha256 cellar: :any, catalina:       "9076cc6161b090edf56b7ffdb0dcb31f3590c5b359b3e74fb78c1c0119b2c256"
+    sha256 cellar: :any, mojave:         "b5cbd61d31bcf899a1940b0e0c00b2a788a6dc1316d90847a0668973525a6048"
+    sha256 cellar: :any, high_sierra:    "4e0560a1eb5ac91fdd4ea3dc89086f0b50cc65d68c32c3c8bb4fa49e0d05454d"
   end
 
   depends_on "bison" => :build # does not appear to work with system bison
