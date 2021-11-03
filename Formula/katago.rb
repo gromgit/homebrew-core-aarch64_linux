36445c2a7b469197d8f194f8fce530106abc82eb
Adrian Petrescu <adrian@apetre.sc>
2021-11-03 12:45:02 -0400

    katago 1.10.0
    Closes #88716.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/katago.rb
+++ Formula/katago.rb
@@ -1,8 +1,8 @@
 class Katago < Formula
   desc "Neural Network Go engine with no human-provided knowledge"
   homepage "https://github.com/lightvector/KataGo"
-  url "https://github.com/lightvector/KataGo/archive/v1.9.1.tar.gz"
-  sha256 "4992ddca600d19630692b8fd468187d872274493d2db2eed5576b7b550895cda"
+  url "https://github.com/lightvector/KataGo/archive/v1.10.0.tar.gz"
+  sha256 "2d9b3fced61df90953dfc92f689ceb3fefba65b816388fe42bddad5c0d68bda5"
   license "MIT"
 
   livecheck do
