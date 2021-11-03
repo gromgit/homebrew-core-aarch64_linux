b8f8acae462313da4fa4cf6a0ea945720c1b856f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 19:01:33 +0000

    prefixsuffix: update 0.6.9_9 bottle.

--- Formula/prefixsuffix.rb
+++ Formula/prefixsuffix.rb
@@ -7,10 +7,12 @@ class Prefixsuffix < Formula
   revision 9
 
   bottle do
-    sha256 arm64_big_sur: "03a102fc5cbf319029a6b536b60caf5db40396b25e1d6de1eda1baf297cb0d81"
-    sha256 big_sur:       "4300af03702b1873b307890bfb3bf9e08da08e43c401c958a2dcf796d56a34e8"
-    sha256 catalina:      "8384f85dfc725d9a754030a2f94320124845b3d63968a2a3348a918afd096415"
-    sha256 mojave:        "3e25c1930c085b61c073b5015895db250ec1113b3102384ebf84c1f8d0a65731"
+    sha256 arm64_monterey: "f4a3934702cc68077a096d0baca50a4baf033309cec38b2225b144a4bd825b45"
+    sha256 arm64_big_sur:  "03a102fc5cbf319029a6b536b60caf5db40396b25e1d6de1eda1baf297cb0d81"
+    sha256 monterey:       "941432357b54d13c0b0333c120b72ec0ce467230a05de210a0a4837e3a898e31"
+    sha256 big_sur:        "4300af03702b1873b307890bfb3bf9e08da08e43c401c958a2dcf796d56a34e8"
+    sha256 catalina:       "8384f85dfc725d9a754030a2f94320124845b3d63968a2a3348a918afd096415"
+    sha256 mojave:         "3e25c1930c085b61c073b5015895db250ec1113b3102384ebf84c1f8d0a65731"
   end
 
   depends_on "intltool" => :build
