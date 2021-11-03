fd2cb26332c7462f5fe20a315f8b25e4d5321e70
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 19:07:58 +0000

    node@10: update 10.24.1_1 bottle.

--- Formula/node@10.rb
+++ Formula/node@10.rb
@@ -7,6 +7,7 @@ class NodeAT10 < Formula
   revision 1
 
   bottle do
+    sha256 cellar: :any, monterey: "5714defcf3505e22bc5af68f26872b34b45c955db2d4a1701b080378c6b56166"
     sha256 cellar: :any, big_sur:  "84095e53ee88c7acc39a574a64e7832bbb7326be76a4de502ed55fdd47e784c4"
     sha256 cellar: :any, catalina: "be200f225fd88d1a4a1efba2470b305accbddff927bf7d5287eeb4777fb02b9c"
     sha256 cellar: :any, mojave:   "cdaf548a304fb2eda428a61c8b25c406e2ada5f9eb7cf5b56174d0753f153598"
