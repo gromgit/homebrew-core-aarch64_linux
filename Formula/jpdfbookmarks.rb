7aac275d15d8ff29449dda899202c6b0d988d01a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:41:14 +0000

    jpdfbookmarks: update 2.5.2 bottle.

--- Formula/jpdfbookmarks.rb
+++ Formula/jpdfbookmarks.rb
@@ -6,11 +6,13 @@ class Jpdfbookmarks < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91470e2dbd04216a27d182e0645399b3460744315b564f2a6ae5b86d76da2b14"
-    sha256 cellar: :any_skip_relocation, big_sur:       "849a02893bc8fea3cd3813695f7c6d30598e53dccb4bba7933d745d221258a24"
-    sha256 cellar: :any_skip_relocation, catalina:      "849a02893bc8fea3cd3813695f7c6d30598e53dccb4bba7933d745d221258a24"
-    sha256 cellar: :any_skip_relocation, mojave:        "849a02893bc8fea3cd3813695f7c6d30598e53dccb4bba7933d745d221258a24"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a55444d161c8b6a7186b252fd79e2f63831bca8f54e4a0d4633c76988cce96c6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "13bd17ff83235a1e154915b59a521ffe910ecb2ca4ae837203441813b32eb176"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91470e2dbd04216a27d182e0645399b3460744315b564f2a6ae5b86d76da2b14"
+    sha256 cellar: :any_skip_relocation, monterey:       "f059129d0e91d4b1791c25c19bf52e20ba971b34167af63993e3f4ae9667ae50"
+    sha256 cellar: :any_skip_relocation, big_sur:        "849a02893bc8fea3cd3813695f7c6d30598e53dccb4bba7933d745d221258a24"
+    sha256 cellar: :any_skip_relocation, catalina:       "849a02893bc8fea3cd3813695f7c6d30598e53dccb4bba7933d745d221258a24"
+    sha256 cellar: :any_skip_relocation, mojave:         "849a02893bc8fea3cd3813695f7c6d30598e53dccb4bba7933d745d221258a24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a55444d161c8b6a7186b252fd79e2f63831bca8f54e4a0d4633c76988cce96c6"
   end
 
   depends_on "openjdk"
