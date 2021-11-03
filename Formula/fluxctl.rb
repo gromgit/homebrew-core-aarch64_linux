ca0a36872a5f9ebea76cc7beef0593a8238fff77
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 03:40:19 +0000

    fluxctl: update 1.24.2 bottle.

--- Formula/fluxctl.rb
+++ Formula/fluxctl.rb
@@ -12,10 +12,12 @@ class Fluxctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5749efbc7c188da67c9bb7f04495fdfb733f549a62539089317bd67b68cd3790"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3279afcbddbbc684c94215618215a848908bff67f76fe6d2aa1ec1b9d90228af"
-    sha256 cellar: :any_skip_relocation, catalina:      "fa429bf0f82793675f7b18b1a7d3f1d9363aa57b5374dca62edd6d8d035beb5e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "647746b9ed6d6b2d2e2a25aeef92d95d0bfcd0c21ee07e8f8f22d4277c137734"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4367099ca4ebba8542d686d6415c886e8869d39de7d24e191b8c7bf305355a8e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5749efbc7c188da67c9bb7f04495fdfb733f549a62539089317bd67b68cd3790"
+    sha256 cellar: :any_skip_relocation, monterey:       "a939665a2dea5b14bb7894146fb9c1528a1fd1013d5fc397441d967f3ab9d425"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3279afcbddbbc684c94215618215a848908bff67f76fe6d2aa1ec1b9d90228af"
+    sha256 cellar: :any_skip_relocation, catalina:       "fa429bf0f82793675f7b18b1a7d3f1d9363aa57b5374dca62edd6d8d035beb5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "647746b9ed6d6b2d2e2a25aeef92d95d0bfcd0c21ee07e8f8f22d4277c137734"
   end
 
   depends_on "go" => :build
