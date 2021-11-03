f5a928de9f87cf0b182341bb05d95ee8e049ce6b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 14:06:03 +0000

    qxmpp: update 1.4.0 bottle.

--- Formula/qxmpp.rb
+++ Formula/qxmpp.rb
@@ -6,10 +6,11 @@ class Qxmpp < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6399d05b012c2b52345d45c1b4cce465e36fdefd3a1a8adf44f19626feacaffa"
-    sha256 cellar: :any, big_sur:       "97d09f017f500de731726453d7e7780087beb963685745b0fd92c30134fdbdfc"
-    sha256 cellar: :any, catalina:      "62795c2f2b6a5c39f5bcfbfeac06a5fa48a289962dce4abec095dc937f237470"
-    sha256 cellar: :any, mojave:        "a30dcbea5faf9369d20c51e0c5d8c743d8f6ef3d3f1527308705041d58138e77"
+    sha256 cellar: :any, arm64_monterey: "c6e0b95b7721e98dc3a507539394fa2f083aed77193b48ed412c690f4e1d1627"
+    sha256 cellar: :any, arm64_big_sur:  "6399d05b012c2b52345d45c1b4cce465e36fdefd3a1a8adf44f19626feacaffa"
+    sha256 cellar: :any, big_sur:        "97d09f017f500de731726453d7e7780087beb963685745b0fd92c30134fdbdfc"
+    sha256 cellar: :any, catalina:       "62795c2f2b6a5c39f5bcfbfeac06a5fa48a289962dce4abec095dc937f237470"
+    sha256 cellar: :any, mojave:         "a30dcbea5faf9369d20c51e0c5d8c743d8f6ef3d3f1527308705041d58138e77"
   end
 
   depends_on "cmake" => :build
