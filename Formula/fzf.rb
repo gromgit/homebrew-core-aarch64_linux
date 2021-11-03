45608de94c9416a5d6b219fd13f3bc2430cf557c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 19:17:09 +0000

    fzf: update 0.28.0 bottle.

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -7,10 +7,12 @@ class Fzf < Formula
   head "https://github.com/junegunn/fzf.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f332e7c67b385d437ec7c7e89f70fb3df2241110a78e9a431fba6316d925a428"
-    sha256 cellar: :any_skip_relocation, big_sur:       "04b9060dad2714767d95e19a1d3d7fdd0afe0f35643f5e72936002010060be74"
-    sha256 cellar: :any_skip_relocation, catalina:      "04b9060dad2714767d95e19a1d3d7fdd0afe0f35643f5e72936002010060be74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "055d03f36d01613b6b46fb1dfd5c8ab845639afb6c564388baf2fa8363838281"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f332e7c67b385d437ec7c7e89f70fb3df2241110a78e9a431fba6316d925a428"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f332e7c67b385d437ec7c7e89f70fb3df2241110a78e9a431fba6316d925a428"
+    sha256 cellar: :any_skip_relocation, monterey:       "04b9060dad2714767d95e19a1d3d7fdd0afe0f35643f5e72936002010060be74"
+    sha256 cellar: :any_skip_relocation, big_sur:        "04b9060dad2714767d95e19a1d3d7fdd0afe0f35643f5e72936002010060be74"
+    sha256 cellar: :any_skip_relocation, catalina:       "04b9060dad2714767d95e19a1d3d7fdd0afe0f35643f5e72936002010060be74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "055d03f36d01613b6b46fb1dfd5c8ab845639afb6c564388baf2fa8363838281"
   end
 
   depends_on "go" => :build
