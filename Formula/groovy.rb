6bc0a35b3363f778a8b52597cdb01a998ef8e418
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:03:12 +0000

    groovy: update 3.0.9 bottle.

--- Formula/groovy.rb
+++ Formula/groovy.rb
@@ -16,6 +16,7 @@ class Groovy < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "edd57e1b12bd69b5b2717649168c95a7d14daef492bb6342d2ce26f54d5e0393"
     sha256 cellar: :any_skip_relocation, mojave:        "edd57e1b12bd69b5b2717649168c95a7d14daef492bb6342d2ce26f54d5e0393"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "dfe39b4c967fad083cd7c6a5f74fdeb5e2104d0322760022e739d7e55f1020aa"
+    sha256 cellar: :any_skip_relocation, all:           "72b30ad60e355ecfb5ae271ed8afcb317dd5387cbbc2110ce212ac4c7e210bc6"
   end
 
   depends_on "openjdk"
