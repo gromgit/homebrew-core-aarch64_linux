4d542e99e75a64d2969406634461e3059431a00c
Rui Chen <rui@chenrui.dev>
2021-11-03 02:02:04 -0400

    halide 13.0.0
    * halide 13.0.0
    * halide: switch to LLVM 13, fix test
    * halide: fix test (again)
      The test requires C++17.
    
    Closes #88691.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/halide.rb
+++ Formula/halide.rb
@@ -1,10 +1,9 @@
 class Halide < Formula
   desc "Language for fast, portable data-parallel computation"
   homepage "https://halide-lang.org"
-  url "https://github.com/halide/Halide/archive/v12.0.1.tar.gz"
-  sha256 "17f7a470c3fcf77205fdcd9d06257f17c1c1a3cda4b8023f56cec160e80bd519"
+  url "https://github.com/halide/Halide/archive/v13.0.0.tar.gz"
+  sha256 "5ab7d5d9bc04ab0902a88751e535ff231ab9da1911e46f272a52cf41131609f3"
   license "MIT"
-  revision 1
 
   livecheck do
     url :stable
@@ -23,7 +22,7 @@ class Halide < Formula
   depends_on "cmake" => :build
   depends_on "jpeg"
   depends_on "libpng"
-  depends_on "llvm@12"
+  depends_on "llvm"
   depends_on "python@3.9"
 
   def install
@@ -35,8 +34,8 @@ class Halide < Formula
   end
 
   test do
-    cp share/"tutorial/lesson_01_basics.cpp", testpath
-    system ENV.cxx, "-std=c++11", "lesson_01_basics.cpp", "-L#{lib}", "-lHalide", "-o", "test"
+    cp share/"doc/Halide/tutorial/lesson_01_basics.cpp", testpath
+    system ENV.cxx, "-std=c++17", "lesson_01_basics.cpp", "-L#{lib}", "-lHalide", "-o", "test"
     assert_match "Success!", shell_output("./test")
   end
 end
