2893b2759769c693f032637db892a4dbff949312
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:12:15 +0000

    groovysdk: update 3.0.9 bottle.

--- Formula/groovysdk.rb
+++ Formula/groovysdk.rb
@@ -11,11 +11,13 @@ class Groovysdk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a3103793592e4d8c6392f5bf218e91698ffd2ad53a99002963ca4b8dd3c03fa7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
-    sha256 cellar: :any_skip_relocation, catalina:      "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
-    sha256 cellar: :any_skip_relocation, mojave:        "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3103793592e4d8c6392f5bf218e91698ffd2ad53a99002963ca4b8dd3c03fa7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3103793592e4d8c6392f5bf218e91698ffd2ad53a99002963ca4b8dd3c03fa7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a3103793592e4d8c6392f5bf218e91698ffd2ad53a99002963ca4b8dd3c03fa7"
+    sha256 cellar: :any_skip_relocation, monterey:       "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
+    sha256 cellar: :any_skip_relocation, catalina:       "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
+    sha256 cellar: :any_skip_relocation, mojave:         "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a3103793592e4d8c6392f5bf218e91698ffd2ad53a99002963ca4b8dd3c03fa7"
   end
 
   depends_on "openjdk"
