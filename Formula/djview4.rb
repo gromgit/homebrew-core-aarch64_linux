178574adac4507ac02bc9c53410b16f599e370bb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 14:07:35 +0000

    djview4: update 4.12_1 bottle.

--- Formula/djview4.rb
+++ Formula/djview4.rb
@@ -13,9 +13,10 @@ class Djview4 < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "d732c90fdab920090c28baf8951d50da4523fc619ce22643819afbf1037e21fb"
-    sha256 cellar: :any, big_sur:       "4e9a79b7d43536f816768e9dd5d5452b5f3f270772a482f5321f7e43712a0a30"
-    sha256 cellar: :any, catalina:      "d55757a01f3e6e843427f018559cd3e881c230096b16238b2a8f5bd82379f2a0"
+    sha256 cellar: :any, arm64_monterey: "2495aff481ce3d1dc1fd6df41669068388956fe89ecd6302a7ed75f4feccc8e8"
+    sha256 cellar: :any, arm64_big_sur:  "d732c90fdab920090c28baf8951d50da4523fc619ce22643819afbf1037e21fb"
+    sha256 cellar: :any, big_sur:        "4e9a79b7d43536f816768e9dd5d5452b5f3f270772a482f5321f7e43712a0a30"
+    sha256 cellar: :any, catalina:       "d55757a01f3e6e843427f018559cd3e881c230096b16238b2a8f5bd82379f2a0"
   end
 
   depends_on "autoconf" => :build
