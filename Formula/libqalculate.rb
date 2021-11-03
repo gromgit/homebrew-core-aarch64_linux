05c728dd20c0b8e9cafc846ef4f2c9fc9798254c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 18:00:15 +0000

    libqalculate: update 3.21.0 bottle.

--- Formula/libqalculate.rb
+++ Formula/libqalculate.rb
@@ -6,10 +6,11 @@ class Libqalculate < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "c5fa566c1bfcee52df79e2b352c851d06e35a6d62cb5e173f468d1db00196236"
-    sha256 big_sur:       "3691489924b0d10808f7b28f2a9a5144e1695195fe9af5fb18cc6cf34aa9902e"
-    sha256 catalina:      "8fe96ee96986175d97ae18b9e10ed75712f28f7a43167a120d1e9fd5eb42e598"
-    sha256 mojave:        "1c11025d7abd80744bc4077822571c77db47d6999e0ad7c5957ec1a87272bb5c"
+    sha256 arm64_monterey: "474bd715cfef115d357e6c5b42937ac31ec6d610d726ed433a166fbe5df27b46"
+    sha256 arm64_big_sur:  "c5fa566c1bfcee52df79e2b352c851d06e35a6d62cb5e173f468d1db00196236"
+    sha256 big_sur:        "3691489924b0d10808f7b28f2a9a5144e1695195fe9af5fb18cc6cf34aa9902e"
+    sha256 catalina:       "8fe96ee96986175d97ae18b9e10ed75712f28f7a43167a120d1e9fd5eb42e598"
+    sha256 mojave:         "1c11025d7abd80744bc4077822571c77db47d6999e0ad7c5957ec1a87272bb5c"
   end
 
   depends_on "intltool" => :build
