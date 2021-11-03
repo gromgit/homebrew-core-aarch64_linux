5bfb17a0d30c30aef46c738b9799ff2b3bcbdc00
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 13:38:15 +0000

    qjson: update 0.9.0_2 bottle.

--- Formula/qjson.rb
+++ Formula/qjson.rb
@@ -7,10 +7,11 @@ class Qjson < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4f0e62ecfb61f24dcc04fac054e6c1c2caf56fe674f4f548bcaa16d5dd1c3d3f"
-    sha256 cellar: :any, big_sur:       "37704aff31b79e0ebe73592149836706f579a2a3dd5231aa4e992647eff07ad5"
-    sha256 cellar: :any, catalina:      "b262d77517e48cdd798e237e787c2058d4fad0acf71457c82c17113635898fed"
-    sha256 cellar: :any, mojave:        "c7b92230e40e860996163997a092575b6317c631221db79cd1031aec61c13b2c"
+    sha256 cellar: :any, arm64_monterey: "50e66e18c82d7808bc50db9a48d9c45bf1fe44cc1089c518d72ac07f41c1e37d"
+    sha256 cellar: :any, arm64_big_sur:  "4f0e62ecfb61f24dcc04fac054e6c1c2caf56fe674f4f548bcaa16d5dd1c3d3f"
+    sha256 cellar: :any, big_sur:        "37704aff31b79e0ebe73592149836706f579a2a3dd5231aa4e992647eff07ad5"
+    sha256 cellar: :any, catalina:       "b262d77517e48cdd798e237e787c2058d4fad0acf71457c82c17113635898fed"
+    sha256 cellar: :any, mojave:         "c7b92230e40e860996163997a092575b6317c631221db79cd1031aec61c13b2c"
   end
 
   depends_on "cmake" => :build
