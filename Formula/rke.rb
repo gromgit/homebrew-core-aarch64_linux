e646f05296400f0b8c307677d30d9bd195a97c94
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 08:31:23 +0000

    rke: update 1.3.2 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -6,10 +6,12 @@ class Rke < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "04b2ae6f339c8975a2cf9ecb87f669ec7e16cd24e9ad9d5621f3879032cef86f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f26b2441ed4e6d86cf7a58d5ba81cb1b4143105714a4408c28195e6ef618a6d4"
-    sha256 cellar: :any_skip_relocation, catalina:      "1e10f4f4ea04cb9f6dedb6d7a12c53d165b8d281b4a5ad77b8a65157963d436a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f456c0eb40b4af83fcd688ce975e6c28d6057e6e178e9e39afdae362fac8f558"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d21e9bd629ea287cc1feb07a29b4c061549fd32fe88c47ff9c4e95fae7d901dc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "04b2ae6f339c8975a2cf9ecb87f669ec7e16cd24e9ad9d5621f3879032cef86f"
+    sha256 cellar: :any_skip_relocation, monterey:       "ef054c17f29bf1e95a322095763c510e374d4a532515255a860040d94bcdaa26"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f26b2441ed4e6d86cf7a58d5ba81cb1b4143105714a4408c28195e6ef618a6d4"
+    sha256 cellar: :any_skip_relocation, catalina:       "1e10f4f4ea04cb9f6dedb6d7a12c53d165b8d281b4a5ad77b8a65157963d436a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f456c0eb40b4af83fcd688ce975e6c28d6057e6e178e9e39afdae362fac8f558"
   end
 
   depends_on "go" => :build
