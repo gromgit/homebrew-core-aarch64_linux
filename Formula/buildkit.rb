105e4d94dcad87000088b09cdb63d952c0b4e32f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-03 05:02:20 +0000

    buildkit 0.9.2
    Closes #88676.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -2,8 +2,8 @@ class Buildkit < Formula
   desc "Сoncurrent, cache-efficient, and Dockerfile-agnostic builder toolkit"
   homepage "https://github.com/moby/buildkit"
   url "https://github.com/moby/buildkit.git",
-      tag:      "v0.9.1",
-      revision: "966bcf4aa3ef397a9d6025fade18f2d59a5bf29d"
+      tag:      "v0.9.2",
+      revision: "a14b4e097ae1dc7514c5febd6d75f742a166ea75"
   license "Apache-2.0"
   head "https://github.com/moby/buildkit.git", branch: "master"
 
