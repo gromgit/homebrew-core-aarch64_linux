4420dde15425a262a8059fec27bf33a2a747d909
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 20:34:41 +0000

    vitess: update 12.0.0 bottle.

--- Formula/vitess.rb
+++ Formula/vitess.rb
@@ -6,10 +6,12 @@ class Vitess < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8c6af5dd5faf4aa7bf6debc58a3cfb68081745f8f7ebcdcaaf2c004e5cb2bb64"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1fb047b2ff939b8cdc7907b9bc95968b94d6837d40432a31dcb266af20304967"
-    sha256 cellar: :any_skip_relocation, catalina:      "79893cb774f092950a70b8f50fb4b7c9bb418c5c94494263733fcf7fa2841338"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e056f986c71427c43be17ac6f75d3a455255cc1388d3d20f3e78c60c6ca69b0b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c81c47e220b7387bee5d003b0f3049db8e5a6d9ba0ef3a48478957ad6f3069d3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c6af5dd5faf4aa7bf6debc58a3cfb68081745f8f7ebcdcaaf2c004e5cb2bb64"
+    sha256 cellar: :any_skip_relocation, monterey:       "8eaaed8085736146033d50f3bdef1443a4843289e4f1bf57787d9fe3313eb7a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fb047b2ff939b8cdc7907b9bc95968b94d6837d40432a31dcb266af20304967"
+    sha256 cellar: :any_skip_relocation, catalina:       "79893cb774f092950a70b8f50fb4b7c9bb418c5c94494263733fcf7fa2841338"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e056f986c71427c43be17ac6f75d3a455255cc1388d3d20f3e78c60c6ca69b0b"
   end
 
   depends_on "go" => :build
