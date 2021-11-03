90fe12d99ae05ccc8be211384ed2c9f27b8e882f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:45:17 +0000

    lcm: update 1.4.0_6 bottle.

--- Formula/lcm.rb
+++ Formula/lcm.rb
@@ -8,11 +8,13 @@ class Lcm < Formula
   head "https://github.com/lcm-proj/lcm.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "a0a53217477597ebd7afe6afcb10f732831af2914ebba6434d90a543ddd09aeb"
-    sha256 cellar: :any,                 big_sur:       "8ae12270c1b2ba9c0c02b22a32bb96326a4694aee2e0c65e694d71ef7e1a4c05"
-    sha256 cellar: :any,                 catalina:      "13a51b7c5ca3ffa82d366ae898ab98dcafa725af2a6f8319bcbf16225b0dba4f"
-    sha256 cellar: :any,                 mojave:        "437bc1978078c4ad00696efb00cf4add0afa7666c1e8b7a8b6080974bed3eae4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f395b8d2bc8dac7f0c332ef8a4aac62b3d9851a00c84820076addef775c29b9"
+    sha256 cellar: :any,                 arm64_monterey: "6f663f930c725a6e01f5186aa3b73ad6f8a247673d5dca58921f3c49dc4992ab"
+    sha256 cellar: :any,                 arm64_big_sur:  "a0a53217477597ebd7afe6afcb10f732831af2914ebba6434d90a543ddd09aeb"
+    sha256 cellar: :any,                 monterey:       "b871f2e30a9ee82cd0830f0db5340f006d7603a15da2982f7df806a66fccddfb"
+    sha256 cellar: :any,                 big_sur:        "8ae12270c1b2ba9c0c02b22a32bb96326a4694aee2e0c65e694d71ef7e1a4c05"
+    sha256 cellar: :any,                 catalina:       "13a51b7c5ca3ffa82d366ae898ab98dcafa725af2a6f8319bcbf16225b0dba4f"
+    sha256 cellar: :any,                 mojave:         "437bc1978078c4ad00696efb00cf4add0afa7666c1e8b7a8b6080974bed3eae4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2f395b8d2bc8dac7f0c332ef8a4aac62b3d9851a00c84820076addef775c29b9"
   end
 
   depends_on "cmake" => :build
