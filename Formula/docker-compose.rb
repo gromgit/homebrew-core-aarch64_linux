ba43d02ba23fa865c4e65233e9f31f0a0a62e493
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 06:19:38 +0000

    docker-compose: update 2.1.0 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -7,10 +7,12 @@ class DockerCompose < Formula
   head "https://github.com/docker/compose.git", branch: "v2"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "58d48d019a76bc87a00dfd01a0cfe29e7695b3a78a0cf498b910959e0bc2fa36"
-    sha256 cellar: :any_skip_relocation, big_sur:       "edb818a881044bb8f4bd2e6ee24608a69ae554a90ee94adaa5a87c3aaa634a54"
-    sha256 cellar: :any_skip_relocation, catalina:      "edb818a881044bb8f4bd2e6ee24608a69ae554a90ee94adaa5a87c3aaa634a54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "717cc79fb5b6e619c787314fc38f9df5460eb720ab98203a9ec87bd02e70ccd4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "58d48d019a76bc87a00dfd01a0cfe29e7695b3a78a0cf498b910959e0bc2fa36"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "58d48d019a76bc87a00dfd01a0cfe29e7695b3a78a0cf498b910959e0bc2fa36"
+    sha256 cellar: :any_skip_relocation, monterey:       "edb818a881044bb8f4bd2e6ee24608a69ae554a90ee94adaa5a87c3aaa634a54"
+    sha256 cellar: :any_skip_relocation, big_sur:        "edb818a881044bb8f4bd2e6ee24608a69ae554a90ee94adaa5a87c3aaa634a54"
+    sha256 cellar: :any_skip_relocation, catalina:       "edb818a881044bb8f4bd2e6ee24608a69ae554a90ee94adaa5a87c3aaa634a54"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "717cc79fb5b6e619c787314fc38f9df5460eb720ab98203a9ec87bd02e70ccd4"
   end
 
   depends_on "go" => :build
