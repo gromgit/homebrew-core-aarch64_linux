a0be91123fdf685710535281c0a8ccfbf22ddd77
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:18:43 +0000

    orc-tools: update 1.7.0 bottle.

--- Formula/orc-tools.rb
+++ Formula/orc-tools.rb
@@ -16,6 +16,7 @@ class OrcTools < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "4a2afb894549011d634dc18b93f8e18c64dbf4a02177fa1347d29a8c9dc321e8"
     sha256 cellar: :any_skip_relocation, mojave:        "4a2afb894549011d634dc18b93f8e18c64dbf4a02177fa1347d29a8c9dc321e8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "650434bdaf880afab5b34cee38e36def2a8a29a01cb791428b35bcf20b7539ff"
+    sha256 cellar: :any_skip_relocation, all:           "05f641694d0470d1996b47d59106cc34f1b97157bf3293919b70602bf2036903"
   end
 
   depends_on "openjdk"
