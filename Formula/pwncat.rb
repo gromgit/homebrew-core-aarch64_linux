e9aad8edceddfe4eb50a3a61b20ffd2238af7ee0
rui <rui@chenrui.dev>
2021-11-03 05:32:15 +0000

    pwncat: update 0.1.2 bottle.

--- Formula/pwncat.rb
+++ Formula/pwncat.rb
@@ -9,10 +9,12 @@ class Pwncat < Formula
   head "https://github.com/cytopia/pwncat.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fa1d3c17981bbba3ef5b35b162c38d2db2ab802c76fd979956bda9e6880d6a7a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fb3f4cb5954da1b5a7fccec4bb40f8807548cf79984b701788f72e364222d1fe"
-    sha256 cellar: :any_skip_relocation, catalina:      "fb3f4cb5954da1b5a7fccec4bb40f8807548cf79984b701788f72e364222d1fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a5bf7b976619100c3cf8763b48aba07b1c5c52e00230c23aeed1ec249fada17"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fa1d3c17981bbba3ef5b35b162c38d2db2ab802c76fd979956bda9e6880d6a7a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa1d3c17981bbba3ef5b35b162c38d2db2ab802c76fd979956bda9e6880d6a7a"
+    sha256 cellar: :any_skip_relocation, monterey:       "fb3f4cb5954da1b5a7fccec4bb40f8807548cf79984b701788f72e364222d1fe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fb3f4cb5954da1b5a7fccec4bb40f8807548cf79984b701788f72e364222d1fe"
+    sha256 cellar: :any_skip_relocation, catalina:       "fb3f4cb5954da1b5a7fccec4bb40f8807548cf79984b701788f72e364222d1fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7a5bf7b976619100c3cf8763b48aba07b1c5c52e00230c23aeed1ec249fada17"
   end
 
   depends_on "python@3.10"
