e72a7e6e5d524d611a2e1adb7c9289c2a63f1556
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 03:22:43 +0000

    syncthing: update 1.18.4 bottle.

--- Formula/syncthing.rb
+++ Formula/syncthing.rb
@@ -12,10 +12,12 @@ class Syncthing < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ada03366791d1adffd7aa7cc39bf0bd339d261fcbd0fb05c6cdbb1d08853a4f1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3b12aba3f84da38e22b4af29d81f1fb276e0bda32b84d3b63505a23d1308f7ea"
-    sha256 cellar: :any_skip_relocation, catalina:      "e358de640d97698911111de9d0b1ddbb192badb7beadd8851e25fec472394f45"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39c3963933f138b2f782fec71ade0c56b49be81025dcdb7785ddcd8b5786b75a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "346d2196e29b00cd6359d179fdaef3e5238d1fc0d6cc1115f011140187284e5c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ada03366791d1adffd7aa7cc39bf0bd339d261fcbd0fb05c6cdbb1d08853a4f1"
+    sha256 cellar: :any_skip_relocation, monterey:       "82eb35ea1ab8f85d7cebc303fde24dc46dc04601d769bcd8fa5e894c3c60eef6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3b12aba3f84da38e22b4af29d81f1fb276e0bda32b84d3b63505a23d1308f7ea"
+    sha256 cellar: :any_skip_relocation, catalina:       "e358de640d97698911111de9d0b1ddbb192badb7beadd8851e25fec472394f45"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39c3963933f138b2f782fec71ade0c56b49be81025dcdb7785ddcd8b5786b75a"
   end
 
   depends_on "go" => :build
