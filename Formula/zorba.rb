3b2bfb31a8ee39b2079b2e476d62ea7de0d69ae5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:58:14 +0000

    zorba: update 3.1_14 bottle.

--- Formula/zorba.rb
+++ Formula/zorba.rb
@@ -7,10 +7,12 @@ class Zorba < Formula
   revision 14
 
   bottle do
-    sha256 arm64_big_sur: "aa4cae0f56ef1d035d5ac36ac60b1dc6e4c0a794e97eb8aed539c5fb5f14a215"
-    sha256 big_sur:       "dadda2e73de44c8994fa09671bda4781ce88bd5a3836c335ecdcfbd9c20f1dfc"
-    sha256 catalina:      "8c0de8c4961888f97768b0f1a9d5ce2af9fdebd7cce2626b4a0c4f9bb7508c3d"
-    sha256 mojave:        "511bab00060dd21851440ad2cc0f64e9edec6b1beef2675505baaa6745154670"
+    sha256 arm64_monterey: "c49d833711490a7297a1f175a80ca3e2ef0c6bbdd35ba444cdd665d824a95d90"
+    sha256 arm64_big_sur:  "aa4cae0f56ef1d035d5ac36ac60b1dc6e4c0a794e97eb8aed539c5fb5f14a215"
+    sha256 monterey:       "ab8e4a76a55d1882c9597262d8dfa988a404198ebc3f6f56001da5ebb19fb014"
+    sha256 big_sur:        "dadda2e73de44c8994fa09671bda4781ce88bd5a3836c335ecdcfbd9c20f1dfc"
+    sha256 catalina:       "8c0de8c4961888f97768b0f1a9d5ce2af9fdebd7cce2626b4a0c4f9bb7508c3d"
+    sha256 mojave:         "511bab00060dd21851440ad2cc0f64e9edec6b1beef2675505baaa6745154670"
   end
 
   depends_on "cmake" => :build
