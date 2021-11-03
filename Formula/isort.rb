311b8afe8e353cdbe293184014a1b778610088a4
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-11-03 15:18:40 +0000

    isort: update 5.10.0 bottle.

--- Formula/isort.rb
+++ Formula/isort.rb
@@ -13,10 +13,12 @@ class Isort < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8a47154bfc16591b733dba25c447f9a66c5623f0d79ebcaf761579f2ce1abcb1"
-    sha256 cellar: :any_skip_relocation, big_sur:       "844debc7be1067c96394d2aca0d0b6168c6c7be985ee5410f39deeb0898e971b"
-    sha256 cellar: :any_skip_relocation, catalina:      "89041c73f3da0300871ba0d2777ac7acbdcb6179cbd2e23e1729c9ca47658e4b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc8dfdb4094129b412849b7f1247dcca83de39c3f774349c9e3509d763678e92"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "52e288997f03692918152f57ed4bbfe666956f151d0fa536a6f8c6781613f4bc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a47154bfc16591b733dba25c447f9a66c5623f0d79ebcaf761579f2ce1abcb1"
+    sha256 cellar: :any_skip_relocation, monterey:       "dc2e7a944b7bc2b517ff0605e1aa58787a36c583746b66ce3eedf540bb0d1970"
+    sha256 cellar: :any_skip_relocation, big_sur:        "844debc7be1067c96394d2aca0d0b6168c6c7be985ee5410f39deeb0898e971b"
+    sha256 cellar: :any_skip_relocation, catalina:       "89041c73f3da0300871ba0d2777ac7acbdcb6179cbd2e23e1729c9ca47658e4b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc8dfdb4094129b412849b7f1247dcca83de39c3f774349c9e3509d763678e92"
   end
 
   depends_on "python@3.10"
