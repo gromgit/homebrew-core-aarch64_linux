4058b89cbd0960d81ccb78b8fe935cbb76e6d205
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 13:36:34 +0000

    matterbridge: update 1.23.2 bottle.

--- Formula/matterbridge.rb
+++ Formula/matterbridge.rb
@@ -6,10 +6,12 @@ class Matterbridge < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e1e8132fc4300ea7eb8d10bf32e2f421e8b61aff1ff1eb89fa399d8732d3953f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "56c14f672c316a1710e8438ea2b8ebf3b1f5510acf1eb64c2718a6ad9a1bd73f"
-    sha256 cellar: :any_skip_relocation, catalina:      "ec8d0ba76fded16e615b5c3e1a484e8223dda43d122c76f24117cbe147c08b72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0ffbac30e1593eedec4650a3b5cf73a46f14c975230fa3dc970e19566839898"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f8f8d98ab840fc86ab6bdfcb9a680d133690afea8ca4302fd6b3c339019b469f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1e8132fc4300ea7eb8d10bf32e2f421e8b61aff1ff1eb89fa399d8732d3953f"
+    sha256 cellar: :any_skip_relocation, monterey:       "673c4d9547eabce6abe1844fdbf58097ce1761bb326a3de8fcd699c6f5ba4054"
+    sha256 cellar: :any_skip_relocation, big_sur:        "56c14f672c316a1710e8438ea2b8ebf3b1f5510acf1eb64c2718a6ad9a1bd73f"
+    sha256 cellar: :any_skip_relocation, catalina:       "ec8d0ba76fded16e615b5c3e1a484e8223dda43d122c76f24117cbe147c08b72"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0ffbac30e1593eedec4650a3b5cf73a46f14c975230fa3dc970e19566839898"
   end
 
   depends_on "go" => :build
