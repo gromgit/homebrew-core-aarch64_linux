c3e26f43eda185d59349dc57e13aabf55d67772f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 13:39:48 +0000

    qtkeychain: update 0.12.0_1 bottle.

--- Formula/qtkeychain.rb
+++ Formula/qtkeychain.rb
@@ -7,10 +7,11 @@ class Qtkeychain < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "58b0ced7662d5f0659ff7d3002d5a8b09d1fb135d1ee51a94bffbdee24d0eb58"
-    sha256 cellar: :any, big_sur:       "f97e360b724536ead639b64f637dc65ed60d90a7c2b3ca775c0af2c0e2c36873"
-    sha256 cellar: :any, catalina:      "94f6a7cef2f131e5193b1eaf7bedb7ab6d89a00de318519624b75a355cd644a4"
-    sha256 cellar: :any, mojave:        "325f6b601b8b004df1203f20d7355fa3f00c82602795ea701055b9eae577b0c5"
+    sha256 cellar: :any, arm64_monterey: "e40058707eccd378c6859412fcee43d034ec2365bae478d490665ae04d3a651c"
+    sha256 cellar: :any, arm64_big_sur:  "58b0ced7662d5f0659ff7d3002d5a8b09d1fb135d1ee51a94bffbdee24d0eb58"
+    sha256 cellar: :any, big_sur:        "f97e360b724536ead639b64f637dc65ed60d90a7c2b3ca775c0af2c0e2c36873"
+    sha256 cellar: :any, catalina:       "94f6a7cef2f131e5193b1eaf7bedb7ab6d89a00de318519624b75a355cd644a4"
+    sha256 cellar: :any, mojave:         "325f6b601b8b004df1203f20d7355fa3f00c82602795ea701055b9eae577b0c5"
   end
 
   depends_on "cmake" => :build
