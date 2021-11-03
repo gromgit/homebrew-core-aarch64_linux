73df4d04047ee1a8aab05c21adf3c7cafa62147c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 03:23:55 +0000

    tm: update 1.11.0 bottle.

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -7,10 +7,12 @@ class Tm < Formula
   head "https://github.com/triggermesh/tm.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91849b4ac672df1bc3029e0e430790e753b703485b72372ec312fe66f8200c8c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ef6ed69cf2940d564a2276426b6c404a4df9961bf694c35db5fa685060e63a50"
-    sha256 cellar: :any_skip_relocation, catalina:      "29f11fd2c1825fc0d4572a541292da44d0f619b6892f4b3979d6b3b9c5aba647"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13436b51d87221b2763b63635273a72b373d08544069d978eb384ed644934b46"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f068e3ff4f31d0e0eb48021d9a7880f3072b409dab1c6705bba0654d7aa0a1f1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91849b4ac672df1bc3029e0e430790e753b703485b72372ec312fe66f8200c8c"
+    sha256 cellar: :any_skip_relocation, monterey:       "36b6ab8c2117dae71a935545a209288f74d24c3457e51814320f29245e1f51be"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ef6ed69cf2940d564a2276426b6c404a4df9961bf694c35db5fa685060e63a50"
+    sha256 cellar: :any_skip_relocation, catalina:       "29f11fd2c1825fc0d4572a541292da44d0f619b6892f4b3979d6b3b9c5aba647"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13436b51d87221b2763b63635273a72b373d08544069d978eb384ed644934b46"
   end
 
   depends_on "go" => :build
