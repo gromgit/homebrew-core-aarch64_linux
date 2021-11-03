17903abfa896dc3d0db80e06d38b8408062f89c1
rinsuki <428rinsuki+git@gmail.com>
2021-11-03 22:16:26 +0900

    yarn: add conflict with corepack
    Closes #88700.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -16,6 +16,7 @@ class Yarn < Formula
   depends_on "node"
 
   conflicts_with "hadoop", because: "both install `yarn` binaries"
+  conflicts_with "corepack", because: "both install `yarn` and `yarnpkg` binaries"
 
   def install
     libexec.install buildpath.glob("*")
