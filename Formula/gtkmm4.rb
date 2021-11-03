1b3cef93fa68bcc76dbe39f59fe92b94f7dd3ecb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 18:54:07 +0000

    gtkmm4: update 4.2.0 bottle.

--- Formula/gtkmm4.rb
+++ Formula/gtkmm4.rb
@@ -11,10 +11,12 @@ class Gtkmm4 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "88cc51b5d7ff82f704b0fd4a1b53d6932ee5aeb3d5319c70226080a502ad7bf9"
-    sha256 cellar: :any, big_sur:       "ac0d436909b6be289508ff5aac82d6ab59c36f144145b7259b77234e2b544116"
-    sha256 cellar: :any, catalina:      "6a67aa2c9ae11a5dea8c142f221a0b6fdaece0f6ceecfd9747aeda99782cea21"
-    sha256 cellar: :any, mojave:        "5f21219575cf773cb21686b592cdf8e05617567490111a7919b87dc5d165fb63"
+    sha256 cellar: :any, arm64_monterey: "8678fd72c6111120f7b5ea4bdf0a55940a34aa52aec23e724b7d0ad2719095ee"
+    sha256 cellar: :any, arm64_big_sur:  "88cc51b5d7ff82f704b0fd4a1b53d6932ee5aeb3d5319c70226080a502ad7bf9"
+    sha256 cellar: :any, monterey:       "fb4d1543767a9513992776d3bcb15d74faa121edafc16616ec166395e60fa980"
+    sha256 cellar: :any, big_sur:        "ac0d436909b6be289508ff5aac82d6ab59c36f144145b7259b77234e2b544116"
+    sha256 cellar: :any, catalina:       "6a67aa2c9ae11a5dea8c142f221a0b6fdaece0f6ceecfd9747aeda99782cea21"
+    sha256 cellar: :any, mojave:         "5f21219575cf773cb21686b592cdf8e05617567490111a7919b87dc5d165fb63"
   end
 
   depends_on "meson" => :build
