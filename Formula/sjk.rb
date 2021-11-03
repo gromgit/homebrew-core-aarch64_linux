9748bab8899385fd175d37fa5081ce6f9493f0ba
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:43:49 +0000

    sjk: update 0.20 bottle.

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -11,6 +11,7 @@ class Sjk < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
     sha256 cellar: :any_skip_relocation, mojave:        "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "51d29b3fd8b210d7dbd7d881ab0dda3aa40b99c99362f5ed3aa8c2a1de5b7d54"
+    sha256 cellar: :any_skip_relocation, all:           "ce4c75268949b89a47d762e67ccbefdf55a8685188edb27f1827f58ebddc4a6e"
   end
 
   depends_on "openjdk"
