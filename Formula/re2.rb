84ced7e68d9434da4f5c9c1bfe3a9c693a972ec5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 20:24:30 +0000

    re2: update 20211101 bottle.

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -19,10 +19,12 @@ class Re2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "debe0b67ea263d4e1cfcb7a7a85a4d70e5db4bee54971daca718c8245b435cfd"
-    sha256 cellar: :any,                 big_sur:       "ee81d89700feecc8ab081c65aa0e6317ae1b580e33736687e10f2a0f8fa6b788"
-    sha256 cellar: :any,                 catalina:      "d3e0fa175a17e9a9b9e624113eb41b44a579829be8d6736e9ae14fb466493ef5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f77942403dca071176d50096e88737a548d1c9e5c90df19202c26aa05c7e2a6"
+    sha256 cellar: :any,                 arm64_monterey: "411d3fd746a54b73c5cfbf4554197d3529a6bc5d74db706edc2973bceb32ea47"
+    sha256 cellar: :any,                 arm64_big_sur:  "debe0b67ea263d4e1cfcb7a7a85a4d70e5db4bee54971daca718c8245b435cfd"
+    sha256 cellar: :any,                 monterey:       "e3750181b190e53efdc2fd2c1f3f6199d0648e993f2c8e7f3de618b8417a228f"
+    sha256 cellar: :any,                 big_sur:        "ee81d89700feecc8ab081c65aa0e6317ae1b580e33736687e10f2a0f8fa6b788"
+    sha256 cellar: :any,                 catalina:       "d3e0fa175a17e9a9b9e624113eb41b44a579829be8d6736e9ae14fb466493ef5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f77942403dca071176d50096e88737a548d1c9e5c90df19202c26aa05c7e2a6"
   end
 
   depends_on "cmake" => :build
