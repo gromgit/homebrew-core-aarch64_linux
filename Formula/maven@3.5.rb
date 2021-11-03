05041c76137ea55510515b4f2dd18b1cf6ae897e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 09:04:16 +0000

    maven@3.5: update 3.5.4_1 bottle.

--- Formula/maven@3.5.rb
+++ Formula/maven@3.5.rb
@@ -8,11 +8,13 @@ class MavenAT35 < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "395084c1ae2cf3ccfa343ed12c100155bb289658c0f82daa6dd1fd3b215c458d"
-    sha256 cellar: :any,                 big_sur:       "395c556575fb1c0d7b89559350c449ded3809b4f711efed178a559cd4d4cc535"
-    sha256 cellar: :any,                 catalina:      "395c556575fb1c0d7b89559350c449ded3809b4f711efed178a559cd4d4cc535"
-    sha256 cellar: :any,                 mojave:        "395c556575fb1c0d7b89559350c449ded3809b4f711efed178a559cd4d4cc535"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f291bcfaa6ed6d4a3c2a0c16dbe8342a55d528cd0c4c66c468f93b6ec7b3883"
+    sha256 cellar: :any,                 arm64_monterey: "4f685fb12ff8d606e753a10f33ada6e3549adfba01632bb709ec969c74ef5a16"
+    sha256 cellar: :any,                 arm64_big_sur:  "395084c1ae2cf3ccfa343ed12c100155bb289658c0f82daa6dd1fd3b215c458d"
+    sha256 cellar: :any,                 monterey:       "1056be877616d89127b7f6b702d11d5cf0ea655480d32d57931353b19bddaf25"
+    sha256 cellar: :any,                 big_sur:        "395c556575fb1c0d7b89559350c449ded3809b4f711efed178a559cd4d4cc535"
+    sha256 cellar: :any,                 catalina:       "395c556575fb1c0d7b89559350c449ded3809b4f711efed178a559cd4d4cc535"
+    sha256 cellar: :any,                 mojave:         "395c556575fb1c0d7b89559350c449ded3809b4f711efed178a559cd4d4cc535"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0f291bcfaa6ed6d4a3c2a0c16dbe8342a55d528cd0c4c66c468f93b6ec7b3883"
   end
 
   keg_only :versioned_formula
