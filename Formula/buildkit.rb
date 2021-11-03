1b6e8fd559237f8187fda96375995ee05dfe4b0a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-03 06:19:27 +0000

    buildkit: update 0.9.2 bottle.

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -13,10 +13,12 @@ class Buildkit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cef0a7f2b373cee7c31aa6bd458fdd31d0f14504b68d39959e98f1c12dc81f6c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4076c656b0122f1aad027149270b2e510bf7c3edf5cab5d5eb13d6659f4ec041"
-    sha256 cellar: :any_skip_relocation, catalina:      "a14409eed8abed2eb4b74fae761f4a85786d19fb0d2a3b2349eaf0d370fd1a6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9de9c6adadeca3f9911d3de8bc2433010b48b45f7c8863eeafdc7c9d1e405b1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "99d47eb2ecd2e36ee0ee7ac63103134dfcd80e208c2886e812d767416d13a658"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cef0a7f2b373cee7c31aa6bd458fdd31d0f14504b68d39959e98f1c12dc81f6c"
+    sha256 cellar: :any_skip_relocation, monterey:       "188b3ca1bc597294653b18c8bf77046bc9cc9ea85cc00a8310bf2df58413aca9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4076c656b0122f1aad027149270b2e510bf7c3edf5cab5d5eb13d6659f4ec041"
+    sha256 cellar: :any_skip_relocation, catalina:       "a14409eed8abed2eb4b74fae761f4a85786d19fb0d2a3b2349eaf0d370fd1a6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9de9c6adadeca3f9911d3de8bc2433010b48b45f7c8863eeafdc7c9d1e405b1"
   end
 
   depends_on "go" => :build
