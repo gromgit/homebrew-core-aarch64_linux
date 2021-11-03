cfa0674c2f837bbe201522e49ed7765cbc3939ae
rui <rui@chenrui.dev>
2021-11-03 23:25:44 +0000

    rover: update 0.4.0 bottle.

--- Formula/rover.rb
+++ Formula/rover.rb
@@ -7,9 +7,11 @@ class Rover < Formula
   head "https://github.com/apollographql/rover.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ab5e8a3c781e7ed16cf6ef376e0216d467d2fb4d10deebf079c5fed0fb7382aa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "13493ea0d4b8581a0b168710dd87701fc4be74d06b620f7e28438b22ded3aca0"
-    sha256 cellar: :any_skip_relocation, catalina:      "398b0d1a8986aef1dcd872dc81dce02e81cc31b32a0362747b560f9f1390f0e9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f34dd0de4ca8ce710205a06ab8be4d43735030820342a5cc33c8b945041655e8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab5e8a3c781e7ed16cf6ef376e0216d467d2fb4d10deebf079c5fed0fb7382aa"
+    sha256 cellar: :any_skip_relocation, monterey:       "095ad3f4d36a2645ea2c30e9243e1c285c4a42b5119cf315d4a1e2ef2275ce71"
+    sha256 cellar: :any_skip_relocation, big_sur:        "13493ea0d4b8581a0b168710dd87701fc4be74d06b620f7e28438b22ded3aca0"
+    sha256 cellar: :any_skip_relocation, catalina:       "398b0d1a8986aef1dcd872dc81dce02e81cc31b32a0362747b560f9f1390f0e9"
   end
 
   depends_on "rust" => :build
