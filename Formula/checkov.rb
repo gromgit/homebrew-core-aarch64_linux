55f6bd663f265868b8fc11164372b5c55daff2ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-04 21:08:30 +0000

    checkov: update 2.0.540 bottle.

--- Formula/checkov.rb
+++ Formula/checkov.rb
@@ -9,12 +9,10 @@ class Checkov < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "09207a0a6993a280b8de2098d510eaadb43de16afbff1bd07bcbc62fca29a2d4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d834fc082081213373d3a61e49dc3e64d61879da671ae50884517857d5845dce"
-    sha256 cellar: :any_skip_relocation, monterey:       "782d5d9c468079df70fa1d10e5896257294843a9e5b7ef5a3a61b125af4b68b3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d8bfb7c14ec1ffb079a6f1d54405da6493708bf45aedbfdbd3898f81f4572f37"
-    sha256 cellar: :any_skip_relocation, catalina:       "241b44b8a889586248a96d9be5977c99ac7b57c20329668029db07f3de9c2053"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e700bf57819e61c48efb07364830b7fc574685986c986e38514e649a383273c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9a2f3d163dbe4906561bc6f183a07340554295e647f00e6554e4e9720d9848ae"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a4c6ade7b9fff2d6dbe2710bc905824f3d871f2b461fd9245ce3719478d24209"
+    sha256 cellar: :any_skip_relocation, catalina:      "1c7ff7996f6a6636fc8853b9cdeea2abd28f0f33656511bfd5d4d7e4207b661d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9c5722b250038e315d21ecc1ce3535a875bb69a6125d44e5936fae9337aadeb"
   end
 
   depends_on "python-tabulate"
