19e05270aa2234c56721a853a78480251a8bd70c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-04 23:07:14 +0000

    serverless: update 2.65.0 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -9,10 +9,12 @@ class Serverless < Formula
   head "https://github.com/serverless/serverless.git", branch: "master"
 
   bottle do
-    sha256                               arm64_big_sur: "e6fa6bd67ed51a5999c80f4f8c585e0fef7896b2f99f48a053dfcedf2bd498de"
-    sha256                               big_sur:       "d9ef3cfdacf18bb4f01434d3dd63873e13d105a676871c2c9ec34b16decb474e"
-    sha256                               catalina:      "0a4cd6635b8bc87e2affd00c8b9db961450e8ec0b170cfe72b045a1065f76f17"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1072b8aed081e8eb00685d8631a772edff6d70792e8f84691c442858681901cf"
+    sha256                               arm64_monterey: "60e6b809c5943673224770bc59544b51affe99e17d4b927481228dcdd040b01e"
+    sha256                               arm64_big_sur:  "e6fa6bd67ed51a5999c80f4f8c585e0fef7896b2f99f48a053dfcedf2bd498de"
+    sha256                               monterey:       "0d31cf37067f69c08f531a75e7c5bbc327428b11a6493034a23ef3a7578547ab"
+    sha256                               big_sur:        "d9ef3cfdacf18bb4f01434d3dd63873e13d105a676871c2c9ec34b16decb474e"
+    sha256                               catalina:       "0a4cd6635b8bc87e2affd00c8b9db961450e8ec0b170cfe72b045a1065f76f17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1072b8aed081e8eb00685d8631a772edff6d70792e8f84691c442858681901cf"
   end
 
   depends_on "node"
