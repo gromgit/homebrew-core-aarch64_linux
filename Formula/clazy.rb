fb7d839dcacdc7ed5d1344a71f9407ec2cedf352
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-04 15:37:14 +0000

    clazy: update 1.10_1 bottle.

--- Formula/clazy.rb
+++ Formula/clazy.rb
@@ -13,10 +13,12 @@ class Clazy < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7d15f90dbe42c90a9920c117854b94f17908622df7def7d4d5dda8b5303ef7e5"
-    sha256 cellar: :any, big_sur:       "a873bc99b80959e45cf74c388bdefd9541a985f87e42de9b35a6ad82bec5953f"
-    sha256 cellar: :any, catalina:      "57301f79bae291ae0817b4b6577f49a4ea2136849d36b8c0b949c026995ebb2a"
-    sha256 cellar: :any, mojave:        "6c2cf6029ba58ad5d7fa13098fcd18d9514315bc57e11c090f351f4e93df5350"
+    sha256 cellar: :any, arm64_monterey: "c44b906300b19fd0f3fa64dd98c6edf58065a59494d040e7d1121944fd36df2e"
+    sha256 cellar: :any, arm64_big_sur:  "7d15f90dbe42c90a9920c117854b94f17908622df7def7d4d5dda8b5303ef7e5"
+    sha256 cellar: :any, monterey:       "a325c3349a7bbcde4d330a19b030a2d54c57ddfd222e66022825ca04cd9d2123"
+    sha256 cellar: :any, big_sur:        "a873bc99b80959e45cf74c388bdefd9541a985f87e42de9b35a6ad82bec5953f"
+    sha256 cellar: :any, catalina:       "57301f79bae291ae0817b4b6577f49a4ea2136849d36b8c0b949c026995ebb2a"
+    sha256 cellar: :any, mojave:         "6c2cf6029ba58ad5d7fa13098fcd18d9514315bc57e11c090f351f4e93df5350"
   end
 
   depends_on "cmake"   => [:build, :test]
