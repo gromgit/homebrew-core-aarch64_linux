34e2fa0320b53b1917332f9231182b9b0ac05f97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-04 21:08:22 +0000

    kubevela: update 1.1.8 bottle.

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -7,12 +7,10 @@ class Kubevela < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "89a45a3ddeb349b19b00e13dd10c330a16b5cfaee1c59b5ff2fc0f2e9845a172"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63897a419d79ef4b66afa0f635507766ed63b90660b8ae2e4ac4d3eec07da080"
-    sha256 cellar: :any_skip_relocation, monterey:       "ac2c3e50533d537c73dda62b283bcd93ec8e82a8c533903dcdcba2b7032e7d6f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4dcb0717494a844f63a55d7e26413197b694169be98bb0b6459f38c703fc50b2"
-    sha256 cellar: :any_skip_relocation, catalina:       "905bc8864bdd727fb9a223490dd2e47af8d58a8f00e6a8fa3b32b0dd323ff409"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b48f6466de06dac30df14811aa01e815ec9c1c8e5ad48bb58a42880fc1c36f6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91beb748bb1baea49a200b9e934fd30e554f3f8e99127383932963f1aadb47ba"
+    sha256 cellar: :any_skip_relocation, big_sur:       "cb0db86500b6c4937815d8f4bb81911dc56c29e0389a8493b7544c282de8fcfc"
+    sha256 cellar: :any_skip_relocation, catalina:      "7ad7c94a35abe1e6bdf149fa1ae556afa3a231cfa700ea0dd771bd36bafd26b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6897864cf9d25f2d7335ea376c13cadc89623b41e972ac001e838c91a79df0f6"
   end
 
   depends_on "go" => :build
