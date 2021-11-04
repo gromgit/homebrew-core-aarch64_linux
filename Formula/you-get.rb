6ee172ea1c6709b86aa7f53b7cc742bf74599519
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-04 07:28:49 +0000

    you-get: update 0.4.1555 bottle.

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -9,10 +9,12 @@ class YouGet < Formula
   head "https://github.com/soimort/you-get.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e6b44337d222234585e6e3997f6d1660550893171979c3974db98f5ea5754abb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "df0dc12c7442eba996568ba4308f522f1850d75a7434fdc891bcea339e9b65e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "df0dc12c7442eba996568ba4308f522f1850d75a7434fdc891bcea339e9b65e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12c1736ef7aa5fd5450201f8640946d21eae71ca0eff2ea903c6ab38f4043784"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e6b44337d222234585e6e3997f6d1660550893171979c3974db98f5ea5754abb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e6b44337d222234585e6e3997f6d1660550893171979c3974db98f5ea5754abb"
+    sha256 cellar: :any_skip_relocation, monterey:       "df0dc12c7442eba996568ba4308f522f1850d75a7434fdc891bcea339e9b65e6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "df0dc12c7442eba996568ba4308f522f1850d75a7434fdc891bcea339e9b65e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "df0dc12c7442eba996568ba4308f522f1850d75a7434fdc891bcea339e9b65e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12c1736ef7aa5fd5450201f8640946d21eae71ca0eff2ea903c6ab38f4043784"
   end
 
   depends_on "python@3.10"
