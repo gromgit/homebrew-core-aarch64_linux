fa03bdcc24187fff1606b80040d6b52eff0348ca
rnsc <github@rnsc.be>
2021-11-04 21:27:13 +0100

    ipcalc 0.51
    * ipcalc 0.51
    * use default livecheck and add license
    * use shorter url
    
    Closes #88794.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ipcalc.rb
+++ Formula/ipcalc.rb
@@ -1,13 +1,9 @@
 class Ipcalc < Formula
   desc "Calculate various network masks, etc. from a given IP address"
   homepage "http://jodies.de/ipcalc"
-  url "http://jodies.de/ipcalc-archive/ipcalc-0.41.tar.gz"
-  sha256 "dda9c571ce3369e5b6b06e92790434b54bec1f2b03f1c9df054c0988aa4e2e8a"
-
-  livecheck do
-    url "http://jodies.de/ipcalc-archive/"
-    regex(/href=.*?ipcalc[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
+  url "https://github.com/kjokjo/ipcalc/archive/0.51.tar.gz"
+  sha256 "a4dbfaeb7511b81830793ab9936bae9d7b1b561ad33e29106faaaf97ba1c117e"
+  license "GPL-2.0-or-later"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "fdb1f6c264ecbd824e0a514704000f46825e6c092487cdd467c964f12d3bf960"
