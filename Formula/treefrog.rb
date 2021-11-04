d150cbb3ad502c8e243298401da8f448eea70b97
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-04 15:30:51 +0000

    treefrog: update 2.2.0 bottle.

--- Formula/treefrog.rb
+++ Formula/treefrog.rb
@@ -12,9 +12,11 @@ class Treefrog < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5abd291b07c9b01836cca2e0c77f1e6dc3efa26f6f4a9bfe5ae44dbadefaa20a"
-    sha256 big_sur:       "8f3785e141e607b61fde6a849d6dd7b3f1b6f60040aa8b81e15a0c3c0c5d330c"
-    sha256 catalina:      "508314c683b84821d88dc5d2a0a7cf4a6f56ecb3f988e9077215e7586c915078"
+    sha256 arm64_monterey: "f3db6295ee07c312b61271c8f1c770231827e303855ff24e00810deaf9b2aa28"
+    sha256 arm64_big_sur:  "5abd291b07c9b01836cca2e0c77f1e6dc3efa26f6f4a9bfe5ae44dbadefaa20a"
+    sha256 monterey:       "cba31d2cef437bf03ce1870df5273b98c36c09a0a9dcb6db2312ac9755f6768d"
+    sha256 big_sur:        "8f3785e141e607b61fde6a849d6dd7b3f1b6f60040aa8b81e15a0c3c0c5d330c"
+    sha256 catalina:       "508314c683b84821d88dc5d2a0a7cf4a6f56ecb3f988e9077215e7586c915078"
   end
 
   depends_on xcode: :build
