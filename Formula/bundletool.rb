85a4bc28456c5679fee688a795fdf9cd89fd3e6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-04 06:07:32 +0000

    bundletool: update 1.8.2 bottle.

--- Formula/bundletool.rb
+++ Formula/bundletool.rb
@@ -6,7 +6,7 @@ class Bundletool < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "14a1731c3cd8f130bec95ecdd90a6368790ab5705fcfa647d9faf9847e963343"
+    sha256 cellar: :any_skip_relocation, all: "75c946b945ceef3db9ee8024009c9a1e81e02dab3344251baba2992728db3240"
   end
 
   depends_on "openjdk"
