e9848fad710399713c4dd5e22c27e1a96b033ab3
Sean Molenaar <smillerdev@me.com>
2021-11-04 09:13:40 +0100

    alpine: revision bump for openldap 2.6.0

--- Formula/alpine.rb
+++ Formula/alpine.rb
@@ -4,6 +4,7 @@ class Alpine < Formula
   url "https://alpine.x10host.com/alpine/release/src/alpine-2.25.tar.xz"
   sha256 "658a150982f6740bb4128e6dd81188eaa1212ca0bf689b83c2093bb518ecf776"
   license "Apache-2.0"
+  revision 1
   head "https://repo.or.cz/alpine.git", branch: "master"
 
   livecheck do
