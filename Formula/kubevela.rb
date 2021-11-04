05849c4d060b93d32766ede0303f7724b3b0a0f6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-04 22:56:45 +0000

    kubevela: update 1.1.8 bottle.

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -7,10 +7,12 @@ class Kubevela < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91beb748bb1baea49a200b9e934fd30e554f3f8e99127383932963f1aadb47ba"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cb0db86500b6c4937815d8f4bb81911dc56c29e0389a8493b7544c282de8fcfc"
-    sha256 cellar: :any_skip_relocation, catalina:      "7ad7c94a35abe1e6bdf149fa1ae556afa3a231cfa700ea0dd771bd36bafd26b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6897864cf9d25f2d7335ea376c13cadc89623b41e972ac001e838c91a79df0f6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d2710a4a7230abf62aa7072899e597728f95668996e52aa3a4d2142acba2278d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91beb748bb1baea49a200b9e934fd30e554f3f8e99127383932963f1aadb47ba"
+    sha256 cellar: :any_skip_relocation, monterey:       "effc058de8ae8c6fca9e9d1f22e9881e1eafe8c01ca42e66e04b887248939746"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb0db86500b6c4937815d8f4bb81911dc56c29e0389a8493b7544c282de8fcfc"
+    sha256 cellar: :any_skip_relocation, catalina:       "7ad7c94a35abe1e6bdf149fa1ae556afa3a231cfa700ea0dd771bd36bafd26b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6897864cf9d25f2d7335ea376c13cadc89623b41e972ac001e838c91a79df0f6"
   end
 
   depends_on "go" => :build
