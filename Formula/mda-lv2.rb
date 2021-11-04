9f37990089f31ce0fcbe0c15d6f756c1de666f73
EricFromCanada <enk3@outlook.com>
2021-11-04 10:18:56 -0400

    mda-lv2: fix homepage URL

--- Formula/mda-lv2.rb
+++ Formula/mda-lv2.rb
@@ -1,6 +1,6 @@
 class MdaLv2 < Formula
   desc "LV2 port of the MDA plugins"
-  homepage "https://drobilla.net/software/mda-lv2/"
+  homepage "https://drobilla.net/software/mda-lv2.html"
   url "https://download.drobilla.net/mda-lv2-1.2.6.tar.bz2"
   sha256 "cd66117024ae049cf3aca83f9e904a70277224e23a969f72a9c5d010a49857db"
   license "GPL-3.0-or-later"
