bc2200a52a6fdc56db5bc75c2521d0832bd32ccd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 01:50:47 +0000

    sfcgal: update 1.4.0 bottle.

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -6,10 +6,11 @@ class Sfcgal < Formula
   license "LGPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b809fe115553e7ffa0679bd2402f1994cb01a98e646a775c6dfe8ae706566d89"
-    sha256 cellar: :any,                 big_sur:       "e69bacf0f31e67adb7a5abc3fb358f0cbb2c1406f664bd5683c4f16241392d7c"
-    sha256 cellar: :any,                 catalina:      "16d0494615f89ec1f418d1e817e894972dbd6497387f2cf714ae9edabac5d3f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd4bc96070f584bbbef5fec11b51ca187cbb2d1be4a60efa9d29ea6e7203a939"
+    sha256 cellar: :any,                 arm64_monterey: "44f6644ec30918f7a23a345fc07904419744378353837ce49ee2d39aa6617c06"
+    sha256 cellar: :any,                 arm64_big_sur:  "b809fe115553e7ffa0679bd2402f1994cb01a98e646a775c6dfe8ae706566d89"
+    sha256 cellar: :any,                 big_sur:        "e69bacf0f31e67adb7a5abc3fb358f0cbb2c1406f664bd5683c4f16241392d7c"
+    sha256 cellar: :any,                 catalina:       "16d0494615f89ec1f418d1e817e894972dbd6497387f2cf714ae9edabac5d3f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cd4bc96070f584bbbef5fec11b51ca187cbb2d1be4a60efa9d29ea6e7203a939"
   end
 
   depends_on "cmake" => :build
