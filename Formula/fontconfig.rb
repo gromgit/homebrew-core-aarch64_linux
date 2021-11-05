ffa3067f7f8418ad998b45b3a344893bf13129ae
oofdog <46097282+oofdog@users.noreply.github.com>
2021-11-05 18:20:22 -0400

    fontconfig: force --disable-docs option (#88364)
    The `without-docs` option which adds `--disable-docs` to the configure step was removed in this commit https://github.com/Homebrew/homebrew-core/commit/0643a07a30b6c3bd69c16860a179c428c29f3e73
    However, this option is still needed as people don't always have docbook2pdf installed

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -65,6 +65,7 @@ class Fontconfig < Formula
     ENV["UUID_CFLAGS"] = "-I#{Formula["util-linux"].include}" if OS.linux?
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--disable-docs",
                           "--enable-static",
                           "--with-add-fonts=#{font_dirs.join(",")}",
                           "--prefix=#{prefix}",
