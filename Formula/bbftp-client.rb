37e736ea512811e5e5cd6c71a7f814fbe6ce9f7f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 08:08:12 +0800

    bbftp-client: update 3.2.1_3 bottle.

--- Formula/bbftp-client.rb
+++ Formula/bbftp-client.rb
@@ -13,10 +13,11 @@ class BbftpClient < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd7a47c27111d4dc064a7009f919a3283360738329dcfde7eb6522ee280e78fd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f30650734e1829a0c399153c78088ccd987f28ede25b8eb13ecde6b138d55076"
-    sha256 cellar: :any_skip_relocation, catalina:      "6d5bed31d69a0ff2f38f2642176cb3c3a4da34c4ea2740567d2698ca62519b7d"
-    sha256 cellar: :any_skip_relocation, mojave:        "bdb7c899dab18816b4cc1d573291ba4691f365c9ed1c9951e73f9225810a8557"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d429a71fe3b54e34d75efd1480062c322cee2a9b471628a671de3e9f1b91b201"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd7a47c27111d4dc064a7009f919a3283360738329dcfde7eb6522ee280e78fd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f30650734e1829a0c399153c78088ccd987f28ede25b8eb13ecde6b138d55076"
+    sha256 cellar: :any_skip_relocation, catalina:       "6d5bed31d69a0ff2f38f2642176cb3c3a4da34c4ea2740567d2698ca62519b7d"
+    sha256 cellar: :any_skip_relocation, mojave:         "bdb7c899dab18816b4cc1d573291ba4691f365c9ed1c9951e73f9225810a8557"
   end
 
   def install
