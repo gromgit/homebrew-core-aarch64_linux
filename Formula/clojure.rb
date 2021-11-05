22c1bd03b3eddfd73291a8b19ec4a2620a6497f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-05 22:08:03 +0000

    clojure: update 1.10.3.1020 bottle.

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -12,11 +12,7 @@ class Clojure < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "16559983e154d0efa4f36ea2b58d5dcaf52c3a1890481275f5a12f13fbdb9ee8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "16559983e154d0efa4f36ea2b58d5dcaf52c3a1890481275f5a12f13fbdb9ee8"
-    sha256 cellar: :any_skip_relocation, catalina:      "16559983e154d0efa4f36ea2b58d5dcaf52c3a1890481275f5a12f13fbdb9ee8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2392e4c09fc15cb976b919dcb1708a62d906e86ed48697effdf2fb2b0027074"
-    sha256 cellar: :any_skip_relocation, all:           "80fa694a5c5db974a80b51c101370775083350d670a1073fe5406d3caa3a7787"
+    sha256 cellar: :any_skip_relocation, all: "839431989929912c573bb3a3eb8530244075716cf5755a0e56850f631ac87dc3"
   end
 
   depends_on "openjdk"
