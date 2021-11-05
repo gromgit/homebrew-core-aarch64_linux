82ba65de240843da4dd276cb75b3f9c05ca99d0d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 15:29:44 +0000

    podman: update 3.4.1 bottle.

--- Formula/podman.rb
+++ Formula/podman.rb
@@ -15,9 +15,11 @@ class Podman < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f0b683729b4567a5b610c1feddbe2c30b958c3729ec4d4185fb73c6c7cfd2c91"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f70e26a2975741a29c22161559dd176fec1d96b085a76d92ff73756a47be6cb2"
-    sha256 cellar: :any_skip_relocation, catalina:      "a9a7c3f81a6cd794aec724a22c052680e923b88ddc57a746cc0c341fad6ea7d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b86bffeca7ae414d3a97db43021bed86cadac22d1ad3b7bc24efc09983066472"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f0b683729b4567a5b610c1feddbe2c30b958c3729ec4d4185fb73c6c7cfd2c91"
+    sha256 cellar: :any_skip_relocation, monterey:       "21ba5f3a140df5344431000efd5dfb941832be0bfa0b27612143f2d8982721f5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f70e26a2975741a29c22161559dd176fec1d96b085a76d92ff73756a47be6cb2"
+    sha256 cellar: :any_skip_relocation, catalina:       "a9a7c3f81a6cd794aec724a22c052680e923b88ddc57a746cc0c341fad6ea7d6"
   end
 
   head do
