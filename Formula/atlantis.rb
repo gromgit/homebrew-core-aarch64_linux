76614db454e897f49a9e9a9cf09cd74ee924466b
rui <rui@chenrui.dev>
2021-11-05 00:48:55 +0000

    atlantis: update 0.17.5 bottle.

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -7,10 +7,12 @@ class Atlantis < Formula
   head "https://github.com/runatlantis/atlantis.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "518735ba7fd8282f97d6046c165de647414d50afe0dd354f92176341f7202787"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ab6914d77bbc729ac1a6ee88ff7ee47402aba9134f58ab341fe814571097cd0d"
-    sha256 cellar: :any_skip_relocation, catalina:      "da635f12319ba42ea59fb66c7f3b964a4838094c60e95ddbe897c6ab064f5dfe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b002b6ed16d07edcba81c9280fad09958be6fc858e550128f92842f7f91d672c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "31aba142586f85511773e7c0938aafe55d36a9b5b5e5b791af257d6d7bf7a917"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "518735ba7fd8282f97d6046c165de647414d50afe0dd354f92176341f7202787"
+    sha256 cellar: :any_skip_relocation, monterey:       "02ea1cf3092a7adf497364a916e076d383850f2676a8dbb8761ffc890046c385"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ab6914d77bbc729ac1a6ee88ff7ee47402aba9134f58ab341fe814571097cd0d"
+    sha256 cellar: :any_skip_relocation, catalina:       "da635f12319ba42ea59fb66c7f3b964a4838094c60e95ddbe897c6ab064f5dfe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b002b6ed16d07edcba81c9280fad09958be6fc858e550128f92842f7f91d672c"
   end
 
   depends_on "go" => :build
