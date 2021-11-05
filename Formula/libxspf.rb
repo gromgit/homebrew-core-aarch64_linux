feea89776de96c0881b44548071d9b8f4baded0d
EricFromCanada <enk3@outlook.com>
2021-11-05 12:36:14 -0400

    libxspf 1.2.1
    Closes #88841.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libxspf.rb
+++ Formula/libxspf.rb
@@ -1,14 +1,9 @@
 class Libxspf < Formula
   desc "C++ library for XSPF playlist reading and writing"
   homepage "https://libspiff.sourceforge.io/"
-  url "https://downloads.xiph.org/releases/xspf/libxspf-1.2.0.tar.bz2"
-  mirror "https://ftp.osuosl.org/pub/xiph/releases/xspf/libxspf-1.2.0.tar.bz2"
-  sha256 "ba9e93a0066469b074b4022b480004651ad3aa5b4313187fd407d833f79b43a5"
-
-  livecheck do
-    url "https://ftp.osuosl.org/pub/xiph/releases/xspf/?C=M&O=D"
-    regex(/href=.*?libxspf[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
+  url "https://downloads.sourceforge.net/project/libspiff/Sources/1.2.1/libxspf-1.2.1.tar.bz2"
+  sha256 "ce78a7f7df73b7420b6a54b5766f9b74e396d5e0b37661c9a448f2f589754a49"
+  license "BSD-3-Clause"
 
   bottle do
     rebuild 2
@@ -18,31 +13,31 @@ class Libxspf < Formula
     sha256 cellar: :any, high_sierra: "ae1e960341fb08826fb30de46f3b1c075c22d9e655acb9e44067327c487328a7"
   end
 
+  depends_on "cpptest" => :build
   depends_on "pkg-config" => :build
-  depends_on "cpptest"
   depends_on "uriparser"
 
-  # Fix build against clang and GCC 4.7+
-  # https://gitlab.xiph.org/xiph/libxspf/-/commit/7f1f68d433f03484b572657ff5df47bba1b03ba6
-  patch :DATA
+  uses_from_macos "expat"
+
+  resource "check.cpp" do
+    url "https://gitlab.xiph.org/xiph/libxspf/-/raw/master/examples/check/check.cpp"
+    sha256 "fdd1e586c4f5b724890eb2ce6a3cd3b9910a8e74ee454fe462ba8eb802f7c4b9"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
+
+  test do
+    resource("check.cpp").stage(testpath)
+    flags = "-I#{include} -L#{lib} -lxspf".split
+    system ENV.cxx, "check.cpp", "-o", "check", *flags
+  end
 end
-
-__END__
-diff --git a/examples/read/read.cpp b/examples/read/read.cpp
-index 411f892..b66a25c 100644
---- a/examples/read/read.cpp
-+++ b/examples/read/read.cpp
-@@ -43,6 +43,7 @@
- #include <cstdio>
- #include <cstdlib> // MAX_PATH
- #include <climits> // PATH_MAX
-+#include <unistd.h>
-
-
- #if defined(__WIN32__) || defined(WIN32)
