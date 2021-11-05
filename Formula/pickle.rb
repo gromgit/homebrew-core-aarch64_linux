d4b7b54ef9cb120a1cc97757c259a85f750c06ef
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 14:32:15 +0000

    pickle: update 0.7.7 bottle.

--- Formula/pickle.rb
+++ Formula/pickle.rb
@@ -6,10 +6,12 @@ class Pickle < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0cb6da56e1d2006229fe4faaedfa70c0e3cd6a0ed54d87a3965175a8831a599d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
-    sha256 cellar: :any_skip_relocation, catalina:      "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
-    sha256 cellar: :any_skip_relocation, mojave:        "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0cb6da56e1d2006229fe4faaedfa70c0e3cd6a0ed54d87a3965175a8831a599d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0cb6da56e1d2006229fe4faaedfa70c0e3cd6a0ed54d87a3965175a8831a599d"
+    sha256 cellar: :any_skip_relocation, monterey:       "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
+    sha256 cellar: :any_skip_relocation, catalina:       "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
+    sha256 cellar: :any_skip_relocation, mojave:         "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
   end
 
   depends_on "php"
