854275fc31b3c2f7a28a4756610b4d424c482a67
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 10:06:52 +0000

    tomcat-native: update 1.2.31 bottle.

--- Formula/tomcat-native.rb
+++ Formula/tomcat-native.rb
@@ -7,12 +7,13 @@ class TomcatNative < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "c6f4dd11e7fbf16ad6f7e11b7d4c81e1ee6159bc025e0464430d920588fd1f24"
-    sha256 cellar: :any,                 monterey:      "966d83820a8738edb2fea2fb2c4dbc4b18c786b70ddbed1c01126821a24e9241"
-    sha256 cellar: :any,                 big_sur:       "41a3d1bd142f9af9709a09ef08fa9c1d4afba8c1df9afcf206680abef9cdc561"
-    sha256 cellar: :any,                 catalina:      "49cb291540334f5a2b15ddbd2b597df9993cd958aa55d301b45b36389178c10e"
-    sha256 cellar: :any,                 mojave:        "24a026ac8fbbaf58b678c8cb8b8eaaf6d1ffca5774fd761943bc49b1e1179366"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "330a4f42782065c9160ff41e5c55d483f1d866b569854db3b91f45959f553f1b"
+    sha256 cellar: :any,                 arm64_monterey: "94de42ed8da515cb63be2b5bab2322975f02810f214c575fb5a48f0f8df00900"
+    sha256 cellar: :any,                 arm64_big_sur:  "c6f4dd11e7fbf16ad6f7e11b7d4c81e1ee6159bc025e0464430d920588fd1f24"
+    sha256 cellar: :any,                 monterey:       "966d83820a8738edb2fea2fb2c4dbc4b18c786b70ddbed1c01126821a24e9241"
+    sha256 cellar: :any,                 big_sur:        "41a3d1bd142f9af9709a09ef08fa9c1d4afba8c1df9afcf206680abef9cdc561"
+    sha256 cellar: :any,                 catalina:       "49cb291540334f5a2b15ddbd2b597df9993cd958aa55d301b45b36389178c10e"
+    sha256 cellar: :any,                 mojave:         "24a026ac8fbbaf58b678c8cb8b8eaaf6d1ffca5774fd761943bc49b1e1179366"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "330a4f42782065c9160ff41e5c55d483f1d866b569854db3b91f45959f553f1b"
   end
 
   depends_on "libtool" => :build
