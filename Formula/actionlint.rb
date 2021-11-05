008d70a380368ea58ccdc2e338bbcbd9decb9b73
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 16:22:44 +0000

    actionlint: update 1.6.6_1 bottle.

--- Formula/actionlint.rb
+++ Formula/actionlint.rb
@@ -7,11 +7,13 @@ class Actionlint < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d63e74ca7623ab80f10e293947a7e397a33f2dc5a7193c63ed303c5d799a3318"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
-    sha256 cellar: :any_skip_relocation, catalina:      "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
-    sha256 cellar: :any_skip_relocation, mojave:        "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7a8893c093a81d33fdc180a79de742930db5a2c173ab7bf8a4567eff0108e75"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d63e74ca7623ab80f10e293947a7e397a33f2dc5a7193c63ed303c5d799a3318"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d63e74ca7623ab80f10e293947a7e397a33f2dc5a7193c63ed303c5d799a3318"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
+    sha256 cellar: :any_skip_relocation, catalina:       "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
+    sha256 cellar: :any_skip_relocation, mojave:         "fd683adc5b4b9b05915d8df60de311c22c5e945f2f222497d887b145cd160d77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7a8893c093a81d33fdc180a79de742930db5a2c173ab7bf8a4567eff0108e75"
   end
 
   depends_on "go" => :build
