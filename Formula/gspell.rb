c9fee89fb304308d9ba52fb0156e594bc3a3a32c
Rui Chen <rui@chenrui.dev>
2021-11-05 00:25:18 -0400

    gspell: bump revision (iso-codes 4.8.0)
    Closes #88782.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gspell.rb
+++ Formula/gspell.rb
@@ -4,7 +4,7 @@ class Gspell < Formula
   url "https://download.gnome.org/sources/gspell/1.8/gspell-1.8.4.tar.xz"
   sha256 "cf4d16a716e813449bd631405dc1001ea89537b8cdae2b8abfb3999212bd43b4"
   license "LGPL-2.1-or-later"
-  revision 3
+  revision 4
 
   bottle do
     sha256 arm64_big_sur: "7dc4740a3245e28caa82c646b1e173336b3a3e2b88de1c87f002767d75938c72"
