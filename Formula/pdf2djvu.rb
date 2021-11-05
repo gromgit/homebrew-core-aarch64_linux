a102e5986c29f08c2e2fc63d9a34265d73f3c896
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-05 01:06:20 +0000

    pdf2djvu: update 0.9.18.1 bottle.

--- Formula/pdf2djvu.rb
+++ Formula/pdf2djvu.rb
@@ -7,10 +7,12 @@ class Pdf2djvu < Formula
   head "https://github.com/jwilk/pdf2djvu.git", branch: "master"
 
   bottle do
-    sha256 arm64_big_sur: "ae36c7cbed57854173c39b024efa8d1f6aadc3d5089ad5bdea63205eefbf615f"
-    sha256 big_sur:       "dccec33cd3662985d4d6605632a78a24c6e73c6c80371e17d47471193e67e1ee"
-    sha256 catalina:      "1e3d479f337cb61df898fe06bce62497cb0408b7fc924a7f463c506991aaaf0e"
-    sha256 x86_64_linux:  "d45308c4972e6175119653dded5770009b152404ed3dd407ae5c47a1ae9b4714"
+    sha256 arm64_monterey: "70f04cdd0ec4d96021281a2038d7b0681d0383db7c8746ec28a5cd57973c8c97"
+    sha256 arm64_big_sur:  "ae36c7cbed57854173c39b024efa8d1f6aadc3d5089ad5bdea63205eefbf615f"
+    sha256 monterey:       "117247d39fb8bd2c94326e5a2bd250eda403d3d35257ad183f5387a2132e3ee3"
+    sha256 big_sur:        "dccec33cd3662985d4d6605632a78a24c6e73c6c80371e17d47471193e67e1ee"
+    sha256 catalina:       "1e3d479f337cb61df898fe06bce62497cb0408b7fc924a7f463c506991aaaf0e"
+    sha256 x86_64_linux:   "d45308c4972e6175119653dded5770009b152404ed3dd407ae5c47a1ae9b4714"
   end
 
   depends_on "pkg-config" => :build
