aff2e00b45a7c961aa327c94d8dcb1cc754d930f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 18:59:09 +0000

    parquet-tools: update 1.12.0 bottle.

--- Formula/parquet-tools.rb
+++ Formula/parquet-tools.rb
@@ -8,12 +8,13 @@ class ParquetTools < Formula
   head "https://github.com/apache/parquet-mr.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "72bdfcf0a71023b65def0a02b1b581ba7a9b228ea5a1594923fddb2d1eb098d6"
-    sha256 cellar: :any_skip_relocation, monterey:      "6eab85ecd2c97e14364443687196750c0cde7dac5733eb13805d866a086b462a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c84021c2e6e9475380715420dec0edc741c59edb525e197a1c355fd3679187e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "0bb7bd347b698537ef9b8690648aa6b13c328fd3c60eec5c6614b94c8d770835"
-    sha256 cellar: :any_skip_relocation, mojave:        "17e6ad97c0d0fcab7989305eabd2b853cdc694f059f29d36cda5d94422de4e33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c27c4802070eea520a2a140569b8f0ec16591786caf4b87c7e8ccb9ee9414d15"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a0b30e5039eff7d29fd1ddfd8e8d840b23077005d7fb512144f0aaa0708f075"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "72bdfcf0a71023b65def0a02b1b581ba7a9b228ea5a1594923fddb2d1eb098d6"
+    sha256 cellar: :any_skip_relocation, monterey:       "6eab85ecd2c97e14364443687196750c0cde7dac5733eb13805d866a086b462a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c84021c2e6e9475380715420dec0edc741c59edb525e197a1c355fd3679187e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "0bb7bd347b698537ef9b8690648aa6b13c328fd3c60eec5c6614b94c8d770835"
+    sha256 cellar: :any_skip_relocation, mojave:         "17e6ad97c0d0fcab7989305eabd2b853cdc694f059f29d36cda5d94422de4e33"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c27c4802070eea520a2a140569b8f0ec16591786caf4b87c7e8ccb9ee9414d15"
   end
 
   # See https://issues.apache.org/jira/browse/PARQUET-1666
