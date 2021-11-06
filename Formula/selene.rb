2087d7ebf0dad2a0329514e9ef540f6c80a97e9d
Rui Chen <rui@chenrui.dev>
2021-11-06 01:56:18 -0400

    selene 0.15.0
    * selene 0.15.0
    * selene: add head build
    
    Closes #88878.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/selene.rb
+++ Formula/selene.rb
@@ -1,9 +1,10 @@
 class Selene < Formula
   desc "Blazing-fast modern Lua linter"
   homepage "https://kampfkarren.github.io/selene"
-  url "https://github.com/Kampfkarren/selene/archive/0.14.0.tar.gz"
-  sha256 "7accab1f2e2c32af7ca3a28da91f0f3ff89a9437a4d6008a9514f13009912fe3"
+  url "https://github.com/Kampfkarren/selene/archive/0.15.0.tar.gz"
+  sha256 "c6acce7207235924bb65e9897b229d6a904f5f7f7ef43a2a02ae4690551eae16"
   license "MPL-2.0"
+  head "https://github.com/Kampfkarren/selene.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6f7793582bed7b72842a91f23e4041eddab76229cc46309c20c4d14a75cae06c"
