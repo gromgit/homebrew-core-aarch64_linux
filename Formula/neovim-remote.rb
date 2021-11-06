6dc2280ff805a2441d508613076c53f48e7f2698
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 18:40:56 +0000

    neovim-remote: update 2.4.0 bottle.

--- Formula/neovim-remote.rb
+++ Formula/neovim-remote.rb
@@ -9,11 +9,13 @@ class NeovimRemote < Formula
   head "https://github.com/mhinz/neovim-remote.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eb32806679e25729e2106e7ee35082d41b460a49305e51402121a21a013f8924"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f94e3d59bc081404b883cd5fa4e8ddd48e33d060fc25099c0070ba52e8ac9682"
-    sha256 cellar: :any_skip_relocation, catalina:      "1bb77fdf6276df0833eed58107681ff725a0b0c6746a5e19c7c98428b903799d"
-    sha256 cellar: :any_skip_relocation, mojave:        "a08d1b3bbae29235c707c59a345ce343a6994e4f9460bbcf16f8279a2e13683e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21360429be62c3459f9181cf4ea899bd8cc077de3758f7efed86e2b7d92d3f5e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1903bb14294d18d5912c533aabfa46320f287cbd7044b279cf5d58fe5a05b7c0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb32806679e25729e2106e7ee35082d41b460a49305e51402121a21a013f8924"
+    sha256 cellar: :any_skip_relocation, monterey:       "ca93482c64d5d855eb90da32566207d939bd02c2febf7788604e67da115b11cd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f94e3d59bc081404b883cd5fa4e8ddd48e33d060fc25099c0070ba52e8ac9682"
+    sha256 cellar: :any_skip_relocation, catalina:       "1bb77fdf6276df0833eed58107681ff725a0b0c6746a5e19c7c98428b903799d"
+    sha256 cellar: :any_skip_relocation, mojave:         "a08d1b3bbae29235c707c59a345ce343a6994e4f9460bbcf16f8279a2e13683e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21360429be62c3459f9181cf4ea899bd8cc077de3758f7efed86e2b7d92d3f5e"
   end
 
   depends_on "neovim"
