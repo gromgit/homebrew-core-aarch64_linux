dde2d20465405b8d1cfd32f80175152a967ffc7d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 20:12:04 +0000

    gpsbabel: update 1.7.0_1 bottle.

--- Formula/gpsbabel.rb
+++ Formula/gpsbabel.rb
@@ -13,6 +13,7 @@ class Gpsbabel < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "5357d3b45f387798979f3cedc684fbcc67cff808472d08c5c0af62923e96d5ef"
+    sha256 cellar: :any,                 arm64_big_sur:  "c7ee4482d5aec2eb9c2a13012fc9ca66f9ffdd8d81350a2a89594ed99d27175c"
     sha256                               big_sur:        "6d5c179704f46781438a06f02a6e83c0d9e5bfa3af0f15b738af0029a4cc56af"
     sha256                               catalina:       "c71c3f2662684e9c03cac17d2f211b58ee07b3dc64ce74121922cca41d6b303c"
     sha256                               mojave:         "0a8e9cb2650be7396304d7486975e4ef82dce6e1890f8f54cbbaa0e05ab99991"
