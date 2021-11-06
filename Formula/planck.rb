b887aa84fd9f8041aa9cefe87d053e48ea53e59d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 22:18:17 +0000

    planck: update 2.25.0_2 bottle.

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -11,6 +11,7 @@ class Planck < Formula
     rebuild 1
     sha256 cellar: :any, arm64_monterey: "ae9c4d505e495c03e50facc262a0c2a4cc924707aadd380d68e72c828f8707b3"
     sha256 cellar: :any, arm64_big_sur:  "d8a3539294b4ba931761adad175e84404a95dd991f977501f89c19e533fff9a2"
+    sha256 cellar: :any, monterey:       "85198ae6acebb93fe6fc8c7f0d00911e8e3806eb06fbb73101ac712f1d9ebac1"
     sha256 cellar: :any, big_sur:        "436bb7f0481e0a6a0edeaa1abd2687c349e6314bf6139a2b8ae4e9a73978ed8e"
     sha256 cellar: :any, catalina:       "2528a360ad99d9d5031ae53138523c691e5a5ccb93da15cc44d214a9ad0d2e3b"
     sha256 cellar: :any, mojave:         "b0d1fe14b9ab71a5a18601e8e21fe3b16dc96247b877ce6842bbc7c7cae93784"
