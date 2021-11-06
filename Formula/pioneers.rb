214361a13c2301932e780e7c519a6633b5d9b9a7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 20:54:19 +0000

    pioneers: update 15.6 bottle.

--- Formula/pioneers.rb
+++ Formula/pioneers.rb
@@ -6,6 +6,7 @@ class Pioneers < Formula
 
   bottle do
     sha256 arm64_big_sur: "ef6e9c03dcc0c5d86ae0b08805352937c74d4ccaadbe4ac9aead56b5f7e0c7bb"
+    sha256 monterey:      "0e14d946b05d63aba6c15ad997bb4bc2998fe33c7293c14aec0763e8cfa89a99"
     sha256 big_sur:       "b9297939922709fc05a78ee98b24091678bf4e434bea4abfc14b089fde6e9728"
     sha256 catalina:      "c2462078e412d1e2b60108f317550690b28675da5a248de69856a558fa4e07d4"
     sha256 mojave:        "47ca43b992b0590f90758d9eef7894361c510dce5279a602decdf8ec019086f4"
