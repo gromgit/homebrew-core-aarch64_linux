7e427c542822d72e90d180347b85d4ede784a27a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 20:38:53 +0000

    kibana@6: update 6.8.12 bottle.

--- Formula/kibana@6.rb
+++ Formula/kibana@6.rb
@@ -7,6 +7,7 @@ class KibanaAT6 < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:    "18d141bdfdb6526042c13e32c270a446e67b419ae2b1cb818ffc79ebe991bd60"
     sha256 cellar: :any_skip_relocation, big_sur:     "c6851cd00abc11d5bf62fd244e601e9b9071443c545e3713077ec0cccfd8fc59"
     sha256 cellar: :any_skip_relocation, catalina:    "d13d9e35a2d55f1e316a16a6dc7662a0be783b11e0707f445902da1a6084a207"
     sha256 cellar: :any_skip_relocation, mojave:      "726bc34894f26bc18d9b1b9d756febefb03d76ef598445c406f18840554f6198"
