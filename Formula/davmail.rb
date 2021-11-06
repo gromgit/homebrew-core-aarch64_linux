0f703d61e6167f0dd349666201d9b4d8730303f1
Sean Molenaar <smillerdev@me.com>
2021-11-06 13:32:42 +0100

    davmail: add service block

--- Formula/davmail.rb
+++ Formula/davmail.rb
@@ -16,37 +16,14 @@ class Davmail < Formula
     bin.write_jar_script libexec/"davmail.jar", "davmail", "-Djava.awt.headless=true"
   end
 
-  plist_options manual: "davmail"
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>/usr/bin:/bin:/usr/sbin:/sbin:#{HOMEBREW_PREFIX}/bin</string>
-          </dict>
-          <key>KeepAlive</key>
-          <false/>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/davmail</string>
-          </array>
-          <key>StartInterval</key>
-          <integer>300</integer>
-          <key>RunAtLoad</key>
-          <true />
-          <key>StandardErrorPath</key>
-          <string>/dev/null</string>
-          <key>StandardOutPath</key>
-          <string>/dev/null</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run opt_bin/"davmail"
+    run_type :interval
+    interval 300
+    keep_alive false
+    environment_variables PATH: std_service_path_env
+    log_path "/dev/null"
+    error_log_path "/dev/null"
   end
 
   test do
