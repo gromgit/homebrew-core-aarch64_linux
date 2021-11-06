f57f58a29200f3a9dd01bdac9c98f81567fea29e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-06 18:34:29 +0000

    libvirt-glib: update 4.0.0 bottle.

--- Formula/libvirt-glib.rb
+++ Formula/libvirt-glib.rb
@@ -11,11 +11,13 @@ class LibvirtGlib < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "4d4918afe72309394ab15e98a5b15cf5c77e8027b20bc7bc7c1f0fb7524dbf78"
-    sha256 big_sur:       "9695bd9cca917eabee5eeaa038470e0a42c13767c420357ece93519958aa7653"
-    sha256 catalina:      "101d1a4bf6b4c45b49261fc97ddfb73d34a30511f6a24fc8f31c48caff8e14f4"
-    sha256 mojave:        "9a3967ba636f27cd1c923603e1df533b5edc7a7d5c90b089bf0154cd7b408b7f"
-    sha256 x86_64_linux:  "dca22d86f5c9e75e1abd763a252c0468da812032f80fae12514f57bb33023ffb"
+    sha256 arm64_monterey: "e8bfac0fafee87488e06debd73ebcc991f1d628afa2106b32161da94a41faa22"
+    sha256 arm64_big_sur:  "4d4918afe72309394ab15e98a5b15cf5c77e8027b20bc7bc7c1f0fb7524dbf78"
+    sha256 monterey:       "7c4c421cc28957cbb15e3e4335908cda7f92f720dee6b749d8a224b141633f48"
+    sha256 big_sur:        "9695bd9cca917eabee5eeaa038470e0a42c13767c420357ece93519958aa7653"
+    sha256 catalina:       "101d1a4bf6b4c45b49261fc97ddfb73d34a30511f6a24fc8f31c48caff8e14f4"
+    sha256 mojave:         "9a3967ba636f27cd1c923603e1df533b5edc7a7d5c90b089bf0154cd7b408b7f"
+    sha256 x86_64_linux:   "dca22d86f5c9e75e1abd763a252c0468da812032f80fae12514f57bb33023ffb"
   end
 
   depends_on "gobject-introspection" => :build
