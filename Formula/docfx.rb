948630a164c174dd934f0555d6bc34db61656240
Caleb Xu <calebcenter@live.com>
2021-11-07 16:38:14 -0500

    docfx 2.58.9
    * docfx 2.58.9
    * docfx: remove `arch` requirement
      This should no longer be necessary as of Homebrew/homebrew-test-bot#694.
    
    Closes #88956.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,15 +1,14 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.58.4/docfx.zip"
-  sha256 "5dc9e63ce084ad16a9f19f0e18a80194db3244f9d9af4ceab699c2729b46378d"
+  url "https://github.com/dotnet/docfx/releases/download/v2.58.9/docfx.zip"
+  sha256 "18e2277704b318d5c785681b69296aa072eca30ee5cab5b19de77ede830bbd3c"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "2d4913eb0b882949f76e6033572cb4b5f13141aa5c06d60500e1ae7b29e637ac"
   end
 
-  depends_on arch: :x86_64
   depends_on "mono"
 
   def install
