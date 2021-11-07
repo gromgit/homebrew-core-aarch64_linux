af3224570e1891f3a05e5f5e49a15546e3b30488
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-07 09:35:28 +0000

    minizinc: update 2.5.5 bottle.

--- Formula/minizinc.rb
+++ Formula/minizinc.rb
@@ -8,6 +8,7 @@ class Minizinc < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "051a8da67510e17e57371dc332cc441f80b366affce08917173b92fb9c7e047e"
+    sha256 cellar: :any,                 monterey:      "cd0204240f3bd8c69c6eb1c00e93544918b72f9581b05a67c04a63aa2c04f31e"
     sha256 cellar: :any,                 big_sur:       "65127ebb8476ee303b45624b7e7a7c9e1f6d4b740056b95c31da6350f819ada6"
     sha256 cellar: :any,                 catalina:      "921698a98271c69f66d66303be5e6852b083e4117a349f4039c482597b863039"
     sha256 cellar: :any,                 mojave:        "2ffec6708a7253410c073e8faa5e8dceb43ddee2d1002beb1032f31c8ee4a492"
