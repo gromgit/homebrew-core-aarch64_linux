044d40dfd3168b53826bea73d31d9c551c1c6fe1
Caleb Xu <calebcenter@live.com>
2021-11-07 16:41:35 -0500

    exiv2 0.27.5
    Closes #88958.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/exiv2.rb
+++ Formula/exiv2.rb
@@ -1,8 +1,8 @@
 class Exiv2 < Formula
   desc "EXIF and IPTC metadata manipulation library and tools"
   homepage "https://www.exiv2.org/"
-  url "https://www.exiv2.org/builds/exiv2-0.27.4-Source.tar.gz"
-  sha256 "84366dba7c162af9a7603bcd6c16f40fe0e9af294ba2fd2f66ffffb9fbec904e"
+  url "https://www.exiv2.org/builds/exiv2-0.27.5-Source.tar.gz"
+  sha256 "35a58618ab236a901ca4928b0ad8b31007ebdc0386d904409d825024e45ea6e2"
   license "GPL-2.0-or-later"
   head "https://github.com/Exiv2/exiv2.git"
 
