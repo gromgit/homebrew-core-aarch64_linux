1a0415ac2ba81b241c37466380e0bbe0813ee78d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-08 17:16:30 +0000

    halibut: update 1.3 bottle.

--- Formula/halibut.rb
+++ Formula/halibut.rb
@@ -7,15 +7,12 @@ class Halibut < Formula
   head "https://git.tartarus.org/simon/halibut.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "edd2a0fb184a9f0b782886d86d149dbc400fc3c32488fe089603e111147a3f88"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bbe2900f2e05ece4838ae138e287192fce7c97ed648994f15b19c91304e26154"
-    sha256 cellar: :any_skip_relocation, monterey:       "7274b17081b5c45aa8986ed7d1d02e1b0f8e95c519e8a5cf5a04bbc1798450dc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2f3dc434449de2f5d18bb5019dc62a966cba2981eb88d59d77d34b2049e44f2d"
-    sha256 cellar: :any_skip_relocation, catalina:       "e9bd74c1ab130f4abc824906bf1f73f910032a4c7c0938798f7fbab2f1346020"
-    sha256 cellar: :any_skip_relocation, mojave:         "05f0236c180aeab690979615812fb72642e7cdeaccb35ebb865a53aadb35e7c6"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "fe74b9670ae0d996a17de4a70a140365d057a83a643125dcbd16b33dacad9f6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f37d878d7f5d8a0869220fc00abdad12f298da1cdb1d2541b3f66793f749e9b4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "15128f8454d3cbfbe26c615f8a49daeeb3f7a6566be8f390370bf1b789ec3aa1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8b45d0f3af3aba76ea7e2b5e0c447f0ae7674db57330c2d03c6a6dc8e5ef9a53"
+    sha256 cellar: :any_skip_relocation, monterey:       "edef3d404fd16343e22399f1f559d6b937585e4e6dbd6f2ff2a3a954f2147cba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4e224f4d6f3757b28c0620022e925ea494c8d416db51c9306c87aba7619f7c45"
+    sha256 cellar: :any_skip_relocation, catalina:       "df8f9c9f491fe0ac2cb55271a2f578260833733dfc7ce3d9f88ee3c9c865c5ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b074c7854ab9c65b56a04c63512c77e8bae8958a2beb2f0eff3b791264fc354"
   end
 
   depends_on "cmake" => :build
