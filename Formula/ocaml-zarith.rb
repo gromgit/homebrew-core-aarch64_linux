4f61daeee2624e794ec3b6fd0d2819a779a43f42
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-08 09:41:20 +0000

    ocaml-zarith: update 1.12_1 bottle.

--- Formula/ocaml-zarith.rb
+++ Formula/ocaml-zarith.rb
@@ -7,11 +7,13 @@ class OcamlZarith < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "0c3cce93d88d87b8b128773e559179cb422b954e4454c25202b17b3445e4af0f"
-    sha256 cellar: :any,                 big_sur:       "503c0161d43c38d83ca74b3860158f16a57fa17cd65d8c5a2d7faf3c833617d6"
-    sha256 cellar: :any,                 catalina:      "3a8f9f2889bf97dc71933e7c4c2e924dcde23f89c76febce45e6e48c0324f319"
-    sha256 cellar: :any,                 mojave:        "8a5b07c6216820cf1929027f3768d0eceeca1676ff17cf5a5cd923fa34e1746d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a826c47a727dafe35794519d24236caa5c532499efc588c804fbc30be4a2b4b"
+    sha256 cellar: :any,                 arm64_monterey: "bab7865c8b5616ad638197376c590dd7bde6740c338c4729bc3c1eafbb1f2192"
+    sha256 cellar: :any,                 arm64_big_sur:  "0c3cce93d88d87b8b128773e559179cb422b954e4454c25202b17b3445e4af0f"
+    sha256 cellar: :any,                 monterey:       "84328ddca7f27827c955400cfed49f56487cd2f48de9b67fb690d1d95ad7d852"
+    sha256 cellar: :any,                 big_sur:        "503c0161d43c38d83ca74b3860158f16a57fa17cd65d8c5a2d7faf3c833617d6"
+    sha256 cellar: :any,                 catalina:       "3a8f9f2889bf97dc71933e7c4c2e924dcde23f89c76febce45e6e48c0324f319"
+    sha256 cellar: :any,                 mojave:         "8a5b07c6216820cf1929027f3768d0eceeca1676ff17cf5a5cd923fa34e1746d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a826c47a727dafe35794519d24236caa5c532499efc588c804fbc30be4a2b4b"
   end
 
   depends_on "ocaml-findlib" => :build
