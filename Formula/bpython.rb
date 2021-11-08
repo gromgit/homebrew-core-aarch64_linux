6d8dd6eb43961cd1db0bdb40b5c814148ff71b88
rui <rui@chenrui.dev>
2021-11-08 16:24:52 +0000

    bpython: update 0.22.1 bottle.

--- Formula/bpython.rb
+++ Formula/bpython.rb
@@ -9,12 +9,12 @@ class Bpython < Formula
   head "https://github.com/bpython/bpython.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3dda1057089040080a87ac13ef04b0ff24717d67ececec6f3fc393382fe1321"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0110e31bc799b06c75445bc78b53313abca5f12bfe7fe9c602a18275ea44af75"
-    sha256 cellar: :any_skip_relocation, monterey:       "cb6907e1f964e13e950f2d99cbf6e83f9d197ce2e7f3b1acb42c2496dd203fd5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8ebf72000020bb9a3093c81bcb2a903b43d034f8ab241623f10f788e4017acf3"
-    sha256 cellar: :any_skip_relocation, catalina:       "5c7a186a5a17c8a6b309290986f567beb972801795dd05e81805ea532c1ecbc9"
-    sha256 cellar: :any_skip_relocation, mojave:         "c4ad1b0670cb800236092f31acbacf7fa66d1a553968a91411cc25223fa939b4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "943fae89e278f19412351539d35e992e5401de8d1a15675dd6ec942c58c67d07"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "651508883e03d6502c7b8e2047c3bedfa0c2f863553d14f4748657879077b26b"
+    sha256 cellar: :any_skip_relocation, monterey:       "8134659e82336cb7758bb7aee16c19107283b598cdf3c5fa03a493b0279283df"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fcf7d95f3cac1b2be748e68c99f4dc2d8a4abbc24d8a43675abe3a94f1f66f0"
+    sha256 cellar: :any_skip_relocation, catalina:       "6022706aa857d60c9505e3ee5577628fd657c29cf92f8139c9e1075f9d68459f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19ef397613d4c93a53f940f6027d0354d630116da8ff645c627d8257893e3b52"
   end
 
   depends_on "python@3.10"
