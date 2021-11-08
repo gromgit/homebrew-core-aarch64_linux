0e83cc42804b47ca304907bf700d3505d4513788
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-08 13:53:16 +0800

    libxls: patch incorrect -flat_namespace usage
    Also, install a test fixture into `pkgshare`.
    
    Closes #88952.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libxls.rb
+++ Formula/libxls.rb
@@ -5,9 +5,18 @@ class Libxls < Formula
   sha256 "5dacc34d94bf2115926c80c6fb69e4e7bd2ed6403d51cff49041a94172f5e371"
   license "BSD-2-Clause"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
+    # Add program prefix `lib` to prevent conflict with another Unix tool `xls2csv`.
+    # Arch and Fedora do the same.
     system "./configure", *std_configure_args, "--disable-silent-rules", "--program-prefix=lib"
     system "make", "install"
+    pkgshare.install "test/files/test2.xls"
   end
 
   test do
@@ -22,7 +31,7 @@ class Libxls < Formula
       {
           xlsWorkBook* pWB;
           xls_error_t code = LIBXLS_OK;
-          pWB = xls_open_file(argv[1],"UTF-8", &code);
+          pWB = xls_open_file(argv[1], "UTF-8", &code);
           if (pWB == NULL) {
               return 1;
           }
@@ -37,6 +46,6 @@ class Libxls < Formula
     EOS
 
     system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-lxlsreader", "-o", "test"
-    system "./test"
+    system "./test", pkgshare/"test2.xls"
   end
 end
