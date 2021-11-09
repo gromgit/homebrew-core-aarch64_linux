653765682f053c0ed65df9f15da1aa4f8ba37c77
Rui Chen <rui@chenrui.dev>
2021-11-09 10:15:30 -0500

    minio 20211109032145
    Closes #89047.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -2,9 +2,9 @@ class Minio < Formula
   desc "High Performance, Kubernetes Native Object Storage"
   homepage "https://min.io"
   url "https://github.com/minio/minio.git",
-      tag:      "RELEASE.2021-11-05T09-16-26Z",
-      revision: "8774d10bdf25fc345e9deaf51131e3582a7f6e23"
-  version "20211105091626"
+      tag:      "RELEASE.2021-11-09T03-21-45Z",
+      revision: "f4b55ea7a70f8a065fb7065797696e815241ff93"
+  version "20211109032145"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/minio.git"
 
