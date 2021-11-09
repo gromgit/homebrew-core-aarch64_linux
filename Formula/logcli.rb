72fa47cf854f5a4650e91950f1db0142f4429764
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-09 20:09:10 +0000

    logcli: update 2.4.1 bottle.

--- Formula/logcli.rb
+++ Formula/logcli.rb
@@ -11,12 +11,12 @@ class Logcli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cba66891c11a66707b0e0c26318cd3904fcfdbfd380c889d462736fdf77dbda7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9245f7f8c3996eaeab8bb9546c49a8645b0365cc91455130456fb45caa0ab593"
-    sha256 cellar: :any_skip_relocation, monterey:       "62763515fe41081122276a3c7ec6d92fd267f1729109447ba212e35f0691c99e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c4acd8ba55f3619b2e4e2da7ce22224d65a0d07547a9b8cbed369a6167e46f79"
-    sha256 cellar: :any_skip_relocation, catalina:       "27db69c1904c15803e28a17b25304f21782da91400664ee12af4e9e513a0a167"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e49ba9447ef974bd4ca5164d603247b48800e0964c1964f7d2f5b6e1bdcaecbd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e48cb9a1712fbf6c060831ad0f9f3b34c967fc9845086d6a85a8f3fa8bcdf30"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "917553c13f5adad23133d5f382b63c7ec609c05fe2b0715dfa0a5203a72a141c"
+    sha256 cellar: :any_skip_relocation, monterey:       "c2771673935d7cdce2f5ddcffda25b7347ed885f744a07d720609b0f299e3674"
+    sha256 cellar: :any_skip_relocation, big_sur:        "edf79dc13d6ad101394a0558e95bc15deae30d337320cf4803844aa464828a8c"
+    sha256 cellar: :any_skip_relocation, catalina:       "3edb14dee5ad3db0948c8dfd0b7641d6cafed457efd78549155369762510a5aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "70b1d6a4f292a591fc876a658fc1c61755439f1d628076a96b68b5552ac2e3eb"
   end
 
   depends_on "go" => :build
