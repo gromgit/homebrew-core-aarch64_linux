df6196e033e112c01c2e1c6cee0d5a39e5fdb643
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-10 18:13:40 +0000

    msmtp: update 1.8.19 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -11,12 +11,12 @@ class Msmtp < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "4bbe3d7e3d0517d3773a511854d0132793a83744f03add396641703b5a0b70d0"
-    sha256 arm64_big_sur:  "b808aa8faa33fd35f611594e650a9b5ec993aeb579d1bae81dc1d72bca9ffff3"
-    sha256 monterey:       "2fdd667a38138fa440d9aab3357676865fa9f15ba91e3cbe030eac754d100433"
-    sha256 big_sur:        "6ee5aa8e7d52373b8249f9d1e34203beead0cc86163d96dac1854bc6648945ec"
-    sha256 catalina:       "28a120b2fda2c50a4efe60367b5fdd9a4e936f46b1adffb422499c3e610d17ac"
-    sha256 x86_64_linux:   "8337630534e09069a6ca50f4006f564058424f37ea3a4e4977c2a7b61dad184c"
+    sha256 arm64_monterey: "cc09836a4d4006b9e20e348e2489b7ff94bf3fe0bca96bad7c6524b5398e21dd"
+    sha256 arm64_big_sur:  "afdf2c4eab253086bd05d3d17bb4fd9a4e6ec5b1f7278fe045691a42d196d5b9"
+    sha256 monterey:       "5b04585e765b4eb238737921a5fd482c5c7f6cb6d50e28401088af359f7ee3a7"
+    sha256 big_sur:        "78aa3acd39b6a9ae44490a55990835836cf5b10d3f5f47180d52313f90c334f5"
+    sha256 catalina:       "49ef11f00b1ec162a5e6c0d7ab457dd6a9cbfee963bbe5a37a7c65309aaf8178"
+    sha256 x86_64_linux:   "6824e305e82046605a01143e3f3dd00ce66ec9a3a71526c24e29026668e58c99"
   end
 
   depends_on "pkg-config" => :build
