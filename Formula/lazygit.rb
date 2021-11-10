d82c908e1d480ce8f05b2305ba49e9120854bef9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-10 01:27:38 +0000

    lazygit: update 0.31.3 bottle.

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -6,12 +6,12 @@ class Lazygit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "982fb3306ac7df0c4ebab398f56d9ea032e8f5b34386ca28d5eab0f14171646d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc90dc6cf45d33fd11fbe5bb8d2131608701aad5220bff1d4c3735c16b91d06d"
-    sha256 cellar: :any_skip_relocation, monterey:       "421c6ba6905839aec280907602b669d5f4d492eb48922d33c975e52c3058adcc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e6e438306b0b3f70fc1914b4e1fefb9a09f139c41cd9146c90e686d8e40ebc5f"
-    sha256 cellar: :any_skip_relocation, catalina:       "28ef7a8965d31b39a2960ff13596e4c1c8c8ab33113d5094afbd0e65c1a39adc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab1b126bac2135036c2ff36cca40216bf7fa0a96b0702f19f3babe267dd33507"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3e1ea5dbda063c0c1cd73f89d69edb5ad7eca21b10d3f6a84936212fe0ae363"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f87e4aa569196383fb022e4085669c35fcab323486d0b9a48f42fbe2657f715a"
+    sha256 cellar: :any_skip_relocation, monterey:       "0e938f2c2db10aaf3dc6521b4c1458db34b8f40dc29400b8c6bfb6b6d3fc9479"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0193f2e973f9c6a49f484167f9fcdeea1aaaf6f37b9bf783d8a47dd481e8a00b"
+    sha256 cellar: :any_skip_relocation, catalina:       "66f40232273c5876f899c1beb8b2bd5d4ff9a12e49a4cf4e3c200ab422dda817"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "636767a737774eae2db68f67ed0be16f82758efe9c1f917b5a268b804d7c959a"
   end
 
   depends_on "go" => :build
