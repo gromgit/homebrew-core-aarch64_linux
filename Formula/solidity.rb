2ee9525d56cb74f02d3ffa20c24a2e918d3b2836
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-10 00:11:55 +0000

    solidity: update 0.8.10 bottle.

--- Formula/solidity.rb
+++ Formula/solidity.rb
@@ -11,13 +11,12 @@ class Solidity < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "667785cbdd41a5160b296fcff6016785b1eac5e4886f902d999115fc66b50397"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c9067a3ad9a391e5b44aebe95698e4c2a1dbf2a7ef9cb4d691f82ca161a14d4e"
-    sha256 cellar: :any_skip_relocation, monterey:       "75b6e2a5f9b6dd7712aadcda5338fa2d6c20b4b70df970954f49cf1702afc377"
-    sha256 cellar: :any_skip_relocation, big_sur:        "67eec440aaadb1bfa3b3e79adcc98595ccf4b20a330815463520a066c9e9d44b"
-    sha256 cellar: :any_skip_relocation, catalina:       "27e39f6e750a012dcedab5d7203e6a6a9d905fcada819345a2d1d35365f0f06e"
-    sha256 cellar: :any_skip_relocation, mojave:         "c1f61c3aaa6a0652d4cba95ebda4d7c4919f30f7ebea70ffa1223dc1092bca54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "90664bc09fd6a7836c6cff3054dce6eba730c3643c4f7ac19addcd840acbec24"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d52228020e2d58c1698737f8d3051d29aca7bea59af0acd231a59cc2018b7f41"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af37c87018dba48d0d665d1ad113ac8126c66a2ce5c4aa52aadb1609581df2da"
+    sha256 cellar: :any_skip_relocation, monterey:       "5e29f0c490124dc43f0d9db5f2119f0c2338d76631f971967614d4babe9b23d4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4226ba214336485fd23a90e2557db477b8e855c02bbe9e58f107b2e2ad1e1421"
+    sha256 cellar: :any_skip_relocation, catalina:       "e6cf6d239c4f0be695202527b99b7b30860ce67078cc799f9e1af1756268fdbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "27825ee32bc85b7a915378aec8cf470bb38718d0478b20b47898e9a5ef739fc9"
   end
 
   depends_on "cmake" => :build
