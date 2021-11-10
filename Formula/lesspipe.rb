f286ca4fb98764edb8a24db5bb48085c321cf81e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-10 06:07:39 +0000

    lesspipe: update 1.90 bottle.

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -6,12 +6,12 @@ class Lesspipe < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e1e2e8c6dfa12655279bc3a6d8869a71727f6fbc014796f9970f4b99bec879bc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e1e2e8c6dfa12655279bc3a6d8869a71727f6fbc014796f9970f4b99bec879bc"
-    sha256 cellar: :any_skip_relocation, catalina:      "e1e2e8c6dfa12655279bc3a6d8869a71727f6fbc014796f9970f4b99bec879bc"
-    sha256 cellar: :any_skip_relocation, mojave:        "e1e2e8c6dfa12655279bc3a6d8869a71727f6fbc014796f9970f4b99bec879bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0ae934aea25951e3b5903b5bfbc3698e296d7ca7b30ed492b69aa7b1486f2a4"
-    sha256 cellar: :any_skip_relocation, all:           "e1e2e8c6dfa12655279bc3a6d8869a71727f6fbc014796f9970f4b99bec879bc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d0298b0f684df89c4b309e65a275a1c00092a36fdedf8fa0322994789ce1537"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d0298b0f684df89c4b309e65a275a1c00092a36fdedf8fa0322994789ce1537"
+    sha256 cellar: :any_skip_relocation, monterey:       "5d0298b0f684df89c4b309e65a275a1c00092a36fdedf8fa0322994789ce1537"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5d0298b0f684df89c4b309e65a275a1c00092a36fdedf8fa0322994789ce1537"
+    sha256 cellar: :any_skip_relocation, catalina:       "5d0298b0f684df89c4b309e65a275a1c00092a36fdedf8fa0322994789ce1537"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b03d0989b4ab81bb0a361a1874535449545efd1b34972a2677dc6698dd08f2b0"
   end
 
   def install
