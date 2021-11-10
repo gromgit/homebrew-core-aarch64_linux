a4761299a69b6b5e94ed321dd65bef8b1b11cb53
Rui Chen <rui@chenrui.dev>
2021-11-10 01:28:04 -0500

    container-structure-test 1.11.0
    * container-structure-test 1.11.0
    * use test_resource as name
    
    Closes #89139.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/container-structure-test.rb
+++ Formula/container-structure-test.rb
@@ -1,8 +1,8 @@
 class ContainerStructureTest < Formula
   desc "Validate the structure of your container images"
   homepage "https://github.com/GoogleContainerTools/container-structure-test"
-  url "https://github.com/GoogleContainerTools/container-structure-test/archive/v1.10.0.tar.gz"
-  sha256 "52ba2ff4e948c6740da6da8804aeb674cf631e4d470ae5d78af07f17ba0ecbec"
+  url "https://github.com/GoogleContainerTools/container-structure-test/archive/v1.11.0.tar.gz"
+  sha256 "38bf793106da6cf7bfead49fae2a75c51609a304de4580af099eb57c5a018761"
   license "Apache-2.0"
   head "https://github.com/GoogleContainerTools/container-structure-test.git", branch: "master"
 
@@ -19,7 +19,7 @@ class ContainerStructureTest < Formula
   depends_on "go" => :build
 
   # Small Docker image to run tests against
-  resource "busybox-image-tar" do
+  resource "test_resource" do
     url "https://gist.github.com/AndiDog/1fab301b2dbc812b1544cd45db939e94/raw/5160ab30de17833fdfe183fc38e4e5f69f7bbae0/busybox-1.31.1.tar", using: :nounzip
     sha256 "ab5088c314316f39ff1d1a452b486141db40813351731ec8d5300db3eb35a316"
   end
@@ -52,7 +52,7 @@ class ContainerStructureTest < Formula
       --config test.yml
     ].join(" ")
 
-    resource("busybox-image-tar").stage testpath
+    resource("test_resource").stage testpath
     json_text = shell_output("#{bin}/container-structure-test test #{args}")
     res = JSON.parse(json_text)
     assert_equal res["Pass"], 2
