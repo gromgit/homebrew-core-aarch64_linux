b49f61380e607b4ece2c0f5f28766ca34958b65a
Rui Chen <rui@chenrui.dev>
2021-11-09 20:22:48 -0500

    ipv6calc 4.0.0
    Closes #89088.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ipv6calc.rb
+++ Formula/ipv6calc.rb
@@ -1,8 +1,12 @@
+require "language/perl"
+
 class Ipv6calc < Formula
+  include Language::Perl::Shebang
+
   desc "Small utility for manipulating IPv6 addresses"
   homepage "https://www.deepspace6.net/projects/ipv6calc.html"
-  url "https://github.com/pbiering/ipv6calc/archive/3.2.0.tar.gz"
-  sha256 "c73e1488a344d5ce3acdd009fb48068eec1bdf7562698011bdbbc7aaf33aa8f7"
+  url "https://github.com/pbiering/ipv6calc/archive/4.0.0.tar.gz"
+  sha256 "4d23c471b472271b48421b5d18309492f615c85c75f2abc17c5c5a4d8e3a4635"
   license "GPL-2.0-only"
 
   bottle do
@@ -15,7 +19,46 @@ class Ipv6calc < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "748772297ce211e30da123ccab4c9204738703e62cfe1ab90238dac434d9fb70"
   end
 
+  uses_from_macos "perl"
+
+  on_linux do
+    resource "URI::Escape" do
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/URI-1.72.tar.gz"
+      sha256 "35f14431d4b300de4be1163b0b5332de2d7fbda4f05ff1ed198a8e9330d40a32"
+    end
+
+    resource "HTML::Entities" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTML-Parser-3.76.tar.gz"
+      sha256 "64d9e2eb2b420f1492da01ec0e6976363245b4be9290f03f10b7d2cb63fa2f61"
+    end
+
+    resource "DIGEST::Sha1" do
+      url "https://cpan.metacpan.org/authors/id/G/GA/GAAS/Digest-SHA1-2.13.tar.gz"
+      sha256 "68c1dac2187421f0eb7abf71452a06f190181b8fc4b28ededf5b90296fb943cc"
+    end
+  end
+
   def install
+    if OS.linux?
+      ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+      ENV.prepend_path "PERL5LIB", libexec/"lib"
+
+      resources.each do |r|
+        r.stage do
+          system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+          system "make", "install"
+        end
+      end
+
+      rewrite_shebang detected_perl_shebang, "ipv6calcweb/ipv6calcweb.cgi.in"
+
+      # ipv6calcweb.cgi is a CGI script so it does not use PERL5LIB
+      # Add the lib path at the top of the file
+      inreplace "ipv6calcweb/ipv6calcweb.cgi.in",
+                "use URI::Escape;",
+                "use lib \"#{libexec}/lib/perl5/\";\nuse URI::Escape;"
+    end
+
     # This needs --mandir, otherwise it tries to install to /share/man/man8.
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make"
