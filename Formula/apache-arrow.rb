c774ab9c89988fb1d388b67ca3f09155a035919e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-10 16:13:32 +0000

    apache-arrow: update 6.0.0_1 bottle.

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -9,12 +9,11 @@ class ApacheArrow < Formula
   head "https://github.com/apache/arrow.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "75d0dd9c4e90cf0187aa6c9eb4c6d17f2cd45790f25631cb0716cf900e1f3db3"
-    sha256 cellar: :any,                 monterey:      "de806adac05b6ce14c2cc5e270b5db1584be616da8210dbcbbe88aeb72af9f43"
-    sha256 cellar: :any,                 big_sur:       "626c83aa2218365f537dc2159f7b97f87dfe772105e85ceda3fef3e2976e412d"
-    sha256 cellar: :any,                 catalina:      "c02e5d8829d4b3fd28f601c594107c4fb6a83d1c1fe7b4abae5dfcefccb5bf55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ca1305cb5335250312a597d489717bb8066ef1ac5343ab9dfd59349b3eadfdf5"
+    sha256 cellar: :any,                 arm64_big_sur: "494c0bfdea7ce33f6a5364c8f34cddeb44adbcd21dbb9a775975f673583e8b16"
+    sha256 cellar: :any,                 monterey:      "5ee9003399039af46b04d38b0c440b17a07660247ab625f081a1b79eb81ab474"
+    sha256 cellar: :any,                 big_sur:       "81bf1f874821856da24e0070757189d26ac548cea0b0820e6e169d3747f2db33"
+    sha256 cellar: :any,                 catalina:      "73123aeebf68ac6c5d7c619415549d9512f8e29a51b9e212e42504660d38fe87"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3cb1bb7687850f34f0c895353b873f99916c0aa4ef96580af530f22b21ed5aec"
   end
 
   depends_on "boost" => :build
