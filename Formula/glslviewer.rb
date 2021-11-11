b71c28d919bc4dd9c33c3a7f0bd38f80cfd32a3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-11 22:07:53 +0000

    glslviewer: update 1.7.0_1 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -8,13 +8,12 @@ class Glslviewer < Formula
   head "https://github.com/patriciogonzalezvivo/glslViewer.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0e68e4c1b610530c7b969538eff09d65c9a7b1ab3ef9d23eae309acd66e97916"
-    sha256 cellar: :any,                 arm64_big_sur:  "0ec97c2e9044bbfd60b9e3a351ac776f2cda6ac13cf9a4bc126d922e2da2d253"
-    sha256 cellar: :any,                 monterey:       "8f0c6bbe1e1f2ecf1e6f0ad4badf62fadcc9189971f622e6b06886b2d306cc9d"
-    sha256 cellar: :any,                 big_sur:        "bcaaa427f4cfaf2736c995c24235be606a8e0e83cbaaf495097f684d2f7de069"
-    sha256 cellar: :any,                 catalina:       "17f665c2d066a6a01023300ed8a1fbad50ef078503978b3e7b4db63e6d483aba"
-    sha256 cellar: :any,                 mojave:         "94c59b694e9feeeb388aaf5aee69c387e4ea02348f178e34b715167b3af636af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "abdda11e8c83614d9af904e946ca537fb5ae9d7333ee9a7d618ed3f9585ce81f"
+    sha256 cellar: :any,                 arm64_monterey: "e1031838f5fa625dd142dd058a812aed71f7f7696fff345b46bec303a66a46df"
+    sha256 cellar: :any,                 arm64_big_sur:  "3fcd463c03fe4174fdf809d7fce0ec612e1148241d1974ce6424a47a8624e894"
+    sha256 cellar: :any,                 monterey:       "5a800f933f5c444247b7f2644578a0cfbd8c283853bee15387769cf6146268be"
+    sha256 cellar: :any,                 big_sur:        "3af1d9e5b62064c8e62f858f5f57759e3078c4f266650aaae5ef6d32d9d3e789"
+    sha256 cellar: :any,                 catalina:       "98b9ccf60ad6b4b7cb614064a6c83a4b531cf10949d1d26a5107902a0cb3ac63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9c5f389e38491eb889fdd306f89e07e2f54c4e75c2454290f31347231c31c1fa"
   end
 
   depends_on "pkg-config" => :build
