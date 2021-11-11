64ec5334bf1c88a48eb77f92b268e6abe9638c11
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-11 11:09:03 +0000

    libtasn1: update 4.18.0 bottle.

--- Formula/libtasn1.rb
+++ Formula/libtasn1.rb
@@ -7,13 +7,12 @@ class Libtasn1 < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2d7ada7beb7876eeac65e86dd5597866755efa26bf03e81540ef534a7c242bfa"
-    sha256 cellar: :any,                 arm64_big_sur:  "8b8ba758c8cb70a6970c69801937090ca5bc3fb68f9fb5358f2183299fbee3fa"
-    sha256 cellar: :any,                 monterey:       "25f3cab11b9113d29e8669a98cae5565b339a01118e72e33c652550302f51ffd"
-    sha256 cellar: :any,                 big_sur:        "a045a7b16828e7c18bad248feb37207815daea23ff313c1cd6e94e94b222bb73"
-    sha256 cellar: :any,                 catalina:       "0b0b6a4b18ff4aef03772082d153be836b406af22e678a0b61553c70fdf95f10"
-    sha256 cellar: :any,                 mojave:         "73201deae5a10cd4b4125b4a7c811f48ed861cd6fe2f82d69fed6003115d7bef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac4599c1f1a9b4858cbf688524cbdec6c412004b447102c034662781e0d96481"
+    sha256 cellar: :any,                 arm64_monterey: "f85aa10d1320087405fd8b5c17593c6238bac842c6714edd09194f28e8e9f25d"
+    sha256 cellar: :any,                 arm64_big_sur:  "1d79a03efd060b5bca10609a841f44f1444e2bb7bab149e3bb624b6bf5806418"
+    sha256 cellar: :any,                 monterey:       "ddaf57bdd4f54323ae30093fbfe3aef9758423b03caf2c4757d592c7c7418e08"
+    sha256 cellar: :any,                 big_sur:        "dc2e936192de2c028feb09681b615d0858d8f29a740ed0cdd0fc50a7265fd363"
+    sha256 cellar: :any,                 catalina:       "2c8cc66c96bdbb7e02304cd00fe80d3e5decef3a03cace1827e9de6e1dd0397c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9964622a7285ce7dbcdc713804d18fc9de0ce1db5037198de69b91fdb3a7f062"
   end
 
   def install
