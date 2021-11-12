08eda30ffb87a6c5e687b5ceb4245c378975aabf
Rui Chen <rui@chenrui.dev>
2021-11-11 19:43:59 -0500

    cosign 1.3.1
    * cosign 1.3.1
    * update build
      relates to sigstore/cosign#1010
    
    Closes #89231.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -2,8 +2,8 @@ class Cosign < Formula
   desc "Container Signing"
   homepage "https://github.com/sigstore/cosign"
   url "https://github.com/sigstore/cosign.git",
-      tag:      "v1.3.0",
-      revision: "a91aa202a01b830dafa969bb46f168e9c44580bd"
+      tag:      "v1.3.1",
+      revision: "645ebf09fc555762a0494baa30edc08c38435368"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
@@ -19,7 +19,7 @@ class Cosign < Formula
   depends_on "go" => :build
 
   def install
-    pkg = "github.com/sigstore/cosign/cmd/cosign/cli/options"
+    pkg = "github.com/sigstore/cosign/pkg/version"
     ldflags = %W[
       -s -w
       -X #{pkg}.GitVersion=#{version}
