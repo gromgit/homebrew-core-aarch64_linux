5e93416ec271eec8f345cd3cf6da5876a969571a
Rui Chen <rui@chenrui.dev>
2021-11-11 19:44:20 -0500

    devspace 5.17.0
    Closes #89232.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.sh/"
   url "https://github.com/loft-sh/devspace.git",
-      tag:      "v5.16.2",
-      revision: "0a34b1427d12992bec25e55f9bdf72e54baddbaa"
+      tag:      "v5.17.0",
+      revision: "ac2f19c286043a623dcadd2df16a623bb0713781"
   license "Apache-2.0"
   head "https://github.com/loft-sh/devspace.git"
 
