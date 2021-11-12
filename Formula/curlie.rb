b7c042bfad43e2364719ef66d00e6a9167934764
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-12 19:07:19 +0000

    curlie: update 1.6.7 bottle.

--- Formula/curlie.rb
+++ Formula/curlie.rb
@@ -6,13 +6,12 @@ class Curlie < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "38833a4eb448889240f2a5dbf9627c1f3253e60b284385d6778e32557da8a09c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c765bf3a15919c4452fb9cf470f30f274ecc77c39a1033e262aca4306b799981"
-    sha256 cellar: :any_skip_relocation, monterey:       "53b67f688bfc1e16d4eae82601f81825167f3ff6a9bc31731e519efe2de53221"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b9e22d8ea5d1459740bc0c0f7a8eac471fd81dc4411af99659fd0d7a36583922"
-    sha256 cellar: :any_skip_relocation, catalina:       "b9e22d8ea5d1459740bc0c0f7a8eac471fd81dc4411af99659fd0d7a36583922"
-    sha256 cellar: :any_skip_relocation, mojave:         "b9e22d8ea5d1459740bc0c0f7a8eac471fd81dc4411af99659fd0d7a36583922"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88e6f1236f129fd2268636d89dad87549739e55050fa23a966fbba5e33517aa4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5e258bc851da6382ecd91feeb7a3ee6be43521040e28a1127bf988bf3fecdcf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f5e258bc851da6382ecd91feeb7a3ee6be43521040e28a1127bf988bf3fecdcf"
+    sha256 cellar: :any_skip_relocation, monterey:       "ace5b733c4cc5290044ef382c42d72c67ad813fded0d3cbafba7aacf66dc9df1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ace5b733c4cc5290044ef382c42d72c67ad813fded0d3cbafba7aacf66dc9df1"
+    sha256 cellar: :any_skip_relocation, catalina:       "ace5b733c4cc5290044ef382c42d72c67ad813fded0d3cbafba7aacf66dc9df1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69aa5acebd2adddc5fbd0058aa8acafb510e9f6988552c459f13cf4a63669bd1"
   end
 
   depends_on "go" => :build
