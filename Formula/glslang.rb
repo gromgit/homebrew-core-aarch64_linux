6481c29a541a736b9d983d72ae9a7d9a0b152699
Rui Chen <rui@chenrui.dev>
2021-11-12 00:36:29 -0500

    glslang 11.7.0
    Closes #89259.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -1,8 +1,8 @@
 class Glslang < Formula
   desc "OpenGL and OpenGL ES reference compiler for shading languages"
   homepage "https://www.khronos.org/opengles/sdk/tools/Reference-Compiler/"
-  url "https://github.com/KhronosGroup/glslang/archive/11.6.0.tar.gz"
-  sha256 "99ecd3a0c2c2219293d76723846f762a9f3e7dd0dc2a4f346d0fc3a05a0ce000"
+  url "https://github.com/KhronosGroup/glslang/archive/11.7.0.tar.gz"
+  sha256 "b6c83864c3606678d11675114fa5f358c519fe1dad9a781802bcc87fb8fa32d5"
   head "https://github.com/KhronosGroup/glslang.git"
 
   livecheck do
