52313851cdc1fa1bb4e6fd3125e37da3bed5b6d9
Rui Chen <rui@chenrui.dev>
2021-11-11 19:45:52 -0500

    mmark 2.2.19
    * mmark 2.2.19
    * update resource
    
    Closes #89236.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mmark.rb
+++ Formula/mmark.rb
@@ -1,8 +1,8 @@
 class Mmark < Formula
   desc "Powerful markdown processor in Go geared towards the IETF"
   homepage "https://mmark.miek.nl/"
-  url "https://github.com/mmarkdown/mmark/archive/v2.2.17.tar.gz"
-  sha256 "e40ef682b3cdc7a479cfafad3e70261194183eb302c072485e0d656ea3c2963b"
+  url "https://github.com/mmarkdown/mmark/archive/v2.2.19.tar.gz"
+  sha256 "1f24ef62fcdf3bbe36880aef4f8b5f4cc623884f399200987aa1b1be9a5a5326"
   license "BSD-2-Clause"
 
   bottle do
@@ -17,7 +17,7 @@ class Mmark < Formula
   depends_on "go" => :build
 
   resource "test" do
-    url "https://raw.githubusercontent.com/mmarkdown/mmark/v2.2.17/rfc/2100.md"
+    url "https://raw.githubusercontent.com/mmarkdown/mmark/v2.2.19/rfc/2100.md"
     sha256 "0e12576b4506addc5aa9589b459bcc02ed92b936ff58f87129385d661b400c41"
   end
 
