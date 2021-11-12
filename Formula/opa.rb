0d823ba809a1c8b9b23cee59d8bf9a6cb78d86bb
Rui Chen <rui@chenrui.dev>
2021-11-11 19:47:29 -0500

    opa 0.34.2
    Closes #89240.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -1,8 +1,8 @@
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/v0.34.1.tar.gz"
-  sha256 "2c720732a0569122719fc653956b66f9af300a4d745bc4f56ee2c1c27a3d569b"
+  url "https://github.com/open-policy-agent/opa/archive/v0.34.2.tar.gz"
+  sha256 "99799081fb227ef79255c04b8c49d299bfd57c277105092e406d7be3331c1581"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
