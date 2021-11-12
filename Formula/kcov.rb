d715552bae50919576ab9fa15da84d303c25deac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-12 13:14:23 +0000

    kcov: update 39 bottle.

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -14,14 +14,12 @@ class Kcov < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "0c6ecf2b03d6aebbf4d4a3a54fee80a7293050ae7bb596a96275f804a3a33103"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "32aab6003ab8ca0cf45e55415ae5b78e6fc59f5e02fa1d84397df1be17261230"
-    sha256 cellar: :any,                 monterey:       "fa475a23f3e008465d4d4b96fa29b1ba7531f75efed0b670ab596a9099202738"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e8181653c02129bd63fd438d3e5965cd25d5408ebe7c6699da2ab86b9a9f4340"
-    sha256 cellar: :any_skip_relocation, catalina:       "becb863fd482145cd67dab0b25df128b5deba598fa89a217cd6ff63ba79edbc5"
-    sha256 cellar: :any_skip_relocation, mojave:         "dd72670ef6f9abd9b44bf70b8fcb64faffe1ba4edfd4704c70de395d3594dc89"
-    sha256                               x86_64_linux:   "d06adbd0e486fd113be4eeb75322e8cad6b63091c39d4bb85562278bde569e76"
+    sha256 arm64_monterey: "75952c1c6810265bc4031f959607496a47f49038ea2e6a4491e5f727e323298a"
+    sha256 arm64_big_sur:  "93ce6e77263bed79a9a53e1e3a8426f91856ae2a431b5d77a249aab30c97fa8c"
+    sha256 monterey:       "949ade6c09d7562e5ff39404429ea97f47156e05e6f8fc433e233281464d016b"
+    sha256 big_sur:        "5c36eeea756c530f7ebe1af690d5ce74fd873ab85e7b7403f0987e1d12dd76a2"
+    sha256 catalina:       "d5f288a2e47be17f364f28140403a77d4cd926fe412480a6485fb396c996d65a"
+    sha256 x86_64_linux:   "00d00da79f43b56559291fc131a55090875321bd279b0065382aa27fc9db04f2"
   end
 
   depends_on "cmake" => :build
