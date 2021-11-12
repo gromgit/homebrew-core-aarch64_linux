febc57ecbec03b130c38b3e38ee10ecbdb168871
Rui Chen <rui@chenrui.dev>
2021-11-11 19:48:16 -0500

    staticcheck 2021.1.2
    Closes #89242.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -1,8 +1,8 @@
 class Staticcheck < Formula
   desc "State of the art linter for the Go programming language"
   homepage "https://staticcheck.io/"
-  url "https://github.com/dominikh/go-tools/archive/2021.1.1.tar.gz"
-  sha256 "25e87964e818adf05c3654079a73adf91457a176a75f92bb282158d0a9994bfe"
+  url "https://github.com/dominikh/go-tools/archive/2021.1.2.tar.gz"
+  sha256 "c3fcadc203e20bc029abc9fc1d97b789de4e90dd8164e45489ec52f401a2bfd0"
   license "MIT"
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
