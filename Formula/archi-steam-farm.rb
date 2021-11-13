ea38ff331e50ddfaed799c0433369a4dcb79a5a2
Caleb Xu <calebcenter@live.com>
2021-11-12 21:12:36 -0500

    archi-steam-farm 5.1.5.3
    Closes #89307.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -2,8 +2,8 @@ class ArchiSteamFarm < Formula
   desc "Application for idling Steam cards from multiple accounts simultaneously"
   homepage "https://github.com/JustArchiNET/ArchiSteamFarm"
   url "https://github.com/JustArchiNET/ArchiSteamFarm.git",
-      tag:      "5.1.4.0",
-      revision: "79fb4da9a69371f2c8d7ea6dac8333cd25efc618"
+      tag:      "5.1.5.3",
+      revision: "cc59875769dbc3e525e000a500da888b02785abb"
   license "Apache-2.0"
   head "https://github.com/JustArchiNET/ArchiSteamFarm.git", branch: "main"
 
