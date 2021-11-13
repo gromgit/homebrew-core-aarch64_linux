d5670f1a87b8391fe45529292ee00399de630858
Caleb Xu <calebcenter@live.com>
2021-11-12 20:34:22 -0500

    elfutils 0.186
    Closes #89303.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elfutils.rb
+++ Formula/elfutils.rb
@@ -1,8 +1,8 @@
 class Elfutils < Formula
   desc "Libraries and utilities for handling ELF objects"
   homepage "https://fedorahosted.org/elfutils/"
-  url "https://sourceware.org/elfutils/ftp/0.185/elfutils-0.185.tar.bz2"
-  sha256 "dc8d3e74ab209465e7f568e1b3bb9a5a142f8656e2b57d10049a73da2ae6b5a6"
+  url "https://sourceware.org/elfutils/ftp/0.186/elfutils-0.186.tar.bz2"
+  sha256 "7f6fb9149b1673d38d9178a0d3e0fb8a1ec4f53a9f4c2ff89469609879641177"
   license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-only"]
 
   livecheck do
