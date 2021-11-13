aba8499163724f6efb29d21aa1c10465908a0a0a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-13 22:09:54 +0000

    timidity: update 2.15.0_1 bottle.

--- Formula/timidity.rb
+++ Formula/timidity.rb
@@ -12,10 +12,12 @@ class Timidity < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "b6a5b9258ca86e58a8f535a3d7d2c8c51faf608df5bc119b37d99dccfb549142"
-    sha256 big_sur:       "513868c11a5ecbc1b8044eea517c19490858173d6b61f0245c54f9b061956237"
-    sha256 catalina:      "31a2aaefcf9e293bbfce210de4a0521bdf6df205f4fb5bb009f98ad1c01bd6f1"
-    sha256 mojave:        "9dec67aa3004c6ad228dd143eea25c2db9fc568269cae1f80320c00addc3c782"
+    sha256 arm64_monterey: "207353939838f83aec0c2fd6f68363f7f961f7f08d69f317aecfece613732583"
+    sha256 arm64_big_sur:  "b6a5b9258ca86e58a8f535a3d7d2c8c51faf608df5bc119b37d99dccfb549142"
+    sha256 monterey:       "61d1189c1afa7ca17680f8e8bcfc4f5277f9e30e7b2e47f89a246714606059e3"
+    sha256 big_sur:        "513868c11a5ecbc1b8044eea517c19490858173d6b61f0245c54f9b061956237"
+    sha256 catalina:       "31a2aaefcf9e293bbfce210de4a0521bdf6df205f4fb5bb009f98ad1c01bd6f1"
+    sha256 mojave:         "9dec67aa3004c6ad228dd143eea25c2db9fc568269cae1f80320c00addc3c782"
   end
 
   depends_on "autoconf" => :build
