0bb01ab0db14775a4d1e1016e34d7ef9118a59f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-13 04:09:26 +0000

    igv: update 2.11.3 bottle.

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -11,12 +11,7 @@ class Igv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bca9700f55959cc195610c67dd83fd6644c2ac8966d90df476113d3793414b9b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bca9700f55959cc195610c67dd83fd6644c2ac8966d90df476113d3793414b9b"
-    sha256 cellar: :any_skip_relocation, catalina:      "bca9700f55959cc195610c67dd83fd6644c2ac8966d90df476113d3793414b9b"
-    sha256 cellar: :any_skip_relocation, mojave:        "bca9700f55959cc195610c67dd83fd6644c2ac8966d90df476113d3793414b9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "977f5d66ce9c94de6648ac1c71006de38da3d912495598b178e0ccad35a53ceb"
-    sha256 cellar: :any_skip_relocation, all:           "5db8b9741ae2a714546427260cc426d89d45018f7abe0b400f9acd121506d38d"
+    sha256 cellar: :any_skip_relocation, all: "22d591a38654ada55c259082571fcac1cfb1ac29bf117c8a5a85b41497b405c6"
   end
 
   depends_on "openjdk"
