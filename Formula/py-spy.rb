b5c41f5a212a979d331b53628963e7af6692ed3f
messense <messense@icloud.com>
2021-11-13 11:25:47 +0800

    py-spy 0.3.11
    Closes #89318.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/py-spy.rb
+++ Formula/py-spy.rb
@@ -1,8 +1,8 @@
 class PySpy < Formula
   desc "Sampling profiler for Python programs"
   homepage "https://github.com/benfred/py-spy"
-  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.10.tar.gz"
-  sha256 "6c07096d227c08dee8a6526ff0a4ceadbe11eb27a131ab147e9d82dde52f3177"
+  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.11.tar.gz"
+  sha256 "094cfb80e2c099763453fc39cfa9c46cfa423afa858268c6a7bc0d867763b014"
   license "MIT"
   head "https://github.com/benfred/py-spy.git", branch: "master"
 
