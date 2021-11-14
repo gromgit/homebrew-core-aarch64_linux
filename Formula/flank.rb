c43244c54966461fec65f4efbed8d45a33be7bd8
Rui Chen <rui@chenrui.dev>
2021-11-14 18:30:37 -0500

    flank 21.11.0
    Closes #89400.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,8 +1,8 @@
 class Flank < Formula
   desc "Massively parallel Android and iOS test runner for Firebase Test Lab"
   homepage "https://github.com/Flank/flank"
-  url "https://github.com/Flank/flank/releases/download/v21.09.0/flank.jar"
-  sha256 "ca38bcb1ac0cdd70d165cef938b9d849007f6d79445ce5822ab488d7d0309dbf"
+  url "https://github.com/Flank/flank/releases/download/v21.11.0/flank.jar"
+  sha256 "633d6a860af2113c5dff21022a5f426ae13003df72bd4b5be27c7e8d2f46924f"
   license "Apache-2.0"
 
   livecheck do
