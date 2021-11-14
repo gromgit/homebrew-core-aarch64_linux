875bfc256b29d577c720b652c05e1676954a19c3
Rui Chen <rui@chenrui.dev>
2021-11-14 18:55:05 -0500

    iproute2 5.15.0
    Closes #89408.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/iproute2.rb
+++ Formula/iproute2.rb
@@ -1,8 +1,8 @@
 class Iproute2 < Formula
   desc "Linux routing utilities"
   homepage "https://wiki.linuxfoundation.org/networking/iproute2"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.14.0.tar.xz"
-  sha256 "210fa785a52f3763c4287fd5ae63e246f6311bfaa48c424baab6d383bb7591d4"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.15.0.tar.xz"
+  sha256 "38e3e4a5f9a7f5575c015027a10df097c149111eeb739993128e5b2b35b291ff"
   license "GPL-2.0-only"
   head "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git", branch: "main"
 
