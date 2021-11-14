7043c907e732f994a1ae84d3efdd40a4f8429e16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-14 05:07:33 +0000

    libtcod: update 1.19.0 bottle.

--- Formula/libtcod.rb
+++ Formula/libtcod.rb
@@ -6,12 +6,11 @@ class Libtcod < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "eb25fc002f492d38ed3a8acefa9b2c8af6b538b0c61e8d7ba76456c161a0902b"
-    sha256 cellar: :any, arm64_big_sur:  "1fab6b1ff1ee7f9ab0ded5b89867f4250b882445ffef5c73b20348227abdd00f"
-    sha256 cellar: :any, monterey:       "d7cb93d38458e8d83b7b61b6dfb5c65e16965572cb40f0e60d9b3809a8bfabde"
-    sha256 cellar: :any, big_sur:        "0110681440d6825ef184dea27219a190126ab176d5e278c5d27c2e38c33444f5"
-    sha256 cellar: :any, catalina:       "55e5e93eb87f3e7b7df2fdaa497b8888bf899139b4cad0cb1c1790eeed77bb6b"
-    sha256 cellar: :any, mojave:         "b9b4bd6b64f52cac8e09e7263caed4bad328eae3a58e3e43cba7b50a957c1371"
+    sha256 cellar: :any, arm64_monterey: "8e3b9a6286afa8f99a943808d73b4b168124a7bfd97b9d3d3b6288d24ea013cd"
+    sha256 cellar: :any, arm64_big_sur:  "f6fb5f18b6567e290ff6e35ad4aaa178839566856003bf2218a1e1c96f6ac3db"
+    sha256 cellar: :any, monterey:       "7c50042db8c573c9d014a95141110a866af161381fcae7efd69e22a9c469e714"
+    sha256 cellar: :any, big_sur:        "d14e704c0eb78e5251a059688da572e52a3a8673e655ae7da1ca48ddb704bb59"
+    sha256 cellar: :any, catalina:       "e6cfce5f2fe46470b24de30b62a463ad94e020cafa4497cee5ea2a9c85e37bed"
   end
 
   depends_on "autoconf" => :build
