6fd334aa7fa25e81f30f39a1f19df7dd870993ae
Rui Chen <rui@chenrui.dev>
2021-11-14 18:10:25 -0500

    diskus 0.7.0
    Closes #89391.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/diskus.rb
+++ Formula/diskus.rb
@@ -1,8 +1,8 @@
 class Diskus < Formula
   desc "Minimal, fast alternative to 'du -sh'"
   homepage "https://github.com/sharkdp/diskus"
-  url "https://github.com/sharkdp/diskus/archive/v0.6.0.tar.gz"
-  sha256 "661687edefa3218833677660a38ccd4e2a3c45c4a66055c5bfa4667358b97500"
+  url "https://github.com/sharkdp/diskus/archive/v0.7.0.tar.gz"
+  sha256 "64b1b2e397ef4de81ea20274f98ec418b0fe19b025860e33beaba5494d3b8bd1"
   license "Apache-2.0"
 
   bottle do
