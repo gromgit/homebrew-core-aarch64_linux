6dee70a39a33d33913541b27face90f4b7368055
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-14 09:07:29 +0000

    kompose: update 1.26.0 bottle.

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -6,12 +6,12 @@ class Kompose < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c9266653cd2e111ce17a5371cddcc28685b16119deb0af44c51c74e3c47a134b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2c00024b499c64df27a4374addb70eaf258e157c1e0b15be7ab504909a22f3e8"
-    sha256 cellar: :any_skip_relocation, monterey:       "95174f14a2788d635ae891a3f31e3be16cf8fa31146ef6692c00c1612059ed96"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f04abb2515153ffaf972a197e9cdc771ba536cf33d48f82459a5639bba1ed56a"
-    sha256 cellar: :any_skip_relocation, catalina:       "830dec446da4081084c5143bbee3408264ddf755337c0f0ba77f24016dc6a29a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6144b420c60897faf0562bc27808875351f7478c53c8d1bbfe74aa310a1f4b4a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3fae1e781bebf43f1bcfe8935e2016cdcf3908ed4560c2700868d3d7ce281a34"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "11159ca2fd486a6a13e415f8b0104ce67b81bf849a1209531ee91b106e24f966"
+    sha256 cellar: :any_skip_relocation, monterey:       "9d9abadd8858189a27d40c7586eaf252ea20966e336d924195ffd2d390e51994"
+    sha256 cellar: :any_skip_relocation, big_sur:        "54124d3c5933c7644cf9ad53ec10c8014ad6ad3e031907438a9cf5afa0018e64"
+    sha256 cellar: :any_skip_relocation, catalina:       "63f80ee2b91e4e796ec3e334759f167f2ccb656f3cfd464fe0b862964f518797"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "387e670a1b49be509660f5001542d44f01c512fb2252595e6c2fd06c17db7992"
   end
 
   depends_on "go" => :build
