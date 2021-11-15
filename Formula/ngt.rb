debe5ed38bf776b56f6fe008f8d0adc89ec5a5a2
Rui Chen <rui@chenrui.dev>
2021-11-14 21:37:06 -0500

    ngt 1.13.7
    Closes #89430.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -1,8 +1,8 @@
 class Ngt < Formula
   desc "Neighborhood graph and tree for indexing high-dimensional data"
   homepage "https://github.com/yahoojapan/NGT"
-  url "https://github.com/yahoojapan/NGT/archive/v1.13.5.tar.gz"
-  sha256 "73adada23380317f0cad8cc11325036232551eefa228e1816f62f527bd599665"
+  url "https://github.com/yahoojapan/NGT/archive/v1.13.7.tar.gz"
+  sha256 "d5fb51849eead3d4d442b1b6d352e2114bcc1bddea5b4578a06a19285fc5f224"
   license "Apache-2.0"
 
   livecheck do
