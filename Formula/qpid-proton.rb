b74648ed41e06bea6e0980d5eed5afa4de2621ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-15 07:08:13 +0000

    qpid-proton: update 0.36.0 bottle.

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -8,12 +8,11 @@ class QpidProton < Formula
   head "https://gitbox.apache.org/repos/asf/qpid-proton.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "5afee190e5de08d145434f129bbc6d0fb152a6dd09ed074e4b0a52f8a1e96d4c"
-    sha256 cellar: :any, arm64_big_sur:  "5771a4a81c0ddaae9e6c8f1620c59c250d96af2eab7891989afa56117d2458d0"
-    sha256 cellar: :any, monterey:       "af60db5847bded1b78802125b5703ab47e38cb67b033749938d27b9c13d8bd6a"
-    sha256 cellar: :any, big_sur:        "7a3a7e1b0e3345e8a818790d1998f0cd23e8b35a4dfeec7e7ba351bd1074949d"
-    sha256 cellar: :any, catalina:       "7a5eb027bc0c1a91fbadc053db6a6aec8d2bda10026fe9e77b0bdbee072e9cdd"
-    sha256 cellar: :any, mojave:         "e14be728833288e29ac4b153e1b99d398a9ea0cdef1aef7665dc62c358892ea9"
+    sha256 cellar: :any, arm64_monterey: "987a082ca3bd1ff2ea46cf92abad14a4281fef6c3741e4e0598890b739c26180"
+    sha256 cellar: :any, arm64_big_sur:  "c930656951faa99f5d58f6dcedc055232f1f3d59af4e6ff0248b574892b70652"
+    sha256 cellar: :any, monterey:       "88afcae9366a99216e04102dd95d596c94085849695457077c90bc91c3f48078"
+    sha256 cellar: :any, big_sur:        "383385b7dee17c6be66dcfa3d951cc20336b6eae4bdc0f37ee48684da64a9892"
+    sha256 cellar: :any, catalina:       "ff5753223dc4f1374dd5e5773c560b5f7e75d0e8cee724ea4b08387649adcf5e"
   end
 
   depends_on "cmake" => :build
