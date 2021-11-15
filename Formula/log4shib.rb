8ec433bfa262def0f1d95d06df326dc989b17942
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-15 09:11:15 +0000

    log4shib: update 2.0.1 bottle.

--- Formula/log4shib.rb
+++ Formula/log4shib.rb
@@ -11,15 +11,12 @@ class Log4shib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b50fde112e722ab2b77786c1ba809237c3b504f75f3e35880c3372740d3681da"
-    sha256 cellar: :any,                 arm64_big_sur:  "07b780239ff655d1c4b5de7bb4cbf5a9daed61f0e691a10c8ad7880a658ce23f"
-    sha256 cellar: :any,                 monterey:       "eaed3226ec7dbd3a3c49335cb8a898fa460e2b41be909a229164676a69215a3f"
-    sha256 cellar: :any,                 big_sur:        "b739ef276c38d293771f5d42185637de5944974cd42d677c88d08e2e2627731e"
-    sha256 cellar: :any,                 catalina:       "8bba779ac511127d2893aa7f90e08fea86e49d54a002363edac8396143b53fd2"
-    sha256 cellar: :any,                 mojave:         "db9aa2c4c1f5f562177d7ab8f772d3634af17ad321866da25da81986c2806941"
-    sha256 cellar: :any,                 high_sierra:    "6a84a5b1db0fa9fef6e23f906543bde2496e5400f498c8de6b64cab2b191eeda"
-    sha256 cellar: :any,                 sierra:         "79197ed691693493ffc4b44dd5450b60c9c6cc97919302ae058c9e9af5cd10f6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "136a804ca90390b69fd57818855efbeefe5d1c5a44fcd523dab57fa54194ca47"
+    sha256 cellar: :any,                 arm64_monterey: "0a24e1932a0b752006d448741f713646761e8d827e8615aa69575b3de674a85f"
+    sha256 cellar: :any,                 arm64_big_sur:  "450ddfec54aca621297964385847c9ac0207dd1cf41d67222bf9f0fcb1207360"
+    sha256 cellar: :any,                 monterey:       "7a8f70e280df362c5f85191ee9586c40436da110824674fb7e451d0a177b165f"
+    sha256 cellar: :any,                 big_sur:        "0eddc0326cf4fbbf0eafe1bf6ebf1c69f55eabc218527624f47871be8fad3d04"
+    sha256 cellar: :any,                 catalina:       "dd41c1980bae36dbbfd7c5ae5fc896354a95592a78f3b3f76b7b8ab35ab02329"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b91c63f0b4e7b7aeeddfee541c3e7a0392438d9fa8ec2576b8f08e1bf7a711f"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
