aa763939a68c3e5d0379d81701c530a6d33cd0f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-15 09:11:25 +0000

    mlt: update 7.2.0 bottle.

--- Formula/mlt.rb
+++ Formula/mlt.rb
@@ -7,11 +7,10 @@ class Mlt < Formula
   head "https://github.com/mltframework/mlt.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d00252804029df19b594f3f50c08349fa31b1d845224b21def935e0366de99cc"
-    sha256 cellar: :any, big_sur:       "e7301086e4ea074fc89d486c097a9199a4a6a5a0fb026729dd07f23a362ff134"
-    sha256 cellar: :any, catalina:      "9fee3844b1061d73a16c713abda9f598ad68b69dc9425b82e9a1ee9a2737103a"
-    sha256 cellar: :any, mojave:        "bb5e8a1b1f218ef5bdf899ee18023014ceecf9d9b360668f9fab2f1d15c49936"
-    sha256               x86_64_linux:  "5f5a888250ab01dddbb4742fa00c32dbb889d46b17dc050883db107d8817c57c"
+    sha256 cellar: :any, arm64_big_sur: "83d8addf8cecf1fea2cd2e1892ff1799b57c982571e605e491e1f4a958cf05fa"
+    sha256 cellar: :any, big_sur:       "a5ed8ed18a6ff72c753eb74b5c2bdb68280d33351ec3a1d35aff321f28f16c66"
+    sha256 cellar: :any, catalina:      "d26f34e0a66f0ec86d0ada2c3bfe41e346623eacc39b5348c22db46b03245806"
+    sha256               x86_64_linux:  "f6543f9c1652493fa4b6c68cf127dedfcccbc61dae4b1a933d343361d6467457"
   end
 
   depends_on "cmake" => :build
