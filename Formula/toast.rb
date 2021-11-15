07404a726437edabec7ebecc9374eeee6275af16
Rui Chen <rui@chenrui.dev>
2021-11-15 10:56:25 -0500

    toast 0.45.2
    Closes #89462.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/toast.rb
+++ Formula/toast.rb
@@ -1,8 +1,8 @@
 class Toast < Formula
   desc "Tool for running tasks in containers"
   homepage "https://github.com/stepchowfun/toast"
-  url "https://github.com/stepchowfun/toast/archive/v0.45.1.tar.gz"
-  sha256 "d71eea4b8aa4b8ef573f8d49e5c39874adbbd3a86837310408ef05807bcf2609"
+  url "https://github.com/stepchowfun/toast/archive/v0.45.2.tar.gz"
+  sha256 "9d1a4c785f62f68effa362c39eab1e0802fd40402416e8938ea7a7d4088945d0"
   license "MIT"
   head "https://github.com/stepchowfun/toast.git", branch: "main"
 
