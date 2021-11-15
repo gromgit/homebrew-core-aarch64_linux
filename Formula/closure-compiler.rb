e918df9e072f7f862a2052a28a8a2e026e61c1dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-15 08:07:57 +0000

    closure-compiler: update 20211107 bottle.

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -11,12 +11,7 @@ class ClosureCompiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4df4438a636ef1af62144b79ba93b55f165d5c1b12e1f239f81b3d00f7f36129"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4df4438a636ef1af62144b79ba93b55f165d5c1b12e1f239f81b3d00f7f36129"
-    sha256 cellar: :any_skip_relocation, catalina:      "4df4438a636ef1af62144b79ba93b55f165d5c1b12e1f239f81b3d00f7f36129"
-    sha256 cellar: :any_skip_relocation, mojave:        "4df4438a636ef1af62144b79ba93b55f165d5c1b12e1f239f81b3d00f7f36129"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3b574f7e1c32f439d7e73a1cd835faa2b4055cdafe1a2b3728cc32a28f45493"
-    sha256 cellar: :any_skip_relocation, all:           "14ab57680ce20aca0ee384133cd0426c1f1eb89b17dd28eb1511459c3c06cd32"
+    sha256 cellar: :any_skip_relocation, all: "e849042d52d6c38b49197ad7f6401dcc2031cc98b0a35aaae9bdc12757b624d5"
   end
 
   depends_on "openjdk"
