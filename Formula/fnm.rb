16ec9c89c66ffa32dbc1f1c754b3e4586b1624d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-16 16:12:44 +0000

    fnm: update 1.28.0 bottle.

--- Formula/fnm.rb
+++ Formula/fnm.rb
@@ -12,13 +12,12 @@ class Fnm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "86b8692f33637fe310b36744e43477fe968631fb3e06b22c2d40399277b1e18e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a7b6d36aa0cf64fb42f99a57686c7e771690b6b97eb19f6e65486365b9e262d"
-    sha256 cellar: :any_skip_relocation, monterey:       "c0473cfa5f4dd2f56ca25cbd06a80af862d5641b5b8481c26012aca73f619659"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1ed1913f1c9f70bcc703bac59e9e46540cb2030c5e42e0fb8cfe42eb1754ee75"
-    sha256 cellar: :any_skip_relocation, catalina:       "3195625f6b556c8eeee0894ba516b7a91a7fddd1fd31090867b942bcb583e917"
-    sha256 cellar: :any_skip_relocation, mojave:         "9921bb97be053afe96cb9a18bdcfd95fb34faa9a00dee89d0735938b52ea0a40"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a237dc5807cb312d8cde5f0d937046cc10a4c58d3d941d82064fe6fa49f0b96"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "905fc55c51ec5c1dc889d2dbce5fdc30fc918c0f463e2319df290cded53ec08c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d403f455797499a498701bc6c61fd2dcca33af9e98957b94cffcb3860d53f8ca"
+    sha256 cellar: :any_skip_relocation, monterey:       "3549453c5e717d8699cb0e39f8d92ce103f0da419bdc08236fb7b1843f20fa1b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9649b950701cf4528fc2ed2706e8b3b50dbb731f94439f4add800fe7c66f5787"
+    sha256 cellar: :any_skip_relocation, catalina:       "37c025276645b7743958863e93909660f4b45838c5e99e2d031ad0559c34076b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b675c9ac0b44a51831efccf9194338aefaab39bad80d8df9f3a74632610343c"
   end
 
   depends_on "rust" => :build
