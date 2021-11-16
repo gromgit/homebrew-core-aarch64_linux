aa2e7852ac078e13e090cfee51ce42eea3e353c5
romes <rodrigo.m.mesquita@gmail.com>
2021-11-16 00:01:35 +0000

    pcalc 2.2
    * pcalc 2.2
    * fix test
    
    Closes #89493.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pcalc.rb
+++ Formula/pcalc.rb
@@ -1,8 +1,8 @@
 class Pcalc < Formula
   desc "Calculator for those working with multiple bases, sizes, and close to the bits"
   homepage "https://github.com/alt-romes/programmer-calculator"
-  url "https://github.com/alt-romes/programmer-calculator/archive/v2.1.tar.gz"
-  sha256 "27cba8e04e73f1a2fbb4900abbf450fee07f7691c709addde21e5bb7a44c9988"
+  url "https://github.com/alt-romes/programmer-calculator/archive/v2.2.tar.gz"
+  sha256 "d76c1d641cdb7d0b68dd30d4ef96d6ccf16cad886b01b4464bdbf3a2fa976172"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -24,6 +24,6 @@ class Pcalc < Formula
 
   test do
     assert_equal "Decimal: 0, Hex: 0x0, Operation:  \nDecimal: 3, Hex: 0x3, Operation:",
-      shell_output("echo \"0x1+0b1+1\nquit\" | #{bin}/pcalc -c").strip
+      shell_output("echo \"0x1+0b1+1\nquit\" | #{bin}/pcalc -n").strip
   end
 end
