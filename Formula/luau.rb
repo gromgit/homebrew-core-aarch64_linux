c34a9285557614467fef74cb8fcebd2341f8f60a
Anoush Mudaliar <anoush.mudaliar@gmail.com>
2021-11-17 18:37:19 -0500

    luau 0.504 (new formula)
    Closes #89589.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/luau.rb
@@ -0,0 +1,27 @@
+class Luau < Formula
+  desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
+  homepage "https://luau-lang.org"
+  url "https://github.com/Roblox/luau/archive/0.504.tar.gz"
+  sha256 "544b3107e1c53f8349b1e327efed55ac29cc20a2983dd23031ab07c5b6944633"
+  license "MIT"
+  head "https://github.com/Roblox/luau.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DLUAU_BUILD_TESTS=OFF"
+    system "cmake", "--build", "build"
+    bin.install "build/luau", "build/luau-analyze"
+  end
+
+  test do
+    (testpath/"test.lua").write "print ('Homebrew is awesome!')\n"
+    assert_match "Homebrew is awesome!", shell_output("#{bin}/luau test.lua")
+  end
+end
