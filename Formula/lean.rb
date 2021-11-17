9d00713cd97e91cc9f816d0c10838f34a613d54f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-17 06:08:15 +0000

    lean: update 3.35.1 bottle.

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -14,13 +14,12 @@ class Lean < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "fb6411ce8203b931dba98878201ccd3b499571fec93d54e905bef5f678a32233"
-    sha256 cellar: :any,                 arm64_big_sur:  "dce6ff86967540c830fa6aecb39e1dcb3dd54beaa0a524ef373e405dbb6f514a"
-    sha256 cellar: :any,                 monterey:       "7c266013ffa92e78c0b1d6771d0849b38014de32d4645b93dfc59a0e57df5eb8"
-    sha256 cellar: :any,                 big_sur:        "a10756134d6e97923dc0425d02a8b1ee0a49b4758f49f27e03282887071cde6f"
-    sha256 cellar: :any,                 catalina:       "014297ad90fee979d9e726fc08d13edd2adab94986541cd67172f62f845aaea5"
-    sha256 cellar: :any,                 mojave:         "386b72209c9b4fdea4f1ee0a5cded5560bd276e4b9e297c4e4586efbb03c4e74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d866e92baca2a841cec6605bcbe9e6246fb5f90edbc5c5e054a15c3afd6d69a"
+    sha256 cellar: :any,                 arm64_monterey: "0372f802742667f07f4b451db61483608adbfeb0a16fb226bdbe3d27ea6da513"
+    sha256 cellar: :any,                 arm64_big_sur:  "2e7d5d72c9341c473f6c72edf633b8eecb93e7bed78a8b72139b9d3062f285e7"
+    sha256 cellar: :any,                 monterey:       "ab53ddc187f71ed59f14e4e041346f276812c5c9c62b5a210e313fc8d0341107"
+    sha256 cellar: :any,                 big_sur:        "8fb3a11e18a2005fe146bd0253634a8b8e980d9e0e12aeda031695697c9a46e7"
+    sha256 cellar: :any,                 catalina:       "5a107a325ca89c60e138458e3a3db3e41782eed33ad602341ae26c5a0793a944"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "49f03064b4af7c9b0e2957dff1a7c737a234ed4d88a5e21c9c719cc10df3bb04"
   end
 
   depends_on "cmake" => :build
