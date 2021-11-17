e0ae75e556503b523bdd467eb26477d6b0b94c43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-17 09:12:11 +0000

    cargo-audit: update 0.16.0 bottle.

--- Formula/cargo-audit.rb
+++ Formula/cargo-audit.rb
@@ -12,13 +12,12 @@ class CargoAudit < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "540ffaf870aabb254754f0015b58ae4641125702163a2c61cfb898214ab746e8"
-    sha256 cellar: :any,                 arm64_big_sur:  "a7954b9aff8d7d2ddac189fbd800a5f38e91e6b6259a8c951df47dc11fe97337"
-    sha256 cellar: :any,                 monterey:       "cdd802d4fcf5d9f916890380d1192e7839793ab5abb64fbd15d25f2d4c35f915"
-    sha256 cellar: :any,                 big_sur:        "f6337c09242e9f1db0b2952e461558c79ae9b95caa7e76e1483d9027ddf67fb8"
-    sha256 cellar: :any,                 catalina:       "8383cf15d8e5f83890e46628786b5aa293453d00255a10e963d16567cdad44bf"
-    sha256 cellar: :any,                 mojave:         "3bc087889fb025d8bd59701e0882d11fec0d4d2678a3fb94a47030d97dc0097b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52fdf4c76b47d450263ce877a991bc4ea68990e1c3ed7003ae34e6ee1b0ebda7"
+    sha256 cellar: :any,                 arm64_monterey: "03bf27001ce35f5885c14b517d1cb9b496a6092f5a91636483f3c11e1dc3cbee"
+    sha256 cellar: :any,                 arm64_big_sur:  "5825e17552507d413d261f30dfc0c3597e5d9e538ea966ba66d6c6f98e2bfafe"
+    sha256 cellar: :any,                 monterey:       "306826ea9ffc42f17de0ff4a92e5ad51e47fcbf92c7d646942eb5fd78f09dfef"
+    sha256 cellar: :any,                 big_sur:        "0930e9a08ceeaa5fde11066edf2b9212f9177c2750ff018fe3ae993156d6f1d7"
+    sha256 cellar: :any,                 catalina:       "618b98672a077ad25f0d71e2fee12f17721b1ab1fe33d4842e46ce512c950b9f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "92cfa76ab250fd2b2dc8be2839eb36bca2d9f73fdf811451022d6a0c0083fe62"
   end
 
   depends_on "rust" => :build
