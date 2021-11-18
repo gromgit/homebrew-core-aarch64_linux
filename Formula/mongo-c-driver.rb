454b2a30a6a4f3d20097d042fb207ea469ff317a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-18 01:27:44 +0000

    mongo-c-driver: update 1.20.0 bottle.

--- Formula/mongo-c-driver.rb
+++ Formula/mongo-c-driver.rb
@@ -7,12 +7,12 @@ class MongoCDriver < Formula
   head "https://github.com/mongodb/mongo-c-driver.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "248678dc27ca4ce53cd4a2373fb0d0dcd02cd2d9caa36a6d0a91ed9d1160d0cc"
-    sha256 cellar: :any,                 arm64_big_sur:  "56e8d90faf11ed935ee43b0900bf1df6fc2307aabb4a6688fe6d6e55d334a5ea"
-    sha256 cellar: :any,                 monterey:       "8abf2f15cda906faf32f8a1142a9655619000039ef648c901bcf1e83f0abb2f2"
-    sha256 cellar: :any,                 big_sur:        "8bf977fb5a1965368179642184981bd0d501560699795659cbe509257058f755"
-    sha256 cellar: :any,                 catalina:       "f752065ec9fd540441383301f006b00fe166f0d23f5ffdcba4930b985b816523"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9576f248394480574314ad3a171a149923349c35c3474bc72dc73a716630c71e"
+    sha256 cellar: :any,                 arm64_monterey: "374efdef4f8445ef889e34b59e36b9aa36f2d1cf278e638ef47a7159ab5e5374"
+    sha256 cellar: :any,                 arm64_big_sur:  "0de2bee416e8b6ba51a400e4e754d87c908fb9a25685eb30fe1faa7ac5e01d39"
+    sha256 cellar: :any,                 monterey:       "7bb3e1580f3897a87ed82ecb8906da04847bdbbac71973b9302be36add965ac4"
+    sha256 cellar: :any,                 big_sur:        "de451485bdcf498cb967e0d68b2254526f36a92514e59501af8a0c4e7eef914e"
+    sha256 cellar: :any,                 catalina:       "84834568b98294d2764d1256ae549e1b02789db8bb09eb2ed85104505f26103a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1514f36700ab9f60f773ca390e5841499cc49277ef500579012d9d3478f456f3"
   end
 
   depends_on "cmake" => :build
