d4eaff97ed294b26ed94ba81e0facb285bfc2c25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-18 00:12:57 +0000

    ipinfo-cli: update 2.5.0 bottle.

--- Formula/ipinfo-cli.rb
+++ Formula/ipinfo-cli.rb
@@ -11,12 +11,12 @@ class IpinfoCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "840e763397b239c5ff3423c06ed9d03aa61661c0a50dc4d6ebf663f3afec9f89"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dfa905b672542169bc3bb6ea8d843fc47bfbce6e1dd2d4777640e1b0e4c2b0cc"
-    sha256 cellar: :any_skip_relocation, monterey:       "645888771a0e2957973d28f83ddd531590b7aa7aa8e15b05b638eb39e2f73107"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4e924abe22e12f47b13fe0dd30df2e0f9bb41fdbc228a96f44cb9a803c7e0e09"
-    sha256 cellar: :any_skip_relocation, catalina:       "eace306eb7517ef959435ce7b8c0efa6fabbb6f5596ac554fc8e71d62687f877"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e6ff38843218dc26e08f2ef2e48d7f5324332502c84416380359d7153ed3ecb9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "48afe956d9772bbbcb311170a821737475f621c07ff09cfd21796aaca6fc23ac"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a28191796d1fc2aabde393f80f99501216a362efda2b9e0cd6a633be039a9b25"
+    sha256 cellar: :any_skip_relocation, monterey:       "2685ac3aad72eb506060caa0b4496abdfdc62fdc0807efccd5e810dcb275d34c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a430fb6a22a36f7d987990834c585103d22a0486644eea6e7b9a1d0fc5a105dc"
+    sha256 cellar: :any_skip_relocation, catalina:       "4729faf85598ee782b2cfba63e94aa1b63366da23c5cf57f00dd5c2258daadfd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea54907f5061484ba073bc19b3584401bafeecaa3e1dda2d0e38922711ca5e53"
   end
 
   depends_on "go" => :build
