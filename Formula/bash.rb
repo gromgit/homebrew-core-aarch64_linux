de196f454e29985a0ccf297d1e9bb1cb5633bda9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-18 15:14:36 +0800

    bash: improve livecheck
    Instead of sorting the scanned versions lexicographically, assuming the
    last one is the latest, and casting that into a `Version` object, we can
    cast all the scanned versions into `Version` objects and sort those.
    
    This will make detecting the newest version a bit more reliable, at
    least insofar as lexicographic sorting differs from version sorting.
    
    Closes #89615.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -43,11 +43,15 @@ class Bash < Formula
     regex(/href=.*?bash[._-]v?(\d+(?:\.\d+)+)\.t/i)
     strategy :gnu do |page, regex|
       # Match versions from files
-      versions = page.scan(regex).flatten.uniq.sort
+      versions = page.scan(regex)
+                     .flatten
+                     .uniq
+                     .map { |v| Version.new(v) }
+                     .sort
       next versions if versions.blank?
 
       # Assume the last-sorted version is newest
-      newest_version = Version.new(versions.last)
+      newest_version = versions.last
 
       # Simply return the found versions if there isn't a patches directory
       # for the "newest" version
