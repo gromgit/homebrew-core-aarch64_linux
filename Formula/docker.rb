b012c1875c1f5316a66103b27bc70ebd7ce556f2
Dawid Dziurla <dawidd0811@gmail.com>
2021-11-18 05:05:11 +0000

    docker 20.10.11
    Closes #89607.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.10",
-      revision: "b485636f4b90ed5a91a1f403e65ffced469c641a"
+      tag:      "v20.10.11",
+      revision: "dea9396e184290f638ea873c76db7c80efd5a1d2"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git"
 
