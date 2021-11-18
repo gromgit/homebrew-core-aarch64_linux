feddfd3b28b7c2ba532b7080854d5a24605011a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-18 20:09:07 +0000

    haskell-language-server: update 1.5.0 bottle.

--- Formula/haskell-language-server.rb
+++ Formula/haskell-language-server.rb
@@ -14,12 +14,12 @@ class HaskellLanguageServer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "39c4925f9ddd98833fd0566badd7a66f4053e53b3bf14a0ce51b48e348ae431b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5fec58d7c3ab8a84979a637f41d0aba636a7a024dfd96582ab2e5ac9374c15e9"
-    sha256 cellar: :any_skip_relocation, monterey:       "d3571c62387650e4b79b2ad558864dace440dfe581d96cfbf08f7fe44c6667bd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5e3427fde42364771cbfab41b4eeb71889ac3a41737b6f54764fee1ec64c3e60"
-    sha256 cellar: :any_skip_relocation, catalina:       "ba30e3d1544596f125755302d973c5c38975253da11018ad22e51722f32b3316"
-    sha256 cellar: :any_skip_relocation, mojave:         "1eb7824102bc6234c8faf8523e50955f819403f3baf01837c72f79d4393536f1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "db6100ae789dd9c323c4226020a0b2c4ae976fe253643a39be557ab6e2f9c34a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1950e3eb9d77b9cb32abf313de8747481b6aa6ed8f949212d23513f8d1f8d92b"
+    sha256 cellar: :any_skip_relocation, monterey:       "0e6306333ebd9de2f257d48a263775a1336789480b614922b517aee362c02a62"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb92175df8b9403eea3aea99be3dc77e9d9e53c051b9d614590843896f615b45"
+    sha256 cellar: :any_skip_relocation, catalina:       "636874b4321f7aa0ae1f0a32b0bceaed96464006e3b4b3c3f04cfc390dcb2020"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88d634406571ed86ec9fb4782fcd5386a8abe9248f0868324feeca4453ebe8f8"
   end
 
   depends_on "cabal-install" => [:build, :test]
