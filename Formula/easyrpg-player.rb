317ea10952fb48530325bb2ebac74d0910250b8f
EricFromCanada <enk3@outlook.com>
2021-11-17 21:16:38 -0500

    easyrpg-player 0.7.0
    Closes #89096.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/easyrpg-player.rb
+++ Formula/easyrpg-player.rb
@@ -1,8 +1,8 @@
 class EasyrpgPlayer < Formula
   desc "RPG Maker 2000/2003 games interpreter"
   homepage "https://easyrpg.org/"
-  url "https://easyrpg.org/downloads/player/0.6.2.3/easyrpg-player-0.6.2.3.tar.xz"
-  sha256 "6702b78949b26aeb6d1e26dbffa33f6352ca14111774bfd433bc140c146087d0"
+  url "https://easyrpg.org/downloads/player/0.7.0/easyrpg-player-0.7.0.tar.xz"
+  sha256 "12149f89cc84f3a7f1b412023296cf42041f314d73f683bc6775e7274a1c9fbc"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -20,7 +20,8 @@ class EasyrpgPlayer < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c4560e369a92cfd63c7077351e0f802e1f8df989be7cf640091255de475613df"
   end
 
-  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "fmt"
   depends_on "freetype"
   depends_on "harfbuzz"
   depends_on "liblcf"
@@ -31,14 +32,25 @@ class EasyrpgPlayer < Formula
   depends_on "mpg123"
   depends_on "pixman"
   depends_on "sdl2"
-  depends_on "sdl2_mixer"
   depends_on "speexdsp"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "alsa-lib"
+  end
+
+  fails_with gcc: "5"
+
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    if OS.mac?
+      prefix.install "build/EasyRPG Player.app"
+      bin.write_exec_script "#{prefix}/EasyRPG Player.app/Contents/MacOS/EasyRPG Player"
+      mv "#{bin}/EasyRPG Player", "#{bin}/easyrpg-player"
+    end
   end
 
   test do
