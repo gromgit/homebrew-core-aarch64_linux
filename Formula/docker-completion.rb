8b5bea3fc2536b8f64e13a26e1cbfe64fe6a85e6
Alexander Bayandin <a.bayandin@gmail.com>
2021-11-19 13:12:29 +0000

    docker-completion 20.10.11
    Closes #89694.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.10",
-      revision: "b485636f4b90ed5a91a1f403e65ffced469c641a"
+      tag:      "v20.10.11",
+      revision: "dea9396e184290f638ea873c76db7c80efd5a1d2"
   license "Apache-2.0"
 
   livecheck do
