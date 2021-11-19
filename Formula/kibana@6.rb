6ea04b7111fceb631b0a401edf7cc1ad246a5b93
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-19 04:09:06 +0000

    kibana@6: update 6.8.20 bottle.

--- Formula/kibana@6.rb
+++ Formula/kibana@6.rb
@@ -7,11 +7,9 @@ class KibanaAT6 < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:    "18d141bdfdb6526042c13e32c270a446e67b419ae2b1cb818ffc79ebe991bd60"
-    sha256 cellar: :any_skip_relocation, big_sur:     "c6851cd00abc11d5bf62fd244e601e9b9071443c545e3713077ec0cccfd8fc59"
-    sha256 cellar: :any_skip_relocation, catalina:    "d13d9e35a2d55f1e316a16a6dc7662a0be783b11e0707f445902da1a6084a207"
-    sha256 cellar: :any_skip_relocation, mojave:      "726bc34894f26bc18d9b1b9d756febefb03d76ef598445c406f18840554f6198"
-    sha256 cellar: :any_skip_relocation, high_sierra: "4bd89cc139a35c58402dabeaab5dcbccf02e6bd269122b454723941449984f9e"
+    sha256 cellar: :any_skip_relocation, monterey: "3fdc7ae371689838ee1c29ce6d29a14780effc178fc5de39f20cf619f6af1c22"
+    sha256 cellar: :any_skip_relocation, big_sur:  "8b89272e13e281a61a7c82f03e279d11731c9e772f7ccdcdabf0f2b67546733f"
+    sha256 cellar: :any_skip_relocation, catalina: "fa529ae4e4890e3b7e7ee56e25a99c7f95852eadfd08d75121c9bc1f1baf4dd5"
   end
 
   keg_only :versioned_formula
