bdd766978739cc6ea452e82c716a026370b77320
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-19 10:09:58 +0000

    buildpulse-test-reporter: update 0.22.0 bottle.

--- Formula/buildpulse-test-reporter.rb
+++ Formula/buildpulse-test-reporter.rb
@@ -7,12 +7,12 @@ class BuildpulseTestReporter < Formula
   head "https://github.com/buildpulse/test-reporter.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "632ad07ab1c55c93c4d306abfa7ac5f42eaafd3cfcc0bbc18fe94f5a84550c3c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cc5b7d41d3e0f9ed4d2f9fd73be416d59eff7002b00a5ac427cb3f987fcc76ba"
-    sha256 cellar: :any_skip_relocation, monterey:       "ed568249c719710bdd82997761cfd14533d2224df1aaa6d9f053f4a6a4614e54"
-    sha256 cellar: :any_skip_relocation, big_sur:        "57d2559e7c0e34e593c657d55ce05ab7e9773064923f7fce45e7b5e8ff391254"
-    sha256 cellar: :any_skip_relocation, catalina:       "6a695e176c6ff0f5d39fe7a9e6967b0b848fbaea26159a28301af612f111080b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb918a49ec30f8a00bb36bd70176621cf6e21307bc0437ef95b8c09263ef94c1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8058fc7befab1fca308927f42c006f5a07422d223db3bb61cb7dc963c6a6196f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e2f8443094f637f0248dab74dd5b35028588c5fb0b303116d1ef22dde5478d6"
+    sha256 cellar: :any_skip_relocation, monterey:       "424c5626fbb0b4f867cf960270128df8fe9e82e4870d418545167681e72b9226"
+    sha256 cellar: :any_skip_relocation, big_sur:        "92d337bcf1ad91fb6a2c6f891f020fad1f843dd37b772bf62160e5743fa58c6e"
+    sha256 cellar: :any_skip_relocation, catalina:       "165d5540840fc4561cd434d9dc52a251570f899978b32f4fb6a126b579569c97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed7ac8faf11d2ad128f4410092400de20b0ffe8565b88ea946bc7a964f606cf6"
   end
 
   depends_on "go" => :build
