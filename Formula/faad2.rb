d1fb11db721a060717864cd5ad37921044bcae4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-19 04:08:31 +0000

    faad2: update 2.10.0 bottle.

--- Formula/faad2.rb
+++ Formula/faad2.rb
@@ -6,16 +6,12 @@ class Faad2 < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8f5e0437e36768d7d85f5e96da07b6e651f39c571749cf9a054ed1073d7892c7"
-    sha256 cellar: :any,                 arm64_big_sur:  "9d15e44991762f7fe0b3ec9a41cf76ebe2ba6ed0f5ff2b2c5a10916214cc7e27"
-    sha256 cellar: :any,                 monterey:       "5b0667b73757e56435c1e1f0e8964ed4d718428b87340c0b780b7ce67e33190e"
-    sha256 cellar: :any,                 big_sur:        "512c0a82b5d332c0558cf27a210ee0cabed163ae8a057d9a13bffe934b6bbd9b"
-    sha256 cellar: :any,                 catalina:       "f12e1d6b2b8bb7e49bbb681711c5da2a45ad7d3957c72105ab6b13c194d9e33d"
-    sha256 cellar: :any,                 mojave:         "a896f898d36455dbb65b19efcc1f574be76c22dca981e3361be08ef234fd6e5d"
-    sha256 cellar: :any,                 high_sierra:    "e8872363a2fda9a3c9872ef697e517c638e54e2af5238d9e94b30e34ecdc505e"
-    sha256 cellar: :any,                 sierra:         "f05989cbd9630fc37c962fc28ff29ec48a5fa7b71fe4ff9e520db6add1d0f09e"
-    sha256 cellar: :any,                 el_capitan:     "94205432c0187c2ccef411b05934b8db57512bd80b53c8f9c00f3792ee478684"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4af4dd151f97c6cf5468330c00d6f8312dceca890224663bcddcc07d55dc2c47"
+    sha256 cellar: :any,                 arm64_monterey: "7d3ed82ad0bc48a71b7341db0d578545bc9fcdd4ea084b4e651d99319c1e7b63"
+    sha256 cellar: :any,                 arm64_big_sur:  "8032f53b871aa9c01950efd796f73f66f3cbd514e5078a0a81cf7f36e42723a8"
+    sha256 cellar: :any,                 monterey:       "fbb0d45e57a091f43f049152b505525a060f4a15c65f455769f88adfafc4e46e"
+    sha256 cellar: :any,                 big_sur:        "3b213d8ff392515c908a505c0ec902f195fcbd7a8ae470896a0a4ed0080a5445"
+    sha256 cellar: :any,                 catalina:       "efc7c8b92b7411b12fb4f63ef641f92d2278b212f49614b7322449dcd24971fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "720a60fc65bf50a3645009ab1e4046f897ebadfc06be17e84e00eec0dfd49a56"
   end
 
   depends_on "autoconf" => :build
