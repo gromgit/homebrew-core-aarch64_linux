8e45ab6a87a0d97b0fce35bf78a5dd9c50d7e33a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-19 22:28:51 +0000

    luau: add 0.504 bottle.

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -6,6 +6,15 @@ class Luau < Formula
   license "MIT"
   head "https://github.com/Roblox/luau.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e86d9937c9a02f3b6c5861c110e7f0c820dff6ac6a55177a88dae35f4bd8b5ad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "94eed4db5cf95cd89902bc8e1f1fefa8f035c7fbbca8cf1c4f07110b3c78b6b8"
+    sha256 cellar: :any_skip_relocation, monterey:       "0fb6130ea6223a61f5b6cc16beb93cba13d9270b8cfebace4ff58492bb645f30"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c6bbf46f9b4004103b5e24d4d3425bff37e23b4201c7bfc4818cad475b213867"
+    sha256 cellar: :any_skip_relocation, catalina:       "3f886319d63106aac52bc0fc045d3d5cf93cb00b679516ac3a46856e367b4d3d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eee7c4093a59872dcad1d74ea3d63012b53f6480cf74581c6dd5af892df10aba"
+  end
+
   depends_on "cmake" => :build
 
   on_linux do
