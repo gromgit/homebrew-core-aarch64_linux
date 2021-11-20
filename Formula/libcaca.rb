746876466c1b14863f2ffdb3517d28fd72e2a274
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-20 20:07:37 +0000

    libcaca: update 0.99b20 bottle.

--- Formula/libcaca.rb
+++ Formula/libcaca.rb
@@ -12,13 +12,12 @@ class Libcaca < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "9ab9784b446494822823d030a3469c05f5195bc399bbb7c572d17469ed9b258c"
-    sha256 cellar: :any,                 arm64_big_sur:  "49547bb139e2ed778c36c881a73d0ec51d3c0b978873db0587b3936b87c55d0b"
-    sha256 cellar: :any,                 monterey:       "e2233e36276fece39a2a1cba166a15f4c07a64ca93ca99e1dd2d9267e87afd0f"
-    sha256 cellar: :any,                 big_sur:        "fca71650e2702ac497560f86779bbc77acb5fd8cf09c8219c2381be20af6d11e"
-    sha256 cellar: :any,                 catalina:       "3d2d080e206d0d7d9720687aadfce949e78588df510b9039ff1b8f4277015d6d"
-    sha256 cellar: :any,                 mojave:         "38488f0e4363948a80d60201da73c6c67856525ff0b67cfd53dc3caa16de602e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "635b337217281620fd72e3ef25992d3c92b8212be752001a3a36f227df13cb51"
+    sha256 cellar: :any,                 arm64_monterey: "b29bc6f1dd407411eab8689bfe190574b9fdc487d00dbdc7636e9483de867e56"
+    sha256 cellar: :any,                 arm64_big_sur:  "afa31ed628299e9d3fb4109b8f05b5b00fc2820c22804f85993eebda9a3097c0"
+    sha256 cellar: :any,                 monterey:       "c7f6b2b19aaaa1417feac203e5c3676b6c0e70fb72816e16e1b85343e8cf55fb"
+    sha256 cellar: :any,                 big_sur:        "efe390bae78561024c804ac37bb5c0cf9f3397229bd91732cb59f9f4e32ecc8c"
+    sha256 cellar: :any,                 catalina:       "f0f86157174d749eedaec913f4d8d1b6d00824b6e580498847ac9e00c9c53d9e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7dab49e0d6bf0ed46dcc831c783da0124e5329f7e0e69f7d8cf847f9b825ebe2"
   end
 
   head do
