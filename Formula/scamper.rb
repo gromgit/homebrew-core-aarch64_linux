ee207c48ee8519a2627cfd13b37665ae86e8c353
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-21 18:08:12 +0000

    scamper: update 20211026 bottle.

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -11,13 +11,12 @@ class Scamper < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "172a57d0927be1c8f7a2fc7f6f50e7b6ab964582c6b4becbbf55dc8bb7b9b15e"
-    sha256 cellar: :any,                 arm64_big_sur:  "855e3ee08cbf5f3faf38ce9882a50726206ae937b5aaea7e05a0428ebd5a3a98"
-    sha256 cellar: :any,                 monterey:       "84a88c7b1f30397f915bb08ad216e80c5f128c4a771a7393dd0197c6f9aace74"
-    sha256 cellar: :any,                 big_sur:        "09a4f52a2be595e32ca3fc36a34382c99b81b3dcd100100c2044f7a062fa26f8"
-    sha256 cellar: :any,                 catalina:       "c33b1518d8c66b25952e32d0b52ce8e44b060818056e0d09f5a594bd349fef52"
-    sha256 cellar: :any,                 mojave:         "0ad15790baa3e9045a7d82ecf4a8e40c35f50006c50bd6229ca8b4485ca35071"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3495fd46b0aa2ad9a540d8143f3d0c4b022f3df80e1874016e4edff219099e29"
+    sha256 cellar: :any,                 arm64_monterey: "d640605d3a5ab0e7783e9f43dcbcc11ea5d1e0e73c6a3911a386aea3836e8dfe"
+    sha256 cellar: :any,                 arm64_big_sur:  "35fe5995a6581b107914360a1e19ba0c80b12909957575b88a340a46f2fd145d"
+    sha256 cellar: :any,                 monterey:       "be47fb7a5e1600008d73ba8464050f2ef4f088a346a9447202db257b62e9fec2"
+    sha256 cellar: :any,                 big_sur:        "17375cca8a4ea9467d21266204387e917add13123f605a91f5645ecbbc9ba688"
+    sha256 cellar: :any,                 catalina:       "20dfc6f01fe9dde757d1517d7a1f80705e5e73793cfaac92a48edaf7d6ae9751"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "447c9c74ff9ff4142d8aa85dd08b09637507110a9dee0960ad253358f3c81ce5"
   end
 
   depends_on "pkg-config" => :build
