1dc83ea09f1eb489360da0dad8b1130d062829d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-21 09:09:09 +0000

    tfupdate: update 0.6.3 bottle.

--- Formula/tfupdate.rb
+++ Formula/tfupdate.rb
@@ -7,13 +7,12 @@ class Tfupdate < Formula
   head "https://github.com/minamijoyo/tfupdate.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6d9805e515c9f50c6da6a769b3a2f6e18ea72b39e57ec0035ebe553ec78a7f47"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "154fdca63039e5c3ed1a2700f601958732233f9db5fc74fec562023dac718b7e"
-    sha256 cellar: :any_skip_relocation, monterey:       "2aa7a0b7daa6dd6bb245a83309b7dd4473b641b9d7ea882b453c4b36c04ffd73"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0c94bb7fde4b8fec7793e8812bd17875f365c527cdb5360b586ff6671123c78a"
-    sha256 cellar: :any_skip_relocation, catalina:       "a3fa447c24b9a8e8cda3f952457d28a65992de3b7693a86cacd4ac681fdf9bc9"
-    sha256 cellar: :any_skip_relocation, mojave:         "51ff4610d05ec391a54b55261847161902706fa5bb4d86e826d7f754e4942028"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1122a930470b2e049d94ff2f43673937bdb36ae7bfa23306e4d9f600183c72c3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "638047caf1f628fcec6582b0c68b8375a686b4bf9d5a5ae985fa62a93144b96e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "feae683aabba444a9b6c390fe00180978e528e76f5d394b3aeae99266229ad34"
+    sha256 cellar: :any_skip_relocation, monterey:       "9379d9cdffb6841d83a0c69dcdd5d9d300cb41a9f283873c4142c5168d91b676"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a16b8c44ed0026e46c483514afae55b3fed2c904ad670066258303ee98b6f551"
+    sha256 cellar: :any_skip_relocation, catalina:       "f29b6f73d35629aef5776535a4d39a373ced7fc923bac95f5f8bff6c7fc35f88"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "576c712d91ee88bd224d75e85e60cffefc6f8873d9ea2409bd337341bfbe9ec0"
   end
 
   depends_on "go" => :build
