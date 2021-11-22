3d05ede231b7265c229c390db7ad95636450d373
Sam Ford <1584702+samford@users.noreply.github.com>
2021-11-22 10:54:35 -0500

    spring-roo: deprecate
    Closes #89826.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/spring-roo.rb
+++ Formula/spring-roo.rb
@@ -5,14 +5,11 @@ class SpringRoo < Formula
   version "2.0.0"
   sha256 "37819adf23b221a4544a7b1e6853b67f695fb915f5a1d433760e04fb4b5d7263"
 
-  livecheck do
-    url :homepage
-    regex(/href=.*?spring-roo[._-]v?(\d+(?:\.\d+)+)\.RELEASE\.zip/i)
-  end
-
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a81e84e71bc6ef221f312d8854cd852a7052516d3fbd6ea98820ddcd12e0d061"
   end
+
+  deprecate! date: "2021-11-18", because: :repo_archived
 
   def install
     rm Dir["bin/*.bat"]
