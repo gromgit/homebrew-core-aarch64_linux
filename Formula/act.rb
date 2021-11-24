65f1de780a5a9817f2bbbe53ef298185fc120c6b
hackercat <me@hackerc.at>
2021-11-24 17:41:40 +0000

    act 0.2.25
    Closes #89923.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/act.rb
+++ Formula/act.rb
@@ -1,8 +1,8 @@
 class Act < Formula
   desc "Run your GitHub Actions locally 🚀"
   homepage "https://github.com/nektos/act"
-  url "https://github.com/nektos/act/archive/v0.2.24.tar.gz"
-  sha256 "c6bc09cac52483e175f25d50a262e6b0815789a243a657b6b0c597f048487e0f"
+  url "https://github.com/nektos/act/archive/v0.2.25.tar.gz"
+  sha256 "63d86e8156be39b8cb073ab0e98cb97f9eab18997b6b9678f9026a2d52f6af13"
   license "MIT"
   head "https://github.com/nektos/act.git", branch: "master"
 
