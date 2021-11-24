e97c3fe263828e6c592dff73ea68e601f50f159c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-24 18:09:04 +0000

    psalm: update 4.13.1 bottle.

--- Formula/psalm.rb
+++ Formula/psalm.rb
@@ -6,12 +6,12 @@ class Psalm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ec28ebeec34b64ce80b5478d26d9cf67484257c50af3f9001b3d663de84a3ef"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6ec28ebeec34b64ce80b5478d26d9cf67484257c50af3f9001b3d663de84a3ef"
-    sha256 cellar: :any_skip_relocation, monterey:       "681f1f612f89d2eab1f60d733ed6dc956bb7b8d9dde894b8caded609f96b4764"
-    sha256 cellar: :any_skip_relocation, big_sur:        "681f1f612f89d2eab1f60d733ed6dc956bb7b8d9dde894b8caded609f96b4764"
-    sha256 cellar: :any_skip_relocation, catalina:       "681f1f612f89d2eab1f60d733ed6dc956bb7b8d9dde894b8caded609f96b4764"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ec28ebeec34b64ce80b5478d26d9cf67484257c50af3f9001b3d663de84a3ef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b89b1dc38a7137dda873dfd7d0bd398b709eb6f373e8bc2f5c0cf8abeaada8d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b89b1dc38a7137dda873dfd7d0bd398b709eb6f373e8bc2f5c0cf8abeaada8d9"
+    sha256 cellar: :any_skip_relocation, monterey:       "59e0d0e54b9c73e3528ab447541deed14879b65c65e46ab145b37955bb5d6b85"
+    sha256 cellar: :any_skip_relocation, big_sur:        "59e0d0e54b9c73e3528ab447541deed14879b65c65e46ab145b37955bb5d6b85"
+    sha256 cellar: :any_skip_relocation, catalina:       "59e0d0e54b9c73e3528ab447541deed14879b65c65e46ab145b37955bb5d6b85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b89b1dc38a7137dda873dfd7d0bd398b709eb6f373e8bc2f5c0cf8abeaada8d9"
   end
 
   depends_on "composer" => :test
