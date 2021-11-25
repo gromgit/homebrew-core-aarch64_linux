01d8a3926a98fab4f0597812bd1d69fd55b95f2d
Rui Chen <rui@chenrui.dev>
2021-11-25 08:23:26 -1000

    argocd-autopilot 0.2.25
    Closes #89965.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -2,8 +2,8 @@ class ArgocdAutopilot < Formula
   desc "Opinionated way of installing Argo CD and managing GitOps repositories"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj-labs/argocd-autopilot.git",
-      tag:      "v0.2.24",
-      revision: "442e6c2dd70b093aa860b49de3626916107b9824"
+      tag:      "v0.2.25",
+      revision: "e30842d25699028be53fc7644288fe1d3e6e8e77"
   license "Apache-2.0"
 
   bottle do
