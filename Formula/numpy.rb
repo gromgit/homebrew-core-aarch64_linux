9666d9c68aeac9c7b878ba94694cc32172769c29
FX Coudert <fxcoudert@gmail.com>
2021-11-25 15:14:52 +0000

    numpy: update 1.21.4 bottle.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -7,11 +7,12 @@ class Numpy < Formula
   head "https://github.com/numpy/numpy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b0553e85982be6edaf90a71dc7561ab64f096188fcc7142dc535602732dc0cfc"
-    sha256 cellar: :any, monterey:      "14dc6f3429ade704765c7f23553b7b7b60ad389ae8348224eb16683408530783"
-    sha256 cellar: :any, big_sur:       "599eac2a70e5c48c9f4fdcf5960eb5165a929f6028cdf641ee1b2f3df019faf2"
-    sha256 cellar: :any, catalina:      "3281f550910f6b4d1b3accf5b1fbd2f08bb1d1eb42b2a424f286d09f8d8ee694"
-    sha256               x86_64_linux:  "1787c0a7ce1c2f89550d9a97b9cbea0f305915e93884f6978b36686ada4c7a60"
+    sha256 cellar: :any, arm64_monterey: "899171d279a5082950b9d7177c055df740008f9b5f86b4481e0b7cf9c8b0ba2f"
+    sha256 cellar: :any, arm64_big_sur:  "b0553e85982be6edaf90a71dc7561ab64f096188fcc7142dc535602732dc0cfc"
+    sha256 cellar: :any, monterey:       "14dc6f3429ade704765c7f23553b7b7b60ad389ae8348224eb16683408530783"
+    sha256 cellar: :any, big_sur:        "599eac2a70e5c48c9f4fdcf5960eb5165a929f6028cdf641ee1b2f3df019faf2"
+    sha256 cellar: :any, catalina:       "3281f550910f6b4d1b3accf5b1fbd2f08bb1d1eb42b2a424f286d09f8d8ee694"
+    sha256               x86_64_linux:   "1787c0a7ce1c2f89550d9a97b9cbea0f305915e93884f6978b36686ada4c7a60"
   end
 
   depends_on "cython" => :build
