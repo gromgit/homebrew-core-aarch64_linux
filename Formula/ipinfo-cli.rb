561537802e41b7e541df251cde3e6c774c3b5523
Rui Chen <rui@chenrui.dev>
2021-11-25 08:27:45 -1000

    ipinfo-cli 2.6.1
    Closes #89966.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ipinfo-cli.rb
+++ Formula/ipinfo-cli.rb
@@ -1,8 +1,8 @@
 class IpinfoCli < Formula
   desc "Official CLI for the IPinfo IP Address API"
   homepage "https://ipinfo.io/"
-  url "https://github.com/ipinfo/cli/archive/ipinfo-2.6.0.tar.gz"
-  sha256 "1230052f9fa953190c0166c54fa223c778ace69863e216c5c6b69d6aa0495009"
+  url "https://github.com/ipinfo/cli/archive/ipinfo-2.6.1.tar.gz"
+  sha256 "58f856b63fdf1c03c83514e7479c8b5fb301c3706435f541aaf031ed9be47a53"
   license "Apache-2.0"
 
   livecheck do
