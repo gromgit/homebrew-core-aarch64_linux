a323477806708599a918c6b38f8b649cd9d4cdd7
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-11-25 16:41:13 +0000

    osm-gps-map: update 1.2.0_1 bottle.

--- Formula/osm-gps-map.rb
+++ Formula/osm-gps-map.rb
@@ -7,11 +7,11 @@ class OsmGpsMap < Formula
   revision 1
 
   bottle do
-    sha256                               arm64_big_sur: "8d4bfe8a748f9c06582dda25792b96b98eab8b21cec98d58b521cb8b8d4c26cf"
-    sha256                               big_sur:       "54380e69472c5e9ae483823a3ec04c8b5bde31a1cdbc581dd9e14efeed2f8324"
-    sha256                               catalina:      "c02c8a26f0a806b356e84ef628f71243007da8811b887ddcde2627f3ad763d2b"
-    sha256                               mojave:        "af136c4438f1b2ff9fd45c1c89a39db9a5703b18d766137abdf2644c8d418ac2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8de5535ae547d6d74899e50979e191e84141262f10b2f746563a22304b577329"
+    rebuild 1
+    sha256 arm64_big_sur: "69c8b2b22877a14f14d04d3f40a890f6b092b992bcb86270c1f82ff79f54ae50"
+    sha256 monterey:      "322015ebc1b2ce52d40d2db2d27662f639725bd474aca83b1af9238abccb903e"
+    sha256 big_sur:       "5e88cd60732ed86ec019f82a136d3445af500893435b804f68c41d25fe8de72c"
+    sha256 catalina:      "3bd120a4480aaf535f90b4660a3029682adf413eca6243c5a69e15856be192fb"
   end
 
   head do
