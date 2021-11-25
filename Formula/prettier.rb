84b2a53f776ffd7afadd117e6d31a9e3822ff8e4
Alexander M. Turek <me@derrabus.de>
2021-11-25 15:28:05 +0100

    prettier 2.5.0
    Closes #89957.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Prettier < Formula
   desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
   homepage "https://prettier.io/"
-  url "https://registry.npmjs.org/prettier/-/prettier-2.4.1.tgz"
-  sha256 "3c19d08daa9f78170493a4022a1901c51bf67b405c0b72c14ad607a28cb243e6"
+  url "https://registry.npmjs.org/prettier/-/prettier-2.5.0.tgz"
+  sha256 "21faa55daf1ea29fb1c021689808d44f7d57a297a9b73cd092bf58faed53aee1"
   license "MIT"
   head "https://github.com/prettier/prettier.git", branch: "main"
 
