9f78fea8f70f664e43a086136783234394935bd2
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-11-25 17:33:18 +0000

    gtranslator: update 40.0_1 bottle.

--- Formula/gtranslator.rb
+++ Formula/gtranslator.rb
@@ -7,10 +7,10 @@ class Gtranslator < Formula
   revision 1
 
   bottle do
-    sha256 arm64_big_sur: "b90853dc169f42e8f9b1dddcc3af59a52c6809c07a011ec74676676e1aa34c61"
-    sha256 big_sur:       "3a1ea832e3c5b8be45917df36d6229fde96c22e3a8077101d40c00367872b8f6"
-    sha256 catalina:      "9ead77c72b7ee59d76a4c73c92cb93a29cee0cf7885bc8aab54eb672809baf0b"
-    sha256 mojave:        "e5c88ca5952d0958cb3f9bacf25e63ca41ade68c86cb8c55648820fa672ca0ab"
+    rebuild 1
+    sha256 arm64_big_sur: "e787ad4e441d4ecd4d9c328fb18d2384a21862b0a389170ee54cbbd01879a556"
+    sha256 big_sur:       "8a5b528e062820cdc3ebd9b024b4e96f3ec2d65d521b03026828d32d748774e1"
+    sha256 catalina:      "91dd2d4c85608fb04f7a4f92423dce39ab2ab04a3f11152c30efad1bd96f14c6"
   end
 
   depends_on "meson" => :build
