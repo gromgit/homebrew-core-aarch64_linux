707a67dc7f29d410f58f542755ee9f2b9f5ff690
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-26 12:10:51 +0000

    qpdf: update 10.4.0 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -6,13 +6,12 @@ class Qpdf < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3e204ee0556e243f051a358e37cda87b6b3302a3a957f59595519429a8b37728"
-    sha256 cellar: :any,                 arm64_big_sur:  "705a38342dd732f78053f1bbeee0fdcabc7907db7451a15616d12d0ea0015894"
-    sha256 cellar: :any,                 monterey:       "0f3291d569fe58fa80c1a9fe803f901c85ebdadf9586e282333d18e751987760"
-    sha256 cellar: :any,                 big_sur:        "d58a95848a41828fd8a8cf0f1bbc2965f7d5c4b253a359d99750e53c01777c63"
-    sha256 cellar: :any,                 catalina:       "2896a1a0b6edd8b19b1f596439d60081325f63a4d6ed6cfd859f28790ca8f4d7"
-    sha256 cellar: :any,                 mojave:         "d017c72291915e439affa7000b113169fd3e48bdd1b2d31760fdfe2f3d1950fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b856a4f28b9a0377c40251172c09763548faff2aacd50c5f1e03f9c680434eb"
+    sha256 cellar: :any,                 arm64_monterey: "e9c0330ed2141fc0d002c713ebdc7eed450eb3983689d2d061dad9136b7fb0d7"
+    sha256 cellar: :any,                 arm64_big_sur:  "74e4f00d627fff311d1732e9f279f1f3f6acd2374b0c741b8c83c97f1e1327b4"
+    sha256 cellar: :any,                 monterey:       "f7086d5e00f0e00f76336ced17b7bcf9f3e16a178b0c67eb404656cb24ee7336"
+    sha256 cellar: :any,                 big_sur:        "2baa98138a4f3375d126ddd336e98a1f95ffd21f43504a672d07f4d7a4ba36ff"
+    sha256 cellar: :any,                 catalina:       "5902aa492e2acf73237342c57f86007fdaff0fd333fc4de0514cb7390e4ae4bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c18e99ac413881dd927c712b1a56261922a39d2cb4034ec99b0d129412e3e3e6"
   end
 
   depends_on "jpeg"
