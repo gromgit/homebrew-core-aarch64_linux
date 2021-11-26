00d975613c3b61cafc9224220925b999b6e727ee
Rui Chen <rui@chenrui.dev>
2021-11-26 09:37:04 -1000

    csvtk 0.24.0
    * csvtk 0.24.0
    * specify homebrew test resource
    
    Closes #90016.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/csvtk.rb
+++ Formula/csvtk.rb
@@ -1,8 +1,8 @@
 class Csvtk < Formula
   desc "Cross-platform, efficient and practical CSV/TSV toolkit in Golang"
   homepage "https://bioinf.shenwei.me/csvtk"
-  url "https://github.com/shenwei356/csvtk/archive/refs/tags/v0.23.0.tar.gz"
-  sha256 "b8ccf2b86c815693af1c7d743ca711a68fcdde94d9a9279ac8557fdffa3f2fc8"
+  url "https://github.com/shenwei356/csvtk/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "d944e55d9555733990783bbe45200da5eaef47a13d4eac242ef084d9384d54f8"
   license "MIT"
 
   bottle do
@@ -16,7 +16,7 @@ class Csvtk < Formula
 
   depends_on "go" => :build
 
-  resource "testdata" do
+  resource "homebrew-testdata" do
     url "https://raw.githubusercontent.com/shenwei356/csvtk/e7b72224a70b7d40a8a80482be6405cb7121fb12/testdata/1.csv"
     sha256 "3270b0b14178ef5a75be3f2e3fdcf93152e3949f9f8abb3382cb00755b62505b"
   end
@@ -26,7 +26,7 @@ class Csvtk < Formula
   end
 
   test do
-    resource("testdata").stage do
+    resource("homebrew-testdata").stage do
       assert_equal "3,bar,handsome\n",
       shell_output("#{bin}/csvtk grep -H -N -n -f 2 -p handsome 1.csv")
     end
