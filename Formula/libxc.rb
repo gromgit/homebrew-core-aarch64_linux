9ae93662558a2db716a58e42f1e3f6c6c1a8015c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-26 18:54:14 +0000

    libxc: update 5.1.7 bottle.

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -6,11 +6,12 @@ class Libxc < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "27be1a841edbc6147a1ce89eee4564da177e7337b09d7861c8a99badb7341b35"
-    sha256 cellar: :any,                 monterey:      "2382891c001007f27add5773c6753478fe69e30ec479470f62fe8a5a9478a466"
-    sha256 cellar: :any,                 big_sur:       "e17c6a274cda913afcc1e549c9a2d9c7c864cb1f4f6b44766023a52ca68f5b52"
-    sha256 cellar: :any,                 catalina:      "ace72161096c3243b983c3d8b3d564c6f8149616fd6dbc49da8c65e3f02a0395"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dee43e7f27ab0b94475bf1b7e1e4c1245c61b2e6e1e4f8d3d15386c3c0f1dc01"
+    sha256 cellar: :any,                 arm64_monterey: "d02b7d6fc60d2c4c3c5a7e4a3793ca9e25f215e0c5b1c2b31901bf8e45e6337e"
+    sha256 cellar: :any,                 arm64_big_sur:  "27be1a841edbc6147a1ce89eee4564da177e7337b09d7861c8a99badb7341b35"
+    sha256 cellar: :any,                 monterey:       "2382891c001007f27add5773c6753478fe69e30ec479470f62fe8a5a9478a466"
+    sha256 cellar: :any,                 big_sur:        "e17c6a274cda913afcc1e549c9a2d9c7c864cb1f4f6b44766023a52ca68f5b52"
+    sha256 cellar: :any,                 catalina:       "ace72161096c3243b983c3d8b3d564c6f8149616fd6dbc49da8c65e3f02a0395"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dee43e7f27ab0b94475bf1b7e1e4c1245c61b2e6e1e4f8d3d15386c3c0f1dc01"
   end
 
   depends_on "autoconf" => :build
