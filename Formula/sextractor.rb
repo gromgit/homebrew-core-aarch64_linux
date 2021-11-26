eff598e4a5412d709ba77142f0b278c12c42c19d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-26 19:24:49 +0000

    sextractor: update 2.25.0 bottle.

--- Formula/sextractor.rb
+++ Formula/sextractor.rb
@@ -6,11 +6,12 @@ class Sextractor < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "84793b087afd51c196bbd1d7cc61ff1e2460342f599d190458fa158e80b5fbb2"
-    sha256 monterey:      "3fc5b83adaebe79b2dc230e8f27add68720c230e3728f046ef81e02fad08fbdc"
-    sha256 big_sur:       "bea4796a93527c68fa743efb9b296ec6debed9224002b3383fe224276127f308"
-    sha256 catalina:      "d8e1cce6c9238c4a99cfefde0548b818a62e1b0f0043a0546d6e7b06ae75ab5c"
-    sha256 x86_64_linux:  "267543a12f47364ce29ffbb0cdfb28862aa6f98cc9412528b50115283706291a"
+    sha256 arm64_monterey: "a331cf4bfc144696b3b36a5706e9abc806d498d3de393caeac651afb3ffba9e5"
+    sha256 arm64_big_sur:  "84793b087afd51c196bbd1d7cc61ff1e2460342f599d190458fa158e80b5fbb2"
+    sha256 monterey:       "3fc5b83adaebe79b2dc230e8f27add68720c230e3728f046ef81e02fad08fbdc"
+    sha256 big_sur:        "bea4796a93527c68fa743efb9b296ec6debed9224002b3383fe224276127f308"
+    sha256 catalina:       "d8e1cce6c9238c4a99cfefde0548b818a62e1b0f0043a0546d6e7b06ae75ab5c"
+    sha256 x86_64_linux:   "267543a12f47364ce29ffbb0cdfb28862aa6f98cc9412528b50115283706291a"
   end
 
   depends_on "autoconf" => :build
