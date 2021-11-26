8bcf01d4ad9b548d8bd9a654e6e64569df73983b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-11-26 16:32:02 +0100

    flow-cli 0.29.0
    Closes #90007.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -1,8 +1,8 @@
 class FlowCli < Formula
   desc "Command-line interface that provides utilities for building Flow applications"
   homepage "https://onflow.org"
-  url "https://github.com/onflow/flow-cli/archive/v0.28.4.tar.gz"
-  sha256 "69d5f85e3e1a62c22384ad878c60ae6fd1a6c9449ee603f30b8a8e4b434f5249"
+  url "https://github.com/onflow/flow-cli/archive/v0.29.0.tar.gz"
+  sha256 "b6db258501690153a95be9379be006e315e3a359394238c27687d9dfd7c24641"
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git"
 
