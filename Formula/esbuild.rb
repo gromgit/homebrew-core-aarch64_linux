a33a5ea6fa643cc10af13621140fca7d2be1ff67
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-27 02:18:27 +0000

    esbuild: update 0.14.0 bottle.

--- Formula/esbuild.rb
+++ Formula/esbuild.rb
@@ -8,12 +8,12 @@ class Esbuild < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8838055a6be0f52a0a9393bf1e0740c7c4774d64f94b15d66ef8e8b00f0af048"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8838055a6be0f52a0a9393bf1e0740c7c4774d64f94b15d66ef8e8b00f0af048"
-    sha256 cellar: :any_skip_relocation, monterey:       "b968e55813a34836d4b216cdd0f0a1af4bfd092ff522cc6852de56bd8d14fa62"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b968e55813a34836d4b216cdd0f0a1af4bfd092ff522cc6852de56bd8d14fa62"
-    sha256 cellar: :any_skip_relocation, catalina:       "b968e55813a34836d4b216cdd0f0a1af4bfd092ff522cc6852de56bd8d14fa62"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e3119f8ec20bd221e665a5872fb78723364901c56a4ffa47eb0935a8c3ece720"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a05a7dc07b375bbf3760057a85f6ab69888f1634bf519dffad61a7528946e69f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a05a7dc07b375bbf3760057a85f6ab69888f1634bf519dffad61a7528946e69f"
+    sha256 cellar: :any_skip_relocation, monterey:       "22c9273783b2b2037ef77a537365b78c2c13f67150112665fe23e081b0181881"
+    sha256 cellar: :any_skip_relocation, big_sur:        "22c9273783b2b2037ef77a537365b78c2c13f67150112665fe23e081b0181881"
+    sha256 cellar: :any_skip_relocation, catalina:       "22c9273783b2b2037ef77a537365b78c2c13f67150112665fe23e081b0181881"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b8e73e6bb3315fa0081a3971a849d1e01d7426a029d55f3915186b84a8ebc694"
   end
 
   depends_on "node"
