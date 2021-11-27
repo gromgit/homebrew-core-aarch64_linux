d48f9db11a6cd704b65617622fba9b1f020d1f38
Dheepak Krishnamurthy <me@kdheepak.com>
2021-11-27 19:50:03 +0000

    taskwarrior-tui 0.15.0
    * taskwarrior-tui 0.15.0
    * feat: Update taskwarrior-tui formula to remove dependency on pandoc
    
    Closes #90060.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -1,8 +1,8 @@
 class TaskwarriorTui < Formula
   desc "Terminal user interface for taskwarrior"
   homepage "https://github.com/kdheepak/taskwarrior-tui"
-  url "https://github.com/kdheepak/taskwarrior-tui/archive/v0.14.14.tar.gz"
-  sha256 "36fcf90e9f0ffbc3d7fedfd094de80dffe6775d77f81c9612de9bd6b70e577a6"
+  url "https://github.com/kdheepak/taskwarrior-tui/archive/v0.15.0.tar.gz"
+  sha256 "9f8ed1257ab6d4608dd73f61ee8542ff3b1b112d201b746c1ba90aa939c1c8ed"
   license "MIT"
   head "https://github.com/kdheepak/taskwarrior-tui.git", branch: "main"
 
@@ -20,20 +20,12 @@ class TaskwarriorTui < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "24b0a4651680213c5766b7708fbf99d401604d9d7fead48effa773ed9dcec698"
   end
 
-  depends_on "pandoc" => :build
   depends_on "rust" => :build
   depends_on "task"
 
   def install
     system "cargo", "install", *std_cargo_args
-
-    args = %w[
-      --standalone
-      --to=man
-    ]
-    system "pandoc", *args, "docs/taskwarrior-tui.1.md", "-o", "taskwarrior-tui.1"
-    man1.install "taskwarrior-tui.1"
-
+    man1.install "docs/taskwarrior-tui.1"
     bash_completion.install "completions/taskwarrior-tui.bash"
     fish_completion.install "completions/taskwarrior-tui.fish"
     zsh_completion.install "completions/_taskwarrior-tui"
