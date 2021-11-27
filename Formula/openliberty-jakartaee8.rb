e99ada4d789f907d32274e33b8a31d54b0592616
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-27 07:08:54 +0000

    openliberty-jakartaee8: update 21.0.0.12 bottle.

--- Formula/openliberty-jakartaee8.rb
+++ Formula/openliberty-jakartaee8.rb
@@ -11,11 +11,7 @@ class OpenlibertyJakartaee8 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9edeadbe31f910b8a7c685b56cd6881aa22113ca7645c22d7ed544b8249eab5d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9edeadbe31f910b8a7c685b56cd6881aa22113ca7645c22d7ed544b8249eab5d"
-    sha256 cellar: :any_skip_relocation, catalina:      "9edeadbe31f910b8a7c685b56cd6881aa22113ca7645c22d7ed544b8249eab5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3659de73d710f70e70d52fdcbdefcc2270a1b24ab9a690483aaf573626106f6"
-    sha256 cellar: :any_skip_relocation, all:           "629f99205e46f325880689e2617822813f7bf42490f9423554620c3aeef5302a"
+    sha256 cellar: :any_skip_relocation, all: "4c896b53067214b01cb048aa2c7544c81dac03c54a779cd967db865d452a9083"
   end
 
   depends_on "openjdk"
