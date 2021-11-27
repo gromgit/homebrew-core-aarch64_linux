fada7ad574fb465169d0543dd036147728a08d47
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-27 20:08:34 +0000

    xh: update 0.14.1 bottle.

--- Formula/xh.rb
+++ Formula/xh.rb
@@ -7,12 +7,12 @@ class Xh < Formula
   head "https://github.com/ducaale/xh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd2689d384ddfa20e08580df6aac5c74d45fd0eafa9b190329c059feded8a32d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ef96333d23bd72060dc5864a6a92403a58adff6706aa522a01512eb4d1d7f3d5"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c09d8acf81dd65cdb77e2ca91eb9192087a061fdfe3a409932944a8843b3210"
-    sha256 cellar: :any_skip_relocation, big_sur:        "664ec489f722f0b5c937e339ac091a8bca030eb7305914342a0d615c2d9fbab0"
-    sha256 cellar: :any_skip_relocation, catalina:       "747f138fbe79a82aec78ad36bb55a05b196423696d5d31e3bd658ef0aa137a8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9702ce14bbeb1dd0a195b194997aae441ecbf4bc1916e829358a0aaa666fc36"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eed2e22467a2c24ef9d87122a5086e57dffd72456214508f6dc30dd2577d3043"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "19a45cf73e122ab38ccb44e24076a174600bbf1390d02bd64cae5663ef75ac53"
+    sha256 cellar: :any_skip_relocation, monterey:       "6d7ebf15d31ef47e8013dcab65e320c2d76e211f596271451ef2d75c4d04df12"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7b27925ccf7648036a90c0a5269dc03cdeded08ca4a066aaec0e71e1b6212c34"
+    sha256 cellar: :any_skip_relocation, catalina:       "8d59bc6b194c00224c61e5f82bd1b759fd86e57f427ad317bfa893c67a5f73cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1852af6e619f4aef29c907573c084ccaf62ab2d27b202056532fd5ad7743434b"
   end
 
   depends_on "rust" => :build
