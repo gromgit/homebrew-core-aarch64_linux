15892f8b42541dc63342a8ae1e7c8415d9dbe581
Dan Davison <dandavison7@gmail.com>
2021-11-27 13:43:31 +0000

    git-delta 0.10.2
    Closes #90054.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -1,8 +1,8 @@
 class GitDelta < Formula
   desc "Syntax-highlighting pager for git and diff output"
   homepage "https://github.com/dandavison/delta"
-  url "https://github.com/dandavison/delta/archive/0.10.1.tar.gz"
-  sha256 "96feda5d979bdb2f3b747be1f6f05227e24ac20c0bfa95ead92f6a8b7de3b6a3"
+  url "https://github.com/dandavison/delta/archive/0.10.2.tar.gz"
+  sha256 "e0f71d72eca543478941401bd96fefc5fa3f70e7860a9f858f63bfecf8fd77a5"
   license "MIT"
   head "https://github.com/dandavison/delta.git"
 
