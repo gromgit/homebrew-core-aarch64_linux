d0d761af51070cf537ac8c7346e42c73cfb5abda
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-28 12:34:44 +0000

    git-branchless: add 0.3.7 bottle.

--- Formula/git-branchless.rb
+++ Formula/git-branchless.rb
@@ -6,6 +6,15 @@ class GitBranchless < Formula
   license "GPL-2.0-only"
   head "https://github.com/arxanas/git-branchless.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "f02ca20ad8e2ae17f3c9d70a97ab93512b7c456d3c3a731d800001513205600b"
+    sha256 cellar: :any,                 arm64_big_sur:  "29f7389c6a175326fc9edb033c688e9bda35d3936311bee5524923522d544d46"
+    sha256 cellar: :any,                 monterey:       "27d7843209fa4a91ba769190879239d47019f70099ef409e30ab536e0402b560"
+    sha256 cellar: :any,                 big_sur:        "69d0dfd0467f1764c402b20f75bdf86f2cd328b3a82370078e7ff34c674dc5cc"
+    sha256 cellar: :any,                 catalina:       "a88c1c8649d4010106050bf21d8824ff26343cc4eae87905218b41a1b5052be6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4427f6afba2a7fb7b220545829e95973f7e969dd9d2d5d0cca39fead8250f19a"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
