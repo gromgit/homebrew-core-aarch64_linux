038bf04282c59a98fa18f7d83aed36ff90bc3bea
Thierry Moisan <thierry.moisan@gmail.com>
2021-11-28 10:59:04 -0500

    bash_unit 1.8.0
    Closes #90095.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bash_unit.rb
+++ Formula/bash_unit.rb
@@ -1,8 +1,8 @@
 class BashUnit < Formula
   desc "Bash unit testing enterprise edition framework for professionals"
   homepage "https://github.com/pgrange/bash_unit"
-  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v1.7.2.tar.gz"
-  sha256 "eabb078da18eb4c4eb1db8211d1197bd651e1d837466218338059cc590e33958"
+  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "fe6543b49297dd10db8804f22b75c985e4c29d3ea6c60dd12d2aeb197c45e0d0"
   license "GPL-3.0-only"
 
   bottle do
