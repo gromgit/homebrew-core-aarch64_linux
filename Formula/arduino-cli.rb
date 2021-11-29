e1b3beb57c3812438a949544fbb66024bc52001c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-29 13:16:30 +0000

    arduino-cli: update 0.20.1 bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -13,12 +13,12 @@ class ArduinoCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b9b99a472d7ea2324e5efa98d2ff74499e6f7b83dda59228f9cc27e8e00968ce"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c76d5f7f572f0ebbc96326b28c027f37a7cb439556883ac4afdaa765b3110110"
-    sha256 cellar: :any_skip_relocation, monterey:       "f60eebd2304821b369dd5b261d720416187d8c0845756efcc8d996d48e678e32"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6b9eb2bc7c3ec495cb3721073df4a74f847fee5eba1d2328e208fe1619845648"
-    sha256 cellar: :any_skip_relocation, catalina:       "b4b991b4742bcb7acf3ea5ca4d6cffa641ad35195a867a94ff8df3a201fe74c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9744fd0a4f0c5fa1a161b45ce249fdbde07a8e430cbbc44694dc688e11b1260"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6352459905f8d54431563996850cd644894c5388d71c44f13d1bf789ab14493"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0c1f140641c3cad9a68a484ef3822cf2a0dcfab52d8bb8568b105470fe4aa672"
+    sha256 cellar: :any_skip_relocation, monterey:       "b20d1d0612b96baa9297a5ec3e0f125f9a1af67ce046f6be5c6b548c2159fb84"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d0ae155db75d22103048e660ca955343bec796368ef7cef04691e50b68c912f8"
+    sha256 cellar: :any_skip_relocation, catalina:       "fdb8f090b9472fdc6520baff997c9c540b8a303788d0b72f35f078d6eaa4d891"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "977f9ebad93c919716ab3bea12a6892dda02b7efc6617e8bbceaf7edd8f59362"
   end
 
   depends_on "go" => :build
