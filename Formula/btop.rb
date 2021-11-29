c15826a0d1c6f8ffb35470db2c45ad3992340f3f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-29 15:24:14 +0000

    btop: add 1.1.2 bottle.

--- Formula/btop.rb
+++ Formula/btop.rb
@@ -5,6 +5,15 @@ class Btop < Formula
   sha256 "997376596cc6ca808b5a26f9fe25da274c19c2970aaf757943a009f4c28b758c"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "68e5a9443d0d0c1774d85a89ca3d3df40047cf0b8744fec7f9dc434c7d5e9481"
+    sha256 cellar: :any,                 arm64_big_sur:  "365263da5a8c7918a2ff86c67ea40aa15fcea93ddbae9bfd801621ada42bef19"
+    sha256 cellar: :any,                 monterey:       "0a5cf31a1ecead5b1b96d38e9cfbde21f8c34378cf7902bafed48a30018d33be"
+    sha256 cellar: :any,                 big_sur:        "66a045df8653859b068eeb8ea34be3561fb3367ca459ea7fa499cc614631e6fc"
+    sha256 cellar: :any,                 catalina:       "1a5401de3bdf14498e72dc6a0536863b8e3e9268a213079e8bce082cd725ad02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "70da8dc40274e2babe7cc6805c39ab972b3f34baf4f211161983eda01f5a80ba"
+  end
+
   depends_on "coreutils" => :build
   depends_on "gcc"
 
