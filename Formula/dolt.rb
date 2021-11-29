474a81e290fa5699fa016fd7b0ece01c1426971d
Caleb Xu <calebcenter@live.com>
2021-11-29 14:19:50 -0500

    dolt: clean up std_go_args usage

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -24,8 +24,8 @@ class Dolt < Formula
   def install
     chdir "go" do
       system "go", "build", *std_go_args, "./cmd/dolt"
-      system "go", "build", *std_go_args, "-o", bin/"git-dolt", "./cmd/git-dolt"
-      system "go", "build", *std_go_args, "-o", bin/"git-dolt-smudge", "./cmd/git-dolt-smudge"
+      system "go", "build", *std_go_args(output: bin/"git-dolt"), "./cmd/git-dolt"
+      system "go", "build", *std_go_args(output: bin/"git-dolt-smudge"), "./cmd/git-dolt-smudge"
     end
   end
 
