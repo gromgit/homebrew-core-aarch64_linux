4c4eb26257348c5dd41398d5b827e0c8f03cefa5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-29 14:19:15 +0000

    solana: update 1.8.5 bottle.

--- Formula/solana.rb
+++ Formula/solana.rb
@@ -11,13 +11,12 @@ class Solana < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "212cbf69c58d483249719ded3814b6c23ede1cf5615df20b7f014d63aad56ce0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "266d90cad35a84c5a5bbab6acb869b1a8a114993868fd854665dbe3712261463"
-    sha256 cellar: :any_skip_relocation, monterey:       "61cbe746175e14b950b66b4ca051e46768717897c16995998f1990c05ca64203"
-    sha256 cellar: :any_skip_relocation, big_sur:        "111ee3902e5cbbccedfe655e926f48a2b67a1dd54d61ea0fade8ce61c77412e9"
-    sha256 cellar: :any_skip_relocation, catalina:       "0bd8991781f51e9c615954b6a16f75ef0ea3f331b330acc44f9a8921dd68f7f1"
-    sha256 cellar: :any_skip_relocation, mojave:         "b472efbaeaf50c046e4bc9dad505b9914feac2c5185654eabe783e72cf5bdd0f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a81ba141e190b00be7519fab1924a2f15e104369e3be6166f946781068093052"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "19cbf1dbaad7410a6a1029a7a7baba39394d8d4fe7a839082569473699c86dfa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f39d1433e9ca4cc5ca63448e0df872f1765ab062e6e080bd74e9fe523f72b10"
+    sha256 cellar: :any_skip_relocation, monterey:       "53a45b976264755143a440e23b2eea19ec17e04ba1ee5bbffb4499718b22f055"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3cfc35ef0b80423e4447a85e467ab2a20c1e7bfb754501f13f96b46f849ec4ad"
+    sha256 cellar: :any_skip_relocation, catalina:       "15f19bf42b61e275c25bc997e08fc94756ebf4f9e897d495ff98a200c3096172"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59c8f56a3a9afdbd3405ea581741052f44cb3f7a535fcb5c0761c875e51dcf98"
   end
 
   depends_on "protobuf" => :build
