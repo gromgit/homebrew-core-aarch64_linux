2fb291579357714abfefa53b959ffbcd2029b65b
Rui Chen <rui@chenrui.dev>
2021-11-30 07:16:39 -1000

    rhit 1.5.4
    * rhit 1.5.4
    * update test resource name
    
    Closes #90186.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rhit.rb
+++ Formula/rhit.rb
@@ -1,8 +1,8 @@
 class Rhit < Formula
   desc "Nginx log explorer"
   homepage "https://dystroy.org/rhit/"
-  url "https://github.com/Canop/rhit/archive/refs/tags/v1.5.3.tar.gz"
-  sha256 "fc2bd613a9ded70906f4c2af67a1540ffe6de165efe6aefeffaea97ceed76a82"
+  url "https://github.com/Canop/rhit/archive/refs/tags/v1.5.4.tar.gz"
+  sha256 "93ec6b5415c8d58d288858ad3e07df0561af0f76a0f46909ff1e335e51ad176e"
   license "MIT"
   head "https://github.com/Canop/rhit.git", branch: "main"
 
@@ -18,7 +18,7 @@ class Rhit < Formula
 
   depends_on "rust" => :build
 
-  resource "testdata" do
+  resource "homebrew-testdata" do
     url "https://raw.githubusercontent.com/Canop/rhit/c78d63b/test-data/access.log"
     sha256 "e9ec07d6c7267ec326aa3f28a02a8140215c2c769ac2fe51b6294152644165eb"
   end
@@ -31,7 +31,7 @@ class Rhit < Formula
     require "pty"
     require "io/console"
 
-    resource("testdata").stage do
+    resource("homebrew-testdata").stage do
       output = ""
       PTY.spawn("#{bin}/rhit --silent-load --length 0 --color no access.log") do |r, _w, _pid|
         r.winsize = [80, 130]
