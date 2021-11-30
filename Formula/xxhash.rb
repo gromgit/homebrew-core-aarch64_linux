b80dbbd3dcceb5f6f080d285074c205b34f81f4b
miccal <miccal.matthews@gmail.com>
2021-11-30 08:34:12 +0800

    xxhash 0.8.1
    Closes #90155.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -1,8 +1,8 @@
 class Xxhash < Formula
   desc "Extremely fast non-cryptographic hash algorithm"
   homepage "https://github.com/Cyan4973/xxHash"
-  url "https://github.com/Cyan4973/xxHash/archive/v0.8.0.tar.gz"
-  sha256 "7054c3ebd169c97b64a92d7b994ab63c70dd53a06974f1f630ab782c28db0f4f"
+  url "https://github.com/Cyan4973/xxHash/archive/v0.8.1.tar.gz"
+  sha256 "3bb6b7d6f30c591dd65aaaff1c8b7a5b94d81687998ca9400082c739a690436c"
   license "BSD-2-Clause"
 
   livecheck do
