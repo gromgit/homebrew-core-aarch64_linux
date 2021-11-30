768a89db03fffedccf3760a85108cf8d5f269e0f
Michka Popoff <michkapopoff@gmail.com>
2021-11-30 08:02:29 +0100

    apache-ctakes: fix url, deprecate
    Closes #90169.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apache-ctakes.rb
+++ Formula/apache-ctakes.rb
@@ -1,8 +1,9 @@
 class ApacheCtakes < Formula
   desc "NLP system for extraction of information from EMR clinical text"
   homepage "https://ctakes.apache.org"
-  url "https://apache.claz.org/ctakes/ctakes-4.0.0.1/apache-ctakes-4.0.0.1-bin.tar.gz"
+  url "https://dlcdn.apache.org//ctakes/ctakes-4.0.0.1/apache-ctakes-4.0.0.1-bin.tar.gz"
   sha256 "f741016e3755054876f3bb27f916a8008af27175ef33785638a6292d300c972e"
+  license "Apache-2.0"
 
   livecheck do
     url "https://ctakes.apache.org/downloads.cgi"
@@ -12,6 +13,8 @@ class ApacheCtakes < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, all: "99b42543678adc7a3d3ae931e52130a48fda2f46df7b5de143f7efb708de31ef"
   end
+
+  deprecate! date: "2021-12-21", because: "installs binaries and does not build from source"
 
   depends_on "openjdk"
 
