9186814073a7d13cf80b998dcd4072f203709052
Rui Chen <rui@chenrui.dev>
2021-11-30 07:17:53 -1000

    aliyun-cli 3.0.100
    Closes #90189.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -2,8 +2,8 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-      tag:      "v3.0.99",
-      revision: "a14b5503f993f0c45ec2f7b3512d9d33b5b3d096"
+      tag:      "v3.0.100",
+      revision: "e424d1492437ebda2485c628c186bc135c21d481"
   license "Apache-2.0"
 
   bottle do
