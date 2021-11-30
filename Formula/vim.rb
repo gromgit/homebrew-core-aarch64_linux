c9cbe249e378684c2008d0f21d6b36cc34afa29e
miccal <miccal.matthews@gmail.com>
2021-11-30 08:35:00 +0800

    vim 8.2.3700
    Closes #90156.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.2.3650.tar.gz"
-  sha256 "a2ea5becb025c9bb7a3c64ec757fa6a410261db5c12e12b783b148972c642413"
+  url "https://github.com/vim/vim/archive/v8.2.3700.tar.gz"
+  sha256 "c6ec0fbfc1493c4538050cc154c6285441ad4a5ebc424ab6cad33df0eac89ba9"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
