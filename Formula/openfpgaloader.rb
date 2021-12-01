a1e3fec3950627fe0a0de7ae13d579296876cc1e
Jean THOMAS <25333063+jeanthom@users.noreply.github.com>
2021-12-01 21:08:01 +0100

    openfpgaloader 0.6.0
    * openfpgaloader 0.6.0
    * Add zlib dependency and apply patch for v0.6.0 verison number
    * Fix trailing whitespace
    * Fix trailing whitespace
    * Use zlib from macOS, add note regarding version number patch
    
    Closes #90231.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -1,8 +1,8 @@
 class Openfpgaloader < Formula
   desc "Universal utility for programming FPGA"
   homepage "https://github.com/trabucayre/openFPGALoader"
-  url "https://github.com/trabucayre/openFPGALoader/archive/v0.5.0.tar.gz"
-  sha256 "39c9686bdfcfa96b6bb1d8b37a8a53732372c16cda562036abe9930b61b29e97"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.6.0.tar.gz"
+  sha256 "0971db2302e704966d2e29b8d34e95f553cfd8f81e5ab70ec0533f03f219cf49"
   license "Apache-2.0"
   head "https://github.com/trabucayre/openFPGALoader.git", branch: "master"
 
@@ -20,6 +20,14 @@ class Openfpgaloader < Formula
   depends_on "pkg-config" => :build
   depends_on "libftdi"
   depends_on "libusb"
+  uses_from_macos "zlib"
+
+  # Fix incorrect version
+  # https://github.com/trabucayre/openFPGALoader/pull/144
+  patch do
+    url "https://github.com/trabucayre/openFPGALoader/commit/efeb0d83c479200e359407245f82000ee4f33558.patch?full_index=1"
+    sha256 "7f15ac39f8d079ebe8e73a763bbb4e3d7b441f74df1d5586dbe15af967d5fc33"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
