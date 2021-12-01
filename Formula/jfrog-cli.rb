2a525d8e47e3db0e403696b265fbce653e59f096
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-01 05:06:16 +0000

    jfrog-cli 2.8.0
    Closes #90217.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -1,8 +1,8 @@
 class JfrogCli < Formula
   desc "Command-line interface for JFrog products"
   homepage "https://www.jfrog.com/confluence/display/CLI/JFrog+CLI"
-  url "https://github.com/jfrog/jfrog-cli/archive/v2.6.2.tar.gz"
-  sha256 "d8d84c0b3e421fe2b8d96ec20eefaf52f3a077ac06ba0fc879298cab8c3ae4c3"
+  url "https://github.com/jfrog/jfrog-cli/archive/v2.8.0.tar.gz"
+  sha256 "ec17389afe4b049ef78002b6a7826a398d43aa6198bbebf59c9a075963992f43"
   license "Apache-2.0"
 
   bottle do
