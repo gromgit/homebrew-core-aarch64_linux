a1d7441a83ecc40d078021e5af3684517f842c4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-01 21:07:32 +0000

    libqalculate: update 3.22.0 bottle.

--- Formula/libqalculate.rb
+++ Formula/libqalculate.rb
@@ -6,11 +6,10 @@ class Libqalculate < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "474bd715cfef115d357e6c5b42937ac31ec6d610d726ed433a166fbe5df27b46"
-    sha256 arm64_big_sur:  "c5fa566c1bfcee52df79e2b352c851d06e35a6d62cb5e173f468d1db00196236"
-    sha256 big_sur:        "3691489924b0d10808f7b28f2a9a5144e1695195fe9af5fb18cc6cf34aa9902e"
-    sha256 catalina:       "8fe96ee96986175d97ae18b9e10ed75712f28f7a43167a120d1e9fd5eb42e598"
-    sha256 mojave:         "1c11025d7abd80744bc4077822571c77db47d6999e0ad7c5957ec1a87272bb5c"
+    sha256 arm64_monterey: "dc1d55f6e15c83b805115f537749d2f0a5e25a8d9be59b707d8bb6e6981bc367"
+    sha256 arm64_big_sur:  "0f90a9aba8beb819f4a200b023cf9193849ad974411c29803f706d1830b39140"
+    sha256 big_sur:        "04690ae693a307c9cd858bcf5c2ea139930840a26312734e61dfa2c5d5a8ecaf"
+    sha256 catalina:       "6eaced3c40f631eadefb39fa05f1fa7b8fde2266b3fc8b9505e6de2a75b7183b"
   end
 
   depends_on "intltool" => :build
