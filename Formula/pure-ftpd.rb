220e5a2bd23d15f8dddd1e79a3015d3b33ee45e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-02 19:07:24 +0000

    pure-ftpd: update 1.0.50 bottle.

--- Formula/pure-ftpd.rb
+++ Formula/pure-ftpd.rb
@@ -10,14 +10,11 @@ class PureFtpd < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "cbe3995b51f724a6c022bcd2986fc5f1d01c605a08c5f29cd6cc5b4172da5c38"
-    sha256 cellar: :any, arm64_big_sur:  "2acbe92870213007b37ca771844c12b211ef8559b08536a2fa371dde91a88565"
-    sha256 cellar: :any, monterey:       "709b2b81905bba7e9e43d4b35ab48bc4b898186e950ded42b2e1c8ef0593ecad"
-    sha256 cellar: :any, big_sur:        "a9531c95bc19d063436ba9fbaa982abcc2cd990222261530fc824ff50516e8da"
-    sha256 cellar: :any, catalina:       "aa0a342b50ae3761120370fc0e6605241e03545441c472d778ef030239784454"
-    sha256 cellar: :any, mojave:         "e3a63b9af91de3c29eef40a76d7962cdf8623a8e8992aeb67bdf3948293c450d"
-    sha256 cellar: :any, high_sierra:    "a6a9549f3d8bde87cf01210e9fa29b403ed258246a7928d195a57f0c5ace6988"
-    sha256 cellar: :any, sierra:         "11dfcec52ae727128c8201a4779fc7feea1d547fe86989a621d4ba339f70de92"
+    sha256 cellar: :any, arm64_monterey: "37b7a18770c4050e170e73e9f91b0c4e89796b27b4bc97383f4c01e5d1b845c2"
+    sha256 cellar: :any, arm64_big_sur:  "be2354790a43f2530fade9684e49afdf6127e720e8a1a3396b284aa21e230a48"
+    sha256 cellar: :any, monterey:       "31ec7d058a66adc3e31d5b550e9dffa8d9d4388d125e25a701ac46e6b4f1480a"
+    sha256 cellar: :any, big_sur:        "f8a22572ca75768fa21b1177c77f8f222429726dbc21b8cd9fa061ddb3ecaaaf"
+    sha256 cellar: :any, catalina:       "a3cfa341bc66691b8f5962d493799c5f2f2c68114cc7a101493728991f18b423"
   end
 
   depends_on "libsodium"
