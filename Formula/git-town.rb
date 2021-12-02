450d0bc20fc1be2e584db75c485b0d7ea7a6861e
Caleb Xu <calebcenter@live.com>
2021-12-02 00:45:02 -0500

    git-town 7.6.0
    Closes #90269.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -1,8 +1,8 @@
 class GitTown < Formula
   desc "High-level command-line interface for Git"
   homepage "https://www.git-town.com/"
-  url "https://github.com/git-town/git-town/archive/v7.5.0.tar.gz"
-  sha256 "4f35a2b4d01bea909161722ee1d5d893d5c676e6f3cf9305a864a01c55038be3"
+  url "https://github.com/git-town/git-town/archive/v7.6.0.tar.gz"
+  sha256 "801d16047a5b74ccbe14f300c721289192d6c68115e97852b21a6eec4be71914"
   license "MIT"
 
   bottle do
