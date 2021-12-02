0da1219003839aca5ca7ccf79be10d465b113d87
Caleb Xu <calebcenter@live.com>
2021-12-02 10:18:23 -0500

    strace 5.15
    Closes #90300.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,8 +1,8 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v5.14/strace-5.14.tar.xz"
-  sha256 "901bee6db5e17debad4530dd9ffb4dc9a96c4a656edbe1c3141b7cb307b11e73"
+  url "https://github.com/strace/strace/releases/download/v5.15/strace-5.15.tar.xz"
+  sha256 "ebcac22f297335294dc654425cbc3ce013343bece6da2699e3aec86aee8d72dc"
   license "LGPL-2.1-or-later"
 
   bottle do
