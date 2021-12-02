277d3f5db64a44bdb3c58e9ad9b01a13da92d8df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-02 12:09:59 +0000

    gojq: update 0.12.6 bottle.

--- Formula/gojq.rb
+++ Formula/gojq.rb
@@ -8,13 +8,12 @@ class Gojq < Formula
   head "https://github.com/itchyny/gojq.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc72a817459e0b415b452b354a8bc7c17fab8d3beb773b7fcb7f367eb40298c8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3d5070f13196e75857508da8cc98f179c1b35f95e0d49159833b48ed0281810c"
-    sha256 cellar: :any_skip_relocation, monterey:       "6303241e11b2fe2621d673c04142bd5c839037d815cc37cf901ec084780b74d0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1d41987fde95d422eaa3cb4401e3b608b9f1f2ff4dcf7b93326cac71f1b67a89"
-    sha256 cellar: :any_skip_relocation, catalina:       "1d41987fde95d422eaa3cb4401e3b608b9f1f2ff4dcf7b93326cac71f1b67a89"
-    sha256 cellar: :any_skip_relocation, mojave:         "1d41987fde95d422eaa3cb4401e3b608b9f1f2ff4dcf7b93326cac71f1b67a89"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ebcdb10123fdf5bea3a1a43eece46ba3bf580fffdf632a56f157258614758ed"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "caf7bede815438e456d70e06320a1978446458eb5d04d7be58774a18819cfb18"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "caf7bede815438e456d70e06320a1978446458eb5d04d7be58774a18819cfb18"
+    sha256 cellar: :any_skip_relocation, monterey:       "b54989ae9bb2edc65694af274051d280cc2b334cd3a1f67f945b2293163d210c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b54989ae9bb2edc65694af274051d280cc2b334cd3a1f67f945b2293163d210c"
+    sha256 cellar: :any_skip_relocation, catalina:       "b54989ae9bb2edc65694af274051d280cc2b334cd3a1f67f945b2293163d210c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b9aa91a2f8dd3cd1e9d7ea0aac5364d2292ff167f75a8dbabba30a54720be09"
   end
 
   depends_on "go" => :build
