fff5d208434c6c1b0c94bd93baab62c621856617
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-12-02 19:44:05 +0000

    dbhash: update 3.37.0 bottle.

--- Formula/dbhash.rb
+++ Formula/dbhash.rb
@@ -11,13 +11,12 @@ class Dbhash < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c8fdda6f47ff0d2f6f4f367f1ae8fdfafd8f115090c6d07a610790aae885adf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93a11d1334f5d87a45445373afe1de1fd99ed87f8dd12a9b02dc92bdcf01735e"
-    sha256 cellar: :any_skip_relocation, monterey:       "6b4121bfc0281460baa8e0703578140a73cc304814b8786b7a548c282c7c3c4e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "23eccd9d6af0e7e0e23e708bd4697cb3334299a5d7127d763fe53a90aab6d064"
-    sha256 cellar: :any_skip_relocation, catalina:       "f7e686231f1748f5655b27cf334fe972a7be8d029a284f291ea2daa8f94c51af"
-    sha256 cellar: :any_skip_relocation, mojave:         "56404056dd0a9b8ece371576e64a1b3ad8c9b19f129dd500c38edfbe58c5cccf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "036505f744fca371174119931c174fc1bfd73d4772a46983fe40e8cfb7b4d90e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7f6bf29a10b241b586f159c9d3613466228a2266c4c6528eb52a993b23b9eb44"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8811e317b4c803223d6453e30f2d632d9aaf6656f5011cd83537b9f363d34b8d"
+    sha256 cellar: :any_skip_relocation, monterey:       "a415ba009d76f052a46ccca4083b67904d2220c3f274cf8c4bdc95991e3759fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "19c216e5f189183306af7edd52e632204a2cd28c8185c23cd9234a50d9df47ff"
+    sha256 cellar: :any_skip_relocation, catalina:       "403f7f10e3ad049375042119b0c27c6a1c6dc95b84ed34a0c09117b9ee1fbfc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4544f7acc745cfdf51550e2c99dddc86d7da8a24ed256e12a6bc15ed561d4f1"
   end
 
   uses_from_macos "tcl-tk" => :build
