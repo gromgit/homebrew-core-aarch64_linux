738988ae1c6fc3dfd4aaf69842f7014f6cd1e6dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-02 19:07:10 +0000

    yaegi: update 0.11.1 bottle.

--- Formula/yaegi.rb
+++ Formula/yaegi.rb
@@ -7,12 +7,12 @@ class Yaegi < Formula
   head "https://github.com/containous/yaegi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c865ccdb37587aabc08e4447b0aff8f69ccac03e649e2a7fd4ce64d8a5177a26"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffcba473b6fb14dcb9a2f898b34440d847c71fc3f09dcbbdd43ede863b79e430"
-    sha256 cellar: :any_skip_relocation, monterey:       "8dd4309ece8f59240796b949dc9dbd1c70c85b4e43bd81e0d24346ebed0cf568"
-    sha256 cellar: :any_skip_relocation, big_sur:        "381e56427ac874bd22d20cfc10bb19b12fe5b3249191953465c1087bb96d478b"
-    sha256 cellar: :any_skip_relocation, catalina:       "0fbdbd762d0b588f005e85ac9664d9fb6ea857936ce3ee048b1bf21ca0ac4802"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e2981491cfa1398e77e78e1e609bc4606d73616c6a24843688ad61fae34b9957"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d85345acd82b5acd71f203b920e446a8767464efa0936c0f0c74a4b61e82a0c2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8dd0addba8d3057dd32038c6cf889b742b9b843cf685e03b8abf76dec590a264"
+    sha256 cellar: :any_skip_relocation, monterey:       "e0907b89625ea7a62b7e4df315a1d72d2bc3e725012ede19c5a2c2663f07f429"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e0b34b91c195e41e2e3a50ac0577baf963510aa4c9cdd985dd1706b3a8801a74"
+    sha256 cellar: :any_skip_relocation, catalina:       "7952d22dd03b3a2982ac12dff47103c2f822b46aee35cab7bd5b6e6191f18968"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38833502a774b2ad4acce38d012431e559efabeb42c9569391cca39639dd61e6"
   end
 
   depends_on "go" => :build
