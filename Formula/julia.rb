c8916ee61756db46e294136b6dad749516ba84e5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-12-02 08:52:24 +0800

    julia: improve test
    I think we should be able to stop skipping the `dlopen` test on Linux
    now.
    
    Closes #90237.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/julia.rb
+++ Formula/julia.rb
@@ -216,23 +216,20 @@ class Julia < Formula
     ]
     system bin/"julia", *args, "--eval", "using #{jlls.join(", ")}"
 
-    # FIXME: The test below will try, and fail, to load the unversioned LLVM's
-    #        libraries since LLVM is not keg-only on Linux, but that's not what
-    #        we want when Julia depends on a keg-only LLVM (which it currently does).
-    llvm = deps.map(&:to_formula)
-               .find { |f| f.name.match?(/^llvm(@\d+(\.\d+)*)$/) }
-    return if OS.linux? && llvm.keg_only?
-
     # Check that Julia can load libraries in lib/"julia".
     # Most of these are symlinks to Homebrew-provided libraries.
     # This also checks that these libraries can be loaded even when
     # the symlinks are broken (e.g. by version bumps).
-    dlext = shared_library("").sub(".", "")
-    libs = (lib/"julia").children
-                        .reject(&:directory?)
+    libs = (lib/"julia").glob(shared_library("*"))
                         .map(&:basename)
                         .map(&:to_s)
-                        .select { |s| s.start_with?("lib") && s.end_with?(dlext) }
+                        .reject do |name|
+                          next true if name.start_with? "sys"
+                          next true if name.start_with? "libjulia-internal"
+                          next true if name.start_with? "libccalltest"
+
+                          false
+                        end
 
     (testpath/"library_test.jl").write <<~EOS
       using Libdl
