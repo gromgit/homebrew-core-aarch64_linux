e58e6316c3ec09b955b1ddcf6e79ff8097d28152
Caleb Xu <calebcenter@live.com>
2021-12-01 21:54:54 -0500

    kustomize: clean up std_go_args usage

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -32,7 +32,7 @@ class Kustomize < Formula
         -X sigs.k8s.io/kustomize/api/provenance.version=#{name}/v#{version}
         -X sigs.k8s.io/kustomize/api/provenance.gitCommit=#{commit}
         -X sigs.k8s.io/kustomize/api/provenance.buildDate=#{time.iso8601}
-      ].join(" ")
+      ]
 
       system "go", "build", *std_go_args(ldflags: ldflags)
     end
