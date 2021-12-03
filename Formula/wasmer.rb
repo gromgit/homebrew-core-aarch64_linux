8b5bd06366c5af7b9415a8094c817d8a99816079
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-03 18:04:55 +0000

    wasmer: update 2.1.0 bottle.

--- Formula/wasmer.rb
+++ Formula/wasmer.rb
@@ -7,13 +7,12 @@ class Wasmer < Formula
   head "https://github.com/wasmerio/wasmer.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5b76fec36c982efda40c1fe32298efcc3f773a0407dcfc5df8cfef52ff77b16"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9634700f38301f64d011c3d82b007c125eb485405eb82a4e104dae24ae42a19b"
-    sha256 cellar: :any_skip_relocation, monterey:       "eeaa02d54747124c57919aaaeb2ddbfdaa89ec6866df252861d27f9bd67abb25"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b3cafc7fdb29abaab57fcb77b56eb4440886d8494a35afa96fe28bcea6f68cce"
-    sha256 cellar: :any_skip_relocation, catalina:       "8fe7313e596a0dde1b09e478e1135077028304a65a3acd9c862c45f9cb22f251"
-    sha256 cellar: :any_skip_relocation, mojave:         "472c4c0f35344b0104656ae6f2af41991d4cff8793d1f38b7e6bb597429332c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1e203711d9f105e62878a0e9dc1a828335c8b03c99e1546763efe28272cf728d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b105f5c06e6869991d7db08ebb6d1efdc6a0e72991dc0940b227f9b455e37a56"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4adb6f84ff9ec0a0200bcbcf2fc918df1cca8aa8a6684ce360be863456ec2405"
+    sha256 cellar: :any_skip_relocation, monterey:       "75ee587ce18720671c3626cfe72416d8ea271c6ebeb12f74e3483592e1f1527f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fffa06eff019f74b361a582ffa95020c90b0a2eed336b15726e21cdf15d40c54"
+    sha256 cellar: :any_skip_relocation, catalina:       "13bb98272c3bae45804c66377d2e9d09dcec71529cee606d5fe1c85c119434b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e51a5e0efded01ea16c2cbbc172746e8ea07f412477d48ead3b3ee5f083d36a2"
   end
 
   depends_on "cmake" => :build
