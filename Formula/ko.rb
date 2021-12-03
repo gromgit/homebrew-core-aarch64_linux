d17c2deec6efbe00024ddcca2d5e1224b68ddd95
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-03 09:07:51 +0000

    ko: update 0.9.3_1 bottle.

--- Formula/ko.rb
+++ Formula/ko.rb
@@ -7,13 +7,12 @@ class Ko < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff04985f72b682cf7d76b78a2b2f16952870f2bc564ddbb4f515dc6ac41f891b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05aee246743e81f1249b21b6972168756588d69e55ed6eb7c313527e1e7ef002"
-    sha256 cellar: :any_skip_relocation, monterey:       "88bed1c5b3dc0d9edf724cd2ada63f52f7646885d709cfdaba0833ca11ea42cd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2f7f3143a1033749e9ea454c920f8a8ced7fecb79ad7f51ba610ce03a2a5150e"
-    sha256 cellar: :any_skip_relocation, catalina:       "2ee78e315a67abd4852c5aed5518c34edb19807a42c19bcbd81d9c28063f038f"
-    sha256 cellar: :any_skip_relocation, mojave:         "2632cb6ac6ba4340008befa04b0b1a81415506a3c0b526006183a22636ce14e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2591ec7bdad23739ba49f2835c9b8d8de0b70fd1ae59ce36c0500fba482c1463"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e660aae5d3e7df1cca605319716dd9ec5ef5b6ba1f2cd1309d891f597e3849f5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a5ee4a862a5328cb8bc2ba75f1ac576ed7bdfced22a37aa823dccd0d6653ee0"
+    sha256 cellar: :any_skip_relocation, monterey:       "094e53771b71110f7df68973e84491adb465799f88655f11bc64276481902d8f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f1ab2b90c847b8ebcd4225d21a604c60eb0e7e4cf236e21694d4d8bf6f7d5d9d"
+    sha256 cellar: :any_skip_relocation, catalina:       "5d9c6dc8c007b469f2db46730d100e8b5610da54c574839d2e10ad59aee1911e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "deea93329a7fcc6f595ca491a25091d8dd78a1cb9c8d743fa074a7d3222b787f"
   end
 
   depends_on "go" => :build
