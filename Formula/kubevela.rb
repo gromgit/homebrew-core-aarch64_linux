d1914a80865c565693b85dd53c2c3966abd85d15
guoxudong <guoxudong.dev@gmail.com>
2021-12-03 14:32:44 +0800

    kubevela 1.1.13
    * fix kubevela install
    * fix
    * review-based fixes
    * add test case
    * fix syntax
    
    Closes #90348.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -2,8 +2,8 @@ class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
   url "https://github.com/oam-dev/kubevela.git",
-      tag:      "v1.1.8",
-      revision: "066c448c1ae5a339e4f8dfc17b60085f137e9de4"
+      tag:      "v1.1.13",
+      revision: "8de36ff2950b3c757f912e81d2fc4be3ea4b60af"
   license "Apache-2.0"
 
   bottle do
@@ -18,13 +18,44 @@ class Kubevela < Formula
   depends_on "go" => :build
 
   def install
-    system "make", "vela-cli", "VELA_VERSION=#{version}"
-    bin.install "bin/vela"
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/oam-dev/kubevela/version.VelaVersion=#{version}
+      -X github.com/oam-dev/kubevela/version.GitRevision=#{Utils.git_head}
+    ]
+
+    system "go", "build", *std_go_args(output: bin/"vela", ldflags: ldflags), "./references/cmd/cli"
   end
 
   test do
     # Should error out as vela up need kubeconfig
     status_output = shell_output("#{bin}/vela up 2>&1", 1)
     assert_match "get kubeConfig err invalid configuration: no configuration has been provided", status_output
+
+    (testpath/"kube-config").write <<~EOS
+      apiVersion: v1
+      clusters:
+      - cluster:
+          certificate-authority-data: test
+          server: http://127.0.0.1:8080
+        name: test
+      contexts:
+      - context:
+          cluster: test
+          user: test
+        name: test
+      current-context: test
+      kind: Config
+      preferences: {}
+      users:
+      - name: test
+        user:
+          token: test
+    EOS
+
+    ENV["KUBECONFIG"] = testpath/"kube-config"
+    version_output = shell_output("#{bin}/vela version 2>&1")
+    assert_match "Version: #{version}", version_output
   end
 end
