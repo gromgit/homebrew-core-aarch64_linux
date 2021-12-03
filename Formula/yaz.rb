4d9501c7f584007f13e44d863b3ecc87fc917b5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-03 18:04:42 +0000

    yaz: update 5.31.0_1 bottle.

--- Formula/yaz.rb
+++ Formula/yaz.rb
@@ -21,13 +21,12 @@ class Yaz < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2b5ea1cae0aa4f18fcf09bc1a79df9cd185d94f6f2029315df6f9e6a1bead3ba"
-    sha256 cellar: :any,                 arm64_big_sur:  "88e9c78b500a5bf61ef7996e873a08508725edeb3d4e2ff364c4c18b9f55fb16"
-    sha256 cellar: :any,                 monterey:       "f3884f5e99387f5dfbb9ae482a1e388fb7ccffc35dd7a42460dbace5bfcfd978"
-    sha256 cellar: :any,                 big_sur:        "968de6c53096f9f0accf06488de7e4ab76428a33eb1bcd27866ae34376b82996"
-    sha256 cellar: :any,                 catalina:       "5a2ead8e67e33724130ae46cfcdd880488f99715816ef803f5b30b64cb3cf898"
-    sha256 cellar: :any,                 mojave:         "2e9e5037cc90d375556fd203e8316e81b0df113a48509bf25df6f581c2dc2f9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1df089100f75e362967e449c15abef1b9ce18f6b38421975614ee19b270609db"
+    sha256 cellar: :any,                 arm64_monterey: "d6212b17e34b8d16cc2d891372a84a688e5e8e24c2c656fd99dc58a7bd987939"
+    sha256 cellar: :any,                 arm64_big_sur:  "0d143388ff7e2ef7816bef13239615cdb1abcc25aec1aa2df07683471c609ab1"
+    sha256 cellar: :any,                 monterey:       "d4220d0ce0551467fec33147d76398b87f3c251354038393b7c930da09a513b2"
+    sha256 cellar: :any,                 big_sur:        "3d3326acb000ca66e8d350ee1c6d25e9cd739ad95d01ab1707e2e0f74ab7936e"
+    sha256 cellar: :any,                 catalina:       "9c02ddc39de080423d4dfaad4b33790b54994a741f02b56ab1e7ca72ea2bc54f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "18a02a816cef7d91d43e7d28950aa359dcb96c3a1d3260f222d925b00c0d57ac"
   end
 
   head do
