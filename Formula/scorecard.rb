251039a4c6a2283e8f4efd47be40901fc2f03a3f
Rui Chen <rui@chenrui.dev>
2021-12-04 12:49:14 -0500

    scorecard 3.2.1
    update test
    
    Closes #90439.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -1,8 +1,8 @@
 class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
-  url "https://github.com/ossf/scorecard/archive/v3.1.1.tar.gz"
-  sha256 "94a471ce9002fce392c9b42d0678d6cddf7fb90c4784de4cc8f51ba2b6798c04"
+  url "https://github.com/ossf/scorecard/archive/v3.2.1.tar.gz"
+  sha256 "009183faea28c561f60f2c7bd84e477130d16e55d581bc354879c66e7123cdd2"
   license "Apache-2.0"
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
@@ -25,6 +25,7 @@ class Scorecard < Formula
   test do
     ENV["GITHUB_AUTH_TOKEN"] = "test"
     output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 1)
-    assert_match "GET https://api.github.com/repos/kubernetes/kubernetes: 401 Bad credentials", output
+    expected_output = "InitRepo: repo unreachable: GET https://api.github.com/repos/google/oss-fuzz: 401"
+    assert_match expected_output, output
   end
 end
