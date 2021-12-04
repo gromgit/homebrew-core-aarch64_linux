4efdf3677099b1fa576c8e1ed1fd18470f9d888b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-04 11:08:26 +0000

    hivemind: update 1.1.0 bottle.

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -7,15 +7,12 @@ class Hivemind < Formula
   head "https://github.com/DarthSim/hivemind.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f2935f664ce82d363c4c1aaea8aec7bff335e3e6f60bb682a44e31652d42891c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "978dcd1f37d644e59dd194f4bc1a5f506fce93bef7313e0f8f944e3444b8b26a"
-    sha256 cellar: :any_skip_relocation, monterey:       "79b104911f50bd1cd326cc927753fa00c421ce8fe6a4000fa1704f0dfcc38e32"
-    sha256 cellar: :any_skip_relocation, big_sur:        "792e06f04a8f0742bb4f0467c24d42a92cce9c403827ebc2055a0a891c5c07f8"
-    sha256 cellar: :any_skip_relocation, catalina:       "ed80238e557ca71f115b2b3cd5031bfac16a3d02d8b37b386b629d89dd26af48"
-    sha256 cellar: :any_skip_relocation, mojave:         "8e6f70ca5e0c8eb1e42d47bee207ec5333b453660d808103a36d53c51a7fb59a"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "7a89018774693681975cea22dcdebe35df043507476d1318f195e6d194978693"
-    sha256 cellar: :any_skip_relocation, sierra:         "4aa25b52b5c7fd3dc7ae29ab31cf19eeddde4e7685fd7e9838be0ea8cf09f3c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2c31da1afe090e3752d4445e593cc0c311e9e54e2c18c3c9b7ff704b9eba776"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f8ca06b6d2dde91c66cd6af2396a58b40e6be52e51fa738f24ff2e23376dc60a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f8ca06b6d2dde91c66cd6af2396a58b40e6be52e51fa738f24ff2e23376dc60a"
+    sha256 cellar: :any_skip_relocation, monterey:       "eaedb2b4739dc4e668ae9bf563750e1bb54fea1f16e800f6bf496226dc1daff0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eaedb2b4739dc4e668ae9bf563750e1bb54fea1f16e800f6bf496226dc1daff0"
+    sha256 cellar: :any_skip_relocation, catalina:       "eaedb2b4739dc4e668ae9bf563750e1bb54fea1f16e800f6bf496226dc1daff0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c702f980c6b3a024c20bd50af65de981ab55b360e10013b22aa6797d834f7c85"
   end
 
   depends_on "go" => :build
