7f05563b08558909bfe62fed041899942251e250
Rui Chen <rui@chenrui.dev>
2021-12-04 14:21:01 -0500

    mame 0.238

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -1,9 +1,9 @@
 class Mame < Formula
   desc "Multiple Arcade Machine Emulator"
   homepage "https://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0237.tar.gz"
-  version "0.237"
-  sha256 "92dc0f38ec1a2412c6b9c1403cae610bc13e1d21b4e647ab290d41215a940cae"
+  url "https://github.com/mamedev/mame/archive/mame0238.tar.gz"
+  version "0.238"
+  sha256 "13aa880b097832acf6cda43629f9253bcc8a340748db071028903358ffbe7faf"
   license "GPL-2.0-or-later"
   head "https://github.com/mamedev/mame.git", branch: "master"
 
