739745283923d64c124c97d1ba075a1742ffee60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-04 11:07:59 +0000

    yosys: update 0.12 bottle.

--- Formula/yosys.rb
+++ Formula/yosys.rb
@@ -7,12 +7,12 @@ class Yosys < Formula
   head "https://github.com/YosysHQ/yosys.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "bfc50cfc59cb10e87ed22a64c81f22146d6ab5dbf4da6077d7bbd3b33df4296c"
-    sha256 arm64_big_sur:  "ef246ea3508f9cef89df7f73150bd3594b16c069bf8e0e6caf01ea6c8766d9af"
-    sha256 monterey:       "e53a242cf85bd8947d5fc34e3c1a5e56438271a056871ae5b96d8ad4980af406"
-    sha256 big_sur:        "6267c1a40a1ee92c4be72637e9164dcd8aa8ce1994d91030c934adc8ed23deb6"
-    sha256 catalina:       "38f730623e17290bbdcacd4d5ff0e53a6579c3d5200c23a807a17efec9caf829"
-    sha256 x86_64_linux:   "e3041800f9385f90248245838d00226e1494b9bc54c175440fcdb5a96e0b3b7c"
+    sha256 arm64_monterey: "d81a1e6b5971460c44973f784cc4ef84a202c4b254b67fcbb007db2c62741c33"
+    sha256 arm64_big_sur:  "e002b92c8b1c113c671e7e5397d9abff862cef18726f783b752314523fe06957"
+    sha256 monterey:       "013e9f44355c047c1ea83dab8c53d744ee3b16ce9af637647ace4a6c2c554605"
+    sha256 big_sur:        "1194d82d8e2c0f39c560294c7e2a798a4f3b1758f1760eadc6dc7c348abe4d3d"
+    sha256 catalina:       "aa97c0ca02f2cb1da6eb49b4ed1baf98ac7cbc47402d345fca8e4ac1c9248928"
+    sha256 x86_64_linux:   "04807cf0d621d4279f9f78d7e16c4017ff632129eccd8f8f36166b718da85589"
   end
 
   depends_on "bison" => :build
