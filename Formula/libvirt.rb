8236d43d7706f1ece977c019c4a296f1891e5436
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-04 00:13:08 +0000

    libvirt: update 7.10.0 bottle.

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -12,12 +12,12 @@ class Libvirt < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "5477d07e4d0af7a7fa2d917e4ec3598f882fc799cc25f8cd4b0caab7362f9486"
-    sha256 arm64_big_sur:  "dbafcc4b737970c148bd250225ac8244199deb48f9c04c0c4803d1ae11fde988"
-    sha256 monterey:       "c874537c4fe979ebea31f708cabc54d76d4b0bcb80e53f790e2b5be5f674cae6"
-    sha256 big_sur:        "4e6e779af1394b7fdcf7d9c04e93d7977067fbcb0489a124e178519f6b044ea9"
-    sha256 catalina:       "da0f91b819fa59872ce93f6f31e0b9f6c579f7a595718728e84ba989a2447ad1"
-    sha256 x86_64_linux:   "69f4a06f0b969e8d704e82b086172850800d835ef12609026e9f7a2850d6b695"
+    sha256 arm64_monterey: "9a3965e2aeac2e96d1dd8d145d1b816b3b2b58a991e9439a7eedb29b08669598"
+    sha256 arm64_big_sur:  "1f0aeb8d5f8290bb9c4f934120c686000f9e4a44e754b4af7a060940c24b23da"
+    sha256 monterey:       "69e8451e0d24773bb7212d46a96fa99fdd9ac0fe827c10c87b256adf64ca98a7"
+    sha256 big_sur:        "2ff2402e0b97c6db4c18a70bd0afbe464c6d041668c3b43ee24edbc5c4ec3b2d"
+    sha256 catalina:       "be50ad0a90b7140d72c6ffc8cad5743a1277c2ceedb68e55b7e661f7a2d771a8"
+    sha256 x86_64_linux:   "1d45a7ce93bcb5dac4878e2d7059bbcc0d15a31921f19b69834ec91f68bae098"
   end
 
   depends_on "docutils" => :build
