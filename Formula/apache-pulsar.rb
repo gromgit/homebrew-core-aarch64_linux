9f684681b212fd3f2b82df8d412e328db0cee05c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-05 10:10:59 +0000

    apache-pulsar: update 2.9.0 bottle.

--- Formula/apache-pulsar.rb
+++ Formula/apache-pulsar.rb
@@ -8,10 +8,9 @@ class ApachePulsar < Formula
   head "https://github.com/apache/pulsar.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "830a4cd96a644df024e9127876c4a11a68af0ca655a20d3a15aaecc133bee023"
-    sha256 cellar: :any_skip_relocation, catalina:     "291d72373020ea60dbbc448d5595c1449cc0171dc3ad7af9ea84aa25614797aa"
-    sha256 cellar: :any_skip_relocation, mojave:       "903b07f8634e992bc7e2e05e8dfa40f8dcfe0a5121769a395092cd6250b37fe4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c57adf0e9275c1f60f00066253a961d166be2bd053f18e9f8fd236ccdbdb6dda"
+    sha256 cellar: :any_skip_relocation, big_sur:      "d57e2d4c3126e93233bdeb397c81ff5f22adf619313aee1128c565704ef50e6e"
+    sha256 cellar: :any_skip_relocation, catalina:     "87a73742d93be7e76343adaf13d661f2742430549972edd5fab2d3f24ca44798"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7340abd20c45907c37588330ae71b5dcd7c08b24c26774dc4c1ed682bcbb5b66"
   end
 
   depends_on "autoconf" => :build
