fc9f8202a679f9a536e62ac11dab1996a2f6f021
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-05 11:08:26 +0000

    dhall-bash: update 1.0.39 bottle.

--- Formula/dhall-bash.rb
+++ Formula/dhall-bash.rb
@@ -7,12 +7,11 @@ class DhallBash < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b815cab1ba135f2682f49756077b7712938e9d84b9487b77498f1158d6e0fbb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a024b04b527c5f6863ace794e5f83afe92b1b16203acd23f1e95e3e27901cbcf"
-    sha256 cellar: :any_skip_relocation, monterey:       "23fbafed4e2a6ac0fc36cb93b475b0a385ad9d56d01547decc0e2cf7f2bc1883"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f5063002c22244dd97c80f34b28b9b185fd46d64b11ece003eace16ea54d9a65"
-    sha256 cellar: :any_skip_relocation, catalina:       "49c267f3236157660b58386c17d9f158c1c4df110dc3f112fdc20b565f90334e"
-    sha256 cellar: :any_skip_relocation, mojave:         "ced176f284777f2578f2432fd2943a5e872fbaa0486cde7790e831ad1c116dc8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c6caad7ccfc94810c715b13339c454c19108d0d1b3f5a8d81583b570e2a947bf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05f1e423404871f9f1e7e4d70b8a0cb5624459192576baa759dba81f283e7ca7"
+    sha256 cellar: :any_skip_relocation, monterey:       "45e68da3431c8afe44a0aab27ec6cc2e8df5bd3b900fb216662d70028bb6cce5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "422da387734aade14504d23209baef56153fa79f7f0818a3b43aaf4ff36cef6c"
+    sha256 cellar: :any_skip_relocation, catalina:       "d10895044abb7447837100eb89220c4b1de1a95002e551900498fe68aca5da64"
   end
 
   depends_on "cabal-install" => :build
