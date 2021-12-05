fe29ecafff7373dc83da0ed7d27e01237645048b
Rui Chen <rui@chenrui.dev>
2021-12-05 18:31:59 -0500

    just 0.10.5
    * just 0.10.5
    * just: add `head` branch
    
    Closes #90528.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/just.rb
+++ Formula/just.rb
@@ -1,10 +1,10 @@
 class Just < Formula
   desc "Handy way to save and run project-specific commands"
   homepage "https://github.com/casey/just"
-  url "https://github.com/casey/just/archive/0.10.4.tar.gz"
-  sha256 "81d7e7d5496861a37f8668dbeb845381c41ea0ef5fa087c7b8bd41b70134218d"
+  url "https://github.com/casey/just/archive/0.10.5.tar.gz"
+  sha256 "02e452c0045879161ce820dc66e60d10b3e2cb9b8b152ff86cdd9221eb65d1b2"
   license "CC0-1.0"
-  head "https://github.com/casey/just.git"
+  head "https://github.com/casey/just.git", branch: "master"
 
   livecheck do
     url :stable
