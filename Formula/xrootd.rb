6fca9942a7b181bab8c1f5952c05b71738cef96c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-05 15:07:04 +0000

    xrootd: update 5.3.4 bottle.

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -12,12 +12,12 @@ class Xrootd < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "125b165586aef72874a20577c50a4c4d8e502bb078ac71285ea2e8a52df6cfb4"
-    sha256 cellar: :any,                 arm64_big_sur:  "a409acd75077722ad8ab59a933c86b97e20a373c112a05175e35d0d21061fc08"
-    sha256 cellar: :any,                 monterey:       "9a88e13af60a26b1a90635287913a4cefe068820cda850f232b0d221dafaa7be"
-    sha256 cellar: :any,                 big_sur:        "910b4b7a1e0b39d2868ddcb69c6732b6f74362c3a381d4f70ac7cd880f15f471"
-    sha256 cellar: :any,                 catalina:       "39ffd96d95a4c06f07050c3dacbfc7b1b0759ab44df6cabd62eabd02130dec66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e3d6b6625a404a28060210c03c107fc6c3544c2d33e82e1fc0f37908c6ceaba"
+    sha256 cellar: :any,                 arm64_monterey: "5d80a38bfc640861314bcd39de696bedbe24be00a5217ec78b4d62d3eccaaf16"
+    sha256 cellar: :any,                 arm64_big_sur:  "2bbc10eaa6661e68288bfe0bd38ffc961c397e361ebd17e5e31bf7de0d01776a"
+    sha256 cellar: :any,                 monterey:       "29606bfbc0efd6a40de2248526e4851d89549d7f1dda21978580b661bbde1dbf"
+    sha256 cellar: :any,                 big_sur:        "b0f6240141856122f67070b4539d4cf637df28805c1bff53b691a9814c88baba"
+    sha256 cellar: :any,                 catalina:       "bba5300c2d7aff0046f6e5483955c4dbdb4de6c96ef55a0ae5f312c344d7c779"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d3990bb497f5c8ddc460febf8050309de6b224645caaacf0fab03c960d52862"
   end
 
   depends_on "cmake" => :build
