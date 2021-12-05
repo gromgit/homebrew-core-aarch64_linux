052210556b0e17f3c9dda4e1e617f0f9778b2d9c
Rui Chen <rui@chenrui.dev>
2021-12-05 02:25:41 -0500

    fabric-installer 0.10.2
    Closes #90489.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fabric-installer.rb
+++ Formula/fabric-installer.rb
@@ -1,8 +1,8 @@
 class FabricInstaller < Formula
   desc "Installer for Fabric for the vanilla launcher"
   homepage "https://fabricmc.net/"
-  url "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.1/fabric-installer-0.10.1.jar"
-  sha256 "8e30ea6298791e5171f993c6011f3ce33b507a981d8a0570564533af5b5b6823"
+  url "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar"
+  sha256 "c639cbd6751102be33d8e64a12a882fc4ebead2bc3a71adf1b09bfe41bf1c67a"
   license "Apache-2.0"
 
   # The first-party download page (https://fabricmc.net/use/) uses JavaScript
