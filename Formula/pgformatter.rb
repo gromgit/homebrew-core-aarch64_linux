371fc540e73978719880a8fb6f115bdb6b05ad88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-05 07:07:28 +0000

    pgformatter: update 5.2 bottle.

--- Formula/pgformatter.rb
+++ Formula/pgformatter.rb
@@ -6,13 +6,12 @@ class Pgformatter < Formula
   license "PostgreSQL"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bde351c61b9e139948bc1188cc1ec8a4e2f8afa97b00f5549283162cb4f935f7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5ed75c0fecbdcf5fdc4232888cecd5201b03d1d9c1640d19cf805c61931e3176"
-    sha256 cellar: :any_skip_relocation, monterey:       "9fb7acff043a08195b0f216a32b926d28e7d10bc36a72aa0977500fc4fcacb09"
-    sha256 cellar: :any_skip_relocation, big_sur:        "597a79f382e5be9ec4af90f3e1499686686912c10092943f659c65b31e97b46c"
-    sha256 cellar: :any_skip_relocation, catalina:       "34ea80e6142dfee05233d417713cc517545f7b62555ccf40997acb7b52431383"
-    sha256 cellar: :any_skip_relocation, mojave:         "34ea80e6142dfee05233d417713cc517545f7b62555ccf40997acb7b52431383"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b7ec5b32bafedef008190142250d561b0c577081db09e8df889a101294dc0cb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "18b1513504f1939029ec9ebf722e04bae563820168f5f24b142bb1e33d73038f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b2828aed1d21971a93897d6a25d3f3e4ac440348ff733fb638179a963d5a6e5a"
+    sha256 cellar: :any_skip_relocation, monterey:       "522fc9941d5b0f75f606b21a861f223b1936ae23518de214956cba730c80acc4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a10d8651dfec5a4c58304105bd92da391a07d39f7df6e14a772f3b4901d83584"
+    sha256 cellar: :any_skip_relocation, catalina:       "dde68d30f85eed7ae33dabe850464afe90035e5d5fe01e17c7c544d7415927f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a884b1b995cbf67af35c229c5467ab59319a9e765ff1e64e4568f66bd24a50a4"
   end
 
   def install
