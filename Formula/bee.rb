2ec12bde2382e524f70d2b8cd10b739c77a1f087
Rui Chen <rui@chenrui.dev>
2021-12-04 21:47:35 -0500

    bee 1.84
    Closes #90466.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -1,8 +1,8 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.82/bee-1.82.zip"
-  sha256 "97b542ccfafad7c573cb946d701eda6490f6b328026c932156b1f9afa428ef65"
+  url "https://github.com/bluesoft/bee/releases/download/1.84/bee-1.84.zip"
+  sha256 "e93bf876ef9701381b677892eea5080faf8c53c2bd8bb96fa99ed31f27167e13"
   license "MPL-1.1"
 
   bottle do
