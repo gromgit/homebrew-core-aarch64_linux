85cd43d87ba42e963902c70c424b9870a4f029ac
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-12-06 16:23:52 +0000

    tsduck: add 3.29-2651 bottle.

--- Formula/tsduck.rb
+++ Formula/tsduck.rb
@@ -6,6 +6,15 @@ class Tsduck < Formula
   license "BSD-2-Clause"
   head "https://github.com/tsduck/tsduck.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "ba626578a795195ab1a610d896648cdd544711596c1d900726b16e671644f8d8"
+    sha256 cellar: :any,                 arm64_big_sur:  "f7d79b8d3da117459e4dad007c27d48ddd7b32b6c81e4e07c26d04b4695da88b"
+    sha256 cellar: :any,                 monterey:       "f7b4fc39da4a2d69b190829426e62bd7c5415d9419e3c28ec434dd94f1a68350"
+    sha256 cellar: :any,                 big_sur:        "58b15df049434efe28d2c01f36af3474a08db0859be41685ec897ae0c9dce246"
+    sha256 cellar: :any,                 catalina:       "ba74f5dd825ad36ab54e274971e64723ee571e93a6e3cd81d3fa1f01093b3ede"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82200750cbc0fe3c6201fee8716b80bfe381e376bcb929d5b0ee764cd6e5fdfa"
+  end
+
   depends_on "dos2unix" => :build
   depends_on "gnu-sed" => :build
   depends_on "grep" => :build
