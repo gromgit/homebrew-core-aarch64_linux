7f2a8a3239e5e9e5ed98086b466ce090c0a5b294
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-06 21:07:29 -0800

    pangomm@2.46 2.46.2
    Closes #90625.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pangomm@2.46.rb
+++ Formula/pangomm@2.46.rb
@@ -1,8 +1,8 @@
 class PangommAT246 < Formula
   desc "C++ interface to Pango"
   homepage "https://www.pango.org/"
-  url "https://download.gnome.org/sources/pangomm/2.46/pangomm-2.46.1.tar.xz"
-  sha256 "c885013fe61a4c5117fda395770d507563411c63e49f4a3aced4c9efe34d9975"
+  url "https://download.gnome.org/sources/pangomm/2.46/pangomm-2.46.2.tar.xz"
+  sha256 "57442ab4dc043877bfe3839915731ab2d693fc6634a71614422fb530c9eaa6f4"
   license "LGPL-2.1-only"
 
   livecheck do
