be6689821c2d1b108e0b855a0672ec0597a642ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-07 14:09:08 +0000

    jimtcl: update 0.81 bottle.

--- Formula/jimtcl.rb
+++ Formula/jimtcl.rb
@@ -6,14 +6,12 @@ class Jimtcl < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 arm64_monterey: "660fb1ab526378adfc2960732548ce9d0bfc905cecae74f67a5fd4faf2c272a7"
-    sha256 arm64_big_sur:  "194b77f5eaea45ef59c1c4f7c458a6cb2840867aab53fda18201a9034beab3d9"
-    sha256 monterey:       "edda70c3e5bef14643c61051f11032e364193d4d50aad5e92f0fe9902a066608"
-    sha256 big_sur:        "f2cd0d86c21972a004859681810edd946bff3e87a5c7582b2222ad29b93a562f"
-    sha256 catalina:       "fc4750e0deaf8025402e6a2ed78bd28bcddb98f39012bf3d0c9c8413480505c7"
-    sha256 mojave:         "d9ae5a5d397bf13c6c9c6f355a007454ef05d00e82938d75cdcc549fd7538c0d"
-    sha256 high_sierra:    "4ba0d399f6622e3ce37433c43f314a5cb8546d0a08f2700cb45d67a04d6f1706"
-    sha256 x86_64_linux:   "ae69bc936ae765f61e0c14ccde362c95054c57492fc3650536df4b30b8674f5f"
+    sha256 arm64_monterey: "7731222d5d1fc542b7bcfe89b34b634d58a4748256227aa3ebaf1a486f9cf8f1"
+    sha256 arm64_big_sur:  "3c02ff8a877c59cc3ae8f58b5012a58fa34794fe490df3ab27656f3c5655aa31"
+    sha256 monterey:       "15ffdd12ec698a5bc33868c21a02abdca859e4326900c6bcdf31250edf108419"
+    sha256 big_sur:        "527d9f121dd3f062480b2fef0db374567efa998efda9ece22fadd3cea89a96cc"
+    sha256 catalina:       "32f879425f0e363ccf2f0f23e983e1ca9b7408314c4accfe05a4d20cd6c54ea9"
+    sha256 x86_64_linux:   "f39bff291903330771406c56f255af5a8d0711cf715ca152d9e10b3994dce13f"
   end
 
   depends_on "openssl@1.1"
