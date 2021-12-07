dc1c7b238d9310edc2b5b309e044200c7e102ce7
Rui Chen <rui@chenrui.dev>
2021-12-07 16:50:18 -0500

    k3d 5.2.1
    Closes #90685.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k3d.rb
+++ Formula/k3d.rb
@@ -2,8 +2,8 @@ class K3d < Formula
   desc "Little helper to run Rancher Lab's k3s in Docker"
   homepage "https://k3d.io"
   url "https://github.com/rancher/k3d.git",
-    tag:      "v5.2.0",
-    revision: "3c783bf523011f82decd99638a5a97813e59b36d"
+    tag:      "v5.2.1",
+    revision: "d243bc9dc1465e15af49e8c1fff61fe171a9aba6"
   license "MIT"
 
   livecheck do
