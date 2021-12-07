57ebf8c48ff673eabf071d0ae7cf1c9f3c603026
Jean THOMAS <25333063+jeanthom@users.noreply.github.com>
2021-12-07 20:57:58 +0100

    openfpgaloader 0.6.1
    * openfpgaloader 0.6.1
    * Remove 0.6.0 patch
    
    Closes #90663.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -1,8 +1,8 @@
 class Openfpgaloader < Formula
   desc "Universal utility for programming FPGA"
   homepage "https://github.com/trabucayre/openFPGALoader"
-  url "https://github.com/trabucayre/openFPGALoader/archive/v0.6.0.tar.gz"
-  sha256 "0971db2302e704966d2e29b8d34e95f553cfd8f81e5ab70ec0533f03f219cf49"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.6.1.tar.gz"
+  sha256 "a862a209d696becff915a77512e6a8c22f92d73480a45cc12273d9ad1db60d23"
   license "Apache-2.0"
   head "https://github.com/trabucayre/openFPGALoader.git", branch: "master"
 
@@ -20,13 +20,6 @@ class Openfpgaloader < Formula
   depends_on "libftdi"
   depends_on "libusb"
   uses_from_macos "zlib"
-
-  # Fix incorrect version
-  # https://github.com/trabucayre/openFPGALoader/pull/144
-  patch do
-    url "https://github.com/trabucayre/openFPGALoader/commit/efeb0d83c479200e359407245f82000ee4f33558.patch?full_index=1"
-    sha256 "7f15ac39f8d079ebe8e73a763bbb4e3d7b441f74df1d5586dbe15af967d5fc33"
-  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
