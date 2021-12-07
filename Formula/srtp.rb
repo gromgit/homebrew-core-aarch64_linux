b627cb0bbc4ace1aefdd4845db2537232b244908
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-07 12:11:03 +0000

    srtp: update 2.4.2_1 bottle.

--- Formula/srtp.rb
+++ Formula/srtp.rb
@@ -13,13 +13,12 @@ class Srtp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7150a4fe5c5a5b5e93a8345d9b64376973e0893dfce37ae132421487d7f9277c"
-    sha256 cellar: :any,                 arm64_big_sur:  "3ebd7be1e003bbf9ec2e1c28472e64267dc24c7ca70b293be4453e612ddce665"
-    sha256 cellar: :any,                 monterey:       "5a05ef714a8ba7e266f133d8614d050f9effdbc289ee1af61972f4be5593e7a0"
-    sha256 cellar: :any,                 big_sur:        "404fd8c6f3f32488086abd47324589e572cca109b9eacff92a1aa05d38178c30"
-    sha256 cellar: :any,                 catalina:       "d968a110acc5db4c0090834cd7716b64d932e257226cf11b9e2efafbed263f84"
-    sha256 cellar: :any,                 mojave:         "69a1d9cc8a5781881845db839adba2a1936c19d771a4527e4ba754571454f226"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "592cdd4d0cb837dea4f7b1e05989d316f113f75a95ef7e7198416b7e225f8241"
+    sha256 cellar: :any,                 arm64_monterey: "4e8db729f069ebd07a19f506d28a3f491328e6d6521d81f3176617fcdfcba2b9"
+    sha256 cellar: :any,                 arm64_big_sur:  "da78f2f142b179a51309a70d2a823960198086609fc6f38dfc45559b2e581e13"
+    sha256 cellar: :any,                 monterey:       "b27445521d7cf59ccecbb3842fab8f6b89c54b98fb22eb8707ec68bc9f2926f8"
+    sha256 cellar: :any,                 big_sur:        "1b530260c922cc08de98cad6dc62e124ed05efd190a79364809268cfe566e7a6"
+    sha256 cellar: :any,                 catalina:       "ec45e12636b266303efe3ca1d4cb3cc595955c62ff197a900e6f0f0519bb51e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6916cca964b049c777c87efd01272c76a246349cd93306e57fc68a8edaae426b"
   end
 
   depends_on "pkg-config" => :build
