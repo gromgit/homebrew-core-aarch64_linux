d64c61e7f4dd921be1dd9bbe33af2a1897f1f2f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-07 14:09:08 +0000

    liblouis: update 3.20.0 bottle.

--- Formula/liblouis.rb
+++ Formula/liblouis.rb
@@ -6,13 +6,12 @@ class Liblouis < Formula
   license all_of: ["GPL-3.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    sha256 arm64_monterey: "523882a8945fb690240b790162c0db3a6fff2b53f6ce6edb52c5332755f068d4"
-    sha256 arm64_big_sur:  "fa9afd58b981aa8d297746bca152e8e073ef1d01159fc112bd30b407887328bb"
-    sha256 monterey:       "5ea887b6b7da5615ec254b90b80136329017913f1818a8ae8f33a96ac266f398"
-    sha256 big_sur:        "136591a6b53c1636d8854c7cc60949db3c2d67818c382124353d460ff460c546"
-    sha256 catalina:       "089c1dd0bdbde0466a631f5deac55f8b608f7edff646ab848e2467aadf1cf552"
-    sha256 mojave:         "7bef026745d51ac3f5329a4c985c1d98a89dc43dfe8460c874a7d83b95825889"
-    sha256 x86_64_linux:   "db7173cf91c28efa928f6f32663a83157223d2b54a1983e2dbf0dd78d694dc56"
+    sha256 arm64_monterey: "00c082f554f504af6fd9924ea39bcb66ab922d36acf4011484b0c5949f5337c1"
+    sha256 arm64_big_sur:  "f6b7e820120dea41c87e41ed697e008cec82b571f5b365eca5714772e8e48dcd"
+    sha256 monterey:       "d39f5c6de0b5fbffdcdfa252e3aa24249a06f964cb6bc76ab2b693fde5fd13c0"
+    sha256 big_sur:        "51c4637215b46c332d3d73e91df48c951919fb88e22172272c388466bf96ea26"
+    sha256 catalina:       "f127cdb0b35a49ff1259f2ef4e21c03bdb0937892bb90998938ea7951fd081a2"
+    sha256 x86_64_linux:   "e63216554b01141867847e13767fa7db5b91e290d4f510a9229e561707bbe119"
   end
 
   head do
