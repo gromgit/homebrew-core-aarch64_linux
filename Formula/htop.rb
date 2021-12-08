ade6ab79c90b12f5779e9ff717dd7df3b3feec3f
xxyzz <gitpull@protonmail.com>
2021-12-08 08:35:05 +0800

    htop: remove Python build dependency
    According to htop changelog, Python was removed from their build system in
    version 3.0.2: https://github.com/htop-dev/htop/blob/ff4f44b22ae8d6522ee22599174a6cdd41bc0314/ChangeLog#L317-L326
    
    Closes #90696.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -24,7 +24,6 @@ class Htop < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
   depends_on "ncurses" # enables mouse scroll
 
   on_linux do
