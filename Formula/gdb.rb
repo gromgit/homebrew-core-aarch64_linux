bd236fc4285c71487888646b7e0c72b54e2a8388
Xuehai Pan <XuehaiPan@pku.edu.cn>
2021-12-08 13:45:44 +0800

    gdb: migrate to python@3.10

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -5,6 +5,7 @@ class Gdb < Formula
   mirror "https://ftpmirror.gnu.org/gdb/gdb-11.1.tar.xz"
   sha256 "cccfcc407b20d343fb320d4a9a2110776dd3165118ffd41f4b1b162340333f94"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
   bottle do
@@ -16,7 +17,7 @@ class Gdb < Formula
   end
 
   depends_on "gmp"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "xz" # required for lzma support
 
   uses_from_macos "texinfo" => :build
@@ -46,7 +47,7 @@ class Gdb < Formula
       --disable-debug
       --disable-dependency-tracking
       --with-lzma
-      --with-python=#{Formula["python@3.9"].opt_bin}/python3
+      --with-python=#{Formula["python@3.10"].opt_bin}/python3
       --disable-binutils
     ]
 
