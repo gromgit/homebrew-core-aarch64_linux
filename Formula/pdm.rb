9c4f9fcfb5a140d048c9caa41d59cec04ad9c2bd
Dustin Rodrigues <dust.rod@gmail.com>
2021-12-08 07:28:54 -0500

    pdm 1.11.1
    Closes #90720.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pdm.rb
+++ Formula/pdm.rb
@@ -3,8 +3,8 @@ class Pdm < Formula
 
   desc "Modern Python package manager with PEP 582 support"
   homepage "https://pdm.fming.dev"
-  url "https://files.pythonhosted.org/packages/16/d8/5e0e61d6a97e3bb9d7d5b24bb9c2e502c687d9c5f7ed712b55ba05f2faae/pdm-1.11.0.tar.gz"
-  sha256 "94ceeeb9b9d1c26a7227832ab9665786b34305cb7dcc206e55682a600ebff53b"
+  url "https://files.pythonhosted.org/packages/ac/5d/17295171f7fc132356177eefa6478948584e9232e699d34e482ab2c814e8/pdm-1.11.1.tar.gz"
+  sha256 "3a3af246b0a1261188c40f9b6e46388f1ee0cc118ff745d29bea93d4759c06d1"
   license "MIT"
   head "https://github.com/pdm-project/pdm.git", branch: "main"
 
@@ -56,8 +56,8 @@ class Pdm < Formula
   end
 
   resource "pdm-pep517" do
-    url "https://files.pythonhosted.org/packages/07/b1/2e3a45645fa01776e5cdc31a4cf2e55538f4dd61ea60e552b01e8e4813c1/pdm-pep517-0.9.2.tar.gz"
-    sha256 "0190934efbc072fef4d93c547c602fb5a4bd6bb9e65a77138beaa2ea8d975408"
+    url "https://files.pythonhosted.org/packages/dd/7c/ac07ce6293258f1d186bc85d686984565c0c8ffbd249faa0855cb5d50a52/pdm-pep517-0.9.4.tar.gz"
+    sha256 "da8d85baebd2e4f5bbba1c65dc41813fbe4267775cca33e8ba0d64d22ae5e757"
   end
 
   resource "pep517" do
