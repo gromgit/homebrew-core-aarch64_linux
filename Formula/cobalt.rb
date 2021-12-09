15c44a8bd51fec842152894e533b3135e90521de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-09 23:06:04 +0000

    cobalt: update 0.17.5 bottle.

--- Formula/cobalt.rb
+++ Formula/cobalt.rb
@@ -6,13 +6,12 @@ class Cobalt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7a8bf7ad61aebf1fd9024b53436c113942bee88eb7db23bc57c0c7d968ff8baa"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "49eea0a06c946f0f3ef485cfcf79997c98acce4464a481184ec76978e3f117bb"
-    sha256 cellar: :any_skip_relocation, monterey:       "fca10f812d48c5b51ea5374642b46d3355ca47757fa7f50d6785eff179c9132e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b508cc2831cc0c9b1f708342c16447641e4885c6477403e669dd547a355883a"
-    sha256 cellar: :any_skip_relocation, catalina:       "ee4cb9dca99bdeb3899d6b52eb09b573470df696e7ef16c5898998465827f9dc"
-    sha256 cellar: :any_skip_relocation, mojave:         "d73ffab8a234961c0028268d622bdac4512cff9f17f5f7defc2c831bc91af8da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95b58a7da1188741a7a462bdc33d1ad49b5172924873aed23c372537b26e48bc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a30f9e2d852af5188fd2ca401d9fedc6e144cfd4396443e090f348d66c6fd046"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fcb27c0f93d84fd91df1880b990170533a8465eb502a456395c4bdb2b234b1cf"
+    sha256 cellar: :any_skip_relocation, monterey:       "0f3cef0a5fd121d1e201e2ea4ee5a1710b3dd74c9050206007813a0ffb86ad8a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4faa2823f4e77bee6fd4eb0c33e812ac70ef146a08cee217ff6bee0187372e2b"
+    sha256 cellar: :any_skip_relocation, catalina:       "b6ec26eb9641e547d6ab8ecd4bc22b75ffb9141b1bad4e14b5e4b8b161241744"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "133bf757f77fd3bbfe47cacd434fd0faed6b1b15b123f60a7aa986fd8f9b5a3b"
   end
 
   depends_on "rust" => :build
