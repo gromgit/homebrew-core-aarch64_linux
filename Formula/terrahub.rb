f96495f1db52680ba33f9c50a36dfce9b5db5a52
Eugene I <eugene@mitocgroup.com>
2021-12-09 11:16:52 -0500

    terrahub 0.5.6
    Closes #90791.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Terrahub < Formula
   desc "Terraform automation and orchestration tool"
   homepage "https://docs.terrahub.io"
-  url "https://registry.npmjs.org/terrahub/-/terrahub-0.5.5.tgz"
-  sha256 "b24521b4f5542d50a900eb64e75d5d796d5cc2ccab76090af50cd4be15adfafc"
+  url "https://registry.npmjs.org/terrahub/-/terrahub-0.5.6.tgz"
+  sha256 "a9d6eda1ccc5acb317a62e97d40aac47ba00f8fcbcc9f40ff456d6350b86c463"
   license "MPL-2.0"
 
   livecheck do
