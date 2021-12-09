bb8561b3c5775ccd74158d466039faecae96d29c
Christian Clauss <cclauss@me.com>
2021-12-09 23:22:38 +0100

    cmark-gfm: migrate to python@3.10
    Closes #90831.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cmark-gfm.rb
+++ Formula/cmark-gfm.rb
@@ -17,7 +17,7 @@ class CmarkGfm < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
 
   conflicts_with "cmark", because: "both install a `cmark.h` header"
 
