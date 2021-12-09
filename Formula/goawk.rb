5fa5cda8f756539eca4bbcb6e86d611928e4c46f
zuisong <com.me@foxmail.com>
2021-12-09 16:27:12 +0800

    goawk 1.9.2
    Closes #90763.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/goawk.rb
@@ -0,0 +1,18 @@
+class Goawk < Formula
+  desc "POSIX-compliant AWK interpreter written in Go"
+  homepage "https://benhoyt.com/writings/goawk/"
+  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.9.2.tar.gz"
+  sha256 "c66a6aecee7d35d6768f2bd826916e0a2c1f77d7c426d5c291a2ad0bc4039136"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    output = pipe_output("#{bin}/goawk '{ gsub(/Macro/, \"Home\"); print }' -", "Macrobrew")
+    assert_equal "Homebrew", output.strip
+  end
+end
