d0786bfd32049e797142dce99947849a571fcce3
Rui Chen <rui@chenrui.dev>
2021-12-10 15:43:48 -0500

    ncspot 0.9.3
    Closes #90950.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ncspot.rb
+++ Formula/ncspot.rb
@@ -1,8 +1,8 @@
 class Ncspot < Formula
   desc "Cross-platform ncurses Spotify client written in Rust"
   homepage "https://github.com/hrkfdn/ncspot"
-  url "https://github.com/hrkfdn/ncspot/archive/v0.9.2.tar.gz"
-  sha256 "406eb92593768757b7fb38e54175a367d794f6d880dfc4ac92c1fa9882b88e23"
+  url "https://github.com/hrkfdn/ncspot/archive/v0.9.3.tar.gz"
+  sha256 "ed873d007ce356e8c6eed56226533b686682a98d2a37487668416539d4e10e78"
   license "BSD-2-Clause"
 
   bottle do
