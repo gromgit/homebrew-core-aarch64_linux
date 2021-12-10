08a99a2e69c3c8b180485f3ea0fcb278cb5e2993
rui <rui@chenrui.dev>
2021-12-10 05:49:31 +0000

    sourcery: update 1.6.1 bottle.

--- Formula/sourcery.rb
+++ Formula/sourcery.rb
@@ -7,10 +7,10 @@ class Sourcery < Formula
   head "https://github.com/krzysztofzablocki/Sourcery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "010fd4876fb047ca2bd14feeda78d755987c71ebbd5320d784e585bb37b5779c"
-    sha256 cellar: :any, arm64_big_sur:  "17f5cb2a8f9037b2b3a106f1d11783271174a68ab582782b08118b3c51226b07"
-    sha256 cellar: :any, monterey:       "0cd3dad2ea6d14c4a50706f7d0640265982dda7aa67cdf8f3ad4d011773bc3bc"
-    sha256 cellar: :any, big_sur:        "1d91abe4f760e2a3d232ac1ee3eb24b636a8948049f8f4c4bbb13be4bc2ce072"
+    sha256 cellar: :any, arm64_monterey: "610c06a6f7861b6a215a390c1e069f739d2990164a84d15aac16afdeeff9c4ac"
+    sha256 cellar: :any, arm64_big_sur:  "2ff7c9ee3d1d140289e00df614a69f470b61be3e87d9b2e0b69ebe8cedb7f3cf"
+    sha256 cellar: :any, monterey:       "e4ac024ab746343d0ae2ffff5b1ecdd0a54ccb0666dd5324f9d00b44b6f2fcec"
+    sha256 cellar: :any, big_sur:        "d880261ffceb5d27a1e00c9bb370465341c4f6ab096ef4bb3d64a804d780cd6f"
   end
 
   depends_on xcode: "13.0"
