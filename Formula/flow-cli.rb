584896429adec3164941cfc0569e40b07c4ba332
Adedayo Akinpelu <adedayoakinpelu@gmail.com>
2021-12-10 19:35:20 +0100

    flow-cli 0.30.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #90947.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -1,8 +1,8 @@
 class FlowCli < Formula
   desc "Command-line interface that provides utilities for building Flow applications"
   homepage "https://onflow.org"
-  url "https://github.com/onflow/flow-cli/archive/v0.29.0.tar.gz"
-  sha256 "b6db258501690153a95be9379be006e315e3a359394238c27687d9dfd7c24641"
+  url "https://github.com/onflow/flow-cli/archive/v0.30.0.tar.gz"
+  sha256 "c4dde65ce3994753ba54f75d03a557db2f2ba4a4defe96f999dea8fdf8cd5fa4"
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git"
 
