78e8eea0c5ae45f2d5a5af7981b68c4c634c0457
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-10 00:11:29 +0000

    crun: update 1.3 bottle.

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -6,7 +6,8 @@ class Crun < Formula
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7459526cd9d01d13c4867fa5eefe42c8dfd63d6a13ba64589ba407f86e320ba2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "97bee9bcd63807320f098f03d4a86f774a16a18bb10ab3f5ae067c0f3a764e86"
   end
 
   depends_on "autoconf" => :build
