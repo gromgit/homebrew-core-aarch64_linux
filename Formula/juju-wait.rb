ddd0784892c33d6a07eda5bbfe78e79e5f4f5093
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-10 19:05:32 +0000

    juju-wait: update 2.8.4_2 bottle.

--- Formula/juju-wait.rb
+++ Formula/juju-wait.rb
@@ -9,14 +9,12 @@ class JujuWait < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "c91fa90978e376e52e9bcadc23a86103d4ac976f372917095d22bed638ae6d14"
-    sha256 cellar: :any,                 arm64_big_sur:  "e6b5a2374dcc6b496d184050039dcb233fdc695219518949ccb7b9d65ca98ef6"
-    sha256 cellar: :any,                 monterey:       "99c4f93f810eb1a635f0b146d001d415013da37be2932b38771aa6399a8e4e5c"
-    sha256 cellar: :any,                 big_sur:        "6e73b48dd92446f5cb69ec22117b9907aeff53ea52524fc5a2a095425430e5a3"
-    sha256 cellar: :any,                 catalina:       "82f581e595e843682782737000c28646f59ce7ceda2572a094c616aeddf26709"
-    sha256 cellar: :any,                 mojave:         "3d09fe21c6290f6a055cae35c06e4cdd7682111b295eea2551430df1f618b3d0"
-    sha256 cellar: :any,                 high_sierra:    "5d6a6e8f8fe65ec7f7e0b132b75d85d3b918ec850f9f9b8d0f5742ab6d37f552"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3aaa7755893eb4a178332afd8e340209f533bb54cd164701e7e3abcc9726f033"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "87e0c8b5441eff8bfa72f04efa8c49b0b3eb6236e0f53e4e7bdaa052ae88e1b3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "87e0c8b5441eff8bfa72f04efa8c49b0b3eb6236e0f53e4e7bdaa052ae88e1b3"
+    sha256 cellar: :any_skip_relocation, monterey:       "2496e4760d1ed75a5b6a7bbfdda55ef7da3a770506a3f37cf45c2b473e6f7c7f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2496e4760d1ed75a5b6a7bbfdda55ef7da3a770506a3f37cf45c2b473e6f7c7f"
+    sha256 cellar: :any_skip_relocation, catalina:       "2496e4760d1ed75a5b6a7bbfdda55ef7da3a770506a3f37cf45c2b473e6f7c7f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "65cd144e6f87645a854ef1f1690f49faec1a4d38cc334998f896446d83d2c1ce"
   end
 
   depends_on "juju"
