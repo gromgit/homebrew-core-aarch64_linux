edf788d92cb8493483bcdb853cbcdf9076099d3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-10 19:06:00 +0000

    lc0: update 0.28.0 bottle.

--- Formula/lc0.rb
+++ Formula/lc0.rb
@@ -7,13 +7,13 @@ class Lc0 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "392f9168c6f244ddff5ae49eeaa336fa96c4c76e29f6cfa8932c40defca12889"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "36aa6aad76aee88ce231dc5a0aac948f097055351813351876acd08dfc450a03"
-    sha256 cellar: :any_skip_relocation, monterey:       "d109979fed723569ff979e1cc24db86f1df7c5c8bf8b8bfcf9be6eb971441e28"
-    sha256 cellar: :any_skip_relocation, big_sur:        "994f3b0045579eccda6c5eb1241a4484c202bb9c8528184ba2e4eee770a7e2c5"
-    sha256 cellar: :any_skip_relocation, catalina:       "e37cf24d564b5649d873692e5162a90e7e1840e2346326be86ef0ba25c8b7737"
-    sha256 cellar: :any_skip_relocation, mojave:         "650ff1cdf5e4aad508572322e89010722c92bf30b89ed07f95cdec6dd1fd0140"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d89547675b9eb3c62073100047d9f9d91e51f9d3063655cf38e7f836dd28063"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c3f06ad878fb931803ee05e95c4fa0ab226ae6c9d27aeb7d7031d9f255704bb5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0a8a8a17f78fc04420a88a0b334cc18b4685f902c434c37a381c39746282e713"
+    sha256 cellar: :any_skip_relocation, monterey:       "cc793affcd08b9b407656000cd0878ef0357d7e215454d1ce144df038ae766e2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "86c82b9188b616a92ac8a8883fc6898b103a811c830db4d92cf30f7fe4449042"
+    sha256 cellar: :any_skip_relocation, catalina:       "dc0ceda99050965775c86847e82f726fa4d7ee8eef2113e02cbaba1ea88bf148"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "289dace72bea35c8064c1b29b504164008f674b6cca1d276370138c535aaae0c"
   end
 
   depends_on "cmake" => :build
