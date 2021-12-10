1bd3860d6a0ded75ced5bba502b2be111c957552
Rui Chen <rui@chenrui.dev>
2021-12-09 22:58:59 -0500

    shairport-sync 3.3.9
    Closes #90860.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -1,8 +1,8 @@
 class ShairportSync < Formula
   desc "AirTunes emulator that adds multi-room capability"
   homepage "https://github.com/mikebrady/shairport-sync"
-  url "https://github.com/mikebrady/shairport-sync/archive/3.3.8.tar.gz"
-  sha256 "c92f9a2d86dd1138673abc66e0010c94412ad6a46da8f36c3d538f4fa6b9faca"
+  url "https://github.com/mikebrady/shairport-sync/archive/3.3.9.tar.gz"
+  sha256 "17990cb2620551caa07a1c3b371889e55803071eaada04e958c356547a7e1795"
   license "MIT"
   head "https://github.com/mikebrady/shairport-sync.git", branch: "master"
 
