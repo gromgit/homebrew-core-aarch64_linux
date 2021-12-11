c4f125723f8f821208dca77a1fad849662f11d54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-11 13:10:33 +0000

    resty: update 3.0_1 bottle.

--- Formula/resty.rb
+++ Formula/resty.rb
@@ -10,16 +10,12 @@ class Resty < Formula
   head "https://github.com/micha/resty.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ebf7e5221cc9828ea21047561a3c031c7ca812614789c729d6e3da9c5d84aab"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "25695263de11d4434bf21750710883185d3630a38d201b7050105296cf503f90"
-    sha256 cellar: :any_skip_relocation, monterey:       "02c552f9fa6eefb34fff30809401420f6da33ae2fe35eaa3de0b454b37c595a2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "03999dd31f795810e8febc7e5e206ff549d30f80062b6ea7a8235707b5b101f9"
-    sha256 cellar: :any_skip_relocation, catalina:       "cb5ad84cbacf18282a5ad172a48471d0e7ac007e4799f358fff049b8309aa27f"
-    sha256 cellar: :any_skip_relocation, mojave:         "beee774062f1c32a72f203d0c8c5b0900ce85589c32b385ade712b74e5e1c73b"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "e65c38b826157c35f2e3acd50846be691b6b1a6231a23c62567c24a052d0dc7e"
-    sha256 cellar: :any_skip_relocation, sierra:         "fb754eb95b4cb573eef1807f5dcddab59e021a4326022a9fb8126fb8e80ff247"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "435854dd9bc54f09e46f3f895fc0801ce90a30b23b8d9f109f361f89666fcfe1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0407ee9d0d4ed110872714b36ba8abc7f95568edddd649b4969be978ca3d2826"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7499f7b0641c562bae0884234014f9147cf382a4056f547345c2317922aae89a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae63bd67ce12adff0eb514b088ee59aa9adbe8c8082921d756f7f126ec3b4259"
+    sha256 cellar: :any_skip_relocation, monterey:       "028a0448e86f15286d17d3c26c2d2ac7e10ecb8db09b702fa72bdb3e6b90ab12"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c5d7a031c90f48e1cfd133b39cf38af25eec9c123d781071924ec30b71d89e87"
+    sha256 cellar: :any_skip_relocation, catalina:       "104d7c808669976126d1906ea863b0f55d3b7612a0ecf5bf1314da0b53c8bdd8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5eaed008e68ef6dd2be18998c2ad5e16f41d7930c8c72e19f336597666885ec7"
   end
 
   uses_from_macos "perl"
