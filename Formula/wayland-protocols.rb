24bab991d6026a6e2f47d889bcb3316a1d8ab0fa
Rui Chen <rui@chenrui.dev>
2021-12-11 10:23:10 -0500

    wayland-protocols 1.24
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -1,8 +1,8 @@
 class WaylandProtocols < Formula
   desc "Additional Wayland protocols"
   homepage "https://wayland.freedesktop.org"
-  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.23.tar.xz"
-  sha256 "6c0af1915f96f615927a6270d025bd973ff1c58e521e4ca1fc9abfc914633f76"
+  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.24.tar.xz"
+  sha256 "bff0d8cffeeceb35159d6f4aa6bab18c807b80642c9d50f66cba52ecf7338bc2"
   license "MIT"
 
   livecheck do
@@ -14,19 +14,18 @@ class WaylandProtocols < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "46ff9ea4c52643e3a0c7291e4dcf3badcb3adc64648e6c3e3c949fdca53b4f49"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "wayland" => :build
   depends_on :linux
 
   def install
-    system "./autogen.sh", "--prefix=#{prefix}",
-                           "--sysconfdir=#{etc}",
-                           "--localstatedir=#{var}",
-                           "--disable-silent-rules"
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
