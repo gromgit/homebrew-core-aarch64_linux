0ab398a02c78e7267489c326394b4f1e1b11f42d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-11 19:05:28 +0000

    grpc-swift: update 1.6.0 bottle.

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -12,12 +12,12 @@ class GrpcSwift < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3fec127f5e67589e99c1fb4aa146632512676bfc2de3fa82ddca739ac4cb5e40"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "442e5ec9bbab09b0d5486f9d241cd88369f79a188831e6e2450d885fd213231d"
-    sha256 cellar: :any_skip_relocation, monterey:       "b3ef832720781eec5ef6f5fe13f9a8bce3d048d533c4ebaff1361124a312e45a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3e545a1d2751ad6b2dea6369b9da778364b879eb54796044423fb4a2c2d2b245"
-    sha256 cellar: :any_skip_relocation, catalina:       "7c6b804c466e2f8d5245e9efd2749c57ece2a4f1dc8f30c2fd88774eaa9dace8"
-    sha256                               x86_64_linux:   "24dd2bb28f194eae0629720b07ef4e8fd2f5ad31fd32ba854ab8cbcc63a3ac42"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "90b8187459abcf0dfc5ee94e196aeb5daff6f6b6b6dc2bd6f45e073af01d76bd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0884f22c6c4f2c192df9a0ebf5374df144381d0fd740d98a881e98d21a0b3153"
+    sha256 cellar: :any_skip_relocation, monterey:       "b337cebaf53fc1313fc6fbf7c1d6e4a1fa96e0023e316015d5b4b0b8f0fcdbbf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a83c67c8a4d56b50fe143d75548a39dcd99ea432034ae6f4474e4d9b9d0427e5"
+    sha256 cellar: :any_skip_relocation, catalina:       "028dc85868746308c824d0aa65602a0848a8de5419b22fe491ab15012705e536"
+    sha256                               x86_64_linux:   "d62e5a558da1222f21fc84350a9618aae7501aab2d3a21ae3d24d11e1bbe58b8"
   end
 
   depends_on xcode: ["12.0", :build]
