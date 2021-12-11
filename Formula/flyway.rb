af5d55ae97bff815cff6b58a261ba00499427e85
Rui Chen <rui@chenrui.dev>
2021-12-11 15:42:38 -0500

    flyway 8.2.1
    Closes #91062.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/8.2.0/flyway-commandline-8.2.0.tar.gz"
-  sha256 "608ba389a0500797c907396e590458823723ad452961512965f4d7f5815166ae"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/8.2.1/flyway-commandline-8.2.1.tar.gz"
+  sha256 "9116263e4912c66a6ecda22146796202441ffbf9e90d7a52809e00c5bb08aedd"
   license "Apache-2.0"
 
   livecheck do
