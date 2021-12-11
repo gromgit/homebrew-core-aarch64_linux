ad7aa7c6950acd301eff60f65150da3a96d5d21e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-11 13:10:34 +0000

    thrax: update 1.3.6_1 bottle.

--- Formula/thrax.rb
+++ Formula/thrax.rb
@@ -14,13 +14,12 @@ class Thrax < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "328d82ce4714605a2e1425733bb6a0e39e954df45f29ee2f060468fcde098542"
-    sha256 cellar: :any,                 arm64_big_sur:  "033c1195f0d64bf2844268850d006e79e410d5796f53b38606fdf6a24ba97d93"
-    sha256 cellar: :any,                 monterey:       "563c12082f7958c0facf2ea7605de585b597b161ace80d51ed7c099f56d8701f"
-    sha256 cellar: :any,                 big_sur:        "7120f61434e7851ae51ffba6ca11e7a20a5df0c90e59ed1f12bd831fab0d7bbc"
-    sha256 cellar: :any,                 catalina:       "fa9e11b34775d0e1e7f12e7b9d15f4c22577033e6eabff8cefdbc3f197c64504"
-    sha256 cellar: :any,                 mojave:         "3ab4c18556c41bec206362d9f564f82f6bc11388848fa7cd00242ce3c66118d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e05985642a3ae9cc5922a2f8ced5f8dd15169a95a4de45980bb2cb19bfef1152"
+    sha256 cellar: :any,                 arm64_monterey: "4a2ff8207ecf653a483c97f6968e7cd63e4f37515fc7ff40f3aebbae06179b95"
+    sha256 cellar: :any,                 arm64_big_sur:  "f0e606af59de88f830aaf01bba3939b34c67fbe5afa745ce8c0f276ebb8fce7d"
+    sha256 cellar: :any,                 monterey:       "078fb8f91d67fc1444f1ce6c75297c381a62fa8caee4696ed216c4db2c088e0a"
+    sha256 cellar: :any,                 big_sur:        "b6fb5ed301f1cc233b82efcc07f16c075cb08daafda6c0d7185be7c8db9ff885"
+    sha256 cellar: :any,                 catalina:       "5fd16a93c02fee43cb9b1b2e0130e0d56fbbe531dff7dd375c73660cfd372e06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d0e00d373787505de5368a5004a5dd99e71c033628acb12d8872a9ba97a0eae"
   end
 
   depends_on "openfst"
