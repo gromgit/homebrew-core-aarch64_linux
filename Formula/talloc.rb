8042abdd02910ed8a82f52ac2b80c7505ccc6874
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-11 16:05:47 +0000

    talloc: update 2.3.3 bottle.

--- Formula/talloc.rb
+++ Formula/talloc.rb
@@ -11,13 +11,13 @@ class Talloc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3a640fa631017960607750d8b9f9cabea5ca093622514451f98d7fd2920b9474"
-    sha256 cellar: :any,                 arm64_big_sur:  "d231c44591841730b29b8b28af7792a5b3c8ed1fa393770c68e3706b0abd02e9"
-    sha256 cellar: :any,                 monterey:       "bf7de6ff2b1363c60ab6f0aeb8ffb56297f4ca971b47d5aa6ceb71a1d46e422e"
-    sha256 cellar: :any,                 big_sur:        "a99376ed4ddbe1ae05d843fc473c8eade0603c729f16fc880fe6e95e597b94bf"
-    sha256 cellar: :any,                 catalina:       "41b41189b1177004fab7ab3764b607fd78844228d30182305dd81e33a85d388c"
-    sha256 cellar: :any,                 mojave:         "da663459e3bf1cdaf72935823451b382bd3dee84cb151553f599921edb589d3d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "801236dc8f98f2e0d466ffc4ee0783ff66c78dff53f58b0905d9fe34a1725628"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "a0b8936aab48e933fe8152fbcf62aaae8fe275eaf633da10d8a3cc8850427ab8"
+    sha256 cellar: :any,                 arm64_big_sur:  "416d686e27607c6cd1341da3ee1db0263bcea028b4644a7051c0acd6d2533b9c"
+    sha256 cellar: :any,                 monterey:       "548b62389799fbc33f2457d242149d49e0347d13241591e370f753458eb4a746"
+    sha256 cellar: :any,                 big_sur:        "72693abba030813551e05e682996bea4b9bcd6d9524faab3ab6a6ede826b482a"
+    sha256 cellar: :any,                 catalina:       "1b69ab0589f68b580776015ad297dfdd12b734fdbe062884e9b5b8cb2ebb31b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f3f4cfdd6c4cad9505ef2cdf8522bb2ea0951d09119b78932d638fbd6aa176c"
   end
 
   depends_on "python@3.10" => :build
