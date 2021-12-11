351f70663b1478d7b79615e398abc643048d2cd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-11 13:10:28 +0000

    youtube-dl: update 2021.6.6_1 bottle.

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -9,13 +9,12 @@ class YoutubeDl < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a010e2e9bef8c7bca72c05aeadb4cbad5fc753f055b073ac249da1b32f1ba4a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8488b8408d6382b7bce6359cc18f7e14ab5f250152f18f69d250fbc6dd4f081"
-    sha256 cellar: :any_skip_relocation, monterey:       "7c22c8dc98b7f2a4721fe9037419bbd6c11aa8c9124e23958e83cacc04653107"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f4c3781a6c30a1da5319e581cac99be7fd533295fa8780ef66c3bf696f755e5f"
-    sha256 cellar: :any_skip_relocation, catalina:       "f4c3781a6c30a1da5319e581cac99be7fd533295fa8780ef66c3bf696f755e5f"
-    sha256 cellar: :any_skip_relocation, mojave:         "f4c3781a6c30a1da5319e581cac99be7fd533295fa8780ef66c3bf696f755e5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8cf1a12113ca9116896c792441ac44197a47df59a347eb09685dd6476fdb3ef3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f0ad5e9003ad43032fd754f626a27a2ef277bedd41952f33e9b5a74d8e22d269"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f0ad5e9003ad43032fd754f626a27a2ef277bedd41952f33e9b5a74d8e22d269"
+    sha256 cellar: :any_skip_relocation, monterey:       "f02c4dcd2a08ed53a58ffd43d8a59ac7e41cae058e24358593476492388aa3a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f02c4dcd2a08ed53a58ffd43d8a59ac7e41cae058e24358593476492388aa3a9"
+    sha256 cellar: :any_skip_relocation, catalina:       "f02c4dcd2a08ed53a58ffd43d8a59ac7e41cae058e24358593476492388aa3a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec8765dadcc82d42b45cb82f4f83d0dec0c98ddccae8072314564860dba0b4cc"
   end
 
   depends_on "python@3.10"
