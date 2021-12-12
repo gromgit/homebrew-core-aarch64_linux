78c8c2579e6ae93ef4a30b7de25c77608882cab8
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-12-12 18:17:18 +0000

    gst-plugins-ugly: update 1.18.5 bottle.

--- Formula/gst-plugins-ugly.rb
+++ Formula/gst-plugins-ugly.rb
@@ -12,13 +12,12 @@ class GstPluginsUgly < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "74c50e6cf754608da7ae14b0201b99a08a1e5b34ddbc32afa854af463ba21bc7"
-    sha256 arm64_big_sur:  "93577cdbd18ee2cbdf026104ad07e9c9ae510c28a487a5fe4fbb18743ff596e8"
-    sha256 monterey:       "838ab9145393003b51a80892a3c91083d2f15a5eaef93f58bd8a7599d74dfbef"
-    sha256 big_sur:        "0a6d18fa102bcea7e8e096a8b114fa415a32c2c6cd477fdaa68255ffb119ac28"
-    sha256 catalina:       "3bf8b148e0901057104047b7392c501986096858e287cc4f11299933f1708931"
-    sha256 mojave:         "beb6b703ebad1a546c0ca4a9e9253e20bdb78c3aeba4231fdd1a645ab8fd11dc"
-    sha256 x86_64_linux:   "4793a5155acb669f37d6e29f4fc48470b9f7ac8bb9332f9cfbba6a53e372f617"
+    sha256 arm64_monterey: "17d964846b2e566aef42536dfd1656e142b932d3f012ea35a12fc21caaf706eb"
+    sha256 arm64_big_sur:  "1eb49d0508fea6b8b6cfc79320851f4218c58c965caa84fb3acb772d599dbb5e"
+    sha256 monterey:       "a0a8716e0f2852b9801e491ce9358368aca40db23930d2469449af54ab25214e"
+    sha256 big_sur:        "4d4929eddbc0d179821e23b1d464ba6b5e9016f8bfdc46d508fea492aa2d17c1"
+    sha256 catalina:       "61c6247e61ea354c16b781fee374c907012f2ad11640ee4de99cbb15f5fb6c9f"
+    sha256 x86_64_linux:   "dc83215500215a27139c960d14f91428aff6dd1f58fb687e770ad48372f8209b"
   end
 
   depends_on "meson" => :build
