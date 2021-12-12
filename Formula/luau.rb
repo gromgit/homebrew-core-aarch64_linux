81594923a11cf741580be8df87f157e7d5c98a9c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-12 15:06:11 +0000

    luau: update 0.507 bottle.

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -7,12 +7,12 @@ class Luau < Formula
   head "https://github.com/Roblox/luau.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "849be2ebf882eff02ef560159838417458b0470a48ebc0e5ca5c529728b81687"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cc950cd53f30705fdee60e95eb322207effa41d7256551365aa93d19a2f93b51"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f15eebbb729a88408b21fec92f4bd6fc25ebe6bc4bacf1459727e94d24ef166"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7e09fd4cc856b0022e6cd05c56b06b77a64bfe4b5bad042757a020ab69447156"
-    sha256 cellar: :any_skip_relocation, catalina:       "83fe0d98b947304153231ff7dc0b6e14b7e7a73eca72e26d1275ad594310b556"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57f2a47a5416087ea65f6b26629d3fa22ce12cc1afb035ad387e80685350bb6f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6a9785cad4d4cf69a3147cb31d691ebbcd2c6cd7eae1c230fe3b671de802f3b0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89ae2a5e3c7531d02d58e7823d943b42479cc030dab9c7c431c38e3985f5e329"
+    sha256 cellar: :any_skip_relocation, monterey:       "98ec35c16b87f8c4950f66f518571d094e00dbb4e6313713e69f0fa32f5cf1cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c9017b21070e3343fecc579162056c1788ccfab517bbb82edf92a06bc2cc992f"
+    sha256 cellar: :any_skip_relocation, catalina:       "316fc171bcd92689971bfd2b349e0fa66aed7752bf804f4ad84fccdd91742d42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2997dd1517af84e576005f6501bb5a7131739cd9e6c252d827b3b6eb0e4071cb"
   end
 
   depends_on "cmake" => :build
