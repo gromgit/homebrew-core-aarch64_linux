dc6c94109cb353319aa2bdc2258e55ae88e23f3b
Rui Chen <rui@chenrui.dev>
2021-12-12 11:25:54 -0500

    keptn 0.11.3
    * keptn 0.11.3
      update build
    
      update test
    * fix test for linux
    
    Closes #91176.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/keptn.rb
+++ Formula/keptn.rb
@@ -1,8 +1,8 @@
 class Keptn < Formula
   desc "Is the CLI for keptn.sh a message-driven control-plane for application delivery"
   homepage "https://keptn.sh"
-  url "https://github.com/keptn/keptn/archive/0.9.2.tar.gz"
-  sha256 "0ba9f79f3428baa2453622e19eeff2a325c36b35a5b22f60b4589ef77cc77826"
+  url "https://github.com/keptn/keptn/archive/0.11.3.tar.gz"
+  sha256 "48c38569735aca7ba287c4cc16a02fe27cabec24cc077bfba4798c02f6972e98"
   license "Apache-2.0"
 
   bottle do
@@ -20,7 +20,7 @@ class Keptn < Formula
   def install
     ldflags = %W[
       -s -w
-      -X main.Version=#{version}
+      -X github.com/keptn/keptn/cli/cmd.Version=#{version}
       -X main.KubeServerVersionConstraints=""
     ]
 
@@ -35,7 +35,14 @@ class Keptn < Formula
 
     assert_match "Keptn CLI version: #{version}", shell_output(bin/"keptn version 2>&1")
 
-    assert_match "This command requires to be authenticated. See \"keptn auth\" for details",
-      shell_output(bin/"keptn status 2>&1", 1)
+    on_macos do
+      assert_match "Error: credentials not found in native keychain",
+        shell_output(bin/"keptn status 2>&1", 1)
+    end
+
+    on_linux do
+      assert_match ".keptn/.keptn____keptn: no such file or directory",
+        shell_output(bin/"keptn status 2>&1", 1)
+    end
   end
 end
