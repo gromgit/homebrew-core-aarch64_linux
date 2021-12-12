081f395a2c3e2245de280999044c114bac5e0fa5
Rui Chen <rui@chenrui.dev>
2021-12-12 00:41:28 -0500

    mockolo 1.6.3
    Closes #91126.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mockolo.rb
+++ Formula/mockolo.rb
@@ -1,8 +1,8 @@
 class Mockolo < Formula
   desc "Efficient Mock Generator for Swift"
   homepage "https://github.com/uber/mockolo"
-  url "https://github.com/uber/mockolo/archive/1.6.2.tar.gz"
-  sha256 "37bd8639349fb292e4244593785be0c63d16fd91b601c10d25a48c0964dd15a2"
+  url "https://github.com/uber/mockolo/archive/1.6.3.tar.gz"
+  sha256 "87384b96d7e9c824c3debb4829c9f003005df946efaa8a4d3c6dbb3794c7efd0"
   license "Apache-2.0"
 
   bottle do
