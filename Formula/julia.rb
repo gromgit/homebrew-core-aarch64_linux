158b62a2e71a763c6f6d8d419c1dc665eea51af4
Rui Chen <rui@chenrui.dev>
2021-12-12 16:21:04 -0500

    julia: revision bump (libunwind 1.6.2)
    Closes #91177.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/julia.rb
+++ Formula/julia.rb
@@ -4,6 +4,7 @@ class Julia < Formula
   url "https://github.com/JuliaLang/julia/releases/download/v1.7.0/julia-1.7.0.tar.gz"
   sha256 "8e870dbef71bc72469933317a1a18214fd1b4b12f1080784af7b2c56177efcb4"
   license all_of: ["MIT", "BSD-3-Clause", "Apache-2.0", "BSL-1.0"]
+  revision 1
   head "https://github.com/JuliaLang/julia.git"
 
   bottle do
