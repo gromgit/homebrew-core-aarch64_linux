fd3cf1540b9c1f5e109aa5ec1a778859d408e5df
Rui Chen <rui@chenrui.dev>
2021-12-12 00:56:31 -0500

    luvit 2.18.1
    Closes #91133.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/luvit.rb
+++ Formula/luvit.rb
@@ -1,8 +1,8 @@
 class Luvit < Formula
   desc "Asynchronous I/O for Lua"
   homepage "https://luvit.io"
-  url "https://github.com/luvit/luvit/archive/2.18.0.tar.gz"
-  sha256 "3c6824878189ca41059d6d4cd8b1646de0ec6b4be1de71b2084f98c36c38e84e"
+  url "https://github.com/luvit/luvit/archive/2.18.1.tar.gz"
+  sha256 "b792781d77028edb7e5761e96618c96162bd68747b8fced9a6fc52f123837c2c"
   license "Apache-2.0"
   head "https://github.com/luvit/luvit.git"
 
