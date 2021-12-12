5b7043d2096b98ca1214d55ca535b6e2e436df6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-12 02:18:16 +0000

    hstr: update 2.4 bottle.

--- Formula/hstr.rb
+++ Formula/hstr.rb
@@ -6,13 +6,12 @@ class Hstr < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0732a00ef8559bcf9674782e5d47ab13fabb7fb55edef9106667624d85707ac8"
-    sha256 cellar: :any,                 arm64_big_sur:  "6f1758717c6c34317a775a0975864d66bdd2696f491859627a00641966ce6ece"
-    sha256 cellar: :any,                 monterey:       "7050eb806ea8b03a5a70cfec84203f7e94830edeb7f2ccdd2b734ed50c4c0f3e"
-    sha256 cellar: :any,                 big_sur:        "23a7e98369fa1ef412f6f374d24a1828b5131425b39b828688004cf6e4cedda8"
-    sha256 cellar: :any,                 catalina:       "eb5a750b04ba6c1908bde96a1bd7fb8733a80632953eeb5d43ad51b7747cc191"
-    sha256 cellar: :any,                 mojave:         "723f7aec818080a72e40a3eda2aad8669d5d952927a7894718946cc0b788ff27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab96608841a90734f14748aa02ef5c614c1dc0f6c6b61ada6d06a736a6656689"
+    sha256 cellar: :any,                 arm64_monterey: "d96af0e7736760fc87ed5e2ac7060305b62691c4e29e5767d95b01efffc764fb"
+    sha256 cellar: :any,                 arm64_big_sur:  "e03468773a860e4f629ddd11fbc02354209ee4eac78992e66c922accc13124d6"
+    sha256 cellar: :any,                 monterey:       "cc58407f21f17b5d2f76ca4c47dbc316109c290c939f96a61197ba2e468fa5bb"
+    sha256 cellar: :any,                 big_sur:        "53d82b6cef30db4afbe9d8cf00a28a29f6c1a7766c6acb2cd965c72a6dee0ff7"
+    sha256 cellar: :any,                 catalina:       "7c3ac14533a2329de8563a8d59887c8459659496fbbe47570183ced1b2b7e96e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b45c4fdce56c1ac4f334e136664112008e86ee845f0bdb63a153c4b9edabed62"
   end
 
   depends_on "autoconf" => :build
