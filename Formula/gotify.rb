783ca831a4359532109606f38454c4f2e8fdee0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-12 20:06:39 +0000

    gotify: update 2.2.1 bottle.

--- Formula/gotify.rb
+++ Formula/gotify.rb
@@ -7,12 +7,12 @@ class Gotify < Formula
   head "https://github.com/gotify/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b7b63037731fd33f8a8eaee905dcfa10e337a67b3a4863b7fc1f7a6e5a5565ec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9cd577a557fc7377191c9ca72a97522a6e1419f840cbbf7258640b25a80512ad"
-    sha256 cellar: :any_skip_relocation, monterey:       "ed62bf203d24de174a32d5c74a1f60f84bd19bbf64256e831cf696f951b6a68f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7dd11a2030ff16042ba0f8ce680ae550ee20568444b94ebed12cd95c7dba75a9"
-    sha256 cellar: :any_skip_relocation, catalina:       "29fe9342e485691f43130afb0665776245a7b6d12d2c907ebbe1c30181d6a731"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa631cdc0e667a7d9ead93739fecca5e04ad1a1b042bd46ee6450f3c231bb565"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cdcc9e52ecb4334c2f4f8da73a8aaf9ef1e3d948d3be34687e531d9734cd7eca"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b5d095346ac0d96c3cf2878dab37dbcf6c6e6193862c60bb6de0f375c46f9aa"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ad6853ff2d82d58c5c0d95d03d5de5a2f3f76efe0283adf9ce7e3a806a2f2ea"
+    sha256 cellar: :any_skip_relocation, big_sur:        "582263f5aeb1a70a319c62324d47a5432cc6d85aafb07e8aad2f981f26f7e781"
+    sha256 cellar: :any_skip_relocation, catalina:       "996726129acb0c6c602363b2864765f8cea3c3b3bf8976a33a33212366a64363"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "899b0f7b64c293f6053be2d747661a5baff2d9783b505dbf5b4d118ead2fb29c"
   end
 
   depends_on "go" => :build
