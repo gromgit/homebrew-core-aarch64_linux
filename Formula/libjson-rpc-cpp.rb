fbcadf8346cc1c0c127a8644966f914e0fccf2d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-12 15:06:02 +0000

    libjson-rpc-cpp: update 1.4.1 bottle.

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -7,12 +7,12 @@ class LibjsonRpcCpp < Formula
   head "https://github.com/cinemast/libjson-rpc-cpp.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2d5595ba06ed6242ea57d93dffc02fbdd17acf0a59ef9c49a3fd5b248e0fd6cc"
-    sha256 cellar: :any,                 arm64_big_sur:  "8f6b4c50f9a8ba7f677f13ee5c3b8edbe18595c738093483ca77ae081949e7d9"
-    sha256 cellar: :any,                 monterey:       "b55de462ab58927008ac9aab8dfd734f17fe3d2ae8c784cabcc8516f9c459e27"
-    sha256 cellar: :any,                 big_sur:        "2665a82edb0caedbfdc16db5c9737bf9ce7d010312e4ef6959cac058ee880030"
-    sha256 cellar: :any,                 catalina:       "168b837f3b9eb9000703524bb3b61a04db09707de52d5bb6891e3a03f558da19"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "388a0dfab5687e1031a4cf317aaae62c5e2673a58197da76547ac8b6e15cb1fc"
+    sha256 cellar: :any,                 arm64_monterey: "92aa868394bc65515e1c39a53afa9a28b5ce68fbad43cc41fbb89850567b5b01"
+    sha256 cellar: :any,                 arm64_big_sur:  "38c70b350ea44a23e38ce549b998e7314dd85528bb89f63acc92c31f983e5ceb"
+    sha256 cellar: :any,                 monterey:       "44c292ca252bd482fce41e1b0587a55671a3c5085ee46d55345357b3d2386079"
+    sha256 cellar: :any,                 big_sur:        "e7e1d233b08f2f8475c41caa0976b0543c7208f6ce946d9b2e9700ec9bf35bbc"
+    sha256 cellar: :any,                 catalina:       "7812baf2337ad266cd66c113461f79f0fd1035a1ef44c698178efe6415f4285c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "72909ebf97a0de14f83f54bc1ac1d6cc8b1cfce2bb96754e522705fad66412d3"
   end
 
   depends_on "cmake" => :build
