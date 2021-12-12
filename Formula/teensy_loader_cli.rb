bb8408362dd2dd163f66cb138e7f1747408bc085
Rui Chen <rui@chenrui.dev>
2021-12-11 21:09:40 -0500

    teensy_loader_cli 2.2
    Closes #91088.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/teensy_loader_cli.rb
+++ Formula/teensy_loader_cli.rb
@@ -1,10 +1,9 @@
 class TeensyLoaderCli < Formula
   desc "Command-line integration for Teensy USB development boards"
   homepage "https://www.pjrc.com/teensy/loader_cli.html"
-  url "https://github.com/PaulStoffregen/teensy_loader_cli/archive/2.1.tar.gz"
-  sha256 "5c36fe45b9a3a71ac38848b076cd692bf7ca8826a69941c249daac3a1d95e388"
+  url "https://github.com/PaulStoffregen/teensy_loader_cli/archive/2.2.tar.gz"
+  sha256 "103c691f412d04906c4f46038c234d3e5f78322c1b78ded102df9f900724cd54"
   license "GPL-3.0-only"
-  revision 2
   head "https://github.com/PaulStoffregen/teensy_loader_cli.git", branch: "master"
 
   bottle do
