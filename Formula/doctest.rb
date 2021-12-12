8259c1d19700072d7b697b38bdd88e32218860d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-12 16:05:21 +0000

    doctest: update 2.4.7 bottle.

--- Formula/doctest.rb
+++ Formula/doctest.rb
@@ -6,12 +6,7 @@ class Doctest < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5271dde9d139e921ef3618dffa2475896123cfc9bb868cd9c1013d8fd66f8529"
-    sha256 cellar: :any_skip_relocation, big_sur:       "48897cfd764b2b3b241c67a8aa8d6f0d203846fa42939a35b2de64e96aff5031"
-    sha256 cellar: :any_skip_relocation, catalina:      "d72ed1e3e4f5897da6e9686cd048f38ad22307d0414b9cbc7496ca9d1f161c63"
-    sha256 cellar: :any_skip_relocation, mojave:        "54e5a4919e7fb93c7eb8cf7001cccf0fc90cac50531138c3966d2c4174953283"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2e9c84fbfb8698c529078bf132aef8686cc65931ea8f0ca86e0bc8e8e2317ef"
-    sha256 cellar: :any_skip_relocation, all:           "da248b97f7c9829354a6dec9594ed7f1d636816ff1f9b04e0f1634ee04beafa4"
+    sha256 cellar: :any_skip_relocation, all: "b2fbb219032438f22fcf0093c89b744f243a0e5199001e19a808fa1b62a10798"
   end
 
   depends_on "cmake" => :build
