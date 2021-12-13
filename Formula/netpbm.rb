c4c8428aa2896dfd73558c89fc63c5f46f15e629
Rui Chen <rui@chenrui.dev>
2021-12-12 23:18:19 -0500

    netpbm 10.86.27
    Closes #91206.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4180"
-  version "10.86.26"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4198"
+  version "10.86.27"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
