c059d86f46183e888d2904b7026978ca34ee6e4a
Caleb Xu <calebcenter@live.com>
2021-12-13 09:52:48 -0500

    scamper 20211212
    * scamper 20211212
    * scamper: remove build patch
    
    Closes #91233.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -1,8 +1,8 @@
 class Scamper < Formula
   desc "Advanced traceroute and network measurement utility"
   homepage "https://www.caida.org/catalog/software/scamper/"
-  url "https://www.caida.org/catalog/software/scamper/code/scamper-cvs-20211026.tar.gz"
-  sha256 "be75919a59d73227598260668b7ed75f6a710507310ca9827789b18cd7dda684"
+  url "https://www.caida.org/catalog/software/scamper/code/scamper-cvs-20211212.tar.gz"
+  sha256 "ae4f84aef28373701568a9e57ec44a31ec20871c33c044a5272de726acbd2d13"
   license "GPL-2.0-only"
 
   livecheck do
@@ -21,12 +21,6 @@ class Scamper < Formula
 
   depends_on "pkg-config" => :build
   depends_on "openssl@1.1"
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
 
   def install
     system "./configure", *std_configure_args
