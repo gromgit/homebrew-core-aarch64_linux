a66d03c413b27ea8563e2c6d22da17039225c4cc
Rui Chen <rui@chenrui.dev>
2021-12-12 22:37:55 -0500

    nef 0.7.0
    * nef 0.7.0
    * fix syntax
    
    Closes #91201.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nef.rb
+++ Formula/nef.rb
@@ -1,8 +1,8 @@
 class Nef < Formula
   desc "💊 steroids for Xcode Playgrounds"
   homepage "https://nef.bow-swift.io"
-  url "https://github.com/bow-swift/nef/archive/0.6.2.tar.gz"
-  sha256 "23915dd21e6485829b5ad88b6a5f4ac6b4ea091fc70820d2322bafba09e2217a"
+  url "https://github.com/bow-swift/nef/archive/0.7.0.tar.gz"
+  sha256 "41c49552df2e98649a93fa0b011d9b380ca1c5255aa8469a085e096118d62be2"
   license "Apache-2.0"
 
   bottle do
@@ -16,6 +16,8 @@ class Nef < Formula
   depends_on xcode: "11.4"
 
   def install
+    inreplace "Makefile", "$(MAKE) bash", ""
+    inreplace "Makefile", "$(MAKE) zsh", ""
     system "make", "install", "prefix=#{prefix}", "version=#{version}"
   end
 
