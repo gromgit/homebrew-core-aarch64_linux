a8f9ac65587fc91c81001b30769beb33e1aedb73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-13 19:05:59 +0000

    cargo-c: update 0.9.6 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -11,12 +11,12 @@ class CargoC < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "154dcef48d8d950595ac0b7a3d8edbf0183996cccb225dc13fead3f9446bc024"
-    sha256 cellar: :any,                 arm64_big_sur:  "76521ebc563528b80a11904b4000e065d47fe1ea52d762dbdd2efb943188d729"
-    sha256 cellar: :any,                 monterey:       "87de59cfb988e6ccb543baf6334db2e30d575d118b5907a4382eef1f46a9c0af"
-    sha256 cellar: :any,                 big_sur:        "261152bdb386fefa8ae96446566511cc4e641ad68d0bf35176d310bd6d6c476a"
-    sha256 cellar: :any,                 catalina:       "2742d00e8f887a6cfb1c0d006159ed29f6b4e6acea56086b969f9023d5cfcff3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dac3184fdb3315d6eed841246dec368796f708aa3f1e3b91126de90d2b803579"
+    sha256 cellar: :any,                 arm64_monterey: "5ae1cd6ceb4a4543e0ef39d009672fc05780386ccf1d03beff214ae44e06fdc4"
+    sha256 cellar: :any,                 arm64_big_sur:  "be28ae9ff8df87545ab95d9c53be0d7746af621d0c7e845a4640f50fbc3026be"
+    sha256 cellar: :any,                 monterey:       "9f0f50fc3e1f21c936ceb7c73b04345871d99fa899bbc346562686f0d11925d9"
+    sha256 cellar: :any,                 big_sur:        "e6dc3290e2b3fe8fd5cd4a1c29456a95edbb67d4001ad6d4707944b6fb7ca8c8"
+    sha256 cellar: :any,                 catalina:       "8cdd97b8ae42958bacb734174b5e89e7c9aaba8411030e6fafbfad01cc64b9e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d370f1ae18a77934ce43c6df29a26ee7d8086a412b2681c3bb0cdec22c36383a"
   end
 
   depends_on "rust" => :build
