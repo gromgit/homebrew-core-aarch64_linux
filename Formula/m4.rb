7b14167765ca686ce6dc02ba46bae7aeb41a6160
Adrian Ho <aho+rpi9@03s.net>
2021-12-14 23:31:16 +0800

    m4: update 1.4.19 bottle

---
+++ Formula/m4.rb
@@ -0,0 +1,26 @@
+class M4 < Formula
+  desc "Macro processing language"
+  homepage "https://www.gnu.org/software/m4"
+  url "https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.xz"
+  mirror "https://ftpmirror.gnu.org/m4/m4-1.4.19.tar.xz"
+  sha256 "63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/m4"
+    sha256 x86_64_aarch64_linux: "9e7fff496df5dec74cc4b150c6dc6d0ddf41e05bb0e2188df7b4abe41a77f94e"
+  end
+
+  keg_only :provided_by_macos
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "Homebrew",
+      pipe_output("#{bin}/m4", "define(TEST, Homebrew)\nTEST\n")
+  end
+end
