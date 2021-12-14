3b740aecf61f14d323ef2a9169b414c9f42eb32f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-14 19:05:22 +0000

    rtl_433: update 21.12 bottle.

--- Formula/rtl_433.rb
+++ Formula/rtl_433.rb
@@ -8,12 +8,12 @@ class Rtl433 < Formula
   head "https://github.com/merbanan/rtl_433.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "d6302d96e4450238a86db6d9cfe01b20841995aa622b881d5d085426716cbc5a"
-    sha256 cellar: :any, arm64_big_sur:  "e6583ff3156f6862160b728a95b1fbb279c6f5a7dd2636200354596057050d15"
-    sha256 cellar: :any, monterey:       "8f0177b866f24179f2cd19b2dd65add10a340ee70db253ea6443ea6ef4c3ca91"
-    sha256 cellar: :any, big_sur:        "d9d81361c72f07d50c0f28552e15190fbcfc91fe862403511be883a72df3c744"
-    sha256 cellar: :any, catalina:       "da1f6e7c5930ed4b98e34fb44d62dd1cefd22225366e35612688a74086afc38e"
-    sha256 cellar: :any, mojave:         "421233496a91d2efb4310ac47557d7d4e75d91116dc829c20c96f36283f60d34"
+    sha256 cellar: :any,                 arm64_monterey: "50d863bfb93b8186913082ae451ad42cd7cdc3b6d56e3a52ff3da031a56459fd"
+    sha256 cellar: :any,                 arm64_big_sur:  "ed915afbe569d6802bd1c2ea70e04c25d3a05a97ff712af46f66e55c8158dbf5"
+    sha256 cellar: :any,                 monterey:       "41da1507a5b0c5f7c91ea129c459cc77fa772c7f1f4f763c11ee00b1939ba0cd"
+    sha256 cellar: :any,                 big_sur:        "0e4334269c387914888c2ae366ff784903dfadd9dd02b6c8507ac1b4202ff24c"
+    sha256 cellar: :any,                 catalina:       "bb968856b95daef8e3880cc8b9ddad3384bff348b4ecc13db96998e0eb4a7a90"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f42cf7cdb9e8bd31e463a6ee3ad67dccf24d89bc1910fb226f39ba86d90b8cb"
   end
 
   depends_on "cmake" => :build
