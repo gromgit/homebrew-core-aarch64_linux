d6b4bb75926f0585dbae9408848c6458c0d34b1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-14 03:05:27 +0000

    gosec: update 2.9.5 bottle.

--- Formula/gosec.rb
+++ Formula/gosec.rb
@@ -7,12 +7,12 @@ class Gosec < Formula
   head "https://github.com/securego/gosec.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f997bc8e076b88bc527d47e23b9376048f59b671b6af5eecac1201f2f7037221"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa5bd5c964d0cd5a8cad1335e85da29dbd1a6e5d434aadc1a025a64185e7685e"
-    sha256 cellar: :any_skip_relocation, monterey:       "0e3ddec7a468a68e8ba4a75dfa9e43833dd35bbfa9f51b4f1ed1d0ed5987e641"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4ca1524597f069851eec405619ff9bfe04b8d4c5ee053af1dbb049d562c864c1"
-    sha256 cellar: :any_skip_relocation, catalina:       "89c69dc24c15c1764fbd0336b9aff91eeb3efa6da36a617ee0df055ef81989fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5112877879247455842de72de02ac63214dc8b472387b4f85d3c3a460d2d081"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1abb42c11452494d96d362d730a6940ad8e5928addbf539d4f37f19b30e8c88b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af14edb4cadc0ffabb08c954fd6e2deeab5c8fdb960afbc2957030342a5d125d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e50eb802df27f163de4ea235c3813baf84df3af4621f441c7f2c3450e3bebc0e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e37d5b497a3600de18c73ebec64caff0e5fcdd4a110a65aa3591d2226cb3b84c"
+    sha256 cellar: :any_skip_relocation, catalina:       "54a41e0b7cf04640a2d6d7e6c4d0c4b6b5b500f7fff61bc3d073e9855bd7e351"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "059a54dea77916ae4d8fd5feb847380e7013747d1240d0883c85bcdac6ba0a56"
   end
 
   depends_on "go"
