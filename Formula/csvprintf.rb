eb5c810219f067852fbaaaefb932346e58b1161b
Archie L. Cobbs <archie.cobbs@gmail.com>
2021-12-14 15:22:47 -0600

    csvprintf 1.3.1
    Closes #91334.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/csvprintf.rb
+++ Formula/csvprintf.rb
@@ -1,8 +1,8 @@
 class Csvprintf < Formula
   desc "Command-line utility for parsing CSV files"
   homepage "https://github.com/archiecobbs/csvprintf"
-  url "https://github.com/archiecobbs/csvprintf/archive/1.3.0.tar.gz"
-  sha256 "f15737526f0505f0a26dbdd7799f7f3acc950001c64b18a5b233b8b0fd301b0c"
+  url "https://github.com/archiecobbs/csvprintf/archive/1.3.1.tar.gz"
+  sha256 "8cd5c7953a94f30eefe5edcee4a76e10e57b60ae9318a1b704c6823b40d09d2f"
   license "Apache-2.0"
 
   bottle do
@@ -18,12 +18,6 @@ class Csvprintf < Formula
   depends_on "automake" => :build
 
   uses_from_macos "libxslt"
-
-  # Fix for missing 'u_char', remove in next version
-  patch do
-    url "https://github.com/archiecobbs/csvprintf/commit/c8798ed8.patch?full_index=1"
-    sha256 "94142117ec45922d8f6aa001ef17421e76600f761689e096015448fd3424f301"
-  end
 
   def install
     ENV.append "LDFLAGS", "-liconv" if OS.mac?
