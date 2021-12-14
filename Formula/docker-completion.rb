f8167dc00fd77c5744f77abb1969ce8ae31019e0
Rui Chen <rui@chenrui.dev>
2021-12-13 23:36:09 -0500

    docker-completion 20.10.12

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.11",
-      revision: "dea9396e184290f638ea873c76db7c80efd5a1d2"
+      tag:      "v20.10.12",
+      revision: "e91ed5707e038b02af3b5120fa0835c5bedfd42e"
   license "Apache-2.0"
 
   livecheck do
