1039088316eb656749ab25b576ebc66d07810115
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-14 05:07:03 +0000

    folly: update 2021.12.13.00 bottle.

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -7,12 +7,12 @@ class Folly < Formula
   head "https://github.com/facebook/folly.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a78745eb4a57c63f25f76f89ae5bdea2fed0ddacebbc0b65ed34154fdb6d592f"
-    sha256 cellar: :any,                 arm64_big_sur:  "6ceafb076283b84796ae6fdffab1042eb297d664753a2b90618bb7b35bc5cba7"
-    sha256 cellar: :any,                 monterey:       "186dc7b75183a31f94b38679aa0e40277f3493ca07939afae74dee9c348cbe89"
-    sha256 cellar: :any,                 big_sur:        "09ead84368e62120026ee786f4d2f338e1aa820183ea34e8290b54ebf8d1597c"
-    sha256 cellar: :any,                 catalina:       "073e6780f9a3c31ee36182a089cadcbfed0b4585f96dc187931019ab6eb5318e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f54b38bafe562df4e6f6d68e75793045f42b16bb2fe124798a9cd79244ca4624"
+    sha256 cellar: :any,                 arm64_monterey: "b331f052c7d54eeee100075a98bc9827c40c9ea99a816ce38675185414720618"
+    sha256 cellar: :any,                 arm64_big_sur:  "1ad779f2e74ac717d3bacc0f5d5e1df1d34adc5391eeffb1bb4d1b4d558859e7"
+    sha256 cellar: :any,                 monterey:       "1a4d4f40e51c54fdb7f70b6b48d5d7547e289e8c1e6736ec11cd23d60569803d"
+    sha256 cellar: :any,                 big_sur:        "4aa51ed8bff4ff3b240fcf17a67c02806cc0c86f0c593c0257d54a7ff18280ca"
+    sha256 cellar: :any,                 catalina:       "5660ad232a8ebec66fe1f711ee1b4bfbffd67349c7091f762682da287d9b8af5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1a2765371a7d15b39619024c7a3af704776bba0cde39512c8b345023d4989e8"
   end
 
   depends_on "cmake" => :build
