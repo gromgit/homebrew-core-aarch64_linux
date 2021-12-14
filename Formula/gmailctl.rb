fa9c69c117a467b22139537d858861a40ab6f764
Rui Chen <rui@chenrui.dev>
2021-12-14 09:39:24 -0500

    gmailctl 0.10.0
    Closes #91320.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gmailctl.rb
+++ Formula/gmailctl.rb
@@ -1,8 +1,8 @@
 class Gmailctl < Formula
   desc "Declarative configuration for Gmail filters"
   homepage "https://github.com/mbrt/gmailctl"
-  url "https://github.com/mbrt/gmailctl/archive/v0.9.0.tar.gz"
-  sha256 "1b4d04c0fa46990231565cb743d3b9aba2011501322c224a96bec747003c35e1"
+  url "https://github.com/mbrt/gmailctl/archive/v0.10.0.tar.gz"
+  sha256 "ebc3f13b71363e49b804daadc3e1fff394cf42769429e4b081de86100fb5f685"
   license "MIT"
   head "https://github.com/mbrt/gmailctl.git", branch: "master"
 
@@ -20,14 +20,12 @@ class Gmailctl < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "cmd/gmailctl/main.go"
-    pkgshare.install ["default-config.jsonnet", "gmailctl.libsonnet"]
   end
 
   test do
-    cp pkgshare/"default-config.jsonnet", testpath
-    cp pkgshare/"gmailctl.libsonnet", testpath
-
-    assert_includes shell_output("#{bin}/gmailctl init --config #{testpath} 2>&1"),
+    assert_includes shell_output("#{bin}/gmailctl init --config #{testpath} 2>&1", 1),
       "The credentials are not initialized"
+
+    assert_match version.to_s, shell_output("#{bin}/gmailctl version")
   end
 end
