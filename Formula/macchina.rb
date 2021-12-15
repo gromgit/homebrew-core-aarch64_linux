4d372a2c71add24dc10a37de993c6a305020843a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-15 03:05:32 +0000

    macchina: update 6.0.1 bottle.

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -6,12 +6,12 @@ class Macchina < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "287ed38e068c0e828f62585c991de91755e22aaced7049111a10624d92ef4633"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa832a1ca6709f1b8cb727eacca232b355962133a72693c4f83284a4e5ca2b7b"
-    sha256 cellar: :any_skip_relocation, monterey:       "311bc5e1f22a509234258049fe3e8456362c1a76d7ac50103ff3ccd59c19833e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3f353c38f5f70a1a2120911ba8d3238bf6214a86c3017a450f5cb53ea70ff490"
-    sha256 cellar: :any_skip_relocation, catalina:       "0ca50b7cb21df0b753ad88f4406fe1d82f2c587226ca52c1cbbb3c36a4e7a4f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c098962b2620cbc5365a43c9ea8defdf678f35338716fa65fef750c055639360"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "02e348e0c8afcd6ba5a1b41853915928fc20541e9e5220cb0cd4d4739e510476"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0da77269184752c41ede9b165eaf4a43f1f5a53d2f5e6c6d5cce29e201005361"
+    sha256 cellar: :any_skip_relocation, monterey:       "558a5a305c64ec2557ae2585f0b8c22a524b1daded14df74e87bab7375d269e5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9148903a5b7e477efbc8f6efe26da0977e8336dad59f4343c2ea86cc0063262a"
+    sha256 cellar: :any_skip_relocation, catalina:       "8afc5042718e5d6c33aad6d751411e9e16998eff4de5b0404e43823226c8ce13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d875e84502adad94e5e8ff23a5070be06385c9fc3cef96b3f713c0f0fd75bbb"
   end
 
   depends_on "rust" => :build
