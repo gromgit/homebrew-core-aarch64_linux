d5a462b2c939f57a07e5deb94323e97dd35a1006
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-15 07:05:55 +0000

    docker-gen: update 0.8.1 bottle.

--- Formula/docker-gen.rb
+++ Formula/docker-gen.rb
@@ -6,12 +6,12 @@ class DockerGen < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4dc799e86dfa297d94d05de294564a04acb28a95bac9871ace56a69f1d3a19a9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eacba5ac36276e0aa5de4f71b3d26a11f5e00f89f10bfb81ecb29415bdfa1cb3"
-    sha256 cellar: :any_skip_relocation, monterey:       "208331344c9feaa4404957d4f4f384b2ca0788320a38e626db541d522fc883c2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9a490c2b1f72c1855d117c7dd88fb0214e4dd69ad1409e940eb7bbb116cedd89"
-    sha256 cellar: :any_skip_relocation, catalina:       "34c5a01ca13bcd79fec6287375b95832455b886d20eb8f799513a0c189523509"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39cc3af1ef20613483280f3785c29ff8694eeb04016c9381c1dcb462ba5ce5f9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cdf608647487efaba341e756cedd5219d754dbdf2c26f8e73e8067c9d633e56e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "883cb75ad27d253a94f3fdb0501411903a4bf80111fe989bac3666209ea36e7e"
+    sha256 cellar: :any_skip_relocation, monterey:       "fcefd5b9c40e8cfcd5e903f304478c013bb9acae780988fbbe4d9c6db90236c3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5f58ad3f97190d1183333122b3cc3e94147ac6dc3aa324aed85646f62b1073f9"
+    sha256 cellar: :any_skip_relocation, catalina:       "5eb1e2b174cdf4ba83615c52103672ef0ce4ed96e893988c8ee33c07b6338e64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b1da7e49caa2b514eb09d8376ceef231d94abda34980bb7cca7f7032793fc1a"
   end
 
   depends_on "go" => :build
