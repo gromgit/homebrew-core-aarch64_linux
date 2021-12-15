4446e13c5efafcceb871762f1a83c2a2fad8523d
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-12-15 09:14:51 +0000

    imap-backup: add 4.0.2 bottle.

--- Formula/imap-backup.rb
+++ Formula/imap-backup.rb
@@ -6,6 +6,15 @@ class ImapBackup < Formula
 
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1e8b91a3c8688a2fe313160c4d9c892f6b92e37753bf1e0a0607c2f1e64af24"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3eebc9979f79e2c8f6eec6067b60896ec1ff76bb83a488f252688420d9650c7a"
+    sha256 cellar: :any_skip_relocation, monterey:       "e1e8b91a3c8688a2fe313160c4d9c892f6b92e37753bf1e0a0607c2f1e64af24"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3eebc9979f79e2c8f6eec6067b60896ec1ff76bb83a488f252688420d9650c7a"
+    sha256 cellar: :any_skip_relocation, catalina:       "3eebc9979f79e2c8f6eec6067b60896ec1ff76bb83a488f252688420d9650c7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "995d0850aee450f3f7cd208c6d048aacb264ef7f28fafd19d90a6f48a91f346a"
+  end
+
   uses_from_macos "ruby"
 
   def install
