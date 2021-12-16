c662296a962cd9fcf3d7bd9280cefbdeaf63f7d5
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-12-16 00:22:18 +0000

    kubernetes-cli@1.22: add 1.22.4 bottle.

--- Formula/kubernetes-cli@1.22.rb
+++ Formula/kubernetes-cli@1.22.rb
@@ -11,6 +11,15 @@ class KubernetesCliAT122 < Formula
     regex(/^v?(1\.22(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dedb8727efaa3d87daf55d0d7ca8346e6c2c6093d30361d65ebc1ab28795e616"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "de31d093ae8f343523c16f45c176945795a2c20072078df290d5f503b7ec27fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "53acdd1ccc07ff486cb43a1b9408b9a9a7e3c897e1608e0bf736d18d2563d9ff"
+    sha256 cellar: :any_skip_relocation, big_sur:        "797ecd05edb1cb70b9057cbf45000eb0effc5a6e342d123f6be19711ab2dcf5e"
+    sha256 cellar: :any_skip_relocation, catalina:       "a4d4c9e9c116068f22cdc630033b596eded6e952c0fd8b3512cec7e6322ab903"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2ea4de0e36753e7a580fe3afbeba3c1158d2503f1d5501b8288429fcadfed15"
+  end
+
   keg_only :versioned_formula
 
   # https://kubernetes.io/releases/patch-releases/#1-22
