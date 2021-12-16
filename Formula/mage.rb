15aa60f568c4f785b05972d3d7754b12818e1547
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-16 16:09:34 +0000

    mage: update 1.12.0 bottle.

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -8,14 +8,12 @@ class Mage < Formula
   head "https://github.com/magefile/mage.git"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "46668f1206dea4aa54148e2ddc3d032882fe8d8b6d870e307466a897dc1bbaef"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "acf15da6b6d2df49eac61aea939b1f2c59917b5ee99ad4f400dc2d9e08e006d2"
-    sha256 cellar: :any_skip_relocation, monterey:       "bbd18be80b12fd2649eb4d46826d0dbd191ac269d3e9f7ca1d30b35b08d06843"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a3707826deeb07ceb26ba6c14a532fad9cdbb865931d248675aa468c16a4c2a9"
-    sha256 cellar: :any_skip_relocation, catalina:       "e5abfae7ded7be5c6cb847a9237ff850620cf01a5d5ec086f8777ece37f12bc9"
-    sha256 cellar: :any_skip_relocation, mojave:         "b116c4a96c95e42a0359976929f20ebe7ebfb8dfcb4f69b911948431da1f89ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4269eaaf9ff193b0f56e327acd9f38d2186006e918ae2ff2c267c106cd1d485e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a76f89e81261e2f223fa94dfe025d989809304a3236cfac36d6816c3f2932e56"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a76f89e81261e2f223fa94dfe025d989809304a3236cfac36d6816c3f2932e56"
+    sha256 cellar: :any_skip_relocation, monterey:       "3fd75c25b71b64fc8a379a013bec915947b2584a1880060c8396b3d6eb5f5a40"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3fd75c25b71b64fc8a379a013bec915947b2584a1880060c8396b3d6eb5f5a40"
+    sha256 cellar: :any_skip_relocation, catalina:       "3fd75c25b71b64fc8a379a013bec915947b2584a1880060c8396b3d6eb5f5a40"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a271902ea09ef3318c350bea322817e8903a97b87069b124ea82069f49c8f81"
   end
 
   depends_on "go"
