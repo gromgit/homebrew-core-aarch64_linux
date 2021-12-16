28c458bdca7242c443b71bca0d1c6a662d376bc9
Rui Chen <rui@chenrui.dev>
2021-12-16 11:15:42 -0500

    libtorch 1.10.1
    Closes #91464.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libtorch.rb
+++ Formula/libtorch.rb
@@ -4,8 +4,8 @@ class Libtorch < Formula
   desc "Tensors and dynamic neural networks"
   homepage "https://pytorch.org/"
   url "https://github.com/pytorch/pytorch.git",
-      tag:      "v1.10.0",
-      revision: "36449ea93134574c2a22b87baad3de0bf8d64d42"
+      tag:      "v1.10.1",
+      revision: "302ee7bfb604ebef384602c56e3853efed262030"
   license "BSD-3-Clause"
 
   livecheck do
