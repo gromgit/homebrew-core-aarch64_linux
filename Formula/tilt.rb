d76f5aaa6a6b5196613f5f908c0126bcedb66caf
Nick Santos <nick@tilt.dev>
2021-12-17 17:31:25 -0500

    tilt 0.23.4
    Closes #91532.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -2,8 +2,8 @@ class Tilt < Formula
   desc "Define your dev environment as code. For microservice apps on Kubernetes"
   homepage "https://tilt.dev/"
   url "https://github.com/tilt-dev/tilt.git",
-    tag:      "v0.23.3",
-    revision: "a71350c746352224017049deb9791c5d12ac0707"
+    tag:      "v0.23.4",
+    revision: "8ff29c834ae3d61ea354109a36e3d483d9347435"
   license "Apache-2.0"
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
