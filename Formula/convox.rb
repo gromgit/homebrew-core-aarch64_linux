af8dd2af50ab89264f74a7b2fc317a78be22bc72
Rui Chen <rui@chenrui.dev>
2021-12-17 11:48:34 -0500

    convox 3.2.2
    Closes #91517.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "Command-line interface for the Convox PaaS"
   homepage "https://convox.com/"
-  url "https://github.com/convox/convox/archive/3.2.1.tar.gz"
-  sha256 "d223f527df1fd41dc1856a058d43bdde042cf562f760fe87d30b53e56d0003f4"
+  url "https://github.com/convox/convox/archive/3.2.2.tar.gz"
+  sha256 "6582301047db51bd0ec283a85ae6d3f0ddc7970620077444174a1871a12a87c6"
   license "Apache-2.0"
   version_scheme 1
 
