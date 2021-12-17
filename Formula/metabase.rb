6330848fa85d8f45d0f6574d83400b8a61b64f9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-17 05:04:22 +0000

    metabase 0.41.5
    Closes #91493.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/metabase.rb
+++ Formula/metabase.rb
@@ -1,8 +1,8 @@
 class Metabase < Formula
   desc "Business intelligence report server"
   homepage "https://www.metabase.com/"
-  url "https://downloads.metabase.com/v0.41.4/metabase.jar"
-  sha256 "8a14b5db169f2f66d8fcc0d9de597822e83a1f250c3cff57d4dddf384f2314f7"
+  url "https://downloads.metabase.com/v0.41.5/metabase.jar"
+  sha256 "0c7d71cb571354334d5f238869ac861f33a2e20d19ba434515b663b9f63e5cb9"
   license "AGPL-3.0-only"
 
   livecheck do
