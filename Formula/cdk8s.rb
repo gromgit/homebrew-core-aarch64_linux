08dac07f4b78a4ddbd866cf544c98c3dfb260bab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-18 18:05:51 +0000

    cdk8s: update 1.0.53 bottle.

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -8,7 +8,12 @@ class Cdk8s < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f9fb6fda23dce4f314c979861706d9809057e035a3ba9685f57f55191f82ed96"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b2d56d6a02591abccbe154bb23b55bd3e94bed45d6fdce28c2860e995e242f6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b2d56d6a02591abccbe154bb23b55bd3e94bed45d6fdce28c2860e995e242f6"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b2d56d6a02591abccbe154bb23b55bd3e94bed45d6fdce28c2860e995e242f6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a61d83250b6f38f12dca8f053c0b7cd165694c321a6e536a0990965f65468024"
+    sha256 cellar: :any_skip_relocation, catalina:       "a61d83250b6f38f12dca8f053c0b7cd165694c321a6e536a0990965f65468024"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b2d56d6a02591abccbe154bb23b55bd3e94bed45d6fdce28c2860e995e242f6"
   end
 
   depends_on "node"
