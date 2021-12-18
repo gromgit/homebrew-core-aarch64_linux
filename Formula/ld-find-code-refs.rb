10797a719bbfd2391a3bd10410de9a6b34a4352e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-18 19:05:13 +0000

    ld-find-code-refs: update 2.4.1 bottle.

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -7,12 +7,12 @@ class LdFindCodeRefs < Formula
   head "https://github.com/launchdarkly/ld-find-code-refs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1516d3b41d605a7bdce618ec740e97d69fee4844e450dbbd2383a59ff201df3f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4c04676862f064979241e4a2c9c1518cb1005a778c645d0ee135c83f610fbf64"
-    sha256 cellar: :any_skip_relocation, monterey:       "ff25a0905d1435e414666d2cef65c2497df258a2e8f4f3dcc25f94358e3bdd3f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3522812ba3bae93f451bcde49872fa93e7bb6da94d244f357b5fad50d1148341"
-    sha256 cellar: :any_skip_relocation, catalina:       "7176c9d6007733071fb7251800ffe7ee335f9808c319a0e1ca053f8ea82b98b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c5449afa5dc6435440c0e4e834d870682aba4b4204808b682707dd474e2dbd1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b23bec45976ffaf80b1351a02e8059e207d8e63f37a5deaa46b45f60fee537f5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a80469e7b9ea6604b1a148d9d1b6e006da67901441676d2916b8e7e682f47170"
+    sha256 cellar: :any_skip_relocation, monterey:       "2dd7c86e3634cad302c609cccdbff4d3cfc17e7992d1a9cde7638bf38a9ca7e4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2451a559af68f58111480e3bb282109e7b6c6e52b66a2dab443f8d8334903665"
+    sha256 cellar: :any_skip_relocation, catalina:       "72ad39ef67542a4f7afc46d08416a0748b05a7dd1f72822daca74ebdd5105d26"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "17c84bc9fc9f0d9c064b7b88425379b94ce6f8ce4033af4faf0b77c8cb9f43fa"
   end
 
   depends_on "go" => :build
