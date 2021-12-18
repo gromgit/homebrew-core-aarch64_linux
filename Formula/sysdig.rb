e795b691f15b9388d5a309a4273e96cd2a2e284b
Rui Chen <rui@chenrui.dev>
2021-12-17 23:52:14 -0500

    sysdig: revision bump (grpc 1.43.0)
    Closes #91571.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sysdig.rb
+++ Formula/sysdig.rb
@@ -4,7 +4,7 @@ class Sysdig < Formula
   url "https://github.com/draios/sysdig/archive/0.27.1.tar.gz"
   sha256 "b9d05854493d245a7a7e75f77fc654508f720aab5e5e8a3a932bd8eb54e49bda"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   livecheck do
     url :stable
