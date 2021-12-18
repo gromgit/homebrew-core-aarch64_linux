ee0dddc39292ed291cedadd4ed2c8d95714eb465
Rui Chen <rui@chenrui.dev>
2021-12-18 17:32:14 -0500

    macos-trash 1.2.0
    * macos-trash 1.2.0
    * Try to fix Catalina build
    
    Closes #91624.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/macos-trash.rb
+++ Formula/macos-trash.rb
@@ -1,8 +1,8 @@
 class MacosTrash < Formula
   desc "Move files and folders to the trash"
   homepage "https://github.com/sindresorhus/macos-trash"
-  url "https://github.com/sindresorhus/macos-trash/archive/v1.1.1.tar.gz"
-  sha256 "e215bda0c485c89a893b5ad8f4087b99b78b3616f26f0c8da3f0bb09022136dc"
+  url "https://github.com/sindresorhus/macos-trash/archive/v1.2.0.tar.gz"
+  sha256 "c4472b5c8024806720779bc867da1958fe871fbd93d200af8a2cc4ad1941be28"
   license "MIT"
   head "https://github.com/sindresorhus/macos-trash.git"
 
@@ -16,6 +16,7 @@ class MacosTrash < Formula
 
   depends_on xcode: ["12.0", :build]
   depends_on :macos
+  uses_from_macos "swift", since: :big_sur # Swift 5.5.0
 
   conflicts_with "trash", because: "both install a `trash` binary"
   conflicts_with "trash-cli", because: "both install a `trash` binary"
