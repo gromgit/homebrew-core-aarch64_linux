994806a8ddae58bb2493af10b11a9fa8a23ad8d9
Rui Chen <rui@chenrui.dev>
2021-12-18 00:47:35 -0500

    vala 0.54.5
    Closes #91612.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -1,8 +1,8 @@
 class Vala < Formula
   desc "Compiler for the GObject type system"
   homepage "https://wiki.gnome.org/Projects/Vala"
-  url "https://download.gnome.org/sources/vala/0.54/vala-0.54.4.tar.xz"
-  sha256 "6051270a2fc30de023c88562566f2f6043e67beb4da4b799c14cdf12048eb40c"
+  url "https://download.gnome.org/sources/vala/0.54/vala-0.54.5.tar.xz"
+  sha256 "0028da1685dedca993792bfb5f460db5ba548c9aa44323b1899f733a89121587"
   license "LGPL-2.1-or-later"
 
   bottle do
