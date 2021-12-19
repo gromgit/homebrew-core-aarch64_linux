5b9d6d0759f01176204b4ee463860b0dce695889
Rui Chen <rui@chenrui.dev>
2021-12-19 16:08:13 -0500

    exenv: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/exenv.rb
+++ Formula/exenv.rb
@@ -4,7 +4,7 @@ class Exenv < Formula
   url "https://github.com/mururu/exenv/archive/v0.1.0.tar.gz"
   sha256 "368095760ecc386a0930954f5f0ce7cea977641fe6d27b1beff032f512598a58"
   license "MIT"
-  head "https://github.com/mururu/exenv.git"
+  head "https://github.com/mururu/exenv.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "ae3d33c35709202895d8d27bff0ea95075cd1455cc20207b139c43b73ca34322"
