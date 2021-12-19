fb54517bc1ff7a8430541618ec34c2767225a92e
Rui Chen <rui@chenrui.dev>
2021-12-19 13:22:05 -0500

    maven-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/maven-completion.rb
+++ Formula/maven-completion.rb
@@ -4,7 +4,7 @@ class MavenCompletion < Formula
   url "https://github.com/juven/maven-bash-completion/archive/20200420.tar.gz"
   sha256 "eb4ef412d140e19e7d3ce23adb7f8fcce566f44388cfdc8c1e766a3c4b183d3d"
   license "Apache-2.0"
-  head "https://github.com/juven/maven-bash-completion.git"
+  head "https://github.com/juven/maven-bash-completion.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a5e2dedc38c74f165c2f1168175215381d8836a6aaa0f952e33bab3b5383c8a3"
