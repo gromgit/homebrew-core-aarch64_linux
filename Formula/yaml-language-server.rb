d5c836b11ab317fa947a5864af11fbb086d6b433
Shun Sambongi <shunsambongi@gmail.com>
2021-12-19 01:50:53 -0700

    yaml-language-server 1.2.2 (new formula)
    Closes #91639.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/yaml-language-server.rb
@@ -0,0 +1,38 @@
+require "language/node"
+
+class YamlLanguageServer < Formula
+  desc "Language Server for Yaml Files"
+  homepage "https://github.com/redhat-developer/yaml-language-server"
+  url "https://registry.npmjs.org/yaml-language-server/-/yaml-language-server-1.2.2.tgz"
+  sha256 "c5413137ec0ebf79a4368fd97611f2ae6d08a0c4ee2d6efb28763982a7e05e2a"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/yaml-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
