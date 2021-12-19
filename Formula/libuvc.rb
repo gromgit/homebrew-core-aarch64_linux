9da126ec126119d51a190766232a302852ef6455
Rui Chen <rui@chenrui.dev>
2021-12-19 13:57:09 -0500

    libuvc: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/libuvc.rb
+++ Formula/libuvc.rb
@@ -4,7 +4,7 @@ class Libuvc < Formula
   url "https://github.com/ktossell/libuvc/archive/v0.0.6.tar.gz"
   sha256 "42175a53c1c704365fdc782b44233925e40c9344fbb7f942181c1090f06e2873"
   license "BSD-3-Clause"
-  head "https://github.com/ktossell/libuvc.git"
+  head "https://github.com/ktossell/libuvc.git", branch: "master"
 
   bottle do
     rebuild 1
