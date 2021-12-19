8d949855c23fd985fd1ad2152c52c16da0623201
Rui Chen <rui@chenrui.dev>
2021-12-19 14:07:44 -0500

    kerl: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -4,7 +4,7 @@ class Kerl < Formula
   url "https://github.com/kerl/kerl/archive/2.2.3.tar.gz"
   sha256 "d3c7d29044f43ddf434d69ab9a0ead866ee01b24ae694df03e398bad777cd6c1"
   license "MIT"
-  head "https://github.com/kerl/kerl.git"
+  head "https://github.com/kerl/kerl.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "b5f2eee1d208669cabab68f7964735da4e260a836fbcbfaa9eaaa462847c925e"
