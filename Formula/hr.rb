26558c7345d907554e864188edc23adab420155c
Rui Chen <rui@chenrui.dev>
2021-12-19 14:53:25 -0500

    hr: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/hr.rb
+++ Formula/hr.rb
@@ -4,7 +4,7 @@ class Hr < Formula
   url "https://github.com/LuRsT/hr/archive/1.3.tar.gz"
   sha256 "258ff3121369d17c5c70fa18e466ac01cdb4cf890c605f7a5e706d5b1a3afebf"
   license "MIT"
-  head "https://github.com/LuRsT/hr.git"
+  head "https://github.com/LuRsT/hr.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "01e0e362b30cb5eb92751375426626c3eb8d6bfd05aa61d3cbba188a12bd1b92"
