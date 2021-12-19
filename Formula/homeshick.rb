74ae7d479e0c42299c5a08ecd55df967408cdb08
Rui Chen <rui@chenrui.dev>
2021-12-19 14:53:24 -0500

    homeshick: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/homeshick.rb
+++ Formula/homeshick.rb
@@ -4,7 +4,7 @@ class Homeshick < Formula
   url "https://github.com/andsens/homeshick/archive/v2.0.0.tar.gz"
   sha256 "14a538bfc2e7cb6bfd35c984cdedbf3d3293413a70cc67f685dbfbd33ce64fdd"
   license "MIT"
-  head "https://github.com/andsens/homeshick.git"
+  head "https://github.com/andsens/homeshick.git", branch: "master"
 
   bottle do
     rebuild 1
