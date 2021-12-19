bed3448339ef9335d3a9feba570ab55a2417dd36
Rui Chen <rui@chenrui.dev>
2021-12-19 14:53:25 -0500

    http-server: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/http-server.rb
+++ Formula/http-server.rb
@@ -6,7 +6,7 @@ class HttpServer < Formula
   url "https://registry.npmjs.org/http-server/-/http-server-14.0.0.tgz"
   sha256 "fbe42ce2338184bc08c414f98b512d272039803f93c3759ecb0678deaa728c7e"
   license "MIT"
-  head "https://github.com/http-party/http-server.git"
+  head "https://github.com/http-party/http-server.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "ab9a881b34551f9dc61292967cdde180ffe801e374b7ccf079e59f09c47438cc"
