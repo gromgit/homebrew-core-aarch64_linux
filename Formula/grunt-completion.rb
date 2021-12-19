13641962c66546217353e26ace948f8b8281953b
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:54 -0500

    grunt-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/grunt-completion.rb
+++ Formula/grunt-completion.rb
@@ -4,7 +4,7 @@ class GruntCompletion < Formula
   url "https://github.com/gruntjs/grunt-cli/archive/v1.4.3.tar.gz"
   sha256 "3bf07d807d61adbf04fa93589a7dd58fbc4da7a5f1febfd8a99b3ccb0d682009"
   license "MIT"
-  head "https://github.com/gruntjs/grunt-cli.git"
+  head "https://github.com/gruntjs/grunt-cli.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "3d2fd1dd2be3cbc084c78eb81679d279e05b83db13604eebc7670467a04b1da1"
