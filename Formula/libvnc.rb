8712f1fc40f521f68f134757487f73710975fe53
Rui Chen <rui@chenrui.dev>
2021-12-19 13:57:09 -0500

    libvnc: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/libvnc.rb
+++ Formula/libvnc.rb
@@ -4,7 +4,7 @@ class Libvnc < Formula
   url "https://github.com/LibVNC/libvncserver/archive/LibVNCServer-0.9.13.tar.gz"
   sha256 "0ae5bb9175dc0a602fe85c1cf591ac47ee5247b87f2bf164c16b05f87cbfa81a"
   license "GPL-2.0-only"
-  head "https://github.com/LibVNC/libvncserver.git"
+  head "https://github.com/LibVNC/libvncserver.git", branch: "master"
 
   livecheck do
     formula "libvncserver"
