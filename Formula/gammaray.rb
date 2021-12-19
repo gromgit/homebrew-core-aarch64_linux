7c1fb34273496f3582c805f69b3f5326d5e25cba
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:32 -0500

    gammaray: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gammaray.rb
+++ Formula/gammaray.rb
@@ -4,7 +4,7 @@ class Gammaray < Formula
   url "https://github.com/KDAB/GammaRay/releases/download/v2.11.3/gammaray-2.11.3.tar.gz"
   sha256 "03d7ca7bd5eb600c9c389d0cf071960330592f1f392a783b7fec5f9eaa5df586"
   license "GPL-2.0-or-later"
-  head "https://github.com/KDAB/GammaRay.git"
+  head "https://github.com/KDAB/GammaRay.git", branch: "master"
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "bdf3eb554fd62c1f4fffb31049006fb6911b77f798adbc4e87061a5066e9b77d"
