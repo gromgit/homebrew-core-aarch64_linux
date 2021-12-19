c88c449400805c01ae62f343e242bfe74404971f
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:43 -0500

    github-keygen: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/github-keygen.rb
+++ Formula/github-keygen.rb
@@ -4,7 +4,7 @@ class GithubKeygen < Formula
   url "https://github.com/dolmen/github-keygen/archive/v1.305.tar.gz"
   sha256 "5a0a68ed9e3eb1c0e3b783c250e0790fffe17fc1a663cefa1348560ff040b940"
   license "GPL-3.0"
-  head "https://github.com/dolmen/github-keygen.git"
+  head "https://github.com/dolmen/github-keygen.git", branch: "release"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "40efaefeb31f64b474d8030ddc268250d52d6f3e17131f36d0eb8e35fa24ab8e"
