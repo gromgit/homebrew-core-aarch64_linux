ed558968a9d82057a87e067c9665c56f7c7cec50
Rui Chen <rui@chenrui.dev>
2021-12-19 14:35:02 -0500

    icdiff: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -7,7 +7,7 @@ class Icdiff < Formula
   sha256 "ec21632b64159990a1bcedc8b25f96b476e7a6d9e18b75422420c0ae9b694eac"
   license "PSF-2.0"
   revision 1
-  head "https://github.com/jeffkaufman/icdiff.git"
+  head "https://github.com/jeffkaufman/icdiff.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "fc5be5fa45223aa9fd58fa6230a7e15c65a1878b94add36c62328f5295438b8e"
