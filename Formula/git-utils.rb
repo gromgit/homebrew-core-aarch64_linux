8be94eb554877f78557b2ad2afc4ebd4ee0dd3a7
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:42 -0500

    git-utils: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/git-utils.rb
+++ Formula/git-utils.rb
@@ -3,7 +3,7 @@ class GitUtils < Formula
   homepage "https://github.com/ddollar/git-utils"
   url "https://github.com/ddollar/git-utils/archive/v1.0.tar.gz"
   sha256 "a65252222222981d769fe2b19508e698fac4a0ce72e4ff07e74851e99a8fc813"
-  head "https://github.com/ddollar/git-utils.git"
+  head "https://github.com/ddollar/git-utils.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "7c7e5940ae9e46d0dfacb6b2b8db5dcf98626012d57d563a9ec1ca852bf9f0fb"
