be7aba35c46237e34e4d20a710ac581e8a8b49b4
Rui Chen <rui@chenrui.dev>
2021-12-19 16:08:14 -0500

    exult: add `head` branch
    Closes #91664.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/exult.rb
+++ Formula/exult.rb
@@ -4,7 +4,7 @@ class Exult < Formula
   url "https://github.com/exult/exult/archive/v1.6.tar.gz"
   sha256 "6176d9feba28bdf08fbf60f9ebb28a530a589121f3664f86711ff8365c86c17a"
   license "GPL-2.0-or-later"
-  head "https://github.com/exult/exult.git"
+  head "https://github.com/exult/exult.git", branch: "master"
 
   livecheck do
     url :stable
