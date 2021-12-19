06fe918dd1bcad600d4badf08ac855a4f56163e7
Rui Chen <rui@chenrui.dev>
2021-12-19 14:53:27 -0500

    hubflow: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/hubflow.rb
+++ Formula/hubflow.rb
@@ -6,7 +6,7 @@ class Hubflow < Formula
       tag:      "1.5.3",
       revision: "ed032438d2100b826d2fd5c8281b5e07d88ab9eb"
   license "BSD-2-Clause"
-  head "https://github.com/datasift/gitflow.git"
+  head "https://github.com/datasift/gitflow.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "5fcc66bfe17f292cb37ee437c016a60c9bc46ff63b8bc5ad5ac13cc649421a94"
