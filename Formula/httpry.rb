864905a8c59a57a4313ffc12add45ed146a60f57
Rui Chen <rui@chenrui.dev>
2021-12-19 14:53:26 -0500

    httpry: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/httpry.rb
+++ Formula/httpry.rb
@@ -4,7 +4,7 @@ class Httpry < Formula
   url "https://github.com/jbittel/httpry/archive/httpry-0.1.8.tar.gz"
   sha256 "b3bcbec3fc6b72342022e940de184729d9cdecb30aa754a2c994073447468cf0"
   license "GPL-2.0"
-  head "https://github.com/jbittel/httpry.git"
+  head "https://github.com/jbittel/httpry.git", branch: "master"
 
   bottle do
     rebuild 1
