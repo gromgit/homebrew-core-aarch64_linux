b8c1ddd817bf516ea23a267da49721c47be4da87
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:38 -0500

    git-extras: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/git-extras.rb
+++ Formula/git-extras.rb
@@ -4,7 +4,7 @@ class GitExtras < Formula
   url "https://github.com/tj/git-extras/archive/6.3.0.tar.gz"
   sha256 "8a218a0c8e10036d5ba14f26b70f994b0d11166b02ef3fed71c593cef026ec3d"
   license "MIT"
-  head "https://github.com/tj/git-extras.git"
+  head "https://github.com/tj/git-extras.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e6d445a34c00d43c1a4804b4b93c34af758c968983779a0781a9ae7bdd0c1882"
