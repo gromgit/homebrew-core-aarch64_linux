c70311cd03ad68cb503d35172c232c8f72d6e981
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:47 -0500

    go-jsonnet: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -4,7 +4,7 @@ class GoJsonnet < Formula
   url "https://github.com/google/go-jsonnet/archive/v0.17.0.tar.gz"
   sha256 "4fd04d0c9e38572ef388d28ea6b1ac151b8a9a5026ff94e3a68bdbc18c4db38a"
   license "Apache-2.0"
-  head "https://github.com/google/go-jsonnet.git"
+  head "https://github.com/google/go-jsonnet.git", branch: "master"
 
   bottle do
     rebuild 1
