30f7e49ac3c46734cb4e2ee72568ef085cc23827
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:44 -0500

    gitwatch: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gitwatch.rb
+++ Formula/gitwatch.rb
@@ -4,7 +4,7 @@ class Gitwatch < Formula
   url "https://github.com/gitwatch/gitwatch/archive/refs/tags/v0.1.tar.gz"
   sha256 "ed52d5c799c19dc6f920f3625964bc4b4948b6f8929c289aece462304e419697"
   license "GPL-3.0-or-later"
-  head "https://github.com/gitwatch/gitwatch.git"
+  head "https://github.com/gitwatch/gitwatch.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "5957f7e3213426ce3de17562735be772158d30c95fbede147a3952d9816b343c"
