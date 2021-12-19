e819988579b1d3bfa79d6d4cd75652dc222e27f8
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:48 -0500

    gollum: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gollum.rb
+++ Formula/gollum.rb
@@ -4,7 +4,7 @@ class Gollum < Formula
   url "https://github.com/trivago/gollum/archive/0.6.0.tar.gz"
   sha256 "2d9e7539342ccf5dabb272bbba8223d279a256c0901e4a27d858488dd4343c49"
   license "Apache-2.0"
-  head "https://github.com/trivago/gollum.git"
+  head "https://github.com/trivago/gollum.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ac1e8bf4e7ddbe753112b22cf9544bbc3f1f831c8878a16c9992c194c7e35fd5"
