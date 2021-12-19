0f55838fc6e7cf03cec3673da67dfae13036d24e
Rui Chen <rui@chenrui.dev>
2021-12-19 14:07:45 -0500

    known_hosts: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/known_hosts.rb
+++ Formula/known_hosts.rb
@@ -3,7 +3,7 @@ class KnownHosts < Formula
   homepage "https://github.com/markmcconachie/known_hosts"
   url "https://github.com/markmcconachie/known_hosts/archive/1.0.0.tar.gz"
   sha256 "80a080aa3850af927fd332e5616eaf82e6226d904c96c6949d6034deb397ac63"
-  head "https://github.com/markmcconachie/known_hosts.git"
+  head "https://github.com/markmcconachie/known_hosts.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f6f7c9c4638ffa5cff8f90e57ff9a8bbb3b2114e87d709e930e88120884859ad"
