0c8339df215c0e4fca03848505885da6fe4df1c5
Jeremy Long <jeremy.long@gmail.com>
2021-12-19 08:06:57 -0500

    dependency-check 6.5.1
    Closes #91641.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -1,8 +1,8 @@
 class DependencyCheck < Formula
   desc "OWASP dependency-check"
   homepage "https://owasp.org/www-project-dependency-check/"
-  url "https://github.com/jeremylong/DependencyCheck/releases/download/v6.5.0/dependency-check-6.5.0-release.zip"
-  sha256 "e27ab0c010a447e5d2ac62063112c3e1a25364193450cb4345fc18defb4511a2"
+  url "https://github.com/jeremylong/DependencyCheck/releases/download/v6.5.1/dependency-check-6.5.1-release.zip"
+  sha256 "b99626a16ee9e761d1747502856896c05386aad777da3a7a5ca21c31d55694fb"
   license "Apache-2.0"
 
   livecheck do
