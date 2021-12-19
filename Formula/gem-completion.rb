152b1c27ed36bd867a22f1c5f9a8e3e4a47ecaf0
Rui Chen <rui@chenrui.dev>
2021-12-19 15:30:35 -0500

    gem-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gem-completion.rb
+++ Formula/gem-completion.rb
@@ -5,7 +5,7 @@ class GemCompletion < Formula
       revision: "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
   version "2"
   license "MIT"
-  head "https://github.com/mernen/completion-ruby.git"
+  head "https://github.com/mernen/completion-ruby.git", branch: "master"
 
   livecheck do
     formula "ruby-completion"
