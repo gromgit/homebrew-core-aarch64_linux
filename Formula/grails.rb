bb3fdab915ccda8d465aaea387edd9d060a4188b
Rui Chen <rui@chenrui.dev>
2021-12-19 22:19:44 -0500

    grails 5.1.1
    Closes #91683.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v5.1.0/grails-5.1.0.zip"
-  sha256 "028299f9bad0eb9a81ffb7e0cb055e378374e08c8d05f6549c1ae97704341c14"
+  url "https://github.com/grails/grails-core/releases/download/v5.1.1/grails-5.1.1.zip"
+  sha256 "ca02c0481b3c18cf553340ccbf46aa3c75e4796294bdaa97d915cb1e58e864ef"
   license "Apache-2.0"
 
   livecheck do
