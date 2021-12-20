409b38abc3547a78413b257ec73ba8faed2bda47
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:44 -0500

    distcc: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/distcc.rb
+++ Formula/distcc.rb
@@ -5,7 +5,7 @@ class Distcc < Formula
   sha256 "2b99edda9dad9dbf283933a02eace6de7423fe5650daa4a728c950e5cd37bd7d"
   license "GPL-2.0-or-later"
   revision 1
-  head "https://github.com/distcc/distcc.git"
+  head "https://github.com/distcc/distcc.git", branch: "master"
 
   livecheck do
     url :stable
