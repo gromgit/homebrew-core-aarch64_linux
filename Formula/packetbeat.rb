0beafa0c93bfc86a1ac6b10454ea9064c9e86057
Rui Chen <rui@chenrui.dev>
2021-12-19 22:16:35 -0500

    packetbeat 7.16.2
    Closes #91678.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -2,8 +2,8 @@ class Packetbeat < Formula
   desc "Lightweight Shipper for Network Data"
   homepage "https://www.elastic.co/products/beats/packetbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v7.16.1",
-      revision: "7e56c4a053a2fe26c0cac168dd974780428a2aa6"
+      tag:      "v7.16.2",
+      revision: "3c518f4d17a15dc85bdd68a5a03d5af51d9edd8e"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
