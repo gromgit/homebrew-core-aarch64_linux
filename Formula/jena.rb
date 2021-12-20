50d54895e4822e9367ef225d90af16dfdb23af51
Caleb Xu <calebcenter@live.com>
2021-12-20 17:07:40 -0500

    jena 4.3.2
    Closes #91732.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,9 +1,9 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-4.3.1.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-4.3.1.tar.gz"
-  sha256 "e1dc24cd94fce3d1213efe22db773aaca6a8c1e52e05c02032d6abaa11ba5a09"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-4.3.2.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-4.3.2.tar.gz"
+  sha256 "f863717fbf519261d45c8f7d7b70592326e88848fc4e255f56d9604000e4bbe6"
   license "Apache-2.0"
 
   bottle do
