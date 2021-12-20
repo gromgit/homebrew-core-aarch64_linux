9be0038b1ef915dbe3d0e37cc9ebd2268217d394
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 17:48:36 +0800

    ccls: use llvm opt_prefix for resource dir
    `ccls` builds automatically run `clang -print-resource-dir` to find the Clang resource dir, which return a Cellar ref like `/usr/local/Cellar/llvm/13.0.0_2/lib/clang/13.0.0`, which breaks unnecessarily on revision bumps.
    
    This PR replaces it with the more stable `opt_prefix` ref instead (`/usr/local/opt/llvm/lib/clang/13.0.0`).
    
    Also added a test to catch mismatched `resource-dir` with LLVM dependency, so `llvm` _version_ bumps will force (required) `ccls` rebuilds.
    
    Closes #91706.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -4,7 +4,7 @@ class Ccls < Formula
   url "https://github.com/MaskRay/ccls/archive/0.20210330.tar.gz"
   sha256 "28c228f49dfc0f23cb5d581b7de35792648f32c39f4ca35f68ff8c9cb5ce56c2"
   license "Apache-2.0"
-  revision 2
+  revision 3
   head "https://github.com/MaskRay/ccls.git", branch: "master"
 
   bottle do
@@ -29,11 +29,16 @@ class Ccls < Formula
   fails_with gcc: "5"
 
   def install
-    system "cmake", *std_cmake_args
+    resource_dir = Utils.safe_popen_read(Formula["llvm"].bin/"clang", "-print-resource-dir").chomp
+    resource_dir.gsub! Formula["llvm"].prefix.realpath, Formula["llvm"].opt_prefix
+    system "cmake", *std_cmake_args, "-DCLANG_RESOURCE_DIR=#{resource_dir}"
     system "make", "install"
   end
 
   test do
-    system bin/"ccls", "-index=#{testpath}"
+    output = shell_output("#{bin}/ccls -index=#{testpath} 2>&1")
+
+    resource_dir = output.match(/resource-dir=(\S+)/)[1]
+    assert_path_exists "#{resource_dir}/include"
   end
 end
