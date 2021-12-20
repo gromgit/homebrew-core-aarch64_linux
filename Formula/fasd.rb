ccc04df2f42cf0725713e1838cf5b4fa5aa4c72b
Rui Chen <rui@chenrui.dev>
2021-12-20 10:05:06 -0500

    fasd: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/fasd.rb
+++ Formula/fasd.rb
@@ -4,7 +4,7 @@ class Fasd < Formula
   url "https://github.com/clvv/fasd/archive/1.0.1.tar.gz"
   sha256 "88efdfbbed8df408699a14fa6c567450bf86480f5ff3dde42d0b3e1dee731f65"
   license "MIT"
-  head "https://github.com/clvv/fasd.git"
+  head "https://github.com/clvv/fasd.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "9241df0f32971ce5a84c977f6908b93114946843813d5375ba7b983a7a783188"
