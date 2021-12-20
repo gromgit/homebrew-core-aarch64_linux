8cc9aec8d6a5e0d53d17bef0bd54782c56291f23
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:46 -0500

    docker-machine-driver-xhyve: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/docker-machine-driver-xhyve.rb
+++ Formula/docker-machine-driver-xhyve.rb
@@ -5,7 +5,7 @@ class DockerMachineDriverXhyve < Formula
       tag:      "v0.4.0",
       revision: "829c0968dac18547636f3ad6aa5ef83677f48267"
   license "BSD-3-Clause"
-  head "https://github.com/machine-drivers/docker-machine-driver-xhyve.git"
+  head "https://github.com/machine-drivers/docker-machine-driver-xhyve.git", branch: "master"
 
   bottle do
     rebuild 1
