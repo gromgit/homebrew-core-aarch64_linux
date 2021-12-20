90b79047ffa9ab886865480cb56f853c8225064f
Caleb Xu <calebcenter@live.com>
2021-12-20 16:40:52 -0500

    dylibbundler 1.0.3
    Closes #91726.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dylibbundler.rb
+++ Formula/dylibbundler.rb
@@ -1,8 +1,8 @@
 class Dylibbundler < Formula
   desc "Utility to bundle libraries into executables for macOS"
   homepage "https://github.com/auriamg/macdylibbundler"
-  url "https://github.com/auriamg/macdylibbundler/archive/1.0.2.tar.gz"
-  sha256 "6aed5e11078e597e3609cc5a02dfacb4218c12acc87066f6ae9e2dfb3b7c0b35"
+  url "https://github.com/auriamg/macdylibbundler/archive/1.0.3.tar.gz"
+  sha256 "84e37c6884b71a6d40d99202729dfc21479d53a82a343488f1d5378f0561d615"
   license "MIT"
   head "https://github.com/auriamg/macdylibbundler.git", branch: "master"
 
