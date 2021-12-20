3a3e6dbf54ec1f8e5ecb03cc8765cb01b35ea019
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:41 -0500

    devd: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/devd.rb
+++ Formula/devd.rb
@@ -4,7 +4,7 @@ class Devd < Formula
   url "https://github.com/cortesi/devd/archive/v0.9.tar.gz"
   sha256 "5aee062c49ffba1e596713c0c32d88340360744f57619f95809d01c59bff071f"
   license "MIT"
-  head "https://github.com/cortesi/devd.git"
+  head "https://github.com/cortesi/devd.git", branch: "master"
 
   bottle do
     rebuild 3
