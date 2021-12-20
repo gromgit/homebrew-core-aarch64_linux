79eddab8d4114777a231437b1a22abb98e3090c5
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:50 -0500

    drake: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/drake.rb
+++ Formula/drake.rb
@@ -4,7 +4,7 @@ class Drake < Formula
   url "https://raw.githubusercontent.com/Factual/drake/1.0.3/bin/drake-pkg"
   sha256 "adeb0bb14dbe39789273c5c766da9a019870f2a491ba1f0c8c328bd9a95711cc"
   license "EPL-1.0"
-  head "https://github.com/Factual/drake.git"
+  head "https://github.com/Factual/drake.git", branch: "develop"
 
   bottle do
     sha256 cellar: :any_skip_relocation, big_sur:  "6496f23e1c0b226eefaff9b6866943f55c6ae07539b0f9f640a5dcb1ee7bfb3f"
