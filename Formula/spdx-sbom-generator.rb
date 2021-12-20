75d0d1af79bec855e0ed288a17327ab7728f8be0
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2021-12-20 15:25:26 +0300

    spdx-sbom-generator: rebuild
    feat: change the org name of the project
    
    Closes #91709.
    
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/spdx-sbom-generator.rb
+++ Formula/spdx-sbom-generator.rb
@@ -1,7 +1,7 @@
 class SpdxSbomGenerator < Formula
   desc "Support CI generation of SBOMs via golang tooling"
-  homepage "https://github.com/spdx/spdx-sbom-generator"
-  url "https://github.com/spdx/spdx-sbom-generator/archive/refs/tags/v0.0.13.tar.gz"
+  homepage "https://github.com/opensbom-generator/spdx-sbom-generator"
+  url "https://github.com/opensbom-generator/spdx-sbom-generator/archive/refs/tags/v0.0.13.tar.gz"
   sha256 "7d088f136a53d1f608b1941362c568d78cc6279df9c1bdb3516de075cb7f10c3"
   license any_of: ["Apache-2.0", "CC-BY-4.0"]
 
