32c83b2fe7c8e15b13f1e54d1022e79d04a4a611
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:49 -0500

    doubledown: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/doubledown.rb
+++ Formula/doubledown.rb
@@ -4,7 +4,7 @@ class Doubledown < Formula
   url "https://github.com/devstructure/doubledown/archive/v0.0.2.tar.gz"
   sha256 "47ff56b6197c5302a29ae4a373663229d3b396fd54d132adbf9f499172caeb71"
   license "BSD-2-Clause"
-  head "https://github.com/devstructure/doubledown.git"
+  head "https://github.com/devstructure/doubledown.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "78f3d65bdfb3d5184fc3a10c8b161e9e41a4d14715c284d2a179ebf5fbe52209"
