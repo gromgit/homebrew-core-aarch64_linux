44db4b96a3cb9ceaba7cfad4e24b889b0ff11292
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-20 07:05:52 +0000

    goose: update 3.5.0 bottle.

--- Formula/goose.rb
+++ Formula/goose.rb
@@ -6,12 +6,12 @@ class Goose < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5c48fc8b54b098ce182190d77b2016971cac4a11bf296e3e226e21343f7cb3b6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e76fdc96dd47d19b39113ee696235be5ec513ae05cd0b2310f7e3d2edc8fa35b"
-    sha256 cellar: :any_skip_relocation, monterey:       "adfdeafe5b3cd096b89108fbb33453f543e800fabf5869b7542c9a46f81446e2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6355e7b08e54bc034f150d24ba9e553abf620566a76b01b7db13c5e3f021af80"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b8beef84abcd5f10954d99ce74c89735e1d3304a7cc72779ea5e584b97f66ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5befba5803fa95ce15bfb9b36b41042d65e38e006256a6f5e0843fcdd8a44609"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dda1576e717341e3962002901164c94dab4a34c51416696fb268de838735bcc5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9952f6a72f01ace913ad6782d598bfa72ca721e5ff751a88841681131141f92b"
+    sha256 cellar: :any_skip_relocation, monterey:       "49638e9e10f889bdadc07f7a68f022c78e671589a44775df929f67b430182e92"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0befd9144c84fa729006829bd62826036fdbe903af42ca78b9bb0ea1f3f8724d"
+    sha256 cellar: :any_skip_relocation, catalina:       "46cd1e2b2e4af854f9379b9275703641b7be6f8a4ac05be6d876d12291e983fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "272d77d3f8445cf9a255a2b4308435833c1915b744927443ae430d0459340e0a"
   end
 
   depends_on "go" => :build
