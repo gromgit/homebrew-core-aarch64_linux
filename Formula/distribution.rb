265abde0a4cf0d6dae23713546830df770a0c713
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:44 -0500

    distribution: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/distribution.rb
+++ Formula/distribution.rb
@@ -4,7 +4,7 @@ class Distribution < Formula
   url "https://github.com/philovivero/distribution/archive/1.3.tar.gz"
   sha256 "d7f2c9beeee15986d24d8068eb132c0a63c0bd9ace932e724cb38c1e6e54f95d"
   license "GPL-2.0"
-  head "https://github.com/philovivero/distribution.git"
+  head "https://github.com/philovivero/distribution.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "46e6afa7ee3cdc08f4fde478e6235b1df80813391abe507505e1452926d5aff2"
