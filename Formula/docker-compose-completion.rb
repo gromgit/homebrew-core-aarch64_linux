997937b83be94e55400066be4ef0a60d86d8dc06
Rui Chen <rui@chenrui.dev>
2021-12-20 10:08:45 -0500

    docker-compose-completion: add `head` branch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/docker-compose-completion.rb
+++ Formula/docker-compose-completion.rb
@@ -4,7 +4,7 @@ class DockerComposeCompletion < Formula
   url "https://github.com/docker/compose/archive/1.29.2.tar.gz"
   sha256 "99a9b91d476062d280c889ae4e9993d7dd6a186327bafb2bb39521f9351b96eb"
   license "Apache-2.0"
-  head "https://github.com/docker/compose.git"
+  head "https://github.com/docker/compose.git", branch: "v2"
 
   bottle do
     rebuild 1
