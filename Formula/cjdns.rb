e9b7c2be422e40fe4f2bca7af90245fbacc97d61
Caleb Xu <calebcenter@live.com>
2021-12-21 11:06:15 -0500

    cjdns 21.2
    Closes #91812.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cjdns.rb
+++ Formula/cjdns.rb
@@ -1,8 +1,8 @@
 class Cjdns < Formula
   desc "Advanced mesh routing system with cryptographic addressing"
   homepage "https://github.com/cjdelisle/cjdns/"
-  url "https://github.com/cjdelisle/cjdns/archive/cjdns-v21.1.tar.gz"
-  sha256 "a6158ce7847159aa44e86f74ccc7b6ded6910a230ed8f3830db53cda5838f0b0"
+  url "https://github.com/cjdelisle/cjdns/archive/cjdns-v21.2.tar.gz"
+  sha256 "dec6ba261b423ea08e3a62a146ffd5db2b49a0b954a37fc37b24b35da2f7f773"
   license all_of: ["GPL-3.0-or-later", "GPL-2.0-or-later", "BSD-3-Clause", "MIT"]
   head "https://github.com/cjdelisle/cjdns.git", branch: "master"
 
@@ -14,18 +14,37 @@ class Cjdns < Formula
   end
 
   depends_on "node" => :build
+  depends_on "rust" => :build
 
   def install
+    # Avoid using -march=native
+    inreplace "node_build/make.js",
+              "var NO_MARCH_FLAG = ['arm', 'ppc', 'ppc64'];",
+              "var NO_MARCH_FLAG = ['x64', 'arm', 'arm64', 'ppc', 'ppc64'];"
+
     system "./do"
-    bin.install "cjdroute"
-    (pkgshare/"test").install "build_darwin/test_testcjdroute_c" => "cjdroute_test"
-    rm_f "build_darwin/test_testcjdroute_c"
-    (pkgshare/"test").install "build_darwin"
+    bins = %w[cjdroute makekeys privatetopublic publictoip6 randombytes sybilsim]
+    bin.install(*bins)
+
+    # Avoid conflict with mkpasswd from `expect`
+    bin.install "mkpasswd" => "cjdmkpasswd"
+
+    man1.install "doc/man/cjdroute.1"
+    man5.install "doc/man/cjdroute.conf.5"
   end
 
   test do
-    cp_r pkgshare/"test/cjdroute_test", testpath
-    cp_r pkgshare/"test/build_darwin", testpath
-    system "./cjdroute_test", "all"
+    sample_conf = JSON.parse(shell_output("#{bin}/cjdroute --genconf"))
+    sample_private_key = sample_conf["privateKey"]
+    sample_public_key = sample_conf["publicKey"]
+    sample_ipv6 = IPAddr.new(sample_conf["ipv6"]).to_s
+
+    expected_output = <<~EOS
+      Input privkey: #{sample_private_key}
+      Matching pubkey: #{sample_public_key}
+      Resulting address: #{sample_ipv6}
+    EOS
+
+    assert_equal expected_output, pipe_output(bin/"privatetopublic", sample_private_key)
   end
 end
