8b218807a925418babb99ffa7b884a4a7e371e63
İsmail Dönmez <ismail@i10z.com>
2021-12-21 19:25:26 +0100

    qpdf 10.5.0
    Closes #91814.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,8 +1,8 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.4.0/qpdf-10.4.0.tar.gz"
-  sha256 "9ac6e691cc3f35a9fe44632e3fba727e1b6ef21181c0a883287abf5cf97ae222"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.5.0/qpdf-10.5.0.tar.gz"
+  sha256 "88257d36a44fd5c50b2879488324dd9cafc11686ae49d8c4922a4872203ce006"
   license "Apache-2.0"
 
   bottle do
