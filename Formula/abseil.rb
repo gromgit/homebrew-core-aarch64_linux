36bc0e10eca6d0f31050483363223228fe30c785
İsmail Dönmez <ismail@i10z.com>
2021-12-21 22:58:03 +0100

    abseil 20211102.0

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -1,10 +1,9 @@
 class Abseil < Formula
   desc "C++ Common Libraries"
   homepage "https://abseil.io"
-  url "https://github.com/abseil/abseil-cpp/archive/20210324.2.tar.gz"
-  sha256 "59b862f50e710277f8ede96f083a5bb8d7c9595376146838b9580be90374ee1f"
+  url "https://github.com/abseil/abseil-cpp/archive/refs/tags/20211102.0.tar.gz"
+  sha256 "dcf71b9cba8dc0ca9940c4b316a0c796be8fab42b070bb6b7cab62b48f0e66c4"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/abseil/abseil-cpp.git", branch: "master"
 
   bottle do
