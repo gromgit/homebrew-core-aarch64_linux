bcc554f55311a9008cc1f16c92903c745a0eb1e0
Michka Popoff <michkapopoff@gmail.com>
2021-12-21 19:37:44 +0100

    libbitcoin-node: revision bump (boost 1.76.0)

--- Formula/libbitcoin-node.rb
+++ Formula/libbitcoin-node.rb
@@ -4,7 +4,7 @@ class LibbitcoinNode < Formula
   url "https://github.com/libbitcoin/libbitcoin-node/archive/v3.6.0.tar.gz"
   sha256 "9556ee8aab91e893db1cf343883034571153b206ffbbce3e3133c97e6ee4693b"
   license "AGPL-3.0"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
@@ -20,6 +20,8 @@ class LibbitcoinNode < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  # https://github.com/libbitcoin/libbitcoin-system/issues/1234
+  depends_on "boost@1.76"
   depends_on "libbitcoin-blockchain"
   depends_on "libbitcoin-network"
 
@@ -30,13 +32,14 @@ class LibbitcoinNode < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--with-boost-libdir=#{Formula["boost"].opt_lib}"
+                          "--with-boost-libdir=#{Formula["boost@1.76"].opt_lib}"
     system "make", "install"
 
     bash_completion.install "data/bn"
   end
 
   test do
+    boost = Formula["boost@1.76"]
     (testpath/"test.cpp").write <<~EOS
       #include <bitcoin/node.hpp>
       int main() {
@@ -48,9 +51,10 @@ class LibbitcoinNode < Formula
       }
     EOS
     system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test",
+                    "-I#{boost.include}",
                     "-L#{Formula["libbitcoin"].opt_lib}", "-lbitcoin",
                     "-L#{lib}", "-lbitcoin-node",
-                    "-L#{Formula["boost"].opt_lib}", "-lboost_system"
+                    "-L#{boost.lib}", "-lboost_system"
     system "./test"
   end
 end
