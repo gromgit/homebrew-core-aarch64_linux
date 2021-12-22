bb339788a014a79a55d4bf8159a6e6a57abedc04
rui <rui@chenrui.dev>
2021-12-22 21:01:48 +0000

    seaweedfs: add 2.82 bottle.

--- Formula/seaweedfs.rb
+++ Formula/seaweedfs.rb
@@ -7,6 +7,15 @@ class Seaweedfs < Formula
   license "Apache-2.0"
   head "https://github.com/chrislusf/seaweedfs.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3e63c6475cb7f0c8d4ece265202a411df77e18780e6918312c15d267e4777c3f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "526188b0256a1919a4de0b929f00aaaea9574b9bccc45c658544133504c0e89a"
+    sha256 cellar: :any_skip_relocation, monterey:       "fe4fd25dff32d0adc789de82ac523d1beb3cb61342e6f0e552d55b84a988dec4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "71044ecf0aef141a4027423afb46428535cef13a563286cec4b11f4ca855a2cc"
+    sha256 cellar: :any_skip_relocation, catalina:       "ef602ee8f0724d479b973a03a6109691fb1b499c8cd25cd0b7eda30e426cb246"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3be0237b19da00f809a3aebb8e40aa433ff88b54b574b4f1fed5bb45c4d89620"
+  end
+
   depends_on "go" => :build
 
   def install
