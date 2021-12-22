98baab91548c4695db2d109b1a7e107a75226ec8
Dustin Rodrigues <dust.rod@gmail.com>
2021-12-22 09:40:32 -0500

    hexo 6.0.0
    Closes #91853.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hexo.rb
+++ Formula/hexo.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Hexo < Formula
   desc "Fast, simple & powerful blog framework"
   homepage "https://hexo.io/"
-  url "https://registry.npmjs.org/hexo/-/hexo-5.4.0.tgz"
-  sha256 "bf80f6d840f2e6cc5f6dd5e4ea0246dc45ae8f387d66f3b8d1ba3d49474f8399"
+  url "https://registry.npmjs.org/hexo/-/hexo-6.0.0.tgz"
+  sha256 "a83164446bd99ef601ccdb7c70ca79d7eb6d4275b31ea2f77d85fd18b340b889"
   license "MIT"
   head "https://github.com/hexojs/hexo.git", branch: "master"
 
