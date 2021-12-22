1f8dd69b6b2bfd0d90e9ee792cdfde70469b8c67
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-22 16:05:44 +0000

    hexo: update 6.0.0 bottle.

--- Formula/hexo.rb
+++ Formula/hexo.rb
@@ -9,13 +9,12 @@ class Hexo < Formula
   head "https://github.com/hexojs/hexo.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "45740245dd9dc4d32082c287c2e8d0690d51cc8ed78f330308196dfc9ceaf02b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4c38e6ac4e73f52e5ecbcfb75cd80e8997653bc1c8d9967f5813486517ccaa01"
-    sha256 cellar: :any_skip_relocation, monterey:       "8d60f23ff0b6e83cf5087da41148271cb40b5f3691992b03633eb525b0ec2ea9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "55bcaac970db5cb434a16c27e5165dc338cdcf6498b8243936c5bcc509ea2eae"
-    sha256 cellar: :any_skip_relocation, catalina:       "5bcbf7709254e2615b78b583f8c70336ecc5f20702ae5a855d7c6bb9ac2c4908"
-    sha256 cellar: :any_skip_relocation, mojave:         "dbc44b1c38643e08908e1978f0142c2caf1a6ec8a3ee6663c8038855de222612"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1cd4a6bb23bbec22299b6ee1e350f14a48b562a54ace403dab0ee0eae84f608e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7fc97861d0cd43452a8d38ffaa13e452014c4a19c80c609204d0dc7fa267683c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7fc97861d0cd43452a8d38ffaa13e452014c4a19c80c609204d0dc7fa267683c"
+    sha256 cellar: :any_skip_relocation, monterey:       "a563af9273158a6f25013ac7db09cd19cc43dc37d6b65aa24c019aa4d214338f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a563af9273158a6f25013ac7db09cd19cc43dc37d6b65aa24c019aa4d214338f"
+    sha256 cellar: :any_skip_relocation, catalina:       "a563af9273158a6f25013ac7db09cd19cc43dc37d6b65aa24c019aa4d214338f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b69ee97a679ccf8efeebb2a7639fcc5ff0225a39aac32a9fd38ae89ffe6b7ab3"
   end
 
   depends_on "node"
