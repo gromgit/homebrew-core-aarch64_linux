4e4189f38d9f4724367967871d4733842795ee82
Rui Chen <rui@chenrui.dev>
2021-12-23 13:03:07 -0500

    ethereum 1.10.14
    Closes #91946.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ethereum.rb
+++ Formula/ethereum.rb
@@ -1,8 +1,8 @@
 class Ethereum < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://geth.ethereum.org/"
-  url "https://github.com/ethereum/go-ethereum/archive/v1.10.13.tar.gz"
-  sha256 "cda0fa3fad8937a425b157c16143385a3fb5b576c1d14922b489106e1c65e4df"
+  url "https://github.com/ethereum/go-ethereum/archive/v1.10.14.tar.gz"
+  sha256 "cf1089e0316bf7b7705a43099b140d10eb5e556849d4dd9c7db101cda55e76e4"
   license "LGPL-3.0-or-later"
   head "https://github.com/ethereum/go-ethereum.git", branch: "master"
 
