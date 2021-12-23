32ace7b0b8a78c6d490ed59bb05ac3dcdb4402a5
Rui Chen <rui@chenrui.dev>
2021-12-23 11:19:03 -0500

    vault-cli 3.5.8
    Closes #91939.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vault-cli.rb
+++ Formula/vault-cli.rb
@@ -1,8 +1,8 @@
 class VaultCli < Formula
   desc "Subversion-like utility to work with Jackrabbit FileVault"
   homepage "https://jackrabbit.apache.org/filevault/index.html"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.5.6/vault-cli-3.5.6-bin.tar.gz"
-  sha256 "3319ea779ec13624405183094b6f3362de489a6d3337278494337a244c8b31f9"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.5.8/vault-cli-3.5.8-bin.tar.gz"
+  sha256 "c41975ad4f6fdb12e88bdd0fb81437f3cb4a1119d6362135eaf843ae223d57ae"
   license "Apache-2.0"
   head "https://github.com/apache/jackrabbit-filevault.git", branch: "master"
 
