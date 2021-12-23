18cdf77a00e090b098a986d7baba04bbc76eab1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-23 09:06:02 +0000

    moreutils: update 0.67 bottle.

--- Formula/moreutils.rb
+++ Formula/moreutils.rb
@@ -11,13 +11,12 @@ class Moreutils < Formula
   head "https://git.joeyh.name/git/moreutils.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c93055c0a353c93a20b0421a85eb055cc54340d1793cf12dd0fad275be123b71"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7bb6a5498e898c303cbbb526f83a12e690315c43260ebab1cf348ecb2a712bb"
-    sha256 cellar: :any_skip_relocation, monterey:       "4d5c2460a0d242df46376e618207038722ffc65a9e3e6379fdd229ce821f4d52"
-    sha256 cellar: :any_skip_relocation, big_sur:        "98e36f509fe2556660dec088c1dcf380cf1ce60167fcb4289782614feb381821"
-    sha256 cellar: :any_skip_relocation, catalina:       "e7774183139434a9f4707a5931793fb2aed0678fd0257a5860571f320f507c8e"
-    sha256 cellar: :any_skip_relocation, mojave:         "099c5b0cc96ae204c211da65446db79bf83469ed0bcbc40d81e5a9e95b5a678c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0890cc88f2e311ec01cc0439cbfa6a7a43166ebaeb54a8bfa9d22fc21fba4bb4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "97487d8fcda41988e9b03a87770260953fc05e2282e069a35fc5742e804ed0ec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8ddb7455bee7e0dfc6b870a36c4cc2eb6462058d69f19d104c9522386a49ef4"
+    sha256 cellar: :any_skip_relocation, monterey:       "4bbca70595aa3ebf16c1297b00725a95d32220e5b849db8b024b724a953e923a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "89bba0318b0d38d10e799a4d8a39b2b1e32bcab22cb416f73aff938c765b8d3e"
+    sha256 cellar: :any_skip_relocation, catalina:       "6436bbcfa0d40fe6977f76e212665c89120a920c1e6b2661ea8fbb675ab2e4e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "585a1be3b05cb1a625571aaa5ecf67333cbd2c329152e0526b6dee4510015a76"
   end
 
   depends_on "docbook-xsl" => :build
