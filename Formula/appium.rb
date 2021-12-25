04651e38e22d14829e66f48700e1c1053bb07296
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-25 10:06:13 +0000

    appium: update 1.22.2 bottle.

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -9,12 +9,12 @@ class Appium < Formula
   head "https://github.com/appium/appium.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0fbd3e1156183a1f11517e5a5a460254e4d922c205668ab2fc7201151e696291"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0fbd3e1156183a1f11517e5a5a460254e4d922c205668ab2fc7201151e696291"
-    sha256 cellar: :any_skip_relocation, monterey:       "3ae4a92de2de3e0bed0dfceb58730df421876c5c2c0eae78c453fc7432e29764"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3ae4a92de2de3e0bed0dfceb58730df421876c5c2c0eae78c453fc7432e29764"
-    sha256 cellar: :any_skip_relocation, catalina:       "3ae4a92de2de3e0bed0dfceb58730df421876c5c2c0eae78c453fc7432e29764"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0fbd3e1156183a1f11517e5a5a460254e4d922c205668ab2fc7201151e696291"
+    sha256                               arm64_monterey: "b303fb595cec5f55e6e5d372beb6e39ebf9df55c65bb9564559f3e887d3328a6"
+    sha256                               arm64_big_sur:  "4a58eb73615f523e0277444f5ed4f2fb524c6885e6b5a1348c6f191d7f62790d"
+    sha256                               monterey:       "ac54199b4ba32708ebc956fd476fedc68146fd609bfc4522955de70de57e974a"
+    sha256                               big_sur:        "b8a9bbea0b0f8bff3f13c68650b085d0719da4e4329029cecb1e28451c3b4b2e"
+    sha256                               catalina:       "a0340dcfc426b989e9b890a568ff546f3d3b6442cd2db7abdbe36d8b6e16b606"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5ef58ddfa93e0b6ac0124601ff4e2918ff6cee7a49e187b182463a31e609f09f"
   end
 
   depends_on "node"
