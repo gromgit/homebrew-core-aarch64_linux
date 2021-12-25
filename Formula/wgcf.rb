5fb8d1fec9d00d3e48e5d41c969cb33ef38d9f92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-25 20:05:59 +0000

    wgcf: update 2.2.11 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -7,12 +7,12 @@ class Wgcf < Formula
   head "https://github.com/ViRb3/wgcf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5480e2917f6c696d1294192025b3e43e9080ff5ab65ba261bc315c6d6768296"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c15330d785fd02054d3840e96b9e4f3e66cc363d12926c9517cbb069bd692605"
-    sha256 cellar: :any_skip_relocation, monterey:       "b24c51bf75256c38bc5c53acc6fb937fdce1ff255d170a94b8eac56d661e2ca8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8532c435ee59f15be84341afabba9aab5c4bbdf752e712b99dacaa485b1a4c43"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8b68351c762af758ea6c9794242a1ad1c8dc9b4a78c6c53fc4c5e5b549318e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "906c27d249dae1de77217b51113826d5cf12f1c301a567105bfea96df9ddb5eb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2c9ab4ee5438ca27381d05b912f33c8ff5d83dd5429ead649407199f8ffe3f7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7b646ca15a17a35bb54438bd7bbdd05101645a16778a7cb148ed7079e376424b"
+    sha256 cellar: :any_skip_relocation, monterey:       "30becd01302bbf87c4d5abc36c67c3fc15cea4d5876ae0324e4c9b86ed01a0cb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "833f58fe2d6394c6ed4e3975e02f0caec437ad9c4b280612aef10ae778477608"
+    sha256 cellar: :any_skip_relocation, catalina:       "cd505bdce04eab3c486056786d2c9f13fd69e035f8e57e903c35259002494e31"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb2129aa956b1f937c285819dcec9cea0845683e082bcfd97bbe69f33e86d2d6"
   end
 
   depends_on "go" => :build
