cf32a9fe010d8aca27a550eac6c1258381450322
Marc-Aurel Audoin <714056+MarcAurel@users.noreply.github.com>
2021-12-25 23:44:55 +0100

    tidy-viewer 1.4.3 (new formula)
    Closes #92031.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/tidy-viewer.rb
@@ -0,0 +1,19 @@
+class TidyViewer < Formula
+  desc "CLI csv pretty printer"
+  homepage "https://github.com/alexhallam/tv"
+  url "https://github.com/alexhallam/tv/archive/refs/tags/1.4.3.tar.gz"
+  sha256 "e308eb088d059d18119dc757c98487d9cabc2f4b97035a8dc9f8253717aa9fe9"
+  license "Unlicense"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    bin.install_symlink "tidy-viewer" => "tv"
+  end
+
+  test do
+    (testpath/"test.csv").write("first header,second header")
+    assert_match "first header", shell_output("#{bin}/tv #{testpath}/test.csv")
+  end
+end
