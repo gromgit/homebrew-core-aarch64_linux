182284b8ec07d2316220da95b0d5dfdff3288e7a
Rui Chen <rui@chenrui.dev>
2021-12-24 22:42:17 -0500

    moarvm 2021.12
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/moarvm.rb
+++ Formula/moarvm.rb
@@ -1,8 +1,8 @@
 class Moarvm < Formula
   desc "Virtual machine for NQP and Rakudo Perl 6"
   homepage "https://moarvm.org"
-  url "https://github.com/MoarVM/MoarVM/releases/download/2021.10/MoarVM-2021.10.tar.gz"
-  sha256 "7f3487a70e8b77be0e4e2f12b14c49f6a01d0378e0940c86958c9016c495ad75"
+  url "https://github.com/MoarVM/MoarVM/releases/download/2021.12/MoarVM-2021.12.tar.gz"
+  sha256 "d49bbeb10d9616c2d87a7fadd1c6bb7a5ce11c19c7c44bbb8bef76f25b505c4f"
   license "Artistic-2.0"
 
   livecheck do
@@ -27,8 +27,8 @@ class Moarvm < Formula
   conflicts_with "rakudo-star", because: "rakudo-star currently ships with moarvm included"
 
   resource "nqp" do
-    url "https://github.com/Raku/nqp/releases/download/2021.10/nqp-2021.10.tar.gz"
-    sha256 "48135bc1b3ce22c1c57ccef46531e7688eec83d6b905b502ffcefd6c23294b49"
+    url "https://github.com/Raku/nqp/releases/download/2021.12/nqp-2021.12.tar.gz"
+    sha256 "0e1d534fd1ee61a4c8072309bafbc04660e9ae72d88618daa0e8f15022f0d913"
   end
 
   def install
