eacca8f519b0e8883a95e384086907c675df2509
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-12-26 13:35:23 +0000

    lighthouse: add 2.0.1 bottle.

--- Formula/lighthouse.rb
+++ Formula/lighthouse.rb
@@ -5,6 +5,15 @@ class Lighthouse < Formula
   sha256 "b1ae766c0b335e963d7ebd6ab2a02386078a6b2ab688ec5da3604191a4d0d3f4"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e76be0c60e372502f6196e737e2b6bb95338177348c81e448c388f198409cb26"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "da72ca40074a9b9fe1c8f6a27e124c8d4cfc9e8d615c946dc52f776bc500cc66"
+    sha256 cellar: :any_skip_relocation, monterey:       "0e3f8a78d2fa3b30686c71e3831c016e390673eb6489ce91ccaae656bed60698"
+    sha256 cellar: :any_skip_relocation, big_sur:        "175904ad8f7c5f407c34168afb001a2ab15ca490d71f70416487073c00c35dde"
+    sha256 cellar: :any_skip_relocation, catalina:       "a97b97e0526557df5640632f1a17bff99d693d6ea307890f8efbbe49bedb1888"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "acec2b6e051ee53d9837d47c71b6c0428328e1b35779996fd1b3c1fe5939a372"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
