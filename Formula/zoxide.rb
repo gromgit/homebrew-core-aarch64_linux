705df7d11d94d21174fa96d3966d76a41cba626d
Kevin Clark <kevin.clark@gmail.com>
2021-12-25 21:46:02 -0800

    zoxide 0.8.0
    Closes #92035.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -1,8 +1,8 @@
 class Zoxide < Formula
   desc "Shell extension to navigate your filesystem faster"
   homepage "https://github.com/ajeetdsouza/zoxide"
-  url "https://github.com/ajeetdsouza/zoxide/archive/v0.7.9.tar.gz"
-  sha256 "aea1a55d107a2367d46be19617fbd8fb440d906121a84d6d9e7b86f3a24fbad4"
+  url "https://github.com/ajeetdsouza/zoxide/archive/v0.8.0.tar.gz"
+  sha256 "111c5f1cd92b1cb54e2f0a801003098a601c653ec5a378f3d0ea1c9659854477"
   license "MIT"
 
   bottle do
