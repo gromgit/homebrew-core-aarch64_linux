66e9c04fd909b7f6a4fcde27953d1838ba0c3494
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-26 19:05:54 +0000

    fselect: update 0.7.9 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -6,12 +6,12 @@ class Fselect < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6d92364f569f400fe378abc06e462c0986de710e3aa000f3422f399244250afc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2ddb0ff605c5602edee7a88c6d743838ee0476cf457c4e094a7b590ae5cd2b57"
-    sha256 cellar: :any_skip_relocation, monterey:       "cdedcb8dd1c50a6fd521823017c9dd15bb56f3ddcdcd61ed2c258e9d80848bc8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fe0aabf4acc22002b1fd6006c4710237673b477e377cdfadaaf115bb58414a2e"
-    sha256 cellar: :any_skip_relocation, catalina:       "27356c38ecd5bc766abe0161dd44a8879187f8eb84e9e65ddf281bc5fd8380f6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ce2089482594c2e4e341bd5846c1ce65920bcfd73d6a62f3298fc937295b4c11"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "de48331a0819e96a03a39749439eda8a425ca8bc779fdc8f6a66cf8c0b265993"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "198e02be02d2fc753043de25b4a3af0413de1e14555aef56fcb2ec4873c8c953"
+    sha256 cellar: :any_skip_relocation, monterey:       "4db23e9443982fb98b5ab51bb21db3546a2a452f7e5b452170d73be37e90237c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b546442020191ec01fb0d6805fd59bc33d2b6b791f1ad8b4f97a252cd4777137"
+    sha256 cellar: :any_skip_relocation, catalina:       "3dbf324dde4a70d1d97fd7ab1331df4dfec1bfc716e022223ef6df88d7e8d9b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "89521abb5efb2d069fb3c8cc8ba885c39f5b2f5b2524445cc78b95b8825349eb"
   end
 
   depends_on "rust" => :build
