53d96951f9f5789bb191098a42b419ae5255d3b8
Rui Chen <rui@chenrui.dev>
2021-12-28 10:01:02 -0500

    k9s 0.25.17
    Closes #92130.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,8 +2,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.25.16",
-      revision: "fc8ffe5d37d068f14e86aed7e8b847d016c99b19"
+      tag:      "v0.25.17",
+      revision: "2a9f5464c38f3c303da03da89eefc6661020cf28"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git", branch: "master"
 
