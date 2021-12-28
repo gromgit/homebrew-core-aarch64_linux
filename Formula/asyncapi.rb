cf1babeeb26c50def373072e0d0a5bac84827fb4
derberg <lpgornicki@gmail.com>
2021-12-28 11:52:01 +0100

    asyncapi 0.11.2 (new formula)
    * asyncapi v0.11.2 (new formula)
    * fix class name
    * add missing newline
    * remove not needed formula part basing on review
    * adjust insall script for postpack
    * added comments
    * styleguide fix
    * add replacement of rimraf with rm
    
    Closes #92115.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/asyncapi.rb
@@ -0,0 +1,25 @@
+require "language/node"
+
+class Asyncapi < Formula
+  desc "All in one CLI for all AsyncAPI tools"
+  homepage "https://github.com/asyncapi/cli"
+  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.11.2.tgz"
+  sha256 "75e4d0e73f0ea8693ccfa684c3fd5ca26cab4f4a634c9064b90520093df882bc"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    # Call rm -f instead of rimraf, because devDeps aren't present in Homebrew at postpack time
+    inreplace "package.json", "rimraf oclif.manifest.json", "rm -f oclif.manifest.json"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    # Replace universal binaries with their native slices
+    deuniversalize_machos
+  end
+
+  test do
+    system bin/"asyncapi", "new", "--file-name=asyncapi.yml", "--example=default-example.yaml", "--no-tty"
+    assert_predicate testpath/"asyncapi.yml", :exist?, "AsyncAPI file was not created"
+  end
+end
