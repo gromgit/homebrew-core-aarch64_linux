f19e21cb6f5f18f0664e737820320e5312d4dc37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-28 18:05:26 +0000

    latexdiff: update 1.3.2 bottle.

--- Formula/latexdiff.rb
+++ Formula/latexdiff.rb
@@ -6,14 +6,12 @@ class Latexdiff < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5fd8db27743a46025e5dbd9ab31181fbdff44648e1b12e850e4c88878e466391"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "90e14ec7fe78b06d74521a9d8ff102589d63935e70f40cfcb7af63d7dbdd0d43"
-    sha256 cellar: :any_skip_relocation, monterey:       "d47104ab0a06e597cea157f9a902d5fa2df1123e195bc4778d611cb1916119fe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9ea5219166059a04ab1f510d92339e2443f883a114384d5f8af30b2631ce4921"
-    sha256 cellar: :any_skip_relocation, catalina:       "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
-    sha256 cellar: :any_skip_relocation, mojave:         "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "8eb979b1b52125f102bbc56bbc4611d5b8075f003318307a2205485d95c789aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fd8db27743a46025e5dbd9ab31181fbdff44648e1b12e850e4c88878e466391"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6cb7541c9dcddb8fa58cff401cea3313d9602f1829f77d16a8bab42e9909253c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6cb7541c9dcddb8fa58cff401cea3313d9602f1829f77d16a8bab42e9909253c"
+    sha256 cellar: :any_skip_relocation, monterey:       "c79073ff78df1073956006d14de25a9f7c88d11ce434508f75ca0a6816d2ec74"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c79073ff78df1073956006d14de25a9f7c88d11ce434508f75ca0a6816d2ec74"
+    sha256 cellar: :any_skip_relocation, catalina:       "c79073ff78df1073956006d14de25a9f7c88d11ce434508f75ca0a6816d2ec74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6cb7541c9dcddb8fa58cff401cea3313d9602f1829f77d16a8bab42e9909253c"
   end
 
   # osx default perl cause compilation error
