ea65a3561ce4b25ee9c1fba2e16dc9106941fde5
Rui Chen <rui@chenrui.dev>
2021-12-27 23:24:36 -0500

    wangle 2021.12.27.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/wangle.rb
+++ Formula/wangle.rb
@@ -1,8 +1,8 @@
 class Wangle < Formula
   desc "Modular, composable client/server abstractions framework"
   homepage "https://github.com/facebook/wangle"
-  url "https://github.com/facebook/wangle/releases/download/v2021.12.20.00/wangle-v2021.12.20.00.tar.gz"
-  sha256 "7ef6616926fea46481591ab3741dc7bccdd2514f354b819500ab99110ef567ee"
+  url "https://github.com/facebook/wangle/releases/download/v2021.12.27.00/wangle-v2021.12.27.00.tar.gz"
+  sha256 "306ac9b25a380fdefe2a6dac91b51802da2758a93d5b414d1cdf17e4c2839cc7"
   license "Apache-2.0"
   head "https://github.com/facebook/wangle.git", branch: "master"
 
