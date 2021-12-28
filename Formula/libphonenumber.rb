cc1acbb8bb8378afd004ab4f9cf8f386a533e35d
Porkepix <porkepix@gmail.com>
2021-12-28 01:35:16 +0100

    libphonenumber 8.12.40
    Closes #92090.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libphonenumber.rb
+++ Formula/libphonenumber.rb
@@ -1,10 +1,9 @@
 class Libphonenumber < Formula
   desc "C++ Phone Number library by Google"
   homepage "https://github.com/google/libphonenumber"
-  url "https://github.com/google/libphonenumber/archive/v8.12.39.tar.gz"
-  sha256 "ff16330f130917e42bc0b1a7efe5e4fba46633bfa62e35268acec855e17e385c"
+  url "https://github.com/google/libphonenumber/archive/v8.12.40.tar.gz"
+  sha256 "2ab57fa740484e1a2354402b29d633869c7b883b8edc8cf9bf093ed984861e1b"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
