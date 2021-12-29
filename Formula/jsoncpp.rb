80cd1e15549de35c288b74df4a1951e3a06f7734
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-29 15:09:12 -0800

    jsoncpp 1.9.5

--- Formula/jsoncpp.rb
+++ Formula/jsoncpp.rb
@@ -1,10 +1,9 @@
 class Jsoncpp < Formula
   desc "Library for interacting with JSON"
   homepage "https://github.com/open-source-parsers/jsoncpp"
-  url "https://github.com/open-source-parsers/jsoncpp/archive/1.9.4.tar.gz"
-  sha256 "e34a628a8142643b976c7233ef381457efad79468c67cb1ae0b83a33d7493999"
+  url "https://github.com/open-source-parsers/jsoncpp/archive/1.9.5.tar.gz"
+  sha256 "f409856e5920c18d0c2fb85276e24ee607d2a09b5e7d5f0a371368903c275da2"
   license "MIT"
-  revision 1
   head "https://github.com/open-source-parsers/jsoncpp.git", branch: "master"
 
   livecheck do
@@ -25,11 +24,10 @@ class Jsoncpp < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "python@3.9" => :build
 
   def install
     mkdir "build" do
-      system "meson", *std_meson_args, "-Dpython=#{Formula["python@3.9"].opt_bin}/python3", ".."
+      system "meson", *std_meson_args, ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
     end
