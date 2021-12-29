7abde515fa3732c5a5e076dfef81fdc67339bca9
Rui Chen <rui@chenrui.dev>
2021-12-28 21:21:40 -0500

    openfpgaloader 0.7.0
    Closes #92148.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -1,8 +1,8 @@
 class Openfpgaloader < Formula
   desc "Universal utility for programming FPGA"
   homepage "https://github.com/trabucayre/openFPGALoader"
-  url "https://github.com/trabucayre/openFPGALoader/archive/v0.6.1.tar.gz"
-  sha256 "a862a209d696becff915a77512e6a8c22f92d73480a45cc12273d9ad1db60d23"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.7.0.tar.gz"
+  sha256 "1731e54eabb49c03f58a9ec5c3fea8d5d9123d68268a3e14301bb42604f273a8"
   license "Apache-2.0"
   head "https://github.com/trabucayre/openFPGALoader.git", branch: "master"
 
