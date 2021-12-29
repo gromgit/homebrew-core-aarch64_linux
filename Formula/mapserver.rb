c5d646953f5bf51075f07d770af63865b3d71c96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-29 11:06:48 +0000

    mapserver: update 7.6.4_1 bottle.

--- Formula/mapserver.rb
+++ Formula/mapserver.rb
@@ -12,10 +12,11 @@ class Mapserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0f54be3a484bb73ca70be85054ec0a7dab004bec30a9f60b8f12eb2f0181f1a8"
-    sha256 cellar: :any, big_sur:       "613314de0ebaeb0df201178f4ee23d36fea4f9302a5ca20d875b356004220cd9"
-    sha256 cellar: :any, catalina:      "3f434172773dc3312a3787f5018e7a61849534f036082c45ad285dd2a8f4280d"
-    sha256 cellar: :any, mojave:        "aa2236e1cc9e14a965996188bc9c1988f6327266ef62891a70c8f0f6588c38a1"
+    sha256 cellar: :any, arm64_monterey: "7e62c905331c71a62ec7cfa886e86aa49f59313a4347ada4b4fd8f99f07129f3"
+    sha256 cellar: :any, arm64_big_sur:  "6fe918f74417fe3d6290472c20d6d3fea2d15fd22ecb881db07bb65b6e362f28"
+    sha256 cellar: :any, monterey:       "957dc74d6938859b25e88d557db1431b8da4fe1a3aa9bb53295d2226aac3aeb0"
+    sha256 cellar: :any, big_sur:        "f6ebf38e5f1741223655e4eb57e50d79d779f28ed6c87aed5d9f707eb3e7348a"
+    sha256 cellar: :any, catalina:       "b2c70beabc7fffe4b00cfdb7ede9ca389494117cbf4f6af85035f450dae3b47c"
   end
 
   depends_on "cmake" => :build
