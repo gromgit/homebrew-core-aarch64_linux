26b1b3810b6ece88faa6cc109aef83b66cff2b2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-29 21:05:29 +0000

    pipx: update 0.17.0 bottle.

--- Formula/pipx.rb
+++ Formula/pipx.rb
@@ -9,12 +9,12 @@ class Pipx < Formula
   head "https://github.com/pypa/pipx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e504760a3c876cdc455205c9bf0d8ca320a28292081979be9127fde74ebec30"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4b24952ec2135dfb158226e3b5448467826ac9910594072ad27074d38dbe4de8"
-    sha256 cellar: :any_skip_relocation, monterey:       "2df03b859fbb038f4b5debafcfb5fa818547b7400c934df786f86a123da64e4f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "48805ee0a153d05f9f52b1ca8ef544d4c2214720e08e239f4c0523c956585510"
-    sha256 cellar: :any_skip_relocation, catalina:       "9c585d0f1796fb58a988c61fc9622d083a539a1ae56e1cdb3c183cf5510f9d53"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "60c81224e5adbdec4f7ff2c5e681c510ec87b7a34a9b021301250a98294924bd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c7611f705eae9262c0a9f9bbef89646cd602f375afad419d3d5f207092aeb87e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22661c01c4975aa00370da996f9adbc4bec54db78e73798018122babbb01d908"
+    sha256 cellar: :any_skip_relocation, monterey:       "22471f6242b4213ea58a198920dc433434df6daa80094c162c7cefc7352b058a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "30d70c14bfa1554c087c9260b053ba8f8d1741c5b249f6e17586de4a8715306d"
+    sha256 cellar: :any_skip_relocation, catalina:       "00d93cbabb973e82139659fc6842020416f3d5b3d50824934a6879d9c536e785"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a595071f2e202d5f1f4fbc25e4d32242098ec9a76cb84d38ed30a46575b87533"
   end
 
   depends_on "python@3.10"
