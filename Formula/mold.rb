c7462131550aec2c7a7e9ecdc651325680662567
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-12-30 08:45:11 +0000

    mold: add 1.0.0 bottle.

--- Formula/mold.rb
+++ Formula/mold.rb
@@ -6,6 +6,15 @@ class Mold < Formula
   license "AGPL-3.0-only"
   head "https://github.com/rui314/mold.git", branch: "main"
 
+  bottle do
+    sha256 arm64_monterey: "34632011c7a7ce08462a21076cf08692f38ca87a8d908de5df6c26370415a52e"
+    sha256 arm64_big_sur:  "95b8feed7e3eb148ee569fe77f3fba0febd66922fc1ec06b5b6ac3681e47fba1"
+    sha256 monterey:       "cf6cfb16a9c7fbda0febf286791f11d707485ddde06b8895bbb9be9313e8aceb"
+    sha256 big_sur:        "378385004a302a55daab4d6b0736d8ec494c53840fd015d8cb2e1ecf43aa0cf1"
+    sha256 catalina:       "81ea214352cb75b194e154dc3b7503bb8dc3e527321bd5cf79405cb7831ade1d"
+    sha256 x86_64_linux:   "c31fc419f53fe542d4dd32ba60c9bbc42728e409cd815d6ff88010b7090acb7f"
+  end
+
   depends_on "tbb"
   depends_on "xxhash"
   uses_from_macos "zlib"
