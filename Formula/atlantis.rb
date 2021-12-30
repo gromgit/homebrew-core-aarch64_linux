718f7b90b29b79de516242536da07cd940bc5b9b
Rui Chen <rui@chenrui.dev>
2021-12-30 11:38:18 -0500

    atlantis 0.18.0
    Closes #92256.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -1,8 +1,8 @@
 class Atlantis < Formula
   desc "Terraform Pull Request Automation tool"
   homepage "https://www.runatlantis.io/"
-  url "https://github.com/runatlantis/atlantis/archive/v0.17.6.tar.gz"
-  sha256 "79124d4df5d2d1bfa7ae8367cdb413fd752b6521812154dfafb74d7cab02b73a"
+  url "https://github.com/runatlantis/atlantis/archive/v0.18.0.tar.gz"
+  sha256 "5403c6518e4c2c9d871bc44979a21d5e8308c48bfd83ea9ff873dc773e87401d"
   license "Apache-2.0"
   head "https://github.com/runatlantis/atlantis.git", branch: "master"
 
