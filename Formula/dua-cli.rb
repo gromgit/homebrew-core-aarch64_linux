112e983a4a19f1bb6aed2ab29667247285739e72
Rui Chen <rui@chenrui.dev>
2021-12-29 23:45:21 -0500

    dua-cli 2.15.0
    Closes #92223.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dua-cli.rb
+++ Formula/dua-cli.rb
@@ -1,8 +1,8 @@
 class DuaCli < Formula
   desc "View disk space usage and delete unwanted data, fast"
   homepage "https://lib.rs/crates/dua-cli"
-  url "https://github.com/Byron/dua-cli/archive/refs/tags/v2.14.11.tar.gz"
-  sha256 "31c95fc4e9e034f9ba892397dc3d3844635c6b03852983fcf3b0cc326b751c83"
+  url "https://github.com/Byron/dua-cli/archive/refs/tags/v2.15.0.tar.gz"
+  sha256 "f90ff47d6fd92528cea14c82f5744861675b3bb69f322542a4442ddea4dc42ad"
   license "MIT"
 
   bottle do
