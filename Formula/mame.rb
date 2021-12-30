7af2a6db50518fbcf9e14e939ce8c9dcccd9904e
Rui Chen <rui@chenrui.dev>
2021-12-30 00:10:43 -0500

    mame 0.239
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -1,9 +1,9 @@
 class Mame < Formula
   desc "Multiple Arcade Machine Emulator"
   homepage "https://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0238.tar.gz"
-  version "0.238"
-  sha256 "13aa880b097832acf6cda43629f9253bcc8a340748db071028903358ffbe7faf"
+  url "https://github.com/mamedev/mame/archive/mame0239.tar.gz"
+  version "0.239"
+  sha256 "120b4d679283ab2c7102b40fefc5165bfd9b8242fbbd5f9977d381cb65051d87"
   license "GPL-2.0-or-later"
   head "https://github.com/mamedev/mame.git", branch: "master"
 
