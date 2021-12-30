54cdbd9ea1da303c5d7468ff941608ce2f34f862
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-12-30 09:26:37 +0000

    libgrape-lite: add 0.1.0 bottle.

--- Formula/libgrape-lite.rb
+++ Formula/libgrape-lite.rb
@@ -5,6 +5,15 @@ class LibgrapeLite < Formula
   sha256 "aafc8e5e9b122c4bde4370ee3f2ab8afb043f2a1547cacc062991ad420e3b9a4"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "a522e2d270893e0abb9a24679444c7b1cea1fb09c19679ee28c908778b00d09f"
+    sha256 cellar: :any,                 arm64_big_sur:  "bf96f8bf63e8dbb34cdf983c5281de60f5f62a6f356849194c357da99a5b2462"
+    sha256 cellar: :any,                 monterey:       "b3f83fd0b36714ad82779de087fbeaeb42d3458def5da07f540f8f9e753e41c2"
+    sha256 cellar: :any,                 big_sur:        "a5a2e84797108299fa6ebfc43ec6c9781abb3685a54025934f9e60a0a0721acd"
+    sha256 cellar: :any,                 catalina:       "a8404a094ee40d02827c7f7bfa64409f2ede29ced343a3588b9c6f53df8c77f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25c62461eb2b5aa859f60f904b0fbd830a71b02a9edf349988427793ff7d88b8"
+  end
+
   depends_on "cmake" => :build
 
   depends_on "glog"
