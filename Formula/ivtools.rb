c5503df7913ddaecba7ccf564d6812839b0b37ea
Scott Johnston <scojohnston@equinix.com>
2021-12-31 11:29:10 -0800

    ivtools 2.0.11d (new formula)
    ivtools 2.0.11
    
    Closes #92320.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/ivtools.rb
@@ -0,0 +1,24 @@
+class Ivtools < Formula
+  desc "X11 vector graphic servers"
+  homepage "https://github.com/vectaport/ivtools"
+  url "https://github.com/vectaport/ivtools/archive/refs/tags/ivtools-2.0.11d.tar.gz"
+  sha256 "8c6fe536dff923f7819b4210a706f0abe721e13db8a844395048ded484fb2437"
+  license "MIT"
+
+  depends_on "ace"
+  depends_on "libx11"
+  depends_on "libxext"
+
+  def install
+    cp "Makefile.orig", "Makefile"
+    ace = Formula["ace"]
+    args = %W[--mandir=#{man} --with-ace=#{ace.opt_include} --with-ace-libs=#{ace.opt_lib}]
+    system "./configure", *std_configure_args, *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/comterp", "exit(0)"
+  end
+end
