53810cdc7b49779415eef116b51cdd34c72e80ab
xxyzz <gitpull@protonmail.com>
2021-12-31 09:46:53 +0800

    remind 03.03.11
    Closes #92280.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -1,8 +1,8 @@
 class Remind < Formula
   desc "Sophisticated calendar and alarm"
   homepage "https://dianne.skoll.ca/projects/remind/"
-  url "https://dianne.skoll.ca/projects/remind/download/remind-03.03.09.tar.gz"
-  sha256 "c9087a8c691136442f3e882e46677ad36e69084b2f3bbc3c5b760d3b6bf3b6f3"
+  url "https://dianne.skoll.ca/projects/remind/download/remind-03.03.11.tar.gz"
+  sha256 "08267b08435e23cccd520b4161eb0659ba9fe77382afd8414cdd626de801e25a"
   license "GPL-2.0-only"
   head "https://git.skoll.ca/Skollsoft-Public/Remind.git", branch: "master"
 
