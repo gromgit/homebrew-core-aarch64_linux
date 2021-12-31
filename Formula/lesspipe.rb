773ffa9079563a72fcda76a217d7d68e1c52b04b
Rui Chen <rui@chenrui.dev>
2021-12-31 12:01:18 -0500

    lesspipe 2.00
    * lesspipe 2.00
    * update test and remove `LESS_ADVANCED_PREPROCESSOR`
      > - tarcolor enhanced and renamed to archive_color, archive listings colorizations
    
      > - incompatible change: LESS_ADVANCED_PREPROCESSOR no longer honored
    
    Closes #92307.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -1,8 +1,8 @@
 class Lesspipe < Formula
   desc "Input filter for the pager less"
   homepage "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html"
-  url "https://github.com/wofr06/lesspipe/archive/1.91.tar.gz"
-  sha256 "6192e7e451c5db26841c6dc3cbcc23c1ef396c1cc4588288effa699dbc665cdb"
+  url "https://github.com/wofr06/lesspipe/archive/v2.00.tar.gz"
+  sha256 "7b64ba4dfe80ef49bef30329ce1f98d70df79c5a62616e24a414f8f6a98dd9a4"
   license "GPL-2.0-only"
 
   bottle do
@@ -23,7 +23,7 @@ class Lesspipe < Formula
   def caveats
     <<~EOS
       Append the following to your #{shell_profile}:
-      export LESSOPEN="|#{HOMEBREW_PREFIX}/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
+      export LESSOPEN="|#{HOMEBREW_PREFIX}/bin/lesspipe.sh %s"
     EOS
   end
 
@@ -33,6 +33,6 @@ class Lesspipe < Formula
     system "tar", "-cvzf", "homebrew.tar.gz", "file1.txt", "file2.txt"
 
     assert_predicate testpath/"homebrew.tar.gz", :exist?
-    assert_match "file2.txt", pipe_output(bin/"tarcolor", shell_output("tar -tvzf homebrew.tar.gz"))
+    assert_match "file2.txt", pipe_output(bin/"archive_color", shell_output("tar -tvzf homebrew.tar.gz"))
   end
 end
