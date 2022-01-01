341c679a839e26650dd4dd7597d771b47ea41e8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-01 11:08:13 +0000

    tealdeer: update 1.5.0 bottle.

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -6,14 +6,12 @@ class Tealdeer < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a88d1a7e6cbbd536fc3fa7f4746ddc3191664943207941f2163eaabc34525a44"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4007bbd16184eea9bb652cfcec1b7456551400f85de61cf080a98046cf79c5dd"
-    sha256 cellar: :any_skip_relocation, monterey:       "4e85ccac7ae235ec3be341399f1a3eb6ff7e37b61652d87d85b111b6f0465f17"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5f323f75b7d481e86187ac45f9931ad114e1a9d4e7035f04a368d690de77e9e0"
-    sha256 cellar: :any_skip_relocation, catalina:       "db4a2fc7dceef4f4f914aaf7b655741e960664b46c61ab2c163f015a74949533"
-    sha256 cellar: :any_skip_relocation, mojave:         "9ad3e3ea878b05fd2764cc8e534888b8bf810f88cc4d986de158ed6b33633b42"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "80a29641c9b29a3cda69adf5afd2c36b27f84fb8f89a555dbf2a676dddf03b70"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a01f8be8f2e5ba17e70706875d0322c8ddfed2c3b3e7847c642d49f71629f94"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0d1d7ba657937c479ae47d27942e66d1ff15cc3f957596d223e28b26361d9021"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d64f393398aea8171ec136a96baeef735adf458c2b53a41410487f94089da0d"
+    sha256 cellar: :any_skip_relocation, monterey:       "0a7f00acd485f3cfd6a99664b39fcf82aee38d8f2ffda8fc48b77f060695e743"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c97e8782cafb0151755c361cf99854a493bda6873fab6fffe46c6ed0c9931593"
+    sha256 cellar: :any_skip_relocation, catalina:       "9abfdc915c51c97cde029a6029adb6c8bcf5d848da738be785316f998449d5a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "130dc8bc4bc2af2d56e9c06766ba71689990b941123448d4de2b84bba8d96fbd"
   end
 
   depends_on "rust" => :build
