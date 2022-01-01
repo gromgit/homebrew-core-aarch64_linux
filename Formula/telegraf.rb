8683a04e083d7874b10527b58122bef2321e1446
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-01 08:07:34 +0000

    telegraf: update 1.21.1 bottle.

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -12,12 +12,12 @@ class Telegraf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd2af97294f4be3520c577cabdb7d9aad0aec2062b5004b0c7e62ceb51363876"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f92247f0ac7e97957e4ab7df252fede7f878682e4e9c36b2503752614b3e3b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "b3b63ca9e351cd897bf50999a3958f8744f9d9851055ba4b2d2c6d65fdee6347"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bab8e8a7e0a31e1252c8dd511b98cf1ed471284d52f747153fb146b30f2466c4"
-    sha256 cellar: :any_skip_relocation, catalina:       "eeca80233a9e2f3c29e752dc99239ceba9ca97f474fb09251b7fdd78381db215"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1ff5cfbf6a28be8cb3640e8961d6c3285a4d9a49a32a1c2ac8f52c29a450ff49"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3037aad7b419b51fb01d17b7c5e824427327cd4c25d5edcb09816ae2dba4748d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9bb497017d671fb3eadbb758bcd2a6f5ffd17fd82fe35f220821f93bb6cf5103"
+    sha256 cellar: :any_skip_relocation, monterey:       "a5f6be6d27e02e79de7d82f9b0db4de037362c445d5776b5ea130dbcd1b16a09"
+    sha256 cellar: :any_skip_relocation, big_sur:        "40ec51cdf19ab6677ef98f71f60bf40fffbb82d7e65df4f1222d05dd51b084e7"
+    sha256 cellar: :any_skip_relocation, catalina:       "4a63d307a97f67e076e2f34226e8e4f1c02905c758344279afa81727b82aaaee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "de271ab9b4c74d5ff826a85613fc76f4af0a93882f27a7278cd8d6cd012fa5b7"
   end
 
   depends_on "go" => :build
