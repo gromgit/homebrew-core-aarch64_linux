d6a997716cd5ddb8414decde579e68f5e1e88a50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-01 15:06:38 +0000

    dosbox-x: update 0.83.21 bottle.

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -13,11 +13,11 @@ class DosboxX < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "f8151e55a8e0189680746b1465b7a88b07a0b25e7cf7a0aedf0385702c6e2692"
-    sha256 cellar: :any, arm64_big_sur:  "de2cd9165ca930d79f407005c778a54ac8a879661cb6828c3b6ac6b65735458b"
-    sha256 cellar: :any, monterey:       "e3e12f937bf5495b440d761964deb3feadd5e7d823647582e477f9372b6ee87a"
-    sha256 cellar: :any, big_sur:        "7c031b5ed275bf3c6eba75e4d42ed5007878cba684cdd7405d0d1599797d79cc"
-    sha256 cellar: :any, catalina:       "7b2166e2420e5f709824221db141bfaaf946fb8dba21f721351ec0605ef4a482"
+    sha256 cellar: :any, arm64_monterey: "6390beb54ce375ecf751c727228481ac224a9d31b3fc570852a58db7f812882f"
+    sha256 cellar: :any, arm64_big_sur:  "9f5cd2f091bc2f7681b83dabb978da766b52757e3cef9319768cefc875088138"
+    sha256 cellar: :any, monterey:       "2cfc76dc15307cdd763d52019f09cd2161928ce0caa138cfc861ab649e7f98db"
+    sha256 cellar: :any, big_sur:        "f26a5eb6e568dbe892daa04de8c2bd8bf0e944051242b0f897f99c34c43ee297"
+    sha256 cellar: :any, catalina:       "ae0758194ca4b5b71d4907a36701545ab08cc7d4691153f735ef69328f3db2cd"
   end
 
   depends_on "autoconf" => :build
