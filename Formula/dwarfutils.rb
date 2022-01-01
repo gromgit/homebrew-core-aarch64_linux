9585d2825ea9971270a1b5c1a275c32623b64f0d
Rui Chen <rui@chenrui.dev>
2022-01-01 14:39:54 -0500

    dwarfutils 0.3.2
    update livecheck
    
    Closes #92370.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dwarfutils.rb
+++ Formula/dwarfutils.rb
@@ -1,14 +1,14 @@
 class Dwarfutils < Formula
   desc "Dump and produce DWARF debug information in ELF objects"
   homepage "https://www.prevanders.net/dwarf.html"
-  url "https://www.prevanders.net/libdwarf-0.3.1.tar.xz"
-  sha256 "f2592e56aa5d8f39869367edc80bfe6529c04a5223219f628b6249b5bdd5f721"
+  url "https://github.com/davea42/libdwarf-code/releases/download/libdwarf-0.3.2/libdwarf-0.3.2.tar.xz"
+  sha256 "a5b150182a1ce18e16b2f4225d3902b3f2b30181d2678f29cbd90b5ee7b067d1"
   license all_of: ["BSD-2-Clause", "LGPL-2.1-or-later", "GPL-2.0-or-later"]
   version_scheme 1
 
   livecheck do
-    url :homepage
-    regex(%r{href=(?:["']?|.*?/)libdwarf[._-]v?(\d+(?:\.\d+)+)\.t}i)
+    url :stable
+    regex(/^libdwarf[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
   bottle do
