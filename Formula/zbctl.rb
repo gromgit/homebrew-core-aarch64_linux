900ec50e587a430aa6c308f9e289f65f0db3f88a
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2022-01-03 01:44:19 +0700

    zbctl 1.2.9 (new formula)
    Closes #92409.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/zbctl.rb
@@ -0,0 +1,38 @@
+class Zbctl < Formula
+  desc "Zeebe CLI client"
+  homepage "https://docs.camunda.io/docs/apis-clients/cli-client/index/"
+  url "https://github.com/camunda-cloud/zeebe.git",
+      tag:      "1.2.9",
+      revision: "b55242898afe0dd93b87fa0b464f7c4d9a10bbad"
+  license "Apache-2.0"
+  head "https://github.com/camunda-cloud/zeebe.git", branch: "develop"
+
+  depends_on "go" => :build
+
+  def install
+    commit = Utils.git_short_head
+    chdir "clients/go/cmd/zbctl" do
+      project = "github.com/camunda-cloud/zeebe/clients/go/cmd/zbctl/internal/commands"
+      ldflags = %W[
+        -w
+        -X #{project}.Version=#{version}
+        -X #{project}.Commit=#{commit}
+      ]
+      system "go", "build", "-tags", "netgo", *std_go_args(ldflags: ldflags)
+    end
+  end
+
+  test do
+    # Check status for a nonexistent cluster
+    status_error_message =
+      "Error: rpc error: code =" \
+      " Unavailable desc = connection error:" \
+      " desc = \"transport: Error while dialing dial tcp 127.0.0.1:26500: connect: connection refused\""
+    output = shell_output("#{bin}/zbctl status 2>&1", 1)
+    assert_match status_error_message, output
+    # Check version
+    commit = stable.specs[:revision][0..7]
+    expected_version = "zbctl #{version} (commit: #{commit})"
+    assert_match expected_version, shell_output("#{bin}/zbctl version")
+  end
+end
