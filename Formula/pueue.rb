6f77ce0d52581a02b12513e68be732ee9834b141
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-02 21:05:27 +0000

    pueue: update 1.0.5 bottle.

--- Formula/pueue.rb
+++ Formula/pueue.rb
@@ -7,12 +7,12 @@ class Pueue < Formula
   head "https://github.com/Nukesor/pueue.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d2a92df8647f7ed6882057cb2592aad3d37614991bd78a007dd5390946c48284"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f9b5b7101590b35413c467ac6b03ed86b256cdbc2832bb2c1c6267c4f168e07b"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f3f898387ad0cccb2d6a2471707df0f3b53b3a8b13b3773b8d26b1904210159"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6d8e111554e48180b9b0ee4d385079fbb6618c9e892441f7056bb7795cff89bd"
-    sha256 cellar: :any_skip_relocation, catalina:       "bed9af0542c44eebb6ab281a3e8022f7e0b5fc6d6fc47006ef8c83db66f47f2d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c5e77735cd4e91f30a2c1836edf6494237e2edd189698736a6d5702554ccfcd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a09fd270a8a53b1655446af42cdc48e3c4c857178b61666aa6f91498837db96b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a739e0a7f078449351c03d43db141ef4467997dd3d4f88abe0097e4cce30f775"
+    sha256 cellar: :any_skip_relocation, monterey:       "51b0cbfba941c44380d5f77907083116564bc51d6e18bff3dfa8fe29f01f9978"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f173437b8a8445b1af7497f34006ce5afcfa054420b8a9a6871b355db84cdfa5"
+    sha256 cellar: :any_skip_relocation, catalina:       "98eb467afb7a87f10b3902287e7d0feba316505b8e9479c99df9a4974cdad975"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7464ddc565805084113038adc09a0b7def5708264641d5f4c76104c91b2d3e93"
   end
 
   depends_on "rust" => :build
