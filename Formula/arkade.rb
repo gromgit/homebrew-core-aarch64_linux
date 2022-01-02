78bc14f9c83a6d2327287f76ae0682e31f0688e1
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-01-02 16:54:38 +0000

    arkade: add 0.8.11 bottle.

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -6,6 +6,15 @@ class Arkade < Formula
       revision: "f6c134889f89c4bfe1b06f0f97037f16caf2b6a3"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "46c9669f13a30d7ac0dc1c634e70884c396b34fb9ebdeae18578d250957d5e8b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "578bcb88db0bf121146a3e642eb9996559ad59dd61136e077d43f6bf0cb1ca56"
+    sha256 cellar: :any_skip_relocation, monterey:       "0030215064e7fab3faa5692b938977daa1e0aa21dc8acadff16dfc102d47ae54"
+    sha256 cellar: :any_skip_relocation, big_sur:        "35490f18b2574191a00088c4c771ece3335690c9b87a707c2996db926ecf5dbd"
+    sha256 cellar: :any_skip_relocation, catalina:       "b93b287c2bbfe3601842b3cc7c460373211b61706f12776e6b9785687b969b51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e034422b6c7cfee895bd018791877c770429c0ec31fe92543cd928344253e438"
+  end
+
   depends_on "go" => :build
 
   def install
