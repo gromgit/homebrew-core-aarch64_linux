2c43a8eb578b6fb9d84b9ce3cfe1dfbe2aec8616
Max Eisner <4730112+max-ae@users.noreply.github.com>
2022-01-02 13:13:22 +0100

    faas-cli: install completions
    Closes #92402.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/faas-cli.rb
+++ Formula/faas-cli.rb
@@ -34,6 +34,11 @@ class FaasCli < Formula
     ]
     system "go", "build", "-ldflags", ldflags.join(" "), "-a", "-installsuffix", "cgo", "-o", bin/"faas-cli"
     bin.install_symlink "faas-cli" => "faas"
+
+    (bash_completion/"faas-cli").write Utils.safe_popen_read("#{bin}/faas-cli", "completion", "--shell", "bash")
+    (zsh_completion/"_faas-cli").write Utils.safe_popen_read("#{bin}/faas-cli", "completion", "--shell", "zsh")
+    # make zsh completions also work for `faas` symlink
+    inreplace zsh_completion/"_faas-cli", "#compdef faas-cli", "#compdef faas-cli\ncompdef faas=faas-cli"
   end
 
   test do
