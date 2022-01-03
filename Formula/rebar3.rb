f4099086c287f146eacba7340a55a84216222aea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-03 04:06:00 +0000

    rebar3: update 3.18.0 bottle.

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -11,13 +11,12 @@ class Rebar3 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f672cc31fcc77f3bf50274b54c4abcc8d1c061c01f5cf32937d2e7470e81954e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0501fc018a924c76dcd7248f12f7002296241874162db6ecf3cda0d6516e3ac3"
-    sha256 cellar: :any_skip_relocation, monterey:       "aa213996443e7164cd29fbc131c000a1cc7a75ea6e41693ba2db8a62cef86349"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8b2581dc2365f4dff9d2533e1d8e18fdeab6282740caf47fc37d3c97365f90c9"
-    sha256 cellar: :any_skip_relocation, catalina:       "7ee16a66ce6d611df47abd8118ed08651a237b15f687824ca5711cb2bb734684"
-    sha256 cellar: :any_skip_relocation, mojave:         "311c51419024d8440c99de8c6711964ad329165d4c2bfea02a566549fcc39272"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0431da6893ab1734b1a69f530b3b9e4e52611785a9c9fab8f993190fecb5117"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "44500038a7b9cd871f2ddba57cfa67d147abb833c6868cf72590a19b544fabee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70362076a1b5523e0c4e789840c30cee30c76b1399c957aba61ce47261594331"
+    sha256 cellar: :any_skip_relocation, monterey:       "c8547511cfe4dfe1b5753d81218ad149d597f01961a5f6e41d1c989500897481"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b76334a1871767352db5e5e641c4cc6c02d0d291c6a16012cf7f2f999674f362"
+    sha256 cellar: :any_skip_relocation, catalina:       "49f8019c79ba3b07f9a91a2aa66c9767f724ffd49a418dce80e9296628bf2202"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39e6def810bc61aaee09615d73e5692bfeb723070e4e6e28902b1ba822e97607"
   end
 
   depends_on "erlang"
