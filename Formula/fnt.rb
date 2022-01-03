6ad2eefe53343a41ade6aec951cf333e2aff3e17
Nanda H Krishna <me@nandahkrishna.com>
2022-01-03 05:50:40 +0000

    fnt: add 1.4.1 bottle.

--- Formula/fnt.rb
+++ Formula/fnt.rb
@@ -5,6 +5,15 @@ class Fnt < Formula
   sha256 "263edd4e3ebd71bb5c63e5f063bfbed6a711b5786e6f6174c6ee586e530c1727"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1d674614b1b87af4d7af534169e5e7f6146f605e78d54ccada7b32ae2afabd5a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d674614b1b87af4d7af534169e5e7f6146f605e78d54ccada7b32ae2afabd5a"
+    sha256 cellar: :any_skip_relocation, monterey:       "504e678ddcb9adb26de967a20e6e5691d68a3a8024de98b81c6c66123c80b9f1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "504e678ddcb9adb26de967a20e6e5691d68a3a8024de98b81c6c66123c80b9f1"
+    sha256 cellar: :any_skip_relocation, catalina:       "504e678ddcb9adb26de967a20e6e5691d68a3a8024de98b81c6c66123c80b9f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d674614b1b87af4d7af534169e5e7f6146f605e78d54ccada7b32ae2afabd5a"
+  end
+
   depends_on "chafa"
   depends_on "lcdf-typetools"
 
