fe6bf26a9461cf8f2928c00f2b46597de75f72f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-03 14:06:10 +0000

    dependency-check: update 6.5.2 bottle.

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -11,7 +11,7 @@ class DependencyCheck < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e085c08a57a1a0065819ca2c5db9f71e9c774e13a5a981b612a10c0dea220e08"
+    sha256 cellar: :any_skip_relocation, all: "18e257c7e03a8e43b9f07dd9659ca71b904c9edd97f0f0ef3790f5ba94f63106"
   end
 
   depends_on "openjdk"
