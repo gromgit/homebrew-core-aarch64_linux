9b5b61d9506396dabfee0ab3a3fea960b33c9520
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-04 11:07:04 +0000

    teku: update 22.1.0 bottle.

--- Formula/teku.rb
+++ Formula/teku.rb
@@ -7,12 +7,12 @@ class Teku < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "455607ee40deb115cad841ada71289c1adb0f97fb93cc781196152ab895fb56d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "65c5bf41bdc3fe5244cfa3fe13a688f752d1dee60b7d5a2793d778a1e534de97"
-    sha256 cellar: :any_skip_relocation, monterey:       "dff4186bcae95e50d6102803a30895e18c04ad904c1013baf12796a47cf1db9a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "af14865fda7f4c5108c417ca53e76eadb4831be0d748d8cf87aeecde47f67065"
-    sha256 cellar: :any_skip_relocation, catalina:       "41790080ac204a0e7775e41034b1aa418cbc8d414f366aec47579f11a23b5208"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1849f777bd999a03665900680f0b1c639ef921a3faaddaea4bc12eda00e4285a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "97c4812694ee813a13e31c262f0a96a81a487e23d86065b6aea8096b2868f346"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "17b9b1daaf04d9b9eb5a58af1a6491370604d85d2be8775e80d61622699b6166"
+    sha256 cellar: :any_skip_relocation, monterey:       "6642ee3a33ee28091e5383a12ccd61f7ae99126ec79bf9c452ede5c7ae79c4ce"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5df91c28062d0f89de22e1c83fa667c9b9b936cefe74e17df28da1fac7f60753"
+    sha256 cellar: :any_skip_relocation, catalina:       "84f38e89404ec53d5219e4c96138a3c93448a842f6fce5c7a8fd50a6fe766ee0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "71a63323115c7087dab604abb812ca1a58e9948a68c22f5319778adf7726ed38"
   end
 
   depends_on "gradle" => :build
