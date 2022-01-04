35f9e73526930d84593de76c2ee1d153c91ab49d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-04 05:06:26 +0000

    minio: update 20220103182258 bottle.

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -17,12 +17,12 @@ class Minio < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "44b991864c8f8a933fb83ce854cd91f41a1e450fecb0a94a7adb14e0fad34550"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa11020066acff665df02c563f31d72d24daecf01a8c04799bc3265fd7a59181"
-    sha256 cellar: :any_skip_relocation, monterey:       "97c3fea268d4570d15d95707cd4a173f50258b59c799e1479d06b6a41b081d20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c25706d0aa6921573c0ff8eb089f2eccaa708a8a12d5d321ab88185394921d60"
-    sha256 cellar: :any_skip_relocation, catalina:       "0009ca071792e18cf62a9a661f5b13264797dd143cf694e4dd39532dcb6667d3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c1acc099f59f5637123647bd1021cb1d0cf8ec9178cfb06e771c3ea564581b3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee33c45121b9d2da2bee7b8ba2c9eb8b65d006c45f90d347a30824618ed5d2b4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ed1f0da2039809e8df830b1689da1a6e74b920651df88e774187eb3d110ba05f"
+    sha256 cellar: :any_skip_relocation, monterey:       "62c4c9acadeb1da92e695512f7d5e743d64bf1e32899f8293d5e58927069bdca"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c88e1ede439f494f3ef565f0290560bf544b0c44c8afeaf2247fbb344392addf"
+    sha256 cellar: :any_skip_relocation, catalina:       "f9d1986971adc6618c823aa8faecbb54b1ade4628673797447df2831fc1cbb01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bedc2a5ec6382c5ba1d055fe6017ad78c1eb28eb0d2aec89c2dc0e770a068e97"
   end
 
   depends_on "go" => :build
