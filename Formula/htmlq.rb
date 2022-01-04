a3426a9daa0e1f424970f952a552b5b4a404ad51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-04 04:05:59 +0000

    htmlq: update 0.4.0 bottle.

--- Formula/htmlq.rb
+++ Formula/htmlq.rb
@@ -7,13 +7,12 @@ class Htmlq < Formula
   head "https://github.com/mgdm/htmlq.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "50250a414769450f0c7a60e7c43a5c0624cec7bdc830363fdf17ac7ba129c4f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "155a5d20ec52baa51c8d5f70f3224ff7707c860a49bc51f020b2afdf8ea1ad2c"
-    sha256 cellar: :any_skip_relocation, monterey:       "32c487944df2e353c7723141f8afccea2c1d9c5fb9cba2da07e9719afdca0997"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8ec8b00331597e951f4f449394d26549d47c1090d2a1562047268c9ed0397580"
-    sha256 cellar: :any_skip_relocation, catalina:       "9be3c8684ee2f1666837195233bfccd29975555f9c7ae36ca1d3870fe0a5ccfc"
-    sha256 cellar: :any_skip_relocation, mojave:         "568439b3497a88d0d2fc3b6d7b0e3330c2b93e6a0e0c9df8de17768321a7e9df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee7e22902b6f9bfe4f9403430dfd16fc242d87ee99e59c99358dcbe3a34a1be2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f975cfa431fbc03c5bc6eab5372313d95b38ce7a19e558a80021d2ac17c15fb4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa4eae2da351814a51f9332500c83b3c18360a2ffabbaaa9126fd7bae21e7421"
+    sha256 cellar: :any_skip_relocation, monterey:       "317689afb09ecbb2d08be598aafd16fcdf9b674868aadab1bff1128d4829ce94"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b136ace66829daaa18c37c91d7bdae098b71c39aedf4cb86d436efd6108d8209"
+    sha256 cellar: :any_skip_relocation, catalina:       "189fa5d4f2a99d4bb7b2002c89da361383a56fbac8c246b1a2fd52a00e2ca825"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "263b6af3a39eb169667349f20261e90faebf9446e350496649a9f51d57de49d3"
   end
 
   depends_on "rust" => :build
