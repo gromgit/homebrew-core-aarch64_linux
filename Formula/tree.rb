750128b0eddfdb5b8866b49a15fc19ff04b4b7a8
miccal <miccal.matthews@gmail.com>
2022-01-04 11:40:29 +0800

    tree 2.0.1
    Closes #92484.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tree.rb
+++ Formula/tree.rb
@@ -1,10 +1,9 @@
 class Tree < Formula
   desc "Display directories as trees (with optional color/HTML output)"
   homepage "http://mama.indstate.edu/users/ice/tree/"
-  url "http://mama.indstate.edu/users/ice/tree/src/tree-2.0.0.tgz"
-  sha256 "782cd73179f65cfca7f29326f1511306e49e9b11d5b861daa57e13fd7262889f"
+  url "http://mama.indstate.edu/users/ice/tree/src/tree-2.0.1.tgz"
+  sha256 "e3339c5a194cf6b4080f15ec59faa3679f02d5a793b2147912fbfcfb4cdf2239"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url "http://mama.indstate.edu/users/ice/tree/src/"
@@ -24,7 +23,7 @@ class Tree < Formula
     ENV.append "CFLAGS", "-fomit-frame-pointer"
     objs = "tree.o list.o hash.o color.o file.o filter.o info.o unix.o xml.o json.o html.o strverscmp.o"
 
-    system "make", "prefix=#{prefix}",
+    system "make", "PREFIX=#{prefix}",
                    "MANDIR=#{man}",
                    "CC=#{ENV.cc}",
                    "CFLAGS=#{ENV.cflags}",
