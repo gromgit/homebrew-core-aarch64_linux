49b862f6113928fc73cd6c391e438ac5036feefc
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-01-04 10:20:38 +0000

    fprettify: add 0.3.7 bottle.

--- Formula/fprettify.rb
+++ Formula/fprettify.rb
@@ -8,6 +8,15 @@ class Fprettify < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/pseewald/fprettify.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fdb3c1fb8f56f2d0a69cd8c64863e10bd4bcab09fae883f745fc28103e20a5ae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fdb3c1fb8f56f2d0a69cd8c64863e10bd4bcab09fae883f745fc28103e20a5ae"
+    sha256 cellar: :any_skip_relocation, monterey:       "003792c125ef0380752d9dde144930d696a8dada1a1c3edce864e2417dfae574"
+    sha256 cellar: :any_skip_relocation, big_sur:        "003792c125ef0380752d9dde144930d696a8dada1a1c3edce864e2417dfae574"
+    sha256 cellar: :any_skip_relocation, catalina:       "003792c125ef0380752d9dde144930d696a8dada1a1c3edce864e2417dfae574"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f95be72c72df4071f4e9e6819f240aea1871aa3a544f9aa4aede5f9e3abb585"
+  end
+
   depends_on "gcc" => :test
   depends_on "python@3.10"
 
