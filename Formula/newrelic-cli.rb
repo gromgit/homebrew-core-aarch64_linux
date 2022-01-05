9bcf27eeddc2eb485abf5568af298b4eac462dd9
Rui Chen <rui@chenrui.dev>
2022-01-05 18:52:30 -0500

    newrelic-cli 0.41.6
    Closes #92588.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,8 +1,8 @@
 class NewrelicCli < Formula
   desc "Command-line interface for New Relic"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.41.2.tar.gz"
-  sha256 "0dd198c27b156c65a55c175ecbf3749de237aa9ffc32752128cb00e54dbe2b95"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.41.6.tar.gz"
+  sha256 "90c1f3ebcce6ba960694105558dfba65e7c39171d698b55cacfd0666a03cdfcf"
   license "Apache-2.0"
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
@@ -22,10 +22,12 @@ class NewrelicCli < Formula
     system "make", "compile-only"
     bin.install "bin/#{OS.kernel_name.downcase}/newrelic"
 
-    output = Utils.safe_popen_read("#{bin}/newrelic", "completion", "--shell", "bash")
+    output = Utils.safe_popen_read(bin/"newrelic", "completion", "--shell", "bash")
     (bash_completion/"newrelic").write output
-    output = Utils.safe_popen_read("#{bin}/newrelic", "completion", "--shell", "zsh")
+    output = Utils.safe_popen_read(bin/"newrelic", "completion", "--shell", "zsh")
     (zsh_completion/"_newrelic").write output
+    output = Utils.safe_popen_read(bin/"newrelic", "completion", "--shell", "fish")
+    (fish_completion/"newrelic.fish").write output
   end
 
   test do
