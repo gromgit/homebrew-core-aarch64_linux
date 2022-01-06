0e8ecc1ed505e2966a563b91c78d3b481ad2564e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-06 16:07:24 +0000

    rke: update 1.3.4 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -6,12 +6,12 @@ class Rke < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9635da12c5b725c67a8e0ec582b745408bf41414d5fabb953466f495bc99a0e1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "54dd3e031cbca601ec75240785e94809d424969dca5a10412ebe4116fcb8e608"
-    sha256 cellar: :any_skip_relocation, monterey:       "034a0854c3f2e01814faec2801f7c2d131be8e242971dcfdbbe9bcd10b739a83"
-    sha256 cellar: :any_skip_relocation, big_sur:        "df8b921f46ab02fdf8f8f60501b102dd95c200e30418a3128c1c48957e5cf2f5"
-    sha256 cellar: :any_skip_relocation, catalina:       "81591c7c14336b0e7a560b27c57d4c578c5f8154ed82db3b63ea882c00eb3ff6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5846f771c774c1813c91388904e7ec89b04c3f6445654c1941fcbff6cb04b0da"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c5dce8f51367033ce3c3d672ec4a28c42fa8e4a793a8023421639278036dfd11"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7a4a02b4738598442619b9cc58e14e79fbbc96fdea1c6e14ac9a8397b6110ecc"
+    sha256 cellar: :any_skip_relocation, monterey:       "58b1c045ad3c45fc80774e0686a1eca4acefad796ea794d2780d08d7f6d1c5f1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8ed119780378957572845efa89a8e72225887ba741ea724b3d42f2da9f32be8b"
+    sha256 cellar: :any_skip_relocation, catalina:       "70d30110dc5e83b6c9debe8b0eaad27cfc91bdcbd40668b00d92d43371ed6a00"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "90abbb6233f843f453c46ab1846422ee08857a98d0d108975a7978e25ae69d3a"
   end
 
   depends_on "go" => :build
