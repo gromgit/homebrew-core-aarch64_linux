5c325c7cec1e5869a9ffbef8e9e79dce9b45ad61
Porkepix <porkepix@gmail.com>
2022-01-06 12:27:37 +0100

    k3sup 0.11.3
    Closes #92630.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -2,8 +2,8 @@ class K3sup < Formula
   desc "Utility to create k3s clusters on any local or remote VM"
   homepage "https://k3sup.dev"
   url "https://github.com/alexellis/k3sup.git",
-      tag:      "0.11.1",
-      revision: "95cf0b1332c3b2b7eed4bb2d7154bec790b8ea41"
+      tag:      "0.11.3",
+      revision: "e2bb18116d3686bf53cf40fe0998af7b6c9cf8a6"
   license "MIT"
   head "https://github.com/alexellis/k3sup.git", branch: "master"
 
