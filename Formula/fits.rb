9f7e56d850e5ddd2a86923e6d865d79f26505cae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-06 17:12:06 +0000

    fits: update 1.5.1 bottle.

--- Formula/fits.rb
+++ Formula/fits.rb
@@ -6,11 +6,7 @@ class Fits < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "18ae09e9e92d45f14502dd3b7a5323be6f1b1ac19fb45fd1c367b2417d9db929"
-    sha256 cellar: :any, monterey:      "6e700d2bab8e41963162e6ac180628f4e9311ed71cae5a3034c756a17628be89"
-    sha256 cellar: :any, big_sur:       "70a94bc9728e70e82c57d726ec958880da89dd5af6c2d65ae4351e6cf7543366"
-    sha256 cellar: :any, catalina:      "70a94bc9728e70e82c57d726ec958880da89dd5af6c2d65ae4351e6cf7543366"
-    sha256 cellar: :any, mojave:        "70a94bc9728e70e82c57d726ec958880da89dd5af6c2d65ae4351e6cf7543366"
+    sha256 cellar: :any, all: "0da280076217b759472c440df96df7c866f0226af4a53f7845c19fc565e09270"
   end
 
   # Installs pre-built x86_64 binaries
