b5b027e147b389e39b19ccf19027df41db1ad69c
Alexander M. Turek <me@derrabus.de>
2022-01-06 18:17:45 +0100

    phpstan 1.3.2
    Closes #92642.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -1,8 +1,8 @@
 class Phpstan < Formula
   desc "PHP Static Analysis Tool"
   homepage "https://github.com/phpstan/phpstan"
-  url "https://github.com/phpstan/phpstan/releases/download/1.3.1/phpstan.phar"
-  sha256 "554ba609fbd07d27385780ed8c5f2e42df19ba5c73cf81e550d2ad1016b3da30"
+  url "https://github.com/phpstan/phpstan/releases/download/1.3.2/phpstan.phar"
+  sha256 "24eade601d1e4e6ab2d0c0878a928455e2ef78504734b420955b010aa2a9cadc"
   license "MIT"
 
   bottle do
