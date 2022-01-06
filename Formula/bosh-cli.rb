89bf87730b41e8f871e431c69744462b1b1e7eab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-06 02:23:53 +0000

    bosh-cli: update 6.4.11 bottle.

--- Formula/bosh-cli.rb
+++ Formula/bosh-cli.rb
@@ -7,12 +7,12 @@ class BoshCli < Formula
   head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3883139cba0611c50b6280133d3e36b763ce9b37284ca9885b66ef7429a60391"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "38951e2ea47d0a4af129464751003833e5e4436538b2d1df3390539bcbec6f98"
-    sha256 cellar: :any_skip_relocation, monterey:       "6e54299698242ba37f3391fb629d93bc478464a91aaa233382a0f989caee1296"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8cd52aa155b2d88a08bb4f3f250813ace7f9fa307af7fc32969ff6347d029102"
-    sha256 cellar: :any_skip_relocation, catalina:       "630c68ab8c3143b8f80a079548a3bdc0a76a960e5ccc573426e05b01913971fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d6c67e95443006891a046f9f2e354781248e12df38039101e6d2b2182492faf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a15feb1c5d39efb166baba609430f2c71ffbe308af801d9d9596e8c124637716"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "881cbb78cd0b7d4b3bd35c48542582e368b486d0ffae852edaefeb06e91308a2"
+    sha256 cellar: :any_skip_relocation, monterey:       "4363269bdf421133a832bdaf4dc4d819645e70ed4d12143c8f185ae4ae064225"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dc327027e1002a1d4caea376d63534f9a38328e6afa7c905606d9c2d5385e5a9"
+    sha256 cellar: :any_skip_relocation, catalina:       "be5cc77a02717042f20a89130a58d57db3755ec153bd390142d7dcd3b66107b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "baefd8172fa59418b63e39df0f41067b1ab45215cf2ad465d7564625b6f8b637"
   end
 
   depends_on "go" => :build
