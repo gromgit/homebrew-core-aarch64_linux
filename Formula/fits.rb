dbe2798f0f15b316e8cc4156dc83d4c3bdd1ea0a
Porkepix <porkepix@gmail.com>
2022-01-06 02:42:46 +0100

    fits 1.5.1
    Closes #92598.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fits.rb
+++ Formula/fits.rb
@@ -1,8 +1,8 @@
 class Fits < Formula
   desc "File Information Tool Set"
   homepage "https://projects.iq.harvard.edu/fits"
-  url "https://github.com/harvard-lts/fits/releases/download/1.5.0/fits-1.5.0.zip"
-  sha256 "1378a78892db103b3a00e45c510b58c70e19a1a401b3720ff4d64a51438bfe0b"
+  url "https://github.com/harvard-lts/fits/releases/download/1.5.1/fits-1.5.1.zip"
+  sha256 "4bf4adfedf0779221cc2f4648f5dfd3040c7a3e5daa4060c5754d73dc1964442"
   license "Apache-2.0"
 
   bottle do
