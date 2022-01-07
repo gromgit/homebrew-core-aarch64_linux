0d8f04cecddbf21b17664b9340e433828b6e9842
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-07 20:05:56 +0000

    lightgbm: update 3.3.2 bottle.

--- Formula/lightgbm.rb
+++ Formula/lightgbm.rb
@@ -7,12 +7,12 @@ class Lightgbm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "34610dd420b6e0e39f5de3575b99dc7991adfa0f8b6ebd032886b20e61596e68"
-    sha256 cellar: :any,                 arm64_big_sur:  "3c8b86c37e5233ed06de0494fd8cdba6fa9faa91af97840f0a72c95fdfb092c4"
-    sha256 cellar: :any,                 monterey:       "d0d1dbb78a2cb0f66096babb71eee9d31fd0fd4a34faaea6ea25eef88fd1f03b"
-    sha256 cellar: :any,                 big_sur:        "a4d65f031a1b2af129f8cc3741e77721fc78ea8c832e848b731b1a144e43d8ab"
-    sha256 cellar: :any,                 catalina:       "4b6a53e88126659be4d6c0a18e6d077cbbf9152d2753363749a29bf79153c4dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "083b5aa1adba57bc086d9917730672f2c4d2ab5f8b03ef55f999e1faf2596e56"
+    sha256 cellar: :any,                 arm64_monterey: "7683c555fca349d6aa5b4d7c83e4499c911b4bbf49e1293c2580d8de2dd89695"
+    sha256 cellar: :any,                 arm64_big_sur:  "d682a3c8519a8e53586e11cbe469cee7407ab3cf9c04c3e1cb3090718f2e1c37"
+    sha256 cellar: :any,                 monterey:       "cd99e43dc3c64bf0416e13546a359e8c213c4b693a8bb90626b6f2c9f3ff5623"
+    sha256 cellar: :any,                 big_sur:        "35ab7f3c1def696fa9800a98dd0a03f07046d6476f1a2ad35bf324e4907a97b6"
+    sha256 cellar: :any,                 catalina:       "29d375544346e724143a1b145b1fa4cdfcdbc743fa7637b955b769274f238049"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb2d01e4572ef9e7a4ab3e3415da0b4b7ccd3d379dd4020e4ec254e3c96faf75"
   end
 
   depends_on "cmake" => :build
