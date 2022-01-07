1fab8aa9f513e69a0802b9cfbd7aa89c58d8ef61
Porkepix <porkepix@gmail.com>
2022-01-07 17:01:29 +0100

    xray 1.5.2
    * xray 1.5.2
    * xray: bump resources
    
    Closes #92711.
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Signed-off-by: Issy Long <me@issyl0.co.uk>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -1,8 +1,8 @@
 class Xray < Formula
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://xtls.github.io/"
-  url "https://github.com/XTLS/Xray-core/archive/v1.4.5.tar.gz"
-  sha256 "54c6a687dd463b25afe8d8eb44d37e18b8177f58308207cd1d74f6cd04619854"
+  url "https://github.com/XTLS/Xray-core/archive/v1.5.2.tar.gz"
+  sha256 "b687a8fd1325bee0f6352c8dc3bfb70a7ee07cd74aacaece4e36c93cf7cda417"
   license all_of: ["MPL-2.0", "CC-BY-SA-4.0"]
   head "https://github.com/XTLS/Xray-core.git", branch: "main"
 
@@ -19,27 +19,25 @@ class Xray < Formula
   depends_on "go" => :build
 
   resource "geoip" do
-    url "https://github.com/v2fly/geoip/releases/download/202109060310/geoip.dat"
-    sha256 "ed94122961f358abede9f1954722039d5a0300b614c77cc27d92618c08b97bb8"
+    url "https://github.com/v2fly/geoip/releases/download/202201060033/geoip.dat"
+    sha256 "27f9cf6d647f018be425188a25ceb095076f6d29544bac843c2d51e0000d00a0"
   end
 
   resource "geosite" do
-    url "https://github.com/v2fly/domain-list-community/releases/download/20210906031055/dlc.dat"
-    sha256 "7618b876fd5a1066d0b44c1c8ce04608495ae991806890f8b1cbfafe79caf6c1"
+    url "https://github.com/v2fly/domain-list-community/releases/download/20220108151752/dlc.dat"
+    sha256 "f1961b28a8a7aa386d69c7480bf5d7bac7fa466fb8dcba499ed2d964f470d9fc"
   end
 
   resource "example_config" do
-    # borrow v2ray (v4.36.2) example config
-    url "https://raw.githubusercontent.com/v2fly/v2ray-core/v4.41.1/release/config/config.json"
+    # borrow v2ray example config
+    url "https://raw.githubusercontent.com/v2fly/v2ray-core/v4.44.0/release/config/config.json"
     sha256 "1bbadc5e1dfaa49935005e8b478b3ca49c519b66d3a3aee0b099730d05589978"
   end
 
   def install
     ldflags = "-s -w -buildid="
     execpath = libexec/name
-    system "go", "build", *std_go_args, "-o", execpath,
-                 "-ldflags", ldflags,
-                 "./main"
+    system "go", "build", *std_go_args(output: execpath, ldflags: ldflags), "./main"
     (bin/"xray").write_env_script execpath,
       XRAY_LOCATION_ASSET: "${XRAY_LOCATION_ASSET:-#{pkgshare}}"
 
