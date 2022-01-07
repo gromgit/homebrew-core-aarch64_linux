59ef831a6b6d815dd35344bdb3396f224932e32f
Porkepix <porkepix@gmail.com>
2022-01-07 06:43:10 +0100

    leakcanary-shark 2.8.1
    Closes #92684.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/leakcanary-shark.rb
+++ Formula/leakcanary-shark.rb
@@ -1,8 +1,8 @@
 class LeakcanaryShark < Formula
   desc "CLI Java memory leak explorer for LeakCanary"
   homepage "https://square.github.io/leakcanary/shark/"
-  url "https://github.com/square/leakcanary/releases/download/v2.8/shark-cli-2.8.zip"
-  sha256 "d1787c5268be1b9a9234f580c4e442eb99d9d35404709a0821392e456d29a2e2"
+  url "https://github.com/square/leakcanary/releases/download/v2.8.1/shark-cli-2.8.1.zip"
+  sha256 "096b8764dcebbe65b2cb212aecf2ac51385a245c24975acfe774f798da64579b"
   license "Apache-2.0"
 
   bottle do
