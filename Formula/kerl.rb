6bafb55f23cd008321e8460ddc4916ce579d95de
homebrew-updater <bepsvpt/homebrew-updater>
2022-01-08 03:00:09 +0800

    kerl 2.2.4
    Closes #92718.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/2.2.3.tar.gz"
-  sha256 "d3c7d29044f43ddf434d69ab9a0ead866ee01b24ae694df03e398bad777cd6c1"
+  url "https://github.com/kerl/kerl/archive/2.2.4.tar.gz"
+  sha256 "89085ab2015b00fb7803053a66dc10ad7bcf66ce6312f345df96ec4849229ef8"
   license "MIT"
   head "https://github.com/kerl/kerl.git", branch: "master"
 
