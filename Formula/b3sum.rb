fee89daa657a1c6f549895fa223c6cd619310449
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-08 11:05:47 +0000

    b3sum: update 1.3.0 bottle.

--- Formula/b3sum.rb
+++ Formula/b3sum.rb
@@ -6,12 +6,12 @@ class B3sum < Formula
   license "CC0-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7e0abedae03cc5c265657ce7d577e135f5e858b596bc3a0d23422061e0f5ba88"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9a4717d41423c61d4ae8a541c22883dc3c2f58da50570470f16218d42cb16052"
-    sha256 cellar: :any_skip_relocation, monterey:       "be0b9f0b27659f8bf81361e240e4ca3f9edb59538717c3c4324c1ba05a2baa23"
-    sha256 cellar: :any_skip_relocation, big_sur:        "509aae19c82540eea310c94d68bac5e69a0cf576bd391ed1eec9ac01c43902cb"
-    sha256 cellar: :any_skip_relocation, catalina:       "ec15d020d408385190a958aa7300cbd8e904bc992653e8d029a2e44a311c8823"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4b19c222d9d00843fea09df330cadf7e6fb67d9abf26f02e592731d9b6e733b3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7dccdd1d7c2d9b8e636f4d78eb1cd1166ab4b4ac69e89f223961ecf5d386e753"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "db0c593c16f5276fb385b03582f261b723da5a8f21709be8b60ec229e7fad462"
+    sha256 cellar: :any_skip_relocation, monterey:       "83b7139a768b15dcfe60da80469aa8a3f750e0dbca4af479dc20fc110ffba825"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ef4df03c023bce47f2de992ba824801a40004ef34a481fe67e19158f51512813"
+    sha256 cellar: :any_skip_relocation, catalina:       "27b9260e41f47be3d2961f04f1cada4b60a9d0485ecca1ff053db435bb444968"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "871dd9192e62a72b35215d3876482822c2b8085f01554a27a615debf85195ea0"
   end
 
   depends_on "rust" => :build
