3b0e1fbde570e8eda4cafcd4b5c3865d85584dda
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-09 13:12:35 +0000

    erofs-utils: add 1.4 bottle.

--- Formula/erofs-utils.rb
+++ Formula/erofs-utils.rb
@@ -6,6 +6,15 @@ class ErofsUtils < Formula
   license "GPL-2.0-or-later"
   head "git://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "575602c8b72d4b7474f08862c92655ee551935ca742508f833e22b88077fde08"
+    sha256 cellar: :any,                 arm64_big_sur:  "6b09c4568245e7ba0442e00bc87342ec5030acb9393c0dccf886a72bdcfb04fe"
+    sha256 cellar: :any,                 monterey:       "df01bcb6e5b5faa32155211916825784564fadce9f610183ef3efd0555dba8b6"
+    sha256 cellar: :any,                 big_sur:        "4bddb7c29191fee61c9c94204d7876096fdcb85ea76d8003b3785ad90ac2bd01"
+    sha256 cellar: :any,                 catalina:       "dddfb64d62d09af5c46bb53dd99f35a8208259ca17d7681e6a6fe608b866bb09"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "650c01bb5fdd4b389c59ac51b3788bdd8cf469145082f007ec94cfda33794045"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
