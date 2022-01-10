0eb09d71d9a11377d01749edf19e38067c28d07e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-10 17:16:39 +0000

    double-conversion: update 3.2.0 bottle.

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -7,12 +7,12 @@ class DoubleConversion < Formula
   head "https://github.com/google/double-conversion.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "352b2b5e23fb43285efcae2e749c8b24fb8184bbb50e7a68f22d2a3b0c90567c"
-    sha256 cellar: :any,                 arm64_big_sur:  "f97fd9898470577cd58fe742917ccaa32bc2697b87aabf25a3c7d08d2a8ece6d"
-    sha256 cellar: :any,                 monterey:       "d479ea9e732d7d60c790ecf2ca4975ab34fb29b82274818c2204c4570b7aaaa1"
-    sha256 cellar: :any,                 big_sur:        "affec63dfbab3c804d0263a7604d3b856c0a4caba03da76af8036aa19a4461e7"
-    sha256 cellar: :any,                 catalina:       "0bb9d82a64fb8f478340bb2d7deb2df539f7c82553fe706c55b5d33d6f339877"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16a406e32a1377c5f9b3184d8dbda192e9fa051543775622397a732fa98d30a8"
+    sha256 cellar: :any,                 arm64_monterey: "d627d25a62f2676f3977dd2001f99d6b6ec00ac1297b682a3a28b1f6a2378100"
+    sha256 cellar: :any,                 arm64_big_sur:  "e2d2777917015468fc626f90340aceea357c644dfb4f6af8080ce577ec66374c"
+    sha256 cellar: :any,                 monterey:       "9aa635e1048ea42d5242a867a538fa5689e25078e75b4c643c4a4234cf911578"
+    sha256 cellar: :any,                 big_sur:        "b12bc5e7981c0ccdd2a69e1a9dd76627de9c712e0efb7128fffedbac8d4404fd"
+    sha256 cellar: :any,                 catalina:       "2aa2c11ee0d8c58d58c7e1b3fc083326ed9248300cb1aadcb4776b41d67993ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c369ccd5f912a3e9d3cdebe0b663a22f19dfaea14c22b6e3b2feab5f6180b35"
   end
 
   depends_on "cmake" => :build
