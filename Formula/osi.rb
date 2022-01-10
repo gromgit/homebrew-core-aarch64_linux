5bd495969d11b888ba20da739de25eae496cfcb6
Porkepix <porkepix@gmail.com>
2022-01-10 10:18:45 +0100

    osi 0.108.7
    Closes #92817.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osi.rb
+++ Formula/osi.rb
@@ -1,8 +1,8 @@
 class Osi < Formula
   desc "Open Solver Interface"
   homepage "https://github.com/coin-or/Osi"
-  url "https://github.com/coin-or/Osi/archive/releases/0.108.6.tar.gz"
-  sha256 "984a5886825e2da9bf44d8a665f4b92812f0700e451c12baf9883eaa2315fad5"
+  url "https://github.com/coin-or/Osi/archive/releases/0.108.7.tar.gz"
+  sha256 "f1bc53a498585f508d3f8d74792440a30a83c8bc934d0c8ecf8cd8bc0e486228"
   license "EPL-1.0"
 
   livecheck do
