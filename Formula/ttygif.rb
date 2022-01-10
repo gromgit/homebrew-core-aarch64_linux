e934ac335407254f56236b550e26c86d600a8187
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-10 23:06:42 +0000

    ttygif: update 1.6.0 bottle.

--- Formula/ttygif.rb
+++ Formula/ttygif.rb
@@ -6,13 +6,11 @@ class Ttygif < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2c31ef2caaf2c5dcb9ee96fd19267f93c4a3e8376e9d44e21686bdede507d85"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c24b1c9714124e4e36eb799ef832ed4e1cd1264dd92c131d235df7cb47dbf10b"
-    sha256 cellar: :any_skip_relocation, monterey:       "4efc5dee59ad2f3dcdde5e6397272f5799a132691a2c3cfddc801963546cb61a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "10887e22e06da0d22817453166f442d619e28058e505d981b159dcf70b20e74a"
-    sha256 cellar: :any_skip_relocation, catalina:       "61f7135b9f03465ac86f26e7b7cad7ca09ec35495841ee868b76f001faefd040"
-    sha256 cellar: :any_skip_relocation, mojave:         "34060f2f53d6388461ca29a81938490bb1768aa9f44303c7cce717c2f8ad6246"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "ab8ee96836d9a9663e94f9dc9e2337a2968a8fe4523f8da166b4e865a1e81ada"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c953e6967a6bc0c649d81c226565818a223a509fc11e556c7bd242b347c888f0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59d6b52ffd6c8f0680e6dda60fdf17dd1f445abb1339be73687dac519b847517"
+    sha256 cellar: :any_skip_relocation, monterey:       "4c955eb6cda1e45e9668ad7eb8cd2f4c8d03754a4fb877a08fc4ffeb6c8602cb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd4346a5d4ff4e7fdbb5fefad4ab5943f927e43d7fb4fe5a45a496d6f8bf62f3"
+    sha256 cellar: :any_skip_relocation, catalina:       "c9fcc9f4e6331acefe39cd12ed8c8ae353d028040526c84f98d6f656cd34af03"
   end
 
   depends_on "imagemagick"
