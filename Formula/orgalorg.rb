bb38fe262563863279856f3fa610f48c3ae39ca6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-10 22:05:58 +0000

    orgalorg: update 1.2.0 bottle.

--- Formula/orgalorg.rb
+++ Formula/orgalorg.rb
@@ -8,12 +8,13 @@ class Orgalorg < Formula
   head "https://github.com/reconquest/orgalorg.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "91ad3f394a710d6f7aa6fede2ce37b21eb10ba5468c92cb33438b5eb3bb2dc3a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "73d99bd8ed8831a00ecc07f6f700025be10c43d89cf33c18ccb1cd7f9de832ab"
-    sha256 cellar: :any_skip_relocation, monterey:       "c6da69f7ae186443e822d2705aba153ae2b13b298df2fb6c75e29a14b2f5d31c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "63b9299de9ec2e76a4e08c513cc112d79ba10315d1aa38040481f5476ced9bdc"
-    sha256 cellar: :any_skip_relocation, catalina:       "a3dfaacf2a623eedfc3ada1459a74d68422b7e1023fd80ada0d4eadae219c1d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea81b423ead484325c0b7b5480d14f11664cb1321c1c67434190a38ca3ca877a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "94c8876f125804235f739721fd76b638142a2d201efa722f80ed9dca67a2bc71"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d351128a7c84c2719ab26b2ff7f4188d8cc511bdd05a9abccaf674ded52baf7f"
+    sha256 cellar: :any_skip_relocation, monterey:       "04019263a064a9c259854951945c31a84ded8847475f607e1452cfaf3d003ed9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a8263d34464253c4bf541e8ad30e1a42fa62c41ec3e1827300fa46192bcb8140"
+    sha256 cellar: :any_skip_relocation, catalina:       "3a76493500a6daa3401c0dba2107f63811794913d621150d391069c44ca9a7a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4612ae3bbd7a798fe80217712774e26bc96b044de4b8aa147baa2e6b93e0dea2"
   end
 
   depends_on "go" => :build
