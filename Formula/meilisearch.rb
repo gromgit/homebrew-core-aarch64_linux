a1604764c0dec9aee1feeb161e49e7579e498d74
Clémentine Urquizar - curqui <clementine@meilisearch.com>
2022-01-11 15:10:26 +0100

    meilisearch 0.25.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #92918.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/meilisearch.rb
+++ Formula/meilisearch.rb
@@ -1,8 +1,8 @@
 class Meilisearch < Formula
   desc "Ultra relevant, instant and typo-tolerant full-text search API"
   homepage "https://docs.meilisearch.com/"
-  url "https://github.com/meilisearch/MeiliSearch/archive/v0.24.0.tar.gz"
-  sha256 "35254c739830d17953194c5817e6b6684d716e3046b2d78f4c89a03ae9c88f4e"
+  url "https://github.com/meilisearch/MeiliSearch/archive/v0.25.0.tar.gz"
+  sha256 "7c7e2caae99c7ec504eb25186c2a67832bc1cdf4572102b85807b9540709a7f8"
   license "MIT"
 
   livecheck do
