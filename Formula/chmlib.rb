aa2f643fd4215653c61d968ca54ff5c56b6f3da6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-11 23:29:22 +0000

    chmlib: update 0.40 bottle.

--- Formula/chmlib.rb
+++ Formula/chmlib.rb
@@ -13,15 +13,17 @@ class Chmlib < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any,                 monterey:     "4d4a29e60712457e4ea3838947a95959dbc0f68338514edd3817d6ee122afbf4"
-    sha256 cellar: :any,                 big_sur:      "af369d3e427b36281f053f65a0d5be2a269c2a0fb80c87443baa066892d0652c"
-    sha256 cellar: :any,                 catalina:     "96d7cb33260c72012f24f383054b7f2505f815f0e3e24298229b5712f8a66cfa"
-    sha256 cellar: :any,                 mojave:       "1718a0a9343788718b4207596ebff457f5214879319292cc1608254374720944"
-    sha256 cellar: :any,                 high_sierra:  "426b95744d071ad76399ee240400ab74bcec9057735cbfeb2d433501105060ef"
-    sha256 cellar: :any,                 sierra:       "9781c76f933beca002df542d2db0644e51766568d9399f9e73dc39b9e896f539"
-    sha256 cellar: :any,                 el_capitan:   "6b834a6ae6e95f8daaa726fd6ae1a2d3e60335f98862fea9e790c24e5a6411d1"
-    sha256 cellar: :any,                 yosemite:     "bdc19058cbf1690e960bd88d06f6c8b2ff47f8b743947eb82c259ba394881a65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "61a085287bba377e847d027575fd848cbadc0f6b5bd8f2efc008cc54d8f32d32"
+    sha256 cellar: :any,                 arm64_monterey: "23e464348836e12bf5835bfdf1acbcdcce344151d12cfa2a055d689c205b6e82"
+    sha256 cellar: :any,                 arm64_big_sur:  "3ab46a541a6aeb2ac904a74fa1433e48bfca91a382e8e8b27290d0597581f520"
+    sha256 cellar: :any,                 monterey:       "4d4a29e60712457e4ea3838947a95959dbc0f68338514edd3817d6ee122afbf4"
+    sha256 cellar: :any,                 big_sur:        "af369d3e427b36281f053f65a0d5be2a269c2a0fb80c87443baa066892d0652c"
+    sha256 cellar: :any,                 catalina:       "96d7cb33260c72012f24f383054b7f2505f815f0e3e24298229b5712f8a66cfa"
+    sha256 cellar: :any,                 mojave:         "1718a0a9343788718b4207596ebff457f5214879319292cc1608254374720944"
+    sha256 cellar: :any,                 high_sierra:    "426b95744d071ad76399ee240400ab74bcec9057735cbfeb2d433501105060ef"
+    sha256 cellar: :any,                 sierra:         "9781c76f933beca002df542d2db0644e51766568d9399f9e73dc39b9e896f539"
+    sha256 cellar: :any,                 el_capitan:     "6b834a6ae6e95f8daaa726fd6ae1a2d3e60335f98862fea9e790c24e5a6411d1"
+    sha256 cellar: :any,                 yosemite:       "bdc19058cbf1690e960bd88d06f6c8b2ff47f8b743947eb82c259ba394881a65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61a085287bba377e847d027575fd848cbadc0f6b5bd8f2efc008cc54d8f32d32"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
