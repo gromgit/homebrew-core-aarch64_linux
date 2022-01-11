e5041759b1950d20258b28e240660421769c8436
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-11 18:15:22 +0000

    neopop-sdl: update 0.2 bottle.

--- Formula/neopop-sdl.rb
+++ Formula/neopop-sdl.rb
@@ -11,13 +11,14 @@ class NeopopSdl < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:     "53e2a47e1f4e3bc4b35a31ea06f757ef62fc11de24347fcca5f4d1799f1adf94"
-    sha256 cellar: :any, catalina:    "c4bd22db58945139a07d7c007c546e2edb3be1c3763f2d3f3008b575f30cef84"
-    sha256 cellar: :any, mojave:      "d84d1d9e2304a21ce915b8a65001a310da3c797e1f89e4d8a86a102e53f92f10"
-    sha256 cellar: :any, high_sierra: "9bdf06235151ae52d85e630021ce810d49ce12ba74e18b27f7584d9584377eb4"
-    sha256 cellar: :any, sierra:      "3510d31984f2f46a59390617e2af3941638a4eb20a42131fc804e5d307cb5059"
-    sha256 cellar: :any, el_capitan:  "e115fe849a0b8e1921a6c36c3d34fcc00b911f0504a0e32543656e76513384ad"
-    sha256 cellar: :any, yosemite:    "a8de30162f9e5146ee7c39480e83588f8036c0b965215e7ce1894c79855c8687"
+    sha256 cellar: :any,                 big_sur:      "53e2a47e1f4e3bc4b35a31ea06f757ef62fc11de24347fcca5f4d1799f1adf94"
+    sha256 cellar: :any,                 catalina:     "c4bd22db58945139a07d7c007c546e2edb3be1c3763f2d3f3008b575f30cef84"
+    sha256 cellar: :any,                 mojave:       "d84d1d9e2304a21ce915b8a65001a310da3c797e1f89e4d8a86a102e53f92f10"
+    sha256 cellar: :any,                 high_sierra:  "9bdf06235151ae52d85e630021ce810d49ce12ba74e18b27f7584d9584377eb4"
+    sha256 cellar: :any,                 sierra:       "3510d31984f2f46a59390617e2af3941638a4eb20a42131fc804e5d307cb5059"
+    sha256 cellar: :any,                 el_capitan:   "e115fe849a0b8e1921a6c36c3d34fcc00b911f0504a0e32543656e76513384ad"
+    sha256 cellar: :any,                 yosemite:     "a8de30162f9e5146ee7c39480e83588f8036c0b965215e7ce1894c79855c8687"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "53a84ba836a1c4f2ce579d149554a865e69a736ec69e6c49de369dd5f0364673"
   end
 
   head do
