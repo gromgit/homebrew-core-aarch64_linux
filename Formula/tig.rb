80838e2664acb1890bfe1503c56f0b47da8f0c1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-12 22:05:38 +0000

    tig: update 2.5.5 bottle.

--- Formula/tig.rb
+++ Formula/tig.rb
@@ -6,13 +6,12 @@ class Tig < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "75fec92f280daf4789ccd06a2ad809185032cbd98097baffeb6b0b283b9212ef"
-    sha256 cellar: :any,                 arm64_big_sur:  "ca3273a9bb53f6b060ce4ccbffaf82308d90d833a7120505a55f877b6f197592"
-    sha256 cellar: :any,                 monterey:       "3ff950a8d1588789c3b4bb86f998464f3b69ebbf37aca1403a43b8499f93f244"
-    sha256 cellar: :any,                 big_sur:        "506b2d47105b97ba9d04f3b91a45e9a164dd409fc1fd58655bf3015bb1403e0b"
-    sha256 cellar: :any,                 catalina:       "5afa0c1a9dcb4d851f88db05b0f911e70014abb6d64b43d1b1e56f12452d233a"
-    sha256 cellar: :any,                 mojave:         "c2dff759957d7b62f5ab947e6637c428c9352c244faeb6f8e02918f8c9587771"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a71079791b2ff79c307250f00163c3320ba6c242f4115892fa490d78a71196f9"
+    sha256 cellar: :any,                 arm64_monterey: "a7d0bf9c1f535420cd5855c037280d5f7e3f7061c8a8f5c3178e567d92e8839c"
+    sha256 cellar: :any,                 arm64_big_sur:  "3038dff468e2e130f161455b51a7a387bc76d1d3f700d7ddf549b2f391accc36"
+    sha256 cellar: :any,                 monterey:       "69a497a92721ff1947ae97cfdacd7003b3991689c6ca81ee2bcf7378384fc9b0"
+    sha256 cellar: :any,                 big_sur:        "b0d7cb650c15d1d07c1210cb27610c971c9216399547af10dcdd16c8bbd74eb6"
+    sha256 cellar: :any,                 catalina:       "74ab1d678111673ba10abce9c05c3d77aa1f9ef66d5a51842d05f13594fb1a3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "09b38c3c63caf15b9cf3d3611692402ca02dbdcbac45e06ff3812810398fd38d"
   end
 
   head do
