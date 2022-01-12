d712b35a634c262088f8500d2124472d4e7a33fb
Rui Chen <rui@chenrui.dev>
2022-01-12 17:00:48 -0500

    promtail 2.4.2
    Closes #93010.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -1,9 +1,10 @@
 class Promtail < Formula
   desc "Log agent for Loki"
   homepage "https://grafana.com/loki"
-  url "https://github.com/grafana/loki/archive/v2.4.1.tar.gz"
-  sha256 "a26c22941b406b8c42e55091c23798301181df74063aaaf0f678acffc66d8c27"
+  url "https://github.com/grafana/loki/archive/v2.4.2.tar.gz"
+  sha256 "725af867fa3bece6ccd46e0722eb68fe72462b15faa15c8ada609b5b2a476b07"
   license "AGPL-3.0-only"
+  head "https://github.com/grafana/loki.git", branch: "main"
 
   livecheck do
     formula "loki"
