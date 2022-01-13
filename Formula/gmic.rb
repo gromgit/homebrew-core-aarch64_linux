ed69d7d5326e5c4d89f4175f2ec7fc44634fed75
Porkepix <porkepix@gmail.com>
2022-01-13 23:03:53 +0100

    gmic 3.0.1
    Closes #93106.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -1,8 +1,8 @@
 class Gmic < Formula
   desc "Full-Featured Open-Source Framework for Image Processing"
   homepage "https://gmic.eu/"
-  url "https://gmic.eu/files/source/gmic_3.0.0.tar.gz"
-  sha256 "3f056bb9e6dbf0674af4c8dce59f4198172187662f7fbb36cc63ebc8c1b71120"
+  url "https://gmic.eu/files/source/gmic_3.0.1.tar.gz"
+  sha256 "6cc20a20e3ab53ce485ccf6e044a30141b3d62cf7743b83bb04906ff29453035"
   license "CECILL-2.1"
   head "https://github.com/dtschump/gmic.git", branch: "master"
 
