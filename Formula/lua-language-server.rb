a8abaf9d5ac3d43f216e7e7ee4c355b203f2792c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 21:05:48 +0000

    lua-language-server: update 2.6.0 bottle.

--- Formula/lua-language-server.rb
+++ Formula/lua-language-server.rb
@@ -9,12 +9,12 @@ class LuaLanguageServer < Formula
   head "https://github.com/sumneko/lua-language-server.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f7aef599415fcdca892571d97e8369c0c4a8c8185a2a624874a58fc2cff98bc5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "03e33fc1fedd5ed8f4c06a6319f3bbd0192a722e4778821a372cc1ce11d5c13d"
-    sha256 cellar: :any_skip_relocation, monterey:       "712d63df4aea65c0f116efb1f3df9b08fa5ff03e5116df3ad33a33e13915b4b8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f5418dcdff4123ce5761362baa47785831335111c31478c22b8b925fbd33515b"
-    sha256 cellar: :any_skip_relocation, catalina:       "ab65794230ed9a76493dc95239adfc1179d1e7dc250c3401eed798c256df5ddf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a1fcc8e1a4c89d036b4d3cbfc615bf518f4cca874619a6012614a879403b625"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "267ee5851d16070e6f5a930552878baa8ea3f9ea249e51c6f7cf672b25fd09ba"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d8ece6d72d3cf71504cc5c4d314cf2b016f014697ee86215cd5e8f6c8f6b6f6"
+    sha256 cellar: :any_skip_relocation, monterey:       "de0cf8f9341cc5c1869c18475bed8d91c5c4d1a1f0e83077b28f156b8cc06891"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8af7fa1e75ad8c7ff68cc126d360ac38e242eba4675aeb1c02116ae90f37cd12"
+    sha256 cellar: :any_skip_relocation, catalina:       "682e006cbea75692f673b988b27a2c07f78e8dd205c300dbd2f480d842a1b126"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "546345d5aa4617ece21dfcef31883a51c68803dd159e5924dc8689de891c166c"
   end
 
   depends_on "ninja" => :build
