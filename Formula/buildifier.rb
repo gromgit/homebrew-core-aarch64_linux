1c5060bc2000e29c9d640ef72138f08e4ae64450
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 19:05:21 +0000

    buildifier: update 4.2.5 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -7,12 +7,12 @@ class Buildifier < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5b20c6f5b4114292d86fd9f78596863eca617bc27a756547f3863957bf760ec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e5b20c6f5b4114292d86fd9f78596863eca617bc27a756547f3863957bf760ec"
-    sha256 cellar: :any_skip_relocation, monterey:       "5f5b652e976e178c143a0a0c98194d624881d4d756d83f0f966834edbfbcfa8c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5f5b652e976e178c143a0a0c98194d624881d4d756d83f0f966834edbfbcfa8c"
-    sha256 cellar: :any_skip_relocation, catalina:       "5f5b652e976e178c143a0a0c98194d624881d4d756d83f0f966834edbfbcfa8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e446730f6b54b58dade1ee10deb6370d11324903c34156c5c0e2ab8f44d85d6e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5116f540496272980ceb3441b8848bba227235ede515da637ccc62895d7c3b8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5116f540496272980ceb3441b8848bba227235ede515da637ccc62895d7c3b8"
+    sha256 cellar: :any_skip_relocation, monterey:       "1788be9f53f8681770f57b4e3b04bffd224e7e705dd8d01d76ac52c2bc0a694d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1788be9f53f8681770f57b4e3b04bffd224e7e705dd8d01d76ac52c2bc0a694d"
+    sha256 cellar: :any_skip_relocation, catalina:       "1788be9f53f8681770f57b4e3b04bffd224e7e705dd8d01d76ac52c2bc0a694d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a7cee2584b709f6ecaeb2bfdfb5147c1013d1251223edd4a1ff782c21d9363e6"
   end
 
   depends_on "go" => :build
