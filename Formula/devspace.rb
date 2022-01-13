e15bcb19cd5fd4cd1fc52afb7a68fc16314633df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 05:19:18 +0000

    devspace 5.18.2
    * devspace 5.18.2
    * fix test
    
    Closes #93032.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.sh/"
   url "https://github.com/loft-sh/devspace.git",
-      tag:      "v5.18.1",
-      revision: "2f47f02edcabbb94dd571f561db438fbdc932a86"
+      tag:      "v5.18.2",
+      revision: "cbe147f38b8c857d72d0d4851523bc7a9727471e"
   license "Apache-2.0"
   head "https://github.com/loft-sh/devspace.git", branch: "master"
 
@@ -35,7 +35,7 @@ class Devspace < Formula
 
   test do
     help_output = "DevSpace accelerates developing, deploying and debugging applications with Docker and Kubernetes."
-    assert_match help_output, shell_output("#{bin}/devspace help")
+    assert_match help_output, shell_output("#{bin}/devspace --help")
 
     init_help_output = "Initializes a new devspace project"
     assert_match init_help_output, shell_output("#{bin}/devspace init --help")
