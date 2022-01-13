4ebbc27b0825b9e059ac2789cd350ebcd2f74f81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 09:09:20 +0000

    ki: update 0.4.1 bottle.

--- Formula/ki.rb
+++ Formula/ki.rb
@@ -7,10 +7,10 @@ class Ki < Formula
   head "https://github.com/Kotlin/kotlin-interactive-shell.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1e9f0d1372315b5518bbc05dab3e5a2ee749c68ea59c0c5f1f31b276db8c33a2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "eb557d13c281da2852c117922d63673ca76f4337bb2328b8b7a5b092836546cb"
-    sha256 cellar: :any_skip_relocation, catalina:      "2fa5ab29776d2d12f81731dd5c89e9416cb44812db796b0fe421b33ae31622cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84f960a850d909a38a77c18c2088ec442a65f47dd22d4e53b45d93906279459a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4a1ee5e5bb0bf0b056a5866d0929205dc18d527eda8440a62472ac754bb37224"
+    sha256 cellar: :any_skip_relocation, big_sur:       "5aabd7a1c7d6950f3f83a2a4b27f93cdc1c280e00934270b58a72a2efc337eae"
+    sha256 cellar: :any_skip_relocation, catalina:      "e02c9d31581795565ef161220c8882a2cf154393ad0ed235f89ed9582fb123cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7c09fe8b6fdf4bf061d72b307b391ee0306f53af63a232a2048cb51a70257e4"
   end
 
   depends_on "maven" => :build
