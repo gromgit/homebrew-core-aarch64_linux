ad5d6e183554d8378b6885cdfcd3928fd5da3f9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 10:11:29 +0000

    parquet-cli: update 1.12.0 bottle.

--- Formula/parquet-cli.rb
+++ Formula/parquet-cli.rb
@@ -8,13 +8,13 @@ class ParquetCli < Formula
   head "https://github.com/apache/parquet-mr.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8ec90d31e13d0630ed26cd3da219a448fafb672a4277ee277d8ab4c1e68fabcc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b3c9cf0f2d194f6ea74d86187d07fea91b26a1dba7911d9648eaefa8b6039c6b"
-    sha256 cellar: :any_skip_relocation, monterey:       "22799743778180bab374cc78565237689ae3a6405ded0022c46bd156b8f3cd12"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a0b9c8859d7dfb3c6e66cef92b191b7a4861338e02be1694ba70f86c646e56db"
-    sha256 cellar: :any_skip_relocation, catalina:       "1cd95d2c049e2d049e799f27dda903fb6497d4701f86924fec4ac8470455dae2"
-    sha256 cellar: :any_skip_relocation, mojave:         "f1c5eb930f2c094ba7e3ef80856420b01f96ce197705f5da0b39039a54ffa498"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75ce0a3f58329686ecec3060d3ffd715265cdccfc0f80d443833d2a7bd03b812"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d0534ab800669ce2048ff05409d7b6568df34bf7aa6507fc2e4417da8418ba89"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e2047e61a6366122dde6a70c01f49b464d16fb4a044ab949278ff0e4cc37e735"
+    sha256 cellar: :any_skip_relocation, monterey:       "25a014f843a9ccd2d1b3e715d8dcf2ffe6b24bcfa0bc78739d1b1caf3529e1e3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9140045b83453b50a13384917993e14068d48285ba4410c09d22347f2c00f9be"
+    sha256 cellar: :any_skip_relocation, catalina:       "b522b0d1481308650dac17bc1e4b7fab207a501038e663d41ee9d11a535288ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e3d896f727c127f43d5e0d476f3f9b7d314f96aada084846d3659eab7e1d200a"
   end
 
   depends_on "maven" => :build
