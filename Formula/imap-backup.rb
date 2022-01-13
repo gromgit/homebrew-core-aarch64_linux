ecf269616f85ebdbcf9d3a2d6a3ea98ce01302fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 05:20:40 +0000

    imap-backup 4.2.0
    * imap-backup 4.2.0
    * update test
    
    Closes #93037.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imap-backup.rb
+++ Formula/imap-backup.rb
@@ -1,9 +1,8 @@
 class ImapBackup < Formula
   desc "Backup GMail (or other IMAP) accounts to disk"
   homepage "https://github.com/joeyates/imap-backup"
-  url "https://github.com/joeyates/imap-backup/archive/refs/tags/v4.0.7.tar.gz"
-  sha256 "785dd28a6d2a33b2774ce09db30c41add5925d737f7840562a2e9b4fb7f9836d"
-
+  url "https://github.com/joeyates/imap-backup/archive/refs/tags/v4.2.0.tar.gz"
+  sha256 "3fce5cbee17617d0284e3f4ad1261b3ba703374b2e14ffff0e29216abd90304d"
   license "MIT"
 
   bottle do
@@ -27,20 +26,6 @@ class ImapBackup < Formula
   end
 
   test do
-    # workaround from homebrew-core/Formula/pianobar.rb
-    on_linux do
-      # Errno::EIO: Input/output error @ io_fread - /dev/pts/0
-      return if ENV["HOMEBREW_GITHUB_ACTIONS"]
-    end
-
-    require "pty"
-    PTY.spawn bin/"imap-backup setup" do |r, w, pid|
-      r.winsize = [80, 43]
-      sleep 1
-      w.write "exit without saving changes\n"
-      assert_match(/Choose an action:/, r.read)
-    ensure
-      Process.kill("TERM", pid)
-    end
+    assert_match "Choose an action:", pipe_output(bin/"imap-backup setup", "3\n")
   end
 end
