41faf53e62d277b8a24266c52306738f03f40fda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 10:10:30 +0000

    regina-rexx: update 3.9.4 bottle.

--- Formula/regina-rexx.rb
+++ Formula/regina-rexx.rb
@@ -6,14 +6,12 @@ class ReginaRexx < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "6ee431b4c05183e8d40cc5a8ae8116bacbd98626e3dc0b77689dea21e60143f2"
-    sha256 arm64_big_sur:  "f3360bfae946de69ba78cd467e4904e7ebbd722b9a2e9ea8b2e9a2052dc78a1d"
-    sha256 monterey:       "082b50b171979ee9634e5738b1ac43ffff1cb43eb524fed50131d39d1e410fb2"
-    sha256 big_sur:        "1afeefc231cf72467ed8f8c28e2671707d779f91eb6d736d672e6dfe30c4841f"
-    sha256 catalina:       "7d39d4158fe41ecbd85c8c05f27d1b291883730ae1b745e1920e14ab41dfa0dc"
-    sha256 mojave:         "396fe213db316516ff28a135217b9c660969244494cb8807111e71b37d5451c9"
-    sha256 high_sierra:    "c8e204d8fb1154c31a4be3d571f4bbcc9e9b9ec5406feb61be82f7c567f9c8a7"
-    sha256 x86_64_linux:   "42fe771604ffbc858e967789d60cb329536dcb0bc5e69df553de3ed85b823f45"
+    sha256 arm64_monterey: "b54fa50c843942c79e745d3ce9468db9310dc70e973fa94e190e3a0fa0e5caf3"
+    sha256 arm64_big_sur:  "ab27f3fe98ea089a93403c418f33f0b1698a47e1161b73118fcb24742e50d2f9"
+    sha256 monterey:       "d4e4746bb04e0dfe7b6c01e83349f1dc896ba22571a95c9dffb6c07095bc0902"
+    sha256 big_sur:        "c6cfcdf1d903dc27e38bb21c0d02fb846f3fc418cc06decdc449f7d512156502"
+    sha256 catalina:       "2b98c5ca16915c08e04828749e7f75644d7b7e1121a64ec78411ef9e9603c294"
+    sha256 x86_64_linux:   "7b44626961634ed07b0c215d78c345dcc1c97d0a36c9ea3a530aa78ca61b9d8b"
   end
 
   def install
