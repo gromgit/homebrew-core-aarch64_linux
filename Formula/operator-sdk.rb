d7587548b228d70e58991841caf40bcc2aa7f9e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-13 05:22:56 +0000

    operator-sdk 1.16.0
    Closes #93042.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/operator-sdk.rb
+++ Formula/operator-sdk.rb
@@ -2,8 +2,8 @@ class OperatorSdk < Formula
   desc "SDK for building Kubernetes applications"
   homepage "https://coreos.com/operators/"
   url "https://github.com/operator-framework/operator-sdk.git",
-      tag:      "v1.15.0",
-      revision: "f6326e832a8a5e5453d0ad25e86714a0de2c0fc8"
+      tag:      "v1.16.0",
+      revision: "560044140c4f3d88677e4ef2872931f5bb97f255"
   license "Apache-2.0"
   head "https://github.com/operator-framework/operator-sdk.git", branch: "master"
 
