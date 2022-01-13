1b0afb7a705f47714d825bac1ace12732c731ee0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-13 10:18:27 +0000

    zk: add 0.9.0 bottle.

--- Formula/zk.rb
+++ Formula/zk.rb
@@ -7,6 +7,15 @@ class Zk < Formula
 
   head "https://github.com/mickael-menu/zk.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "ce112a10870e5cdd752cbe76281d209cfec19247719771b52391028ad1b7139c"
+    sha256 cellar: :any,                 arm64_big_sur:  "d36bd7d94f0008c736de5cfe2e4223d0c7c20787e14cf649889a9a6ebe6683f9"
+    sha256 cellar: :any,                 monterey:       "1b25ef03a856fd00f78fb9abab558388f7382ab4060d518ffe8304be02b46c64"
+    sha256 cellar: :any,                 big_sur:        "8e775095be3f5055eae9e8187c954a887068a0c8de021e43e1514cce13fa2768"
+    sha256 cellar: :any,                 catalina:       "9d28c3e240b0ceed53da7701f39c8927642d17e8f90bc9c2388cfbbb02685551"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5aca583595a78d57225fab2d6dc7f9fc0c84223e725c6c3f6b287fd283f917c6"
+  end
+
   depends_on "go" => :build
 
   depends_on "icu4c"
