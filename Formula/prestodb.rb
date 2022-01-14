64fd9940ab04ded03d43e9a3523ce0970c0fca42
Porkepix <porkepix@gmail.com>
2022-01-14 12:58:47 +0100

    prestodb 0.268
    * prestodb 0.268
    * prestodb-0.268: update presto-cli resource
    
    Closes #93146.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prestodb.rb
+++ Formula/prestodb.rb
@@ -1,8 +1,8 @@
 class Prestodb < Formula
   desc "Distributed SQL query engine for big data"
   homepage "https://prestodb.io"
-  url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-server/0.267/presto-server-0.267.tar.gz"
-  sha256 "2866d271a0e5dda8c3861ce16ebad043bda250917a278589f5b0bfe3ed4f802d"
+  url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-server/0.268/presto-server-0.268.tar.gz"
+  sha256 "74be89b6564c2955deb60e11e519479703351ff4c57b785bc5bc17a0b20bda67"
   license "Apache-2.0"
 
   # Upstream has said that we should check Maven for Presto version information
@@ -21,8 +21,8 @@ class Prestodb < Formula
   depends_on "openjdk"
 
   resource "presto-cli" do
-    url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-cli/0.267/presto-cli-0.267-executable.jar"
-    sha256 "fc61eb9a1d527705f209f0ee85016612508d74d9a378eeb25d98f675732223c0"
+    url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-cli/0.268/presto-cli-0.268-executable.jar"
+    sha256 "d40dc657e852a344ba22f0a013c67f808e6fa3ce0a79d9fc9d6783c4f59f8951"
   end
 
   def install
