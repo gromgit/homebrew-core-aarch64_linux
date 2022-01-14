33179230dd8327a376a0f456d0e080fa865aac4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-14 13:10:54 +0000

    yaml-language-server: update 1.3.0 bottle.

--- Formula/yaml-language-server.rb
+++ Formula/yaml-language-server.rb
@@ -8,12 +8,12 @@ class YamlLanguageServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9263e1c32411bccec08e034997088e8911139704992e5eb427992f1bfdb3198c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9263e1c32411bccec08e034997088e8911139704992e5eb427992f1bfdb3198c"
-    sha256 cellar: :any_skip_relocation, monterey:       "47dccc5d1968883137593117fc16944ae29edb65ffce2ef0782a9de2526a56f4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "47dccc5d1968883137593117fc16944ae29edb65ffce2ef0782a9de2526a56f4"
-    sha256 cellar: :any_skip_relocation, catalina:       "47dccc5d1968883137593117fc16944ae29edb65ffce2ef0782a9de2526a56f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9263e1c32411bccec08e034997088e8911139704992e5eb427992f1bfdb3198c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "76bc2f0f7e22eebe4af0100370afe5f521513fb1f40943675bcf96e567212737"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "76bc2f0f7e22eebe4af0100370afe5f521513fb1f40943675bcf96e567212737"
+    sha256 cellar: :any_skip_relocation, monterey:       "edfc7608ff4d55596c1b65fef083bd5b2c65e45a61ec3361a0d7a4f178927a97"
+    sha256 cellar: :any_skip_relocation, big_sur:        "edfc7608ff4d55596c1b65fef083bd5b2c65e45a61ec3361a0d7a4f178927a97"
+    sha256 cellar: :any_skip_relocation, catalina:       "edfc7608ff4d55596c1b65fef083bd5b2c65e45a61ec3361a0d7a4f178927a97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76bc2f0f7e22eebe4af0100370afe5f521513fb1f40943675bcf96e567212737"
   end
 
   depends_on "node"
