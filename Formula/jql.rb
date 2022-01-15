8dcdbbc28ce0ffad521d0e7b649a601fb18fea48
Rui Chen <rui@chenrui.dev>
2022-01-15 13:15:29 -0500

    jql 3.0.7
    Closes #93205.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -1,8 +1,8 @@
 class Jql < Formula
   desc "JSON query language CLI tool"
   homepage "https://github.com/yamafaktory/jql"
-  url "https://github.com/yamafaktory/jql/archive/v3.0.6.tar.gz"
-  sha256 "d931301c920b35ed2943902e1460fcd5c71afccce10c4d334a410d30707b6a0e"
+  url "https://github.com/yamafaktory/jql/archive/v3.0.7.tar.gz"
+  sha256 "cce5e6b0c5523526e2d70f57895c336cf31c4f87a8d0c59f94e6a3d7ed35713d"
   license "MIT"
   head "https://github.com/yamafaktory/jql.git", branch: "main"
 
