5130bfe526e1a959c0ba889785b17d1da5da238d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-15 03:07:48 +0000

    waypoint: update 0.7.0 bottle.

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -7,12 +7,12 @@ class Waypoint < Formula
   head "https://github.com/hashicorp/waypoint.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9dfc21ba34b838bd87b3846e4f8ee6cc0e1bb6ca9d8d18b1ee4f4a4ba99baff7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7e2423eb1814d4fbebb839fbd468547d6d38b36887312bf866a8716ed0e029d3"
-    sha256 cellar: :any_skip_relocation, monterey:       "700a542b4761ccaca3a306511b0e30fa959c221136ecc651d1612521cf423e54"
-    sha256 cellar: :any_skip_relocation, big_sur:        "13e5d5eb373f1146c550bd2ea8517d49588b25732100aa64b1ddca2e8545ba2b"
-    sha256 cellar: :any_skip_relocation, catalina:       "d3a0fb49494acb604d908ce662ce5acaaeb6afde8e5c87c5a349d99048bbd6ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b85957fa64ffe96ef0d4ea827f08d1f30ca0367b457aacc3fd267211256d34e6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "05cb79b1339050d7ef2779cdde61b2236dd1161c2cee03a15f9d65bea750bfc3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "481fa811ce113775604a4a4e21f35818d851f396aa7e3b42bb2f2f1950901ac6"
+    sha256 cellar: :any_skip_relocation, monterey:       "762f7c09181808f0605f7d04ef791abdd563a9faa5fb0f8a055f2f7f7ac4b9f8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "39d77c3fd3c3ce70e46e0aadd2819f710662b7d7484e5bd61bc4b311d4d58a22"
+    sha256 cellar: :any_skip_relocation, catalina:       "2c8e1b222c6ab9975e1fa5b8c70e8feb8a302a6d17386d7f49574460fd11967e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dbed671774917fe5f4d7c025e3028e62eefae266be2b0008cfaecd9f840f2291"
   end
 
   depends_on "go" => :build
