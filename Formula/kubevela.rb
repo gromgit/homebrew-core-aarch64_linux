36d3101686b84ec8a00b690d399f95644d2e2c20
Rui Chen <rui@chenrui.dev>
2022-01-14 19:11:24 -0500

    kubevela 1.2.0
    * kubevela 1.2.0
    * Update kubevela.rb
    * update test assertion
    
    Closes #93177.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -2,8 +2,8 @@ class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
   url "https://github.com/oam-dev/kubevela.git",
-      tag:      "v1.1.13",
-      revision: "8de36ff2950b3c757f912e81d2fc4be3ea4b60af"
+      tag:      "v1.2.0",
+      revision: "6cee4687437958cbaa34e41112c8a1734503ec3c"
   license "Apache-2.0"
 
   bottle do
@@ -31,7 +31,7 @@ class Kubevela < Formula
   test do
     # Should error out as vela up need kubeconfig
     status_output = shell_output("#{bin}/vela up 2>&1", 1)
-    assert_match "get kubeConfig err invalid configuration: no configuration has been provided", status_output
+    assert_match "Error: invalid configuration: no configuration", status_output
 
     (testpath/"kube-config").write <<~EOS
       apiVersion: v1
