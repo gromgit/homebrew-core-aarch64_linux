fb30ba56298ee67bc9a238366b4dc92e56310ca2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-16 15:05:16 +0000

    arb: update 2.22.0 bottle.

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -7,12 +7,12 @@ class Arb < Formula
   head "https://github.com/fredrik-johansson/arb.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "41583ad89df5a7377eed7820a2362b1c541d71b946a8984c3de80deef25b8c87"
-    sha256 cellar: :any,                 arm64_big_sur:  "90a180e2d81802642eebee8b799be87fee105fe21ee96a387d0d0fa0e6e6bb6f"
-    sha256 cellar: :any,                 monterey:       "dca91f023aa5bb528ab8f43a5973180d014385818dc2fdc8108be7de60f3d02c"
-    sha256 cellar: :any,                 big_sur:        "43d1508bf8b2902ad5dc670db8827f3183df9ce1069fb15072c2fb578c4c2ebb"
-    sha256 cellar: :any,                 catalina:       "4f9d704d41d0b413bfd7523c7217cc7b0b31accf060565dc188424d1ce3bf44d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39945070871bece010b60f8af6d4ed6b79633bd6cf5dbde78a52faebd8cbfea4"
+    sha256 cellar: :any,                 arm64_monterey: "84c04a9ef6babf020a9b206952a190a3615ec6f4308511ca6e3666d918daea6c"
+    sha256 cellar: :any,                 arm64_big_sur:  "114fc3d09584a41b1a9b575614336cf967256538af25a8d8f606bdc667e76dee"
+    sha256 cellar: :any,                 monterey:       "30f935efac95853a4811364722ea7c937061d41262d5e926c6138ac26157265f"
+    sha256 cellar: :any,                 big_sur:        "1214b6c2a51060fefc333407e5cfe3cbd1389bbed5a5e858836b9fa62b508346"
+    sha256 cellar: :any,                 catalina:       "02d6f6518b610cf5edd8331efb75287e51c89a09a59b8e3d22e1554960d8cd34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d91ac6193c1afb9b98325a4501572c3a493a2a168fd9461105c84e859ee051b4"
   end
 
   depends_on "cmake" => :build
