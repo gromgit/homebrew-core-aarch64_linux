fbdd58c0cd478266c9a4ee6ff6f942877a61ac3c
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2022-01-17 14:10:31 +0000

    idris: update 1.3.4 bottle.

--- Formula/idris.rb
+++ Formula/idris.rb
@@ -7,9 +7,10 @@ class Idris < Formula
   head "https://github.com/idris-lang/Idris-dev.git", branch: "master"
 
   bottle do
-    sha256 monterey: "704add14a985699c70650b364a9069f1a2b4ad52f9238890067d695744db63e4"
-    sha256 big_sur:  "19ad0c1b5aed35799c1a7199418e96b509b19b5fdad8dcf6492ca0ec10b14676"
-    sha256 catalina: "c38a0f42b48ace8818f060ed26834d9e395e614f12fe6449b1e48513c73dcec8"
+    sha256 monterey:     "704add14a985699c70650b364a9069f1a2b4ad52f9238890067d695744db63e4"
+    sha256 big_sur:      "19ad0c1b5aed35799c1a7199418e96b509b19b5fdad8dcf6492ca0ec10b14676"
+    sha256 catalina:     "c38a0f42b48ace8818f060ed26834d9e395e614f12fe6449b1e48513c73dcec8"
+    sha256 x86_64_linux: "f85bad06ae4d62578f441d915a3c0d47e3bb1064c1615883ebaa90f2a51c106c"
   end
 
   depends_on "cabal-install" => :build
