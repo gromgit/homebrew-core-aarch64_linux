47feb4c6c92d1c0d5c6f37f6e5801ba652cc5379
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-18 05:29:00 +0000

    nicotine-plus: update 3.2.0 bottle.

--- Formula/nicotine-plus.rb
+++ Formula/nicotine-plus.rb
@@ -9,11 +9,12 @@ class NicotinePlus < Formula
   head "https://github.com/Nicotine-Plus/nicotine-plus.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a7fa6eec8feed00ddc8740ca997a1e10ec7589e9db24f5cf1736e55994bf6b87"
-    sha256 cellar: :any_skip_relocation, monterey:      "e747f405686ef55476b094601d70d00af6793219fa9805871bb977770760621b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e747f405686ef55476b094601d70d00af6793219fa9805871bb977770760621b"
-    sha256 cellar: :any_skip_relocation, catalina:      "e747f405686ef55476b094601d70d00af6793219fa9805871bb977770760621b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ddee5cdc6d4015953c6097cfb273aef30ccb3cf1e8502cb59db6a799c53b3d7a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b238a3872d2d05e9abf72ed166c77d5e9dc74c6ef4a9b9108ed63282e9a3fb6d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a7fa6eec8feed00ddc8740ca997a1e10ec7589e9db24f5cf1736e55994bf6b87"
+    sha256 cellar: :any_skip_relocation, monterey:       "e747f405686ef55476b094601d70d00af6793219fa9805871bb977770760621b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e747f405686ef55476b094601d70d00af6793219fa9805871bb977770760621b"
+    sha256 cellar: :any_skip_relocation, catalina:       "e747f405686ef55476b094601d70d00af6793219fa9805871bb977770760621b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ddee5cdc6d4015953c6097cfb273aef30ccb3cf1e8502cb59db6a799c53b3d7a"
   end
 
   depends_on "adwaita-icon-theme"
