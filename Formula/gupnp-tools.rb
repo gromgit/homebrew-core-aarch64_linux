4bd3abc65071b4eb553ceff6be162cb3e57ff52f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-18 15:31:02 +0000

    gupnp-tools: update 0.10.2 bottle.

--- Formula/gupnp-tools.rb
+++ Formula/gupnp-tools.rb
@@ -6,11 +6,12 @@ class GupnpTools < Formula
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later"]
 
   bottle do
-    sha256 arm64_big_sur: "8418c2570591b15a49305251ef6e56a78def7ed72f848f1bd02a303e8416e6e3"
-    sha256 monterey:      "743c63f60784485413f15bf30cbd0a6164ce4f463dab22a2cbb3d5abb8241771"
-    sha256 big_sur:       "512b29e2e56968496991b3ba51952e21bf47353caa4625aceae382969f78047b"
-    sha256 catalina:      "2da164d05b713d158b8e537ee77cbd32546bef71223c23f067205b8e9c750fc0"
-    sha256 x86_64_linux:  "a131f92a504507638f37272d73bfc5d0e546dab25aa2664507e7b1a09bbe7517"
+    sha256 arm64_monterey: "60c27e301bcaba5c95b491b9865db6b453a92e8a403f0d4897b6fe16a10689aa"
+    sha256 arm64_big_sur:  "8418c2570591b15a49305251ef6e56a78def7ed72f848f1bd02a303e8416e6e3"
+    sha256 monterey:       "743c63f60784485413f15bf30cbd0a6164ce4f463dab22a2cbb3d5abb8241771"
+    sha256 big_sur:        "512b29e2e56968496991b3ba51952e21bf47353caa4625aceae382969f78047b"
+    sha256 catalina:       "2da164d05b713d158b8e537ee77cbd32546bef71223c23f067205b8e9c750fc0"
+    sha256 x86_64_linux:   "a131f92a504507638f37272d73bfc5d0e546dab25aa2664507e7b1a09bbe7517"
   end
 
   depends_on "meson" => :build
