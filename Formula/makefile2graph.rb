d84295981eb033fa5894e9660432f26ea30f090b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-18 05:22:19 +0000

    makefile2graph: update 2021.11.06 bottle.

--- Formula/makefile2graph.rb
+++ Formula/makefile2graph.rb
@@ -7,11 +7,12 @@ class Makefile2graph < Formula
   head "https://github.com/lindenb/makefile2graph.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5eb90095c4579d7d8616732bec7f266164f7b5f7665ef15fd3272720a4577cae"
-    sha256 cellar: :any_skip_relocation, monterey:      "6010cadf93231ec03033a5248ad53ff57b099915ec8a72aa847a9a80a401257f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7018264b838135807ab0a5e062a6315cf20b8de88b8dcff3f71afa03251eb8a2"
-    sha256 cellar: :any_skip_relocation, catalina:      "58d4ab28a477688fb01b4db124c5d7deda3d7c2076e94e4c303ea1e8ab9a65c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "073d9b3b0eb9ff852a6f3f12aba6e3e2547d84e0d1257382b423a176df567299"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "96deab75f00bf9fa63e78af5b6623816b3a390c32adc0c4667099f3879938883"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5eb90095c4579d7d8616732bec7f266164f7b5f7665ef15fd3272720a4577cae"
+    sha256 cellar: :any_skip_relocation, monterey:       "6010cadf93231ec03033a5248ad53ff57b099915ec8a72aa847a9a80a401257f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7018264b838135807ab0a5e062a6315cf20b8de88b8dcff3f71afa03251eb8a2"
+    sha256 cellar: :any_skip_relocation, catalina:       "58d4ab28a477688fb01b4db124c5d7deda3d7c2076e94e4c303ea1e8ab9a65c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "073d9b3b0eb9ff852a6f3f12aba6e3e2547d84e0d1257382b423a176df567299"
   end
 
   depends_on "graphviz"
