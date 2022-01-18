312213d88b5eebfeb4dd5fd10a6c15699912de3d
Porkepix <porkepix@gmail.com>
2022-01-18 15:23:36 +0100

    feroxbuster 2.5.0
    Closes #93334.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/feroxbuster.rb
+++ Formula/feroxbuster.rb
@@ -1,8 +1,8 @@
 class Feroxbuster < Formula
   desc "Fast, simple, recursive content discovery tool written in Rust"
   homepage "https://epi052.github.io/feroxbuster"
-  url "https://github.com/epi052/feroxbuster/archive/v2.4.1.tar.gz"
-  sha256 "ada7e0633125c981cee15d4376f99985909515506c465334a445c06e80b52d6c"
+  url "https://github.com/epi052/feroxbuster/archive/v2.5.0.tar.gz"
+  sha256 "26684c4945e5a19ee44ef374f582354c520823581c9c3d9a3881cfe28b09b479"
   license "MIT"
 
   bottle do
