986cf46002deb2f4c09b67a88d9f36a681f3e7ed
Porkepix <porkepix@gmail.com>
2022-01-18 15:07:06 +0100

    dialog 1.3-20220117
    Closes #93331.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -1,8 +1,8 @@
 class Dialog < Formula
   desc "Display user-friendly message boxes from shell scripts"
   homepage "https://invisible-island.net/dialog/"
-  url "https://invisible-mirror.net/archives/dialog/dialog-1.3-20211214.tgz"
-  sha256 "cc2965e05c6a8c37070b36b56414eaa560df8d87f2cea21759e28950e9b27a4b"
+  url "https://invisible-mirror.net/archives/dialog/dialog-1.3-20220117.tgz"
+  sha256 "754cb6bf7dc6a9ac5c1f80c13caa4d976e30a5a6e8b46f17b3bb9b080c31041f"
   license "LGPL-2.1-or-later"
 
   livecheck do
