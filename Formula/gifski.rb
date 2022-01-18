5927dd5573e461d84fca503ebd4bc49adc2ef176
Victor Song <vms2@rice.edu>
2022-01-17 23:32:24 -0600

    gifski: explicitly depend on `ffmpeg@4`

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -4,6 +4,7 @@ class Gifski < Formula
   url "https://github.com/ImageOptim/gifski/archive/1.6.4.tar.gz"
   sha256 "1bcc1bdbfdb206f44de75662ee5a8a8c61d336c04cb62457e02c9fa8456f3928"
   license "AGPL-3.0-only"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "e05fd57ae2a3248640a61aaf0670e046406c96692739cb12be5c9e698cfa45de"
@@ -16,7 +17,7 @@ class Gifski < Formula
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
 
   uses_from_macos "llvm" => :build
 
