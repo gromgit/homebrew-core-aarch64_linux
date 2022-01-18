8ca63b4bdab4792d14fc5aced7c50c4f8363cc48
Mark Dain <mark@markdain.net>
2022-01-18 18:05:07 +0000

    aerc 0.7.1
    Closes #93362.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -1,8 +1,8 @@
 class Aerc < Formula
   desc "Email client that runs in your terminal"
   homepage "https://aerc-mail.org/"
-  url "https://git.sr.ht/~sircmpwn/aerc/archive/0.5.2.tar.gz"
-  sha256 "dec6560c1359d1d56124a85692e877e319036f0312ce9b7a31f9828f99b92c61"
+  url "https://git.sr.ht/~rjarry/aerc/archive/0.7.1.tar.gz"
+  sha256 "e149236623c103c8526b1f872b4e630e67f15be98ac604c0ea0186054dbef0cc"
   license "MIT"
 
   bottle do
