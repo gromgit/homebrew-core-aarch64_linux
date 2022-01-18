1ccc46fc1de4bbdf1ef2b1c9a3d69eef6a90140c
Rui Chen <rui@chenrui.dev>
2022-01-18 10:10:02 -0500

    dssim 3.1.2
    Closes #93342.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -1,8 +1,8 @@
 class Dssim < Formula
   desc "RGBA Structural Similarity Rust implementation"
   homepage "https://github.com/kornelski/dssim"
-  url "https://github.com/kornelski/dssim/archive/3.1.0.tar.gz"
-  sha256 "32ee74086a743eaf9dfde8b3cf83bd7165c06adc563d32541341b0cf79b2e58d"
+  url "https://github.com/kornelski/dssim/archive/3.1.2.tar.gz"
+  sha256 "464bc639bb0551435e606841db79fa97e044695f7c1062caf07dd3713dc2a09f"
   license "AGPL-3.0-or-later"
 
   bottle do
@@ -18,6 +18,11 @@ class Dssim < Formula
   depends_on "nasm" => :build
   depends_on "rust" => :build
 
+  # build patch, commit ref,
+  # https://github.com/kornelski/dssim/commit/5039fa8c96d4a0ceac207968b3ef15819822cf54
+  # remove in next release
+  patch :DATA
+
   def install
     system "cargo", "install", *std_cargo_args
   end
@@ -26,3 +31,18 @@ class Dssim < Formula
     system "#{bin}/dssim", test_fixtures("test.png"), test_fixtures("test.png")
   end
 end
+
+__END__
+diff --git a/src/lib.rs b/src/lib.rs
+index c04be07..319dc7f 100644
+--- a/src/lib.rs
++++ b/src/lib.rs
+@@ -8,7 +8,7 @@ use load_image::*;
+ use std::path::Path;
+
+ fn load(attr: &Dssim, path: &Path) -> Result<DssimImage<f32>, lodepng::Error> {
+-    let img = load_image::load_path(path, false)?;
++    let img = load_image::load_path(path)?;
+     Ok(match img.bitmap {
+         ImageData::RGB8(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgblu(), img.width, img.height)),
+         ImageData::RGB16(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgblu(), img.width, img.height)),
