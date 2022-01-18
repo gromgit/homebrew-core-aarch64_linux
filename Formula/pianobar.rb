311f94c7000e1cb7ac7695e9cf9b4964c26ae6fa
Victor Song <vms2@rice.edu>
2022-01-18 00:41:00 -0600

    pianobar: explicitly depend on `ffmpeg@4`

--- Formula/pianobar.rb
+++ Formula/pianobar.rb
@@ -4,7 +4,7 @@ class Pianobar < Formula
   url "https://6xq.net/pianobar/pianobar-2020.11.28.tar.bz2"
   sha256 "653bfb96b548259e3ac360752f66fdb77e8e220312e52a43c652f7eb96e7d4fe"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/PromyLOPh/pianobar.git", branch: "master"
 
   bottle do
@@ -18,12 +18,14 @@ class Pianobar < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
   depends_on "json-c"
   depends_on "libao"
   depends_on "libgcrypt"
 
   uses_from_macos "curl"
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   def install
     # Discard Homebrew's CFLAGS as Pianobar reportedly doesn't like them
