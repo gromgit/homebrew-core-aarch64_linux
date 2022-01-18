a5b5032747b0ede89e2d7690e05069d2cc584707
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-18 03:48:10 +0000

    librsvg: update 2.50.7 bottle.

--- Formula/librsvg.rb
+++ Formula/librsvg.rb
@@ -6,12 +6,13 @@ class Librsvg < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256                               arm64_big_sur: "373e91782be11de752c5d738a19c4b0b7631319612982fa9366e5477d4f444b2"
-    sha256                               monterey:      "3753625c61c6f0753a816bed581b6acb54be4dd8d17049dcecf52c2ed9e825bb"
-    sha256                               big_sur:       "7849cab410dfd63c3f8f635e0a0152cf34425524f728c2f01a36d7631f34688b"
-    sha256                               catalina:      "454d94e2c72567744613b69a9d879dde0a0bd9593c32929d520266b04f6b51b8"
-    sha256                               mojave:        "312d0f2fade5525cfe58f0c0e16cccc1e9c73925bb2fc8f1c320d3a0f8a3f4fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48c4693a1f62fc8c870f3831867d9f082d0f9f42cb3151f0be77733b4c3e7ac4"
+    sha256                               arm64_monterey: "fae0a16eb6cd51db5ae7dd74a5a51054f83567cce821ece3dc5aeb620a8df787"
+    sha256                               arm64_big_sur:  "373e91782be11de752c5d738a19c4b0b7631319612982fa9366e5477d4f444b2"
+    sha256                               monterey:       "3753625c61c6f0753a816bed581b6acb54be4dd8d17049dcecf52c2ed9e825bb"
+    sha256                               big_sur:        "7849cab410dfd63c3f8f635e0a0152cf34425524f728c2f01a36d7631f34688b"
+    sha256                               catalina:       "454d94e2c72567744613b69a9d879dde0a0bd9593c32929d520266b04f6b51b8"
+    sha256                               mojave:         "312d0f2fade5525cfe58f0c0e16cccc1e9c73925bb2fc8f1c320d3a0f8a3f4fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48c4693a1f62fc8c870f3831867d9f082d0f9f42cb3151f0be77733b4c3e7ac4"
   end
 
   depends_on "gobject-introspection" => :build
