c4be1bd8e7990bf26bd3f8efce38c10207156f94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-18 23:06:04 +0000

    mysql@5.7: update 5.7.37 bottle.

--- Formula/mysql@5.7.rb
+++ Formula/mysql@5.7.rb
@@ -11,13 +11,12 @@ class MysqlAT57 < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "db78d3c777c2712cf651550932d8cfbc6a0d75557ffe292272201a760c81df62"
-    sha256 arm64_big_sur:  "e23df0f6e8bfc83fe2e982ec60e89be6ca069d35f96981ea6073591563bf7917"
-    sha256 monterey:       "5289b66475399406bf4d55e0c853eb1ed44c0c2804d58cc95a658c3f3e27e888"
-    sha256 big_sur:        "b85e04576a333c835f3a0e7f21b565b5a374628d6011226e04523c4528fb158a"
-    sha256 catalina:       "fe60bdb3fa8f8c35070243c865d3e7b7eacad528430683d91fb2c0e4abc44829"
-    sha256 mojave:         "948c484748bc757040fdc928e4f9e2e1cd8972211f7b07b28aad2dd9406701cf"
-    sha256 x86_64_linux:   "b6ca44f90f74bf354f5c552f81306d8a3b989fe28de3d76b539cc67e4dcd8e82"
+    sha256 arm64_monterey: "7ad25346d102c0ff244b0d0d944bdbfc0b13371e3456d1d89c60bd2eeaa36a91"
+    sha256 arm64_big_sur:  "113e288f22bd3c2101ad5f50e00fc2cd951f6e01120e844bbbd0c0cb54dc20d8"
+    sha256 monterey:       "535a7735816733f35d4bce45bc889acd45cdf3935491480425f261f2ac50e300"
+    sha256 big_sur:        "1e84b43b06d67220f37a96ffbd93b6785661aa7c3939df386eb394d45a24cc29"
+    sha256 catalina:       "c32552d92c733aee42cd43d5de752edc423079fe8aeeed7de781f99e664ef6ad"
+    sha256 x86_64_linux:   "8d0234fbe1065f899061768af56e54d24c7c0c0fe83be5c1299728898491f597"
   end
 
   keg_only :versioned_formula
