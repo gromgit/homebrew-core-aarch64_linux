eb85a86c017e51d2011b0fc0151fb65c2d6c7e17
Rui Chen <rui@chenrui.dev>
2022-01-18 10:00:09 -0500

    serverless 2.72.0
    Closes #93340.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://www.serverless.com/"
-  url "https://github.com/serverless/serverless/archive/v2.71.0.tar.gz"
-  sha256 "91486d996029f3e5f7871521cab23b3f33a1685ac8ceaf11cfddae6939a14475"
+  url "https://github.com/serverless/serverless/archive/v2.72.0.tar.gz"
+  sha256 "f226a562ff22c3f181e58587d373a1aec8177c31f1653e55d0799059bd0a1782"
   license "MIT"
   head "https://github.com/serverless/serverless.git", branch: "master"
 
