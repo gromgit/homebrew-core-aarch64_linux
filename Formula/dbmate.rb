5f28f7add7e45b07c8d823d52ee688a6cfdc9b04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-18 19:05:22 +0000

    dbmate: update 1.13.0 bottle.

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -7,13 +7,12 @@ class Dbmate < Formula
   head "https://github.com/amacneil/dbmate.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2cd5ad3a25f80d2d2f42baa1b230180f8ff223eb782fd303834e41597427266c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a7e44584e98abac0f1ee3724238308cc320de4d80ef8bc161727392c948a1b32"
-    sha256 cellar: :any_skip_relocation, monterey:       "4e2928199d940bfc6d5ce94683df56caab8874ee026bef65ed794dabe224261e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "24765d6c4a62dbe0da38fadece43cd5370496187b1c0cc8f46c385155aad2d27"
-    sha256 cellar: :any_skip_relocation, catalina:       "20c977cf5fd8c4770eed868e1143b2b4faa1897f94628fb325a535b62745912c"
-    sha256 cellar: :any_skip_relocation, mojave:         "daabcc8391777571779235dd6bb86a04b8a8505ca64efd3c8c8c4aa455faa925"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c9044a1ed9432cd4f370d60a726dbb0282d3f6c3d5014d4eae5d6d7d7ecef2c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aef7f9ddb8d783becbafc4ca64acd73f5d46de964d74d7c8b422b9800c822e2e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b2d361283fadb11ec3f761fcc9cf9cc73ab6dd2eaeff1b25e1f4bfd6d827945"
+    sha256 cellar: :any_skip_relocation, monterey:       "93f662bbabd6cb2d5defba87e4b8361130ee295cbfdfd304b3afdd6ac440eb16"
+    sha256 cellar: :any_skip_relocation, big_sur:        "35733f4892d6f58a6eb2c6d05bfd0a4785f2fd7f12eb52dfeebc17cc6bb3c7f2"
+    sha256 cellar: :any_skip_relocation, catalina:       "d6787f44ed02e998412e4a6127c6b7693f26f62dee3a8211d542b54e18352e23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "44899cedd8540d78c9d962ef8df1e60c6b4a4e6ecd5ac03bf623e0986aa50cd2"
   end
 
   depends_on "go" => :build
