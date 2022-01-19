27446071036c97ecfea26ff3f1a5e889f7a024f9
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-01-19 08:02:58 +0000

    elixir-ls: add 0.9.0 bottle.

--- Formula/elixir-ls.rb
+++ Formula/elixir-ls.rb
@@ -5,6 +5,15 @@ class ElixirLs < Formula
   sha256 "eaa2f5fd4ab9e497889e25419e4409cfaf808daca06259718668a71de8616e3f"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "58db48a0b346fa46459a1ce9f218a7d6ff0dc04dd04cd036e9f22e7790d5e265"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7741ff3f47a66c229add6d8cb41fe36743323d34ebcbf86927555530fa7abf9"
+    sha256 cellar: :any_skip_relocation, monterey:       "1465b0c582eb53a898cfdc107f9569b09905cf85060a82739124ac30bc0d4472"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c93d53ef7e88d9c4718f2cf968bd45dd71585c8dc585a3c0f3fee588f3e8c0fa"
+    sha256 cellar: :any_skip_relocation, catalina:       "cb7117dbc37b193f3e41b85d06b780c72cb2ed4e589d51096ada2402d8469eec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ca791a9e0f4e50fb5eb8b52aaf15965fb9f85fe9d98b7036f918a91762d1dc5"
+  end
+
   depends_on "elixir"
 
   def install
