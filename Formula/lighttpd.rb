838d8840f5fb1efc7b6b47e99c12ba47f5c3352a
Nanda H Krishna <me@nandahkrishna.com>
2022-01-19 15:11:14 -0500

    lighttpd: update `livecheck` URL

--- Formula/lighttpd.rb
+++ Formula/lighttpd.rb
@@ -6,7 +6,7 @@ class Lighttpd < Formula
   license "BSD-3-Clause"
 
   livecheck do
-    url "https://download.lighttpd.net/lighttpd/releases-1.4.x/"
+    url "https://www.lighttpd.net/download/"
     regex(/href=.*?lighttpd[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
