191726dd6f8bc36b552d93374f4cfce925355ed4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-19 08:38:10 +0000

    anjuta: update 3.34.0_4 bottle.

--- Formula/anjuta.rb
+++ Formula/anjuta.rb
@@ -7,10 +7,12 @@ class Anjuta < Formula
   revision 4
 
   bottle do
-    sha256 arm64_big_sur: "185ac50d99816b00213f7e3a6430c06dcef89408d92b0b8285772789ed600dde"
-    sha256 big_sur:       "cb89537f1f0f79d74b348604fdf02a0d8c7e48a8b9211aade1a18e2d4eb1d70b"
-    sha256 catalina:      "2b2f88450c12c599e2c730bafabd678006b75ab74eee017743ba9a34338e1f3c"
-    sha256 mojave:        "1c63382333afdfbcb3cc0c9b2c75f2dff445bbdc749464252067ab707dab7e85"
+    sha256 arm64_monterey: "f94220711948d150b55bd7fb45b7d0ed6b22074cce634196cefe563280eda75c"
+    sha256 arm64_big_sur:  "185ac50d99816b00213f7e3a6430c06dcef89408d92b0b8285772789ed600dde"
+    sha256 monterey:       "896ca7644a4dd62f90461343097932c1b64974def062add7296c5f265f6bfdb0"
+    sha256 big_sur:        "cb89537f1f0f79d74b348604fdf02a0d8c7e48a8b9211aade1a18e2d4eb1d70b"
+    sha256 catalina:       "2b2f88450c12c599e2c730bafabd678006b75ab74eee017743ba9a34338e1f3c"
+    sha256 mojave:         "1c63382333afdfbcb3cc0c9b2c75f2dff445bbdc749464252067ab707dab7e85"
   end
 
   depends_on "intltool" => :build
