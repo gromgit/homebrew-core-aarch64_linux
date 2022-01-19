91e2b949725807c47fda688f3d6117018256b033
Porkepix <porkepix@gmail.com>
2022-01-19 19:12:14 +0100

    cimg 3.0.2
    Closes #93409.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "https://cimg.eu/"
-  url "https://cimg.eu/files/CImg_3.0.1.zip"
-  sha256 "cb637d69a4dca6f0280ca64bfc53c3e56a75220fb59bcea44eacc6cbae574002"
+  url "https://cimg.eu/files/CImg_3.0.2.zip"
+  sha256 "ee55a37c33d503a64ff264b53952e502ba7c2887b59ded47c47c86ea52ac5c31"
   license "CECILL-2.0"
 
   livecheck do
