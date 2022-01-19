6d09e3c42b894a1772b7cd76abd5adac9e6f54f8
Porkepix <porkepix@gmail.com>
2022-01-19 03:52:31 +0100

    tctl 1.14.3
    Closes #93391.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tctl.rb
+++ Formula/tctl.rb
@@ -1,8 +1,8 @@
 class Tctl < Formula
   desc "Temporal CLI (tctl)"
   homepage "https://temporal.io/"
-  url "https://github.com/temporalio/temporal/archive/v1.14.2.tar.gz"
-  sha256 "bdb37931fa16867d9b3f5af50979369e9211b2bd95de42ed1d908531296edbaf"
+  url "https://github.com/temporalio/temporal/archive/v1.14.3.tar.gz"
+  sha256 "00b6404be565f6c9886efcbcd1796b5ba8ac3199cf12fb9228dbd798bf2ea7d0"
   license "MIT"
 
   bottle do
