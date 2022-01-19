9bfe8b73c50727bcd3b569bfdfa94793908c947c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-19 20:05:43 +0000

    nef: update 0.7.1 bottle.

--- Formula/nef.rb
+++ Formula/nef.rb
@@ -6,11 +6,10 @@ class Nef < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b18b03102e950a93c946b0a92638a22e114e6cf56829e6249acb7a465b0ce0a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ebc41b222f6ec924c8e9cfa96436b35499ef16002169d27d01755655a06ecc4e"
-    sha256 cellar: :any_skip_relocation, monterey:       "8a6cc8c5520d6d300cff4e86ab5076fc628e588c897abd65aab85fad50cf2460"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ce01b5d542de2cc0dde3b16ae7250d238b7a06ef5139c63458dea8918a63c170"
-    sha256 cellar: :any_skip_relocation, catalina:       "770c78fd20020f2a5fa6fc30759d55af95e7aabab81f91680b6113917bdc2457"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8918c48c922141c187e2271884864118e01b8cc821d53d3bf82f25ed61cf6075"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b8453d3a8cb3b1cdcc4c042f63efd772a70b2e28f822faca6adf710688f7cf9b"
+    sha256 cellar: :any_skip_relocation, monterey:       "8841fde2a11375a65c32ac4e8c88dfc44f64935921a71fa546026fb40e8acef1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4a80e27e8474a6100f79b2845121660f3fec14e1f9f90a09b12f5b9fc804b5ef"
   end
 
   depends_on :macos
