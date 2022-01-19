bfae0f91eec191864db70865277d0c3af11ca38e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-19 22:05:52 +0000

    keydb: update 6.2.2 bottle.

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -6,12 +6,12 @@ class Keydb < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3edc5f6fff1387faccc130742a7e9f6b63362587e735eb847ed028022a12e951"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "17eac0584f9f378ef65a36a6b99a573f0c9c47278c2bd90702e9c5613b20d906"
-    sha256 cellar: :any_skip_relocation, monterey:       "d11afa68b82e1377f2d35ae5b3b2d4d32d23556530e5cf696d1146ca5b7b1cd6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dc79925e9e1269a7f9299fb63b594c04fd93e700b17b3c4d947e9e4e31315ce7"
-    sha256 cellar: :any_skip_relocation, catalina:       "0e2e47bc09b13d17c672bc3002ab41838723d310c5bfa23a277f51d63d2df925"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c02b4344bff06c19347219de736dd85953c18d14d5205e2d44b4bceff77393a3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6d9c8d5d77a71cade23c85b2258f3b0fb74e2bfd3c58ea9fd5aadfbb0ea1778"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ae26fdb9109fc9960a31aa4ac9e290846ad22bd24f27abbe89777b2f3f915a6"
+    sha256 cellar: :any_skip_relocation, monterey:       "040fe3beb451c44631ee4e6b016c3db0e28a3aafffb88813392ef32eb139aac7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "348803202838f745a4bb966eca19a7610f2530e72b4b61a2457d082b810c5aa6"
+    sha256 cellar: :any_skip_relocation, catalina:       "355c9f6fd3854b0a45413af336a8c81f91207b1405643b8aa3f0db1f9888291c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "535b7f2c5d08ca981e8a20fdf264c0fa47c9a1410c1811b36d62dac43e29d852"
   end
 
   uses_from_macos "curl"
