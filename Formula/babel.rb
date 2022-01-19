b478fd644ff78527c322f4bb58e20ef9ae69b674
Bevan Kay <email@bevankay.me>
2022-01-20 08:59:39 +1100

    babel 7.16.10
    * babel 7.16.10
    * Bump babel-cli resource
    
    Closes #93429.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -4,8 +4,8 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/core/-/core-7.16.7.tgz"
-  sha256 "2bf08c9f7984e419ad7445f797d6bab31dc86cca6d599b8a34ecdddaca172065"
+  url "https://registry.npmjs.org/@babel/core/-/core-7.16.10.tgz"
+  sha256 "4538e75b9b8d0df62ca249504b019f612c2143494deb0c7eae5c17a5a6cc7f5c"
   license "MIT"
 
   bottle do
@@ -15,8 +15,8 @@ class Babel < Formula
   depends_on "node"
 
   resource "babel-cli" do
-    url "https://registry.npmjs.org/@babel/cli/-/cli-7.16.7.tgz"
-    sha256 "c44e6c63d64c6d7874231e7c0305fe871fb5de9502fbbf472362f69feb91f406"
+    url "https://registry.npmjs.org/@babel/cli/-/cli-7.16.8.tgz"
+    sha256 "beca3f6f5d5ed98a69eecdfd10e457f482588ea72c5ccdf98a81e5d8cc848b81"
   end
 
   def install
