a0c27facbdd19843e1027bcf7c733b41a677febf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-19 09:10:35 +0000

    frugal: update 3.14.13 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -6,12 +6,12 @@ class Frugal < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "43dfcc13ee052a311a5c0bc52e804c8f8daa7ea64e85986515ca984b3600d97d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "43dfcc13ee052a311a5c0bc52e804c8f8daa7ea64e85986515ca984b3600d97d"
-    sha256 cellar: :any_skip_relocation, monterey:       "6d0fc585e502beaac8094e45f4eaf2d31c57f1d82fd0a1d62d407af4fdf7e776"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6d0fc585e502beaac8094e45f4eaf2d31c57f1d82fd0a1d62d407af4fdf7e776"
-    sha256 cellar: :any_skip_relocation, catalina:       "6d0fc585e502beaac8094e45f4eaf2d31c57f1d82fd0a1d62d407af4fdf7e776"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cd9aed82da2d4f6a67c33660628205613a9b6a6fbaa684bae44e572f300cf103"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7f70b5f7f18736c91b28298f14fc3a1a44ff73b4c14b5a9a61ceadecda740be6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f70b5f7f18736c91b28298f14fc3a1a44ff73b4c14b5a9a61ceadecda740be6"
+    sha256 cellar: :any_skip_relocation, monterey:       "f7a4adab8c0f86be813998450c0ee096730a70c265cd61a84f7d217203beb82d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7a4adab8c0f86be813998450c0ee096730a70c265cd61a84f7d217203beb82d"
+    sha256 cellar: :any_skip_relocation, catalina:       "f7a4adab8c0f86be813998450c0ee096730a70c265cd61a84f7d217203beb82d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61d37b71b2512786dd9b7d588c75174aa5a9f156fa065ad704d46d875d7593dd"
   end
 
   depends_on "go" => :build
