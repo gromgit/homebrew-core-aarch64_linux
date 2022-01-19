ae527f26cf6ee5ed6cdb29edfe3dee38e6d05d2d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-19 10:38:35 +0000

    juliaup: add 1.5.28 bottle.

--- Formula/juliaup.rb
+++ Formula/juliaup.rb
@@ -6,6 +6,15 @@ class Juliaup < Formula
   license "MIT"
   head "https://github.com/JuliaLang/juliaup.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "abdd9330ba64268bc423074e7eb2590217d0fd6c7b6d2a8e8c02f57173e3f08a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc4e55e31e154e154b51c04a0cc9569238d72eb1cbabc2c1d0d93327dba79d1f"
+    sha256 cellar: :any_skip_relocation, monterey:       "0a83fc481ff486512f68274ba1d4b1e32d51884fe9bfb00c69c8be6a7e7e1715"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bae0cbf1754d380b7d0f594671689ef808dcb57c1221e58f134d9e1b33712631"
+    sha256 cellar: :any_skip_relocation, catalina:       "94046034cb965550392a3ac37bdc03cf66d071c1847fdb022b6fbcb653261aaa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b35b2e8360ba2cb813f0f5359cbe2472d8a5c22a2ae106a38c84732743d86fac"
+  end
+
   depends_on "rust" => :build
 
   def install
