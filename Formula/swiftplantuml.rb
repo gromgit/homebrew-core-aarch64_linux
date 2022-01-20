392148a8a41aca9d5e3c7863f18ecc12efc8d5bb
Porkepix <porkepix@gmail.com>
2022-01-20 13:25:01 +0100

    swiftplantuml 0.5.2
    Closes #93462.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swiftplantuml.rb
+++ Formula/swiftplantuml.rb
@@ -1,8 +1,8 @@
 class Swiftplantuml < Formula
   desc "Generate UML class diagrams from Swift sources"
   homepage "https://github.com/MarcoEidinger/SwiftPlantUML"
-  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.5.1.tar.gz"
-  sha256 "9bdfb08563907d44a45b628f75594768e4ed96d065f145424cb97a99ea0b629c"
+  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.5.2.tar.gz"
+  sha256 "3c785f7f4095d40be9749e0bb717268a51015e4be0a44dc81e5d3e01902dbf7b"
   license "MIT"
 
   bottle do
