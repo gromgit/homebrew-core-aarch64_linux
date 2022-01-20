95904f8473ba3bb643e28de2cf85029b7919c58b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-20 08:53:21 +0000

    libadwaita: add 1.0.1 bottle.

--- Formula/libadwaita.rb
+++ Formula/libadwaita.rb
@@ -5,6 +5,15 @@ class Libadwaita < Formula
   sha256 "bb49cf5a09d2e8bc144946c2c3272aecd611667fd027f3808b95d7101ed473d6"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 arm64_monterey: "4912d1df5884d1ac12df98026bf8b9e3bc77493ee682e3852fad7cc1467110e4"
+    sha256 arm64_big_sur:  "e356fe20ec6553d912ca7cafd2ab865d3d5f37aaebdc3d88d66258a16ce8224b"
+    sha256 monterey:       "aeea3cb18115883487efea75fc0f8318eb6b7272f0815168256c7516d3f8308c"
+    sha256 big_sur:        "57cfa283583385ee480fc3cf5b89e2245cab7f4d590ced36929dc7ec4d74720c"
+    sha256 catalina:       "c432dd1122cc382263cf78ee3d7db5f7f3cff57b8bb50b7c184ced2ac1a147a9"
+    sha256 x86_64_linux:   "3a35ef42ae77db6761be7e0b70930388a03cd9c1605e1b2a89dc0d764c7619f6"
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
