7501a1b1288b73399e0697035dac4c264f5f48fb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-20 02:46:59 +0000

    aravis: update 0.8.20 bottle.

--- Formula/aravis.rb
+++ Formula/aravis.rb
@@ -6,10 +6,11 @@ class Aravis < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "aee90cd98142cf3b96c0e0bad4fe8c12610c4f41ead1c0443596bc88c3acec56"
-    sha256 monterey:      "364f6d689990e6cae629e1a01bd1d0689f4fe0d6b67e1de6cc370ca449e47928"
-    sha256 big_sur:       "8ed3f87980bb7d8974008f3523b5904478d66b1efc17f331465f3123b2ba637f"
-    sha256 catalina:      "bb1e49d6cb99b95531097fcc9ed030a509f170a007f3e247ccf8ad26d9f4d593"
+    sha256 arm64_monterey: "2208b800f086da816639d748248453eddcb2d7f85b311cb05a96ed611f848e56"
+    sha256 arm64_big_sur:  "aee90cd98142cf3b96c0e0bad4fe8c12610c4f41ead1c0443596bc88c3acec56"
+    sha256 monterey:       "364f6d689990e6cae629e1a01bd1d0689f4fe0d6b67e1de6cc370ca449e47928"
+    sha256 big_sur:        "8ed3f87980bb7d8974008f3523b5904478d66b1efc17f331465f3123b2ba637f"
+    sha256 catalina:       "bb1e49d6cb99b95531097fcc9ed030a509f170a007f3e247ccf8ad26d9f4d593"
   end
 
   depends_on "gobject-introspection" => :build
