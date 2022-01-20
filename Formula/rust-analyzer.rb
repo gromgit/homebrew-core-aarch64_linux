379bd13b86d2c15c50bfef0763366edd841d2d6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-20 07:06:04 +0000

    rust-analyzer: update 2022-01-17 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -8,12 +8,12 @@ class RustAnalyzer < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9fe0507d3902db1e52dbe3961ce6afe1bb956079c6c6e0126cbcd73e2b33c854"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "035c745a235edc8e671a18cd2a4070bc283f97bb97a2a7c22047de2c1aa879c1"
-    sha256 cellar: :any_skip_relocation, monterey:       "d421b3dfef0f90e5b62002da2053a4852715587241ee824424493b95ed7e08d9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c879006f8efaa92d000416978408c27d9c0554c96927341ceb6d4db38ff43cd9"
-    sha256 cellar: :any_skip_relocation, catalina:       "7043f497e728369333c10395a4d10c82062631466b6621e8cc1d59c6473fee04"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e7203f02dc407471dc529e666e1d257b2a6c7dcfca9dd420f1bab62089a42c4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2323676e678222417fb0f9f65bafc00e7a2f73f978d4bff1d93e54140c3a49fc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f4c892dbed2103b1ae8d0ab66403515b0c9dba28f294c66447726be411f553b"
+    sha256 cellar: :any_skip_relocation, monterey:       "6f1f089003642e8fc8c22cd13f5a8d40887047ff5cdf06173ec2b8c7240a8fc0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c56836dbadcf962e94de2e13a45b552c9e0da135b50f67f855f6802023447424"
+    sha256 cellar: :any_skip_relocation, catalina:       "2f5de807124379fc3d47e2b204b4e9cee64775385c17812fd54c918fd739a32d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40b86683777ddc550ba0c8ca713fe0c11ce1c65deb18cbf1a94717a9d9be0f73"
   end
 
   depends_on "rust" => :build
