a99ed76df2d99b5fc256a3aae08ba7125edc842c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-20 13:11:52 +0000

    mrtg: update 2.17.10 bottle.

--- Formula/mrtg.rb
+++ Formula/mrtg.rb
@@ -10,13 +10,12 @@ class Mrtg < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "581c14076645c5902c08c129f76c83cc864ce675f13befe35c31b22c31924735"
-    sha256 cellar: :any,                 arm64_big_sur:  "d18d7f28aefcf8a4d927191ea39d7e7180ef0fd6f9cbfa8f78a7c479ae180225"
-    sha256 cellar: :any,                 monterey:       "0a675103885fcee03abc942649d4caad174c9df19c0e6ed5bffcc568878a51a0"
-    sha256 cellar: :any,                 big_sur:        "07e807ddaf2a41cefd8511bf441c403da9062eaaa4978628d99ee82fc9204947"
-    sha256 cellar: :any,                 catalina:       "16df660ec359b69b2459c924b53531e858d5cbf07ae2d182c3d8af150c62274d"
-    sha256 cellar: :any,                 mojave:         "f2d1b8898e2cdda752a6456bed569a37a61c48b6d3f881ecc9a8e8ec2543257a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "110cdf1481f558df5b6b2438b5087ff0e13d07ee2a287c2bd29617dc431a0843"
+    sha256 cellar: :any,                 arm64_monterey: "df8611100c34ebb4c553b81493006f954fca61669b4a6331914529bce73a6348"
+    sha256 cellar: :any,                 arm64_big_sur:  "fedc3e50c0a75c2ae6e719a1ef5502ce38efdba9e51d0f9201d2ad02d5c0a1db"
+    sha256 cellar: :any,                 monterey:       "47c8ae5d5466514d50393ec5f48219313a2a9b7b1544b08dd923bf1e5642762e"
+    sha256 cellar: :any,                 big_sur:        "28151b3e97ec16b70623caab573a79eddbb2e86ab8c25812e3339ac9612c38f5"
+    sha256 cellar: :any,                 catalina:       "a9cf00745f42b6db7026d3954948319f2528526099a6aa7386c63224952f1732"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25d13a8441a4958962e6812e1811c529021505443bf4fb5cde24214f8cc1332a"
   end
 
   depends_on "gd"
