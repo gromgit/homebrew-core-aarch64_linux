895464a904a1c4b271d97e76803441efee4492d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-20 12:06:13 +0000

    freeipmi: update 1.6.9 bottle.

--- Formula/freeipmi.rb
+++ Formula/freeipmi.rb
@@ -7,12 +7,11 @@ class Freeipmi < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "92acf4bfe30602ff62420cb4b2c6824066553410bf2fd4fae5986f5e28bf178c"
-    sha256 arm64_big_sur:  "961064dca3e312d23150cb3b1fee87ef5903a3ea863404b04b90ae6fc3282d9a"
-    sha256 monterey:       "ce2d911b60b36dd247441befcc18553e322428c04222be0d4c1166368eb253a2"
-    sha256 big_sur:        "94211efde5f73e6eca23983c67514ca977be9ce821e4e6266a95c4423531348d"
-    sha256 catalina:       "5b6dd5d1c757fc344be9401e63ee1774658efc1eeb5486171161acafb60a5b89"
-    sha256 mojave:         "96bd9a3bde2379bfaf2b88b2de7e0e8c185c93a71f0b9cd864afd1d9d693bb77"
+    sha256 arm64_monterey: "247f80609bc18e42e708a1ed4a3009450b3eeac788fe56bae0bb1a95fb220545"
+    sha256 arm64_big_sur:  "c10f92144bc24391fa7b0c35cc3ef2cea428c7feba13a6d66ac9a46b1f67b874"
+    sha256 monterey:       "e5ea92942763199ad1e53b7ebf95418466ff707e851e10914c2ed9a490e2f950"
+    sha256 big_sur:        "961dddefca891df95335bffcca5e6bdc7c6e906ea6ecf77f48be8fa14d833a6d"
+    sha256 catalina:       "5d14764fbabda846afaf0ea7d7cd662aaadded927ff71961367a7ca139bbcefa"
   end
 
   depends_on "argp-standalone"
