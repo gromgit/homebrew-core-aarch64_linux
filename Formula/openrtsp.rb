6ab6d58ff592dc99913e385eda55a5f90ba98926
Porkepix <porkepix@gmail.com>
2022-01-21 12:26:46 +0100

    openrtsp 2022.01.21
    Closes #93531.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openrtsp.rb
+++ Formula/openrtsp.rb
@@ -1,10 +1,10 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "http://www.live555.com/liveMedia/public/live.2022.01.11.tar.gz"
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2022.01.11.tar.gz"
+  url "http://www.live555.com/liveMedia/public/live.2022.01.21.tar.gz"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2022.01.21.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  sha256 "3c72cf04ae80655e9d566f18114a01b9a5f12fb4123350286922e03a09af37ec"
+  sha256 "762579c142db3aba8c4430b223d36355a47a2546d897d2961d496f03f8e3a90e"
   license "LGPL-3.0-or-later"
 
   livecheck do
