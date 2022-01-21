a715b318129be430a44f431cd7a8970f2d3a967b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-21 13:07:43 +0000

    pgweb: update 0.11.10 bottle.

--- Formula/pgweb.rb
+++ Formula/pgweb.rb
@@ -6,13 +6,12 @@ class Pgweb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9119c191615e7b656cd56561be91fa6491205df19a7f744c6a206946bd6916df"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8850d3804355efc5d38091e65d3f534526dfb1d054948f7204ebeafabf6cbe67"
-    sha256 cellar: :any_skip_relocation, monterey:       "52c113300ac67f9baa714f347a32cf5f6b744527d307fa3a3c975d85ca4c7e20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ec49476c0f86d53843e63e7d0d19bce5a7f8db471bba1d050d0650bea83f8e21"
-    sha256 cellar: :any_skip_relocation, catalina:       "973ebc01c360e25e1b88e274e93e6a948390eeaadd669901916aaffb91888d5f"
-    sha256 cellar: :any_skip_relocation, mojave:         "94a5ede934e422a47e9ba408b6dd5ff7182ff4b69301420b220f4ec6fffdccfb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2fcde820a38532a5a5371ff9a390841b9013ba5a40e814002092a40061c98b78"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac959c59a21786dd20a6ea036f44ee40da7c3fa1ab73ae21aa1f544241947daf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40e6e5cb7feda53342820becfc49eeb7ae1a60232a2a10a1e70d9888ec7702a2"
+    sha256 cellar: :any_skip_relocation, monterey:       "264a47da7f6c0bf2d6840f5330bf3c165814c5a0de20b532c56c9f4f3e5472c6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fb450b8dd4d43d8d5bf7d89d290518acdc7e5d2fac618014ac75c6a4fcdceec4"
+    sha256 cellar: :any_skip_relocation, catalina:       "4ca29fcdcc1d452f75d0382056367ead92063c5631f238a860d96f507bf133aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "df7f70d589c08e5245e25ea2741a833354aa562a0ce45090596e952a70d51d55"
   end
 
   depends_on "go" => :build
