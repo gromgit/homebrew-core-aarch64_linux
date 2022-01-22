e0ef32cbdf5b0191319b43f4fa7732ea46e2fc34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-22 02:16:42 +0000

    swiftlint: update 0.46.1 bottle.

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -8,10 +8,10 @@ class Swiftlint < Formula
   head "https://github.com/realm/SwiftLint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2c5ee554b70d936ad2e50743071abc18cd2afc9daecaa279b92502954b2c5bf5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "280e4a16fd6c18252cb2ee7232021b45b8fba6393ba2e3f74cf62f435b76baef"
-    sha256 cellar: :any_skip_relocation, monterey:       "977afd519f518fbf9e5d145e0938f17261c5dcf34bd838bc4677715e53a1d383"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ce2f1f3f19922e723ce89f72a7da8dfce4399b6fca787025122c353207ce3a66"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9db36183ea18b36a94add8be40a93a2d555afdfa2e011dd17eb354455a4743ba"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4345d39ede1a8aee254ec2c94bfa15c823437069d606fc62837f10c994c21eb"
+    sha256 cellar: :any_skip_relocation, monterey:       "0efebf1269594c8d62c6b5d884b4bc786bfe28dbf33340564fe44952bbc131f3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eba1dd7b36b7e11c09ffcca0387228414d472ee77a225c117dc06487eb1fdc55"
   end
 
   depends_on xcode: ["12.5", :build]
