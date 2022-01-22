eec80b5192ef33f518514d116524be10e3ede128
owine <owine@users.noreply.github.com>
2022-01-22 15:31:12 -0600

    poppler-qt5 22.02.0
    * poppler-qt5 22.01.0
    * poppler-qt5 22.02.0

--- Formula/poppler-qt5.rb
+++ Formula/poppler-qt5.rb
@@ -1,8 +1,8 @@
 class PopplerQt5 < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
   homepage "https://poppler.freedesktop.org/"
-  url "https://poppler.freedesktop.org/poppler-21.12.0.tar.xz"
-  sha256 "acb840c2c1ec07d07e53c57c4b3a1ff3e3ee2d888d44e1e9f2f01aaf16814de7"
+  url "https://poppler.freedesktop.org/poppler-22.02.0.tar.xz"
+  sha256 "e390c8b806f6c9f0e35c8462033e0a738bb2460ebd660bdb8b6dca01556193e1"
   license "GPL-2.0-only"
   head "https://gitlab.freedesktop.org/poppler/poppler.git", branch: "master"
 
