3372bfe8e9c81412e25bbc9c8f50ba42fb933c7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-22 15:05:35 +0000

    xmake: update 2.6.3 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -7,12 +7,12 @@ class Xmake < Formula
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "64de68ec1f3b4c65054398fba3ba7f57de8ac68f2a9fcb6924f88a4a47ed37a1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "64de68ec1f3b4c65054398fba3ba7f57de8ac68f2a9fcb6924f88a4a47ed37a1"
-    sha256 cellar: :any_skip_relocation, monterey:       "3ac594add4d87340c56376ba161dd58866bd0a441b43563b248eb6773ddb4dfe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "798922e0a67b774125825507cbb6729554e8ed6b9332cbd03becf9b1c872aae0"
-    sha256 cellar: :any_skip_relocation, catalina:       "acf12d34fd511b2cb51ca71ee5eb1654c23d06743e5d3446f5a5a21b3125932f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "094af63335ec54cd5c33a5cde34d9310462ba4e2fe64789930fd79f835bf21b5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "304fbc5d5dc9f058a52e5fc34596ac2992fb8b10a599cac8cb53132ddc85f4dd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "304fbc5d5dc9f058a52e5fc34596ac2992fb8b10a599cac8cb53132ddc85f4dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "1238a11b383969d646d468eacce1e72505d3dba055825141633e8c8c33cca00a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "aaa62929287d0207c7e8e8f41002901babf920ceab9e2280f767442f9724954a"
+    sha256 cellar: :any_skip_relocation, catalina:       "974ee287dceff056a0900edc1234410f4e5eda87bb4c6a73fd3727a46e2f5436"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aaf9c99ca22dccf829afc4cce4b21c50327fc12a8bc1d17bdda465f55a8a51f5"
   end
 
   on_linux do
