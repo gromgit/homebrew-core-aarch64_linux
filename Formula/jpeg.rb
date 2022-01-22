cd84e279256fdcc9414cb2025a781cd9f1b3559b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-22 18:10:51 +0000

    jpeg: update 9e bottle.

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -12,14 +12,12 @@ class Jpeg < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ea3b46eb66c711e99ccbab7f9b821efeecbc737ab07e1104e37eafc0bdba8a5f"
-    sha256 cellar: :any,                 arm64_big_sur:  "e511143cd72a76572dabe52cef0034996b2ed24334f1b3466ba339746230e37b"
-    sha256 cellar: :any,                 monterey:       "1a67c415e4636c77057bf80bf37ea3978e4888064a8b7fd04c0cf71a24f32cee"
-    sha256 cellar: :any,                 big_sur:        "c565929a4901365a3408b57275802f943625c1e29e1b48a186edd2e97d8c0bdb"
-    sha256 cellar: :any,                 catalina:       "8f7b82a952fb3937889c7f22da1403e5338cd320495917eb26b0c5b2e614791c"
-    sha256 cellar: :any,                 mojave:         "b931e7725c83275c56f962b51b83c10f31a01ac8d823c6722edaf16d893970b1"
-    sha256 cellar: :any,                 high_sierra:    "64286932634fbe1e0d07eacda334d2f4967b20bce0737424df56ec5eaa34ccca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87555fde40181720eab295d1599bc75e6a80520eb2dfc8d90197c933e646b3ed"
+    sha256 cellar: :any,                 arm64_monterey: "5d4520a90181dd83b3f58b580cd3b952cacf7f7aa035d5fd7fddd98c1e6210d1"
+    sha256 cellar: :any,                 arm64_big_sur:  "27409eb75ac182025c27b4aa9c2290c40feec924cbe9edc095c754120c87bdf4"
+    sha256 cellar: :any,                 monterey:       "208af924cc7a42f53ab8ce50084eb76faadc3c1942e842484acbb2e74a54465c"
+    sha256 cellar: :any,                 big_sur:        "085e31212006e6afefc6e5141a02a06cb5bdebdbc8ca5edba50de0d95dd495fc"
+    sha256 cellar: :any,                 catalina:       "4c19f39c827ee7cdbc0f770b56c8ce553e94a5090e58da7eac3e2596b9408612"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b15c19b1cfdee81b6c3ebb96b1a743157da600030f943c9e18cbbda0612924a"
   end
 
   def install
