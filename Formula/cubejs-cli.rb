f07e82c70ee0179e06b1548f32c3b3821e243425
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-22 20:06:09 +0000

    cubejs-cli: update 0.29.22 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -8,12 +8,12 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3287d79fbb3beacb2f58c2f48cdb01e26b5a435197a109570a2928afc32787b0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3287d79fbb3beacb2f58c2f48cdb01e26b5a435197a109570a2928afc32787b0"
-    sha256 cellar: :any_skip_relocation, monterey:       "b0dbf476d6c163ef5d90d5b3c9df300cae7fcfea8a96699eb068252ec9f3517d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5ed634acbecb54536ea8645fd4ba85e13d9c369dcd1f9afc884bd91421feeb99"
-    sha256 cellar: :any_skip_relocation, catalina:       "5ed634acbecb54536ea8645fd4ba85e13d9c369dcd1f9afc884bd91421feeb99"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3287d79fbb3beacb2f58c2f48cdb01e26b5a435197a109570a2928afc32787b0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0603b447512017cae64d069bd51c6e0ea263b585b731fb93611af5f38621e04c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0603b447512017cae64d069bd51c6e0ea263b585b731fb93611af5f38621e04c"
+    sha256 cellar: :any_skip_relocation, monterey:       "6481075efad797011c0bde9a3e43a74325d872618aa9b42e402db6c4a32c20bf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6481075efad797011c0bde9a3e43a74325d872618aa9b42e402db6c4a32c20bf"
+    sha256 cellar: :any_skip_relocation, catalina:       "6481075efad797011c0bde9a3e43a74325d872618aa9b42e402db6c4a32c20bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0603b447512017cae64d069bd51c6e0ea263b585b731fb93611af5f38621e04c"
   end
 
   depends_on "node"
