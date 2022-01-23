befd2d4a533bf37da456e48dc504c9ceedd29407
Bevan Kay <email@bevankay.me>
2022-01-24 09:00:29 +1100

    parallel 20220122
    Closes #93678.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20211222.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20211222.tar.bz2"
-  sha256 "058491cf4c52a4855977bde803c63688b2ca9c5991be10e7b8b0c0aede46d19c"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20220122.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220122.tar.bz2"
+  sha256 "b8221a21412bca572ad8445b7981dfd625a3c6d48772cda468dfb5b886337e00"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
