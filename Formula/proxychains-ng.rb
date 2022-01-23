677484e11fd4307b9a88733f3f26ce91b09b390c
Porkepix <porkepix@gmail.com>
2022-01-23 18:47:06 +0100

    proxychains-ng 4.16
    * proxychains-ng 4.16
    * proxychains-ng: remove patch
    
    Closes #93666.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/proxychains-ng.rb
+++ Formula/proxychains-ng.rb
@@ -1,8 +1,8 @@
 class ProxychainsNg < Formula
   desc "Hook preloader"
   homepage "https://sourceforge.net/projects/proxychains-ng/"
-  url "https://github.com/rofl0r/proxychains-ng/archive/v4.15.tar.gz"
-  sha256 "c94edded38baa0447766f6e5d0ec1963bb27c7b55b2a78b305d6f58e171388f8"
+  url "https://github.com/rofl0r/proxychains-ng/archive/v4.16.tar.gz"
+  sha256 "5f66908044cc0c504f4a7e618ae390c9a78d108d3f713d7839e440693f43b5e7"
   license "GPL-2.0-or-later"
   head "https://github.com/rofl0r/proxychains-ng.git", branch: "master"
 
@@ -13,13 +13,6 @@ class ProxychainsNg < Formula
     sha256 big_sur:        "1672e57460c43b61c1b40b18db1015a9a2e4156d202687bb8480b76d1d987df0"
     sha256 catalina:       "afdbb2881164696d82c98f03cb24f299d06cd1cd8bc42ae2e7554215f2576a55"
     sha256 x86_64_linux:   "999f3b69f901f7f5d1604e28193a0fd585f4e4e92346e1876c25313a6c1202b8"
-  end
-
-  # Fix regression in detecting linker options, resulting in build failure for v4.15
-  # Patch included upstream, remove on next release
-  patch do
-    url "https://github.com/rofl0r/proxychains-ng/commit/7de7dd0de1ff387a627620ac3482b4cd9b3fba95.patch?full_index=1"
-    sha256 "dd38fec48f675e17207e320d6f708d7c0c747de57cdd8aafb59bbb0ab805a984"
   end
 
   def install
