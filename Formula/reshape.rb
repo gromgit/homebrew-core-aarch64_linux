ee86d4ad2fabbcfe4ac445830c42698f91bb5bdd
Rui Chen <rui@chenrui.dev>
2022-01-23 00:51:00 -0500

    reshape 0.3.1 (new formula)
    Closes #93651.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/reshape.rb
@@ -0,0 +1,38 @@
+class Reshape < Formula
+  desc "Easy-to-use, zero-downtime schema migration tool for Postgres"
+  homepage "https://github.com/fabianlindfors/reshape"
+  url "https://github.com/fabianlindfors/reshape/archive/v0.3.1.tar.gz"
+  sha256 "a8d576597cb225a94bcc3441905c9fc88a3a8a41fbdbb84ead722f6933e2e616"
+  license "MIT"
+  head "https://github.com/fabianlindfors/reshape.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"migrations/test.toml").write <<~EOS
+      [[actions]]
+      type = "create_table"
+      name = "users"
+      primary_key = ["id"]
+
+        [[actions.columns]]
+        name = "id"
+        type = "INTEGER"
+        generated = "ALWAYS AS IDENTITY"
+
+        [[actions.columns]]
+        name = "name"
+        type = "TEXT"
+    EOS
+
+    assert_match "SET search_path TO migration_test",
+      shell_output("#{bin}/reshape generate-schema-query")
+
+    assert_match "Error: error connecting to server:",
+      shell_output("#{bin}/reshape migrate 2>&1", 1)
+  end
+end
