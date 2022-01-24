e1a779f0c4fde3fab4a68f40faa16bc2bde435fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-24 04:08:16 +0000

    proxychains-ng: update 4.16 bottle.

--- Formula/proxychains-ng.rb
+++ Formula/proxychains-ng.rb
@@ -7,12 +7,12 @@ class ProxychainsNg < Formula
   head "https://github.com/rofl0r/proxychains-ng.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "99ceac372f00a1ce0834d400e58a2f22719bbd0c6cf97c1e522ca978cd8dc1bc"
-    sha256 arm64_big_sur:  "6a8e236352cf019230c9c65845b76263018b25f1a05317752eab45b2055b2140"
-    sha256 monterey:       "c1c7e76c78d7f5e0917a17605caf2f4109491df05322faa7a056d65f72a928cd"
-    sha256 big_sur:        "1672e57460c43b61c1b40b18db1015a9a2e4156d202687bb8480b76d1d987df0"
-    sha256 catalina:       "afdbb2881164696d82c98f03cb24f299d06cd1cd8bc42ae2e7554215f2576a55"
-    sha256 x86_64_linux:   "999f3b69f901f7f5d1604e28193a0fd585f4e4e92346e1876c25313a6c1202b8"
+    sha256 arm64_monterey: "ce214ff6acd1265dc03907d4415c1b7d883862d56e046f305fb7ad1a23295840"
+    sha256 arm64_big_sur:  "4a5d12dcf616731f16127b8c2a0f77adccaa813a74e9952bf7de504f4f6818c2"
+    sha256 monterey:       "68cdf4b1a018cc0a94513227ff073dfa430658f330dce45dcc041e001639e8f1"
+    sha256 big_sur:        "e1c1f4075d1b7bc8e4aa3053ff0fddb53f0813080785583fff7bf5037c8fa835"
+    sha256 catalina:       "b85d3715f51bd972366c176cc9a75c8b1303812fde82c879e12b17657db08789"
+    sha256 x86_64_linux:   "b88fdbfcdada3fca4d4e8e8195d848aad96fe2b242888f6466df7b5535b598a6"
   end
 
   def install
