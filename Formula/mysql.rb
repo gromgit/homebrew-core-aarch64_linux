b3d0e79efa37e91aa0afe458ea85152bb31eb331
Yasuo Honda <yasuo.honda@gmail.com>
2022-01-25 00:04:14 +0900

    mysql 8.0.28
    * mysql 8.0.28
    * Revert "mysql: fix build on Monterey"
      This reverts commit a848f9e4bd9245dd61fbf2cc7805fbf45a551f6c.
    * Add `depends_on "libfido2"` at mysql.rb
      To address "  -- Looking for fido.h - not found"
      https://github.com/Homebrew/homebrew-core/runs/4924099412?check_suite_focus=true#step:6:469
    * mysql: fix `libfido2` detection
    * mysql: fix Linux patch
    * mysql: fix `pkg-config` args
    * mysql: fix finding `pkg-config` on Linux
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/mysql.rb
+++ Formula/mysql.rb
@@ -1,10 +1,9 @@
 class Mysql < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/8.0/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.27.tar.gz"
-  sha256 "74b5bc6ff88fe225560174a24b7d5ff139f4c17271c43000dbcf3dcc9507b3f9"
+  url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.28.tar.gz"
+  sha256 "6dd0303998e70066d36905bd8fef1c01228ea182dbfbabc6c22ebacdbf8b5941"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
-  revision 1
 
   livecheck do
     url "https://dev.mysql.com/downloads/mysql/?tpl=files&os=src"
@@ -24,6 +23,7 @@ class Mysql < Formula
   depends_on "pkg-config" => :build
   depends_on "icu4c"
   depends_on "libevent"
+  depends_on "libfido2"
   depends_on "lz4"
   depends_on "openssl@1.1"
   depends_on "protobuf"
@@ -39,22 +39,12 @@ class Mysql < Formula
     depends_on "gcc" # for C++17
 
     ignore_missing_libraries "metadata_cache.so"
-
-    # Disable ABI checking
-    patch :DATA
   end
 
   conflicts_with "mariadb", "percona-server",
     because: "mysql, mariadb, and percona install the same binaries"
 
   fails_with gcc: "5"
-
-  # Fix build on Monterey.
-  # Remove with the next version.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/fcbea58e245ea562fbb749bfe6e1ab178fd10025/mysql/monterey.diff"
-    sha256 "6709edb2393000bd89acf2d86ad0876bde3b84f46884d3cba7463cd346234f6f"
-  end
 
   def datadir
     var/"mysql"
@@ -66,6 +56,9 @@ class Mysql < Formula
       # against `_ZN17Gcs_debug_options12m_debug_noneB5cxx11E' can not be used when making
       # a shared object; recompile with -fPIC
       ENV.append_to_cflags "-fPIC"
+
+      # Disable ABI checking
+      inreplace "cmake/abi_check.cmake", "RUN_ABI_CHECK 1", "RUN_ABI_CHECK 0"
     end
 
     # -DINSTALL_* are relative to `CMAKE_INSTALL_PREFIX` (`prefix`)
@@ -83,6 +76,7 @@ class Mysql < Formula
       -DWITH_SYSTEM_LIBS=ON
       -DWITH_BOOST=boost
       -DWITH_EDITLINE=system
+      -DWITH_FIDO=system
       -DWITH_ICU=system
       -DWITH_LIBEVENT=system
       -DWITH_LZ4=system
@@ -94,6 +88,22 @@ class Mysql < Formula
       -DENABLED_LOCAL_INFILE=1
       -DWITH_INNODB_MEMCACHED=ON
     ]
+
+    # Their CMake macros check for `pkg-config` only on Linux and FreeBSD,
+    # so let's set `MY_PKG_CONFIG_EXECUTABLE` and `PKG_CONFIG_*` to make
+    # sure `pkg-config` is found and used.
+    if OS.mac?
+      args += %W[
+        -DMY_PKG_CONFIG_EXECUTABLE=pkg-config
+        -DPKG_CONFIG_FOUND=TRUE
+        -DPKG_CONFIG_VERSION_STRING=#{Formula["pkg-config"].version}
+        -DPKG_CONFIG_EXECUTABLE=#{Formula["pkg-config"].opt_bin}/pkg-config
+      ]
+
+      if ENV["HOMEBREW_SDKROOT"].present?
+        args << "-DPKG_CONFIG_ARGN=--define-variable=homebrew_sdkroot=#{ENV["HOMEBREW_SDKROOT"]}"
+      end
+    end
 
     system "cmake", ".", *std_cmake_args, *args
     system "make"
@@ -192,18 +202,3 @@ class Mysql < Formula
     system "#{bin}/mysqladmin", "--port=#{port}", "--user=root", "--password=", "shutdown"
   end
 end
-
-__END__
-diff --git a/cmake/abi_check.cmake b/cmake/abi_check.cmake
-index 0e1886bb..87b7aff7 100644
---- a/cmake/abi_check.cmake
-+++ b/cmake/abi_check.cmake
-@@ -30,7 +30,7 @@
- # (Solaris) sed or diff might act differently from GNU, so we run only 
- # on systems we can trust.
- IF(LINUX)
--  SET(RUN_ABI_CHECK 1)
-+  SET(RUN_ABI_CHECK 0)
- ELSE()
-   SET(RUN_ABI_CHECK 0)
- ENDIF()
