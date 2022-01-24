dfd7e99746bd00d7b6b9602efe24a6f356fe5563
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-24 14:08:40 +0000

    topgrade: update 8.2.0 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -6,12 +6,12 @@ class Topgrade < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e4004a5e335073ca546024bb1f94e2537ff6a08e8093569f9b641ccfe5d21e96"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "423afd7f664fbc3bc1ae1e8a37ef95aaaddf319f367fdf86e7ac9a3ef7538850"
-    sha256 cellar: :any_skip_relocation, monterey:       "4bbb5fefb404ca2a39e06819913f019dde67e6de75f1c1b749158a2886d2f7dc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8cc8a044174db649284c0d04fd9673f91c43dc99c3e20ec7b80e8790e798f3d6"
-    sha256 cellar: :any_skip_relocation, catalina:       "225f33e5195378987773ac83dff3d689ef8d6e3647896ddc8cfd35e2b33d24ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "264ead537ab66acfb2c401b4f9de6ddbd0b253b3806c52f46bf4e7ed30dc0d90"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea0b2ef10e1dd2875e10b2adc940fe1afda9eafb7757feba50a6afcceb7c065c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "87020ea515d4f51e67e0c0df3b80a50f787c79708bb752bf44083ea00272f035"
+    sha256 cellar: :any_skip_relocation, monterey:       "5fe4a8b351db46efa12f98781fed1acf8705f9846e89d457ea712e43d2b494f7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ab9e955518844bdb34c01d1e5331825146cf75f395f08c8a5f29dbeba0e3f32c"
+    sha256 cellar: :any_skip_relocation, catalina:       "3effde42f63353395c69a41300e5b4108d73b5276189d000d6119d3f486a97ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76aac7b5595c1ef680d263e7d327998053cee7ecac865205a72ddc0bcc728f8a"
   end
 
   depends_on "rust" => :build
