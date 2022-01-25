c594b32f72dc0a008ba5fe7afc2951f0d5a987b6
Rui Chen <rui@chenrui.dev>
2022-01-24 20:36:17 -0500

    calicoctl 3.21.4
    Closes #93735.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -1,11 +1,11 @@
 class Calicoctl < Formula
   desc "Calico CLI tool"
   homepage "https://www.projectcalico.org"
-  url "https://github.com/projectcalico/calicoctl.git",
-      tag:      "v3.21.2",
-      revision: "17461419f0b20d784b2929b4aa700d4b14c2e10d"
+  url "https://github.com/projectcalico/calico.git",
+      tag:      "v3.21.4",
+      revision: "220d04c9429e8f157caccc70e4ed4c92e1d24e8d"
   license "Apache-2.0"
-  head "https://github.com/projectcalico/calicoctl.git", branch: "master"
+  head "https://github.com/projectcalico/calico.git", branch: "master"
 
   livecheck do
     url :stable
@@ -24,14 +24,17 @@ class Calicoctl < Formula
   depends_on "go" => :build
 
   def install
-    commands = "github.com/projectcalico/calicoctl/v3/calicoctl/commands"
+    commands = "github.com/projectcalico/calico/calicoctl/calicoctl/commands"
     ldflags = "-X #{commands}.VERSION=#{version} " \
               "-X #{commands}.GIT_REVISION=#{Utils.git_short_head} " \
               "-s -w"
-    system "go", "build", *std_go_args(ldflags: ldflags), "calicoctl/calicoctl.go"
+    system "go", "build", *std_go_args(ldflags: ldflags), "calicoctl/calicoctl/calicoctl.go"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/calicoctl version", 1)
+
+    assert_match "invalid configuration: no configuration has been provided",
+      shell_output("#{bin}/calicoctl datastore migrate lock 2>&1", 1)
   end
 end
