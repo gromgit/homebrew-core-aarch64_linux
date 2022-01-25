6b74cb05d5effefb2ff89e14b99f56d36d01bd47
Porkepix <porkepix@gmail.com>
2022-01-25 12:01:00 +0100

    proguard 7.2.0
    Closes #93772.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/proguard.rb
+++ Formula/proguard.rb
@@ -1,8 +1,8 @@
 class Proguard < Formula
   desc "Java class file shrinker, optimizer, and obfuscator"
   homepage "https://www.guardsquare.com/en/products/proguard"
-  url "https://github.com/Guardsquare/proguard/releases/download/v7.1.1/proguard-7.1.1.tar.gz"
-  sha256 "28128dc08035927962c8a405109aab7cbafd509ea9e5860420b839102adb373a"
+  url "https://github.com/Guardsquare/proguard/releases/download/v7.2.0/proguard-7.2.0.tar.gz"
+  sha256 "829a3420e52f49d5d2b751b5d9ab6f543ff503a87ffd835014222b40250c0a6d"
   license "GPL-2.0"
 
   livecheck do
