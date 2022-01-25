742362b9813279aa1d9f5f2120f4caf33f046f53
Porkepix <porkepix@gmail.com>
2022-01-25 10:25:15 +0100

    awk 20220122
    * awk 20220122
    * awk: remove workarounds
      `ENV.deparallelize` is no longer needed, and setting `YACC` breaks the
      build.
    
    Closes #93759.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/awk.rb
+++ Formula/awk.rb
@@ -1,8 +1,8 @@
 class Awk < Formula
   desc "Text processing scripting language"
   homepage "https://www.cs.princeton.edu/~bwk/btl.mirror/"
-  url "https://github.com/onetrueawk/awk/archive/20180827.tar.gz"
-  sha256 "c9232d23410c715234d0c26131a43ae6087462e999a61f038f1790598ce4807f"
+  url "https://github.com/onetrueawk/awk/archive/20220122.tar.gz"
+  sha256 "720a06ff8dcc12686a5176e8a4c74b1295753df816e38468a6cf077562d54042"
   # https://fedoraproject.org/wiki/Licensing:MIT?rd=Licensing/MIT#Standard_ML_of_New_Jersey_Variant
   license "MIT"
   head "https://github.com/onetrueawk/awk.git", branch: "master"
@@ -20,16 +20,10 @@ class Awk < Formula
 
   uses_from_macos "bison"
 
-  conflicts_with "gawk",
-    because: "both install an `awk` executable"
+  conflicts_with "gawk", because: "both install an `awk` executable"
 
   def install
-    ENV.deparallelize
-    # the yacc command the makefile uses results in build failures:
-    # /usr/bin/bison: missing operand after `awkgram.y'
-    # makefile believes -S to use sprintf instead of sprint, but the
-    # -S flag is not supported by `bison -y`
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "YACC=yacc -d"
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     bin.install "a.out" => "awk"
     man1.install "awk.1"
   end
