60387b4bb223d440f6aff9a1b290282491a8b5b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-25 12:06:27 +0000

    pandoc-crossref: update 0.3.12.2 bottle.

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -6,12 +6,12 @@ class PandocCrossref < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4bc1d588c2f72a51e5694a1e311bc82f7d6ae1be9aa27e5b8de5c5562d161b2d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c4e05b0d6a715b76d84a8bfc89875b1f3469079f1d3256a2420ab43ed6b3818"
-    sha256 cellar: :any_skip_relocation, monterey:       "de95ee53a12b8b282e3ebca195feb4844b7d3d10f2bd022fccc8f874b86fd7bd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a48a94b65a99e2c9af87515dd879ee44a604bf207443a8f4d7d8dafa57d8ad74"
-    sha256 cellar: :any_skip_relocation, catalina:       "f310898a96092aa369c8fb97163f5441ca83859872e34c2f74109e07fa2b2cde"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9c1d977fc70a8c02ad43017a8908f83113dbd8b76c83f71af879dc626bb9c9d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "576649f2e937c1231d0af6909e92153808471b19463691c7b38f3ccd1927dca0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bfd697182b040325c1bceb049a5121faa05f4143eabb5c879b7a2e95459d8033"
+    sha256 cellar: :any_skip_relocation, monterey:       "ca887409b851f6167f4f4304ee12d718b5b41a7b3291fab011e4a6e723bf8afa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4109269adeb80e636a428c04c0ee016103820df84147b543d6221e56b8587c8b"
+    sha256 cellar: :any_skip_relocation, catalina:       "f77c6a8d5ad38bef3d24cd3ef8fabf9bc18b1c3ff82e4d54b0850eabf37780e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f587334df22fa23eeb001dabb50852dc23669be7ff71e2b4159a632cabe204e1"
   end
 
   depends_on "cabal-install" => :build
