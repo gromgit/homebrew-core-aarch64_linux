f629813723dd70b37aaad0922b4e6d53923b74c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-25 23:05:48 +0000

    awk: update 20220122 bottle.

--- Formula/awk.rb
+++ Formula/awk.rb
@@ -8,14 +8,12 @@ class Awk < Formula
   head "https://github.com/onetrueawk/awk.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "41e60320408494ad391cefeedf05d31d0692fdf29461aee70a75f110796efe4e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "09cf1fe371a924c163732e19310f635953ae3f4909a9c3baa9db4ea4d850d905"
-    sha256 cellar: :any_skip_relocation, monterey:       "aecf06d8731df59c04db7b67b4d4e8e4acfbf3a455e9eebafc082acc91e8385f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a73059712fb0cc9a57389f72596ca5c52978ad54bf9dce04baedbf653b154022"
-    sha256 cellar: :any_skip_relocation, catalina:       "681acac2c4bac4b8f275f640abf46e5391216d800ff34ad8c57c8d116674fae6"
-    sha256 cellar: :any_skip_relocation, mojave:         "202e81f1562d8b46d4b932f91c64ab58fc6017f6cdd2cc3f2a636038abc76fc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a6d47998c2945313c463fb2736c6381f2bb6bfa725d902e17b7dded8f3025bb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "65041e32f93bb2c9fb34499a142599d1197812e746a97538dd6ea0d0b952c26d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b2d1a70b2bbb181d8d3a4372600c89f066085bd622af35ba5286d066d79d78fa"
+    sha256 cellar: :any_skip_relocation, monterey:       "36440051d67edfd36eda51c6e37faac85b8d4cfbdd650160f66445b043686e43"
+    sha256 cellar: :any_skip_relocation, big_sur:        "50e132ca2ee5ccd0e5eb143ae3184f9d787bee4a1906b5465bcc2fdcee18626d"
+    sha256 cellar: :any_skip_relocation, catalina:       "b8169a72b8a1c318c426837f59044fc9c67f1b31d4029bb0e9f4f67d8d4c8602"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b875a21cdac385ac38de2474281d539e3e668b8eb2bb94043aa41c468d637df5"
   end
 
   uses_from_macos "bison"
