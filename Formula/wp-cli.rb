2cb09b301515f50e7e2b45d608a8e484ee8166cd
Alain Schlesser <alain.schlesser@gmail.com>
2022-01-25 18:38:26 -0600

    wp-cli 2.6.0
    Closes #93802.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,8 +1,8 @@
 class WpCli < Formula
   desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/releases/download/v2.5.0/wp-cli-2.5.0.phar"
-  sha256 "be0853e9f443f3848566070871d344e8ad81eb1e15d15dcf9324b4a75e272789"
+  url "https://github.com/wp-cli/wp-cli/releases/download/v2.6.0/wp-cli-2.6.0.phar"
+  sha256 "d166528cab60bc8229c06729e7073838fbba68d6b2b574504cb0278835c87888"
   license "MIT"
 
   livecheck do
