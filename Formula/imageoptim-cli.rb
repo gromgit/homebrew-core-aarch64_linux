8f856249f4a91cccbd002906cb05ed4f69da3cb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-26 08:06:22 +0000

    imageoptim-cli: update 3.0.7 bottle.

--- Formula/imageoptim-cli.rb
+++ Formula/imageoptim-cli.rb
@@ -13,11 +13,9 @@ class ImageoptimCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:    "fec70bca8360bb34c5f5758da13615cc5c833dceb4559a71ee62223b0684c73b"
-    sha256 cellar: :any_skip_relocation, big_sur:     "fd8ea02974a34708556c132b2258c12d68e379abfd1f0e591d193f97489d03fb"
-    sha256 cellar: :any_skip_relocation, catalina:    "56a9b2dba8f47850a26c335311f8c436b683c0b92ef5ab0b83e91688cf64ec7a"
-    sha256 cellar: :any_skip_relocation, mojave:      "b7b1923ed31ab32540a5dffcf798675401ca48249fae54f49d67bc6c78feede9"
-    sha256 cellar: :any_skip_relocation, high_sierra: "6f1aa4b2e4de3e7a1502f1f8747283589697e5f0f0506f4d24acd53381311706"
+    sha256 cellar: :any_skip_relocation, monterey: "5e330d75ae3187599414a272f05823d9e2e829cee4d4e75ca176e0e62aea2cdb"
+    sha256 cellar: :any_skip_relocation, big_sur:  "633de8d348a6618d839ff4e16694c8634d59accafe13488cd72c88f44386cbe4"
+    sha256 cellar: :any_skip_relocation, catalina: "633de8d348a6618d839ff4e16694c8634d59accafe13488cd72c88f44386cbe4"
   end
 
   depends_on "node" => :build
