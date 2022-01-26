e9be13965163df4663088d4d1bde4084a5628a2b
Branch Vincent <branchevincent@gmail.com>
2022-01-25 22:56:01 -0500

    kubernetes-cli 1.23.3
    Closes #93803.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      tag:      "v1.23.2",
-      revision: "9d142434e3af351a628bffee3939e64c681afa4d"
+      tag:      "v1.23.3",
+      revision: "816c97ab8cff8a1c72eccca1026f7820e93e0d25"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kubernetes.git", branch: "master"
 
@@ -65,7 +65,7 @@ class KubernetesCli < Formula
     version_output = shell_output("#{bin}/kubectl version --client 2>&1")
     assert_match "GitTreeState:\"clean\"", version_output
     if build.stable?
-      revision = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
+      revision = stable.specs[:revision]
       assert_match revision.to_s, version_output
     end
   end
