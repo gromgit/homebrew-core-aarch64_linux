22132d33729cfdedb5fa806b30f75750ee0e5a15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-26 11:10:38 +0000

    gitui: update 0.20.1 bottle.

--- Formula/gitui.rb
+++ Formula/gitui.rb
@@ -6,12 +6,12 @@ class Gitui < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ece4f1a613bc88b05a3b00e88f0623617c19befd3e3cec5de934cc5e49fdd4a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c98fc4b91b6e326f529be870428721e2f21060d466db73f7f1d809888924928b"
-    sha256 cellar: :any_skip_relocation, monterey:       "8ab951d31a956c3f5c2d716db769819a2fe9628e4a7fb9cbcaf96ad2cb3c37fe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "442e39bf1e63fdf6c0c2282b4907a6fe571d86789f7673ed575730c6369f88dd"
-    sha256 cellar: :any_skip_relocation, catalina:       "fb7126d4ab88082a8c8b34818ddabbff25dc16be98d13b004295f444748a359d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fae322f86d610a28a3214cb6583d5d19db4dcefba59919238d9b91f61ec318a1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d68a6ab46e7610a1e05f2111edaa588d1fc9c9f3ed8b77a9586acd46eea6819"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2eb961ab0ad57527c95921f4835da9ff10f6924cd5c5e792f361b92446771d5a"
+    sha256 cellar: :any_skip_relocation, monterey:       "09aaa256f4779237ee2f3981a84c5060b816246545d0cb58c9f120d2e48cb11d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1652b8f6822c2bdbe5627025d18d85a307597d880182b0dd0251e0027ecac721"
+    sha256 cellar: :any_skip_relocation, catalina:       "60156c4ac3ce0632dc713de11d9643f1312191e7a7600a68d01fb433968820e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33a79b2a12fbff18d42be963c43936c1bdf4db7ff0a474259b499528e737cde2"
   end
 
   depends_on "rust" => :build
