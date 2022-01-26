cf91d46a7bd9d0d069fa5db9d9bc39f9ac1999cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-26 19:05:10 +0000

    wp-cli: update 2.6.0 bottle.

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -11,13 +11,12 @@ class WpCli < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1f99dcae45bc3a8f8ea09d534e1050201391afea5fcd42287a5d24aef303dcc4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f99dcae45bc3a8f8ea09d534e1050201391afea5fcd42287a5d24aef303dcc4"
-    sha256 cellar: :any_skip_relocation, monterey:       "5f130b0733ba0765afdb46836875c83e848b5b6bf53990cdb4b1e2282cf03171"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5f130b0733ba0765afdb46836875c83e848b5b6bf53990cdb4b1e2282cf03171"
-    sha256 cellar: :any_skip_relocation, catalina:       "5f130b0733ba0765afdb46836875c83e848b5b6bf53990cdb4b1e2282cf03171"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1f99dcae45bc3a8f8ea09d534e1050201391afea5fcd42287a5d24aef303dcc4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee8ae5d8225ee58093857e74e69c4b148f23cf3c0c0c6c41c27f1b0b9b410121"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee8ae5d8225ee58093857e74e69c4b148f23cf3c0c0c6c41c27f1b0b9b410121"
+    sha256 cellar: :any_skip_relocation, monterey:       "c02c370321bcf6458c96d5169b0752840b914f2224676aa32ce32cd416dcd280"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c02c370321bcf6458c96d5169b0752840b914f2224676aa32ce32cd416dcd280"
+    sha256 cellar: :any_skip_relocation, catalina:       "c02c370321bcf6458c96d5169b0752840b914f2224676aa32ce32cd416dcd280"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee8ae5d8225ee58093857e74e69c4b148f23cf3c0c0c6c41c27f1b0b9b410121"
   end
 
   depends_on "php"
