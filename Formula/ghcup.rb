de6e01fb289837f18b5ac89a45b9024036e53bc5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-27 05:31:35 +0800

    ghcup 0.1.17.4 (new formula)
    Closes #93865.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/ghcup.rb
@@ -0,0 +1,32 @@
+class Ghcup < Formula
+  desc "Installer for the general purpose language Haskell"
+  homepage "https://www.haskell.org/ghcup/"
+  # There is a tarball at Hackage, but that doesn't include the shell completions.
+  url "https://gitlab.haskell.org/haskell/ghcup-hs/-/archive/v0.1.17.4/ghcup-hs-v0.1.17.4.tar.bz2"
+  sha256 "9973a42397bcdecfaf5f5d8251b4513422b80e901b2f2e77b80b0ad28d19f406"
+  license "LGPL-3.0-only"
+  head "https://gitlab.haskell.org/haskell/ghcup-hs.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  def install
+    system "cabal", "v2-update"
+    system "cabal", "v2-install", *std_cabal_v2_args
+
+    bash_completion.install "scripts/shell-completions/bash" => "ghcup"
+    fish_completion.install "scripts/shell-completions/fish" => "ghcup.fish"
+    zsh_completion.install "scripts/shell-completions/zsh" => "_ghcup"
+  end
+
+  test do
+    assert_match "ghc", shell_output("#{bin}/ghcup list")
+  end
+end
