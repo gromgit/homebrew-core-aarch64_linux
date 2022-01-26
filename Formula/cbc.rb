004de5255153e4c0a6c4160229e53d4704a52385
Porkepix <porkepix@gmail.com>
2022-01-26 11:55:39 +0100

    cbc 2.10.7
    Closes #93823.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cbc.rb
+++ Formula/cbc.rb
@@ -1,8 +1,8 @@
 class Cbc < Formula
   desc "Mixed integer linear programming solver"
   homepage "https://github.com/coin-or/Cbc"
-  url "https://github.com/coin-or/Cbc/archive/releases/2.10.6.tar.gz"
-  sha256 "59d0f45c4c6ce399b723e528d637fb8e409dba7449b91ae27edbb5c0617cc65d"
+  url "https://github.com/coin-or/Cbc/archive/releases/2.10.7.tar.gz"
+  sha256 "5aa5490e2bc39c3c03f3636c9bca459cb3f8f365e0280fd0c4759ce3119e5b19"
   # update to EPL-2.0 on next release
   license "EPL-1.0"
 
