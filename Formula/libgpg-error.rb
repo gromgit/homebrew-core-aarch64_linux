2ae2daf0ef073e17af0a96cb38916411c3156e18
Porkepix <porkepix@gmail.com>
2022-01-27 11:46:35 +0100

    libgpg-error 1.44
    Closes #93904.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -1,8 +1,8 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/related_software/libgpg-error/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.43.tar.bz2"
-  sha256 "a9ab83ca7acc442a5bd846a75b920285ff79bdb4e3d34aa382be88ed2c3aebaf"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.44.tar.bz2"
+  sha256 "8e3d2da7a8b9a104dd8e9212ebe8e0daf86aa838cc1314ba6bc4de8f2d8a1ff9"
   license "LGPL-2.1-or-later"
 
   livecheck do
