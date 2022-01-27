2e23184601a4e729f6db136401bdc749e65ec536
César Román <thecesrom@gmail.com>
2022-01-27 08:49:51 -0800

    pyenv 2.2.4-1
    Closes #93925.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -1,8 +1,8 @@
 class Pyenv < Formula
   desc "Python version management"
   homepage "https://github.com/pyenv/pyenv"
-  url "https://github.com/pyenv/pyenv/archive/refs/tags/v2.2.4.tar.gz"
-  sha256 "7eb5a444cbcde94cbc4f67d056dff3a4db1502d4c491d7fcabb7870d8ecb595a"
+  url "https://github.com/pyenv/pyenv/archive/refs/tags/v2.2.4-1.tar.gz"
+  sha256 "6aa38fb7c7d7aa68e6a3c8b812b273e15e367f629f31dd9d24552403b48df459"
   license "MIT"
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git", branch: "master"
