b606956d69f70e3b12a732318d7546fa81d8bf9b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-27 17:12:22 +0000

    spirv-tools: update 2022.1 bottle.

--- Formula/spirv-tools.rb
+++ Formula/spirv-tools.rb
@@ -6,13 +6,12 @@ class SpirvTools < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "0614a813a7ce60d0058e49fc16efcd7b7cc42aa88cdf24125b6f4d911ca99d83"
-    sha256 cellar: :any,                 arm64_big_sur:  "8295ab8e466b1fb4762e0ed3cf73c319959e09a59576d293eb96e74f7538bcf7"
-    sha256 cellar: :any,                 monterey:       "9f839427e49ce5d0211249157289800927672ca7afeefa8d961649a4704d5052"
-    sha256 cellar: :any,                 big_sur:        "3ee9a3f37165bab272d6a1f4c7d8f58648101974f24759fe5e5b30ada07b7a14"
-    sha256 cellar: :any,                 catalina:       "8c19649797b273df39a4220d293a2c7afc9215e3a308e826f99c945c8f324d97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a332b0f4241fe4805202f4e255088680ed5c0b84fc2379cf00f169e249a6eea7"
+    sha256 cellar: :any,                 arm64_monterey: "ffeeb83915c6ad55978bfb7ebd8423442ae93e4ed449e9eab438e6494602b7de"
+    sha256 cellar: :any,                 arm64_big_sur:  "ae2c2e66cc6ab3b6fbd52473ec9b22618bdae11d1b91824015287f5174074a81"
+    sha256 cellar: :any,                 monterey:       "99864fe56b7d8b7ab7c89ce5657404bab2b12e3c06ec8022a5a2d3d85d7c89c6"
+    sha256 cellar: :any,                 big_sur:        "21a9e7876cfae49779f503dc29831a609290bcbb350a37cef01db66dd6983e39"
+    sha256 cellar: :any,                 catalina:       "877145f635f567b800159607057ecf63ab50f32c21b073d6cf69e92be8ea943c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "349dc462ee43b71d0cec01d0b2e98c487d6881facf8ff24eef32e4b10d53af21"
   end
 
   depends_on "cmake" => :build
