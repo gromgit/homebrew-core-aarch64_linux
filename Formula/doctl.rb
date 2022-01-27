50991e0884189415abaabe6568e932e2df689513
Nanda H Krishna <me@nandahkrishna.com>
2022-01-27 22:22:37 +0000

    doctl: update 1.70.0 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -7,12 +7,12 @@ class Doctl < Formula
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b337eb11ddce1016c2555699de61088d17efb9eaff28829bde664f309ca8b77"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cddfd1b7963998e1a16c70a783be3e5a070ade9a7d1256e437eeb32984979ee3"
-    sha256 cellar: :any_skip_relocation, monterey:       "5efb1a209cf0f28da7afea08944cd5c70b9acbd772fb0a6b522aecb68da64993"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1249fceae9b79e191104a6a0fdb11fd193b3efbd79b7623b3f983abe20c78737"
-    sha256 cellar: :any_skip_relocation, catalina:       "a9f8bca2ba666c31603dae004ae525c0fa93110633bb1ada5fdf691eb4626a96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3bdb79a5e704c18b62d537e42ed5fac0e4aa24888e8c203190e09c6c04beaec4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "38ff8eb380adf54840f16b346f07cfef339e57065aac54ed474cf9dde1bd1be8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "16a80ceb75a97aa896c53a9afd3dc03badfcd9f0bcffbd52179b196f5944c133"
+    sha256 cellar: :any_skip_relocation, monterey:       "87766c760569b86f5050a3eb94bb01b5bd66aa814b2cc5b85005d652050e6fff"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f2347ee26d1e9e7082eab370564fd706009d7695b6570b538b73602faef902a4"
+    sha256 cellar: :any_skip_relocation, catalina:       "b17a08925d8b86f8003e789a2b61722b5b9d5d95af2e0788e5e48577fbda2809"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f86834dd9a1a937b238a193069b901a435d9d92b3cbefd1dc7661d14619486a2"
   end
 
   depends_on "go" => :build
