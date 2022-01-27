31417e376a620bd3cd82a9294a893d4b9f7fa1a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-27 04:05:59 +0000

    tendermint: update 0.35.1 bottle.

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -7,11 +7,12 @@ class Tendermint < Formula
   head "https://github.com/tendermint/tendermint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c446caa05d5232396ea0efa1fc0181584c318c9076e83d9d6da67675f300780b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c446caa05d5232396ea0efa1fc0181584c318c9076e83d9d6da67675f300780b"
-    sha256 cellar: :any_skip_relocation, monterey:       "bd49b0bd382275d36982aae65c8b30463666ef1d83456b27538e50be803ef751"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bd49b0bd382275d36982aae65c8b30463666ef1d83456b27538e50be803ef751"
-    sha256 cellar: :any_skip_relocation, catalina:       "bd49b0bd382275d36982aae65c8b30463666ef1d83456b27538e50be803ef751"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2413ad4ad056b32627c2b3b0a02a12fcfeddd4a47924bfe917eadecd3edceb93"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2413ad4ad056b32627c2b3b0a02a12fcfeddd4a47924bfe917eadecd3edceb93"
+    sha256 cellar: :any_skip_relocation, monterey:       "d575717959d09a1bdb54cb5aad76fe0e64153bf6d19b3da732af2183ac689cbc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d575717959d09a1bdb54cb5aad76fe0e64153bf6d19b3da732af2183ac689cbc"
+    sha256 cellar: :any_skip_relocation, catalina:       "d575717959d09a1bdb54cb5aad76fe0e64153bf6d19b3da732af2183ac689cbc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5706e2a4287571a080412ddc7d5d5e4b53a43c2d63a82a2e4f3ea57191c88be4"
   end
 
   depends_on "go" => :build
