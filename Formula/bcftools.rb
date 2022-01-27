13e283ce89d2b60c7a729379180235f1d42a0a64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-27 15:13:48 +0000

    bcftools: update 1.14_1 bottle.

--- Formula/bcftools.rb
+++ Formula/bcftools.rb
@@ -14,12 +14,12 @@ class Bcftools < Formula
   end
 
   bottle do
-    sha256                               arm64_monterey: "744a97a27e3bad5c82d483d388fb49af8c13b67c3186da29c57da2fb5fda0c41"
-    sha256                               arm64_big_sur:  "13bc31d2086972697f374bcf68a24deaa389a57ed2adb4a3d432fd72a60ffcda"
-    sha256                               monterey:       "0f9241650659b12f4196cc1401714cee3688f6e72021c654c93f43eff066de1d"
-    sha256                               big_sur:        "e4cd74edeaa7c41ae71263732822b732b8d1e7cde98ce813dcd1fc1d2bf0529e"
-    sha256                               catalina:       "ddf5d0fe3d61c1386a383469a3bc7bb6bacc0ae11e6eaa3ef21ca3d38968e6a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8db6b53f0fe4b88ec4d1e41297a1487429b937e0579c0ba8ee2c67a5729216c3"
+    sha256                               arm64_monterey: "f6dc63f7c76b06013acd6cc56917d92a36ead9ec1280f876dfbe93a724a1f143"
+    sha256                               arm64_big_sur:  "57ac52202f614792d964d18f706ee0285e4f39f34f794d42686a861591b5d1c3"
+    sha256                               monterey:       "02e025616ff6daf116c39cf5fbf62a0dab4c5cbcc460eac607e2a105ce30efd1"
+    sha256                               big_sur:        "df5c39b00efc020cbf282dcf10f489d67c3dea591d7eb733131e3d08f7a14c06"
+    sha256                               catalina:       "927aab202ad520ba0c44847404b6e1f2e6f1442dee49d08235125b767643ba07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57c32fb494027d88d9efabd8cfe15b158e32f0eb6e720ea6a228e04a8af53eaa"
   end
 
   depends_on "gsl"
