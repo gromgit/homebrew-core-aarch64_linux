21909413eb1f3d643c159a20992a452797e882a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-28 13:07:10 +0000

    gambit: update 16.0.2 bottle.

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -6,13 +6,12 @@ class Gambit < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b92ff6853ffbf21cb1999cce85620f289cbaeecb51b06c219eff10afe839794e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "067cb4ce13d125296e4db92c28c63ae90c6107079b8cd4a6e1fc935565bf864f"
-    sha256 cellar: :any_skip_relocation, monterey:       "c35cdfa436f76f4054c666b8b2e7ea4b72c1afd22c169c32eef2e32e1d5bafe9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c99a930bc6bd33cd8ccd07602c472c9a64006b8a6ca2a846081c0faecaf39bf7"
-    sha256 cellar: :any_skip_relocation, catalina:       "ca119805ce3e9aa8a02d91362ba8cab410762b34e84c67616c78006acebd7d44"
-    sha256 cellar: :any_skip_relocation, mojave:         "0ed6547bd2c50529879b3f1d19dcd1afa685dcc3ed030866d6cbd104c6402dc6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "946f395529bd8d582781d198adac36c2617b0636db95b8b7b5337d0542f0f7eb"
+    sha256 cellar: :any,                 arm64_monterey: "0779354b7180b2a9ef458329b0e9e7479ba124a1682f9f6068c2eef9837b96d6"
+    sha256 cellar: :any,                 arm64_big_sur:  "f528e0b09dd94b129e8e6a37b1ac7b881b5f94602b4c7dd73c3f9304fc3f461a"
+    sha256 cellar: :any,                 monterey:       "65e3c4196da9261bea2df1694700ed9dfca563f8390269c9e85d7e694d4e9679"
+    sha256 cellar: :any,                 big_sur:        "9a13115f4a60933306b00fef69aa5b020b6fbe60b19291c73515c908bfce4464"
+    sha256 cellar: :any,                 catalina:       "43e0f79f7009fae5cea81be024b0bab1a7f9dd444da8ac43cb78cac10d0c3ec1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "551f35c78edac8bc7a7e62511c7f9bb34b6143211b56ba00b3d9931b0ca96504"
   end
 
   depends_on "autoconf" => :build
