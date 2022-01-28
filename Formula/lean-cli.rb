f9e373b4ef9ef30cbe6ae76a8d518d158f1464a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-28 22:05:32 +0000

    lean-cli: update 0.29.2 bottle.

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -7,12 +7,12 @@ class LeanCli < Formula
   head "https://github.com/leancloud/lean-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "297e5b3365b8b9962c9043f83187d134f1dfbde6a5e9bf701ffdf930a04aef34"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93f973a8f75638ba51473f543a9a6df01072e9d6a83f2565d4afb0fe4dbb31b1"
-    sha256 cellar: :any_skip_relocation, monterey:       "0ae987771c7232f15febd496216f1a6c58c9a480ce07177021dfe0f13ccebd34"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2569f335f70633cf3b4934985b28a75c8901ea7d1bcee18f6c72dc17121db48b"
-    sha256 cellar: :any_skip_relocation, catalina:       "28f91cea2e135ad326b1809dc847830cffb7c1817cf5de08dd9f9a17f0e5e38d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43cc33d0a8b9c99482403d3ec6ed07d8ac0dacd5943d18636f51022edc7c4a77"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f2068012dbe4e9ba5a477728c73c1d1027c9bc2143bea22efac9f46a6371d889"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "938d04c38bd74357e7881fabbc8a123d39ca7bb5f64f7a03c90d9b162ac057b8"
+    sha256 cellar: :any_skip_relocation, monterey:       "b99aa01419ea57946c7934481737de4ba380e67ed9d280f0babac885519de406"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b51cd54264423bb54aa14cb70d609f0a628c9e50ddcfb12d7af7c9df42c5d96b"
+    sha256 cellar: :any_skip_relocation, catalina:       "376934101ecb42fa60761fe9f7c4848fe363588b7262ccbebf2584d915e8018b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "099834ac3c6bc5ac2d6157e0bf6a9d1b219daf6316cc672810deeedc26621301"
   end
 
   depends_on "go" => :build
