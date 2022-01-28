81f04e29701191b309efa69586594640a10ea498
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-28 04:34:45 +0000

    weggli: add 0.2.3 bottle.

--- Formula/weggli.rb
+++ Formula/weggli.rb
@@ -6,6 +6,15 @@ class Weggli < Formula
   license "Apache-2.0"
   head "https://github.com/googleprojectzero/weggli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0944ab09e4cac0f09bfe25748caa10d83d99de52d1379fc30d64d7c22b87a08e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39e53596e47f0ea171008c7f8c12de2f8e14ae134425cfcbbb1d1ae75698b778"
+    sha256 cellar: :any_skip_relocation, monterey:       "95c818831ee4ef72ada7b4b316454cb46d4345809a2501ef985472508765888f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2269a1b9996695a50e8c494018a424bb9ceb9cfd060610bd30feb1b7b8fa92a3"
+    sha256 cellar: :any_skip_relocation, catalina:       "a9b54765d5adda4ac03351babe93312e959212b7ebf38987f788855cbae73503"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8a29e869828ffb19877f1c760a308b53b58fa81e6a62aea80f9dbc0b23fc106"
+  end
+
   depends_on "rust" => :build
 
   def install
