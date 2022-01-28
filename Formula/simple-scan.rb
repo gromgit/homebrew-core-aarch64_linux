a7089e9e29f38c901a7ab77cb527449eb4c317a4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-28 19:26:31 +0800

    simple-scan: skip test on Linux (#93974)
    This seems to fail on headless CI.

--- Formula/simple-scan.rb
+++ Formula/simple-scan.rb
@@ -47,6 +47,9 @@ class SimpleScan < Formula
   end
 
   test do
+    # Errors with `Cannot open display`
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"].present?
+
     system "#{bin}/simple-scan", "-v"
   end
 end
