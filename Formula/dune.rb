256a6a7b913e36638440ff50e6554d3be6f68455
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-28 22:05:38 +0000

    dune: update 2.9.3 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -7,12 +7,12 @@ class Dune < Formula
   head "https://github.com/ocaml/dune.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6a4b54dd7247b91672a1dde5eb527a51d4fc150c3fad18c25e8170501fced830"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b7d058d01e441fe189a98c6c599714df63d7e23d38d711e42f56a266390252a"
-    sha256 cellar: :any_skip_relocation, monterey:       "46cf2177b00aa94c8c3226b24114c2450fc4fb76cef65f9928ade922dfe3048e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2d437c08892e45e72736063ff90dbab9a98953117d43922f5005f25c03112ee3"
-    sha256 cellar: :any_skip_relocation, catalina:       "8b93c7b1b8ad5053b87ed8c1dad302c8021b78e0127656f81215d03e4da2738f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e26c214bb13b411185f4c138c3023ba55e4d0b8af57f217272d5305c64934b5f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dff83bb542bcd1beb4fddf7965bc4e51d5c3575da59c0719647534df50eb37bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae69be7082e78cad895c5377c158881c7063b6f43d8bb4211a735aa424ff0c24"
+    sha256 cellar: :any_skip_relocation, monterey:       "2e55f5af9300013485a4ce7b31294ec1f6e2220e73d641a3c329d315dfb1b2da"
+    sha256 cellar: :any_skip_relocation, big_sur:        "70559f83f94845a6a5f055fa7ccb53d849a7fe8df969aa58e70d9e6eeaeb9dfc"
+    sha256 cellar: :any_skip_relocation, catalina:       "9364090d1fe8cac052e73af4791d7f709156e8be547d9e58a52ae97eec8fe5a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75b4c5a1271aae59f6fae9269f76f0320b9976a5b14be57193d380596a2363b8"
   end
 
   depends_on "ocaml" => [:build, :test]
