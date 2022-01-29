16938734f8f0b4d616f3a3ed5d92c28b9eba6803
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-29 18:07:23 +0000

    vivid: update 0.8.0 bottle.

--- Formula/vivid.rb
+++ Formula/vivid.rb
@@ -6,13 +6,12 @@ class Vivid < Formula
   license any_of: ["MIT", "Apache-2.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ab52d6ee4b485875a3b3c999d443c5eca3b045c752ca11cff2bd7b054df376c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "db529c13331abf864f50a4e1db4f06cf090d1121bfc26ffae0d4924f77f4cdeb"
-    sha256 cellar: :any_skip_relocation, monterey:       "424aa4faa3daaddadc92176cb5a56a58327c362d8d89cb6029896d839df71756"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9ac9d9ca0cc06b51e932e8e1447659848fb6ddc38f522fc6fa4df9bbc894c868"
-    sha256 cellar: :any_skip_relocation, catalina:       "a3e8186f762051a6cf1b3c7dcfbea4a47761653fd282bbfe43fbd563ff168dc9"
-    sha256 cellar: :any_skip_relocation, mojave:         "5198e3c4ece298eeb42895d0961d74976890ef422d29dfa8e23ade3ba58ade02"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3bf8009e9771733833317fee2810b482a569265732d22539446ba8d2a67a78bd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5812d1ee1b84676ed4f7f40e2b6bbd637598994a6972e2f5f374951aae3b9d74"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6d63fb2bb636e28b0c96d1fb30a0c96ca2b9d6d8ae96cf759b5c188b17ab01bd"
+    sha256 cellar: :any_skip_relocation, monterey:       "62f24fd28f1c34d545901fd9f1131c9b68f4dfcd3c24b80f237786a2a4773d1c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d05e79b8400ba8d0ea9951e049b60872f2b0a5cdc46eab2a2f0aa1d9a36517b0"
+    sha256 cellar: :any_skip_relocation, catalina:       "084ed6ec5118a90caab68edd0f18921f21b2ae4430dca72eacc346b411ea825a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9ebf69fb2697860406c4d8f2cc41e015be8ce6a763eaf08d89cae0ab616b991"
   end
 
   depends_on "rust" => :build
