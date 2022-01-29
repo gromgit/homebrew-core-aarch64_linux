2216831720f2a6842ede1b01f9c764fe48a6100c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-29 08:07:54 +0000

    git-credential-libsecret: update 2.35.1 bottle.

--- Formula/git-credential-libsecret.rb
+++ Formula/git-credential-libsecret.rb
@@ -11,12 +11,12 @@ class GitCredentialLibsecret < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "67b99a0bef773231492d03921808e6a40dd5cee09fffba3c4f7a5572fd7f2649"
-    sha256 cellar: :any,                 arm64_big_sur:  "d48080738a3f5b046606411471a60eb8e4e83c2fe8c1f48d67b68a9400209fbb"
-    sha256 cellar: :any,                 monterey:       "299cde0b6347e18e71ae5a3b878d83227590842e72e5debeef3706cf1a9e1d8c"
-    sha256 cellar: :any,                 big_sur:        "137adf976b5afadb81c78bcdc7d702e1f48d5d15033904092d736e1fef777e8f"
-    sha256 cellar: :any,                 catalina:       "d2d82f0f0f8199560d666c708994ba7786e67be9594524807a72cfa6a052417e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e67d78e353ed25e49bac11c640742b9d999e4972a044054689d6d28a160abe4"
+    sha256 cellar: :any,                 arm64_monterey: "acb839b5afbc60a2ff80c9ef1875de1a9e2f3796024a96677111360e686e40e0"
+    sha256 cellar: :any,                 arm64_big_sur:  "b884b34e6cee884df6a3f7d39fffbc63c9e3c12cd86a881116722e2eb696a2ba"
+    sha256 cellar: :any,                 monterey:       "7bddf11f1e6e34af10f45751d91c5b14de29d909bf7eda85ec93d71017b73b9c"
+    sha256 cellar: :any,                 big_sur:        "f6047fea127f586a364cc88a07ded89c7475551917bcb37d9f9dadbbe63def73"
+    sha256 cellar: :any,                 catalina:       "3b8697883d3a76a165599b0200da6ca002b38a1ddab4d2efac1294dcc286af3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a96317944d29d474ecb49efd43597cb6c3912653fc73a9b48c3590ed688e7894"
   end
 
   depends_on "pkg-config" => :build
