c47645d1f9998f65aff3a9272c3397dfd6d70d90
Porkepix <porkepix@gmail.com>
2022-01-29 16:05:07 +0100

    primesieve 7.8
    Closes #94065.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -1,8 +1,8 @@
 class Primesieve < Formula
   desc "Fast C/C++ prime number generator"
   homepage "https://github.com/kimwalisch/primesieve"
-  url "https://github.com/kimwalisch/primesieve/archive/v7.7.tar.gz"
-  sha256 "fcb3f25e68081c54e5d560d6d1f6448d384a7051e9c56d56ee0d65d6d7954db1"
+  url "https://github.com/kimwalisch/primesieve/archive/v7.8.tar.gz"
+  sha256 "0cd0490259e6e919c6e3dd3f3e69ac6d91e6cbe616e22a219abe4006d9293d5d"
   license "BSD-2-Clause"
 
   bottle do
