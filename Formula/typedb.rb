2529b6d8918bb7b102207f67f91f7625aeb17753
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-30 18:06:43 +0000

    typedb: update 2.6.2 bottle.

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -6,11 +6,7 @@ class Typedb < Formula
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c3d3ead692d5963f807280528ccd8e78d6f37d2a3230cb4c9442a31c44c49a1b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c3d3ead692d5963f807280528ccd8e78d6f37d2a3230cb4c9442a31c44c49a1b"
-    sha256 cellar: :any_skip_relocation, catalina:      "c3d3ead692d5963f807280528ccd8e78d6f37d2a3230cb4c9442a31c44c49a1b"
-    sha256 cellar: :any_skip_relocation, mojave:        "c3d3ead692d5963f807280528ccd8e78d6f37d2a3230cb4c9442a31c44c49a1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "899d7b2dbbfba0279aed9df963356dae18c0624490a1232062e603fc3efbe435"
+    sha256 cellar: :any_skip_relocation, all: "75422adab190f8c4de8f70182130a9e9f7057f5326cd4ea661fd9d458dee16cc"
   end
 
   depends_on "openjdk@11"
