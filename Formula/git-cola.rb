42a3272a634b22a660717bd0b3438f05f8fc27e6
Rui Chen <rui@chenrui.dev>
2022-01-30 11:08:27 -0500

    git-cola 3.12.0
    Closes #94146.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-cola.rb
+++ Formula/git-cola.rb
@@ -1,8 +1,8 @@
 class GitCola < Formula
   desc "Highly caffeinated git GUI"
   homepage "https://git-cola.github.io/"
-  url "https://github.com/git-cola/git-cola/archive/v3.11.0.tar.gz"
-  sha256 "5f14ab41508ee9c8756097d6fa81f471fce58089b178317cc25d948d42620994"
+  url "https://github.com/git-cola/git-cola/archive/v3.12.0.tar.gz"
+  sha256 "ec1167ea9a472214bf18f5537d96e137c724f3d28a85b3642f07dba35f04b24a"
   license "GPL-2.0-or-later"
   head "https://github.com/git-cola/git-cola.git", branch: "main"
 
@@ -21,13 +21,17 @@ class GitCola < Formula
   uses_from_macos "rsync"
 
   def install
+    # setuptools>=60 prefers its own bundled distutils, which breaks the installation
+    # Remove when disutils is no longer used: https://github.com/git-cola/git-cola/issues/1201
+    ENV["SETUPTOOLS_USE_DISTUTILS"] = "stdlib"
+
     ENV.delete("PYTHONPATH")
-    system "make", "PYTHON=#{Formula["python@3.9"].opt_bin}/python3", "prefix=#{prefix}", "install"
-    system "make", "install-doc", "PYTHON=#{Formula["python@3.9"].opt_bin}/python3}", "prefix=#{prefix}",
-           "SPHINXBUILD=#{Formula["sphinx-doc"].opt_bin}/sphinx-build"
+    ENV["PYTHON"] = which("python3")
+    system "make", "prefix=#{prefix}", "install"
+    system "make", "install-doc", "prefix=#{prefix}", "SPHINXBUILD=#{Formula["sphinx-doc"].opt_bin}/sphinx-build"
   end
 
   test do
-    system "#{bin}/git-cola", "--version"
+    system bin/"git-cola", "--version"
   end
 end
