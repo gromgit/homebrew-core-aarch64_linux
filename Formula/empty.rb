60aa02125cab04248efc78ba4d7a698bf1ed9b9e
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2022-01-30 23:35:35 +0000

    empty: update 0.6.21b bottle.

--- Formula/empty.rb
+++ Formula/empty.rb
@@ -11,11 +11,13 @@ class Empty < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b330bcc238599680bc706db6b9f5a2947ad46c60196afce716811ccd2deb0ce0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e33747b4b2d1fc4e3fcfd35471c283e64c60c6cf6d558c6783d7a076cb84e233"
-    sha256 cellar: :any_skip_relocation, catalina:      "8eac558272ccf2338a374ea2e7158a4b0cf9008cc0111fefa8c85a80cfab2ee1"
-    sha256 cellar: :any_skip_relocation, mojave:        "8fb4ab0e88893f107afe0e69a48ed6f257a11b370bd56b2237ecadec771e1a17"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "3c5daa156ad925469841f360ca2687011a96086f7d6c5b8af0fedea97ee059ca"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5bf2f0dabd46de8bef94154d66dfab1b9650097e92d92126b572fb7acd894764"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b330bcc238599680bc706db6b9f5a2947ad46c60196afce716811ccd2deb0ce0"
+    sha256 cellar: :any_skip_relocation, monterey:       "d5a02c5a6c5cb8973346e2f70326eb90d078900fa9da930164276cf9d9052c02"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e33747b4b2d1fc4e3fcfd35471c283e64c60c6cf6d558c6783d7a076cb84e233"
+    sha256 cellar: :any_skip_relocation, catalina:       "8eac558272ccf2338a374ea2e7158a4b0cf9008cc0111fefa8c85a80cfab2ee1"
+    sha256 cellar: :any_skip_relocation, mojave:         "8fb4ab0e88893f107afe0e69a48ed6f257a11b370bd56b2237ecadec771e1a17"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "3c5daa156ad925469841f360ca2687011a96086f7d6c5b8af0fedea97ee059ca"
   end
 
   def install
