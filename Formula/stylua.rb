fecc297b5af20cadab8f3db4a96fe6d076a41712
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-31 09:05:21 +0000

    stylua: update 0.12.0 bottle.

--- Formula/stylua.rb
+++ Formula/stylua.rb
@@ -6,12 +6,12 @@ class Stylua < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c800520fabb0b576262c4fbead43730747fff6f25d4a89506862009c2d7504f4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "630ddc02f136da94b9f064212e58375eab2640207ef423bde4848836bd65dd9f"
-    sha256 cellar: :any_skip_relocation, monterey:       "e8efba5bb52394f926463920dbd85ec3ac5bce7e9b0f489b1cc4583b79a8dfca"
-    sha256 cellar: :any_skip_relocation, big_sur:        "835f92db504cef1fcd02a333feb5dafe1cc2bdb4c127c88ec7b83232a1e28ef0"
-    sha256 cellar: :any_skip_relocation, catalina:       "081053917d2a04867fb45926011036e9274f1d6cbf74dd4294f91273712db51e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e62e24cbb79b69f2c69a944467e7264f0cfcb2141d792137361bd90e871e013e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5717fdd05c47904c7fa26bd84579d91fdbe89eebc21921f436482b06095469bd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9cead650320f2ecc3b4695ab9762b25da417087008e51d257a21718f76bc03e4"
+    sha256 cellar: :any_skip_relocation, monterey:       "997ad1879a6a6875a576e31e15128354e8a6be0e89c2f6cc3dd473edd7f6fa93"
+    sha256 cellar: :any_skip_relocation, big_sur:        "12fbcff0c10ad9af613fa3c46c05cea822103bc65448aba01a01c5e669a8216f"
+    sha256 cellar: :any_skip_relocation, catalina:       "48fed2074fff65820e0dc6481fa12aa64ede957eedc2a14ab932c05e69c103ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37796db26b0f777cda4631b66a4c4d07f47637bc47dd0490d59ca642333a62cc"
   end
 
   depends_on "rust" => :build
