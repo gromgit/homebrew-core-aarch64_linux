d96b082f8077fba467676a41c24b8644ffdeab99
Porkepix <porkepix@gmail.com>
2022-01-31 13:42:46 +0100

    z.lua 1.8.14
    Closes #94182.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/z.lua.rb
+++ Formula/z.lua.rb
@@ -1,8 +1,8 @@
 class ZLua < Formula
   desc "New cd command that helps you navigate faster by learning your habits"
   homepage "https://github.com/skywind3000/z.lua"
-  url "https://github.com/skywind3000/z.lua/archive/1.8.13.tar.gz"
-  sha256 "a1215d727527e358363687a7e0b3605a132465e60a915f8e99bb5338a9c62ec6"
+  url "https://github.com/skywind3000/z.lua/archive/1.8.14.tar.gz"
+  sha256 "93784896021f84efe8430fbfcd9f0238426600d62a88780a1abda97943822bd5"
   license "MIT"
   head "https://github.com/skywind3000/z.lua.git", branch: "master"
 
