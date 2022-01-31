19fdf59ecb024af622a807b537ae3ff8a4233699
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-31 14:10:24 +0000

    zabbix: update 5.4.10 bottle.

--- Formula/zabbix.rb
+++ Formula/zabbix.rb
@@ -7,12 +7,12 @@ class Zabbix < Formula
   head "https://github.com/zabbix/zabbix.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "7b2a4a6b8b76a05c3715fb97fd63880015372da3f706d5486299e8c141e580d4"
-    sha256 arm64_big_sur:  "e6243079ba8cc2e8b897472b12d9487cf7358e6e355d862ceed1420b5bb9434c"
-    sha256 monterey:       "bd9ddfee43eaa57a4a0a9f774423d6ca862010d644b89d35934b771ed97934dd"
-    sha256 big_sur:        "4030456db52f5c1b30ad5a0ff036c82af008e23683d444df80feef1125c7a75c"
-    sha256 catalina:       "c074d5aee35a3695245b0e85907488f2a91218a7f10754815d24342386933ea3"
-    sha256 x86_64_linux:   "f6c4d0316876e8123da73c418390e30c8dfef6d23dc1f3aca74cb4c1a63637fb"
+    sha256 arm64_monterey: "2efc79af9056617493577ab25993352cd15b4c4690ae8f36d28174d5516093d3"
+    sha256 arm64_big_sur:  "7c93dc751146ecf7f5c4096a245265dd58c1487449fdd625a8dc779744bd9061"
+    sha256 monterey:       "4a94b72a723256969b5b781452c1cf2ffa00321bd892ea4a2413daf690043715"
+    sha256 big_sur:        "b02e1a23252dc45699904cd087e35f20caa77f7821a4f29bc85d764d1e035ed9"
+    sha256 catalina:       "591db0838bb425f4c04d8af53d717981588659512fb088bd27e9e146e4325047"
+    sha256 x86_64_linux:   "0ef2a057c75eced6194dd52e6cf607bb605e969ded59579ddac5535ce42bb85e"
   end
 
   depends_on "openssl@1.1"
