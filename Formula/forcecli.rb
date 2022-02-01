79dbb865a8f0309995f4e7c6d4726743219b18c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-01 00:15:45 +0000

    forcecli: update 0.33.0 bottle.

--- Formula/forcecli.rb
+++ Formula/forcecli.rb
@@ -7,12 +7,12 @@ class Forcecli < Formula
   head "https://github.com/ForceCLI/force.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2104638f28f680848e219fbe3ad57988306b4ea25d6bb06fb04a451d3feaecb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0549f14810f77112b8d642c6453dc636adb519ee072ec993e98d1cabf2dd8380"
-    sha256 cellar: :any_skip_relocation, monterey:       "6c6110abb43a3e522a9201c464b26c9cc85c30d3b2d36df32bca739fe2994584"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9bffce24112b4e7c120964811bd779e95a6153ba1bc5951a99ebdec81dfa8e9c"
-    sha256 cellar: :any_skip_relocation, catalina:       "1772f7e189f229021992d27874c7c417eb5765b888ccf051ab1a6a5cd560540e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0be83b963b60e9b15475d6a3558b9d172d2b7a4255de1287137900a04e266fc5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eff077dec6432fc688990fc5b0acc752af3d87da68329a49635037c2aabd49dc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f616183637df2265dc216d20f7ec100a5531c63ead75d8f90f6b73ed616c362"
+    sha256 cellar: :any_skip_relocation, monterey:       "48696261fb6bdab3782558f7d7dfe1f173bc47c3f2cdd57d828023192a67e628"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7b95b76500298c417ce385351b37f181740416c2ce05a54968d96bd85430d5d8"
+    sha256 cellar: :any_skip_relocation, catalina:       "4904051712d530ea84ed6b300f69b6dd1129d9ed718d501d55d9cd9f80886ee0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69d52200ee937994496e91aacfaa32584f13e55964bd306cb26dd82397c3e23e"
   end
 
   depends_on "go" => :build
