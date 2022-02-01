18e37830b031a808ac8e58cbf9a04099a45e41e5
Porkepix <porkepix@gmail.com>
2022-02-01 11:20:14 +0100

    hello 2.12
    Closes #94242.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,8 +1,8 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftp.gnu.org/gnu/hello/hello-2.11.tar.gz"
-  sha256 "8c9ce0572d3c44ed0670eb1cde980584e038b6f62c25fdfde8ef128de15004bd"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz"
+  sha256 "cf04af86dc085268c5f4470fbae49b18afbc221b78096aab842d934a76bad0ab"
   license "GPL-3.0-or-later"
 
   bottle do
