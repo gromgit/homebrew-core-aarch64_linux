6fd773e07e993414818aca34c693f86c9d5f961b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-01 00:16:18 +0000

    glooctl: update 1.10.4 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -15,12 +15,12 @@ class Glooctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "665f15c79341ec7bf9257c21edf02d1afb98d50fdac81ce156ec7dd563a7128d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85eda9fc09c40a0c33f253491c88a2db8f27489e965e6c9fab9bf5f367da4548"
-    sha256 cellar: :any_skip_relocation, monterey:       "f6a8f2667a6b1b1a5d21dfc7c217a09d4b595f68acc8abed75a625aa7c566d22"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9cb515a7b8d54d6e676bad60af7ab2b156091f6022e293d2258ec1f2cd96e93d"
-    sha256 cellar: :any_skip_relocation, catalina:       "62048d581fddeaea1a0f3c11ddc2e460d2ac1a48b904425b08763d381b25190d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "456ba5a8180ad3f72828615966f01564883d781553ee98ff5bbfdfe6386beff4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5940e79da540516f598d798699fb9b1a20d7e748c3925238a536d7463b1132f0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "550f96900c616b6c8084a258f6dcebecda66d7c22712e9e98cc04a5e3b144b63"
+    sha256 cellar: :any_skip_relocation, monterey:       "f43edb3033d489bf2b30919275633b3dd11591b7ae96d2de5990a04ba09c55f1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a7842fbd4d2e84906879fbe056bfd5402a6de9f05f06bf9c407877422bf85781"
+    sha256 cellar: :any_skip_relocation, catalina:       "ef7c95993f8d6f34831c13b0ae683ea27ed0eaa585b4ed9d388ae9588c33952e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "406bd6eeceddfd0291f1201c04708721f2a350d58cdec53e7dcf631c32f21693"
   end
 
   depends_on "go" => :build
