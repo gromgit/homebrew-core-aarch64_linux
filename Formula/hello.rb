ca4fbfa2aed3f392f1c471a7183a2e7a29e6b6f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-01 12:08:08 +0000

    hello: update 2.12 bottle.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -6,12 +6,12 @@ class Hello < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb2c428f4d0667df4049a1a94b8ecc7ceb3a63be30423dbdcee77bdee859cd7c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cfbce13d943feb89318c14f97cc71d686b4c59100dde0f125b7cf086ae491fc0"
-    sha256 cellar: :any_skip_relocation, monterey:       "557501b7ccf982b496e484b0be41ec3e85f6d8f5ad18e99c950fb61f4b57f2ca"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7dd1beef8bfcc1c4bf1aa6dbd4baf262419fef189e8dffa72455853c0236fd59"
-    sha256 cellar: :any_skip_relocation, catalina:       "8368282ba548c6c551d1ab9374cbfb93304cbd890092a156d851b6bfe19f2931"
-    sha256                               x86_64_linux:   "f262a5395166c745eddcd002d029f4b1d8461976251330c5fd1ee1ef5599aab8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "be8bba73844b60a684885c91b61bd131a8655ae5b1d748dd7562f878e78fa8f7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85d1e1b66826f3e5b9767af9f2fad00e5395a0c559519d4a55a0136518c81dbd"
+    sha256 cellar: :any_skip_relocation, monterey:       "f7883fe8258feef2ff49fc3b2f8c7a9665a6c51afe34042b7dcb69d805b1376b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2e4325a96ed1cbfe67a93a4df85b0edb1aa8d55a7d2c33396b95801b2d6dc047"
+    sha256 cellar: :any_skip_relocation, catalina:       "9f70c809590bc170cd73273c95f21bb1b78c7bd9d0b9cbb8b191f17ff7ce8be6"
+    sha256                               x86_64_linux:   "d5a213b36a6dedc2dc2194e364e65f74208e7ea46fbd78d2442791e703a61e7a"
   end
 
   conflicts_with "perkeep", because: "both install `hello` binaries"
