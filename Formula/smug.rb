cc475a9f5ef372c2c68c5ff6f203932ec6fd2059
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-01 14:10:21 +0000

    smug: update 0.2.6 bottle.

--- Formula/smug.rb
+++ Formula/smug.rb
@@ -6,12 +6,12 @@ class Smug < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3035675211a0c82ed566573df01fa2c23f5ccb44a77eb338bcb41e167ce12f8f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad559a52f54f9ed84601bbb6e563d1f415880880cd969e88b209351c2128fca3"
-    sha256 cellar: :any_skip_relocation, monterey:       "bd47ab8f577c219df5c1f39c42b98c105b02b36356519e44b2499f922314af21"
-    sha256 cellar: :any_skip_relocation, big_sur:        "77ffea790e36ac16ca2835dd17d58857e916bd58f104ee0701f7a7af4a376974"
-    sha256 cellar: :any_skip_relocation, catalina:       "22e4404ea1eeb196143642e5ab789a4101a9d7a66d63e3f3ae55004ccc15a214"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9c492188f01dab148da458e79f6d5d4f4089a2709791629bbd38722646365270"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "656fa4ac3bea46df6b3e98d7949965d61f5b9930c4fe7a1dbaaad1a96f8b43e1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a0176d3b1cad3174f2ff360c4b57cdc6e5ac0575e53c9c73387ce234b9a746fa"
+    sha256 cellar: :any_skip_relocation, monterey:       "bbb7b973854757b86d825af723f2d4b88ad87e35dc2f6f34be458d0380a11239"
+    sha256 cellar: :any_skip_relocation, big_sur:        "43527e1048bd72d1beb68c369a292d9ab9865018085c34bcd7aa2b2b9702c206"
+    sha256 cellar: :any_skip_relocation, catalina:       "454884ff2732db10eb9b2a1e2c4ea692079dbda5cbc47a63c4274bc2e420d5d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc966ca38ec8860c768966d2999fe9e536567ac4142bca6e8454942ef09c2f9c"
   end
 
   depends_on "go" => :build
