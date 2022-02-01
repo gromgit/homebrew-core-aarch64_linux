4f46540a2846054790e6b10e82b95f0fd10042f8
Ivan <hello@iillexial.me>
2022-02-01 13:24:47 +0000

    smug 0.2.6
    Closes #94251.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/smug.rb
+++ Formula/smug.rb
@@ -1,8 +1,8 @@
 class Smug < Formula
   desc "Automate your tmux workflow"
   homepage "https://github.com/ivaaaan/smug"
-  url "https://github.com/ivaaaan/smug/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "314711f616ea71613fe883fe579e7d3bc0051f540f27c7925a1ce6a0ece69378"
+  url "https://github.com/ivaaaan/smug/archive/refs/tags/v0.2.6.tar.gz"
+  sha256 "ec72748a8425b0aadb65cb1d5fa956b33258a29256583c635e07caba440a0788"
   license "MIT"
 
   bottle do
