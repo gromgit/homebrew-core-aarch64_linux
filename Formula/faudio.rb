26b07b24bafb7962debfd3c87a826885ef139621
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-02 08:05:45 +0000

    faudio: update 22.02 bottle.

--- Formula/faudio.rb
+++ Formula/faudio.rb
@@ -7,12 +7,12 @@ class Faudio < Formula
   head "https://github.com/FNA-XNA/FAudio.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e0bb9195b9c4bbd8fdd559b9c1fd1b03e3ceacf39b1092cdcfe55ed31381e9ca"
-    sha256 cellar: :any,                 arm64_big_sur:  "a362750f126c8377d4da980260e31c8dbbaf0ee20e08f31f7161ceb0bf08898c"
-    sha256 cellar: :any,                 monterey:       "085c4271d5c25762c479492b762eb63825834b6c71ef602696efb9871f7760b7"
-    sha256 cellar: :any,                 big_sur:        "6a57e1f9bac80a4a3a5060d4af15caae43d0e602536af15d712e321490bcdda9"
-    sha256 cellar: :any,                 catalina:       "493e4f6c2d40be26636274bd2600daef24756813b2d145e0124149dd23be51a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "046a13dd4a6f6bd01d05590bd1815e136e9e20d225a6c239d1ad383327be7587"
+    sha256 cellar: :any,                 arm64_monterey: "44260f7cd79011efeec18b07ba1f14e04899b7450038d8a93d870e975336060a"
+    sha256 cellar: :any,                 arm64_big_sur:  "79cc2a40413f6002b11a001cf50e477f3f052da74a71d58f5c6f3817ada0ae50"
+    sha256 cellar: :any,                 monterey:       "4ecaa72d1f1ca90476e3f2e8cd8589d5b0deab1bff72d9049baf17663dba74f1"
+    sha256 cellar: :any,                 big_sur:        "98b0bd397a6f6d1b7948ce4eafb95f002595641a0b0b1a7a80a3de7a2a5e8f70"
+    sha256 cellar: :any,                 catalina:       "c36e8538ed877963e0dc4b1a8abb64ce54b9bff576d46e5631a0a6364eb4b8dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5808f616fe8908a3a9f7455035c8ac01e001dd9117a8099f7263fc63f478761"
   end
 
   depends_on "cmake" => :build
