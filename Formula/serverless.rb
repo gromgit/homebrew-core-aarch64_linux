8c2d5f13c7f24db367bc57795548b5b494f94bfd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-02 10:07:16 +0000

    serverless: update 3.1.0 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -9,12 +9,12 @@ class Serverless < Formula
   head "https://github.com/serverless/serverless.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ec6f7f9281ed65700fdc0c129dd7da9d098321370846a8220d95755c56e8164"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2ec6f7f9281ed65700fdc0c129dd7da9d098321370846a8220d95755c56e8164"
-    sha256 cellar: :any_skip_relocation, monterey:       "fed5443091242cd91ce3efc974ca3a87226aec8c089432ee04b425de26d2be90"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fed5443091242cd91ce3efc974ca3a87226aec8c089432ee04b425de26d2be90"
-    sha256 cellar: :any_skip_relocation, catalina:       "fed5443091242cd91ce3efc974ca3a87226aec8c089432ee04b425de26d2be90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83799e2ebf595bd76e2aba4b3e3c13d5620c53ed83ace1246818bccc81f99854"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9f5045452a00b257ed86c539dc793ac5ae9eba317d97224039855daf65a25250"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f5045452a00b257ed86c539dc793ac5ae9eba317d97224039855daf65a25250"
+    sha256 cellar: :any_skip_relocation, monterey:       "d0c51b6ad21a258cd03a5f419ea1b5d7c44291052395e1039c67169db30b8483"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d0c51b6ad21a258cd03a5f419ea1b5d7c44291052395e1039c67169db30b8483"
+    sha256 cellar: :any_skip_relocation, catalina:       "d0c51b6ad21a258cd03a5f419ea1b5d7c44291052395e1039c67169db30b8483"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ab68b3cf7688c544317a7c21eee0cdb4b144eb0265a59fe2b7a11a135c81562"
   end
 
   depends_on "node"
