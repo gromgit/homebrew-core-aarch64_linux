79976290e4df33179db615025402e84ed4e58a35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-02 15:11:17 +0000

    citus: update 10.2.4 bottle.

--- Formula/citus.rb
+++ Formula/citus.rb
@@ -7,11 +7,11 @@ class Citus < Formula
   head "https://github.com/citusdata/citus.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "5f6430e06badacfd9c70e898ac854b5926af1c56231ac2999de5629c6caad611"
-    sha256 cellar: :any, arm64_big_sur:  "e5fa12d8b0b3c07ce08f1cbbf8d6b1d986b8e82f86e19210d17a4323dfc2f2fe"
-    sha256 cellar: :any, monterey:       "e54159991712803e281906b82e2c180e5113820b17eb6a5cd6f8bb87ba98922a"
-    sha256 cellar: :any, big_sur:        "ddf037c6fc8ad951ba360aa9e8f105967d293a498f6c76361ef4ba1938a50fac"
-    sha256 cellar: :any, catalina:       "e152e154ba4a75aaba4465b17470b1737a2d604b040f383089c6baf18ea6de4f"
+    sha256 cellar: :any, arm64_monterey: "a473d61c192253889ece25d8805963fc0a06eed2a2f6ab2415a94d17c4036b01"
+    sha256 cellar: :any, arm64_big_sur:  "8d9ff24d6d01e1a89056327cbc75aa13b983b4938894c568f784aaa612390b19"
+    sha256 cellar: :any, monterey:       "e08b487cae176a7e2adf08a6c94a279aeaab5cf52908cc1fd6d23973fd3e5489"
+    sha256 cellar: :any, big_sur:        "edabf4e6e3658330fa31a0bc13ee47a3bac5c87dd4713d0aaa49806ae66991d7"
+    sha256 cellar: :any, catalina:       "93271095bc9abb5650f50da895b245978259483ef27194dab87022a9897d4211"
   end
 
   depends_on "lz4"
