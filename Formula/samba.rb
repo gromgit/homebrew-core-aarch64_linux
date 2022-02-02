dcfab25a2a484bd8afe5ca463a79b88d9d5cb32e
Alexander Bayandin <a.bayandin@gmail.com>
2022-02-02 10:00:49 +0000

    samba 4.15.5
    Closes #94321.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/samba.rb
+++ Formula/samba.rb
@@ -4,8 +4,8 @@ class Samba < Formula
   # option. The shared folder appears in the guest as "\\10.0.2.4\qemu".
   desc "SMB/CIFS file, print, and login server for UNIX"
   homepage "https://www.samba.org/"
-  url "https://download.samba.org/pub/samba/stable/samba-4.15.1.tar.gz"
-  sha256 "a1811fbb4110d64969f6c108f8d161e2c3e20ddf475529a3d32bd94bb7459f00"
+  url "https://download.samba.org/pub/samba/stable/samba-4.15.5.tar.gz"
+  sha256 "69115e33831937ba5151be0247943147765aece658ba743f44741672ad68d17f"
   license "GPL-3.0-or-later"
 
   livecheck do
