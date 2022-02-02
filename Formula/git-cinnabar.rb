1fdfa94b126f2e4ff479141749e6639c861c94ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-02 00:12:34 +0000

    git-cinnabar: update 0.5.8_1 bottle.

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -8,11 +8,12 @@ class GitCinnabar < Formula
   head "https://github.com/glandium/git-cinnabar.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "19ed5d8d7c8a59fe542ed94aa565a8d134aedb3d10936ed6a797516ee8e6f315"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a2415da3841ac338a70dd500083dddf4eafd210b3bf8be37dbb4a87d49beb0d"
-    sha256 cellar: :any_skip_relocation, monterey:       "b85500153d25ede5b84757cabed62051049083ebce4736b6b3d2d37c70a13fa6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0f6a6716f169369b40161c8a2abe5efd68de5bf67a789cf15049389ce3d3f2be"
-    sha256 cellar: :any_skip_relocation, catalina:       "29f41d5d2c312a84893e3d4d284dd3d72a8d518b512c5caebb1c19a6bad980b7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e82376d75a8481c1d14fc79a8c6ec7093260e05c7cb5007184914203ae91365c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "963c3f71356e10162265b3c2cd1abed252c84fe9424d350e4b96eabc360474d7"
+    sha256 cellar: :any_skip_relocation, monterey:       "8436e3cc13ffbba4dacb0ae8f78689b841c428e6a9ea1c77c502c69ad5eeed9f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "290369e1d739f2d99209a8eed259efcff164af3ddf3c825b307450d59c2cfefb"
+    sha256 cellar: :any_skip_relocation, catalina:       "1c0507389075c31707cf9024512d1c5cca9c996d07e4ce06a52859df9f039d9c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "131494059a460bfe7f8364528af2ab6b9cb365638244f3ad2598f82f39e76fc4"
   end
 
   depends_on "mercurial"
