8329b2cbd32242826ac5b3f58a11548bd501565d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-03 08:11:42 +0000

    css-crush: update 4.1.0 bottle.

--- Formula/css-crush.rb
+++ Formula/css-crush.rb
@@ -7,7 +7,7 @@ class CssCrush < Formula
   head "https://github.com/peteboere/css-crush.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8423edc4a4b02e4114edd4a824db1563e6f8a3580b8b3cac52d283eaaaef09e2"
+    sha256 cellar: :any_skip_relocation, all: "b0f588a762d2dc621b5dbb577ee9b0cf85020ce85a5ab986a815724735f8027a"
   end
 
   depends_on "php"
