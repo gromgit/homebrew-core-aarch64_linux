f7a1764525524beb9e4d30d4a0a214b805f9e90b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-03 01:29:29 +0000

    usbutils: add 014 bottle.

--- Formula/usbutils.rb
+++ Formula/usbutils.rb
@@ -6,6 +6,15 @@ class Usbutils < Formula
   sha256 "59398ab012888dfe0fd12e447b45f36801e9d7b71d9a865fc38e2f549afdb9d0"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "b92a68d48cfd148d424c9e856b58a6f35409f39943ec03571647454b1f5e951a"
+    sha256 cellar: :any,                 arm64_big_sur:  "4348cacea6a03dd09f24fb6b81d9c81db1d5b2e7b4aacf72aa23e90e8c4e4d44"
+    sha256 cellar: :any,                 monterey:       "d906f5f22f4cea31c8e4ba89f734639fbbd5719da4312c146c801980214cad3f"
+    sha256 cellar: :any,                 big_sur:        "67d46074026c0a78518a2d5b00e0a3d96a64b4b33d256b1bddfe9c0d9bceda89"
+    sha256 cellar: :any,                 catalina:       "8f0724c218cc91cfbb9da1a69db8c67962f296581aaaaa1305fc8af77f569f3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cb9055ef6f7e14c7ada67c07f72e85ee24399e87fb4ad67dd67811616679e1c"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
