64e6f2f58581fbd5ba1aeb39d16cad0170030bb6
Nanda H Krishna <me@nandahkrishna.com>
2022-02-03 01:42:10 -0500

    libmonome: needs Python, add test, build on Linux
    Closes #94398.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libmonome.rb
+++ Formula/libmonome.rb
@@ -1,9 +1,12 @@
 class Libmonome < Formula
-  desc "Interact with monome devices via C, Python, or FFI"
+  include Language::Python::Shebang
+
+  desc "Library for easy interaction with monome devices"
   homepage "https://monome.org/"
   url "https://github.com/monome/libmonome/archive/v1.4.4.tar.gz"
   sha256 "466acc432b023e6c0bfa8dfb46d79abb1fb8c870f16279ffca7cf5286a63a823"
   license "ISC"
+  revision 1
   head "https://github.com/monome/libmonome.git", branch: "main"
 
   bottle do
@@ -15,6 +18,7 @@ class Libmonome < Formula
     sha256 cellar: :any, mojave:         "a75784a9297378f3c477ee251f9e729f3e98230113908af03078a92cd4c7d076"
   end
 
+  depends_on "python@3.10" => :build
   depends_on "liblo"
 
   def install
@@ -23,8 +27,16 @@ class Libmonome < Formula
     inreplace "wscript", /conf.env.append_unique.*-mmacosx-version-min=10.5.*/,
                          "pass"
 
+    rewrite_shebang detected_python_shebang, *Dir.glob("**/{waf,wscript}")
+
     system "./waf", "configure", "--prefix=#{prefix}"
     system "./waf", "build"
     system "./waf", "install"
+
+    pkgshare.install Dir["examples/*.c"]
+  end
+
+  test do
+    assert_match "failed to open", shell_output("#{bin}/monomeserial", 1)
   end
 end
