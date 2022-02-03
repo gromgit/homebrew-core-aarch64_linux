aea8454882effaba999d1071542274a38dc9bc90
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2022-02-03 07:34:44 +0000

    libnxml: update 0.18.3 bottle.

--- Formula/libnxml.rb
+++ Formula/libnxml.rb
@@ -9,14 +9,16 @@ class Libnxml < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "c9fb3bcc767561392f500093ca5549248153ba874b7d3df6ae17a9a94c9135b7"
-    sha256 cellar: :any, big_sur:       "646e960c9d78476dd4102b5ede1aac8bf0ea3dd06f51de6cec429f0851b4f1ec"
-    sha256 cellar: :any, catalina:      "af92d830dbb7a103cd5a512c03c1cf2777742ea72c998ecbf1fc80912679cb47"
-    sha256 cellar: :any, mojave:        "61e076a06cab737a7410a8a2adf9c29c3d32e44467caaef25d54c7be63093bd6"
-    sha256 cellar: :any, high_sierra:   "a6b51b3ed4d09a603b7d232040b7e53fb26013a16ea9b4b86f415c45200faf43"
-    sha256 cellar: :any, sierra:        "ddeb6f19f803f29eb44f498ed687dd76a5bdeb0b6416c67759e1690ab9fa4f14"
-    sha256 cellar: :any, el_capitan:    "de106efa2da60ccb8567403547f904485c1c6431dd492ce4e1bbd66599c7f961"
-    sha256 cellar: :any, yosemite:      "7c2bff9c49c93ef6a3901050212671c60e0cb4e72f2faf968eb4ae57f3d6fbeb"
+    sha256 cellar: :any, arm64_monterey: "7b9f770e09e58cede9baff8e2cb8b588529368a451ea876edc7d72edb00e324b"
+    sha256 cellar: :any, arm64_big_sur:  "c9fb3bcc767561392f500093ca5549248153ba874b7d3df6ae17a9a94c9135b7"
+    sha256 cellar: :any, monterey:       "db15774e70f1d15202c5c3a0412c87bf2a40625064976ee71b004c9f2a9f439a"
+    sha256 cellar: :any, big_sur:        "646e960c9d78476dd4102b5ede1aac8bf0ea3dd06f51de6cec429f0851b4f1ec"
+    sha256 cellar: :any, catalina:       "af92d830dbb7a103cd5a512c03c1cf2777742ea72c998ecbf1fc80912679cb47"
+    sha256 cellar: :any, mojave:         "61e076a06cab737a7410a8a2adf9c29c3d32e44467caaef25d54c7be63093bd6"
+    sha256 cellar: :any, high_sierra:    "a6b51b3ed4d09a603b7d232040b7e53fb26013a16ea9b4b86f415c45200faf43"
+    sha256 cellar: :any, sierra:         "ddeb6f19f803f29eb44f498ed687dd76a5bdeb0b6416c67759e1690ab9fa4f14"
+    sha256 cellar: :any, el_capitan:     "de106efa2da60ccb8567403547f904485c1c6431dd492ce4e1bbd66599c7f961"
+    sha256 cellar: :any, yosemite:       "7c2bff9c49c93ef6a3901050212671c60e0cb4e72f2faf968eb4ae57f3d6fbeb"
   end
 
   # Regenerate `configure` to avoid `-flat_namespace` bug.
