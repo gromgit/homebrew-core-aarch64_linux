e7621d28fc3b3d1555d21a978f0fe5042cfb5627
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-03 05:19:02 +0000

    jena 4.4.0
    Closes #94386.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,9 +1,9 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-4.3.2.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-4.3.2.tar.gz"
-  sha256 "f863717fbf519261d45c8f7d7b70592326e88848fc4e255f56d9604000e4bbe6"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-4.4.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-4.4.0.tar.gz"
+  sha256 "ffe17bc0a9251622777531ddeb9d080529c060e031fd92db93da1fa1917d13c9"
   license "Apache-2.0"
 
   bottle do
