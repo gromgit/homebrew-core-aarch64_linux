bc60328a5de1c4cbb731c9d7731f0eb94263f55c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-03 15:35:15 +0000

    ndpi: update 4.2 bottle.

--- Formula/ndpi.rb
+++ Formula/ndpi.rb
@@ -7,11 +7,12 @@ class Ndpi < Formula
   head "https://github.com/ntop/nDPI.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "67f203560cd7389e2eeefcaf4333d1c2c177184c363fd049d38c954e28f54151"
-    sha256 cellar: :any, arm64_big_sur:  "65f77fdd73e981b20f3bb393533d2b721cce0842cf1ce3874a651d95eeca571f"
-    sha256 cellar: :any, monterey:       "0a194f0a5086f827e76947b695dd06908ecc0d08b4066e1cfc2d291592b7c76c"
-    sha256 cellar: :any, big_sur:        "6d492a1b34e348a6d39711c406b7c582b17bcdfe3c056a58fafe63904b1543de"
-    sha256 cellar: :any, catalina:       "ab97d26b5b5b8beeed6a8d9157c21d8d29a0d36776c1ba73017cbc37e3286ec2"
+    sha256 cellar: :any,                 arm64_monterey: "67f203560cd7389e2eeefcaf4333d1c2c177184c363fd049d38c954e28f54151"
+    sha256 cellar: :any,                 arm64_big_sur:  "65f77fdd73e981b20f3bb393533d2b721cce0842cf1ce3874a651d95eeca571f"
+    sha256 cellar: :any,                 monterey:       "0a194f0a5086f827e76947b695dd06908ecc0d08b4066e1cfc2d291592b7c76c"
+    sha256 cellar: :any,                 big_sur:        "6d492a1b34e348a6d39711c406b7c582b17bcdfe3c056a58fafe63904b1543de"
+    sha256 cellar: :any,                 catalina:       "ab97d26b5b5b8beeed6a8d9157c21d8d29a0d36776c1ba73017cbc37e3286ec2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e388612f8fec3e9e5f8135845dbde90671fa67e9b9fd75613c11a1111da640e0"
   end
 
   depends_on "autoconf" => :build
