f30f6cd064a5ccbf7e8e885d94da170d74887a01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-04 05:07:25 +0000

    bee 1.95
    Closes #94471.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -1,8 +1,8 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.93/bee-1.93.zip"
-  sha256 "4c4ee0fcef31dcedc524442c53e39eb18546dba65dfc8241df8c86de6b6ec185"
+  url "https://github.com/bluesoft/bee/releases/download/1.95/bee-1.95.zip"
+  sha256 "f999eca2d55a0d0ab9644be16b96dc2664a4b31ecbfad1990d8bb3c03ec4102b"
   license "MPL-1.1"
 
   bottle do
