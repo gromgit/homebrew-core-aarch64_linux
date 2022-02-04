8ab60e51529ee972a06bcd2cc89a433ea5f73588
Rui Chen <rui@chenrui.dev>
2022-02-03 22:53:04 -0500

    operator-sdk 1.17.0
    * operator-sdk 1.17.0
    * update sha
    * update sha
    
    Closes #94465.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/operator-sdk.rb
+++ Formula/operator-sdk.rb
@@ -2,8 +2,8 @@ class OperatorSdk < Formula
   desc "SDK for building Kubernetes applications"
   homepage "https://coreos.com/operators/"
   url "https://github.com/operator-framework/operator-sdk.git",
-      tag:      "v1.16.0",
-      revision: "560044140c4f3d88677e4ef2872931f5bb97f255"
+      tag:      "v1.17.0",
+      revision: "704b02a9ba86e85f43edb1b20457859e9eedc6e6"
   license "Apache-2.0"
   head "https://github.com/operator-framework/operator-sdk.git", branch: "master"
 
