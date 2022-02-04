7d40f88e32c36a22f5d3769a418208c230730994
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-04 05:06:31 +0000

    jfrog-cli: update 2.12.0 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -6,12 +6,12 @@ class JfrogCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "93278c5b45621dac3b4ac40895d2bf50eac5940fb6990d550352882c7e773a9b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ba258ee7164324232480621e85a237981c1ac927090048e5f88f99bcd6b23f71"
-    sha256 cellar: :any_skip_relocation, monterey:       "a9f16468e5b1579c7a3134ac379f0197e5252b124f3605a397d1f347a8cd397c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "33e3471c66a29f072cc30c51489ec51d7f4bd6d3187c670a7a42720078c6222c"
-    sha256 cellar: :any_skip_relocation, catalina:       "e4f2508a7c5f09c944644b0525de8262e6962a5a47907a75a6b843fe1ee60e42"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9358934789f7d2dacbe19f9ae1689eac711c5544372a643ffc3765fa654d825"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b51c6aeac710e8dc3e5e929a359f660bb1a30f409bd69cc1c77abc7cd234ba3e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d4a838bae115be7911bcfaf46bb9e5eb989b9f977f7cdd0f27b933d65a5e18bd"
+    sha256 cellar: :any_skip_relocation, monterey:       "159b9cf5765640c343ce2f5cad4904a3fc1e37976ddda3d67730bb196621f049"
+    sha256 cellar: :any_skip_relocation, big_sur:        "583e1aed2e1197b3f583df9d649cbc1f69a5b6ed0f3f35ace78a6e2083d254ea"
+    sha256 cellar: :any_skip_relocation, catalina:       "5270e18c2f07eef87b270c72b75413e9dcc3d103874efcb152c280ff5482cfd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a496f25a5d9c653b6c1fe271296203b1061514de66967740f3f08c71c298720"
   end
 
   depends_on "go" => :build
