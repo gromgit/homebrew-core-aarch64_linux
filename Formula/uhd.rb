380e63f4bd385a278da16749ee5aa0939504228a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-04 05:06:49 +0000

    uhd: update 4.1.0.5 bottle.

--- Formula/uhd.rb
+++ Formula/uhd.rb
@@ -14,12 +14,11 @@ class Uhd < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "70594fad59c2faea1b522830123a22765740fc2ced86e2619ee7c83e70784529"
-    sha256 arm64_big_sur:  "4187dd90c18fa8709dc09087efc235f3fd21db604e082fce43c4d469aa789bc3"
-    sha256 monterey:       "dd58c2cfb90bce1ebdfc1f6af3757da33082124217506569b1469d63de2ce8e9"
-    sha256 big_sur:        "49bcc2c07aef5d7fa61702c3f52265b4d3557dba27a884784a1748c24ce2fafe"
-    sha256 catalina:       "a44e180043a677bfacbba30f7fffb4ecdf42359672cd5336689e44b57934301b"
-    sha256 mojave:         "30abc4dd1774370d969095ef735b05450785ac68b008cdde2e5d22fa634c65e6"
+    sha256 arm64_monterey: "189a96d00eafaf8bf4e91fad49eb7878a2825cb55c52313fbe9640def83d5906"
+    sha256 arm64_big_sur:  "4d43cc1da5488afcb6e05db8df948bccf42dd6c1c96a081a762e9bc9d2372350"
+    sha256 monterey:       "13c5c120c8574c980867c918772705ceb94ace679ce3cf69d1a4c3d10466555c"
+    sha256 big_sur:        "04db68b8d74125c3a8b6cfa24877671755f6a6770dbee94adaa7cb9cf7646759"
+    sha256 catalina:       "ded65b991342dd6e28ecf922cd3131f12fb106115a2986430092ad02a5213703"
   end
 
   depends_on "cmake" => :build
