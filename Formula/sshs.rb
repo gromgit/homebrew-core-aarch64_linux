eed1129c0277ce23fe555f4abe910cd8e5379857
quantumsheep <nathanael.dmc@outlook.fr>
2022-02-06 00:41:35 +0100

    Add more tests to SSHS formula

--- Formula/sshs.rb
+++ Formula/sshs.rb
@@ -12,6 +12,40 @@ class Sshs < Formula
   end
 
   test do
-    assert_equal "sshs version #{version}", shell_output("#{bin}/sshs --version").strip
+    assert_equal "sshs version #{version}", shell_output(bin/"sshs --version").strip
+
+    # Homebrew testing environment doesn't have ~/.ssh/config by default
+    assert_match "no such file or directory", shell_output(bin/"sshs 2>&1 || true").strip
+
+    require "pty"
+    require "io/console"
+
+    ENV["TERM"] = "xterm"
+
+    (testpath/".ssh/config").write <<~EOS
+      Host "Test"
+        HostName example.com
+        User root
+        Port 22
+    EOS
+
+    PTY.spawn(bin/"sshs") do |r, w, _pid|
+      r.winsize = [80, 40]
+      sleep 1
+
+      # Search for Test host
+      w.write "Test"
+      sleep 1
+
+      # Quit
+      w.write "\003"
+      sleep 1
+
+      begin
+        r.read
+      rescue Errno::EIO
+        # GNU/Linux raises EIO when read is done on closed pty
+      end
+    end
   end
 end
