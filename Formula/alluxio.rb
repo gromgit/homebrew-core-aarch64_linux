c4617ddbcea3b079e72b38ca5095ca0404d80eb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-05 05:04:13 +0000

    alluxio 2.7.3
    Closes #94521.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/alluxio.rb
+++ Formula/alluxio.rb
@@ -1,8 +1,8 @@
 class Alluxio < Formula
   desc "Open Source Memory Speed Virtual Distributed Storage"
   homepage "https://www.alluxio.io/"
-  url "https://downloads.alluxio.io/downloads/files/2.7.2/alluxio-2.7.2-bin.tar.gz"
-  sha256 "e428acfe0704cc68801ae2aa7b7ba920a0e35af9dded66b280649fc1d280a3d4"
+  url "https://downloads.alluxio.io/downloads/files/2.7.3/alluxio-2.7.3-bin.tar.gz"
+  sha256 "33838d275f1acc8ec382e82535ce8363b52384ef443409dc53ae9c6299ccd1b0"
   license "Apache-2.0"
 
   livecheck do
