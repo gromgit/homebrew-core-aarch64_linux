f58a396110370403bf0b47526b85f0a4550431f3
Chenchen Zhu <czhu@me.com>
2022-02-04 17:06:42 -0800

    igv 2.12.0
    Closes #94516.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.11/IGV_2.11.9.zip"
-  sha256 "9a5b5861be38c653cf7402817a1b068e8f65a1090de2d9dffb28872f05a8ac99"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.12/IGV_2.12.0.zip"
+  sha256 "3b4f2f66472574317b1cde8bc20e4a719cda35b12a3876cc2446c56d1e5d1ff8"
   license "MIT"
 
   livecheck do
