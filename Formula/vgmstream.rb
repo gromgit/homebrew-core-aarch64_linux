4b979f8a61c69fb8c5cf4141145c48ee0a0eaae5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-05 15:06:24 +0000

    vgmstream: update r1702 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -29,12 +29,12 @@ class Vgmstream < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "657854dafa6263213d881e2686e838eee4ee1556d5da97706b15db67e1b16bfc"
-    sha256 cellar: :any,                 arm64_big_sur:  "1da9bdf68de35520d505dbc59153df161db4e967e2e610a0d5214ee9bfec30f9"
-    sha256 cellar: :any,                 monterey:       "f0c52e047a2de316fb3bd9ed331339bcbe826b4e5884dccfc563ac53ad75fae3"
-    sha256 cellar: :any,                 big_sur:        "8645bf02e96c0a5e4bc52a51b355af86d7362f60413ed388f14a80ac6c6082fc"
-    sha256 cellar: :any,                 catalina:       "d96550fc66015250ef8a2fe7a3864c7e688a0b912a66fc306d0997edbfecd5e3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb21895ea0256c867a24bc300cc5bc254c2e769e4e74a4cb2766ae9f5662fe57"
+    sha256 cellar: :any,                 arm64_monterey: "af643dfc03fec86043c11237e5d37cb2361950cd790996fd9dc5c2e8679993fc"
+    sha256 cellar: :any,                 arm64_big_sur:  "ab0f747f8e4d16fe25eb2705bfbfda69bd60b99615d811cf58b13be15d43ed25"
+    sha256 cellar: :any,                 monterey:       "ac7fe6850d70179edd15a5f0c791bfcbba90434d5051b85bd86cb34656c93102"
+    sha256 cellar: :any,                 big_sur:        "37496780f4f66b8f0abb1da2e94a02535e128b8d3c80308656c6366702f82832"
+    sha256 cellar: :any,                 catalina:       "c08665d1213fc63ce37aecd5e9d0fd01383c2c7ddffb3d0460336f690f304092"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2fa68bd410e8c05d727d42614a8d2a394cdb639d5ff3c207ba4faecff55d7341"
   end
 
   depends_on "autoconf" => :build
