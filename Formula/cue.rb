79b64aea27e722038c6b732b98af4802bf5e5548
Porkepix <porkepix@gmail.com>
2022-02-05 12:54:48 +0100

    cue 0.4.2
    Closes #94540.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cue.rb
+++ Formula/cue.rb
@@ -1,8 +1,8 @@
 class Cue < Formula
   desc "Validate and define text-based and dynamic configuration"
   homepage "https://cuelang.org/"
-  url "https://github.com/cue-lang/cue/archive/v0.4.1.tar.gz"
-  sha256 "40728522fd6a58eeadc0525f07eb7b6b2baabff5cbf458f5c13cf25bbdb820cd"
+  url "https://github.com/cue-lang/cue/archive/v0.4.2.tar.gz"
+  sha256 "a45e45552b9b3e104fb65ec85891550d8675656be6dece3837334720be5d8150"
   license "Apache-2.0"
   head "https://github.com/cue-lang/cue.git", branch: "master"
 
