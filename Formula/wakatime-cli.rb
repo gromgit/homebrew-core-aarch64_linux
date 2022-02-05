874af46ca38ca4b4e748bc95088ac5a5c9e7025f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-05 08:05:19 +0000

    wakatime-cli: update 1.35.4 bottle.

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -8,12 +8,12 @@ class WakatimeCli < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "906fb0174cc54da00d681d3992a75b96a4a2fffc7ec24a1741363f38b8ff48ef"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7fa347aede6d84072f7e7f17ea485f7f0e99b1f69c356b6cf6e60ce4ae24b88"
-    sha256 cellar: :any_skip_relocation, monterey:       "bbfa9dc3ba8e79f728ab459b3d5ba495ce615eabbbfe4372890fc0ffa61b096d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9fb521535fc571da68463c75a276576f3a6a7709a1027367061a9014c7d67f8d"
-    sha256 cellar: :any_skip_relocation, catalina:       "c456b67502894360fdfcfb02e6f027eae230ac9ab34f655c699ccb7cdf5cd984"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "021bc12a0dcff15beeda94c29364a4d66593a9c918a24c580473340099e82b17"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6cecb39c3ca60b5626e87419823b452a106e2c169e18ee37382f2a19bc0eec18"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "46da81a2e03d29faa889c5fb619cdf1afc1ffd1439be9a4ea1bde0049f1ac94f"
+    sha256 cellar: :any_skip_relocation, monterey:       "67f663719d1be18b4f2ccb8f044fa3440f9078b3c4c4f10c878d67523cc650f9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4e56f82c49d982e2a0f767c1d8538b10a246ebe4af2accacb9b6e5452348590a"
+    sha256 cellar: :any_skip_relocation, catalina:       "ffb823d1e79afb9c24cba04d1e690e40c19260afd8bca7ee978f3dc24ea31bd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f0f01b47a60f0f9b4dec5f15df6271cb8d9d538ff0de1a30f6dc74721d98872"
   end
 
   depends_on "go" => :build
