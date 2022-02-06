ba8a8fc701031f108dde7b7451594d6c0bc2ddc9
Rui Chen <rui@chenrui.dev>
2022-02-05 22:16:48 -0500

    libbsd 0.11.5
    * libbsd 0.11.5
    * libbsd: Fix test
    
    Closes #94578.
    
    Co-authored-by: Michka Popoff <michkapopoff@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libbsd.rb
+++ Formula/libbsd.rb
@@ -1,8 +1,8 @@
 class Libbsd < Formula
   desc "Utility functions from BSD systems"
   homepage "https://libbsd.freedesktop.org/"
-  url "https://libbsd.freedesktop.org/releases/libbsd-0.11.3.tar.xz"
-  sha256 "ff95cf8184151dacae4247832f8d4ea8800fa127dbd15033ecfe839f285b42a1"
+  url "https://libbsd.freedesktop.org/releases/libbsd-0.11.5.tar.xz"
+  sha256 "1a9c952525635c1bb6770cb22e969b938d8e6a9d7912362b98ee8370599b0efd"
   license "BSD-3-Clause"
 
   livecheck do
@@ -26,6 +26,6 @@ class Libbsd < Formula
   end
 
   test do
-    assert_match "strtonum", shell_output("nm #{lib/"libbsd.so"}")
+    assert_match "strtonum", shell_output("nm #{lib/"libbsd.so.#{version}"}")
   end
 end
