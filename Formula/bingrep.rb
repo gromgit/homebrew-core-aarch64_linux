5a65b87b7218c4b3f9ae816c01a7f33a36108842
messense <messense@icloud.com>
2022-02-06 14:26:27 +0800

    bingrep 0.9.1
    Closes #94588.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bingrep.rb
+++ Formula/bingrep.rb
@@ -1,8 +1,8 @@
 class Bingrep < Formula
   desc "Greps through binaries from various OSs and architectures"
   homepage "https://github.com/m4b/bingrep"
-  url "https://github.com/m4b/bingrep/archive/v0.9.0.tar.gz"
-  sha256 "4db58c2d5b0fcf8c325ddb5ed6e63e2f1da5007743359bac1868587460657e4d"
+  url "https://github.com/m4b/bingrep/archive/v0.9.1.tar.gz"
+  sha256 "25db3c1d3e8c8dc9a41c6b87199f320c79b1a2706e8a4f68d2590fce71dabf8a"
   license "MIT"
 
   bottle do
