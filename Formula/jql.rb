75a674293fc555ca3d687d4191e866baf8882814
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-06 14:06:43 +0000

    jql: update 3.1.0 bottle.

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -7,12 +7,12 @@ class Jql < Formula
   head "https://github.com/yamafaktory/jql.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f3f23b7135a99a8be57324b118d3031401ed6f87b9fcbc1de5b9e030f17e68f5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "42012d5573eaad9be80a22345db834b761e7b5528795da736bd68bc8f9662c1f"
-    sha256 cellar: :any_skip_relocation, monterey:       "ffddbc9e1416eafab6842989187d6e9556732b8060f6dfb7362f298b70c08775"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fcad1128efe6a75adb4549b11c664137b68a0ee95a26663f05c2de66f322b4b9"
-    sha256 cellar: :any_skip_relocation, catalina:       "c47383bf417c5428f2276eef508599332b906909560b63c40f98eded895ec281"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a96afcf83b84f585136d3014a63fe1657e6f2ea9086102f4b47830a45aaefe26"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "72ce102d60f8c9611ecc22e8e8e0d0a29b1a8de4a876d51465ef258c4cebeb82"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "55f9f3ed8fff8c2ce34d445c61edaf79de51ab3668c966b03215dc5253fc9d4a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4a71d08be88dee6075221f46f0e16863f2976ad82f8df0fe57e50f1408760c36"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e9e5438eed53f8c76b6f838747ed166eddf626fe8f059c7af0d17a5bece427cb"
+    sha256 cellar: :any_skip_relocation, catalina:       "15005cb2033d052aa3adca59ad48925ec1f22fb6dbd464920154a3cd110b0004"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41c749a73995fe80c4fbef9f721e1ad9ea578b0638bfc96e016a27bbb6c4e0fd"
   end
 
   depends_on "rust" => :build
