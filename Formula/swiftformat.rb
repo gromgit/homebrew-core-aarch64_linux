c64b0a165632b15977707d78a60bfb74e390e0c1
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-02-07 23:09:56 +0000

    swiftformat 0.49.4
    Closes #94660.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,8 +1,8 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.3.tar.gz"
-  sha256 "9ba69aff222e89d75491c974f4fba157fab2beea98d6d63d846b2d197f494f68"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.4.tar.gz"
+  sha256 "34ecc5a04fce9f3ed63359d5f8529474eabb249a93e45d3e1df34b787b9a7899"
   license "MIT"
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
 
