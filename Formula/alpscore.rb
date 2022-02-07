849974b88efb2b2b3b7e9546b905890f7aeac8d1
Dawid Dziurla <dawidd0811@gmail.com>
2022-02-07 18:30:03 +0000

    alpscore: add 2.2.0 bottle.

--- Formula/alpscore.rb
+++ Formula/alpscore.rb
@@ -6,6 +6,15 @@ class Alpscore < Formula
   license "GPL-2.0-only"
   head "https://github.com/ALPSCore/ALPSCore.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "60542a579fdbcfd7e115a21906681ef53ba08541a6811d9adf3ce1de11390f88"
+    sha256 cellar: :any,                 arm64_big_sur:  "3fb8eeebac63feda029c063b3a74de6c9998e97b88431618b8b724c55f743b10"
+    sha256 cellar: :any,                 monterey:       "c84c178fa07cd5d9aad47530baae782ec008945486fb52551385668f4ec27ca8"
+    sha256 cellar: :any,                 big_sur:        "578b46e4adf6a968c29f6e33b30ccdfb1552d9a209a5acc1410bd755b47a80b0"
+    sha256 cellar: :any,                 catalina:       "eacb1c74617905918455e0b6bb4cbcd2b8404582ab360d6e4c3e7fd02bcb920e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae800598e03dde4b7e42035be7fc8b2858276ea5ef02d207882dd3f13d85c1c8"
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "eigen"
