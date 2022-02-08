650e583505ae29449eca81d6f319ac54d8f06b71
Grigorii Merkushev <brushknight@gmail.com>
2022-02-08 14:37:36 +0100

    newrelic-infra-agent 1.23.1
    Closes #94718.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.23.0",
-      revision: "c19c3bad1913255fa1438775eb10b77ecb5f09c8"
+      tag:      "1.23.1",
+      revision: "c380b5c1c3681117f6ccc295d797678b04f992c2"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
