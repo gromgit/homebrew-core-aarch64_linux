681e1dafaaa1dd0028ce9788d076d6cccb797a31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-08 05:28:02 +0000

    tika 2.3.0
    * tika 2.3.0
    * tika: update resource
    
    Closes #94689.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,9 +1,9 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tika/2.2.1/tika-app-2.2.1.jar"
-  mirror "https://archive.apache.org/dist/tika/2.2.1/tika-app-2.2.1.jar"
-  sha256 "9fc945031d45f1601f5cd55a560b412a88a5bb66b909506be0cc8110a52ffdf0"
+  url "https://www.apache.org/dyn/closer.lua?path=tika/2.3.0/tika-app-2.3.0.jar"
+  mirror "https://archive.apache.org/dist/tika/2.3.0/tika-app-2.3.0.jar"
+  sha256 "73e93aa3b6325227d4f1da6ce9d18695437240b45f3610337c3e7faa084274ad"
   license "Apache-2.0"
 
   bottle do
@@ -13,9 +13,9 @@ class Tika < Formula
   depends_on "openjdk"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.lua?path=tika/2.2.1/tika-server-standard-2.2.1.jar"
-    mirror "https://archive.apache.org/dist/tika/2.2.1/tika-server-standard-2.2.1.jar"
-    sha256 "0bb16f8d07a41126a8ea7b94d9c715a1411a573d7c4217133b26972ee0f74c68"
+    url "https://www.apache.org/dyn/closer.lua?path=tika/2.3.0/tika-server-standard-2.3.0.jar"
+    mirror "https://archive.apache.org/dist/tika/2.3.0/tika-server-standard-2.3.0.jar"
+    sha256 "45df8a64a5090d13109d30644a00f705282bfea0cae0948e93b2ec834c14b192"
   end
 
   def install
