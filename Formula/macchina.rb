e3cba28a2897f7d3de201fdd90b82a4d5b617814
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-08 05:18:40 +0000

    macchina 6.0.6
    Closes #94680.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -1,8 +1,8 @@
 class Macchina < Formula
   desc "System information fetcher, with an emphasis on performance and minimalism"
   homepage "https://github.com/Macchina-CLI/macchina"
-  url "https://github.com/Macchina-CLI/macchina/archive/v6.0.5.tar.gz"
-  sha256 "88de2c9718e071dcd9486cf1e7d87d46533100e589d99cd7b18ff43c21a8a053"
+  url "https://github.com/Macchina-CLI/macchina/archive/v6.0.6.tar.gz"
+  sha256 "da77e1899b13e4612b5ca6a22e8e266beabc734153e7a59c7c8b82c142510435"
   license "MIT"
 
   bottle do
