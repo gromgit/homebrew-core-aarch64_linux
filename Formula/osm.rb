3ab35783ff237ad8e44e290bf915641f8224f4cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-08 07:05:46 +0000

    osm: update 1.0.0 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -13,12 +13,12 @@ class Osm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c83f900219fa1b556c68a1eb7761f8d0bb0ad3dc6072977ad36eb6fadc7e4eec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "339a133e39c101135a735c6d6a42210fbe82be3fa022b602913bbef7d51c6cba"
-    sha256 cellar: :any_skip_relocation, monterey:       "f486ee23471840560a9efb385661693e4215c3a7c4e05d7974e8b4f980e4e958"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6c364faa784fdcc6459263149144ba6ce0c153e53afb5f6aa638a8c8508282de"
-    sha256 cellar: :any_skip_relocation, catalina:       "691b86178c205485e2dcf9e904b09904418abe2371cbb9c930cfd56c0220ed37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8179406aa361fa495fc40f24d778cac43af2940b77abd135ea1d4b0edda7f6f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "35ecdd0a7ed86d6e60d30714ce6e17f2078d559dc1c5ab21c7465ab680a24c96"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "722102c6b0bc60a31b4f809142b23ddb35f9425337555cb1e4bf8a7e3a63415e"
+    sha256 cellar: :any_skip_relocation, monterey:       "3fff2216b51719f1d89de6d5598bada47d99f8cf12b33f2022fd29187db126f4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ba298b991a42baaa06746c805bad695a1b116e3c752e3d0880e7f52423833025"
+    sha256 cellar: :any_skip_relocation, catalina:       "0676bb631e653e08f4ef70ccaa47da52fd3cb9b426e4545b67194643c30a6add"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "206f1c52d1bc05bcb021033972e78c6c32be0098ef94fc714826005da6d73408"
   end
 
   depends_on "go" => :build
