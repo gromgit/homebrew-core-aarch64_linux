9a30f29fdd20d06219ae96140a3faedc9e019afe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-08 06:09:41 +0000

    matterbridge: update 1.24.0 bottle.

--- Formula/matterbridge.rb
+++ Formula/matterbridge.rb
@@ -6,12 +6,12 @@ class Matterbridge < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f8f8d98ab840fc86ab6bdfcb9a680d133690afea8ca4302fd6b3c339019b469f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1e8132fc4300ea7eb8d10bf32e2f421e8b61aff1ff1eb89fa399d8732d3953f"
-    sha256 cellar: :any_skip_relocation, monterey:       "673c4d9547eabce6abe1844fdbf58097ce1761bb326a3de8fcd699c6f5ba4054"
-    sha256 cellar: :any_skip_relocation, big_sur:        "56c14f672c316a1710e8438ea2b8ebf3b1f5510acf1eb64c2718a6ad9a1bd73f"
-    sha256 cellar: :any_skip_relocation, catalina:       "ec8d0ba76fded16e615b5c3e1a484e8223dda43d122c76f24117cbe147c08b72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0ffbac30e1593eedec4650a3b5cf73a46f14c975230fa3dc970e19566839898"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a1dc605ef22d7b5bd92536b4e31322b40016a6ffbc84840ebe0e0904bc1f850"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06b43e635594d8f047b3d42b18cc56a7b68e1aa00fc69e057a4386e66c24b9b3"
+    sha256 cellar: :any_skip_relocation, monterey:       "9191ba95abee3c87c76a15372694aad7daa97d29fcbad693d818835281509637"
+    sha256 cellar: :any_skip_relocation, big_sur:        "588954e17bb83b13befc6effd3f4538f6b28704f50042e4217310c6e6fa74f67"
+    sha256 cellar: :any_skip_relocation, catalina:       "023d20e5a8d51e1ca2e57fc6fa6fea56eb658e29a6dc94d39ca129ace23e5cb1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f835ac8526405d0c152b132b344bb126daf3e0665f2679a6bc76dedc57e42619"
   end
 
   depends_on "go" => :build
