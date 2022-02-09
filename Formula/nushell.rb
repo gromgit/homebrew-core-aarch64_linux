99b8f828181963a69d5c591146549bb53331479b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-09 09:16:33 +0000

    nushell: update 0.44.0 bottle.

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -13,12 +13,12 @@ class Nushell < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3fd1cf91545339da414b6c5d28deb6decec46fc9d70ce60e704d453d9c6e5c35"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e9da5c5697a65e845ca1c155f55b143e74e2df4dd6d13d0ec8bfe9eddeb0a73"
-    sha256 cellar: :any_skip_relocation, monterey:       "cdaae79a20e32787380660663f1d0594c1f965b5c000832e26283f39b9feee13"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5ea5fc604ea1a7ae555403ae98127a365d4364731c1cb716139f5fb90859569e"
-    sha256 cellar: :any_skip_relocation, catalina:       "28d8f5ff90381b5d65584e5defc702a3586f42c2d8d5d83df6c2eee9832e42e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76d019423b8f4d41bca9fa15c9df16e7ce4636af993e73260a461ae667ba0bfe"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "55b4c2b4217b6e57ac905fb4d5ccd01f023f2eab61a6fc48a591fc46752e4b2b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68375be184031009b9ba3f70f88329c4b30b01e2c33f7b7b5fb7eab2c3e9d877"
+    sha256 cellar: :any_skip_relocation, monterey:       "d95cf2c196375b7a7bd7af0da22e67dafd99f7b751bbf73ec0dfd7fe2b4bba56"
+    sha256 cellar: :any_skip_relocation, big_sur:        "44a3ae04dda52e4e50ca739324852b9f470345d4e5e72311e7ad861ec9c13eeb"
+    sha256 cellar: :any_skip_relocation, catalina:       "d574f9a36d9f381e2dec16d19145653af8198b06a4b7c0a0fbc4875c8e54bdd9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19bcbcbb435742a69e5806f09e5b8199fc224d70cfc218fdbb83965cb9a05c56"
   end
 
   depends_on "rust" => :build
