a8cbd0d7c56c83694096becd5c5aa693a2c279a4
Aladdin Koraiem <alaadahmed@gmail.com>
2022-02-09 23:16:08 +0200

    elixir 1.13.3
    Closes #94798.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -1,8 +1,8 @@
 class Elixir < Formula
   desc "Functional metaprogramming aware language built on Erlang VM"
   homepage "https://elixir-lang.org/"
-  url "https://github.com/elixir-lang/elixir/archive/v1.13.2.tar.gz"
-  sha256 "03afed42dccf4347c4d3ae2b905134093a3ba2245d0d3098d75009a1d659ed1a"
+  url "https://github.com/elixir-lang/elixir/archive/v1.13.3.tar.gz"
+  sha256 "652779f7199f5524e2df1747de0e373d8b9f1d1206df25b2e551cd0ad33f8440"
   license "Apache-2.0"
   head "https://github.com/elixir-lang/elixir.git", branch: "main"
 
