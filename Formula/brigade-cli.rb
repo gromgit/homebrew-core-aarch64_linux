32b1b8412f0870a010aac28a51867c0d91f4b98b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-10 19:05:02 +0000

    brigade-cli: update 2.3.0 bottle.

--- Formula/brigade-cli.rb
+++ Formula/brigade-cli.rb
@@ -8,12 +8,12 @@ class BrigadeCli < Formula
   head "https://github.com/brigadecore/brigade.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f4dd1f838e598091458ef918d05bd852942c7067a1615f1f1e702a9f15352e6f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "863bf65be33e8a9e986e63b04f5b7ab9673d24f484af96e8ed1bdf3d04a92ca5"
-    sha256 cellar: :any_skip_relocation, monterey:       "b587ad76c9d46dc5d2acd959849c7bc07b3b9bbac02d6d3f48d4a8735d5083e8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c20b9b671b50a9827bc60f64a8312f246d772eaf2995fc376210d43521ef6cb0"
-    sha256 cellar: :any_skip_relocation, catalina:       "ae703f89adbe8a4a856209b3361eff903c523201a60ac7aa4caa343c0c3697a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b72328b4955ca9bf9c25e445d5157147550783e516069e7a590f973b41d740af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2559e91eaa1b6209fe3f77759b86fab5346e5ddb277a45c7c42201e865e546ff"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c080b4b201eb38ac321bb0d9f8106a8d1f269cc18411cbb03a1a83b05e36c7c7"
+    sha256 cellar: :any_skip_relocation, monterey:       "19c423a645438269645ec87bccc5921c1410892cf7c2727e4dd8025cdfef3732"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c58cf3a9c3fe20eee54262f3cb2a1ca227dad97e9b0395f53e81436528450a67"
+    sha256 cellar: :any_skip_relocation, catalina:       "5a833713e21e69a53ee2dadde2672c040a9e4d6824f84ab7da8ad862a243ed38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aba65a916c2552a341f6e3e945bf9d3d25fd353ef5d6daa626487fd8777eac6f"
   end
 
   depends_on "go" => :build
