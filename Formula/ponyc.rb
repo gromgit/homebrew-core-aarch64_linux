21ad6c0f278f5a59c8cd08f4594737d83cbdae31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-10 15:10:57 +0000

    ponyc: update 0.48.0 bottle.

--- Formula/ponyc.rb
+++ Formula/ponyc.rb
@@ -7,12 +7,12 @@ class Ponyc < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1645f4eaadab03a24705be15a1350007a375040b3ccd71e4d7b7fc43e399f84b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b1e34b2f4237dacc80ddcf3eb2c8cfc09277990edb6cc104a518d116f2e89e20"
-    sha256 cellar: :any_skip_relocation, monterey:       "82154810c921a8f10a7bca4c304a34fb70a48fcf642d623bdced7dc34d8e4330"
-    sha256 cellar: :any_skip_relocation, big_sur:        "780fa5e4f22a6cf5d3af56a74ff4dba9a70f79668cb86a6b7eea1b0db1d8854b"
-    sha256 cellar: :any_skip_relocation, catalina:       "9fcea4a7a4e31f3f984a1298cf584e7086cd171d30cc13c70941967c24785ed8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63387ce10637660df540920a178cc8b4c4bb9e4ef993608320b47ca0f9703f98"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2c8f81baf0dbf4539bc6dda139fd46a5a70521cc739f9b2507d1c730c37b36b8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b9092cb374423746b3b1a4fb1449ce8a2b0d652c507b582a75d4862a74a7b2b"
+    sha256 cellar: :any_skip_relocation, monterey:       "54fc22099af71cb7dd8f31017a2a2ad0f4c0b320852632e3e2368553aeb1b8e9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b4f3806405fd19e372ebe2c277b9a88932f52182668b8f3f07a9e6d68a5e3d0f"
+    sha256 cellar: :any_skip_relocation, catalina:       "40610701ec8983ed84108be8e41ecac49eafacd495a6cc1782d87dd10cc729a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21e960076f0b3b358ea58d9e2a71e03c12f1c947fae0d25f66742f7d7409428c"
   end
 
   depends_on "cmake" => :build
