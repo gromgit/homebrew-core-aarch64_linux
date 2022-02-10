3e8b3be0c57cb04968b84f9991f36c09baf9a06f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-10 15:11:08 +0000

    xidel: add 0.9.8 bottle.

--- Formula/xidel.rb
+++ Formula/xidel.rb
@@ -10,6 +10,15 @@ class Xidel < Formula
     regex(/^(?:Xidel[-_])?v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "724da191fd997aadd63d61cc7f1fc90e9cf2bb9ef76e478b6a46e199f93f7a31"
+    sha256 cellar: :any,                 arm64_big_sur:  "2bf1944684e71b179ea8d128e753b4d038a2028fdaa6fd818bdbc8cb6598bd39"
+    sha256 cellar: :any,                 monterey:       "8386db4ca45746f4a6163a8e39f4720e5782e01b92af7b042e1c2bebaf5d2f21"
+    sha256 cellar: :any,                 big_sur:        "e8ff32f1fb0cd5691c9e6afecc8c61f81d00aa5f2ea7a6cd96eed04090e5fe9d"
+    sha256 cellar: :any,                 catalina:       "e074bd06a5093ce2f89ccd48568adfdf1a356dae797ea5034daf23b99e9b967d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "89385fcaa91e5e59a4efdfafdd32e4748d858bad370efec8fb86757773a46a84"
+  end
+
   head do
     url "https://github.com/benibela/xidel.git", branch: "master"
     resource("flre") { url "https://github.com/benibela/flre.git", branch: "master" }
