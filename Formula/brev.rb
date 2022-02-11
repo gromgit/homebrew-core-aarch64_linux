a25d06326ceddeefca6bc4f2d5cba24d03d0cf14
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-02-11 19:45:54 +0000

    brev 0.6.17 (new formula)
    brev 0.6.17
    
    Closes #94960.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/brev.rb
@@ -0,0 +1,18 @@
+class Brev < Formula
+  desc "CLI tool for managing workspaces provided by brev.dev"
+  homepage "https://docs.brev.dev"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.17.tar.gz"
+  sha256 "cf69be0558cd87ca5838372aeca6b85fe937f1bd8eb44c03a88355ca088aa9ea"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-X github.com/brevdev/brev-cli/pkg/cmd/version.Version=v#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    system bin/"brev", "healthcheck"
+  end
+end
