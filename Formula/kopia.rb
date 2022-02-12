35bee406984db3454e932476114fa6c60a61801d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-12 21:06:40 +0000

    kopia: update 0.10.5 bottle.

--- Formula/kopia.rb
+++ Formula/kopia.rb
@@ -7,12 +7,12 @@ class Kopia < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cc40c8e0ed974ba5ea8a3cafa05a5dd8a5400fbc72fd8585391314fd23ea2b55"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "41866d63f5cc6cdb0e56bafd621b7252f784c3de160e85f0516e163a58c685d9"
-    sha256 cellar: :any_skip_relocation, monterey:       "54cc61c3d4eb9a92c321bbace006b40ae3a031d6ebfb5eccf4ad35b8be5fad89"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3ead702b48aac3e940399b81a6e09470ba8197c71d757d64419e7aeea0d8aef8"
-    sha256 cellar: :any_skip_relocation, catalina:       "707d905cd012de78f58091fc9648f28a2355cb9058838236978ec284ca67d282"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6920ca4acad3ad3673999d8c353af39882235745e1502e01fa8e3e36b2f60d95"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a9aace7affc33b85370b794d2dc595faceb2ce9658b683e3f71b90d2307b9db5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4520ba7f3577c1df2c488fda9c1fcc67c34576a72d811e78ad588321f590734a"
+    sha256 cellar: :any_skip_relocation, monterey:       "79263d89764b2b139f1f1353b3315b6873e39d102b553e9ae7b59e9dc66747c6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "25b0e3cc9254cf39629f86abf04a31cfbd7e246699f1ca94ccb6e91e78688c5c"
+    sha256 cellar: :any_skip_relocation, catalina:       "5b9161d189b341d76bbba128919b874179894c0e6da6075bca80eb863fd72dda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "575d2c46d17999589f9efda01c41f2cd018bea6b780cf13931acccb741a7f101"
   end
 
   depends_on "go" => :build
