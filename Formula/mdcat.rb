d6f6bdb952b0436211b50d6928ff6282f8ca591d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-12 23:06:01 +0000

    mdcat: update 0.26.1 bottle.

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -7,12 +7,12 @@ class Mdcat < Formula
   head "https://codeberg.org/flausch/mdcat.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "19e450a947d840bd6ed967f796c3ac20b645907b5a5a7e723cd294601bb5e38e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ddb52338aae3c74c93785f2032a2d444c6741715742731c1755d9547e90eaff"
-    sha256 cellar: :any_skip_relocation, monterey:       "1fce50454732f9dfc98e0675946dd9f3c8cab5d38537cbd286413d8404329406"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0ae99f84394b6ab2a8c2ea7461d8f8dd00d05c189223f5186fa206fd282f2940"
-    sha256 cellar: :any_skip_relocation, catalina:       "bbf93e222fc49642ba5dde42ef7bb1ef6128aac7df091cc1b9260b34c579ec4b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1dbc2a75907812174e291b1ba20f784645750cc9bd82827831df3e87c39ca3f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "12f57010161981a444c3026977e1eebeda9f3719e85d8036eb3c01426055fe6e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "10f6d24300205f1d513976d8abe7a7dfe1efd045383956fca7c19272ca9896b2"
+    sha256 cellar: :any_skip_relocation, monterey:       "b2dfb2acadf101ca418cc96a2f3de30ac03fab1b7d7977f47e13e880cdb5cc9f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2dacdc8793ee6cedc0fa76db28153fb57c27ba42260714d3fcbec84fdad800d3"
+    sha256 cellar: :any_skip_relocation, catalina:       "9ab6093b76b7903b60780ad348a4780087ca5cdc19052e851a198e8920f413da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9fdbe394e6c5ba0e940d52ed35aeea543ba679b20bb577a75f825d549280890"
   end
 
   depends_on "cmake" => :build
