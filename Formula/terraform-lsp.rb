51b44181071794316c1be6fac18c3714210c0d1e
rui <rui@chenrui.dev>
2022-02-12 19:31:36 +0000

    terraform-lsp: add 0.0.12 bottle.

--- Formula/terraform-lsp.rb
+++ Formula/terraform-lsp.rb
@@ -7,6 +7,15 @@ class TerraformLsp < Formula
   license "MIT"
   head "https://github.com/juliosueiras/terraform-lsp.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ecb868c9f9037a6797df54f8280d01309e34e700a4b26bced14555e8287ef96f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d30386f77e1057c954a62ff46ced4e7b6cf8ba69b26bcbf137200932d498e788"
+    sha256 cellar: :any_skip_relocation, monterey:       "10cca16bfcddb58b30bfbe1e3a1aea9c58e4433b9b6260e0108d86fca7cb48c1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4f3c322749538d6e2872b0c7741d448e60d8552028c378a37ec91fc1fe9f1ab0"
+    sha256 cellar: :any_skip_relocation, catalina:       "0606bf7a3d018590555ff5060a38dcec78f57a11927791e7f20caa614caa49db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7eae59625f858958621455404b365659464230b5a54783cb20be44e4569d539f"
+  end
+
   depends_on "go" => :build
 
   def install
