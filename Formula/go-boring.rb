6a429299e4d00f7c13e51bcdb2c7845a68574bef
Rui Chen <rui@chenrui.dev>
2022-02-12 16:01:33 -0500

    go-boring 1.17.7b7
    Closes #95026.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/go-boring.rb
+++ Formula/go-boring.rb
@@ -1,9 +1,9 @@
 class GoBoring < Formula
   desc "Go programming language with BoringCrypto"
   homepage "https://go.googlesource.com/go/+/dev.boringcrypto/README.boringcrypto.md"
-  url "https://go-boringcrypto.storage.googleapis.com/go1.17.6b7.src.tar.gz"
-  version "1.17.6b7"
-  sha256 "5178f595dea6d618f7cdf6b9e2346a1a28430b4d547996b766fcd5b34379ca2f"
+  url "https://go-boringcrypto.storage.googleapis.com/go1.17.7b7.src.tar.gz"
+  version "1.17.7b7"
+  sha256 "eff84edf4f676c691ac21747277e15777789be32dcaa53a0779737b280351453"
   license "BSD-3-Clause"
 
   livecheck do
