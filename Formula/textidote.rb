8277620df3d5dbf1d4f1e40fcb69261844ecec65
rchaput <rchaput.pro@gmail.com>
2022-02-13 16:47:55 +0100

    textidote 0.8.3 (new formula)
    * textidote 0.8.3
      Added new formula, textidote: a correction tool for LaTeX documents.
      The formula includes completions (for both bash and zsh), as well
      as a few tests.
    * fix textidote 0.8.3 (new formula)
      Added suggestions from the code reviewers:
      * Changed license to GPL 3 or later.
      * Target JDK17 since it is provided by openjdk.
      * Avoid unnecessary calls to "mkpath" (already handled by brew).
      * Test that the return code is exactly the number of warnings, instead of ignoring it.
    * fix textidote 0.8.3 (new formula)
      As suggested, use the current OpenJDK (major) version as a target when building the app.
      Thus, we should not need to modify the formula when new JDKs are released.
    * fix textidote 0.8.3 (new formula)
      Fixed the formula (syntax error)
    
    Closes #95047.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Remy Chaput <rchaput.pro@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/textidote.rb
@@ -0,0 +1,49 @@
+class Textidote < Formula
+  desc "Spelling, grammar and style checking on LaTeX documents"
+  homepage "https://sylvainhalle.github.io/textidote"
+  url "https://github.com/sylvainhalle/textidote/archive/refs/tags/v0.8.3.tar.gz"
+  sha256 "8c55d6f6f35d51fb5b84e7dcc86a4041e06b3f92d6a919023dc332ba2effd584"
+  license "GPL-3.0-or-later"
+  head "https://github.com/sylvainhalle/textidote.git", branch: "master"
+
+  depends_on "ant" => :build
+  depends_on "openjdk"
+
+  def install
+    # Build the JAR
+    system "ant", "download-deps"
+    system "ant", "-Dbuild.targetjdk=#{Formula["openjdk"].version.major}"
+
+    # Install the JAR + a wrapper script
+    libexec.install "textidote.jar"
+    bin.write_jar_script libexec/"textidote.jar", "textidote"
+
+    bash_completion.install "Completions/textidote.bash"
+    zsh_completion.install "Completions/textidote.zsh" => "_textidote"
+  end
+
+  test do
+    output = shell_output("#{bin}/textidote --version")
+    assert_match "TeXtidote", output
+
+    (testpath/"test1.tex").write <<~EOF
+      \\documentclass{article}
+      \\begin{document}
+        This should fails.
+      \\end{document}
+    EOF
+
+    output = shell_output("#{bin}/textidote --check en #{testpath}/test1.tex", 1)
+    assert_match "The modal verb 'should' requires the verb's base form..", output
+
+    (testpath/"test2.tex").write <<~EOF
+      \\documentclass{article}
+      \\begin{document}
+        This should work.
+      \\end{document}
+    EOF
+
+    output = shell_output("#{bin}/textidote --check en #{testpath}/test2.tex")
+    assert_match "Everything is OK!", output
+  end
+end
