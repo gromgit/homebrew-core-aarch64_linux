a3dae9f23343886da9f9a07a7a83c04df6d878ad
Bevan Kay <email@bevankay.me>
2022-02-15 10:38:02 +1100

    bitwarden-cli 1.21.1
    Closes #95109.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class BitwardenCli < Formula
   desc "Secure and free password manager for all of your devices"
   homepage "https://bitwarden.com/"
-  url "https://registry.npmjs.org/@bitwarden/cli/-/cli-1.20.0.tgz"
-  sha256 "7c2fe1cd213c36fa8d86205e0300ddc5557857d11ab73fa41e34bc9fe51a1003"
+  url "https://registry.npmjs.org/@bitwarden/cli/-/cli-1.21.1.tgz"
+  sha256 "5df82a6ec7ecab6ed587ff3675f88b926404ed4e59fb912956722d840a742b8a"
   license "GPL-3.0-only"
 
   bottle do
