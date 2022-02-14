860c4705d4fbc69374ee88d5592aed4883f18cdd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-14 23:05:32 +0000

    remind: update 03.04.00 bottle.

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -12,12 +12,12 @@ class Remind < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e6bc38a9340f48278b1a8753566f338379f3889b1ba45f632fc5dc80bd5c0e3e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d30e36523053c01dce20c8090ffd04774d93cdc912fa963c3f73fe228f81e9e"
-    sha256 cellar: :any_skip_relocation, monterey:       "5cdbb26b9750ad704d8224c17a3387f03fd711aa80b75563792bdcee29a68b42"
-    sha256 cellar: :any_skip_relocation, big_sur:        "016e021500b05d5ad420f54564125be50807d3ca9764c7a02260f5ca05a0baee"
-    sha256 cellar: :any_skip_relocation, catalina:       "5ed2f039bc0730edf9de3b416b4df24ab4c5ce0e919c8b5c2c8cb9a019340733"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ee7764c920542b2d990f5d8eb44dd0cc3a3bb042ef6aba084d826d9f1ae503d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9573483c77af49e52088d766d3c0f548789f3e709d4ec35a35653d3c68711024"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7066f56602d865a30b595cd90f847cf1a5cddc9d9122701bf5ef4aaa13cd785"
+    sha256 cellar: :any_skip_relocation, monterey:       "449e45e90bd3764aecb2deaa736563d6e18129cabfa7537204fbb32daf4a0d1c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fed7ca6f5f2d6cbaa515cc7162fd63816a9c6272521d42d3e07539d8174ebeb4"
+    sha256 cellar: :any_skip_relocation, catalina:       "50962095453bfac586ae0977e6bb7fa309344f756670e43496e97ba1a075ac07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "22a3a3df2462715a1107fd0c4811808545b1d9986240832e50b3e15b3b37aa1d"
   end
 
   conflicts_with "rem", because: "both install `rem` binaries"
