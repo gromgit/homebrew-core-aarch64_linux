78ce0b8efe5ca16b9607363f5ac48b273492ecb8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-14 20:48:40 +0800

    fb303 2022.02.14.00

--- Formula/fb303.rb
+++ Formula/fb303.rb
@@ -1,8 +1,8 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/v2022.02.07.00.tar.gz"
-  sha256 "82724de4c5ce5667fabfa56114b7432813a2aa620faf3ae1d41ecbe1a2c1910e"
+  url "https://github.com/facebook/fb303/archive/v2022.02.14.00.tar.gz"
+  sha256 "2f517506dc6cdf17bdbe4a2251be6c47eee77887650d83970741f2beee35743e"
   license "Apache-2.0"
   head "https://github.com/facebook/fb303.git", branch: "main"
 
