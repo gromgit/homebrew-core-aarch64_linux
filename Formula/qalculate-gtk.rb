0cdaf100bb98e405204163eb87514049529d435d
Minghua Zheng <m.zheng2@outlook.com>
2022-02-14 21:31:57 +0000

    qalculate-gtk 4.0.0

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -1,8 +1,8 @@
 class QalculateGtk < Formula
   desc "Multi-purpose desktop calculator"
   homepage "https://qalculate.github.io/"
-  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v3.22.0/qalculate-gtk-3.22.0.tar.gz"
-  sha256 "ba6c0238b5f926ac94e234e15a2dfa84215938da5df6fea136db75c5db488556"
+  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v4.0.0/qalculate-gtk-4.0.0.tar.gz"
+  sha256 "51f705737fe6defb6b57058ddd357ca1dd11af57655a90f9e41a40e8dbb9d81c"
   license "GPL-2.0-or-later"
 
   bottle do
