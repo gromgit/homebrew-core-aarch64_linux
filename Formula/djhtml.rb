5cc1381c30442695efbc43e4e9ac72bae6baaa53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-15 06:07:17 +0000

    djhtml: update 1.5.0 bottle.

--- Formula/djhtml.rb
+++ Formula/djhtml.rb
@@ -8,12 +8,12 @@ class Djhtml < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d1a41b935d1de5261e14e8e18bcb5585111bc897baa3b170fc7501dff6810832"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7319a2c5864be8d3b5c2cf106507206d8ee2d856361e8d17925436a651d8e34d"
-    sha256 cellar: :any_skip_relocation, monterey:       "45505bea25ce5b1bae0793c5e50d832588fd1c3615c6669fe3f9da884632fa03"
-    sha256 cellar: :any_skip_relocation, big_sur:        "eef0bf283afb4e3c68b4d88ba402e2b695cb7d377b61b7131744e4f9841ce004"
-    sha256 cellar: :any_skip_relocation, catalina:       "dbc50ee9db05d985ef7b6259db13452bbfc7ee98255fdac0b6b0330d683cd413"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76215a1c81615c3a721cf93f63c07ac27f8af343e8118a08f650fb011a8dc992"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c716ae3f7e31dd6fd478b9cd128892c04fd58330f1f02ffaee6208015a9dc5a5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "db8591b1d3b05551a30cdc8409019395f90af4c92f7eba627d88081cf7a4d311"
+    sha256 cellar: :any_skip_relocation, monterey:       "727a8e1272e93ab3c433ca7f7a6bd2780383d7fb885a212faeb559b9824c679d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fda302276d2701c8c5439fb36583c597a99492f111ff747735f36d069fc8151e"
+    sha256 cellar: :any_skip_relocation, catalina:       "54b7dc6ca0517fef397e7302191f4aa7ac4cb5f871315ca9a1daf8deb4398303"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c93f336938e2a2deb9ec4f90204b257c9e82b488e5994dbb942418b20ab213d"
   end
 
   depends_on "python@3.10"
