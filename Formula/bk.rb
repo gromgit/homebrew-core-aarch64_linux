6632148fbc981dbbf67c2f4ef54952a644700bfb
rui <rui@chenrui.dev>
2022-02-15 15:24:33 +0000

    bk: add 0.5.3 bottle.

--- Formula/bk.rb
+++ Formula/bk.rb
@@ -6,6 +6,15 @@ class Bk < Formula
   license "MIT"
   head "https://github.com/aeosynth/bk.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "83888caacb2e9c2a3f168143958b4dc5d8398ed8372cfa30187780bac498d376"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7800246f87267b642a3c4c4cccd4363a26f58db3245bcdde0dd9c161a6a0ddf"
+    sha256 cellar: :any_skip_relocation, monterey:       "9c45def155516544d952e060e78b44c3e9973f5e90cd25828029133d48fbcfba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a0e03c86b30c329f2b77d7436d154c365588acdacdcf1681e1370f13302cdfc2"
+    sha256 cellar: :any_skip_relocation, catalina:       "2ffc3317ecb58f4e22c832a674e64b972cadcfc7e30a3598a820187fe90eb4e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d7a7201991a14e455d5912fe064b0a34db4882c0054e7f2094a00e11aaff3e16"
+  end
+
   depends_on "rust" => :build
 
   def install
