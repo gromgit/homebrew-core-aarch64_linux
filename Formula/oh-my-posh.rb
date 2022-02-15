b96372695f08fcd3bf04a2f7fe4823ecc063359e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-15 06:00:31 +0000

    oh-my-posh: add 7.18.0 bottle.

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -6,6 +6,15 @@ class OhMyPosh < Formula
   license "GPL-3.0-only"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d11ca81148a17fdeb57eb56035ecf5828f99dd499fa587513a81001a23e2128"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "511ae2937e25634142a7fa36ae7910ce96b92047bbc997c75b6bf65ef2c9fc9f"
+    sha256 cellar: :any_skip_relocation, monterey:       "dbe8641b8ffa936f9a56952be15ebd304ccc490e9425d286455f125cfb983ffa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ec1fb28f038cd74f18db3b3530b754764e0ee59cdb51cb291a3f42ea135224ee"
+    sha256 cellar: :any_skip_relocation, catalina:       "fb2d253e3111e6ad02f6450f50afebfc8bffbd1866da2d8e9293c5bbb7973d3d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03bd7155ea747adfa2474317e0b9f66313f92e2dc64b0a49104ce8e893ea0571"
+  end
+
   depends_on "go" => :build
 
   def install
