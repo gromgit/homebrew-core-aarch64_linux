170b70917b038e439f4d9cdda973170b545f3051
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-15 16:10:52 +0000

    vala: update 0.54.7 bottle.

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -6,12 +6,12 @@ class Vala < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_monterey: "38ba166e43db02c9d974b08d3206d6122b8979497cf6a6f250ea5d1fd71e68b1"
-    sha256 arm64_big_sur:  "ecc2603c29b855b1a2a096e2fadfe932992773f580ff68a47ca8707ac32cb7fe"
-    sha256 monterey:       "dd53beb4c72134ae0965c292e762d64e793f391655e6f547a805ae2d8fdf4c43"
-    sha256 big_sur:        "0a126eaa756b14302ad96dd2f65c74eab2a9fc31bca8107a0b3e3b0c05c1e0dd"
-    sha256 catalina:       "552cf9bd5758904e49bb68fd2843bf09b054ad4e40b652104ac20ef85ffbb2f9"
-    sha256 x86_64_linux:   "4ae96d3686511de7b69b52376c8a9ea8749a628349f236b1a0998fa657e24911"
+    sha256 arm64_monterey: "6843c183a9fcd2198de13cd7ce3de015c5aee0cd7831ef3095951207abbab6ce"
+    sha256 arm64_big_sur:  "3136cf5ba11f1eeabf2f85202acf53a3fc84ad5d3972f51c4910bedf84ecbd87"
+    sha256 monterey:       "98e39f8070fe543feaea94ca8e71889d778cdf26b0414e141c3c540470da36e1"
+    sha256 big_sur:        "d2eff8191696c583fc421dab73e3b9ca6c763dd4a1ef75304ea99ea25462b86b"
+    sha256 catalina:       "980ac9fbf9d12a5e5e61c692ae7fe5c530d1c4cbdf3c00273ba61a2b17df5080"
+    sha256 x86_64_linux:   "2030b988f35004147510120c2856663875e76a07d534fc124075e304a4d1df9b"
   end
 
   depends_on "gettext"
