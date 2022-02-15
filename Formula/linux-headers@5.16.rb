4d99efca39429f7981bf094d9dfd974a0079de9b
Michka Popoff <michkapopoff@gmail.com>
2022-02-15 23:07:00 +0100

    linux-headers@5.16 5.16.9 (new formula)
    * linux-headers@5.16 5.16.9 (new formula)
    * linux-headers@5.16: fix style
    
    Closes #95177.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/linux-headers@5.16.rb
@@ -0,0 +1,21 @@
+class LinuxHeadersAT516 < Formula
+  desc "Header files of the Linux kernel"
+  homepage "https://kernel.org/"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.16.9.tar.gz"
+  sha256 "ca84521580c9e2a5d7648a6b98150780b0e65bc8fac85c3a07d00b1b9aa68ad3"
+  license "GPL-2.0-only"
+
+  keg_only :versioned_formula
+
+  depends_on "rsync" => :build
+  depends_on :linux
+
+  def install
+    system "make", "headers_install", "INSTALL_HDR_PATH=#{prefix}"
+    rm Dir[prefix/"**/{.install,..install.cmd}"]
+  end
+
+  test do
+    assert_match "KERNEL_VERSION", File.read(include/"linux/version.h")
+  end
+end
