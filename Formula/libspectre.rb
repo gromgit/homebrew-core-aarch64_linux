18aad221bba7d63bfa33e4ac9f5b96bf06726dbb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-15 14:10:43 +0000

    libspectre: update 0.2.10 bottle.

--- Formula/libspectre.rb
+++ Formula/libspectre.rb
@@ -11,13 +11,12 @@ class Libspectre < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0c25377b7059c38ad35ba5cff9764113e46ff101341854cf0003d4886cf40063"
-    sha256 cellar: :any,                 arm64_big_sur:  "3d5c0d07bfb27f55e4cd938e5e12ced37b0fa8b0776460bfa8748e8d113ca0af"
-    sha256 cellar: :any,                 monterey:       "5a3f63e8e02536ba3ba0e2c1347fc69806cc9ab87029451f97be99d5251ecf54"
-    sha256 cellar: :any,                 big_sur:        "0690d94faad89f7c1bacf2729dbcc0f965475080cc94be1bebcfa352d605507f"
-    sha256 cellar: :any,                 catalina:       "7b2dbf0cce05aba6282aa3090b519c95584dc2021eaccecfc97b6c66c67ba4bd"
-    sha256 cellar: :any,                 mojave:         "aa33dcb97455ec208315f8a3d57b904bfa98e219b705f3992776f167b08b35bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "467c578adafbc5e93ae51017635192aa8696a642fe6ec458afce221092fc6049"
+    sha256 cellar: :any,                 arm64_monterey: "feb7cfda3ee56fa06a53f733f68d69c7ae69cbde08469e9a2c074cb9191db12d"
+    sha256 cellar: :any,                 arm64_big_sur:  "d534a89facbba541742df755c49b8fd1e3ccedda8aeb5b9428e7b818fa8dfb73"
+    sha256 cellar: :any,                 monterey:       "9a070c179f2fea6f85c4638c0a5cff5543fdccb7fa9157d8c49ca3c8170da540"
+    sha256 cellar: :any,                 big_sur:        "72fb8d9117c3922ac7bad067b974b45fe91ccabfe9396613d463be36d60033c5"
+    sha256 cellar: :any,                 catalina:       "252cdeeb91d17de587e7b93cf9bdec8a1c5ae95f1ac4af6e0ad540f20a1b354d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9ec982a240d4a7b5ced514c3842046e4beaf428595d3ec1b9575519dcf2b002"
   end
 
   depends_on "ghostscript"
