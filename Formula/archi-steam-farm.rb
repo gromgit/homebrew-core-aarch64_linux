d8c74690dbb87a6df8205a606dddc2db5e14bc41
Michael Cho <cho-m@tuta.io>
2022-02-14 20:06:59 -0800

    archi-steam-farm 5.2.2.5

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -2,8 +2,8 @@ class ArchiSteamFarm < Formula
   desc "Application for idling Steam cards from multiple accounts simultaneously"
   homepage "https://github.com/JustArchiNET/ArchiSteamFarm"
   url "https://github.com/JustArchiNET/ArchiSteamFarm.git",
-      tag:      "5.1.5.3",
-      revision: "cc59875769dbc3e525e000a500da888b02785abb"
+      tag:      "5.2.2.5",
+      revision: "29fae79aa03e8d916a387099dc6decd541386e81"
   license "Apache-2.0"
   head "https://github.com/JustArchiNET/ArchiSteamFarm.git", branch: "main"
 
