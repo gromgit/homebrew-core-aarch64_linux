765c41b1e4b5e9201a6ea0a207c2a8c77c600578
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2022-02-16 18:26:42 +0000

    linux-headers@5.16: add 5.16.9 bottle.

--- Formula/linux-headers@5.16.rb
+++ Formula/linux-headers@5.16.rb
@@ -5,6 +5,10 @@ class LinuxHeadersAT516 < Formula
   sha256 "ca84521580c9e2a5d7648a6b98150780b0e65bc8fac85c3a07d00b1b9aa68ad3"
   license "GPL-2.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "71cb27ac2bb2f30bf7eba3d3da5500d6b69d389b23d755902ef22b6afc1570b2"
+  end
+
   keg_only :versioned_formula
 
   depends_on "rsync" => :build
