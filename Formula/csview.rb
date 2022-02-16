77291af23fe6604cb14a8c25235e2761b277e9c3
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-02-16 08:32:57 +0000

    csview: add 1.0.1 bottle.

--- Formula/csview.rb
+++ Formula/csview.rb
@@ -5,6 +5,15 @@ class Csview < Formula
   sha256 "34df6838dd9407197511887cdc9b2a3ed08b4b508f9c6bb660def326ea953e8c"
   license any_of: ["MIT", "Apache-2.0"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea5d8b3edfe9549584da1f2550bf57c6a9ed6d415bc7cb1a113d429154989c6e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a0f4600bb7c94e7c021ba27155f25ca4d3af4605cb02a59c09b789e681a23815"
+    sha256 cellar: :any_skip_relocation, monterey:       "ae25c1b64d95399a046d79e1ad974e96c8578261b439496d04f6a5703ca8ecd0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f1a7702a85d3752f18609707e4c65ee0330de495b9c4c994c2ed28b4cbce96a3"
+    sha256 cellar: :any_skip_relocation, catalina:       "bfbf60817be5424b5f75a144b80d6da10e81a846b208ebf67c9073a0bb58a368"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4098aaa0251e12198718faa550477c8657fc56694ed78f43fc7a5813ccf5207b"
+  end
+
   depends_on "rust" => :build
 
   def install
