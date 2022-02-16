7417cac49c1852da366a952066c927a70a145886
Rui Chen <rui@chenrui.dev>
2022-02-16 16:05:49 -0500

    git-lfs 3.1.2
    Closes #95260.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -1,8 +1,8 @@
 class GitLfs < Formula
   desc "Git extension for versioning large files"
   homepage "https://github.com/git-lfs/git-lfs"
-  url "https://github.com/git-lfs/git-lfs/releases/download/v3.1.1/git-lfs-v3.1.1.tar.gz"
-  sha256 "668147fabf314d32b86ff1fe921155cd899621b24eed212bf4a3a80e440eb8db"
+  url "https://github.com/git-lfs/git-lfs/releases/download/v3.1.2/git-lfs-v3.1.2.tar.gz"
+  sha256 "5c9bc449068d0104ea124c25f596af16da85e7b5bf256bc544d8ce5f4fe231f2"
   license "MIT"
 
   bottle do
