07cc47bfe916e37e227962faaf49bc1375fa4828
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-16 16:10:40 +0000

    revive: update 1.1.4 bottle.

--- Formula/revive.rb
+++ Formula/revive.rb
@@ -8,12 +8,12 @@ class Revive < Formula
   head "https://github.com/mgechev/revive.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "87313e42a472abcdd943b266ee2e5aedb0e35190ec02691c91d98d07c6bf8e80"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "87313e42a472abcdd943b266ee2e5aedb0e35190ec02691c91d98d07c6bf8e80"
-    sha256 cellar: :any_skip_relocation, monterey:       "ab579952fb9f57161996b3e2e82cee54d15c3939d46cd97c8c2c60ea20edffb6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ab579952fb9f57161996b3e2e82cee54d15c3939d46cd97c8c2c60ea20edffb6"
-    sha256 cellar: :any_skip_relocation, catalina:       "ab579952fb9f57161996b3e2e82cee54d15c3939d46cd97c8c2c60ea20edffb6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3892ad1ad91059c5e5ded18f53d1bb963eb752ce24c83903e1f8b2a8979c18e3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "44e25ab98c22c8984d211a45109b05869724ec8db661373c9ad797732b876243"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "44e25ab98c22c8984d211a45109b05869724ec8db661373c9ad797732b876243"
+    sha256 cellar: :any_skip_relocation, monterey:       "061e8b155d55c20008d5aeb941d5f3cf62545366fa4107bf7a4d320a192386f5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "061e8b155d55c20008d5aeb941d5f3cf62545366fa4107bf7a4d320a192386f5"
+    sha256 cellar: :any_skip_relocation, catalina:       "061e8b155d55c20008d5aeb941d5f3cf62545366fa4107bf7a4d320a192386f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc9cbb36397b518dc7f84223eaad078f5819c5063be9d21692080a47315b7570"
   end
 
   depends_on "go" => :build
