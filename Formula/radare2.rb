1cd0a3dd07c7547b562507d4824051f91f273846
Porkepix <porkepix@gmail.com>
2022-02-16 12:40:57 +0100

    radare2 5.6.2
    Closes #95226.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -1,8 +1,8 @@
 class Radare2 < Formula
   desc "Reverse engineering framework"
   homepage "https://radare.org"
-  url "https://github.com/radareorg/radare2/archive/5.6.0.tar.gz"
-  sha256 "263bd171853b2f668dec20170550c8101215267f72457e68781c2a816e3ccfb1"
+  url "https://github.com/radareorg/radare2/archive/5.6.2.tar.gz"
+  sha256 "e1aa173b29f8c494e461b7a91022be33e6de6e15bf185e055fc999500e83bbac"
   license "LGPL-3.0-only"
   head "https://github.com/radareorg/radare2.git", branch: "master"
 
