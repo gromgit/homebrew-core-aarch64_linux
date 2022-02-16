38d1244c475236b79e10f5fb873802e94d86d52c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-16 12:06:02 +0000

    basis_universal: update 1.16.2 bottle.

--- Formula/basis_universal.rb
+++ Formula/basis_universal.rb
@@ -6,12 +6,12 @@ class BasisUniversal < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "593a6447170ce543fdd79e57374937e820fddab05b746a3c7344cad23156e101"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93af0b17489acb28c692ef8634312e428412d4e4a8dbc3cd02d5db1df8591069"
-    sha256 cellar: :any_skip_relocation, monterey:       "bd680f0d63293311b1b2bb839a8ff62254e0a6211ff4d8c9b415c0a810ac2c96"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8661008511d38f47a2a0765a45d482db70339578f930332d5aa47a647808f3de"
-    sha256 cellar: :any_skip_relocation, catalina:       "15dbfe6e9a2c91ae7e34583addd2461e692f05c7219449e49be5898a01e16f08"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b421053f9844cc7fe2465b6edf0a340d9cab4237da35857cd9b119f81198ffc7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a7ac9098df6c21052a799525237d519d0a51eddc48c657559a25e4460224848"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e7d04b017f2970b278442d1c93876750236bec6cf97cdeaaecf9e5acff6cdd2d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e09f9f13045a76c990ae7412be39057d915ba663225478ec6342908038205657"
+    sha256 cellar: :any_skip_relocation, big_sur:        "04dd92b15a1935d768c4ed1bf02005113e6ad7eeb488d6b9a4237659709191bf"
+    sha256 cellar: :any_skip_relocation, catalina:       "a007613e46d3594a34188c1e540985d845f014ec50b8297d34312a635662c0f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12dc500008fa872be4094cfa375e650395181f565baa3091e348618933d096a3"
   end
 
   depends_on "cmake" => :build
