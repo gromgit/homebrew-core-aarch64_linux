17b974d812472cc4092614a1e44e3751225ffc04
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2022-02-16 14:27:37 +0800

    minio-mc 20220216055401
    Closes #95206.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -2,9 +2,9 @@ class MinioMc < Formula
   desc "Replacement for ls, cp and other commands for object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git",
-      tag:      "RELEASE.2022-02-07T09-25-34Z",
-      revision: "dabc6c598e5e1fc6579140adc636beb15164d984"
-  version "20220207092534"
+      tag:      "RELEASE.2022-02-16T05-54-01Z",
+      revision: "220f7c217a8ff5067195fdee6c76d011cb099740"
+  version "20220216055401"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/mc.git", branch: "master"
 
