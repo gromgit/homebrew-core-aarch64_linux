da3344dee34ab989b2e94b2ccb5d532aa4254e2b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-16 05:21:19 +0000

    keptn 0.12.1
    Closes #95195.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/keptn.rb
+++ Formula/keptn.rb
@@ -1,8 +1,8 @@
 class Keptn < Formula
   desc "Is the CLI for keptn.sh a message-driven control-plane for application delivery"
   homepage "https://keptn.sh"
-  url "https://github.com/keptn/keptn/archive/0.12.0.tar.gz"
-  sha256 "c14ff6ed3a5f677da6c6d0eb9c56d72cefa154f871659ecd28733c4aeb8af435"
+  url "https://github.com/keptn/keptn/archive/0.12.1.tar.gz"
+  sha256 "57f6c41255550ff7002a95901f01059ad34962c33c0a04696b862c7d303f14ee"
   license "Apache-2.0"
 
   livecheck do
