1f06e41b1de9f0880755ebaac145714c1751fd0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-17 17:06:46 +0000

    golangci-lint: update 1.44.1 bottle.

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -8,12 +8,12 @@ class GolangciLint < Formula
   head "https://github.com/golangci/golangci-lint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b42eb53c7921abbbc6e05d055cfe43c52f4b98065b59ba7ebe6ec8f03d4ca51"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a6f3bb4ba129ebe89ebbd569675285cd7426e4f6daf3f4c3044ca9f2acdde2e"
-    sha256 cellar: :any_skip_relocation, monterey:       "95d5384656d52a3274b6b107c213930101d857ea54b84aa883ed296d4331055b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7300525291a8452685b37487e4e15adebd9f5545ba7c97068914abeccea47f04"
-    sha256 cellar: :any_skip_relocation, catalina:       "7492f258cdba289a32fe72befd57aa308f494cbe63943462a6a62e160dd271cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d25c23a542624e2dc083c5bc768ab31eebcc4a21f84ee35aa624958726a11cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f42528341e702bdbb53fb485d836911347b43996e9460cf63d7e304e06400ddc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc31e7befdb1020327d7248c4fbb89864a08d89937c777703bf1e84e87c2f847"
+    sha256 cellar: :any_skip_relocation, monterey:       "e5d2cc629c6b97d5c0c8d7622f75022de289db92aadaee378cfee0d1740f26ae"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d077bef9f443fa477459f7bd941347af688896746d63bbf380926995c2a100d3"
+    sha256 cellar: :any_skip_relocation, catalina:       "0bb937cb57bbf331da747cb1423f4bec62d50070366e1eb6e38b620c004b9461"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06e8fffbf1a9bc27738c4541f76f5b58e9b73dc6397da5681875bbe484ceca32"
   end
 
   depends_on "go"
