676e2af4ae8e54c41107aaf533e2490a4fc28b7c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-02-17 18:47:43 +0000

    gitversion: update 5.8.1 bottle.

--- Formula/gitversion.rb
+++ Formula/gitversion.rb
@@ -6,10 +6,12 @@ class Gitversion < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "dda1da1aeeaa5636493bc80a17263e5a8816a476b33661c7326422d27e69beb1"
-    sha256 cellar: :any,                 big_sur:      "5162d8ade4eecc0e4d35f4907a28942b0a2bcfbbcc73e65f500cb17fd49cebcf"
-    sha256 cellar: :any,                 catalina:     "0b00bd24212432502ddb04b1a04f98c6ee055056744cb20a253ee6b349aae894"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7b803214f62636fcd0fe455f5f6c0587124679e377b53486763d9322c7948b80"
+    sha256 cellar: :any,                 arm64_monterey: "476cbc76c69777456537b11d08044c896aada8b658523e771082095b70087de6"
+    sha256 cellar: :any,                 arm64_big_sur:  "185ad5a268d07c03b1ea6dde0b38be57ea7bafdf8e84e5626d3fc61edfa9763e"
+    sha256 cellar: :any,                 monterey:       "dda1da1aeeaa5636493bc80a17263e5a8816a476b33661c7326422d27e69beb1"
+    sha256 cellar: :any,                 big_sur:        "5162d8ade4eecc0e4d35f4907a28942b0a2bcfbbcc73e65f500cb17fd49cebcf"
+    sha256 cellar: :any,                 catalina:       "0b00bd24212432502ddb04b1a04f98c6ee055056744cb20a253ee6b349aae894"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b803214f62636fcd0fe455f5f6c0587124679e377b53486763d9322c7948b80"
   end
 
   depends_on "dotnet"
