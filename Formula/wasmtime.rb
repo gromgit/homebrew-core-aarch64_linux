48b6db43df44c232fe08841cfbf8852d7608ce4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-17 07:05:38 +0000

    wasmtime: update 0.34.1 bottle.

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -13,12 +13,12 @@ class Wasmtime < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7cd332068f1e32e92edcbdc5468d4081656e839be1c132ff029c9bdc1029362e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d65fd688253ed611c2940ce0f853b02dc9e5a7fe7c5dca0b9dd530e923ee5013"
-    sha256 cellar: :any_skip_relocation, monterey:       "cdc94eff30d625a7f7d92d45a35fb1e8ba743ddc2d688c4ba1c584b66ac80bbb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e6eec6a20f4ce99e21232dbf051ed94cd57a55e84dba24a059252faa636440a1"
-    sha256 cellar: :any_skip_relocation, catalina:       "f36d52af1655ee10941b9d1ef07303ce7f1b782c85ff59271e6ec0825c0d4bc2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f163750e7c40e9395bef76b623131958f972fed86c28277cc1ad8bb84f3a5199"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ad1885cf8724b9fe8bd31d04e2127ec046ec72ee6ef358e78d5a299a067b6318"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ce35d0a6553f4aee6590d56aba186bc00baea28f51f2659c77606edb9ba66d4f"
+    sha256 cellar: :any_skip_relocation, monterey:       "86f47b4d111748cc4781d64d2f269e9bd1f3f3989f288ea09dcc75bbe57bb53d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a007d9fa89b991dc63b6bc4bcff695530d59d365bc9b3321fd8d4954f58c848e"
+    sha256 cellar: :any_skip_relocation, catalina:       "13b4bca12d950da463d1c9b2d1135cd344a065c57dcd4861f59277725088032d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a211270ea9d57becf8dfe674a199df4c9406475b294da6f1de87dc6106a352e1"
   end
 
   depends_on "rust" => :build
