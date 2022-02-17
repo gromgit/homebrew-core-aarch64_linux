98998802b5b18ebe9342c29fd4e2bdc6c8cfb677
Mike Prasuhn <mike@mikeyp.net>
2022-02-17 16:02:19 -0500

    tmuxinator 3.0.2
    * tmuxinator 3.0.2
    * Bump thor resource
    
    Authored-by: Mike Prasuhn <mike@mikeyp.net>

--- Formula/tmuxinator.rb
+++ Formula/tmuxinator.rb
@@ -1,8 +1,8 @@
 class Tmuxinator < Formula
   desc "Manage complex tmux sessions easily"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.1.tar.gz"
-  sha256 "1d79dd13beaaf2ed24c8a76410c1a41bedc7785498e199534fa3928bbf8aab01"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.2.tar.gz"
+  sha256 "3992e4a13113ebc6d1d7571c2fae05a056655ee7bfd2eba1455242e30a6dcdd8"
   license "MIT"
   head "https://github.com/tmuxinator/tmuxinator.git", branch: "master"
 
@@ -27,8 +27,8 @@ class Tmuxinator < Formula
   end
 
   resource "thor" do
-    url "https://rubygems.org/downloads/thor-1.1.0.gem"
-    sha256 "cacae12a3761be4ccbe63be19261352b108f86c721c37d87664328efeaa6d0a3"
+    url "https://rubygems.org/downloads/thor-1.2.1.gem"
+    sha256 "b1752153dc9c6b8d3fcaa665e9e1a00a3e73f28da5e238b81c404502e539d446"
   end
 
   resource "xdg" do
