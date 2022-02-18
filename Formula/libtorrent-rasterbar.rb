6a5acae6f9410e5a6446c9e9a16b278769866cb1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-18 19:33:19 +0800

    libtorrent-rasterbar: apply patch for Boost 1.78 (#95358)
    This (hopefully) unblocks #91224.

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -1,10 +1,20 @@
 class LibtorrentRasterbar < Formula
   desc "C++ bittorrent library with Python bindings"
   homepage "https://www.libtorrent.org/"
-  url "https://github.com/arvidn/libtorrent/releases/download/v2.0.5/libtorrent-rasterbar-2.0.5.tar.gz"
-  sha256 "e965c2e53170c61c0db3a2d898a61769cb7acd541bbf157cbbef97a185930ea5"
   license "BSD-3-Clause"
   head "https://github.com/arvidn/libtorrent.git", branch: "RC_2_0"
+
+  # Remove `stable do` block when patch is no longer needed.
+  stable do
+    url "https://github.com/arvidn/libtorrent/releases/download/v2.0.5/libtorrent-rasterbar-2.0.5.tar.gz"
+    sha256 "e965c2e53170c61c0db3a2d898a61769cb7acd541bbf157cbbef97a185930ea5"
+
+    # Fix build with Boost 1.78. Remove in next release.
+    patch do
+      url "https://github.com/arvidn/libtorrent/commit/71d608fceca7e61c9d124f9ea83f71b06eda3b17.patch?full_index=1"
+      sha256 "20b8e93b67f81af22e50bd668fbeee30147dd85d3ffdff9d624531c32f54e567"
+    end
+  end
 
   livecheck do
     url :stable
