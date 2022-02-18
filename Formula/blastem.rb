c2b3cf90b272de0415df6b5c39e838b88c2a56c6
danielnachun <daniel.nachun@gmail.com>
2022-02-17 18:22:27 -0800

    blastem: use Python 3 and remove unneeded resources
    Closes #95337.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/blastem.rb
+++ Formula/blastem.rb
@@ -22,58 +22,34 @@ class Blastem < Formula
 
   depends_on "freetype" => :build
   depends_on "gettext" => :build
+  depends_on "imagemagick" => :build
   depends_on "jpeg" => :build
   depends_on "libpng" => :build
   depends_on "openjpeg" => :build
+  depends_on "pillow" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
+  depends_on arch: :x86_64
   depends_on "glew"
-  depends_on :macos # Due to Python 2
   depends_on "sdl2"
 
-  resource "Pillow" do
-    url "https://files.pythonhosted.org/packages/b3/d0/a20d8440b71adfbf133452d4f6e0fe80de2df7c2578c9b498fb812083383/Pillow-6.2.2.tar.gz"
-    sha256 "db9ff0c251ed066d367f53b64827cc9e18ccea001b986d08c265e53625dab950"
-  end
+  uses_from_macos "libffi"
 
   resource "vasm" do
     url "http://phoenix.owl.de/tags/vasm1_8i.tar.gz"
     sha256 "9ae0b37bca11cae5cf00e4d47e7225737bdaec4028e4db2a501b4eca7df8639d"
   end
 
-  resource "xcftools" do
-    url "http://henning.makholm.net/xcftools/xcftools-1.0.7.tar.gz"
-    sha256 "1ebf6d8405348600bc551712d9e4f7c33cc83e416804709f68d0700afde920a6"
+  # Convert Python 2 script to Python 3. Remove with next release.
+  patch do
+    url "https://www.retrodev.com/repos/blastem/raw-rev/dbbf0100f249"
+    sha256 "e332764bfa08e08e0f9cbbebefe73b88adb99a1e96a77a16a0aeeae827ac72ff"
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python2.7/site-packages"
-
     if MacOS.sdk_path_if_needed
       ENV.append "CPPFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers"
       ENV.append "CPPFLAGS", "-I#{MacOS.sdk_path}/usr/include/ffi" # libffi
-    end
-
-    resource("Pillow").stage do
-      inreplace "setup.py" do |s|
-        sdkprefix = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
-        s.gsub! "ZLIB_ROOT = None",
-          "ZLIB_ROOT = ('#{sdkprefix}/usr/lib', '#{sdkprefix}/usr/include')"
-        s.gsub! "JPEG_ROOT = None",
-          "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
-        s.gsub! "FREETYPE_ROOT = None",
-          "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
-      end
-
-      begin
-        # avoid triggering "helpful" distutils code that doesn't recognize Xcode 7 .tbd stubs
-        saved_sdkroot = ENV.delete "SDKROOT"
-        unless MacOS::CLT.installed?
-          ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers"
-        end
-        system "python", *Language::Python.setup_install_args(buildpath/"vendor")
-      ensure
-        ENV["SDKROOT"] = saved_sdkroot
-      end
     end
 
     resource("vasm").stage do
@@ -81,22 +57,9 @@ class Blastem < Formula
       (buildpath/"tool").install "vasmm68k_mot"
     end
 
-    # FIXME: xcftools is not in the core tap
-    # https://github.com/Homebrew/homebrew-core/pull/1216
-    resource("xcftools").stage do
-      # Apply patch to build with libpng-1.5 or above
-      # https://anonscm.debian.org/cgit/collab-maint/xcftools.git/commit/?id=c40088b82c6a788792aae4068ddc8458de313a9b
-      inreplace "xcf2png.c", /png_(voidp|error_ptr)_NULL/, "NULL"
-
-      system "./configure", "LIBS=-lintl"
-
-      # Avoid `touch` error from empty MANLINGUAS when building without NLS
-      touch "manpo/manpages.pot"
-      ENV.deparallelize { system "make", "manpo/manpages.pot" }
-      touch "manpo/manpages.pot"
-      system "make"
-      (buildpath/"tool").install "xcf2png"
-    end
+    # Use imagemagick to convert XCF files instead of xcftools, which is unmaintained and broken.
+    # Fix was sent to upstream developer.
+    inreplace "Makefile", "xcf2png \$< > \$@", "convert $< $@"
 
     ENV.prepend_path "PATH", buildpath/"tool"
 
