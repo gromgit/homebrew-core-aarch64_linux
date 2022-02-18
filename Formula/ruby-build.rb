b03849006d2b59b122a9be394bb191f4a6df7917
Mislav Marohnić <hi@mislav.net>
2022-02-18 13:23:04 +0100

    ruby-build 20220218
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #95379.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20220125.tar.gz"
-  sha256 "6b13f1ab6cf1bb8c9254c5034aad228bdbd8301aa087dd3706ca35e3b0ba153f"
+  url "https://github.com/rbenv/ruby-build/archive/v20220218.tar.gz"
+  sha256 "35c82b13b7bc3713eee5615b0145c79fbbac32873f55f2ab796620d76970d8e3"
   license "MIT"
   head "https://github.com/rbenv/ruby-build.git", branch: "master"
 
