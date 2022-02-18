2f4968341bb590dc06b1d9d989761cc109eae100
Branch Vincent <branchevincent@gmail.com>
2022-02-17 19:16:41 -0500

    speedtest-cli: depend on python@3.10
    Closes #95332.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/speedtest-cli.rb
+++ Formula/speedtest-cli.rb
@@ -1,4 +1,6 @@
 class SpeedtestCli < Formula
+  include Language::Python::Shebang
+
   desc "Command-line interface for https://speedtest.net bandwidth tests"
   homepage "https://github.com/sivel/speedtest-cli"
   url "https://github.com/sivel/speedtest-cli/archive/v2.1.3.tar.gz"
@@ -11,7 +13,16 @@ class SpeedtestCli < Formula
     sha256 cellar: :any_skip_relocation, all: "ebf51e9de63f99e3fcea09b61015488a25e7769422fd9656fee9c0ecf7ba08b3"
   end
 
+  depends_on "python@3.10"
+
+  # Support Python 3.10, remove on next release
+  patch do
+    url "https://github.com/sivel/speedtest-cli/commit/22210ca35228f0bbcef75a7c14587c4ecb875ab4.patch?full_index=1"
+    sha256 "d0456eb9fded20fb1580dbc6e3bc451a10c3fbcd3441efea66035aa848440c09"
+  end
+
   def install
+    rewrite_shebang detected_python_shebang, "speedtest.py"
     bin.install "speedtest.py" => "speedtest"
     bin.install_symlink "speedtest" => "speedtest-cli"
     man1.install "speedtest-cli.1"
