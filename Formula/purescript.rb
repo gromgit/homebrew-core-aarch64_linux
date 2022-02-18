43cc59b10f7bb5657cadafeca297eb7e23f7e326
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-18 11:29:15 +0000

    purescript: update 0.14.5 bottle.

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -7,10 +7,12 @@ class Purescript < Formula
   head "https://github.com/purescript/purescript.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5f30397c026c4a302ab5b7d35411a14a73a928aec418f160aa51749df0faa008"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d84a41a6c2d4f7a24193e30d9b47e25e700805000829367ef6027fd01bd7c113"
-    sha256 cellar: :any_skip_relocation, catalina:      "86759c6e909aa900c4c01d2345b033d29f7121f1fb36c58955ef86c9c47cb61d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b3e65f7777018972678cb3d3b18f992958fa7c1278f056c240bcf1e6deca940c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dde992e18cfe91360783262de1941bba833b546e196888b959b43e592320f9a2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f30397c026c4a302ab5b7d35411a14a73a928aec418f160aa51749df0faa008"
+    sha256 cellar: :any_skip_relocation, monterey:       "763f4e027f06834a0f6f4c53b89e6bc1a9d16fa86af92b2acfe1bc23a05fdfe3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d84a41a6c2d4f7a24193e30d9b47e25e700805000829367ef6027fd01bd7c113"
+    sha256 cellar: :any_skip_relocation, catalina:       "86759c6e909aa900c4c01d2345b033d29f7121f1fb36c58955ef86c9c47cb61d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b3e65f7777018972678cb3d3b18f992958fa7c1278f056c240bcf1e6deca940c"
   end
 
   depends_on "ghc" => :build
