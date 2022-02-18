7f91b1d0afa291286777b55641d6d9c346ebdd46
Curudel <andreas.florin.1988@outlook.com>
2022-02-18 15:16:21 +0100

    tfsec 1.3.0
    * tfsec 1.3.0
    * tfsec: update sha
    
    Closes #95382.
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/aquasecurity/tfsec/archive/v1.2.1.tar.gz"
-  sha256 "8b9c329025fd3a2c6ca0e10362fd425c39312549e93be37f315a13bf440dace6"
+  url "https://github.com/aquasecurity/tfsec/archive/v1.3.0.tar.gz"
+  sha256 "00059677c5e2f64acbd99df8978ecd60e7616531bdba100f563267a53827424f"
   license "MIT"
   head "https://github.com/aquasecurity/tfsec.git", branch: "master"
 
