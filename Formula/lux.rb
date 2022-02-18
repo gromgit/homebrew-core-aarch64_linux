f503d26fb904972e3287c44a108626f500a2d3c1
Porkepix <porkepix@gmail.com>
2022-02-18 11:22:39 +0100

    lux 0.13.0
    Closes #95370.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -1,8 +1,8 @@
 class Lux < Formula
   desc "Fast and simple video downloader"
   homepage "https://github.com/iawia002/lux"
-  url "https://github.com/iawia002/lux/archive/v0.12.0.tar.gz"
-  sha256 "f5bcbe1039219a299908fdd5a540052ef603ff5c8c21c0d64f44c53132c41cdd"
+  url "https://github.com/iawia002/lux/archive/v0.13.0.tar.gz"
+  sha256 "4fe99247a718dc1038cb6b9b1808c8e836bbf6a8464afefddf4bf8a161b08236"
   license "MIT"
   head "https://github.com/iawia002/lux.git", branch: "master"
 
