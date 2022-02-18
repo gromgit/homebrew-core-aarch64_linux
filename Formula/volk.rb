6ace59e3dbfb86fbb32a348418d43669bd4fe0e0
Thierry Moisan <thierry.moisan@gmail.com>
2022-02-18 15:16:54 -0500

    volk 2.5.1
    Also, use Homebrew `cpu_features`.
    
    We restrict this to Intel, since `cpu_features` hasn't been bottled on
    ARM.

--- Formula/volk.rb
+++ Formula/volk.rb
@@ -3,9 +3,8 @@ class Volk < Formula
 
   desc "Vector Optimized Library of Kernels"
   homepage "https://www.libvolk.org/"
-  url "https://github.com/gnuradio/volk.git",
-      tag:      "v2.5.0",
-      revision: "237a6fc9242ea8c48d2bbd417a6ea14feaf7314a"
+  url "https://github.com/gnuradio/volk/releases/download/v2.5.1/volk-2.5.1.tar.gz"
+  sha256 "8f7f2f8918c6ba63ebe8375fe87add347046b8b3acbba2fb582577bebd8852df"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -18,7 +17,7 @@ class Volk < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "boost"
+  depends_on "cpu_features" if Hardware::CPU.intel?
   depends_on "orc"
   depends_on "python@3.9"
 
@@ -29,15 +28,14 @@ class Volk < Formula
   fails_with gcc: "5" # https://github.com/gnuradio/volk/issues/375
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/5c/db/2d2d88b924aa4674a080aae83b59ea19d593250bfe5ed789947c21736785/Mako-1.1.4.tar.gz"
-    sha256 "17831f0b7087c313c0ffae2bcbbd3c1d5ba9eeac9c38f2eb7b50e8c99fe9d5ab"
+    url "https://files.pythonhosted.org/packages/af/b6/42cd322ae555aa770d49e31b8c5c28a243ba1bbb57ad927e1a5f5b064811/Mako-1.1.6.tar.gz"
+    sha256 "4e9e345a41924a954251b95b4b28e14a301145b544901332e658907a7464b6b2"
   end
 
   def install
     # Set up Mako
     venv_root = libexec/"venv"
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", "#{venv_root}/lib/python#{xy}/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", venv_root/Language::Python.site_packages("python3")
     venv = virtualenv_create(venv_root, "python3")
     venv.pip_install resource("Mako")
 
@@ -47,14 +45,20 @@ class Volk < Formula
       s.gsub! "${CMAKE_CXX_COMPILER}", ENV.cxx
     end
 
-    args = %W[-DPYTHON_EXECUTABLE=#{venv_root}/bin/python -DENABLE_TESTING=OFF]
-    system "cmake", ".", *std_cmake_args, *args
-    system "make", "install"
+    # cpu_features fails to build on ARM macOS.
+    args = %W[
+      -DPYTHON_EXECUTABLE=#{venv_root}/bin/python
+      -DENABLE_TESTING=OFF
+      -DVOLK_CPU_FEATURES=#{Hardware::CPU.intel?}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     # Set up volk_modtool paths
-    site_packages = lib/"python#{xy}/site-packages"
+    site_packages = prefix/Language::Python.site_packages("python3")
     pth_contents = "import site; site.addsitedir('#{site_packages}')\n"
-    (venv_root/"lib/python#{xy}/site-packages/homebrew-volk.pth").write pth_contents
+    (venv_root/Language::Python.site_packages("python3")/"homebrew-volk.pth").write pth_contents
   end
 
   test do
