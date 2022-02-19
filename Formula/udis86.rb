e83fe1f677b08482c23c3bae25314f6ee5b5d4fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-19 15:05:49 +0000

    udis86: update 1.7.2_1 bottle.

--- Formula/udis86.rb
+++ Formula/udis86.rb
@@ -11,14 +11,12 @@ class Udis86 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2784b5cdecb76514ce3a56f1343e851915cf1f9e5453a9db9eb54a42436903be"
-    sha256 cellar: :any, big_sur:       "8d2407363a0ba95163d3689f83e7b1fce331c61a9fecbc6e6c0476af9435f347"
-    sha256 cellar: :any, catalina:      "28f761b215237ed656359cce94fd4787be86a50057dc064589f6bbedbcf2fe06"
-    sha256 cellar: :any, mojave:        "b510a8349883e86135cf030953df54b671cf668a1e3e5020fc059a0f6f92a52d"
-    sha256 cellar: :any, high_sierra:   "cefad0043918886a862a040adf2450699c00cbef3fd561bbc8867e2328b16ac8"
-    sha256 cellar: :any, sierra:        "e3774a825eda78db57585c75b739dc60d0c069e35c8666575f5889908b0735d5"
-    sha256 cellar: :any, el_capitan:    "e763db7beca50f11ab341f13f5fd571513f4847772bb70ef83d94bb576427673"
-    sha256 cellar: :any, yosemite:      "bcd6eb347f55bc856ceb64604d3bace30219e346de34caa8be7de2b52a1cb35d"
+    sha256 cellar: :any,                 arm64_monterey: "d21c7de8ded29dbf2218802883f3a2e702e338722345b06aeab3a5146b81b29e"
+    sha256 cellar: :any,                 arm64_big_sur:  "97a5c453ede751fa70387f5c66f3c618bdc421d29ed1da6ec6e002b0efd7705f"
+    sha256 cellar: :any,                 monterey:       "d3b5ae26039dad3f35b3ead709fb8ee9be23ccaeb619d0b009830e7d94e151db"
+    sha256 cellar: :any,                 big_sur:        "d34571cf019e170edc18b80d678db9d27d1cbbeab7e4c1ba9e667868a1d3dd43"
+    sha256 cellar: :any,                 catalina:       "6e9b87a5a4d1de46246e92bc536113a6a56ec0c4565c2c0c0d122eb34ff4025b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8a15d4f3b8bad23184fdace5ddc482e4d1b5d7f98030791ecf91983ec909d5a"
   end
 
   depends_on "python@3.10" => :build
