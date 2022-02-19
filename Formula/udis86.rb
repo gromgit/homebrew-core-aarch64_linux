10644e16ea70b0fb4d418e4bebce1a8507426984
Branch Vincent <branchevincent@gmail.com>
2022-02-18 23:22:43 -0500

    udis86: depend on python@3.10
    Closes #95414.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/udis86.rb
+++ Formula/udis86.rb
@@ -3,6 +3,7 @@ class Udis86 < Formula
   homepage "https://udis86.sourceforge.io"
   url "https://downloads.sourceforge.net/project/udis86/udis86/1.7/udis86-1.7.2.tar.gz"
   sha256 "9c52ac626ac6f531e1d6828feaad7e797d0f3cce1e9f34ad4e84627022b3c2f4"
+  revision 1
 
   livecheck do
     url :stable
@@ -20,9 +21,18 @@ class Udis86 < Formula
     sha256 cellar: :any, yosemite:      "bcd6eb347f55bc856ceb64604d3bace30219e346de34caa8be7de2b52a1cb35d"
   end
 
+  depends_on "python@3.10" => :build
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
-                          "--enable-shared"
+                          "--enable-shared",
+                          "--with-python=#{which("python3")}"
     system "make"
     system "make", "install"
   end
