a2d187742fd3afaa7ba2b4a6e7100381fd3c8e53
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-19 13:38:00 +0000

    apngasm: update 3.1.6_4 bottle.

--- Formula/apngasm.rb
+++ Formula/apngasm.rb
@@ -8,13 +8,12 @@ class Apngasm < Formula
   head "https://github.com/apngasm/apngasm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7d1619bafdac3d49debab0aab9af730d26cd1136dae3f100465af02a79868d46"
-    sha256 cellar: :any,                 arm64_big_sur:  "e7ae49e492cf07670d473742b64ab25103e0e94780181b78173c10b5f5c4fba7"
-    sha256 cellar: :any,                 monterey:       "a36162ed6dedb8816551c7e766178d35ef02433b08a0ad2d801cc9595b652f39"
-    sha256 cellar: :any,                 big_sur:        "d94b80958f9782e98a7bcd7461b22d5239c376d4b1fb26b49bfb9d5c5c25b6e6"
-    sha256 cellar: :any,                 catalina:       "db0dc40f3fd4e8a8b7435da56211356e669b42ba47b8107d0f840777197202cf"
-    sha256 cellar: :any,                 mojave:         "569b760c848add596a639397ebe63f631e2ad3faabd1fa77ea6609f24f240e2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab95dd3612c31e8ba296774bf173605838a649df0716aa9285ec660483bfbbbe"
+    sha256 cellar: :any,                 arm64_monterey: "219c6587b5ecec5060a05735e32c55deb126562aaceb6dcbfdda341d70c8adc8"
+    sha256 cellar: :any,                 arm64_big_sur:  "105eed022dae4f6baa80cb282a77f1309c9f8ff8cfac8662a992f06d0adbfe08"
+    sha256 cellar: :any,                 monterey:       "24ad3bddaa293a14347b0cf5a4eedaaa4a96cbc3fb00a07b7a4f33e9d7e40684"
+    sha256 cellar: :any,                 big_sur:        "6510d9c52c774ebf62478b0d4bd40fe5109d7b1c867b5c6be68b2afa2b910442"
+    sha256 cellar: :any,                 catalina:       "e04e89431ce9dace1d79e858b52711fedd6ef8a44f7ca5499bbfd8e612905e36"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2fac26c5d767fcef20f109fe26ce42648dfae3a4162389ec0849c7110102638f"
   end
 
   depends_on "cmake" => :build
