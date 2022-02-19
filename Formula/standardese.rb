f59fe624be92b67968f133a1a19f9e669ea7ab31
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-19 16:13:10 +0000

    standardese: update 0.5.2_3 bottle.

--- Formula/standardese.rb
+++ Formula/standardese.rb
@@ -10,11 +10,12 @@ class Standardese < Formula
   head "https://github.com/standardese/standardese.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "e1fe493b847108a48c5cb4e6e032845c7aed99b3edf48541a49058cd185ac297"
-    sha256 arm64_big_sur:  "f98cef4f60828772102b69e5ad5a42ed491e41ad34e6dd264504fa717e8ac318"
-    sha256 monterey:       "2eda251a6f304c9c65f645a0bbdd401d926a6c87bd53557c146880498a858054"
-    sha256 big_sur:        "c8b5a7fb67afdbc7513a47a6e675119cbbc6b7f8e7e0d8573c05feae3f222e2e"
-    sha256 catalina:       "2d943cbf611d175de619d386510c1577e1cdef483aa4c5b78f506b07263d82ea"
+    sha256                               arm64_monterey: "e1fe493b847108a48c5cb4e6e032845c7aed99b3edf48541a49058cd185ac297"
+    sha256                               arm64_big_sur:  "f98cef4f60828772102b69e5ad5a42ed491e41ad34e6dd264504fa717e8ac318"
+    sha256                               monterey:       "2eda251a6f304c9c65f645a0bbdd401d926a6c87bd53557c146880498a858054"
+    sha256                               big_sur:        "c8b5a7fb67afdbc7513a47a6e675119cbbc6b7f8e7e0d8573c05feae3f222e2e"
+    sha256                               catalina:       "2d943cbf611d175de619d386510c1577e1cdef483aa4c5b78f506b07263d82ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4deccfc6003f006aaceb6e6ce988e8ca913166049ccc6606ffd80bf1ef0231d7"
   end
 
   depends_on "cmake" => :build
