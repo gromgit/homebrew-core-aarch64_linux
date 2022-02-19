3eecda3ad61d09ef937d19c1dca34c06f050b905
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-19 13:37:53 +0000

    libbitcoin-network: update 3.6.0_3 bottle.

--- Formula/libbitcoin-network.rb
+++ Formula/libbitcoin-network.rb
@@ -7,13 +7,12 @@ class LibbitcoinNetwork < Formula
   revision 3
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f3793425b364b897fc7916d488a7645f735dc385edd645beb96e6b5681891ea8"
-    sha256 cellar: :any,                 arm64_big_sur:  "424e25564e199005eb3944f8e682ac6c07803833494b9b89df2175e93b7ba34b"
-    sha256 cellar: :any,                 monterey:       "7c51f436a31f94b18ca3d462c5797b7bf9fb63e1dabcbfdcc77f881a61ffe923"
-    sha256 cellar: :any,                 big_sur:        "21053287aadad7716c0b0471778e8b88d542d8b8628e505f917ffd20f8ebe78c"
-    sha256 cellar: :any,                 catalina:       "6ab4e56e5f996fe7441564b5998b4bd7ef7350fb6cfc5dda22b0efd55d64ef80"
-    sha256 cellar: :any,                 mojave:         "3f856ae06429e04d02fafefd40ad3ec6732f0b644e126fc3f5f3d42ad92c7e2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ab35bf2bc8d91ab8cdb61a68883fa383676acf5088851a8affae7962175bd9a"
+    sha256 cellar: :any,                 arm64_monterey: "f63e59fce512e4075d886ca920977a340092a06ca6a77a4a30a3e29487f30af1"
+    sha256 cellar: :any,                 arm64_big_sur:  "d5b25de58273e7a75f3ab79ec9b191b01bebdf9901a3345ff2e9a1df23ba0bad"
+    sha256 cellar: :any,                 monterey:       "7a342860fe4bc338f783efd3f1d1f7dee8247c5aed93f313c679ac02c3a09b9a"
+    sha256 cellar: :any,                 big_sur:        "afb63dab758788b425675d7e6920c74259e682c7a8e67ebe3c47016b715e70b2"
+    sha256 cellar: :any,                 catalina:       "7ace1c57a1959c1dbbf2fe3dfe468dc5f5ffb37944bc21ad7f817df308b7d661"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b6f7ca20b8d9bfd7599d1358f9b38458f247120a2ccd6d3ef1f082de188fd21"
   end
 
   depends_on "autoconf" => :build
