9ff479e39804de964bfdc5738e82618af34bd6bc
Porkepix <porkepix@gmail.com>
2022-02-19 16:06:00 +0100

    minetest 5.5.0
    * minetest 5.5.0
    * minetest: add IrrlichtMT, fix Linux build, don't fixup_bundle
    
    Closes #95452.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minetest.rb
+++ Formula/minetest.rb
@@ -4,12 +4,17 @@ class Minetest < Formula
   license "LGPL-2.1-or-later"
 
   stable do
-    url "https://github.com/minetest/minetest/archive/5.4.1.tar.gz"
-    sha256 "de9e4410583c845c104b4be25f9d0b8743d8573c120149b8910ae2519f9ab14e"
+    url "https://github.com/minetest/minetest/archive/5.5.0.tar.gz"
+    sha256 "8b9bef6054c8895cc3329ae6d05cb355eef9c7830600d82dc9eaa4664f87c8f9"
+
+    resource "irrlichtmt" do
+      url "https://github.com/minetest/irrlicht/archive/1.9.0mt4.tar.gz"
+      sha256 "a0e2e5239ebca804adf54400ccaacaf228ec09223cfb2e1daddc9bf2694176e6"
+    end
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game/archive/5.4.1.tar.gz"
-      sha256 "b4bfa0755b88230cf4bdb6af6a0951dd1248f6cdf87fecc340e43ac12c80b0b2"
+      url "https://github.com/minetest/minetest_game/archive/5.5.0.tar.gz"
+      sha256 "1e87252e26d6b1d3efe7720e3e097d489339dea4dd25980a828d5da212b01aaa"
     end
   end
 
@@ -28,33 +33,68 @@ class Minetest < Formula
   head do
     url "https://github.com/minetest/minetest.git"
 
+    resource "irrlichtmt" do
+      url "https://github.com/minetest/irrlicht.git", branch: "master"
+    end
+
     resource "minetest_game" do
       url "https://github.com/minetest/minetest_game.git", branch: "master"
     end
   end
 
   depends_on "cmake" => :build
+  depends_on "gettext" => :build
   depends_on "freetype"
-  depends_on "gettext"
-  depends_on "irrlicht"
+  depends_on "gmp"
   depends_on "jpeg"
+  depends_on "jsoncpp"
   depends_on "libogg"
+  depends_on "libpng"
   depends_on "libvorbis"
-  depends_on "luajit"
+  depends_on "luajit-openresty"
+  depends_on "zstd"
+
+  uses_from_macos "curl"
+  uses_from_macos "ncurses"
+  uses_from_macos "sqlite"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libx11"
+    depends_on "libxxf86vm"
+    depends_on "mesa"
+    depends_on "openal-soft"
+  end
 
   def install
+    inreplace "src/CMakeLists.txt" do |s|
+      # These flags are not needed for LuaJIT 2.1 (Ref: https://luajit.org/install.html).
+      # On Apple ARM, the flags results in broken binaries and need to be removed.
+      s.gsub! " -pagezero_size 10000 -image_base 100000000\"", "\""
+      # Disable CMake fixup_bundle to prevent copying dylibs into app bundle
+      s.gsub! "fixup_bundle(", "# \\0"
+    end
+
+    # Remove bundled libraries to prevent fallback
+    %w[lua gmp jsoncpp].each { |lib| (buildpath/"lib"/lib).rmtree }
+
     (buildpath/"games/minetest_game").install resource("minetest_game")
+    (buildpath/"lib/irrlichtmt").install resource("irrlichtmt")
 
-    args = std_cmake_args
-    args << "-DBUILD_CLIENT=1" << "-DBUILD_SERVER=0"
-    args << "-DENABLE_FREETYPE=1" << "-DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'"
-    args << "-DENABLE_GETTEXT=1" << "-DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}"
-    args << "-DIRRLICHT_LIBRARY=#{Formula["irrlicht"].opt_frameworks}/IrrFramework.framework"
+    args = %W[
+      -DBUILD_CLIENT=1
+      -DBUILD_SERVER=0
+      -DENABLE_FREETYPE=1
+      -DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'
+      -DENABLE_GETTEXT=1
+      -DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}
+    ]
+    # Workaround for 'Could NOT find GettextLib (missing: ICONV_LIBRARY)'
+    args << "-DICONV_LIBRARY=#{MacOS.sdk_path}/usr/lib/libiconv.tbd" if MacOS.version >= :big_sur
 
-    system "cmake", ".", *args
-    system "make", "package"
-    system "unzip", "minetest-*-osx.zip"
-    prefix.install "minetest.app"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   def caveats
@@ -69,6 +109,10 @@ class Minetest < Formula
   end
 
   test do
-    system "#{prefix}/minetest.app/Contents/MacOS/minetest", "--version"
+    minetest = OS.mac? ? prefix/"minetest.app/Contents/MacOS/minetest" : bin/"minetest"
+    output = shell_output("#{minetest} --version")
+    assert_match "USE_CURL=1", output
+    assert_match "USE_GETTEXT=1", output
+    assert_match "USE_SOUND=1", output
   end
 end
