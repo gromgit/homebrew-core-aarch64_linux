7cf3bd685ccdbe24516183d7e8a641c41c45dddd
Rui Chen <rui@chenrui.dev>
2022-02-20 13:55:37 -0500

    link-grammar 5.10.3
    * link-grammar 5.10.3
    * link-grammar: `uses_from_macos "flex" => :build`
    
    Closes #95492.
    
    Co-authored-by: Issy Long <me@issyl0.co.uk>
    Signed-off-by: Issy Long <me@issyl0.co.uk>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/link-grammar.rb
+++ Formula/link-grammar.rb
@@ -1,8 +1,8 @@
 class LinkGrammar < Formula
   desc "Carnegie Mellon University's link grammar parser"
   homepage "https://www.abisource.com/projects/link-grammar/"
-  url "https://www.abisource.com/downloads/link-grammar/5.10.2/link-grammar-5.10.2.tar.gz"
-  sha256 "28cec752eaa0e3897ae961333b6927459f8b69fefe68c2aa5272983d7db869b6"
+  url "https://www.abisource.com/downloads/link-grammar/5.10.3/link-grammar-5.10.3.tar.gz"
+  sha256 "fd0a3bb5dc72097a8e3decb18091d1acd6e7fd52947b114755be0098b0f79ce0"
   license "LGPL-2.1"
   head "https://github.com/opencog/link-grammar.git", branch: "master"
 
@@ -29,6 +29,7 @@ class LinkGrammar < Formula
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => :build
 
+  uses_from_macos "flex" => :build
   uses_from_macos "sqlite"
 
   def install
