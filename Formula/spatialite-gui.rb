1d21896dff198aeee8089bc474478143b664972d
danielnachun <daniel.nachun@gmail.com>
2022-02-20 13:43:18 -0800

    spatialite-gui: update proj dependency

--- Formula/spatialite-gui.rb
+++ Formula/spatialite-gui.rb
@@ -1,10 +1,9 @@
 class SpatialiteGui < Formula
   desc "GUI tool supporting SpatiaLite"
   homepage "https://www.gaia-gis.it/fossil/spatialite_gui/index"
-  url "https://www.gaia-gis.it/gaia-sins/spatialite-gui-sources/spatialite_gui-1.7.1.tar.gz"
-  sha256 "cb9cb1ede7f83a5fc5f52c83437e556ab9cb54d6ace3c545d31b317fd36f05e4"
+  url "https://www.gaia-gis.it/gaia-sins/spatialite-gui-sources/spatialite_gui-2.1.0-beta1.tar.gz"
+  sha256 "ba48d96df18cebc3ff23f69797207ae1582cce62f4596b69bae300ca3c23db33"
   license "GPL-3.0-or-later"
-  revision 9
 
   livecheck do
     url "https://www.gaia-gis.it/gaia-sins/spatialite-gui-sources/"
@@ -24,43 +23,39 @@ class SpatialiteGui < Formula
   depends_on "pkg-config" => :build
   depends_on "freexl"
   depends_on "geos"
-  depends_on "libgaiagraphics"
+  depends_on "libpq"
+  depends_on "librasterlite2"
+  depends_on "librttopo"
   depends_on "libspatialite"
-  depends_on "proj@7"
+  depends_on "libtiff"
+  depends_on "libxlsxwriter"
+  depends_on "lz4"
+  depends_on "minizip"
+  depends_on "openjpeg"
+  depends_on "proj"
   depends_on "sqlite"
+  depends_on "virtualpg"
+  depends_on "webp"
   depends_on "wxwidgets@3.0"
+  depends_on "xz"
+  depends_on "zstd"
 
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/spatialite-gui/1.7.1.patch"
-    sha256 "37f71f3cb2b0b9649eb85a51296187b0adf2972c5a1d3ee0daf3082e2c35025e"
-  end
+  uses_from_macos "curl"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
 
   def install
     wxwidgets = Formula["wxwidgets@3.0"]
-    ENV["WX_CONFIG"] = wxwidgets.opt_bin/"wx-config-#{wxwidgets.version.major_minor}"
 
     # Link flags for sqlite don't seem to get passed to make, which
     # causes builds to fatally error out on linking.
     # https://github.com/Homebrew/homebrew/issues/44003
-    #
-    # spatialite-gui uses `proj` (instead of `proj@7`) if installed
     sqlite = Formula["sqlite"]
-    proj = Formula["proj@7"]
-    ENV.prepend "LDFLAGS", "-L#{sqlite.opt_lib} -lsqlite3 -L#{proj.opt_lib}"
-    ENV.prepend "CFLAGS", "-I#{sqlite.opt_include} -I#{proj.opt_include}"
+    ENV.prepend "LDFLAGS", "-L#{sqlite.opt_lib} -lsqlite3"
+    ENV.prepend "CFLAGS", "-I#{sqlite.opt_include}"
 
-    # Use Proj 6.0.0 compatibility headers
-    # https://www.gaia-gis.it/fossil/spatialite_gui/tktview?name=8349866db6
-    ENV.append_to_cflags "-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H"
-
-    # Add aui library; reported upstream multiple times:
-    # https://groups.google.com/forum/#!searchin/spatialite-users/aui/spatialite-users/wnkjK9pde2E/hVCpcndUP_wJ
-    inreplace "configure" do |s|
-      s.gsub! "WX_LIBS=\"$(wx-config --libs)\"", "WX_LIBS=\"$(wx-config --libs std,aui)\""
-      # configure does not make proper use of `WX_CONFIG`
-      s.gsub! "S=\"$(wx-config --", "S=\"$($WX_CONFIG --"
-    end
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+           "--with-wxconfig=#{wxwidgets.opt_bin}/wx-config-#{wxwidgets.version.major_minor}"
     system "make", "install"
   end
 end
