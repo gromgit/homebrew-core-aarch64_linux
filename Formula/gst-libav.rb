fa01e67c29ead76451c194b381d85d94d86cf91f
Rui Chen <rui@chenrui.dev>
2022-02-20 14:10:17 -0500

    gst-libav 1.20.0

--- Formula/gst-libav.rb
+++ Formula/gst-libav.rb
@@ -1,10 +1,9 @@
 class GstLibav < Formula
   desc "GStreamer plugins for Libav (a fork of FFmpeg)"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.18.5.tar.xz"
-  sha256 "822e008a910e9dd13aedbdd8dc63fedef4040c0ee2e927bab3112e9de693a548"
+  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.20.0.tar.xz"
+  sha256 "5eee5ed8d5082a31b500448e41535c722ee30cd5f8224f32982bbaba2eedef17"
   license "LGPL-2.1-or-later"
-  revision 1
   head "https://gitlab.freedesktop.org/gstreamer/gst-libav.git", branch: "master"
 
   livecheck do
@@ -25,9 +24,13 @@ class GstLibav < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "yasm" => :build
-  depends_on "ffmpeg@4"
+  depends_on "ffmpeg"
   depends_on "gst-plugins-base"
   depends_on "xz" # For LZMA
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5" # ffmpeg is compiled with GCC
 
