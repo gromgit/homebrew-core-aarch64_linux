01f89e903c63844a9282c46e72cca912a76a5a23
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-21 16:33:51 +0800

    fizz 2022.02.21.00

--- Formula/fizz.rb
+++ Formula/fizz.rb
@@ -1,10 +1,9 @@
 class Fizz < Formula
   desc "C++14 implementation of the TLS-1.3 standard"
   homepage "https://github.com/facebookincubator/fizz"
-  url "https://github.com/facebookincubator/fizz/releases/download/v2022.02.14.00/fizz-v2022.02.14.00.tar.gz"
-  sha256 "7b5d4a00d551b5b0b610ac8a69e69283e4d700838cf0ab64f606b35478a08d42"
+  url "https://github.com/facebookincubator/fizz/releases/download/v2022.02.21.00/fizz-v2022.02.21.00.tar.gz"
+  sha256 "cf314c558fa3264a6acdbb652f688335c0c377cfeadd863b52ac143ec4c83b31"
   license "BSD-2-Clause"
-  revision 1
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
   bottle do
