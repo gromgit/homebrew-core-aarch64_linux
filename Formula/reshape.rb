b2ce87c19575b27d6535e7020f93241f1f062d29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-21 17:05:49 +0000

    reshape: update 0.5.1 bottle.

--- Formula/reshape.rb
+++ Formula/reshape.rb
@@ -7,12 +7,12 @@ class Reshape < Formula
   head "https://github.com/fabianlindfors/reshape.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3bd9972c5d4827af99697a63fc17da06f4e0d5ad31867fde1ea644e117b01e20"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd78047e9268d619535734fd6ca7dc3770e5adeb3cb581d10df977a87c9c76e0"
-    sha256 cellar: :any_skip_relocation, monterey:       "e5b9319b0a959d5a6b8f248bdfb255165f52a7cbaa8e4a64dfc7660d75adbf88"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7217f769a48c2ca504e3215076c01b638d7fa55ce02556c47fb494667a3554cf"
-    sha256 cellar: :any_skip_relocation, catalina:       "308282ce640c2dccf57eb5383ab5e05fd3642689663129d5a843f9fad124f2af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e76e39f127b72a2b21503484753fcd07fd09cea36a81ae85772bd52f8850349f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a210b909222bd8944a6b0c72f99a8c4a9d953db5191df1937bc953aba91f3cb5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70fd7ff6e70322f04ce9e678bc692e5ac16929ac4c45b61e060fd3f5d5ef8e64"
+    sha256 cellar: :any_skip_relocation, monterey:       "15b90da4bf240cf35d8671d2a0f1bf33a6371193749b7063514eca28ef94cac4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cf22431372514ee64b8269ea2e2025d82714ed59a6ab61038a47c583f7465970"
+    sha256 cellar: :any_skip_relocation, catalina:       "b4b55cc7e25e542f1e5e937f9eb21f75ffc4cb7e86801d53d9977262733ed798"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc062c96a3d2d577db7344ff08a551ac8f38565879a0bee73fab0b0861b78714"
   end
 
   depends_on "rust" => :build
