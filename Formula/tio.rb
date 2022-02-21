d2906edca53fb9e15d86f611940a9270c598ab2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-21 09:10:28 +0000

    tio: update 1.35 bottle.

--- Formula/tio.rb
+++ Formula/tio.rb
@@ -6,12 +6,12 @@ class Tio < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b3ab6cbc1c6ef8a13aa64e4adcfbf02d56451e3981dcb7cd4975286cc94f8e0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "516b796261634baa606b8755b7be0b526bcfefae1b34533bc6827a7b1df0a368"
-    sha256 cellar: :any_skip_relocation, monterey:       "904cf361a84d7f65822c7be564d1e11efb6c3b26d8d4324af94656a697f0f7e4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "89bf7790e90ee884dc6801c819933e41321dc107559fe797382ff3fea4b29d5c"
-    sha256 cellar: :any_skip_relocation, catalina:       "4eda9ec8d5786034668b555fceb015f4fcf6a7c21fdfa08e0ad2cd929c22040a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f526b06ccd39b370481f66f3397f6d9614506194a41c451d93fbde9d6421dde7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "04047da7820a4df22bb71deee9da2e2f9b55195f6321505278c1e37d71c25424"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "94faedc8fa192ecff1a76f19d178337ac04feffd1847a8fd183bb6218a2ec410"
+    sha256 cellar: :any_skip_relocation, monterey:       "0338511323c35aa48d53230b699c0b87b7cebf3a0dfad4e877a7a9ddd3820b40"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e2cf8a642d6cd6a8d24aa5c8067c07e3dcbdf0920222db1feb14c8ff8c2583f8"
+    sha256 cellar: :any_skip_relocation, catalina:       "4b8f0ec39609e707a5b73a0b62e8a4ff350a92f6dffb7f7fa3b47d6008992f17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a85eb4b9c70f7f6490d4bd4f8eb71fd3133f3dcda2df5d52384671e5ac8f0ceb"
   end
 
   depends_on "meson" => :build
