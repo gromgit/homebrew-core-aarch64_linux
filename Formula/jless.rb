1b5f0c362cca46fd61223e93135c4ca3ca313140
Caleb Xu <calebcenter@live.com>
2022-02-21 00:01:39 -0500

    jless 0.7.2
    Closes #95534.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jless.rb
+++ Formula/jless.rb
@@ -1,8 +1,8 @@
 class Jless < Formula
   desc "Command-line pager for JSON data"
   homepage "https://pauljuliusmartinez.github.io/"
-  url "https://github.com/PaulJuliusMartinez/jless/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "8b36d117ac0ef52fa0261e06b88b5ae77c2ff4beeb54f2c906a57f066dc46179"
+  url "https://github.com/PaulJuliusMartinez/jless/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "5d776cb6488743ccdaeeffb4bfc54d84862028170cee852a8bb5c156526ed263"
   license "MIT"
 
   bottle do
