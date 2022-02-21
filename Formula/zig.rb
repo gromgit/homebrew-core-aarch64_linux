a0885e2756dadadb0ab223b69cc435fd6513f8f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-21 10:11:50 +0000

    zig: update 0.9.1 bottle.

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -11,12 +11,12 @@ class Zig < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3d388804326dc5475ce377832327cbae7c32d3468255c462394feb89d3ed743d"
-    sha256 cellar: :any,                 arm64_big_sur:  "894049ce2d60bba662770aa28bc8c52347f039840172c291f848ab5a7e11776c"
-    sha256 cellar: :any,                 monterey:       "5825b4b6610cad3d73eac8ff5915c01609a82a58bde9cf9309c6dc1fa32c9feb"
-    sha256 cellar: :any,                 big_sur:        "937d9e3adbfddfa2f7cb2902d9410b5636bd33f4cf47341b935bb68e87893d88"
-    sha256 cellar: :any,                 catalina:       "1b0c7260cfcd87901ad399f1082d59c0a8e8f87da521807eb8e6641c7e12c890"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8fd87d8186cb47914cdc179cb28e789cb59999f6e1b11d79f2ca9e701ac43e23"
+    sha256 cellar: :any,                 arm64_monterey: "61d958015313e75d98c407902e6aba1b4f359e392eda0c090199eec76d20534a"
+    sha256 cellar: :any,                 arm64_big_sur:  "accea97f675c8fb0c6049225ccaba3b9ce4a0d7f778ed47b4b2d2f34c8bccdf5"
+    sha256 cellar: :any,                 monterey:       "da8c2def2c9d0d09a84dd745a52b9d4acd136014b4fc1c383f06df05d99e6cb3"
+    sha256 cellar: :any,                 big_sur:        "ce7fefb8d1e9de6374a0daf605ae5ec7756377a6f60fa6b34bb11dfadf57e7c4"
+    sha256 cellar: :any,                 catalina:       "f53b3254181523c12e382404fec21583b408390ce2668b1c97d94b8af343acd8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d8cb3768434244333ab7164ba5806a924db341a1f86c9471009fc1f6177fcfe"
   end
 
   head do
