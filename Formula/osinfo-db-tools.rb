7a3f36805cf653a22efaaf6d7f3e2781fa3bcd13
Caleb Xu <calebcenter@live.com>
2022-02-20 20:50:09 -0500

    osinfo-db-tools 1.10.0
    Closes #95524.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osinfo-db-tools.rb
+++ Formula/osinfo-db-tools.rb
@@ -1,10 +1,9 @@
 class OsinfoDbTools < Formula
   desc "Tools for managing the libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.9.0.tar.xz"
-  sha256 "255f1c878bacec70c3020ff5a9cb0f6bd861ca0009f24608df5ef6f62d5243c0"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.10.0.tar.xz"
+  sha256 "802cdd53b416706ea5844f046ddcfb658c1b4906b9f940c79ac7abc50981ca68"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url "https://releases.pagure.org/libosinfo/?C=M&O=D"
@@ -28,7 +27,7 @@ class OsinfoDbTools < Formula
   depends_on "glib"
   depends_on "json-glib"
   depends_on "libarchive"
-  depends_on "libsoup@2"
+  depends_on "libsoup"
   depends_on "python@3.9"
 
   uses_from_macos "pod2man" => :build
