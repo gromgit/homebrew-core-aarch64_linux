b39906256496378b1f9f37292e1f9a66b7f843ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-22 05:27:27 +0000

    serverless 3.3.0
    Closes #95646.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://www.serverless.com/"
-  url "https://github.com/serverless/serverless/archive/v3.2.1.tar.gz"
-  sha256 "cc52ca47f8b6b836991e7c44efe6c3cabaf6acd0e7ddece57cbb0109367fbe36"
+  url "https://github.com/serverless/serverless/archive/v3.3.0.tar.gz"
+  sha256 "cdc5b2b5ef47294bc820e30a73e7e43f85c835a75f6ae93b41a24257cd78b583"
   license "MIT"
   head "https://github.com/serverless/serverless.git", branch: "master"
 
