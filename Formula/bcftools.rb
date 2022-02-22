e563953890bc42afd88defb0988e2d70454d8f9c
Rui Chen <rui@chenrui.dev>
2022-02-21 19:03:53 -0500

    bcftools 1.15

--- Formula/bcftools.rb
+++ Formula/bcftools.rb
@@ -1,12 +1,11 @@
 class Bcftools < Formula
   desc "Tools for BCF/VCF files and variant calling from samtools"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/bcftools/releases/download/1.14/bcftools-1.14.tar.bz2"
-  sha256 "b7ef88ae89fcb55658c5bea2e8cb8e756b055e13860036d6be13756782aa19cb"
+  url "https://github.com/samtools/bcftools/releases/download/1.15/bcftools-1.15.tar.bz2"
+  sha256 "1885ccb450a86e97a00aa905d7381ca9e07bd8967c05705a61d0007d2e22296e"
   # The bcftools source code is MIT/Expat-licensed, but when it is configured
   # with --enable-libgsl the resulting executable is GPL-licensed.
   license "GPL-3.0-or-later"
-  revision 1
 
   livecheck do
     url :stable
