e8804383e7355f12b452bc90ce46bad0403e46a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-23 05:35:39 +0000

    tomee-plume 8.0.10
    Closes #95707.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomee-plume.rb
+++ Formula/tomee-plume.rb
@@ -1,9 +1,9 @@
 class TomeePlume < Formula
   desc "Apache TomEE Plume"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.9/apache-tomee-8.0.9-plume.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.9/apache-tomee-8.0.9-plume.tar.gz"
-  sha256 "2e83d8d040519b0a5c16e75058493a1478c04f41a15f2d10cc853d0480ece303"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.10/apache-tomee-8.0.10-plume.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.10/apache-tomee-8.0.10-plume.tar.gz"
+  sha256 "c0ac9b9f28720c1a0d8e412a18c26e62f06e344ba3c713ee26acafc3db873611"
   license "Apache-2.0"
 
   bottle do
