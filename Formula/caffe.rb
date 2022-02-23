9e0f7525d32c1683f82f5dc6be48870d3a22f601
Sean Molenaar <smillerdev@me.com>
2022-02-23 11:10:01 +0100

    caffe: revision bump for opencv 4.5.5
    Closes #92092.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/caffe.rb
+++ Formula/caffe.rb
@@ -4,7 +4,7 @@ class Caffe < Formula
   url "https://github.com/BVLC/caffe/archive/1.0.tar.gz"
   sha256 "71d3c9eb8a183150f965a465824d01fe82826c22505f7aa314f700ace03fa77f"
   license "BSD-2-Clause"
-  revision 35
+  revision 36
 
   livecheck do
     url :stable
