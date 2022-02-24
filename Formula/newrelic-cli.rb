7afa5c2ed3e76e0bfb9cc86a683b08dbba0ddecb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-24 05:29:39 +0000

    newrelic-cli 0.41.30
    Closes #95767.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,8 +1,8 @@
 class NewrelicCli < Formula
   desc "Command-line interface for New Relic"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.41.24.tar.gz"
-  sha256 "6f864384a92c996569b4774c50778f9bc2ba5dcd288ad4ab489c06acb1e3fbed"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.41.30.tar.gz"
+  sha256 "4d8799716028cd37a489aef3fb5979bbd003bd6e5a14c22bd7fa1fc950a7de35"
   license "Apache-2.0"
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
