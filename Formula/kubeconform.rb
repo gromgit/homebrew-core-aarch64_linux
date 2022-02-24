fe93f82c61b0022707ab9eee9c93b09999a8a418
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-24 06:05:54 +0000

    kubeconform: update 0.4.13 bottle.

--- Formula/kubeconform.rb
+++ Formula/kubeconform.rb
@@ -7,13 +7,12 @@ class Kubeconform < Formula
   head "https://github.com/yannh/kubeconform.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "22cdc56e1e472411eba3441dec3a07ef340601bb775a82fd96aad40c6e701dfb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aca71402ebdb7b383b9b2a0d1202681bcc816c23d0cdeaa8fe14253a15e1790f"
-    sha256 cellar: :any_skip_relocation, monterey:       "2fbee45972642dcb803874a2186c9d7e8352551870b3b840246b7d95bef34998"
-    sha256 cellar: :any_skip_relocation, big_sur:        "59896d6b0a79a9214e780493a7178e3a2fd376206d079d746c8601a1c39c7572"
-    sha256 cellar: :any_skip_relocation, catalina:       "057e364e1e8f55490f97960fbd4f9a155de3198730c91cb24f325fa554bce3bb"
-    sha256 cellar: :any_skip_relocation, mojave:         "5342902c7d6a76596cb9fa432dc545e79f8a6397c49313df5acf891c03e7602a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae4a06a6f2e2ae77140ba4a99a313ef8459e7441f94a7e7aa7ff78ab586fd763"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "74dd1d995e736daa504ef1368e4f7a4ba541c944cb8e3b476dbda52d36e08595"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa049949d32864521c74edd74c9d375d3e2eb4858014bfbb7ea0a9fadb777f78"
+    sha256 cellar: :any_skip_relocation, monterey:       "78a8ab92ad7fda829a09aa68c4fa1414a59c18bfe2f33f22ab2557c17a45b6fc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8e542f30ed6a67d429ed2747252e7de8bb2b0d4ea7fa1be2221c8559c84acec7"
+    sha256 cellar: :any_skip_relocation, catalina:       "fad2ce7e4674c30adef19bdb85c77010c2eb6d0f6b897c98b2fd6d45448b2d0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1cdb8becf0620eb5cb8b1dc1e7caaaf06183047c61ba97bb430057af4077ab7b"
   end
 
   depends_on "go" => :build
