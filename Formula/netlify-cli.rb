dcad62427e4859e622ef5ffa11de7913e3dde858
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-24 05:29:22 +0000

    netlify-cli 9.8.0
    Closes #95766.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-9.7.0.tgz"
-  sha256 "b5c848027e35a5fead4f9ff8638bb4b831e9418d51d0613081b9266735eb1001"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-9.8.0.tgz"
+  sha256 "40b878b9ec0b2a889308d45bb01b5f49dff41b92bae9d8dacfa1ad6aeda6f85f"
   license "MIT"
   head "https://github.com/netlify/cli.git", branch: "main"
 
