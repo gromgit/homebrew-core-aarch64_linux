6931fa3917ae05ebfbaf3ed1b621dc5835fba9ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-24 09:10:05 +0000

    svgbob: update 0.6.6 bottle.

--- Formula/svgbob.rb
+++ Formula/svgbob.rb
@@ -7,12 +7,12 @@ class Svgbob < Formula
   head "https://github.com/ivanceras/svgbob.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2de0a6bcf8df742b05f729a2e73b9105748ed64ba20f97a02a50be12eef525a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2e074ccc877947e38115ef7f5e964d07532e84c004b3613cce137b2f9a7d919"
-    sha256 cellar: :any_skip_relocation, monterey:       "c535a74a7afb59e3a1c333259d3dec359deee8618ce1e56649afdaa547f444ae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "757448194ab944b65ff4db03746e5f74755cab16a81381f289c754f59aa2e253"
-    sha256 cellar: :any_skip_relocation, catalina:       "c3dd296f61b00496c20d890712e9d8c1695f1f58b9d2906f0b2c90a4b0c71b38"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12f87109973c709d85a1ad499f6dd3313516c221e3e0e3cc47b2e9522eb84385"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2443d5067fd38e94c885f65d5204e8e3335966eea9ed3846d9351366a2e6260d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3f3014ac10293974989cf739595cc4ee9b77582779b02a7b94cdd06184881e79"
+    sha256 cellar: :any_skip_relocation, monterey:       "a4eb89193a763ae7bedf422cdce031a0292e8691021f4fbeb93515fe7913caf4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2f8984bce1a18ad210d9a2b591136a5e0a527aca11df4ac6ea4b598546f412f9"
+    sha256 cellar: :any_skip_relocation, catalina:       "f1e0b4abfba05cf7b4a9f1f9741e1ff0f5a2bdc3306e02e1eb982a5e338df091"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aeaddd4070e0181a7cf669e562a49f4af3f6e75ab58858f7bbd4b58925bbf2ba"
   end
 
   depends_on "rust" => :build
