a84f01f422f8e524834d799f30fbe384490311be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-25 05:06:25 +0000

    bosh-cli 6.4.17
    Closes #95821.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bosh-cli.rb
+++ Formula/bosh-cli.rb
@@ -1,8 +1,8 @@
 class BoshCli < Formula
   desc "Cloud Foundry BOSH CLI v2"
   homepage "https://bosh.io/docs/cli-v2/"
-  url "https://github.com/cloudfoundry/bosh-cli/archive/v6.4.16.tar.gz"
-  sha256 "f549ee3359dfe858538989304d0f071783dd682d6101f77c690c7bdb5cd363d0"
+  url "https://github.com/cloudfoundry/bosh-cli/archive/v6.4.17.tar.gz"
+  sha256 "d71ebdb783d9694e96db8200c85dab304b55b158daad208ccd8fb8830ab270ef"
   license "Apache-2.0"
   head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
 
