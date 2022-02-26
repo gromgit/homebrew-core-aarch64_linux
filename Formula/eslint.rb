711a3eb74613a8326dcf9ed4795f8df15ab69f89
Dustin Rodrigues <dust.rod@gmail.com>
2022-02-25 19:01:07 -0500

    eslint 8.10.0
    Closes #95876.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Eslint < Formula
   desc "AST-based pattern checker for JavaScript"
   homepage "https://eslint.org"
-  url "https://registry.npmjs.org/eslint/-/eslint-8.9.0.tgz"
-  sha256 "df6cae949ca03a0297d2eebfb82d995aac13de5e7f911e17d36c2864f51e131f"
+  url "https://registry.npmjs.org/eslint/-/eslint-8.10.0.tgz"
+  sha256 "dadd508c13ac5c54ad0f38d37f338c6f86707101915e6dec84f3e4511c96baa5"
   license "MIT"
 
   bottle do
