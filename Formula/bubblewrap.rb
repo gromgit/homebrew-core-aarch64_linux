0ea45e8352668137c98dce4b23c2914574e46efc
Caleb Xu <calebcenter@live.com>
2022-02-25 23:02:52 -0500

    bubblewrap 0.6.1
    Closes #95889.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bubblewrap.rb
+++ Formula/bubblewrap.rb
@@ -1,8 +1,8 @@
 class Bubblewrap < Formula
   desc "Unprivileged sandboxing tool for Linux"
   homepage "https://github.com/containers/bubblewrap"
-  url "https://github.com/containers/bubblewrap/releases/download/v0.6.0/bubblewrap-0.6.0.tar.xz"
-  sha256 "11393cf2058f22e6a6c6e9cca3c85ff4c4239806cb28fee657c62a544df35693"
+  url "https://github.com/containers/bubblewrap/releases/download/v0.6.1/bubblewrap-0.6.1.tar.xz"
+  sha256 "9609c7dc162bc68abc29abfab566934fdca37520a15ed01b675adcf3a4303282"
   license "LGPL-2.0-or-later"
 
   bottle do
