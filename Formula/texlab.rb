fecd1b02db93d1edfbc138d06f6ec5fd99280fb6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-26 18:06:28 +0000

    texlab: update 3.3.2 bottle.

--- Formula/texlab.rb
+++ Formula/texlab.rb
@@ -7,12 +7,12 @@ class Texlab < Formula
   head "https://github.com/latex-lsp/texlab.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd11cf24bca68961984697b24b111a8b5e47a4e559d8190fd5cd677ad00faae8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4dccfaaedccefdeb181c4a2dff3e5cf8cc1347795c71b02ee78fae466da0540"
-    sha256 cellar: :any_skip_relocation, monterey:       "a30ce78a3342b5437ecdbe55d4b9d131720d720109a86eca1f18e059f6072ca4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6dc5319b6c717275af9e7a21454e0ea5cd9637fc9120822ae58f16a57fe4a028"
-    sha256 cellar: :any_skip_relocation, catalina:       "4555ace0b8df960855919f8c017bcd2e1c542e433cfa46871567e92e9670a05f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d21d3570548017d53847ff7f8ae2e02e9f6b124f8276e9515ad6c2050ea7e48"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e6c6579f14b369215a26f8e05bbdeb4b245a6142df3c470e46430f8b517bb7b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aad770a9da7aa03b4a72a2156a3392c6a40759ce730a3aef80e78ccc81693efb"
+    sha256 cellar: :any_skip_relocation, monterey:       "0f19090f58cc85a63a1fea0c9039bc948c6bf56c78ce36c0452147d59119d5c5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a57291d25a1e5fb1a1de4f6aacfa25672044bb642a82cfe8e5cd0769f47c8c36"
+    sha256 cellar: :any_skip_relocation, catalina:       "ff9008fa28752508f19d90f04baf2b83220b1662d77baf48db571bcab3cb617e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0aaf981f93f011a183cd38d4d0adece290908735785d5196d2965c2de674cab5"
   end
 
   depends_on "rust" => :build
