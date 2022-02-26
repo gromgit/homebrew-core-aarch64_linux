50e03780a34a32bba9abf81ee5574f39d12ebb9b
Caleb Xu <calebcenter@live.com>
2022-02-25 21:51:06 -0500

    roll 2.6.1
    Closes #95879.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -1,8 +1,8 @@
 class Roll < Formula
   desc "CLI program for rolling a dice sequence"
   homepage "https://matteocorti.github.io/roll/"
-  url "https://github.com/matteocorti/roll/releases/download/v2.6.0/roll-2.6.0.tar.gz"
-  sha256 "f550e91a4a483a567cfe5ff59fecebce81b01b48e330f80cb7ffe817a4e21460"
+  url "https://github.com/matteocorti/roll/releases/download/v2.6.1/roll-2.6.1.tar.gz"
+  sha256 "399bd4958d92f82fb75ff308decb2d482c9a8db80234014f6d42f6513b144179"
   license "GPL-2.0-or-later"
 
   bottle do
