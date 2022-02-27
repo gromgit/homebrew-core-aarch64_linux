88f8d90a16222fa91a5f378a8d7a3721b55fd639
Bo Anderson <mail@boanderson.me>
2022-02-27 22:50:13 +0000

    tepl: revision
    * tepl: revision bump for icu4c
    * tepl: update 6.00.0_1 bottle.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/tepl.rb
+++ Formula/tepl.rb
@@ -4,15 +4,15 @@ class Tepl < Formula
   url "https://download.gnome.org/sources/tepl/6.00/tepl-6.00.0.tar.xz"
   sha256 "a86397a895dca9c0de7a5ccb063bda8f7ef691cccb950ce2cfdee367903e7a63"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
-    sha256 arm64_monterey: "0e4dbf7150d529840a821f20d2d0eaf2b569c6e3bd1afbbec242fedec7b9aaaf"
-    sha256 arm64_big_sur:  "ffb98f11bf159e2352d8ed7ca0bb4c62c48f4ba5781c7e2757a02ad5458fc1a0"
-    sha256 monterey:       "cec974116423a8d584025acde11cef9b680be26899e18d8dc986cd0d63b04cc5"
-    sha256 big_sur:        "b5c8dfff1540875f6bd03ec4a7e58ba68d5df65597e80eb7e469ce88c6bd4175"
-    sha256 catalina:       "0c2d2161a8f65a3728e479d26ba2dc736e163b7fe902ec59666d138bfc0de47e"
-    sha256 mojave:         "44b4a1c68e07df9275cb2275f6852e6b713e306f764833c620542a2cb741e565"
-    sha256 x86_64_linux:   "427e014f684cb2d7b3ddea724baa0d93123564be224efbd8632283df1f9fbd14"
+    sha256 arm64_monterey: "778a7adeb2248b9f542c80adcd699256d5806294ac483646b9b1ea5dde73df55"
+    sha256 arm64_big_sur:  "c259a640eb035f7792a4d29f83a812527b3ab131c63c83f87f3225e2e9617819"
+    sha256 monterey:       "05031f88d0d4d4452cfa1333d64ab85e738bcde14e06380fba3706e775f2cd60"
+    sha256 big_sur:        "ef1dd471afe0896ad6e290d66a21a3158318b3cdcbf51fa78a17898fc7e8beac"
+    sha256 catalina:       "b68f14c1ee9bdf418154ea431897a61ca4827669ac7fbd6fa120430972c16cf2"
+    sha256 x86_64_linux:   "935c8e224e39e236bd3f84eaac5baca6bf2a19ba60554434b0a4ffcab21f5ce5"
   end
 
   # See: https://gitlab.gnome.org/Archive/tepl
@@ -24,6 +24,7 @@ class Tepl < Formula
   depends_on "pkg-config" => :build
   depends_on "amtk"
   depends_on "gtksourceview4"
+  depends_on "icu4c"
   depends_on "uchardet"
 
   # Submitted upstream at https://gitlab.gnome.org/GNOME/tepl/-/merge_requests/8
