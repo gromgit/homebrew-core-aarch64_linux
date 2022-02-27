6d4e5799fe9b415c6aefd47d2eddbe027420807c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-27 09:05:34 +0000

    moar: update 1.8.6 bottle.

--- Formula/moar.rb
+++ Formula/moar.rb
@@ -6,12 +6,12 @@ class Moar < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "08111cd36e0982a48174dc3e1ce11e59152b73849024691d6d3704f54bc43441"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "08111cd36e0982a48174dc3e1ce11e59152b73849024691d6d3704f54bc43441"
-    sha256 cellar: :any_skip_relocation, monterey:       "e2f62f1961fd3ff664998fe3efe921324a9254ad0b87250c029406b273a83286"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e2f62f1961fd3ff664998fe3efe921324a9254ad0b87250c029406b273a83286"
-    sha256 cellar: :any_skip_relocation, catalina:       "e2f62f1961fd3ff664998fe3efe921324a9254ad0b87250c029406b273a83286"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "01b9b740754c9cbb0e817c523406acd083982bb7a4328aa50bdab4bab10811a4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc865768ab090a321c1dda2abd277538d1ae16daf387530e47d4e10bb97c7cc9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc865768ab090a321c1dda2abd277538d1ae16daf387530e47d4e10bb97c7cc9"
+    sha256 cellar: :any_skip_relocation, monterey:       "3b8f4b7b5c2aa317509f434605a6a8a7296147a5dd11c73a6809db519f5a46ba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3b8f4b7b5c2aa317509f434605a6a8a7296147a5dd11c73a6809db519f5a46ba"
+    sha256 cellar: :any_skip_relocation, catalina:       "3b8f4b7b5c2aa317509f434605a6a8a7296147a5dd11c73a6809db519f5a46ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4b1fe6ba2e005b08033e0005d68d7245c998ef2ec5a69c28611898419e40b5a8"
   end
 
   depends_on "go" => :build
