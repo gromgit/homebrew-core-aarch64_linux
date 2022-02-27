3b0cd02af9c802525690628c13e161efe9131f01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-27 06:07:21 +0000

    dust: update 0.8.0 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -12,13 +12,12 @@ class Dust < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e6edca1e8a6965f9a88bce456deb43d05270c732b5486666754595de01693026"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "96f49e81189c0120c16bb3da858f7a9c6daad1fba6efaeccf6a3d0d34ce1253b"
-    sha256 cellar: :any_skip_relocation, monterey:       "e32c73ed14a8ef6e0f17ab6346b4bb497acdb243da0edeebc35acdfd4be6c191"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1e21970d1f6b01d5c472c9a56f4d2415f721d3534fb600cf3dd22782c7b10294"
-    sha256 cellar: :any_skip_relocation, catalina:       "1c9527d0399c8b1235c9291b448bfb30c0a4590fd4e9129d17b4bbbbe6ace74a"
-    sha256 cellar: :any_skip_relocation, mojave:         "b044d0c3122ea62432349dd64374ca257d5e9265f158973a2e11ceaf8955cb1d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ea0ed558b2941ac32109b8b8a83bf4f55c3177b26a7b1436e98c71616cf822c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b05f263268332944a219d854546f999de90ac029d10e2947c98702239e6eb7c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f303d90b315765441bc593d0855559ceb289a9e8637833173b283df76f3a3d79"
+    sha256 cellar: :any_skip_relocation, monterey:       "b5b9e12bd8ebee98e9d1cb193832e4ab138e6bf74e71e6188986f1082dfc9d9b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6fdd946e035486cfd039e7f9aa515060a0ce37ae77d8500b177b46c74e2397e2"
+    sha256 cellar: :any_skip_relocation, catalina:       "c39afa0bf516035198979a2bbe71090f0d893fa0329a3e4fe5f926589fcfc919"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d7e2fd3c7e1d34fb2cbdb10a383c28cd3de967c5152f20df26d7d2fffea3308c"
   end
 
   depends_on "rust" => :build
