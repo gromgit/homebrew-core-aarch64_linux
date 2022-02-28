adb440db9723b296e890a2a3108615c654dd1c75
rui <rui@chenrui.dev>
2022-02-28 14:23:50 +0000

    dynaconf: add 3.1.7 bottle.

--- Formula/dynaconf.rb
+++ Formula/dynaconf.rb
@@ -7,6 +7,15 @@ class Dynaconf < Formula
   sha256 "e9d80b46ba4d9372f2f40c812594c963f74178140c0b596e57f2881001fc4d35"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c29b1041d65f3e3681269e8bd8e10d1d7aa5777de261c93ebf78fe0490aaeea9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c29b1041d65f3e3681269e8bd8e10d1d7aa5777de261c93ebf78fe0490aaeea9"
+    sha256 cellar: :any_skip_relocation, monterey:       "dfdc9796f1364ba9713577378b6cf1d3056f3776fa7bcf6a79038c8823fd5908"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dfdc9796f1364ba9713577378b6cf1d3056f3776fa7bcf6a79038c8823fd5908"
+    sha256 cellar: :any_skip_relocation, catalina:       "dfdc9796f1364ba9713577378b6cf1d3056f3776fa7bcf6a79038c8823fd5908"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0515e8630c6f13cdc4fc43e07e4ee5b45df17d9fbdaa033c925788cd829c5e20"
+  end
+
   depends_on "python@3.10"
 
   def install
