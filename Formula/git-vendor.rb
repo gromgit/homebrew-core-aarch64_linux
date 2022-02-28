3479b588dbd06fb0c7b29db5f02f7aea3eb32411
Porkepix <porkepix@gmail.com>
2022-02-28 10:41:43 +0100

    git-vendor 1.3.0
    Closes #95980.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-vendor.rb
+++ Formula/git-vendor.rb
@@ -1,8 +1,8 @@
 class GitVendor < Formula
   desc "Command for managing git vendored dependencies"
   homepage "https://brettlangdon.github.io/git-vendor"
-  url "https://github.com/brettlangdon/git-vendor/archive/v1.2.2.tar.gz"
-  sha256 "f7b3b73ab2246a7572d55eec4e634467bd1ae9414aae8f11a1d4e59e587326ca"
+  url "https://github.com/brettlangdon/git-vendor/archive/v1.3.0.tar.gz"
+  sha256 "774c0ba9596f3231c846dad096f61d7e2906f6fad38c031bf6c01bb8d6c0a338"
   license "MIT"
   head "https://github.com/brettlangdon/git-vendor.git", branch: "master"
 
