2fba37c26742e5daa98293b29144b7488650d0d9
Bo Anderson <mail@boanderson.me>
2022-02-28 15:19:02 +0000

    postgis: migrate to pcre2
    Closes #95998.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/postgis.rb
+++ Formula/postgis.rb
@@ -4,6 +4,7 @@ class Postgis < Formula
   url "https://download.osgeo.org/postgis/source/postgis-3.2.1.tar.gz"
   sha256 "fbab68dde6ca3934b24ba08c8ab0cff2594f57f93deab41a15c82ae1bb69893e"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://download.osgeo.org/postgis/source/"
@@ -31,7 +32,7 @@ class Postgis < Formula
   depends_on "gdal" # for GeoJSON and raster handling
   depends_on "geos"
   depends_on "json-c" # for GeoJSON and raster handling
-  depends_on "pcre"
+  depends_on "pcre2"
   depends_on "postgresql"
   depends_on "proj@7"
   depends_on "protobuf-c" # for MVT (map vector tiles) support
