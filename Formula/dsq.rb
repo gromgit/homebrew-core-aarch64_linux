fd78da3b8bcf2238e184e95924545b563a3e77f0
Rui Chen <rui@chenrui.dev>
2022-02-28 01:10:08 -0500

    dsq 0.4.0 (new formula)
    Closes #95975.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/dsq.rb
@@ -0,0 +1,22 @@
+class Dsq < Formula
+  desc "CLI tool for running SQL queries against JSON, CSV, Excel, Parquet, and more"
+  homepage "https://github.com/multiprocessio/dsq"
+  url "https://github.com/multiprocessio/dsq/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "2875f659816e824f4b064f3097f6a0ac3d2b72f44a331b9ace5118f4b16cbbd5"
+  license "Apache-2.0"
+  head "https://github.com/multiprocessio/dsq.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}")
+
+    pkgshare.install "testdata/userdata.json"
+  end
+
+  test do
+    query = "\"SELECT count(*) as c FROM {} WHERE State = 'Maryland'\""
+    output = shell_output("#{bin}/dsq #{pkgshare}/userdata.json #{query}")
+    assert_match "[{\"c\":19}]", output
+  end
+end
