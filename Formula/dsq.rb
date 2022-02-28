01d54e37a97d489e251f944f03054a37a3cfa6a3
rui <rui@chenrui.dev>
2022-02-28 14:23:59 +0000

    dsq: add 0.4.0 bottle.

--- Formula/dsq.rb
+++ Formula/dsq.rb
@@ -6,6 +6,15 @@ class Dsq < Formula
   license "Apache-2.0"
   head "https://github.com/multiprocessio/dsq.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4d2cad78261156a231f8c26cb7814874b7553fb4834ff567bcf5eab4aef47c19"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63f5370867b5434bb4999178d91d9dde5c511af648d3c0e7e86099768d871ae6"
+    sha256 cellar: :any_skip_relocation, monterey:       "8d8da11a22f687fe433838c37e793d63666142fb032b18536706b1422b5fffe2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3fd9604f9b7b92fa81921333bcf2b812953b605342d4baeb0167de25dde7893b"
+    sha256 cellar: :any_skip_relocation, catalina:       "0af7e7ad3ccac1f3eccd5a6cde2a1a3eb1c33b8faa2088e9354b6f002bdc6cc4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91cbb897fa8b97fec977b75082e438c2b0ee87d8be706a8912c4ae378f1d6439"
+  end
+
   depends_on "go" => :build
 
   def install
