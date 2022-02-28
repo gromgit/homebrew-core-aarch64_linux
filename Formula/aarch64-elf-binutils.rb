5fe2956575da708a90e33df3f21997944c75bfed
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-02-28 03:09:50 +0000

    aarch64-elf-binutils: add 2.38 bottle.

--- Formula/aarch64-elf-binutils.rb
+++ Formula/aarch64-elf-binutils.rb
@@ -10,6 +10,15 @@ class Aarch64ElfBinutils < Formula
     formula "binutils"
   end
 
+  bottle do
+    sha256 arm64_monterey: "8df73acaded40e59291ce9afdc17918f655a4ceb8884f04647b90eb94d59af13"
+    sha256 arm64_big_sur:  "a86842be0ff1841bdc7666b2c6ff4143a83e18a6fc6de147e088055ef4da77a5"
+    sha256 monterey:       "80f0787d5735fa015bebde54cd449f4ca7553163c8e869315dca9c89d077801f"
+    sha256 big_sur:        "def04262f48a5bc1a5b3a9b1331c7eabde55b65c1ca4cf565bda1b075b656846"
+    sha256 catalina:       "707c2aa4f1b263090f811dfa3aba67b890ad64cab3e932266d308ce12a6ba41d"
+    sha256 x86_64_linux:   "adc92ac904443623aa1a2a27c03b5ed7f89dd07dede7f297cd4bdaa83c20850a"
+  end
+
   uses_from_macos "texinfo"
 
   def install
