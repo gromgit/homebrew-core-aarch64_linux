6d3d2ce42dabd7a29600e620c6e56af57cb16e45
Dustin Rodrigues <dust.rod@gmail.com>
2022-02-28 20:37:17 -0500

    vite 2.8.5
    Closes #96017.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vite.rb
+++ Formula/vite.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Vite < Formula
   desc "Next generation frontend tooling. It's fast!"
   homepage "https://vitejs.dev/"
-  url "https://registry.npmjs.org/vite/-/vite-2.8.4.tgz"
-  sha256 "6a2e7ff810c7278c202f287839bea3df51d496f38571ecffeba0f5fbbdd223d1"
+  url "https://registry.npmjs.org/vite/-/vite-2.8.5.tgz"
+  sha256 "5acae2d43d4a566c1326008f278fa775270d364ba4b30fc835460928e221a024"
   license "MIT"
 
   bottle do
