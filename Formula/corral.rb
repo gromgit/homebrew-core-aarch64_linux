5aa255fecbcd439c7b1b76615ade611307dbbb96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-01 00:16:26 +0000

    corral: update 0.5.7 bottle.

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -7,12 +7,12 @@ class Corral < Formula
   head "https://github.com/ponylang/corral.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b389a70284f5f1da146d50765e4ff9267bd3b02b5206c38f3c072842c3cc9f2a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "addcb1c6dff12839ebf340ec658aa1aadb9c4037160e09c419672e3cebd79b94"
-    sha256 cellar: :any_skip_relocation, monterey:       "db06c3ca882a8a111c273671d574561c1644aaa72c20558e46fbbf9391cd1f91"
-    sha256 cellar: :any_skip_relocation, big_sur:        "566c52bcb06f64ea9f526b1eab740cbbdbd6017c3b9add34c03388177cc930be"
-    sha256 cellar: :any_skip_relocation, catalina:       "cee74a03702afdab7071744ac16aa0b433ba5edfc2273b42c6519be760cc4159"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f68ec51f4514b2ec9ad9d729d045b3f6df92952e25cd527723e47c326dcb1f69"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0686d038672fa53840d27dfd661a7c013a93eb97ce76128e4a72321392dc14fe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30aa4c949fd82b68a86be425b9960e0f34d7d9ed88c893be90ef44062c6482a1"
+    sha256 cellar: :any_skip_relocation, monterey:       "386d7d7ba90d915aac87e2d5d9b7b591beb29303f484fca68c9c583981cbbaf2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a8d1119dce9ebcd488c79b9771a7c83858e88733ba70619aa2762fae96b226c9"
+    sha256 cellar: :any_skip_relocation, catalina:       "f75e543d17a1855c87df8a71343c9353aa4acb1e4c9d765134d3b256eca4a286"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59dfb4a4c76b0feec7726eb395eb312b308737702ac4b071d33704ffa0eff2f8"
   end
 
   depends_on "ponyc"
