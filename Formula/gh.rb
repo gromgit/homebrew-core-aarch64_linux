82b93d344de0dc3c95787d67afab7c9bfe577c07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-01 21:10:32 +0000

    gh: update 2.5.2 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -13,12 +13,12 @@ class Gh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "709c1387709be0afdfe2c7ed44eeb9fd51c66516002518fffd29c8b50ca43631"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "71d7cf81d87062f25170183931cddab0b064202abb562273cb6194c737fb3943"
-    sha256 cellar: :any_skip_relocation, monterey:       "d98f6a232a92b8bf7b2ef38a5ecaf05357d83062f5d65fb558cdd6a85a58627e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "058f522856949348778c0b29c6a3562ca859d8a5e5663bf87c4a22b7806415cc"
-    sha256 cellar: :any_skip_relocation, catalina:       "d65fe8d6e4ede5159da1f39f6aa2c6909d04eabb8ff006a449a08c8c0f537fde"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0501de9bfc41637573a4f3c80a85fddaf9938d1ccc3af9a51b14fa74d7c7274a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c1b37e5f0d683586a13844cf54d60ba53974b6358a29de2b0f12308f0df2c84f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a588a3be973bbc49bd3c037713a9688dc95a617734b5b5560131174121554f9"
+    sha256 cellar: :any_skip_relocation, monterey:       "045e0cac75d41a91920962ed9492be496eb244c03fea6515a4d1ecab7cdfdef2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "516db270657d2bb61f7d33fcf8d1eddf996fab5fbce19b17c987a250aa378203"
+    sha256 cellar: :any_skip_relocation, catalina:       "ce758931eba5c13b1eff7ca5933ac11239dcf383c0b9599936632b5e27faa76a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "928c73d5a730c632988ba9688fe67f755e4e379c9eee1b5043fac9d48c9191bf"
   end
 
   depends_on "go" => :build
