1e137502a478bb509f139ced837bd2acfbbc392c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-02 05:34:54 +0000

    zbctl 1.3.5
    Closes #96097.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zbctl.rb
+++ Formula/zbctl.rb
@@ -2,8 +2,8 @@ class Zbctl < Formula
   desc "Zeebe CLI client"
   homepage "https://docs.camunda.io/docs/apis-clients/cli-client/index/"
   url "https://github.com/camunda-cloud/zeebe.git",
-      tag:      "1.3.4",
-      revision: "eaebfb524a6ce9bd21670d17fd38d6ee7f1cc49e"
+      tag:      "1.3.5",
+      revision: "a3abd00949456ff2c4510de34809496afb48f7ee"
   license "Apache-2.0"
   head "https://github.com/camunda-cloud/zeebe.git", branch: "develop"
 
