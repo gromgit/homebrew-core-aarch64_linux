201a39864593f802df8e90ee0d4d2e560cd17a7c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-02 14:09:20 +0000

    sqlite-utils: update 3.25 bottle.

--- Formula/sqlite-utils.rb
+++ Formula/sqlite-utils.rb
@@ -7,12 +7,12 @@ class SqliteUtils < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "77d176c7fb992d88c2395bdf4737af080bd57431cb80ae5ce6a7a6d4cd4a1709"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5e8d591df00e9a0a40037152cb2a062f7cd754a6b5784a7939d66dd3106a030"
-    sha256 cellar: :any_skip_relocation, monterey:       "5a318c629e1121208d3ee398fa033a4e0aad49312421bf69c3b9db2329f452f7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "029399fcdcebee2a7d272a88d9212d00ac20d7cf39dac1d55f982cdb3b141b7d"
-    sha256 cellar: :any_skip_relocation, catalina:       "51807b5f8532a2d1da94392ab038a1e13a666aaecb63b9733c9b43e16e9baffb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eff0dbb99bd73de9941f4cefec03c095451fe692aee8ed61d6235a800125f914"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "179eb92a600e18104c93bd36c22d3febc28f676a0fa8acb94d51383b6c1fe8bf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae1da8bfcf8c55ffad298aa601664f4b998f86172a17e84e635832f1710ad3b7"
+    sha256 cellar: :any_skip_relocation, monterey:       "19395b017a6138868fc1d0810e70ffa5214f4da2e81a6712babd4f79f6f8e54b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a43578ebc00b40941ea94ade6704dd346df5a1477579f37cccf933ac9deb6722"
+    sha256 cellar: :any_skip_relocation, catalina:       "793191feb407cff741aee9b1fceda90902b44dabe3418e33950950183b1eee3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b342ca8c00bfd4b0584b23aedf0622561af2b055e3bf9135eee2f82da58f0dec"
   end
 
   depends_on "python-tabulate"
