deb7dcecc774690b33741336f768c3fb01e50876
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-02 10:10:44 +0000

    pinboard-notes-backup: update 1.0.5.4 bottle.

--- Formula/pinboard-notes-backup.rb
+++ Formula/pinboard-notes-backup.rb
@@ -7,10 +7,12 @@ class PinboardNotesBackup < Formula
   head "https://github.com/bdesham/pinboard-notes-backup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "57a89dec64c38b1742914b5ff0cb7e0a1555715644b465ecae8b2c67c7580ec2"
-    sha256 cellar: :any_skip_relocation, catalina:     "a1c7eb9a928d9461b66bcd43af6dbaa276d7cd730706e2b57833f70c503fdaaa"
-    sha256 cellar: :any_skip_relocation, mojave:       "d0e2d4abf24f35b65aa3f5454febc68444f33fcba3d0c01ce33a5f571ef4477e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3ed421ceef39a006065676d8b8f2c3ba553e0986ab7187a7956f6504389904b2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3069c8f2bf3d19e00854023d614cbfaa54124714bc24f19add7342fddadbc7fb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "872d668ef3797167027e12761a3d7f75624c1fa0f2d2fd319e003647f6d67d0e"
+    sha256 cellar: :any_skip_relocation, monterey:       "014147bd4fab3f85d1d01efa02132303dbe6240ef67c7a71c1fe4f4595ea7d0f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b383069c859d37cc356106c445768f3996f1eac4a83e20d57d9800a0c4328485"
+    sha256 cellar: :any_skip_relocation, catalina:       "8f86319b5bd957198edc1a19cf7af1a11fba9ee5ef950f8f8bd8b8f56af0d37c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "183578ddb3979e5736e4d317eb393c4b967f63421b30eb8a5c8403fafc398624"
   end
 
   depends_on "cabal-install" => :build
