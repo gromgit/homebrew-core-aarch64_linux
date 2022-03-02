78cb2aeb58d7a030b30c96c5cfa140774c25ae1b
Caleb Xu <calebcenter@live.com>
2022-03-02 13:10:49 -0500

    libgusb: migrate to python@3.10
    Closes #96122.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libgusb.rb
+++ Formula/libgusb.rb
@@ -26,7 +26,7 @@ class Libgusb < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
   depends_on "vala" => :build
   depends_on "glib"
   depends_on "libusb"
