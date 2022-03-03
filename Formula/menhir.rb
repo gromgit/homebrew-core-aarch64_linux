31d12e95a520c9c2a30c7a67bc30d15fb16304b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-03 20:05:21 +0000

    menhir: update 20220210 bottle.

--- Formula/menhir.rb
+++ Formula/menhir.rb
@@ -6,13 +6,12 @@ class Menhir < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "90ec00c926e42b81ce0571955fd748fa45fe59f76885d5b99084af73868cfe75"
-    sha256 cellar: :any,                 arm64_big_sur:  "39c8b455817f7a124d0a5cd8b576ba9de5243dfa5486d3f803b7e3c8e36fd22f"
-    sha256 cellar: :any,                 monterey:       "921cfc82b42dc804a7fde786567c8341799cfd6c47d9c41bdb2f9af5375563e2"
-    sha256 cellar: :any,                 big_sur:        "20086a5d407eaf8162f56dd1f9225491db6b987db6f00ecc9aa5a966cdc411e8"
-    sha256 cellar: :any,                 catalina:       "793848617f63013de384e88dc5ac2201b1d8cc2ac80e89f4b26b9f33661262f9"
-    sha256 cellar: :any,                 mojave:         "ab05cc4ce18b62a041365fd4d34f04dc55ae756845288dffe47786a0f2844efc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "efdd4cbdf040c3dfec55f9fd0b256450cc0c678d30e874df912b429f3dce06fc"
+    sha256 cellar: :any,                 arm64_monterey: "034b3aeab807e0f5c9b61b1ac62937cab695e39c3cf9f6cd3b024687ff963822"
+    sha256 cellar: :any,                 arm64_big_sur:  "b5cf9ea01bd4ef559ce640dddf2bbc4602b9e5726d2d1d198a9a6a41d78f631f"
+    sha256 cellar: :any,                 monterey:       "25465835b11b2d3aa42a82587acebb885b8d6c9329fb4d78d746964bbf1d757b"
+    sha256 cellar: :any,                 big_sur:        "a7e520e7f276a33543b8b267b8f66bdd9173c627ae2d25c49012ee2d6e690848"
+    sha256 cellar: :any,                 catalina:       "d5d243ea886ec400aa0eb78576fa3879e991e1265b52abf408cc02034efe4e3e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e6da4390a6dcf83abd5bcfe897946de7f7dd0a7a30ebe5f2eb08a7a32b521be"
   end
 
   depends_on "dune" => :build
