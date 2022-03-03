81eac10b1cca2b4df639621066a6f82cf0443c33
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-03-03 09:51:24 +0000

    uutils-findutils: add 0.3.0 bottle.

--- Formula/uutils-findutils.rb
+++ Formula/uutils-findutils.rb
@@ -6,6 +6,15 @@ class UutilsFindutils < Formula
   license "MIT"
   head "https://github.com/uutils/findutils.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b9afb8c023172346ae4c0fcaf09a5b3ae1902947e63eb7966cc74eaa5497727"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d66b8426c09906c18d3ce3b82d082caef83d5cf701a3858fe49fb8d02a9bd81"
+    sha256 cellar: :any_skip_relocation, monterey:       "9a75049acd7a12e7db1bbb38a97b7b66e1425332f00258347b3821e7a435e6f8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fe2889981a6503b67a0c55ef7d8cb2dd8e078480f298ad577e86791b74e7f3e4"
+    sha256 cellar: :any_skip_relocation, catalina:       "e486fbe2380550b180a10bd9bff432fead265be2430889c14d8c885988f447f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3595b1ba387b3d1c2139edb0ca4612cf393929b4c59a798712e69affeb331a7a"
+  end
+
   depends_on "rust" => :build
   uses_from_macos "llvm" => :build
 
