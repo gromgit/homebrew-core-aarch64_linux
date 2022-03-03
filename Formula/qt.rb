8f87ecb77ad15ee3ffde8753a236a9588e950d28
Michael Cho <cho-m@tuta.io>
2022-03-02 21:45:34 -0800

    qt: revision bump for jasper 3.0.0
    Closes #95096.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -4,6 +4,7 @@ class Qt < Formula
   url "https://download.qt.io/official_releases/qt/6.2/6.2.3/single/qt-everywhere-src-6.2.3.tar.xz"
   sha256 "f784998a159334d1f47617fd51bd0619b9dbfe445184567d2cd7c820ccb12771"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  revision 1
   head "https://code.qt.io/qt/qt5.git", branch: "dev"
 
   # The first-party website doesn't make version information readily available,
