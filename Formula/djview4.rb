6e5f7375b0100172398d981ddc1dd08eccf55ea3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-03 16:03:42 +0000

    djview4: update 4.12_1 bottle.

--- Formula/djview4.rb
+++ Formula/djview4.rb
@@ -15,6 +15,7 @@ class Djview4 < Formula
     rebuild 2
     sha256 cellar: :any, arm64_monterey: "2495aff481ce3d1dc1fd6df41669068388956fe89ecd6302a7ed75f4feccc8e8"
     sha256 cellar: :any, arm64_big_sur:  "d732c90fdab920090c28baf8951d50da4523fc619ce22643819afbf1037e21fb"
+    sha256 cellar: :any, monterey:       "7a692725678245bacf5a728ffb9acdfd87f2e362e3853b2952fc27ca6fe1fc59"
     sha256 cellar: :any, big_sur:        "4e9a79b7d43536f816768e9dd5d5452b5f3f270772a482f5321f7e43712a0a30"
     sha256 cellar: :any, catalina:       "d55757a01f3e6e843427f018559cd3e881c230096b16238b2a8f5bd82379f2a0"
   end
