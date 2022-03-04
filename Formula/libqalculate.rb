8c9685029b7c0282c36b92a73e6daad46e543c25
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-04 08:33:20 +0000

    libqalculate: update 4.0.0 bottle.

--- Formula/libqalculate.rb
+++ Formula/libqalculate.rb
@@ -8,6 +8,7 @@ class Libqalculate < Formula
   bottle do
     sha256 arm64_monterey: "94cfc754a3f82039c24a7438fb060615f38428d0e765e28bbecf47964cab0f95"
     sha256 arm64_big_sur:  "f8f4df3fe84120fd30ee89176f52c48db4149166ae0a0096b3e9f00387906afc"
+    sha256 monterey:       "863c4c1ac56c73210d41bf7f9621de281f1303d8222028be5bb0a58e7e708815"
     sha256 big_sur:        "b37c13c6a799cecf2904b9f3ca68ebaee386b12da7ca6cf11a41d097f6ec7852"
     sha256 catalina:       "1237bc763a1519d58e2c9526ddd43ecabfae09113a509e602a4442ac56f8d5c6"
   end
