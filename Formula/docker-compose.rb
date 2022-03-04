e3e4fa9489db37595b9ab2aff429d7ba87a7bd23
Branch Vincent <branchevincent@gmail.com>
2022-03-04 12:23:10 -0500

    docker-compose 2.3.0
    Closes #96256.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,8 +1,8 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.2.3.tar.gz"
-  sha256 "22210187e73732edd9fc02f122ea61481806c703af7b73d0a7351f2e8ed7c0b8"
+  url "https://github.com/docker/compose/archive/v2.3.0.tar.gz"
+  sha256 "a602aa3f4fb9d71dbcbe26d8cd32733549f45910d6e9f7090a8ed01be53a6c4f"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
