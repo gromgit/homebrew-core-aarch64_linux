fab4b683a0017d44225bddecc32ad428b4ea728d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-04 09:06:53 +0000

    calicoctl: update 3.22.1 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -13,12 +13,12 @@ class Calicoctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b4cefb141c7b7e903d51fe3fb5f9c374becb8a4c4adcafcfcd960675a4df9591"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eadde0fa34fd6fe3efab7218a1f63169d7d1496d264baba918a07063ccea131d"
-    sha256 cellar: :any_skip_relocation, monterey:       "e8025fe02bce16a3051f89c75c391bb52176588c1c461af866efd7910a5da75f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "002342e7381a45112555f34fbc5747c1e5bda6bd643fd845598ff78e65a0a798"
-    sha256 cellar: :any_skip_relocation, catalina:       "6d7ab8e38c91210855da42f8f82f6a1240f69e4b65ce18ac1c21390f55ef5a1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "efc58d8a85af0ff097510c8c10e8817bd0f0ea8a0b686830f0e778aeb45033eb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "782ec16444f4376094729779772f29e30fb7900b13736e30c5a4a3814cd8140f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5093f71e67827a71dae31f963a44a4765259affc1c14a54bdaaec38251aed780"
+    sha256 cellar: :any_skip_relocation, monterey:       "dd82c411170cea69ecb2c381da2caa01672dc0126cf0f8732b50f3ff6d511322"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3f3393d94c1d65f74157063ebf45be5428feaeb389d40d285a6e2ca22435ee60"
+    sha256 cellar: :any_skip_relocation, catalina:       "823aecf02877405103699aaeb00ec3a0dfb9d0b1d590694f803a9a093dc08d02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca0261a747fe3b0fbadb51158106532983306a8a1f60299d962aadd62c0f06dc"
   end
 
   depends_on "go" => :build
