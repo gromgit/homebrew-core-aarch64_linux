3566b51d02aa7482a516ef079510e83b11906f0e
Curudel <andreas.florin.1988@outlook.com>
2022-03-04 13:14:55 +0100

    tfsec 1.6.0
    Closes #96234.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/aquasecurity/tfsec/archive/v1.5.0.tar.gz"
-  sha256 "ebeb3393bdc5911335e5c7a1c38d34f75381d644c0964bbf8d0611b7fe9b0e4e"
+  url "https://github.com/aquasecurity/tfsec/archive/v1.6.0.tar.gz"
+  sha256 "41f6d9da8f5e466d36984eef65514335310c3f62d4ba605e108cde980c35f39f"
   license "MIT"
   head "https://github.com/aquasecurity/tfsec.git", branch: "master"
 
