00f510515c57cec924dd80af05d1bb7c8a69890d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-05 05:23:17 +0000

    himalaya 0.5.8
    Closes #96293.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/himalaya.rb
+++ Formula/himalaya.rb
@@ -1,8 +1,8 @@
 class Himalaya < Formula
   desc "CLI email client written in Rust"
   homepage "https://github.com/soywod/himalaya"
-  url "https://github.com/soywod/himalaya/archive/v0.5.7.tar.gz"
-  sha256 "7bdcf140401aaefd5bf6b639c44124169c5e4d738ed5097dbc8f24b3e4726a91"
+  url "https://github.com/soywod/himalaya/archive/v0.5.8.tar.gz"
+  sha256 "c7b6c63f22a7a8b06f195f7da8bf1de715eab20220a5a5278ee9c533e8a2d505"
   license "BSD-4-Clause"
 
   bottle do
