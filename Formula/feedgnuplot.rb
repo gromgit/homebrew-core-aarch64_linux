5fb08e38d2dd6e31c36f5db1549f2583906dc7e3
Porkepix <porkepix@gmail.com>
2022-03-05 12:41:15 +0100

    feedgnuplot 1.61
    * feedgnuplot 1.61
    * feedgnuplot: wrap scripts
    * feedgnuplot: add perl module dependencies on Linux
    
    Closes #96311.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -1,8 +1,8 @@
 class Feedgnuplot < Formula
   desc "Tool to plot realtime and stored data from the command-line"
   homepage "https://github.com/dkogan/feedgnuplot"
-  url "https://github.com/dkogan/feedgnuplot/archive/v1.60.tar.gz"
-  sha256 "ac84f6f316fc1b5ecc5e5afb47da1385110e3fde39e0d7429b67beee8f465722"
+  url "https://github.com/dkogan/feedgnuplot/archive/v1.61.tar.gz"
+  sha256 "4239fe61a03232ea47909edd59af767b98481b205f58910b2e30d2d41f9caa80"
   license any_of: ["GPL-1.0-or-later", "Artistic-1.0"]
 
   bottle do
@@ -16,11 +16,40 @@ class Feedgnuplot < Formula
 
   depends_on "gnuplot"
 
+  uses_from_macos "perl"
+
+  on_linux do
+    resource "Exporter::Tiny" do
+      url "https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/Exporter-Tiny-1.002002.tar.gz"
+      sha256 "00f0b95716b18157132c6c118ded8ba31392563d19e490433e9a65382e707101"
+    end
+
+    resource "List::MoreUtils" do
+      url "https://cpan.metacpan.org/authors/id/R/RE/REHSACK/List-MoreUtils-0.430.tar.gz"
+      sha256 "63b1f7842cd42d9b538d1e34e0330de5ff1559e4c2737342506418276f646527"
+    end
+
+    resource "List::MoreUtils::XS" do
+      url "https://cpan.metacpan.org/authors/id/R/RE/REHSACK/List-MoreUtils-XS-0.430.tar.gz"
+      sha256 "e8ce46d57c179eecd8758293e9400ff300aaf20fefe0a9d15b9fe2302b9cb242"
+    end
+  end
+
   def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    resources.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make", "install"
+      end
+    end
+
     system "perl", "Makefile.PL", "prefix=#{prefix}"
     system "make"
     system "make", "install"
     prefix.install Dir[prefix/"local/*"]
+    bin.env_script_all_files libexec/"bin", PERL5LIB: ENV["PERL5LIB"] if OS.linux?
 
     bash_completion.install "completions/bash/feedgnuplot"
     zsh_completion.install "completions/zsh/_feedgnuplot"
