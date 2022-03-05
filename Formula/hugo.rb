71b3e35d94ce7f22c6c6fd8aa9cb8234ae28a204
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-05 02:19:49 +0000

    hugo: update 0.93.2 bottle.

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -7,12 +7,12 @@ class Hugo < Formula
   head "https://github.com/gohugoio/hugo.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "925926dd489892dd7beb102d448138889d3a490e990fc02e9bb869667731f78d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dce61bce77c27dd518412fe68fb588fb13b85658606ad6febb3ff465009e4529"
-    sha256 cellar: :any_skip_relocation, monterey:       "3ccaa37f9a0c0102520b80574a09e1fd487ad579fadc581a624572a2c16ebe7f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "360f46cfc7659df7a034d17a297771d5f67422d029ff554ec23269ceb46c76c6"
-    sha256 cellar: :any_skip_relocation, catalina:       "445aa00c087a508209f741e537b1b063f44b3f17bdef8573de4852eeafde4edc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d208accfd6de24a4a812cd93cf3a2ebe20b97a219c2ad9395c7d6b0af225ef40"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fda90210b196e0242ae1c9d7954ed9f151345b0e89b74d48b859d95bcfcbf2fd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e59fa027aa98f1071d132e02ba423412ae09a4c9d9e180b168209c0c7eac2e77"
+    sha256 cellar: :any_skip_relocation, monterey:       "e7ae4c70372ddcb09213c69ba76861dbb8c95881147d4628e919cd6ecf49d74b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b3602dcd5a5fb39b5635c1bcf32473e9623c700aa95f9b1f9b7c5c924b43f010"
+    sha256 cellar: :any_skip_relocation, catalina:       "b46da4a7250868b36ec65f6b84f88fba3776d9b6e914189def38bbda2dc60ed5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c4924f623a3c5b85eb225e2e47351796995c7f2d441e53aa779ed44c6395607b"
   end
 
   depends_on "go" => :build
