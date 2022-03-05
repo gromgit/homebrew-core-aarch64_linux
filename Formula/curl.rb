4b42246f306046554a0c866f49f5ce2b840de805
Michihito Shigemura <i.am.shige@gmail.com>
2022-03-05 19:46:16 +0900

    curl 7.82.0
    Closes #96307.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,11 +1,11 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
-  url "https://curl.se/download/curl-7.81.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-7_80_0/curl-7.81.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-7.81.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-7.81.0.tar.bz2"
-  sha256 "1e7a38d7018ec060f1f16df839854f0889e94e122c4cfa5d3a37c2dc56f1e258"
+  url "https://curl.se/download/curl-7.82.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.82.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-7.82.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-7.82.0.tar.bz2"
+  sha256 "46d9a0400a33408fd992770b04a44a7434b3036f2e8089ac28b57573d59d371f"
   license "curl"
 
   livecheck do
