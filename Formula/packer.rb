ea5391a7923ddaa4088b0446d69839ec18f41acd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-05 10:05:05 +0000

    packer: update 1.8.0 bottle.

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -12,12 +12,12 @@ class Packer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f17dcd55f8fd69fe835752b162f6243771594c147060d2e0816eb4cddd2a293"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "583f04348076c0d4c8cb3f76f2893095f63011e1e92d0aec14d97121ce66397b"
-    sha256 cellar: :any_skip_relocation, monterey:       "43c180132579cb560fc264dec8adcb6e10156e9b889385bf62cadc36d5cc179a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4b139ee1aa9eec2b296aef7989776c858d4662a3d401b594996d80bcd877d6af"
-    sha256 cellar: :any_skip_relocation, catalina:       "e92e0e7206ecd1ba19e91f59b26cfc5815d86bd25e2af9ea470547ed6dfb095f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87e8c49c2f1d1a1bba913f9993e6b61f20595242195ba82b62fc3b2eed533385"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5178d82b7487833b7fd5c1321dfe0580c12abaa0d8d9fdfb60c975b7c54a99da"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f52ae41581a05c0f7d83b6cd7cfb84bb2af8dabbf0e2eb2661b47cb392ce578c"
+    sha256 cellar: :any_skip_relocation, monterey:       "4a2b550d17768d1a00e03bccc8699f544dd29249acd657700d9d7112f5026f05"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eb0787c5942f56790806d20d44e55566b4891a4632f1b7632a0cd3c5535aeaef"
+    sha256 cellar: :any_skip_relocation, catalina:       "904c3f2176e98f2e84bd61c136be1f5e4f293abd508f2815101e31f0df7f90e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "525896c4b4cfdc0c48d643586ac3118ff5414bc7ec88425c4018fc2664bf027f"
   end
 
   depends_on "go" => :build
