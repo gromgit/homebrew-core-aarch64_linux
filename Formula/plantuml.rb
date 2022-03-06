60b7b822db9cb1173280f28ed79ddfdd67ee91e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-06 05:28:30 +0000

    plantuml 1.2022.2
    Closes #96357.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2022.1/plantuml.1.2022.1.jar"
-  sha256 "a043a17fb9cb54626aa0afe9c935b317eb37398b06704cb9a488fca1b4ab9116"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2022.2/plantuml.1.2022.2.jar"
+  sha256 "e0b3d1f207697de6eae54fee9b17057ea7bb8baa891cbaa1380bbb0df6334d8f"
   license "GPL-3.0-or-later"
   version_scheme 1
 
