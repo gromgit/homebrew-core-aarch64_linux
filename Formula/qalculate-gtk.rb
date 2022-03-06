4504a9c8e6ee8de467c4109cbcc1ae05b82505ae
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-06 20:09:26 +0000

    qalculate-gtk: update 4.0.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -8,6 +8,7 @@ class QalculateGtk < Formula
   bottle do
     sha256 arm64_monterey: "beb7ce63f37ed90091ce6e532f6e0b5a8bd456ecd45f0168ddb56d1aa6a0172c"
     sha256 arm64_big_sur:  "c23118504c013f91448dd4adc9e71e5e3132c6cb16971aae9156b049d9f998d6"
+    sha256 monterey:       "a12d3155d392672125b59f8cea3340da6fc1b457d7ef210174ab7fe8a2de639d"
     sha256 big_sur:        "268c069a300fed37259e2033fc80751488e9c15e8f51724bcc4509db7c94a650"
     sha256 catalina:       "bfdece419f5a09959d20d637704d51747bb23cd244b66650aef60bc88f144e72"
   end
