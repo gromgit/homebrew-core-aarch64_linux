5da6e2eda47e4e9ff28b0f1010c486a170f1d532
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-07 14:04:57 +0000

    libvdpau: update 1.5 bottle.

--- Formula/libvdpau.rb
+++ Formula/libvdpau.rb
@@ -11,13 +11,12 @@ class Libvdpau < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "78a1291976be6c185b02c4113fd107a36fe6599bd1cc6e955b78d2652ad1cb87"
-    sha256 arm64_big_sur:  "08dc576dba7a86a6a7fe5b76a3e988f97203b55a7ec52cfa22316cf734960391"
-    sha256 monterey:       "1c6f370c27474352fc1851fe8c0acedd2d3907c5066dfa5062f53b92c0b7973f"
-    sha256 big_sur:        "37a873028601ad2d18ba85aa20c5a0e1b6ffe9bdbce860d4878498d3acd6746e"
-    sha256 catalina:       "bec2b355ee24c1bd5a7c399c073ed99f470ab79adb87bdc4bd739ca70bd7b2db"
-    sha256 mojave:         "e9580c9e952c63e6a4d02fa3e80255f0fb10c71d6b8ff4815769cd4fb80b42e2"
-    sha256 x86_64_linux:   "daf6a4d77a23fa2f55de33361946a05b9cd4fae71e89e93cd86e04537272bfd9"
+    sha256 arm64_monterey: "983ddb3ecfdacb086fd056315553adf0e458e6f4da959a0381d4889e55947635"
+    sha256 arm64_big_sur:  "d474b20b3cd5675c303af15e353b2e23b4107fb43660de06fdca174c2b8a6ac7"
+    sha256 monterey:       "b1ca92eb755c147f47c63a590705159a099cb74f97b0bb3260e26e46979acd04"
+    sha256 big_sur:        "19e0e92759c99ab2942d2b750bd32065b31829015bee25c384929a12f9eea5ca"
+    sha256 catalina:       "d5bdf31825ef0083a0a426f98de307d1f00376804d03e020d096bee5da273def"
+    sha256 x86_64_linux:   "efe7bfed2aff2b6b4d259a9adc1601a17c8c567750a2dfa0ba354a7d9ba0ca42"
   end
 
   depends_on "meson" => :build
