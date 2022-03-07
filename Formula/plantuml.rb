2b2719a8e5c7efbd468cc9a893e2c37a1f2766d7
Michael Cho <cho-m@tuta.io>
2022-03-06 22:17:12 -0800

    plantuml: fix detection of graphviz 3.0.0
    Closes #95933.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,9 +1,12 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2022.2/plantuml.1.2022.2.jar"
-  sha256 "e0b3d1f207697de6eae54fee9b17057ea7bb8baa891cbaa1380bbb0df6334d8f"
+  # Temporarily using tarball to patch in compatibility with graphviz 3+.
+  # TODO: Switch back to official Jar url when issue is fixed.
+  url "https://downloads.sourceforge.net/project/plantuml/1.2022.2/plantuml-1.2022.2.tar.gz"
+  sha256 "f99dd7d35c2aa57e119c78ebb76ca80b5adf229c7a5a46166bac16cb6e550e63"
   license "GPL-3.0-or-later"
+  revision 1
   version_scheme 1
 
   livecheck do
@@ -15,12 +18,16 @@ class Plantuml < Formula
     sha256 cellar: :any_skip_relocation, all: "cd5a6877e5b41eacf286bdb9c425730c0a045c9fa4de612a287566da2d9b6415"
   end
 
+  depends_on "ant" => :build # remove when switching to Jar url
   depends_on "graphviz"
   depends_on "openjdk"
 
+  patch :DATA
+
   def install
+    system "ant", "dist"
     jar = "plantuml.jar"
-    libexec.install "plantuml.#{version}.jar" => jar
+    libexec.install jar
     (bin/"plantuml").write <<~EOS
       #!/bin/bash
       if [[ "$*" != *"-gui"* ]]; then
@@ -35,3 +42,24 @@ class Plantuml < Formula
     system bin/"plantuml", "-testdot"
   end
 end
+
+__END__
+diff --git a/src/net/sourceforge/plantuml/cucadiagram/dot/GraphvizUtils.java b/src/net/sourceforge/plantuml/cucadiagram/dot/GraphvizUtils.java
+index faebb12..95f0696 100644
+--- a/src/net/sourceforge/plantuml/cucadiagram/dot/GraphvizUtils.java
++++ b/src/net/sourceforge/plantuml/cucadiagram/dot/GraphvizUtils.java
+@@ -195,12 +195,12 @@ public class GraphvizUtils {
+ 		if (s == null) {
+ 			return -1;
+ 		}
+-		final Pattern p = Pattern.compile("\\s([12].\\d\\d)\\D");
++		final Pattern p = Pattern.compile("\\s([123])\\.(\\d\\d?)\\D");
+ 		final Matcher m = p.matcher(s);
+ 		if (m.find() == false) {
+ 			return -1;
+ 		}
+-		return Integer.parseInt(m.group(1).replaceAll("\\.", ""));
++		return 100 * Integer.parseInt(m.group(1)) + Integer.parseInt(m.group(2));
+ 	}
+ 
+ 	public static int getDotVersion() throws IOException, InterruptedException {
