24f787507aaa51508445005f87c4720f16433962
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-07 05:19:18 +0000

    opencv: update 4.5.5 bottle.

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -13,6 +13,7 @@ class Opencv < Formula
   bottle do
     sha256                               arm64_monterey: "c30513f997a7b463842be5723dc3524b403ae2e8851c4d6572c3554a9f2ac0b9"
     sha256                               arm64_big_sur:  "3340b75c1daa8c1fc969d40b68b39437b4061d36f54db11c8f566e420e890328"
+    sha256                               monterey:       "b7034490e2db15c8c73efc32550e9ba9e759787e8bf5b30c355ea6c47540579d"
     sha256                               big_sur:        "5ab8654c831a1ad5225dabed141ee4a0f6d945c70a0cec677e4ab320d33f0fa1"
     sha256                               catalina:       "2df7d2874b1669736a0b89fabdc35b3bea0a0572e1cb3eee5fe7cd8a95459ad8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0dbcd7860f347972dcedcd46ed2f3d40568aa10b94bffe90c31abc4a50064b2"
