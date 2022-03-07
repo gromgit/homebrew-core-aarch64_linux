074c899f7e39724f5cfcd4b58e00e308198ae8ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-07 05:29:32 +0000

    pnpm 6.32.3
    Closes #96410.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -3,8 +3,8 @@ class Pnpm < Formula
 
   desc "📦🚀 Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-6.32.2.tgz"
-  sha256 "db76fda0fc0bae5a22652504129a0356edd8aa6e0bebb67a33d941de87bf1684"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-6.32.3.tgz"
+  sha256 "8a1f56162e733959e5367864130429df2a8075749a4e241d8c910010767f7f00"
   license "MIT"
 
   livecheck do
