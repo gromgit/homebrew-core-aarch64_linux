fd7aa0d799b79b389b5b97da7d14a663d6757341
Jacob Ford <j@cobford.com>
2022-03-07 18:34:57 -0500

    whois 5.5.12
    Closes #96453.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/whois.rb
+++ Formula/whois.rb
@@ -1,8 +1,8 @@
 class Whois < Formula
   desc "Lookup tool for domain names and other internet resources"
   homepage "https://packages.debian.org/sid/whois"
-  url "https://deb.debian.org/debian/pool/main/w/whois/whois_5.5.11.tar.xz"
-  sha256 "966281eb8301c14a1acc17e46c70061f73179d80e370aef43065c22f0947fb87"
+  url "https://deb.debian.org/debian/pool/main/w/whois/whois_5.5.12.tar.xz"
+  sha256 "f1c5bab781b7f2357dab1039e8875d41ff7b5d03a78c27443fa26351952a0822"
   license "GPL-2.0-or-later"
   head "https://github.com/rfc1036/whois.git", branch: "next"
 
