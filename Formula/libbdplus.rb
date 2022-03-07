09d13328a74cc12ef950db195981f55e3f7ca980
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-07 12:10:28 +0000

    libbdplus: update 0.2.0 bottle.

--- Formula/libbdplus.rb
+++ Formula/libbdplus.rb
@@ -12,17 +12,12 @@ class Libbdplus < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "cb6b78de3bdd4e5158919461939900bbe9283a6ac09c34d2c24bb7fd53561b40"
-    sha256 cellar: :any,                 arm64_big_sur:  "9bd085bb8401db223ce376d6820337ce1fa5dbf87c4012e50eb203f608f25ab5"
-    sha256 cellar: :any,                 monterey:       "2d0941381a8755b0512cb068e4112979cb56eb8eeb9ec65f8206ae35f5d4b148"
-    sha256 cellar: :any,                 big_sur:        "2641a985c689b115e5c4725688caa4728d1e8aab80b8b7e8763a362406c2d04d"
-    sha256 cellar: :any,                 catalina:       "0f6679a9e46eebf5d7a37a7b09d77b57512774fb3766eb4a359a60de8997a0e0"
-    sha256 cellar: :any,                 mojave:         "d8f4b53ec0ea12bbc02b2962e94dfe5df98ef55005f10209f4fd40213a80f601"
-    sha256 cellar: :any,                 high_sierra:    "478e405b0f9687edcea3f651f4ec922a1bd12c12476c3aa14d1a35d0bb0362bb"
-    sha256 cellar: :any,                 sierra:         "8205ed5218393f7aa7f2035f089e91a417f13d73f4b7e3d46f3afc5073ce7e37"
-    sha256 cellar: :any,                 el_capitan:     "7136cdf433318efb9691d43d078eb20e9647f2ae4999b42cf791736d95047a81"
-    sha256 cellar: :any,                 yosemite:       "13f271c0bb73d496cda7314d6665478c19193f0eb3b9b7a9fbc1eb4a957894c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3928cbc98c769fd8bfd1547393039e8786de55397e9a31db4ec8b353590d8515"
+    sha256 cellar: :any,                 arm64_monterey: "c5e53b35cf902a1b1bed758850dc351c5fa005f78147a39085e284b02ddded9f"
+    sha256 cellar: :any,                 arm64_big_sur:  "f4b3ecf7789e87ae8ebc97cf9a3c00321c79a3cca58d7a6679ea686da6c415e3"
+    sha256 cellar: :any,                 monterey:       "6e72efd19ac6ebf39f3c22240a1e706cd101540a2c2b55c1c83cf4048642339f"
+    sha256 cellar: :any,                 big_sur:        "d0e37545cdc9aa4e23e56dfead1e17ed894431e06bcdc06dec76a4ffb32b6deb"
+    sha256 cellar: :any,                 catalina:       "f4887dd87e0d8ef822f629b603436db20ce331bf4105c2b3d5d2864ac82c9e3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a6adab3c63417c05c7b4e68bebd99dd6e0d133d7d60e1dcfe29552150cb484e0"
   end
 
   head do
