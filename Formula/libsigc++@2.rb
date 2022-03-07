e3ed140a00b5397c9871697254d9d6d557e403ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-07 05:07:09 +0000

    libsigc++@2: update 2.10.8 bottle.

--- Formula/libsigc++@2.rb
+++ Formula/libsigc++@2.rb
@@ -6,13 +6,12 @@ class LibsigcxxAT2 < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6b6ee5de019405fdc5b58a8b8761f60c3fe8d64a916999a0ad39a96117aab9ea"
-    sha256 cellar: :any,                 arm64_big_sur:  "3592c888aba18303eaa7e7837a27e386e311bac18684b613f705917ca6356ed6"
-    sha256 cellar: :any,                 monterey:       "5cba3fb26934f5f573872a72741b08261619e8e73eebe4384e2266b87ad2af4a"
-    sha256 cellar: :any,                 big_sur:        "c24567ac4b9a732a434afcadda0d1a02f573607c73ebb00625e8ffcc6506f331"
-    sha256 cellar: :any,                 catalina:       "6a51eeae589ad4511165b0236a64a7055315eafcf790fcc1ed3df24853fe4bdb"
-    sha256 cellar: :any,                 mojave:         "38886af39d68b304c8e9f4eac36f6a5e6bd21b79ee9a353df7266649f0490128"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c36d5580b4f8970a6db2027820bbf1eb3cca66f56fbf8c56b32c72c3a23cc6c"
+    sha256 cellar: :any,                 arm64_monterey: "f08cb049ca155fb8ac09d1586ea415084ba95e3e2aa760c5b4ddabe51ea44b08"
+    sha256 cellar: :any,                 arm64_big_sur:  "5d024a8626df8d8bb872a2e1f1452ebe793f3b95a9c08814abe36a48e9f19297"
+    sha256 cellar: :any,                 monterey:       "155cb09e024335504393bc4ea4921348449bbcbf08384f7e6e1210c2cee3f403"
+    sha256 cellar: :any,                 big_sur:        "5ea91db7ec5618625126b12e5bf7de2b2d8cc21b77170536f0d9d33de3bc8ffa"
+    sha256 cellar: :any,                 catalina:       "f4aa31cd03380890a69669687fee5a978586ea8cd8613e871864f2a5dcc7bd97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a2909597897d782656e62646e426c9e0f29a11d845b986de0f13a0e07adcd77"
   end
 
   depends_on "meson" => :build
