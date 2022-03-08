6afa0691f08370d55b50b5fa28159cd4daf3cbf6
Porkepix <porkepix@gmail.com>
2022-03-08 14:09:09 +0100

    oh-my-posh 7.33.1
    Closes #96504.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v7.33.0.tar.gz"
-  sha256 "b78dba9f5b544ad509dfd9eb243508b7f7e75fe9c66410dc48cbe92a2c2704b7"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v7.33.1.tar.gz"
+  sha256 "6c297b2a7f1b4c89f359db25a9ebab84f6865ac8c21808b719ece568754dbbc4"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
