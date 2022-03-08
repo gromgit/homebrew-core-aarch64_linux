d491c7d8d8fadc06bcee11d47ddfecfbd8e418a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-08 22:04:59 +0000

    atlas: update 0.3.7 bottle.

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -7,12 +7,12 @@ class Atlas < Formula
   head "https://github.com/ariga/atlas.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d8087b8c013939e733b89a8fd986f398f3a09eb717a05124361cd51412284991"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7e2af5c704497c70143742c2eae646befa616ea55b16d56737263cb9154533a"
-    sha256 cellar: :any_skip_relocation, monterey:       "2fe83d45e8dd146e277f5be4322d8d76cf9c32a3db63af4a9a5ab60997a8518e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e9a98ab598c54fe5be3c61b05b68d96dfc578f4bf1425d40ccb746569abe9fbf"
-    sha256 cellar: :any_skip_relocation, catalina:       "1dbf604bcb4763ea2b43f3f3994139cbf369e86b79537cb61e8da33fe67209d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0d0df3c8fc619f614401ba7b9c050fbd84705a12badfdb5cfdc3859494a23f2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4eed4bc6c5f3feb77e7418b226de83a65de552a357575cb01ab5694cd383fa0b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "586520c4cd1b5fef5dd3b303a28db3fb5b7a18f7d4c11dd2d458dafc7cb0876f"
+    sha256 cellar: :any_skip_relocation, monterey:       "f09196071bec3303a4f70d12c728b05e1419921dbfe5330c6087a47fe8f67b45"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0e3d8f12bffbd62192607bfe8c2c6d72e088029555d246aafede105087e381bc"
+    sha256 cellar: :any_skip_relocation, catalina:       "7e431d5b913036492241df603d3dc89dd03180bfa2d94eeb690a773338e49ea8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d3a318452852655cab9aaa051007485885204ef9d572405beeaa541c371ac37a"
   end
 
   depends_on "go" => :build
