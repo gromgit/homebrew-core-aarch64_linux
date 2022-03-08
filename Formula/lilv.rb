176d099f0ed4b8e0b4f858ce1fb6621fd87dd616
Caleb Xu <calebcenter@live.com>
2022-03-08 11:39:13 -0500

    lilv: use brewed python
    Closes #96525.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -4,6 +4,7 @@ class Lilv < Formula
   url "https://download.drobilla.net/lilv-0.24.12.tar.bz2"
   sha256 "26a37790890c9c1f838203b47f5b2320334fe92c02a4d26ebbe2669dbd769061"
   license "ISC"
+  revision 1
 
   livecheck do
     url "https://download.drobilla.net/"
@@ -20,14 +21,30 @@ class Lilv < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "python@3.10" => [:build, :test]
   depends_on "lv2"
   depends_on "serd"
   depends_on "sord"
   depends_on "sratom"
 
   def install
-    system "./waf", "configure", "--prefix=#{prefix}"
-    system "./waf"
-    system "./waf", "install"
+    system "python3", "./waf", "configure", "--prefix=#{prefix}"
+    system "python3", "./waf"
+    system "python3", "./waf", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <lilv/lilv.h>
+
+      int main(void) {
+        LilvWorld* const world = lilv_world_new();
+        lilv_world_free(world);
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}/lilv-0", "-L#{lib}", "-llilv-0", "-o", "test"
+    system "./test"
+
+    system Formula["python@3.10"].opt_bin/"python3", "-c", "import lilv"
   end
 end
