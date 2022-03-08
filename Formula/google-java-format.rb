21ee9ee2e64098c2e674cd2986d6e91a26f39720
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-08 05:19:48 +0000

    google-java-format 1.15.0
    * google-java-format 1.15.0
    * google-java-format: update resource and remove exports
    
    Closes #96473.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -3,8 +3,8 @@ class GoogleJavaFormat < Formula
 
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/releases/download/v1.14.0/google-java-format-1.14.0-all-deps.jar"
-  sha256 "0d27d3e58c3a9d91d89b424d467fabde24e8c67b4662606153bcd01b383ebd77"
+  url "https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar"
+  sha256 "a356bb0236b29c57a3ab678f17a7b027aad603b0960c183a18f1fe322e4f38ea"
   license "Apache-2.0"
 
   bottle do
@@ -15,18 +15,13 @@ class GoogleJavaFormat < Formula
   depends_on "python@3.10"
 
   resource "google-java-format-diff" do
-    url "https://raw.githubusercontent.com/google/google-java-format/v1.14.0/scripts/google-java-format-diff.py"
+    url "https://raw.githubusercontent.com/google/google-java-format/v1.15.0/scripts/google-java-format-diff.py"
     sha256 "4c46a4ed6c39c2f7cbf2bc7755eefd7eaeb0a3db740ed1386053df822f15782b"
   end
 
   def install
     libexec.install "google-java-format-#{version}-all-deps.jar" => "google-java-format.jar"
-    bin.write_jar_script libexec / "google-java-format.jar", "google-java-format",
-      "--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
-      --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
+    bin.write_jar_script libexec/"google-java-format.jar", "google-java-format"
     resource("google-java-format-diff").stage do
       bin.install "google-java-format-diff.py" => "google-java-format-diff"
       rewrite_shebang detected_python_shebang, bin/"google-java-format-diff"
