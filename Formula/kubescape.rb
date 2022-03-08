5fb23c11a89d05e6d4211d4552b529d2bfc11368
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-08 16:10:16 +0000

    kubescape: update 2.0.149_1 bottle.

--- Formula/kubescape.rb
+++ Formula/kubescape.rb
@@ -8,12 +8,12 @@ class Kubescape < Formula
   head "https://github.com/armosec/kubescape.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5c614412a4eca569f33702e96723669f82604db357ff3b13931efce7305a86ee"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66c8de2156a525ccf9172900af8f0ffa34b911815fcb2cfd6fb3ada1dacf5f1e"
-    sha256 cellar: :any_skip_relocation, monterey:       "4accde3031c36b1c54191bc6404fe235f45ca4245ffb6c645a6efdbf9524780e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "de70944f5b3232b82f92327c702c7a6acf393d1d64a833cfb395c3790a3f9ba2"
-    sha256 cellar: :any_skip_relocation, catalina:       "0d2e6b1de2c308c63e8a0fd47b7144f8564b8e1b2a7c696849cfdd3b9a81132d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2bffb1adcf3c8ecd0a82c0535d46c6175f14a67fe1e43b5617b2b8ec1cb9ee5d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3028b98618616a19367060d2a0db49cb86d22566be3266b0fe4fabb911361a29"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c1027f1d6e1185232b8afc256f26fd5c5ad3c30596f73d2ecbfddf4d0db45d5"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd902604e65a755f2b9bdadf2f4d2c67152613979ccaa381f2ccaad5d1b8a520"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8cac59fdbd8ef6894eff5bab5b0b4086837350e771d4d86cb18c6fb972fe89fd"
+    sha256 cellar: :any_skip_relocation, catalina:       "c61c7cec44760b9b1fb369dbc08837b3f6905a47398f747ed07654b83b03e162"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "762723b1d4624f3c186a814befa3f8bf44d9beb872c2ce918203e4286077bd18"
   end
 
   depends_on "go" => :build
