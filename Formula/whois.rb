ef83d6885f1cd5c668e89376bb5bb0c7617b2d85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-08 09:04:47 +0000

    whois: update 5.5.12 bottle.

--- Formula/whois.rb
+++ Formula/whois.rb
@@ -7,12 +7,12 @@ class Whois < Formula
   head "https://github.com/rfc1036/whois.git", branch: "next"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "be657821ee4224c09ebe3909fc441b2a8baa78cd369425c4669effae0abba9b3"
-    sha256 cellar: :any,                 arm64_big_sur:  "5b42ff43ea4711c8ca3c0c40bff695aa5963c1414326fb837a47e9712f88f99b"
-    sha256 cellar: :any,                 monterey:       "d09d231e93cdc0425c53400bac35585454b2ab6e440517f6f91f8fbad0636d1d"
-    sha256 cellar: :any,                 big_sur:        "3df50be53c085527789f072e304be94afaa43d7a4a5f7723b52c937fd424215c"
-    sha256 cellar: :any,                 catalina:       "c3660283e22b3999805d3b777c766b1f002fe4979af09595ad8f6ec822f38df7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28a3b321ea1423176b44bb8892411ebaa806bda4de732e29a7a94488c08d58b8"
+    sha256 cellar: :any,                 arm64_monterey: "b4720acfa1b987e9738c3acd8948698fd325588763a7593b5f87a276b9595b93"
+    sha256 cellar: :any,                 arm64_big_sur:  "dd6d8c5e9bb131addb48082059705d0abbe7f4ecc71b6d59a539ce8e9ae08dcf"
+    sha256 cellar: :any,                 monterey:       "83b24159f0fca357cb892915ae6a249a32736966e789cfd3539721b00b479ece"
+    sha256 cellar: :any,                 big_sur:        "66c3e022a8c075da62d4df8bb4500bce90d7ee9fb82d638cc93a71399f40fc2c"
+    sha256 cellar: :any,                 catalina:       "55d9dd9d902b5467555633eacb61ca356eba7b48b2712b13b7861735ddaafaf1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "723ca03647ec2a5e6179a9d1d227bef92589d79a0f699aacd13151c416754358"
   end
 
   keg_only :provided_by_macos
