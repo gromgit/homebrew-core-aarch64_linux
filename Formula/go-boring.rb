d20705a2b584f061e24473b27a9cce508f2f8cac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-08 15:11:49 +0000

    go-boring: update 1.17.8b7 bottle.

--- Formula/go-boring.rb
+++ Formula/go-boring.rb
@@ -12,12 +12,12 @@ class GoBoring < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "4de553db29a5b32f1ddc2e4053a80e790ff14ff2e695d12bece712ccb6187df0"
-    sha256 arm64_big_sur:  "63a994467543d5093854c6411ee1468c3d943d04ed905bb2dcb30743e2a725a6"
-    sha256 monterey:       "c86b010202fb30ba6aae2cb6f7afc696f8e5c17a4c5ed54f7c08106679a456c9"
-    sha256 big_sur:        "54c31aff5809f3d658e20f342826fd5f27c45ada2a246726d97177ac369eef65"
-    sha256 catalina:       "6abded057bd85d4f82bc9be18c768dc9e9a92f797216fc1c29509fba1e6f4a89"
-    sha256 x86_64_linux:   "0bff363b5e8cdfdc59fd3da75740d4e1587a8a6c938383cbeeb4872c964bbb44"
+    sha256 arm64_monterey: "0992d27043050890c51f91ea0d053a9979372599eb97102ce5df73a4ef7b9bd9"
+    sha256 arm64_big_sur:  "867089c22223091956b419130dd52f13783bb6a261c9245e5fba0417551b5dc8"
+    sha256 monterey:       "b2b7b18974cf7a41c4cc4063db493e0c6e1fcdd9ff791ac7becdddc7777e7cd3"
+    sha256 big_sur:        "623d7a3fc706b41f10c9225e80d7d1f00c763c0748b5734dd618d4c2885ff363"
+    sha256 catalina:       "ab721b770c9486ab05d242a61012e21212406bc485372ba22aa318b8d4c4830c"
+    sha256 x86_64_linux:   "916466fa44beb8a4a12f5b618cdbdc8d278629d9b753c377b1002e8d75460730"
   end
 
   keg_only "it conflicts with the Go formula"
