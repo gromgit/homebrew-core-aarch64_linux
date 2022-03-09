d09362505c6e8628088ba30aa0b881dbf15c7cb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-09 16:10:02 +0000

    dscanner: update 0.12.1 bottle.

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -8,10 +8,12 @@ class Dscanner < Formula
   head "https://github.com/dlang-community/D-Scanner.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "56a476c84db1007ff97be57ee3ed13c4f36f82cb7534850cc7f6f282cccf7dd5"
-    sha256 cellar: :any_skip_relocation, big_sur:      "7ceae49df874f59fa2f86cb46f59d85f9d0df6525eac979442bf2be3dccafbea"
-    sha256 cellar: :any_skip_relocation, catalina:     "1907fe417ebf7472cb6aadddca739500a99221fee4a16a8d62f42b368cfcfb59"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "8dc52ce3f3a82d9eba76280c8cbfc70a08227288cd05cf2c829cc32b76662fdc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b5d0c35d95ebfb026a56d523b754ce1d872cadcee2f7ea031459f929cb54755e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23838b253d81dc344dc222e4e3b020021cf1ccdfbc32ffae5458a9cbe0584f81"
+    sha256 cellar: :any_skip_relocation, monterey:       "0d48ae238ece9f6e2919117e20ce07a9b9d3d02ef50e14397cff8688ecc7594c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8240e6a2d9f8f18e7a4d9a1f561b701181917745a867204a7449496b4bddfe4e"
+    sha256 cellar: :any_skip_relocation, catalina:       "58984bf1dbe3703f5dab362774773d4ee7bbc7e87bc09939f825da2feaafc867"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "60efa9016af0bb078a47e3608a192d25203843166346caa27a6a71141b37a100"
   end
 
   if Hardware::CPU.arm?
