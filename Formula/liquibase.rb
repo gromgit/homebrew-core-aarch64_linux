510b84bb57dbd6087976027ac13230e333743541
Derek Smart <derek@grindaga.com>
2022-03-09 10:28:37 -0500

    liquibase: fix JAVA_HOME
    Liquibase is created to look for the system JAVA_HOME and this is expected behavior. The formulae has had a hardcoded JAVA_HOME since 4.2.1 creating an non-standard liquibase experience.
    
    Closes #96583.
    
    Signed-off-by: Derek Smart <derek@grindaga.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -4,6 +4,7 @@ class Liquibase < Formula
   url "https://github.com/liquibase/liquibase/releases/download/v4.8.0/liquibase-4.8.0.tar.gz"
   sha256 "7462b6e92f7077e1858865c403d52f0dce1bd66d03b1fae907815c10825feb33"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "834f5d7086283d639aba0b0df361dc65f84863adcbeef0be4519e440d264ca17"
@@ -16,7 +17,7 @@ class Liquibase < Formula
     chmod 0755, "liquibase"
     prefix.install_metafiles
     libexec.install Dir["*"]
-    (bin/"liquibase").write_env_script libexec/"liquibase", JAVA_HOME: Formula["openjdk"].opt_prefix
+    (bin/"liquibase").write_env_script libexec/"liquibase", Language::Java.overridable_java_home_env
     (libexec/"lib").install_symlink Dir["#{libexec}/sdk/lib-sdk/slf4j*"]
   end
 
