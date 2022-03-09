ac45e827fb77027142b57ef4bcae65e85a825bfd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-09 20:05:04 +0000

    packetbeat: update 8.1.0 bottle.

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -8,12 +8,12 @@ class Packetbeat < Formula
   head "https://github.com/elastic/beats.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dfd9610145038f7ac193c16843d07145894630ca572c2bde95af55288382f974"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "53fa8799495dcef7be1ca214b9af289aa318159bff7f231ead9060775bfa2043"
-    sha256 cellar: :any_skip_relocation, monterey:       "243a333e3d346a915cbd33903d2db969044b30516d4e1fdd2bc3ade8bb3415ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4ad577b35a2118131e7f488d40637e343ce85350e67aa93e0b397cfc6b003ae1"
-    sha256 cellar: :any_skip_relocation, catalina:       "8d408349bd180d574da274ebe0825f2d50124747b2fbbac4b32246a112d64772"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6a6f9aa912c80eff523285e0ccfe039eb0a654fde3a734e70d1c0f8b9cebf89"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6db9116237eb34e6f5d839528129326f95391dd4c705900ea1a3025c336b0906"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4cdef56089c465c947d35500d03a7406d92ac0c119bd1f70a70daf0b05d95226"
+    sha256 cellar: :any_skip_relocation, monterey:       "b0533b020c8c2d1489b0c8e431895c056aeebcd297a26c7fb050c3fe2d1e36e9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0c6265d1cf290633e91fd04a78ef7083a332528210b4737e38762764710eafd8"
+    sha256 cellar: :any_skip_relocation, catalina:       "f4f3e649bf77fa64eae59fb3ed4b3243b35cc0ca82b93f70ab85369284be1f94"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b111e35dd7e76ef554ca7397386d799842f45460ca1e609a0bf23e013c339878"
   end
 
   depends_on "go" => :build
