a76755a31b5f4974941a0ad4c5b8d189315c28bd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-09 06:05:41 +0000

    delve: update 1.8.2 bottle.

--- Formula/delve.rb
+++ Formula/delve.rb
@@ -6,12 +6,12 @@ class Delve < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "61846ca640b7c59d98e84753fece1bc7d8a3bd7f9e25451bb8ff5bdf1febf1a8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b0009b587b2f599afb29349691a3a1104a76a21063e1d03cf7d5cb4813f75f05"
-    sha256 cellar: :any_skip_relocation, monterey:       "25735f6efd51c6fca8ca01d4febc436f21528b4c2e01956258cf6736abfe254e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "55a52711df12f049e0bc2c1ac2b477fa0b5495f4d337b64df22159593ca16f38"
-    sha256 cellar: :any_skip_relocation, catalina:       "43e1d025ac54b525e62baa90bc371e0eab724082283fe7064d8dacdde0d55d02"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d4c8cf69d9ca0dae198573b788429080018f11259e509509661d989cb624d41"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "957539f169cc7ec13f2ceea9df59f6ff0792ae1ba85eb05d4033c74685e4f36e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f9063fff4cf1c00a915319d2eb1dcdf8d77dd56cab2dd3b90eba0572d1d5e179"
+    sha256 cellar: :any_skip_relocation, monterey:       "b979ccf6e22b876b9616cab7b85a491f752c38ba9c34ddc7a180f94610930dee"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d5b22700bfc0154f602b8ba011dc28f6c7dbb404f8d0ba7cd4b2f13d28f19a17"
+    sha256 cellar: :any_skip_relocation, catalina:       "a70c4a1439d4c897f9a8cf7f62cca277db7ae84cf35ce076d270ecca0f419db7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1bb6826f4e58c92b5dab6f2e505f954292f08456821311adff7e108af4d99935"
   end
 
   depends_on "go" => :build
