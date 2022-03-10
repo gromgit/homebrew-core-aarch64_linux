affc623788fdb4acd20e96607998e1d768c5adc6
Porkepix <porkepix@gmail.com>
2022-03-10 12:33:52 +0100

    zenith 0.13.0
    * zenith 0.13.0
    * zenith: remove ansi codes in test and fix Linux build
    
    Closes #96646.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zenith.rb
+++ Formula/zenith.rb
@@ -1,8 +1,8 @@
 class Zenith < Formula
   desc "In terminal graphical metrics for your *nix system"
   homepage "https://github.com/bvaisvil/zenith/"
-  url "https://github.com/bvaisvil/zenith/archive/0.12.0.tar.gz"
-  sha256 "2b33892be95149550c84179b341e304c4222e3489bc121ea8c8346e075433aa6"
+  url "https://github.com/bvaisvil/zenith/archive/0.13.0.tar.gz"
+  sha256 "e3f914e5effb842f5931b5b8310e05e90a40f6aff7384b54a9f18a73ba567032"
   license "MIT"
   version_scheme 1
   head "https://github.com/bvaisvil/zenith.git", branch: "master"
@@ -23,6 +23,8 @@ class Zenith < Formula
 
   depends_on "rust" => :build
 
+  uses_from_macos "llvm" => :build
+
   def install
     system "cargo", "install", *std_cargo_args
   end
@@ -32,11 +34,14 @@ class Zenith < Formula
     require "io/console"
 
     (testpath/"zenith").mkdir
-    r, w, pid = PTY.spawn "#{bin}/zenith --db zenith"
+    cmd = "#{bin}/zenith --db zenith"
+    cmd += " | tee #{testpath}/out.log" unless OS.mac? # output not showing on PTY IO
+    r, w, pid = PTY.spawn cmd
     r.winsize = [80, 43]
     sleep 1
     w.write "q"
-    assert_match(/PID\s+USER\s+P\s+N\s+↓CPU%\s+MEM%/, r.read)
+    output = OS.mac? ? r.read : (testpath/"out.log").read
+    assert_match(/PID\s+USER\s+P\s+N\s+↓CPU%\s+MEM%/, output.gsub(/\e\[[;\d]*m/, ""))
   ensure
     Process.kill("TERM", pid)
   end
