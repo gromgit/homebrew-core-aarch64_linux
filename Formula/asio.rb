0af8a89dba0b58289e7d01f489d31e782e4f4ae6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-03-10 08:48:08 +0000

    asio: update 1.22.0 bottle.

--- Formula/asio.rb
+++ Formula/asio.rb
@@ -12,13 +12,12 @@ class Asio < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "55ef1bffa04528d8b719a1d4885a66dedb61417cf6ffb148bbf9cbb46f19c744"
-    sha256 cellar: :any,                 arm64_big_sur:  "6ae63027103addf746fc975cf3b9e521b7ab590e4e1a7891e8e10b433533607e"
-    sha256 cellar: :any,                 monterey:       "74df62b424d6eb41db252df523b3148e73e9d635aabf515434e3e93bec435967"
-    sha256 cellar: :any,                 big_sur:        "685e304d5cbb0291585a2941c4bc521e3d60175e31ecfc91146d480a84f04325"
-    sha256 cellar: :any,                 catalina:       "d98a3f8267b7e3971fb5c712324858aa02252c718b55dc7e842c31922ad72d1c"
-    sha256 cellar: :any,                 mojave:         "ae04393a3164eff530766bf44aa2983534d3eb1115879b0a9f9c6e027b1b9fca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7011f43a03777135319c7e21f5a96e57b31ced599c41fbf07aba6c0c46871e4b"
+    sha256 cellar: :any,                 arm64_monterey: "bb9bc380f31b310a58903d3279f8063bf43b8d01fcc0b017b7d27038cdff73cb"
+    sha256 cellar: :any,                 arm64_big_sur:  "f0c0a759b985698ae75f3518f6433709ec35e049b8dd31f6214d6f558a1866be"
+    sha256 cellar: :any,                 monterey:       "f0087aa5a0c0d1abc32f7012a83b5e1e20a8821d9dd64902982d6ef676571feb"
+    sha256 cellar: :any,                 big_sur:        "b9c4dcfc70f7f67ad60386c29c99295ebf1e175a9388983dd229cf5e0045678e"
+    sha256 cellar: :any,                 catalina:       "75f5d5616511210eb6e208b72c6676998c5354bb08751207f494cb9c4225a674"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2a855ff871ff92b07db6dc50b667b0e97caa9f31f1963f6b1761d8dd9f8ec14a"
   end
 
   depends_on "autoconf" => :build
