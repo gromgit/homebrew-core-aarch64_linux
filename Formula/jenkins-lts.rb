efcdbc81505cc6a65416b1652ae80880cf0c2edf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-10 05:17:08 +0000

    jenkins-lts 2.332.1
    Closes #96625.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jenkins-lts.rb
+++ Formula/jenkins-lts.rb
@@ -1,8 +1,8 @@
 class JenkinsLts < Formula
   desc "Extendable open-source CI server"
   homepage "https://www.jenkins.io/index.html#stable"
-  url "https://get.jenkins.io/war-stable/2.319.3/jenkins.war"
-  sha256 "73dd91b395bcf62fe25e6d3074765f1510c9e584ecc1e9faf44051064181b434"
+  url "https://get.jenkins.io/war-stable/2.332.1/jenkins.war"
+  sha256 "5a14b379574419abb14123b45d0c6c32276c3198bf694239c93585ef78c33977"
   license "MIT"
 
   livecheck do
