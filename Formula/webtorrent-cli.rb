b04e0783f1889fdb58140a18a9a41e8bf4fedd91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-10 08:05:01 +0000

    webtorrent-cli: update 4.0.4 bottle.

--- Formula/webtorrent-cli.rb
+++ Formula/webtorrent-cli.rb
@@ -8,12 +8,12 @@ class WebtorrentCli < Formula
   license "MIT"
 
   bottle do
-    sha256                               arm64_monterey: "527a416531ee2a2b2c5df2a15d2983df7546059c73de4142bdc00744d3fe6b82"
-    sha256                               arm64_big_sur:  "798c810d0489bd8d6664ec648c97d07407c5647bc1cbf7bd36de2804bf4b1154"
-    sha256                               monterey:       "84bf71b3a59f9073f40ba6b4cd8d172d5039303922f8d9ee0d25241f4121c90a"
-    sha256                               big_sur:        "3b266de81074ecaed9edda04a70a02731e3b8db3428a5b8de5f6e58fcd6f757a"
-    sha256                               catalina:       "79a0dba49fdc352a4aca1641dc658140ca2eab2ae7fe41df7f10ecc25b4a3d5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5735224576fb1a2f80b3c37d92795f52faa75e19a1f31bc380fa06e63ac7012"
+    sha256                               arm64_monterey: "86c1ef7f88c13e56146713df15a33f85a5f355a6d91bf57c19780b5e7bb33278"
+    sha256                               arm64_big_sur:  "9bf6bb347cfe2258439be6bc0a4648103de1fc7556cc43f909d6844f38731bcb"
+    sha256                               monterey:       "82e63d789856811c2b77fc4243cd00bca469f9b73965a3f2d94d256ea4b31f62"
+    sha256                               big_sur:        "8eb99390349f4c412182832ef688af98066ae25ec7dc7642871d119ed13a684a"
+    sha256                               catalina:       "b67456ce6764331e165004e0a13cb41562bc9fb6c16b736c9ac34b1d9151792d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5460bb5cb67b8c4d3208df36d641d020c22c147209ff0e721ef734ebf4c6077f"
   end
 
   depends_on "node"
