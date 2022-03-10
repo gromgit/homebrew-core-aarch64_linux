af422d431054316856b7e4bc57567da296bbcdf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-10 05:27:22 +0000

    trino 373
    * trino 373
    * trino: update resources
    
    Closes #96630.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trino.rb
+++ Formula/trino.rb
@@ -3,8 +3,8 @@ class Trino < Formula
 
   desc "Distributed SQL query engine for big data"
   homepage "https://trino.io"
-  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/372/trino-server-372.tar.gz"
-  sha256 "ca2dfc24f86050b3454ec018d273b9f5d58e0f71a9b4aea86b94059dabe827a3"
+  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/373/trino-server-373.tar.gz"
+  sha256 "3545c1ba5ab1cd5cb4f13c21a9c0ab29a4b59a164b8accbf8c8625fec36a0b8f"
   license "Apache-2.0"
 
   livecheck do
@@ -20,18 +20,17 @@ class Trino < Formula
   end
 
   depends_on "gnu-tar" => :build
-  depends_on arch: :x86_64
   depends_on "openjdk"
   depends_on "python@3.10"
 
   resource "trino-src" do
-    url "https://github.com/trinodb/trino/archive/372.tar.gz", using: :nounzip
-    sha256 "d81b88f823e22b0a09ee749adfc15a9c589cf3fbf0c68219297d84ef59ed08e5"
+    url "https://github.com/trinodb/trino/archive/373.tar.gz", using: :nounzip
+    sha256 "bb31510a9a69a1adcf612c007b4f8b6055eaa75494bdb29fa81d805a5a42045c"
   end
 
   resource "trino-cli" do
-    url "https://search.maven.org/remotecontent?filepath=io/trino/trino-cli/372/trino-cli-372-executable.jar"
-    sha256 "61121f9dda87586ddaddc1d969102a2587f6408b7285e6a2a8218be9ecc4c9b9"
+    url "https://search.maven.org/remotecontent?filepath=io/trino/trino-cli/373/trino-cli-373-executable.jar"
+    sha256 "4a2ec84185253b7238bb92515aba28869fe0a71190168734f41bb5e5b0c4c6ee"
   end
 
   def install
