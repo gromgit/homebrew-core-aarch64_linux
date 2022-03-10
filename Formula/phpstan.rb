46ab2a8d513524d644dd533b07bb2a42f715e273
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-10 14:56:49 +0000

    phpstan: update 1.4.9 bottle.

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -6,12 +6,12 @@ class Phpstan < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1fc915d6a834f9fd06bcf6c5150c417346a07aa53012b00cdda880ecb7aeca87"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1fc915d6a834f9fd06bcf6c5150c417346a07aa53012b00cdda880ecb7aeca87"
-    sha256 cellar: :any_skip_relocation, monterey:       "caf4c7bb7ce5efcedf3c8ce2caa3365ff829edf84868ddaae0eceaf878bf5c0b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "caf4c7bb7ce5efcedf3c8ce2caa3365ff829edf84868ddaae0eceaf878bf5c0b"
-    sha256 cellar: :any_skip_relocation, catalina:       "caf4c7bb7ce5efcedf3c8ce2caa3365ff829edf84868ddaae0eceaf878bf5c0b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1fc915d6a834f9fd06bcf6c5150c417346a07aa53012b00cdda880ecb7aeca87"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "017b4d9594fd1dce422cd535abac271baea6f462f99fc5255cd0c1e82f53e5ec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "017b4d9594fd1dce422cd535abac271baea6f462f99fc5255cd0c1e82f53e5ec"
+    sha256 cellar: :any_skip_relocation, monterey:       "7e033e27ba4bc09e9b0c96d9e8341a2d151720519c772b5a7709fee193403214"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7e033e27ba4bc09e9b0c96d9e8341a2d151720519c772b5a7709fee193403214"
+    sha256 cellar: :any_skip_relocation, catalina:       "7e033e27ba4bc09e9b0c96d9e8341a2d151720519c772b5a7709fee193403214"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "017b4d9594fd1dce422cd535abac271baea6f462f99fc5255cd0c1e82f53e5ec"
   end
 
   depends_on "php" => :test
