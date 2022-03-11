d2dc77c8a294888419887e4c693deaf5b06f61a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-11 05:06:52 +0000

    cargo-instruments 0.4.5
    Closes #96669.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -1,8 +1,8 @@
 class CargoInstruments < Formula
   desc "Easily generate Instruments traces for your rust crate"
   homepage "https://github.com/cmyr/cargo-instruments"
-  url "https://github.com/cmyr/cargo-instruments/archive/v0.4.4.tar.gz"
-  sha256 "e71b29af433f5701483827620866fd6302999d08ddb559578261951da15601cc"
+  url "https://github.com/cmyr/cargo-instruments/archive/v0.4.5.tar.gz"
+  sha256 "aa7539badcca7ede2973face64f0daacfa8c76fac511d21397b9b6837c138cda"
   license "MIT"
 
   bottle do
