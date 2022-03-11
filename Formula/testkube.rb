dec6ddd3d31b5abcb6913ad1c51963068c335088
Vladislav Sukhin <vladislav@kubeshop.io>
2022-03-11 12:17:17 +0300

    testkube 0.10.1 (new formula)
    Closes #96685.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/testkube.rb
@@ -0,0 +1,29 @@
+class Testkube < Formula
+  desc "Kubernetes-native framework for test definition and execution"
+  homepage "https://testkube.io"
+  url "https://github.com/kubeshop/testkube/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "ddcc802c4150bf456dcc3c4b0a3323675ad41e4b200908d4eec23aadb27063d1"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.builtBy=#{tap.user}
+    ]
+
+    system "go", "build", *std_go_args(output: bin/"kubectl-testkube", ldflags: ldflags),
+      "cmd/kubectl-testkube/main.go"
+  end
+
+  test do
+    output = shell_output("#{bin}/kubectl-testkube get tests 2>&1", 1)
+    assert_match("error: invalid configuration: no configuration has been provided", output)
+
+    output = shell_output("#{bin}/kubectl-testkube help")
+    assert_match("Testkube entrypoint for kubectl plugin", output)
+  end
+end
