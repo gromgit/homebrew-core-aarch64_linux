0afcb0f12522eedbe3e7f05cc4fc1126c7a019d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-11 06:21:28 +0000

    argocd: update 2.3.1 bottle.

--- Formula/argocd.rb
+++ Formula/argocd.rb
@@ -7,12 +7,12 @@ class Argocd < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "47b900b32e8861403240a70a90190cf21062c63d1c05cf2c49a54ce6e97b57e1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b16c674cd23534d25825d702a629e6694550a3daa176992d4779962009fb8f78"
-    sha256 cellar: :any_skip_relocation, monterey:       "4d2cea719df7030ebcae8414f05d0fec9271a0405803d30b015a053f3558fa5e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f23b36cf9262652b8f194c0c97b4babd1bb3e5f5b03daa0320abe48ee154ded3"
-    sha256 cellar: :any_skip_relocation, catalina:       "6fedcc077a618d3c3c2758d1a5cff26801e82a0fae4effcd7a2d5a1d066c55b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d9149e7145db37340ccee01922105c17698276795031a0b66a9d45378234b89"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "adc3c5cf3aee31ba633eae6d1684172b27cf1b70d82873fd47f079f3736b5022"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e5f9eadc0930ed618a2911d704628de05721683ad5feca26eb5d6efcdc3b66e"
+    sha256 cellar: :any_skip_relocation, monterey:       "25b58259e9db0d7bd6a5cca7241be184b3be637a6f7e2fedb777a638736d3370"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cfb9ca9cd1028a61c57181beea4d039440a10ea36f29a35ba19974df7c398e3d"
+    sha256 cellar: :any_skip_relocation, catalina:       "245b6de4a9381ccba7e890bc5193df66269a922fe22561844e28d8ce488b33a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b9b48cfa9949468e2b587f73988c589f74be669d8761b1a4a5462444ae7d201"
   end
 
   depends_on "go" => :build
