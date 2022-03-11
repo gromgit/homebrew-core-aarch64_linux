53210f03e3f1c0a767670ea5547a25158ca7d0f8
Caleb Xu <calebcenter@live.com>
2022-03-11 01:36:20 -0500

    gistit: fix build for Linux (#96682)

--- Formula/gistit.rb
+++ Formula/gistit.rb
@@ -19,6 +19,8 @@ class Gistit < Formula
   depends_on "automake" => :build
   depends_on "jansson"
 
+  uses_from_macos "curl"
+
   def install
     mv "configure.in", "configure.ac" # silence warning
     system "./autogen.sh", "--disable-dependency-tracking",
@@ -28,6 +30,9 @@ class Gistit < Formula
   end
 
   test do
-    system "#{bin}/gistit", "-v"
+    (testpath/"test.txt").write "Hello"
+
+    # Gist creation should fail due to lack of authentication token
+    assert_match "- code 401", shell_output("#{bin}/gistit -priv test.txt", 1)
   end
 end
