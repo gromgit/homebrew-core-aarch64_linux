3e5c5cc691623d972bf4c5519bf0161f448b734e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-11 06:20:59 +0000

    talisman: update 1.26.0 bottle.

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -13,12 +13,12 @@ class Talisman < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea4952bced60c28f401c03f7cd47d7f9c58f719ee5c4e96776602533bee1a8f7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3d80848a9639a4fa368c126770c8f9f4f2db24d6f0a968acfeef2f5e5a2ef515"
-    sha256 cellar: :any_skip_relocation, monterey:       "bfafb27238557da2537a13380b9a80004e190ca4bb840443768456669978621c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f786a38009d08c0296cf1608bfa556b03ca2790d8d8c8399039a4e88dbbe33d5"
-    sha256 cellar: :any_skip_relocation, catalina:       "dccd44ab7e17e7f82841825f21f49411ce6e60f04329583031e286fe2b09486f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c45166f1f677019a8f7b167fb7b6bcf90739f34c63143abee2abb07ad58b2fb1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "26143f107f651cba6f724fe73a8b6bf8a6d727a1e6d836977537e4a05b2afe5e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7eaba421e70acdd7b321fab608de434954c00a58b6a25b2774705efbbe5feac"
+    sha256 cellar: :any_skip_relocation, monterey:       "02f61d6766dd252a33352f007699cdad4be741167274b40ec26ab88d6ce5c07e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2feff7680aba6ddcd044d633d95aaa356bda7ccc0719f37eaebf4bf715ac4604"
+    sha256 cellar: :any_skip_relocation, catalina:       "87a3a909d25a2169614f2f74c40da2afbf1f39924e5f346fb0f85e7256e0ae79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1fd2f37b7d3e6d51b984e767981472ed41deafe8e1776bdcc15268523937e68f"
   end
 
   depends_on "go" => :build
