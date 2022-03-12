55320dfc86a5cc03b168f782abea6f36c8786678
Michael Cho <cho-m@tuta.io>
2022-03-11 22:14:31 -0800

    mongrel2 1.13.0
    Closes #96742.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mongrel2.rb
+++ Formula/mongrel2.rb
@@ -1,19 +1,10 @@
 class Mongrel2 < Formula
   desc "Application, language, and network architecture agnostic web server"
   homepage "https://mongrel2.org/"
+  url "https://github.com/mongrel2/mongrel2/releases/download/v1.13.0/mongrel2-v1.13.0.tar.bz2"
+  sha256 "b6f1f50c9f65b605342d8792b1cc8a1c151105339030313b9825b6a68d400c10"
   license "BSD-3-Clause"
   head "https://github.com/mongrel2/mongrel2.git", branch: "develop"
-
-  stable do
-    url "https://github.com/mongrel2/mongrel2/releases/download/v1.11.0/mongrel2-v1.11.0.tar.bz2"
-    sha256 "917f2ce07c0908cae63ac03f3039815839355d46568581902377ba7e41257bed"
-
-    # ensure unit tests work on 1.11.0. remove after next release
-    patch do
-      url "https://github.com/mongrel2/mongrel2/commit/7cb8532e2ecc341d77885764b372a363fbc72eff.patch?full_index=1"
-      sha256 "fa7be14bf1df8ec3ab8ae164bde8eb703e9e2665645aa627baae2f08c072db9a"
-    end
-  end
 
   livecheck do
     url :stable
@@ -33,6 +24,13 @@ class Mongrel2 < Formula
 
   uses_from_macos "sqlite"
 
+  # Fix src/server.c:185:23: error: #elif with no expression
+  # PR ref: https://github.com/mongrel2/mongrel2/pull/358
+  patch do
+    url "https://github.com/mongrel2/mongrel2/commit/d6c38361cb31a3de8ddfc3e8a3971330a40eb241.patch?full_index=1"
+    sha256 "52afa21830d5e3992136c113c5a54ad55cccc07f763ab7532f7ba122140b3e6b"
+  end
+
   def install
     # Build in serial. See:
     # https://github.com/Homebrew/homebrew/issues/8719
@@ -40,10 +38,16 @@ class Mongrel2 < Formula
 
     # Mongrel2 pulls from these ENV vars instead
     ENV["OPTFLAGS"] = "#{ENV.cflags} #{ENV.cppflags}"
-    ENV["OPTLIBS"] = "#{ENV.ldflags} -undefined dynamic_lookup" if OS.mac?
+    ENV["OPTLIBS"] = ENV.ldflags
+    if OS.mac?
+      ENV.append "OPTFLAGS", "-DHAS_ARC4RANDOM"
+      ENV.append "OPTLIBS", "-undefined dynamic_lookup"
+    end
 
-    system "make", "all"
-    system "make", "install", "PREFIX=#{prefix}"
+    # The Makefile now uses system mbedtls, but `make` fails during filter_tests.
+    # As workaround, use previous localmbedtls.mak that builds with bundled mbedtls.
+    # Issue ref: https://github.com/mongrel2/mongrel2/issues/342
+    system "make", "-f", "localmbedtls.mak", "install", "PREFIX=#{prefix}"
   end
 
   test do
