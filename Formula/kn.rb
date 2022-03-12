b842cac29d89d2745c4ecec9410f481401f053e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-12 10:05:16 +0000

    kn: update 1.3.1 bottle.

--- Formula/kn.rb
+++ Formula/kn.rb
@@ -8,12 +8,12 @@ class Kn < Formula
   head "https://github.com/knative/client.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b8143eec4ebac5055ab52bc88646e088ffb49e3ad406eeac8994521807e3c1c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4b8143eec4ebac5055ab52bc88646e088ffb49e3ad406eeac8994521807e3c1c"
-    sha256 cellar: :any_skip_relocation, monterey:       "cd57a939d6ade2a3d5142e7e73be56e4c36339731112408a2b8847e21da78f5b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cd57a939d6ade2a3d5142e7e73be56e4c36339731112408a2b8847e21da78f5b"
-    sha256 cellar: :any_skip_relocation, catalina:       "cd57a939d6ade2a3d5142e7e73be56e4c36339731112408a2b8847e21da78f5b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c1b9cd24d2e3d916f4660413d5b6710d151329df6528b634a30b1c422f802485"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f536caa54ff5bf9197f358f18003ad44ea7a730fcd0f197d64c0a714d9f689fb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f536caa54ff5bf9197f358f18003ad44ea7a730fcd0f197d64c0a714d9f689fb"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ed4e11b2419f941e4aaa3cf5fd8e23b6b36b80d871b6df1e9ccceff43cc38c5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5ed4e11b2419f941e4aaa3cf5fd8e23b6b36b80d871b6df1e9ccceff43cc38c5"
+    sha256 cellar: :any_skip_relocation, catalina:       "5ed4e11b2419f941e4aaa3cf5fd8e23b6b36b80d871b6df1e9ccceff43cc38c5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "09c1dc5c1de3e67551ad5d462411e4d793f9e78c3159b79d746609f1bab103d1"
   end
 
   depends_on "go" => :build
