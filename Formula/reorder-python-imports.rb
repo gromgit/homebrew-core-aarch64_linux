0192df1b39000963f2ed73660f6448bbb9f29bcb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-14 03:05:57 +0000

    reorder-python-imports: update 3.0.1 bottle.

--- Formula/reorder-python-imports.rb
+++ Formula/reorder-python-imports.rb
@@ -9,12 +9,12 @@ class ReorderPythonImports < Formula
   head "https://github.com/asottile/reorder_python_imports.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "aecaa7da5f3a020decbec59f1e8d0564ae90884c903976de0f778f55d7f3ba7e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aecaa7da5f3a020decbec59f1e8d0564ae90884c903976de0f778f55d7f3ba7e"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d4968105151ffb53ca89e47413613a42a5bc240cc10aac5e6d40e67f6f660ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0d4968105151ffb53ca89e47413613a42a5bc240cc10aac5e6d40e67f6f660ac"
-    sha256 cellar: :any_skip_relocation, catalina:       "0d4968105151ffb53ca89e47413613a42a5bc240cc10aac5e6d40e67f6f660ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f567420cc1dc170f016bb447604cf1e1f69afda321ab7b054ae2cbd855788d0d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc7dcebec417cef0f7c31c1e2aad83e16281c5bc91556e0aa9b188a0a3799dd3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc7dcebec417cef0f7c31c1e2aad83e16281c5bc91556e0aa9b188a0a3799dd3"
+    sha256 cellar: :any_skip_relocation, monterey:       "b803c194964b602a1c2851f3906696b8686aab4b7c504aef78ea5fb064da1d6a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b803c194964b602a1c2851f3906696b8686aab4b7c504aef78ea5fb064da1d6a"
+    sha256 cellar: :any_skip_relocation, catalina:       "b803c194964b602a1c2851f3906696b8686aab4b7c504aef78ea5fb064da1d6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a15b57300b47e60e8a804f252055adc9a4ef6dc549660b3928cd338200ac717e"
   end
 
   depends_on "python@3.10"
