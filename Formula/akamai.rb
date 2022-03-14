a1abe41744a92ea0a666dae2b7a8bda6dbfe3df5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-14 11:08:58 +0000

    akamai: update 1.4.0 bottle.

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -6,10 +6,10 @@ class Akamai < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "9cc4acc54d8c5fbc15161c248d5bd04adcc09b10652c2617b27a84bab09ec436"
-    sha256 cellar: :any_skip_relocation, big_sur:      "f9fd6cd269dd353fdfb0bcf860a1d7beabf5a63e1c273f7e7b71fdfccfdbd1df"
-    sha256 cellar: :any_skip_relocation, catalina:     "413bde847fd271e49cbdddf8c5743279dd80ac867e91253674f3ea0ee81089ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5d4f64f123d54ef676aafac3d51c4fbfe0c063d65d80aca3a340147613647fa4"
+    sha256 cellar: :any_skip_relocation, monterey:     "d9c38e6056460422cebccc1f0d05556e39fd2b9fcc6760eb12f06554c18c6c4a"
+    sha256 cellar: :any_skip_relocation, big_sur:      "5f41b6da29dafbb90a707d65e3223b7fa00ebec63ec650e7883dcb0ec145c965"
+    sha256 cellar: :any_skip_relocation, catalina:     "f60c0e63cfd19a9a72895657f8d97ce72542b88b6c1ff1a103c8166bf52e17dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8daead23033f6a276a0dfc8db04a3946521a9501210422580f4d8b18093e9121"
   end
 
   depends_on "go" => :build
