b8fcda32cf47516478493daa82c564beca074dd8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-03-14 07:17:43 +0000

    kamel: update 1.8.2 bottle.

--- Formula/kamel.rb
+++ Formula/kamel.rb
@@ -13,10 +13,12 @@ class Kamel < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d2ef5195a2aa1ed8b0997e5115cbc5549840af60f3073d05f5fc6abdf61fc374"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d7fa06f538c67fdef00d3b75f5c9dd5a8ad2825895e2a5d272c41a434e2ac081"
-    sha256 cellar: :any_skip_relocation, catalina:      "319129766d0e75ca1961b49aaf57be6d029d31dbc29eb134f44b348b30359dae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6818471e7e687205a370d4c3ce7a4e3c55be4a86041c9d32fda20f5d2faf489"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a44a42d1828d68e38c603f9150b7ed14a8e5726d5d3d260803e92f1b42ac7363"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d2ef5195a2aa1ed8b0997e5115cbc5549840af60f3073d05f5fc6abdf61fc374"
+    sha256 cellar: :any_skip_relocation, monterey:       "8b726717247ca42b12e8fa9564106fea8dce5ac3eeb4d189709f16c24da5a087"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d7fa06f538c67fdef00d3b75f5c9dd5a8ad2825895e2a5d272c41a434e2ac081"
+    sha256 cellar: :any_skip_relocation, catalina:       "319129766d0e75ca1961b49aaf57be6d029d31dbc29eb134f44b348b30359dae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c6818471e7e687205a370d4c3ce7a4e3c55be4a86041c9d32fda20f5d2faf489"
   end
 
   depends_on "go" => :build
