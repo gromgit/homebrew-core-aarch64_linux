633280d6a2068e76d50fefa374abd97553d45ddd
László Várady <laszlo.varady@protonmail.com>
2022-03-14 12:53:37 +0100

    criterion 2.4.0 (new formula)
    Closes #96826.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/criterion.rb
@@ -0,0 +1,36 @@
+class Criterion < Formula
+  desc "Cross-platform C and C++ unit testing framework for the 21st century"
+  homepage "https://github.com/Snaipe/Criterion"
+  url "https://github.com/Snaipe/Criterion/releases/download/v2.4.0/criterion-2.4.0.tar.xz"
+  sha256 "b13bdb9e007d4d2e87a13446210630e95e3e3d92bb731951bcea4993464b9911"
+  license "MIT"
+  head "https://github.com/Snaipe/Criterion.git", branch: "bleeding"
+
+  depends_on "cmake" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libgit2"
+  depends_on "nanomsg"
+  uses_from_macos "libffi"
+
+  def install
+    system "meson", "setup", *std_meson_args, "--force-fallback-for=boxfort", "build"
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "--skip-subprojects", "-C", "build"
+  end
+
+  test do
+    (testpath/"test-criterion.c").write <<~EOS
+      #include <criterion/criterion.h>
+
+      Test(suite_name, test_name)
+      {
+        cr_assert(1);
+      }
+    EOS
+
+    system ENV.cc, "test-criterion.c", "-I#{include}", "-L#{lib}", "-lcriterion", "-o", "test-criterion"
+    system "./test-criterion"
+  end
+end
