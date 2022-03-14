77bf49dd6f5c222365840ccff09955a42a96b503
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-03-14 08:00:10 +0000

    fortran-language-server: add 1.12.0 bottle.

--- Formula/fortran-language-server.rb
+++ Formula/fortran-language-server.rb
@@ -8,6 +8,15 @@ class FortranLanguageServer < Formula
   license "MIT"
   head "https://github.com/hansec/fortran-language-server.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b3ef36cf1efbde6f4777037f112161c717629f8be4a58b2208fe82861c58323"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b3ef36cf1efbde6f4777037f112161c717629f8be4a58b2208fe82861c58323"
+    sha256 cellar: :any_skip_relocation, monterey:       "32839345a2cdc7894b65d7b6b6a06d439db922a7708973a4fd3e4b3213f6a899"
+    sha256 cellar: :any_skip_relocation, big_sur:        "32839345a2cdc7894b65d7b6b6a06d439db922a7708973a4fd3e4b3213f6a899"
+    sha256 cellar: :any_skip_relocation, catalina:       "32839345a2cdc7894b65d7b6b6a06d439db922a7708973a4fd3e4b3213f6a899"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ffd2580a9385f37d0169c8d2e455a413cb9f26c720c269656c4ced9c14bf1365"
+  end
+
   depends_on "python@3.10"
 
   def install
