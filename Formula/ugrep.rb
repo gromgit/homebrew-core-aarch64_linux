6df1b1e15eb156efe8aaa5766765d1df73c79832
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-14 02:26:38 +0000

    ugrep: update 3.7.5 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -6,12 +6,12 @@ class Ugrep < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_monterey: "6c1fb75c2fcf54e5963ba5201300eb209b1d4a370b363ac7808032debe70f23e"
-    sha256 arm64_big_sur:  "7d05e75ca0b9158dbfa297fc1098dd4b7dd42af5b212921b6dfc2dc10b9edfa8"
-    sha256 monterey:       "73160369cba389ed4b0fb47938b24c7bf4163cd722663c2ebc706a5c58621022"
-    sha256 big_sur:        "1a204d2054a1c1cacc526416ac3df074ac2bb392d14bfa820eceff3707df7212"
-    sha256 catalina:       "80a95e118d1410411e14a5b5704f6f392aa8b22090389ab0d0e3ebcd31b6c112"
-    sha256 x86_64_linux:   "90b1d616d28f8ee81c4a09e65c295cfc796d453326c99835def762377809bcf7"
+    sha256 arm64_monterey: "9097adc31efa43c5e6bca065ac9a4e79c83a4b6a7df75080d8c9777bcb70df99"
+    sha256 arm64_big_sur:  "b5247f7c00af6dd62e27ad925d84893b38f2b0d4aa399ad49ce5a9533e32db07"
+    sha256 monterey:       "aed9d8eb59d5bd7c78426e074b2d73034fb5d34255c4465138724e76efad231a"
+    sha256 big_sur:        "4e68707013358af6031fdf5b32ddd9de4efdd1fa629657eab87eb0eff9810938"
+    sha256 catalina:       "87363e3c5838f8889ec93ec4d1fd3cd5c2c5c5631e8df64d3bd4db684f336513"
+    sha256 x86_64_linux:   "b05e08280d242fdfae3e55ca47cb47ab9b2133971d0f9c367851ceeedcf6931f"
   end
 
   depends_on "pcre2"
