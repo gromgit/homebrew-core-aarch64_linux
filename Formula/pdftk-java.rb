a2ef12f319e371d689d26368ed5c8d9aa4668353
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-03-14 06:49:08 +0000

    pdftk-java: update 3.3.2 bottle.

--- Formula/pdftk-java.rb
+++ Formula/pdftk-java.rb
@@ -12,10 +12,12 @@ class PdftkJava < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "358a5e0e9594cb02df4275c16f8775ecded3bf2823a15564372491e407ae80fd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "91eed73d4a0f66b24a36d70c79901fa1691546580c47d6dbbde24d3d18ac882a"
-    sha256 cellar: :any_skip_relocation, catalina:      "4d5f5f28dcab3b1db472272038c9613031c73b528ac9b4ac27fca2a6c678f4b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "67921a471c816007935efe800e90321318ff1c6f78d5b031ed84b866490c89f5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "48ed05a44116056bfa719f0c05b8ad15fed3d149c6d38a95040a3c0494c93028"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "358a5e0e9594cb02df4275c16f8775ecded3bf2823a15564372491e407ae80fd"
+    sha256 cellar: :any_skip_relocation, monterey:       "26e14850fc1c1e24ae7d52492d8cb07e0dd5273b246edc329d84b915da3b9ca0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "91eed73d4a0f66b24a36d70c79901fa1691546580c47d6dbbde24d3d18ac882a"
+    sha256 cellar: :any_skip_relocation, catalina:       "4d5f5f28dcab3b1db472272038c9613031c73b528ac9b4ac27fca2a6c678f4b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67921a471c816007935efe800e90321318ff1c6f78d5b031ed84b866490c89f5"
   end
 
   depends_on "gradle" => :build
