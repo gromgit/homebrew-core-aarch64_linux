7f1b309124682fe301e6633d9f1732957d3f5d1d
rui <rui@chenrui.dev>
2022-03-14 02:43:52 +0000

    bvm: add 0.4.2 bottle.

--- Formula/bvm.rb
+++ Formula/bvm.rb
@@ -6,6 +6,15 @@ class Bvm < Formula
   license "MIT"
   head "https://github.com/bvm/bvm.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a284f10dc418073f3f9920acd946b6d494e9d81c6c9628532d352546ff47c683"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e05342a9aaa52b60891218158811a9c6848a4f7e37919e9029e022db54226490"
+    sha256 cellar: :any_skip_relocation, monterey:       "03fa1710f7665d0910ee7a501572de68fcd5977b98ed265d456674e7aedf5d1d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0de6d3a5c96bd9131e5d075a44d727afa4ccdf7ad7f91cbdef65edf7f42a2149"
+    sha256 cellar: :any_skip_relocation, catalina:       "03feaea85d097ba5b2e6248f001b49859f808b263d0184023c03946bc0e9b198"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9dc4ab8b9c8addd472add6233b07154a7b10886accfc76854637c980bdc28c4d"
+  end
+
   depends_on "rust" => :build
 
   def install
