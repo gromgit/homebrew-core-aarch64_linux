50d05e51f1e33ba43f601fe2940a34dc05f19802
Rui Chen <rui@chenrui.dev>
2022-03-13 22:41:03 -0400

    trivy 0.24.2 (new formula)
    * trivy 0.24.2 (new formula)
      remove dup line
    
      trivy: update test
    * trivy: fix typo
    
    Closes #96803.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/trivy.rb
@@ -0,0 +1,21 @@
+class Trivy < Formula
+  desc "Vulnerability scanner for container images, file systems, and Git repos"
+  homepage "https://aquasecurity.github.io/trivy/"
+  url "https://github.com/aquasecurity/trivy/archive/v0.24.2.tar.gz"
+  sha256 "ddbffec10e32de2dfbef14169028d60df7504ae255ac674febbafdcf61c7906b"
+  license "Apache-2.0"
+  head "https://github.com/aquasecurity/trivy.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X=main.version=#{version}"), "./cmd/trivy"
+  end
+
+  test do
+    output = shell_output("#{bin}/trivy image alpine:3.10")
+    assert_match(/\(UNKNOWN: \d+, LOW: \d+, MEDIUM: \d+, HIGH: \d+, CRITICAL: \d+\)/, output)
+
+    assert_match version.to_s, shell_output("#{bin}/trivy --version")
+  end
+end
