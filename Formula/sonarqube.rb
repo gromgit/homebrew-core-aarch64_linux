204e799a6cbd03c1ae5e833b3c45de99704e68aa
Michael Cho <cho-m@tuta.io>
2022-03-14 19:40:41 -0700

    sonarqube: revision
    * sonarqube: symlink java service wrapper and set defaults
    * sonarqube: resolve last task
    
    Closes #97025.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sonarqube.rb
+++ Formula/sonarqube.rb
@@ -4,6 +4,7 @@ class Sonarqube < Formula
   url "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip"
   sha256 "df8d97ae93002593fcaceacfe4dfdab68982a8dc52002c441ee6218f4d71961c"
   license "LGPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url "https://www.sonarqube.org/success-download-community-edition/"
@@ -25,34 +26,62 @@ class Sonarqube < Formula
   conflicts_with "sonarqube-lts", because: "both install the same binaries"
 
   def install
-    # Use Java Service Wrapper 3.5.46 which is Apple Silicon compatible
-    # Java Service Wrapper doesn't support the  wrapper binary to be symlinked, so it's copied
-    jsw_libexec = Formula["java-service-wrapper"].opt_libexec
-    ln_s jsw_libexec/"lib/wrapper.jar", "#{buildpath}/lib/jsw/wrapper-3.5.46.jar"
-    ln_s jsw_libexec/"lib/libwrapper.dylib", "#{buildpath}/bin/macosx-universal-64/lib/"
-    cp jsw_libexec/"bin/wrapper", "#{buildpath}/bin/macosx-universal-64/"
-    cp jsw_libexec/"scripts/App.sh.in", "#{buildpath}/bin/macosx-universal-64/sonar.sh"
-    sonar_sh_file = "bin/macosx-universal-64/sonar.sh"
-    inreplace sonar_sh_file, "@app.name@", "SonarQube"
-    inreplace sonar_sh_file, "@app.long.name@", "SonarQube"
-    inreplace sonar_sh_file, "../conf/wrapper.conf", "../../conf/wrapper.conf"
-    inreplace "conf/wrapper.conf", "wrapper-3.2.3.jar", "wrapper-3.5.46.jar"
-    rm "lib/jsw/wrapper-3.2.3.jar"
-    rm "bin/macosx-universal-64/lib/libwrapper.jnilib"
+    # Delete older packaged java-service-wrapper
+    wrapper_version = "3.2.3"
+    buildpath.glob("bin/*-64").map(&:rmtree)
+    (buildpath/"lib/jsw/wrapper-#{wrapper_version}.jar").unlink
 
-    # Delete native bin directories for other systems
-    remove, keep = if OS.mac?
-      ["linux", "macosx-universal"]
-    else
-      ["macosx", "linux-x86"]
+    platform = OS.mac? ? "macosx-universal-64" : "linux-x86-64"
+    platform_bin = buildpath/"bin"/platform
+    (platform_bin/"lib").mkpath
+
+    # Link newer java-service-wrapper formula which is Apple Silicon compatible
+    jsw_libexec = Formula["java-service-wrapper"].opt_libexec
+    ln_s jsw_libexec/"lib/wrapper.jar", buildpath/"lib/jsw/wrapper.jar"
+    ln_s jsw_libexec/"lib"/shared_library("libwrapper"), platform_bin/"lib/"
+    ln_s jsw_libexec/"bin/wrapper", platform_bin/"wrapper"
+
+    # Create new sonar.sh script and update conf files
+    cp jsw_libexec/"scripts/App.sh.in", platform_bin/"sonar.sh"
+    chmod 0755, platform_bin/"sonar.sh"
+    inreplace platform_bin/"sonar.sh" do |s|
+      s.gsub! "@app.name@", "SonarQube"
+      s.gsub! "@app.long.name@", "SonarQube"
+      s.gsub! "../conf/wrapper.conf", "../../conf/wrapper.conf"
+      # Write PID file outside of installation directory
+      s.sub!(/^PIDDIR="\."$/, "PIDDIR=#{var}/run")
+    end
+    inreplace "conf/wrapper.conf" do |s|
+      s.gsub! "wrapper-#{wrapper_version}.jar", "wrapper.jar"
+      # Set wrapper.working.dir to allow using symlinked wrapper
+      s.sub!(/^wrapper\.java\.command=.*/, "\\0\nwrapper.working.dir=#{libexec}/bin/#{platform}")
+      # Write log files outside of installation directory
+      s.sub! %r{^wrapper\.logfile=.*/([^/\s]+\.log)$}, "wrapper.logfile=#{var}/sonarqube/logs/\\1"
+    end
+    inreplace "conf/sonar.properties" do |s|
+      # Write log/data/temp files outside of installation directory
+      s.sub!(/^#sonar\.path\.data=.*/, "sonar.path.data=#{var}/sonarqube/data")
+      s.sub!(/^#sonar\.path\.logs=.*/, "sonar.path.logs=#{var}/sonarqube/logs")
+      s.sub!(/^#sonar\.path\.temp=.*/, "sonar.path.temp=#{var}/sonarqube/temp")
     end
 
-    rm_rf Dir["bin/{#{remove},windows}-*"]
-
     libexec.install Dir["*"]
+    env = Language::Java.overridable_java_home_env("11")
+    env["PATH"] = "$JAVA_HOME/bin:$PATH"
+    (bin/"sonar").write_env_script libexec/"bin"/platform/"sonar.sh", env
+  end
 
-    (bin/"sonar").write_env_script libexec/"bin/#{keep}-64/sonar.sh",
-      Language::Java.overridable_java_home_env("11")
+  def post_install
+    (var/"run").mkpath
+    (var/"sonarqube/logs").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      Data: #{var}/sonarqube/data
+      Logs: #{var}/sonarqube/logs
+      Temp: #{var}/sonarqube/temp
+    EOS
   end
 
   service do
@@ -61,6 +90,24 @@ class Sonarqube < Formula
   end
 
   test do
-    assert_match "SonarQube", shell_output("#{bin}/sonar status", 1)
+    port = free_port
+    ENV["SONAR_WEB_PORT"] = port.to_s
+    ENV["SONAR_EMBEDDEDDATABASE_PORT"] = free_port.to_s
+    ENV["SONAR_SEARCH_PORT"] = free_port.to_s
+    ENV["SONAR_PATH_DATA"] = testpath/"data"
+    ENV["SONAR_PATH_LOGS"] = testpath/"logs"
+    ENV["SONAR_PATH_TEMP"] = testpath/"temp"
+    ENV["SONAR_TELEMETRY_ENABLE"] = "false"
+
+    assert_match(/SonarQube.* is not running/, shell_output("#{bin}/sonar status", 1))
+    pid = fork { exec bin/"sonar", "console" }
+    begin
+      sleep 15
+      output = shell_output("#{bin}/sonar status")
+      assert_match(/SonarQube.* is running:.* Wrapper:STARTED, Java:STARTED/, output)
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
   end
 end
