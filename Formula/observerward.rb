ec2d9de1bd82adee28e6cda780bf0d5270c04e2b
Rvn0xsy <rvn0xsy@gmail.com>
2022-03-15 12:59:16 +0800

    observerward 2022.3.15 (new formula)
    * add observerward
    * update test
    * update test
    * update suggestion
    
    Closes #96872.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/observerward.rb
@@ -0,0 +1,18 @@
+class Observerward < Formula
+  desc "Cross platform community web fingerprint identification tool"
+  homepage "https://0x727.github.io/ObserverWard/"
+  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.3.15.tar.gz"
+  sha256 "e1420fe744edbafaaf267fb85e3e016c3cca454117e1ffd360d6d25debf2f560"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"observer_ward", "-u"
+    assert_match "swagger", shell_output("#{bin}/observer_ward -t https://httpbin.org")
+  end
+end
