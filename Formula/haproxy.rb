6632c30acc7ddd9e361c3b00ecd76b3b7fdbc64a
Porkepix <porkepix@gmail.com>
2022-03-15 11:36:57 +0100

    haproxy 2.5.5
    Closes #96899.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.4.tar.gz"
-  sha256 "dc4015d85c7fef811b459803b763001d809b07a9251dc1864fedb9a07b44aefb"
+  url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.5.tar.gz"
+  sha256 "063c4845cdb2d76f292ef44d9c0117a853d8d10ae5d9615b406b14a4d74fe4b9"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   livecheck do
