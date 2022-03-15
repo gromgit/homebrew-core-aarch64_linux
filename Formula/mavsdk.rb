250ab876b4a6cf9f1ecbf7439f858eef8e2a5fe7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-15 05:27:19 +0000

    mavsdk 1.1.0
    Closes #96882.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -4,10 +4,9 @@ class Mavsdk < Formula
   desc "API and library for MAVLink compatible systems written in C++17"
   homepage "https://mavsdk.mavlink.io"
   url "https://github.com/mavlink/MAVSDK.git",
-      tag:      "v1.0.8",
-      revision: "a6836be251e216f9967183a7551cc6ccb2a60382"
+      tag:      "v1.1.0",
+      revision: "2e89c9d9e8584b93d2ef3ea80b6ef4a6d4bffd9c"
   license "BSD-3-Clause"
-  revision 1
 
   livecheck do
     url :stable
