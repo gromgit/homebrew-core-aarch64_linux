9fd6796ed035855dc45d10c294bd1fe2404c97d2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-15 10:32:13 +0000

    observerward: add 2022.3.15 bottle.

--- Formula/observerward.rb
+++ Formula/observerward.rb
@@ -5,6 +5,15 @@ class Observerward < Formula
   sha256 "e1420fe744edbafaaf267fb85e3e016c3cca454117e1ffd360d6d25debf2f560"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9381048907e393eae9986454f6aae39a43faabfe09baf990da2d8944bee2544b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "101b7ea33ac9e54822aacf7bb1cfe74d12c4b74b4741dc1e6f51ad69e3de1cef"
+    sha256 cellar: :any_skip_relocation, monterey:       "d1dac0cdb8f62edbf478a0898d33a7a3bb34a771d7eaa8528d4bebee0e6cda9e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e0a0aab542985840f8c4c07a4325952a7030c640b71f689c71f84efdc1af1b28"
+    sha256 cellar: :any_skip_relocation, catalina:       "1475ff445d397e2453f10036aebc90cab3511b37c521725da21dde4fb23f5ad2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "995d5cdb3aa4e46d3759ce63eaa2595cab97634a04b577cb333409676604c80e"
+  end
+
   depends_on "rust" => :build
 
   def install
