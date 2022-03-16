70750edb6a4f99e41e215dedbac486915ad6802f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-16 05:14:22 +0000

    flarectl 0.35.1
    Closes #96967.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,8 +1,8 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.34.0.tar.gz"
-  sha256 "4ec0a0f1d27086a5c62fcb4ae8c4d914ae2904ef71e8269aaf0ebd35b498988a"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.35.1.tar.gz"
+  sha256 "8d37113e9faf1ba63666fd6cb33b14a66a96eb0f0f354f9f15faac8c94942305"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
