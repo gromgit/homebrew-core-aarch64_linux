eaa6470f5fec668517065f3fbf40f5f258f1f28d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-16 12:10:36 +0000

    prettier: update 2.6.0 bottle.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -9,12 +9,7 @@ class Prettier < Formula
   head "https://github.com/prettier/prettier.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3b226eeb3f448795140ca2339340b2adc0d67b84b571c0b372642d9539ec0189"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b226eeb3f448795140ca2339340b2adc0d67b84b571c0b372642d9539ec0189"
-    sha256 cellar: :any_skip_relocation, monterey:       "c1250d366af6a799659dee93c22beeca6f8526d98a1903a03429cd20bb601206"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c1250d366af6a799659dee93c22beeca6f8526d98a1903a03429cd20bb601206"
-    sha256 cellar: :any_skip_relocation, catalina:       "c1250d366af6a799659dee93c22beeca6f8526d98a1903a03429cd20bb601206"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b226eeb3f448795140ca2339340b2adc0d67b84b571c0b372642d9539ec0189"
+    sha256 cellar: :any_skip_relocation, all: "0b7d1ece21b6f5489544fb81305b66cc96ca075a9cd09f7252b6cc4417b87809"
   end
 
   depends_on "node"
