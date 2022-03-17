248f80b54041a84563c2833518e4e0166201836d
Bevan Kay <email@bevankay.me>
2022-03-17 14:00:44 +1100

    firebase-cli 10.3.1
    Closes #97055.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-10.3.0.tgz"
-  sha256 "882ec6ebc1d0f3dc632dd2ba6fd68cb66ba00b96bf4be95d7230f4ce68202d2b"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-10.3.1.tgz"
+  sha256 "bd68926378a4a82370656b64caa834c5abf8b93038fea6a8a6c622d38370b8c3"
   license "MIT"
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
