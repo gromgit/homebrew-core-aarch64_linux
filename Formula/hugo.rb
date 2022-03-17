7fc0e4d763d46b8b268049cb0595ced905e39d41
Rui Chen <rui@chenrui.dev>
2022-03-16 21:54:24 -0400

    hugo 0.95.0
    Closes #97032.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,8 +1,8 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.94.2.tar.gz"
-  sha256 "315bc0d22977e84ba25125b1d23333648e36194a46a4d6ae4f4c6c683dc8979c"
+  url "https://github.com/gohugoio/hugo/archive/v0.95.0.tar.gz"
+  sha256 "a3d80e3614c8755b664469b3dd5e4503efdad62b10b78653bfbf25363ce09b5a"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git", branch: "master"
 
