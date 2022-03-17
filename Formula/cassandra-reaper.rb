8003f239276294befcfdb32e0bbe12cd6bfb268f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-17 10:10:20 +0000

    cassandra-reaper: update 3.1.1_1 bottle.

--- Formula/cassandra-reaper.rb
+++ Formula/cassandra-reaper.rb
@@ -7,10 +7,7 @@ class CassandraReaper < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "ff2caf27f8ba961fff6b24980c4fb9b8d7d19b28bc7b13a71a60999e0e1ee3db"
-    sha256 cellar: :any_skip_relocation, big_sur:      "ff2caf27f8ba961fff6b24980c4fb9b8d7d19b28bc7b13a71a60999e0e1ee3db"
-    sha256 cellar: :any_skip_relocation, catalina:     "ff2caf27f8ba961fff6b24980c4fb9b8d7d19b28bc7b13a71a60999e0e1ee3db"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5ef86195c0f8f1b9ef8620e9d4abdf3cfaee2b5b6a68503252bb78d8f0375b6c"
+    sha256 cellar: :any_skip_relocation, all: "808c52e9c2375318ba1d94a6bcfdfa7f5c51d37b0e05d0e065ca74cb46f8379e"
   end
 
   depends_on "openjdk@11"
