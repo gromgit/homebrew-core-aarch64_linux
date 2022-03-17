a194114cab0cc2a3eed1632f601b5f530cbce9a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-17 23:04:45 +0000

    meilisearch: update 0.26.1 bottle.

--- Formula/meilisearch.rb
+++ Formula/meilisearch.rb
@@ -11,12 +11,12 @@ class Meilisearch < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "69b5020b68bc66643c3e2d5cd80d494253eef6d0a16d457d8c95b1e904c1e667"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "acb24a0326ce20d6d4922132e24572714d853f8e3346efe16a7a0e296683f141"
-    sha256 cellar: :any_skip_relocation, monterey:       "98b7b7d6ce88e1ad59e9d2ae6293872d9c5599ffd145724a0d0449cf62c941c1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "116bcb7d92edc62ef4dac7fbf415ad7a4e2931bc5f87ecd96cb6ca7d6a409321"
-    sha256 cellar: :any_skip_relocation, catalina:       "4723a67f3e7a5d6ccfd7aff898f6a8a1d765688eeffb19e30d5222ad54773074"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "615aff1d2d2e4fd9c77f022abce086773e8b60c9e5abff0962cdafa23e55b00b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "265c6ee0ae6ade20d94587a5274dc2646b9aa465d0479dd3295a728c9bc8e2da"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d1b4fd908da51bc3a927a527697b7a52ccc683a00bcdf54995aa30b8b271bde"
+    sha256 cellar: :any_skip_relocation, monterey:       "9662788067ceb52acc00e8438f35272bcc5085d903a1afbfedaafa5c0f41c403"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8445b52b2d2ace8ad7d2290684945e71f3bed7571fa17e07b1d925b78f8385da"
+    sha256 cellar: :any_skip_relocation, catalina:       "8eba0bb2b788e1df1a4fba3adc471ba28f83e7ff2bccc965d597e3e6dbe6c3b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "60d58c854bb3e7ad0e1a44cabdb18666ff3849c277d539b77df4540dd57ccb05"
   end
 
   depends_on "rust" => :build
