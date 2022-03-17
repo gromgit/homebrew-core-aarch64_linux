15c8138915105cf886116d8fc8eda6b70309e48e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-17 05:33:57 +0000

    solana 1.9.13
    Closes #97071.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/solana.rb
+++ Formula/solana.rb
@@ -1,8 +1,8 @@
 class Solana < Formula
   desc "Web-Scale Blockchain for decentralized apps and marketplaces"
   homepage "https://solana.com"
-  url "https://github.com/solana-labs/solana/archive/v1.9.12.tar.gz"
-  sha256 "93a8705090eb329e5ba2ea1416da84f48ce667de379b0af13e85a7e14ed1c18a"
+  url "https://github.com/solana-labs/solana/archive/v1.9.13.tar.gz"
+  sha256 "65f6a162a8443067bcf1c7abc51ebe2ac579b8af090c677c8077a0636cf44354"
   license "Apache-2.0"
 
   livecheck do
