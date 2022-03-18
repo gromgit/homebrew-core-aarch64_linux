4d901f363a83fde31e700f2d1de71b72149bc60b
Caleb Xu <calebcenter@live.com>
2022-03-18 02:13:37 -0400

    liboil: fix build from source
    Closes #97181.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/liboil.rb
+++ Formula/liboil.rb
@@ -23,6 +23,7 @@ class Liboil < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "gtk-doc" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
