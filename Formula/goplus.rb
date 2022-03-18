3b92587fa509c77061515a4a9b3653ead4c24ac2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-18 09:10:14 +0000

    goplus: update 1.0.39_1 bottle.

--- Formula/goplus.rb
+++ Formula/goplus.rb
@@ -8,12 +8,12 @@ class Goplus < Formula
   head "https://github.com/goplus/gop.git", branch: "main"
 
   bottle do
-    sha256 arm64_monterey: "f28b542d6a219c6aa78b1aba273320808d856021ece092225ebec34394fa6ac4"
-    sha256 arm64_big_sur:  "2b6112cf6d92d8b8ba403513f49fca91435e07cb008e0312f047bf828dfd35a8"
-    sha256 monterey:       "90ecc73e7444e82f4c9c4d8eb59665525ffff12ebff3e2524e3cfbb798790df0"
-    sha256 big_sur:        "15d1fa6df5c60f44d0ba5149bc278171c60a5467da976505957bb541ee9ab527"
-    sha256 catalina:       "efea3c85f4ec6236a5f273d54dba3d41bec76a69cebeb3b15c8b8a4a2e30edae"
-    sha256 x86_64_linux:   "c3b8fa7a5fe0440182591f553cc83016378026bb2ae41f7f3fec6f56fd24a4b1"
+    sha256 arm64_monterey: "787817b6d30ce045804b5cd4124f2c157ca3ebc04b0f8c49f6c7576b5ea322aa"
+    sha256 arm64_big_sur:  "4d9b065bbec3d8b4b9e161c3ac9fdf4517e0301500faf0567fb60b166b336af7"
+    sha256 monterey:       "dba5db488bf81f97428478ca4615291d6a09021c06ba77102c144d7f2a97ae63"
+    sha256 big_sur:        "fab34d637e3cc69e5ee473f31ce2888092c3dc1cd771942103ec57a28406489a"
+    sha256 catalina:       "a59780cc2d6f9170de630ed38da0680b963076a821fb4dd5ee01d73b98ee2db9"
+    sha256 x86_64_linux:   "36a85ee921d8c8a65cb03c6b7e57c35ae1fbb4edc8dc529288bf8fd0b21b5507"
   end
 
   # Bump to 1.18 on the next release (1.1.0).
