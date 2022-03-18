7f54b86bee45c486338dc1fde374c1140ec559f3
Sha Sha Chu <shasha@pinterest.com>
2022-03-18 10:24:54 -0700

    ktlint 0.45.0
    Closes #97207.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ktlint.rb
+++ Formula/ktlint.rb
@@ -1,8 +1,8 @@
 class Ktlint < Formula
   desc "Anti-bikeshedding Kotlin linter with built-in formatter"
   homepage "https://ktlint.github.io/"
-  url "https://github.com/pinterest/ktlint/releases/download/0.44.0/ktlint"
-  sha256 "79d753be94191058401c31a06b96829ab212009261dca66b175466c4f1ae57d0"
+  url "https://github.com/pinterest/ktlint/releases/download/0.45.0/ktlint"
+  sha256 "33a335858035dd04dc5f86ed7278be181d4d10960411eb884c8a66636a92f423"
   license "MIT"
 
   bottle do
