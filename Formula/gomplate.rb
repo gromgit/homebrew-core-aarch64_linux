c251ab0a5e04b3a6f9a04d8782d59880cd0f6043
Bo Anderson <mail@boanderson.me>
2022-03-18 05:31:55 +0000

    gomplate: use go@1.17
    Closes #97166.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gomplate.rb
+++ Formula/gomplate.rb
@@ -16,7 +16,8 @@ class Gomplate < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f026fafcaeba0eb20e509c298edcd80c0158f0ab0a45a68184f754f8aa2e2754"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "make", "build", "VERSION=#{version}"
