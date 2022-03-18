aa11a3ac9b1fc6c15dbcf52193d64181a07e94c8
Bo Anderson <mail@boanderson.me>
2022-03-18 05:32:09 +0000

    gotify: use go@1.17
    Closes #97168.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gotify.rb
+++ Formula/gotify.rb
@@ -15,7 +15,8 @@ class Gotify < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "899b0f7b64c293f6053be2d747661a5baff2d9783b505dbf5b4d118ead2fb29c"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.Version=#{version}
