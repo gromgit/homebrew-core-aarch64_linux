cc9e5ac2829ac642560b3019b07195f78a3225c0
Bo Anderson <mail@boanderson.me>
2022-03-18 17:56:25 +0000

    lazydocker: use go@1.17
    Closes #97212.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lazydocker.rb
+++ Formula/lazydocker.rb
@@ -15,7 +15,8 @@ class Lazydocker < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb946c3fa3d69068bd29ffa13eb6bbda0d987bf398bb60d34bf9c1dfb01a2007"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", "-mod=vendor", "-o", bin/"lazydocker",
