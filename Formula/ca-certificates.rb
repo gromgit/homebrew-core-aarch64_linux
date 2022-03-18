9dbaf9021218cc29a4aba3ac75b351de391d8930
Johannes Klein <johannes.klein@lineupr.com>
2022-03-18 14:43:32 +0100

    ca-certificates 2022-03-18
    Closes #97191.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ca-certificates.rb
+++ Formula/ca-certificates.rb
@@ -1,8 +1,8 @@
 class CaCertificates < Formula
   desc "Mozilla CA certificate store"
   homepage "https://curl.se/docs/caextract.html"
-  url "https://curl.se/ca/cacert-2022-02-01.pem"
-  sha256 "1d9195b76d2ea25c2b5ae9bee52d05075244d78fcd9c58ee0b6fac47d395a5eb"
+  url "https://curl.se/ca/cacert-2022-03-18.pem"
+  sha256 "2d0575e481482551a6a4f9152e7d2ab4bafaeaee5f2606edb829c2fdb3713336"
   license "MPL-2.0"
 
   livecheck do
