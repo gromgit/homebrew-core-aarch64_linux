e82c6d255346545c9d52d8717939deddb4d6f723
Porkepix <porkepix@gmail.com>
2022-03-19 23:17:26 +0100

    citus 10.2.5
    Closes #97349.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/citus.rb
+++ Formula/citus.rb
@@ -1,8 +1,8 @@
 class Citus < Formula
   desc "PostgreSQL-based distributed RDBMS"
   homepage "https://www.citusdata.com"
-  url "https://github.com/citusdata/citus/archive/v10.2.4.tar.gz"
-  sha256 "bfe62893ad8b571737b38e9b6bca35650fdf8f89ce9da3996d6634090c97db7e"
+  url "https://github.com/citusdata/citus/archive/v10.2.5.tar.gz"
+  sha256 "748beaf219163468f0b92bf5315798457f9859a6cd9069a7fd03208d8d231176"
   license "AGPL-3.0-only"
   head "https://github.com/citusdata/citus.git", branch: "master"
 
