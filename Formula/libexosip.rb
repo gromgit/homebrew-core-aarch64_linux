4bea91b5106fec101c55b5026228bf6cff6d0709
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-03-19 17:40:19 +0000

    libexosip: update 5.3.0 bottle.

--- Formula/libexosip.rb
+++ Formula/libexosip.rb
@@ -12,11 +12,12 @@ class Libexosip < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "f25383f6e18e92d09bea5ce9a5355de38897736a00a1b5b73198f093e9f0302e"
-    sha256 cellar: :any, arm64_big_sur:  "e5862acc819d00bfe377cb07242481b6bf0749c358eb3d7e3523a22efa05b893"
-    sha256 cellar: :any, monterey:       "15e973aa1ca096bd2f5120d2fc9a99549eef1349e73d44225370f47ddb1e3e5b"
-    sha256 cellar: :any, big_sur:        "5a9c2568c86ffd96558f1d3c30dba6b088db674016df2ca5a70b265309108e59"
-    sha256 cellar: :any, catalina:       "f5afd5d2f0a37b824d6d054eaceae651c8b91484f0e8ca7f501c04f52e4daee6"
+    sha256 cellar: :any,                 arm64_monterey: "f25383f6e18e92d09bea5ce9a5355de38897736a00a1b5b73198f093e9f0302e"
+    sha256 cellar: :any,                 arm64_big_sur:  "e5862acc819d00bfe377cb07242481b6bf0749c358eb3d7e3523a22efa05b893"
+    sha256 cellar: :any,                 monterey:       "15e973aa1ca096bd2f5120d2fc9a99549eef1349e73d44225370f47ddb1e3e5b"
+    sha256 cellar: :any,                 big_sur:        "5a9c2568c86ffd96558f1d3c30dba6b088db674016df2ca5a70b265309108e59"
+    sha256 cellar: :any,                 catalina:       "f5afd5d2f0a37b824d6d054eaceae651c8b91484f0e8ca7f501c04f52e4daee6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15d4453f9ff8cf68f1e4862fe2e4bfcffbf5ccc3c17b735b78cd14b68f218759"
   end
 
   depends_on "pkg-config" => :build
