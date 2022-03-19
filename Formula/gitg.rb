edcfb0c778ae44d4d6942cf3aa641629db5f1465
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-03-19 05:26:40 +0000

    gitg: update 41 bottle.

--- Formula/gitg.rb
+++ Formula/gitg.rb
@@ -11,9 +11,12 @@ class Gitg < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "a2bf23c4cb3fdfdcdb05f250b5bf62d33bc60d01072f182f1209a40b604674df"
-    sha256 big_sur:       "45de0fdc63fdd84c90e4ccfcbd99e7f2338b5d5510f7b8364fc32b49d6335528"
-    sha256 catalina:      "6243ae261e1994f5b72c79bb469c23270c67e25a1e0f1a4a1fa39f1f248fd3f4"
+    sha256 arm64_monterey: "f68e86061156ba405410156aa3e4066015cdb9227162d68f2581664a6a7b4ba9"
+    sha256 arm64_big_sur:  "a2bf23c4cb3fdfdcdb05f250b5bf62d33bc60d01072f182f1209a40b604674df"
+    sha256 monterey:       "70d587c967403aafdff322736896ad013be0b1bc0a4a9fbe60329913a4f07fad"
+    sha256 big_sur:        "45de0fdc63fdd84c90e4ccfcbd99e7f2338b5d5510f7b8364fc32b49d6335528"
+    sha256 catalina:       "6243ae261e1994f5b72c79bb469c23270c67e25a1e0f1a4a1fa39f1f248fd3f4"
+    sha256 x86_64_linux:   "e25650fea8b7537a43e868afc3f0e1ac7aa1ed8153b655556bafe6fbdf883132"
   end
 
   depends_on "intltool" => :build
