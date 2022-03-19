f2a34207200e2f32c26b00975c861aa77b309d02
Bo Anderson <mail@boanderson.me>
2022-03-19 00:19:11 +0000

    tunnel: use go@1.17
    Closes #97255.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -15,7 +15,8 @@ class Tunnel < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a079e012dffd514c536355269716695a150db0dc30cd3ea2c27aee789615805"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", "-o", bin/"tunnel", "./cmd/tunnel"
