3eec6ad0c40de66a56a74d9b70c159cd737b3d3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-20 04:05:30 +0000

    regula: update 2.6.0 bottle.

--- Formula/regula.rb
+++ Formula/regula.rb
@@ -8,12 +8,12 @@ class Regula < Formula
   head "https://github.com/fugue/regula.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "44e7d41aa5d94a7cd8db2382da8c46771e9eece9551e1b7eda78c453da382e5c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "653752a60f08222b044fbdacf7852e30fbd1c1d4a7635bcd6ed0d3ea69fc91ed"
-    sha256 cellar: :any_skip_relocation, monterey:       "187e75b29cb8f3a083f1c13e9b4ef5b23b0bc1d066a9e3409be02bf152a533b6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "41f0c09b3f6a2b879af1acc7cdfff98c25b5f80d2cf91882ccc7c928fd9d3f7a"
-    sha256 cellar: :any_skip_relocation, catalina:       "29f7937f5b7480473d686ff6a3c0a378fe815b476387a44f1688b551e51b45cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a04343e498507f6a1ca9f86044be2c4fd731d682afe30a5cf9385fe9b01eed6a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e9af837240cb5565f33c56878b35e901c0856ff5f8a66c1a0797e4d09ebe322d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "86cacd41957eebef65432ad34f6c55d4a5eb033c9a7eb4cf45789c99b6b3cdd5"
+    sha256 cellar: :any_skip_relocation, monterey:       "20f4b7293bb8d7964854f9fa1dd0d06f4c8b947023d42d2850af84e9631dbe33"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7b9e47c79d15172cf9f18a1f65de541145493695b72dccb699e863bc75d20d4c"
+    sha256 cellar: :any_skip_relocation, catalina:       "180695182d5a0d74a680e807684e31cfbabc8cf9828f27dd5c3e37ffc148a6f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8fc8b59faf600c599a07de3c61cdb2d5e1d37159861eee071bc93da7e7f156c0"
   end
 
   depends_on "go" => :build
