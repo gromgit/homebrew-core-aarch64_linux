56cda173e3bb0043a3c0d74a10cac6f88e44cf25
Bo Anderson <mail@boanderson.me>
2022-03-20 05:02:14 +0000

    mockery: update 2.10.0_1 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -8,12 +8,12 @@ class Mockery < Formula
   head "https://github.com/vektra/mockery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ba1b26d9a01a32f68d8848e26a3b2e6c8441164e4dee9ba57b5cfeb9c7d143d9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4cf12466108fa5009e463bb89771e85e0a6a10d50058cb1fd3dd7d16577ae027"
-    sha256 cellar: :any_skip_relocation, monterey:       "addd15aa97afb69c5fd9a465e69951e2afd2d3769a449e46289eded0a00f256f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a66120fbaff7d926d3db6187299b21469d3488a40ebc5206f4149ca909318e7f"
-    sha256 cellar: :any_skip_relocation, catalina:       "1074d77c9a6b53bccb53437e9909c842dd6f9dd55b4255916150ecc746c1fefe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24f67449331a23eef1ba830dac1576504470f73bcbf40f42a022ec7127667c8d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e320c8e29f2776131e62354b8ddabe980f2751c51081cd8834c6715fe8825529"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c52f5e705990c9217fc0901708300f1f3f3b4911e74bab645b79e16b30c157c7"
+    sha256 cellar: :any_skip_relocation, monterey:       "057a0fe6cd9e1b16fa518c2b5e5d440bced0ff3e4ade79099d1b4216f6ccc122"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cd2ba93b917573b75a1b572d1b771b290b4b88cc7cddf8639c0b524cad1c9a4f"
+    sha256 cellar: :any_skip_relocation, catalina:       "9ba9be4482c5bf960760ba26778fcfa294da90e0937e70027e540777620712af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2218f44e889eebcc8967e410b5bac3de40b1c4a4b2362b852a013e071aefed20"
   end
 
   depends_on "go" => :build
