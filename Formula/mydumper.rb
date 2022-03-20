2458695e5f2a13a1301ba33eb9285190f5580c63
Rui Chen <rui@chenrui.dev>
2022-03-20 18:44:10 -0400

    mydumper 0.12.1
    Closes #97433.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mydumper.rb
+++ Formula/mydumper.rb
@@ -1,8 +1,8 @@
 class Mydumper < Formula
   desc "How MySQL DBA & support engineer would imagine 'mysqldump' ;-)"
   homepage "https://launchpad.net/mydumper"
-  url "https://github.com/mydumper/mydumper/archive/v0.11.5-2.tar.gz"
-  sha256 "5ca8fbca52abaf42d0c21eca2a182772a958709b3b150bfd3c10a47496591727"
+  url "https://github.com/mydumper/mydumper/archive/v0.12.1.tar.gz"
+  sha256 "f3c8ae09573d9a37512984cff24ade1cd87b50ae772944ef57d5bd1d5fac8e5b"
   license "GPL-3.0-or-later"
 
   bottle do
