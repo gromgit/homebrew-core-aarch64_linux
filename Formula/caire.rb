7224bc7593572ecab0c9b664323aa3f0c37ed0da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-20 08:08:06 +0000

    caire: update 1.4.3 bottle.

--- Formula/caire.rb
+++ Formula/caire.rb
@@ -7,12 +7,12 @@ class Caire < Formula
   head "https://github.com/esimov/caire.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "14f70b3438845397060eeb951fe97db41389f709acf0f13a954b23e3e3797c69"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7a01b854816989ac5df266cdb211588b87c35bc889ef4960764c932ff4bf995e"
-    sha256 cellar: :any_skip_relocation, monterey:       "a483a740c1895b6834e4bbb76c70b92d7711c7568671b7740ca2410ce72c438c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7c2faee11e04a2a2a2816f3de11aa700ffbad6d1d1636045a2e3e7edf94117e2"
-    sha256 cellar: :any_skip_relocation, catalina:       "f7218ea67bedb743c074e9f2ba1507deea1d752f6d205bb69a66aefb7d579074"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1ec617fa865ca313ca11d9f40680702dd0f3e94b87e4e2af3dabb43e029ee170"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bee65859e250a2a4956ac79d801d153ebb9bba79855af30b044cd46163270b7a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d4131ae8539f485e273560df33d4805f6fe36d8e40a246f6a0493cff1ac857c8"
+    sha256 cellar: :any_skip_relocation, monterey:       "fb89fb30bcca34b701351098260051614d177b5688396116f9c64cdde8a2b3cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "392b71adf8059ca52b33e368141453086732d7d9ca887226d2bfaa339ffd8f58"
+    sha256 cellar: :any_skip_relocation, catalina:       "98882a53c798df215e9989b351870aa5250334256799a7ed24e8daada3c3b743"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eddfdd672969291471f7690ae96a379d3b0e4fc2bca1911ca441b646cbece01e"
   end
 
   depends_on "go" => :build
