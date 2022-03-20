0ceae66698f6129700cb14610194aebefc749d87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-20 01:32:16 +0000

    nave: update 3.3.0 bottle.

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -7,12 +7,12 @@ class Nave < Formula
   head "https://github.com/isaacs/nave.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "15cf715e852ecfa6b1e27db2e85882a2ce4c186d4c884ef499ab3235f0fd031f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "15cf715e852ecfa6b1e27db2e85882a2ce4c186d4c884ef499ab3235f0fd031f"
-    sha256 cellar: :any_skip_relocation, monterey:       "415b8d8291219249edfa41148152288c201a08fea28ab0a44413ea656efdf1e2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "415b8d8291219249edfa41148152288c201a08fea28ab0a44413ea656efdf1e2"
-    sha256 cellar: :any_skip_relocation, catalina:       "415b8d8291219249edfa41148152288c201a08fea28ab0a44413ea656efdf1e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15cf715e852ecfa6b1e27db2e85882a2ce4c186d4c884ef499ab3235f0fd031f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "18d0ec2aa28f1556b535d1e1c818fdcc63f29278749ec9a384cf510dcd4e3b97"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "18d0ec2aa28f1556b535d1e1c818fdcc63f29278749ec9a384cf510dcd4e3b97"
+    sha256 cellar: :any_skip_relocation, monterey:       "45ee6cd3fbe703c724da73a37c8fea7bfde5bb59f87c71dea630a5c3a0d1df9f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "45ee6cd3fbe703c724da73a37c8fea7bfde5bb59f87c71dea630a5c3a0d1df9f"
+    sha256 cellar: :any_skip_relocation, catalina:       "45ee6cd3fbe703c724da73a37c8fea7bfde5bb59f87c71dea630a5c3a0d1df9f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "18d0ec2aa28f1556b535d1e1c818fdcc63f29278749ec9a384cf510dcd4e3b97"
   end
 
   def install
