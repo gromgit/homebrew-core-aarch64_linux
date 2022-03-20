d15f65acfa09f1535c8169836e629ece7efad9e1
Rui Chen <rui@chenrui.dev>
2022-03-20 18:15:06 -0400

    ispell 3.4.05
    * ispell 3.4.05
    * ispell: add license
    
    Closes #97417.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ispell.rb
+++ Formula/ispell.rb
@@ -1,9 +1,10 @@
 class Ispell < Formula
   desc "International Ispell"
   homepage "https://www.cs.hmc.edu/~geoff/ispell.html"
-  url "https://www.cs.hmc.edu/~geoff/tars/ispell-3.4.04.tar.gz"
-  mirror "https://deb.debian.org/debian/pool/main/i/ispell/ispell_3.4.04.orig.tar.gz"
-  sha256 "87bcd6f0521d85a0a3a7834215956d74ebc493144cc7c791f87be6872ccfe13e"
+  url "https://www.cs.hmc.edu/~geoff/tars/ispell-3.4.05.tar.gz"
+  mirror "https://deb.debian.org/debian/pool/main/i/ispell/ispell_3.4.05.orig.tar.gz"
+  sha256 "cf0c6dede3fd25fada4375d86acafe583cb96d8fe546de746a92ebb6df895602"
+  license :cannot_represent # modified BSD license
 
   livecheck do
     url :homepage
