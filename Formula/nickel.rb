e6d30d7f0db3855ccd23b9fc5ff777f11b175212
rui <rui@chenrui.dev>
2022-03-20 21:34:19 +0000

    nickel: add 0.1.0 bottle.

--- Formula/nickel.rb
+++ Formula/nickel.rb
@@ -6,6 +6,15 @@ class Nickel < Formula
   license "MIT"
   head "https://github.com/tweag/nickel.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "253153935a0851deb6b71ab13af3aebe111135036bf9d0678f1d33aa2f6e3274"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "46c530c40043a0c23148c855b81d173e6d35ac214ba8e9c053144243128dc840"
+    sha256 cellar: :any_skip_relocation, monterey:       "51ebfce96fc385913a762cace6ba8c40547a1aabcfea5d7e34c01e2fe218dd12"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3477ce1b7d66726fda834b900ae1ccdb4b89a6dd0883cf1edd830c73217d93dd"
+    sha256 cellar: :any_skip_relocation, catalina:       "392ba12ce5fbd2438bab43e06b680f253cb1c1bfd0d0df765f93442c410ace5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8f54a5740f84e3be7c449a8a5e3465f859b784820764ce387214cac24c1ce1a"
+  end
+
   depends_on "rust" => :build
 
   def install
