3c796ad54eb040cc9f098809a88e9341338c266c
Bevan Kay <email@bevankay.me>
2022-03-20 20:12:18 +1100

    cask 0.8.8
    * cask 0.8.8
    * cask: remove templates
    
    Closes #97386.
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cask.rb
+++ Formula/cask.rb
@@ -1,8 +1,8 @@
 class Cask < Formula
   desc "Emacs dependency management"
   homepage "https://cask.readthedocs.io/"
-  url "https://github.com/cask/cask/archive/v0.8.7.tar.gz"
-  sha256 "6b664da044e8faef77717f79bb90069ec9e7868e9c47da498057236b409a501b"
+  url "https://github.com/cask/cask/archive/v0.8.8.tar.gz"
+  sha256 "94f99d4161dedda3024312dc6b929be6319aff593a6d31f1cf4f2845ae6ca5c6"
   license "GPL-3.0-or-later"
   head "https://github.com/cask/cask.git", branch: "master"
 
@@ -15,11 +15,10 @@ class Cask < Formula
 
   def install
     bin.install "bin/cask"
-    prefix.install "templates"
     # Lisp files must stay here: https://github.com/cask/cask/issues/305
     prefix.install Dir["*.el"]
-    elisp.install_symlink "#{prefix}/cask.el"
-    elisp.install_symlink "#{prefix}/cask-bootstrap.el"
+    elisp.install_symlink prefix/"cask.el"
+    elisp.install_symlink prefix/"cask-bootstrap.el"
 
     # Stop cask performing self-upgrades.
     touch prefix/".no-upgrade"
