10df2e087697f398416a19d59748352c7b647673
Rui Chen <rui@chenrui.dev>
2022-03-20 19:36:44 -0400

    trivy 0.24.4
    Closes #97439.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -1,8 +1,8 @@
 class Trivy < Formula
   desc "Vulnerability scanner for container images, file systems, and Git repos"
   homepage "https://aquasecurity.github.io/trivy/"
-  url "https://github.com/aquasecurity/trivy/archive/v0.24.2.tar.gz"
-  sha256 "ddbffec10e32de2dfbef14169028d60df7504ae255ac674febbafdcf61c7906b"
+  url "https://github.com/aquasecurity/trivy/archive/v0.24.4.tar.gz"
+  sha256 "3048b3e2009e0a4fc96c12e4162ef106b45d84479e2aadb357971f907ef9d235"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/trivy.git", branch: "main"
 
