1510ae7d5f746501cf49356ee53e99e09344dd51
Porkepix <porkepix@gmail.com>
2022-03-21 23:31:57 +0100

    gofumpt 0.3.1
    Closes #97487.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gofumpt.rb
+++ Formula/gofumpt.rb
@@ -1,8 +1,8 @@
 class Gofumpt < Formula
   desc "Stricter gofmt"
   homepage "https://github.com/mvdan/gofumpt"
-  url "https://github.com/mvdan/gofumpt/archive/v0.3.0.tar.gz"
-  sha256 "41d88f985ca145d8f3fc3a02bfd92a8fd17aa9d581623f0b5d4b022ca88f2712"
+  url "https://github.com/mvdan/gofumpt/archive/v0.3.1.tar.gz"
+  sha256 "514faa1401511c5634eb906ebaa12c26dd1f7227f80b835c9b21af15bbd0ec3a"
   license "BSD-3-Clause"
 
   bottle do
