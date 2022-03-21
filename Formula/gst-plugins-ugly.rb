a258f99926260d5f1d954fbd4cddfb0c84d87d0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-21 10:10:36 +0000

    gst-plugins-ugly: update 1.20.0 bottle.

--- Formula/gst-plugins-ugly.rb
+++ Formula/gst-plugins-ugly.rb
@@ -12,12 +12,12 @@ class GstPluginsUgly < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "17d964846b2e566aef42536dfd1656e142b932d3f012ea35a12fc21caaf706eb"
-    sha256 arm64_big_sur:  "1eb49d0508fea6b8b6cfc79320851f4218c58c965caa84fb3acb772d599dbb5e"
-    sha256 monterey:       "a0a8716e0f2852b9801e491ce9358368aca40db23930d2469449af54ab25214e"
-    sha256 big_sur:        "4d4929eddbc0d179821e23b1d464ba6b5e9016f8bfdc46d508fea492aa2d17c1"
-    sha256 catalina:       "61c6247e61ea354c16b781fee374c907012f2ad11640ee4de99cbb15f5fb6c9f"
-    sha256 x86_64_linux:   "dc83215500215a27139c960d14f91428aff6dd1f58fb687e770ad48372f8209b"
+    sha256 arm64_monterey: "7b867439e878a489ba7711eeee5246fe11b22d45b6a31fa3bdcbe92262321dc8"
+    sha256 arm64_big_sur:  "c26fcfcb00d379e78ca6ef6604bb6fbd2009a115f4539028ceb30ad285e4f5cb"
+    sha256 monterey:       "4cefe0973223b6cadbcdc25c689c79b99ffd9f223c766fd084c70aba589b0a3a"
+    sha256 big_sur:        "68e560c958f51b56fca506ecec7184eb9b7e5ff65975bea175b89a4356b32bcb"
+    sha256 catalina:       "802a9a55d3b39fc00acb53a457bcc6cf1e94660c314d4560ffaba928191706d6"
+    sha256 x86_64_linux:   "57829a3d6af878758e67e6f6fd5642aac37a6ed8f199a9e334599f796223546d"
   end
 
   depends_on "meson" => :build
