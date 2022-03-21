feb575ff4e288b0d98a7eb96a2183e7e4fa708b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-21 09:09:43 +0000

    cask: update 0.8.8 bottle.

--- Formula/cask.rb
+++ Formula/cask.rb
@@ -7,7 +7,7 @@ class Cask < Formula
   head "https://github.com/cask/cask.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bd85befe31659e948617b988ed604d70199af9e7dd22dcfc8be6d76ecd92ee0b"
+    sha256 cellar: :any_skip_relocation, all: "f50a59d4337bc0a5f0a82cb680cec21ca11c058eec8f22d0319d972af568b162"
   end
 
   depends_on "coreutils"
