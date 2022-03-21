b8bedd0d40809bfcd334cca328d9899acce00d97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-21 05:05:32 +0000

    brook 20220406
    * brook 20220406
    * update test
    
    Closes #97449.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/brook.rb
+++ Formula/brook.rb
@@ -1,8 +1,8 @@
 class Brook < Formula
   desc "Cross-platform strong encryption and not detectable proxy. Zero-Configuration"
   homepage "https://txthinking.github.io/brook/"
-  url "https://github.com/txthinking/brook/archive/refs/tags/v20220404.tar.gz"
-  sha256 "a119adf673df8f61fcaec841e471392cfdd9d307fe52ec9d6b3d9393846a7630"
+  url "https://github.com/txthinking/brook/archive/refs/tags/v20220406.tar.gz"
+  sha256 "cf4433263cc755edfe56be66d206b7ee5083faaaa8b30bb4102174ad73e22764"
   license "GPL-3.0-only"
 
   bottle do
@@ -22,6 +22,6 @@ class Brook < Formula
 
   test do
     output = shell_output "#{bin}/brook link --server 1.2.3.4:56789 --password hello"
-    assert_match "brook://server?address=&insecure=&name=&password=hello&server=1.2.3.4%3A56789&username=", output
+    assert_match "brook://server?address=&insecure=&name=&password=hello", output
   end
 end
