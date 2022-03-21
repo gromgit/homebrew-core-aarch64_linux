9b8f4a85b7ee410ec1f422b9a631921c6add55d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-21 09:09:38 +0000

    hadolint: update 2.9.2 bottle.

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -6,12 +6,12 @@ class Hadolint < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "01aa18637c357a4f555d0f8a0acf576bb56e26f75f77e9c34c6d0c09917ba9c3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "506557e125b3c6af303a5353f62ca26eea0d4572d3509a17a99fb5cce5cf7220"
-    sha256 cellar: :any_skip_relocation, monterey:       "88170823421008d191d8f10b1370de5fc1414c0f014a13e8d3efefca5ebfe2d6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "346b2c201167e0325b71df9b478a1bc5267b49b28e1acaa5187e6d295365886d"
-    sha256 cellar: :any_skip_relocation, catalina:       "6d74701f7ea667825b602633643c6421b89ba0a99d82c5936e92c2ac3c643fb8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bff3d3fa9d4aaf076cec506c270db855a67d87c5753b0918a77c93cb5c05f292"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fbd4167b4f1c90c86b581c33472d9327da687c9a22bae5ddc8976ea470bf8513"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e94ba605593f2811e50db5f2ce74ce7550a33b241f23f353f7c4a9d030d33922"
+    sha256 cellar: :any_skip_relocation, monterey:       "5862dc8527aa60c018986c8a07edfb95b64a2aa75a9d1f54c46ed775d78ea470"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b35ebd0fddcdaf07b371605f88736e8717d262a0f3c005c17d58831dd8b8d735"
+    sha256 cellar: :any_skip_relocation, catalina:       "9e5f758e4878e698b4bf637d69724e62966565f1cdc828b5a7ad87d3ed5274df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "977ffe9a745b23adc8a7a88aae9c1cee7a78054a253204c2cedd47b06bf64bc7"
   end
 
   depends_on "ghc" => :build
