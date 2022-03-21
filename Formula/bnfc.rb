ed6a9a3f0ae86637dfe0d0b96b9a1bd9ec49125c
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-21 11:16:42 -0700

    bnfc 2.9.4
    Closes #97478.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bnfc.rb
+++ Formula/bnfc.rb
@@ -1,8 +1,8 @@
 class Bnfc < Formula
   desc "BNF Converter"
   homepage "https://bnfc.digitalgrammars.com/"
-  url "https://github.com/BNFC/bnfc/archive/v2.9.1.tar.gz"
-  sha256 "d79125168636fdcf0acd64a9b83ea620c311b16dcada0848d8a577aa8aeddec4"
+  url "https://github.com/BNFC/bnfc/archive/v2.9.4.tar.gz"
+  sha256 "0c2ebee6ff3f7603f650f22fbb9836919761a130994b934d6ab03a5f29583254"
   license "BSD-3-Clause"
   head "https://github.com/BNFC/bnfc.git", branch: "master"
 
@@ -20,11 +20,11 @@ class Bnfc < Formula
   depends_on "cabal-install" => [:build, :test]
   depends_on "ghc" => [:build, :test]
   depends_on "sphinx-doc" => :build
+  depends_on "agda" => :test
   depends_on "antlr" => :test
+  depends_on "bison" => :test
+  depends_on "flex" => :test
   depends_on "openjdk" => :test
-
-  uses_from_macos "bison" => :test
-  uses_from_macos "flex" => :test
 
   def install
     cd "source" do
@@ -45,6 +45,11 @@ class Bnfc < Formula
 
   test do
     ENV.prepend_create_path "PATH", testpath/"tools-bin"
+    system "cabal", "v2-update"
+    system "cabal", "v2-install",
+           "--jobs=#{ENV.make_jobs}", "--max-backjumps=100000",
+           "--install-method=copy", "--installdir=#{testpath/"tools-bin"}",
+           "alex", "happy"
 
     (testpath/"calc.cf").write <<~EOS
       EAdd. Exp  ::= Exp  "+" Exp1 ;
@@ -56,13 +61,11 @@ class Bnfc < Formula
       entrypoints Exp ;
       comment "(#" "#)" ;
     EOS
+    system bin/"bnfc", "--check", testpath/"calc.cf"
+
     (testpath/"test.calc").write "14 * (# Parsing is fun! #) (3 + 2 / 5 - 8)"
-    treespace = if build.head?
-      " "
-    else
-      ""
-    end
-    check_out = <<~EOS
+    space = " "
+    check_out_c = <<~EOS
 
       Parse Successful!
 
@@ -70,47 +73,66 @@ class Bnfc < Formula
       (EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8)))
 
       [Linearized Tree]
-      14 * (3 + 2 / 5 - 8) #{treespace}
+      14 * (3 + 2 / 5 - 8)#{space}
 
+    EOS
+    check_out_hs = <<~EOS
+      #{testpath/"test.calc"}
+
+      Parse Successful!
+
+      [Abstract Syntax]
+
+      EMul (Just (1,1)) (EInt (Just (1,1)) 14) (ESub (Just (1,29)) (EAdd (Just (1,29)) (EInt (Just (1,29)) 3) (EDiv (Just (1,33)) (EInt (Just (1,33)) 2) (EInt (Just (1,37)) 5))) (EInt (Just (1,41)) 8))
+
+      [Linearized tree]
+
+      14 * (3 + 2 / 5 - 8)
+    EOS
+    check_out_agda = <<~EOS
+      PARSE SUCCESSFUL
+
+      14 * (3 + 2 / 5 - 8)
+    EOS
+    check_out_java = <<~EOS
+
+      Parse Succesful!
+
+      [Abstract Syntax]
+
+      (EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8)))#{space}
+
+      [Linearized Tree]
+
+      14 * (3 + 2 / 5 - 8)
     EOS
 
     mktemp "c-test" do
       system bin/"bnfc", "-m", "-o.", "--c", testpath/"calc.cf"
-      system "make", "CC=#{ENV.cc}", "CCFLAGS=#{ENV.cflags}"
+      system "make", "CC=#{ENV.cc}", "CCFLAGS=#{ENV.cflags}",
+             "FLEX=#{Formula["flex"].bin/"flex"}",
+             "BISON=#{Formula["bison"].bin/"bison"}"
       test_out = shell_output("./Testcalc #{testpath}/test.calc")
-      assert_equal check_out, test_out
+      assert_equal check_out_c, test_out
     end
 
     mktemp "cxx-test" do
       system bin/"bnfc", "-m", "-o.", "--cpp", testpath/"calc.cf"
-      system "make", "CC=#{ENV.cxx}", "CCFLAGS=#{ENV.cxxflags}"
+      system "make", "CC=#{ENV.cxx}", "CCFLAGS=#{ENV.cxxflags}",
+             "FLEX=#{Formula["flex"].bin/"flex"}",
+             "BISON=#{Formula["bison"].bin/"bison"}"
       test_out = shell_output("./Testcalc #{testpath}/test.calc")
-      assert_equal check_out, test_out
+      assert_equal check_out_c, test_out
     end
 
-    mktemp "haskell-test" do
-      system "cabal", "v2-update"
-      system "cabal", "v2-install",
-             "--jobs=#{ENV.make_jobs}", "--max-backjumps=100000",
-             "--install-method=copy", "--installdir=#{testpath/"tools-bin"}",
-             "alex", "happy"
-      system bin/"bnfc", "-m", "-o.", "--haskell", "--ghc", "-d", testpath/"calc.cf"
+    mktemp "agda-test" do
+      system bin/"bnfc", "-m", "-o.", "--haskell", "--text-token",
+             "--generic", "--functor", "--agda", "-d", testpath/"calc.cf"
       system "make"
-      test_out = shell_output("./Calc/Test #{testpath/"test.calc"}")
-      check_out_hs = <<~EOS
-        #{testpath/"test.calc"}
-
-        Parse Successful!
-
-        [Abstract Syntax]
-
-        EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8))
-
-        [Linearized tree]
-
-        14 * (3 + 2 / 5 - 8)
-      EOS
+      test_out = shell_output("./Calc/Test #{testpath/"test.calc"}") # Haskell
       assert_equal check_out_hs, test_out
+      test_out = shell_output("./Main #{testpath/"test.calc"}") # Agda
+      assert_equal check_out_agda, test_out
     end
 
     mktemp "java-test" do
@@ -120,22 +142,10 @@ class Bnfc < Formula
       antlr_jar = Dir[Formula["antlr"].prefix/"antlr-*-complete.jar"][0]
       ENV["CLASSPATH"] = ".:#{antlr_jar}"
       system bin/"bnfc", "-m", "-o.", "--java", "--antlr4", testpath/"calc.cf"
-      system "make", "JAVAC=#{jdk_dir/"javac"}", "JAVA=#{jdk_dir/"java"}", "LEXER=#{antlr_bin}", "PARSER=#{antlr_bin}"
+      system "make", "JAVAC=#{jdk_dir/"javac"}", "JAVA=#{jdk_dir/"java"}",
+             "LEXER=#{antlr_bin}", "PARSER=#{antlr_bin}"
       test_out = shell_output("#{jdk_dir}/java calc.Test #{testpath}/test.calc")
-      space = " "
-      check_out_j = <<~EOS
-
-        Parse Succesful!
-
-        [Abstract Syntax]
-
-        (EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8)))#{space}
-
-        [Linearized Tree]
-
-        14 * (3 + 2 / 5 - 8)
-      EOS
-      assert_equal check_out_j, test_out
+      assert_equal check_out_java, test_out
     end
   end
 end
