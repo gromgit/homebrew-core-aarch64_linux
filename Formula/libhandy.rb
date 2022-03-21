c6ece19f5ed24ce00771bfd5f01cdb69d5d61214
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-21 08:07:18 +0000

    libhandy: update 1.6.1 bottle.

--- Formula/libhandy.rb
+++ Formula/libhandy.rb
@@ -6,13 +6,12 @@ class Libhandy < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_monterey: "a1bc08ba132d264744d3f8f5101d6997bae896e4ce3f0d420c9d2ace092aec2d"
-    sha256 arm64_big_sur:  "67c5f4982894646b07942c0f387443c2aac2813167aaa3de9197cd760fc93e5b"
-    sha256 monterey:       "9846518bcebff7818836942e043f03f4e7e8ac07f618646b6d0396407085e1c9"
-    sha256 big_sur:        "337f9be107164821d8436c443c20dcb79d66bc3268fac909ace2878432bcc652"
-    sha256 catalina:       "2e04f3f8004ac544fea47905a65a6597908b8ced6dadd5c330d87b8a310951d9"
-    sha256 mojave:         "a8972dc9efc8c2d6a6fe0f038ebda941a741c3e0d7be57b51b9c634120d1a479"
-    sha256 x86_64_linux:   "17515106b873bbdbc591ca0daca12c043485e6a52b478cff912f9a835620c825"
+    sha256 arm64_monterey: "ebbe54fcb78893c4c8de374c49c8fe723ee732f54a97c38d0260d2e690bf0470"
+    sha256 arm64_big_sur:  "789ef01caeb83be07da4dfb85964833b6ad43701f5cf94a5794578719d5d0e7d"
+    sha256 monterey:       "0147123e7fedf3ef43f2837a81d9769355cc89614c0990880b6c6c6b018d6be6"
+    sha256 big_sur:        "c215f07ff7d085c9e7686e6a3ec32bc1e79af334347e31642ff0e02a87198b57"
+    sha256 catalina:       "9d1c112eb85f490a73e749e6a11b2a5cd7a2be0e8aaa9353c982c57e8ae530bf"
+    sha256 x86_64_linux:   "cf43b401b5a54201077e6782f9dc539691506a4dd35bdcc64de46b5125fd3058"
   end
 
   depends_on "gettext" => :build
