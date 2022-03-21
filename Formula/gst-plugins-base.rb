faedd86a23d7cf593935d49a06b01f6238b313cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-21 10:10:34 +0000

    gst-plugins-base: update 1.20.0 bottle.

--- Formula/gst-plugins-base.rb
+++ Formula/gst-plugins-base.rb
@@ -12,12 +12,12 @@ class GstPluginsBase < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "30ca2c483d53fb6dc7ef736f117d38f656f1c42599265c5c062465a9dcb67ae8"
-    sha256 arm64_big_sur:  "24d9c4c3e469da3a3f06049924de4018f49fdd3c4cb49172e2396030d4dc52a3"
-    sha256 monterey:       "8224b5894d255a57d8346f8cd50067f8c107bfe5d9fe155aa5f1cf6a13f7e41e"
-    sha256 big_sur:        "9d6ce345011d18a70a2c5388b9dae08d8fe9df95fdbe12ce7cd0a6e704f03b30"
-    sha256 catalina:       "ca3dd9389dba3c782bde7ca1097cc216a00542fd17a0a05b19371c26a54f1c14"
-    sha256 x86_64_linux:   "4619b056143e59a6e726a9fbabceddafd9927980a23912b95879e9bb7d6952ec"
+    sha256 arm64_monterey: "2491152f4f84cb9b5229c84e75a5b21a72ff9d410eac12ed1003e0526efb9ab2"
+    sha256 arm64_big_sur:  "ce62108d6e9b1508b8cc388bbabf50eb176a9c687a7a9c7f35d935d8ea770b75"
+    sha256 monterey:       "6270c08e52101f3df5734bce8c41edae27a40f19316e8e0f2f1039da4e981f0b"
+    sha256 big_sur:        "7b9fa237ee26eefc873430fb1a1e742eb489a75a83cc2ed66e392fa575616bca"
+    sha256 catalina:       "22980d6f57e18ecbcfb567ef018ecaf648cbf26cc51b0ca76af2d08277dc388e"
+    sha256 x86_64_linux:   "65ed258e4436ad5d9b6496e0a4d2f4989ad696204d6d9297a7d85720e3bc1ea8"
   end
 
   depends_on "gobject-introspection" => :build
