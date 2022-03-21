adffe606f2e2305de632076f12748b0b2c7240c1
rui <rui@chenrui.dev>
2022-03-21 22:54:37 +0000

    rslint: add 0.3.2 bottle.

--- Formula/rslint.rb
+++ Formula/rslint.rb
@@ -6,6 +6,15 @@ class Rslint < Formula
   license "MIT"
   head "https://github.com/rslint/rslint.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c31bc15e61ed4cf42203a3e658c1eb71b3d806b4babf99e683e9f365fcedccb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f196a45f40c69b3dbdc20adc1255301306ba98b016e397901bae778f2a6cb04"
+    sha256 cellar: :any_skip_relocation, monterey:       "7c51e40ac4683471f768d958bf5fe2375d226534395b2bc882f1060138c442e0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d1fed3b6b5f370f98cacb49bae2275d20a1e3b96615c0433098c047e98648c4e"
+    sha256 cellar: :any_skip_relocation, catalina:       "d4343b248401217eee1ae79f9c8125f753fced9400810eff170e7ff902f5c92a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94c0c4400342ef622293d652665f592894fc472810efb11b94526b80373d75e0"
+  end
+
   depends_on "rust" => :build
 
   def install
