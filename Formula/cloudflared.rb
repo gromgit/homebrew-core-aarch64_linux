b578ae78d0e1010e7425df2a7ca2920f071a584e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-22 19:05:40 +0000

    cloudflared: update 2022.3.3 bottle.

--- Formula/cloudflared.rb
+++ Formula/cloudflared.rb
@@ -7,12 +7,12 @@ class Cloudflared < Formula
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d0352ac06b86b91ebfec6de82e454cd2e020f6f00e77faba31220d9a30187ce9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05dfbcf4771ae091abda834284ba71dcc545790c000bd5a32c4d3ab8e62e33a9"
-    sha256 cellar: :any_skip_relocation, monterey:       "530f2344ffbc20523bd80964c75a0f24bd550926bbbb64fb7512cbe163270a44"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9e1e3a05899e252cb321f4cec573c09f8372ec51ebdbf85f12499bace9ad6803"
-    sha256 cellar: :any_skip_relocation, catalina:       "44a6acfa04d0b8d1b703e2067bf5e5033a687e7fba38e7892142e49d5077d923"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a0308e3298dfdfaedbfaa6c82aa444a1e5c9cec849306e5c57e4c0a7c023b35c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "737b9bd88b50a6ffe46a83d7afc2bc2a747c30dac72c6a372e390e1ab8330185"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "765d261b11cb13cc20da0113c596adfa02c4426ebe727ce8f151f7a657e05ead"
+    sha256 cellar: :any_skip_relocation, monterey:       "c3b71f084594773441abd7aa85169c9ec61bb7cfd92acf5362be7c8936f78ed2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d44cf68ddc7fe6766ce143429c83dc8800df6b594aa83549b7afadcda6640c5f"
+    sha256 cellar: :any_skip_relocation, catalina:       "06c1f4029ffa33a46cf89b6c928344bc5b50c22aefc168ad74fc8747129f68f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b9e33d4ccdd923e693c746875758754219cb13cb9008f9fe13abf285174c7a8"
   end
 
   depends_on "go" => :build
