757d6d5b90c9258ba5f36eaa5ab0bb0e23934cbb
Porkepix <porkepix@gmail.com>
2022-03-23 00:42:14 +0100

    kubecfg 0.26.0
    Closes #97574.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -1,8 +1,8 @@
 class Kubecfg < Formula
   desc "Manage complex enterprise Kubernetes environments as code"
   homepage "https://github.com/kubecfg/kubecfg"
-  url "https://github.com/kubecfg/kubecfg/archive/v0.25.0.tar.gz"
-  sha256 "884708337842341d4f777e00321bfa72b16bf73027a583305ebb281000096714"
+  url "https://github.com/kubecfg/kubecfg/archive/v0.26.0.tar.gz"
+  sha256 "322ed2b6d4214bafac63ee3d666aa240b077a0949d68bc97e5b6dfc484345b7e"
   license "Apache-2.0"
 
   bottle do
