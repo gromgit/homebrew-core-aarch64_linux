5c94158a005816313d6b96d5d1234687f0e609a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-22 05:31:51 +0000

    okteto 2.0.0
    Closes #97531.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,8 +1,8 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/1.15.8.tar.gz"
-  sha256 "fea0f43c9f24e9388463cf036f0133a5d852c935f5f52b4c49b1ae8077d7f8fa"
+  url "https://github.com/okteto/okteto/archive/2.0.0.tar.gz"
+  sha256 "f5d9dbb07447a21a8b7b36ed81c165d9367a0bfda5183cb381e609b55448bc98"
   license "Apache-2.0"
   head "https://github.com/okteto/okteto.git", branch: "master"
 
