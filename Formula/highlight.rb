0410fffc31516d4e4bd21680cb014cdb1e318b41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-23 10:09:47 +0000

    highlight: update 4.2 bottle.

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -12,13 +12,12 @@ class Highlight < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "6727bd0348ec08e9855fd4574cf8a564d94cba5c3195f1bb9852f9733c351f08"
-    sha256 arm64_big_sur:  "90c97137575b00aaa9050fb14f049e6b1dcfe236ae4266d7e684706f09a58f0f"
-    sha256 monterey:       "4186adee8552304558aa2fd7af70db3355b92650564324b6cd02770bd22d4887"
-    sha256 big_sur:        "e364d145b581f0622acf1cc9476142b8e318efa60495ca6e9c1f44ed560a2d66"
-    sha256 catalina:       "4238b72def77283c7cedf385639bd9b28b5d0e3fb5d5c0bc6ec6fe6d06ae7bcf"
-    sha256 mojave:         "81057a5898f59793eda32340275cb8bf87da61e6330bb5d0e925690c678b606b"
-    sha256 x86_64_linux:   "f6f656122700a511ed946ca5a3d4db0e05db5c6f4f988567e866dfc50938a406"
+    sha256 arm64_monterey: "caf613faeea04598af9f40e33f0035584250cb690bd2747374b932a562f16f04"
+    sha256 arm64_big_sur:  "4334f3d71cd6c4e5b452154f7b9d1386d921afc9805bcbafd18c4a945a88dbac"
+    sha256 monterey:       "f9fd20137f1c3bba43234e0d1ef181ff37001e226f4887d1c168be726c989514"
+    sha256 big_sur:        "2cf01ec552ef0d2367aa9aecd1606cbee37c17e009bd72e371f8375e416d2a3f"
+    sha256 catalina:       "5bb748bd4cdc0211a157d2a846bb2921cda8c35a3c2508acb17290a376b66010"
+    sha256 x86_64_linux:   "e60a62a021aee25dd40cd2fed076a7700f8a943da70927a0d4fd934945b69245"
   end
 
   depends_on "boost" => :build
