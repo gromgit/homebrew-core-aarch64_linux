651d4d33cffac6ea709341716e70866aa32679d9
Caleb Xu <calebcenter@live.com>
2022-03-23 17:00:44 -0400

    gibo 2.2.7
    Closes #97662.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gibo.rb
+++ Formula/gibo.rb
@@ -1,8 +1,8 @@
 class Gibo < Formula
   desc "Access GitHub's .gitignore boilerplates"
   homepage "https://github.com/simonwhitaker/gibo"
-  url "https://github.com/simonwhitaker/gibo/archive/2.2.6.tar.gz"
-  sha256 "0a4b3a9063ac8724a2deda5b47ac8324e01905b794330a0c40ea9e972022f01e"
+  url "https://github.com/simonwhitaker/gibo/archive/2.2.7.tar.gz"
+  sha256 "9f44af2d1b1f3c5582620b7b5202ee2eb897a19bffc6e0d7ce95c6f0da2688bb"
   license "Unlicense"
 
   bottle do
