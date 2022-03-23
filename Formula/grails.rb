b1906a5c46e0c49b6287f40f49145a0533066108
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-23 07:06:29 +0000

    grails: update 5.1.4 bottle.

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -11,7 +11,7 @@ class Grails < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c079f11de5a04134d4b493923e8821ecb58a709ade2ab13becfd4a6baede863f"
+    sha256 cellar: :any_skip_relocation, all: "4b0c85b8797303ef4960c7a7cbbf81bc825704489a8c16265877e1d3efd00eee"
   end
 
   depends_on "openjdk@11"
