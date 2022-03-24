b8c402a5d4dc407fa237d523f578806b00e123d5
Porkepix <porkepix@gmail.com>
2022-03-24 02:01:13 +0100

    vulkan-headers 1.3.209
    Closes #97687.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.208.tar.gz"
-  sha256 "15050e9748633484957a166150f680a0ba8030074db599aad7c2d432191712af"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.209.tar.gz"
+  sha256 "345011af2369963ef65eff2f678419efca728a3035741882d52f871bbd3575bd"
   license "Apache-2.0"
 
   bottle do
