bc0f701a3351565b27960cffb7d25d776cc870be
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-03-24 19:30:40 +0000

    qalculate-gtk: update 4.1.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -11,6 +11,7 @@ class QalculateGtk < Formula
     sha256 monterey:       "5e56e60f9d551607f55257e0b2cc6a07b03bfeaaf4acf18949597dc63eba35fa"
     sha256 big_sur:        "49b4f3146e8c166880801fbe2b5962df83c79ff0b6cbe87ec59c23c8fe4e15be"
     sha256 catalina:       "81d643833a88969558f9417cfcf230fa4b83c23be9d2028da02af2395235c093"
+    sha256 x86_64_linux:   "daf946c8961ba17b6b772bc180b92595e2696a73308e1d79b10166d6118bfb80"
   end
 
   depends_on "intltool" => :build
