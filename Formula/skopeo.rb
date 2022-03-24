22eb05cc23bf284d0d880a74038e25187bb677ca
Porkepix <porkepix@gmail.com>
2022-03-25 00:17:25 +0100

    skopeo 1.7.0
    Closes #97733.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/skopeo.rb
+++ Formula/skopeo.rb
@@ -1,8 +1,8 @@
 class Skopeo < Formula
   desc "Work with remote images registries"
   homepage "https://github.com/containers/skopeo"
-  url "https://github.com/containers/skopeo/archive/v1.6.1.tar.gz"
-  sha256 "304016ec4eb802bf1e1b350854db50bdc99a93dcbcc71f9d531dbb916dc146f3"
+  url "https://github.com/containers/skopeo/archive/v1.7.0.tar.gz"
+  sha256 "453bdcce16767696ed71046b60ad7b34358b183b50eb5aa708ced0b5ea2927b1"
   license "Apache-2.0"
 
   bottle do
