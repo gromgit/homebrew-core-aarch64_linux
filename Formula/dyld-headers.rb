106026a8ed451d8476170e3ddef2b231b4e1f4b4
Bo Anderson <mail@boanderson.me>
2022-03-25 03:51:29 +0000

    dyld-headers 941.5
    Closes #97747.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dyld-headers.rb
+++ Formula/dyld-headers.rb
@@ -1,14 +1,9 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://opensource.apple.com/tarballs/dyld/dyld-852.2.tar.gz"
-  sha256 "f38e62fa9d3aec0bbc487cc27bf141b060e163195878c2dde31cb54609e436c4"
+  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-941.5.tar.gz"
+  sha256 "1041fc6794579593d244fb1e88f99d1385d6170ec92247af8246d67799b6553b"
   license "APSL-2.0"
-
-  livecheck do
-    url "https://opensource.apple.com/tarballs/dyld/"
-    regex(/href=.*?dyld[._-]v?(\d+(?:\.\d+)*)\.t/i)
-  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "221d2dec7a44925607f6ed07474cc044333c8f93e14a769bfdd4c4131cd27909"
