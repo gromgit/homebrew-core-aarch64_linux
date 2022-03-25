edbee5d10031b19a66fdff4bf44a0151c90dc147
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2022-03-25 23:10:17 +0530

    flyctl 0.0.307 (new formula)
    Closes #97781.
    
    Signed-off-by: Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/flyctl.rb
@@ -0,0 +1,39 @@
+class Flyctl < Formula
+  desc "Command-line tools for fly.io services"
+  homepage "https://fly.io"
+  url "https://github.com/superfly/flyctl.git",
+      tag:      "v0.0.307",
+      revision: "23f2c2540714c09643e3213f436938b28f18f2c1"
+  license "Apache-2.0"
+  head "https://github.com/superfly/flyctl.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/superfly/flyctl/internal/buildinfo.environment=production
+      -X github.com/superfly/flyctl/internal/buildinfo.buildDate=#{time.iso8601}
+      -X github.com/superfly/flyctl/internal/buildinfo.version=#{version}
+      -X github.com/superfly/flyctl/internal/buildinfo.commit=#{Utils.git_short_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bin.install_symlink "flyctl" => "fly"
+
+    bash_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "bash")
+    (bash_completion/"flyctl").write bash_output
+    zsh_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "zsh")
+    (zsh_completion/"_flyctl").write zsh_output
+    fish_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "fish")
+    (fish_completion/"flyctl.fish").write fish_output
+  end
+
+  test do
+    assert_match "flyctl v#{version}", shell_output("#{bin}/flyctl version")
+
+    flyctl_status = shell_output("flyctl status 2>&1", 1)
+    assert_match "Error No access token available. Please login with 'flyctl auth login'", flyctl_status
+  end
+end
