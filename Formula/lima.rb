784bc933482af69141966d6b7904a612b5cddacb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-26 14:07:31 +0000

    lima: update 0.9.2 bottle.

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -7,12 +7,12 @@ class Lima < Formula
   head "https://github.com/lima-vm/lima.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "980ed1038f56923a830b2016c14bb1693b3cfd483e4902b95b124e9c58b936b6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9e9819038a44a85734b55c7cf880797bf1f99ff672b0874c74e152e3640b4c9f"
-    sha256 cellar: :any_skip_relocation, monterey:       "14ae542f9998529ed0952cef4c74ea6bd28a99fbe15655e81421dfc7964ec114"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cfe45877c3777db3633c52967df098a5d6abcbe04ec0afa5f84fc112faf1d8a5"
-    sha256 cellar: :any_skip_relocation, catalina:       "0d35c0b10d4892034f7a9717b239457b099aa35415e228fb5ae79f93012f21fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6bde2974f9446d7a4ba2b7d3c3f9e6af74f3eff3d823c079bf0ea1bd6b00630e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "41f7cf37b3fa55b46884b355f3bc00d9885fc88dcd8402559475046c7ea98e1a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "03a2f7ddcf2f2acfb483a01c0cda74cce47b66ae4845d2aac39474ca8df185ab"
+    sha256 cellar: :any_skip_relocation, monterey:       "62b108b56c46c065e987e072d1aa4dacb5febbe825b57c55a492f66af4e271fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cbbb8e2b759d4585c157f530f9c55fd17b186b74616f45f60f85003f84bfb098"
+    sha256 cellar: :any_skip_relocation, catalina:       "3e763a4c7131b342915b30c3ef3059efc739c62355e3c0b512d83f3a900f8732"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "49e9a8dc5cc1bef043bb4d7a7f0d28ec3748184785c0d03646d125358f299da4"
   end
 
   depends_on "go" => :build
