5889b4a3e603c66293db411c7f64cd7a63730284
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-26 22:04:54 +0000

    bartycrouch: update 4.10.2 bottle.

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -8,10 +8,10 @@ class Bartycrouch < Formula
   head "https://github.com/Flinesoft/BartyCrouch.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "70be38e64705f2ce9d8c17a3726f5bb8985107442968b452a9c20896d5a020e9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01b027a64112756fb762a39e689ec55fee526c0521fb6847447851f6728e20e2"
-    sha256 cellar: :any_skip_relocation, monterey:       "739e9a0ba1d6d4482d935b8e6ec01aa36791fc5b72bb8e4319f4a7239308c2c1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "38fa4742fe695b7b88ace488d89941c83027f17dca38c4dd871e0491f2a9ef18"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9cc3772fc61860a1a5f0013f9b7cfc214545dc7815e0759df68c3764885327ee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22c833293a3638f2fc037b8d3acde3d9470422bcd18f7d932f048f3f56d2b88f"
+    sha256 cellar: :any_skip_relocation, monterey:       "7fd5de567eda72492d0ad8488c76530a9e1ebc3770710ecb17ba2047310da247"
+    sha256 cellar: :any_skip_relocation, big_sur:        "97f91089d2c485d33fa7becac46b1f5d9d102da5c9c58233d2a2ca85524eaab5"
   end
 
   depends_on xcode: ["12.5", :build]
