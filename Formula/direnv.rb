eb242f372dcc020d65bc2436439e7ffd49ea8d45
homebrew-updater <bepsvpt/homebrew-updater>
2022-03-26 23:00:13 +0800

    direnv 2.31.0
    Closes #97844.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -1,8 +1,8 @@
 class Direnv < Formula
   desc "Load/unload environment variables based on $PWD"
   homepage "https://direnv.net/"
-  url "https://github.com/direnv/direnv/archive/v2.30.3.tar.gz"
-  sha256 "7fb5431b98d57fb8c70218b4a0fab4b08f102790e7a92486f588bf3d5751ac3b"
+  url "https://github.com/direnv/direnv/archive/v2.31.0.tar.gz"
+  sha256 "f82694202f584d281a166bd5b7e877565f96a94807af96325c8f43643d76cb44"
   license "MIT"
   head "https://github.com/direnv/direnv.git", branch: "master"
 
