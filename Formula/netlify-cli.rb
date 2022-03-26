c5869cbaf6e6420cc8269cea88afdb67dc1cef27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-26 08:05:51 +0000

    netlify-cli: update 9.13.5 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -9,12 +9,12 @@ class NetlifyCli < Formula
   head "https://github.com/netlify/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b04cd58f497da046af0c2bd8fdcb2b87c568da1589c6ce6ef98fe643106885a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b04cd58f497da046af0c2bd8fdcb2b87c568da1589c6ce6ef98fe643106885a"
-    sha256 cellar: :any_skip_relocation, monterey:       "d0431aac03692d693e017914cb0f5f06011b44f7c00bf4f04e4c9247d31d925a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d0431aac03692d693e017914cb0f5f06011b44f7c00bf4f04e4c9247d31d925a"
-    sha256 cellar: :any_skip_relocation, catalina:       "d0431aac03692d693e017914cb0f5f06011b44f7c00bf4f04e4c9247d31d925a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "718ef3725a611ac13a5609596594cc769aa6eaf52a2848e0b6f578bc527f90db"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7fa83b6e08a2f5cb39f88719742082cc3ca851d2ad995fb6461fa835aa292015"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7fa83b6e08a2f5cb39f88719742082cc3ca851d2ad995fb6461fa835aa292015"
+    sha256 cellar: :any_skip_relocation, monterey:       "36b917c21c782dd67e5116b9556007a9b4db57a0b1e6745b7e2922f2c80b5fd0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "36b917c21c782dd67e5116b9556007a9b4db57a0b1e6745b7e2922f2c80b5fd0"
+    sha256 cellar: :any_skip_relocation, catalina:       "36b917c21c782dd67e5116b9556007a9b4db57a0b1e6745b7e2922f2c80b5fd0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cf3b0ddf366394fe75a9825ad09a15651250a717337cee8624312fe7eccbf817"
   end
 
   depends_on "node"
