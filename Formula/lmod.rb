d6bf0160152c15cf7c28c09d98c41e5a6cb5654e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-26 08:05:22 +0000

    lmod: update 8.6.17 bottle.

--- Formula/lmod.rb
+++ Formula/lmod.rb
@@ -6,12 +6,12 @@ class Lmod < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d8d748f1da26a40b8968bf5cddb085ecf275e1798f8e8327769d89d565dd5153"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c25dcf4bdd1b1012e3292c4b333eb8d8b0f01622510168abde22769042f54efa"
-    sha256 cellar: :any_skip_relocation, monterey:       "0034c1378af17a60907fecd64d05746aa89b32a5d795a6358def8a2d53413af0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e732bc442fedef9cb31dac2c2ec2add3c8594267032f311528017f79c37da064"
-    sha256 cellar: :any_skip_relocation, catalina:       "55fb8a5dd5d671bd69ac2346866a20b8e0b34edc9644a3ca7f450676180b2210"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b99448217a2664673719e733d915258dce309c85be9cfd3841d3e225cd8655ad"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "79f4a85d16eff2e75df1ec955438b9ab3e1f1267716178f0495e7a49f58f3dd2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1ebcd473d921c43eb7793262b534338dc05f32a93a405fe758542220ba794635"
+    sha256 cellar: :any_skip_relocation, monterey:       "b6f28a76160f5a6141ab783d88ec1daf43c9f13d1ffc5acc1d9791e762025981"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ea3744e1ba1afb52662ad378fed35a5e06f18263a799bfb6c92a6dbc9764c263"
+    sha256 cellar: :any_skip_relocation, catalina:       "9db211b999fa30f438e52d133ffc12fffbcd8ea9c4b6216d28cb1094d77d4f0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9b432a9c2f25744ab46eb4b3da744daa4a109f3c79f6bb41a68e021c0a4bbcc"
   end
 
   depends_on "luarocks" => :build
