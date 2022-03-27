24c8f4f00d2bcbc24167ed5528db2cff52e1ef4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-27 12:08:03 +0000

    tflint: update 0.35.0 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -7,12 +7,12 @@ class Tflint < Formula
   head "https://github.com/terraform-linters/tflint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d764cd60856304c5c0b20057c0a391631f45cb4f9b9d1ea2edf5036a10dad3c2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5dbf07a1996733a32c42f1b4f9f21a87e38b3afa0e30d5825f56d9059f2766d8"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c270d5d051d5a40c659cc8c1669c190eb1c91fc4c94f18fff0c821f0dce18c3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8de1f55ec5965dfe07ea46331e8fcf9224cd7e06860a2ec391856ea24338964b"
-    sha256 cellar: :any_skip_relocation, catalina:       "40df6049fdb5e8fc2e7fbcb8343e8ea21c39b9033b45312b1ed6f9d0501ec405"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "74081a60f5dd9d545dd9fbfd5a3d4d8acc05cc21d2f12a19df03dcc615ac32d5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6adbb9521a0edff3c7a0cc4eb8e6796aef138d91efda0dcf8993f06962558868"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7712be10a5035dc5c9ec66406cfed66b4a34d4ecad8762db8409a36cc7266865"
+    sha256 cellar: :any_skip_relocation, monterey:       "11fb9f7a18c876973dfa0108c38ab736ee4b5d189d5859c61f2a2152c4c04647"
+    sha256 cellar: :any_skip_relocation, big_sur:        "39c9104f8dc3bd5c54289119b74c36b90ffcefcfb7ef0d619a9fe5d462a55a60"
+    sha256 cellar: :any_skip_relocation, catalina:       "7b6022ba10225581003ea9c9d07121a15812c2cd63072b71de6002cd47deef1f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "132b774e4917a27039a8cdc599d6bef9ad8022dcf6429c3d5a2bdf22a9a6cae2"
   end
 
   depends_on "go" => :build
