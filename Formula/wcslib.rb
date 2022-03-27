676ce72f000516f27f8cbb8c2b6668b89f43ff2a
Porkepix <porkepix@gmail.com>
2022-03-27 10:50:47 +0200

    wcslib 7.9
    Closes #97906.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -1,8 +1,8 @@
 class Wcslib < Formula
   desc "Library and utilities for the FITS World Coordinate System"
   homepage "https://www.atnf.csiro.au/people/mcalabre/WCS/"
-  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-7.7.tar.bz2"
-  sha256 "a708e6b4e90e2cd6427434715b591bb9c3d4005c99725ec494b8e0bed2de5355"
+  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-7.9.tar.bz2"
+  sha256 "beff8c1f0e8600078813c032d1afcd9cb305c31f567567434824233d582aba58"
   license "GPL-3.0-or-later"
 
   livecheck do
