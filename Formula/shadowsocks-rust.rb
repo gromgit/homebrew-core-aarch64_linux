f01f0bef01bb5d8a1c2d2eae47f7a86a4abcda8c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-03-27 03:33:51 +0000

    shadowsocks-rust: add 1.14.2 bottle.

--- Formula/shadowsocks-rust.rb
+++ Formula/shadowsocks-rust.rb
@@ -6,6 +6,15 @@ class ShadowsocksRust < Formula
   license "MIT"
   head "https://github.com/shadowsocks/shadowsocks-rust.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "604ad0748ece4ac7c9d0c23f316005c73cf7c8e2da64f0dcd3365c58e0f0ea13"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e42ef2d538a53959243634e3fe26a31ed9e54da12a2d310af55ff6920560742e"
+    sha256 cellar: :any_skip_relocation, monterey:       "325aa94568961ffabd71a20a82b6eaa6270bbcadff260a313d9e917d78e107a4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "07aeab5a26c455c857c846064ac9e7a7b067663dd7e94b814a6983a6157a5b8b"
+    sha256 cellar: :any_skip_relocation, catalina:       "ef7e7827093e1c2599a78094c2e73f763aedcc9cb2c9c5ccb82e9eb5486e0233"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5571d2b8ad5181288ff24dcdcc408d9eb9a1ac569d02ec4ae135f7d25545893c"
+  end
+
   depends_on "rust" => :build
 
   def install
