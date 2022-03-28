df26aaa7485dc7b366bb97e46b080a85ddb59ac9
Porkepix <porkepix@gmail.com>
2022-03-28 21:52:26 +0200

    argocd-vault-plugin 1.10.1
    Closes #97975.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argocd-vault-plugin.rb
+++ Formula/argocd-vault-plugin.rb
@@ -2,8 +2,8 @@ class ArgocdVaultPlugin < Formula
   desc "Argo CD plugin to retrieve secrets from Secret Management tools"
   homepage "https://argocd-vault-plugin.readthedocs.io"
   url "https://github.com/IBM/argocd-vault-plugin.git",
-      tag:      "v1.10.0",
-      revision: "2829a416984d4ee4f38e6e76d7a38d9dd4d49dda"
+      tag:      "v1.10.1",
+      revision: "142fc413f4fdf5e69db4f99675df3d9d00317553"
   license "Apache-2.0"
 
   bottle do
