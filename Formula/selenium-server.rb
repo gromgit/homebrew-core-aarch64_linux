24687548a2fef6f84bd78ee3ca2fff72211fb83c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-28 05:30:35 +0000

    selenium-server 4.1.3
    Closes #97949.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/selenium-server.rb
+++ Formula/selenium-server.rb
@@ -1,8 +1,8 @@
 class SeleniumServer < Formula
   desc "Browser automation for testing purposes"
   homepage "https://www.selenium.dev/"
-  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.2.jar"
-  sha256 "9d47fde8932ed567b831a0a1ee725c56b9564723fbc3fec4326fda0caf35c1e3"
+  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.3.jar"
+  sha256 "4bd34eeb04a33edbd5a18d22d71a4d22993e974971911f57c0d3c0e8ffea99ab"
   license "Apache-2.0"
 
   livecheck do
