6f972c8492a7a9c449190953b5ea32a39c7c63e2
Rui Chen <rui@chenrui.dev>
2022-03-28 13:53:04 +0000

    wasm-tools: add 1.0.3 bottle.

--- Formula/wasm-tools.rb
+++ Formula/wasm-tools.rb
@@ -11,6 +11,15 @@ class WasmTools < Formula
     regex(/^wasm-tools[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d2495917b186bf420410f7f60fd6a6edf19fe6522e8b9601c1e908df1b9f277f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0ccf7b6b8ab736664fccb41ca17413b5c425b7a0c8ee6b659859206af0278e3f"
+    sha256 cellar: :any_skip_relocation, monterey:       "6140bc39ff29a773e58da0d89e863bfd514be50005b7bb5604dee85b2543e63e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "620875a7b177ab74bb0ab3088549db62556f349778c10f1e1ab6ba823d28dd58"
+    sha256 cellar: :any_skip_relocation, catalina:       "51d4acb6b032a04d2ddbbc58eed24a3126ae037f707afd10dd178d8a20f6a920"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "81930cc76f585a6bb5c484cad1ddf3c3e21b78e7de8f2e674d54cbb98d8e204a"
+  end
+
   depends_on "rust" => :build
 
   def install
