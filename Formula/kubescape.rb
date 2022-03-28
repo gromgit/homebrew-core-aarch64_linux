68b1a07f66229df57982919fa446416749ab9292
Porkepix <porkepix@gmail.com>
2022-03-28 22:17:48 +0200

    kubescape 2.0.150
    Closes #97983.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubescape.rb
+++ Formula/kubescape.rb
@@ -1,10 +1,9 @@
 class Kubescape < Formula
   desc "Kubernetes testing according to Hardening Guidance by NSA and CISA"
   homepage "https://github.com/armosec/kubescape"
-  url "https://github.com/armosec/kubescape/archive/v2.0.149.tar.gz"
-  sha256 "6a97eab6d41ea65216fa399c676337434783465bedf06ce8827ae1be939fccee"
+  url "https://github.com/armosec/kubescape/archive/v2.0.150.tar.gz"
+  sha256 "af6dcda929ab46087c1e1e0c03d05ea0c63189e3e9af97eddc13fe08752541b8"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/armosec/kubescape.git", branch: "master"
 
   bottle do
@@ -21,9 +20,11 @@ class Kubescape < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/armosec/kubescape/cautils.BuildNumber=v#{version}
+      -X github.com/armosec/kubescape/core/cautils.BuildNumber=v#{version}
     ]
-    system "go", "build", *std_go_args(ldflags: ldflags)
+    cd "cmd" do
+      system "go", "build", *std_go_args(ldflags: ldflags)
+    end
 
     output = Utils.safe_popen_read(bin/"kubescape", "completion", "bash")
     (bash_completion/"kubescape").write output
