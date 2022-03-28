3818f7a4f7f9cf3821c8cdd7d21c6c0256a946dc
Rui Chen <rui@chenrui.dev>
2022-03-28 13:51:37 +0000

    flyctl: add 0.0.307 bottle.

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -7,6 +7,15 @@ class Flyctl < Formula
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2373205aceb730b70e3d60581fc12538384027e601f9c8d87b89e09bbc46ad41"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2373205aceb730b70e3d60581fc12538384027e601f9c8d87b89e09bbc46ad41"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab95cf5f428a113f06050be58bb60a9429475236424c61990ddc8e7d3fe05136"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ab95cf5f428a113f06050be58bb60a9429475236424c61990ddc8e7d3fe05136"
+    sha256 cellar: :any_skip_relocation, catalina:       "ab95cf5f428a113f06050be58bb60a9429475236424c61990ddc8e7d3fe05136"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9b82a08e288dc86eacc280c9d049a0c286894e072eb2ec727e55f8fa7be8a04"
+  end
+
   depends_on "go" => :build
 
   def install
