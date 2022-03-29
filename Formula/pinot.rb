9f5a6644be06c81e4b375e7ffd68fabca67211c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-29 09:10:05 +0000

    pinot: update 0.10.0 bottle.

--- Formula/pinot.rb
+++ Formula/pinot.rb
@@ -7,7 +7,7 @@ class Pinot < Formula
   head "https://github.com/apache/pinot.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5e2a741e6ea7370a8a476c6972fbacecf0a507718335acca4e2769a046382d8a"
+    sha256 cellar: :any_skip_relocation, all: "8323dd5ffe20cd57eb84997675d05ccb927668c7717b37e31a41ec41dc27293a"
   end
 
   depends_on "openjdk"
