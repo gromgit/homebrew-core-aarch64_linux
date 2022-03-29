3ab10dc319d6def3108cf4649d24920a4faaaaa6
Porkepix <porkepix@gmail.com>
2022-03-29 19:04:36 +0200

    oha 0.5.1
    * oha 0.5.1
    * oha: fix url and sha256
    
    Closes #98065.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -1,8 +1,8 @@
 class Oha < Formula
   desc "HTTP load generator, inspired by rakyll/hey with tui animation"
   homepage "https://github.com/hatoo/oha/"
-  url "https://github.com/hatoo/oha/archive/v0.5.0.tar.gz"
-  sha256 "763f44ae37c27d98f0ef32eebdef94b7409f3d25208c152e50af48e4c073256c"
+  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "1879b2e155679241560ce805965bbd7ac5fb1b602115d97b2b31afe8a0964051"
   license "MIT"
   head "https://github.com/hatoo/oha.git", branch: "master"
 
