27749ed7da956c268a2d29311d549bb4837ab927
Michael Cho <cho-m@tuta.io>
2022-03-28 19:33:30 -0700

    librem 2.0.0

--- Formula/librem.rb
+++ Formula/librem.rb
@@ -1,8 +1,8 @@
 class Librem < Formula
   desc "Toolkit library for real-time audio and video processing"
   homepage "https://github.com/baresip/rem"
-  url "https://github.com/baresip/rem/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "bcc91bb521fae183357fb422b00a3981477a22e99d3afe165c4ec50a6bbed9da"
+  url "https://github.com/baresip/rem/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "aa1729c170804667ef4542693cada38608377c8bc257a5f0171e6d4015d6870d"
   license "BSD-3-Clause"
 
   bottle do
