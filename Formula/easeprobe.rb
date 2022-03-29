4b96c79fe7d95825b83a6aea092556cba693c4a2
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-03-29 08:59:53 +0000

    easeprobe: add 1.1.0 bottle.

--- Formula/easeprobe.rb
+++ Formula/easeprobe.rb
@@ -7,6 +7,15 @@ class Easeprobe < Formula
   license "Apache-2.0"
   head "https://github.com/megaease/easeprobe.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3814745c0cfafad66c1c16a4ece8346241be07ebbae955b4be5db3a52f8f3ea8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "afde1e8d74946f9347e31c442c877d19ebb0697b7b7240151a5b2a35c1f625fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "b026c29a03d3274577da184ef0a3c52a4f549eda3a81cabecf0ee5f3de044cb2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "806a03ac0cc089db88a55951f6dae8a731f4941d0e4282bb7143eaa63074c30c"
+    sha256 cellar: :any_skip_relocation, catalina:       "2836cee4c1c9a593f5274a492c3b2ef47881d9a8e9f7620b075063f35b52e001"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a90e9e240f1a0ff60033f2330fa93947d6adef2107c91a3e87f26695733fdf4f"
+  end
+
   depends_on "go" => :build
 
   def install
