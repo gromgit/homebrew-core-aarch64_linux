a571167c7187cf4c1cbcaaa92b2eddc96f898b03
Jenkins CI <noreply@cyberduck.io>
2022-03-29 23:12:07 +0200

    duck 8.3.0.37309
    duck 8.3.0
    
    Closes #98076.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/duck.rb
+++ Formula/duck.rb
@@ -1,8 +1,8 @@
 class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/duck-src-8.2.3.36880.tar.gz"
-  sha256 "1a8b9b983ff21aad75a3ebd2897fd22b62ef8535817a8d4383b6ef1444f1cf65"
+  url "https://dist.duck.sh/duck-src-8.3.0.37309.tar.gz"
+  sha256 "8026494f8b387647bf4208732fa5195488b7c0cadb0b5ef974240ff97b75a1c9"
   license "GPL-3.0-only"
   head "https://github.com/iterate-ch/cyberduck.git", branch: "master"
 
