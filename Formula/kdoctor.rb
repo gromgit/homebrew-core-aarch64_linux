2ad47d094a1069293f26691d4068a48dfe85432c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-29 13:15:58 +0000

    kdoctor: update 0.0.3 bottle.

--- Formula/kdoctor.rb
+++ Formula/kdoctor.rb
@@ -7,10 +7,10 @@ class Kdoctor < Formula
   head "https://github.com/Kotlin/kdoctor.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea987c7db59ea361a50681b7311a28ba3736957a8dbffe52593686903e747a7f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea987c7db59ea361a50681b7311a28ba3736957a8dbffe52593686903e747a7f"
-    sha256 cellar: :any_skip_relocation, monterey:       "d84364629903fae72979bffc6bdf89f31e6d766e6cf88ad9231cdd400f712834"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d84364629903fae72979bffc6bdf89f31e6d766e6cf88ad9231cdd400f712834"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc24bbbd334193ad52837e46c7e0724a25968d0e481970f5e2e127094aa0344d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e6246e75bb09c13406a087261587cdd52fb0c46556ae7f7162cea336a950113a"
+    sha256 cellar: :any_skip_relocation, monterey:       "41f6c721d8a7f721b825aca465ef2c41d6600d1f8a422bb018fb9cfc885a15bf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1da6a21f983709d788aa6cd2563669ab788a5cd9edc1b07dc3f4970177cde17d"
   end
 
   depends_on "gradle" => :build
