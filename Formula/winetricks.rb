96cee1c073fa13d69ce4a15ec8131d7fa53a2025
Porkepix <porkepix@gmail.com>
2022-03-29 18:50:56 +0200

    winetricks 20220328
    Closes #98063.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/winetricks.rb
+++ Formula/winetricks.rb
@@ -1,8 +1,8 @@
 class Winetricks < Formula
   desc "Automatic workarounds for problems in Wine"
   homepage "https://github.com/Winetricks/winetricks"
-  url "https://github.com/Winetricks/winetricks/archive/20210825.tar.gz"
-  sha256 "bac77918ef4d58c6465a1043fd996d09c3ee2c5a07f56ed089c4c65a71881277"
+  url "https://github.com/Winetricks/winetricks/archive/20220328.tar.gz"
+  sha256 "e72543f6ddf1283f8290a11d1698c2f91af3db659cec2bfa0c0933ebbc23d777"
   license "LGPL-2.1-or-later"
   head "https://github.com/Winetricks/winetricks.git", branch: "master"
 
