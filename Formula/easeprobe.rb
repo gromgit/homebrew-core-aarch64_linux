48dbedc4a901fbaef3c532c0509c5d019dafae7f
Rui Chen <rui@chenrui.dev>
2022-03-28 21:02:56 -0400

    easeprobe 1.1.0 (new formula)
    Closes #98014.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/easeprobe.rb
@@ -0,0 +1,41 @@
+class Easeprobe < Formula
+  desc "Simple, standalone, and lightWeight tool that can do health/status checking"
+  homepage "https://github.com/megaease/easeprobe"
+  url "https://github.com/megaease/easeprobe.git",
+      tag:      "v1.1.0",
+      revision: "593a5b714a9fe247c0c38e013dd72598590f0092"
+  license "Apache-2.0"
+  head "https://github.com/megaease/easeprobe.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/megaease/easeprobe/pkg/version.RELEASE=#{version}
+      -X github.com/megaease/easeprobe/pkg/version.COMMIT=#{Utils.git_head}
+      -X github.com/megaease/easeprobe/pkg/version.REPO=megaease/easeprobe
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/easeprobe"
+  end
+
+  test do
+    (testpath/"config.yml").write <<~EOS.chomp
+      http:
+        - name: "brew.sh"
+          url: "https://brew.sh"
+    EOS
+
+    easeprobe_stdout = (testpath/"easeprobe.log")
+
+    pid = fork do
+      $stdout.reopen(easeprobe_stdout)
+      exec bin/"easeprobe", "-f", testpath/"config.yml"
+    end
+    sleep 2
+    assert_match "Ready to monitor(http): brew.sh", easeprobe_stdout.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
