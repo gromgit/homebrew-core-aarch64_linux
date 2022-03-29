1f9f01047fb8881c83d19b29604dd8155e07e87b
Mike McQuaid <mike@mikemcquaid.com>
2022-03-29 16:48:31 +0100

    tepl: fix style
    Needed for https://github.com/Homebrew/brew/pull/12926

--- Formula/tepl.rb
+++ Formula/tepl.rb
@@ -116,9 +116,7 @@ class Tepl < Formula
       -lpango-1.0
       -lpangocairo-1.0
     ]
-    on_macos do
-      flags << "-lintl"
-    end
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
