cd659d0afc4441ea4d410438ac10df4b267e4775
miccal <miccal.matthews@gmail.com>
2022-03-31 05:21:17 +0800

    vim 8.2.4650
    Closes #98184.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.2.4600.tar.gz"
-  sha256 "3a49148998c19946bb4dd78ee764b763647c83203f0b9692a76e13acd1f68288"
+  url "https://github.com/vim/vim/archive/v8.2.4650.tar.gz"
+  sha256 "f91b3bddaeb538a7a2cc3325cbc80bd1b0009a92407b3a8cb29f7618498fa950"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
