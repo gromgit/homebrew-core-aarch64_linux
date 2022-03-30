c48d75b66f81d359be177f8f399784f8da39fb2b
Azeem Shaikh <azeemshaikh38@gmail.com>
2022-03-30 18:56:22 +0000

    scorecard: revision
    * scorecard: update install formula
    * update build/test and revision bump
    
    Closes #98177.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -1,9 +1,11 @@
 class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
-  url "https://github.com/ossf/scorecard/archive/v4.1.0.tar.gz"
-  sha256 "c460ae4cf019dbf7a582c353cf052f14524f187e9fab76132a1af1d674ef8613"
+  url "https://github.com/ossf/scorecard.git",
+      tag:      "v4.1.0",
+      revision: "33f80c93dc79f860d874857c511c4d26d399609d"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
   bottle do
@@ -18,8 +20,15 @@ class Scorecard < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
-    cd("docs/checks/internal/generate") { system "go", "run", "main.go", "../../checks.md" }
+    ldflags = %W[
+      -s -w
+      -X github.com/ossf/scorecard/v4/pkg.gitVersion=#{version}
+      -X github.com/ossf/scorecard/v4/pkg.gitCommit=#{Utils.git_head}
+      -X github.com/ossf/scorecard/v4/pkg.gitTreeState=clean
+      -X github.com/ossf/scorecard/v4/pkg.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+    system "make", "generate-docs"
     doc.install "docs/checks.md"
   end
 
@@ -28,5 +37,7 @@ class Scorecard < Formula
     output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 2)
     expected_output = "InitRepo: repo unreachable: GET https://api.github.com/repos/google/oss-fuzz: 401"
     assert_match expected_output, output
+
+    assert_match version.to_s, shell_output("#{bin}/scorecard version")
   end
 end
