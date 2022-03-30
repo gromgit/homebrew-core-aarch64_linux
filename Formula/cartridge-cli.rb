c5edd122d0944820003cb00b0a749787f3f7cc36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-30 16:10:45 +0000

    cartridge-cli: update 2.12.0 bottle.

--- Formula/cartridge-cli.rb
+++ Formula/cartridge-cli.rb
@@ -7,12 +7,12 @@ class CartridgeCli < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "adea02d0bb82c6edc65e55691de32065999ecf44869d9cd4cd22d6bb814ae9c0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9566c82761596c0efea8b04685247671f48f4e0fad52730731dbdc5877f39e73"
-    sha256 cellar: :any_skip_relocation, monterey:       "5261e98e5b065378a6a913efe99449ebba2949bf439c810cd20a99efc27ef706"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7ac5137cb5498f7bc492b2fdf9aedf77b83ab1099aef9e848771f9fd7979db5d"
-    sha256 cellar: :any_skip_relocation, catalina:       "ca6198294842f852154f96578c3b4895b7158aaf65b3280c5442a51709346660"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aac526e592b74c95030cb027997769a4c1760d416e0deb1691fd1f4e7e62a584"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "97a5095dbf74396e4339f08b8834e08206e14b4f53e800bcf141f67ab7bae070"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d11608d43883c32ac2f82445ae404fb4e470a12d21252f9e455e61d6d0494d43"
+    sha256 cellar: :any_skip_relocation, monterey:       "ae0d202ae2e87ff301d40837a011c434a288845946ac4c501f386dc84d192fbd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "75404c41ea00d59d5b34e940bd66d9bc5001a681ef1f0f5d4cbc6f7303166812"
+    sha256 cellar: :any_skip_relocation, catalina:       "fc8a1f7bf9d7d2d4a6240890dbe005ddce1c9a82a8de96de712e30de902f51b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed9fb0337aaa5754fb1048f87158f45b4579532fda57c3a9379f349cc79f2444"
   end
 
   depends_on "go" => :build
