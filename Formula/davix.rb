522e7f72dec9aed79c290dcda49d1fef76a50720
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-30 21:09:39 +0000

    davix: update 0.8.1 bottle.

--- Formula/davix.rb
+++ Formula/davix.rb
@@ -7,13 +7,12 @@ class Davix < Formula
   head "https://github.com/cern-fts/davix.git", branch: "devel"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "be805f236e6bda40acfbccc744bf856ffdb10bdcf51dc77c4113dc02e8d58de9"
-    sha256 cellar: :any,                 arm64_big_sur:  "c48fc0895374ecf6c509f59fe07306b717fc34b2e5c4ce0773740ce7a5bf392f"
-    sha256 cellar: :any,                 monterey:       "0fe6613982086ecfd30f9d52de7f3262a48ffd57348bd3c7f9d1f9db54ad5e0f"
-    sha256 cellar: :any,                 big_sur:        "4181ed1f39699aace812fdff70a09344abc920c6f4aad9fb61bfb97f3fecbd17"
-    sha256 cellar: :any,                 catalina:       "f5c6cbfd4ed205e39a5401cc890ac0a5c0768e36e9e7a515519278144b0ec95a"
-    sha256 cellar: :any,                 mojave:         "847ba0b1e3227c85215c5eb54a10da5fc9aa231b98059e3e01080f02b5bc9f55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2e6cb42036948a4128c8b0c6cc04ec459375113ab93b81d20d6708dd8baf6a3b"
+    sha256 cellar: :any,                 arm64_monterey: "94b126cc7cd16f3e057e3e53cae4f29ac0c67b33493e7e233088cbf3a031eb08"
+    sha256 cellar: :any,                 arm64_big_sur:  "1352499f99f63e3db5b8bee1c5c33a3c91f01fe0469aba37e730cf345630b8ea"
+    sha256 cellar: :any,                 monterey:       "078b736ba2804b062d528d51c1573ce63650efc171fff6be48bda1930a26880a"
+    sha256 cellar: :any,                 big_sur:        "d7a35a8730e5b829bb96af49ada8ce8e506306d1bc339811054ac16fb6f25faa"
+    sha256 cellar: :any,                 catalina:       "e5e1f1c20c98c6ec7938021b254262689b9a7ede8f948cd9278123b1d8825831"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55792b06f189367e5f8e0c0820234502d059507794e52ea84992764041704236"
   end
 
   depends_on "cmake" => :build
