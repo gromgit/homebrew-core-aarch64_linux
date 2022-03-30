ef44e758a2ea9cc5f4203e0de337b5aef566cd7a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-03-30 06:50:31 +0000

    xrick: update 021212 bottle.

--- Formula/xrick.rb
+++ Formula/xrick.rb
@@ -8,10 +8,12 @@ class Xrick < Formula
 
   bottle do
     rebuild 2
-    sha256 arm64_big_sur: "06c5d5c23d3a83ae82c8efa8303c186cb8b07f2f274b911062c0a56df4207e96"
-    sha256 big_sur:       "07143daa9065782675e5288a66dc2ea8ce418754d4842175b016dbc097b982cd"
-    sha256 catalina:      "67d2a811a5ecd67de860f226fc88feec5ed479ae05fc22e917d6fbe0686be783"
-    sha256 mojave:        "1a2f4d0cc564fd627d83f2b54606c59845b50fb3db04e7a118ca281377b4805a"
+    sha256 arm64_monterey: "7030fd13f0c9ff2779deb3d3c80437d3b9903dd494928fc3aa1237b98598baa0"
+    sha256 arm64_big_sur:  "06c5d5c23d3a83ae82c8efa8303c186cb8b07f2f274b911062c0a56df4207e96"
+    sha256 monterey:       "39454803d7aad7a03e382ffa547f5b8a77f6af1e939f3edcd092e56c493d95fb"
+    sha256 big_sur:        "07143daa9065782675e5288a66dc2ea8ce418754d4842175b016dbc097b982cd"
+    sha256 catalina:       "67d2a811a5ecd67de860f226fc88feec5ed479ae05fc22e917d6fbe0686be783"
+    sha256 mojave:         "1a2f4d0cc564fd627d83f2b54606c59845b50fb3db04e7a118ca281377b4805a"
   end
 
   depends_on "sdl"
