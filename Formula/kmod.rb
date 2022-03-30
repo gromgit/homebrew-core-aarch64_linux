108cef132499e7117a6592d52e26cf5dc2cf43e8
Manuel Rüger <manuel@rueg.eu>
2022-03-30 13:55:36 +0200

    kmod 29 (new formula)
    Closes #97621.
    
    Signed-off-by: Manuel Rüger <manuel@rueg.eu>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/kmod.rb
@@ -0,0 +1,28 @@
+class Kmod < Formula
+  desc "Linux kernel module handling"
+  homepage "https://git.kernel.org/?p=utils/kernel/kmod/kmod.git"
+  url "https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-29.tar.xz"
+  sha256 "0b80eea7aa184ac6fd20cafa2a1fdf290ffecc70869a797079e2cc5c6225a52a"
+  license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
+
+  depends_on :linux
+
+  def install
+    system "./configure", "--with-bashcompletiondir=#{bash_completion}",
+      *std_configure_args, "--disable-silent-rules"
+    system "make"
+    system "make", "install"
+
+    bin.install_symlink "kmod" => "depmod"
+    bin.install_symlink "kmod" => "lsmod"
+    bin.install_symlink "kmod" => "modinfo"
+    bin.install_symlink "kmod" => "insmod"
+    bin.install_symlink "kmod" => "modprobe"
+    bin.install_symlink "kmod" => "rmmod"
+  end
+
+  test do
+    system "#{bin}/kmod", "help"
+    assert_match "Module", shell_output("#{bin}/kmod list")
+  end
+end
