2705e69e13dbfb3218e9b78f5676ecc36f61424b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-30 08:10:04 +0000

    onednn: update 2.6 bottle.

--- Formula/onednn.rb
+++ Formula/onednn.rb
@@ -12,12 +12,12 @@ class Onednn < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "9672c35e99cbdae7e5df778cbe49bd984fc94cc5916d07bff04105e08f33c23f"
-    sha256 cellar: :any,                 arm64_big_sur:  "5de335a07c74904d16dc8e85b293a4da3c977b490ca2fe158b1d2882ad46c8f1"
-    sha256 cellar: :any,                 monterey:       "8a4e3e1a0d75a117652ed071b33c7213749d82099e0b31d03cb2c5153d4a33d5"
-    sha256 cellar: :any,                 big_sur:        "fb659ae283897c68f839e0c99194b6f353ff4e000bfb9f8805dc6b1c2b91d885"
-    sha256 cellar: :any,                 catalina:       "8081692c9939f440bcda139731efebee95072e7ef6ca81c2992a6a6ea154f74c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c5ea66b8325cdc71da8664b875d78d290348cc19633e8c347af959b6fda23119"
+    sha256 cellar: :any,                 arm64_monterey: "db00ab12ff94df6310d42d84c5bca895cc2c693b7c34b40d84dcf26386eb7f46"
+    sha256 cellar: :any,                 arm64_big_sur:  "aa6038dd47c886faa7f3d4440112c9eb6ccc0d9aac020ddb7881196bf2eba23f"
+    sha256 cellar: :any,                 monterey:       "469479abe1b8f7160217dcebe981f890edd1fc742933db9d718d0a180cb64eb7"
+    sha256 cellar: :any,                 big_sur:        "30847804f81e8a9aef031b5ba343d8610e4de5bb0087d411a571740f7260afd2"
+    sha256 cellar: :any,                 catalina:       "a22af2b242cf823267351d0df62717bdec5f852288af58f7e22c339d9460d447"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82dc17714eeee14130bfeb769c8c0c257cd117e42b383fcd185d26e6efe4b170"
   end
 
   depends_on "cmake" => :build
