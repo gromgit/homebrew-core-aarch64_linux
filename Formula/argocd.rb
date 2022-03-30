051150c226b21850ac230ef873885200c4da71c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-30 05:04:27 +0000

    argocd 2.3.3
    Closes #98112.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argocd.rb
+++ Formula/argocd.rb
@@ -2,8 +2,8 @@ class Argocd < Formula
   desc "GitOps Continuous Delivery for Kubernetes"
   homepage "https://argoproj.github.io/cd"
   url "https://github.com/argoproj/argo-cd.git",
-      tag:      "v2.3.2",
-      revision: "ecc2af9dcaa12975e654cde8cbbeaffbb315f75c"
+      tag:      "v2.3.3",
+      revision: "07ac038a8f97a93b401e824550f0505400a8c84e"
   license "Apache-2.0"
 
   bottle do
