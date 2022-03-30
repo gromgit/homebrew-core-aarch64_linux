920b37985c25cb8af71c6954be27da03628997d5
Yurii Kolesnykov <root@yurikoles.com>
2022-03-30 18:26:20 +0300

    spirv-headers 1.3.204.1 (new formula)
    * Add spirv-headers v1.3.204.1
    * spirv-headers: make cmake a test dep
    * spirv-headers: build test
    
    Closes #98159.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/spirv-headers.rb
@@ -0,0 +1,22 @@
+class SpirvHeaders < Formula
+  desc "Headers for SPIR-V"
+  homepage "https://github.com/KhronosGroup/SPIRV-Headers"
+  url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/sdk-1.3.204.1.tar.gz"
+  sha256 "262864053968c217d45b24b89044a7736a32361894743dd6cfe788df258c746c"
+  license "MIT"
+
+  depends_on "cmake" => [:build, :test]
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "example"
+  end
+
+  test do
+    system "cmake", "-S", pkgshare/"example", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+  end
+end
