96c15d4b6f71b5c9a5fbd6f2c11f3d994b6426bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-30 11:09:52 +0000

    composer: update 2.3.0 bottle.

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -11,12 +11,12 @@ class Composer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a2f0ef6df194b3874f9952960a12e83e21e5291ad63040baa0f26bff9fd272b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a2f0ef6df194b3874f9952960a12e83e21e5291ad63040baa0f26bff9fd272b"
-    sha256 cellar: :any_skip_relocation, monterey:       "934fa9823a140d1c5334134810ef3e6e8662ee008207092bcd80c143a1a8c9b0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "934fa9823a140d1c5334134810ef3e6e8662ee008207092bcd80c143a1a8c9b0"
-    sha256 cellar: :any_skip_relocation, catalina:       "934fa9823a140d1c5334134810ef3e6e8662ee008207092bcd80c143a1a8c9b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5a2f0ef6df194b3874f9952960a12e83e21e5291ad63040baa0f26bff9fd272b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a752a129066d7fef5596ca734e82c8def5525ed299659700f16c53d6299f6d8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a752a129066d7fef5596ca734e82c8def5525ed299659700f16c53d6299f6d8"
+    sha256 cellar: :any_skip_relocation, monterey:       "b2e847d900823756198aac270cae405e8e0b92dcf026fbab1193de707263acb1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b2e847d900823756198aac270cae405e8e0b92dcf026fbab1193de707263acb1"
+    sha256 cellar: :any_skip_relocation, catalina:       "b2e847d900823756198aac270cae405e8e0b92dcf026fbab1193de707263acb1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a752a129066d7fef5596ca734e82c8def5525ed299659700f16c53d6299f6d8"
   end
 
   depends_on "php"
