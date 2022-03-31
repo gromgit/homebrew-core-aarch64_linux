6de3741b123f87dd40675e162f41519bc3bda470
owine <owine@users.noreply.github.com>
2022-03-31 09:25:26 -0500

    fontconfig 2.14.0
    * fontconfig 2.14.0
    * fontconfig: Remove patch
    
    Closes #98252.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -1,8 +1,8 @@
 class Fontconfig < Formula
   desc "XML-based font configuration API for X Windows"
   homepage "https://wiki.freedesktop.org/www/Software/fontconfig/"
-  url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.bz2"
-  sha256 "f655dd2a986d7aa97e052261b36aa67b0a64989496361eca8d604e6414006741"
+  url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.14.0.tar.xz"
+  sha256 "dcbeb84c9c74bbfdb133d535fe1c7bedc9f2221a8daf3914b984c44c520e9bac"
   license "MIT"
 
   livecheck do
@@ -43,13 +43,6 @@ class Fontconfig < Formula
     depends_on "gettext" => :build
     depends_on "json-c" => :build
     depends_on "util-linux"
-  end
-
-  # Fix crash issues on arm64.
-  # Remove with the next release.
-  patch do
-    url "https://github.com/freedesktop/fontconfig/commit/6def66164a36eed968aae872d76acfac3173d44a.patch?full_index=1"
-    sha256 "1dbe6247786c75f2b3f5a7e21133a3f9c09189f59fff08b2df7cb15389b0e405"
   end
 
   def install
