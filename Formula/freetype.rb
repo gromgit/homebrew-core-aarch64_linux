3a6f275d06ccac1d4ab7d18e21a5652e1e1390d6
owine <owine@users.noreply.github.com>
2022-03-31 12:05:29 -0500

    freetype 2.12.0
    Closes #98274.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,9 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.11.1/freetype-2.11.1.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.11.1.tar.xz"
-  sha256 "3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.12.0/freetype-2.12.0.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.0.tar.xz"
+  sha256 "ef5c336aacc1a079ff9262d6308d6c2a066dd4d2a905301c4adda9b354399033"
   license "FTL"
 
   livecheck do
