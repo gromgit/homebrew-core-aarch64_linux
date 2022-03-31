343592894f1bbbe832e03e9c290359cf43a02974
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-03-31 15:45:52 +0000

    alembic: update 1.8.3_2 bottle.

--- Formula/alembic.rb
+++ Formula/alembic.rb
@@ -8,12 +8,12 @@ class Alembic < Formula
   head "https://github.com/alembic/alembic.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "20af15e4b2c129acfdc29ece5ab967bf69feb9a581e80e14c19062467f29487c"
-    sha256 cellar: :any,                 arm64_big_sur:  "40bd5931e665dfa050f071522734b0f414b1fad4744e7485cf2426007977fc3f"
-    sha256 cellar: :any,                 monterey:       "dacc5d8d8703ffdc042f0a199b7537634a3921752f8e7ed5202eeac086d8f1b3"
-    sha256 cellar: :any,                 big_sur:        "e7e91332c264c371051de27322c77cacc5513227566129206983c46718230aa4"
-    sha256 cellar: :any,                 catalina:       "f9a875fef0cb0efb4e15e3f054bd9c6e2110677edef22766b92f61fb05c2a549"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a60ba5cb5042b72122e86d346e1964a8d2fc2bcc42dcba1976cda0c5017ef57d"
+    sha256 cellar: :any,                 arm64_monterey: "d6f162ac7821f82d52c9f255160d690a9e87200d9944297725183198cfa12dda"
+    sha256 cellar: :any,                 arm64_big_sur:  "299a3284cb5e8750db7de0658b293689517dd53b43ef2fa30d7a7a925ea17107"
+    sha256 cellar: :any,                 monterey:       "a4837dd18418c4c209cfe7864646137f8c4e2e07c7fb08766ee19e1d5c9c2a8a"
+    sha256 cellar: :any,                 big_sur:        "03119f89473ce48243ee8942d43f189b2f9c5a0d5b3462f03df5b3fc6bbd5e52"
+    sha256 cellar: :any,                 catalina:       "6516fc5e2ed5dd868e8852859e2b48c48036d6eadbc8d55d66142b68d0fc4769"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dacfa0ab6c46714a1f0cb57527d16b816c57ab2a6746e17ba9b322a05b161782"
   end
 
   depends_on "cmake" => :build
