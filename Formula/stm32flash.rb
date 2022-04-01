14ec702f4cce3552f90db8484ea531f4dd2b083b
Rui Chen <rui@chenrui.dev>
2022-04-01 01:14:31 -0400

    stm32flash: add license
    Closes #98309.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stm32flash.rb
+++ Formula/stm32flash.rb
@@ -3,6 +3,7 @@ class Stm32flash < Formula
   homepage "https://sourceforge.net/projects/stm32flash/"
   url "https://downloads.sourceforge.net/project/stm32flash/stm32flash-0.7.tar.gz"
   sha256 "c4c9cd8bec79da63b111d15713ef5cc2cd947deca411d35d6e3065e227dc414a"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
