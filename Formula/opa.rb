1853d3d97b997a0e68d1c11ecd5455da8c0706ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-01 00:14:42 +0000

    opa: update 0.39.0 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -7,12 +7,12 @@ class Opa < Formula
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc22c3963eb12a6aadffb47f3b924b8b81ae9b2a290cb6354a0589f5b2750900"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f46b08f9599e50ec099e0f8c67d9f86dc901ca2668925fc8c6a9156ac9a0a49e"
-    sha256 cellar: :any_skip_relocation, monterey:       "4eef4dc6f8b8c8ab37185acdc280d4e851203055864a7f9d9ab71d8df9bc4e80"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fa04ff7c700e29141b9b6a874cee032134554a5e8f409f5bd1bb10daa3610f1b"
-    sha256 cellar: :any_skip_relocation, catalina:       "0ead3481cdee51be20011c30532da7cc7b6775c370d5ec55de58c98854330a2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "587fbbfd7debdad67e252c98b64e854f521abfb6e4a4d9eab7b8bd9b7ba992a2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aee2eb0d6bee17b8da29f307f88bdeeec1e144bd4baf1a9a2ab9d445e6fb81d8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6748b0b70632f216835bc0d87ed255653302b2e281bc41351a7e73c2539f25d2"
+    sha256 cellar: :any_skip_relocation, monterey:       "ca52dd9795cda215c9c0f0dec5665ead404f27fc941ff642121c0e8159fed124"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6a8885440384120ea9deda007eb161ac68af7e0ddbf5805cb9bf89be4a9b5299"
+    sha256 cellar: :any_skip_relocation, catalina:       "08c4fb85bdb5e7c10ee419ec0775de99337f11360304bfd9e11ed3817143ebde"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4b3dec86f640ad968b6890af1969168b64d62d499af08b90f84745d8a85c4a90"
   end
 
   depends_on "go" => :build
