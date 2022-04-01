3721e95cae932b8a36a13aeaedc3cace80fc27bc
Porkepix <porkepix@gmail.com>
2022-04-01 12:30:48 +0200

    trivy 0.25.0
    Closes #98341.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -1,8 +1,8 @@
 class Trivy < Formula
   desc "Vulnerability scanner for container images, file systems, and Git repos"
   homepage "https://aquasecurity.github.io/trivy/"
-  url "https://github.com/aquasecurity/trivy/archive/v0.24.4.tar.gz"
-  sha256 "3048b3e2009e0a4fc96c12e4162ef106b45d84479e2aadb357971f907ef9d235"
+  url "https://github.com/aquasecurity/trivy/archive/v0.25.0.tar.gz"
+  sha256 "42c956ec41dad926437f099c983d24029713a18a3d3a26706f1db64df8f1dd20"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/trivy.git", branch: "main"
 
