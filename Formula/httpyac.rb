3bf9adede5b63e58524ac98aa0fef92b406e66da
Rui Chen <rui@chenrui.dev>
2022-04-02 19:43:58 -0400

    httpyac 5.2.3 (new formula)
    Closes #98447.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/httpyac.rb
@@ -0,0 +1,53 @@
+require "language/node"
+
+class Httpyac < Formula
+  desc "Quickly and easily send REST, SOAP, GraphQL and gRPC requests"
+  homepage "https://httpyac.github.io/"
+  url "https://registry.npmjs.org/httpyac/-/httpyac-5.2.3.tgz"
+  sha256 "5d5d67a6dc4cdf67415b29f6999174f5138f7e4fc4bca81e4ca0144ff3052782"
+  license "MIT"
+
+  depends_on "node"
+
+  on_linux do
+    depends_on "xsel"
+  end
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir[libexec/"bin/*"]
+
+    clipboardy_fallbacks_dir = libexec/"lib/node_modules/#{name}/node_modules/clipboardy/fallbacks"
+    clipboardy_fallbacks_dir.rmtree # remove pre-built binaries
+    if OS.linux?
+      linux_dir = clipboardy_fallbacks_dir/"linux"
+      linux_dir.mkpath
+      # Replace the vendored pre-built xsel with one we build ourselves
+      ln_sf (Formula["xsel"].opt_bin/"xsel").relative_path_from(linux_dir), linux_dir
+    end
+
+    # Replace universal binaries with their native slices
+    deuniversalize_machos
+  end
+
+  test do
+    (testpath/"test_cases").write <<~EOS
+      GET https://httpbin.org/anything HTTP/1.1
+      Content-Type: text/html
+      Authorization: Bearer token
+
+      # @keepStreaming
+      MQTT tcp://broker.hivemq.com
+      Topic: testtopic/1
+      Topic: testtopic/2
+    EOS
+
+    output = shell_output("#{bin}/httpyac send test_cases --all")
+    # for httpbin call
+    assert_match "HTTP/1.1 200  - OK", output
+    # for mqtt calls
+    assert_match "testtopic/2:  Check with Mazin", output
+
+    assert_match version.to_s, shell_output("#{bin}/httpyac --version")
+  end
+end
