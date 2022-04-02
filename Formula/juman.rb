b7da34d124b8178fe0c94731546af2e23d03a5db
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-04-02 14:55:18 +0000

    juman: update 7.01 bottle.

--- Formula/juman.rb
+++ Formula/juman.rb
@@ -6,14 +6,16 @@ class Juman < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_big_sur: "9b0c1166c946ef258a558961fa82660502d705bbbecf6b8735a805b093802432"
-    sha256 big_sur:       "69ca5acb9395c257b591bd6eedde58c0707929af25b767d470dcb5fef786c054"
-    sha256 catalina:      "0cb4d99f79b907922d8352e841096301a132ab0f385c75910ab53198b1f72ab7"
-    sha256 mojave:        "36bae86cd2b24c5b3b4e75aed31ab0cf5da261b7a77e7ffe8a9b279ca3b801d6"
-    sha256 high_sierra:   "7e2b144bf77ccdb11ae0166827dd45feae62a950de00310dcb863d7f926a9510"
-    sha256 sierra:        "5c1dfea7f62d1afce55c9d1ed2478f9ff3b1744285fbbf08c29eb171cc672fa7"
-    sha256 el_capitan:    "6bd46cdc6ff4e159463f8d4fecda2b803c3054ec28305f3baa1ea4969c4da723"
-    sha256 yosemite:      "b2ccfe90011dead77ca0789cbdcdf30aa24e2ebcd3dd19c8d01b6adacbf7c816"
+    sha256 arm64_monterey: "cf0f825ad7796245c453f7993f4b7f5d069c4e4eb190ee5fcc46f86ad74f61b5"
+    sha256 arm64_big_sur:  "9b0c1166c946ef258a558961fa82660502d705bbbecf6b8735a805b093802432"
+    sha256 monterey:       "b8076e4c5626f942eff9a9e95ef8f06a9a2e013c344b626b2dc7c30756eb64aa"
+    sha256 big_sur:        "69ca5acb9395c257b591bd6eedde58c0707929af25b767d470dcb5fef786c054"
+    sha256 catalina:       "0cb4d99f79b907922d8352e841096301a132ab0f385c75910ab53198b1f72ab7"
+    sha256 mojave:         "36bae86cd2b24c5b3b4e75aed31ab0cf5da261b7a77e7ffe8a9b279ca3b801d6"
+    sha256 high_sierra:    "7e2b144bf77ccdb11ae0166827dd45feae62a950de00310dcb863d7f926a9510"
+    sha256 sierra:         "5c1dfea7f62d1afce55c9d1ed2478f9ff3b1744285fbbf08c29eb171cc672fa7"
+    sha256 el_capitan:     "6bd46cdc6ff4e159463f8d4fecda2b803c3054ec28305f3baa1ea4969c4da723"
+    sha256 yosemite:       "b2ccfe90011dead77ca0789cbdcdf30aa24e2ebcd3dd19c8d01b6adacbf7c816"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
