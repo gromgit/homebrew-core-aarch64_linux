aeac6c72a271f22da84ae9f2b2ee3878a07b5fcc
Michael Cho <cho-m@tuta.io>
2022-04-02 09:49:31 -0700

    ski: update sha256 and fix Linux build
    Closes #98414.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ski.rb
+++ Formula/ski.rb
@@ -1,9 +1,18 @@
 class Ski < Formula
+  include Language::Python::Shebang
+
   desc "Evade the deadly Yeti on your jet-powered skis"
   homepage "http://catb.org/~esr/ski/"
-  url "http://www.catb.org/~esr/ski/ski-6.13.tar.gz"
-  sha256 "34e95547ecfe7b3791df0a81fef9af422ececf49b1aa1e93bbb1ba067ccdc955"
   license "BSD-2-Clause"
+
+  stable do
+    url "http://www.catb.org/~esr/ski/ski-6.13.tar.gz"
+    sha256 "cfbd251ce2052d24f06db3197d11fa0c0028fc4b5c9bc724653b3dfebed7d028"
+
+    # Fix AttributeError: 'str' object has no attribute 'decode'
+    # Issue ref: https://gitlab.com/esr/ski/-/issues/2
+    patch :DATA
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "80da0faf28f34d0440e89fa1761ad91e46be161925f42d6f3cfe6e2f74e718a3"
@@ -14,14 +23,20 @@ class Ski < Formula
   end
 
   head do
-    url "https://gitlab.com/esr/ski.git"
+    url "https://gitlab.com/esr/ski.git", branch: "master"
     depends_on "xmlto" => :build
   end
 
+  uses_from_macos "python"
+
   def install
     if build.head?
-      ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+      ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
       system "make"
+    end
+    if MacOS.version <= :mojave
+      rw_info = OS.mac? ? python_shebang_rewrite_info("/usr/bin/env python") : detected_python_shebang
+      rewrite_shebang rw_info, "ski"
     end
     bin.install "ski"
     man6.install "ski.6"
@@ -31,3 +46,20 @@ class Ski < Formula
     assert_match "Bye!", pipe_output("#{bin}/ski", "")
   end
 end
+
+__END__
+diff --git a/ski b/ski
+index 2fdbb7e..067fe60 100755
+--- a/ski
++++ b/ski
+@@ -489,8 +489,8 @@ if __name__ == "__main__":
+         if color:
+             colordict[ch] = curses.tparm(color, idx)
+         else:
+-            colordict[ch] = ""
+-    reset = (curses.tigetstr("sgr0") or "").decode("ascii")
++            colordict[ch] = b""
++    reset = (curses.tigetstr("sgr0") or b"").decode("ascii")
+     terrain_key = colorize(terrain_key)
+
+     print("SKI!  Version %s.  Type ? for help." % version)
