dccd7c8bcf138a7a9ccda1d556ba70b240051647
Rui Chen <rui@chenrui.dev>
2022-04-02 14:18:26 +0000

    ifacemaker: add 1.1.0 bottle.

--- Formula/ifacemaker.rb
+++ Formula/ifacemaker.rb
@@ -6,6 +6,15 @@ class Ifacemaker < Formula
   license "Apache-2.0"
   head "https://github.com/vburenin/ifacemaker.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fad9412e1b854ee824e7e02094591fa390a91669407bfe918d41273ac9e39b10"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fad9412e1b854ee824e7e02094591fa390a91669407bfe918d41273ac9e39b10"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b7cce651aa5e1078136f44d975b0cb82edadda113c36c169cc02ea8ead0a298"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9b7cce651aa5e1078136f44d975b0cb82edadda113c36c169cc02ea8ead0a298"
+    sha256 cellar: :any_skip_relocation, catalina:       "9b7cce651aa5e1078136f44d975b0cb82edadda113c36c169cc02ea8ead0a298"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b3030a7be7c380c654ef1434fe106932f6c897eab9675c15da34239b3dec0305"
+  end
+
   depends_on "go" => :build
 
   def install
