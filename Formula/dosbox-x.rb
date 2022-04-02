638c9c4f9675422b53ac8e9599383a544b7468de
Porkepix <porkepix@gmail.com>
2022-04-02 15:23:11 +0200

    dosbox-x 0.83.24
    Closes #98404.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,8 +1,8 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "https://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.23.tar.gz"
-  sha256 "026f3986aae61d5f5cc7a95c7ad8ee9646f3249b282c8136a00b239bf6fed711"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.24.tar.gz"
+  sha256 "f4746f1524cac58756123c7acbbc565e215d7f298a19667fd845dbba040c6021"
   license "GPL-2.0-or-later"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git", branch: "master"
