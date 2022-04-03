37be2ac13da085f8cbe67273190eef95df6001c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-03 13:08:33 +0000

    sonobuoy: update 0.56.4 bottle.

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -6,12 +6,12 @@ class Sonobuoy < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5dfb75a03623471ad07da92a8d0d2e375739751415d43c6885e5a67165e6a0bf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8509a78551e03f91cf3f7dec1f878a74b24c999a7fd622c55099f010600c0bd8"
-    sha256 cellar: :any_skip_relocation, monterey:       "b31002f99e0926ad22f455e0c7db2b564350c9b9cd79b6d52b2e8089feebfc67"
-    sha256 cellar: :any_skip_relocation, big_sur:        "027f159e331b4d4ef1c758b1d5ac18972caf0262c14ac243740d31cf076180df"
-    sha256 cellar: :any_skip_relocation, catalina:       "ec2f81142931b130b96f27e873c501155ae9da02dfa104ab30ce094228e00860"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f3ed63fc6bf9c035431ac7dbd743cf378604b1c2f84dfec68eaefc98bc131f7a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "22643fb291902c896897832b0a8dd451ee6f02e9420b3720dc9f45ec224f7352"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e71a07fb9d03d175f0111607cbc0c07bfc1cd663fa6a1a03d69a64333d7b2ded"
+    sha256 cellar: :any_skip_relocation, monterey:       "84ee96c38ba58776fa0a6565195efa38d34d568d0e2d3f259fea692885d17c58"
+    sha256 cellar: :any_skip_relocation, big_sur:        "47977b64f18f2cec3fb0bee24436225083fc5190a27a247a96908dfca62f5ac1"
+    sha256 cellar: :any_skip_relocation, catalina:       "ea127d38edc308d882edc8c227ff58ee85982c5218e113e61ef91af2e58e5965"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9cb85110f52994c7d95b1c44f514e27d2b8095a0568b79b17577bf38cf9d9a8"
   end
 
   # Segfaults on Go 1.18 - try test it again when updating this formula.
