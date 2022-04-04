3bdb761a3c03b3ed5c545ac97bb4f9be53cba462
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-04 18:10:47 +0000

    claws-mail: update 3.19.0 bottle.

--- Formula/claws-mail.rb
+++ Formula/claws-mail.rb
@@ -11,12 +11,11 @@ class ClawsMail < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "5f1a0bc9c1950c7bf5169f927e7f0cc6895e43e6188dafc38a2cf1cbc029f9e4"
-    sha256 arm64_big_sur:  "1e94b0baa4142502cd2f18e4bbdeafe3d73b7d0a5a6c13c16b5a74b6e49a908d"
-    sha256 monterey:       "fe2336c158d68f1b821f1f625c0a2837c5708f84f4594140ec6bec2262b9e29f"
-    sha256 big_sur:        "2c94ba339b902091781d9201c6cd71a2b9dfd401224317ac1f1368d17e44ee03"
-    sha256 catalina:       "e1691b2e1a632759a0c4aef4167265af4edde36d777eb5bfce0310c3235f2b1d"
-    sha256 mojave:         "b279ad0a5d167795deed2bdadafeea2f7fff4dca2b26c580a91c28cdaaa4b1c5"
+    sha256 arm64_monterey: "74b9d0de5a9b8500e2a71ee118bcb5cd72a7901639ff1bccb3f2bcb633362de7"
+    sha256 arm64_big_sur:  "00d742baadda6934b41f056e76741da56021a70f405169d91cfae04cf2052cf6"
+    sha256 monterey:       "f6a71dae91ade0eb99d4ec6b9aeca45c7c9e67a98ecd25b7618ba82ddf152685"
+    sha256 big_sur:        "ff579b37e2968ffc54055e5d8fdc2cd07af57cb26859b4cda388ab4f83597a3b"
+    sha256 catalina:       "fc87978da74bb95b34c5ab7a2a01c478be7087e49f46549c55c3908aa8371623"
   end
 
   depends_on "pkg-config" => :build
