0d6927d1f10b309b38ef1ab04e4ed1755b08188a
Phil Budne <phil@ultimate.com>
2022-04-04 16:40:12 -0400

    snobol4 2.3.1
    Closes #98542.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/snobol4.rb
+++ Formula/snobol4.rb
@@ -1,9 +1,8 @@
 class Snobol4 < Formula
   desc "String oriented and symbolic programming language"
-  homepage "https://www.snobol4.org/"
-  url "ftp://ftp.ultimate.com/snobol/old/snobol4-1.5.tar.gz"
-  mirror "https://src.fedoraproject.org/lookaside/pkgs/snobol/snobol4-1.5.tar.gz/54ac3ddd51fb34ec63b1eb0ae7b99794/snobol4-1.5.tar.gz"
-  sha256 "9f7ec649f2d700a30091af3bbd68db90b916d728200f915b1ba522bcfd0d7abd"
+  homepage "https://www.regressive.org/snobol4/"
+  url "https://ftp.regressive.org/snobol/snobol4-2.3.1.tar.gz"
+  sha256 "91244d67d4e29d2aadce5655bd4382ffab44c624a7ea4ad6411427f3abf17535"
   license "BSD-2-Clause"
 
   bottle do
@@ -19,8 +18,62 @@ class Snobol4 < Formula
     sha256 yosemite:       "6903e1b05a795eae13f2f97fd2f1f4b883b03e1c94ba28e3747b3df98c6a955d"
   end
 
+  depends_on "openssl@1.1"
+
+  uses_from_macos "m4" => :build
+  uses_from_macos "libffi"
+  uses_from_macos "sqlite"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "readline"
+  end
+
   def install
+    ENV.append_to_cflags "-I#{MacOS.sdk_path_if_needed}/usr/include/ffi" if OS.mac?
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make", "install"
+    ENV.deparallelize
+    # avoid running benchmark:
+    system "make", "install_notiming"
+  end
+
+  test do
+    # Verify modules build, test DBM.
+    # NOTE! 1960's language! -include, comment, and labels (fail, end)
+    # must all be in first column
+    testfile = testpath/"test.sno"
+    dbmfile = testpath/"test.dbm"
+    (testpath/"test.sno").write <<~EOS
+      -include 'digest.sno'
+      -include 'dirs.sno'
+      -include 'ezio.sno'
+      -include 'ffi.sno'
+      -include 'fork.sno'
+      -include 'ndbm.sno'
+      -include 'logic.sno'
+      -include 'random.sno'
+      -include 'sprintf.sno'
+      -include 'sqlite3.sno'
+      -include 'stat.sno'
+      -include 'zlib.sno'
+      * DBM test
+              t = 'dbm'
+              k = '🍺'
+              v = '🙂'
+              fn = '#{dbmfile}'
+              h1 = dbm_open(fn, 'cw')         :f(fail)
+              dbm_store(h1, k, v)             :f(fail)
+              dbm_close(h1)                   :f(fail)
+              h2 = dbm_open(fn, 'r')          :f(fail)
+              v2 = dbm_fetch(h2, k)           :f(fail)
+              ident(v, v2)                    :f(fail)
+      * more tests here? (set t to test name before each test)
+	      output = 'OK'
+              :(end)
+      fail    output = t ' test failed at ' &LASTLINE
+              &code = 1
+      end
+    EOS
+    assert_match "OK", shell_output("#{bin}/snobol4 #{testfile}")
   end
 end
