6a337c8940dfd9c5c3b03c5222a0947087148f84
Mike McQuaid <mike@mikemcquaid.com>
2022-04-04 08:05:03 +0000

    difftastic: add 0.24.0 bottle.

--- Formula/difftastic.rb
+++ Formula/difftastic.rb
@@ -5,6 +5,15 @@ class Difftastic < Formula
   sha256 "195bc08b36132b4fea90942243dece209573f6123bd8c60dabaad869e7441489"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0e773ea75d5c13b4d55106976888a6d8126b2a4e09eb0800482fee0d23d52c41"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "500bf6742dd567cf61de824f077ecd2a749ff8c3e15502aa25f5d9b76815d6d2"
+    sha256 cellar: :any_skip_relocation, monterey:       "9dbb5a46a6a2aab7939d8c9bc5821c0ee109194171dcdb31fa8a52e6c9eb0c32"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4084d4863a0028da988d04f61500dc48522e7253c18081c54811b55f08844f20"
+    sha256 cellar: :any_skip_relocation, catalina:       "bd530a59b7f15f60946d47a9d3d5592c59c6c751f0c77eed411addedb8706472"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "baba5779f8a42c5d67d2bca4589a339494982d7575f51b6b0126e18fe15f1d78"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
