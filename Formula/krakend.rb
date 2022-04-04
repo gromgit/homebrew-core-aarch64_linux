7ab0cb1f256e671b25d25360dfd4a7680a63f706
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-04 06:07:49 +0000

    krakend: update 2.0.1 bottle.

--- Formula/krakend.rb
+++ Formula/krakend.rb
@@ -6,12 +6,12 @@ class Krakend < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "66ab126253dca6455754caa44cccc8ce4030df71e96c98f9f2cc9eb183b40846"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "44688e4b5565386a7fb7802027ddcbd4d0e7234aba5da50bc35b10b567781d90"
-    sha256 cellar: :any_skip_relocation, monterey:       "9fe818a27bdd90c98dd43127276c205f2ffa2dc3cbc2f487fe78219e6eb8204c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9741367a11c025db5e35028ca92c47c18d016e55981ff4759e88042aa49d7f52"
-    sha256 cellar: :any_skip_relocation, catalina:       "9d105d4c64a7b0ebb809a5bf8e6f93c0d38307453c5acf8ef1da2cdccf1204f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "01627ad13e5e16854b672413ad74722026afd6071b8dfb3780ffaf969f82a312"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "684183cf0ee8b759dc0cdeda4a1cf62ce715422b446e204b189e8419d0841224"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f766f400d42527adaca881a2e10a02677126e29e8004a7cb7c63343d6335b6c"
+    sha256 cellar: :any_skip_relocation, monterey:       "13f1a476d7ab6d9143ab3286a4789cbd59c708038e9aee274424b417a1449abe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d3b4827433c0ee693da6251e53efb34bd271c6fe35c3f3b27f25aa413eefad91"
+    sha256 cellar: :any_skip_relocation, catalina:       "62024a4f84cd638b0cb6bf795332cbe53faafff0f311805941e5d65c95aa9ac2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "81eba06c4a0d8ab38a58753f8848c9b0e9a8eab281ff2362aa13fe030ca18ea8"
   end
 
   depends_on "go" => :build
