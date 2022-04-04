a4edcaa0dcc4725f0c8d9820955f76d5b37264c5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-04-04 12:34:31 +0000

    dfmt: update 0.14.2 bottle.

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -8,10 +8,12 @@ class Dfmt < Formula
   head "https://github.com/dlang-community/dfmt.git", branch: "v0.x.x"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "b6f7197f2f1580bff0f06ed086ca679c310d87d64ed506ce8deb89fd49a7d514"
-    sha256 cellar: :any_skip_relocation, big_sur:      "e558a7feb8739e2064851d66769050b26b8bab8bf009356a773cda5a39e924e4"
-    sha256 cellar: :any_skip_relocation, catalina:     "3b322f9dbfb2062678b18e6e53719204446315e2fc70a788759505cdc663b839"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "852cdd41ee99f72be5b7e22af09e07a5da3457791016a78ec0038f3172c96120"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2aeb82c327f80c2308b8dfa2572a746853a04bc39b0d04afda767eb01c2a83e7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "366d877bb066339d120387fea51f5145354c3c43a8d0c65b43b887722c59f7eb"
+    sha256 cellar: :any_skip_relocation, monterey:       "b6f7197f2f1580bff0f06ed086ca679c310d87d64ed506ce8deb89fd49a7d514"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e558a7feb8739e2064851d66769050b26b8bab8bf009356a773cda5a39e924e4"
+    sha256 cellar: :any_skip_relocation, catalina:       "3b322f9dbfb2062678b18e6e53719204446315e2fc70a788759505cdc663b839"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "852cdd41ee99f72be5b7e22af09e07a5da3457791016a78ec0038f3172c96120"
   end
 
   on_macos do
