e44bd68d4e5c75572440b6c8642cd63f7806054b
Branch Vincent <branchevincent@gmail.com>
2022-04-04 15:09:04 -0400

    latexindent 3.17.1
    Closes #98525.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/latexindent.rb
+++ Formula/latexindent.rb
@@ -1,8 +1,8 @@
 class Latexindent < Formula
   desc "Add indentation to LaTeX files"
   homepage "https://ctan.org/pkg/latexindent"
-  url "https://github.com/cmhughes/latexindent.pl/archive/V3.17.tar.gz"
-  sha256 "e8ffdc07f2992d44e8ec9c8ac560c7f020fe9eca2e85fb00c654848c40b2431c"
+  url "https://github.com/cmhughes/latexindent.pl/archive/V3.17.1.tar.gz"
+  sha256 "a2f7f0c68b6c86bdcde269a4cb43c02d7e6276b29e0e962d5b0b4b8d5efe75e7"
   license "GPL-3.0-or-later"
 
   bottle do
