8bd662c916a4374fa6555f7afd1717144d8db3ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-05 20:05:23 +0000

    nest: update 3.3_1 bottle.

--- Formula/nest.rb
+++ Formula/nest.rb
@@ -7,12 +7,12 @@ class Nest < Formula
   revision 1
 
   bottle do
-    sha256 arm64_monterey: "bd86e86c4725591e48faa03a59f2bc31d3bc91eca1dff6445a586cbf0592c62d"
-    sha256 arm64_big_sur:  "03bc734c832fd24a0f2a46554c6c1b04f21c832861a2a478a946e6323a0847e5"
-    sha256 monterey:       "2e726432e90deff4137df825c2996840f83557a679c979e7bb93763bc85bb5f9"
-    sha256 big_sur:        "c2e1d71a36d08f0a89536f62743850c35118df7c6529cf8b285b9526eda90be8"
-    sha256 catalina:       "f77781c41103a4cabdf5b7323761b70451ba0ced67d4266babf7b371b92aa0cf"
-    sha256 x86_64_linux:   "250502b46ccc825980322c4b7511c520fe636d2ed46a3561e929529dbc35888f"
+    sha256 arm64_monterey: "4173ccaf82e72b43f9633c541579dd73965fee0de632881e3a27689277f4d7f5"
+    sha256 arm64_big_sur:  "6f2a955de6185ab1da109a32ffac7fb6f99e8b41776e963b67bb8d67f5fe3974"
+    sha256 monterey:       "c7ecebf829cc51d4028e539adc31bf5521094f06bf5c8dc63100c056bb002037"
+    sha256 big_sur:        "dd7a521295ac1a4fd79b7eb1849eaa4362fd5cb543803793cf10397df0501b5f"
+    sha256 catalina:       "648ea3fcf3cf6ac164b5fc880208b7ed671e07af1f81760256f5895357e79bea"
+    sha256 x86_64_linux:   "fa76ea85f9a48e0faa0e7620a39e7621f795dc33f3623f8a0d6d5b7577567e6b"
   end
 
   depends_on "cmake" => :build
