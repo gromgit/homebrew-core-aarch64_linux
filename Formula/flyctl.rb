f83005e730d274cc1090310b4e219fc30c6f2000
Michael Dwan <michaeldwan@users.noreply.github.com>
2022-04-05 08:48:45 +0000

    flyctl 0.0.314
    Closes #98574.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.313",
-      revision: "13e33505653977110a8a7021ac0bafc90bb40556"
+      tag:      "v0.0.314",
+      revision: "b05764043e5c998df56a19432f5a09daf2010805"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
