3b2acd57eb90554f9d813448d898bed3f54c2454
Michael Cho <cho-m@tuta.io>
2022-04-05 10:02:55 -0700

    groovy: fix groovysh on Java 18 and Apple Silicon
    Closes #98612.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/groovy.rb
+++ Formula/groovy.rb
@@ -4,6 +4,7 @@ class Groovy < Formula
   url "https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-4.0.1.zip"
   sha256 "8ac4a0ccbdc69db3ff17cf20a43dafacc6396f031666f13ff2e7bc6e376e4497"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/"
@@ -16,15 +17,50 @@ class Groovy < Formula
 
   depends_on "openjdk"
 
+  on_macos do
+    # Temporary build dependencies for compiling jansi-native
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "maven" => :build
+
+    # jansi-native is used to build native binary to support Apple Silicon.
+    # Source version is from jline-2.14.6 -> jansi-1.12 -> jansi-native-1.6
+    # TODO: Remove once updated to jline-3.x: https://issues.apache.org/jira/browse/GROOVY-8162
+    resource "jansi-native" do
+      url "https://github.com/fusesource/jansi-native/archive/refs/tags/jansi-native-1.6.tar.gz"
+      sha256 "f4075ad012c9ed79eaa8d3240d869e10d94ca8b130f3e7dac2ba3978dce0fb21"
+
+      # Update pom.xml to replace unsupported Java 6 source and to disable universal binary
+      patch :DATA
+    end
+  end
+
   conflicts_with "groovysdk", because: "both install the same binaries"
 
   def install
+    if OS.mac?
+      jline_jar = buildpath/"lib/jline-2.14.6.jar"
+      resource("jansi-native").stage do
+        system "mvn", "-Dplatform=osx", "prepare-package"
+        system "zip", "-d", jline_jar, "META-INF/native/*"
+        system "jar", "-uvf", jline_jar,
+                      "-C", "target/generated-sources/hawtjni/lib",
+                      "META-INF/native/osx64/libjansi.jnilib"
+      end
+    end
+
     # Don't need Windows files.
     rm_f Dir["bin/*.bat"]
 
     libexec.install "bin", "conf", "lib"
     bin.install Dir["#{libexec}/bin/*"] - ["#{libexec}/bin/groovy.ico"]
-    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+    env = Language::Java.overridable_java_home_env
+    # Work around for exception starting `groovysh` on OpenJDK 18:
+    # java.lang.UnsupportedOperationException: The Security Manager is deprecated ...
+    # TODO: Remove when groovy no longer uses deprecated Security Manager.
+    env["JAVA_OPTS"] = "$JAVA_OPTS -Djava.security.manager=allow"
+    bin.env_script_all_files libexec/"bin", env
   end
 
   def caveats
@@ -35,6 +71,61 @@ class Groovy < Formula
   end
 
   test do
-    system "#{bin}/grape", "install", "org.activiti", "activiti-engine", "5.16.4"
+    output = shell_output("#{bin}/grape install org.activiti activiti-engine 5.16.4")
+    assert_match "found org.activiti#activiti-engine;5.16.4", output
+    assert_match "65536\n===> null\n", pipe_output("#{bin}/groovysh", "println 64*1024\n:exit\n")
   end
 end
+
+__END__
+diff --git a/pom.xml b/pom.xml
+index 369cc8c..6dbac6f 100644
+--- a/pom.xml
++++ b/pom.xml
+@@ -151,8 +151,8 @@
+         <groupId>org.apache.maven.plugins</groupId>
+         <artifactId>maven-compiler-plugin</artifactId>
+         <configuration>
+-          <source>1.5</source>
+-          <target>1.5</target>
++          <source>1.7</source>
++          <target>1.7</target>
+         </configuration>
+       </plugin>
+       
+@@ -306,35 +306,5 @@
+       </build>
+     </profile>
+     
+-
+-    <!-- Profile which enables Universal binaries on OS X -->
+-    <profile>
+-      <id>mac</id>
+-      <activation>
+-        <os><family>mac</family></os>
+-      </activation>
+-      <build>
+-        <plugins>
+-          <plugin>
+-            <groupId>org.fusesource.hawtjni</groupId>
+-            <artifactId>maven-hawtjni-plugin</artifactId>
+-            <configuration>
+-              <osgiPlatforms>
+-                <osgiPlatform>osname=MacOS;processor=x86-64</osgiPlatform>
+-                <osgiPlatform>osname=MacOS;processor=x86</osgiPlatform>
+-                <osgiPlatform>osname=MacOS;processor=PowerPC</osgiPlatform>
+-                <osgiPlatform>*</osgiPlatform>
+-              </osgiPlatforms>
+-              <configureArgs>
+-                <arg>--with-universal</arg>
+-              </configureArgs>
+-              <platform>osx</platform>
+-            </configuration>
+-          </plugin>
+-        </plugins>
+-      </build>
+-    </profile>
+-    
+-    
+   </profiles>
+ </project>
