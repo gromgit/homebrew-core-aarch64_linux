47d3d7f993893a2102f96e668288634df167e01a
Porkepix <porkepix@gmail.com>
2022-04-05 18:40:42 +0200

    wasm-tools 1.0.4
    * wasm-tools 1.0.4
    * update test
    * update test
    
    Closes #98610.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wasm-tools.rb
+++ Formula/wasm-tools.rb
@@ -1,8 +1,8 @@
 class WasmTools < Formula
   desc "Low level tooling for WebAssembly in Rust"
   homepage "https://github.com/bytecodealliance/wasm-tools"
-  url "https://github.com/bytecodealliance/wasm-tools/archive/refs/tags/wasm-tools-1.0.3.tar.gz"
-  sha256 "95684121c3f8ab8de5b46b754a5093a675d56cbfd90fafb23c344f1865964c3d"
+  url "https://github.com/bytecodealliance/wasm-tools/archive/refs/tags/wasm-tools-1.0.4.tar.gz"
+  sha256 "5ca1ebf67de418d7b953dd9d67620fa1b90d1828321953744a1f69abfecac5ec"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/bytecodealliance/wasm-tools.git", branch: "main"
 
@@ -37,8 +37,10 @@ class WasmTools < Formula
         (func (;0;) (type 0) (param i32 i32) (result i32)
           local.get 0
           local.get 1
-          i32.add)
-        (export "sum" (func 0)))
+          i32.add
+        )
+        (export "sum" (func 0))
+      )
     EOS
     assert_equal expected, shell_output("#{bin}/wasm-tools print #{testpath}/sum.wasm")
   end
