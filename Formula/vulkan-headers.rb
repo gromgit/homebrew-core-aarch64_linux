f88776b962963a0e6e744842f11314bcdc090cc7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-05 18:05:24 +0000

    vulkan-headers: update 1.3.211 bottle.

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -6,7 +6,7 @@ class VulkanHeaders < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4df81649b7179f85347c3bdc371ca1618ac379a8dc50a201c22bae4366c02381"
+    sha256 cellar: :any_skip_relocation, all: "d446551d340b9c76ee21d35e1a044a4a3f98a782988affa00c6c95870ff9f38d"
   end
 
   depends_on "cmake" => :build
