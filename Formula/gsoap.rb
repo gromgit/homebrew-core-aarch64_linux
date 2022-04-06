e0f7c1b26d73b1849a1f5f5ac7eb7ab4ee3a9b7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-06 06:07:13 +0000

    gsoap: update 2.8.121 bottle.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -11,12 +11,12 @@ class Gsoap < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e58bdaca820e0e52871d19f654abd0a5affab9216f3701243f7788d3104f07da"
-    sha256 arm64_big_sur:  "a4012ba9470960ce9c711e9c65cb9c29b72ac1dc83b3b1f2ab12bcfe4c57e846"
-    sha256 monterey:       "34cf51a748e4e9209af9624ade47a7d1109af16cb61f80539ca9a2e39b871891"
-    sha256 big_sur:        "0f78a3f1220528590766c8a81b08fbf7b7b8302a348a1d1659593596bcde8fa0"
-    sha256 catalina:       "ea4244f8503a5f3faa3df546b682d7cfe683cfc8ee8d8dbc93213134cde02de4"
-    sha256 x86_64_linux:   "9b242046579d639fc3b3ac5933e2bc9645146b4ab2d5ac6d40b64776d3b78ac5"
+    sha256 arm64_monterey: "6c9763446f8b08409976e6c588f87be2f8849889a3e8602953c5785351bd6a0e"
+    sha256 arm64_big_sur:  "87fda0d44082a6c1e33f06d619c129d533805d58da44f9519e5735a824a5a6b8"
+    sha256 monterey:       "748387f41ec885ef826bae8e3bd1c9c5650345b003c4d2c03d9c2446911bb36c"
+    sha256 big_sur:        "1ed9ba760001cf9fb5c28ac118f8efcc57d3819b26c0fc54e01356c7906ab36a"
+    sha256 catalina:       "4481fc46d1b241cce9f5cf30024e2eb6b386a7d3dfe52e7d2bf40132094bb017"
+    sha256 x86_64_linux:   "a4774948dcd4b77d5c97193962565c7b25b5b8143415d89076ccedb540936f2e"
   end
 
   depends_on "autoconf" => :build
