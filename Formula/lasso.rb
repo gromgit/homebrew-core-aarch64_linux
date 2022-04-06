72f0b0216a23d5046c7367b0c0144b1fe030f5a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-06 19:04:26 +0000

    lasso: update 2.8.0 bottle.

--- Formula/lasso.rb
+++ Formula/lasso.rb
@@ -11,12 +11,13 @@ class Lasso < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8943a75c665721e4bdb0a8b0d078cbc466fb15b3f9d51e31af01d62cd795e090"
-    sha256 cellar: :any,                 arm64_big_sur:  "30e41c13e7fbfeab7f94bf316a430c986e191c14d92b6166dac9cdcdd88d54e5"
-    sha256 cellar: :any,                 monterey:       "0bb3ae919005771283aacb36ab7ba373ac2495401006bd9e561b54e6152a73a6"
-    sha256 cellar: :any,                 big_sur:        "1f9f21862e2f5aea3ae37b396ca58b8d09b002e1d44fb961603b78d7e79d7f52"
-    sha256 cellar: :any,                 catalina:       "1a5b3eb78b0bb7606fa7a0cdf03c9f75555f9eacb129db91d8f2fc33d59240eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d69063c5d7572e5dd764a461dc4f73277cbe156ea6a540b3597ae7c34931878"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "1234da53fe66054f3b22628b155ddad4b701e039aef6f648d4233ed0fbba0177"
+    sha256 cellar: :any,                 arm64_big_sur:  "41c8f358f24567d8e813a30362c67e14a66ae7534746aaad23f5b36eaa1c35e6"
+    sha256 cellar: :any,                 monterey:       "24604ecf58d01e58194a75e57c69237ecf85cceaf544b314cd4a59d4fd5e4f77"
+    sha256 cellar: :any,                 big_sur:        "8a395f2aa86ef1a5f22f23b3bc4b7fbca51e108b0c3afbea149e731932d59033"
+    sha256 cellar: :any,                 catalina:       "149148a36bada2068998128bdccb2887149cb8bbf8da23398546f734a1a8e03b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5818abda0bcd05f97c6bc342142623a13e70f3b0ae4fe036703b3d2e944f354"
   end
 
   depends_on "pkg-config" => :build
