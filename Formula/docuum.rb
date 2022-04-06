60ef08978df32401a1f28779c06631ac57b2c927
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-06 06:07:42 +0000

    docuum: update 0.21.0 bottle.

--- Formula/docuum.rb
+++ Formula/docuum.rb
@@ -6,12 +6,12 @@ class Docuum < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "522dfc02c0e048df75890eb180260c029bbd2697b98b6e58f1a508000c5afd44"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ba50a8305769c48caad3e9bbddaf5cbd158bb08ee0b1c0523a8d037e15bde6b"
-    sha256 cellar: :any_skip_relocation, monterey:       "601f6974757e348ba9993f070bae3eca308f9d51462c72b80a866458bc78fabf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "31f94906755b535e2f7ced1370823ef9398a050bb6ad7b3b8c5eaa75f1ab4e86"
-    sha256 cellar: :any_skip_relocation, catalina:       "4ac55b613e69442098752f2e8928397320842bb2541dab968a35a8ec39b4403e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f4f68d38631516ce33424792b0cf638548df0d192534a508202b6ece3220728a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff164cce77b7b3e28551d9206dc52d91748ad53f169720232ae83957edee49f0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8fc4b1e4baad7e4cb583d5c85bbad65d61e7f4d6f437ce1b4e83a29652bd1c36"
+    sha256 cellar: :any_skip_relocation, monterey:       "a87c1b976646cf155b6953787f3cc74f4643dd5d69648b065f7df9e69f825722"
+    sha256 cellar: :any_skip_relocation, big_sur:        "995b7ea0e07deb7d6f4723b7515d3845c4e96fad2e3ad72fb73aad663add02dd"
+    sha256 cellar: :any_skip_relocation, catalina:       "77b0a7af44208651f7bfcec8d8a171512c5994c65ed05c6463c851898072753e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7494bf54032c56cd8604f7af576b2976ac054f0b6e0e1b2b87dafe1497caef51"
   end
 
   depends_on "rust" => :build
