ff1681c07f5a3306d56365bc06ba49ce7c43e8e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-06 20:06:30 +0000

    opencv: update 4.5.5_1 bottle.

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -12,12 +12,12 @@ class Opencv < Formula
   end
 
   bottle do
-    sha256                               arm64_monterey: "c30513f997a7b463842be5723dc3524b403ae2e8851c4d6572c3554a9f2ac0b9"
-    sha256                               arm64_big_sur:  "3340b75c1daa8c1fc969d40b68b39437b4061d36f54db11c8f566e420e890328"
-    sha256                               monterey:       "b7034490e2db15c8c73efc32550e9ba9e759787e8bf5b30c355ea6c47540579d"
-    sha256                               big_sur:        "5ab8654c831a1ad5225dabed141ee4a0f6d945c70a0cec677e4ab320d33f0fa1"
-    sha256                               catalina:       "2df7d2874b1669736a0b89fabdc35b3bea0a0572e1cb3eee5fe7cd8a95459ad8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0dbcd7860f347972dcedcd46ed2f3d40568aa10b94bffe90c31abc4a50064b2"
+    sha256                               arm64_monterey: "28cea8c7cac2f622cd815e04d9f2dc9cc106132b8654b6beda587943d793423b"
+    sha256                               arm64_big_sur:  "67f05da33b4db4c23b105fcdc4515b30b170fd17d664bc0a6d1511a45b366406"
+    sha256                               monterey:       "664889fb0be611c14d4f8d3ca42749c5fc0a2c6868b1f4b1d55fcd4d5744cb3e"
+    sha256                               big_sur:        "54d0d7a916a9b6bbf35d7fb139c782b41ece399e45e03bcc612e08fc76840135"
+    sha256                               catalina:       "cfa752c37366ad6688d1a53324dee7468ac3356aad2b9a5ad270016303420611"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06cd8a8c81a838096ff13acbabec437652a0dfdcb2277dd36dff6da738fa719b"
   end
 
   depends_on "cmake" => :build
