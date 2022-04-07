948794881418a8ff8288730ea6ab61ef6454a25e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-07 21:05:03 +0000

    flyctl: update 0.0.316 bottle.

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -8,12 +8,12 @@ class Flyctl < Formula
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d1999bfd876b0e0a91ea1d6b2ed119fc6cb2c74164b2a49df4194f193b9946b8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d1999bfd876b0e0a91ea1d6b2ed119fc6cb2c74164b2a49df4194f193b9946b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "fa6af4c0d27c11dbcec9a5efa10b804030cd6930cc4ef7aaae8b5ed3749a0d0c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fa6af4c0d27c11dbcec9a5efa10b804030cd6930cc4ef7aaae8b5ed3749a0d0c"
-    sha256 cellar: :any_skip_relocation, catalina:       "fa6af4c0d27c11dbcec9a5efa10b804030cd6930cc4ef7aaae8b5ed3749a0d0c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6aff23b0e2038c211ff2ea604d18254f857f0a4fb7d4e2a018c4f4cabb69f8b6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb870802df5acf376c8e63e0cf0204d145ccf1ac6346cd666f508ff1fac1e11f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fb870802df5acf376c8e63e0cf0204d145ccf1ac6346cd666f508ff1fac1e11f"
+    sha256 cellar: :any_skip_relocation, monterey:       "e33c8c1fae8169bfdb5faef218005b5e6464ef438af7a71e3cfaa4d14ac074f8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e33c8c1fae8169bfdb5faef218005b5e6464ef438af7a71e3cfaa4d14ac074f8"
+    sha256 cellar: :any_skip_relocation, catalina:       "e33c8c1fae8169bfdb5faef218005b5e6464ef438af7a71e3cfaa4d14ac074f8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "385544428ae2d0bf7c713bf1efd10f92ac9eab2c306419425b501ef0c60394cc"
   end
 
   depends_on "go" => :build
