571d86f5f94058f35ece091ee4ec08e5179094f1
Porkepix <porkepix@gmail.com>
2022-04-07 14:18:51 +0200

    iconsur 1.7.0
    Closes #98745.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/iconsur.rb
+++ Formula/iconsur.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Iconsur < Formula
   desc "macOS Big Sur Adaptive Icon Generator"
   homepage "https://github.com/rikumi/iconsur"
-  url "https://registry.npmjs.org/iconsur/-/iconsur-1.6.2.tgz"
-  sha256 "86d0f47221ec638c7157e0857f9e4648f9fee89de443b94c252bd7661747bf35"
+  url "https://registry.npmjs.org/iconsur/-/iconsur-1.7.0.tgz"
+  sha256 "d732df6bbcaf1418c6f46f9148002cbc1243814692c1c0e5c0cebfcff001c4a1"
   license "MIT"
 
   bottle do
