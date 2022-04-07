675830afa1cd9d913cad46056aa1b01ef6ac7057
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-07 18:05:19 +0000

    gost: update 2.11.2 bottle.

--- Formula/gost.rb
+++ Formula/gost.rb
@@ -7,14 +7,12 @@ class Gost < Formula
   head "https://github.com/ginuerzh/gost.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "df106492b26740e17ae7036416ca7cb2ca737117e8a7115d1e12f9d14c790baf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f90ea396927ac88ed90a566a8c3c76b7f794886508d8721500f23a6e89c68379"
-    sha256 cellar: :any_skip_relocation, monterey:       "925fb92a9096c77ff50d147517e0eb484089f18d24d2f980394e4e0a38d8de5e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7622f651be0def2be1b80cf0a290f0b42d87deee3107488ae0a13ba4a0cf6799"
-    sha256 cellar: :any_skip_relocation, catalina:       "4669c79b11e368446e14667c237c710b05e69b2be23a699fe7e4d9355765c063"
-    sha256 cellar: :any_skip_relocation, mojave:         "fe984a0b5d0323ddb2306a078265b246c751c0dc08d0f14cbe0ddb0c00293f13"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "6db1c66d9a848ea55930dfb17fc0da9ec64a3e053631a29667026d58a34c2246"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32d9a2df2bee7fb06e9da9ba7d7e24cd13463b5c868fee95564d4783351b6f3f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5112d60ba5a6090f3f3b36d0c59b6400cd24ea07bf03c186de395372b3d0862"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3fcdb66e5d378771ffb01b848990ab32cdc5447597f5b075f1373023ead4aa72"
+    sha256 cellar: :any_skip_relocation, monterey:       "a803beb44f47f36f821f1eb0aa15892d9f95e20431150107c531f3d74fb9d464"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b4e97c302058c91679e66f481745d7361045ccf85919eefe3aa8b282d3555dff"
+    sha256 cellar: :any_skip_relocation, catalina:       "d032da856ce53e033dea8c91142e8f218b55fb35f3d6e515038fd1256f2431d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cf7813ce42f66f176ddc7f37aa5db4d466f38d831389bb3e13fe8b591c73c1bb"
   end
 
   # Bump to 1.18 on the next release, if possible.
