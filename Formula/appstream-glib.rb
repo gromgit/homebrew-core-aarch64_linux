92cf525af26bd5f7205a789c1322515b976f0346
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-04-07 02:22:25 +0000

    appstream-glib: update 0.7.18_1 bottle.

--- Formula/appstream-glib.rb
+++ Formula/appstream-glib.rb
@@ -8,11 +8,12 @@ class AppstreamGlib < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "7389aa5d3dc05124ddab77a39fcde0b5b44c74b4ef6bb283f652cfc1f1de5175"
-    sha256 cellar: :any, monterey:      "53220b9e712d6a473fd09d9a82df67c7d7b93b9eb171978f0e8180055166ebb5"
-    sha256 cellar: :any, big_sur:       "1aded5fd6345ce3337cea5141bded4945f481392bf8686532f211d8b5bb2a777"
-    sha256 cellar: :any, catalina:      "2382482db7b4ef8f7b3682014498eb1e49ec626a88d9baa6e3bc71d9ad23c13b"
-    sha256               x86_64_linux:  "c9a65c92ae6b80096abf132de339721f0c5e709310105fa47313b1c2d6b0eed1"
+    sha256 cellar: :any, arm64_monterey: "8e8bc17a4e8db65f27b2021ec044bed07fe3230cf7418669b245b855e40f3a52"
+    sha256 cellar: :any, arm64_big_sur:  "7389aa5d3dc05124ddab77a39fcde0b5b44c74b4ef6bb283f652cfc1f1de5175"
+    sha256 cellar: :any, monterey:       "53220b9e712d6a473fd09d9a82df67c7d7b93b9eb171978f0e8180055166ebb5"
+    sha256 cellar: :any, big_sur:        "1aded5fd6345ce3337cea5141bded4945f481392bf8686532f211d8b5bb2a777"
+    sha256 cellar: :any, catalina:       "2382482db7b4ef8f7b3682014498eb1e49ec626a88d9baa6e3bc71d9ad23c13b"
+    sha256               x86_64_linux:   "c9a65c92ae6b80096abf132de339721f0c5e709310105fa47313b1c2d6b0eed1"
   end
 
   depends_on "docbook" => :build
