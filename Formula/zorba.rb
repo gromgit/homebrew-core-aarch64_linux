3b22da5fdd2753e12e5e35fcd3cc544e53a4e576
Bo Anderson <mail@boanderson.me>
2022-04-08 21:19:02 +0100

    zorba: revision bump for icu4c
    Closes #98809.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/zorba.rb
+++ Formula/zorba.rb
@@ -4,7 +4,7 @@ class Zorba < Formula
   url "https://github.com/28msec/zorba/archive/3.1.tar.gz"
   sha256 "05eed935c0ff3626934a5a70724a42410fd93bc96aba1fa4821736210c7f1dd8"
   license "Apache-2.0"
-  revision 15
+  revision 16
 
   bottle do
     rebuild 1
