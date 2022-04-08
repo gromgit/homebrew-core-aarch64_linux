77ada6ff5c64f94f55ca27898e3ced65ef22a3d9
Bo Anderson <mail@boanderson.me>
2022-04-08 21:06:58 +0100

    libical: revision bump for icu4c

--- Formula/libical.rb
+++ Formula/libical.rb
@@ -4,7 +4,7 @@ class Libical < Formula
   url "https://github.com/libical/libical/releases/download/v3.0.14/libical-3.0.14.tar.gz"
   sha256 "4284b780356f1dc6a01f16083e7b836e63d3815e27ed0eaaad684712357ccc8f"
   license any_of: ["LGPL-2.1-or-later", "MPL-2.0"]
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "c27b4e5e0183e2a3c00df25be49016a32eabefe48df38495607cbe77cd1c4488"
