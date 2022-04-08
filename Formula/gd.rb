58ad84840544f8598267d9e12976056aecf62075
Caleb Xu <calebcenter@live.com>
2022-04-08 01:22:19 -0400

    gd: revision bump for libavif update
    Closes #98747.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gd.rb
+++ Formula/gd.rb
@@ -4,7 +4,7 @@ class Gd < Formula
   url "https://github.com/libgd/libgd/releases/download/gd-2.3.3/libgd-2.3.3.tar.xz"
   sha256 "3fe822ece20796060af63b7c60acb151e5844204d289da0ce08f8fdf131e5a61"
   license :cannot_represent
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
