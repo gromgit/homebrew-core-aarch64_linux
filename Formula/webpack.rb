6bb687199709b9cfed362efc9581e0ef24917acd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-08 09:10:05 +0000

    webpack: update 5.72.0 bottle.

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -10,12 +10,12 @@ class Webpack < Formula
   head "https://github.com/webpack/webpack.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "60502e2a16df036cb99977f2b74ac031fbf0046f58d7170ee54080eadf17aa4b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "60502e2a16df036cb99977f2b74ac031fbf0046f58d7170ee54080eadf17aa4b"
-    sha256 cellar: :any_skip_relocation, monterey:       "6379809cddbbe0231f6991acd6f75be38fa7b76576187ca7fc8ba33f368e354d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6379809cddbbe0231f6991acd6f75be38fa7b76576187ca7fc8ba33f368e354d"
-    sha256 cellar: :any_skip_relocation, catalina:       "6379809cddbbe0231f6991acd6f75be38fa7b76576187ca7fc8ba33f368e354d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32f74b0690782c870c92a6e79f28aef2642e2b7b4770a8a6d0aef947ea14cbec"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9fa9116caefa2cf8e0d9f2c8569decb340555a073ef950cad5a8da1f28a62e6f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9fa9116caefa2cf8e0d9f2c8569decb340555a073ef950cad5a8da1f28a62e6f"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a89e48db42178eeadecb5eebcb0fe004e8265c84aa21fb6430e7d99e7d05f38"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8a89e48db42178eeadecb5eebcb0fe004e8265c84aa21fb6430e7d99e7d05f38"
+    sha256 cellar: :any_skip_relocation, catalina:       "8a89e48db42178eeadecb5eebcb0fe004e8265c84aa21fb6430e7d99e7d05f38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0286ce5115ede0b64b7cc6395f5a539447b8faf28fc8e228af6ee6e1dc440e1d"
   end
 
   depends_on "node"
