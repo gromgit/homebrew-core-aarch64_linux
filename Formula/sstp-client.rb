72b236cd3bd73e1867a8465b5d7b0c58144b7436
eivind.naess <eivind.naess@netmotionsoftware.com>
2022-04-08 16:08:56 -0700

    sstp-client 1.0.17
    * Updating sstp-client to 1.0.17, and linking with OpenSSL 3
    * Missing ,
    * Forget the OpenSSL 3.0 upgrade
    
    Closes #98843.
    
    Signed-off-by: eivind.naess <eivind.naess@netmotionsoftware.com>
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sstp-client.rb
+++ Formula/sstp-client.rb
@@ -1,8 +1,8 @@
 class SstpClient < Formula
   desc "SSTP (Microsofts Remote Access Solution for PPP over SSL) client"
   homepage "https://sstp-client.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/sstp-client/sstp-client/sstp-client-1.0.16.tar.gz"
-  sha256 "afbe14fd122f7875f4fad0ed982d1745136d2e66cc36b97f72ef1a1111784ca1"
+  url "https://downloads.sourceforge.net/project/sstp-client/sstp-client/sstp-client-1.0.17.tar.gz"
+  sha256 "29dd3b9c7111ad6983cd663d5a2f069e1f8a95a913aabc8e166970146657925d"
   license "GPL-2.0-or-later"
   version_scheme 1
 
