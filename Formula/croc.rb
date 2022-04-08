119c2d4ea3f8a35a589e3f25d658e1f3b586413d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-08 02:34:12 +0000

    croc: update 9.5.3 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -7,12 +7,12 @@ class Croc < Formula
   head "https://github.com/schollz/croc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f03e4ec0fbeda621b33ac5b1957133cedb22406bc85ebbfb2098357c45315cfb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f732d2872f368826327114b7c2e02695b574b92a41d8bab95f0149793029584"
-    sha256 cellar: :any_skip_relocation, monterey:       "8ce8a08c906e8c32484c7168622a834855c3b3341e303e624e1ef68bd53c8fd2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b09dc4d7fd14cd6b17c7ffef809b5e8bc1a39fb839e95937addae2ee6b197968"
-    sha256 cellar: :any_skip_relocation, catalina:       "f523bad5c0c56567301de7800e8a5aba8a364adeafc92a59d094736ca8520315"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a60285fde61871383133ccd28a24deec5b59311f20095a88a495bb220b90953e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd1c5a7e60e900ec42d5a3969f5a5b80a6409a746537c5067f488beeaa6bb9da"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8f8fe3dbe43c8aa9bc60b3f0b402f9f1894085ce891beb912921af14d0293b3d"
+    sha256 cellar: :any_skip_relocation, monterey:       "ae27eb240baab52ca87c7f04d8a1c70cfb0cae947df83af919063230cb120686"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ccf3aea10715b34b1d0cfded1551c271c8c6b65719620c36c615d816f32af186"
+    sha256 cellar: :any_skip_relocation, catalina:       "546303d2121496a3e1e291ec915c247c177124c9577145b7dfcd954c1c85a030"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb59391edcd8bf6a045f4d467e9d7a993b7b361ca25563ed166eb94dc604bf4c"
   end
 
   depends_on "go" => :build
