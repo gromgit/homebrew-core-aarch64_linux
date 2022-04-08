190efe4540e22230fe46ba95f4503a7b3d98b7fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-08 05:33:10 +0000

    picard-tools 2.27.1
    Closes #98800.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.27.0/picard.jar"
-  sha256 "0354d10f0a65614e1a0f15a05717529a4792ad96bccb57b541a90246c8c0ad82"
+  url "https://github.com/broadinstitute/picard/releases/download/2.27.1/picard.jar"
+  sha256 "87e94cebeeeaaa19dd2a5ab2d76b9ac51b007fbe5926294f9bb83b6811513152"
   license "MIT"
 
   livecheck do
