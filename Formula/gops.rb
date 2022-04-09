d44eb34026ee5bd5a9c5e67e0b40869127a3ea80
Rui Chen <rui@chenrui.dev>
2022-04-09 17:58:48 +0000

    gops: add 0.3.22 bottle.

--- Formula/gops.rb
+++ Formula/gops.rb
@@ -6,6 +6,15 @@ class Gops < Formula
   license "BSD-3-Clause"
   head "https://github.com/google/gops.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5531dfba3e026925dd13fbcece02096a54e2ffb4179db48c0f5deb87d53923be"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa0e48e32124e96c5cd3a46d905a5f9009fe5ad7a2177c0ab82cd5c6f8b18927"
+    sha256 cellar: :any_skip_relocation, monterey:       "73a426fd2c973c1032b1da9e3492ccf04846be94af5eb471702e63addcd5b5ed"
+    sha256 cellar: :any_skip_relocation, big_sur:        "259cd5b92827f5b6ef3594a3beee292a5bc8c72bdba2d3c6fe3eb574205898a1"
+    sha256 cellar: :any_skip_relocation, catalina:       "b73deb2fd695acb47f4de3b159a648230f0dc5f6e5816a9c1b76faa4919ce8f8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82c7a434378ec9aad1f1c80c4cca5f98d9a357a969dadf0604ba85c285ad86e4"
+  end
+
   depends_on "go" => [:build, :test]
 
   def install
