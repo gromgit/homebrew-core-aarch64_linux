5689a05640ea2c3388252b3d2a25fdf309678c2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-09 05:08:03 +0000

    chamber 2.10.9
    Closes #98877.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chamber.rb
+++ Formula/chamber.rb
@@ -1,8 +1,8 @@
 class Chamber < Formula
   desc "CLI for managing secrets through AWS SSM Parameter Store"
   homepage "https://github.com/segmentio/chamber"
-  url "https://github.com/segmentio/chamber/archive/v2.10.8.tar.gz"
-  sha256 "3d6cd696438994c029e9ff6130baf8ea7fdd32aa17a4c9e88e5c4c05cbb71409"
+  url "https://github.com/segmentio/chamber/archive/v2.10.9.tar.gz"
+  sha256 "4c78a56bd99258335b9ecb1ca70eaa4d7a7b0956463130ecb395ac9301f68f82"
   license "MIT"
   head "https://github.com/segmentio/chamber.git", branch: "master"
 
