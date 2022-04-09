3a3497249ab829d5cedbf9e2606147a3855e62df
Yurii Kolesnykov <root@yurikoles.com>
2022-04-09 21:15:59 +0300

    pdf2djvu: revision
    * pdf2djvu: revision bump (poppler 22.04.0)
    * pdf2djvu: add patches for compatibility with poppler-22.04.0
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/pdf2djvu.rb
+++ Formula/pdf2djvu.rb
@@ -4,7 +4,7 @@ class Pdf2djvu < Formula
   url "https://github.com/jwilk/pdf2djvu/releases/download/0.9.18.2/pdf2djvu-0.9.18.2.tar.xz"
   sha256 "9ea03f21d841a336808d89d65015713c0785e7295a6559d77771dc795333a9fa"
   license "GPL-2.0-only"
-  revision 1
+  revision 2
   head "https://github.com/jwilk/pdf2djvu.git", branch: "master"
 
   bottle do
@@ -27,6 +27,25 @@ class Pdf2djvu < Formula
   end
 
   fails_with gcc: "5" # poppler compiles with GCC
+
+  # poppler 22.04.0 compatibility, remove in next release
+  patch do
+    url "https://github.com/jwilk/pdf2djvu/commit/e170ad557d5f13daeeac047dfaa79347bbe5062f.patch?full_index=1"
+    sha256 "424c4fe330e01d9fbf33eb7bce638ea6d3788e1e1b8b3932364257631c867d8a"
+  end
+
+  # poppler 22.04.0 compatibility, remove in next release
+  # TODO: switch to main repo after this PR is merged: https://github.com/Homebrew/formula-patches/pull/858
+  patch do
+    url "https://raw.githubusercontent.com/yurikoles/formula-patches/4c18443/pdf2djvu/main-use-pdf-link-Destination-copy-constructor.patch"
+    sha256 "3d1e9fc0d7f2861142721befff9e33b5c2665dacc78ac12b613af7b1e6fb0ce6"
+  end
+
+  # poppler 22.04.0 compatibility, remove in next release
+  patch do
+    url "https://github.com/jwilk/pdf2djvu/commit/956fedc7e0831126b9006efedad5519c14201c52.patch?full_index=1"
+    sha256 "4ab8d3ff7f8474b86949d4acf23da6f6ac77303e94feeb849fb3298358efd23f"
+  end
 
   def install
     ENV.append "CXXFLAGS", "-std=gnu++17" # poppler uses std::optional
