09dbdb9ac925cb24cefab9f8aaf49f70df708d40
Rui Chen <rui@chenrui.dev>
2022-04-09 02:11:21 +0000

    httpyac: add 5.2.3 bottle.

--- Formula/httpyac.rb
+++ Formula/httpyac.rb
@@ -7,6 +7,15 @@ class Httpyac < Formula
   sha256 "5d5d67a6dc4cdf67415b29f6999174f5138f7e4fc4bca81e4ca0144ff3052782"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f2fd4455b1f48333696759cfe3689c8becee20abdb9b2c37a696ec3e640cd84d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f2fd4455b1f48333696759cfe3689c8becee20abdb9b2c37a696ec3e640cd84d"
+    sha256 cellar: :any_skip_relocation, monterey:       "41cb4d68692c7fb704d67d6f271c0a133097d884f112e1e267409b0a346b2e39"
+    sha256 cellar: :any_skip_relocation, big_sur:        "41cb4d68692c7fb704d67d6f271c0a133097d884f112e1e267409b0a346b2e39"
+    sha256 cellar: :any_skip_relocation, catalina:       "41cb4d68692c7fb704d67d6f271c0a133097d884f112e1e267409b0a346b2e39"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1fdc7a7a16d9b7f595874c34b6d2ac6b08c9a31bf6803639be4df25ca108bacc"
+  end
+
   depends_on "node"
 
   on_linux do
