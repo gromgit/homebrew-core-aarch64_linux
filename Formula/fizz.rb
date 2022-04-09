1d52f9dcd4312b3c60aca5f74ff3b28c9bc8a7cb
Yurii Kolesnykov <root@yurikoles.com>
2022-04-09 19:40:41 +0300

    fizz: revision bump (glog 0.6.0)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/fizz.rb
+++ Formula/fizz.rb
@@ -4,6 +4,7 @@ class Fizz < Formula
   url "https://github.com/facebookincubator/fizz/releases/download/v2022.03.21.00/fizz-v2022.03.21.00.tar.gz"
   sha256 "b2bb1f303ecb801ef691f8e63aea861d36ccfe17e6bdaa8160748639be6141b9"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
   bottle do
