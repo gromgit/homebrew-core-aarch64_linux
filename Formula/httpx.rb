30630e87e461fc954f8ed82af69ca1069ef389e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-09 10:05:06 +0000

    httpx: update 1.2.1 bottle.

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -7,12 +7,12 @@ class Httpx < Formula
   head "https://github.com/projectdiscovery/httpx.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "98851efed9baef9f0adeb339b6f9308e2f7f11bc9e6b702c22938b5e5f99fec7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e79859079b920f030243af6f2d9332895d7745879fd3963ab262ed09f44ea9f4"
-    sha256 cellar: :any_skip_relocation, monterey:       "4cd92a1f94d8befcf8cf84fd76fc3ee5e950b7c6ad0997f4071b78b73a5f4837"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fcc78ce8d87134dd5d2381e5a3d87c076003f76b8d6b878cd20d9fc93888fdd9"
-    sha256 cellar: :any_skip_relocation, catalina:       "faf1d4f61ca079acc894be34a1c0ad5943a76678bdeb24e473bf4a624486b7c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c65ceaa745489f84ae7d98883085679052068d0702c6fbb70e6e86722893be3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5b2b32168c9b5aef0ed8a27e72d2f1cabd4586b74a40e71f4a502cb789b0e2a1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0e39c0b080a3d53fdee619dd88956f3935a50742e363513e4c434f2d12d20753"
+    sha256 cellar: :any_skip_relocation, monterey:       "714125d9f3ef91afe6b875bcbe3a6df33465a8d4226016c4839d152c5a4b610a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0b2bbbd43c199b79f9e51257dc8d47bdc0c161088f9108f6782b1667d61a868c"
+    sha256 cellar: :any_skip_relocation, catalina:       "00f93faf2ce86fd8a084d78382972335c1f590d8b87ff42218dbe6f1dde89105"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca1884650508a9cb517d060ab0359821b7f8b10a7338f65e4f7a06aece7a5a18"
   end
 
   depends_on "go" => :build
