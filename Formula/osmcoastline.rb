4848d067d3ba15e970a6a560967a1a5f65dff929
Yurii Kolesnykov <root@yurikoles.com>
2022-04-09 21:19:05 +0300

    osmcoastline: revision bump (poppler 22.04.0)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -4,7 +4,7 @@ class Osmcoastline < Formula
   url "https://github.com/osmcode/osmcoastline/archive/v2.3.1.tar.gz"
   sha256 "ab4a94b9bc5a5ab37b14ac4e9cbdf113d5fcf2d5a040a4eed958ffbc6cc1aa63"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "eafdd4eb3ac17b514218a3c67b8f4562ada4b7cb2b8db9985619c4357855477e"
