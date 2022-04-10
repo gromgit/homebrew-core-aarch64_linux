14166e49b51db0bb8bffa94b6040c174193900a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-10 05:04:51 +0000

    brev: update 0.6.44 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -6,12 +6,12 @@ class Brev < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "01c96298fe35a26aacc1c2f2870d4e087ef578d35aeba6c2bc078ee55739ea46"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6efdec7e07bf1d9e3df6fb6b04f22b9d234b9f72d87645b3f99c5b2aff768f86"
-    sha256 cellar: :any_skip_relocation, monterey:       "686c75e53bfe295cc0b24f1e704ed30d60afa1df322174e77a901156deb15f60"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f3488d1b4797053e06107a4aa92cf4979cfc13eafb6ef10965ab3038a9110da1"
-    sha256 cellar: :any_skip_relocation, catalina:       "9e9d83bc9af58d3ca21efe2882fef5f1e3ef7c79f77361bfaf0f95f612eea3e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "774cdac18eb5d03a8a0e41de916312dd9e0e7e9ea348c3fe7123dae3acf3a288"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e74f927c300aaf307b7db7daae79a1ed58ba08b3ad295654853b20b3db266bac"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3dd5bcab40ec7911a7ab39b9b17db224cbc86aeb91c22f88166533cf90e8b061"
+    sha256 cellar: :any_skip_relocation, monterey:       "c655b04ccb951641f84d5c4cce2097f92059e4d178cab40710cffb1193ea5566"
+    sha256 cellar: :any_skip_relocation, big_sur:        "90b5b50ebf2e1c894abeb5aa9a112bb29af79d6e308193fbba8f07181e7492fb"
+    sha256 cellar: :any_skip_relocation, catalina:       "7b013c366c49156370c6f762f595130052a6f7eb01a66d6500e8bdc824aa4014"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3d6d23454cd7a3a56759a6a43c227ae0340ebfda55b15dbd6de51b4e6da8faaa"
   end
 
   depends_on "go" => :build
