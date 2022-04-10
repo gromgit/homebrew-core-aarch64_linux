47fc208a009aec4b006e420fb4eb50f44daa23db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-10 05:04:41 +0000

    ipfs: update 0.12.2 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -16,12 +16,12 @@ class Ipfs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b05d86148b5d741f53cdbb8658cd2b6d42b47afea9a69ded9d5b082bd50e1724"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5c4b8b4bfa9461256ff2c7218c40916e9513250afb5a1c70707e91abaa9cbf26"
-    sha256 cellar: :any_skip_relocation, monterey:       "572f8d61c77b1d01e0f4adbe419b920669a8db46955ac98af684606234b0f01e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5eca26d4afc1bf304562bf71c71b5d39da4d9c4396563bab302d3d726f40e274"
-    sha256 cellar: :any_skip_relocation, catalina:       "ffa21879fa0c12be217b84991d27c1cb09ee5a234e3e7e531b788be7f7d5a478"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "630453b5bb5748ed4d8d1d1b8561980f14f75c7b1c65f583594468c61efe86e0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7b096d6f28b2c338b27ae4bc89e2d5d948f70530ec03aef8d623b5162bf3f33c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66a04fee70fd546ed59d1b0889f726e72d307e7cb75485e97e5226fc7b080d3a"
+    sha256 cellar: :any_skip_relocation, monterey:       "f40b8ac2e2ff38e4be021c6f220851abf93a0f52be3294230531cafa979a9041"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c252151aea93f3d1338a52fe20bef3d7b02489bced42fea4c00e0d6c15b3db0f"
+    sha256 cellar: :any_skip_relocation, catalina:       "6101a28948da2d09cdebdfeedabb17ab59e999b3677b327ed95822bed2cc0e9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdd652dd1d3e09ddfc815feaa00039a03468da60739a41957f5c5b073afc5b37"
   end
 
   # Bump to 1.18 on the next release, if possible.
