e0f96c808e001db5f163e61e3990b78d82362cb4
Rui Chen <rui@chenrui.dev>
2022-04-10 18:54:40 -0400

    pwnat 0.3.0
    Closes #99014.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pwnat.rb
+++ Formula/pwnat.rb
@@ -1,9 +1,9 @@
 class Pwnat < Formula
   desc "Proxy server that works behind a NAT"
   homepage "https://samy.pl/pwnat/"
-  url "https://samy.pl/pwnat/pwnat-0.3-beta.tgz"
-  sha256 "d5d6ea14f1cf0d52e4f946be5c3630d6440f8389e7467c0117d1fe33b9d130a2"
-  license "GPL-3.0"
+  url "https://github.com/samyk/pwnat/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "c784ac0ef2249ae5b314a95ff5049f16c253c1f9b3720f3f88c50fc811140b44"
+  license "GPL-3.0-or-later"
   head "https://github.com/samyk/pwnat.git", branch: "master"
 
   bottle do
@@ -23,9 +23,10 @@ class Pwnat < Formula
   def install
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "LDFLAGS=-lz"
     bin.install "pwnat"
+    man1.install "manpage.txt" => "pwnat.1"
   end
 
   test do
-    shell_output("#{bin}/pwnat -h", 1)
+    assert_match "pwnat <-s | -c> <args>", shell_output("#{bin}/pwnat -h", 1)
   end
 end
