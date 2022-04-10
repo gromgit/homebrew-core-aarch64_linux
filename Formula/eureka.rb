d19e1a7c31e3952eed808d68dabf4d9bb73c8ab6
Rui Chen <rui@chenrui.dev>
2022-04-10 15:31:15 -0400

    eureka 2.0.0
    update test
    
    Closes #98969.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/eureka.rb
+++ Formula/eureka.rb
@@ -1,8 +1,8 @@
 class Eureka < Formula
   desc "CLI tool to input and store your ideas without leaving the terminal"
   homepage "https://github.com/simeg/eureka"
-  url "https://github.com/simeg/eureka/archive/v1.8.1.tar.gz"
-  sha256 "d10d412c71dea51b4973c3ded5de1503a4c5de8751be5050de989ac08eb0455e"
+  url "https://github.com/simeg/eureka/archive/v2.0.0.tar.gz"
+  sha256 "e874549e1447ee849543828f49c4c1657f7e6cfe787deea13d44241666d4aaa0"
   license "MIT"
   head "https://github.com/simeg/eureka.git", branch: "master"
 
@@ -31,12 +31,12 @@ class Eureka < Formula
   end
 
   test do
-    assert_match "eureka [FLAGS]", shell_output("#{bin}/eureka --help 2>&1")
+    assert_match "eureka [OPTIONS]", shell_output("#{bin}/eureka --help 2>&1")
 
     (testpath/".eureka/repo_path").write <<~EOS
       homebrew
     EOS
 
-    assert_match "homebrew/README.md: No such file or directory", pipe_output("#{bin}/eureka --view 2>&1")
+    assert_match "ERROR eureka > No such file or directory", pipe_output("#{bin}/eureka --view 2>&1")
   end
 end
