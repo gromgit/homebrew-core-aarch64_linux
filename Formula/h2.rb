6c0329f4a395c1f81f154843ff117989ffe67c55
Rui Chen <rui@chenrui.dev>
2022-04-10 16:18:54 -0400

    h2 2.1.212
    Closes #98986.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/h2.rb
+++ Formula/h2.rb
@@ -1,9 +1,9 @@
 class H2 < Formula
   desc "Java SQL database"
   homepage "https://www.h2database.com/"
-  url "https://github.com/h2database/h2database/releases/download/version-2.1.210/h2-2022-01-17.zip"
-  version "2.1.210"
-  sha256 "f3a82b598526270b60ea60d439e21bd290bacc2d18d262512bb9b37748055691"
+  url "https://github.com/h2database/h2database/releases/download/version-2.1.212/h2-2022-04-09.zip"
+  version "2.1.212"
+  sha256 "013cc5765d1033edc8e792024aed7d91b169a7f3b2c52c6dc78a866a0a761f57"
   license "MPL-2.0"
 
   bottle do
