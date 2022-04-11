0f913ca90f134e29418e39d06320d66efd3b70b3
Rui Chen <rui@chenrui.dev>
2022-04-11 19:39:00 -0400

    werf 1.2.87 (new formula)
    make it macos only first
    
    update tags
    
    Closes #99080.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/werf.rb
@@ -0,0 +1,60 @@
+class Werf < Formula
+  desc "Consistent delivery tool for Kubernetes"
+  homepage "https://werf.io/"
+  url "https://github.com/werf/werf/archive/refs/tags/v1.2.87.tar.gz"
+  sha256 "f008304325e96e6c7f28f68d49ffcc6e36ba26631d5a6bcd808c1b15d1d7c0d1"
+  license "Apache-2.0"
+  head "https://github.com/werf/werf.git", branch: "main"
+
+  depends_on "go" => :build
+  # due to missing libbtrfs headers, only supports macos at the moment
+  depends_on :macos
+
+  def install
+    ldflags = "-s -w -X github.com/werf/werf/pkg/werf.Version=#{version}"
+    tags = "dfrunmount dfssh containers_image_openpgp"
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "-tags", tags, "./cmd/werf"
+
+    bash_output = Utils.safe_popen_read(bin/"werf", "completion", "bash")
+    (bash_completion/"werf").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"werf", "completion", "zsh")
+    (zsh_completion/"_werf").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"werf", "completion", "fish")
+    (fish_completion/"werf.fish").write fish_output
+  end
+
+  test do
+    werf_config = testpath/"werf.yaml"
+    werf_config.write <<~EOS
+      configVersion: 1
+      project: quickstart-application
+      ---
+      image: vote
+      dockerfile: Dockerfile
+      context: vote
+      ---
+      image: result
+      dockerfile: Dockerfile
+      context: result
+      ---
+      image: worker
+      dockerfile: Dockerfile
+      context: worker
+    EOS
+
+    output = <<~EOS
+      - image: vote
+      - image: result
+      - image: worker
+    EOS
+
+    system "git", "init"
+    system "git", "add", werf_config
+    system "git", "commit", "-m", "Initial commit"
+
+    assert_equal output, shell_output("#{bin}/werf config graph")
+
+    assert_match version.to_s, shell_output("#{bin}/werf version")
+  end
+end
