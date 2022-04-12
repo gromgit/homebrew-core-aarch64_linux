7c3e8688c13c172803f974dab9e85b2d82193da0
danielnachun <daniel.nachun@gmail.com>
2022-04-12 16:41:43 -0700

    hashlink: fix build on Linux
    Closes #99237.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hashlink.rb
+++ Formula/hashlink.rb
@@ -23,8 +23,15 @@ class Hashlink < Formula
   depends_on "openal-soft"
   depends_on "sdl2"
 
+  on_linux do
+    depends_on "mesa"
+    depends_on "mesa-glu"
+  end
+
   def install
     inreplace "Makefile", /\$\{LFLAGS\}/, "${LFLAGS} ${EXTRA_LFLAGS}" unless build.head?
+    # On Linux, also pass EXTRA_FLAGS to LIBFLAGS, so that the linker will also add the RPATH to .hdll files.
+    inreplace "Makefile", "LIBFLAGS =", "LIBFLAGS = ${EXTRA_LFLAGS}"
     system "make", "EXTRA_LFLAGS=-Wl,-rpath,#{libexec}/lib"
     system "make", "install", "PREFIX=#{libexec}"
     bin.install_symlink Dir[libexec/"bin/*"]
@@ -62,13 +69,20 @@ class Hashlink < Formula
     system "#{haxebin}/haxelib", "install", "hashlink"
 
     system "#{haxebin}/haxe", "-hl", "HelloWorld/main.c", "-main", "HelloWorld"
-    system ENV.cc, "-O3", "-std=c11", "-IHelloWorld", "-I#{libexec}/include", "-L#{libexec}/lib", "-lhl",
-                   "HelloWorld/main.c", "-o", "build/HelloWorld"
+
+    flags = %W[
+      -I#{libexec}/include
+      -L#{libexec}/lib
+    ]
+    flags << "-Wl,-rpath,#{libexec}/lib" unless OS.mac?
+
+    system ENV.cc, "HelloWorld/main.c", "-O3", "-std=c11", "-IHelloWorld",
+                   *flags, "-lhl", "-o", "build/HelloWorld"
     assert_equal "Hello world!\n", `./build/HelloWorld`
 
     system "#{haxebin}/haxe", "-hl", "TestHttps/main.c", "-main", "TestHttps"
-    system ENV.cc, "-O3", "-std=c11", "-ITestHttps", "-I#{libexec}/include", "-L#{libexec}/lib", "-lhl",
-                   "TestHttps/main.c", "-o", "build/TestHttps", libexec/"lib/ssl.hdll"
+    system ENV.cc, "TestHttps/main.c", "-O3", "-std=c11", "-ITestHttps",
+                   *flags, "-lhl", "-o", "build/TestHttps", libexec/"lib/ssl.hdll"
     assert_equal "200\n", `./build/TestHttps`
   end
 end
