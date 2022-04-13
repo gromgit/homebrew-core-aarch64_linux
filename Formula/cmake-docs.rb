2a5d175d836be0bb860c73250d62fc3778675f19
Yurii Kolesnykov <root@yurikoles.com>
2022-04-13 11:35:20 +0300

    cmake-docs 3.23.1
    * cmake-docs 3.23.1, remove useless mirror
    * cmake-doc return removed mirror
    
    Closes #99165.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cmake-docs.rb
+++ Formula/cmake-docs.rb
@@ -1,10 +1,10 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.23.0/cmake-3.23.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.23.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.0.tar.gz"
-  sha256 "5ab0a12f702f44013be7e19534cd9094d65cc9fe7b2cd0f8c9e5318e0fe4ac82"
+  url "https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.23.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.1.tar.gz"
+  sha256 "33fd10a8ec687a4d0d5b42473f10459bb92b3ae7def2b745dc10b192760869f3"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
