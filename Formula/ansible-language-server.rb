008d18c8112fe8c63315453b0298e424ef647a5e
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-13 15:13:19 +0200

    ansible-language-server 0.6.0
    New formula for ansible-language-server 0.6.0. Based on existing formula
    for yaml-language-server.
    
    Closes #99170.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/ansible-language-server.rb
@@ -0,0 +1,38 @@
+require "language/node"
+
+class AnsibleLanguageServer < Formula
+  desc "Language Server for Ansible Files"
+  homepage "https://github.com/ansible/ansible-language-server"
+  url "https://registry.npmjs.org/@ansible/ansible-language-server/-/ansible-language-server-0.6.0.tgz"
+  sha256 "388e1610726a8715955caa10afb4a926b9cf748779916ae1ea1d368c2ca1eb2f"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/ansible-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
