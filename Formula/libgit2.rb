ab2f2e61f4cc3ca2f6d8f19def48afacb42e2dfb
Porkepix <porkepix@gmail.com>
2022-04-13 17:05:22 +0200

    libgit2 1.4.3

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -1,10 +1,9 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v1.4.2.tar.gz"
-  sha256 "901c2b4492976b86477569502a41c31b274b69adc177149c02099ea88404ef19"
+  url "https://github.com/libgit2/libgit2/archive/v1.4.3.tar.gz"
+  sha256 "f48b961e463a9e4e7e7e58b21a0fb5a9b2a1d24d9ba4d15870a0c9b8ad965163"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/libgit2/libgit2.git", branch: "main"
 
   livecheck do
