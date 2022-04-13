464b74d483b5d164aac1e6990ccfe822bc3b004c
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2022-04-13 12:09:41 +0800

    minio 20220412065535
    Closes #99143.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -2,9 +2,9 @@ class Minio < Formula
   desc "High Performance, Kubernetes Native Object Storage"
   homepage "https://min.io"
   url "https://github.com/minio/minio.git",
-      tag:      "RELEASE.2022-04-09T15-09-52Z",
-      revision: "601a744159fda388eb6fe2fad315dc9c6017f94a"
-  version "20220409150952"
+      tag:      "RELEASE.2022-04-12T06-55-35Z",
+      revision: "e162a055cc29feedfb20e1be1f5402571cb36100"
+  version "20220412065535"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/minio.git", branch: "master"
 
