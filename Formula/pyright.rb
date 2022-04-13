491dec4ad35a30230a57a1bb9945b721476f3647
Porkepix <porkepix@gmail.com>
2022-04-13 17:39:20 +0200

    pyright 1.1.237
    Closes #99187.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyright.rb
+++ Formula/pyright.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Pyright < Formula
   desc "Static type checker for Python"
   homepage "https://github.com/microsoft/pyright"
-  url "https://registry.npmjs.org/pyright/-/pyright-1.1.236.tgz"
-  sha256 "9aa6d07d54874e6d0ff005dd56307cfc88fc53440e2339c1560d7ec126118d2b"
+  url "https://registry.npmjs.org/pyright/-/pyright-1.1.237.tgz"
+  sha256 "df951580dbfa8b326f6093efcd9107719f1f0205a3e477fde9af7b1d43eece1d"
   license "MIT"
   head "https://github.com/microsoft/pyright.git", branch: "main"
 
