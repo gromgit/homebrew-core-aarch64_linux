787b6c5eed0034a4febf35b0efa67a27756a10cd
Pavel Kalian <pavel@kalian.cz>
2022-04-13 19:53:48 -0300

    wxwidgets 3.1.6
    Bump wxWidgets version to 3.1.6

--- Formula/wxwidgets.rb
+++ Formula/wxwidgets.rb
@@ -1,8 +1,8 @@
 class Wxwidgets < Formula
   desc "Cross-platform C++ GUI toolkit"
   homepage "https://www.wxwidgets.org"
-  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxWidgets-3.1.5.tar.bz2"
-  sha256 "d7b3666de33aa5c10ea41bb9405c40326e1aeb74ee725bb88f90f1d50270a224"
+  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.6/wxWidgets-3.1.6.tar.bz2"
+  sha256 "4980e86c6494adcd527a41fc0a4e436777ba41d1893717d7b7176c59c2061c25"
   license "wxWindows"
   head "https://github.com/wxWidgets/wxWidgets.git", branch: "master"
 
