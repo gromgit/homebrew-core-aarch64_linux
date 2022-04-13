06b7aba67145675d1446b8006e06026c2775f106
Porkepix <porkepix@gmail.com>
2022-04-13 16:56:30 +0200

    jpegoptim 1.4.7
    Closes #99179.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jpegoptim.rb
+++ Formula/jpegoptim.rb
@@ -1,8 +1,8 @@
 class Jpegoptim < Formula
   desc "Utility to optimize JPEG files"
   homepage "https://github.com/tjko/jpegoptim"
-  url "https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.6.tar.gz"
-  sha256 "c44dcfac0a113c3bec13d0fc60faf57a0f9a31f88473ccad33ecdf210b4c0c52"
+  url "https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.7.tar.gz"
+  sha256 "9d2a13b7c531d122f360209422645206931c74ada76497c4aeb953610f0d70c1"
   license "GPL-2.0"
   head "https://github.com/tjko/jpegoptim.git", branch: "master"
 
