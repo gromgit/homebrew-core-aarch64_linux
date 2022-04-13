452541069e79b7b14e3313156b12d1d479981244
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-13 05:11:10 +0000

    cosign 1.7.2
    Closes #99146.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -2,8 +2,8 @@ class Cosign < Formula
   desc "Container Signing"
   homepage "https://github.com/sigstore/cosign"
   url "https://github.com/sigstore/cosign.git",
-      tag:      "v1.7.1",
-      revision: "53c28e44f10548c8839d4c72e5909fa74e08b5f6"
+      tag:      "v1.7.2",
+      revision: "1b1bca3280994eebe38d35e03bbd66af6214f0f1"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
