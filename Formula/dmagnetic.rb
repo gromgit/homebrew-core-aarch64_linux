d76492394ceaaae3d5212f744681d537119443d5
Porkepix <porkepix@gmail.com>
2022-04-14 15:35:36 +0200

    dmagnetic 0.33
    * dmagnetic 0.33
    * update test
    * dmagnetic: fix test
    * dmagnetic: avoid hard-coded share/etc paths
    
    Closes #99270.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -1,8 +1,8 @@
 class Dmagnetic < Formula
   desc "Magnetic Scrolls Interpreter"
   homepage "https://www.dettus.net/dMagnetic/"
-  url "https://www.dettus.net/dMagnetic/dMagnetic_0.32.tar.bz2"
-  sha256 "9671b863cbb126e122923fa974806ff0e998af471c98e878c1392c20a3606206"
+  url "https://www.dettus.net/dMagnetic/dMagnetic_0.33.tar.bz2"
+  sha256 "4199966f214667c78c7133b8b0c93ff4b8c65c8dfdb2ff9487a0b3b1726af212"
   license "BSD-2-Clause"
 
   livecheck do
@@ -21,15 +21,26 @@ class Dmagnetic < Formula
   end
 
   def install
+    # Look for configuration and other data within the Homebrew prefix rather than the default paths
+    inreplace "src/toplevel/dMagnetic_pathnames.h" do |s|
+      s.gsub! "/etc/", "#{etc}/"
+      s.gsub! "/usr/local/", "#{HOMEBREW_PREFIX}/"
+    end
+
     system "make", "PREFIX=#{prefix}", "install"
     (share/"games/dMagnetic").install "testcode/minitest.mag", "testcode/minitest.gfx"
   end
 
   test do
-    command_output = pipe_output("#{bin}/dMagnetic "\
-                                 "-vmode none -vcols 300 -vrows 300 -vecho -sres 1024x768 "\
-                                 "-mag #{share}/games/dMagnetic/minitest.mag "\
-                                 "-gfx #{share}/games/dMagnetic/minitest.gfx", "Hello\n")
-    assert_equal "ab9ec7787593e310ac4d8187db3f6cee", Digest::MD5.hexdigest(command_output)
+    args = %W[
+      -vmode none
+      -vcols 300
+      -vrows 300
+      -vecho -sres 1024x768
+      -mag #{share}/games/dMagnetic/minitest.mag
+      -gfx #{share}/games/dMagnetic/minitest.gfx
+    ]
+    command_output = pipe_output("#{bin}/dMagnetic #{args.join(" ")}", "Hello\n")
+    assert_match(/^Virtual machine is running\..*\n> HELLO$/, command_output)
   end
 end
