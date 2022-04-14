3a3fdacb69d0c6d9d9b733b3251b9df9c5014829
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-14 22:04:38 +0000

    ctemplate: update 2.4_1 bottle.

--- Formula/ctemplate.rb
+++ Formula/ctemplate.rb
@@ -9,11 +9,12 @@ class Ctemplate < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_monterey: "2af8837c0e6f6cb3405008c71795fcc3def16818aa2512365ec027cb3ad4b48e"
-    sha256 cellar: :any, arm64_big_sur:  "d9b6bdf4a7d13079ea3eb55d1cae8307513a8aaa7d782eda9333a3a96ff45523"
-    sha256 cellar: :any, monterey:       "407f8bdf5dea727de91e5436cab5b0e271fdd935806aa985d38d5ed4c2db57e9"
-    sha256 cellar: :any, big_sur:        "3e4c9c7028cf8037cc61000e24d25fd34bc8741a863b440653c087908ff33169"
-    sha256 cellar: :any, catalina:       "9451278bdf27133395761b18b00e88fb5ac3765bb1b5a0da7acb88a671ef7977"
+    sha256 cellar: :any,                 arm64_monterey: "2af8837c0e6f6cb3405008c71795fcc3def16818aa2512365ec027cb3ad4b48e"
+    sha256 cellar: :any,                 arm64_big_sur:  "d9b6bdf4a7d13079ea3eb55d1cae8307513a8aaa7d782eda9333a3a96ff45523"
+    sha256 cellar: :any,                 monterey:       "407f8bdf5dea727de91e5436cab5b0e271fdd935806aa985d38d5ed4c2db57e9"
+    sha256 cellar: :any,                 big_sur:        "3e4c9c7028cf8037cc61000e24d25fd34bc8741a863b440653c087908ff33169"
+    sha256 cellar: :any,                 catalina:       "9451278bdf27133395761b18b00e88fb5ac3765bb1b5a0da7acb88a671ef7977"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aef9050d0aa26b1a38f26df4e71d084ef439f5f26600f9defd8ce8b5de2221c6"
   end
 
   depends_on "autoconf" => :build
