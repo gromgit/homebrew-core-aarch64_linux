15b3dabf3b03192b5bfc757a0c90466bba22b4e7
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-14 22:05:30 +0000

    vroom: update 1.11.0 bottle.

--- Formula/vroom.rb
+++ Formula/vroom.rb
@@ -6,11 +6,12 @@ class Vroom < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "7a823e112cf6cac7b986ca5e142c1517a8be92fc34048ea26ad4cd97e0b68703"
-    sha256 cellar: :any, arm64_big_sur:  "3bf10d29ff9e95edd39ac68c2ffde3e87ebb86f42a220486471d3e641b1048e9"
-    sha256 cellar: :any, monterey:       "93d5164b0e7630e9af621d7da2eb9c550b23c5701ca28117eb43f16861762377"
-    sha256 cellar: :any, big_sur:        "8e22ef70463048645622fa6892cd17561dff683b261e4b0e4133ddded27f6714"
-    sha256 cellar: :any, catalina:       "aa67ba7ad30b43826f38d798f67689ad224080791950e82ef763d29420a30e50"
+    sha256 cellar: :any,                 arm64_monterey: "7a823e112cf6cac7b986ca5e142c1517a8be92fc34048ea26ad4cd97e0b68703"
+    sha256 cellar: :any,                 arm64_big_sur:  "3bf10d29ff9e95edd39ac68c2ffde3e87ebb86f42a220486471d3e641b1048e9"
+    sha256 cellar: :any,                 monterey:       "93d5164b0e7630e9af621d7da2eb9c550b23c5701ca28117eb43f16861762377"
+    sha256 cellar: :any,                 big_sur:        "8e22ef70463048645622fa6892cd17561dff683b261e4b0e4133ddded27f6714"
+    sha256 cellar: :any,                 catalina:       "aa67ba7ad30b43826f38d798f67689ad224080791950e82ef763d29420a30e50"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b43cccb5dfda7f01cee6c9ca1fa43a3cb11879cb35e85183ec05df8e26770c0"
   end
 
   depends_on "pkg-config" => :build
