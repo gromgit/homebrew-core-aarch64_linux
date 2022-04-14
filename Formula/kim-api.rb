850a79252bb71947570e2e0700b59efe5750b923
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-14 08:13:24 +0000

    kim-api: update 2.3.0 bottle.

--- Formula/kim-api.rb
+++ Formula/kim-api.rb
@@ -11,14 +11,12 @@ class KimApi < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "a749243959bbd89189fe9b83da1a9f992de0b8346d08dabe2172415abadc2ffd"
-    sha256 cellar: :any,                 arm64_big_sur:  "bc69df6522ce9a9f76399e03b78255ae82faedd70c5a8f3d4535de4d3f89f6a8"
-    sha256 cellar: :any,                 monterey:       "0efba513c8067de36eb7ae2b27c538380f8d323a87a63dc7432965ee83c777b6"
-    sha256 cellar: :any,                 big_sur:        "0324105634345be3ed3cb5f7bdd7d31379e84e5afdf966b3a699ad5355b8da49"
-    sha256 cellar: :any,                 catalina:       "2d3e050c3af7adb392213906db110f79bc6c637a68a77231bcd94ca201555f2e"
-    sha256 cellar: :any,                 mojave:         "34a1e8943c72ae99512a7069c08738f57f1b11e0adfb9a6ba5724ad953d06745"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25cab6d99de536975418877447a40e436a884b16423056b06902f3aaf0884a61"
+    sha256 cellar: :any,                 arm64_monterey: "b92cc266d748a98e897612a3492173ed6f4e697bab3456cfe73c164238acf44c"
+    sha256 cellar: :any,                 arm64_big_sur:  "8f5408a02b09db05073d67e0a5b9f434227ed4fe5a5283152a0c35ec1c6b4a14"
+    sha256 cellar: :any,                 monterey:       "625429556163329924c57a73aa91396a3ff10e9e459781d4b29f7143e193a68f"
+    sha256 cellar: :any,                 big_sur:        "f5e4e77f769ac4bdb55a6cd3872fc0157602f7a8e9ca0d4a49c11cb09ddebd71"
+    sha256 cellar: :any,                 catalina:       "f91f86a9cb4cfaca70ae47d038914c9a083505c5584ae7f1e977472f2028ae11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "433e83ed6dff896362dc21c6a2e2221f2b94d2e9a70fcebf230ce13a86ed434a"
   end
 
   depends_on "cmake" => :build
