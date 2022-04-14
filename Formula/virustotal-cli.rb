c5887ecfd6f33737be34ce923f52fa906eeef283
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-14 05:45:16 +0000

    virustotal-cli 0.10.2
    Closes #99262.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/virustotal-cli.rb
+++ Formula/virustotal-cli.rb
@@ -1,8 +1,8 @@
 class VirustotalCli < Formula
   desc "Command-line interface for VirusTotal"
   homepage "https://github.com/VirusTotal/vt-cli"
-  url "https://github.com/VirusTotal/vt-cli/archive/0.10.1.tar.gz"
-  sha256 "b9004ebdd7a66eff15d160fe795b7f3c5577af7c316f896e7e05418e89c3792c"
+  url "https://github.com/VirusTotal/vt-cli/archive/0.10.2.tar.gz"
+  sha256 "09953ce45c2fbf872f70294d5fb58e856c0167ed6a7e1312f770e45bab18356b"
   license "Apache-2.0"
 
   bottle do
