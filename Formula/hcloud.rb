7a8b3eb78cb2c62041878ba90423ac4fc78c977b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-15 05:27:19 +0000

    hcloud 1.29.5
    Closes #99323.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -1,8 +1,8 @@
 class Hcloud < Formula
   desc "Command-line interface for Hetzner Cloud"
   homepage "https://github.com/hetznercloud/cli"
-  url "https://github.com/hetznercloud/cli/archive/v1.29.4.tar.gz"
-  sha256 "49dab6fc46e1fa10eb49800df463f6fab413edb62b585ea9474e981d8bf98323"
+  url "https://github.com/hetznercloud/cli/archive/v1.29.5.tar.gz"
+  sha256 "8a8a0a8ee86eaae329090657f236c8f736c5c67209fedf5047977f8ef8c2afb8"
   license "MIT"
 
   bottle do
