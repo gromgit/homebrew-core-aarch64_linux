8af703b0994638c99d7dfa6a65ab7ab78b598f78
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-15 22:04:55 +0000

    fortio: update 1.26.0 bottle.

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -12,12 +12,12 @@ class Fortio < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1edfec0ab9fb2fea5434454c93af797e478231f9809f637d8a8fb9e40488a02b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5dc3e508eb5eaa01a20be303a002795c43b474ed688505b924bef9ac7db512c6"
-    sha256 cellar: :any_skip_relocation, monterey:       "590d2ea94d81078e1ba8be686920ffaa5e2f52f2dae0ae6fd8c65be10fda2d63"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6eb52f59789165e9d78e79305cfba66c247f99b1168ebeb94a678d648bfb43b6"
-    sha256 cellar: :any_skip_relocation, catalina:       "c66eb4ef443b95067d166310095be368f4ceb6d06a6ecd72e9ff049e9c39ddd6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03380cf33e28d0bf094443ee99c85546ad73d418a0ac9a2cbfd136ddd93eccdc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7c81d8ef1fc19e7463ac437face3b8bd0a8e22393a832d392e4c6633cbf66460"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "712702a9e24f006f3306d4063df248eb4cd1980b9c5ff6141ca1e1f6c344ac4a"
+    sha256 cellar: :any_skip_relocation, monterey:       "04de351918c38d479096db6018158f8cee047635b77d918db7ba3b2254cf61b0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1e8edbb1421cdc257307152417b30d5390ef55af46a67f2a16e02fd2dcafffe9"
+    sha256 cellar: :any_skip_relocation, catalina:       "fdbb013a95d8135def974874adba010a90692f1976805702acee7240e3c6c1df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "575f8f85688a51bdccbd9490a198d3ce9ee17531b58369e18d1419d45fde2d63"
   end
 
   depends_on "go" => :build
