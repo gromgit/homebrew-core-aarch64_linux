dc1b7b98c1f03cdd3ba186d84718f05b7c59abca
Rui Chen <rui@chenrui.dev>
2022-04-16 15:00:57 -0400

    localtunnel 2.0.2 (new formula)
    Closes #99429.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/localtunnel.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class Localtunnel < Formula
+  desc "Exposes your localhost to the world for easy testing and sharing"
+  homepage "https://theboroer.github.io/localtunnel-www/"
+  url "https://registry.npmjs.org/localtunnel/-/localtunnel-2.0.2.tgz"
+  sha256 "6fb76f0ac6b92989669a5b7bb519361e07301965ea1f5a04d813ed59ab2e0c34"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "pty"
+    port = free_port
+
+    stdout, _stdin, _pid = PTY.spawn("#{bin}/lt --port #{port}")
+    assert_match "your url is:", stdout.readline
+
+    assert_match version.to_s, shell_output("#{bin}/lt --version")
+  end
+end
