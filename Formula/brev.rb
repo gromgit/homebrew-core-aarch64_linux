143466fe227042735997675bc64d01c70cd17ea1
Rui Chen <rui@chenrui.dev>
2022-04-16 16:19:34 -0400

    brev 0.6.49
    Closes #99425.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.44.tar.gz"
-  sha256 "6c11ecb7dc80ae0433b32ebac0ea77baac55fe71a1d42d0a49f6e929d444134f"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.49.tar.gz"
+  sha256 "1fb231a76d8e35983da6ae1d66612be15b3aa5cb395f8a9bc7b7a879f3ba37bd"
   license "MIT"
 
   livecheck do
