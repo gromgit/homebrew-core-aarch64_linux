63476719621eef9d6e726cd8c9d01c3fd0c61a16
Rui Chen <rui@chenrui.dev>
2022-04-16 18:59:48 -0400

    zenith 0.13.1
    Closes #99442.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zenith.rb
+++ Formula/zenith.rb
@@ -1,8 +1,8 @@
 class Zenith < Formula
   desc "In terminal graphical metrics for your *nix system"
   homepage "https://github.com/bvaisvil/zenith/"
-  url "https://github.com/bvaisvil/zenith/archive/0.13.0.tar.gz"
-  sha256 "e3f914e5effb842f5931b5b8310e05e90a40f6aff7384b54a9f18a73ba567032"
+  url "https://github.com/bvaisvil/zenith/archive/0.13.1.tar.gz"
+  sha256 "1f2914a1ddd7dd76cca0e0c07ca77bd048addfd80fc6329ea7b83647ea66020a"
   license "MIT"
   version_scheme 1
   head "https://github.com/bvaisvil/zenith.git", branch: "master"
