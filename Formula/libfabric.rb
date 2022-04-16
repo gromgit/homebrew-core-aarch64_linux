2d637df45e8a500437fb9094be16f4f37920504a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-16 10:06:25 +0000

    libfabric: update 1.14.1 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -7,12 +7,12 @@ class Libfabric < Formula
   head "https://github.com/ofiwg/libfabric.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d940a430c5b1d773851e425239fc924155f08fab327a46c5c60cec3d5bb538d3"
-    sha256 cellar: :any,                 arm64_big_sur:  "6c0148c987d5d86eb54c790e7bc528bd0b744c2ae4b4ac34af3ef8d67a731e72"
-    sha256 cellar: :any,                 monterey:       "de6acfe3a9f224363d3995b7e895b2017a78d4249ceff84be4a44b4f3a08e433"
-    sha256 cellar: :any,                 big_sur:        "40b80728c6b9925662a5e488a187c50d7f9aeab33e65cda017f6fa8afa57745e"
-    sha256 cellar: :any,                 catalina:       "7f1af2e9f62d3460dc39db7b2d5cb1bb6490fb6fe7f7fef5d9c52938b2e13c9c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd8c220a845d899c34e53bcfb4e6fe49201138b8c4161ed0201b1fd4cbbc6540"
+    sha256 cellar: :any,                 arm64_monterey: "67e1890e5424205e4d47db665aef0c9f197e5e1648b7d3371d1f5902ce72241c"
+    sha256 cellar: :any,                 arm64_big_sur:  "2dfe3c2fad7e8a913c2919989627bc8401ac84994ec3381e4261ad110263e636"
+    sha256 cellar: :any,                 monterey:       "213f99b7bf3755739201a7d19c2871381a92ec60bdd174990c8308cfb57a4ec5"
+    sha256 cellar: :any,                 big_sur:        "5753837b80e8fa3494ba685738d51b5e6193b8258ee1f4951a1f44637ff36b15"
+    sha256 cellar: :any,                 catalina:       "683db1d11a19802de82992e96b212ae749e6a82627819f45f20d02f2d6723420"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61554857ee7be51161c443afa9859ec41dce2acd6de8a2ea263a884e71ee95b4"
   end
 
   depends_on "autoconf" => :build
