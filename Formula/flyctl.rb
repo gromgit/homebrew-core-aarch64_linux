0cedac87c37255096d3292384d3575ff6b160c2b
Rui Chen <rui@chenrui.dev>
2022-04-16 16:59:30 -0400

    flyctl 0.0.320
    Closes #99435.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.318",
-      revision: "a7c1084992a8c9aa9b0dd5bbe17ba28c32986d1d"
+      tag:      "v0.0.320",
+      revision: "58aae1e8553414efcc6502ad07c5d61a21443e3e"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
