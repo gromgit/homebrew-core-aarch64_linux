edfdcde49984c1d34d16ea8c638c53b4e6b53059
Rui Chen <rui@chenrui.dev>
2022-04-16 19:00:51 -0400

    zabbix 6.0.3
    * zabbix 6.0.3
    * zabbix: update to use pcre2
    * zabbix: use --with-libpcre2
    * zabbix: autodetect pcre2
    
    Closes #99443.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zabbix.rb
+++ Formula/zabbix.rb
@@ -1,8 +1,8 @@
 class Zabbix < Formula
   desc "Availability and monitoring solution"
   homepage "https://www.zabbix.com/"
-  url "https://cdn.zabbix.com/zabbix/sources/stable/6.0/zabbix-6.0.2.tar.gz"
-  sha256 "7ca89554bd998abcb9df819264293f7e222b4ef4a225acae3a9362d93c081411"
+  url "https://cdn.zabbix.com/zabbix/sources/stable/6.0/zabbix-6.0.3.tar.gz"
+  sha256 "e6195328942744eca5f0051db42f9830d26612a93f439e89656588d9488a3042"
   license "GPL-2.0-or-later"
   head "https://github.com/zabbix/zabbix.git", branch: "master"
 
@@ -21,7 +21,7 @@ class Zabbix < Formula
   end
 
   depends_on "openssl@1.1"
-  depends_on "pcre"
+  depends_on "pcre2"
 
   def install
     args = %W[
@@ -29,7 +29,7 @@ class Zabbix < Formula
       --prefix=#{prefix}
       --sysconfdir=#{etc}/zabbix
       --enable-agent
-      --with-libpcre=#{Formula["pcre"].opt_prefix}
+      --with-libpcre2
       --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
