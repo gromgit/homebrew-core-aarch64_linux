2d1270792495a056cc16dbd6ee8960eb959dd303
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-16 10:06:34 +0000

    sqlx-cli: update 0.5.13 bottle.

--- Formula/sqlx-cli.rb
+++ Formula/sqlx-cli.rb
@@ -6,12 +6,12 @@ class SqlxCli < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1c2758f78ff1e0c1c709b64838e00d928adf7f26d1f52e69528baf8dc1b178cd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f698c1f1cba462044fec3f64e1f4332efb8fdf71c995970d694b74faedc7bfc5"
-    sha256 cellar: :any_skip_relocation, monterey:       "d1f3ceb95df8cf11b109ca731fc3b6a8734ab9be581510561924deb29dfce45d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b34f1fd15c8efe55431ffcc18eaf5ea426c62729ed69d4dea9fc970b80fd3fe6"
-    sha256 cellar: :any_skip_relocation, catalina:       "a7622dc0963fbdf5de2faf5dfa5b4a3fe7ccaabb3e24e95c5adae8c549028b65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c779d4731021df755c4d494727ad37fef1ddcdcb07b74b1fdb7851ceb2f3c175"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "63d8c26c177dba3904884314de2ad823e1981e79a7f57063af722e5aeace91dd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "568f764ffff0b7ee1cc8675943c3d139a80600a4c3e8ceeaf91b17a1551da2a7"
+    sha256 cellar: :any_skip_relocation, monterey:       "bed00d7a571260b707384660f8701793e1d18a15211d3e730f82196419042ade"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6d10e9be7df2d5a01761e4f2a60b9d4eb521c81ef34efcf2d4cdb8ecb958bfb0"
+    sha256 cellar: :any_skip_relocation, catalina:       "e9bfe1d2b6b14177b4214860b274b254324920ed4a6385d4d23f79e689eb750a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48954a5a477c2d253afa68a0dea041f6071ddd67785fc67abed0af2e13f7ab27"
   end
 
   depends_on "rust" => :build
