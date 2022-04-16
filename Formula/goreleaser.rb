f18f554763b11af9255bfc259db45e4854292c82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-16 10:06:40 +0000

    goreleaser: update 1.8.1 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -8,12 +8,12 @@ class Goreleaser < Formula
   head "https://github.com/goreleaser/goreleaser.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "22fea6d24bd9c9da0173cfd2a41bebb07dc61e456b99e7e1d8ff373f4064cdcc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1bb1f53ee108cf3c63e690293618fdfca7df9f4bde80e063d6e08e3abcb47e0c"
-    sha256 cellar: :any_skip_relocation, monterey:       "9cb81351d7913caff5d50250afc02927ed0e3b7b8e2b99eafb033d53e1adac3c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3d7e8e8945d5f193a9e69f7a896d6d11c2294e0dc394c050a859735f846aab0f"
-    sha256 cellar: :any_skip_relocation, catalina:       "8d6425bcf30313b2697f8715b981ce89d212202e2ae70516bd3c2636570f6671"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9a632bcc6ee92a6502c70a3725964caff6f29b3def4320e874ee74af82bde51"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "30aabda024fdceb26b2b6a692a1ec6487d1472ebe52d381009738cb4cd1ad29e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a246a9c784f88ac85938b6b00f8c587c0e6b21105714bddabd2e32c08b13e170"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b64fe725c1aea9cb0c4b7730c2f5c50084a930b2ace79e4b62ef54dcd8fc396"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7f82bc64994fc8f056a3fdc47d16b8d9b6542d6f6f23838613456ed3a5266e55"
+    sha256 cellar: :any_skip_relocation, catalina:       "d88a428e225b58f0e24f651ce024ba0b80ec8eb5e3c4b4c21a523c5c9aa6f59a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c88452e1a92fe7d38ece3ca6103148027838b2ffa4e3aa55c4a97d1ac4fb996c"
   end
 
   depends_on "go" => :build
