fbbaaf2208f90c5f631444679b290ac27c6ce648
Rui Chen <rui@chenrui.dev>
2022-04-17 13:03:19 -0400

    saxon 11.3
    * saxon 11.3
    * add lib dir (due to artifact layout change)
    
    Closes #99509.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/saxon.rb
+++ Formula/saxon.rb
@@ -1,9 +1,9 @@
 class Saxon < Formula
   desc "XSLT and XQuery processor"
   homepage "https://saxon.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/saxon/Saxon-HE/10/Java/SaxonHE10-6J.zip"
-  version "10.6"
-  sha256 "0e590ede60eef6d8a98e759f72769c20417173f99191ebbc2f9ec4e331dbc296"
+  url "https://downloads.sourceforge.net/project/saxon/Saxon-HE/11/Java/SaxonHE11-3J.zip"
+  version "11.3"
+  sha256 "c462e29994e1622d1cdc50514469fd4c9eb13cb7658abe129667b2be8df35fb4"
 
   livecheck do
     url :stable
@@ -21,7 +21,7 @@ class Saxon < Formula
   depends_on "openjdk"
 
   def install
-    libexec.install Dir["*.jar", "doc", "notices"]
+    libexec.install Dir["*.jar", "doc", "lib", "notices"]
     bin.write_jar_script libexec/"saxon-he-#{version.major_minor}.jar", "saxon"
   end
 
