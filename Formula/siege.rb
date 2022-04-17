bc05ac232a4ee2fd2bf1de191ef1fac8f0bc0904
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-17 19:04:11 +0000

    siege: update 4.1.3 bottle.

--- Formula/siege.rb
+++ Formula/siege.rb
@@ -11,12 +11,12 @@ class Siege < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "be440f6540a10977b1c5d7b6406fba6912d1180f02166a182bcdb36d1f19cd68"
-    sha256 arm64_big_sur:  "bc2eb934f93364fa2bf0e19778df12a07e52bab918b723cd6e2165368cdcdc9d"
-    sha256 monterey:       "69201e329616cc0f6c3b1b96f83f3082bae76933fc32245902761219c2fde29c"
-    sha256 big_sur:        "1cef6df6277b9a7e8365777d33fdc93001bf018b0f84c914e4acfb4cba2f489f"
-    sha256 catalina:       "8f0fe495d1c2f315fbd264cd46c96df2deb4c637c8107da8add5571d91a655f8"
-    sha256 x86_64_linux:   "ef1e5a8d7ca1ad4b4bbf0ea5b19f68e75a69b61661cc3efcb4742b66830eb52e"
+    sha256 arm64_monterey: "463170796c52f7651386b889298f25e249fa8d4d03dbc9090a01069e2f2124fc"
+    sha256 arm64_big_sur:  "84588fcd2627114af6e79ea402e1195885204d2ab47e1c2d7ba6032a5196ed50"
+    sha256 monterey:       "576231668f70ff5a63041e862727db42553d976748a112e03bd88fccd4849f76"
+    sha256 big_sur:        "c94fa95c94c212539d98615cc2001e074de0be743e6bd31801bbdccabcb0ab81"
+    sha256 catalina:       "db47466f69c5e7c6737099424a4c4e1139ad487c0f2380ee80ce8f530f0711b4"
+    sha256 x86_64_linux:   "908d5fb2d6ec753c8061c35c7a3fa7ea383257ae649bd68e634c99620ac65b16"
   end
 
   depends_on "openssl@1.1"
