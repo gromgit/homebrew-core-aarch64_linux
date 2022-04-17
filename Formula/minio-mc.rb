2a3d146d3c2691c00de96a8d1255194b2c3c74e2
Rui Chen <rui@chenrui.dev>
2022-04-17 12:03:06 -0400

    minio-mc 20220416211121
    Closes #99497.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -2,9 +2,9 @@ class MinioMc < Formula
   desc "Replacement for ls, cp and other commands for object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git",
-      tag:      "RELEASE.2022-04-07T21-43-27Z",
-      revision: "3f0953f9f47326f9554c5067b39cd39ab82f30a3"
-  version "20220407214327"
+      tag:      "RELEASE.2022-04-16T21-11-21Z",
+      revision: "0530f5a47110c7a9325212d6093a214be0b8fd60"
+  version "20220416211121"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/mc.git", branch: "master"
 
