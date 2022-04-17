a9ba7681701a2d5469839001da54dc3ebab88f81
Rui Chen <rui@chenrui.dev>
2022-04-17 13:07:52 -0400

    scs 3.2.0
    Closes #99510.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scs.rb
+++ Formula/scs.rb
@@ -1,8 +1,8 @@
 class Scs < Formula
   desc "Conic optimization via operator splitting"
   homepage "https://web.stanford.edu/~boyd/papers/scs.html"
-  url "https://github.com/cvxgrp/scs/archive/3.1.0.tar.gz"
-  sha256 "90a7e58364ed3ea3375945a7f6f013de81c46100df80664a18f1c9b45a56be9c"
+  url "https://github.com/cvxgrp/scs/archive/3.2.0.tar.gz"
+  sha256 "df546b8b8764cacaa0e72bfeb9183586e1c64bc815174cbbecd4c9c1ef18e122"
   license "MIT"
 
   bottle do
@@ -37,7 +37,7 @@ class Scs < Formula
         _scs_read_data("#{pkgshare}/random_prob", &d, &k, &stgs);
         result = scs(d, k, stgs, sol, &info);
 
-        _scs_free_data(d, k, stgs); _scs_free_sol(sol);
+        _scs_free_data(d); _scs_free_data(k); _scs_free_sol(sol);
         return result - SCS_SOLVED;
       }
     EOS
