05c5cfa2c520281cef3656e9e3864a6f84e09a80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-17 08:05:45 +0000

    logcli: update 2.5.0 bottle.

--- Formula/logcli.rb
+++ Formula/logcli.rb
@@ -11,12 +11,12 @@ class Logcli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f6970a743893bea4d6a56780234f7ca0a2b7885e04ba8d4391a1d2b5d3969497"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89ac54597c71253e1c6082ae2402583753425815db899c813f730f64c88927e9"
-    sha256 cellar: :any_skip_relocation, monterey:       "25fc576bd6ae13176c98a2929219581d5a4ba840b60c188577fd930beffeb7e7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ad76d57bc0341f332d2c1f777935e423a45e79968a8568d0ba19e9dbbbcffa38"
-    sha256 cellar: :any_skip_relocation, catalina:       "18e23f7d9b91d5514e330e53ba65f192784d9824e868f5bd43478b3a237704c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e1c676fe5b3133dceadab697f245f34d57a83c49793886095f8870d0cdab326"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "88e5bc3436ea5c2f26c8af399b6b6f6f54eacf8026130cd43f409ecb2779e4c3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0dcb73f437252873bae70c90d45128b77e66c4f24923d40a17d31cbbad316a07"
+    sha256 cellar: :any_skip_relocation, monterey:       "22db2cd34d128a91c89c40b25fcbc6f5161f81559a1d4bdbd91ca668698ac339"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3ce5085f2333685aae241263f81d4ea6ae4f882a219dbaddd3cf68db2b3d85ad"
+    sha256 cellar: :any_skip_relocation, catalina:       "32a2e0e5a268502e3963f40534170b833cc2183a3e1eee547eb1cd333773c5a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42067a26764bbb5b3eeb1d03e5979a3f390107fbaeed9cd40df6f03e1c4f5132"
   end
 
   # Bump to 1.18 on the next release, if possible.
