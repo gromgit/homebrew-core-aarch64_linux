be5647475c256da4dd7aeda45031af26e1fb75dd
Porkepix <porkepix@gmail.com>
2022-04-18 13:13:19 +0200

    libofx 0.10.5
    * libofx 0.10.4
    * libofx: fix compilation on Linux
    * libofx 0.10.5
    
    Closes #99546.
    
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -1,14 +1,9 @@
 class Libofx < Formula
   desc "Library to support OFX command responses"
-  homepage "https://libofx.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/libofx/libofx/libofx-0.10.3.tar.gz"
-  sha256 "7b5f21989afdd9cf63ab4a2df4ca398782f24fda2e2411f88188e00ab49e3069"
+  homepage "https://github.com/libofx/libofx"
+  url "https://github.com/libofx/libofx/releases/download/0.10.5/libofx-0.10.5.tar.gz"
+  sha256 "570ea744fb654750ed9c60456c771e7cbb035465f409e9ee1118f671b47b3bc3"
   license "GPL-2.0-or-later"
-
-  livecheck do
-    url :stable
-    regex(%r{url=.*?/libofx[._-]v?(\d+(?:\.\d+)+)\.t}i)
-  end
 
   bottle do
     sha256 arm64_monterey: "8d46f692bc2952487c01b35a9c5ca8dc3bc4fbb0dd298135001110a6a9ac7a60"
@@ -20,10 +15,21 @@ class Libofx < Formula
     sha256 x86_64_linux:   "0539cc89e0874fe414f30db5b87edf7cfdc2d899694b8546f4312068ba408273"
   end
 
+  head do
+    url "https://github.com/libofx/libofx.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gengetopt" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "open-sp"
 
   def install
     ENV.cxx11
+
+    system "./autogen.sh" if build.head?
 
     opensp = Formula["open-sp"]
     system "./configure", "--disable-dependency-tracking",
@@ -34,6 +40,96 @@ class Libofx < Formula
   end
 
   test do
-    assert_equal "ofxdump #{version}", shell_output("#{bin}/ofxdump -V").chomp
+    (testpath/"test.ofx").write <<~EOS
+      OFXHEADER:100
+      DATA:OFXSGML
+      VERSION:102
+      SECURITY:NONE
+      ENCODING:USASCII
+      CHARSET:1252
+      COMPRESSION:NONE
+      OLDFILEUID:NONE
+      NEWFILEUID:NONE
+
+      <OFX>
+        <SIGNONMSGSRSV1>
+          <SONRS>
+            <STATUS>
+              <CODE>0
+              <SEVERITY>INFO
+            </STATUS>
+            <DTSERVER>20130525225731.258
+            <LANGUAGE>ENG
+            <DTPROFUP>20050531060000.000
+            <FI>
+              <ORG>FAKE
+              <FID>1101
+            </FI>
+            <INTU.BID>51123
+            <INTU.USERID>9774652
+          </SONRS>
+        </SIGNONMSGSRSV1>
+        <BANKMSGSRSV1>
+          <STMTTRNRS>
+            <TRNUID>0
+            <STATUS>
+              <CODE>0
+              <SEVERITY>INFO
+            </STATUS>
+            <STMTRS>
+              <CURDEF>USD
+              <BANKACCTFROM>
+                <BANKID>5472369148
+                <ACCTID>145268707
+                <ACCTTYPE>CHECKING
+              </BANKACCTFROM>
+              <BANKTRANLIST>
+                <DTSTART>20000101070000.000
+                <DTEND>20130525060000.000
+                <STMTTRN>
+                  <TRNTYPE>CREDIT
+                  <DTPOSTED>20110331120000.000
+                  <TRNAMT>0.01
+                  <FITID>0000486
+                  <NAME>DIVIDEND EARNED FOR PERIOD OF 03
+                  <MEMO>DIVIDEND ANNUAL PERCENTAGE YIELD EARNED IS 0.05%
+                </STMTTRN>
+                <STMTTRN>
+                  <TRNTYPE>DEBIT
+                  <DTPOSTED>20110405120000.000
+                  <TRNAMT>-34.51
+                  <FITID>0000487
+                  <NAME>AUTOMATIC WITHDRAWAL, ELECTRIC BILL
+                  <MEMO>AUTOMATIC WITHDRAWAL, ELECTRIC BILL WEB(S )
+                </STMTTRN>
+                <STMTTRN>
+                  <TRNTYPE>CHECK
+                  <DTPOSTED>20110407120000.000
+                  <TRNAMT>-25.00
+                  <FITID>0000488
+                  <CHECKNUM>319
+                  <NAME>RETURNED CHECK FEE, CHECK # 319
+                  <MEMO>RETURNED CHECK FEE, CHECK # 319 FOR $45.33 ON 04/07/11
+                </STMTTRN>
+              </BANKTRANLIST>
+              <LEDGERBAL>
+                <BALAMT>100.99
+                <DTASOF>20130525225731.258
+              </LEDGERBAL>
+              <AVAILBAL>
+                <BALAMT>75.99
+                <DTASOF>20130525225731.258
+              </AVAILBAL>
+            </STMTRS>
+          </STMTTRNRS>
+        </BANKMSGSRSV1>
+      </OFX>
+    EOS
+
+    output = shell_output("#{bin}/ofxdump #{testpath}/test.ofx")
+    assert_equal output.scan(/Account ID\s?: 5472369148  145268707/).length, 5
+    %w[0000486 0000487 0000488].each do |fid|
+      assert_match "Financial institution's ID for this transaction: #{fid}", output
+    end
   end
 end
