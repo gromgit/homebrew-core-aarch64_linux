169560ae2c45ae13cd3870a2de3dc4ada585fbf5
Caleb Xu <calebcenter@live.com>
2022-04-18 14:55:08 -0400

    camellia: fix build and test on Linux
    Closes #99562.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/camellia.rb
+++ Formula/camellia.rb
@@ -3,6 +3,7 @@ class Camellia < Formula
   homepage "https://camellia.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/camellia/Unix_Linux%20Distribution/v2.7.0/CamelliaLib-2.7.0.tar.gz"
   sha256 "a3192c350f7124d25f31c43aa17e23d9fa6c886f80459cba15b6257646b2f3d2"
+  license "BSD-3-Clause"
 
   livecheck do
     url :stable
@@ -23,6 +24,14 @@ class Camellia < Formula
   end
 
   def install
+    # Fix missing include - https://sourceforge.net/p/camellia/bugs/1/
+    # cam_demo_cpp.cpp:212:52: error: ‘sprintf’ was not declared in this scope
+    if OS.linux?
+      inreplace "cam_demo_cpp.cpp",
+                "#include <stdlib.h>\r\n",
+                "#include <stdlib.h>\r\n#include <stdio.h>\r\n"
+    end
+
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
@@ -37,7 +46,7 @@ class Camellia < Formula
       }
     EOS
 
-    system ENV.cc, "-I#{include}", "-L#{lib}", "-lcamellia", "-o", "test", "test.cpp"
+    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-lCamellia", "-o", "test"
     system "./test"
   end
 end
