926390b6bf4b16ddc827a74ee20aefff19c90058
pratikjagrut <26519653+pratikjagrut@users.noreply.github.com>
2022-04-18 18:52:49 +0530

    vcluster: 0.7.1 (new formula)
    This PR adds formula for vcluster cli.
    
    Closes #99552.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/vcluster.rb
@@ -0,0 +1,37 @@
+class Vcluster < Formula
+  desc "Creates fully functional virtual k8s cluster inside host k8s cluster's namespace"
+  homepage "https://www.vcluster.com"
+  url "https://github.com/loft-sh/vcluster.git",
+      tag:      "v0.7.1",
+      revision: "dc0ff6f96e9c96fe2caa77e79c2dffc921b4fd49"
+  license "Apache-2.0"
+  head "https://github.com/loft-sh/vcluster.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "helm"
+  depends_on "kubernetes-cli"
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.commitHash=#{Utils.git_head}
+      -X main.buildDate=#{time.iso8601}
+      -X main.version=#{version}
+    ]
+    system "go", "build", "-mod", "vendor", *std_go_args(ldflags: ldflags), "./cmd/vclusterctl/main.go"
+    (zsh_completion/"_vcluster").write Utils.safe_popen_read(bin/"vcluster", "completion", "zsh")
+    (bash_completion/"vcluster").write Utils.safe_popen_read(bin/"vcluster", "completion", "bash")
+  end
+
+  test do
+    help_output = "vcluster root command"
+    assert_match help_output, shell_output("#{bin}/vcluster --help")
+
+    create_output = "there is an error loading your current kube config "\
+                    "(invalid configuration: no configuration has been provided, "\
+                    "try setting KUBERNETES_MASTER environment variable), "\
+                    "please make sure you have access to a kubernetes cluster and the command "\
+                    "`kubectl get namespaces` is working"
+    assert_match create_output, shell_output("#{bin}/vcluster create vcluster -n vcluster --create-namespace", 1)
+  end
+end
