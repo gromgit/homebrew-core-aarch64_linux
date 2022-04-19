f412c4168f433785c01ececc35358c70c0e0162b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-19 19:05:17 +0000

    nanomsg: update 1.2 bottle.

--- Formula/nanomsg.rb
+++ Formula/nanomsg.rb
@@ -12,15 +12,12 @@ class Nanomsg < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "db706d4dd4d8eb9ea4a1c90a211b793e04583d62d8ef7ac60410330200c11e88"
-    sha256                               arm64_big_sur:  "87ca8b11ff0c9645bcf2d30e25ef03ee957ac9187677ec03d194417ef76d3e5e"
-    sha256 cellar: :any,                 monterey:       "3f59f226fe111172467d9d7d187aa486659e5b58abc3571b6612b544d4dc43e9"
-    sha256                               big_sur:        "86a6e6d8764bc3b276ff9f96d28390153d5287c22293124203af015e62df50d3"
-    sha256                               catalina:       "81cd453e3fdf65da66a54fda36c84248a1eb923ac92125fd14bdf68989aeb9b7"
-    sha256                               mojave:         "95609047c54b0207587db3a5b3cc8985b35fc922fe8785c63d4d2a44a78ff57f"
-    sha256                               high_sierra:    "11390e904a94e60865186a846af14565b379ec84942a9bc512ba4e5e3ea7ec85"
-    sha256                               sierra:         "95192ebc59926ff064d7f4cff5ebf9037c7549af61d2f1c23375827c91b88282"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b7b86a0e81583a24dabfbbb58cf5d72b7b107cfd8dfd7b8f1f652de0dd3db9b4"
+    sha256 cellar: :any,                 arm64_monterey: "dcfbd463f07433a38c053f412678fcf5eb718ba13bafc86930bac6c7af651d55"
+    sha256 cellar: :any,                 arm64_big_sur:  "78c5546a8a36be470a1aab0bfd05c473e841981ce51388ea395dcc94ce5c7a93"
+    sha256 cellar: :any,                 monterey:       "112db66905b5f3b99bc8740e33b7735a5ea3da4eb4d5e14ddd466c736b24e4eb"
+    sha256 cellar: :any,                 big_sur:        "4ef65cd7590b96d868f21168e970892fdbe216f3bc0a74beb35006b24049b6ea"
+    sha256 cellar: :any,                 catalina:       "421059d935dabba7625c58d56408f0658dab708c3dae59caf7f459c38d9bb632"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "099a03bfe5111a28fe413cc3e15958844adaf5324b68f86d30497ad4a87ded53"
   end
 
   depends_on "cmake" => :build
