22f86f8b9d3f7eef45e8e25b1e15aa8ada88d704
MoonFruit <dkmoonfruit@gmail.com>
2022-04-19 15:00:54 +0800

    kettle 9.3.0.0-428
    * kettle 9.3.0.0-428
    * kettle: use openjdk
    * kettle: add license
    * kettle: bug fix for openjdk
    
    Closes #99604.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kettle.rb
+++ Formula/kettle.rb
@@ -1,8 +1,9 @@
 class Kettle < Formula
   desc "Pentaho Data Integration software"
   homepage "https://www.hitachivantara.com/en-us/products/data-management-analytics.html"
-  url "https://downloads.sourceforge.net/project/pentaho/Pentaho-9.2/client-tools/pdi-ce-9.2.0.0-290.zip"
-  sha256 "8e64d1125b2403df66f212488762f1558968a3900d079c730b2f6943e346a7e7"
+  url "https://downloads.sourceforge.net/project/pentaho/Pentaho-9.3/client-tools/pdi-ce-9.3.0.0-428.zip"
+  sha256 "5c7a453ec448d4b8a568e445b119bcf4f0f41517b42e3626bc437f882c9f46c1"
+  license "Apache-2.0"
 
   livecheck do
     url :stable
@@ -14,7 +15,7 @@ class Kettle < Formula
     sha256 cellar: :any_skip_relocation, all: "0ccda9f659deb86487578d026cf3e82dfd26bd2c3850d6ef486c6fa2d44de07f"
   end
 
-  depends_on "openjdk@8"
+  depends_on "openjdk"
 
   def install
     rm_rf Dir["*.{bat}"]
@@ -27,7 +28,7 @@ class Kettle < Formula
     (var+"log/kettle").mkpath
 
     # We don't assume that carte, kitchen or pan are in anyway unique command names so we'll prepend "pdi"
-    env = { BASEDIR: libexec, JAVA_HOME: Language::Java.java_home("1.8") }
+    env = { BASEDIR: libexec, JAVA_HOME: Language::Java.java_home }
     %w[carte kitchen pan].each do |command|
       (bin+"pdi#{command}").write_env_script libexec+"#{command}.sh", env
     end
