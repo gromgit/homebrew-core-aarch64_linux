5399a1b135eacd1ac55bb0e80b4170da0176d669
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-19 17:26:16 +0000

    libkml: update 1.3.0 bottle.

--- Formula/libkml.rb
+++ Formula/libkml.rb
@@ -5,16 +5,12 @@ class Libkml < Formula
   sha256 "8892439e5570091965aaffe30b08631fdf7ca7f81f6495b4648f0950d7ea7963"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "93812e697f75c3ddebdb3f15b8b7e98773c49c69e929a244439c66076f81ff76"
-    sha256 cellar: :any, arm64_big_sur:  "589b147706bc4aec04f96039cb2e61e80d85bcbedf0e919ebcca29fe09d81e26"
-    sha256 cellar: :any, monterey:       "a217fb8977e5923462c00746cd607f510e76a9a20be9c53db554f5c72ffdfeef"
-    sha256 cellar: :any, big_sur:        "a0694686c535fa33f6222e11dde9858881e6a5eaa12c6b11c5ef310a32635087"
-    sha256 cellar: :any, catalina:       "2b73d6ea2eacd6e11229a0a9747444c28a455bb24943108b0351f689d17eb3d9"
-    sha256 cellar: :any, mojave:         "c79c73e048728d0497b7f91c0e174bd97e27f65ff471e00324483a3557b6a13f"
-    sha256 cellar: :any, high_sierra:    "31d567e2e0d87794adea3507cb34ace0483309de7ba5b32fc98bc1ca59a461c5"
-    sha256 cellar: :any, sierra:         "860294d677de2f8a4c18e4d750d59aeafa2b38801b12eb76b5e951a23a8ec108"
-    sha256 cellar: :any, el_capitan:     "57b9693cdf9a6abaeeea9648cd84a81d17ba0f056bd8d8e8442e68d97dbc7828"
-    sha256 cellar: :any, yosemite:       "a3cdfca3ed0acbc93949683a8bb2862c36ec8bf06f20b9fe3752ac624667f455"
+    sha256 cellar: :any,                 arm64_monterey: "a563610ef8923bbc46478295b172df18acf09807708db7bcc678342f9fc1edcb"
+    sha256 cellar: :any,                 arm64_big_sur:  "d821b3e1fe0181c3f0d98b6a75c6499bf62ec07845e7ff408e44fc562d95130e"
+    sha256 cellar: :any,                 monterey:       "c1c7f462a02c8e2db02d9066019c26b76b768d996743618ccfe48fd4a11b536c"
+    sha256 cellar: :any,                 big_sur:        "9209953f47c04fcca293095e2f3e17bf11116321f9fe6fe2482f7759b8adc83a"
+    sha256 cellar: :any,                 catalina:       "c6b04631c27926eba07603f5a2ef77678f9f53f249ea6931f9f08a97f73230ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ee9e4dec15a9d674c0cc8404ce5551467bbdc4604ae96d7a82b780e59fb8468"
   end
 
   depends_on "cmake" => :build
