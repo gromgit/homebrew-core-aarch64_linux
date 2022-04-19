e5eac06e93abb73569939f04b47fd6dd4cb67d96
Porkepix <porkepix@gmail.com>
2022-04-19 02:19:42 +0200

    wartremover 3.0.1
    Closes #99578.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wartremover.rb
+++ Formula/wartremover.rb
@@ -1,8 +1,8 @@
 class Wartremover < Formula
   desc "Flexible Scala code linting tool"
   homepage "https://github.com/wartremover/wartremover"
-  url "https://github.com/wartremover/wartremover/archive/v3.0.0.tar.gz"
-  sha256 "fe39ce82184be0fe2b1e4b085234583ab2459dcbe5e4162b6682190940f46c40"
+  url "https://github.com/wartremover/wartremover/archive/v3.0.1.tar.gz"
+  sha256 "57a464f198dfe392c58e73bee38bb7ca6f4851d3eea4c9f924933a19b36796e0"
   license "Apache-2.0"
   head "https://github.com/wartremover/wartremover.git", branch: "master"
 
