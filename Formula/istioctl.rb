0f0ee148bfed5a916da0ddb09ee3b54b375ad6d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-19 05:19:28 +0000

    istioctl 1.13.3
    Closes #99586.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -2,8 +2,8 @@ class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://istio.io/"
   url "https://github.com/istio/istio.git",
-      tag:      "1.13.2",
-      revision: "91533d04e894ff86b80acd6d7a4517b144f9e19a"
+      tag:      "1.13.3",
+      revision: "b28579cb30c12c428ea58279b7c06f3302abe924"
   license "Apache-2.0"
   head "https://github.com/istio/istio.git", branch: "master"
 
