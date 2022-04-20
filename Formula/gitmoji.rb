d073cb89dac1d794d14bfd91d334a376743c315e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-20 01:35:30 +0000

    gitmoji: update 5.0.0 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -8,12 +8,12 @@ class Gitmoji < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f681d6949ea935980dc21221e49cba4769dff8d8b566b985707879cc9572d8b8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f681d6949ea935980dc21221e49cba4769dff8d8b566b985707879cc9572d8b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "782c1a6c6761e505799703f32c515c1ead946cf321649eac04bd704f441d038c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "782c1a6c6761e505799703f32c515c1ead946cf321649eac04bd704f441d038c"
-    sha256 cellar: :any_skip_relocation, catalina:       "782c1a6c6761e505799703f32c515c1ead946cf321649eac04bd704f441d038c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f681d6949ea935980dc21221e49cba4769dff8d8b566b985707879cc9572d8b8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "385246ee625614e22f59422efd70a2e1af31d84ca41556c8be39dc21dcd06a2f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "385246ee625614e22f59422efd70a2e1af31d84ca41556c8be39dc21dcd06a2f"
+    sha256 cellar: :any_skip_relocation, monterey:       "d4445a3365a84dd6f4b8da87d19b5fc6adec4311100896bfb9f67002da21b586"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d4445a3365a84dd6f4b8da87d19b5fc6adec4311100896bfb9f67002da21b586"
+    sha256 cellar: :any_skip_relocation, catalina:       "d4445a3365a84dd6f4b8da87d19b5fc6adec4311100896bfb9f67002da21b586"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "385246ee625614e22f59422efd70a2e1af31d84ca41556c8be39dc21dcd06a2f"
   end
 
   depends_on "node"
