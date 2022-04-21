78560e89b5ddaf385a13a9ab173a108e0f539a77
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-21 06:26:40 +0000

    geogram: update 1.7.8 bottle.

--- Formula/geogram.rb
+++ Formula/geogram.rb
@@ -6,9 +6,10 @@ class Geogram < Formula
   license all_of: ["BSD-3-Clause", :public_domain, "LGPL-3.0-or-later", "MIT"]
 
   bottle do
-    sha256 cellar: :any, monterey: "14eae5569bb7755822db7dd6db76569ff6b3a9ec14d52ebcd32a01d859209db8"
-    sha256 cellar: :any, big_sur:  "c02fbf13cac94ee730282f326322b9ccc78a9b2747fea1ec31c937cafa543725"
-    sha256 cellar: :any, catalina: "ef86799ac68bfd1f5e982acd10e991acf2f4b74f5c7623ac54b30a4af9a90bc5"
+    sha256 cellar: :any,                 monterey:     "14eae5569bb7755822db7dd6db76569ff6b3a9ec14d52ebcd32a01d859209db8"
+    sha256 cellar: :any,                 big_sur:      "c02fbf13cac94ee730282f326322b9ccc78a9b2747fea1ec31c937cafa543725"
+    sha256 cellar: :any,                 catalina:     "ef86799ac68bfd1f5e982acd10e991acf2f4b74f5c7623ac54b30a4af9a90bc5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "15a4143539e68a38f813fdf87b4d2123fd68e304f3aa53c36ceca7d12f2be907"
   end
 
   depends_on "cmake" => :build
