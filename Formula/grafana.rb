f6b1cb616e4679fe41055961c8e23d1920e19a1e
Will Browne <will.browne@grafana.com>
2022-04-21 15:42:05 +0100

    grafana 8.5.0
    Closes #99782.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v8.4.7.tar.gz"
-  sha256 "5609a7af19561e74ba6ad1aad1c6596bf67b76ce43eb05d0c7316f7713726685"
+  url "https://github.com/grafana/grafana/archive/v8.5.0.tar.gz"
+  sha256 "f5e21977a0d8c473cd76f7f769f6a27434e5f3b3099c6fa3586108749819e56e"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
 
