e68306c242232f1bfd982d91d3ac0dbbbc4041b6
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-21 06:19:14 +0000

    libvisio: update 0.1.7_5 bottle.

--- Formula/libvisio.rb
+++ Formula/libvisio.rb
@@ -11,11 +11,12 @@ class Libvisio < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "7e680fc261f5931963c064165e477f2f9db5be3671e35b702ad6bc2d4a476c95"
-    sha256 cellar: :any, arm64_big_sur:  "ecbbbde8509b32ae1d382dc37676993561c7dfc1eac82502cd6a1aafac054059"
-    sha256 cellar: :any, monterey:       "928901f88d373e8e0f45f59da46cdec7e44b0d22414203a1333986431a20dc4b"
-    sha256 cellar: :any, big_sur:        "3c66b712f07830c60a548d529a67eebc443a8dc3ca655b59ce78ffd7020bc5cc"
-    sha256 cellar: :any, catalina:       "4fd2ae7f2b825a64f2a246e1bff48440dff2b133745f0f4833cc72cdb30dce1a"
+    sha256 cellar: :any,                 arm64_monterey: "7e680fc261f5931963c064165e477f2f9db5be3671e35b702ad6bc2d4a476c95"
+    sha256 cellar: :any,                 arm64_big_sur:  "ecbbbde8509b32ae1d382dc37676993561c7dfc1eac82502cd6a1aafac054059"
+    sha256 cellar: :any,                 monterey:       "928901f88d373e8e0f45f59da46cdec7e44b0d22414203a1333986431a20dc4b"
+    sha256 cellar: :any,                 big_sur:        "3c66b712f07830c60a548d529a67eebc443a8dc3ca655b59ce78ffd7020bc5cc"
+    sha256 cellar: :any,                 catalina:       "4fd2ae7f2b825a64f2a246e1bff48440dff2b133745f0f4833cc72cdb30dce1a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ce944eadfcdd306e2d490035577631ba05cbb2dc8a63976e967033b4ceae06cb"
   end
 
   depends_on "cppunit" => :build
