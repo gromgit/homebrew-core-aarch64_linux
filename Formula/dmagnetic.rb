00aea9e50ee3b85f34ddac9860458368cf5285da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-21 12:10:47 +0000

    dmagnetic: update 0.33 bottle.

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -11,13 +11,12 @@ class Dmagnetic < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cce9bb5d4d70d513016c7f2d4f3493a659ac4a81b01fadff20b93e0df2ce69e0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8780abff0b126cca9e22f89a1e84c6cb9187c5638743764329b84df8063098a"
-    sha256 cellar: :any_skip_relocation, monterey:       "8c6e154812b219b37d2764643d28b8ad97f8e91215a155f36df93f1997c2c2a0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bd532ceab76672bdbcb72e04c88eda4095c60f1c14bea92ca44055a175e886da"
-    sha256 cellar: :any_skip_relocation, catalina:       "785b5fdcba51200e389341550837690f9796139e50770831d8d08a36a2acfafb"
-    sha256 cellar: :any_skip_relocation, mojave:         "31873834919bd8ddd75ce8c608f8866894c369c1bcc1fa46e4778bb6531199ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cc01bdeff82b14153f599b72b7d92de4eb5d037c0f4b8cdcbd90e3fe0b0a5b2d"
+    sha256 arm64_monterey: "885e8f3ef646dae1dace1b98aaece45f34cbe2620df6ec3bfce1a365389846c9"
+    sha256 arm64_big_sur:  "71ba39c336103b2afbd4e56bc83757f1670f605fe9aaabe57ffefc4f8d81d062"
+    sha256 monterey:       "0aa1ab87277e0f6906392207207b1201f4dc95f7d7c711f1ac804cf0e5a7ddd9"
+    sha256 big_sur:        "c7ab46e3d6a4417e636f98ad4d57c710f5ff21be10bdbaea256f73a1c3882543"
+    sha256 catalina:       "74a2565a3f522514d5fa43421d117c296294e8fc87b067bb5a9774e269106c81"
+    sha256 x86_64_linux:   "2236c9dc5af99734e518c6c3245ce02f14fd5f8596897f7d4efe643e6ec085dd"
   end
 
   def install
