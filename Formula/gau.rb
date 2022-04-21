56567669dffb2c41b3040ad52be7f3aa6d0a90bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-21 05:28:27 +0000

    gau 2.1.1
    Closes #99729.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gau.rb
+++ Formula/gau.rb
@@ -1,8 +1,8 @@
 class Gau < Formula
   desc "Open Threat Exchange, Wayback Machine, and Common Crawl URL fetcher"
   homepage "https://github.com/lc/gau"
-  url "https://github.com/lc/gau/archive/v2.0.9.tar.gz"
-  sha256 "3a83671c77e6040ada89f8a53e7cca566b67cc9a2b2c788d2f1d782f365adbf4"
+  url "https://github.com/lc/gau/archive/v2.1.1.tar.gz"
+  sha256 "17bd5b8e3a6a554d82ab5de66bc1f34ce2810a3ba6b8e1456432faff9b191c8f"
   license "MIT"
   head "https://github.com/lc/gau.git", branch: "master"
 
