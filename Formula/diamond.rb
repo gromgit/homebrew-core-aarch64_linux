1b50d40cb2ed4e285ea428552a9be72894f5ca79
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-22 11:04:42 +0000

    diamond: update 2.0.15 bottle.

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -6,12 +6,12 @@ class Diamond < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b25c5a7ba1e13f49d35da8b4900cb32cd0c2f7b4fe50c817c13d8b3e34f0ca60"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab37a61dbad817a0e4dca0310e0473420fde013ffc15f7af5725cde4be961cb0"
-    sha256 cellar: :any_skip_relocation, monterey:       "ce33ea102c71dd819f4f9d9e3ba5ebffaa259de08e768c1ceeebec4a42684628"
-    sha256 cellar: :any_skip_relocation, big_sur:        "72fb7f8ebf9a4ab1cc82ff79b935d37131c00e8cbbae8d9fe159d781d93b30f7"
-    sha256 cellar: :any_skip_relocation, catalina:       "431477a165d719fa67ece1e9bedea04c0377d6a7d4513f631d8bf1ad2e9d859f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d2e5729a550d2d2394448aaebd968875ecacef041789a0bab314e398701bfe8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b1e1daedcd1ea44e29b70859d57bcd6f0983b9d38ac27f32e63b1072002f452c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa0e405531a810c30e7e86414dd6bc5a1a3d362af243329008623c703134dafb"
+    sha256 cellar: :any_skip_relocation, monterey:       "63dd3620cc1447aeb81b99e636301489ca6d2d3ba6a42f7c3c3432eacf82539b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "619e8ff1bbfe88b3336831c3f324ba076461cdbc5a9cf8638d510ca0bf1a9dd3"
+    sha256 cellar: :any_skip_relocation, catalina:       "392f3045b0972f2ec771f1afd3349da404e0f083b57115dc093125247d22e5db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c3558c6daf8a9f202cc9c1ae5081aef8a5671f4b0879229e6b6d1e886fb91ce"
   end
 
   depends_on "cmake" => :build
