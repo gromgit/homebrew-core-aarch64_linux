4dab5af2f8577f19df2fafe480b9f880e0a53fb2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-22 13:20:36 +0000

    bmake: update 20220418 bottle.

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -11,12 +11,12 @@ class Bmake < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f154178c943f7d57a7afe81d12e0812fd60db4a71418e00ac7f74347ca628366"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2bffee032b042a7b31f30cd1dbf8762d4226487f0cdea6e02770057c8cbbace4"
-    sha256                               monterey:       "e4b88b5b338cd15aee868373330659ec18a4acfb61fbd26da9bb788dc70a82f8"
-    sha256                               big_sur:        "793f8f16fd82309cb8bddfa0b0d367e2b158362748b33230e3de48816f8bbc3f"
-    sha256                               catalina:       "c25d1e795818a73984daf27c544ee9c06cda1b994dd214a3880de97976dbef3f"
-    sha256                               x86_64_linux:   "1e8093d067c905bb6565294e75ff4d4f14ca0e3e8a74832b236e98907e825b9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea6d347e27b41d0fe9e672910c829123de6563da61b665b2d0f17a45d7f14994"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "801ad5c98a414c1efc6be2d83ffe15c2c25af0ef3d973421ea2a1ed6864856bc"
+    sha256                               monterey:       "cc70133aa230c37eca6973c9759b8d08ffcc1d3ffc9f147f8ac73cc978d075b9"
+    sha256                               big_sur:        "467fbaa19a541763345a973dc9041a2a88c6ad4eef1e31bf660814c32bdff409"
+    sha256                               catalina:       "a9d6f26a78e81862230f1cee75746431fb049235dd811028b032fe6f52716bec"
+    sha256                               x86_64_linux:   "e420bc8442cf5eb48296e98d142a9de2000baf2b600444c1f6cd1e4937f2725d"
   end
 
   def install
