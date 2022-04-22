c0365a1c6176e01f08522e67f1155d2494eb1c36
Sanchay <sanchay@betteromics.com>
2022-04-22 05:04:39 -0700

    excel-compare 0.7.0
    * excel-compare 0.7.0
    * Update excel-compare.rb
    * Update excel-compare.rb
    
    Closes #99839.
    
    Co-authored-by: Sanchay Harneja <sanchay.h@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/excel-compare.rb
+++ Formula/excel-compare.rb
@@ -1,8 +1,8 @@
 class ExcelCompare < Formula
   desc "Command-line tool (and API) for diffing Excel Workbooks"
   homepage "https://github.com/na-ka-na/ExcelCompare"
-  url "https://github.com/na-ka-na/ExcelCompare/releases/download/0.6.1/ExcelCompare-0.6.1.zip"
-  sha256 "9da80954db03f23ebcb6571fdd24520a020a0ea05f46fbe4aba8e2af54c6048c"
+  url "https://github.com/na-ka-na/ExcelCompare/releases/download/0.7.0/ExcelCompare-0.7.0.zip"
+  sha256 "bf5709fc7c86a59f6f535685b0e08a7c8bcb73c48c4c03e4d54b1fd816c90825"
   license "MIT"
 
   bottle do
@@ -12,17 +12,17 @@ class ExcelCompare < Formula
   depends_on "openjdk"
 
   resource "sample_workbook" do
-    url "https://github.com/na-ka-na/ExcelCompare/raw/0.6.1/test/resources/ss1.xlsx"
+    url "https://github.com/na-ka-na/ExcelCompare/raw/0.7.0/src/test/resources/ss1.xlsx"
     sha256 "f362153aea24092e45a3d306a16a49e4faa19939f83cdcb703a215fe48cc196a"
   end
 
   def install
-    libexec.install Dir["bin/dist/*"]
+    libexec.install Dir["lib/*"]
 
     (bin/"excel_cmp").write <<~EOS
       #!/bin/bash
       export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
-      exec "${JAVA_HOME}/bin/java" -ea -Xmx512m -cp "#{libexec}/*" com.ka.spreadsheet.diff.SpreadSheetDiffer "$@"
+      exec "${JAVA_HOME}/bin/java" -ea -cp "#{libexec}/*" com.ka.spreadsheet.diff.SpreadSheetDiffer "$@"
     EOS
   end
 
