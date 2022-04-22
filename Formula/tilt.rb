e3f402274933857e25ed0bd3d56bece2c898b6e8
Tilt Dev <it@tilt.dev>
2022-04-22 18:02:11 +0000

    tilt 0.27.3
    Closes #99849.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -2,8 +2,8 @@ class Tilt < Formula
   desc "Define your dev environment as code. For microservice apps on Kubernetes"
   homepage "https://tilt.dev/"
   url "https://github.com/tilt-dev/tilt.git",
-    tag:      "v0.27.2",
-    revision: "325f5b6d4c680375fec7cc6d3918cd3c0f847e93"
+    tag:      "v0.27.3",
+    revision: "52e5ccaf646c79a0234ca9f0e031331f3a470438"
   license "Apache-2.0"
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
