28e7f1d9497b89b01fa19e202e00209e67ba8efd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-22 11:04:47 +0000

    newrelic-cli: update 0.45.2 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -7,12 +7,12 @@ class NewrelicCli < Formula
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c51f99ef15e9e8da7016a80e4b436f529a358aac8c27a7e5f0a558df1579959e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7e36917f7a029b877585316d343699615a94a5d70688f03a52d33cfb13370555"
-    sha256 cellar: :any_skip_relocation, monterey:       "aae48a13f30330601f0a5836a66f5fbfd7930cd241f6aca133820e20d1b2d971"
-    sha256 cellar: :any_skip_relocation, big_sur:        "36a3391e3e3ea1536a8a408c1edfcaef604da09cf02e50f700f9a262a5e2913c"
-    sha256 cellar: :any_skip_relocation, catalina:       "c5181c4bebb4d1d031801a3a7e28b7699380899c54fbfb4e22ea98d9bfe6da1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6e400466239e9cf3bf16a954b2dd923f26fa8e41197cb3710f1a4d9e838cd2b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7cb5aac6745a25ae47a9c7ead7eb08f4983841426a7bf65832c1b2ae79c5c536"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eeca964e2f55ef5c3dfccc1a24f321ec4169c63b1b089a10b972c37544dd2b2b"
+    sha256 cellar: :any_skip_relocation, monterey:       "177f99596b4d7dfb076a5bb9a275a7286743fa44e4b0b438844a30c5573b494d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5aec6f98c880481ac7e44dd6733a2d61ff8aa9a08769ce5b26bde47edc4ec078"
+    sha256 cellar: :any_skip_relocation, catalina:       "4c9b07a6eb9b0de3c5420c460777d5103048489bc0c39eed4b812d3068f3bb1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75a8074425bc9a3b6461e9f42d1a891a25b1ae4c137e2ac6506b952a5878c5b3"
   end
 
   depends_on "go" => :build
