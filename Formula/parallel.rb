57b4a48cc7b5f97d3e1728c599ba86b2cf01e69e
Porkepix <porkepix@gmail.com>
2022-04-24 01:13:05 +0200

    parallel 20220422
    Closes #99964.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20220322.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220322.tar.bz2"
-  sha256 "df93ccf6a9f529ad2126b7042aef0486603e938c77b405939c41702d38a4e6d8"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20220422.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220422.tar.bz2"
+  sha256 "96e4b73fff1302fc141a889ae43ab2e93f6c9e86ac60ef62ced02dbe70b73ca7"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
