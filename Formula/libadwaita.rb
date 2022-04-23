76977c038b9462f880dc94e5b8cde67c797dccaa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-23 07:04:28 +0000

    libadwaita: update 1.1.1 bottle.

--- Formula/libadwaita.rb
+++ Formula/libadwaita.rb
@@ -14,12 +14,12 @@ class Libadwaita < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e180b42f0cfda95c43b8d973eff6112e2a34ad8ccb21e6176802b775ac8fb053"
-    sha256 arm64_big_sur:  "1de51d9ddba98d15390d2c3b23f5ff6b07c79086d7fd87e3013276cd7e0e09db"
-    sha256 monterey:       "76e1ef7e7be0cac54891951b76925eece61a47a9abf6ace7496f4dee65c3809a"
-    sha256 big_sur:        "3727c2f743a37632469ed224b78b8703c85ef0c37a457a7a09ae17ac8390cc7e"
-    sha256 catalina:       "001e7972c6a6e6d1e117675bbc6164af899903aa72d0cbab6bc3ce9d2b818506"
-    sha256 x86_64_linux:   "d88bd980f437c8500090bd27961da6a12e5dbbe13ae790aa8f96fed28a1b2b8a"
+    sha256 arm64_monterey: "5c6234832fe8d366f2050cef5954a11e00c2d8723c15b8c11ad6ab5aa4e63000"
+    sha256 arm64_big_sur:  "6036c1f23448039f0e716db5acc98a8c2e8886b87f8fbf0a98e92709eac3c621"
+    sha256 monterey:       "7a55dff30538fc820c1002c26d57ea9c5e79650c6c65cfd65c12662bfc4b532f"
+    sha256 big_sur:        "ab60d5de5b3c1d9957d06a2e4f11662fb541af8e4b1e28411c790325fa478918"
+    sha256 catalina:       "1f7fef555d61730f4a5829d61451a8fb78168e29b66d66c624f69f167f70f6b8"
+    sha256 x86_64_linux:   "ddaf7ea295e44050179461ecac915fc0435a3427c77df71d329ca7c154a037ef"
   end
 
   depends_on "gobject-introspection" => :build
