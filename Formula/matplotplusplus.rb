d67c8ab63c01358c82fe069d44e88e51195e0208
Daniel Nachun <daniel.nachun@gmail.com>
2022-04-22 19:57:42 -0700

    matplotplusplus: fix build on Linux (#99612)

--- Formula/matplotplusplus.rb
+++ Formula/matplotplusplus.rb
@@ -23,10 +23,16 @@ class Matplotplusplus < Formula
   depends_on "libtiff"
   depends_on "openexr"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
   fails_with :clang do
     build 1100
     cause "cannot run simple program using std::filesystem"
   end
+
+  fails_with gcc: "5"
 
   def install
     mkdir "build" do
@@ -38,6 +44,9 @@ class Matplotplusplus < Formula
   end
 
   test do
+    # Set QT_QTP_PLATFORM to "minimal" on Linux so that it does not fail with this error:
+    # qt.qpa.xcb: could not connect to display
+    ENV["QT_QPA_PLATFORM"] = "minimal" unless OS.mac?
     cp pkgshare/"examples/exporting/save/save_1.cpp", testpath/"test.cpp"
     system ENV.cxx, "-std=c++17", "test.cpp", "-L#{lib}", "-lmatplot", "-o", "test"
     system "./test"
