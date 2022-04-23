15fbdfbd3e2566342c006257066752819e36db08
Alexander Bayandin <a.bayandin@gmail.com>
2022-04-23 15:58:54 +0000

    pjproject: update 2.11.1 bottle.

--- Formula/pjproject.rb
+++ Formula/pjproject.rb
@@ -12,10 +12,12 @@ class Pjproject < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "dd65ff7722e647f5441aad3eba86a3493274f4850169b3332b7c2527ba38031d"
-    sha256 cellar: :any,                 big_sur:      "31319f83ddc57bc93650d5fc1890869b823ebc04f8584a9c280e23ab8d8e2caf"
-    sha256 cellar: :any,                 catalina:     "90b5e44910157070b4e1d9b494076c30cf16d9b1ef9790cfa080331348dfea89"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "51fbe4542de05e29af4379cc48b8702560032197b829033806c85a0c70e04928"
+    sha256 cellar: :any,                 arm64_monterey: "ae7ac8144de063925b9ddcdfac52ec5cfa8286fc0e21255ef1cc11b36270b726"
+    sha256 cellar: :any,                 arm64_big_sur:  "ebe2c56bf87c397d2e2269acfbfc345b54f3a8b117515fad1827c20da620a227"
+    sha256 cellar: :any,                 monterey:       "dd65ff7722e647f5441aad3eba86a3493274f4850169b3332b7c2527ba38031d"
+    sha256 cellar: :any,                 big_sur:        "31319f83ddc57bc93650d5fc1890869b823ebc04f8584a9c280e23ab8d8e2caf"
+    sha256 cellar: :any,                 catalina:       "90b5e44910157070b4e1d9b494076c30cf16d9b1ef9790cfa080331348dfea89"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51fbe4542de05e29af4379cc48b8702560032197b829033806c85a0c70e04928"
   end
 
   depends_on macos: :high_sierra # Uses Security framework API enum cases introduced in 10.13.4
