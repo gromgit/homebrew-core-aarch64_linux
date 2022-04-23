aebb4ee0f81f0591ac0b13aab9a0a9266a78b0b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-23 15:04:35 +0000

    dasel: update 1.24.3 bottle.

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -7,12 +7,12 @@ class Dasel < Formula
   head "https://github.com/TomWright/dasel.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d847f926532d59d4c341cb1b52ed814ad2facb05b783ddff1da39ce274ca8a76"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "be5610bcfbe142228787b903afb275bb6cf34cde298e5e8b4a6cd2d4ace73cfe"
-    sha256 cellar: :any_skip_relocation, monterey:       "8eabf8630581f24875801946915049e43da4722b2a3e303c23bb57091d94e04c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0c369a3739e2446f1d33917626f436286e88355ff52ade0b54e951ea56d8cdd8"
-    sha256 cellar: :any_skip_relocation, catalina:       "c2d969cf7cf64481e5bd30db2dfcda2d858582c5cff0ce07083d79dad54d348b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8a7d819d8471b4243fb7c01c276c587da39bfbae2db45543bdfc46aede23853f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3209bc7e6ac32b5ef767b6b7f4ae961f4c3c5dec60230d5f606bc332a48c0be5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "76c8a2470a82485705f18bdc75aa30466c9eec3f77fb86ae6618ab9c8de18126"
+    sha256 cellar: :any_skip_relocation, monterey:       "ccd8b02b9dd2a695b05e047ec03dffc1df4d35dfc185f2c2cc6b05744e6eec58"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0f542cc5e063d01b5673d6265a0f99abf455a165f8e2d533799b53edb9b9bc5a"
+    sha256 cellar: :any_skip_relocation, catalina:       "b275412822dd45d3efac2f90f88f4309c9f89b6f7a13f9eb864319c6f08dae3d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69d30163cf4b4e1360d67d768bc5d5b2a475b43998e044fe9d1f3f1639482e7a"
   end
 
   depends_on "go" => :build
