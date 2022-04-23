baa96a795b3ee774921f82ba19a08f23a6a276a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-23 19:04:24 +0000

    git-crypt: update 0.7.0 bottle.

--- Formula/git-crypt.rb
+++ Formula/git-crypt.rb
@@ -11,15 +11,12 @@ class GitCrypt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "546a3085d07478b8658337c472bc1e90fd685b898478aff541957ae56a2cea2f"
-    sha256 cellar: :any,                 arm64_big_sur:  "b30c2ac4ab305cc72b8be7253b7bf3dcca3487a579ebf00e21da793d4afc8bd4"
-    sha256 cellar: :any,                 monterey:       "cf130a9cb498f2ee2a4227d4971053ec991a2c084161fd6334b96678a958bc23"
-    sha256 cellar: :any,                 big_sur:        "7567932a504ce3c08a087f9d3d020f5ca8307f41fe2a16a843e7df862120abc9"
-    sha256 cellar: :any,                 catalina:       "f38bb645c3eff62cfb43802199370d85e4785fcf10c063e4d7453e032788bcba"
-    sha256 cellar: :any,                 mojave:         "89d2058a4dd5afc565696707c8e93621fd644f9ab303fe378727ae999783d156"
-    sha256 cellar: :any,                 high_sierra:    "0d2cf3c93ab2ca4059163f8da8a3ab845b566b13debf5e1b43a734dc86138a18"
-    sha256 cellar: :any,                 sierra:         "6b2c2773e5c327282d461f5d49600928ae97d432e5f4d8b7acfcaaa6e6d1ef68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13febdb36a936377e0ff5f78883dbaba43f70cb9f91ec7fee833d3057f92d85d"
+    sha256 cellar: :any,                 arm64_monterey: "d86478df1906cc9337c7ea815005c1bbd7e970462476cb03fa645afb06c5eb68"
+    sha256 cellar: :any,                 arm64_big_sur:  "7adc43408c0cbea8bf9d5f01a70e5559d3282062d40cc99a6bfdd831aacea10c"
+    sha256 cellar: :any,                 monterey:       "2f3fe932411fdcf3e156bef702067812d395b65d95319e62b6e9308d7cf487fc"
+    sha256 cellar: :any,                 big_sur:        "c6080d2eaa5f60f0a7236137d6bc7ae18f707ad2c95f7a5f28d601732eda8fa2"
+    sha256 cellar: :any,                 catalina:       "a12782583f1f22cd862db9b4b6507044066623fff29d59123b4ddc35fb2bcd21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62d497660a8206870bb049f002cdac71550c654da92ca447e3ade56895bbd039"
   end
 
   depends_on "openssl@1.1"
