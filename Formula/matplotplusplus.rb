1bba7cc822af5b25181e99447ffad53a392cc22a
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-23 03:53:50 +0000

    matplotplusplus: update 1.1.0_1 bottle.

--- Formula/matplotplusplus.rb
+++ Formula/matplotplusplus.rb
@@ -7,11 +7,12 @@ class Matplotplusplus < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "da37512436dab0d5d57a352a3af69cfd93408afbcda7b81e91dc16f299981c90"
-    sha256 cellar: :any, arm64_big_sur:  "74d1140f6be97349816d42070c8e3846ac64be9798c0b3e2e72e8906f117baf1"
-    sha256 cellar: :any, monterey:       "c44a46d9b834941888f3e3c6cae35d544659012620d68d20bc2a2a79e72c3c61"
-    sha256 cellar: :any, big_sur:        "8803e7a64e17c43e5dfb22dd6286b3693477e28be948817afda4c51e9cbdba2c"
-    sha256 cellar: :any, catalina:       "6662b7fbec4d171db186c4dbf1d64648a15a718a24a3ab0c59bdd635b401c578"
+    sha256 cellar: :any,                 arm64_monterey: "da37512436dab0d5d57a352a3af69cfd93408afbcda7b81e91dc16f299981c90"
+    sha256 cellar: :any,                 arm64_big_sur:  "74d1140f6be97349816d42070c8e3846ac64be9798c0b3e2e72e8906f117baf1"
+    sha256 cellar: :any,                 monterey:       "c44a46d9b834941888f3e3c6cae35d544659012620d68d20bc2a2a79e72c3c61"
+    sha256 cellar: :any,                 big_sur:        "8803e7a64e17c43e5dfb22dd6286b3693477e28be948817afda4c51e9cbdba2c"
+    sha256 cellar: :any,                 catalina:       "6662b7fbec4d171db186c4dbf1d64648a15a718a24a3ab0c59bdd635b401c578"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac10208b0596edfca04508461d1a620813b432b7a5a8a0182c12a5f53e8b70ed"
   end
 
   depends_on "cmake" => :build
