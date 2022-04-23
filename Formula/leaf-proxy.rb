b4276b0316607925e70cde0cf54fab99eb79f857
Rui Chen <rui@chenrui.dev>
2022-04-23 02:35:46 -0400

    leaf-proxy 0.5.0
    * leaf-proxy 0.5.0
    * Update leaf-proxy.rb
    * leaf-proxy: use lwip from upstream
    
    Closes #99910.
    
    Co-authored-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/leaf-proxy.rb
+++ Formula/leaf-proxy.rb
@@ -1,8 +1,8 @@
 class LeafProxy < Formula
   desc "Lightweight and fast proxy utility"
   homepage "https://github.com/eycorsican/leaf"
-  url "https://github.com/eycorsican/leaf/archive/v0.4.4.tar.gz"
-  sha256 "6ce46ba0eb4b357fccc36b3be5bdab32dd6879b53130a496b7a8790a47b14f26"
+  url "https://github.com/eycorsican/leaf/archive/v0.5.0.tar.gz"
+  sha256 "34e2c067a7fc3e03a4eb7f7d24888d8a7895fdfdf35490865560a655eaf90bb8"
   license "Apache-2.0"
   head "https://github.com/eycorsican/leaf.git", branch: "master"
 
@@ -19,14 +19,7 @@ class LeafProxy < Formula
 
   conflicts_with "leaf", because: "both install a `leaf` binary"
 
-  resource "lwip" do
-    url "https://github.com/eycorsican/lwip-leaf.git",
-        revision: "86632e2747c926a75d32be8bd9af059aa38ae75e"
-  end
-
   def install
-    (buildpath/"leaf/src/proxy/tun/netstack/lwip").install resource("lwip")
-
     cd "leaf-bin" do
       system "cargo", "install", *std_cargo_args
     end
@@ -42,6 +35,6 @@ class LeafProxy < Formula
     EOS
     output = shell_output "#{bin}/leaf -c #{testpath}/config.conf -t SS"
 
-    assert_match "dispatch to outbound SS failed", output
+    assert_match "TCP failed: all attempts failed", output
   end
 end
