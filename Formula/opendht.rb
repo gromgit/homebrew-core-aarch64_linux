077e0bafbd39503478cfcdd0c1a18de12ee181af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-25 19:09:41 +0000

    opendht: update 2.4.3 bottle.

--- Formula/opendht.rb
+++ Formula/opendht.rb
@@ -6,12 +6,12 @@ class Opendht < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e52c964ad81c338f401e4b291406c1fe2ce63340661294f10acff61612eb2b50"
-    sha256 cellar: :any,                 arm64_big_sur:  "a291ba71d6ae75df8efee0d05e35a78b8ff6e6712afc2c74dc1ffd76b1a111a9"
-    sha256 cellar: :any,                 monterey:       "d2bf5a2d95dc4a210e0e836fe5b4da21c9843f6921b6d106fca9e7e4451706c4"
-    sha256 cellar: :any,                 big_sur:        "7d82f3e24cde6aad0a834a1c3830eee8c32746d1d812f74414615ff1fa31835b"
-    sha256 cellar: :any,                 catalina:       "441ac4e81dc49438410de99af317fe1ac93190af9f5f366ec0c656804287f3d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0628e6a79543d5f554910a7669f911e9503d308a17dc025209feb1e2e733415"
+    sha256 cellar: :any,                 arm64_monterey: "7dabc73c643cb66bb5c10264216acd346fd2dc73400b54a5e7e5334483981a05"
+    sha256 cellar: :any,                 arm64_big_sur:  "56610b9952fef8031f9c13e868c8e630fdacea01e300aa9113ffec92a8d1fcf1"
+    sha256 cellar: :any,                 monterey:       "edb40db3e6c3a54b3f3e224880b409b0e17da7b1c9a4a23706912d06ae0a1c22"
+    sha256 cellar: :any,                 big_sur:        "1547259e336a0c55ac4000ccd462afc66f9c6255dfc100f1461f6aeb1ad55c6a"
+    sha256 cellar: :any,                 catalina:       "0ca0cdce79dda6ed1b98f184efc7f3bdde63d3507a7574c075a07a393f4579e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63553c7fc05cb84f229073a54f124138c5deec54c38f870c9e9eca6eab048e75"
   end
 
   depends_on "cmake" => :build
