82a65acf9728b1671167cc38f0a2634d967ef789
Rui Chen <rui@chenrui.dev>
2022-04-25 15:36:49 +0000

    ddh: add 0.12.0 bottle.

--- Formula/ddh.rb
+++ Formula/ddh.rb
@@ -5,6 +5,15 @@ class Ddh < Formula
   sha256 "f16dd4da04852670912c1b3fd65ce9b6ebd01ba2d0df97cb8c9bdf91ba453384"
   license "LGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bddfd92127ffb3ef31b00d86ec550206148c166d1c06482598377d994c4bc373"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4623ac62b866bfe712e7a773913868d2c8d961e90817a7bf71d4ba215c331aa3"
+    sha256 cellar: :any_skip_relocation, monterey:       "5e34e90440cbaedafa0d6a6d5f1c31ab39c237b97f2622cea06343aef091dcf6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5a297fb91f7a38ea49776e3c74815fe9163087e29aaeac6dd573e9aa8e727102"
+    sha256 cellar: :any_skip_relocation, catalina:       "5ff60c70bc0f14279c88ca8d9169801c2ba9e6b9fd23034c36241c13acdcfcc6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a74d6464ee92da9d5b8c5a979a8b86a53dd54f7677a4d7a1ca6e5ca9b8df96db"
+  end
+
   depends_on "rust" => :build
 
   def install
