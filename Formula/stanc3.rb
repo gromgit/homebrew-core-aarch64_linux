e9bd993e5bb78d6cf63e2c173b2afd6fd03e0614
Andrew Kane <andrew@ankane.org>
2022-04-25 04:15:38 -0700

    stanc3 2.29.2 (new formula)
    Closes #99930.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/stanc3.rb
@@ -0,0 +1,38 @@
+class Stanc3 < Formula
+  desc "Stan transpiler"
+  homepage "https://github.com/stan-dev/stanc3"
+  # git is needed for dune subst
+  url "https://github.com/stan-dev/stanc3.git",
+      tag:      "v2.29.2",
+      revision: "2c254b3b11a4dbb82395f016886b557201dad130"
+  license "BSD-3-Clause"
+
+  depends_on "ocaml" => :build
+  depends_on "opam" => :build
+
+  uses_from_macos "unzip" => :build
+
+  def install
+    Dir.mktmpdir("opamroot") do |opamroot|
+      ENV["OPAMROOT"] = opamroot
+      ENV["OPAMYES"] = "1"
+      ENV["OPAMVERBOSE"] = "1"
+
+      system "opam", "init", "--no-setup", "--disable-sandboxing"
+      system "bash", "-x", "scripts/install_build_deps.sh"
+      system "opam", "exec", "dune", "subst"
+      system "opam", "exec", "dune", "build", "@install"
+
+      bin.install "_build/default/src/stanc/stanc.exe" => "stanc"
+      pkgshare.install "test"
+    end
+  end
+
+  test do
+    assert_match "stanc3 v#{version}", shell_output("#{bin}/stanc --version")
+
+    cp pkgshare/"test/integration/good/algebra_solver_good.stan", testpath
+    system bin/"stanc", "algebra_solver_good.stan"
+    assert_predicate testpath/"algebra_solver_good.hpp", :exist?
+  end
+end
