3553db2f51be68dea5f682766f57bb67ae382b02
rami3l <rami3l@outlook.com>
2022-04-25 22:21:31 +0200

    gleam 0.21.0
    Closes #100092.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -1,8 +1,8 @@
 class Gleam < Formula
   desc "✨ A statically typed language for the Erlang VM"
   homepage "https://gleam.run"
-  url "https://github.com/gleam-lang/gleam/archive/v0.20.1.tar.gz"
-  sha256 "2cd909bdef561b480a536be20606a7617d19680e067c8d4a14d0f05fb6a0cd22"
+  url "https://github.com/gleam-lang/gleam/archive/v0.21.0.tar.gz"
+  sha256 "9abd3ec53a2c7758e59d7f9d30ecff25cf193e7c161f380d71293a2d5b82e098"
   license "Apache-2.0"
 
   bottle do
