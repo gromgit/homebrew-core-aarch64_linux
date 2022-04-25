6bdd0a5b810753a145cd715d0e6a5db969b35166
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-25 08:10:33 +0000

    checkstyle: update 10.2 bottle.

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -6,7 +6,7 @@ class Checkstyle < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6b8eec9c0bebac1004dc1c55ea9b60a5c12fb07d4e0ff112b8e6857f76ead43e"
+    sha256 cellar: :any_skip_relocation, all: "53e644cd52b4972a15aefaa1d5f3f9a713e3de4d0a84bb07ada5babc56a1b56e"
   end
 
   depends_on "openjdk"
