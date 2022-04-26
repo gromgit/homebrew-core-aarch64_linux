242fc7928e4c3df59ccc4d11df505638b61fee3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 12:11:09 +0000

    wcslib: update 7.11 bottle.

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -11,12 +11,12 @@ class Wcslib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7201671c6a32549d357a616926be707e2e578d86fe6a491d368ae603f4dd726f"
-    sha256 cellar: :any,                 arm64_big_sur:  "221cc7e8d8872400912339897cd01454ea5597c45cad9c9a380f5aa015a140ff"
-    sha256 cellar: :any,                 monterey:       "b1771728f18d06004a29d45f511a6f45beca469e3138392eec818c7f7fe58de9"
-    sha256 cellar: :any,                 big_sur:        "3a83285077a761232eef2acbc4cacdadde4126c770724f35b3eb91cf51d8022f"
-    sha256 cellar: :any,                 catalina:       "6754297d561baf8393433faceab38bd75df3eb01214c0b3affe10032c3755b2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "672880fb76adb3d8d5e927913a0062b41a781df96e97437e32ba1da552b2a852"
+    sha256 cellar: :any,                 arm64_monterey: "ba8a5a2e24278f12c7888731a70c4a71a09a92c2f6fb8ffc07fb4e62fa369bb4"
+    sha256 cellar: :any,                 arm64_big_sur:  "236fb88295dfe22ed4859cb53c90d6c611c8782ae626ae90895686829b98f6a3"
+    sha256 cellar: :any,                 monterey:       "4711e76f1cf788259eea9171fa196a229e332ebb706d27735d4796ce4bea8d43"
+    sha256 cellar: :any,                 big_sur:        "bcd0f7f0f62c2ab101d3847132ccce9a3e4794aba5cebc25f3328b418c9cfdd6"
+    sha256 cellar: :any,                 catalina:       "bf858d0c1f059f078c01dac277c6daebbfcd062ea45774948322b7fdf072f9f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d48dade506586e08e6e297e8c8c3c04dacb43a8b96381d50ea0360f4985bd54"
   end
 
   depends_on "cfitsio"
