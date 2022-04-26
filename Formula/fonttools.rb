801cecffff12a0f5c0e8d1bee50822b1d610eab5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 12:11:15 +0000

    fonttools: update 4.33.3 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,12 +9,12 @@ class Fonttools < Formula
   head "https://github.com/fonttools/fonttools.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0fbf5291b68997571458349d72d327cb2a215fe4242a942ec85e74d017483da7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0fbf5291b68997571458349d72d327cb2a215fe4242a942ec85e74d017483da7"
-    sha256 cellar: :any_skip_relocation, monterey:       "0b2196edadba358c09f056d1d5eb23f00771cd5b3c0ce317da176b26fdf5d546"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b2196edadba358c09f056d1d5eb23f00771cd5b3c0ce317da176b26fdf5d546"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b2196edadba358c09f056d1d5eb23f00771cd5b3c0ce317da176b26fdf5d546"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "855102c6543427aa4cc08141bcb913a97310f0f8938a5d595d990734d0418c0d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "56f41f0375107a44669e6a82555de8b22f2867b47bc16dfaba7f4ff2e0a9cabc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56f41f0375107a44669e6a82555de8b22f2867b47bc16dfaba7f4ff2e0a9cabc"
+    sha256 cellar: :any_skip_relocation, monterey:       "97e2a260bf9c29b0c35b389c2ccb5b502a98ef218f2ea7ca9cacfff3e0fcefa4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "97e2a260bf9c29b0c35b389c2ccb5b502a98ef218f2ea7ca9cacfff3e0fcefa4"
+    sha256 cellar: :any_skip_relocation, catalina:       "97e2a260bf9c29b0c35b389c2ccb5b502a98ef218f2ea7ca9cacfff3e0fcefa4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7140d4b1148c215d469beee9d1f15cb330398e7051893c8edf800239a778044a"
   end
 
   depends_on "python@3.10"
