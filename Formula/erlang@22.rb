c9ede029e3a42b360f4e393ed86c54666557dcdf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 08:14:46 +0000

    erlang@22: update 22.3.4.25_1 bottle.

--- Formula/erlang@22.rb
+++ Formula/erlang@22.rb
@@ -13,12 +13,12 @@ class ErlangAT22 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "1ddc19ec9f5d05bc4ce353695bfff6bfbe87b72dcfb67d156a904f4d1f290441"
-    sha256 cellar: :any,                 arm64_big_sur:  "3ac8c19abd1acf42258d796a73e022ef39591a7d6994bf610ffbc6f7af91ea50"
-    sha256 cellar: :any,                 monterey:       "3f1d624a0eb6d44e6afb8a4314d302b81ecdbfaeeb61dff57ce913ecbe4b8570"
-    sha256 cellar: :any,                 big_sur:        "c455c6b165d8b584fa4eb770e0cf20c64eb94f2ad0e69b01a0f95ac737d1cc70"
-    sha256 cellar: :any,                 catalina:       "b782227fbb163e41caa5796f4c81308f599a011048b86c80fb0b100985d5efd3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c7a9552e79564ddeea2a1695a3c58febc0e836cfc2b7f41a47b65acfccde280"
+    sha256 cellar: :any,                 arm64_monterey: "fe94c08097f877343680617d8cf297bb6bba5aa62785edf7b0c95f2c1191f576"
+    sha256 cellar: :any,                 arm64_big_sur:  "7fefc63bf9aa8c76d34016da1f62d8914d504b3f898b256a4d6e268ad4f9921d"
+    sha256 cellar: :any,                 monterey:       "5d7407b7351731b0282597ea32853813da2e52ea8ac283e0bfba0a8e8d0d889b"
+    sha256 cellar: :any,                 big_sur:        "9f2cd9905eab2c0febb23740259b06d3c0c8197313525b1ddfec87a5e38da859"
+    sha256 cellar: :any,                 catalina:       "639cdd72c78eb0148e2e7ab502bf7e6b53c5efbda0218bf59f173f9585129bbc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3896d1e7edbd1b5c23f3f0bd3d85a0b46076f0ef30b6de6ce1613caec693e7b7"
   end
 
   keg_only :versioned_formula
