e3a118369f027402fea16394ee9a87526f69fc4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 09:08:19 +0000

    hpack: update 0.35.0 bottle.

--- Formula/hpack.rb
+++ Formula/hpack.rb
@@ -7,12 +7,12 @@ class Hpack < Formula
   head "https://github.com/sol/hpack.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d18bd435a0fe0b891f212320e697f25a35757b51f2ddd581b92b4401ffe213c4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6692c6be0b4c7b1914ee7ec5e83435e38f34d5019414ed7aa64300ad236f150f"
-    sha256 cellar: :any_skip_relocation, monterey:       "f45d2e7079d888906b6920c051ed742906dc4ed23c737d7684fced5e951a3832"
-    sha256 cellar: :any_skip_relocation, big_sur:        "84332baf3417325603d33b6b31e6b906c038eba223eb331f1b5bcd3909a3b806"
-    sha256 cellar: :any_skip_relocation, catalina:       "90a1ac57d94e60b1613494eaa2ec1dea42cd481a41a391958b5c736d515d2812"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff4d2204baa90231b4940640b00d5b2bfd40518a583f0d108c4117603b1200d5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a3a2a22f4ec0040e49ea3c9f0cee6b0aa9088626b145c01db092364f951b87a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "151c61eb08afd855006104db4ad884225afa63b562ef1b5261986c5ad67b85fd"
+    sha256 cellar: :any_skip_relocation, monterey:       "b2ca3c047995f49715b8a9f5f7b8e18611d33699b9d910fe3f2c39955bf01be7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9d3a127911fe1c275e37351611d5d8b35bf6009b6006c9df9a163072ee8d1c3c"
+    sha256 cellar: :any_skip_relocation, catalina:       "35be3997828e2752693e8f02b80bfd918f3e9d3707364e8aa0f5db552504a104"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "758f4e3ebcfc75ff2d7a74c9c8a52d55664583e1286f15299cd8777f460be012"
   end
 
   depends_on "cabal-install" => :build
