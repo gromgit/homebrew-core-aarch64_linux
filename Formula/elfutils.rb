7a3fd9b18b3f8f89ae23cc2c5af4dcc5bde6ff4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 05:16:36 +0000

    elfutils 0.187
    Closes #100144.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elfutils.rb
+++ Formula/elfutils.rb
@@ -1,8 +1,8 @@
 class Elfutils < Formula
   desc "Libraries and utilities for handling ELF objects"
   homepage "https://fedorahosted.org/elfutils/"
-  url "https://sourceware.org/elfutils/ftp/0.186/elfutils-0.186.tar.bz2"
-  sha256 "7f6fb9149b1673d38d9178a0d3e0fb8a1ec4f53a9f4c2ff89469609879641177"
+  url "https://sourceware.org/elfutils/ftp/0.187/elfutils-0.187.tar.bz2"
+  sha256 "e70b0dfbe610f90c4d1fe0d71af142a4e25c3c4ef9ebab8d2d72b65159d454c8"
   license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-only"]
 
   livecheck do
