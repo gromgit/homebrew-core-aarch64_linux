b2f692c77b123d479d5fe7e4377674d6a46ee27d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 20:19:29 +0000

    prestodb: update 0.272_1 bottle.

--- Formula/prestodb.rb
+++ Formula/prestodb.rb
@@ -17,7 +17,7 @@ class Prestodb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4f52ed17e534be51ca8a6e6f551850bb948c301e78a467589394edc5152ffdc3"
+    sha256 cellar: :any_skip_relocation, all: "a0343b74214e654e47eba813091834d5646f651c94dc6e4207bdb28fbeb440c8"
   end
 
   # https://github.com/prestodb/presto/issues/17146
