d17cb09a27664621d9425a4d8229289343b2c39d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 05:07:46 +0000

    argo 3.3.3
    Closes #100137.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argo.rb
+++ Formula/argo.rb
@@ -2,8 +2,8 @@ class Argo < Formula
   desc "Get stuff done with container-native workflows for Kubernetes"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj/argo-workflows.git",
-      tag:      "v3.3.2",
-      revision: "35492a1700a0f279694cac874b6d9c07a08265d1"
+      tag:      "v3.3.3",
+      revision: "9c08aedc880026161d394207acbac0f64db29a53"
   license "Apache-2.0"
 
   bottle do
