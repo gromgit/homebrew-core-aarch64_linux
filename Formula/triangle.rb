fbd6c69626731afe0729c97dded353a22dc79e5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-26 05:42:13 +0000

    triangle 2.0.0
    * triangle 2.0.0
    * use go1.18
    
    Closes #100151.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -1,8 +1,8 @@
 class Triangle < Formula
   desc "Convert images to computer generated art using Delaunay triangulation"
   homepage "https://github.com/esimov/triangle"
-  url "https://github.com/esimov/triangle/archive/v1.2.5.tar.gz"
-  sha256 "e7b729601023620aaba3707db80aeeeee06286e131095da25d248a8325d2c549"
+  url "https://github.com/esimov/triangle/archive/v2.0.0.tar.gz"
+  sha256 "071ba2a39b62e7914a233af74e7935ddb7a875bc2a5f193cd43862da65b1c516"
   license "MIT"
 
   bottle do
@@ -14,8 +14,7 @@ class Triangle < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "58a213dc45b97b2bf52c5403f52cdae98793da95dd1ac5b9bbaa6fe56197c346"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", "-mod=vendor", "-o", "#{bin}/triangle", "./cmd/triangle"
