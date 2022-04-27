4969a1d8b57b2dfb6e5537c3e7eaf90c73b1b906
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-27 13:19:48 +0000

    libslirp: update 4.7.0 bottle.

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -6,13 +6,12 @@ class Libslirp < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "475c2d9ad2e1a2b44c52e9ea1cd5627d392adb216b8e9969d059a5e17d9dafc5"
-    sha256 cellar: :any, arm64_big_sur:  "93fca2e4a0c689c366680a9b56ca0c90cb424eb8fb00136a170cea67e7919d67"
-    sha256 cellar: :any, monterey:       "4738978c9d94ebd569757a5c4df3918bf7f8e2aaf75f9d04b63cafcb24b29064"
-    sha256 cellar: :any, big_sur:        "120eb1362ba0645a96ace10153fb41b5669f0a32669947cca96b2e1b3108edd3"
-    sha256 cellar: :any, catalina:       "fc267a6871f5459a38e174ffd519a3016c849b6c99c2bce2ca714c20b71ae1b6"
-    sha256 cellar: :any, mojave:         "3a19812499b688c6698c0e50b1b0a567727cf9a5af26ed71ff99a412a98f1c44"
-    sha256               x86_64_linux:   "e49c9b8b88c0f9cb434cac8b488d38450bc2b148f71ba934c7562437a5b46c58"
+    sha256 cellar: :any, arm64_monterey: "dbfc3fabef1a14eb7807e97bac7e318dbf0ca0ac631cb949cd165ca79c57d16d"
+    sha256 cellar: :any, arm64_big_sur:  "7ff75ad4ca2b56e4df3f139c1a265d1198174b676856f5eb019730d8f97db557"
+    sha256 cellar: :any, monterey:       "82fe4e66ac490e882aaf245d4bdf9382826d82fdb3c7bad29b7dc4e77a5b8657"
+    sha256 cellar: :any, big_sur:        "3b31c4120ac2abad0cb5480576b957d5dd335e9a7cfa4cea0ef7bcd27e6cdcc6"
+    sha256 cellar: :any, catalina:       "63ecdea6ce7b3784bab8f2aa956468a1b90c67bb5e466afd80ff8ecadfe4f09f"
+    sha256               x86_64_linux:   "dda8b4369eada1f246aebcc4f013966dfac010cb84045a5b80e034d9a725f582"
   end
 
   depends_on "meson" => :build
