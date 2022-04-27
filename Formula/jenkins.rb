ef80932c51907a989199617ae1f5f444a5b18167
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-27 05:30:19 +0000

    jenkins 2.345
    Closes #100219.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://www.jenkins.io/"
-  url "https://get.jenkins.io/war/2.344/jenkins.war"
-  sha256 "c0184ffc54d50a0fb6b270f10149dddeebc365dc34fa45e3113a24f844f802cd"
+  url "https://get.jenkins.io/war/2.345/jenkins.war"
+  sha256 "e06a4289afe2877d024e49bf72b53494b912eb2c4f931e00254b5b1dd9fee447"
   license "MIT"
 
   livecheck do
