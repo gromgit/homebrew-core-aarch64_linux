742314ad08375d1fbb317c58c3fbe07aae186acd
Porkepix <porkepix@gmail.com>
2022-04-27 10:24:56 +0200

    libslirp 4.7.0
    Closes #100236.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -1,8 +1,8 @@
 class Libslirp < Formula
   desc "General purpose TCP-IP emulator"
   homepage "https://gitlab.freedesktop.org/slirp/libslirp"
-  url "https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v4.6.1/libslirp-v4.6.1.tar.gz"
-  sha256 "69ad4df0123742a29cc783b35de34771ed74d085482470df6313b6abeb799b11"
+  url "https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v4.7.0/libslirp-v4.7.0.tar.gz"
+  sha256 "9398f0ec5a581d4e1cd6856b88ae83927e458d643788c3391a39e61b75db3d3b"
   license "BSD-3-Clause"
 
   bottle do
