0dcab3952b09c0c5770fcc526915d7019a3cea0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-27 21:09:48 +0000

    kerl: update 2.4.0 bottle.

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -7,7 +7,7 @@ class Kerl < Formula
   head "https://github.com/kerl/kerl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bf06c1b511ee2d1307268028f8794eaeeb9f67262f9842458603a7d1ab6e41c0"
+    sha256 cellar: :any_skip_relocation, all: "d2307a33891bcca81d40c51a2994c4c6b13a29b430bdf7091cc727386ef9ca13"
   end
 
   def install
