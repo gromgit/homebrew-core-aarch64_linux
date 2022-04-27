adad0630032a0601ff6a5867c970ac6b710c1cef
Michael Cho <cho-m@tuta.io>
2022-04-27 10:31:15 -0700

    rethinkdb 2.4.2
    Closes #100256.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rethinkdb.rb
+++ Formula/rethinkdb.rb
@@ -1,8 +1,8 @@
 class Rethinkdb < Formula
   desc "Open-source database for the realtime web"
   homepage "https://rethinkdb.com/"
-  url "https://download.rethinkdb.com/repository/raw/dist/rethinkdb-2.4.1.tgz"
-  sha256 "5f1786c94797a0f8973597796e22545849dc214805cf1962ef76969e0b7d495b"
+  url "https://download.rethinkdb.com/repository/raw/dist/rethinkdb-2.4.2.tgz"
+  sha256 "35e6a76a527d473a7d962515a0414dea6e09300fff368ae713964ce2494d9c0d"
   license "Apache-2.0"
   head "https://github.com/rethinkdb/rethinkdb.git", branch: "next"
 
@@ -20,29 +20,29 @@ class Rethinkdb < Formula
   end
 
   depends_on "boost" => :build
-  depends_on :macos # Due to Python 2 (v8 and gyp fail to build)
-  # https://github.com/Homebrew/linuxbrew-core/pull/19614
-  # https://github.com/rethinkdb/rethinkdb/pull/6401
   depends_on "openssl@1.1"
+  depends_on "protobuf"
 
+  uses_from_macos "python" => :build
   uses_from_macos "curl"
-
-  # patch submitted to upstream, https://github.com/rethinkdb/rethinkdb/pull/6934
-  # remove in next release
-  patch do
-    url "https://github.com/chenrui333/rethinkdb/commit/d7d22b4.patch?full_index=1"
-    sha256 "45d387c3360cf6265e27485c273c37224b2ef0000ebd4fdd361dcecc4d2e9791"
-  end
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
 
   def install
-    # rethinkdb requires that protobuf be linked against libc++
-    # but brew's protobuf is sometimes linked against libstdc++
-    args = %W[--prefix=#{prefix} --fetch protobuf]
+    ENV.cxx11
+    # Can use system Python 2 for older macOS. See https://rethinkdb.com/docs/build
+    ENV["PYTHON"] = which("python3") if !OS.mac? || MacOS.version >= :catalina
+
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+    ]
     args << "--allow-fetch" if build.head?
 
     system "./configure", *args
     system "make"
-    system "make", "install-osx"
+    system "make", "install-binaries"
 
     (var/"log/rethinkdb").mkpath
 
