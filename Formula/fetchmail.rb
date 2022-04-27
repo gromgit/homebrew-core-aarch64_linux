697af45576fd91fa415eae75dd636781f8045f25
Caleb Xu <calebcenter@live.com>
2022-04-26 22:47:41 -0400

    fetchmail 6.4.30
    Closes #100199.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fetchmail.rb
+++ Formula/fetchmail.rb
@@ -1,8 +1,8 @@
 class Fetchmail < Formula
   desc "Client for fetching mail from POP, IMAP, ETRN or ODMR-capable servers"
   homepage "https://www.fetchmail.info/"
-  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.29.tar.xz"
-  sha256 "397df984082abae51edec6831700d68636f0e117cfaffcbdd3eed66d04b97321"
+  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.30.tar.xz"
+  sha256 "35824c20aa1d8d97e3f1611fef49b9f7f68396f7cbc4d3a807fed27ae9ea445a"
   license all_of: [
     "LGPL-2.1-or-later",
     "ISC",
