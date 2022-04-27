4a3470098bbc28f177941bdbd268c07ac1741ee7
Rui Chen <rui@chenrui.dev>
2022-04-27 18:38:15 -0400

    flyctl 0.0.324
    Closes #100278.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.323",
-      revision: "c4d632865c927cf823f6b031a1d465b337490065"
+      tag:      "v0.0.324",
+      revision: "dcb5132241b59a22c00ed0851ee683fb15e2ebe5"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
