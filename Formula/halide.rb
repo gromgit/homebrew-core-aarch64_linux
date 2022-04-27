4d986a38d729d4461c65996599f7f754d817ed85
Michael Cho <cho-m@tuta.io>
2022-04-26 21:39:20 -0700

    halide: revision bump (llvm 14.0.5)

--- Formula/halide.rb
+++ Formula/halide.rb
@@ -4,6 +4,7 @@ class Halide < Formula
   url "https://github.com/halide/Halide/archive/v14.0.0.tar.gz"
   sha256 "f9fc9765217cbd10e3a3e3883a60fc8f2dbbeaac634b45c789577a8a87999a01"
   license "MIT"
+  revision 1
 
   livecheck do
     url :stable
