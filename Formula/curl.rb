57d4f82fe7cced88072889b2c4ae1c5ebebccbad
Alexander M. Turek <me@derrabus.de>
2022-04-27 09:45:16 +0200

    curl 7.83.0
    Closes #100229.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,11 +1,11 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
-  url "https://curl.se/download/curl-7.82.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.82.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-7.82.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-7.82.0.tar.bz2"
-  sha256 "46d9a0400a33408fd992770b04a44a7434b3036f2e8089ac28b57573d59d371f"
+  url "https://curl.se/download/curl-7.83.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.83.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-7.83.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-7.83.0.tar.bz2"
+  sha256 "247c7ec7521c4258e65634e529270d214fe32969971cccb72845e7aa46831f96"
   license "curl"
 
   livecheck do
@@ -74,7 +74,7 @@ class Curl < Formula
     system "./configure", *args
     system "make", "install"
     system "make", "install", "-C", "scripts"
-    libexec.install "lib/mk-ca-bundle.pl"
+    libexec.install "scripts/mk-ca-bundle.pl"
   end
 
   test do
