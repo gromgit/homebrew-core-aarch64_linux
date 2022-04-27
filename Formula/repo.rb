4b65bed877874bcd535bbe8534dfd0e1c72271e0
Yurii Kolesnykov <root@yurikoles.com>
2022-04-27 23:42:45 +0300

    repo 2.24.1
    Closes #100263.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -4,8 +4,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.24",
-      revision: "4c11aebeb934817407266787d58247e1ca802057"
+      tag:      "v2.24.1",
+      revision: "0de4fc3001b36db7e107e61586296ca25bbf6d6d"
   license "Apache-2.0"
   version_scheme 1
 
