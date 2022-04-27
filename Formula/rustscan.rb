61f5e4269a51377302e6ca613df0609958e361f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-27 19:09:32 +0000

    rustscan: update 2.1.0 bottle.

--- Formula/rustscan.rb
+++ Formula/rustscan.rb
@@ -6,12 +6,12 @@ class Rustscan < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "3b9fda4219ae6932bc7725b11709405731ccc2ca3c04df7d4abe4ed00219ef8d"
-    sha256 cellar: :any_skip_relocation, big_sur:      "83c8a3e9b4a71e21590bd377b1db9518cb32794d341d161519e974dd3e837f2e"
-    sha256 cellar: :any_skip_relocation, catalina:     "21f4b50689613475ca9194ecae3b062e874d49bb987b1949f453885360715200"
-    sha256 cellar: :any_skip_relocation, mojave:       "135ddda8cfa5a670c0c49e3730148e8f50dfa1b597d9bdf2c35b5fc4d5a8fd9e"
-    sha256 cellar: :any_skip_relocation, high_sierra:  "d830a6c803a7cd609c6287452f9c02bb855e1f193463b2655cc2df19e46e1f6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "cbe4db4bdd1680a3ecb79f4b0a010d342583dc43fc740fee7c80589dd85dae09"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b0c49e379f4a26ce81c5efb99befcf90fef1415b03c85105a9f119aab8b6273"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40dfd5fafb1b91e5b98866ade1562aa44b2d1b9ec10183bdd49d4b771c596d0f"
+    sha256 cellar: :any_skip_relocation, monterey:       "b92354edb90c2ee5414c003d0d430a8800fdf9925592b72578c500ab50587694"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c8f2ecf1e087109d362dc6cf649ebeeacdc4aee3c8f4c32ee0920e825695af68"
+    sha256 cellar: :any_skip_relocation, catalina:       "a1f7ef6167f4f2756ae5418447773ef610fe8961a8e57f8576d308c9589d7bfe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4713f24aa727940fad2bf26127cfef0267130356a99ceac50459b1674a06bd5f"
   end
 
   depends_on "rust" => :build
