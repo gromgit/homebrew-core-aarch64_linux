89cce873e1d2cd45cd660889a6bde2b172cdce88
Curudel <andreas.florin.1988@outlook.com>
2022-04-27 18:58:39 +0200

    tfsec 1.19.1
    Closes #100253.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/aquasecurity/tfsec/archive/v1.19.0.tar.gz"
-  sha256 "587067b92ccaf93aa7e7f231991f4d2f30394c527aa44b87902189cf4470a6c2"
+  url "https://github.com/aquasecurity/tfsec/archive/v1.19.1.tar.gz"
+  sha256 "c73f78558d27c58881063243b14c9176760f8c288d17eae82c341bd0251155fe"
   license "MIT"
   head "https://github.com/aquasecurity/tfsec.git", branch: "master"
 
