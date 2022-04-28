90525396628a557a0b88af4715954045f059ede2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-28 04:32:00 +0000

    observerward: update 2022.4.28 bottle.

--- Formula/observerward.rb
+++ Formula/observerward.rb
@@ -6,12 +6,12 @@ class Observerward < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e31041e605b2f9444fff1df75291ed7ada0de4e20d283781bbab1122ada8d398"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51f00c97897809442b6393e0fa83833b1037689b45297009f76e3f87feef4861"
-    sha256 cellar: :any_skip_relocation, monterey:       "5d7cf661b42c300fb9af7f193de7ae5f943551d6024f266f68075190d425cc63"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3e55fc3f7b7207c38152981880684a694c0a1f839df62d871c04c5ca6a482f20"
-    sha256 cellar: :any_skip_relocation, catalina:       "99fe13d548df4bee200c5cd9d731c34ad542fdc7308e647f078dae823e379438"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ad098c290b8b815e81fc59ca8019c03b79181cdfa0bde1e1f9152c71f69bb77"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2e2dc5cbec5126dfcc49173c2ec5438a28f88a68da50682d69d7f1c4b7f6aeae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eec0743074017d3964915a6d601e31ce96e11f760c77069f3d0d72607a334e11"
+    sha256 cellar: :any_skip_relocation, monterey:       "6efe0a8a383ffe4754f641595abde4b5ad6712528dc877f1608618adff319c47"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d7544ff93728b534940bf0e405f7b32dea786d5749da8b66525c2bb059bbefdc"
+    sha256 cellar: :any_skip_relocation, catalina:       "ff5465e9b20c13fc13f5d4722aec3548948138b3cb5b6eb594c05073d63a3c6f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec2a594d45daee88326ac39e4ae444a38770737b8beea36d82dfc786ed3fb239"
   end
 
   depends_on "rust" => :build
