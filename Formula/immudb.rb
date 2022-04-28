732dc97df788ec1a29cf382dbd2c25bda46992a7
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2022-04-28 08:13:07 -0300

    immudb 1.2.4
    Closes #100321.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/immudb.rb
+++ Formula/immudb.rb
@@ -1,8 +1,8 @@
 class Immudb < Formula
   desc "Lightweight, high-speed immutable database"
   homepage "https://www.codenotary.io"
-  url "https://github.com/codenotary/immudb/archive/v1.2.3.tar.gz"
-  sha256 "9ac11c3a34ff5d438867aed811bc00bec03e27b746908dbed87a44d69b7bc7e4"
+  url "https://github.com/codenotary/immudb/archive/v1.2.4.tar.gz"
+  sha256 "dd7f7ef1bae8d34ebaa1bb8d1099a1b0809dd6bac0d36fdc279139087dcedb79"
   license "Apache-2.0"
 
   livecheck do
