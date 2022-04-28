0094e28a396bec4c2e10755f72362c56293665e3
Bo Anderson <mail@boanderson.me>
2022-04-28 01:42:05 +0100

    zlib: update 1.2.12 bottle.

--- Formula/zlib.rb
+++ Formula/zlib.rb
@@ -13,17 +13,12 @@ class Zlib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "753d7a7fa1fa07a857e6ff7858bcabb470b7b3510ceebc87289eda3c13b676e8"
-    sha256 cellar: :any,                 arm64_big_sur:  "b480ed6baf10880f61b5a3097fb0921d44466857e1dde53a09e2ae4e378b1a8c"
-    sha256 cellar: :any,                 monterey:       "7b3c7d2e0992f824cdc9948bc5da8d9e9f739614c13e0c0f94dfcb90fea3f912"
-    sha256 cellar: :any,                 big_sur:        "b95aa332dfc7c6dfb5e86fd30068f78e2cf87ee0232e5bef0adddae8215f543d"
-    sha256 cellar: :any,                 catalina:       "8ec66cf6faa310712767efc3022fdd16568a79234439f64bf579acb628f893bc"
-    sha256 cellar: :any,                 mojave:         "245a43a59c57f83848e7382974bb80a46eac1d53bcaefb1bdebd1f85107d4169"
-    sha256 cellar: :any,                 high_sierra:    "30548658b43cf66979f2756680fbb32d3c19c967e478ceea22d07f536b22bbce"
-    sha256 cellar: :any,                 sierra:         "f822b4dbab4a15b889316b89248c7b4d15d6af9dc460bf209b9425b0accb7fa3"
-    sha256 cellar: :any,                 el_capitan:     "3f912f6f1ce6c586128ebde29756c883b89409e652ca7aa9a29a773c2d4d0915"
-    sha256 cellar: :any,                 yosemite:       "5b969eb38b90a3e31869586df9d62e59d359212b16c6a270aee690dd67caa491"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db54bf590275839d3f4cdf31d9527aa3a4c19a8984b5605cedc3f7c22a65eea4"
+    sha256 cellar: :any,                 arm64_monterey: "b3c956cfb722564f26a8c50a89293971089a7017f363c823d432e1c90c16b87c"
+    sha256 cellar: :any,                 arm64_big_sur:  "0cb865dc3adb641c0f4f7301d7ad66ab5b23ac76afe61e21d865c12f0ab5d03a"
+    sha256 cellar: :any,                 monterey:       "5072d7b94690a52220f7a9f6cc566f87998a380e3f2fcd8a386caf7dbd5f19c4"
+    sha256 cellar: :any,                 big_sur:        "3db997820d0f7cbd3de13fda10f731949f44cb19f3923be18686ae5d65ec2e7f"
+    sha256 cellar: :any,                 catalina:       "71657247458cf1aa4f1b548aff059e65b182ef5fdf86740071f7f60c5520b370"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23b1d8f0500bbccdf5cc466e7acbd7eddc40cd1465687239af423389abe4f46e"
   end
 
   keg_only :provided_by_macos
