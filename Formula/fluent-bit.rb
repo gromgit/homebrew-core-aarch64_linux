0b55956fb4c61f2cf46febf0e09deaac0ff2b4da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-28 08:10:41 +0000

    fluent-bit: update 1.9.3 bottle.

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -12,12 +12,12 @@ class FluentBit < Formula
   end
 
   bottle do
-    sha256                               arm64_monterey: "fdbf4302afaec2bc8242a7cdcf1109e6deac51901e709d5068c85cd366441f36"
-    sha256                               arm64_big_sur:  "8a6ad8356c263cddb8c4b83611f018b31fee8a56dafc3492d45fe879b3841229"
-    sha256                               monterey:       "80992d850f32f83e88989fa5ea9d9bdb089172b663fabf25a532cfbd93816e8f"
-    sha256                               big_sur:        "40213fd5a81c95c460f6bc52b40f255faa236ef6527006d0a4eae23621b12428"
-    sha256                               catalina:       "15adaaadde23817406c10a1310d22e45e2f0123e9255b780b2958e3fe3a98991"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb7dc43dded380a49bf209389251b61b39f7c450024c59ac5426bc72ec71484a"
+    sha256                               arm64_monterey: "d84e697db42b0c1009d151b654a35d5ac17d5c5639d0385008969a9716a4cb80"
+    sha256                               arm64_big_sur:  "6fb69ae473814383acfdea4e8e211bd71101fbd58e85e3cd1e2edc01fddfb260"
+    sha256                               monterey:       "6d9a92311600e127595859a3d4e631506319ca91f8787f6ec2d1181b3bdb1d73"
+    sha256                               big_sur:        "645d9d1037bd5895a708a3e12bb2a1e7c8b5bce2b45494ecaab0c85d6143e9ca"
+    sha256                               catalina:       "f83df8bfd421a3b28895deb596b4dd1fef3c69be875a585bf06cf24a117cbce0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0dfa4e37609b191ef19a0f3836ed7ec09f889045b7472b6995442e105a652d4"
   end
 
   depends_on "bison" => :build
