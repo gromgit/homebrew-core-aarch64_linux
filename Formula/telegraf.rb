060b761517cde343cd80b74babea7d835381f587
Porkepix <porkepix@gmail.com>
2022-04-28 19:18:51 +0200

    telegraf 1.22.3
    Closes #100350.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -1,8 +1,8 @@
 class Telegraf < Formula
   desc "Server-level metric gathering agent for InfluxDB"
   homepage "https://www.influxdata.com/"
-  url "https://github.com/influxdata/telegraf/archive/v1.22.2.tar.gz"
-  sha256 "c4efc78a28324c742202dce43599fc7063ed681cd95d32c14500edb6078c7855"
+  url "https://github.com/influxdata/telegraf/archive/v1.22.3.tar.gz"
+  sha256 "1c260902d713ede64e6004557bcf1c545c7e0f4a543a1f6f84b60b5254f6c266"
   license "MIT"
   head "https://github.com/influxdata/telegraf.git", branch: "master"
 
