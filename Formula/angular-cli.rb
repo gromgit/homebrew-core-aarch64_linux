08f44d07d6cb8d1e1a80d66267f81bd193837ad4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-28 05:21:54 +0000

    angular-cli 13.3.4
    Closes #100299.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-13.3.3.tgz"
-  sha256 "085deff248b578cb620a8c95ec76c3a31c56e41aaae653e1869f5f86fe413b6d"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-13.3.4.tgz"
+  sha256 "f8d472d52e0d11b269c97a29880e87c9bdbc7a2b183b25e97fe7341bf30891a7"
   license "MIT"
 
   bottle do
