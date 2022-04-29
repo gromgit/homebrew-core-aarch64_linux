3feee5fec58541f2279cd3ef511b15c9b876fb3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-29 05:36:52 +0000

    oha 0.5.2
    Closes #100378.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -1,8 +1,8 @@
 class Oha < Formula
   desc "HTTP load generator, inspired by rakyll/hey with tui animation"
   homepage "https://github.com/hatoo/oha/"
-  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "1879b2e155679241560ce805965bbd7ac5fb1b602115d97b2b31afe8a0964051"
+  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "07df9d54b6103b4fad2e4ff0cff7f70972a5494adc986c5a864ff43cae610b5a"
   license "MIT"
   head "https://github.com/hatoo/oha.git", branch: "master"
 
