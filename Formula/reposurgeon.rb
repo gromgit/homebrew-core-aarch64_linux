94272c8239cdc47087675837f331c1acb692d9cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-29 05:41:25 +0000

    reposurgeon 4.32
    Closes #100380.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/reposurgeon.rb
+++ Formula/reposurgeon.rb
@@ -2,8 +2,8 @@ class Reposurgeon < Formula
   desc "Edit version-control repository history"
   homepage "http://www.catb.org/esr/reposurgeon/"
   url "https://gitlab.com/esr/reposurgeon.git",
-      tag:      "4.29",
-      revision: "1b708dceed752d16dbe6ea095a4928a7231e5bbc"
+      tag:      "4.32",
+      revision: "2e221c19802176479d98d080409273896dd43997"
   license "BSD-2-Clause"
   head "https://gitlab.com/esr/reposurgeon.git", branch: "master"
 
