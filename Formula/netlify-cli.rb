60e45db1057f53266590660e23d4629b8e9eadf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-29 05:35:45 +0000

    netlify-cli 10.1.0
    Closes #100377.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-10.0.0.tgz"
-  sha256 "6b4e4ad3eedcadda9ea0328f76055e18e5a151dedba0cb566b335ecb3c2d4556"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-10.1.0.tgz"
+  sha256 "11295ee815d6318e5cfb47e885e37733e1529346ecd272d03fd05a51cc5e5086"
   license "MIT"
   head "https://github.com/netlify/cli.git", branch: "main"
 
