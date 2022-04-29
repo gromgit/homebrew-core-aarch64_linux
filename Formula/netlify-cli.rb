2f60729554e2fe13448ba15697b7654e94f3d763
Alexander Bayandin <a.bayandin@gmail.com>
2022-04-29 11:47:27 +0000

    netlify-cli: update 10.1.0 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -9,12 +9,12 @@ class NetlifyCli < Formula
   head "https://github.com/netlify/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6df8d664bf527da02698e2b6680f6444f8b6c330cff26021f9821fb89e3d01b1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6df8d664bf527da02698e2b6680f6444f8b6c330cff26021f9821fb89e3d01b1"
-    sha256 cellar: :any_skip_relocation, monterey:       "88ee7b67925ade08231cee209384e4fa581ed926d24da8296757dca0f2294459"
-    sha256 cellar: :any_skip_relocation, big_sur:        "88ee7b67925ade08231cee209384e4fa581ed926d24da8296757dca0f2294459"
-    sha256 cellar: :any_skip_relocation, catalina:       "88ee7b67925ade08231cee209384e4fa581ed926d24da8296757dca0f2294459"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "169779e007cc1a12f4471de4891cafe79ea082e06f05453acaf62324efc1ac5b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3fcf62795dafb916bac51286e8b733d9e7fc83b9a5bcf15c7493b74e01a795c1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3fcf62795dafb916bac51286e8b733d9e7fc83b9a5bcf15c7493b74e01a795c1"
+    sha256 cellar: :any_skip_relocation, monterey:       "937fc10f943b7bd99ea5215c454e30dc9c7ef6677f543abe235e42923912eaff"
+    sha256 cellar: :any_skip_relocation, big_sur:        "937fc10f943b7bd99ea5215c454e30dc9c7ef6677f543abe235e42923912eaff"
+    sha256 cellar: :any_skip_relocation, catalina:       "937fc10f943b7bd99ea5215c454e30dc9c7ef6677f543abe235e42923912eaff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8361dd797bcd359704de162f9f9c6d6f3a5b041ee208ea11e781add81301f870"
   end
 
   depends_on "node"
