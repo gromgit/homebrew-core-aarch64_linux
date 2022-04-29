d679b7262d1f8f3240b0fc5a988f7cad243df0fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-29 05:30:02 +0000

    knot 3.1.8
    Closes #100373.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.1.7.tar.xz"
-  sha256 "ffb6887e238ce4c7df0cc76bb55a5093465275201ac12156a3390782dc49857b"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.1.8.tar.xz"
+  sha256 "767e458a56277a1270b359294c3be6c63fd734884d62a045e01756a46507aa94"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
