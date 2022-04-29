e937ec006ef687ad69943c7ee46c152381fbdbb5
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-04-29 03:50:09 +0000

    restund: update 0.4.12 bottle.

--- Formula/restund.rb
+++ Formula/restund.rb
@@ -11,11 +11,12 @@ class Restund < Formula
 
   bottle do
     rebuild 1
-    sha256 big_sur:     "389b79ebc0bd2352f739496d27e8d684db16a4a371ff24c511dc0b2a6059e5ec"
-    sha256 catalina:    "904de3a9379dac2c1215b992e1aff7cfd42e09f288e5b88df1969c9ba1675050"
-    sha256 mojave:      "7ec584f71cc4b6f54f30c1dfcae29e11f110b8f26506e1154e5646ce326923b1"
-    sha256 high_sierra: "2d5b243b9971a38fdc00c1d2d332e7875aa17f74ea4d1f083eeacbfaa38d004f"
-    sha256 sierra:      "ea2c7e202307b9a48ed65020570d5ce3236b556757263cb16c35143baa92ca79"
+    sha256 big_sur:      "389b79ebc0bd2352f739496d27e8d684db16a4a371ff24c511dc0b2a6059e5ec"
+    sha256 catalina:     "904de3a9379dac2c1215b992e1aff7cfd42e09f288e5b88df1969c9ba1675050"
+    sha256 mojave:       "7ec584f71cc4b6f54f30c1dfcae29e11f110b8f26506e1154e5646ce326923b1"
+    sha256 high_sierra:  "2d5b243b9971a38fdc00c1d2d332e7875aa17f74ea4d1f083eeacbfaa38d004f"
+    sha256 sierra:       "ea2c7e202307b9a48ed65020570d5ce3236b556757263cb16c35143baa92ca79"
+    sha256 x86_64_linux: "aff35fd4d51faf83a326f656c14c96d0ed32e3c7a45eee6f160bb159c7bb629d"
   end
 
   depends_on "libre"
