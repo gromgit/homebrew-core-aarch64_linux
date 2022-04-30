92f919054c7751b7d9c88fc8945c125533a94f02
Porkepix <porkepix@gmail.com>
2022-04-30 23:47:17 +0200

    pmd 6.45.0
    Closes #100485.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.44.0/pmd-bin-6.44.0.zip"
-  sha256 "7e6dceba88529a90b2b33c8f05b53bc409fa9eab79be592c875f6bd996aaade7"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.45.0/pmd-bin-6.45.0.zip"
+  sha256 "ee23c659011f1458714d43b92e4a251683910adfc6ebbf55e008402c7b4531b8"
   license "BSD-4-Clause"
 
   bottle do
