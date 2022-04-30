d7d416ccd677031845269a3032f998dd4c5c109e
Porkepix <porkepix@gmail.com>
2022-05-01 00:07:14 +0200

    xray 1.5.5
    * xray 1.5.5
    * xray-1.5.5: update geoip and geosite resources
    
    Closes #100491.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -1,8 +1,8 @@
 class Xray < Formula
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://xtls.github.io/"
-  url "https://github.com/XTLS/Xray-core/archive/refs/tags/v1.5.4.tar.gz"
-  sha256 "af9b9b5b0a2d4f055d19f3580d1c2d3141bbaab9dd7824428c12ae0ced5f511e"
+  url "https://github.com/XTLS/Xray-core/archive/refs/tags/v1.5.5.tar.gz"
+  sha256 "3f8d04fef82a922c83bab43cac6c86a76386cf195eb510ccf1cc175982693893"
   license all_of: ["MPL-2.0", "CC-BY-SA-4.0"]
   head "https://github.com/XTLS/Xray-core.git", branch: "main"
 
@@ -23,13 +23,13 @@ class Xray < Formula
   depends_on "go" => :build
 
   resource "geoip" do
-    url "https://github.com/v2fly/geoip/releases/download/202203170039/geoip.dat"
-    sha256 "12c183defbc052e6bd96eb088a7f955f9d3a62d662009d5fb63da040e954e6c8"
+    url "https://github.com/v2fly/geoip/releases/download/202204280105/geoip.dat"
+    sha256 "38fe72a33f23920cf14e804bf14c26ea0210db3ea2108a2d51fa32c48ac53170"
   end
 
   resource "geosite" do
-    url "https://github.com/v2fly/domain-list-community/releases/download/20220320055819/dlc.dat"
-    sha256 "644c20f0df438b618c4200370b21c27885699d73af9cd781dfb21a922317cef8"
+    url "https://github.com/v2fly/domain-list-community/releases/download/20220429034939/dlc.dat"
+    sha256 "32b0ebeb8d4c6b06584a7e14d1488c3628dce4ad7c7bad65a5af180513c311aa"
   end
 
   resource "example_config" do
