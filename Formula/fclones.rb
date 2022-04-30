e1ecd594b7b1b1489177d58188a8d132d66ef066
Piotr Kołaczkowski <pkolaczk@datastax.com>
2022-04-30 18:35:54 +0200

    fclones 0.22.0 (new formula)
    Closes #100472.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/fclones.rb
@@ -0,0 +1,25 @@
+class Fclones < Formula
+  desc "Efficient Duplicate File Finder"
+  homepage "https://github.com/pkolaczk/fclones"
+  url "https://github.com/pkolaczk/fclones/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "502fb889bc3caab4240d21103086f105072160b53b37293ff3bff29628687bbc"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"foo1.txt").write "foo"
+    (testpath/"foo2.txt").write "foo"
+    (testpath/"foo3.txt").write "foo"
+    (testpath/"bar1.txt").write "bar"
+    (testpath/"bar2.txt").write "bar"
+    output = shell_output("fclones group #{testpath}")
+    assert_match "Redundant: 9 B (9 B) in 3 files", output
+    assert_match "a9707ebb28a5cf556818ea23a0c7282c", output
+    assert_match "16aa71f09f39417ecbc83ea81c90c4e7", output
+  end
+end
