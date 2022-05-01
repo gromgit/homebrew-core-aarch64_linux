091f6fa94c05e8bc86584d656172e3bf6ec704a9
Rui Chen <rui@chenrui.dev>
2022-05-01 11:31:56 -0400

    efl: revision bump (bullet 3.24)
    Closes #100511.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/efl.rb
+++ Formula/efl.rb
@@ -4,7 +4,7 @@ class Efl < Formula
   url "https://download.enlightenment.org/rel/libs/efl/efl-1.26.2.tar.xz"
   sha256 "2979cfbc728a1a1f72ad86c2467d861ed91e664d3f17ef03190fb5c5f405301c"
   license all_of: ["GPL-2.0-only", "LGPL-2.1-only", "BSD-2-Clause", "FTL", "zlib-acknowledgement"]
-  revision 1
+  revision 2
 
   livecheck do
     url "https://download.enlightenment.org/rel/libs/efl/"
