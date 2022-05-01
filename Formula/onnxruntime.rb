0ef36a97c91a49367b86dd7b23cac07c07bcaee5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-01 00:18:11 +0000

    onnxruntime: update 1.11.1 bottle.

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -12,12 +12,12 @@ class Onnxruntime < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "1cdcb9598a23ad0be8a1776199d73e733365ca978164176d0fa90bb5b063edfb"
-    sha256 cellar: :any,                 arm64_big_sur:  "f601a703b06780d06913558687e96da6d78e21223511dd8be136e8b18905c552"
-    sha256 cellar: :any,                 monterey:       "cb84a2af1cb8ec21bdfae68b9092ae7143289c92f4b3f44141715ca3401902e7"
-    sha256 cellar: :any,                 big_sur:        "17c628ab8e1dd66589f77ff8636b029d898a436cfcfe0adb9a97341468227652"
-    sha256 cellar: :any,                 catalina:       "4f1a2221ae2b1a476f227c4a606e54224dfa6c7c88f5efa7de7594ab43901d58"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "534b624d5f270e3884828d45359155587dc0bd2154ec0ed9c767b23f9031b292"
+    sha256 cellar: :any,                 arm64_monterey: "67395f4da47c0fe369eca4f1ddefcd3bc1ac80602e5028ae831d3ea45a501cb1"
+    sha256 cellar: :any,                 arm64_big_sur:  "0aefe3bc18e877529c4558690872dc6427668830b49b234d3dfdc0fffc5994a4"
+    sha256 cellar: :any,                 monterey:       "d605eab89cf11209f25aab6eb4f371c0b345f5927132e5ff1a3e71d40b369a13"
+    sha256 cellar: :any,                 big_sur:        "0d781494db579b7a7297840ab2a899dde515a12f2c42e2e47d6c941830692815"
+    sha256 cellar: :any,                 catalina:       "c27046441db151b7f36a23b4685b4e30de6e7b8223306e5a7837353b3f7cc077"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38ae3063265cd6fd3d177a482053e27d90891797cbc78513bcd8ed3651204e98"
   end
 
   depends_on "cmake" => :build
