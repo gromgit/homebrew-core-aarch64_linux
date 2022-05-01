28a2540628bef94313a0e62c94183ee72f98494c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-01 00:18:27 +0000

    easeprobe: update 1.4.0 bottle.

--- Formula/easeprobe.rb
+++ Formula/easeprobe.rb
@@ -8,12 +8,12 @@ class Easeprobe < Formula
   head "https://github.com/megaease/easeprobe.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "01ab7e9004a5a2f5cf4637f27c9ff11c4c94cd736c685a9eb01fb81693ab3c99"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc78d1f43d63ce42ed1bb00a23ad974059fcc9ccda3ce6c3874db789d017df0a"
-    sha256 cellar: :any_skip_relocation, monterey:       "b611fabb9873047cd2ecf996bdf3c94fd37db6f2fcb7b3cc476248631123e518"
-    sha256 cellar: :any_skip_relocation, big_sur:        "78eb4d3d9967955f8584235588b675a4b90a449bf377294298d775ef0772e962"
-    sha256 cellar: :any_skip_relocation, catalina:       "170c5b1eb202eaf01457fdd06f38c8f30f60f33108fcdf44c2feb15845454d8f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "998ad8d82769297a78667c1ca90e5a130ac410143f063568bad5b1fa81909c09"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "65614a9f926fb9eeb6cdc07bcd5c152cd622c53c93b78904fc486bb88d8127c6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dcb076860023b6c2088c922f42029224a08520a707174179bf56a13df0c326b8"
+    sha256 cellar: :any_skip_relocation, monterey:       "9f166bb0967b571b011c38ee69868a0db507452bf8cda1910e175a816d164fad"
+    sha256 cellar: :any_skip_relocation, big_sur:        "286cb976a3e53b39e8a80118bd14938178162d53f2e31922444b3252933fad64"
+    sha256 cellar: :any_skip_relocation, catalina:       "c0a635acd17029c4fec06987a2dbd7a197527fd6eafe5803fdd95c09c7a3242f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ae99766e9af864ac1a2886917a00c9343502afc1fa3cf87a57a8e7f87a20400"
   end
 
   depends_on "go" => :build
