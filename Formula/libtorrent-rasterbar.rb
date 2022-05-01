cf77c8f857c3d5c9d23ca0164ca0cef533903c4d
Rui Chen <rui@chenrui.dev>
2022-04-30 20:31:26 -0400

    libtorrent-rasterbar 2.0.6
    * libtorrent-rasterbar 2.0.6
    * libtorrent-rasterbar: remove patch
    * libtorrent-rasterbar: fix rpath
    
    Closes #100501.
    
    Co-authored-by: Alexander Bayandin <a.bayandin@gmail.com>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -1,21 +1,10 @@
 class LibtorrentRasterbar < Formula
   desc "C++ bittorrent library with Python bindings"
   homepage "https://www.libtorrent.org/"
+  url "https://github.com/arvidn/libtorrent/releases/download/v2.0.6/libtorrent-rasterbar-2.0.6.tar.gz"
+  sha256 "438e29272ff41ccc68ec7530f1b98d639f6d01ec8bf680766336ae202a065722"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/arvidn/libtorrent.git", branch: "RC_2_0"
-
-  # Remove `stable do` block when patch is no longer needed.
-  stable do
-    url "https://github.com/arvidn/libtorrent/releases/download/v2.0.5/libtorrent-rasterbar-2.0.5.tar.gz"
-    sha256 "e965c2e53170c61c0db3a2d898a61769cb7acd541bbf157cbbef97a185930ea5"
-
-    # Fix build with Boost 1.78. Remove in next release.
-    patch do
-      url "https://github.com/arvidn/libtorrent/commit/71d608fceca7e61c9d124f9ea83f71b06eda3b17.patch?full_index=1"
-      sha256 "20b8e93b67f81af22e50bd668fbeee30147dd85d3ffdff9d624531c32f54e567"
-    end
-  end
 
   livecheck do
     url :stable
@@ -45,6 +34,7 @@ class LibtorrentRasterbar < Formula
       -Dencryption=ON
       -Dpython-bindings=ON
       -Dpython-egg-info=ON
+      -DCMAKE_INSTALL_RPATH=#{lib}
     ]
     args += std_cmake_args
 
