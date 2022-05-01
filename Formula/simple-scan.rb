e72259106f8218e7895c1138c23caafd2c56f710
Porkepix <porkepix@gmail.com>
2022-05-01 15:16:27 +0200

    simple-scan 42.1
    Closes #100538.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/simple-scan.rb
+++ Formula/simple-scan.rb
@@ -1,8 +1,8 @@
 class SimpleScan < Formula
   desc "GNOME document scanning application"
   homepage "https://gitlab.gnome.org/GNOME/simple-scan"
-  url "https://download.gnome.org/sources/simple-scan/42/simple-scan-42.0.tar.xz"
-  sha256 "ac1f857afd0bc8897dd2045023ad7c5713e5ceefca56b0b3cc5e9a4795329586"
+  url "https://download.gnome.org/sources/simple-scan/42/simple-scan-42.1.tar.xz"
+  sha256 "859bc0611c1769b5bdaba9639deed359f50474c2eecf58bbbfd7ce21911b2226"
   license "GPL-3.0-or-later"
 
   bottle do
