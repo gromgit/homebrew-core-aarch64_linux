580e425f8443c052388ee6a7c6e64e8c9dc99f48
Rui Chen <rui@chenrui.dev>
2022-05-01 15:15:08 -0400

    spirv-headers: add livecheck (#100546)
    * spirv-headers: add livecheck
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * spirv-headers: update livecheck
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Formula/spirv-headers.rb
+++ Formula/spirv-headers.rb
@@ -5,6 +5,11 @@ class SpirvHeaders < Formula
   sha256 "262864053968c217d45b24b89044a7736a32361894743dd6cfe788df258c746c"
   license "MIT"
 
+  livecheck do
+    url :stable
+    regex(/^sdk[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "766e327a320f213123f6f2b5e394cae54e6d80da6f6caac0f4e4c7799b561811"
   end
