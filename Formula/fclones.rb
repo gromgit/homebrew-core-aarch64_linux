8ac3ebcb5f43ff0123520ca9ca689c3b10ec5ec7
Nanda H Krishna <me@nandahkrishna.com>
2022-05-01 19:36:24 +0000

    fclones: add 0.22.0 bottle.

--- Formula/fclones.rb
+++ Formula/fclones.rb
@@ -5,6 +5,15 @@ class Fclones < Formula
   sha256 "502fb889bc3caab4240d21103086f105072160b53b37293ff3bff29628687bbc"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ad4bafb7c2df590dcb840f25ceb9b2af4007d14b23102bb6b8b97acd7ca06041"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f03493f4f4b69ee2a417a3971bd804d607cb21a738002e9249e304a555624d53"
+    sha256 cellar: :any_skip_relocation, monterey:       "5237ca1a8cf7725691a05001f82317a5b5ee9df9f1b0291e69552e21ec365be7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "30555e6e952ead488a4f6e8274481299e4e10fd407abed510df727fd5f356881"
+    sha256 cellar: :any_skip_relocation, catalina:       "4eb97490c09c32843867ad6f4283240cab6442e925e7ca37b419e233fce5ef16"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d0e40ffbdc396481a86549da3875f0cdd646f09c340fba09a1dae29a733e429"
+  end
+
   depends_on "rust" => :build
 
   def install
