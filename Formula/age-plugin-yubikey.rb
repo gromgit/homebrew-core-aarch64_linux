6b49f14dde00cca67ba847fdaa0cc954d293a23a
Filippo Valsorda <hi@filippo.io>
2022-05-02 04:20:23 +0200

    age-plugin-yubikey 0.3.0 (new formula)
    Closes #100591.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/age-plugin-yubikey.rb
@@ -0,0 +1,26 @@
+class AgePluginYubikey < Formula
+  desc "Plugin for encrypting files with age and PIV tokens such as YubiKeys"
+  homepage "https://github.com/str4d/age-plugin-yubikey"
+  url "https://github.com/str4d/age-plugin-yubikey/archive/v0.3.0.tar.gz"
+  sha256 "3dfd7923dcbd7b02d0bce1135ff4ba55a7860d8986d1b3b2d113d9553f439ba9"
+  license "MIT"
+  head "https://github.com/str4d/age-plugin-yubikey.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  uses_from_macos "pcsc-lite"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    ENV["LANG"] = "en_US.UTF-8"
+    assert_match "Let's get your YubiKey set up for age!",
+      shell_output("#{bin}/age-plugin-yubikey 2>&1", 1)
+  end
+end
