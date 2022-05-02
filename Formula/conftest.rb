0830840886489f9f103b69f03b32636147d30618
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-02 05:17:27 +0000

    conftest 0.32.0
    Closes #100599.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/conftest.rb
+++ Formula/conftest.rb
@@ -1,8 +1,8 @@
 class Conftest < Formula
   desc "Test your configuration files using Open Policy Agent"
   homepage "https://www.conftest.dev/"
-  url "https://github.com/open-policy-agent/conftest/archive/v0.31.0.tar.gz"
-  sha256 "67ac9863e5e4ccae45fc039d51d20dbe7470f05ae4bd59de292f03cfe038d38c"
+  url "https://github.com/open-policy-agent/conftest/archive/v0.32.0.tar.gz"
+  sha256 "7e2bd2ae486d72bbc55e3ba376336fb55df62f3453ad45fcbcff3dbe766f9925"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/conftest.git", branch: "master"
 
