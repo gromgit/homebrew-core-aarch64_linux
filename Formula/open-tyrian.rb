2749a7c8a20e7c91455488fd8968153757159698
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-02 19:10:33 +0000

    open-tyrian: update 2.1.20220318 bottle.

--- Formula/open-tyrian.rb
+++ Formula/open-tyrian.rb
@@ -7,15 +7,12 @@ class OpenTyrian < Formula
   head "https://github.com/opentyrian/opentyrian.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 arm64_monterey: "9ed480acd179bddda65dd78860b77cb7f4d2fecac5821ba7b6c37b2943059bab"
-    sha256 arm64_big_sur:  "ea6b692f0a033ee9312793a19563517010626a790f3111b7a32295b500a90c2f"
-    sha256 monterey:       "6808546de187ebd39a492ab42ab87a99a5ed708f45bc9820b07c76e10d4ff390"
-    sha256 big_sur:        "8c265532f9d757adbdad46208a926d6c5216f5f59c0ad2998a756ab6af8d5692"
-    sha256 catalina:       "5e66ea919c426dcce63be043164439b258a22a906ece3b3b1ec1e41147ac0b76"
-    sha256 mojave:         "f49d2282ee86031f1c1442b6d1eb2fb2753286cb89207b63f03ee98e51e9221f"
-    sha256 high_sierra:    "541d2f9a00f4a56b9464c44994c94da0afeab340c1b3809b61a0c437fda69b7f"
-    sha256 x86_64_linux:   "723f3b0f366b3515276c2e35c6776447b48285cafe565dab1d988abe60f909d4"
+    sha256 arm64_monterey: "39a8c8ce3eb29356265616c12f10d23fd1fab1384e2c14bb664594111d2daaa0"
+    sha256 arm64_big_sur:  "b4d5f6274b1ea73030ec283ceae442a11b82b461514ceead9d1cfb6d835d0976"
+    sha256 monterey:       "4b9fdf300031dd097e1e204a65db08ead73e5139d6e43571eebdd60e07298a5a"
+    sha256 big_sur:        "b815a24ff59c0748826dc34b72d691dd4d3a48fdf5ee5650650a7f89c7087d63"
+    sha256 catalina:       "e537f93b6c659004897ae34ee7a21741a2e66621cb09ae725042e6032435edef"
+    sha256 x86_64_linux:   "d83c9e026139d4bd492da588dfa1f00976bee1ef11f4f76c2ea44bbc8d581e6f"
   end
 
   depends_on "pkg-config" => :build
