fea6ad5870ecfa72c5f98248a1cd129ebaee9e3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-02 08:10:33 +0000

    libscrypt: update 1.22 bottle.

--- Formula/libscrypt.rb
+++ Formula/libscrypt.rb
@@ -6,17 +6,12 @@ class Libscrypt < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e9a021a77ed0361c955d6c87520e37cb5fcab46668e14ccf0b576b32a45dc553"
-    sha256 cellar: :any,                 arm64_big_sur:  "1073aa38a72ed089bf6e6a6a4fbddb6e6123b394e7562d1e1ad5b26cc67906dd"
-    sha256 cellar: :any,                 monterey:       "7ead68bfb6c9f5e0d1a6cd6b76e6cfd79d2729031cd5d7b8493cb46a998b481b"
-    sha256 cellar: :any,                 big_sur:        "c2c67b09b54467e47709dbe7340c1916e0802a5423b4f2224156ce7bb977e389"
-    sha256 cellar: :any,                 catalina:       "66ea017c5361346903add978ce85b09a2a6f2e8eabdf9fb2cfb58809da1d29cd"
-    sha256 cellar: :any,                 mojave:         "81c603f27fbda0bde330506d2745f62d3ba16d3290addc5f1eeecbcd110aa801"
-    sha256 cellar: :any,                 high_sierra:    "46cf17f2a05e5e418822a306899de14be3fbdfe71fc017f6eb1169fc3ad1de3a"
-    sha256 cellar: :any,                 sierra:         "3adc43863f9b966dcecd89f507a4706891f94129dd88ba810ed0269278e931cf"
-    sha256 cellar: :any,                 el_capitan:     "bc2c8318384a72f82802937f7e6dd8017ec44fb6fc94583e5f0c38056e1a660c"
-    sha256 cellar: :any,                 yosemite:       "0e870b01dbbfc49432cc8ea81c90ee6d8732b6d8adc4665368844536d5c6e092"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "017ce8d5d59d3912f2f73cd35d33b6bffae8d8dd10a91bc7b53644a5d959645e"
+    sha256 cellar: :any,                 arm64_monterey: "df9e62c90fb8530ad765f2128a892ba91904901167bb5dcb7f0e1a199b43f59f"
+    sha256 cellar: :any,                 arm64_big_sur:  "6b39d428937056b1a25080c87d9af446ae1397f824d362ca6a791e683a997ed2"
+    sha256 cellar: :any,                 monterey:       "d8e0b6fe9b5e2f14fc281fa859fb3339eb98610863cb0b39652f5cb6522205ad"
+    sha256 cellar: :any,                 big_sur:        "836c0ae075b9e3b580eea4d3c1b554f861166f74657303103bb0415c34650fb8"
+    sha256 cellar: :any,                 catalina:       "d53d94bee86fdb65f96abdb62f07f5f2867773fd0719562a21ad320465ebd686"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62ae9fdeea1cbe282839585250e2adacea715d313975bf6eb863a579aa669a21"
   end
 
   def install
