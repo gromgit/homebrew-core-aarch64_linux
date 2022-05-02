9586aaa88c25f5fd90ef77365c920b43099e9b85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-02 08:10:49 +0000

    minimal-racket: update 8.5 bottle.

--- Formula/minimal-racket.rb
+++ Formula/minimal-racket.rb
@@ -15,11 +15,11 @@ class MinimalRacket < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "5741499d6f681a61cd90a15ab03f051d63838d714fb17b1038c07c9065159f8e"
-    sha256 arm64_big_sur:  "22293e93e5cce501d4e7355b2b07eb268b8e9cf7b3c4fc3ce8fc97af27dd655c"
-    sha256 big_sur:        "6a34a94e8a2b10d3a054788329206494d71830ef5e5db05721c4da73bd49a5c1"
-    sha256 catalina:       "7954ec82ba334e28f2733a6b847bb70227efc7542911d66a1ab0d9d40fed913d"
-    sha256 x86_64_linux:   "c307b4d3b0e5cf5e9d330f65041f9084474861f19b132873453e96b0db3f8ba9"
+    sha256 arm64_monterey: "acf973e4a02fb8ccf6a9f882d67597c2106a580143cf2ab362fb1771d7df2921"
+    sha256 arm64_big_sur:  "8a8cb26ce71f6ca21a8896439cfd95dcc775167c3600ecbcb7e1b710dc175f0b"
+    sha256 big_sur:        "b107fab5bc5575c25db9e9e0c8193eed7aa111bf46d9ac7ffd8c178a6fbaf806"
+    sha256 catalina:       "7e42dd037afd1e8c8f44dea0c771c6483bf1794ee64470118fdf9193ff566073"
+    sha256 x86_64_linux:   "c39f5edd41c16a55b9df0e9b12b43de2407aa1bd18c40556143e6011a6652d4d"
   end
 
   depends_on "openssl@1.1"
