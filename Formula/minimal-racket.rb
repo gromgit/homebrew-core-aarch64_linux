1582ef0cedacbfeee6bf2726ba64cc46763059f9
Bevan Kay <email@bevankay.me>
2022-05-02 14:18:34 +1000

    minimal-racket 8.5
    Closes #100595.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minimal-racket.rb
+++ Formula/minimal-racket.rb
@@ -1,8 +1,8 @@
 class MinimalRacket < Formula
   desc "Modern programming language in the Lisp/Scheme family"
   homepage "https://racket-lang.org/"
-  url "https://mirror.racket-lang.org/installers/8.4/racket-minimal-8.4-src.tgz"
-  sha256 "1599545af8ed8a87b84bc80f7ad8fbbdd9de557ea310582e268e23db026c280c"
+  url "https://mirror.racket-lang.org/installers/8.5/racket-minimal-8.5-src.tgz"
+  sha256 "55d585e3ac9fbaacfbe840a6ec74ce3e7bee9fe85e32213f1b3e4f6f593cae39"
   license any_of: ["MIT", "Apache-2.0"]
 
   # File links on the download page are created using JavaScript, so we parse
