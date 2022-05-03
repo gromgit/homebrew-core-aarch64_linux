d7d4b8015687e2a4a0f7ee81d9ebc4057c5414cb
kimwalisch <kim.walisch@gmail.com>
2022-05-03 19:35:15 +0200

    primecount 7.3 (new formula)
    * primecount 7.3 (new formula)
    * primecount: use default CMake settings
    
    Closes #100700.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/primecount.rb
@@ -0,0 +1,24 @@
+class Primecount < Formula
+  desc "Fast prime counting function program and C/C++ library"
+  homepage "https://github.com/kimwalisch/primecount"
+  url "https://github.com/kimwalisch/primecount/archive/v7.3.tar.gz"
+  sha256 "471fe21461e42e5f28404e17ff840fb527b3ec4064853253ee22cf4a81656332"
+  license "BSD-2-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "libomp"
+  depends_on "primesieve"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON",
+                                              "-DBUILD_LIBPRIMESIEVE=OFF",
+                                              "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                                              *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_equal "37607912018\n", shell_output("#{bin}/primecount 1e12")
+  end
+end
