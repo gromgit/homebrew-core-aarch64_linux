aaf6020167324d7689fe148fd23506c01ed4c6fa
Alexander Bayandin <a.bayandin@gmail.com>
2022-05-03 13:36:15 +0000

    arxiv_latex_cleaner: add 0.1.27 bottle.

--- Formula/arxiv_latex_cleaner.rb
+++ Formula/arxiv_latex_cleaner.rb
@@ -8,6 +8,15 @@ class ArxivLatexCleaner < Formula
   license "Apache-2.0"
   head "https://github.com/google-research/arxiv-latex-cleaner.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9c603251ca83de97eb58fd76af0f2ee2cdcf0409d9d30474e37e4511e0daaa4b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9cd5f50cb2db8b9c881e5671d5883e0e9ef361c71945907a3b0337cc47e6c8af"
+    sha256 cellar: :any_skip_relocation, monterey:       "8747083f2512160f739f4b95ac2d3fdee31fc8817dfd9927a54318efb70af8e2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7eb0a1b1dd83e0a6b76f93fe676571493ae3ccb2eb68bd697379db1d56a9281a"
+    sha256 cellar: :any_skip_relocation, catalina:       "81305bb01f4d3bf1d3e34af381cb29bdddc8e34066e3741374c1e73eeb1d8c8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd9ecb403ac536e919cb8d0eae0f23fbd4355ff7456701697cff5f69d253ecf4"
+  end
+
   depends_on "pillow"
   depends_on "python@3.10"
   depends_on "six"
