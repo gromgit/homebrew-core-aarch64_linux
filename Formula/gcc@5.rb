0ae6e219a8e6463753277c17de6c7578346ad23f
Michka Popoff <michkapopoff@gmail.com>
2022-05-04 21:38:59 +0200

    gcc@5: Fix glibc installation logic
    See https://github.com/Homebrew/brew/pull/13231#issuecomment-1115417732
    
    This code was present at one point in linuxbrew-core but we made
    an error while migrating it.

--- Formula/gcc@5.rb
+++ Formula/gcc@5.rb
@@ -30,7 +30,7 @@ class GccAT5 < Formula
 
   on_linux do
     depends_on "binutils"
-    depends_on "glibc" if Formula["glibc"].any_version_installed?
+    depends_on "glibc" if OS::Linux::Glibc.system_version < Formula["glibc"].version
   end
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
