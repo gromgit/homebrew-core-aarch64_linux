74190ee416695d4eeb4a842d63d89f823e0a9058
Gustavo Bazan <gus.bazan@hey.com>
2022-05-05 00:18:53 +0100

    mongocli: add head, livecheck (#100779)

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -4,6 +4,12 @@ class Mongocli < Formula
   url "https://github.com/mongodb/mongodb-atlas-cli/archive/refs/tags/mongocli/v1.25.0.tar.gz"
   sha256 "aab93f9702df598461f0de5a7d8949e4c1d45c73c1f01198cff0d60b12ac9fd9"
   license "Apache-2.0"
+  head "https://github.com/mongodb/mongodb-atlas-cli.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(%r{^mongocli/v?(\d+(?:\.\d+)+)$}i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a2c4bcd9669f917539527d17707777d70f543149cef622892cc535973cbab543"
