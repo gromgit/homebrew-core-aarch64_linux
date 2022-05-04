377f606a2bd00758800f366e26fbce9f28819977
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-04 00:13:57 +0000

    syncthing: update 1.20.0 bottle.

--- Formula/syncthing.rb
+++ Formula/syncthing.rb
@@ -12,12 +12,12 @@ class Syncthing < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3e9a8b0d0a953067fd419e85da3d3a50ca413844b96317b73dbc53e3967d389f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56ddac3b0b14ad4bbea6ccc75180c06a24e07420629a37cdb52d7d9ace1fd1f4"
-    sha256 cellar: :any_skip_relocation, monterey:       "b3adf0004ba7b12b600435b9c9e8e86f18f9834fab13f7c99e618bcad49dd9d6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7414e39803b47bdae5dd717e341865124bb521e25fb4d22959310adc1bd8e93b"
-    sha256 cellar: :any_skip_relocation, catalina:       "69018658005706c5b3fa81b0e963451db1d22e3c0cc049621520d9380bc58285"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53b44fb97aa289d016f5344fd819a82e46d7283b6dc9a49ec9174863d97534c5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c197f314e4bf07afbc736f2ff7e1dd5d4d9ac99454a731e4fe3e3d27f76e987f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "152b621ee168099a4ae008c8d1c7bd1b1606b1b160b225f1d7a6a6524470b079"
+    sha256 cellar: :any_skip_relocation, monterey:       "1f8b969d4d08bce4409128534fefe51caef10058389c29e21ebcb25df77063c2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f3ba1b7f2beedb323e2cbba26908d6e3c313ef6cd427fe3dffc92f133f61b26d"
+    sha256 cellar: :any_skip_relocation, catalina:       "d6a873af84cacef36d7a7199b838094155534f849dbd5b8021e648b7e7c076b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee1c53458c191f046b9bb6bde6c5cf4efd86c7375c6734f043533bd976d910eb"
   end
 
   depends_on "go" => :build
