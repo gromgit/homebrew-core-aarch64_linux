cff9177299bedbd8e96c6c5ef74faaa95a09dd31
Porkepix <porkepix@gmail.com>
2022-05-04 03:45:10 +0200

    xcb-proto 1.15
    Closes #100730.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xcb-proto.rb
+++ Formula/xcb-proto.rb
@@ -1,10 +1,9 @@
 class XcbProto < Formula
   desc "X.Org: XML-XCB protocol descriptions for libxcb code generation"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.14.1.tar.xz"
-  sha256 "f04add9a972ac334ea11d9d7eb4fc7f8883835da3e4859c9afa971efdf57fcc3"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.15.tar.xz"
+  sha256 "d34c3b264e8365d16fa9db49179cfa3e9952baaf9275badda0f413966b65955f"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "172e8c721816f274068bd9ec31a02ca14f036bb7f55ead5879c67dc42fabec16"
