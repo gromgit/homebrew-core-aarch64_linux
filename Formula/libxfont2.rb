0d4e78396518740806d6c49778a795c0931efaf5
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-05-04 10:20:42 +0000

    libxfont2: add 2.0.5 bottle.

--- Formula/libxfont2.rb
+++ Formula/libxfont2.rb
@@ -5,6 +5,15 @@ class Libxfont2 < Formula
   sha256 "aa7c6f211cf7215c0ab4819ed893dc98034363d7b930b844bb43603c2e10b53e"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "83ddeec421d8fe2eb3495608d9685cda2ab2da4a9d3972194f8a83f3ec6820a3"
+    sha256 cellar: :any,                 arm64_big_sur:  "dd658e51a88632bcf96758a55bc40ac412b9dae9ba09d1d070c55f90901bdccb"
+    sha256 cellar: :any,                 monterey:       "0c42b5f12cb49bcf04131bce885438578ea24e9f8bdfe841775d51ebf2a39947"
+    sha256 cellar: :any,                 big_sur:        "607dc750c3f3cd66d73d00110c394a9834d0c694de5934733022961c681b6fbc"
+    sha256 cellar: :any,                 catalina:       "58ea0e61969102d984913c1832fc88a2a32cff84a00a70b5de6178fc3998931a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1070ca317656349b712eac3509de1fed8e0c746ec1f076d5d0d8e10b65ca87ca"
+  end
+
   depends_on "pkg-config"  => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto"   => [:build, :test]
