79ef0cab508b1df17f94a598bfdf8b8d92771d83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-04 13:17:18 +0000

    chafa: update 1.10.3 bottle.

--- Formula/chafa.rb
+++ Formula/chafa.rb
@@ -11,12 +11,12 @@ class Chafa < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d5e90692c1975eaf2fd6de06601d5dc00da71b43bd726097a5538c0b2b980a94"
-    sha256 cellar: :any,                 arm64_big_sur:  "d064702055239a04785debdfb1337f9fd3a286ccfd17eb3d6de35979e4ed6c59"
-    sha256 cellar: :any,                 monterey:       "f5d084b5f3945b027c8a0041bf0fe71d0fa1dc7605e00ef32e8a23b1d2453f67"
-    sha256 cellar: :any,                 big_sur:        "c8956bdbeb2a0bbb4525875b2c089bad467edbcdc4dd7e0e2ffe49fd977b1f0b"
-    sha256 cellar: :any,                 catalina:       "f78ee0e8889ad13e58e29b92bc731ebaf7388026dbc618ee6cc5179b7400889f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "110de3d3360e9dbf8fddb87590a0b0ee876ce38fc2e6ea0f7f0a4cc69ab5c5aa"
+    sha256 cellar: :any,                 arm64_monterey: "deadb7d044c91f82f2becd3f703cdee32892b5dd808559aecf7c44087e01ff4a"
+    sha256 cellar: :any,                 arm64_big_sur:  "1fa2cd6c1e0909a1b06ad52b394d42305a611a13b2f97336d916917e4a9f2395"
+    sha256 cellar: :any,                 monterey:       "b9d82222f2d7ec948e5af12c2470352846b3bdd18737f83e260a15bff0745265"
+    sha256 cellar: :any,                 big_sur:        "6739c7f365e403105694fb7bd539348fca8bf1c01cb85700ebb93e8ce46bd904"
+    sha256 cellar: :any,                 catalina:       "16153e929a68f9da52fe709793031432b33344717fa3217a93dc2e3887595436"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "682396e890521341156d6590ae0354d1e5f8f8337dec00bd4d5523d67fbd91da"
   end
 
   depends_on "pkg-config" => :build
