6ef4c38f4a37462e58bdb2d018742bba2b1604a6
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-05-05 06:52:37 +0000

    xkbcomp: add 1.4.5 bottle.

--- Formula/xkbcomp.rb
+++ Formula/xkbcomp.rb
@@ -5,6 +5,15 @@ class Xkbcomp < Formula
   sha256 "6851086c4244b6fd0cc562880d8ff193fb2bbf1e141c73632e10731b31d4b05e"
   license all_of: ["HPND", "MIT-open-group"]
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "da94cb771debb09217e45550470e2299982cd7a01ecb648d3ea0ddd940c80f04"
+    sha256 cellar: :any,                 arm64_big_sur:  "c2007073201bb91ad1fa13fb49aef7ef2a89c2eec3d5812f98414eeb7c2198b7"
+    sha256 cellar: :any,                 monterey:       "2c78f64a5b041ae4c98d993a9640802b55c4424292c5fe22effc5df6a7668b80"
+    sha256 cellar: :any,                 big_sur:        "0890c9c6ba0c4eaedb5d5d241a751af3f40d45a4f8fa5c3487c2674fb0db02b8"
+    sha256 cellar: :any,                 catalina:       "9836909cc79f81d13fbe2f4a361f07d9013f16ab6ddcab8ebced35ebdf158790"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "503bab67d583ae0ad8ae02b5e1bced7c6db9804ff57aad88eca351e83ec230e3"
+  end
+
   depends_on "pkg-config" => :build
 
   depends_on "libx11"
