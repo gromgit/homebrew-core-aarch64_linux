6c330b2958dd4fd134c81cd9d90bd1556330eda4
Michael Cho <cho-m@tuta.io>
2022-05-05 16:17:33 -0700

    jigdo: use `berkeley-db@4`, add license and livecheck
    Closes #100883.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jigdo.rb
+++ Formula/jigdo.rb
@@ -3,10 +3,16 @@
 # tool that has been on life support and still works. Only build the CLI tool.
 class Jigdo < Formula
   desc "Tool to distribute very large files over the internet"
-  homepage "http://atterer.org/jigdo/"
+  homepage "https://www.einval.com/~steve/software/jigdo/"
   url "http://atterer.org/sites/atterer/files/2009-08/jigdo/jigdo-0.7.3.tar.bz2"
   sha256 "875c069abad67ce67d032a9479228acdb37c8162236c0e768369505f264827f0"
-  revision 6
+  license "GPL-2.0-only" => { with: "openvpn-openssl-exception" }
+  revision 7
+
+  livecheck do
+    url "https://www.einval.com/~steve/software/jigdo/download/"
+    regex(/href=.*?jigdo[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
     sha256 arm64_monterey: "2c59f74d01a6edcfbcf506eba52abfbfb869f8660a4b16c4b629a3f0bf65df53"
@@ -20,20 +26,31 @@ class Jigdo < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "berkeley-db"
+  depends_on "berkeley-db@4" # keep berkeley-db < 6 to avoid AGPL incompatibility
   depends_on "wget"
 
-  # Use MacPorts patch for compilation on 10.9; this software is no longer developed.
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/e101570/jigdo/patch-src-compat.hh.diff"
-    sha256 "a21aa8bcc5a03a6daf47e0ab4e04f16e611e787a7ada7a6a87c8def738585646"
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  on_macos do
+    # Use MacPorts patch for compilation on 10.9. Remove when updating to 0.8+.
+    patch :p0 do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e101570/jigdo/patch-src-compat.hh.diff"
+      sha256 "a21aa8bcc5a03a6daf47e0ab4e04f16e611e787a7ada7a6a87c8def738585646"
+    end
+  end
+
+  on_linux do
+    # Use Fedora patch for compilation with GCC. Remove when updating to 0.8+.
+    patch do
+      url "https://src.fedoraproject.org/rpms/jigdo/raw/27c01e27168b62157e98c7ffad1aa0b4aad405e9/f/jigdo-0.7.3-gcc43.patch"
+      sha256 "57e13ca6c283cb086d1c5ceb5ed3562fab548fa19e1d14ecc045c3a23fa7d44a"
+    end
   end
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    system "./configure", *std_configure_args,
                           "--disable-x11",
-                          "--prefix=#{prefix}",
                           "--mandir=#{man}"
     system "make"
     system "make", "install"
