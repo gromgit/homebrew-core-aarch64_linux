1750b30900b7366296de29c8d8f5defa181b87b8
Lars Kappert <lars@webpro.nl>
2022-05-05 10:23:09 -0300

    release-it 15.0.0 (new formula)
    Closes #100870.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/release-it.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class ReleaseIt < Formula
+  desc "Generic CLI tool to automate versioning and package publishing related tasks"
+  homepage "https://github.com/release-it/release-it"
+  url "https://registry.npmjs.org/release-it/-/release-it-15.0.0.tgz"
+  sha256 "52a22039887a5a131db2a3b8c524bd57f9bda4a091567711f5c5ee9cee8f8751"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/release-it -v")
+    (testpath/"package.json").write("{\"name\":\"test-pkg\",\"version\":\"1.0.0\"}")
+    assert_match(/Let's release test-pkg.+\(1\.0\.0\.\.\.1\.0\.1\).+Empty changelog.+Done \(in \d+s\.\)/m,
+      shell_output("#{bin}/release-it --npm.skipChecks --no-npm.publish --ci"))
+    assert_match "1.0.1", (testpath/"package.json").read
+  end
+end
