d84a19b3c0488e59db7a3531014f5f830896f78e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-05 08:08:45 +0000

    wabt: update 1.0.29 bottle.

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -12,12 +12,12 @@ class Wabt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ddff7f8d977d0f3b80853d46ec4429c5addde05077234737b204d8e02b8bd84"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e3beb4f82b667b6b2210e3e077b8ace61d75cad18f205db79e936ac905736bf8"
-    sha256 cellar: :any_skip_relocation, monterey:       "9bf660b5619c87efaaf3b44cb62c0b345a561e60e6675496e112a2573e1d0d02"
-    sha256 cellar: :any_skip_relocation, big_sur:        "da864d370bdb3de3878007c7e003b90666698b7676ab0bf246523c0f0021a6cf"
-    sha256 cellar: :any_skip_relocation, catalina:       "9d34e64abf6a46d9cb00c9327f8c95dae92226698e73d660ad3d96cd684fe1e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5b5993fa6cb0e649de405c34fdf439502dc348d3dc54a026e158b43e4579ecd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ed9228130cae7caaf99878bbb659c2ffd6004b2ce8fb1d93105aa0e0f993c4b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "35c8a5d9cbe6e488251dcb3dd8c0d129230585b4e3911ec0b0621d31257109d1"
+    sha256 cellar: :any_skip_relocation, monterey:       "9c56b047954e585faa6f5870104f4ced672c4d5f87b3fc38463cdca98ccc714b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "373276a1f3bb3cf8569facf06baece468478d3eaab13f92d0d780c5215044b2b"
+    sha256 cellar: :any_skip_relocation, catalina:       "6d1178fb069d4e7a109ae7a5105d8290a1e753670bcb2b205815abf113043e88"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0cfeafe8d40277ebc638324223ea361dd4e1711dcba42006b9483dcb74b7456d"
   end
 
   depends_on "cmake" => :build
