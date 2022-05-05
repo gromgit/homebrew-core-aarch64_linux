8c43b3fca6bd947f28687235fbef4a48b2d5478c
Rui Chen <rui@chenrui.dev>
2022-05-04 20:08:11 -0400

    autocannon 7.9.0
    Closes #100801.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/autocannon.rb
+++ Formula/autocannon.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Autocannon < Formula
   desc "Fast HTTP/1.1 benchmarking tool written in Node.js"
   homepage "https://github.com/mcollina/autocannon"
-  url "https://registry.npmjs.org/autocannon/-/autocannon-7.8.1.tgz"
-  sha256 "9b7bdd863b3fc66158aa0f69db75ac40369bdbcd28fd0a8d38d94f86435c1583"
+  url "https://registry.npmjs.org/autocannon/-/autocannon-7.9.0.tgz"
+  sha256 "7b195366e2e942f80fb3869daa29dfdf19380d3ee16740e6984acad533ab5051"
   license "MIT"
 
   bottle do
