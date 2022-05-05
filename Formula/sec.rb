fcdd3aa2da98148a768532e94fb6c949902c609a
Porkepix <porkepix@gmail.com>
2022-05-05 12:47:41 +0200

    sec 2.9.1
    Closes #100862.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -1,8 +1,8 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
   homepage "https://simple-evcorr.sourceforge.io/"
-  url "https://github.com/simple-evcorr/sec/releases/download/2.9.0/sec-2.9.0.tar.gz"
-  sha256 "741154d25db69706e2200e119b5cd32d65ae0b803d9c0faefcccfbcfe1c97503"
+  url "https://github.com/simple-evcorr/sec/releases/download/2.9.1/sec-2.9.1.tar.gz"
+  sha256 "63a4125930a7dc8d71ee67f2ebb42e607ac0c66216e1349f279ece8f28720a34"
   license "GPL-2.0-or-later"
 
   bottle do
