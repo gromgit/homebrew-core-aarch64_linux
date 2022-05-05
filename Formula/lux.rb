787602bf1d1a4b7eaebab2bf5707bc0a7a1e18af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-05 12:08:25 +0000

    lux: update 0.15.0 bottle.

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -7,12 +7,12 @@ class Lux < Formula
   head "https://github.com/iawia002/lux.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f580049bd8033547af807289952deccb1435752511e0cff46562a2e0ee0f710f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ec0c44a43b989408ca3bf89b1372dcc510080a4485dfbde8e6fe171184711ce"
-    sha256 cellar: :any_skip_relocation, monterey:       "3ca33d92a5aebb2374c5f4905591cc5ac1205d8186eb5d23e7c61b8a85ddd681"
-    sha256 cellar: :any_skip_relocation, big_sur:        "383a81d4b43aa2a2591c4bfe791ed29dcab48682ec012f7e09eb252cf3e92ab7"
-    sha256 cellar: :any_skip_relocation, catalina:       "04df6baff952484442ba52c9e5f658ad588e5ff0b3d76818f8ad5cc46ad0b0f9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9731bb27deb30763d11090536359db7cb40d42fbfcc7d589f1b1ad6450ce59e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c88913410858105cb233f3076167d8cfcc7cc21374a9cc1134e1a520c6ec6618"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d31fd3e601f2b122ad705fdbc8044b6a55f5b94a512949aca0359cd869d39c56"
+    sha256 cellar: :any_skip_relocation, monterey:       "a2039236ff2da7493d2a539dae8af265f0f09dc565f5b6a569e8be6378eda475"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9f883e14b543c0df497f7ab66c4e76bd335b78b03d41e2063e97707075a7aa75"
+    sha256 cellar: :any_skip_relocation, catalina:       "2cb75509d7e2047811af1dd550a0c7febeedae1b935a3a6da16a9732f2e2b319"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1324047684ba3a1479977df3b979df584299e63c8aa4924f1c8f6d8fda07b08a"
   end
 
   depends_on "go" => :build
