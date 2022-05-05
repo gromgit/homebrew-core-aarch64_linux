c256d4bb617497901b96dba026483bc4b566e3fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-05 05:37:59 +0000

    youtubedr 2.7.13
    Closes #100847.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/youtubedr.rb
+++ Formula/youtubedr.rb
@@ -1,8 +1,8 @@
 class Youtubedr < Formula
   desc "Download Youtube Video in Golang"
   homepage "https://github.com/kkdai/youtube"
-  url "https://github.com/kkdai/youtube/archive/v2.7.12.tar.gz"
-  sha256 "64a59341424d30f8a87c71a19ffa775f8b56ad0d02b7820c6dd474f1a956165e"
+  url "https://github.com/kkdai/youtube/archive/v2.7.13.tar.gz"
+  sha256 "082a6df600d390817ea3765196f5f5ca11a41ab81bfd591cb83a523acd011025"
   license "MIT"
 
   bottle do
