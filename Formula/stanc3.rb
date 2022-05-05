889b6b02b1a592df699c95d34f775885392402b4
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-05-05 08:39:09 +0000

    stanc3: add 2.29.2 bottle.

--- Formula/stanc3.rb
+++ Formula/stanc3.rb
@@ -7,6 +7,15 @@ class Stanc3 < Formula
       revision: "2c254b3b11a4dbb82395f016886b557201dad130"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bd4b41210dbcf5a6ffef5c4290ee15323dbe03b9fc79d80d9511e6900f699a6b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ab7b4f9ebd51d78882c0864ef03386060152690b8712b1b6153d5995e785430"
+    sha256 cellar: :any_skip_relocation, monterey:       "3bf2f015ff3055d27aac8cd834f946c6ab357bc7bbd4a7f6c910a90473b6ae88"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d951f0550f2f650e96b642e9fccb9722116098e07c7561075adeeb674698e3d0"
+    sha256 cellar: :any_skip_relocation, catalina:       "5010820eb8e638d588eccfe8a7de119326ed999ea63e4fc748a178ffa9bf8895"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "315bc18c788eae0d1c8498446c75949c56c95c8b9570b702606356efa9237c71"
+  end
+
   depends_on "ocaml" => :build
   depends_on "opam" => :build
 
