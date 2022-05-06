034ffe2fca0629e41d433ffbbca80b8f5e97aa3e
0az <0az@afzhou.com>
2022-05-06 01:26:48 -0700

    jenv: Patch out the obsolete export.fish shim
    Closes #100954.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jenv.rb
+++ Formula/jenv.rb
@@ -4,11 +4,16 @@ class Jenv < Formula
   url "https://github.com/jenv/jenv/archive/0.5.4.tar.gz"
   sha256 "15a78dab7310fb487d2c2cad7f69e05d5d797dc13f2d5c9e7d0bbec4ea3f2980"
   license "MIT"
+  revision 1
   head "https://github.com/jenv/jenv.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "521a1ad6e28b90f1e37893d279950e35957a0580464d639ec74c398f8da6d466"
   end
+
+  # Upstream ships an unnecessary export shim for fish shell, which breaks scripts
+  # https://github.com/Homebrew/homebrew-core/pull/100234#issuecomment-1111862141
+  patch :DATA
 
   def install
     libexec.install Dir["*"]
@@ -35,3 +40,14 @@ class Jenv < Formula
     shell_output("eval \"$(#{bin}/jenv init -)\" && jenv versions")
   end
 end
+__END__
+diff --git a/fish/export.fish b/fish/export.fish
+deleted file mode 100644
+index 14dbbec..0000000
+--- a/fish/export.fish
++++ /dev/null
+@@ -1,4 +0,0 @@
+-function export
+-  set arr (echo $argv|tr = \n)
+-  set -gx $arr[1] $arr[2]
+-end
