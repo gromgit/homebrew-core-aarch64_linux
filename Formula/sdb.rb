e5780d16629b019b257d10a671f2c42e4a723f53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-06 16:09:10 +0000

    sdb: update 1.8.8 bottle.

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -7,12 +7,12 @@ class Sdb < Formula
   head "https://github.com/radare/sdb.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "be36bdcd1618af66b970c9065c53f4fd8decc8b1444a2ba50c64de87baf2e000"
-    sha256 cellar: :any,                 arm64_big_sur:  "f57086d47de992aed35787eb990c7e5cf9a678c45b61c004188156156623ce27"
-    sha256 cellar: :any,                 monterey:       "b9fad697dbb950e3b7cf84fa876a4557e56bbb94eea3b9a952184073db3de54f"
-    sha256 cellar: :any,                 big_sur:        "ce9d738ae013083d6993b255468964919b2b19c911c95a27a8c7eb13ed3eaa3a"
-    sha256 cellar: :any,                 catalina:       "dd9e453d898cc2c456fb5cd5ee943aa5a843c83381fea81b542cba014cad6886"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dcb88db1e47a0e12c96ecf7cb7486f20161e2a377c73e59118a2104dba943268"
+    sha256 cellar: :any,                 arm64_monterey: "7a4065eb647b0b58d67819fc581813fef16e9b0b489c14b4bb510f65dff7e781"
+    sha256 cellar: :any,                 arm64_big_sur:  "f02dabbac809bcf85a23dcd8361839643d6d79d8a2e8a716b3f014a8032b1eb8"
+    sha256 cellar: :any,                 monterey:       "6160ad028353f086087c56601ff7c47a3522e8249898a4dc2869ee56ae57e667"
+    sha256 cellar: :any,                 big_sur:        "f3c1f0e9f9fd6cad879a6b1c58445876400b099b6e581210d5088978a860840e"
+    sha256 cellar: :any,                 catalina:       "2504b2cf1b92990e415f45154545a0a92ffdfb4df536a5ca274f22cf402123fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1633e6de9e6ef24ba56638b3207ca205c2bce652aba9d3bfe3ac456f188e9ed1"
   end
 
   depends_on "pkg-config" => :build
