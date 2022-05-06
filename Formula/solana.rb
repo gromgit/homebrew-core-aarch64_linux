b646f180facbbce4dfd229f3d8a18f5b1bcbc677
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-06 05:26:52 +0000

    solana 1.9.20
    Closes #100925.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/solana.rb
+++ Formula/solana.rb
@@ -1,8 +1,8 @@
 class Solana < Formula
   desc "Web-Scale Blockchain for decentralized apps and marketplaces"
   homepage "https://solana.com"
-  url "https://github.com/solana-labs/solana/archive/v1.9.19.tar.gz"
-  sha256 "7f816fcaa7647d1e94d318e3f0ce7a98efab2f418561ffb19dc04c5fbe132231"
+  url "https://github.com/solana-labs/solana/archive/v1.9.20.tar.gz"
+  sha256 "b7cb1e792167247dc575d49f1a1302e909c68df89d647c313b8c15ede243e99a"
   license "Apache-2.0"
 
   # This formula tracks the stable channel but the "latest" release on GitHub
