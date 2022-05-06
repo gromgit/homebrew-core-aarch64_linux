e9db6079207a42ba6fb9a4d242cbb700c108a0a2
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-05-06 01:08:26 +0000

    fuego: update 1.1.SVN_3 bottle.

--- Formula/fuego.rb
+++ Formula/fuego.rb
@@ -12,6 +12,7 @@ class Fuego < Formula
     sha256 monterey:       "32b9463d445e6655142662879346d4e6e19d4acc3df20cf79ee7d04d08763fbf"
     sha256 big_sur:        "ec7e854d2bfa8853265051a3979e5dd49e357f5ae3a92fba709ba87016cfc4ac"
     sha256 catalina:       "46cb29e00532a085978e00fa78a763b0273a7722e670f97ea1bd779c554cda62"
+    sha256 x86_64_linux:   "2abd98ef2ba46b27da1b058174d93bc41ea3707c0dc444c1203117fff43071bb"
   end
 
   depends_on "autoconf" => :build
