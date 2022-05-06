f2c661cd6bb99590d4f9c1df04b0943d472d35f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-06 08:10:34 +0000

    opensearch: update 1.3.2 bottle.

--- Formula/opensearch.rb
+++ Formula/opensearch.rb
@@ -6,12 +6,12 @@ class Opensearch < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "82c9268efa73564391f6b99ea0c37d7d48c3a61f3652c275f499c31c57a11e2c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "82c9268efa73564391f6b99ea0c37d7d48c3a61f3652c275f499c31c57a11e2c"
-    sha256 cellar: :any_skip_relocation, monterey:       "cf84778e6ec1b476640697735bcad761f0594b6c3b4a931c2841440b19aa31d0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cf84778e6ec1b476640697735bcad761f0594b6c3b4a931c2841440b19aa31d0"
-    sha256 cellar: :any_skip_relocation, catalina:       "cf84778e6ec1b476640697735bcad761f0594b6c3b4a931c2841440b19aa31d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac5d9a44a6f2577800a443da819f9de9d5c0e9d71c45c149102cc61876e2e82e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "13436e7ca3dea29388f88f99dea00da2440631ec960fce1e44f9e87a1d3de603"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "13436e7ca3dea29388f88f99dea00da2440631ec960fce1e44f9e87a1d3de603"
+    sha256 cellar: :any_skip_relocation, monterey:       "13436e7ca3dea29388f88f99dea00da2440631ec960fce1e44f9e87a1d3de603"
+    sha256 cellar: :any_skip_relocation, big_sur:        "13436e7ca3dea29388f88f99dea00da2440631ec960fce1e44f9e87a1d3de603"
+    sha256 cellar: :any_skip_relocation, catalina:       "13436e7ca3dea29388f88f99dea00da2440631ec960fce1e44f9e87a1d3de603"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ca92a782597ec470db653f486e683e0bd42323916e877875de15ef2b42d1b48"
   end
 
   depends_on "gradle@6" => :build
