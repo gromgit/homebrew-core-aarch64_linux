77f7b096b6ac15e1d5c306a2b4a6d2a08079cf8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-06 11:04:45 +0000

    wolfmqtt: update 1.13.0 bottle.

--- Formula/wolfmqtt.rb
+++ Formula/wolfmqtt.rb
@@ -7,12 +7,12 @@ class Wolfmqtt < Formula
   head "https://github.com/wolfSSL/wolfMQTT.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "116d2d344c4d84e366acdfba68429f7b063b637ad4336a798a33b5e96516f23f"
-    sha256 cellar: :any,                 arm64_big_sur:  "5d1b6c622566fdeecf98ae6aba2802ab5f4a1ec94572b6d79714164ac040c265"
-    sha256 cellar: :any,                 monterey:       "38d2cd4ec1831dee9cf3ca1438ac4a0589e3bfe155cd4457ce1bb7fb844d5743"
-    sha256 cellar: :any,                 big_sur:        "7bfe8733dc442facf67d29f53fad757fff4135361a600b7035fffb238571fd90"
-    sha256 cellar: :any,                 catalina:       "3bcd431af7a2572da711cc4479f080cd3b70e2ef99017c487b997fd6137b7d9c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5447230b2e036bc2474f6dbd663586745655b7c143d8fb5a04a87ad9ed256208"
+    sha256 cellar: :any,                 arm64_monterey: "fbbccc871914e2218ea6825597ebd726939f5115c61e0b54c96993b618d46961"
+    sha256 cellar: :any,                 arm64_big_sur:  "cd5ab1f427678382887f63ed8391796d7144c161c84a5b0e6bda68173809dde5"
+    sha256 cellar: :any,                 monterey:       "54b7dbe324e4a26a46d83298227223a8bceb54f7b46503b518b9278de961ec56"
+    sha256 cellar: :any,                 big_sur:        "a29a7949afd165200cdb66b1031d80ae0caf23d706e2682e8c6bac3b58847acf"
+    sha256 cellar: :any,                 catalina:       "2856e58fa5809a24b85633d13d9c2d7c76a750a0989cc63194b3775989381a3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed92de5ea87809d6ef9d9aeab01d83fea0caeb176fcd0e2a8477fb1a3b975305"
   end
 
   depends_on "autoconf" => :build
