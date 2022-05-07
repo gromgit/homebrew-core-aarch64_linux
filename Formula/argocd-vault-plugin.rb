8b540bf5e14b27908d4b9dc209f0e3709a181714
Rui Chen <rui@chenrui.dev>
2022-05-07 17:02:26 -0400

    argocd-vault-plugin: update repo location
    Closes #101064.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argocd-vault-plugin.rb
+++ Formula/argocd-vault-plugin.rb
@@ -1,7 +1,7 @@
 class ArgocdVaultPlugin < Formula
   desc "Argo CD plugin to retrieve secrets from Secret Management tools"
   homepage "https://argocd-vault-plugin.readthedocs.io"
-  url "https://github.com/IBM/argocd-vault-plugin.git",
+  url "https://github.com/argoproj-labs/argocd-vault-plugin.git",
       tag:      "v1.11.0",
       revision: "4133295001e037b917a8884da84af42f12a51cae"
   license "Apache-2.0"
@@ -22,9 +22,9 @@ class ArgocdVaultPlugin < Formula
 
     ldflags = %W[
       -s -w
-      -X github.com/IBM/argocd-vault-plugin/version.Version=#{version}
-      -X github.com/IBM/argocd-vault-plugin/version.BuildDate=#{time.iso8601}
-      -X github.com/IBM/argocd-vault-plugin/version.CommitSHA=#{Utils.git_head}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.Version=#{version}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.BuildDate=#{time.iso8601}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.CommitSHA=#{Utils.git_head}
     ]
 
     system "go", "build", *std_go_args(ldflags: ldflags)
