92a23c21d0a9309c19f7b1b698c51713c4f3b7a6
Porkepix <porkepix@gmail.com>
2022-05-07 10:53:27 +0200

    fcitx-remote-for-osx 0.4.0
    Closes #101018.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fcitx-remote-for-osx.rb
+++ Formula/fcitx-remote-for-osx.rb
@@ -1,8 +1,10 @@
 class FcitxRemoteForOsx < Formula
+  include Language::Python::Shebang
+
   desc "Handle input method in command-line"
   homepage "https://github.com/xcodebuild/fcitx-remote-for-osx"
-  url "https://github.com/xcodebuild/fcitx-remote-for-osx/archive/0.3.0.tar.gz"
-  sha256 "b4490a6a0db3c28ce3ddbe89dd038f5ab404744539adc5520eab1a1a39819de6"
+  url "https://github.com/xcodebuild/fcitx-remote-for-osx/archive/0.4.0.tar.gz"
+  sha256 "453c99a0c2e227c29e2db640c592b657342a9294a3386d1810fd4c9237deeaae"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -17,9 +19,13 @@ class FcitxRemoteForOsx < Formula
     sha256 cellar: :any_skip_relocation, sierra:         "16efcc3f2a5ac6fd63bfea3d85286fac823cc7b21520d85f46d0b3c066668671"
   end
 
+  # need py3.6+ for f-strings
+  depends_on "python@3.10" => :build
   depends_on :macos
 
   def install
+    rewrite_shebang detected_python_shebang, "./build.py"
+
     system "./build.py", "build", "general"
     bin.install "fcitx-remote-general"
     bin.install_symlink "fcitx-remote-general" => "fcitx-remote"
