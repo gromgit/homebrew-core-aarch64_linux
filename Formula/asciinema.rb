0e24bdc3bdd92e5371512657558bff55b14909ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-07 23:04:37 +0000

    asciinema: update 2.2.0 bottle.

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -9,13 +9,12 @@ class Asciinema < Formula
   head "https://github.com/asciinema/asciinema.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1fea4ae9e201966f38b7b1d5a5edd46f047b8ab80ca382e5a4d218081ae5c8d5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1fea4ae9e201966f38b7b1d5a5edd46f047b8ab80ca382e5a4d218081ae5c8d5"
-    sha256 cellar: :any_skip_relocation, monterey:       "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, catalina:       "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, mojave:         "3fe5b03bd3cb9eec10a3c4627e02421abe10ab1c96e264606d2bf5254914152b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "388d5d894e1150768c822e978ec7c86cd62a1e730d98eb5055823dbbd728d7a8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b7156153fe5acb94ceefbcfda04d2e85a1e360746ae8a664483bb3c317bbc9e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "875fa2c3235403134afa78532bd65e97709f7660ad7d1f6452c119acb04cf877"
+    sha256 cellar: :any_skip_relocation, monterey:       "d559e0bd651c20fbd3af3dcaf1723cf711f2bdf9f4614bf8c6b4815b98c6aa03"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8a7cd8ba3389f298b673403020b9bd290e3da2a5030d8ea447bb2be98fe8ebf5"
+    sha256 cellar: :any_skip_relocation, catalina:       "1a852bcd64112ff5d16c76c558871fee755f17d3feb71bc7bb0401830b8fdae2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5777ace4f226567175cf753959964e78f12ac98cd77dbe9752ace1a87fda0bb7"
   end
 
   depends_on "python@3.10"
