4489307d36e03c4a07831ba2d79acdea2b4ce98b
Jun Lin <linjunpop@gmail.com>
2022-05-09 19:47:26 +0800

    imessage-ruby 0.4.0
    * imessage-ruby 0.3.3
    * Bump to v0.4.0.
    * Update imessage-ruby.rb
    * Update imessage-ruby.rb
    * Always uses ruby from macOS.
    * Format with rubocop.
    * Align hash key and values
    * Update imessage-ruby.rb
    * Update imessage-ruby.rb
    * Bring the `bottle` closure back.
    * No need to print the gem info.
      It was used for debugging.
    * Remove extra space.
    
    Closes #101155.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imessage-ruby.rb
+++ Formula/imessage-ruby.rb
@@ -1,8 +1,8 @@
 class ImessageRuby < Formula
   desc "Command-line tool to send iMessage"
   homepage "https://github.com/linjunpop/imessage"
-  url "https://github.com/linjunpop/imessage/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "3d93570dd3ff000c51859aaa34c4a8e8b80b86e6bdf3018c1c7f6f8d85939b30"
+  url "https://github.com/linjunpop/imessage/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "09031e60548f34f05e07faeb0e26b002aeb655488d152dd811021fba8d850162"
   license "MIT"
   head "https://github.com/linjunpop/imessage.git", branch: "master"
 
@@ -10,13 +10,24 @@ class ImessageRuby < Formula
     sha256 cellar: :any_skip_relocation, all: "7c538ac24ec3ce437d868267582bd68aaa500eeb5a9bdbd3d0d80398b4bab19d"
   end
 
-  depends_on :macos
+  uses_from_macos "ruby"
 
   def install
-    system "rake", "standalone:install", "prefix=#{prefix}"
+    ENV["GEM_HOME"] = libexec
+    ENV["GEM_PATH"] = libexec
+
+    system "gem", "build", "imessage.gemspec", "-o", "imessage-#{version}.gem"
+    system "gem", "install", "--local", "--verbose", "imessage-#{version}.gem", "--no-document"
+
+    bin.install libexec/"bin/imessage"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
 
   test do
-    system "#{bin}/imessage", "--version"
+    if build.head?
+      system "#{bin}/imessage", "--version"
+    else
+      assert_match "imessage v#{version}", shell_output("#{bin}/imessage --version")
+    end
   end
 end
