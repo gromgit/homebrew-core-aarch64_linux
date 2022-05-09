662d9f2b14716716ee39a2bcb515d98ff71c2e1a
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-05-09 06:42:23 +0000

    alluxio: update 2.8.0 bottle.

--- Formula/alluxio.rb
+++ Formula/alluxio.rb
@@ -16,6 +16,7 @@ class Alluxio < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "8f28c665a49d478eaab64e044bc9a6773cd0f66548277434b53a00b29ac6dec9"
     sha256 cellar: :any_skip_relocation, big_sur:        "8f28c665a49d478eaab64e044bc9a6773cd0f66548277434b53a00b29ac6dec9"
     sha256 cellar: :any_skip_relocation, catalina:       "8f28c665a49d478eaab64e044bc9a6773cd0f66548277434b53a00b29ac6dec9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10e26cdd6744eb4db06575f50f636aa0b16db47ca284a25e3bb827352c27b0e4"
   end
 
   # Alluxio requires Java 8 or Java 11
