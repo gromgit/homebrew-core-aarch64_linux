0178154dfca646a327def8a477e0632ac02a0cdd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-10 13:20:13 +0000

    chezmoi: update 2.15.4 bottle.

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -8,12 +8,12 @@ class Chezmoi < Formula
   head "https://github.com/twpayne/chezmoi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5395446ab76ee2c516b687e274ce43cd96f53864266387ae6fe5c7fc217eb163"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "81d6cd66eb881a18579a1e2d6a08e775170471c3f168be1dc79235b88d42d627"
-    sha256 cellar: :any_skip_relocation, monterey:       "0680723287929e48d2546eabeb62ea8dcc89397556e9fd77ff7513a14db31fbb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "33a90eae50d7811f99185e8675be33e7821975bf4f45ffbc363dc69eb5fae7ea"
-    sha256 cellar: :any_skip_relocation, catalina:       "636a9d77601e8e63bf81ac057f2aa0660b55976141ba80cd6177378ada599971"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a75c298fd8119bfaf7779185806e0111300a5198dcac48f992b3b5268fac0e35"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "70420e13d9fd8bc935ca7081e5fe25e8b9030901af642c812fd01330ad8256bd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "243d872ec65934b2b508a57b48202b56f06aa6de53eaeaaf744e227a0548ae2e"
+    sha256 cellar: :any_skip_relocation, monterey:       "07d9395368ae6b1822dce8e72cba7a69ebee8e2ba99e63294de34db7c89a2b13"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b5caf9bab7935e74ccf1a91678b0bdb113cf23763bb07481226870b3b000b69e"
+    sha256 cellar: :any_skip_relocation, catalina:       "380fc8b4453e400bb90403fc664dce377518b53cdac007d2c96bb941dab0b278"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5fed887338e708abd6a01226151fa99a58cd49e0abef2bb2cbf3623cccf8282"
   end
 
   depends_on "go" => :build
