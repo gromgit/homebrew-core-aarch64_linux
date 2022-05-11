a0cc6951cc81682c39004c0467e5832e3acab758
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-11 05:33:23 +0000

    naabu 2.0.7
    Closes #101267.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/naabu.rb
+++ Formula/naabu.rb
@@ -1,8 +1,8 @@
 class Naabu < Formula
   desc "Fast port scanner"
   homepage "https://github.com/projectdiscovery/naabu"
-  url "https://github.com/projectdiscovery/naabu/archive/v2.0.5.tar.gz"
-  sha256 "ec9ef8c7cff41d43b754859e4eba9c7aa2453100371c4bd01e82ac46dcf8f424"
+  url "https://github.com/projectdiscovery/naabu/archive/v2.0.7.tar.gz"
+  sha256 "dabd5f7b883f806483b2a5a879c583840cdda74dc6f3d3781b1b375744e9feac"
   license "MIT"
   head "https://github.com/projectdiscovery/naabu.git", branch: "master"
 
