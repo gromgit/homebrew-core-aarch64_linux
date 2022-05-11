2dbcb22a55cf4f557b381816d44cbd9b3047dcb6
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-05-11 16:27:00 +0000

    kind: update 0.13.0 bottle.

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -7,12 +7,12 @@ class Kind < Formula
   head "https://github.com/kubernetes-sigs/kind.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "544f1036a81b47d91b8ad81360fde3c063a4add7d50977f90992c9cada3eeaf8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d2a2b35caeebe678726302b67ab3e1f72440baad3b3d2faf7150ffcf834c0223"
-    sha256 cellar: :any_skip_relocation, monterey:       "eb73ee98fae1727acc28ed0dbc86ad6c6ee1178c84f43c25402e1fc71f97d189"
-    sha256 cellar: :any_skip_relocation, big_sur:        "65f176fbfc2d8af2612cb8d92f5f89575a821fa4f08abe1bb565c8f8a5beaddb"
-    sha256 cellar: :any_skip_relocation, catalina:       "78e39031c372fe01a862f80e64f245cdbf221ba3c9606f690d6d9e8bbe7446c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d565d46ce69dfa673243aa998ba4936a4f575e10f33b2877940f6bb7fdba00f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a7b6b34dfd45ad097b5ad3b5001a2e135141bba2a34863b0edc7ba2f52b038e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e6f5afdbd4bb2fa83f9b03ef8781f440529b60e18b937150aa852d5c240966e"
+    sha256 cellar: :any_skip_relocation, monterey:       "41f08d541f68838b9eb5ad31a17db48e97224b6a36f45dae736a8c8a195a590e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1cdd06d155b338767a66bb296ff81910fbaa3557cfc4cf43ed9a82053dc4a9c9"
+    sha256 cellar: :any_skip_relocation, catalina:       "674b16ed62f8ee9b7d330d88b2492e037968927d3a852e0f54a88984ec008df6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91e6cb05ccfb3d310f1c108af286f8323f7892a7c8faaec468634dca753b422d"
   end
 
   depends_on "go" => :build
