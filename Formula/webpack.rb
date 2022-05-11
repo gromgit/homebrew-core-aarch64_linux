0c4055d7f35d45744b160d7d1bb2c8c33970e2f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-11 05:49:14 +0000

    webpack 5.72.1
    Closes #101276.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -4,8 +4,8 @@ require "json"
 class Webpack < Formula
   desc "Bundler for JavaScript and friends"
   homepage "https://webpack.js.org/"
-  url "https://registry.npmjs.org/webpack/-/webpack-5.72.0.tgz"
-  sha256 "2aeb4cf8d9fffced8a1fa24183dbe8d6a821c88662db6d2e019365db2fe5731b"
+  url "https://registry.npmjs.org/webpack/-/webpack-5.72.1.tgz"
+  sha256 "98c9dc5a5d7a88e151af0d79f7257b8b6a9fffb41398edc56db065aea226da14"
   license "MIT"
   head "https://github.com/webpack/webpack.git", branch: "main"
 
