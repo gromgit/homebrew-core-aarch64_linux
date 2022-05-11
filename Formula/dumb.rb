b0c5814c4d640a91ae64aae77101078033fc57ca
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-05-11 20:28:04 +0000

    dumb: update 2.0.3 bottle.

--- Formula/dumb.rb
+++ Formula/dumb.rb
@@ -5,17 +5,12 @@ class Dumb < Formula
   sha256 "99bfac926aeb8d476562303312d9f47fd05b43803050cd889b44da34a9b2a4f9"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "eafff4c73e4401c1d6cbb6a5ae9098dce369766057bf08516540f0ff24d48a92"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d907efc07aefb58f43bca29a17b58957eb071c73e6aef3abd91e59687e5a78ed"
-    sha256 cellar: :any_skip_relocation, monterey:       "bf474d0e438d7be3d3a7a432e4f0d4f8a27ae23165e37fe52f28c15118dec743"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8ea105e8d30bae67ddb21a9e7eaba69cd2f372c41dd11d05f80756ec9d3fffee"
-    sha256 cellar: :any_skip_relocation, catalina:       "dbb9dbb86ec1c5ff1bc9da976fe3ec481888b4c3cd5cd8b10b6c803b83d934f2"
-    sha256 cellar: :any_skip_relocation, mojave:         "e2956b48f246b68f98a5b39e81e371bd544d78b7bb0e97f5282cfc27e9b307cd"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "674db2be479a742057619122759da52683c74b724b3e318f2fc71a4fa6bd7287"
-    sha256 cellar: :any_skip_relocation, sierra:         "04219fcc6bf6cd174cb5c2ddde4bfdbff266ed665e543c9948911e731d682dc9"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "d2352df11bee735e963b887609578ec1b3acf0e07748385f472a6add0e1cd2b6"
-    sha256 cellar: :any_skip_relocation, yosemite:       "317ac8139d8efb03022bb4f9a76ad61f2358570680563924d13229c52b282dff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b9703a5ec46976384a0fb16f851ef2ed5017eceba818383f0b7993b09a30542"
+    sha256 cellar: :any,                 arm64_monterey: "f046a0784378b98ac76156a7a5c306a8c4d931130309c64d6205b1bfd2d0dcfe"
+    sha256 cellar: :any,                 arm64_big_sur:  "94dd00c18fd4f11400f30074357c4979fe727f49df6e45ad457e79a51d801f46"
+    sha256 cellar: :any,                 monterey:       "a0933282bbe2feb52a06cac1a1a189b83af422e422036f78b0fcfa0e55f5726c"
+    sha256 cellar: :any,                 big_sur:        "2dade8ff6646f71df07f3b2d586c9bb49ae24c3f0b5ddddea7a09a3762501f5c"
+    sha256 cellar: :any,                 catalina:       "f9d0768b3b50614adfb2190899362e250f20a14be0fb0c2d21d37bee0afea672"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "de4310947cbbc1807cf9c2bbdb9dcc42cc381de7153b3dc4f206718a1923fe40"
   end
 
   depends_on "cmake" => :build
