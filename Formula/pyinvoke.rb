eeccbd8753238fb788be4fdbe3eb0e57d503b5c3
Dustin Rodrigues <dust.rod@gmail.com>
2022-05-11 15:02:57 -0400

    pyinvoke 1.7.1
    Closes #101309.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyinvoke.rb
+++ Formula/pyinvoke.rb
@@ -3,8 +3,8 @@ class Pyinvoke < Formula
 
   desc "Pythonic task management & command execution"
   homepage "https://www.pyinvoke.org/"
-  url "https://files.pythonhosted.org/packages/55/a2/763dc56e746ca013061b26c95868cb7f832c2a87dc27ed749641a734957f/invoke-1.7.0.tar.gz"
-  sha256 "e332e49de40463f2016315f51df42313855772be86435686156bc18f45b5cc6c"
+  url "https://files.pythonhosted.org/packages/df/59/41b614b9d415929b4d72e3ee658bd088640e9a800e55663529a8237deae3/invoke-1.7.1.tar.gz"
+  sha256 "7b6deaf585eee0a848205d0b8c0014b9bf6f287a8eb798818a642dff1df14b19"
   license "BSD-2-Clause"
   head "https://github.com/pyinvoke/invoke.git", branch: "master"
 
