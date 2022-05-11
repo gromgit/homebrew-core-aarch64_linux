c8e5a111072a8c244c767eab593549d786ef2aad
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-05-11 16:23:34 +0000

    fnlfmt: update 0.2.3 bottle.

--- Formula/fnlfmt.rb
+++ Formula/fnlfmt.rb
@@ -6,7 +6,7 @@ class Fnlfmt < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "18b456dcb6cc242942c5677c9b8ab8e74274426f5eba01038819dd5b71f9db79"
+    sha256 cellar: :any_skip_relocation, all: "28781ba4b856d98bae1c75b51e9d164dbb1f91c569ea421e5a41586410c363d5"
   end
 
   depends_on "lua"
