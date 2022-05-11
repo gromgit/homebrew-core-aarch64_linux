7d2fd8ac11addc87232103e4d5b163c166437ef8
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-05-11 16:17:53 +0000

    brev: update 0.6.57 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -11,12 +11,12 @@ class Brev < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b709561853c80d1c8caa11b3c44c8e1e1c3371f39cf5e59b219048acc9d4782"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c02d97c985ddd7f23c655ddd1f45e199cea89225e1d7567a004c51f2a09c8fcb"
-    sha256 cellar: :any_skip_relocation, monterey:       "95dd51b793277e529da8eb2df604194412cf5b9f3a11151120459966681d7f1a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "582955037d15256dbfd559b67d529430a89f6170066ee6f9ad4cbea04edf910f"
-    sha256 cellar: :any_skip_relocation, catalina:       "60e35b4198b7426ef9e8a529fbf2e30b33098aba0a343a87178d4936e6e76376"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fcf58f39af35476fb7041da67c2bcde351bdea165916991a918b36fd23e94f9f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2cea77226baf2abca2e636246ff96db34a2e479aed65281d5c1f664772392d38"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa123bb893f37a6e6de1a0544fb9b866e312a4e8c8eba7731c95ed8100429da3"
+    sha256 cellar: :any_skip_relocation, monterey:       "ce7be9930f582e1e9f6d45f578a7fcccbe202da966db277e86fede1d4ed0c689"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f3473506a37b9a7ba5366f3f5f938c4dd808035e30285a09bef1981eb999c734"
+    sha256 cellar: :any_skip_relocation, catalina:       "3999f961451c70b4fbb7fc9562a2cfbcd1febfde7a4d0503595041df573a0ad6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "270c171409348961dc260eb850a8d546ea2d080bd0a98fbfd43fe2ebf60f31ff"
   end
 
   depends_on "go" => :build
