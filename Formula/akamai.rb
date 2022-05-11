903dfd0447aae2d0599e51bd03f95eb5171ebc6e
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-05-11 16:36:03 +0000

    akamai: update 1.4.2 bottle.

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -6,10 +6,10 @@ class Akamai < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "cc8ef6a5777d028e6b478a5c27a92fa203f6a3f1de0912d2770f8de7671df239"
-    sha256 cellar: :any_skip_relocation, big_sur:      "2a7fac5947fec9ed49ee8b2cbc3d70b6f80adc106c6d66d2a5921ea18f155c15"
-    sha256 cellar: :any_skip_relocation, catalina:     "909dde4413ebf4f4232cf1b384cfc2135b9829cc384048eda44bd4afb0a38129"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d119c09c03c9c041ee91aff4b7ede4ce651935dcdaab7d49ed8394fc7c74f75b"
+    sha256 cellar: :any_skip_relocation, monterey:     "356f7d8cbd9c999795ebb918fa004368596e1e9992741e3a46d7c0243cf0f16a"
+    sha256 cellar: :any_skip_relocation, big_sur:      "67beb33a60760f11cdad4e30c2a45ff85ad63de081677b5d31011692e87dd2ac"
+    sha256 cellar: :any_skip_relocation, catalina:     "2a0d8a62bf53a295697b9c9e5e6ef137507b55ffad07a6f25100a2e6134dd4a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d68de8020c21317cae498f11a875cb3b19ea9b1e20355940071627caeb43239f"
   end
 
   depends_on "go" => :build
