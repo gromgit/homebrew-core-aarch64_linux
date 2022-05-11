8872609173101de376b757eba31109eae80bc1d4
Bo Anderson <mail@boanderson.me>
2022-05-11 04:08:42 +0000

    wakatime-cli: update 1.45.1 bottle.

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -8,12 +8,12 @@ class WakatimeCli < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9409d2962406666dcc76f31c95598d172b36424da435cabc990ef87a249e923e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "782bad0eb4d4c294c3604463663891f044307d58258ee027c0051af28410e834"
-    sha256 cellar: :any_skip_relocation, monterey:       "26c5d21777a2f95da753966c2298afd99c5f42c61f9de4611fde65d8d212cfcc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "454ec02a875eccf124b169394665790358bfcc7b615eb4e1b72bbc261ace6d0b"
-    sha256 cellar: :any_skip_relocation, catalina:       "70c05b7e4655c109dc8e608efbfb1200bb1c95aea33f04a727f363a63192875c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cbc2d4f95e4cf4e00e7ee13faf239c5389fab6e1876d47e4d9037bdee0dfb0f2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cda49187377dee064d369fa864d540aa101c0786cbe1d2c91077a75b50d00c12"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b731de13c04a8823f1943fff1589b667808deadeb6cdefdc88ee6493f4405d99"
+    sha256 cellar: :any_skip_relocation, monterey:       "a826454338271b40d93f7e5e1fdfd8c0209c907043a5f6430bf65a8042e04164"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b1af0a9a095a21ea8060aea3273937f73c50bf4c8ff1418530e5ba26cb5bd743"
+    sha256 cellar: :any_skip_relocation, catalina:       "e06cd9cedaf3882c81203da199672d43457ce20d0f1f8daa4a570028e103d429"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33a2a2808d2ce6e3c82e3271fff07610b955bb13c27f9ce23a666bda37c74b92"
   end
 
   depends_on "go" => :build
