a1bed31bed6cc10c97b021589d4292e9e704fbb1
Porkepix <porkepix@gmail.com>
2022-05-12 13:37:40 +0200

    proteinortho 6.0.35
    Closes #101351.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -1,8 +1,8 @@
 class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
-  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0.34/proteinortho-v6.0.34.tar.gz"
-  sha256 "2559eee1967d066afbb3fb182e386761360eaac644b0f56986216b75eb3fcef1"
+  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0.35/proteinortho-v6.0.35.tar.gz"
+  sha256 "1aa4a887d82a1074f1032d5a56858b35505d4c9937080a4f366fbdcc83e483d3"
   license "GPL-3.0-or-later"
 
   bottle do
