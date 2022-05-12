1349c730cb91c4243c204f1f546a774f5f95782b
owine <owine@users.noreply.github.com>
2022-05-12 12:44:41 -0500

    postgresql 14.3

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -1,10 +1,9 @@
 class Postgresql < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v14.2/postgresql-14.2.tar.bz2"
-  sha256 "2cf78b2e468912f8101d695db5340cf313c2e9f68a612fb71427524e8c9a977a"
+  url "https://ftp.postgresql.org/pub/source/v14.3/postgresql-14.3.tar.bz2"
+  sha256 "279057368bf59a919c05ada8f95c5e04abb43e74b9a2a69c3d46a20e07a9af38"
   license "PostgreSQL"
-  revision 1
   head "https://github.com/postgres/postgres.git", branch: "master"
 
   livecheck do
