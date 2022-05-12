856ea7371cc6029b3665950368543510922fe020
Vlad <vzhd1701@gmail.com>
2022-05-12 21:38:05 +0500

    mupdf: revision
    * mupdf: add linux-only flag
    * mupdf: build shared library
    * mupdf: bump revision
    * mupdf: comply with style
    
    Closes #101359.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -4,6 +4,7 @@ class Mupdf < Formula
   url "https://mupdf.com/downloads/archive/mupdf-1.19.1-source.tar.xz"
   sha256 "b5eac663fe74f33c430eda342f655cf41fa73d71610f0884768a856a82e3803e"
   license "AGPL-3.0-or-later"
+  revision 1
   head "https://git.ghostscript.com/mupdf.git", branch: "master"
 
   livecheck do
@@ -32,6 +33,7 @@ class Mupdf < Formula
     glut_libs = `pkg-config --libs glut gl`.chomp
     system "make", "install",
            "build=release",
+           "shared=yes",
            "verbose=yes",
            "CC=#{ENV.cc}",
            "SYS_GLUT_CFLAGS=#{glut_cflags}",
@@ -41,6 +43,8 @@ class Mupdf < Formula
     # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
     bin.install_symlink bin/"mutool" => "mudraw"
     man1.install_symlink man1/"mutool.1" => "mudraw.1"
+
+    lib.install_symlink lib/shared_library("libmupdf") => shared_library("libmupdf-third")
   end
 
   test do
