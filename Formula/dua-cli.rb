96b4ff23bae73371a087319af50a3287f7c56551
Alexander Bayandin <a.bayandin@gmail.com>
2022-05-13 14:50:41 +0000

    dua-cli: update 2.17.5 bottle.

--- Formula/dua-cli.rb
+++ Formula/dua-cli.rb
@@ -6,12 +6,12 @@ class DuaCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fa6a9d66ce4a8e4c4291cc2fe3722d50c5e228fc7e5db2d5081e8e07680e306f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59a046772504e3a3b1f22b0a851ed3bd2c9cfd872b5484df3d56eae0456c9d1f"
-    sha256 cellar: :any_skip_relocation, monterey:       "71367cbd189d1402027b59ba1f8bf10702a7828b784c7b264607d01e27f798e8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "324890547ab10bf2037e679d5c5af783ad2bdbb7ef7536931e479f07ab2f9b8c"
-    sha256 cellar: :any_skip_relocation, catalina:       "7803368131eb3479e627fa421b048928bce5630ae6455fe1ea6888f32e004255"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "776d65307d1233c1f90f066046e031e562ab754de8f48fbb4cc7fea749ef6a74"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f1b6edc596cd0a2e594aca9cf83bc02116bb2f2c3ff3b061cc14a65e27b51e08"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af7243328afc93f8152ffd0ad59eeb7a9be5a20a72d7f64784a7f6e8369941af"
+    sha256 cellar: :any_skip_relocation, monterey:       "2be85ef50c0d59a700d9c75691b832d388603486fec442025c56dba6c0a82d70"
+    sha256 cellar: :any_skip_relocation, big_sur:        "918a0a4b462b32b109dc1d346c69c13e7d6d86aab67ac576b36063afb445d606"
+    sha256 cellar: :any_skip_relocation, catalina:       "93742651935ebf0fd8fd11b9b7df6c8dd73c228ce02cd03d376c34303fedba59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a3aff18890ef773bd817bb05277c237e175fd626f1dc6169383f3c97d54e2034"
   end
 
   depends_on "rust" => :build
