6750e6aae9acd62dfd3953f09cc3421458cb150a
Eric Nielsen <eric@amalgamar.com.br>
2022-05-13 11:32:38 -0500

    oh-my-posh 7.82.1
    Closes #101418.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v7.81.1.tar.gz"
-  sha256 "7aceb5ed51e0fcdbaeb9fc59050b35d304db8a211cf52b02d50309249dabe81d"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v7.82.1.tar.gz"
+  sha256 "48d66db46b6afcac02458f60494dc7b131607e8023f0da4e05b2d9b39f5afd59"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
