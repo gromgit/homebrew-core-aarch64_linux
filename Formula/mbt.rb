7a6abf930fd9be12b2ca55b8766be0b06d0e088f
Bo Anderson <mail@boanderson.me>
2022-05-13 03:43:42 +0000

    mbt: update 1.2.11 bottle.

--- Formula/mbt.rb
+++ Formula/mbt.rb
@@ -7,12 +7,12 @@ class Mbt < Formula
   head "https://github.com/SAP/cloud-mta-build-tool.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c6d60acc5e116ac8a3cc267147179cacb0feeee5f9a36b3ec3706cf63c3c9f3c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "502581d4017ab9b7390499a64548e734aac9bde91cd64576eeb2858babec0b8d"
-    sha256 cellar: :any_skip_relocation, monterey:       "0739815ad15027d18ab113d2af0f753d281042b862040cd6b4a39bd92679618c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dd01cbe62a21847d06c277dfafb5b1b3ad8cd6b7174fe351eb136d80a273297d"
-    sha256 cellar: :any_skip_relocation, catalina:       "e796e45c3a1ce2795ed7a6d16f2b8584fac1f87042b1efb83fb5b063ed3f1eb6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1153edbb465f46fdbdf43ccd2d8c7b0aa906ae0972276afc105be9bf95c6933"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f994f70a2c287dcf97ba828272e4c9e635391ab17520da0a477d493b4d7a0ae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5c931394d506d969ef608f7c034d35d7b4fd4ccc4dd2c64f0e715f03cc9d0dd1"
+    sha256 cellar: :any_skip_relocation, monterey:       "c03f19206bd48baaf55cf90892ab76b338386763be4aec86038fdf4409a23477"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e42e96f7bb7062c1caf4f25b019e8120951580470f0dd4cf6ec9e007c77a5ee9"
+    sha256 cellar: :any_skip_relocation, catalina:       "27f6c41449d35590475614cc3c2db9a9364d6520a26e44a6a647a2a7c1b49662"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85b5154a0acd2ee1b1a6aafedbf82a213a883ccdb5f3edbff1fdb96697591952"
   end
 
   depends_on "go" => :build
