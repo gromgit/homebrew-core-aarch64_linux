4fa7d6cdff1038da438f837ee31e024668a7a0a6
Alexander Bayandin <a.bayandin@gmail.com>
2022-05-13 14:50:44 +0000

    epinio: update 0.8.0 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -6,12 +6,12 @@ class Epinio < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d06c1f2d6ba97932c384523524156c3ef3a85233136929cceeee6af5f8214a8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b4e39f6c06d93d6dc04bfeb9b9edb32e22e8b2b35400366ca4eac66080cf77bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb5ec09403d1bae1f7c735ae1421a971ea36f66adad487f658168c0d36cbef9c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ab564a1321193caf8ae2c811634076db7b20c2d75be4bea442b6e0569520fb10"
-    sha256 cellar: :any_skip_relocation, catalina:       "4884df241229b354bee98edb7f60169c9f253a14c077641cd6ae27579d9cb27b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af8db289bd0578f756eff57d1a120d260e2e62d4c7d087f3ed7debc7f236bc80"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f9083daeb922bb4916f229dc688175f08ae882eb4cd99680de75a29df57fd288"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7004a3c3518099f33202eb1031a022f9c331209f5f5f4b49276110f02a4c7d1f"
+    sha256 cellar: :any_skip_relocation, monterey:       "5e7e8e0e8bd9286b4c9b83b08606303554f32b5391074c8a47c3a1aac1dabfbc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce1252568594868ec0484fad9d13de6121a15701b8e2d87ac421a6b75b813bf2"
+    sha256 cellar: :any_skip_relocation, catalina:       "d5595f9cfbb9e40be0e3c70cb35d6121151eb305efe10a522473775b25577c3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d72503c041ff65c575ab66644f347c3f003d318ac78eb4a2f2151e73871a90b9"
   end
 
   depends_on "go" => :build
