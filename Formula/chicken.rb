7b9358bb3a4464e5e0dcddd6655fadab808df554
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-05-14 20:20:17 +0800

    chicken: set `RPATH`
    Fixes Homebrew/discussions#3268.
    
    Closes #101481.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chicken.rb
+++ Formula/chicken.rb
@@ -4,6 +4,7 @@ class Chicken < Formula
   url "https://code.call-cc.org/releases/5.3.0/chicken-5.3.0.tar.gz"
   sha256 "c3ad99d8f9e17ed810912ef981ac3b0c2e2f46fb0ecc033b5c3b6dca1bdb0d76"
   license "BSD-3-Clause"
+  revision 1
   head "https://code.call-cc.org/git/chicken-core.git", branch: "master"
 
   livecheck do
@@ -27,7 +28,8 @@ class Chicken < Formula
       PREFIX=#{prefix}
       C_COMPILER=#{ENV.cc}
       LIBRARIAN=ar
-      ARCH=x86-64
+      ARCH=#{Hardware::CPU.arch.to_s.tr("_", "-")}
+      LINKER_OPTIONS=-Wl,-rpath,#{rpath},-rpath,#{HOMEBREW_PREFIX}/lib
     ]
 
     if OS.mac?
@@ -43,5 +45,6 @@ class Chicken < Formula
 
   test do
     assert_equal "25", shell_output("#{bin}/csi -e '(print (* 5 5))'").strip
+    system bin/"csi", "-ne", "(import (chicken tcp))"
   end
 end
