1557fbe99d50d80e7c1e770172877e73f87927f6
mikael <13247629622@163.com>
2022-05-14 18:51:36 +0800

    goctl 1.3.5 (new formula)
    * goctl 1.3.5
    * goctl 1.3.5
    
    Closes #101476.
    
    Co-authored-by: Mikael <13247629622@163.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/goctl.rb
@@ -0,0 +1,21 @@
+class Goctl < Formula
+  desc "Generates server-side and client-side code for web and RPC services"
+  homepage "https://go-zero.dev"
+  url "https://github.com/zeromicro/go-zero/archive/refs/tags/tools/goctl/v1.3.5.tar.gz"
+  sha256 "cbb131ba03fd26e4871ce9543cf8aa64fa1547d6db5c10390f512e72d095a722"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    chdir "tools/goctl" do
+      system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"goctl"), "goctl.go"
+    end
+  end
+
+  test do
+    assert_match "goctl version #{version}", shell_output("#{bin}/goctl --version")
+    system bin/"goctl", "template", "init", "--home=#{testpath}/goctl-tpl-#{version}"
+    assert_predicate testpath/"goctl-tpl-#{version}", :exist?, "goctl install fail"
+  end
+end
