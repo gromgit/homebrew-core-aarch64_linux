d70c9048f8ac583d182f2ff173782b1d008bac68
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-05-14 21:38:48 +0000

    klee: update 2.2_3 bottle.

--- Formula/klee.rb
+++ Formula/klee.rb
@@ -8,9 +8,10 @@ class Klee < Formula
   head "https://github.com/klee/klee.git", branch: "master"
 
   bottle do
-    sha256 big_sur:  "3534cffd757f8fa4c3be4f05c7534dbe705e54657512bb4a1b9d8b13cbe6b337"
-    sha256 catalina: "508ab6444c02c26e061edf84519c18d888c4d9c1098c89215b5b788224838d37"
-    sha256 mojave:   "b29dd739b4644aafc918f40a1c5abce7c00657c09a8959401c9ac8c77397a560"
+    sha256 big_sur:      "3534cffd757f8fa4c3be4f05c7534dbe705e54657512bb4a1b9d8b13cbe6b337"
+    sha256 catalina:     "508ab6444c02c26e061edf84519c18d888c4d9c1098c89215b5b788224838d37"
+    sha256 mojave:       "b29dd739b4644aafc918f40a1c5abce7c00657c09a8959401c9ac8c77397a560"
+    sha256 x86_64_linux: "687e221b5f04745e4f60aad142974d38d06b4c48e717c35edc14b5b2de7be832"
   end
 
   depends_on "cmake" => :build
