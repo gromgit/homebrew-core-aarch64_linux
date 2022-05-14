4a9babde2dba11e645b9626fe3386e908e3bdd25
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-05-14 21:20:20 +0000

    spiped: update 1.6.2 bottle.

--- Formula/spiped.rb
+++ Formula/spiped.rb
@@ -10,11 +10,12 @@ class Spiped < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "63451c713c91962ed596254ff1924de72d6adc4bb7d950082d420cafa70596a0"
-    sha256 cellar: :any, arm64_big_sur:  "3df7565403ad24361e05d3661242f8724bde49bdee1124bcc07aa234dbdcecf2"
-    sha256 cellar: :any, monterey:       "b343cf4da6e41a3c30d76ea2e25bf18c1e63d0479e7ceb13e8764232a035b727"
-    sha256 cellar: :any, big_sur:        "4e78ef161701b82728993c1107fbb155bb9a17cea82df586c9a1df520ec8d656"
-    sha256 cellar: :any, catalina:       "f3edfef25280658dbeaee09e50d2b284a5d9768ffb2632502b3bfc050590e073"
+    sha256 cellar: :any,                 arm64_monterey: "63451c713c91962ed596254ff1924de72d6adc4bb7d950082d420cafa70596a0"
+    sha256 cellar: :any,                 arm64_big_sur:  "3df7565403ad24361e05d3661242f8724bde49bdee1124bcc07aa234dbdcecf2"
+    sha256 cellar: :any,                 monterey:       "b343cf4da6e41a3c30d76ea2e25bf18c1e63d0479e7ceb13e8764232a035b727"
+    sha256 cellar: :any,                 big_sur:        "4e78ef161701b82728993c1107fbb155bb9a17cea82df586c9a1df520ec8d656"
+    sha256 cellar: :any,                 catalina:       "f3edfef25280658dbeaee09e50d2b284a5d9768ffb2632502b3bfc050590e073"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5688ae1fdf0012b91c336d8a802358a3a248d83ff97fa882ec5319454e1f8823"
   end
 
   depends_on "openssl@1.1"
