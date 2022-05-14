c23eef8f98f4f64bad4380f736272376eaf9f3db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-14 05:38:17 +0000

    eksctl 0.97.0
    Closes #101459.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,8 +2,8 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.96.0",
-      revision: "59b50db419362c7dc57f0f94a3fd4c15ae2210da"
+      tag:      "0.97.0",
+      revision: "0ea20abd3da771aa55a9ce15e94859c81c052ae8"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
