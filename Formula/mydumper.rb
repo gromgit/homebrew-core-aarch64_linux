4954ef2be030441d54fdbfe723a36057215954bd
Rui Chen <rui@chenrui.dev>
2022-05-15 20:44:22 +0000

    mydumper: update 0.12.3-2 bottle.

--- Formula/mydumper.rb
+++ Formula/mydumper.rb
@@ -6,12 +6,12 @@ class Mydumper < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f8dcdf00e92dd854349c1e0698934c8b337c38ea653288dedabdf1ad61c916b0"
-    sha256 cellar: :any,                 arm64_big_sur:  "66b108fa2909b02a98fffa1197270f6cf5a3c5b46c8c648940a42a2985abaaad"
-    sha256 cellar: :any,                 monterey:       "62168e340cdfc0d1fdc63831218b52ad3a2cab6c723e21277a837451139574ed"
-    sha256 cellar: :any,                 big_sur:        "4c5707d62cf6152d8106e1a17e36b8dc62e67663045bd29448b8f50cf8f92ee7"
-    sha256 cellar: :any,                 catalina:       "ca473525bb5b1d3ba3fc2917c38235afa930536fcca4b83b4d57c7d14d1529ae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdce77ac9629c4dc3f624692de449bf9c144a1e9f0f789c20abe13f2ea7e6c57"
+    sha256 cellar: :any,                 arm64_monterey: "34456dc63582bc6b1e8ef67449b655ac2d3123c581bd122fed5877cca9259c3e"
+    sha256 cellar: :any,                 arm64_big_sur:  "1103f55c7d4074bfc8641b83a00b86ae0978ff1851bba2769aaef6173df750ea"
+    sha256 cellar: :any,                 monterey:       "f7455b5cf3f953bed7eb0e1d994da5694a51ed80ed082f54e5e8c46e843d4246"
+    sha256 cellar: :any,                 big_sur:        "6086bae7941f0b31b8315846c32f4d429246a2f748bcfe6eae770fc87ef265c0"
+    sha256 cellar: :any,                 catalina:       "3be74acf157ae90396e74b63b8e3e8bcdf40940c2e52e9b279fbb1b7e5b111a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83c91cdf8b26d42788dfc150e03ca1090dd0506e80bb7a6b1c92bda98b68c2ed"
   end
 
   depends_on "cmake" => :build
