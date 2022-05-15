08131f50a5dd0c85cf1b8a9d89cf12eba5a83e1e
Rui Chen <rui@chenrui.dev>
2022-05-14 21:28:20 -0400

    easy-tag: revision bump (libid3tag 0.16.2)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/easy-tag.rb
+++ Formula/easy-tag.rb
@@ -4,7 +4,7 @@ class EasyTag < Formula
   url "https://download.gnome.org/sources/easytag/2.4/easytag-2.4.3.tar.xz"
   sha256 "fc51ee92a705e3c5979dff1655f7496effb68b98f1ada0547e8cbbc033b67dd5"
   license "GPL-2.0-or-later"
-  revision 6
+  revision 7
 
   bottle do
     sha256 arm64_monterey: "39d4f47a2a8310fbef48070f247e2ec45cc0761bf5e1baa8ebda5446e1aee0b4"
