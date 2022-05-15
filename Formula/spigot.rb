e4c73160b18ab24bdce180e123bcccb8a91d89ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-15 17:08:35 +0000

    spigot: update 20220509 bottle.

--- Formula/spigot.rb
+++ Formula/spigot.rb
@@ -12,13 +12,12 @@ class Spigot < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f29fed5c8bcde09370f28cb44769301b51a5e9070b56aafdce2b49149d6fb73b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea601301d75ed829ff918c22884199fef4a35afcffbdc03da464d7051b55d3e1"
-    sha256 cellar: :any_skip_relocation, monterey:       "3d46df4506b0880add1c37d6d7ad9af0ff4a33062504b83a54e48a2f8c4d5001"
-    sha256 cellar: :any_skip_relocation, big_sur:        "daf9a73394d08cdfc2b5746b367112ea1790f924169d36e2ec288b201433b148"
-    sha256 cellar: :any_skip_relocation, catalina:       "7a78a2d6416a800148fbd44f269865c3baacf150164f757459f9dbf622b41fa9"
-    sha256 cellar: :any_skip_relocation, mojave:         "f7f31fcf972afaecdb231e4b7e413a98a7ec8f05f7d3e4717cc9128c7cae0fab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0e15ff42ceacf124df8655ee07bebb44dcb0ee0a5f8785ab4da5c8b27ea1467"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cffb7805cd6598cc367c1f5397eb05f7747975fb7324b4a0d844c088658a132"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "134af87f8accb08eeb9525873272c6b2d27a83a51b96c47179549058b1404264"
+    sha256 cellar: :any_skip_relocation, monterey:       "415f626dd8ae573be81470a44d46b6b334a4d92918e7855023ec52dc7992d431"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ffb9d89bc879172ee5634c78abf185288ca6e2d62995170515fe4f83fd09d3ef"
+    sha256 cellar: :any_skip_relocation, catalina:       "21e32f1ca9031a72a0e0aa158b22c3975b2a67a0c31c5ea2b918512677d3f3d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2a2856e215663689cdcedafe21e84efcb1fb0447a131de87c6b0bd288228ca89"
   end
 
   depends_on "cmake" => :build
