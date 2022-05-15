eefcf5566171216e59c89a8c9cf88d38b97f4c74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-15 17:08:44 +0000

    devspace: update 5.18.5 bottle.

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -13,12 +13,12 @@ class Devspace < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4f41a80a6ddf4518be8daaaa833e389d5ca11c56f138849aaa1b462a283ccc0f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "164eca4c8beca77c064da2de9e1a37c96b997daa05ad78a0ad28ffe63dc05424"
-    sha256 cellar: :any_skip_relocation, monterey:       "b05f88a7b686af8aa26658eb2738b270d76b9bde9522427b4a4d80841bb66039"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0c0f7d917ba87d622c5068ace19ade6a96761430400df2acd7cdd3b9bffaf8fe"
-    sha256 cellar: :any_skip_relocation, catalina:       "b094c13f05db4aea653f6e480156b687ad227cefb9c66c5b5d21aac5e9c16638"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd3faf453f3ef1742bf8af349e74542b679c583a1001e66e74e65d5e3ab4b724"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc90548b4efa2c9ce4f1cca9a17a06545be1646cc05c0c2c2a61a5ae9b4506d3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "feb64268e9148bd71ac8c867ae0c82443cd2c2a156f6d7cbfe33d3d93c42a33b"
+    sha256 cellar: :any_skip_relocation, monterey:       "230692d7ab0d836f6359b0c7e66f5b935d4c3582909c8ab7ed95a5b23db02eb1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "60bb0bc610c206f50943fb00e322de386dcacabc4439cd83e39f00d120df2306"
+    sha256 cellar: :any_skip_relocation, catalina:       "516ac8bfed0d2627bdf239b1b7ae7f619f69f8b4fd1dc9f249005d07da0f6544"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e98e00b8e7de436cccc3417dcd9e74c7adf096352260f265a0ad06de61d16923"
   end
 
   depends_on "go" => :build
