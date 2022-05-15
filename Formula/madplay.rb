3d78d50bf526a48c6eb5395a21784198020f8a2d
Rui Chen <rui@chenrui.dev>
2022-05-14 21:28:20 -0400

    madplay: revision bump (libid3tag 0.16.2)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/madplay.rb
+++ Formula/madplay.rb
@@ -3,7 +3,7 @@ class Madplay < Formula
   homepage "https://www.underbit.com/products/mad/"
   url "https://downloads.sourceforge.net/project/mad/madplay/0.15.2b/madplay-0.15.2b.tar.gz"
   sha256 "5a79c7516ff7560dffc6a14399a389432bc619c905b13d3b73da22fa65acede0"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
