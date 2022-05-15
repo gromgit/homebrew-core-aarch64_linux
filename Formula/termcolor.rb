716619e8e304b1f3188a26137cfb9d4bec54cae5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-15 20:39:30 +0000

    termcolor: update 2.1.0 bottle.

--- Formula/termcolor.rb
+++ Formula/termcolor.rb
@@ -6,12 +6,7 @@ class Termcolor < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6600a6345ddca25f57a8ac394b62edac659399eb1c7eab287c201108ac390461"
-    sha256 cellar: :any_skip_relocation, big_sur:       "86b0707099a56545dc78d87c81407b998c900c22d14b23d53ffa35eafa2e01f2"
-    sha256 cellar: :any_skip_relocation, catalina:      "41012d8fe7ea77d22b30dc9baeac6aa0472b66c4f40e490b214ae34d20076cb1"
-    sha256 cellar: :any_skip_relocation, mojave:        "49ed93a14e7bd15d692c8222d161dc732b3836b829f1dbc4b2e7326d66620674"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "52499ef8a399cb540793ce3f0ed8e3c857ea60681942fed858170642017fb4ba"
-    sha256 cellar: :any_skip_relocation, all:           "51b0b25be9b4a3cc951c6252dfc94c2dd06787eb1cea9bb100257a5359d2c770"
+    sha256 cellar: :any_skip_relocation, all: "559fc780d17bbeaf1e09e1178775a0e88322d1f000a3bbe5e4937ba189468fbd"
   end
 
   depends_on "cmake" => :build
