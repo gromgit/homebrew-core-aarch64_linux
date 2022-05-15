3a69b9537492f6ad30e1c181a7cd7d9ecaf08608
Rui Chen <rui@chenrui.dev>
2022-05-15 20:48:42 +0000

    gnome-latex: update 3.40.0 bottle.

--- Formula/gnome-latex.rb
+++ Formula/gnome-latex.rb
@@ -7,12 +7,11 @@ class GnomeLatex < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "6c25d1dc2043da0fd0618c57316b19e59fa7627c61ea01051a38384c1e6683ac"
-    sha256 arm64_big_sur:  "118ac80bf869460f820e12a63bf8808b7ad9158eeb3ee594e494e344d54cc97c"
-    sha256 monterey:       "ec4d6862103ee03a5198148a0ea22acb97c1da93ef91af2b793bddb339f4e9c5"
-    sha256 big_sur:        "91916490eae6b8b5e9c8717ea9a37a2e8e383c6504a3d59c7d4f209d2f2e5db0"
-    sha256 catalina:       "34723bd50c23bc34d54750606f99247544a222f8d32ee1017422a618e7d8255c"
-    sha256 mojave:         "282a45a8580c354c10f112895d0448cc97e206da12460f75b6dbcc8906401314"
+    sha256 arm64_monterey: "603b189c7d6d338281cf59115a702070dc3ebfbf6b30d6ab243dbb5c5fdaccc7"
+    sha256 arm64_big_sur:  "16a97325509f677e4bd1411c7a4fb46d7e5d88b202194a8f1c8dedd30d7ffb56"
+    sha256 monterey:       "afd5544db8861e12439e36e3e340446e258c69283d37af14179db69ee6c7b220"
+    sha256 big_sur:        "724e5f45e9f393d3a190eee881a444860e4598ee73e2c7657a341b669f903904"
+    sha256 catalina:       "16543e61eeee2a269f9cb5b62c7bd5ff9f67f29714ad77e3cef8828883c8de23"
   end
 
   depends_on "appstream-glib" => :build
