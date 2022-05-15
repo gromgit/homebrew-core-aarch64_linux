4d4c8e00d3c525a8c8479aaa2e3c0de51d3b27c9
Rui Chen <rui@chenrui.dev>
2022-05-14 22:49:12 -0400

    libuvc: update repo location
    Closes #101534.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libuvc.rb
+++ Formula/libuvc.rb
@@ -1,10 +1,10 @@
 class Libuvc < Formula
   desc "Cross-platform library for USB video devices"
-  homepage "https://github.com/ktossell/libuvc"
-  url "https://github.com/ktossell/libuvc/archive/v0.0.6.tar.gz"
+  homepage "https://github.com/libuvc/libuvc"
+  url "https://github.com/libuvc/libuvc/archive/v0.0.6.tar.gz"
   sha256 "42175a53c1c704365fdc782b44233925e40c9344fbb7f942181c1090f06e2873"
   license "BSD-3-Clause"
-  head "https://github.com/ktossell/libuvc.git", branch: "master"
+  head "https://github.com/libuvc/libuvc.git", branch: "master"
 
   bottle do
     rebuild 1
