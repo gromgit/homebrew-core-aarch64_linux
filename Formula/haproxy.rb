a4a7fa8c8dff5bdc78b597902df0a8352e026e5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-15 20:39:35 +0000

    haproxy: update 2.5.7 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -11,12 +11,12 @@ class Haproxy < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0c326a830aacd5f57e8f8ef9fbd1df2a4aa6b3ba5630682ac9d15707918b7ff0"
-    sha256 cellar: :any,                 arm64_big_sur:  "9a047f0d68096c98da8dd39b1195f3949fde22ae77d3bb31a0adf8294a5c6087"
-    sha256 cellar: :any,                 monterey:       "ad4fd02122073ac6f9d7184e89a87fddf5f5f549e4599fe22c4344d6232afa24"
-    sha256 cellar: :any,                 big_sur:        "656aecc60891908f93130f6ee39db64ccfa26acfb03ac64f6cb74251d2534907"
-    sha256 cellar: :any,                 catalina:       "74bea03b45e17d914e655f1b2b4b8a52722af56e0c59f45be52be46bf2ece1e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb7477dd16e034f7e3630b5561a942d57b39b94e4d25556731104f8b140fbc36"
+    sha256 cellar: :any,                 arm64_monterey: "4b9fc3b432d19bddb046323c909c7dc30e04c607a847133eccdc066531eb4aa3"
+    sha256 cellar: :any,                 arm64_big_sur:  "dc193cac5615c9c040bc4b407a95a9e3747a6a2d0a462bbec12bf5a649efa05c"
+    sha256 cellar: :any,                 monterey:       "ba438308c678b8b56e411fa632f242feabd5b821eb1a8a0dcb282669b7b68d80"
+    sha256 cellar: :any,                 big_sur:        "e675d35052f5553ad3775acc8ab65a031ba858e3946a6942dcac0f5c55a80ec8"
+    sha256 cellar: :any,                 catalina:       "17609b3b7a317d15561f457ecece598080ab94fc1dfd1e4282d8548d9b8d7b93"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94d6e9fdd1c6a3f9d55af123e36f7f3b6092aff089dabb4bc5a214bab27fbb88"
   end
 
   depends_on "openssl@1.1"
