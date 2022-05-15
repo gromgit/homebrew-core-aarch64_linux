8611227a38c6b8700dd4a95a9dd90ee9a7f8c79c
Rui Chen <rui@chenrui.dev>
2022-05-14 21:55:43 -0400

    quran 1.0.1
    Closes #101526.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/quran.rb
+++ Formula/quran.rb
@@ -2,8 +2,8 @@ class Quran < Formula
   desc "Print Qur'an chapters and verses right in the terminal"
   homepage "https://git.hanabi.in/quran-go"
   url "https://git.hanabi.in/repos/quran-go.git",
-      tag:      "v1.0.0",
-      revision: "2558e37fc5be4904a963cea119bb6c836217c27b"
+      tag:      "v1.0.1",
+      revision: "c0e271a69a2e817bf75c8ad79a1fc93aa1b868c9"
   license "AGPL-3.0-only"
 
   bottle do
