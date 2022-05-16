1db28ea50226fb28a1f15bbdb32e8775337d2710
Rui Chen <rui@chenrui.dev>
2022-05-16 02:29:25 +0000

    mapserver: update 7.6.4_5 bottle.

--- Formula/mapserver.rb
+++ Formula/mapserver.rb
@@ -12,12 +12,12 @@ class Mapserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "eb063e619b10833ecf47eb794e3f2d9e96f9105f9030270d79f03e7bfb302aff"
-    sha256 cellar: :any,                 arm64_big_sur:  "4ff7d584fc0c64391f45e8a264e01fb77c742ca77b737c4af8f7a673eaf37d06"
-    sha256 cellar: :any,                 monterey:       "c254261912cb57007202ad57480c44212ae8839583be745cda024b2966b644ce"
-    sha256 cellar: :any,                 big_sur:        "ed04e8fe8f1da5d91258b8449b60e95f7be592b6d817ad662cb01b629daa5e88"
-    sha256 cellar: :any,                 catalina:       "017de885523933e74cffc69391374be105af07e64099681aaef4ea4986922008"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7aa822d4049082c9aecb7100b75d985b62929e0e5a7d4ded45460ca3612dd8c3"
+    sha256 cellar: :any,                 arm64_monterey: "477c3b98cef266210bfa9595d324ed0da0cef4be754888ec5d1da11cc5d3d77a"
+    sha256 cellar: :any,                 arm64_big_sur:  "15d25c55cb6a0d8db29a293c523fa7c56b2b274d24413682993b1915fe508e06"
+    sha256 cellar: :any,                 monterey:       "807575f997e470030d0f05eee9fded42afc59fde2edbea28e30a41405cbfe713"
+    sha256 cellar: :any,                 big_sur:        "dff219470b797278023f5d68231db74d08b1a3c04d0dab0c77d47a51b7bab382"
+    sha256 cellar: :any,                 catalina:       "e08713491a56f62f3b5ec16103c55b41b860a84fc13a87f9af70539e7cd58cda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6211abb6f2950be9ac4e5cae4910e65612773968bd0abb3a042eac9d2827de4"
   end
 
   depends_on "cmake" => :build
