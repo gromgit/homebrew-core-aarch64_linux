2eb6ad6c1e46215d6f7cefc54012b26d00100ca1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-16 09:05:13 +0000

    quill: update 1.7.3 bottle.

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -7,12 +7,12 @@ class Quill < Formula
   head "https://github.com/odygrd/quill.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e4d1156c5e15f71b4431e3bfce7813de6ecac7f65fe564ff444a8f34fff89192"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cd3a53ddde24590eb81882f8f297e74fd8555de87f08d6a9c7e2ace4df41739f"
-    sha256 cellar: :any_skip_relocation, monterey:       "0545c76cfaf31b9183fbe1a307a022bba058be60fd719af2e302be5d412875e9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f3a50bf8db2b560acd26ab61519ab0fd9664520cc4f88955ebe72f3e4b215f35"
-    sha256 cellar: :any_skip_relocation, catalina:       "2526b1dd073fbc7c872787128585765bdc61c420e687ae675aec2cdfd6f989a1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "014d8a5d06e471b29604c26c0b167d7328f5cd95fbb6cdc77d9c172f1a16c6da"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "de206cf0780f75b68232542308d4048b0a3d8401d1f53830cb59870a858c1801"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7257a7071e685eed3ade9269180791cfb797c13112cf4846e4a495423901f919"
+    sha256 cellar: :any_skip_relocation, monterey:       "22cc4b956f8655f63898b32c5866b973aa44883243b3383447f8b459922d8982"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1307e4c852a0709a5a659f88820ac4b1c498df9ae7a0cf678f5f94734538491b"
+    sha256 cellar: :any_skip_relocation, catalina:       "7f744ab6e5233bb07966cd656d49e3c564aed8dbf9efe651c6ffe92f4ab1959b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bf9ab49b4b2d60db611dcdc6c9e59beac5a8a244c298b1a2b40f5a4fc02ea32"
   end
 
   depends_on "cmake" => :build
