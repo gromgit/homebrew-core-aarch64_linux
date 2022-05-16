695e8ee541bf2e81080fc374f1ac4c9cbceca47a
Rui Chen <rui@chenrui.dev>
2022-05-15 22:54:29 -0400

    openliberty-webprofile8 22.0.0.5
    Closes #101615.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openliberty-webprofile8.rb
+++ Formula/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.4/openliberty-webProfile8-22.0.0.4.zip"
-  sha256 "c6a7c1d85f53ce7e03e60a3b78fcb2460e21351092e4b30d86218ad9f372202d"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.5/openliberty-webProfile8-22.0.0.5.zip"
+  sha256 "8f6716343162d8657917b4d188e0affd3c0c169c6c7a2cc771b14bad7353f266"
   license "EPL-1.0"
 
   livecheck do
