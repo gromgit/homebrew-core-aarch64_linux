71ff499b5ed8537eba1ef4be80d935852e57817f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-16 05:39:00 +0000

    whistle 2.9.16
    update build
    
    Closes #101645.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -3,14 +3,17 @@ require "language/node"
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.9.13.tgz"
-  sha256 "fa82d0eee96e779f1b1b48bd308617925fa19c065860edeb94ae011e8fd6b7b9"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.9.16.tgz"
+  sha256 "a9ef49bc2dc2e749e9c7565cd205afe70a6083dafa12164456ac805e0f78f117"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "11f4fa842ae5e2711e1b5ea28a6d87fcfbe559baa5dc6eb297db239f66cb64d8"
   end
 
+  # `bin/proxy/mac/Whistle` was only built for `x86_64`
+  # upstream issue tracker, https://github.com/avwo/whistle/issues/734
+  depends_on arch: :x86_64
   depends_on "node"
 
   def install
