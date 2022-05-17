f6565fd5958c4bf127d790d194c3745a0bb11640
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-17 06:05:44 +0000

    libxc: update 5.2.3 bottle.

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -6,12 +6,12 @@ class Libxc < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e5023e89f8acff465eba113360911e6b666c4487f9547271da78be13ccfb9c9d"
-    sha256 cellar: :any,                 arm64_big_sur:  "4e064d20223090cc16b6a9056bca6cbd98683408c42987b7a0050016bac46b41"
-    sha256 cellar: :any,                 monterey:       "1751dc87e5235aa9854442dea65fb1355f2f847157fdae990e315fb96b3d34b0"
-    sha256 cellar: :any,                 big_sur:        "4fb029189385ada09e3c4a167257bb1e39ff02fc3fa00c4d0e03c6b45dd5cafd"
-    sha256 cellar: :any,                 catalina:       "091fc935e2496b8132bfefc25372b496af7a1c9a93952ff6ff36dbc130445808"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41cf6427cdce0071ce177114d46b588fd24a75512b005b2da0b14d2c1cc1c503"
+    sha256 cellar: :any,                 arm64_monterey: "ab53f54b78b5af31ed1ec2e0f366153e72a6d52b556afda8b4f4e83590ac986b"
+    sha256 cellar: :any,                 arm64_big_sur:  "e6e36a5e5b432010f6fe490cda08777e9f73cc724ba695e20fe0eebd7f0e4c11"
+    sha256 cellar: :any,                 monterey:       "89b024d837b6bc171be052458332dc08ca642998b3ad853ca27992178855b352"
+    sha256 cellar: :any,                 big_sur:        "4d4e5db654f348da273358f4a7268793945b12a100f6ac7aa69e4b6b2f129d6b"
+    sha256 cellar: :any,                 catalina:       "c74354016c997bc59947f5b6882fd8b0df5e65237bcd7879a2a44b6aaf4c0e28"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d8b90733fdaed749ef20fce3360c1eff0c0e247c8716eb4754aa7ca8869ca86"
   end
 
   depends_on "autoconf" => :build
