8e411fa75a66ee8fe3a0c9440e51a0c81c5648b0
Rui Chen <rui@chenrui.dev>
2022-05-17 16:23:46 +0000

    release-it: add 15.0.0 bottle.

--- Formula/release-it.rb
+++ Formula/release-it.rb
@@ -7,6 +7,15 @@ class ReleaseIt < Formula
   sha256 "52a22039887a5a131db2a3b8c524bd57f9bda4a091567711f5c5ee9cee8f8751"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e05630e5ab2ad2d2397a793d2b5a1b3bd1a142f56752e206a2bc757ecf1ad6b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e05630e5ab2ad2d2397a793d2b5a1b3bd1a142f56752e206a2bc757ecf1ad6b"
+    sha256 cellar: :any_skip_relocation, monterey:       "b2c693ac53ca3f66b721e78da2e60173a9467c3b3c81cb00e3eb9c1522ff2249"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b2c693ac53ca3f66b721e78da2e60173a9467c3b3c81cb00e3eb9c1522ff2249"
+    sha256 cellar: :any_skip_relocation, catalina:       "b2c693ac53ca3f66b721e78da2e60173a9467c3b3c81cb00e3eb9c1522ff2249"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1e05630e5ab2ad2d2397a793d2b5a1b3bd1a142f56752e206a2bc757ecf1ad6b"
+  end
+
   depends_on "node"
 
   def install
