de456a7204e4de16313592d3eb9ec77985c32854
owine <owine@users.noreply.github.com>
2022-05-17 09:22:00 -0500

    cyrus-sasl: use libxcrypt on Linux

--- Formula/cyrus-sasl.rb
+++ Formula/cyrus-sasl.rb
@@ -4,6 +4,7 @@ class CyrusSasl < Formula
   url "https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-2.1.28/cyrus-sasl-2.1.28.tar.gz"
   sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
   license "BSD-3-Clause-Attribution"
+  revision 1
 
   bottle do
     sha256 arm64_monterey: "ab2db74328cfcf6e6e6d9871dbd9997abf9663f8406f7413eae3297f31df1af5"
@@ -18,6 +19,8 @@ class CyrusSasl < Formula
 
   depends_on "krb5"
   depends_on "openssl@1.1"
+
+  uses_from_macos "libxcrypt"
 
   def install
     system "./configure",
