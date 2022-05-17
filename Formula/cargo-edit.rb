b768521e7cfbda287c7c88fd1cf4b13e0464be51
Branch Vincent <branchevincent@gmail.com>
2022-05-17 19:50:27 -0400

    cargo-edit 0.9.1
    Closes #101793.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-edit.rb
+++ Formula/cargo-edit.rb
@@ -1,8 +1,8 @@
 class CargoEdit < Formula
   desc "Utility for managing cargo dependencies from the command-line"
   homepage "https://killercup.github.io/cargo-edit/"
-  url "https://github.com/killercup/cargo-edit/archive/v0.9.0.tar.gz"
-  sha256 "96c231b30339b1a05444c8faa036be84ecbb7f8eaead95c77de9a05f0c190b64"
+  url "https://github.com/killercup/cargo-edit/archive/v0.9.1.tar.gz"
+  sha256 "bae2a59dcf6110fe0c8bf8562e58d550b2b3b3a02e89b233af5a3be12d41cdf0"
   license "MIT"
 
   bottle do
