333b197057a50fd49be4c6ca87a783b7b0c7d176
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-05-17 10:48:12 +0000

    libxcb: update 1.15 bottle.

--- Formula/libxcb.rb
+++ Formula/libxcb.rb
@@ -6,12 +6,12 @@ class Libxcb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6c7154002b268eb74f36215a5d0b6496dacb3ff786bb40c29a2151359c219476"
-    sha256 cellar: :any,                 arm64_big_sur:  "e473ad5c0877fa07f64037bcf12df7396f5aed59a2ff89a1b5eaa1a6e2885fcd"
-    sha256 cellar: :any,                 monterey:       "aa439c61a2c0174f7d9f505fed55798a2dd4309221f3c51535c4fba5caa5162e"
-    sha256 cellar: :any,                 big_sur:        "c499695d4451922107256515154334e89fbafb3825b68f9c9f5d957f1398968e"
-    sha256 cellar: :any,                 catalina:       "118d9fa08edc88e58530f3920ddccba38e3eae0d08b280ff61e5eed9b5a87e9a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "105c626775006b99f03f67dd80a0ec5ef84e9163353cd33a8b39193fdf231936"
+    sha256 cellar: :any,                 arm64_monterey: "b9ed936a5ee43ec58cfa7db03a75ff2b336836c219c024c58f68fa3eecd91976"
+    sha256 cellar: :any,                 arm64_big_sur:  "dbb71439521a388431894b8ba9ea8b9ee628046ccc71cc94acdd3511eceb4df1"
+    sha256 cellar: :any,                 monterey:       "21ed8d16c03b188edebd5e0b20b1fca8e36763e159d75a63d5214873e78b1807"
+    sha256 cellar: :any,                 big_sur:        "adfd6a48ce689095e518b3e05a7d1d775808c84aad660d6763ba27c95d154052"
+    sha256 cellar: :any,                 catalina:       "78f6cd5fce9028f7909f2089c8307fd2dacc44d5edc8fe22ab9833e468dee48d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "993d37bb436fab0157ed5f3c031f9a18168053439e93edb4bdce9abe6e99373d"
   end
 
   depends_on "pkg-config" => :build
