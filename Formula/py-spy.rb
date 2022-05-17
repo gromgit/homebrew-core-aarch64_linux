ffb377ad38b967a6950a50882e40988d8c6f4897
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-17 05:34:51 +0000

    py-spy 0.3.12
    Closes #101744.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/py-spy.rb
+++ Formula/py-spy.rb
@@ -1,8 +1,8 @@
 class PySpy < Formula
   desc "Sampling profiler for Python programs"
   homepage "https://github.com/benfred/py-spy"
-  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.11.tar.gz"
-  sha256 "094cfb80e2c099763453fc39cfa9c46cfa423afa858268c6a7bc0d867763b014"
+  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.12.tar.gz"
+  sha256 "6a4b0537e0bf9cf40fc4557931955222a25db01c110309d0b642dd28211bffeb"
   license "MIT"
   head "https://github.com/benfred/py-spy.git", branch: "master"
 
