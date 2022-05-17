6c9bcdb4285a00078ac92a7850ccc04604251297
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-17 06:05:46 +0000

    trivy: update 0.28.0 bottle.

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -7,12 +7,12 @@ class Trivy < Formula
   head "https://github.com/aquasecurity/trivy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "88d64a390db9361cdf387f92dba12e720d824af4076c9e21fbdcef843933a673"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b673fe8c9abef9fe54bf6417e951628d87b4c4b6d970e89e847cefb316a201be"
-    sha256 cellar: :any_skip_relocation, monterey:       "518abc5347fd017a35891746270359845ef9d42430fbb98e43df51fd58204166"
-    sha256 cellar: :any_skip_relocation, big_sur:        "67e0bce13bd39bb03d27ff62b047c9700308147801a58c8209c822edb867c163"
-    sha256 cellar: :any_skip_relocation, catalina:       "d0d60fab894c7dc38546c71661b822318e984b6112aa85f46c89b4f7e5f7f97e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "17ca9decef0dad1a5cf113b91c3aec9452a6315dcbfb849e652bd713ae38dde6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0fee9e79df5b80ac210f7cfc459ecd0d64bdd5b437990028c221d95b7f787693"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e368ea9fb6ba920bda39d72f67134660c4178676fc7975f08c7a91b74cb52f8e"
+    sha256 cellar: :any_skip_relocation, monterey:       "64ea1dc6a9d548bf66e9800df0be56a24de4cbc0b113947f75328c430cb210fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a8741dd1744ab1a4be9f1a5cc7aebd1989efaccabdcf2e6036faba6da115bf59"
+    sha256 cellar: :any_skip_relocation, catalina:       "aa22947918b07ffbaac123c592ddea9766623827ff8a4f7d0b57b6c20c5c4b55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "09e0bf37d667574932ef74a56084d1ff2f00f07b7853e0fa5ba3cf2ef4520395"
   end
 
   depends_on "go" => :build
