48d012248fe291ffd395d801561e5f3bdd5dc908
Rui Chen <rui@chenrui.dev>
2022-05-17 14:40:51 +0000

    gst-plugins-ugly: update 1.20.2 bottle.

--- Formula/gst-plugins-ugly.rb
+++ Formula/gst-plugins-ugly.rb
@@ -12,12 +12,12 @@ class GstPluginsUgly < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "c10bb4e208b4b0a365b11f5a1db6caa3f81d84bb7068d5d0531ba0c6ac84ab9f"
-    sha256 arm64_big_sur:  "7850cc79f7a37e90ce45be14440a52129522846b84f78e073e3a7d3b8ed39fe9"
-    sha256 monterey:       "2bfa4940846d3ab0ca62e185c168c20795e6b35dd073af27ba2a738771f4a84e"
-    sha256 big_sur:        "90c4a81f113917509a2a321c593836f907dfc5972972f3a82db8e0d04f379c6a"
-    sha256 catalina:       "60a3f0a3264e8f1cc42660cb3fe5eb32f03dda311065ea3008f9b15b95ab3905"
-    sha256 x86_64_linux:   "ec842b0e266d5c0ab23b5c6a6bba9ede158295f341f5944b0814f3e58149b950"
+    sha256 arm64_monterey: "b47b114074a08d06a74f9a76c8fbfc3825192be31b5ff6fdd95af8ffdb5d7c30"
+    sha256 arm64_big_sur:  "67aeb5a674489a7423c2f62f76cb12d201ec1ed30ad23a52274c6d53da6931bb"
+    sha256 monterey:       "33a3b85a1e514d2670d72116e15e54ff86254e8175cafe9975d4f37652ca5d04"
+    sha256 big_sur:        "e6e5c6a2af088b4e984680955d640a5de859562cb8998082cd3167083f8b1ece"
+    sha256 catalina:       "b2dc01ccf29be35afa8d72d860b5f12381584e304c65c1517a20e7264fbae5bb"
+    sha256 x86_64_linux:   "5946f500bd61be2acf7c06f3e1be12d01adbd9767f06ab2ca26d95ff04563174"
   end
 
   depends_on "meson" => :build
