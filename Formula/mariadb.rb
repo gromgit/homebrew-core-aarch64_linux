2392a3ccb60720d9f3daa9db16294b2a12e59ba1
owine <owine@users.noreply.github.com>
2022-05-17 09:23:12 -0500

    mariadb: use libxcrypt on Linux

--- Formula/mariadb.rb
+++ Formula/mariadb.rb
@@ -4,6 +4,7 @@ class Mariadb < Formula
   url "https://downloads.mariadb.com/MariaDB/mariadb-10.8.3/source/mariadb-10.8.3.tar.gz"
   sha256 "887eadc55176ac1ead1fccfc89ade4b5990ef192745ad4dcd879acb41c050892"
   license "GPL-2.0-only"
+  revision 1
 
   # This uses a placeholder regex to satisfy the `PageMatch` strategy
   # requirement. In the future, this will be updated to use a `Json` strategy
@@ -38,6 +39,7 @@ class Mariadb < Formula
   depends_on "zstd"
 
   uses_from_macos "bzip2"
+  uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
