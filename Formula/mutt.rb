1369840662b4a5b4a3cf9d6abfc97aed2ef2da7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-17 05:28:00 +0000

    mutt 2.2.5
    Closes #101741.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mutt.rb
+++ Formula/mutt.rb
@@ -10,8 +10,8 @@
 class Mutt < Formula
   desc "Mongrel of mail user agents (part elm, pine, mush, mh, etc.)"
   homepage "http://www.mutt.org/"
-  url "https://bitbucket.org/mutt/mutt/downloads/mutt-2.2.4.tar.gz"
-  sha256 "b82776bebff0e10820a33fa1e7eafa5cba4ab3a011b08e83b66ed62d75c6e060"
+  url "https://bitbucket.org/mutt/mutt/downloads/mutt-2.2.5.tar.gz"
+  sha256 "ff8b781159794adcca126b551d5e419174b7b38a582a159bfe4e13451a757e47"
   license "GPL-2.0-or-later"
 
   bottle do
