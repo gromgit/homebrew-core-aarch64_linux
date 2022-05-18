51444a2490daa0c1fc144342c79455696731d151
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-18 21:05:10 +0000

    cri-tools: update 1.24.1 bottle.

--- Formula/cri-tools.rb
+++ Formula/cri-tools.rb
@@ -7,12 +7,12 @@ class CriTools < Formula
   head "https://github.com/kubernetes-sigs/cri-tools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d962159b8e8280d96805ae736d4bebf816319fb26ed04b2240c2a1e58767be70"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d962159b8e8280d96805ae736d4bebf816319fb26ed04b2240c2a1e58767be70"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f348ee2e484cd03e8569cfef8be86d783966d443d6910695020b7183f72bc2a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9f348ee2e484cd03e8569cfef8be86d783966d443d6910695020b7183f72bc2a"
-    sha256 cellar: :any_skip_relocation, catalina:       "9f348ee2e484cd03e8569cfef8be86d783966d443d6910695020b7183f72bc2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51b02c8fd89aaf71142ea32f33f93a6335b2784391c7df5999bc17dccb3105db"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a07bb8823636c1302757dc9d9a08d723e2fa646994ede8d5a2b000ec7c76adcf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a07bb8823636c1302757dc9d9a08d723e2fa646994ede8d5a2b000ec7c76adcf"
+    sha256 cellar: :any_skip_relocation, monterey:       "869aa39a5eab3ffdc5788110d4f4c9990fae441d600e2ebc970861ae4f8b300f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "869aa39a5eab3ffdc5788110d4f4c9990fae441d600e2ebc970861ae4f8b300f"
+    sha256 cellar: :any_skip_relocation, catalina:       "869aa39a5eab3ffdc5788110d4f4c9990fae441d600e2ebc970861ae4f8b300f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9b8011cab6cbb45bbbfc35068d36efc4e0d90ed02776d27da187ff2026ea4d0b"
   end
 
   depends_on "go" => :build
