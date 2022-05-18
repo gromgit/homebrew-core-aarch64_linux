0fa5175b2c682be57440850cf41dcc0426f4e662
Rui Chen <rui@chenrui.dev>
2022-05-18 16:44:17 -0400

    mbt 1.2.16
    Closes #101862.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mbt.rb
+++ Formula/mbt.rb
@@ -1,8 +1,8 @@
 class Mbt < Formula
   desc "Multi-Target Application (MTA) build tool for Cloud Applications"
   homepage "https://sap.github.io/cloud-mta-build-tool"
-  url "https://github.com/SAP/cloud-mta-build-tool/archive/v1.2.13.tar.gz"
-  sha256 "41ecfe2b7d91ce757cf35509c75b90e8bd90f7248ae781d25216f86f145416a0"
+  url "https://github.com/SAP/cloud-mta-build-tool/archive/v1.2.16.tar.gz"
+  sha256 "6fbd2a77f1da05129fce5776202543e9a033729880d8b42f59a9694d3d773d8a"
   license "Apache-2.0"
   head "https://github.com/SAP/cloud-mta-build-tool.git", branch: "master"
 
