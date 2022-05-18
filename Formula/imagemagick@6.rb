c2f92524d3f29cb219ca71c4311e5475fb404a4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-18 05:24:51 +0000

    imagemagick@6 6.9.12-50
    Closes #101819.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -1,8 +1,8 @@
 class ImagemagickAT6 < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://legacy.imagemagick.org/"
-  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.12-49.tar.xz"
-  sha256 "57bd77747248d0546996666310be9cba17f6001166b7028a31272bf1e20f0a49"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.12-50.tar.xz"
+  sha256 "8108981d60f21a9190e1f3b12011c8b3b75d318c76eef0b36a58512008266fff"
   license "ImageMagick"
   head "https://github.com/imagemagick/imagemagick6.git", branch: "main"
 
