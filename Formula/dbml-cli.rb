963fd61f15151c8149a9eeb45d194fbac2b4bb45
Rui Chen <rui@chenrui.dev>
2022-05-18 14:31:19 +0000

    dbml-cli: add 2.4.1 bottle.

--- Formula/dbml-cli.rb
+++ Formula/dbml-cli.rb
@@ -7,6 +7,15 @@ class DbmlCli < Formula
   sha256 "3d3cb0aef2e19f6c9ba77421768f70e3d24a8cfc8f905d64a8b84a3a53b419c6"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e813a5ed4c64ada675c10a639b581e91546aed656278c9e23853d9cbe7daa2ae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e813a5ed4c64ada675c10a639b581e91546aed656278c9e23853d9cbe7daa2ae"
+    sha256 cellar: :any_skip_relocation, monterey:       "22c543ffd72019c6ce39104e0e359c3d368939623edfec8a11fe8a330e432f47"
+    sha256 cellar: :any_skip_relocation, big_sur:        "22c543ffd72019c6ce39104e0e359c3d368939623edfec8a11fe8a330e432f47"
+    sha256 cellar: :any_skip_relocation, catalina:       "22c543ffd72019c6ce39104e0e359c3d368939623edfec8a11fe8a330e432f47"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0f7a426f76fdf7d261bb6c99c3b2798f76f1950a878e6025c72a5e80b892643a"
+  end
+
   depends_on "node"
 
   def install
