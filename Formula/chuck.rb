29cf3d66a7921bf17f494844a6bc80669c43f72c
Porkepix <porkepix@gmail.com>
2022-05-19 00:20:28 +0200

    chuck 1.4.1.1
    Closes #101866.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chuck.rb
+++ Formula/chuck.rb
@@ -1,9 +1,9 @@
 class Chuck < Formula
   desc "Concurrent, on-the-fly audio programming language"
   homepage "https://chuck.cs.princeton.edu/"
-  url "https://chuck.cs.princeton.edu/release/files/chuck-1.4.1.0.tgz"
-  mirror "http://chuck.stanford.edu/release/files/chuck-1.4.1.0.tgz"
-  sha256 "74bf99ad515e3113c55b833152936fad02a3cf006a54105ff11777c473194928"
+  url "https://chuck.cs.princeton.edu/release/files/chuck-1.4.1.1.tgz"
+  mirror "http://chuck.stanford.edu/release/files/chuck-1.4.1.1.tgz"
+  sha256 "4459ee6f151da72dcde1525e0afe05329d61086356b168ecfc0bc3a570290f63"
   license "GPL-2.0-or-later"
   head "https://github.com/ccrma/chuck.git", branch: "main"
 
