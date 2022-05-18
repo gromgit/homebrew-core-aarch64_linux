15df9a58711fd25f78c68fa5b14d11fd8fe4b424
Roger Sheen <infotexture@gmail.com>
2022-05-17 22:48:37 -0400

    dita-ot 3.7.2
    Closes #101805.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dita-ot.rb
+++ Formula/dita-ot.rb
@@ -1,8 +1,8 @@
 class DitaOt < Formula
   desc "DITA Open Toolkit is an implementation of the OASIS DITA specification"
   homepage "https://www.dita-ot.org/"
-  url "https://github.com/dita-ot/dita-ot/releases/download/3.7.1/dita-ot-3.7.1.zip"
-  sha256 "9a9790de79da566c636b805e5f42e94a8d3c0c187919e5159d1b8a64998b5bbd"
+  url "https://github.com/dita-ot/dita-ot/releases/download/3.7.2/dita-ot-3.7.2.zip"
+  sha256 "4f65e9cc471758d72c5b1640ea2c2d9875c5cb82780c500c97bf244dd20e5bd9"
   license "Apache-2.0"
 
   livecheck do
