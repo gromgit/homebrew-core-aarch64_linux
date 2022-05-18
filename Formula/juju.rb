f020e0d6d993893a1dd38cdc1f2e2d707942c3b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-18 05:27:20 +0000

    juju 2.9.30
    Closes #101822.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-2.9.29",
-      revision: "2ad800a0f8585e139eff34a1e214aac2da8e7073"
+      tag:      "juju-2.9.30",
+      revision: "a461c98909458b689b373b45cf1433a067c34661"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
