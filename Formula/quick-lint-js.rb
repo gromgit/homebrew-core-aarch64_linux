f7b813a58f2d4497e1b263bf32c1e2e757b62057
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-18 05:08:08 +0000

    quick-lint-js: update 2.4.2_1 bottle.

--- Formula/quick-lint-js.rb
+++ Formula/quick-lint-js.rb
@@ -8,12 +8,12 @@ class QuickLintJs < Formula
   head "https://github.com/quick-lint/quick-lint-js.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "89a1773ca9906bdde6449170f7c1f1dd49361f2335a9f310cd42058662e0f860"
-    sha256 cellar: :any,                 arm64_big_sur:  "b06bd33d74a281673276b1713de5e3fdfb89f5574a42b1837d0639f5344cce80"
-    sha256 cellar: :any,                 monterey:       "acefec368522b6d17d21a3fa780e197eeb5d305d6d8db64095ee5418a5a0cbed"
-    sha256 cellar: :any,                 big_sur:        "cba4ae88794eb5dc71568135e85593ab1f1c043f4851a9ed5379f6029da12e37"
-    sha256 cellar: :any,                 catalina:       "22a563cbbc25c78dda6896ad3689de97f7f45986483d699f1c557c785fd65e07"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50b6994fbc6b784a77899e6b2549af29ba391c5a12c26befd65b50af115efd63"
+    sha256 cellar: :any,                 arm64_monterey: "b44001ea2edb8e03eeb893f8f058a269b41831146057990b282d002b3e3e0029"
+    sha256 cellar: :any,                 arm64_big_sur:  "583de790c3199885d996f8e1cc8ad6baaa172dce11001fae10c4d54a8560fc85"
+    sha256 cellar: :any,                 monterey:       "fa7a70b2fa8bcf80a8e9a2e4e285f0394edeb2a7494212f352824482f7495aba"
+    sha256 cellar: :any,                 big_sur:        "92e002c03fcc14372899c246f74dc849bf8206803cb23de1a809558994f8d3dc"
+    sha256 cellar: :any,                 catalina:       "21903b2719081c1a79d8f12f59473795dcd732ab11bd7814cb530293f2c0cf5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61ac5a56ad923677b0a8983ce548c900a8d0ace8c951140101cffe4289406829"
   end
 
   depends_on "cmake" => :build
