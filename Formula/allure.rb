ba9e510162e92a714eb4753d59be43e59e341d0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-18 05:07:19 +0000

    allure 2.18.0
    Closes #101814.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/allure.rb
+++ Formula/allure.rb
@@ -1,8 +1,8 @@
 class Allure < Formula
   desc "Flexible lightweight test report tool"
   homepage "https://github.com/allure-framework/allure2"
-  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.17.3/allure-commandline-2.17.3.zip"
-  sha256 "11c5aa3e8d24763316a69da9af79b9821fa85d8e03e95e19250b33cae7809c62"
+  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.18.0/allure-commandline-2.18.0.zip"
+  sha256 "60afdf88fea6eba56fdeb2ea854a907b2dd445913f6f128b8b89b49f2d5ddc9e"
   license "Apache-2.0"
 
   livecheck do
