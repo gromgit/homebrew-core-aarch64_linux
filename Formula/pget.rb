9941848283529b0352b53c0152ec1dde4ab1d4e1
Rui Chen <rui@chenrui.dev>
2022-05-19 21:37:51 +0000

    pget: add 0.1.1 bottle.

--- Formula/pget.rb
+++ Formula/pget.rb
@@ -6,6 +6,15 @@ class Pget < Formula
   license "MIT"
   head "https://github.com/Code-Hex/pget.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "161dab036f92f8ed27d33852a62f0702deedf2c862ab59f00a5dc79c18c165bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ced15f422006c330d99f63db09a0263f6001d67308381cac67cbe59bd4688e84"
+    sha256 cellar: :any_skip_relocation, monterey:       "73838f1c861df1e0db812a511a9bd83074bc3934b8271c0d07259acca99bd013"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d3634ac25b2f88d3c25c0dc431560520dd3ee1d200c20731efe87374e8597263"
+    sha256 cellar: :any_skip_relocation, catalina:       "b1b8f24e110550393b67f7b079c345c9622970a1fb304c4782ec61e9aafe918e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8f774d4539a1ebb52d217dfdc936eca88d839454f4d5581b3c015eb9f6cc093"
+  end
+
   depends_on "go" => :build
 
   def install
