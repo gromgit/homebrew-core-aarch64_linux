b9ed929512cad92c8c1f402d7a4c265b3434f5d9
danielnachun <daniel.nachun@gmail.com>
2022-05-19 01:20:54 -0700

    gabedit 2.5.1
    Closes #102001.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gabedit.rb
+++ Formula/gabedit.rb
@@ -1,13 +1,12 @@
 class Gabedit < Formula
   desc "GUI to computational chemistry packages like Gamess-US, Gaussian, etc."
   homepage "https://gabedit.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/gabedit/gabedit/Gabedit250/GabeditSrc250.tar.gz"
-  version "2.5.0"
-  sha256 "45cdde213a09294bbf2df5f324ea11fc4c4045b3f9d58e4d67979e6f071c7689"
-  revision 2
+  url "https://downloads.sourceforge.net/project/gabedit/gabedit/Gabedit251/GabeditSrc251.tar.gz"
+  version "2.5.1"
+  sha256 "efcb00151af383f662d535a7a36a2b0ed2f14c420861a28807feaa9e938bff9e"
 
   # Consider switching back to checking SourceForge releases once we can alter
-  # the matched version from `250` to `2.5.0`.
+  # the matched version from `251` to `2.5.1`.
   livecheck do
     url "https://sites.google.com/site/allouchear/Home/gabedit/download"
     regex(/current stable version of gabedit is v?(\d+(?:\.\d+)+)/i)
@@ -27,18 +26,41 @@ class Gabedit < Formula
   depends_on "gtk+"
   depends_on "gtkglext"
 
+  on_linux do
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "mesa"
+    depends_on "mesa-glu"
+  end
+
   def install
-    opengl_headers = MacOS.sdk_path/"System/Library/Frameworks/OpenGL.framework/Headers"
+    opengl_headers = if OS.mac?
+      MacOS.sdk_path/"System/Library/Frameworks/OpenGL.framework/Headers"
+    else
+      Formula["mesa"].opt_include
+    end
+
     (buildpath/"brew_include").install_symlink opengl_headers => "GL"
 
     inreplace "CONFIG" do |s|
-      s.gsub! "-lX11", ""
-      s.gsub! "-lpangox-1.0", ""
+      if OS.mac?
+        s.gsub! "-lX11", ""
+        s.gsub! "-lpangox-1.0", ""
+      else
+        # Add PKG_CONFIG_PATH to pangox-compat in gtkglext.
+        ENV.append_path "PKG_CONFIG_PATH", Formula["gtkglext"].libexec/"lib/pkgconfig"
+        s.gsub! "OGLLIB=-L/usr/lib -lGL -L/usr/lib -lGLU",
+                "OGLLIB=-L#{Formula["mesa"].opt_lib} -lGL -L#{Formula["mesa-glu"].opt_lib} -lGLU"
+      end
       s.gsub! "GTKCFLAGS =", "GTKCFLAGS = -I#{buildpath}/brew_include"
     end
 
     args = []
-    args << "OMPLIB=" << "OMPCFLAGS=" if ENV.compiler == :clang
+    if OS.mac?
+      args << "OMPLIB=" << "OMPCFLAGS="
+      ENV.append "LDFLAGS", "-undefined dynamic_lookup"
+    end
     system "make", *args
     bin.install "gabedit"
   end
