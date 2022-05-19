764e226af37632624164c5199186955408901115
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-19 07:05:06 +0000

    scipy: update 1.8.1 bottle.

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -7,13 +7,12 @@ class Scipy < Formula
   head "https://github.com/scipy/scipy.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_monterey: "3750231845bfdb26a1f4295c3f91e70b879be62e3e3b6106e537da2c2d71870f"
-    sha256 cellar: :any, arm64_big_sur:  "c2dd138f5a909512fb5756196e90f2220640b9629d3474250a1632a50d6ef96b"
-    sha256 cellar: :any, monterey:       "116114cd1d9a36e3646c457a758286b143f31a82be833de512421b7fe33afbed"
-    sha256 cellar: :any, big_sur:        "5bc525993c6524fe61f04abfd47d6e5447ab8d7ad5c5df1baca97fbe2a3e255e"
-    sha256 cellar: :any, catalina:       "637f5820cad2cff149bf8471ad431ab4593b9bf458e4f62bae134252802cc3cd"
-    sha256               x86_64_linux:   "97be2faad3606d207d1def7ed58074b3223be18a0867e9061fc863c6e1a162fe"
+    sha256 cellar: :any, arm64_monterey: "e70703dd223d9b678f465c268e77a892cfe2602b7f7ad9a288a8efbd65ec3c92"
+    sha256 cellar: :any, arm64_big_sur:  "1382ef60f57a259d00f76239d5763f70e3ce7b922da295023ea2970068527054"
+    sha256 cellar: :any, monterey:       "556d91d10b74c316dc5fdbcb3ab5382b6b4d3d4f33ea0ac4faa412155cbb75c8"
+    sha256 cellar: :any, big_sur:        "5b5eafe225aceef063771c22197b882dd6b6dfd2e9a7656cd65c3967ba8f0537"
+    sha256 cellar: :any, catalina:       "4484ed1e364d8175548e3f59080669f1a6563310ec3a8a355bc6130029eb3b3c"
+    sha256               x86_64_linux:   "ec5adbad1bff9d60dc84e9e0b4f92f9b1dc3c25c2532e744ef78b6b3a0cae92c"
   end
 
   depends_on "libcython" => :build
