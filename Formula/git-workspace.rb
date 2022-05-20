903a219ba8bf5deb5aea4fcc6fcf2bf1a6ff53f8
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-05-20 02:57:30 +0000

    git-workspace: add 1.0.3 bottle.

--- Formula/git-workspace.rb
+++ Formula/git-workspace.rb
@@ -5,6 +5,15 @@ class GitWorkspace < Formula
   sha256 "dbbca1194990203049e6e0c95b2e8242a61e2be1d37261ae9168f0c02a309935"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "45c05688ed39c64f93bc3b8e72483047da54de75c368c057cd670e48b9ddc0b4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5dfca2198b7160d94283337fe6e27fd6fe10d657a81b1f4c8535e64d45c07720"
+    sha256 cellar: :any_skip_relocation, monterey:       "c221ee1fa14e2b2f66e600b191d8ac0e7e355002fa31fda61135447d91f3bf13"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4926641c71a67efebc0a5a754f0278ccc98c06a57ae3d80ac3d3333c1209a7ee"
+    sha256 cellar: :any_skip_relocation, catalina:       "f970f18bfeb3880158276a0c2a28e5e6a2a0fd68ace3dd2e3cc9efebe8c2dfa6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10fd6cc73258a0325c6849e053caaa635793d770cdfeec4a381ecde800ed8e25"
+  end
+
   depends_on "rust" => :build
 
   uses_from_macos "zlib"
