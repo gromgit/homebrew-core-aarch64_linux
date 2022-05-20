e210b73d55e2ee204f0bb3372d08023569cad48a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-20 07:04:59 +0000

    pgpool-ii: update 4.3.2 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -10,12 +10,12 @@ class PgpoolIi < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "074b03b6c0e8d7b84f8e903ed560e2ce3cfe11c3b5e13876033e6b2135010987"
-    sha256 arm64_big_sur:  "c7b7bb9a3f1e2728cde2033e1307a3bfb1765ec3ebb1a67ea76f69822755b0f5"
-    sha256 monterey:       "f0e8e796d169bd27b1b8af149723d96caded0a85c219886a757dea79bb88685a"
-    sha256 big_sur:        "b17e928829e63bcfbdb05e6907d871444fd8298280fba54ea7431f790c22de12"
-    sha256 catalina:       "90de27f6ea8a5e8d5a3f71f2751571ff9b5de72cd45f0ebb9a9207f8cc8f90ea"
-    sha256 x86_64_linux:   "4f05dcf54d2066c9bd0f3b706e6b4e08c701ff25d71c0c0be62f86aa902c828c"
+    sha256 arm64_monterey: "ef9e1d85dd0408f231620471f8973e9938bca70703858bdd6ce377e9dc972e04"
+    sha256 arm64_big_sur:  "428cfb6fa6ca6907f8bd8bc8e2fd0033190e3b41b75044db00df22c546f45ab7"
+    sha256 monterey:       "f2990632c2c5d29b42a984ffee627658416e8bb63f558921edf29f20b85ff4f5"
+    sha256 big_sur:        "1e1226f8866f6452abe59dc763d5030a24fa825146f364daeb30f3fd174e8bfd"
+    sha256 catalina:       "01bfd000c01f4a917e5689e348c9b1d7bb0bd17aecb2db0ae2c7155bc5672cbc"
+    sha256 x86_64_linux:   "9b1443b0444aca2330656f3d0c557249e537b551626a9c5124fbd8c11cb9f54f"
   end
 
   depends_on "postgresql"
