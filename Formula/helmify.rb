74f86cf4aceccd73d0b946450aca9debed59ea48
Rui Chen <rui@chenrui.dev>
2022-05-20 14:37:14 +0000

    helmify: add 0.3.12 bottle.

--- Formula/helmify.rb
+++ Formula/helmify.rb
@@ -7,6 +7,15 @@ class Helmify < Formula
   license "MIT"
   head "https://github.com/arttor/helmify.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cef8cd4760798dc9eb966857e1c614bf91c78cfe3aafbf930a7fd4d32b30a0a2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e68f0e9274faf61830ebb2156f2edb3877a86e94f41d35eb77685dd2252f0bfb"
+    sha256 cellar: :any_skip_relocation, monterey:       "3d615588d2f06e755cba838168270b7bf3d191cfb8353a419345df131804e40d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ea7c24fd72e2022f9e371e8f2f96dd0e83a6161d38dbdf4b917639c70500e122"
+    sha256 cellar: :any_skip_relocation, catalina:       "1bb265ae23580f56ff3126570ec2401e82aa4d0550cfe02a037fa7b779e98996"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f485c5ce0cdce27ed86fe54a913728410245c9e49b88f7334d3a9c5968ceb6a"
+  end
+
   depends_on "go" => :build
 
   def install
