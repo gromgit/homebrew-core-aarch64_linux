6156458f63fdec7e7cf0215ab856f6b377e213d8
Milas Bowman <milas@tilt.dev>
2022-05-20 14:21:00 -0400

    tilt 0.30.1
    Closes #101999.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -2,8 +2,8 @@ class Tilt < Formula
   desc "Define your dev environment as code. For microservice apps on Kubernetes"
   homepage "https://tilt.dev/"
   url "https://github.com/tilt-dev/tilt.git",
-    tag:      "v0.30.0",
-    revision: "e33830b0a517386437902d3aa3f7a2c72504aaf5"
+    tag:      "v0.30.1",
+    revision: "b7382fd509bc2a3fe0fae1e16820ae58ddfc4b39"
   license "Apache-2.0"
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
