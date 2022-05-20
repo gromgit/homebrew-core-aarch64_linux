c6ad2bce6a5eee5a445b2633eec1742f8dd32092
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-20 14:04:47 +0000

    vapor: update 18.4.1 bottle.

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -7,12 +7,12 @@ class Vapor < Formula
   head "https://github.com/vapor/toolbox.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9844f4021880ae9c897816e239ec7b8e2a4db84db38c901b23831aa475229066"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f2297e3b959aa4e8708c8319c6408057c0ef65b9804e249862a818c93433231"
-    sha256 cellar: :any_skip_relocation, monterey:       "c1d30a46e7df99f7da46e2aff3c9e8aa4e70f5d290295e81ff32f8b49be0819d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a9f72599af6fc398847a947a80e8fef567e5c1d4ecf2ab9ecfa3161fb1c80187"
-    sha256 cellar: :any_skip_relocation, catalina:       "19c812ee543c37232b4ede3bb38de34b92812fa3ffa6ac32962728a974fd9201"
-    sha256                               x86_64_linux:   "7e9c0e6e4bedd6a176c73b01d3658677f052fe9db01e3b5feb3cb91e4e4dd4f8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c45f6c189831f339694506406b6716d6d6b63303d27c89b5d237a5dfc344cd9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b70ae397fbe4c87e4e5cc404393cd0cf6d10d8b4ff8bac578cc7cddcc893673"
+    sha256 cellar: :any_skip_relocation, monterey:       "9551505865840cf27bc732632b9bbddfa96898baeef9c2ddf3e0178bec3cc203"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b9135c9f2904d15196cbb726b070fff83d7b8796da1b7334bf2401f47c8c20f3"
+    sha256 cellar: :any_skip_relocation, catalina:       "dec39f0259abb6a8203172cae7f7bb79f0d3db106cf53d5210d79fabe4cd5894"
+    sha256                               x86_64_linux:   "ffc52ce11835b32fb16e009c246cc20ab5d1d576bf136339609ac0f02a155ca1"
   end
 
   depends_on xcode: "11.4"
