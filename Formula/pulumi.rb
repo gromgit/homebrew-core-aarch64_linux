8cc29942092cdc2ade9c1b0d120a7da345dd1e2e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-20 01:34:18 +0000

    pulumi: update 3.33.1 bottle.

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -8,12 +8,12 @@ class Pulumi < Formula
   head "https://github.com/pulumi/pulumi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce92d35df36106da4c3142e10b21c00fe38a41e9a258b46ce53393c49af5e827"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d10ad2716d756bb939ab9e777edf7cd0e962b6c7297fa2b647be52670303babd"
-    sha256 cellar: :any_skip_relocation, monterey:       "8488fc8d4f4f5cc8b453b7340b93399a279199b3d4211f24dccfb68eca568074"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5cb2e05b38cf924e9b16fcac3e478142ff8ebf10066a2948cc80bcd51a677929"
-    sha256 cellar: :any_skip_relocation, catalina:       "b676ae39274ece491b07a0c0864c85ea320deefcf6a780fef067a748163cdb3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b84060bc6fd256c985f0bec0b9dbe0594089858b258ec8d56cf4c4fd657ac57"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1ce1918c12d5a48e6adf30b4e1ef922936e40412a5b74b27a7e1ae668d58e3a8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6cfae0a3d0ec87cf98122f7f5b1c58b3da5556b3df0966741dc46e4bd2ad50f3"
+    sha256 cellar: :any_skip_relocation, monterey:       "2894566a84b7fa7907b720e9d350fc3e24926a709dee7d80138c11d8e8b9064e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a263796fa2fd0545696be54462e41a9bbe13cc292942694535faaef30a97c40e"
+    sha256 cellar: :any_skip_relocation, catalina:       "2f8fbe70e9310224eeaac6825203d8bc2cb359605856b8c0d91daa5b51ef1ca9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f150446ef8ccd1c3d67949eca0a76512c236dd655a78be7672341e83a6ac9c9"
   end
 
   depends_on "go" => :build
