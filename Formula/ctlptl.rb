4a1d0e44c662a115141494b0002e147559f8fbe7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-21 02:28:43 +0000

    ctlptl: update 0.8.2 bottle.

--- Formula/ctlptl.rb
+++ Formula/ctlptl.rb
@@ -6,12 +6,12 @@ class Ctlptl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b78a118ac9fa727f86dbf60d1a0b72a3e269641ee8ef14ad3284f505fe93692"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "586c1f6f8758effbcf478a3554c6e695db6947ea541a4350f05f228c6a6fa91f"
-    sha256 cellar: :any_skip_relocation, monterey:       "91b3b902065592edb5cfa5504ef7bf6216c91af6992d3cc817673c0657f3c3f8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f3b1dc9c1a38b146a5836eeed95ceab7656723d8137bc449a651c28c5eec7374"
-    sha256 cellar: :any_skip_relocation, catalina:       "60c65326d04213cd552f6064d9b056232001ee25c9fb27a87821951484f3d2a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06d10ea0e88f20e80c5801a78cf040744e4b0dcd316636350fcefb861eb031cf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0a5eafa517c7e73c94cebf50895c0b468cf9175e2d05bc80dee4ba60d64f7795"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3be6042e792085f9779ee46b42950d3c97366e534ad0edd73bce1c3c0770f752"
+    sha256 cellar: :any_skip_relocation, monterey:       "cf9a670fd472bd47817d718f0c029f0f571f621f6a978b1775c791162ae78f71"
+    sha256 cellar: :any_skip_relocation, big_sur:        "36e76ead9fffa9971e3c66f88a3101fbd5bca3bf76690f54d6ae5ac5f3a0170e"
+    sha256 cellar: :any_skip_relocation, catalina:       "1dcf3f76356d289bcb28e514c7a755a5648b73f3526a20a3440e2387b8845e02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "34e6aa9ac48a099f1caf49a928f3d845fb12ed56935fe25a7614b1a51c5d05e8"
   end
 
   depends_on "go" => :build
