8e0313ebad6269cde783ea3430feaf7016f9396b
Dustin Rodrigues <dust.rod@gmail.com>
2022-05-20 20:48:26 -0400

    diffoscope 213
    Closes #102018.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -3,8 +3,8 @@ class Diffoscope < Formula
 
   desc "In-depth comparison of files, archives, and directories"
   homepage "https://diffoscope.org"
-  url "https://files.pythonhosted.org/packages/1f/cf/e98d1a0031fc8d1ee970a150147c2b834297eb521124738844d4d7e9d0c3/diffoscope-212.tar.gz"
-  sha256 "744260ccf2bb869c58c50dfab68957dd494b12e446520993d0925b2394ea1db7"
+  url "https://files.pythonhosted.org/packages/0f/63/9e720347fce585a9b3f1406d5289de39a97ccf8b18e5ebf0f79379031674/diffoscope-213.tar.gz"
+  sha256 "ce41bcde13df94c00c7bad57ea5662471a247b08f847ae20a41469341592d0ce"
   license "GPL-3.0-or-later"
 
   bottle do
