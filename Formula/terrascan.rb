75043f3e4ffe045a1d32b8359132b3d67678e091
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-21 10:05:14 +0000

    terrascan: update 1.15.1 bottle.

--- Formula/terrascan.rb
+++ Formula/terrascan.rb
@@ -7,12 +7,12 @@ class Terrascan < Formula
   head "https://github.com/tenable/terrascan.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "70faa89bfd032ee416fbe169defe8bfe5fa6b16c9631818c0faccfe1a3191b8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c3009304b8200262a603111147172038132840339738c6c5ea925d4a1361c822"
-    sha256 cellar: :any_skip_relocation, monterey:       "7f233c3c85f91b7c1eaae9a091241809136f9dade8d937bdea128890409d3a72"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0e7e0f6cfbd6ac0419fc8c25bc739c16dab3d3150233a848fede5427f6ce19e6"
-    sha256 cellar: :any_skip_relocation, catalina:       "04cfd04912a6a5842e9d336b4884dfe198f136f84d5199485583940688d5883e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87849071e4f38a979d7b1d40315a45211d65ada80941e98e81bccc973205406d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6391e53f88655344b1bcf87c5d7b762beeca98617695bd2443744f60e38b0ace"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2bd8ac2c007ea3769acce43a7afcc0e1d167a1642643e78ee9b5587cab85f806"
+    sha256 cellar: :any_skip_relocation, monterey:       "ddccc3912725170ddc975c7128fd4d973209a958f36649b014e3e7b90ca4e1ab"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6605379f6b8641891f4a60906987256e82d2b5fce6819d5b0981cd73e1fc5500"
+    sha256 cellar: :any_skip_relocation, catalina:       "b50cbe49d7d715da3c9b3d2c4d47fb00a34b7fd2c807d26596e280a16366b3ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "92231bf958a345a8fac5673ae056802178271c7608f2337710b4e266ccf7b687"
   end
 
   depends_on "go" => :build
