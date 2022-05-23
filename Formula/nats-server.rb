a8e210daf48622e12b781da50373f7f3293f3994
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-23 23:04:47 +0000

    nats-server: update 2.8.3 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -7,12 +7,12 @@ class NatsServer < Formula
   head "https://github.com/nats-io/nats-server.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "041bcdebc7b947a87b7ca544be24121694633d65a851e39e245e2d6b5c28b181"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f1f231bb5208dd55da7ecd555700952c74348d474d102385a24849b0764d1867"
-    sha256 cellar: :any_skip_relocation, monterey:       "52620d7ec67506c9c7b7c01cf3a625ec9daff2e20d97fa5df706ada577c6a896"
-    sha256 cellar: :any_skip_relocation, big_sur:        "08fba35e0667e536c1b379fd41f95420303d6b1cd153c3f4ec8548e630027243"
-    sha256 cellar: :any_skip_relocation, catalina:       "8e9189b6a62a93b612da20b262162dfb00a990f6d0b544bf961de7a21256231f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "564852f5bf4cdeefa50fc4f284efba19f5ce709c61703f4d92e875b4b447e000"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c8336ae4d915424b05b581970da5f3b508c648a15d5f6662a6c090ea359ee46a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffb39caaa17e7f7e3cd6dd3e783b45e0dff4f366f35fa3cb74f13b0e56ece260"
+    sha256 cellar: :any_skip_relocation, monterey:       "1f4d3aa9bd34e73f6afcf3eff5333138f5dc4e99c0a0a9ba47efe826548efe60"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d8a61e5a296eb3e169b38ce7d06f11c728c25bb273d943062d3ec02639c260a3"
+    sha256 cellar: :any_skip_relocation, catalina:       "f1c22e0db576873093c9f7951fa2d7143f6a47fce8f616e667b7c8bec9b64c16"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aa0f28c8c58c056ea28bda721cf3bbb8b71fb561500b29f775200659c1737687"
   end
 
   depends_on "go" => :build
