d26dc26176646b369528ca5634b924bd84a6b220
Porkepix <porkepix@gmail.com>
2022-05-24 01:19:55 +0200

    helmsman 3.10.1
    * helmsman 3.10.0
    * update test
    * Update helmsman.rb
    * Update helmsman.rb
    * helmsman 3.10.1
    * update test
    
    Closes #102145.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -2,8 +2,8 @@ class Helmsman < Formula
   desc "Helm Charts as Code tool"
   homepage "https://github.com/Praqma/helmsman"
   url "https://github.com/Praqma/helmsman.git",
-      tag:      "v3.9.1",
-      revision: "6c4a6b08354111e21fa411f98b063fbd896c3ffa"
+      tag:      "v3.10.1",
+      revision: "a70ae4bdc1d45088ee50564a8b5557edea6be88b"
   license "MIT"
   head "https://github.com/Praqma/helmsman.git", branch: "master"
 
@@ -23,12 +23,16 @@ class Helmsman < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/helmsman"
     pkgshare.install "examples/example.yaml"
+    pkgshare.install "examples/job.yaml"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/helmsman version")
+    ENV["ORG_PATH"] = "brewtest"
+    ENV["VALUE"] = "brewtest"
 
-    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1")
+    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1", 1)
     assert_match "helm diff not found", output
+
+    assert_match version.to_s, shell_output("#{bin}/helmsman version")
   end
 end
