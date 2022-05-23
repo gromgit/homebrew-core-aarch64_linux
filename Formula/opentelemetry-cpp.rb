7a04fe3ea6702232ff1e24b289f4c1f300fadace
Rui Chen <rui@chenrui.dev>
2022-05-23 14:41:17 +0000

    opentelemetry-cpp: add 1.4.0 bottle.

--- Formula/opentelemetry-cpp.rb
+++ Formula/opentelemetry-cpp.rb
@@ -6,6 +6,15 @@ class OpentelemetryCpp < Formula
   license "Apache-2.0"
   head "https://github.com/open-telemetry/opentelemetry-cpp.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "745dd5ed81a51956e186084587a92ef7c34a794ee506b2dc9475529540ff2537"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c5ffb797635f9aedcbc40d9deec7e0c0743019c6071d2e9dfa8efc2d79e5bf2"
+    sha256 cellar: :any_skip_relocation, monterey:       "1eac961b2711bcf5e67874caba6e0bf0e7850403b59becc944f86b6108b3607b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0a5befaa3407b12db656a1525888edd239713303271f19c2b31bf717590c8f95"
+    sha256 cellar: :any_skip_relocation, catalina:       "9cc30ee3b3bbe1b38d1c832fc0424a6d588bfd2111687de8b73181273c33fc4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55d32db7e719901681270a6edbef6662d9ffbbd08b24bac1846dfbef5b95a02a"
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "grpc"
