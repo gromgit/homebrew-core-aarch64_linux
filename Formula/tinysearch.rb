2d308fb17c5a5192800d0f55c808191c74e0710c
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-05-24 07:19:03 +0000

    tinysearch: add 0.6.0 bottle.

--- Formula/tinysearch.rb
+++ Formula/tinysearch.rb
@@ -6,6 +6,15 @@ class Tinysearch < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/tinysearch/tinysearch.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "de6a50a196939ef8de1b56226bc90fcfb4d8c923d7e272070e9763989b6e7874"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae9454c83fc0e8a5e2cd96a8c4fdfa98f3634bf18e4458b5d0adb3c23c4c36d4"
+    sha256 cellar: :any_skip_relocation, monterey:       "4cb19ba79149afbba9bffd5f53b03200b4be771f866ec34104ce897a4671d45a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cbefbb548fe76f81e9fcf33b2982003e27fcdcbd65ce77c9db882579a8caa55c"
+    sha256 cellar: :any_skip_relocation, catalina:       "065e519c7ffec7ec0015e687b2d03622d128004475d360fe4e70372b5c575f4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e00fd4ae20a58edda0866fe5dbd5772d7535328375d540c36966a17e2c2d957e"
+  end
+
   depends_on "rust" => :build
 
   depends_on "rustup-init"
