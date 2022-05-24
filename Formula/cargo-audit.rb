59a1c8a8607f09a1ac4e140e5e99aaeb5c6c5434
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-24 05:11:29 +0000

    cargo-audit 0.17.0
    * cargo-audit 0.17.0
    * cargo-audit: fix test
    
    Closes #102161.
    
    Co-authored-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-audit.rb
+++ Formula/cargo-audit.rb
@@ -1,8 +1,8 @@
 class CargoAudit < Formula
   desc "Audit Cargo.lock files for crates with security vulnerabilities"
   homepage "https://rustsec.org/"
-  url "https://github.com/RustSec/rustsec/archive/cargo-audit/v0.16.0.tar.gz"
-  sha256 "f0370c87c7a7976387303c7b5cc06651979968358b4f4dc867d30d65fab4d323"
+  url "https://github.com/RustSec/rustsec/archive/cargo-audit/v0.17.0.tar.gz"
+  sha256 "18fc40de40b95adc55e4c4e767f969d62f60bba23805ea9455dd12c1c19f01c0"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/RustSec/rustsec.git", branch: "main"
 
@@ -40,7 +40,7 @@ class CargoAudit < Formula
   test do
     output = shell_output("#{bin}/cargo-audit audit 2>&1", 2)
     assert_predicate HOMEBREW_CACHE/"cargo_cache/advisory-db", :exist?
-    assert_match "couldn't open Cargo.lock: No such file or directory", output
+    assert_match "not found: Couldn't load Cargo.lock", output
 
     cp_r "#{pkgshare}/support/base64_vuln/.", testpath
     assert_match "error: 1 vulnerability found!", shell_output("#{bin}/cargo-audit audit 2>&1", 1)
