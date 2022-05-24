c2549b345a28d3ac8633b08b39d8b5e679bf8cd9
Daniel Foster <daniel@Daniels-MacBook-Pro.local>
2022-05-24 12:46:28 +1000

    gtksourceview5 5.4.1
    Closes #102156.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gtksourceview5.rb
+++ Formula/gtksourceview5.rb
@@ -1,8 +1,8 @@
 class Gtksourceview5 < Formula
   desc "Text view with syntax, undo/redo, and text marks"
   homepage "https://projects.gnome.org/gtksourceview/"
-  url "https://download.gnome.org/sources/gtksourceview/5.2/gtksourceview-5.2.0.tar.xz"
-  sha256 "c9b34fa02654f56ce22fa08827d89db4ba81631b2e6d7d31ea65d13c729430e9"
+  url "https://download.gnome.org/sources/gtksourceview/5.4/gtksourceview-5.4.1.tar.xz"
+  sha256 "eb3584099cfa0adc9a0b1ede08def6320bd099e79e74a2d0aefb4057cd93d68e"
   license "LGPL-2.1-or-later"
 
   livecheck do
