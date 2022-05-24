6e27138aadc358dc9a851fa25b6baf8ef88812e6
Porkepix <porkepix@gmail.com>
2022-05-25 00:47:12 +0200

    cdktf 0.11.0
    Closes #102198.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdktf < Formula
   desc "Cloud Development Kit for Terraform"
   homepage "https://github.com/hashicorp/terraform-cdk"
-  url "https://registry.npmjs.org/cdktf-cli/-/cdktf-cli-0.10.4.tgz"
-  sha256 "4d7f3a117df1fed05016db1dd0f243a4acdc808d0c0cfd3473dc6367be2c1cf7"
+  url "https://registry.npmjs.org/cdktf-cli/-/cdktf-cli-0.11.0.tgz"
+  sha256 "3f597306e3733fc60cceb5f1beacd3218594be693cc15e9950401696bf6a3c1c"
   license "MPL-2.0"
 
   bottle do
