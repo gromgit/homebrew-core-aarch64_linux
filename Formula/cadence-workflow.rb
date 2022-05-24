b5cb3669e2dc7d5633d807c905bae04d5522d736
Porkepix <porkepix@gmail.com>
2022-05-25 00:46:17 +0200

    cadence-workflow 0.24.0
    Closes #102196.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cadence-workflow.rb
+++ Formula/cadence-workflow.rb
@@ -2,8 +2,8 @@ class CadenceWorkflow < Formula
   desc "Distributed, scalable, durable, and highly available orchestration engine"
   homepage "https://cadenceworkflow.io/"
   url "https://github.com/uber/cadence.git",
-      tag:      "v0.23.2",
-      revision: "8dd7a0818dfe6a09c25bf8a7afd267834262f625"
+      tag:      "v0.24.0",
+      revision: "517c6c135a24a4f23eea6f3a3747e14e59b5d49e"
   license "MIT"
   head "https://github.com/uber/cadence.git", branch: "master"
 
