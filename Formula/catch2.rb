22d53dcc7181d5a0880dfaebf74ed310edb39a83
Chris Thrasher <chrisjthrasher@gmail.com>
2022-05-24 14:32:24 -0600

    catch2 3.0.1

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -1,8 +1,8 @@
 class Catch2 < Formula
   desc "Modern, C++-native, header-only, test framework"
   homepage "https://github.com/catchorg/Catch2"
-  url "https://github.com/catchorg/Catch2/archive/v2.13.9.tar.gz"
-  sha256 "06dbc7620e3b96c2b69d57bf337028bf245a211b3cddb843835bfe258f427a52"
+  url "https://github.com/catchorg/Catch2/archive/v3.0.1.tar.gz"
+  sha256 "8c4173c68ae7da1b5b505194a0c2d6f1b2aef4ec1e3e7463bde451f26bbaf4e7"
   license "BSL-1.0"
 
   bottle do
@@ -10,6 +10,12 @@ class Catch2 < Formula
   end
 
   depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
 
   def install
     mkdir "build" do
@@ -20,8 +26,7 @@ class Catch2 < Formula
 
   test do
     (testpath/"test.cpp").write <<~EOS
-      #define CATCH_CONFIG_MAIN
-      #include <catch2/catch.hpp>
+      #include <catch2/catch_all.hpp>
       TEST_CASE("Basic", "[catch2]") {
         int x = 1;
         SECTION("Test section 1") {
@@ -33,7 +38,7 @@ class Catch2 < Formula
         }
       }
     EOS
-    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    system ENV.cxx, "test.cpp", "-std=c++14", "-L#{lib}", "-lCatch2Main", "-lCatch2", "-o", "test"
     system "./test"
   end
 end
