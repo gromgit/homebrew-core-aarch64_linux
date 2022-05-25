e0a21c4303dffb12b1c0b4b2b7984eaec55448c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-25 02:43:28 +0000

    marp-cli: update 2.0.0 bottle.

--- Formula/marp-cli.rb
+++ Formula/marp-cli.rb
@@ -8,12 +8,12 @@ class MarpCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9891f6bdccd810f899b2acf46e9ea145a70ab15e629842c6a4713134f56789e7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9891f6bdccd810f899b2acf46e9ea145a70ab15e629842c6a4713134f56789e7"
-    sha256 cellar: :any_skip_relocation, monterey:       "30504eb972765913d2e2ca08b4e35980ca3a9fb373cb6c0fca5a875d08299cc4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "30504eb972765913d2e2ca08b4e35980ca3a9fb373cb6c0fca5a875d08299cc4"
-    sha256 cellar: :any_skip_relocation, catalina:       "30504eb972765913d2e2ca08b4e35980ca3a9fb373cb6c0fca5a875d08299cc4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "775b30d9938fdf229312de757eb67f6041846db54b2069726bba130c7be7a2f3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c4ecf53181280ee929ff54aadbd8a835e5494a5f897766a5e8d4ecb35c1d714d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c4ecf53181280ee929ff54aadbd8a835e5494a5f897766a5e8d4ecb35c1d714d"
+    sha256 cellar: :any_skip_relocation, monterey:       "692b0a95e68ef44405e77df9c884dfc149bbb3b7ff14fc0f17d82d15eaf946cd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "692b0a95e68ef44405e77df9c884dfc149bbb3b7ff14fc0f17d82d15eaf946cd"
+    sha256 cellar: :any_skip_relocation, catalina:       "692b0a95e68ef44405e77df9c884dfc149bbb3b7ff14fc0f17d82d15eaf946cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "54af39c88f2e19da643b0e58b322c55d65928001092ee689b79f24d886867dc7"
   end
 
   depends_on "node"
