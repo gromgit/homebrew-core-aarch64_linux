afa2774f0587153a637fc6f5f5ac1e8e3dbe3e95
Rui Chen <rui@chenrui.dev>
2022-05-25 15:30:45 -0400

    terramate 0.1.1 (new formula)
    Closes #102260.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/terramate.rb
@@ -0,0 +1,19 @@
+class Terramate < Formula
+  desc "Managing Terraform stacks with change detections and code generations"
+  homepage "https://github.com/mineiros-io/terramate"
+  url "https://github.com/mineiros-io/terramate/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "e143651f9b8df81ee1b434a927f783bfc75529f7db4edf911379a786ae712ba2"
+  license "Apache-2.0"
+  head "https://github.com/mineiros-io/terramate.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/terramate"
+  end
+
+  test do
+    assert_match "project root not found", shell_output("#{bin}/terramate list 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/terramate version")
+  end
+end
