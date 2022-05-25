9a82c78fc68c435a311a813147decede8caf207b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-25 08:05:29 +0000

    flarectl: update 0.40.0 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -7,12 +7,12 @@ class Flarectl < Formula
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "100ebbe899f81aa9b7d031ec1a8c35ba83993cb8849f451fa93e2aadf171be3f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cd57d841ea031ca9e3a9eecf6d63c8ce3282d22247a2dfba491cded31feae417"
-    sha256 cellar: :any_skip_relocation, monterey:       "77beef33039f1bc2e9a79586aa5d51a4ea9075f175e7b478bd9330f9689bdcb9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4c099f5960387d6e1b79f8517d4876a99c8e72f2fac50a9c2a3782670efb2fbe"
-    sha256 cellar: :any_skip_relocation, catalina:       "22a3f9ee53249cda272f8b88588154d18372aa7a1bef96321015547adab717da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "501566b78059f068821949edc821b12a9194c7be405e5ff49924158c748f8ec3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cc6a46dd605242ed6b5ab4795019dd4c60650a7f9962f142b54bceba75bb9841"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad2f033e06b88fac2124f288efee2fb29a32b8c32d93de5738d049c12bea7849"
+    sha256 cellar: :any_skip_relocation, monterey:       "967fc52d942cb41d6fc7e3f4523f5eea40dddbcc602170869b10bf03f9581607"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d3bdd15ae14e52e4ebcec7abea126890ae46dd7aab4cd56e4c129e50758942e0"
+    sha256 cellar: :any_skip_relocation, catalina:       "4a4fa7adaa0822a8e4faeb31939670b645c2a94883556bc9647bc6a69371977f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58d4be684ee41d1feee65952489eb9042d1649d6cd819f39af65844cf4c00d18"
   end
 
   depends_on "go" => :build
