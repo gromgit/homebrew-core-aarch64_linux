1e432a5e02c261d7dccf5f15dac09698fa446e77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-25 16:09:29 +0000

    catch2: update 3.0.1 bottle.

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -6,7 +6,12 @@ class Catch2 < Formula
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "457e6aad19afaea53718942729f62e17ea7d1a18be2a8a6b4c94cc7fa1d20d30"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c391abecbf8f2794b8365ab46f14f50469593e307bbc3a20d615fb79304b20ad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f64cc7402e35a668b4b85101443c555145cba6e922c33120cfdaf7bbdd7666f5"
+    sha256 cellar: :any_skip_relocation, monterey:       "6ef2c771a61e5f114dba5a6342bd01cde956b9c055cfd764a5bb0764763f0622"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f683d29973220f5982f30f969da569624e1358bb7166fa27ffc69ee71cbe0ed9"
+    sha256 cellar: :any_skip_relocation, catalina:       "194f0045df5d29fd739930562f4ed2d3f9c000076f1d9e8f8ff2029f67c7cd7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ffdcf88c50ca418402e47bae17775334d1c39a9b58830c1421d007e13dac2202"
   end
 
   depends_on "cmake" => :build
