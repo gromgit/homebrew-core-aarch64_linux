c2c2dabbc38d9cbcc94a9188a0faf3001866dca9
Porkepix <porkepix@gmail.com>
2022-05-26 03:43:16 +0200

    juju 2.9.31
    Closes #102291.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-2.9.30",
-      revision: "a461c98909458b689b373b45cf1433a067c34661"
+      tag:      "juju-2.9.31",
+      revision: "0f2ce8e528a67fa3f735dff39a1a68c44540bb97"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
