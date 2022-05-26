84987e6e018a777fff8a5edd897cce40f50303a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-26 10:04:58 +0000

    mask: update 0.11.2 bottle.

--- Formula/mask.rb
+++ Formula/mask.rb
@@ -6,12 +6,12 @@ class Mask < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "642f6b33ca9fb556b0e25c639c0b0b72bd577c16b38abc7aacb0186b06a5e6a7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9a8d12727a7304ab91f1efff0a936d0749ced7b93a23d95341f2d1bb4eb7fe50"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f94a81fb949d160b9b08c21ee23ddea7003cb7e1203f91f7147c5b910e00e6b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "feb8ceded3fb6fa99b2660eb22a649f7ec13ee72dacc3cb71c82b43688685d8e"
-    sha256 cellar: :any_skip_relocation, catalina:       "cc2cb845008b7772b2bd9f87cba495a5ed7702b7a5fc67ba4d79992e278e5cb4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dfe51d1fbb5949917a95262d8ebb2fba5448676e9349ae27297abeb0721e39a0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2c80321512808c157aa739263f16ac6497a0488c587b84abeb5690831a0ff8c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "43388f2bac72ba6c337e96acb1a9257177e97eb807bec15d1f62106a402c5ba0"
+    sha256 cellar: :any_skip_relocation, monterey:       "07d3bab6e155d245ac877c361093f3c109ae86b510daa4d9ede2393883b5219d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1d05edf78de9bba766f6283b2c9746cd356ffed037b35b4fc20e55e8660e6c18"
+    sha256 cellar: :any_skip_relocation, catalina:       "8d394f2f60f398dd483c33b30213f75525ea7e7e22cfd41ba7b0b027e835f42d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5edff1c56e8cf96f9b89323cf02883f41a9048b9c05867c900dc177268f35a14"
   end
 
   depends_on "rust" => :build
