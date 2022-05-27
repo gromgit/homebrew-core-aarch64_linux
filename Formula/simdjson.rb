d114c983bcaa1de74ed20b9c121e55bd4bf5d2e9
Alexander Bayandin <a.bayandin@gmail.com>
2022-05-27 10:04:10 +0000

    simdjson: update 2.0.1 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -7,12 +7,12 @@ class Simdjson < Formula
   head "https://github.com/simdjson/simdjson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3f3bc094257433369b6d14d04f5d9b46ecbdd119599992e09c5faae57e0d1d19"
-    sha256 cellar: :any,                 arm64_big_sur:  "410ce5b1f2ffcb1c9e537cf438176ce04b84b7b005617cb5a5cfafb3aaf01b03"
-    sha256 cellar: :any,                 monterey:       "545a3c8c20f740830fcf548405aab54d91f1d51c1e7c733b2462d9a58cd6f02c"
-    sha256 cellar: :any,                 big_sur:        "7f2199a54cd23fe8e89b2753c076730b77d3d454986324ad279031ef0f17a7d5"
-    sha256 cellar: :any,                 catalina:       "3f08f942a90c878b2fa0045a5c553e7de237dee5de1f015c677288c87fbcbbf3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5ae18ec7e3f38a94bc633dda79fd3dcc428ad4cadd2819d199ede85f5177fd8"
+    sha256 cellar: :any,                 arm64_monterey: "2d56a7a10bf960005696106b34b925e2351facbb0ad46c1c0e7e17a2927a8a17"
+    sha256 cellar: :any,                 arm64_big_sur:  "1ba6d99b2c0c08b51c406b06ba0a304b2032d02999d3f57318395b4906d5c9b9"
+    sha256 cellar: :any,                 monterey:       "edea3e6fb9e7227a6e07a4d88637e657b4bcef21eb7c7ec5291e03177a972e32"
+    sha256 cellar: :any,                 big_sur:        "35ce3872381e3a1cd7044a38baca0f44524b9614628e2fe0a53a84473f2665de"
+    sha256 cellar: :any,                 catalina:       "295f10b13635009b5951d2f777b768a525fde7e29a443438099a86cbe35cc395"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea4ea7e76ef351a54d809e56d5b92b05af7e86000ce9df13e85f620b989c59db"
   end
 
   depends_on "cmake" => :build
