549c33bb599ec883eea801a89d302f125c8c5d62
Rui Chen <rui@chenrui.dev>
2022-05-27 16:12:28 +0000

    dtrx: add 8.3.1 bottle.

--- Formula/dtrx.rb
+++ Formula/dtrx.rb
@@ -7,6 +7,15 @@ class Dtrx < Formula
   sha256 "5587258e762074d5395a6824fd7968ca4f4a1dc481f4852fb84d14e7624433fb"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7ca7a9b060a281c5e3483bff43d8311291530a28756a4ea2d4c6c5621839d58c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "da738e87e28fcd982398d9e6692b48f4f2cefc559c36c398263247d159441b79"
+    sha256 cellar: :any_skip_relocation, monterey:       "84cd22f24bbb11aa0dbfcb4861878e6c3c4dbf1e61d0e8f04271b6cf40be1f9b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "94b86cd5d6573b013ba01c25e621b20001cf1fb7059f62a6e923896003281909"
+    sha256 cellar: :any_skip_relocation, catalina:       "c3f597698b93f8a454b6b36d5dca0bf69f8fa32477a893a3f2125014f681cccf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "922c1a0c5b010798328531ba30313d2f461d0458abf7b7d11fa0d365dc86b6fc"
+  end
+
   # Include a few common decompression handlers in addition to the python dep
   depends_on "p7zip"
   depends_on "python@3.10"
