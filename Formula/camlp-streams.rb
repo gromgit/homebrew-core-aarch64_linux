49024071cfcced65a14042250780ebf5013bc079
Michael Cho <cho-m@tuta.io>
2022-05-27 13:41:20 -0700

    camlp-streams 5.0.1 (new formula)

---
+++ Formula/camlp-streams.rb
@@ -0,0 +1,25 @@
+class CamlpStreams < Formula
+  desc "Stream and Genlex libraries for use with Camlp4 and Camlp5"
+  homepage "https://github.com/ocaml/camlp-streams"
+  url "https://github.com/ocaml/camlp-streams/archive/refs/tags/v5.0.1.tar.gz"
+  sha256 "ad71f62406e9bb4e7fb5d4593ede2af6c68f8b0d96f25574446e142c3eb0d9a4"
+  license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
+
+  depends_on "dune" => :build
+  depends_on "ocaml-findlib" => :test
+  depends_on "ocaml"
+
+  def install
+    system "dune", "build", "@install"
+    system "dune", "install", "--prefix=#{prefix}", "--libdir=#{lib}/ocaml", "--docdir=#{doc.parent}"
+  end
+
+  test do
+    (testpath/"test.ml").write <<~EOS
+      let stream = Stream.of_list ([] : unit list)
+    EOS
+    system "ocamlfind", "ocamlopt", "-linkpkg", "-package", "camlp-streams",
+                                    "-warn-error", "+3", "-o", "test", "test.ml"
+    assert_predicate testpath/"test", :exist?
+  end
+end
