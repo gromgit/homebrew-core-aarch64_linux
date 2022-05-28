5f4acfbd68defc06c264922468ce3d7845520d90
Porkepix <porkepix@gmail.com>
2022-05-28 10:20:45 +0200

    chart-testing 3.6.0
    Closes #102472.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chart-testing.rb
+++ Formula/chart-testing.rb
@@ -2,8 +2,8 @@ class ChartTesting < Formula
   desc "Testing and linting Helm charts"
   homepage "https://github.com/helm/chart-testing"
   url "https://github.com/helm/chart-testing.git",
-      tag:      "v3.5.1",
-      revision: "c891fb09f2c5ba548574c4bcf31229755a94e711"
+      tag:      "v3.6.0",
+      revision: "49167c48fd3180c183290f5be178f7aa28ff1c49"
   license "Apache-2.0"
   head "https://github.com/helm/chart-testing.git", branch: "main"
 
