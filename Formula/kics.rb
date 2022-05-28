ec93ab4fec32798bb7a042422f0a9463322018fa
Rui Chen <rui@chenrui.dev>
2022-05-28 18:29:45 -0400

    kics 1.5.9 (new formula)
    * kics 1.5.9 (new formula)
    * kics: replace `pkgshare` with `opt_pkgshare`
    
    Closes #102507.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/kics.rb
@@ -0,0 +1,37 @@
+class Kics < Formula
+  desc "Detect vulnerabilities, compliance issues, and misconfigurations"
+  homepage "https://kics.io/"
+  url "https://github.com/Checkmarx/kics/archive/refs/tags/v1.5.9.tar.gz"
+  sha256 "d5573e2ba63272cb32221043e0df54c49a4bdb218bd62745ca08d2a8e705d689"
+  license "Apache-2.0"
+  head "https://github.com/Checkmarx/kics.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/Checkmarx/kics/internal/constants.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/console"
+
+    pkgshare.install "assets"
+  end
+
+  def caveats
+    <<~EOS
+      KICS queries are placed under #{opt_pkgshare}/assets/queries
+      To use KICS default queries add KICS_QUERIES_PATH env to your ~/.zshrc or ~/.zprofile:
+          "echo 'export KICS_QUERIES_PATH=#{opt_pkgshare}/assets/queries' >> ~/.zshrc"
+      usage of CLI flag --queries-path takes precedence.
+    EOS
+  end
+
+  test do
+    ENV["KICS_QUERIES_PATH"] = pkgshare/"assets/queries"
+    ENV["DISABLE_CRASH_REPORT"] = "0"
+
+    assert_match "No files were scanned", shell_output("#{bin}/kics scan -p #{testpath}")
+    assert_match version.to_s, shell_output("#{bin}/kics version")
+  end
+end
