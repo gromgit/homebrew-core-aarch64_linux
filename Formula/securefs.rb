52f0741d5d878ae96d77428da6d117573598449b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-28 06:05:25 +0000

    securefs: update 0.13.0 bottle.

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -8,9 +8,7 @@ class Securefs < Formula
   head "https://github.com/netheril96/securefs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, catalina:    "8a8c7dd74f9b3082b2b128cc058714a27206d910273e4148959a25b7d30c51b5"
-    sha256 cellar: :any, mojave:      "9bee23af87518f68df56b862ad0ef88f842ebe65f8feff02e90bf5ab8e91522e"
-    sha256 cellar: :any, high_sierra: "632496d8e9ed9fe91d18e9a2c9fef49c920dc091e10108246f8ab2056f75ea38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "99d70dae414d78c230684f94f42cc2f36c5a8f1934c3eb066b687e199866c421"
   end
 
   depends_on "cmake" => :build
