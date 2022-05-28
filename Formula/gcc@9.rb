fba5ca9a9856920e9d232f84b85be7f46c5beb2e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-28 06:06:05 +0000

    gcc@9: update 9.5.0 bottle.

--- Formula/gcc@9.rb
+++ Formula/gcc@9.rb
@@ -12,10 +12,10 @@ class GccAT9 < Formula
   end
 
   bottle do
-    sha256 big_sur:      "ef05afedb14a945c18e8ab08af9e96293a8ef285af2df365a676b9df0be9c93f"
-    sha256 catalina:     "01e1eb5be5910cd743653c25de299ac7614ca3910de50b0ae3c25f9ba89c108d"
-    sha256 mojave:       "c480dc44d4a5e568077e452c49c7c8e8daa61a924e7137fbeefc8821449d7d10"
-    sha256 x86_64_linux: "4534a693580cebb9f091bb50cd1e99018c8b59af8d1181123b3d02145295cc98"
+    sha256                               monterey:     "9aa22339b3002ae9b3bde3ed9034238d80b07cff4a5c3c60f3f3653f52c55ce3"
+    sha256                               big_sur:      "ea000947da4131b653f137e3e275c061b34d7faa6b961896ecabc3717009df32"
+    sha256                               catalina:     "464e3f7571feace6ab25eb79e10c5a882656f26cb83b07b4052020da152e0d96"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b42b21daea2631464b36bdb47c6fe99ed6f14e0fab563c89857d13b1466f4b40"
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
