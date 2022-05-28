1ed9836c98dfc49b685bf9ecd7cf068819b3ec47
Rui Chen <rui@chenrui.dev>
2022-05-28 16:45:54 -0400

    assh 2.14.0
    Closes #102503.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -1,8 +1,8 @@
 class Assh < Formula
   desc "Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts"
   homepage "https://manfred.life/assh"
-  url "https://github.com/moul/assh/archive/v2.12.2.tar.gz"
-  sha256 "bc22b229cac9f5c1f43b534788cfa0a024b6b70ed62eb215f81d443d571e10e8"
+  url "https://github.com/moul/assh/archive/v2.14.0.tar.gz"
+  sha256 "711721e2aece3b4d39c170924db8bf0f82c75690811d8b50f2310ccb2aa6b07f"
   license "MIT"
   head "https://github.com/moul/assh.git", branch: "master"
 
