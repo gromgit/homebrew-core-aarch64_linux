c2f73bb8cef09a64f449c19f7d3b7e6fbf4bf0fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-28 13:10:42 +0000

    chart-testing: update 3.6.0 bottle.

--- Formula/chart-testing.rb
+++ Formula/chart-testing.rb
@@ -8,12 +8,12 @@ class ChartTesting < Formula
   head "https://github.com/helm/chart-testing.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5c225372eb15f1546f8e5d8f5a5c2a0035955b2edbf378efad954a02c4da5126"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e2fb6c62d017c6167d205750558782fa871b75c0aee709834e8f6e6a340ac4d"
-    sha256 cellar: :any_skip_relocation, monterey:       "ae47595638dca3841e79d924f13ea079f5c958052b3e43aa7afdb1efbdff34fe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39682e0b4f25506ad93e8a50bb6d178e8ae2c68f9328fa601903362b70682163"
-    sha256 cellar: :any_skip_relocation, catalina:       "8f117ec2933662d8c49df81381e6ced2dc0f16e78bf4f6f6a3bc2bd440bbfaaf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e2409b0aeab320b9841aa346b451832ed6619013fb920a2e516baa88534b4c7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9cd2a960c4644ac5ad2bb144a62a18647e2870a06ea3fca098678516cfd40138"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f368c20c49acebf8da3d4dab0172f8eb4d30d9a7015c2f364dd3b9dc74ccfe8c"
+    sha256 cellar: :any_skip_relocation, monterey:       "ff34fe86e7dc85d95374e060ecb6ce32c6a064f8e889cba5cbf1b55c0a7a284a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c8dbde1207e2eb206e69555d55afb07202c666d90fef50699e758d3fb72d2f31"
+    sha256 cellar: :any_skip_relocation, catalina:       "7953cdb033a650426204408d8d52e702104911b2ce15a7a3b57d92b53b736695"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c880d6bc8971f046d406c70ee4139c29497a7e984fb61304939fdf6b8dd399c1"
   end
 
   depends_on "go" => :build
