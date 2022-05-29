5da787ba1eaef414dbd697039a4ccc5b426906df
Rui Chen <rui@chenrui.dev>
2022-05-29 14:21:25 -0400

    nifi-registry 1.16.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/nifi-registry.rb
+++ Formula/nifi-registry.rb
@@ -1,9 +1,9 @@
 class NifiRegistry < Formula
   desc "Centralized storage & management of NiFi/MiNiFi shared resources"
   homepage "https://nifi.apache.org/registry"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.1/nifi-registry-1.16.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/nifi/1.16.1/nifi-registry-1.16.1-bin.tar.gz"
-  sha256 "4a46072b2825cd8e897cb5978f6e11ef66aa610a572b85327a45c61f1e5360ed"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.2/nifi-registry-1.16.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.16.2/nifi-registry-1.16.2-bin.tar.gz"
+  sha256 "dbf46cdcc6fbba96253affc953298b0eb23154af1a8c5b5cdd2c93c6886ba729"
   license "Apache-2.0"
 
   bottle do
