9cc289bce5e003fa09809c6178ac1a24a103e609
Rui Chen <rui@chenrui.dev>
2022-05-28 20:34:39 -0400

    flix 0.28.0
    Closes #102515.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flix.rb
+++ Formula/flix.rb
@@ -1,8 +1,8 @@
 class Flix < Formula
   desc "Statically typed functional, imperative, and logic programming language"
   homepage "https://flix.dev/"
-  url "https://github.com/flix/flix/archive/refs/tags/v0.27.0.tar.gz"
-  sha256 "60f88048c517164deb8283435a21e942c3ea39f4bb7ea674a6f83fb0d211bc09"
+  url "https://github.com/flix/flix/archive/refs/tags/v0.28.0.tar.gz"
+  sha256 "e2ab09b2ad3a0f0f2d0a21321bee3532af974b97c7cc265606ddd3b49debbd5b"
   license "Apache-2.0"
   head "https://github.com/flix/flix.git", branch: "master"
 
