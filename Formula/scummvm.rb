4a0108e8c6d0f16291ae99caf9a12b36def12120
Michael Cho <cho-m@tuta.io>
2022-05-28 21:48:35 -0700

    scummvm: fix M1 Monterey build
    Closes #102541.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scummvm.rb
+++ Formula/scummvm.rb
@@ -1,11 +1,28 @@
 class Scummvm < Formula
   desc "Graphic adventure game interpreter"
   homepage "https://www.scummvm.org/"
-  url "https://downloads.scummvm.org/frs/scummvm/2.5.1/scummvm-2.5.1.tar.xz"
-  sha256 "9fd8db38e4456144bf8c34dacdf7f204e75f18e8e448ec01ce08ce826a035f01"
+  # TODO: Update license to GPL-3.0-or-later and remove from
+  # permitted_formula_license_mismatches.json on next release
   license "GPL-2.0-or-later"
   revision 1
   head "https://github.com/scummvm/scummvm.git", branch: "master"
+
+  stable do
+    url "https://downloads.scummvm.org/frs/scummvm/2.5.1/scummvm-2.5.1.tar.xz"
+    sha256 "9fd8db38e4456144bf8c34dacdf7f204e75f18e8e448ec01ce08ce826a035f01"
+
+    # Fix Apple Silicon build: ld: unaligned pointer(s) for architecture arm64
+    # clang: error: linker command failed with exit code 1 (use -v to see invocation)
+    # Remove on the next release.
+    patch do
+      url "https://github.com/scummvm/scummvm/commit/7003cf793f2ddc5b773293088a2b5e485bc8c105.patch?full_index=1"
+      sha256 "616fd3c2e8128c0c4c9d7bb56622f0abc27a101bc45cc7d2d79078d77cc03dbd"
+    end
+    patch do
+      url "https://github.com/scummvm/scummvm/commit/6791dc23196cd71d09ecc44c510c6dd35a3f0787.patch?full_index=1"
+      sha256 "45c6c6e70a3bae70d02b09f139d9f47cafe561524dfff9939c0e5bd63f7386b2"
+    end
+  end
 
   livecheck do
     url "https://www.scummvm.org/downloads/"
