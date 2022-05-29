6341d601284eafac44a8ef913d451b51d01acb67
Rui Chen <rui@chenrui.dev>
2022-05-28 21:15:39 -0400

    nuget 6.2.0
    Closes #102522.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nuget.rb
+++ Formula/nuget.rb
@@ -1,8 +1,8 @@
 class Nuget < Formula
   desc "Package manager for Microsoft development platform including .NET"
   homepage "https://www.nuget.org/"
-  url "https://dist.nuget.org/win-x86-commandline/v6.1.0/nuget.exe" # make sure libexec.install below matches case
-  sha256 "432d322b16ffab3fed5d24ab7ea30ae10c555a00339a9bf53060aa4b2aee7925"
+  url "https://dist.nuget.org/win-x86-commandline/v6.2.0/nuget.exe" # make sure libexec.install below matches case
+  sha256 "c89875d937b7faff3ac024c3ceeceaaecbd786c47b33c09dd4129275a405594b"
   license "MIT"
 
   livecheck do
