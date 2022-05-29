55f8a4d50ac3e08354d680ac01a2cb74e2bf204a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-29 16:04:43 +0000

    youtubedr: update 2.7.15 bottle.

--- Formula/youtubedr.rb
+++ Formula/youtubedr.rb
@@ -6,12 +6,12 @@ class Youtubedr < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e2e2f0ad2385e3aaabeacec3ca5d6aaa4daaac4aa05206c95fa1baf7566aa8e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e2e2f0ad2385e3aaabeacec3ca5d6aaa4daaac4aa05206c95fa1baf7566aa8e"
-    sha256 cellar: :any_skip_relocation, monterey:       "11f9686eb0a0bcc63c0c3bfbf3e0516e08ede934757c9dfc8ee554261b5e72d1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "11f9686eb0a0bcc63c0c3bfbf3e0516e08ede934757c9dfc8ee554261b5e72d1"
-    sha256 cellar: :any_skip_relocation, catalina:       "11f9686eb0a0bcc63c0c3bfbf3e0516e08ede934757c9dfc8ee554261b5e72d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ebb2b26fe97e737dd0a9d7386beb8d5825df15a229f600a3cc7ae09786ce0f03"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eaa869be01efa3d0ae8300b1704700cc65904438784e112941c7dfc0529cf837"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eaa869be01efa3d0ae8300b1704700cc65904438784e112941c7dfc0529cf837"
+    sha256 cellar: :any_skip_relocation, monterey:       "95b83c67af793112b1f18197971a1a4cfcfbb3056bab45a7c07c91c2947eb1b2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "95b83c67af793112b1f18197971a1a4cfcfbb3056bab45a7c07c91c2947eb1b2"
+    sha256 cellar: :any_skip_relocation, catalina:       "95b83c67af793112b1f18197971a1a4cfcfbb3056bab45a7c07c91c2947eb1b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "790dda94568b605b0c3464dbd0c7ee98c0fbe7d9d7603d7a6f801f974ada52a4"
   end
 
   depends_on "go" => :build
