56336c281b422a32e0bbdbd1e1507e29d24f802b
Rui Chen <rui@chenrui.dev>
2022-05-29 13:40:57 -0400

    gerbv 2.9.1
    Closes #102568.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gerbv.rb
+++ Formula/gerbv.rb
@@ -1,8 +1,8 @@
 class Gerbv < Formula
   desc "Gerber (RS-274X) viewer"
   homepage "https://gerbv.github.io/"
-  url "https://github.com/gerbv/gerbv/archive/refs/tags/v2.9.0.tar.gz"
-  sha256 "afbe700edc3287a534cc26ff64b63ed295d717c6764bba54f5434f90b5de5ff5"
+  url "https://github.com/gerbv/gerbv/archive/refs/tags/v2.9.1.tar.gz"
+  sha256 "bf0400cbd888fcffc5374d41f57a7c80ed859b603183d488da381c4ef8168e41"
   license "GPL-2.0-or-later"
 
   bottle do
