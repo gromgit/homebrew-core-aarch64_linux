48d5fbcedd6c197b3bc18094dc07ec82709dfe01
Rui Chen <rui@chenrui.dev>
2022-05-29 16:25:10 +0000

    frps: update 0.43.0 bottle.

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -7,12 +7,12 @@ class Frps < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2a81991278a1f027dc90898eeb39cd591cfb17f5af422847b141b2ca43beeb14"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2a81991278a1f027dc90898eeb39cd591cfb17f5af422847b141b2ca43beeb14"
-    sha256 cellar: :any_skip_relocation, monterey:       "248cd6a192017a7f38afb9b1f9dfcc3fa42e2f9587ba05a337d6b7f4805a59c0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "248cd6a192017a7f38afb9b1f9dfcc3fa42e2f9587ba05a337d6b7f4805a59c0"
-    sha256 cellar: :any_skip_relocation, catalina:       "248cd6a192017a7f38afb9b1f9dfcc3fa42e2f9587ba05a337d6b7f4805a59c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c6e4a0eb7135cbbedaf480b61c687614ace0c13b61de00f9bd1bec2726800398"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "daade03620d283833a8557dac4e8a894e24ab42664782216910dbbec9dc422fc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "daade03620d283833a8557dac4e8a894e24ab42664782216910dbbec9dc422fc"
+    sha256 cellar: :any_skip_relocation, monterey:       "6bc3baea146f12b33a794c8a02b52e77e5d325df70d389de071cbd5b16a777c2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6bc3baea146f12b33a794c8a02b52e77e5d325df70d389de071cbd5b16a777c2"
+    sha256 cellar: :any_skip_relocation, catalina:       "6bc3baea146f12b33a794c8a02b52e77e5d325df70d389de071cbd5b16a777c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6bb86c9fa43fc44639adfb41edb23c40babe55f2b1e4f5060f3e9fe2a662745"
   end
 
   depends_on "go" => :build
