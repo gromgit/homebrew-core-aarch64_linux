f3101672e046c66d25ecbfc5fbfd8355381b4eff
Rui Chen <rui@chenrui.dev>
2022-05-29 14:21:57 -0400

    nifi 1.16.2
    Closes #102583.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,9 +1,9 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.1/nifi-1.16.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/nifi/1.16.1/nifi-1.16.1-bin.tar.gz"
-  sha256 "c02fa82aaf101842ae0fa076d8dcbdd8d2b4cf74812a64684c48addef017250b"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.2/nifi-1.16.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.16.2/nifi-1.16.2-bin.tar.gz"
+  sha256 "27932e264c80985a12fa90439447c7db050f879b46398b74a09d9a734d74e147"
   license "Apache-2.0"
 
   bottle do
