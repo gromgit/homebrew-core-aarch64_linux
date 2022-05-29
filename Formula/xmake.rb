458b16ead418db43d42ffa15ec432beb74aaa8b2
Porkepix <porkepix@gmail.com>
2022-05-29 12:02:06 +0200

    xmake 2.6.7
    Closes #102553.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -1,8 +1,8 @@
 class Xmake < Formula
   desc "Cross-platform build utility based on Lua"
   homepage "https://xmake.io/"
-  url "https://github.com/xmake-io/xmake/releases/download/v2.6.6/xmake-v2.6.6.tar.gz"
-  sha256 "02267dc2af26088275661c4c6d986af0cdc283ff7684910bee3d8ab45c30389d"
+  url "https://github.com/xmake-io/xmake/releases/download/v2.6.7/xmake-v2.6.7.tar.gz"
+  sha256 "206d9532e80722292b944ada91f558e0b265a176e3476eeb3e1633ea53bb15a6"
   license "Apache-2.0"
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
