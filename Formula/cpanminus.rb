32916ec6c36b42719b910448ede60fb3356ad598
Alexander Bayandin <a.bayandin@gmail.com>
2022-05-30 11:58:50 +0100

    cpanminus 1.7046
    * cpanminus 1.7046
    * cpanminus: test that we use cpan url
    
    Closes #102617.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -1,15 +1,13 @@
 class Cpanminus < Formula
   desc "Get, unpack, build, and install modules from CPAN"
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.9019.tar.gz"
-  sha256 "d0a37547a3c4b6dbd3806e194cd6cf4632158ebed44d740ac023e0739538fb46"
+  # Don't use git tags, their naming is misleading
+  url "https://cpan.metacpan.org/authors/id/M/MI/MIYAGAWA/App-cpanminus-1.7046.tar.gz"
+  sha256 "3e8c9d9b44a7348f9acc917163dbfc15bd5ea72501492cea3a35b346440ff862"
   license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
-  head "https://github.com/miyagawa/cpanminus.git", branch: "devel"
+  version_scheme 1
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
+  head "https://github.com/miyagawa/cpanminus.git", branch: "devel"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c279cbdfa489e4b0be72e46a8e325fc4f8070ff83c7d78026e7b7c5831e3678d"
@@ -21,13 +19,18 @@ class Cpanminus < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "78c0fc8d2fcb14cb94e62d972f06ae6a1762846471eb5d0669909121c24fa08d"
   end
 
+  uses_from_macos "perl"
+
   def install
-    cd "App-cpanminus" do
-      bin.install "cpanm"
-    end
+    cd "App-cpanminus" if build.head?
+
+    system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}", "INSTALLSITEMAN1DIR=#{man1}",
+                                                            "INSTALLSITEMAN3DIR=#{man3}"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/cpanm", "Test::More"
+    assert_match "cpan.metacpan.org", stable.url, "Don't use git tags, their naming is misleading"
+    system "#{bin}/cpanm", "--local-lib=#{testpath}/perl5", "Test::More"
   end
 end
