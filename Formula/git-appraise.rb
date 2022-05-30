f6bb8f0b7f4d9067cb72999409e9c31e405cf929
Michael Cho <cho-m@tuta.io>
2022-05-29 22:08:17 -0700

    git-appraise 0.7
    Closes #102596.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-appraise.rb
+++ Formula/git-appraise.rb
@@ -1,10 +1,16 @@
 class GitAppraise < Formula
   desc "Distributed code review system for Git repos"
   homepage "https://github.com/google/git-appraise"
-  url "https://github.com/google/git-appraise/archive/v0.6.tar.gz"
-  sha256 "5c674ee7f022cbc36c5889053382dde80b8e80f76f6fac0ba0445ed5313a36f1"
   license "Apache-2.0"
   head "https://github.com/google/git-appraise.git", branch: "master"
+
+  stable do
+    url "https://github.com/google/git-appraise/archive/v0.7.tar.gz"
+    sha256 "b57dd4ac4746486e253658b2c93422515fd8dc6fdca873b5450a6fb0f9487fb3"
+
+    # Backport go.mod from https://github.com/google/git-appraise/pull/111
+    patch :DATA
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "60da80e86332479908e34bd54fa119a7a59211a9d188f0bf0264d25985306880"
@@ -22,13 +28,7 @@ class GitAppraise < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "auto"
-
-    (buildpath/"src/github.com/google").mkpath
-    ln_s buildpath, buildpath/"src/github.com/google/git-appraise"
-
-    system "go", "build", "-o", bin/"git-appraise", "github.com/google/git-appraise/git-appraise"
+    system "go", "build", *std_go_args, "./git-appraise"
   end
 
   test do
@@ -46,3 +46,24 @@ class GitAppraise < Formula
     assert_predicate testpath/".git/refs/notes/devtools/reviews", :exist?
   end
 end
+
+__END__
+diff --git a/go.mod b/go.mod
+new file mode 100644
+index 00000000..28bed68b
+--- /dev/null
++++ b/go.mod
+@@ -0,0 +1,5 @@
++module github.com/google/git-appraise
++
++go 1.18
++
++require golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12
+diff --git a/go.sum b/go.sum
+new file mode 100644
+index 00000000..b22c466b
+--- /dev/null
++++ b/go.sum
+@@ -0,0 +1,2 @@
++golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12 h1:QyVthZKMsyaQwBTJE04jdNN0Pp5Fn9Qga0mrgxyERQM=
++golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
