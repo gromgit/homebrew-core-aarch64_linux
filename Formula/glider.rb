2f12465c93df26f89deaadd50e8d7a81305877f8
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-05-30 09:07:05 +0000

    glider: add 0.16.2 bottle.

--- Formula/glider.rb
+++ Formula/glider.rb
@@ -6,6 +6,15 @@ class Glider < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/nadoo/glider.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8339ced3042241d3f128068b05cffc5f8f728cb6ab073272e0f76081b099b1e9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffcafc5e458c78e115b94f45abfb105e69d02f7a03b5d065414fdc4b996ef251"
+    sha256 cellar: :any_skip_relocation, monterey:       "8b956d24977eaf0ad44ff2112ada966d7e77d26276b6751792241bbb68d1c487"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1b31c976c5a2d2c0e5e06a667cf2778c59a7d8bdf346c206efb455cce63f2232"
+    sha256 cellar: :any_skip_relocation, catalina:       "20dd59e4d32da80636537d41a57ebec46944ba3754141360a7f2b7b6d5a1c5d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38800c290f046908cfc31de29be0bcd4bf5f617c036a532d961d438c6004e601"
+  end
+
   depends_on "go" => :build
 
   uses_from_macos "curl" => :test
