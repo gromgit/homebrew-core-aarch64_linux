b4dca0ad01b10081df91a690162c2660f21f7970
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-31 13:18:21 +0000

    seaweedfs: update 3.07 bottle.

--- Formula/seaweedfs.rb
+++ Formula/seaweedfs.rb
@@ -8,12 +8,12 @@ class Seaweedfs < Formula
   head "https://github.com/chrislusf/seaweedfs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7291dd7de6d825311f32ade733e215a02b79a81c8a60fd6e518be13657a6ccfc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8228ab3991bed611bec4158bab30fb96e92e0fd0dcb7f2d524e1509f446f89dc"
-    sha256 cellar: :any_skip_relocation, monterey:       "5a6bb2d317f12c0afcd92f1831ea01ff23cf58ff3ebd75809c3a99877382b87b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a5edfeef0d6d11ada1f390ec1c9349bd6efd81fd4df490fe7ce7fd19b6c90daa"
-    sha256 cellar: :any_skip_relocation, catalina:       "09d77a129f38b1783c68ad97fd1e3056140c52e1909e66afbeb0ab39c22b261e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "035044630c752d7380769891702b2a10c34890bf0a549a7c10c4c5e4adc60394"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f977b17dac540c611f83b8f1625abdb307ada5536b8aef273cc7b047bef9981e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d08966cf35b67c4eb4329063e9aef373e3e46de53a7df5c5c1829808599d0be"
+    sha256 cellar: :any_skip_relocation, monterey:       "d8cd39e47ed7aa9b58d29d0150edb2d4955f8a129ad287e94efeb1a689c9cdfe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3012a78811d1d94e906552f32d241235762825d1471eb16d879f22f6d348284e"
+    sha256 cellar: :any_skip_relocation, catalina:       "4130c68dafbb94a1201e636f4a51a9bf48f42c32160361cfe0af953115e84154"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4efa9d1c8fee602a7972b6d544ecb657dc90b2d2b43ee5dbec297d5f70fbd4c1"
   end
 
   depends_on "go" => :build
