cb1220088cdb10571b9d4a805466689a9cea4627
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-31 09:04:48 +0000

    ncdc: update 1.23 bottle.

--- Formula/ncdc.rb
+++ Formula/ncdc.rb
@@ -11,15 +11,12 @@ class Ncdc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3851c0505db606f727353c388b30563b23001ea02e9190216495f7e6df1ce03d"
-    sha256 cellar: :any,                 arm64_big_sur:  "7dfeff7bcbd463dd11bcc08b52114a445cacfae8ca7f0f6c8fae65c9e4b19d4a"
-    sha256 cellar: :any,                 monterey:       "1eaf5107e1f62d171a4468bf01356120a033fbc5fa82f21666786c5a255c0159"
-    sha256 cellar: :any,                 big_sur:        "f0ea06824da1588311108a37cd60eb32692383a062c0d59f7c366f870692ca1f"
-    sha256 cellar: :any,                 catalina:       "2f074f7eb6aa1a1d0024e1f900315cb4bd056ad711c0f504acb160714e07597a"
-    sha256 cellar: :any,                 mojave:         "4490571bb2101e00e3edc39ab25baf75a63dfd9617381ae0f102a274a2fbf622"
-    sha256 cellar: :any,                 high_sierra:    "bd143dd032839762597253796c562607e43c654c128d4f4162fd382fd660dbfd"
-    sha256 cellar: :any,                 sierra:         "59f5011b9c39b78c75a2bf5d93ae398a16660852a3b8fd8ed3a3389ea463fbf7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9e49b825062b7702a59fff600e5d623d252ea6bb1833707c51290abcf68e5290"
+    sha256 cellar: :any,                 arm64_monterey: "727da515f470c72550a7460230aa2172f6c49ad915a8dbc0130e6f5ecb04d631"
+    sha256 cellar: :any,                 arm64_big_sur:  "bbd57ae7047c9f06bbde9ed28e168abc5d392d36ed1a9c125edf059611f07694"
+    sha256 cellar: :any,                 monterey:       "e5ba6e94980ebfec5e26e78289da6212c1da5719ed5e120cd98e6521819d824e"
+    sha256 cellar: :any,                 big_sur:        "f34ecec366f3d0779c80ce292eadaa23f00f7d817fbfcd26b6888dfd1bf58990"
+    sha256 cellar: :any,                 catalina:       "0a929234d7c4d2a3585a0f5bee7cd6a6cd4487e0e2b04dd501ec680f03785522"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46cf9b85ad77d41c2f4159b70a9456416cced4887e1aaf8997e1734da0ebf464"
   end
 
   head do
