bfaf7aa1afe24d1cccc26b5799642b444e80aa09
Porkepix <porkepix@gmail.com>
2022-05-31 19:47:08 +0200

    stellar-core 19.1.0
    Closes #102692.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v19.0.1",
-      revision: "2b38097cbe980b0b602db023956a37460d5bd29d"
+      tag:      "v19.1.0",
+      revision: "e801fd93b2757bebb18ae8b4550afba444d225af"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git", branch: "master"
 
