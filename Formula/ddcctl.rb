0b0bac7301b40bd535a7520191fa3fae5eaedb07
Porkepix <porkepix@gmail.com>
2022-05-31 10:38:45 +0200

    ddcctl 1
    * ddcctl 1
    * ddcctl-1: remove patches not needed anymore
    
    Closes #102663.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ddcctl.rb
+++ Formula/ddcctl.rb
@@ -1,8 +1,8 @@
 class Ddcctl < Formula
   desc "DDC monitor controls (brightness) for Mac OSX command-line"
   homepage "https://github.com/kfix/ddcctl"
-  url "https://github.com/kfix/ddcctl/archive/refs/tags/v0.tar.gz"
-  sha256 "8440f494b3c354d356213698dd113003245acdf667ed3902b0d173070a1a9d1f"
+  url "https://github.com/kfix/ddcctl/archive/refs/tags/v1.tar.gz"
+  sha256 "1b6eddd0bc20594d55d58832f2d2419ee899e74ffc79c389dcdac55617aebb90"
   license "GPL-3.0-only"
 
   bottle do
@@ -15,18 +15,6 @@ class Ddcctl < Formula
   end
 
   depends_on :macos
-
-  # Apply 2 upstream commits to fix build failures with Xcode 13.
-  # Remove with next release.
-  patch do
-    url "https://github.com/kfix/ddcctl/commit/d3ab6ecfd649fa8c335cd45d632cfc8ee2069174.patch?full_index=1"
-    sha256 "59fbd6f9e0aefdc65de967c350b9ab353001972bbc1b8fbe1bffb458e81a2700"
-  end
-
-  patch do
-    url "https://github.com/kfix/ddcctl/commit/8395b07150508305ff92ead307f1563163212383.patch?full_index=1"
-    sha256 "11646455bcb08fd3d8daad4e311df39a26936c4bc5da9180474eed33ccc52256"
-  end
 
   def install
     bin.mkpath
