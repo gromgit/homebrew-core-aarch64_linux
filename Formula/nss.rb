2b3b65ac14b1b4f3b67f40a0683a6247505cf9db
owine <owine@users.noreply.github.com>
2022-05-31 14:53:05 -0500

    nss 3.79
    Closes #102697.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nss.rb
+++ Formula/nss.rb
@@ -1,8 +1,8 @@
 class Nss < Formula
   desc "Libraries for security-enabled client and server applications"
   homepage "https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS"
-  url "https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_78_RTM/src/nss-3.78.tar.gz"
-  sha256 "f455f341e787c1167328e80a84f77b9a557d595066dda6486a1874d72da68800"
+  url "https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_79_RTM/src/nss-3.79.tar.gz"
+  sha256 "ebdf2d6a96613b6fe70ad579e9f983e0e94e0110171cfb2999db633d3394a514"
   license "MPL-2.0"
 
   livecheck do
