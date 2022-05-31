5b726e0992b3eda6142bd3dfcf6f4f44914adaa2
owine <owine@users.noreply.github.com>
2022-05-31 14:55:21 -0500

    assimp 5.2.4
    Closes #102698.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/assimp.rb
+++ Formula/assimp.rb
@@ -1,8 +1,8 @@
 class Assimp < Formula
   desc "Portable library for importing many well-known 3D model formats"
   homepage "https://www.assimp.org/"
-  url "https://github.com/assimp/assimp/archive/v5.2.3.tar.gz"
-  sha256 "b20fc41af171f6d8f1f45d4621f18e6934ab7264e71c37cd72fd9832509af2a8"
+  url "https://github.com/assimp/assimp/archive/v5.2.4.tar.gz"
+  sha256 "6a4ff75dc727821f75ef529cea1c4fc0a7b5fc2e0a0b2ff2f6b7993fe6cb54ba"
   license :cannot_represent
   head "https://github.com/assimp/assimp.git", branch: "master"
 
