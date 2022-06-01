e9de6825db13a98a97c5a855c266dd3a04c18608
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-01 05:38:12 +0000

    grails 5.1.8
    Closes #102714.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v5.1.7/grails-5.1.7.zip"
-  sha256 "4a4f95bee132cd9f6c49e23d7ceb87826812bbcf568eb8a22bfe9453f796a9a8"
+  url "https://github.com/grails/grails-core/releases/download/v5.1.8/grails-5.1.8.zip"
+  sha256 "c2c8f48dc8f6f95033a80a3df98298cd6c0e77494269eff4a45505825b41d846"
   license "Apache-2.0"
 
   livecheck do
