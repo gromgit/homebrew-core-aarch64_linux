51beb42deaa9df1c7f6caae90ccab4ca98c16380
Ben Iofel <ben@flow.io>
2022-06-01 16:17:57 -0400

    apibuilder-cli 0.1.43
    Closes #102776.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -1,8 +1,8 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.42.tar.gz"
-  sha256 "bffdf934888dd9ef78a5a0b029fea743732899551cfbbe5d8203222718ffe4f4"
+  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.43.tar.gz"
+  sha256 "69c8c100ba9d56e83c146b5338c2a68c189fc6d1ca2eb184f357a84091224077"
   license "MIT"
 
   bottle do
