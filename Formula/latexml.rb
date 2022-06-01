5d2e39cceb5f5f9908c1faa59523a4bb18069287
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-06-01 00:37:07 +0000

    latexml: update 0.8.5 bottle.

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -12,11 +12,13 @@ class Latexml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1baf39e551bf724eec7bfa7f63e6f2f80307c3eb21f53ff05fad1bcd620a6ef9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5311fc896eb2d27a5fa149bea4d30e6be0f6693cde9da9709b32f11212f726e1"
-    sha256 cellar: :any_skip_relocation, catalina:      "3af6c43c91d733210e1e75db13093c72aa91addc1cc8ae94cd716de6ec73ea37"
-    sha256 cellar: :any_skip_relocation, mojave:        "f51f58dc03c62130191b1bf2bb223459da93cdb7a7a1c3052a227a4bdc67dfe9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8cfc70adb30f6d9427642a94fa48618b2242bcb2d11ec3c6256e6313fb12694"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6f602fd9d3cd02c6a120158cde3a9e997dc67f85092e64ebdaf9b593f568256f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1baf39e551bf724eec7bfa7f63e6f2f80307c3eb21f53ff05fad1bcd620a6ef9"
+    sha256 cellar: :any_skip_relocation, monterey:       "6841adcaa396b0187fde550a4eca7bdcf20563fe78c0197c85b2c265c24cda69"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5311fc896eb2d27a5fa149bea4d30e6be0f6693cde9da9709b32f11212f726e1"
+    sha256 cellar: :any_skip_relocation, catalina:       "3af6c43c91d733210e1e75db13093c72aa91addc1cc8ae94cd716de6ec73ea37"
+    sha256 cellar: :any_skip_relocation, mojave:         "f51f58dc03c62130191b1bf2bb223459da93cdb7a7a1c3052a227a4bdc67dfe9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8cfc70adb30f6d9427642a94fa48618b2242bcb2d11ec3c6256e6313fb12694"
   end
 
   uses_from_macos "libxml2"
