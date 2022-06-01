efcd7186829254cc065969d920ce26ad11a92e2a
Porkepix <porkepix@gmail.com>
2022-06-01 09:55:23 +0200

    alsa-lib 1.2.7
    Closes #102725.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/alsa-lib.rb
+++ Formula/alsa-lib.rb
@@ -1,8 +1,8 @@
 class AlsaLib < Formula
   desc "Provides audio and MIDI functionality to the Linux operating system"
   homepage "https://www.alsa-project.org/"
-  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.6.1.tar.bz2"
-  sha256 "ad582993d52cdb5fb159a0beab60a6ac57eab0cc1bdf85dc4db6d6197f02333f"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.tar.bz2"
+  sha256 "8814e61f7ec6812c76e23a85cab00e0b0d3bba40816af36b726beb1bc04c74a7"
   license "LGPL-2.1-or-later"
 
   livecheck do
