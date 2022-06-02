384aef58b7af6791646bb315363ddc93022d1a66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-02 22:04:34 +0000

    simdjson: update 2.0.3 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -7,12 +7,12 @@ class Simdjson < Formula
   head "https://github.com/simdjson/simdjson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2d56a7a10bf960005696106b34b925e2351facbb0ad46c1c0e7e17a2927a8a17"
-    sha256 cellar: :any,                 arm64_big_sur:  "1ba6d99b2c0c08b51c406b06ba0a304b2032d02999d3f57318395b4906d5c9b9"
-    sha256 cellar: :any,                 monterey:       "edea3e6fb9e7227a6e07a4d88637e657b4bcef21eb7c7ec5291e03177a972e32"
-    sha256 cellar: :any,                 big_sur:        "35ce3872381e3a1cd7044a38baca0f44524b9614628e2fe0a53a84473f2665de"
-    sha256 cellar: :any,                 catalina:       "295f10b13635009b5951d2f777b768a525fde7e29a443438099a86cbe35cc395"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea4ea7e76ef351a54d809e56d5b92b05af7e86000ce9df13e85f620b989c59db"
+    sha256 cellar: :any,                 arm64_monterey: "b69ccb868a8f06dc6bcbebfcaf1b3dd005a983941253fff7842d1845e63ef37c"
+    sha256 cellar: :any,                 arm64_big_sur:  "7dfd01f82fb1b19dbed13fccda45c677f99640335aeb7b0b286664c653ff6289"
+    sha256 cellar: :any,                 monterey:       "247da92775194238330ebe32d5e5faf810f12cb9aa4030e3d56a200054bec430"
+    sha256 cellar: :any,                 big_sur:        "2bcde7afd6beaaace35bfca1fd872576eedf4e990908f4cf15b939191e7da270"
+    sha256 cellar: :any,                 catalina:       "7235cd30c8b09d65cd0a8eaf6a1145e16fcc67711ed42dd7290c72d039afb4c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51efb343063b7846fc36d26d57d05ba10537cd033801e2870b507dbfd79d8d47"
   end
 
   depends_on "cmake" => :build
