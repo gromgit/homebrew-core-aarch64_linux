633b18264b82fa0ec575bf7a89e3c3b6b0a0d83a
Porkepix <porkepix@gmail.com>
2022-06-02 12:29:56 +0200

    octosql 0.7.3
    Closes #102811.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/octosql.rb
+++ Formula/octosql.rb
@@ -1,8 +1,8 @@
 class Octosql < Formula
   desc "SQL query tool to analyze data from different file formats and databases"
   homepage "https://github.com/cube2222/octosql/"
-  url "https://github.com/cube2222/octosql/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "60a111c38be45c7c987605e7c4c16ba1821de24a2a046aba4234f3343061f3e6"
+  url "https://github.com/cube2222/octosql/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "859af30e263756d9fd9bd5cd6bc987ab057de7c9a89794be80371f6e4355ae11"
   license "MPL-2.0"
   head "https://github.com/cube2222/octosql.git", branch: "main"
 
