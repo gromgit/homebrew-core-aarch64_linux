a1215933b6e6e33967de5f2f5c09c3bee1144cf8
Rui Chen <rui@chenrui.dev>
2022-06-02 16:56:27 +0000

    monit: update 5.32.0_1 bottle.

--- Formula/monit.rb
+++ Formula/monit.rb
@@ -12,12 +12,12 @@ class Monit < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "daa3ed9e9e1a2fe150082565d8d98f88f22d9812fcfe106e03fa7044e6b9ee62"
-    sha256 cellar: :any,                 arm64_big_sur:  "7f7cbfd474af9c46ed2d3bf33d7d33439d26f2346fa0f24fc32a00bc9557c92f"
-    sha256 cellar: :any,                 monterey:       "e9c9b1d3fcbdd3aebb24bab99370239a6b3c8c455dc6693883ceafe50fca011d"
-    sha256 cellar: :any,                 big_sur:        "50715f7eab06cfdfd51030b020b62aa7409e0cad8ece6e176804adf536e34d69"
-    sha256 cellar: :any,                 catalina:       "46170c651cc00edb1916a89bf4943131cf7662db499fef5337c8744f5f641711"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db93e068d4d6bf3525b3d6a8993dfd0ddccc82b154921fab3ca0be0522ce885d"
+    sha256 cellar: :any,                 arm64_monterey: "21eb3d11227e8bda20795b418f8f355b5694a363e0f8b8694d2559475f2d7421"
+    sha256 cellar: :any,                 arm64_big_sur:  "aa19c34d3170a64dc8c825ea17f20c2a7f5b622b6344a0fe6b7e6a8a725a4733"
+    sha256 cellar: :any,                 monterey:       "0224a3b0547aac17b806f2c7a7039dc69c4b4189e58f7738861f5fb853a2d10c"
+    sha256 cellar: :any,                 big_sur:        "a6ed1f2d8263769e3796f296a108656f07e854887f4ac648d0f69711a44699f0"
+    sha256 cellar: :any,                 catalina:       "76e57e50992213d7f62dc6d33e622013f4252bb2ebb99f1c0dacb89898d04fe8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa1e9ac3c7c0c129debf80185f30ec4b8dcea96a392649edd46f7bea7188b6f4"
   end
 
   depends_on "openssl@1.1"
