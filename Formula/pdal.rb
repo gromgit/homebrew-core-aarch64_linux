14880f20cad2bb23390ddddc8d228abdb22b16f0
Rui Chen <rui@chenrui.dev>
2022-06-02 21:43:12 +0000

    pdal: update 2.4.1_1 bottle.

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -19,12 +19,12 @@ class Pdal < Formula
   end
 
   bottle do
-    sha256                               arm64_monterey: "2cc2d40e46a6ba825e7433a891dda892aa99f096ad9f4a7bbe5e579181be1c82"
-    sha256                               arm64_big_sur:  "de708dfbb95cd7909f75bb248d5b277f69c6b40c8866ff177609d7f76c64b7b1"
-    sha256                               monterey:       "479ad24653ab0f409e4d450e4a960937e83aa982ebed5957594b09cb2d656886"
-    sha256                               big_sur:        "f78507d9035a9ff5ad7f4b124c0af86a9b99c8a0ccc2ee55324067b7b8cc8fb2"
-    sha256                               catalina:       "e0bfb84ef816bad5e47a63d7ab82335c3dcdaf4e776217d01d71debf92184937"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06958cd3f681635b2ec5501394d63326c3c84b7b768c439414b35dcdfb508ece"
+    sha256                               arm64_monterey: "5bbac712512fe9720c36f7589d04a8183537e372781964fce6d05b370ddc661e"
+    sha256                               arm64_big_sur:  "aa03c24450cc9f5cd7c67a08873e7c2ef61872307b8f3b8d501500295795e739"
+    sha256                               monterey:       "b76a0c6665c4a3d1a535755f6f6df8c3c9dc7b14d50d128605765a23ba7c3424"
+    sha256                               big_sur:        "4005f577331532b9c2d909af46540e0d24dc664d3c7cc33e2663093f106a5b3d"
+    sha256                               catalina:       "e2357208c86665de7300dc9d2587edad80d8c332d7baf084a24f3c514ba5fe05"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5435c0ec7fa4800c6c9600ca02e7a7946cd2e7f23a3be6e90db05135693cb29a"
   end
 
   depends_on "cmake" => :build
