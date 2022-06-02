1f5283df428f4ad70e296e63d51283a002d713b7
Rui Chen <rui@chenrui.dev>
2022-06-02 12:48:55 -0400

    osc 0.179.0
    Closes #102839.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osc.rb
+++ Formula/osc.rb
@@ -3,8 +3,8 @@ class Osc < Formula
 
   desc "Command-line interface to work with an Open Build Service"
   homepage "https://openbuildservice.org"
-  url "https://github.com/openSUSE/osc/archive/0.178.0.tar.gz"
-  sha256 "c616bf2824422c0a04f9bce0f53f34ffbbcf192e185b85c09767c59389befce9"
+  url "https://github.com/openSUSE/osc/archive/0.179.0.tar.gz"
+  sha256 "543a9e1a4f2dc722a5228b41a8da92867a35fa52f9701dc213108fa9b35085e8"
   license "GPL-2.0-or-later"
   head "https://github.com/openSUSE/osc.git", branch: "master"
 
