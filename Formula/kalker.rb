9f4119552403295b2e6bef4f09b384f2c92166bc
Rui Chen <rui@chenrui.dev>
2022-06-02 21:44:00 -0400

    kalker 2.0.0
    * kalker 2.0.0
    * kalker: fix test
    
    Closes #102865.
    
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kalker.rb
+++ Formula/kalker.rb
@@ -1,8 +1,8 @@
 class Kalker < Formula
   desc "Full-featured calculator with math syntax"
   homepage "https://kalker.strct.net"
-  url "https://github.com/PaddiM8/kalker/archive/v1.1.0.tar.gz"
-  sha256 "4332563b18e1c2c8c54871e46f40e55791d3f86612d4651c69b690827051a484"
+  url "https://github.com/PaddiM8/kalker/archive/v2.0.0.tar.gz"
+  sha256 "092ab13726515125ec3664c15e61fa7b8eec09ad590f0c5ef00df6e33b3b3da7"
   license "MIT"
 
   bottle do
@@ -25,6 +25,6 @@ class Kalker < Formula
   end
 
   test do
-    assert_equal shell_output("#{bin}/kalker 'sum(1, 3, 2n+1)'").chomp, "15"
+    assert_equal shell_output("#{bin}/kalker 'sum(n=1, 3, 2n+1)'").chomp, "15"
   end
 end
