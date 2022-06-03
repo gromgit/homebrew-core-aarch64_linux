97fb5ef89d4c1093213f12667598e4ca96177133
Dustin Brown <dustin@dolthub.com>
2022-06-03 14:27:43 -0700

    dolt 0.40.5
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #102916.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -1,8 +1,8 @@
 class Dolt < Formula
   desc "Git for Data"
   homepage "https://github.com/dolthub/dolt"
-  url "https://github.com/dolthub/dolt/archive/v0.40.4.tar.gz"
-  sha256 "29e748422207a1b67abe2b04f1d81d2a123c750c662d7de01007b0f6c13c9f5f"
+  url "https://github.com/dolthub/dolt/archive/v0.40.5.tar.gz"
+  sha256 "6c1fbf2caed1f1adebce5b5bf5e33d8a06a6bf67c3a47f71775655f623803a8b"
   license "Apache-2.0"
 
   livecheck do
