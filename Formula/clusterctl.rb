91d6c15c0ccfd94ea8809601444d324c07a421b6
Christophe Jauffret <reg-github@geo6.net>
2022-06-03 21:10:44 +0200

    clusterctl 1.1.4
    Closes #102914.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clusterctl.rb
+++ Formula/clusterctl.rb
@@ -2,8 +2,8 @@ class Clusterctl < Formula
   desc "Home for the Cluster Management API work, a subproject of sig-cluster-lifecycle"
   homepage "https://cluster-api.sigs.k8s.io"
   url "https://github.com/kubernetes-sigs/cluster-api.git",
-      tag:      "v1.1.3",
-      revision: "31146bd17a220ef6214c4c7a21f1aa57380b6b1f"
+      tag:      "v1.1.4",
+      revision: "1c3a1526f101d4b07d2eec757fe75e8701cf6212"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api.git", branch: "master"
 
