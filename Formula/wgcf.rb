f23ece86471c69da24e968792b238b1c10c02f75
Rui Chen <rui@chenrui.dev>
2022-06-03 11:26:45 -0400

    wgcf 2.2.15
    Closes #102906.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -1,8 +1,8 @@
 class Wgcf < Formula
   desc "Generate WireGuard profile from Cloudflare Warp account"
   homepage "https://github.com/ViRb3/wgcf"
-  url "https://github.com/ViRb3/wgcf/archive/v2.2.14.tar.gz"
-  sha256 "64b6e62cc9b7ca1b91757ee067e7c6adf923faff64e3daeebbf07ad817ce73aa"
+  url "https://github.com/ViRb3/wgcf/archive/v2.2.15.tar.gz"
+  sha256 "b12971018c40d0a04492a9da9e9fea393394291044045e0117ec292364de1b57"
   license "MIT"
   head "https://github.com/ViRb3/wgcf.git", branch: "master"
 
