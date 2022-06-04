3d7ff70251d50ac02413113b19b49f97132f557f
Adrian Ho <the.gromgit@gmail.com>
2022-06-04 16:22:09 +0800

    hello: update 2.12 bottle.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -6,12 +6,8 @@ class Hello < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "be8bba73844b60a684885c91b61bd131a8655ae5b1d748dd7562f878e78fa8f7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85d1e1b66826f3e5b9767af9f2fad00e5395a0c559519d4a55a0136518c81dbd"
-    sha256 cellar: :any_skip_relocation, monterey:       "f7883fe8258feef2ff49fc3b2f8c7a9665a6c51afe34042b7dcb69d805b1376b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2e4325a96ed1cbfe67a93a4df85b0edb1aa8d55a7d2c33396b95801b2d6dc047"
-    sha256 cellar: :any_skip_relocation, catalina:       "9f70c809590bc170cd73273c95f21bb1b78c7bd9d0b9cbb8b191f17ff7ce8be6"
-    sha256                               x86_64_linux:   "d5a213b36a6dedc2dc2194e364e65f74208e7ea46fbd78d2442791e703a61e7a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hello"
+    sha256 aarch64_linux: "b721ffedcdc1fe4e54dc1d799fe72d6970a0b38c26f8bff10e35e5b9c3f3fe06"
   end
 
   conflicts_with "perkeep", because: "both install `hello` binaries"
