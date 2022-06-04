ee93ac4a22f78cc121a75dcad5f0f084dee431ce
Adrian Ho <aho+rpi9@03s.net>
2022-06-04 17:07:26 +0800

    jetty: no rebuilding required

--- Formula/jetty.rb
+++ Formula/jetty.rb
@@ -1,9 +1,9 @@
 class Jetty < Formula
   desc "Java servlet engine and webserver"
   homepage "https://www.eclipse.org/jetty/"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-distribution/9.4.46.v20220331/jetty-distribution-9.4.46.v20220331.tar.gz"
-  version "9.4.46.v20220331"
-  sha256 "ea018b057102181d26ce4e05d358fc7bbe07393cc6d0f80add78ec29c60d3ed9"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-distribution/9.4.45.v20220203/jetty-distribution-9.4.45.v20220203.tar.gz"
+  version "9.4.45.v20220203"
+  sha256 "c26334dea02736c8840ec2e45b224a6486f748f091182c53b202815b0a29300c"
   license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
@@ -12,10 +12,7 @@ class Jetty < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "7a72038f30df909b4a4549cc985853ad68dc87d9ca6d73d3871808f134415a67"
-    sha256 cellar: :any,                 big_sur:      "7a72038f30df909b4a4549cc985853ad68dc87d9ca6d73d3871808f134415a67"
-    sha256 cellar: :any,                 catalina:     "7a72038f30df909b4a4549cc985853ad68dc87d9ca6d73d3871808f134415a67"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7faa56d4513e249f8bd7e8eb2fdc7452e81cd924f0f2afbbabbdf04879a631a1"
+    sha256 cellar: :any, all: "822a768ebe1e8467a2fcfc3add2ab8747a0216435c9531107a820e2ac1db8b5f"
   end
 
   # Ships a pre-built x86_64-only `libsetuid-osx.so`.
@@ -26,31 +23,28 @@ class Jetty < Formula
     libexec.install Dir["*"]
     (libexec/"logs").mkpath
 
-    env = Language::Java.overridable_java_home_env
-    env["JETTY_HOME"] = libexec
-    Dir.glob(libexec/"bin/*.sh") do |f|
-      (bin/File.basename(f, ".sh")).write_env_script f, env
+    bin.mkpath
+    Dir.glob("#{libexec}/bin/*.sh") do |f|
+      scriptname = File.basename(f, ".sh")
+      (bin/scriptname).write <<~EOS
+        #!/bin/bash
+        export JETTY_HOME='#{libexec}'
+        export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+        exec #{f} "$@"
+      EOS
+      chmod 0755, bin/scriptname
     end
   end
 
   test do
-    http_port = free_port
-    ENV["JETTY_ARGS"] = "jetty.http.port=#{http_port} jetty.ssl.port=#{free_port}"
+    ENV["JETTY_ARGS"] = "jetty.http.port=#{free_port} jetty.ssl.port=#{free_port}"
     ENV["JETTY_BASE"] = testpath
-    ENV["JETTY_RUN"] = testpath
-    cp_r Dir[libexec/"demo-base/*"], testpath
-
-    log = testpath/"jetty.log"
-    pid = fork do
-      $stdout.reopen(log)
-      $stderr.reopen(log)
-      exec bin/"jetty", "run"
-    end
-
+    cp_r Dir[libexec/"*"], testpath
+    pid = fork { exec bin/"jetty", "start" }
+    sleep 5 # grace time for server start
     begin
-      sleep 20 # grace time for server start
-      assert_match "webapp is deployed. DO NOT USE IN PRODUCTION!", log.read
-      assert_match "Welcome to Jetty #{version.major}", shell_output("curl --silent localhost:#{http_port}")
+      assert_match(/Jetty running pid=\d+/, shell_output("#{bin}/jetty check"))
+      assert_equal "Stopping Jetty: OK\n", shell_output("#{bin}/jetty stop")
     ensure
       Process.kill 9, pid
       Process.wait pid
