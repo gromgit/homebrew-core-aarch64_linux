8e31957f4adc31af5f0228baa6a6f598e6938ffd
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 23:32:56 +0800

    bosh-cli: update 7.0.0 bottle.

--- Formula/bosh-cli.rb
+++ Formula/bosh-cli.rb
@@ -7,12 +7,8 @@ class BoshCli < Formula
   head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3dcb91010cbf391be2a590cabfea690f92e3d3c17fa760a7009ef92857338058"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1cef0fe5fe99d5860ccc27905c45d48115b4c3ce4555dec4cc65f64a88f7f146"
-    sha256 cellar: :any_skip_relocation, monterey:       "59c2009ad2cc5e37fd7197c2215cf26ec78a1200981e93fa13272ee610400368"
-    sha256 cellar: :any_skip_relocation, big_sur:        "29182c031aff1b8646d025fadd060fc814fec83c6c9bd4c282c5de00477d08be"
-    sha256 cellar: :any_skip_relocation, catalina:       "e34c90845644e3277e0b2f7763a70dd5f945db175ff839ecd4a4610e4bedaf5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6a382bc3c2548212df61b7c0ebf60c17aa7e16737195d20beb93adb33c3e75f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bosh-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a9d91f70d19564abe9cbf3ae8139fe3319bf4a51fb9417929e701976979d2a85"
   end
 
   depends_on "go" => :build
