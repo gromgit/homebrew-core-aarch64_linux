fec45b4dfd70bee9a01098fa389fcaef37ceb7bc
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 01:44:55 +0800

    calicoctl: update 3.22.2 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -13,12 +13,8 @@ class Calicoctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b62d3d4c1a956f06c0d588a5e1b40c5e218fb2c657f8c0c3ce3776bcec530f19"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d27be3fc96c7eca089decb0b037e34cdf19e8881a3cbf79374949726878c2d07"
-    sha256 cellar: :any_skip_relocation, monterey:       "f8bfd561792dd43ab797e70a06fb01d74cfaa3051be4799d07ce2b4ac08a398d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4fa3981a0d9d66df61ae06b7fcc6f88e7d4aaeb67470fae8d8f88468806f489e"
-    sha256 cellar: :any_skip_relocation, catalina:       "d0780e06a66fd68cbc360479deebbe8f90cd6652c1a748bb911360860fb681a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4bc8fafb49ae87a4fd0daaed483e3e1099f5dd8c74ad3d51bd87d971acc1013d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/calicoctl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "852e424bef250b327080a84adf8a93576d6280fe930a4f3066cf9dd263d3c77e"
   end
 
   depends_on "go" => :build
