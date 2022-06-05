7a2d2bc47b65acbda22408572fca2aa5a5081b3a
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 23:56:23 +0800

    brigade-cli: update 2.3.1 bottle.

--- Formula/brigade-cli.rb
+++ Formula/brigade-cli.rb
@@ -8,12 +8,8 @@ class BrigadeCli < Formula
   head "https://github.com/brigadecore/brigade.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3d1422953622717b44b91c67895c603fbb441df317529b9092ef4265b495e373"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4fe0721183b16c2a77f22df01ebc08b34e51ed8f9247da453803b3789b7d530b"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f88d6c5369c1309a27a5c767d8a875dfe44c7d5bf066569efb5adbc151779fa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1bc20d15b1463693dbd245c1a79d6390b3ba941d8f50660a4fba6311fcfdc5aa"
-    sha256 cellar: :any_skip_relocation, catalina:       "594117c8ed56dd5c62837f97d75d09a5ad6604275edaaeebd5cd3028b348420e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d3d3e936a6acba1ca751714470016f2f0299964aa4ff8266728ea5f06ee265f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/brigade-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cb6ffd59671a32caafa3bbfd96113eb6b5145d6bec8f0262d9a6fe114e2d1a51"
   end
 
   depends_on "go" => :build
