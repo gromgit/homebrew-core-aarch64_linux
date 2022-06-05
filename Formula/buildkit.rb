91d53bc48d8d67121edeb0a905059b17da7bdc3c
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 00:27:09 +0800

    buildkit: update 0.10.3 bottle.

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -13,12 +13,8 @@ class Buildkit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ca4cb92d901ac8ccbcadf27a2ec7aba789816346a99ca68f8700bb21150da70"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b2cb56f1d346308e379052a3d02d2d9123d07068d97bacb25763d0830186d3eb"
-    sha256 cellar: :any_skip_relocation, monterey:       "e9b62fcea0f70acabdaf22a63c4bbc655653a418e8eaee037ab446990e1f0721"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6d2ee0328e9b27729d7e00e659ab64edbd4ae972376c9775eee18dc9a4fcedd7"
-    sha256 cellar: :any_skip_relocation, catalina:       "d3916f6b179834f2ba3df2dbc0bece7a1e901da24530e6d4688644688281e45b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40f9e1372206c7663c8806406f88028036b19c0320ec0ff806e2d93fca0d37eb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/buildkit"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b22bfcf66b73d2fe4587f7f804d2d6b9a5d44c1aac3ac108a9d0e37b34882ea4"
   end
 
   depends_on "go" => :build
