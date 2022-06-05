393e8e5a674ee73acdd77695e6869c9f6e84c33e
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 14:29:57 +0800

    aespipe: update 2.4f bottle.

--- Formula/aespipe.rb
+++ Formula/aespipe.rb
@@ -11,14 +11,8 @@ class Aespipe < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "67e4984fc6794d5e6c5b973b7faa472d86b1e434d3c4ca8757ce8e7ab7625e4d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "204ddb0b9c6fde98f4bdec7c3c93aa2ad95cde565dd22538f2c61a55875a398e"
-    sha256 cellar: :any_skip_relocation, monterey:       "3182b08b5f9aa35f3a56e4012da53b569f7e4458e86c0c522957b26daf247e0d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b94579255152f8761049784697e757d2399f075bb77b7c194741311aad2943c3"
-    sha256 cellar: :any_skip_relocation, catalina:       "c96c3f1ba5bcd7672630d7c9d693cb5d9333e3473ecdca6771290a68ac54db2e"
-    sha256 cellar: :any_skip_relocation, mojave:         "f52e6c3afc951ca588522d8073b62300113a30cb6d3927a25de643cc10622d74"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "00d7cb8240e8e1beb4b8cf701bf38961531df8a9f2d497c4ff5a95747ac3dbae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff7fc1cc1e3cdc08237e3136d528c11c80fdc6c0ded76d812f9d3c141046bc00"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aespipe"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e377424de306dca0798f0d0d7e7de79d42c08b70452c20cc3f8f8972391f4d98"
   end
 
   def install
