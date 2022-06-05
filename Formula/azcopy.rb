829f27f33f0d9ece07ade295ba2b46507ecc4939
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 19:22:05 +0800

    azcopy: update 10.14.1 bottle.

--- Formula/azcopy.rb
+++ Formula/azcopy.rb
@@ -6,12 +6,8 @@ class Azcopy < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c088ae07f46e676d90f6c9dcd104b2c77d5e8174a8cd06e890d8c0f0059e50d0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a051632c48668b7665e0626da21fd1db04d13bce85568032cbdbd7c09c0a405d"
-    sha256 cellar: :any_skip_relocation, monterey:       "6a9004e902d55f71dfe5d533b245d2327898213941448e0a02ef12d3773a7233"
-    sha256 cellar: :any_skip_relocation, big_sur:        "829202256ed5cfb0c7b08c130dec058912d0d78e5fbd8163a053369747690d59"
-    sha256 cellar: :any_skip_relocation, catalina:       "e7a53b2e80471386c06b5b6b0c257cbeeb1eb2e1f4ffdcd9c9232323d7e4ccc3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67c0752afecd0bfeecf7485616612888e5c7d9690993fa9d476bd394c8a83625"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/azcopy"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9576de2e2a9c837aa39421e2800ee679d99b7ca0a25c5f60f1aa3ab90d093ceb"
   end
 
   depends_on "go" => :build
