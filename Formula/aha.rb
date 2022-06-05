bd8c2a2f6e733fab3f75f57f701653138af922de
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 14:49:41 +0800

    aha: update 0.5.1 bottle.

--- Formula/aha.rb
+++ Formula/aha.rb
@@ -7,14 +7,8 @@ class Aha < Formula
   head "https://github.com/theZiz/aha.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "638f024391f63bcaeb96fc614f0bd1d18ca42a42db7aaaacf939c5a473aa70b5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fed59f6650f5b40100644b0baf1bee57b0b3ca6a05d413af70350d9d0eaa8441"
-    sha256 cellar: :any_skip_relocation, monterey:       "ea27c4b1e45d668521568a5d5e425dc607aadd74ac7378a6e100607a90330cbf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "68281908da802d600716d979b84a47109fabd6770da0cc9a1a689b609b2024b8"
-    sha256 cellar: :any_skip_relocation, catalina:       "bcd5f7ea0e30795e05719351823769f9a7ac434e57bf09cb738eeef50c0f0f85"
-    sha256 cellar: :any_skip_relocation, mojave:         "b8def8fe2809928ffbf3ae5746f1157bacfef12e720d0eef798b4d77902d8f4f"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9de609b23501a93b6fc39422bc51f4b79c31eba3c39272a06f2710aa7e2d6a3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c018344a8a20792dbcc444893c62279ea87a97539e1cf0141ddf7b2cf538a9fb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aha"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "020b49fb62093391aec8ef75d3d23b09aa5e9ab0c1f1e9af8ec2039c6fd00364"
   end
 
   def install
