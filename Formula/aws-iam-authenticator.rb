350c3bab7103d859fe3239e89461031ce7958d47
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 19:05:38 +0800

    aws-iam-authenticator: update 0.5.7 bottle.

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -13,12 +13,8 @@ class AwsIamAuthenticator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b1ea442e5f2e2f476ab7e3b8090d26b747fc4c29297204c018242d829a0c9ec5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "150311fcae7892a872ae8da3c736e0206631a32f1820a02fd48d17b741b7f8ca"
-    sha256 cellar: :any_skip_relocation, monterey:       "cfd4fa055f8bcf948df948270bfb69a740869aa25b16cb8574ca4c8d208676c6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b5a8a0217de3f690af033c17326e81ab2955c5bd9e696fae5a1fce00f1f2a915"
-    sha256 cellar: :any_skip_relocation, catalina:       "48cdc1062c9af6a88bf473663573c42024f88b635516a7ce30fa17d96cc79a5c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "98db0cba058afbe8736744f3b168b4e346a53211d1199b283ecaa454197eacb9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aws-iam-authenticator"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8f1b468a6522c3101eac38bc72ef572da29466694280d69cb3e1b6e0bb43f565"
   end
 
   depends_on "go" => :build
