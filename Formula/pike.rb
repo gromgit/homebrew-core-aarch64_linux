717701d1c194ea1d60e572b56079f218af458d42
danielnachun <daniel.nachun@gmail.com>
2022-06-05 14:19:21 -0700

    pike 8.0.1738
    Closes #103026.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pike.rb
+++ Formula/pike.rb
@@ -1,10 +1,10 @@
 class Pike < Formula
   desc "Dynamic programming language"
   homepage "https://pike.lysator.liu.se/"
-  url "https://pike.lysator.liu.se/pub/pike/latest-stable/Pike-v8.0.1116.tar.gz"
-  mirror "http://deb.debian.org/debian/pool/main/p/pike8.0/pike8.0_8.0.1116.orig.tar.gz"
+  url "https://pike.lysator.liu.se/pub/pike/latest-stable/Pike-v8.0.1738.tar.gz"
+  mirror "http://deb.debian.org/debian/pool/main/p/pike8.0/pike8.0_8.0.1738.orig.tar.gz"
   # Homepage has an expired SSL cert as of 16/12/2020, so we add a Debian mirror
-  sha256 "5020063c755bb182177750221013b199198a7f1eb8ff26c1fb879d375c755891"
+  sha256 "1033bc90621896ef6145df448b48fdfa342dbdf01b48fd9ae8acf64f6a31b92a"
   license any_of: ["GPL-2.0-only", "LGPL-2.1-only", "MPL-1.1"]
 
   livecheck do
@@ -25,6 +25,12 @@ class Pike < Formula
   depends_on "nettle"
   depends_on "pcre"
 
+  uses_from_macos "libxcrypt"
+
+  on_macos do
+    depends_on "gnu-sed" => :build
+  end
+
   on_linux do
     depends_on "jpeg"
   end
@@ -35,6 +41,11 @@ class Pike < Formula
 
     # Workaround for https://git.lysator.liu.se/pikelang/pike/-/issues/10058
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    # Use GNU sed on macOS to avoid this build failure:
+    # sed: RE error: illegal byte sequence
+    # Reported upstream here: https://git.lysator.liu.se/pikelang/pike/-/issues/10082.
+    ENV.prepend_path "PATH", Formula["gnu-sed"].libexec/"gnubin" if OS.mac?
 
     system "make", "CONFIGUREARGS='--prefix=#{prefix} --without-bundles --with-abi=64'"
 
