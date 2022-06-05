2dd0733b306deefa4043db5d5cfb6ec0fec4f9bb
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 21:37:54 +0800

    benthos: update 4.0.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -7,12 +7,8 @@ class Benthos < Formula
   head "https://github.com/benthosdev/benthos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2fee406d14cd3e8b3e78ca95acfc73c7ff0b6cceca2384817602f6e0effb85b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "34a2d72ff883732b8cb87eef183edda87387ead728379aa08109c7a01b6f9e0b"
-    sha256 cellar: :any_skip_relocation, monterey:       "033597d7a79de364cc7ed35ee17f5b38741624fa3c7a764c2e076c87585dc7a8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3ac7aede7de15c1740cbfe4c62e90dc1b1b5ef85d61de2bae405260211e47f8d"
-    sha256 cellar: :any_skip_relocation, catalina:       "4a7a4cbeecd8d1830953447a8e246fdaf01dcabfb181ef1f576c1b656e3868d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6571a773a8b57ff604f72f073e493cca7ef6ea113a99f7ea959a69ada704166"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/benthos"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ddfac683582214a3e02c3de6328a070442bf3021cf8bde1fc64260b2e6f1f47d"
   end
 
   depends_on "go" => :build
