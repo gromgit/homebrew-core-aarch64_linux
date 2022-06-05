f90c025ab91bff9e6a58f03269afa941e28eb18f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-05 05:08:08 +0000

    cargo-bloat 0.11.1
    Closes #102978.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-bloat.rb
+++ Formula/cargo-bloat.rb
@@ -1,8 +1,8 @@
 class CargoBloat < Formula
   desc "Find out what takes most of the space in your executable"
   homepage "https://github.com/RazrFalcon/cargo-bloat"
-  url "https://github.com/RazrFalcon/cargo-bloat/archive/v0.11.0.tar.gz"
-  sha256 "16be80bb0486cb0a2fd9164402b2d7449b07c18738a6259d42035c738d3b4a32"
+  url "https://github.com/RazrFalcon/cargo-bloat/archive/v0.11.1.tar.gz"
+  sha256 "4f338c1a7f7ee6bcac150f7856ed1f32cf8d9009cfd513ca6c1aac1e6685c35f"
   license "MIT"
   head "https://github.com/RazrFalcon/cargo-bloat.git", branch: "master"
 
