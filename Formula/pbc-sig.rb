67c1318bd160556c85db1fa437d114f969b34d42
danielnachun <daniel.nachun@gmail.com>
2022-06-05 12:14:50 -0700

    pbc-sig: fix flat namespace usage
    Closes #103023.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pbc-sig.rb
+++ Formula/pbc-sig.rb
@@ -21,10 +21,20 @@ class PbcSig < Formula
 
   depends_on "pbc"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   # https://groups.google.com/forum/#!topic/pbc-devel/ZmFCHZmrhcw
   patch :DATA
 
   def install
+    # Disable -fnested-functions CFLAG on ARM, which will cause it to fail with:
+    # incompatible redeclaration of library function 'pow'
+    # Reported upstream here: https://groups.google.com/g/pbc-devel/c/WXwVWKoouj0.
+    inreplace "configure", "-fnested-functions", "" if Hardware::CPU.arm?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
