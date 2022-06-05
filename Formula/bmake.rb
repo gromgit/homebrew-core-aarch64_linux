41cbb33998654aaaeee3a1b448ae245b00a9aef3
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 23:14:29 +0800

    bmake: update 20220418 bottle.

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -11,12 +11,8 @@ class Bmake < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea6d347e27b41d0fe9e672910c829123de6563da61b665b2d0f17a45d7f14994"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "801ad5c98a414c1efc6be2d83ffe15c2c25af0ef3d973421ea2a1ed6864856bc"
-    sha256                               monterey:       "cc70133aa230c37eca6973c9759b8d08ffcc1d3ffc9f147f8ac73cc978d075b9"
-    sha256                               big_sur:        "467fbaa19a541763345a973dc9041a2a88c6ad4eef1e31bf660814c32bdff409"
-    sha256                               catalina:       "a9d6f26a78e81862230f1cee75746431fb049235dd811028b032fe6f52716bec"
-    sha256                               x86_64_linux:   "e420bc8442cf5eb48296e98d142a9de2000baf2b600444c1f6cd1e4937f2725d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bmake"
+    sha256 aarch64_linux: "d8d32542416e2c3e80af9951601b0a566e9eeed0ddc9412aa248bb0575028037"
   end
 
   def install
