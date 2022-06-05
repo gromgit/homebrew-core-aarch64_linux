24527b20bdfce95a6d8713fe359856be8537c103
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 05:02:19 +0800

    chroma: update 0.10.0 bottle.

--- Formula/chroma.rb
+++ Formula/chroma.rb
@@ -6,12 +6,8 @@ class Chroma < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3d760ced5f31db4e69703ba2fddff9cd69273ec31981531691704701fb6bbe55"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5c1f528c2f1e7f6f9f677578cd0922d2a891d2545be5450db684212ca4ba18f7"
-    sha256 cellar: :any_skip_relocation, monterey:       "2cc922fe4835d6b493ac83257a40abe3ce8ebfb8b0b849ddb8a16189e659be32"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5fb90ee11ea3f5fd3c19c8bca3aba69cc988c5e8a3cb606223e25444fa66b984"
-    sha256 cellar: :any_skip_relocation, catalina:       "35f46598926c60c2b90ce54ff2e91b1a6ef52548fa6402f58ea46dba11731152"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4514c41955707aefd692278cef659e252324170bcf0c61a4e9e324aca20d1a31"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/chroma"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6d2edd6153a88b705f28f504fd76ee8b456babf2496b4719a1eeae8938c00403"
   end
 
   depends_on "go" => :build
