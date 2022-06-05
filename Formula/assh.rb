e547ea1442335bd280a5718048240dff1e1840fe
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 17:54:50 +0800

    assh: update 2.12.2 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -7,12 +7,8 @@ class Assh < Formula
   head "https://github.com/moul/assh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "30be296faa0a138b81486bdd3a6cd885fdf7807c944aae1d7b7ed53fa0f7fdb9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f5d104eea6feedbbb5f94c88cb134ff42ac0d7d2bc51cdc3319e941807fa0155"
-    sha256 cellar: :any_skip_relocation, monterey:       "6db39cf5878d81d48e1075d07e64e26615f454db33a60ac39b3ebbda80e99f13"
-    sha256 cellar: :any_skip_relocation, big_sur:        "161366bff34f651097fbbe6bd182067cfcb24b37572237f91ef5c0748cf5e33f"
-    sha256 cellar: :any_skip_relocation, catalina:       "5346fbc61136dc4ad80fcda051669a45cc0334aa9005dfca2fa547e730ea54ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6615d9f78af09e09968affc05689a8f00c4a8454d9c008ea1126f461d43186fb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/assh"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "173ce6e265c74a07963cafb038672b951f5c1011e46787afff0cdf2d60db86e2"
   end
 
   depends_on "go" => :build
