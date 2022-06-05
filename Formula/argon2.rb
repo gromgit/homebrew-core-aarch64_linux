ce5b9cf3f90db1731005df738e9efdd24f4c8358
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 17:20:01 +0800

    argon2: update 20190702_1 bottle.

--- Formula/argon2.rb
+++ Formula/argon2.rb
@@ -8,15 +8,8 @@ class Argon2 < Formula
   head "https://github.com/P-H-C/phc-winner-argon2.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "498cea03c8c9f5ab7b90a0c333122415f0360c09f837cafae6d8685d6846ced2"
-    sha256 cellar: :any,                 arm64_big_sur:  "192f3381abe337df8af214cf4dccef2cbfaa9c88df489b5cf9276cea9f8c6080"
-    sha256 cellar: :any,                 monterey:       "decd61f1d853225582aaa70e9f67438c21f45105118d86ddb69a5e494311a841"
-    sha256 cellar: :any,                 big_sur:        "a9dd363964a2a633ace13aff04e4c5eac7e720d44faf377456de55396647ff13"
-    sha256 cellar: :any,                 catalina:       "f8e550c8597728bb9edc5a548497fd7b1219203932cd0f93ecc97a4fbf0bdad8"
-    sha256 cellar: :any,                 mojave:         "a76192a41826619fc399e7f6de5e6cb1c8a5fbe6bea4f2c1554daa830fa0e296"
-    sha256 cellar: :any,                 high_sierra:    "830016982e60870f50b3f6fc9a215d8cc4bda6061595f4883f7c11ab19ecba39"
-    sha256 cellar: :any,                 sierra:         "21889ac6ed40c792f1b372b5aa0d6b3be1be86577a4c1b06b08569124d2d0da2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58b4014f120e135a991f1023f210366ef3e9175a37a05a3fffb06a9fe3c23ff4"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/argon2"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cd3f57ef94354c02b1d3af783ea1d68f4777abb3faaa135b4eb5a42a96f03fb6"
   end
 
   def install
