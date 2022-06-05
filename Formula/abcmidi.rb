1956af4472603ee2af66ce5ec011a81473a9a6a0
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 13:26:47 +0800

    abcmidi: update 2022.05.05 bottle.

--- Formula/abcmidi.rb
+++ Formula/abcmidi.rb
@@ -11,12 +11,8 @@ class Abcmidi < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "911ce112dcb4e6e58aa2dbe59f07b88d787cb88dc50eba7a0d7db5f671ddc7ff"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "07156d0c7ca62c800f39ea5eeda2f9e3af3d4e36b74a1cf73afc2c3c2c6a0455"
-    sha256 cellar: :any_skip_relocation, monterey:       "78b0ef370161fd0877dbde9b89353374eeff459d7e2d9cd399a27b18c88478d8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "21d1ee3db44a41e383c878172c287a0df0ec467ae09d483c06b821ad6efbba34"
-    sha256 cellar: :any_skip_relocation, catalina:       "9f6491602e756a6aed63e53d64bc0e8da5802203b4bb527f9f76ce1991bdafbb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4a997da14f167168baf99a1d62178d54cb366d97adf750886973178764f9ad8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/abcmidi"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "fa99204979f9534c137ce69ebf8226216defdc618821fed69385b6840d14c2db"
   end
 
   def install
