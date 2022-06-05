ac5400e74596d984d5dd6cefa5c1ad845cd24a17
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 21:38:42 +0800

    berglas: update 0.6.2 bottle.

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -6,12 +6,8 @@ class Berglas < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "09a71601520e580e8b9ae6c653e6fa7722970e8ebdee32d29733c7abd1aab0ac"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d18e997593760bca7f35d083b0bb43bba0460cecfeb599a7117c1c492545a0aa"
-    sha256 cellar: :any_skip_relocation, monterey:       "26eb31f335d672d4873c87a884b60ce7cf75dceb9a5fdb81c4737ccb3b0d2933"
-    sha256 cellar: :any_skip_relocation, big_sur:        "46676eb6c7f8da3b900eaf229e3d100f610248e9b26dadbca0c1c1e752fdb6dd"
-    sha256 cellar: :any_skip_relocation, catalina:       "f01578325bfc8f7ce98b4bdd4fde31c6f00ab394c001ff4758e401d7688659e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c54aad0272e2803002792311bd75ca06d45aaabeaab4e2e7f492718ca7e098e7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/berglas"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4c73bb8f57da2455cf1d72d33608f79268febaa4f1f17313e8e534a8291a9722"
   end
 
   depends_on "go" => :build
