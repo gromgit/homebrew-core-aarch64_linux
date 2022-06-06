b98dc44deb3b33dbc032e4a9c31e0e1061578a82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-06 20:08:01 +0000

    allegro: update 5.2.8.0 bottle.

--- Formula/allegro.rb
+++ Formula/allegro.rb
@@ -12,13 +12,12 @@ class Allegro < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3995b983d76331b0e7d61b5e85504e04496e63c57ad22c96a91a6b8c7677989c"
-    sha256 cellar: :any,                 arm64_big_sur:  "111f5e8474a0abd37641c2db543664b53f89d83201493a6e22d846a25290a16e"
-    sha256 cellar: :any,                 monterey:       "2701bd5309623abe46a268fbc9567c37bf2465f0dfe52de737bd24273f5dbf64"
-    sha256 cellar: :any,                 big_sur:        "d681ad8e081082bbb8ac3036b4697ce03cbfc139037977c3d45880cd3b9f8396"
-    sha256 cellar: :any,                 catalina:       "dc2b03c9441a55e8501a1e330a1c0d673756ca06efbbcd8970012ace01c7d232"
-    sha256 cellar: :any,                 mojave:         "b9b9dfdb3d26e50ee7f67a678fb20c6874366fe9eeeaf1300b6fb020050e6b37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8044df31111ea3e4f3aba7a28e12a690cd91d19b21374387d962934fa82c67b8"
+    sha256 cellar: :any,                 arm64_monterey: "943c0bcffe61c4ac162a9c366801be695deb82028416b735ba62159df24d9ec9"
+    sha256 cellar: :any,                 arm64_big_sur:  "b1ef1fcf152a1811ce51ead541c3a29dd1b29999c77dfad92e431360a9d5fee2"
+    sha256 cellar: :any,                 monterey:       "a4bec0f69c3cdf94e838505d5daad20853e0be88ba3c4aa2b46952f64c62e4a9"
+    sha256 cellar: :any,                 big_sur:        "becf765b1d4061243aa4f72dcfc0ca8c06c3ef625f2e775824f9a63fe83822d1"
+    sha256 cellar: :any,                 catalina:       "e026e88044ae9d37c663f31aad61fd6f3bb3350298715200fb728769ef542b63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05302ddcc81c5eb504846cbef286537436e20835e4d957eb3b64b3db52b05be9"
   end
 
   depends_on "cmake" => :build
