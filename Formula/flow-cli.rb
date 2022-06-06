4e1a8dcf2be57c4eded49ae22688a62e0b13c5f2
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 23:05:44 +0800

    flow-cli: update 0.33.1 bottle.

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -12,12 +12,8 @@ class FlowCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9f01dee7c261f26650e014e75f49cc5b043992a1d442d4faf1a6866542ed5fcb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6ebd269b30c7277e955c80dd7bef9684482359b189bb19f031a41fce854c7d0"
-    sha256 cellar: :any_skip_relocation, monterey:       "0478229a225dcfc3a99fd108ae7d5f44944b3728921ff2afe8021b4399b80c9e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d61150d5c6ad814524a22e399df1423771dac2e246acf63f5797db5035872972"
-    sha256 cellar: :any_skip_relocation, catalina:       "b1a8d9ff1d5bddc61ff19715e944412dda15d69e198475e8905967b23bdbe54f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb64090b32d573e1d077edd1df915fc95d6b68c1b867ab42d6ebca194d1efb71"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/flow-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "01731ef727173f7735f119129ea00e954ae5f705fe3b87745fa8740dadc06c39"
   end
 
   depends_on "go" => :build
