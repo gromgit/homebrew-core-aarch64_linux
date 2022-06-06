b703db0cfaeb72ab0b0ca3a27be5a969ee34057f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-06 22:05:27 +0000

    earthly: update 0.6.15 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -7,11 +7,12 @@ class Earthly < Formula
   head "https://github.com/earthly/earthly.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6ced4b644da7733596ddb225d4d07dddcdf3cea9975a1dcdce724e65093142fb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "45b1406d85fbc167590873e727dd63624ed17bceadc289bb4c6c7f8e8a669317"
-    sha256 cellar: :any_skip_relocation, catalina:      "16c593502fd9a7270edab13a2ed8c9ca44486eb90bc97dceef99ec8c092ddadf"
-    sha256 cellar: :any_skip_relocation, mojave:        "a9a09599ccebca0c987ea802cfc861097055ab2662db97c417bfeb83756fdb90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78f6b33643db61f1944ceaf27d27ad001f8ba7a865b358a040cf1374fd618a96"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aa732b3294f66db383bc8f6d7a33a23543f8488679881d369a2656451db6094c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "26803f7f2f0100ef18baf1c5b346dbba297a475c27ff523444a342e60ef2cee4"
+    sha256 cellar: :any_skip_relocation, monterey:       "6e4a11a9174e4123f96297076367e194e2718b78d327a318a0adf6c5c22b2f05"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7c1aecb5821538e5a45237f756f5c0dc91ed4e287aff4f5dd047da095763e3d7"
+    sha256 cellar: :any_skip_relocation, catalina:       "f56636b9fa8335719f6db70af480351df273a036b649a30377431bae6e3e7995"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ac98c87feba61de05059aa03686bd493ea5f5f9b45a9194cddd20fe8b5bd3b4"
   end
 
   depends_on "go" => :build
