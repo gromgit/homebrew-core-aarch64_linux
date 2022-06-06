3f2994445c0a2e34e8256cb276f702cc70efb58b
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 23:38:08 +0800

    fortio: update 1.28.0 bottle.

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -12,12 +12,8 @@ class Fortio < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "adb728ef422a576f53a92cade23faa88f5040d1c73fbb355aacb180fab720bee"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40b699bb85a052898186932217a5a4551cf50414ea9232ebd32e085055a08087"
-    sha256 cellar: :any_skip_relocation, monterey:       "d045fa44263402f2a89f462fc1ab27126d8b9253b4d3c167dba72dade92b67b6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5ff0d1f1f12214da8fc64363a914c0b93278d5291889b39c6fffd400c8e935c1"
-    sha256 cellar: :any_skip_relocation, catalina:       "0255f89757ad491878e032413368c498a29cc0a0457f6ec57f2d30899a549866"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10960de58ec4395b3dfc98b7b2650e4d59913ed417dc00ce9e311b5e57bd9179"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fortio"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "318d934dc2c73662851606f9081bea17b330fb008111ffd56f82d5747064630e"
   end
 
   depends_on "go" => :build
