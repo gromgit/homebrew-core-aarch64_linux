89052179d602782a27da4926b7d36cc0f8034b3f
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 07:57:55 +0800

    gopls: update 0.8.3 bottle.

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -12,13 +12,8 @@ class Gopls < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e65bf8063b67a017625da0175a83fb8d44dd4c78687a53ac33b71587f4602858"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0fbc416321ca666d572fca4e73985f68a2fa46e4a9ec69e2ddf0d83e98f21aa6"
-    sha256 cellar: :any_skip_relocation, monterey:       "ad01b0570d2da32a39004097021898df929898d37c245b037703db49a4063c48"
-    sha256 cellar: :any_skip_relocation, big_sur:        "58e9c04f4e1832519fdbd7bd9d31196ed4d38c4ce0991812c5c18c909066659e"
-    sha256 cellar: :any_skip_relocation, catalina:       "521bdc13d461df52a2d006e755c64dc40cf63a4f4246464ed3ba86e90c93fcb1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b68e8ef9e3c52d48a5951fbff7c8d7c454c946e061077bb6da43d8b1f04ad527"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gopls"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6cae276a1c15423700a5f0b95195d028adc2d470a815954df5eed698736c3408"
   end
 
   depends_on "go" => :build
