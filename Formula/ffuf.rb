3d9b44ed5baeb957cddc3709f92050b7245b4b36
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 22:10:35 +0800

    ffuf: update 1.4.1 bottle.

--- Formula/ffuf.rb
+++ Formula/ffuf.rb
@@ -6,12 +6,8 @@ class Ffuf < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b3adf6343d84038a25dcbe9c184c8c03335dfca5be65cc9c43f031547736dc4a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b321758985e783b5ab0a605c1134b5b4eeb06b060daf18536582247710480474"
-    sha256 cellar: :any_skip_relocation, monterey:       "c302207ea855b2997a9d6cd8ed9478fe341cce2856917e89b397fc7c7b5af22e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "479517b3c6de780ee992656a510a700d5be719b98c8b8edb2872eee99da67c9d"
-    sha256 cellar: :any_skip_relocation, catalina:       "3d1e560ccef4aef2b20be515f0b990c2d7775be501d1e7d7d1ded13d7e704ee9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b8f97d9b1af9dfcb5d981bc26324fb93a6f9a38f473a0a4d2820cc75fdea78f0"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ffuf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "28ba7e750a998e7690ce56c56d4be138e58117a773b67fb9f7761362c19cfaf1"
   end
 
   depends_on "go" => :build
