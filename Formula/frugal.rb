89cba66479be7e32996a9dc042726a7f16de8679
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 00:04:26 +0800

    frugal: update 3.15.0 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -6,12 +6,8 @@ class Frugal < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "43025724ab5a12edf6630b73b31da54cf3cfdd4e917dcf1ba8feaa6ac164ade4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "43025724ab5a12edf6630b73b31da54cf3cfdd4e917dcf1ba8feaa6ac164ade4"
-    sha256 cellar: :any_skip_relocation, monterey:       "bd76c5f9b3be90c91bd7f48b1e294ff753bbc425e0aca9179e4269a1e9cfe684"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bd76c5f9b3be90c91bd7f48b1e294ff753bbc425e0aca9179e4269a1e9cfe684"
-    sha256 cellar: :any_skip_relocation, catalina:       "bd76c5f9b3be90c91bd7f48b1e294ff753bbc425e0aca9179e4269a1e9cfe684"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8a23ba8a889599d792c3e4ded8784376e0591252539e576155cd3b0a9119c8c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/frugal"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2e08c8d010a2d8615d4f7bcfa416eea65cc12c239a7ac7225e2c322e9c9d2a76"
   end
 
   depends_on "go" => :build
