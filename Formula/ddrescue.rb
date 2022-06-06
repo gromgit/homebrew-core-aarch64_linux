697cace29e0296cd8efd8546b3c7e47725aa856f
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 12:38:29 +0800

    ddrescue: update 1.26 bottle.

--- Formula/ddrescue.rb
+++ Formula/ddrescue.rb
@@ -7,12 +7,8 @@ class Ddrescue < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d7e53ede5ffb258402da89d8ec5caa6b7b430d2d09b736f625f1b95c30049125"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7da116694fc665cca45f353ce50732a15bfb3628d8e1c438b30325ecb696efc"
-    sha256 cellar: :any_skip_relocation, monterey:       "5d90acf7354dc1734a50f2b0e2aaa9063da223ab7373e076ef3d803f94b48a4d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6ac9aec95d746be7efee55c9666bf5f43b3feb60049d10cd06748a68bf5d68f8"
-    sha256 cellar: :any_skip_relocation, catalina:       "78a4d6ab9370c966716ae4e9b4ea10bfc724af6bb4516d9e18b16ce3546c87e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f44286f1d0466b4906edce7dfd63796907d85c0cb57c6415ec2dfa2d5a44603b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ddrescue"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0647abfe5a12233554db650163865b0e3996ccc513b5eafb32b9f3af31199b58"
   end
 
   def install
