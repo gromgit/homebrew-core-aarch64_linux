63d6850d32b3ac96b79b1945b9c2f96f0d373102
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 08:35:19 +0800

    cosign: update 1.8.0 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -8,12 +8,8 @@ class Cosign < Formula
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e41f2d5d02f3f52b46bb91ee2d2fc3eaad8f630953d32f061dad62f0bf36419e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9525e88fd1186b6f86e6bc17e331ee74e534b602955545afee5790d95f7ecb85"
-    sha256 cellar: :any_skip_relocation, monterey:       "0dd5cbb0d6b8be42fdeb59a01a72b06fd0efab055f6afac96e8be07655643090"
-    sha256 cellar: :any_skip_relocation, big_sur:        "879b88e2067a8d6ee13687ebf7a93e9142ea604c89308e62090f52114df0e3d1"
-    sha256 cellar: :any_skip_relocation, catalina:       "34d1f3429665d336ebd0f228deeff949b4e0280857762245de97b8271a520bfd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee0b3f1ec3bb1a78d340abc37eb1efcc21830a5adfea31b0b3a3e87a902db46b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cosign"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "737aacf87177bc8b57d2e4723f4873fc93881aca70eab4acb32ee258c07fa92d"
   end
 
   depends_on "go" => :build
