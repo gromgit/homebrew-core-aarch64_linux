13e07fd559c57bc6524a94363e0b85addcc4fe5b
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 18:15:45 +0800

    dvm: update 1.0.3 bottle.

--- Formula/dvm.rb
+++ Formula/dvm.rb
@@ -6,12 +6,8 @@ class Dvm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8f7ffdde663a096778f5674b405dcf14b063fd9987998b46f25d9f1d48501f47"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "810cc709e90853dbf04bad20b47e027b242a7b2e97d7a0ab7d16628371f51e1f"
-    sha256 cellar: :any_skip_relocation, monterey:       "c61954e5f72bd6c013abe06b891f4881a89ae146e615c6a4e01b2b671efefd16"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d6633823d962ccff3c002e8faab0ccb8b7b91f438ca51b5150eeeaf044db6211"
-    sha256 cellar: :any_skip_relocation, catalina:       "129f33fa691fca5b6cdafc4963b69d82fa0c2c8f5373e7d79db653e53af8e24b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a776882a25fc3164629b4ddb7a117870f0f04dd78ef2c8421c0abe08bf7c7752"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dvm"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "fb270922ce6918236c04bde6e3ebe96710c5d8abf6c618352811c22c936e9055"
   end
 
   depends_on "go" => :build
