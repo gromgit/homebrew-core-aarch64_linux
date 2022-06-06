6556f407567e21fc346cad3c923728e7ebee6720
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 16:39:06 +0800

    drone-cli: update 1.5.0 bottle.

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -8,12 +8,8 @@ class DroneCli < Formula
   head "https://github.com/harness/drone-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e7db30f414e4fed8855cdf0ebfd23704ca5d98481c1e4b9cce1c432c818c39c6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c3fa702e5a13f34f1dc989c216b84147ad896112fe503932aa34d5b911157a73"
-    sha256 cellar: :any_skip_relocation, monterey:       "0acbad2a3c296d8c07606aff9f246675fe04bca6e801900ec26b4b9a310d3690"
-    sha256 cellar: :any_skip_relocation, big_sur:        "65cc3a62fabbb34266675510fa01c9a924b3aa158231404d6ed440e519fc0b97"
-    sha256 cellar: :any_skip_relocation, catalina:       "cf0e94994336f4345837b93628ab2b70e17211eccd7d581eb2704469307597ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6ed05521633d4cb92588ad4adb811f2c86ecd0c388f01b4cc2de183af643f55"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/drone-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8ecbdaae4675cc09dd31f0269f5c70271de254eac0af7532721c8c5f08a24329"
   end
 
   depends_on "go" => :build
