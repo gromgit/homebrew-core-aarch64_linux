bbd4a10f0322a24e23c98a6745f29d60be63fd9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-06 08:05:02 +0000

    dos2unix: update 7.4.3 bottle.

--- Formula/dos2unix.rb
+++ Formula/dos2unix.rb
@@ -12,14 +12,12 @@ class Dos2unix < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d8216cc883ecd01f9c4faaeb4e93d9258a0c20cdbc1c718a58119c8ddaa6e337"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "54cb2c42f9f016e03ccf712d0927963fc629d8419de3bfdf324dc51088f87157"
-    sha256 cellar: :any_skip_relocation, monterey:       "539e9ff3c1ebce403da947ed6acdabbe45a09e005f2ece880cd3044c32cbefb1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7a4b0e40724b8721ff76a36cb74fd6c9816ce0e9b518c3abf0e2ec5bab5b0cce"
-    sha256 cellar: :any_skip_relocation, catalina:       "eb14b6db2fbf8fa0f52b69aa33101e1eb04bf3d4ef5dae7234754046e4cd54a6"
-    sha256 cellar: :any_skip_relocation, mojave:         "7a23d283d81cad13690d788c61117cbfe091282503077ad56bde83f026dd5097"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "98858938fe95e2453056d80c03c35396913a5c6902b2df00b618c884a4b51521"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "331d745aab1034b696b0f873ccdb1431266ed813215669a5446e7c2285984a2b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "99e6489bf197a6708e23fe54c6a3dd01c9faf91807aad90a21a4a19ba0b7a7fb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5422ac9cfe1635baa4032ff17a4bc1d8e1e991f988164376e0a706851c6c5b99"
+    sha256 cellar: :any_skip_relocation, monterey:       "7eb1a12fcb8ba89fcdaae8fe3b82cd9cb068d7edf7b966fabf80f0428476c329"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ecb0a8cfd9b346462640ab20b4f07aa8d2e8f10ce55c9f0539b0039d4979cb54"
+    sha256 cellar: :any_skip_relocation, catalina:       "c1b511a509a5779a767a864b6d9ae26bbe9528dc2271726cc0e000071028a700"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a6536d3129b4d80da09c431a0ccafb48cfde1acb9896a78b11daca84a8b296b5"
   end
 
   def install
