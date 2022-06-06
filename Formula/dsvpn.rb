a954437c0f9b807617e91fe82095e2e849be20e7
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 17:13:06 +0800

    dsvpn: update 0.1.4 bottle.

--- Formula/dsvpn.rb
+++ Formula/dsvpn.rb
@@ -7,14 +7,8 @@ class Dsvpn < Formula
   head "https://github.com/jedisct1/dsvpn.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "533ca096976136d7eeb0923c0164a0b3e8305d344036c1ad095a586f8768f5a2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f0269b49b371b7330a7397c73ca1a8791da2e1215242eacd3e612f6705dd9c4"
-    sha256 cellar: :any_skip_relocation, monterey:       "aaedaf19c0e6217233075e082167848fb03b2b2e3d3f27e690ac8f82064647f2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9113686d8da5885ab67bae686914b0d27cc5eb2588a49b83f7c3fe66c89c039b"
-    sha256 cellar: :any_skip_relocation, catalina:       "a08464eca0167991c580594ecd9f1893a7be6d1cb522ceb385ff1883dca507c3"
-    sha256 cellar: :any_skip_relocation, mojave:         "31a8359d756b673788aad04e1b776c0e1d5b6331f7e64494d3c6680280ea11ec"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "d34ff5d83b0b259c5051de2e2e8cf4599679d1d7e61dd282065afb0516fe62b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6cc8aa7ec1d462fb028d5858de3863d2d131175ddfa102cfd44c9c0614d003f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dsvpn"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "baf5999a715a41ac425ced44b91d08c51f9e83998eabcd0520031ea965ff918e"
   end
 
   def install
