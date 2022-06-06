90e095aa149af50105db9045616a7669c04249ce
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 02:45:09 +0800

    ghz-web: update 0.108.0 bottle.

--- Formula/ghz-web.rb
+++ Formula/ghz-web.rb
@@ -10,12 +10,8 @@ class GhzWeb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd2e8196d8e9f69811439fffa8e04c5e17a0ab6aed527c78b62e57a0d74ca754"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7684013b41101cffff1f49b9953bc2a2ff98bfb3ef1b4612227dd2aa1448f443"
-    sha256 cellar: :any_skip_relocation, monterey:       "65be6d04437d6a602128e6c625fa98a517e515fe5db78b9746cc2e0bd320aa7b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1214f08a996d4fb4091d0253ea853dcb784ca0dec1470e77a4e66024c7ac61b4"
-    sha256 cellar: :any_skip_relocation, catalina:       "291832ffebc6d598950c60aec32fb142bc8855674b33aa452bf4b2f69bf327c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f203fec32c711c8c946d2e9502b2a049a8920e8db5a7591bebbfd752a48459b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ghz-web"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d0d3a83bedc940728e46213b4438fd8f82ff71777cc4b2854ed18cd97f7e28c1"
   end
 
   depends_on "go" => :build
