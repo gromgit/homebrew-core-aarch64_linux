6889ba88bfbb61b0ba14d2b95ee7ec077a5c1370
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 10:33:27 +0800

    cxgo: update 0.3.4 bottle.

---
+++ Formula/cxgo.rb
@@ -0,0 +1,56 @@
+class Cxgo < Formula
+  desc "Transpiling C to Go"
+  homepage "https://github.com/gotranspile/cxgo"
+  url "https://github.com/gotranspile/cxgo.git",
+      tag:      "v0.3.4",
+      revision: "d57c260fb00fa8bdae1c146f79dc03c4a13ae7c8"
+  license "MIT"
+  head "https://github.com/gotranspile/cxgo.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cxgo"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "19e14b669af31374d079c4eb4a56237de2d47de352c1b637c57526a77dcc1fdf"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{Utils.git_head}
+      -X main.date=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/cxgo"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main() {
+        printf("Hello, World!");
+        return 0;
+      }
+    EOS
+
+    expected = <<~EOS
+      package main
+
+      import (
+      \t"github.com/gotranspile/cxgo/runtime/stdio"
+      \t"os"
+      )
+
+      func main() {
+      \tstdio.Printf("Hello, World!")
+      \tos.Exit(0)
+      }
+    EOS
+
+    system bin/"cxgo", "file", testpath/"test.c"
+    assert_equal expected, (testpath/"test.go").read
+
+    assert_match version.to_s, shell_output("#{bin}/cxgo version")
+  end
+end
