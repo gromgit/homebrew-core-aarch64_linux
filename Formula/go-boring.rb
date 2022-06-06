cbb60522e3baae8cded44e992475f8294074d2e2
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 06:01:34 +0800

    go-boring: update 1.18.1b7 bottle.

--- Formula/go-boring.rb
+++ Formula/go-boring.rb
@@ -12,12 +12,8 @@ class GoBoring < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "d405ac2add5812b6b6742dda0a5064612af67a6c730ef82facf92b28a6017c97"
-    sha256 arm64_big_sur:  "a7e57b12658af9fb8c074a8a5874b0d3aca146e67c6418d9888fa08711937941"
-    sha256 monterey:       "22c8ff691719a951a1995e7f8a81cd06ff872f19281990a42ecce28f711800ef"
-    sha256 big_sur:        "08429c8280ab4e30d7ae3c3e5c6472d386e0696951121881e199e00506a20f9b"
-    sha256 catalina:       "a3cebf591ec7460774695fee9e8cc504991d715a18b90e82812a27cc2b232518"
-    sha256 x86_64_linux:   "bcffddb759274f558f9495509c60c375e27041082f1c11eeeb0e21fd2a537ea3"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/go-boring"
+    sha256 aarch64_linux: "1c2179800a015132e4cdbf2ced4912b6ad36524d17f17579545c4770ce04ca69"
   end
 
   keg_only "it conflicts with the Go formula"
