d6f4afd56fc44d7d7204e4af60349c798d671452
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 12:06:14 +0800

    datree: update 1.4.4 bottle.

--- Formula/datree.rb
+++ Formula/datree.rb
@@ -12,12 +12,8 @@ class Datree < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2237e7a597de2c7ebf5e2906b2583375a6813bed67c9d6f60bcbb8908a3bd6ad"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d65b5064ad6bc5ca25869a108986eafeec38a62bdf90505283d6311c30afbb59"
-    sha256 cellar: :any_skip_relocation, monterey:       "adad8473e6f77c07be36d20ce37e187535c5b395c518f3435849536ed33d3643"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5e1a0a9cb274bb1e8b65c67c86287ae509dc83c0c449a0918bdcc8f1fefd1d28"
-    sha256 cellar: :any_skip_relocation, catalina:       "4389899d6ef72d5a8a861916d31445444f62f88363d7efff71cae26d38807ddb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c341a70bc4a073cc8b79274ab53c128d3f90e4bb04da82fa0d2b37222939798"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/datree"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "85f7362261c039d0fd8d089ad9ab98a0d024dd2f7a60089e750bc1f3ebf5fd3b"
   end
 
   depends_on "go" => :build
