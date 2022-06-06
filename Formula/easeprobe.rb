04d571790027cce22338717cb7b2936f01b7ee9c
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 18:26:44 +0800

    easeprobe: update 1.4.0 bottle.

--- Formula/easeprobe.rb
+++ Formula/easeprobe.rb
@@ -8,12 +8,8 @@ class Easeprobe < Formula
   head "https://github.com/megaease/easeprobe.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "65614a9f926fb9eeb6cdc07bcd5c152cd622c53c93b78904fc486bb88d8127c6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dcb076860023b6c2088c922f42029224a08520a707174179bf56a13df0c326b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f166bb0967b571b011c38ee69868a0db507452bf8cda1910e175a816d164fad"
-    sha256 cellar: :any_skip_relocation, big_sur:        "286cb976a3e53b39e8a80118bd14938178162d53f2e31922444b3252933fad64"
-    sha256 cellar: :any_skip_relocation, catalina:       "c0a635acd17029c4fec06987a2dbd7a197527fd6eafe5803fdd95c09c7a3242f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ae99766e9af864ac1a2886917a00c9343502afc1fa3cf87a57a8e7f87a20400"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/easeprobe"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cf4764760e58d9285cbe9c0807bb04587deef4a8bc3a8f912f5f75f147f3edd6"
   end
 
   depends_on "go" => :build
