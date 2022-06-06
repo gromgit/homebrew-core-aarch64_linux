c2e32111a06b61be922d86157a1d9afc523a94bd
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 16:04:44 +0800

    doctl: update 1.74.0 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -7,12 +7,8 @@ class Doctl < Formula
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dda0c5fd894d39f6766107391e2c7373aae26b8b55bc03a0a6089226d9cd9ca4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "20a57316c0731beec19e3c0f3c69bbff95c7c71a3b2baa0ec000186fca7db21d"
-    sha256 cellar: :any_skip_relocation, monterey:       "10df1f44121351ea7571c9b84b373ea6959900263d9e32c95818f44c9be66b96"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8f5071172e82754f753db9ea94c9e07777b03e78a795e6d02f3105a77c243e26"
-    sha256 cellar: :any_skip_relocation, catalina:       "8ef3d7596ff713b268024fb5e5f72ac8fcbdac9018697c49be0f53c0dc8e06d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "31266cc3be8fca18776884e8f7a620947843de671efedc8f39a99c2e45c20026"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/doctl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b6af73bb461d17614618ea9183942728e8ce4299545235f04f91748a8077af8f"
   end
 
   depends_on "go" => :build
