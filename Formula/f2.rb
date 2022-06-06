afa8d53fbacf220f4be313fd672e603c484b9add
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 20:26:02 +0800

    f2: update 1.8.0 bottle.

--- Formula/f2.rb
+++ Formula/f2.rb
@@ -7,12 +7,8 @@ class F2 < Formula
   head "https://github.com/ayoisaiah/f2.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7b675674c09aa6c6922bcc2c3fec2ca0d56cb2842091b3210ac6e94fb0363e9c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af42f08006c442d62b1db1e604dfc5c3e1906086f65902b8f069d51e5edced6f"
-    sha256 cellar: :any_skip_relocation, monterey:       "365170c4d4cd8994a2110db618df37f70c6b684c71fbb6857961b420d041ca10"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3849c35a21e22d529b109a63c1fdbd89257016e09cff433ad39a79394dacfd34"
-    sha256 cellar: :any_skip_relocation, catalina:       "cf6d3ca529060c841f6f06634770fa2657efb97e2604ea06bb4a136d5c4cc2e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ffb45d637be8c319e3936c2afb6f47efc35540ec5ac5f7bfc1cb27515f24826e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/f2"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cc3c3e74d55704fbc197c6ed7489a66e2a314242fadf4c6715c7ea5229929dd6"
   end
 
   depends_on "go" => :build
