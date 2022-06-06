9925cb0bedd86fe4b5740b5a28e1fe8ebf3f34d3
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 02:21:56 +0800

    gh: update 2.9.0 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -13,12 +13,8 @@ class Gh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d4aa659a9c6de950610168a61ca79a0f5b1cd0fb4b40abd5766d281fe0ee617a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b4a1f3ba9d17a2dc5bf120fe3d1f8faa8399965abd544b1ba0c2ee3c18596916"
-    sha256 cellar: :any_skip_relocation, monterey:       "ac84162e94e6033cd36a6dd7c09306b8cff60b31b0f5b2eedec964d8e83ec47a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f782d4f7c7f54947be17f3731302c150a928912d5024a1e8610d491d0628d820"
-    sha256 cellar: :any_skip_relocation, catalina:       "90d095ddbb8403d3379b0b8969f2f4034034b338a261e60257c85d546e941eb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a04ae9008e82b09a5cbaedb4c3ea67849d54d209a9227ee679ca82e49fbe90d1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gh"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d5d4d6549e32ab6b386db6d0b481a57aebcb62443678b499c7e1477e731ae6a6"
   end
 
   depends_on "go" => :build
