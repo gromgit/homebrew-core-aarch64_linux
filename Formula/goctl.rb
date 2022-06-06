42f27ab945190df9b212462c5865d50c556b9104
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-06 17:07:34 +0000

    goctl: update 1.3.8 bottle.

--- Formula/goctl.rb
+++ Formula/goctl.rb
@@ -6,12 +6,12 @@ class Goctl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2804d96b6e9e78a11a4cd381b66c10f29fdeb2eff17b4c96ed038478e91344a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4c0c137fdf2ea2befeadf794b73a4e95c04b0eecc1b555cb234f273172f2fc9d"
-    sha256 cellar: :any_skip_relocation, monterey:       "d75ff2947324e3ad06393230f3d23f81beb7a9697d414ab3d88c083316f4d85f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f184f54a63d4619f0d8ed0cd9dc703b4e7bf9363aa44e7d1bcc4a322b74f4202"
-    sha256 cellar: :any_skip_relocation, catalina:       "d8f88627242441bf9733b753aaf075273fa7ad9d2d5fd7e9000d9be1ed2dcbf2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ce3ad74cd0aa5e9176d5880111ae929d06ba74d58a833418f638a25f34459d24"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1a4cca10122b2d8dea66573cbd475a478beefdd65a1962db34f4dbfb75352fa2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3986991fcbeaa7e05c911429423172ea0bd484575f04bf14224e12a110d23fdf"
+    sha256 cellar: :any_skip_relocation, monterey:       "8d50eed8c412d2980842b9ea323654734217845b85bc1a02c908ac27ca671b3b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "05649429ad1bcb16bfba7db35d27e106c135416352bd53589ebe8f5e52dc1e84"
+    sha256 cellar: :any_skip_relocation, catalina:       "83f964355d440fcab9c35550fd3abea999b0b6b73a347250061fbc750c5b5b51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97e2cdf1f76cd71414375384261084b517e27aaeb18c0254d1a41191ae553c5b"
   end
 
   depends_on "go" => :build
