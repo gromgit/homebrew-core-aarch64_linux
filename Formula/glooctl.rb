c08afe5fe6e962c280517cd12b20cf008b19d2a9
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 04:55:18 +0800

    glooctl: update 1.11.9 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -15,12 +15,8 @@ class Glooctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ebca5a001251db68d2f6d626ccf4904e755ad65756345fdf52d7b57f0ad3e981"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8f5ef8583981a67b79d99c48fd6eae7165780312f4f052ded55a900729c9da75"
-    sha256 cellar: :any_skip_relocation, monterey:       "542d7685a36f50f9bf42c49473e9cb94d37e7f552f4379f07e535c0acf282191"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d34ea2734a013f5942d3f3d8f8e957022014dc9eeafb4416f8ccdd0264d31d14"
-    sha256 cellar: :any_skip_relocation, catalina:       "5e847f92d830717dc8e3351bc6bcaaf03cf2af067b8f3a48053d82983bf65e6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "859f56e3c4eb180f764a4f89f1b6c82973bdebdd33c91d2186402ee55d642064"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/glooctl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c2351292f94479d3fd8456f13c633464874c7138c1c55665c4773f8d727c1acd"
   end
 
   depends_on "go" => :build
