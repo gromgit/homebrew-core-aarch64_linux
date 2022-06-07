b10c3762b58e86d5fec5a1625fa63a7e9c2a8512
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-07 08:11:11 +0000

    mongodb-atlas-cli: update 1.1.2 bottle.

--- Formula/mongodb-atlas-cli.rb
+++ Formula/mongodb-atlas-cli.rb
@@ -12,12 +12,12 @@ class MongodbAtlasCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0af3606951fe106dd16186d1f8b74d9f75f9f4f06abf371651994c914d34d826"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "14169fce8278aa036a28f0946a7c354e5e72185618959874fde8e5c8373cac8d"
-    sha256 cellar: :any_skip_relocation, monterey:       "438d91ac88c8056ddb5599992d95afa75029edb302098d0e9c85bd310e5db595"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c0762d461712d7150a6f882bb2b24e570b6dd2ebb1a1ae541c2c2c11ce4d05a2"
-    sha256 cellar: :any_skip_relocation, catalina:       "c08696c435c14bee3dd2f422d282e6c1ab864bfb53b60b31020ec71880f38881"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dfaa59bbc049250cb6754aa2e9fb073176c8a29098b1e834b80c7eb18ac4dd38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f1480c96c8ecc91ea6c8d64f0054b5263d1d4575ea47b82b7daf3e9b3d20eeb5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93f9ac2cb09fc697f22130bc596b1ed81cb951cf91aa4c68a2a580a6d3550434"
+    sha256 cellar: :any_skip_relocation, monterey:       "0536985b1cfbf42634a14cdb4b3eea20f7f1cdef3a93984a04f1b3f47da18a38"
+    sha256 cellar: :any_skip_relocation, big_sur:        "211aa326b4c11135442a8dd567bd1130126c37b767a4daffd54e535d9d33192a"
+    sha256 cellar: :any_skip_relocation, catalina:       "fafc7ccc385e1d13e457b98cedc30b8fa29dd10a9c55cc8111130b2b9471d9fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2e86a3b78c4dbffa5204a4310f33ebc8608e30f03830d8f49ca3da0c956f2310"
   end
 
   depends_on "go" => :build
