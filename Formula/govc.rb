101798eff26f462f3025e0b0a925f37065876f5f
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 08:52:43 +0800

    govc: update 0.28.0 bottle.

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -6,12 +6,8 @@ class Govc < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5891e596b518ba7ac2f69f52334614fa31153c1cab7942e2dc26547937e623ab"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d89f8febacb84effce5fdd331d3536b5a3a78473a0d431d30b0d7cb240091a3e"
-    sha256 cellar: :any_skip_relocation, monterey:       "b120936654930e272cdc50cd067f839d2b096b19171d996bccbaa6467a62b54f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "718ff4efa76cf607ab0244732d6b4a8b06b0a8b738a81934e7d4df4435a28fe8"
-    sha256 cellar: :any_skip_relocation, catalina:       "921b797168fe9dd04ffc326beab7125702b4656afdd02c861da72cc2ea1a2076"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e09e9c4d8e7a59a1f98a5eda4c7a1c85f4df2e58ec064d71f04e2023aa229435"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/govc"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3ebe97c2eb5bd1c1c6d8b54df5f9f0f5c71b3cf284a6a1ad31e634e90f67ddeb"
   end
 
   depends_on "go" => :build
