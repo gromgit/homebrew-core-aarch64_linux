9a53ab89b295b2f9a7f3be3ce2b6f7e408d4597e
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 10:06:50 +0800

    grpcurl: update 1.8.6 bottle.

--- Formula/grpcurl.rb
+++ Formula/grpcurl.rb
@@ -6,12 +6,8 @@ class Grpcurl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "69c6ef510f421c1363eec22620c089a015c52e53844afb9072b518704a5e4070"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f906e658d4519e95eb37b43b840c76d16f19b6e9865c34314be8e2c63ddbf46"
-    sha256 cellar: :any_skip_relocation, monterey:       "f0a2db8478055277328b6346fc57204a5c251441c63814bda46672f8307413d1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9a43b1767f708398f73b3e647a0f7915717ff10ac340cffcf7e93c261e375549"
-    sha256 cellar: :any_skip_relocation, catalina:       "4d9f35fec2785e437dccb9c5836799ab90d6d2439cf011846ef9171676491575"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02adce763e2fd45ea99627ef172416d3534db2c51ab5138c6ef9cd32c8a9fdc2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/grpcurl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8f43abd656201a550bcaf1b5c846acf0241bac90ab773a48bbf57eb32daa1dc0"
   end
 
   depends_on "go" => :build
