7f9275479b89e503b46662e15323f9b0c1e27d6a
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 08:23:29 +0800

    goreman: update 0.3.12 bottle.

--- Formula/goreman.rb
+++ Formula/goreman.rb
@@ -12,12 +12,8 @@ class Goreman < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b835de9ad6465257f2e83d72e32066640e21d550af4d2a7db0a20d024ec4659c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f5c838a8c0d0622ca7dafec485ace3d0161eee628dd14a4e82c95fbc6f4cfb3"
-    sha256 cellar: :any_skip_relocation, monterey:       "a210952b6b9f43b17e4bc346016933baa67042d3684a4559b75ebbd9eb4673fe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2e8706a404fa519b75b6d3c1b7644cc1938d312ecc8771dd80610e1fbd83dc8d"
-    sha256 cellar: :any_skip_relocation, catalina:       "fa55372eb815aca1025b6055806ce017369850852621d0d2ad7d233ff51af317"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "df2f3b5165bb0635f982d8d06573ced7b9451d8bc8253cf3de25cff2ab5fe215"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/goreman"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1a8af42bade9869c0efbe3c21d60248f3f705b63bad3c62a771eb561dd2ad158"
   end
 
   depends_on "go" => :build
