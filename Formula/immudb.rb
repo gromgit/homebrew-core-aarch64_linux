db45121afdfce53d51b88debade4af949532ad1f
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 14:08:21 +0800

    immudb: update 1.2.4 bottle.

--- Formula/immudb.rb
+++ Formula/immudb.rb
@@ -11,12 +11,8 @@ class Immudb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2eb5d591b8b3d5cba6f21b72a25b45bd9e877826e385d775f1f8d0943915e85c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56d2e49783d2e7cd1d8d294b8296103456798d8cbc88dc928f26ea61b39322aa"
-    sha256 cellar: :any_skip_relocation, monterey:       "b79c1fbba63722c2f5d8b4baeff37c95d28cf8363d4acaaea3ab5abe0b09eb98"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b1c13f1a07cac939a8a1423106a05e63a11150b0cb228c160353323f57cddb98"
-    sha256 cellar: :any_skip_relocation, catalina:       "481624cee7d5f2d109f4c4f27befb332bee9e4772efb5ab655f001037ef3f9b7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf822b40d8efd272a64b408352dcedf234003c94eed601b08df29c7214ece017"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/immudb"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b935b7e84e355541917817e7592f646b5481aaab974efa0858953d1641aa1f23"
   end
 
   depends_on "go" => :build
