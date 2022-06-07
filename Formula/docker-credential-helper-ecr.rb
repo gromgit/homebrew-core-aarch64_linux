43801cf0199eb825a11814365e4220dfd549e39e
Caleb Xu <calebcenter@live.com>
2022-06-06 23:39:06 -0400

    docker-credential-helper-ecr: remove GOPATH usage
    Closes #103107.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-credential-helper-ecr.rb
+++ Formula/docker-credential-helper-ecr.rb
@@ -23,15 +23,8 @@ class DockerCredentialHelperEcr < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/github.com/awslabs/amazon-ecr-credential-helper"
-    dir.install buildpath.children - [buildpath/".brew_home"]
-
-    cd dir do
-      system "make", "build"
-      bin.install "bin/local/docker-credential-ecr-login"
-      prefix.install_metafiles
-    end
+    system "make", "build"
+    bin.install "bin/local/docker-credential-ecr-login"
   end
 
   test do
