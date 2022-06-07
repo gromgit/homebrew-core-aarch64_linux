3c8ff8f2cee222ee582945629f094d7acf765828
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 10:54:09 +0800

    hcloud: update 1.29.5 bottle.

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -6,12 +6,8 @@ class Hcloud < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "250b63879cde52e657a738f978a1766714b30c78580c7112de7523dc32073311"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffdac50445bff8c904564c7cfb1e7d8c9da5c66ad7a52ba960c33c1bdbb0fa7c"
-    sha256 cellar: :any_skip_relocation, monterey:       "9595dea61370a7b75a69795160147ce236d0b9d5e309a7579b209f2c2454d736"
-    sha256 cellar: :any_skip_relocation, big_sur:        "be8d2a512b4cc468bc88fd9656b8d4d4591077b2d9523ea4e9c20039d1d761ea"
-    sha256 cellar: :any_skip_relocation, catalina:       "54eac566e7bad55d5b361c92bc02d237ec8a6d1db75878d0999b511d65ed5dbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea5bffe22bdd226d88dea8d7cdd839c7d6f19232d9bfb9b571d0438a8aabc1c6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hcloud"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c90c0c43de0382c7f7c9965db59c96283e50e39f4cc3fe1dff72f4052e893f33"
   end
 
   depends_on "go" => :build
