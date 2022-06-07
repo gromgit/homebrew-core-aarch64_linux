c934bb4d3145de7497c970658273d9026b5768cf
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 21:47:42 +0800

    ld-find-code-refs: update 2.5.7 bottle.

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -7,12 +7,8 @@ class LdFindCodeRefs < Formula
   head "https://github.com/launchdarkly/ld-find-code-refs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a71e0062ca94ea01f5e3b8d77e2ee7999b11fd3861f81690735ee4dc9948447"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2a2513e149eeff2e443da0f29a8f58973eef359e8a9612e81d95ab6fe4f90feb"
-    sha256 cellar: :any_skip_relocation, monterey:       "a687bd8cd39296f99fff39e0d113ea423a3f8a310a448bae467e98456da66a00"
-    sha256 cellar: :any_skip_relocation, big_sur:        "68eac3629b44c7b77950814a4c6a92617adbc9fb971f585b5a99d7e4c6dc6727"
-    sha256 cellar: :any_skip_relocation, catalina:       "b23d4b60cc64f468e932ccedd502effa7c076d92ba3cd03e54f8e30a516d0902"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9623c411433ff1891ee2b2e7471ba28785e364f22ef82a7df65f0210de9b621"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ld-find-code-refs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "079ce1c5212a8fd6ec3954e503f0e7f30921dd8264f9d38f612601685b3d924b"
   end
 
   depends_on "go" => :build
