6539a0f28bbcba6ea9d8fb3353ca233ccbd74d2f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-06-07 14:17:52 +0800

    homeworlds: revision bump (wxwidgets 3.1.7)

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -5,7 +5,7 @@ class Homeworlds < Formula
       revision: "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
   version "20141022"
   license "BSD-2-Clause"
-  revision 3
+  revision 4
 
   livecheck do
     skip "No version information available to check"
