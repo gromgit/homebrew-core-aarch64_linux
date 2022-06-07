eda184bd02eb41264f7a9554efcbd28eb59bcb42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-07 17:11:07 +0000

    diff-pdf: update 0.5_4 bottle.

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -7,12 +7,12 @@ class DiffPdf < Formula
   revision 4
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "83766e8b62534bbec80b603d08c56be97548fa1c67200565300b148466a4f2e7"
-    sha256 cellar: :any,                 arm64_big_sur:  "6d52ffd8c73772030febeaccbce369e4ebd3aa8f5d48ecaebc871fd209bd8281"
-    sha256 cellar: :any,                 monterey:       "62c63cea4828d2cee726103208f3ce6059fc9032b895e6f10462895fd475e007"
-    sha256 cellar: :any,                 big_sur:        "930be3aca2edfcffd9d6e77bb8b11e02569efb34f36721038b23e498755aa315"
-    sha256 cellar: :any,                 catalina:       "98fc9b5ebbeacf9d27d1d06c6b4d1e401ba15eea9e2d155674a95f450c72b61e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ce2184b6f83a05827d1fe7623fb37f5241d370c37b01084a22e2e2dd6a95ee5"
+    sha256 cellar: :any,                 arm64_monterey: "6221f00c375eebe4d356c14dec2b7b28a8486636a10333c8ef99d0fb8c9f95b1"
+    sha256 cellar: :any,                 arm64_big_sur:  "f6b1b97f6a30c4318e2c38526887bd4f6dc92dc3badef15e9af39df27819eb96"
+    sha256 cellar: :any,                 monterey:       "acc8ff103fa29c4c2f42efc1010f5cfadc364b735a4b6b1750091502c69696be"
+    sha256 cellar: :any,                 big_sur:        "07ca313f26653f86bc93318ef6c8c544331837ef46f592f95c4b436f29c9b4e9"
+    sha256 cellar: :any,                 catalina:       "5623153835d7e0f13cd40f691b8a28d795e9157991c8ffef01b75ea7fc3c082b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37f48305aae394028a8a12f3e82b1e1b0a19771e8735c104e554875e0abde3e7"
   end
 
   depends_on "autoconf" => :build
