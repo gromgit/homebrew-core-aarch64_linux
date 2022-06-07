db4c88822a13c8e09190fa3b07c9e1e3826266e7
Porkepix <porkepix@gmail.com>
2022-06-08 01:31:15 +0200

    terraform-ls 0.28.0
    Closes #103168.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -1,8 +1,8 @@
 class TerraformLs < Formula
   desc "Terraform Language Server"
   homepage "https://github.com/hashicorp/terraform-ls"
-  url "https://github.com/hashicorp/terraform-ls/archive/v0.27.0.tar.gz"
-  sha256 "25ef850db974bc54d30f6d66d534313c61c16e5bddf8d137654f26fd2c4c274d"
+  url "https://github.com/hashicorp/terraform-ls/archive/v0.28.0.tar.gz"
+  sha256 "3c52faab9ef60f246c1f046b5689306ec6265eca6d560833dd797b8ec80b6129"
   license "MPL-2.0"
   head "https://github.com/hashicorp/terraform-ls.git", branch: "main"
 
