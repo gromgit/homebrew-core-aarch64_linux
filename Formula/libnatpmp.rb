2c560de8bfea2e532bd0df286f2d4ce06bb17bae
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 01:05:56 +0800

    libnatpmp: update 20150609 bottle.

--- Formula/libnatpmp.rb
+++ Formula/libnatpmp.rb
@@ -10,16 +10,8 @@ class Libnatpmp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6e47414293d016589ebba6a965daf4c2716613fce6759ba5d98e067b517d1c8e"
-    sha256 cellar: :any,                 arm64_big_sur:  "748f4fea8075f967191c0f1862c2e20289bee92883c816b191490a35e0d0a9d4"
-    sha256 cellar: :any,                 monterey:       "1fbd6904aea36ed66fe55fa4d0cd9a532542f4384df0bd6351e9fba663270dc8"
-    sha256 cellar: :any,                 big_sur:        "b62f39f2cf735ed55841676e6d55473aa1fb5d9247c10151b71184ba20dccba1"
-    sha256 cellar: :any,                 catalina:       "69bd0b362260f89b76113fbfec36235ec6265434c365d18790e8bb1a4988ae67"
-    sha256 cellar: :any,                 mojave:         "1f0e89186c04cd7c7ce9ba88bee87ae31be9c6f5b0ebbcee46f38876d90bfb78"
-    sha256 cellar: :any,                 high_sierra:    "04c286ebb17bf08728749e390dd9ccabf3fcc4b660ffe4b6f315dcf89012f15a"
-    sha256 cellar: :any,                 sierra:         "d1aaa97c827918f7d35d121399cb8f59b4442b94c3283a51b7931f0e008ff934"
-    sha256 cellar: :any,                 el_capitan:     "667fe1a26fdd6e1a36f6e7b263f2f8e3d01f884da9d9edeb182dbb40b08475ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "730ab2eb6c6b4e9bc470d78033d161fb669622a9bc555ca989720e992144b3cf"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libnatpmp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e93d337722343bebfffecc2ff01b4a2029837e6106b936ecda6c5976268032a0"
   end
 
   def install
