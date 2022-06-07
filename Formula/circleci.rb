e8717f9ef1dc28106853bbdf6ce3cb8fd91ce8e2
circleci <ops+deploy-bot@circleci.com>
2022-06-07 13:17:58 +0000

    circleci 0.1.18869
    Closes #103148.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.18828",
-      revision: "e96ff599fd236c5ec0e81a77649a3bf4a9ab8849"
+      tag:      "v0.1.18869",
+      revision: "f5ffef7737272d19e697d0312226d2029a009de3"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
