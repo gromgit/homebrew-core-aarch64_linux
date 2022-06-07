8202e6f2bfe43afcf180c14ff6ea82f2b41027ff
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 15:29:45 +0800

    jansson: update 2.14 bottle.

--- Formula/jansson.rb
+++ Formula/jansson.rb
@@ -6,13 +6,8 @@ class Jansson < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f8a132e116364ead3e428b1ad39768791f7a11ad26c07f5040c41d3514b7dea2"
-    sha256 cellar: :any,                 arm64_big_sur:  "08a95c23eb5aa8cfe0af9dc360b4bb3ecab89cfb42db9d5e68bc6490b571321c"
-    sha256 cellar: :any,                 monterey:       "b17770854e930d4302809dd4549142205f99a153a231492a9740f0c18d8e3258"
-    sha256 cellar: :any,                 big_sur:        "bb129dc922c0610c35a7b161429033a9123f03c4171df35717ff086b9cb52922"
-    sha256 cellar: :any,                 catalina:       "ddf25d83863396b864697529e837b869220ce86b8bb7b2cc03b77bdf1129563c"
-    sha256 cellar: :any,                 mojave:         "e219fa24f9fa034654592f6626c4a09d01fdeb888343a259a08ab4b1d08ac4ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f29060c36272b9dd76c5215e4118c04c0ef9235565281f87c34f8e8029cc3cb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/jansson"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "acbaeda024e2c900de9c3fcaf5c2cf5a05c3dcbad023522ba78de90904e6cc5e"
   end
 
   def install
