8b08e7ee0ad6ab522e9083606b35080bee5e4699
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 11:42:37 +0800

    hivemind: update 1.1.0 bottle.

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -7,12 +7,8 @@ class Hivemind < Formula
   head "https://github.com/DarthSim/hivemind.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f8ca06b6d2dde91c66cd6af2396a58b40e6be52e51fa738f24ff2e23376dc60a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f8ca06b6d2dde91c66cd6af2396a58b40e6be52e51fa738f24ff2e23376dc60a"
-    sha256 cellar: :any_skip_relocation, monterey:       "eaedb2b4739dc4e668ae9bf563750e1bb54fea1f16e800f6bf496226dc1daff0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "eaedb2b4739dc4e668ae9bf563750e1bb54fea1f16e800f6bf496226dc1daff0"
-    sha256 cellar: :any_skip_relocation, catalina:       "eaedb2b4739dc4e668ae9bf563750e1bb54fea1f16e800f6bf496226dc1daff0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c702f980c6b3a024c20bd50af65de981ab55b360e10013b22aa6797d834f7c85"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hivemind"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4666685247260186433cd1b814cc99c372d2ddbf47994c2d7e60ccd4c658c384"
   end
 
   depends_on "go" => :build
