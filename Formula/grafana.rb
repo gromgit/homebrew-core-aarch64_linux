eb41e1e64dd16ec9364de4e40bafbfc62580af65
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2022-06-07 14:14:00 -0300

    grafana 8.5.5
    Closes #103155.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v8.5.4.tar.gz"
-  sha256 "cdcfe87834a6fd4bdaab73ada4765b1ee80fc8bf157321ae90ce9bc23c15642b"
+  url "https://github.com/grafana/grafana/archive/v8.5.5.tar.gz"
+  sha256 "421d39cdc262abf9b94c25acc90e8808e4f2bb4e2f870878fb5fd929b534943f"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
 
