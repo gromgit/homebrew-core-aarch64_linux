0b56c13ad1b970efb34c97051894910b46d08c36
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 22:48:31 +0800

    libcmph: update 2.0.2 bottle.

--- Formula/libcmph.rb
+++ Formula/libcmph.rb
@@ -5,14 +5,8 @@ class Libcmph < Formula
   sha256 "365f1e8056400d460f1ee7bfafdbf37d5ee6c78e8f4723bf4b3c081c89733f1e"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "85743179d6c3127e57f41d11b451f708a653b3b033e1b725b30fa0c1c6712b9e"
-    sha256 cellar: :any,                 arm64_big_sur:  "30d22ddad3521ec07248910864e8caae7f8d959597663a9d21d2447c56e6639c"
-    sha256 cellar: :any,                 monterey:       "248ea1c47707f4baf30f540f50803e1f3678ebaf5c80215ed4871f96cf77b314"
-    sha256 cellar: :any,                 big_sur:        "f1cc2211ac56a2702405246535a55613855c3879885ca73aa65d76890c2aa0e5"
-    sha256 cellar: :any,                 catalina:       "c38019c153c728a28acbfe340cc86764285ec24edbdba5234b0593f83d355c22"
-    sha256 cellar: :any,                 mojave:         "d02c761bd6b52424528bfdcd56b8d469d7cdd2e55f625c719229edb7f011889c"
-    sha256 cellar: :any,                 high_sierra:    "abffeaf075db6387e636d43eb8fda9b76f02091bdb5533368306f899a46406c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b4a556fa47365d3ebf9312acddf3fc64921094161bd6d6e1bcda3df92be70cd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libcmph"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "53bff3293ce5735e72c4944503bbbbd52f450c0a4e5fc7c3575da6f49bd7fe86"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
