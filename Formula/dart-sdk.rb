2e8e00598435da0f7f2b4b9bab7dcf6c50a0f58e
Sean Molenaar <smillerdev@me.com>
2022-06-08 12:03:55 +0200

    dart-sdk 2.17.3 (new formula)
    Closes #103222.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/dart-sdk.rb
@@ -0,0 +1,39 @@
+class DartSdk < Formula
+  desc "Dart Language SDK, including the VM, dart2js, core libraries, and more"
+  homepage "https://dart.dev"
+  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.17.3.tar.gz"
+  sha256 "10fdcb6980aee7d5d04b278f337f3519af7fa6bfa6290d737ea9327954b515a1"
+  license "BSD-3-Clause"
+
+  depends_on "ninja" => :build
+  depends_on "rust" => :build
+
+  uses_from_macos "curl" => :build
+  uses_from_macos "python" => :build
+
+  resource "depot-tools" do
+    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
+        revision: "138bff2823590b3f3db440425bf712392defb7de"
+  end
+
+  def install
+    resource("depot-tools").stage(buildpath/"depot-tools")
+
+    ENV.append_path "PATH", "#{buildpath}/depot-tools"
+    system "fetch", "--no-history", "dart"
+    chdir "sdk" do
+      arch = Hardware::CPU.arm? ? "arm64" : "x64"
+      system "./tools/build.py", "--no-goma", "--mode=release", "--arch=#{arch}", "create_sdk"
+      out = OS.linux? ? "out" : "xcodebuild"
+      libexec.install Dir["#{out}/Release#{arch.capitalize}/dart-sdk/*"]
+    end
+    bin.install_symlink libexec/"bin/dart"
+  end
+
+  test do
+    system bin/"dart", "create", "dart-test"
+    chdir "dart-test" do
+      assert_match "Hello world: 42!", shell_output(bin/"dart run")
+    end
+  end
+end
