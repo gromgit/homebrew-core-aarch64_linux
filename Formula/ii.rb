af0352bd456991d3f6b6364c60f6ed568dad0b78
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-08 07:04:38 +0000

    ii: update 1.9 bottle.

--- Formula/ii.rb
+++ Formula/ii.rb
@@ -12,17 +12,12 @@ class Ii < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c9452385f64727d3e54a45e3e7e24a41bb4014df20ffd6d1ec7cd75de14749d0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5de14cd12cba7a7ff58f2c5db87c300a3dcf69268dbbfbca552dbaf575568c0"
-    sha256 cellar: :any_skip_relocation, monterey:       "760f3e4bd676ff26340b2796568d5e5b3727feb5da2094996ecb7ea0c1ecc4cc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a58cf380ab73f600dadce4712328013a3ea3b0b65009fda1a3d2dc5e92741716"
-    sha256 cellar: :any_skip_relocation, catalina:       "deb41b4c38d4b8e6cd2f3e9d8acea872a8daa694486ee59edd684248a7e74f4b"
-    sha256 cellar: :any_skip_relocation, mojave:         "c8e535b535af9adf8c3c3e760849f581d3e93ec227ae9f0ae2f30490b44e9c4d"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "dcc9e7c86395491f5a62dd87dfcfb0f1b8b89a8f5ceb4e767ac70cf60ef350cd"
-    sha256 cellar: :any_skip_relocation, sierra:         "a83511296e08d8ec1d126bb09574b02856f382f3f504b6f2b256cab6bd645ed1"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "eeba4fb4ec437895a9946bbbb00186ff05277ce9d57e8bbe29e1db5596d8a70f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "65635369b704678c7b8e9e116df8421b2599b4fda0aa728cffc86d5c10438320"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0300f524271b1dda6684b35ed4b15e532ef75ccc64a9b23299ec9f1bda14307f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a6c755033afefd3157a99c07be5b901c8b6c16043fbffca79db6dbbb4d317125"
+    sha256 cellar: :any_skip_relocation, monterey:       "c686540cf1e05a9e6af37bbd128d845d2348392c6f7e20176d068ee8ed50a1b3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2df8591a79a05357b25a2c83ad9da2c2f0e58260e04cfbae584301f4173f7949"
+    sha256 cellar: :any_skip_relocation, catalina:       "24860645a705891e28753b62ce7338764d6f46310e79756df3626c703fc196ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "71a077a04a5c61a79083459e659d252920c24661edbeeb6c8248914aac1931d1"
   end
 
   def install
