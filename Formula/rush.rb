5e50126e13fdbf3215dd8c145cb0b6c8d5c607d9
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 07:42:05 +0800

    rush: update 2.2 bottle.

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -7,12 +7,8 @@ class Rush < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 arm64_monterey: "445f930202419132422e29d8077a67883f57f84b79c655a867b53a9da1541011"
-    sha256 arm64_big_sur:  "02bf135ba37d2a68adf4d089bd893f1e8c7b4892e61e78a0720f074fdb591adb"
-    sha256 monterey:       "7b5c12cd913819383a2afb21272602fed6523bc97871bf469911d09f0fb12f69"
-    sha256 big_sur:        "433fea9129d74f349d42ecb66d67d036ee8e9e397582d23f16ad4b209861a06a"
-    sha256 catalina:       "9b152b080009d9b887921772631b786b2ddf5893dd2845f11355b637d397d027"
-    sha256 x86_64_linux:   "07ff7b8fae886c1728cc2244296bbc583b802dce004ea17a5f34b6009b06ffd4"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rush"
+    sha256 aarch64_linux: "941ed0236caca0944d74bada7143d5ada0f8a8ea5c268515abee43dc81b412c1"
   end
 
   def install
