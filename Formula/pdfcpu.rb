169a779ed9014b85b14c90acd143ab3f2f16bd6c
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:01:20 +0800

    pdfcpu: update 0.3.13 bottle.

--- Formula/pdfcpu.rb
+++ Formula/pdfcpu.rb
@@ -6,12 +6,8 @@ class Pdfcpu < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f46dd0330f54227c000c61238248d227df416744ce1fd99b7a74b975451d380c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1016c487bc865695907f891850566d7e4c173523c3bd1a98123799499c257b66"
-    sha256 cellar: :any_skip_relocation, monterey:       "e694688e96e813ad589e5adc61a236597491e92508f8e0254de767da16e74025"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0456a7517b517bcac4e7ee4715507a1d79eb0f03eb80984eb8e47108ad398317"
-    sha256 cellar: :any_skip_relocation, catalina:       "47fadfa37f5e32411565c4975ddef9a1cca7b44b685a03cf95cfd4e873c4834c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ce876f76d9a9d2cbf20a2c5c1d162126c6ce026036cd82f42c7e76f787e0082"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pdfcpu"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a2b5318f49f988e653de50e62488fedb290dc4fb48b35dd1ede7b2ed258077fd"
   end
 
   depends_on "go" => :build
