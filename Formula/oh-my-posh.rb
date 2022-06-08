e2ccde86c5155cbe074d9ea982f1a576a907c2ca
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 15:59:12 +0800

    oh-my-posh: update 7.79.1 bottle.

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -7,12 +7,8 @@ class OhMyPosh < Formula
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "19f2c6169c7692d2d9ac20983ce4a2909593fc39b8e6bf04f08ce09746ad3e71"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cff5e9415f5c50525fa58d4598bee03465419984ba4d3c04b7bbbcb39fdad73b"
-    sha256 cellar: :any_skip_relocation, monterey:       "d24b8482fba6aef3a49484037dcef2a2921c927bfa28730659f5bda9a640b7ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "50b6629a4abd8b0e1cbb2a5a66177e315dcb3f288b09b78ab579982f17f9f2ae"
-    sha256 cellar: :any_skip_relocation, catalina:       "6a35a825ebc3069614c8ba13a44aa0783f98640ba33f04dc65457b66b34f3ac3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cade5c9f646146283004919db2ce86aa5cc1fe6c9457a4a6c34fe01a8a615d9a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/oh-my-posh"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a532a6da635e2f9fefff2d9c52f911f6c868e69c9708a0f63662f31a50b35c99"
   end
 
   depends_on "go" => :build
