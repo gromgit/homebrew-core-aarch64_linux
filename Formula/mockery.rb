9449035a23b64fcabc0914154905fbf3ae21f391
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 10:27:12 +0800

    mockery: update 2.12.2 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -7,12 +7,8 @@ class Mockery < Formula
   head "https://github.com/vektra/mockery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9d34b198b93ff51e90b4f0d94879a84f805cba4c0c6ef5aeeda5627dd35bf866"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f7792a9b01f3172e313d67e09ce12ef63e9a3624d457246f839791dcd48cefa"
-    sha256 cellar: :any_skip_relocation, monterey:       "6d4c126ec4f057384c1f110db9e4829ec0211a4b3f89628a898bd396b62750ee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "aac59ffd8c933897b1c86db3e930cc3c4a8c4af478aee132abdc22e5ccbfc575"
-    sha256 cellar: :any_skip_relocation, catalina:       "0f312a81858b0144d47de7416aa24cfcc572ff6938fb2ed9ed9af3f6a620a05c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "efa7e6c47602cc82ec08eedb908dc21fd5ef2901a1dcd62e112ddce8dcd019a6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/mockery"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "89ea3971f88173afab40dd3a4a4282f98191d50080e70da9cec057c4713bbe9d"
   end
 
   depends_on "go" => :build
