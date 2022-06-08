c3d3776718f57d0d8688d2827662ac0a4239c07b
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 23:33:48 +0800

    powerman-dockerize: update 0.16.0 bottle.

--- Formula/powerman-dockerize.rb
+++ Formula/powerman-dockerize.rb
@@ -6,12 +6,8 @@ class PowermanDockerize < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b965893a233913bcc6dc0de32a96175be11ece33bb6db69a948be96626fa1f85"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5833b5267052b02eea8deb8ae57358388a498bbc691470915feabddd66e73287"
-    sha256 cellar: :any_skip_relocation, monterey:       "c67519547af3bca7f8b638306924679b87757e367fd45200309e1f6062cb34ab"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1097c8f356642794fb9c0c97882bc85d87b25fca3df3be61eb90a45231b788f8"
-    sha256 cellar: :any_skip_relocation, catalina:       "357204a92e944afa92adbf31b721b6ca29c149cb53afec160d320d8ecb2d0a08"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b905cbbef080a1823a8c8da7a3369832640b7def780d581e48c2c5ead5d53e7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/powerman-dockerize"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b9ac88a7fb0de897d0cc8b4c7f815837fee282e63d57de12fb6eda45f5ac7752"
   end
 
   depends_on "go" => :build
