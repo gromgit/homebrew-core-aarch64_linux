a24429a386553e417ff4317a660ccaea94b3eb42
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:52:28 +0800

    pfetch: update 0.6.0 bottle.

--- Formula/pfetch.rb
+++ Formula/pfetch.rb
@@ -7,14 +7,8 @@ class Pfetch < Formula
   head "https://github.com/dylanaraps/pfetch.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1a91953275ab9b2daa26d9ffe6d6f60b5ca3ce5d556223d24c1020aa9e8285ec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d3d84a024e20f9a85803389150748894f9a9dab94593af5c0ee3967a82ebe55a"
-    sha256 cellar: :any_skip_relocation, monterey:       "7dfa1cdbfc450e489c88d72808c87de7930ebc2cbc5da51c2be784eeba7076c3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3e5ceb26959ab4f137221f87eefe0ba1912695cce887b30f2e4894c699d86261"
-    sha256 cellar: :any_skip_relocation, catalina:       "f93914feee7f4e3cda77341c3bddf2cf51eb4b2aed01f6ace771db75078da570"
-    sha256 cellar: :any_skip_relocation, mojave:         "f93914feee7f4e3cda77341c3bddf2cf51eb4b2aed01f6ace771db75078da570"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "f93914feee7f4e3cda77341c3bddf2cf51eb4b2aed01f6ace771db75078da570"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dfa6a98908cbd7ac49f1fd4813011ee6f18849130d84b4c4fa3c01282a33c568"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pfetch"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "aab8eaf9895d948032d3b57c9937ae201c7472f88913f01f83f7cb4800725a5c"
   end
 
   def install
