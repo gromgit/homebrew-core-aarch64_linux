bbd0e8ea6239fa5a8abcd113f719389827ed2240
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 10:47:40 +0800

    mongodb-atlas-cli: update 1.0.3 bottle.

--- Formula/mongodb-atlas-cli.rb
+++ Formula/mongodb-atlas-cli.rb
@@ -12,12 +12,8 @@ class MongodbAtlasCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1d8bd8d53558eb15c33f351e0fc17a194a2a4eba3450fbf117640615c574f8b8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c09a27b0be83355ed47bdd9a2ae1f3a0001a0d956d5b885c2a2f8e3a57a09ad0"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d23a205eb8dbe181f30cdb22deb0ba667e3101a3af00b8463f8278cc7f55684"
-    sha256 cellar: :any_skip_relocation, big_sur:        "96f6b27fb0ad29dc7f0c7a8ac104ff24186ecaf40033153615c75d0eb5c7dfc1"
-    sha256 cellar: :any_skip_relocation, catalina:       "2f228db821e617f05c66a52bc2499ec56bce708da0163e0ef80e1d11c9600b73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6476f418353e5ff1e7aa83c6cf9c6fd447cb7d04ec9c2522e9698feed14c4afa"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/mongodb-atlas-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "227b356247b2b718a6bdfc07ad07e3d5e077a682edc086f9c9d8242d1d6d652d"
   end
 
   depends_on "go" => :build
