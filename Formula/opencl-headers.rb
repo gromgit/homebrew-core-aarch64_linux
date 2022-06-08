994346f498309376748990c14f6ac069363b8325
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-06-08 07:00:58 +0000

    opencl-headers: add 2022.05.18 bottle.

--- Formula/opencl-headers.rb
+++ Formula/opencl-headers.rb
@@ -6,6 +6,15 @@ class OpenclHeaders < Formula
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/OpenCL-Headers.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "60b2ccfedf9128ca15a2e63e5526981c3a9d3d1ab339987c9f585d5bf3e12afd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "60b2ccfedf9128ca15a2e63e5526981c3a9d3d1ab339987c9f585d5bf3e12afd"
+    sha256 cellar: :any_skip_relocation, monterey:       "60b2ccfedf9128ca15a2e63e5526981c3a9d3d1ab339987c9f585d5bf3e12afd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "60b2ccfedf9128ca15a2e63e5526981c3a9d3d1ab339987c9f585d5bf3e12afd"
+    sha256 cellar: :any_skip_relocation, catalina:       "60b2ccfedf9128ca15a2e63e5526981c3a9d3d1ab339987c9f585d5bf3e12afd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b887fed5a6c8cb4a5d7d9f87c49ac68c394295c84696783512247d653c8b10f8"
+  end
+
   keg_only :shadowed_by_macos, "macOS provides OpenCL.framework"
 
   depends_on "cmake" => :build
