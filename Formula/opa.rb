f46b66270572096c7c0059271565179b38551ad1
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 16:30:42 +0800

    opa: update 0.40.0 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -7,12 +7,8 @@ class Opa < Formula
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3a9cba5d436806eab97e55773250ba8bd04e5cef4ab523a4ea5e95f21ec80c7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40f8773b3c75d66f5d5d0636ea982d00c16535260a712cbd211aaee5042694f1"
-    sha256 cellar: :any_skip_relocation, monterey:       "e752b9f3e5d7626ca924ccb349bb9aea67ac015e2b7a51b01ee01f3b0e513186"
-    sha256 cellar: :any_skip_relocation, big_sur:        "662694af2af9c5529273c75acfa6be144e24dc2a59a67ed4f51c01846cf0b742"
-    sha256 cellar: :any_skip_relocation, catalina:       "66a3730a90b667781c72546e3f651057d22e703f401df4ebe1de479527c79988"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0f63d006e822e4e7fb8940f4b168541d588b7edda29e423067f285e99a7c34cc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/opa"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cc2c60316a07e0b898bf0a88fe761ea6fed538464ac0e6b6988adfaf73194147"
   end
 
   depends_on "go" => :build
