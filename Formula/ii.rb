0ac9ad48ffb93a2a4796f47d54e2cf22ab778911
Caleb Xu <calebcenter@live.com>
2022-06-07 21:15:31 -0400

    ii 1.9
    Closes #103178.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ii.rb
+++ Formula/ii.rb
@@ -1,8 +1,8 @@
 class Ii < Formula
   desc "Minimalist IRC client"
   homepage "https://tools.suckless.org/ii/"
-  url "https://dl.suckless.org/tools/ii-1.8.tar.gz"
-  sha256 "b9d9e1eae25e63071960e921af8b217ab1abe64210bd290994aca178a8dc68d2"
+  url "https://dl.suckless.org/tools/ii-1.9.tar.gz"
+  sha256 "850cb323b583d261b51bda9993ee733334352a8e6ca1e2f02b57c154bf568c17"
   license "MIT"
   head "https://git.suckless.org/ii", using: :git, branch: "master"
 
@@ -26,9 +26,20 @@ class Ii < Formula
   end
 
   def install
-    # Fixed upstream, drop for next version
-    inreplace "Makefile", "SRC = ii.c strlcpy.c", "SRC = ii.c" if OS.mac?
+    # macOS already provides strlcpy
+    if OS.mac?
+      inreplace "config.mk" do |s|
+        s.gsub! "= -DNEED_STRLCPY -Os", "= -Os"
+        s.gsub! "= strlcpy.o", "="
+      end
+    end
 
     system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    port = free_port
+    output = shell_output("#{bin}/ii -s localhost -p #{port} 2>&1", 1)
+    assert_match "#{bin}/ii: could not connect to localhost:#{port}:", output.chomp
   end
 end
