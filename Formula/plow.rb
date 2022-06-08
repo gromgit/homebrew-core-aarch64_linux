5a00083bb358a83e5595224d2f1447c5fffc0155
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 22:24:56 +0800

    plow: update 1.1.0 bottle.

--- Formula/plow.rb
+++ Formula/plow.rb
@@ -6,13 +6,8 @@ class Plow < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "efe290ac3d86bec107df0e22c15f4cbdf960dc88df929b4a7b578440b3412389"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb13e15c3cb95c4e9ae36c498f5eafbbe2401bba7846e3eef7764e56927bafb2"
-    sha256 cellar: :any_skip_relocation, monterey:       "f42f7ff80afff18956562d7c1d98a09571efb50951fe88e8363d2a16caa74dd0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "35efcee910b6391bcb00a74e174bd2c7961742c75b7ce5d81cf739f5f8907e41"
-    sha256 cellar: :any_skip_relocation, catalina:       "d30cae49b41472ed81c454b2e5ef2031e1e46307f8b838b4892fedd80c41d318"
-    sha256 cellar: :any_skip_relocation, mojave:         "beb6ba83885124eb4ba8e2e4bb5bcaf8951bb574ef8d9b94c2c48307b92caff6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "528c3066e659d894685009cc237c87d3dabca4a64c5a0d2e9e2fc8d021ecdbcf"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/plow"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "065cd2e338c63fccc7cddbef1f804b19d36fdb517cc4488bded0433feeec313d"
   end
 
   depends_on "go" => :build
