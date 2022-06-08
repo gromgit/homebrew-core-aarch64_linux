f6252bd8439b42947a30e3adf9b9d8cf53a0cf50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-08 05:41:55 +0000

    selene 0.18.1
    Closes #103211.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/selene.rb
+++ Formula/selene.rb
@@ -1,8 +1,8 @@
 class Selene < Formula
   desc "Blazing-fast modern Lua linter"
   homepage "https://kampfkarren.github.io/selene"
-  url "https://github.com/Kampfkarren/selene/archive/0.17.0.tar.gz"
-  sha256 "c9dd792345c2802ed4bc41fa94a21945e5d67bd51d3c13fff61bbe2a08c0a287"
+  url "https://github.com/Kampfkarren/selene/archive/0.18.1.tar.gz"
+  sha256 "34b886e0d616f54277f434918e0ef6003e987eb12495ab2217477678fa53ac2c"
   license "MPL-2.0"
   head "https://github.com/Kampfkarren/selene.git", branch: "main"
 
