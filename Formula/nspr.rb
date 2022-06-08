2c859f60e16f7e088b93c33817e4c7a539d85e13
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 14:41:30 +0800

    nspr: update 4.33 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -11,12 +11,8 @@ class Nspr < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "36817ecd49395967e9aab79407d26ad6146b05900cf80135204ba5e7dee11b96"
-    sha256 cellar: :any,                 arm64_big_sur:  "4559755473db5d74932b2304f4be480147cf4ce1bbc2a36ab4ff1747e31dd864"
-    sha256 cellar: :any,                 monterey:       "b7e83de84f2acb392f8f4a0fc95d3f2bc23e902f506c28a731e8712bfdeaf108"
-    sha256 cellar: :any,                 big_sur:        "e07523d89970090c42cbf650924713fff254ecb9efcd8b1f1db5813fdde60fef"
-    sha256 cellar: :any,                 catalina:       "0f08ec37a94646830d499592cc4271f562e7feb8a4ffe1522d818d0be227839b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b46f42da9640c71484ca2c4f234b960efc7e2fd5002bbf0888f70311d577b35d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/nspr"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c5bb610104c95003867d421009c1f4ba857e443de7e1e224b5ac11ee2a4950bb"
   end
 
   def install
