8a036aa37f481bfdaa5bfa4b65b5c0fb128054e5
Caleb Xu <calebcenter@live.com>
2022-06-07 20:33:47 -0400

    gtk4 4.6.5
    Closes #103176.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gtk4.rb
+++ Formula/gtk4.rb
@@ -1,8 +1,8 @@
 class Gtk4 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk/4.6/gtk-4.6.4.tar.xz"
-  sha256 "a79a2bbcc7931b703fc4fa1fcbe1b906e229a1522d854d522801766a79bcac9f"
+  url "https://download.gnome.org/sources/gtk/4.6/gtk-4.6.5.tar.xz"
+  sha256 "fa42c371f49c90916711e15591d87d4bee4438c27bf0692715581807628be9c2"
   license "LGPL-2.1-or-later"
 
   livecheck do
