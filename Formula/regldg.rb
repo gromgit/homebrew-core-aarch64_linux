e3c2957b83f07a18aaae2985aaf23f90b617a81a
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 05:25:50 +0800

    regldg: update 1.0.0 bottle.

--- Formula/regldg.rb
+++ Formula/regldg.rb
@@ -10,15 +10,8 @@ class Regldg < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d6456b6abf558106d2cae0459145d3070b07dc37d3757f84d325f11aaa7badf4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "30966f99bf5fa0f3af539ce5b8eaca2666db734ac561d2b3a261532636b2a54c"
-    sha256 cellar: :any_skip_relocation, catalina:      "6c69006dc5eb93be0eb6b39cb396e59e8c09aa5d65f56a216cd19753a7f28232"
-    sha256 cellar: :any_skip_relocation, mojave:        "15f7e95f3d84d091a942e836ab9a27b3df2594e3f378da26f10371e7ba01be5c"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "45950c0b432b227711570e3b9ea79fe9bf96b3239a062c5a736f9a3fdf294fb5"
-    sha256 cellar: :any_skip_relocation, sierra:        "26f12ca7e41b36a167d94f403b97557490fd1ad0ed1a2d4d0b30c86164ae9d39"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "52c64d6766b68a1ed602d3878368109d3ac3e5e60d6fc14a4606518d14f6e678"
-    sha256 cellar: :any_skip_relocation, yosemite:      "c4157a77e2620b868b2dbbb3ebf126193b238c6a69d2a895420950d4203d7a17"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69cdbf331fd88fc5891aadd99835d8497f30a27a5bc42c3e10c0d32792f43d0c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/regldg"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9b113b771386ff850c244865d885251e8c937737cf315617d1be13892e6f0c80"
   end
 
   def install
