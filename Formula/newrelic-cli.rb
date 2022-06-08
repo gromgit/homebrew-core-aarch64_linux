035ac0edfd6c36c114f2570a213b35d857989bb8
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 13:50:14 +0800

    newrelic-cli: update 0.47.1 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -7,12 +7,8 @@ class NewrelicCli < Formula
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2b55b6d29c2849c4788a9d62f6cc953e45905507a14a4b3ff45128f6a412855"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47be514aacda82fc053b138b4abdaf93b1d2280627de69b6347319b767fd6664"
-    sha256 cellar: :any_skip_relocation, monterey:       "2d1f7dbb902cc2c716a4cf3c543e81a507e32277a95ae244dafd6d99ebe77350"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f222d002f799005e68da319ab81cea4c2e7d2360bafe51b50fdbb7780b1e9d64"
-    sha256 cellar: :any_skip_relocation, catalina:       "062e8d7eebe763b5e4a9e55226273688bf8b9617b0cffa206b2d809dd3e2a452"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0bbc9901360a9268403a5cb1cc6b0a84d0a48bb46c984589fafc69a7225ae60b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/newrelic-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "019398482eddcf1a52993f5bc1ee42bebef793c143b778dbcad293909a111d62"
   end
 
   depends_on "go" => :build
