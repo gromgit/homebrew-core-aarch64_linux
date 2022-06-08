62ec94ab99fa8a7b03aa6969493a8bd980e34473
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 15:54:38 +0800

    odpi: update 4.3.0 bottle.

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -6,12 +6,8 @@ class Odpi < Formula
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7244b4b1b2491a0bca077aa4d2bb530df059206e56d1a5d6103983f9e88edaba"
-    sha256 cellar: :any,                 arm64_big_sur:  "8315751a4d068e761b9710daa4251edd61c01752c021fc93045f02083e5dec14"
-    sha256 cellar: :any,                 monterey:       "a6efa2963f2a11b1316f7f883cdab0ba8a99b0a1e1b5324c9c960094de152e85"
-    sha256 cellar: :any,                 big_sur:        "7dc8341678ef3c3b5e8e3ec798b50aab4fe5f9330b57931c55a25e8f6c457ca5"
-    sha256 cellar: :any,                 catalina:       "05db0489fa10e27600c35f22669e3090b3bf17ca00cd7c97966f62e1d314c7d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1b415bbfb0f8b13d559f2b5f2119a96e24d58d1a35d8f2ecde4ca76169d74028"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/odpi"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e7843aacd745214e5b8d3f096b5f0bd86c25cd80af0876957a739df56eab13a3"
   end
 
   def install
