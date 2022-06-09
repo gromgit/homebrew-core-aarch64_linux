3587fd276a4a29510613c21026a9afbf1c4bcccc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-09 05:25:46 +0000

    node-sass 1.52.3
    Closes #103285.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -3,8 +3,8 @@ class NodeSass < Formula
 
   desc "JavaScript implementation of a Sass compiler"
   homepage "https://github.com/sass/dart-sass"
-  url "https://registry.npmjs.org/sass/-/sass-1.52.2.tgz"
-  sha256 "4d54e11813d188e1b528b870c5b46fd77d9961fa11a284194dc2558fc80e8a34"
+  url "https://registry.npmjs.org/sass/-/sass-1.52.3.tgz"
+  sha256 "e064b7f3c8b0a7202c2d8ae5890a8a22cf243510f2fe0968a8e78b73150079f5"
   license "MIT"
 
   bottle do
