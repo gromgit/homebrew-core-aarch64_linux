64fc710dbc2af58041b923243cf4698c2a17a576
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-09 01:32:51 +0000

    convox: update 3.5.1 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -12,12 +12,12 @@ class Convox < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4960f962536ed9497465e43d06861c7edcf72c378f4dd6d7ce5a28f70f4faa69"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b564b17831d6f289ae4803d88a7c9cc1fd8bcebbeaa301b1fa4b0b7d3a7908f1"
-    sha256 cellar: :any_skip_relocation, monterey:       "4a3851e4a32aaaf37f82b4a0b2b1c57408743137f643ee120dffb1145bb79ca2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1906cfaaa3393df25b0adcd21553112790e718f2534a774277ffa90e1f797406"
-    sha256 cellar: :any_skip_relocation, catalina:       "f97f14ee98931650bacd57583d1117ef0090f9a85e1291e74e928ebcffce2513"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "290d2fc2b019243533f064659ce16bb2b58c72ba3b2b910ba48c5f5f8c8e78a1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "98440e6200545507ab3d4cee35075495dff6b915abaed3da4470f795f1179da8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d4bb76b4c8ce3d2acd3167fb7df06c447d0fd0dcd547dbda26d20ae1dc92a404"
+    sha256 cellar: :any_skip_relocation, monterey:       "bd1b840fc6836923c0b401486da91f0212095eb4e62b86bf65b9b6ddd13f6147"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b7b4762af923add4583acb93ec69b639f3d132ca2ae067f456b9d3f7572bc140"
+    sha256 cellar: :any_skip_relocation, catalina:       "4821bb829c9e7f275f9553d83da4742ff4a6ce29fedd71b268b9e55a162ed169"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ad33224a2575d9de5ec593cc237c07eeb031a14ec258eda38f49713558b355c"
   end
 
   depends_on "go" => :build
