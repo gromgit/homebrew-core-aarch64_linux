803be004eaaa1d78bc9919b5cd0cfb97e6c514e0
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 18:17:15 +0800

    tektoncd-cli: update 0.23.1 bottle.

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -11,12 +11,8 @@ class TektoncdCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "854facd575ffe796bed332b0a5718761ab93070a7e31569f3902383ee367ee02"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a6521c797be2cb7afab88984e3487517f5fcd86702463c174eca40d8dcefc3e"
-    sha256 cellar: :any_skip_relocation, monterey:       "cf30f50ed353283c3da876f1616f2633d471992438125f3be766087b79140109"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6301e48ae1b13c59e2edf6d3645484233f0417c395d58ba8ba8f69627f8a57b0"
-    sha256 cellar: :any_skip_relocation, catalina:       "917f87b9949d7e48dfe8376b3fef5a841036e0737add3f41582ccb88433db78b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4437c493f8e8bd5a9aa538d97b68d86d389cfce0c684bebe9f3c346c4680fd86"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tektoncd-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c9f28defc59a8d0824ec6f3f3b1e6aa12e5b90ec9f97d9f293c1d177423f5c82"
   end
 
   depends_on "go" => :build
