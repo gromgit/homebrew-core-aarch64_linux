e9a2318808aaebaaf12dbb99c19db59e9956c02c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-09 21:04:33 +0000

    libre: update 2.4.0 bottle.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -6,12 +6,12 @@ class Libre < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "aa4dbc36738ce82ed6e91cd77406561830f4dae19b8e7b78e4386c0a186d7ea6"
-    sha256 cellar: :any,                 arm64_big_sur:  "d4df50164520a7e0719b4966fc9b03f56cab24eb690be348dbff997f453b34c0"
-    sha256 cellar: :any,                 monterey:       "0b125d515d560f8fea33a3c8f971ee814c1aa60016b549ba82a2f8c0a55438e1"
-    sha256 cellar: :any,                 big_sur:        "4f4f2d4306780227fa8dae00e79822db56b76cee4d8634cf29155617831f29d6"
-    sha256 cellar: :any,                 catalina:       "d32a9749b10b3cb069bf141daaed0070360bc385a14c92a228f99be3cf4b9615"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f3f51ef121a2c0b1116e96725e1b9daa9caa58d6109b77d896f33779c4e0758"
+    sha256 cellar: :any,                 arm64_monterey: "cec4a0c328582b03b7b386e11d99ef2d70e3b7506071d88c60ddb6498e4aa69d"
+    sha256 cellar: :any,                 arm64_big_sur:  "bb013249efb0bad8fe1ec42eb0b2f9694ad1ccf5ebe5f167e16006d3d07170f4"
+    sha256 cellar: :any,                 monterey:       "83fec7ba79ae65020328ea93eae1a2b58250e6cdaa3d4d3b630f8ff38774170b"
+    sha256 cellar: :any,                 big_sur:        "0d17e28d0ce1946cb9a5b723b73b9c2ca3b1a4990297e732fe4689eb2a595dac"
+    sha256 cellar: :any,                 catalina:       "714908f0cec04646844815add153e9a7b42028981cc03b4d60e40ce0d92bfa69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cd7b38953d5d92d54e8c58d07883cbc451275e536b5cebf2ec1f998fa8daad22"
   end
 
   depends_on "openssl@1.1"
