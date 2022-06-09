93d4db3672c3ecdc62a581023394e5580ec2eeb9
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 06:52:28 +0800

    tflint: update 0.36.2 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -7,12 +7,8 @@ class Tflint < Formula
   head "https://github.com/terraform-linters/tflint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b071d358d67c1d5d5e639b1f553aa1f762511dd878b822a0f8d42f5b0a6e2d6e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bab30b420d44eb384c33e3f3f8079ac72a243e3bea300a85619d02fe7290665d"
-    sha256 cellar: :any_skip_relocation, monterey:       "437acb8953f98a09edb8222c6496bf1013f1fc75e9b852488be964cb4af206f9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "07dfcf7bd0435bb0cea9bc1593ae69c366dbc096e79cd314a58dec099174e590"
-    sha256 cellar: :any_skip_relocation, catalina:       "96ccf781b15bfeae54e456e07f083e9ba2ad2ff97d6b56ecfa1ea685f6e383ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "489b86cc9ef0f588902e3cb91d18bb25523fb587adf527991cda82e69b2499b1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tflint"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a6e59f8311742a45b73e0cb2316dc314f4db8cfd93f7c0bc46f90c9683926054"
   end
 
   depends_on "go" => :build
