8ef2d2f32780258a93bc3910d382b0a5109e63bd
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 15:03:54 +0800

    staticcheck: update 2022.1.1 bottle.

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -7,12 +7,8 @@ class Staticcheck < Formula
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0c9327adaead8b33acf9ac31a7543f4881b443fa064acc96b6c034a7bdb764b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ec11a5ad26ae2ee8e5d31f2ceafbc96b38c44b0ab63cc6d6375ae01b6c71bb6a"
-    sha256 cellar: :any_skip_relocation, monterey:       "98959fc5a4362c9e9bcaa52b6d9012975805a751a40421edd11cc7d14c36db94"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a4f53c3b2bf2c2b94ee71c8635eeea9626b87313809f601b0df5f71784daa199"
-    sha256 cellar: :any_skip_relocation, catalina:       "b05ed2c4f9a64d1d3c4fe43655188c0e2a5ffd6c3cf1180e2437587664d07b8a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd69e33fa3c007c141e6a767933c0b086ee51e1a6f073afa022d1cc56eba3c56"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/staticcheck"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e8efd5ad598b5c80ec158abc30d480b40decc3e9136e3329449f19ec8b7179a4"
   end
 
   depends_on "go"
