0bb52d4ecbc1713891a35f8aa497f36d3403c85a
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 06:31:19 +0800

    testkube: update 1.1.0 bottle.

--- Formula/testkube.rb
+++ Formula/testkube.rb
@@ -6,12 +6,8 @@ class Testkube < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5472c21536cc96b8c604d8206ce2cf6fd8ab4d9a7a4c89b06188edad2ccee81e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5472c21536cc96b8c604d8206ce2cf6fd8ab4d9a7a4c89b06188edad2ccee81e"
-    sha256 cellar: :any_skip_relocation, monterey:       "7aab954a2f73385966b0c785c28b3d82044b66ec02e7e51f1ad0da99568f0e9f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7aab954a2f73385966b0c785c28b3d82044b66ec02e7e51f1ad0da99568f0e9f"
-    sha256 cellar: :any_skip_relocation, catalina:       "7aab954a2f73385966b0c785c28b3d82044b66ec02e7e51f1ad0da99568f0e9f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "850787cc6f8108252c515e535473bee53fc90eb12f6649f2aa8d3e3fd5f40714"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/testkube"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bbc00b87f15ee2a327f53b749e10d93baffbc03ba2113d620913f40063150573"
   end
 
   depends_on "go" => :build
