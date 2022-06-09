18b666d27e9d6498f71dfb0a52538029f14064f7
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 08:54:30 +0800

    scw: update 2.5.1 bottle.

--- Formula/scw.rb
+++ Formula/scw.rb
@@ -6,12 +6,8 @@ class Scw < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5e0ac68f17ccae80eaedb97c664036e83a3895de6c6270e170c7d3334ad99d5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffdeaa85b9fcfaf5bcb6c64d396fc0d3f7ef7fd32c0022386f6cabf4c0e41288"
-    sha256 cellar: :any_skip_relocation, monterey:       "0987c424fd44bf9db4e4f1216afe91d5feada2163bd65326c8bd1cbdafa98e96"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b899a77c674e34c350ca9f87c3afc8bd91b146eb5ddf9e99af27ae49e082d8c4"
-    sha256 cellar: :any_skip_relocation, catalina:       "36bfb953aff18e6fc48f2f43c00b805ee21339a04508b8829b9e5661afba6a46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2417dd45ec659a6b2005e1bff6c6f85cf24d4c5ed1ca9e16d94d05d4e1b6df2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/scw"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a71b42175c5938d0fe7803cc823a29ccc7e95319465c80caf85cfbf1b9d29efb"
   end
 
   depends_on "go" => :build
