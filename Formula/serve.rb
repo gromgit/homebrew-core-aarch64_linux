54a42446b8b821acf3f11236d0da80c4294f97b2
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 09:41:32 +0800

    serve: update 0.5.0 bottle.

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -7,14 +7,8 @@ class Serve < Formula
   head "https://github.com/syntaqx/serve.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7f453c52d159e364237cf66103ba3a09d6abb0d2cb25701f78ec5640ea9f69ee"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99f115fe9c19d946db2604146c1bc1f1b85b5d2bb840951f0d105e031e4b0f0d"
-    sha256 cellar: :any_skip_relocation, monterey:       "da0614469aa0f0e59e54689c9e0d63e6dd442822548d5a9cbc11bf0d1e1005ef"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b56e69171f148421442997d819297a608316565d3664d04b771f1c7b67f8c4fa"
-    sha256 cellar: :any_skip_relocation, catalina:       "b6358efb493acb673bf7513a252f9f48e9ebb2c6a7824208d89ae631cfad9439"
-    sha256 cellar: :any_skip_relocation, mojave:         "9521fba08dd7bcfd843464b3c12a4f841007f28052104072768e052c194de6aa"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "0735a5f8002970bc5d1cb1ca68681713de9dfcffc7eeb30bcdcb7fc33bc58551"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "683623afac3844c56554be98423121f2e3bb488f48afdb0ad173e3392180dc22"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/serve"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1fced5c05525c9f144dc3a82a62e678ddf2e86d581ff2c6f071cce2ec1abfc00"
   end
 
   depends_on "go" => :build
