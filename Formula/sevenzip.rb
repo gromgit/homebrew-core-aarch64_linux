dfb91a9bc098ba6d12f1a7aa4750c009a0dade8b
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 09:48:40 +0800

    sevenzip: update 21.07 bottle.

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -12,13 +12,8 @@ class Sevenzip < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b7283890b7e26f7049acc6b39f4f39d0d0bb6e1d70cb06173bcef4c51e7ce9a0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab906227f52335777f0da449a9cd4e26a5e4dd144bdd1bd8a1c717122f003749"
-    sha256 cellar: :any_skip_relocation, monterey:       "16c9f1e8e2596337d06be004b9fc406a1268dc1ed0256fb2842bf9dde3644524"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dadaed910b2c853c75b965c25cfc7601c8bb4cac03ab98d53c5c9174d0088f4b"
-    sha256 cellar: :any_skip_relocation, catalina:       "c627888d7cf8967a729cbc48d3b7f5576a296a96b29f5d17ad863a0e97550a09"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "36f227bae1087fa8b9523d9fcce434ce496201287f81a18c2a65668cca544e7f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/sevenzip"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e33674a8788f3d80e1c18765615356014f527d7e35d5accda96817409d81db1e"
   end
 
   def install
