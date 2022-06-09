faaf8678a355d71dc020889813af782894c9bf35
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-06-08 21:35:09 -0500

    eget 1.1.0 (new formula)
    * eget 1.1.0 (new formula)
    * eget: Update formula
    * eget: Update formula
    * eget: Reorder tests
    
    Closes #103276.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: Ethan Kinnear <ethanjameskinnear@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/eget.rb
@@ -0,0 +1,29 @@
+class Eget < Formula
+  desc "Easily install prebuilt binaries from GitHub"
+  homepage "https://github.com/zyedidia/eget"
+  url "https://github.com/zyedidia/eget/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "0075a032e31e0525a8171b326ed8fb923e3d1e5e30201557d6820cf2ffd047ee"
+  license "MIT"
+  head "https://github.com/zyedidia/eget.git", branch: "master"
+
+  depends_on "go" => :build
+  depends_on "pandoc" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}")
+    system "make", "eget.1"
+    man1.install "eget.1"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/eget -v")
+
+    # Use eget to install a v1.1.0 release of itself,
+    # and verify that the installed binary is functional.
+    system bin/"eget", "zyedidia/eget",
+                       "--tag", "v1.1.0",
+                       "--to", testpath,
+                       "--file", "eget"
+    assert_match "eget version 1.1.0", shell_output("./eget -v")
+  end
+end
