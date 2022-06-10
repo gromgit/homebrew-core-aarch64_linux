680e64c746171eb5ba844975571121972d904c32
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 15:05:59 +0800

    wgcf: update 2.2.14 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -7,12 +7,8 @@ class Wgcf < Formula
   head "https://github.com/ViRb3/wgcf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "218e65c535faf4c931b52f49a3993f2e161ca67459c0af2028258a45e1e215b4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "934e17a34b48e2f56aa4278f30de006cc5ca781d31bc09851ad2b3337fa65963"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb54096917d918e1f6a59cb7b434a142c5251caebbab4da14ebd0a5eb2dca0bb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e7bb88537cce776ca52ce2e8e5cf3fbb803026f67e21a2a2871670b36a0978b1"
-    sha256 cellar: :any_skip_relocation, catalina:       "7b4e81922a4b6ca2d83d0ca32e98dd54c8815f357f2dcb4b9d3964e76fe42645"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8a4e5e01061db8ea8d0f9ac3adf8d851bada70a3a021eb43938888bd61ef6e8d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/wgcf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "93fb969af13c9f8f423b88cb0e7c892370b84e24aa613ad154593eef5f80e209"
   end
 
   depends_on "go" => :build
