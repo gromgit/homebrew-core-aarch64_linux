a295bcd70930458a5bd5ac85ff63e79213e34672
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 12:18:54 +0800

    vale: update 2.16.1 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -6,12 +6,8 @@ class Vale < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9ca5b46965fc83d57a042ce97de34844a7a5713a70351f3a86a43dce1baac745"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "69aeba3d79d02c49ee10a6669ee05df36b9fed30f0a85f243a2544bb9f2d66ef"
-    sha256 cellar: :any_skip_relocation, monterey:       "146405ee818169338237daaba4fa9e279e3a7a9d61e8088402f7e06777afcb9a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dfc62368ffde2333a9360bfdea54c5f5318eba377514aa45da8b426a094de1c3"
-    sha256 cellar: :any_skip_relocation, catalina:       "983f5c4537b581b98ed5b36ba1ad8f400760b67a79508c939ddae856c463ea7f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a0d4bdb89504d6834ca529967a35dc0bfee46ac1b20ba93ecaec07fcdba4e5e2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vale"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0f714cbd9daf7f717e445e881d751d34003998ba59bf4cf9ae6e990c607426b4"
   end
 
   depends_on "go" => :build
