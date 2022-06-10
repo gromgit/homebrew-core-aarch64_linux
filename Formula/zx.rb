c6092ccada89ec4511d6779120310117c99c523d
Rui Chen <rui@chenrui.dev>
2022-06-09 21:26:34 -0400

    zx 6.2.4 (new formula)
    Closes #103348.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/zx.rb
@@ -0,0 +1,32 @@
+require "language/node"
+
+class Zx < Formula
+  desc "Tool for writing better scripts"
+  homepage "https://github.com/google/zx"
+  url "https://registry.npmjs.org/zx/-/zx-6.2.4.tgz"
+  sha256 "4989d758519b7c5e82692ba5247e98d28b8acee064a2572b8fae9956d4a20997"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.mjs").write <<~EOS
+      #!/usr/bin/env zx
+
+      let name = YAML.parse('foo: bar').foo
+      console.log(`name is ${name}`)
+      await $`touch ${name}`
+    EOS
+
+    output = shell_output("#{bin}/zx #{testpath}/test.mjs")
+    assert_match "name is bar", output
+    assert_predicate testpath/"bar", :exist?
+
+    assert_match version.to_s, shell_output("#{bin}/zx --version")
+  end
+end
