b613d9a9d17b14ec54ce24a492e8e72d65b72024
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 14:18:09 +0800

    wakatime-cli: update 1.44.0 bottle.

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -8,12 +8,8 @@ class WakatimeCli < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9409d2962406666dcc76f31c95598d172b36424da435cabc990ef87a249e923e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "782bad0eb4d4c294c3604463663891f044307d58258ee027c0051af28410e834"
-    sha256 cellar: :any_skip_relocation, monterey:       "26c5d21777a2f95da753966c2298afd99c5f42c61f9de4611fde65d8d212cfcc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "454ec02a875eccf124b169394665790358bfcc7b615eb4e1b72bbc261ace6d0b"
-    sha256 cellar: :any_skip_relocation, catalina:       "70c05b7e4655c109dc8e608efbfb1200bb1c95aea33f04a727f363a63192875c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cbc2d4f95e4cf4e00e7ee13faf239c5389fab6e1876d47e4d9037bdee0dfb0f2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/wakatime-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bf10134e667ca4898c76dc6f5f95db4d96633d53995217d4f7f18334969142f8"
   end
 
   depends_on "go" => :build
