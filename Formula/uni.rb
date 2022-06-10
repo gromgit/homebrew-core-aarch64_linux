95327ffe6f2751b75678d6aeafa2f4f1f6489ec5
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 10:40:58 +0800

    uni: update 2.5.0 bottle.

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -6,12 +6,8 @@ class Uni < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "84c7d34fcb7282ee83755530db6e2a95cfbcfe9e532471a40662626c2306357d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eec5604de6223abf296f5816ef9ce9c301e5afb807ff9311ee4ce6ae1b14f7b3"
-    sha256 cellar: :any_skip_relocation, monterey:       "3353fdff0ae506db6db053cd822a41d42c925a2b4095c25e30fa1ec0654e87b5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2d7850c4eea36c669444b2e97c3a7de6fcb3ddf15be0fc2a71b27639c89799db"
-    sha256 cellar: :any_skip_relocation, catalina:       "d53c6fdadf9b1b19b705db1462343bf298c3870718427baecffbfff7453a3776"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1f375e073263ce56706c096ebe8ad3c13aee597f4a7ee752538d7e047a9eaac9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/uni"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1e9c31df33193b702b3d0035f96f8dbf4d81018f71050745f9dc9373ac6b36f4"
   end
 
   depends_on "go" => :build
