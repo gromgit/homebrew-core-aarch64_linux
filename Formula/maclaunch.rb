cca3abcf18d51c522f7d4cb78ad5755f281b69c9
Rui Chen <rui@chenrui.dev>
2022-06-10 20:06:58 +0000

    maclaunch: add 2.3.1 bottle.

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -4,6 +4,10 @@ class Maclaunch < Formula
   url "https://github.com/hazcod/maclaunch/archive/2.3.1.tar.gz"
   sha256 "abba1f7cffd7f694b23745f6ccc137b17b6c9ea38fe2fbb55a8bd9646f6ae1a1"
   license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "46c223f29cab091c07512a07eabe567730dc3a555f04b20dbf3b85d887aa8087"
+  end
   depends_on :macos
 
   def install
