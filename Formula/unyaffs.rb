3df5fa2726fcd28cfd5e6db604a86ca723b56455
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 11:19:10 +0800

    unyaffs: update 0.9.7_1 bottle.

--- Formula/unyaffs.rb
+++ Formula/unyaffs.rb
@@ -12,12 +12,8 @@ class Unyaffs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6bcb9d49cf20172488361e571f7411b6db6e42e9052cf005c4035c6a223d5c0d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b70c51c64fb6a69a0b26295d8b1d444c9a1a3b69f0283449657039537074fc64"
-    sha256 cellar: :any_skip_relocation, monterey:       "1ff1b1841d784d1fccc11d986113a60bad0d61d3faf21ce8f456d8b47d00ae1b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "961d0e37689b4e339382d8354c452640a52df505b2466e49e99775a6870f654b"
-    sha256 cellar: :any_skip_relocation, catalina:       "936d36f9cbe3df837dff0759f65c46e6083c922c1a8e7e504a6ac3b734fd3805"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ea922f7eba91f9c6d6f3769fc9491e0f1414d9ecf006855414f8a544ef01638"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/unyaffs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3d196f972a2e97987012c4cf369a79d2f9b2728ee83aaa5cc107e63b81075460"
   end
 
   def install
