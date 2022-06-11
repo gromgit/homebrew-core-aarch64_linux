ea3ae70721808a2cda44bb7b646fb0265e88f811
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 06:15:52 +0800

    libidn: update 1.38 bottle.

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -7,13 +7,8 @@ class Libidn < Formula
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "b36c5a702e23a79b713fa8ef1cde2f45f3547507146928d25649e34704b89810"
-    sha256 cellar: :any, arm64_big_sur:  "7b3adb7115f8e786852ce270f633f726525f0017ce2f44b99b1b05c42c672c25"
-    sha256 cellar: :any, monterey:       "97ae6e40039b1abaa15ea01851a407c6330ece0687f436fcac14f2ed1ed7bfee"
-    sha256 cellar: :any, big_sur:        "edf0fcd338457708fb6ee986f85c380b744d7ea7b9e03df1bec892959d2a9e0d"
-    sha256 cellar: :any, catalina:       "ce069c2b604d6dd0194541bc510a43d0e1eecacd816dfa4d2f537dde7de3cd5f"
-    sha256 cellar: :any, mojave:         "1e5c4afc066727df497031e2cefbd012fc7b5b4e4cee793cd4b84f68bf9ff3de"
-    sha256               x86_64_linux:   "cdebd0a0f73d6fe29f8ef539aa1be4a8f576245aa07697de123bf436bfdd24d0"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libidn"
+    sha256 aarch64_linux: "15426f27be74fa059f7a79f6811028b7df80f8e914f28265c440786924f14a94"
   end
 
   depends_on "pkg-config" => :build
