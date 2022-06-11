81793d1d0a405c3f50cd0edf791be7a4a6a3eb9e
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 22:49:20 +0800

    dune: update 3.1.1 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -7,12 +7,8 @@ class Dune < Formula
   head "https://github.com/ocaml/dune.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e893ba9599a68b8b1e080849f1f96955ab6820a53f60bfe4fdf1713a098b6a1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6e5d5eef3c24376019f907793a422a9e95b9647440bfab286405aa8e48ad3e4f"
-    sha256 cellar: :any_skip_relocation, monterey:       "37cf75fc9a8302974aa0322d8a5e07130be629f2aa20b9dc376c8c0fa79465eb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39dbc498ef4d5d7cd1858a0a13b3e97561d468cabbda39f6a6a520ebd85f3ebd"
-    sha256 cellar: :any_skip_relocation, catalina:       "4a577eaf6533fed3744747f530644879e5b039967698597ad24266d1f3834f51"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7cd81f6d26d3083dc2fde2a97dfe2e089548e09de2b5b5b1f06c874889b98ff9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dune"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e929273b4799a0c1ba3bbe6a0a281e7f7f488f428127add1b07dd1ae3dd82b60"
   end
 
   depends_on "ocaml" => [:build, :test]
