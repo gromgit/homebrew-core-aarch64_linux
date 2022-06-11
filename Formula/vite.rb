8218fe5bf68c03634a71df8630692885531f8934
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-11 05:45:58 +0000

    vite 2.9.12
    Closes #103452.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vite.rb
+++ Formula/vite.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Vite < Formula
   desc "Next generation frontend tooling. It's fast!"
   homepage "https://vitejs.dev/"
-  url "https://registry.npmjs.org/vite/-/vite-2.9.11.tgz"
-  sha256 "d2a2a5002a83dbd77ac0ec780dfc92c312f98a6b9c950e4c98d30279e6ed5549"
+  url "https://registry.npmjs.org/vite/-/vite-2.9.12.tgz"
+  sha256 "f6f455e02bc7669a313ff89d1df9b9a4af26932f7523ffb06a653ad15613a53d"
   license "MIT"
 
   bottle do
