476044ee616704fc0fa12c15b054bf4cfdc8cfbe
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 14:11:51 +0800

    freetype: update 2.12.0 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -12,12 +12,8 @@ class Freetype < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "be3cb9b02246eba786391282c6a9a2f522ef6675eda2aaef77ba72cbce7e4567"
-    sha256 cellar: :any,                 arm64_big_sur:  "171677a9c53044381b2a1d9e1060e6153617e72ddaed4867b95f1982850a9e5b"
-    sha256 cellar: :any,                 monterey:       "3424dbc7bc95ec38890c3ff7b6bd3c6a1ed6e56a9a42814951076f4485516547"
-    sha256 cellar: :any,                 big_sur:        "e43fe6566bbb42a76cb09e3bb5a0efa201a6a25555ae4c057d856e20fb6d540e"
-    sha256 cellar: :any,                 catalina:       "c16bc37cc0d9840f87b7f89eb54f6c3e79f3757d8eb1acc371dd5af5d5cdbb7a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9bf649c16033dc4fcea694384136f8ee13fe2f924b4a443b02fd7bab0ff368b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/freetype"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f1bb0a5a5ad96684d7fb7e8c608f655c132c1732ee0ae2c665b95406f11ce315"
   end
 
   depends_on "libpng"
