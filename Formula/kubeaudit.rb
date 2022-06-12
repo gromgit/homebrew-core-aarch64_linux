d28e1027c7acdbb6698e637b4b7e73b07c8fe614
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-12 15:04:44 +0000

    kubeaudit: update 0.18.0 bottle.

--- Formula/kubeaudit.rb
+++ Formula/kubeaudit.rb
@@ -7,12 +7,12 @@ class Kubeaudit < Formula
   head "https://github.com/Shopify/kubeaudit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2bad1e9aa367e2eb825b0fc05b6e7b39fa1acd7f8905fcc5dd89de3c7ff8c9c4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "819f98d293fb3dc8db5e2d0e764f8f3ef98297e61e0e7f3fb065a08f1bd8c966"
-    sha256 cellar: :any_skip_relocation, monterey:       "9b611f53c7ed25c4a1cd6f274f2f505b1e1cec52d9c2109d951e4285e9719d1c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f51a42612debecb09872e6ab4f7837cd150dc8b7472ca875dd097c331330bb2d"
-    sha256 cellar: :any_skip_relocation, catalina:       "25ff8e0cd8a67d5685aedf6e3c6f488fae5a9aefe3206ef5926cee5223b7834e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "854b237ba6a399cc64cc084ec3324be4dc9f47e80521d509b3b0680a10a1cb8f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0a76a637fa82f30b5d7d8ca89be8d04a859ea6320fdc3ecb866f21b55e8c9594"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a53a07d37d8aefefb0c96d661ba42f9c715e33a126971baef4841e6b382e771"
+    sha256 cellar: :any_skip_relocation, monterey:       "25f0b3829979a784e2a9fd4ce42a0067fabbe982493c82f8bb7b0d7dc8cae0a0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "541582d9c1aa954e7583bd395f690c0013a05908254a646b32601c5ecdb5c6e2"
+    sha256 cellar: :any_skip_relocation, catalina:       "a08e36f85ca3e87e5e275ee824441217593db75c5e4c1fd754012c782b303122"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "622aa47244813f8db4a2b434780c1e67ef9e7b8df0dc6364f97c5471417a293b"
   end
 
   depends_on "go" => :build
