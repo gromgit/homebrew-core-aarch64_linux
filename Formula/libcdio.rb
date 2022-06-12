b8417d5dd87e66d706e22c45d879a40e53f6d616
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 12:54:39 +0800

    libcdio: update 2.1.0 bottle.

--- Formula/libcdio.rb
+++ Formula/libcdio.rb
@@ -7,15 +7,8 @@ class Libcdio < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "1a494adcac50f09c217499a52a3bc585164e31bc655208f9d1239599b6687c61"
-    sha256 cellar: :any,                 arm64_big_sur:  "48111a6c9c6f82aeafae559a73aa8acb1c33eb12f71e059a5d6a4bcdab846206"
-    sha256 cellar: :any,                 monterey:       "bedfea3e35f4b1a7fa77ee2f8da00fb603e012b9be281a449b924e9487a5fd18"
-    sha256 cellar: :any,                 big_sur:        "d8bddd24c6d4686f77bd507fdb3380ce6acd3b3f799188e8961d1feeb269c422"
-    sha256 cellar: :any,                 catalina:       "3ec17ce98e129db74cb883941e429286b9ab762c740dcb6ee8c7ff077d6e3304"
-    sha256 cellar: :any,                 mojave:         "55014a60373e44384aa7f797c613ccd5289c55d759c3521b7e5d6819ff54b2ac"
-    sha256 cellar: :any,                 high_sierra:    "32604fb219cc4e59e5eb1e0937b320edfacf31d97f04b9a5fbfcd4354a6a56d0"
-    sha256 cellar: :any,                 sierra:         "61095f7c4888b1c0e022ec9eb314fe389feae1eb030d65e7d91512515528e439"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c87bf684fc0785e0b70ce4ff982625326e65f0e79fdbe528693a81f979e12253"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libcdio"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ff15d7df956dcf6861244230fda0f0474a27c452e7917542335cf4ee6aba77dc"
   end
 
   depends_on "pkg-config" => :build
