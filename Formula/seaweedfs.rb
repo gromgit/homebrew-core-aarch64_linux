a6fce5f9418b5af7cc3630da71714e2c19baa308
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 03:08:44 +0800

    seaweedfs: update 3.02 bottle.

--- Formula/seaweedfs.rb
+++ Formula/seaweedfs.rb
@@ -8,12 +8,8 @@ class Seaweedfs < Formula
   head "https://github.com/chrislusf/seaweedfs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dfbc529db1a87327284fa0376d1b1579447ec8e79aa5d5ef2e7fffb3bd98b7be"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70d427f9154c4db776e5909e42e8e95838e7f1d41a2ae1bd5f7ee6de0c26d72a"
-    sha256 cellar: :any_skip_relocation, monterey:       "e33ab21e2c2013582fced7e34428d37c579afe3ebae2c2f5fafe4261226ee457"
-    sha256 cellar: :any_skip_relocation, big_sur:        "833414e95f676b8343fbcec802656a311e03cff587ecf17fd3d29bd66f0a0f64"
-    sha256 cellar: :any_skip_relocation, catalina:       "0aae83f4a5ceb57e8399138f4e1dbff764a574e3f7b4b4ea4cfc1cda24508c13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1cb23db6fd9fef4b626b72fdf6367a30959944c182e8e403753d2d3abad7896"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/seaweedfs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7baeccf7d968d0370dbd7e382034973342fe596bc05c888fc393f4406c790d51"
   end
 
   depends_on "go" => :build
