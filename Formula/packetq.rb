e1c0b72e786c2d5415e00531a190439a559fa44e
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 22:02:44 +0800

    packetq: update 1.7.0 bottle.

--- Formula/packetq.rb
+++ Formula/packetq.rb
@@ -11,12 +11,8 @@ class Packetq < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3b18d16fd291fbcc6e7b89e94d1beb38c28c55d9058aecc5a47762e544451842"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "602151d75bfa0684f3aa52b957b6e63a51757741e8a67647abbc5a500e9bf838"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd2d3343a6a827431b84d4ca63a61b025cbed8c8bacfe89f82ed2af66d0290ec"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c2a675266e49a77bd5ccc5b354506c0fe41bcaca3254126363ad9f3df1b5f8a6"
-    sha256 cellar: :any_skip_relocation, catalina:       "a7ab7f52ebef6e9ef147be842e3cc1ca4e25a4a456850072e1b33841391451b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87eac1791759336c6189f9b4d055ddbab4674523848b1aa54511e9f5dcc9f5ab"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/packetq"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3365d92d5ffb5501d064f4d0c070ab92ba1374b3d41d59737cd89caafea5ee24"
   end
 
   uses_from_macos "zlib"
