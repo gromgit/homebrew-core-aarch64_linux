c31fbbaa58c3267841c8cfd37bc990f3581e0063
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 11:36:47 +0800

    kind: update 0.12.0 bottle.

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -7,12 +7,8 @@ class Kind < Formula
   head "https://github.com/kubernetes-sigs/kind.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "544f1036a81b47d91b8ad81360fde3c063a4add7d50977f90992c9cada3eeaf8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d2a2b35caeebe678726302b67ab3e1f72440baad3b3d2faf7150ffcf834c0223"
-    sha256 cellar: :any_skip_relocation, monterey:       "eb73ee98fae1727acc28ed0dbc86ad6c6ee1178c84f43c25402e1fc71f97d189"
-    sha256 cellar: :any_skip_relocation, big_sur:        "65f176fbfc2d8af2612cb8d92f5f89575a821fa4f08abe1bb565c8f8a5beaddb"
-    sha256 cellar: :any_skip_relocation, catalina:       "78e39031c372fe01a862f80e64f245cdbf221ba3c9606f690d6d9e8bbe7446c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d565d46ce69dfa673243aa998ba4936a4f575e10f33b2877940f6bb7fdba00f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/kind"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f365441d709ef1897246ae69126631f2534556b2d3de77cedf169015fb3c3d03"
   end
 
   depends_on "go" => :build
