5a79ddda023402ed3c9f0e16a22e1c5d40702d35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-12 20:05:13 +0000

    stress-ng: update 0.14.02 bottle.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -6,12 +6,12 @@ class StressNg < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4cef3fce1752f55d80c5ec17cd02884ad129cef371ab15e0d8d677a46fa54d60"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "48317638a24525fd5a1e6d4279b1a23dbb3de7922e96773cf5d43e8608ed95ce"
-    sha256 cellar: :any_skip_relocation, monterey:       "261ac1ed0712aa23b2119f4e7fcd62d8e2cdca6dc5d07ca5b14962f4a959ccd5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0e466cab2063a41cff34826b6eb95a714b9110d2b6d9ce435f2193a5c9207861"
-    sha256 cellar: :any_skip_relocation, catalina:       "19274fd86a07aa9178a8810b56e72935f39f7a8e799221efd7b0aecc39a0618b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d98573f50516cea506dc8dd2681613ebb0173ead526f543c51bbf6ac23b0f95"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "43f0213982bb72e42c2f9dd4b1e1aaf7882d9d46083628577ce7a00ba77742d0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "931e90bbf1ac2bef57779d9a530e9dcafaf6d3e0af1288be2c3df6ecf6498393"
+    sha256 cellar: :any_skip_relocation, monterey:       "317ae7333ad61144ffe2f3357d876362a235d78a4f7b674949d4281378c342b6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c7f57e2e0a06e2acb14d452f0edb4df7c1da06498f6499de4b51f3bbd13f3f30"
+    sha256 cellar: :any_skip_relocation, catalina:       "edd37d3c1b7cef2060efab462b1f284b341815e0ff989d59acf09634e7d3d82b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f2387b9517035b0adc1e7781cdea35e1a349701f438061eb8a02af8aec38680"
   end
 
   depends_on macos: :sierra
