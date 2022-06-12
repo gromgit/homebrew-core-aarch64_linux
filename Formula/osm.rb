bc8c41ab5a0dc6cdb7583096308e316e96bc737d
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 21:27:48 +0800

    osm: update 1.1.0 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -13,12 +13,8 @@ class Osm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "11074db63d63e06de1d07e1996b46a8b9c1101174da38974cc17ed7b059e88fc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bf572cad0b85d05813d5ac7292756a5525998bb4b486c8cb1e3df4caef1c01ba"
-    sha256 cellar: :any_skip_relocation, monterey:       "080f4e0b2841af610913a460cf75b8299ceefd76fc5fd7e3b4ec20fbf95943c7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8a133be8b3d0e388487f1309d77c44cf053067239af16f7415f512e7fb7cbe8f"
-    sha256 cellar: :any_skip_relocation, catalina:       "1a510cbf17d29b0167a30955e2897d4598aa8b71389563285ff32d1deb9b049c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3bfb9e57efe7df8dda8af8184defd6cd1fa85abed46d1c57bfda3bb748fee3b1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/osm"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "913c7f73bd94486c78716698c9080120ba52b72fd7f3f53429a42338b7d0eaa1"
   end
 
   depends_on "go" => :build
