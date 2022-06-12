b788c1d56049bea3f041e139dfd4568b5acdbf84
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 03:29:43 +0800

    shfmt: update 3.4.3 bottle.

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -7,12 +7,8 @@ class Shfmt < Formula
   head "https://github.com/mvdan/sh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1a9fb0705340684312ac9c92fc448743de717587440518267d4adac0ad83655c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "37adbd3e95760b7c729d045882e5b2db2528037ca8980c4216cd8176cd2aac3d"
-    sha256 cellar: :any_skip_relocation, monterey:       "4e1f4404a999dfaad2a7304210dddef98a9871593a7ab69362fe99e457400ba3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3847119018b86770f101e86dfc4ebfd73fd43a77a5dceb003d0d2aa6ade2014f"
-    sha256 cellar: :any_skip_relocation, catalina:       "10d8a6351ec8f7aba3965e153e73bbd099570047de3f134f8b81824f95945a4f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a25494a19bccb9ab403ce35141dd846ef1e0b3fa66ef517a399a738ef21a6830"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/shfmt"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "73bd29ed5ca6f36c7254f09070d1a66b1aa8320b7a5a6d69c25a4ee13dd8e23d"
   end
 
   depends_on "go" => :build
