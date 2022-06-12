d829b772829697ae41cb0764d355987729d44f4d
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 01:48:41 +0800

    rke: update 1.3.10 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -17,12 +17,8 @@ class Rke < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ce2407ea06571f5d4c83b0c877455ee8600dd713f3db1093d727e9f3eb171d8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9a87c96258e9a20bb1649f4d66e57cd32abaf715646df92dd41e0cff7c0a1e03"
-    sha256 cellar: :any_skip_relocation, monterey:       "bde562d3b39af0dfc6561c25e79e31f35525ca522690b04ad5550c83ee89ac5f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8e32208e6aebfcdc819063eabb57cf91121c6ac57dfac9a23764f88258a06a9e"
-    sha256 cellar: :any_skip_relocation, catalina:       "0d5ef049a7b2240732c197018430b96ea091adb8d1c38c833d0acc12c5aab228"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d7e599138e074c47b838f8ed0556f1afd10da08bc6dd63c9474e95a437a779b8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rke"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "31ac7318e64c545f1af9caa40374d0e29cb29f432152a0e5d7827d77c4fca593"
   end
 
   depends_on "go" => :build
