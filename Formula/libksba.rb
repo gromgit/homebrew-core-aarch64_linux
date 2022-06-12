3bdb6c5c6c588376fefbc9a3d6a6e19653733a57
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 13:37:32 +0800

    libksba: update 1.6.0 bottle.

--- Formula/libksba.rb
+++ Formula/libksba.rb
@@ -11,13 +11,8 @@ class Libksba < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0352df73a00db8db517c8a5e48ed15f9a90ea51fc526269a5e7363a45615ce5f"
-    sha256 cellar: :any,                 arm64_big_sur:  "d7eae0a2f8294b8515e2c68ad16a898998828d8d63fe2a434fd304af49cc7fb9"
-    sha256 cellar: :any,                 monterey:       "cf75a7581708d1b0f22e94fbfe3082598fe7cb34b5f43fd7e415fae6bf6cf1c7"
-    sha256 cellar: :any,                 big_sur:        "3b2917e9ee9d7accc72f8366773406c7721b6085b6993bb92a696b8ac38ff866"
-    sha256 cellar: :any,                 catalina:       "3065405373d29d0542eccad99df604559572e03fa6af5c95599704f98365cf34"
-    sha256 cellar: :any,                 mojave:         "adce4966a82c538788b73fc22b56d8ed9d876a7610746aac35c37cf430381088"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52f1929b0e22ddc0526c64af5306dc2ebfcb0c8d02ce565f9576fdea96c2b2e1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libksba"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a7da2f884dd7dae3b8648ef4339129d0d05e8b9d2b00500e2179c4f88846efa7"
   end
 
   depends_on "libgpg-error"
