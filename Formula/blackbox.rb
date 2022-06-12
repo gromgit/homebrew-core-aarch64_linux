533e008dabd0761eab6bcf9ccba3f61e0e3fd2f0
Ben Iofel <ben@flow.io>
2022-06-12 14:00:30 -0400

    blackbox 1.20220610
    * Update blackbox to v1.20220610
      This is looks like a downgrade, because the [author says](https://github.com/StackExchange/blackbox/issues/354#issuecomment-1152902701) v2.0.0 was not supposed to be released. Will `brew update` handle this correctly?
    * blackbox: add version scheme
    * blackbox: add livecheck
    
    Closes #103505.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/blackbox.rb
+++ Formula/blackbox.rb
@@ -1,9 +1,15 @@
 class Blackbox < Formula
   desc "Safely store secrets in Git/Mercurial/Subversion"
   homepage "https://github.com/StackExchange/blackbox"
-  url "https://github.com/StackExchange/blackbox/archive/v2.0.0.tar.gz"
-  sha256 "0a8fee39dc46436472528ea3a5743c42ebefc068519545fe6fca57041f42deae"
+  url "https://github.com/StackExchange/blackbox/archive/v1.20220610.tar.gz"
+  sha256 "f1efcca6680159f244eb44fdb78e92b521760b875fa5a36e4c433b93ed0f87c1"
   license "MIT"
+  version_scheme 1
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)*\.\d{6,8}(?:\.\d+)*)$/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "6910c4f3a0b2b04288b9ca0eedc866e6b83d14ac5276d4eb46651a0bb0c14333"
