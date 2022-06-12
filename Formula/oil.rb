85c2a653cca5fcf11558cd55ceb78a152467442e
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 20:27:58 +0800

    oil: update 0.10.0 bottle.

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -11,12 +11,8 @@ class Oil < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "3f74e9a96414497c68116d936de9acad7140ffe8484dd36f2908250105b34389"
-    sha256 arm64_big_sur:  "675f621c454aab4a1ffa5edb62fd686fff0f672df1c901865cc7e000dbb2e9d7"
-    sha256 monterey:       "7024f1396846816b9650a9e0cf932a395930543447d8aca1966ab28a10aee2c6"
-    sha256 big_sur:        "d5cc753481a5691b6c4f9ddf0d30a4bd5cb82bcca77f95e97b56b2c2269b827f"
-    sha256 catalina:       "1337b6aa1f9be392874648117ccd2f79c86c529c12465d7bc10e559ec0d4dfcb"
-    sha256 x86_64_linux:   "41ff1d7433893d66399af9a2cf43ffa8a30dc5f6953bb9e51e143c184cbf010c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/oil"
+    sha256 aarch64_linux: "30b594ef278041d35da5dafdec0941c8e59a3f3e99d612314e340bff37023ad5"
   end
 
   depends_on "readline"
