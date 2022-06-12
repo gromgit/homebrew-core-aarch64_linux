674796f99cc2b7d65838aa85a02df78ef07e2845
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-12 14:46:54 +0000

    universal-ctags: update p5.9.20220612.0 bottle.

--- Formula/universal-ctags.rb
+++ Formula/universal-ctags.rb
@@ -13,12 +13,12 @@ class UniversalCtags < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "70e4ddb9009c14cc44993b5166f17b4ba5ed94280f7fb8becb2a0acf594487c4"
-    sha256 cellar: :any,                 arm64_big_sur:  "6952a9680eca846b53530638b434fda449f6d63fa78e585013cffc695e7d35c4"
-    sha256 cellar: :any,                 monterey:       "950abe6849583c2efd99fb919e7b5667da3319e82bc85ca8baa03df635f9147d"
-    sha256 cellar: :any,                 big_sur:        "84b98a0eceb7db85146dc1824aee5d07301870db304452bd1d7ab16c10dc6e9b"
-    sha256 cellar: :any,                 catalina:       "afedf55e62f23bfba9b86756df74d1140d3fd51f03213c7e426819b956580d04"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9c67abcc2d77bf8ea0d1e71c80decf8784e6fad5f2988a5f938767f719e07433"
+    sha256 cellar: :any,                 arm64_monterey: "0cd6b98b5df564a149806eccc9446ab75456fbc0d9a240644bb04b4f73e60636"
+    sha256 cellar: :any,                 arm64_big_sur:  "06336469065ac3d183feae9b03dd50bbc3deab69b0b7be7d3cff15bee257aa75"
+    sha256 cellar: :any,                 monterey:       "170d84a607bf47d47d3a1cc60747bb8a6cb6d98e8a2aca76df176df49eeb0050"
+    sha256 cellar: :any,                 big_sur:        "8df244b90d6466ba8197aab0eaf84b4c4d744033e3e37f0fca19b9b21907cf21"
+    sha256 cellar: :any,                 catalina:       "f87b4af94e311efe6f1fdadfb2de3e8a1bbffac7e8b0d840cfa8276a9752d876"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "771edfef96762e928b4b3a3a1a18798760b8f0454da04f20de812b9499576b33"
   end
 
   depends_on "autoconf" => :build
