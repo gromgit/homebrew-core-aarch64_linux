86b6a47d1a760823229e51d7f9cd1a9d029fef5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-12 12:05:38 +0000

    htmldoc: update 1.9.16_1 bottle.

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -8,12 +8,12 @@ class Htmldoc < Formula
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "8cdac6cf0ecdd40a8b9e3f9c4b43dcb70b907a334b7e3828b008c15898b04b1d"
-    sha256 arm64_big_sur:  "c6fa3410c8a048d651cedf14b2266fa72b9f2fd50fa15ab79b978cd69ca71b79"
-    sha256 monterey:       "8a34873ddee81da158ef8c31764de1d1abfee31e47a09e9186338cd72742e611"
-    sha256 big_sur:        "0d25208b4ca215b4cda2a8d9871c5fc6c5923dcdcc0298515029db80aa31a065"
-    sha256 catalina:       "bb3a9d85875ec4d166deab5e5d4281b72399d01c5343035e988cf64f651e14ac"
-    sha256 x86_64_linux:   "cbb3b4d685e129ca007d7da8455273edad23470034da8fc450a0363ebc91a53d"
+    sha256 arm64_monterey: "e8d3042fd923158ab250600c7a465010ea8c329e11326f36a2d10f9406a4e827"
+    sha256 arm64_big_sur:  "98325540be8b32bfc44cc1a7ebb976a11a95d63140aba964085e4df8b6af59a3"
+    sha256 monterey:       "ddd36ac6cc9fbf5e585bbfc37f1dc595f207691ed702900b3354866c411ddaac"
+    sha256 big_sur:        "4ec9fe099c857e2024e854462641e5620056585059d7a587ca6322df6d1c9a5a"
+    sha256 catalina:       "7b208d7bfd479b71ee86eabfbba86e72d9a354ff28b56f6dc1afb4121617d402"
+    sha256 x86_64_linux:   "6fb8d354d0807ff8a643f13ae974cde5f0f1d76c51e904dae4b9b9a40b1ee927"
   end
 
   depends_on "pkg-config" => :build
