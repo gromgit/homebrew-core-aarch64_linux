b7bc3d51a3a5d7bf43b9ff887373c1138185caf9
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 13:19:42 +0800

    waypoint: update 0.8.1 bottle.

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -7,12 +7,8 @@ class Waypoint < Formula
   head "https://github.com/hashicorp/waypoint.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5fc64ecb69cf5206a7b49a3a694ba17355dce90de582d0fefc77a5f5eaaefc81"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "97c43de5b5e15f199b25ce473105c8905a946e73793d2fa7bbbf2a740d3315e8"
-    sha256 cellar: :any_skip_relocation, monterey:       "95a1acd530c196592fdeaa804e559cc4b4d4ca2949ad0f3d7522c9eac9cfdf67"
-    sha256 cellar: :any_skip_relocation, big_sur:        "061fb004a5d055067dfdf199a7ce6a2c7bbc13c718db285fd0c9a4b51a161fa2"
-    sha256 cellar: :any_skip_relocation, catalina:       "e05ed36da7a1b4f8c4045356a99b5ef526a7b898ef38acb342f0e5d0b6bc516b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f920bf5ecb1a4a63fef52336cfefbb51278694831ac27989a326a8f7fa2d2849"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/waypoint"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a46b4bbd90c4106b7cd2892d549b3ede842875e277a9af2a475bdbdc7c9b46da"
   end
 
   depends_on "go" => :build
