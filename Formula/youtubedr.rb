6698aa5bcf7bf7fa91934bf17f79c8be9ed1d8a4
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 16:09:12 +0800

    youtubedr: update 2.7.13 bottle.

--- Formula/youtubedr.rb
+++ Formula/youtubedr.rb
@@ -6,12 +6,8 @@ class Youtubedr < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ef95c2836b3fb007cfb4ec7145e0755499c8625e2907c639ef807756231d2e8a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ef95c2836b3fb007cfb4ec7145e0755499c8625e2907c639ef807756231d2e8a"
-    sha256 cellar: :any_skip_relocation, monterey:       "0b16f6015eb65f1318bf3bf1edc8f12a922a273971b4f1b31927392a38f4db87"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b16f6015eb65f1318bf3bf1edc8f12a922a273971b4f1b31927392a38f4db87"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b16f6015eb65f1318bf3bf1edc8f12a922a273971b4f1b31927392a38f4db87"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45a9f86e792764aed25ed76564dfd9832982f6b6967ad5c5a1ab7bdc43848ecc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/youtubedr"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "fa0549f78f460346a23a01348a2401e7b9ef8dc3ffd89dcebb188eea59127b4b"
   end
 
   depends_on "go" => :build
