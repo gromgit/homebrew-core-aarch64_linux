8eebc2b501f97b5fd9acfd8bca6c6c692f9bf027
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 10:01:22 +0800

    traefik: update 2.6.6 bottle.

--- Formula/traefik.rb
+++ Formula/traefik.rb
@@ -7,12 +7,8 @@ class Traefik < Formula
   head "https://github.com/traefik/traefik.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d70f93ae91535e01551267b2fce9c54f3f47dbd5a79f6911fb84b837a9630b2a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39be2af0f89acbdc30de3b6be374b87b5e50c76735ef6db75ce3b91e2bfe18d4"
-    sha256 cellar: :any_skip_relocation, monterey:       "cac706f204900a74d6656121dcf1971fe82f4e323e9d6995301fa50ebacec143"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e603595bfd6d615017102be892a1766ed3ee5ea6e49dc972b07735cc4a01e923"
-    sha256 cellar: :any_skip_relocation, catalina:       "31aa1ccd9527888486fe56964e457798eb8732580ce1b5a1d017e29826c39699"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32bf3d7c658dfde6df4221c6d5d2b0680afff8350b6fbd29a536f3fd6689a988"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/traefik"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e7f88fabecb966f5dc08ac229dc0ef2e2df4a1436183d3bf84f8d5babb400b71"
   end
 
   depends_on "go" => :build
