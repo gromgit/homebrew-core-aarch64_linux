d45702eb2c86bec70a5ae0e55bba8803a4ecdbc6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-13 10:05:41 +0000

    v2ray: update 4.45.2 bottle.

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -12,12 +12,12 @@ class V2ray < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ed35ca9af1b8676f23c13c634b0d90adf7dd64d71447d336691ea103c5335eff"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "525ca0430ea28dfe431e95f37d8068bb2f8d8f10a9a6feb3eaf883cc71c4bd04"
-    sha256 cellar: :any_skip_relocation, monterey:       "70af4156221e1bc7fccdce3e9ac05fdb80eb8ee5149e81a507207fceb23d529a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d9aee6f3becb85153af3fa3ac37e82ab3e2dac66390551841fabe777116950df"
-    sha256 cellar: :any_skip_relocation, catalina:       "193dcccc183edfa7a5aa54e28dbdb4e93986e65671a8faa0930c72d3f57ded9f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3525fd22ab391059ed786f2baf7fb58d435f5381a4ecc60758d8c78b79e5be44"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "39375e667ddca5e5d93f0c399d8969be3f0ca8015f8179c6dfc9ecfa1aa2222c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f743a933cca4e588c3f8ce883aa62944be19360e330b6d55f559eb1b72bb4f6"
+    sha256 cellar: :any_skip_relocation, monterey:       "6ca8683200055af0e1d7895b44aad91ac1f47ad2abe42a73bf3da2a59db714e5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "10ac146769d892d242d4bd2c15c1630653ad85e0ac6d36d111aa03517be3a5be"
+    sha256 cellar: :any_skip_relocation, catalina:       "39ad8bcc96f63ca58873ec903c3974dcfdf5c6c9b38fc1f3cd1e787e2da57330"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a9d347cd01bddb5e1a4c81c0374b4c6ca62b5ea3f1f4e353c1c68f59895089d"
   end
 
   # Bump to Go 1.18 with when v5 releases.
