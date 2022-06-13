dc378583a4df2f9e6a17852e392e046f4b1de1c0
Porkepix <porkepix@gmail.com>
2022-06-13 02:24:11 +0200

    gtksourceview5 5.4.2
    Closes #103519.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gtksourceview5.rb
+++ Formula/gtksourceview5.rb
@@ -1,8 +1,8 @@
 class Gtksourceview5 < Formula
   desc "Text view with syntax, undo/redo, and text marks"
   homepage "https://projects.gnome.org/gtksourceview/"
-  url "https://download.gnome.org/sources/gtksourceview/5.4/gtksourceview-5.4.1.tar.xz"
-  sha256 "eb3584099cfa0adc9a0b1ede08def6320bd099e79e74a2d0aefb4057cd93d68e"
+  url "https://download.gnome.org/sources/gtksourceview/5.4/gtksourceview-5.4.2.tar.xz"
+  sha256 "ad140e07eb841910de483c092bd4885abd29baadd6e95fa22d93ed2df0b79de7"
   license "LGPL-2.1-or-later"
 
   livecheck do
