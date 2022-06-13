a0a7820b682b8b3aa422ff165fc015c1b40867f4
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 10:30:02 +0800

    tty-solitaire: update 1.3.1 bottle.

--- Formula/tty-solitaire.rb
+++ Formula/tty-solitaire.rb
@@ -6,13 +6,8 @@ class TtySolitaire < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8ea8909f9e522b9e6fbbbe1db5901a0f584ef25bb788099267ee6e035f84580"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91eea2f4dd14f6a81e3b3cc44fdb72916e1ae2dec5075b85136d5a224cc8d655"
-    sha256 cellar: :any_skip_relocation, monterey:       "0f84c6f6c017c4db02499d375ce1d6be389d39327fb0d5f8d050a797380c705e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ca5bfa5041e8870b14c3ba92e73ad2766c8edc8cf139f03e9d983fc8d9a8976f"
-    sha256 cellar: :any_skip_relocation, catalina:       "b315a94abfcf02186d94a6d4e78c71fec3538a65c5cc885df5dd9245efbca51c"
-    sha256 cellar: :any_skip_relocation, mojave:         "ae38452ea77edde4a1c62085e6f1703cce1f91f04f869783761fa71399c03bf5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5a265fd4dae6787d2560da92701d889f8413e1f9c13955e7ce640ff8d67714e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tty-solitaire"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "84d80a39f7b4be799e8fa24e7a34dc02a24995538c35e01faa71d237d48d1416"
   end
 
   uses_from_macos "ncurses"
