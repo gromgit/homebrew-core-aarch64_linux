f984d0f9cd6a08a991e20a14cf06faebcd683ce1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-13 18:05:53 +0000

    pugixml: update 1.12.1_1 bottle.

--- Formula/pugixml.rb
+++ Formula/pugixml.rb
@@ -12,12 +12,12 @@ class Pugixml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7413361442c9abad17da7d95698553b64f32218d45bfbfb25c0eccbadc64b3d2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a8bcaab7ffa6bc363b3ee06bc27d93512d86fb3b805fd52b15f2cf22850d4c0"
-    sha256 cellar: :any_skip_relocation, monterey:       "9a053f8669cb6a274c35bbd2c3c070a8d42d68e7f25f870cc49344b15cc1f06f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ea310c8f98a257ef09cd30c6dcd7e811b198b8612e277151a312d9b8c3554842"
-    sha256 cellar: :any_skip_relocation, catalina:       "7a047ad63c666fabf1c3a65b7e3d9d6f5156dba4e663f0fa37c13e6ba3fb4517"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad31c8f9950b70f43cc6f28ebaae6c9ce755a5c65c2fd1aab52707a8329bca1a"
+    sha256 cellar: :any,                 arm64_monterey: "19e760a6589f9d8359754ee3a6bb1bb1b916df570d52299299eb2d7b98868697"
+    sha256 cellar: :any,                 arm64_big_sur:  "44de01d90730246427618bb49d3069ca3b4a3965f458036cf8f789d1e021fd78"
+    sha256 cellar: :any,                 monterey:       "589881fa373d36a21c84c4c42f6175ef7ae1918f5c7a037386695dca054a9005"
+    sha256 cellar: :any,                 big_sur:        "48f559efb06c1d60d553db9273141247018ee7aa68ab37679de9c09a999a6d1f"
+    sha256 cellar: :any,                 catalina:       "a925fd3a353e10226a70e6695f69210a5f117ab8b7c8a8f98970fa59fbfa2654"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9efcb2f9e87b4a51fee55123c9900d332c6f3c9c3fce67eed2d55f19becadb9"
   end
 
   depends_on "cmake" => :build
