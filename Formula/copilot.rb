e7864de941804e2b8b1214f73b652d58c799b812
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-14 05:17:44 +0000

    copilot 1.19.0
    Closes #103591.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -4,8 +4,8 @@ class Copilot < Formula
   desc "CLI tool for Amazon ECS and AWS Fargate"
   homepage "https://aws.github.io/copilot-cli/"
   url "https://github.com/aws/copilot-cli.git",
-      tag:      "v1.18.1",
-      revision: "61a05a60ee990865a1160f5c2e0dcc3575adb596"
+      tag:      "v1.19.0",
+      revision: "33ee39e861e148867b7ecb62f6dc46d50ac5c5e4"
   license "Apache-2.0"
   head "https://github.com/aws/copilot-cli.git", branch: "mainline"
 
