a6104c9aafe54172f3b9f20d6305548fa028c5cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-14 16:04:56 +0000

    mmctl: update 6.7.1 bottle.

--- Formula/mmctl.rb
+++ Formula/mmctl.rb
@@ -13,12 +13,12 @@ class Mmctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d915116220f808e1940d7545075508c429de98b660c1a8dc495c990d4322bf31"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "26b632edd81841fbaada4434ad3d3f4aaa0ee43e50a4c9736bf4a58ad94fc67f"
-    sha256 cellar: :any_skip_relocation, monterey:       "5184a7315ebce60a54e41675bebc30a7ce1c8993b970eb53778d15fde19ec823"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bb51f5c41c4c5d0d986ac0065fe583fac732e0b6b05c5678a9f50739bcc030a5"
-    sha256 cellar: :any_skip_relocation, catalina:       "54be23acf1dcc34488e475d2c2c7b7ea558ca3a4cdbee7c757bb9b4241c6ff18"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f6686bd8844104354fa72bebba0ae8e88021ed11c0d879572e81d88115e3eb0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "245074ad14e65976afb32c509981231e28983480282f55089808f1f6e0e8d9dc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9025f3300f6aea47315cfecd9c277d7fde7a423c6baea714e77ca7243260167d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e3218661b573e4c37f943a9818d241ffc2286cfc40ea988dc870074ebb50b47b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ac5db3e36d3c4f1367dc4e452969a7fd75f75ee6a876c5bff91bf7a91e7a68cf"
+    sha256 cellar: :any_skip_relocation, catalina:       "8c8659e3bb55ebc17f689ffa9a896ce0f8dcc31c4d46fee77216004527a043dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4480d074c3f6f4b174cc3c584316eb0b704303e95c9846a01da1a43d4aca143d"
   end
 
   depends_on "go" => :build
