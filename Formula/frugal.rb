a598e03125bb0db57bfbcf670f22b05bc87da765
Porkepix <porkepix@gmail.com>
2022-06-14 02:22:44 +0200

    frugal 3.15.2
    Closes #103578.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -1,8 +1,8 @@
 class Frugal < Formula
   desc "Cross language code generator for creating scalable microservices"
   homepage "https://github.com/Workiva/frugal"
-  url "https://github.com/Workiva/frugal/archive/v3.15.1.tar.gz"
-  sha256 "ea7a16da061082d2016ec058835ec7e7e42f42cd6f3fa94fb455be8157f9ddf6"
+  url "https://github.com/Workiva/frugal/archive/v3.15.2.tar.gz"
+  sha256 "c8e4b9a64c08765ad4e758dbe0d537e48c1051f903883ac660108ad6f93fe089"
   license "Apache-2.0"
 
   bottle do
