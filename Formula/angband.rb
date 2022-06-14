4199cd93ee4879704a5255ea3ed1252675ec77c0
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 21:43:01 +0800

    angband: fix Linux build
    Current binary doesn't link to ncurses, so it exits immediately without displaying the game UI.
    
    Before:
    ```
    $ brew linkage angband
    System libraries:
      /lib/x86_64-linux-gnu/libc.so.6
      /lib/x86_64-linux-gnu/libm.so.6
    ```
    After:
    ```
    $ brew linkage angband
    System libraries:
      /lib/x86_64-linux-gnu/libc.so.6
      /lib/x86_64-linux-gnu/libm.so.6
    Homebrew libraries:
      /home/linuxbrew/.linuxbrew/opt/ncurses/lib/libncursesw.so.6 (ncurses)
    ```
    
    Also improved test to actually run the UI.
    
    Closes #103626.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/angband.rb
+++ Formula/angband.rb
@@ -4,6 +4,7 @@ class Angband < Formula
   url "https://github.com/angband/angband/releases/download/4.2.4/Angband-4.2.4.tar.gz"
   sha256 "a07c78c1dd05e48ddbe4d8ef5d1880fcdeab55fd05f1336d9cba5dd110b15ff3"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/angband/angband.git", branch: "master"
 
   livecheck do
@@ -20,21 +21,38 @@ class Angband < Formula
     sha256 x86_64_linux:   "3cfa3f60f7b88f2aa13cbbcae6b90e0aec128f5aef92e49c2860e61075c32af3"
   end
 
+  uses_from_macos "expect" => :test
+  uses_from_macos "ncurses"
+
   def install
-    ENV["NCURSES_CONFIG"] = "#{MacOS.sdk_path}/usr/bin/ncurses5.4-config"
-    system "./configure", "--prefix=#{prefix}",
-                          "--bindir=#{bin}",
-                          "--libdir=#{libexec}",
-                          "--enable-curses",
-                          "--disable-ncursestest",
-                          "--disable-sdltest",
-                          "--disable-x11",
-                          "--with-ncurses-prefix=#{MacOS.sdk_path}/usr"
+    ENV["NCURSES_CONFIG"] = "#{MacOS.sdk_path}/usr/bin/ncurses5.4-config" if OS.mac?
+    args = %W[
+      --prefix=#{prefix}
+      --bindir=#{bin}
+      --libdir=#{libexec}
+      --enable-curses
+      --disable-ncursestest
+      --disable-sdltest
+      --disable-x11
+    ]
+    args << "--with-ncurses-prefix=#{MacOS.sdk_path}/usr" if OS.mac?
+    system "./configure", *args
     system "make"
     system "make", "install"
   end
 
   test do
-    system bin/"angband", "--help"
+    script = (testpath/"script.exp")
+    script.write <<~EOS
+      #!/usr/bin/expect -f
+      set timeout 10
+      spawn angband
+      sleep 2
+      send -- "\x18"
+      sleep 2
+      send -- "\x18"
+      expect eof
+    EOS
+    system "expect", "-f", "script.exp"
   end
 end
