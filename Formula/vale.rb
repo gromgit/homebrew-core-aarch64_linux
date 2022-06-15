d00b7b458029b72b90e27fcd98648d4aabf6a0ad
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:53:57 +0800

    vale: no rebuilding required

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -1,17 +1,13 @@
 class Vale < Formula
   desc "Syntax-aware linter for prose"
   homepage "https://docs.errata.ai/"
-  url "https://github.com/errata-ai/vale/archive/v2.18.0.tar.gz"
-  sha256 "5b1b5a3c61ee8d0a3a7aab6df8086721f17e69957ab7e5230f6402326b7cd088"
+  url "https://github.com/errata-ai/vale/archive/v2.16.1.tar.gz"
+  sha256 "f28f7365a3779919333c56d530f49ce2bbdebd4c05b5dcecd8b9ed77dcea126e"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "485acfa04ebc6631384c0ef2808a8ba835e05df2b9ffb25bffa1df973b6fe281"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "642cbb7cf98e7d5090ebe3c511a0062f902c19a6a4bc3a224b5e97e5f2695a50"
-    sha256 cellar: :any_skip_relocation, monterey:       "7ef960754bf56288b8fa60b6195a295be161823a7c7971cd5ba7ecd2e3b27269"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0526c7c059a23080308e17b705c92420ce3e9853f4c7de3782796b9bc6a81066"
-    sha256 cellar: :any_skip_relocation, catalina:       "5da2a9a9965e6db9547ab1bcc246dd0018b2986d0b99f351cff1c35968c445aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "49c67f48c25596cd234fe97d16b654d58de1664caefc0ec499356c3b742459a1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vale"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0f714cbd9daf7f717e445e881d751d34003998ba59bf4cf9ae6e990c607426b4"
   end
 
   depends_on "go" => :build
