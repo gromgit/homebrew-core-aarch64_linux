f125478f3a2c5873677603709f263a269c1cecda
Porkepix <porkepix@gmail.com>
2022-06-15 18:54:13 +0200

    vcluster 0.10.0
    Closes #103720.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vcluster.rb
+++ Formula/vcluster.rb
@@ -2,8 +2,8 @@ class Vcluster < Formula
   desc "Creates fully functional virtual k8s cluster inside host k8s cluster's namespace"
   homepage "https://www.vcluster.com"
   url "https://github.com/loft-sh/vcluster.git",
-      tag:      "v0.9.1",
-      revision: "c2382e17e6de5ad72c9b88e81a083d9e3823df5d"
+      tag:      "v0.10.0",
+      revision: "05ba31337722314266d4e04f93609713f8152972"
   license "Apache-2.0"
   head "https://github.com/loft-sh/vcluster.git", branch: "main"
 
