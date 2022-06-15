8c1bff12eb25abf9143aa6acbecbc621e5b58b69
Adrian Ho <aho+rpi9@03s.net>
2022-06-16 00:53:15 +0800

    cilium-cli: no rebuilding required

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -1,17 +1,13 @@
 class CiliumCli < Formula
   desc "CLI to install, manage & troubleshoot Kubernetes clusters running Cilium"
   homepage "https://cilium.io"
-  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.11.8.tar.gz"
-  sha256 "da0d10e9f8e4a433323f00f757e7b27e4aafce3680db4b3dbda1cfea67338f1e"
+  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.11.5.tar.gz"
+  sha256 "f1becdf87c0f1cfedcf206297f9f2371d31bc467b6c3f6d35af074c73f5d30ed"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "622ab1a1c9c19d489b4ad023c47c5cd7eee82e58c396f9863c2f938c16d0e3ef"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "20d4a4bf822fce74dc2b77a4aa0e672c4d5c65dc325dd38b146b5f56b5cf8c91"
-    sha256 cellar: :any_skip_relocation, monterey:       "f7c9a3cfa1d16133cd0129e897a68d37505b703e2f88c5f00da78b08f7cca2ba"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6e0c379b67e6100e3f248f5850b4fe462c0815cf5bd1cda406096c38fad95271"
-    sha256 cellar: :any_skip_relocation, catalina:       "314291ca0d240f241c18471d26b8527f07974f5bf42c7fcf2b48794634b8307d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0acf2004b4e1f20bacdf254555de669c53cd7b88df43bd86e15c445abbf0ba59"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cilium-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5f13d8696f66f91feede6f94f9bf7a17d526dffe88d4e2e6bb693c5e313e49aa"
   end
 
   depends_on "go" => :build
