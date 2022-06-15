e3bfa761e04841867b0e469b9a081439011ddd12
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:53:39 +0800

    tengo: no rebuilding required

--- Formula/tengo.rb
+++ Formula/tengo.rb
@@ -1,17 +1,13 @@
 class Tengo < Formula
   desc "Fast script language for Go"
   homepage "https://tengolang.com"
-  url "https://github.com/d5/tengo/archive/v2.11.2.tar.gz"
-  sha256 "bce3d6180eaaca682eaf814148a4b8278e8b0ad87bf322647e9817e29604508d"
+  url "https://github.com/d5/tengo/archive/v2.10.1.tar.gz"
+  sha256 "00c892a7cb4e847eefd36f5b8db695e184da5c090c6b509339c3b5d3a746232f"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "67b83d6f2cebdda0b7ade6d5790658c0ef044eb47b59ca8d2fb4f085e1b3eeba"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "67b83d6f2cebdda0b7ade6d5790658c0ef044eb47b59ca8d2fb4f085e1b3eeba"
-    sha256 cellar: :any_skip_relocation, monterey:       "12d25c5ef97f07d7097d00f4e00d2d20e3569b371a11a1ed31d36ca2a842d6aa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "12d25c5ef97f07d7097d00f4e00d2d20e3569b371a11a1ed31d36ca2a842d6aa"
-    sha256 cellar: :any_skip_relocation, catalina:       "12d25c5ef97f07d7097d00f4e00d2d20e3569b371a11a1ed31d36ca2a842d6aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdc490fd77b549902c5fc5bc895d6450f2167be13bd708122039604246d463d9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tengo"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1f0aebf5cd913bd726428a1f4d77f5aba9c537e10660040c99eb50a0c93c5b43"
   end
 
   depends_on "go" => :build
