65dcc09fa360c069c5e2f0a0f7bd8808aeb0a946
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:53:11 +0800

    regula: no rebuilding required

--- Formula/regula.rb
+++ Formula/regula.rb
@@ -2,18 +2,14 @@ class Regula < Formula
   desc "Checks infrastructure as code templates using Open Policy Agent/Rego"
   homepage "https://regula.dev/"
   url "https://github.com/fugue/regula.git",
-      tag:      "v2.8.0",
-      revision: "611dbd38f7df934e8001d11d52863d73cf0ee266"
+      tag:      "v2.6.1",
+      revision: "0d40e66c4bec929779b8bb5cde5f22ed1e522ede"
   license "Apache-2.0"
   head "https://github.com/fugue/regula.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2bafa1dbdf904dceb7f1b02f140fa897ac4abfa0ca2aef36f7538f24cf2f1ae6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ce0cc7083faf220e0efabeb95e1e5d265be89c6ff0cbfd240a600f1fd9258b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "354da246c053862bd9ddd9691fcff789b3dd875a127dd2221445728d4e027b00"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b8aa1569dcc008bb99d65b9ebeae0eaedac00389556bd518a65ff8bdda594390"
-    sha256 cellar: :any_skip_relocation, catalina:       "7be23165956985c1ebaa3fc5643537877c5b34807d15d03311768584bad378e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7cd042ff096af3de962fcc4e11e7629f3a86e0d254315e2c3d3e600588dd489a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/regula"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2a1927a2dad3747c06dcae950fe410e4b525658790a60e1035e75df334b83b30"
   end
 
   depends_on "go" => :build
