8c2d37f700196aaca5f6a9832791d1baa04966e8
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-15 12:58:48 -0400

    archi-steam-farm 5.2.6.3
    Closes #103722.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -2,8 +2,8 @@ class ArchiSteamFarm < Formula
   desc "Application for idling Steam cards from multiple accounts simultaneously"
   homepage "https://github.com/JustArchiNET/ArchiSteamFarm"
   url "https://github.com/JustArchiNET/ArchiSteamFarm.git",
-      tag:      "5.2.5.7",
-      revision: "7532b89fd02696aa680ac001df6e85ecbf114926"
+      tag:      "5.2.6.3",
+      revision: "03c2ba049e814df1050c859aae0edd104e95022c"
   license "Apache-2.0"
   head "https://github.com/JustArchiNET/ArchiSteamFarm.git", branch: "main"
 
