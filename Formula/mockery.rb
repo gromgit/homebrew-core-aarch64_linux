9b37cfa79c9c877fcb8bd437e818b279fdd47d0d
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:52:30 +0800

    mockery: no rebuilding required

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -1,18 +1,14 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/v2.13.0.tar.gz"
-  sha256 "9653b30f7e8d9d48ce18ab99e9ce4473c85d4f19f952d5a47b0cc167d27b74d4"
+  url "https://github.com/vektra/mockery/archive/v2.12.2.tar.gz"
+  sha256 "f5962e4506dd749b9bcc2785c1e5ac683bddac5ba0219382677365d558d2080c"
   license "BSD-3-Clause"
   head "https://github.com/vektra/mockery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ae3601ea655992f354ebe12fdcc5d696fed4e0fbf27750501bcc78f7702ec108"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7acb6d31fc688a4c93b0241b75f8e91dadfd912bc4a7c6029d4d5c4de064e617"
-    sha256 cellar: :any_skip_relocation, monterey:       "3a62c9944aba9f1d3f5e6c993a2a12b039455c4563f07b629fffa0eb4e02d67c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "706972d7a1e1bc5fb41899e4e91dac3bba7f4842ded729cad568acbf5ea07aa7"
-    sha256 cellar: :any_skip_relocation, catalina:       "d6fe934b90554c3296e0b534391c38757536abb2f2e82a7aaae4e963a1ca8ab8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "075203d21350a3bb76872d02bc206a5d816b96aa6022e0a95318a72f54e97c54"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/mockery"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "89ea3971f88173afab40dd3a4a4282f98191d50080e70da9cec057c4713bbe9d"
   end
 
   depends_on "go" => :build
