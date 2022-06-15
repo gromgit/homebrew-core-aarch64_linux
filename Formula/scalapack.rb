78496ea62e9c4398afb6aead33b972d5dc75800d
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-15 14:24:51 -0400

    scalapack 2.2.0
    * scalapack 2.2.0
    * scalapack: remove patch
    * scalapack: add new patch
    * scalapack: add new patch
    
    Closes #103772.
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>
    Co-authored-by: danielnachun <daniel.nachun@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scalapack.rb
+++ Formula/scalapack.rb
@@ -1,10 +1,9 @@
 class Scalapack < Formula
   desc "High-performance linear algebra for distributed memory machines"
   homepage "https://www.netlib.org/scalapack/"
-  url "https://www.netlib.org/scalapack/scalapack-2.1.0.tgz"
-  sha256 "61d9216cf81d246944720cfce96255878a3f85dec13b9351f1fa0fd6768220a6"
+  url "https://www.netlib.org/scalapack/scalapack-2.2.0.tgz"
+  sha256 "40b9406c20735a9a3009d863318cb8d3e496fb073d201c5463df810e01ab2a57"
   license "BSD-3-Clause"
-  revision 3
 
   livecheck do
     url :homepage
@@ -26,12 +25,13 @@ class Scalapack < Formula
   depends_on "open-mpi"
   depends_on "openblas"
 
-  # Patch for compatibility with GCC 10
-  # https://github.com/Reference-ScaLAPACK/scalapack/pull/26
+  # Apply upstream commit to fix build with gfortran-12.  Remove in next release.
   patch do
-    url "https://github.com/Reference-ScaLAPACK/scalapack/commit/bc6cad585362aa58e05186bb85d4b619080c45a9.patch?full_index=1"
-    sha256 "f0892888e5a83d984e023e76eabae8864ad89b90ae3a41d472b960c95fdab981"
+    url "https://github.com/Reference-ScaLAPACK/scalapack/commit/a0f76fc0c1c16646875b454b7d6f8d9d17726b5a.patch?full_index=1"
+    sha256 "2b42d282a02b3e56bb9b3178e6279dc29fc8a17b9c42c0f54857109286a9461e"
   end
+
+  patch :DATA
 
   def install
     mkdir "build" do
@@ -59,3 +59,18 @@ class Scalapack < Formula
     end
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 85ea82a..86222e0 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -232,7 +232,7 @@ append_subdir_files(src-C "SRC")
+ 
+ if (UNIX)
+    add_library(scalapack ${blacs} ${tools} ${tools-C} ${extra_lapack} ${pblas} ${pblas-F} ${ptzblas} ${ptools} ${pbblas} ${redist} ${src} ${src-C})
+-   target_link_libraries( scalapack ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
++   target_link_libraries( scalapack ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES} ${MPI_Fortran_LIBRARIES})
+    scalapack_install_library(scalapack)
+ else (UNIX) # Need to separate Fortran and C Code
+    OPTION(BUILD_SHARED_LIBS "Build shared libraries" ON )
