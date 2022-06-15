27eca983a346fb4430cf1e2991fa95b8932a149a
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:53:37 +0800

    tektoncd-cli: no rebuilding required

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -1,8 +1,8 @@
 class TektoncdCli < Formula
   desc "CLI for interacting with TektonCD"
   homepage "https://github.com/tektoncd/cli"
-  url "https://github.com/tektoncd/cli/archive/v0.24.0.tar.gz"
-  sha256 "6382bd5be03a6c1ae51f09b9450934b71f84e96b6b14be92a3407b70ceb18fa4"
+  url "https://github.com/tektoncd/cli/archive/v0.23.1.tar.gz"
+  sha256 "49ea8c907c10514e219b3536fad481c537c09b8fa264eb0c0f3c4ece61bcabc5"
   license "Apache-2.0"
 
   livecheck do
@@ -11,12 +11,8 @@ class TektoncdCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9f2414d5b90eb4ffac778f9c4818367955541ffa26d27ce8de66ea9c25c176eb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f1519a8ff80358f52f589f5e6d7fdea655081f81849ae46d329abd5f7c127b49"
-    sha256 cellar: :any_skip_relocation, monterey:       "c3d5ce70a2b32d0f36b4270d694ba4e85c39da6f2cfaf281526ddeff85a8e687"
-    sha256 cellar: :any_skip_relocation, big_sur:        "20025c65a7990b2fae828919daf811ab1b39c56ecff3b9ba4edd29dd1a0fff82"
-    sha256 cellar: :any_skip_relocation, catalina:       "7c9d99fa7894f00f739a331ed6b7ab8a5d697ef5e54a3e22dbf1d588cb60552a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7a0b5ad8f82cacbb02b339c49362111534e4b9e01e851e782e84bf0b3c7642ec"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tektoncd-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c9f28defc59a8d0824ec6f3f3b1e6aa12e5b90ec9f97d9f293c1d177423f5c82"
   end
 
   depends_on "go" => :build
