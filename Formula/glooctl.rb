7caebf203199c4fefd958870249ea5b394603d97
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:51:39 +0800

    glooctl: no rebuilding required

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.11.14",
-      revision: "0d4ebc2f3903ce14ccab4cff8f879f6a738d5e80"
+      tag:      "v1.11.9",
+      revision: "7c18dfacc60e1b82262721d58a7d23f022d995ac"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
@@ -15,12 +15,8 @@ class Glooctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b4934f45ce20a3509c94d894dac2f88870dfb618e1990cca5b24914711d9551"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b67ef5ef1ac8cec281abbf489cb79c962f685a14a1c7ebb4bef2591b0f262070"
-    sha256 cellar: :any_skip_relocation, monterey:       "21e6b635489626c717c0d60902be93be6a8e604878eefe1b347f9c9f8c147196"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8062778c7990cf9a6b0358a367536371e1c0a70d31a46fb9f3e8d227e99b3dee"
-    sha256 cellar: :any_skip_relocation, catalina:       "60654ffc06702b1b9595b8a97fa778d5b583b82d750437b7a61600b305f3123d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be8e2e183f3003026af9fbffc1242d9376bea05f61dcbbc86e4d96f94424d893"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/glooctl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c2351292f94479d3fd8456f13c633464874c7138c1c55665c4773f8d727c1acd"
   end
 
   depends_on "go" => :build
