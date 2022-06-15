cf6f52f225f82254e13f677890db34fe3dd61207
Adrian Ho <aho+rpi9@03s.net>
2022-06-15 12:51:40 +0800

    goawk: no rebuilding required

--- Formula/goawk.rb
+++ Formula/goawk.rb
@@ -1,17 +1,13 @@
 class Goawk < Formula
   desc "POSIX-compliant AWK interpreter written in Go"
   homepage "https://benhoyt.com/writings/goawk/"
-  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.19.0.tar.gz"
-  sha256 "dccc38d29b552db43262b9876c27e27c7ac0b5658fd34b2866205cdb4bb1a534"
+  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.16.0.tar.gz"
+  sha256 "f610e57396cac89aaa1070ab9edfd46f09929775b58dad2ba78ed8a59e01d6a1"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b185f46a02eef6c2f8199c2f53c92ca4c6095ef40821380e5b610394586d1c4a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b185f46a02eef6c2f8199c2f53c92ca4c6095ef40821380e5b610394586d1c4a"
-    sha256 cellar: :any_skip_relocation, monterey:       "6d0dba5bfe43346c5451035c1c6537744bb4379d83f74d538dae0cafb215ab6f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6d0dba5bfe43346c5451035c1c6537744bb4379d83f74d538dae0cafb215ab6f"
-    sha256 cellar: :any_skip_relocation, catalina:       "6d0dba5bfe43346c5451035c1c6537744bb4379d83f74d538dae0cafb215ab6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a302ce19d6712c42c180629333da4452e38d0e9abafc0e86fca969882b409aca"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/goawk"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "85297d56574fa3ec760db56dfac3434aa5fa0e18722a186cda62417361c54a85"
   end
 
   depends_on "go" => :build
