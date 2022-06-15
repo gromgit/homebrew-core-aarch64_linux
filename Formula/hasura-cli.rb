e0ed0ed8cffdec5be064dfd9858b78c7fabe3f38
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-15 13:38:59 -0400

    hasura-cli 2.8.0
    Closes #103740.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hasura-cli.rb
+++ Formula/hasura-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class HasuraCli < Formula
   desc "Command-Line Interface for Hasura GraphQL Engine"
   homepage "https://hasura.io"
-  url "https://github.com/hasura/graphql-engine/archive/v2.7.0.tar.gz"
-  sha256 "2ce8d24025f4d864ab6222e9a73957a620534c487d6a8de2a9b2c3631b5a7e29"
+  url "https://github.com/hasura/graphql-engine/archive/v2.8.0.tar.gz"
+  sha256 "1e5df7d5c4ff69ea46daac626e97bc6217ec46662effdba8b23d87a90de998db"
   license "Apache-2.0"
 
   bottle do
