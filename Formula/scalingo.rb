4cf99c37e7a70c8f2e7560035f52ffa6b911d723
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-15 22:04:34 +0000

    scalingo: update 1.23.0 bottle.

--- Formula/scalingo.rb
+++ Formula/scalingo.rb
@@ -6,12 +6,12 @@ class Scalingo < Formula
   license "BSD-4-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc4fc82df67e3e442bdcb90861a2b065f39a900fb604cc78692ff90964eb9be6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "de67f307a7f6dd0106fd291d936874c6efd1b125543fa76ec8c19dc50d0ccfe4"
-    sha256 cellar: :any_skip_relocation, monterey:       "bf5399dd1ba25e9784741e736d92f301d60918ea63e28d16041db7ac3e9e31a9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8590b1c689749bc6f962ea7e3708bfc2311f4168a89472564f91b1ebacde3d7b"
-    sha256 cellar: :any_skip_relocation, catalina:       "ff3101e6f0f5daf0e8aa17d75b602841cd0d152e629aeca427bced870e550486"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24bfd1a8adfcb4b9e01c5df9ff354b4057d79902231cc714e01ac3d5f35a6d74"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "897840c13000d92579aa1066443ac0d7ec9df705b604b36a6b3b4eb0330c26af"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4631b61b8903c96cc9fcf58406860b1a7ed17b62d45002704beabadd6b8f97aa"
+    sha256 cellar: :any_skip_relocation, monterey:       "6ea9eb9933907793e3b79a9c35f8e9bfbee9f242e52f0ef9995e7dc26d871027"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f144e90ba794319911b2c5cd8cbeed709c9cd0e08a168806498b58458abf9435"
+    sha256 cellar: :any_skip_relocation, catalina:       "328deeec81ca75c0bb7a511bb97145b95ec41081ea2fd9043475b7ac0df554a2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85bf751d4b347efac2d4dce7845fac6c5bcbbaee166c7be1fc6d2d244e6a76e5"
   end
 
   depends_on "go" => :build
