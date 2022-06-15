5b8b1afbfdcddfa31a7a5ea1ed960b59b89fcf4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-15 04:17:13 +0000

    tomee-plume: update 8.0.12 bottle.

--- Formula/tomee-plume.rb
+++ Formula/tomee-plume.rb
@@ -7,7 +7,7 @@ class TomeePlume < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "dad8ba85ad7e1ba9c55b482ffcf1f8a9b8642979f5abf2816be9dfcb97c7015e"
+    sha256 cellar: :any_skip_relocation, all: "252045e6c4e5896c8259f76665c74409afe503dfed10dd42949f5973b1db9cab"
   end
 
   depends_on "openjdk"
