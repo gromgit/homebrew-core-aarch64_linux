f37dee1652722e20a4344870e0689c7cad289efa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-15 09:04:57 +0000

    cdk8s: update 2.0.21 bottle.

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -8,7 +8,7 @@ class Cdk8s < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d908bbf5f6a8ff13f9ff43c2c68a9428a0d98475d7eb8ce3f0026d44d7efe2d4"
+    sha256 cellar: :any_skip_relocation, all: "481acd79902721d8b13bf54e44e5dd1dd7c3bb83d6b3ffaffda0becc723c6229"
   end
 
   depends_on "node"
