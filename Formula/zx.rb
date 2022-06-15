50275198c04ce7fa9b92eee58f6ce314271dbb74
Dustin Rodrigues <dust.rod@gmail.com>
2022-06-15 01:26:40 +0000

    zx: update 7.0.0 bottle.

--- Formula/zx.rb
+++ Formula/zx.rb
@@ -8,12 +8,12 @@ class Zx < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "611b406a0f40c29297753643686113ffe7995b551431a9ee61ea709215dbf9a6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "611b406a0f40c29297753643686113ffe7995b551431a9ee61ea709215dbf9a6"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b19b70cc024c62017f82faa65ba417393f328607d1b8488028b34aa16c5d42d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8b19b70cc024c62017f82faa65ba417393f328607d1b8488028b34aa16c5d42d"
-    sha256 cellar: :any_skip_relocation, catalina:       "8b19b70cc024c62017f82faa65ba417393f328607d1b8488028b34aa16c5d42d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "611b406a0f40c29297753643686113ffe7995b551431a9ee61ea709215dbf9a6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b5ce50d1e3fc137fd552c5355601a1fddd508bab0e2c72b227999793869c864"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9b5ce50d1e3fc137fd552c5355601a1fddd508bab0e2c72b227999793869c864"
+    sha256 cellar: :any_skip_relocation, monterey:       "30e76f3fc400193aa95ceb3f79199a450bfbd3c60a8a811fd292aae47785b729"
+    sha256 cellar: :any_skip_relocation, big_sur:        "30e76f3fc400193aa95ceb3f79199a450bfbd3c60a8a811fd292aae47785b729"
+    sha256 cellar: :any_skip_relocation, catalina:       "30e76f3fc400193aa95ceb3f79199a450bfbd3c60a8a811fd292aae47785b729"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9b5ce50d1e3fc137fd552c5355601a1fddd508bab0e2c72b227999793869c864"
   end
 
   depends_on "node"
