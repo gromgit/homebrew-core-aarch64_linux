05c4f1a4ea6c6a21f53f54423bbbb53dbc53bd25
Rui Chen <rui@chenrui.dev>
2022-06-16 15:44:58 +0000

    astro: add 1.0.2 bottle.

--- Formula/astro.rb
+++ Formula/astro.rb
@@ -5,6 +5,15 @@ class Astro < Formula
   sha256 "5121b7697f7db7a85b4e35af2410cb4832fa0fb3983217e3168f3cdbc2ef72b1"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f1fa2750156f778ab88619df1ef4e5b6ffe8dfaa8461c025ac1210ff909790f8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f1fa2750156f778ab88619df1ef4e5b6ffe8dfaa8461c025ac1210ff909790f8"
+    sha256 cellar: :any_skip_relocation, monterey:       "30d1ce9a9eb4ade2fc5efd9c707592831b76083ae061d455d3691044383bfb9d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "30d1ce9a9eb4ade2fc5efd9c707592831b76083ae061d455d3691044383bfb9d"
+    sha256 cellar: :any_skip_relocation, catalina:       "30d1ce9a9eb4ade2fc5efd9c707592831b76083ae061d455d3691044383bfb9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e5816dca197a2b95bb4717d1cb08c03932534a5af5b305d2df407f29031438ca"
+  end
+
   depends_on "go" => :build
 
   def install
