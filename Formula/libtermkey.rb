d5b91241bd806323e204fb9b247190bd8bfcc6fb
mhmdanas <triallax@tutanota.com>
2022-06-16 16:16:54 +0300

    libtermkey: use HTTPS
    Closes #103821.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libtermkey.rb
+++ Formula/libtermkey.rb
@@ -1,7 +1,7 @@
 class Libtermkey < Formula
   desc "Library for processing keyboard entry from the terminal"
-  homepage "http://www.leonerd.org.uk/code/libtermkey/"
-  url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.22.tar.gz"
+  homepage "https://www.leonerd.org.uk/code/libtermkey/"
+  url "https://www.leonerd.org.uk/code/libtermkey/libtermkey-0.22.tar.gz"
   sha256 "6945bd3c4aaa83da83d80a045c5563da4edd7d0374c62c0d35aec09eb3014600"
   license "MIT"
 
