660a362dc4651e28f3a6bdd7197579d35796ea96
Rui Chen <rui@chenrui.dev>
2022-06-16 15:47:46 +0000

    phrase-cli: add 2.4.10 bottle.

--- Formula/phrase-cli.rb
+++ Formula/phrase-cli.rb
@@ -5,6 +5,15 @@ class PhraseCli < Formula
   sha256 "46a7783f5db2372c523d267bf1b3eed811993b2a2e8469cabd5ea4746c6844b8"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "262bc8795ea676d3b82a0df610c57efb7d531c764fd77774f59cc4764d8ee52d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f9afdeb185db095e57909c48f714b2c82bb999b4ab16d98037f783fd15e8ac5c"
+    sha256 cellar: :any_skip_relocation, monterey:       "03c36f31b96eda1e44b3428e291a9d83e26087e24bfb3ff7f668aaa5bd764bd1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e473f2e5573c7332e0000f5dcfb61b645a2e43ba16ce7b53c70a7d8b4bbf0ce9"
+    sha256 cellar: :any_skip_relocation, catalina:       "a623579167f4e18569d0ae31bafd310ea800f3621521b8e8b81ce179ae89001c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d2c1f948113922a89236e09a78eca06833c16aa6f2f685a99190fee3772238d"
+  end
+
   depends_on "go" => :build
 
   def install
