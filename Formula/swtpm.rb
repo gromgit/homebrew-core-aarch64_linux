0aef812c404187777ba66e79574dd28389050921
Rui Chen <rui@chenrui.dev>
2022-06-16 15:50:47 +0000

    swtpm: add 0.7.3 bottle.

--- Formula/swtpm.rb
+++ Formula/swtpm.rb
@@ -5,6 +5,15 @@ class Swtpm < Formula
   sha256 "e856d1f5842fb3335164f02f2c545dd329efbc3416db20b7a327e991a4cd49c8"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 arm64_monterey: "e64ec8dfc81f9199988ced1cf856376ee943c08960339c37e5e07204c53a745f"
+    sha256 arm64_big_sur:  "db386f2737ef9305ce649a1cc776511ee84ca80094f91c07b8587f31a007195b"
+    sha256 monterey:       "a706d9534f003e7fbf8d3cd42594e51f3d15e65f0ee05b8d187f6342eff68a0a"
+    sha256 big_sur:        "f178191eb052c516e200709635508e8b707af00ff1d787d4260d2926f12ef6cb"
+    sha256 catalina:       "a2e95c5c912c5e86a0552b69a496a8b611f54a18b989e61055ae161113284068"
+    sha256 x86_64_linux:   "e49ae25d839661e0add9e92edb2165f4764b2ccf5c67e88316c9765c3ff8f252"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gawk" => :build
