dd50a5fe89a7c383d215080b9db708f4320c8810
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-16 08:06:57 +0000

    atmos: update 1.4.21 bottle.

--- Formula/atmos.rb
+++ Formula/atmos.rb
@@ -6,12 +6,12 @@ class Atmos < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0bdb209da38f7629ec13e8865ee684ce0646aedcd37bde534da11fe58467b91a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2a6ebb10235617b22bf69bd16ef792b6af95c9e0147cac624b1f236c9f65578"
-    sha256 cellar: :any_skip_relocation, monterey:       "1ec9405059e0cfe772511576e32f1f4259ed945bfbd4b91cefeaa656600738af"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fff734771d6e8082b59e9ff2208efbbd1241550e54c82646a38f710b2c522e44"
-    sha256 cellar: :any_skip_relocation, catalina:       "0c893b4d553013692723fd48a097f6e8547182f5b4fac66b48581037f5f49d1d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a39b5315845c69f06198e82c5a22764a7e4ee4521ffcec7dd1de9edd57b9828f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0cfdd9c08e1dd0ddf005377582e6d8c2cd14f3ece0e3481e336b0527615b0c10"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c6a5afe18ff4bf97a639c0c042035ec83ead10edb6eac5298e74824f847a8dd3"
+    sha256 cellar: :any_skip_relocation, monterey:       "2a63b14bc88524051ebdccfb20be77d433136e8c429ca60aadd209948f445caf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "caaf787a748892d98cc0055bedc99bf3cde72ab41cae65fca104e5b9c2946044"
+    sha256 cellar: :any_skip_relocation, catalina:       "e6187bd5394e151c715ca6bc9849aafc4eb10dcf8c27649cd626edecf5e1f513"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb46d0d4972b8cd64d182f08a5133c55f13eff753780de19abf5e640eb2fd16b"
   end
 
   depends_on "go" => :build
