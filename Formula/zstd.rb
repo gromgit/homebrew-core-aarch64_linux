e12cdbde09b0bfef54ebc1f0c6edd3c13fc227c5
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 04:00:54 +0800

    zstd: update 1.5.2 bottle.

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -9,13 +9,8 @@ class Zstd < Formula
   head "https://github.com/facebook/zstd.git", branch: "dev"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_monterey: "c7084442d57331eb75dc51926bdbaab33c31059587a1626c714be87c1c43df49"
-    sha256 cellar: :any,                 arm64_big_sur:  "f4be0a469b31cebf7a44d4602d1f31303a211405ce566b6ce1a8743093603b3d"
-    sha256 cellar: :any,                 monterey:       "3cd1dc42736c2873b0684f0da4ffe3e9fdac30ec9ed5b4c0f9f657bc0d5c3f50"
-    sha256 cellar: :any,                 big_sur:        "c79db7b0ca3d67b1094221c062a74e504aeddf2e8df782c01bc551a16794f73f"
-    sha256 cellar: :any,                 catalina:       "bb4069e2e9d4eb1bfe3c8b4e4c7d4fdedb689b48f46a7ab008591aa7230b765f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b612ded8e47a6049f2645d2de06aa74a3a6d47b3c6d8cb166a008cbe808e9bc6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/zstd"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "02a6d7e30c654ca1f2c6446ced0eeaf12a8b7da8ebf111cecbd9013185bcd9b1"
   end
 
   depends_on "cmake" => :build
