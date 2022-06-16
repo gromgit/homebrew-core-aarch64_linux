c5932a42c3d7ce4ad97e649573efef14417b6b88
messense <messense@icloud.com>
2022-06-16 12:24:32 +0800

    maturin 0.12.20
    Closes #103797.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/maturin.rb
+++ Formula/maturin.rb
@@ -1,8 +1,8 @@
 class Maturin < Formula
   desc "Build and publish Rust crates as Python packages"
   homepage "https://github.com/PyO3/maturin"
-  url "https://github.com/PyO3/maturin/archive/refs/tags/v0.12.19.tar.gz"
-  sha256 "293e796589cf3dd24404973c67bace3d80b2f44c41c9f04531cb7e4b445431b9"
+  url "https://github.com/PyO3/maturin/archive/refs/tags/v0.12.20.tar.gz"
+  sha256 "5a1c9ed2acd8ea1edbe7e8b7ed78f2f6887eff77f696dbdd93f81b606960efbe"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/PyO3/maturin.git", branch: "main"
 
