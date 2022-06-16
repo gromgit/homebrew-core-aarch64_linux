78ff1081015214e8fd5944872d82ac646fc19413
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-16 05:10:45 +0000

    cobalt 0.18.0
    Closes #103801.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cobalt.rb
+++ Formula/cobalt.rb
@@ -1,8 +1,8 @@
 class Cobalt < Formula
   desc "Static site generator written in Rust"
   homepage "https://cobalt-org.github.io/"
-  url "https://github.com/cobalt-org/cobalt.rs/archive/v0.17.5.tar.gz"
-  sha256 "17603490ba4817ec6ab3c486e39ac5863dd8625b58d8f8bfb534c83d7af334ef"
+  url "https://github.com/cobalt-org/cobalt.rs/archive/v0.18.0.tar.gz"
+  sha256 "1ecab64da429ab362fa2c1eced7b1bb91bae0c3d144808db82d7410059099aff"
   license "MIT"
 
   bottle do
