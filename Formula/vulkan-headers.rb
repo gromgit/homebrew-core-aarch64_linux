a3a3c672dd1ec152d9ef9191e0863b87b338ec71
Porkepix <porkepix@gmail.com>
2022-06-16 16:49:04 +0200

    vulkan-headers 1.3.218
    Closes #103846.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.217.tar.gz"
-  sha256 "004b4f7841bd912d1cc3f5ac5694d5dea2f944f10053451233b533874368df87"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.218.tar.gz"
+  sha256 "9fd435580db06105036dee3c98a1c6460dea5e9ec618b8ca02fa2da463d14e27"
   license "Apache-2.0"
 
   bottle do
