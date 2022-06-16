71cf841261af3c7421c64f66bab495b104f74cd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-16 08:05:51 +0000

    prettier: update 2.7.1 bottle.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -9,7 +9,7 @@ class Prettier < Formula
   head "https://github.com/prettier/prettier.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "639dedd67e122fc2e44c4485b3c2ffa907dfb24f40194af6694ae67cc2bda2ca"
+    sha256 cellar: :any_skip_relocation, all: "282fde3b3edffa092dc3f199d35c66a6c4a335c1ad0bc267aacd54794223bdb4"
   end
 
   depends_on "node"
