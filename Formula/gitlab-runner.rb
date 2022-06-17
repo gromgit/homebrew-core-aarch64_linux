571c6dbb220eb4139cd61491588f0386eeefdf8b
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 04:19:28 +0800

    gitlab-runner: update 14.10.1 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -13,12 +13,8 @@ class GitlabRunner < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0e2b596c6d479c431753ec81c13460a72394ae3279acc06bf9a670e9540f0f25"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "33300fe4f56c96893fc59b0672bbe82f84123a839b84bb29848ed866cf0baaef"
-    sha256 cellar: :any_skip_relocation, monterey:       "2a80f289cd3224c9eaede4716f7f06ca4b396ae78af1e65c63b2276a65ed0f1e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "caaab675fad7df55e758b4b16f1563cccad4732c396cf775db90df8618fcf6d1"
-    sha256 cellar: :any_skip_relocation, catalina:       "a7e0bedbf11e2944eac06481e388a9ce3643b4dba91e7c82dec7f60b52f9f433"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad76e489c8be01b275e91ce8f7e4acbb35dcb1ff3e6893197e12e1c2bc5eada9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gitlab-runner"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bb6a173d4905dcc7a9a570b33804d4f2a07c3d3a6fdb5cbe361f6a81fc7f77e9"
   end
 
   # Bump to 1.18 when x/sys is updated (likely 14.9).
