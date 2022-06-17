cb4ea5cbb6590bd742ccc45502f0f5f22d9861dd
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 05:47:20 +0800

    goplus: update 1.0.39_1 bottle.

--- Formula/goplus.rb
+++ Formula/goplus.rb
@@ -8,12 +8,8 @@ class Goplus < Formula
   head "https://github.com/goplus/gop.git", branch: "main"
 
   bottle do
-    sha256 arm64_monterey: "787817b6d30ce045804b5cd4124f2c157ca3ebc04b0f8c49f6c7576b5ea322aa"
-    sha256 arm64_big_sur:  "4d9b065bbec3d8b4b9e161c3ac9fdf4517e0301500faf0567fb60b166b336af7"
-    sha256 monterey:       "dba5db488bf81f97428478ca4615291d6a09021c06ba77102c144d7f2a97ae63"
-    sha256 big_sur:        "fab34d637e3cc69e5ee473f31ce2888092c3dc1cd771942103ec57a28406489a"
-    sha256 catalina:       "a59780cc2d6f9170de630ed38da0680b963076a821fb4dd5ee01d73b98ee2db9"
-    sha256 x86_64_linux:   "36a85ee921d8c8a65cb03c6b7e57c35ae1fbb4edc8dc529288bf8fd0b21b5507"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/goplus"
+    sha256 aarch64_linux: "c518f61987b1891cc2bce4bcf1c5650b039bf380765afb800b234867d69311c9"
   end
 
   # Bump to 1.18 on the next release (1.1.0).
