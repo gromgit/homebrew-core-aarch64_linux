541923176181101f28f4528ff742c6030c5e6eec
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 19:49:15 +0800

    file-formula: update 5.41 bottle.

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -14,13 +14,8 @@ class FileFormula < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "62330e525abefef9b0ee14c3c68eda5b3929885d40047ed3ff986a55530c1291"
-    sha256 cellar: :any,                 arm64_big_sur:  "15e4b048fc9187617a94167c6a380334ba6b007f10c0b2e053f7eb819a07f487"
-    sha256 cellar: :any,                 monterey:       "c7e02240e32e51a7864951464452210f93ab0d69b1db271620bdbca05c10664b"
-    sha256 cellar: :any,                 big_sur:        "23a026b284b4a3b4d35bd2523dd2ff29e7067ba1a9398237f22187ff4b03ea99"
-    sha256 cellar: :any,                 catalina:       "42b22fa1942a019e33f96f30af4e58bbffd586a14d9fba7318973b9d519aa264"
-    sha256 cellar: :any,                 mojave:         "5b11f9096a34a2d37b3fdfc89836936d539b3db6da1e5468fc4d21e25d76160c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a7e755b89c571f80b085d72602f34faefdaab64f7d879c3bb876c46480e8c69"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/file-formula"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a29f451b11028b8e149f9955ef6f3678759f36468cb7716893b30c940939c035"
   end
 
   keg_only :provided_by_macos
