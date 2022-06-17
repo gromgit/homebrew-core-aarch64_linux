256e8b212b9f78a9c54c3a15a172d40d4b1fa3ae
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 14:07:32 +0800

    circleci: update 0.1.17554 bottle.

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -9,12 +9,8 @@ class Circleci < Formula
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0487f94d6b6431b2ed76bdf922671d6638511f4fac5bfd223a77efafc1c79c85"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "90ba50c6086c8801002bfe6557e292cb262d6c83c4ceed14c96e6420170bd704"
-    sha256 cellar: :any_skip_relocation, monterey:       "b2c7ac67c6f9ccd755ca9321b3f3c2f718fb628319cbda1225e112b43e6aa87f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "57f5c9ae958f9af677a8beb5d334a539ed81c2089b7bf524702a8d324b429445"
-    sha256 cellar: :any_skip_relocation, catalina:       "4b1f33838372650f8f76dcefe7eaa9b1b821cca2e30b28f3b64a96864dd7fa92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee60746d7ca17a80ed9d4d0860ba32476f27128f4840a99447a0e7c805ea7ad2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/circleci"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "54bf2e44fd008a5d51d1441cea5a3d5a598f97b47c7f260190b573b3a732eef8"
   end
 
   # Bump to 1.18 when the x/sys dependency is updated upstream.
