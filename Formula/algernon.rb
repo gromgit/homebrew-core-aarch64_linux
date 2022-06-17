711f5808d2d22fe4a9db04a4f4242937b9908629
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 09:09:32 +0800

    algernon: update 1.12.14 bottle.

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -13,13 +13,8 @@ class Algernon < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1da0199a842951f84657d8bcba74637c80ea2266576f9ff83d58aa0482c1ea85"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0eaa6910677a3aa0a1be868af31c73e7390d420f41c7950e905d6d52556bde0b"
-    sha256 cellar: :any_skip_relocation, monterey:       "b2ad82e5156e897e0000273928084cb059ad30c73683b2f69cba53ce212fbcae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ffe7eed6b3576166e41b66beecdccc47aabed4644119190a1534ec8210fb25cc"
-    sha256 cellar: :any_skip_relocation, catalina:       "57e11ff2b146da5e254189058ec5502bda66d7213996daf8846756cca5de38ec"
-    sha256 cellar: :any_skip_relocation, mojave:         "c06af8b3677a3d46e7be0160533e8da8b7512b848a24105d498c0a9b1d381125"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ccdca9ac607c215c4981e35dc13101c5acc0533edd1a5441bd3c874dea275b2a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/algernon"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c7f77e9d279b0d4859f08c97506baee7b75a0b2a447231b001837ef59c790072"
   end
 
   # Go 1.18 can be used after the next release (1.12.15 or later).
