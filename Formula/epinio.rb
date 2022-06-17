396ba6c5661e92776d21941fa4eb33da55a531ce
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 18:54:08 +0800

    epinio: update 0.7.1 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -6,12 +6,8 @@ class Epinio < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d06c1f2d6ba97932c384523524156c3ef3a85233136929cceeee6af5f8214a8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b4e39f6c06d93d6dc04bfeb9b9edb32e22e8b2b35400366ca4eac66080cf77bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb5ec09403d1bae1f7c735ae1421a971ea36f66adad487f658168c0d36cbef9c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ab564a1321193caf8ae2c811634076db7b20c2d75be4bea442b6e0569520fb10"
-    sha256 cellar: :any_skip_relocation, catalina:       "4884df241229b354bee98edb7f60169c9f253a14c077641cd6ae27579d9cb27b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af8db289bd0578f756eff57d1a120d260e2e62d4c7d087f3ed7debc7f236bc80"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/epinio"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d894c9c40c922cb4befb7f11f8601b7a0c33bd2eb30f286f5d0e3293d4bf98a6"
   end
 
   depends_on "go@1.17" => :build
