7388230ed01bf477bf528b29ce060179c60d53c5
Rui Chen <rui@chenrui.dev>
2022-06-17 23:35:44 +0000

    adamstark-audiofile: add 1.1.0 bottle.

--- Formula/adamstark-audiofile.rb
+++ Formula/adamstark-audiofile.rb
@@ -5,6 +5,10 @@ class AdamstarkAudiofile < Formula
   sha256 "7546e39ca17ac09c653f46bfecce4a9936fae3784209ad53094915c78792a327"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7d3026d78153c7dae8d493ed0265c9ef87888d2645a37a1b8b564c202fc897df"
+  end
+
   def install
     include.install "AudioFile.h"
   end
