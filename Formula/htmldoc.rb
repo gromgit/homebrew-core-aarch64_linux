c919efe9f859352a21556b6b7dae5d4702028352
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 07:09:18 +0800

    htmldoc: update 1.9.15 bottle.

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -7,12 +7,8 @@ class Htmldoc < Formula
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "39479bbbf19a5a2c35320aa410690830862ddc48f35c47b15a6f8e0388859765"
-    sha256 arm64_big_sur:  "1e9132616e07ca6999ddb9d6bb9b8456969b27e15e228bebfeca44dc05f34711"
-    sha256 monterey:       "9cbc78f597a80f0e2807cbf299b1a0cbb17d06cd27fa55697ae6ed8e1e5a132b"
-    sha256 big_sur:        "c4f86d41cf26e314deb251bde3a59cbaefcc15077b4fb365e8aa9e5b57723f33"
-    sha256 catalina:       "e0b03ef14f3c9ffa4ba04434f8cad7fa9646f701b633455b623312a57765fc10"
-    sha256 x86_64_linux:   "ce5c9fcc2a61dfb9784e7c60ab9cc9211ce0bec0b5f71537ccdf4bdbd59f2fd2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/htmldoc"
+    sha256 aarch64_linux: "5bc88f1b638183c353b63b024d6746f9857237ec3049742587889e8c34030564"
   end
 
   depends_on "pkg-config" => :build
