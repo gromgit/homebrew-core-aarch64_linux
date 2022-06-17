2aa5649617333e445b515bcabb545a7a67bb3059
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 11:13:58 +0800

    bastet: update 0.43.2_1 bottle.

--- Formula/bastet.rb
+++ Formula/bastet.rb
@@ -7,12 +7,8 @@ class Bastet < Formula
   revision 1
 
   bottle do
-    sha256 arm64_monterey: "e51ad7ddfc917c6c9cb1ee49df6b3ab37679df786d9b3dbedfc2e17e0d85b477"
-    sha256 arm64_big_sur:  "10a5c192d9f90bb6b54d453a9d397bfd48d202a01d0725edd71c80612d23aa2f"
-    sha256 monterey:       "3c198fb3b132431c891263f11e29a7966e40446030a233adcc0ee468aef0bf00"
-    sha256 big_sur:        "6a997a680e2592f04456eba07c1a15ff2ec6289b7509d3d187d044b221beb64c"
-    sha256 catalina:       "2e78ee311ca8b920efeff2c5a877a7e351b3c4e8edfa16f35ff8a953ef1586ad"
-    sha256 x86_64_linux:   "ea80b12f8cf782502f803e24e168e394ba009a853770fdd6761d3e89149dbe91"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bastet"
+    sha256 aarch64_linux: "6045022d9c99a692a6ae060d49551b3de591b50966dae835e91fba26834382b7"
   end
 
   depends_on "boost"
