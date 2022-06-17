072c06f1b92df195a11744781d0d7175e57c5fbc
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 12:12:23 +0800

    boost-bcp: update 1.78.0 bottle.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -12,12 +12,8 @@ class BoostBcp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b94358c7073feca948b1c65ba8d5f2f83f9f2d1faea94e71f26ea1bbf205a7f3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c705ee1805c2b72cc152a2e17e2bd095b1621311d4df05e6264aa1f0ba4cb53d"
-    sha256 cellar: :any_skip_relocation, monterey:       "a6b89f26b19cba2576beed6b14663dfdb994bc5723a305d7e26b40e401c8fa7c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "61636de3949e7652cd497d380b39932b8853d35fd2736865eb52eea7ccda8a5a"
-    sha256 cellar: :any_skip_relocation, catalina:       "aa980455feef4a55c85ee15552b5f3e49a1a7a8caacccb71f168da0fc847859d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9e71b1847793712fa48aed2a4c45a2ecd013129d416c221e773696784d455f1f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/boost-bcp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bc5fb969c35ecaf90d115d2dda9536b4497bd9f9d087c1f2255998cfffa5c719"
   end
 
   depends_on "boost-build" => :build
