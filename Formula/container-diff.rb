943f919185370d2fec9d59b2bbd56a84524ca42e
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 15:11:43 +0800

    container-diff: update 0.17.0 bottle.

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -6,13 +6,8 @@ class ContainerDiff < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9df5a58421447c581cf17ec7b7ccd2f131156004bf9b7adb87f4fe6a6fd54bec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e41a2030963aa17e984e444844f065e21f3db400500602dd9fb70c15fab6efd3"
-    sha256 cellar: :any_skip_relocation, monterey:       "0eea29192581a1dea12c189c7e8fc9a2a339cc721c12306676fffcb330f34bce"
-    sha256 cellar: :any_skip_relocation, big_sur:        "339c0ec5e9dbe0b5255a3ca87c316cc159741bb7b6ae43189a9d20af8fb5a63e"
-    sha256 cellar: :any_skip_relocation, catalina:       "7b09d72b8cea67e283520a37ffb5082b7070443a5da1f78584270488ea6f8f74"
-    sha256 cellar: :any_skip_relocation, mojave:         "4c9f7078b38379711d7eb961e9ed670a13a3240ce0c1d99d910d8313daa412bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0bd5bb07cb846d439d1e88afbdc6a9e88301f155b470465d1e8a0c8c015032d3"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/container-diff"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "fdedf4e9066f52918e2de230790119f83afaee312404c9a51d530219095fb336"
   end
 
   # Bump to 1.18 on the next release, if possible.
