8a9af112c47dc3568958a4795f450a3f52454cb1
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-17 16:36:29 -0400

    kyverno 1.7.1
    Closes #103933.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kyverno.rb
+++ Formula/kyverno.rb
@@ -2,8 +2,8 @@ class Kyverno < Formula
   desc "Kubernetes Native Policy Management"
   homepage "https://kyverno.io/"
   url "https://github.com/kyverno/kyverno.git",
-      tag:      "v1.7.0",
-      revision: "704dc46ec30aeb548cd2ad6aa4c02c39829a4823"
+      tag:      "v1.7.1",
+      revision: "060b12d2a281720a29c0de99ea860a6db1c95c6a"
   license "Apache-2.0"
   head "https://github.com/kyverno/kyverno.git", branch: "main"
 
