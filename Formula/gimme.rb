0fb71ee44ed944ef36b33a3bd569dd129cf9fb7c
Porkepix <porkepix@gmail.com>
2022-06-17 11:36:45 +0200

    gimme 1.5.5
    Closes #103908.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gimme.rb
+++ Formula/gimme.rb
@@ -1,8 +1,8 @@
 class Gimme < Formula
   desc "Shell script to install any Go version"
   homepage "https://github.com/travis-ci/gimme"
-  url "https://github.com/travis-ci/gimme/archive/v1.5.4.tar.gz"
-  sha256 "71036f892b3cae08f29be6fd4c69fb20d9b003ec80a24221d73e995e12ab0fe0"
+  url "https://github.com/travis-ci/gimme/archive/v1.5.5.tar.gz"
+  sha256 "7854c3f90c2274e14041138f53c9c0bf671be86e49dfb4a61b024270a514fb40"
   license "MIT"
 
   bottle do
