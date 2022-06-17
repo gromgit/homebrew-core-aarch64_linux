81879a503250250c65d179767e595f35e1b3edc6
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 05:08:44 +0800

    go-jsonnet: update 0.18.0 bottle.

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -7,12 +7,8 @@ class GoJsonnet < Formula
   head "https://github.com/google/go-jsonnet.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5c0d56b389893d87a8358a31775561c373cd117a311ee9f64bbc5988c8d91dd0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "09a74a4df5e13d87696fd6c610d8e999e7edeee71de24f1ec4f537b53d156bf6"
-    sha256 cellar: :any_skip_relocation, monterey:       "420b6afe22b881a4383719795bb4691c07da03d4b116a3c02b99bea1117cad24"
-    sha256 cellar: :any_skip_relocation, big_sur:        "855113843b02eaa3569e714f14bdfccc3adc98cb53b29aac76b713ef946fc04c"
-    sha256 cellar: :any_skip_relocation, catalina:       "71bacab1628ec14c8cd732fcb6f4bb3ace3acf73b10a8169fca0aeccd524c08c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5dcfab621b2f70e5411635f841346ac28ce59328ac0508d775d4b394970effe9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/go-jsonnet"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7d563fb0175397a120d634bb1a42504d61e8e5450e29363e7d51aa806bae2cfb"
   end
 
   # Bump to 1.18 on the next release, if possible.
