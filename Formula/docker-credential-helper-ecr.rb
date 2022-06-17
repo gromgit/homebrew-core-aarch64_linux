339e78e51021fe1bb41397276e341b6818ff1a48
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 16:52:27 +0800

    docker-credential-helper-ecr: update 0.6.0 bottle.

--- Formula/docker-credential-helper-ecr.rb
+++ Formula/docker-credential-helper-ecr.rb
@@ -12,12 +12,8 @@ class DockerCredentialHelperEcr < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "04e5ea0120e3f7fc82748a7a76e0fa7b74b266c1dc3906de1eb15b2d3be9e95b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ba8838fba8008c13eab1584b0c793dde3056d8e6aaabb25b622461eba910942"
-    sha256 cellar: :any_skip_relocation, monterey:       "5b8acfc95b569fa6514079f6988dbd8299c63428b1f77d2dce6566ae992da76a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "304a36764da647e5e058b853114949e797f8f57a2c529f2b869d6d6ae2da2305"
-    sha256 cellar: :any_skip_relocation, catalina:       "c3075232b5bebdd119e6cee8734d1c5ea56bdbbbc756d166610e1eaf11696fb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd1a454497ce215850e2d524d74630a114095da43135b6ddf2bcef8db2e1aee4"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/docker-credential-helper-ecr"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a7e8114c1d6f3e06487d88fdf2502860361bac72f4b26a234474b62803bfa262"
   end
 
   depends_on "go" => :build
