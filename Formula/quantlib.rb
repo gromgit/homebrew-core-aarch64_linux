c970d050578713a03fea7615e0940c17a89b6e0c
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 04:53:11 +0800

    quantlib: update 1.26 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -6,12 +6,8 @@ class Quantlib < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2ddeb386e7eb27aab8f00620125603599dd3371039ad911a6e0196c1ffaba809"
-    sha256 cellar: :any,                 arm64_big_sur:  "cfce6241b00cc27687f707239e799d8d3d1cc8867cabfcd19170dea66c82020c"
-    sha256 cellar: :any,                 monterey:       "ed88f2b4b58d73bc14a509d387a9c5f78949f29dc82438a240d35b2fee800bd5"
-    sha256 cellar: :any,                 big_sur:        "abbf1c1ae1dd97370d5368bc59e18ec2ef85217c7d75a073326558111d665f29"
-    sha256 cellar: :any,                 catalina:       "a2d53f05e70f1ee733ae7d7f6617c00bd8fd032a725c8ab02dc87581864352fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a08314adef6c54e10a7fb4eee7c38f811791a055a4cf90241208a802db7fbba6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/quantlib"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "12cff90963e5b4f3d16044f6f3524786e44afd15a035f1d1cbe676b5d1661f10"
   end
 
   head do
