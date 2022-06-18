4bba984c02f57d0bfd3905567b994b788795fc5c
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 22:09:07 +0800

    openrtsp: update 2022.04.26 bottle.

--- Formula/openrtsp.rb
+++ Formula/openrtsp.rb
@@ -13,12 +13,8 @@ class Openrtsp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "622a394f9c99ac33337c313c4230f3a181f8775556fd8599e638a3d85aceb29f"
-    sha256 cellar: :any,                 arm64_big_sur:  "2f9ead171859af4e4368b8da5e7324b91c21543615b3fa2ab7db4b0d3e7a6c8b"
-    sha256 cellar: :any,                 monterey:       "f375a56bcd91d78edb03fe2896bd0d73211e19219af3317e445beb1455a787ab"
-    sha256 cellar: :any,                 big_sur:        "9f903163cc730e42c97132101505ad774ed1f7a1c8461d0699e8820efb34e7f6"
-    sha256 cellar: :any,                 catalina:       "cc5237f1fa833ea1714fdfceb0d2ca4d8d9437bfdb7229c731eda771e70202b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c62c1e77768cbb167003e6bc8ea963ea2d4897f08a5277a4e9ed93cc643f7e07"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/openrtsp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d4b3e5cb452f8e29ca4a030d3f20273c7de4df05bd12ebe5754b8c174b03d3a0"
   end
 
   depends_on "openssl@1.1"
