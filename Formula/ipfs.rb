83939a45691d2bafb75d44ef123a130583373175
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 09:04:03 +0800

    ipfs: update 0.12.2 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -16,12 +16,8 @@ class Ipfs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7b096d6f28b2c338b27ae4bc89e2d5d948f70530ec03aef8d623b5162bf3f33c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66a04fee70fd546ed59d1b0889f726e72d307e7cb75485e97e5226fc7b080d3a"
-    sha256 cellar: :any_skip_relocation, monterey:       "f40b8ac2e2ff38e4be021c6f220851abf93a0f52be3294230531cafa979a9041"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c252151aea93f3d1338a52fe20bef3d7b02489bced42fea4c00e0d6c15b3db0f"
-    sha256 cellar: :any_skip_relocation, catalina:       "6101a28948da2d09cdebdfeedabb17ab59e999b3677b327ed95822bed2cc0e9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdd652dd1d3e09ddfc815feaa00039a03468da60739a41957f5c5b073afc5b37"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ipfs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "17c29a0c0f1730184560f22944bab81b462bb063965ad528782dae610fd2284b"
   end
 
   # Bump to 1.18 on the next release, if possible.
