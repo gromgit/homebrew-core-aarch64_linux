f8ea6cf2a626aba2bde5925851809e298655915b
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 06:15:47 +0800

    s-nail: update 14.9.24 bottle.

--- Formula/s-nail.rb
+++ Formula/s-nail.rb
@@ -18,12 +18,8 @@ class SNail < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "c76c42e8a887cf6ab33dbcecfd0644c6bc6aef38e27234bdf97e56aac6cd5372"
-    sha256 arm64_big_sur:  "f3124f53d46be06a975bd71b9ddebb1eead0104094a32c97e55a8e6f0b3cc635"
-    sha256 monterey:       "4d68b331ebc8b46aad429e3e91adc5009ee9ea936467979c2744b24b5304b35d"
-    sha256 big_sur:        "60c960dbe634d90101cd9c1fb9efc6119ae896a9ed8387592cf9300b6ac81573"
-    sha256 catalina:       "7eebdbe58288d603c2f584e961839fd233437c887b1e3bcd0f200c7f6a3436a5"
-    sha256 x86_64_linux:   "1783d8ab84a3d696f709685f91dff33cc1decbc5424271d9f2adc0f39e43aeef"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/s-nail"
+    sha256 aarch64_linux: "56652710c161ee272b53a3ca0dc1750d773a848ec6e22a6a95b47cb4ca97be77"
   end
 
   depends_on "awk" => :build
