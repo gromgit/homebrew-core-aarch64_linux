bd14803ba46a105003eaa60565ab2246fa116567
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 10:33:11 +0800

    k3sup: update 0.11.3 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -13,12 +13,8 @@ class K3sup < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3d66c72cdaadc1dc97b2d28763d1943b16f5d96124e19e703b9618d22a23d7c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5284958a0f20f78aa4fc879482d167276119ed3ade631839d31ea568fa65d774"
-    sha256 cellar: :any_skip_relocation, monterey:       "5da6c2673b94b2331820707e4d7fa075d4db08dcd5b57afb586e788ed31a6888"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bb8bd85925544da7369dd123383d22c60046e1a0861c56b584cdd9ab6e691ded"
-    sha256 cellar: :any_skip_relocation, catalina:       "fa6e400fc912c6bf18c95ca45d24b7307ba81895e8424c92ef27f9c29f6a4439"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1e7d21a5c3b460560123484eb0d9fb10150f64b75410a7d455ff64282de3fd1f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/k3sup"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4fd69fc3a2ee82809c5308828c64ade64fb401fc726f42866e3c6056e43cd90c"
   end
 
   # Bump to 1.18 on the next release, if possible.
