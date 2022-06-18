12c7027724f1811fa62d6676fb22971d99f49d85
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 07:32:19 +0800

    sipsak: update 0.9.8.1 bottle.

--- Formula/sipsak.rb
+++ Formula/sipsak.rb
@@ -6,13 +6,8 @@ class Sipsak < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f2f4bf2277f852ef0efc351e36ff629a9f17d9779656e9c5f15022571b6a8350"
-    sha256 cellar: :any,                 arm64_big_sur:  "4525ec303fc0f5ffd3b752ccf1dcdc7fdb14921ad35d8e12109e257c47ce14fc"
-    sha256 cellar: :any,                 monterey:       "3a220f3f6e45c89f05948970e5bfd8ee64030674e50dea30f4713173abcc3a25"
-    sha256 cellar: :any,                 big_sur:        "70442c0b739c1fb357e8eef8246d56425fdde5f094bfe8048304cc2b42fbfe0b"
-    sha256 cellar: :any,                 catalina:       "3d7c198e46fd2e183d199718d175111e9024d4ea8f453685fe973e76c342f988"
-    sha256 cellar: :any,                 mojave:         "9f27279cd8a53e5d707d7208ba2ba5f5170dd775854f30396135a296dd9c55dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7a953aef939a2e020ceb548db309d98462b4b2ca0ea2e9f70c0f298a7f0dbd3c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/sipsak"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "49047dfdd3f5ad41de669a3855a883eac18fce2a9f8bbefd05cc95b352a68f56"
   end
 
   depends_on "openssl@1.1"
