8f307011ec65329662c15e385cf46376048b58db
Adrian Ho <the.gromgit@gmail.com>
2022-06-18 13:30:30 +0800

    lazydocker: update 0.13 bottle.

--- Formula/lazydocker.rb
+++ Formula/lazydocker.rb
@@ -7,12 +7,8 @@ class Lazydocker < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5206bb97f1b97937bd3a7f55ef60e3cdcf7dcd11a528213669647ed5624dd87"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d5fc99b6f69ee9397be2f136247ad7d049419144de1fc6cd767ae229c93385c"
-    sha256 cellar: :any_skip_relocation, monterey:       "513c7b6ab8549e9e59a31172167a69b1dd96ed4c3c20147b42174ae32ab725d0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "abadea905b75a2569f452f77a19126057fbfb2a702e982e3bdb01a8308aa3935"
-    sha256 cellar: :any_skip_relocation, catalina:       "67e73e0d74ff5920cdfca6c786b729e18207166d4cfb1b7237f981a57ab494ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb946c3fa3d69068bd29ffa13eb6bbda0d987bf398bb60d34bf9c1dfb01a2007"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/lazydocker"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4e5f60e68b0d667a3f441209f103823e66170e9c4afb2a1964109d0d91c338da"
   end
 
   # Bump to 1.18 on the next release, if possible.
