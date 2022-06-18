c9196c8b71fe17916e5465fa0a229189f88182a9
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 05:17:41 +0800

    re-flex: update 3.2.6 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -6,12 +6,8 @@ class ReFlex < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "da621d951d442b682f6832593d139ae16ff7ad6a084b8e1859a0bd42c98a5361"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6dafaa4763272d11b8cd20f36b2590e900ac764e220357123485f6569823a255"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e2d0de6dcc85206edd166175f0ac58e305b5d0e59e4e1eed94838485e0afa92"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dc9165ea553bad82e557fe2bc61dd9cf87bda4b301f89ba98aea23291e9c09e9"
-    sha256 cellar: :any_skip_relocation, catalina:       "dbdf80c6f2bf5a6c34d0279a69145ce29ca40bdfdcca0a60f54b0dfc8f75c20c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6b5bd8f1b6f86768a4fdd29ebd07bd03150fce85cc5dc22cf482a9ea044aae1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/re-flex"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cd3551736ad13af3c375d47d3c1b0070f277aa83abde82eab58575e0b3cb7315"
   end
 
   depends_on "pcre2"
