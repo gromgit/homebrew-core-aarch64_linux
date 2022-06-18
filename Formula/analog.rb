a2e8fc975bb36682ae4d12b543f4fd66fc84cd12
Michael Cho <cho-m@tuta.io>
2022-06-18 13:10:25 -0700

    analog 6.0.17
    Closes #103987.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/analog.rb
+++ Formula/analog.rb
@@ -1,10 +1,9 @@
 class Analog < Formula
   desc "Logfile analyzer"
-  # Last known good original homepage: https://web.archive.org/web/20140822104703/analog.cx/
-  homepage "https://tracker.debian.org/pkg/analog"
-  url "https://deb.debian.org/debian/pool/main/a/analog/analog_6.0.orig.tar.gz"
-  sha256 "31c0e2bedd0968f9d4657db233b20427d8c497be98194daf19d6f859d7f6fcca"
-  revision 1
+  homepage "https://www.c-amie.co.uk/software/analog/"
+  url "https://github.com/c-amie/analog-ce/archive/refs/tags/6.0.17.tar.gz"
+  sha256 "0e5794c2eaa5826dc014916e413e90eb2714a646ff8d6ec026437182d789b117"
+  head "https://github.com/c-amie/analog-ce.git", branch: "master"
 
   bottle do
     rebuild 2
@@ -22,28 +21,26 @@ class Analog < Formula
   end
 
   depends_on "gd"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
+  depends_on "pcre"
 
+  uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
   def install
     args = [
       "CC=#{ENV.cc}",
       "CFLAGS=#{ENV.cflags}",
-      %Q(DEFS='-DLANGDIR="#{pkgshare}/lang/"' -DHAVE_ZLIB),
-      "LIBS=-lz -lm",
+      %Q(DEFS='-DLANGDIR="#{pkgshare}/lang/"' -DHAVE_GD -DHAVE_ZLIB -DHAVE_BZLIB -DHAVE_PCRE),
+      "LIBS=-lgd -lpng -ljpeg -lz -lbz2 -lpcre -lm",
+      "OS=#{OS.mac? ? "OSX" : "UNIX"}",
     ]
-    args << if OS.mac?
-      "OS=OSX"
-    else
-      "OS=UNIX"
-    end
     system "make", *args
 
     bin.install "analog"
     pkgshare.install "examples", "how-to", "images", "lang"
-    pkgshare.install "analog.cfg" => "analog.cfg-dist"
+    pkgshare.install "analog.cfg-sample"
     (pkgshare/"examples").install "logfile.log"
     man1.install "analog.man" => "analog.1"
   end
