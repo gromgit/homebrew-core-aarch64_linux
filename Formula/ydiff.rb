43f2508481f8fbf228b05b34fb5feaf34389b294
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-19 21:04:26 +0000

    ydiff: update 1.2_2 bottle.

--- Formula/ydiff.rb
+++ Formula/ydiff.rb
@@ -9,7 +9,13 @@ class Ydiff < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6496ac2da3fe8681060dc732141f174db77cca1f3a8fe7aeffa8201ba2638c1b"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "20ed345c77f5c6d4012ffd27af864750775d764d1755547395bf33c6e81d8a14"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "20ed345c77f5c6d4012ffd27af864750775d764d1755547395bf33c6e81d8a14"
+    sha256 cellar: :any_skip_relocation, monterey:       "8cfafc1dfa19564f3322fca2f64a82545a942432b7ad995c99fcf32ce8986ed2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8cfafc1dfa19564f3322fca2f64a82545a942432b7ad995c99fcf32ce8986ed2"
+    sha256 cellar: :any_skip_relocation, catalina:       "8cfafc1dfa19564f3322fca2f64a82545a942432b7ad995c99fcf32ce8986ed2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0bfd77f8a27a1093ee647a2d9632fc32d13a00744bbfac2f6a6931107cbd39f"
   end
 
   depends_on "python@3.10"
