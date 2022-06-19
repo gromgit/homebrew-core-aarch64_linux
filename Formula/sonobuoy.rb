e2ad44595e0e9112b6140697295ff3ba90b60df9
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 09:00:04 +0800

    sonobuoy: update 0.56.4 bottle.

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -6,12 +6,8 @@ class Sonobuoy < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "22643fb291902c896897832b0a8dd451ee6f02e9420b3720dc9f45ec224f7352"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e71a07fb9d03d175f0111607cbc0c07bfc1cd663fa6a1a03d69a64333d7b2ded"
-    sha256 cellar: :any_skip_relocation, monterey:       "84ee96c38ba58776fa0a6565195efa38d34d568d0e2d3f259fea692885d17c58"
-    sha256 cellar: :any_skip_relocation, big_sur:        "47977b64f18f2cec3fb0bee24436225083fc5190a27a247a96908dfca62f5ac1"
-    sha256 cellar: :any_skip_relocation, catalina:       "ea127d38edc308d882edc8c227ff58ee85982c5218e113e61ef91af2e58e5965"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9cb85110f52994c7d95b1c44f514e27d2b8095a0568b79b17577bf38cf9d9a8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/sonobuoy"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "943c84876c45961db7b694b77069863e0c8669a58376824385bfab7afc270334"
   end
 
   # Segfaults on Go 1.18 - try test it again when updating this formula.
