5ce90f5342673e8aa03deca918245952d0adb009
Branch Vincent <branchevincent@gmail.com>
2022-06-19 14:09:30 -0400

    honcho: use `virtualenv_install_with_resources`
    Closes #104019.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/honcho.rb
+++ Formula/honcho.rb
@@ -1,4 +1,6 @@
 class Honcho < Formula
+  include Language::Python::Virtualenv
+
   desc "Python clone of Foreman, for managing Procfile-based applications"
   homepage "https://github.com/nickstenning/honcho"
   url "https://files.pythonhosted.org/packages/0e/7c/c0aa47711b5ada100273cbe190b33cc12297065ce559989699fd6c1ec0cb/honcho-1.1.0.tar.gz"
@@ -13,12 +15,7 @@ class Honcho < Formula
   depends_on "python@3.10"
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
   end
 
   test do
