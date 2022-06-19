3de7ea8d34550a1177d526faf49dd8fbd55f3312
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-18 20:35:41 -0500

    leapp-cli 0.1.13 (new formula)
    Closes #103998.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/leapp-cli.rb
@@ -0,0 +1,36 @@
+require "language/node"
+
+class LeappCli < Formula
+  desc "Cloud credentials manager cli"
+  homepage "https://github.com/noovolari/leapp"
+  url "https://registry.npmjs.org/@noovolari/leapp-cli/-/leapp-cli-0.1.13.tgz"
+  sha256 "944ce4928c2d2dd562890310ea6f2f96d3ace5f1c3b7e8bbc9ef8323403359d8"
+  license "MPL-2.0"
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
+  depends_on "node"
+
+  on_linux do
+    depends_on "libsecret"
+  end
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    <<~EOS
+      This formula only installs the command-line utilities by default.
+
+      Install Leapp.app with Homebrew Cask:
+        brew install --cask leapp
+    EOS
+  end
+
+  test do
+    assert_match "Leapp app must be running to use this CLI",
+      shell_output("#{bin}/leapp idp-url create --idpUrl https://example.com 2>&1", 2).strip
+  end
+end
