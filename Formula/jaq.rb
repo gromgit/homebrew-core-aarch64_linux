1bca4c191ec572ade82eda8aba31cd4d5f2a1133
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2022-06-19 08:39:55 +0000

    jaq: add 0.6.0 bottle.

--- Formula/jaq.rb
+++ Formula/jaq.rb
@@ -11,6 +11,15 @@ class Jaq < Formula
     regex(/"num":\s*"(\d+(?:\.\d+)+)"/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dbede05c6da0f769eb033adff55af1d561def4928c62fa11f3c9845ef0e1ff42"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e9d0eb9590fe2e290752e9124961bb84ea933b9d323ca4e877f68b52ba32d62"
+    sha256 cellar: :any_skip_relocation, monterey:       "d93b38e5ca16cddb190b53be481f1125419c97d174f0379895762c1dfc71c3c2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "093579da0f00561e64ae79e48a353c3cc9c38992856a53eac53ee0a5d881d493"
+    sha256 cellar: :any_skip_relocation, catalina:       "674dc1acab4faef28bbb727dd4ac9f08ff2f4bf92340dbff882aad94960e1e81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e169ace9d2c22725bf5c80a33f4e6c081df5539a9e4fc384700e3e0c568dec6"
+  end
+
   depends_on "rust" => :build
 
   def install
