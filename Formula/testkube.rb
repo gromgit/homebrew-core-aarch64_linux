3bc5e0e348c4092d8c510f0cdb21744c4282cd01
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:29:51 +0800

    testkube: no rebuilding required

--- Formula/testkube.rb
+++ Formula/testkube.rb
@@ -1,17 +1,13 @@
 class Testkube < Formula
   desc "Kubernetes-native framework for test definition and execution"
   homepage "https://testkube.io"
-  url "https://github.com/kubeshop/testkube/archive/refs/tags/v1.2.26.tar.gz"
-  sha256 "bd52d7fd40bcaab3b5a9e667d072b7d0ba3d93177bee3e41c3a48941790b23ed"
+  url "https://github.com/kubeshop/testkube/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "dd0f2e27f836f2baab8ee0ddaa107d411eb6a9b3dfeeb8182934dc4cd99493dc"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c28cccd919113c79d80aac02da93ada1f72acfee9a8cb888d6f30c631f408ad"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c28cccd919113c79d80aac02da93ada1f72acfee9a8cb888d6f30c631f408ad"
-    sha256 cellar: :any_skip_relocation, monterey:       "0a87d94048b1ed39a465956387eb4c618666d9f579a50cfb6582e71e13cda7b5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0a87d94048b1ed39a465956387eb4c618666d9f579a50cfb6582e71e13cda7b5"
-    sha256 cellar: :any_skip_relocation, catalina:       "0a87d94048b1ed39a465956387eb4c618666d9f579a50cfb6582e71e13cda7b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff0cd3951567141179d47ded1eddfda0366a689b50493b0ab2f0c401bf37fb41"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/testkube"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bbc00b87f15ee2a327f53b749e10d93baffbc03ba2113d620913f40063150573"
   end
 
   depends_on "go" => :build
