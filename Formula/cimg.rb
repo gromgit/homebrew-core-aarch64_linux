26fa5e1f0870cb661698bfc7fe8d6b4e4a641134
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:08 +0800

    cimg: no rebuilding required

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "https://cimg.eu/"
-  url "https://cimg.eu/files/CImg_3.1.3.zip"
-  sha256 "6814588d5f40c2dd8641a5866603f4a92e71db8d84f36f87b5d8981663a43850"
+  url "https://cimg.eu/files/CImg_3.1.0.zip"
+  sha256 "8c5392ac57764bdaeced142f12c8a79c8dd93277dc61e90dca6b7e1e25cdb01a"
   license "CECILL-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class Cimg < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0f0d4cadf7baf8c6d24010b80048446337b6289b801d9147c7039c9ff73d4da1"
+    sha256 cellar: :any_skip_relocation, all: "e00b458f65da683573c2933c06e8cba72f9fda047bb8ab5a46b44fdd6290ae67"
   end
 
   on_linux do
