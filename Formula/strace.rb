53fe418a03dcb564c4b278124e42ab46c49f5503
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:29:47 +0800

    strace: no rebuilding required

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,12 +1,13 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v5.18/strace-5.18.tar.xz"
-  sha256 "60293ea79ac9253d600cdc9be077ad2988ca22284a439c9e66be5150db3d1187"
+  url "https://github.com/strace/strace/releases/download/v5.17/strace-5.17.tar.xz"
+  sha256 "5fb298dbd1331fd1e1bc94c5c32395860d376101b87c6cd3d1ba9f9aa15c161f"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3a8a7c084e8e214f05fe5fc3c3c3008e9fca5e4a430b8db3d56bcda206c9a1ea"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/strace"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "66f9e72e2ab2463610babf0feb24598a5bc39817d01da306e772ca83d254c803"
   end
 
   head do
