539cd74974ec7ff12a2004285f52a74141456403
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:12 +0800

    csvq: no rebuilding required

--- Formula/csvq.rb
+++ Formula/csvq.rb
@@ -1,8 +1,8 @@
 class Csvq < Formula
   desc "SQL-like query language for csv"
   homepage "https://mithrandie.github.io/csvq"
-  url "https://github.com/mithrandie/csvq/archive/v1.17.1.tar.gz"
-  sha256 "c11e3818aa0f7467a77ed2e11f4db5213d14d53ab7fadb49a7f6a9220533b135"
+  url "https://github.com/mithrandie/csvq/archive/v1.17.0.tar.gz"
+  sha256 "f723b7f8d6263bee4048e73fd2010b4275550b9a2ca2eeca16602c1d6fb3ac49"
   license "MIT"
 
   livecheck do
@@ -11,12 +11,8 @@ class Csvq < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ab68631a0b290f43ee26c64817194d0977615ab666b759d96b9f9703f5434395"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8072040ce8a18aa69acba8a9a0f4e9e427f872dc1d9efc1907f8aa8d3ee72279"
-    sha256 cellar: :any_skip_relocation, monterey:       "92e14af2804855d27a789afa4fe86bc91a354a8a3b7357ba4cbc6a81834a4f51"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d09fa94bbf5fe9e60b646e8e5a72266c14aa887f256e4d002d388090b384df4b"
-    sha256 cellar: :any_skip_relocation, catalina:       "f408fe731f8f021f147f28ff0f499420fc5017939c97db7ff382651ea731d33d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "139576ac0de862ebc3c3265b58ef5187726edb4b5994f82230099b79da219a34"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/csvq"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7869d2381de80d9fb5bde031c98307cea4a5004e76f7949be7483ca3760e784b"
   end
 
   depends_on "go" => :build
