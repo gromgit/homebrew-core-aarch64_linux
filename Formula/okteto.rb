32d8fe6f39a62064d166f3ae201bbfc48f2e33cd
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:29:12 +0800

    okteto: no rebuilding required

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,18 +1,14 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/2.4.0.tar.gz"
-  sha256 "c24ccd498639f0737851e2c5372cf8ac4824cf926c0b3163445117ba12ab8464"
+  url "https://github.com/okteto/okteto/archive/2.2.2.tar.gz"
+  sha256 "d11fe64407335c67d402ee9ff323465e5c5727e8b7261255540b464f7c70c241"
   license "Apache-2.0"
   head "https://github.com/okteto/okteto.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb36377640e2c328fedbabeac5b584a622effd2412b42912417aa18f28b432bd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a29f6ad5581f3be468ecc22f5e309c41581c53e96a3b796291af5c3a4448d20"
-    sha256 cellar: :any_skip_relocation, monterey:       "a2f7fb20c8ae6ba8ede57f85b49a8b74bc1efed53954d5b388aa54c06d7e5cf9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c8cc6beb1e1bca7c47813ec9cf88a65718e48bf22f59b60c576723b251554490"
-    sha256 cellar: :any_skip_relocation, catalina:       "08c1ec637e30f28e7791695abb456f51a66884d56d9f114bb32486fa345d6ca8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57b5d6af901776e7443489472e4cc6aa7151839b448ac9c62184f82b9c94161a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/okteto"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2c218b32735f37da173b36bd16fcc5ace84c7e665bb40f1a2ce80e9904465556"
   end
 
   depends_on "go" => :build
