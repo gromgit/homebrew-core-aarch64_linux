c99fc00ce5b6081a946aeb9bb18219efa0a7c173
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:49 +0800

    ktlint: no rebuilding required

--- Formula/ktlint.rb
+++ Formula/ktlint.rb
@@ -1,12 +1,12 @@
 class Ktlint < Formula
   desc "Anti-bikeshedding Kotlin linter with built-in formatter"
   homepage "https://ktlint.github.io/"
-  url "https://github.com/pinterest/ktlint/releases/download/0.46.0/ktlint"
-  sha256 "6d6e06a189fd095e4128ce32b6033dd916a5c8a1d4186dff829acde4adc0df2d"
+  url "https://github.com/pinterest/ktlint/releases/download/0.45.2/ktlint"
+  sha256 "89491ea865d369b39cfaca2dcf60b38adbdcd74985f5e0170c0bb73034000135"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "022f512313df7bde00ece207961330fc93c71e08e10b58d5b1c3afa04bfa7ed0"
+    sha256 cellar: :any_skip_relocation, all: "480f62bbcaecf11ce13b9be6d39af9e77221c30a98fae58578b331e9be8773f2"
   end
 
   depends_on "openjdk@11"
@@ -21,13 +21,13 @@ class Ktlint < Formula
   end
 
   test do
-    (testpath/"Main.kt").write <<~EOS
+    (testpath/"In.kt").write <<~EOS
       fun main( )
     EOS
     (testpath/"Out.kt").write <<~EOS
       fun main()
     EOS
-    system bin/"ktlint", "-F", "Main.kt"
-    assert_equal shell_output("cat Main.kt"), shell_output("cat Out.kt")
+    system bin/"ktlint", "-F", "In.kt"
+    assert_equal shell_output("cat In.kt"), shell_output("cat Out.kt")
   end
 end
