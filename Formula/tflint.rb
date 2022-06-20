286fe220b588157b39b828e96d1ef2dd1f58c8ec
Rui Chen <rui@chenrui.dev>
2022-06-20 13:09:42 -0400

    tflint 0.38.1
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #104076.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -1,8 +1,8 @@
 class Tflint < Formula
   desc "Linter for Terraform files"
   homepage "https://github.com/terraform-linters/tflint"
-  url "https://github.com/terraform-linters/tflint/archive/v0.37.0.tar.gz"
-  sha256 "ec2024762b9e6e12629cb17f724585cc0c29ddd201590562f7f9b4be2da472fb"
+  url "https://github.com/terraform-linters/tflint/archive/v0.38.1.tar.gz"
+  sha256 "615b114cc0f740d34bdd2b2e148810d1a91ceeeda6a07bfce66f8b94661cf8fb"
   license "MPL-2.0"
   head "https://github.com/terraform-linters/tflint.git", branch: "master"
 
