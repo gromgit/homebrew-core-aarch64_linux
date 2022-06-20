2095efe8ee13bfc236bcbed174fca7a1814b38b3
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:19 +0800

    driftctl: no rebuilding required

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,17 +1,13 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.34.0.tar.gz"
-  sha256 "b8b75d0756f7e703a6bc3ad2c44de75f03402854eff28f0752a9eca80691c186"
+  url "https://github.com/snyk/driftctl/archive/v0.29.0.tar.gz"
+  sha256 "3544e2937f11bdabba65e6e32cdc1d37dd81129002138a00cb72696b10125958"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a017a6fa26b406e8c0e6a39224e4ebaf6b179b5533c873424b12e0dccd2c9f3c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a017a6fa26b406e8c0e6a39224e4ebaf6b179b5533c873424b12e0dccd2c9f3c"
-    sha256 cellar: :any_skip_relocation, monterey:       "393e0801fe0a9366a7aed72d585164480938355b771e9b1b309c14289a865259"
-    sha256 cellar: :any_skip_relocation, big_sur:        "393e0801fe0a9366a7aed72d585164480938355b771e9b1b309c14289a865259"
-    sha256 cellar: :any_skip_relocation, catalina:       "393e0801fe0a9366a7aed72d585164480938355b771e9b1b309c14289a865259"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ff5337bfd89a7fc56e95c6d4265b095c579d4c69da5265a05fccafebe6e2a1e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/driftctl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9805d20e737c155b0fdcdcb000effd46463bff568998abfe65c0a4e58c730b30"
   end
 
   depends_on "go" => :build
@@ -39,7 +35,7 @@ class Driftctl < Formula
 
   test do
     assert_match "v#{version}", shell_output("#{bin}/driftctl version")
-    assert_match "Could not find a way to authenticate on AWS!",
+    assert_match "Downloading terraform provider: aws",
       shell_output("#{bin}/driftctl --no-version-check scan 2>&1", 2)
   end
 end
