2a966901d5ee2de23be22c7c7b810db558b11512
Anselm Hahn <Anselm.Hahn@gmail.com>
2022-06-20 09:41:05 +0200

    black: add repo-head to formula (#104028)
    Add head to formula

--- Formula/black.rb
+++ Formula/black.rb
@@ -6,6 +6,7 @@ class Black < Formula
   url "https://files.pythonhosted.org/packages/ee/1f/b29c7371958ab41a800f8718f5d285bf4333b8d0b5a5a8650234463ee644/black-22.3.0.tar.gz"
   sha256 "35020b8886c022ced9282b51b5a875b6d1ab0c387b31a065b84db7c33085ca79"
   license "MIT"
+  head "https://github.com/psf/black", branch: "main"
 
   livecheck do
     url :stable
