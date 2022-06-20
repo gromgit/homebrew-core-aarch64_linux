42718c8f4a3578aec0834f4aac0014dc5083d6bf
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:25 +0800

    ethereum: no rebuilding required

--- Formula/ethereum.rb
+++ Formula/ethereum.rb
@@ -1,8 +1,8 @@
 class Ethereum < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://geth.ethereum.org/"
-  url "https://github.com/ethereum/go-ethereum/archive/v1.10.19.tar.gz"
-  sha256 "4aefb4a26d2146c315955f714ed0d14bb0358c95ff6bad99845678263555c7ee"
+  url "https://github.com/ethereum/go-ethereum/archive/v1.10.17.tar.gz"
+  sha256 "00db123e1b23d3d904bd8a6bd4de8f3330d14db1622e8f22bf1653c94f03e09c"
   license "LGPL-3.0-or-later"
   head "https://github.com/ethereum/go-ethereum.git", branch: "master"
 
@@ -12,12 +12,8 @@ class Ethereum < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "92220d0cd9e7e6c4c0bcca09db7248c4fc8e9e0a1ea6490c9c37e3079ea72955"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a86716cc5589bbb46f344a59ab716b2b1f25b210739c6fb25061ceb2563c4b29"
-    sha256 cellar: :any_skip_relocation, monterey:       "d3847fda48c657b7a885285e0eff8c6cd57e74e200e9df7bb5c1f2b352745d56"
-    sha256 cellar: :any_skip_relocation, big_sur:        "22e2fc3af63eca723cd24883348b1e84ecb6f2ca74d9a89b9bde387d085e7a7f"
-    sha256 cellar: :any_skip_relocation, catalina:       "2554df34c329d8b87e9fd50c96086667b00fb2a10081729d0c429d4b567a031b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15f044a6c0646a350f20cc7d2700b37030f0864118457bd81cb8a2d1d6baa55e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ethereum"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "49ffa004a241e98bb5e918e4754fa93b93ede4a82c99745c50f32b8c2e23afb6"
   end
 
   depends_on "go" => :build
