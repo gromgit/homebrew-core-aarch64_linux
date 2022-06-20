26166baafd1140d7f7aa4c76e1f49fadc2fab8f6
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:56 +0800

    liquibase: no rebuilding required

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,8 +1,8 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "https://www.liquibase.org/"
-  url "https://github.com/liquibase/liquibase/releases/download/v4.12.0/liquibase-4.12.0.tar.gz"
-  sha256 "7127d3303df5318821d418856bdd751e32baafa302ac3fa2083ea54e32ae16dc"
+  url "https://github.com/liquibase/liquibase/releases/download/v4.10.0/liquibase-4.10.0.tar.gz"
+  sha256 "ef54411974918a43b67b0a3d14c8eec4700e9c16df9dd8498dd709af31089e15"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class Liquibase < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "94920883feb99d1b9a18ad919884b12dad0c9b655827f93f0f5b599caa9305c0"
+    sha256 cellar: :any_skip_relocation, all: "3db0f7af4ada1edcab528b492544df08c126c952288055ac7eaba2d74fb8b64e"
   end
 
   depends_on "openjdk"
