5b8d642bb223f8e4253e6683105ae4db2f9f94c0
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:29:06 +0800

    newrelic-cli: no rebuilding required

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,18 +1,14 @@
 class NewrelicCli < Formula
   desc "Command-line interface for New Relic"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.50.7.tar.gz"
-  sha256 "f0af862133d598320db6c1f85601b988817a0baa3735828452e4813461d95de3"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.47.1.tar.gz"
+  sha256 "f128e0f8c994393cb2de754d1cefd870b3c5292c45b225778531e2ecae243093"
   license "Apache-2.0"
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "777268597597dbdcb55f2d43d232fb08385ed8dc56739a777091ce80841dcf71"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9433a171336da1216bca1804c906d5d90c8747a657f38d221e23d540a9f090d0"
-    sha256 cellar: :any_skip_relocation, monterey:       "d1b3ced6ec7e28ce7c9ed473591d9bb7ac89b4d17c585de18660c46c446c980a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7fbcaeb41e4575dc34f23c02e704376ae70d37f7117de5101f6cc3b6dccd5618"
-    sha256 cellar: :any_skip_relocation, catalina:       "926a3ff894194a1c446d888c0dc03f693bcef1a1ff7262b7ba02e99d41fcbd19"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0c14ca42a2a3c29f2da238ba0c6a53c747a4b71d8c0128d666458752161951b2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/newrelic-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "019398482eddcf1a52993f5bc1ee42bebef793c143b778dbcad293909a111d62"
   end
 
   depends_on "go" => :build
