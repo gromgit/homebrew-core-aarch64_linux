ee9add4f14d4380edb048206fa7f7cef79841c14
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:29:49 +0800

    tbox: no rebuilding required

--- Formula/tbox.rb
+++ Formula/tbox.rb
@@ -1,18 +1,14 @@
 class Tbox < Formula
   desc "Glib-like multi-platform C library"
   homepage "https://tboox.org/"
-  url "https://github.com/tboox/tbox/archive/v1.6.9.tar.gz"
-  sha256 "31db6cc51af7db76ad5b5da88356982b1e0f1e624c466c749646dd203b68adae"
+  url "https://github.com/tboox/tbox/archive/v1.6.8.tar.gz"
+  sha256 "f437a31caa769a980e2e38ecc5bf37f1e572325d5d60fd242b9d6d49174b66fd"
   license "Apache-2.0"
   head "https://github.com/tboox/tbox.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ad121f69e2c9c818fcce62bfb477017ed1d1d47352f257842ff385368ad66c7a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f0c03016b30bd27603a1e6b10eeff077fc1437d80ca1f6f828715a129a183fb"
-    sha256 cellar: :any_skip_relocation, monterey:       "896de58b8fe02cc9ac808d6a3e569dd4b5a5649c6a43b6f9164b4c86617b032c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ed28f33c4dd5f7e15596a641de2f3175a8af11d529fd31e146d01bb4a733696e"
-    sha256 cellar: :any_skip_relocation, catalina:       "2050132144784c9dcb2080499d1961ccba6d0555ac3b823bc9dd4348fc80991e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd483cc00fc3484da59c334413386fb7e29ef6db05f6f98b5f152c458eaa29d7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tbox"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a04ad63adca72f032f49e105dadc87ae220b60e9b2bfd31f33f4d838d22ec3c6"
   end
 
   depends_on "xmake" => :build
