2d4e5b3904b894d83a2b179ad2b4fd61ec0467ed
Adrian Ho <aho+rpi9@03s.net>
2022-06-20 12:28:51 +0800

    kumactl: no rebuilding required

--- Formula/kumactl.rb
+++ Formula/kumactl.rb
@@ -1,8 +1,8 @@
 class Kumactl < Formula
   desc "Kuma control plane command-line utility"
   homepage "https://kuma.io/"
-  url "https://github.com/kumahq/kuma/archive/1.7.0.tar.gz"
-  sha256 "6c62b9f8d34c53cd3eb92914e5828ed9a4036251c8d812c47fcdbdcf56fc4aa8"
+  url "https://github.com/kumahq/kuma/archive/1.6.0.tar.gz"
+  sha256 "5dfd6c561a0b3e190868df00f4ec4aa93a9ba159cabe982b98b20c5c0c5df6aa"
   license "Apache-2.0"
 
   livecheck do
@@ -11,12 +11,8 @@ class Kumactl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c1737f54d79bb9b13f129d467e629e8dbd3d48767f08695df57a3862b3c1a225"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a46df0ed8dcb4bc791245bd6d98a42c6d3baac74990e7d45d26eddf4ff012d11"
-    sha256 cellar: :any_skip_relocation, monterey:       "0cbd339e275645f07407567f23d1425921bb1db9f397ae3fa4bf39f8ffe549bf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6bf0f70c291bde3d0f62acbfd4e230ca8307daaa1242fb4eecfd268005b35982"
-    sha256 cellar: :any_skip_relocation, catalina:       "9495203eba813725b643798cd04adb7f78282f82d1fc59b5b8e8d70faa307239"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1aaa59c7e5b3592d2c15c47664c2ba87d2c0e3f61e53666835fa47fdb7a758bf"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/kumactl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "25d1c6e00fd79d693518736b799ba1f73748b98af1036a4a1186d820d750426f"
   end
 
   depends_on "go" => :build
