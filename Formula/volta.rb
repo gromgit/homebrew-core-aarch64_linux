01482869b6d6a7af33184dcda3bede91ee8aa9a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-20 08:05:27 +0000

    volta: update 1.0.8 bottle.

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -12,12 +12,13 @@ class Volta < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "93ef1b6dc3561e5935cbf7645effa74022e3e51df7523f3d5478835ac2a960c2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4735a331c4535af9ca6b957faf09d668094269eb01908004182769ef860fe194"
-    sha256 cellar: :any_skip_relocation, monterey:       "7908212da7ba34b9d006ded2bca7798a9d87eafcc2bd7a7d7ec651a91ba8d990"
-    sha256 cellar: :any_skip_relocation, big_sur:        "12b612dea76ca6d73c76c90a1abd434ff80ebc8ffdedd9ee5982b8a0f4fbeb24"
-    sha256 cellar: :any_skip_relocation, catalina:       "05e8f989a06a953bed701d3152933f9c1055ad10cc9a70f2412c1af8a2ce4bd5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "695e420e300d607a77cf956b80cd45bc924ad2ebfe839487601838006f667705"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b3dcfd6c65b371576ca8160e66ce951a025ae361f8fcfd8fa0bfa144421945cc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d4275cd296aab5456e9622d5059bf2da5f13d2a76cb38790542feb0b8b08d09d"
+    sha256 cellar: :any_skip_relocation, monterey:       "f37c5126cdf7772300510b7f9024a12ce732ccc72a9de0473c1529fec2addbbc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "97046fcb096d1855088dd079a83ff369b8e687f58e217170d6626d691cbcb7bb"
+    sha256 cellar: :any_skip_relocation, catalina:       "66d958ce29dec5420fb8b9694c385a7e4db82feb19691564f1ffa2eb1ecff8e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae5aa47390ca45161d6416fd3df3c84662d6058c2bea8cc999a52ec715588f67"
   end
 
   depends_on "rust" => :build
