722d3e17f96dc4ac5eec629dd69f70f8c2593c93
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-06-21 17:52:38 +0000

    glade: update 3.38.2 bottle.

--- Formula/glade.rb
+++ Formula/glade.rb
@@ -6,11 +6,13 @@ class Glade < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "2a55e22c571d0d158c1b66bf18c35c44aeaed0694d139f13c48f5a6642b4785b"
-    sha256 big_sur:       "0fb77b21e6176c6690410a76d843f4582c1ef833e54ce5efa620bfce514e7af7"
-    sha256 catalina:      "e5c239c3d05350ff8a8710ce6beecf7fd22461336e77d55febb338b6a1456a61"
-    sha256 mojave:        "0b641d56f385a798fafe8fe424191de83207dea5b0edcf9d06c8b8b03ad0c68f"
-    sha256 x86_64_linux:  "1ab1f368edebbb88fd763659089ffa8f987f2416583f3f59fb732300088b4f8e"
+    sha256 arm64_monterey: "2fa733feec86b8379ad38126164c4f4e14500cd72c8564afe505110f503c66f3"
+    sha256 arm64_big_sur:  "2a55e22c571d0d158c1b66bf18c35c44aeaed0694d139f13c48f5a6642b4785b"
+    sha256 monterey:       "a232d76fa175b219228c15125b8e1b7ba47a5e6788787dd55fdd4773186bae1e"
+    sha256 big_sur:        "0fb77b21e6176c6690410a76d843f4582c1ef833e54ce5efa620bfce514e7af7"
+    sha256 catalina:       "e5c239c3d05350ff8a8710ce6beecf7fd22461336e77d55febb338b6a1456a61"
+    sha256 mojave:         "0b641d56f385a798fafe8fe424191de83207dea5b0edcf9d06c8b8b03ad0c68f"
+    sha256 x86_64_linux:   "1ab1f368edebbb88fd763659089ffa8f987f2416583f3f59fb732300088b4f8e"
   end
 
   depends_on "docbook-xsl" => :build
