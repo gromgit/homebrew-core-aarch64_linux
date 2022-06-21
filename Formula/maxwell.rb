dbe3bd0dbc8c79f2847d7dd3f655dae544e54fd4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-21 05:24:11 +0000

    maxwell 1.37.7
    Closes #104112.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/maxwell.rb
+++ Formula/maxwell.rb
@@ -1,8 +1,8 @@
 class Maxwell < Formula
   desc "Reads MySQL binlogs and writes row updates as JSON to Kafka"
   homepage "https://maxwells-daemon.io/"
-  url "https://github.com/zendesk/maxwell/releases/download/v1.37.6/maxwell-1.37.6.tar.gz"
-  sha256 "73e61df7e5fbabc3fb3efdb1762fdb15dc4ef23ed574872a2a0bd9faa1d761af"
+  url "https://github.com/zendesk/maxwell/releases/download/v1.37.7/maxwell-1.37.7.tar.gz"
+  sha256 "13a491c61f2a96a6eeffefae652cde0fbcdc2f010f845e75f8e6e90bbae1e845"
   license "Apache-2.0"
 
   livecheck do
