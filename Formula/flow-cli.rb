92bcf80efb2943b9b9ec4a6576081127a01c0fc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-22 19:04:57 +0000

    flow-cli: update 0.37.0 bottle.

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -12,12 +12,12 @@ class FlowCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "036d8b2e3e2c11dbdb2491edc731812a69d9a7761cca74c737909a938fe1cb6b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6698fc06dfc3d082d97fdc5eba6a1588fa25ab5aaf32cdd3d59e5ec53946c05f"
-    sha256 cellar: :any_skip_relocation, monterey:       "8f665f32c6420905bd24cb1a060677259cc7b07242532270a3c901b1419404cd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "31eb6aa7b0bfb6b055073db17e2b19161c9eaa27d6604ec36cd8b5b0258cbf11"
-    sha256 cellar: :any_skip_relocation, catalina:       "53641e8d83caa04c003b9eaebffcc3fd1c3105104ecbd7b649204a4a8a9a8ca7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "839c9fb33343cdc4c68f2f474157ec54ebb31d84051100ce32b8f9701cdcc398"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "25ff664bdf88598a3ad251d39dcb9eeb4b6dae74ce86b8bb33ec2bc122d43922"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e005128bf7b008d4d2f310d94db466cbddfd28c62209768c95523b1739ea5b16"
+    sha256 cellar: :any_skip_relocation, monterey:       "8ca06e35c836fd49332d5928b7da2230783929ae89f71d4604098190e0479776"
+    sha256 cellar: :any_skip_relocation, big_sur:        "09dac2cb22df8f38467b83410916c6d6cf3c697afdf7652277c51f541ebae3fb"
+    sha256 cellar: :any_skip_relocation, catalina:       "acb4a5eba4833b20c385b1ee831f9018a1f39f1345116fa7fb59d64d6ac4691d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "668564c97b8b310b78b409a24ba364a0913e307afd0581a6454819f4adaf9c75"
   end
 
   depends_on "go" => :build
