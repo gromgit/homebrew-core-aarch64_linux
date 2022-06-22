45b2f7b931709d2f36b1a6ec7191f52cccb2a5db
Rui Chen <rui@chenrui.dev>
2022-06-22 18:29:11 +0000

    tuc: add 0.11.0 bottle.

--- Formula/tuc.rb
+++ Formula/tuc.rb
@@ -6,6 +6,15 @@ class Tuc < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/riquito/tuc.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c35d0097152baf416b76d4a86777d0ab9b48f9da0046c1a800874b3618adc03d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "537ca7a98e77de6b5f4cfc1958fad85057ccda999304de0671d789a2ca169aa3"
+    sha256 cellar: :any_skip_relocation, monterey:       "56e293f4f47c68952f0e8c2fa7e627cfc32b38f1ea86f04f2422644aad972326"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6fa9c63920893eda18123703506c11fe389c5223bb87d4ce86f921e8da30acd8"
+    sha256 cellar: :any_skip_relocation, catalina:       "c0bfaa86f7ff202b485e5eda4e391af965a18780bfd2876c80fa9426e3eab9c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26cf29e247aa014c052ecbd9e9a40a210a455c32ee97ba6ec6326da29c4be3c8"
+  end
+
   depends_on "rust" => :build
 
   def install
