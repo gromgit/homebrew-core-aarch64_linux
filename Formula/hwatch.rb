0f7d683b6a963865a0a8b4a438582d3cd06315ac
Rui Chen <rui@chenrui.dev>
2022-06-22 18:30:49 +0000

    hwatch: add 0.3.5 bottle.

--- Formula/hwatch.rb
+++ Formula/hwatch.rb
@@ -6,6 +6,15 @@ class Hwatch < Formula
   license "MIT"
   head "https://github.com/blacknon/hwatch.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "66701800e379439109e0c092aac068cbd21a176f0e3aa5792f7795fe3d71ed59"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "69a3237769b7ed644605bb2784f2d07f90078432ced2b6bb2ea651124995e725"
+    sha256 cellar: :any_skip_relocation, monterey:       "9f21d461b648d4862d3b686b708c922d8891dc51fed2b56f56093b9c536ed985"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9e3ed9522d16320179fef3e28fefb7736a1acbe816bd0d078bf15717015ad537"
+    sha256 cellar: :any_skip_relocation, catalina:       "97d100b1262644c177382c3fd8651197eb200dd1c9db53300287676183540788"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a47586d29869a09e2012921e2adca9e7c3ded7bb4be3b8b2bf911ae95043fa4d"
+  end
+
   depends_on "rust" => :build
 
   def install
