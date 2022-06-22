e7baa39593fcfa5caba213945ce7c985193b0cae
Michael Cho <cho-m@tuta.io>
2022-06-22 00:02:22 -0700

    kore: remove openssl cellar references
    Closes #104171.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kore.rb
+++ Formula/kore.rb
@@ -4,7 +4,7 @@ class Kore < Formula
   url "https://kore.io/releases/kore-4.2.2.tar.gz"
   sha256 "77c12d80bb76fe774b16996e6bac6d4ad950070d0816c3409dc0397dfc62725f"
   license "ISC"
-  revision 1
+  revision 2
   head "https://github.com/jorisvink/kore.git", branch: "master"
 
   livecheck do
@@ -28,6 +28,10 @@ class Kore < Formula
   def install
     ENV.deparallelize { system "make", "PREFIX=#{prefix}", "TASKS=1" }
     system "make", "install", "PREFIX=#{prefix}"
+
+    # Remove openssl cellar references, which breaks kore on openssl updates
+    openssl = Formula["openssl@1.1"]
+    inreplace [pkgshare/"features", pkgshare/"linker"], openssl.prefix.realpath, openssl.opt_prefix if OS.mac?
   end
 
   test do
