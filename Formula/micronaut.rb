6a60006d6ba23b07dd592597bad56039d3fd21c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-22 05:28:03 +0000

    micronaut 3.5.2
    Closes #104164.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,8 +1,8 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-starter/archive/v3.5.1.tar.gz"
-  sha256 "7ba41abb768021f108d2b244a85fc419ea84e11098a0492971d0ac144eee4cd9"
+  url "https://github.com/micronaut-projects/micronaut-starter/archive/v3.5.2.tar.gz"
+  sha256 "e424a51b84cbdaf9f41238204a4604beffc8ec97a02b87bc121aa4c846596812"
   license "Apache-2.0"
 
   livecheck do
