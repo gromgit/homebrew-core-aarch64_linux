4d12074a9dc32949172ac3c39374ccd126738b2a
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-06-22 23:05:05 +0100

    swiftformat 0.49.11
    Closes #104211.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,8 +1,8 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.10.tar.gz"
-  sha256 "fef4bd56a65fd082343a629b304ed8e3497b4ff7a5c2875d8fec26326b3d7cbb"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.11.tar.gz"
+  sha256 "418b0b44a85005787b1bf6ce89a0660cdc2bed22768e948f13ac268beaa910df"
   license "MIT"
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
 
@@ -12,19 +12,16 @@ class Swiftformat < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "2fa46cd612b35fb97d678db2da6ba426f986b8d2549a02c55ed2beb0106679dc"
     sha256 cellar: :any_skip_relocation, big_sur:        "705d9f77262a53336e6367083ea1554683bc402d781259ed23cb07177afe37f6"
     sha256 cellar: :any_skip_relocation, catalina:       "216167d1fe1034933900d93056f97b169987ad1e0a0b9238a2889dccc7f79141"
+    sha256                               x86_64_linux:   "f00160b4eb1d370d6baf4341341f813af80ba2fcfa2e65657f96153713d1c774"
   end
 
   depends_on xcode: ["10.1", :build]
-  depends_on :macos
+
+  uses_from_macos "swift"
 
   def install
-    xcodebuild "-arch", Hardware::CPU.arch,
-        "-project", "SwiftFormat.xcodeproj",
-        "-scheme", "SwiftFormat (Command Line Tool)",
-        "-configuration", "Release",
-        "CODE_SIGN_IDENTITY=",
-        "SYMROOT=build", "OBJROOT=build"
-    bin.install "build/Release/swiftformat"
+    system "swift", "build", "--disable-sandbox", "--configuration", "release"
+    bin.install ".build/release/swiftformat"
   end
 
   test do
