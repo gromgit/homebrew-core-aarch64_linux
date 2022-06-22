32e6ee2aad86a2c7484414886a883c97195a693a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-22 20:04:50 +0000

    jaq: update 0.7.0 bottle.

--- Formula/jaq.rb
+++ Formula/jaq.rb
@@ -7,12 +7,12 @@ class Jaq < Formula
   head "https://github.com/01mf02/jaq.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dbede05c6da0f769eb033adff55af1d561def4928c62fa11f3c9845ef0e1ff42"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e9d0eb9590fe2e290752e9124961bb84ea933b9d323ca4e877f68b52ba32d62"
-    sha256 cellar: :any_skip_relocation, monterey:       "d93b38e5ca16cddb190b53be481f1125419c97d174f0379895762c1dfc71c3c2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "093579da0f00561e64ae79e48a353c3cc9c38992856a53eac53ee0a5d881d493"
-    sha256 cellar: :any_skip_relocation, catalina:       "674dc1acab4faef28bbb727dd4ac9f08ff2f4bf92340dbff882aad94960e1e81"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e169ace9d2c22725bf5c80a33f4e6c081df5539a9e4fc384700e3e0c568dec6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "23baf66aa217c44fa52dce9b7e9e57db48e824aecd8f71ff8317095616bef3d7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e9654c1dd291ba19a7f968270bad5c33f63274c9b7ce700971e0fa4a8418004a"
+    sha256 cellar: :any_skip_relocation, monterey:       "ee23b2f2b1885a81f48e9b6529fe9cdb030cd4fc0562bc6f54ebebececb6a87c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5bab2b3e9428e7a579d96028eb26f107581c7e4511cba80500f634ea998de2c9"
+    sha256 cellar: :any_skip_relocation, catalina:       "108c7bc622cddba7784d6cc96adc6e026a42914f110eaaba50784b238c247af7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0490ba511363f3dc1ee1f68650b3efb28c157d71f39bcb60b550e9b4a4b7aff9"
   end
 
   depends_on "rust" => :build
