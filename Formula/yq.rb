95e539d9565f16152e3369cad5e4ca93ce34592a
Porkepix <porkepix@gmail.com>
2022-06-23 12:59:43 +0200

    yq 4.25.3
    Closes #104279.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -1,8 +1,8 @@
 class Yq < Formula
   desc "Process YAML documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/v4.25.2.tar.gz"
-  sha256 "2aa2d3e4e44a74bc8a2213f60620f69366a86bbc9f5deffcc15047eaa4cf9e19"
+  url "https://github.com/mikefarah/yq/archive/v4.25.3.tar.gz"
+  sha256 "30309ae4efbe8b4f0a26e3c878bac72288faa0ba54f544c7fecdb3e0373966eb"
   license "MIT"
   head "https://github.com/mikefarah/yq.git", branch: "master"
 
