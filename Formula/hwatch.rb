25a9e2280445fc4204defb7449541b6ee939f2f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-23 10:04:44 +0000

    hwatch: update 0.3.6 bottle.

--- Formula/hwatch.rb
+++ Formula/hwatch.rb
@@ -7,12 +7,12 @@ class Hwatch < Formula
   head "https://github.com/blacknon/hwatch.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "66701800e379439109e0c092aac068cbd21a176f0e3aa5792f7795fe3d71ed59"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "69a3237769b7ed644605bb2784f2d07f90078432ced2b6bb2ea651124995e725"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f21d461b648d4862d3b686b708c922d8891dc51fed2b56f56093b9c536ed985"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9e3ed9522d16320179fef3e28fefb7736a1acbe816bd0d078bf15717015ad537"
-    sha256 cellar: :any_skip_relocation, catalina:       "97d100b1262644c177382c3fd8651197eb200dd1c9db53300287676183540788"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a47586d29869a09e2012921e2adca9e7c3ded7bb4be3b8b2bf911ae95043fa4d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "207e32f2bbcb0178bf81c7901047fbd8a9ee5c348101d0a2680e61ed83838309"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "41bde24420c67c60ab75f465401275b0d8424d20c191f02023f44c9d8e444b9f"
+    sha256 cellar: :any_skip_relocation, monterey:       "7c2dbd52999f807c0a16e4714a9b0dd68f0d199c578cbd320a828593c1d7b2ad"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd278d2294ef73c5555e3ead67cb3006a3817e3791d41b637df58269ebb5f539"
+    sha256 cellar: :any_skip_relocation, catalina:       "8c183292c7f3297fc53998ba9821a439d2a7834a2cba33bae6a198af242654d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e21942f63a34751f20e641d4c7c44f5f0e662fe2494ac0afb0b58f480ffd2dc3"
   end
 
   depends_on "rust" => :build
