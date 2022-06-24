2dfe4b0108ce7c8a4b3ecce7f304d1a8e92455f0
Porkepix <porkepix@gmail.com>
2022-06-24 11:45:10 +0200

    dtm 1.14.4
    Closes #104333.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dtm.rb
+++ Formula/dtm.rb
@@ -1,8 +1,8 @@
 class Dtm < Formula
   desc "Cross-language distributed transaction manager"
   homepage "https://en.dtm.pub/"
-  url "https://github.com/dtm-labs/dtm/archive/refs/tags/v1.14.3.tar.gz"
-  sha256 "c2fd7c1ded3839e713277fd3c2141bd1e74b484e6ab970c895798a89416578dd"
+  url "https://github.com/dtm-labs/dtm/archive/refs/tags/v1.14.4.tar.gz"
+  sha256 "94c53132d6b3679c643178f9db66922f6441e81e8a03094ff95be6bc8bce3cac"
   license "BSD-3-Clause"
 
   bottle do
