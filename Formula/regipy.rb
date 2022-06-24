d1db95e37eecbfa520bb1603aa95ea716f7a959f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-24 23:04:50 +0000

    regipy: update 2.5.3_1 bottle.

--- Formula/regipy.rb
+++ Formula/regipy.rb
@@ -10,12 +10,12 @@ class Regipy < Formula
   head "https://github.com/mkorman90/regipy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "12e64447c14e5ec71ca4e46dcd45ebe2402768778fa9a3dd55d61706e14055f1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bdea63bb5919174e7c76e0d21a31313c357bb17751afb15df406fc187f91d3e7"
-    sha256 cellar: :any_skip_relocation, monterey:       "926d95ad52d4509626611aba9248fcebbf6e4fa575a87c22f70ccf1d56496103"
-    sha256 cellar: :any_skip_relocation, big_sur:        "58a94eeab978e129269fa3e062451ecefa1a5f259ab266d319af20d2cd5d1d62"
-    sha256 cellar: :any_skip_relocation, catalina:       "89faf5f8c9dee3ab9daca294f543a505fe2f3c5bde9ccc38556b161b4a348f23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba2e9703f2e5d139971903eb5ad9eb9d2164324b93af0571881954c6eb62da61"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea6ec8f86d572f9862bd99d51b09a615fbf214774650018fb4ef3cce29d24e21"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee1a19314aa9b566e6cc032e1307f417ddcef87518b780820242195226fa2d56"
+    sha256 cellar: :any_skip_relocation, monterey:       "3ea29584e37aa8c4302714c5ea908bbdd8c47bca32943c90787e3b36ac6680e1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ee8224a8c3ca5b9c0a49d90c2e911baac9ed8b2a1f749b209984d196676a6fbc"
+    sha256 cellar: :any_skip_relocation, catalina:       "eef67fe052fd505ca123478c47eb7416e25a6130516ae4a8e3d0a78ae4f4b10d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cea5dad8fdd301244b2c9ba9bca820b54529f2e7e57cd228ff26a33ac74fc522"
   end
 
   depends_on "libpython-tabulate"
