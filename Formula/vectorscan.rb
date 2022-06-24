abecc0c04d18add4bfb9b016be00ac53acb97c1b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-06-24 15:13:42 +0000

    vectorscan: add 5.4.7 bottle.

--- Formula/vectorscan.rb
+++ Formula/vectorscan.rb
@@ -5,6 +5,11 @@ class Vectorscan < Formula
   sha256 "cd70c2a7bf632b5374083a450019703605520c10c5614a4c12011c99ab8435dd"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any, arm64_monterey: "abe4b344d8deccfef0b799220de4f167f9f4bf7036cab75a5046c709fffc36a1"
+    sha256 cellar: :any, arm64_big_sur:  "dd42e4b9620e17d37f250beb78be6cdc8547b78a38d02bcf838fd5f986f8fb04"
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "pcre" => :build
