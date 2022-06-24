9194a1d95fce0276a9b81b9afb42cf5343751a76
Vihang Mehta <vihang@pixielabs.ai>
2022-06-23 23:12:24 -0700

    pixie 0.7.15 (new formula)
    This adds a homebrew based installation for the Pixie CLI.
    
    Pixie is an open-source observability tool for Kubernetes applications,
    and the CLI is used to deploy Pixie, manage it, and interact with the
    platform (for e.g. run queries).
    
    Pixie already provides release binaries and install scripts but some folks
    prefer to manage all their tools using homebrew so this gives folks that option.
    
    Fixes https://github.com/pixie-io/pixie/issues/401
    
    Closes #104328.
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/pixie.rb
@@ -0,0 +1,31 @@
+class Pixie < Formula
+  desc "Observability tool for Kubernetes applications"
+  homepage "https://px.dev/"
+  url "https://github.com/pixie-io/pixie.git",
+      tag:      "release/cli/v0.7.15",
+      revision: "3f9bc4b39151eae5227cf4ad61088ce4b321da04"
+  license "Apache-2.0"
+  head "https://github.com/pixie-io/pixie.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    semver = build.head? ? "0.0.0-dev" : version
+    ldflags = %W[
+      -s -w
+      -X px.dev/pixie/src/shared/goversion.buildSCMRevision=#{Utils.git_short_head}
+      -X px.dev/pixie/src/shared/goversion.buildSCMStatus=Distribution
+      -X px.dev/pixie/src/shared/goversion.buildSemver=#{semver}
+      -X px.dev/pixie/src/shared/goversion.buildTimeStamp=#{time.to_i}
+      -X px.dev/pixie/src/shared/goversion.buildNumber=#{revision + bottle&.rebuild.to_i + 1}
+      -X px.dev/pixie/src/shared/goversion.builtBy=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"px"), "./src/pixie_cli"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/px version")
+    assert_match tap.user.to_s, shell_output("#{bin}/px version")
+    assert_match "You must be logged in to perform this operation.", shell_output("#{bin}/px deploy 2>&1", 1)
+  end
+end
