d1b38008b74898baca53d4341b48ce3a6274062c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-25 05:24:51 +0000

    skaffold 1.39.1
    Closes #104404.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -2,8 +2,8 @@ class Skaffold < Formula
   desc "Easy and Repeatable Kubernetes Development"
   homepage "https://skaffold.dev/"
   url "https://github.com/GoogleContainerTools/skaffold.git",
-      tag:      "v1.39.0",
-      revision: "73c46de08489140e8c034407a136c7491dd073d8"
+      tag:      "v1.39.1",
+      revision: "cd3f6fa3231ae8abf7f028eb7163d74aafd6ae94"
   license "Apache-2.0"
   head "https://github.com/GoogleContainerTools/skaffold.git", branch: "main"
 
