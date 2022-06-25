04e3ef8f34d5325e8120fd7d2af61b09087dcfe5
owine <owine@users.noreply.github.com>
2022-06-25 14:29:15 -0500

    speex 1.2.1
    Closes #104427.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -1,9 +1,9 @@
 class Speex < Formula
   desc "Audio codec designed for speech"
   homepage "https://speex.org/"
-  url "https://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz", using: :homebrew_curl
-  mirror "https://ftp.osuosl.org/pub/xiph/releases/speex/speex-1.2.0.tar.gz"
-  sha256 "eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094"
+  url "https://downloads.xiph.org/releases/speex/speex-1.2.1.tar.gz", using: :homebrew_curl
+  mirror "https://ftp.osuosl.org/pub/xiph/releases/speex/speex-1.2.1.tar.gz"
+  sha256 "4b44d4f2b38a370a2d98a78329fefc56a0cf93d1c1be70029217baae6628feea"
   license "BSD-3-Clause"
 
   livecheck do
