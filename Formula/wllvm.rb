b66f82d5da26613b9b5f5b76fa0ef9e3ffe63bd6
Michka Popoff <michkapopoff@gmail.com>
2022-06-25 14:28:32 +0200

    wllvm: migrate to Python@3.10
    Closes #104288.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wllvm.rb
+++ Formula/wllvm.rb
@@ -6,6 +6,7 @@ class Wllvm < Formula
   url "https://files.pythonhosted.org/packages/4b/df/31d7519052bc21d0e9771e9a6540d6310bfb13bae7dacde060d8f647b8d3/wllvm-1.3.1.tar.gz"
   sha256 "3e057a575f05c9ecc8669a8c4046f2bfdf0c69533b87b4fbfcabe0df230cc331"
   license "MIT"
+  revision 1
 
   bottle do
     rebuild 1
@@ -19,7 +20,7 @@ class Wllvm < Formula
   end
 
   depends_on "llvm" => :test
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
