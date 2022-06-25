941742ce73742e9a1e8168819e595f2677ab27bd
David Zhao <dz@livekit.io>
2022-06-25 13:30:40 -0700

    livekit 1.1.0 (new formula)
    * livekit 1.1 (new formula)
      livekit: adding new package
    * fixed test
    * address feedback
    * feedback 2
    * add head
    * update port logic & description
    
    Closes #104437.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/livekit.rb
@@ -0,0 +1,26 @@
+class Livekit < Formula
+  desc "Scalable, high-performance WebRTC server"
+  homepage "https://livekit.io"
+  url "https://github.com/livekit/livekit/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "acc55775cca1648940706842ace7d453dd71d301689c4f368bf8467ee1460507"
+  license "Apache-2.0"
+  head "https://github.com/livekit/livekit.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(output: bin/"livekit-server"), "./cmd/server"
+  end
+
+  test do
+    http_port = free_port
+    fork do
+      exec bin/"livekit-server", "--keys", "test: key", "--config-body", "port: #{http_port}"
+    end
+    sleep 3
+    assert_match "OK", shell_output("curl localhost:#{http_port}")
+
+    output = shell_output("#{bin}/livekit-server --version")
+    assert_match "livekit-server version #{version}", output
+  end
+end
