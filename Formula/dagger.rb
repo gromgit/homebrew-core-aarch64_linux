e95d9224d91758995ee3a59f848bdaed4ea0c299
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-26 01:34:11 +0000

    dagger: update 0.2.20 bottle.

--- Formula/dagger.rb
+++ Formula/dagger.rb
@@ -8,12 +8,12 @@ class Dagger < Formula
   head "https://github.com/dagger/dagger.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bdf85b33a7f4a8970e893666e44b3050a18edbc1b91c2fc6f09c2ba10e557f13"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bdf85b33a7f4a8970e893666e44b3050a18edbc1b91c2fc6f09c2ba10e557f13"
-    sha256 cellar: :any_skip_relocation, monterey:       "1ddb326dfca7b8ceb20db5f9369cc2c7f25c1dfedc965342a04571f9deefe276"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1ddb326dfca7b8ceb20db5f9369cc2c7f25c1dfedc965342a04571f9deefe276"
-    sha256 cellar: :any_skip_relocation, catalina:       "1ddb326dfca7b8ceb20db5f9369cc2c7f25c1dfedc965342a04571f9deefe276"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8750ad15f21e19835f834487bd66d8dd1195ef60f8f31f5a58623bb19d39f4b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e67cfa7e60a6924294b8672c85bd3d87ca5a9e9732484538e10253f4a7545a04"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e67cfa7e60a6924294b8672c85bd3d87ca5a9e9732484538e10253f4a7545a04"
+    sha256 cellar: :any_skip_relocation, monterey:       "36a2b8980ce145e23cf78abeafaf3f8282740cf1d9c80f5d47800bd039404b01"
+    sha256 cellar: :any_skip_relocation, big_sur:        "36a2b8980ce145e23cf78abeafaf3f8282740cf1d9c80f5d47800bd039404b01"
+    sha256 cellar: :any_skip_relocation, catalina:       "36a2b8980ce145e23cf78abeafaf3f8282740cf1d9c80f5d47800bd039404b01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f70075ccb617e4c1ee0a27eb740cc28d9345c2af2740e37592474296ec145823"
   end
 
   depends_on "go" => :build
