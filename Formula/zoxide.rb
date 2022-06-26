c22c3834e400c78dc2d2f2ef6c2b0e9dfe7aa4cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-26 06:04:46 +0000

    zoxide: update 0.8.2 bottle.

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -6,13 +6,12 @@ class Zoxide < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "17b9866b8538f7c106137df3aedf78fe227893c5d7536a065b563b1bf7314cc6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "933ace32800c0d1980159c8220a3e9b097a505c3131ebfcc728a51e8a9d7b0c3"
-    sha256 cellar: :any_skip_relocation, monterey:       "df0711110ded8e845e73ed7d5290bd9559d366e6882068417afbf7d8ac013563"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a2d7264be887ddc03b8b1de9a3b3625e5b1ac63d400bffae41616b624b8e1fc0"
-    sha256 cellar: :any_skip_relocation, catalina:       "92894d5bf6376124fcc07992946e879849efdde1bc43f28bd45d87546518da46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "866927ab09b1a26d41659e2aeed76fbd8d719da32cc97137d72316d90175f7d1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d4cdc7e519771286cef66580c336d36a464d694a528793dce9c8e48016667f2d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cb48744aa267bc52bccc4b610bfd7ae1b6fa75006b5259ecfa66c7a74bc5249d"
+    sha256 cellar: :any_skip_relocation, monterey:       "f88ad659d706245b0b6e7c317b8c96e3566f013eb45f2a4a55d56e8e045126bf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "486dd1713034921782f6c66c6a6e1aefea95a62cfaed21d7597868e376085c19"
+    sha256 cellar: :any_skip_relocation, catalina:       "71a8f9f3c9e7724ea2466d98f29d9c7241e8f296a3aa95d8911e81de54e76cb4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "17ed1ebc50f44d8c232c09d39e8a09611ac3b99b0cce38f7b7a6f4223135f4e8"
   end
 
   depends_on "rust" => :build
