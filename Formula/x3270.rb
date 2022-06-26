6e6eb01d2dbb12abaad2690ce95e3f6e3175c36c
chimurai <655241+chimurai@users.noreply.github.com>
2022-06-26 19:14:59 +0000

    x3270: revision
    x3270 4.2ga4 patch
    
    Closes #104495.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -4,6 +4,7 @@ class X3270 < Formula
   url "http://x3270.bgp.nu/download/04.02/suite3270-4.2ga4-src.tgz"
   sha256 "6a89ebdc7817bc204f79ebcdc6b5e7fef8c19aedfbc80284fedc647caa086790"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url "https://x3270.miraheze.org/wiki/Downloads"
@@ -22,6 +23,13 @@ class X3270 < Formula
   depends_on "readline"
 
   uses_from_macos "tcl-tk"
+
+  # Apply patch on 4.2ga4. Remove after next release
+  # https://github.com/pmattes/x3270/issues/36
+  patch do
+    url "https://github.com/pmattes/x3270/commit/80168fbc69544fee00517fb5b14bc662501929b8.patch?full_index=1"
+    sha256 "a1a00c9396e565879e3cb5ff230861c914ebdf82e44911dd2799c43e2e2ccfad"
+  end
 
   def install
     args = %W[
