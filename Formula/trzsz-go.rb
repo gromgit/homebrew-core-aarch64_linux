979a5eece6f696d65d649271df9acb60768f653a
Rui Chen <rui@chenrui.dev>
2022-06-26 02:54:48 +0000

    trzsz-go: add 0.1.7 bottle.

--- Formula/trzsz-go.rb
+++ Formula/trzsz-go.rb
@@ -5,6 +5,15 @@ class TrzszGo < Formula
   sha256 "93fb87141bb8306fbb9a465c4cfe5401dd0646bc1180caf1cd50b2503c93e454"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "365bb728f3b80b9453fd1c8901074d62f97630a0cd798fa1d10b9b58687c68c1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1fc5c6c8431df9aa3150fbcbb816cd9e404f0db63a8b65ed47e556ab727b1db4"
+    sha256 cellar: :any_skip_relocation, monterey:       "5f18645a783d21f6810833f3d652e1160b626a8e871603de7192b2bc3c785b94"
+    sha256 cellar: :any_skip_relocation, big_sur:        "464cdfea213d0dcaa6daa3625af0832213dba0fa5be9df1e9adfb35c761c7064"
+    sha256 cellar: :any_skip_relocation, catalina:       "a26d06979d89608e691b1943b96410dd315b3161cb827d893113e1eeeac3afd4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc784bdea84bca3eaf7ac6435d624c7950eb8dbab22b91d2cd4e0b49f91ea06b"
+  end
+
   depends_on "go" => :build
 
   def install
