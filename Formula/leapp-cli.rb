88188921a2aaeadfe309cee2a76fa4fbd6769e6b
Rui Chen <rui@chenrui.dev>
2022-06-27 14:40:15 +0000

    leapp-cli: add 0.1.13 bottle.

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -7,6 +7,15 @@ class LeappCli < Formula
   sha256 "944ce4928c2d2dd562890310ea6f2f96d3ace5f1c3b7e8bbc9ef8323403359d8"
   license "MPL-2.0"
 
+  bottle do
+    sha256                               arm64_monterey: "551419ca4c96713baf2705fab5e7d86991b74af86d3daa82393b9204bee74289"
+    sha256                               arm64_big_sur:  "9eadb95caad479ae848818f794b36e4a58cf2dab42c4add574c41befeff9a1be"
+    sha256                               monterey:       "1273b1157d3cbfcadbf5fb22aaf7cbcbb37c5364af1e181d538f5ee77a330b7a"
+    sha256                               big_sur:        "9461e3f6a60c8646a4c685082b08e878786205907e5d831f56d0ff82e15e49ed"
+    sha256                               catalina:       "d5f3e7d44b68355dc9a4e2b067b0b7e615de1144f6949612dd88ab1d49ef27b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9304cce19db719aa1a2032876310ab3d3354a6795e67a61c29db7349d7425b2b"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => :build
   depends_on "node"
