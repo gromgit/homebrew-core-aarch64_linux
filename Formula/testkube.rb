3a709471683371a4c9c724dc20837e4fe3c62274
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-27 09:04:39 +0000

    testkube: update 1.2.35 bottle.

--- Formula/testkube.rb
+++ Formula/testkube.rb
@@ -6,12 +6,12 @@ class Testkube < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5aa47f38599230b737e0a2b46c177f72e5f5300b4fd78495426abeafcba91b9f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5aa47f38599230b737e0a2b46c177f72e5f5300b4fd78495426abeafcba91b9f"
-    sha256 cellar: :any_skip_relocation, monterey:       "6e13bc153b8bfed3e0bdb0466cf0a2bc5936ebdb21cf233cd9aff76e0860b4cb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6e13bc153b8bfed3e0bdb0466cf0a2bc5936ebdb21cf233cd9aff76e0860b4cb"
-    sha256 cellar: :any_skip_relocation, catalina:       "6e13bc153b8bfed3e0bdb0466cf0a2bc5936ebdb21cf233cd9aff76e0860b4cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "01536b6b35fbba54b9b83b336a9d7230f63a6c7d9fdefc299c2bac8fb627e302"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9d94ee7fba3abb1bb256408eb46b5b68fc399ed66beb9388cd37dc040dd901b4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d94ee7fba3abb1bb256408eb46b5b68fc399ed66beb9388cd37dc040dd901b4"
+    sha256 cellar: :any_skip_relocation, monterey:       "8e461e5e941e9efeddcbc480cdfe7c2e7cd3823e7939e1e7a604d5b1aae48ac2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8e461e5e941e9efeddcbc480cdfe7c2e7cd3823e7939e1e7a604d5b1aae48ac2"
+    sha256 cellar: :any_skip_relocation, catalina:       "8e461e5e941e9efeddcbc480cdfe7c2e7cd3823e7939e1e7a604d5b1aae48ac2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "614eec3cd6f7a73e3bced687998f517fcca5d972c54eb7971ca34a68a47966d0"
   end
 
   depends_on "go" => :build
