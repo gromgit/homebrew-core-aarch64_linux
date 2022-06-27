99ef74a57563d1a4286ff622ff4a716f589aa815
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-27 18:05:51 +0000

    lefthook: update 1.0.4 bottle.

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -7,12 +7,12 @@ class Lefthook < Formula
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "81a0841dd6ebd3318596c9d73d8e675d29439101986592bfafb123a793774077"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ba38813a512f92169d67e37a59aff474cc667154d4019b3635d3bb0129a694f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "0c9e5b24620618a6eabde324097bd0344044a5b2ad651885d357ce70b05b5de1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a1adbd3210777d77a318de082e294d8118fffc3057a2472ac435a041a45e086c"
-    sha256 cellar: :any_skip_relocation, catalina:       "5ac5a1642048341273704288c9294061283a8357c35ff6349c6c2bf97ee65af4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d24b7a7de42c3ee5732815692d15b05f9e775da59a146fdb8ba48b877d26231a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "973faa1ea3c0a69abcaf8a15f095bcfd3bce9b057b6f4c3ee5e8d55dd0f981b0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f3ebc1cd8c8d296568ba15f3e8fd6978bac6e06dc4c971ffa206cf28dc6e9050"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b84b2c8b49ec898d677b6e71e8f8eee985f86aa4607591fc931e5a68244e4e6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8f956cfbfc7bc37a1dc6c48fb62bd90927a6ebc694f17b4e0983c6408c3cbe48"
+    sha256 cellar: :any_skip_relocation, catalina:       "fbd174620b3c8107bbb0c2afcfd5692875344f71368205e5f81b6dfb60d159c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2887a9f988a9ea35ff6f12245afb99f79e5a56f19629a921fc7a9676bba53d91"
   end
 
   # Bump to 1.18 on the next release, if possible.
