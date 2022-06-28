74370b0ed90ff109a60c855896df01923e774870
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-28 15:05:53 +0000

    jolie: update 1.10.7 bottle.

--- Formula/jolie.rb
+++ Formula/jolie.rb
@@ -6,7 +6,7 @@ class Jolie < Formula
   license "LGPL-2.1-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "50dcf57021a1915c8403e3596b6189dc435f3e8f286dd66d9efb3a5e2799cbe9"
+    sha256 cellar: :any_skip_relocation, all: "56f8f9b008a733c98119fd49c7e966510cc3c502496000d91d0a6d1aa9f10585"
   end
 
   depends_on "openjdk"
