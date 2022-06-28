d30b979200ecc20a391673eb80db189fa45af19b
Alexander Bayandin <alexander@bayandin.dev>
2022-06-28 17:35:16 +0100

    openssl@3: fix AVX-512-specific buffer overflow
    Closes #104642.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openssl@3.rb
+++ Formula/openssl@3.rb
@@ -5,6 +5,7 @@ class OpensslAT3 < Formula
   mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-3.0.4.tar.gz"
   sha256 "2831843e9a668a0ab478e7020ad63d2d65e51f72977472dc73efcefbafc0c00f"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://www.openssl.org/source/"
@@ -62,6 +63,15 @@ class OpensslAT3 < Formula
       args += (ENV.ldflags || "").split
     end
     args
+  end
+
+  # Fix AVX512-specific heap buffer overflow
+  # See https://github.com/openssl/openssl/pull/18626
+  #
+  # Remove in the next release
+  patch do
+    url "https://github.com/openssl/openssl/commit/71ad6a8da3e39bd4caf5c6c767287ddd9bce8bae.patch?full_index=1"
+    sha256 "ab26fe6240a1d3b9d7214fa3937fe36f22d69acf6a6819903e8ebf2884711f26"
   end
 
   def install
