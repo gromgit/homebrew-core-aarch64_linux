8d535545e85029106ef29df1097f8c2f51974652
Porkepix <porkepix@gmail.com>
2022-06-28 17:20:38 +0200

    chafa 1.12.2
    * chafa 1.12.2
    * remove build patch
    * chafa: use `gcc` on Linux
    
    Closes #104635.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chafa.rb
+++ Formula/chafa.rb
@@ -1,8 +1,8 @@
 class Chafa < Formula
   desc "Versatile and fast Unicode/ASCII/ANSI graphics renderer"
   homepage "https://hpjansson.org/chafa/"
-  url "https://hpjansson.org/chafa/releases/chafa-1.12.1.tar.xz"
-  sha256 "f08cbcd87f604ba20cf8699dc107349400f21b825be20491e8e0ada8995194ea"
+  url "https://hpjansson.org/chafa/releases/chafa-1.12.2.tar.xz"
+  sha256 "f41d44afb325a7fa0c095160723ddcc10fbd430a3ad674aa23e2426d713a96f5"
   license "LGPL-3.0-or-later"
 
   livecheck do
@@ -23,17 +23,14 @@ class Chafa < Formula
   depends_on "glib"
   depends_on "imagemagick"
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  on_linux do
+    depends_on "gcc"
   end
 
+  fails_with gcc: "5" # imagemagick is built with GCC
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
     man1.install "docs/chafa.1"
   end
