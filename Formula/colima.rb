ee4c52d49235355e161de357442c844b07cda64d
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-06-29 13:35:03 +0530

    colima 0.4.3
    Closes #104699.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/colima.rb
+++ Formula/colima.rb
@@ -2,8 +2,8 @@ class Colima < Formula
   desc "Container runtimes on MacOS (and Linux) with minimal setup"
   homepage "https://github.com/abiosoft/colima/blob/main/README.md"
   url "https://github.com/abiosoft/colima.git",
-      tag:      "v0.4.2",
-      revision: "f112f336d05926d62eb6134ee3d00f206560493b"
+      tag:      "v0.4.3",
+      revision: "3a85204fa783ea0d73c9f073f89da2393bc05e5e"
   license "MIT"
   head "https://github.com/abiosoft/colima.git", branch: "main"
 
