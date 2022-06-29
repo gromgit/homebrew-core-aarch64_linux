e592289a7b58163b598336732427addadd5fe261
Ben Moss <ben@mossity.com>
2022-06-29 16:44:11 -0400

    docker-buildx 0.8.2 (new formula)
    * docker-buildx: Add formula
    * Address feedback from branchvincent
    * Add build command test
    * Fix test on Linux
    
    Closes #104738.
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Co-authored-by: Ben Moss <moss.127@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/docker-buildx.rb
@@ -0,0 +1,37 @@
+class DockerBuildx < Formula
+  desc "Docker CLI plugin for extended build capabilities with BuildKit"
+  homepage "https://docs.docker.com/buildx/working-with-buildx/"
+  url "https://github.com/docker/buildx.git",
+      tag:      "v0.8.2",
+      revision: "6224def4dd2c3d347eee19db595348c50d7cb491"
+  license "Apache-2.0"
+  head "https://github.com/docker/buildx.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/docker/buildx/version.Version=v#{version}
+      -X github.com/docker/buildx/version.Revision=#{Utils.git_head}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/buildx"
+
+    doc.install Dir["docs/reference/*.md"]
+  end
+
+  def caveats
+    <<~EOS
+      docker-buildx is a Docker plugin. For Docker to find this plugin, symlink it:
+        mkdir -p ~/.docker/cli-plugins
+        ln -sfn #{opt_bin}/docker-buildx ~/.docker/cli-plugins/docker-buildx
+    EOS
+  end
+
+  test do
+    assert_match "github.com/docker/buildx v#{version}", shell_output("#{bin}/docker-buildx version")
+    output = shell_output(bin/"docker-buildx build . 2>&1", 1)
+    assert_match(/(denied while trying to|Cannot) connect to the Docker daemon/, output)
+  end
+end
