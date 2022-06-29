cfb2ec1b6cd5db78084af2dd61497f0de1080561
Stephen <stephengroat@users.noreply.github.com>
2022-06-28 22:19:00 -0700

    cdktf: rebuild
    * cdktf: install zsh completion
    * cdktf: bash and zsh completions
    * cdktf: add comment about completion configuration
    
    Closes #104682.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -22,6 +22,13 @@ class Cdktf < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # completion script currently requires --help run without error https://github.com/hashicorp/terraform-cdk/issues/1905
+    output = Utils.safe_popen_read({ "SHELL" => "bash" }, libexec/"bin/cdktf", "completion", "--help")
+    (bash_completion/"cdktf").write output
+
+    output = Utils.safe_popen_read({ "SHELL" => "zsh" }, libexec/"bin/cdktf", "completion", "--help")
+    (zsh_completion/"_cdktf").write output
   end
 
   test do
