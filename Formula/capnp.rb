45274f28c7632f40a81d41453e87236def72fc17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-30 12:05:58 +0000

    capnp: update 0.10.2 bottle.

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -12,12 +12,12 @@ class Capnp < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "cf4352d304acbc3efc850209f34cec32ec70472edbe139ac83caebbbc90d94ba"
-    sha256 arm64_big_sur:  "772932eac02c7c407d574d2cc8e1ead9f68225b0094137e2d63c3c1caf195fe4"
-    sha256 monterey:       "5d87dc87b14828f2afbac22a055b3cdb7b08d3c7bd4275b925df04e8c6a025c2"
-    sha256 big_sur:        "4be05582ce7938b7aab9082cb3f7a3c75fc6b1a527fc1d83fbd9627c54dbe422"
-    sha256 catalina:       "c8b13089224a39ce6b8632861fc52f333be2d634854decdf23e5907efe0fe77b"
-    sha256 x86_64_linux:   "484bf4c433b99c0ddb1cf7aae38844e4dc112469d266236c158b3de40e6d7c65"
+    sha256 arm64_monterey: "082b9e9538d5edd7757b0365d2661862cbb0c72b8a673d90595b9bab82ac8a55"
+    sha256 arm64_big_sur:  "5e323e78ea4dcb77095c408bb66edcae2709f087225d40d5c3d49be64249ed9a"
+    sha256 monterey:       "c481ae927ef7c4fc42ded6c265e65d41a676abe459923d2265f5ea1af2ce0be8"
+    sha256 big_sur:        "fe5ea65e9885cf41986d70ca90f1e2ff9f8ffdd631d97047003af60889e8c7f6"
+    sha256 catalina:       "8fbf305bd1ee071768784f6f6797dc6116d2eda34d96aa2278b9cb2675c1230a"
+    sha256 x86_64_linux:   "9c647ac6b614b8a20aeb785c16ec9977231c05c0cfc13d0425b3beb9b749d15f"
   end
 
   depends_on "cmake" => :build
