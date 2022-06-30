e8a5e3ff741b2f59774f88e4bad6169b09d13440
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-30 02:48:37 +0000

    dsq: update 0.21.0 bottle.

--- Formula/dsq.rb
+++ Formula/dsq.rb
@@ -7,12 +7,12 @@ class Dsq < Formula
   head "https://github.com/multiprocessio/dsq.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c51e4df3650aab8ad49f892af2b652e3920468e82928a9c3f2884208c7bcdf89"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fcacd2629ce072d71771cd7f8738b0758d76eeb60fcb1d49cc8b4f974a586f17"
-    sha256 cellar: :any_skip_relocation, monterey:       "a66bacd8cbc463c285ae3ab0cee3a2cdd564fae893cb9285a279669a4e4895e2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b07db8aaa14235d0362eacc2a941ef995f821ce859db12a90857bb06628f270c"
-    sha256 cellar: :any_skip_relocation, catalina:       "7f69cc06c12c4cd81140b0242ec62f71a048906d4dc0312996a88b3e6fec5dc1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdb6483ba039f052ec3d17511bfe06c556d4d22c9159e5c4e49a821d95ed93f5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "732fd7172290873d737a4836b038864241da83575315e503b32edb39135de9ab"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9eae773da09bec36776d0f03318cf9343c1874919951f4fc6eb8c65b220bb09c"
+    sha256 cellar: :any_skip_relocation, monterey:       "9cb8ef9bb608c3ef51525cde188183f0bbb303a757958ca62944885f2dca325d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f3ba067c67b609f11896d5de810bd98cb376d7c251f33457a016a75b113cc93b"
+    sha256 cellar: :any_skip_relocation, catalina:       "0abed0687bbf108fb34fe342f5d573dfeb1ab54456f3f5e1f734242a99fb43be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7a8e608439e930392ae9e122e8d1f157531b4a74749e241abbde64bc385b76d9"
   end
 
   depends_on "go" => :build
