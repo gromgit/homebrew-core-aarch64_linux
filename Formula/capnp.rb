928867130ed515ae47054210cad9ea87e9ea2ab4
Porkepix <porkepix@gmail.com>
2022-06-30 11:17:40 +0200

    capnp 0.10.2
    Closes #104781.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -1,8 +1,8 @@
 class Capnp < Formula
   desc "Data interchange format and capability-based RPC system"
   homepage "https://capnproto.org/"
-  url "https://capnproto.org/capnproto-c++-0.10.1.tar.gz"
-  sha256 "91413335e2f078017c22e0b1be91c28af9dd7dd8127b88914d21c2bcea55df51"
+  url "https://capnproto.org/capnproto-c++-0.10.2.tar.gz"
+  sha256 "7cd91a244cb330cda5b41f4904f94b61f39ba112835b31fa8c3764cedbed819f"
   license "MIT"
   head "https://github.com/capnproto/capnproto.git", branch: "master"
 
