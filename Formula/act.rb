7df431869b4b28670bf4b42593dc273a0a44cc28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-01 13:17:03 +0000

    act: update 0.2.29 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -7,12 +7,12 @@ class Act < Formula
   head "https://github.com/nektos/act.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "271f88b2f5a6c89218ff72777400b5410905455fb4c3b4a7805554b23ea1e11d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66a693422fff57c4fcc10fc0d35dadddfeb6a8149c6d512560f6e463cadcfd1f"
-    sha256 cellar: :any_skip_relocation, monterey:       "e6733f3d6dc2fcdc0a2b29b72763f500f20f469e6757108e81f80959d36d8c66"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7ac7e074d64e8fce7d042fbaed0d88fe4b09efe03dada203bf86779771f60dc7"
-    sha256 cellar: :any_skip_relocation, catalina:       "c13bc4b8989890a35ebbb630dcdb0f80171e83d0b88cdb8b5cbc3e25724fe8c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d767d5e5f94a165f2b505c77552c7d655b96fd9f437e818d76ec53d8cb415cd5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "93df973291a43931e5daf45c3af27d53041ab083825f37f8ff843a527b03789b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0c0956174f1bd2ea40c05026b794e9a69b3cc31a46d42fd2128e30216c6427fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "5215af5563695f5d0c11f4762c309c3bdf8a80ed3798e7a15ae49665d63c864c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "68e14ba5fb0453147f9fe090d8daee4af8d3bf7f5c3c02a0c917a3b4f715f711"
+    sha256 cellar: :any_skip_relocation, catalina:       "368b9522f56066dc5737970e1e698304d0040aa4c5575cf1510c690a8f954571"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1fa51c27af6b9618ba9614210141fd4897b27d8175c533bb406da7a9f29bb1d9"
   end
 
   depends_on "go" => :build
