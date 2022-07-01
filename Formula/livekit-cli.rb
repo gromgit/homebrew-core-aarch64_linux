be468e6ba62345a46a43c7e0632ec18119f5e4cf
David Zhao <dz@livekit.io>
2022-06-30 17:13:31 -0700

    livekit-cli 1.0.2 (new formula)
    * livekit-cli 1.0.1 (new formula)
    * incorporate suggestions
    * v1.0.2
    
    Closes #104900.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/livekit-cli.rb
@@ -0,0 +1,20 @@
+class LivekitCli < Formula
+  desc "Command-line interface to LiveKit"
+  homepage "https://livekit.io"
+  url "https://github.com/livekit/livekit-cli/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "6c3102aa027d8d8f735fff4e16608e91227497650aa93f07ea0d8d1111f98f9d"
+  license "Apache-2.0"
+  head "https://github.com/livekit/livekit-cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/livekit-cli"
+  end
+
+  test do
+    output = shell_output("#{bin}/livekit-cli create-token --list --api-key key --api-secret secret")
+    assert output.start_with?("valid for (mins):  5")
+    assert_match "livekit-cli version #{version}", shell_output("#{bin}/livekit-cli --version")
+  end
+end
