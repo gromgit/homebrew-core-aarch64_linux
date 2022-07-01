b96b566e50d630633031b8cee381ac448bd8e5c6
Porkepix <porkepix@gmail.com>
2022-07-02 01:35:51 +0200

    faudio 22.07
    Closes #104912.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/faudio.rb
+++ Formula/faudio.rb
@@ -1,8 +1,8 @@
 class Faudio < Formula
   desc "Accuracy-focused XAudio reimplementation for open platforms"
   homepage "https://fna-xna.github.io/"
-  url "https://github.com/FNA-XNA/FAudio/archive/22.06.tar.gz"
-  sha256 "6cc2480a191ecfd6a2b6c57af3c21307ffb8a4d4af95769386590e9a33ea1750"
+  url "https://github.com/FNA-XNA/FAudio/archive/22.07.tar.gz"
+  sha256 "591c8bf63873856d051d4ba8ff4a43e853391465d4eacd164c1d10898d848d04"
   license "Zlib"
   head "https://github.com/FNA-XNA/FAudio.git", branch: "master"
 
