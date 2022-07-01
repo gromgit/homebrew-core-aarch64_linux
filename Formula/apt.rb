81d1a7a2e37c3984c0de000db0246fe617189115
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-01 05:16:25 +0000

    apt 2.5.1
    Closes #104864.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apt.rb
+++ Formula/apt.rb
@@ -1,8 +1,8 @@
 class Apt < Formula
   desc "Advanced Package Tool"
   homepage "https://wiki.debian.org/Apt"
-  url "https://deb.debian.org/debian/pool/main/a/apt/apt_2.5.0.tar.xz"
-  sha256 "61cd84d04e2a7858f70dca21160977479c74150853cdeef7c843d59e72488c1f"
+  url "https://deb.debian.org/debian/pool/main/a/apt/apt_2.5.1.tar.xz"
+  sha256 "dafeab05f985a4851daa6baf4e3358c6585a161a1d6c6867db1a538a6a71fbb1"
   license "GPL-2.0-or-later"
 
   livecheck do
