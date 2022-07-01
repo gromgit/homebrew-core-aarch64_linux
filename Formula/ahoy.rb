8b490dcea26afea12b5268c2f7b9bb6c27586235
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-01 15:07:16 +0000

    ahoy: update 2.0.1 bottle.

--- Formula/ahoy.rb
+++ Formula/ahoy.rb
@@ -6,15 +6,12 @@ class Ahoy < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2e46722a7462714b2365104084a2da3200d8517f2211bac3341803b0fdc38b15"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6d677fd11b20884b0ee00a028d6d8983e3eb88f006de381bd2d59c156215158d"
-    sha256 cellar: :any_skip_relocation, monterey:       "789a5ed82fd38438338df61f878454675adab3719fa7063707e185784cfda190"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4d796f66f413571680fa23532f3f1384670daa4cff4d91e5b05ff3b0ca478462"
-    sha256 cellar: :any_skip_relocation, catalina:       "fb8e03826f9109edc8bb5b1e0c7c9d8054d76e364bca13e0afdf7a23b022a817"
-    sha256 cellar: :any_skip_relocation, mojave:         "eabaf2c0faa64d878f3fd552823b9d5103e0755ba5f3120628e605964fc93257"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "93db889b646270f7a92d32f649c9e256e4e90cfa006a04c614334f28557ce7ca"
-    sha256 cellar: :any_skip_relocation, sierra:         "5743854a4e6553adb3318a2facfd941bcf4d95a7ab3c2399400c7818c6e19c6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6388f35dab72772d6fe2cb701b46717f3ac9435023f1a5b51747dee36568b97b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a9a8a26d50fdc9820edcc65c0a7ac24b9eeac3849c74473a4e394a70c5408bd4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a9a8a26d50fdc9820edcc65c0a7ac24b9eeac3849c74473a4e394a70c5408bd4"
+    sha256 cellar: :any_skip_relocation, monterey:       "d24c807627792ed952b4577bccbdded4f0229d600fed97f11e565abd986d8916"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d24c807627792ed952b4577bccbdded4f0229d600fed97f11e565abd986d8916"
+    sha256 cellar: :any_skip_relocation, catalina:       "d24c807627792ed952b4577bccbdded4f0229d600fed97f11e565abd986d8916"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "429c103dc30811b54b33171b2a1cb9bba47b1f99efe1d878161a424da8d83ae9"
   end
 
   depends_on "go" => :build
