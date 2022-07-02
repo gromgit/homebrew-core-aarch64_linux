655bc03e6c1eda0f57641c0bfdd1ae2609b16b0d
Rui Chen <rui@chenrui.dev>
2022-07-02 15:29:46 -0400

    sonarqube-lts 8.9.9.56886
    Closes #104982.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sonarqube-lts.rb
+++ Formula/sonarqube-lts.rb
@@ -1,8 +1,8 @@
 class SonarqubeLts < Formula
   desc "Manage code quality"
   homepage "https://www.sonarqube.org/"
-  url "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.8.54436.zip"
-  sha256 "a491240b2066222680d9770c6da4d5f0cf9873c86e0d0e3fbe4d1383bbf3ce85"
+  url "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.9.56886.zip"
+  sha256 "bc31dcdadf85edcbde51fff9c0977412a5e87996f4f51f68a053ec7a44cdb818"
   license "LGPL-3.0-or-later"
 
   livecheck do
