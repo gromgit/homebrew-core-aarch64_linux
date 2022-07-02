bc867e366222433fdb29d707c6bf15fd8ece1af1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-02 05:06:57 +0000

    fswatch: update 1.17.1 bottle.

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -11,12 +11,12 @@ class Fswatch < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "91748b16bf63b64ddd0d388fdb2da055b536a4ff3ff0765e4ba03fd5149b4a6c"
-    sha256 cellar: :any, arm64_big_sur:  "220bbb702c2bd76ddbe1547368e3e6adaeb7a6e3642265b2bf663007787e4fca"
-    sha256 cellar: :any, monterey:       "0f086fbed007ce65e76beb344b7140426fd78ead5dc49bb41f990ff68c911fa0"
-    sha256 cellar: :any, big_sur:        "3ff1a1373e2918d6ae8615ba6db2b5ddf449acf99c8e0cc5a917b86bc7cb2bdb"
-    sha256 cellar: :any, catalina:       "1030176db64c0babafed0055afc1740b96372ef801b31a4f6e74178a5adab0fb"
-    sha256               x86_64_linux:   "68ec0add9fda7ef354233a32495c2ba9d58734e7970fd2dcd4948185cdb0d5af"
+    sha256 cellar: :any, arm64_monterey: "b7f5facb15c82b5dc9eb94e8cfaa4857e562609be24fdd716051c35bd2e85e8b"
+    sha256 cellar: :any, arm64_big_sur:  "ec08b3bf8f659a864d0c54f022939b45ea647c25769a8ab908f60f28ffbd803c"
+    sha256 cellar: :any, monterey:       "6c57d2ea9ff9e425069580bba25c74f5890f454b807f4a94810271909d47283e"
+    sha256 cellar: :any, big_sur:        "1da6e45f4051477e02acbf2f3d13a7917b8a80a38ca35d6ac8cbaff780df4651"
+    sha256 cellar: :any, catalina:       "c97ee3973b847257ad99f6ffff3c6ba3d33dbf2a333e0bbe289832b7e490f051"
+    sha256               x86_64_linux:   "60e3f628f00ace185e22523a1850f0521184e7477263ef69a1a0fcebd8b0f077"
   end
 
   def install
