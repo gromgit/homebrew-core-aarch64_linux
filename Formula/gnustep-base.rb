e70ad86350c0ed0d25e26032943826379d60c4c9
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-07-02 02:26:29 +0000

    gnustep-base: add 1.28.0 bottle.

--- Formula/gnustep-base.rb
+++ Formula/gnustep-base.rb
@@ -5,6 +5,15 @@ class GnustepBase < Formula
   sha256 "c7d7c6e64ac5f5d0a4d5c4369170fc24ed503209e91935eb0e2979d1601039ed"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "06f520ea70f310d6e9a60c7a4282bb01f3aa00ac2809f1fe066d9a866f5d842d"
+    sha256 cellar: :any,                 arm64_big_sur:  "1ed40ca235f00fd679d2cde5f913392227df66b2453915df0a0b841662859ed5"
+    sha256 cellar: :any,                 monterey:       "01b2126e3e693efae4ab141ecf52540d67dd2c8b2d2c5e9814856271e43f971b"
+    sha256 cellar: :any,                 big_sur:        "8c4b5a57e5b030d29db38a2aa445abb7f77dff91567850a4370c171511b9e90e"
+    sha256 cellar: :any,                 catalina:       "ff6c552ba77b712323250d2ad3a625a0a4e91f5d7b4c28f8074e605c525d6158"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "840187316351f0872d3c6a3721203939e0229b89f35ec55e1c1ff662a1f5f156"
+  end
+
   depends_on "gnustep-make" => :build
   depends_on "gmp"
   depends_on "gnutls"
