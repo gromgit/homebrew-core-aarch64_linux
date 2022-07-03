1d1c25707f26bc0f9e1e6c4ab244397770920a02
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-07-03 22:52:19 +0000

    libetonyek: update 0.1.10 bottle.

--- Formula/libetonyek.rb
+++ Formula/libetonyek.rb
@@ -10,11 +10,13 @@ class Libetonyek < Formula
   end
 
   bottle do
-    sha256               arm64_big_sur: "8ca7825177d98f44ac9f3d6ad409eb3bc79d4621cb6c75aea43f60ca66234d0f"
-    sha256 cellar: :any, big_sur:       "c0d419840c9454c6fe46fcffe7e27d57a8a5ea6a26a9bdd75ab6756f9399b2d0"
-    sha256 cellar: :any, catalina:      "d40376bdfb4527e035ec8c0c1d65927303f81a456d02ce9fb0503a41f5e9ee60"
-    sha256 cellar: :any, mojave:        "1d30b4258651cc6edbd2e7e39d6af095a25f52994baa3ed34e2be0e2f606ecf2"
-    sha256               x86_64_linux:  "50c4ec0dd235b3cde8180781e8392183d8b2f7e932f571a4317e0437deec01cd"
+    sha256 cellar: :any, arm64_monterey: "823ff94f1331fb0f24e356c2326d27ea8b71cd12aeea2cbbea26cf953a86f95a"
+    sha256               arm64_big_sur:  "8ca7825177d98f44ac9f3d6ad409eb3bc79d4621cb6c75aea43f60ca66234d0f"
+    sha256               monterey:       "b402d2d2b2e6e885b0ee19d55d43adb5b8e7e2af5d0b913f5bbc05456f493b1c"
+    sha256 cellar: :any, big_sur:        "c0d419840c9454c6fe46fcffe7e27d57a8a5ea6a26a9bdd75ab6756f9399b2d0"
+    sha256 cellar: :any, catalina:       "d40376bdfb4527e035ec8c0c1d65927303f81a456d02ce9fb0503a41f5e9ee60"
+    sha256 cellar: :any, mojave:         "1d30b4258651cc6edbd2e7e39d6af095a25f52994baa3ed34e2be0e2f606ecf2"
+    sha256               x86_64_linux:   "50c4ec0dd235b3cde8180781e8392183d8b2f7e932f571a4317e0437deec01cd"
   end
 
   depends_on "boost" => :build
