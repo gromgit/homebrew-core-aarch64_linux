c5b6dc14741239da8e6661b9eb08ed22ceb7eed7
Rui Chen <rui@chenrui.dev>
2022-07-03 10:57:40 -0400

    aws-iam-authenticator 0.5.9
    Closes #105006.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -2,8 +2,8 @@ class AwsIamAuthenticator < Formula
   desc "Use AWS IAM credentials to authenticate to Kubernetes"
   homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
   url "https://github.com/kubernetes-sigs/aws-iam-authenticator.git",
-      tag:      "v0.5.7",
-      revision: "2a9ee95fecab59fab41a0b646a63227d66113434"
+      tag:      "v0.5.9",
+      revision: "1209cfe28e95e32e719d0d69a323e6172a423333"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/aws-iam-authenticator.git", branch: "master"
 
