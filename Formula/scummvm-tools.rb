21a6c8034a1e2c1dcf120e779d915c76cafbc656
Sean Molenaar <smillerdev@me.com>
2022-07-03 13:24:59 +0200

    scummvm-tools: update 2.5.0_2 bottle.

--- Formula/scummvm-tools.rb
+++ Formula/scummvm-tools.rb
@@ -13,12 +13,12 @@ class ScummvmTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "73ef45568a2f2c3eb2a8380d8e82d91883e65f29b68ce8075b6c6c70cecec2c1"
-    sha256 cellar: :any,                 arm64_big_sur:  "183d19df8632730d2151dd50f4cea6d2a1b84aadc781b6060bcbc97a0d30987d"
-    sha256 cellar: :any,                 monterey:       "2fdfe65529343f31ba2546a77cfd6dee2231154398ffbe16499b02128ec0a959"
-    sha256 cellar: :any,                 big_sur:        "3ad6ba0e27b1af1efe346f322ffbe516ac3f86231c502988121f20ba1b30351e"
-    sha256 cellar: :any,                 catalina:       "d8fc65646ca7e7ef48152d0b480fa7741ca8293adca7b8fe4d26f0345c848e25"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e336efaaf645f0ec4f87e1de4355dd6fd745d8debf64c7724936a5b361f48862"
+    sha256 cellar: :any,                 arm64_monterey: "25802720d5990ecf69ef61dfdfba57dd7a2c017a91ce31eb85dbb55cc0e86216"
+    sha256 cellar: :any,                 arm64_big_sur:  "46153393342ac8d4b9ebffaa30bf404c2d2a04228fd0303a8aada5bb2d84286d"
+    sha256 cellar: :any,                 monterey:       "c328946d81f8025c6240e0e75b18369d9475ecafa4349d6781547dc51e96586c"
+    sha256 cellar: :any,                 big_sur:        "35772d6de4633c6fbfeaed8e800fdc188591dda5dc28f9a960d91c1e43094bee"
+    sha256 cellar: :any,                 catalina:       "bcc9754b82e993b53eeca37efa6351d54593de0e2204cde353fa5df52bcd24b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e20689e030d11aed9c77ec9df02beb39547ec8b5f9dfe97791ecc7ab3f59af01"
   end
 
   depends_on "boost"
