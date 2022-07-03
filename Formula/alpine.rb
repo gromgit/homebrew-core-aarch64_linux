a2fa973dc1c7ea8043c85ddf3aaf7a8bdc74dad0
Bo Anderson <mail@boanderson.me>
2022-07-04 00:29:56 +0100

    alpine: move to new upstream website
    Also add mirror for non-latest versions.
    
    Fixes #104992.

--- Formula/alpine.rb
+++ Formula/alpine.rb
@@ -1,7 +1,8 @@
 class Alpine < Formula
   desc "News and email agent"
-  homepage "https://alpine.x10host.com/alpine/release/"
-  url "https://alpine.x10host.com/alpine/release/src/alpine-2.25.tar.xz"
+  homepage "https://alpineapp.email"
+  url "https://alpineapp.email/alpine/release/src/alpine-2.25.tar.xz"
+  mirror "https://alpineapp.email/alpine/release/src/Old/alpine-2.25.tar.xz"
   sha256 "658a150982f6740bb4128e6dd81188eaa1212ca0bf689b83c2093bb518ecf776"
   license "Apache-2.0"
   revision 1
