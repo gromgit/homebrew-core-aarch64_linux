f1c52b5873546de61014ebbcd5d6d120a582c656
Sean Molenaar <smillerdev@me.com>
2022-07-03 13:25:05 +0200

    povray: update 3.7.0.10_2 bottle.

--- Formula/povray.rb
+++ Formula/povray.rb
@@ -13,12 +13,12 @@ class Povray < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "86cff67d1cc28d4da2cf009ccaa095338cc361a83a8e77533e241a0a9483b171"
-    sha256 arm64_big_sur:  "323a45aa39fc07851a6ff9e0c5c52df5e79db3885ffa5a92e9848a7aa0b1cd6a"
-    sha256 monterey:       "42aff4310d6e07f5992311c44666ee751c75907ec2cc8def1e78b5d4f7794bf0"
-    sha256 big_sur:        "411a4d7227b73cc564ab110bc223b53f29bd6bd29a05127fbd02bfdd5cf468b9"
-    sha256 catalina:       "78e8ed7577c8d5640365d09edc79ad55e43deccb5c181fc8a537ad0f31685954"
-    sha256 x86_64_linux:   "3c721140cae7a89f1e0517b9241610ab8b981627c326a7801e68d9754f66fdf6"
+    sha256 arm64_monterey: "50c1033aa599a0c313e1321ac13556c7010149e55ee8e250f6cd91bba1b90d11"
+    sha256 arm64_big_sur:  "b81e5f00128b2b215a73cd4dacd27829faadb4aea63c10139736e851f9858fe6"
+    sha256 monterey:       "91a6fa846d5220e1b2f7e20ece98f52cf9ee34d301c339cb07f90bc09c4dfd15"
+    sha256 big_sur:        "497cfaba5d266ce6e1d5906fa8724418817b5faff66f521bf434cf6fa33fa5b4"
+    sha256 catalina:       "82bf8090b26e6f470a6c7c76a050ca53ca5c2ae8c0445b172050f01a29cfc070"
+    sha256 x86_64_linux:   "8acd14c9e9e02e4965712979981063184361285aff59695b2a779dad49e022ea"
   end
 
   depends_on "autoconf" => :build
