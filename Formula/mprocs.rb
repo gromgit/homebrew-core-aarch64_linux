10882bceb717b4fc22a8e9f7bc8ea5e7735eb11b
Rui Chen <rui@chenrui.dev>
2022-07-04 22:53:15 +0000

    mprocs: add 0.6.0 bottle.

--- Formula/mprocs.rb
+++ Formula/mprocs.rb
@@ -6,6 +6,15 @@ class Mprocs < Formula
   license "MIT"
   head "https://github.com/pvolok/mprocs.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "881e28ef9ab2ddea122a3c0560f0dbe02a4f117e805e96eb6dd96ab4d7e8b873"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0fa301d5516472a02108a7430f07fb22c36148b65b671b87695fbffd8a9184d2"
+    sha256 cellar: :any_skip_relocation, monterey:       "e4be9f220284d9c168821fbd14393cdaed68fa6ca81eb1eb356ebc2f62ce0b80"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b73ce3e76a181868dfaf2b84ddb41a869926d8a7151ed4eedd451d51249a1193"
+    sha256 cellar: :any_skip_relocation, catalina:       "39671f9f21d8fd9063c54a8362d505372e5c8aaad7d8a05cccdaa45d2a90f5c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "04ea3965855d3af00e6a65bed5ad20caed5ba4b592d80573278224cdf36aa5ad"
+  end
+
   depends_on "rust" => :build
 
   def install
