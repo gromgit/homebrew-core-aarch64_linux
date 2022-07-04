6f3b8445bcf9da92ebda53657995fef9172fb1fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-04 12:09:21 +0000

    sbcl: update 2.2.6 bottle.

--- Formula/sbcl.rb
+++ Formula/sbcl.rb
@@ -11,12 +11,12 @@ class Sbcl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "71409c947f8913657a9d76e34d1e7bd6e61341a9e239ce2f7c416b1d263e25c8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e2ded6378b6c3c1ad354a72b439e0bf3ea770524a8112457951af4ce9149d2c8"
-    sha256 cellar: :any_skip_relocation, monterey:       "74050aa4cfbd6c9438e9a0b9ccddc1e7b97dd4d804e5d6af5182fc9b5c00ff3a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "76a902371fbd89891ac8f175ee7c9ea06061548c2fb861b7f9a0d87bcff28f39"
-    sha256 cellar: :any_skip_relocation, catalina:       "1978a420753d12d814aac77b7eb54bc66c67ee4a2bd55f3efd725ea907af00cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d66a7ff7718002ecf8c562c6bc937698abb1b0928d6467dcb0149a5e248daed0"
+    sha256 cellar: :any,                 arm64_monterey: "e16068a0809bae797fd26f008199ceed86c7c18205710a62c7b0db995b05e7d8"
+    sha256 cellar: :any,                 arm64_big_sur:  "4e931278c566d7886940112073ce713fda39d4b93f8637292788cfe71ebc1bbf"
+    sha256 cellar: :any,                 monterey:       "4c7ca056ef2c4827fb750273945a50dcde9c3f8872a0a46d747934a5a7374191"
+    sha256 cellar: :any,                 big_sur:        "14bf977877ab106d87d6328aada25de73013874547d6e8646840b721b0c91f5c"
+    sha256 cellar: :any,                 catalina:       "e9402c538fd3bd116ffc6664b5f5d4962ae458484a2267a3e7422f98cd9da896"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f7fa408bb4d3b57e041c6f5449193970f4f5bc828078cda252de238eddb31c2"
   end
 
   depends_on "ecl" => :build
