3282cf9dae21c2632c6c9e4a7961678aed38dc72
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-04 02:32:40 +0000

    verapdf: add 1.20.3 bottle.

--- Formula/verapdf.rb
+++ Formula/verapdf.rb
@@ -12,6 +12,15 @@ class Verapdf < Formula
     regex(/^v?(\d+\.\d*[02468](?:\.\d+)*)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "95a2266e2f0b9dc0b50cf8363c53d793ec881e0dbce1fa752ce242cac9b6b55f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f638ec0317c74ccb17bb44fdffa5b2e4c1820b061f8ec4c90d730d75c816bc3"
+    sha256 cellar: :any_skip_relocation, monterey:       "1da748cac0e4ab29f372c2f8dbf441d79bc59991ac76535d791abb84f61fdb32"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9243f0befca09bf3a14400085a134516393665ef3f16d84da6c58ce46b6f8a4b"
+    sha256 cellar: :any_skip_relocation, catalina:       "c813cfcde5c3f5c0ff102f34b8cfaf99b3209e253d52cf9ce6472414a7bc83ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f300e2830f22d602f6e0c758af0d14a9c946d57aeb259d5014e14cf64991b45f"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk"
 
