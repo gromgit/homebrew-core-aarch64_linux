cf9ccef117c2143a377b7ff9e617210af8384dd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-04 20:04:50 +0000

    liqoctl: update 0.5.1 bottle.

--- Formula/liqoctl.rb
+++ Formula/liqoctl.rb
@@ -6,12 +6,12 @@ class Liqoctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d5ba2ec39f27f2c4f47d0fe48c0be431d9a0ca70ab594e96521db829ad45927"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8d5ba2ec39f27f2c4f47d0fe48c0be431d9a0ca70ab594e96521db829ad45927"
-    sha256 cellar: :any_skip_relocation, monterey:       "39a39ce3e2297dd5db6d528ae42e5821d58be76e1a644c43008b130dbf22b2ef"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39a39ce3e2297dd5db6d528ae42e5821d58be76e1a644c43008b130dbf22b2ef"
-    sha256 cellar: :any_skip_relocation, catalina:       "39a39ce3e2297dd5db6d528ae42e5821d58be76e1a644c43008b130dbf22b2ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "14b1421b9ed7e36dc8c29090b76206434e3f154f28ce41bf37fc09e4eda0e3a5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ebe9ee1a1d0e0df1ec22d3cc27b81f5730511ac51af837717ac51cc66c58a96c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ebe9ee1a1d0e0df1ec22d3cc27b81f5730511ac51af837717ac51cc66c58a96c"
+    sha256 cellar: :any_skip_relocation, monterey:       "692f3763866e821a640116267b755b4f87d7874e34a2ad5a7eb5a13f895d9266"
+    sha256 cellar: :any_skip_relocation, big_sur:        "692f3763866e821a640116267b755b4f87d7874e34a2ad5a7eb5a13f895d9266"
+    sha256 cellar: :any_skip_relocation, catalina:       "692f3763866e821a640116267b755b4f87d7874e34a2ad5a7eb5a13f895d9266"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc9cf71f210205fbcce1120b4b4050eb7c353f2935a17c876f23119d7d4bb4c4"
   end
 
   depends_on "go" => :build
