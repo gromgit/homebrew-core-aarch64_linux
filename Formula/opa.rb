9d52136be8d192107d85a62dbffd1e2b1956b9c7
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2022-07-04 12:36:28 +0000

    opa 0.42.0
    Closes #105066.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -1,8 +1,8 @@
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/v0.41.0.tar.gz"
-  sha256 "51771d3b0f5d84f6c35948b2253b499041719f683989cf72a596ff8ff23eebeb"
+  url "https://github.com/open-policy-agent/opa/archive/v0.42.0.tar.gz"
+  sha256 "b16cf10e5120605bf5d1836f2815d218c471c7aca157c0d586e76752f91e2fd3"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
