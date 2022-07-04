f8d322dc1a5101858352af208fc0729b37a9f6ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-04 20:04:43 +0000

    nali: update 0.4.7 bottle.

--- Formula/nali.rb
+++ Formula/nali.rb
@@ -7,12 +7,12 @@ class Nali < Formula
   head "https://github.com/zu1k/nali.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7c16ca7ce26ecc4d6ba8b42603ecaea7313171bb5ca55723c17d4c9fede6c1bf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4fec7a7576443abdf105736336ec9e6eb79ed23da59e9b60ad18413a9478331b"
-    sha256 cellar: :any_skip_relocation, monterey:       "a60d4926f9e7d07ff9e462a5e438f474585daf275b8443cd799fbaad088a8bca"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1bc421f5d25991fda289f93bbffc1d4f66e17dd64534f3ed9610d5dce817b206"
-    sha256 cellar: :any_skip_relocation, catalina:       "0dc48cbce0f3249a306403840a76535720de9040d4322ae81f5ab3942c2bed5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b4cd90f2fe6033a8882c326d259bc6349e0e40bde696d656680f56f17ec2977"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff5b4a5b63e41068fb53d1b64ce5731a4ae0beb7a9195a7e8d3797dea12cb8ec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c1a88f6d3a194e8809aff3158847b291623ef17f1b812faac4e790daaab08987"
+    sha256 cellar: :any_skip_relocation, monterey:       "191ae60904bfcf17d5eede4f1ebca9905a7ccf200b0ad33dd1e0bb7379cf5950"
+    sha256 cellar: :any_skip_relocation, big_sur:        "838721f7469b809baf29137ad32fcde22cdfc708079b87429bb2dd032dba8bb5"
+    sha256 cellar: :any_skip_relocation, catalina:       "bd10ed0576a21abe2198bc995e9fa9eb130e1827558213fc77982afe7f8e66c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff99592469a9843ae3f40632617d6b649e9f47c02bf2b01f3954d0ad43656da7"
   end
 
   depends_on "go" => :build
