a9982d083a97ac982d7340c0cf05a2dd546c3fe6
owine <owine@users.noreply.github.com>
2022-07-05 09:43:38 -0500

    fmt 9.0.0
    * fmt 9.0.0
    * fmt: Remove patch

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -1,10 +1,9 @@
 class Fmt < Formula
   desc "Open-source formatting library for C++"
   homepage "https://fmt.dev/"
-  url "https://github.com/fmtlib/fmt/archive/8.1.1.tar.gz"
-  sha256 "3d794d3cf67633b34b2771eb9f073bde87e846e0d395d254df7b211ef1ec7346"
+  url "https://github.com/fmtlib/fmt/archive/9.0.0.tar.gz"
+  sha256 "9a1e0e9e843a356d65c7604e2c8bf9402b50fe294c355de0095ebd42fb9bd2c5"
   license "MIT"
-  revision 1
   head "https://github.com/fmtlib/fmt.git", branch: "master"
 
   bottle do
@@ -17,13 +16,6 @@ class Fmt < Formula
   end
 
   depends_on "cmake" => :build
-
-  # Fix Watchman build.
-  # https://github.com/fmtlib/fmt/issues/2717
-  patch do
-    url "https://github.com/fmtlib/fmt/commit/8f8a1a02d5c5cb967d240feee3ffac00d66f22a2.patch?full_index=1"
-    sha256 "ac5d7a8f9eabd40e34f21b1e0034fbc4147008f13b7bf2314131239fb3a7bdab"
-  end
 
   def install
     system "cmake", ".", "-DBUILD_SHARED_LIBS=TRUE", *std_cmake_args
