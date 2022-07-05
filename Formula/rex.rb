5a550f30683613ca4132bce983405593056c7170
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-07-05 17:22:11 +0000

    rex: update 1.13.4 bottle.

--- Formula/rex.rb
+++ Formula/rex.rb
@@ -5,9 +5,14 @@ class Rex < Formula
   sha256 "a86e9270159b41c9a8fce96f9ddc97c5caa68167ca4ed33e97908bfce17098cf"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, catalina:    "de0ca43e439023982668c5563f41340a82d3ae8c45159b457749c1ab0f15d3c5"
-    sha256 cellar: :any_skip_relocation, mojave:      "24da3a602c3b434d0069244f546ed33f14e8bd3bbee1f7a99b91ca97a48b0c37"
-    sha256 cellar: :any_skip_relocation, high_sierra: "dc0b2bb90327f2fc716eb95655366fd7a3ac36d7880f25a69777c9976260d508"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "656a990a252550d057e8cea6994e1270081c7263b1978ebaf4dd21a5d59783f8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0cee1b2c63e0123547566a2e3faf8569d55708cca6a5617c8797b4980c124521"
+    sha256 cellar: :any_skip_relocation, monterey:       "bb2fbd2b2a675df48693d4c25287a9072904f0fe75e8fb3482028bd64f25472c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dfe95c2a7c71ad25c596a989b30294f24bcd8f7e17bb01b22d97d68ad73903e9"
+    sha256 cellar: :any_skip_relocation, catalina:       "de0ca43e439023982668c5563f41340a82d3ae8c45159b457749c1ab0f15d3c5"
+    sha256 cellar: :any_skip_relocation, mojave:         "24da3a602c3b434d0069244f546ed33f14e8bd3bbee1f7a99b91ca97a48b0c37"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "dc0b2bb90327f2fc716eb95655366fd7a3ac36d7880f25a69777c9976260d508"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84905c6bacd76ee95deacca3830ccbb2927bbb5d946e472a7b3f65e0e5ad7ecb"
   end
 
   uses_from_macos "perl"
