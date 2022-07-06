b33e198f1deea6e42e1bb082bba5d7512792c5b4
p-linnane <patrick@linnane.io>
2022-07-06 09:55:20 -0700

    dart-sdk 2.17.5
    Closes #105255.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dart-sdk.rb
+++ Formula/dart-sdk.rb
@@ -1,8 +1,8 @@
 class DartSdk < Formula
   desc "Dart Language SDK, including the VM, dart2js, core libraries, and more"
   homepage "https://dart.dev"
-  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.17.3.tar.gz"
-  sha256 "10fdcb6980aee7d5d04b278f337f3519af7fa6bfa6290d737ea9327954b515a1"
+  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.17.5.tar.gz"
+  sha256 "4a9bc470b593b6e138343ec318f4a98912e92cfb9c2e10fc2b84ec508541e7cc"
   license "BSD-3-Clause"
 
   bottle do
