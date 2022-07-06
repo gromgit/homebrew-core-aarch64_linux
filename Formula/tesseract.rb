fef071b835a5a360f31143c82c465b97bd409b05
Porkepix <porkepix@gmail.com>
2022-07-07 00:55:34 +0200

    tesseract 5.2.0
    Closes #105283.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tesseract.rb
+++ Formula/tesseract.rb
@@ -1,8 +1,8 @@
 class Tesseract < Formula
   desc "OCR (Optical Character Recognition) engine"
   homepage "https://github.com/tesseract-ocr/"
-  url "https://github.com/tesseract-ocr/tesseract/archive/5.1.0.tar.gz"
-  sha256 "fdec8528d5a0ecc28ab5fff985e0b8ced60726f6ef33f54126f2868e323d4bd2"
+  url "https://github.com/tesseract-ocr/tesseract/archive/5.2.0.tar.gz"
+  sha256 "eba4deb2f92a3f89a6623812074af8c53b772079525b3c263aa70bbf7b748b3c"
   license "Apache-2.0"
   head "https://github.com/tesseract-ocr/tesseract.git", branch: "main"
 
