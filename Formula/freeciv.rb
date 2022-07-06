d56b6aae4078fc37bcd599fa49b687281443113d
Sean Molenaar <smillerdev@me.com>
2022-07-06 10:05:12 +0200

    freeciv: revision bump for icu4c 71

--- Formula/freeciv.rb
+++ Formula/freeciv.rb
@@ -4,6 +4,7 @@ class Freeciv < Formula
   url "https://downloads.sourceforge.net/project/freeciv/Freeciv%203.0/3.0.3/freeciv-3.0.3.tar.xz"
   sha256 "13215adc96be9f2894d5f3a12c78b8ebb9ae06ecdab25fe6bb1794f6e6d2b61b"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :stable
