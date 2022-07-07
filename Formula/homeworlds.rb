2e38444a307baf1692de55643fcbc5b8a76b0645
Sean Molenaar <smillerdev@me.com>
2022-07-07 09:52:09 +0200

    homeworlds: revision bump for wxwidgets 3.2

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -5,7 +5,7 @@ class Homeworlds < Formula
       revision: "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
   version "20141022"
   license "BSD-2-Clause"
-  revision 4
+  revision 5
 
   livecheck do
     skip "No version information available to check"
