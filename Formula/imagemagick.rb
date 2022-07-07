06147a8e55749dd56e610b4f827eedddfc273817
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-07 05:38:01 +0000

    imagemagick 7.1.0-41
    Closes #105299.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -1,8 +1,8 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://imagemagick.org/index.php"
-  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.0-40.tar.xz"
-  sha256 "445286dd6b5ec3e23d386c9bb4f7a6d7746c126a78088671a853a088efed446a"
+  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.0-41.tar.xz"
+  sha256 "feb437041a636fa9b6ddc6f66464d4554111bfdce8a54294ff894e82c9d22a19"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
 
