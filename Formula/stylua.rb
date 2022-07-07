ca195c5c3488726cb44dbb8b7d4888ff7a807f7f
Porkepix <porkepix@gmail.com>
2022-07-07 02:25:17 +0200

    stylua 0.14.0
    Closes #105286.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stylua.rb
+++ Formula/stylua.rb
@@ -1,8 +1,8 @@
 class Stylua < Formula
   desc "Opinionated Lua code formatter"
   homepage "https://github.com/JohnnyMorganz/StyLua"
-  url "https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v0.13.1.tar.gz"
-  sha256 "21158028569158ec7c1ad71352f3cb1906a005eb797508aa2b0b4a861162cf72"
+  url "https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "2cb07ad462b2c8931e60fe8943c91121b0612c9c41dc56d57a1f2161af5e1162"
   license "MPL-2.0"
 
   bottle do
