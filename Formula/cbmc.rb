1c956daa39b60e2c065aea90d47b683c9b28958b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-07 15:15:02 +0000

    cbmc: update 5.61.0 bottle.

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -7,12 +7,12 @@ class Cbmc < Formula
   license "BSD-4-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "be27b095148e4870af7cdb6e7b4709e88f831ec73e1769e7781242971f098316"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8bfc1f074842b8b2fd40323af2bfdf2b1a9bf02e9ad2ad5fd66402de9f7f9d8f"
-    sha256 cellar: :any_skip_relocation, monterey:       "7f5710ae2d1cc7089de8493eb248e031ee748c2ec8392fb2422b05bca2b4e199"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5ec886e503579fdf2ce45dccc15d5e3c2bc91741d140bdaa36a1aaf9a4f1c059"
-    sha256 cellar: :any_skip_relocation, catalina:       "ec3c098c1bba2122c438d4283ee075be84e965ad9acd096ea58470d80596c72f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "86752f1da3cc8e8c8be30914ae128b5e05663a7155c3e4fc88bb2f0993ac7cc4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d375bb18ae140386579c98dcfa47c650690ce927b98ad1d886013a99bdbb126"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c7fc52c64ad41c67bf53734d634b5e679d0c3555d1b962ca00f7a08f9f50e04"
+    sha256 cellar: :any_skip_relocation, monterey:       "43cc2245e1b041988733dfc0cef7647ad52b524443a6ed5a4d86fa30a3aef69b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "67e0f152bdb1fbdd0495212bfd26ec456aaedf16292a4070465c87a7034a77a0"
+    sha256 cellar: :any_skip_relocation, catalina:       "17824331878a73ced0d6b05ac8037d853bd2d790fb0282127ecbdceaa5f2df59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "579d060e49739a22e5461d211732342520ed5184f589712b5cd4402aa64a6a21"
   end
 
   depends_on "cmake" => :build
