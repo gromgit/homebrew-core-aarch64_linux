df59e17bfc178dd5b076aefffdf8227c4ab3f8d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-07 09:05:01 +0000

    monika: update 1.9.0 bottle.

--- Formula/monika.rb
+++ Formula/monika.rb
@@ -8,12 +8,12 @@ class Monika < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb0c9f13d633fd175c17b547e0b3f587449b7ec0dcec6a02fa7141f60aab72e1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68fb7bfeb74c771f6336762ac7f6ca614638abafce4f419d5e2b1ce142cb1eca"
-    sha256 cellar: :any_skip_relocation, monterey:       "5ab1b9a18353f13807d28c475d6cbe638faaa1c16370130c86f8a4affde43606"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b98de1e772f4604edf7f0c3ce8a9a80848418c512c0f5fc265df069ccc52cd22"
-    sha256 cellar: :any_skip_relocation, catalina:       "5dfd842197702ffadc51af19b7f8a72581b583c254b99c1e1e731f3c609e41ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9789bff5160d732e9f61fdeef51d1c5762833b4b7d574a6e9e4721f75eeee4ea"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "93bff965d37f1b3a45efded923fd4243f2de2fcc485c5360fa2fea626c9faac7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9dab1f3918d54d3c17dc18acdcfde755950060981833443faa777f7f5ad64bf7"
+    sha256 cellar: :any_skip_relocation, monterey:       "5d3d74cc18e79b0b281254ea330157c3a59f45d38a1f5ec20b2e14bbeff58dd2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "98d0f113b8a390736cef0fb5b92f8243de32b068bdefde001e33aea3290d95c8"
+    sha256 cellar: :any_skip_relocation, catalina:       "1aea242aba4f0d58ef9ba2cb9fe98f687d5f534e6c2cc358a446a1422295a05e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2cafeed84449b1249fedb6d91decd8398d26d42831940fb60d83251f3a14133"
   end
 
   depends_on "node"
