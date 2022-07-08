8e7f548f186a0d4b2307e9bbcd752b6a3b972088
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-08 09:05:07 +0000

    dtm: update 1.15.0 bottle.

--- Formula/dtm.rb
+++ Formula/dtm.rb
@@ -6,12 +6,12 @@ class Dtm < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea5e1d23e08371917dcf44b140263e262c12d3d6a25251161f9f544afae0b039"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f2861f0800c9895d95a569f5f1691c98a3d8b7dd0e1cd77df6f03fc74ccf0f8d"
-    sha256 cellar: :any_skip_relocation, monterey:       "c530422c825601fa541b4666565d91d2ddd2f3a328d4cb48e8521d2260bf9e44"
-    sha256 cellar: :any_skip_relocation, big_sur:        "03725db3cea1d0bdb3802045b2ae9d8abfe030ba44555c56cfc113565ad23b5f"
-    sha256 cellar: :any_skip_relocation, catalina:       "c4d04a3f8d93e3b5072c95c9dd1346e4193e9ca375f44423f7e5ed2332e72188"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9df5669005b45a2b11c602c236146bf6656ae60c7992124e8e21d218e151ce27"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b3992499d12586fd07829e3a817e7cb6a4d8cfc52180291783c8828178ede209"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "991647adfeb6a574fa729563c32dc841b53add862b3b6ed1133dff6f692a978c"
+    sha256 cellar: :any_skip_relocation, monterey:       "66a70577e40e1c3a47bf090837a7220c027fb04a655d6c990b37009da0181a45"
+    sha256 cellar: :any_skip_relocation, big_sur:        "388f759f64cae727e32d9baa069737e84806f7a5e71d06cad8905357b905651c"
+    sha256 cellar: :any_skip_relocation, catalina:       "e12f3c8f95b607420e1b722e1ee8e51249ba0545897cdd381891790b72ed99c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae44759abcf1e5f3277720e78f6d7a4065042af734567d1030d47d193753ec00"
   end
 
   depends_on "go" => :build
