94f646a7b388f04c859bc3f0d1fdce939684e117
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-08 06:05:27 +0000

    cfonts: update 1.1.0 bottle.

--- Formula/cfonts.rb
+++ Formula/cfonts.rb
@@ -6,12 +6,12 @@ class Cfonts < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "69f920b8101381698c98ee2f8279bfbfdb60411e76a102517d1e9d1646a22fbc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "909c675b070bf4dbbf13655e7263ce7e73b9c006431871e92cac79cb9126bcd0"
-    sha256 cellar: :any_skip_relocation, monterey:       "b663353dc3812860984b98f0f422ad70ac3afeb332f3ec53f8ba7fe365b1f4c7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d3d77e41e913c73c751c50773f87e69555604e5b2ff5143f1261b905ef573c1e"
-    sha256 cellar: :any_skip_relocation, catalina:       "e6a2c408945981bbd2095827fb7c4e7de1b995d1a7582be04256d9d8118455a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33dbebecbf017436acae02884df0708ceba60af94e782fd56957f060b0c00dad"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f0c159afb368d8b9e13fb37a74541565a42dd93b87bcd8c0da87db15a7badde2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b1e69ea5ab21f9d023604f303a391ad62719983d4d149c7e93fd861bac3b2fac"
+    sha256 cellar: :any_skip_relocation, monterey:       "f437c2cc028c51e771dda66e97615ac4d16a34e8a1c54946cd15cb6f08a0393b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "895ed126b72d7d98b02746f0740f7a59e6845186d12160bc9f971631982bec20"
+    sha256 cellar: :any_skip_relocation, catalina:       "d4e904e4b54af0233a7d482b811d1d671613a813b6fef4ca1fbe1d5925cc2ca6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "77d509e8ab022436a9c3877e63d08cc734b27ed1ddd99791f0416c96a415d913"
   end
 
   depends_on "rust" => :build
