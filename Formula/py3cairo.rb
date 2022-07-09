21b7afc47de2b592f6d5e03d19e516717ac2c3ce
danielnachun <daniel.nachun@gmail.com>
2022-07-08 18:09:34 -0700

    py3cairo: support python@3.10

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -4,6 +4,7 @@ class Py3cairo < Formula
   url "https://github.com/pygobject/pycairo/releases/download/v1.21.0/pycairo-1.21.0.tar.gz"
   sha256 "251907f18a552df938aa3386657ff4b5a4937dde70e11aa042bc297957f4b74b"
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "8e3a08d98747e940a973e78b4ed55c4a1111ab87e7e7bf342951cb4b9bc86886"
@@ -15,14 +16,28 @@ class Py3cairo < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
   depends_on "cairo"
-  depends_on "python@3.9"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/python@\d\.\d+/) }
+        .map(&:opt_bin)
+        .map { |bin| bin/"python3" }
+  end
 
   def install
-    system Formula["python@3.9"].bin/"python3", *Language::Python.setup_install_args(prefix)
+    pythons.each do |python|
+      system python, *Language::Python.setup_install_args(prefix),
+                     "--install-lib=#{prefix/Language::Python.site_packages(python)}",
+                     "--install-data=#{prefix}"
+    end
   end
 
   test do
-    system Formula["python@3.9"].bin/"python3", "-c", "import cairo; print(cairo.version)"
+    pythons.each do |python|
+      system python, "-c", "import cairo; print(cairo.version)"
+    end
   end
 end
