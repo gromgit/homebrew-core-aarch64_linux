4ba206dbf122b438241793f5f65b788f92bf9edd
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2022-07-10 17:42:53 +0000

    licensor: add 2.1.0 bottle.

--- Formula/licensor.rb
+++ Formula/licensor.rb
@@ -6,6 +6,15 @@ class Licensor < Formula
   license "MIT"
   head "https://github.com/raftario/licensor.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ced2f38da0f60e150836a6e9b5a8d4552f74779ef3059c708c1a325c6e8b7a00"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b107c7fdf50267fca917c134940c0147cd4604ecb0ad1c2df0c2deeddc62b542"
+    sha256 cellar: :any_skip_relocation, monterey:       "8444527ca924949045edce53ff444f25b96823ab8d99835d4e60cc9264576379"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0fe890312aaebe71da671a94c192c952fb46b1afc0f8f7b9f7a6933a25c8be7a"
+    sha256 cellar: :any_skip_relocation, catalina:       "16315e2c93f90aadf94c47fa73066f5d2f89c7d9d5923700a32d08a806f95284"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6dab62bd32bce3a4958d79111b496f9f21fbf7a94fa9a791fd21fc442175395"
+  end
+
   depends_on "rust" => :build
 
   def install
