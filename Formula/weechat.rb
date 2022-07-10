9efec6e1abd0204aaac815cd532e1b6ef70f5ea2
neutric <5984479+neutric@users.noreply.github.com>
2022-07-10 15:29:43 +0200

    weechat 3.6
    * weechat 3.6
    * weechat: add zstd dependency
    * weechat: use current ruby
    * weechat: use canonical name for latest ruby
    
    Closes #105506.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -1,8 +1,8 @@
 class Weechat < Formula
   desc "Extensible IRC client"
   homepage "https://www.weechat.org"
-  url "https://weechat.org/files/src/weechat-3.4.1.tar.xz"
-  sha256 "7e088109ad5dfbcb08a9a6b1dd70ea8236093fed8a13ee9d9c98881d7b1aeae7"
+  url "https://weechat.org/files/src/weechat-3.6.tar.xz"
+  sha256 "9d85d71b3b7d04c03bd35ab6501afa8b5b3c609dce7691709ec740fecc31f2de"
   license "GPL-3.0-or-later"
   head "https://github.com/weechat/weechat.git", branch: "master"
 
@@ -26,7 +26,8 @@ class Weechat < Formula
   depends_on "ncurses"
   depends_on "perl"
   depends_on "python@3.10"
-  depends_on "ruby@3.0"
+  depends_on "ruby"
+  depends_on "zstd"
 
   uses_from_macos "curl"
   uses_from_macos "tcl-tk"
