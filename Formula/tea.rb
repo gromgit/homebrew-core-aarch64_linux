54f568450176b9d847d6bdfd6c2c8f92a53d61c5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-11 08:39:25 +0000

    tea: add 0.8.0 bottle.

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -6,6 +6,15 @@ class Tea < Formula
   license "MIT"
   head "https://gitea.com/gitea/tea.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "db6f55b7cc1820df14e9811d4bc07e64955fb8941b20aa966423cd8899b51172"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "49e489bc4e912dfa35bb7e7b705116abe414a4b521af23647aa5cb0bec5ff2ca"
+    sha256 cellar: :any_skip_relocation, monterey:       "b1ff8af1bbe8ec4a642a35acab00f997becbb2cdfa36322a2599de6964281ca7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "69c29f84e5706464b5f4e755179706727732f449c2a9ec76f183e51d2f220952"
+    sha256 cellar: :any_skip_relocation, catalina:       "3c11613b652e2f3336d0decd2477f565584fdaf2456d042893e95a30d0dc2102"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1335b013d625a316a416ccf89babbf84e676cc90243972beacb9b68006d10c30"
+  end
+
   depends_on "go" => :build
 
   def install
