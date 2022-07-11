01eb7060b67b8389c44aaf654993f3a984a7b19f
Porkepix <porkepix@gmail.com>
2022-07-12 00:53:59 +0200

    boringtun 0.5.0
    * boringtun 0.5.0
    * boringtun: update cargo args
    * boringtun: update binary name to boringtun-cli
    * boringtun: test with default error verbosity
    
    Closes #105576.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/boringtun.rb
+++ Formula/boringtun.rb
@@ -1,8 +1,8 @@
 class Boringtun < Formula
   desc "Userspace WireGuard implementation in Rust"
   homepage "https://github.com/cloudflare/boringtun"
-  url "https://github.com/cloudflare/boringtun/archive/v0.4.0.tar.gz"
-  sha256 "23a02ae0c01d194ce428c465de46538f683c696fa23a82cfc42d07d40e668e74"
+  url "https://github.com/cloudflare/boringtun/archive/v0.5.0.tar.gz"
+  sha256 "a7eaea4ceb6d0b7e4edc97f60557740dfd89288ca73a1ffb985e9fc39fec7f8a"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/boringtun.git", branch: "master"
 
@@ -18,22 +18,21 @@ class Boringtun < Formula
   depends_on "rust" => :build
 
   def install
-    args = build.head? ? std_cargo_args(path: "boringtun-cli") : std_cargo_args
-    system "cargo", "install", *args
+    system "cargo", "install", *std_cargo_args(path: "boringtun-cli")
   end
 
   def caveats
     <<~EOS
-      boringtun requires root privileges so you will need to run `sudo boringtun utun`.
+      boringtun-cli requires root privileges so you will need to run `sudo boringtun-cli utun`.
       You should be certain that you trust any software you grant root privileges.
     EOS
   end
 
   test do
-    system "#{bin}/boringtun", "--help"
-    assert_match "boringtun #{version}", shell_output("#{bin}/boringtun -V").chomp
+    system "#{bin}/boringtun-cli", "--help"
+    assert_match "boringtun #{version}", shell_output("#{bin}/boringtun-cli -V").chomp
 
-    output = shell_output("#{bin}/boringtun utun -v --log #{testpath}/boringtun.log 2>&1", 1)
+    output = shell_output("#{bin}/boringtun-cli utun --log #{testpath}/boringtun.log 2>&1", 1)
     assert_predicate testpath/"boringtun.log", :exist?
     # requires `sudo` to start
     assert_match "BoringTun failed to start", output
