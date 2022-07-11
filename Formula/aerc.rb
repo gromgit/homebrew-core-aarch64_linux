f96de853be201b76fb89ee0f8c53b24578e747b0
Porkepix <porkepix@gmail.com>
2022-07-12 00:42:14 +0200

    aerc 0.11.0
    Closes #105574.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -1,8 +1,8 @@
 class Aerc < Formula
   desc "Email client that runs in your terminal"
   homepage "https://aerc-mail.org/"
-  url "https://git.sr.ht/~rjarry/aerc/archive/0.10.0.tar.gz"
-  sha256 "14d6c622a012069deb1a31b51ecdd187fd11041c8e46f396ac22830b00e4c114"
+  url "https://git.sr.ht/~rjarry/aerc/archive/0.11.0.tar.gz"
+  sha256 "3d8f3a2800946fce070e3eb02122e77c427a61c670a06337539b3e7f09e57861"
   license "MIT"
   head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
