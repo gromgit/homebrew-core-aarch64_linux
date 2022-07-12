085612b93d2be6d59a68758d55bbeca1bdf8dd9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-12 14:05:18 +0000

    gcovr: update 5.1_1 bottle.

--- Formula/gcovr.rb
+++ Formula/gcovr.rb
@@ -10,12 +10,12 @@ class Gcovr < Formula
   head "https://github.com/gcovr/gcovr.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6e8adcfba821a1015ca5264d6b657168d0607361816c3b6125a4df45d9692392"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "74ae8e3269cba62fd168a6ff8dfccf708ee967c19ae1368112af8154e1151389"
-    sha256 cellar: :any_skip_relocation, monterey:       "f88c045db39ce682304ccf040906760d7981d07a6b5dce820c0aa618e9320c54"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e902129f8caf3f14edd1632428d0bb3a96057734f6ceacfb62b6692451b17a7a"
-    sha256 cellar: :any_skip_relocation, catalina:       "45d4549efd00c40e1aeb64d5fd551530d49a2fea3a10b48c5b11e9f0ff3fa7d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "250ca2155f0eb9545d77fb6947cfac0df80d540947050438a523d9c30c86ba89"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4ccc5a4c686da2482677c5424b796a05f5cdf100d11cba23bcb2d44ab151e01a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c39d6703dfdc2c802ec91b22b8ee21c52bba80d59f0c1c83a41926c039af2f7f"
+    sha256 cellar: :any_skip_relocation, monterey:       "fedaf0456dc1ed174214ced2037eb7d2f84e00ecab7744816486c708437dd6fc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "632d7ac5e69248e41a153c96b3ea64285a4974c0dc67c9e5c3f7d488ed9d0d52"
+    sha256 cellar: :any_skip_relocation, catalina:       "deed239ab7c982d76f4591cbf35b7d95bf262bda29458e06e779991efd518faa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f23a195eeea6262e493cf5597f36e95771ef6d9de50584a8b23f22d3350fdadf"
   end
 
   depends_on "python@3.10"
