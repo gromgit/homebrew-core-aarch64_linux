af9b48ad5c02a7be3ed9854a17887607f941b01a
Sam Ford <1584702+samford@users.noreply.github.com>
2022-07-12 15:48:08 -0400

    libpcl: update urls, license

--- Formula/libpcl.rb
+++ Formula/libpcl.rb
@@ -1,9 +1,9 @@
 class Libpcl < Formula
   desc "C library and API for coroutines"
-  homepage "http://xmailserver.org/libpcl.html"
-  url "http://xmailserver.org/pcl-1.12.tar.gz"
+  homepage "http://www.xmailserver.org/libpcl.html"
+  url "http://www.xmailserver.org/pcl-1.12.tar.gz"
   sha256 "e7b30546765011575d54ae6b44f9d52f138f5809221270c815d2478273319e1a"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :homepage
