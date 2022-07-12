0eb7ab1d43c4c0ce18557aa3da3faf7e62a9e93e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-12 08:08:51 +0000

    aerc: update 0.11.0 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -7,12 +7,12 @@ class Aerc < Formula
   head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "07b0f639519dfeebab62d97d34e2be7948d7a7eed34c2296200f2753f4e8cb1f"
-    sha256 arm64_big_sur:  "7e5b591f483879bace3dc00f675e15aa1b07bc4fdd7af0c913b5d9f884325131"
-    sha256 monterey:       "a78fa9e52072723610b91543d8e5a1cf8beccf9ed39247d6564015cdb6e2d929"
-    sha256 big_sur:        "a71ff65f4b44d97327fb92ab2cd4f3adb387fe82b546875deeb6e782abdad7d7"
-    sha256 catalina:       "9ddcc5a17bd657c13e4fc170d8d48c0c6593fabfefde103c0f31d0a8ad45f753"
-    sha256 x86_64_linux:   "57c91eb2697be7bedba392caa3b2594b33ef66167a276ac68aabcd1cc495497d"
+    sha256 arm64_monterey: "014f876c6ebfb577d561ea4d46b11e1fd682be0833fd633555450d40224c9acb"
+    sha256 arm64_big_sur:  "02eaa8629b073f130d86bd6782687f2108b4dbff0fb06eca0915d807e11d1d3c"
+    sha256 monterey:       "fbef7f195c660ccef0dd623ec8b2b8e5a55ebaf0f93f2ab0b04f4475a70216fb"
+    sha256 big_sur:        "b44760c1cb2e8d1eb90564bb8ae7578b8d8e93f62147100374c0469570879eb8"
+    sha256 catalina:       "b07d4a14920b54e5833bccedcd70a28c2145a87ce9bb146fee46b45039993717"
+    sha256 x86_64_linux:   "16085d0bebccfbfbf7548537c481f26b415f69b72db190edabf27dc1979d4841"
   end
 
   depends_on "go" => :build
