7ef8ef2950816d84bf37e1cccfd7c2a25077cc61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-12 08:09:00 +0000

    gitleaks: update 8.8.12 bottle.

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -6,12 +6,12 @@ class Gitleaks < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a10df05f4d91a8da67ec81b436564992daae80f80e7d741fd775c54b2de98025"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6669518af0718e86708668770d98489b7425aea9ab9c4c5beaef537d781cdf6"
-    sha256 cellar: :any_skip_relocation, monterey:       "0a21060a5bd994339b0efe279ad404396959c5557a3f6a3ae83fdb0bd107f84f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d85278548a6bf6ed4eb5a79dc9c98ebfec3fafd78425b5d01936e2767dd467a9"
-    sha256 cellar: :any_skip_relocation, catalina:       "6f91badf79def15cbd72529a6fb500685f842fc7b35e307723178d6b32b78271"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8555bdf88135c588d27970e6f23151663422fc8ae13d85c9f7c442cd0c906770"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "febe51676526287037d70c7f44f5276d6a62333dc32b3510f77fbce284acecf9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d80c45192ada33d83b5246b8d1d870be8e7cf587f6e5148f3023f68f43efa8a1"
+    sha256 cellar: :any_skip_relocation, monterey:       "886bd197470cd8e27b1b74075be620d6fbec4ee58700434e01a6b6eefc2be9da"
+    sha256 cellar: :any_skip_relocation, big_sur:        "93cb04a442bd17725e565ab8cd60e6a83c829b75c410b4ad9bb86dfc39a0c350"
+    sha256 cellar: :any_skip_relocation, catalina:       "9391c27bc18dad3b309ef36038f2d9c6a5436446fb177b8ac3128265c7f68427"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "99447704c48bf2192f66bc7f6bfebae774564c4f1db96867946a0664f031806e"
   end
 
   depends_on "go" => :build
