42475cf6aa448a826511449ceb8890f05597f19f
Rui Chen <rui@chenrui.dev>
2022-07-12 23:00:40 +0000

    argocd: update 2.4.5 bottle.

--- Formula/argocd.rb
+++ Formula/argocd.rb
@@ -7,12 +7,12 @@ class Argocd < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f44e9e1ae531f6317fad521eb3235f08ce906acf12e861ee177c8a8382036f2a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2dc5d25f87f8805ab41fc0669f3cb1199641c5f4872e41a4f52ce83359c58d7c"
-    sha256 cellar: :any_skip_relocation, monterey:       "8340967ba972fdc48c16b2c6cc3f2ef1bc88b5fa024ab2ca3884080007e3258e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "74ce5fde2306be5be86f2493763bf623f39dee9df0e0eb48dea50f4d71235ad6"
-    sha256 cellar: :any_skip_relocation, catalina:       "a7641acfb52773923ab1edce21cf48a126383917f37972641685d10255688114"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "287a43e7195f9fc736d9dc91bde2704a58bdba5c3433ef01b2cac51c947f76ea"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cf490661ca5380c99f2808472045b5f4bd29f6e80b402460aaeec4f655f0ae50"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7b016a7f0e937a9d3951dfe53f15dca837758a271f77e7927e071d5450d4ea12"
+    sha256 cellar: :any_skip_relocation, monterey:       "5bd59457b05ead50c05c20164ec0058e19af2426bd15f9c714ba140f5d3beb53"
+    sha256 cellar: :any_skip_relocation, big_sur:        "010fc4156d2d3ae6cb4ba94b133be5743f8abacd78a92e18b16026ee258d933f"
+    sha256 cellar: :any_skip_relocation, catalina:       "f5a44abab5471856288696866f400759acaf67671aff9d86420b653d6935e39f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2932eb7d7c3a5653667be03b89cdb922d90dea47a9e2ae38e3dfee26953a7daf"
   end
 
   depends_on "go" => :build
