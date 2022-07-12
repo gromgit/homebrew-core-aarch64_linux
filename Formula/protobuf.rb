13f46794cc7d5df2cc885aec84f362c286197717
Wolfgang Sanyer <wolfgangsanyer@google.com>
2022-07-12 20:40:50 +0000

    protobuf 21.2
    Note: Starting with
    [v21.0-rc1](https://github.com/protocolbuffers/protobuf/releases/tag/v21.0-rc1),
    protobuf is using a new versioning scheme.
    
    Signed-off-by: Wolfgang Sanyer <wolfgangsanyer@google.com>

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,8 +1,8 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.19.4/protobuf-all-3.19.4.tar.gz"
-  sha256 "ba0650be1b169d24908eeddbe6107f011d8df0da5b1a5a4449a913b10e578faf"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v21.2/protobuf-all-21.2.tar.gz"
+  sha256 "9ae699200f3a80c735f9dc3b20e46d447584266f4601403e8fe5b97005f204dd"
   license "BSD-3-Clause"
 
   livecheck do
