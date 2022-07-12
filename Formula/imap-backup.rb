19e94bae73bcc7eae33891bffb6b9a821332496a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-12 08:08:51 +0000

    imap-backup: update 6.1.0 bottle.

--- Formula/imap-backup.rb
+++ Formula/imap-backup.rb
@@ -6,12 +6,12 @@ class ImapBackup < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0534506025d81847a24c1167e69a5d1000bf25e11b4f3e69a50eb2b8c391b06a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "913bff23e4c77f3114fa215b4da133882ac622c004e169fd9b4555361293ab16"
-    sha256 cellar: :any_skip_relocation, monterey:       "0534506025d81847a24c1167e69a5d1000bf25e11b4f3e69a50eb2b8c391b06a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "913bff23e4c77f3114fa215b4da133882ac622c004e169fd9b4555361293ab16"
-    sha256 cellar: :any_skip_relocation, catalina:       "913bff23e4c77f3114fa215b4da133882ac622c004e169fd9b4555361293ab16"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84c1ec4edabb3710bc67f786436f7fec4ace15da09f481912277b445ec91924f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "14e54c1dd3875213132d34a8218088fb1b59221da695dd49fc42481171c472c6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1bf858915fff9715cced1cc407115e293b3faa9bc4aa500d0ea3ad9cfa342b7d"
+    sha256 cellar: :any_skip_relocation, monterey:       "14e54c1dd3875213132d34a8218088fb1b59221da695dd49fc42481171c472c6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1bf858915fff9715cced1cc407115e293b3faa9bc4aa500d0ea3ad9cfa342b7d"
+    sha256 cellar: :any_skip_relocation, catalina:       "1bf858915fff9715cced1cc407115e293b3faa9bc4aa500d0ea3ad9cfa342b7d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "77ea795bca1ae2bec5ddf47040bb225cad6014d38ad666bd4f3bf587f30fcd25"
   end
 
   uses_from_macos "ruby", since: :catalina
