5bebba319ff98165b302f61cb1ea079355ee6e7b
Patch the dog <53427653+patch-the-dog@users.noreply.github.com>
2022-07-13 08:06:02 +0000

    driftctl 0.35.1
    Closes #105697.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,8 +1,8 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.35.0.tar.gz"
-  sha256 "c897e32502517b9e16cf50f51d10030a1ff9698a90cc1108022037e47f096abc"
+  url "https://github.com/snyk/driftctl/archive/v0.35.1.tar.gz"
+  sha256 "16328b78687770a80efe455a6ceb4bed6a18b82968d86c0aabfbd1f3ee5b6a64"
   license "Apache-2.0"
 
   bottle do
