575ace6dd2480a2b851afc39991f34a8fa635d52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-13 07:04:43 +0000

    tfschema: update 0.7.4 bottle.

--- Formula/tfschema.rb
+++ Formula/tfschema.rb
@@ -7,12 +7,12 @@ class Tfschema < Formula
   head "https://github.com/minamijoyo/tfschema.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "10d59f3a627acdfa33237ddf78e7e7b0cd3a9a97f6a7de79890d835380bb921a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ff3333d7e6548454ac126da65a61b30805a2359af1557c8559382d9460f5c27c"
-    sha256 cellar: :any_skip_relocation, monterey:       "08f7755ba039a88e324c6499ef59460c839159fa53ab736bcaaa62b257871d1f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dc4a9205a316f4d71bd144616173d22402d84581f04346f8953a111dbcf754fe"
-    sha256 cellar: :any_skip_relocation, catalina:       "18cc3140d396d8629444b20f1a26f35e4eb9df5a44a4ea9127e3178efcd0a5ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2136cc24b73af28cc0e59ee1576208f6dab5f3e5bf48aaceefd544725c5bbd83"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "34bc7541423b20c907504005dd7cacead7e28700f96ecb75ed94eebdad9e1fb6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d6676c102aa7391dd1f512a64ab64a8b8ddc190aa5ebfd0ef0be480a86cd8488"
+    sha256 cellar: :any_skip_relocation, monterey:       "6185117edc1c8c4e26525dc6af6783a2790dc0dab12e750334d44a4981a9e174"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6cfa21e5b07ca35458512c8290b756c5fe4631b26d9329648efd45d40c2b8121"
+    sha256 cellar: :any_skip_relocation, catalina:       "f680e7861c2bd8795722597e7cb252eb2c202cddbadca1c5e4775413d696fb39"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e100da387b08ad06a1ebddf5f5fd6d3bad35bdfab7155e14f45c2e3cce557a5d"
   end
 
   # Bump to 1.18 on the next release, if possible.
