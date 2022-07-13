30bc8a4ce1b558b9f913e9618f5ba817559882e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-13 17:09:07 +0000

    doggo: update 0.5.4 bottle.

--- Formula/doggo.rb
+++ Formula/doggo.rb
@@ -7,12 +7,12 @@ class Doggo < Formula
   head "https://github.com/mr-karan/doggo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "046a13968d24a5a43fee494ab987c7568a3111905f807f2448aef4d1a0fc2b90"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "55a818010580cc97f183bd42a6279553733f6672de2dceb27ec56643ec0fd5a4"
-    sha256 cellar: :any_skip_relocation, monterey:       "07183902cc7b73f0a09a7d2ab6f11369d261d39805bc11577c026fcba44c64ce"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3f9d8e9ff1e45734211f8ebe02eebeb1c9daafaaa96f380f9fd9513d02e6445b"
-    sha256 cellar: :any_skip_relocation, catalina:       "764f07add2600a4c6df47f41aee089b444e03e18e2a4777a6a5969d0b9878572"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "08a993241db6f80d4ac98d617c4987962a6595c6dedcc0981798e85c14aab9a1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "047bdd261712857f6c8cf8644b33670da8fbfb83035589e06924e675f41a8617"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47203457278798c08c74e6e048f036e8ef1a3579c329a43de903f270374f16e8"
+    sha256 cellar: :any_skip_relocation, monterey:       "16e3025df96d2b684e0c946b089f63ead6876552213c23ccf04e0e0791880b75"
+    sha256 cellar: :any_skip_relocation, big_sur:        "56d48773f860eea3955bec5939c04e5dd1cb30ffdc5774711b0cb09c461b6c60"
+    sha256 cellar: :any_skip_relocation, catalina:       "6bbf6b52c703cd3ae160011c5d3df3d26baf39d08c4748be596bf5dd47782cd6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "656758ccedb107018e5cba479e134d17295fa939771162d8d2e001738dec89eb"
   end
 
   depends_on "go" => :build
