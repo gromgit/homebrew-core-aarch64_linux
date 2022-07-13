f64911e5d701fa830ade1e6a312b03ff1dd2c3a3
Ishaan Ahuja <Ishaan7190@gmail.com>
2022-07-13 16:50:03 -0500

    meek: revision
    * Meek 0.37.0: Full installation, new revision
    * Update meek.rb
    * Update meek.rb
    
    Closes #105762.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/meek.rb
+++ Formula/meek.rb
@@ -4,7 +4,7 @@ class Meek < Formula
   url "https://gitweb.torproject.org/pluggable-transports/meek.git/snapshot/meek-0.37.0.tar.gz"
   sha256 "f5650e26638f94954d0b89892ac0f4241cfeb55c17f555ee890609544ea85474"
   license "CC0-1.0"
-  revision 1
+  revision 2
   head "https://git.torproject.org/pluggable-transports/meek.git", branch: "main"
 
   bottle do
@@ -19,12 +19,14 @@ class Meek < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./meek-client"
-
-    man1.install "doc/meek-client.1" => "meek.1"
+    system "go", "build", *std_go_args(output: bin/"meek-client"), "./meek-client"
+    system "go", "build", *std_go_args(output: bin/"meek-server"), "./meek-server"
+    man1.install "doc/meek-client.1"
+    man1.install "doc/meek-server.1"
   end
 
   test do
-    assert_match "ENV-ERROR no TOR_PT_MANAGED_TRANSPORT_VER environment variable", shell_output("#{bin}/meek", 1)
+    assert_match "ENV-ERROR no TOR_PT_MANAGED_TRANSPORT_VER", shell_output("#{bin}/meek-client 2>/dev/null", 1)
+    assert_match "ENV-ERROR no TOR_PT_MANAGED_TRANSPORT_VER", shell_output("#{bin}/meek-server 2>/dev/null", 1)
   end
 end
