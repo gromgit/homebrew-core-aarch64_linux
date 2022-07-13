1d39a3bc8689f54ea7ea207174866c29a7df514c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-13 05:14:21 +0000

    difftastic 0.31.0
    * difftastic 0.31.0
    * update test
    
    Closes #105689.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/difftastic.rb
+++ Formula/difftastic.rb
@@ -1,8 +1,8 @@
 class Difftastic < Formula
   desc "Diff that understands syntax"
   homepage "https://github.com/Wilfred/difftastic"
-  url "https://github.com/Wilfred/difftastic/archive/refs/tags/0.30.0.tar.gz"
-  sha256 "2fc9e1f2ddde235794ebb1f3828b3674215db27af244d8fe6980fd21b20ec523"
+  url "https://github.com/Wilfred/difftastic/archive/refs/tags/0.31.0.tar.gz"
+  sha256 "0f6370aef09eed8cfd03add1b347dc2c3b84009493af7cbcf3ee5c12935cdac7"
   license "MIT"
 
   bottle do
@@ -30,7 +30,7 @@ class Difftastic < Formula
     (testpath/"a.py").write("print(42)\n")
     (testpath/"b.py").write("print(43)\n")
     expected = <<~EOS
-      b.py --- Python
+      a.py --- Python
       1 print(42) 1 print(43)\n
     EOS
     assert_equal expected, shell_output("#{bin}/difft --color never --width 80 a.py b.py")
