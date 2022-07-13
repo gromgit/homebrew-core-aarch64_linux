6a4c6b8d5d59973238aba719d5b89964a589828c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-13 21:50:23 +0800

    ola: revision bump (protobuf 21.2)
    Also, set `-DNDEBUG` to ensure we have a proper destructor for
    `InternalMetadata`.

--- Formula/ola.rb
+++ Formula/ola.rb
@@ -4,7 +4,7 @@ class Ola < Formula
   url "https://github.com/OpenLightingProject/ola/releases/download/0.10.8/ola-0.10.8.tar.gz"
   sha256 "102aa3114562a2a71dbf7f77d2a0fb9fc47acc35d6248a70b6e831365ca71b13"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
-  revision 5
+  revision 6
   head "https://github.com/OpenLightingProject/ola.git", branch: "master"
 
   bottle do
@@ -37,6 +37,9 @@ class Ola < Formula
   end
 
   def install
+    # https://github.com/protocolbuffers/protobuf/issues/9947
+    ENV.append_to_cflags "-DNDEBUG"
+
     args = %W[
       --disable-fatal-warnings
       --disable-dependency-tracking
