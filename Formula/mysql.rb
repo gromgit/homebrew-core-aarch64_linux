96e97725a25c0df8b04f4ff9085fe890af0a1cd5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-13 21:50:22 +0800

    mysql: revision bump (protobuf 21.2)

--- Formula/mysql.rb
+++ Formula/mysql.rb
@@ -4,6 +4,7 @@ class Mysql < Formula
   url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.29.tar.gz"
   sha256 "fd34a84c65fc7b15609d55b1f5d128c4d5543a6b95fa638569c3277c5c7bb048"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
+  revision 1
 
   livecheck do
     url "https://dev.mysql.com/downloads/mysql/?tpl=files&os=src"
