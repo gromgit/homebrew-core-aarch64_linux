976b6fe6cf6df65c795f3e04655f5f1fe2985e9b
danielnachun <daniel.nachun@gmail.com>
2022-07-13 23:43:07 -0700

    cf4ocl: fix build on Linux
    Closes #106245.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cf4ocl.rb
+++ Formula/cf4ocl.rb
@@ -21,8 +21,20 @@ class Cf4ocl < Formula
   depends_on "pkg-config" => :build
   depends_on "glib"
 
+  on_linux do
+    depends_on "opencl-headers" => :build
+    depends_on "ocl-icd"
+    depends_on "pocl"
+  end
+
+  # Fix build failure on Linux caused by undefined Windows-only constants.
+  # Upstreamed here: https://github.com/nunofachada/cf4ocl/pull/40
+  patch :DATA
+
   def install
-    system "cmake", ".", *std_cmake_args
+    args = *std_cmake_args
+    args << "-DBUILD_TESTS=OFF"
+    system "cmake", ".", *args
     system "make", "install"
   end
 
@@ -30,3 +42,38 @@ class Cf4ocl < Formula
     system bin/"ccl_devinfo"
   end
 end
+
+__END__
+diff --git a/src/lib/ccl_event_wrapper.c b/src/lib/ccl_event_wrapper.c
+index 0bfbf8a..0ba8bf9 100644
+--- a/src/lib/ccl_event_wrapper.c
++++ b/src/lib/ccl_event_wrapper.c
+@@ -282,6 +282,7 @@ const char* ccl_event_get_final_name(CCLEvent* evt) {
+ 			case CL_COMMAND_GL_FENCE_SYNC_OBJECT_KHR:
+ 				final_name = "GL_FENCE_SYNC_OBJECT_KHR";
+ 				break;
++            #if defined(__MSC_VER)
+ 			case CL_COMMAND_ACQUIRE_D3D10_OBJECTS_KHR:
+ 				final_name = "ACQUIRE_D3D10_OBJECTS_KHR";
+ 				break;
+@@ -300,6 +301,7 @@ const char* ccl_event_get_final_name(CCLEvent* evt) {
+ 			case CL_COMMAND_RELEASE_D3D11_OBJECTS_KHR:
+ 				final_name = "RELEASE_D3D11_OBJECTS_KHR";
+ 				break;
++            #endif
+ 			case CL_COMMAND_ACQUIRE_EGL_OBJECTS_KHR:
+ 				final_name = "ACQUIRE_EGL_OBJECTS_KHR";
+ 				break;
+diff --git a/src/lib/ccl_oclversions.h b/src/lib/ccl_oclversions.h
+index 4e82c9f..598a7e6 100644
+--- a/src/lib/ccl_oclversions.h
++++ b/src/lib/ccl_oclversions.h
+@@ -33,7 +33,7 @@
+ 	#include <OpenCL/opencl.h>
+ #else
+ 	#include <CL/opencl.h>
+-	#ifdef CL_VERSION_1_2
++	#if defined(CL_VERSION_1_2) && defined(__MSC_VER)
+ 		#include <CL/cl_dx9_media_sharing.h>
+ 	#endif
+ #endif
