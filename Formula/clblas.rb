5c00980910c01d24ae99fc5f2ab490be39112c32
danielnachun <daniel.nachun@gmail.com>
2022-07-13 23:01:01 -0700

    clblas: rebuild
    * clblas: fix build on Linux
    * clblas: fix build on Linux
    
    Closes #106235.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clblas.rb
+++ Formula/clblas.rb
@@ -19,6 +19,13 @@ class Clblas < Formula
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
+  depends_on "python@3.10" => :build
+
+  on_linux do
+    depends_on "opencl-headers" => [:build, :test]
+    depends_on "ocl-icd"
+    depends_on "pocl"
+  end
 
   def install
     system "cmake", "src", *std_cmake_args,
@@ -34,7 +41,8 @@ class Clblas < Formula
   test do
     # We do not run the test, as it fails on CI machines
     # ("clGetDeviceIDs() failed with -1")
+    opencl_lib = OS.mac? ? ["-framework", "OpenCL"] : ["-lOpenCL"]
     system ENV.cc, pkgshare/"example_srot.c", "-I#{include}", "-L#{lib}",
-                   "-lclBLAS", "-framework", "OpenCL", "-Wno-implicit-function-declaration"
+                   "-lclBLAS", *opencl_lib, "-Wno-implicit-function-declaration"
   end
 end
