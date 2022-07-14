4e5019863af8048d56c66cf85e39fc319f5a93a6
geeohgeegeeoh <ggm@pobox.com>
2022-07-15 08:23:39 +1000

    rpki-client 7.9
    * Update rpki-client.rb version 7.9
    * Update rpki-client.rb version 7.9
      Test needed changing, they made it more verbose by default (sigh)
    * Update rpki-client.rb version 7.9 update
      remove revision 1 line.
    
    Closes #105827.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rpki-client.rb
+++ Formula/rpki-client.rb
@@ -1,10 +1,9 @@
 class RpkiClient < Formula
   desc "OpenBSD portable rpki-client"
   homepage "https://www.rpki-client.org/index.html"
-  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-7.8.tar.gz"
-  sha256 "7a87a6fe7b1bd36a1ce277cf50e125ece7b2ed0236e252a66e2b34ca8f88b7f5"
+  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-7.9.tar.gz"
+  sha256 "accf531c885a9d95a37a6627399a59b360fa29a11810aba15b27d7526ce43e75"
   license "ISC"
-  revision 1
 
   bottle do
     sha256 arm64_monterey: "3e61ce6a33ccc4852aa3dba1bbfeeb256eeb84886db3b627e4c7ed2cfa3a0538"
@@ -35,6 +34,6 @@ class RpkiClient < Formula
   end
 
   test do
-    assert_match "parse file ta/", shell_output("#{sbin}/rpki-client -n -d . -R . 2>&1").lines.last
+    assert_match "VRP Entries: 0 (0 unique)", shell_output("#{sbin}/rpki-client -n -d . -R . 2>&1").lines.last
   end
 end
