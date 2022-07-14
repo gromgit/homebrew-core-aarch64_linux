90bc5783be3fba00801d57d13d5f36762e90b47b
danielnachun <daniel.nachun@gmail.com>
2022-07-13 23:04:30 -0700

    clblast: fix build on Linux
    Closes #106236.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clblast.rb
+++ Formula/clblast.rb
@@ -16,14 +16,21 @@ class Clblast < Formula
 
   depends_on "cmake" => :build
 
+  on_linux do
+    depends_on "opencl-headers" => [:build, :test]
+    depends_on "ocl-icd"
+    depends_on "pocl"
+  end
+
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
     system "make", "install"
     pkgshare.install "samples" # for a simple library linking test
   end
 
   test do
+    opencl_library = OS.mac? ? ["-framework", "OpenCL"] : ["-lOpenCL"]
     system ENV.cc, pkgshare/"samples/sgemm.c", "-I#{include}", "-L#{lib}",
-                   "-lclblast", "-framework", "OpenCL"
+                   "-lclblast", *opencl_library
   end
 end
