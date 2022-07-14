573c5a049088a51fb5013c5229679aa32a1fb7f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-14 05:25:27 +0000

    glooctl 1.11.22
    Closes #105794.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.11.21",
-      revision: "5789663694a97d66fef4d1ccb52f65be2d3f0bf0"
+      tag:      "v1.11.22",
+      revision: "2a526828b5fc7b685ca40a2922ba1bad83764253"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
