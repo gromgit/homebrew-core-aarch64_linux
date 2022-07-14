b0ea69208567a37c55a9b0f1258f353d41ad21a1
Rui Chen <rui@chenrui.dev>
2022-07-14 18:08:10 +0000

    snowflake: add 2.2.0 bottle.

--- Formula/snowflake.rb
+++ Formula/snowflake.rb
@@ -6,6 +6,15 @@ class Snowflake < Formula
   license "BSD-3-Clause"
   head "https://git.torproject.org/pluggable-transports/snowflake.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "158fcc636e9f65f1c768d2eba743aa068dd0cc78ac5d65f7d6119e025f719161"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "54b06a05f91cf300625badf1efb7e6dcde9361d24b401fe787dd9c97770aec36"
+    sha256 cellar: :any_skip_relocation, monterey:       "e9071fb2ddc7ebdc89e3d398842be1b8710c6839de51b7203409fc5a66a6e10b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bf503f6ff658f72770216d5bbf0347f7c912ea9bb1ade2e50037d6c2a0208965"
+    sha256 cellar: :any_skip_relocation, catalina:       "a3b47e2662ac5bc4d62bddb9452a22b7d5d5d20740f1c97d97cffaf6783178b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e829e1bd035833e3824a3159882d0a29e7a03d70a217dd48fd06db245412b7a"
+  end
+
   depends_on "go" => :build
 
   def install
