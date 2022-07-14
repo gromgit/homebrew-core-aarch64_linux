b151d892c5d215213efbb3fde9cef1e037741313
PaperChalice <lgamma@163.com>
2022-07-14 21:33:15 +0800

    cpp-httplib 0.10.9 (new formula)
    Closes #106003.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cpp-httplib.rb
@@ -0,0 +1,59 @@
+class CppHttplib < Formula
+  desc "C++ header-only HTTP/HTTPS server and client library"
+  homepage "https://github.com/yhirose/cpp-httplib"
+  url "https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.10.9.tar.gz"
+  sha256 "95ac0740ef760829a079c01a44164fd74af3fdc0748a40fc6beefd0276fd2345"
+  license "MIT"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+
+  def install
+    system "meson", "build", *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    (testpath/"server.cpp").write <<~CPP
+      #include <httplib.h>
+      using namespace httplib;
+
+      int main(void) {
+        Server svr;
+
+        svr.Get("/hi", [](const Request &, Response &res) {
+          res.set_content("Hello World!", "text/plain");
+        });
+
+        svr.listen("0.0.0.0", 8080);
+      }
+    CPP
+    (testpath/"client.cpp").write <<~CPP
+      #include <httplib.h>
+      #include <iostream>
+      using namespace httplib;
+      using namespace std;
+
+      int main(void) {
+        Client cli("localhost", 8080);
+        if (auto res = cli.Get("/hi")) {
+          cout << res->status << endl;
+          cout << res->get_header_value("Content-Type") << endl;
+          cout << res->body << endl;
+          return 0;
+        } else {
+          return 1;
+        }
+      }
+    CPP
+    system ENV.cxx, "server.cpp", "-I#{include}", "-lpthread", "-std=c++11", "-o", "server"
+    system ENV.cxx, "client.cpp", "-I#{include}", "-lpthread", "-std=c++11", "-o", "client"
+
+    fork do
+      exec "./server"
+    end
+    sleep 3
+    assert_match "Hello World!", shell_output("./client")
+  end
+end
