3a9b04c6ff33d397c4aa0a509bfa01df2984874e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-15 08:03:14 +0000

    protobuf@3: add 3.20.1 bottle.

--- Formula/protobuf@3.rb
+++ Formula/protobuf@3.rb
@@ -5,6 +5,15 @@ class ProtobufAT3 < Formula
   sha256 "3a400163728db996e8e8d21c7dfb3c239df54d0813270f086c4030addeae2fad"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "3951f5b59adfae94b4a6713dca404cb22e02b6a0d7bf59567920c45147a28af0"
+    sha256 cellar: :any,                 arm64_big_sur:  "3841a64bfe6cf8095d7cb282e10514a630fe979cb5b464957351771b6b747b8e"
+    sha256 cellar: :any,                 monterey:       "7e37643e16e29ad7fc9c68b91d389fb87e72d4f36d875b93e45a1da08fa1d22a"
+    sha256 cellar: :any,                 big_sur:        "651bbf4f120fb332c8d1ce7c8b73a8422357ac2c51d223a057140507cc8ae502"
+    sha256 cellar: :any,                 catalina:       "32f50ae919d727a1b633a50206370d032b553270373c4d13574707fb232ea151"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cc3cb502279e7153fba94fdfdb5fbd5ad431c2e9cbfc3928a0e8f570063bf3ff"
+  end
+
   keg_only :versioned_formula
 
   depends_on "python@3.10" => [:build, :test]
