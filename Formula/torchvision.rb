be5d75f7323c7e57f74699618e5c6ad36aad4f1e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-15 08:03:13 +0000

    torchvision: update 0.13.0_1 bottle.

--- Formula/torchvision.rb
+++ Formula/torchvision.rb
@@ -12,12 +12,12 @@ class Torchvision < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a8abc7dbfca08d51ad4ca48365d6a512ba3352d9e147e5aea9818175f0115003"
-    sha256 cellar: :any,                 arm64_big_sur:  "bd5a3236f787baf4cdb36e1c17b7c99c498d8369c4f707680657380ea23c8f01"
-    sha256 cellar: :any,                 monterey:       "17bc494ea07ffd5df509b27c170321853628a06d90b8b381606a5676a3fbf5a3"
-    sha256 cellar: :any,                 big_sur:        "d6e168be48958da4f48e80078275fbd3bc6d76734d89ed143efdd19b47855aaa"
-    sha256 cellar: :any,                 catalina:       "fc2bfe3cf064e88c096a2fb867be43cec9ffbaef7ecc9981e721c5e57cd2a1e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6159968886d9c1c86e4f1f2e63d1555179e282e9836bed0e7db92908f87d04a"
+    sha256 cellar: :any,                 arm64_monterey: "a62246f4fc7b3a9ae5c60fa639acbcb5e816bceafd8012d9c8b9043a4ab2fab9"
+    sha256 cellar: :any,                 arm64_big_sur:  "eeaf9f79fe88550ffcaceb197da8988d790be5ed9615a72782a29298d11486fe"
+    sha256 cellar: :any,                 monterey:       "ae5f08fc5d3ef71123240741bc0d52ca9fdf0b1172c49774b602aa8e1c2f3748"
+    sha256 cellar: :any,                 big_sur:        "0e2007b37beb9e4e93109d49c0ecd9a10b8693f4cc9321bc31c158cbd78d5d1b"
+    sha256 cellar: :any,                 catalina:       "a4b28b4758ef43a587ab8b2256d90e5226494e167c45f2553368b8fdd7e3c01a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75d663161fc5639845c3c8193215ebc54af2842790ab6fd2fab193062f0c3460"
   end
 
   depends_on "cmake" => :build
