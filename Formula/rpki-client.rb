767f2c1eef16e9db0d5ea2d8117746fa813940d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-15 07:04:46 +0000

    rpki-client: update 7.9 bottle.

--- Formula/rpki-client.rb
+++ Formula/rpki-client.rb
@@ -6,11 +6,11 @@ class RpkiClient < Formula
   license "ISC"
 
   bottle do
-    sha256 arm64_monterey: "3e61ce6a33ccc4852aa3dba1bbfeeb256eeb84886db3b627e4c7ed2cfa3a0538"
-    sha256 arm64_big_sur:  "0f982491437041f53160c0248e653c2e1e0d40ba4dd7c767168fcd8bab76ae5f"
-    sha256 monterey:       "00450aa3f5296beb2859a654b12fdf4c2f841755d5d07e91a8cefbc82f2ff35e"
-    sha256 big_sur:        "e2a3f21af703e38864d15c7fcde0e5acf0802bd4ddb292fd35761d414c5b5cba"
-    sha256 catalina:       "cf6086ae67b81b0dd49b1befafc83fb942377d5a5ab1f400a80db11de4a6e1d7"
+    sha256 arm64_monterey: "69384664e6a739e4a961434920a17a0b70033383c7c688cbb350af5ae06b01a6"
+    sha256 arm64_big_sur:  "1396185417a4287311398c2ee298dc1673a13beda4fc93c84ad5e537448a4e56"
+    sha256 monterey:       "9dee973ef3c51bb9ade9d0e0b154f09ddca26c42fed785a9356d5c780c0335ea"
+    sha256 big_sur:        "cc11ae071a0d37348fc60d4980551db9c4aa56dcc513fe32cab4a75bffedb974"
+    sha256 catalina:       "5b1e79efec36ee324bb3acd1f7e14bf58fd31be3b2355a752ac207ffbbbf0b6f"
   end
 
   depends_on "pkg-config" => :build
