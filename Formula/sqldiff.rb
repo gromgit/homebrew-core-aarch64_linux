999cade420ff5a439f38a7e16191bd1d40a51ef6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-15 17:22:36 +0000

    sqldiff: update 3.39.1 bottle.

--- Formula/sqldiff.rb
+++ Formula/sqldiff.rb
@@ -11,12 +11,12 @@ class Sqldiff < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0fa9f3587b40d9aa0c435d68c2a010d9de917bb86724160731f2161c8cc7516c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a2a597b137bf1b60ed245d4932d826072cafe88128a1a6c167edfb2f4acfc5e"
-    sha256 cellar: :any_skip_relocation, monterey:       "ad13051e58b7c3e4896a49b6da6c8ec2054d9021738d973c7115fdbf9fa8c0e9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "85d53b8785f2629e9f7c78f6c43fbe904047a48b927aa92952e061ae662fad35"
-    sha256 cellar: :any_skip_relocation, catalina:       "ca357355cf05ab18d49c24f6ee58d32f94931964a93be822d48a1797305505e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "361a72476e26366889ca4638d470fd74c1ea35366d7ed9fd77def6955b4f5bdd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "29f24cfe7960f1fe71a1b74b7ae83e2249d78982efc3f75911ffb8e3337c1266"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bf434836d8b579e565893d3047d1d6266b917d8feac1f80e991dbaf0abc6a31c"
+    sha256 cellar: :any_skip_relocation, monterey:       "9d8f644764184570a449cafb024d4b548b25d4a9afe4ac529d65a381d2951c88"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dcf376eaa73c8ef17e46cdd759df7d3655266d64278bdc8092b95110190f23bc"
+    sha256 cellar: :any_skip_relocation, catalina:       "91690f582cdfe95b30dcabee73afdfaf24b470e47c0301a34c663f42519af3d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8c38ab30b0b3b1d5d794c4d10f996e58f09a8b9577a11d950dc6a4405cf0131"
   end
 
   uses_from_macos "tcl-tk" => :build
