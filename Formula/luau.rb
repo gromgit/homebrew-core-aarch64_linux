88611365832a2be1c3922e146dacb2f7cb455b64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-15 05:40:16 +0000

    luau 0.536
    Closes #105859.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -1,8 +1,8 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau-lang.org"
-  url "https://github.com/Roblox/luau/archive/0.535.tar.gz"
-  sha256 "2acb39fb00b4836226033a3ef17665c71e30e6bd62550941710d66e286ff7a9e"
+  url "https://github.com/Roblox/luau/archive/0.536.tar.gz"
+  sha256 "e6de36e83e9c537d92bcc94852ab498e3c15a310fd2c4cc4e21c616d8ae1a84f"
   license "MIT"
   head "https://github.com/Roblox/luau.git", branch: "master"
 
