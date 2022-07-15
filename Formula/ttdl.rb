ac94f9940f4357cf32a6fdf4e0cda8f5bd659e01
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-07-15 12:15:43 -0700

    ttdl 3.3.0 (new formula)
    Closes #105882.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/ttdl.rb
@@ -0,0 +1,19 @@
+class Ttdl < Formula
+  desc "Terminal Todo List Manager"
+  homepage "https://github.com/VladimirMarkelov/ttdl"
+  url "https://github.com/VladimirMarkelov/ttdl/archive/refs/tags/v3.3.0.tar.gz"
+  sha256 "b32ad96b84bec3e7aa3daa5414a82eaa00c509cdc4211c3bc81fb570a54a8c03"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "Added todo", shell_output("#{bin}/ttdl 'add readme due:tomorrow'")
+    assert_predicate testpath/"todo.txt", :exist?
+    assert_match "add readme", shell_output("#{bin}/ttdl list")
+  end
+end
