484a7019b50e3baa814b294f2196aa9e5947d416
Rui Chen <rui@chenrui.dev>
2022-07-15 22:34:46 +0000

    mkp224o: add 1.6.1 bottle.

--- Formula/mkp224o.rb
+++ Formula/mkp224o.rb
@@ -6,6 +6,15 @@ class Mkp224o < Formula
   license "CC0-1.0"
   head "https://github.com/cathugger/mkp224o.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "5fd449e1ed8792a81732494b7362c75507fa96567b5b1248c76522193762a3d0"
+    sha256 cellar: :any,                 arm64_big_sur:  "77672818a99d4b11411863bd952acf9472b1b2b89415839c41600c8c7bae3cfd"
+    sha256 cellar: :any,                 monterey:       "3cef4d9204063c22271e2faca7e67b7b603ba5c2e4270431933d6715b7048dd7"
+    sha256 cellar: :any,                 big_sur:        "a3766fdff5c7010b3995300f688a0545961a19bec4137aad2c5304d3f849da85"
+    sha256 cellar: :any,                 catalina:       "4133de045c807282326009a66f7bc1d5e56be43d75cd2be4201ac450391add06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13c559ae3f988d2a848f7932862b22a03820ce0f2d1409d67c96c41762a74b16"
+  end
+
   depends_on "libsodium"
 
   def install
