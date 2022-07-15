e302ca5d917a629057f14e9408c543de929b7023
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-15 08:03:20 +0000

    osm-pbf: update 1.5.0_4 bottle.

--- Formula/osm-pbf.rb
+++ Formula/osm-pbf.rb
@@ -7,12 +7,12 @@ class OsmPbf < Formula
   revision 4
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7c54ae6cece29667791513344f8ca1f07412dbf7c0030eb000c835864dda96b7"
-    sha256 cellar: :any,                 arm64_big_sur:  "91108e1f36a68528f39a4c90a15b21de280f25e1846b7b7d2014e056d8373308"
-    sha256 cellar: :any,                 monterey:       "df65cb7860d85aaf7f2b0273cd4bd36438440120e275580c72cb118051bf63ee"
-    sha256 cellar: :any,                 big_sur:        "3e563c0635212cdbad3330addbbbb32e054ccb64267d8dc28b2c79ce223d1c2a"
-    sha256 cellar: :any,                 catalina:       "4e43efc37899fb2ea13d831f24ac192fec230a9265e25337fbbffdb4c5196914"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "72f304c07b272e036f0aea9601c22d1e06cb44376bebb4d26cdefdf994c1d429"
+    sha256 cellar: :any,                 arm64_monterey: "0b4a492003e5dd41f96fbc40ca087cc64f7651992b65b7028434bf3c4c20eb74"
+    sha256 cellar: :any,                 arm64_big_sur:  "b7db0864c304ea5d04094b88c0fa078d49424fa98950aed10f40ed596ef9b194"
+    sha256 cellar: :any,                 monterey:       "c9c32991c08e164406494c12a5c3c9ad3b46ee249386cb5777068b1d1b12e80b"
+    sha256 cellar: :any,                 big_sur:        "2a1309dd7c2a7634add17179732f452415febfb6b2a39b8f498b57f4d8ee222d"
+    sha256 cellar: :any,                 catalina:       "6cea77ead1d01a4516a2a8e20a38a0dde0024eade075fa0738ba0f6910bb5df6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1467f12dbe00979de9bfc47c37b59d2b918938a78ae9d476091902f886685f44"
   end
 
   depends_on "cmake" => :build
