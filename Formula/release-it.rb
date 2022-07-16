0c0f8687381fb9f9cde653cd7bbb12ceb17dcce1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-16 13:09:20 +0000

    release-it: update 15.1.2 bottle.

--- Formula/release-it.rb
+++ Formula/release-it.rb
@@ -8,12 +8,12 @@ class ReleaseIt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ad895947779ba9a8fa398d2ceb4a8000d17022eca0adfbb0c04488462108d3c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5ad895947779ba9a8fa398d2ceb4a8000d17022eca0adfbb0c04488462108d3c"
-    sha256 cellar: :any_skip_relocation, monterey:       "2d3929e8dd1ba8f998ab0ba1719820fa04b0bda55a9ec09b9fc4c44ff1fa7bff"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2d3929e8dd1ba8f998ab0ba1719820fa04b0bda55a9ec09b9fc4c44ff1fa7bff"
-    sha256 cellar: :any_skip_relocation, catalina:       "2d3929e8dd1ba8f998ab0ba1719820fa04b0bda55a9ec09b9fc4c44ff1fa7bff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5ad895947779ba9a8fa398d2ceb4a8000d17022eca0adfbb0c04488462108d3c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5829fb235e79906c5ddc034216e21100995810df516910a0a947d9971eb1410f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5829fb235e79906c5ddc034216e21100995810df516910a0a947d9971eb1410f"
+    sha256 cellar: :any_skip_relocation, monterey:       "c11ab3963e56dbaa265bfe6f5ed2ebf0980bd73dc065d02987572d39c0632202"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c11ab3963e56dbaa265bfe6f5ed2ebf0980bd73dc065d02987572d39c0632202"
+    sha256 cellar: :any_skip_relocation, catalina:       "c11ab3963e56dbaa265bfe6f5ed2ebf0980bd73dc065d02987572d39c0632202"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5829fb235e79906c5ddc034216e21100995810df516910a0a947d9971eb1410f"
   end
 
   depends_on "node"
