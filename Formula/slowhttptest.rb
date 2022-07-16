ea75730a208ed5093788001f3ef0f9a98e0a3a3e
Rui Chen <rui@chenrui.dev>
2022-07-16 01:09:05 -0400

    slowhttptest 1.9.0
    remove build patch
    
    Closes #105942.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/slowhttptest.rb
+++ Formula/slowhttptest.rb
@@ -1,10 +1,9 @@
 class Slowhttptest < Formula
   desc "Simulates application layer denial of service attacks"
   homepage "https://github.com/shekyan/slowhttptest"
-  url "https://github.com/shekyan/slowhttptest/archive/v1.8.2.tar.gz"
-  sha256 "faa83dc45e55c28a88d3cca53d2904d4059fe46d86eca9fde7ee9061f37c0d80"
+  url "https://github.com/shekyan/slowhttptest/archive/v1.9.0.tar.gz"
+  sha256 "a3910b9b844e05ee55838aa17beddc6aa9d6c5c0012eab647a21cc9ccd6c8749"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/shekyan/slowhttptest.git", branch: "master"
 
   bottle do
@@ -18,15 +17,9 @@ class Slowhttptest < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f6da4dc3f06b54d5bb48e22ef34810b779c950bfb108cc49ee8970975030879"
   end
 
-  # Remove these in next version
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-
   depends_on "openssl@1.1"
 
   def install
-    inreplace "configure.ac", "1.8.1", "1.8.2"
-    system "autoconf" # Remove in next version
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
