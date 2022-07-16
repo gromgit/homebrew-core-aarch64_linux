93a09f5c407abd5c797cd8a0716697465ce4085e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-16 15:04:30 +0000

    libnl: update 3.7.0 bottle.

--- Formula/libnl.rb
+++ Formula/libnl.rb
@@ -6,7 +6,7 @@ class Libnl < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 x86_64_linux: "2b24a546a772b9abc57a7662eb2f427800e9fbcd744c4709667d0a5b26c88435"
+    sha256 x86_64_linux: "17a90fc0e510aeaee7135f7ddf604e29c95d4d0dc1f71c3a23d6febd3b48e79b"
   end
 
   depends_on "pkg-config" => :test
