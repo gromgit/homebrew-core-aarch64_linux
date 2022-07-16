cf44313b9d9850af67e197014317456ad4ee775b
Rui Chen <rui@chenrui.dev>
2022-07-16 01:29:56 -0400

    tio 1.45
    Closes #105952.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tio.rb
+++ Formula/tio.rb
@@ -1,8 +1,8 @@
 class Tio < Formula
   desc "Simple TTY terminal I/O application"
   homepage "https://tio.github.io"
-  url "https://github.com/tio/tio/releases/download/v1.44/tio-1.44.tar.xz"
-  sha256 "3d3e20ecc44ed674816d2d0421cce42c1a7af96753d3b3bc1d7b7f6b03192cd0"
+  url "https://github.com/tio/tio/releases/download/v1.45/tio-1.45.tar.xz"
+  sha256 "4e72a946792346f86504865ed2923688f4dd9281905bb81e76ba3fd6f3b96014"
   license "GPL-2.0-or-later"
   head "https://github.com/tio/tio.git", branch: "master"
 
@@ -19,13 +19,6 @@ class Tio < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "inih"
-
-  # PR, https://github.com/tio/tio/pull/159
-  # remove in next release
-  patch do
-    url "https://github.com/tio/tio/commit/223f0c5d1304dd6295c77313fb6bd0c156755b62.patch?full_index=1"
-    sha256 "09459cd348fd3d451a82e9712599f82de5dc7457270147228bba1beed7b1545f"
-  end
 
   def install
     mkdir "build" do
