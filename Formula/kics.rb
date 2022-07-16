f282f82d99428e7cbd5b145553b716964368f713
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-16 15:05:06 +0000

    kics: update 1.5.12 bottle.

--- Formula/kics.rb
+++ Formula/kics.rb
@@ -12,12 +12,12 @@ class Kics < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9afd2b5105dd46a489fd06510689461728c1f39717db8a0dcfa9df673739aafb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d6ca5307dc87de438f33933855d21f132ea20af37578e935692561555ce4f9a"
-    sha256 cellar: :any_skip_relocation, monterey:       "cb235a66bf26b2488277988902822039e40325555708b333315ddeac169fbe84"
-    sha256 cellar: :any_skip_relocation, big_sur:        "87d669fa25a49911927d17d3fb9176907b434a2ddd10c7c093d11662cb4ec943"
-    sha256 cellar: :any_skip_relocation, catalina:       "274fcf2837dfc2ce6a86b6d635dde16ca950d9e5f5959f85b267e7da0a5ec7e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "731d604db538852b2f525c9c0ad520d527982152251cb24b8f4c7fe282c8dfb2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "62ca8ce784a30bf457b937068671ee664f9fc6533904b8b1ac03e48f684e6964"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "07f6ec5db45c5f97e1c7581596980130b70eb1290b090daaf3c881617fdfc534"
+    sha256 cellar: :any_skip_relocation, monterey:       "aa6c3f6d5199c9713e8e24a462664329e9573ea538ede7e590177be63ee551a6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a88d88c348e7c9ba5fd30359d61536c48c7ee69792e82e79bdb0983ff8455086"
+    sha256 cellar: :any_skip_relocation, catalina:       "1be1d82015962a8eccdcc08c169caac8f80b949e391556c1fdec64c374e70b44"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8aaf3cf8c3ebab246e03f841a3fc9bee16018643fe6025dfbb835243717380de"
   end
 
   depends_on "go" => :build
