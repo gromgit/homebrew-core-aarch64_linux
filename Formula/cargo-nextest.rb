35d87baa88b8f7ff6d92a7595c4d524ab059c766
Andy Freeland <andy@andyfreeland.net>
2022-07-16 00:52:28 -0700

    cargo-nextest 0.9.26 (new formula)
    * cargo-nextest 0.9.26 (new formula)
    * Update Formula/cargo-nextest.rb
    
    Closes #105954.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cargo-nextest.rb
@@ -0,0 +1,37 @@
+class CargoNextest < Formula
+  desc "Next-generation test runner for Rust"
+  homepage "https://nexte.st"
+  url "https://github.com/nextest-rs/nextest/archive/refs/tags/cargo-nextest-0.9.26.tar.gz"
+  sha256 "2d06259263e01cbde94efbe1aa489889143539de5e5f84141dac833a4ad0e89a"
+  license "Apache-2.0"
+
+  depends_on "rust" # uses `cargo` at runtime
+
+  def install
+    system "cargo", "install", "--no-default-features", "--features", "default-no-update",
+                    *std_cargo_args(path: "cargo-nextest")
+  end
+
+  test do
+    crate = testpath/"demo-crate"
+    mkdir crate do
+      (crate/"src/main.rs").write <<~EOS
+        #[cfg(test)]
+        mod tests {
+          #[test]
+          fn test_it() {
+            assert_eq!(1 + 1, 2);
+          }
+        }
+      EOS
+      (crate/"Cargo.toml").write <<~EOS
+        [package]
+        name = "demo-crate"
+        version = "0.1.0"
+      EOS
+
+      output = shell_output("cargo nextest run 2>&1")
+      assert_match "Starting 1 tests across 1 binaries", output
+    end
+  end
+end
