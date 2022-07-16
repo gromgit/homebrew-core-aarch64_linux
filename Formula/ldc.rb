21e4f6fc7cc34c6bc96508bfe8896822bd6b0802
Rylan Polster <rslpolster@gmail.com>
2022-07-16 02:34:51 +0200

    ldc: use `on_{system}` blocks
    Generated using `brew style --fix` and then updated manually if needed

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -33,10 +33,12 @@ class Ldc < Formula
 
   resource "ldc-bootstrap" do
     on_macos do
-      if Hardware::CPU.intel?
+      on_intel do
         url "https://github.com/ldc-developers/ldc/releases/download/v1.28.1/ldc2-1.28.1-osx-x86_64.tar.xz"
         sha256 "9aa43e84d94378f3865f69b08041331c688e031dd2c5f340eb1f3e30bdea626c"
-      else
+      end
+
+      on_arm do
         url "https://github.com/ldc-developers/ldc/releases/download/v1.28.1/ldc2-1.28.1-osx-arm64.tar.xz"
         sha256 "9bddeb1b2c277019cf116b2572b5ee1819d9f99fe63602c869ebe42ffb813aed"
       end
