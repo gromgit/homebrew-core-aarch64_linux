c78a2cedc3afb2c0a0ef6239a2cfb7d5e8e67395
Rui Chen <rui@chenrui.dev>
2022-07-15 23:43:50 -0400

    libnl 3.7.0
    Closes #105922.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libnl.rb
+++ Formula/libnl.rb
@@ -1,8 +1,8 @@
 class Libnl < Formula
   desc "Netlink Library Suite"
   homepage "https://github.com/thom311/libnl"
-  url "https://github.com/thom311/libnl/releases/download/libnl3_6_0/libnl-3.6.0.tar.gz"
-  sha256 "532155fd011e5a805bd67121b87a01c757e2bb24112ac17e69cb86013b970009"
+  url "https://github.com/thom311/libnl/releases/download/libnl3_7_0/libnl-3.7.0.tar.gz"
+  sha256 "9fe43ccbeeea72c653bdcf8c93332583135cda46a79507bfd0a483bb57f65939"
   license "LGPL-2.1-or-later"
 
   bottle do
