bedc1492d26b3872167262459f16c87256d82926
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-16 07:04:44 +0000

    python-markdown: update 3.4.1 bottle.

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -9,12 +9,12 @@ class PythonMarkdown < Formula
   head "https://github.com/Python-Markdown/markdown.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "28d3ac4ef942f4b316f5f7fc70fd5e84d2f11854d983fad86ffbf5088d9d52d5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ecf032f9bcd6c4951c8460933b5ce77929b9921aabcccdb902d89cd7947b5100"
-    sha256 cellar: :any_skip_relocation, monterey:       "7c0fc31fedf83687330b982bf5cc8a0f8ab37bdfe1237866832ba1794e90d0db"
-    sha256 cellar: :any_skip_relocation, big_sur:        "43cd65bb521ae69854123efe0c67581be69782b9f962ee66c31e95ac4ea943b6"
-    sha256 cellar: :any_skip_relocation, catalina:       "80bb1c032f4eb2c2c6321623f273ff90aa04d1ae6b9a32dce48b80678ef7ab88"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd8cf1bc63bd2effe8e6f4ae7ba0f6680e4f0d238c7e969b6fdf98994b83e67f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2769293a7454b35d7f75479ad103d707a0381bcfdc33bbb29959c12aefa9e1b9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ff572b2a634ff50fd3f3ed624abc1373688b69a4fc86df145551f9de65883d74"
+    sha256 cellar: :any_skip_relocation, monterey:       "94a10dbb13fd3ef84d4e17fc7a05e1cbbc35df891f162238dcdd0d6c4d9deed7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "79e8b93951a7398cc782fc44eb631112a512017852b9aa8513a61e677cbb9272"
+    sha256 cellar: :any_skip_relocation, catalina:       "51a5b491a577e86a32e8b54f4be3cc7aa46a89e8d832cf57323afda27f43a8b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2badf01076b76fa01791be39f2db662763a5254ca1a715eab4060b178eea583"
   end
 
   depends_on "python@3.10"
