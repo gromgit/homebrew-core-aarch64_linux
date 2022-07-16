41c73d795e0f0f34f18d144f90822aafad59a9bd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-16 15:04:31 +0000

    questdb: update 6.4.3 bottle.

--- Formula/questdb.rb
+++ Formula/questdb.rb
@@ -6,7 +6,7 @@ class Questdb < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "aa6e20d04b1e79e4222b42df056bd2effb9908b04d1044b6764abffac95dc1b6"
+    sha256 cellar: :any_skip_relocation, all: "b28b1aa2d4c0fe6750e23031d29891b5157f9784e2e225e45e8e30c4b42dbede"
   end
 
   depends_on "openjdk@11"
