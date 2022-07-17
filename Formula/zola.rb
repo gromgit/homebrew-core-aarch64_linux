bb47bb4a1c9dc5cbb300ef1299ba3de47447660a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-17 10:04:47 +0000

    zola: update 0.16.0 bottle.

--- Formula/zola.rb
+++ Formula/zola.rb
@@ -6,12 +6,12 @@ class Zola < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "63760f215220bd08487475ebaf4f80ab0b273bb55457edcc6b04b56caff5fb65"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "139d609fcfac6874762aa4d087c38408e2bae0b8c4ff01111503f64300a50aae"
-    sha256 cellar: :any_skip_relocation, monterey:       "1a27978ece92ac0d189fb774c4eafb6001b7bace4a85530b3fe3e8843670ca65"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ebfaee43ae5ce7653c4efc97abbf9c26af1141443b4af31863236b684786b0fd"
-    sha256 cellar: :any_skip_relocation, catalina:       "903262361a0baa26ceb1dd259e987b89cdc9404437138c891f3af40379c8de6c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d4b9cd2103cc6584e1aeef9d2976a0f269f3feae4ce15edb454ad8740938984"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a7e7fcf273e9278c89dd3d5be63d64b85cc9f1ed7154e9776fe8d397cec0ac11"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5ab3ed9f431decf8808065e32876df4150e1c15fa65c99ff2dc3c3510a470805"
+    sha256 cellar: :any_skip_relocation, monterey:       "9bde7931286b6c622527f948e1accf0a2e00d722105ef0d8bb6dd9b361a137c0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "34cbf42be25d08000e2918169bff997de69b263192bbc951b463bcd79037e66f"
+    sha256 cellar: :any_skip_relocation, catalina:       "5c9c4ff1a9517b7c2ff742c5496be4f98333a7a62bdc6cbdc589ceb3eb541ab4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1e39a314568faecc92a6a464344e31174e2d4c9150d9206842c95f3146d5cfe"
   end
 
   depends_on "cmake" => :build
