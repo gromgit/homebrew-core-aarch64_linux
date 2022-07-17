814310804a7d6af5173b19962b30545e9b9c229e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-17 14:04:19 +0000

    asio: update 1.22.2 bottle.

--- Formula/asio.rb
+++ Formula/asio.rb
@@ -12,12 +12,12 @@ class Asio < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b3ea7931fb9e539f59bbf21f4647268487036aace40cda956d08507fe87bb90b"
-    sha256 cellar: :any,                 arm64_big_sur:  "5a8c945335115a2da264ec74162e57a5d92f4064544899c4df375a3cabc5e06a"
-    sha256 cellar: :any,                 monterey:       "98f1add58eeb5d6eb9d886781ef62dd0b6572dd8252252acdaa2bc1d3674c875"
-    sha256 cellar: :any,                 big_sur:        "dc03f2f6433eadeb86832d94a241c36a5be0eb23afde03644a11a62403a9b795"
-    sha256 cellar: :any,                 catalina:       "7c8ae5863a8abf20d8d811ec943c8bff0e702b7dc4e79ebbab9310bcc7a99e44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "723acb5f60519bdc4426ac9d6e4e5689f94c794b615b6f71759d26eda1385df1"
+    sha256 cellar: :any,                 arm64_monterey: "1773aecd9c44d0809d41b366077d373ba5dd3c9744d893781354688d854562eb"
+    sha256 cellar: :any,                 arm64_big_sur:  "d68afb7c39e98f18f94fc0904a8ba8177ec54bc2ee1842ffcebcabac5aeb390e"
+    sha256 cellar: :any,                 monterey:       "0d4b2e8d1622d360f32dc35200e034d7d1d54718a91755227e5f2ae630da96a6"
+    sha256 cellar: :any,                 big_sur:        "75273b50ad4246511b3f3980f657a4b695ced63270b839884bda21cde0eb7c15"
+    sha256 cellar: :any,                 catalina:       "c53c6dae2288924630d7349d714d1c1ca803e609269d33a60c671916c1c02a04"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6263dc3ea91618ba50a05da33771e44ef5f19119d6f9ecc7ac252726a66f01a6"
   end
 
   depends_on "autoconf" => :build
