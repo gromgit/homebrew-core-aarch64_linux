d657d60b748faca86687c30eaf3f11bbb2efeb16
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-17 15:15:52 +0000

    cargo-udeps: add 0.1.30 bottle.

--- Formula/cargo-udeps.rb
+++ Formula/cargo-udeps.rb
@@ -5,6 +5,15 @@ class CargoUdeps < Formula
   sha256 "a1dd8b533fa915783919b78705d4a377fb021b67f8386023866017a139caf935"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "8c7e006cb049e371afab1a3257a04927618f879712320f8dd1e594de14a5c423"
+    sha256 cellar: :any,                 arm64_big_sur:  "8ac32a45c4f270ff2086ee67f3c0c9d1aed6b4bbaaaf35de024330c4f0a81d92"
+    sha256 cellar: :any,                 monterey:       "208a877aeaf835e33c3ffb727bae9656921e401976aa51455c913b0d6a2b3713"
+    sha256 cellar: :any,                 big_sur:        "8dbe2ba1c06246158c9c2afde166d3d9a6171896752fb4f3b71c6512c2667496"
+    sha256 cellar: :any,                 catalina:       "25669c49fab35c58d74a0378a7e5e1692a6ec1f71bbc0a385c8704ac1f3786e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "683e4363ea5f54a95ce6dceb70a187eb2850c8b21d3997959bd0cd50e4927732"
+  end
+
   depends_on "rust" => [:build, :test]
 
   on_linux do
