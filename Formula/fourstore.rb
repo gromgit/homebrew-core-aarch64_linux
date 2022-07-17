452e0f1445ed52fbf5f453ac534204980b7cebab
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-07-17 23:16:54 +0000

    fourstore: update 1.1.6_2 bottle.

--- Formula/fourstore.rb
+++ Formula/fourstore.rb
@@ -7,15 +7,12 @@ class Fourstore < Formula
   revision 2
 
   bottle do
-    sha256 arm64_monterey: "a58ee8282103b2789731c582a0c71cef2c97b65cb0f2d9d488fc969823b1c29a"
-    sha256 arm64_big_sur:  "53cd222505740f7e67439b25598c301b08e8cbe0a66f5d6f1ad31a470ca1d9a1"
-    sha256 monterey:       "bec773a591ca23a508f2e9a5e9f9726d45ab3d2304ad83bf55f39e7b836b38fd"
-    sha256 big_sur:        "5b3bb657725c208e1b2692ca91b8964d4a26590f1da418d5ea24621994c7245c"
-    sha256 catalina:       "e8414af069796d108f66d9676366de2cb6f82d4dbb694a7cbc8ede58e5c646ff"
-    sha256 mojave:         "9743086b58984f5b97820b6c591edcbf551f60ae49c9f655c577f7ca50e3de23"
-    sha256 high_sierra:    "abfb1051513308bda54e9e7a6c3e62dad74feafa46e6b739fb33405efb0153e4"
-    sha256 sierra:         "7a89fcec9a355a501880289ab072d95219e760aad3102f9332a2e7310346155f"
-    sha256 x86_64_linux:   "89ea77f8554486bf70bc2685fb5825f10328c40434084010d9c24bce233a2b8c"
+    sha256 arm64_monterey: "7aed20fb578c260f7d1764679d6ecaad032d68dd6bdacca012002ea815509a4a"
+    sha256 arm64_big_sur:  "ad26cc8bd35c75d1988f97de3fbf3281061afc8002a082d61e99800168c5f4c3"
+    sha256 monterey:       "6323983ec1a097a3a6a0473232531037223957d4c41ff2a5514315a7a21fb1af"
+    sha256 big_sur:        "045a48f298bedfd23a4fc9d93191de328f686c4fea889a926e5f8b2d6e65baa7"
+    sha256 catalina:       "e292048a085d8583547af82380dfcadbb92c15d28e56a75ae909ef853d363391"
+    sha256 x86_64_linux:   "5a43f121222346aa9f89b8ed1609656ab0fbb696d05f719989ae751e0cae34b2"
   end
 
   depends_on "autoconf" => :build
