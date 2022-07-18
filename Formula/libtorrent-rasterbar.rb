1c01189e1965ceead59d9271204356f4c5d2312e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-18 08:06:10 +0000

    libtorrent-rasterbar: update 2.0.7 bottle.

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -12,12 +12,12 @@ class LibtorrentRasterbar < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "dd1dc2a54c6342429f965f3f3537e09ab9e18ba139124e049772199afcd25461"
-    sha256 cellar: :any,                 arm64_big_sur:  "b88584145df91d1b1e6aa7f2e4f06cfee6e36bd005c30b331d823c6e6e78c1f3"
-    sha256 cellar: :any,                 monterey:       "e53ecdcd88a1034d7d1545477a1264bd392570e68ad19c0fc20d9fe2711c703f"
-    sha256 cellar: :any,                 big_sur:        "73dd5ca5761d81cc96ea6046e323879e7b7f81296deabb58664499f696dee87b"
-    sha256 cellar: :any,                 catalina:       "7257ac59c530b06f2fa48f26b8e1580ae480c40c6af6826955985b57b9acae3b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed2dc5e2e3e1f4115b66c6a24bf779e37e8d2f17854dac20bc0ca3d7dc2b9939"
+    sha256 cellar: :any,                 arm64_monterey: "71f49d2a6b339da7f99879697340c8c11b4ee5d006008ef427ad44357ac6babd"
+    sha256 cellar: :any,                 arm64_big_sur:  "80bb116e01e4c70adbf4290280d647442cb659bdb66b84bf589d67f4a8def5ad"
+    sha256 cellar: :any,                 monterey:       "b8927cbc31325e6d03aada3fff69c256a1a572bccaa852de6e76c11a4f5d768b"
+    sha256 cellar: :any,                 big_sur:        "1f7cd2a3c57ba4254e9243ca79704a22ff5fae699b8cde5abee0f390da8b5cfd"
+    sha256 cellar: :any,                 catalina:       "df1666bc529a90e5d2883ffaa581cc767f1b7a661922f46c6dd8012b01cee73f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41daca9c42f9b5faae184ac94afe14048f2194782776f8b3454b5345d096ce62"
   end
 
   depends_on "cmake" => :build
