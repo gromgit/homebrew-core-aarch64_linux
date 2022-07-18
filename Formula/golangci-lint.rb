7fa92a104d8e316563db12d2dfaeaf8c174af725
jidicula <johanan@forcepush.tech>
2022-07-18 08:05:44 -0400

    golangci-lint 1.47.0
    Closes #106063.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -2,8 +2,8 @@ class GolangciLint < Formula
   desc "Fast linters runner for Go"
   homepage "https://golangci-lint.run/"
   url "https://github.com/golangci/golangci-lint.git",
-      tag:      "v1.46.2",
-      revision: "a3336890904cd3efa4f1c7e3f82ce207fe125a6f"
+      tag:      "v1.47.0",
+      revision: "b415402720aa2f54f85993669d264cdae703bd7a"
   license "GPL-3.0-only"
   head "https://github.com/golangci/golangci-lint.git", branch: "master"
 
