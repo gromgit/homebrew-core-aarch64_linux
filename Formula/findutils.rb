4218ddc84d3e8e2fadf6072fc391d576dbe4db1c
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-07-18 11:42:12 +0200

    findutils: remove old hacks

--- Formula/findutils.rb
+++ Formula/findutils.rb
@@ -16,19 +16,6 @@ class Findutils < Formula
   end
 
   def install
-    # Work around unremovable, nested dirs bug that affects lots of
-    # GNU projects. See:
-    # https://github.com/Homebrew/homebrew/issues/45273
-    # https://github.com/Homebrew/homebrew/issues/44993
-    # This is thought to be an el_capitan bug:
-    # https://lists.gnu.org/archive/html/bug-tar/2015-10/msg00017.html
-    ENV["gl_cv_func_getcwd_abort_bug"] = "no" if MacOS.version == :el_capitan
-
-    # Workaround for build failures in 4.8.0
-    # https://lists.gnu.org/archive/html/bug-findutils/2021-01/msg00050.html
-    # https://lists.gnu.org/archive/html/bug-findutils/2021-01/msg00051.html
-    ENV.append "CFLAGS", "-D__nonnull\\(params\\)="
-
     args = %W[
       --prefix=#{prefix}
       --localstatedir=#{var}/locate
