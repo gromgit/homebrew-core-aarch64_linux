8f291cf83f90bb0b5862fd4110b2fd585c468cdc
Porkepix <porkepix@gmail.com>
2022-07-19 10:59:56 +0200

    fnm 1.31.1
    Closes #106121.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fnm.rb
+++ Formula/fnm.rb
@@ -1,8 +1,8 @@
 class Fnm < Formula
   desc "Fast and simple Node.js version manager"
   homepage "https://github.com/Schniz/fnm"
-  url "https://github.com/Schniz/fnm/archive/v1.31.0.tar.gz"
-  sha256 "c501220d8469f0082c0813d172171f1a19e4d2c4999573bf51a23094773e7041"
+  url "https://github.com/Schniz/fnm/archive/v1.31.1.tar.gz"
+  sha256 "ddb7bde503ef990c95c762863f4c858499f17c00d8e6ded7885b4fbbf1600250"
   license "GPL-3.0-only"
   head "https://github.com/Schniz/fnm.git", branch: "master"
 
