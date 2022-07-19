3ca1297f4f2cb6b65055c59a2b338d1eab778088
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-19 20:04:48 +0000

    kumactl: update 1.7.1 bottle.

--- Formula/kumactl.rb
+++ Formula/kumactl.rb
@@ -11,12 +11,12 @@ class Kumactl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c1737f54d79bb9b13f129d467e629e8dbd3d48767f08695df57a3862b3c1a225"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a46df0ed8dcb4bc791245bd6d98a42c6d3baac74990e7d45d26eddf4ff012d11"
-    sha256 cellar: :any_skip_relocation, monterey:       "0cbd339e275645f07407567f23d1425921bb1db9f397ae3fa4bf39f8ffe549bf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6bf0f70c291bde3d0f62acbfd4e230ca8307daaa1242fb4eecfd268005b35982"
-    sha256 cellar: :any_skip_relocation, catalina:       "9495203eba813725b643798cd04adb7f78282f82d1fc59b5b8e8d70faa307239"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1aaa59c7e5b3592d2c15c47664c2ba87d2c0e3f61e53666835fa47fdb7a758bf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b312eec84a89cbd47c3682082ec0539e4e2fa7ecfb50c13f8d7cf573436d218"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "73a226f5864e8e6dcb18db989edf344025ea4322e0cf406cf1b7bd73b341f71c"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a2ca244d7af918cad7d3624c832de398c6d38a6ea1d2db72ac947aa2b8b26d1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "403d23e84e307f16a3862b5c3e2a7646421edb34f1031b1184ae23f4e03c1aff"
+    sha256 cellar: :any_skip_relocation, catalina:       "12289d4e5d755ae22647376974aa9f25b60976c59ae0fb8bea187148f767cad4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f52ff66b2f8bf89d4efc10f5edd18ed76d6c538ee6a18a7b80edd5a8bfb0991"
   end
 
   depends_on "go" => :build
