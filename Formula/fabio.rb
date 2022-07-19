db1d187326c209a82cc9095045fb13409dbe6876
Nathan Johnson <njohnson@ena.com>
2022-07-19 09:41:54 -0500

    fabio 1.6.1
    Closes #106134.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fabio.rb
+++ Formula/fabio.rb
@@ -1,8 +1,8 @@
 class Fabio < Formula
   desc "Zero-conf load balancing HTTP(S) router"
   homepage "https://github.com/fabiolb/fabio"
-  url "https://github.com/fabiolb/fabio/archive/v1.6.0.tar.gz"
-  sha256 "2a3a678a3ee7a5415512a1b3b799352186859f5d4dd50330d1117b21a643c398"
+  url "https://github.com/fabiolb/fabio/archive/v1.6.1.tar.gz"
+  sha256 "dafb85fb89a8d23a8edc6e96da54c4bdc0b86fce936fa6378e9f49fa70a04793"
   license "MIT"
   head "https://github.com/fabiolb/fabio.git", branch: "master"
 
