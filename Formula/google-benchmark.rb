f15d01cc0cbab4df555e68ece2060b1862e18d5c
Porkepix <porkepix@gmail.com>
2022-07-19 19:01:45 +0200

    google-benchmark 1.6.2
    Closes #106152.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/google-benchmark.rb
+++ Formula/google-benchmark.rb
@@ -1,8 +1,8 @@
 class GoogleBenchmark < Formula
   desc "C++ microbenchmark support library"
   homepage "https://github.com/google/benchmark"
-  url "https://github.com/google/benchmark/archive/v1.6.1.tar.gz"
-  sha256 "6132883bc8c9b0df5375b16ab520fac1a85dc9e4cf5be59480448ece74b278d4"
+  url "https://github.com/google/benchmark/archive/v1.6.2.tar.gz"
+  sha256 "a9f77e6188c1cd4ebedfa7538bf5176d6acc72ead6f456919e5f464ef2f06158"
   license "Apache-2.0"
   head "https://github.com/google/benchmark.git", branch: "main"
 
