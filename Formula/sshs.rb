edf6a0e517ee4158a9672b3fd293cad637e0a69a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-19 17:11:32 +0000

    sshs: update 3.3.0 bottle.

--- Formula/sshs.rb
+++ Formula/sshs.rb
@@ -6,12 +6,12 @@ class Sshs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "25ddb81546acf421a7314d16e113049b92247e6f5d9ef82d21b616c9c9b203dd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2d99b452bc112129f2cd1c0db08c54b89cc6c9d6efa12ad7d9a6eab77d514869"
-    sha256 cellar: :any_skip_relocation, monterey:       "a94fbcb2090a8762122c6ebfed9f2b5fcbfdfbda259293ebeb5dfdd7919262f1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4523020639af5422641feec22d3ff6c5b2027fc65d13c3ac1563e0d5e0a3e407"
-    sha256 cellar: :any_skip_relocation, catalina:       "bcf3b701609f1a4eb851178d6e97ba0f18a8becd5e5537b046ffb79d32052bac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fde91b2d26c5147e1f2a97a2e26f7755419e9c1861ac68e591fc1662e49ba6f4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "afeaae953e5defbe2cc44026633202ab86bd5b26e71a26edde6fbd00f3d85285"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "188aaaa4fcc0b606ffd75d5b05db5e430584394dc4e5e4ae18d2f4d0d1f6c244"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b8b87450edc7f5c5457ac1f4e2ba292e1bb3b78458e3e4133441b9d8d073f31"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4f5b45adcca5f7ee50d95f291d8929c5f7aa9b60c81e9ac28051f4dc7d785f29"
+    sha256 cellar: :any_skip_relocation, catalina:       "2a1f316d5d4708c02acdf3cf48ab41ffffb164dc0e984f03bc1f8aceec39cfc9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a1321a22ffd0991d94a3e1aed3b070584d5f86f751c5376a4e82b38516ade6a"
   end
 
   depends_on "go" => :build
