671dec915c7e1b44040135bffba9c51f259c16b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-19 17:11:38 +0000

    vsce: update 2.10.0 bottle.

--- Formula/vsce.rb
+++ Formula/vsce.rb
@@ -14,12 +14,12 @@ class Vsce < Formula
   end
 
   bottle do
-    sha256                               arm64_monterey: "e5a7376992e94aa8976d1d93abf15031320f29bd9f0beffde9429ed6ee99bd58"
-    sha256                               arm64_big_sur:  "150b93867c97d0bc5fb51dcf5549eed6acb16b938118b3dd2e43231f767dabde"
-    sha256                               monterey:       "0f594a283f3375a08232c3d4883fc756d5e946f87bd1f5f4f55552801d8bd7ce"
-    sha256                               big_sur:        "24174d6a1e6135544e00568d3d76366e46d02f115b0aa16f26c909949b86c24e"
-    sha256                               catalina:       "dbf82096b1021000d3dc02c83885a33f205c9b30c24840983457fbb96cffd509"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3416dc6414061e4e6c391f958cb45f9916f2dc11206f656e38ccf1f5e94237a6"
+    sha256                               arm64_monterey: "124444548868453bef200e1e26846fbd15054f8c5915d7a19def4e36aae5d6c2"
+    sha256                               arm64_big_sur:  "f274fda85e20c41480b885e82d15ce20a459baf18203301d460f61a185ac20c0"
+    sha256                               monterey:       "30a45a6780fac9de2e08028394db17bcb99294f265836a711ec0a8612102b868"
+    sha256                               big_sur:        "69c59898b3ad77aecc323928976b9420dd7a79f8f1062c67e3c6979755921fca"
+    sha256                               catalina:       "8aeea5dea401ffc23f234e27b0b2d5740c3a3b4e22773c613afe8e26025a8aa9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9388ec24de5d42c383546ca1eeaa93dbd3a1346d4cbbcc388aeefa7abe0eaed9"
   end
 
   depends_on "node"
