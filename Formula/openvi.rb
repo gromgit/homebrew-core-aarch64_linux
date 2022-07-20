ce127751a3ae8732d09aaed2c02ea8a050e93bff
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-20 14:50:52 +0000

    openvi: add 7.1.17 bottle.

--- Formula/openvi.rb
+++ Formula/openvi.rb
@@ -5,6 +5,15 @@ class Openvi < Formula
   sha256 "02974603f1cd3ba5727fd40f69f97925510d8f30c1ff3041adbc3e54e11f6d5f"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f611f0b540acbf421d2974ee16160abae040d936ac4967a35ea43927b4420cb5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63752e1d0c23b9a02a2a9587bc5a9c0a22f2883b95ea2b5039119b9de33b04d1"
+    sha256 cellar: :any_skip_relocation, monterey:       "b79c000683e5bc7e3279b185f018299e5568d2e79c31000d3607f68e2bc52777"
+    sha256 cellar: :any_skip_relocation, big_sur:        "93be6c7314fccb3ff9e9a77b7b06af9adcef66a4ec33d440953b74288511ff25"
+    sha256 cellar: :any_skip_relocation, catalina:       "d6073577026b5c8421ccda24d9a6b87dc176795cf7a9631151c2b5114d70e36d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8132b1ccd79a17fd5d539c78cca430af927f5f0a3b60f3d89ca91f0b215e018f"
+  end
+
   uses_from_macos "ncurses"
 
   def install
