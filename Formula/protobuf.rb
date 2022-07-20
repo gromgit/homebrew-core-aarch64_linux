9b35d566395d6c8445694947c43c178ff7feaf94
Bevan Kay <email@bevankay.me>
2022-07-21 09:30:53 +1000

    protobuf 21.3

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,8 +1,8 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v21.2/protobuf-all-21.2.tar.gz"
-  sha256 "9ae699200f3a80c735f9dc3b20e46d447584266f4601403e8fe5b97005f204dd"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v21.3/protobuf-all-21.3.tar.gz"
+  sha256 "2f723218f6cb709ae4cdc4fb5ed56a5951fc5d466f0128ce4c946b8c78c8c49f"
   license "BSD-3-Clause"
 
   livecheck do
