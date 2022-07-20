4eaa1648b63eeb87c444b22e5db343fc4f8f0b11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-20 01:34:03 +0000

    wavpack: update 5.5.0 bottle.

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -6,13 +6,12 @@ class Wavpack < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6648965ef20e4dc6d115aa12ab7b627711b6a4ab8ac8555a87249e11594a423a"
-    sha256 cellar: :any,                 arm64_big_sur:  "3e51f2f593fffc58cd550236d6a9711900841d388d6ac7833c09f272270d5156"
-    sha256 cellar: :any,                 monterey:       "1de517ac8c52b75c8928a9b3f1ce325acd9e4aecff3a21aed26e11d79be7352f"
-    sha256 cellar: :any,                 big_sur:        "711535e6329af607e974ee9e3e957fd82b056af39e95a78226bd9e8f9a0025dd"
-    sha256 cellar: :any,                 catalina:       "29360357a18df2827e8acb41fdd874495b62f431deb230c96b8d5dcca5f6d316"
-    sha256 cellar: :any,                 mojave:         "89815dc5f1b4af0d647d2b1d3ad484ddce39208958bb315793240aa7be0c4565"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c6258f61ddd3478cbbeffc8cd939dd65222d209214a1f291859f2c00108039e3"
+    sha256 cellar: :any,                 arm64_monterey: "f7b8b75495e5d9ee17b60243322a52349ce65a89de9c0dd6f9d9d35461d8b0c6"
+    sha256 cellar: :any,                 arm64_big_sur:  "e34bfb3ebb2f06c4f7fc22a587400acbbb4853bb106cbe3b679b4512cacc254b"
+    sha256 cellar: :any,                 monterey:       "effd5ef1000609272a70f2d84e6a2bf61a200a5b1bc239a7d89eb13d97be6a57"
+    sha256 cellar: :any,                 big_sur:        "ab5cb74fc57673c4bbae53a5325d486ab3a4151a00fbfc89f2c06905a00ad345"
+    sha256 cellar: :any,                 catalina:       "cce469c6e14c269c49e71f1bc71631bb286728a77af844ce68567b9645cdf01a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58baedd42d91d72acd6fe3f4624e60fb7a24c6fdc0df36f20b6cd5739d557ce3"
   end
 
   head do
