4ef67eb34442bbc4be1a2a341fe138dca7faed17
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-22 00:13:36 +0900

    poac: fix Catalina build
    Also, fix installation of completions.
    
    Closes #106329.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/poac.rb
+++ Formula/poac.rb
@@ -18,28 +18,39 @@ class Poac < Formula
   depends_on "boost"
   depends_on "fmt"
   depends_on "libgit2"
-  depends_on macos: :big_sur # C++20
   depends_on "openssl@1.1"
   depends_on "spdlog"
 
   uses_from_macos "libarchive"
 
+  on_macos do
+    depends_on "llvm" => [:build, :test] if DevelopmentTools.clang_build_version <= 1200
+  end
+
   on_linux do
     depends_on "gcc"
   end
+
+  fails_with :clang do
+    build 1200
+    cause "Requires C++20"
+  end
+
   fails_with gcc: "5" # C++20
 
   def install
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+    ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1200)
+
     system "cmake", "-B", "build", "-DCPM_USE_LOCAL_PACKAGES=ON", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
     man1.install (buildpath/"src/etc/man/man1").children
-    bash_completion.install_symlink "src/etc/poac.bash" => "poac"
-    zsh_completion.install_symlink "src/etc/poac.bash" => "_poac"
   end
 
   test do
+    ENV.clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1200)
     system bin/"poac", "create", "hello_world"
     cd "hello_world" do
       assert_match "Hello, world!", shell_output("#{bin}/poac run")
