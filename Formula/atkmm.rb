800e5fa2e07f503aeb53a02ff70df35f79dd45e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-21 09:04:56 +0000

    atkmm: update 2.36.2 bottle.

--- Formula/atkmm.rb
+++ Formula/atkmm.rb
@@ -6,13 +6,12 @@ class Atkmm < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "62a52bf13cb3e8ae0f3de2c22fe5eb99c82e2bfcf70562e7299b81e3b0eb46ab"
-    sha256 cellar: :any,                 arm64_big_sur:  "82e89f908e2a3cc349c8478eef60d7b755f968d3523f037b8b78ea4edc4031ba"
-    sha256 cellar: :any,                 monterey:       "8d00072e06cb6fa4748cad51217427499ace42f1177e0f1d6bfbb00363329378"
-    sha256 cellar: :any,                 big_sur:        "4b147e97566fe8f13c6e72e1d350da5c8bbabe3abb6f4966eec44f227c9fa807"
-    sha256 cellar: :any,                 catalina:       "09b0e72f5d9fd8e2a3328202d8c2b8fc7f896cdd208ab51fa75bb87c79eee53d"
-    sha256 cellar: :any,                 mojave:         "a3ed94c51be9ae22f3a3c1d6650ade4037748f2c8602db9b647effd71b4d5108"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2e8f09c113ecfd68cac31cc2e96daecc200a4260a076ef5c58bf852774fbdc1"
+    sha256 cellar: :any, arm64_monterey: "060aaa8eff1cde559d778477b5d3b0b707de855a590a13a9ff9b416ce7a4d2ee"
+    sha256 cellar: :any, arm64_big_sur:  "664288c4e6fb17a1d7b496e77ed85ca6f1133aeb56f96ba643cd0d1878544935"
+    sha256 cellar: :any, monterey:       "59d500b3a99b28004129b67bdd0e7b01e380641775c3a8f419c8ef946ebfb885"
+    sha256 cellar: :any, big_sur:        "6faaf66dbd24fd9a6653cd123c5ea7e3d859f6271889315ebdf53a1c66548ea3"
+    sha256 cellar: :any, catalina:       "b90948dbd7c51cc8a6a1cddadb90942b8edec8e682b37f168be4fd5eca3c6cee"
+    sha256               x86_64_linux:   "1d659fec4e9807cd89ff4dfb30ccebcde19856021ff78bc15a85107eded62152"
   end
 
   depends_on "meson" => :build
