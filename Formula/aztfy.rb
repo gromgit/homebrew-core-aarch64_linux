de9129f274697a7e3aa791521a7c1eb1eff2edf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-21 09:05:36 +0000

    aztfy: update 0.6.0 bottle.

--- Formula/aztfy.rb
+++ Formula/aztfy.rb
@@ -8,12 +8,12 @@ class Aztfy < Formula
   head "https://github.com/Azure/aztfy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5b92b9d72ac203afb014c060d61be6766b39d676a1c827f6cef52f36aedc4b13"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5b92b9d72ac203afb014c060d61be6766b39d676a1c827f6cef52f36aedc4b13"
-    sha256 cellar: :any_skip_relocation, monterey:       "dee2f8e188d185fe215f26749f1a4f3546fe421c944c88d1529b700203f92c02"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dee2f8e188d185fe215f26749f1a4f3546fe421c944c88d1529b700203f92c02"
-    sha256 cellar: :any_skip_relocation, catalina:       "dee2f8e188d185fe215f26749f1a4f3546fe421c944c88d1529b700203f92c02"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c164eaf7a02031b4a4c5738bdcbcda0228134a49b3d474e59ed64b1a8a0f33ce"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "79dae50a2d5034ed78479cc512cac7f027335a80657314c8c9d46fb50e6bbe15"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "79dae50a2d5034ed78479cc512cac7f027335a80657314c8c9d46fb50e6bbe15"
+    sha256 cellar: :any_skip_relocation, monterey:       "0c1fbbb4c588cb9e035adada127e107b56f13381a9c34bdefcf212e6c858f3bc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0c1fbbb4c588cb9e035adada127e107b56f13381a9c34bdefcf212e6c858f3bc"
+    sha256 cellar: :any_skip_relocation, catalina:       "0c1fbbb4c588cb9e035adada127e107b56f13381a9c34bdefcf212e6c858f3bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e422b3038399eff6c079156404710063f7a2b9b99e85b88e53abf543035fa77e"
   end
   depends_on "go" => :build
 
