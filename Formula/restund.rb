3d7c2d3c978e8761b502ffa6b896c118f8b0d27e
Sam Ford <1584702+samford@users.noreply.github.com>
2022-07-20 22:14:10 -0400

    restund: update livecheck
    Closes #106274.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/restund.rb
+++ Formula/restund.rb
@@ -5,9 +5,13 @@ class Restund < Formula
   sha256 "3170441dc882352ab0275556b6fc889b38b14203d936071b5fa12f39a5c86d47"
   revision 3
 
+  # The sources.openwrt.org directory listing page is 2+ MB in size and
+  # growing. This alternative check is less ideal but only a few KB. Versions
+  # on the package page can use a format like 1.2.3-4, so we omit any trailing
+  # suffix to match the tarball version.
   livecheck do
-    url "https://sources.openwrt.org/"
-    regex(/href=.*?restund[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    url "https://openwrt.org/packages/pkgdata/restund"
+    regex(/<dd [^>]*?class="version"[^>]*?>\s*?v?(\d+(?:\.\d+)+)/im)
   end
 
   bottle do
