00fa510331f56fb65b1f74e9cac90791ee01700d
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-07-21 01:26:09 +0000

    clblas: update 2.12 bottle.

--- Formula/clblas.rb
+++ Formula/clblas.rb
@@ -6,15 +6,16 @@ class Clblas < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "05f10b95b407296d687e06648a0c4abaa7d590ede330b687ac7874d8afbbe863"
-    sha256 cellar: :any, arm64_big_sur:  "f0292a882d4f60d4e59ed42e1c41f063b90a2dcecdfb24cc8757f9f216eaf935"
-    sha256 cellar: :any, monterey:       "bcf26f9fa5be8cee77f022a7364119e73281dc8bd18451ddf3c7b98fbf8513ad"
-    sha256 cellar: :any, big_sur:        "e68a3ce1669b5f88aa5ca31e57ccb687f99601d35f3fcf7470de8e38d53671c9"
-    sha256 cellar: :any, catalina:       "97d7206bae700bdba2b4f1c6570d7e772ab5ade56d82af61d98d75eebe764f72"
-    sha256 cellar: :any, mojave:         "de18e1f78894ad83aa80a1d2a6d21973d61507be13a657055bd19b1f11b80c0b"
-    sha256 cellar: :any, high_sierra:    "47e08f87365e11a57d2ffc2fb81a3cfcd8bd784c438c1e08e1fe4116fc774553"
-    sha256 cellar: :any, sierra:         "22a6cc8252ed5d431ccea7d51631f57bcee3876be7f65a0ac0fbaabfe09a9484"
-    sha256 cellar: :any, el_capitan:     "e18aa93ecbd78f5f70607653a1e1c48f73952aeef1a568e2205362368c40ba4c"
+    sha256 cellar: :any,                 arm64_monterey: "05f10b95b407296d687e06648a0c4abaa7d590ede330b687ac7874d8afbbe863"
+    sha256 cellar: :any,                 arm64_big_sur:  "f0292a882d4f60d4e59ed42e1c41f063b90a2dcecdfb24cc8757f9f216eaf935"
+    sha256 cellar: :any,                 monterey:       "bcf26f9fa5be8cee77f022a7364119e73281dc8bd18451ddf3c7b98fbf8513ad"
+    sha256 cellar: :any,                 big_sur:        "e68a3ce1669b5f88aa5ca31e57ccb687f99601d35f3fcf7470de8e38d53671c9"
+    sha256 cellar: :any,                 catalina:       "97d7206bae700bdba2b4f1c6570d7e772ab5ade56d82af61d98d75eebe764f72"
+    sha256 cellar: :any,                 mojave:         "de18e1f78894ad83aa80a1d2a6d21973d61507be13a657055bd19b1f11b80c0b"
+    sha256 cellar: :any,                 high_sierra:    "47e08f87365e11a57d2ffc2fb81a3cfcd8bd784c438c1e08e1fe4116fc774553"
+    sha256 cellar: :any,                 sierra:         "22a6cc8252ed5d431ccea7d51631f57bcee3876be7f65a0ac0fbaabfe09a9484"
+    sha256 cellar: :any,                 el_capitan:     "e18aa93ecbd78f5f70607653a1e1c48f73952aeef1a568e2205362368c40ba4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "89bf323af5a469bbf9dfc587bf029621dd292b20369315b1b8b5a98feaf1f0d2"
   end
 
   depends_on "boost" => :build
