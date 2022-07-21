fd194e3bcb9b06b1edfcf701fdb1cc5553277611
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-22 01:44:29 +0800

    libbpf: cleanup
    1. Improve the description
    2. Make `pkg-config` a build dependency
    
    Closes #106348.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libbpf.rb
+++ Formula/libbpf.rb
@@ -1,5 +1,5 @@
 class Libbpf < Formula
-  desc "Automated upstream mirror for libbpf stand-alone build"
+  desc "Berkeley Packet Filter library"
   homepage "https://github.com/libbpf/libbpf"
   url "https://github.com/libbpf/libbpf/archive/refs/tags/v0.8.1.tar.gz"
   sha256 "7bda8187efc619d1eb20a1ba5ab949dd68d40dd44945310c91ac0f915fa4a42b"
@@ -9,10 +9,10 @@ class Libbpf < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "ec4c77b184073770c4d8486c2b86bc59ef7a7398e7c0b99164b725b802c813e3"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "linux-headers@5.16" => :test
   depends_on "elfutils"
   depends_on :linux
-  depends_on "pkg-config"
 
   uses_from_macos "zlib"
 
