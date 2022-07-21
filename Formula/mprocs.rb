f34b944fb9cf08fd82390d599f138a69315dc5aa
Porkepix <porkepix@gmail.com>
2022-07-21 17:54:20 +0200

    mprocs 0.6.1
    * mprocs 0.6.1
    * mprocs-0.6.1: add Python dependency on Linux
      This is required by the xcb crate.
    * mprocs-0.6.1: depends on libxcb for Linux
    * mprocs-0.6.1: new hash
    
    Closes #106334.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mprocs.rb
+++ Formula/mprocs.rb
@@ -1,8 +1,8 @@
 class Mprocs < Formula
   desc "Run multiple commands in parallel"
   homepage "https://github.com/pvolok/mprocs"
-  url "https://github.com/pvolok/mprocs/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "c9314e81b0a5584e73a1afc2c1b5b83df0ed31213992447324cb95abbda618ce"
+  url "https://github.com/pvolok/mprocs/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "d7ac94e7053c5d3fa95d6c5582ffff9c5df091a198c88cb9d89d974d247f843b"
   license "MIT"
   head "https://github.com/pvolok/mprocs.git", branch: "master"
 
@@ -16,6 +16,12 @@ class Mprocs < Formula
   end
 
   depends_on "rust" => :build
+
+  uses_from_macos "python" => :build # required by the xcb crate
+
+  on_linux do
+    depends_on "libxcb"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
