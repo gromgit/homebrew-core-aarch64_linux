f4f742af8ebde4bd18c5c21dda44773d61af210f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-21 16:04:57 +0000

    datree: update 1.5.37 bottle.

--- Formula/datree.rb
+++ Formula/datree.rb
@@ -12,12 +12,12 @@ class Datree < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bff3c0f064ee4ca74aa622c5ef77e857ad4aa581d54ac832d6e8c0f84b65ab6b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "739180dfda3c3b1e0176be49aef924063e644bb688f390cc7af0bb2928132b4c"
-    sha256 cellar: :any_skip_relocation, monterey:       "0a2df49a01e7b14b823130fca4c705ef1ad5666c4e202f38d6c497770024acb5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bdc0a810973815c710841fe8f4f49034bfbbe9667cbf13136a2a4e61d61f7e0a"
-    sha256 cellar: :any_skip_relocation, catalina:       "9a2aec6726f93001e87fece158ede780cf8e24163b7fd4b38e9114e1be00a26f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7f487641a8060cc0e5b1442800d1bf8bbcce5faa402cc36177621602adcf7698"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "308b4f20e81dcee120b91369b01fad77d83960e088db324c8dfadcea7c3a56b6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85601fe79e4671160ab8146d20500b4ea0e12c06c64f8e97ef298d1db206fee9"
+    sha256 cellar: :any_skip_relocation, monterey:       "081167d9a44d3d4bf2cd9cf5646dd6f463c0e6385472a42d5d4163d143019a99"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6020e7575fa062164ddbc62e65a17f2ce7ef570870ad18799ba1415399d82906"
+    sha256 cellar: :any_skip_relocation, catalina:       "17421fff473c24c5936ff4bad9d6010b8aad664a2e4b8b0ea0dbcabd55f608d0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c742779ab845c34535d08dd377392a1ddeb486d671b168c7c0bae9f4ad7a6ece"
   end
 
   depends_on "go" => :build
