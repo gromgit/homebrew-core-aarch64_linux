5b7e220563393867240cd538287666ec3ea788a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-21 05:14:09 +0000

    boringtun: update 0.5.2 bottle.

--- Formula/boringtun.rb
+++ Formula/boringtun.rb
@@ -7,12 +7,12 @@ class Boringtun < Formula
   head "https://github.com/cloudflare/boringtun.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "63c04b158add7733fb8edfe4bfb56a0098ded750282720639dbe0d62e4f9b98a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b79542bd106b76ca6954733164e7ffee30506d4c43b343edfe1b368d681dc4a8"
-    sha256 cellar: :any_skip_relocation, monterey:       "b21b6bdc08718e301d7a412c522f4fe0ed580b0b233e541c669639705fef45fc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7c3c6a06623640f07e36039db7c7e87d39b413d034cbf6541ba80b25caece6c7"
-    sha256 cellar: :any_skip_relocation, catalina:       "fa05784c502a336f8bbbe232ba7c4b480f27861edc5878085a1e259403b5e513"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1e64e3b6ae780ce082c0a1727553de7c18fc16446bf4397b911b302d1aa8d17"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "63895d49881fb0e40ea40ddbb0414233a42d3311bc77f0193266752c7723ca4e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57d1a55c819072fb79fd26e479fb874070f4c86f662c5b17b040a63637e21c2d"
+    sha256 cellar: :any_skip_relocation, monterey:       "359ad2ce46b578bd55f2faa7dbd41a6202b3b39e4b40beb0de5c0782a7a8ed25"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0c0545c856571a954f151aeb643ff1f77283165b1bc95a5bc762b44184a0716f"
+    sha256 cellar: :any_skip_relocation, catalina:       "dd64f6c9bde4f8e668736dd9aff22912e99a393c868605ec98d19dcf6885f596"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c6bcd0a73c3fbc14c3d786c68952699637cc5bd502dd06de29468e27bdded4e"
   end
 
   depends_on "rust" => :build
