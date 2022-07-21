0840f659cc0ab8be476e1973a4f53bbeeb78656c
Porkepix <porkepix@gmail.com>
2022-07-21 17:07:23 +0200

    git-xargs 0.1.0
    Closes #106327.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-xargs.rb
+++ Formula/git-xargs.rb
@@ -1,8 +1,8 @@
 class GitXargs < Formula
   desc "CLI for making updates across multiple Github repositories with a single command"
   homepage "https://github.com/gruntwork-io/git-xargs"
-  url "https://github.com/gruntwork-io/git-xargs/archive/v0.0.16.tar.gz"
-  sha256 "2e2d719df83beaaa59d62ee5b7be140d3e6a683ed570b5b640d4e813d1f4c531"
+  url "https://github.com/gruntwork-io/git-xargs/archive/v0.1.0.tar.gz"
+  sha256 "449ddfe8f76a3dfd79b415a11f8c531dcc1913be7173d57f269dcaf8f36b65b5"
   license "Apache-2.0"
 
   bottle do
