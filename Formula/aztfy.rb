a1f2ac63c2808f4dd11e8bfe8715be3740c04fb3
koudaiii <cs006061@gmail.com>
2022-07-21 15:21:40 +0900

    aztfy 0.6.0
    * aztfy 0.6.0
    * Fix spec
    
    Closes #106301.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aztfy.rb
+++ Formula/aztfy.rb
@@ -2,8 +2,8 @@ class Aztfy < Formula
   desc "Bring your existing Azure resources under the management of Terraform"
   homepage "https://azure.github.io/aztfy"
   url "https://github.com/Azure/aztfy.git",
-      tag:      "v0.5.0",
-      revision: "f957fc158684aaf66551543327124865c7fe4eca"
+      tag:      "v0.6.0",
+      revision: "18b614bb384b1d413554867b8601feb753715575"
   license "MPL-2.0"
   head "https://github.com/Azure/aztfy.git", branch: "main"
 
@@ -26,7 +26,7 @@ class Aztfy < Formula
     version_output = shell_output("#{bin}/aztfy -v")
     assert_match version.to_s, version_output
 
-    no_resource_group_specified_output = shell_output("#{bin}/aztfy 2>&1", 1)
+    no_resource_group_specified_output = shell_output("#{bin}/aztfy rg 2>&1", 1)
     assert_match("Error: No resource group specified", no_resource_group_specified_output)
   end
 end
