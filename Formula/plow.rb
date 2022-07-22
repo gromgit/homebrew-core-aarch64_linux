28b8533ddbc3523219feedd6739684c690636b2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-22 10:04:58 +0000

    plow: update 1.3.0 bottle.

--- Formula/plow.rb
+++ Formula/plow.rb
@@ -6,12 +6,12 @@ class Plow < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "950704a53979a9b0380eea38d28fbeb3a66a75c7c70b98cf520b27c173ea365f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a1b8d9d830b45deefdd51587d41207e24be13ffa94243635285ad533d21cc539"
-    sha256 cellar: :any_skip_relocation, monterey:       "120d725087be1cd4ca85f80eb276f6b09f137604fd8cfda088245c0f915dd2c3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3809db189ca62f72ae3a9f984849d8189dff597e636a4b7b16665b7f91379abe"
-    sha256 cellar: :any_skip_relocation, catalina:       "ae918fe58274757e2de7d7b530f6d651292f111d64d6156ebb6aa4a433c82a5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1ee6813afc113044fa3308ba268eabc9e85794cc72cd393b5494514fb292cfac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bdbdb17b932fcd47e52e65beb57b1917f0e62fa5e78ee6301e273a258a375b4d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "65f90d41e43b71ded4a070fc8dc3af7d3371d6ba210d730041331ce841239731"
+    sha256 cellar: :any_skip_relocation, monterey:       "c78f54aeaf5e40485ffcde64629249c685770212349511d7a252e613b63f44aa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b8521a8707a251267207edc66b5d990523836e1b7e6588f34c323f7447a7d32e"
+    sha256 cellar: :any_skip_relocation, catalina:       "b31d2b22b0047bd7f6654e8338c46cc6d46dd3c42a3ce011f4dd4b9a25ace457"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c613c35223b31a5cf1c9e1168a5be07f1a466b4ab8f7bc8823adf792877f87d4"
   end
 
   depends_on "go" => :build
