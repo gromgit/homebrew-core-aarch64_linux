3b83af33cd2bf9e394e3a3c8a323c4d934850b3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-22 11:04:39 +0000

    libwmf: update 0.2.8.4_2 bottle.

--- Formula/libwmf.rb
+++ Formula/libwmf.rb
@@ -12,16 +12,13 @@ class Libwmf < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "1f89c6e1b24e93fd7da667bef0e58476e8db973bbdff90803f45ce45a58df0fc"
-    sha256 arm64_big_sur:  "641d8c9c953f8b1e5c5b122a38582ccfdeeb37f92d11dbe0f353bed473053c92"
-    sha256 monterey:       "e149c022245668436bcab7911f8344468c270ee437012edad17ca3c4db42fd7d"
-    sha256 big_sur:        "2c1e4e481c00fdc9a6590bb65d39df1b87bbd054a1ecf20369049127b198c3a9"
-    sha256 catalina:       "4fa5b3dc0b38f72ac7c4d15f4e23bc96c0808d48de28005c4dc74d50292ddf62"
-    sha256 mojave:         "3e0e8ecd7072819856393b472017d1e7fc3995a6d8568c6ad65b7d1055efc2e7"
-    sha256 high_sierra:    "a96fe2e0aef8cd0f8eecce05b8789c2637f973a6ae358924c451b8f36b3a70ef"
-    sha256 sierra:         "9df806eb6a4a3ca1a2b4b656ff02623175892981fbf136c89d4df5b5853bd20c"
-    sha256 el_capitan:     "205bf519460576ecf73e9314ba1171542be58ea22cea81c26424d661734f2d2f"
-    sha256 x86_64_linux:   "601eaf0675dc226c04cb3b52179f8af294317b51307de9067d6506ae60e0dbbb"
+    rebuild 1
+    sha256 arm64_monterey: "5138927a0d8d528ab1a4f4929c1131c7c6e15995c22410ba722a8581b933de11"
+    sha256 arm64_big_sur:  "2f03f6ad89f67e27361b64a23ab0e231e09c3ef0e6be5c6456bbaa4cadc38f91"
+    sha256 monterey:       "c654530d1c940888ac542442827063f85850e18386008a4dedf1ca3711b80ca1"
+    sha256 big_sur:        "aece69a239468a68f86c84ed6eff00120c821aec6b8f69674776f3e6a46b7c6e"
+    sha256 catalina:       "9adf1c5052c7c0036c13a501f4f720fc7e8e511fcc8012ad8c8bfa3dcc457c94"
+    sha256 x86_64_linux:   "06c5049479f0c63291c8f7c8ed6f73a5f3cc4acf6e9a495aeb8635eeecc72642"
   end
 
   depends_on "pkg-config" => :build
