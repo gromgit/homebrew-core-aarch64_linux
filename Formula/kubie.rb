93be625a9edf14cb9e43c7c550a3ae93483c9817
Porkepix <porkepix@gmail.com>
2022-07-23 01:45:45 +0200

    kubie 0.17.1
    * kubie 0.17.1
    * kubie: update test/completions and add head
    
    Closes #106416.
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubie.rb
+++ Formula/kubie.rb
@@ -1,9 +1,10 @@
 class Kubie < Formula
   desc "Much more powerful alternative to kubectx and kubens"
   homepage "https://blog.sbstp.ca/introducing-kubie/"
-  url "https://github.com/sbstp/kubie/archive/v0.17.0.tar.gz"
-  sha256 "775bebfadcb075abb519ff3a79bee4ef1e1d6a55dbff707a7cf3cd0a54c871c1"
+  url "https://github.com/sbstp/kubie/archive/v0.17.1.tar.gz"
+  sha256 "2de65f47648f5467fc57bb1cc3f643a009314e950e6da68524460512c71fe0c6"
   license "Zlib"
+  head "https://github.com/sbstp/kubie.git", branch: "master"
 
   livecheck do
     url :stable
@@ -25,10 +26,10 @@ class Kubie < Formula
   def install
     system "cargo", "install", *std_cargo_args
     bash_completion.install "./completion/kubie.bash"
+    fish_completion.install "./completion/kubie.fish"
   end
 
   test do
-    mkdir_p testpath/".kube"
     (testpath/".kube/kubie-test.yaml").write <<~EOS
       apiVersion: v1
       clusters:
@@ -48,8 +49,6 @@ class Kubie < Formula
       - user:
         name: kubie-test-user
     EOS
-
-    assert_match "kubie #{version}", shell_output("#{bin}/kubie --version")
 
     assert_match "The connection to the server 0.0.0.0 was refused - did you specify the right host or port?",
       shell_output("#{bin}/kubie exec kubie-test kubie-test-namespace kubectl get pod 2>&1")
