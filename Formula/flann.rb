d35da78594742042687315ec29caa0f24d32da48
Rylan Polster <rslpolster@gmail.com>
2022-07-22 07:46:53 +0200

    flann: reorganize patch and resource blocks
    Generated using `brew style --fix` and then updated manually if needed

--- Formula/flann.rb
+++ Formula/flann.rb
@@ -18,18 +18,18 @@ class Flann < Formula
   depends_on "cmake" => :build
   depends_on "hdf5"
 
-  on_linux do
-    # Fix for Linux build: https://bugs.gentoo.org/652594
-    # Not yet fixed upstream: https://github.com/mariusmuja/flann/issues/369
-    patch do
-      url "https://raw.githubusercontent.com/buildroot/buildroot/0c469478f64d0ddaf72c0622a1830d855306d51c/package/flann/0001-src-cpp-fix-cmake-3.11-build.patch"
-      sha256 "aa181d0731d4e9a266f7fcaf5423e7a6b783f400cc040a3ef0fef77930ecf680"
-    end
-  end
-
   resource("dataset") do
     url "https://github.com/flann-lib/flann/files/6518483/dataset.zip"
     sha256 "169442be3e9d8c862eb6ae4566306c31ff18406303d87b4d101f367bc5d17afa"
+  end
+
+  # Fix for Linux build: https://bugs.gentoo.org/652594
+  # Not yet fixed upstream: https://github.com/mariusmuja/flann/issues/369
+  patch do
+    on_linux do
+      url "https://raw.githubusercontent.com/buildroot/buildroot/0c469478f64d0ddaf72c0622a1830d855306d51c/package/flann/0001-src-cpp-fix-cmake-3.11-build.patch"
+      sha256 "aa181d0731d4e9a266f7fcaf5423e7a6b783f400cc040a3ef0fef77930ecf680"
+    end
   end
 
   def install
