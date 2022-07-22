9f6dcf17a4e31479cfe51199cd6b82b1873e6157
Rylan Polster <rslpolster@gmail.com>
2022-07-22 07:46:54 +0200

    jpegrescan: reorganize patch and resource blocks
    Generated using `brew style --fix` and then updated manually if needed

--- Formula/jpegrescan.rb
+++ Formula/jpegrescan.rb
@@ -20,8 +20,8 @@ class Jpegrescan < Formula
 
   uses_from_macos "perl"
 
-  on_linux do
-    resource "File::Slurp" do
+  resource "File::Slurp" do
+    on_linux do
       url "https://cpan.metacpan.org/authors/id/C/CA/CAPOEIRAB/File-Slurp-9999.32.tar.gz"
       sha256 "4c3c21992a9d42be3a79dd74a3c83d27d38057269d65509a2f555ea0fb2bc5b0"
     end
