533341781b78b1a316ab6005564ec8108cebb69b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-23 08:05:03 +0000

    naabu: update 2.0.8 bottle.

--- Formula/naabu.rb
+++ Formula/naabu.rb
@@ -7,12 +7,12 @@ class Naabu < Formula
   head "https://github.com/projectdiscovery/naabu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1458efaa4be0fc56ac4a6a969b2300e349b1c847114c88e095e12c9a0bad96f8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7c9483e3d20a000df560343d7f51e1e6451f7d069dc5ebd3ab8ebde95e9691d8"
-    sha256 cellar: :any_skip_relocation, monterey:       "34efd719d9c83cca8f846b7ea75dea398c15e2333f28f7ce1f9b2bfac57a0cb5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d4b66039e0fe378db420419624650c006acd9e13aff32c38a1a7f3c8d20006c7"
-    sha256 cellar: :any_skip_relocation, catalina:       "f5d14c3c4b5c6f282215a026fb8a9f109d198cefde354cc8b9e4b6c212050f10"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3e1d193a8d1693bbc0399ff007e1c8eb93f788f333869b9a0c3dee45b8c8e3b8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "66ea56697b7ef444f3dce2ead35eb30b8c2866d21a0f96bef6d9752cb542d26f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "261700cc346a79b467599b09d736d31ac13b1a8609105711d3245991bf3a3b44"
+    sha256 cellar: :any_skip_relocation, monterey:       "7b6aeca4a8eb6c4e02aa9c9a78cc468e2fb0aabf5a3a51ccf1cac2bb4b546dbb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "798df64023eaa7c53385f59369327b326ee1a75cc622ccafaa7ddc2f682f9f47"
+    sha256 cellar: :any_skip_relocation, catalina:       "a2270c49741656f39a2688639fe24be109174678751f92ec98a087804c3455e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69ba011532beeaa4d73e51e95830d9fcf5e997bf1430437aca86596fa1ece333"
   end
 
   depends_on "go" => :build
