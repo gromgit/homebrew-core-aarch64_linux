90e43ed46629be3d42ab9fd988518fa930d78828
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-23 15:51:14 -0400

    openvi 7.1.18
    Closes #106451.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openvi.rb
+++ Formula/openvi.rb
@@ -1,8 +1,8 @@
 class Openvi < Formula
   desc "Portable OpenBSD vi for UNIX systems"
   homepage "https://github.com/johnsonjh/OpenVi#readme"
-  url "https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.1.17.tar.gz"
-  sha256 "02974603f1cd3ba5727fd40f69f97925510d8f30c1ff3041adbc3e54e11f6d5f"
+  url "https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.1.18.tar.gz"
+  sha256 "39c4ac933f52c65021be06fcece8bfd308fc1ac08e8ff4604b2fdd1994192d08"
   license "BSD-3-Clause"
 
   bottle do
