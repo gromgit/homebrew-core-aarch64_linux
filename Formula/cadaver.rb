b6356fba279133612e085945c52198965eca894c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-07-23 01:54:11 +0000

    cadaver: update 0.23.3_5 bottle.

--- Formula/cadaver.rb
+++ Formula/cadaver.rb
@@ -16,11 +16,13 @@ class Cadaver < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "859215276f7fda671ceee3b7908772d84fccd12873e6bb6cac0f90c50982cbcc"
-    sha256 big_sur:       "240a41ea5b71aa144bea0fdb28b6233130d5368e8a221171eaa7bee24f5075a6"
-    sha256 catalina:      "da94dea10afd90e1d0e41f24d4319ea006bf909381de2c2379c3144374c3feff"
-    sha256 mojave:        "a232491b47135718f6cf65d00954099d92a43f5fcc6b01838a676faa77f2ed13"
-    sha256 x86_64_linux:  "03a8734a293f551585c100ce10389aa2a66222ab711d9c80b7300d139eb8546b"
+    sha256 arm64_monterey: "4ffec4d1167cb17f80cb63c4142909b6f208cc653518cee7f46fb7c1f270e192"
+    sha256 arm64_big_sur:  "859215276f7fda671ceee3b7908772d84fccd12873e6bb6cac0f90c50982cbcc"
+    sha256 monterey:       "72eea3f287da7f77740b72db463cdf967c355a4044d0730150d637145d6be312"
+    sha256 big_sur:        "240a41ea5b71aa144bea0fdb28b6233130d5368e8a221171eaa7bee24f5075a6"
+    sha256 catalina:       "da94dea10afd90e1d0e41f24d4319ea006bf909381de2c2379c3144374c3feff"
+    sha256 mojave:         "a232491b47135718f6cf65d00954099d92a43f5fcc6b01838a676faa77f2ed13"
+    sha256 x86_64_linux:   "03a8734a293f551585c100ce10389aa2a66222ab711d9c80b7300d139eb8546b"
   end
 
   depends_on "pkg-config" => :build
