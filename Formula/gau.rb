ab7f6598f99545346de7d6cb7c4bf66c3e6611d2
Porkepix <porkepix@gmail.com>
2022-07-24 00:46:56 +0200

    gau 2.1.2
    Closes #106456.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gau.rb
+++ Formula/gau.rb
@@ -1,8 +1,8 @@
 class Gau < Formula
   desc "Open Threat Exchange, Wayback Machine, and Common Crawl URL fetcher"
   homepage "https://github.com/lc/gau"
-  url "https://github.com/lc/gau/archive/v2.1.1.tar.gz"
-  sha256 "17bd5b8e3a6a554d82ab5de66bc1f34ce2810a3ba6b8e1456432faff9b191c8f"
+  url "https://github.com/lc/gau/archive/v2.1.2.tar.gz"
+  sha256 "2900ba86dfda01b5d8a90e1547f158feb134f6d2b757ff8fc77d96d290f72e4c"
   license "MIT"
   head "https://github.com/lc/gau.git", branch: "master"
 
