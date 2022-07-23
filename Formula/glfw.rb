3203650aaddc0a18498ac6279582b8edc491bc83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-23 08:04:57 +0000

    glfw: update 3.3.8 bottle.

--- Formula/glfw.rb
+++ Formula/glfw.rb
@@ -7,12 +7,12 @@ class Glfw < Formula
   head "https://github.com/glfw/glfw.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8ee8f78d6379db633345f9d51bdd9924c0392521b09647e5a0db19231222431b"
-    sha256 cellar: :any,                 arm64_big_sur:  "05b75b5525eb543cd01deb20ff1fda5e606b62bc15d45e7a5d25c47eb7c7835a"
-    sha256 cellar: :any,                 monterey:       "76fa6ed8a21701f59d2d9fc3085f2e4fbd6dab4bf4cf12171ec2e9480fbe3b32"
-    sha256 cellar: :any,                 big_sur:        "2c7bf990e4a3c50884748cf8076f5cf7798a62797523f4100f7a8016e47d42d8"
-    sha256 cellar: :any,                 catalina:       "6732f2291ca4a5438818dda21411f18f37f14649fa13139f8e26988dbe14076e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d667993dc9496b0beebe0c52e8d9b878a76942e47836ff667bc5111b9abafc1d"
+    sha256 cellar: :any,                 arm64_monterey: "d979c217a8fc5751683d81a282adba61ed04cb7f6849fec877ca6c91738b2f29"
+    sha256 cellar: :any,                 arm64_big_sur:  "6cbdaf38af57b2184d5d5fd9100008ebf7ca38920e36c0047b0d10214c806c8c"
+    sha256 cellar: :any,                 monterey:       "38ebd6a36fb6197a334f7bafa907470c87d4f1e9a8b12f0901204ce366c896a0"
+    sha256 cellar: :any,                 big_sur:        "9fdc214eeb24662a4d7ac08a049d5dc0a48b7dbd31de40e1ee1530e343a9cf2a"
+    sha256 cellar: :any,                 catalina:       "7fa08baa7a8e14084d1b2d7a593529c3d38b47fad444af9212b2f37fad83fde5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "348b005f5bacae71410ff4ec043a1079edc349e0926332f1a80cb8834079bdfc"
   end
 
   depends_on "cmake" => :build
