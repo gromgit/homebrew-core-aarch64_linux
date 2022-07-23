5bdd611edc0c9abd060b30d815ace60f3477dc4a
Porkepix <porkepix@gmail.com>
2022-07-23 02:59:56 +0200

    z3 4.10.1
    Closes #106424.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/z3.rb
+++ Formula/z3.rb
@@ -1,8 +1,8 @@
 class Z3 < Formula
   desc "High-performance theorem prover"
   homepage "https://github.com/Z3Prover/z3"
-  url "https://github.com/Z3Prover/z3/archive/z3-4.9.1.tar.gz"
-  sha256 "ca08ba933481242507b2f8b303c3ebdf5d16b0005d397fb45018321dc639a0d7"
+  url "https://github.com/Z3Prover/z3/archive/z3-4.10.1.tar.gz"
+  sha256 "a86071a03983b3512c44c2bf130adbc3320770dc0198805f6f51c43b0946e11a"
   license "MIT"
   head "https://github.com/Z3Prover/z3.git", branch: "develop"
 
