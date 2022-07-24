9f70fcec05f8bde412417b75162bc27aa78ab8bd
Yonas Kolb <yonaskolb@users.noreply.github.com>
2022-07-24 16:16:30 +1000

    xcodegen 2.31.0
    Closes #106469.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xcodegen.rb
+++ Formula/xcodegen.rb
@@ -1,8 +1,8 @@
 class Xcodegen < Formula
   desc "Generate your Xcode project from a spec file and your folder structure"
   homepage "https://github.com/yonaskolb/XcodeGen"
-  url "https://github.com/yonaskolb/XcodeGen/archive/2.30.0.tar.gz"
-  sha256 "1f1d65166389da442c43442ad8d8cc1964be379b110371fd774bb5380cbb6a6d"
+  url "https://github.com/yonaskolb/XcodeGen/archive/2.31.0.tar.gz"
+  sha256 "df8b44a03c3c79122d87c179fa8d6b86314b7a8882ba02c7773d7a04120da565"
   license "MIT"
   head "https://github.com/yonaskolb/XcodeGen.git", branch: "master"
 
