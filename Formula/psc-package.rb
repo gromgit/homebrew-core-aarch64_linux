53746986d5b77f05e27d4720afa6af92395cde72
mhmdanas <triallax@tutanota.com>
2022-07-24 17:12:23 +0300

    psc-package: fix build
    Related to #94212.
    
    Closes #106481.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/psc-package.rb
+++ Formula/psc-package.rb
@@ -4,7 +4,7 @@ class PscPackage < Formula
   url "https://github.com/purescript/psc-package/archive/v0.6.2.tar.gz"
   sha256 "96c3bf2c65d381c61eff3d16d600eadd71ac821bbe7db02acec1d8b3b6dbecfc"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "54f1d5c06e4c59a36e9cd96aa826dce5fce68e13d3cd6572ded1133b90d26fde"
@@ -25,6 +25,9 @@ class PscPackage < Formula
     sha256 "e49585ff8127ccca0b35dc8a7caa04551de1638edfd9ac38e031d1148212091c"
   end
 
+  # Another patch to fix build. See https://github.com/purescript/psc-package/pull/169.
+  patch :DATA
+
   def install
     system "cabal", "v2-update"
     system "cabal", "v2-install", *std_cabal_v2_args
@@ -38,3 +41,28 @@ class PscPackage < Formula
     assert_match "Install complete", shell_output("#{bin}/psc-package install")
   end
 end
+
+__END__
+diff --git a/app/Types.hs b/app/Types.hs
+index e0a6b73..3614dab 100644
+--- a/app/Types.hs
++++ b/app/Types.hs
+@@ -10,6 +10,7 @@ module Types
+ 
+ import           Control.Category ((>>>))
+ import           Data.Aeson (FromJSON, ToJSON, FromJSONKey(..), ToJSONKey(..), ToJSONKeyFunction(..), FromJSONKeyFunction(..), parseJSON, toJSON, withText)
++import           Data.Aeson.Types (toJSONKeyText)
+ import qualified Data.Aeson.Encoding as AesonEncoding
+ import           Data.Char (isAscii, isLower, isDigit)
+ import           Data.Text (Text)
+@@ -34,9 +35,7 @@ fromText t =
+ 
+ instance ToJSONKey PackageName where
+   toJSONKey =
+-    ToJSONKeyText
+-      runPackageName
+-      (AesonEncoding.text . runPackageName)
++    toJSONKeyText runPackageName
+ 
+ instance FromJSONKey PackageName where
+   fromJSONKey =
