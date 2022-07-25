07b093908f1e4dc49d9592d77d21d3082fc90f8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-25 05:21:43 +0000

    esbuild 0.14.50
    Closes #106503.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/esbuild.rb
+++ Formula/esbuild.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Esbuild < Formula
   desc "Extremely fast JavaScript bundler and minifier"
   homepage "https://esbuild.github.io/"
-  url "https://registry.npmjs.org/esbuild/-/esbuild-0.14.49.tgz"
-  sha256 "4bf7f5fdee4568b517536a929bea54d56042b574a695fc929b23bf12c53ae49e"
+  url "https://registry.npmjs.org/esbuild/-/esbuild-0.14.50.tgz"
+  sha256 "af0a48f7521ccde10e2f49db1c4bb9155530921a9a4cfdb9a774bdbabb5b12e3"
   license "MIT"
 
   bottle do
