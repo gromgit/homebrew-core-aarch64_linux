326f2a71fe09478d99e6e2812ddb3c273cdc6172
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-07-25 11:03:46 +0000

    tere: add 1.1.0 bottle.

--- Formula/tere.rb
+++ Formula/tere.rb
@@ -6,6 +6,15 @@ class Tere < Formula
   license "EUPL-1.2"
   head "https://github.com/mgunyho/tere.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c90a30f6d79dd77be92534b3dbe0db18204798a2c6464b42544078867f8fd7b9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "797b411bb4f8069362bc7809cad0fe149d1a1f57a32d0214cf1d6835e7469c31"
+    sha256 cellar: :any_skip_relocation, monterey:       "42c122cb50d28655f99301d7edb9a3e6a449977810b99eaa347ebbfc5a31d3c0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "12a353a4d351ac2775958546090e1a4a17a8048807cee5219250241176a473b8"
+    sha256 cellar: :any_skip_relocation, catalina:       "0ace0a80b9b94ef938f79d71327a5f787ed5a8f3eefd4251c5b09135c07ed76b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e6a04c0104dc6b03238944e3011c288935911439b65c99d0e0b488340a0ce67c"
+  end
+
   depends_on "rust" => :build
 
   def install
