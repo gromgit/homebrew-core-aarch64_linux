fa998bd13908c331993e486b3444645808f256b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-25 22:04:54 +0000

    tth: update 4.16 bottle.

--- Formula/tth.rb
+++ Formula/tth.rb
@@ -10,14 +10,12 @@ class Tth < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f37a1410128cc821a0d786a3e016010537191a31dd6d04bc0e38a1d924d5f2ba"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "087ea392bb9b6de0921381bbf9b635e5dfe6e6c2d75e00e2143d5b6506e6cb14"
-    sha256 cellar: :any_skip_relocation, monterey:       "fcc42fa0ad0c88c57b2659ec16959520824e7fff9573c744cd4642aa105b74c8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9f8333051d4b9025641302175bac6db82a883dc722c8dd8286a424af16cab3a1"
-    sha256 cellar: :any_skip_relocation, catalina:       "eba438c9ec2902578cd3a043fdfbc1fa6bf22bedbde36e868c4fe02740448b06"
-    sha256 cellar: :any_skip_relocation, mojave:         "a0fcd3029a8a43a59a530537c5fef4df45c0155924f41b07ac14ec2362d73ba7"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "dc8224b651a9bbb3ab8a5f5dd2e213bfd42518962bd7132cb3d8a2db12ddd5bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa2cdf102a31eaee0fe9a64f9d5e0db28fc5284b02f0d005c2d91d473a574514"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a80f7935ebe70d616800844afada0ab97f6d9f6ef0ab486dd2905444692e0df"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cebda67056a4417f92be161280b8cc077db34fac85cbfa5e4a660b0620913263"
+    sha256 cellar: :any_skip_relocation, monterey:       "8412801643019ab0fbd7642b63a165e895b86ab590ee41a9a4477cd27913f059"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7e6b2ec14f714eba733cc395c3bc9f2508834a8b26d8d3f67d973ee10a43d2d"
+    sha256 cellar: :any_skip_relocation, catalina:       "3e3902e915b07ebd6527c8ae2dd755b9134c97ff0efad473803ed8cca06f5a08"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c446bed3720c8c0492ecf875b8b14069e32d593b887ba5ccd956dc604e4a913e"
   end
 
   def install
