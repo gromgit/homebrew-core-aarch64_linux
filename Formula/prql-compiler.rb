f03b29a5db25e83fe578d6f2f20733f2b71655d1
Andrew Kane <andrew@ankane.org>
2022-07-25 09:07:40 -0700

    prql-compiler 0.2.2 (new formula)
    Closes #106489.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Caleb Xu <3536482+alebcay@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/prql-compiler.rb
@@ -0,0 +1,19 @@
+class PrqlCompiler < Formula
+  desc "Simple, powerful, pipelined SQL replacement"
+  homepage "https://prql-lang.org"
+  url "https://github.com/prql/prql/archive/refs/tags/0.2.2.tar.gz"
+  sha256 "c23d30611b4851f8d62297eca8e3851b16772548dfde00ca2df23e17e8264cd0"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "prql-compiler")
+  end
+
+  test do
+    stdin = "from employees | filter has_dog | select salary"
+    stdout = pipe_output("#{bin}/prql-compiler compile", stdin)
+    assert_match "SELECT", stdout
+  end
+end
