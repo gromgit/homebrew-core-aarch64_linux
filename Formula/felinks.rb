bbbb5bb22547d3cb1199099de49c5801d80e2d4e
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-07-25 11:03:12 +0000

    felinks: add 0.15.0 bottle.

--- Formula/felinks.rb
+++ Formula/felinks.rb
@@ -6,6 +6,15 @@ class Felinks < Formula
   license "GPL-2.0-only"
   head "https://github.com/rkd77/elinks.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "7a070cd5babe443b93682187f5e21a4a7af5511325742051bcf3aeb0dd2b54a0"
+    sha256 cellar: :any,                 arm64_big_sur:  "53cba55f13fc1a0116269e0ee3d4008e68e531445a4cbd346372f06ccdee49af"
+    sha256 cellar: :any,                 monterey:       "33c90d200713f6b3ea7cfe769b9ed58d12b628e36db1a81b25adb2a3fb41d80e"
+    sha256 cellar: :any,                 big_sur:        "d238453ea8cc15e9b620c51b3d8eb04829109cd93972811e6de4da691b6a9f78"
+    sha256 cellar: :any,                 catalina:       "78b779010dce7a5329f9e403e6cb542e591a53e190dfd067b4c7074b8cf299b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e83d713d25d71bdca270848cde391df40879c94c1fd84e45c8112ee53e74c27"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "groff" => :build
