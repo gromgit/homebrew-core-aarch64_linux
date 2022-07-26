fda2e0c5fe7b1dbb7d042f0634d09d20dbc22126
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-26 13:00:17 +0000

    prql-compiler: add 0.2.2 bottle.

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -5,6 +5,15 @@ class PrqlCompiler < Formula
   sha256 "c23d30611b4851f8d62297eca8e3851b16772548dfde00ca2df23e17e8264cd0"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "18802d861d4f11cc33621b67cdbca4290a46982e29beec26d107e5bd01b2de4d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a3b282e30a3ff33887874faabd2fd9db3d9bda5070f8eba985825f2b56f34d4a"
+    sha256 cellar: :any_skip_relocation, monterey:       "3684c9b169907a1b3bf18474254801212426d9f6e67e4900d9a88a1a96e98c45"
+    sha256 cellar: :any_skip_relocation, big_sur:        "819efa366f783ee03e0bd0f42daea3f08260c8a3221b0db46ee341d11418200b"
+    sha256 cellar: :any_skip_relocation, catalina:       "4f207403b24b939275658b2e4fbad0cf40da733bd236d3b873553a2e35a34ef2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb847f49f14fbd77f5969de453f70123a250b108ce610ac6fad4521a8e25fcae"
+  end
+
   depends_on "rust" => :build
 
   def install
