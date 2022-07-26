f11401d384226ab64dc405512fa986353567b348
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-26 09:05:13 +0000

    driftctl: update 0.36.0 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -6,12 +6,12 @@ class Driftctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "470eaf851537187c46780cadf55a2af2c73986171924e89a9c6fa802599cf0f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "470eaf851537187c46780cadf55a2af2c73986171924e89a9c6fa802599cf0f9"
-    sha256 cellar: :any_skip_relocation, monterey:       "438badb9043df400bacc37a423ccc46306ac30fddf66bbdf3cd19f641a26a3c1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "438badb9043df400bacc37a423ccc46306ac30fddf66bbdf3cd19f641a26a3c1"
-    sha256 cellar: :any_skip_relocation, catalina:       "438badb9043df400bacc37a423ccc46306ac30fddf66bbdf3cd19f641a26a3c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c57042f6202ce67f813d478aab11840767c1d1560aad29af939ee3348806ce9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d21e8c83225a0ffff6f6c9728267fe6eb3eb7ece92d66464c3f3c572608daf0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d21e8c83225a0ffff6f6c9728267fe6eb3eb7ece92d66464c3f3c572608daf0"
+    sha256 cellar: :any_skip_relocation, monterey:       "94da93082ff8911c56a911ae2da35566f517d0bdc6d9f6fcb4ccfa5d39f070e1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "94da93082ff8911c56a911ae2da35566f517d0bdc6d9f6fcb4ccfa5d39f070e1"
+    sha256 cellar: :any_skip_relocation, catalina:       "94da93082ff8911c56a911ae2da35566f517d0bdc6d9f6fcb4ccfa5d39f070e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88617b8800a8523981d6de4b183f68c625e34d9390c4e36f1f036f95c02107ba"
   end
 
   depends_on "go" => :build
