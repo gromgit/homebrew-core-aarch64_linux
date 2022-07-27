6f59a394d077bdf60b6cabf999dfdece0478b0aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-27 05:48:09 +0000

    oci-cli 3.12.0
    Closes #106647.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oci-cli.rb
+++ Formula/oci-cli.rb
@@ -3,8 +3,8 @@ class OciCli < Formula
 
   desc "Oracle Cloud Infrastructure CLI"
   homepage "https://docs.cloud.oracle.com/iaas/Content/API/Concepts/cliconcepts.htm"
-  url "https://files.pythonhosted.org/packages/98/25/1337831a945c908821e10fa974f9f5a5190c8ecf5d4aa79723240476af5e/oci-cli-3.11.1.tar.gz"
-  sha256 "d3bee084f28f9ae98320dc20ef06eb0e745247a650bc378e458705e799b35f8a"
+  url "https://files.pythonhosted.org/packages/46/75/400bdf19f854637d0dcba118dd774035c07d741eb6f3faf27ac51bd6bae2/oci-cli-3.12.0.tar.gz"
+  sha256 "b8264a32692593ee57cf77fbd44ffd24777d5c39384ac4140275f2f7cdd497ad"
   license any_of: ["UPL-1.0", "Apache-2.0"]
   head "https://github.com/oracle/oci-cli.git", branch: "master"
 
@@ -57,8 +57,8 @@ class OciCli < Formula
   end
 
   resource "oci" do
-    url "https://files.pythonhosted.org/packages/cd/14/8624d70ce32325a2230a35d031b1941540eecf5f14e6b026271bf15dbd7e/oci-2.75.1.tar.gz"
-    sha256 "7d214cba990d5fae0463890c54f1c5a27e24be450062f55a6934816c842c16b8"
+    url "https://files.pythonhosted.org/packages/4d/7f/e90e23e549b96bf2cd8276b1e431020f8ceb2c9a1f35a1b33cc46a133e99/oci-2.76.0.tar.gz"
+    sha256 "219a806716e3fa36126413c79cfd1e48d2f05ce0101cc6198ddc3442858613d7"
   end
 
   resource "prompt-toolkit" do
