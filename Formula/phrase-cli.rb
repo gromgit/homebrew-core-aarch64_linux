56aae95961606d7de8adc3ee2dafc0543aada367
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-27 14:06:50 +0000

    phrase-cli: update 2.5.0 bottle.

--- Formula/phrase-cli.rb
+++ Formula/phrase-cli.rb
@@ -6,12 +6,12 @@ class PhraseCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4fcb40e8faa86f3e686b0598b8fe0e9304a480eb152d812d1320365fe8ee3b97"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "096c75f210dcc8c097ca89377cf01a364035695290b1988ac9ca10ffe88129fb"
-    sha256 cellar: :any_skip_relocation, monterey:       "82324ca1225778d75328f6f06ff65c2218e3c447f4ada4eaeaa12cde9d34fa62"
-    sha256 cellar: :any_skip_relocation, big_sur:        "29e960fc9e1d974ce80de9adbdd8713d4ed55f19350629a4bc9ea0025fafbd78"
-    sha256 cellar: :any_skip_relocation, catalina:       "fe7550a4af2d2dcef2b8eadd5ec77529b8504b52c649b1b6323c1be31bd35955"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85ae1d093038c152d31272f0fb4d494b681bc2062394325d8c723e7a20df9bcf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e8c4ef3d9d2ea22aa4681ee7b65ec4947b6cf1afa761e901e9f7edc82e979bc3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ec88db22bf86d803ad21f6ba38d0fc65f01e3b6a069e6fb72c238ad849657772"
+    sha256 cellar: :any_skip_relocation, monterey:       "52677eb884c6e24297a8f2acd6a296a7162d5eb83f528a7a57615659e2f2e373"
+    sha256 cellar: :any_skip_relocation, big_sur:        "06830a601514fecb63525c6e1b11b3777fcad0e4bda772887d92c6e8016e91de"
+    sha256 cellar: :any_skip_relocation, catalina:       "8ee764e04803f08c73cd909635b1e0d3b2f3d980b09839420a04c42b6d700e3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fecc5702b2e82cd17f55511c5c13189e2e7c1ca49b563d2c4b7203e99fbabfbf"
   end
 
   depends_on "go" => :build
