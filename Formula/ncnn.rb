ca5c4481b1bf2748b8704d26aed9e305cec88c35
Porkepix <porkepix@gmail.com>
2022-07-27 02:36:22 +0200

    ncnn 20220721
    Closes #106615.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ncnn.rb
+++ Formula/ncnn.rb
@@ -1,10 +1,9 @@
 class Ncnn < Formula
   desc "High-performance neural network inference framework"
   homepage "https://github.com/Tencent/ncnn"
-  url "https://github.com/Tencent/ncnn/archive/refs/tags/20220701.tar.gz"
-  sha256 "e222f2ac54cc52ed7b1f6edc83ce4bf8ea1dc729c81f6a1560e4196032ec88ca"
+  url "https://github.com/Tencent/ncnn/archive/refs/tags/20220721.tar.gz"
+  sha256 "67d0331203c5041e25526e9a1cb879779024ce262439c5cc80aa7f1e8650524e"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/Tencent/ncnn.git", branch: "master"
 
   bottle do
