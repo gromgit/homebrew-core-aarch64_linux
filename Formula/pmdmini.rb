af5510e2dfd1cc2c0bfbb38caf34f8061f09f7df
Misty De Meo <mistydemeo@gmail.com>
2022-07-26 19:25:11 -0700

    pmdmini 2.0.0
    * pmdmini 2.0.0
    * pmdmini: fix build on Linux
    * pmdmini: see if CFLAGS work on macOS
    * pmdmini: add PR link
    
    Closes #106629.
    
    Co-authored-by: danielnachun <daniel.nachun@gmail.com>
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pmdmini.rb
+++ Formula/pmdmini.rb
@@ -1,10 +1,9 @@
 class Pmdmini < Formula
   desc "Plays music in PC-88/98 PMD chiptune format"
   homepage "https://github.com/mistydemeo/pmdmini"
-  url "https://github.com/mistydemeo/pmdmini/archive/v1.0.1.tar.gz"
-  sha256 "5c866121d58fbea55d9ffc28ec7d48dba916c8e1bed1574453656ef92ee5cea9"
-  license "GPL-2.0"
-  revision 1
+  url "https://github.com/mistydemeo/pmdmini/archive/v2.0.0.tar.gz"
+  sha256 "e3288dcf356e83ef4ad48cde44fcb703ca9ce478b9fcac1b44bd9d2d84bf2ba3"
+  license "GPL-2.0-or-later"
 
   bottle do
     sha256 cellar: :any, arm64_monterey: "1bdf417d67bfd4312694a9c5e8cdc70d1ed9c749d39a7082ac92fff2210c8fec"
@@ -14,19 +13,22 @@ class Pmdmini < Formula
     sha256 cellar: :any, catalina:       "422add6136c6829f5ba656e0a052dd2c03f118c665a3fc3bb74a3e4091d8f2e5"
   end
 
-  depends_on "sdl12-compat"
+  depends_on "sdl2"
 
   resource "test_song" do
     url "https://ftp.modland.com/pub/modules/PMD/Shiori%20Ueno/His%20Name%20Is%20Diamond/dd06.m"
     sha256 "36be8cfbb1d3556554447c0f77a02a319a88d8c7a47f9b7a3578d4a21ac85510"
   end
 
+  # Add missing include
+  # Upstreamed here: https://github.com/mistydemeo/pmdmini/pull/3
+  patch :DATA
+
   def install
-    # Specify Homebrew's cc
-    inreplace "mak/general.mak", "gcc", ENV.cxx
     # Add -fPIC on Linux
-    inreplace "mak/general.mak", "CFLAGS = -O2", "CFLAGS = -fPIC -O2" unless OS.mac?
-    system "make"
+    # Upstreamed here: https://github.com/mistydemeo/pmdmini/pull/3
+    inreplace "mak/general.mak", "CFLAGS = -O2", "CFLAGS = -fPIC -O2 -fpermissive"
+    system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "LD=#{ENV.cxx}"
 
     # Makefile doesn't build a dylib
     flags = if OS.mac?
@@ -41,8 +43,8 @@ class Pmdmini < Formula
 
     bin.install "pmdplay"
     lib.install "libpmdmini.a", shared_library("libpmdmini")
-    (include+"libpmdmini").install Dir["src/*.h"]
-    (include+"libpmdmini/pmdwin").install Dir["src/pmdwin/*.h"]
+    (include/"libpmdmini").install Dir["src/*.h"]
+    (include/"libpmdmini/pmdwin").install Dir["src/pmdwin/*.h"]
   end
 
   test do
@@ -65,3 +67,13 @@ class Pmdmini < Formula
     assert_equal "mus #06", result
   end
 end
+
+__END__
+diff --git a/sdlplay.c b/sdlplay.c
+index 14c721e..1338cf9 100644
+--- a/sdlplay.c
++++ b/sdlplay.c
+@@ -1,3 +1,4 @@
++#include <signal.h>
+ #include <stdio.h>
+ #include <SDL.h>
