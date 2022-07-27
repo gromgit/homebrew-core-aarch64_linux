84b568fe98b0ed970866fb10bc90018d579ab2a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-27 05:21:37 +0000

    circleci 0.1.20323
    Closes #106635.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.20239",
-      revision: "571ae88aea10d2f551932a3a901d0591ccfa2b28"
+      tag:      "v0.1.20323",
+      revision: "990c791ab36038b0ac3dc0d0be16bc840a11745f"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
