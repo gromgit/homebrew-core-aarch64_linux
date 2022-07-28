c0b7873de9fb1e7998d10c34429a8a3c77d2304f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-28 12:16:28 +0000

    tlsx: add 0.0.5 bottle.

--- Formula/tlsx.rb
+++ Formula/tlsx.rb
@@ -6,6 +6,15 @@ class Tlsx < Formula
   license "MIT"
   head "https://github.com/projectdiscovery/tlsx.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2546597a2dbc490f555f5db8d372c3f2e443d2458afe19b776d4ebf55fd93a4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "efe4c6c23fc4feb5204ce943fd74801ef0311f8f39797db2eefe1b1813e99c5d"
+    sha256 cellar: :any_skip_relocation, monterey:       "0f3f8c81a9a016a79b770a0e673561911429198d5e36fe8f741fe587e4dc7485"
+    sha256 cellar: :any_skip_relocation, big_sur:        "237012a6cf3fb26ad8e809050bae494a16bd029fe79611144dda22c46787af78"
+    sha256 cellar: :any_skip_relocation, catalina:       "579aa97a7ca57b3be3551d581210a783cf748458401197df68600b2949ef704d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23f0cf20239ae66d4075d8c59476ed21dc426c689a637297052f414e6f6d33fd"
+  end
+
   depends_on "go" => :build
 
   def install
