9b33f4efc25cdedcd7e096658f444ea6096373e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-28 11:05:05 +0000

    gsasl: update 2.0.1 bottle.

--- Formula/gsasl.rb
+++ Formula/gsasl.rb
@@ -7,13 +7,12 @@ class Gsasl < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "8f14a65d314339266a3b82be273714e0450b9e392a96372c691e736e2422f6a8"
-    sha256 cellar: :any, arm64_big_sur:  "29fe198c3ae4c00a487b94cb8e711cb1c293c3a28bd0fb21f6f56e18cf1c1e5e"
-    sha256 cellar: :any, monterey:       "7d76bbbafef8b2d065d6c32289e5204620c0a8ef888a3758aca842748350c977"
-    sha256 cellar: :any, big_sur:        "42ef3c24427817c75c74804f31cd0d039a9900c8da5f96dbf9ab7b76e2563168"
-    sha256 cellar: :any, catalina:       "6f7aa6d0a2276a8d3434f2c16cfd7f60d85fbb4204194dcf6a678b7bb8c4e0f2"
-    sha256 cellar: :any, mojave:         "f9518412c5a6f631a78ef1ed3ed8989914446f2be1bad0de786ad82dc4c190e1"
-    sha256               x86_64_linux:   "4b5524f680771e938f850d849bbb39e71f2f89fe95fe5626fe129f961a2e6505"
+    sha256 cellar: :any, arm64_monterey: "dbe49591e9788dc2c44090bbae030d3541d527d1245eb739f213b16a2e04ffba"
+    sha256 cellar: :any, arm64_big_sur:  "b9ed09d62ce6512a251f0f2854ea8b9ca1ffa55dc60295d734aa3b6657067960"
+    sha256 cellar: :any, monterey:       "455b3d081fdd6975bb912c0e98c0aad7847e832d290257746a2877d77044fafc"
+    sha256 cellar: :any, big_sur:        "e227dc44fb334144a7227819b63b2ca3b874ef3c65a1c42baece683769a50bc5"
+    sha256 cellar: :any, catalina:       "bdafb4164a5d387abf68523afae178d0646624002f1df628c1a20dd27f82d282"
+    sha256               x86_64_linux:   "5da4b3ddd5711f42eaf0fd27278489408122c3896337f8bec1a38fdd95344675"
   end
 
   depends_on "libgcrypt"
