0648921607cd2db930c55daf9c997513fb4b9ed7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-28 08:05:15 +0000

    rover: update 0.8.0 bottle.

--- Formula/rover.rb
+++ Formula/rover.rb
@@ -7,12 +7,12 @@ class Rover < Formula
   head "https://github.com/apollographql/rover.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1ac00e0c8d06abd182f43ec35bffc2820559f60aeb9a8ed0d163c86511dcbad3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "21f398e14823a077be8d2a9a08bc0a37645c6a6dbc00a852d275c1795e9c8ea9"
-    sha256 cellar: :any_skip_relocation, monterey:       "1316817da978952466178b1c883ac370ab9ef091b27677405ae00f13eedfbf7d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e15ae8e4ca72171cdf2804267a88d4af73c36d86714706d52b728103d042f6d2"
-    sha256 cellar: :any_skip_relocation, catalina:       "8e422120b530996d36e10694edb57149c2dcae9483229b6697a3f5f1a5416791"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "215bdc4e753af6d58ea001696a5266245c00636df1c7478946fbf5ea97d9c1c3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dffb4922afd9eab5e5af01f7d102ad59b8a2c71e3a49ba7176f34166a44ef58d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "de67121f2521a76986da9f78c4e90b8c7dddeebff0129e84627e7ef370d63b1d"
+    sha256 cellar: :any_skip_relocation, monterey:       "6e6e5901c5ccddf3191a20d3ed73a15dda5e8b532f8fc8c65a0c83349d49faef"
+    sha256 cellar: :any_skip_relocation, big_sur:        "95c4d83bd9f590fc484dc85965e848b0038e55500d613492723375e67fb29f46"
+    sha256 cellar: :any_skip_relocation, catalina:       "2ee8724291f6ec8423f1416d5a0be0c57db7826e144152baff0e758ba3b14a39"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c539f729424f4aedb21f48fefb53003c92d6f034f65ed1e74c7d8180093f4991"
   end
 
   depends_on "rust" => :build
