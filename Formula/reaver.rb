53b77aaf21a0bf620a509b5453571613572555a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-28 08:05:15 +0000

    reaver: update 1.6.6 bottle.

--- Formula/reaver.rb
+++ Formula/reaver.rb
@@ -6,16 +6,12 @@ class Reaver < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "c5aebd64ad2c3055146fea9e91914d689914d1aab4e945f354264183a190bd97"
-    sha256 arm64_big_sur:  "c4ba873b94ad8d7f968474660417e7af8ea607f9b42d3971533f84377a56a3d2"
-    sha256 monterey:       "2c8d6a61a5f75a853946a9f982c81b7d1c37f5ddd9fafaf61d862bb2dfb24be3"
-    sha256 big_sur:        "0cd13169a7c0f7603fab7ec7ac55e8896ffd5518c4e790a59183e894291a5cab"
-    sha256 catalina:       "73539f97836b5df80e030e429eb7f209dec3067c14b1bfd6753bcf7796c1f541"
-    sha256 mojave:         "386ed8ae2562ae032f0d622d52d7302be2e99bbe671f1ca5ba3acb88b86f6417"
-    sha256 high_sierra:    "c2c6d45abc45f5639b74da0bdb294a5ee83548f548642f6b61d764d05801352e"
-    sha256 sierra:         "e7fc0f43b7a306d5fe2baaf4b41e9ce445db12e2e74d41904b3de5d2c372741d"
-    sha256 el_capitan:     "d9adddf27928b284492cc87b565d2748490c1017b0b463bc15223c935f63bb6c"
-    sha256 x86_64_linux:   "1a1e6c2edefa97af1a4295d859695c6dbc055a47676f9ec845916362bcc884f6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3e4fe541cec1d31a1e6836829dcf050586e385cd030664c8971f9aac369e2313"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa4d8cefff1f3c59bcb4cad2a696bc5e16c1f82d201afe7bc78b22f28f182493"
+    sha256 cellar: :any_skip_relocation, monterey:       "69e3fcd6c69e7e71bfdbf84a780f466097163cc8a34f8928350d02b4fb57a1ce"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ec036d6b49bce14bfdee5ff3f7f03468148d60b48c06ee75bfd156a9c387baf1"
+    sha256 cellar: :any_skip_relocation, catalina:       "e4f10cca5698e3ae3d03841e67d84d277d8e05a06c181567a2a1de5ad74f40a3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5301d082e3bd0a833d6b352e994d3b39edced488a46ef597b17aaa1e25669989"
   end
 
   depends_on "pixiewps"
