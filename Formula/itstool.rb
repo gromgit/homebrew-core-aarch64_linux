af805d0a4a4069b3111a0b74226cba3c368d00b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-29 06:05:13 +0000

    itstool: update 2.0.7_1 bottle.

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -8,13 +8,12 @@ class Itstool < Formula
   head "https://github.com/itstool/itstool.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
-    sha256 cellar: :any_skip_relocation, monterey:       "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, big_sur:        "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, catalina:       "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, mojave:         "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "108962bdce11e7b353a94f61ae30c82212295963d157cd667fb9af6069801cbd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "108962bdce11e7b353a94f61ae30c82212295963d157cd667fb9af6069801cbd"
+    sha256 cellar: :any_skip_relocation, monterey:       "211f56048a0498fd935fac91466be64590276ae236d5a60be45ff2061713087f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "211f56048a0498fd935fac91466be64590276ae236d5a60be45ff2061713087f"
+    sha256 cellar: :any_skip_relocation, catalina:       "211f56048a0498fd935fac91466be64590276ae236d5a60be45ff2061713087f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "108962bdce11e7b353a94f61ae30c82212295963d157cd667fb9af6069801cbd"
   end
 
   depends_on "autoconf" => :build
