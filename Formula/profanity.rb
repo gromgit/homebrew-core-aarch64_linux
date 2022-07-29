43e6246cda463ae40c5602f650b1929d8b97c335
Michael Cho <cho-m@tuta.io>
2022-07-29 10:39:21 -0700

    profanity: migrate to `python@3.10`
    Closes #106876.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -4,7 +4,7 @@ class Profanity < Formula
   url "https://profanity-im.github.io/tarballs/profanity-0.12.1.tar.gz"
   sha256 "e344481e7bf3b16baf58a169d321b809c4700becffb70db6f1c39adc3fad306e"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 arm64_monterey: "4491270163d04d1d5874bf2b5e53867e68ba8dc1f9a3748b8a7b3e05c53d0446"
@@ -16,7 +16,7 @@ class Profanity < Formula
   end
 
   head do
-    url "https://github.com/profanity-im/profanity.git"
+    url "https://github.com/profanity-im/profanity.git", branch: "master"
 
     depends_on "autoconf" => :build
     depends_on "autoconf-archive" => :build
@@ -33,7 +33,7 @@ class Profanity < Formula
   depends_on "libsignal-protocol-c"
   depends_on "libstrophe"
   depends_on "openssl@1.1"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "readline"
 
   on_macos do
@@ -41,16 +41,15 @@ class Profanity < Formula
   end
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python@3.10"].opt_libexec/"bin"
 
     system "./bootstrap.sh" if build.head?
 
     # We need to pass `BREW` to `configure` to make sure it can be found inside the sandbox in non-default
     # prefixes. `configure` knows to check `/opt/homebrew` and `/usr/local`, but the sanitised build
     # environment will prevent any other `brew` installations from being found.
-    system "./configure", "--disable-dependency-tracking",
+    system "./configure", *std_configure_args,
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}",
                           "BREW=#{HOMEBREW_BREW_FILE}"
     system "make", "install"
   end
