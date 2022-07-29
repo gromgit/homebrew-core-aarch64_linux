43fe40ba3110900f367826bb669606b5ef4b6fe5
svc-devtoolsbot <79531021+svc-devtoolsbot@users.noreply.github.com>
2022-07-28 23:50:46 -0400

    mongosh 1.5.3
    Closes #106812.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Mongosh < Formula
   desc "MongoDB Shell to connect, configure, query, and work with your MongoDB database"
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-1.5.2.tgz"
-  sha256 "980158d90fe23df978e80b82b738c6deaf2fef4adc64b050332a0da4db7505ae"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-1.5.3.tgz"
+  sha256 "529886214e7bb67cbed7f79f931fa2108f67b780dc4ae2af64f052a4cc53aded"
   license "Apache-2.0"
 
   bottle do
