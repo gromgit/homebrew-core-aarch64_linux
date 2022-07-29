027899f88f457fae58ce1306f7bb9946f693adbd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-29 13:17:19 +0000

    drill: update 0.8.1 bottle.

--- Formula/drill.rb
+++ Formula/drill.rb
@@ -7,12 +7,12 @@ class Drill < Formula
   head "https://github.com/fcsonline/drill.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f52cd08c09b0d24b2ec87387e4ed6a1dc66460dc4d3c10dcd400718ffbce81a4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7b4433825dbb0805b99581c92f797170bbe96ea973859feaf3cf2bf5e6a00a8"
-    sha256 cellar: :any_skip_relocation, monterey:       "0c7bdb0d36a979d56495d07233af47b3bfe373833591e620c8333c10158bc487"
-    sha256 cellar: :any_skip_relocation, big_sur:        "35f3f0ee64c57c2eb4cc27c757eada8b5ba2f17c790baa86bb2f1048888df66f"
-    sha256 cellar: :any_skip_relocation, catalina:       "d2a18dd4b009cde8029a99a0b8a8dad9582e23484938eca039afe4ddd9fa5912"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "017593add7b77348f32eaa071c2ed94571bc377c8286b5c05f7fb2683cfa92ec"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f0b4313cb4832e360a70f570affa8de41b10c3ee811c09f8aab13f7cd7a7ce71"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3e43c6d9e3ef9ca48962b7951afbb98dd6dc38d57821d618cf8b6dc4a2ca9083"
+    sha256 cellar: :any_skip_relocation, monterey:       "4803852249e8bc6b3b8e4670e4bc4c3c7760c2ba836376a56beb1726eb5f5321"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2fcc32b1d88e24a1194a74792da6a8f3f0e914407c44a4996ad18e9f89e44559"
+    sha256 cellar: :any_skip_relocation, catalina:       "28ba37e907b166f55c3c17a0a598fcf1c56214f906c99aabcb607a4d9c40696b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cbfe9b9c63b2fa46ed44ac9823d72e3f6fa53bc7649bc9c2583b2e578fc2236e"
   end
 
   depends_on "rust" => :build
