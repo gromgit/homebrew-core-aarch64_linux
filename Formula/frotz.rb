3702fdf5dd3b7f646b828a71b9712eb4eb304244
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-29 11:05:24 +0000

    frotz: update 2.54_1 bottle.

--- Formula/frotz.rb
+++ Formula/frotz.rb
@@ -8,12 +8,12 @@ class Frotz < Formula
   head "https://gitlab.com/DavidGriffith/frotz.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "549071e77d620452882d0de93e6f9ea8c1626841f5061c78a3e61a611bbb12ce"
-    sha256 arm64_big_sur:  "b5508362bd7be4cbebbd2d927cad4a22454122534d03908c60d2fa5c28be89c7"
-    sha256 monterey:       "4eeaf3343a939186a6151d565abf4c08416b2759f096e8acdc7466bb474dc454"
-    sha256 big_sur:        "21ff203a161a1ef98a86b2cab28e8ec5f1ab15ef1529debff8a69342158851e7"
-    sha256 catalina:       "973fe5f8cc67f7679260e4bf6779bbd739990c4cb077415d5e5d6be72add919e"
-    sha256 x86_64_linux:   "ea64d973cee2ef29d37d4b975b8d6c522df4b60a0e9b6d632397b078003734a5"
+    sha256 arm64_monterey: "a0c7658d7b137bea28fdb1b1f577d91c103158c31d30082f8bfbb352c6d72edb"
+    sha256 arm64_big_sur:  "81e96f649f4c6b2f2d530effe32d1d5ab48a39c9aa47891b715062f1b768c565"
+    sha256 monterey:       "0af910a56a445405c751aafb5c63df26b0ff4d65e254dd34f832bac4d16ccbe5"
+    sha256 big_sur:        "50c1afd7e1de0f785f7a6a6b0587f5a1a6be7738131c9506d848e81a59d7cc72"
+    sha256 catalina:       "300f82c7b6bf644a6883519eadc1e71d2463f5a8490fdf3b5f5ca3f4202d7e1e"
+    sha256 x86_64_linux:   "43dbed2c89d4671dbe3a9469b3981297a5ef02e53332daa6fb7757eb32b3f875"
   end
 
   depends_on "pkg-config" => :build
