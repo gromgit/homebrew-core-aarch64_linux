d26c9e8c190d6e45bda3922cdbeb2a4b86ac758f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-29 09:04:47 +0000

    git-xargs: update 0.1.1 bottle.

--- Formula/git-xargs.rb
+++ Formula/git-xargs.rb
@@ -6,12 +6,12 @@ class GitXargs < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "34efbd2e17ce5bc1833f30075cfa74da7e9a5fcc1e22a19ea5f0b780178b23c2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa1c98904291d749cbe8241248e1273c546890c48955b44018721247588f7265"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e95040459c34ba3b6cfd5552068d82010c53ad0fc4c8423c743f0363bf16442"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a96d7ff285da8007a6cedb960255f273e8646ef0822d2fd2c3c3babcb604aa65"
-    sha256 cellar: :any_skip_relocation, catalina:       "ee109548b8134701e4b5a7c1dd660ff9f1fae87049b03b31d9dc2369a6111ec6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "116331d201da0648ec2e3253ac141beac7f21840929393b79499690f8f58618f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e49c58def0241dd26f019c7f8a38389d5c4757adadf5c74184b270cc95075ad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "38fb57824be03c96ae7e20e152ff3f7fb6ced837d24ec247e518b19180d82358"
+    sha256 cellar: :any_skip_relocation, monterey:       "4aef9183e49fbc62af2f588575bfa59959368a02fc0195340aa16eb14edcd9b8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8cbc68ea2a1cbacf37f09c027c9b6b6740e2d0a49b6dd8bc55702c3d9bb74e1f"
+    sha256 cellar: :any_skip_relocation, catalina:       "d674cebe3eec7778d00e7654249f09d991934f75b1e6803bfbfec194b7c71824"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bfe672f7648fea6b0a09432219aafb9c5f9b68271a5cb39b4e8888f0ec8c5956"
   end
 
   depends_on "go" => :build
