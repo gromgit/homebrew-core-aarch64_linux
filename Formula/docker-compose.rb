c493c3abb353b6f3d9d219898f92fe051761d7da
Branch Vincent <branchevincent@gmail.com>
2022-07-29 16:06:12 -0400

    docker-compose 2.8.0
    Closes #106881.
    
    Signed-off-by: Zach <zachauten@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,8 +1,8 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.7.0.tar.gz"
-  sha256 "bc3e4c0aadfd3c87e8ebcd89d5236977ba8234a8211fea63351bb752d28883ae"
+  url "https://github.com/docker/compose/archive/v2.8.0.tar.gz"
+  sha256 "fa3e7242c0f3a2158e6ccc20b05dd5a5124cfe8ed96a76b16bb32145f410728d"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
