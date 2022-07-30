e3a389dc0a6d259374df2b2f98a26e307744a34b
Michael Cho <cho-m@tuta.io>
2022-07-30 04:37:55 -0700

    httpstat: use any python3 on Linux
    Closes #106922.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -6,7 +6,7 @@ class Httpstat < Formula
   url "https://github.com/reorx/httpstat/archive/1.3.1.tar.gz"
   sha256 "7bfaa0428fe806ad4a68fc2db0aedf378f2e259d53f879372835af4ef14a6d41"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8d9ac6f7d7da555221bffc4f49b59f60bcc9405439e5d418a49f15a547026dd5"
@@ -21,8 +21,10 @@ class Httpstat < Formula
   uses_from_macos "python"
 
   def install
-    rw_info = OS.mac? ? python_shebang_rewrite_info("/usr/bin/env python3") : detected_python_shebang
-    rewrite_shebang rw_info, "httpstat.py"
+    if OS.linux? || MacOS.version >= :catalina
+      rw_info = python_shebang_rewrite_info("/usr/bin/env python3")
+      rewrite_shebang rw_info, "httpstat.py"
+    end
     bin.install "httpstat.py" => "httpstat"
   end
 
