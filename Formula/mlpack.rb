8e57d808e57ab22a7c9dfe05542a0b643663ca93
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-30 23:36:06 +0800

    mlpack: revision bump (gcc 12.1.0)

--- Formula/mlpack.rb
+++ Formula/mlpack.rb
@@ -4,7 +4,7 @@ class Mlpack < Formula
   url "https://mlpack.org/files/mlpack-3.4.2.tar.gz"
   sha256 "9e5c4af5c276c86a0dcc553289f6fe7b1b340d61c1e59844b53da0debedbb171"
   license all_of: ["BSD-3-Clause", "MPL-2.0", "BSL-1.0", "MIT"]
-  revision 6
+  revision 7
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "f819a5f6a724607ea5bc22e057174099315224c3210f05418717ec02b3df3808"
