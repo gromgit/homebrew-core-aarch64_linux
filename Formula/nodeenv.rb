a3481de5d8fba790944d5986f0e951746c62fd71
Michael Cho <cho-m@tuta.io>
2022-07-30 04:33:35 -0700

    nodeenv: use any python3 on Linux

--- Formula/nodeenv.rb
+++ Formula/nodeenv.rb
@@ -6,6 +6,7 @@ class Nodeenv < Formula
   url "https://github.com/ekalinin/nodeenv/archive/1.7.0.tar.gz"
   sha256 "a9e9e36e1be6439e877c53e7f27ce068f75b82cc08201f2c68471687199cfd7b"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b62820fdf34eb99d433736cc707c41a506f404ff2043418b62a4ab47a46fe99d"
@@ -19,8 +20,10 @@ class Nodeenv < Formula
   uses_from_macos "python"
 
   def install
-    rw_info = OS.mac? ? python_shebang_rewrite_info("/usr/bin/env python3") : detected_python_shebang
-    rewrite_shebang rw_info, "nodeenv.py"
+    if OS.linux? || MacOS.version >= :catalina
+      rw_info = python_shebang_rewrite_info("/usr/bin/env python3")
+      rewrite_shebang rw_info, "nodeenv.py"
+    end
     bin.install "nodeenv.py" => "nodeenv"
   end
 
