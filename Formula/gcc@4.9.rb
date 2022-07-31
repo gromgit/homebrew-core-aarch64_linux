59452d3d11cf638ee223c195dfca65bec9d2f784
Sean Molenaar <smillerdev@me.com>
2022-07-31 20:40:17 +0200

    gcc@4.9: disable

--- Formula/gcc@4.9.rb
+++ Formula/gcc@4.9.rb
@@ -16,7 +16,7 @@ class GccAT49 < Formula
   pour_bottle? only_if: :clt_installed
 
   # https://gcc.gnu.org/gcc-4.9/
-  deprecate! date: "2021-04-11", because: :deprecated_upstream
+  disable! date: "2022-07-31", because: :deprecated_upstream
 
   depends_on maximum_macos: [:high_sierra, :build]
 
