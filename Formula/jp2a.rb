ad914140ce3712cdd800d53ed6bf8c70950b3a20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-31 06:04:55 +0000

    jp2a: update 1.0.6_2 bottle.

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -12,15 +12,12 @@ class Jp2a < Formula
   version_scheme 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "2446f6228b6059e993b3ff60c738449cd11c21d1c3005c133b1ab19d1e85b2fb"
-    sha256 cellar: :any,                 arm64_big_sur:  "d76bf96595bd8e388e19adfaa4fc9d635f497f597ff1a9f1ba2abb3f1c2f3dc1"
-    sha256 cellar: :any,                 monterey:       "39c167e125eed22c08a1c1c8053e7023492ddb1af07b18c9d9aa634b07bbd324"
-    sha256 cellar: :any,                 big_sur:        "8ab042d2d660fbc55e4a055e0618d85fbbd32029f703d35c59f3d12c0b000936"
-    sha256 cellar: :any,                 catalina:       "a9aa7c8893c63ad5621788af3813ed9758e09e0c79b9ba3a8262d5c17b2272f9"
-    sha256 cellar: :any,                 mojave:         "4e62b310889a384daf9058267ac0b1bdc73d2cb408f05b9e3d3072be52355bfd"
-    sha256 cellar: :any,                 high_sierra:    "8400fccf2a4459fe37ce0f3856459127f4f66ff002c356f36942462c0c9c3809"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06ca49b6a1b7d58b2a65e998e0e4636c72a13f5e4af51704c36950ef89818356"
+    sha256 cellar: :any,                 arm64_monterey: "9da454ebb89a11e1074e54975b9bae9f93c4bc38a53025a48c2f7e78c2d59765"
+    sha256 cellar: :any,                 arm64_big_sur:  "81e6bcf5a143c3867f9f883e7e5c5a5c7c38743a646ce6f5fac36eb54ec95c02"
+    sha256 cellar: :any,                 monterey:       "0088d29ea1ec66a0f5ce9f9249b2922fe32d1c3e8c37f32656a444ddae004aa5"
+    sha256 cellar: :any,                 big_sur:        "1e24bbf2f7aa650f995992f6b8ae80938873dfc396ecd23341984188c285c6f9"
+    sha256 cellar: :any,                 catalina:       "438f6b2d10513c8960f67db6e688599ef42282287ae64d8e366815bd4f545e9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdd434fbaabfb9b2f5d10a1cb47b95cc1838c2d91e5e0a25b4fd8ae592055782"
   end
 
   depends_on "jpeg-turbo"
