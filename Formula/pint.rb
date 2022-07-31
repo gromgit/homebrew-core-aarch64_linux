55668bd83efdabb6157af1338fea57519c2ec556
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-31 10:59:45 +0000

    pint: add 0.28.3 bottle.

--- Formula/pint.rb
+++ Formula/pint.rb
@@ -5,6 +5,15 @@ class Pint < Formula
   sha256 "5c12d379a08de6483e8c7b7503c3b317cc639c8ecbd8015b8d415ec98018328a"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "af1d800ac082a0a40d91da8d515d6b19d07331f431127c48d2d13caf9a57fba8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a1a8af97b11f83ce30c91e9c7f1d9917fc40dfd7cf4d6b666af9ae81ca9fec23"
+    sha256 cellar: :any_skip_relocation, monterey:       "99f5ab5230bcc10ac6f06e46f523be2226bf251bd079e4635453f016c60dd790"
+    sha256 cellar: :any_skip_relocation, big_sur:        "97a686d0d5e992de88ada7a0505b37a0ae561508bcf9563d4d91a28822dc410e"
+    sha256 cellar: :any_skip_relocation, catalina:       "8732d22ec340a5a1ea5cd1cfd9d70bb6f36cd062085ee6078faffdb758013f24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "653bc0517a20763347a6c1b55881551316c13f5b4a18be178bdaf4f7c2e6441f"
+  end
+
   depends_on "go" => :build
 
   def install
