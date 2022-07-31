4756a212c31e92f92f515279cb099769417ac7b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-31 14:04:46 +0000

    aptly: update 1.5.0 bottle.

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -7,13 +7,13 @@ class Aptly < Formula
   head "https://github.com/aptly-dev/aptly.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cfed7144faee9ecf6c6f1d17c62f8f421655d32f19375a76847f54a8c8eadbcb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e87f870317644e20437b88d8c41c2af49a6f8094b5a8a791a22a21d1110aeb1"
-    sha256 cellar: :any_skip_relocation, monterey:       "a22d51e4d224df6aee376957eccc2794c8c958f00c984777e48f28419a6b78bd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9215c97346fa95bfc6766a3c9f1e52d93db83d6a621676abe4f64c7796a894b1"
-    sha256 cellar: :any_skip_relocation, catalina:       "2a9bbbfb27151d55e385a5731bf6a166f31d647238d3aa6c28ee5783874e4c27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16e4a617927df43a88a0eeb394433f1d72776e3e24fa08e126c86050707d7878"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "19c0c8c0b35c1c5faa2a71fc0bd088725f5623f465369dcca5b2cea59322714c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2314abe4aae7ea53660920d311cacccd168045994e1a9eddf12a381b215c1908"
+    sha256 cellar: :any_skip_relocation, monterey:       "86111a102d0782a77bab0d48015bd275f120a36964d86f8f613f1a8f73d94664"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d622cfe1d925f0058f583b8bf48b0bdcee36a441f1bcf145040e5f93879f8765"
+    sha256 cellar: :any_skip_relocation, catalina:       "5d9d495ec8215cfade3e856528dfa233496849517813b19a9ba8d60cb72c4751"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bbff5503f74ef5dcaae33846e285ecf1a23c23de1c858760ae1789ef6fc99524"
   end
 
   depends_on "go" => :build
