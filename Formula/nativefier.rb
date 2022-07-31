8c5793193725f86cb411793471afa1d9946fb586
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-31 11:05:16 +0000

    nativefier: update 49.0.0 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -8,12 +8,12 @@ class Nativefier < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7cc403bcd4677573ba9b5a959c40ad4d0e567e19b0e153c1f3a596efd8de945b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7cc403bcd4677573ba9b5a959c40ad4d0e567e19b0e153c1f3a596efd8de945b"
-    sha256 cellar: :any_skip_relocation, monterey:       "a20d1f57f17c1ff9cb66afd9bf2448d0ebdc3d9826f91a702e9622042289f592"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a20d1f57f17c1ff9cb66afd9bf2448d0ebdc3d9826f91a702e9622042289f592"
-    sha256 cellar: :any_skip_relocation, catalina:       "a20d1f57f17c1ff9cb66afd9bf2448d0ebdc3d9826f91a702e9622042289f592"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7cc403bcd4677573ba9b5a959c40ad4d0e567e19b0e153c1f3a596efd8de945b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bbbb57715d0b467de7e82b27ce18f4dcf28fac940bd9eb0dc5881c99cfb11979"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bbbb57715d0b467de7e82b27ce18f4dcf28fac940bd9eb0dc5881c99cfb11979"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd14c692c55bf2c6b9501b529f1e15a50058f0965a3523fcda33adf46ffa107f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd14c692c55bf2c6b9501b529f1e15a50058f0965a3523fcda33adf46ffa107f"
+    sha256 cellar: :any_skip_relocation, catalina:       "fd14c692c55bf2c6b9501b529f1e15a50058f0965a3523fcda33adf46ffa107f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bbbb57715d0b467de7e82b27ce18f4dcf28fac940bd9eb0dc5881c99cfb11979"
   end
 
   depends_on "node"
