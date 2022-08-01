165d4d3166405cef8abde34fe5fb857e46919452
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-01 15:00:03 +0000

    libgxps: update 0.3.2_2 bottle.

--- Formula/libgxps.rb
+++ Formula/libgxps.rb
@@ -13,13 +13,11 @@ class Libgxps < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "2e78b1e36e2e6095af5cbf6f86e1d544d3225402474164aa1d667fbeb97a6e91"
-    sha256 cellar: :any, arm64_big_sur:  "0243fa2f8e5b1559b417e47e5aea3b6ab8745164f397963c2ac94952c3915324"
-    sha256 cellar: :any, monterey:       "acb2b38e9fa9925dc00c2b8497e9e0c98e38a279d81dd3b2ca6946017bff8367"
-    sha256 cellar: :any, big_sur:        "6ad3b9179f42d68083b2b8bf54fe2d36433ed45d51e8ee13af392638e1b07174"
-    sha256 cellar: :any, catalina:       "52dc9223c583d315cc9b6edd29e696ac4e8dab1fe5d4d452c9ac3f20af185412"
-    sha256 cellar: :any, mojave:         "e113e3685b5f6a000d1e23f2bf67cb78e67c5bba58562156d5a78311ee28c05c"
-    sha256               x86_64_linux:   "5c4da3716214bcc70b6a4cfe2b3dd0c5b624e74a34cc2f72f2e3ac00f7211258"
+    sha256 cellar: :any, arm64_monterey: "ef4bbc6216124d5e51c73cca76d1c4a4bed7747931b7a519e6e829d20cd7021c"
+    sha256 cellar: :any, arm64_big_sur:  "e45ca333cf831e269ac4ed11864da0f09524c87780e9c7f194415505753b6f2a"
+    sha256 cellar: :any, monterey:       "7956a71251563f6589ffd7a061a0e53cdfadcf90893220c94b004231640e3860"
+    sha256 cellar: :any, big_sur:        "19265edef8bb260d7a1adca6f6044f5ba94558b06d54d751c5f1144deb5b820e"
+    sha256 cellar: :any, catalina:       "1b25899eda944421ff3e97ecd872afc97b15f31cd7e651e1eb1ef05959327fa0"
   end
 
   keg_only "it conflicts with `ghostscript`"
