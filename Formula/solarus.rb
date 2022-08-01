7b3daff87cd803104c42d7fbc333d604b2f9a661
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 14:27:50 +0200

    solarus: remove remove gcc dependency on linux

--- Formula/solarus.rb
+++ Formula/solarus.rb
@@ -7,13 +7,13 @@ class Solarus < Formula
   license "GPL-3.0-or-later"
   revision 2
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "2ae97a8f18bee5115f7f801ec8a8b31d6ad6ba98491402084432b954406af8aa"
     sha256 cellar: :any,                 arm64_big_sur:  "f88cd9f0a9883baa813108017034bdb54dd4d5ae8c1c651fd2a0bb23076896d6"
     sha256 cellar: :any,                 monterey:       "979da74628804c8e2517ffd5ee1dd97331d8b88f9148ecb6943db3985a574bfe"
     sha256 cellar: :any,                 big_sur:        "e134a16e60f8b9c9aeedc4824acba770e01c70ca5ddbf623a87b112bd708757e"
     sha256 cellar: :any,                 catalina:       "2ca4689984b74144664fc5e620df94ac3b1bbece2ab8ad0a2641fad3aba07880"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50e9579c71bd23cf4a0eb8f84ffba1be2f101f3278ec1df94fe2150100509909"
   end
 
   depends_on "cmake" => :build
@@ -28,7 +28,6 @@ class Solarus < Formula
   depends_on "sdl2_ttf"
 
   on_linux do
-    depends_on "gcc"
     depends_on "mesa"
     depends_on "openal-soft"
   end
