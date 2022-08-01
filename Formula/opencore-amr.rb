620db25361517e790ee683993df21ca259c98842
mhmdanas <triallax@tutanota.com>
2022-08-01 15:29:07 +0300

    opencore-amr 0.1.6
    Closes #107056.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opencore-amr.rb
+++ Formula/opencore-amr.rb
@@ -1,8 +1,8 @@
 class OpencoreAmr < Formula
   desc "Audio codecs extracted from Android open source project"
   homepage "https://opencore-amr.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/opencore-amr/opencore-amr/opencore-amr-0.1.5.tar.gz"
-  sha256 "2c006cb9d5f651bfb5e60156dbff6af3c9d35c7bbcc9015308c0aff1e14cd341"
+  url "https://downloads.sourceforge.net/project/opencore-amr/opencore-amr/opencore-amr-0.1.6.tar.gz"
+  sha256 "483eb4061088e2b34b358e47540b5d495a96cd468e361050fae615b1809dc4a1"
   license "Apache-2.0"
 
   livecheck do
@@ -26,5 +26,19 @@ class OpencoreAmr < Formula
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <opencore-amrwb/dec_if.h>
+      int main(void) {
+        void *s = D_IF_init();
+        D_IF_exit(s);
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lopencore-amrwb", "-o", "test"
+    system "./test"
   end
 end
