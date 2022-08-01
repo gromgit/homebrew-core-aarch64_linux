e29afc5fae2ce4a1b8c3ec2d62ff5bb305b8f76f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-01 00:17:46 +0000

    swiftgen: update 6.6.1 bottle.

--- Formula/swiftgen.rb
+++ Formula/swiftgen.rb
@@ -7,10 +7,8 @@ class Swiftgen < Formula
   head "https://github.com/SwiftGen/SwiftGen.git", branch: "stable"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf3abab4eac9a3886d79d6eb625b1d010c4490cd04391d02a78d44ea67ffbc5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea599c0ad955644eb3f946360603090f0b05b859c892ccb94d85795eb68612d6"
-    sha256 cellar: :any_skip_relocation, monterey:       "ea47c833c24c992ecba7fff825d94efd15ed4be586c6c39188fdad1c11605f2f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d23540aaccbe68f26fc50260a29904afa1e84f97c678e76d4c842927215e91e9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "661272d66717f8fa88060fd2802b98843c7c27c518a9b57f4abc214d02ba7b86"
+    sha256 cellar: :any_skip_relocation, monterey:       "38c56d7a8b88c07d6761d2fbff99d009c04a8fd580a7eafb650236d38f90004c"
   end
 
   depends_on xcode: ["13.3", :build]
