3e845d1c9de46d41ea7fa0203252f60f3a39edaf
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 14:27:48 +0200

    mpv: remove remove gcc dependency on linux

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -7,13 +7,13 @@ class Mpv < Formula
   revision 1
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 arm64_monterey: "09e223bc45b0968497077eacfe9eeb7e1143ecc782ccd18af454ef215b4c1483"
     sha256 arm64_big_sur:  "47c1c8f8cd49e071be6cda0f729aeaef829ac941e26c87f5ba266d41da423c12"
     sha256 monterey:       "60f51e9c67a707139b2cfa763a961c0778323eae81c1a5aec69c17840ce49e61"
     sha256 big_sur:        "3ee4dfdaea28f1b0c4ebabba8eb677949d1462af4133d4f6b94a60661ab615e7"
     sha256 catalina:       "aca6e4cfc8598dfbd88882622aaeb117f97f1f843c82b87a5308b3fe90740c68"
-    sha256 x86_64_linux:   "f61254f8629ce7d463d36db3bd0c5ad36a52ef3ac5989316756eb3703e86a300"
   end
 
   depends_on "docutils" => :build
