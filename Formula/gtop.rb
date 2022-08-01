109430ccba2f1aaaac9e243d834679e1003a1a59
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 12:42:27 +0200

    gtop: remove remove gcc dependency on linux

--- Formula/gtop.rb
+++ Formula/gtop.rb
@@ -7,13 +7,13 @@ class Gtop < Formula
   sha256 "5bd04175c5d075b58448cf4fff3a2c6a760e28807e73f4a8f1ab0adf14d7c926"
   license "MIT"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f7bfefab47efb569bebb4e3c5ee45159c8f26aaf6fda49b74015bdc665be8dcf"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7bfefab47efb569bebb4e3c5ee45159c8f26aaf6fda49b74015bdc665be8dcf"
     sha256 cellar: :any_skip_relocation, monterey:       "bd9f489110a6841a2306afef5b77e160e34e650a1fc63978698d54a982358a8b"
     sha256 cellar: :any_skip_relocation, big_sur:        "bd9f489110a6841a2306afef5b77e160e34e650a1fc63978698d54a982358a8b"
     sha256 cellar: :any_skip_relocation, catalina:       "bd9f489110a6841a2306afef5b77e160e34e650a1fc63978698d54a982358a8b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b8a49fe170889400a5976142f6131b06f4f3d0f366a9c821beac1997f29be6f7"
   end
 
   depends_on "node"
