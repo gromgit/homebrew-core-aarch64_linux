2b0560da155fd0a765429f74e75011251ec784dc
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 14:27:48 +0200

    mlt: remove remove gcc dependency on linux

--- Formula/mlt.rb
+++ Formula/mlt.rb
@@ -6,13 +6,13 @@ class Mlt < Formula
   license "LGPL-2.1-only"
   head "https://github.com/mltframework/mlt.git", branch: "master"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 arm64_monterey: "dcaf9747d3aad9909d225e3b98864072103d2d2b93391678ef66732ee970e8eb"
     sha256 arm64_big_sur:  "b9b6b9540a4c478a0f64612f430c821c67b2652fa483cce1f22098cb04ac4851"
     sha256 monterey:       "b6e91b91d5640a87dbe02513c88f698ca4dd35e03d3e0ba5abb67a426d224039"
     sha256 big_sur:        "c552e946cad9bb2752889885296d9983f96ab13edd609371ba766577d1e0a1bf"
     sha256 catalina:       "561db9de0a8f913913d8190071dd3f08bfc7ad7b535cd83112c258a911392ca1"
-    sha256 x86_64_linux:   "59f7e0cb66e63fcd8961a22e578622f3086d5af31a6f633e370461d126ad0ba0"
   end
 
   depends_on "cmake" => :build
@@ -30,10 +30,6 @@ class Mlt < Formula
   depends_on "qt@5"
   depends_on "sdl2"
   depends_on "sox"
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5"
 
