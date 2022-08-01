78bfe316bfda2d8d8a0d03753270d5af011cc615
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 14:27:48 +0200

    mgba: remove remove gcc dependency on linux

--- Formula/mgba.rb
+++ Formula/mgba.rb
@@ -12,13 +12,13 @@ class Mgba < Formula
     strategy :github_latest
   end
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any, arm64_monterey: "34235cb6f1aaeca67d11e4b060bfd2d7adf462b61b115beb029fd81ec0adc563"
     sha256 cellar: :any, arm64_big_sur:  "0dc3200fa947b5872c48500c3c7b4841668edfc2143d2977226e850fa53db2dd"
     sha256 cellar: :any, monterey:       "8486c6db482c218845f22d3dfbc5402066d002c6712b1fc0faefa3ce88d186f0"
     sha256 cellar: :any, big_sur:        "575163b96818d53848c6b4a536fbccea185cce14487394974519b1655c3cb03f"
     sha256 cellar: :any, catalina:       "fe54e4803c93036beb054b6e649f722e21f1ae08fa4efb4c2808d9b5b875fd8f"
-    sha256               x86_64_linux:   "2e521f7ea91d0e5b77583d818c0eed31ece44389d4ac3235dd6ed52bfd6141ae"
   end
 
   depends_on "cmake" => :build
@@ -30,10 +30,6 @@ class Mgba < Formula
   depends_on "qt@5"
   depends_on "sdl2"
   depends_on "sqlite" # try to change to uses_from_macos after python is not a dependency
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5" # ffmpeg is compiled with GCC
 
