3829ce6327b3d31a044d0d8c1dfc6692b53fdc39
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 12:42:32 +0200

    netlify-cli: remove remove gcc dependency on linux

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -8,13 +8,13 @@ class NetlifyCli < Formula
   license "MIT"
   head "https://github.com/netlify/cli.git", branch: "main"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3719df5ff383da51eea5a2cf244ac08f596c26b3ec96af9ce5cdbdb5678675d7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3719df5ff383da51eea5a2cf244ac08f596c26b3ec96af9ce5cdbdb5678675d7"
     sha256 cellar: :any_skip_relocation, monterey:       "c7aa8ea285efbda5358d9c1558ffad086a39f88eab831bb5b7e296a676ab58c6"
     sha256 cellar: :any_skip_relocation, big_sur:        "c7aa8ea285efbda5358d9c1558ffad086a39f88eab831bb5b7e296a676ab58c6"
     sha256 cellar: :any_skip_relocation, catalina:       "c7aa8ea285efbda5358d9c1558ffad086a39f88eab831bb5b7e296a676ab58c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d3c8da394d64da8a46ba1ad60f3797924042576a4385f3e3eff5d029bb12c911"
   end
 
   depends_on "node"
