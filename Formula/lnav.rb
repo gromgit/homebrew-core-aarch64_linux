329fe0c4557aeeaf07bd6a95b2248e9c8656f137
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 12:42:30 +0200

    lnav: remove remove gcc dependency on linux

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -10,13 +10,13 @@ class Lnav < Formula
     strategy :github_latest
   end
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "19feba2a42503e1bddbc247e2d6c72e59ccf4cdb25f8ef18fef050e054d4eb89"
     sha256 cellar: :any,                 arm64_big_sur:  "d162151f7043144d24b47c6ae23b61cdbb71959b5c77bba016e0f3d538dc432b"
     sha256 cellar: :any,                 monterey:       "eab9bc062c791cf77864916cc055b281b593d153c5afd165171fb554203ecd67"
     sha256 cellar: :any,                 big_sur:        "57dcbc897e03444f03e78e2fbeaf9c20c65b8b4ba93c759da5118b206007fe04"
     sha256 cellar: :any,                 catalina:       "c137529abc15c63b8e4d865f433712355b5f6fc750ac76c177f805cb7813dd44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd04107f50f8d01f00f476ad87ae8228416f81a3124e0a3d64948d7fd2d695fc"
   end
 
   head do
@@ -31,10 +31,6 @@ class Lnav < Formula
   depends_on "pcre"
   depends_on "readline"
   depends_on "sqlite"
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5"
 
