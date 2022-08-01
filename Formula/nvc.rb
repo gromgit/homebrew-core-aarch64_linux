9555ec38cc73730aa8d7404c8b4b5344aa73695e
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 12:42:33 +0200

    nvc: remove remove gcc dependency on linux

--- Formula/nvc.rb
+++ Formula/nvc.rb
@@ -6,13 +6,13 @@ class Nvc < Formula
   license "GPL-3.0-or-later"
   revision 1
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 arm64_monterey: "a2b12eab1789c86431f2c2243e07f012f8bc23eb8bdc753cc460e2c548f80e63"
     sha256 arm64_big_sur:  "e2af86fea8247b02a6fae2a9dd6a197f180647950bcbda07a0e7a865054873c5"
     sha256 monterey:       "9c4dbeb21c316cef280370437fe31c6a2d8536ae66bd8f6f13de8303adf4f1fc"
     sha256 big_sur:        "41afd05b5d20c00d5f2051858bbaae4c46c200eed14e25a77916b7072341787c"
     sha256 catalina:       "806ec3dff942acf027597a0c712527ed90d04f732cf111be011bad506fbeb16a"
-    sha256 x86_64_linux:   "58200eb93611d5b1ea8efe69f1f7d26e117f8d184c09a39ab9445fef24b17b98"
   end
 
   head do
