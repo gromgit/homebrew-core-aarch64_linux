e238294beb859a7744d7f03396aaee73a2250725
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 14:27:47 +0200

    ffmpegthumbnailer: remove remove gcc dependency on linux

--- Formula/ffmpegthumbnailer.rb
+++ Formula/ffmpegthumbnailer.rb
@@ -7,13 +7,13 @@ class Ffmpegthumbnailer < Formula
   revision 7
   head "https://github.com/dirkvdb/ffmpegthumbnailer.git", branch: "master"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "692c0b3202acf1e7d3bb6e0dc49abfeb9eae91d87f7ace9f52fcce45caf77889"
     sha256 cellar: :any,                 arm64_big_sur:  "044ab71c693e108bcc7734cee4377ee53400e95eedee527618e480d06e0f0caa"
     sha256 cellar: :any,                 monterey:       "c3151551d8b47f7d7314cd08144a22214ef47ca9c079b14dc84a799ce4cd9a12"
     sha256 cellar: :any,                 big_sur:        "a1ea81c204ac623893693f403375053eb8fce33ca9fddd1964630786147cc1e5"
     sha256 cellar: :any,                 catalina:       "0bacb1352eb215908d5217433493d11867ec205f9364dfb33a2b62323f70090a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b35981ab4fc303232a0602adceff292e014f8d6e248d2a60581891436affd1b"
   end
 
   depends_on "cmake" => :build
@@ -21,10 +21,6 @@ class Ffmpegthumbnailer < Formula
   depends_on "ffmpeg@4"
   depends_on "jpeg"
   depends_on "libpng"
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5" # rubberband is built with GCC
 
