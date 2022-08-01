391b368e50df88907d822530d69d81e3e24a4af9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-01 08:44:50 +0000

    ccls: update 0.20220729 bottle.

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -8,11 +8,12 @@ class Ccls < Formula
 
   # Linux bottle removed for GCC 12 migration
   bottle do
-    sha256                               arm64_monterey: "7a700c856f8fddcb51898902e141a7bf2b00cf1d534ad44e368761eaea80c6a9"
-    sha256                               arm64_big_sur:  "f8de1cf356810a3b604fd13f526608cf4ad4ab4c4e70b8474693b580552b0f8f"
-    sha256                               monterey:       "5a6b516516d46abbf4668b5e9eec5e88840c5a2ca73432696d8ab2d9a641a074"
-    sha256                               big_sur:        "b68c5cbc9bdfcb100fd62c6b358a3686735d0e3557ce0a1237499dcb8080bf64"
-    sha256                               catalina:       "831dfbfb728bffc850add851055b9d6eedf5adb74250a08eed8e05997d760695"
+    sha256                               arm64_monterey: "b8c13816cd75809e6ea41113e55f37c547aeba195c648c47af3863921ac85115"
+    sha256                               arm64_big_sur:  "11204e506ee765088067057072547cb7b190fb466ff719d8a1de0fd0b4116a7f"
+    sha256                               monterey:       "a6eecb14e3e58561d8e4d76049624d31df4ea00921ba77116d0dc5e100caf5a9"
+    sha256                               big_sur:        "c48483f72436fd6e4b1a8c3928b436bd48047e396540ce47de06914554b5b064"
+    sha256                               catalina:       "338d1b8848c2a3b380e65551a1a5f38da4c9eb8d8f6968fdef694daf529023e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1da50c69833e7af97b6ebf40b1b6a1d45ba1577b3a80baede95990ed8270488"
   end
 
   depends_on "cmake" => :build
