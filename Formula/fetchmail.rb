847930e5a501c909bbca047fafb096d0981ab79e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-01 10:05:00 +0000

    fetchmail: update 6.4.32 bottle.

--- Formula/fetchmail.rb
+++ Formula/fetchmail.rb
@@ -17,12 +17,12 @@ class Fetchmail < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "95a454ff4c733edcff83be104fd94cef1886ffb4930d4e4a17c8dbd300069a95"
-    sha256 cellar: :any, arm64_big_sur:  "2f789ee52867688896da0998c3cb3bd60a75c99450a8b8dc881e4b1cd7026216"
-    sha256 cellar: :any, monterey:       "a635627e32189a1ba0dc9f2d2b8b7cc251559007d987dc07e6f950274f0612b9"
-    sha256 cellar: :any, big_sur:        "a7a827beadb201e4b0ce83d9336ab5ef178b2e7ec93871ad72308f4ac90d6ce9"
-    sha256 cellar: :any, catalina:       "e4389d3ec951029222b15652f35fc1091a8753607b66f1ad7ec93a050e52dcdf"
-    sha256               x86_64_linux:   "8739bf253f025d01d9218710167e5280185bd201232aeef05077ab80d387481f"
+    sha256 cellar: :any, arm64_monterey: "13e667f7d8bcd32ea5ce2535948ba302bdf2d6d93976d2df7e4af9e4f7d2edd7"
+    sha256 cellar: :any, arm64_big_sur:  "8856e54cb2529de54d6ad058b294d0e49f8933bf5f611b47eb189f5e05b322a9"
+    sha256 cellar: :any, monterey:       "d9a3dc3e5dfefff402eedf98da7a0ac1a20df92c77eb52a85723410371da26c9"
+    sha256 cellar: :any, big_sur:        "4bcbec8680eebf363a22a217d0951c2b0350ce70a64c047eb46da92bb0347a29"
+    sha256 cellar: :any, catalina:       "106edb1d1d6677b7fdc3b3f8d76e84d9eae22dbf56a5b45a7c5331486687087b"
+    sha256               x86_64_linux:   "08e7fe9b3448ecc3259b3cf5ca1a24fd18c4ebd0c39266b99583a1b87d03162f"
   end
 
   depends_on "openssl@3"
