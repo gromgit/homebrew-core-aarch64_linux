437ae73002bac70924aaf7f938b178c8e780466f
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-08-01 14:27:46 +0200

    fceux: remove remove gcc dependency on linux

--- Formula/fceux.rb
+++ Formula/fceux.rb
@@ -8,13 +8,13 @@ class Fceux < Formula
   revision 2
   head "https://github.com/TASEmulators/fceux.git", branch: "master"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "014868c8f0454c13fb6ad090a4335c8348fc630bc95f7382b811498cf8991bac"
     sha256 cellar: :any,                 arm64_big_sur:  "530a4dd105ea8da50b1bcddc1a729b3788af6a817f8864a7af7c9bc3c1376f01"
     sha256 cellar: :any,                 monterey:       "4c5b724f4e3466dd522f875a5419a57353e9c8dd493562dc7ae35d4ff2f976df"
     sha256 cellar: :any,                 big_sur:        "e131aa7191a0a861b2b6c667bd0b42a2bb9e763e96a8e5ef6083d7083ebce88d"
     sha256 cellar: :any,                 catalina:       "7e3b7db25b0357708a66d3c8fa64599cbf0128bc4cb198842958796cce9a4e45"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d417b558825996c6281881206d0493d6ee914f38b51bedfdf7fc9dd3b66436e6"
   end
 
   depends_on "cmake" => :build
@@ -26,7 +26,6 @@ class Fceux < Formula
   depends_on "x264"
 
   on_linux do
-    depends_on "gcc"
     depends_on "mesa-glu"
   end
 
