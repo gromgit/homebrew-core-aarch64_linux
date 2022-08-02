40ff38b25f27346691ec3dc3a4e6f79f60dd4e65
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:16 +0800

    pianod: revert Linux bottle removal

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -10,13 +10,13 @@ class Pianod < Formula
     regex(/href=.*?pianod2[._-]v?(\d+(?:\.\d+)*)\.t/i)
   end
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 arm64_monterey: "e0f97c293764bc1d6ec7e9e9e2d18d554b39ad95071e295592e33e4087376064"
     sha256 arm64_big_sur:  "f2fa087fb17f1e2cfc9aa1fb33a2a2c8438a024e06d510c43f2c15c9bf0cc2b8"
     sha256 monterey:       "b9aa59f530b8a6663a6483ef9e5dc18d28f024aaada85113b5ddcca42481b3d0"
     sha256 big_sur:        "4f100221aa096f62ff4accc0e2797781c4df7c4dbc4496668f6e2615fbc084fa"
     sha256 catalina:       "ca896c858edd71e8ec1545c4f8c5a107a40bfafa23304b521d1a5ee1c5861d01"
+    sha256 x86_64_linux:   "f00f9a7fbdb2e6483a1ffa15b5d6841d312e742b8907b60321ebdb9fce067210"
   end
 
   depends_on "pkg-config" => :build
@@ -33,6 +33,7 @@ class Pianod < Formula
   on_linux do
     # pianod uses avfoundation on macOS, ffmpeg on Linux
     depends_on "ffmpeg@4"
+    depends_on "gcc"
     depends_on "gnutls"
     depends_on "libbsd"
   end
