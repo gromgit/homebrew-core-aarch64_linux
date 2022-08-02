c74bd00828e45076cf587677f79bb44a14f5bda4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:20 +0800

    stockfish: revert Linux bottle removal

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -11,13 +11,17 @@ class Stockfish < Formula
     regex(/^sf[._-]v?(\d+(?:\.\d+)*)$/i)
   end
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "775aa929a7068fe8c55b17658a75a09a8b3191fe95339ec8366bf4b5caf48be5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d584b40e695a1ed4eb19b4b6e1013c580b28e74f3ca2bdb25a15ce1b5473db4"
     sha256 cellar: :any_skip_relocation, monterey:       "6747b74d9cc107ab4d0e7b6f051a9f96a30f5dac53c2fbfbac49c0ba246326e4"
     sha256 cellar: :any_skip_relocation, big_sur:        "5336ae6cdc7d0ad712a527102209156c5a5b12ef8238606798f996b528e41d57"
     sha256 cellar: :any_skip_relocation, catalina:       "fb242d484c460218a677549600fd51a6d82f1cc47751283d2047ae8b5dbbaa4f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87210cffd22b657169dfc10428a1ab92ebfd3029f5c051fdc5006e35cc61b5fb"
+  end
+
+  on_linux do
+    depends_on "gcc" # For C++17
   end
 
   fails_with gcc: "5"
