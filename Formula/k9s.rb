d330c1df78d1857e5231430df75bf7aee5cb963c
Nick Hobart <nick@hobart.io>
2022-08-02 14:25:21 -0700

    k9s 0.26.1
    Closes #107169.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,8 +2,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.26.0",
-      revision: "ee07b98755437c6a85cd82dfdb377d2c30337f65"
+      tag:      "v0.26.1",
+      revision: "e309c28d260b4e9d4b0ea02cfb562126baf9b886"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git", branch: "master"
 
