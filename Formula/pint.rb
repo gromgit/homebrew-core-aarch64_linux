5a056900181d6b7e1142c2785e002f0779f161c3
Porkepix <porkepix@gmail.com>
2022-08-02 10:33:38 +0200

    pint 0.28.6
    update test
    
    Closes #107137.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pint.rb
+++ Formula/pint.rb
@@ -1,8 +1,8 @@
 class Pint < Formula
   desc "Prometheus rule linter/validator"
   homepage "https://cloudflare.github.io/pint/"
-  url "https://github.com/cloudflare/pint/archive/refs/tags/v0.28.3.tar.gz"
-  sha256 "5c12d379a08de6483e8c7b7503c3b317cc639c8ecbd8015b8d415ec98018328a"
+  url "https://github.com/cloudflare/pint/archive/refs/tags/v0.28.6.tar.gz"
+  sha256 "48b14fa9c1a961a7a1c763c97bcc085c6ac312f129a966d4c59c7fa21a6424c6"
   license "Apache-2.0"
 
   bottle do
@@ -23,6 +23,8 @@ class Pint < Formula
       -X main.commit=#{tap.user}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/pint"
+
+    pkgshare.install "docs/examples"
   end
 
   test do
@@ -39,7 +41,12 @@ class Pint < Formula
             summary: High request latency
     EOS
 
-    assert_equal("level=info msg=\"File parsed\" path=#{testpath}/test.yaml rules=1\n",
-                 shell_output("#{bin}/pint -n lint #{testpath}/test.yaml 2>&1"))
+    cp pkgshare/"examples/simple.hcl", testpath/".pint.hcl"
+
+    output = shell_output("#{bin}/pint -n lint #{testpath}/test.yaml 2>&1")
+    assert_match "level=info msg=\"Loading configuration file\" path=.pint.hcl", output
+    assert_match "level=info msg=\"Problems found\" Warning=3", output
+
+    assert_match version.to_s, shell_output("#{bin}/pint version")
   end
 end
