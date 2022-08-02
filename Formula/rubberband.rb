082f26b34df9d06ea7003d2f229b024ec36862fe
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:17 +0800

    rubberband: revert Linux bottle removal

--- Formula/rubberband.rb
+++ Formula/rubberband.rb
@@ -11,13 +11,13 @@ class Rubberband < Formula
     regex(/href=.*?rubberband[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any, arm64_monterey: "5e69d82b8ac49175b0d5f7ce7cd144cf5ad0a1a53256ec3d320a88e049f519d4"
     sha256 cellar: :any, arm64_big_sur:  "fe47590afe376f2f7e1b24a239a5e4947534d8425e97c42a3a34b30d343f3d02"
     sha256 cellar: :any, monterey:       "ff93a71a132ce14e836d9f3d80d430ef41c39470da40d4088d3d8d3e58cf827f"
     sha256 cellar: :any, big_sur:        "b02d05078216b43fe33e2b1354fd8a67fa5c3330f893480ccb3bc2b6bb9880f2"
     sha256 cellar: :any, catalina:       "b73a156422b447a2c5a9426abbc70e58e97b0bb644080849e796d8ddbbbab630"
+    sha256               x86_64_linux:   "2edf30d682c1aeebe61f194a30e8d06203baa8bf88a05a83a5465e4fa30496d3"
   end
 
   depends_on "meson" => :build
@@ -28,6 +28,7 @@ class Rubberband < Formula
 
   on_linux do
     depends_on "fftw"
+    depends_on "gcc"
     depends_on "ladspa-sdk"
     depends_on "vamp-plugin-sdk"
   end
