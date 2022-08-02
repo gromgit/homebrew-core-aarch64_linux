7235356e61272009829806a8dc2795404bb7b628
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-08-02 22:13:03 +0000

    geeqie: update 1.7.3_1 bottle.

--- Formula/geeqie.rb
+++ Formula/geeqie.rb
@@ -12,11 +12,12 @@ class Geeqie < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "3b3cf00e0d5942d061756b850edc8675a7fcbef19bb44db675deab85f2f89b49"
-    sha256 cellar: :any, arm64_big_sur:  "a16904a1405682d3f7b2ce364349a13b849d8484bc64d3ed6e50a5b039c0602c"
-    sha256 cellar: :any, monterey:       "0b11ddf837b2078f6732f993a86fb38bd09b75e1d759e75f4ab6a4a25a261c99"
-    sha256 cellar: :any, big_sur:        "b0a079d4973057f1d6ff163c9bac4313ca8e1ee458aa31a46e76fa93c001e77f"
-    sha256 cellar: :any, catalina:       "64b9a1210db86f04315c80a722feef26fd428b19625e9d9c3da71059b1697912"
+    sha256 cellar: :any,                 arm64_monterey: "3b3cf00e0d5942d061756b850edc8675a7fcbef19bb44db675deab85f2f89b49"
+    sha256 cellar: :any,                 arm64_big_sur:  "a16904a1405682d3f7b2ce364349a13b849d8484bc64d3ed6e50a5b039c0602c"
+    sha256 cellar: :any,                 monterey:       "0b11ddf837b2078f6732f993a86fb38bd09b75e1d759e75f4ab6a4a25a261c99"
+    sha256 cellar: :any,                 big_sur:        "b0a079d4973057f1d6ff163c9bac4313ca8e1ee458aa31a46e76fa93c001e77f"
+    sha256 cellar: :any,                 catalina:       "64b9a1210db86f04315c80a722feef26fd428b19625e9d9c3da71059b1697912"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d4a1a0535b8015c07d3a490e454216480af9d68d67e00e16af779d3aeaa9a57"
   end
 
   depends_on "autoconf" => :build
