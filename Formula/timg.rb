08d394070247d84a140b31315f18f07c3058d342
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:23 +0800

    timg: revert Linux bottle removal

--- Formula/timg.rb
+++ Formula/timg.rb
@@ -6,13 +6,13 @@ class Timg < Formula
   license "GPL-2.0-only"
   head "https://github.com/hzeller/timg.git", branch: "main"
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "de4c65235281a2b9c5d62d28fa39053e21f30c4852ae3f6a0cb419a5ef4231d4"
     sha256 cellar: :any,                 arm64_big_sur:  "da8576af220bdd5052fcd031c0e2b0e1fb206d449c40354f72d10dfb5d9c6d7a"
     sha256 cellar: :any,                 monterey:       "f9f639aac4e106fb0c3ab2f4776acee6d6423f4f497491f334e43547e734ccea"
     sha256 cellar: :any,                 big_sur:        "0efbad0c7a082200d97ec158e1934362d82f5db03aa1130f8ea54088b73c478b"
     sha256 cellar: :any,                 catalina:       "5a2f9661a9e5ad1ada45486c2efb99a16f070f955d373cefdb63a6c9079e8b24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c173cd55ef26536b606b64e5044777732fcf67b625d17f6c430947325e8f2ec1"
   end
 
   depends_on "cmake" => :build
@@ -23,6 +23,10 @@ class Timg < Formula
   depends_on "libpng"
   depends_on "openslide"
   depends_on "webp"
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5" # rubberband is built with GCC
 
