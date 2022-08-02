d01de490de9fab5725b0e4af4841b17af773c154
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-08-02 22:19:37 +0000

    qbs: update 1.23.0 bottle.

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -12,11 +12,12 @@ class Qbs < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "20cf06d480c614636c57f448d675a60082a2661281e6f92df5960fb7320af1b9"
-    sha256 cellar: :any, arm64_big_sur:  "9be8c4f798473371aed50a01c76b347af44087c60475b8d4f0089833b1498243"
-    sha256 cellar: :any, monterey:       "153c91b26950bcedfd25e2540d6921d5ddc49f9411f7e4ee4cfc0bb69b9e359b"
-    sha256 cellar: :any, big_sur:        "3e89be607cbed4696cb85d185a0b52a8616b8adcb716e4ba840e4cd8f4d94790"
-    sha256 cellar: :any, catalina:       "c6791affb7691cbfe035cf59f7c1a496474f31b1554df318d6e111d920d70901"
+    sha256 cellar: :any,                 arm64_monterey: "20cf06d480c614636c57f448d675a60082a2661281e6f92df5960fb7320af1b9"
+    sha256 cellar: :any,                 arm64_big_sur:  "9be8c4f798473371aed50a01c76b347af44087c60475b8d4f0089833b1498243"
+    sha256 cellar: :any,                 monterey:       "153c91b26950bcedfd25e2540d6921d5ddc49f9411f7e4ee4cfc0bb69b9e359b"
+    sha256 cellar: :any,                 big_sur:        "3e89be607cbed4696cb85d185a0b52a8616b8adcb716e4ba840e4cd8f4d94790"
+    sha256 cellar: :any,                 catalina:       "c6791affb7691cbfe035cf59f7c1a496474f31b1554df318d6e111d920d70901"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f7d55bccb3c374b2e9426cf7c38fe0d1c21a5e34a5dbcbd11b14d7a49b4ef90d"
   end
 
   depends_on "cmake" => :build
