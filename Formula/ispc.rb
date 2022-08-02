62b34fa2381fa48e6038d8af4b4f028d754dc02d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:03 +0800

    ispc: revert Linux bottle removal

--- Formula/ispc.rb
+++ Formula/ispc.rb
@@ -6,13 +6,13 @@ class Ispc < Formula
   license "BSD-3-Clause"
   revision 2
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "5a5fc37bbdf6fcbb4fba27258c45375a26eea734edcbecae41715a990b989d99"
     sha256 cellar: :any,                 arm64_big_sur:  "f69fc55e33fd45a8bd12da6c97604f4f72c043e03cbca2f11095d0d46a638c16"
     sha256 cellar: :any,                 monterey:       "17bffc3892dd6cce820a77585f04f6b3edb3394ccd76104b7e0b5243c2e1c40a"
     sha256 cellar: :any,                 big_sur:        "25c32b978928b322c5742da89ce024016daefbba49e4b9fb63d70073756bd914"
     sha256 cellar: :any,                 catalina:       "8fce16395de7f76c88eae707da678b1bf63c39f6768e3404b87749ae081ab99e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97be3aa73e6249c6b65a4352f37a2157fa660a027292ab57c2850432e82a8376"
   end
 
   depends_on "bison" => :build
@@ -20,6 +20,10 @@ class Ispc < Formula
   depends_on "flex" => :build
   depends_on "python@3.10" => :build
   depends_on "llvm" # Must be LLVM 13
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5"
 
