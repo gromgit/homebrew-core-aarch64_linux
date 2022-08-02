4563fe503c21ee4adf59981b99f7dfc22d6d2396
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:46:57 +0800

    ceres-solver: revert Linux bottle removal

--- Formula/ceres-solver.rb
+++ Formula/ceres-solver.rb
@@ -12,13 +12,13 @@ class CeresSolver < Formula
     regex(/href=.*?ceres-solver[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "c727ac20864b6d83a0ca462b57d7aa4f36119eba132565faa9c4a21eec1bf0cb"
     sha256 cellar: :any,                 arm64_big_sur:  "3319708adf812ed908c95dba4984a51f39d49f57d08e71ad2e5b2c124c433348"
     sha256 cellar: :any,                 monterey:       "ab39d83b1ae0299e88f3fc9d5b23f19c98903d6452e21b6d363966ef5f4817d0"
     sha256 cellar: :any,                 big_sur:        "d1918fe281cb00dbb2d773c03252a0d50384688c32cb1ad92510f55c37276dca"
     sha256 cellar: :any,                 catalina:       "d6a945ae9d978b449228a795cfab9ab0061920f6c73f9cb93bb0a72946f4b206"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f095246dbff97818863a99864400c25278c6c23ee5bf42546eca75ae04f7de1"
   end
 
   depends_on "cmake" => [:build, :test]
@@ -29,6 +29,10 @@ class CeresSolver < Formula
   depends_on "openblas"
   depends_on "suite-sparse"
   depends_on "tbb"
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5" # C++17
 
