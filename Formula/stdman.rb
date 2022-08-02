bfffffdcf23e9fe1bd98a6a1a6abd6c84368b1c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-02 05:55:15 +0000

    stdman 2022.07.30
    * stdman 2022.07.30
    * stdman: remove patch
    
    Closes #107119.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stdman.rb
+++ Formula/stdman.rb
@@ -1,8 +1,8 @@
 class Stdman < Formula
   desc "Formatted C++ stdlib man pages from cppreference.com"
   homepage "https://github.com/jeaye/stdman"
-  url "https://github.com/jeaye/stdman/archive/2022.02.01.tar.gz"
-  sha256 "84d36791514f20a814f1530e9f4e6ff67e538e0c9b3ef25db4b007f9861c4890"
+  url "https://github.com/jeaye/stdman/archive/2022.07.30.tar.gz"
+  sha256 "332383e5999e1ac9a6210be8b256608187bb7690a2bff990372e93c2ad4e76ff"
   license "MIT"
   version_scheme 1
   head "https://github.com/jeaye/stdman.git", branch: "master"
@@ -14,14 +14,6 @@ class Stdman < Formula
 
   on_linux do
     depends_on "man-db" => :test
-  end
-
-  # Preserve timestamps when doing `make install`.
-  # This helps ensure uniform bottles across builds.
-  # https://github.com/jeaye/stdman/pull/49
-  patch do
-    url "https://github.com/jeaye/stdman/commit/862e5132c18275661c468083baeab42dc4c335f2.patch?full_index=1"
-    sha256 "8b2c785fc859e31fcb8c58884eff64c371bb9afd001979ad1158517c77ea1041"
   end
 
   def install
