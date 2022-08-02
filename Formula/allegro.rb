0878b0b2f50fcf201e3f9bcaae7737efa6d3ab28
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:46:51 +0800

    allegro: revert Linux bottle removal

--- Formula/allegro.rb
+++ Formula/allegro.rb
@@ -12,13 +12,13 @@ class Allegro < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "d837a297e5b7a4b135831b2549a9388250de130fb86201c9c160c1211f1ff773"
     sha256 cellar: :any,                 arm64_big_sur:  "674a59e675e01fb444a806ec712cef6de9d42be7ff07a35811a80bc4259c3bdc"
     sha256 cellar: :any,                 monterey:       "0f8f973ab20f49f58f84d61aa5238c83c46ea7ced40cc426c55efc5a5dbfc4be"
     sha256 cellar: :any,                 big_sur:        "42a27ed5656e9ddb3a4cc8b84e1684063cdd87229c3979f0efad7a5cf30077a3"
     sha256 cellar: :any,                 catalina:       "8e271ed2ef392df9cf6bf0d491be1ec8a303099ffe6634000407869969c2d85c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0c3f329f2a98021d677a6eaf3812e5d198f27417d52da22df7e1854f6afafa0"
   end
 
   depends_on "cmake" => :build
@@ -33,6 +33,7 @@ class Allegro < Formula
   depends_on "webp"
 
   on_linux do
+    depends_on "gcc"
     depends_on "jpeg-turbo"
     depends_on "libpng"
     depends_on "libx11"
