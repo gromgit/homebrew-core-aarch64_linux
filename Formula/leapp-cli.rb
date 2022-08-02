afbbd41dda7ff7ee58ca9b1ff4424932ccb0bdaf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:05 +0800

    leapp-cli: revert Linux bottle removal

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -7,13 +7,13 @@ class LeappCli < Formula
   sha256 "408d06dfbf5291f6eda330dcfb6ab6c1f5e4f069f46a22aa3f5bab5808b4daea"
   license "MPL-2.0"
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256                               arm64_monterey: "fa76e24aed7da3f084585c75eefd0f8a0bed353af3fe33132f780bdfb11a5690"
     sha256                               arm64_big_sur:  "b88501ffd2707219fb17ad2b2b6a85aadb9704f668449fa90eb6e051803e65fd"
     sha256                               monterey:       "88e4b93484bfc687d1a39dd1df019d9d83147a611f76e1ca1bb17346a45e609d"
     sha256                               big_sur:        "272674d4d72f9cccb475660f4eb86284bc2a8979dfb2ebe22b21406e31ef20a4"
     sha256                               catalina:       "0f5ac650baca5b1719aea5b4b1c45e45866b5870ebae34a39b62edcc5e5dff1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e128d28688834ccea232695b4d748a55c46dc1d0bb4bb66871f09210fce7a79f"
   end
 
   depends_on "pkg-config" => :build
