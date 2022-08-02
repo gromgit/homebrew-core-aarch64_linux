15e06232b6071e4b808ebb8bbecd55e406fec86b
三米前有蕉皮 <root@kali-team.cn>
2022-08-02 08:14:24 +0000

    observerward 2022.8.2
    Closes #107132.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/observerward.rb
+++ Formula/observerward.rb
@@ -1,8 +1,8 @@
 class Observerward < Formula
   desc "Cross platform community web fingerprint identification tool"
   homepage "https://0x727.github.io/ObserverWard/"
-  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.7.27.tar.gz"
-  sha256 "cf2d4cb334611520eccb24578eafa679fb5a9a11e53c04620c833c774110c769"
+  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.8.2.tar.gz"
+  sha256 "2d1772ec6d078a2de3668fda64ab882f52c5e4d67f690fbe38b1e349346ffbd9"
   license "MIT"
 
   bottle do
