8df465be960303e54dda37e1fee3d735a8d9769d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 20:47:04 +0800

    kakoune: revert Linux bottle removal

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -11,13 +11,13 @@ class Kakoune < Formula
     strategy :github_latest
   end
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "04ba0d6fed5371d8f5012cb0731be90a011cf05dabcffaa51b467117dcf62275"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "edc941221942a73d79767e254798319842b1849988706e0c41deaaa65c0f3407"
     sha256 cellar: :any_skip_relocation, monterey:       "6361db56b495a4be2855981c95bcfa0ed81ec0e63aace99b6fa06e9340d4ca28"
     sha256 cellar: :any_skip_relocation, big_sur:        "95949d504b4b3d6c87bd4e3cf0e391b3a346ee0166187cf8b43945dd3e6cb826"
     sha256 cellar: :any_skip_relocation, catalina:       "0bf1d69af4e88dd3e7c441c77bbc9ea5f211205883f9a02914b4590035f616e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2fa06946316f28a0375d5d883b45c899775f9aacff98e13bc8bca7d4721d336e"
   end
 
   depends_on macos: :high_sierra # needs C++17
@@ -29,6 +29,7 @@ class Kakoune < Formula
     depends_on "binutils" => :build
     depends_on "linux-headers@4.4" => :build
     depends_on "pkg-config" => :build
+    depends_on "gcc"
   end
 
   fails_with gcc: "5"
