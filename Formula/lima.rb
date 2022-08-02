9d4968039b3c0f122add8f15c43a9f03111e4ab6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-02 01:16:48 +0000

    lima: update 0.11.3 bottle.

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -7,12 +7,11 @@ class Lima < Formula
   head "https://github.com/lima-vm/lima.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ad7dc8b57742eba8a88fa12162d5686ce0575114fbb2183c9d874019123dde00"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "88cf71134e0223169252b836df74962a34103e63559791bb231fb2255c0644cf"
-    sha256 cellar: :any_skip_relocation, monterey:       "241003ff895f2ffa25320dc6a370d590edb5cf4cef3e2698b416179c87dc0553"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5cf1a86b4fe1b5d27b6368f00e8126775d072e8ca6f8e1f084989a15a5bf6b53"
-    sha256 cellar: :any_skip_relocation, catalina:       "449fa6d775d4c32b339416d12d99d49e94e271a4c265cfb76adfca170d6ff2b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9da2792ebe616f34eff15cf6569af469c40ec73be421e8eb90cacf109d24ab3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f1d94b3fab9e20cbb6cc3be2f9b8681aa9af432515e47cd6e28d4fc652670d04"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "304d4c1434678c4fb958b22bb04086e25018d7a9e9866501fff223b95de199fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "2ed0d377f31ed7e1448098dcaf07b33303c2afc9615a140261efed037bde773f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ddf61fd17410b83e9b6540c61d601f71660f8360dc0b1555b47edb982b177dab"
+    sha256 cellar: :any_skip_relocation, catalina:       "827fb3183a3407b53a500a5b5904755c16b2bac7fddcb93e73493523a202b0e7"
   end
 
   depends_on "go" => :build
