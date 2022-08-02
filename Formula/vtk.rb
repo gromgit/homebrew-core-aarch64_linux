e000d6e2f669baef95bd9fef4d9e13a2a08db8b7
Michael Cho <cho-m@tuta.io>
2022-08-01 22:49:01 -0700

    vtk: migrate to `jpeg-turbo` and `python@3.10`

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -1,11 +1,22 @@
 class Vtk < Formula
   desc "Toolkit for 3D computer graphics, image processing, and visualization"
   homepage "https://www.vtk.org/"
-  url "https://www.vtk.org/files/release/9.1/VTK-9.1.0.tar.gz"
-  sha256 "8fed42f4f8f1eb8083107b68eaa9ad71da07110161a3116ad807f43e5ca5ce96"
   license "BSD-3-Clause"
-  revision 5
-  head "https://github.com/Kitware/VTK.git", branch: "master"
+  revision 6
+  head "https://gitlab.kitware.com/vtk/vtk.git", branch: "master"
+
+  stable do
+    url "https://www.vtk.org/files/release/9.1/VTK-9.1.0.tar.gz"
+    sha256 "8fed42f4f8f1eb8083107b68eaa9ad71da07110161a3116ad807f43e5ca5ce96"
+
+    # Fix vtkpython support for Python 3.10. Remove in the next release.
+    # First patch backports part of older commit so we can directly patch in upstream commit.
+    patch :DATA
+    patch do
+      url "https://gitlab.kitware.com/vtk/vtk/-/commit/3eea0e12acfb608a76d6ae36fb36566a4a6b0e9b.diff"
+      sha256 "1c1c4622a58f8c852d196759c8d9036e4d513a5ebe16fe0bfa14583832886572"
+    end
+  end
 
   bottle do
     sha256                               arm64_monterey: "0eba3f3335584a0074ff5b706d433d64441651f8d8052a9c6845b987dcdf8ba3"
@@ -24,7 +35,7 @@ class Vtk < Formula
   depends_on "gl2ps"
   depends_on "glew"
   depends_on "hdf5"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "jsoncpp"
   depends_on "libogg"
   depends_on "libpng"
@@ -33,7 +44,7 @@ class Vtk < Formula
   depends_on "netcdf"
   depends_on "pugixml"
   depends_on "pyqt@5"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "qt@5"
   depends_on "sqlite"
   depends_on "theora"
@@ -70,7 +81,6 @@ class Vtk < Formula
 
     args = %W[
       -DBUILD_SHARED_LIBS:BOOL=ON
-      -DBUILD_TESTING:BOOL=OFF
       -DCMAKE_INSTALL_NAME_DIR:STRING=#{opt_lib}
       -DCMAKE_INSTALL_RPATH:STRING=#{rpath}
       -DCMAKE_DISABLE_FIND_PACKAGE_ICU:BOOL=ON
@@ -100,7 +110,7 @@ class Vtk < Formula
       -DVTK_MODULE_USE_EXTERNAL_VTK_tiff:BOOL=ON
       -DVTK_MODULE_USE_EXTERNAL_VTK_utf8:BOOL=ON
       -DVTK_MODULE_USE_EXTERNAL_VTK_zlib:BOOL=ON
-      -DPython3_EXECUTABLE:FILEPATH=#{Formula["python@3.9"].opt_bin}/python3
+      -DPython3_EXECUTABLE:FILEPATH=#{which("python3.10")}
       -DVTK_GROUP_ENABLE_Qt:STRING=YES
       -DVTK_QT_VERSION:STRING=5
     ]
@@ -142,7 +152,7 @@ class Vtk < Formula
       }
     EOS
 
-    system "cmake", "-DCMAKE_BUILD_TYPE=Debug", "-DCMAKE_VERBOSE_MAKEFILE=ON", "-DVTK_DIR=#{vtk_dir}", "."
+    system "cmake", ".", "-DCMAKE_BUILD_TYPE=Debug", "-DCMAKE_VERBOSE_MAKEFILE=ON", "-DVTK_DIR=#{vtk_dir}"
     system "make"
     system "./Distance2BetweenPoints"
 
@@ -156,3 +166,18 @@ class Vtk < Formula
     system bin/"vtkpython", "Distance2BetweenPoints.py"
   end
 end
+
+__END__
+diff --git a/Documentation/release/dev/python-3.10-wheels.md b/Documentation/release/dev/python-3.10-wheels.md
+new file mode 100644
+index 0000000000000000000000000000000000000000..f4e81411c73f30724ad420ccb7f3c6c07a6f8e3d
+--- /dev/null
++++ b/Documentation/release/dev/python-3.10-wheels.md
+@@ -0,0 +1,7 @@
++## Python 3.10 wheels
++
++VTK now generates Python 3.10 wheels. Note that `vtkpython` and other tools
++using `vtkPythonInterpreter` still do not support the new initialization
++behaviors introduced in Python 3.10. See [this issue][vtk-python-3.10-support].
++
++[vtk-python-3.10.support]: https://gitlab.kitware.com/vtk/vtk/-/issues/18317
