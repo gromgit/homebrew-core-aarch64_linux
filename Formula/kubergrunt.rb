78f9cb96d5b97d303eb1962c60bea928be93a19c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-02 08:05:42 +0000

    kubergrunt: update 0.9.2 bottle.

--- Formula/kubergrunt.rb
+++ Formula/kubergrunt.rb
@@ -6,12 +6,12 @@ class Kubergrunt < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b28253f592a18170eac8bc754c4a41f2cf76481063d760156afa7e81112dea2d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b40b0c7931cbe6e14f64fa1674ed4d3029b9a6facb05e4fa6433b43dbf8404e7"
-    sha256 cellar: :any_skip_relocation, monterey:       "680210953d896ebd68457f2078b58500ea5eb92f95f2f8fb0820573c4b64f399"
-    sha256 cellar: :any_skip_relocation, big_sur:        "401e869549b868b8681f80c5c80fa0a35574e2e81d38eb7ec70c1b35480bacab"
-    sha256 cellar: :any_skip_relocation, catalina:       "c303f8d436e03fb99339642fac47c2d8d5bfa6eb9db61486859383b50a8e9f90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1bc886ee8abb2f8e2a104e43874c4bd281f50cff4c0ef2e573ff129534d9f171"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "68effe374818b5c91daf501d1c32fd985d2fdec644e7d8bb9a3d1fb9c0144f86"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7810095334d4947510d6df76df7d3c9410c78c5d66b218afe38b7c153f244abe"
+    sha256 cellar: :any_skip_relocation, monterey:       "adb10fb31cbb38e99a9808f94a82c4cd73e115dd9f5eb0ca72f9d077ef6dbf72"
+    sha256 cellar: :any_skip_relocation, big_sur:        "03c1017e5d3e1490e40a140167b5eed305728179d16fc10ae0bd69e09d5e1959"
+    sha256 cellar: :any_skip_relocation, catalina:       "d4f173ddb23848a1206eec66de95471177c7b4212780611c14d46d43ca8d6a86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "782d0bfff784c40ec147bad45fd5bc4c912509d87ef9e6a9543353274e5dd85c"
   end
 
   # Bump to 1.18 on the next release, if possible.
