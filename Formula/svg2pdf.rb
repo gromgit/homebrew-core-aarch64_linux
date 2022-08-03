f613fe3990e9f25d733aa0a749f65469096ae4bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-03 07:04:52 +0000

    svg2pdf: update 0.1.3_2 bottle.

--- Formula/svg2pdf.rb
+++ Formula/svg2pdf.rb
@@ -12,16 +12,12 @@ class Svg2pdf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b83f5a88a5b1e88ffcaf5c81186fbe2162e27d7b4fd93c60acd54c1d7897fb2d"
-    sha256 cellar: :any,                 arm64_big_sur:  "dd41fd5b107ce28bbb2dae882cb0c86f7ba7a57005f0e826d492ffa68a760978"
-    sha256 cellar: :any,                 monterey:       "9188bb1576693c48b47a326593d0a0ee1f14c1584ccc4c060c6ff042ef2fd191"
-    sha256 cellar: :any,                 big_sur:        "358f4578b7a5fb09569411c4ac192314e2fa082aeb14cba604f2275ed888b72a"
-    sha256 cellar: :any,                 catalina:       "7dff42459bf1ab33b0938f062d42c1857cb8274d1935f356b4f7dec76aac865c"
-    sha256 cellar: :any,                 mojave:         "ba3e83fc0bf7a58166f2c4449b0f0d4590b5902ac2072ece48b9ca5eed13429a"
-    sha256 cellar: :any,                 high_sierra:    "7a1c4ac8748a9c9013d6d6e50bd04b024e092dd718c878a0b7bcde3d9ca51a97"
-    sha256 cellar: :any,                 sierra:         "bba8555de1a81fb92de544d77dc62fbe03e005b1b371d16127472890b7697503"
-    sha256 cellar: :any,                 el_capitan:     "28e18b196650002c5c40c8cd6e38ecf26d16a5525f7d9ff9e2e3fe6dbfb9e17a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b96ed00f483970629081f0d297f7884327f10f58cde0fbac5d7f871ac06f95b"
+    sha256 cellar: :any,                 arm64_monterey: "059061cd7c6f0466c2ae93003220d0a4559659393d8c4d519511a08410dc9a09"
+    sha256 cellar: :any,                 arm64_big_sur:  "74f2c15d9de7f737aedc70ff715b238fb3482ca67483e29547a0d608c2f78db6"
+    sha256 cellar: :any,                 monterey:       "944236f1828f69922b87cd63b55f5cff0e20f3a565ceb977aaf0bad2f72374e6"
+    sha256 cellar: :any,                 big_sur:        "b1275e6db5e5512c89394381f1e3e6649225e656df36412dc87e28cd3bd9130f"
+    sha256 cellar: :any,                 catalina:       "806321a4363be84920038c530898354f98dd8e852ec14e963b7959b2a1ff28f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "da8f29a9fa362e43d754a326cf274898b7ee24dda8591b4169cfb8e31b6a51a4"
   end
 
   depends_on "pkg-config" => :build
