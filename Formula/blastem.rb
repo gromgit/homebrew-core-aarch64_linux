752985fde0f7e4b9b169af88b068b89f7df7862d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-03 06:05:26 +0000

    blastem: update 0.6.2_2 bottle.

--- Formula/blastem.rb
+++ Formula/blastem.rb
@@ -13,12 +13,10 @@ class Blastem < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "6f3f83fd9bc9b5a259eb21ea43bdc37e4d4a8665c809b8f34d456f681d3c1d17"
-    sha256 cellar: :any,                 big_sur:      "003bbd7d1f5f9d81fb471d1fff692951c9400a8bf2f1511f0d83c9bea9cb8e63"
-    sha256 cellar: :any,                 catalina:     "7b9652bffa8c28d6f23e1ad88534b5f2bbd49a916566650c3090366a556f11b2"
-    sha256 cellar: :any,                 mojave:       "9972096dbef1b35d3d98894c77575a4fce7c674660498e0877b95fe22383f1eb"
-    sha256 cellar: :any,                 high_sierra:  "74e39ac321fe48f06927b3ac455a382f14342c007b06b083860175edca1e0062"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e55cd02a092ef31e3d13db74b551bdab7909fc5bdec0953a608407542976bf92"
+    sha256 cellar: :any,                 monterey:     "a05456feadfedff7fc89b7e018b801121e92d27cf280dce0bb5edcbb18fcb488"
+    sha256 cellar: :any,                 big_sur:      "5a1d5caf1b0fc3f0f7887432c80c6cb49f1b3dc336a9873558d5f6b7c94ae099"
+    sha256 cellar: :any,                 catalina:     "684ac27d7251db585b84112c9721fbafab44e24bb4dbee60a161f1b772ccb82d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "34eecdcd05c078faa3ca11075058a9fc3c5f4c80f69de0bdc927779fe623a9c9"
   end
 
   depends_on "imagemagick" => :build
