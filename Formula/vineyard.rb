e4febe8ff53c0513b975d8d6bf2d8acb07629243
Tao He <sighingnow@gmail.com>
2022-08-03 09:00:20 +0800

    vineyard 0.7.2 (new formula)
    * vineyard 0.7.2 (new formula)
      We need to add this to `versioned_dependencies_conflicts_allowlist`
      since we have multiple versions of Python in the dependency tree.
    * Add a comments of drop libclang requirements after LLVM 15 merged.
    * Update Formula/vineyard.rb
    * Use spawn to start the vineyardd process.
    
    Closes #104618.
    
    Signed-off-by: Tao He <sighingnow@gmail.com>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/vineyard.rb
@@ -0,0 +1,107 @@
+class Vineyard < Formula
+  include Language::Python::Virtualenv
+
+  desc "In-memory immutable data manager. (Project under CNCF)"
+  homepage "https://v6d.io"
+  url "https://github.com/v6d-io/v6d/releases/download/v0.7.2/v6d-0.7.2.tar.gz"
+  sha256 "d0cbb4bf894c8b089fc4c612dcf198656e57cd4612918c38efb3112bf7581a06"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "apache-arrow"
+  depends_on "boost"
+  depends_on "etcd"
+  depends_on "etcd-cpp-apiv3"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "libgrape-lite"
+  depends_on "llvm"
+  depends_on "nlohmann-json"
+  depends_on "open-mpi"
+  depends_on "openssl@1.1"
+  depends_on "tbb"
+
+  fails_with gcc: "5"
+
+  resource "libclang" do
+    url "https://files.pythonhosted.org/packages/ea/ec/94fefe778caa8f2ecf9bb996917535a49b36580846af908b2f38fe6396c9/libclang-14.0.1.tar.gz"
+    sha256 "332e539201b46cd4676bee992bbf4b3e50450fc17f71ff33d4afc9da09cf46cb"
+  end
+
+  def install
+    # We install the libclang from pypi for build as the `clang` package
+    # bundled in LLVM cannot find libclang.dylib easily.
+    #
+    # The requirement for libclang python package will be removed once
+    # LLVM been upgrade to 15, see also: Homebrew/homebrew-core#106925
+    venv = virtualenv_create(buildpath/"venv", "python3")
+    venv.pip_install resource("libclang")
+
+    # make libclang.so/libclang.dll findable by clang.cindex.Index.create()
+    ENV["LIBCLANG_LIBRARY_PATH"] = Formula["llvm"].opt_lib
+
+    # link against system libc++ instead of llvm provided libc++
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DCMAKE_CXX_STANDARD=14",
+                    "-DCMAKE_CXX_STANDARD_REQUIRED=TRUE",
+                    "-DPYTHON_EXECUTABLE=#{buildpath}/venv/bin/python",
+                    "-DUSE_EXTERNAL_ETCD_LIBS=ON",
+                    "-DUSE_EXTERNAL_TBB_LIBS=ON",
+                    "-DUSE_EXTERNAL_NLOHMANN_JSON_LIBS=ON",
+                    "-DBUILD_VINEYARD_TESTS=OFF",
+                    "-DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <iostream>
+      #include <memory>
+
+      #include <vineyard/client/client.h>
+
+      int main(int argc, char **argv) {
+        vineyard::Client client;
+        VINEYARD_CHECK_OK(client.Connect(argv[1]));
+
+        std::shared_ptr<vineyard::InstanceStatus> status;
+        VINEYARD_CHECK_OK(client.InstanceStatus(status));
+        std::cout << "vineyard instance is: " << status->instance_id << std::endl;
+
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "test.cc", "-std=c++17",
+                    "-I#{Formula["apache-arrow"].include}",
+                    "-I#{Formula["boost"].include}",
+                    "-I#{include}",
+                    "-I#{include}/vineyard",
+                    "-I#{include}/vineyard/contrib",
+                    "-L#{Formula["apache-arrow"].lib}",
+                    "-L#{Formula["boost"].lib}",
+                    "-L#{lib}",
+                    "-larrow",
+                    "-lboost_thread-mt",
+                    "-lboost_system-mt",
+                    "-lvineyard_client",
+                    "-o", "test_vineyard_client"
+
+    # prepare vineyardd
+    vineyardd_pid = spawn bin/"vineyardd", "--norpc",
+                                           "--meta=local",
+                                           "--socket=#{testpath}/vineyard.sock"
+
+    # sleep to let vineyardd get its wits about it
+    sleep 10
+
+    assert_equal("vineyard instance is: 0\n", shell_output("./test_vineyard_client #{testpath}/vineyard.sock"))
+  ensure
+    # clean up the vineyardd process before we leave
+    Process.kill("HUP", vineyardd_pid)
+  end
+end
