be2078145ef88730e37f6e74d914bb6b653a36d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-03 10:05:51 +0000

    kustomize: update 4.5.7 bottle.

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -13,12 +13,12 @@ class Kustomize < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0ea453c9787b6f397ff01d089fbd2cccfac4c40d43bf14fe11ca0f1ba8336303"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6bfab4a38897992fb9d93d11a0c25419fa5db5192210450e0acbd89bd58882ac"
-    sha256 cellar: :any_skip_relocation, monterey:       "fdcbf55e2f4a3ce77a4fa3a9867097bf0617d4b322ae34ac01ba40ca250aec35"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a22d9fbd4cc8f1ea41f7374b20c6962ed1514efb97e421c996772d9b3933cdab"
-    sha256 cellar: :any_skip_relocation, catalina:       "a15d4f2f75819f6a7efc1e2d81c02c3aa121393ae0026895c63bb19979a1656f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50c68b811ad54c09b9ab70935a69059ec333e4afd53b1d240faf6537c8acfb04"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "001f113a50c425d7c0cdbd42461be315b9696dbc89f4d775dce572a87d6cb4ea"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b573afb3aff29de518a7b60d13dceedf956521a233ee82722cdda4b7fa47e389"
+    sha256 cellar: :any_skip_relocation, monterey:       "7878dd7bad7bde751736bac6de721f0e20ba3b75bea81c017105218cf96df369"
+    sha256 cellar: :any_skip_relocation, big_sur:        "628577f3b6148a07e7f96b33f2e69d1f778170ed0814a6592b4d32da3ed61266"
+    sha256 cellar: :any_skip_relocation, catalina:       "e471ba925c2bb5544a31e1af7eb15cec88b94fc578c902585bf492f240f7bff1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "18cab0642b06d0f60ed61b4fbca2264e475c1564eeff9f83249cc24ffa1c48ba"
   end
 
   depends_on "go" => :build
