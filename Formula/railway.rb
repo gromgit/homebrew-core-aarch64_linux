5f8ac19fc1fc156599f9e3dce15d133d5c42fbda
Michael Cho <cho-m@tuta.io>
2022-08-03 16:35:13 -0700

    railway 2.0.5
    Closes #107261.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/railway.rb
+++ Formula/railway.rb
@@ -1,11 +1,10 @@
-require "language/node"
-
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app/"
-  url "https://registry.npmjs.org/@railway/cli/-/cli-1.8.4.tgz"
-  sha256 "b057db0b76df651e982f79928d9f1f8dd37c6fce91995e41c56bf8ea6559b11c"
+  url "https://github.com/railwayapp/cli/archive/refs/tags/v2.0.5.tar.gz"
+  sha256 "76700c2b8327f1228a7b1cfa5238c1739de029bd20caf3b10c264e3e00239387"
   license "MIT"
+  head "https://github.com/railwayapp/cli.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1a4fa8e05f74aa696ee22ef23635a7830851faa96ac939faff170a5fe933cafe"
@@ -16,11 +15,12 @@ class Railway < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f82ce3fe592056a356c7ce02806fd4ed8666b4ea78fa876079bf492ab4e1bf4"
   end
 
-  depends_on "node"
+  depends_on "go" => :build
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = "-s -w -X github.com/railwayapp/cli/constants.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags)
 
     # Install shell completions
     output = Utils.safe_popen_read(bin/"railway", "completion", "bash")
