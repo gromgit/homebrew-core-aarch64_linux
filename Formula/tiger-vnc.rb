f86e929bb39515f780180b32ba9e420fb716ca38
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-08-02 21:52:10 -0700

    tiger-vnc: fix dependency order

--- Formula/tiger-vnc.rb
+++ Formula/tiger-vnc.rb
@@ -22,7 +22,6 @@ class TigerVnc < Formula
   depends_on "pixman"
 
   on_linux do
-    depends_on "linux-pam"
     depends_on "libx11"
     depends_on "libxcursor"
     depends_on "libxdamage"
@@ -34,6 +33,7 @@ class TigerVnc < Formula
     depends_on "libxrandr"
     depends_on "libxrender"
     depends_on "libxtst"
+    depends_on "linux-pam"
   end
 
   def install
