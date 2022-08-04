16d02d3f04317625a655e894f73b5604b1b9fcd2
Yurii Kolesnykov <root@yurikoles.com>
2022-08-04 19:23:46 +0300

    cmake-docs 3.24.0
    Closes #107314.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cmake-docs.rb
+++ Formula/cmake-docs.rb
@@ -1,10 +1,10 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.23.3/cmake-3.23.3.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.23.3.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.3.tar.gz"
-  sha256 "06fefaf0ad94989724b56f733093c2623f6f84356e5beb955957f9ce3ee28809"
+  url "https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.24.0.tar.gz"
+  sha256 "c2b61f7cdecb1576cad25f918a8f42b8685d88a832fd4b62b9e0fa32e915a658"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
