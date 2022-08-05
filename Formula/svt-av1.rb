6b4aa7ba36034c1b37011b9b0e20657af620b46a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-05 21:26:04 +0000

    svt-av1: add 1.1.0 bottle.

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -6,6 +6,15 @@ class SvtAv1 < Formula
   license "BSD-3-Clause"
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "93ece0ab58a536c5af08a714aadd6c9f71c3ec0b0bbd01a109aef101f8ff4b5b"
+    sha256 cellar: :any,                 arm64_big_sur:  "39de4d54b32a234127bb47f23cd6d44240d979a6a7c88d842f576c57eab87721"
+    sha256 cellar: :any,                 monterey:       "e6e41ce5ea74abf0274becb65aee5ec47b3544885687b04974f22e52f44f5b94"
+    sha256 cellar: :any,                 big_sur:        "69ca769124878485be07a54ffe6f4d77f0afd78278223154061442c58ee9680b"
+    sha256 cellar: :any,                 catalina:       "4606c4e84ce463713a0545faf2007bea7d91d6e56879207eb75d74f3b5fd409c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c330226af6cfb7cdc90736434d07630dddb9d006f8f37bdb01827be3525123b4"
+  end
+
   depends_on "cmake" => :build
   depends_on "make" => :build
   depends_on "yasm" => :build
