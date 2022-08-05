8ef167583686018d4b3e2aa00278a4f062b33a2f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-05 14:40:21 +0000

    qrupdate: update 1.1.2_15 bottle.

--- Formula/qrupdate.rb
+++ Formula/qrupdate.rb
@@ -11,13 +11,12 @@ class Qrupdate < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ce911ff244c5287b103c70b3720ac1ebce6f3215e6d8f12f503ce0db2b71f93c"
-    sha256                               arm64_big_sur:  "37f1e9c921973d004aa17330658a7e3b8eab48f8b0c3795aaf75acc36b0adad8"
-    sha256 cellar: :any,                 monterey:       "7a0890711406e18b5cd4851abb3c83011a98766e93933c934fefade2beabb0fd"
-    sha256 cellar: :any,                 big_sur:        "bf048deb2737ada46b63c53b36bfb39cc1ba536d810ef6daad38e21a949777f9"
-    sha256 cellar: :any,                 catalina:       "ac9f87f4e27825031f0d15e097a6e1fd644fa0e6eac9e0f9e605c35c9e7c3ab6"
-    sha256 cellar: :any,                 mojave:         "02ae54ea1999c2df3d37ed9f07af0f2a038e35d528432143951bb9a2062af619"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f4e8ae6842fe139dfb889c7748d54660d9d30cb6b7c3314ab123e4270e34b82"
+    sha256 cellar: :any,                 arm64_monterey: "c8c154ece840edeeb4b4d0fe76383b60177ada3a525d96a124dc3fac80d9ae34"
+    sha256 cellar: :any,                 arm64_big_sur:  "349ff7a34aacf021f8df3d129fa7e8897bd1b87a41d7df6b270fa98d73039ab4"
+    sha256 cellar: :any,                 monterey:       "20e6d9ac347bc1903177aa5273e25ac2fd1f6dd56211e4170d46741ebdbd0b4d"
+    sha256 cellar: :any,                 big_sur:        "12240dfe307818f58b11e495c81258979570e55640173382d3be77e39ee8dd0a"
+    sha256 cellar: :any,                 catalina:       "b5a26e72c3d49e5b8b70432c11c93ffb392325c4a8a16cce2497b203bd23559d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf21846969fac2323ea2d8762b04c7f090bb0d9092c98636a853c50aaab49230"
   end
 
   depends_on "gcc" # for gfortran
