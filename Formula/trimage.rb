518cad357d4b25ae62007a4f5eb3d84765ac8a79
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-05 07:04:40 +0000

    trimage: update 1.0.6_3 bottle.

--- Formula/trimage.rb
+++ Formula/trimage.rb
@@ -8,13 +8,7 @@ class Trimage < Formula
   head "https://github.com/Kilian/Trimage.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "06945f0b2785db7539c7c40b1ed21f88d263906b565a20282c87830d35ab56d4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a6dca1721ee66b96bcc5f010fb96657024b17059eefc82db0e61c6e2ff12a21"
-    sha256 cellar: :any_skip_relocation, monterey:       "341ee3867c7448c29cc5e33ba137e7b0b14eb131bbb85fe7bfc1ac0532415a58"
-    sha256 cellar: :any_skip_relocation, big_sur:        "eb3768183ebb466b03e1134b498a0331e43f9aa19ca2f5fb3550f14bf28c1998"
-    sha256 cellar: :any_skip_relocation, catalina:       "2174157bed654961ae8f5b1b60653c40a6a336aa4f26292ebe842d02652c62c5"
-    sha256 cellar: :any_skip_relocation, mojave:         "638e2b76d476287fad690b99c20759798e1eab19bf3beadfad35c8f177bd59c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "822f4cf83f95f708e32d3fe8befe189d4d621b87a70dcd285cca243b8ee879d0"
+    sha256 cellar: :any_skip_relocation, all: "32065c70413c2a9855f948cf34ab031c5705172ef9ee5d5dd7a70098bff99abb"
   end
 
   depends_on "advancecomp"
