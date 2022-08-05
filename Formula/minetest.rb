c73a506d4e299421fece3701eb638279e33ec8bd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-05 05:43:57 +0000

    minetest 5.6.0
    * minetest 5.6.0
    * minetest: update resources
    * minetest: depend on xinput on linux
    * minetest: switch to luajit
    * minetest: fix audit
    
    Closes #107360.
    
    Co-authored-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minetest.rb
+++ Formula/minetest.rb
@@ -2,36 +2,19 @@ class Minetest < Formula
   desc "Free, open source voxel game engine and game"
   homepage "https://www.minetest.net/"
   license "LGPL-2.1-or-later"
-  revision 2
 
   stable do
-    url "https://github.com/minetest/minetest/archive/5.5.1.tar.gz"
-    sha256 "dc0ae5188ef351db85c38b27f38f8549b133ed82aa46daea6deee148aa3454f4"
-
-    # This patch fixes https://github.com/minetest/minetest/issues/12172
-    # It has been merged upstream, and so should not be necessary for the next
-    # minetest release (5.5.2)
-    patch do
-      url "https://github.com/minetest/minetest/commit/951604e29ff9d4b796003264574e06031c014a3f.patch?full_index=1"
-      sha256 "46ca51997cadba9ec714de1988097ae37dec013def1cc4ae560d8de6b0f1d0bc"
-    end
+    url "https://github.com/minetest/minetest/archive/5.6.0.tar.gz"
+    sha256 "3fdbc0c8d9f6a18c12954ba0caedb548a22f367520f59d079804a21de0347a91"
 
     resource "irrlichtmt" do
-      url "https://github.com/minetest/irrlicht/archive/1.9.0mt4.tar.gz"
-      sha256 "a0e2e5239ebca804adf54400ccaacaf228ec09223cfb2e1daddc9bf2694176e6"
-
-      # This patch fixes https://github.com/minetest/minetest/issues/11541
-      # It has been merged upstream, and so should not be necessary for the
-      # next irrlicht release (1.9.0mt7)
-      patch do
-        url "https://github.com/minetest/irrlicht/commit/392df9bae3de8a71bf1d119a58dc2d9f1388751d.patch?full_index=1"
-        sha256 "127b6ec571e6d6b2617bf9ece98b756da651dc1a9842fbf0e5a53f982fef1d6d"
-      end
+      url "https://github.com/minetest/irrlicht/archive/1.9.0mt7.tar.gz"
+      sha256 "c12cdbd4a852e1e6ebf7ba22789aa057a1a7f2d585dd81a2412a62f57a0e2619"
     end
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game/archive/5.5.0.tar.gz"
-      sha256 "1e87252e26d6b1d3efe7720e3e097d489339dea4dd25980a828d5da212b01aaa"
+      url "https://github.com/minetest/minetest_game/archive/5.6.0.tar.gz"
+      sha256 "fd991d42c253db380559c593a2b035f22e07a81f867b5380a2f045e9a4d04c87"
     end
   end
 
@@ -70,7 +53,7 @@ class Minetest < Formula
   depends_on "libogg"
   depends_on "libpng"
   depends_on "libvorbis"
-  depends_on "luajit-openresty"
+  depends_on "luajit"
   depends_on "zstd"
 
   uses_from_macos "curl"
@@ -83,6 +66,7 @@ class Minetest < Formula
     depends_on "libxxf86vm"
     depends_on "mesa"
     depends_on "openal-soft"
+    depends_on "xinput"
   end
 
   def install
@@ -114,22 +98,12 @@ class Minetest < Formula
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-  end
 
-  def caveats
-    <<~EOS
-      Put additional subgames and mods into "games" and "mods" folders under
-      "~/Library/Application Support/minetest/", respectively (you may have
-      to create those folders first).
-
-      To start minetest, from a terminal run
-      "#{prefix}/minetest.app/Contents/MacOS/minetest".
-    EOS
+    bin.write_exec_script prefix/"minetest.app/Contents/MacOS/minetest" if OS.mac?
   end
 
   test do
-    minetest = OS.mac? ? prefix/"minetest.app/Contents/MacOS/minetest" : bin/"minetest"
-    output = shell_output("#{minetest} --version")
+    output = shell_output("#{bin}/minetest --version")
     assert_match "USE_CURL=1", output
     assert_match "USE_GETTEXT=1", output
     assert_match "USE_SOUND=1", output
