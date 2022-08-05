465cb66bd0a563e378af7234bb50ca3b8a603192
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-05 01:33:38 +0000

    algernon: update 1.14.0 bottle.

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -13,12 +13,12 @@ class Algernon < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "54189c2ada66deb8da6f827a6e23f1085c3455f6b3fed8265b2c21a3a38fc654"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a37f04e1db3b4db20f9e4bfc1e0e6a7bca9cca80555fe8c66295a79384cf7608"
-    sha256 cellar: :any_skip_relocation, monterey:       "9379b04db4148f53b9d435e0868d3a0af2041c3c45402e55a150e24d4ec070da"
-    sha256 cellar: :any_skip_relocation, big_sur:        "aed213b4eb7454bc8cd6d6fcb4bda60c821497dac5dde820dc41c891af856ed5"
-    sha256 cellar: :any_skip_relocation, catalina:       "5069b96a60ade19ce3bb97a020cbb432eda8fd65a60e241755d3294048e4e656"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2250b3159431e738598e16abbfc0f6b621ebcbe5e76444915a642de6c84245ba"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "343a3153a0af6ad0fc1ad27939eb9a2dbc9f379bd5bf6b4e373b9fd2f87d3ed4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "401c99640d17d66677f419c8da8719618c499e02e2b6f70b74eefec104378912"
+    sha256 cellar: :any_skip_relocation, monterey:       "a0f090b5d513eedc54de13a6f954a33ac0a126ea2b1bd1dde009629bd0854707"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3f25029f7b5dcf462b1b6e3d3a69e246d881201a7f9ab35657cd11dd25916f6a"
+    sha256 cellar: :any_skip_relocation, catalina:       "fff5b9bfa21524d8f9bff2266978dc7dcb4a8a6a35a19eb20397d3993a0e3445"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1fb0615287ba76cf2372cc91154d458c881d175e877b4140eeab7487282a7269"
   end
 
   depends_on "go" => :build
