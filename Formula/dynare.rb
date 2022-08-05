997f7b98c4e3a7c5c445b7fa73996ebd4f2c8172
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-05 18:52:02 +0000

    dynare: update 5.2_2 bottle.

--- Formula/dynare.rb
+++ Formula/dynare.rb
@@ -32,12 +32,12 @@ class Dynare < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "577765a34cad796a8cd5c4af4e1d840bb227e32622da7938f8a8f88e0009f192"
-    sha256 cellar: :any, arm64_big_sur:  "9fdf81f60dc8000844a09b14bf1bfba19d527598eed242045aa9f58bdfc7bce1"
-    sha256 cellar: :any, monterey:       "6cea8b141d86b09e4cc8d66599271a4ce43b8160614c751f11eb5f54a6368f8a"
-    sha256 cellar: :any, big_sur:        "708994cc5337116c60a58b37250b4381babbb3880ba9856880614863497e2d72"
-    sha256 cellar: :any, catalina:       "b06faa15aac9723dc15dd350de4ff846dd201ad71a338a0222f7b7acf6f3dafd"
-    sha256               x86_64_linux:   "ce6f2cb07b981c5ac0eb1b17a2d07b6279da54f0a22c71bea842e5c70f595101"
+    sha256 cellar: :any, arm64_monterey: "729657626a5dd8b7019b4594dc9d6c1c586863b24bc538da696fc0503127ecd9"
+    sha256 cellar: :any, arm64_big_sur:  "b9a6d4a197f260137a3db9915d2dc18296d2afa7ba175c162dad175f65701391"
+    sha256 cellar: :any, monterey:       "31b80cc41d34278e8a20ec410c0f2e8aa51b39051fc99f99216384ba76e53114"
+    sha256 cellar: :any, big_sur:        "b132c70628fbf91bfda7ad89254106f44d0cae4d1b6f918c0e9dc6546b682270"
+    sha256 cellar: :any, catalina:       "2ad82626a67004ec88a33688e51e08a9b07f0d4a3ef67a459ec1ba73be21360e"
+    sha256               x86_64_linux:   "13bb74fafff238cb14e388f3fe6784da7d38ad3563d2b82e07eafb72ca622faa"
   end
 
   head do
