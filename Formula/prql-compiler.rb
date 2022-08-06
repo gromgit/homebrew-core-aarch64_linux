c198da140a0442eda0c7c05a626e0b0dcc82fbd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-06 13:09:49 +0000

    prql-compiler: update 0.2.6 bottle.

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -6,12 +6,12 @@ class PrqlCompiler < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f6c18de881fa5a79d71a39d3ddb2ba213a319250efa202776714e3a71115136c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1bf112635d3a6376522fee4301765b86b8e523178579f7829426db59ad998098"
-    sha256 cellar: :any_skip_relocation, monterey:       "8290a090f7cc180d8f67072d31783407693836406b759c22914baf02757869e8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cb16a09401ed22e58b0217019d41a52d1936e50da669bf3385c908c79141a3d7"
-    sha256 cellar: :any_skip_relocation, catalina:       "76dd3c32e8a9afe0e20cdecebbad0aa779986ef485f5777c3b0d82181bee65bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94136b5e2eb88017984c1e52b4a6a97158d4719d0fa5ea27075db09c7ceb9277"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "31dbb40b0ca78f1af403149790deb0db05704b7890fc162d8b7e5f6e118bbd5c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "774fb758905e3b1fb48cbb279b412eb81b9bc61efc607de2711e2bdc728e584e"
+    sha256 cellar: :any_skip_relocation, monterey:       "993080157b0e7ddd3140ac95a70994dd393f09f3c8f5072bdc7d3f233b21d7d0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2c4eec55bb2ad1b3c80d7b38d6fe3e64b8566cecd1c2dcf01313afef94e52df5"
+    sha256 cellar: :any_skip_relocation, catalina:       "289e483f97d21e4dd94c88ab33274a98cc3db2be5653f7e3fe541ad6786a6116"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d942a8c7d71ec638a4d56ae13b9d48d22a72647b3ca3f4e51e659e16fce1fccd"
   end
 
   depends_on "rust" => :build
