cf38480d59ae4ac354b31f215a3845843636d94b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-06 03:27:12 +0000

    little-cms: update 1.19_2 bottle.

--- Formula/little-cms.rb
+++ Formula/little-cms.rb
@@ -7,16 +7,12 @@ class LittleCms < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2ffcd73342ab7d70a8a355578f62947ed40701d3fd2a443ff505235bbb9a26fc"
-    sha256 cellar: :any,                 arm64_big_sur:  "e16ca9dc1d92be7f1aefb1496d713c4635aaa032dac246df26dd7e86ad1abd65"
-    sha256 cellar: :any,                 monterey:       "03ba1b40b43757c686de1f5ad61b386fda288ab4f1acfee1e8aa773cb55fe00b"
-    sha256 cellar: :any,                 big_sur:        "1936e2e42994578c57a209c4bcbca9f817af24ebe6f89ef6ab42d90b93e3bc85"
-    sha256 cellar: :any,                 catalina:       "73cda76fd98e9466e570243f5190e68b45ffeeea2073185a51dd14dbde11a21a"
-    sha256 cellar: :any,                 mojave:         "d04e4cc09f471260e6e86eb866743eb0205b3345bc9e687d85307cdbd1a1fa9a"
-    sha256 cellar: :any,                 high_sierra:    "cead96af013b65c05e98c89890e66de1cdf864d1b6ed7da811f6618f2e551275"
-    sha256 cellar: :any,                 sierra:         "227c16cbe117abeac7398265543c20b905396b214785e1a9dc48041f0f3ce128"
-    sha256 cellar: :any,                 el_capitan:     "c1125a0074a82747ffc33ab79c617ea448b605ace47d6c5cf788f2d3a49d7c5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c687f10094faebcec6573681241dc7f45e3f19ffcaab3ef2a56c987d13ba18a3"
+    sha256 cellar: :any,                 arm64_monterey: "06506916f644bbf3aa1138360c29f811a0bb0a5ab23cc2941a4e9a94c5c1f94a"
+    sha256 cellar: :any,                 arm64_big_sur:  "c5716bd45fe8a883db5ca7b25d68dd7b31a04a5d9114aea8fbc1a2cd653d13c6"
+    sha256 cellar: :any,                 monterey:       "0ee9976dd6fccec2cec2d9dd4fddf00a86400a9ad86eae581c7ce209bcb12a55"
+    sha256 cellar: :any,                 big_sur:        "68517589d37a67656069fbb39cd57080f7887cd855a8533601dc09b77005e160"
+    sha256 cellar: :any,                 catalina:       "459d6cd293c906cff22b981734661ab8d56c2269ab0c914ecd57e619f092a864"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "077ed4556feb380643d0aa9aab9bcbf1285f8b0c8f0702d422ab5c03c431668c"
   end
 
   deprecate! date: "2022-06-12", because: :unmaintained
