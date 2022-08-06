c74aa3b7d5e2509a39dfc5ec8906dded6aeaf834
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-06 20:05:02 +0000

    open-babel: update 3.1.1_2 bottle.

--- Formula/open-babel.rb
+++ Formula/open-babel.rb
@@ -9,14 +9,12 @@ class OpenBabel < Formula
   head "https://github.com/openbabel/openbabel.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "a540dd5b2915c605891f9d97f9afad7095b18ea37777001b44af287e37e7a61e"
-    sha256 arm64_big_sur:  "990abdbe32d92c4d6e7a17f1d5a2c1c4cd979f4daba30a8eba4a9e20a3e0d099"
-    sha256 monterey:       "9fb8ab5ed561791e5610bac7fe2a8ac4ccfbed05f87de3e0884311dff67129d9"
-    sha256 big_sur:        "f741147b739d71d7bfb79e80ad89ad49f05a6922a36c6f663690c1a5f65cdcdb"
-    sha256 catalina:       "770cdafc4dfdd0c216ca9308d5b5ae6b9b00be8d30b387dd2e86cbff82db5acd"
-    sha256 mojave:         "6c44b3e574a786396b8099192e5154f1d751ee413ec265c99f8046fc5167876a"
-    sha256 high_sierra:    "49d5dc2fc8c9a857bf08e6e6711fbaf48d8a0165c328cc400e904aa5a37080a4"
-    sha256 x86_64_linux:   "d71a0afa160bb08ca47f40b3081ee0655b0e4890604fe1a3434a8a13d86273c7"
+    sha256 arm64_monterey: "f62cb276d94338ddfefe3610db7242b07071522e51b644993d538b27cdd67336"
+    sha256 arm64_big_sur:  "d21957b0b507d271ee125ba0ce47bd87eea70c807e78510a06c4066f476eb27d"
+    sha256 monterey:       "fe236b2a01b1ac206432b3954c66071bdb0f2b55540c699974a9d6410d0ddfe7"
+    sha256 big_sur:        "414dc53a50dd9eb8e5cedfead87ca2c79aae360075ef565c860526a7902167bd"
+    sha256 catalina:       "40e04ad79487b0394ea3b34443c9e2d4a6409915e941c54806e4d1fd2b6e3214"
+    sha256 x86_64_linux:   "6654aeb57160c573dbe4d069fc36fc8a3ecb46be163cabf0252f17c7a15480a6"
   end
 
   depends_on "cmake" => :build
