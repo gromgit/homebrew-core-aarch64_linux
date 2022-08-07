16d0a994e43ad632cdae9238abc30db963f566d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-07 11:05:03 +0000

    librem: update 2.6.0 bottle.

--- Formula/librem.rb
+++ Formula/librem.rb
@@ -6,12 +6,12 @@ class Librem < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "699e3eab63cd9cf5866b49f5e8983fb5ac55780ddb5167ce2ca5dec5a04fa82f"
-    sha256 cellar: :any,                 arm64_big_sur:  "b178354be3ffaa400644548e9129b6d0dbb1ef18f9f51a165420ea13b2c7527d"
-    sha256 cellar: :any,                 monterey:       "a8836ec9dffaccca68e922a0cb0a704b2485f904ddc7f80328bd7547abe88cc0"
-    sha256 cellar: :any,                 big_sur:        "53ef8d92e586a48a92f6f7497297be6c9257c50b390fa72d1c039d5b405593d9"
-    sha256 cellar: :any,                 catalina:       "99aba430d95b96c6f1549e2b5ec177553681ddf629556d056faf5352040a46f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "80ab1e9df9f49c15c3f47db89d1415d96be6c65e8fd8ad6c85d75e97e5269784"
+    sha256 cellar: :any,                 arm64_monterey: "e5f8a521da904deffef99314ed36c6c43888a999dfbf465c52dee9cb05452ef6"
+    sha256 cellar: :any,                 arm64_big_sur:  "2200670c86080f78d23c1adb9b1f2229c91dcc94f0f309a7594eaf7304c5d45b"
+    sha256 cellar: :any,                 monterey:       "1454c2f9eba38d761f4f99e45d766bdd30507f6a59a68ba095dc516ff5545e0f"
+    sha256 cellar: :any,                 big_sur:        "4c097ba26f1374bb892d5134661042d8518a57a4b59ec87b5a8328311aaaf7e7"
+    sha256 cellar: :any,                 catalina:       "ec13bba6c8d857c5f06cca994ce4048c97589ebe8943bb1d1f40605199a3c1e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "176e84e3c75d0c4c723f1ea8d980d8694ef18933073142e4b40cba593ec5082a"
   end
 
   depends_on "libre"
