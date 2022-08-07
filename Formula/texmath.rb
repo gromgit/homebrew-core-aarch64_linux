a132bc7cfc2c84832c60e81a587736946ec9fc12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-07 11:05:17 +0000

    texmath: update 0.12.5.2 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -7,12 +7,12 @@ class Texmath < Formula
   head "https://github.com/jgm/texmath.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ede9fba426d612e361ed9c9a555df8626430705848762ff508cc2f968f8df1b2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c2de9e1e654e02a6a8ef31c7460a6b7734cbd7736d75133825d21531c92c5cae"
-    sha256 cellar: :any_skip_relocation, monterey:       "17fb6cd730166f793a69f4f89a6525c4fe951430d7c0633427df1a02b61a1354"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0dc6314da2349f54a25d80f4bac178ca76f902b2672a24a1ff43125190f416b9"
-    sha256 cellar: :any_skip_relocation, catalina:       "2fbdd8536dedf910fceed12941303db12d116c84668b70e96c4ae0edcffd5c05"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae79c2d327212a2de6cac0e0acf915d8e6aa44c2bca365bc7d0dce99b29a468d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8148a6edc93d707170e432b93375740f90d1dcdf633c78732cc9eabc5ed44953"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "32e5646b41724c6c9783806fe09a3fe4e9b6fadea39991ed756bdae7051f08eb"
+    sha256 cellar: :any_skip_relocation, monterey:       "113b22dc9fc5f6e8f96738f67325b18baa47942ec89646a3226d1cc57e292e10"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c4f9e468a0072856d1e7f5dab61a6f5e1699881c862064b7c5d9d1df1d9af036"
+    sha256 cellar: :any_skip_relocation, catalina:       "cba74dc0e4fd05bd102441b557224bb58a2c7f1e254001becc685d43f14c61c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4af06fb5e1031316e9e2ab40a9a92b03e85fbd40e2bd9f1806b055e047cd175"
   end
 
   depends_on "cabal-install" => :build
