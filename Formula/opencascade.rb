d9dbb420be439705cb2ef3178f1b20b417005dae
Rui Chen <rui@chenrui.dev>
2022-08-07 01:07:32 -0400

    opencascade 7.6.3
    Closes #107512.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opencascade.rb
+++ Formula/opencascade.rb
@@ -1,9 +1,9 @@
 class Opencascade < Formula
   desc "3D modeling and numerical simulation software for CAD/CAM/CAE"
   homepage "https://dev.opencascade.org/"
-  url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_6_2;sf=tgz"
-  version "7.6.2"
-  sha256 "443f782f4adfd316e3f81bfad01d5367ae55ceb0d04c1acec1b87baae5c75e1c"
+  url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_6_3;sf=tgz"
+  version "7.6.3"
+  sha256 "baae5b3a7a38825396fc45ef9d170db406339f5eeec62e21b21036afeda31200"
   license "LGPL-2.1-only"
 
   # The first-party download page (https://dev.opencascade.org/release)
