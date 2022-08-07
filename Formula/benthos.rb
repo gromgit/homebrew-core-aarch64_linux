c6ffc437cfc9468fa8adecc67f0a290a6301bf78
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-07 23:05:26 +0000

    benthos: update 4.5.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -7,12 +7,12 @@ class Benthos < Formula
   head "https://github.com/benthosdev/benthos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cdc42783aae3cb8ce22ca90a61f1bed411f4e6ae30f603507ff84d4c54054806"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "599464785290b05b860fccc3c4ef43d6ba8a532ac086f18753f3efd4401b70ef"
-    sha256 cellar: :any_skip_relocation, monterey:       "b43df153be63c8fae299bdde52c42f5dbd8da8351884085fa8f32ef7e371c513"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b1d979f9faca05a8b109df61474ba49d6fe22863d5583c07c311bbf41374af18"
-    sha256 cellar: :any_skip_relocation, catalina:       "9ddd9d83656a3c603743f247e1fa544ad991b0ac27a9a6965d308a5a07ae23a5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d1c760314764874b85bbd54695cf06bb0b45053377d2a886b7b000cb8d5529cf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f90438f7d064b5f8defb2920df9108d54a6201b598e9dc753151a77878121e7f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c69308b6016249a3e29e819f730b94f8dda0b077966143a46bbb4e1d407f8e16"
+    sha256 cellar: :any_skip_relocation, monterey:       "84dba2b096ae21adb0458dac2064f367580e6ad6d45d525a5971bb29f279b3ad"
+    sha256 cellar: :any_skip_relocation, big_sur:        "637a2d098b298ec70068e968371066ae7f1ba15b5cfff072e9785b95089e124c"
+    sha256 cellar: :any_skip_relocation, catalina:       "e55a498a3db868b1e77b65a61edba00d766e0a36d88840b5b5c495838f86f072"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7726973dd04de8f91a2ca9efbb3154c5a23819830840f635d9c69eaab25c4b4d"
   end
 
   depends_on "go" => :build
