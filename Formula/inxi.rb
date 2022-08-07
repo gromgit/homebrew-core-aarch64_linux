c5a95a4f7b0fc2af57df1d00c8ece9dfe0218e5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-07 11:05:14 +0000

    inxi: update 3.3.20-1 bottle.

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -7,12 +7,12 @@ class Inxi < Formula
   head "https://github.com/smxi/inxi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cb51b22181b68460ade685b88d697c3b97e01534cc1ba1d6c4a4d245b2a55f6d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cb51b22181b68460ade685b88d697c3b97e01534cc1ba1d6c4a4d245b2a55f6d"
-    sha256 cellar: :any_skip_relocation, monterey:       "6d8315a833249edc40f1bb82f50a64ecc0d7f0749cd9a1fa79f74417e1d6d635"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6d8315a833249edc40f1bb82f50a64ecc0d7f0749cd9a1fa79f74417e1d6d635"
-    sha256 cellar: :any_skip_relocation, catalina:       "6d8315a833249edc40f1bb82f50a64ecc0d7f0749cd9a1fa79f74417e1d6d635"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb51b22181b68460ade685b88d697c3b97e01534cc1ba1d6c4a4d245b2a55f6d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6d7465cc68039981acdea5a227a46fc4e819f4bf927f27d417aa0c592736fdf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b6d7465cc68039981acdea5a227a46fc4e819f4bf927f27d417aa0c592736fdf"
+    sha256 cellar: :any_skip_relocation, monterey:       "17f0e402a6db124b8642978c0dc9495383e25d114088a90ab0f5620d01488722"
+    sha256 cellar: :any_skip_relocation, big_sur:        "17f0e402a6db124b8642978c0dc9495383e25d114088a90ab0f5620d01488722"
+    sha256 cellar: :any_skip_relocation, catalina:       "17f0e402a6db124b8642978c0dc9495383e25d114088a90ab0f5620d01488722"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6d7465cc68039981acdea5a227a46fc4e819f4bf927f27d417aa0c592736fdf"
   end
 
   uses_from_macos "perl"
