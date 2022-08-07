b093bfba9d838cb56c95323290e009bc1210340d
Michael Cho <cho-m@tuta.io>
2022-08-06 18:44:45 -0700

    cp2k 2022.1
    Closes #107496.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cp2k.rb
+++ Formula/cp2k.rb
@@ -1,10 +1,9 @@
 class Cp2k < Formula
   desc "Quantum chemistry and solid state physics software package"
   homepage "https://www.cp2k.org/"
-  url "https://github.com/cp2k/cp2k/releases/download/v8.2.0/cp2k-8.2.tar.bz2"
-  sha256 "2e24768720efed1a5a4a58e83e2aca502cd8b95544c21695eb0de71ed652f20a"
+  url "https://github.com/cp2k/cp2k/releases/download/v2022.1/cp2k-2022.1.tar.bz2"
+  sha256 "2c34f1a7972973c62d471cd35856f444f11ab22f2ff930f6ead20f3454fd228b"
   license "GPL-2.0-or-later"
-  revision 2
 
   bottle do
     sha256 cellar: :any, arm64_monterey: "2f1fa6d5a62af7f9714d31837435f398be4261f89c3b029e0db964faf4ce38d7"
@@ -21,6 +20,10 @@ class Cp2k < Formula
   depends_on "open-mpi"
   depends_on "scalapack"
 
+  on_linux do
+    depends_on "openblas"
+  end
+
   fails_with :clang # needs OpenMP support
 
   resource "libint" do
@@ -35,47 +38,95 @@ class Cp2k < Formula
       ENV.deparallelize { system "make", "install" }
     end
 
-    # libint needs `-lstdc++` (https://github.com/cp2k/cp2k/blob/master/INSTALL.md)
-    # Can remove if added upstream to Darwin-gfortran.psmp and Darwin-gfortran.ssmp
-    libs = %W[
-      -L#{Formula["fftw"].opt_lib}
-      -lfftw3
-      -lstdc++
-    ]
+    arch = "local"
+    if OS.mac?
+      arch = "Darwin-gfortran"
 
-    ENV["LIBXC_INCLUDE_DIR"] = Formula["libxc"].opt_include
-    ENV["LIBXC_LIB_DIR"] = Formula["libxc"].opt_lib
-    ENV["LIBINT_INCLUDE_DIR"] = libexec/"include"
-    ENV["LIBINT_LIB_DIR"] = libexec/"lib"
+      # libint needs `-lstdc++` (https://github.com/cp2k/cp2k/blob/master/INSTALL.md)
+      # Can remove if added upstream to Darwin-gfortran.psmp and Darwin-gfortran.ssmp
+      libs = %W[
+        -L#{Formula["fftw"].opt_lib}
+        -lfftw3
+        -lstdc++
+      ]
 
-    # CP2K configuration is done through editing of arch files
-    inreplace Dir["arch/Darwin-gfortran.*"].each do |s|
-      s.gsub!(/DFLAGS *=/, "DFLAGS = -D__FFTW3")
-      s.gsub!(/FCFLAGS *=/, "FCFLAGS = -I#{Formula["fftw"].opt_include}")
-      s.gsub!(/LIBS *=/, "LIBS = #{libs.join(" ")}")
+      ENV["LIBXC_INCLUDE_DIR"] = Formula["libxc"].opt_include
+      ENV["LIBXC_LIB_DIR"] = Formula["libxc"].opt_lib
+      ENV["LIBINT_INCLUDE_DIR"] = libexec/"include"
+      ENV["LIBINT_LIB_DIR"] = libexec/"lib"
+
+      # CP2K configuration is done through editing of arch files
+      inreplace Dir["arch/Darwin-gfortran.*"].each do |s|
+        s.gsub!(/DFLAGS *=/, "DFLAGS = -D__FFTW3")
+        s.gsub!(/FCFLAGS *=/, "FCFLAGS = -I#{Formula["fftw"].opt_include}")
+        s.gsub!(/LIBS *=/, "LIBS = #{libs.join(" ")}")
+      end
+
+      # MPI versions link to scalapack
+      inreplace Dir["arch/Darwin-gfortran.p*"],
+                /LIBS *=/, "LIBS = -L#{Formula["scalapack"].opt_lib}"
+
+      # OpenMP versions link to specific fftw3 library
+      inreplace Dir["arch/Darwin-gfortran.*smp"],
+                "-lfftw3", "-lfftw3 -lfftw3_threads"
+    else
+      args = %W[
+        -j #{ENV.make_jobs}
+        --mpi-mode=openmpi
+        --math-mode=openblas
+        --with-gcc=system
+        --with-intel=no
+        --with-cmake=no
+        --with-openmpi=#{Formula["open-mpi"].opt_prefix}
+        --with-mpich=no
+        --with-intelmpi=no
+        --with-libxc=#{Formula["libxc"].opt_prefix}
+        --with-libint=#{libexec}
+        --with-fftw=#{Formula["fftw"].opt_prefix}
+        --with-acml=no
+        --with-mkl=no
+        --with-openblas=#{Formula["openblas"].opt_prefix}
+        --with-scalapack=#{Formula["scalapack"].opt_prefix}
+        --with-libxsmm=no
+        --with-elpa=no
+        --with-ptscotch=no
+        --with-superlu=no
+        --with-pexsi=no
+        --with-quip=no
+        --with-plumed=no
+        --with-sirius=no
+        --with-gsl=no
+        --with-libvdwxc=no
+        --with-spglib=no
+        --with-hdf5=no
+        --with-spfft=no
+        --with-spla=no
+        --with-cosma=no
+        --with-libvori=no
+      ]
+      args << "--generic" if build.bottle?
+
+      cd "tools/toolchain" do
+        # Need OpenBLAS source to get proc arch info in scripts/get_openblas_arch.sh
+        Formula["openblas"].stable.stage Pathname.pwd/"build/OpenBLAS"
+
+        system "./install_cp2k_toolchain.sh", *args
+        (buildpath/"arch").install (Pathname.pwd/"install/arch").children
+      end
     end
-
-    # MPI versions link to scalapack
-    inreplace Dir["arch/Darwin-gfortran.p*"],
-              /LIBS *=/, "LIBS = -L#{Formula["scalapack"].opt_prefix}/lib"
-
-    # OpenMP versions link to specific fftw3 library
-    inreplace Dir["arch/Darwin-gfortran.*smp"],
-              "-lfftw3", "-lfftw3 -lfftw3_threads"
 
     # Now we build
     %w[ssmp psmp].each do |exe|
-      # Issue with parallel build: https://github.com/cp2k/cp2k/issues/1316
-      ENV.deparallelize { system "make", "ARCH=Darwin-gfortran", "VERSION=#{exe}" }
-      bin.install "exe/Darwin-gfortran/cp2k.#{exe}"
-      bin.install "exe/Darwin-gfortran/cp2k_shell.#{exe}"
+      system "make", "ARCH=#{arch}", "VERSION=#{exe}"
+      bin.install "exe/#{arch}/cp2k.#{exe}"
+      bin.install "exe/#{arch}/cp2k_shell.#{exe}"
     end
 
     (pkgshare/"tests").install "tests/Fist/water512.inp"
   end
 
   test do
-    system "#{bin}/cp2k.ssmp", "#{pkgshare}/tests/water512.inp"
-    system "mpirun", "#{bin}/cp2k.psmp", "#{pkgshare}/tests/water512.inp"
+    system bin/"cp2k.ssmp", pkgshare/"tests/water512.inp"
+    system "mpirun", bin/"cp2k.psmp", pkgshare/"tests/water512.inp"
   end
 end
