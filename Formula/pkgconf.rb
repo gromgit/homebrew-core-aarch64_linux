88475718f5e9747093998c2798b1b3d4febc4b5c
Porkepix <porkepix@gmail.com>
2022-08-07 09:30:25 +0200

    pkgconf 1.9.2
    Closes #107522.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pkgconf.rb
+++ Formula/pkgconf.rb
@@ -1,8 +1,8 @@
 class Pkgconf < Formula
   desc "Package compiler and linker metadata toolkit"
   homepage "https://git.sr.ht/~kaniini/pkgconf"
-  url "https://distfiles.dereferenced.org/pkgconf/pkgconf-1.8.0.tar.xz"
-  sha256 "ef9c7e61822b7cb8356e6e9e1dca58d9556f3200d78acab35e4347e9d4c2bbaf"
+  url "https://distfiles.dereferenced.org/pkgconf/pkgconf-1.9.2.tar.xz"
+  sha256 "db6bf5426e0e9fc107042cc85fc62b1f391f1d7af46c4a3c39b7f5b5231dfa09"
   license "ISC"
 
   livecheck do
