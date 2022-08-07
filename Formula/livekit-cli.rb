d3335349c7c8468704b8ea3f3847a52d3cbfb0ea
Rui Chen <rui@chenrui.dev>
2022-08-06 23:20:45 -0400

    livekit-cli 1.0.3
    Closes #107509.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/livekit-cli.rb
+++ Formula/livekit-cli.rb
@@ -1,8 +1,8 @@
 class LivekitCli < Formula
   desc "Command-line interface to LiveKit"
   homepage "https://livekit.io"
-  url "https://github.com/livekit/livekit-cli/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "6c3102aa027d8d8f735fff4e16608e91227497650aa93f07ea0d8d1111f98f9d"
+  url "https://github.com/livekit/livekit-cli/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "06378b0a4e440eed7fd2df8daefe464c5425ffdb6d7220b141215f1f51f3e527"
   license "Apache-2.0"
   head "https://github.com/livekit/livekit-cli.git", branch: "main"
 
