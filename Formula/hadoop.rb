21546a769df89b995288216518267ee7b4d6a57b
William Tang <galaxyking0419@gmail.com>
2022-08-08 11:19:38 +0800

    hadoop: depends_on "openjdk@11"
    According to https://cwiki.apache.org/confluence/display/HADOOP/Hadoop+Java+Versions, hadoop only supports java runtime 11 at most.
    - Switched dependency from "openjdk" to "openjdk@11" for flume, hadoop and mahout
    - Added a test case to check if resource manager server can start successfully with the JDK
    - Appended HADOOP_LOG_DIR to env scripts
    - Minor refactoring
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/hadoop.rb
+++ Formula/hadoop.rb
@@ -15,27 +15,62 @@ class Hadoop < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "92cb92589ac48035f2482eca9321cebe31dfd3873272da601e24f30cc758cb5e"
   end
 
-  depends_on "openjdk"
+  # WARNING: Check https://cwiki.apache.org/confluence/display/HADOOP/Hadoop+Java+Versions before updating JDK version
+  depends_on "openjdk@11"
 
   conflicts_with "yarn", because: "both install `yarn` binaries"
 
   def install
     rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
     libexec.install %w[bin sbin libexec share etc]
-    Dir["#{libexec}/bin/*"].each do |path|
-      (bin/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+
+    hadoop_env = Language::Java.java_home_env("11")
+    hadoop_env[:HADOOP_LOG_DIR] = var/"hadoop"
+
+    (libexec/"bin").each_child do |path|
+      (bin/File.basename(path)).write_env_script path, hadoop_env
     end
-    Dir["#{libexec}/sbin/*"].each do |path|
-      (sbin/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    (libexec/"sbin").each_child do |path|
+      (sbin/File.basename(path)).write_env_script path, hadoop_env
     end
-    Dir["#{libexec}/libexec/*.sh"].each do |path|
-      (libexec/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    libexec.glob("libexec/*.sh").each do |path|
+      (libexec/File.basename(path)).write_env_script path, hadoop_env
     end
+
     # Temporary fix until https://github.com/Homebrew/brew/pull/4512 is fixed
-    chmod 0755, Dir["#{libexec}/*.sh"]
+    chmod 0755, libexec.glob("*.sh")
   end
 
   test do
     system bin/"hadoop", "fs", "-ls"
+
+    # Test if resource manager can start successfully
+    port = free_port
+    classpaths = %w[
+      etc/hadoop
+      share/hadoop/common/lib/*
+      share/hadoop/common/*
+      share/hadoop/hdfs
+      share/hadoop/hdfs/lib/*
+      share/hadoop/hdfs/*
+      share/hadoop/mapreduce/*
+      share/hadoop/yarn
+      share/hadoop/yarn/lib/*
+      share/hadoop/yarn/*
+      share/hadoop/yarn/timelineservice/*
+      share/hadoop/yarn/timelineservice/lib/*
+    ].map { |path| libexec/path }
+
+    pid = Process.spawn({
+      "JAVA_HOME" => Language::Java.java_home("11"),
+      "CLASSPATH" => classpaths.join(":"),
+    }, Formula["openjdk@11"].opt_bin/"java", "org.apache.hadoop.yarn.server.resourcemanager.ResourceManager",
+                                             "-Dyarn.resourcemanager.webapp.address=127.0.0.1:#{port}")
+    sleep 8
+
+    Process.getpgid pid
+    system "curl", "http://127.0.0.1:#{port}"
+  ensure
+    Process.kill "TERM", pid
   end
 end
