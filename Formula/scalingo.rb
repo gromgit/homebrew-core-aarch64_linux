d9bffb3c736f93b5a48efa489ed748d7dda82d02
Rui Chen <rui@chenrui.dev>
2022-08-08 16:53:52 -0400

    scalingo 1.24.0
    Closes #107607.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scalingo.rb
+++ Formula/scalingo.rb
@@ -1,8 +1,8 @@
 class Scalingo < Formula
   desc "CLI for working with Scalingo's PaaS"
   homepage "https://doc.scalingo.com/cli"
-  url "https://github.com/Scalingo/cli/archive/1.23.0.tar.gz"
-  sha256 "382e0272296595f0327146cb37c5b991c5e3387e26d0f0a4a01b9ab6df76befe"
+  url "https://github.com/Scalingo/cli/archive/1.24.0.tar.gz"
+  sha256 "0bc043ecc6f8b536800d7cab2974addec2737d51a80c4cef020bc47f82a5f7f2"
   license "BSD-4-Clause"
 
   bottle do
