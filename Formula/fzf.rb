e2e206d1e11cf6bc11092d1cd3f7c9be38c43441
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-08 17:08:16 +0000

    fzf: update 0.32.1 bottle.

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -7,12 +7,12 @@ class Fzf < Formula
   head "https://github.com/junegunn/fzf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8382d9b295388607f5683f58c68bb9601935fb1cd639161b5fe980423c127b3f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8382d9b295388607f5683f58c68bb9601935fb1cd639161b5fe980423c127b3f"
-    sha256 cellar: :any_skip_relocation, monterey:       "c294f52838622afbaf2ec8b7cfe0b882cebf202d63a48aecd9a22c288aa25f41"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c294f52838622afbaf2ec8b7cfe0b882cebf202d63a48aecd9a22c288aa25f41"
-    sha256 cellar: :any_skip_relocation, catalina:       "c294f52838622afbaf2ec8b7cfe0b882cebf202d63a48aecd9a22c288aa25f41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ee47c63dbbc25d4499df6eeeac871e9f345b8d19ce03e97506e8d50dd6e156d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "475d6e470c916d77c2e41d5a6684e2d8152b541a992eeb9b7a979a28d71b86c4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "475d6e470c916d77c2e41d5a6684e2d8152b541a992eeb9b7a979a28d71b86c4"
+    sha256 cellar: :any_skip_relocation, monterey:       "1159c0ae09118f29f568c854a654ac6004456bf747c19a7df8062ed4a41229e7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1159c0ae09118f29f568c854a654ac6004456bf747c19a7df8062ed4a41229e7"
+    sha256 cellar: :any_skip_relocation, catalina:       "1159c0ae09118f29f568c854a654ac6004456bf747c19a7df8062ed4a41229e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c8ae9c530d8f4e44061f13886be6249b0a2b4bbb399ebab12b126ba4a76e3c0"
   end
 
   depends_on "go" => :build
