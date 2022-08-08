c994576c777028915bf1113870764ea0dd6d4c4f
Scott Blum <dragonsinth@gmail.com>
2022-08-08 14:26:22 -0400

    grpcui 1.3.1
    Closes #107603.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grpcui.rb
+++ Formula/grpcui.rb
@@ -1,8 +1,8 @@
 class Grpcui < Formula
   desc "Interactive web UI for gRPC, along the lines of postman"
   homepage "https://github.com/fullstorydev/grpcui"
-  url "https://github.com/fullstorydev/grpcui/archive/v1.3.0.tar.gz"
-  sha256 "56519818d08a47339dece319cb4c8387a65bf24623f49242ef6a1201a1eb8b15"
+  url "https://github.com/fullstorydev/grpcui/archive/v1.3.1.tar.gz"
+  sha256 "01cfa0bbaf9cfdaa61ae0341c83cde3372854133d62cb9b91c3a111eaa145815"
   license "MIT"
 
   bottle do
