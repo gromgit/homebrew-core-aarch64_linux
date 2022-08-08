e5e14bbeb9de95dd478af690717f9f617afda440
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-08-08 15:54:18 +0300

    tarantool 2.10.1
    Also, bump version of openssl dependency from 1.1 to 3.
    
    Closes #107587.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tarantool.rb
+++ Formula/tarantool.rb
@@ -1,8 +1,8 @@
 class Tarantool < Formula
   desc "In-memory database and Lua application server"
   homepage "https://tarantool.org/"
-  url "https://download.tarantool.org/tarantool/src/tarantool-2.10.0.tar.gz"
-  sha256 "42bb5a714ddf788a89ba4b2062582aee16a2bc7b68c7c3b8c03d7a30be878e2b"
+  url "https://download.tarantool.org/tarantool/src/tarantool-2.10.1.tar.gz"
+  sha256 "615c9471ed4b4277427bd24b008a7ca7942da349d3391ba466823dab2bc34e6f"
   license "BSD-2-Clause"
   version_scheme 1
   head "https://github.com/tarantool/tarantool.git", branch: "master"
@@ -24,7 +24,7 @@ class Tarantool < Formula
   depends_on "cmake" => :build
   depends_on "icu4c"
   depends_on "libyaml"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "readline"
   depends_on "zstd"
 
