caba1488f232fb45b92ae961ae46d763f7c8aa33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-08 08:15:07 +0000

    mahout: update 0.13.0_1 bottle.

--- Formula/mahout.rb
+++ Formula/mahout.rb
@@ -8,7 +8,8 @@ class Mahout < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "baaec00c1e35a0a264b4ef3d0e7e5043f4ea8dfb264d3df278bd5fc629d7faf0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "3b7e05dbbd5838236245dd37e3ed7a1cc10d4dd151adbc7979b345cb19daab0e"
   end
 
   head do
