4d67472210b895da671a6cc1b46eb77165fd2dbd
Aaron Liu <aaronliu0130@gmail.com>
2022-08-08 13:02:58 -0400

    quilt-installer 0.4.3 (new formula)

---
+++ Formula/quilt-installer.rb
@@ -0,0 +1,24 @@
+class QuiltInstaller < Formula
+  desc "Installer for Quilt for the vanilla launcher"
+  homepage "https://quiltmc.org/"
+  url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.4.3/quilt-installer-0.4.3.jar"
+  sha256 "045668265303d41dae13abc5eb000e988ad409b792431663e06b46ad0811f80a"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://meta.quiltmc.org/v3/versions/installer"
+    regex(/["']url["']:\s*["'][^"']*?quilt-installer[._-]v?(\d+(?:\.\d+)+)\.jar/i)
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "quilt-installer-#{version}.jar"
+    bin.write_jar_script libexec/"quilt-installer-#{version}.jar", "quilt-installer"
+  end
+
+  test do
+    system "#{bin}/quilt-installer", "install", "server", "1.19.2"
+    assert_predicate testpath/"server/quilt-server-launch.jar", :exist?
+  end
+end
