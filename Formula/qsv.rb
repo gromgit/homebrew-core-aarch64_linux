6b81b4f28e5135a9d1d17faa048dee613554a56a
MoonFruit <dkmoonfruit@gmail.com>
2022-08-08 12:30:58 +0800

    qsv 0.61.3
    Closes #107560.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/qsv.rb
+++ Formula/qsv.rb
@@ -1,8 +1,8 @@
 class Qsv < Formula
   desc "Ultra-fast CSV data-wrangling toolkit"
   homepage "https://github.com/jqnatividad/qsv"
-  url "https://github.com/jqnatividad/qsv/archive/0.61.2.tar.gz"
-  sha256 "f2c81f12a8fbadf0893b946a8da59feaa604b117c67bd5b22c435f50e3ab8191"
+  url "https://github.com/jqnatividad/qsv/archive/0.61.3.tar.gz"
+  sha256 "20daecb65b952a3376759e15a14c5c413a17497cdd17f9905566a15155096b26"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/jqnatividad/qsv.git", branch: "master"
 
