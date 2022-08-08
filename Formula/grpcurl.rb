80719350f5c90480fe54f7adf45fd630a4f27796
Scott Blum <dragonsinth@gmail.com>
2022-08-08 14:08:39 -0400

    grpcurl 1.8.7
    Closes #107601.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grpcurl.rb
+++ Formula/grpcurl.rb
@@ -1,8 +1,8 @@
 class Grpcurl < Formula
   desc "Like cURL, but for gRPC"
   homepage "https://github.com/fullstorydev/grpcurl"
-  url "https://github.com/fullstorydev/grpcurl/archive/v1.8.6.tar.gz"
-  sha256 "18b457f644baabeef0de350596dd8d23563586ee94a3ed3cb290063e097ab934"
+  url "https://github.com/fullstorydev/grpcurl/archive/v1.8.7.tar.gz"
+  sha256 "7f7a59f8a5ef8833d30a94e1c36ddb0d76bab1ae64cd5c8bcb87d42e877c3bca"
   license "MIT"
 
   bottle do
