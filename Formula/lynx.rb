7bcc3df3355618f1e68b6f04458822361f3ad92a
Mohammed Anas <triallax@tutanota.com>
2022-08-10 02:50:36 +0300

    lynx: add Fossies mirror (#107673)

--- Formula/lynx.rb
+++ Formula/lynx.rb
@@ -2,6 +2,7 @@ class Lynx < Formula
   desc "Text-based web browser"
   homepage "https://invisible-island.net/lynx/"
   url "https://invisible-mirror.net/archives/lynx/tarballs/lynx2.8.9rel.1.tar.bz2"
+  mirror "https://fossies.org/linux/www/lynx2.8.9rel.1.tar.bz2"
   version "2.8.9rel.1"
   sha256 "387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
   license "GPL-2.0"
