3b388202b2f90115f0af16f506eddd77fd4ace1b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-09 23:15:30 +0800

    gobject-introspection: migrate to `python@3.10`

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -6,6 +6,7 @@ class GobjectIntrospection < Formula
   url "https://download.gnome.org/sources/gobject-introspection/1.72/gobject-introspection-1.72.0.tar.xz"
   sha256 "02fe8e590861d88f83060dd39cda5ccaa60b2da1d21d0f95499301b186beaabc"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later", "MIT"]
+  revision 1
 
   bottle do
     sha256 arm64_monterey: "f99f2db1c00cdde18f0cbfa00e70604dfaea7aa512256750eabc31cbb0181204"
@@ -21,11 +22,12 @@ class GobjectIntrospection < Formula
   depends_on "ninja" => :build
   depends_on "cairo"
   depends_on "glib"
-  depends_on "libffi"
   depends_on "pkg-config"
-  depends_on "python@3.9"
+  # TODO: Consider using `uses_from_macos "python"` instead.
+  depends_on "python@3.10"
 
   uses_from_macos "flex" => :build
+  uses_from_macos "libffi", since: :catalina
 
   resource "tutorial" do
     url "https://gist.github.com/7a0023656ccfe309337a.git",
@@ -47,19 +49,16 @@ class GobjectIntrospection < Formula
       "config.set_quoted('GOBJECT_INTROSPECTION_LIBDIR', join_paths(get_option('prefix'), get_option('libdir')))",
       "config.set_quoted('GOBJECT_INTROSPECTION_LIBDIR', '#{HOMEBREW_PREFIX}/lib')"
 
-    mkdir "build" do
-      system "meson", *std_meson_args,
-        "-Dpython=#{Formula["python@3.9"].opt_bin}/python3",
-        "-Dextra_library_paths=#{HOMEBREW_PREFIX}/lib",
-        ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-      rewrite_shebang detected_python_shebang, *bin.children
-    end
+    system "meson", "setup", "build", "-Dpython=#{Formula["python@3.10"].opt_bin}/python3.10",
+                                      "-Dextra_library_paths=#{HOMEBREW_PREFIX}/lib",
+                                      *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+
+    rewrite_shebang detected_python_shebang, *bin.children
   end
 
   test do
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libffi"].opt_lib/"pkgconfig"
     resource("tutorial").stage testpath
     system "make"
     assert_predicate testpath/"Tut-0.1.typelib", :exist?
