f4f987d60d270722ebece306a227a8637d61a97b
mhmdanas <triallax@tutanota.com>
2022-08-09 23:53:28 +0300

    isync: use berkeley-db@5
    See #100796.
    
    Closes #107672.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/isync.rb
+++ Formula/isync.rb
@@ -4,6 +4,7 @@ class Isync < Formula
   url "https://downloads.sourceforge.net/project/isync/isync/1.4.4/isync-1.4.4.tar.gz"
   sha256 "7c3273894f22e98330a330051e9d942fd9ffbc02b91952c2f1896a5c37e700ff"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://git.code.sf.net/p/isync/isync.git", branch: "master"
 
   bottle do
@@ -17,7 +18,7 @@ class Isync < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "berkeley-db"
+  depends_on "berkeley-db@5"
   depends_on "openssl@1.1"
 
   uses_from_macos "zlib"
