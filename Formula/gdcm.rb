ad4e6504d3f56866f5b94895c2e505c6a21d2975
Michael Cho <cho-m@tuta.io>
2022-08-09 09:05:17 -0700

    gdcm: drop `vtk@8.2` and migrate to `python@3.10`

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -4,6 +4,7 @@ class Gdcm < Formula
   url "https://github.com/malaterre/GDCM/archive/v3.0.14.tar.gz"
   sha256 "12582a87a1f043ce77005590ef1060e92ad36ec07ccf132da49c59f857d413ee"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url :stable
@@ -25,54 +26,49 @@ class Gdcm < Formula
   depends_on "swig" => :build
   depends_on "openjpeg"
   depends_on "openssl@1.1"
-  depends_on "python@3.9"
-  depends_on "vtk@8.2"
+  depends_on "python@3.10"
 
   uses_from_macos "expat"
   uses_from_macos "zlib"
 
   on_linux do
     depends_on "gcc"
+    depends_on "util-linux" # for libuuid
   end
 
   fails_with gcc: "5"
 
   def python3
-    which("python3.9")
+    which("python3.10")
   end
 
   def install
     ENV.cxx11
+    ENV.append "LDFLAGS", "-undefined dynamic_lookup" if OS.mac?
 
     python_include =
       Utils.safe_popen_read(python3, "-c", "from distutils import sysconfig;print(sysconfig.get_python_inc(True))")
            .chomp
-    python_executable = Utils.safe_popen_read(python3, "-c", "import sys;print(sys.executable)").chomp
 
-    args = %W[
-      -GNinja
-      -DGDCM_BUILD_APPLICATIONS=ON
-      -DGDCM_BUILD_SHARED_LIBS=ON
-      -DGDCM_BUILD_TESTING=OFF
-      -DGDCM_BUILD_EXAMPLES=OFF
-      -DGDCM_BUILD_DOCBOOK_MANPAGES=OFF
-      -DGDCM_USE_VTK=ON
-      -DGDCM_USE_SYSTEM_EXPAT=ON
-      -DGDCM_USE_SYSTEM_ZLIB=ON
-      -DGDCM_USE_SYSTEM_UUID=ON
-      -DGDCM_USE_SYSTEM_OPENJPEG=ON
-      -DGDCM_USE_SYSTEM_OPENSSL=ON
-      -DGDCM_WRAP_PYTHON=ON
-      -DPYTHON_EXECUTABLE=#{python_executable}
-      -DPYTHON_INCLUDE_DIR=#{python_include}
-      -DGDCM_INSTALL_PYTHONMODULE_DIR=#{prefix/Language::Python.site_packages(python3)}
-      -DCMAKE_INSTALL_RPATH=#{lib}
-      -DGDCM_NO_PYTHON_LIBS_LINKING=ON
-    ]
-
-    ENV.append "LDFLAGS", "-undefined dynamic_lookup" if OS.mac?
-
-    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
+                    "-GNinja",
+                    "-DGDCM_BUILD_APPLICATIONS=ON",
+                    "-DGDCM_BUILD_SHARED_LIBS=ON",
+                    "-DGDCM_BUILD_TESTING=OFF",
+                    "-DGDCM_BUILD_EXAMPLES=OFF",
+                    "-DGDCM_BUILD_DOCBOOK_MANPAGES=OFF",
+                    "-DGDCM_USE_VTK=OFF", # No VTK 9 support: https://sourceforge.net/p/gdcm/bugs/509/
+                    "-DGDCM_USE_SYSTEM_EXPAT=ON",
+                    "-DGDCM_USE_SYSTEM_ZLIB=ON",
+                    "-DGDCM_USE_SYSTEM_UUID=ON",
+                    "-DGDCM_USE_SYSTEM_OPENJPEG=ON",
+                    "-DGDCM_USE_SYSTEM_OPENSSL=ON",
+                    "-DGDCM_WRAP_PYTHON=ON",
+                    "-DPYTHON_EXECUTABLE=#{python3}",
+                    "-DPYTHON_INCLUDE_DIR=#{python_include}",
+                    "-DGDCM_INSTALL_PYTHONMODULE_DIR=#{prefix/Language::Python.site_packages(python3)}",
+                    "-DCMAKE_INSTALL_RPATH=#{lib}",
+                    "-DGDCM_NO_PYTHON_LIBS_LINKING=ON"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
