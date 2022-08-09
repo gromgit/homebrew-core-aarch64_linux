93750c1ea44f9c5724aa35dcb536c1e77f373d8b
Michael Cho <cho-m@tuta.io>
2022-08-09 09:14:22 -0700

    itk: migrate to `jpeg-turbo` and `vtk`
    Closes #107657.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/itk.rb
+++ Formula/itk.rb
@@ -4,7 +4,7 @@ class Itk < Formula
   url "https://github.com/InsightSoftwareConsortium/ITK/releases/download/v5.2.1/InsightToolkit-5.2.1.tar.gz"
   sha256 "192d41bcdd258273d88069094f98c61c38693553fd751b54f8cda308439555db"
   license "Apache-2.0"
-  revision 2
+  revision 3
   head "https://github.com/InsightSoftwareConsortium/ITK.git", branch: "master"
 
   livecheck do
@@ -22,13 +22,14 @@ class Itk < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "double-conversion"
   depends_on "fftw"
   depends_on "gdcm"
   depends_on "hdf5"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
-  depends_on "vtk@8.2" # needed for gdcm
+  depends_on "vtk"
 
   on_linux do
     depends_on "alsa-lib"
@@ -41,30 +42,32 @@ class Itk < Formula
   fails_with gcc: "5"
 
   def install
-    args = std_cmake_args + %W[
+    # Avoid CMake trying to find GoogleTest even though tests are disabled
+    (buildpath/"Modules/ThirdParty/GoogleTest").rmtree
+
+    args = %W[
       -DBUILD_SHARED_LIBS=ON
-      -DBUILD_TESTING=OFF
       -DCMAKE_INSTALL_RPATH:STRING=#{lib}
       -DCMAKE_INSTALL_NAME_DIR:STRING=#{lib}
-      -DITK_USE_64BITS_IDS=ON
-      -DITK_USE_STRICT_CONCEPT_CHECKING=ON
-      -DITK_USE_SYSTEM_ZLIB=ON
-      -DITK_USE_SYSTEM_EXPAT=ON
-      -DModule_SCIFIO=ON
       -DITKV3_COMPATIBILITY:BOOL=OFF
-      -DITK_USE_SYSTEM_FFTW=ON
+      -DITK_LEGACY_REMOVE=ON
+      -DITK_USE_64BITS_IDS=ON
       -DITK_USE_FFTWF=ON
       -DITK_USE_FFTWD=ON
+      -DITK_USE_SYSTEM_FFTW=ON
       -DITK_USE_SYSTEM_HDF5=ON
       -DITK_USE_SYSTEM_JPEG=ON
       -DITK_USE_SYSTEM_PNG=ON
       -DITK_USE_SYSTEM_TIFF=ON
       -DITK_USE_SYSTEM_GDCM=ON
-      -DITK_LEGACY_REMOVE=ON
+      -DITK_USE_SYSTEM_ZLIB=ON
+      -DITK_USE_SYSTEM_EXPAT=ON
+      -DITK_USE_SYSTEM_DOUBLECONVERSION=ON
+      -DITK_USE_SYSTEM_LIBRARIES=ON
       -DModule_ITKReview=ON
       -DModule_ITKVtkGlue=ON
+      -DModule_SCIFIO=ON
     ]
-
     args << "-DITK_USE_GPU=ON" if OS.mac?
 
     # Avoid references to the Homebrew shims directory
@@ -80,11 +83,9 @@ class Itk < Formula
               "\\\"#{ENV.cxx}\\\", \\\"The CXX compiler.\\\");")
     end
 
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
@@ -104,10 +105,10 @@ class Itk < Formula
     system ENV.cxx, "-std=c++11", "-isystem", "#{include}/ITK-#{v}", "-o", "test.cxx.o", "-c", "test.cxx"
     # Linking step
     system ENV.cxx, "-std=c++11", "test.cxx.o", "-o", "test",
-                    shared_library("#{lib}/libITKCommon-#{v}", 1),
-                    shared_library("#{lib}/libITKVNLInstantiation-#{v}", 1),
-                    shared_library("#{lib}/libitkvnl_algo-#{v}", 1),
-                    shared_library("#{lib}/libitkvnl-#{v}", 1)
+                    lib/shared_library("libITKCommon-#{v}", 1),
+                    lib/shared_library("libITKVNLInstantiation-#{v}", 1),
+                    lib/shared_library("libitkvnl_algo-#{v}", 1),
+                    lib/shared_library("libitkvnl-#{v}", 1)
     system "./test"
   end
 end
