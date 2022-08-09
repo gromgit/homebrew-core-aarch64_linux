c4bdee630908348f14f6b3795e946bd35b7dcca9
mhmdanas <triallax@tutanota.com>
2022-08-09 21:53:32 +0300

    bitcoin: use berkeley-db@5
    Closes #107667.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -4,7 +4,7 @@ class Bitcoin < Formula
   url "https://bitcoincore.org/bin/bitcoin-core-23.0/bitcoin-23.0.tar.gz"
   sha256 "26748bf49d6d6b4014d0fedccac46bf2bcca42e9d34b3acfd9e3467c415acc05"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/bitcoin/bitcoin.git", branch: "master"
 
   livecheck do
@@ -25,7 +25,7 @@ class Bitcoin < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "berkeley-db@4"
+  depends_on "berkeley-db@5"
   depends_on "boost"
   depends_on "libevent"
   depends_on macos: :catalina
