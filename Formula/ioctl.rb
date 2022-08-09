f304c73d2d8866aaab902503c19d1814ffbbcf2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-09 09:05:23 +0000

    ioctl: update 1.8.2 bottle.

--- Formula/ioctl.rb
+++ Formula/ioctl.rb
@@ -6,12 +6,12 @@ class Ioctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d37a5e5bcca09467bc7d7cefbb0336ca498d535c2cd36f0ec5a98883b79e133"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "17c7ddf1212bf27d47e36b641dd970286d10cdbaa6a2bab09738020b18b1c467"
-    sha256 cellar: :any_skip_relocation, monterey:       "11111a5b6e47da8150d9ce1cc704bff4a46456b9a8402767474778db6f0c1abc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc4dcd7ca5f42cbc28b27f3467a456a371716c5e18e9b55d67950252edb96a39"
-    sha256 cellar: :any_skip_relocation, catalina:       "bbec34fa65b70162bddcf72affe5e91958f1d21600d3ac70bf0b9a7ef4a766f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "206b6170ee8f718709257a17b00a2cbcab4e718d0adbd3069dda2d2d1230a60c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "238bbf68466c2a0ffb4057116658d2f664ca6bfdb75f99dfe841fe12ee82474b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8648755e9b80d98587fd01d86c0815b43bca7eb5ffc3517234d6820741f4e7ce"
+    sha256 cellar: :any_skip_relocation, monterey:       "233e7f64948cd1d8ed98995c295117a62389e1d3a66a1b13de347a4f086a3140"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ad1494fff0f4b040027c2b90cfb829de49499243075e1a1b492ef40a2e125887"
+    sha256 cellar: :any_skip_relocation, catalina:       "a703e25f13c3a223e51a65b41b77c947303aab5bd6f5fc3b3e45084438f8bf4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4a8173e54c8097a6d1f91a4c880ed63ab13a80ccfb1be05799c172013ecd389"
   end
 
   depends_on "go" => :build
