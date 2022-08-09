3340de0b7019f8ab2928136774038279710a04fa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-09 17:58:34 +0000

    lucky-commit: add 2.2.1 bottle.

--- Formula/lucky-commit.rb
+++ Formula/lucky-commit.rb
@@ -5,6 +5,15 @@ class LuckyCommit < Formula
   sha256 "3b35472c90d36f8276bddab3e75713e4dcd99c2a7abc3e412a9acd52e0fbcf81"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d09b8c8990932d835c997abaedd007136627d5f2a0d708620fd8fecd8702ec70"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06ba55d31250dfaf342d2f9a4e37158555844b1949e6fe2abad7220fb64be8ff"
+    sha256 cellar: :any_skip_relocation, monterey:       "c47445b77ad8843994438d5bee1ea191029159fca8c019619fa586eff2caddbb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a016f90b4168d4ac97bb303ae86fecbb27b873a7677134537bd44509c6f96573"
+    sha256 cellar: :any_skip_relocation, catalina:       "a54c93a1164be44359e8626cfdd8ce6ac07e7f0e78c8b53d4618593b20c5026b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4ead90e0bb96f4df966b4222db8e4a7543636a8f2ced16666f86c662f8f12de"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
