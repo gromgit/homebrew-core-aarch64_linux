1fa133a51fe3c648933aa08e81ce3d13689606a4
Mike McQuaid <mike@mikemcquaid.com>
2022-08-10 17:26:43 +0100

    wal2json: depend on postgresql@14.

--- Formula/wal2json.rb
+++ Formula/wal2json.rb
@@ -4,6 +4,7 @@ class Wal2json < Formula
   url "https://github.com/eulerto/wal2json/archive/wal2json_2_4.tar.gz"
   sha256 "87e627cac2d86f7203b1580a78b51e1da8aa61bb0af9ebfa14435370f3878237"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url :stable
@@ -21,11 +22,33 @@ class Wal2json < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "e390370160cf6213efc8b3436d544a2c48d82c47404a243ed7a384940ce53ea6"
   end
 
-  depends_on "postgresql"
+  depends_on "postgresql@14"
+
+  def postgresql
+    Formula["postgresql@14"]
+  end
 
   def install
+    ENV["PG_CONFIG"] = postgresql.opt_bin/"pg_config"
+
     mkdir "stage"
     system "make", "install", "USE_PGXS=1", "DESTDIR=#{buildpath}/stage"
-    lib.install Dir["stage/#{HOMEBREW_PREFIX}/lib/*"]
+
+    stage_path = File.join("stage", HOMEBREW_PREFIX)
+    lib.install (buildpath/stage_path/"lib").children
+  end
+
+  test do
+    pg_ctl = postgresql.opt_bin/"pg_ctl"
+    port = free_port
+
+    system pg_ctl, "initdb", "-D", testpath/"test"
+    (testpath/"test/postgresql.conf").write <<~EOS, mode: "a+"
+
+      shared_preload_libraries = 'wal2json'
+      port = #{port}
+    EOS
+    system pg_ctl, "start", "-D", testpath/"test", "-l", testpath/"log"
+    system pg_ctl, "stop", "-D", testpath/"test"
   end
 end
