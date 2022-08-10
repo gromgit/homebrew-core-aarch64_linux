dea0a43e1f19302da7ceb687085c4bec3900893d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-10 09:06:50 +0000

    terracognita: update 0.8.1 bottle.

--- Formula/terracognita.rb
+++ Formula/terracognita.rb
@@ -7,12 +7,12 @@ class Terracognita < Formula
   head "https://github.com/cycloidio/terracognita.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "779356406af8029270120a4431f8becc18ea033f12feead3a18537155b78b2eb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "190d7f3e4c0a501826773434b18ff360390668b971b0a306570e3497951237be"
-    sha256 cellar: :any_skip_relocation, monterey:       "5bee638b0b203afa91aefafff36c0660374442426433f843e1beec24dd7a767f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "efe1ab20d9ffb256a0185056df055d4aa7ea147942f85715b3f484070f4f4575"
-    sha256 cellar: :any_skip_relocation, catalina:       "6012ae24cf3906b66f47fc9861967dd366f91cd1f0cd9221dd4aa97548de88cc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eadc6248219a335076c8b317d98a1ad88208b7b5fd9390441c4ca85c238046ce"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b53dd1ceec5ddfff075c398cd24ca74f21b4138db629bc7f65978862369bcd34"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7ecd47c9c3e4021264cdf69f8e70dc94a149eb4076d7cf8e97f78fefa5ca3cc"
+    sha256 cellar: :any_skip_relocation, monterey:       "8345abff73f0893b1ac9328f0cfdf4bbb926b79bd2101fe0b5566d3032dc49cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7460a4ddf60cec0fe51e86b98dc39abab4ba5b50ad6a5c4c987fc9dd3765a014"
+    sha256 cellar: :any_skip_relocation, catalina:       "b86f9559051ced717bef608d9997f073d1b4af491fdafa736e679b7bbc6c0fd6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cca175e1cbaf3603eb6a2fcc5fe52369a0b644a586028aaae3505950fe6569e1"
   end
 
   depends_on "go" => :build
