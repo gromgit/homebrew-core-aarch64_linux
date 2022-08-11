2d6e9f704593407be515f1d28ee32fba266d82d5
Max Eisner <4730112+max-ae@users.noreply.github.com>
2022-08-11 17:54:33 +0200

    rbw: rebuild
    Formulaw: use generate_completions DSL

--- Formula/rbw.rb
+++ Formula/rbw.rb
@@ -21,14 +21,7 @@ class Rbw < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    bash_output = Utils.safe_popen_read(bin/"rbw", "gen-completions", "bash")
-    (bash_completion/"rbw").write bash_output
-
-    zsh_output = Utils.safe_popen_read(bin/"rbw", "gen-completions", "zsh")
-    (zsh_completion/"_rbw").write zsh_output
-
-    fish_output = Utils.safe_popen_read(bin/"rbw", "gen-completions", "fish")
-    (fish_completion/"rbw.fish").write fish_output
+    generate_completions_from_executable(bin/"rbw", "gen-completions")
   end
 
   test do
