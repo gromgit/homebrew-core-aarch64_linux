d7f3e336455881950cb0c181ee01451bace09816
mhmdanas <triallax@tutanota.com>
2022-08-12 02:02:17 +0300

    xorgproto 2022.2
    For whatever reason, there isn't a bz2 tarball for this release.
    
    Closes #107854.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xorgproto.rb
+++ Formula/xorgproto.rb
@@ -1,8 +1,8 @@
 class Xorgproto < Formula
   desc "X.Org: Protocol Headers"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2022.1.tar.bz2"
-  sha256 "1d2dcc66963f234d2c1e1f8d98a0d3e8725149cdac0a263df4097593c48bc2a6"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2022.2.tar.gz"
+  sha256 "da351a403d07a7006d7bdc8dcfc14ddc1b588b38fb81adab9989a8eef605757b"
   license "MIT"
 
   livecheck do
