6964a087067d323f14e9ebc188be8d72be46e275
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-11 08:05:40 +0000

    firebase-cli: update 11.6.0 bottle.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -9,12 +9,12 @@ class FirebaseCli < Formula
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
   bottle do
-    sha256                               arm64_monterey: "fc654dac079a82e229a185cda0c88668f2e750a9461d360f8eaac4fe26eaf5bb"
-    sha256                               arm64_big_sur:  "e8914bc41d6b62158296ac6a79aba3afdf40bdc24904f6c2f2f2eae110434e41"
-    sha256 cellar: :any_skip_relocation, monterey:       "b02163d6bc6b77842873a3996838a6d0be4539849a8015cd8428c160f6a0738b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b02163d6bc6b77842873a3996838a6d0be4539849a8015cd8428c160f6a0738b"
-    sha256 cellar: :any_skip_relocation, catalina:       "b02163d6bc6b77842873a3996838a6d0be4539849a8015cd8428c160f6a0738b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37a6cdb9b2d48fb785a13f8f73bcc05859593576db20ea1a699250973a736048"
+    sha256                               arm64_monterey: "e45574334fb3b8654d22587dc46dfc00f2d2df37b54d792c4cbfb7dc1fe0d678"
+    sha256                               arm64_big_sur:  "cc10edeefba296f03070900a8b7b1152917f24eadb23afa157a0665cd95e5e30"
+    sha256 cellar: :any_skip_relocation, monterey:       "7e8e4d2fac3b63f1edd6ae0a08d7f0d3eefe622936038a8f4fcc9aeefd4f492f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7e8e4d2fac3b63f1edd6ae0a08d7f0d3eefe622936038a8f4fcc9aeefd4f492f"
+    sha256 cellar: :any_skip_relocation, catalina:       "7e8e4d2fac3b63f1edd6ae0a08d7f0d3eefe622936038a8f4fcc9aeefd4f492f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f7445b1360a375d6594f9cbb8d6a88d54f5fbcd96a76b31a45eb0b2b5634a4e"
   end
 
   depends_on "node"
