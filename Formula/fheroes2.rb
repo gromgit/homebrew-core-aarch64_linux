4f1b5eef121dc537f06a939d6301b97fb5248491
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-11 00:17:07 +0000

    fheroes2: update 0.9.18 bottle.

--- Formula/fheroes2.rb
+++ Formula/fheroes2.rb
@@ -12,12 +12,12 @@ class Fheroes2 < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "47f4a8336778e11c7f6eb11ec2251a661d739d4d3d4ca00153942b7fd82bbe3b"
-    sha256 arm64_big_sur:  "62d964a871ed55bcf12f933754bab754fe39d3af209d3740fc6a3cc40ec5cb07"
-    sha256 monterey:       "a7f757ac4c2468bbab0e87359fe0116e9b15a928ec1af8117c6931a6d56af585"
-    sha256 big_sur:        "5a56a45450fa2ead28aeb3e7e3d96cfb8007e326cbd25597079265a56453daa3"
-    sha256 catalina:       "3585707b2f70fc524f4714362928b4ff8d90d6c9f3038faa12cfabef1fc109ef"
-    sha256 x86_64_linux:   "7b72498452c57834bdc3afe97b39f6b00b35da4d720a4faabd8afd25336f20f9"
+    sha256 arm64_monterey: "69c9a533f239193b06146dfe4b0b008626a6388bff4b31347b2ebc31360a7962"
+    sha256 arm64_big_sur:  "f0f7fcb57b6f09209431a7dfc8ee92c184a39a22e9e8f99a7d4ce92222b725b0"
+    sha256 monterey:       "9a8ceb085dfdcf9b469dfd04df2c8e6ea7ed6f2083606495bc20dc898cf39c54"
+    sha256 big_sur:        "85edf07788903707cb4fe5f8573c02018342c799371b4d5469b0f314c35d1578"
+    sha256 catalina:       "fdc9cab4c43d028d34e1339812c9803d4db3629c1950460e79215c97d6fbe568"
+    sha256 x86_64_linux:   "c28595b79336768b8bbc419b4161fb732263ae416f4c827c4f31797f36db8917"
   end
 
   depends_on "cmake" => :build
