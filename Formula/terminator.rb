21d2d4bb26677174281575bedff819176634cc53
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-11 06:08:31 +0000

    terminator: update 2.1.1_1 bottle.

--- Formula/terminator.rb
+++ Formula/terminator.rb
@@ -9,13 +9,12 @@ class Terminator < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2c180712581d30b046b14a4ee5279f2cea55b3bff7a62b2737b21693e993996c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c177f5b4ceed9612e512051ae5ce29a4d04a9d26e942fbe485e4f5053f437e1"
-    sha256 cellar: :any_skip_relocation, monterey:       "cf82ea3cadca21e1ae94e3b2fdb8e3d814f04d33623203797ec08fda18071ba3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8039dfc9e641c81615932afdb6bec92a512e123079974e4d87acbf6bd904a5fb"
-    sha256 cellar: :any_skip_relocation, catalina:       "8c257067fdc58cceaa2f4f68fd569aa408db036faff673fe27f3950224745987"
-    sha256 cellar: :any_skip_relocation, mojave:         "cbaae7d439b7f9fb6d2149c3fba76eefefb2120aac26504c60ebaab4d842846b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f1dc76a8265f1bdfc5c593661afe56dbd247c3ee8ece4963c22894078b80adc9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "098c77a630f6f02fa66e6eb7a592f3229bd70cb91584ac2de78c75944bd8fa19"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0c2883a811abc01c06c9386915c6fd3a74f1e0ea62319bc7314d33651165d3f3"
+    sha256 cellar: :any_skip_relocation, monterey:       "1c85992e04ffab1a37b0fe92dfbef27e89c2eceb83fda20eaf5c4c57c0147289"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fe4124f38acbd8f8a8632e6b56b6f6556a8317343ac3dcc5c41ac45507e3570a"
+    sha256 cellar: :any_skip_relocation, catalina:       "6ea19d85c2f874bd47e34fc6b25ecd72e9c64a73c18390a1088ce75bf3633144"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9b2da44020cf6178179d8fe022202e14cd95b76e4ac0c24d87d49cce361b0ce"
   end
 
   depends_on "pygobject3"
