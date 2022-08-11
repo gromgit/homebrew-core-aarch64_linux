e06131943885f31e6af941cb1a52a9f66abaca51
Alexander Bayandin <alexander@bayandin.dev>
2022-08-11 09:23:44 +0100

    check_postgres: revision bump for libpq

--- Formula/check_postgres.rb
+++ Formula/check_postgres.rb
@@ -4,7 +4,7 @@ class CheckPostgres < Formula
   url "https://bucardo.org/downloads/check_postgres-2.25.0.tar.gz"
   sha256 "11b52f86c44d6cc26e9a4129e67c2589071dbe1b8ac1f8895761517491c6e44b"
   license "BSD-2-Clause"
-  revision 1
+  revision 2
   head "https://github.com/bucardo/check_postgres.git", branch: "master"
 
   livecheck do
@@ -22,13 +22,19 @@ class CheckPostgres < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8213c30652e0d80ba84867c9b8db6924801a43c7b0df1273c20cafbd6ee4160"
   end
 
-  depends_on "postgresql"
+  depends_on "libpq"
+  uses_from_macos "perl"
 
   def install
     system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}", "INSTALLSITEMAN1DIR=#{man1}"
     system "make", "install"
-    mv bin/"check_postgres.pl", bin/"check_postgres"
-    inreplace [bin/"check_postgres", man1/"check_postgres.1p"], "check_postgres.pl", "check_postgres"
+
+    mkdir_p libexec/"bin"
+    mv bin/"check_postgres.pl", libexec/"bin/check_postgres.pl"
+    inreplace [libexec/"bin/check_postgres.pl", man1/"check_postgres.1p"], "check_postgres.pl", "check_postgres"
+
+    (bin/"check_postgres").write_env_script libexec/"bin/check_postgres.pl", PATH: "#{Formula["libpq"].opt_bin}:$PATH"
+
     rm_rf prefix/"Library"
     rm_rf prefix/"lib"
   end
