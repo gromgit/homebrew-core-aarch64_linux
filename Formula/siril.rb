9644c0717540fd4c2f26c0a391fe31279d947fe7
Michael Cho <cho-m@tuta.io>
2022-08-10 18:27:41 -0700

    siril: revision bump (opencv 4.6.0)

--- Formula/siril.rb
+++ Formula/siril.rb
@@ -4,7 +4,7 @@ class Siril < Formula
   url "https://free-astro.org/download/siril-1.0.3.tar.bz2"
   sha256 "2fefa7b7e1378f4ba277818c92ec7c4fca1fdcaa6df95bb65aed0163750be2c6"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
   head "https://gitlab.com/free-astro/siril.git", branch: "master"
 
   bottle do
@@ -66,6 +66,6 @@ class Siril < Formula
   end
 
   test do
-    system "#{bin}/siril", "-v"
+    system bin/"siril", "-v"
   end
 end
