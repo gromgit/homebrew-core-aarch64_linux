065a1f5567e350d47c6421fecf12939da91ad2a2
Porkepix <porkepix@gmail.com>
2022-08-13 00:24:24 +0200

    re-flex 3.2.9
    Closes #107936.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "Regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v3.2.8.tar.gz"
-  sha256 "9dc3729080377f5a67b44630010ae76f45324917a8820cb6a240702a4d6fe7c8"
+  url "https://github.com/Genivia/RE-flex/archive/v3.2.9.tar.gz"
+  sha256 "b24eee8d1c8a1c18be260aa170cb8076a0a5bb1fc4cb3f5e21b05ae80ba7f377"
   license "BSD-3-Clause"
 
   bottle do
