3171bec9157ff1ab0d33562bbde627f609f8394c
mhmdanas <triallax@tutanota.com>
2022-08-12 21:31:57 +0300

    purescript-language-server 0.16.6 (new formula)
    Closes #107912.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/purescript-language-server.rb
@@ -0,0 +1,38 @@
+require "language/node"
+
+class PurescriptLanguageServer < Formula
+  desc "Language Server Protocol server for PureScript"
+  homepage "https://github.com/nwolverson/purescript-language-server"
+  url "https://registry.npmjs.org/purescript-language-server/-/purescript-language-server-0.16.6.tgz"
+  sha256 "b7a705c51c7bad8cc1c2e8d9e827a57140e90c2e290f1381e64fb16418453e99"
+  license "MIT"
+
+  depends_on "node"
+  depends_on "purescript"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/purescript-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
