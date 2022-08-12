ee747eef4946e53c1b8c7dc0273d167d3a951b9c
Rui Chen <rui@chenrui.dev>
2022-08-12 18:48:26 -0400

    postgresql@12 12.12
    Closes #107942.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/postgresql@12.rb
+++ Formula/postgresql@12.rb
@@ -1,8 +1,8 @@
 class PostgresqlAT12 < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v12.11/postgresql-12.11.tar.bz2"
-  sha256 "1026248a5fd2beeaf43e4c7236ac817e56d58b681a335856465dfbc75b3e8302"
+  url "https://ftp.postgresql.org/pub/source/v12.12/postgresql-12.12.tar.bz2"
+  sha256 "34b3f1c69408e22068c0c71b1827691f1c89153b0ad576c1a44f8920a858039c"
   license "PostgreSQL"
 
   livecheck do
