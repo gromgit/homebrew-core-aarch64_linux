ddd730fec2bec8645c7c22fd3eb3d5477fdeef18
owine <owine@users.noreply.github.com>
2022-08-12 12:59:20 -0500

    xz 5.2.6
    * xz 5.2.6
    * xz: Remove patch
    * xz: fix mirror
    
    Closes #107909.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -3,19 +3,18 @@
 class Xz < Formula
   desc "General-purpose data compression with high compression ratio"
   homepage "https://tukaani.org/xz/"
-  url "https://downloads.sourceforge.net/project/lzmautils/xz-5.2.5.tar.gz"
-  mirror "https://tukaani.org/xz/xz-5.2.5.tar.gz"
-  # This mirror needs to be manually created at `archive.org`.
-  mirror "https://archive.org/download/xz-5.2.5.tar.gz/xz-5.2.5.tar.gz"
-  mirror "http://archive.org/download/xz-5.2.5.tar.gz/xz-5.2.5.tar.gz"
-  sha256 "f6f4910fd033078738bd82bfba4f49219d03b17eb0794eb91efbae419f4aba10"
+  # The archive.org mirror below needs to be manually created at `archive.org`.
+  url "https://downloads.sourceforge.net/project/lzmautils/xz-5.2.6.tar.gz"
+  mirror "https://tukaani.org/xz/xz-5.2.6.tar.gz"
+  mirror "https://archive.org/download/xz-5.2.6/xz-5.2.6.tar.gz"
+  mirror "http://archive.org/download/xz-5.2.6/xz-5.2.6.tar.gz"
+  sha256 "a2105abee17bcd2ebd15ced31b4f5eda6e17efd6b10f921a01cda4a44c91b3a0"
   license all_of: [
     :public_domain,
     "LGPL-2.1-or-later",
     "GPL-2.0-or-later",
     "GPL-3.0-or-later",
   ]
-  revision 1
 
   bottle do
     sha256 cellar: :any, arm64_monterey: "b2f0ff235854d96ba7e8ffce77bb21e0d1d179aca9ffdb8f7233b2d57e96b8dd"
@@ -24,13 +23,6 @@ class Xz < Formula
     sha256 cellar: :any, big_sur:        "a61f86356450826377490cf6b22e867a423ba88dd3a1dc91792a7cbf57fcac84"
     sha256 cellar: :any, catalina:       "efcb62b10858d4f3ca16e9409eff9f93ac0ff7adee546d21f4638dae20d89300"
     sha256               x86_64_linux:   "5308bba4329d4ca980f8a2a8cb6b26e746f498e5dc76cc32b02ff97a7a61a49c"
-  end
-
-  # Fix arbitrary-file-write vulnerability in `xzgrep`.
-  # https://seclists.org/oss-sec/2022/q2/18
-  patch do
-    url "https://tukaani.org/xz/xzgrep-ZDI-CAN-16587.patch"
-    sha256 "98c6cb1042284fe704ec30083f3fc87364ce9ed2ea51f62bbb0ee9d3448717ec"
   end
 
   def install
