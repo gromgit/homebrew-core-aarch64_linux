2d8b94978ca65e0959ddea6a25b5cc17535550fb
Alexander Bayandin <alexander@bayandin.dev>
2022-08-12 08:42:19 +0000

    psql2csv: update 0.12_1 bottle.

--- Formula/psql2csv.rb
+++ Formula/psql2csv.rb
@@ -7,7 +7,7 @@ class Psql2csv < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6c33e20a01b09f6161318136ae5e4529cea292fd1783ce8b81bdf13a58c5453b"
+    sha256 cellar: :any_skip_relocation, all: "7b159e3b53c7d0b6bd568e504e918a1c96cff36007bcd009550c21e18c3cd269"
   end
 
   depends_on "libpq"
