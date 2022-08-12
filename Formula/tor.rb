732557b990d3074089c217940ee25f599ff00683
fanquake <fanquake@gmail.com>
2022-08-12 17:33:46 +0100

    tor 0.4.7.10
    Closes #107907.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -1,9 +1,9 @@
 class Tor < Formula
   desc "Anonymizing overlay network for TCP"
   homepage "https://www.torproject.org/"
-  url "https://www.torproject.org/dist/tor-0.4.7.9.tar.gz"
-  mirror "https://www.torservers.net/mirrors/torproject.org/dist/tor-0.4.7.9.tar.gz"
-  sha256 "d39d38598208f4d6201d7edc6ad573b3a898a932a5c68d3074016a9525519b22"
+  url "https://www.torproject.org/dist/tor-0.4.7.10.tar.gz"
+  mirror "https://www.torservers.net/mirrors/torproject.org/dist/tor-0.4.7.10.tar.gz"
+  sha256 "647e56dfa59ea36dab052027fcfc7663905c826c03509363c456900ecd435a5b"
   # Complete list of licenses:
   # https://gitweb.torproject.org/tor.git/plain/LICENSE
   license all_of: [
