47fd7dbd098cac4324d49616e4b94fb9f150d328
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-12 13:31:24 +0000

    libvatek: add 3.04 bottle.

--- Formula/libvatek.rb
+++ Formula/libvatek.rb
@@ -6,6 +6,15 @@ class Libvatek < Formula
   license "BSD-2-Clause"
   head "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "220f72cc369ab7e69b3547f6a71e4c078dfeeec1485e6a331eb4755743c529bb"
+    sha256 cellar: :any,                 arm64_big_sur:  "4a7a98f3ab3ab91232086cfc10a448f8ef1df4d94fb664550ba970c7fdb933a5"
+    sha256 cellar: :any,                 monterey:       "e8f1907b5f376fb41fc5a72f4b1965252cbdcb0c871c2469c8c8bfabad0f438f"
+    sha256 cellar: :any,                 big_sur:        "72cbbe10f0973578f4137045f1bd9278a18c4f207f7ea396d46d4e4c18241465"
+    sha256 cellar: :any,                 catalina:       "8fc16b833cf8a37dcd54289bb3927fd9dd7e672e8ebec4029f8de182e161f25b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "daab2851756390d5d1a854c2f03fde280e296ef69ae631198db4c13b327b753f"
+  end
+
   depends_on "cmake" => :build
   depends_on "libusb"
 
