871dfb353c29d2b6b5d9d5ea5ce3e0c30e5069b6
Alexander Bayandin <alexander@bayandin.dev>
2022-08-12 08:42:23 +0000

    postgraphile: update 4.12.11_1 bottle.

--- Formula/postgraphile.rb
+++ Formula/postgraphile.rb
@@ -10,12 +10,12 @@ class Postgraphile < Formula
   head "https://github.com/graphile/postgraphile.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "079f749387e1b4e48a548f89be058aeb148633e8dfa4b52033a2240c63109920"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "079f749387e1b4e48a548f89be058aeb148633e8dfa4b52033a2240c63109920"
-    sha256 cellar: :any_skip_relocation, monterey:       "097a752102d1126b9c5a4efbe290770414fea245463fef2b7d5e56988456d8d4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "097a752102d1126b9c5a4efbe290770414fea245463fef2b7d5e56988456d8d4"
-    sha256 cellar: :any_skip_relocation, catalina:       "097a752102d1126b9c5a4efbe290770414fea245463fef2b7d5e56988456d8d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "079f749387e1b4e48a548f89be058aeb148633e8dfa4b52033a2240c63109920"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb2b7fc86011d7f6262bb40919faff5180008ffd80b908f22c969bd828df7849"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fb2b7fc86011d7f6262bb40919faff5180008ffd80b908f22c969bd828df7849"
+    sha256 cellar: :any_skip_relocation, monterey:       "13ca030630db036b5e9b28231f9c542800f78951c7619f9c5bd6e83c5060a546"
+    sha256 cellar: :any_skip_relocation, big_sur:        "13ca030630db036b5e9b28231f9c542800f78951c7619f9c5bd6e83c5060a546"
+    sha256 cellar: :any_skip_relocation, catalina:       "13ca030630db036b5e9b28231f9c542800f78951c7619f9c5bd6e83c5060a546"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb2b7fc86011d7f6262bb40919faff5180008ffd80b908f22c969bd828df7849"
   end
 
   depends_on "postgresql" => :test
