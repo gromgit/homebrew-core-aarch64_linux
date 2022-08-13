670f6c406fcabb6373d0d543eb73a0c625cf29f2
Porkepix <porkepix@gmail.com>
2022-08-13 12:05:20 +0200

    treefrog 2.4.0
    * treefrog 2.4.0
    * treefrog: add workaround for broken Qt on Linux
    * treefrog: try a different fix
    * treefrog: skip `qmake` test on Linux
    
    Closes #107975.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/treefrog.rb
+++ Formula/treefrog.rb
@@ -1,8 +1,8 @@
 class Treefrog < Formula
   desc "High-speed C++ MVC Framework for Web Application"
   homepage "https://www.treefrogframework.org/"
-  url "https://github.com/treefrogframework/treefrog-framework/archive/v2.3.1.tar.gz"
-  sha256 "1877efe236d8407ce401c6e21670a0b0714a6dd07f786714c8ea885d8c6393de"
+  url "https://github.com/treefrogframework/treefrog-framework/archive/v2.4.0.tar.gz"
+  sha256 "d7fc8459013097c0798f2b57ac1ff684077c8417c48fb536913edd94dda31738"
   license "BSD-3-Clause"
   head "https://github.com/treefrogframework/treefrog-framework.git", branch: "master"
 
@@ -57,6 +57,11 @@ class Treefrog < Formula
     assert_predicate testpath/"hello", :exist?
     cd "hello" do
       assert_predicate Pathname.pwd/"hello.pro", :exist?
+      # FIXME: `qmake` has a broken mkspecs file on Linux.
+      # Remove when the following PR is merged:
+      # https://github.com/Homebrew/homebrew-core/pull/107400
+      return if OS.linux?
+
       system Formula["qt"].opt_bin/"qmake"
       assert_predicate Pathname.pwd/"Makefile", :exist?
       system "make"
