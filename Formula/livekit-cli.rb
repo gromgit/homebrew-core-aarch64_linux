dee1f8fccd3d9c069ddbc3ad70432a638bba2551
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-13 01:33:52 +0000

    livekit-cli: update 1.1.0 bottle.

--- Formula/livekit-cli.rb
+++ Formula/livekit-cli.rb
@@ -7,12 +7,12 @@ class LivekitCli < Formula
   head "https://github.com/livekit/livekit-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7fd84e0f34d09993d558f57ead4041e750e9baa7eee746b28347c72a3fc735e8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "155a1af90748ef7cd209ac8c4f7d9317ade8975fb488310068a3a58e3b1b86fc"
-    sha256 cellar: :any_skip_relocation, monterey:       "1e76abaeb9aa24b7c8fbea7aa50f2c5ecd3948e847155468d3665106b87081d4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f1c9f1643b706244dbb926a845d00119587d5c1a2d3490eeb1d657305462515d"
-    sha256 cellar: :any_skip_relocation, catalina:       "afaa85702a6cf2060bf5bdf8a04576e52fea1fec0103fc42a6072d55af22a4fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "49f9949df3683f0e27e4b2a6890253e177da7ffa7b27eeb2b50737909b86e473"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e67d0b834597fd15fca890d5d9e560ff899c9d916c393f28c35f6bae9e1d2ef8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b842bf91c871e49a1106e87ebd654b163dbb72ddec71de15b910238f694057a1"
+    sha256 cellar: :any_skip_relocation, monterey:       "d445c780a053ef3870ca5f6c2728474d9e637f0fb7ed9fbe124e90c5de0c2797"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0929026b63aba3c081a187b071ac078f172c41e68dcbbfcd43b88f9f2c15dee2"
+    sha256 cellar: :any_skip_relocation, catalina:       "d9bfcbcc8076639422d65179b292304976061528b0c31cc9a94ff6d2ba03c8d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "316ccdb07db7d7587fd7f1b06857a8d65b15d69df20efe1a525cb7b5ee683646"
   end
 
   depends_on "go" => :build
