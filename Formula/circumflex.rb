a1de2b6d34d5db9f32d71609df86510143fcca80
Rui Chen <rui@chenrui.dev>
2022-08-13 13:59:15 -0400

    circumflex 2.3
    Closes #108001.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/circumflex.rb
+++ Formula/circumflex.rb
@@ -1,10 +1,9 @@
 class Circumflex < Formula
   desc "Hacker News in your terminal"
   homepage "https://github.com/bensadeh/circumflex"
-  url "https://github.com/bensadeh/circumflex/archive/refs/tags/2.2.tar.gz"
-  sha256 "6a2467bf6bad00fb3fe3a7b9bdb4e6ea6d8a721b1c9905e6161324cfb3f34c01"
+  url "https://github.com/bensadeh/circumflex/archive/refs/tags/2.3.tar.gz"
+  sha256 "ba898331d76e266ae3af4e5eab5c6af30ef004c133104fec3e0bbb9497f5c70a"
   license "AGPL-3.0-only"
-  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "18b4dcd42ef0bc7989c1af5911222c6d4fa384cf13ee61fe921fe20194f2bb5d"
