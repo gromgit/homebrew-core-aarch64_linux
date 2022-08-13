f73fad9c7739bbf38ac3326611a9840b69461c0b
Porkepix <porkepix@gmail.com>
2022-08-13 16:25:35 +0200

    swiftformat 0.49.15
    Closes #107983.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,8 +1,8 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.14.tar.gz"
-  sha256 "3a890e8582d78088db43cfe6bc84a98fd3176b4dacdd3ee10d04b044e96d91c3"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.15.tar.gz"
+  sha256 "716bd16fc1a1472ef2993e2ffbcc69b480dbebe7e305f049923b97f6acd7824c"
   license "MIT"
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
 
