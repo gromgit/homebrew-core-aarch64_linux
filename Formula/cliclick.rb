8c26b8b847ab4e16e5dc6b4113fae45c2681064d
Porkepix <porkepix@gmail.com>
2022-08-14 15:11:06 +0200

    cliclick 5.1
    Closes #108034.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cliclick.rb
+++ Formula/cliclick.rb
@@ -1,8 +1,8 @@
 class Cliclick < Formula
   desc "Tool for emulating mouse and keyboard events"
   homepage "https://www.bluem.net/jump/cliclick/"
-  url "https://github.com/BlueM/cliclick/archive/5.0.1.tar.gz"
-  sha256 "798fb8b26f6a42b5002ca58e018b91f7677162c4f035b38aee8d905829db64a7"
+  url "https://github.com/BlueM/cliclick/archive/5.1.tar.gz"
+  sha256 "58bb36bca90fdb91b620290ba9cc0f885b80716cb7309b9ff4ad18edc96ce639"
   license "BSD-3-Clause"
   head "https://github.com/BlueM/cliclick.git", branch: "master"
 
