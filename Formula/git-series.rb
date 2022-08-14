c148570351560283ef95cf8c63f30c5486d8a8dc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-14 15:07:47 +0000

    git-series: update 0.9.1_7 bottle.

--- Formula/git-series.rb
+++ Formula/git-series.rb
@@ -7,12 +7,12 @@ class GitSeries < Formula
   revision 7
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8e62e399cbba54341ee1bc7102862926e8c12d08b852d24fab6b8e45e253cdcb"
-    sha256 cellar: :any,                 arm64_big_sur:  "e394b22f7dad47f5fad8551ac8ec850581dfb30014a476a6adbdadb170d89d98"
-    sha256 cellar: :any,                 monterey:       "85dc9d45f805faf62c56845d34dbbcba950ff1a894399b8ada665349c58141db"
-    sha256 cellar: :any,                 big_sur:        "e5e96220146d2666fffa5bfbf61c56a03213227583e09f7f82780a5113233f1e"
-    sha256 cellar: :any,                 catalina:       "628b077ef0b4a60f22637802a65369eeea31d778af2ce926f548f8b4e1aefcd3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdd1be3900aa1bf6e374228cb0f8bf0bd37e34592bf077fe412109e28c9ea8d0"
+    sha256 cellar: :any,                 arm64_monterey: "776b0da4754574da103f9e40a2ecfd7c681c2f65f975982e5a205ad0a1934321"
+    sha256 cellar: :any,                 arm64_big_sur:  "bf45b2ebdb40c6c2e38cb2b3ab15b5a17ac250c6f0a6ad6930b8a8a4561716f3"
+    sha256 cellar: :any,                 monterey:       "19b97cd74d2bf96feb0c7852391aca459f586c7775e3255aba7573ecde2bb423"
+    sha256 cellar: :any,                 big_sur:        "fedb5ea7626a89ace95948ed1b0f12da3ff070cbf1850807093292df777513e2"
+    sha256 cellar: :any,                 catalina:       "e169a38cff276920853242b4ce1b31ee8493b9abb6a0a34e79a153c64c95cb5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e584bfcb9fdfbd4385b6865faf9123342f9fcceef3bb14ad9ee213243b69de1"
   end
 
   depends_on "cmake" => :build
