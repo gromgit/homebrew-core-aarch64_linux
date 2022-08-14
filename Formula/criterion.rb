246a40b6c107e4a47664478b23bd906cb45443e4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-14 15:07:44 +0000

    criterion: update 2.4.1_1 bottle.

--- Formula/criterion.rb
+++ Formula/criterion.rb
@@ -8,12 +8,12 @@ class Criterion < Formula
   head "https://github.com/Snaipe/Criterion.git", branch: "bleeding"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "5a284e7291b02f9b99c157baf3023e3a6bbc4ba904999b8b094f6f19db625f71"
-    sha256 cellar: :any, arm64_big_sur:  "756811bab129f1d12072e184806f3d0c2d340ea88eeba2dba470d0e46a0c5cc0"
-    sha256 cellar: :any, monterey:       "7f58f9eaeb7370f5ee50b871afa82dd6da248822b09495e602e551f7345cf7e2"
-    sha256 cellar: :any, big_sur:        "c29c0ee229ff746bbe8b1ff0040376fce39d8ee0a359566ac2304d9b39759b3f"
-    sha256 cellar: :any, catalina:       "2eaaa7be419c50953093f6ad8a9ee33eb22e820f245b1804e3c163a53420aaac"
-    sha256               x86_64_linux:   "fb380b5c7cdb5d367b46807a8ca453a468ed262d1f054fb6824c45c9e8c5d8d5"
+    sha256 cellar: :any, arm64_monterey: "936db451f3b333687d75ee02f987793b5a2a6e799bdf19d4c402f4942a6466bb"
+    sha256 cellar: :any, arm64_big_sur:  "893ed763895b094bfdcfaae2891fcb9c07c308d1d207da5aa988340a6d41f56b"
+    sha256 cellar: :any, monterey:       "ccea6bdbe908966b8152948d39672bbe475a85dfa2d34e702a02ab17df236358"
+    sha256 cellar: :any, big_sur:        "d6f833935149e7b6a85b2735db748251c076cdc3e4fb4ad9268ca26df0392d51"
+    sha256 cellar: :any, catalina:       "5abf4ef75d576ccdfdd7d482cd74e9a97b82f86aef95c8aa731722da87bc93d0"
+    sha256               x86_64_linux:   "6953412c2e5eb4676c7c3f8a46faf5329455f3416a076a23a71d4fdc163a72cc"
   end
 
   depends_on "cmake" => :build
