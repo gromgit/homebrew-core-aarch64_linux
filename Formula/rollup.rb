64c7ccde50cbb2624b3c4d0b7c9531acbd353c52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-14 14:04:28 +0000

    rollup: update 2.78.0 bottle.

--- Formula/rollup.rb
+++ Formula/rollup.rb
@@ -8,12 +8,12 @@ class Rollup < Formula
   license all_of: ["ISC", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "726c8f78a6c0e0119e9a9656741d6d6dad3b15b35f70ce45c1a9d06fbceea492"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "726c8f78a6c0e0119e9a9656741d6d6dad3b15b35f70ce45c1a9d06fbceea492"
-    sha256 cellar: :any_skip_relocation, monterey:       "f9eedf52c64b2234500f0135d4db517b48ed6ce37e69c9c3b3473f33e4782a81"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f9eedf52c64b2234500f0135d4db517b48ed6ce37e69c9c3b3473f33e4782a81"
-    sha256 cellar: :any_skip_relocation, catalina:       "f9eedf52c64b2234500f0135d4db517b48ed6ce37e69c9c3b3473f33e4782a81"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9091e323c653cef2b368d5facbba3477e40c5ab04d7d24ecec63c6f8755e4feb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "faba05bc525c631a2da5d44d871dd9712f520753e4c7fa9c003bccfcf9a2ceef"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "faba05bc525c631a2da5d44d871dd9712f520753e4c7fa9c003bccfcf9a2ceef"
+    sha256 cellar: :any_skip_relocation, monterey:       "edd5f9ea563a8ce5f4699d773fdc89a02acd40655aed6f2c84351929b2319af2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "edd5f9ea563a8ce5f4699d773fdc89a02acd40655aed6f2c84351929b2319af2"
+    sha256 cellar: :any_skip_relocation, catalina:       "edd5f9ea563a8ce5f4699d773fdc89a02acd40655aed6f2c84351929b2319af2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ebdc72145968b6a7b496d67b04dafed7c7ea0bb40aba0405cebb3030ee2b89ea"
   end
 
   depends_on "node"
