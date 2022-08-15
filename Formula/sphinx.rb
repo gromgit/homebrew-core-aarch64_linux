068eda096299bbe39ab7e13d7da2444a8d1fe101
Michael Cho <cho-m@tuta.io>
2022-08-15 14:35:49 -0700

    sphinx: revision
    * sphinx: deprecate
    * sphinx: add patches
    * sphinx: fix style, license
    * sphinx: tweak patch
    
    Closes #108116.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sphinx.rb
+++ Formula/sphinx.rb
@@ -3,8 +3,8 @@ class Sphinx < Formula
   homepage "https://sphinxsearch.com/"
   url "https://sphinxsearch.com/files/sphinx-2.2.11-release.tar.gz"
   sha256 "6662039f093314f896950519fa781bc87610f926f64b3d349229002f06ac41a9"
-  license "GPL-2.0"
-  revision 3
+  license "GPL-2.0-or-later"
+  revision 4
   head "https://github.com/sphinxsearch/sphinx.git", branch: "master"
 
   bottle do
@@ -19,6 +19,9 @@ class Sphinx < Formula
     sha256 x86_64_linux:   "3736780f2809b0449953cfb4ed85cc7d65b86e6c5c55aa98186062f8aff81d99"
   end
 
+  # Ref: https://github.com/sphinxsearch/sphinx#sphinx
+  deprecate! date: "2022-08-15", because: "is using unsupported v2 and source for v3 is not publicly available"
+
   depends_on "mysql@5.7"
   depends_on "openssl@1.1"
 
@@ -29,6 +32,11 @@ class Sphinx < Formula
   resource "stemmer" do
     url "https://github.com/snowballstem/snowball.git",
         revision: "9b58e92c965cd7e3208247ace3cc00d173397f3c"
+  end
+
+  patch do
+    url "https://sources.debian.org/data/main/s/sphinxsearch/2.2.11-8/debian/patches/06-CVE-2020-29050.patch"
+    sha256 "a52e065880b7293d95b6278f1013825b7ac52a1f2c28e8a69ed739882a4a5c3a"
   end
 
   def install
@@ -45,6 +53,13 @@ class Sphinx < Formula
       --with-mysql
       --without-pgsql
     ]
+
+    # Security fix: default to localhost
+    # https://sources.debian.org/data/main/s/sphinxsearch/2.2.11-8/debian/patches/config-default-to-localhost.patch
+    inreplace %w[sphinx-min.conf.in sphinx.conf.in] do |s|
+      s.gsub! "9312", "127.0.0.1:9312"
+      s.gsub! "9306:mysql41", "127.0.0.1:9306:mysql41"
+    end
 
     system "./configure", *args
     system "make", "install"
