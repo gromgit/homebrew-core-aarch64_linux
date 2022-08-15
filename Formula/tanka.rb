c251250a38e312ed0ee839c761c560b0c9dc00dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-15 19:04:35 +0000

    tanka: update 0.22.1 bottle.

--- Formula/tanka.rb
+++ Formula/tanka.rb
@@ -13,12 +13,13 @@ class Tanka < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f744a4582aa42a9894676beb884d9447eca55052f0154e8723e894d835f23e0b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d5c2707e608317939161ca5d30838ea7bff710564c598d239379d986cd1b6661"
-    sha256 cellar: :any_skip_relocation, monterey:       "1af841fb48ce0a9378429dace5ad386a71a2497980158ec75712bc852cd7f407"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fb83c2ec32f12459b6b3cf7319e4d4c2c4e2ebb51e20a355fd8f9a9fe00dec7a"
-    sha256 cellar: :any_skip_relocation, catalina:       "811b407ac01b08e403bcd13a9b4a0426056099a1d7f83babff1dcd11c9cfe9fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "afda0edf8d7324157d32ba15dfdc369a0b3c884dd1d4f73b1293ea78acd8e174"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d763332d1fe381e9ebd93051ae94d1a055e064647fb6456fdf64c3662f6bcae8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d763332d1fe381e9ebd93051ae94d1a055e064647fb6456fdf64c3662f6bcae8"
+    sha256 cellar: :any_skip_relocation, monterey:       "3a81a8efadf9bf91865374e4033a9a16eafbd379992984b427d10df31ff84322"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3a81a8efadf9bf91865374e4033a9a16eafbd379992984b427d10df31ff84322"
+    sha256 cellar: :any_skip_relocation, catalina:       "3a81a8efadf9bf91865374e4033a9a16eafbd379992984b427d10df31ff84322"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f5fea4572a64cdb750cd912425e5c6c23cb57231172721d2f5ce27bc21d00aa"
   end
 
   depends_on "go" => :build
