b661b7a658a21dd0ba2ed3496ac4b5beed0e5022
Rui Chen <rui@chenrui.dev>
2022-08-15 00:22:04 -0400

    suil 0.10.16
    * suil 0.10.16
    * suil: switch to meson build
    * add build patch
    
    Closes #108065.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/suil.rb
+++ Formula/suil.rb
@@ -1,10 +1,19 @@
 class Suil < Formula
   desc "Lightweight C library for loading and wrapping LV2 plugin UIs"
   homepage "https://drobilla.net/software/suil.html"
-  url "https://download.drobilla.net/suil-0.10.12.tar.bz2"
-  sha256 "daa763b231b22a1f532530d3e04c1fae48d1e1e03785e23c9ac138f207b87ecd"
   license "ISC"
   head "https://gitlab.com/lv2/suil.git", branch: "master"
+
+  stable do
+    url "https://download.drobilla.net/suil-0.10.16.tar.xz"
+    sha256 "bc9f36c13863e70fd65bf7134afc2b7b141e9ca4b279590efae1d4b25f4211f9"
+
+    # remove in next version
+    patch do
+      url "https://github.com/lv2/suil/commit/ecebfdb35fd8af72dc918ff34ae3f3366521925d.patch?full_index=1"
+      sha256 "d12cc51870da6009555e8f40fddd64931b68e6e09362224eec0ee227d97fe62d"
+    end
+  end
 
   livecheck do
     url "https://download.drobilla.net/"
@@ -20,34 +29,18 @@ class Suil < Formula
     sha256 x86_64_linux:   "44cdafe1344964b295ed7bc86dc9a96cc73ccb86aab8bc44adb4dd93cfa187ee"
   end
 
-  depends_on "pkg-config" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "python@3.10" => :build
   depends_on "gtk+"
   depends_on "gtk+3"
   depends_on "lv2"
   depends_on "qt@5"
 
-  # Fix build issue, remove in next release
-  # upstream commit ref, https://github.com/lv2/suil/commit/7183178b8e35b9a05f2a90e1d091b34c5f846ef5
-  patch :DATA
-
   def install
-    ENV.cxx11
-    ENV.prepend_path "PATH", Formula["python@3.10"].libexec/"bin"
-    args = [
-      "--prefix=#{prefix}",
-      "--gtk3-lib-name=#{shared_library("libgtk-3.0")}",
-    ]
-    if OS.mac?
-      args += [
-        "--no-x11",
-        "--gtk2-lib-name=#{shared_library("libgtk-quartz-2.0.0")}",
-      ]
-    else
-      args << ["--gtk2-lib-name=#{shared_library("libgtk-x11-2.0")}"]
-    end
-    system "./waf", "configure", *args
-    system "./waf", "install"
+    system "meson", "build", *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
   end
 
   test do
@@ -64,40 +57,3 @@ class Suil < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/src/gtk2_in_qt5.cpp b/src/gtk2_in_qt5.cpp
-index a5c98f1..47c8271 100644
---- a/src/gtk2_in_qt5.cpp
-+++ b/src/gtk2_in_qt5.cpp
-@@ -41,6 +41,7 @@ SUIL_DISABLE_GTK_WARNINGS
- #include <gtk/gtk.h>
- SUIL_RESTORE_WARNINGS
-
-+#include <cstdint>
- #include <cstdlib>
-
- extern "C" {
-@@ -95,8 +96,7 @@ wrapper_wrap(SuilWrapper* wrapper, SuilInstance* instance)
-   gtk_container_add(GTK_CONTAINER(plug), widget);
-   gtk_widget_show_all(plug);
-
--  const WId wid =
--    static_cast<WId>(gtk_plug_get_id(reinterpret_cast<GtkPlug*>(plug)));
-+  const WId wid = (WId)gtk_plug_get_id(GTK_PLUG(plug));
-
-   QWindow* window = QWindow::fromWinId(wid);
-   QWidget* container =
-diff --git a/src/qt5_in_gtk.cpp b/src/qt5_in_gtk.cpp
-index 6277daa..1c614c7 100644
---- a/src/qt5_in_gtk.cpp
-+++ b/src/qt5_in_gtk.cpp
-@@ -125,7 +125,7 @@ suil_qt_wrapper_realize(GtkWidget* w, gpointer)
- {
-   SuilQtWrapper* const wrap = SUIL_QT_WRAPPER(w);
-   GtkSocket* const     s    = GTK_SOCKET(w);
--  const WId            id   = static_cast<WId>(gtk_socket_get_id(s));
-+  const WId            id   = (WId)gtk_socket_get_id(s);
-
-   wrap->qembed->winId();
-   wrap->qembed->windowHandle()->setParent(QWindow::fromWinId(id));
