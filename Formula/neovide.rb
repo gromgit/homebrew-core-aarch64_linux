174a8aebf91316f9a67556f32badaa13addf1d21
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-15 20:22:25 +0800

    neovide 0.10.1
    Closes #108089.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/neovide.rb
+++ Formula/neovide.rb
@@ -1,8 +1,8 @@
 class Neovide < Formula
   desc "No Nonsense Neovim Client in Rust"
   homepage "https://github.com/neovide/neovide"
-  url "https://github.com/neovide/neovide/archive/tags/0.10.0.tar.gz"
-  sha256 "3ae859b3f1ba5e733d413071e3a33082350ca6bf14500b437a2259c5d5c4b706"
+  url "https://github.com/neovide/neovide/archive/tags/0.10.1.tar.gz"
+  sha256 "b94b5cc387b1aea03c6d967b738b487bf8e3ca876690716f34bd5ba6a7964d13"
   license "MIT"
   head "https://github.com/neovide/neovide.git", branch: "main"
 
