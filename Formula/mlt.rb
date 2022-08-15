cb434ab34cb4576c7ca3535969abb6d24ec215bb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-15 11:42:45 +0000

    mlt: update 7.8.0_1 bottle.

--- Formula/mlt.rb
+++ Formula/mlt.rb
@@ -8,12 +8,12 @@ class Mlt < Formula
   head "https://github.com/mltframework/mlt.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "dcaf9747d3aad9909d225e3b98864072103d2d2b93391678ef66732ee970e8eb"
-    sha256 arm64_big_sur:  "b9b6b9540a4c478a0f64612f430c821c67b2652fa483cce1f22098cb04ac4851"
-    sha256 monterey:       "b6e91b91d5640a87dbe02513c88f698ca4dd35e03d3e0ba5abb67a426d224039"
-    sha256 big_sur:        "c552e946cad9bb2752889885296d9983f96ab13edd609371ba766577d1e0a1bf"
-    sha256 catalina:       "561db9de0a8f913913d8190071dd3f08bfc7ad7b535cd83112c258a911392ca1"
-    sha256 x86_64_linux:   "59f7e0cb66e63fcd8961a22e578622f3086d5af31a6f633e370461d126ad0ba0"
+    sha256 arm64_monterey: "8bb4eba794b94fb65abfa87a053c6ff5ed62d0fa055591a5107d976ceba20759"
+    sha256 arm64_big_sur:  "c093eede0d92f63e5730e7412ce860d909734f0fd2266839c8aff299dd92d0c5"
+    sha256 monterey:       "0a82525c05654c1c0f83fc612a05e3764e108204d069d909f2ef4ed6b1a86ce5"
+    sha256 big_sur:        "bf5d2d5b61b8e9619f4b92f622d0b5861e6188580eb0004b9e0342c670757607"
+    sha256 catalina:       "f8998f3b0d01afc3ab67fbfa65ecfc1a2ea5f843d1b393c6914ef1ba32ebd6af"
+    sha256 x86_64_linux:   "d68540fe7c7ebdb3fca1d7f0f6f7f90930c0ec1301ab00810ac61e248cdf22c4"
   end
 
   depends_on "cmake" => :build
