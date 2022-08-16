0b8a5cba52be2f3feb9c01a80f6a508b18b068a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-16 19:04:32 +0000

    yorkie: update 0.2.16 bottle.

--- Formula/yorkie.rb
+++ Formula/yorkie.rb
@@ -8,12 +8,12 @@ class Yorkie < Formula
   head "https://github.com/yorkie-team/yorkie.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d08ebac9dcf38e66b119d23cd2b7cfd980e0f0d97f274d65db29c0f2dbd98667"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f699bc82a0c1feef8ab85cbb40f16837a49f7c9f04c2dc7b16c49af85ab160c"
-    sha256 cellar: :any_skip_relocation, monterey:       "767fd30b3ba0ef4f2875fc0d5c2cc3e0ebe7e40387729a0bd3025a6d52c7ec52"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bdd8d81c6d6ec09d460842b64bfb05038eb82d5bbdc169291b689184eb84ec3a"
-    sha256 cellar: :any_skip_relocation, catalina:       "aac43fef3f9964a496cbde0df24be181d63d84428bee4c6e3f75e6e3c1969c56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d45720c72660a513a56582cef4144f3a71832e8d67f07620d37719926c59951b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3347f51efceb0a6b54a4abd9e9e4c9af38804ba61cd695fcea1f2b4f0a3d72f9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a9459dea7d576e5a71b232c0cbb3a591a14e638196bf27634f5b6fe74407ce9"
+    sha256 cellar: :any_skip_relocation, monterey:       "d1fa1c50ccf0af19f4f46ba76ab27d779aa4f60169beb3c3706fdf60fce85dd2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fbc1ce5e5dd831eaff66aca1acf37101786f64f942bfe8b51ec828dabec1fa9d"
+    sha256 cellar: :any_skip_relocation, catalina:       "5f1c4752b90bda4cdb6065ba40ff9bb14501584a33e3eadf55d170406d92ee5f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db807b8b9f8e83b62e176ec2da0c7787830f4644b401151903e330c249314315"
   end
 
   # Doesn't build with latest go
