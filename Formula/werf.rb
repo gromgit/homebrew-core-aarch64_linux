2e9388521240cf7ba60e1c1bb0b9c30313be240a
Porkepix <porkepix@gmail.com>
2022-08-17 01:17:49 +0200

    werf 1.2.160
    Closes #108223.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/werf.rb
+++ Formula/werf.rb
@@ -1,8 +1,8 @@
 class Werf < Formula
   desc "Consistent delivery tool for Kubernetes"
   homepage "https://werf.io/"
-  url "https://github.com/werf/werf/archive/refs/tags/v1.2.155.tar.gz"
-  sha256 "49cb19a93cb4eb57dd46a1572e2066853b4c9fd0b8d15c970118bcd1814e9fca"
+  url "https://github.com/werf/werf/archive/refs/tags/v1.2.160.tar.gz"
+  sha256 "1ee431d3afcc3bcddbf57ec7c77100bed74a1f89b7bdc7ecd198f36b1159fc4a"
   license "Apache-2.0"
   head "https://github.com/werf/werf.git", branch: "main"
 
