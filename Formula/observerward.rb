e3e0ee267e7795d3d516acbbe17180d7ea14867b
三米前有蕉皮 <root@kali-team.cn>
2022-08-16 03:51:47 +0000

    observerward 2022.8.16
    Closes #108139.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/observerward.rb
+++ Formula/observerward.rb
@@ -1,8 +1,8 @@
 class Observerward < Formula
   desc "Cross platform community web fingerprint identification tool"
   homepage "https://0x727.github.io/ObserverWard/"
-  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.8.11.tar.gz"
-  sha256 "17a9ee8e4a29debaa92b7af1c2f8e72149a01e9673c493128e922fd47a49a8e8"
+  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.8.16.tar.gz"
+  sha256 "1f1dc78f5305ec0e64dc003e471404cca1746d55bdb3ff63dc92e2dff55a90e8"
   license "MIT"
 
   bottle do
