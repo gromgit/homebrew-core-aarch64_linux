b445e0aa01e458a9b3966c480647ef93276f6213
Porkepix <porkepix@gmail.com>
2022-08-17 01:05:17 +0200

    jruby 9.3.7.0
    Closes #108214.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jruby.rb
+++ Formula/jruby.rb
@@ -1,8 +1,8 @@
 class Jruby < Formula
   desc "Ruby implementation in pure Java"
   homepage "https://www.jruby.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/jruby/jruby-dist/9.3.6.0/jruby-dist-9.3.6.0-bin.tar.gz"
-  sha256 "747af6af99a674f208f40da8db22d77c6da493a83280e990b52d523abd9499e2"
+  url "https://search.maven.org/remotecontent?filepath=org/jruby/jruby-dist/9.3.7.0/jruby-dist-9.3.7.0-bin.tar.gz"
+  sha256 "94a7a8b3beeac2253a8876e73adfac6bececb2b54d2ddfa68f245dc81967d0c1"
   license any_of: ["EPL-2.0", "GPL-2.0-only", "LGPL-2.1-only"]
 
   livecheck do
