37ac704e315ec7dc06c3af0e05105f64b22d5b19
miccal <miccal.matthews@gmail.com>
2022-08-16 08:53:46 +0800

    lz4 1.9.4
    Closes #108135.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lz4.rb
+++ Formula/lz4.rb
@@ -1,10 +1,10 @@
 class Lz4 < Formula
   desc "Extremely Fast Compression algorithm"
   homepage "https://lz4.github.io/lz4/"
-  url "https://github.com/lz4/lz4/archive/v1.9.3.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/lz4-1.9.3.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/lz4-1.9.3.tar.gz"
-  sha256 "030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
+  url "https://github.com/lz4/lz4/archive/v1.9.4.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/lz4-1.9.4.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/lz4-1.9.4.tar.gz"
+  sha256 "0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b"
   license "BSD-2-Clause"
   head "https://github.com/lz4/lz4.git", branch: "dev"
 
