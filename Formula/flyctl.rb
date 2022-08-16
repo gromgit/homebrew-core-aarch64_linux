da43817c0e76ab47dfc967262fc8c9e154614394
flyio-buildbot <63733732+flyio-buildbot@users.noreply.github.com>
2022-08-16 19:07:53 +0000

    flyctl 0.0.373
    Closes #108199.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.372",
-      revision: "33637703666e8bdba7b445162af06039d17984d7"
+      tag:      "v0.0.373",
+      revision: "52a33bddbb90f1bba4f856f3d2d48e384ffd52e1"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
