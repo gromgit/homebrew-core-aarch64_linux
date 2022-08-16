7b577aab5598f0270e5f1f9b8ab4be4e46fe4a71
Porkepix <porkepix@gmail.com>
2022-08-17 00:27:34 +0200

    flyway 9.1.5
    Closes #108210.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.1.4/flyway-commandline-9.1.4.tar.gz"
-  sha256 "1ecf43666824697d34708ed85d37e076f0b4c77b2df147d4d2943b043187a436"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.1.5/flyway-commandline-9.1.5.tar.gz"
+  sha256 "7cef0270e4a1e7df9897f81811eaf94f12789a67bd673020781b913813bceac9"
   license "Apache-2.0"
 
   livecheck do
