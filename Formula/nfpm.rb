44ccfd3f899080bc3680eb4c338480cea9363619
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-16 08:04:51 +0000

    nfpm: update 2.18.0 bottle.

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -7,12 +7,12 @@ class Nfpm < Formula
   head "https://github.com/goreleaser/nfpm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d111bd2d627fd45c4b77bf21403cf1f0ab2a0e404231a3b971f789e5d8e45430"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d9532ff55e0f7df0cd04251f1f4aaf02825bf8efb222e8370b341d042c86fcdf"
-    sha256 cellar: :any_skip_relocation, monterey:       "24beaf2d3557403a5235bce05fb43d3459d43cc0d9737cda27c8d75c04f5e881"
-    sha256 cellar: :any_skip_relocation, big_sur:        "977e7154765786bd1f6dc42dac2c78a54cb043d7656786855c76c0d775be6e14"
-    sha256 cellar: :any_skip_relocation, catalina:       "af90e7428824bcee5d7c35a5be3581b3efad75a5fdb20add26082eb46bcf1137"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e1d3d2321dd3b60135509a1af7407f0fd8fa98fe634257d93ef9b61a44336f7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b036e248314ed0b2c0d87c238a431de14b3bcaeca12820c97c0868606f8776e7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "86cd9beca8417d1b39a158bf17c123ae0a645c5db798e82662480fffe0296dbc"
+    sha256 cellar: :any_skip_relocation, monterey:       "2d227091696e9f6e5e10d43242066a41d275e66232664ebe01e724e8f9cf9c30"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e7c84172e26d7d2398557c7e91b889da253f5b59fc89fb494601aa632f981ece"
+    sha256 cellar: :any_skip_relocation, catalina:       "06262df845c101ccfb8f403507d376089d7d227e7d5171c7b9aac87d6ba4392b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a49d88a164d5ef9f0f642b569fa264e7c4ad305f0517ffa9d79b90238f5809a1"
   end
 
   depends_on "go" => :build
