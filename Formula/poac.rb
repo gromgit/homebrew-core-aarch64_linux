8ec3d8a74239d06c3c418e23d8a555a75ad5d2e9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-17 00:07:42 +0000

    poac: update 0.4.1_2 bottle.

--- Formula/poac.rb
+++ Formula/poac.rb
@@ -8,12 +8,12 @@ class Poac < Formula
   head "https://github.com/poacpm/poac.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "1afba3c3f77ff0bd2d84927b83eeb80b417c30a9a24912b86a6d96977cdef2ee"
-    sha256 cellar: :any,                 arm64_big_sur:  "f7343916254c9c858175041c1211bfc16cd753e161d88290670f2cb2c7af50e6"
-    sha256 cellar: :any,                 monterey:       "8e7759ebbd3a9f05d1cb54fde3297f696a80a968e982a9a56e9d8a6d6a2d1171"
-    sha256 cellar: :any,                 big_sur:        "56fcc8f4e572f87b80fa64e6c740e6c546f31e97789296e00d7147a0f29ae923"
-    sha256 cellar: :any,                 catalina:       "99e6f041fc94715f6c621d2f22e666c4d9f2e75e7fb641f402eb6ab4621a675c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d13d94485675e688f1281d36e5c0fb5379218007d0c704115c7f1e81558113ab"
+    sha256 cellar: :any,                 arm64_monterey: "9f0db4443983c815b98429e668f8d7d0fdf143876eb57e9a16bc769cb7b3f797"
+    sha256 cellar: :any,                 arm64_big_sur:  "b6c74adbd8a5dd5773f46aeda390db16f48e3a48112dad1af5707809ece6f556"
+    sha256 cellar: :any,                 monterey:       "88d05562ee26ed90a2958b2f957d83661c8231e05019469001d958ba79032c7e"
+    sha256 cellar: :any,                 big_sur:        "c4b414c7a0634f2e5cd605c154d714e1a70a63b34dcf7f1de1ae86b816e188af"
+    sha256 cellar: :any,                 catalina:       "4a3d7a94885150f390acac3f709fadf55a9996e44554cc10f0d9f6f253abad6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c77c222c0d87bf25bdc18930b7a4ebb01d718eab06f2a1047bef7a11f85c3bb0"
   end
 
   depends_on "cmake" => :build
