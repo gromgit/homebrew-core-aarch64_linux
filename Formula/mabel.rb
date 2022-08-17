b429bae6e35ee9ba5c50cf6bc73d5a96ee71f695
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-17 21:05:10 +0000

    mabel: update 0.1.6 bottle.

--- Formula/mabel.rb
+++ Formula/mabel.rb
@@ -8,12 +8,12 @@ class Mabel < Formula
   head "https://github.com/smmr-software/mabel.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a23960e59fe71026596b30f9bebbc16e4a39ff7e072d165ecaff127d55e0941f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab556e901b131b2fc6d9f94ec20dd5cc84929dd24772afd6b62c6939c4901b73"
-    sha256 cellar: :any_skip_relocation, monterey:       "279d19a2e45d86503c03547678bff0696ec50a28a7a1d96f3b6598d5aeca8169"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1587e1486203466e8bdafb79858d22d14b543730a2e1b7bbed39f89af2beaf08"
-    sha256 cellar: :any_skip_relocation, catalina:       "a768c23d24d13566cdbbb75929767141a8fcbbee20aab3a01df019c08ef57e14"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f4f2edb7ea7410b9568effafed6c4f57ff2c95ebce6b380cc003423c73badfef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "97cfdf838faaed2ac42530932354e9be4b7c5d4e6abec6168f11bf14aa302f1a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "db387b01505c1d28d97aa8551b66a23655a8b21e3ffd553f830ecbdba74ab1ed"
+    sha256 cellar: :any_skip_relocation, monterey:       "17588697f1cb530ff498aaa1060e79a3474215b2be8653e611c18deaed3d16e9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "deb9adc1cb8f5ad3d6655e3e16fff9c5be3f5527b03224a192ba35d9339040f9"
+    sha256 cellar: :any_skip_relocation, catalina:       "79283f76f6003ed4814d791de60b4689b1e51a1cbbd0efa12220d90ce3c80756"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d1bef6b6aa347b701a37aff20108af879c617b18a333df2feed670b08da89098"
   end
 
   depends_on "go" => :build
