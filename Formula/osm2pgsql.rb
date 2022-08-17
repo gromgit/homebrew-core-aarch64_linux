7941d306f183cdc4eadd38918761bd6c25376795
Rui Chen <rui@chenrui.dev>
2022-08-17 13:55:14 -0400

    osm2pgsql 1.7.0
    * osm2pgsql 1.7.0
    * osm2pgsql: build needs c++17 compiler
    * osm2pgsql: remove Linux bottle
      This has only 11 installs in the past year, so adding a Linux-only GCC
      dependency here is not worth it at all.
    
    Closes #108294.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -1,10 +1,9 @@
 class Osm2pgsql < Formula
   desc "OpenStreetMap data to PostgreSQL converter"
   homepage "https://osm2pgsql.org"
-  url "https://github.com/openstreetmap/osm2pgsql/archive/1.6.0.tar.gz"
-  sha256 "0ec8b58ab972ac8356185af4161270c1b625a77299f09e5fb7f45e616ef1a9a5"
+  url "https://github.com/openstreetmap/osm2pgsql/archive/1.7.0.tar.gz"
+  sha256 "0f722baf0f04eda387d934d86228aae07d848993900db6b9e7ab312c91fd84e5"
   license "GPL-2.0-only"
-  revision 4
   head "https://github.com/openstreetmap/osm2pgsql.git", branch: "master"
 
   bottle do
@@ -13,7 +12,6 @@ class Osm2pgsql < Formula
     sha256 monterey:       "c55c28a4a7d61e2ecc74e439d178c2479ba6bb1f70d5a4c8a419432141d477f8"
     sha256 big_sur:        "461f444d3d7da2f549b445e3d16839ac42105c01aa859b086d6ca7f9f8c141ca"
     sha256 catalina:       "2094de94b79ac68652e5671b360d9fa158146b6c3d0263dee84f5008c7c52d99"
-    sha256 x86_64_linux:   "753d07bfe5ff3a913144fb09cd892918f77569e9a5b44977fc37ad7d93c27a97"
   end
 
   depends_on "cmake" => :build
