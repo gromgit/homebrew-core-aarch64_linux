4ec8666d3b96758b7bb4580ce379cbaee0389f7d
Rui Chen <rui@chenrui.dev>
2022-08-17 13:47:31 -0400

    picat 3.2#8
    Closes #108289.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/picat.rb
+++ Formula/picat.rb
@@ -1,9 +1,9 @@
 class Picat < Formula
   desc "Simple, and yet powerful, logic-based multi-paradigm programming language"
   homepage "http://picat-lang.org/"
-  url "http://picat-lang.org/download/picat326_src.tar.gz"
-  version "3.2.6"
-  sha256 "7954396866e3e12944c09a7ca4123f9ce5d6193caecb8899b79cfd098ada399d"
+  url "http://picat-lang.org/download/picat328_src.tar.gz"
+  version "3.2#8"
+  sha256 "1b679edec1586c6fd4f92e0b4241598c38969ae82fb7b6e0ed1b5a33e0ef61a7"
   license "MPL-2.0"
 
   livecheck do
