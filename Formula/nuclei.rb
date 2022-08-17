1e418dce4f81a24e8cc90c8302cda5bd7f1ff3dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-17 05:40:33 +0000

    nuclei 2.7.6
    Closes #108248.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nuclei.rb
+++ Formula/nuclei.rb
@@ -1,8 +1,8 @@
 class Nuclei < Formula
   desc "HTTP/DNS scanner configurable via YAML templates"
   homepage "https://nuclei.projectdiscovery.io/"
-  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.5.tar.gz"
-  sha256 "19d421db99f0f1269adb152b619de4c4b820c822adb48f6bf9b0d89cf23d441c"
+  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.6.tar.gz"
+  sha256 "ac015810143035a7540669b4e931e71a2f1fdcc0a97deca02469b867adb0f778"
   license "MIT"
   head "https://github.com/projectdiscovery/nuclei.git", branch: "master"
 
