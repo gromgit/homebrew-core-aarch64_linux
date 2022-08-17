68a02f541230420f989984ab033da9a2d5f18cd4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-17 21:04:50 +0000

    bazelisk: update 1.12.2 bottle.

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -8,12 +8,12 @@ class Bazelisk < Formula
   head "https://github.com/bazelbuild/bazelisk.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "357415da1bbc4693035c651d1ce6f1be6522f83180c206d0790cd231afd59b44"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c48f4b21b6773047daba340f67e98612a44e7c733c8c38b2c31bc0570663a117"
-    sha256 cellar: :any_skip_relocation, monterey:       "8fbe827bbe3ec081ceefa37c83b243a9b14a864cd4d8037c5409d4b3a003dcae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "15792aadb9f31666abdb2269d7bfc419fb4ba4aa4cbeafce711d21b1562f93fc"
-    sha256 cellar: :any_skip_relocation, catalina:       "c20c259803b34c30974b3ace922b69ea90bae9ddaf734c290f47245752978404"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d1c9e929b8f11faf8563a1d091df34d35f40a0c921a2c230430736c239c2533"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bd5d9a2734548ff08f39a2a53fc7ce4d8719e3bbeff7eb6169197161f0a687b4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8448953351ba2335ff7a1d74d869a8fac7a4bad55ab77039f4c8dfd3ef2b95fa"
+    sha256 cellar: :any_skip_relocation, monterey:       "5d05bd774c257ee80bc70c778e082a39e6c253397b5c0dcc098d15c5da69aef9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "06cdd52b9e0995cda6e967db59051887e2239ea6e214f12dd1be9e19cd587b3d"
+    sha256 cellar: :any_skip_relocation, catalina:       "b7ceff633799299633d82f447b6e4ee9343fd55edd7fb52e2220996f9a573d43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97b1f68adb48083b96d2a873c5cd51457cf5998a778129786815f2715df6b40c"
   end
 
   depends_on "go" => :build
