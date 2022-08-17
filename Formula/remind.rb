073916e7b8bc5903a480d82f0974941ee8485122
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-17 06:05:17 +0000

    remind: update 04.00.03 bottle.

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -12,12 +12,12 @@ class Remind < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "a690ac54133cb7bce12080a0dc9217bc547e354b562f782c18033294991add86"
-    sha256 arm64_big_sur:  "a743d76c2759052884e56943346b3583dc1d7c0d2476ed04f945f6c60feacfd1"
-    sha256 monterey:       "c85cd7481d6ecebb52b75bc621a754212d08bcb80d00343293876c1d785d9d16"
-    sha256 big_sur:        "24c1e6191f059785a65160c2e8308dc89a05f64264a1a5c50445a60ad05a2460"
-    sha256 catalina:       "f843dd7fd0414a6265ddbca478dc873bc950665f945e6e3f10e59620503aa5dc"
-    sha256 x86_64_linux:   "0c199d684dbc3238cc59ab87ad25a2f0977f72eadb9382f409175a9ebc810c9e"
+    sha256 arm64_monterey: "0dcc714622c6e79cf665a69cb9ecaccb5d4e9985322f1dd84d511329c0892e9f"
+    sha256 arm64_big_sur:  "2ee39daf9f277066c0ea76b9b3f5a21d72cbeca1cf69466b1842a529eb1913f5"
+    sha256 monterey:       "c3e48c285ac06fd04fd5ada07a159dc72d7d86716d04a0addb04584dfaac58d3"
+    sha256 big_sur:        "bf5a719dd6e8557adc3203ca99be06a410adaebdf3dcc054f1005c98f4aec804"
+    sha256 catalina:       "485dda1beadad00ba6da6fa7919c8fd1ec8d4d984498bb03b14aa615cd13e2a7"
+    sha256 x86_64_linux:   "e39a2bbcd33ef1f68781a1111e92a616d8b5733cc59fed3d4d80cb80d67d9588"
   end
 
   conflicts_with "rem", because: "both install `rem` binaries"
