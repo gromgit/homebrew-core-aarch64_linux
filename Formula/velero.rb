730b4bd6df825f5f46d5023b7de06832e5c725b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-17 12:06:07 +0000

    velero: update 1.9.1 bottle.

--- Formula/velero.rb
+++ Formula/velero.rb
@@ -6,12 +6,12 @@ class Velero < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3c4fef74c83b07c1d9d195554c924ff4adb90705abb0bbde7f5b1ab799a45ac4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c7b96f49e100f98d138ce47b2ffeb2098695791d7db254bdfc010a4475376512"
-    sha256 cellar: :any_skip_relocation, monterey:       "7c91d88283327d154e2438639b91dc58ce71a2e81dec0396193bb591be20d0ae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4999a63622ff95f83af7749c48a66debe93ac7e31bc34520a30a8c171bce55db"
-    sha256 cellar: :any_skip_relocation, catalina:       "0c0feb320ab1f409e259663707bd95da3054e78376bc399c9eb85874a8c642e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8c6fe417db6d1422ecf596501c470d24697461e2a320bfee06f67e793857d97"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6fc6a8a6356d3c1d3461c623fc6b6e18b68f738440b039a327c700218a9a8810"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01fbfe905cbf47d056de89346dc486b9229ec929cdb1a8a2f1e100fddadcb8e3"
+    sha256 cellar: :any_skip_relocation, monterey:       "3be6d81159876050b7f83718968d24946a492caecc0900dd541c98edfbd854e7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "949012a43992cfde3a2273cbfdefdb18ec13c68dcbf62d24195c7c0d56fcb63c"
+    sha256 cellar: :any_skip_relocation, catalina:       "74890025164a0e62dc87f99e18de8d43c5a1c93dad8294f9930d7e754b4f2cc1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ce55c308a2a22611139e0d49c857a953d2a818584521f0ec964b76fb6c88f77a"
   end
 
   depends_on "go" => :build
