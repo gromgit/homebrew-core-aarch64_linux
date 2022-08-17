2d58b2dd310e543872e9d6d6f259b0524d243c44
Rui Chen <rui@chenrui.dev>
2022-08-17 14:17:27 -0400

    dhall-json 1.7.11
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -1,8 +1,8 @@
 class DhallJson < Formula
   desc "Dhall to JSON compiler and a Dhall to YAML compiler"
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-json"
-  url "https://hackage.haskell.org/package/dhall-json-1.7.10/dhall-json-1.7.10.tar.gz"
-  sha256 "e15cd486b287ed281cd56da8d0603bb70dbfa33f2b3e107682d82935dad3f785"
+  url "https://hackage.haskell.org/package/dhall-json-1.7.11/dhall-json-1.7.11.tar.gz"
+  sha256 "26c2acf33bfc597f4d2ac333a431180c9929d7a9b4ef7cf70c4bd45eba65ef28"
   license "BSD-3-Clause"
   head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
