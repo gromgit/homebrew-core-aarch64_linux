328dfe2f46440d61e55c94a67273d59c54793d51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-18 19:04:33 +0000

    gost: update 2.11.3 bottle.

--- Formula/gost.rb
+++ Formula/gost.rb
@@ -7,12 +7,12 @@ class Gost < Formula
   head "https://github.com/ginuerzh/gost.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5112d60ba5a6090f3f3b36d0c59b6400cd24ea07bf03c186de395372b3d0862"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3fcdb66e5d378771ffb01b848990ab32cdc5447597f5b075f1373023ead4aa72"
-    sha256 cellar: :any_skip_relocation, monterey:       "a803beb44f47f36f821f1eb0aa15892d9f95e20431150107c531f3d74fb9d464"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b4e97c302058c91679e66f481745d7361045ccf85919eefe3aa8b282d3555dff"
-    sha256 cellar: :any_skip_relocation, catalina:       "d032da856ce53e033dea8c91142e8f218b55fb35f3d6e515038fd1256f2431d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cf7813ce42f66f176ddc7f37aa5db4d466f38d831389bb3e13fe8b591c73c1bb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "14e62616254d43f72f8705e1a00e53b1a80d9acec991502c089c47fd6a6d7109"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "42352933d08a976b8f00afd0ce00546bcceb860367c3709f09a562b3ca131542"
+    sha256 cellar: :any_skip_relocation, monterey:       "efec94d4398a9af35440fba87074ce305b24ca9b62b57f78142a9d5321041c5e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "908c9b5c168570994e93e83ce436075fad09467a0b49ca418c42f60acbbbdca9"
+    sha256 cellar: :any_skip_relocation, catalina:       "185c4c7c5e432f2239347c64bd73cd7d93dca332e52765d14e1498d8c433e41e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa9b695ece2ed1a5f9d95c8ea08b76dd35ce5701f200caf77fbedd6f6838e215"
   end
 
   # Bump to 1.18 on the next release, if possible.
