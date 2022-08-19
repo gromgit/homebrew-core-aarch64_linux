149b366a00e096811c7a132139043d4abfe059eb
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2022-08-19 01:53:10 +0000

    janet 1.24.0
    * janet 1.24.0
    * Update janet.rb
    
    Closes #108403.
    
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -1,8 +1,8 @@
 class Janet < Formula
   desc "Dynamic language and bytecode vm"
   homepage "https://janet-lang.org"
-  url "https://github.com/janet-lang/janet/archive/v1.23.0.tar.gz"
-  sha256 "0b4d5d3632e0d376d9512ea8ea262f31f75c132b488dd7870f472acae709a865"
+  url "https://github.com/janet-lang/janet/archive/v1.24.0.tar.gz"
+  sha256 "ae794c7b4ffe7cfb6f1edec60556ccda34165b2fea5c23045e332eeebc1077f4"
   license "MIT"
   head "https://github.com/janet-lang/janet.git", branch: "master"
 
@@ -19,8 +19,8 @@ class Janet < Formula
   depends_on "ninja" => :build
 
   resource "jpm" do
-    url "https://github.com/janet-lang/jpm/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "858d4ef2f6ac78222c53154dd91f8fb5994e3c3cbe253c9b0d3b9d52557eeb9b"
+    url "https://github.com/janet-lang/jpm/archive/refs/tags/v1.1.0.tar.gz"
+    sha256 "337c40d9b8c087b920202287b375c2962447218e8e127ce3a5a12e6e47ac6f16"
   end
 
   def install
