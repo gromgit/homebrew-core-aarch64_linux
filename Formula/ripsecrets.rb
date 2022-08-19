9893bc2ac2ccd1669b1d345c97f6583f9bf2c4aa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-19 13:19:48 +0000

    ripsecrets: add 0.1.5 bottle.

--- Formula/ripsecrets.rb
+++ Formula/ripsecrets.rb
@@ -6,6 +6,15 @@ class Ripsecrets < Formula
   license "MIT"
   head "https://github.com/sirwart/ripsecrets.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bd93296a62b2ecd77ffc7a1dccf3796c432037f3cf014f161009c27016074050"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3d9d148980dc49756443f738fbe8e39d247781c8ae1ee5f8891ef0b7d4263216"
+    sha256 cellar: :any_skip_relocation, monterey:       "91cae699291e925c8ac3b0e2e3456a3bc8363429bb3ce45b22f7fd794b83ae57"
+    sha256 cellar: :any_skip_relocation, big_sur:        "28608d5a2f111dce271d708bfff08791f0b7e78aaf33f3b5ac62fbebf57282c0"
+    sha256 cellar: :any_skip_relocation, catalina:       "4d2dde370ee2f884d1e4329b37fad76b0b31522622e66de69ba83170bb14998d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "92c2f7acad2bdb500f44166c6ad4a7cf9ea8eb3c3f9f56d294d190e8af2338f0"
+  end
+
   depends_on "rust" => :build
 
   def install
