816ca5be6ff9507fc6f318f2ad0d94dbc94911b5
Pierre Wendling <pierre.wendling.4@gmail.com>
2022-08-19 14:38:15 -0400

    sdl2_mixer 2.6.2
    Closes #108468.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sdl2_mixer.rb
+++ Formula/sdl2_mixer.rb
@@ -1,8 +1,8 @@
 class Sdl2Mixer < Formula
   desc "Sample multi-channel audio mixer library"
   homepage "https://github.com/libsdl-org/SDL_mixer"
-  url "https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.1/SDL2_mixer-2.6.1.tar.gz"
-  sha256 "bc5c7b85f297e5f3cb34c99c366a4746922fc41365b15fd78a164dda71a861a3"
+  url "https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.2/SDL2_mixer-2.6.2.tar.gz"
+  sha256 "8cdea810366decba3c33d32b8071bccd1c309b2499a54946d92b48e6922aa371"
   license "Zlib"
 
   # This formula uses a file from a GitHub release, so we check the latest
