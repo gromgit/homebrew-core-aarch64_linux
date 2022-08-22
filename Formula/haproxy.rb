20a318ab7038c79003acfdbf85b483aaeb629424
Porkepix <porkepix@gmail.com>
2022-08-23 00:09:16 +0200

    haproxy 2.6.4
    Closes #108641.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/2.6/src/haproxy-2.6.3.tar.gz"
-  sha256 "d18f7224a87b5cd6bbabb04d238f79a79fa461f0f8e1f257575cef8da2a307d9"
+  url "https://www.haproxy.org/download/2.6/src/haproxy-2.6.4.tar.gz"
+  sha256 "f07d67ada2ff3a999fed4e34459c0489536331a549665ac90cb6a8df91f4a289"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   livecheck do
