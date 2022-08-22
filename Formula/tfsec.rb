a847e51401ecde29336b1e209bb8a06c964a4846
Porkepix <porkepix@gmail.com>
2022-08-22 15:39:04 +0200

    tfsec 1.27.5
    Closes #108614.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/aquasecurity/tfsec/archive/v1.27.4.tar.gz"
-  sha256 "aa20296840d561e6cb15df92aff812fa6210ccb6c9317c10015e6f9ce4e06201"
+  url "https://github.com/aquasecurity/tfsec/archive/v1.27.5.tar.gz"
+  sha256 "bfa657d38946d48a4b23acc9698b1148250fe344159a5af31037892e6bbe8e63"
   license "MIT"
   head "https://github.com/aquasecurity/tfsec.git", branch: "master"
 
