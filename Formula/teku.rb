f6b8bd3fbc2179c90ffb3ead77580d76dcd7056b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-22 05:56:09 +0000

    teku 22.8.1
    * teku 22.8.1
    * update teku.rb
    
    Closes #108589.
    
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/teku.rb
+++ Formula/teku.rb
@@ -2,8 +2,8 @@ class Teku < Formula
   desc "Java Implementation of the Ethereum 2.0 Beacon Chain"
   homepage "https://docs.teku.consensys.net/"
   url "https://github.com/ConsenSys/teku.git",
-        tag:      "22.8.0",
-        revision: "a9f80cb9f757a3ad68a8081dd06b32925a29ba34"
+        tag:      "22.8.1",
+        revision: "ebfb0449d245df5ef09ebf5954122985fb8c139c"
   license "Apache-2.0"
 
   bottle do
@@ -26,7 +26,7 @@ class Teku < Formula
 
     rest_port = free_port
     fork do
-      exec bin/"teku", "--rest-api-enabled", "--rest-api-port=#{rest_port}", "--p2p-enabled=false"
+      exec bin/"teku", "--rest-api-enabled", "--rest-api-port=#{rest_port}", "--p2p-enabled=false", "--ee-endpoint=http://127.0.0.1"
     end
     sleep 15
 
