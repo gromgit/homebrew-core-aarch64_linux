4ddf92a714bea0083dffa50f711dca3d8dc6e5da
David Muckle <dvdmuckle@dvdmuckle.xyz>
2022-08-22 19:24:35 -0400

    mpd 0.23.9
    * mpd 0.23.9
    * Remove no longer necessary patches
    * mpd: remove embedded patch
    * Update shasum
    
    Closes #108656.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -1,10 +1,9 @@
 class Mpd < Formula
   desc "Music Player Daemon"
   homepage "https://www.musicpd.org/"
-  url "https://www.musicpd.org/download/mpd/0.23/mpd-0.23.8.tar.xz"
-  sha256 "86bb569bf3b519821f36f6bb5564e484e85d2564411b34b200fe2cd3a04e78cf"
+  url "https://www.musicpd.org/download/mpd/0.23/mpd-0.23.9.tar.xz"
+  sha256 "2becaba980402e8dc7972ccc3476e493b7ae2eeb720d31fa6712472ed48e9f2d"
   license "GPL-2.0-or-later"
-  revision 1
   head "https://github.com/MusicPlayerDaemon/MPD.git", branch: "master"
 
   livecheck do
@@ -55,29 +54,11 @@ class Mpd < Formula
 
   fails_with gcc: "5"
 
-  # Fix build with FFmpeg 5.1. Backported from
-  # https://github.com/MusicPlayerDaemon/MPD/commit/59792cb0b801854ee41be72d33db9542735df754
-  patch :DATA
-
   def install
     # mpd specifies -std=gnu++0x, but clang appears to try to build
     # that against libstdc++ anyway, which won't work.
     # The build is fine with G++.
     ENV.libcxx
-
-    # Replace symbols available only on macOS 12+ with their older versions.
-    # https://github.com/MusicPlayerDaemon/MPD/issues/1580
-    #
-    # This workaround can be removed when the following commit lands in a tagged release (likely 0.23.9):
-    # https://github.com/MusicPlayerDaemon/MPD/commit/bbc088ae4ea19767c102ca740765a30b98ffa96b
-    if MacOS.version <= :big_sur
-      new_syms = ["kAudioObjectPropertyElementMain", "kAudioHardwareServiceDeviceProperty_VirtualMainVolume"]
-      # Doing `ENV.append_to_cflags` twice results in line length errors.
-      new_syms.each do |new_sym|
-        old_sym = new_sym.sub("Main", "Master")
-        ENV.append_to_cflags "-D#{new_sym}=#{old_sym}"
-      end
-    end
 
     args = %W[
       --sysconfdir=#{etc}
@@ -147,24 +128,3 @@ class Mpd < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/decoder/plugins/FfmpegIo.cxx b/src/decoder/plugins/FfmpegIo.cxx
-index 2e22d9599102ac445fc269c69863f4c2c34bfe1c..5b5c8b40e3a0b95fbf5e75a0cf9b53e2c416a36f 100644
---- a/src/decoder/plugins/FfmpegIo.cxx
-+++ b/src/decoder/plugins/FfmpegIo.cxx
-@@ -21,10 +21,13 @@
- #define __STDC_CONSTANT_MACROS
- 
- #include "FfmpegIo.hxx"
--#include "libavutil/mem.h"
- #include "../DecoderAPI.hxx"
- #include "input/InputStream.hxx"
- 
-+extern "C" {
-+#include <libavutil/mem.h>
-+}
-+
- AvioStream::~AvioStream()
- {
- 	if (io != nullptr) {
