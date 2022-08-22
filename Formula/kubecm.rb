a351aef343fbda4427f9212c960ebbbb15236dd8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-22 02:53:21 +0000

    kubecm: update 0.19.2 bottle.

--- Formula/kubecm.rb
+++ Formula/kubecm.rb
@@ -6,12 +6,12 @@ class Kubecm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fe875634f09789e9758d927599cd44185117b3fbebdfb9b2342b61a5411dc3bf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "18c9914983f0b92d51aa6f17d0cc9c445df7c15871b732c06a8af3dd339cfd51"
-    sha256 cellar: :any_skip_relocation, monterey:       "8eaa2c9ae0c844c31ce5714f89e676a231210e9a26b746d5f9a30691a2f85df6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e566971a6b9ce9b93e6543d00055e65d2819d040a7c965fe0f9428bb11e0c879"
-    sha256 cellar: :any_skip_relocation, catalina:       "25a6c0a0010aa7b3612f9d8ae90a58d95f09a146dd6a793faefad50a59adfac0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "73ddbeb43cb607abc92102242ec61ca9712adb8708b212497b03f7e75b854eeb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8f7e70eb9fe80f775b84330b4514c138c605006ece4be20c59b1aecf47c3dfde"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a02ef59053b51f47d78669e167c48a12bf2e3036f86571e0f7a9ab709036b752"
+    sha256 cellar: :any_skip_relocation, monterey:       "570c3cb5c09817f65d89d567b14812d10803146f1697f536f5cc1c2ef246eef5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "798aaeb99741e0accc82a3d4dab9fbda9c06e5c4de83cc131948c89ac3851b11"
+    sha256 cellar: :any_skip_relocation, catalina:       "673f37a9fc7e0fefe7595d10b4b479aa924c788bd1f84f575ec2657bb2d38742"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52cd70a0806e4a5cfecb2ab8f359cc9facb4a813348cf7b1e25e9993a22b10e4"
   end
 
   depends_on "go" => :build
