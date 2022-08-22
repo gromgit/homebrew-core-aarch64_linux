638c203439d5f25d5e9177a6dee156bf1804b1a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-22 11:04:57 +0000

    prr: update 0.5.1 bottle.

--- Formula/prr.rb
+++ Formula/prr.rb
@@ -7,12 +7,12 @@ class Prr < Formula
   head "https://github.com/danobi/prr.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7c6547be0918cb4bf48a65ebabe4676f195054b55a483f62f09f012f759c428c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c983c6f377274015d35e6e5d1c6293c2181feebe2f00d57135dec566dd4ba7c"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b0e195a99a1e888d178e7fa2ff524113bdb419c213f1d8afe3dc764e217f3fa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5d3d253a2498a52df17d0c0fb83daf024ef5f9199727d9cb83f27551e309d01c"
-    sha256 cellar: :any_skip_relocation, catalina:       "7604a13cf21a13c66dceb07bb3b6d7fcb5c6501a8c602c72543525c08e650e56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ecaa3243521d2b217fdfdf2c6bfc3e9609f26131aaee4d0aec8a77577403612"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "493114022fa2da300e062dc36f47d6ac315455c0f840aa9a18299103b85205f7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "edc44bf9d78e0e61c7519a226f987b9826163eefcb9481af83c689d32906e3c9"
+    sha256 cellar: :any_skip_relocation, monterey:       "c29898cd94bdc158267567b3628382c0a062e78663cadc2f207c1ef8e7720c63"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f5b7b0cceaef4516a76982fda2fb09c97eff6b9e317a4f4e721504ad54bf5775"
+    sha256 cellar: :any_skip_relocation, catalina:       "07f21c7b2328d65c1075d0706b62dc7f452704e60e528124d8611c55d13b67e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f33d3c5149712d7b33d8286e14e279bc4575a81e615eff727233b4c5da09e3aa"
   end
 
   depends_on "rust" => :build
