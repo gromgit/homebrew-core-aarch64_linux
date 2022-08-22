0b215f885adc2535e5f2c3fcfb96e9adb17afd39
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-22 19:04:32 +0000

    elixir: update 1.13.4_1 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -8,12 +8,12 @@ class Elixir < Formula
   head "https://github.com/elixir-lang/elixir.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "463486dc9feeae42feedc2081d5d8a4453049ccc9378ff7d499d69e74f5d9e5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "53f1cc0ba551cf479e8a94bf37f8a9cb81fbe947b167f01eac720e6e77837be5"
-    sha256 cellar: :any_skip_relocation, monterey:       "34042b6dffc153355879c4f7e626729cf3d26e4b4ca2cafead11c27fe94ce50a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc58982b64740829904dbf23e621998bc29df54fc46275532803dca28f56921f"
-    sha256 cellar: :any_skip_relocation, catalina:       "11560072909428556a398a7578485821f678f1faf85f9ca94ff48fc400c013c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "269ed963791ded716de20ad620a03a12bfb70fc9d44c61e37cb7ebd881407dd8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "18301739c2d532906881333d0f0c4a1435cd7f928774514fe2f74a07a8ea5ba5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6bfc7700ab8b3940c8f3a64305e66b9eae45836f905f54adf32af1e57baecbba"
+    sha256 cellar: :any_skip_relocation, monterey:       "98449564d88814d1c5520dcee22ac106d8bc0b2656f18b566b7f1bd791ad54e9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "667216b6efffc119f51a161ec71ff7bd85a52166786a653c5717749ba9db042f"
+    sha256 cellar: :any_skip_relocation, catalina:       "79f0d91769a100fcd667ea8b5a64a99698191483cccc98efe1bf85aee4bf3488"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88f963206774ba8f59448dfd20d8bbe33504a8f4249cbf0b2e756ac66bdbb587"
   end
 
   depends_on "erlang"
