1d11727b8ae9a38fc62ff73b3e21dc5d95eb9849
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-22 09:31:45 +0800

    emacs-clang-complete-async: revision bump (llvm 15.0.0)

--- Formula/emacs-clang-complete-async.rb
+++ Formula/emacs-clang-complete-async.rb
@@ -2,7 +2,7 @@ class EmacsClangCompleteAsync < Formula
   desc "Emacs plugin using libclang to complete C/C++ code"
   homepage "https://github.com/Golevka/emacs-clang-complete-async"
   license "GPL-3.0"
-  revision 6
+  revision 7
   head "https://github.com/Golevka/emacs-clang-complete-async.git", branch: "master"
 
   stable do
