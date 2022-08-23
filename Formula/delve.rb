39801df94a7fa254da170ae1c58fbd06881e86b1
Rui Chen <rui@chenrui.dev>
2022-08-23 16:39:47 -0400

    delve 1.9.1
    Closes #108713.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/delve.rb
+++ Formula/delve.rb
@@ -1,8 +1,8 @@
 class Delve < Formula
   desc "Debugger for the Go programming language"
   homepage "https://github.com/go-delve/delve"
-  url "https://github.com/go-delve/delve/archive/v1.9.0.tar.gz"
-  sha256 "daec5909ce6e44b7b2c49eea90b4aa964d2ff96c1e875b09891a26546f9ae871"
+  url "https://github.com/go-delve/delve/archive/v1.9.1.tar.gz"
+  sha256 "d8d119e74ae47799baa60c08faf2c2872fefce9264b36475ddac8e3a7efc6727"
   license "MIT"
 
   bottle do
