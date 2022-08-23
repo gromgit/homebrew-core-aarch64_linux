922f09eb9c01abc87921453634159c5cc30af531
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-23 05:28:53 +0000

    pandoc: update 2.19.2 bottle.

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -7,12 +7,12 @@ class Pandoc < Formula
   head "https://github.com/jgm/pandoc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "446141960ac626b10762f6b6534c4ba8467eb851ccb40f1e69bc900b16b7ce3b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d03001f8a238a2eb21523a8ec33b305aaa26b30cf68aaf5f7c5a99ccad7c4c8c"
-    sha256 cellar: :any_skip_relocation, monterey:       "de7dd6cfd6e5a21d169ea3a78a7f87fe4f0f8f1c611497c9693c7603d18818dd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "72388faa56c673946a087fc488e170426e51528b3688636979d8532554bbd665"
-    sha256 cellar: :any_skip_relocation, catalina:       "e876f14ff71a12c826bee2e0261a5dd44016a3a4a2a9c92f7cbd8ac2f2152ef6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a18ccee7c9315691d44f749b252e5614071541769e1a7d473e68626062fcffd5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "62c83be98424758ef00e8536944ec94a0b8d824fddccc11c257020e3ead5a0c0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9de935646243eaca586584f3d30f93378e4adbb51008c7c25e7582ab0bebcd5d"
+    sha256 cellar: :any_skip_relocation, monterey:       "dff07e47cfdaedef4512aa7f12f45c449b8463ec3a296abc9b5f9b3b95945440"
+    sha256 cellar: :any_skip_relocation, big_sur:        "df691fef798f877b42e8eda10d0a8ede824dff72af31702bd002942ca08a9188"
+    sha256 cellar: :any_skip_relocation, catalina:       "0f636ddefda36e965678d5e1f570b1396a18aa8d0fb01bd3daf85dc7a727630c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e79774dabf337a91162206aa53b008b205cf55511366c9784adb68bdfaafff5"
   end
 
   depends_on "cabal-install" => :build
