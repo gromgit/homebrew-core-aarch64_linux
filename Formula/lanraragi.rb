2dab0958c5fc2fe9efd7340c71d7baca51f57ac3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-23 12:04:32 +0800

    lanraragi: revision bump for macOS libffi

--- Formula/lanraragi.rb
+++ Formula/lanraragi.rb
@@ -6,7 +6,7 @@ class Lanraragi < Formula
   url "https://github.com/Difegue/LANraragi/archive/v.0.8.5.tar.gz"
   sha256 "2cf53f7405a6a4e16e6d61a1109bdd87c25471c2ee6cf2fb79129c7f5666fa31"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/Difegue/LANraragi.git", branch: "dev"
 
   bottle do
