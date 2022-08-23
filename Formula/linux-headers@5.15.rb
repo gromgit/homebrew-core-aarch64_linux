cc998a843cdd3180971438d9660b557c014df390
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-08-23 02:45:28 +0000

    linux-headers@5.15: add 5.15.57 bottle.

--- Formula/linux-headers@5.15.rb
+++ Formula/linux-headers@5.15.rb
@@ -5,6 +5,10 @@ class LinuxHeadersAT515 < Formula
   sha256 "d33d0e62f05d90d579eb0cc51343f48c32eb11e4fef0306c374b908212f51578"
   license "GPL-2.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "adb558844cfbf7fb8af6bd8134bc61120218246bd40b4746250fba1ae0a7933a"
+  end
+
   keg_only :versioned_formula
 
   depends_on "rsync" => :build
