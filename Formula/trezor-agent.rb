4b8476872437fb101d5795b2256d721dd84a5493
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-24 10:46:15 +0800

    trezor-agent: revision bump for macOS `libffi`
    Needed after #108307.
    
    Closes #108756.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trezor-agent.rb
+++ Formula/trezor-agent.rb
@@ -6,7 +6,7 @@ class TrezorAgent < Formula
   url "https://files.pythonhosted.org/packages/f1/a7/8989377dfce48abf9115055baf6d2cfba55102929a7c5f3e5b9c8e8b5c18/trezor_agent-0.11.0.tar.gz"
   sha256 "139d917d6495bf290bcc21da457f84ccd2e74c78b4d59a649e0cdde4288cd20c"
   license "LGPL-3.0"
-  revision 4
+  revision 5
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "a3c0407e4a2171cb1001c39af67ff449963eccef917469b5051feecba3a42bc5"
