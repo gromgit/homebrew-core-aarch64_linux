ae9a3a8293426b04ee5f804de780aa6b0095e9b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-24 11:04:31 +0000

    ioping: update 1.3 bottle.

--- Formula/ioping.rb
+++ Formula/ioping.rb
@@ -7,14 +7,12 @@ class Ioping < Formula
   head "https://github.com/koct9i/ioping.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "152051de44d80aee0e50f7d96b0a548335150e40bd0ca176bfbcda12ec785846"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8b319c5bec1083064f584e6760a1db5a93e588e64983f0306e474ae277357a7"
-    sha256 cellar: :any_skip_relocation, monterey:       "a13430f2776847bbe161f5a68267cc39de49b819cb72076dae13a04c6923113e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5a4e1fd55d87324605c6af3b70fecb3780ab00ddea13fc058a7911688014ef35"
-    sha256 cellar: :any_skip_relocation, catalina:       "622678afe9bf88bae08cc264dece76f0abefc854915f5b3d5355cde767aa61e1"
-    sha256 cellar: :any_skip_relocation, mojave:         "4c88038d68f17bbc405c5ed253542890e0fc1e44ece8650f1a68b6ff6df7fabf"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9a5ee7cd526c89d70c75fe6fcf61d7b0a777d8bf3a823fe99348864a9838b6ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "821ef4ae7a4bb2a8a26f6b812556cbd3f079fc4bd87eeca9303d1dec1f8bc4dc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c80c269c2105571ae9ea183372a238c784553652783ea417365010422dd1b2cc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f3560ce6ce206bb2e7296eca549d45714a945dc001a595dae743f6cb9b3120cd"
+    sha256 cellar: :any_skip_relocation, monterey:       "b13267eb009500e2ecd3655390e1b39c0083ef38b4cc4945be7a0dfb7fe12746"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ca6704be7e6eb5d2e774cebd8afbf125e86f4bf08c9c0ab1140ae283b3bd9cdf"
+    sha256 cellar: :any_skip_relocation, catalina:       "6a7926f6e4b0e04d4ba5382f63c3a2434b5744901c26ad865544d887fc888145"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19e6f04bd51ce45365b256877f1b894023415a07f8966bab528c9eeba9feacb9"
   end
 
   def install
