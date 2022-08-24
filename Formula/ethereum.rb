f6dd7db03808b2ca73d47278ec9cebfb6999f5b4
Rui Chen <rui@chenrui.dev>
2022-08-24 17:19:53 -0400

    ethereum 1.10.23
    Closes #108814.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ethereum.rb
+++ Formula/ethereum.rb
@@ -1,8 +1,8 @@
 class Ethereum < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://geth.ethereum.org/"
-  url "https://github.com/ethereum/go-ethereum/archive/v1.10.22.tar.gz"
-  sha256 "1c22605fd457efae4f683bf0b59e110ed69496bb9191f5920f353373cd8a7760"
+  url "https://github.com/ethereum/go-ethereum/archive/v1.10.23.tar.gz"
+  sha256 "44d89b50a9cce9198aee8b2f10692949ef9b477b315f18bbd7e89b084416f4de"
   license "LGPL-3.0-or-later"
   head "https://github.com/ethereum/go-ethereum.git", branch: "master"
 
