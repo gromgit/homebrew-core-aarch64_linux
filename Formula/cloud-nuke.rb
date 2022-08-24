a0e5050e7030c0fd899ced78e145c21cad49ba2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-24 02:54:13 +0000

    cloud-nuke: update 0.19.0 bottle.

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -7,12 +7,12 @@ class CloudNuke < Formula
   head "https://github.com/gruntwork-io/cloud-nuke.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8df8d54e26e42c5c06653be854d2558119afa0bc3b84f3c03d34d0441ebd7e0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a3c8c54c4c0e0a85ab880f20cd9d0856b29182c62645c98fafb33a8cf263ce9"
-    sha256 cellar: :any_skip_relocation, monterey:       "18b7cec0e8e456f7a846ab55a641de3b1c00660d4338eb84d5e3adb6e7be7fa1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "634688caa9257fd71370e6394deca1e68d25ee9f614b894794f27fc437084e17"
-    sha256 cellar: :any_skip_relocation, catalina:       "a21624f32359c0e58b1296d2e825cfbed042799addadcc85ab3141445c7305c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b2bbb8af379688425bfa124c537f271f7360567eff8c5a7dfab6a54f94c9c14"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "74fac28f159626c752c869126ed96fb10687cfb5014c68eacdb716ceb289484e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "270de53932bebd75b7799725b5402127465b721f23bbd6fc41a5e9e2d150e1b9"
+    sha256 cellar: :any_skip_relocation, monterey:       "c9266c9bfc03539a2650796eab44f3c4d23f7c2dac112505740868e05f191583"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ba163701443c80783624937c334fb76072f22edf98d70a62ed0c381313919ac1"
+    sha256 cellar: :any_skip_relocation, catalina:       "8d202bed099a1c12199f0e379db6043ada4885d8de942091793cd0c9c79e5347"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e15f0c18c3f484e52f21c47691b919277008ed27d8930019d5020015582a113a"
   end
 
   depends_on "go" => :build
