192bb7f4962d64c2606392e04c4c2d57bb07f24c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-24 13:40:58 +0000

    hunspell: update 1.7.1 bottle.

--- Formula/hunspell.rb
+++ Formula/hunspell.rb
@@ -6,14 +6,12 @@ class Hunspell < Formula
   license any_of: ["MPL-1.1", "GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "057006a24f29ef2568bf8254c8514cef3e34f34f2d6b32e2c0b7512dc07bc89f"
-    sha256 cellar: :any,                 arm64_big_sur:  "8d48ffd83009503b5a2c1b968ddd930e80f41731e02576d6dfea2f53fc50d97c"
-    sha256 cellar: :any,                 monterey:       "ff7ca6c9c4cfa932414609b4b68c0adddc8816804910658deccd8796e7a7ee85"
-    sha256 cellar: :any,                 big_sur:        "20b0f5dc7973a4cca3a8c3ea5778938d169bde2d183210f64526df8bca9512f7"
-    sha256 cellar: :any,                 catalina:       "d5144178eba9ff325c297a5a0ae05f1caa9b1d567803250d5dbd86876e0718a3"
-    sha256 cellar: :any,                 mojave:         "eaabf6b66c21a94a7f6810287ff84e83fdef0ed6d3db635c8ad2c810d9f84d46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b57b04052406ed148c7c44e045586787293ecc9c27701e2e9c6308cdd63f3b9"
+    sha256 cellar: :any,                 arm64_monterey: "e2b55b5d8ba672d91525b15ef165eec3bc1292e1e46ee9dbf01a069eda621a4d"
+    sha256 cellar: :any,                 arm64_big_sur:  "849e8289515dc32df0e28fb66371dbb6db15d4b283f970bc1fb5067c2d9bf225"
+    sha256 cellar: :any,                 monterey:       "134ffae149cb7cef8adbf76a165219a88ba0a9057b64ed03cee8cbe22444c004"
+    sha256 cellar: :any,                 big_sur:        "b4f0e1ab5c6df7381d0b62a746e40721fc432cafcba9b2d35eb4178e0f7ecb27"
+    sha256 cellar: :any,                 catalina:       "1d472e55eb42c1d0029fb306813f79593b7a7924d8180613e19fd45d402bc161"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "687239fa7f643f582bd3bc61fa0b35eb0821ab762ea92ab5f05a625e4d64e1f4"
   end
 
   depends_on "gettext"
