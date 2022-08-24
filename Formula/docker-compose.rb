f5ed7d4387e23cb71a9c576bc6ce9f28165fd3c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-24 20:04:53 +0000

    docker-compose: update 2.10.1 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -7,12 +7,12 @@ class DockerCompose < Formula
   head "https://github.com/docker/compose.git", branch: "v2"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b9efd7c510de3ccb1e4417c02543518c1e104def1dc7fe3add8fdb604522c787"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b9efd7c510de3ccb1e4417c02543518c1e104def1dc7fe3add8fdb604522c787"
-    sha256 cellar: :any_skip_relocation, monterey:       "2e93919f4e070a6cf792e3d47f0224dcaa328e2844b7c82f5a8ab0e013891904"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2e93919f4e070a6cf792e3d47f0224dcaa328e2844b7c82f5a8ab0e013891904"
-    sha256 cellar: :any_skip_relocation, catalina:       "2e93919f4e070a6cf792e3d47f0224dcaa328e2844b7c82f5a8ab0e013891904"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9c6d73a22c49facdfeae939c6ce1130258e1d1afada77c1872e52166d9cbb0f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "99d746a2e370718b6a6947bdcdcf275aae79f1866c70d7410e1b5a5f673f507b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99d746a2e370718b6a6947bdcdcf275aae79f1866c70d7410e1b5a5f673f507b"
+    sha256 cellar: :any_skip_relocation, monterey:       "e7f01edca79e860b97bfce9597ee2ce520da84c20c0e083a81d4b525cfb0af22"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e7f01edca79e860b97bfce9597ee2ce520da84c20c0e083a81d4b525cfb0af22"
+    sha256 cellar: :any_skip_relocation, catalina:       "e7f01edca79e860b97bfce9597ee2ce520da84c20c0e083a81d4b525cfb0af22"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0158d66ca49f4568c7e55949323ae9030656377411cec22e2602ee83cd3f067e"
   end
 
   depends_on "go" => :build
