835214e9c4c841b8ab1329b5bd1809b3422a824f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-25 15:24:27 +0800

    arrayfire: use `rpath` DSL
    `rpath` can now passed arguments to compute the correct `RPATH` entry on
    both macOS and Linux.
    
    See Homebrew/brew#13722.
    
    Closes #108857.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arrayfire.rb
+++ Formula/arrayfire.rb
@@ -29,15 +29,19 @@ class Arrayfire < Formula
 
   def install
     # Fix for: `ArrayFire couldn't locate any backends.`
-    if OS.mac?
-      ENV.append "LDFLAGS", "-Wl,-rpath,@loader_path/#{Formula["fftw"].opt_lib.relative_path_from(lib)}"
-      ENV.append "LDFLAGS", "-Wl,-rpath,@loader_path/#{Formula["openblas"].opt_lib.relative_path_from(lib)}"
-      ENV.append "LDFLAGS", "-Wl,-rpath,@loader_path/#{(HOMEBREW_PREFIX/"lib").relative_path_from(lib)}"
-    end
+    # We cannot use `CMAKE_INSTALL_RPATH` since upstream override this:
+    #   https://github.com/arrayfire/arrayfire/blob/590267d2/CMakeModules/InternalUtils.cmake#L181
+    linker_flags = [
+      rpath(source: lib, target: Formula["fftw"].opt_lib),
+      rpath(source: lib, target: Formula["openblas"].opt_lib),
+      rpath(source: lib, target: HOMEBREW_PREFIX/"lib"),
+    ]
+    linker_flags.map! { |path| "-Wl,-rpath,#{path}" }
 
     system "cmake", "-S", ".", "-B", "build",
                     "-DAF_BUILD_CUDA=OFF",
                     "-DAF_COMPUTE_LIBRARY=FFTW/LAPACK/BLAS",
+                    "-DCMAKE_SHARED_LINKER_FLAGS=#{linker_flags.join(" ")}",
                     *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
