bd133d03d0405d6885e891feb46e2cd7eeee044c
chiefmikey <wolfemikl@gmail.com>
2022-08-25 07:24:23 -0600

    jackett 0.20.1796
    Closes #108886.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jackett.rb
+++ Formula/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.1759.tar.gz"
-  sha256 "6b55f664aefabcb520b1ceea1c62747a16f0528af5f6a25bee0d91ab96f933b7"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.1796.tar.gz"
+  sha256 "d3c4a274fc6508af0c8ef91967b9ba9fd853f039a6e75486557c213875f49458"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
