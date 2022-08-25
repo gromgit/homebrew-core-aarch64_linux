a6ca0deb4375420a7826a540ba804fbb0161f2f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-25 14:06:48 +0000

    cmark-gfm: update 0.29.0.gfm.5 bottle.

--- Formula/cmark-gfm.rb
+++ Formula/cmark-gfm.rb
@@ -7,12 +7,12 @@ class CmarkGfm < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3fce983843324792b1a4df8be5b242fc0cee23200e7015141bf12ef36ca5f2a9"
-    sha256 cellar: :any,                 arm64_big_sur:  "ddf818d97cc545f1c13b7613109f45786a3c2b653b2b148b36ecb40fb48af04e"
-    sha256 cellar: :any,                 monterey:       "dc4f61c2d6e50fc750f3b82404bda9bc4ce9f47f6415722b78dc75cf8216b77d"
-    sha256 cellar: :any,                 big_sur:        "6f342b5088afeeeee23b0ea4fed9bc0ef4f46ad5532985fe93a2c131c37c9d2c"
-    sha256 cellar: :any,                 catalina:       "8c99f03ce18b3c4fdc9e836e8c3fc1054883c9fd7fd4f98be1e87d2b09a86c42"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9c19ca94d92227351f56497a01faa27f20a30cdbe04a8c8636f6d55da0a044e"
+    sha256 cellar: :any,                 arm64_monterey: "e319253d9762d75a1d72ba449cedd86a1a2362568b10dd91fdc0b7d60ba7435d"
+    sha256 cellar: :any,                 arm64_big_sur:  "30b8c1e28d62302b0b91d260315bd4208ece7edb582ba9cb1ee90ed32df44ea3"
+    sha256 cellar: :any,                 monterey:       "c6c52d9b82aacdcb35dd8e92ac20815c419c15a648bcc4ff9345c43346c8f93c"
+    sha256 cellar: :any,                 big_sur:        "27fa10766c92a82c2fc81485c21ebd2ba37da3730fd3032d4eb51b71eb005d64"
+    sha256 cellar: :any,                 catalina:       "caa4c5bf3a77a079799e330f17af0144dcb9b068f0f5a55978c1142591743ef0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a03572908861ae5eaf0c1e611d68e40c391097917ed8f87287b3adc46f6595a"
   end
 
   depends_on "cmake" => :build
