0da7e2eb1fc7db65b04f6e1f8c85a1e879f0c72e
Kid <44045911+kidonng@users.noreply.github.com>
2022-08-25 03:45:08 +0000

    dust: add shell completions
    Closes #108845.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -4,6 +4,7 @@ class Dust < Formula
   url "https://github.com/bootandy/dust/archive/v0.8.2.tar.gz"
   sha256 "890972fbf1a7f0a336c0f20e1e9ecc756c62d3debd75d22b596af993a3d8af01"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/bootandy/dust.git", branch: "master"
 
   livecheck do
@@ -24,6 +25,10 @@ class Dust < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    bash_completion.install "completions/dust.bash"
+    fish_completion.install "completions/dust.fish"
+    zsh_completion.install "completions/_dust"
   end
 
   test do
