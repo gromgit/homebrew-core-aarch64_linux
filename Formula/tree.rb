e72d17d9a30b4ae68f099de04e03caa263d1d2c6
Porkepix <porkepix@gmail.com>
2022-08-27 01:40:43 +0200

    tree 2.0.3
    Closes #109013.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tree.rb
+++ Formula/tree.rb
@@ -1,8 +1,8 @@
 class Tree < Formula
   desc "Display directories as trees (with optional color/HTML output)"
   homepage "http://mama.indstate.edu/users/ice/tree/"
-  url "http://mama.indstate.edu/users/ice/tree/src/tree-2.0.2.tgz"
-  sha256 "7d693a1d88d3c4e70a73e03b8dbbdc12c2945d482647494f2f5bd83a479eeeaf"
+  url "http://mama.indstate.edu/users/ice/tree/src/tree-2.0.3.tgz"
+  sha256 "ba14e77b5f9dc7f8250c3f702ec5b6be2f93cd0fa87311bab3239676866a3b1d"
   license "GPL-2.0-or-later"
 
   livecheck do
