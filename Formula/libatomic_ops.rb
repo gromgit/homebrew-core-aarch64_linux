bd4bd4d7af544bdf3601fdc316a42c97dd61eb66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-26 12:06:40 +0000

    libatomic_ops: update 7.6.14 bottle.

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -11,13 +11,12 @@ class LibatomicOps < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8b1c3b5131abdae9c2df1e915a9128c6e892fe05a5975d5ef2dd2c5c56d946b2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51e94ad755b4f85d03f3ac3d67a089f7fa0588340682838fce8b3ca5de193e81"
-    sha256 cellar: :any_skip_relocation, monterey:       "89a4f8f01bd7c3991eabbdb321dff7395cff0765f600e59dea1ccc6bf95906e7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "017fae77e5ec0d547f9e8fb08cf907af0fbfabd6eeda54d4353a446e37371971"
-    sha256 cellar: :any_skip_relocation, catalina:       "c9e0de613f4d3dbd8f0aa005c5ed038d8054ed87f8d9e27d10d182113227c923"
-    sha256 cellar: :any_skip_relocation, mojave:         "40c602dbee95c6e67b3c0b63f32a3e4ed81e6281941a1cf484fc8a95394f8f1a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e88da0d1511648aae7d26e80745f9a85884ba0a347733ebdbe6cc043eb6708cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2c524cf7b2a2066d9629190b75039283ca008f5a74752cca3b3c7387bed94a6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b6eb5d6a03178df1b704517496c79ddc4ceda06ec3d80e5c1230bd938232e629"
+    sha256 cellar: :any_skip_relocation, monterey:       "a2e24f4fb5d734c6407a87e6d92aad0fa8a6fae7d18c22794661b1f93a6484d7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4b41940cd48c41a9cb04a2ad951430240508c6153c51e97e237091d5618c8f04"
+    sha256 cellar: :any_skip_relocation, catalina:       "cfb78b72dd3fcccea0bb983380505c5b21b55811a9aa81c34996154ce59108b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67609f40170efe9725285e1fa97ae6042acf52eeded974f6e085db707decebd8"
   end
 
   def install
