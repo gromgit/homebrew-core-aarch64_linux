c343719febc4547370ee3e7926068854a2e9df77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-27 17:06:16 +0000

    oha: update 0.5.4 bottle.

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -7,12 +7,12 @@ class Oha < Formula
   head "https://github.com/hatoo/oha.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "57a4a28659eac04151087e0b8f9ef32dd76d7a7fa3b5742ac3f5385fa48eaeb1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4dd3d09328fd92ae234cf25cb0a26e537a6cdf88c59ba2382d0e4ebc98d6f5cd"
-    sha256 cellar: :any_skip_relocation, monterey:       "d3a3a661db004f1bc5a0887d6adc6e864b8187242ea544045567d185574c01ee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b0f45564406275d7edd6e25b794cb10c06a6ff5d26f4842237455df2bf8ed9aa"
-    sha256 cellar: :any_skip_relocation, catalina:       "83d863516ee02e981bb60a31202d93871bca69a67f4190f86e44a2bc59e40f18"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "875570714b58569467f202f6f032f0bb38ce79c09c09a5d120cd04de1ecd4b72"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "05e05f83d3a1fe67503079a84ea0f8651f8debd1f2748f5a776b3d5b64d7a46e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66447693cbdd2ff3aae06724ec22f1c07a2e3b84dac1d81445c4620fc41040db"
+    sha256 cellar: :any_skip_relocation, monterey:       "f6b4215af45315b369bae1a39e2918cd9fc1083c2c6d4d22acb0cad06e5e2a38"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5aad30bfec3ecd0b3db4d6f4d01f6c58632498388d52bc984e1c5e96cbc8ed94"
+    sha256 cellar: :any_skip_relocation, catalina:       "115ca7cc97fcb829e7cf841fd7a199545a2a500faa982305808593efb1cfb6cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b4529ca2e9734d0a2667b63c2a135de5034dfd9bd9a5b8dfa4d672b19125899b"
   end
 
   depends_on "rust" => :build
