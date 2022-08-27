9ce0e21160bfc6e55bdf8e3b30afc861bb19676f
Rui Chen <rui@chenrui.dev>
2022-08-26 22:10:44 -0400

    cilium-cli 0.12.2
    Closes #109018.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -1,8 +1,8 @@
 class CiliumCli < Formula
   desc "CLI to install, manage & troubleshoot Kubernetes clusters running Cilium"
   homepage "https://cilium.io"
-  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "8bba3eadd2ddde5b308f89302e53c37301dbac9131e1e1dedb86fe224556f4a5"
+  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "4700e07b6b437a4b6c88a3993f2641b58d5f6598a2bf62ae5ec87e8ffe3e905f"
   license "Apache-2.0"
 
   bottle do
