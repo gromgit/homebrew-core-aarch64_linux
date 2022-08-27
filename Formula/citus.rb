e4344c861c537f79058efcd2a6a9d5eb5dd0428a
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-08-27 20:24:17 +0000

    citus: update 11.0.6_1 bottle.

--- Formula/citus.rb
+++ Formula/citus.rb
@@ -8,12 +8,12 @@ class Citus < Formula
   head "https://github.com/citusdata/citus.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f7f6ca6058fc3467a5b23712d4fbc39a3bd709b0a918775a91dff2ac5b162448"
-    sha256 cellar: :any,                 arm64_big_sur:  "5b848a29bb3bea79c0c0ab59c06c1b779c6d842a1ce31ee5db693fcacd84dff8"
-    sha256 cellar: :any,                 monterey:       "86cdd94e6cb0f364e24bea191faa0e1879a2270f8b4dd876ccbc6e66b2559d48"
-    sha256 cellar: :any,                 big_sur:        "c34c4867fdd13b9da219096a614b29b6245a90b6d8d0bdbf343345091b428e58"
-    sha256 cellar: :any,                 catalina:       "bb16722b2a08999b842792033a0e4f98701c565fa177f288a0b2959ec4fafb08"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2a469ee4c58763417ecde8b473aade4f7a34c4ce8b05b594d95b568315df1e4a"
+    sha256 cellar: :any,                 arm64_monterey: "8b100d270c5592ff2b31fe164fe52574f92bfde8a0be940033dad3dc7bb46a40"
+    sha256 cellar: :any,                 arm64_big_sur:  "91ff724e488baca4d611e5370ba17e4c809883d0fb6dc270dc296c0e3abe3867"
+    sha256 cellar: :any,                 monterey:       "186d97c45e8918e40feeb65b51ba4562515dc032ddb099ce03b664c4be2e07f8"
+    sha256 cellar: :any,                 big_sur:        "e9237364eb50e0228f5bd699b9dc9a5f1854f94ce3a7b40c43e037cef5ea2b56"
+    sha256 cellar: :any,                 catalina:       "29d036d72f03db70589b736fa5c11759c60fec3b627efbfcf503049864d45a3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b76b4de2742d1664e46b153f20dde07e2bb98914b51b4121fdac7524448cbf21"
   end
 
   depends_on "lz4"
