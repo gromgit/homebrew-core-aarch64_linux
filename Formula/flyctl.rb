40dde84b906f969a4a0ebd7eb8f453754f15159d
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-08-27 17:35:58 +0900

    flyctl 0.0.383
    Closes #109033.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.382",
-      revision: "654d2efee76659dd0d0bf434c9cf16bf720b6b5c"
+      tag:      "v0.0.383",
+      revision: "16a17b209ab416983c70c3e3ab16ae64bf80059a"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
