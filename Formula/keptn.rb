ef28fd00bfbd2f05b329608cd8b4f73306e7530c
Porkepix <porkepix@gmail.com>
2022-08-28 00:32:31 +0200

    keptn 0.18.2
    Closes #109062.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/keptn.rb
+++ Formula/keptn.rb
@@ -1,8 +1,8 @@
 class Keptn < Formula
   desc "Is the CLI for keptn.sh a message-driven control-plane for application delivery"
   homepage "https://keptn.sh"
-  url "https://github.com/keptn/keptn/archive/0.18.1.tar.gz"
-  sha256 "f21ced9d6782b91c7c46f7613d5ac49a1e204f6f50d8dea3a9ec62a7d2694ba0"
+  url "https://github.com/keptn/keptn/archive/0.18.2.tar.gz"
+  sha256 "806b74f1478922b80a184608c1b8e6a52e5c72f064443d10306fde4b5c3d3a60"
   license "Apache-2.0"
 
   livecheck do
