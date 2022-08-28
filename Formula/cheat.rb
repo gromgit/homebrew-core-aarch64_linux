68e5df95e8a7cd7f2f2e75e3a4e68f7be2d72041
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-28 10:05:12 +0000

    cheat: update 4.3.3 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -6,12 +6,12 @@ class Cheat < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "75c5c4b4a534e0f2f66a515832f26716362797741b7f2b10ba2fe9ccd3ba4cb9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6f66cd80b18fec6f7ee048e2f26223ff69f92c8a7c41ed46128aed797079958"
-    sha256 cellar: :any_skip_relocation, monterey:       "418753455cbc979237ff1064db90126f50057839a91ff09aea883f1ece434b0e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5962c9a7260c806d10d6d1390292992ffb6b76998b38a06f2e92005b990ec91b"
-    sha256 cellar: :any_skip_relocation, catalina:       "4a74092dfbbcd49854da13b54fec2e7a4b6b5b886f836601c56181aeb712db85"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c4be83cf164047fa585c0bb63aaef2414540448314d3246a15e1c323bd261a6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "98c84f7314ecfb5ae455be12af26b4483864e5068b3fcc45d0f2cf3d81f449b4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0ef382c3fed925701edfddd8077411534ba8dac27882f1e9fadfecaaa0af57ab"
+    sha256 cellar: :any_skip_relocation, monterey:       "9504bdece78cf2dba163b48913f50768599e0b20feede2aed8d89600a6fc80a4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6ec70300e5d6ab42f15790f210ef06a7ce5a178987fc06a5dbe8bf944ffa41c2"
+    sha256 cellar: :any_skip_relocation, catalina:       "ef88f03232c81382740fcc6516e83622c12f94f054167969f463b5676b85a967"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fcfbbb8a7df772c836af738f0fc8afeb56ea3c2653eaefd8767db02cf95e4a77"
   end
 
   depends_on "go" => :build
