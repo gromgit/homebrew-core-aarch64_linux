2b891cc9b737776b94c1abcfd456781acccacc1b
Porkepix <porkepix@gmail.com>
2022-08-29 01:18:08 +0200

    findent 4.2.1
    Closes #109117.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/findent.rb
+++ Formula/findent.rb
@@ -1,8 +1,8 @@
 class Findent < Formula
   desc "Indent and beautify Fortran sources and generate dependency information"
   homepage "https://www.ratrabbit.nl/ratrabbit/findent/index.html"
-  url "https://downloads.sourceforge.net/project/findent/findent-4.2.0.tar.gz"
-  sha256 "6e6411eecfe6586d991ed7dbeeb47859fcfe3655615619912703da6da6bdc3c2"
+  url "https://downloads.sourceforge.net/project/findent/findent-4.2.1.tar.gz"
+  sha256 "c010ce4d020ec77c055e0dea1f29a2f52815cb4debe052fd559cf09049fac702"
   license "BSD-3-Clause"
 
   livecheck do
