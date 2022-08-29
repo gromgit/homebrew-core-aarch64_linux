1a07db657164471e1ce49eb38fd4d60934793ff3
Porkepix <porkepix@gmail.com>
2022-08-30 00:08:49 +0200

    chezmoi 2.21.1
    Closes #109176.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -2,8 +2,8 @@ class Chezmoi < Formula
   desc "Manage your dotfiles across multiple diverse machines, securely"
   homepage "https://chezmoi.io/"
   url "https://github.com/twpayne/chezmoi.git",
-      tag:      "v2.21.0",
-      revision: "3af5c7fa9c744356ae5032f8a20b0aeaf13f0845"
+      tag:      "v2.21.1",
+      revision: "9f207f4f92429377387177d07e2eb55dcbc5ded5"
   license "MIT"
   head "https://github.com/twpayne/chezmoi.git", branch: "master"
 
