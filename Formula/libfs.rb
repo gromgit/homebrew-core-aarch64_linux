95fad1602abe303270af2413c3b5ce838a999a1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-29 10:05:09 +0000

    libfs: update 1.0.9 bottle.

--- Formula/libfs.rb
+++ Formula/libfs.rb
@@ -6,14 +6,12 @@ class Libfs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "4fd9651bb8810063376f7b5b132fc0d6f8b2df7d5dd49a18ef95a05e4a48ac38"
-    sha256 cellar: :any,                 arm64_big_sur:  "3509ffdca42e13e77c8b0031cef59d661f0f2213ea3367b29ebfe738f4e9a715"
-    sha256 cellar: :any,                 monterey:       "82b91461b6b0de196af6722c83e2c622fe7fb0b36442186cee4b30e344afb67c"
-    sha256 cellar: :any,                 big_sur:        "c12e8b46907935f4b59326c3d98372f9b4bd040e0ceb42af244a909fa5c09e17"
-    sha256 cellar: :any,                 catalina:       "6ccad0f27601c96ffffac5229d5a25c0ece882b0faf626ae115dfd57a1ac09cb"
-    sha256 cellar: :any,                 mojave:         "b8edb6c54600c14a791a33e7997927d394254b1212ea646ac786a667db263921"
-    sha256 cellar: :any,                 high_sierra:    "f62ce9b989c58747a5b5764478cab8acdf021b60d2a6d306547605305ad41f04"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75c0f9fb47e9a8a0f6b633252ba7c907a0eeb5935545f057f343f981d73ced56"
+    sha256 cellar: :any,                 arm64_monterey: "f0bb367c87a5ba7277d18ba85b182f2798fb97c452efb2eb009babfdd9e643ed"
+    sha256 cellar: :any,                 arm64_big_sur:  "cb3721124d0d238ab17cb95b3cfcb0e2f33fcfb108b4d4c1dd7e06bd86c0e8f8"
+    sha256 cellar: :any,                 monterey:       "256188f15600637e5840d3d2a555754909136abcd4526f4bdb6381c8ec69a2e9"
+    sha256 cellar: :any,                 big_sur:        "c763b89eb5b60b5ccd7868d99dc96c02e1d988b86fe0cfdcea1353e60f39a73e"
+    sha256 cellar: :any,                 catalina:       "c164c40a1951e3f30488202d1c872e7c6ed8cf020ff7af0b4d338fceecc00617"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4da549deeaec2724ca7d5ef88df95998aab656a61710ca48eb71a27ddc55a9b8"
   end
 
   depends_on "pkg-config" => :build
