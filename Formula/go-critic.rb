7bed35b0f4108455c8dbd25541c087632b8578af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-29 19:04:44 +0000

    go-critic: update 0.6.5 bottle.

--- Formula/go-critic.rb
+++ Formula/go-critic.rb
@@ -12,12 +12,12 @@ class GoCritic < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "45e767285d0a9ef074588503f5f0df9436a5e249a996f344823f37de0b791081"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "865f8482139ec2583e0d3d6e28d4c86cca00dc5d1b5b0c9a1400773624e0b05d"
-    sha256 cellar: :any_skip_relocation, monterey:       "6e866c19a692a8b2d9d5eda2135acfd0166ddef4ecc35e7db49c727daa71cb1d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cbee92db712e502ec417857befd4ad9ac2d66fe92095dab87acb700d7cf2931c"
-    sha256 cellar: :any_skip_relocation, catalina:       "26959062f1aba68b61b9dd1139b559d61c6bb5a13a28df20fbd0b71ae337f2e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b38fb5d4b775c7376c48e92be3f856d6a44b78860f7c43db46e6928274fd65fd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "46a8fcf4fc017cd4c452980eb25b19f638885aa330709acfa507be54744fb0cc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ac7fe744a03ad47c2606e46bfd2ecf341d4f7348a622630feda47f5b95d28462"
+    sha256 cellar: :any_skip_relocation, monterey:       "3a88b8e86c45ef9b6e4ad3aef49cb3fd9246abfa7dc7df6183abf5a95e4c6824"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8e101c91c8a509a89be4f4392304d2f91d3cd6b9c0cee5da0fb2d0ee4dfd8214"
+    sha256 cellar: :any_skip_relocation, catalina:       "470b539b01aa70d4e1ba0a54325c2e1c30c935fda537dda83938b9e142eddfe5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c1bb31f7fcbd7772dbcdd66ec9ce4ce188e7ef9e689223ebd6ed959951f587f6"
   end
 
   depends_on "go"
