0b472cf728a6bfac84cbcc4911fb32089335f4a6
Porkepix <porkepix@gmail.com>
2022-08-30 00:26:26 +0200

    gum 0.5.0
    Closes #109181.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gum.rb
+++ Formula/gum.rb
@@ -1,8 +1,8 @@
 class Gum < Formula
   desc "Tool for glamorous shell scripts"
   homepage "https://github.com/charmbracelet/gum"
-  url "https://github.com/charmbracelet/gum/archive/v0.4.0.tar.gz"
-  sha256 "504a92791dacaa06e025a7fea32f96f9d4f67b26a38b1a07eb2703e5519cea1b"
+  url "https://github.com/charmbracelet/gum/archive/v0.5.0.tar.gz"
+  sha256 "0f8e071cc3610c641cc0539f7feb9652b0e8a1fa8f7409c81625cbe71027f28e"
   license "MIT"
   head "https://github.com/charmbracelet/gum.git", branch: "main"
 
