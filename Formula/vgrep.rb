f09099d38d12741db41cd8a84c8937e71c353a72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-29 15:04:55 +0000

    vgrep: update 2.6.1 bottle.

--- Formula/vgrep.rb
+++ Formula/vgrep.rb
@@ -19,11 +19,11 @@ class Vgrep < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ba320fea76d46597b5cb1d750ad03e56d1e8481264f12f906ca33092d7f3fb0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d0d068dfb51958e4327d4b453e9effda29915a8902e8c2aefef2324751b25af"
-    sha256 cellar: :any_skip_relocation, monterey:       "c59e5f9cdc3b7f49631f85ed5e24f4ac6e6e9aa31b950a572d33081a84415ece"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2ee5a3d20e1d70cf4e0de278b8fa4149ca8195a3fd1afb0eb39f463c7a6f9868"
-    sha256 cellar: :any_skip_relocation, catalina:       "199a9605dfc3964c6ccdd94bd4c2f1b7e4b4fba2850a935a7aa31c87879f5ed3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "535140983985f51fc0aa1d4f0e2536596d8f6f621b8b051525d9e1a5297eac4a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d636d0c3519f258615c5bfe6c1c5d19287e82673df0f4f9b7537a58fa255a5dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "955f7b40061f709f7b6d508776ac670e5ec9a41609fd1cabae130260d2de8f6a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d797ec24b06eaebcdeda87a6b1e8880f0f067428f6f35943fe0134ec4392ec95"
+    sha256 cellar: :any_skip_relocation, catalina:       "df09022412dbf0ede1c3680e73466bb848c5383ea0a9e2d57476805d9d266248"
   end
 
   depends_on "go" => :build
