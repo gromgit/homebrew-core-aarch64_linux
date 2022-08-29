717df8cb78c377a69730e41aae36c7b8eda6b21e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-29 20:05:01 +0000

    dprint: update 0.31.1 bottle.

--- Formula/dprint.rb
+++ Formula/dprint.rb
@@ -7,12 +7,12 @@ class Dprint < Formula
   head "https://github.com/dprint/dprint.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a034519f7bee3f403e9d087b2c3989efa92240c20b283a8c9a3fb435a185e51"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0d0ef65e7e2b36ad6f0b6d01e80e6a1fea1f51fa589083e85f542c103f79a18c"
-    sha256 cellar: :any_skip_relocation, monterey:       "934abdf59f15c4fa2c48007d36fd0529300faa4a5eeb3448f27e9323400f6419"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3d5b978204a48d50eff7d31a3c9b1509c007db631597acae3238b66b77831e12"
-    sha256 cellar: :any_skip_relocation, catalina:       "f469e72e2737509c009e72a1b88e1d6455f7e023ebb1caf85fc1b4f8705a6380"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52597aac1e79e541e8baf75d6460796a4ce7bfdde54338340e3976de5304c203"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7e66e41e474cab896dc6f5489683d16283e836d1c7b7abfc95dde0406baf1969"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1de0e67330c87448e700857b3eb75170f5f2be8a30b9907dd21ee19a571b2afb"
+    sha256 cellar: :any_skip_relocation, monterey:       "19bdbddd64b39cf5b74f5cb48a3b11ca6a6b84d4090adff85d4986300518f291"
+    sha256 cellar: :any_skip_relocation, big_sur:        "20c38ff77d9e0f02011650ef1a7c668e5a951d9fa459483d68037944e3edb9c6"
+    sha256 cellar: :any_skip_relocation, catalina:       "a33d3255a22a68a3d0dfe0e1494c28ecc41eddd64ae2ff45da7e6f44e4e0f575"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bc2b5e0e77677100bcebf9d6a2a16da84342a2883bc6f453359465459e863e8e"
   end
 
   depends_on "rust" => :build
