e40f57e66fc8736998831f1f2afcfa6636e280f0
Porkepix <porkepix@gmail.com>
2022-08-31 00:42:17 +0200

    osinfo-db 20220830
    Closes #109259.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osinfo-db.rb
+++ Formula/osinfo-db.rb
@@ -1,8 +1,8 @@
 class OsinfoDb < Formula
   desc "Osinfo database of operating systems for virtualization provisioning tools"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-20220727.tar.xz"
-  sha256 "2291e5234ed899a830c36f2b4056ff5e76235f4ba07b593421f4865290634d5e"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20220830.tar.xz"
+  sha256 "8111643d90deab838db7f213f154bef2e057350a9c834245ee01dd64433baafb"
   license "GPL-2.0-or-later"
 
   livecheck do
