cd1f6237267935ca305ef180cac859f381b6512a
graelo <graelo@grael.cc>
2022-08-30 20:54:59 +0200

    popeye 0.10.1 (new formula)
    This is a new formula for a popular tool in the k8s ecosystem.
    
    Closes #109248.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/popeye.rb
@@ -0,0 +1,18 @@
+class Popeye < Formula
+  desc "Kubernetes cluster resource sanitizer"
+  homepage "https://popeyecli.io"
+  url "https://github.com/derailed/popeye/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "6dba28376f3016e49a597d1bb3b9365cdd5ba5cd6e21c848e1c97dca49d6bdaf"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match "connect: connection refused",
+      shell_output("#{bin}/popeye --save --out html --output-file report.html", 1)
+  end
+end
