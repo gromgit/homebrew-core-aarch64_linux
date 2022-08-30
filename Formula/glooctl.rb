3a021882df2d04708962b89edbdc4c7f7ff1cdc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-30 05:42:44 +0000

    glooctl 1.12.10
    * glooctl 1.12.10
    * glooctl: update build and test
    
    Closes #109209.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.12.8",
-      revision: "0fb49a386d926285a92e3e74447c5defcdea4335"
+      tag:      "v1.12.10",
+      revision: "7fd96294a5f77a9377f4f7b0a47febfc6e857dde"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
@@ -26,7 +26,7 @@ class Glooctl < Formula
   depends_on "go" => :build
 
   def install
-    system "make", "glooctl", "TAGGED_VERSION=v#{version}"
+    system "make", "glooctl", "VERSION=v#{version}"
     bin.install "_output/glooctl"
   end
 
@@ -35,7 +35,7 @@ class Glooctl < Formula
     assert_match "glooctl is the unified CLI for Gloo.", run_output
 
     version_output = shell_output("#{bin}/glooctl version 2>&1")
-    assert_match "Client: {\"version\":\"#{version}\"}", version_output
+    assert_match "Client: {\"version\":\"v#{version}\"}", version_output
 
     version_output = shell_output("#{bin}/glooctl version 2>&1")
     assert_match "Server: version undefined", version_output
