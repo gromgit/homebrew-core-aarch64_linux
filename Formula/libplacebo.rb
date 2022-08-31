e449f97765b5ed7c7468417c282465f924b756c3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-31 15:46:50 +0000

    libplacebo: add 4.208.0 bottle.

--- Formula/libplacebo.rb
+++ Formula/libplacebo.rb
@@ -8,6 +8,15 @@ class Libplacebo < Formula
   license "LGPL-2.1-or-later"
   head "https://code.videolan.org/videolan/libplacebo.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any, arm64_monterey: "3f113d415c26bf237f93941a74496fa9a0895f95391bcbe7d40979c4aed51ff8"
+    sha256 cellar: :any, arm64_big_sur:  "f59770598c4b472ded9c4a7dba4940abbe329e06d4a2ddd6321cb1428f5c3316"
+    sha256 cellar: :any, monterey:       "19852df5a17236a60a765dce3dcb06854349da6a9761384e0df3a27ffa811e98"
+    sha256 cellar: :any, big_sur:        "93c1e3ea5040219a5498b1140481316a81c7424a2fc1a8cd56d361dcd3e2c667"
+    sha256 cellar: :any, catalina:       "cd55187c55c1bee4be2420bf092a60b541550727263ac717ca5030a898492bc4"
+    sha256               x86_64_linux:   "98ca562ff165a97fd38574ece4987534ef15deebfe3e9e945159ba9e2dddd45a"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "python@3.10" => :build
