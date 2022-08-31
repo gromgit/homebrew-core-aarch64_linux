fb38d0b6bc1ec2ed7b8b93df600ad90eac5805ea
Rui Chen <rui@chenrui.dev>
2022-08-30 23:35:48 -0400

    flarectl 0.49.0
    Closes #109281.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,8 +1,8 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.48.0.tar.gz"
-  sha256 "030a1cf4ca292b32ed34536294fcbb98a258a140da0a6d91c8c933ded4852547"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.49.0.tar.gz"
+  sha256 "43044abb5bafa20a7fda9ecfb952388bd1d016ebaa28b3e5c4a4fb6277ad6e83"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
