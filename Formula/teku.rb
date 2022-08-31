d96879118726087846a2326f63ad719ab71e381d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-31 05:57:38 +0000

    teku 22.8.2
    Closes #109292.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/teku.rb
+++ Formula/teku.rb
@@ -2,8 +2,8 @@ class Teku < Formula
   desc "Java Implementation of the Ethereum 2.0 Beacon Chain"
   homepage "https://docs.teku.consensys.net/"
   url "https://github.com/ConsenSys/teku.git",
-        tag:      "22.8.1",
-        revision: "ebfb0449d245df5ef09ebf5954122985fb8c139c"
+        tag:      "22.8.2",
+        revision: "84788438deda2ec82319ac8ad118dafc397b0cc6"
   license "Apache-2.0"
 
   bottle do
