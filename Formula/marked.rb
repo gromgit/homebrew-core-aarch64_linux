c054c2a26f27edae2495581dfee02881a0709a7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-31 01:38:17 +0000

    marked: update 4.1.0 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -8,7 +8,7 @@ class Marked < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9490740141a2ad190b8d22ae3ca340b817827722d0d01dc1bd35b57ba249fb10"
+    sha256 cellar: :any_skip_relocation, all: "75d3a30d7ca5127e8cf3333175fc322e1f1d8901eb5241e257515373f150b198"
   end
 
   depends_on "node"
