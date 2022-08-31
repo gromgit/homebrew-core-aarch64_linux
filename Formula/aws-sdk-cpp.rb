cb51d97c62d68ef69e9204746beb44b86a70ed11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-31 11:05:46 +0000

    aws-sdk-cpp: update 1.9.330 bottle.

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -9,12 +9,12 @@ class AwsSdkCpp < Formula
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "cc3f9114e7ce1a83c6d2701761c22a2eceebdbf81a9c2108f79dca024dc1b35f"
-    sha256 cellar: :any,                 arm64_big_sur:  "99249c813c5a119f76313f701465c1be617e5828d9737edaf460ae8900c568d4"
-    sha256 cellar: :any,                 monterey:       "d6f84409ba7037998d9e9857506227623890f396df239877a6a3088d6bc9df0a"
-    sha256 cellar: :any,                 big_sur:        "f002c9f7d14dad4cb8b4d819160ee7233d4d26b8a2b1a103f65ed8b740dbaef6"
-    sha256 cellar: :any,                 catalina:       "b523cdf15137e2ad74be5bba21eecca2cd38362f268ddf66165dd9ae6ba6d10e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cae66bcd17a867b8ca60567821e18d676bbfb71317a1a49262ac76db3e1b994c"
+    sha256 cellar: :any,                 arm64_monterey: "86258655b071552b36bbfbd42a5f48a2e6e6e4805b7942bf053b9863e7d27fdd"
+    sha256 cellar: :any,                 arm64_big_sur:  "2656f9fb24a1be29c8a70779fabe1717a78a0ac04dc6217069a2fa98e523bd1e"
+    sha256 cellar: :any,                 monterey:       "4e402f63c90bc99d99a5c6cda99d004c407ae9f3d029409d59d5a64c9b5792c7"
+    sha256 cellar: :any,                 big_sur:        "533a40295827e78653409f48b1d75cf05f0258cd5280f71626c64b22e4fece97"
+    sha256 cellar: :any,                 catalina:       "3ab0ccbdd143e7cfed238f96574c46e7fb26be78e07af4bb090cbb0b3a9aa96e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f4d3c3a6cf66e4303ae93c1301cc6583c7205715dbb91f5ab80a0aa6ca6dcfd"
   end
 
   depends_on "cmake" => :build
