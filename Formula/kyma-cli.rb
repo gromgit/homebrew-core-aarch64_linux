adb1608503f408b59946bb530b91fd061046d2ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-31 09:05:13 +0000

    kyma-cli: update 2.6.1 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -7,12 +7,12 @@ class KymaCli < Formula
   head "https://github.com/kyma-project/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2337afb7cd41ae97a9c83272b1d4bc9917fc2956f76b028d7f6b108473115ff4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fb2a43f70a4a58f4b6377129149341a648b4d6217c8823a3e0c7b47fb80d0235"
-    sha256 cellar: :any_skip_relocation, monterey:       "f4a8692915623518bc2503a383bad5dbe132a3112be4d68b1b1aa60eb5c04c0a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a2482edd0ff4a36ba6649b9b8edfa803a5aa619f6e3c51fd0a1e7125421323bd"
-    sha256 cellar: :any_skip_relocation, catalina:       "bfe867a74c00e8c7c93e5108033362a22ce439fc2de65b3449ec4def53814020"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8def9efef4f5f60c01735ab1b81789bfcfb689b35ffb57ca8120d54a17d06735"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d6da0e860837c02cf64ba5717ca60eb09f5fb8d606221134f4741b9ca828424a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "12913c2b3bff815506f2fff324c045f9799faa5ea218e07a7cc5bdb0329a5a27"
+    sha256 cellar: :any_skip_relocation, monterey:       "0ee137ae9f4230dc7449bd7ff09cf2cd3a1bf67244c01e83476e4f3907a2ee90"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c069e65ca1afc1c2b8495eac7d776e4270be8022da880213a1923e6f400fdc2d"
+    sha256 cellar: :any_skip_relocation, catalina:       "630cd2be65d8ed573a2e9960d73ee6002d406733470fa070236707232db621c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "72179278d9210d782b45cd173119c843daae48d5cd8999eb1de52267226eb8d4"
   end
 
   depends_on "go" => :build
