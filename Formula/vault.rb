f395dc6ac98750d2672bc910798fbf6907ce400c
Porkepix <porkepix@gmail.com>
2022-09-01 06:32:49 +0200

    vault 1.11.3
    Closes #109349.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -5,8 +5,8 @@ class Vault < Formula
   desc "Secures, stores, and tightly controls access to secrets"
   homepage "https://vaultproject.io/"
   url "https://github.com/hashicorp/vault.git",
-      tag:      "v1.11.2",
-      revision: "3a8aa12eba357ed2de3192b15c99c717afdeb2b5"
+      tag:      "v1.11.3",
+      revision: "17250b25303c6418c283c95b1d5a9c9f16174fe8"
   license "MPL-2.0"
   head "https://github.com/hashicorp/vault.git", branch: "main"
 
