bb47673626e2dedf47d017c58e47b450dbf5f0b0
danielnachun <daniel.nachun@gmail.com>
2022-09-01 15:25:16 -0700

    collectd: revision bump

--- Formula/collectd.rb
+++ Formula/collectd.rb
@@ -2,7 +2,7 @@ class Collectd < Formula
   desc "Statistics collection and monitoring daemon"
   homepage "https://collectd.org/"
   license "MIT"
-  revision 3
+  revision 4
 
   stable do
     url "https://collectd.org/files/collectd-5.12.0.tar.bz2"
