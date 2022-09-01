9f5ae9262c281481f879511d604bfe1447c68f9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-01 07:40:55 +0000

    gdcm: update 3.0.16 bottle.

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -11,12 +11,12 @@ class Gdcm < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e5545cbba07e64949d430fcbf8f748ab44812715981963cacb791fe351caaca6"
-    sha256 arm64_big_sur:  "1e9a6f71f51e17fffe1e015ea24a4e3b7d8bf8d000750fccc08cd7c55dc576e8"
-    sha256 monterey:       "aaca417828c252769fe1f6dcc392caeea4bffe9c8fdf034dea2d361ca943e914"
-    sha256 big_sur:        "55e74700b796709ff3dabf35a1cef14b6e8aa1554f6d81281b3b16b597d93003"
-    sha256 catalina:       "5881919c7d28e2eb1ab3f769be347e03c56257761499bc34a25fc5cfbf60c79d"
-    sha256 x86_64_linux:   "7811ccfe3dad3ec98dfc068d327bbec0d12b6c9da1f198deab1aede0c2d9b84c"
+    sha256 arm64_monterey: "382a9cf85f8d7c7ce033fec6834b9f2faea2d10d7be982eb3e0d2d31c43b6dfb"
+    sha256 arm64_big_sur:  "c9d2dd1b29ca9e72eb9f80dc7c72dd73818c49b4cdfbadb9e122280985cdaada"
+    sha256 monterey:       "50825a3af975e8acd3eb2d022d447706b3ce381ce929bf934ab9b430598f9194"
+    sha256 big_sur:        "5971add90a3b14682d9c9f611b1388e1c4792f9c02d1ca85a1eec7fab92e236b"
+    sha256 catalina:       "15e474581d4a9ed8b9aeb93d104181b4477f509e3d6e0e48592698f7e3261a24"
+    sha256 x86_64_linux:   "c9516f6894788f85a4091b7486122651a7f272fe1925f038415e07ba7ec6fa9e"
   end
 
   depends_on "cmake" => :build
