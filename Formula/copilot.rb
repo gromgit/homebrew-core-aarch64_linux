307e34caf354f56bbb41e23da459ffc5ec546667
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-01 07:40:58 +0000

    copilot: update 1.21.1 bottle.

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -15,12 +15,12 @@ class Copilot < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "be836be71841188bd183a63192f1eb17bd1c355402fefc3b50c132003433532e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66294769cb39f4f03516db223b26c4f57efd61b19ed2d477fa2012ea7af23586"
-    sha256 cellar: :any_skip_relocation, monterey:       "9461233d7c35e3d418675d02029aac5e44e368b50c436a23677ba7a307dd4d79"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3893c599f7522de5d45c998145fb139c4b0a53fb09bc9d0712089a8eb723b1a0"
-    sha256 cellar: :any_skip_relocation, catalina:       "66be2794a15ddfe850ae0679983d563d08431b44c6ce9594e96851371a212826"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dfdbc82b05b7e18e9aa1955191423394d4c0561be4e453fb0c3a36033af1fe06"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4c0cda510f47b6966ef14d3de5286c08ca1e6fb0935148742ec63ebcd7887854"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c2dc3d46e1cfeaa243f017ac3c9abd16cc6252f95ece1560800d66916e491fa2"
+    sha256 cellar: :any_skip_relocation, monterey:       "b45ef71297f3fe5e74c739cd5e01a000a4e691a7a1e3bee74c2695a94ef21d13"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6354a5a1fa31168289fd93c07e0445fe374ca1d6a23dea335f74d250ffb0dde6"
+    sha256 cellar: :any_skip_relocation, catalina:       "f63a0890fb78bcc86cbd451462bab7e4f8646869ffeb64618e8bb91f08241f13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50044f3dd3ce842b109c946068581ec89b86b0d384204a4d800770244b22964e"
   end
 
   depends_on "go" => :build
