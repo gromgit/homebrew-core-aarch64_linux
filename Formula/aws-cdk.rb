890828aad3552f0151a56ef2b706277f31609ebc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-01 02:55:39 +0000

    aws-cdk: update 2.40.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -8,12 +8,12 @@ class AwsCdk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e5201b4b9f93596f49f27da25bbd941917ac8fb04b73560f5f271724c4426ad"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e5201b4b9f93596f49f27da25bbd941917ac8fb04b73560f5f271724c4426ad"
-    sha256 cellar: :any_skip_relocation, monterey:       "f0b8f7424f084e49a72be22283125eb764f49253595767e845c80c6849c26a5a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f0b8f7424f084e49a72be22283125eb764f49253595767e845c80c6849c26a5a"
-    sha256 cellar: :any_skip_relocation, catalina:       "f0b8f7424f084e49a72be22283125eb764f49253595767e845c80c6849c26a5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f0193573df3628567d5f4d1da0e061eb7b6f14e25cb35076998eaa4e26880e53"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee78c8a6b647cd12f7ed2d81dcc52da09ee95d71d034ff6075f54df0b1c1d199"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee78c8a6b647cd12f7ed2d81dcc52da09ee95d71d034ff6075f54df0b1c1d199"
+    sha256 cellar: :any_skip_relocation, monterey:       "7cb3d8b0a9e9d641203c41f5be1ee02ee103a226b6c964294caa9f52c7229de9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7cb3d8b0a9e9d641203c41f5be1ee02ee103a226b6c964294caa9f52c7229de9"
+    sha256 cellar: :any_skip_relocation, catalina:       "7cb3d8b0a9e9d641203c41f5be1ee02ee103a226b6c964294caa9f52c7229de9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be93cd65cb654c390ef159d6dbb3133949e0138aa2c7b55e7e0ede60db16a277"
   end
 
   depends_on "node"
