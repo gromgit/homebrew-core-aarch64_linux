21e3844e68c0daa98e2462b0937c88df1864b292
Rui Chen <rui@chenrui.dev>
2022-09-02 13:51:29 -0400

    hashcat 6.2.6
    * hashcat 6.2.6
    * hashcat: update installation & test, remove patch
    
    Closes #109515.
    
    Co-authored-by: EricFromCanada <enk3@outlook.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hashcat.rb
+++ Formula/hashcat.rb
@@ -1,11 +1,10 @@
 class Hashcat < Formula
   desc "World's fastest and most advanced password recovery utility"
   homepage "https://hashcat.net/hashcat/"
-  url "https://hashcat.net/files/hashcat-6.2.5.tar.gz"
-  mirror "https://github.com/hashcat/hashcat/archive/v6.2.5.tar.gz"
-  sha256 "6f6899d7ad899659f7b43a4d68098543ab546d2171f8e51d691d08a659378969"
+  url "https://hashcat.net/files/hashcat-6.2.6.tar.gz"
+  mirror "https://github.com/hashcat/hashcat/archive/v6.2.6.tar.gz"
+  sha256 "b25e1077bcf34908cc8f18c1a69a2ec98b047b2cbcf0f51144dcf3ba1e0b7b2a"
   license "MIT"
-  revision 1
   version_scheme 1
   head "https://github.com/hashcat/hashcat.git", branch: "master"
 
@@ -24,6 +23,7 @@ class Hashcat < Formula
   end
 
   depends_on "gnu-sed" => :build
+  depends_on macos: :high_sierra # Metal implementation requirement
   depends_on "minizip"
   depends_on "xxhash"
 
@@ -34,10 +34,6 @@ class Hashcat < Formula
     depends_on "ocl-icd"
     depends_on "pocl"
   end
-
-  # Fix build failure from missing include for limits.h.
-  # Upstreamed here: https://github.com/hashcat/hashcat/pull/3387
-  patch :DATA
 
   def install
     args = %W[
@@ -50,27 +46,13 @@ class Hashcat < Formula
     ]
     system "make", *args
     system "make", "install", *args
+    bin.install "hashcat" => "hashcat_bin"
+    (bin/"hashcat").write_env_script bin/"hashcat_bin", XDG_DATA_HOME: share
   end
 
   test do
     ENV["XDG_DATA_HOME"] = testpath
-    ENV["XDG_CACHE_HOME"] = testpath
-    cp_r pkgshare.children, testpath
-    cp bin/"hashcat", testpath
-    system testpath/"hashcat --benchmark -m 0 -D 1,2 -w 2"
+    mkdir testpath/"hashcat"
+    assert_match "Hash-Mode 0 (MD5)", shell_output("#{bin}/hashcat_bin --benchmark -m 0 -D 1,2 -w 2")
   end
 end
-
-__END__
-diff --git a/src/filehandling.c b/src/filehandling.c
-index 345d451..05baae9 100644
---- a/src/filehandling.c
-+++ b/src/filehandling.c
-@@ -5,6 +5,7 @@
- 
- #include "common.h"
- #include "types.h"
-+#include "limits.h"
- #include "memory.h"
- #include "shared.h"
- #include "filehandling.h"
