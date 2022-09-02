ce03de4b3f08f486c2f00f7642bbb4d3bf383fb2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-02 18:05:11 +0000

    spaceship: update 4.2.0 bottle.

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -7,12 +7,12 @@ class Spaceship < Formula
   head "https://github.com/spaceship-prompt/spaceship-prompt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3922e5ce767b2b21732bbafb8b92673166e2a95e90e536b1a02f7ade6c931450"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "384419ff230a911c5b0c00a2ce38e2833255cf22d9b1f45409fb044344fae97f"
-    sha256 cellar: :any_skip_relocation, monterey:       "ca5f5f8ea5e403bcb1d34d5df588f9ecb08a24a4e03416d89ecbf117b07965d6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "49a8b8e77c9c22a53cdc9e9c20c05b5f6c7a324dcb9d26b046b7d3c74522b058"
-    sha256 cellar: :any_skip_relocation, catalina:       "c4879cbfb351f19c8a499e42142f42c8bd1ce56e5d4b2b50123b60d063e39ee1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6441f178dddfaaa56c3ef964b21cb724809d8f2bcf3bad76e1daede520b7419f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5384f8075e349091da1b7d21e32e90a7cb71d4c4b391c436619badc7e6ea3154"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dddeb5264a9f36d12d2313d1984621d8b2411d8b2d22fd732f015164887ab332"
+    sha256 cellar: :any_skip_relocation, monterey:       "71787b25a9a430fd2c972ff7972779ed6948885ad993ef3f885b10b9e4005f73"
+    sha256 cellar: :any_skip_relocation, big_sur:        "493357766099d139e4863e14eec2497a3cdf09e5eddfe58ec8c578e851376a18"
+    sha256 cellar: :any_skip_relocation, catalina:       "b5d6ec54aba18eeb684745eac0143c8bbfb17fb6ed7b46ed3c1d69f76f90302f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ce8d0c63eaf1ab2619629b65d6fbe0467f892fc6d119239f96a48dd89ca2f21"
   end
 
   depends_on "zsh-async"
