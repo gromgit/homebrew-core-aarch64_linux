4561ceaf14de67dd3f05ad32257696e434a7a836
Rui Chen <rui@chenrui.dev>
2022-09-02 09:29:21 -0400

    tkdiff 5.5.2
    Closes #109496.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tkdiff.rb
+++ Formula/tkdiff.rb
@@ -1,9 +1,9 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.5.1/tkdiff-5-5-1.zip"
-  version "5.5.1"
-  sha256 "f8afa4e491f89df2868993f511807ece5ae51535bc268e674e92de54554a62c8"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.5.2/tkdiff-5-5-2.zip"
+  version "5.5.2"
+  sha256 "31718411fa181ea4a6c9c7f8eea115a7b86c50dff5a1a070bc7ad26381d7176b"
   license "GPL-2.0-only"
 
   livecheck do
