0aae0deaf0a6332d2d96ac36510bb2d30f94b763
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-09-02 14:49:08 +0300

    tarantool 2.10.2
    Closes #109488.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tarantool.rb
+++ Formula/tarantool.rb
@@ -1,8 +1,8 @@
 class Tarantool < Formula
   desc "In-memory database and Lua application server"
   homepage "https://tarantool.org/"
-  url "https://download.tarantool.org/tarantool/src/tarantool-2.10.1.tar.gz"
-  sha256 "615c9471ed4b4277427bd24b008a7ca7942da349d3391ba466823dab2bc34e6f"
+  url "https://download.tarantool.org/tarantool/src/tarantool-2.10.2.tar.gz"
+  sha256 "2873992c88a7f549d953d9bc155e102300a78af24eb928fb40b642b2d1aca0ae"
   license "BSD-2-Clause"
   version_scheme 1
   head "https://github.com/tarantool/tarantool.git", branch: "master"
