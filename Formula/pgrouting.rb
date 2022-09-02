437bce67c9f567a309b8ffdabaee18bafb014378
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-02 15:59:04 +0000

    pgrouting: update 3.3.2 bottle.

--- Formula/pgrouting.rb
+++ Formula/pgrouting.rb
@@ -12,12 +12,12 @@ class Pgrouting < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "da4a462e3b0b8ad4252dbde960f1654183f89412534f15adee3b335ca797b51b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b868ee67cd37ef035ef788a13e2382267ea9e39b1ae64a0e3b9ff3031eea5cc2"
-    sha256 cellar: :any_skip_relocation, monterey:       "60ae6614ddadee5c3be8c467ed002bc90df64ad65fe93593bfc099e6da105f66"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b1c37e09fbeef1864f60de68b6374e22b0aad6bf6f1c3787df9ae985acaec89f"
-    sha256 cellar: :any_skip_relocation, catalina:       "d31374cd447498a9bc7689d007c45c4ec6fd784feb6e28a9302f0f46a492bc13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53eb7a057195593a96297145478d6c54f7d537cfe25670eb6d2a865b56d2d387"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "151793f043090e7300dab326939a0611a900b420e2ae369d84031d25fd98ed7e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "60fbb6bda0d8d542aaf5c3a07ce25d23de7bd0a8dfad39ccd80d99bd020b332c"
+    sha256 cellar: :any_skip_relocation, monterey:       "7d94f7f46a681456703c8a46c204c89d954be716fd9b785bae1a8d97e23716b3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3d9384323835e65147ff62b6c3a8e2ff10b80c028743aac04d4022d330f9c810"
+    sha256 cellar: :any_skip_relocation, catalina:       "cd9a422b6d885c5cfbef8ee2b6afe26525633dbe5477545fb7bdba89e54560f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9b0acc620b1380a1e5be9182224a88e656b4b2f8cb228691a270cfe7df82923"
   end
 
   depends_on "cmake" => :build
