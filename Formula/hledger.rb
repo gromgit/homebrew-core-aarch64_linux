125b3dcd7f68b776f656f83fb5a16fd1b883d5db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-02 05:47:46 +0000

    hledger 1.27
    * hledger 1.27
    * hledger: update resources
    * hledger: use github tarball for maintained stack.yaml
    
    Closes #109472.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hledger.rb
+++ Formula/hledger.rb
@@ -1,9 +1,10 @@
 class Hledger < Formula
   desc "Easy plain text accounting with command-line, terminal and web UIs"
   homepage "https://hledger.org/"
-  url "https://hackage.haskell.org/package/hledger-1.26.1/hledger-1.26.1.tar.gz"
-  sha256 "245af2ec616a754fb488a7ad8ffa360732b733d96c7d6b1e49d4b0fbd435e247"
+  url "https://github.com/simonmichael/hledger/archive/refs/tags/1.27.tar.gz"
+  sha256 "1a3d1d321cf5fb9ae6d214a5a4a71775680a8146cf43f77e523a2e6c0af9d366"
   license "GPL-3.0-or-later"
+  head "https://github.com/simonmichael/hledger.git", branch: "master"
 
   # A new version is sometimes present on Hackage before it's officially
   # released on the upstream homepage, so we check the first-party download
@@ -28,48 +29,17 @@ class Hledger < Formula
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
-  resource "hledger-lib" do
-    url "https://hackage.haskell.org/package/hledger-lib-1.26.1/hledger-lib-1.26.1.tar.gz"
-    sha256 "2c9ef04c2cda669e2b83b8d2d8512f5a298cd95e5f9e825e697204ba96c85da5"
-  end
-
-  resource "hledger-ui" do
-    url "https://hackage.haskell.org/package/hledger-ui-1.26.1/hledger-ui-1.26.1.tar.gz"
-    sha256 "8bdeea8383201328e85d49be823787fe7ea1daf811189a41d3d4ce8d98d47e3d"
-  end
-
-  resource "hledger-web" do
-    url "https://hackage.haskell.org/package/hledger-web-1.26.1/hledger-web-1.26.1.tar.gz"
-    sha256 "e2b0d21c6657a234d4f991439dfbbf511490c8eae09bf832c824c272c7d0e6c7"
-  end
-
   def install
-    (buildpath/"../hledger-lib").install resource("hledger-lib")
-    (buildpath/"../hledger-ui").install resource("hledger-ui")
-    (buildpath/"../hledger-web").install resource("hledger-web")
-    cd ".." do
-      system "stack", "update"
-      (buildpath/"../stack.yaml").write <<~EOS
-        resolver: lts-17.5
-        compiler: ghc-#{Formula["ghc"].version}
-        compiler-check: newer-minor
-        packages:
-        - hledger-#{version}
-        - hledger-lib
-        - hledger-ui
-        - hledger-web
-      EOS
-      system "stack", "install", "--system-ghc", "--no-install-ghc", "--skip-ghc-check", "--local-bin-path=#{bin}"
-
-      man1.install Dir["hledger-*/*.1"]
-      man5.install Dir["hledger-lib/*.5"]
-      info.install Dir["hledger-*/*.info"]
-    end
+    system "stack", "update"
+    system "stack", "install", "--system-ghc", "--no-install-ghc", "--skip-ghc-check", "--local-bin-path=#{bin}"
+    man1.install Dir["hledger*/*.1"]
+    info.install Dir["hledger*/*.info"]
+    bash_completion.install "hledger/shell-completion/hledger-completion.bash" => "hledger"
   end
 
   test do
-    system "#{bin}/hledger", "test"
-    system "#{bin}/hledger-ui", "--version"
-    system "#{bin}/hledger-web", "--test"
+    system bin/"hledger", "test"
+    system bin/"hledger-ui", "--version"
+    system bin/"hledger-web", "--test"
   end
 end
