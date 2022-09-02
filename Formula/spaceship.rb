87a1866eda8eda2dcbcf8ce33c088e96157c8546
Denys Dovhan <denysdovhan@gmail.com>
2022-09-02 15:18:04 +0300

    spaceship: update installation method for v4
    Closes #109489.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -4,22 +4,31 @@ class Spaceship < Formula
   url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v4.0.0.tar.gz"
   sha256 "9f442dbac524e13df89458fb1e45bbd6753c0cfda51943f674ce29bd05aae163"
   license "MIT"
+  revision 1
   head "https://github.com/spaceship-prompt/spaceship-prompt.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "ecaafd4b16aafc13fc96c336ebe876c383653ab3a6f49c470e23fad63bbbe421"
   end
 
-  depends_on "zsh" => :test
+  depends_on "zsh-async"
+  uses_from_macos "zsh" => :test
 
   def install
-    libexec.install "spaceship.zsh", "lib", "sections"
-    zsh_function.install_symlink libexec/"spaceship.zsh" => "prompt_spaceship_setup"
+    system "make", "compile"
+    prefix.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      To activate Spaceship, add the following line to ~/.zshrc:
+        source "#{opt_prefix}/spaceship.zsh"
+      If your .zshrc sets ZSH_THEME, remove that line.
+    EOS
   end
 
   test do
-    ENV["SPACESHIP_CHAR_SYMBOL"] = "🍺"
-    prompt = "setopt prompt_subst; autoload -U promptinit; promptinit && prompt -p spaceship"
-    assert_match ENV["SPACESHIP_CHAR_SYMBOL"], shell_output("zsh -c '#{prompt}'")
+    assert_match "SUCCESS",
+      shell_output("zsh -fic '. #{opt_prefix}/spaceship.zsh && (( ${+SPACESHIP_VERSION} )) && echo SUCCESS'")
   end
 end
