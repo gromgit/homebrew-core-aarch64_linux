4f09ba183edde0fe979b483bdd02b5c701e3ce81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-03 05:50:30 +0000

    snap: update 2.57.2 bottle.

--- Formula/snap.rb
+++ Formula/snap.rb
@@ -12,12 +12,12 @@ class Snap < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "aaa0d0cd5ff19f1241e566cb8f699f162d0ed11949f9c1b4da55fce708da46e0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7cbaec7698fa9d99bcfbe2f4ecc15d39a92c63273a6a797a9a57dc3f73e31f8"
-    sha256 cellar: :any_skip_relocation, monterey:       "94146e3f5e4af2d6fcd02b567ff1ec88caec622876c2b6c65c03fc2a15db4aa4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "73d995f0ccafbbd84c430e27e45180198a019583f37cd67d9adfe19e00a25c05"
-    sha256 cellar: :any_skip_relocation, catalina:       "fbf1b47a9a9114c306bcc64f896288512e356ce3702578c54904fc2883155d9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f7522c9b4f2bdd6be38c03fd485e17208ae8c563268933f5a155f3350a794653"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9a6d4e2addefd503d9be7c5758c126f923099b8bb5d566a9cd9ace5599ee589a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9188fb9c88b66e2c74258488e9ee5039b0eaa0d47e4f29303985f9a97d79a3da"
+    sha256 cellar: :any_skip_relocation, monterey:       "76857f0952172192547771e3d41418912f671753c5dfcc6617b4884abf87aaa3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6df7766950cc3974aaa30f0fd272681f6d23cf3ad17f651090df939dece66b89"
+    sha256 cellar: :any_skip_relocation, catalina:       "6bc1ac71759d1911239ba09db22bd30f23a26737404bd73351902cb94d7577d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91fe2ccbc740b52683863c98d2a6b0798ac4566ccc9bdf9d7aa756576e372f75"
   end
 
   depends_on "go" => :build
