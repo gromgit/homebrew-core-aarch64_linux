25411a71efab37a8bca189c410bce1087dcdb77a
Rui Chen <rui@chenrui.dev>
2022-09-03 12:44:26 -0400

    dbdeployer 1.69.1
    Closes #109567.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dbdeployer.rb
+++ Formula/dbdeployer.rb
@@ -1,8 +1,8 @@
 class Dbdeployer < Formula
   desc "Tool to deploy sandboxed MySQL database servers"
   homepage "https://github.com/datacharmer/dbdeployer"
-  url "https://github.com/datacharmer/dbdeployer/archive/v1.69.0.tar.gz"
-  sha256 "6508adb262a72481b842eeb6f43dc8f68e81a9808505bf03f51c4ccc9af370a5"
+  url "https://github.com/datacharmer/dbdeployer/archive/v1.69.1.tar.gz"
+  sha256 "165950a32760a5e0f32f951eaa0c67a6956cb2cea75ac0381c65f39b7e9789c7"
   license "Apache-2.0"
   head "https://github.com/datacharmer/dbdeployer.git", branch: "master"
 
