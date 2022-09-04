490dcb985f6ed2080f18e04821603b90e969c568
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-04 21:04:24 +0000

    fclones: update 0.27.2 bottle.

--- Formula/fclones.rb
+++ Formula/fclones.rb
@@ -6,12 +6,12 @@ class Fclones < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea7b013d29ff3d10b512b3a8d78516b9c70cc3b8126f462d61231a7e7288d105"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bfa96f05b2179a214399ebb2b2889fe4b2f2a02479ced108f5e3a57162a0be9c"
-    sha256 cellar: :any_skip_relocation, monterey:       "0b9726dcb4bfb14a5c4453929b89991c50ef3a7efe38f212531a3bbf7733fafa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f4292ecf8b26d650dfad1b0d5f90f46d645bd5da474fff75be81db96e29c20dc"
-    sha256 cellar: :any_skip_relocation, catalina:       "710bcc218dbdeb9eb5405811fb1a08fdb784808aa79b19b298a479d1d0ea824e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8dab8d4f3adccc9cb325a5638c4646354f4125a17f0a28eee3aa3f4f431eefc2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e408a1c73177742571a1fee62907dbe626f89517acb7d017af97dd6332f9b0ec"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc7f42aacab8536facc6012e9403dab3c8313cd6c086bb25191171225c40b57d"
+    sha256 cellar: :any_skip_relocation, monterey:       "5c9cebcf5607ebd55e18bc6a4b87ef6152b6b45dd50125bb8d4d119f9b9c5c27"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7aa9b7276f36cb8f827af8d4419556344ab0df9d04340929cc6f9edbeb43e0e"
+    sha256 cellar: :any_skip_relocation, catalina:       "074641dd55c2124c59ec75c32acea70dcb4e53aebe8612f61b8b58f4b2a1f5af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ca9dbeb870b74dfaf58154ff3657d5a808e9892100ce8bd870ceac4d42a731b"
   end
 
   depends_on "rust" => :build
