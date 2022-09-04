bbacece17296bf75e30aa82fd1ad854003c2a53e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-04 17:06:54 +0000

    cxgo: update 0.3.6 bottle.

--- Formula/cxgo.rb
+++ Formula/cxgo.rb
@@ -8,12 +8,12 @@ class Cxgo < Formula
   head "https://github.com/gotranspile/cxgo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc4e999c6066727547b5e2ea7391c70fd0457c0a5a48b65ffb6f281ef89d622c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ef46c289a0961764a7ab92d225ab1f076a949d02a561974c4d1273eaeac26d6"
-    sha256 cellar: :any_skip_relocation, monterey:       "cffb8bf0c1f1704cc432c21b466afe56426beebb8168d9c19def54654b247cce"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d32a3894a5269db489c2d47277cbb9397c78b9e439084d428438283e5374722c"
-    sha256 cellar: :any_skip_relocation, catalina:       "6072b4bca8b8f7b2ef412257c9a58ab76aec0ca873cb92d31c36a1d747005e3e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "357879ed2c51c279b3d4e2a74dbe7db78a36b448af8a0377ac53a89c1297cc91"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "035b673ef60a34ff7530917b5900944270ceb7dd341b463651162479f4693f5d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c15447701a5ecb2d2a71146edaf4179dc263cf72736d379b8b092b456eb644fa"
+    sha256 cellar: :any_skip_relocation, monterey:       "38ede1b76309edfa8133861ad5a6c2f7585312f738d351fc2c469ae171db7858"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b25a381fefe12281fd957c59d30b75118139caf7562dbb3f043485b78979d8e8"
+    sha256 cellar: :any_skip_relocation, catalina:       "067789924d51c646ed958b36d7ee84efd98914b04c14098c3839a5fe316a641d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bbda0baedff1788890b8afd6ceca3a52a3687be91172cf730f1b115f4442e7b9"
   end
 
   depends_on "go" => :build
