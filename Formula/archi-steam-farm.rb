191eb0199a4908de1210c5d5922589df21184ce3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-04 00:17:21 +0000

    archi-steam-farm: update 5.3.0.3 bottle.

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -13,12 +13,12 @@ class ArchiSteamFarm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "449c3ecc952e64abc089b71cf1f2107fdab1696c335090e802b34f264d25106c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "606e7c80dfa647a99a615bbc1b0421954c245d32686986a7c8c71697531f29c2"
-    sha256 cellar: :any_skip_relocation, monterey:       "14278aaf726732b96d0125fb94daff1b0d96c87f8c80f997b59f1f81c22d7c15"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e7d29da0fe3fa33688491b0adcb0d5de05d66189efbbf393b7d3fe16f82187aa"
-    sha256 cellar: :any_skip_relocation, catalina:       "677de91556ec16856f19db9313e3d05a81a3f818e22a4f3dd0adebf44a8fc081"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5ae61cdfac6d8bbad689444b72186aacbe3b10308c97c4e987411d2871ac3c92"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "327993e7afc86ce64a792abec480382bd093c829ce100d930f814ee53a3eebdc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f722041477efd5a6a1b72f194c204a2def82c8d321c10483deec93b964c1732"
+    sha256 cellar: :any_skip_relocation, monterey:       "3a04a935a20276fadbee76c96ec0295634ddb09a30ae5f5a55eba74d41a25d01"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1eaa72d7ef0b81cb8991249d9ae2fd0bc95e6fee71052e66878661926019778f"
+    sha256 cellar: :any_skip_relocation, catalina:       "ecd9a3c99de7f291ceb28b768ab66ad2a8416038cb8441a16867757a837110e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc12df6e306adaf19ce1370f32e4ccf57142c5d993ca6963b219e6441b3cf89d"
   end
 
   depends_on "dotnet"
