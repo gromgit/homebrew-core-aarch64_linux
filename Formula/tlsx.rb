3fd4037d13226fa3f0e5fd136fbf8f8df647ed8f
Rui Chen <rui@chenrui.dev>
2022-09-04 12:04:59 -0400

    tlsx 0.0.7
    Closes #109608.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tlsx.rb
+++ Formula/tlsx.rb
@@ -1,8 +1,8 @@
 class Tlsx < Formula
   desc "Fast and configurable TLS grabber focused on TLS based data collection"
   homepage "https://github.com/projectdiscovery/tlsx"
-  url "https://github.com/projectdiscovery/tlsx/archive/v0.0.6.tar.gz"
-  sha256 "79c663951053e866d0ad28a61bc218054e80611c29b4de6680d4eb7448c67593"
+  url "https://github.com/projectdiscovery/tlsx/archive/v0.0.7.tar.gz"
+  sha256 "82b54e89ff5c73f5a8536443466c897a6b0bb5ee50ea101390c10211bdf4f00e"
   license "MIT"
   head "https://github.com/projectdiscovery/tlsx.git", branch: "main"
 
