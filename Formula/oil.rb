3e6dcabef90f5deae97044b6496eaa72017ca72f
Porkepix <porkepix@gmail.com>
2022-09-05 10:46:24 +0200

    oil 0.12.5
    Closes #109671.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -1,8 +1,8 @@
 class Oil < Formula
   desc "Bash-compatible Unix shell with more consistent syntax and semantics"
   homepage "https://www.oilshell.org/"
-  url "https://www.oilshell.org/download/oil-0.12.4.tar.gz"
-  sha256 "ac5ed4b43963ed385939fb8d9ab2ae312ab037bdd368ed52232dce2ab44b65b2"
+  url "https://www.oilshell.org/download/oil-0.12.5.tar.gz"
+  sha256 "e7fad0b14deb64fa28e9db40060dcfa8288f04f0f019acf8d15fc85b60ea5770"
   license "Apache-2.0"
 
   livecheck do
