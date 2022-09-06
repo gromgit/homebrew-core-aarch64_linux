a2a76530c19dc3ca4996fa0308e39baec8579e0b
Branch Vincent <branchevincent@gmail.com>
2022-09-06 19:07:43 -0400

    python-tk@3.9 3.9.14
    Closes #109810.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/python-tk@3.9.rb
+++ Formula/python-tk@3.9.rb
@@ -1,8 +1,8 @@
 class PythonTkAT39 < Formula
   desc "Python interface to Tcl/Tk"
   homepage "https://www.python.org/"
-  url "https://www.python.org/ftp/python/3.9.13/Python-3.9.13.tar.xz"
-  sha256 "125b0c598f1e15d2aa65406e83f792df7d171cdf38c16803b149994316a3080f"
+  url "https://www.python.org/ftp/python/3.9.14/Python-3.9.14.tar.xz"
+  sha256 "651304d216c8203fe0adf1a80af472d8e92c3b0e0a7892222ae4d9f3ae4debcf"
   license "Python-2.0"
 
   livecheck do
