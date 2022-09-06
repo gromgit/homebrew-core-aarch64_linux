00e12cc65b811ae248ade1451e6bc3e82984ea74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-06 03:00:05 +0000

    stencil: update 1.26.0 bottle.

--- Formula/stencil.rb
+++ Formula/stencil.rb
@@ -7,12 +7,12 @@ class Stencil < Formula
   head "https://github.com/getoutreach/stencil.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e0d72a5a20d24a397c6a29382fa3e114de4069bec26ce2b256839a81c6681da"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "65fbe2f65bd6256acd0b765ce9a32878e5c81e9251a54a1db08063970bfde819"
-    sha256 cellar: :any_skip_relocation, monterey:       "f59fac2c5790ec978dbeb7521fa371532043d2fd9e07789296851bd5e8daa616"
-    sha256 cellar: :any_skip_relocation, big_sur:        "81b13611986bdb9f436863e2519f46fdb2057f835403267245b6f7965525f59f"
-    sha256 cellar: :any_skip_relocation, catalina:       "b3a50c058647bccee50ce5f0c9661fb3ec9001ddbf863839765f6ca46bf08d45"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b3a559f3262b2bec4d174b381d8c634d424fd2db7df4a54cc61a11dff18c464"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e7e405bffaccaad5b43e75881f0344f3a9c692ff8f67ec85036716b89c9c44df"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c52b14396f4806fee12f3a6d852bfa94d5e530b3079226b1809c6d548392055"
+    sha256 cellar: :any_skip_relocation, monterey:       "a5424f272ee27b89caa13da6cc4ead751a0ac123fa58f2f9b0c593ef1db01bf3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "18f2b156abc416037d504b56831740938ddfd4420b8dda5de880aa901bfa379c"
+    sha256 cellar: :any_skip_relocation, catalina:       "e078005a945e71299d3bc9d2c68870d3daf76589c005f116f8030d2552ff07fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e336ef2bdd47a5c169a50feada13552e0b353299110263fcfe8d1ad4316dab0e"
   end
 
   depends_on "go" => :build
