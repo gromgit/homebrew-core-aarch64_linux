5bd6c1fd2d8113069afb3be84028737cc76986e7
Joe Previte <jjprevite@gmail.com>
2022-09-06 11:04:41 -0700

    code-server 4.6.1-1
    * code-server 4.6.1-1
    * fix: use npm instead of yarn
    * fixup
    * fixup
    * try removing parcel fix
    * try again
    * try local_npm_install_args
    * fixup
    
    Closes #109799.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/code-server.rb
+++ Formula/code-server.rb
@@ -1,8 +1,10 @@
+require "language/node"
+
 class CodeServer < Formula
   desc "Access VS Code through the browser"
   homepage "https://github.com/coder/code-server"
-  url "https://registry.npmjs.org/code-server/-/code-server-4.6.0.tgz"
-  sha256 "010ba1b4349730f412c72255f842c56f292d6944c2220e10d7f5b5bf34491f82"
+  url "https://registry.npmjs.org/code-server/-/code-server-4.6.1-1.tgz"
+  sha256 "6c66bbb59e06dc38ded2492af5806e6d1987ec42551ab66fd887231ec874f4b6"
   license "MIT"
 
   bottle do
@@ -27,7 +29,7 @@ class CodeServer < Formula
 
   def install
     node = Formula["node@16"]
-    system "yarn", "--production", "--frozen-lockfile"
+    system "npm", "install", *Language::Node.local_npm_install_args, "--unsafe-perm", "--omit", "dev"
     # @parcel/watcher bundles all binaries for other platforms & architectures
     # This deletes the non-matching architecture otherwise brew audit will complain.
     prebuilds = buildpath/"lib/vscode/node_modules/@parcel/watcher/prebuilds"
