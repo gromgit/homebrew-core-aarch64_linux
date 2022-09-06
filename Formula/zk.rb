419716f451862e940f46c2c1de76f734b3820ef1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-06 10:16:37 +0800

    zk: update 0.11.1_1 bottle.

--- Formula/zk.rb
+++ Formula/zk.rb
@@ -8,12 +8,12 @@ class Zk < Formula
   head "https://github.com/mickael-menu/zk.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "14837986dabab8aaaf4b7ea3997c30c177f92d8c7bd152a688bc7624f836da58"
-    sha256 cellar: :any,                 arm64_big_sur:  "e097162b85a06edf96a6b5e25a941ca259d640fedbf00d9b1ec8a6318d57e46a"
-    sha256 cellar: :any,                 monterey:       "15aa4ad201f17ed789054dbbaf2836d397ba9747abed208963e53335b599b00c"
-    sha256 cellar: :any,                 big_sur:        "2b60d285a933a1b095f2d89815c6a9c17bf386f5d4f2a3908f7009f04da860fc"
-    sha256 cellar: :any,                 catalina:       "ce58623d1d9cacc6a91404290a329edbffc9f2b06f28321afa80b29ac602a4de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb28c15fd8efd70080d3a94a54fdf4a8a5e1c5f347d5b33451cbbf63677296a1"
+    sha256 cellar: :any,                 arm64_monterey: "7583df1a2bc12b3c70dcfdd02484a057d659a7aed167241af27457d3f9a6f2c6"
+    sha256 cellar: :any,                 arm64_big_sur:  "cdc7eb09c7a77ba562d146b72e215f4bb4f0386bc91c56881e5b6c3cefa7d757"
+    sha256 cellar: :any,                 monterey:       "410df6600a4bbbbcef8e61a9c15c0f620988325826a296250fea5c4ba6cf16f3"
+    sha256 cellar: :any,                 big_sur:        "21f5fca1009f3e9239f0109992332b7ba534d00a08dc83913a891f1c4597e2ac"
+    sha256 cellar: :any,                 catalina:       "c6107720d53ce13225f3909ce9c2a47d79539f542e86fa6895f04300fe1214e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "30d712376cba92622014448e341294205410000341838ce5618092f5d5b6cb9f"
   end
 
   depends_on "go" => :build
