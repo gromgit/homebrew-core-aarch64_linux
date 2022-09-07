1fe9b0466f8368a63e53eacf999141525a9bad2e
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:26:51 +0800

    kubekey: no rebuilding required

--- Formula/kubekey.rb
+++ Formula/kubekey.rb
@@ -7,12 +7,8 @@ class Kubekey < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ac9dfd34d27cf27477454c1951cd3fd224321c2e0839a1cf652c7f70354c78d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8157398cf8a15778435bce1b33b4fc7476a0910e3feca058d2d0117cd3c13432"
-    sha256 cellar: :any_skip_relocation, monterey:       "ac42a1034fe7d370d249264bbf8d7ec781631896bc715d0d2250f9fa4416ee90"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6cf0b1bcb708ecca28a216e8917e8189ad6bd50a44e51dac338df0ef73100d30"
-    sha256 cellar: :any_skip_relocation, catalina:       "471957acddb20d7ff1c31cbbdced6dae3ebbd3a73e87004dfaff4742a3c84d9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d94667b2cfae8f37e5e21882310b4ebf54c7291a3507359bcc73041ac9f2f121"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/kubekey"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "42db4a35be1524de8a4e70c18ffeaf9f1ba4998139e379f1b31f1fd3f105fe15"
   end
 
   depends_on "go" => :build
@@ -26,7 +22,8 @@ class Kubekey < Formula
     ]
     system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"kk"), "./cmd"
 
-    generate_completions_from_executable(bin/"kk", "completion", "--type", shells: [:bash, :zsh], base_name: "kk")
+    (zsh_completion/"_kk").write Utils.safe_popen_read(bin/"kk", "completion", "--type", "zsh")
+    (bash_completion/"kk").write Utils.safe_popen_read(bin/"kk", "completion", "--type", "bash")
   end
 
   test do
