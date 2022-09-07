bb51250704dd2f10867f5e01fa3a9ece1971a3e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-07 05:41:25 +0000

    docfx 2.59.4
    Closes #109846.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.59.3/docfx.zip"
-  sha256 "129ab6f8f3683d258ab84771f7ebfd3c18cbcd4bae10e6e138dbe3c233dcaec1"
+  url "https://github.com/dotnet/docfx/releases/download/v2.59.4/docfx.zip"
+  sha256 "6de3058630cd89eeee8157d26a81d508a3ce5c0b64b0473c6360bf10a985a52b"
   license "MIT"
 
   bottle do
