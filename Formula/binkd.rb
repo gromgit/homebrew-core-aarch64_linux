60c74a9e32b1eb999dfc05f754b0d3c620623925
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:17:31 +0800

    binkd: no rebuilding required

--- Formula/binkd.rb
+++ Formula/binkd.rb
@@ -6,17 +6,8 @@ class Binkd < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea0018141c5ebc35b7dca7fb49322682120a2eb5868eb16ff40c8f670123a3fc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4cd8c44619d3c04b646a1ea8ad7db21feb95e950656c78d5dc1a983d6472b469"
-    sha256 cellar: :any_skip_relocation, monterey:       "111b63381a81029c8e2b1618e34d7f6def295e9abd5f6b4c2073b636650ad026"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3b309f2c4a6ee43441a32c0396a570454737bb85c5467f3e27a8c666140bda59"
-    sha256 cellar: :any_skip_relocation, catalina:       "263219b174c193cd86454770d838013daa797994d8d21cdca45493a59d8a5a79"
-    sha256 cellar: :any_skip_relocation, mojave:         "1582b44b77979d7258c51baec8bb54f367fc21f8cc03838c9c1b1351ea9f77e7"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "e890bed8ae5c89dfabd589e2c9654b2c8da6811bd24fbfd99aa4fc520a535e26"
-    sha256 cellar: :any_skip_relocation, sierra:         "d685be9cb23ecb98dc34c2ea185c47ec39e54db1a8ca88782d11cbd96c78862a"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "d69c67a3cb68789a0a96196b5d2d92e44e6dd9bab3eb870ec9727987ae538c35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83ac41663d506bea26d30d168d2b5193f2d42da32ed8530ec8422f990281c3b7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/binkd"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c22507b230be44d9b087a1155bcb03e09f2f1b42c1fa8a92f954b12d406c6a33"
   end
 
   uses_from_macos "zlib"
