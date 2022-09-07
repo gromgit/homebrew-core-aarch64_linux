7542083dc0054063279eeddb01d2905ececad7cb
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:16:40 +0800

    arkade: no rebuilding required

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -2,8 +2,8 @@ class Arkade < Formula
   desc "Open Source Kubernetes Marketplace"
   homepage "https://blog.alexellis.io/kubernetes-marketplace-two-year-update/"
   url "https://github.com/alexellis/arkade.git",
-      tag:      "0.8.42",
-      revision: "cd54f6a171afb07ef27f8e8e23e9cb13a4e3878b"
+      tag:      "0.8.23",
+      revision: "be5a8cc594cbe860055ede8ec36635fcf36ae283"
   license "MIT"
 
   livecheck do
@@ -12,12 +12,8 @@ class Arkade < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fa4e4928144a0528cb6a3bd9c026e0adaa09626b4146aed9edbf627098a42d63"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c7b67647c8511706a3956cebfd2f6164e5a974526238bd26eb580532650a8af"
-    sha256 cellar: :any_skip_relocation, monterey:       "0eca5fc7d660a959292c710981b45874870916e6782f94e8c1dae052cd45c9d0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ff3413ccfb0be599343c0a1d6acd65462b5c949d6532df8d64d8347cedfdb290"
-    sha256 cellar: :any_skip_relocation, catalina:       "943d1677c62a9c0e0c1e7185520367d12d9ea5a76068b015b218917fa4d53238"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bddd9f1e396f9522a3697dfa757fe834337b0813f1e7fb04dac1ea42b46a56a5"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/arkade"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a8e29fd2a31ff720e099d7718dc4043e4423248cd885f8dd22d5a5085138696c"
   end
 
   depends_on "go" => :build
@@ -32,9 +28,11 @@ class Arkade < Formula
 
     bin.install_symlink "arkade" => "ark"
 
-    generate_completions_from_executable(bin/"arkade", "completion")
+    (zsh_completion/"_arkade").write Utils.safe_popen_read(bin/"arkade", "completion", "zsh")
+    (bash_completion/"arkade").write Utils.safe_popen_read(bin/"arkade", "completion", "bash")
+    (fish_completion/"arkade.fish").write Utils.safe_popen_read(bin/"arkade", "completion", "fish")
     # make zsh completion also work for `ark` symlink
-    inreplace zsh_completion/"_arkade", "#compdef arkade", "#compdef arkade ark=arkade"
+    inreplace zsh_completion/"_arkade", "#compdef _arkade arkade", "#compdef _arkade arkade ark=arkade"
   end
 
   test do
