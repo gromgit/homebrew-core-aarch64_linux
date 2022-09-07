c34affbae7aad20789bd067678766ba37c5770e8
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:34:54 +0800

    smartmontools: no rebuilding required

--- Formula/smartmontools.rb
+++ Formula/smartmontools.rb
@@ -4,15 +4,10 @@ class Smartmontools < Formula
   url "https://downloads.sourceforge.net/project/smartmontools/smartmontools/7.3/smartmontools-7.3.tar.gz"
   sha256 "a544f8808d0c58cfb0e7424ca1841cb858a974922b035d505d4e4c248be3a22b"
   license "GPL-2.0-or-later"
-  revision 1
 
   bottle do
-    sha256 arm64_monterey: "de5a9816fe979e1f6e95727f2b23946e8d6594718b0b0fcb77da6b3b01bd47b3"
-    sha256 arm64_big_sur:  "3ae9274990e2845f9d833e267c3443da5d8ef6daedf0293233847710b2116c72"
-    sha256 monterey:       "7df715580bf7adde46c4abac502ee920cd2384be22a279f8da5813db2f974253"
-    sha256 big_sur:        "b353b05f39600a28070f9aeb7c4cff62bf250deff02a03ee8aa4ecabb9639925"
-    sha256 catalina:       "c79a0e6dc93a4e0416b5724cecb1a36d6f0977ae0143698abad407eb013b15cb"
-    sha256 x86_64_linux:   "5702653e6385bc4388d919fcfebe0ee26860627ab3117b117d582fa79f13ae46"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/smartmontools"
+    sha256 aarch64_linux: "bde935e06c2a015f7db660fa91f93dacf2e69234e94b99ba1fa8018b6d46f334"
   end
 
   def install
@@ -25,8 +20,7 @@ class Smartmontools < Formula
                           "--sysconfdir=#{etc}",
                           "--localstatedir=#{var}",
                           "--with-savestates",
-                          "--with-attributelog",
-                          "--with-nvme-devicescan"
+                          "--with-attributelog"
     system "make", "install"
   end
 
