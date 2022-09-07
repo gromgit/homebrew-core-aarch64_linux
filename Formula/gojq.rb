ff1a79dfb62984ec601d8b75725aab2a025189d9
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:23:50 +0800

    gojq: no rebuilding required

--- Formula/gojq.rb
+++ Formula/gojq.rb
@@ -2,18 +2,14 @@ class Gojq < Formula
   desc "Pure Go implementation of jq"
   homepage "https://github.com/itchyny/gojq"
   url "https://github.com/itchyny/gojq.git",
-      tag:      "v0.12.9",
-      revision: "f2e333c56832b92658add0f4712994427ba70919"
+      tag:      "v0.12.7",
+      revision: "7d0d14d3627624028e019a1f9ecca6cdb6231297"
   license "MIT"
   head "https://github.com/itchyny/gojq.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "89452cdc6c06c0a03a6a6b2c888795df2727f3751e1874aa0c3a4c17d26d46a6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89452cdc6c06c0a03a6a6b2c888795df2727f3751e1874aa0c3a4c17d26d46a6"
-    sha256 cellar: :any_skip_relocation, monterey:       "df62983ad243d969a7f893a8259cf26ebbc5ae6f9c369aeca0c07cccb8243c88"
-    sha256 cellar: :any_skip_relocation, big_sur:        "df62983ad243d969a7f893a8259cf26ebbc5ae6f9c369aeca0c07cccb8243c88"
-    sha256 cellar: :any_skip_relocation, catalina:       "df62983ad243d969a7f893a8259cf26ebbc5ae6f9c369aeca0c07cccb8243c88"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba1b202fb288dc5c050c400a784abe0ffa70f8f681d064b562fc446895180764"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gojq"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e41e6185cd31aaf18b2eb0dd709172c81bdc60d73f9f7a26c48442aa7a6d8d19"
   end
 
   depends_on "go" => :build
