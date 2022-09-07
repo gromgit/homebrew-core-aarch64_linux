f5b1cffcc434b29ffdc7ae7ff4c7b12af3b6f96c
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:37:36 +0800

    whistle: no rebuilding required

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -3,17 +3,14 @@ require "language/node"
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.9.29.tgz"
-  sha256 "e19a558beb0b55a683c4686511f29dbe3f5abf3a892e08250155a29e3f4375c6"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.9.13.tgz"
+  sha256 "fa82d0eee96e779f1b1b48bd308617925fa19c065860edeb94ae011e8fd6b7b9"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e567cb83cfe1602b341cc5e5b023d2bb903ade119334ace4e9d47d2f8f105e7b"
+    sha256 cellar: :any_skip_relocation, all: "11f4fa842ae5e2711e1b5ea28a6d87fcfbe559baa5dc6eb297db239f66cb64d8"
   end
 
-  # `bin/proxy/mac/Whistle` was only built for `x86_64`
-  # upstream issue tracker, https://github.com/avwo/whistle/issues/734
-  depends_on arch: :x86_64
   depends_on "node"
 
   def install
