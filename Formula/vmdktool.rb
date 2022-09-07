69ff88b229c35fecf5b8fcb70b37618fb92da795
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:37:21 +0800

    vmdktool: no rebuilding required

--- Formula/vmdktool.rb
+++ Formula/vmdktool.rb
@@ -11,16 +11,8 @@ class Vmdktool < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7823dbeb8f044ce3183f1ea2d6ebec16ce30fc9fe2951cc55b84c5a9043f8569"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2a19ea885fcf69d6929cb155489aab52543b1b1f456eedf9f49f3f6eebf51ec9"
-    sha256 cellar: :any_skip_relocation, monterey:       "9d68c339c6dfb87f9cc70d1df15e36337b2a57cbae54825b24a2c7dc1e4096dc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9f3f1adccbe9d28c54b0009c00866636ab7872914ff6587ccf206f15cb08ac68"
-    sha256 cellar: :any_skip_relocation, catalina:       "2d4faffbb4ae8f2aba0822834278e532c08fad14e8b07ef534415e1535e3c369"
-    sha256 cellar: :any_skip_relocation, mojave:         "13ed1b70d5c6d7f7411df7736940bf9fcd220fa92b229b79558e648cbdc0a641"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "276a35d178515782c7a741a2ebd45c6b47aee0d7ecfd725c386f589e69336fdc"
-    sha256 cellar: :any_skip_relocation, sierra:         "3fa294be9d6e9e6b56435526520262aaa86f5909cc10b9ccf9d9670ae3ac0e3c"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "8604a90f9ad0f3b04767c021a4d24dacdcabd788767df56a45e3913231d4336e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c1d93d16f35a13226a5b332895c50d04badd06732ff6b69094dc1844db8c98d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vmdktool"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "95fbd8556cec01ce3731ec47c66a3e325c8c2611194366faf79f8965b53590cb"
   end
 
   uses_from_macos "zlib"
