e27314676f1a63831836430de44f34d0ce4611d7
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:20:51 +0800

    dtm: no rebuilding required

--- Formula/dtm.rb
+++ Formula/dtm.rb
@@ -1,17 +1,13 @@
 class Dtm < Formula
   desc "Cross-language distributed transaction manager"
   homepage "https://en.dtm.pub/"
-  url "https://github.com/dtm-labs/dtm/archive/refs/tags/v1.16.2.tar.gz"
-  sha256 "99f1c0fb56a4e3211612952fceb2b09d4b6043a955527dc2f0f30a68c6b1fff0"
+  url "https://github.com/dtm-labs/dtm/archive/refs/tags/v1.13.5.tar.gz"
+  sha256 "67a6c145ac548529f93712fd252df8e7df964e01f8c12d97e136eb4b22015d42"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "73ea6d4b78b5ee4c8b69af0b54a165fffcba84e571f59b0e29bf144bbf28f047"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0ee1668df5e2987588d3c096c0434a65f30084dcb5999d332f17a52fb451869e"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c563386499b25e2691996f277b46ebfd03ee29ebea5e9b15ad0650d80fcd663"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7279cb0b9c74abe8d5221485f322009e4b871ad47e377ceeb6fea06e0e9b3a6a"
-    sha256 cellar: :any_skip_relocation, catalina:       "6fb9f46aaee75dd89834c8ef3b8ae211f3f12ea9aced18d34a3ac4431b765fbb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cc06f6585ce4e0696e425dd9758b1ec8c70e62cd0056bb38a1d5c13e4eb01720"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dtm"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cb4b828b2c3955dc7d375dc284645bbc5c4bffcb9838103cf1fca04df1f231bf"
   end
 
   depends_on "go" => :build
