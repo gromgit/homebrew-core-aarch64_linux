f5f08060fd412502d0dbdc1f24d2397e1c147a42
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:16:49 +0800

    atool: no rebuilding required

--- Formula/atool.rb
+++ Formula/atool.rb
@@ -11,17 +11,8 @@ class Atool < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9772f860038b7901645159630516942318faf1c300f47ff7d393b018993c2927"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a498806923cebdf510cefbd1dc5e1dd8e88b8e50e317e3531dccb08d808d09f"
-    sha256 cellar: :any_skip_relocation, monterey:       "ca6b1d8ed7a241588990b84006bd047b05734f9f70a50354d2ea129e0adfdaee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b9492434916b077c29cfadeb04d853748db63bd54e9f4869f4efb03bcc3f00de"
-    sha256 cellar: :any_skip_relocation, catalina:       "78769c7244232e9ba4b403f0dae560e61bc69d08d76936e9797c3f9b18b778f3"
-    sha256 cellar: :any_skip_relocation, mojave:         "afc78205d3558294d008a801f7b06e8fcc94509a34bb0832f914575c196d6f8d"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "0f28ddbd664675c6b3fe440f6cfba6ac8cc6fc1f97141979bbce485080a759f4"
-    sha256 cellar: :any_skip_relocation, sierra:         "656b59fcaa79956c81af4ce21afc06dbf9f6ffaecc0ff52b1a063da2c911fe89"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "dcfdcb720aa3704b9103aa01bb8efac42d24327bc8664baa420a9a69d75a98b6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9772f860038b7901645159630516942318faf1c300f47ff7d393b018993c2927"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/atool"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7be2995f3b711959973cbe9e83535a27c062ee493f1c899da4e4e3e88a6a051c"
   end
 
   def install
