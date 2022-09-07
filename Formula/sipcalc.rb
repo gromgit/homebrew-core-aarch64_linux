8f9227fae5b1fadfdfc5d24beed990a7a68667f9
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:34:45 +0800

    sipcalc: no rebuilding required

--- Formula/sipcalc.rb
+++ Formula/sipcalc.rb
@@ -10,16 +10,8 @@ class Sipcalc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce581c927477b75b12e92cf656e7de4c93c304be04621a7a4a65a8f8aadeaa02"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9eb0d11f79f4a89148dcfba3ff714cad9c345276ce5ca0e8e937782cbc0d0e1d"
-    sha256 cellar: :any_skip_relocation, monterey:       "dc22b86797deef059d90b7bd8c07b235556acbad13e6df0575c99ea30359570a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7ecd4de1c66d06136e36ec83e076b253212294f0407bf049e1bdf7746505c2ab"
-    sha256 cellar: :any_skip_relocation, catalina:       "4b211b4978bd165adb71435e19f19f146ee84f905555c3bce2d4652375067d3d"
-    sha256 cellar: :any_skip_relocation, mojave:         "50bc96758ca5ecdb86fb29ca39bf07f6c4e44192310481436afccc191c6f2cd2"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9cff165f5e2b98d0c7d4729d4d6309b679cae7d161996242c666053d37134640"
-    sha256 cellar: :any_skip_relocation, sierra:         "1ccdaec0a816dde9f7caa0f7a77cd984ece78a61a5886032c4c8821915753482"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "56aa686252ac703ed3dbe91f5737ec4d4b95d52516f4ab52947df15b77d1c58f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32b27f7515668c3962b6f3e629928524026bf005b6ed247449779064bb65e1d7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/sipcalc"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "179b3fa4af79a10957f89971f47d5c50211e40784948ec2df74189f925ed6383"
   end
 
   def install
