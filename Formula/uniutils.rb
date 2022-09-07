56ca422a11df2ad4289f7a41564f9d91d71e00fb
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:36:59 +0800

    uniutils: no rebuilding required

--- Formula/uniutils.rb
+++ Formula/uniutils.rb
@@ -11,16 +11,8 @@ class Uniutils < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "211d63bf402c81b1ff57f098a7bd517090a35f47374c2d92fa6234678bcb613f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8cb5a86b69e6efe758a353744ac48a0ec1777f3b1ed814848906d6365ad7ba81"
-    sha256 cellar: :any_skip_relocation, monterey:       "eece3849b5ed4da3c2ff4a850338a523cfc8fe2d64c7f663147e17d673c410aa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "df42759537263cec13ae2662eac1de96d0692b34e146eff756dbb52b79c7c5d7"
-    sha256 cellar: :any_skip_relocation, catalina:       "c2991a6dc15937fd36591ef3a56134d9474b544a3d0f9407a9555adebf3a53e7"
-    sha256 cellar: :any_skip_relocation, mojave:         "2ea235c47ef4ede643ccb1adaccbc376b0cbe39bbdeb3d5623bcbea210cd7519"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "6717501e797865b956ded03f433b9353e033328727bcdd6263b1f3721c8ef30d"
-    sha256 cellar: :any_skip_relocation, sierra:         "5f6609e92596f48fbb20bd0245437f4e967ebc9d06d9fc5e130584f394dce21a"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "9e83bffe9268c4be17f6e37254da13d2edfeee19869bd075580eeaa8f770078b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8cf517fda500768eb4c44d1595e4f49bd4b9bb20c94981a11db42ac8aded62d7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/uniutils"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8de161626d46038d19429f792ea3d598e180a0c189429602d7ed1d5d65ffe3d2"
   end
 
   # Allow build with clang. This patch was reported to debian here:
