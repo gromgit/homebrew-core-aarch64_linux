a97a9590d4c605bd3c23e66fc956e297227c3cf2
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:17:45 +0800

    brev: no rebuilding required

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.98.tar.gz"
-  sha256 "3917aa3c2ad622294e901a09bcea5b2005ba69100b2729e046d5ea3b0b56df78"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.56.tar.gz"
+  sha256 "a3d2de2475c64d6504cd8013de968f9ad52206f634d7e616d008b079247bcf4d"
   license "MIT"
 
   livecheck do
@@ -11,17 +11,11 @@ class Brev < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c5cc884fa9bca8c7f3436652e5eea80c68f686811e20ea743eca949872ba433"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c483c1f0600f2bf1aeac19bf5d468fe412bb9701c8f0246e7cdd1de0e66b9c82"
-    sha256 cellar: :any_skip_relocation, monterey:       "5956266e9ce5186f6ea70bf2f70e094dd17f34ee43edb674d117d6ef5bf6d136"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e3e773bd37ec39349532767d973ead000d585c9b7116f7da6b99c0c60cc8e101"
-    sha256 cellar: :any_skip_relocation, catalina:       "8417c21d66703e773868bbb4b438eb847b5bde3cbd643d90bf054c4a1d218a07"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61590c4d349be498c8634285313ea920597d9cee7b81cc588a9eb24963a2b166"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/brev"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d902ce6d743c2c6e4c1fa10513bf68f99db9dadfd03acfff5521fb85d942bfc2"
   end
 
-  # Required latest gvisor.dev/gvisor/pkg/gohacks instead of inet.af/netstack/gohacks
-  # Try to switch to the latest go on the next release
-  depends_on "go@1.18" => :build
+  depends_on "go" => :build
 
   def install
     ldflags = "-X github.com/brevdev/brev-cli/pkg/cmd/version.Version=v#{version}"
