aefaf18a47eb39f270618000385918b33422e6dc
Enrico Candino <enrico.candino@gmail.com>
2022-09-07 11:19:30 +0200

    epinio 1.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #109875.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -1,8 +1,8 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "2066d00a6d91cc1b5410b90cad4f88d8e1be9dc4c013db50e80c516bb25c07f7"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "54c194a3877074b3e9790d086abaa3d7004ace5970f856947098dcc65948fdb3"
   license "Apache-2.0"
 
   bottle do
