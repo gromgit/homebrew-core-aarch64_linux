c0d63d181b87f8435fed43db8df7de854e051bff
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:27:08 +0800

    ld-find-code-refs: no rebuilding required

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -1,18 +1,14 @@
 class LdFindCodeRefs < Formula
   desc "Build tool for sending feature flag code references to LaunchDarkly"
   homepage "https://github.com/launchdarkly/ld-find-code-refs"
-  url "https://github.com/launchdarkly/ld-find-code-refs/archive/v2.6.1.tar.gz"
-  sha256 "55ae5bf53691007392f940ae6575c5be37bd9d131ea425e66740ccf5fb24e928"
+  url "https://github.com/launchdarkly/ld-find-code-refs/archive/v2.5.7.tar.gz"
+  sha256 "45f39b225616930091e1746c09520335dcf498fd9947f902a15074fbafe0637c"
   license "Apache-2.0"
   head "https://github.com/launchdarkly/ld-find-code-refs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "badb91b262f990919f4cea6de16b3e58c0d981f8937a5c5cff00953e9c0f03de"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c7469a1ec8a0a1732367b1bb968af7adb4577768e221db111f1ddd143acabd7c"
-    sha256 cellar: :any_skip_relocation, monterey:       "3c9059356699116f9ef7673744a88ec0361fa18a55c6be771743353cd87b8b01"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c9e56dd9ccfc504affed64860b3e5d821dcef3d4592a56258843d8baeecb3c4a"
-    sha256 cellar: :any_skip_relocation, catalina:       "cf1e24ac162b3021036fc4a3216dc5c4f0cb70397f2588037f8f87f2e7ffd02c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d74a6aca65d50a15f614c772d94de0c64f805ac080ad6929e72174c39e8e691a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ld-find-code-refs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "079ce1c5212a8fd6ec3954e503f0e7f30921dd8264f9d38f612601685b3d924b"
   end
 
   depends_on "go" => :build
