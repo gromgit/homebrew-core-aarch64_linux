c429363a64a6ddf200a07c40771ab72f6a902413
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:26:49 +0800

    kubecm: no rebuilding required

--- Formula/kubecm.rb
+++ Formula/kubecm.rb
@@ -1,26 +1,32 @@
 class Kubecm < Formula
   desc "KubeConfig Manager"
   homepage "https://kubecm.cloud"
-  url "https://github.com/sunny0826/kubecm/archive/v0.20.0.tar.gz"
-  sha256 "e6d99e61df5cf787249a6b57c844404c4b6733c08be2f0f2e4f9d36a174ce272"
+  url "https://github.com/sunny0826/kubecm/archive/v0.16.4.tar.gz"
+  sha256 "c4b4db829b909c9047d466850deab2f72ba727ed8d9ee9fd0b26dc2802bd0807"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "48fe08cb1ba840d7e9f767de2ebecab580bbc0815c2d5b022c8025e7a612bb51"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "17acf90a23ac3425245784fd87d64d4d611e26073fe39faed7c8e0f2f2f6ffb3"
-    sha256 cellar: :any_skip_relocation, monterey:       "86e197ba8e1f390354a6b29bf81522a9785e67ac7273190830e995f4a5a7b096"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f2b2a60bd69c4b74615ce235e82164ee5c1c4ebd024db1e05918f47db7a69657"
-    sha256 cellar: :any_skip_relocation, catalina:       "77591d88014105f3f0cfb5c6643db9a382e4af5afdb5ad7c2b530563ce100560"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b51357b0ba3d284fc68b2c4f5a1a93af9785a5f6ed161757dda21e98e69b477e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/kubecm"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "df0d27c9fb3e0ffc4eb786cb1c2afb18cd87410a22297937c175c1a09c5bb4d7"
   end
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X github.com/sunny0826/kubecm/version.Version=#{version}"
+    ldflags = "-s -w -X github.com/sunny0826/kubecm/cmd.kubecmVersion=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags)
 
-    generate_completions_from_executable(bin/"kubecm", "completion")
+    # Install bash completion
+    output = Utils.safe_popen_read(bin/"kubecm", "completion", "bash")
+    (bash_completion/"kubecm").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read(bin/"kubecm", "completion", "zsh")
+    (zsh_completion/"_kubecm").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read(bin/"kubecm", "completion", "fish")
+    (fish_completion/"kubecm.fish").write output
   end
 
   test do
