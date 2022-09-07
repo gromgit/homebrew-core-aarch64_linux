e5d0bbb3c95a5200b8a131c35c93218b9e7804ff
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:27:17 +0800

    libatomic_ops: no rebuilding required

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -1,8 +1,8 @@
 class LibatomicOps < Formula
   desc "Implementations for atomic memory update operations"
   homepage "https://github.com/ivmai/libatomic_ops/"
-  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.14/libatomic_ops-7.6.14.tar.gz"
-  sha256 "390f244d424714735b7050d056567615b3b8f29008a663c262fb548f1802d292"
+  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.12/libatomic_ops-7.6.12.tar.gz"
+  sha256 "f0ab566e25fce08b560e1feab6a3db01db4a38e5bc687804334ef3920c549f3e"
   license "GPL-2.0"
 
   livecheck do
@@ -11,12 +11,8 @@ class LibatomicOps < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2c524cf7b2a2066d9629190b75039283ca008f5a74752cca3b3c7387bed94a6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b6eb5d6a03178df1b704517496c79ddc4ceda06ec3d80e5c1230bd938232e629"
-    sha256 cellar: :any_skip_relocation, monterey:       "a2e24f4fb5d734c6407a87e6d92aad0fa8a6fae7d18c22794661b1f93a6484d7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4b41940cd48c41a9cb04a2ad951430240508c6153c51e97e237091d5618c8f04"
-    sha256 cellar: :any_skip_relocation, catalina:       "cfb78b72dd3fcccea0bb983380505c5b21b55811a9aa81c34996154ce59108b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67609f40170efe9725285e1fa97ae6042acf52eeded974f6e085db707decebd8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libatomic_ops"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2750f4129484e8bb09da388a5f034aa6a4075b48c375db8ea37933afa32ede6a"
   end
 
   def install
