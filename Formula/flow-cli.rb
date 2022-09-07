fa1ef1b2a0c2520e5592613a6c9502a94f4ee56e
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:22:01 +0800

    flow-cli: no rebuilding required

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -1,8 +1,8 @@
 class FlowCli < Formula
   desc "Command-line interface that provides utilities for building Flow applications"
   homepage "https://onflow.org"
-  url "https://github.com/onflow/flow-cli/archive/v0.39.3.tar.gz"
-  sha256 "e51d1db4c01147ca3f94c63c03d532c733055aa7bba608559700f3aac957a5f3"
+  url "https://github.com/onflow/flow-cli/archive/v0.33.1.tar.gz"
+  sha256 "c4ca8713cdb85b62d83005935d5dd60a22805f9ceb82ed5f2e6c43d9f382adf9"
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git", branch: "master"
 
@@ -12,12 +12,8 @@ class FlowCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e4f6b9a471e6869d054495fd9d72fcbae23dfe0722564ca4e6bde6a5bdee288"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "edc6a0c078b73a01383f263d5f20710c32a243b803b7be7f7cd75e4594b98842"
-    sha256 cellar: :any_skip_relocation, monterey:       "540134af7f9d7a3cfc543c29aaa0b8964e0929cc292b43036e36c6fcec70f165"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6e7157105d0bbcf91fa6447d8e5a597397ec05e952801f08a6fb5f423a528049"
-    sha256 cellar: :any_skip_relocation, catalina:       "807a70c30385d78afc176f012f833d83a4e97088104e0104fc21910c0d78ccc5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62b91cfe82381f09e105da07a28b54382cb7605a2088dd6d023ef5e4cab5c4bd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/flow-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "01731ef727173f7735f119129ea00e954ae5f705fe3b87745fa8740dadc06c39"
   end
 
   depends_on "go" => :build
