fe00db5e504bff28091109ebcb48bc07dc8a8b25
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:18:44 +0800

    circleci: no rebuilding required

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,18 +3,14 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.20856",
-      revision: "991bd2aae331cf5668175acc77c7c8fb8e49accb"
+      tag:      "v0.1.17554",
+      revision: "2b4b29543b9a07cf2feeec4c32dca1e4760ede8e"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce173dbc2ee334b4348477616b1242e2d0d63105229d901abe1d7ab40d3fb679"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "292e7454b4e9aa82fe1f1d571c837514a9cd10921d663868aefb6b008fbee4ed"
-    sha256 cellar: :any_skip_relocation, monterey:       "265ba252f3be7a76095836bdc7fbe4c71605a4241894d60adadfc67bb8fbcd8f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "87f1f4f8b807b3a836535b4b0d88797ff91cfce280f72e41022b10320422d857"
-    sha256 cellar: :any_skip_relocation, catalina:       "ef25c84b7e8a17cffc23b2e9b2ee327e97c0d5cbbcf5fad2e0b0e8f36ef6c40e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b8e51198ecb97394fbdeee99b83b63abba4edb64abc6c7dde101e94c5229465b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/circleci"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "54bf2e44fd008a5d51d1441cea5a3d5a598f97b47c7f260190b573b3a732eef8"
   end
 
   # Bump to 1.18 when the x/sys dependency is updated upstream.
@@ -32,8 +28,11 @@ class Circleci < Formula
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
 
-    generate_completions_from_executable(bin/"circleci", "--skip-update-check", "completion",
-                                        shells: [:bash, :zsh])
+    output = Utils.safe_popen_read("#{bin}/circleci", "--skip-update-check", "completion", "bash")
+    (bash_completion/"circleck").write output
+
+    output = Utils.safe_popen_read("#{bin}/circleci", "--skip-update-check", "completion", "zsh")
+    (zsh_completion/"_circleci").write output
   end
 
   test do
