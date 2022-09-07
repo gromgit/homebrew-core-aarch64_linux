46e7c03326b303a928deed47d6494ff4ad188d4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-07 10:03:44 +0000

    vespa-cli: update 8.48.22 bottle.

--- Formula/vespa-cli.rb
+++ Formula/vespa-cli.rb
@@ -12,12 +12,12 @@ class VespaCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "880b00e5ac1aed8f25126fd5a6cdd9bd7b51f00f42b68f6d790c60889832a1f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d8862d6bc57958a2161b375c890ca0ac7f15560c447b9116f9dfb1003aa37863"
-    sha256 cellar: :any_skip_relocation, monterey:       "b068779d1653cb3715a26d021cba6838dc10f9ee0e0517c0435b0823184576ca"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9c555d717f84de6d02f4f721a1a6568947922c78b5d8e33a23f4f520ac07a18b"
-    sha256 cellar: :any_skip_relocation, catalina:       "e88df20c041c73c2a1bf3ae20b0effca548f40e3a4c6816c9a597af1ae86e211"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa1650bb9cb08eb072cec333a8b9b447dca005bffa899d846020d796c918792e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d262a37d6f2c02467a36842daa8a555a4b01cae9907c1315870690821931a79"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a480309133ec75720d7b2f8e233dca9bce294c5ec2aeafcd9ccf35bc513016f0"
+    sha256 cellar: :any_skip_relocation, monterey:       "38e55d71c0d60533a61078120bcbbeb84b916825b7de55deaa996bbe19146a6e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1b70527e3c8c3f2886453066cad8889fdb603960feb63bcd0db785b21c4eb621"
+    sha256 cellar: :any_skip_relocation, catalina:       "f1f4ef84290faba13add6c83af810039464d5c60dc60bc55655dafcd80eab50c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ddd62bb2416a407876a282a2e74b51b97ec76a585bb7a9581201f63dae6fc984"
   end
 
   depends_on "go" => :build
