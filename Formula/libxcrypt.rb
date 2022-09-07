382adc4e5a94bb5e50fd83849c28d3d8e7e40da6
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:28:00 +0800

    libxcrypt: no rebuilding required

--- Formula/libxcrypt.rb
+++ Formula/libxcrypt.rb
@@ -6,18 +6,11 @@ class Libxcrypt < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "c9167b6a2004c17ae0e8c2bfa87b836f46117e2feaf11e81bceb4f10f88a1797"
-    sha256 cellar: :any,                 arm64_big_sur:  "379a8c183e939aae7c2cf36e8e4d6f0d4a48663bfd6ba0a07685840a8644ede7"
-    sha256 cellar: :any,                 monterey:       "10efe49691ec1185d451d7525597fb8f74de2cad09901d0b0e0ea02b2efffb7c"
-    sha256 cellar: :any,                 big_sur:        "bd3ae4e71a255aca43f3ec06111094b6f8d245552388f734c6e4ee2f33089333"
-    sha256 cellar: :any,                 catalina:       "bcda7c42bf3a2021aa83bd00a8fcb3158f02e791f792eccceda4257bda67cb6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84269a82afe1a9e94ba6828c983a4b8e65d8d672d36d88232daac832017ab327"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxcrypt"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3d85fe1e4f17d9369169dd496d0c78a4e85d88b836322d9ecbaaa20eabd9de3a"
   end
 
   keg_only :provided_by_macos
-
-  link_overwrite "include/crypt.h"
-  link_overwrite "lib/libcrypt.so"
 
   # Fix -flat_namespace being used on Big Sur and later.
   patch do
