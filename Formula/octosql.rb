a288b6c221ee9944f4428775b02830a17d118968
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:30:53 +0800

    octosql: no rebuilding required

--- Formula/octosql.rb
+++ Formula/octosql.rb
@@ -1,18 +1,14 @@
 class Octosql < Formula
   desc "SQL query tool to analyze data from different file formats and databases"
   homepage "https://github.com/cube2222/octosql/"
-  url "https://github.com/cube2222/octosql/archive/refs/tags/v0.9.3.tar.gz"
-  sha256 "129ce13c45ab2d2d4a7f0db2fc1e63a93d38e80f929f9b387b8c530f9be56b59"
+  url "https://github.com/cube2222/octosql/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "859af30e263756d9fd9bd5cd6bc987ab057de7c9a89794be80371f6e4355ae11"
   license "MPL-2.0"
   head "https://github.com/cube2222/octosql.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb01845a33431e8b0ccc3e6c7913341347d9116c2facb0473746f180702321b3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "635e89addfb8ad8712858ad53955c96920c1def582f5027b7b38bf3b15a2f862"
-    sha256 cellar: :any_skip_relocation, monterey:       "ee6cf273a562a9aa42d1b7dc06cf0c8d84628d7c6313aaae94aa20423417d244"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b8dfb66ee3b61cbca2e943d1a1fbf782bce619d82ddf61fc56b2877beee95eae"
-    sha256 cellar: :any_skip_relocation, catalina:       "9f5941ab3ce494840c1d8c5d5850ad3951be3ef2e3a3cae13c9c0e550027114a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a583135ce1753863e82e779b40446b4687559b20dbafbea6401d1c2e1605e50"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/octosql"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c5883e9841a25d1998a2849223375d173f9b6c619c672f2576c750095f9e4fdb"
   end
 
   depends_on "go" => :build
@@ -21,7 +17,14 @@ class Octosql < Formula
     ldflags = "-s -w -X github.com/cube2222/octosql/cmd.VERSION=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags)
 
-    generate_completions_from_executable(bin/"octosql", "completion")
+    bash_output = Utils.safe_popen_read(bin/"octosql", "completion", "bash")
+    (bash_completion/"octosql").write bash_output
+
+    zsh_output = Utils.safe_popen_read(bin/"octosql", "completion", "zsh")
+    (zsh_completion/"_octosql").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"octosql", "completion", "fish")
+    (fish_completion/"octosql.fish").write fish_output
   end
 
   test do
@@ -34,13 +37,13 @@ class Octosql < Formula
     EOS
 
     expected = <<~EOS
-      +---------+--------+--------------------------+
-      | field1  | field2 |          field3          |
-      +---------+--------+--------------------------+
-      | 'value' |     42 | { <null>, 'eulav',       |
-      |         |        | 'value' }                |
-      | 'value' |     42 | { 'eulav', 24, <null> }  |
-      +---------+--------+--------------------------+
+      +-------------+-------------+--------------------------+
+      | test.field1 | test.field2 |       test.field3        |
+      +-------------+-------------+--------------------------+
+      | 'value'     |          42 | { <null>, 'eulav',       |
+      |             |             | 'value' }                |
+      | 'value'     |          42 | { 'eulav', 24, <null> }  |
+      +-------------+-------------+--------------------------+
     EOS
 
     assert_equal expected, shell_output("#{bin}/octosql \"select * from test.json\"")
