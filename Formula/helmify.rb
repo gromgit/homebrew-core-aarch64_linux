9e19655012745b5a6a33fee180cd9b850dcbf318
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:24:48 +0800

    helmify: no rebuilding required

--- Formula/helmify.rb
+++ Formula/helmify.rb
@@ -2,18 +2,14 @@ class Helmify < Formula
   desc "Create Helm chart from Kubernetes yaml"
   homepage "https://github.com/arttor/helmify"
   url "https://github.com/arttor/helmify.git",
-      tag:      "v0.3.18",
-      revision: "13d7c886579bb702fc9e710281c06db30800ace9"
+      tag:      "v0.3.12",
+      revision: "3634ef55f022db202f123e43e55ebb15161e1892"
   license "MIT"
   head "https://github.com/arttor/helmify.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6bb4f8b23d0d0599db25cc8ff8a05076e7c825a176e88a4b1ba20ab508d657f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56371bbf7545036d83b7d6c2da4caef6dbf23e25d8579c41f86eba4f72ed8949"
-    sha256 cellar: :any_skip_relocation, monterey:       "b51afa95b7047718aae8b73cdb2fd86665f18c67560dfeda486d76479a20b676"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2e58b84cc234731806da2d3b677d04f84d90fab126fd7e9324d4510e7200d871"
-    sha256 cellar: :any_skip_relocation, catalina:       "58321a4febb76b550746fdbe17504961d16995ae5cd7157e982201d76b224724"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eac877fe73c7851970d47f55efb58c1eb8c44253ede8498a6424310d7daa4078"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/helmify"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cc04ee220acaccbbc35e1fa3fe78c753d1a872f80cad68ee0812e686b0627230"
   end
 
   depends_on "go" => :build
