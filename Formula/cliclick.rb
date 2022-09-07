89340ab28e7c31c326062db9fc0774dc83acfdf2
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:18:50 +0800

    cliclick:

--- Formula/cliclick.rb
+++ Formula/cliclick.rb
@@ -1,17 +1,13 @@
 class Cliclick < Formula
   desc "Tool for emulating mouse and keyboard events"
   homepage "https://www.bluem.net/jump/cliclick/"
-  url "https://github.com/BlueM/cliclick/archive/5.1.tar.gz"
-  sha256 "58bb36bca90fdb91b620290ba9cc0f885b80716cb7309b9ff4ad18edc96ce639"
+  url "https://github.com/BlueM/cliclick/archive/5.0.1.tar.gz"
+  sha256 "798fb8b26f6a42b5002ca58e018b91f7677162c4f035b38aee8d905829db64a7"
   license "BSD-3-Clause"
   head "https://github.com/BlueM/cliclick.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f06719b325bfd00c2aa4af5d8f4017c1d84b85228b191955d63a0d064ffd219a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c7a5cbf3668f3cc8ef45be0f90d2de0bb5803abaf750d0ca77d1f19464e81e22"
-    sha256 cellar: :any_skip_relocation, monterey:       "021849385e2be5067946b18bb27d5fdad68d82d40d776f4e6ef98abe45379d68"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2a52d08ab8c32e39912191316022fbc11e264b41cebe15ce6276e1a73d801011"
-    sha256 cellar: :any_skip_relocation, catalina:       "65b6fcb0620720f8cc572bd3cc7ab260664e39629b9ff4fdf26e5fa24f81e6ea"
+    sha256 aarch64_linux: "45492fb95de6a5adad25722737543da5f5b1b1b0a26c1816138fb9b43673ab37" # fake aarch64_linux
   end
 
   depends_on :macos
