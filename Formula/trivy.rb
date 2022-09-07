80d7a16e798c7ab6bf7fcbe2950039c5dd9b9647
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:36:39 +0800

    trivy: no rebuilding required

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -1,18 +1,14 @@
 class Trivy < Formula
   desc "Vulnerability scanner for container images, file systems, and Git repos"
   homepage "https://aquasecurity.github.io/trivy/"
-  url "https://github.com/aquasecurity/trivy/archive/v0.31.3.tar.gz"
-  sha256 "1eed7e948cbe5a41963cd8c59caad49c54d043b662dcfe45a9f1a6a88e52a111"
+  url "https://github.com/aquasecurity/trivy/archive/v0.27.1.tar.gz"
+  sha256 "68efac954a31d33c438c5604fa1269aba164f275c7580260f3c8c033a3be2ac7"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/trivy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1787a579a70f4311700da31efcb9e091176ae5c688d8fdb072c8647adfb2ba5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05b4ee6c113e3fc5fbd37bd42fbe69a679126a8e88aea209d978cdc76cc8ae37"
-    sha256 cellar: :any_skip_relocation, monterey:       "df8a19d2dc3c657c3a48b824a2188c26bf2962e651d20bf2ca2b218683bcedb8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "280e9a5b109c3a16ea82b160c86970d1ab2980f272ea5153b95706f5d4284fd8"
-    sha256 cellar: :any_skip_relocation, catalina:       "65258b29ab15ce44dd94c106300e49bedf957a998d0684875a1bd47832d1ea6e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10ff0896e973eae042425b1e99851ec3ebe5291d3003dc4382ecf005ea98f10a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/trivy"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ffd9ffe0b7f10e520ab91db14504438d2557191d63b2f17bd2c55616bcdee38d"
   end
 
   depends_on "go" => :build
