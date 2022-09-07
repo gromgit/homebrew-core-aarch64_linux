a79c5e637c8450bfbc58807a5cdc5db7b655103b
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:21:16 +0800

    envv: no rebuilding required

--- Formula/envv.rb
+++ Formula/envv.rb
@@ -6,16 +6,8 @@ class Envv < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "023e55714270ba7a388b290d867a4fd334a962b33f4bd9d0ed8de513ad7c034b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "21e6f6e3c94dd0f14178ba1d5a53317bf1a6bf269762b5b79d9f93eff1ae3f00"
-    sha256 cellar: :any_skip_relocation, monterey:       "f58b94b534868176986117c6f3f2eb470388cc439c98308de6b2e99db8990c0a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39f8b46cce79836ebbc2281f1836a30eb2440e5af70bdc251469c0cca36f7828"
-    sha256 cellar: :any_skip_relocation, catalina:       "54b7b425a3db83134fc9038b8672bd84a943413f5386d9cef92711eeaaade467"
-    sha256 cellar: :any_skip_relocation, mojave:         "59acc1f13ed58898376a14ffcb23766f62ff7c0446eebb3ee8aa1f8162f0994c"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "35e2781067a3f5429c36546a20faca9d4762882bf3908122efc58c8b752968e9"
-    sha256 cellar: :any_skip_relocation, sierra:         "cc30a2317f78124c609d6313a33cea58c9d428a95903966da4cb42051630ef97"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "3b2fb0b35749280461b3982797ceea34bfa42d63cb5c6547986cf106669ee744"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cb0c8481042567e17ff04b7d5dd3cc46fccbc0e321203770791588fc51d11b7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/envv"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3a650fad1da4cb14dc201362d79f64ae9df0fb97d69ec8beb0b4fa57d42ff061"
   end
 
   def install
