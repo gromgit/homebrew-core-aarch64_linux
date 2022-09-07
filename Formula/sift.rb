35823186d2a88c35af3bb01706416a01c8c04b62
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:34:42 +0800

    sift: no rebuilding required

--- Formula/sift.rb
+++ Formula/sift.rb
@@ -8,16 +8,8 @@ class Sift < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c93899f610215c5f879a7dc895c6d549573bde6389ecd1b5624bf119e585d7a6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28fd605e32b0dbf9526939fd5e366c46f5ad0c45b2a5e9cb2bb7fb8815a7f97c"
-    sha256 cellar: :any_skip_relocation, monterey:       "eaa4c4e84606b8dd32e3064819fc05f16b22a76f0be493bc283a2d256c050ff8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "08978d211e26b262c551418ca7c6d93f2b05a0e7887e10831a5a70f23f445e8f"
-    sha256 cellar: :any_skip_relocation, catalina:       "d2c0f8e64a32bcbaa45976a350d302dd13e3b68595162d69005dae7599d9be40"
-    sha256 cellar: :any_skip_relocation, mojave:         "08aae3031f30b5502bd93b26c4a2e655077f3a91c212b04898c19d14444ec0e6"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "b0d584ae926816c4f525c9070cb67c7622e851c3cbba67e7c0b9cae5d30feb00"
-    sha256 cellar: :any_skip_relocation, sierra:         "42fbf76075951fd28a27b4e2763b3af58eb93b0260c3a3c82719d7a32ef7baec"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "6ee1bdf8b60fe3c3528a4a2698f19518a7bf71838ceba58ab9a199a6624f3dba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d9feea7d6e51c11b1a56043ed9b15731dac5440dd4eb492c1468ef0497a1af9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/sift"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a0f3fcf33ba09f1c489c02dc312ba47b8a4666c406d3eee10500cc797b788b0b"
   end
 
   depends_on "go" => :build
