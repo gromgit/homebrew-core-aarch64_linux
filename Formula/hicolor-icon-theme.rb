f971f614b6c1d97c767115ee9869c0f8d1c129d6
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:24:50 +0800

    hicolor-icon-theme: no rebuilding required

--- Formula/hicolor-icon-theme.rb
+++ Formula/hicolor-icon-theme.rb
@@ -11,16 +11,8 @@ class HicolorIconTheme < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1153e436e03439c74a2eae87b6b5fbb6c8192607420dd5b01d0295a8ef29d2e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "16e984b4dc9809fb223592f9b2c8a45cc49e796bd3b03f0b9507e80953167292"
-    sha256 cellar: :any_skip_relocation, monterey:       "1153e436e03439c74a2eae87b6b5fbb6c8192607420dd5b01d0295a8ef29d2e5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f4cd50751f22d1aae6156ce3e552dbe0afb21ce1aaa5a7cc7ce284c867a20865"
-    sha256 cellar: :any_skip_relocation, catalina:       "8ba8d6065b652396583c55a0e73cff0007f96064a330ac20499ff1d887771eb8"
-    sha256 cellar: :any_skip_relocation, mojave:         "5ba4bb6a7e89f5fb0d43504d68d657a536be9540d4cc72552bd5965e15a82b91"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "b33f58b98a6ca6bb72777eaf7b7a4bb393d5cc9ced6954dd7a7e52e18c214799"
-    sha256 cellar: :any_skip_relocation, sierra:         "cd8699f3944eb87b76fc89e4ca69f19df5d66aa8a4c89d636660d299e807f5b0"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "cd8699f3944eb87b76fc89e4ca69f19df5d66aa8a4c89d636660d299e807f5b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1153e436e03439c74a2eae87b6b5fbb6c8192607420dd5b01d0295a8ef29d2e5"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hicolor-icon-theme"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6525db05b2d7b7870938e04b1d7700c229160081dcc218681b0e4172d620b88e"
   end
 
   head do
