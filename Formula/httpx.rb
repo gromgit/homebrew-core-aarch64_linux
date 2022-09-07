749c68714b04d10ea6e8f94034805cd14fd8949e
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:25:01 +0800

    httpx: no rebuilding required

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,18 +1,14 @@
 class Httpx < Formula
   desc "Fast and multi-purpose HTTP toolkit"
   homepage "https://github.com/projectdiscovery/httpx"
-  url "https://github.com/projectdiscovery/httpx/archive/v1.2.4.tar.gz"
-  sha256 "5d95a35586c851bba43be2fb79b790ff417e58aa34ae515a79b3bd8c6f2df7e0"
+  url "https://github.com/projectdiscovery/httpx/archive/v1.2.1.tar.gz"
+  sha256 "ff7c623adc0594ba3da2f4f806513c0542bb4da32ea0ddcd3ee0f4413b0217da"
   license "MIT"
   head "https://github.com/projectdiscovery/httpx.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8847479dcfd206f2b400c59275b5c89c99f61a432f3f9b4c0b26872509480d15"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e16f44d2f3ffaa3f0826ee09d02917587474aa353e1b3c3cbcf135c6c47b1b90"
-    sha256 cellar: :any_skip_relocation, monterey:       "35d1275e6be68e1ecff9d1df7000e10943a67a059756fa114f8bf93cd5526bb8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "49d8597fec0d9e68d1365ecf7d8b8d8a825073c0f8ad7b49057c11b17bf7308f"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b1f4f21353bd9d413209252e44ba281f1a363e2ebede6f697f32d7dfd244b85"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19f6383086cd06ca7bbade724e0852f83e53a5a2eb9bae00a56cbec588e263ae"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/httpx"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "38e14a6ae736844bc7458d1cbd8742ede6f554951478026599babffebd517507"
   end
 
   depends_on "go" => :build
