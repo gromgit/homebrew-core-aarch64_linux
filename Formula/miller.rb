b1a7b030c4683d019d0f2a918a62ebd1bb98ec89
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:29:21 +0800

    miller: no rebuilding required

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -1,18 +1,14 @@
 class Miller < Formula
   desc "Like sed, awk, cut, join & sort for name-indexed data such as CSV"
   homepage "https://github.com/johnkerl/miller"
-  url "https://github.com/johnkerl/miller/releases/download/v6.4.0/miller-6.4.0.tar.gz"
-  sha256 "20a8687a0c5b5fedf4fc3a794ef1cee7e9872e87476e1f24bde8de25799f8c51"
+  url "https://github.com/johnkerl/miller/releases/download/v6.2.0/miller-6.2.0.tar.gz"
+  sha256 "da26531fe9bca1c1917ea41dd1f1d7338c61f50275ad3ea41d3bc4685da34687"
   license "BSD-2-Clause"
   head "https://github.com/johnkerl/miller.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e8a075f599c7d6a2f0ce13d802e47e9d4d59134cd93b598b09eb0473128fe7d9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "62d9c454310272bd9c0df8b30ae7022471348012d6381024ae83a5f7f0cec250"
-    sha256 cellar: :any_skip_relocation, monterey:       "ed9eb29e632e431dda2df12ca7fb4455f497a3c82fe4fe669620c2472fddea97"
-    sha256 cellar: :any_skip_relocation, big_sur:        "76478dd9ec9682086846c7818b863c4dfdef97c0ef5af409ec586d638ceacf1d"
-    sha256 cellar: :any_skip_relocation, catalina:       "863621c64345cbe99cd55f536f3a60a2b82f11c5bfe426d75b938a85c9b21698"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "703ae98f955923ebf40485811b7d7e1b26c7ca028e25e2152fb0192af4124612"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/miller"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0e0f7475e7934f1bfd72f4a0c5fa5bae4d9b710e4cafa24b18ea1961640d3cd2"
   end
 
   depends_on "go" => :build
