51860cf47a6673db4f4667ce6a57f8d16c29fdb0
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:32:29 +0800

    png2ico: no rebuilding required

--- Formula/png2ico.rb
+++ Formula/png2ico.rb
@@ -12,17 +12,8 @@ class Png2ico < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_monterey: "065215647e66fd79ec6412ce65189d5f26ecda3e6f71220707e57952351a8c80"
-    sha256 cellar: :any,                 arm64_big_sur:  "af73312990d3438e1a996e9f22cd034805b4851b2fa13d8fae17437e8123538b"
-    sha256 cellar: :any,                 monterey:       "df5fa87e241b6bf89efb2fc809cc499151ca2911030b33aa53547b6837810a35"
-    sha256 cellar: :any,                 big_sur:        "b1fd25cc9bdcb94af6aa9bfa1a3b3fb401561e1c923ba5d88eef9fd12dd62678"
-    sha256 cellar: :any,                 catalina:       "dfe2ebcf6a6b8c7e97e7b80c9d98aa46b27c27de7ace88464750d8db61aadf55"
-    sha256 cellar: :any,                 mojave:         "52180eb9b080ae4cfbe33f441e0119d2cbcd2654c2b7c7d1b37120912215df95"
-    sha256 cellar: :any,                 high_sierra:    "986b5a9efe66ddeec63f2f523a36214f0bbf3ce43a9697c83adb3c237912f38b"
-    sha256 cellar: :any,                 sierra:         "63d789e767bf5fdfd3b26102441a7331531d83215c73fa61ae2b548ecf08ea74"
-    sha256 cellar: :any,                 el_capitan:     "6b3b8e132ff06ed21308e73e1a30a3b74a593092e56dc94693c27ae4d03add09"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52b7eb707f96b3b8526ca15ce86c442247f0e4c34112ccef3ed22fe6cafb5a3b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/png2ico"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "36f5c90e597f5dda353772c739fd0e0dd75e96f41f3a84b9f882fcf44faf60e4"
   end
 
   depends_on "libpng"
