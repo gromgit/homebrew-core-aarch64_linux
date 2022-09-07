27701f58d24a1944f1733ad0df0fda81f323b74e
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:37:25 +0800

    wakatime-cli: no rebuilding required

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -2,18 +2,14 @@ class WakatimeCli < Formula
   desc "Command-line interface to the WakaTime api"
   homepage "https://wakatime.com/"
   url "https://github.com/wakatime/wakatime-cli.git",
-    tag:      "v1.54.0",
-    revision: "67bc07eb6e1cd2f4ede81faa8cdf53e2554327b0"
+    tag:      "v1.44.0",
+    revision: "c81cc0f3d9833453c88f2e959eadf2bca325d662"
   license "BSD-3-Clause"
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "544271958fcea23c76c485bae8b3fb78eac10fe0462f572d8f6f8871bd0f0e57"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "edfe4520455be489ccb02a5ff50bc45ecb0df237bc711ad3ff93db6e59953155"
-    sha256 cellar: :any_skip_relocation, monterey:       "a9eba8061030c52e6d43c76ad3ff2a1d1c94a1556217518f7888b76e3af05de3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2b46432ef295139f165c85a96f30da0dab301c47256585f2d01f7bf84300aa4a"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8a1fa148eb7f87175d978f8a2eaf9550e9d5a6e569c15cf1ee2702f402e02e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2f7aa3dea5a90c4f180ce0a46a1efe9a46967270068e04cfb402ad79cc736c0"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/wakatime-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bf10134e667ca4898c76dc6f5f95db4d96633d53995217d4f7f18334969142f8"
   end
 
   depends_on "go" => :build
