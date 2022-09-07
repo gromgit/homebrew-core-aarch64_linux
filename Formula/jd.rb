b7914252e014b3565e21277d24671e33cc8a3a12
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:25:47 +0800

    jd: no rebuilding required

--- Formula/jd.rb
+++ Formula/jd.rb
@@ -1,17 +1,13 @@
 class Jd < Formula
   desc "JSON diff and patch"
   homepage "https://github.com/josephburnett/jd"
-  url "https://github.com/josephburnett/jd/archive/v1.6.1.tar.gz"
-  sha256 "3e81a8dadb125628430a31e3ea67b8b3053c346d684b74784c6bf0451cc2def0"
+  url "https://github.com/josephburnett/jd/archive/v1.5.2.tar.gz"
+  sha256 "ff6cf8fabe65a5786652ab2a3ef762d5dc6b5595a9516f81eb0916fdfe13bab6"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "150ed5128112728670d002d0e499b64b33cf9580c52696c42a85a2bf8b74eec4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "464a7b3b8736329c9415ec9613730f9827cf4481e6907eb51e3b801080b6f682"
-    sha256 cellar: :any_skip_relocation, monterey:       "fa9d0b17b270bf97d46e0a64789b3fbfcc779586b999f0e14f87246dfc690bf4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "98deeb2becde902f3ea470330433f430543054624619512b20dc33c273fea970"
-    sha256 cellar: :any_skip_relocation, catalina:       "9a71da0f9235ca3ff552451fdc1c5219d7391dd267b09622932077351a793b45"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f2eefc094cd2d2e9b17dfcd46b04652974e73d9cc8c72c77a114eb12fa0e75f2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/jd"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5e62562661430bcf25116b7ad84fdf66236b561ecb071dec11b53270d3fb5ab9"
   end
 
   depends_on "go" => :build
