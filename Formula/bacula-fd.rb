acade0fe393ee5484ca4feb1f7b831606c1f1e05
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:17:05 +0800

    bacula-fd: no rebuilding required

--- Formula/bacula-fd.rb
+++ Formula/bacula-fd.rb
@@ -1,17 +1,12 @@
 class BaculaFd < Formula
   desc "Network backup solution"
   homepage "https://www.bacula.org/"
-  url "https://downloads.sourceforge.net/project/bacula/bacula/13.0.1/bacula-13.0.1.tar.gz"
-  sha256 "d63848d695ac15c1ccfc117892753314bcb9232a852c40e32cca88c0e918978a"
-  license "AGPL-3.0-only"
+  url "https://downloads.sourceforge.net/project/bacula/bacula/11.0.6/bacula-11.0.6.tar.gz"
+  sha256 "0195a08bcd4f578ae4a9ce0d91f7f86731c634d56b810534722d721b2a9eecb7"
 
   bottle do
-    sha256                               arm64_monterey: "42489dd6be579995365266af6ac1e9e0efb0f059114ca11aea2e403d7f07c6bc"
-    sha256                               arm64_big_sur:  "10036f12b6348491a9b6721c8df2a1ea79158e6e8c50710aeece83b513c9bbaa"
-    sha256                               monterey:       "bad36d8e6a56caf6ec72230e48d71fcf513a09c7dc019a24f1fa3970f6509d1b"
-    sha256                               big_sur:        "4026b2d20490a6be9d3648e74b074f615c54af6257d9dbb8b2a6608e5bda8e26"
-    sha256                               catalina:       "aee4a24eac06896ca037c18918c264e73e71c5e5ab10cb64758a4baf85ee411d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48579a00a95f1864cb86adf6b9e43bb59faee5b76e7f78d20c9e20159d0e627f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bacula-fd"
+    sha256 aarch64_linux: "09fbd728bd0b5f08462061c6afa8cd94df206e334e7987497e185e13c68ea43f"
   end
 
   depends_on "openssl@1.1"
