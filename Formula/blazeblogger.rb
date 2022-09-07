142b9a331e07a70731a8111ce8e6f2f3823fd714
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:17:36 +0800

    blazeblogger: no rebuilding required

--- Formula/blazeblogger.rb
+++ Formula/blazeblogger.rb
@@ -6,17 +6,8 @@ class Blazeblogger < Formula
   license "GPL-3.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "971cf089545d50d6b751253a1ffe63b4bc3cb10c55287bf5f43cabcee9088afa"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7c1a4c1663a7da5774e534eec48e6ab245f22d20f1f98a3186b7877f8bc4bcf"
-    sha256 cellar: :any_skip_relocation, monterey:       "6cbe0fa6acb72e64523b98192c50de16a12c919e91a7467136876d25c60372fe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "595702af4db6b12264bffdfa430b29c55474e15fcb9fc95cf7dec509dbe1bce5"
-    sha256 cellar: :any_skip_relocation, catalina:       "cb9f78c2ae445f20f90c62b634fa4ee32ac282fc0a005099dcb5593b5008f99e"
-    sha256 cellar: :any_skip_relocation, mojave:         "c7350b4fc7cb74eb436f431aed0e54160bb2da31593f623573b6396287342148"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "7cb9d122a9c892a89d36a886c2be63536ca339def18d2766fde8f96e87c0d0cd"
-    sha256 cellar: :any_skip_relocation, sierra:         "8e6e405d5b586a95006ab1f47d2f5cef961a2dbdaa9759fb4427663edcd12adf"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "0d6bf439fa6f880cb9457581da66082f49f514f8b0fd4b57ac81180948aaa5e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "534e43639b7ee566bbc9f675f94043e4579b0e01ba1b8b5651dcd308f640a182"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/blazeblogger"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "95b91a60db03ed0ed29054d209ebb03feacd86f1aa9cbf383ca1f9f7d13f25fd"
   end
 
   def install
