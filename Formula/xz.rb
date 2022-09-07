5a2cf96b24cf27f7b3330396c31fc2b0da751e87
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:38:02 +0800

    xz: no rebuilding required

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -1,14 +1,11 @@
 # Upstream project has requested we use a mirror as the main URL
-# https://github.com/Homebrew/legacy-homebrew/pull/21419
+# https://github.com/Homebrew/homebrew/pull/21419
 class Xz < Formula
   desc "General-purpose data compression with high compression ratio"
   homepage "https://tukaani.org/xz/"
-  # The archive.org mirror below needs to be manually created at `archive.org`.
-  url "https://downloads.sourceforge.net/project/lzmautils/xz-5.2.6.tar.gz"
-  mirror "https://tukaani.org/xz/xz-5.2.6.tar.gz"
-  mirror "https://archive.org/download/xz-5.2.6/xz-5.2.6.tar.gz"
-  mirror "http://archive.org/download/xz-5.2.6/xz-5.2.6.tar.gz"
-  sha256 "a2105abee17bcd2ebd15ced31b4f5eda6e17efd6b10f921a01cda4a44c91b3a0"
+  url "https://downloads.sourceforge.net/project/lzmautils/xz-5.2.5.tar.gz"
+  mirror "https://tukaani.org/xz/xz-5.2.5.tar.gz"
+  sha256 "f6f4910fd033078738bd82bfba4f49219d03b17eb0794eb91efbae419f4aba10"
   license all_of: [
     :public_domain,
     "LGPL-2.1-or-later",
@@ -17,12 +14,8 @@ class Xz < Formula
   ]
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "345b942fb105c850d6243d91502fa285408cfcc7d78d9aaf27322acea608e901"
-    sha256 cellar: :any, arm64_big_sur:  "08ae5dd072653e49b921bc68ed69cbd36581f32fa12d49658e9481f55990a88d"
-    sha256 cellar: :any, monterey:       "853c17e5062001c0bf5d59875b7fcda3610438747680187586f96db93395fc11"
-    sha256 cellar: :any, big_sur:        "e2d22d68de9a20e44053eb9dbcc64a6a0beb5ab33b6d6653b4fea0e42342a948"
-    sha256 cellar: :any, catalina:       "2cc7627ad8dedb17a04f341b2a43e1307f7bd4f1516d344ca52547ec732ae305"
-    sha256               x86_64_linux:   "607a3d993f45efe858d3e4f002603e323a1b1f0c87b4db6fb57d1280f479809d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/xz"
+    sha256 aarch64_linux: "da5e7e118ca463e353d573323d241d04f18cc6fff2069db312c6f5b143008831"
   end
 
   def install
