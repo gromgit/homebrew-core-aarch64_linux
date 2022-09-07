650004eef69e5787b3772f1d4f7a065fef2c6459
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:33:38 +0800

    regula: no rebuilding required

--- Formula/regula.rb
+++ Formula/regula.rb
@@ -2,18 +2,14 @@ class Regula < Formula
   desc "Checks infrastructure as code templates using Open Policy Agent/Rego"
   homepage "https://regula.dev/"
   url "https://github.com/fugue/regula.git",
-      tag:      "v2.9.1",
-      revision: "b46cb8745cf9c50de1d89d8c196c6b11401d2c78"
+      tag:      "v2.6.1",
+      revision: "0d40e66c4bec929779b8bb5cde5f22ed1e522ede"
   license "Apache-2.0"
   head "https://github.com/fugue/regula.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c93dfb33e51d1dc517922649778ce442a0803e707babaef8b0d31654a134c93c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2a5173af76c35839829f988649d68a3d6a3106227d80891c282bd85842e0e743"
-    sha256 cellar: :any_skip_relocation, monterey:       "851dcbe5d267c09b1b64d2464e0bbd761ab5a24b6f8bb230b8e70611f2f4af31"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4d2fcea9ea5a34b78be9400d9929178c75fb7207da4b32dff2a4ebe3392ec813"
-    sha256 cellar: :any_skip_relocation, catalina:       "f4e40bc48603ff64b46183b96c719ef5a5d02c21f4f5f52435f19ba27ae3d12a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad0de2fc459acefe703b91374169422679129b10627791618764b52ae3f283d6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/regula"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2a1927a2dad3747c06dcae950fe410e4b525658790a60e1035e75df334b83b30"
   end
 
   depends_on "go" => :build
@@ -27,7 +23,12 @@ class Regula < Formula
 
     system "go", "build", *std_go_args(ldflags: ldflags)
 
-    generate_completions_from_executable(bin/"regula", "completion")
+    bash_output = Utils.safe_popen_read(bin/"regula", "completion", "bash")
+    (bash_completion/"regula").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"regula", "completion", "zsh")
+    (zsh_completion/"_regula").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"regula", "completion", "fish")
+    (fish_completion/"regula.fish").write fish_output
   end
 
   test do
