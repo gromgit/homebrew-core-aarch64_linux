654a6a25198fc136cb3188359d38ea3cd5c89a4c
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:33:07 +0800

    qpdf: no rebuilding required

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -4,7 +4,6 @@ class Qpdf < Formula
   url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.6.3/qpdf-10.6.3.tar.gz"
   sha256 "e8fc23b2a584ea68c963a897515d3eb3129186741dd19d13c86d31fa33493811"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
@@ -12,15 +11,11 @@ class Qpdf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d73e9ba981fe6c063254b713d7a224b20b96f8395ed8adbab64793a5e9075d28"
-    sha256 cellar: :any,                 arm64_big_sur:  "e1b12dc5b9fa7bf382d82482349e3863b2896702c5f20abfbe54d293cd0bf06f"
-    sha256 cellar: :any,                 monterey:       "e7dadd66b88ebbb70f2f675063efd6b2db51cc2229fdcf5f4455e94755a3bad4"
-    sha256 cellar: :any,                 big_sur:        "584b60be8b5b00a8a13cac2ed0f91570d647fccc9bf784ed470f36f46feeafc8"
-    sha256 cellar: :any,                 catalina:       "a094fb34fd6834d6d813fcdbc9cc93f1f8a6c77f140867cdb10ff7f890493d11"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "72c36f09952ffc50cbde983d248609d7c3be695103348ebf1cdafc84b3d8a608"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/qpdf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "caf6cbab295a2124da7306a2e63eb83417645a6a5b182b56d8101681363722f9"
   end
 
-  depends_on "jpeg-turbo"
+  depends_on "jpeg"
   depends_on "openssl@1.1"
 
   uses_from_macos "zlib"
