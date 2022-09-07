4f4f451c65d264ccd98a5a4b92480cb02d8ae303
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:35:23 +0800

    staticcheck: no rebuilding required

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -1,18 +1,14 @@
 class Staticcheck < Formula
   desc "State of the art linter for the Go programming language"
   homepage "https://staticcheck.io/"
-  url "https://github.com/dominikh/go-tools/archive/2022.1.3.tar.gz"
-  sha256 "b95691c8e7cfff90120249dbd827ee021f23031a498e201713d4dec23deb5974"
+  url "https://github.com/dominikh/go-tools/archive/2022.1.1.tar.gz"
+  sha256 "988756d12527bf9843459a30cc6fbbf2a7d6d75e62326b0387eab6273e992543"
   license "MIT"
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "796446395086877328615acec37c81a1edc4410c49f179516ac6260934a46638"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "67447db0bf46527f21fc2ac8b3a11c0628516c20d8ab7da5799993ec674ff23b"
-    sha256 cellar: :any_skip_relocation, monterey:       "303e35bb9bd4b378f7ed86cdb6d58c2b54cd16818ede6d9f5fbd8b4a2001a3a4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e689ef99aebf15b523e668a507ae791352a0bdc608c672ef476a002ca3365305"
-    sha256 cellar: :any_skip_relocation, catalina:       "1638b6f0c9fa5e3f7604ebe780118af69cdf6e0c658cf88e64ca162b25d375eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "015fea1a4798a30c0aff8b612f5f1ac00e8716504c43b4f6f05dc976c686a823"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/staticcheck"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e8efd5ad598b5c80ec158abc30d480b40decc3e9136e3329449f19ec8b7179a4"
   end
 
   depends_on "go"
