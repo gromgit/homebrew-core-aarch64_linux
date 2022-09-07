bc6e097f68108d6702b2705d670aaf0b45e518ee
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:21:54 +0800

    flarectl: no rebuilding required

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,18 +1,14 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.49.0.tar.gz"
-  sha256 "43044abb5bafa20a7fda9ecfb952388bd1d016ebaa28b3e5c4a4fb6277ad6e83"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.38.0.tar.gz"
+  sha256 "d024a5cc1187084ed1c3ac3f46bf029c8e138ed8d20b1ff262e19f895724ad40"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "03b06e5a589fb9577dae88ea1167bbbe91b704e1d21a8d624056eb09954fea1c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99f9390c3c904a5754c93e1eaced90220a98f50e28aa68d1e38673dfa881add8"
-    sha256 cellar: :any_skip_relocation, monterey:       "5719ce1d5763bcc7c5fdd4c5aa90ea0fabcc28eaf3e49e2bbe9b21abc97296e7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ccea9ae9e89a25a81723b4ffc6c2cafdfbad8c27710dd4d424662b2b4915f87d"
-    sha256 cellar: :any_skip_relocation, catalina:       "1a243664741f1305ab4a8dfb51faa34ba7a4474c84c47055fa70473baa5262b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "924453664da3a568f7b7efea8bc241486a20bfec109043729be94efd778a1dce"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/flarectl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b10ce43bbf6784bcfbe4e4cf3d036a733236db7901305943efe40dd0e9aec2c7"
   end
 
   depends_on "go" => :build
