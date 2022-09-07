1040dac1ab464fe99396d6f09ba0077600e2c7e2
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:34:03 +0800

    ruby-install: no rebuilding required

--- Formula/ruby-install.rb
+++ Formula/ruby-install.rb
@@ -1,13 +1,13 @@
 class RubyInstall < Formula
   desc "Install Ruby, JRuby, Rubinius, TruffleRuby, or mruby"
   homepage "https://github.com/postmodern/ruby-install#readme"
-  url "https://github.com/postmodern/ruby-install/archive/v0.8.5.tar.gz"
-  sha256 "793fcf44dce375c6c191003c3bfd22ebc85fa296d751808ab315872f5ee0179b"
+  url "https://github.com/postmodern/ruby-install/archive/v0.8.3.tar.gz"
+  sha256 "e2f69949757d032d48ee5c028be020bdc8863c41d5648b53328903d2e16ab3b2"
   license "MIT"
   head "https://github.com/postmodern/ruby-install.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e0fe47d5a5ca9f84ffe59b40d48981341a0ab937342069d7f79c51dd4b95af0e"
+    sha256 cellar: :any_skip_relocation, all: "dc8ccd83e5f95f77186431f37297226dfc15042ab835da617c03dd052e73e6b7"
   end
 
   def install
