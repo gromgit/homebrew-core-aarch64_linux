ca0ecaea9cfdf11c931c45e2b9b4a39dd9e350e3
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:35:27 +0800

    stern: no rebuilding required

--- Formula/stern.rb
+++ Formula/stern.rb
@@ -7,12 +7,8 @@ class Stern < Formula
   head "https://github.com/stern/stern.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b70d60729da9ae4aabbf89522296774d0cd5c72c857bca9feb9798b8b3d4aaae"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "091dd476809a0005987aac1b3b504f3228ca02e47d685978cb4fc00e9a6ebfd2"
-    sha256 cellar: :any_skip_relocation, monterey:       "ff3e1b21a6a10d56010f47ee3ddada080cf9486ba6854f57093c7513b9856249"
-    sha256 cellar: :any_skip_relocation, big_sur:        "432ad8338d36e21c10a41e2480172fa6799eed8b7294709bafab80a040c47a63"
-    sha256 cellar: :any_skip_relocation, catalina:       "d58b0773d75096a4003b2a84484c5544e5df33f492bae485f69694b3593a84e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4455d5cb038d7134bf34b9b6678a2258da4ce950a540860b0d4290232403eab4"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/stern"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7e533acfa6771d62b190bdb55b13bb8c8d9d45edf1fd276c6c6704606475c3f5"
   end
 
   depends_on "go" => :build
@@ -21,7 +17,14 @@ class Stern < Formula
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/stern/stern/cmd.version=#{version}")
 
     # Install shell completion
-    generate_completions_from_executable(bin/"stern", "--completion")
+    output = Utils.safe_popen_read("#{bin}/stern", "--completion=bash")
+    (bash_completion/"stern").write output
+
+    output = Utils.safe_popen_read("#{bin}/stern", "--completion=zsh")
+    (zsh_completion/"_stern").write output
+
+    output = Utils.safe_popen_read("#{bin}/stern", "--completion=fish")
+    (fish_completion/"stern.fish").write output
   end
 
   test do
