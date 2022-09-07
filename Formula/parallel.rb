11998dcb68ebf441ca4453a47a58d3db289321e1
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:31:46 +0800

    parallel: no rebuilding required

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20220822.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220822.tar.bz2"
-  sha256 "9d0d4457c40b45ac6034f3085a11fee94489e5d58e422c0b492cb143d71ab016"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20220422.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220422.tar.bz2"
+  sha256 "96e4b73fff1302fc141a889ae43ab2e93f6c9e86ac60ef62ced02dbe70b73ca7"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
@@ -14,31 +14,23 @@ class Parallel < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5bf2e34ec842abea40e7366e58a7f9bf3b6ab0a37fd7546ed50e36ab812ae35f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5bf2e34ec842abea40e7366e58a7f9bf3b6ab0a37fd7546ed50e36ab812ae35f"
-    sha256 cellar: :any_skip_relocation, monterey:       "08d4b2cb728aa314f047ce81e3f0442e792a39661ed89f47632cd6f1d20501ce"
-    sha256 cellar: :any_skip_relocation, big_sur:        "08d4b2cb728aa314f047ce81e3f0442e792a39661ed89f47632cd6f1d20501ce"
-    sha256 cellar: :any_skip_relocation, catalina:       "08d4b2cb728aa314f047ce81e3f0442e792a39661ed89f47632cd6f1d20501ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5bf2e34ec842abea40e7366e58a7f9bf3b6ab0a37fd7546ed50e36ab812ae35f"
+    sha256 cellar: :any_skip_relocation, all: "2e78960d0de32d093a48f6512b71b60ad6cc37a604aad812d67dc51b7326e898"
   end
 
   conflicts_with "moreutils", because: "both install a `parallel` executable"
 
   def install
-    ENV.append_path "PATH", bin
-
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
 
     inreplace_files = [
       bin/"parallel",
+      doc/"parallel.texi",
       doc/"parallel_design.texi",
       man1/"parallel.1",
       man7/"parallel_design.7",
     ]
-
-    # Ignore `inreplace` failures when building from HEAD or not building a bottle.
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX, build.stable? && build.bottle?
+    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
   end
 
   def caveats
