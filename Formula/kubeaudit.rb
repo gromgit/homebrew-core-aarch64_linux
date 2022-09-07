fba67963aa8533e50e6e0599f1cba39d8ca1aaeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-07 05:55:48 +0000

    kubeaudit 0.20.0
    Closes #109849.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubeaudit.rb
+++ Formula/kubeaudit.rb
@@ -1,8 +1,8 @@
 class Kubeaudit < Formula
   desc "Helps audit your Kubernetes clusters against common security controls"
   homepage "https://github.com/Shopify/kubeaudit"
-  url "https://github.com/Shopify/kubeaudit/archive/refs/tags/v0.19.0.tar.gz"
-  sha256 "846ad5000e43e37c6089332f964d421a72fdded710066b58de8dcae655627749"
+  url "https://github.com/Shopify/kubeaudit/archive/refs/tags/v0.20.0.tar.gz"
+  sha256 "352a1c9d3d3a2492da8ab4ab20b19185f2277112821eb72c3aa06cd1c10c6c4e"
   license "MIT"
   head "https://github.com/Shopify/kubeaudit.git", branch: "main"
 
