102daa724b5e0228ab536c6b580c6996d560bf2b
Adrian Ho <aho+rpi9@03s.net>
2022-09-08 02:29:11 +0800

    sbjson:

--- Formula/sbjson.rb
+++ Formula/sbjson.rb
@@ -7,11 +7,7 @@ class Sbjson < Formula
   head "https://github.com/SBJson/SBJson.git", branch: "trunk"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d3cb04de93bf5460c41623c296b5cd81884379d277afc874a64b0ff1cc34ac6a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "695ced76533bfe9a4e893ffe22ea58402dbd93bd180e2ca4b4a96004d5c60581"
-    sha256 cellar: :any_skip_relocation, catalina:      "e703b87ff205bfec1cfc09e9c200ebca6be643df15ec99b85c590110a4885fb2"
-    sha256 cellar: :any_skip_relocation, mojave:        "8b145bcfef84733c00d94e57cbe0eac56a7981654cda6068ff219264353b25bd"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "649463e051c03596a72400a04b95f993222f5ba6d42a879241291660fef8605c"
+    sha256 aarch64_linux: "45492fb95de6a5adad25722737543da5f5b1b1b0a26c1816138fb9b43673ab37" # fake aarch64_linux
   end
 
   depends_on xcode: :build
