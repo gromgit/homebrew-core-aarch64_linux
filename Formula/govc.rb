c7cd9f8b5bbb3766f8afd069250d25103667217a
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:24:08 +0800

    govc: no rebuilding required

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -1,17 +1,13 @@
 class Govc < Formula
   desc "Command-line tool for VMware vSphere"
   homepage "https://github.com/vmware/govmomi/tree/master/govc"
-  url "https://github.com/vmware/govmomi/archive/v0.29.0.tar.gz"
-  sha256 "0b517b04ac8709a4ef3d92421b555710b67a5234f5e5e2e8d6c3479aee01697d"
+  url "https://github.com/vmware/govmomi/archive/v0.28.0.tar.gz"
+  sha256 "6bfc60ff105b0ea2839f574bc84e9f66d5886c2a7769c359686b01a49818651f"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9957781456f042e13183f10c15592cf18be29b3b7e429d5bf6a93478b517141a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3205e16957f1a1b3a019550677d8531fc3875b68339b15c7892d7d4c7526366b"
-    sha256 cellar: :any_skip_relocation, monterey:       "39ac20022024122d20aa0336caa2f75a60107e547c8c45215a0c1da1c09caa98"
-    sha256 cellar: :any_skip_relocation, big_sur:        "31bdfaac215c256960b0665dbaeb81ce898e41d5ddfd381439d39a45a2164cb9"
-    sha256 cellar: :any_skip_relocation, catalina:       "fe23c3a9bb07e9abbf172aed5e3b1a6d0b6cf7c46504ad6c5946aef2a816036e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2aba7f2a11bd35be3a0a3c64cf6a68941be599f2ad1ecc127298308f29514ea5"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/govc"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3ebe97c2eb5bd1c1c6d8b54df5f9f0f5c71b3cf284a6a1ad31e634e90f67ddeb"
   end
 
   depends_on "go" => :build
