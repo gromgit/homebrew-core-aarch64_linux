c0479da2af370db9b9ec57b3624a14b45068c8f0
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:35:31 +0800

    strace: no rebuilding required

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,13 +1,13 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v5.19/strace-5.19.tar.xz"
-  sha256 "aa3dc1c8e60e4f6ff3d396514aa247f3c7bf719d8a8dc4dd4fa793be786beca3"
+  url "https://github.com/strace/strace/releases/download/v5.17/strace-5.17.tar.xz"
+  sha256 "5fb298dbd1331fd1e1bc94c5c32395860d376101b87c6cd3d1ba9f9aa15c161f"
   license "LGPL-2.1-or-later"
-  revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "938a66a86d6392a261e2feb0c533310fd3bb2f5b26ed504b1fd40c73cd7d069b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/strace"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "66f9e72e2ab2463610babf0feb24598a5bc39817d01da306e772ca83d254c803"
   end
 
   head do
@@ -17,7 +17,7 @@ class Strace < Formula
   end
 
   depends_on :linux
-  depends_on "linux-headers@5.15"
+  depends_on "linux-headers@4.4"
 
   def install
     system "./bootstrap" if build.head?
