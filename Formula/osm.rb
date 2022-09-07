2496f973ccdcf7dd9f5dd2b5276b60f0980c80e8
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:31:33 +0800

    osm: no rebuilding required

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -2,8 +2,8 @@ class Osm < Formula
   desc "Open Service Mesh (OSM)"
   homepage "https://openservicemesh.io/"
   url "https://github.com/openservicemesh/osm.git",
-      tag:      "v1.2.1",
-      revision: "0b6a18f080d3e29922332464ffa81a8457f847e1"
+      tag:      "v1.1.0",
+      revision: "a23afae930033c22438008c1f730f13c7408d951"
   license "Apache-2.0"
   head "https://github.com/openservicemesh/osm.git", branch: "main"
 
@@ -13,12 +13,8 @@ class Osm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4faf805c4c67dd4f32a568903fe3c5f13c617e066d8b2e54da38d64f90d72021"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7a9bd8e723337350814d60152b8eb3452e1a5fa74d705ea11588f825285ac4f"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e5f480b48f4558c71e4d92a0905bc8a0c0d2c6e8cbdf866b6f7cd35c71302fc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "48325be15038b809bcbf508f752555b5737ca158941af2ea61c125ad4842c282"
-    sha256 cellar: :any_skip_relocation, catalina:       "612fa0aee77eee6b2db2b4085d2519d22fc53815bfac3d25ae45aa50d56811e3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5bbe8d33a1e61d83f6b1fa4b3c0f28b78bc9f75f1786bfd0f29b526bedcc6a67"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/osm"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "913c7f73bd94486c78716698c9080120ba52b72fd7f3f53429a42338b7d0eaa1"
   end
 
   depends_on "go" => :build
