4f1570faf8395d30257fe6ad28d8e6f74c1aa08f
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:16:13 +0800

    alsa-lib: no rebuilding required

--- Formula/alsa-lib.rb
+++ Formula/alsa-lib.rb
@@ -1,8 +1,8 @@
 class AlsaLib < Formula
   desc "Provides audio and MIDI functionality to the Linux operating system"
   homepage "https://www.alsa-project.org/"
-  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.2.tar.bz2"
-  sha256 "8a35b7218e50f2a2c79342d0de98ded81439ce19e12809385ec9be9596de7c2f"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.6.1.tar.bz2"
+  sha256 "ad582993d52cdb5fb159a0beab60a6ac57eab0cc1bdf85dc4db6d6197f02333f"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,7 +11,8 @@ class AlsaLib < Formula
   end
 
   bottle do
-    sha256 x86_64_linux: "9e9409e572680975dd401de6d0466ebc5b8bf2832cd013f2c88fc2022d48addb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/alsa-lib"
+    sha256 aarch64_linux: "b5c841671f9a3fc3acdd4d7c788edc46f27eeae460e7230b4d3cf81ec9840bc9"
   end
 
   depends_on :linux
