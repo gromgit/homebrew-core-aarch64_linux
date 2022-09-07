c54568905c5947d242199e6c3b695e6dff875278
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:16:07 +0800

    algernon: no rebuilding required

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -1,9 +1,9 @@
 class Algernon < Formula
   desc "Pure Go web server with Lua, Markdown, HTTP/2 and template support"
   homepage "https://github.com/xyproto/algernon"
-  url "https://github.com/xyproto/algernon/archive/refs/tags/v1.14.0.tar.gz"
-  sha256 "2d30fe7a3f7c9b985f5fde7d6035888ad0c31ae4342fb38a96404de320ccd883"
-  license "BSD-3-Clause"
+  url "https://github.com/xyproto/algernon/archive/1.12.14.tar.gz"
+  sha256 "cab5b01923142e0326ea2a01797814bb2e8ea9f7c6c41a3ea0ae7df3b667e86e"
+  license "MIT"
   version_scheme 1
   head "https://github.com/xyproto/algernon.git", branch: "main"
 
@@ -13,18 +13,15 @@ class Algernon < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "343a3153a0af6ad0fc1ad27939eb9a2dbc9f379bd5bf6b4e373b9fd2f87d3ed4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "401c99640d17d66677f419c8da8719618c499e02e2b6f70b74eefec104378912"
-    sha256 cellar: :any_skip_relocation, monterey:       "a0f090b5d513eedc54de13a6f954a33ac0a126ea2b1bd1dde009629bd0854707"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3f25029f7b5dcf462b1b6e3d3a69e246d881201a7f9ab35657cd11dd25916f6a"
-    sha256 cellar: :any_skip_relocation, catalina:       "fff5b9bfa21524d8f9bff2266978dc7dcb4a8a6a35a19eb20397d3993a0e3445"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1fb0615287ba76cf2372cc91154d458c881d175e877b4140eeab7487282a7269"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/algernon"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c7f77e9d279b0d4859f08c97506baee7b75a0b2a447231b001837ef59c790072"
   end
 
-  depends_on "go" => :build
+  # Go 1.18 can be used after the next release (1.12.15 or later).
+  depends_on "go@1.17" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "-mod=vendor"
+    system "go", "build", *std_go_args, "-mod=vendor"
 
     bin.install "desktop/mdview"
   end
