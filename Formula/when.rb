412184a0875611b5030095f1721043d7acfd5049
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:37:35 +0800

    when: no rebuilding required

--- Formula/when.rb
+++ Formula/when.rb
@@ -1,18 +1,14 @@
 class When < Formula
   desc "Tiny personal calendar"
   homepage "https://www.lightandmatter.com/when/when.html"
-  url "https://github.com/bcrowell/when/archive/1.1.43.tar.gz"
-  sha256 "ddc3332aeadb4b786182128d3b04db38176b59969ae6980a7187d40b14528576"
+  url "https://github.com/bcrowell/when/archive/1.1.42.tar.gz"
+  sha256 "85a8ab4df5482de7be0eb5fe1e90f738dfb8c721f2d86725dc19369b89dd839d"
   license "GPL-2.0-only"
   head "https://github.com/bcrowell/when.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, monterey:       "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, catalina:       "2f9df2c93da3424c51eb901c46f847a99ba1f4c025a2759bd36c467ed9e0eeaf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c6f757cbf95f81a1f836702132f50e5c782f9be02069f4df3ce4e93a5012dd0c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/when"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9a067c0ab5cf248ca681ff2e5cf459cbf621220b02883c2eed10bd75e5fb6090"
   end
 
   def install
