06c294f848ea2e75d13bb5c9efa2531000de1bad
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:21:59 +0800

    flint: no rebuilding required

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -1,8 +1,8 @@
 class Flint < Formula
   desc "C library for number theory"
   homepage "https://flintlib.org"
-  url "https://flintlib.org/flint-2.9.0.tar.gz"
-  sha256 "2fc090d51033c93208e6c10d406397a53c983ae5343b958eb25f72a57a4ce76a"
+  url "https://flintlib.org/flint-2.8.5.tar.gz"
+  sha256 "5911fedff911100f15781f35e3a4fa934fe60e4aea02a8c10cc8918101c1eed8"
   license "LGPL-2.1-or-later"
   head "https://github.com/wbhart/flint2.git", branch: "trunk"
 
@@ -12,12 +12,8 @@ class Flint < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "be89510010a3268664926b3b400a6bfb04c68bbc49e1458db1ade0d394cbc585"
-    sha256 cellar: :any,                 arm64_big_sur:  "c1ba1710148d555a57c7b0ae9623c5799af577c3cdafb8286f57bd623eb93528"
-    sha256 cellar: :any,                 monterey:       "9f90ceb53de5d8d10c75074ab6aa4b8d634bc532b9e3afc91b61c8e0e849518e"
-    sha256 cellar: :any,                 big_sur:        "1337e5c2c7937e5a4d86946c2d15741d55fa7a0b54b99ea552cdec1e18807ce2"
-    sha256 cellar: :any,                 catalina:       "3149763887d901d8f4c322b8bdac03c1118c285dfd72df588facadf02e24ebb3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4449052b84499bc199348182c456d61bd4f1ce6d1ac4020a74d045d0b670bc8b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/flint"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "af1fc7e4c917fe42a4fa4f546452272dca9419eac6bfd5bc49bf01b3c4cc596e"
   end
 
   depends_on "gmp"
