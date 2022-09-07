655b165a661dedcec6c6d2c05bfd23c7d2ed1b50
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:37:37 +0800

    wildfly-as:

--- Formula/wildfly-as.rb
+++ Formula/wildfly-as.rb
@@ -1,8 +1,8 @@
 class WildflyAs < Formula
   desc "Managed application runtime for building applications"
   homepage "https://www.wildfly.org/"
-  url "https://github.com/wildfly/wildfly/releases/download/26.1.2.Final/wildfly-26.1.2.Final.tar.gz"
-  sha256 "dd2a97e7bf32a13adfc6782868afb5e48fb033b83480fc5bf94128807a83b17c"
+  url "https://github.com/wildfly/wildfly/releases/download/26.1.0.Final/wildfly-26.1.0.Final.tar.gz"
+  sha256 "c8a478e7a57daeb767d88cd63de2b26e9e22562bed77a09a74b39c489ec5d7e3"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,11 +11,7 @@ class WildflyAs < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "b82e2694c5e900adcf712db3a6892557107b4f6e13b191ecaf6f97345babbc68"
-    sha256 cellar: :any, arm64_big_sur:  "b82e2694c5e900adcf712db3a6892557107b4f6e13b191ecaf6f97345babbc68"
-    sha256 cellar: :any, monterey:       "39845e499ea7f079f75fc1b31991672af90f35f28a7c6b69df69ed04ad2dfa5f"
-    sha256 cellar: :any, big_sur:        "39845e499ea7f079f75fc1b31991672af90f35f28a7c6b69df69ed04ad2dfa5f"
-    sha256 cellar: :any, catalina:       "39845e499ea7f079f75fc1b31991672af90f35f28a7c6b69df69ed04ad2dfa5f"
+    sha256 aarch64_linux: "45492fb95de6a5adad25722737543da5f5b1b1b0a26c1816138fb9b43673ab37" # fake aarch64_linux
   end
 
   # Installs a pre-built `libartemis-native-64.so` file with linkage to libaio.so.1
@@ -52,10 +48,38 @@ class WildflyAs < Formula
     EOS
   end
 
-  service do
-    run [opt_libexec/"bin/standalone.sh", "--server-config=standalone.xml"]
-    environment_variables JBOSS_HOME: opt_libexec, WILDFLY_HOME: opt_libexec
-    keep_alive successful_exit: false, crashed: true
+  plist_options manual: "#{HOMEBREW_PREFIX}/opt/wildfly-as/libexec/bin/standalone.sh --server-config=standalone.xml"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+          <key>Crashed</key>
+          <true/>
+        </dict>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_libexec}/bin/standalone.sh</string>
+          <string>--server-config=standalone.xml</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>JBOSS_HOME</key>
+          <string>#{opt_libexec}</string>
+          <key>WILDFLY_HOME</key>
+          <string>#{opt_libexec}</string>
+        </dict>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
