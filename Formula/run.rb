ae95347df4a23d440c5cd4ea16f20bfc7a7f0a22
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:34:05 +0800

    run: no rebuilding required

--- Formula/run.rb
+++ Formula/run.rb
@@ -1,17 +1,13 @@
 class Run < Formula
   desc "Easily manage and invoke small scripts and wrappers"
   homepage "https://github.com/TekWizely/run"
-  url "https://github.com/TekWizely/run/archive/v0.9.0.tar.gz"
-  sha256 "f7bd2dbcf41b27f67797c2b51291245f64dcf4aef243df205aed22f6f321c4d7"
+  url "https://github.com/TekWizely/run/archive/v0.8.0.tar.gz"
+  sha256 "60b4f20152be20f0d7b76adb98ac49eff5f283d9e748165508fd54b62603bb1e"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8048f0c0b0672b58f87c1f62d82902cf2040436591f3fc69f0ca50f60c5257e1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8048f0c0b0672b58f87c1f62d82902cf2040436591f3fc69f0ca50f60c5257e1"
-    sha256 cellar: :any_skip_relocation, monterey:       "8fe61559884892ff9ac980ef6002d13ee1edc54de63045845dfd8532a81377a4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8fe61559884892ff9ac980ef6002d13ee1edc54de63045845dfd8532a81377a4"
-    sha256 cellar: :any_skip_relocation, catalina:       "8fe61559884892ff9ac980ef6002d13ee1edc54de63045845dfd8532a81377a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5a91284adc9fb7d1a2b67cd30dc365f37ef420b1f53e050426f7fe3a3a1f038"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/run"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "643cdcabbeb640d974eda4cc6123f98f873f00d995b40187111b90ca65ccf02c"
   end
 
   depends_on "go" => :build
