05cb5f19e4cece98ec32089d8a3654b4e4c731b1
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:20:24 +0800

    disktype: no rebuilding required

--- Formula/disktype.rb
+++ Formula/disktype.rb
@@ -12,16 +12,8 @@ class Disktype < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "154bd7b1f165caf396b4c1659fb1af90f8a64cfdcdf47a421d4d6ee2af32e555"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4587dd61a91f93d065f3f169b690f8f194d1177c5b3cb7a78c0edec9bc0a23a9"
-    sha256 cellar: :any_skip_relocation, monterey:       "edc7efe783d43679fea498893be6c511023d8ccf7d823eaf05ca57cde41202e6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "06ea5af49f19f974e3d7f91f9a8e9e178f90b5e8390c59c324179773e17e21ac"
-    sha256 cellar: :any_skip_relocation, catalina:       "6821d802c4418c949b8e3394893f03cf6152020881096b304ab0c87313fff2e3"
-    sha256 cellar: :any_skip_relocation, mojave:         "7b401cb017bbe0f119b590839941ca7a8d77136483f651504382ed595f4280ec"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "b6212feab524e86a8fc1f3c366092af206dee279900ea2753d331b295dd22c14"
-    sha256 cellar: :any_skip_relocation, sierra:         "18ed63d389b55d3dabb84e355323f303013acd46a1905c194b470cc74fc95e4f"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "c1f45dc2bdcec2e3b56741bf03d673f3a99534f851d1c77de59d6832d0f75236"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4b0dcc67cc8fee509011e50ff1299b4205b424f83ee9aedff5d97fb2e603b6bc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/disktype"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8bbaecf29ea23d563448ab04aba6838d9ad9107ff4ae1a8e0a6319436c760811"
   end
 
   def install
