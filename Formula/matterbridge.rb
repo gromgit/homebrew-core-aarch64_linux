4bdf65451c798d951e03080776c33e1d968d6377
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:28:58 +0800

    matterbridge: no rebuilding required

--- Formula/matterbridge.rb
+++ Formula/matterbridge.rb
@@ -1,17 +1,13 @@
 class Matterbridge < Formula
   desc "Protocol bridge for multiple chat platforms"
   homepage "https://github.com/42wim/matterbridge"
-  url "https://github.com/42wim/matterbridge/archive/v1.25.2.tar.gz"
-  sha256 "e078a4776b1082230ea0b8146613679c23d3b0d322706c987261df987a04bfc5"
+  url "https://github.com/42wim/matterbridge/archive/v1.25.0.tar.gz"
+  sha256 "c8ae52a07d06f416ba9439f0b8fa9163c6f19ca4520a941eb4f6aa5452682017"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a6d54394cacd4fac5f2aceb85a80f287b74f4534ff971b77de2021be54954e45"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "62838814ac245e65dce8998e5b009d9ee99849897c81af44199bd9a3c44db20d"
-    sha256 cellar: :any_skip_relocation, monterey:       "d91055137b8898b9f133108ad6e07a643c5a4453140e8857c5ea45a1973ea639"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f52d497a23d97561087e1762a8a0ec878549fdec336ba3625c1089e581d27f0c"
-    sha256 cellar: :any_skip_relocation, catalina:       "eeda6283cc709b2c85b7f6bdd62e7115edc2c3db730d96a6d277cf1abc9ed75a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7a176037043824d9aa1cda3ca9d06527db5a2b3ed1c7f481e0857980ae21dbe"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/matterbridge"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1f3e7c278959ab808a99076be91600870c43745486fa63b392f6a3dab6c9d30c"
   end
 
   depends_on "go" => :build
