534c4e69866658c5432aa181e7a9a3835e15cb7c
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:18:00 +0800

    c2048: no rebuilding required

--- Formula/c2048.rb
+++ Formula/c2048.rb
@@ -8,16 +8,8 @@ class C2048 < Formula
   head "https://github.com/mevdschee/2048.c.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "aa6a9009c2300a5ecb2dcde91e5c416363d5293e2e166a715eb4792c33e188a2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89bc7f84063b8621271115ee8f9c84c836ae1e57a72db5533b11d8247d57e043"
-    sha256 cellar: :any_skip_relocation, monterey:       "011a8529dc50ea6349f9b1d288ae11fac8e4a4969372c66036b31d158e960b5b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "abe17673a8930d93f04d97f5e209621b83968e28a075578e9b1f42c07464145d"
-    sha256 cellar: :any_skip_relocation, catalina:       "727165d714b210f559b5f5450d6608bed0e7bfbf87c7a7cd5994259b65865411"
-    sha256 cellar: :any_skip_relocation, mojave:         "dd0cc60f407ccb43f471d7123b9a09fa0b2161ee083638a432ee25795a96ca8f"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "e5f553baf87fc7ac9f0fa4471d3e9be29328df167700181d9663f61293436888"
-    sha256 cellar: :any_skip_relocation, sierra:         "d2f33783cf7cd2ac69eaed113d940aca31e02e5863fcdb40e200e3fe9a4d0623"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "8f9e75196f87718be0c572f731cecba0c8cd4e8dc35f8b3027392cd6e1c45f5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a13bac58f461c8fff9257a3af3f01569d0bf819248ddbf5840bbf1fc9492adfa"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/c2048"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "abf5af88d96d7ea1f0f26c67e7649b568740866194675ac1012698d4b5f5f386"
   end
 
   def install
