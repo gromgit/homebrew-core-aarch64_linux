992aa43330bafe973786a3bb3eff7c7a4eccdc7c
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:23:25 +0800

    glktermw: no rebuilding required

--- Formula/glktermw.rb
+++ Formula/glktermw.rb
@@ -11,16 +11,8 @@ class Glktermw < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "78f059cc1bcee84516edb414bf2e837c249da02ca26894cdf38961d7d4b54c23"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c24eb437d3ffab00f829aec8fa2bec0846d97bd6cf34d8801235746aca61760c"
-    sha256 cellar: :any_skip_relocation, monterey:       "d9504855781898144107ee963ecb9b7d2e9d5d0cc0f0d413657a52412a45dab5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d68c298b6f2c6a8b4275207809d3e075a3d8a0f4545660c3474803073385781e"
-    sha256 cellar: :any_skip_relocation, catalina:       "6959c1eebd57b190196abeee87a79bceffe67dfb1454c78d0068a576648fc4aa"
-    sha256 cellar: :any_skip_relocation, mojave:         "cbb9467a9639470772bb010a05c79b396ab12bad33726f2fe6cb60dc29bda9b2"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "c8ecb98e15edfdb02c5aed42590291e45c0dae29640a209428f1382991a23a2a"
-    sha256 cellar: :any_skip_relocation, sierra:         "8f62b5b2b920573742886d31a7c579b174bb60fad1bfeabae346f8893dc440cf"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "5b302ada83cd6185c262277c3836d9e071a050a677fd41d86cab31aa0e8257d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6227f4c828cb24588c204db1c1a820204f2046fc22b573b71e7e3f6971909430"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/glktermw"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b5c4251740272eb1223f4598b84805715102eb17c0adb9b47723263696fa8ebe"
   end
 
   keg_only "conflicts with other Glk libraries"
