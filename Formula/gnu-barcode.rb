18351d5857c29c13a283abe4236cd3e6681c5053
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:23:30 +0800

    gnu-barcode: no rebuilding required

--- Formula/gnu-barcode.rb
+++ Formula/gnu-barcode.rb
@@ -7,16 +7,8 @@ class GnuBarcode < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ed996c9d8421dcd926b85eefc7e3dd04cc9d9430f72f0b466f62647a5f322e94"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e621e1e142ab7c323b6bc133b4b3c373df050986e4282814f07a8ca1928d83c"
-    sha256 cellar: :any_skip_relocation, monterey:       "8442ab838d1f32813d7e8ffc4efec8a5c9bc32241cdbb8a9a3acd4efe506ff9f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8db9cd7477dfce32af8a9451c792683d97ef0ab81d7929881ac59a6fab9d88aa"
-    sha256 cellar: :any_skip_relocation, catalina:       "237ba00c7acb6a0343b17cae529d6a854ae321a03136d0f2882b010f4107230c"
-    sha256 cellar: :any_skip_relocation, mojave:         "a24619af860a3658774fdcb5b5439ed751e1284b724e2f5dc8bb0c35736f879a"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "bd55ad14c9e7411d952d9243b6c4c7aa84162afe34ed1e3c3d9e9a368d2d6485"
-    sha256 cellar: :any_skip_relocation, sierra:         "7588bb4800b5c348e103ed92e8bcc2f38812b8fbf4e254315e6429b3961e9f05"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "1885abad5bc70c2e9952e131307ca7282d851856ebdea58dadc69f0e125a7c22"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cea83cbded5ce1bf557f237984a5085ba5b5534980c64e0b7d97e6756ab01dd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gnu-barcode"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8695b4d62c48a47c95eb08314dcd340dc5b124a63474890916c64c9f0d79aa76"
   end
 
   # Patch and ac_cv_func_calloc_0_nonnull config addresses the following issue:
