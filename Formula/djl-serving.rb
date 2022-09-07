092f20f5699a255d0f2faa2911e69bc927d2b20c
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:20:27 +0800

    djl-serving: no rebuilding required

--- Formula/djl-serving.rb
+++ Formula/djl-serving.rb
@@ -1,12 +1,12 @@
 class DjlServing < Formula
   desc "This module contains an universal model serving implementation"
   homepage "https://github.com/deepjavalibrary/djl-serving"
-  url "https://publish.djl.ai/djl-serving/serving-0.18.0.tar"
-  sha256 "badc950b17870710e67079bcfaee1fb2b2d23fd2288a3895489d459f135b8e0c"
+  url "https://publish.djl.ai/djl-serving/serving-0.16.0.tar"
+  sha256 "52cd5d36e9df8766d8f6fe502e1abce97b2d75a6fad38dc22c02728b2b2b10b9"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9d13fca5ba4f5e05482e007e56377a94b8cb12e10ebc33cadc75846b56d77a0f"
+    sha256 cellar: :any_skip_relocation, all: "aca44346531b303612d7b22151453294b6fb0268f4af954c344c337209a39f87"
   end
 
   depends_on "openjdk"
