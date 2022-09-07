9ccd4785bf4cd71291a2d347c48a9f97396c84b3
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:21:50 +0800

    findent: no rebuilding required

--- Formula/findent.rb
+++ Formula/findent.rb
@@ -1,8 +1,8 @@
 class Findent < Formula
   desc "Indent and beautify Fortran sources and generate dependency information"
   homepage "https://www.ratrabbit.nl/ratrabbit/findent/index.html"
-  url "https://downloads.sourceforge.net/project/findent/findent-4.2.1.tar.gz"
-  sha256 "c010ce4d020ec77c055e0dea1f29a2f52815cb4debe052fd559cf09049fac702"
+  url "https://downloads.sourceforge.net/project/findent/findent-4.2.0.tar.gz"
+  sha256 "6e6411eecfe6586d991ed7dbeeb47859fcfe3655615619912703da6da6bdc3c2"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,12 +11,8 @@ class Findent < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c9c2b26be712bc1bb1ce379e61f00310b7e16c21b407a3bd324033b701503fc3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a8b268dff924a4eef8ac6c1b8a9483eb507a75f0201b7dd6378d15fb58b1ae6"
-    sha256 cellar: :any_skip_relocation, monterey:       "0bd83c8652c99951f4b821fe365448c435d4b5f19c69d0bc23dbc1a7308e1411"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fe2bc46144328becdc142de53d172c2c41bb536a0519cf14dc6404b91dd1c41d"
-    sha256 cellar: :any_skip_relocation, catalina:       "b7f1fc9ce343ada496989a2a7d32498da3db143e034411906c1396a05a3d562a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "670cb312112c73b5a2bf72e1e6a7d1d9c37363d5816670fa1e4011c8fabc1803"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/findent"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "513af891d63d7b2563e94f13581299d093f523880d242b59ea13091338232555"
   end
 
   def install
