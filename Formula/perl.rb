19a4c7aebe68b92ac63d7b5ead19ef161078f970
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:31:58 +0800

    perl: no rebuilding required

--- Formula/perl.rb
+++ Formula/perl.rb
@@ -4,7 +4,6 @@ class Perl < Formula
   url "https://www.cpan.org/src/5.0/perl-5.34.0.tar.xz"
   sha256 "82c2e5e5c71b0e10487a80d79140469ab1f8056349ca8545140a224dbbed7ded"
   license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
-  revision 1
   head "https://github.com/perl/perl5.git", branch: "blead"
 
   livecheck do
@@ -13,47 +12,34 @@ class Perl < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "74b3f24c7c08a99b569e7d6159c68aa9912b222d7e4d40c9895436130fc1aba1"
-    sha256 arm64_big_sur:  "c1edffa4e60b7c3801e1437e9bbf1d6a7eab0b77e9dd551a556dc9fd91b7dfcd"
-    sha256 monterey:       "5baa007a4878f165e1e8f5fde45d8b670fc77043ff30458650513c37ddb4b495"
-    sha256 big_sur:        "dc450ab3b43888382fcc46e975617a9c97f77ff5a6a690022f6745fd53ca8c72"
-    sha256 catalina:       "34a5ce2d637952eb163fb9bac91767322971158106dcd88a44b7128d88bf050d"
-    sha256 x86_64_linux:   "99dbd9c282ec8c2bdef461ff2c54b78d92ee19a315c69537b0f3aa420a4973ab"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/perl"
+    sha256 aarch64_linux: "4c4241cd33ea8b6fcfa9c07d279a7dc81c096d295059b3b6dc41a4dc194eebc8"
   end
 
   depends_on "berkeley-db"
   depends_on "gdbm"
 
   uses_from_macos "expat"
-  uses_from_macos "libxcrypt"
 
   # Prevent site_perl directories from being removed
   skip_clean "lib/perl5/site_perl"
 
-  # Apply upstream commit to remove nsl from libswanted:
-  # https://github.com/Perl/perl5/commit/7e19816aa8661ce0e984742e2df11dd20dcdff18
-  # Remove with next tagged release that includes the change.
-  patch do
-    url "https://github.com/Perl/perl5/commit/7e19816aa8661ce0e984742e2df11dd20dcdff18.patch?full_index=1"
-    sha256 "03f64cf62b9b519cefdf76a120a6e505cf9dc4add863b9ad795862c071b05613"
-  end
-
   def install
     args = %W[
       -des
-      -Dinstallprefix=#{prefix}
-      -Dprefix=#{opt_prefix}
-      -Dprivlib=#{opt_lib}/perl5/#{version.major_minor}
-      -Dsitelib=#{opt_lib}/perl5/site_perl/#{version.major_minor}
-      -Dotherlibdirs=#{HOMEBREW_PREFIX}/lib/perl5/site_perl/#{version.major_minor}
+      -Dprefix=#{prefix}
+      -Dprivlib=#{lib}/perl5/#{version}
+      -Dsitelib=#{lib}/perl5/site_perl/#{version}
+      -Dotherlibdirs=#{HOMEBREW_PREFIX}/lib/perl5/site_perl/#{version}
       -Dperlpath=#{opt_bin}/perl
       -Dstartperl=#!#{opt_bin}/perl
-      -Dman1dir=#{opt_share}/man/man1
-      -Dman3dir=#{opt_share}/man/man3
+      -Dman1dir=#{man1}
+      -Dman3dir=#{man3}
       -Duseshrplib
       -Duselargefiles
       -Dusethreads
     ]
+    args << "-Dsed=/usr/bin/sed" if OS.mac?
 
     args << "-Dusedevel" if build.head?
 
@@ -66,7 +52,7 @@ class Perl < Formula
 
   def post_install
     if OS.linux?
-      perl_archlib = Utils.safe_popen_read(bin/"perl", "-MConfig", "-e", "print $Config{archlib}")
+      perl_archlib = Utils.safe_popen_read("perl", "-MConfig", "-e", "print $Config{archlib}")
       perl_core = Pathname.new(perl_archlib)/"CORE"
       if File.readlines("#{perl_core}/perl.h").grep(/include <xlocale.h>/).any? &&
          (OS::Linux::Glibc.system_version >= "2.26" ||
