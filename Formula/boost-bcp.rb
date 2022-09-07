a1ffb718d0d9ea74b025b3d6f612268b2201d3c6
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:17:40 +0800

    boost-bcp: no rebuilding required

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -1,8 +1,9 @@
 class BoostBcp < Formula
   desc "Utility for extracting subsets of the Boost library"
   homepage "https://www.boost.org/doc/tools/bcp/"
-  url "https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.bz2"
-  sha256 "475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39"
+  # Please add to synced_versions_formulae.json once version synced with boost
+  url "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2"
+  sha256 "8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc"
   license "BSL-1.0"
   head "https://github.com/boostorg/boost.git", branch: "master"
 
@@ -11,12 +12,8 @@ class BoostBcp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2554f876bdc8e1855fdd363317ecac0cad44fe347d7fa5d29f4b0266262b560e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "143a1b99dc5026110ee64264382d9ff3ec6fdcb87dbfd51649b9d20f53547092"
-    sha256 cellar: :any_skip_relocation, monterey:       "bab5e2f23f461f1643b1c841cd59cbc77ad06f0aabd4b362d28c1cb29fa696a2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "67002735a12b9bc625aeb01fce3f7308a2c14548b587501df29be73fe3c7da33"
-    sha256 cellar: :any_skip_relocation, catalina:       "453aebc4b61fbba898665ce83b95d298e3176c427df29199930f87d4996c756c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1ebd56254dd815d37fcb539bbd160476628df6b67f6c2e4acd6eb019c49e726a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/boost-bcp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bc5fb969c35ecaf90d115d2dda9536b4497bd9f9d087c1f2255998cfffa5c719"
   end
 
   depends_on "boost-build" => :build
