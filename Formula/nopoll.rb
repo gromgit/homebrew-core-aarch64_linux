92adfb27731ed6bd686f15689d7fc84be85f2509
Adrian Ho <aho+rpi9@03s.net>
2022-09-07 13:30:41 +0800

    nopoll: no rebuilding required

--- Formula/nopoll.rb
+++ Formula/nopoll.rb
@@ -1,9 +1,9 @@
 class Nopoll < Formula
   desc "Open-source C WebSocket toolkit"
   homepage "https://www.aspl.es/nopoll/"
-  url "https://www.aspl.es/nopoll/downloads/nopoll-0.4.8.b429.tar.gz"
-  version "0.4.8.b429"
-  sha256 "4031f2afd57dbcdb614dd3933845be7fcf92a85465b6228daab3978dc5633678"
+  url "https://www.aspl.es/nopoll/downloads/nopoll-0.4.7.b429.tar.gz"
+  version "0.4.7.b429"
+  sha256 "d5c020fec25e3fa486c308249833d06bed0d76bde9a72fd5d73cfb057c320366"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -12,12 +12,8 @@ class Nopoll < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "63203f653864f3d95320946e141b562087c2efebb5a0c78776a6715157ff79df"
-    sha256 cellar: :any,                 arm64_big_sur:  "9bd568c95d44276b460b9b9f42df93276093388885770a8d635592bb068602f7"
-    sha256 cellar: :any,                 monterey:       "064c5b6a8793964950a51ebaa22711c2aa092c5f285499325c7c1d725a617c8c"
-    sha256 cellar: :any,                 big_sur:        "63a54c90684fd19105629ec224096e6d8b5b0b29305598a418de2dc7ea3a34d2"
-    sha256 cellar: :any,                 catalina:       "baa8fafc0b418c402f497559c32f4181a72ff86fe6713c13d3e740b2bbe29327"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91fb815d3495b95770615b23075c3b628e96758815c490ef851dc2191396fcc9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/nopoll"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "39d3a3cf8a9004061dd9370385180936a17b46dfdf728a5182ae8f94f93c5637"
   end
 
   depends_on "openssl@1.1"
