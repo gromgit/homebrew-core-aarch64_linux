6abce5c2dd48d55d749d7896ea05df772cc75d37
Porkepix <porkepix@gmail.com>
2022-09-09 00:43:17 +0200

    flyway 9.3.0
    Closes #110040.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.2.3/flyway-commandline-9.2.3.tar.gz"
-  sha256 "156ee4ed6e3ab4a910e331882cd3fec0761a342f2ebbd5b63d33466e4879749a"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.3.0/flyway-commandline-9.3.0.tar.gz"
+  sha256 "3a726eea03339e9cc0ae6e8a45ff6082763edbceedbeb75d75ff4ebc4e51f5af"
   license "Apache-2.0"
 
   livecheck do
