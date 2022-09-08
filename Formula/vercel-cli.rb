e431bf1476ce81f4d6eaf2dcfde5fbd67de600e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-08 05:53:23 +0000

    vercel-cli 28.2.3
    Closes #109964.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vercel-cli.rb
+++ Formula/vercel-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class VercelCli < Formula
   desc "Command-line interface for Vercel"
   homepage "https://vercel.com/home"
-  url "https://registry.npmjs.org/vercel/-/vercel-28.2.2.tgz"
-  sha256 "3207e8055854383df5823e3ec9792356a9851727d69a5ff9996a2e003ea00d52"
+  url "https://registry.npmjs.org/vercel/-/vercel-28.2.3.tgz"
+  sha256 "28886d11097dc0da2e96dc937f5fd8355f7c11b780ba50a3f1a04119e5afe034"
   license "Apache-2.0"
 
   bottle do
