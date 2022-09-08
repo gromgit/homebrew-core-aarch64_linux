32692fe7e15ea8bf9aaf1df24e3c9388115da160
Porkepix <porkepix@gmail.com>
2022-09-08 02:01:32 +0200

    distrobox 1.4.0
    Closes #109934.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/distrobox.rb
+++ Formula/distrobox.rb
@@ -1,8 +1,8 @@
 class Distrobox < Formula
   desc "Use any Linux distribution inside your terminal"
   homepage "https://distrobox.privatedns.org/"
-  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.3.1.tar.gz"
-  sha256 "22b6625ca243f55c08630d37015cdbfbe1939516022bfef502aa6603f42b4d00"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.4.0.tar.gz"
+  sha256 "e643a702c3f3f0d7467afe840b4f3ee6e4c369b6464618b6ec09f7ad27eb64f5"
   license "GPL-3.0-only"
   head "https://github.com/89luca89/distrobox.git", branch: "main"
 
