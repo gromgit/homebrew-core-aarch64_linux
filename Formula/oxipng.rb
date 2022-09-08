d3012c1922cdde994cbdb4bcb5f73ac922fb1bbe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-08 07:29:12 +0000

    oxipng: update 6.0.1 bottle.

--- Formula/oxipng.rb
+++ Formula/oxipng.rb
@@ -6,12 +6,12 @@ class Oxipng < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1055ca4bdbfa5cdff1d4cf084bf7e28334a271d7892d443b1b8ceb0892a60309"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "989ac0424081ebe862650cb53e1cdec933ff8b8b33d183348a111fe1f87062c1"
-    sha256 cellar: :any_skip_relocation, monterey:       "6ebdb1c3cfe91ce94f5279b2746df7a4d09e8d71332df5c15f2d8978b7076372"
-    sha256 cellar: :any_skip_relocation, big_sur:        "670844c3073e286bb16bd06a58f054ed1dd57a04b47c91e7c3e3e386592a6458"
-    sha256 cellar: :any_skip_relocation, catalina:       "303f52271d3ebd838db74ac5bdff17c45db7f311a69034c513ee84f7a91492e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6cd5cfd86ef830f93adc741e6da8bbc0b4b8ebf86fbd63cb6096474bfc1aff91"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "64c7d8e83ac7657429b5cbb75325c415c4bedc0c334462504e478ce703a6d746"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59ae76e9603847923229e462c114cd37b8547cfbc53e889c67dced66b90c9fa7"
+    sha256 cellar: :any_skip_relocation, monterey:       "daf313743fd6775f02d088ae9984898ab3e6f9b7c597cedec86ebcf5d4d55b1e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4e7cdddf3d65e220529c20bcc7d92b4e435f855117a00f012baa07f2eea99569"
+    sha256 cellar: :any_skip_relocation, catalina:       "e184013d39757544f2459041f0b8964af2135218cfcacdad703de34e073ee1b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d849fa2d01d46a6d6ce925882b7261d0d4a22794a026be19ae5a91bf1116246"
   end
 
   depends_on "rust" => :build
