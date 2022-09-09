20d71d50d7e4765c0a768c0ab27572283896212c
Adrian Ho <the.gromgit@gmail.com>
2022-09-10 04:26:56 +0800

    json-c: add 0.16 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -13,13 +13,10 @@ class JsonC < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0f71590b7e5045f6c8c892527a7f6f3e7d3b5d7cf0f43b93cf50bf662cdd84f2"
-    sha256 cellar: :any,                 arm64_big_sur:  "efc09489ecb843511393e5166d8351ab8bf3c5e263d91a5838735b574e8dd4e7"
-    sha256 cellar: :any,                 monterey:       "2d032280aa3e434671354c91a191b865f8de9290f5f8fba2336cac88037c1f33"
-    sha256 cellar: :any,                 big_sur:        "e31bc0e529950442ccc42719107e14de5e995847f8abab34f0961d19d9d45976"
-    sha256 cellar: :any,                 catalina:       "425524c4c8a10f50da278278e624b4dce61ff94f464511095d30a17a3bea68ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "afd62a8176c2684a0385f921312805e17b8c85db63a617266e24ec7b93488563"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/json-c"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "723fe799b9cbbd9e4c7998fb7362bf9fe4cb495c61872039d7c4e9b24df92de4"
   end
+
 
   depends_on "cmake" => :build
 
