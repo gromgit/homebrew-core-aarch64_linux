b3aca5e6c68fc519a76fd73d9ba8db4541378cea
Adrian Ho <the.gromgit@gmail.com>
2022-09-09 23:38:42 +0800

    alsa-lib: add 1.2.7.2 bottle.

--- Formula/alsa-lib.rb
+++ Formula/alsa-lib.rb
@@ -1,8 +1,8 @@
 class AlsaLib < Formula
   desc "Provides audio and MIDI functionality to the Linux operating system"
   homepage "https://www.alsa-project.org/"
-  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.6.1.tar.bz2"
-  sha256 "ad582993d52cdb5fb159a0beab60a6ac57eab0cc1bdf85dc4db6d6197f02333f"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.2.tar.bz2"
+  sha256 "8a35b7218e50f2a2c79342d0de98ded81439ce19e12809385ec9be9596de7c2f"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -12,8 +12,9 @@ class AlsaLib < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/alsa-lib"
-    sha256 aarch64_linux: "b5c841671f9a3fc3acdd4d7c788edc46f27eeae460e7230b4d3cf81ec9840bc9"
+    sha256 aarch64_linux: "2c2a0924315246839f944d65f587a614d40eeb6bba5f13b69b688df4cf39db4f"
   end
+
 
   depends_on :linux
 
