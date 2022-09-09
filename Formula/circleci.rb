c110666080573cb031596767709ce05e99a166b4
Rui Chen <rui@chenrui.dev>
2022-09-09 19:24:28 -0400

    circleci 0.1.21041
    Closes #110164.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.20856",
-      revision: "991bd2aae331cf5668175acc77c7c8fb8e49accb"
+      tag:      "v0.1.21041",
+      revision: "56e791be647558532901ce7c566bcdf47964daed"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
