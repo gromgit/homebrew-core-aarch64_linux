21b1f3cba49e21da7baacc69aaf3b75166bffed9
Rui Chen <rui@chenrui.dev>
2022-09-09 18:11:45 -0400

    goredo 1.27.1
    Closes #110157.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/goredo.rb
+++ Formula/goredo.rb
@@ -1,8 +1,8 @@
 class Goredo < Formula
   desc "Go implementation of djb's redo, a Makefile replacement that sucks less"
   homepage "http://www.goredo.cypherpunks.ru/"
-  url "http://www.goredo.cypherpunks.ru/download/goredo-1.27.0.tar.zst"
-  sha256 "63cfdb24f14ec7071b5bb8ed237023410826ea9f21d48e8e6c9f43ae2af34176"
+  url "http://www.goredo.cypherpunks.ru/download/goredo-1.27.1.tar.zst"
+  sha256 "98c1a8a189b33753fcd61e1dbdbe627dba3820621d2032a3856d89829eea08d9"
   license "GPL-3.0-only"
 
   livecheck do
