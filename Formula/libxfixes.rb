d213b5ba28ba2be0c4e87c3844c41bd7eb934845
Adrian Ho <the.gromgit@gmail.com>
2022-09-10 06:38:52 +0800

    libxfixes: add 6.0.0 bottle.

--- Formula/libxfixes.rb
+++ Formula/libxfixes.rb
@@ -6,14 +6,10 @@ class Libxfixes < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "c7f7012b751ab6c83a41cec7c95f6628a2c1d3e2a9f2f1271ee7a8a5844c1a5d"
-    sha256 cellar: :any,                 arm64_big_sur:  "9668a973a8a220609add6d2af60547745b81cb198936930fcd05bfd6b9aca47c"
-    sha256 cellar: :any,                 monterey:       "7325a129c439a5239c04b469d456ca5ec3ca6d93bc444aa25be30973b61f5cfd"
-    sha256 cellar: :any,                 big_sur:        "a0317884b16dfd0e391d9ab48b4a9785fa2c4fdf3fd5dca857be98b1ae6fded2"
-    sha256 cellar: :any,                 catalina:       "916d95deb297ee98b57a87586a90a5a880792efab259c511230844a6ab94d06e"
-    sha256 cellar: :any,                 mojave:         "5c560e7ae60a53ea414c34a7d5159da0e4fc04a9159baa4ce9d67d942d05b9a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b10945be16e84b8e7cd08c600d64408e1332574f1fa8e33a44aadb3bf5e06d68"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxfixes"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1e7d984e4b86fa7c9e169223c7fd611332e09fcdcad17cad3e8b5ef3c7b0157e"
   end
+
 
   depends_on "pkg-config" => :build
   depends_on "libx11"
