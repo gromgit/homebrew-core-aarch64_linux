cd6aa7d30020bb88bb6433986f4865d25e4c0427
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-09 09:21:48 +0000

    gibo: update 2.2.8 bottle.

--- Formula/gibo.rb
+++ Formula/gibo.rb
@@ -6,12 +6,12 @@ class Gibo < Formula
   license "Unlicense"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2c14c60bc5f047063f1f65d49b677a885ba1176503902c7619da7985354c9859"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2c14c60bc5f047063f1f65d49b677a885ba1176503902c7619da7985354c9859"
-    sha256 cellar: :any_skip_relocation, monterey:       "77563daca18aeb4823cccfe99a385b2e5e4619897e47e69a4699dd82de916287"
-    sha256 cellar: :any_skip_relocation, big_sur:        "77563daca18aeb4823cccfe99a385b2e5e4619897e47e69a4699dd82de916287"
-    sha256 cellar: :any_skip_relocation, catalina:       "77563daca18aeb4823cccfe99a385b2e5e4619897e47e69a4699dd82de916287"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2c14c60bc5f047063f1f65d49b677a885ba1176503902c7619da7985354c9859"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "010d9cc6f4ec371787d333657483eeb7980b640041688a60514db8376e44eaa8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "010d9cc6f4ec371787d333657483eeb7980b640041688a60514db8376e44eaa8"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd7791136b3bc08945b017def9fcecee3904250e0870ccd20c8a28b68160198e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd7791136b3bc08945b017def9fcecee3904250e0870ccd20c8a28b68160198e"
+    sha256 cellar: :any_skip_relocation, catalina:       "fd7791136b3bc08945b017def9fcecee3904250e0870ccd20c8a28b68160198e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "010d9cc6f4ec371787d333657483eeb7980b640041688a60514db8376e44eaa8"
   end
 
   def install
