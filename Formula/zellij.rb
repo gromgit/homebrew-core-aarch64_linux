b4149a6f864c586b9203721c51653f5b97c51ef3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-09 15:04:39 +0000

    zellij: update 0.31.4 bottle.

--- Formula/zellij.rb
+++ Formula/zellij.rb
@@ -6,12 +6,12 @@ class Zellij < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d905ffd50add8ad25d06ea1fea1a55b684e8e161433754bf9be8c2c6d0333dbd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "351d9cb81a69beaf0a89672a68877e9d8c18c61749f0055961a2f3ca7ff661c8"
-    sha256 cellar: :any_skip_relocation, monterey:       "cb66f7df3af8548dfaa5b2df936c32368d6b510d9ff7d50cec844b20e4916492"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6fa08e8f6c63b4426bd9a5b061eca97821d06c466a8e572ef50fd81cc5f51c30"
-    sha256 cellar: :any_skip_relocation, catalina:       "fe1dc5cfbf7a278984d9d338058f832f3d9a974051ec91f057ed3586a3b4c252"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d53f59182700ab47572e4715240f3e90fd942415d8563ec5950e720cfd0184b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2e93a21f0963b6071a60bdb572c008fb9aa80f1b80ad985b0feed01b33c4f1bf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5b811eb9747bb2195848372c95a5bfe01390404ad5dd361a9436940cf2470209"
+    sha256 cellar: :any_skip_relocation, monterey:       "9e2fa6f827a5751dc62c0e0dca528f12d2205d8872ce551ba48b3f8b8d9afffc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "aa40ddd39c2c743a2947f51867e02fa313c0e24397b8b35d91a72c73c55e2eba"
+    sha256 cellar: :any_skip_relocation, catalina:       "dee94b6b2596c37675313aa07c72bee3d4cd6fe50850d5644887d9dff00386c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3eedaa5cb90d3b1392104d0fb27080030f518889011ccf27bc8e269334965315"
   end
 
   depends_on "rust" => :build
