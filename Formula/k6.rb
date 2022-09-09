74eba8f6620af9e1157e3049f6939ca527aa737a
Max Eisner <4730112+max-ae@users.noreply.github.com>
2022-09-09 21:25:40 +0200

    k6: add completions
    Closes #111380.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -18,6 +18,8 @@ class K6 < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
+
+    generate_completions_from_executable(bin/"k6", "completion")
   end
 
   test do
