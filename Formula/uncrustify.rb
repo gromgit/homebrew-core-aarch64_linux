af1d2f454e734d65482b0f5abe1bbc46cc59fc5b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-09 09:05:57 +0000

    uncrustify: update 0.75.1 bottle.

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -7,12 +7,13 @@ class Uncrustify < Formula
   head "https://github.com/uncrustify/uncrustify.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7eb7fec9fa0613684ce387cd7f139b50247ca224c971606c2496c5e4a1a75265"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a0c32a534c3fd8de75d6496b0d104ad160e8751c2a2f74640bd3b415b6c75a02"
-    sha256 cellar: :any_skip_relocation, monterey:       "cac15eb7c3979bf75b6a24c2f57a366878ea6a98befbdde5912798a4b1b4ea20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "185507a328eaba61d4cf56ffeecd728aedbc800d6d84b8ea6a5f749f25c32e8b"
-    sha256 cellar: :any_skip_relocation, catalina:       "efb42357c245fc69f64d000b33043504e67d28c587df47ce5b1c237e7e6e11b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61ecc6df45fafbe1c44b02c154bf1a0aed9dad0e9694d20209ed16aed392ad88"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "18fd62fc09199932669b7dae942275db9b01cb0f41426bb498d4c0c62fa281d4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "125f9215b38b8479bf19a3e2bd2a26b69d852330c5ea814efc4f321fd7c7845c"
+    sha256 cellar: :any_skip_relocation, monterey:       "91b46324697bb64ff4dd19a14b03c76f121413cee986b1fbbadc28ee9606dbe6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d3aa70ab113276545fe3dcf0a29a1549f41e1f2237ffa3ee41ece14aeb321a4e"
+    sha256 cellar: :any_skip_relocation, catalina:       "d9718ce807174f7673bd384b14df90b3e0089c95ae2d023f0dd51cbf895c5c51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33c4e0474ecf3ef68598a8babe888a568b87db5c1156c87c72c289e00e2df078"
   end
 
   depends_on "cmake" => :build
