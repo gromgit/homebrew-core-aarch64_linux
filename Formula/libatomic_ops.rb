2eabfdf34b3206d7e852a64ccea551f69908fe1b
Adrian Ho <the.gromgit@gmail.com>
2022-09-09 23:42:43 +0800

    libatomic_ops: add 7.6.14 bottle.

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -1,8 +1,8 @@
 class LibatomicOps < Formula
   desc "Implementations for atomic memory update operations"
   homepage "https://github.com/ivmai/libatomic_ops/"
-  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.12/libatomic_ops-7.6.12.tar.gz"
-  sha256 "f0ab566e25fce08b560e1feab6a3db01db4a38e5bc687804334ef3920c549f3e"
+  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.14/libatomic_ops-7.6.14.tar.gz"
+  sha256 "390f244d424714735b7050d056567615b3b8f29008a663c262fb548f1802d292"
   license "GPL-2.0"
 
   livecheck do
@@ -12,8 +12,9 @@ class LibatomicOps < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libatomic_ops"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "2750f4129484e8bb09da388a5f034aa6a4075b48c375db8ea37933afa32ede6a"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e27e8ccbec5b11d18cb2d924a9ebd4183e7fc90c7bdff41283afaa998c623938"
   end
+
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
