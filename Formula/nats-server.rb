93d06804c2dc93bf9e1c09d18ffbae9b9bb0ce3f
Ivan Kozlovic <ivan@synadia.com>
2022-09-09 08:59:28 -0600

    nats-server 2.9.0
    Closes #110114.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -1,8 +1,8 @@
 class NatsServer < Formula
   desc "Lightweight cloud messaging system"
   homepage "https://nats.io"
-  url "https://github.com/nats-io/nats-server/archive/v2.8.4.tar.gz"
-  sha256 "172c5d04c3867adcb6b2322d87d7f7029b63e9465fffffcf99d4ca652820635f"
+  url "https://github.com/nats-io/nats-server/archive/v2.9.0.tar.gz"
+  sha256 "8510b50f8c8cbc8e7c82686fcf696520cab9fd0d48df0ccad3816156ad9dfdfd"
   license "Apache-2.0"
   head "https://github.com/nats-io/nats-server.git", branch: "main"
 
