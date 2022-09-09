0204f0ba9f83958a053fa34145234453f5b86349
Adrian Ho <the.gromgit@gmail.com>
2022-09-10 03:30:42 +0800

    freetype: add 2.12.1 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,9 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.12.0/freetype-2.12.0.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.0.tar.xz"
-  sha256 "ef5c336aacc1a079ff9262d6308d6c2a066dd4d2a905301c4adda9b354399033"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.12.1/freetype-2.12.1.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz"
+  sha256 "4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f"
   license "FTL"
 
   livecheck do
@@ -13,8 +13,9 @@ class Freetype < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/freetype"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "f1bb0a5a5ad96684d7fb7e8c608f655c132c1732ee0ae2c665b95406f11ce315"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4370070b8bf114a2c6f179b51ec8b6a23d9e8d09117a94576d8d21412a8730a5"
   end
+
 
   depends_on "libpng"
 
