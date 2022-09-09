f551d3eb6b427bc458e1564960a9f0626b35db62
Michael Cho <cho-m@tuta.io>
2022-09-09 16:45:09 -0700

    allureofthestars: revision bump (ghc 9.2.4)
    Closes #110086.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -4,6 +4,7 @@ class Allureofthestars < Formula
   url "https://hackage.haskell.org/package/Allure-0.11.0.0/Allure-0.11.0.0.tar.gz"
   sha256 "6125cc585e2a5f28c88855c3c328385c1f21bed093d7606478f1b2af0cb2b6d6"
   license all_of: ["AGPL-3.0-or-later", "GPL-2.0-or-later", "OFL-1.1", "MIT", :cannot_represent]
+  revision 1
   head "https://github.com/AllureOfTheStars/Allure.git", branch: "master"
 
   bottle do
