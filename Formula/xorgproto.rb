472811434b4123a06f1862d5d808b201204129a1
Adrian Ho <the.gromgit@gmail.com>
2022-09-10 05:50:25 +0800

    xorgproto: add 2022.2 bottle.

--- Formula/xorgproto.rb
+++ Formula/xorgproto.rb
@@ -1,8 +1,8 @@
 class Xorgproto < Formula
   desc "X.Org: Protocol Headers"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2022.1.tar.bz2"
-  sha256 "1d2dcc66963f234d2c1e1f8d98a0d3e8725149cdac0a263df4097593c48bc2a6"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2022.2.tar.gz"
+  sha256 "da351a403d07a7006d7bdc8dcfc14ddc1b588b38fb81adab9989a8eef605757b"
   license "MIT"
 
   livecheck do
@@ -12,8 +12,9 @@ class Xorgproto < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/xorgproto"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "0cf281bdfe9544d28ab48a5fbbad64259ad3551389ffef144207d554db582046"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8a6648acebff9adee9d1f5cecf7b628ff7410bb04eb04a43408d5acfd4ce0984"
   end
+
 
   depends_on "pkg-config" => [:build, :test]
   depends_on "util-macros" => :build
