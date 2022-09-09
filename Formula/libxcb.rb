7ee5bef1c38fcce38904d0c1cc661112c6571bb6
Adrian Ho <the.gromgit@gmail.com>
2022-09-10 06:01:30 +0800

    libxcb: add 1.15 bottle.

--- Formula/libxcb.rb
+++ Formula/libxcb.rb
@@ -1,19 +1,15 @@
 class Libxcb < Formula
   desc "X.Org: Interface to the X Window System protocol"
   homepage "https://www.x.org/"
-  url "https://xcb.freedesktop.org/dist/libxcb-1.14.tar.gz"
-  sha256 "2c7fcddd1da34d9b238c9caeda20d3bd7486456fc50b3cc6567185dbd5b0ad02"
+  url "https://xcb.freedesktop.org/dist/libxcb-1.15.tar.gz"
+  sha256 "1cb65df8543a69ec0555ac696123ee386321dfac1964a3da39976c9a05ad724d"
   license "MIT"
-  revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6c7154002b268eb74f36215a5d0b6496dacb3ff786bb40c29a2151359c219476"
-    sha256 cellar: :any,                 arm64_big_sur:  "e473ad5c0877fa07f64037bcf12df7396f5aed59a2ff89a1b5eaa1a6e2885fcd"
-    sha256 cellar: :any,                 monterey:       "aa439c61a2c0174f7d9f505fed55798a2dd4309221f3c51535c4fba5caa5162e"
-    sha256 cellar: :any,                 big_sur:        "c499695d4451922107256515154334e89fbafb3825b68f9c9f5d957f1398968e"
-    sha256 cellar: :any,                 catalina:       "118d9fa08edc88e58530f3920ddccba38e3eae0d08b280ff61e5eed9b5a87e9a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "105c626775006b99f03f67dd80a0ec5ef84e9163353cd33a8b39193fdf231936"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxcb"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4eebb264329c362fde325762342e5094c5a367122348f2b7e64f1cc42f1b06e1"
   end
+
 
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => :build
@@ -36,6 +32,7 @@ class Libxcb < Formula
       --disable-silent-rules
       --enable-devel-docs=no
       --with-doxygen=no
+      PYTHON=python3
     ]
 
     system "./configure", *args
