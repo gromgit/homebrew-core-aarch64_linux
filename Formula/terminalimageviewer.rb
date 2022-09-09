795140fd451b95bc130db11c98ef943f580b9124
danielnachun <daniel.nachun@gmail.com>
2022-09-08 21:45:12 -0700

    terminalimageviewer: drop GCC dependency
    Closes #110082.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terminalimageviewer.rb
+++ Formula/terminalimageviewer.rb
@@ -18,10 +18,6 @@ class Terminalimageviewer < Formula
   depends_on "imagemagick"
   depends_on macos: :catalina
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with gcc: "5"
 
   def install
