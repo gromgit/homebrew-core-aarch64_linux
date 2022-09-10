61b8ae1717c7eceda449a148422ab8f3a00773a6
Porkepix <porkepix@gmail.com>
2022-09-10 16:37:26 +0200

    marp-cli 2.1.4
    Closes #110206.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/marp-cli.rb
+++ Formula/marp-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class MarpCli < Formula
   desc "Easily convert Marp Markdown files into static HTML/CSS, PDF, PPT and images"
   homepage "https://github.com/marp-team/marp-cli"
-  url "https://registry.npmjs.org/@marp-team/marp-cli/-/marp-cli-2.1.2.tgz"
-  sha256 "cd8acb76d3d7a5f1cf128c5504a0861dbe86fbb22f83b664c290c1b00e1c9c16"
+  url "https://registry.npmjs.org/@marp-team/marp-cli/-/marp-cli-2.1.4.tgz"
+  sha256 "c8d464caf40f97cd3207e31d404cdc00f6c8e355e252f956ce4c1a9de088a466"
   license "MIT"
 
   bottle do
