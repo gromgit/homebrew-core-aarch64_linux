941ff530dc2aaf11473d7690a1a6d25a3bcaf4cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-10 23:04:29 +0000

    shtools: update 4.10.1 bottle.

--- Formula/shtools.rb
+++ Formula/shtools.rb
@@ -7,11 +7,11 @@ class Shtools < Formula
   head "https://github.com/SHTOOLS/SHTOOLS.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2fe638c34f0ec33f504a990ccab910339912a634330cbc0935a89a157feaf1ee"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57b9dc9c75b80fad0002b3e813d501ac12ea6f6f7e4bdad18c56dc03844a954b"
-    sha256 cellar: :any_skip_relocation, monterey:       "3ef8010ed479290fa79b47aa891f3f8af18645c0d7bcc48f686e422be304ba99"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f15a2bb074aee900190a141b3115ada6c4bbb820112d9b518a6f3d3f0ce00677"
-    sha256 cellar: :any_skip_relocation, catalina:       "f6a1f62c15469c160092763e470822f81bbc5d9138f3ed618f8887c64f70758b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "038729713c3013c2c06e453ea99ff52eb83f01adcd3bfd6435e1101054d4698f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "063c1e3013bf6dfc56ae3c0dbafe3214d52c3446b84f084accc2214838cd8d46"
+    sha256 cellar: :any_skip_relocation, monterey:       "8164fa3de31c5909455ec628c680e35c051b0858fd643e0694b10debbd85d134"
+    sha256 cellar: :any_skip_relocation, big_sur:        "170def5eead4a05b223b214bdff418edd84c62008687a246117362ea0e0f4cb3"
+    sha256 cellar: :any_skip_relocation, catalina:       "e1e43ac26408f5592cc0b2140d18dee20048d91efb55782fe93389229fceba3c"
   end
 
   depends_on "fftw"
