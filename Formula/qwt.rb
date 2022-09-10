3b8e7b3315d8934471345cb56aaa12a990cbaa69
danielnachun <daniel.nachun@gmail.com>
2022-09-10 15:23:05 -0700

    qwt: drop GCC dependency
    Closes #110243.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/qwt.rb
+++ Formula/qwt.rb
@@ -23,10 +23,6 @@ class Qwt < Formula
 
   depends_on "qt"
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with gcc: "5"
 
   # Update designer plugin linking back to qwt framework/lib after install
