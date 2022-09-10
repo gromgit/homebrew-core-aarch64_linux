7156b953b1357a7ab81b2d1b9a9b7b8c616b06f2
Rui Chen <rui@chenrui.dev>
2022-09-10 12:35:38 -0400

    duply 2.4.1
    Closes #110218.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/duply.rb
+++ Formula/duply.rb
@@ -3,8 +3,8 @@ class Duply < Formula
   # Canonical domain: duply.net
   # Historical homepage: https://web.archive.org/web/20131126005707/ftplicity.sourceforge.net
   homepage "https://sourceforge.net/projects/ftplicity/"
-  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.4.x/duply_2.4.tgz"
-  sha256 "0c2ae9de8fee93391f9d568b6c82bce1b9989d7bea2acbe59069f718963788d8"
+  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.4.x/duply_2.4.1.tgz"
+  sha256 "03fd28b06206505edf0e0820d098208708842999f21b1d132461e34e2c0b6973"
   license "GPL-2.0-only"
 
   livecheck do
