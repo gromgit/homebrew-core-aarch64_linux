f19ab08ed78ef76885ff9b064076b5a7ae97e45e
Rui Chen <rui@chenrui.dev>
2022-09-11 10:07:19 -0400

    gopass-jsonapi 1.14.6
    Closes #110304.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gopass-jsonapi.rb
+++ Formula/gopass-jsonapi.rb
@@ -1,8 +1,8 @@
 class GopassJsonapi < Formula
   desc "Gopass Browser Bindings"
   homepage "https://github.com/gopasspw/gopass-jsonapi"
-  url "https://github.com/gopasspw/gopass-jsonapi/releases/download/v1.14.5/gopass-jsonapi-1.14.5.tar.gz"
-  sha256 "4f6ff0fdf1cf371af465b59320332c2a78fae903018dbf0d824bd29b3e8b4ff8"
+  url "https://github.com/gopasspw/gopass-jsonapi/releases/download/v1.14.6/gopass-jsonapi-1.14.6.tar.gz"
+  sha256 "3ce434ee44c2cd17ac241bfc4a62e95d98d0e78e068f78a7d71106a4d555c6b3"
   license "MIT"
 
   bottle do
