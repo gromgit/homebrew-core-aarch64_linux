4374a04e6bae938c7f175f547aedd8de316eb28a
danielnachun <daniel.nachun@gmail.com>
2022-09-11 15:13:12 -0700

    onnxruntime: drop GCC dependency

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -23,10 +23,6 @@ class Onnxruntime < Formula
   depends_on "cmake" => :build
   depends_on "python@3.10" => :build
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with gcc: "5" # GCC version < 7 is no longer supported
 
   def install
