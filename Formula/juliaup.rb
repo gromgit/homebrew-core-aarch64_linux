b0040769916d78ba9dfa94e04747030abf864c8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-11 02:57:54 +0000

    juliaup: update 1.7.23 bottle.

--- Formula/juliaup.rb
+++ Formula/juliaup.rb
@@ -12,12 +12,12 @@ class Juliaup < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dce25e27aae94675977bf89819c93f8cba7b6d2705c4e90e2512e3da80b2e1ec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9deafce74a848362de49cea25d03eef936a66508f1256cfe86e7c5a6bc7f441c"
-    sha256 cellar: :any_skip_relocation, monterey:       "2372d69533ed62436c77a9d6c390b902d59bdfccd94736fa55240313d091d005"
-    sha256 cellar: :any_skip_relocation, big_sur:        "eaba1aa54675d577fc937a881d6d2267adf7a09a7d3fd6782cbc909bc9908e76"
-    sha256 cellar: :any_skip_relocation, catalina:       "408ce852b63ca21055808005d7d130c5f9155a267bb424ca277a2e29c29bc8fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c46078ecd6884e37af49005645d14ee6d878b3bff9ce95ca1431a13d020fd43f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2487ce26d253ad61a73d0e00918827c3f931fe661c21e0fe14f5fe853fa29c2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc0984f51f2abb146ab734630916258e97b3e8229ea03c630a85bb02969fbda0"
+    sha256 cellar: :any_skip_relocation, monterey:       "7f48d50a44cbf56b5127d255641b179314fc45847ba40eb7f81f0f18a666abe5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1ff859b9a30301078b1803f0a798bf1fb33b3af5c17cc3a96eefba65fed0259a"
+    sha256 cellar: :any_skip_relocation, catalina:       "45d6d3ac5b7e517fd1f06077bddd11fbc4afe92963342111663eb4173e1c0317"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2afb8b6d7fbc6d9bb91d2bb1a8b75618a9f290c44684453440d56a9a6dc4a6f"
   end
 
   depends_on "rust" => :build
