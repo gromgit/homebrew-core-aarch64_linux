eef259d388a57631c2c850c018d2370b27c217eb
William Cheng <wing328hk@gmail.com>
2022-09-11 21:25:37 +0800

    openapi-generator 6.1.0
    openapi-generator 6.1.0
    
    Closes #110301.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openapi-generator.rb
+++ Formula/openapi-generator.rb
@@ -1,8 +1,8 @@
 class OpenapiGenerator < Formula
   desc "Generate clients, server & docs from an OpenAPI spec (v2, v3)"
   homepage "https://openapi-generator.tech/"
-  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/6.0.1/openapi-generator-cli-6.0.1.jar"
-  sha256 "ba9279900d1fefc9f0b977264b709f7d481c278d6780090b36673b65cb1a9122"
+  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/6.1.0/openapi-generator-cli-6.1.0.jar"
+  sha256 "3bcbff776072e4e5161307f837d34ab4713a13ed9edc20e4983c2321791ea037"
   license "Apache-2.0"
 
   livecheck do
