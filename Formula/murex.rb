9d923e53c914331ae09eea9dc957f9704c92f613
Rui Chen <rui@chenrui.dev>
2022-09-12 07:57:26 -0400

    murex 2.11.2000
    Closes #110418.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/murex.rb
+++ Formula/murex.rb
@@ -1,8 +1,8 @@
 class Murex < Formula
   desc "Bash-like shell designed for greater command-line productivity and safer scripts"
   homepage "https://murex.rocks"
-  url "https://github.com/lmorg/murex/archive/v2.10.2400.tar.gz"
-  sha256 "4503e20d144cdadf60e01b97d420bc3ab7d35b3c079712fd3e37477bdeac67b3"
+  url "https://github.com/lmorg/murex/archive/v2.11.2000.tar.gz"
+  sha256 "b8037307ee8da27ea218a8670d583fb392ca54f320d7cc7251e7a881a36b692a"
   license "GPL-2.0-only"
   head "https://github.com/lmorg/murex.git", branch: "master"
 
