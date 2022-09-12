e6dbf77dc9017ee3ecbf9bacc46f5e8e7be73f10
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-12 00:16:49 +0000

    ns-3: update 3.36.1 bottle.

--- Formula/ns-3.rb
+++ Formula/ns-3.rb
@@ -6,12 +6,13 @@ class Ns3 < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "dd8388b7afa86166b4390d2765b44bc2fafdff36994692077af65f9800e47a73"
-    sha256 cellar: :any,                 arm64_big_sur:  "6d4bb953d0bdf69a52b98a5518c3e6bd838f8cc516133ae638855523548b7540"
-    sha256 cellar: :any,                 monterey:       "5fe92587f3fdbcc942bd0c771f8e2b5153afb4ed96100da86112dac6454a511c"
-    sha256 cellar: :any,                 big_sur:        "8037a6167d7b50b2e2349ce6d7fb25049aa65b36123d99128be1c2382ca3530c"
-    sha256 cellar: :any,                 catalina:       "8ebabc28901eaf3ab45ddbb20e2da25399f871edc88844125c672656af010627"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ce00bfe0a855a282a1b8498623fca9c6bf5aca22cb755dc99c360583207b9e0"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "3d489bfd184d65cc26ccb3b09ca005e6ddf41ce6ca15abce25c59d9a9ae95ca5"
+    sha256 cellar: :any,                 arm64_big_sur:  "68aac9355130e2cc027c95b9255d150c10c817d4f9aefa94708543ecc0b20a64"
+    sha256 cellar: :any,                 monterey:       "3cae5abbf94429cdcef4d13f8b51ef34e16204761c29e9551332b5859854f740"
+    sha256 cellar: :any,                 big_sur:        "5a88644764053c9a4852adaa074b0bcf503b1616129821afa689e7fadd5d2bb3"
+    sha256 cellar: :any,                 catalina:       "5167dc9662ae8e1998bcd132c9af32b235dd848321cd357f3532d072427e763c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b46bb6dbfa98be9574f3ca0e2f69f1d0aa0749be73a868f33f6da5733259e3e9"
   end
 
   depends_on "boost" => :build
