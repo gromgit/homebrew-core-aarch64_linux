302ca8ddcc5fdd20224a29504bcd8af1ba103bee
messense <messense@icloud.com>
2022-09-12 17:07:50 +0800

    cargo-zigbuild 0.12.2 (new formula)
    Closes #110404.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cargo-zigbuild.rb
@@ -0,0 +1,30 @@
+class CargoZigbuild < Formula
+  desc "Compile Cargo project with zig as linker"
+  homepage "https://github.com/messense/cargo-zigbuild"
+  url "https://github.com/messense/cargo-zigbuild/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "90397c5170eb02ebcf8c378e499f2c9be3b1b63c85d20e1e0584aec459321ed7"
+  license "MIT"
+  head "https://github.com/messense/cargo-zigbuild.git", branch: "main"
+
+  depends_on "rustup-init" => :test
+  depends_on "rust"
+  depends_on "zig"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # https://github.com/ziglang/zig/issues/10377
+    ENV.delete "CPATH"
+
+    system "#{Formula["rustup-init"].bin}/rustup-init", "-y", "--no-modify-path"
+    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+    system "rustup", "target", "add", "aarch64-unknown-linux-gnu"
+
+    system "cargo", "new", "hello_world", "--bin"
+    cd "hello_world" do
+      system "cargo", "zigbuild", "--target", "aarch64-unknown-linux-gnu.2.17"
+    end
+  end
+end
