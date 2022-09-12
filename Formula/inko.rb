f74f186ef55e6d8c209985d480275a58b4a9b74b
Rui Chen <rui@chenrui.dev>
2022-09-12 13:15:14 -0400

    inko 0.10.0
    * inko 0.10.0
    * fix build and update test
    
    Closes #110448.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/inko.rb
+++ Formula/inko.rb
@@ -1,10 +1,9 @@
 class Inko < Formula
   desc "Safe and concurrent object-oriented programming language"
   homepage "https://inko-lang.org/"
-  url "https://releases.inko-lang.org/0.9.0.tar.gz"
-  sha256 "311f6e675e6f7ca488a71022b62edbbc16946f907d7e1695f3f96747ece2051f"
+  url "https://releases.inko-lang.org/0.10.0.tar.gz"
+  sha256 "d38e13532a71290386164246ac8cf7efb884131716dba6553b66a170dd3a2796"
   license "MPL-2.0"
-  revision 1
   head "https://gitlab.com/inko-lang/inko.git", branch: "master"
 
   bottle do
@@ -24,15 +23,19 @@ class Inko < Formula
   uses_from_macos "ruby", since: :sierra
 
   def install
-    system "make", "build", "PREFIX=#{prefix}", "FEATURES=libinko/libffi-system"
+    system "make", "build", "PREFIX=#{prefix}", "FEATURES=libffi/system"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
     (testpath/"hello.inko").write <<~EOS
-      import std::stdio::stdout
+      import std::stdio::STDOUT
 
-      stdout.print('Hello, world!')
+      class async Main {
+        fn async main {
+          STDOUT.new.print('Hello, world!')
+        }
+      }
     EOS
     assert_equal "Hello, world!\n", shell_output("#{bin}/inko hello.inko")
   end
