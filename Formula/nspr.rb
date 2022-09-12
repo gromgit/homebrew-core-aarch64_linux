01c80928b1a62d3d6d07af3034b9154f3c59446c
owine <owine@users.noreply.github.com>
2022-09-12 09:29:37 -0500

    nspr 4.35
    Closes #110439.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -1,8 +1,8 @@
 class Nspr < Formula
   desc "Platform-neutral API for system-level and libc-like functions"
   homepage "https://hg.mozilla.org/projects/nspr"
-  url "https://archive.mozilla.org/pub/nspr/releases/v4.34.1/src/nspr-4.34.1.tar.gz"
-  sha256 "c5b8354c48b632b8f4c1970628146c0e0c0ca8f32c7315d7d5736c002e03774f"
+  url "https://archive.mozilla.org/pub/nspr/releases/v4.35/src/nspr-4.35.tar.gz"
+  sha256 "7ea3297ea5969b5d25a5dd8d47f2443cda88e9ee746301f6e1e1426f8a6abc8f"
   license "MPL-2.0"
 
   livecheck do
