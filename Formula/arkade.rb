c0b24c27cac6d11424ffc5c901a2588a2ca3c7df
Rui Chen <rui@chenrui.dev>
2022-09-11 23:11:49 -0400

    arkade 0.8.44
    Closes #110354.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -2,8 +2,8 @@ class Arkade < Formula
   desc "Open Source Kubernetes Marketplace"
   homepage "https://blog.alexellis.io/kubernetes-marketplace-two-year-update/"
   url "https://github.com/alexellis/arkade.git",
-      tag:      "0.8.42",
-      revision: "cd54f6a171afb07ef27f8e8e23e9cb13a4e3878b"
+      tag:      "0.8.44",
+      revision: "b543e60a68285ce8a147d9b0a6493f573747b8d1"
   license "MIT"
 
   livecheck do
