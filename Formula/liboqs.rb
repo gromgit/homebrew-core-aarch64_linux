93b873811526d77232b770500573386089dffe1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-12 23:04:28 +0000

    liboqs: update 0.7.2 bottle.

--- Formula/liboqs.rb
+++ Formula/liboqs.rb
@@ -11,12 +11,13 @@ class Liboqs < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "5eb35c4cf7d2445589282fa25c8c34cd26aab762c176f7222f625e9e595b1eb5"
-    sha256 cellar: :any,                 arm64_big_sur:  "ecb1d30e647211ede634816fd69937fbdbaca6960fd8d4d483ade292aaa72617"
-    sha256 cellar: :any,                 monterey:       "2ae8858740cbffa92d27949dd716326e351dd9582542e48541f059a49be55a9d"
-    sha256 cellar: :any,                 big_sur:        "2a57edaa4a1efbeeaf51e277b0d7ab7f2cc1ead2bc5d4fb6d93ae492d4c9dd5f"
-    sha256 cellar: :any,                 catalina:       "1b1db31d67ba3e3b46b5af0e79da4b45bf6697440ce378c923cb838217f886a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6e90fee4c3a6d0b963c85d858a4a76655e811f9b5698dd7a2a82c9b0209c571"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "229cc26dc8f4ebd3f178ae31e40c1627a5e3b5dfd30d15c440e26dd8b3e289e3"
+    sha256 cellar: :any,                 arm64_big_sur:  "4e80fd4cfee7fd4cdaf4560befaddb01f02886123409036daeea2ab73ebf253a"
+    sha256 cellar: :any,                 monterey:       "41ac926d23bb6c05e82d4a30072a3e7b9f5bd573110b06fa931f032aa81cb1b2"
+    sha256 cellar: :any,                 big_sur:        "98ffdb22f4e52fb8ba6eba99834d3b9c729d2030f99d8691b059cb307ea60390"
+    sha256 cellar: :any,                 catalina:       "a4ab1395ce808507b8af6316ceb58d1a081c172e21bd24d107e98829620f5b86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10d3d91f8b15c231eb1f95bfb886761304429f210f30bfcfdbe45ac6541c4f01"
   end
 
   depends_on "cmake" => :build
