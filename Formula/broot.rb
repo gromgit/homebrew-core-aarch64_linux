5f299cd3240c726c42ac6f95900ed153a1fae49f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-13 17:10:25 +0000

    broot: update 1.14.3 bottle.

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -7,12 +7,12 @@ class Broot < Formula
   head "https://github.com/Canop/broot.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd9551fbe5d06f4d8135ab14da388f0ab1b1563f87c0c7664f1daea3a497a1b4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "64bb766c75a3ec4aef4af35f86fb2889ef8b6e8ffdb7844adb7a99fc051a3298"
-    sha256 cellar: :any_skip_relocation, monterey:       "0a0d2ca68b19d6db4a0ff4d3ef83f3f3b862297401f416c9254053db068b9f3e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "723d031140ee91feabd754ecbdd565e6a80b86174b52aaee0dd84f4a41d9ec09"
-    sha256 cellar: :any_skip_relocation, catalina:       "eeea92683b58ae57ebb6619ac54af620699f23afee6742e7a0569c7050740482"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12877ce201e9f4fb97e659e44c5cd3c473d5ab65e10f7b7c69dfe2205a02dab0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f26459b74e886b129653b421aa4e726f8fb1c225b6929c5843217b11a68bb2f0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "43b5a5fc602d866e64158c6ac206df03b2463734c3b3d220c913da92babe4cd5"
+    sha256 cellar: :any_skip_relocation, monterey:       "1aa6a0f58bfa7ce8e575dbd2cc7d847a68d92fdcdad177a211b4f61f30392a9b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a21cb06c67023ee8e6e9883bc012a7d7e7858adb1830d8103a7a95882a56ef77"
+    sha256 cellar: :any_skip_relocation, catalina:       "e368c986ff9858ada1c26b527d23bb22b0784455b4d66f22884ca534480535e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd28471f38752be2f05b014b2e8e20b61cfdda6a2c99dd9d6fc95eaa36877a60"
   end
 
   depends_on "rust" => :build
