6721ca91a0e6b0d8ea49ad93122faeeb0ee77fb7
danielnachun <daniel.nachun@gmail.com>
2022-09-13 09:46:57 -0700

    grpc: drop GCC dependency
    Closes #110574.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -44,10 +44,6 @@ class Grpc < Formula
     depends_on "llvm" => [:build, :test] if DevelopmentTools.clang_build_version <= 1100
   end
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with :clang do
     build 1100
     cause "Requires C++17 features not yet implemented"
