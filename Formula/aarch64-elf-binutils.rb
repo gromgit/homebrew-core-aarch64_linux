90920f6b82fe97c99cc7898b009a87f1a0da63be
Adrian Ho <the.gromgit@gmail.com>
2022-09-14 06:23:08 +0800

    aarch64-elf-binutils: update 2.39 bottle.

--- Formula/aarch64-elf-binutils.rb
+++ Formula/aarch64-elf-binutils.rb
@@ -1,9 +1,9 @@
 class Aarch64ElfBinutils < Formula
   desc "GNU Binutils for aarch64-elf cross development"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.xz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.38.tar.xz"
-  sha256 "e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
+  sha256 "645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -11,13 +11,10 @@ class Aarch64ElfBinutils < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "8df73acaded40e59291ce9afdc17918f655a4ceb8884f04647b90eb94d59af13"
-    sha256 arm64_big_sur:  "a86842be0ff1841bdc7666b2c6ff4143a83e18a6fc6de147e088055ef4da77a5"
-    sha256 monterey:       "80f0787d5735fa015bebde54cd449f4ca7553163c8e869315dca9c89d077801f"
-    sha256 big_sur:        "def04262f48a5bc1a5b3a9b1331c7eabde55b65c1ca4cf565bda1b075b656846"
-    sha256 catalina:       "707c2aa4f1b263090f811dfa3aba67b890ad64cab3e932266d308ce12a6ba41d"
-    sha256 x86_64_linux:   "adc92ac904443623aa1a2a27c03b5ed7f89dd07dede7f297cd4bdaa83c20850a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aarch64-elf-binutils"
+    sha256 aarch64_linux: "3bfaad2ae71628722a97dbd244c75be4c1e02007e00bd2a4c0409fbe9bee25c1"
   end
+
 
   uses_from_macos "texinfo"
 
