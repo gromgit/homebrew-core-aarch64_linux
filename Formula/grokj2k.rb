ede493038faa60e213663765ff5fad9c027def53
danielnachun <daniel.nachun@gmail.com>
2022-09-13 09:47:27 -0700

    grokj2k: drop GCC dependency
    Closes #110575.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grokj2k.rb
+++ Formula/grokj2k.rb
@@ -39,10 +39,6 @@ class Grokj2k < Formula
     depends_on "llvm" => [:build, :test] if DevelopmentTools.clang_build_version >= 1205
   end
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   # https://github.com/GrokImageCompression/grok/blob/master/INSTALL.md#compilers
   fails_with :gcc do
     version "9"
