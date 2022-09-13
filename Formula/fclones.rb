4050461bfdb52e0d1879603e969430ab4481d1af
Piotr Kołaczkowski <pkolaczk@datastax.com>
2022-09-13 21:04:37 +0200

    fclones 0.28.0
    * fclones 0.28.0
    * update test
    
    Closes #110593.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fclones.rb
+++ Formula/fclones.rb
@@ -1,8 +1,8 @@
 class Fclones < Formula
   desc "Efficient Duplicate File Finder"
   homepage "https://github.com/pkolaczk/fclones"
-  url "https://github.com/pkolaczk/fclones/archive/refs/tags/v0.27.3.tar.gz"
-  sha256 "77281f9c3dead007b427b827cedcafab4ad8de2e6692b2cf67415afe9b817e0e"
+  url "https://github.com/pkolaczk/fclones/archive/refs/tags/v0.28.0.tar.gz"
+  sha256 "f1b79ef82140b4a09403ea9d5616d853684a08a163db2c7c62f437f266aa4177"
   license "MIT"
 
   bottle do
@@ -28,7 +28,7 @@ class Fclones < Formula
     (testpath/"bar2.txt").write "bar"
     output = shell_output("fclones group #{testpath}")
     assert_match "Redundant: 9 B (9 B) in 3 files", output
-    assert_match "a9707ebb28a5cf556818ea23a0c7282c", output
-    assert_match "16aa71f09f39417ecbc83ea81c90c4e7", output
+    assert_match "2c28c7a023ea186855cfa528bb7e70a9", output
+    assert_match "e7c4901ca83ec8cb7e41399ff071aa16", output
   end
 end
