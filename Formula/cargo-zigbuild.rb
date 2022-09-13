718482d7af2e881b20e7cba373376905fe147602
Rui Chen <rui@chenrui.dev>
2022-09-13 03:57:37 +0000

    cargo-zigbuild: add 0.12.2 bottle.

--- Formula/cargo-zigbuild.rb
+++ Formula/cargo-zigbuild.rb
@@ -6,6 +6,15 @@ class CargoZigbuild < Formula
   license "MIT"
   head "https://github.com/messense/cargo-zigbuild.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "278b643cc9620375704c82808de46d6634a4f315e7780bdcc58c4a2c971c56b6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ec8e08f96462df47c2224203ea46f62633e1d315a8019461b79e83f0252ffd8d"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab6c5d7b85cf3ef89d668f8c0c1779c708b63c3b92a596b5815f09a4dc324d54"
+    sha256 cellar: :any_skip_relocation, big_sur:        "13822761ab5a0f48092e99ca6bfb0cad58ae1feba0b92d909ffbe4460ac2ac04"
+    sha256 cellar: :any_skip_relocation, catalina:       "7b45f7e88a137b8f5967018a088b309c051b2760fc59a5abb6a87f9c8c8ab9a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "410fd635f3d703f9696402cccd27f49efa390fe45bd20b2eefc38cd21a411c5f"
+  end
+
   depends_on "rustup-init" => :test
   depends_on "rust"
   depends_on "zig"
