4866745a994f52f26d50a63cfe50323bfae9afc4
flyio-buildbot <63733732+flyio-buildbot@users.noreply.github.com>
2022-09-13 02:38:15 +0000

    flyctl 0.0.390
    Closes #110506.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.389",
-      revision: "b82ec99e957138a3d518a29d5dc84b188c0238dd"
+      tag:      "v0.0.390",
+      revision: "94fec0925c75cfe30921f1a4df25fa9cbf2877e9"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
