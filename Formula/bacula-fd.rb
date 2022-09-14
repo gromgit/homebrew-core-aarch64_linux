e911f646d62d70728422076f8326ebcba83224c1
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 07:46:11 +0800

    bacula-fd: update 13.0.1 bottle.

--- Formula/bacula-fd.rb
+++ Formula/bacula-fd.rb
@@ -1,13 +1,15 @@
 class BaculaFd < Formula
   desc "Network backup solution"
   homepage "https://www.bacula.org/"
-  url "https://downloads.sourceforge.net/project/bacula/bacula/11.0.6/bacula-11.0.6.tar.gz"
-  sha256 "0195a08bcd4f578ae4a9ce0d91f7f86731c634d56b810534722d721b2a9eecb7"
+  url "https://downloads.sourceforge.net/project/bacula/bacula/13.0.1/bacula-13.0.1.tar.gz"
+  sha256 "d63848d695ac15c1ccfc117892753314bcb9232a852c40e32cca88c0e918978a"
+  license "AGPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bacula-fd"
-    sha256 aarch64_linux: "09fbd728bd0b5f08462061c6afa8cd94df206e334e7987497e185e13c68ea43f"
+    sha256 aarch64_linux: "b33c9bacb6edfa06929c93046e3e025faeabf25b1a6f24a9bc0e3f21489e9481"
   end
+
 
   depends_on "openssl@1.1"
   depends_on "readline"
