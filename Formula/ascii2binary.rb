a5602a7e0f0f25f67e5ee5122b2161460c97abc1
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 04:07:13 +0800

    ascii2binary: update 2.14 bottle.

--- Formula/ascii2binary.rb
+++ Formula/ascii2binary.rb
@@ -6,13 +6,10 @@ class Ascii2binary < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f0c93f44f94301da7726208ebff6c51c83b751827518a92c7347c0312bafabcc"
-    sha256 cellar: :any,                 arm64_big_sur:  "c205cd2ae106cbbd23999f85812a51bbd0c6453caa761be24082cec7c721fc7f"
-    sha256 cellar: :any,                 monterey:       "3a8a279dfc5c852ab3fa081cd61eeb49ced7a94f3b3fb5fa0b9b747211cb2b51"
-    sha256 cellar: :any,                 big_sur:        "8b85d75f6bdcf06c7ab2cd68f6f276532aeed3b258dfa7c370c913a5cf1e4e70"
-    sha256 cellar: :any,                 catalina:       "7ad654dc498763cb63634191cb9c9c697604faa88fc41d51060df1bb6c0e42ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb4b9cc5fe32d49bbb8e0b8acd72396dbbd8fde547777f441b0deab8be17ac57"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ascii2binary"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a12e79a38e05c2a84f50f4c04fa3830ccaf21d0af07fc99d10d42befd2e44476"
   end
+
 
   depends_on "gettext"
 
