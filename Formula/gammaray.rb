bb8f203f2685ace74f8748b691dc72aa00706f16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-14 09:06:41 +0000

    gammaray: update 2.11.3 bottle.

--- Formula/gammaray.rb
+++ Formula/gammaray.rb
@@ -7,13 +7,13 @@ class Gammaray < Formula
   head "https://github.com/KDAB/GammaRay.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3cc302e1f904652a114308302ed2b1e8b42f704963e62a3c2c288118f4545f85"
-    sha256 cellar: :any,                 arm64_big_sur:  "bdf3eb554fd62c1f4fffb31049006fb6911b77f798adbc4e87061a5066e9b77d"
-    sha256 cellar: :any,                 monterey:       "d99a1367c0a43a9e0a71b4fb6061ce6386d87612e41fba412990e433400dfecf"
-    sha256 cellar: :any,                 big_sur:        "c2233705c3f3c5b0af61f67ebcd4c891313903c857a1a7773ab426ecd0e7acfb"
-    sha256 cellar: :any,                 catalina:       "990a0bd03354b751b3913cc6b308beba880fef6c97898290420221d9f8a080fa"
-    sha256 cellar: :any,                 mojave:         "3eeedcdccc07fc4dba59a90a3672f1a208fd608c6127839d5437019bfbd5adc6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "794c0222e2240c0b125f48f3ad1ef653d4b52b7f24557eb2a094d6287edeb559"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "e84fc150dc66a7e32b44546f038cb32bb156aae65688f14055f4182fa9cba79d"
+    sha256 cellar: :any,                 arm64_big_sur:  "7de52e10f8bbe77f1104f358602c4b25613c8f9d874d5ab2fa53828941cb1e23"
+    sha256 cellar: :any,                 monterey:       "f7e0316e417b5e6518acf88f61615cc274a7724bff18e5a7c923fd0af1684c49"
+    sha256 cellar: :any,                 big_sur:        "0d62f89cc2cd25a325d136fe6d2989d449265b01c023369da60b94955bafa58f"
+    sha256 cellar: :any,                 catalina:       "355dff5be6e35a6d5e4bb182d63eb88813c6db74b196b49109513612a7406b84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4872f5bb91e7ecc4589bcad43a2cabef330548c52e89413fd0d9c8e8c0676ab2"
   end
 
   depends_on "cmake" => :build
