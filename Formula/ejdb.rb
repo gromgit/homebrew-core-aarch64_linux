2087d7b74f3df2e09e5023c7ff2feabefea7e76b
danielnachun <daniel.nachun@gmail.com>
2022-09-13 21:10:19 -0700

    ejdb: drop GCC dependency
    Closes #110642.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ejdb.rb
+++ Formula/ejdb.rb
@@ -20,10 +20,6 @@ class Ejdb < Formula
 
   uses_from_macos "curl" => :build
 
-  on_linux do
-    depends_on "gcc" => [:build, :test]
-  end
-
   fails_with :gcc do
     version "7"
     cause <<-EOS
