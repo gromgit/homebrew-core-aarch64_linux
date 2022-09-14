1fc54e7b40ec6a4004f33891e2cf379c479a5c97
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 04:50:29 +0800

    asio: update 1.24.0 bottle.

--- Formula/asio.rb
+++ Formula/asio.rb
@@ -1,8 +1,8 @@
 class Asio < Formula
   desc "Cross-platform C++ Library for asynchronous programming"
   homepage "https://think-async.com/Asio"
-  url "https://downloads.sourceforge.net/project/asio/asio/1.22.1%20%28Stable%29/asio-1.22.1.tar.bz2"
-  sha256 "6874d81a863d800ee53456b1cafcdd1abf38bbbf54ecf295056b053c0d7115ce"
+  url "https://downloads.sourceforge.net/project/asio/asio/1.24.0%20%28Stable%29/asio-1.24.0.tar.bz2"
+  sha256 "8976812c24a118600f6fcf071a20606630a69afe4c0abee3b0dea528e682c585"
   license "BSL-1.0"
   head "https://github.com/chriskohlhoff/asio.git", branch: "master"
 
@@ -12,22 +12,14 @@ class Asio < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b3ea7931fb9e539f59bbf21f4647268487036aace40cda956d08507fe87bb90b"
-    sha256 cellar: :any,                 arm64_big_sur:  "5a8c945335115a2da264ec74162e57a5d92f4064544899c4df375a3cabc5e06a"
-    sha256 cellar: :any,                 monterey:       "98f1add58eeb5d6eb9d886781ef62dd0b6572dd8252252acdaa2bc1d3674c875"
-    sha256 cellar: :any,                 big_sur:        "dc03f2f6433eadeb86832d94a241c36a5be0eb23afde03644a11a62403a9b795"
-    sha256 cellar: :any,                 catalina:       "7c8ae5863a8abf20d8d811ec943c8bff0e702b7dc4e79ebbab9310bcc7a99e44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "723acb5f60519bdc4426ac9d6e4e5689f94c794b615b6f71759d26eda1385df1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/asio"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "750c5afa2077b7e7c57f07bf67acdd1962ad5c621f6b4469d3146794c584614e"
   end
+
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "openssl@1.1"
-
-  # Tarball is missing `src/examples/cpp20`, which causes error:
-  # config.status: error: cannot find input file: `src/examples/cpp20/Makefile.in'
-  # TODO: Remove in the next release
-  patch :DATA
 
   def install
     ENV.cxx11
@@ -63,15 +55,3 @@ class Asio < Formula
     end
   end
 end
-
-__END__
-diff --git a/configure.ac b/configure.ac
-index 56365c2..84045ba 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -241,4 +241,4 @@ AC_OUTPUT([
-   src/examples/cpp11/Makefile
-   src/examples/cpp14/Makefile
-   src/examples/cpp17/Makefile
--  src/examples/cpp20/Makefile])
-+])
