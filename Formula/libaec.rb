412e96d88be7d2535e0c784e5bc7c39a0eb3dbc9
Adrian Ho <the.gromgit@gmail.com>
2022-09-14 18:41:51 +0800

    libaec: update 1.0.6 bottle.

--- Formula/libaec.rb
+++ Formula/libaec.rb
@@ -7,14 +7,10 @@ class Libaec < Formula
   head "https://gitlab.dkrz.de/k202009/libaec.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "efb772c032399b490e5ba1f197e5cbf07efa3f9a83994ca770790f748a659587"
-    sha256 cellar: :any,                 arm64_big_sur:  "435271c305d6e1dcc07de9875dc3dfc8a0c6527a22c942e4961cabe49d8e75c3"
-    sha256 cellar: :any,                 monterey:       "2d40180340fb2ac382a9324d049dde7ab47f13d9ec812c9df6656dbd0ea91ffd"
-    sha256 cellar: :any,                 big_sur:        "1dbb32dfbf75c7abc923f53a8ebfcbfca74426c2386521b424ce84232e3ce0ac"
-    sha256 cellar: :any,                 catalina:       "8fb8196910a91e85aaa1343f904ffbc3e3e0565774aab6d4ab6455cd1795dda8"
-    sha256 cellar: :any,                 mojave:         "540f342390a38bb62dad9ccaca77890be88d903bd3092c17054b692eee8b7120"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f7b059adad11a2a31efa3331663189c7800af8666843b22c9cc7ccf4add762a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libaec"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1234ea7352afe99702283eb256bfeb9f61332552988c2dc620e46cec72e0f091"
   end
+
 
   depends_on "cmake" => :build
 
