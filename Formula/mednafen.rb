cc5eb1ae01fe01850133c63845788c6b80c92965
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-14 02:16:10 +0000

    mednafen: update 1.29.0_3 bottle.

--- Formula/mednafen.rb
+++ Formula/mednafen.rb
@@ -12,12 +12,12 @@ class Mednafen < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "46497ce4ed8f15cbd87f47ae217299b0e7335e7a2c23c7b530bfabdd0059d71b"
-    sha256 arm64_big_sur:  "ed468b186353eb0ad71c36a49388cade47fc2636d2ee30ec358209638ec31bee"
-    sha256 monterey:       "521859a49df7733c5049d673e8f5bb54e4ae063c26eb58484fe29fb1561e611c"
-    sha256 big_sur:        "e5e790528b981bf9da07c3017f05cf3eaa16151609a76d780e45d9d7710995b5"
-    sha256 catalina:       "58fbb821874164c2a0017a09a0693e8090972300cba974fa57c7f8486941d25e"
-    sha256 x86_64_linux:   "f24de2c8d65dfc7c2cea862da3fe9b0da7d875e3cb6b7afac422ee4c12bb01cb"
+    sha256 arm64_monterey: "59e6b6f8f9d8797c87b78a7d80d41a75375cec35123e574406f80d8e0464a06c"
+    sha256 arm64_big_sur:  "48a7df09b1d9e6244883287fe1ba28cfcb73b94b4d4ca40b90aeda35a0d076d0"
+    sha256 monterey:       "0d17cda29047b8b86656601227e40e678e358cac1a8acb60841e31c1f96ca4a8"
+    sha256 big_sur:        "e04c8f424849bf8afdfd1bfa55e7f768f87ddb5dcd9449543ecb39a9d8cc7c72"
+    sha256 catalina:       "558b3492c6a264effc024be0462fd05f60d94d78ad20cf117b84256fa47a8969"
+    sha256 x86_64_linux:   "c72f67035b985636b2d6e0c204f6218e43575b6c20d22e547b9516706d4310c5"
   end
 
   depends_on "pkg-config" => :build
