4cac7981ab2b3508245942b82b9a0e177cc74540
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-14 14:45:24 +0800

    llvm: tweak `scan-build` test.
    `scan-build` is designed to wrap a build tool, so calling `make` might
    make this test behave better.

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -631,8 +631,8 @@ class Llvm < Formula
         return 0;
       }
     EOS
-    assert_includes shell_output("#{bin}/scan-build clang++ scanbuildtest.cpp 2>&1"),
-      "warning: Use of memory after it is freed"
+    assert_includes shell_output("#{bin}/scan-build make scanbuildtest 2>&1"),
+                    "warning: Use of memory after it is freed"
 
     (testpath/"clangformattest.c").write <<~EOS
       int    main() {
