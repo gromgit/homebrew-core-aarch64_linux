e554847f286301c324da75de0ce5938802802e8e
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 00:31:49 +0800

    apibuilder-cli: update 0.1.43 bottle.

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -1,13 +1,15 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.42.tar.gz"
-  sha256 "bffdf934888dd9ef78a5a0b029fea743732899551cfbbe5d8203222718ffe4f4"
+  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.43.tar.gz"
+  sha256 "69c8c100ba9d56e83c146b5338c2a68c189fc6d1ca2eb184f357a84091224077"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "15e755ce46ae2dec90cdd7da2ac2fad35b09ef63f66140f441d70af07074fd59"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/apibuilder-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "32df141964fc2d4029c510c758333051244147eccd84931d71a21b3903025167"
   end
+
 
   uses_from_macos "ruby"
 
