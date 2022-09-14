a1dcd5e6794b833f2d5c6576290b9c9421c7a798
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 00:20:42 +0800

    apgdiff: update 2.7.0 bottle.

--- Formula/apgdiff.rb
+++ Formula/apgdiff.rb
@@ -1,40 +1,38 @@
 class Apgdiff < Formula
   desc "Another PostgreSQL diff tool"
   homepage "https://www.apgdiff.com/"
-  url "https://www.apgdiff.com/download/apgdiff-2.4-bin.zip"
-  sha256 "12d95fbb0b8188d7f90e7aaf8bdd29d0eecac26e08d6323624b5b7e3f7c7a3f7"
+  url "https://github.com/fordfrog/apgdiff/archive/refs/tags/release_2.7.0.tar.gz"
+  sha256 "932a7e9fef69a289f4c7bed31a9c0709ebd2816c834b65bad796bdc49ca38341"
   license "MIT"
 
   livecheck do
-    url "https://www.apgdiff.com/download.php"
-    regex(/href=.*?apgdiff[._-]v?(\d+(?:\.\d+)+)[._-]bin\.zip/i)
+    url :stable
+    regex(/^release[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "79ad8fdae60b989fb5e986ce237ed17d35dbddce6793a611ee5f05fbf9767ec6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/apgdiff"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "bbf03a4049337fc1e304c24d66ba326b4e401a3096f5415b0946ae9d3f8b06a0"
   end
 
+
   head do
-    url "https://github.com/fordfrog/apgdiff.git"
+    url "https://github.com/fordfrog/apgdiff.git", branch: "develop"
     depends_on "ant" => :build
   end
 
   depends_on "openjdk"
 
   def install
-    jar = "apgdiff-#{version}.jar"
+    jar = "releases/apgdiff-#{version}.jar"
 
     if build.head?
-      system "ant"
-      cd "dist" do
-        jar = Dir["apgdiff-*.jar"].first
-        mv jar, ".."
-      end
+      system "ant", "-Dnoget=1"
+      jar = Dir["dist/apgdiff-*.jar"].first
     end
 
     libexec.install jar
-    bin.write_jar_script libexec/jar, "apgdiff"
+    bin.write_jar_script libexec/File.basename(jar), "apgdiff"
   end
 
   test do
