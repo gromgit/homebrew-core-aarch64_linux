3363228c75dae41fc8b6bbf6710c5783f2a22f19
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-14 02:16:08 +0000

    cmus: update 2.10.0_1 bottle.

--- Formula/cmus.rb
+++ Formula/cmus.rb
@@ -8,12 +8,12 @@ class Cmus < Formula
   head "https://github.com/cmus/cmus.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "db4d3a8fc7365aebb2d04c00fab660995e17fd33e1e858d227320f0132c5f750"
-    sha256 arm64_big_sur:  "c5b09c75e7b2dba15327fd4d9e44558bc0e6349a38c812329930e5f699361f5b"
-    sha256 monterey:       "33b6f93095d734ad3b94662251373c97ffd089da3487cb63bd8aa25c68feb326"
-    sha256 big_sur:        "e45b48a6d19b61633897b73389c8c1648026543072735330018e85960f2b85cd"
-    sha256 catalina:       "3b5595c657158e338d4ee9665afbc7ba4691e0eb228637eb42fb147b7cc33aee"
-    sha256 x86_64_linux:   "7ee4112ba4f7a8c80b20a5edbee93bf01fae4c333bd24398023bc8d3c1e71f68"
+    sha256 arm64_monterey: "4d9a04e3f219777e94ee43abbc36a08a308a4b038349b8421bb050fea197dcdc"
+    sha256 arm64_big_sur:  "1da2b55446e9adbc6d72b4b22d91530ae4e8c07b8d5658b33622ec64313198a9"
+    sha256 monterey:       "36f976d5db181233588ec371ad862f91725aed2d0797a53d96eb1dc99908a2b6"
+    sha256 big_sur:        "1a2494a1a8ba3cdbe59c06617fd9fc1f1bb2f36b1126ce425e1b9d9f9fe34adf"
+    sha256 catalina:       "965774a8170b4d6f194974736f4ac8d188a15da9bb75469a7ce915d4bbf93da0"
+    sha256 x86_64_linux:   "1a95494ac6bbf9215e929bb5b51723eea1f5b08e15eeb66492544c2bf9df8778"
   end
 
   depends_on "pkg-config" => :build
