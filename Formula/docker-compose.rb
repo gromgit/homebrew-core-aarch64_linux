856ca59a83a5617c405db42681dc406e82276386
Rui Chen <rui@chenrui.dev>
2022-09-14 09:21:04 -0400

    docker-compose 2.11.0
    Closes #110681.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,8 +1,8 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.10.2.tar.gz"
-  sha256 "74c86d544fcfb80bb2d3b58187bd017adb0e62863d22114a66c14fc94fdbc421"
+  url "https://github.com/docker/compose/archive/v2.11.0.tar.gz"
+  sha256 "fe01c85ad31767c22d5f6ea24e6809f5b675c9253d534138ee4095428dfcb96a"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
