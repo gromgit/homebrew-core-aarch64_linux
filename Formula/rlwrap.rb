ad6c71bc6be3c2f6bfb9b160b83723806326b9c9
Adrian Ho <the.gromgit@gmail.com>
2022-09-14 08:12:41 +0800

    rlwrap: update 0.45.2 bottle.

--- Formula/rlwrap.rb
+++ Formula/rlwrap.rb
@@ -12,14 +12,10 @@ class Rlwrap < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "2e026b8952dfc5806e289f72d741d03c3a53fa028a5aab2965b4b7793cdd1bf7"
-    sha256 arm64_big_sur:  "d7f6b7527f6b090fe587c18ba314460a84949a81158399ff143521c2bf949e91"
-    sha256 monterey:       "7a3f36bd7365d80d9182e1b1ff9f77177d9203c2358a36794fba1547dadc51fa"
-    sha256 big_sur:        "4776bfe5bf3753463d331b150b37be72f4729aa144d9bb45030c56e9ad16c6a0"
-    sha256 catalina:       "ca0fa52e2eb8649716938dd6d21a15652a7eeaf2ba0acfcb9cbd6cb9a5dae490"
-    sha256 mojave:         "fcfe56a0eac619bcf9d9d1f8f5ddda648e09719c8c78c0db3d599f7edb7a59b7"
-    sha256 x86_64_linux:   "525f35129e98623fbebda88c217862faf363bd9641d20858a9b8c14c120f4458"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rlwrap"
+    sha256 aarch64_linux: "70d87ce5642df392a24618e7bb13fd8d3bfc4824a341cfe430f12305433c655b"
   end
+
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
