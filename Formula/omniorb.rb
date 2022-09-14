bb283b5f76f69184847e6469b3e0176030019545
Miles Liu <miles@bung.cc>
2022-09-14 10:51:10 +0800

    omniorb: fix `python3` reference.
    See #110462, and also update the license.
    
    Closes #110631.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/omniorb.rb
+++ Formula/omniorb.rb
@@ -3,7 +3,7 @@ class Omniorb < Formula
   homepage "https://omniorb.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-4.2.4/omniORB-4.2.4.tar.bz2"
   sha256 "28c01cd0df76c1e81524ca369dc9e6e75f57dc70f30688c99c67926e4bdc7a6f"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   revision 2
 
   livecheck do
@@ -30,12 +30,19 @@ class Omniorb < Formula
   end
 
   def install
-    ENV["PYTHON"] = which("python3")
+    ENV["PYTHON"] = python3 = which("python3.10")
+    xy = Language::Python.major_minor_version python3
+    inreplace "configure",
+              /am_cv_python_version=`.*`/,
+              "am_cv_python_version='#{xy}'"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
 
     resource("bindings").stage do
+      inreplace "configure",
+                /am_cv_python_version=`.*`/,
+                "am_cv_python_version='#{xy}'"
       system "./configure", "--prefix=#{prefix}"
       system "make", "install"
     end
