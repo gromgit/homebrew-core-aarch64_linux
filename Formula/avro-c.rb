bf7ace228b683b2a70735103410b7be827263d61
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 06:47:01 +0800

    avro-c: update 1.11.0 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -7,13 +7,10 @@ class AvroC < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "003f19bcd51baea53f86d97488f83552dc8477eedef78a371bdc166ac0d183ab"
-    sha256 cellar: :any,                 arm64_big_sur:  "f30243ab877db4e81ff07c5dea0e266228ac04bf178144d3f9caaab45a876f86"
-    sha256 cellar: :any,                 monterey:       "35a23d1c97d7e5b50adf7ff2f8ff3ddcb6f9271c85fc033306e8447484e23b5c"
-    sha256 cellar: :any,                 big_sur:        "cbb5817beab9e6dfb2dd1a5fafed449c6aa5dabf8525b71142854771ba46ab35"
-    sha256 cellar: :any,                 catalina:       "d91479cfd09b368e971ed2c926c480edfe88f62df39476e780ec0d863cc19dd5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "80da35f4303fb1c13aa3a8728d2e4dd35537ba984a075531c2f7d17f6d03e823"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/avro-c"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "03ccf3cf6073ac02af5dc66da4f6bde183c01a581b20e695da7447f94a36c721"
   end
+
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
