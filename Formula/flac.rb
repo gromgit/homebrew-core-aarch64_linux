4ee48fb77a10938cb6372aaf04bf61aa6eceeeb5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-14 02:16:17 +0000

    flac: update 1.4.0 bottle.

--- Formula/flac.rb
+++ Formula/flac.rb
@@ -20,12 +20,12 @@ class Flac < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ca0f491b3a3c353e7562d0e8f25a1b54d07c760d4164251ab328239d276ba600"
-    sha256 cellar: :any,                 arm64_big_sur:  "7e13abd55a15c99201005e8f311c0015225f59c82f7408e314d3cbfb5f98f06e"
-    sha256 cellar: :any,                 monterey:       "cf8ac3c3150544be3b809a549c7cf7bb71344904e1b4d4c9c9970c38aa7b2072"
-    sha256 cellar: :any,                 big_sur:        "2edc8dddafb0731d45237cdcb5036ff60988575fa9171691b7dc9aa8215430e4"
-    sha256 cellar: :any,                 catalina:       "48463384c101270b29fb1b436443ee8b67dec9af0830c58adce44225142de8a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7dab52777c3c995c3d7b93b5a86d2ca6ad1e9f5be97f993132ace30c50cc3af5"
+    sha256 cellar: :any,                 arm64_monterey: "b28eb662fa249a1c7a52c2ddcbfcc0a8234ab6af1c4106a802d72c831eb4b2f4"
+    sha256 cellar: :any,                 arm64_big_sur:  "cd5f7c81c96292f443d87c300fb384c79cd482cc1d9634a5127b08e7ba9d7044"
+    sha256 cellar: :any,                 monterey:       "01e8e78f2b677a6baa484baff6e58bdbf5c848ee2f9a1356461678a7301478e7"
+    sha256 cellar: :any,                 big_sur:        "4c8f98283d1643179dd1ab5dc0a25a1634786bda6dc9156277b9e2c78b097553"
+    sha256 cellar: :any,                 catalina:       "aecc4b3af655c6c55fa644eded750fcaaceaf5a829bac2145f99d2aa3b871f8e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bc67c5532ae6c74b1799498f8a1bdb53fe0553f04c4e20069e73e3bf6ca2394f"
   end
 
   head do
