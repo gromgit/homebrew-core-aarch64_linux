2fa6c0286e6ed8a6be0361c4f346466d2520cdce
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2022-09-15 20:04:55 +0300

    got 0.75 (new formula)
    * got 0.75 (new formula)
    * got: miscellaneous build fixes.
    
    Closes #110811.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/got.rb
@@ -0,0 +1,36 @@
+class Got < Formula
+  desc "Version control system"
+  homepage "https://gameoftrees.org"
+  url "https://gameoftrees.org/releases/portable/got-portable-0.75.tar.gz"
+  sha256 "b35324893c9aefbd6bf49602a6c9bb9d6e20670aff3c9734fefead0205bb9cb2"
+  license "ISC"
+
+  depends_on "bison" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on :macos # FIXME: build fails on Linux.
+  depends_on "ncurses"
+  depends_on "openssl@1.1"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libmd"
+    depends_on "util-linux" # for libuuid
+  end
+
+  def install
+    # The `configure` script hardcodes our `openssl@3`, but we can't use it due to `libevent`.
+    inreplace "configure", %r{\$\{HOMEBREW_PREFIX?\}/opt/openssl@3}, Formula["openssl@1.1"].opt_prefix
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    ENV["GOT_AUTHOR"] = "Flan Hacker <flan_hacker@openbsd.org>"
+    system bin/"gotadmin", "init", "repo.git"
+    mkdir "import-dir"
+    %w[haunted house].each { |f| touch testpath/"import-dir"/f }
+    system bin/"got", "import", "-m", "Initial Commit", "-r", "repo.git", "import-dir"
+    system bin/"got", "checkout", "repo.git", "src"
+  end
+end
