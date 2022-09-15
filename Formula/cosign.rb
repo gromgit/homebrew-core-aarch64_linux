dda1ded38825d19e77c5ab9e186f436f0689210b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-15 05:13:51 +0000

    cosign: update 1.12.0 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -8,12 +8,12 @@ class Cosign < Formula
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "22a461c17af6d5f27b4805eacd863f36f8f6f9cf0020c9b9aba0ac6dc5af71fc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae27e25dc5d9f52a183ef6f318e175076781f3f8bf73604c9e7555a43f2ead94"
-    sha256 cellar: :any_skip_relocation, monterey:       "7f93fdf8e905664856998dd3cf2ac3fdbee7d306be390c3fda54feea78efcf68"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8a7e760f427e3a073c249964a736793243b299865328d66556049af344555db2"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8699b89b6afb1b9438444782ef80554bffa5fe778bda85f720d116920aef83a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1402eed37b78efa18f3e42d0536c818d2ff5ef7be6ca58abe977d335475ab925"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "63f6d06218eaef81f17988ab8d703ea65c3c14cd976b5d5f33735c04d49cc10d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1eb5d5e23fb056d22ec1f080b681596d4ec666a228c26c7eb4f4651f08e6ad4d"
+    sha256 cellar: :any_skip_relocation, monterey:       "3b13e0ccd162ee7a858c3ca93cf831664ce6e798ec23bc8d71c3135d35896fe6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "386b6de77b50ffe80fd6a0200bbe3a2620077d6e4d7bcfda07b7ebc76437113e"
+    sha256 cellar: :any_skip_relocation, catalina:       "d26eb1f9dc0412ce93071eb7f0b939f730dbe89627e184d1c5c7071b13c81727"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e4b4b3f910c85389816063dcbd8e4fac62df151a0e89ae4d46c9fc2fdca53358"
   end
 
   depends_on "go" => :build
