75e0e0166393fff673a8bc35edebdc9507fbeca3
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 01:39:15 +0800

    capnp: update 0.10.2 bottle.

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -1,8 +1,8 @@
 class Capnp < Formula
   desc "Data interchange format and capability-based RPC system"
   homepage "https://capnproto.org/"
-  url "https://capnproto.org/capnproto-c++-0.9.1.tar.gz"
-  sha256 "83680aaef8c192b884e38eab418b8482d321af6ae7ab7befa3a9370b8e716aad"
+  url "https://capnproto.org/capnproto-c++-0.10.2.tar.gz"
+  sha256 "7cd91a244cb330cda5b41f4904f94b61f39ba112835b31fa8c3764cedbed819f"
   license "MIT"
   head "https://github.com/capnproto/capnproto.git", branch: "master"
 
@@ -12,14 +12,10 @@ class Capnp < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "b2859736c7a3979fa5a11535ef2ddb55d5613020e48160f3825d7bf58c82c5f4"
-    sha256 arm64_big_sur:  "8043e17871dd912ba43bea535529002ad50cf6cb4915b97e09be64e37549630f"
-    sha256 monterey:       "524e7613b378c1562df99ec4129fb253cf6d6bcc5b0904167faa89a6473d5924"
-    sha256 big_sur:        "bba67dd6a19595bcf2ca687bcce24a725fb05ee3e862506ddc6cbb7aec87defd"
-    sha256 catalina:       "587630a09f37214d09864150ba694cc089b326cc57fe787427a71114b4b26244"
-    sha256 mojave:         "b0d06dffe22d722e87a066b80cf667cb41372f175a80cd6877b5d856bfb080ac"
-    sha256 x86_64_linux:   "23877c82c6b6661adbdd98aad98ccb9b42e23210ad0001b27ea81983acc0db77"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/capnp"
+    sha256 aarch64_linux: "16ff5895e7108c2429ccd9146680a5c07629a355345c87151d7a701a6c14650d"
   end
+
 
   depends_on "cmake" => :build
 
