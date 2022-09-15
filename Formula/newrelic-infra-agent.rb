265220d0e1ed2d07a683ffc1119d3903545faa14
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-15 02:48:41 +0000

    newrelic-infra-agent: update 1.29.1 bottle.

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -15,10 +15,12 @@ class NewrelicInfraAgent < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "7114f1128f5e4b3fcfa14b2aac969e3e5a3ea1f78306166bd95788a5df42290d"
-    sha256 cellar: :any_skip_relocation, big_sur:      "4799d41ae6f2245f48f420226280bbeedd0eb6cc1954cbe5530f113008dcc42b"
-    sha256 cellar: :any_skip_relocation, catalina:     "8dc13322b51a01f6f21d4a0b16e8b5b91a71a940a240efa8cad6665faf5bb77a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "13317cc0fcc57a38b7df1c54fdf76405ec6ae582b8ebf75325d178aeaa0aad87"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5622210f203ecf07f3819b9d6bf97354cd50bdae4328f67bafd3dba2b2935b2a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a07c352f6a5aea2909fbb935be1e4eb954ff8672bbf3e536f7e2635e2d1b1086"
+    sha256 cellar: :any_skip_relocation, monterey:       "7114f1128f5e4b3fcfa14b2aac969e3e5a3ea1f78306166bd95788a5df42290d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4799d41ae6f2245f48f420226280bbeedd0eb6cc1954cbe5530f113008dcc42b"
+    sha256 cellar: :any_skip_relocation, catalina:       "8dc13322b51a01f6f21d4a0b16e8b5b91a71a940a240efa8cad6665faf5bb77a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13317cc0fcc57a38b7df1c54fdf76405ec6ae582b8ebf75325d178aeaa0aad87"
   end
 
   depends_on "go" => :build
