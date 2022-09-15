b924bd85d5de58cf65349953455d33dd989428fe
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 06:29:25 +0800

    xterm: update 372 bottle.

--- Formula/xterm.rb
+++ Formula/xterm.rb
@@ -12,13 +12,10 @@ class Xterm < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "8777bf4b0d816aa1f5d3b97af989f408c17004410b45d91ab3c1fca7634397cb"
-    sha256 arm64_big_sur:  "c6a6bc4089c043cb806c47201931fd03508b181b591ad6b9f0800e3c4acab2b9"
-    sha256 monterey:       "b12e5eebb766e0711b2615cc148d91a19a2d7c602052cdd3812ae4b63f0c8cd1"
-    sha256 big_sur:        "97817ff7fbaf2ce0fdf0725fdacc163cbb84d29b1e783613e67a24f2c91a068f"
-    sha256 catalina:       "a5e2b36250af85d48df3745eb34aa3ca74f3f8bf6db449da76668bafc1761c4c"
-    sha256 x86_64_linux:   "8f514c7c980670eecb001022ae8104507022ca19c3da7a1d3c1359b9bad0713b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/xterm"
+    sha256 aarch64_linux: "813c84f44fdcc7917e0cab938b9973feed8698b4c3a5d91874256315373d5456"
   end
+
 
   depends_on "fontconfig"
   depends_on "freetype"
