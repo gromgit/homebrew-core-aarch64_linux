70f991a51814d5df948c19f6ef38a2dffb0f7e64
Rui Chen <rui@chenrui.dev>
2022-09-15 09:48:28 -0400

    mmctl 7.3.0
    Closes #110792.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mmctl.rb
+++ Formula/mmctl.rb
@@ -2,8 +2,8 @@ class Mmctl < Formula
   desc "Remote CLI tool for Mattermost server"
   homepage "https://github.com/mattermost/mmctl"
   url "https://github.com/mattermost/mmctl.git",
-      tag:      "v7.2.0",
-      revision: "9e6121b94efac5f6a0dfa74ec707a01c3e1f6ceb"
+      tag:      "v7.3.0",
+      revision: "0cf891b9f535570479e3a8b9c923212528f407fc"
   license "Apache-2.0"
   head "https://github.com/mattermost/mmctl.git", branch: "master"
 
