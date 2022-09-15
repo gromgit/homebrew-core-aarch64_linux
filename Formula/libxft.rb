3eb5c1f9cdd04b5dd64c023a5f472cb793c4c81a
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 06:10:38 +0800

    libxft: update 2.3.4 bottle.

--- Formula/libxft.rb
+++ Formula/libxft.rb
@@ -6,14 +6,10 @@ class Libxft < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "df14d3422f930c8339a90745ea96ac4ed19dbd320e79bfea4870dae1d2b9bf2a"
-    sha256 cellar: :any,                 arm64_big_sur:  "78a5e3d5795d9a95a012e1b9cbdc431373d1a2b9215dd4cc467ed8fe8833c8fd"
-    sha256 cellar: :any,                 monterey:       "e5e3c477b818f83abb22998e773b850a63ac57011549c2d7cb7e3fd36099dfe6"
-    sha256 cellar: :any,                 big_sur:        "1409b0b789f87502f146806ca110c91de010d4815a02d2a2b01a1fca8a9b34e1"
-    sha256 cellar: :any,                 catalina:       "59ee89bf143385cf4d64b79b2b324874cbe8b5a1ecdaef2a9739fe6104bf240e"
-    sha256 cellar: :any,                 mojave:         "b192514f22f8e6fe250afc95e471512622942dd02d3e58b7fbb0b085a281c667"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32e3761cd7474d9f427959ce47db1b7449a1d210e51ad08d664a2f6b2092e229"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxft"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4e43c6e8ec4c85cb54c37e2cf02dda6c72781a9edab2e33cd49a3da012b8b63e"
   end
+
 
   depends_on "pkg-config" => :build
   depends_on "fontconfig"
