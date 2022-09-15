4896194a76cfc025237372f909ce9c172e08814e
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 13:20:05 +0800

    blitz: update 1.0.2 bottle.

--- Formula/blitz.rb
+++ Formula/blitz.rb
@@ -7,15 +7,10 @@ class Blitz < Formula
   head "https://github.com/blitzpp/blitz.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3403559216a7fe96f965537612fdbe8852810870a54c2ba96b6c0d15c9d03726"
-    sha256 cellar: :any,                 arm64_big_sur:  "0b04264665a05ca8b018a1f9b8e7452297d675b5bb5b50f49af2dd5176de462e"
-    sha256 cellar: :any,                 monterey:       "d18fe13200228947c919659133af057d66da2ccbcbf65febd057a612b2014a8d"
-    sha256 cellar: :any,                 big_sur:        "eaf888ad2387b3aabccdc8ba82104b942dfa91b058b335449a7bdeb26213ce7d"
-    sha256 cellar: :any,                 catalina:       "2bfa3e5a52f0f51e9e02c84f10f804093b7080c158b3376f330dd51c0f9e3d23"
-    sha256 cellar: :any,                 mojave:         "a06052c039592fe7b41face9c72d715ba0602456a9df07a40a472d3ceba02c00"
-    sha256 cellar: :any,                 high_sierra:    "79901f790ea3583942a72ababfba3dc6569169f228b0428c047da52f1f99c02d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69b643943846376a48b8cf266f639fff523ad74430a2e784c2bfdfab21137179"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/blitz"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a9b005618325f5e4f9eb329a9699bec772f25cc10da0d800e1eebaea2caf8c15"
   end
+
 
   depends_on "cmake" => :build
 
