fbff4d32fd0435b8ce09c3eb669341808697b7c6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-15 16:37:29 +0000

    fonts-encodings: add 1.0.6 bottle.

--- Formula/fonts-encodings.rb
+++ Formula/fonts-encodings.rb
@@ -5,6 +5,15 @@ class FontsEncodings < Formula
   sha256 "77e301de661f35a622b18f60b555a7e7d8c4d5f43ed41410e830d5ac9084fc26"
   license :public_domain
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "863542f9892912c4f98cf5f8af13dbb2bc2be16d674559759e8d2b003d12ce56"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "863542f9892912c4f98cf5f8af13dbb2bc2be16d674559759e8d2b003d12ce56"
+    sha256 cellar: :any_skip_relocation, monterey:       "863542f9892912c4f98cf5f8af13dbb2bc2be16d674559759e8d2b003d12ce56"
+    sha256 cellar: :any_skip_relocation, big_sur:        "863542f9892912c4f98cf5f8af13dbb2bc2be16d674559759e8d2b003d12ce56"
+    sha256 cellar: :any_skip_relocation, catalina:       "863542f9892912c4f98cf5f8af13dbb2bc2be16d674559759e8d2b003d12ce56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ccc20d215d88365698fdd40a5066d867e2d465e5057a0084ded551a433ab088f"
+  end
+
   depends_on "font-util"   => :build
   depends_on "mkfontscale" => :build
   depends_on "util-macros" => :build
