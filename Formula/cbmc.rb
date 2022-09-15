a3aeffb10c979ebe9d1b04f160fbb58bda7c5788
danielnachun <daniel.nachun@gmail.com>
2022-09-15 09:38:12 -0700

    cbmc: drop GCC dependency
    Closes #110805.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -22,10 +22,6 @@ class Cbmc < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with gcc: "5"
 
   def install
