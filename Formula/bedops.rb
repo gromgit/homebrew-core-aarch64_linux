6cb0fd687d488f4448f8c83279c7429c4c12c7ff
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 10:01:23 +0800

    bedops: update 2.4.41 bottle.

--- Formula/bedops.rb
+++ Formula/bedops.rb
@@ -1,14 +1,15 @@
 class Bedops < Formula
   desc "Set and statistical operations on genomic data of arbitrary scale"
   homepage "https://github.com/bedops/bedops"
-  url "https://github.com/bedops/bedops/archive/v2.4.40.tar.gz"
-  sha256 "8c01db76669dc58c595e2e1b9bdb6d462f3363fc569b15c460a63a63b8b6bf30"
-  license "GPL-2.0"
+  url "https://github.com/bedops/bedops/archive/v2.4.41.tar.gz"
+  sha256 "3b868c820d59dd38372417efc31e9be3fbdca8cf0a6b39f13fb2b822607d6194"
+  license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bedops"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "4f717d0cb6a17047d3e5ee50a1fec2c235fe67c793aa4280a9d0c1b8653f9e39"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ba933a550166b0877437ccd3f51b5e5561bb7fa4a94a6064ee978e326354d29b"
   end
+
 
   def install
     system "make"
