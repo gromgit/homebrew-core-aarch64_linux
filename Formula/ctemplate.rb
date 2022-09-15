5b8c143c31ca81d26730234d73e2e69db99e9a34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-15 14:12:19 +0000

    ctemplate: update 2.4_1 bottle.

--- Formula/ctemplate.rb
+++ Formula/ctemplate.rb
@@ -8,13 +8,13 @@ class Ctemplate < Formula
   head "https://github.com/olafvdspek/ctemplate.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "2af8837c0e6f6cb3405008c71795fcc3def16818aa2512365ec027cb3ad4b48e"
-    sha256 cellar: :any,                 arm64_big_sur:  "d9b6bdf4a7d13079ea3eb55d1cae8307513a8aaa7d782eda9333a3a96ff45523"
-    sha256 cellar: :any,                 monterey:       "407f8bdf5dea727de91e5436cab5b0e271fdd935806aa985d38d5ed4c2db57e9"
-    sha256 cellar: :any,                 big_sur:        "3e4c9c7028cf8037cc61000e24d25fd34bc8741a863b440653c087908ff33169"
-    sha256 cellar: :any,                 catalina:       "9451278bdf27133395761b18b00e88fb5ac3765bb1b5a0da7acb88a671ef7977"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aef9050d0aa26b1a38f26df4e71d084ef439f5f26600f9defd8ce8b5de2221c6"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_monterey: "fc6f772b98ecb4ac32af2525fb14e00e78d3869965b0b2cef4e9052ae7920f15"
+    sha256 cellar: :any,                 arm64_big_sur:  "4857a36c04ea358a584ccc4bf814cf14ea132f719044982bba4295ca3bee54ce"
+    sha256 cellar: :any,                 monterey:       "1186d9da15756058d715022d75e654ddf8c30573f79490a6d945ba3501e651d9"
+    sha256 cellar: :any,                 big_sur:        "5e2edf873c559dbe6fee25404fb5f73dd0237358d553384c65284db99c1e1aeb"
+    sha256 cellar: :any,                 catalina:       "03716a690ca0715b4832e3cbc95441a3e9765c36b277a33f85ac7806469d9928"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0782447bd58da47c13e2c3fc367e616addbe434c4fef762daea8bf6ea0b85626"
   end
 
   depends_on "autoconf" => :build
