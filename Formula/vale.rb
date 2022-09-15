7b5192f3f9905e445e823d3d76e20d357d0172ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-15 05:13:55 +0000

    vale: update 2.20.2 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -6,12 +6,12 @@ class Vale < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d6945d621bbda7fb4bffcf093e1f88e438cdc135b7e139a883e8115de18f3ae"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3bd680e73b11ea28c8f189e831d51dc58916b0a06c22e39990eed443b2a983b2"
-    sha256 cellar: :any_skip_relocation, monterey:       "d3049e525eeb2e4c56b1aa77b4c794b919584a27a6a59fc24f79191ee49f0727"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39969b944c5c616b18d03d35ddc4a824f7ee2fd25974eb91a58af9a7ccbe8b71"
-    sha256 cellar: :any_skip_relocation, catalina:       "2778ba29ec8a6f2e96866550b2b6a135984b47cd52f33e551da93acd017e35ae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05725596b1dbe72766634f941ad42539013ccb5f1804dc115c9750adba8e64c2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "163d127945df35524d5a39bf174178979e01535f716a86df4b1522849eefdce0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2384e5de6ae05576d69ce26b8e18068efc8b1a53ebd907a122ffce07f9c59e2b"
+    sha256 cellar: :any_skip_relocation, monterey:       "e5d15a8bbe627f737664d2765712a1bf81c1d26c2a860dc64bbd5dd557ee2c88"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fe7a880822063535d8fa5033fba65c27491d373c371a422d9ce4ea72b635cce"
+    sha256 cellar: :any_skip_relocation, catalina:       "885fc1efd88c9ef46b5bb1226d894e914dc578639361112bec4ec66c32178e37"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d97b0e59be2cee11de3dc12733c142f8f5b40a8e01750380a2cfc4fad0df404"
   end
 
   depends_on "go" => :build
