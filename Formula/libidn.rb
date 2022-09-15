eb95958f982246b8d73c238708cfbfd4764b13d1
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 12:31:49 +0800

    libidn: update 1.41 bottle.

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -1,15 +1,16 @@
 class Libidn < Formula
   desc "International domain name library"
   homepage "https://www.gnu.org/software/libidn/"
-  url "https://ftp.gnu.org/gnu/libidn/libidn-1.38.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.38.tar.gz"
-  sha256 "de00b840f757cd3bb14dd9a20d5936473235ddcba06d4bc2da804654b8bbf0f6"
+  url "https://ftp.gnu.org/gnu/libidn/libidn-1.41.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.41.tar.gz"
+  sha256 "884d706364b81abdd17bee9686d8ff2ae7431c5a14651047c68adf8b31fd8945"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libidn"
-    sha256 aarch64_linux: "15426f27be74fa059f7a79f6811028b7df80f8e914f28265c440786924f14a94"
+    sha256 aarch64_linux: "5d31c9adecba628b47ec689e84aada9de34f2ac7f502fbee549716636f99bb72"
   end
+
 
   depends_on "pkg-config" => :build
 
