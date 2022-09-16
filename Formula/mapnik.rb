a030acf88da475dabf0a519d9f36aa00ceb20ac9
danielnachun <daniel.nachun@gmail.com>
2022-09-15 22:23:12 -0700

    mapnik: revision bump (boost 1.80.0)

--- Formula/mapnik.rb
+++ Formula/mapnik.rb
@@ -2,7 +2,7 @@ class Mapnik < Formula
   desc "Toolkit for developing mapping applications"
   homepage "https://mapnik.org/"
   license "LGPL-2.1-or-later"
-  revision 13
+  revision 14
   head "https://github.com/mapnik/mapnik.git", branch: "master"
 
   stable do
