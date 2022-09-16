8ba09819e61e28bf695448f2a3fefe59ffe156bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-16 04:02:29 +0000

    siril: update 1.0.5 bottle.

--- Formula/siril.rb
+++ Formula/siril.rb
@@ -7,13 +7,12 @@ class Siril < Formula
   head "https://gitlab.com/free-astro/siril.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 arm64_monterey: "d465b3231d1ddbab8f18950081fec346ddb73156c03a12e8f3fabb88a3e6d350"
-    sha256 arm64_big_sur:  "1455e68e9340e8f24f2fd24c4381a9979673d8a371cbdc27d3902128c0b24504"
-    sha256 monterey:       "e540a0e960b6d46a31ccca757d2b214e30fb37df4da6647fff4187f2d60130a2"
-    sha256 big_sur:        "ec230fac61723a6a7d7fa9d92ee4a6f710010c2e581bd97290fbcce62ae51ddb"
-    sha256 catalina:       "6883ffffb4f2efcfd497b428f475ca5fbf81107daeaf684058546f19b9e35757"
-    sha256 x86_64_linux:   "1f3a7e43e36830578db39bad3c06aa25cd59b6851755c48b9f8c2069306e7359"
+    sha256 arm64_monterey: "e32f148b276101d6841dfb8c211898a1d7272d4916a284da8c8183c835a1b88e"
+    sha256 arm64_big_sur:  "dab00cd2e285de9b6f3a64cb088d1e8f0aac71392b60b96e0f16a6c0bed310d6"
+    sha256 monterey:       "a6a7bbf11fb0ec6e0642ea75f6f199f8df3050df9877a1d39e8c04d88beba6f6"
+    sha256 big_sur:        "d4eb9b1bd38a4353e37974e732aee0d9ee5f09d4a46a84a4a557de692e5ac0b2"
+    sha256 catalina:       "f1173327fd55e2feb7d866af35677fc98ee54a142b3dad4c635f19545ecc7315"
+    sha256 x86_64_linux:   "8da1ab7b1d365d5177ae4be0cf2fcd0a631d5329fb539e2659b25539b3d39a85"
   end
 
   depends_on "autoconf" => :build
