add11d9e0c8f3275733d8cd882d51384f9bdc202
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-16 20:04:56 +0000

    autodiff: update 0.6.11 bottle.

--- Formula/autodiff.rb
+++ Formula/autodiff.rb
@@ -7,12 +7,13 @@ class Autodiff < Formula
   head "https://github.com/autodiff/autodiff.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "791f4bcc0c3443bea4e021975e796a995373c24c8324eee6d87c59a2c92b52b5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fe483c1c5f4e1e77869218dc0733774c69a468ac1d5a5b703bc8eb2c8350856a"
-    sha256 cellar: :any_skip_relocation, monterey:       "46e42cf85d0383662c1b1e147f8aeebd1ee7a355f62d74edfd673207f273e54f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "47ca8ddf8ce79fdc95104ddd74a2ff37473445c9d7ae3a77ff67248388c04cc1"
-    sha256 cellar: :any_skip_relocation, catalina:       "004781f0224d9bdcefcf720f2defc2d82f112338fde7229cc4f446c1108214a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5242b237a2bb5edd12d26569c7fceb9eaf3a73a48c88dc2fbe8003aa57d00632"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "406ead47f01c74683d3ad50e34e95400abc4958dcc2b7790bccba3125bfc3c3f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7a73ac5bc2db3d82213d448986b23e76de92f8d20db99cdbda50a02021d8ab5"
+    sha256 cellar: :any_skip_relocation, monterey:       "9be76087019acfe20b9ba2291de93af0919f1f1b352100137fe76cb0b30a0af4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a0a44913812ab1f56b57bb8ef06963e54b77cf07a73c834c973c45e4ed9eb51c"
+    sha256 cellar: :any_skip_relocation, catalina:       "dd8a303a162a586491296596e636404da223eb136201fdb4962ef8ef507ab478"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "349adde7dab336c96737265fdc4e3d8e93d4c3b25d2db321e360d0a455a69d68"
   end
 
   depends_on "cmake" => :build
