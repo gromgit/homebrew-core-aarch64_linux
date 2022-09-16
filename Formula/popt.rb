f4d0b23c22fe14d5bb8c4c3e847a6c43e4713b84
miccal <miccal.matthews@gmail.com>
2022-09-16 16:48:12 +0800

    popt 1.19

--- Formula/popt.rb
+++ Formula/popt.rb
@@ -1,9 +1,9 @@
 class Popt < Formula
   desc "Library like getopt(3) with a number of enhancements"
   homepage "https://github.com/rpm-software-management/popt"
-  url "http://ftp.rpm.org/popt/releases/popt-1.x/popt-1.18.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/rpm/popt/releases/popt-1.x/popt-1.18.tar.gz"
-  sha256 "5159bc03a20b28ce363aa96765f37df99ea4d8850b1ece17d1e6ad5c24fdc5d1"
+  url "http://ftp.rpm.org/popt/releases/popt-1.x/popt-1.19.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/rpm/popt/releases/popt-1.x/popt-1.19.tar.gz"
+  sha256 "c25a4838fc8e4c1c8aacb8bd620edb3084a3d63bf8987fdad3ca2758c63240f9"
   license "MIT"
 
   # The stable archive is found at https://ftp.osuosl.org/pub/rpm/popt/releases/popt-1.x/
