8560fd02e40d2866da74c52a7782b33cd81f488b
danielnachun <daniel.nachun@gmail.com>
2022-09-15 22:39:13 -0700

    sfcgal: revision bump (boost 1.80.0)

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -4,7 +4,7 @@ class Sfcgal < Formula
   url "https://gitlab.com/Oslandia/SFCGAL/-/archive/v1.4.1/SFCGAL-v1.4.1.tar.gz"
   sha256 "1800c8a26241588f11cddcf433049e9b9aea902e923414d2ecef33a3295626c3"
   license "LGPL-2.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
