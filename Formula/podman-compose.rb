a062bb31838e3301e6fdef7fd4f124f8488020e7
David Baumgold <david@davidbaumgold.com>
2022-09-16 12:06:46 +0200

    podman-compose: depend on pyyaml
    Closes #110888.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/podman-compose.rb
+++ Formula/podman-compose.rb
@@ -21,15 +21,11 @@ class PodmanCompose < Formula
   depends_on :macos
   depends_on "podman"
   depends_on "python@3.10"
+  depends_on "pyyaml"
 
   resource "python-dotenv" do
     url "https://files.pythonhosted.org/packages/02/ee/43e1c862a3e7259a1f264958eaea144f0a2fac9f175c1659c674c34ea506/python-dotenv-0.20.0.tar.gz"
     sha256 "b7e3b04a59693c42c36f9ab1cc2acc46fa5df8c78e178fc33a8d4cd05c8d498f"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   def install
