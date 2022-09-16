1844c0bf298da4726439d9afbf959b1d8d7ade91
Rui Chen <rui@chenrui.dev>
2022-09-16 09:30:19 -0400

    chroma 2.3.0
    Closes #110905.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chroma.rb
+++ Formula/chroma.rb
@@ -1,8 +1,8 @@
 class Chroma < Formula
   desc "General purpose syntax highlighter in pure Go"
   homepage "https://github.com/alecthomas/chroma"
-  url "https://github.com/alecthomas/chroma/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "6870cb8c47897f2a75abaf3e6cbb160989b4e915b86fdcd9322fb17e073ccd81"
+  url "https://github.com/alecthomas/chroma/archive/refs/tags/v2.3.0.tar.gz"
+  sha256 "1dc319a4b5f584858165900c94aa4cdef03f0f8e7a95fe3a3d0ab04bc0403c8c"
   license "MIT"
 
   bottle do
