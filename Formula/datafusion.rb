48018a3ab2c2bdda555da31326509da86b3c14f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-16 05:31:18 +0000

    datafusion: update 12.0.0 bottle.

--- Formula/datafusion.rb
+++ Formula/datafusion.rb
@@ -7,12 +7,12 @@ class Datafusion < Formula
   head "https://github.com/apache/arrow-datafusion.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8f6cdde4ebb85a208c570c04f3cd09345f8b2cd22df16c6eba3dba81ffad872a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "87a0da36c0793e42e0f0ee76a6d26cb3f37c400b6bb38b63d8fa5c6427ef2fcb"
-    sha256 cellar: :any_skip_relocation, monterey:       "0b548e62cfbed62b79948f231c1ee4a4b284caa76ada521d99981283f7d52f19"
-    sha256 cellar: :any_skip_relocation, big_sur:        "71c90b641121e2444400f33914e42a36e4243af0235e986fb1f0217159918580"
-    sha256 cellar: :any_skip_relocation, catalina:       "b2c1f7b6dd2406f206935d85a7d4cfda62969ee00163072c3618baf24cc3f494"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8507a2b794180fefd6b3284130aada79cd0e85a5a6dff119eb49c2ecb4faedc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "83680081c48190909c6a5f522bde19e3458d238a7de8f6bfc0a70e97f8e873cb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c9b9996f1d917cc33b7a03638313f8fd67a40893f7143b1d4e4bb5b9bdc341b"
+    sha256 cellar: :any_skip_relocation, monterey:       "8db2ae193c60e950e3139c0050edf875773273df2f6b30be86b0747911aac243"
+    sha256 cellar: :any_skip_relocation, big_sur:        "41019ff5b78255bf2110b4df6358461b58b84d414419566daf2c2c63405affce"
+    sha256 cellar: :any_skip_relocation, catalina:       "36842a858a6247a16201f2d04a7acb39d33c3fc8b40b1298be8ed4fffae032fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "270b0f9e921d33319e2200a42568a7d4f50fb9f5edc63e4227732da64c8e4e9b"
   end
 
   depends_on "rust" => :build
