1df899b6ee72a39721e7754ed09e3a63c4881771
asyncapi-bot-eve <bot+eve@asyncapi.io>
2022-09-16 16:20:28 +0200

    asyncapi 0.22.8
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #110912.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/asyncapi.rb
+++ Formula/asyncapi.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Asyncapi < Formula
   desc "All in one CLI for all AsyncAPI tools"
   homepage "https://github.com/asyncapi/cli"
-  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.22.7.tgz"
-  sha256 "0f183b18fb273bd331070d7c75117b21ba74c3672c3d9f4fde044560bbe95ab8"
+  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.22.8.tgz"
+  sha256 "058dd1a4634a7b845103d32b3aa0622fa884c1262b26f434a4fd9e9ac7a4c2fb"
   license "Apache-2.0"
 
   bottle do
