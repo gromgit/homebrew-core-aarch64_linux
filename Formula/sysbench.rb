589ed1b4f1fd61e6c8fdd63a4ec85ba24968fa6f
Alexander Bayandin <alexander@bayandin.dev>
2022-09-17 15:36:06 +0100

    sysbench: enable postgres support
    Closes #110988.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -4,7 +4,7 @@ class Sysbench < Formula
   url "https://github.com/akopytov/sysbench/archive/1.0.20.tar.gz"
   sha256 "e8ee79b1f399b2d167e6a90de52ccc90e52408f7ade1b9b7135727efe181347f"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
   head "https://github.com/akopytov/sysbench.git", branch: "master"
 
   bottle do
@@ -20,7 +20,8 @@ class Sysbench < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "luajit-openresty"
+  depends_on "libpq"
+  depends_on "luajit"
   depends_on "mysql-client"
   depends_on "openssl@1.1"
 
@@ -28,7 +29,7 @@ class Sysbench < Formula
 
   def install
     system "./autogen.sh"
-    system "./configure", *std_configure_args, "--with-mysql", "--with-system-luajit"
+    system "./configure", *std_configure_args, "--with-mysql", "--with-pgsql", "--with-system-luajit"
     system "make", "install"
   end
 
