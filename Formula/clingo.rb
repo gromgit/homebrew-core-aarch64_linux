897bee955ceb9458b580bc720cc8dfb027beaf0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-17 15:04:30 +0000

    clingo: update 5.6.1 bottle.

--- Formula/clingo.rb
+++ Formula/clingo.rb
@@ -11,12 +11,12 @@ class Clingo < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "516d1802c4d72a58f5a29333619d9368fe86be8271585c8a2538e5f4f7e1315c"
-    sha256 cellar: :any,                 arm64_big_sur:  "24a2afa23f80f6d606a79df6884979834596fa20d80b4c1d2c7d371e7b401a99"
-    sha256 cellar: :any,                 monterey:       "5a95cbc5c5feff84a3f273632901aa64bd7e9a0308d7d362be2841b85fc212b4"
-    sha256 cellar: :any,                 big_sur:        "0601e130693a5907c3202794c0ff054f2764b891e829be9c0e45b9748bd41e25"
-    sha256 cellar: :any,                 catalina:       "8e5a8a8cbcafbf382075fb9a1cad3c378e04f37ddcc93fb63a545b25936e5f05"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff669faa185e5071638026628e823ee24b647e72c21cdee41c00305e16bea8cf"
+    sha256 cellar: :any,                 arm64_monterey: "59a702866b71893332c09d463aee424dbce467efee30fbaff0affb284fe545ac"
+    sha256 cellar: :any,                 arm64_big_sur:  "4e7d7da17268f8c325dd5062b501d33020e7b697c4a5e3fc1c0c8a29477fe2c3"
+    sha256 cellar: :any,                 monterey:       "65022b7095c2240d1f1355abd673a0536dd6809c0b25c428a5c731900e8000f8"
+    sha256 cellar: :any,                 big_sur:        "fc01fe54d8d85abe30545ed1b77e2a85d4a304c33b588278ac4e145bb8f56a1d"
+    sha256 cellar: :any,                 catalina:       "49527938d130b822cc499e4488a2a96e8ba078b613a7181f607a4fcb3c876493"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aced8c24d4ac698ea55b4c81685dcdafd97ed832a893d4e9f11fd5cf42a89ac2"
   end
 
   head do
