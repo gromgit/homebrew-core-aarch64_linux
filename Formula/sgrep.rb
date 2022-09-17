88acf713d7c827d4b414c675243f7951f8a002c9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-17 13:51:39 +0000

    sgrep: update 1.94a bottle.

--- Formula/sgrep.rb
+++ Formula/sgrep.rb
@@ -16,12 +16,14 @@ class Sgrep < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, monterey:     "192436239783803b977e5804d989dc67f7760d87ed7831bb3c30dae5146204cf"
-    sha256 cellar: :any_skip_relocation, big_sur:      "fedcff86ec032617015882c5729298bbe1f1fcbda14cdde6167b00ae2af586b8"
-    sha256 cellar: :any_skip_relocation, catalina:     "29e528a52ae36131ded52bb08d9cf9b12b1455fbc715f7b7bbd3b97f637862e5"
-    sha256 cellar: :any_skip_relocation, mojave:       "bfb1f484dd474727fec463b1b90ffe7250f5c82e0e65bec96903e38f6e0a8e48"
-    sha256 cellar: :any_skip_relocation, high_sierra:  "a243589e79a4cde4f7bba21ec618e3c323c049589707bde6e2c20c4bf1014464"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4b9d7e430b35750659c93cfd9308b5cf32211eb19b79a8ee3bf0c0b62ef2712b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "213de82d5aff5377df50fbe7cedcaa3e099feb2ccee33c7f9dbc35ff4bfb8c7f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "775257fda7135fce998eafe41e1c39f32da44a2c5440dd205ff0a7801e6ef9f9"
+    sha256 cellar: :any_skip_relocation, monterey:       "192436239783803b977e5804d989dc67f7760d87ed7831bb3c30dae5146204cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fedcff86ec032617015882c5729298bbe1f1fcbda14cdde6167b00ae2af586b8"
+    sha256 cellar: :any_skip_relocation, catalina:       "29e528a52ae36131ded52bb08d9cf9b12b1455fbc715f7b7bbd3b97f637862e5"
+    sha256 cellar: :any_skip_relocation, mojave:         "bfb1f484dd474727fec463b1b90ffe7250f5c82e0e65bec96903e38f6e0a8e48"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "a243589e79a4cde4f7bba21ec618e3c323c049589707bde6e2c20c4bf1014464"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4b9d7e430b35750659c93cfd9308b5cf32211eb19b79a8ee3bf0c0b62ef2712b"
   end
 
   uses_from_macos "m4"
