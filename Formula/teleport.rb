7ee0837c686cd9b5de0ad89e444476a318a8c230
Rui Chen <rui@chenrui.dev>
2022-09-17 00:08:00 -0400

    teleport 10.2.2
    Closes #110957.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v10.2.1.tar.gz"
-  sha256 "c0b6de4a9d1b32be9c6f8af8a0c0cdb437bc72b5305bed1ee410dca9b42caa24"
+  url "https://github.com/gravitational/teleport/archive/v10.2.2.tar.gz"
+  sha256 "aaa475f8538cffe443e6138a54cd6eebec54444a48fc70c761579832518736c6"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
