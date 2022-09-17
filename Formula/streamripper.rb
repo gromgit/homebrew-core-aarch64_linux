a0f6a7359b39c17897bc4f587dbf0622d7f6fdb3
Michael Cho <cho-m@tuta.io>
2022-09-16 17:52:14 -0700

    streamripper: use `mad`
    Closes #110945.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/streamripper.rb
+++ Formula/streamripper.rb
@@ -3,7 +3,7 @@ class Streamripper < Formula
   homepage "https://streamripper.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/streamripper/streamripper%20%28current%29/1.64.6/streamripper-1.64.6.tar.gz"
   sha256 "c1d75f2e9c7b38fd4695be66eff4533395248132f3cc61f375196403c4d8de42"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
@@ -22,17 +22,19 @@ class Streamripper < Formula
 
   depends_on "pkg-config" => :build
   depends_on "glib"
+  depends_on "mad"
 
   def install
     # the Makefile ignores CPPFLAGS from the environment, which
     # breaks the build when HOMEBREW_PREFIX is not /usr/local
     ENV.append_to_cflags ENV.cppflags
 
+    # remove bundled libmad
+    (buildpath/"libmad-0.15.1b").rmtree
+
     chmod 0755, "./install-sh" # or "make install" fails
 
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
