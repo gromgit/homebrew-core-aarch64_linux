7c532451f7392dfaf067266c48fe432278004146
Michael Cho <cho-m@tuta.io>
2022-09-17 02:14:58 -0700

    acl2: revision bump (sbcl 2.2.8)

--- Formula/acl2.rb
+++ Formula/acl2.rb
@@ -4,6 +4,7 @@ class Acl2 < Formula
   url "https://github.com/acl2/acl2/archive/8.5.tar.gz"
   sha256 "dcc18ab0220027b90f30cd9e5a67d8f603ff0e5b26528f3aab75dc8d3d4ebc0f"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 arm64_monterey: "65f160edeb7343a5ea41ec0ab91fbb36902e5a02cd33dca6d0b6e29125c52e31"
