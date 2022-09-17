3378303c485501d11d16e1ea048e018532e2c161
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-17 14:04:22 +0000

    quicktype: update 15.1.0 bottle.

--- Formula/quicktype.rb
+++ Formula/quicktype.rb
@@ -10,12 +10,7 @@ class Quicktype < Formula
   head "https://github.com/quicktype/quicktype.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2fa5aaec1d5d1cb1809dd3ad614dda13af59447772cfa4526945e4fbedcd780e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b2a13d97e30ebcbff8e32903e82ad8857ed108ad84e695cb92df7e870c7948bd"
-    sha256 cellar: :any_skip_relocation, catalina:      "313dd23d76101d388259319a688d84a0cb5d44a16e7d34b4a0b7611048d995a4"
-    sha256 cellar: :any_skip_relocation, mojave:        "81f40f8f11e16c51c4e30881094256be0b9f91228e2ebf131d261c5ee6a8339c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "59a2b6555eebd5c338dee762e7685577bd1ea0e5db27686c139a9bdb393e9548"
-    sha256 cellar: :any_skip_relocation, all:           "2570eed33dcd2c39f8ef430e0a5ac3df428d5d74b9755b7064140ced95cc80c5"
+    sha256 cellar: :any_skip_relocation, all: "ef19f40d3d759a983be9a6ea044fed7e1953ebd8ad6be27d39cc6f21a4b1ca94"
   end
 
   depends_on "node"
