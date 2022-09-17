6b20b6618a80cd7c92ef6ef472706a3fcf2d51f3
David Baumgold <david@davidbaumgold.com>
2022-09-17 16:23:05 +0200

    mathlibtools: depend on pyyaml
    Closes #110986.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mathlibtools.rb
+++ Formula/mathlibtools.rb
@@ -18,6 +18,7 @@ class Mathlibtools < Formula
 
   depends_on "lean" => :test
   depends_on "python@3.10"
+  depends_on "pyyaml"
   depends_on "six"
 
   resource "atomicwrites" do
@@ -98,11 +99,6 @@ class Mathlibtools < Formula
   resource "pyparsing" do
     url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
     sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   resource "requests" do
