bf4c41c706e28f648fee913959c85fd063403a8d
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-09-18 21:40:40 +0000

    imgproxy: update 3.7.2 bottle.

--- Formula/imgproxy.rb
+++ Formula/imgproxy.rb
@@ -7,11 +7,12 @@ class Imgproxy < Formula
   head "https://github.com/imgproxy/imgproxy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "3d50f21bfb26856bd154051da17f28571dbb206c34b69aded19965f4c1efc3c3"
-    sha256 cellar: :any, arm64_big_sur:  "3954b34f8f8495e043aaaf93a04ea4cd009467df1df60efabf3a57730fc8b08a"
-    sha256 cellar: :any, monterey:       "18ae223c477bc7ae41ff7ef556ec97f6aae9f8020cc5117a7ad517e24c8b4d6f"
-    sha256 cellar: :any, big_sur:        "2b048778844f3944ec199162c6ab74110a5ec477c3cccee6e1407aa5ae69b55e"
-    sha256 cellar: :any, catalina:       "667b858dcd1e96b030615852934fa5221a422f700bfcb123c96a96dffc6c16ae"
+    sha256 cellar: :any,                 arm64_monterey: "3d50f21bfb26856bd154051da17f28571dbb206c34b69aded19965f4c1efc3c3"
+    sha256 cellar: :any,                 arm64_big_sur:  "3954b34f8f8495e043aaaf93a04ea4cd009467df1df60efabf3a57730fc8b08a"
+    sha256 cellar: :any,                 monterey:       "18ae223c477bc7ae41ff7ef556ec97f6aae9f8020cc5117a7ad517e24c8b4d6f"
+    sha256 cellar: :any,                 big_sur:        "2b048778844f3944ec199162c6ab74110a5ec477c3cccee6e1407aa5ae69b55e"
+    sha256 cellar: :any,                 catalina:       "667b858dcd1e96b030615852934fa5221a422f700bfcb123c96a96dffc6c16ae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20a7c5f70ba84e88337da2c719ceab64945329de77759794e7423c49b2b89187"
   end
 
   depends_on "go" => :build
