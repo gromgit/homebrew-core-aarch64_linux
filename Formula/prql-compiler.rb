0e79bed5b4170348ae9d76390bd0dd6d6eaf8872
Rui Chen <rui@chenrui.dev>
2022-09-17 20:11:28 -0400

    prql-compiler 0.2.7
    Closes #111015.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -1,8 +1,8 @@
 class PrqlCompiler < Formula
   desc "Simple, powerful, pipelined SQL replacement"
   homepage "https://prql-lang.org"
-  url "https://github.com/prql/prql/archive/refs/tags/0.2.6.tar.gz"
-  sha256 "4079d5f505250e6fe071215499b21e8ac7ecea23b55a16a4054bc5bf5707faf6"
+  url "https://github.com/prql/prql/archive/refs/tags/0.2.7.tar.gz"
+  sha256 "955dde62493e9e58e9b06d80e6592f08b97c0d1cc701c3a9dc868f02e2f2f369"
   license "Apache-2.0"
 
   bottle do
