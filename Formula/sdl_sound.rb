46236fa901bb69985dc3075f7a82a74e26d72cf2
Michael Cho <cho-m@tuta.io>
2022-09-18 21:18:56 -0700

    sdl_sound: migrate `sdl` to `sdl12-compat`

--- Formula/sdl_sound.rb
+++ Formula/sdl_sound.rb
@@ -4,7 +4,7 @@ class SdlSound < Formula
   url "https://icculus.org/SDL_sound/downloads/SDL_sound-1.0.3.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/s/sdl-sound1.2/sdl-sound1.2_1.0.3.orig.tar.gz"
   sha256 "3999fd0bbb485289a52be14b2f68b571cb84e380cc43387eadf778f64c79e6df"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "3d4f5a23c1903de8dd7af9d9c736213c0d9acc103b842b87bced96d4d1f271aa"
@@ -34,7 +34,7 @@ class SdlSound < Formula
   depends_on "pkg-config" => :build
   depends_on "libogg"
   depends_on "libvorbis"
-  depends_on "sdl"
+  depends_on "sdl12-compat"
 
   def install
     system "./autogen.sh" if build.head?
