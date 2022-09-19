91307ad16d1b351c7e974bab33424623b9ce1dde
Rui Chen <rui@chenrui.dev>
2022-09-19 10:57:57 -0400

    duckdb 0.5.1
    Closes #111131.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/duckdb.rb
+++ Formula/duckdb.rb
@@ -2,8 +2,8 @@ class Duckdb < Formula
   desc "Embeddable SQL OLAP Database Management System"
   homepage "https://www.duckdb.org"
   url "https://github.com/duckdb/duckdb.git",
-      tag:      "v0.5.0",
-      revision: "109f932c41fba9d61189e01ab0e5496cb9749506"
+      tag:      "v0.5.1",
+      revision: "7c111322de1095436350f95e33c5553b09302165"
   license "MIT"
 
   bottle do
