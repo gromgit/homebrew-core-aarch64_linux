a429e14780fb849f57dfe61be23e85f7f7769c30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-19 07:19:05 +0000

    chain-bench: update 0.1.4 bottle.

--- Formula/chain-bench.rb
+++ Formula/chain-bench.rb
@@ -7,12 +7,12 @@ class ChainBench < Formula
   head "https://github.com/aquasecurity/chain-bench.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "22006efe31216dd66d00a2ab5e17a82aa798bfc270cf7256722b8cd9b3bc9d6c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6be5250cb83710943d4f0966ebc6dc0e9cf635e12216ac855382131cc42dfe7b"
-    sha256 cellar: :any_skip_relocation, monterey:       "f65e5d9f17d95f41e406f86c83b8f1ad6fd47660b9971bd87644a97661a4cfc2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f063e6bc1448424b54a2ee644d72574704ece8de749cc0402a29e7de7cddef3c"
-    sha256 cellar: :any_skip_relocation, catalina:       "b3bcf78ab76e0069465e68d944facdacaae0b87d3f7cabf20ed7e644016d6012"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "532e867459a3a6c0e8ca3b049e426904461c9ced9da2e4f6545dd6eb1be39e24"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "466264c745d4f9c842c1903967a13c05075ca0e0f6e3b2b57a5c64738c06d640"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fcef25065fc2e4b5d58a5f2a2e3c1b3b63b6c1dd65205e6f002db13871ecb683"
+    sha256 cellar: :any_skip_relocation, monterey:       "6ebffa05566d36ff452e9039cb588cc27b078c027ff903d67ae1c4c934bfbcf0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5c5a6597863c975e0bc5980ce46388b2c6bc5e53025293a91dfab78c915b1786"
+    sha256 cellar: :any_skip_relocation, catalina:       "2b25624f1a3879b415135c7aae0150209a2fc6de45c5d345f785030d2d90be74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "77eb844b11cd70a260c6af93d1ba9acc743e8fee16c9f6384d612896e94c2df8"
   end
 
   depends_on "go" => :build
