9787339122efb9f2f27477cbf6fc90a3bc69429d
Rui Chen <rui@chenrui.dev>
2022-09-19 19:08:29 -0400

    libsoup 3.2.0
    Closes #111171.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libsoup.rb
+++ Formula/libsoup.rb
@@ -1,8 +1,8 @@
 class Libsoup < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://wiki.gnome.org/Projects/libsoup"
-  url "https://download.gnome.org/sources/libsoup/3.0/libsoup-3.0.8.tar.xz"
-  sha256 "c8739dc1c23c2b1e3b816d7598b3fa1764a3e1a2a2f5257b1bc4466d867caced"
+  url "https://download.gnome.org/sources/libsoup/3.2/libsoup-3.2.0.tar.xz"
+  sha256 "2832370698ca8f9fbf174c345b73d89b60561103a626c2df70726b0707f79bd3"
   license "LGPL-2.0-or-later"
 
   bottle do
