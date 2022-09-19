cd3f950ccd5850fa4ba4e484d6cf2659c0690632
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-20 00:21:02 +0800

    v8: add reminder to remove workaround.
    This is no longer needed after #106925.

--- Formula/v8.rb
+++ Formula/v8.rb
@@ -2,6 +2,7 @@ class V8 < Formula
   desc "Google's JavaScript engine"
   homepage "https://github.com/v8/v8/wiki"
   # Track V8 version from Chrome stable: https://omahaproxy.appspot.com
+  # TODO: Remove `ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib` at rebuild.
   url "https://github.com/v8/v8/archive/10.2.154.4.tar.gz"
   sha256 "6f4865ffe499f51da3e422cf7e4d85d3dab1b0a99b2d5bf204910ce423505597"
   license "BSD-3-Clause"
