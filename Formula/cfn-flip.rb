082a44e7c83c4bb3e5b735ae4481fa2ad514bfb7
David Baumgold <david@davidbaumgold.com>
2022-09-20 17:24:06 +0200

    cfn-flip: depend on pyyaml
    Closes #111225.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cfn-flip.rb
+++ Formula/cfn-flip.rb
@@ -18,16 +18,12 @@ class CfnFlip < Formula
   end
 
   depends_on "python@3.10"
+  depends_on "pyyaml"
   depends_on "six"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
     sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   def install
