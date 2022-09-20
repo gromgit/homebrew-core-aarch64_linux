a646864343734928641af9e36845ab5ee55bc9bd
Jeremy Long <jeremy.long@gmail.com>
2022-09-20 08:34:25 -0400

    dependency-check 7.2.1
    Closes #111207.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -1,8 +1,8 @@
 class DependencyCheck < Formula
   desc "OWASP dependency-check"
   homepage "https://owasp.org/www-project-dependency-check/"
-  url "https://github.com/jeremylong/DependencyCheck/releases/download/v7.2.0/dependency-check-7.2.0-release.zip"
-  sha256 "cd83f113739662a73bad9b18eeac82d882232b7777ce998d2e526ccdb7ba4e33"
+  url "https://github.com/jeremylong/DependencyCheck/releases/download/v7.2.1/dependency-check-7.2.1-release.zip"
+  sha256 "d1dd9092684a85c2958b60c4ad729d6a02972de39ca17f71f9cfec7fc7100451"
   license "Apache-2.0"
 
   livecheck do
