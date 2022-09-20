ea5e8abbfdca4124a1bbb8e722a20e5f408ff365
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-20 14:13:42 +0000

    selene: update 0.21.1 bottle.

--- Formula/selene.rb
+++ Formula/selene.rb
@@ -7,12 +7,12 @@ class Selene < Formula
   head "https://github.com/Kampfkarren/selene.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "54040c47bfe3bea2e48ce2b0fddd0ed3f70598d28a94e5e7725689cc745fdf7b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47fc0bf33032d556f5cca4b2e8cc5673ef3b177474b5e855e942e23626924e03"
-    sha256 cellar: :any_skip_relocation, monterey:       "45ca78e09185af543c09ecef61aae2a00784497f32999f29b3fcdf762baef6e5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "81f20004a994af5c74d2b06271dcd4b57421727076af2d1fbbd76e0698e40f58"
-    sha256 cellar: :any_skip_relocation, catalina:       "051bbee945f273b325df60fe650c6fc6b35eaeb8e6f9350f787b4c9a25256c2d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b545c8c9d667584d258fedf46fcb38409f248018265ed38d7395ecfb5c4103fb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "96928adb367a3a67a942daebad8329de4fba5c87af534fbf438842142c11e777"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d832d34a72d9f63fb37fefc411ade62acebf13024713e227b2c77662704dbfd1"
+    sha256 cellar: :any_skip_relocation, monterey:       "52f8b8dfaa3e5f5a36676846b7987904443f1346c9b09ea9f6957df79ef0147d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "69a0c9ada96537cf285fc36f036f72885994397bbce6ed484a511d642306ac13"
+    sha256 cellar: :any_skip_relocation, catalina:       "3d89f2e77c7532f34c027b1a2748310d0f614f954b3010cd2b74641b18110d71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "98958af9ff9fca6eb46b40d4d81e7a5ed83c63ed64bc81636cf5884ad10aabc1"
   end
 
   depends_on "rust" => :build
