9cc5e4fd1790ca14d9661a53cd37f2b7e4ffd85a
Porkepix <porkepix@gmail.com>
2022-09-21 00:59:32 +0200

    libfreenect 0.6.4
    Closes #111275.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libfreenect.rb
+++ Formula/libfreenect.rb
@@ -1,8 +1,8 @@
 class Libfreenect < Formula
   desc "Drivers and libraries for the Xbox Kinect device"
   homepage "https://openkinect.org/"
-  url "https://github.com/OpenKinect/libfreenect/archive/v0.6.2.tar.gz"
-  sha256 "e135f5e60ae290bf1aa403556211f0a62856a9e34f12f12400ec593620a36bfa"
+  url "https://github.com/OpenKinect/libfreenect/archive/v0.6.4.tar.gz"
+  sha256 "6169600f999729a7f99dd71a9825ed6e7aec8b8aac4f532ecf2911f76133c125"
   license any_of: ["Apache-2.0", "GPL-2.0-only"]
   head "https://github.com/OpenKinect/libfreenect.git", branch: "master"
 
