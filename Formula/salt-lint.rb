844ba2a92ad68d51df2cd6ac3e422096949491a3
David Baumgold <david@davidbaumgold.com>
2022-09-20 18:07:39 +0200

    salt-lint: depend on pyyaml
    Closes #111237.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/salt-lint.rb
+++ Formula/salt-lint.rb
@@ -17,15 +17,11 @@ class SaltLint < Formula
   end
 
   depends_on "python@3.10"
+  depends_on "pyyaml"
 
   resource "pathspec" do
     url "https://files.pythonhosted.org/packages/f6/33/436c5cb94e9f8902e59d1d544eb298b83c84b9ec37b5b769c5a0ad6edb19/pathspec-0.9.0.tar.gz"
     sha256 "e564499435a2673d586f6b2130bb5b95f04a3ba06f81b8f895b651a3c76aabb1"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   def install
