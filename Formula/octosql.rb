1f4d8a32e0023db68eb5a3f09b4f0dc84932e06a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-20 14:13:46 +0000

    octosql: update 0.9.4 bottle.

--- Formula/octosql.rb
+++ Formula/octosql.rb
@@ -7,12 +7,12 @@ class Octosql < Formula
   head "https://github.com/cube2222/octosql.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb01845a33431e8b0ccc3e6c7913341347d9116c2facb0473746f180702321b3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "635e89addfb8ad8712858ad53955c96920c1def582f5027b7b38bf3b15a2f862"
-    sha256 cellar: :any_skip_relocation, monterey:       "ee6cf273a562a9aa42d1b7dc06cf0c8d84628d7c6313aaae94aa20423417d244"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b8dfb66ee3b61cbca2e943d1a1fbf782bce619d82ddf61fc56b2877beee95eae"
-    sha256 cellar: :any_skip_relocation, catalina:       "9f5941ab3ce494840c1d8c5d5850ad3951be3ef2e3a3cae13c9c0e550027114a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a583135ce1753863e82e779b40446b4687559b20dbafbea6401d1c2e1605e50"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8f06ed08c993812025bbff7a882f3d56de9fc92c8f22df379410549c7dc623bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5a58e2557f45f531fc8c50aaad6692e58f52aac5716d5bb887c6a6fed16ee1d"
+    sha256 cellar: :any_skip_relocation, monterey:       "d9e33a95899cb8550ef08b8a2bdd460e693ae2a0db9b36bab5548e1e912d8c5a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2362edef5272b9a96223942ba3561f6e3c7d6c4c0623f3fc193611f79723b717"
+    sha256 cellar: :any_skip_relocation, catalina:       "b3371299be8e66c74cb4b9f74abe889890eb4d9a8ca0c7d57d567862bbf0313c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c5d273ecdb6a309f9596b4739e35a88cb58c40133e7b48b8b08b34c020ca3266"
   end
 
   depends_on "go" => :build
