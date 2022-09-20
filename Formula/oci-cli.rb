09c6246176568412817317a9146491704e15c4c7
Rui Chen <rui@chenrui.dev>
2022-09-20 10:09:25 -0400

    oci-cli 3.17.0
    Closes #111211.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oci-cli.rb
+++ Formula/oci-cli.rb
@@ -3,8 +3,8 @@ class OciCli < Formula
 
   desc "Oracle Cloud Infrastructure CLI"
   homepage "https://docs.cloud.oracle.com/iaas/Content/API/Concepts/cliconcepts.htm"
-  url "https://files.pythonhosted.org/packages/46/db/ded89d2c7291cdeac4eff7b10afe5b7bdb259f2f33327f939c73110eae68/oci-cli-3.16.1.tar.gz"
-  sha256 "609b3e7a3758ca592db21b7ec04b9237aa6974b5ce709999a040dabd2dee4ff3"
+  url "https://files.pythonhosted.org/packages/4c/e8/5cf64c45cf34750dae9f966048a5665c7a87b1d3034e2eeaab206f7c71fe/oci-cli-3.17.0.tar.gz"
+  sha256 "ad13e86d4062f069a5295cdd9c14ae37b005c0f2844643b90e189a2b38550299"
   license any_of: ["UPL-1.0", "Apache-2.0"]
   head "https://github.com/oracle/oci-cli.git", branch: "master"
 
@@ -27,8 +27,8 @@ class OciCli < Formula
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/90/c2/4e37394b66e7211ad120f216fc2e8b38d4f43b89c8100dd3917c9da9bfc6/certifi-2022.6.15.1.tar.gz"
-    sha256 "cffdcd380919da6137f76633531a5817e3a9f268575c128249fb637e4f9e73fb"
+    url "https://files.pythonhosted.org/packages/ca/48/88ec470f8b68319b6782ca3a0570789886ad5ca24c1af2f3771699135baa/certifi-2022.9.14.tar.gz"
+    sha256 "36973885b9542e6bd01dea287b2b4b3b21236307c56324fcc3f1160f2d655ed5"
   end
 
   resource "cffi" do
@@ -57,8 +57,8 @@ class OciCli < Formula
   end
 
   resource "oci" do
-    url "https://files.pythonhosted.org/packages/86/da/cb259b7b83ef1e006c90ebdfe16b6bbfc9f91d8648c43c35de989ed90aa5/oci-2.82.0.tar.gz"
-    sha256 "afaa9bfabe0e6270de550022b705c35fc5542c4c6b535dabfd6cc36cd0241d63"
+    url "https://files.pythonhosted.org/packages/75/f5/184a7ec334c36960be8eff385bb9ba83e0037d7852f4e723dc2f696e665d/oci-2.83.0.tar.gz"
+    sha256 "180d7aec253cacfc99fbe737dd3687e8183c7d9cbbf5420af0b3c2f9d9c37f6c"
   end
 
   resource "prompt-toolkit" do
