03329964951e8b4fcc7151d824ab8634f2eaf5a7
David Baumgold <david@davidbaumgold.com>
2022-09-20 17:41:40 +0200

    linode-cli: depend on pyyaml
    Closes #111232.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/linode-cli.rb
+++ Formula/linode-cli.rb
@@ -20,6 +20,7 @@ class LinodeCli < Formula
   depends_on "poetry" => :build # for terminaltables
   depends_on "openssl@1.1"
   depends_on "python@3.10"
+  depends_on "pyyaml"
 
   resource "linode-api-spec" do
     url "https://raw.githubusercontent.com/linode/linode-api-docs/refs/tags/v4.114.0/openapi.yaml"
@@ -39,11 +40,6 @@ class LinodeCli < Formula
   resource "idna" do
     url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
     sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   resource "requests" do
