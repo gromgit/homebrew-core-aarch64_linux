3946a6378e8d9c18ad7f365e79038e9c3282161a
darklore <zodiac.brave.story@gmail.com>
2022-09-21 13:11:38 +0900

    coursier: rebuild
    * coursier: add shell completions
    * Increment revision
    * Avoid permission error.
    * Update Formula/coursier.rb
    
    Closes #111320.
    
    Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>
    Co-authored-by: darklore <958690+darklore@users.noreply.github.com>
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/coursier.rb
+++ Formula/coursier.rb
@@ -21,6 +21,11 @@ class Coursier < Formula
     (libexec/"bin").install "coursier.jar"
     chmod 0755, libexec/"bin/coursier.jar"
     (bin/"coursier").write_env_script libexec/"bin/coursier.jar", Language::Java.overridable_java_home_env
+
+    generate_completions_from_executable("bash", "#{bin}/coursier", "completions", "bash",
+                                         shell_parameter_format: :none, shells: [:bash])
+    generate_completions_from_executable("bash", "#{bin}/coursier", "completions", "zsh",
+                                         shell_parameter_format: :none, shells: [:zsh])
   end
 
   test do
