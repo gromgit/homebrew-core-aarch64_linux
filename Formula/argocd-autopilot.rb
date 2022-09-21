eeacbbd7180fcf28bab6caf5745b3a9ba065b40d
Adrian Ho <aho+rpi9@03s.net>
2022-09-21 17:59:41 +0800

    argocd-autopilot: no rebuilding required

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -2,17 +2,13 @@ class ArgocdAutopilot < Formula
   desc "Opinionated way of installing Argo CD and managing GitOps repositories"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj-labs/argocd-autopilot.git",
-      tag:      "v0.4.6",
-      revision: "534f514aedf439157aa1079e92b74ac6c0fb3991"
+      tag:      "v0.3.6",
+      revision: "52ac98b00e970b30343df8694fd119274a88c773"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3692961d1c2e694299dd1036755bd170d71d292bef35b81ac10ce49d78b87364"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8de7be4965af377d7f0b9a69617cb932958273827d1409788348e8120e64739d"
-    sha256 cellar: :any_skip_relocation, monterey:       "68145fbdd45527313a5dfd4a1f5f6c626a6a408e6be2f8f454774d2b6d762044"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f7f8b5a915bb339ef51f8f376b455ce74bd51a7386736943c8f237bbecff9e8e"
-    sha256 cellar: :any_skip_relocation, catalina:       "e03859725edcc51e55e52f105362ab3f488cf795e0aa01c675f010841a30adf7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "088f820af0e6c774e5bc9fed4145c75828d22a335284a7a17ab9a8717f79fcac"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/argocd-autopilot"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7672141d98fcec379fb241a9ca69172f589f717e747ff1a44370644870fd15f6"
   end
 
   depends_on "go" => :build
