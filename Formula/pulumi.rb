f52b9a7af26f762b1336ea730e6cb0294cbb0a0d
Adrian Ho <aho+rpi9@03s.net>
2022-09-21 18:01:55 +0800

    pulumi: no rebuilding required

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -2,18 +2,14 @@ class Pulumi < Formula
   desc "Cloud native development platform"
   homepage "https://pulumi.io/"
   url "https://github.com/pulumi/pulumi.git",
-      tag:      "v3.39.3",
-      revision: "f677abf06be5a358f1e7204ab9d6c5de54a9f8e1"
+      tag:      "v3.32.1",
+      revision: "fb8070829f9965455f351acb9bb52f57492518c2"
   license "Apache-2.0"
   head "https://github.com/pulumi/pulumi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c990fe5e0faa367ddf8b9bf7f511c0991b434b460559ac7b0c477a0195b874b2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1fb8c8104126db9b90b4badf58dacae019ae55af9368bcbc1daff34621d7485"
-    sha256 cellar: :any_skip_relocation, monterey:       "0096399fc655a8a1c2c8c95f023853a8a2a0a3a57f9947e10c001bb939d7f42f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "feae2e1d6868d1d5d185971ed5f24f3c494febdf5efc7edfcacb1b179d594077"
-    sha256 cellar: :any_skip_relocation, catalina:       "e701df90c45fe665975e3a2d53e030c205a753391d53e167275ee502243e5130"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f7af002b65ead8734647010ac53a07b3a476569124a514311b38c4a38c7401d2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pulumi"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "358442fbe9e01220641448dada3e3c9e9b5c0672f2c1b3270b494de16fc8ce7b"
   end
 
   depends_on "go" => :build
