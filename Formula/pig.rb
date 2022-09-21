cdbb3dd534adf46a723e821d04435428685b92c8
Michael Cho <cho-m@tuta.io>
2022-09-21 03:10:25 -0700

    pig: use `openjdk@17`
    Closes #111338.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pig.rb
+++ Formula/pig.rb
@@ -5,7 +5,7 @@ class Pig < Formula
   mirror "https://archive.apache.org/dist/pig/pig-0.17.0/pig-0.17.0.tar.gz"
   sha256 "6d613768e9a6435ae8fa758f8eef4bd4f9d7f336a209bba3cd89b843387897f3"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "05975bde330ef940fa753ee188f16b3a4136e22e05cb98d2aa0f566c0db08cda"
@@ -16,23 +16,14 @@ class Pig < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "12c02619e8fbfee0603e8d11ffa0facfed80717df4eae3f4176a9fe5b33a4076"
   end
 
-  on_arm do
-    depends_on "openjdk@11"
-  end
-  on_intel do
-    depends_on "openjdk"
-  end
+  depends_on "openjdk@17"
 
   def install
     (libexec/"bin").install "bin/pig"
     libexec.install Dir["pig-#{version}-core-h*.jar"]
     libexec.install "lib"
 
-    env = if Hardware::CPU.arm?
-      Language::Java.overridable_java_home_env("11")
-    else
-      Language::Java.overridable_java_home_env
-    end
+    env = Language::Java.overridable_java_home_env("17")
     env["PIG_HOME"] = libexec
     (bin/"pig").write_env_script libexec/"bin/pig", env
   end
