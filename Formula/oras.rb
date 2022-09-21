d235fd562da46dced1239bf702938a10e027d09b
Adrian Ho <the.gromgit@gmail.com>
2022-09-21 23:38:19 +0800

    oras: update 0.14.1 bottle.

--- Formula/oras.rb
+++ Formula/oras.rb
@@ -1,13 +1,13 @@
 class Oras < Formula
   desc "OCI Registry As Storage"
   homepage "https://github.com/oras-project/oras"
-  url "https://github.com/oras-project/oras/archive/v0.12.0.tar.gz"
-  sha256 "5e19d61683a57b414efd75bd1b0290c941b8faace5fcc9d488f5e4aa674bf03e"
-  license "MIT"
+  url "https://github.com/oras-project/oras/archive/v0.14.1.tar.gz"
+  sha256 "29e934ef24092209c488a411a89af80cfcd93a4c880a36192e91085a58f0c2f7"
+  license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/oras"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "6e18a3c8a826caa3970db3de2f5c03ff53bbd3eeda36615af73b485a18a4aa21"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4f46d349575052a44f50330bbbe25c7c3d4b39acfa93a69614e41350f3eb9c2d"
   end
 
   depends_on "go" => :build
@@ -15,10 +15,10 @@ class Oras < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/oras-project/oras/internal/version.Version=#{version}
-      -X github.com/oras-project/oras/internal/version.BuildMetadata=Homebrew
+      -X oras.land/oras/internal/version.Version=#{version}
+      -X oras.land/oras/internal/version.BuildMetadata=Homebrew
     ]
-    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/oras"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/oras"
   end
 
   test do
@@ -32,16 +32,12 @@ class Oras < Formula
       }
     EOS
     (testpath/"test.json").write(contents)
-    hash = Digest::SHA256.hexdigest(contents)
 
     # Although it might not make much sense passing the JSON as both manifest and payload,
     # it helps make the test consistent as the error can randomly switch between either hash
     output = shell_output("oras push localhost:#{port}/test-artifact:v1 " \
-                          "--manifest-config test.json:application/vnd.homebrew.test.config.v1+json " \
+                          "--config test.json:application/vnd.homebrew.test.config.v1+json " \
                           "./test.json 2>&1", 1)
-    assert_match %r{
-       ^Error:\ failed\ to\ do\ request(.*)
-       http://localhost:#{port}/v2/test-artifact/blobs/sha256:#{hash}
-    }x, output
+    assert_match "#{port}: connect: connection refused", output
   end
 end
