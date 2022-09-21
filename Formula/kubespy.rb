b9707c18b6dd41420c43d7593f55629c0786c436
Adrian Ho <aho+rpi9@03s.net>
2022-09-21 18:01:13 +0800

    kubespy: no rebuilding required

--- Formula/kubespy.rb
+++ Formula/kubespy.rb
@@ -1,20 +1,17 @@
 class Kubespy < Formula
   desc "Tools for observing Kubernetes resources in realtime"
   homepage "https://github.com/pulumi/kubespy"
-  url "https://github.com/pulumi/kubespy/archive/v0.6.1.tar.gz"
-  sha256 "431f4b54ac3cc890cd3ddd0c83d4e8ae8a36cf036dfb6950b76577a68b6d2157"
+  url "https://github.com/pulumi/kubespy/archive/v0.6.0.tar.gz"
+  sha256 "ff8f54a2a495d8ebb57242989238a96c2c07d26601c382a25419498170fc3351"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6cc273c106fc5bfcd196266862c0d6d2b7c41dddc193a6af3f04b4a57eafc3da"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4b636df3c960c6070f39ed27d5150f49b138ae270577fcc3281b6bd730463c61"
-    sha256 cellar: :any_skip_relocation, monterey:       "32f272603fb848cca0a022a6e9eaadea61369cc04de7726c46cca9adc9a98d3c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0e2501ef6eb8341e9cc420da16061c3527aa062b5e7150cfa8fd16249eb168ea"
-    sha256 cellar: :any_skip_relocation, catalina:       "277ef57273c272caf8a8a3a7028d850daf6d3499b23ebbcd53a6b5e535c3d205"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd7e54744796814089311a48ad6210bf82c65236058558f4c93f4896fe63c049"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/kubespy"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "434f59254f28cff7f9cf13fba0435eacf8051634760cd3e2b2931984b9d1840e"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-X github.com/pulumi/kubespy/version.Version=#{version}")
@@ -23,7 +20,6 @@ class Kubespy < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/kubespy version")
 
-    assert_match "invalid configuration: no configuration has been provided",
-                 shell_output("#{bin}/kubespy status v1 Pod nginx 2>&1", 1)
+    assert_match "Unable to read kubectl config", shell_output("#{bin}/kubespy status v1 Pod nginx 2>&1", 1)
   end
 end
