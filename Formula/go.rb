f95a051b86748300c264e9bcba1c2e3488024be4
Adrian Ho <the.gromgit@gmail.com>
2022-09-21 23:30:44 +0800

    go: update 1.19 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.18.1.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.18.1.src.tar.gz"
-  sha256 "efd43e0f1402e083b73a03d444b7b6576bb4c539ac46208b63a916b69aca4088"
+  url "https://go.dev/dl/go1.19.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.19.src.tar.gz"
+  sha256 "9419cc70dc5a2523f29a77053cafff658ed21ef3561d9b6b020280ebceab28b9"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
 
@@ -13,35 +13,35 @@ class Go < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/go-1.18.1"
-    sha256 aarch64_linux: "c56ec10f59bdac98b4266fee04795fea5fd90b7bd490db589505bc5f5d16b2f9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/go"
+    sha256 aarch64_linux: "189254a8510958d770a6f6c7f1c8d2339b40a6785594bb421613080b2f993676"
   end
 
   # Don't update this unless this version cannot bootstrap the new version.
   resource "gobootstrap" do
-    on_macos do
-      if Hardware::CPU.arm?
-        url "https://storage.googleapis.com/golang/go1.16.darwin-arm64.tar.gz"
-        version "1.16"
-        sha256 "4dac57c00168d30bbd02d95131d5de9ca88e04f2c5a29a404576f30ae9b54810"
-      else
-        url "https://storage.googleapis.com/golang/go1.16.darwin-amd64.tar.gz"
-        version "1.16"
-        sha256 "6000a9522975d116bf76044967d7e69e04e982e9625330d9a539a8b45395f9a8"
-      end
+    checksums = {
+      "darwin-arm64" => "4dac57c00168d30bbd02d95131d5de9ca88e04f2c5a29a404576f30ae9b54810",
+      "darwin-amd64" => "6000a9522975d116bf76044967d7e69e04e982e9625330d9a539a8b45395f9a8",
+      "linux-arm64"  => "3770f7eb22d05e25fbee8fb53c2a4e897da043eb83c69b9a14f8d98562cd8098",
+      "linux-amd64"  => "013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2",
+    }
+
+    arch = "arm64"
+    platform = "darwin"
+
+    on_intel do
+      arch = "amd64"
     end
 
     on_linux do
-      if Hardware::CPU.arm?
-        url "https://storage.googleapis.com/golang/go1.16.linux-arm64.tar.gz"
-        version "1.16"
-        sha256 "3770f7eb22d05e25fbee8fb53c2a4e897da043eb83c69b9a14f8d98562cd8098"
-      else
-        url "https://storage.googleapis.com/golang/go1.16.linux-amd64.tar.gz"
-        version "1.16"
-        sha256 "013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2"
-      end
+      platform = "linux"
     end
+
+    boot_version = "1.16"
+
+    url "https://storage.googleapis.com/golang/go#{boot_version}.#{platform}-#{arch}.tar.gz"
+    version boot_version
+    sha256 checksums["#{platform}-#{arch}"]
   end
 
   def install
@@ -63,6 +63,7 @@ class Go < Formula
     # Remove useless files.
     # Breaks patchelf because folder contains weird debug/test files
     (libexec/"src/debug/elf/testdata").rmtree
+    (libexec/"src/debug/dwarf/testdata").rmtree if OS.linux? && Hardware::CPU.arm?
     # Binaries built for an incompatible architecture
     (libexec/"src/runtime/pprof/testdata").rmtree
   end
