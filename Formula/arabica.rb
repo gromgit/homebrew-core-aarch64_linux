cddce07f3e3bd7fce83d818ee67519c7c0161bcd
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 06:37:04 +0800

    arabica: update 20200425 bottle.

--- Formula/arabica.rb
+++ Formula/arabica.rb
@@ -18,14 +18,8 @@ class Arabica < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3e92d822c2e0c5d314a92e5e26df14b3a84774494fb100f401c3a2d0c7e54768"
-    sha256 cellar: :any,                 arm64_big_sur:  "6875acb418a0c10026c5356fe927a7c91a1825d8b314599ee1a64a309f30ed77"
-    sha256 cellar: :any,                 monterey:       "db7acb62fe52ebc6b315b9e1e94cbf5ead317e7856af95efa8d5eeb0a41f62bf"
-    sha256 cellar: :any,                 big_sur:        "c1a63f10d7451ba663ad8d974a69d83091be30730ca962a2fbd0e36b95ab16d2"
-    sha256 cellar: :any,                 catalina:       "4fbf676c46941de213b095ab74f0b4973e5984c2bbaa7679757b0db4b369480a"
-    sha256 cellar: :any,                 mojave:         "acc299016dbd644658880e9fa29af6d3f0b9f8e226b16ccd3fcaea8dae23febf"
-    sha256 cellar: :any,                 high_sierra:    "62920d4f26c2da71c6abf60c90c1322457e340df8142d7133a9ee1f7c2b46745"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "129967d8e801a766a2d8209dff39cc8358bff641249838682ac1a943d0b7d385"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/arabica"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b1b95252db60067363ca4b9da1c6cf81e4a2444c434dbf8a5eff9331f2adc4cf"
   end
 
   depends_on "autoconf" => :build
