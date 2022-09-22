6e64150fa81a97a8a7269fa3eba762c199687393
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 12:01:20 +0800

    aarch64-elf-gcc: update 12.2.0 bottle.

--- Formula/aarch64-elf-gcc.rb
+++ Formula/aarch64-elf-gcc.rb
@@ -1,9 +1,9 @@
 class Aarch64ElfGcc < Formula
   desc "GNU compiler collection for aarch64-elf"
   homepage "https://gcc.gnu.org"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz"
-  sha256 "62fd634889f31c02b64af2c468f064b47ad1ca78411c45abe6ac4b5f8dd19c7b"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
+  sha256 "e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   livecheck do
@@ -11,12 +11,8 @@ class Aarch64ElfGcc < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e3e61b1ddba97ee4156243b6e4a75d2e7ab7772d7427f136d2db7f65e91c5429"
-    sha256 arm64_big_sur:  "e6f7eaabc4d43eed86b0a0b737c2bf0e922a608686ddc1d033b872bba4f766f1"
-    sha256 monterey:       "cea22bcae56ca4ab90f52d5cee01c68362a5a59cfe9e82e1e1003f5912346b72"
-    sha256 big_sur:        "db306ab8ee3a4fab34538e632a83e8adc6e71cf714a6b5f047d4d20bed4b331b"
-    sha256 catalina:       "ab2276b06f5740bdfc54f98a0469f94b9135456deaf785cf7318f622d35746e3"
-    sha256 x86_64_linux:   "0a6b4a81b2d56f4b10e82735c29a20787f9d45d93d5425031ab4f089adab2008"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aarch64-elf-gcc"
+    sha256 aarch64_linux: "da2b21262d1b5cc136358a3dac01c1f1ccd3c4418c80319c6d012462b20bc025"
   end
 
   depends_on "aarch64-elf-binutils"
