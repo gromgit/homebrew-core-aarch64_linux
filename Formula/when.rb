95f9016dc619c382429a6e9bb6b9b86c84f553a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-22 06:00:55 +0000

    when: update 1.1.44 bottle.

--- Formula/when.rb
+++ Formula/when.rb
@@ -7,12 +7,12 @@ class When < Formula
   head "https://github.com/bcrowell/when.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, monterey:       "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "132c7a507ca3f7e5984eb87f804a69a93d01501327d64ce9866a57ee23a8af6e"
-    sha256 cellar: :any_skip_relocation, catalina:       "2f9df2c93da3424c51eb901c46f847a99ba1f4c025a2759bd36c467ed9e0eeaf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c6f757cbf95f81a1f836702132f50e5c782f9be02069f4df3ce4e93a5012dd0c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "005cc29f69bc43959e1702e482c33b6894bc9eec2078a96687dfd09c258fa18e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "005cc29f69bc43959e1702e482c33b6894bc9eec2078a96687dfd09c258fa18e"
+    sha256 cellar: :any_skip_relocation, monterey:       "005cc29f69bc43959e1702e482c33b6894bc9eec2078a96687dfd09c258fa18e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "005cc29f69bc43959e1702e482c33b6894bc9eec2078a96687dfd09c258fa18e"
+    sha256 cellar: :any_skip_relocation, catalina:       "6cddc86c59d8cdf0ba79ecc974dd57705c34656891fe402b920baa07d801685c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a8266c047098f0549959cb83da63bac863f1d9c0c4cd57dff48e2c560255d69"
   end
 
   def install
