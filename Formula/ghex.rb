6fc69c2ea86d4ffa6aa5de4db6c68d04576dee76
Michael Cho <cho-m@tuta.io>
2022-09-21 23:05:54 -0700

    ghex 42.3
    Closes #111417.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ghex.rb
+++ Formula/ghex.rb
@@ -1,9 +1,8 @@
 class Ghex < Formula
   desc "GNOME hex editor"
   homepage "https://wiki.gnome.org/Apps/Ghex"
-  url "https://download.gnome.org/sources/ghex/3.18/ghex-3.18.4.tar.xz"
-  sha256 "c2d9c191ff5bce836618779865bee4059db81a3a0dff38bda3cc7a9e729637c0"
-  revision 3
+  url "https://download.gnome.org/sources/ghex/42/ghex-42.3.tar.xz"
+  sha256 "add40f8ab24921db30d27be58f00273201977d87fdc8d79eceadfa8b0e354def"
 
   bottle do
     sha256 arm64_monterey: "292f84d1b19188dcb9b6ad7e8c812e1b4bb0189fbf377009118905daa4db7017"
@@ -16,59 +15,32 @@ class Ghex < Formula
     sha256 x86_64_linux:   "162e20b386fe920b63142876b0e0100a471d69b9737c516a9c30ed04b27d5801"
   end
 
+  depends_on "desktop-file-utils" => :build
   depends_on "itstool" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "gtk+3"
+  depends_on "gtk4"
   depends_on "hicolor-icon-theme"
-
-  # submitted upstream as https://gitlab.gnome.org/GNOME/ghex/merge_requests/8
-  patch :DATA
 
   def install
     # ensure that we don't run the meson post install script
     ENV["DESTDIR"] = "/"
 
-    mkdir "build" do
-      system "meson", *std_meson_args, ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-    end
+    system "meson", *std_meson_args, "build", "-Dmmap-buffer-backend=#{OS.linux?}"
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   def post_install
     system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
-    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    system "#{Formula["gtk4"].opt_bin}/gtk4-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
   end
 
   test do
-    system "#{bin}/ghex", "--help"
+    # (process:30744): Gtk-WARNING **: 06:38:39.728: cannot open display
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    system bin/"ghex", "--help"
   end
 end
-
-__END__
-diff --git a/src/meson.build b/src/meson.build
-index fdcdcc2..ac45c93 100644
---- a/src/meson.build
-+++ b/src/meson.build
-@@ -23,9 +23,9 @@ libghex_c_args = [
-   '-DG_LOG_DOMAIN="libgtkhex-3"'
- ]
-
--libghex_link_args = [
-+libghex_link_args = cc.get_supported_link_arguments([
-   '-Wl,--no-undefined'
--]
-+])
-
- install_headers(
-   libghex_headers,
-@@ -36,6 +36,7 @@ libghex = library(
-   'gtkhex-@0@'.format(libghex_version_major),
-   libghex_sources + libghex_headers,
-   version: '0.0.0',
-+  darwin_versions: ['1', '1.0'],
-   include_directories: ghex_root_dir,
-   dependencies: libghex_deps,
-   c_args: libghex_c_args,
