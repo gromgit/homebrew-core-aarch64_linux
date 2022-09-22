ce05b86138661625f55a9ba1cbe53eb808959edf
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 13:27:05 +0800

    abseil: update 20220623.1 bottle.

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -1,18 +1,14 @@
 class Abseil < Formula
   desc "C++ Common Libraries"
   homepage "https://abseil.io"
-  url "https://github.com/abseil/abseil-cpp/archive/refs/tags/20211102.0.tar.gz"
-  sha256 "dcf71b9cba8dc0ca9940c4b316a0c796be8fab42b070bb6b7cab62b48f0e66c4"
+  url "https://github.com/abseil/abseil-cpp/archive/refs/tags/20220623.1.tar.gz"
+  sha256 "91ac87d30cc6d79f9ab974c51874a704de9c2647c40f6932597329a282217ba8"
   license "Apache-2.0"
   head "https://github.com/abseil/abseil-cpp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0c19e80720547d2ef348842242568e27bbc662f4be6b4dc4b40a7477645b9dbf"
-    sha256 cellar: :any,                 arm64_big_sur:  "23ad9b293aad20112786ec85f80a54781edb42081277e18bbda61752965b1301"
-    sha256 cellar: :any,                 monterey:       "a890728e03ba9359d371d55430ca1b86e4b7c28420179b6d05ae4fed2134b966"
-    sha256 cellar: :any,                 big_sur:        "c707d13cb25a75e52333bcbd57e17f522e09e5f073302ce904d61ec026ccd708"
-    sha256 cellar: :any,                 catalina:       "9a2c8277e6d4b348914220580486bf707b2e09d70f6457a60496cc4254426875"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "513087011325dd2d8a006ba7d12c07e6077dc9048fed2d5e36d20c0905681c81"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/abseil"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ee25405e8e2dc018c717cd76bb3e393b6d4712c230cc2b927ae0a53258eab180"
   end
 
   depends_on "cmake" => :build
