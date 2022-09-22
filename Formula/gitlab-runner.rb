01c2abd8922ebb2abcdcfd66d59b39d81410eb28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-22 05:37:52 +0000

    gitlab-runner 15.4.0
    Closes #111408.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -2,8 +2,8 @@ class GitlabRunner < Formula
   desc "Official GitLab CI runner"
   homepage "https://gitlab.com/gitlab-org/gitlab-runner"
   url "https://gitlab.com/gitlab-org/gitlab-runner.git",
-      tag:      "v15.3.0",
-      revision: "bbcb5aba76d966f60c76629301d0e2cac4ee09b7"
+      tag:      "v15.4.0",
+      revision: "43b2dc3d83ed12442f8a106ceef1be993797f355"
   license "MIT"
   head "https://gitlab.com/gitlab-org/gitlab-runner.git", branch: "main"
 
