bf633b1d3c1ec57c6ac49af609b40498d466ca92
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 12:34:43 +0800

    vorbis-tools: update 1.4.2 bottle.

--- Formula/vorbis-tools.rb
+++ Formula/vorbis-tools.rb
@@ -11,14 +11,8 @@ class VorbisTools < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "84a6be2cfa26af111e5e557792030f954ee027cb8375fdc8bcf4ab219b21668e"
-    sha256 cellar: :any,                 arm64_big_sur:  "04a17ea863d62a4ea5839e6a2c2a86314d06ed6d13883a62b19745994d745317"
-    sha256 cellar: :any,                 monterey:       "ea85c845fa0f2ef26128624c219fc748448ca45aaf4c1998dc62943b7d089624"
-    sha256 cellar: :any,                 big_sur:        "96ba6cde73391d6a892e9bf2dc858264450481fd82abf7b5b18d51e50f925337"
-    sha256 cellar: :any,                 catalina:       "b01d184e0950457c156be75a93e58f33d3e68fc1c563d11c3d4201bd91835f1e"
-    sha256 cellar: :any,                 mojave:         "a9564d93b704d270a94dc5831f20a1033a18f4ad8f7c90270eeeb1c65abcd578"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a6e13cfa641da45da07d912c1b6dcacf9e3603ea2152266605ad212188183259"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vorbis-tools"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "87048da5512101813f98e00420a84f4f044fd5380b6e919f73ab5d4d121926e9"
   end
 
   depends_on "pkg-config" => :build
