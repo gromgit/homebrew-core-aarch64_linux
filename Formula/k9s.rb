3282efa56cc2a446de0e283f526e684a05be4b15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-22 21:04:29 +0000

    k9s: update 0.26.5 bottle.

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -8,12 +8,12 @@ class K9s < Formula
   head "https://github.com/derailed/k9s.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4ec2e9e2a43a66b4bb40edcd6c09a03a7e74c1c1917f8e84e7efa2aa038a4027"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c241e1954a515692265e178ed505a7403ad7e420baf0cdd3867c2643c2746643"
-    sha256 cellar: :any_skip_relocation, monterey:       "facb0994e6e532599a57a6dc2a38ff09d5c6f359cf6b1bbdb6e1372b19f76a99"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c218720dadeaef28d1dee43b3ec21fa99c88e6e41344ef769251969dd3df4520"
-    sha256 cellar: :any_skip_relocation, catalina:       "2c85cab4766018eb0ff301a4c1ede711b254187c03cb2c3b2bbf410d6494e29f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e0675e4568f2562cf857c9cbea965bd6931c473405024051ca7b2e04a4a9ed6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0f5c6886fc96c0844b1ce214e1841a0fa16bf853b5260e4afbbd8d8cb3bcc316"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f783b8eb009d5a8b0580fb6dcc16dfcd0f0f723910de208e3a167487783ff95"
+    sha256 cellar: :any_skip_relocation, monterey:       "af8b3d235793a1cc7307dde63726801d0a48557817bd7775728ee03e077fb9bf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4793008d982a0fb704c748d1b342ab2ced8bb4f1bf93fbcbfcc8144f987fcbba"
+    sha256 cellar: :any_skip_relocation, catalina:       "af36395465d0d4038feded022d06f0218c08e93a37ac2cf8cf8f9860de6d9ef5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5f72250766271528001344a74e04325670dc76c8e164a1a3335c1283ec15d7a"
   end
 
   depends_on "go" => :build
