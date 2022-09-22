596cf33d555c240f5293195db07b8976472371d3
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-09-22 08:51:57 +0000

    quasi88: update 0.6.4_1 bottle.

--- Formula/quasi88.rb
+++ Formula/quasi88.rb
@@ -11,16 +11,12 @@ class Quasi88 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "86fa89cfefe5c7cc7359dc6a0c4dad83c17810a660c19f165d160574103f0144"
-    sha256 cellar: :any,                 arm64_big_sur:  "d62e6a88ff70815e139f210668f4e8e433ba5c00124f1ec1464c71d29afd6fb0"
-    sha256 cellar: :any,                 monterey:       "de876cce41def50514c8240deb59104ae3c7dff82ad59ca164606b535592b414"
-    sha256 cellar: :any,                 big_sur:        "0666b5a2b84dede66965c0085d397c05bbd44f09338076c7860baa3790eb84ef"
-    sha256 cellar: :any,                 catalina:       "2a1d1f01c210c06e49f3091dcebb2a30e62e14596e23bc43f349e151e3771d09"
-    sha256 cellar: :any,                 mojave:         "8b16ac77e4b8c6481fb7f518d5f7f446ff3b8b39465fa99d7bcbb8b28a3c745f"
-    sha256 cellar: :any,                 high_sierra:    "8199a69a8ecad4247752091f3eeaf5181eaa1dd0e4b2670059e21df807c646c6"
-    sha256 cellar: :any,                 sierra:         "d9ff4c5657c4179371d60317e0455cbadd59d45d81d0cc71d62d14d681619e95"
-    sha256 cellar: :any,                 el_capitan:     "4bef6f92d4fcdf3547e0e7b9d699f392de0ff4764bbed0d8b23ea37e22e33f78"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "96d4f4f49723f1a6309bfa403c2cee367425301794c7da4cc7ffb51f3d802927"
+    sha256 cellar: :any,                 arm64_monterey: "467c760fbe3ff26108e82a47876e626bd1e9ecbfce352c4c2d2bba4d9de9d2f9"
+    sha256 cellar: :any,                 arm64_big_sur:  "c0d20d39a966111e0a9f48ea3ceb420f8de9f163d31ee5a2dbc325078e014420"
+    sha256 cellar: :any,                 monterey:       "67e5b32ec92bceee098d501e77746e834f936238121840f373bcac6e1d6106ef"
+    sha256 cellar: :any,                 big_sur:        "134a3b6d943790bdec6634337ec3e64c9e5715c479e1f7abb4aa4aefa2c709ab"
+    sha256 cellar: :any,                 catalina:       "8a074931492249bebc0b573e1974d255adef1bf904be9ff65cc7824abc43ee07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1bab4b61c3d6420c5342a7ccc376a4d79c3238af2955fe1bb079546a29197937"
   end
 
   depends_on "sdl12-compat"
