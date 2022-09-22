939e59b5b5cc2b1b0129337e55a01686859fb309
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 03:14:48 +0800

    libxvmc: update 1.0.13 bottle.

--- Formula/libxvmc.rb
+++ Formula/libxvmc.rb
@@ -6,12 +6,8 @@ class Libxvmc < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8dd27f8a1dfc1d9d1a8de409a18365eda5ac632f1e07a25489cc49074076f552"
-    sha256 cellar: :any,                 arm64_big_sur:  "1f3cbc8fdb6acd20a304917bd4bd4ac4f8e8d9ab3cecb94f2a0dc63e59e63f41"
-    sha256 cellar: :any,                 monterey:       "d4aa0e1d0640d0508c623c22c8d3818f5fa58eda8f64fbc0863b21b318b40e55"
-    sha256 cellar: :any,                 big_sur:        "22eb48198116b542d5c038d83acadf2eaf7354ba68bb3d5ec147172d6bf776a2"
-    sha256 cellar: :any,                 catalina:       "e6ccee73e8041296f51cfea28b2f5a3f3800611df6364605827240190d5c8752"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f82dbdd0f31764d35dc28ab31057bed67d0eb38610f7d6158ecfdb201af1411"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxvmc"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a7eae09b648a2228745b3a1dd515d472e9ef8de17c217dd627a5b986ded01977"
   end
 
   depends_on "pkg-config" => :build
