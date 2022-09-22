4915a4b9aea3b9a708e452b6c6f2b508b1530ff8
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 21:50:07 +0800

    libcap: update 2.65 bottle.

--- Formula/libcap.rb
+++ Formula/libcap.rb
@@ -1,8 +1,8 @@
 class Libcap < Formula
   desc "User-space interfaces to POSIX 1003.1e capabilities"
   homepage "https://sites.google.com/site/fullycapable/"
-  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.64.tar.xz"
-  sha256 "c8465e1f0b068d5fc06199231135ccac7adb56d662b1de93589252e8cd071e13"
+  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.65.tar.xz"
+  sha256 "73e350020cc31fe15360879d19384ffa3395a825f065fcf6bda3a5cdf965bebd"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
 
   livecheck do
@@ -12,7 +12,7 @@ class Libcap < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libcap"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "65a32f2cc0cc2f51bef9c3f5a47825c14b1a8949f439ba44b4033305df5cf8f5"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "00d12f1e35f7d4a5a90c91a09d7822a3d4e34dc457af55c45f222fa95e10792c"
   end
 
   depends_on :linux
