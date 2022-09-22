f76429c8a39a4decb7baa72656cfdb769891d090
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-22 09:06:35 +0000

    gpredict: update 2.2.1_3 bottle.

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -31,15 +31,12 @@ class Gpredict < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "f873648c8df2b0e87c6b2c6e7553379ca0e239738a01f83a702788f33ba0dcc9"
-    sha256 arm64_big_sur:  "2c367d6266bd0af3583827c588ab864c26043444ad6b6379821c1b93e5093352"
-    sha256 monterey:       "650854e63dd2ed7f88d40188575d69f5e83e311ebecfc489c13c472adfd5e947"
-    sha256 big_sur:        "eccf4afd811d590ed5c930840933905bd5b1ea9bdf42e32e52cf4926d0c1eb05"
-    sha256 catalina:       "99fff9473dcc5eaa0c58cf0b2bf04f4240e1598aada45565e4dbbf050d2ac7dc"
-    sha256 mojave:         "952941a2ecdb5f75805888dfd020acce48c4f1b29a9c2e3ec8742d35fcd9c829"
-    sha256 high_sierra:    "189249444c490bc7984506a3d041de1d057fff671ff774871f549f6b32efa042"
-    sha256 sierra:         "9a0a4b0e63b3b1f84830f508d60ee3fc5b5fd0b9a5731241873168baa88209cf"
-    sha256 x86_64_linux:   "e87f34490c8549a80627a714275d1ab1f9b73285ea76f28c91b6f45a360963bc"
+    sha256 arm64_monterey: "269c3a416e968dcc1ebddeb7580660acb9568387121a52cd5dae4a421747ea2f"
+    sha256 arm64_big_sur:  "ef852387a0eeb9e19c38fabcf717c58f5a4924bb11c93a79ed214aa41dc5e0a3"
+    sha256 monterey:       "6486c396b33ef98057502fa04c9f8e53cd91493d541635ba9c04b61323be189f"
+    sha256 big_sur:        "914edd91da98b60ce958b96c2ff8c83f23d94ccb9b486beb745acc7766fa1686"
+    sha256 catalina:       "a7b78f175543acdab3731927cf266e33ccc571e439fe8e8662539bcc877f7956"
+    sha256 x86_64_linux:   "1256d49da41bbf2ebdda5da63e372143a323021e93e7e5e12cc9db3faf833272"
   end
 
   head do
