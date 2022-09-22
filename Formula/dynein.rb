2292f070e8a1e47100c76911a7da98a444bbd32d
Rui Chen <rui@chenrui.dev>
2022-09-22 13:57:43 -0400

    dynein 0.2.1 (new formula)
    * dynein 0.2.1 (new formula)
      dynein: need openssl and pkg-config for linux build
    * dynein: update test
    
    Closes #111466.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/dynein.rb
@@ -0,0 +1,24 @@
+class Dynein < Formula
+  desc "DynamoDB CLI"
+  homepage "https://github.com/awslabs/dynein"
+  url "https://github.com/awslabs/dynein/archive/v0.2.1.tar.gz"
+  sha256 "1946d521b74da303bafd19a0a36fd7510a9f8c9fc5cf64d2e6742b4b0b2c9389"
+  license "Apache-2.0"
+  head "https://github.com/awslabs/dynein.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@3" # need to build `openssl-sys`
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "To find all tables in all regions", shell_output("#{bin}/dy desc 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/dy --version")
+  end
+end
