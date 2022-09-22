c5b2e04e1c7cfc89b08df3ed352af86e3fadab64
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 14:51:49 +0800

    aerc: update 0.11.0 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -1,14 +1,14 @@
 class Aerc < Formula
   desc "Email client that runs in your terminal"
   homepage "https://aerc-mail.org/"
-  url "https://git.sr.ht/~rjarry/aerc/archive/0.9.0.tar.gz"
-  sha256 "b5901feb37a55edd1f713e76c1012ac3fc0757202ddacd7d388cc7ce60638023"
+  url "https://git.sr.ht/~rjarry/aerc/archive/0.11.0.tar.gz"
+  sha256 "3d8f3a2800946fce070e3eb02122e77c427a61c670a06337539b3e7f09e57861"
   license "MIT"
   head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aerc"
-    sha256 aarch64_linux: "40669a887923da7ee73a1c36060bbb450b7156ae0f5ef393a66e243e9b5dd6a9"
+    sha256 aarch64_linux: "a34450838bc2349a6e71a6cfc4e2e5e3dfc67ff9b42e66fd40b096a96ad674c6"
   end
 
   depends_on "go" => :build
