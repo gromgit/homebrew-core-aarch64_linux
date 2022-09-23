a0ea1981d0da5deefecb4661fd352cc3a38d1cf3
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 00:42:13 +0800

    cilium-cli: update 0.12.3 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -1,13 +1,13 @@
 class CiliumCli < Formula
   desc "CLI to install, manage & troubleshoot Kubernetes clusters running Cilium"
   homepage "https://cilium.io"
-  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.11.5.tar.gz"
-  sha256 "f1becdf87c0f1cfedcf206297f9f2371d31bc467b6c3f6d35af074c73f5d30ed"
+  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.12.3.tar.gz"
+  sha256 "0b62ada53c987db4bfcbdc3eecee1ce05c3cf6d19a11e60be215b1b64f4d574d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cilium-cli"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "5f13d8696f66f91feede6f94f9bf7a17d526dffe88d4e2e6bb693c5e313e49aa"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "007c72bd326db97b61b55f57068b5395044dc22486a0f21fb21ebfe79d389882"
   end
 
   depends_on "go" => :build
@@ -16,12 +16,7 @@ class CiliumCli < Formula
     ldflags = "-s -w -X github.com/cilium/cilium-cli/internal/cli/cmd.Version=#{version}"
     system "go", "build", *std_go_args(output: bin/"cilium", ldflags: ldflags), "./cmd/cilium"
 
-    bash_output = Utils.safe_popen_read(bin/"cilium", "completion", "bash")
-    (bash_completion/"cilium").write bash_output
-    zsh_output = Utils.safe_popen_read(bin/"cilium", "completion", "zsh")
-    (zsh_completion/"_cilium").write zsh_output
-    fish_output = Utils.safe_popen_read(bin/"cilium", "completion", "fish")
-    (fish_completion/"cilium.fish").write fish_output
+    generate_completions_from_executable(bin/"cilium", "completion", base_name: "cilium")
   end
 
   test do
