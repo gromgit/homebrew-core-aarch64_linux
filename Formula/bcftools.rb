baa213c341d06793ecd7f74ac6191bfe5384f2ae
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 14:32:22 +0800

    bcftools: update 1.16 bottle.

--- Formula/bcftools.rb
+++ Formula/bcftools.rb
@@ -1,8 +1,8 @@
 class Bcftools < Formula
   desc "Tools for BCF/VCF files and variant calling from samtools"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/bcftools/releases/download/1.15.1/bcftools-1.15.1.tar.bz2"
-  sha256 "f21f9564873eb27ccf22d13b91a64acb8fbbfe4f9e4c37933a54b9a95857f2d7"
+  url "https://github.com/samtools/bcftools/releases/download/1.16/bcftools-1.16.tar.bz2"
+  sha256 "90bf647c9ad79e10a243b8058c873de057fd7012e250d5ae6574839787a2ecd6"
   # The bcftools source code is MIT/Expat-licensed, but when it is configured
   # with --enable-libgsl the resulting executable is GPL-licensed.
   license "GPL-3.0-or-later"
@@ -13,12 +13,8 @@ class Bcftools < Formula
   end
 
   bottle do
-    sha256                               arm64_monterey: "547be287eadb9b767ab27232071c0fbd6895cea8de8470a657912d82ea90183c"
-    sha256                               arm64_big_sur:  "856cc821f42aa3a34efc45c71fe5be9139605e52373d123bd04be511d1476380"
-    sha256                               monterey:       "51b716a9f1b3b6889ee91f67135f0b3ef19bd4fbc298f1c3607497842684a5a2"
-    sha256                               big_sur:        "da96ced1e7dea42220ef0bea1deecc4f38bb1f30d68cd980dbdb37cdd61a719c"
-    sha256                               catalina:       "622b3e4e56e7b716824e3552e7cb9468753ba4dddacb7ef9b14e7e128cf68fc8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7d57e0c0fbe7639362ba27ef336000e43e6be81c68d920cf7f86d6b4f50eeb5"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bcftools"
+    sha256 aarch64_linux: "216d482528081929523e9f4be93bb7465c4fece371767966d6f87c1da24798a2"
   end
 
   depends_on "gsl"
