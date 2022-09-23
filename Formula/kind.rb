bab5c11bd3932325600a6a9680b0587b91756331
Porkepix <porkepix@gmail.com>
2022-09-23 04:49:19 +0200

    kind 0.16.0
    Closes #111489.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -1,8 +1,8 @@
 class Kind < Formula
   desc "Run local Kubernetes cluster in Docker"
   homepage "https://kind.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/kind/archive/v0.15.0.tar.gz"
-  sha256 "a3a0abbce70c5da267fabcb0409e0e373e8bc657679cc4cc38844743dd8a97d0"
+  url "https://github.com/kubernetes-sigs/kind/archive/v0.16.0.tar.gz"
+  sha256 "28b85260e326dc6b5baab28b927dcbf73f4f48c276364310ea0a7c9bd5e69f51"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/kind.git", branch: "main"
 
