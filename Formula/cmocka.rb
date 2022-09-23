d7d367ff2b043bd1d451757de2a181826b58c695
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 05:58:10 +0800

    cmocka: update 1.1.5 bottle.

--- Formula/cmocka.rb
+++ Formula/cmocka.rb
@@ -7,15 +7,8 @@ class Cmocka < Formula
   head "https://git.cryptomilk.org/projects/cmocka.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a504b57668ab63e04fd3ca656d6a0a48a215779b9779dad64e6e65e56441c2fd"
-    sha256 cellar: :any,                 arm64_big_sur:  "e2ed51c48c56006bb4b8591259eb206968e46457e78b15570c567d990b5f97d3"
-    sha256 cellar: :any,                 monterey:       "221d35e014f83b7ba5c115ed5dc058508f956ff551141676a13ece2be1c476a3"
-    sha256 cellar: :any,                 big_sur:        "a852c9033a2ca9543dff361a5a5d19027dddab7d207e9a080cf9f8bf75751354"
-    sha256 cellar: :any,                 catalina:       "719b81c50a85d95dfc0bdd88b52e5642cc81e22f95776fc8d92065217bef879e"
-    sha256 cellar: :any,                 mojave:         "a05bfdbe08b08dc01db59d0c2c724b2a58c4f9e12c260dc5865e27dd456e7771"
-    sha256 cellar: :any,                 high_sierra:    "c4fc9fe8a73b23206c0db8907c2f67dea482d689afea18c5e746556aff8098b5"
-    sha256 cellar: :any,                 sierra:         "a8d32491c7cfd1670be11c022faa07619d7821a4328fb034e76f225933b5c4dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0f6e5f5ffe5cdf0605d128b361f338ae8f986d7fbd5713681c07747738ff4266"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cmocka"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "03022b9291eafa538ff24eb4da6a4dacc10e00c85d88c365183558f4723107ec"
   end
 
   depends_on "cmake" => :build
