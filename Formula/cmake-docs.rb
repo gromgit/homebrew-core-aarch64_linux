c8672b46a50cc958a629463baf3aade2a8bcc514
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 05:26:45 +0800

    cmake-docs: update 3.24.1 bottle.

--- Formula/cmake-docs.rb
+++ Formula/cmake-docs.rb
@@ -1,10 +1,10 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.23.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.1.tar.gz"
-  sha256 "33fd10a8ec687a4d0d5b42473f10459bb92b3ae7def2b745dc10b192760869f3"
+  url "https://github.com/Kitware/CMake/releases/download/v3.24.1/cmake-3.24.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.24.0.tar.gz"
+  sha256 "4931e277a4db1a805f13baa7013a7757a0cbfe5b7932882925c7061d9d1fa82b"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
@@ -13,12 +13,8 @@ class CmakeDocs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c059eac37b2f074c1280791697e9433ff50120626ebc9ea327a250020967a6c6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c059eac37b2f074c1280791697e9433ff50120626ebc9ea327a250020967a6c6"
-    sha256 cellar: :any_skip_relocation, monterey:       "240eba24648b69bf6007a4bac5ca9ce577280d0135a414460be05f335181b147"
-    sha256 cellar: :any_skip_relocation, big_sur:        "240eba24648b69bf6007a4bac5ca9ce577280d0135a414460be05f335181b147"
-    sha256 cellar: :any_skip_relocation, catalina:       "240eba24648b69bf6007a4bac5ca9ce577280d0135a414460be05f335181b147"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c059eac37b2f074c1280791697e9433ff50120626ebc9ea327a250020967a6c6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cmake-docs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4d2d66e35c7c67bac425f636ea80ab1a29bd7dc0d4c5118f14383fdf95585cfe"
   end
 
   depends_on "cmake" => :build
