4ab193dd0118da8978351dc34336abc4ca2a6104
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 23:37:56 +0800

    cheat: update 4.3.3 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -1,13 +1,13 @@
 class Cheat < Formula
   desc "Create and view interactive cheat sheets for *nix commands"
   homepage "https://github.com/cheat/cheat"
-  url "https://github.com/cheat/cheat/archive/4.2.3.tar.gz"
-  sha256 "9624160ba542fb51bbd959d8c68b76f82ea324a6186d8d6d544b0efd8c9cc8ca"
+  url "https://github.com/cheat/cheat/archive/refs/tags/4.3.3.tar.gz"
+  sha256 "6a1739b71d436f45dc7c028ec79863a34e30cc13da7159bf23604b77f43faaf2"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cheat"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "95efa15c7e228d045e89e1dfc42d4d9e1d134ab84eaf7051b262df750afbe36a"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "117e08843e105e20faea716c4b5a703a779e89798fec72391f92e3d6286af491"
   end
 
   depends_on "go" => :build
@@ -26,6 +26,6 @@ class Cheat < Formula
     assert_match version.to_s, shell_output("#{bin}/cheat --version")
 
     output = shell_output("#{bin}/cheat --init 2>&1")
-    assert_match "editor: vim", output
+    assert_match "editor: EDITOR_PATH", output
   end
 end
