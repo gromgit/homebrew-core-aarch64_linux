4512213e31aa8bc2f0e46b2daebcee7e88f11077
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 12:37:47 +0800

    axel: update 2.17.11 bottle.

--- Formula/axel.rb
+++ Formula/axel.rb
@@ -7,12 +7,8 @@ class Axel < Formula
   head "https://github.com/axel-download-accelerator/axel.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "483732a5fda41e8592d78ce2d4df86362e60f61bbeb41081beaf382f211edea3"
-    sha256 cellar: :any, arm64_big_sur:  "8eeafff7031764fcc18d94121c6889a7bc3224ecf3f20225ef5cd96f09c6bfb3"
-    sha256 cellar: :any, monterey:       "c5e9ea169ba1985bb10ce39e1c11237bbe4d2c9a1f14b62f38756dcb87456cb9"
-    sha256 cellar: :any, big_sur:        "7ce859bfaebd9429851f7b8fcdaf8983c542c91bbcaec2e2d789e8c0e496306f"
-    sha256 cellar: :any, catalina:       "a9938c2a4753dcc84631739c7ddba105933ab110995b296c7e4a6da2bd9207fc"
-    sha256               x86_64_linux:   "145f3f4fec5782251254a8dd1adbd989118d1be0fe3d4808dce962a586f68119"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/axel"
+    sha256 aarch64_linux: "2861c0499b767e324470ac27ab746e4a4246da266ee38627f65cd591bcc6a0b3"
   end
 
   depends_on "autoconf" => :build
