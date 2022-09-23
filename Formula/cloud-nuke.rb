87de49a8207e66aa2b6449ff9345b35870478431
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 04:51:05 +0800

    cloud-nuke: update 0.19.0 bottle.

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -1,14 +1,14 @@
 class CloudNuke < Formula
   desc "CLI tool to nuke (delete) cloud resources"
   homepage "https://gruntwork.io/"
-  url "https://github.com/gruntwork-io/cloud-nuke/archive/v0.11.5.tar.gz"
-  sha256 "9292877e639d1db7a13dd3bde83fa2d7a894d50a6dcd7c363d8ab92bfc021e58"
+  url "https://github.com/gruntwork-io/cloud-nuke/archive/v0.19.0.tar.gz"
+  sha256 "e7ada221412aa88619aef52f884e1a60de4f317deb636734e345ba08a035b013"
   license "MIT"
   head "https://github.com/gruntwork-io/cloud-nuke.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cloud-nuke"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "d108b61c45a8f2ab66892958f89a26c2222e166b0a9f5b127bd1409ccfbe8120"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cd6bf7f1c855bb1be5d81db23a01d5443f33fa8b85c51b047df798786b9ec2c1"
   end
 
   depends_on "go" => :build
