bdb0ad418409ae3325673499d274bef18d02eb85
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 17:32:48 +0800

    re2: update 20220601 bottle.

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -1,9 +1,9 @@
 class Re2 < Formula
   desc "Alternative to backtracking PCRE-style regular expression engines"
   homepage "https://github.com/google/re2"
-  url "https://github.com/google/re2/archive/2022-04-01.tar.gz"
-  version "20220401"
-  sha256 "1ae8ccfdb1066a731bba6ee0881baad5efd2cd661acd9569b689f2586e1a50e9"
+  url "https://github.com/google/re2/archive/2022-06-01.tar.gz"
+  version "20220601"
+  sha256 "f89c61410a072e5cbcf8c27e3a778da7d6fd2f2b5b1445cd4f4508bee946ab0f"
   license "BSD-3-Clause"
   head "https://github.com/google/re2.git", branch: "main"
 
@@ -19,12 +19,8 @@ class Re2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "37132b360c414438108478cce8b4a2e0f3f0cda6964a0f2234b43db286c2fd21"
-    sha256 cellar: :any,                 arm64_big_sur:  "34486668ba2da5ea41ad6fd8a66af1b43ba1b019597c6b44fe7804da093dc80b"
-    sha256 cellar: :any,                 monterey:       "62cd468dafec02c1a1c0d28fd93b93364b5ea67f0fdbdcbb3b91044c9a5bfeec"
-    sha256 cellar: :any,                 big_sur:        "eed008dc0f16e4e27c6261a485e2de246129fdf2ee6e66c68377bba5a801c0fc"
-    sha256 cellar: :any,                 catalina:       "1de4aeb39e788675d4fed079ce848e18bdcda373034030ae282f74f4776880e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d42453c1a06530cd54dbcc66dd71f8707eb5c06e15f9233abcbf3aeeb4cc321e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/re2"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d5d26d03ecb88dd22f2b4841f93d15ca83d12df6b84172166531fe2ee3ac0482"
   end
 
   depends_on "cmake" => :build
