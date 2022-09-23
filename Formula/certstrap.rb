e4ad90ec7bc20df95891c422a2148d3fd5b9c6fc
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 21:58:26 +0800

    certstrap: update 1.3.0 bottle.

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -1,13 +1,13 @@
 class Certstrap < Formula
   desc "Tools to bootstrap CAs, certificate requests, and signed certificates"
   homepage "https://github.com/square/certstrap"
-  url "https://github.com/square/certstrap/archive/v1.2.0.tar.gz"
-  sha256 "0eebcc515ca1a3e945d0460386829c0cdd61e67c536ec858baa07986cb5e64f8"
+  url "https://github.com/square/certstrap/archive/v1.3.0.tar.gz"
+  sha256 "4b32289c20dfad7bf8ab653c200954b3b9981fcbf101b699ceb575c6e7661a90"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/certstrap"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "126d81d7761e50cf7a4e8fcef718ae2b1c9028cf20e5273312900f01245b1915"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8935f7fd839ba624232147a99d168be1ab688b617ee303a83cf99fdb155cbcd2"
   end
 
   depends_on "go" => :build
