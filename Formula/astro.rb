de03056d5a1dbba38260ae2bd657e16c08057543
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 09:31:08 +0800

    astro: update 1.5.0 bottle.

---
+++ Formula/astro.rb
@@ -0,0 +1,36 @@
+class Astro < Formula
+  desc "To build and run Airflow DAGs locally and interact with the Astronomer API"
+  homepage "https://www.astronomer.io/"
+  url "https://github.com/astronomer/astro-cli/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "904be5c6f2a0d50fdec8b750b5cea81efb9a8b39efa38376427baa2f32008cc6"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/astro"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "26789fa6e5d65307b4249fc94620423bd6b3ab58dadbb7778a2420e7bf07f754"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/astronomer/astro-cli/version.CurrVersion=#{version}")
+  end
+
+  test do
+    version_output = shell_output("#{bin}/astro version")
+    assert_match("Astro CLI Version: #{version}", version_output)
+
+    run_output = shell_output("echo 'y' | #{bin}/astro dev init")
+    assert_match(/^Initializing Astro project*/, run_output)
+    assert_predicate testpath/".astro/config.yaml", :exist?
+
+    run_output = shell_output("echo 'test@invalid.io' | #{bin}/astro login astronomer.io", 1)
+    assert_match(/^Welcome to the Astro CLI*/, run_output)
+  end
+end
