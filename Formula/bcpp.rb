75b873a33ad9d5f4ee195e6020af618c4b2cda41
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 14:39:07 +0800

    bcpp: update 20210108 bottle.

--- Formula/bcpp.rb
+++ Formula/bcpp.rb
@@ -11,13 +11,8 @@ class Bcpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1ef362f6583a9a5779f8fb0a5237cc554615161d4f7f9fd5b054e0a59b51d917"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c7332e45d68c7c34e04b36935495e8e189944442650379f2c920757f2b210b7"
-    sha256 cellar: :any_skip_relocation, monterey:       "43a5d452995b4befa7498697435d30594bf4aa619750ef9f60d8192a35332ee0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "447070d7c227cdb2e5c8df360c8ea31c8f9fa89b39e2092a3a888a40caedb523"
-    sha256 cellar: :any_skip_relocation, catalina:       "1f2a9da46190bde2855e3bdc5d430302c831e3ff0eb3e3c34f8754bbe73744da"
-    sha256 cellar: :any_skip_relocation, mojave:         "1872e08cd8d7addb8459865d451622d05ed4f4fc2f91e3a6f144ba1fe483b27a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec23404179ff7f080c3af852e5df992e01126b5a7d22b34ff509c20c863f7e18"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bcpp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "17c4494a162b3c1aa53d7aaeb53c62d07b5b3b40d10cd2f91a907910186fcd15"
   end
 
   # Build succeeds with system gcc (5.4.0) but seems to segfault at runtime.
