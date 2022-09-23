471959ed7af997cde673442da5a79a70ae5ddbf1
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 05:33:11 +0800

    cmark-gfm: update 0.29.0.gfm.5 bottle.

--- Formula/cmark-gfm.rb
+++ Formula/cmark-gfm.rb
@@ -1,18 +1,14 @@
 class CmarkGfm < Formula
   desc "C implementation of GitHub Flavored Markdown"
   homepage "https://github.com/github/cmark-gfm"
-  url "https://github.com/github/cmark-gfm/archive/0.29.0.gfm.3.tar.gz"
-  version "0.29.0.gfm.3"
-  sha256 "56fba15e63676c756566743dcd43c61c6a77cc1d17ad8be88a56452276ba4d4c"
+  url "https://github.com/github/cmark-gfm/archive/0.29.0.gfm.5.tar.gz"
+  version "0.29.0.gfm.5"
+  sha256 "f665079062e05abc44ae933a74f1bdc5c89af1e1d2f4152042fc1ec5571093fe"
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "215ca612a34a62533cffd87ca075b07e4b7000462ce0597863806941a35e7dba"
-    sha256 cellar: :any,                 arm64_big_sur:  "65fb4a92ad77038e9ed86c326bf70cd3da114c6b81263f5cd3d540cdb584f717"
-    sha256 cellar: :any,                 monterey:       "d50121029ed09bf22c80fdcac22cfb7ea71191785bb0f90cc570d93411b92053"
-    sha256 cellar: :any,                 big_sur:        "5a957847eff309fb0119460a301f851a282774c9d8601a2f7ddb484fbb9e340a"
-    sha256 cellar: :any,                 catalina:       "632243f006c84b2ef42110844f6ba7344b76172f9bfdc35d031c025376c93db9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f7008a12bc36b961dc37bf3d574592c82930ff843a7701f81cca40628a20c1c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cmark-gfm"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ff1e29e1f67861925c5fba4b5afedb63c62a5c92f656401c0b441432d4130cd1"
   end
 
   depends_on "cmake" => :build
