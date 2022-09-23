71e4a1ebc7e549d4c47ceec557054166758a61b1
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 18:18:15 +0800

    borgbackup: update 1.2.2 bottle.

--- Formula/borgbackup.rb
+++ Formula/borgbackup.rb
@@ -3,18 +3,13 @@ class Borgbackup < Formula
 
   desc "Deduplicating archiver with compression and authenticated encryption"
   homepage "https://borgbackup.org/"
-  url "https://files.pythonhosted.org/packages/0f/e9/0b07aaff39e22eee9a983a48507bff46cf0d634e8a24e635e336cd7b3bce/borgbackup-1.2.0.tar.gz"
-  sha256 "e39a5547902ef456101aa4c779fa66b345bda70d16788e8bd18e458f93af7f67"
+  url "https://files.pythonhosted.org/packages/a2/98/479f364b69182b9a29c82607a17a9cf2e3cdc8743bb9570a4da9d511d0a6/borgbackup-1.2.2.tar.gz"
+  sha256 "d730687443f1beb602b1d72bae36318f6f9654818fcdc50458540ec579e57260"
   license "BSD-3-Clause"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "ee53a6af415e93a0f186326b7e5585726e1eeea909c10e4e4ab6dc4d8e1adde3"
-    sha256 cellar: :any,                 arm64_big_sur:  "0b4d3404888df9c398824514724777d008844d619cbdc38e625f77a9e93df0bc"
-    sha256 cellar: :any,                 monterey:       "07c7748f8ee9f85e3b2d6862bd78429308ba78577f8e81f145b00576658fa309"
-    sha256 cellar: :any,                 big_sur:        "d84a10fd893d39ffadb8d344a37274a9ce32de75d500262878daaf682a223521"
-    sha256 cellar: :any,                 catalina:       "b9951690869d998fbb12941c1299a5721951d92dfe5eb8c8f6f89986ffe6f997"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fbbd4b0aa5d572304f57a47267418693249dc94611ecddb926a7fbe623d4181e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/borgbackup"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2d07e3794f9f4037df6cc3ae527a947c9137c0f82970c2757cf93dfa0dd5854b"
   end
 
   depends_on "pkg-config" => :build
@@ -30,8 +25,8 @@ class Borgbackup < Formula
   end
 
   resource "msgpack" do
-    url "https://files.pythonhosted.org/packages/61/3c/2206f39880d38ca7ad8ac1b28d2d5ca81632d163b2d68ef90e46409ca057/msgpack-1.0.3.tar.gz"
-    sha256 "51fdc7fb93615286428ee7758cecc2f374d5ff363bdd884c7ea622a7a327a81e"
+    url "https://files.pythonhosted.org/packages/22/44/0829b19ac243211d1d2bd759999aa92196c546518b0be91de9cacc98122a/msgpack-1.0.4.tar.gz"
+    sha256 "f5d869c18f030202eb412f08b28d2afeea553d6613aee89e200d7aca7ef01f5f"
   end
 
   resource "packaging" do
@@ -40,21 +35,21 @@ class Borgbackup < Formula
   end
 
   resource "pyparsing" do
-    url "https://files.pythonhosted.org/packages/d6/60/9bed18f43275b34198eb9720d4c1238c68b3755620d20df0afd89424d32b/pyparsing-3.0.7.tar.gz"
-    sha256 "18ee9022775d270c55187733956460083db60b37d0d0fb357445f3094eed3eea"
+    url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
+    sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
   end
 
   def install
-    bash_completion.install "scripts/shell_completions/bash/borg"
-    fish_completion.install "scripts/shell_completions/fish/borg.fish"
-    zsh_completion.install "scripts/shell_completions/zsh/_borg"
-
     ENV["BORG_LIBB2_PREFIX"] = Formula["libb2"].prefix
     ENV["BORG_LIBLZ4_PREFIX"] = Formula["lz4"].prefix
     ENV["BORG_LIBXXHASH_PREFIX"] = Formula["xxhash"].prefix
     ENV["BORG_LIBZSTD_PREFIX"] = Formula["zstd"].prefix
     ENV["BORG_OPENSSL_PREFIX"] = Formula["openssl@1.1"].prefix
     virtualenv_install_with_resources
+
+    bash_completion.install "scripts/shell_completions/bash/borg"
+    fish_completion.install "scripts/shell_completions/fish/borg.fish"
+    zsh_completion.install "scripts/shell_completions/zsh/_borg"
   end
 
   test do
