150cca0c295dbf3a65038a08d9a8fb93331f15fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-23 14:15:53 +0000

    dory: update 1.2.0 bottle.

--- Formula/dory.rb
+++ Formula/dory.rb
@@ -6,13 +6,12 @@ class Dory < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a98dd50fdc6cabcd7f200958b32c6c8c448174b861efd576170d7ac696e57e8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cbe36a5c87731cae07c1de8b9f7e2cfc04e17bba6e1a6918678ef49f62256bad"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d97718bff64b71e82a4c538cf681a9521c7e5cbf1b79e640355c541d9082909"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
-    sha256 cellar: :any_skip_relocation, mojave:         "52197dfd1668a9c0c3c2daa1cd7e808d89bbb4ba3f3500746e60b56cbc05058b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "858fd6ba202acedbc27ffea81e407ed0d25e6d77386e2809b412fb776a07e5d9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "baf1bf19aae6f4be3df208ba5cf524cb9d4179fd8d2e11ffc74d4f676033e670"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "baf1bf19aae6f4be3df208ba5cf524cb9d4179fd8d2e11ffc74d4f676033e670"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b997c97ef0977274fc7cf99d73c7685d641f7f512c48fc96067dcff3fe6e138"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2b997c97ef0977274fc7cf99d73c7685d641f7f512c48fc96067dcff3fe6e138"
+    sha256 cellar: :any_skip_relocation, catalina:       "2b997c97ef0977274fc7cf99d73c7685d641f7f512c48fc96067dcff3fe6e138"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc25156512dc5b729b7b732d58752f4d7b17d19c884f1f5c6db21e42db9244f2"
   end
 
   depends_on "ruby@2.7"
