068b4dd498a730ecae8683223bcaf510d9184f44
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 08:33:36 +0800

    arm-linux-gnueabihf-binutils: update 2.39 bottle.

--- Formula/arm-linux-gnueabihf-binutils.rb
+++ Formula/arm-linux-gnueabihf-binutils.rb
@@ -1,9 +1,9 @@
 class ArmLinuxGnueabihfBinutils < Formula
   desc "FSF/GNU binutils for cross-compiling to arm-linux"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.xz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.38.tar.xz"
-  sha256 "e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
+  sha256 "645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -11,12 +11,8 @@ class ArmLinuxGnueabihfBinutils < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "729928ef4235b88356212ad30314b0a5543133b87d6a269bd16e609f853d1bc5"
-    sha256 arm64_big_sur:  "affec31d67e52ebf0356255ee7b0b898f333e6019e11cc9004878116ffa266b8"
-    sha256 monterey:       "f3d38b79845de8e16ae966af77f55ec09b137f28731c82b06af0c56bcb94525a"
-    sha256 big_sur:        "00e3c86d150d5b6f31b4ac2b62690a3df9e241a4e35d8f2a5ccbee115873829c"
-    sha256 catalina:       "e7e27fc6eaf46d76fa75111061e6e94b43cda5c3d121b4e9bb4ba946b2f1b501"
-    sha256 x86_64_linux:   "d95ef9a584dec102bc33909ff25ff80e32f6f064f2d9da4b4a85a1ba873ac190"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/arm-linux-gnueabihf-binutils"
+    sha256 aarch64_linux: "28b26fa9874200c3f69c71ac3e2aa64389f5c377e7bbaa2d90b56572735616cf"
   end
 
   uses_from_macos "texinfo"
