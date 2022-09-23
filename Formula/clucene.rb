8f162addcbcc8673d498868844743bf58e5749f0
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 05:14:54 +0800

    clucene: update 2.3.3.4 bottle.

--- Formula/clucene.rb
+++ Formula/clucene.rb
@@ -11,16 +11,8 @@ class Clucene < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 monterey:     "c557e0403dce15f14e8584b4542fbe6dbd7ddc467d085d99e396b3fd16651dcb"
-    sha256 cellar: :any,                 big_sur:      "22366684958d7ee8fd5d0b07efe750a74347a4394dbc56ea80db1cf25db70c2f"
-    sha256 cellar: :any,                 catalina:     "c5b00eb94db40324fb66bc7f79737f3963ea5f0df6f7c07c39583fe95bfe0ae6"
-    sha256 cellar: :any,                 mojave:       "1e19a680030d8c4863deb4d5a5bc40675c58df5c21e5e7217f2ed8fcbc624d21"
-    sha256 cellar: :any,                 high_sierra:  "3986feb447660b2732e2983f54d22086b10da442cb2d7b6ceac45a91ad58f09b"
-    sha256 cellar: :any,                 sierra:       "5d25768dc355533553e6c1605a1dc89985ddd26b4d17d10078c39d6b085c03fb"
-    sha256 cellar: :any,                 el_capitan:   "5ee0c98072c18c75dd8c25fd309ccb49f033300d474367a8e325ec601c427f66"
-    sha256 cellar: :any,                 yosemite:     "182db4f73e058e9d28b77cbbd642c40ecc403fbf1d9dc8357387b2c54dba8d1e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4c61f6b320d11670d29ae7294ede5bcd3c1d5684c41cc01ed689de688dbab6e7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/clucene"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "feda239c37c4b449916e1fcd814520bcd3cd8678d84363691b14ba2d4b9b8174"
   end
 
   depends_on "cmake" => :build
