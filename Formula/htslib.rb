491dbefe043db8c6e4828d3ff543b98feb355027
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 14:26:05 +0800

    htslib: update 1.16 bottle.

--- Formula/htslib.rb
+++ Formula/htslib.rb
@@ -1,8 +1,8 @@
 class Htslib < Formula
   desc "C library for high-throughput sequencing data formats"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2"
-  sha256 "8d7f8bf9658226942eeab70af2a22aca618577eaa8fe2ed9416ee306d5351aa1"
+  url "https://github.com/samtools/htslib/releases/download/1.16/htslib-1.16.tar.bz2"
+  sha256 "606b7c7aff73734cf033ecd156f40529fa5792f54524952a28938ca0890d7924"
   license "MIT"
 
   livecheck do
@@ -11,12 +11,8 @@ class Htslib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "126ee5017717e388750cafbea6b2915b5550c9be5f5623798e173e3ed35d91e5"
-    sha256 cellar: :any,                 arm64_big_sur:  "6ffc3560ccf65563420b32c35177b738783fc5b7499c2e16efdf6f2157264497"
-    sha256 cellar: :any,                 monterey:       "51952a370f820e68811bd9c891c2f134eb0366f751acf5f23e665ee2b2b15a65"
-    sha256 cellar: :any,                 big_sur:        "ea69b2d7b0f29325dda506c73a8cd726fac704397d2d991422660b0f56f13f53"
-    sha256 cellar: :any,                 catalina:       "fad753cd51694302972903cf8b4a8655aa5a76014a7a1ed272ec611ab1791da0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4888416ae72370da97c251d870fbc5e88f53b3abdf262ff73779b9095b55a365"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/htslib"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "518800bb2da7c9643f43b42d4d21d3b993f8250b398932a9dd561ea2e953b7cc"
   end
 
   depends_on "xz"
