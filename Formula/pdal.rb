dd9dfb15e347b520e31188502554610bf61e17e8
Piero Toffanin <pt@masseranolabs.com>
2022-09-22 21:30:16 -0400

    pdal: remove pcl dependency
    Closes #111480.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -35,7 +35,6 @@ class Pdal < Formula
   depends_on "laszip"
   depends_on "libpq"
   depends_on "numpy"
-  depends_on "pcl"
 
   fails_with gcc: "5" # gdal is compiled with GCC
 
@@ -44,7 +43,6 @@ class Pdal < Formula
                          "-DWITH_LASZIP=TRUE",
                          "-DBUILD_PLUGIN_GREYHOUND=ON",
                          "-DBUILD_PLUGIN_ICEBRIDGE=ON",
-                         "-DBUILD_PLUGIN_PCL=ON",
                          "-DBUILD_PLUGIN_PGPOINTCLOUD=ON",
                          "-DBUILD_PLUGIN_PYTHON=ON",
                          "-DBUILD_PLUGIN_SQLITE=ON"
