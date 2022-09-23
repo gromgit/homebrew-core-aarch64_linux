ac9da2b6c6dd041e6c6ce66584189fa64e90eb19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-23 05:54:56 +0000

    pdftk-java 3.3.3
    Closes #111500.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pdftk-java.rb
+++ Formula/pdftk-java.rb
@@ -1,8 +1,8 @@
 class PdftkJava < Formula
   desc "Port of pdftk in java"
   homepage "https://gitlab.com/pdftk-java/pdftk"
-  url "https://gitlab.com/pdftk-java/pdftk/-/archive/v3.3.2/pdftk-v3.3.2.tar.gz"
-  sha256 "23bd8ff9a9c021c9bcd87e0d4eb3359ecdb12bef6879062e8d9bb25e18cb67df"
+  url "https://gitlab.com/pdftk-java/pdftk/-/archive/v3.3.3/pdftk-v3.3.3.tar.gz"
+  sha256 "9c947de54658539e3a136e39f9c38ece1cf2893d143abb7f5bf3a2e3e005b286"
   license "GPL-2.0-or-later"
   head "https://gitlab.com/pdftk-java/pdftk.git", branch: "master"
 
