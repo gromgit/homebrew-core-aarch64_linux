cb3edb40d4833919c4f463bffeb2f521731894e8
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 23:51:39 +0800

    daq: update 3.0.9 bottle.

--- Formula/daq.rb
+++ Formula/daq.rb
@@ -1,19 +1,15 @@
 class Daq < Formula
   desc "Network intrusion prevention and detection system"
   homepage "https://www.snort.org/"
-  url "https://github.com/snort3/libdaq/archive/v3.0.7.tar.gz"
-  mirror "https://fossies.org/linux/misc/libdaq-3.0.7.tar.gz"
-  sha256 "e3af1ef17d764294ae428e662f7d2a6187a0085c6e0f15fc230e754a298cabe2"
+  url "https://github.com/snort3/libdaq/archive/v3.0.9.tar.gz"
+  mirror "https://fossies.org/linux/misc/libdaq-3.0.9.tar.gz"
+  sha256 "c0e8535533720a6df05ab884b7c8f5fb4222f3aac12bdc11829e08c79716d338"
   license "GPL-2.0-only"
   head "https://github.com/snort3/libdaq.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6134622f0d881a752dd52c54481f188026868d1d5df83ccb881b1ecc06226ed3"
-    sha256 cellar: :any,                 arm64_big_sur:  "13f029804a664d28ba6443ab2d2b9c6dd59ed6b870946d3ccc1734ffe022caeb"
-    sha256 cellar: :any,                 monterey:       "1203103b74dfd43731f233e70b856b33bce8f7a5df514e05a405101d8c8b8c13"
-    sha256 cellar: :any,                 big_sur:        "5b1ae8880f97bc8020d03e4057ddaeb345755414c534e0fb5a43ea99689c4eac"
-    sha256 cellar: :any,                 catalina:       "b16673133466e6fd4394a7e81a6c1effbb33ab6cef5b897ee6dfd1292c302bd9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4bcbd83b743a5f422372e213fd9c5f2289c76801cf018926ceb030761542ed2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/daq"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1d4a037eddd727cf1082436347d42563291919257459a68e6eef3fce5c242624"
   end
 
   depends_on "autoconf" => :build
