53851bc8536008a89489576e8b818a8be9ab1265
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 08:54:55 +0800

    conftest: update 0.34.0 bottle.

--- Formula/conftest.rb
+++ Formula/conftest.rb
@@ -1,14 +1,14 @@
 class Conftest < Formula
   desc "Test your configuration files using Open Policy Agent"
   homepage "https://www.conftest.dev/"
-  url "https://github.com/open-policy-agent/conftest/archive/v0.32.0.tar.gz"
-  sha256 "7e2bd2ae486d72bbc55e3ba376336fb55df62f3453ad45fcbcff3dbe766f9925"
+  url "https://github.com/open-policy-agent/conftest/archive/v0.34.0.tar.gz"
+  sha256 "7d305b88dd2aae866851301ee0a0db7f4ea37688ff09b77ec2a748240d5c92b9"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/conftest.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/conftest"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "625e2ec1ed6c3c18179796a5a8fa10ad323707cc866a1db2ff9f1f96d1677d4f"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "abf74b892458d51251ac823205376b1d621adab3748e874d0a97c27716544247"
   end
 
   depends_on "go" => :build
@@ -16,14 +16,7 @@ class Conftest < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-X github.com/open-policy-agent/conftest/internal/commands.version=#{version}")
 
-    bash_output = Utils.safe_popen_read(bin/"conftest", "completion", "bash")
-    (bash_completion/"conftest").write bash_output
-
-    zsh_output = Utils.safe_popen_read(bin/"conftest", "completion", "zsh")
-    (zsh_completion/"_conftest").write zsh_output
-
-    fish_output = Utils.safe_popen_read(bin/"conftest", "completion", "fish")
-    (fish_completion/"conftest.fish").write fish_output
+    generate_completions_from_executable(bin/"conftest", "completion")
   end
 
   test do
