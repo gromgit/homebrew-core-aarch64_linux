b7f2ad98a19314dee1711b7e05c89d43274e030f
Sam Ford <1584702+samford@users.noreply.github.com>
2022-09-24 15:28:16 -0400

    gauche: update livecheck
    Closes #111608.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -8,8 +8,10 @@ class Gauche < Formula
 
   livecheck do
     url :stable
-    strategy :github_latest
-    regex(/href=.*?Gauche[._-]v?(\d+(?:\.\d+)+(?:[._-]p\d+)?)\.t/i)
+    regex(%r{href=["']?[^"' >]*?/tag/\D*?(\d+(?:[._]\d+)+(?:[._-]?p\d+)?)["' >]}i)
+    strategy :github_latest do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
   end
 
   bottle do
