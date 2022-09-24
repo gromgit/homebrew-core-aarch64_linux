f76e1e7df7a368a226a7450b59236ea924bddd39
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 22:10:28 +0800

    cuetools: update 1.4.1 bottle.

--- Formula/cuetools.rb
+++ Formula/cuetools.rb
@@ -7,17 +7,8 @@ class Cuetools < Formula
   head "https://github.com/svend/cuetools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b8f56e6ee35c74523629176408833a619c16aba5b81ab260222ed1682f7f938"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d6065775e7e6286464caa093613104c747720e1b9ea98f29f71abea5a365ac05"
-    sha256 cellar: :any_skip_relocation, monterey:       "65929af8ca94c0c28f16886667d1869924da3bb193ed10797af30f0e43de1744"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f649575d3661f08e573a8e72aa3e45580ed8f75f89fbe2409b2684732ab0f0a3"
-    sha256 cellar: :any_skip_relocation, catalina:       "dc2d7bfcb8fd048421265da986fdb381007d64c7d2a45d45a53b896bad78bf18"
-    sha256 cellar: :any_skip_relocation, mojave:         "1e36c3c8d2d53947b73a9f0a0aed74145e2b1890f83764de02f1d12566d0300f"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "4393d6db857a9568a34de3a09ff049fbec9a55a95b029eacd24e35d6ce792074"
-    sha256 cellar: :any_skip_relocation, sierra:         "9456e5957a78f993f5a8cef76aa583ac6a42a8298fb05bded243dbaf810f9a44"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "7f0effc75d64fca0f2695b5f7ddb4d8713cc83522d40dcd37842e83c120ac117"
-    sha256 cellar: :any_skip_relocation, yosemite:       "81d06ef2e3d98061f332a535b810102c1be0505371c1ac1aed711cf2ae8de5a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45fd4566e2ce0650ad162fb32f3a12f30523efeded3f6d30612cdd8efca73ffc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cuetools"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ee00ce79c6fbf2ad64db87a9bc4b5a5943029d1031edb19465b9912406796520"
   end
 
   depends_on "autoconf" => :build
