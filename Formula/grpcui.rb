168f23cba8555b03ab6f926788a31d2c5958f0aa
Michael Cho <cho-m@tuta.io>
2022-09-23 18:27:40 -0700

    grpcui: use `go` to build
    Closes #111571.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grpcui.rb
+++ Formula/grpcui.rb
@@ -14,8 +14,7 @@ class Grpcui < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "a243154ac04bccb01591b0f07c7c56dbf86e48ffbdea0d296295f7a91f18ca82"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}"), "./cmd/grpcui"
