0f09092f74ca77df3e8110ddc53a38629377cf7f
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 15:00:44 +0800

    cpputest: update 4.0 bottle.

--- Formula/cpputest.rb
+++ Formula/cpputest.rb
@@ -7,15 +7,8 @@ class Cpputest < Formula
   head "https://github.com/cpputest/cpputest.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "12658e53f9987e135b9e375e9f143a9a2a259591057cb230ebe8fc2a8edd138b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51cc4f2febbce066e2c57af7a50c816cbb02ec5c61394657e9fb4fef5977e8d0"
-    sha256 cellar: :any_skip_relocation, monterey:       "2ebba02ed9e1c87a6805e2c402433e59c9c9a0e377e179dc65e3d7c9f56dbbea"
-    sha256 cellar: :any_skip_relocation, big_sur:        "37ccb80c5598e80ecacd6b5b33a610ce38666e9878cb7365a0f79e5705df49a0"
-    sha256 cellar: :any_skip_relocation, catalina:       "9e06d26ed7a552c818c7f1d6bb68ef16e7185238a14bdf0ae337a410ecb46384"
-    sha256 cellar: :any_skip_relocation, mojave:         "59881c464ae17f1a2381145f78f614d174c83fbe8f4900e362e9a6830fcf446e"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9cea67d4098efe30dd499d1a999467800ff91a9e7954ec6407b03d181a20761d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4fb771aa9c96a9d5fa6f88f1172d94ed171b1b2ce87a353828771b48d732c68"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cpputest"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8e4a9605064e19a7add52b46b60ceb94bdb65674db25f48096081fc7e26c69f0"
   end
 
   depends_on "cmake" => :build
