13c134fd43991d0eac0d7f2c6056d23d8591b59f
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 16:19:31 +0800

    croc: update 9.6.0 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -1,14 +1,14 @@
 class Croc < Formula
   desc "Securely send things from one computer to another"
   homepage "https://github.com/schollz/croc"
-  url "https://github.com/schollz/croc/archive/v9.5.5.tar.gz"
-  sha256 "221ebf034e05ef8c06b2e2290dfb83c6f8b4b3f8a9168d6d826b87ed3c62d51a"
+  url "https://github.com/schollz/croc/archive/v9.6.0.tar.gz"
+  sha256 "15fccb905872ae160c626ef7e47a92a8d78915b738d097865f177dfec925480b"
   license "MIT"
   head "https://github.com/schollz/croc.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/croc"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "835bab5e0f32c1dcffedcd62a3535d75ea589c0ec72141878088e6ca26a33eda"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "07ce89e716be833661bdc710362b2832b18ef93a3e720b70b20955830fb91caf"
   end
 
   depends_on "go" => :build
