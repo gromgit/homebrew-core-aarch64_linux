b834b3fdef7fe26bb96b0458eae2817ba0d04ab1
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 15:47:30 +0800

    cri-tools: update 1.25.0 bottle.

--- Formula/cri-tools.rb
+++ Formula/cri-tools.rb
@@ -1,14 +1,14 @@
 class CriTools < Formula
   desc "CLI and validation tools for Kubelet Container Runtime Interface (CRI)"
   homepage "https://github.com/kubernetes-sigs/cri-tools"
-  url "https://github.com/kubernetes-sigs/cri-tools/archive/v1.23.0.tar.gz"
-  sha256 "c6a2e7fdd76d16f1bb5bbdb3c71a335a383e54bc6114058f16bf2789faf808de"
+  url "https://github.com/kubernetes-sigs/cri-tools/archive/v1.25.0.tar.gz"
+  sha256 "bd900fac00b3247fa601f7aca4e0e44825dfd67d399c2830cbc643b7d73dbe52"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cri-tools.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cri-tools"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "833a3517f71fc0401d652d4b233512050c68d904fedadd6b419ab9374c09d605"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c13894b8bf31fd28dc55d9e246bf45d9f9a4a82eec4b934e91e193c979111d1e"
   end
 
   depends_on "go" => :build
@@ -22,21 +22,14 @@ class CriTools < Formula
       system "make", "install", "VERSION=#{version}"
     end
 
-    output = Utils.safe_popen_read("#{bin}/crictl", "completion", "bash")
-    (bash_completion/"crictl").write output
-
-    output = Utils.safe_popen_read("#{bin}/crictl", "completion", "zsh")
-    (zsh_completion/"_crictl").write output
-
-    output = Utils.safe_popen_read("#{bin}/crictl", "completion", "fish")
-    (fish_completion/"crictl.fish").write output
+    generate_completions_from_executable(bin/"crictl", "completion", base_name: "crictl")
   end
 
   test do
     crictl_output = shell_output(
       "#{bin}/crictl --runtime-endpoint unix:///var/run/nonexistent.sock --timeout 10ms info 2>&1", 1
     )
-    assert_match "context deadline exceeded", crictl_output
+    assert_match "unable to determine runtime API version", crictl_output
 
     critest_output = shell_output("#{bin}/critest --ginkgo.dryRun 2>&1")
     assert_match "PASS", critest_output
