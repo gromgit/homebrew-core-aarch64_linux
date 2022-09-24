ce4f11deb67d88d3d6b872a4fd20e1fde81f0fe0
Adrian Ho <the.gromgit@gmail.com>
2022-09-24 14:53:07 +0800

    cppp: update 2.9 bottle.

--- Formula/cppp.rb
+++ Formula/cppp.rb
@@ -1,8 +1,8 @@
 class Cppp < Formula
   desc "Partial Preprocessor for C"
   homepage "https://www.muppetlabs.com/~breadbox/software/cppp.html"
-  url "https://www.muppetlabs.com/~breadbox/pub/software/cppp-2.8.tar.gz"
-  sha256 "a369cec68cbc3b9ad595ee83c130ae7ce7d5f74479387755c22a4a5ff7387ff5"
+  url "https://www.muppetlabs.com/~breadbox/pub/software/cppp-2.9.tar.gz"
+  sha256 "76a95b46c3e36d55c0a98175c0aa72b17b219e68062c2c2c26f971e749951c07"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Cppp < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cppp"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "30fdadd08fa950e8ea3dc50979f842845059e7d7a051124834055a705ee8a3e2"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5c70aa6a1084bfc0796a1ecdfd46b6a51e095543a600bab8666240a65d92abbb"
   end
 
   def install
