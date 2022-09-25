62d860469ee5202daf783669e305d0114855afa6
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 21:50:59 +0800

    dunamai: update 1.13.0 bottle.

---
+++ Formula/dunamai.rb
@@ -0,0 +1,41 @@
+class Dunamai < Formula
+  include Language::Python::Virtualenv
+
+  desc "Dynamic version generation"
+  homepage "https://github.com/mtkennerly/dunamai"
+  url "https://files.pythonhosted.org/packages/2d/e4/d92c4e1ac3115575c9a8d528625adf08ad3a980f8236a34486e49a7ad381/dunamai-1.13.0.tar.gz"
+  sha256 "5bf9e5baaa673b692a9613e4961b18b6d2d9743412f7bbcf4a39c84a979f47e2"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dunamai"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "df934dd5ebaa3cf1202bac5fbe498f6aaa4797b2cf7eea7fa769b83c4dead94d"
+  end
+
+  depends_on "python@3.10"
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/df/9e/d1a7217f69310c1db8fdf8ab396229f55a699ce34a203691794c5d1cad0c/packaging-21.3.tar.gz"
+    sha256 "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
+    sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "git", "init"
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "BrewTestBot@test.com"
+    touch "foo"
+    system "git", "add", "foo"
+    system "git", "commit", "-m", "bar"
+    system "git", "tag", "v0.1"
+    assert_equal "0.1", shell_output("#{bin}/dunamai from any").chomp
+  end
+end
