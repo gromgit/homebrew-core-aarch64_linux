2367ace3a8ef7277217ed29976bfe599c9155c7c
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 08:49:28 +0800

    kmod: update 30 bottle.

--- Formula/kmod.rb
+++ Formula/kmod.rb
@@ -1,13 +1,13 @@
 class Kmod < Formula
   desc "Linux kernel module handling"
   homepage "https://git.kernel.org/pub/scm/utils/kernel/kmod/kmod.git"
-  url "https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-29.tar.xz"
-  sha256 "0b80eea7aa184ac6fd20cafa2a1fdf290ffecc70869a797079e2cc5c6225a52a"
+  url "https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-30.tar.xz"
+  sha256 "f897dd72698dc6ac1ef03255cd0a5734ad932318e4adbaebc7338ef2f5202f9f"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/kmod"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "947621ce3b66f7306506b0e08e574c4f96cc2aa8a4823bae8a0c10135f921f71"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "fb6a9b18176dcb50468d5296bcce12c0b6d670e865de33a98fb87dd150c3a9a2"
   end
 
   depends_on :linux
