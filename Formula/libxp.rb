12413f52a3f8067a969272c5a1641d8bd42db224
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 09:01:41 +0800

    libxp: update 1.0.3 bottle.

--- Formula/libxp.rb
+++ Formula/libxp.rb
@@ -11,14 +11,8 @@ class Libxp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "15125f120ead5edb95bf231ee97dd9faacd845589e8c12f085e86d53e107dd72"
-    sha256 cellar: :any,                 arm64_big_sur:  "d9e17f99eb3a084585f049a3243affb67c0bc494d55ecea910ddbeb6b4c7d79c"
-    sha256 cellar: :any,                 monterey:       "e49c62d4d05ac0ed1ff300817124973bd03a0c30160eda4cb18e7d45cdf3c005"
-    sha256 cellar: :any,                 big_sur:        "21e04a90fe93b05faeb09b3a7e65c68a3449bcd5596c5df8d21ff1afb22b0a93"
-    sha256 cellar: :any,                 catalina:       "e70342d93c5cf690582f559318b05b26da9175fc7620493fa15a224a847ec1da"
-    sha256 cellar: :any,                 mojave:         "1cc823e7fe3acb64e58b554e5e956302959f28cb5737eea5c6d15655128aee15"
-    sha256 cellar: :any,                 high_sierra:    "8e904b533b4c4264232ae6391e7d4bc37dade77d5d20539ed3d42900ab3950ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d81a760a81e1602499f04fd3b9809a73324f4eaec3c521326869ddb824372c88"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ab7a73707e5130d7e78ad8af46b0fccd50672b27fe37b2da38a66e4b5c8e01d3"
   end
 
   depends_on "autoconf" => :build
