d5578c7f543f6654da8142c17f9c18ec40e09f6e
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 21:23:35 +0800

    duckdb: update 0.3.4 bottle.

--- Formula/duckdb.rb
+++ Formula/duckdb.rb
@@ -7,12 +7,8 @@ class Duckdb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "5ac818edec1695c29278e57ad3a45ea9eb87be6e3173324e02eb4d7291426ec0"
-    sha256 cellar: :any,                 arm64_big_sur:  "47fa2b4d19d1badb42da49c64b736d58e30db52227269edbe9c6967df5657303"
-    sha256 cellar: :any,                 monterey:       "6cfb2e1ab4e1d633a1bbab6f11c57e18b2b7bdec6aea030e62f8b9e6ab7df93a"
-    sha256 cellar: :any,                 big_sur:        "c49438029913b6fe4b252ebc4a9dacaec989f1818a2f5eec4274520c65ebc56c"
-    sha256 cellar: :any,                 catalina:       "4bc94c421c8ae3e3dc16765515daa4fff650ae7f9e64f458570c98e6386dc98c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "96da98d417df0acd7cb219ed81d6bb89b841f3f98ae9717fd62002483462ff4b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/duckdb"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3926358da0100771a69369d67a38b8c5b297e1ed87053a2b0a73991435e9f4a5"
   end
 
   depends_on "cmake" => :build
