425134df312fa9680311b6be587e569a68cef069
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 17:54:00 +0800

    dory: update 1.1.1 bottle.

--- Formula/dory.rb
+++ Formula/dory.rb
@@ -6,13 +6,8 @@ class Dory < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a98dd50fdc6cabcd7f200958b32c6c8c448174b861efd576170d7ac696e57e8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cbe36a5c87731cae07c1de8b9f7e2cfc04e17bba6e1a6918678ef49f62256bad"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d97718bff64b71e82a4c538cf681a9521c7e5cbf1b79e640355c541d9082909"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
-    sha256 cellar: :any_skip_relocation, mojave:         "52197dfd1668a9c0c3c2daa1cd7e808d89bbb4ba3f3500746e60b56cbc05058b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "858fd6ba202acedbc27ffea81e407ed0d25e6d77386e2809b412fb776a07e5d9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dory"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d65c174f584ea6dc2175b35a46978526e05067367986a47e57ed5d0f0948654b"
   end
 
   depends_on "ruby@2.7"
