ee27d7361f3fc33349910415f6c8eb8b0408afa6
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 13:59:43 +0800

    docker-compose: update 2.10.2 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,14 +1,14 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.5.0.tar.gz"
-  sha256 "e002f4f50bfb1b3c937dc0a86a8a59395182fe1288e4ed3429db5771f68f7320"
+  url "https://github.com/docker/compose/archive/v2.10.2.tar.gz"
+  sha256 "74c86d544fcfb80bb2d3b58187bd017adb0e62863d22114a66c14fc94fdbc421"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/docker-compose"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "d5c7170b73054f7041ee9a441091cf7a1947a659deb22b81d87c86a325080e82"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "52ba4a7c9bd586e34f4302473ddf28ff2d7241c324bd9f89ecdacae0b3793b21"
   end
 
   depends_on "go" => :build
