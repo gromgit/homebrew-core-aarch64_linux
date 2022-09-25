29924c598ff8c5e975b137f5ca0beaa8e811024f
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 17:57:10 +0800

    dos2unix: update 7.4.3 bottle.

--- Formula/dos2unix.rb
+++ Formula/dos2unix.rb
@@ -1,9 +1,9 @@
 class Dos2unix < Formula
   desc "Convert text between DOS, UNIX, and Mac formats"
   homepage "https://waterlan.home.xs4all.nl/dos2unix.html"
-  url "https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.4.2.tar.gz"
-  mirror "https://fossies.org/linux/misc/dos2unix-7.4.2.tar.gz"
-  sha256 "6035c58df6ea2832e868b599dfa0d60ad41ca3ecc8aa27822c4b7a9789d3ae01"
+  url "https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.4.3.tar.gz"
+  mirror "https://fossies.org/linux/misc/dos2unix-7.4.3.tar.gz"
+  sha256 "b68db41956daf933828423aa30510e00c12d29ef5916e715e8d4e694fe66ca72"
   license "BSD-2-Clause"
 
   livecheck do
@@ -13,7 +13,7 @@ class Dos2unix < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dos2unix"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "144120b0993efa9567fa99001101a3205f670fde1ff7e088eae148fdd5a11ad4"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2837373b0c1f503204e634c31e7913e4e3b9b8ee32c46f66e63ec9af817cdc7d"
   end
 
   def install
