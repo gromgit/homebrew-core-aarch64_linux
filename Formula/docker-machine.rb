95872cfca6d7e26c358fd3eb3c614ef5b710e5bf
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 14:10:17 +0800

    docker-machine: update 0.16.2-gitlab.18 bottle.

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -1,23 +1,20 @@
 class DockerMachine < Formula
   desc "Create Docker hosts locally and on cloud providers"
   homepage "https://docs.docker.com/machine"
-  url "https://github.com/docker/machine.git",
-      tag:      "v0.16.2",
-      revision: "bd45ab13d88c32a3dd701485983354514abc41fa"
+  url "https://gitlab.com/gitlab-org/ci-cd/docker-machine.git",
+      tag:      "v0.16.2-gitlab.18",
+      revision: "cd8285a7e2310276c7d20575f15bba40a0678ed9"
+  version "0.16.2-gitlab.18"
   license "Apache-2.0"
-  head "https://github.com/docker/machine.git", branch: "master"
+  head "https://gitlab.com/gitlab-org/ci-cd/docker-machine.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c941d688b50d6eae302320aa5e702d5da26e4e38ceac2f925a24b6efe6c589db"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ed6a73a1d30c911811e8f6fb0e61e41bc3be4aea62bc2b77f7b6dca50b517a9"
-    sha256 cellar: :any_skip_relocation, monterey:       "9132d28324994e3908e2b3e26a5c7f3070dbb0d2061a9e37f6d8b84a76b2cc57"
-    sha256 cellar: :any_skip_relocation, big_sur:        "720ea8bbbfdc6b9d0701f02014e09f6a46e6785bcbdb36ebe3e95bddd0849dfa"
-    sha256 cellar: :any_skip_relocation, catalina:       "e27501077ccc67fc468ca8e2881366a9fc23260296ed93a3f436b4d12f41ec43"
-    sha256 cellar: :any_skip_relocation, mojave:         "0cfe7d344bd6c2b3bc0d1c1de472c430162a45dd54454b268e82750094b9cf9f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f08e7ba29eb793a79b8126631485ee4100cf07b9e1e5654a7c4db8c2d229d5af"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/docker-machine"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c4a693067b535571d809627b2b81c03b6ef0e6f6fa93e3a7a036f4ba5f7bfa8d"
   end
 
-  deprecate! date: "2021-09-30", because: :repo_archived
+  # Commented out while this formula still has dependents.
+  # deprecate! date: "2021-09-30", because: :repo_archived
 
   depends_on "automake" => :build
   depends_on "go" => :build
