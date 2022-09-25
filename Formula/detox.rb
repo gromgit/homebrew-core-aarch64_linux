92a33ff878915fcf99acb84e9270ccfb039fe875
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 10:13:34 +0800

    detox: update 1.4.5 bottle.

--- Formula/detox.rb
+++ Formula/detox.rb
@@ -6,13 +6,8 @@ class Detox < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_monterey: "5636ac88e873246ab060735d242fe4059703c6bed30bea931c63757851739dab"
-    sha256 arm64_big_sur:  "4e440bf09a205498b109d633d30216632df0657d0956ce85eb0c49224b3916e2"
-    sha256 monterey:       "6dd49020e0f4d3deeb230e098df0b231da26aa6d2e65f58ae4fcf3d5e3f26340"
-    sha256 big_sur:        "6e3621ec9c99de5bd834aeb7f1547282630355be71f74e973fe2918dba2ada85"
-    sha256 catalina:       "622b6efed1e93de18b858e6c6c4d49dac0d6f568dcad9df35ea6a7ee61356b39"
-    sha256 mojave:         "6201f2a5eb286f6f42f01d817f0eaa12c357049f6c97851984ba622aafd1641c"
-    sha256 x86_64_linux:   "8fffea4f4d51f410ae7671e659c63b35aab854cb785dd71c0c26c58586145616"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/detox"
+    sha256 aarch64_linux: "a2f869d332bcc2ae745bac6190fe5d3b5928e22f8523dddaa00d1d564632e7bf"
   end
 
   depends_on "autoconf" => :build
