2514648da63f7fad97ac1a40a10238f8ba4241af
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 13:02:31 +0800

    dnsmap: update 0.36 bottle.

--- Formula/dnsmap.rb
+++ Formula/dnsmap.rb
@@ -1,17 +1,22 @@
 class Dnsmap < Formula
   desc "Passive DNS network mapper (a.k.a. subdomains bruteforcer)"
-  homepage "https://code.google.com/archive/p/dnsmap/"
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dnsmap/dnsmap-0.30.tar.gz"
-  sha256 "fcf03a7b269b51121920ac49f7d450241306cfff23c76f3da94b03792f6becbc"
+  homepage "https://github.com/resurrecting-open-source-projects/dnsmap"
+  url "https://github.com/resurrecting-open-source-projects/dnsmap/archive/refs/tags/0.36.tar.gz"
+  sha256 "f52d6d49cbf9a60f601c919f99457f108d51ecd011c63e669d58f38d50ad853c"
+  head "https://github.com/resurrecting-open-source-projects/dnsmap.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dnsmap"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "2f56a7dbeec500487b2a703508bca20388f0a17002dcb28e1646c0ba2cc69d25"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "333e48d86ea73b6a843160f9106ca701b9825f244ef676672b2897d11420b150"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
   def install
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}",
-                   "BINDIR=#{bin}", "install"
+    system "./autogen.sh"
+    system "./configure", *std_configure_args
+    system "make", "install"
   end
 
   test do
