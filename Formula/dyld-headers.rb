65649a960fd8846bf7b80ff3a7158209397d559c
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 22:31:45 +0800

    dyld-headers: update 960 bottle.

--- Formula/dyld-headers.rb
+++ Formula/dyld-headers.rb
@@ -1,12 +1,13 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-955.tar.gz"
-  sha256 "3cf4e0b964bf1c1cb5daa78a21ea692b82358d7c5f94ebd876c14f8ca4473f43"
+  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-960.tar.gz"
+  sha256 "4d5a878221ba7e099e1d8f0ff20e816fdad9fda3587e5c5c74e2a52fcc5c41d0"
   license "APSL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bbc47bd6942602a943962154dc4a3abaf7a4499c9eb5ef4f8d9fda85e81dbff9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dyld-headers"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c6a13a03992f22866cc09bbfa20878de02343b1ddd12d724374ae13fd7c935fa"
   end
 
   keg_only :provided_by_macos
