95fa14173cc2d09fc402cb62667d1ed2b5f32cd3
Adrian Ho <the.gromgit@gmail.com>
2022-09-25 18:50:16 +0800

    driftctl: update 0.37.0 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,13 +1,13 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.29.0.tar.gz"
-  sha256 "3544e2937f11bdabba65e6e32cdc1d37dd81129002138a00cb72696b10125958"
+  url "https://github.com/snyk/driftctl/archive/v0.37.0.tar.gz"
+  sha256 "6eb05e26edd1d53182e58ae1ed7b5231bf2fb05f5b7b04ab4be39e38461b965f"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/driftctl"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "9805d20e737c155b0fdcdcb000effd46463bff568998abfe65c0a4e58c730b30"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b94da7426bb99076d8c99f13b321bf24de328364cb2ea1a3c8fbc9b986fbe967"
   end
 
   depends_on "go" => :build
@@ -23,19 +23,12 @@ class Driftctl < Formula
 
     system "go", "build", *std_go_args(ldflags: ldflags)
 
-    output = Utils.safe_popen_read("#{bin}/driftctl", "completion", "bash")
-    (bash_completion/"driftctl").write output
-
-    output = Utils.safe_popen_read("#{bin}/driftctl", "completion", "zsh")
-    (zsh_completion/"_driftctl").write output
-
-    output = Utils.safe_popen_read("#{bin}/driftctl", "completion", "fish")
-    (fish_completion/"driftctl.fish").write output
+    generate_completions_from_executable(bin/"driftctl", "completion")
   end
 
   test do
     assert_match "v#{version}", shell_output("#{bin}/driftctl version")
-    assert_match "Downloading terraform provider: aws",
+    assert_match "Could not find a way to authenticate on AWS!",
       shell_output("#{bin}/driftctl --no-version-check scan 2>&1", 2)
   end
 end
