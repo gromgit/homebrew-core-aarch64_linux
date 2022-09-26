8520e6df13cd6a31f221fd68fab3cdb55b2d452f
Rui Chen <rui@chenrui.dev>
2022-09-26 17:17:36 -0400

    nebula 1.6.1
    Closes #111733.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nebula.rb
+++ Formula/nebula.rb
@@ -1,8 +1,8 @@
 class Nebula < Formula
   desc "Scalable overlay networking tool for connecting computers anywhere"
   homepage "https://github.com/slackhq/nebula"
-  url "https://github.com/slackhq/nebula/archive/v1.6.0.tar.gz"
-  sha256 "b16638b99d80a4ae6373f7757a0064dc0defd3f9e165617e7b5c3be9e64d3605"
+  url "https://github.com/slackhq/nebula/archive/v1.6.1.tar.gz"
+  sha256 "9c343d998d2eab9473c3bf73d434b8a382d90b1f73095dd1114ecaf2e1c0970f"
   license "MIT"
 
   bottle do
