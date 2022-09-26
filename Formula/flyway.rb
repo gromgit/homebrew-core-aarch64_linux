45f15445c3f53d0a36c11e0ff75e5fb06e306764
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 04:06:31 +0800

    flyway: update 9.2.2 bottle.

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/8.5.10/flyway-commandline-8.5.10.tar.gz"
-  sha256 "b0729831618ff4ffb8b92fae1f6d65d8f16adeaecaa287c9b428b68b1ea7749f"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.2.2/flyway-commandline-9.2.2.tar.gz"
+  sha256 "687b017b2d56ce646172b09e1de01e56e12f5f889ac5b21bfc2ecdead08ecae6"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,8 @@ class Flyway < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a62ea6ff395cf5029c236279d1e701f7d35504784a13342ee2329e1896d4f1d8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/flyway"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e86114c01bfe0314ce69dab10ca634e369b2f943fd7ac9023be6352063a041de"
   end
 
   depends_on "openjdk"
