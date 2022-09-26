554e069bffbedfc1df66f5ea68816d85cf450b7a
Porkepix <porkepix@gmail.com>
2022-09-26 02:57:21 +0200

    plantuml 1.2022.8
    Closes #111684.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2022.7/plantuml.1.2022.7.jar"
-  sha256 "e30d9c8bc68b285590f9ec00ce88870270eb83d86aadd167d2ee2717ec4cb787"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2022.8/plantuml.1.2022.8.jar"
+  sha256 "9e8472b15e9deef57f57a49db1548d777d7a9fd87c0c02f27c383af260a89e53"
   license "GPL-3.0-or-later"
   version_scheme 1
 
