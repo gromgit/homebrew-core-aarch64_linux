97b71f356ee384fa2b76ff7d505c0f730ce1d100
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 07:27:49 +0800

    frugal: update 3.16.2 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -1,13 +1,13 @@
 class Frugal < Formula
   desc "Cross language code generator for creating scalable microservices"
   homepage "https://github.com/Workiva/frugal"
-  url "https://github.com/Workiva/frugal/archive/v3.15.0.tar.gz"
-  sha256 "a7f97de2dd6765bf166d5763a44bdc30cc067b06d3f236cb1715c3bed5a0e557"
+  url "https://github.com/Workiva/frugal/archive/v3.16.2.tar.gz"
+  sha256 "ad0b8b04dc82c30f70251bb2253f0678146c1546dbb874d01e1249d702b230bc"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/frugal"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "2e08c8d010a2d8615d4f7bcfa416eea65cc12c239a7ac7225e2c322e9c9d2a76"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "380c7e60a934e8cf3dbf19ff6997aabc4fa6d96d1c89f2abd6b8c9c78b854e73"
   end
 
   depends_on "go" => :build
