1f8e8a74be9bd8e095b72da0b7e70d3bc259bc9f
Michael Cho <cho-m@tuta.io>
2022-09-25 20:48:33 -0700

    augeas 1.13.0
    Closes #111692.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/augeas.rb
+++ Formula/augeas.rb
@@ -1,10 +1,19 @@
 class Augeas < Formula
   desc "Configuration editing tool and API"
   homepage "https://augeas.net/"
-  url "http://download.augeas.net/augeas-1.12.0.tar.gz"
-  sha256 "321942c9cc32185e2e9cb72d0a70eea106635b50269075aca6714e3ec282cb87"
   license "LGPL-2.1"
-  revision 1
+  head "https://github.com/hercules-team/augeas.git", branch: "master"
+
+  stable do
+    url "https://github.com/hercules-team/augeas/releases/download/release-1.13.0/augeas-1.13.0.tar.gz"
+    sha256 "5002f33f42365ab78be974609a0f3b76a4c277fc404ec79f516305cab5ce5de1"
+
+    # Replace deprecated 'security_context_t' with 'char *'. Remove in the next release.
+    patch do
+      url "https://github.com/hercules-team/augeas/commit/f38398a2d07028b892eac59449a35e1a3d645fac.patch?full_index=1"
+      sha256 "1697379e0676edf94346a3377a75c871d1d0d033e3a37a29d69ae66f6e57553a"
+    end
+  end
 
   livecheck do
     url "http://download.augeas.net/"
@@ -21,10 +30,6 @@ class Augeas < Formula
     sha256 x86_64_linux:   "1d769bb0e887b5a321e5c89d15959e71fc7d616fb79c3e2e926ff2d1cefc106f"
   end
 
-  head do
-    url "https://github.com/hercules-team/augeas.git"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bison" => :build
@@ -35,15 +40,13 @@ class Augeas < Formula
   uses_from_macos "libxml2"
 
   def install
-    args = %W[--disable-debug --disable-dependency-tracking --prefix=#{prefix}]
-
     if build.head?
-      system "./autogen.sh", *args
+      system "./autogen.sh", *std_configure_args
     else
       # autoreconf is needed to work around
       # https://debbugs.gnu.org/cgi/bugreport.cgi?bug=44605.
       system "autoreconf", "--force", "--install"
-      system "./configure", *args
+      system "./configure", *std_configure_args
     end
 
     system "make", "install"
