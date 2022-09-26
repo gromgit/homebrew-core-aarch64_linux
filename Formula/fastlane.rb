037aa60873f84d614b328114af740f202199d31d
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 16:54:55 +0800

    fastlane: update 2.209.1 bottle.

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.205.2.tar.gz"
-  sha256 "b2f89b1383a64877b3daa9cdeb93f6f33bd712d3dd5437adadc7d5d5278b39fa"
+  url "https://github.com/fastlane/fastlane/archive/2.209.1.tar.gz"
+  sha256 "06283d22e5978433b7c015e4f6af3b02794bc67a317ab3fa617cafb31498b071"
   license "MIT"
   head "https://github.com/fastlane/fastlane.git", branch: "master"
 
@@ -12,12 +12,8 @@ class Fastlane < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f4acc17c6df13e0e684359e1a8d289b1292b7ef783e46a04073f43a7584bc44f"
-    sha256 cellar: :any,                 arm64_big_sur:  "e8e7c0a51fc5bedfb195471daaeba3af48ce5ce0e9bd3739eb9dd7e638e7edf4"
-    sha256 cellar: :any,                 monterey:       "5a56962728d8ccd503efb36df6480b4c9f0b4f51559262a70d87ce9007c7d7c5"
-    sha256 cellar: :any,                 big_sur:        "12ef4fddff602b87da3b1f76b57c8bfe5e05bee49ae4bc05cb70cb95ee7dc490"
-    sha256 cellar: :any,                 catalina:       "54f827f9087d88a5c27e070c688d1fbdffb30d4018d6ecc493b84d9e327b1a66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40c79f277bef84ab23c835ef780b8516abb98fa750524c88b7981021e4dc99fb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fastlane"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "db04e882bf1c17615a43e01e8d30e2cc884c932223dd02da6842fe9b4c227057"
   end
 
   depends_on "ruby"
