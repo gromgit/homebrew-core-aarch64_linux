024452b27b144a842300b49514626380e34ee43e
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 17:19:30 +0800

    fatsort: update 1.6.4 bottle.

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -12,13 +12,8 @@ class Fatsort < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b7d4a8f9701c12a1d3dd1acf54c007e7e554c139e13b1ec0038ef9c1203cb77e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8b9d29782c670f94450fadca0c8f79027da00b123811f405b0d3b09075ab8d5c"
-    sha256 cellar: :any_skip_relocation, monterey:       "422321e0d2f3d63e9d547a7e5b647cb6da4ba62f633c97aa065bfcbeb346a06c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "01b749fcfecc1b3a506b7f4e61562625861d693086803b1251491027be95e682"
-    sha256 cellar: :any_skip_relocation, catalina:       "956dec116bc78d27db0553cf8f1bb9dcb499e22d7d162dfc1cd9ec13acc66412"
-    sha256 cellar: :any_skip_relocation, mojave:         "8e3d9f4a4a63557db3aa3016a611bf96f4a1c46e15b305cbc803bce2036b28f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56f58efa4edaa7205e96e9267d2066dcb56e271c3e9140dd2edbaa63dc9ae617"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fatsort"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5ab7c54b033414319aee7d6847cd8e4b452e193a4fcc893439d4131af6a3d224"
   end
 
   depends_on "help2man"
