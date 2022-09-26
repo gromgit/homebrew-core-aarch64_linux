43bff164b41c8ae04f89b2d0e6e5f57c89e7c0a4
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 13:10:08 +0800

    epinio: update 1.1.0 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -1,16 +1,16 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "3216304faef81e7668cf55f98c39ca5aef65bc3bf7a34dc8e3a9e582ba472e49"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "2066d00a6d91cc1b5410b90cad4f88d8e1be9dc4c013db50e80c516bb25c07f7"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/epinio"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "d894c9c40c922cb4befb7f11f8601b7a0c33bd2eb30f286f5d0e3293d4bf98a6"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ada1882d879960a6eb5d1626b68eb444c18102b8e2c7b67abd3d4f69023abe7c"
   end
 
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/epinio/epinio/internal/version.Version=#{version}")
@@ -20,7 +20,7 @@ class Epinio < Formula
     output = shell_output("#{bin}/epinio version 2>&1")
     assert_match "Epinio Version: #{version}", output
 
-    output = shell_output("#{bin}/epinio settings update 2>&1")
+    output = shell_output("#{bin}/epinio settings update-ca 2>&1")
     assert_match "failed to get kube config", output
     assert_match "no configuration has been provided", output
   end
