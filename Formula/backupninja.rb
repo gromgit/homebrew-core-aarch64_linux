a0d9ddf5c7ac907c15aee89c330616b0f69991c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-26 00:19:09 +0000

    backupninja: update 1.2.1 bottle.

--- Formula/backupninja.rb
+++ Formula/backupninja.rb
@@ -12,15 +12,12 @@ class Backupninja < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "87d0526c41bc181f8c0064bfa5807572d4d4f7cf6dc134ef895e146e16d48558"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "afe1343c5eb5d63d9aa242b75965b90f6ee749283b04e9f51db125085d0caec5"
-    sha256 cellar: :any_skip_relocation, monterey:       "b64703392bc6668074756ec577a8ba62a5c9d67704a17153061fa81bb104f65a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4e0e81d5f30ac2a634fb6ac26c3d9efcc168b810ebe07eefc899411feda1710a"
-    sha256 cellar: :any_skip_relocation, catalina:       "10006896f517296c2a62d1f510d3795afa2777f24d401206cfb69ce06bdf5d3c"
-    sha256 cellar: :any_skip_relocation, mojave:         "39df0693351a58ac9f406d6c16ab9c15ddb5d96ddf7ac9e98cf94061ccffe9a8"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "39df0693351a58ac9f406d6c16ab9c15ddb5d96ddf7ac9e98cf94061ccffe9a8"
-    sha256 cellar: :any_skip_relocation, sierra:         "071ac37b853475ae44c4b3dde995d694a5fce322e68d7eeb39eb52a85c781cea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "322a0ff49ad63c3960f46ef07b5326b0d7e31e737009bb560bcc6ed40caed591"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c8f559e4cb9deca4861b7beff6083a5da8d70597d99b4ec6d2c447e57d5331c6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8f559e4cb9deca4861b7beff6083a5da8d70597d99b4ec6d2c447e57d5331c6"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e0f3c562e13469570fa3852a72b05745f4ebc4154e306fdf152d33d3b79c0a5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3e0f3c562e13469570fa3852a72b05745f4ebc4154e306fdf152d33d3b79c0a5"
+    sha256 cellar: :any_skip_relocation, catalina:       "3e0f3c562e13469570fa3852a72b05745f4ebc4154e306fdf152d33d3b79c0a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8f559e4cb9deca4861b7beff6083a5da8d70597d99b4ec6d2c447e57d5331c6"
   end
 
   depends_on "bash"
