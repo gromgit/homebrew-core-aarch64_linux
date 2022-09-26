58be73524a91f8b2cd677b82e11e55f619e291f8
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 12:27:03 +0800

    enkits: update 1.11 bottle.

--- Formula/enkits.rb
+++ Formula/enkits.rb
@@ -6,12 +6,8 @@ class Enkits < Formula
   license "Zlib"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d73910b2e106da68ef13675c76e4d18a265b2c514f898a1e3ab557a5a7d52caa"
-    sha256 cellar: :any,                 arm64_big_sur:  "6adefc872b396df076df8123bd17d61dd0fc1309dea9b3699df3ade05b28c578"
-    sha256 cellar: :any,                 monterey:       "cfd9a6e5f1c88e091ae948867231254e177bbce5c04acc48a05117dd16ff9ea8"
-    sha256 cellar: :any,                 big_sur:        "d8a9e68c02f80beb48beb2f8b39bf12bebb5077682cc623fa25244ce9ca0364f"
-    sha256 cellar: :any,                 catalina:       "675a3d006f1afd2efdcb396b178160fbfa64a40799e12d5a58364d6fa468cff7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb5764d88d00d8df8970a0253f177b70a4d11535dfcf87a8f98a984cc81d0b1a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/enkits"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8742e13a932c3751e5dac800935f8b72a655d53ac9ba97dbe9c27c99b22a8e80"
   end
 
   depends_on "cmake" => :build
