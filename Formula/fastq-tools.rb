d45831b8f6ecaa94e248e9799358deee31b5820a
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 17:07:48 +0800

    fastq-tools: update 0.8.3 bottle.

--- Formula/fastq-tools.rb
+++ Formula/fastq-tools.rb
@@ -6,13 +6,8 @@ class FastqTools < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8580b8ff6e5de04a060b60b5251d01fad27a25c8c4e1b1afdc9534e9ae445cdc"
-    sha256 cellar: :any,                 arm64_big_sur:  "ac48791014e14979ad786e59178d0b468510d02f5d51a86608b388adad4405f1"
-    sha256 cellar: :any,                 monterey:       "0ead212cb078edbf77f9e58d4186dd4aac103fadd8291c6bc328312cf6383b4c"
-    sha256 cellar: :any,                 big_sur:        "18f3e795ec5c2c182bfc995ce662816cf17ccbd719fef30937f5456d28bbccc5"
-    sha256 cellar: :any,                 catalina:       "20105d6a89abbf493ceeedfc29a956b9b4cc90bf2843cf73e544b03c0456b0ad"
-    sha256 cellar: :any,                 mojave:         "adf7e00692719889bdaee28870700857b9e1b0ec096b62f48e76eac5ac27f52c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "abd98e72c698e16a077443d29259f946f7ac36c6e5f5a6e172be5466c4cbf2cd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fastq-tools"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "734805bdd6105fa0ee5a89ddecc5c4d614db0c4e71024a83806b6ce68dccfd4d"
   end
 
   depends_on "autoconf" => :build
