e0d87ca64bd9e2b1756b80e18fa006ce8be75ea9
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 03:05:52 +0800

    flow-cli: update 0.39.3 bottle.

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -1,8 +1,8 @@
 class FlowCli < Formula
   desc "Command-line interface that provides utilities for building Flow applications"
   homepage "https://onflow.org"
-  url "https://github.com/onflow/flow-cli/archive/v0.33.1.tar.gz"
-  sha256 "c4ca8713cdb85b62d83005935d5dd60a22805f9ceb82ed5f2e6c43d9f382adf9"
+  url "https://github.com/onflow/flow-cli/archive/v0.39.3.tar.gz"
+  sha256 "e51d1db4c01147ca3f94c63c03d532c733055aa7bba608559700f3aac957a5f3"
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git", branch: "master"
 
@@ -13,7 +13,7 @@ class FlowCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/flow-cli"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "01731ef727173f7735f119129ea00e954ae5f705fe3b87745fa8740dadc06c39"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "becd72a33d6b064c2e3fcc42aaef0a038e294a5af8e0f64eccc603dc301adafc"
   end
 
   depends_on "go" => :build
