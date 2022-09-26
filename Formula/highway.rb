e7316e734e0e73874062e08fc98bd4a8f84c18f7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-26 18:13:00 +0800

    highway 1.0.1 (new formula)
    This can be used to replace the `highway` resource in `jpeg-xl`:
    
        https://github.com/Homebrew/homebrew-core/blob/c75ac607ce6a019d41287e3734ed6654d4acad3d/Formula/jpeg-xl.rb#L40-L43

---
+++ Formula/highway.rb
@@ -0,0 +1,32 @@
+class Highway < Formula
+  desc "Performance-portable, length-agnostic SIMD with runtime dispatch"
+  homepage "https://github.com/google/highway"
+  url "https://github.com/google/highway/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "7ca6af7dc2e3e054de9e17b9dfd88609a7fd202812b1c216f43cc41647c97311"
+  license "Apache-2.0"
+  head "https://github.com/google/highway.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  # These used to be bundled with `jpeg-xl`.
+  link_overwrite "include/hwy/*", "lib/pkgconfig/libhwy*"
+
+  def install
+    ENV.runtime_cpu_detection
+    system "cmake", "-S", ".", "-B", "builddir",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DHWY_ENABLE_TESTS=OFF",
+                    "-DHWY_ENABLE_EXAMPLES=OFF",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+    (share/"hwy").install "hwy/examples"
+  end
+
+  test do
+    system ENV.cxx, "-std=c++11", "-I#{share}", "-I#{include}",
+                    share/"hwy/examples/benchmark.cc", "-L#{lib}", "-lhwy"
+    system "./a.out"
+  end
+end
