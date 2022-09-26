27b82efe1dac03b0ab9f9b96d332fd50f5ef3c7c
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 04:11:00 +0800

    fnlfmt: update 0.2.3 bottle.

--- Formula/fnlfmt.rb
+++ Formula/fnlfmt.rb
@@ -1,12 +1,13 @@
 class Fnlfmt < Formula
   desc "Formatter for Fennel code"
   homepage "https://git.sr.ht/~technomancy/fnlfmt"
-  url "https://git.sr.ht/~technomancy/fnlfmt/archive/0.2.2.tar.gz"
-  sha256 "9e14d6750495c78daa7a474ff2d44bccee260e3d39e55dd61d86986e75bd9def"
+  url "https://git.sr.ht/~technomancy/fnlfmt/archive/0.2.3.tar.gz"
+  sha256 "0a7d7aaf4d88a81b6f13ed02fa88e74ed6c634ba50254de09eeb5a7777ae90d6"
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "18b456dcb6cc242942c5677c9b8ab8e74274426f5eba01038819dd5b71f9db79"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fnlfmt"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0698e6f840f6682f69eeb95da63d3ba414acdf3604efcbaf9a3011b4a8259280"
   end
 
   depends_on "lua"
