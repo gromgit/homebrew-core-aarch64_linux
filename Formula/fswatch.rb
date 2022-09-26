7af6225621a6b37ce60d375b63e37e1ef71a54e9
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 07:45:45 +0800

    fswatch: update 1.17.1 bottle.

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -1,8 +1,8 @@
 class Fswatch < Formula
   desc "Monitor a directory for changes and run a shell command"
   homepage "https://github.com/emcrisostomo/fswatch"
-  url "https://github.com/emcrisostomo/fswatch/releases/download/1.16.0/fswatch-1.16.0.tar.gz"
-  sha256 "95ece85eb01af71e99afef0173755fcedb737b639163f8efc7fed674f6f5372f"
+  url "https://github.com/emcrisostomo/fswatch/releases/download/1.17.1/fswatch-1.17.1.tar.gz"
+  sha256 "c38e341c567f5f16bfa64b72fc48bba5e93873d8572522e670e6f320bbc2122f"
   license all_of: ["GPL-3.0-or-later", "Apache-2.0"]
 
   livecheck do
@@ -12,7 +12,7 @@ class Fswatch < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fswatch"
-    sha256 aarch64_linux: "1b27de6c29ec8a01a3243b3984cd63609adb41e37557b3b4ebb389b981cdf1d9"
+    sha256 aarch64_linux: "ab273aee4991e2db33720bcad43b2c1a5d081e1db0bed966fc6306273b75eed6"
   end
 
   def install
