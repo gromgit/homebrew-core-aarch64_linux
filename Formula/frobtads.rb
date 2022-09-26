4a6d7465c62a67f0c909afc33b3d4da5c3493934
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 07:21:34 +0800

    frobtads: update 2.0 bottle.

--- Formula/frobtads.rb
+++ Formula/frobtads.rb
@@ -5,13 +5,8 @@ class Frobtads < Formula
   sha256 "893bd3fd77dfdc8bfe8a96e8d7bfac693da0e4278871f10fe7faa59cc239a090"
 
   bottle do
-    sha256 arm64_monterey: "0d58231276310d6f781bf35dcc430fa6495fe40222248cf6763f8042e92b87ec"
-    sha256 arm64_big_sur:  "c73a8dc0cb339a9f039ccedd7950372a0c2fa136f11d59ac7d0f0cc03a2a1651"
-    sha256 monterey:       "4238e7e1b6e4bc87e4b23ad9e73f5b801a5aabc29301ab5cbaaa332176a47fdb"
-    sha256 big_sur:        "b58665483ecdcaa4ebca91dc048884c6168dfc9d0265bd7bb62bc7a57f1814ed"
-    sha256 catalina:       "9f7593c956b859ba6592f85e9741e04c2092e13ab4d689548bf5d4d92042501f"
-    sha256 mojave:         "944c0aabe26e083c818100225920aa3c2d967bf07bf8a59d58455c92b99f61d6"
-    sha256 x86_64_linux:   "1874c9c656d5e4a542300baca25b3d28f6e841f3fed9b123acbd958417874495"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/frobtads"
+    sha256 aarch64_linux: "46718e74cb3722e7d6ca57cab177c7e15424be6039bc7f64ffa6c617b7dd7fb7"
   end
 
   depends_on "cmake" => :build
