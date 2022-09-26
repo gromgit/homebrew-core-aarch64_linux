2fed50baa1efa714d438a79038d2d1c6583d1f87
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 12:58:57 +0800

    epic5: update 2.1.12 bottle.

--- Formula/epic5.rb
+++ Formula/epic5.rb
@@ -1,9 +1,9 @@
 class Epic5 < Formula
   desc "Enhanced, programmable IRC client"
   homepage "http://www.epicsol.org/"
-  url "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/epic5-2.1.7.tar.xz"
-  mirror "https://www.mirrorservice.org/sites/distfiles.macports.org/epic5/epic5-2.1.7.tar.xz"
-  sha256 "71627b14e26390f1e216047f40ca5ee1e7d55651667787466433bf7abdb6e317"
+  url "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/epic5-2.1.12.tar.xz"
+  mirror "https://www.mirrorservice.org/sites/distfiles.macports.org/epic5/epic5-2.1.12.tar.xz"
+  sha256 "c89ae4fc014ddcf0084b748e22f47b46a85ab1ac9bf0b22560360ba8626b6da6"
   license "BSD-3-Clause"
   head "http://git.epicsol.org/epic5.git", branch: "master"
 
@@ -14,7 +14,7 @@ class Epic5 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/epic5"
-    sha256 aarch64_linux: "83b72403e2f2a79108da8faaea790954597ac9f92d7081b2ba453a978f3ec9ab"
+    sha256 aarch64_linux: "833ca7c0c7fa92f24bcbf626e281150122083bb0308034a8e36a242ff142f4da"
   end
 
   depends_on "openssl@1.1"
