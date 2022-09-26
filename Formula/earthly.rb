fb6b8c7c6a20d213540f28e64f3f393ac286b71d
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 10:03:28 +0800

    earthly: update 0.6.22 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -1,47 +1,42 @@
 class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://earthly.dev/"
-  url "https://github.com/earthly/earthly/archive/v0.5.17.tar.gz"
-  sha256 "1dcc56b419413480fa2e116606cab2c8003483e0b8052443aa7b7da0572ce47f"
-  license "BUSL-1.1"
+  url "https://github.com/earthly/earthly.git",
+      tag:      "v0.6.22",
+      revision: "1c8bcfbab64b9b52302ef63c897e6b989b88a794"
+  license "MPL-2.0"
   head "https://github.com/earthly/earthly.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6ced4b644da7733596ddb225d4d07dddcdf3cea9975a1dcdce724e65093142fb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "45b1406d85fbc167590873e727dd63624ed17bceadc289bb4c6c7f8e8a669317"
-    sha256 cellar: :any_skip_relocation, catalina:      "16c593502fd9a7270edab13a2ed8c9ca44486eb90bc97dceef99ec8c092ddadf"
-    sha256 cellar: :any_skip_relocation, mojave:        "a9a09599ccebca0c987ea802cfc861097055ab2662db97c417bfeb83756fdb90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78f6b33643db61f1944ceaf27d27ad001f8ba7a865b358a040cf1374fd618a96"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/earthly"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "25e2ddc8257e626be9b49aad2b0bd879145324acfc8eea8bacc9d083e89b358b"
   end
-
-  disable! date: "2021-07-15", because: "has an incompatible license"
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-X main.DefaultBuildkitdImage=earthly/buildkitd:v#{version} -X main.Version=v#{version} " \
-              "-X main.GitSha=bdeda2542465cb0bc0c8985a905aa2e3579a3f7b "
+    ldflags = %W[
+      -s -w
+      -X main.DefaultBuildkitdImage=earthly/buildkitd:v#{version}
+      -X main.Version=v#{version}
+      -X main.GitSha=#{Utils.git_head}
+      -X main.BuiltBy=homebrew
+    ]
     tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork"
-    system "go", "build",
-        "-tags", tags,
-        "-ldflags", ldflags,
-        *std_go_args,
-        "./cmd/earthly/main.go"
+    system "go", "build", "-tags", tags, *std_go_args(ldflags: ldflags), "./cmd/earthly"
 
-    bash_output = Utils.safe_popen_read("#{bin}/earthly", "bootstrap", "--source", "bash")
-    (bash_completion/"earthly").write bash_output
-    zsh_output = Utils.safe_popen_read("#{bin}/earthly", "bootstrap", "--source", "zsh")
-    (zsh_completion/"_earthly").write zsh_output
+    generate_completions_from_executable(bin/"earthly", "bootstrap", "--source", shells: [:bash, :zsh])
   end
 
   test do
-    (testpath/"build.earthly").write <<~EOS
-
-      default:
+    # earthly requires docker to run; therefore doing a complete end-to-end test here is not
+    # possible; however the "earthly ls" command is able to run without docker.
+    (testpath/"Earthfile").write <<~EOS
+      VERSION 0.6
+      mytesttarget:
       \tRUN echo Homebrew
     EOS
-
-    output = shell_output("#{bin}/earthly --buildkit-host 127.0.0.1 +default 2>&1", 6).strip
-    assert_match "buildkitd failed to start", output
+    output = shell_output("#{bin}/earthly ls")
+    assert_match "+mytesttarget", output
   end
 end
