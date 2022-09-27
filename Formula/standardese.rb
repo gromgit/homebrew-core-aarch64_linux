f9d45f433c70c5aa300e857b215833b0f6623326
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-27 21:16:21 +0800

    standardese: revision bump (boost 1.80.0)

--- Formula/standardese.rb
+++ Formula/standardese.rb
@@ -3,7 +3,7 @@ class Standardese < Formula
   homepage "https://standardese.github.io"
   # TODO: use resource blocks for vendored deps
   license "MIT"
-  revision 9
+  revision 10
   head "https://github.com/standardese/standardese.git", branch: "master"
 
   # Remove stable block when patch is no longer needed.
