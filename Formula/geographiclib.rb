49319138762698f6c34a0201434994f4f2e2e872
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 05:20:16 +0800

    geographiclib: update 2.1.1 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -1,17 +1,13 @@
 class Geographiclib < Formula
   desc "C++ geography library"
   homepage "https://geographiclib.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/geographiclib/distrib-C++/GeographicLib-2.0.tar.gz"
-  sha256 "906b862aa9e988534fd5b8d9f3bae07437e0079a4236e19942ab61fe8c83960b"
+  url "https://downloads.sourceforge.net/project/geographiclib/distrib-C++/GeographicLib-2.1.1.tar.gz"
+  sha256 "28080fc48e1c76560eb2f8c306404de80c13d35687f676ff47a51695506e4a0a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3fc3f421dddd658cc96b68c967532c25d055b9a859bda415c701955f72d12542"
-    sha256 cellar: :any,                 arm64_big_sur:  "e132e08e23c5085c1e19f93031b0cc026865e4982f7f7ddcbf0e119294424c4a"
-    sha256 cellar: :any,                 monterey:       "098f8b7de3b85fb22e4d2f576765c6a697d4f7521fb2c3ee08f4ef6c6611fff9"
-    sha256 cellar: :any,                 big_sur:        "0557d7dc687a21e488ab5d9312006ab575bc6fe889f4d73a2d7e5fdecb637060"
-    sha256 cellar: :any,                 catalina:       "34d13afd308b36029e264f082a65b88cf4deb16ba34b1060f9ed12bc7d3395b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9c16104286c395af78eb7ec2c9be9050676bc753815d0b6b83d144a559257920"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/geographiclib"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1218f12cff88ccc42063cc356813fba4255aadc447ca89c6d473cc1382a6f360"
   end
 
   depends_on "cmake" => :build
