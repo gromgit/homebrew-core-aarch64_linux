a9db6cedf8462cdd5d2a593ed716eae98a7afb10
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-27 08:53:47 +0000

    snowflake: update 2.3.1 bottle.

--- Formula/snowflake.rb
+++ Formula/snowflake.rb
@@ -7,12 +7,12 @@ class Snowflake < Formula
   head "https://git.torproject.org/pluggable-transports/snowflake.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1111591b910c86ac7a8c045c37ae337cbddc0a1cc8de63a8f735fd06bdab3d8e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05d392f2c7416ede06416960475e89742fdcea225beeab57253dd5ec919a149e"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f15836a855ef013487689194c0cfe2436455d5c7760ae90420dc7097e106e77"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d7725e9f625de97e909213d414c288dbabf4a329901b8c4317fac886a49ba41a"
-    sha256 cellar: :any_skip_relocation, catalina:       "bb6a1660ed47a3b43848d6d295b3eb6f60ac61cf48f86d9e84be98c3b93ef2ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d060708a684062e7530f3cd486ff0ded5251f75aaefefc156cc01bc49865202d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ca274c6678b71d1ea47f0e5b6ee0b873a6a2826da634186225f0f0d37f2459f3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2836b655208a177b5459630bab88560aed22b704d5bf2f621ddcaeec9838fb9"
+    sha256 cellar: :any_skip_relocation, monterey:       "383ca25f5e69b9f5aa9458816c4eb1730a5907de5e7fc1a55d65089cfff5224b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "61a06facc6a248c6590ac9d82d5ce1165cf8313a131e3c825442b7a882d0bd2b"
+    sha256 cellar: :any_skip_relocation, catalina:       "bf26de9c93dfd2f49a9d9e8a37c785b41cf42eb460e786e51d4cbeec86828f12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f20ca5db9a0d6d3e1fa6906a1d91622367b3d8306dd36c767a2da5a00efe0c6"
   end
 
   depends_on "go" => :build
