280604743991d0657c91f661e0ce0b22bb8f8da0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-27 08:53:25 +0000

    oras: update 0.15.0 bottle.

--- Formula/oras.rb
+++ Formula/oras.rb
@@ -6,12 +6,12 @@ class Oras < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "295dca4d63c8237aa2997cbf7f1d4c4127f11d1d02371402eef872f7bee2b147"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2c888364c1776c6ac9ecfca313d0120588af1db23823797d333cf4913b9c5bf9"
-    sha256 cellar: :any_skip_relocation, monterey:       "6ccd5cc4ec66cbc091ea0cd70b8dccf13e9cfe5153bf0f2fe10b3179d4ffee77"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7e94939922ab6f2eed00190ece18cd033a441a22810d90aaa46cead418d3244a"
-    sha256 cellar: :any_skip_relocation, catalina:       "67191f5046451d48e8daf57f7d0b5b87f749f8bb4f4b6030bc172988747319cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85d93c180e60d0da16b814f79d3458c0c7fc2a449546d8074ed4b04576b2a1c8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2a7b233c3a29b6a396f2efe1a5f74d63825f52e5d6fd60927a9965bca682c3d2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c8c747631a3ecb103ac4a464b2ea08414a766fe245b4fe7207e4aca1fe3afa5"
+    sha256 cellar: :any_skip_relocation, monterey:       "02bda84966ef1bf5e9ca10c2a603b167367d2641ff9ff862c9be2187cd02232a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "12551fa46c04d1add6671d15a299a0aceb6a4f4fe94a40c167c7ad5d53614024"
+    sha256 cellar: :any_skip_relocation, catalina:       "784f79b53a54993958df9a24d4efab705cccaa3c60e774daeb658d0fb217d894"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b740f00aa9aeb063f8e6fada3313902b667a7d7c50b714d228ad356ddbba1e71"
   end
 
   depends_on "go" => :build
