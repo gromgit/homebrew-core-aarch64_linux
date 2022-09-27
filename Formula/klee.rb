c60ea7042e6cb3667948068f5b715a6faf8bc5cc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-27 08:01:16 +0800

    klee: revision bump (cryptominisat 5.11.2)

--- Formula/klee.rb
+++ Formula/klee.rb
@@ -4,6 +4,7 @@ class Klee < Formula
   desc "Symbolic Execution Engine"
   homepage "https://klee.github.io/"
   license "NCSA"
+  revision 1
   head "https://github.com/klee/klee.git", branch: "master"
 
   stable do
