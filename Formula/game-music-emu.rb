b066bd61a5a36430ea05f26f7e49f91a28a1b7b3
Adrian Ho <the.gromgit@gmail.com>
2022-09-27 09:27:48 +0800

    game-music-emu: update 0.6.3_2 bottle.

--- Formula/game-music-emu.rb
+++ Formula/game-music-emu.rb
@@ -8,14 +8,8 @@ class GameMusicEmu < Formula
   head "https://bitbucket.org/mpyne/game-music-emu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "1346614b5a9561f7eaace297b5493eeb99ec4c3e561acc65669ca6dbb0cd6793"
-    sha256 cellar: :any,                 arm64_big_sur:  "e83fbee26086cc93f7d2eed7d3b93f00a0a0c9eb9d59abf3aba91216fe89d3d8"
-    sha256 cellar: :any,                 monterey:       "7b1e5a6934c8ff16fff726c1963e465abd11458f5773f26b38ce8771da3289a1"
-    sha256 cellar: :any,                 big_sur:        "a0abdc4c5ae05ea22ad3627a1a717ed8a1a137065188b995858c0f301dfda640"
-    sha256 cellar: :any,                 catalina:       "ee658e16c3d9d0061b0b930ca387a1cb2fa6b6b50d23c9f6f4ae7799ddb6f46d"
-    sha256 cellar: :any,                 mojave:         "754ab0c8bc0a6de76adcb56a59913c930196e8e44154958081c093fb7763edad"
-    sha256 cellar: :any,                 high_sierra:    "596497823bb1ebb30f20fa01c8656bb15544c12fad5d67c4de165f9ef3122e68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "08b3b37367e9fada0881a14a96a8970eecb79426a378d02dc7ed8881923096f3"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/game-music-emu"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4143bb45af97eefc94f0b765372ce29afff3e2f2d84415f04eeb0fa7b7434cb1"
   end
 
   depends_on "cmake" => :build
