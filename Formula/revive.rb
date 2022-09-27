353e951eaf8a7bb3a3a5fb27bac9ca5e2e8fc679
Porkepix <porkepix@gmail.com>
2022-09-28 01:09:53 +0200

    revive 1.2.4
    Closes #111840.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/revive.rb
+++ Formula/revive.rb
@@ -2,8 +2,8 @@ class Revive < Formula
   desc "Fast, configurable, extensible, flexible, and beautiful linter for Go"
   homepage "https://revive.run"
   url "https://github.com/mgechev/revive.git",
-      tag:      "v1.2.3",
-      revision: "556d4852f934a726fae0e8cb3d1dcdaef8bf2440"
+      tag:      "v1.2.4",
+      revision: "3116818e5957e649afda1e2762c4b0bb1fa9736c"
   license "MIT"
   head "https://github.com/mgechev/revive.git", branch: "master"
 
