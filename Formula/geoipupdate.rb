b9eef656dd68cad02330243f3bf6f576c37938d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-27 08:53:41 +0000

    geoipupdate: update 4.10.0 bottle.

--- Formula/geoipupdate.rb
+++ Formula/geoipupdate.rb
@@ -7,12 +7,12 @@ class Geoipupdate < Formula
   head "https://github.com/maxmind/geoipupdate.git", branch: "main"
 
   bottle do
-    sha256 arm64_monterey: "304bb59cdd78e93f82fa132e9cdb52176fc3dab7c7780ba6ba76115a6c708e1a"
-    sha256 arm64_big_sur:  "0ddd44ee3db354ba26d8b15031fb366366e0de4aa580c5db3ff60f0a4f83888a"
-    sha256 monterey:       "01040a579d4d17c095333fc706434d542d6dfd9bec6b4e6d7e039e285a9a9f6f"
-    sha256 big_sur:        "2c1a3cb140c23ec20b0736b53de2e2036d03fabcf385414a7cd79b4b301f340d"
-    sha256 catalina:       "d85ec718f4bd27036a505a5bc995cc25998aa7a9d2cb6006fc74e0a8050e86b2"
-    sha256 x86_64_linux:   "80150859d8042cc5d70165c637b6fe20b2c547d867f4944826a04e938faacb73"
+    sha256 arm64_monterey: "fa1c58550c7a922498c844a0ab9b94edcc3dfd790b9ea4c02380ef11d47f5a5a"
+    sha256 arm64_big_sur:  "ef5c0597ef2faf8809975df62ba4438e15768683f043e6296e59a50ac858782d"
+    sha256 monterey:       "2f38b64724d094657593800a7973e7fbf39b230835865d2108cfe63d0a103fa6"
+    sha256 big_sur:        "d0d3b637cba55a86581b099329cbbd3254bddd59306b58e32bce2b787c081dfe"
+    sha256 catalina:       "cfeed208e46eb157a2c97ebbb71d9505e47da872ccfa67eae5ae2c594b037b0d"
+    sha256 x86_64_linux:   "e15f1b36f5a8348e414bd94fde2bf39095b1b60b0eae81f69ffb30c36d7cc316"
   end
 
   depends_on "go" => :build
