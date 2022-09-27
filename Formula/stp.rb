1d459cba29cd4b24b75abe670b050a8fe1863fd8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-27 08:01:16 +0800

    stp: revision bump (cryptominisat 5.11.2)
    Closes #111742.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stp.rb
+++ Formula/stp.rb
@@ -4,7 +4,7 @@ class Stp < Formula
   url "https://github.com/stp/stp/archive/refs/tags/2.3.3.tar.gz"
   sha256 "ea6115c0fc11312c797a4b7c4db8734afcfce4908d078f386616189e01b4fffa"
   license "MIT"
-  revision 2
+  revision 3
   head "https://github.com/stp/stp.git", branch: "master"
 
   livecheck do
