a8af1641d6030df823db80c90987f1e07f3f602d
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 05:13:12 +0800

    gengetopt: update 2.23 bottle.

--- Formula/gengetopt.rb
+++ Formula/gengetopt.rb
@@ -6,15 +6,8 @@ class Gengetopt < Formula
   sha256 "b941aec9011864978dd7fdeb052b1943535824169d2aa2b0e7eae9ab807584ac"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0187835bf0f0b221a2b1bb2bcc454aede83fe8cd15e74ea4bca09c7b7feccb29"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0d869a2ffa87824b4e5854df92e2f60f652fe4e5e20d188207bf6171d7d18f84"
-    sha256 cellar: :any_skip_relocation, monterey:       "90b1ee25cdf823bad41c76f92afabe8a1b7a8db6f29b4cc533c6914d833d992f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f57ffafddb271d729ec0c07bdc564fc1bf0a52ee9b060cde1c5c8da3dbe3f515"
-    sha256 cellar: :any_skip_relocation, catalina:       "7134042a80bb314db08216b1de2d293b5925ab729ba87649fdab4dc6298256f4"
-    sha256 cellar: :any_skip_relocation, mojave:         "2ae5eeef439a6abc4d1f65965e1bafa9ac5ad0620cb4ef5e9444a4b2dbef1872"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "00f2578e7697c01d060a422e1be0ce8f4c6d23b365967ff7b5501d5cd6306dd1"
-    sha256 cellar: :any_skip_relocation, sierra:         "57acd0ca20988a1b4f0f16383edb985549597b8a5266316e3a314b7775bab3c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "379c0b354e21a5da13e62c6dbce3edfc1580561fb59015f015769c18cc804a60"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gengetopt"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "adcdeec7396b45bf582fe980a0682d8bd1568b719163a026d80e36d9479a013c"
   end
 
   uses_from_macos "texinfo"
