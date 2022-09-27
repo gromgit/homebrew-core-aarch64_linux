2d689adb7c0ac892d3b7f4b22a474768023536d6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-27 22:28:10 +0800

    netpbm 10.86.35
    Closes #111808.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4365"
-  version "10.86.34"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4436"
+  version "10.86.35"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
