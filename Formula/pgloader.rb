0c0b01a8f2e3c38a2ab9277a78d4f6b4798c02f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-27 08:53:40 +0000

    pgloader: update 3.6.8 bottle.

--- Formula/pgloader.rb
+++ Formula/pgloader.rb
@@ -12,11 +12,11 @@ class Pgloader < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "e8f8544529e1d6c8934f17c5c92c5bad0ee1082a4366b65e2dd534f9d8d378f3"
-    sha256 cellar: :any, arm64_big_sur:  "449f4d45a3ff33c3bbedaa299763a90b13a6dd150826ed83f8fae1fdf0fb89c3"
-    sha256 cellar: :any, monterey:       "b262feb4f5af872aed21e8b49a307c562f749b9282d81bf15275b340ec864690"
-    sha256 cellar: :any, big_sur:        "e0e509ad7d4cb90507d6fcf47f41c55b70157d4877cced859977c4453aab6f50"
-    sha256 cellar: :any, catalina:       "a65ab10ee01510e3d2fa664d3049fe116dc09a5d6580f3ef28302dae4584346f"
+    sha256 cellar: :any, arm64_monterey: "adb28f200082b1bbfa36592f61d339099ffddb124b97151eb9122fec5d79a626"
+    sha256 cellar: :any, arm64_big_sur:  "e9dab1b1c654d6d86eac9d613a21f96afd64b662eb36f919398d93e5ac3d7a09"
+    sha256 cellar: :any, monterey:       "d6db8dd33e781a6b6eb88944d6f6f94113015d06c787b8ee847d891ed112b6a5"
+    sha256 cellar: :any, big_sur:        "8e7792576a8dcd5fb4c36a665da15c134982ef5b1298fbec7886fdedaa427144"
+    sha256 cellar: :any, catalina:       "397290ccc7b1135e622606ae625e55ce07d2af7c9a3c72aec8c8f8638a245969"
   end
 
   depends_on "buildapp" => :build
