a7a438e8183781a2a8baf0250b46c6dd63cb8b2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-28 03:55:15 +0000

    wolfssl: update 5.5.1 bottle.

--- Formula/wolfssl.rb
+++ Formula/wolfssl.rb
@@ -14,12 +14,12 @@ class Wolfssl < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "32eb066adb36d3571598d5e04996f1aa986fe246793021b14db8d7c482b83399"
-    sha256 cellar: :any,                 arm64_big_sur:  "1a5cb2e50d0c4a090b6a6bae0f2c93b37628f268bc5fc4f41b803da55c291b4a"
-    sha256 cellar: :any,                 monterey:       "e62531cfe6f1639c2bbf190f26f61666f4139219a2355d86964c7cb26f8b32bf"
-    sha256 cellar: :any,                 big_sur:        "5f627ea578328854825fea3955db5d43692d207f80492e22f3db47a862cc561c"
-    sha256 cellar: :any,                 catalina:       "220bcf83e70671b379700dd07892d59b585646b0ff15b5a5b609f7b9976a063b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a37199e7ab1c218d6a57b2611df5e3007d9485375d7de3787c988df6d1b16a36"
+    sha256 cellar: :any,                 arm64_monterey: "d58ece55f910b429a31687cba72757f437bcb99a55a520043977cc3d36b6cb20"
+    sha256 cellar: :any,                 arm64_big_sur:  "2139c747398b2c05dab02eb5f96f29c5c086487a3929f759674c57bf42be32fe"
+    sha256 cellar: :any,                 monterey:       "967f55dde5a99e53c796b09b3a77c0e45362a9b992531b56e4e60744712b9549"
+    sha256 cellar: :any,                 big_sur:        "9776785ec4381ece723d5c2f252d7b05c8612e8f602513e0852257e3166e002c"
+    sha256 cellar: :any,                 catalina:       "3b76ba018498f8d81f226d4bfad17307c3f29035694cd775b2ad6c0bc1b6f5ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e2f1df8a292ed260dbf4ccdb81dbd1ea7bbf95f421a574c18a89e71301e1e56c"
   end
 
   depends_on "autoconf" => :build
