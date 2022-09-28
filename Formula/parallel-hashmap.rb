dfa7d1875c9a42e5c73b367beaac134cd8ffc5e6
Michael Cho <cho-m@tuta.io>
2022-09-28 00:28:36 -0700

    parallel-hashmap 1.36
    Closes #111875.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/parallel-hashmap.rb
+++ Formula/parallel-hashmap.rb
@@ -1,8 +1,8 @@
 class ParallelHashmap < Formula
   desc "Family of header-only, fast, memory-friendly C++ hashmap and btree containers"
   homepage "https://greg7mdp.github.io/parallel-hashmap/"
-  url "https://github.com/greg7mdp/parallel-hashmap/archive/1.35.tar.gz"
-  sha256 "308ab6f92e4c6f49304562e352890cf7140de85ce723c097e74fbdec88e0e1ce"
+  url "https://github.com/greg7mdp/parallel-hashmap/archive/1.36.tar.gz"
+  sha256 "33acf44158a9661a9d630d13f9250a2aa27a770cb3771df77b1ba1a661c0b766"
   license "Apache-2.0"
   head "https://github.com/greg7mdp/parallel-hashmap.git", branch: "master"
 
