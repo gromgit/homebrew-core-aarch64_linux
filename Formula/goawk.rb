51cfa558f9d326dab344972becb3e61b62947ceb
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 20:47:27 +0800

    goawk: update 1.20.0 bottle.

--- Formula/goawk.rb
+++ Formula/goawk.rb
@@ -1,13 +1,13 @@
 class Goawk < Formula
   desc "POSIX-compliant AWK interpreter written in Go"
   homepage "https://benhoyt.com/writings/goawk/"
-  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.16.0.tar.gz"
-  sha256 "f610e57396cac89aaa1070ab9edfd46f09929775b58dad2ba78ed8a59e01d6a1"
+  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.20.0.tar.gz"
+  sha256 "c24ef4a9b1c0b416c1aeb786368b36736617c60cfd1f4e871798f5abb2a18e0b"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/goawk"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "85297d56574fa3ec760db56dfac3434aa5fa0e18722a186cda62417361c54a85"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "45843a57c1422d0a7d6f66d22771dd4618bcec9664e47c2151faea9968f8177d"
   end
 
   depends_on "go" => :build
