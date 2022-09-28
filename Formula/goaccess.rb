1516659bdf8b6c3373f0286200330aee97d79db9
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 20:40:58 +0800

    goaccess: update 1.6.3 bottle.

--- Formula/goaccess.rb
+++ Formula/goaccess.rb
@@ -1,8 +1,8 @@
 class Goaccess < Formula
   desc "Log analyzer and interactive viewer for the Apache Webserver"
   homepage "https://goaccess.io/"
-  url "https://tar.goaccess.io/goaccess-1.5.7.tar.gz"
-  sha256 "d66de6d576eb7f0ac4f97b1b234063251478f27481e29700bd160aef6287cec1"
+  url "https://tar.goaccess.io/goaccess-1.6.3.tar.gz"
+  sha256 "d95646c42af02ece2e246a1aa21452445fd18abe45d4a7dcbbc8394a8c4aeaa8"
   license "MIT"
   head "https://github.com/allinurl/goaccess.git", branch: "master"
 
@@ -12,12 +12,8 @@ class Goaccess < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "8ac00ccf044bf01877ce8e1461e21ba10144529c7c322f0fe2555378c129a8a1"
-    sha256 arm64_big_sur:  "329c70f0acc84c76447a8a2639a994a221483730dca8f0302841003311f0b4d4"
-    sha256 monterey:       "8d659d27dcb96049cd5f3f459b4ffc432ca4063e7d978da4eef145cc925b6c00"
-    sha256 big_sur:        "ef9c0bc52331ec3aef80aa58f8a9cd3ce9001825aca514a5ab6e4cc50274ab96"
-    sha256 catalina:       "e6734fc40187fa84f5aba824e7d9060c92a77270505a2a53413f20cdb27f767a"
-    sha256 x86_64_linux:   "93796acf4a45abe3cb973c661ef364ccb49f32a9466f238d3330d654232a5f35"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/goaccess"
+    sha256 aarch64_linux: "d0474c74df3a6a6596e3c3bce1d1133bb2523230ce7863510128a658eabb3144"
   end
 
   depends_on "autoconf" => :build
