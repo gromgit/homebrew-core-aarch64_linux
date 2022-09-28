700b89017843c385aeee453ddf029a53d6d50e55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-28 17:17:10 +0000

    qsv: update 0.69.0 bottle.

--- Formula/qsv.rb
+++ Formula/qsv.rb
@@ -7,12 +7,12 @@ class Qsv < Formula
   head "https://github.com/jqnatividad/qsv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e0d122bd81be13ab4f62ebecb0306926e4e8c10cd2390619b8472d5c4a858fe1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8ffff2db50702c1b1974b7b69e574234052bbdb8ae2c0b858fdc8d6f862716b"
-    sha256 cellar: :any_skip_relocation, monterey:       "aeaf83d6b65d54f712516cc0bbd6944b784a9aa44856df76e9b4650cd2719ae2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8a75f75b61abcb3f39e3beb40d17314ac127570618fad808e6ed9d7d91cd5bb7"
-    sha256 cellar: :any_skip_relocation, catalina:       "e0c1fb9f712f80c1784065c9630592a5666fa441d6ec874524c2fb70649cbc70"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "27f95774e009e718a17eb921003bc47c3dc7d27f5c612ef6a7970890a16f8d88"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fcabb6b59afe8a624b924e7cb8d95e2bb565851851a74773df19148a1e68bc0f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7979cbce89cf23f0c6872598418905eeba1ecd4941456f4b94aadfac871c9718"
+    sha256 cellar: :any_skip_relocation, monterey:       "cc77ee5bbda942711fd64fca5fc73ac46e3a95dc65fdde22b4b7280a07f81161"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ba4fb11c01b967b2b03d63961d7cacb2d30c40e37b72e588a4dee2a109e27c3f"
+    sha256 cellar: :any_skip_relocation, catalina:       "0339ca3f5dc64ccd24e8a12b135114b559420f391b11d043ebd92a76d062774a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "044045d7e4da7bf14429553a96f0f4c58bc68c49a9c8da97e72c3606e0a9902f"
   end
 
   depends_on "rust" => :build
