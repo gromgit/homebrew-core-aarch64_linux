851543d1096f609983d9725296c668d621a81c6a
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 23:23:55 +0800

    grap: update 1.46 bottle.

--- Formula/grap.rb
+++ Formula/grap.rb
@@ -11,14 +11,8 @@ class Grap < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e1978deb80eeb33de6b6aef7be24a3dcf5f2710238ba2a8c86ff1eb577c58406"
-    sha256 arm64_big_sur:  "b881e8e5a9e9b93597d99ad6fc3ffa06e277855fa2b49ccb697c53d463ae597b"
-    sha256 monterey:       "ec648b01a035f781aa3ab6c027ee8095d85b6d49376f564cefe16fad11574fc3"
-    sha256 big_sur:        "a36a748595465d9a1a85db3613a4cbd6c1511e802e56b77408581f9af567326f"
-    sha256 catalina:       "8eb83388db58c42ae00a343e1382c52948c5b203ff754fed7b6582eeb989fa3c"
-    sha256 mojave:         "d7f05f3fc8eb5c0c3f3a5a66bf4d43262a84ae1edaf7ec92897122fd069e4a96"
-    sha256 high_sierra:    "b7394034b2898da9e7a61d578f9789f642f29d3191f84041b4fb9763bdfdcc73"
-    sha256 x86_64_linux:   "0908d98399141b1cf93340d87df9055ea0ae3da80d4a06e7d7b02e98027c5252"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/grap"
+    sha256 aarch64_linux: "913b2478cd9aa97607ea81ddf027014a783114ca85dfaf89fbd0c4b644f3bf2b"
   end
 
   uses_from_macos "bison" => :build
