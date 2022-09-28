23f124f9e2599f5699a2488c5123fea48a6f56e4
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 21:04:27 +0800

    goctl: update 1.4.0 bottle.

--- Formula/goctl.rb
+++ Formula/goctl.rb
@@ -1,13 +1,13 @@
 class Goctl < Formula
   desc "Generates server-side and client-side code for web and RPC services"
   homepage "https://go-zero.dev"
-  url "https://github.com/zeromicro/go-zero/archive/refs/tags/tools/goctl/v1.3.6.tar.gz"
-  sha256 "2e8c57bc2d6197d9bb88a32d4401a054c937af0ff1fdd40fe4afe2300eafca97"
+  url "https://github.com/zeromicro/go-zero/archive/refs/tags/tools/goctl/v1.4.0.tar.gz"
+  sha256 "f462ea2e6ab3e66d83dd59fab22066798693c972b2ef82b6c27ed5c630746d13"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/goctl"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "b910f0af0d29b0c9e1e7279522bc19b8dec6320029bd3af3d429ab8d5e9feb36"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "71b264b43e3743bacf4452d8307c52df3dd9bcec46c2eba48c5304d8876a98cb"
   end
 
   depends_on "go" => :build
