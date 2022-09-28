825317ad86b41dabc7c75814189c5aa8312f0cad
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 12:17:03 +0800

    github-keygen: update 1.306 bottle.

--- Formula/github-keygen.rb
+++ Formula/github-keygen.rb
@@ -1,13 +1,14 @@
 class GithubKeygen < Formula
   desc "Bootstrap GitHub SSH configuration"
   homepage "https://github.com/dolmen/github-keygen"
-  url "https://github.com/dolmen/github-keygen/archive/v1.305.tar.gz"
-  sha256 "5a0a68ed9e3eb1c0e3b783c250e0790fffe17fc1a663cefa1348560ff040b940"
+  url "https://github.com/dolmen/github-keygen/archive/v1.306.tar.gz"
+  sha256 "69fc7ef1bf5c4e958f2ad634a8cc21ec4905b16851e46455c47f9ef7a7220f5d"
   license "GPL-3.0"
   head "https://github.com/dolmen/github-keygen.git", branch: "release"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "40efaefeb31f64b474d8030ddc268250d52d6f3e17131f36d0eb8e35fa24ab8e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/github-keygen"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8f0a239ea1439c0cfc2e01858831beb85549633c649a9bd5c3e6b0d679c3abd5"
   end
 
   def install
