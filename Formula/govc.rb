cebf6583e311f419e6ea6eee05526ded246b8d05
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 23:00:35 +0800

    govc: update 0.29.0 bottle.

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -1,13 +1,13 @@
 class Govc < Formula
   desc "Command-line tool for VMware vSphere"
   homepage "https://github.com/vmware/govmomi/tree/master/govc"
-  url "https://github.com/vmware/govmomi/archive/v0.28.0.tar.gz"
-  sha256 "6bfc60ff105b0ea2839f574bc84e9f66d5886c2a7769c359686b01a49818651f"
+  url "https://github.com/vmware/govmomi/archive/v0.29.0.tar.gz"
+  sha256 "0b517b04ac8709a4ef3d92421b555710b67a5234f5e5e2e8d6c3479aee01697d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/govc"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "3ebe97c2eb5bd1c1c6d8b54df5f9f0f5c71b3cf284a6a1ad31e634e90f67ddeb"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8c9f6f1275176bfbc2a121b7e2d2b7dace9ffd4d3b77166c55da39da9e8069eb"
   end
 
   depends_on "go" => :build
