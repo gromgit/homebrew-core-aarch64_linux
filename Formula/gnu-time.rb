6ff0c96da03b8fca50f689dd1ca708014ac3fe02
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 19:34:07 +0800

    gnu-time: update 1.9 bottle.

--- Formula/gnu-time.rb
+++ Formula/gnu-time.rb
@@ -7,16 +7,8 @@ class GnuTime < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff4691a2d76432eb0222284ccbeda79b3375cefdb1c606ba74ea3e8e06ac25f5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3930463651363f08ca7a90ec25deafd85c57f7a71be8ee236f7e15f20de7ff22"
-    sha256 cellar: :any_skip_relocation, monterey:       "e7b649410f591aa0d0068ae267e7357db99c86c73fd5e992db0f5512614a07a6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f4fc9d2c49b65130d04a476d4cd887b1e1033a7870df9805be28aba09be901f0"
-    sha256 cellar: :any_skip_relocation, catalina:       "9a1d1160f85f46b3022dc4d978dfafe6b3a02fc97446bc51f8b1ae4580b7c69a"
-    sha256 cellar: :any_skip_relocation, mojave:         "dc007b95e2f9fb0df3380da55d3c9337529b1a4a3cd762972eb88512f567ea1c"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "ad5d776c38e43f16fad8976770eeaa18e40562c166fa65fdaa12af61981c7b90"
-    sha256 cellar: :any_skip_relocation, sierra:         "d51ef948a5a87281175fef771cb28469cbdb3085e3c51ad325d780ff921cc013"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9c5ae5e7ac2f00cf8655ce4b6095e4706bcc36300d36a1c7121ab03d010ea5f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gnu-time"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b815e3388cf19f600d38b9aef6a67796a23a7a116f2cc883a5fa6ece45b5c28f"
   end
 
   uses_from_macos "ruby" => :test
