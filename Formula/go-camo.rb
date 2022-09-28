2cee8b169e225442616e52b74de9251b3fd31ae3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-28 13:20:02 +0000

    go-camo: add 2.4.0 bottle.

--- Formula/go-camo.rb
+++ Formula/go-camo.rb
@@ -5,6 +5,15 @@ class GoCamo < Formula
   sha256 "546341d25710802d01b6253d7c7b4049a46b6a7ad352439f0f6855babfb56cda"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0c109736fe43f92e4283f9da9fa3a472f75324f6a78b421ffcf0aa288bb24247"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0c109736fe43f92e4283f9da9fa3a472f75324f6a78b421ffcf0aa288bb24247"
+    sha256 cellar: :any_skip_relocation, monterey:       "9aeb1790075eb725a34e3d5f07901a65c27ca98a6821316918a817949f378432"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9aeb1790075eb725a34e3d5f07901a65c27ca98a6821316918a817949f378432"
+    sha256 cellar: :any_skip_relocation, catalina:       "9aeb1790075eb725a34e3d5f07901a65c27ca98a6821316918a817949f378432"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48c7f81d8f6e30bc86dab0a26bdb20b545d8b4f9e98e73e1f29efb894330c32a"
+  end
+
   depends_on "go" => :build
 
   def install
