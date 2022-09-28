552a780dc2f926e9185805397e3d488ad169efb7
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 11:43:39 +0800

    git-when-merged: update 1.2.1 bottle.

--- Formula/git-when-merged.rb
+++ Formula/git-when-merged.rb
@@ -1,20 +1,34 @@
 class GitWhenMerged < Formula
+  include Language::Python::Shebang
+
   desc "Find where a commit was merged in git"
   homepage "https://github.com/mhagger/git-when-merged"
-  url "https://github.com/mhagger/git-when-merged/archive/v1.2.0.tar.gz"
-  sha256 "3fb3ee2f186103c2dae1e4a2e104bc37199641f4ffdb38d85ca612cf16636982"
-  license "GPL-2.0"
+  url "https://github.com/mhagger/git-when-merged/archive/v1.2.1.tar.gz"
+  sha256 "46ba5076981862ac2ad0fa0a94b9a5401ef6b5c5b0506c6e306b76e5798e1f58"
+  license "GPL-2.0-only"
+  head "https://github.com/mhagger/git-when-merged.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "67a1db2415c0aa7026ad1958a6966b909575ded3f7c5c63297aff72ca8eb76b0"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/git-when-merged"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "00f52d1fdf74ecb58187df897e301f45dcfd2f41a62761f7afab4d61ceabb27a"
   end
 
+  # TODO: Update this to whichever python has `bin/python3`.
+  depends_on "python@3.10" => :test
+  uses_from_macos "python"
+
   def install
-    bin.install "bin/git-when-merged"
+    bin.install "src/git_when_merged.py" => "git-when-merged"
+
+    if !OS.mac? || MacOS.version >= :catalina
+      rewrite_shebang detected_python_shebang(use_python_from_path: true), bin/"git-when-merged"
+    end
   end
 
   test do
     system "git", "init"
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "BrewTestBot@example.com"
     touch "foo"
     system "git", "add", "foo"
     system "git", "commit", "-m", "foo"
@@ -27,6 +41,11 @@ class GitWhenMerged < Formula
     touch "baz"
     system "git", "add", "baz"
     system "git", "commit", "-m", "baz"
-    system "#{bin}/git-when-merged", "bar"
+    system bin/"git-when-merged", "bar"
+
+    # Test with both Homebrew Python3 and system Python3 to validate our shebang.
+    which_all("python3").each do |python|
+      system python, bin/"git-when-merged", "bar"
+    end
   end
 end
