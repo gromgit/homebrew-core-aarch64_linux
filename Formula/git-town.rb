a5c44446acb8d861543ad5c0aa19a6a856ad847b
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 11:37:59 +0800

    git-town: update 7.8.0 bottle.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -1,13 +1,13 @@
 class GitTown < Formula
   desc "High-level command-line interface for Git"
   homepage "https://www.git-town.com/"
-  url "https://github.com/git-town/git-town/archive/v7.7.0.tar.gz"
-  sha256 "edc4f87ef904ac297b9fbb30014e2ab474ee633c1687ed5011b38cd6f8b950e2"
+  url "https://github.com/git-town/git-town/archive/v7.8.0.tar.gz"
+  sha256 "a5c04923307ffe8e6cf6ec3ea720170e1565078af5eebba743556db855da8d03"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/git-town"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "0d146c4a7670f73a59897cd3f680e7f4b9470bf36c134bc0eb654f8f4d1d8bf0"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "67321e5d552535e3074ee9b4df464ac1122b53006db2f2ba4b03c0f8f5fe422a"
   end
 
   depends_on "go" => :build
@@ -21,9 +21,7 @@ class GitTown < Formula
     system "go", "build", *std_go_args(ldflags: ldflags)
 
     # Install shell completions
-    (bash_completion/"git-town").write Utils.safe_popen_read(bin/"git-town", "completions", "bash")
-    (zsh_completion/"_git-town").write Utils.safe_popen_read(bin/"git-town", "completions", "zsh")
-    (fish_completion/"git-town.fish").write Utils.safe_popen_read(bin/"git-town", "completions", "fish")
+    generate_completions_from_executable(bin/"git-town", "completions")
   end
 
   test do
