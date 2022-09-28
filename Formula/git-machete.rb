7987f142ab6c5bcbaa102d37d575278a63d7eb31
amalota <amalota@virtuslab.com>
2022-09-28 16:58:51 +0200

    git-machete 3.12.0 (new formula)
    Closes #111891.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/git-machete.rb
@@ -0,0 +1,37 @@
+class GitMachete < Formula
+  include Language::Python::Virtualenv
+
+  desc "Git repository organizer & rebase workflow automation tool"
+  homepage "https://github.com/VirtusLab/git-machete"
+  url "https://pypi.org/packages/source/g/git-machete/git-machete-3.12.0.tar.gz"
+  sha256 "b999ff0ead4b856436ee74aae394ae3c68941fab8286256488304382d0fc8452"
+  license "MIT"
+
+  depends_on "python@3.10"
+
+  def install
+    virtualenv_install_with_resources
+
+    bash_completion.install "completion/git-machete.completion.bash"
+    zsh_completion.install "completion/git-machete.completion.zsh"
+    fish_completion.install "completion/git-machete.fish"
+  end
+
+  test do
+    system "git", "init"
+    system "git", "config", "user.email", "you@example.com"
+    system "git", "config", "user.name", "Your Name"
+    (testpath/"test").write "foo"
+    system "git", "add", "test"
+    system "git", "commit", "--message", "Initial commit"
+    system "git", "branch", "-m", "main"
+    system "git", "checkout", "-b", "develop"
+    (testpath/"test2").write "bar"
+    system "git", "add", "test2"
+    system "git", "commit", "--message", "Other commit"
+
+    (testpath/".git/machete").write "main\n  develop"
+    expected_output = "  main\n  |\n  | Other commit\n  o-develop *\n"
+    assert_equal expected_output, shell_output("git machete status --list-commits")
+  end
+end
