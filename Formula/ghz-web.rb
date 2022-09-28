6890653d0f594cb1f242e508918d3f7deaadac54
Adrian Ho <the.gromgit@gmail.com>
2022-09-28 08:19:36 +0800

    ghz-web: update 0.110.0 bottle.

--- Formula/ghz-web.rb
+++ Formula/ghz-web.rb
@@ -1,8 +1,8 @@
 class GhzWeb < Formula
   desc "Web interface for ghz"
   homepage "https://ghz.sh"
-  url "https://github.com/bojand/ghz/archive/v0.108.0.tar.gz"
-  sha256 "fd3f4f451ead288622ebf122bb52edf18828a34357489edc8446c64b0cc10770"
+  url "https://github.com/bojand/ghz/archive/v0.110.0.tar.gz"
+  sha256 "254463fd61b316f709a84b184da5309be1c0a4a442145665da26d9ad98da1351"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class GhzWeb < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ghz-web"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "d0d3a83bedc940728e46213b4438fd8f82ff71777cc4b2854ed18cd97f7e28c1"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ae19dc31b1a9dcab5909bcbd48cfb7e2e508b75e8a6d18dd6fd089d5eb467a5b"
   end
 
   depends_on "go" => :build
