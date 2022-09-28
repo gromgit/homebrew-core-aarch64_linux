061d93f0821351a3deab96b00e4913410ff9b8c5
Alistair Scott <aliscott@users.noreply.github.com>
2022-09-28 13:09:33 +0000

    infracost 0.10.12
    Closes #111902.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -1,8 +1,8 @@
 class Infracost < Formula
   desc "Cost estimates for Terraform"
   homepage "https://www.infracost.io/docs/"
-  url "https://github.com/infracost/infracost/archive/v0.10.11.tar.gz"
-  sha256 "25956ef500ae171d2d36c3c61017b427a8ea986c2ef17ba6f89e5f64745dfe2a"
+  url "https://github.com/infracost/infracost/archive/v0.10.12.tar.gz"
+  sha256 "026948076935a51e37d00cce1536338f61ceac5c4838fcbd189e0fab721befc3"
   license "Apache-2.0"
   head "https://github.com/infracost/infracost.git", branch: "master"
 
