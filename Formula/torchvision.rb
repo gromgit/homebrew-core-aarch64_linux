34e45bac0a189ef36d34b8817996eedd0cc18634
David Baumgold <david@davidbaumgold.com>
2022-09-28 15:55:38 +0200

    torchvision: install Python module
    Closes #111986.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/torchvision.rb
+++ Formula/torchvision.rb
@@ -1,4 +1,6 @@
 class Torchvision < Formula
+  include Language::Python::Virtualenv
+
   desc "Datasets, transforms, and models for computer vision"
   homepage "https://github.com/pytorch/vision"
   url "https://github.com/pytorch/vision/archive/refs/tags/v0.13.1.tar.gz"
@@ -21,22 +23,61 @@ class Torchvision < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "python@3.10" => [:build, :test]
   depends_on "jpeg-turbo"
   depends_on "libpng"
+  depends_on "numpy"
+  depends_on "pillow"
+  depends_on "python-typing-extensions"
   depends_on "pytorch"
 
   on_macos do
     depends_on "libomp"
   end
 
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
+    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/a5/61/a867851fd5ab77277495a8709ddda0861b28163c4613b011bc00228cc724/requests-2.28.1.tar.gz"
+    sha256 "7c5599b102feddaa661c826c56ab4fee28bfd17f5abca1ebbe3e7f19d7c97983"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/a1/34/44964211e5410b051e4b8d2869c470ae8a68ae274953b1c7de6d98bbcf94/charset-normalizer-2.1.1.tar.gz"
+    sha256 "5a3d016c7c547f69d6f81fb0db9449ce888b418b5b9952cc5e6e66843e9dd845"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/cb/a4/7de7cd59e429bd0ee6521ba58a75adaec136d32f91a761b28a11d8088d44/certifi-2022.9.24.tar.gz"
+    sha256 "0d9c601124e5a6ba9712dbc60d9c53c21e34f5f641fe83002317394311bdce14"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/b2/56/d87d6d3c4121c0bcec116919350ca05dc3afd2eeb7dc88d07e8083f8ea94/urllib3-1.26.12.tar.gz"
+    sha256 "3fa96cf423e6987997fc326ae8df396db2a8b7c667747d47ddd8ecba91f4a74e"
+  end
+
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+
+    jpeg = Formula["jpeg-turbo"]
+    inreplace "setup.py",
+      "(jpeg_found, jpeg_conda, jpeg_include, jpeg_lib) = find_library(\"jpeglib\", vision_include)",
+      "(jpeg_found, jpeg_conda, jpeg_include, jpeg_lib) = (True, False, \"#{jpeg.include}\", \"#{jpeg.lib}\")"
+
+    virtualenv_install_with_resources using: "python@3.10"
+
     pkgshare.install "examples"
   end
 
   test do
+    # test that C++ libraries are available
     (testpath/"test.cpp").write <<~EOS
       #include <assert.h>
       #include <torch/script.h>
@@ -66,5 +107,14 @@ class Torchvision < Formula
                     "-L#{lib}", "-ltorchvision"
 
     system "./test"
+
+    # test that the `torchvision` Python module is available
+    cp test_fixtures("test.png"), "test.png"
+    system libexec/"bin/python", "-c", <<~EOS
+      import torch
+      import torchvision
+      t = torchvision.io.read_image("test.png")
+      assert isinstance(t, torch.Tensor)
+    EOS
   end
 end
