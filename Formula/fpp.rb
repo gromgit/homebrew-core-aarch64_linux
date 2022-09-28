7b6684ef3e28b41a273ef6848462ed0ecddad052
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-28 09:08:15 +0000

    fpp: update 0.9.5 bottle.

--- Formula/fpp.rb
+++ Formula/fpp.rb
@@ -7,12 +7,7 @@ class Fpp < Formula
   head "https://github.com/facebook/pathpicker.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
-    sha256 cellar: :any_skip_relocation, catalina:      "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
-    sha256 cellar: :any_skip_relocation, mojave:        "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f51509aa7ff591a83d65bf092b43f19ecb446c6dd08d55f33f52725b4618fe52"
-    sha256 cellar: :any_skip_relocation, all:           "6efaf75a2e334b6c87e51be515d5bd32ba6ad1aedc8cd64253020772a9be63da"
+    sha256 cellar: :any_skip_relocation, all: "dfbe5a6badfc4d4f4d0c21d04b0270457cfcf0d78eed4a93f7c6b8ce18433f93"
   end
 
   uses_from_macos "python", since: :catalina
