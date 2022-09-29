94f17c005b35c3192fe8c5f88754ca679a8b9c7e
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 12:36:57 +0800

    hello: update 2.12.1 bottle.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,13 +1,13 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz"
-  sha256 "cf04af86dc085268c5f4470fbae49b18afbc221b78096aab842d934a76bad0ab"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz"
+  sha256 "8d99142afd92576f30b0cd7cb42a8dc6809998bc5d607d88761f512e26c7db20"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hello"
-    sha256 aarch64_linux: "b721ffedcdc1fe4e54dc1d799fe72d6970a0b38c26f8bff10e35e5b9c3f3fe06"
+    sha256 aarch64_linux: "73284b14b02218762b9c9e4718fd670b7047a6cd42d77ecbf147dacb66ad02e9"
   end
 
   conflicts_with "perkeep", because: "both install `hello` binaries"
