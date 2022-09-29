c2153a6a8ff9763988d27ecf58645fb56b9c328f
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 14:04:55 +0800

    howard-hinnant-date: update 3.0.1 bottle.

--- Formula/howard-hinnant-date.rb
+++ Formula/howard-hinnant-date.rb
@@ -6,13 +6,8 @@ class HowardHinnantDate < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "52811eb710a07d879d153a65bc6c771a8ff801f990a6bd2f968d1238c6000b03"
-    sha256 cellar: :any,                 arm64_big_sur:  "deff47e2027f805ef5cd430d0700470cf8bada0cde442e8674ae6a832e3b9888"
-    sha256 cellar: :any,                 monterey:       "0098680dad7ff5cb5854d04ab0aff279641892d1c8c3079658bfe2762bb1b6f9"
-    sha256 cellar: :any,                 big_sur:        "b8fc90e684f2d3b711fcb405c082f8ad637eac8f6c5816b746284c911950eb5a"
-    sha256 cellar: :any,                 catalina:       "bebf754666baa69673a77fb5eeb3c0ebe9931b7aa2d3991a3f6fa235a439d11b"
-    sha256 cellar: :any,                 mojave:         "d140b4b590c5ef8c25e80abaa8466dbcb6f10a95ca0dec551de7fb0e213171b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2361559d178154d8e6f69b1da915838ab17271a61d3ff808db1ed2ca8ce7091f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/howard-hinnant-date"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "741cf43305a065048bf8f39d2ce34a5092566dac7590a9a721177002f2e2dcaf"
   end
 
   depends_on "cmake" => :build
