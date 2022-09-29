7b7056135c166e90777fb8269a042fd5abd0f1f8
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 10:26:46 +0800

    haruhi-dl: update 2021.8.1_1 bottle.

--- Formula/haruhi-dl.rb
+++ Formula/haruhi-dl.rb
@@ -9,12 +9,8 @@ class HaruhiDl < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "966c61dfcc9b8849d593007e07c3254a5e243093ef7b3b3d74f7f1e06be569b6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "966c61dfcc9b8849d593007e07c3254a5e243093ef7b3b3d74f7f1e06be569b6"
-    sha256 cellar: :any_skip_relocation, monterey:       "127ff0efc125444133103fd03f0f8b51c2492b1cf9af89db62eb78db12fabc94"
-    sha256 cellar: :any_skip_relocation, big_sur:        "127ff0efc125444133103fd03f0f8b51c2492b1cf9af89db62eb78db12fabc94"
-    sha256 cellar: :any_skip_relocation, catalina:       "127ff0efc125444133103fd03f0f8b51c2492b1cf9af89db62eb78db12fabc94"
-    sha256 cellar: :any_skip_relocation, mojave:         "127ff0efc125444133103fd03f0f8b51c2492b1cf9af89db62eb78db12fabc94"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/haruhi-dl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "034fd249200cc3a624226be96f3f66c4a934d3676e97552a161e0b67b8f249a7"
   end
 
   deprecate! date: "2022-01-15", because: :deprecated_upstream
