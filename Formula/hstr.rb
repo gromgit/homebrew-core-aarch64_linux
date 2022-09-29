63b9dbaecf6b278b91ade354278c5edeb5c09f51
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 14:24:10 +0800

    hstr: update 2.5 bottle.

--- Formula/hstr.rb
+++ Formula/hstr.rb
@@ -6,12 +6,8 @@ class Hstr < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "bdb7df0cde335ab4ff34ff96a84dde6ef39141e9e1e78570b66a2f85696b67c9"
-    sha256 cellar: :any,                 arm64_big_sur:  "64d0b3fa9b402b79747f2f4551d1a0fb194fe442bdf28c457c5bc67304c7278d"
-    sha256 cellar: :any,                 monterey:       "8cbf171433521bd0e4ca9b0523397a411301b33ae31d86ea16ee79f385448b8f"
-    sha256 cellar: :any,                 big_sur:        "42756cb38b429efb90b340ac6574d51a2846aef73b35fa7037298cad436ff05d"
-    sha256 cellar: :any,                 catalina:       "d16a1175f61a6e533c6b2717c03a5b2cd401d374c4482aee7dc5173e598be838"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8d4361666421b2a865c23bd37b386eccd4c5f0052923c534b6c8435b1a9dc8f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hstr"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "55afde7718ec288de9f41c8a40aa2244387d1cc4d3697cb209696acbe9f27651"
   end
 
   depends_on "autoconf" => :build
