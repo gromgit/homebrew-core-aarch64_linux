31fc31c739234b9662c43be71ee56983eb3c9053
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-09-29 05:53:18 +0000

    rqlite 7.7.0
    Closes #111961.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rqlite.rb
+++ Formula/rqlite.rb
@@ -1,8 +1,8 @@
 class Rqlite < Formula
   desc "Lightweight, distributed relational database built on SQLite"
   homepage "http://www.rqlite.com/"
-  url "https://github.com/rqlite/rqlite/archive/v7.6.1.tar.gz"
-  sha256 "c618aae00c7e4e4b8abd88b46f65acf5ba31d1672f05f46b785b4e80cebfb8e3"
+  url "https://github.com/rqlite/rqlite/archive/v7.7.0.tar.gz"
+  sha256 "ae25bda002626fb568402304c1da2d6a0bf18a0a6d2ca5e114f2d4236496578a"
   license "MIT"
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 
