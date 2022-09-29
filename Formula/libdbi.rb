83ac16486725be8fda3315a13fc3fc816f38573b
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-09-29 08:50:23 +0000

    libdbi: update 0.9.0 bottle.

--- Formula/libdbi.rb
+++ Formula/libdbi.rb
@@ -7,12 +7,14 @@ class Libdbi < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any,                 monterey:     "4a524cd1c3ee5cb9c053b01f01125ecca43d4def593b5afcca64b84307dcf505"
-    sha256 cellar: :any,                 big_sur:      "af53bebb0b59917b87468a5cc52b168c01e40b83c5eff987ffa2655c9e64ac3b"
-    sha256 cellar: :any,                 catalina:     "ce66e90000681c5f9174c3698ac4ceefd5d1be6ca4ffa574053f0705217c6837"
-    sha256 cellar: :any,                 mojave:       "3aff10515535dc3f99dfa56644229daba74f719838d3e580754b3bbdc3c0429d"
-    sha256 cellar: :any,                 high_sierra:  "eb3d8474601267d835b74b5a29944dc6d987486745dcfd17389be3a44b2c0175"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ac0e87e837a96a2147f0f95157ecbf6df333145cbbbfec466a9c18d794ffe8c1"
+    sha256 cellar: :any,                 arm64_monterey: "3e46a3c790195ad94277912f505bce7d29aee03dddc112f1473e5094b6f1de97"
+    sha256 cellar: :any,                 arm64_big_sur:  "61c6d495c3d1459c663a25e3436401f57a4b8df6745befa5214076fa3555979e"
+    sha256 cellar: :any,                 monterey:       "4a524cd1c3ee5cb9c053b01f01125ecca43d4def593b5afcca64b84307dcf505"
+    sha256 cellar: :any,                 big_sur:        "af53bebb0b59917b87468a5cc52b168c01e40b83c5eff987ffa2655c9e64ac3b"
+    sha256 cellar: :any,                 catalina:       "ce66e90000681c5f9174c3698ac4ceefd5d1be6ca4ffa574053f0705217c6837"
+    sha256 cellar: :any,                 mojave:         "3aff10515535dc3f99dfa56644229daba74f719838d3e580754b3bbdc3c0429d"
+    sha256 cellar: :any,                 high_sierra:    "eb3d8474601267d835b74b5a29944dc6d987486745dcfd17389be3a44b2c0175"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac0e87e837a96a2147f0f95157ecbf6df333145cbbbfec466a9c18d794ffe8c1"
   end
 
   on_arm do
