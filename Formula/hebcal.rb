a53fc3ec9a32dda467311b9afc0aa824e0b11e26
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 12:09:54 +0800

    hebcal: update 4.31 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -1,17 +1,13 @@
 class Hebcal < Formula
   desc "Perpetual Jewish calendar for the command-line"
   homepage "https://github.com/hebcal/hebcal"
-  url "https://github.com/hebcal/hebcal/archive/v4.28.tar.gz"
-  sha256 "31b1fb0f8b692d985e4d2efaaed8de37132d102b03786a08626ddb03a126c0ff"
+  url "https://github.com/hebcal/hebcal/archive/v4.31.tar.gz"
+  sha256 "0a083aa0f4e561987f15914c1d6807a5107eb7e7f2e981aef5725130a36893cd"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "291f7605c1f11823fd9a4317c8350707ece09c87167d71dd53f7ba8935d6f012"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bed65edd0b44f94de7f7fe0299c3b70cbc22acfbfda09a9752b730c58e73677e"
-    sha256 cellar: :any_skip_relocation, monterey:       "445ea0c4745faf3278d15bc414c9f2ebca52e0a85ac1a77240dc756f867d4ba1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0d7e6b52475fe53384847f0edf270a7387a77fe7a08eb9a39508c79c265e2198"
-    sha256 cellar: :any_skip_relocation, catalina:       "2e33a09ab00828ad4cb786d5a9767541b130f002e78ac54915b0b89b6db652f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d99da6b0c7fba792b35f88065e824d163c197fd4369bb1b2e1825ddac464468d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hebcal"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "93631e267ade3988ffd5d8860f3cd3c839e4a4836b27dbab4c72a220a00ffcdc"
   end
 
   depends_on "autoconf" => :build
