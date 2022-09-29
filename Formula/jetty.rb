fdb1ce4669116795c2d90a62dfd05720fed8dc2a
Michael Cho <cho-m@tuta.io>
2022-09-29 04:31:18 -0700

    jetty: build native ARM libsetuid
    Closes #111978.
    
    Signed-off-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jetty.rb
+++ Formula/jetty.rb
@@ -18,9 +18,28 @@ class Jetty < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "8533506b9adbcbc7de65fa95c7e08abd12e3585ca403db885778bad9fb58b61e"
   end
 
-  # Ships a pre-built x86_64-only `libsetuid-osx.so`.
-  depends_on arch: :x86_64
   depends_on "openjdk"
+
+  on_arm do
+    depends_on "maven" => :build
+
+    # We get jetty-setuid source code from the jetty.toolchain repo to build a native ARM binary.
+    # The last jetty-setuid-1.0.4 release has some issues building, so we pick a more recent commit.
+    # The particular commit was selected as it aligned to jetty version at time of PR.
+    # Once 1.0.5 is available, we can switch to stable versions. Afterward, the version should
+    # probably match the jetty-setuid version that is included with jetty.
+    resource "jetty.toolchain" do
+      url "https://github.com/eclipse/jetty.toolchain/archive/ce0f110e0b95baf85775897aa90f5b6c0cc6cd4d.tar.gz"
+      sha256 "06a3ac033e5c4cc05716e7d362de7257e73aad1783b297bd57b6e0f7661555ab"
+
+      # Fix header paths on macOS to follow modern JDKs rather than old system Java.
+      # PR ref: https://github.com/eclipse/jetty.toolchain/pull/211
+      patch do
+        url "https://github.com/eclipse/jetty.toolchain/commit/4c3744d79f414f43fe45636fdabd5595f17daab6.patch?full_index=1"
+        sha256 "6a7f3f16f7fb5f3d8ccf1562612da0d1091049bbafa4a0bf2ff0754551743312"
+      end
+    end
+  end
 
   def install
     libexec.install Dir["*"]
@@ -30,6 +49,17 @@ class Jetty < Formula
     env["JETTY_HOME"] = libexec
     Dir.glob(libexec/"bin/*.sh") do |f|
       (bin/File.basename(f, ".sh")).write_env_script f, env
+    end
+    return if Hardware::CPU.intel?
+
+    # We build a native ARM libsetuid to enable Jetty's SetUID feature.
+    # https://www.eclipse.org/jetty/documentation/jetty-9/index.html#configuring-jetty-setuid-feature
+    libexec.glob("lib/setuid/libsetuid-*.so").map(&:unlink)
+    resource("jetty.toolchain").stage do
+      cd "jetty-setuid"
+      system "mvn", "clean", "install", "-Penv-#{OS.mac? ? "mac" : "linux"}"
+      libsetuid = "libsetuid-#{OS.mac? ? "osx" : "linux"}"
+      (libexec/"lib/setuid").install "#{libsetuid}/target/#{libsetuid}.so"
     end
   end
 
@@ -55,5 +85,13 @@ class Jetty < Formula
       Process.kill 9, pid
       Process.wait pid
     end
+
+    # Do a basic check that SetUID can run native library. Actually running command
+    # requires creating a separate user account and running with sudo
+    java = Formula["openjdk"].opt_bin/"java"
+    system java, "-jar", libexec/"start.jar", "--add-to-start=setuid"
+    output = shell_output("#{java} -Djava.library.path=#{libexec}/lib/setuid -jar #{libexec}/start.jar 2>&1", 254)
+    refute_match "java.lang.UnsatisfiedLinkError", output
+    assert_match "User jetty is not found", output
   end
 end
