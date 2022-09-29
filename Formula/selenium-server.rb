fdb231bab753befb2aacde004ef3cb08204618d7
Porkepix <porkepix@gmail.com>
2022-09-29 05:31:46 +0200

    selenium-server 4.5.0
    Closes #111947.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/selenium-server.rb
+++ Formula/selenium-server.rb
@@ -1,8 +1,8 @@
 class SeleniumServer < Formula
   desc "Browser automation for testing purposes"
   homepage "https://www.selenium.dev/"
-  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.4.0/selenium-server-4.4.0.jar"
-  sha256 "c508fe3f201e3a00196373bfab1bb5a27b0a772c5b4a6069b7ce0f350225fc66"
+  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.5.0/selenium-server-4.5.0.jar"
+  sha256 "74ed61b1b8c4b6d30449e50be17ecb7b9fd70013373dcfa8447f5c66e3eb53cd"
   license "Apache-2.0"
 
   livecheck do
