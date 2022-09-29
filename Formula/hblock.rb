1b5aee8881e13d981f2a9048d6d8a1a282a91442
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 11:01:29 +0800

    hblock: update 3.4.0 bottle.

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -1,17 +1,13 @@
 class Hblock < Formula
   desc "Adblocker that creates a hosts file from multiple sources"
   homepage "https://hblock.molinero.dev/"
-  url "https://github.com/hectorm/hblock/archive/v3.3.2.tar.gz"
-  sha256 "35bd4af1dbae3b57de6cede6c05f3d4ce25227b33a53358ef47c76a491304eb0"
+  url "https://github.com/hectorm/hblock/archive/v3.4.0.tar.gz"
+  sha256 "762dbe5f2a0ea84078b194190f4fb51b9fe3e6ef043c1899fdda6a083328225f"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b408e7de0b8d0d3634deac27adcf630e303dcf03eee853a16af4b8b9d2f75714"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b408e7de0b8d0d3634deac27adcf630e303dcf03eee853a16af4b8b9d2f75714"
-    sha256 cellar: :any_skip_relocation, monterey:       "b408e7de0b8d0d3634deac27adcf630e303dcf03eee853a16af4b8b9d2f75714"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b408e7de0b8d0d3634deac27adcf630e303dcf03eee853a16af4b8b9d2f75714"
-    sha256 cellar: :any_skip_relocation, catalina:       "b408e7de0b8d0d3634deac27adcf630e303dcf03eee853a16af4b8b9d2f75714"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "31d98acdc362571796f04dee5c4fca5ed0949a5cf7347c024de713764f68a3c1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hblock"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "47d00fdba6c2771c0f856bfa4f22696b0f7d875fe6cf871a99f47836b435c332"
   end
 
   uses_from_macos "curl"
