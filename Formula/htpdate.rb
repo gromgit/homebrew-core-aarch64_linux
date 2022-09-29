2a37289b0d7249f565e6353743244beba0058543
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 15:01:05 +0800

    htpdate: update 1.3.6 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -1,8 +1,8 @@
 class Htpdate < Formula
   desc "Synchronize time with remote web servers"
   homepage "https://www.vervest.org/htp/"
-  url "https://www.vervest.org/htp/archive/c/htpdate-1.3.4.tar.gz"
-  sha256 "744f9200cfd3b008a5516c5eb6da727af532255a329126a7b8f49a5623985642"
+  url "https://www.vervest.org/htp/archive/c/htpdate-1.3.6.tar.gz"
+  sha256 "3cdc558ec8e53ef374a42490b2f28c0b23981fa8754a6d7182044707828ad1e9"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Htpdate < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/htpdate"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "f505cf9e71cbd5920e37debac21612a2a0360ba7958bb80f950d33753ed67d50"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f3e342a397d270003fa1a4942bb7ff78bdc46851b701ab2318c6db133c0226fe"
   end
 
   def install
