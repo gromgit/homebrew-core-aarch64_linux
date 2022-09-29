49ff128ac2f106a07c29c596d7c628e3b1ed5251
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 13:15:45 +0800

    libid3tag: update 0.16.2 bottle.

--- Formula/libid3tag.rb
+++ Formula/libid3tag.rb
@@ -1,17 +1,13 @@
 class Libid3tag < Formula
   desc "ID3 tag manipulation library"
   homepage "https://www.underbit.com/products/mad/"
-  url "https://github.com/tenacityteam/libid3tag/archive/refs/tags/0.16.1.tar.gz"
-  sha256 "185a6cec84644cf1aade8397dcf76753bcb3bd85ec2111a9e1079214ed85bef0"
+  url "https://github.com/tenacityteam/libid3tag/archive/refs/tags/0.16.2.tar.gz"
+  sha256 "96198b7c8803bcda44e299615e1929a85bd5a5da02e67ebc442735bc50018190"
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b2464c33c9031a7de18c5d3328e9f34ec6b8a0d14b83339f8137e9d8ace8689e"
-    sha256 cellar: :any,                 arm64_big_sur:  "5131938bc5b5877f75de69be030893d5b0a31e252d36e9579723f7370f5b8bee"
-    sha256 cellar: :any,                 monterey:       "790f7ef2e0cab3a2c6552728d06843b5c490da42cca1e5ef34e76a110a2ed5f1"
-    sha256 cellar: :any,                 big_sur:        "1f3fba83270f945e708fbe4b935608aac1564c57f04ada6077ef4c73aa12ad37"
-    sha256 cellar: :any,                 catalina:       "203fc54e1c97e801e3a5c38a094217fefc60f5ee613b8dd662981b8a77ed3848"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82e1a4d5bdf7328cd39d9eab69d04bcbb9d509b1f479a022b30487058898b0cd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libid3tag"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8d5ffc15cd869ad76318d7b3a8d9962b28bd63486a140ca0eae85696013f6351"
   end
 
   depends_on "cmake" => :build
