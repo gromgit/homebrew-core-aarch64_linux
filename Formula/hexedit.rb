117ed290662061c3c935140bebd8957d58624250
Adrian Ho <the.gromgit@gmail.com>
2022-09-29 13:35:23 +0800

    hexedit: update 1.6 bottle.

--- Formula/hexedit.rb
+++ Formula/hexedit.rb
@@ -7,12 +7,8 @@ class Hexedit < Formula
   head "https://github.com/pixel/hexedit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "036e68e8552287bbf3c4f37232a3eda16f05993b0635573f04f87f9a89b71392"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4127abcc6c5a2dcd8e5b535b89de6bc2a32a5c0f92f26fef10114ad5b03d5c72"
-    sha256 cellar: :any_skip_relocation, monterey:       "f24ca3b6ffe8ab46993b0044f5fa94f2663f4122d65ae290ff8355241d41ca0f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "145c1c6e983965f64b0e19df3a0c6adaff3577486c42ffbe1e79939ef855d9c5"
-    sha256 cellar: :any_skip_relocation, catalina:       "6306cc37f5c56d10fd058db6881681e2406adf397537616b37d45aab2e732964"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26a91b13749f1e68542e41109fed36f4001a23d79a412bf7a5830f2a08ddbe5c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hexedit"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e527b34cffb89bd6217ce6d0b7d683c07e7457c29b952063174a1bfec8d27bd3"
   end
 
   depends_on "autoconf" => :build
