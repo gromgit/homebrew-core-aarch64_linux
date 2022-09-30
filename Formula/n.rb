5d6a8f9145ddd56044d6626a5d07684b295fc870
Adrian Ho <the.gromgit@gmail.com>
2022-09-30 09:16:46 +0800

    n: update 9.0.0 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -1,14 +1,14 @@
 class N < Formula
   desc "Node version management"
   homepage "https://github.com/tj/n"
-  url "https://github.com/tj/n/archive/v8.2.0.tar.gz"
-  sha256 "75efd9e583836f3e6cc6d793df1501462fdceeb3460d5a2dbba99993997383b9"
+  url "https://github.com/tj/n/archive/v9.0.0.tar.gz"
+  sha256 "37a987230d1ed0392a83f9c02c1e535a524977c00c64a4adb771ab60237be1c6"
   license "MIT"
   head "https://github.com/tj/n.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/n"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "f69ed17898d4d01ece9542405042904ad0985bb36cd175e19c015e92d3d59e47"
+    sha256 cellar: :any_skip_relocation, x86_64_aarch64_linux: "fa979454a3c6965732a20b0ab4166a9789edce29093d686907a0b52898f174bf"
   end
 
   def install
