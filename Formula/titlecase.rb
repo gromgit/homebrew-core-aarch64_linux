cf7880b9548ca63700e8f72102dbeb08d2712784
Adrian Ho <the.gromgit@gmail.com>
2022-09-30 22:46:48 +0800

    titlecase: update 1.005 bottle.

--- Formula/titlecase.rb
+++ Formula/titlecase.rb
@@ -1,13 +1,14 @@
 class Titlecase < Formula
   desc "Script to convert text to title case"
   homepage "http://plasmasturm.org/code/titlecase/"
-  url "https://github.com/ap/titlecase/archive/v1.004.tar.gz"
-  sha256 "fbaafdb66ab4ba26f1f03fa292771d146c050fff0a3a640e11314ace322c2d92"
+  url "https://github.com/ap/titlecase/archive/v1.005.tar.gz"
+  sha256 "6483798bac1e359be4b3c48b8f710fd35cc4671dfe201322cbb3461a200b4f76"
   license "MIT"
   head "https://github.com/ap/titlecase.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "27a6bbb0ade7749f3ac3f72871f7f9f611a61e8af02046511d33f723881fba0d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/titlecase"
+    sha256 cellar: :any_skip_relocation, x86_64_aarch64_linux: "fb24026539e4fd3db91084f2bf78b21d17e08e0cbd6aaf2acbdccf4cb9079ed6"
   end
 
   def install
