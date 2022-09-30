f26aaf3eaaa24f31aeffb5d930db0d7e8631e36d
Adrian Ho <the.gromgit@gmail.com>
2022-09-30 10:56:04 +0800

    odpi: update 4.4.1 bottle.

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -1,13 +1,13 @@
 class Odpi < Formula
   desc "Oracle Database Programming Interface for Drivers and Applications"
   homepage "https://oracle.github.io/odpi/"
-  url "https://github.com/oracle/odpi/archive/v4.3.0.tar.gz"
-  sha256 "e8be95e7061cad52caaa98a4d2a25d6bff8fa29c2fa609c3c447124d46b1712b"
+  url "https://github.com/oracle/odpi/archive/v4.4.1.tar.gz"
+  sha256 "c5fc27ef90d12417cb3c2bab32ed539bb4c389fde24ceb6d1df06a0985543c1a"
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/odpi"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "e7843aacd745214e5b8d3f096b5f0bd86c25cd80af0876957a739df56eab13a3"
+    sha256 cellar: :any_skip_relocation, x86_64_aarch64_linux: "e5b2cf74c97d5802049bccf6ba3e596c9a7f4da1d2a5e824933683de54082887"
   end
 
   def install
