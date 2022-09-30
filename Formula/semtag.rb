9f2efff99dfaf3052cfe76abf8a6b2ed92e783e3
Adrian Ho <the.gromgit@gmail.com>
2022-09-30 17:56:44 +0800

    semtag: update 0.1.1 bottle.

--- Formula/semtag.rb
+++ Formula/semtag.rb
@@ -1,12 +1,13 @@
 class Semtag < Formula
   desc "Semantic tagging script for git"
-  homepage "https://github.com/pnikosis/semtag"
-  url "https://github.com/pnikosis/semtag/archive/v0.1.1.tar.gz"
+  homepage "https://github.com/nico2sh/semtag"
+  url "https://github.com/nico2sh/semtag/archive/v0.1.1.tar.gz"
   sha256 "c7becf71c7c14cdef26d3980c3116cce8dad6cd9eb61abcc4d2ff04e2c0f8645"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4d61d959bcec95146dc409dd098ed0dadaf578b7fec0df2d0f031838bad2f85b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/semtag"
+    sha256 cellar: :any_skip_relocation, x86_64_aarch64_linux: "6a6b37e1523c6ebf66395fc503680c0a069b09d2d97607aee0025591c8e4c8a8"
   end
 
   def install
