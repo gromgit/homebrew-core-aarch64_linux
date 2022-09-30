fc0d2096e551c2d6f620ff4bb096728097832094
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-10-01 00:54:55 +0800

    mkvtoolnix: revision bump (libebml 1.4.3)
    Closes #112047.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mkvtoolnix.rb
+++ Formula/mkvtoolnix.rb
@@ -5,7 +5,7 @@ class Mkvtoolnix < Formula
   mirror "https://fossies.org/linux/misc/mkvtoolnix-70.0.0.tar.xz"
   sha256 "64324b7ee6b4aa6fab5f517ca93667d12260a4ea92ea80b97575beda3a91693a"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://mkvtoolnix.download/sources/"
