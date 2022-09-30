88e038cdd879063f2aeccaa6ae75a52c79edec5a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-30 06:36:25 +0000

    libvpx: update 1.12.0 bottle.

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -7,12 +7,13 @@ class Libvpx < Formula
   head "https://chromium.googlesource.com/webm/libvpx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "c4582d6d18d184b3d7087dc63d0868861830ba3501828dc51686037dd8b52491"
-    sha256 cellar: :any,                 arm64_big_sur:  "ad0ed26552883987edf5a30af668a00212105044d290db0d1177bc526e4ce429"
-    sha256 cellar: :any,                 monterey:       "6b091bdf2175b31d57ae699efdf288589c989439dcd7a3abf0311ebdf5029a6d"
-    sha256 cellar: :any,                 big_sur:        "1946876cf2681f292207a096d854816b4c874a3895993acab783342d129509b4"
-    sha256 cellar: :any,                 catalina:       "925e31b0ac36757562c95eeb2c9c0172f6813ab8670a5a245d966c000dfcb79f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51d009028436923653479737066c8735385e404bf6813e68af9f074fcbbfe4c2"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "95d64306db3eb31d8f8da747a067a6560acf9484f33bd8cca64b74835b8073ce"
+    sha256 cellar: :any,                 arm64_big_sur:  "d571549026b2c719d055a9009ed7ce65060ff448da7a68e014f19d7543379a49"
+    sha256 cellar: :any,                 monterey:       "5c54abac298c4b6d2113d17343304ae9e0520f284a185f46e1baa03b10f60f9b"
+    sha256 cellar: :any,                 big_sur:        "79e7d1a069bb6219b094fa9c51cd794e163623cda2b2fd8cbd5e41a77ace0eef"
+    sha256 cellar: :any,                 catalina:       "9dcef4d29542d3506e3a2c458f6ae2eb36df48599cadf1a82fec972fcfacd935"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "763bca80c18bed0bea0704494e7370b919a498ac3521ff6f6f193a8902f78295"
   end
 
   depends_on "yasm" => :build
