df1d5ba147442a0f2303c492f36d3cb5b7feb84a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-09-30 03:13:21 +0000

    dgen: update 1.33_1 bottle.

--- Formula/dgen.rb
+++ Formula/dgen.rb
@@ -6,10 +6,12 @@ class Dgen < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "1fbc47cc8c293c0c1284bdc01cb08216deabd210f806be8c4555416094a4265f"
-    sha256 cellar: :any,                 big_sur:      "5b5217280e09f36cdd8650b7d0951c2a10e7996ec6bde83d90843d08e876d7b7"
-    sha256 cellar: :any,                 catalina:     "3d68b5d75ca02d4686dc87be5f5d8da36925d26964aec24a6850bfccefc8a85d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "55774c1de8d53707d3e330814f836d6e034a0f68f87d0507768dd0eed55f7336"
+    sha256 cellar: :any,                 arm64_monterey: "818fa7b9017947cead9d4b8d0ae13e940f59bce44868c41536ed6140159c8ed9"
+    sha256 cellar: :any,                 arm64_big_sur:  "1107fdda6b8977cb8e962d9a7d353576f5a6e41d1de97f0c64fdfedb98253fe1"
+    sha256 cellar: :any,                 monterey:       "1fbc47cc8c293c0c1284bdc01cb08216deabd210f806be8c4555416094a4265f"
+    sha256 cellar: :any,                 big_sur:        "5b5217280e09f36cdd8650b7d0951c2a10e7996ec6bde83d90843d08e876d7b7"
+    sha256 cellar: :any,                 catalina:       "3d68b5d75ca02d4686dc87be5f5d8da36925d26964aec24a6850bfccefc8a85d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55774c1de8d53707d3e330814f836d6e034a0f68f87d0507768dd0eed55f7336"
   end
 
   head do
