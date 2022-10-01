179a0068b0df324ccbff9e0474c6996006cda93c
Adrian Ho <the.gromgit@gmail.com>
2022-10-01 08:41:06 +0800

    aria2: add 1.36.0 bottle.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -6,13 +6,8 @@ class Aria2 < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "74859913b0f8dc82fede202f5b6cb3384202e76887549addc050009e4d277aec"
-    sha256 cellar: :any, arm64_big_sur:  "22b11ee6f82f8d693fb2f6c0dd90a81174033640974003e63e4e8f98fbd3f145"
-    sha256 cellar: :any, monterey:       "ab509067c72eb555b1583189054622744f39e344e1ee51bfd91c3102167be95b"
-    sha256 cellar: :any, big_sur:        "8960d7b6b56fb29e020fb96da5305c3340d266f3e5e0c552248759e8c6169244"
-    sha256 cellar: :any, catalina:       "1e9db3f9fd405ed7a303b89379c2a8f3f01c5f5aa92cfbc8ae560c4e28618f8e"
-    sha256 cellar: :any, mojave:         "3ecd30ea4573748ec438182dbba6fad002c1465da7ef9f7a32f227fdeddde464"
-    sha256               x86_64_linux:   "c7dacb09aaa9eb22baf6ee6ee198896fb8a851ddf69d7c534c968f1b79edd69d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aria2"
+    sha256 x86_64_aarch64_linux: "46c586c17422b76d07220725bbbee755063df9824647ff74da660b146aaa2706"
   end
 
   depends_on "pkg-config" => :build
