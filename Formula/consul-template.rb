9a44e3449787955cfb7d287d46ec4e3b2a2ec7f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-01 17:10:13 +0000

    consul-template: update 0.29.4 bottle.

--- Formula/consul-template.rb
+++ Formula/consul-template.rb
@@ -8,12 +8,12 @@ class ConsulTemplate < Formula
   head "https://github.com/hashicorp/consul-template.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9e1ceb20678227873ff04fb77b3a54fb06ffaf78c2eaf6e7125b799f25ebd9c1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4773d916294ed2c1c32d4e3fa016cac868750ec961b9944e47e66cec3f5c48d9"
-    sha256 cellar: :any_skip_relocation, monterey:       "e32dae1c5380895e888f7625beadc05cd0df7069c603d0393095ea33b2cf686e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c8cfde1173c437206df2453df9fd4ce059ad8a4ff73d7994b9d76837914658b2"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b8b8b925d68b2ca7ae861f96040c956e5b45ba34e7630ffcccdba66e68f5cb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f38a13e8e3c128afdeeca1790e8a297c5628bfb7fc2cbc624f1503229ef463f5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a48da2a728be32abcbbb1ab31604080dcd90c4bcc03c63fdb0841aaa47bbfadb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c5dd8a3d2d92cd26a6adf2750ea9248598a42d815fc41980848e62521e53faa6"
+    sha256 cellar: :any_skip_relocation, monterey:       "c1f4d8312270538e4fdde3078363a5c9dab070722b56527c23b96964bd8e908c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8bfc440e690a3ad431c582a24a1d2273150697624fa9b73c570c83a24953f35a"
+    sha256 cellar: :any_skip_relocation, catalina:       "5774d0ca4c1dfee32ead5c5dc0b6f090f7a8014b99b630e4fd89e0b11a7457dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d66882462769c320e17896a73728077a8afddd8ba4066611638d39ca7c3e3fce"
   end
 
   depends_on "go" => :build
