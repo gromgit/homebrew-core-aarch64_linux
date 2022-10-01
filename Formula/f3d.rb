f8600fb661603a2476ac95cd5acc3f0f7f7cb0d0
Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
2022-10-01 23:04:01 +0000

    f3d: update 1.3.1 bottle.

--- Formula/f3d.rb
+++ Formula/f3d.rb
@@ -6,11 +6,12 @@ class F3d < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "eb43ad624a8cda6e60911b4de70631e5cfa71c7bc0ce2260337f1442243060f7"
-    sha256 cellar: :any, arm64_big_sur:  "267fcff261a954ec13a7cd01f59f4db0dbf01215e7df0404121edd9e88cae7c9"
-    sha256 cellar: :any, monterey:       "7e8cb99a85f499057447134306a1fc80d2a7d7a74f1dd58faa3ed633afcb90aa"
-    sha256 cellar: :any, big_sur:        "90819e0682f31fb0df1b634b0554d6cf672a37d0d2b06c0218f1d0c786565fdf"
-    sha256 cellar: :any, catalina:       "9ef83d637737e119fefc46f957d43c8d22c1e3a9fed97985919e6a7bad2da58e"
+    sha256 cellar: :any,                 arm64_monterey: "eb43ad624a8cda6e60911b4de70631e5cfa71c7bc0ce2260337f1442243060f7"
+    sha256 cellar: :any,                 arm64_big_sur:  "267fcff261a954ec13a7cd01f59f4db0dbf01215e7df0404121edd9e88cae7c9"
+    sha256 cellar: :any,                 monterey:       "7e8cb99a85f499057447134306a1fc80d2a7d7a74f1dd58faa3ed633afcb90aa"
+    sha256 cellar: :any,                 big_sur:        "90819e0682f31fb0df1b634b0554d6cf672a37d0d2b06c0218f1d0c786565fdf"
+    sha256 cellar: :any,                 catalina:       "9ef83d637737e119fefc46f957d43c8d22c1e3a9fed97985919e6a7bad2da58e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15009a2d7db2bb5e73156a0aa5f24a0b13239791ea47591113d4b57af5e44158"
   end
 
   depends_on "cmake" => :build
