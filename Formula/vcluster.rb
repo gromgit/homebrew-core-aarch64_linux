136927f7157804204c502f7585b937b8fbbc26cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-02 07:37:43 +0000

    vcluster: update 0.12.1 bottle.

--- Formula/vcluster.rb
+++ Formula/vcluster.rb
@@ -8,12 +8,12 @@ class Vcluster < Formula
   head "https://github.com/loft-sh/vcluster.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "58c259a7e59a67d9359c2f805988fdf00657e9fa9d247fc85473275e41f16291"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e39866d1f075e2ee00796a2f6f3f430066bbc330ea5f540e635b823be229e955"
-    sha256                               monterey:       "21010b445b655663c5679ee2c3cac5b2286bd0e2a1534bef6b4dfcf94bc1ab4b"
-    sha256                               big_sur:        "891c16de3f14e1c5c0127e0bf103b28e0bd37091fda87013384c4ecd7714c93f"
-    sha256                               catalina:       "325f1d4a300b02200bc0e1ce9d142f14cfba243b9ae9c7c85a5c1a45b1fceeda"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed74f2631e7c981a6b07e719e492851287e25a776d03bf92f9bde473499677ae"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5743a9ac3882ef84eff379afefd865f66d92a557c2f1b1a36c52688eeef7f321"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "58e5c94f090a7230dc930bbc16f31e538f61e412cb189416543e7b4b6a37b661"
+    sha256 cellar: :any_skip_relocation, monterey:       "6edfabede311db8835fda8fa4fad5044df2c50325311933e6d40c384d276500f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "032ab431fe634f1dc567a27db1ca5e41ee16571e46ab105c58a236774f1a4ba0"
+    sha256 cellar: :any_skip_relocation, catalina:       "85488ddcf6741e5604d6a9d5217539fd3ce1ae983b8ca3c69e8f9f09642260cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ec1d1cae2b5040fcf37813a07a6f6658bf404d2c40ff36fbfcc6c624e248087"
   end
 
   depends_on "go" => :build
