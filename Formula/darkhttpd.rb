a517db697a90844801415b3b8fce61267e9e0ea4
Porkepix <porkepix@gmail.com>
2022-10-02 10:49:04 +0200

    darkhttpd 1.14
    Closes #112139.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/darkhttpd.rb
+++ Formula/darkhttpd.rb
@@ -1,8 +1,8 @@
 class Darkhttpd < Formula
   desc "Small static webserver without CGI"
   homepage "https://unix4lyfe.org/darkhttpd/"
-  url "https://github.com/emikulic/darkhttpd/archive/v1.13.tar.gz"
-  sha256 "1d88c395ac79ca9365aa5af71afe4ad136a4ed45099ca398168d4a2014dc0fc2"
+  url "https://github.com/emikulic/darkhttpd/archive/v1.14.tar.gz"
+  sha256 "e063de9efa5635260c8def00a4d41ec6145226a492d53fa1dac436967670d195"
   license "ISC"
 
   bottle do
