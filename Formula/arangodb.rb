06e37c27f0d3136124026d895b651732fdba5f0a
Michael Cho <cho-m@tuta.io>
2022-10-02 02:28:51 -0700

    arangodb 3.9.3
    Closes #112141.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arangodb.rb
+++ Formula/arangodb.rb
@@ -1,8 +1,8 @@
 class Arangodb < Formula
   desc "Multi-Model NoSQL Database"
   homepage "https://www.arangodb.com/"
-  url "https://download.arangodb.com/Source/ArangoDB-3.9.2.tar.bz2"
-  sha256 "35ac1678b91c0cc448454ef3a76637682d095328570674a5765ae5d060c5721b"
+  url "https://download.arangodb.com/Source/ArangoDB-3.9.3.tar.bz2"
+  sha256 "7650781ba21723b6a361d4cf8a03acbf45a1091bc77704236597a934768b059d"
   license "Apache-2.0"
   head "https://github.com/arangodb/arangodb.git", branch: "devel"
 
@@ -18,40 +18,31 @@ class Arangodb < Formula
     sha256 x86_64_linux: "8c311faac036bab8fc2acee10adf136275ccbf5c976795fadae2b08adaefffaf"
   end
 
-  depends_on "ccache" => :build
   depends_on "cmake" => :build
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
   depends_on "python@3.10" => :build
   depends_on macos: :mojave
   depends_on "openssl@1.1"
 
-  on_linux do
-    depends_on "gcc"
+  # https://www.arangodb.com/docs/stable/installation-compiling-debian.html
+  fails_with :gcc do
+    version "8"
+    cause "requires at least g++ 9.2 as compiler since v3.7"
   end
-
-  fails_with gcc: "5"
 
   # the ArangoStarter is in a separate github repository;
   # it is used to easily start single server and clusters
   # with a unified CLI
   resource "starter" do
     url "https://github.com/arangodb-helper/arangodb.git",
-        tag:      "0.15.4",
-        revision: "ed743d2293efd763309f3ba0a1ba6fb68ac4a41a"
-  end
-
-  # Fix compilation with Apple clang 13.1.6, remove in next release
-  patch do
-    url "https://github.com/arangodb/arangodb/commit/fd43fbc27.patch?full_index=1"
-    sha256 "0298670362e04ec0870f6b7032dff83bfcdf9a04f2fa4763ce5186d4e10a3abb"
+        tag:      "0.15.5",
+        revision: "7832707bbf7d1ab76bb7f691828cfda2a7dc76cb"
   end
 
   def install
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version if OS.mac?
 
     resource("starter").stage do
-      ENV["GO111MODULE"] = "on"
-      ENV["DOCKERCLI"] = ""
       ldflags = %W[
         -s -w
         -X main.projectVersion=#{resource("starter").version}
@@ -60,22 +51,18 @@ class Arangodb < Formula
       system "go", "build", *std_go_args(ldflags: ldflags), "github.com/arangodb-helper/arangodb"
     end
 
-    openssl = Formula["openssl@1.1"]
     args = std_cmake_args + %W[
       -DHOMEBREW=ON
       -DCMAKE_BUILD_TYPE=RelWithDebInfo
       -DUSE_MAINTAINER_MODE=Off
       -DUSE_JEMALLOC=Off
-      -DCMAKE_LIBRARY_PATH=#{openssl.opt_lib}
-      -DOPENSSL_ROOT_DIR=#{openssl.opt_lib}
+      -DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}
       -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
       -DUSE_CATCH_TESTS=Off
       -DUSE_GOOGLE_TESTS=Off
       -DCMAKE_INSTALL_LOCALSTATEDIR=#{var}
     ]
     args << "-DTARGET_ARCHITECTURE=nehalem" if build.bottle? && Hardware::CPU.intel?
-
-    ENV["V8_CXXFLAGS"] = "-O3 -g -fno-delete-null-pointer-checks" if ENV.compiler == "gcc-6"
 
     system "cmake", "-S", ".", "-B", "build", *args
     system "cmake", "--build", "build"
