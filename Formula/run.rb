dba2b976a191847d67fda78cc4dd2a48c75cd17d
Porkepix <porkepix@gmail.com>
2022-10-02 12:47:42 +0200

    run 0.9.1
    Closes #112144.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/run.rb
+++ Formula/run.rb
@@ -1,8 +1,8 @@
 class Run < Formula
   desc "Easily manage and invoke small scripts and wrappers"
   homepage "https://github.com/TekWizely/run"
-  url "https://github.com/TekWizely/run/archive/v0.9.0.tar.gz"
-  sha256 "f7bd2dbcf41b27f67797c2b51291245f64dcf4aef243df205aed22f6f321c4d7"
+  url "https://github.com/TekWizely/run/archive/v0.9.1.tar.gz"
+  sha256 "39aad9167c73101839749d27bd8c915bfd8e4a26ed21d729981b646c2c171ebf"
   license "MIT"
 
   bottle do
