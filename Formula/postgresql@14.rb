1399ac6bebcac07b4f73f8dd6afc2ee5832a8e30
Michael Cho <cho-m@tuta.io>
2022-10-02 18:19:33 -0700

    postgresql@14: revision bump (perl 5.36.0)

--- Formula/postgresql@14.rb
+++ Formula/postgresql@14.rb
@@ -4,7 +4,7 @@ class PostgresqlAT14 < Formula
   url "https://ftp.postgresql.org/pub/source/v14.5/postgresql-14.5.tar.bz2"
   sha256 "d4f72cb5fb857c9a9f75ec8cf091a1771272802f2178f0b2e65b7b6ff64f4a30"
   license "PostgreSQL"
-  revision 4
+  revision 5
 
   livecheck do
     url "https://ftp.postgresql.org/pub/source/"
