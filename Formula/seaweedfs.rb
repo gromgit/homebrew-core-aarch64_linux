5cc1bf89867cf747b16b80e132c57dba6ad22cf1
Porkepix <porkepix@gmail.com>
2022-10-03 04:22:52 +0200

    seaweedfs 3.30
    Closes #112181.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/seaweedfs.rb
+++ Formula/seaweedfs.rb
@@ -2,8 +2,8 @@ class Seaweedfs < Formula
   desc "Fast distributed storage system"
   homepage "https://github.com/chrislusf/seaweedfs"
   url "https://github.com/chrislusf/seaweedfs.git",
-      tag:      "3.29",
-      revision: "1ffb1e696e1f651c6294fc34c86b9abb6db1985d"
+      tag:      "3.30",
+      revision: "29632d5a34b2b8fba4060642b212ea46a3a09a91"
   license "Apache-2.0"
   head "https://github.com/chrislusf/seaweedfs.git", branch: "master"
 
