fc1bb67d065553020a82bec7c0b90ff644f38541
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-03 08:08:22 +0000

    vitess: update 14.0.3 bottle.

--- Formula/vitess.rb
+++ Formula/vitess.rb
@@ -6,12 +6,12 @@ class Vitess < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0941341f66d3a6080acec1ae051ba41a23eff2920d0b5ea28613010f7944fb3c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e42569bda5970732c90ca421f54dbff1fd0566f62f604c518fdb0c9aa0db9969"
-    sha256 cellar: :any_skip_relocation, monterey:       "874b8f51b421951bed01a7285aafc483dd13d2871cb92a0b96993c2cffe5b294"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7cf46a0e20c4f013a4bf4901da4e3c80ccc596cf9f038cdd1e2cb3ec32a81494"
-    sha256 cellar: :any_skip_relocation, catalina:       "6af3734265dac7fd2ec6b9968a83040a20a393ae2952886628695ad1e5401c1f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61d5ae2c2119d207909bdbbecd04daa992418764fb08db9263e422b689e9a7ae"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "978406c8fa38a9234d7f7734277d44fb787588c9dcb3d46aef7e8dc02a35abe6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7b171ce6976b62e76d9a61ca75ebf28528526da7c6e9fb6760b15225543d8a4"
+    sha256 cellar: :any_skip_relocation, monterey:       "6eb65c4772350034e8c5fe04907fbd7e62564d3efa1bad50bb1d08b8a361a6fb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0d30ddbac7090dec3909d3ba3dfba02b58eb0d98a258c898d5c2c97464f12c54"
+    sha256 cellar: :any_skip_relocation, catalina:       "9fc566c5d51a3f4f96cdf03b7c1026870b311733b34de35fea4acc363178da03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "352b48b5dcc9bc5dbe4bd2f9eb0d6cb282e42ed059bd248c642790479f5a2890"
   end
 
   # Try to switch to the latest go on the next release
