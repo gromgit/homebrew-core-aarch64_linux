86f11e1f68929eacad4e2feda72a4dd624046c24
Michael Cho <cho-m@tuta.io>
2022-10-02 17:55:09 -0700

    collectd: revision bump (perl 5.36.0)

--- Formula/collectd.rb
+++ Formula/collectd.rb
@@ -2,7 +2,7 @@ class Collectd < Formula
   desc "Statistics collection and monitoring daemon"
   homepage "https://collectd.org/"
   license "MIT"
-  revision 4
+  revision 5
 
   stable do
     url "https://collectd.org/files/collectd-5.12.0.tar.bz2"
