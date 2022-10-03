d1276ddc8cdd3d376818fb5f47ffb1e7de1aeff3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-03 21:04:46 +0000

    got: update 0.76 bottle.

--- Formula/got.rb
+++ Formula/got.rb
@@ -11,11 +11,11 @@ class Got < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "9640150bdbdefb3bfc59ff7d40feb0e052b15babc635849f9033858e10bcae72"
-    sha256 arm64_big_sur:  "fa01024e3d2dce9e3cdf795f2b1c35dc230824e57b1143d657347ef68e0af897"
-    sha256 monterey:       "fd0a05acd67321140e1f09e71c623183eef553805b199517d6b15b39779cd07f"
-    sha256 big_sur:        "bc7dbd2f0aa44f4abc74ded2cbbfdf818417c7ebd7b916e547afab82f925b05f"
-    sha256 catalina:       "5030e68b85ee3390dd2a45be10d0d59cc8ccc1f53d4636c72a7cf9203bf04c4b"
+    sha256 arm64_monterey: "35dba646a672c747d8ae4d994361507ff7cc45543f6cf6afe6044dab83ee96a8"
+    sha256 arm64_big_sur:  "f444d72268aa821d2d79b10e435bbb2cf9ba724b93a92376118cd1ad083cfc9d"
+    sha256 monterey:       "72fd71b7129d6d1d9d49928968e87800e1ed909da7aade7f8a7e42db15e1c7f4"
+    sha256 big_sur:        "29179856dc8dfe305cb8a17bee13fda5b366387c6c79f99275c146ccf8e5ddf4"
+    sha256 catalina:       "5b1251dbd1203376a237edd4465d16f4903ede332ab6a5817d594464841b63a9"
   end
 
   depends_on "bison" => :build
