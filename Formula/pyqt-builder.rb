185914627b1c2f3e71e6658ed6b911f1c68dabb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-03 16:07:38 +0000

    pyqt-builder: update 1.14.0 bottle.

--- Formula/pyqt-builder.rb
+++ Formula/pyqt-builder.rb
@@ -9,7 +9,7 @@ class PyqtBuilder < Formula
   head "https://www.riverbankcomputing.com/hg/PyQt-builder", using: :hg
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4c6c982868677c0dbeb7bcd0305cf38ea200b24a74be296fbdaa93e26d44c501"
+    sha256 cellar: :any_skip_relocation, all: "428980753b642e14385fe1e5e60ce0a1faf3ef8d1d2b4157f6f9ddebbdcea946"
   end
 
   depends_on "python@3.10"
