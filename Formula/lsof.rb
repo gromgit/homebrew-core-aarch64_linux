9dcf256c1cc250c619076d4f72d54238c9e1343c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-04 04:26:49 +0000

    lsof: update 4.96.3 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -6,12 +6,12 @@ class Lsof < Formula
   license "Zlib"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ca6a2618c47390c2c21a77eafef3fcefa7f139df3a3148f33e147aa1508adc91"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c691f9b32a92b4da66a0b0936317604e7f9861bfc2b3bf82fc5969e9c355bb01"
-    sha256 cellar: :any_skip_relocation, monterey:       "86f1ed5ca097d51d5484e3e935270076f9f118aac8dff9918c14c29333663730"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2ad5bdb2879e358fb7b268b667068552f8068a0d6b1df02d9b8c134ab1d3b68c"
-    sha256 cellar: :any_skip_relocation, catalina:       "5756f095537745ba7e2782a36636779965af3a4bb911c03c0fa46b3dad88ae50"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "905f85033932bae924e52ba4b3cd8a1288901686df6b28a376a60c7db5442ff3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f7c6dce4f448d166e1588a944d0dd3ca5ad402371a665b7dbc5020a2479b551e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ed91f788498429ebb4ed4cc1fa544d47b9c57bdf962302c9ef161b0d5660b09b"
+    sha256 cellar: :any_skip_relocation, monterey:       "26f22fc4df1974bb1066a40aa1c3f13cb0800530c06c08060a0c35554e617c5c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "47fa3e6049ce65e694409c6b37f683c33c6dadc0dda30d3f2450fc9e5eb0f6db"
+    sha256 cellar: :any_skip_relocation, catalina:       "dc9bce7d117ff773df8c1bc431685f2e5c4c8e380ac688d1d8144000ffc95fe2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f296ae44ad1a92009ffc94989c49c34410bbc23eb852d7cad4e436837ee9c21d"
   end
 
   keg_only :provided_by_macos
