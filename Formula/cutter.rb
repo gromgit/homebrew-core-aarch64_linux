0518e7bf041b185ee632238f1f954367984b93ff
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 06:43:41 +0800

    cutter: add 1.2.8 bottle.

--- Formula/cutter.rb
+++ Formula/cutter.rb
@@ -12,14 +12,8 @@ class Cutter < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "3e314f0acebc224eabaa266508356e09142f2834d7b6b2b1611d66eacc2496e3"
-    sha256 arm64_big_sur:  "ac45c9987b4d770856db1f5e2c8fc20fb1ed882297c22691fe29fb153f7b9828"
-    sha256 monterey:       "f6288d98ed9a5fd49d2223ae5a426bed1c9503672f971aae3d1433ebaf2f0d13"
-    sha256 big_sur:        "3ac33f6c41d14b9d1fd3486fe811dda6219d45930b0359f4300b69c50a56572d"
-    sha256 catalina:       "237aebfb6d39c2efcbbc27e550fbac0a6d1477b549416b69aa71c53c06dce231"
-    sha256 mojave:         "70999a7a96da94c5de52da9edb4bf9b3fe5e7b2372d189ccc5a7328f0c21400c"
-    sha256 high_sierra:    "ccff0989fe28eeb233bf0cc1f3681041d1945f6e3b0c2700899b8f02581426b6"
-    sha256 x86_64_linux:   "1f0d55c82c767d2f7d947bdc054a43d381bc6c1b2b09adc2bd3e7e8381059eb2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cutter"
+    sha256 aarch64_linux: "9da730679a9611e7140dc1e0820159eeb46c7462ee6593b216caf93b37d6a50f"
   end
 
   depends_on "intltool" => :build
