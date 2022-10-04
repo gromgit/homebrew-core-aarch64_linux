1daa4caae534ca63bcc753655019152574423cc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-04 19:13:07 +0000

    maxima: update 5.46.0_5 bottle.

--- Formula/maxima.rb
+++ Formula/maxima.rb
@@ -12,12 +12,12 @@ class Maxima < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "86a67871890efae4a5e7b45d4e695a745774e5c09b22c950d0be29e8ec37a325"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59ccd0c85e788d1933944160ac78703633a0513290f01932331c74fb384b8d0d"
-    sha256 cellar: :any_skip_relocation, monterey:       "e1b0f8a598c743bab5094d02583d0c590590aef9dbfbd0399080b7a142132deb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "765d758ced51c1756c0423c1cd9921c4d9afb3845980acea2842c69fc29f1138"
-    sha256 cellar: :any_skip_relocation, catalina:       "45cfbe16c195ded0c8ef33d14bb92107e1e7a65afd049df6b6aa9ec6de539092"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "edc2dbc0a7bcd1318546e0bc3342d6f09acc2c5d1d641a28daf61c147031cf60"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ab666fe2f72acf79b6c17df2a741c4d45ca9e5e3047d74627fb287b7308bce10"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f9c4dedb0129387aa107588f9cff5419249774fbeaaf2860ce5d25a8d4565df8"
+    sha256 cellar: :any_skip_relocation, monterey:       "56e7dbb2b5928893e2cf37d93ffe0bfd8581405da9993222d22921f406554ffd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fc584ffe3cd9ec799ceba2904f80611dfd01807502e40818b7b0107863bbfe6a"
+    sha256 cellar: :any_skip_relocation, catalina:       "f24ac077158d061c058530f4267dd8bc013a1874a97eee7f3c3edf79104736cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c5b4252b1660638798624dd8cd621327bed69f7f63940a9c3cc9232a1517411"
   end
 
   depends_on "gawk" => :build
