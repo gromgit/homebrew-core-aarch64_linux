edcc815eb7835791dba19328ff40ee2151d7fd00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-04 08:06:09 +0000

    git-branchless: update 0.5.1 bottle.

--- Formula/git-branchless.rb
+++ Formula/git-branchless.rb
@@ -7,12 +7,12 @@ class GitBranchless < Formula
   head "https://github.com/arxanas/git-branchless.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7abef4d45e8d02edc34efcfb3b3f283b589fa109d37eed6f71f76fe9bc85fd3c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c65f5936a1f25cb26f8adffd381f756bcc3e8ecfc754fce9edf8e15b7df822e7"
-    sha256 cellar: :any_skip_relocation, monterey:       "6d51dfb0daeaf23b223c28edeb310a76efc211528028dc55df4324bb9db05009"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4f19f476e3d2f8222a7ad733b845bce3ebe672aa7a4a01c8054e76f90f9e3e64"
-    sha256 cellar: :any_skip_relocation, catalina:       "5bcdf8eefad3412aeda382737364e40b6e7c5768eec8c6b9d5c2e51cf7f86a61"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "335ff8cf5ab5f374e5193cffd3691f161ce9e51f28195a542b5bbad0b40a5ba1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cfbc8bc603e716d8eeee574b15e24a3c1bab54e2c90513a97095081d6f67b616"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d7506dece1acd940a882d6679386351bef5ec3bec259370249a9c4d1703109e"
+    sha256 cellar: :any_skip_relocation, monterey:       "5e7373546f19bce2386ef60c04f66bd05fb5818febc11014260dfec64a0bdae3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "21cc2ab14bd209b131e868076a959f50b7c3a3ca18ce30aca9b7d8bbc7862e89"
+    sha256 cellar: :any_skip_relocation, catalina:       "bcfbf85d7623b28b891bd2b812033e87484b3783a1fd8b5158f081db7e376bf3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8c1b3e8702efbe09187902c68e45aa7037725892459e4b1efa61ea044faadacc"
   end
 
   depends_on "rust" => :build
