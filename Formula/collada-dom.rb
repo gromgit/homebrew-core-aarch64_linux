c36bf320806ac240aeb18689112e6f0867e8be29
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 05:59:18 +0800

    collada-dom: add 2.5.0_1 bottle.

--- Formula/collada-dom.rb
+++ Formula/collada-dom.rb
@@ -7,12 +7,8 @@ class ColladaDom < Formula
   head "https://github.com/rdiankov/collada-dom.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6baf8c9373cc9694ee34a651bcc5e7f334853969394d3d4b6b64d20688cf396a"
-    sha256 cellar: :any,                 arm64_big_sur:  "d4c611de9f56ddadaf4e34fdea1049c02289fc81bd0bd9c6e6665f7f15c0f4cb"
-    sha256 cellar: :any,                 monterey:       "764bd5de44b51b088e61df10fd3b8f66581b31ec37821810c6e24d9dd2bc4e32"
-    sha256 cellar: :any,                 big_sur:        "b0987e22d6e0eb6a4b5d5c2413fd629204d1b32c13dd4798bca499446ff89391"
-    sha256 cellar: :any,                 catalina:       "7f2ca06cafbed79ee7df9d03f7842a6bdee997f5d77ed8421b528f79fbe75dd3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e38b4a56ab6e5192f8f8d287fb33f0810c3a09fc1bbd32a490bc1486484594c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/collada-dom"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f66761c2d4394acad86abe042bfdaf304d88d0d7a3bd9470b1cce722367b0ce3"
   end
 
   depends_on "cmake" => :build
