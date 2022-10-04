e6c9d05a4c62092b5b867c20d9afd8426a3289b8
Rui Chen <rui@chenrui.dev>
2022-10-04 13:10:10 -0700

    glooctl 1.12.28
    Closes #112357.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.12.27",
-      revision: "db58ad0bb4da44a0329d728259a3765d28eb47c0"
+      tag:      "v1.12.28",
+      revision: "802f24b56ebd2a88741a58b13dbc1033301c5c36"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
