e478effd74656e5559b9dcf606b24b906125cfcb
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-10-04 18:06:07 +0000

    metview: add 5.17.1 bottle.

--- Formula/metview.rb
+++ Formula/metview.rb
@@ -6,6 +6,15 @@ class Metview < Formula
   sha256 "4706b5eaeb43192c5177c384dcf0860b73415f292bf01c541b622d280672b578"
   license "Apache-2.0"
 
+  bottle do
+    sha256 arm64_monterey: "b2de521f0c422db7499f808e2eb554972657780105377b1e6838b3624b01f0df"
+    sha256 arm64_big_sur:  "d686618aa0f3d897fceb562b7a13b83598912ebf8ab4258ed5e1417de30b4fd9"
+    sha256 monterey:       "b00680591df2df90c560e4dd8d016d044b92297ba9ca0c00dab3b4f14ff2147f"
+    sha256 big_sur:        "e73c9909003de8aadf1083eaab94c4b5e5527dccc362218becbb0fb674b4acf3"
+    sha256 catalina:       "0646ed0d1d06194121176d6126c3245d7a26380e1f9e9d016419c489d86337a6"
+    sha256 x86_64_linux:   "a760b7838c2bee1ac566b55523bb2f62d84ec2de3f8d034badb60b53dec340e1"
+  end
+
   depends_on "cmake" => :build
   depends_on "cairo"
   depends_on "eccodes"
