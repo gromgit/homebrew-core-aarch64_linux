6841109d56f4c18441c73c3a5b9f6ca5e92bace5
Štefan Baebler <stefan.baebler@gmail.com>
2022-10-04 20:59:41 +0200

    go 1.19.2
    Closes #112350.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.19.1.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.19.1.src.tar.gz"
-  sha256 "27871baa490f3401414ad793fba49086f6c855b1c584385ed7771e1204c7e179"
+  url "https://go.dev/dl/go1.19.2.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.19.2.src.tar.gz"
+  sha256 "2ce930d70a931de660fdaf271d70192793b1b240272645bf0275779f6704df6b"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
 
