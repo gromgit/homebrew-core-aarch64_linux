ba1cf885f256cfe8482a953bcf37a017c0f9a3da
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 18:09:07 +0800

    rsync: add 3.2.5 bottle.

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -1,10 +1,10 @@
 class Rsync < Formula
   desc "Utility that provides fast incremental file transfer"
   homepage "https://rsync.samba.org/"
-  url "https://rsync.samba.org/ftp/rsync/rsync-3.2.4.tar.gz"
-  mirror "https://mirrors.kernel.org/gentoo/distfiles/rsync-3.2.4.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-3.2.4.tar.gz"
-  sha256 "6f761838d08052b0b6579cf7f6737d93e47f01f4da04c5d24d3447b7f2a5fad1"
+  url "https://rsync.samba.org/ftp/rsync/rsync-3.2.5.tar.gz"
+  mirror "https://mirrors.kernel.org/gentoo/distfiles/rsync-3.2.5.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-3.2.5.tar.gz"
+  sha256 "2ac4d21635cdf791867bc377c35ca6dda7f50d919a58be45057fd51600c69aba"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -13,12 +13,8 @@ class Rsync < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "5203ca4a96801c82df6304be050d16676edeb1179d060055c0a0d024719c647f"
-    sha256 cellar: :any,                 arm64_big_sur:  "7a97c83e5bdc7abfec4e3a605e5e7f2c142378dbb14719930b5da58ac11f8c58"
-    sha256 cellar: :any,                 monterey:       "57d1cf3bed28db1f850a5ce119dbd9a4ca6de38ec446a7a406e0f9510446af50"
-    sha256 cellar: :any,                 big_sur:        "1b9467be7a8c6199fd3b86ff1e316d8d6ada4ddd22b9ba3c4f0449207b9c36e0"
-    sha256 cellar: :any,                 catalina:       "76221855501830c9a97f08cdf24003d2540934f12d8758883cfbea2780bb827e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25827ec52d77b05cadd5930d0d0b81f327477639badaafb538b755c886a9f677"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rsync"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "18af2c8e3eae39fc22d14c0c9096a9b05edc0e0b880fa337523dd8b3d2f136c8"
   end
 
   depends_on "lz4"
