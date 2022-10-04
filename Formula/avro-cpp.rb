53ab57f69af6debd724935241f6cfb3e2e283e8d
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 03:05:09 +0800

    avro-cpp: add 1.11.0_1 bottle.

--- Formula/avro-cpp.rb
+++ Formula/avro-cpp.rb
@@ -8,12 +8,8 @@ class AvroCpp < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "afc1ca56809de324699a3591a9cfc69b64910abd765ed5ed23fc147ed3f3e981"
-    sha256 cellar: :any,                 arm64_big_sur:  "a1dc26c7cc77ab1336819aa3b9cfa89d92829a58b1379fa7fc272e292dc3a7ca"
-    sha256 cellar: :any,                 monterey:       "62f6406c61b7ee09446b866e30b2718ff17323ae7341d9c30cba748cf1ef0e93"
-    sha256 cellar: :any,                 big_sur:        "c6d71d0ccb148b12c2d3418a9b9a39c224e3166e5f375f9833b5c11d070c48a0"
-    sha256 cellar: :any,                 catalina:       "4d94de0a0aaf77588d3c0f0b3255861ec89c24d5235dedd4a190f3ba4256b94b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f29988613e601a8240626aac694acebd0410c9777521ce30b30a550a9c84035"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/avro-cpp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a7574446d37df2897f18bc0682872b317531fdd5d2066ea6fc7722697fe50992"
   end
 
   depends_on "cmake" => :build
