a6dee0eee76f09f9aa1ab54156210562d8aa9dcf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-04 08:06:15 +0000

    mist: update 1.9 bottle.

--- Formula/mist.rb
+++ Formula/mist.rb
@@ -7,10 +7,10 @@ class Mist < Formula
   head "https://github.com/ninxsoft/mist-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8214125ac548a63db1c1ea0b59c0b315b15ca8497809d6e1d5587d3c18ac3919"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2de8592da1c11ff5d2dd5a6b9c78caca53641d481f4ee642aa11277be3d2d519"
-    sha256 cellar: :any_skip_relocation, monterey:       "359271650bdc927599b8a19a78b97392cd352e8c415defd4eec4f0de0a0ad5a4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "117db2644b9547c56bd56adfde3d8285ee7c3fa8391dcd3d5b493574d3c29c16"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ace98c851f364b27361f705ea322f9666128dcdae270c642b61f199ed951bed9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0e4b84bef799cb4a99f0360c9238b890ac4e1262e42d638fcbcfb9108ec93048"
+    sha256 cellar: :any_skip_relocation, monterey:       "95b39064f9cbc917aa44c96306673d03b449f762177112ead711dcaa31ae0593"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3aa5c93b6d4fd30b1f540984df272ef17acfd6fac15f5d2e0afd7c228e2709bd"
   end
 
   # Mist requires Swift 5.5
