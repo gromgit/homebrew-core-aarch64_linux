868d78ce615eee65d856acadc3f83bbbb29db991
Rui Chen <rui@chenrui.dev>
2022-10-04 13:18:28 -0700

    zbctl 8.1.0
    Closes #112359.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zbctl.rb
+++ Formula/zbctl.rb
@@ -2,8 +2,8 @@ class Zbctl < Formula
   desc "Zeebe CLI client"
   homepage "https://docs.camunda.io/docs/apis-clients/cli-client/index/"
   url "https://github.com/camunda/zeebe.git",
-      tag:      "8.0.6",
-      revision: "eab2ac867d0dba7208b2322679085f395bbb6db2"
+      tag:      "8.1.0",
+      revision: "9233af41da323e67867ad1896eaeb59730880f43"
   license "Apache-2.0"
   head "https://github.com/camunda/zeebe.git", branch: "develop"
 
