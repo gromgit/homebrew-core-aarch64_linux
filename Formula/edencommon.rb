2b4f3c9d05842ef9676e7086abb95be5c3d1b130
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-10-04 13:27:53 +0000

    edencommon: add 2022.10.03.00 bottle.

--- Formula/edencommon.rb
+++ Formula/edencommon.rb
@@ -6,6 +6,15 @@ class Edencommon < Formula
   license "MIT"
   head "https://github.com/facebookexperimental/edencommon.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "26c216c6ac767a1c375e0628f308fddda47cdfd74a2541bd4d3414bf74d039f3"
+    sha256 cellar: :any,                 arm64_big_sur:  "b8ce2fe2b11704dc94755e111a0661ab093dde5b71b9dc275701bea4ac15e3b7"
+    sha256 cellar: :any,                 monterey:       "9d84be36d7c4ac1ae077fa732233e86fc2e11ef3511c0a31df3bac55d3827867"
+    sha256 cellar: :any,                 big_sur:        "7cb5bb9ab26431ae81b5d343b53e4787a5b15a5a827417b8cf0c5563608f01db"
+    sha256 cellar: :any,                 catalina:       "0587ec366afbafb7ea93e42ff9208f8c94b3aeafa254203bdb6619502ac7ef45"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ecb93a06baed0e0385f97c683fc9665f082f6599f9c14ec1fb453ec8b216b51c"
+  end
+
   depends_on "cmake" => :build
   depends_on "googletest" => :build
   depends_on "folly"
