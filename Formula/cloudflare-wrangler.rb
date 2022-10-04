c3f9ff6b766a24e9fcbb4501c19bf21f8d6e6e86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-04 08:06:22 +0000

    cloudflare-wrangler: update 1.20.0 bottle.

--- Formula/cloudflare-wrangler.rb
+++ Formula/cloudflare-wrangler.rb
@@ -7,12 +7,12 @@ class CloudflareWrangler < Formula
   head "https://github.com/cloudflare/wrangler.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f84b69516bbe150fef52a9dd20619008dcb80572ccea80412013e4054fa9a594"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad4f239eee085f4aa2dfd7fccf64bf64446a25cd661d6e9593567d67f48c3a80"
-    sha256 cellar: :any_skip_relocation, monterey:       "dabdde3faa724209d1100f35191bedcc6b1c4e091c0936d64aff45317dcc50f6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "63e222a03ab1f63317ad25a57f2a1c8c30d0f6c5c015fe7408f87f3305cc16b3"
-    sha256 cellar: :any_skip_relocation, catalina:       "d3107d9cbfee644078a2d0254ae96d9716d202a0cb08f7200f5d416fcdfaf3c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c40cdcd3ed3657dfc7faddbe5f7fc5486e3b3dc5883272a4f3e643c5f0a78cf2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "770767175d90b1b0ce0efba7e0ed89a9455a1c3b7597a2121215175fc7d40918"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2a6bb4463361a162f236e8d7f7714e643ea5318928ada6b527ceefdcd8eb435d"
+    sha256 cellar: :any_skip_relocation, monterey:       "fb5ee30ee2c08e317bf17a36e2219bd06a9be8ce41551a769e64c6d79ea8d7cb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8e7fd0bf436fc73ecf82a3575500b5dd37a2d1efc549762767a9899a7824f576"
+    sha256 cellar: :any_skip_relocation, catalina:       "56e6d54716315c45b5198782c5f00b95fc9a9a99b36dbba1a06cb3b7c9c819d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "89909585925717cb4e0e97532da21898d3cc027ae9ab1e14dd56225cb72e3a8a"
   end
 
   depends_on "rust" => :build
