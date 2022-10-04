1739bb0f214eba7bd129f2446b6c93a6e1aa9b33
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 06:22:32 +0800

    criterion: add 2.4.1 bottle.

--- Formula/criterion.rb
+++ Formula/criterion.rb
@@ -7,12 +7,8 @@ class Criterion < Formula
   head "https://github.com/Snaipe/Criterion.git", branch: "bleeding"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "5a284e7291b02f9b99c157baf3023e3a6bbc4ba904999b8b094f6f19db625f71"
-    sha256 cellar: :any, arm64_big_sur:  "756811bab129f1d12072e184806f3d0c2d340ea88eeba2dba470d0e46a0c5cc0"
-    sha256 cellar: :any, monterey:       "7f58f9eaeb7370f5ee50b871afa82dd6da248822b09495e602e551f7345cf7e2"
-    sha256 cellar: :any, big_sur:        "c29c0ee229ff746bbe8b1ff0040376fce39d8ee0a359566ac2304d9b39759b3f"
-    sha256 cellar: :any, catalina:       "2eaaa7be419c50953093f6ad8a9ee33eb22e820f245b1804e3c163a53420aaac"
-    sha256               x86_64_linux:   "fb380b5c7cdb5d367b46807a8ca453a468ed262d1f054fb6824c45c9e8c5d8d5"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/criterion"
+    sha256 aarch64_linux: "c283cbacd50da667135904658f961ceedcb80b865c1610eb285f9ce05b30a364"
   end
 
   depends_on "cmake" => :build
