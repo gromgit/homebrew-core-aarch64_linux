a5ad74724d6b4ee44f245f299792acac2725a010
Michael Cho <cho-m@tuta.io>
2022-10-05 01:00:12 -0700

    arangodb 3.10.0
    Closes #112392.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arangodb.rb
+++ Formula/arangodb.rb
@@ -1,8 +1,8 @@
 class Arangodb < Formula
   desc "Multi-Model NoSQL Database"
   homepage "https://www.arangodb.com/"
-  url "https://download.arangodb.com/Source/ArangoDB-3.9.3.tar.bz2"
-  sha256 "7650781ba21723b6a361d4cf8a03acbf45a1091bc77704236597a934768b059d"
+  url "https://download.arangodb.com/Source/ArangoDB-3.10.0.tar.bz2"
+  sha256 "48b36216541f9da8ca86ce304a3ea96964da53a324c94c9a229552af70949726"
   license "Apache-2.0"
   head "https://github.com/arangodb/arangodb.git", branch: "devel"
 
@@ -24,6 +24,19 @@ class Arangodb < Formula
   depends_on macos: :mojave
   depends_on "openssl@1.1"
 
+  on_macos do
+    depends_on "llvm" => :build
+  end
+
+  fails_with :clang do
+    cause <<-EOS
+      .../arangod/IResearch/AqlHelper.h:563:40: error: no matching constructor
+      for initialization of 'std::string_view' (aka 'basic_string_view<char>')
+              std::forward<Visitor>(visitor)(std::string_view{prev, begin});
+                                             ^               ~~~~~~~~~~~~~
+    EOS
+  end
+
   # https://www.arangodb.com/docs/stable/installation-compiling-debian.html
   fails_with :gcc do
     version "8"
@@ -40,7 +53,14 @@ class Arangodb < Formula
   end
 
   def install
-    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version if OS.mac?
+    if OS.mac?
+      ENV.llvm_clang
+      ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+      # Fix building bundled boost with newer LLVM by avoiding removed `std::unary_function`.
+      # .../boost/1.78.0/boost/container_hash/hash.hpp:132:33: error: no template named
+      # 'unary_function' in namespace 'std'; did you mean '__unary_function'?
+      ENV.append "CXXFLAGS", "-DBOOST_NO_CXX98_FUNCTION_BASE=1"
+    end
 
     resource("starter").stage do
       ldflags = %W[
@@ -48,21 +68,36 @@ class Arangodb < Formula
         -X main.projectVersion=#{resource("starter").version}
         -X main.projectBuild=#{Utils.git_head}
       ]
-      system "go", "build", *std_go_args(ldflags: ldflags), "github.com/arangodb-helper/arangodb"
+      system "go", "build", *std_go_args(ldflags: ldflags)
+    end
+
+    arch = if Hardware::CPU.arm?
+      "neon"
+    elsif !build.bottle?
+      # Allow local source builds to optimize for host hardware.
+      # We don't set this on ARM since auto-detection isn't supported.
+      "auto"
+    elsif Hardware.oldest_cpu == :core2
+      # Closest options to Homebrew's core2 are `core`, `merom`, and `penryn`.
+      # `core` only enables up to SSE3 so we use `merom` which enables up to SSSE3.
+      # As -march=merom doesn't exist in GCC/LLVM, build will fall back to -march=core2
+      "merom"
+    else
+      Hardware.oldest_cpu
     end
 
     args = std_cmake_args + %W[
       -DHOMEBREW=ON
       -DCMAKE_BUILD_TYPE=RelWithDebInfo
-      -DUSE_MAINTAINER_MODE=Off
-      -DUSE_JEMALLOC=Off
-      -DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
-      -DUSE_CATCH_TESTS=Off
-      -DUSE_GOOGLE_TESTS=Off
       -DCMAKE_INSTALL_LOCALSTATEDIR=#{var}
+      -DCMAKE_INSTALL_SYSCONFDIR=#{etc}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      -DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}
+      -DTARGET_ARCHITECTURE=#{arch}
+      -DUSE_GOOGLE_TESTS=OFF
+      -DUSE_JEMALLOC=OFF
+      -DUSE_MAINTAINER_MODE=OFF
     ]
-    args << "-DTARGET_ARCHITECTURE=nehalem" if build.bottle? && Hardware::CPU.intel?
 
     system "cmake", "-S", ".", "-B", "build", *args
     system "cmake", "--build", "build"
