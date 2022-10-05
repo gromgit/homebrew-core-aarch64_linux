f28cedba93b88f2d96aac2326bfdd9fb2eda47f3
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 02:40:55 +0800

    proj@7: add 7.2.1 bottle.

--- Formula/proj@7.rb
+++ Formula/proj@7.rb
@@ -6,13 +6,8 @@ class ProjAT7 < Formula
   license "MIT"
 
   bottle do
-    sha256 arm64_monterey: "29f65a3b916e6b967c157713b7a00daf1b91c01092628c292451ff5ade277ecf"
-    sha256 arm64_big_sur:  "de4c07c82bb48aaacce06186daaa95e975ec13d9c63978ec74e46d00e29c9b4e"
-    sha256 monterey:       "4315a9c41f5f97d1fbf80cf011b23e901009d28536897aeb0b640e8324a379af"
-    sha256 big_sur:        "85fe28900d71302e7a6a433cf97f6c2bbffd760578a731f502a3dbea2be02917"
-    sha256 catalina:       "34dc4a8dda8470ec24951126171ac370232d215a4623e8c8b67161b52a875706"
-    sha256 mojave:         "cfda390ef15a53e47786071d599ded6ecfb540c3c70ae0174135d50c481f058b"
-    sha256 x86_64_linux:   "00f08eda239998c9ecb8448a71320c8376ef03e0e02f27035b2628bcc61a74a6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/proj@7"
+    sha256 aarch64_linux: "095d950ca03a9dd34239633158e47d39797b5710bdaef185325804eeab41b6f1"
   end
 
   keg_only :versioned_formula
