b8a318ab9bbfbb2b04b919d30551786cf2a06d90
Rui Chen <rui@chenrui.dev>
2022-10-05 11:34:12 -0700

    filebeat 8.4.3
    Closes #112429.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -2,8 +2,8 @@ class Filebeat < Formula
   desc "File harvester to ship log files to Elasticsearch or Logstash"
   homepage "https://www.elastic.co/products/beats/filebeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.4.2",
-      revision: "b00a6bca7be493b01a134a6ad8c415f2be297414"
+      tag:      "v8.4.3",
+      revision: "c2f2aba479653563dbaabefe0f86f5579708ec94"
   # Outside of the "x-pack" folder, source code in a given file is licensed
   # under the Apache License Version 2.0
   license "Apache-2.0"
