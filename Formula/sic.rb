6d6fa125dfd684e83e5437b6301c23816fb88cb7
Porkepix <porkepix@gmail.com>
2022-10-05 10:43:40 +0200

    sic 1.3
    Closes #112405.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sic.rb
+++ Formula/sic.rb
@@ -1,8 +1,8 @@
 class Sic < Formula
   desc "Minimal multiplexing IRC client"
   homepage "https://tools.suckless.org/sic/"
-  url "https://dl.suckless.org/tools/sic-1.2.tar.gz"
-  sha256 "ac07f905995e13ba2c43912d7a035fbbe78a628d7ba1c256f4ca1372fb565185"
+  url "https://dl.suckless.org/tools/sic-1.3.tar.gz"
+  sha256 "30478fab3ebc75f2eb5d08cbb5b2fedcaf489116e75a2dd7197e3e9c733d65d2"
   license "MIT"
   head "https://git.suckless.org/sic", branch: "master", using: :git
 
