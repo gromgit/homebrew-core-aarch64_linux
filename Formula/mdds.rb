04dff2961bc98b994e26afcd28351695edb3ef92
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 22:04:17 +0800

    mdds: add 2.0.3 bottle.

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -1,12 +1,13 @@
 class Mdds < Formula
   desc "Multi-dimensional data structure and indexing algorithm"
   homepage "https://gitlab.com/mdds/mdds"
-  url "https://kohei.us/files/mdds/src/mdds-2.0.2.tar.bz2"
-  sha256 "13211f2f2e387ef3b74d73a1dcee52a1ad5ce06df8f8e6647679df9278a3116a"
+  url "https://kohei.us/files/mdds/src/mdds-2.0.3.tar.bz2"
+  sha256 "9771fe42e133443c13ca187253763e17c8bc96a1a02aec9e1e8893367ffa9ce5"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "800ca1b6aab72e6e32965fd3e156c05e603c5686473c61611e2ae1c7c99b1d9b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/mdds"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2c5400bc8e73fdc54ef7e1b6c87279f7624f80528da0e3f7a8afccfc57104e0e"
   end
 
   head do
