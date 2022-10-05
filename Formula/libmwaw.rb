e225602af8314386b3eb9aa10ca50a37661e2ac9
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 21:02:13 +0800

    libmwaw: add 0.3.21 bottle.

--- Formula/libmwaw.rb
+++ Formula/libmwaw.rb
@@ -6,12 +6,8 @@ class Libmwaw < Formula
   license any_of: ["LGPL-2.1-or-later", "MPL-2.0"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8e8b9c289bfacec49f6698fb9b3024c92b86e89d6712069112faf4e777616b45"
-    sha256 cellar: :any,                 arm64_big_sur:  "6079d619891c7379f984bb8e9c50f090183ee97fc291bbbcf354b7e73cf334ab"
-    sha256 cellar: :any,                 monterey:       "ec17c3a3d8014901bb3dedca4c2d532c8f343c28f39130d49ac29c88b5dd47e6"
-    sha256 cellar: :any,                 big_sur:        "81fab7be2a6f5352ab9757cfa5a93827debc0a0469897a4b28922ca31a53fba5"
-    sha256 cellar: :any,                 catalina:       "98c7ef307cac9580e285a6c871b392cc7e67036906ff8565d55bbb5c6498850a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8853b2f34f3f561be4cef972c911697afd90e1404207f73f782cb113cb0e4ec9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libmwaw"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2ca4a92a3dac5383543570d3c8730dcdaa11d81d2e498192a16b86fd95178f77"
   end
 
   depends_on "pkg-config" => :build
