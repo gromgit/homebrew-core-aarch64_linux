81916685e1dfc3c1753ee7d5b8c6425b322ab8b6
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 21:44:44 +0800

    luau: add 0.543 bottle.

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -1,18 +1,14 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau-lang.org"
-  url "https://github.com/Roblox/luau/archive/0.526.tar.gz"
-  sha256 "64b39fd654db44f66eed8be235d33ee94d624d5d847bba8fa69a2003a6977712"
+  url "https://github.com/Roblox/luau/archive/0.543.tar.gz"
+  sha256 "107d9408e2db71ec19434138b428f673ac1674d021f0ccae98af39b364cc2912"
   license "MIT"
   head "https://github.com/Roblox/luau.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a89d0f7d9f410d2cb9b088d54fa3cba2271329398a08b81ffa45b8cf2d265f1f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c18d270097c78cef46acb074db3f16a3ce4c15d0e60ff78be9ad25f9fa2b866d"
-    sha256 cellar: :any_skip_relocation, monterey:       "55c1978d606bc10ff3dacaac56c03fd41e5adf1b57ffc95683a7c48071f47bc3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e64c3c45a11bb6612c849baaa62227503391f0e25ef2821bf6745df52167ba72"
-    sha256 cellar: :any_skip_relocation, catalina:       "5ef8da9c25369191bea2c41d077bf90a24c9f698ec159bdc718488ac94d4eda2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f13197767670c0ed6d29648bbf88de04ed75d20bb7ff35b8065251c3f3aaaa6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/luau"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f6d5606c5fb2f850dc2548daf637fe96c5057d80c12acfef4c766a6198f30851"
   end
 
   depends_on "cmake" => :build
