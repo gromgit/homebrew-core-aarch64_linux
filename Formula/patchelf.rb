baa0a49cfb7db8e854f2bd11f444d0385458946b
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 01:37:57 +0800

    patchelf: add 0.15.0_1 bottle.

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -1,9 +1,10 @@
 class Patchelf < Formula
   desc "Modify dynamic ELF executables"
   homepage "https://github.com/NixOS/patchelf"
-  url "https://github.com/NixOS/patchelf/releases/download/0.14.5/patchelf-0.14.5.tar.bz2"
-  sha256 "b9a46f2989322eb89fa4f6237e20836c57b455aa43a32545ea093b431d982f5c"
+  url "https://github.com/NixOS/patchelf/releases/download/0.15.0/patchelf-0.15.0.tar.bz2"
+  sha256 "f4036d3ee4d8e228dec1befff0f6e46d8a40e9e570e0068e39d77e62e2c8bdc2"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://github.com/NixOS/patchelf.git", branch: "master"
 
   livecheck do
@@ -12,12 +13,8 @@ class Patchelf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "93f86615872480fa711de7c7abb8874b102a0ec71a6c7b526f24de390e768c64"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c44eb9355929d5d5a29eef98ba237bb0be07e887dd1a78d597f9193eb4c6a86f"
-    sha256 cellar: :any_skip_relocation, monterey:       "56aa96ca13ab94c578f37dbd6f27a66df3f8a7ff065c69e24fe96945570dc5ea"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4066a672a259b9f5e63c8733848436c7b327a7855cdf9befd0634f437314971b"
-    sha256 cellar: :any_skip_relocation, catalina:       "c1c28ffdfd4dc20b6fbea1db1f1d26c1456968a041bf99e84bf9973719164884"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b78bf9f5d693ab680158e2bdf341c4915712078fee53421c05f5a87af7f2d9cc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/patchelf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7a6aa5eaee3e20ad74d1f907ce22e14c0e9dde34c87ad4ec8a806ab126632d64"
   end
 
   on_linux do
