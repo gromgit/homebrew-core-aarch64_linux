9f47218d986aed04f2ba328a2af0d8c3f07b3691
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 18:00:09 +0800

    tcpflow: add 1.6.1 bottle.

--- Formula/tcpflow.rb
+++ Formula/tcpflow.rb
@@ -11,13 +11,8 @@ class Tcpflow < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2e25618ff226f705ee66793309e8f1d01b9adf8e51f1930e5c3ff52fb904cd28"
-    sha256 cellar: :any,                 arm64_big_sur:  "45666c536a212cbc2b76a6663e051f432e4b82910a440d5fa6cebad4562e70f9"
-    sha256 cellar: :any,                 monterey:       "a964e150a0429750379df21f3a1e24b4ac336ca15b66cffdbeaba98e3edc35c4"
-    sha256 cellar: :any,                 big_sur:        "ec65cbfeff09cd48c9accca03cf14a733034b96f0d01d47cbcf43ef9e0e859de"
-    sha256 cellar: :any,                 catalina:       "78b9e40f778060e2a0a277dfa1ff2d3ee720be679f8ade7b98e274ace2a05e7c"
-    sha256 cellar: :any,                 mojave:         "752820d85c73654edd4b2eef81a36d6d3be542e8cb6f7f62af7906b0740ba98f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a0a2015d8bb5ac2ddca983dd577325a59288527fc77309fe628cdf7a6e55922e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tcpflow"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8fe8ac526e9bfc1fd7bb4b2d002ebbc38f531275f82cb2390c88c6ea12167b6b"
   end
 
   head do
