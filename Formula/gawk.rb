77f92bfdde74bedecd3e1e18d96d0d91f6b48870
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-10-06 23:19:27 +0800

    gawk 5.2.0
    Closes #112422.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gawk.rb
+++ Formula/gawk.rb
@@ -1,11 +1,22 @@
 class Gawk < Formula
   desc "GNU awk utility"
   homepage "https://www.gnu.org/software/gawk/"
-  url "https://ftp.gnu.org/gnu/gawk/gawk-5.1.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gawk/gawk-5.1.1.tar.xz"
-  sha256 "d87629386e894bbea11a5e00515fc909dc9b7249529dad9e6a3a2c77085f7ea2"
   license "GPL-3.0-or-later"
   head "https://git.savannah.gnu.org/git/gawk.git", branch: "master"
+
+  # Remove stable block when patch is no longer needed.
+  stable do
+    url "https://ftp.gnu.org/gnu/gawk/gawk-5.2.0.tar.xz"
+    mirror "https://ftpmirror.gnu.org/gawk/gawk-5.2.0.tar.xz"
+    sha256 "e4ddbad1c2ef10e8e815ca80208d0162d4c983e6cca16f925e8418632d639018"
+
+    # Patch taken from:
+    # https://git.savannah.gnu.org/cgit/gawk.git/patch/?id=53d97efad03453b0fff5a941170db6b7abdb2083
+    # This fixes build on macOS arm64. Persistent memory allocator (PMA) is not
+    # working there.
+    # Remove on next release, which will supposedly come with this patch.
+    patch :DATA
+  end
 
   bottle do
     sha256 arm64_monterey: "093465f34b94ec8ddeb4ff8dab2a02dafbccf8ec05f6ef0391673b7c4fd0a91f"
@@ -48,3 +59,28 @@ class Gawk < Formula
     assert_equal "Homebrew", output.strip
   end
 end
+
+__END__
+--- a/configure
++++ b/configure
+@@ -12722,8 +12722,18 @@ fi
+ 
+ 			;;
+ 		*darwin*)
+-			LDFLAGS="${LDFLAGS} -Xlinker -no_pie"
+-			export LDFLAGS
++			# 30 September 2022: PMA works on Intel but not
++			# on M1, disable it, until it gets fixed
++			case $host in
++			x86_64-*)
++				LDFLAGS="${LDFLAGS} -Xlinker -no_pie"
++				export LDFLAGS
++				;;
++			*)
++				# aarch64-*
++				use_persistent_malloc=no
++				;;
++			esac
+ 			;;
+ 		*cygwin* | *CYGWIN* | *solaris2.11* | freebsd13.* | openbsd7.* )
+ 			true	# nothing do, exes on these systems are not PIE
