79341d62cb094391ab578037a990e667cdef2e48
Adrian Ho <the.gromgit@gmail.com>
2022-10-07 02:15:50 +0800

    vcpkg: add 2022.07.21_1 bottle.

--- Formula/vcpkg.rb
+++ Formula/vcpkg.rb
@@ -1,10 +1,11 @@
 class Vcpkg < Formula
   desc "C++ Library Manager"
   homepage "https://github.com/microsoft/vcpkg"
-  url "https://github.com/microsoft/vcpkg-tool/archive/2022-03-09.tar.gz"
-  version "2022.03.09"
-  sha256 "174a9081059efc29f8c617e6d673d342388816872a3c57c7042051d029b77841"
+  url "https://github.com/microsoft/vcpkg-tool/archive/2022-07-21.tar.gz"
+  version "2022.07.21"
+  sha256 "44085e694a913d529d8f16d03cb9d3c7ba614e82452fc0156bb075f7f6df5920"
   license "MIT"
+  revision 1
   head "https://github.com/microsoft/vcpkg-tool.git", branch: "main"
 
   # The source repository has pre-release tags with the same
@@ -16,12 +17,8 @@ class Vcpkg < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "9876b58572d7337e07b2528070037258682d2f6ba3ca3c37df221cfd10605d29"
-    sha256 cellar: :any,                 arm64_big_sur:  "39df2d10001621063de926d427f854a7a5bb419f813782c23fca71ab2a4ef09e"
-    sha256 cellar: :any,                 monterey:       "62c62da16ae59fd1b003722aef6b4d0853d68bb5f696ba15f626b3c63d026c03"
-    sha256 cellar: :any,                 big_sur:        "e937ec387b962ec67f15c3e0cb9456f22b6d7be4dfbd0a517598c5ab3a7abf6b"
-    sha256 cellar: :any,                 catalina:       "14440a9255341d0a2d68e78ff54bff9deca91ed1dfa645dfb022ea964321b303"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85287583bb21bda7eb08c6badc6594cbc97634000559ff8287fa00d75f1a7cb6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vcpkg"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f74458788fc20019b9c46e7736538ea03f2ffc1bd3e30ba5a599a997aa853ee2"
   end
 
   depends_on "cmake" => :build
@@ -34,9 +31,16 @@ class Vcpkg < Formula
 
   fails_with gcc: "5"
 
+  # Fix build with fmt 9+
+  # https://github.com/microsoft/vcpkg-tool/pull/634
+  patch do
+    url "https://github.com/microsoft/vcpkg-tool/commit/5fba654cc47c175202491a5d80f280c0e39b364d.patch?full_index=1"
+    sha256 "e9a61b8fc8e2ce21ae5eae36bf36b5e5214b0a624ef7c938ddea0e739a61340a"
+  end
+
   def install
     # Improve error message when user fails to set `VCPKG_ROOT`.
-    inreplace ["src/vcpkg/vcpkgpaths.cpp", "locales/messages.json"],
+    inreplace ["include/vcpkg/base/messages.h", "locales/messages.json", "locales/messages.en.json"],
               "If you are trying to use a copy of vcpkg that you've built, y", "Y"
 
     system "cmake", "-S", ".", "-B", "build",
@@ -52,14 +56,15 @@ class Vcpkg < Formula
   # This is specific to the way we install only the `vcpkg` tool.
   def caveats
     <<~EOS
-      This formula provieds only the `vcpkg` executable. To use vcpkg:
+      This formula provides only the `vcpkg` executable. To use vcpkg:
         git clone https://github.com/microsoft/vcpkg "$HOME/vcpkg"
         export VCPKG_ROOT="$HOME/vcpkg"
     EOS
   end
 
   test do
-    message = "Error: Could not detect vcpkg-root. You must define the VCPKG_ROOT environment variable"
+    # DO NOT CHANGE. If the test breaks then the `inreplace` needs fixing.
+    message = "error: Could not detect vcpkg-root. You must define the VCPKG_ROOT environment variable"
     assert_match message, shell_output("#{bin}/vcpkg search sqlite", 1)
   end
 end
