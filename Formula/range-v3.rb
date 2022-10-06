3ad361071d1af4b1c62e0f9ae19ca2b624fc7172
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 10:10:35 +0800

    range-v3: add 0.12.0 bottle.

--- Formula/range-v3.rb
+++ Formula/range-v3.rb
@@ -1,20 +1,13 @@
 class RangeV3 < Formula
   desc "Experimental range library for C++14/17/20"
   homepage "https://ericniebler.github.io/range-v3/"
-  url "https://github.com/ericniebler/range-v3/archive/0.11.0.tar.gz"
-  sha256 "376376615dbba43d3bef75aa590931431ecb49eb36d07bb726a19f680c75e20c"
+  url "https://github.com/ericniebler/range-v3/archive/0.12.0.tar.gz"
+  sha256 "015adb2300a98edfceaf0725beec3337f542af4915cec4d0b89fa0886f4ba9cb"
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "764f7e1ace531b874b0e1b5e6fd927323fb14e1ba1b53b11e34a65fd9cd2a7db"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2500d54ef2231b737505f3cad48f4a3ffb996b3941b8cbee4f8e2b1a44692aec"
-    sha256 cellar: :any_skip_relocation, monterey:       "764f7e1ace531b874b0e1b5e6fd927323fb14e1ba1b53b11e34a65fd9cd2a7db"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f22133b9d6ec765bef17f0ad8a81796ceafff1067954eb46a019b63e6aaa4f91"
-    sha256 cellar: :any_skip_relocation, catalina:       "bffbe0872b344db9b7838d3a63b10e95df57385d26bfaeffc4da5a3d940893c6"
-    sha256 cellar: :any_skip_relocation, mojave:         "bffbe0872b344db9b7838d3a63b10e95df57385d26bfaeffc4da5a3d940893c6"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "bffbe0872b344db9b7838d3a63b10e95df57385d26bfaeffc4da5a3d940893c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "764f7e1ace531b874b0e1b5e6fd927323fb14e1ba1b53b11e34a65fd9cd2a7db"
-    sha256 cellar: :any_skip_relocation, all:            "946ebe20e9c73966759c6a388c1ac11d08702acb80cd3022354e5fae756eec05"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/range-v3"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f93402844737dfd78847e950ee96fe8d95e0dba1a1c71a3e3cdefd11e5b1ec9b"
   end
 
   depends_on "cmake" => :build
