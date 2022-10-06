606aabc3a706c19266c9a20c57be205f1fec07b6
Adrian Ho <the.gromgit@gmail.com>
2022-10-07 03:45:59 +0800

    z3: add 4.11.2 bottle.

--- Formula/z3.rb
+++ Formula/z3.rb
@@ -1,8 +1,8 @@
 class Z3 < Formula
   desc "High-performance theorem prover"
   homepage "https://github.com/Z3Prover/z3"
-  url "https://github.com/Z3Prover/z3/archive/z3-4.8.17.tar.gz"
-  sha256 "1e57637ce8d5212fd38453df28e2730a18e0a633f723682267be87f5b858a126"
+  url "https://github.com/Z3Prover/z3/archive/z3-4.11.2.tar.gz"
+  sha256 "e3a82431b95412408a9c994466fad7252135c8ed3f719c986cd75c8c5f234c7e"
   license "MIT"
   head "https://github.com/Z3Prover/z3.git", branch: "develop"
 
@@ -13,12 +13,8 @@ class Z3 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b8749a0b889793fb7cfa72d1d380b74bdfeba5607e60d1d1deb0080dea7115bb"
-    sha256 cellar: :any,                 arm64_big_sur:  "2f59c37465aecdfbcc50a5ba0fe627535c0ff4cffb9080bfbddc0a2af0f97b53"
-    sha256 cellar: :any,                 monterey:       "d5dc7cb5a4a51bb544531f8ef9add76f22eda73d65c5603523c3573203b48bae"
-    sha256 cellar: :any,                 big_sur:        "b71b46a3a98b756ff456a591c2cd372013fd166f7e9ee8e25ceda8f990f9880d"
-    sha256 cellar: :any,                 catalina:       "2da92fd5596dd0a2c0a0b21f63bd474872933a5910a8d9f2e93619c687418a25"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f76abc05ef9587b811f14c70f430278e47405c7d68f610e65bd2157d11c99687"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/z3"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1731ad821dc866738ef2267deb129d6b286e143712a04cfbe4ee06eaf77dd8a6"
   end
 
   # Has Python bindings but are supplementary to the main library
@@ -32,7 +28,7 @@ class Z3 < Formula
   fails_with gcc: "5"
 
   def install
-    python3 = Formula["python@3.10"].opt_bin/"python3"
+    python3 = Formula["python@3.10"].opt_bin/"python3.10"
     system python3, "scripts/mk_make.py",
                      "--prefix=#{prefix}",
                      "--python",
