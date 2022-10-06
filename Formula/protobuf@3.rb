cfc89cd8b9ce1174b0581d7a1665d129464b43a2
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 09:45:27 +0800

    protobuf@3: add 3.20.1 bottle.

---
+++ Formula/protobuf@3.rb
@@ -0,0 +1,72 @@
+class ProtobufAT3 < Formula
+  desc "Protocol buffers (Google's data interchange format)"
+  homepage "https://github.com/protocolbuffers/protobuf/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.20.1/protobuf-all-3.20.1.tar.gz"
+  sha256 "3a400163728db996e8e8d21c7dfb3c239df54d0813270f086c4030addeae2fad"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/protobuf@3"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e91c16a74b6b5b44cb1370c8af2bb6b034764fa5100c97b4b11f3e1a003a7b67"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  uses_from_macos "zlib"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    # Don't build in debug mode. See:
+    # https://github.com/Homebrew/homebrew/issues/9279
+    # https://github.com/protocolbuffers/protobuf/blob/5c24564811c08772d090305be36fae82d8f12bbe/configure.ac#L61
+    ENV.prepend "CXXFLAGS", "-DNDEBUG"
+    ENV.cxx11
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *std_configure_args, "--with-zlib"
+    system "make"
+    system "make", "install"
+
+    # Install editor support and examples
+    pkgshare.install "editors/proto.vim", "examples"
+    elisp.install "editors/protobuf-mode.el"
+
+    ENV.append_to_cflags "-I#{include}"
+    ENV.append_to_cflags "-L#{lib}"
+
+    cd "python" do
+      pythons.each do |python|
+        system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
+      end
+    end
+  end
+
+  test do
+    testdata = <<~EOS
+      syntax = "proto3";
+      package test;
+      message TestCase {
+        string name = 4;
+      }
+      message Test {
+        repeated TestCase case = 1;
+      }
+    EOS
+    (testpath/"test.proto").write testdata
+    system bin/"protoc", "test.proto", "--cpp_out=."
+
+    pythons.each do |python|
+      with_env(PYTHONPATH: prefix/Language::Python.site_packages(python)) do
+        system python, "-c", "import google.protobuf"
+      end
+    end
+  end
+end
