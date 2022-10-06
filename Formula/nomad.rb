01f12f1bc17cfe909893dc7569886e086a321125
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-06 05:34:31 +0000

    nomad 1.4.0
    Closes #112465.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -1,8 +1,8 @@
 class Nomad < Formula
   desc "Distributed, Highly Available, Datacenter-Aware Scheduler"
   homepage "https://www.nomadproject.io"
-  url "https://github.com/hashicorp/nomad/archive/v1.3.5.tar.gz"
-  sha256 "79217b094831b0901dfe5ae9c03ff45ddde15ea02078a0fd07fabe2ff8867c83"
+  url "https://github.com/hashicorp/nomad/archive/v1.4.0.tar.gz"
+  sha256 "e79fe16c8fa94231bc9e1a1ae93926343e16f0892112fc84e95e014ac56dcb6b"
   license "MPL-2.0"
   head "https://github.com/hashicorp/nomad.git", branch: "main"
 
