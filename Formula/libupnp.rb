56dd8c925a46e58de8495c842ec098a5aa7921b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-06 20:04:52 +0000

    libupnp: update 1.14.14 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -11,12 +11,12 @@ class Libupnp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "07f8ecbee06854dc6072b10e80840e720523d82d43f5bec5bf1e4ec7da4522b8"
-    sha256 cellar: :any,                 arm64_big_sur:  "1b20e444ebcf0d98931a66965781221ca4350c14f938e36436ab9dbada82a473"
-    sha256 cellar: :any,                 monterey:       "7944d7c54c3fd7b339ced4aa400eeba7abc4f141115e0c7b9894d9b73f5667c4"
-    sha256 cellar: :any,                 big_sur:        "7d977eeb2e8a51f28cb9e026f704c7bb8259950126ad4bccee08b646a52e8ee3"
-    sha256 cellar: :any,                 catalina:       "b67e74278edf97fd18131b1291f8d1e39a76065fd23174db1df1d4591aee3452"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad9fa3404c5fefb005c2059664f877db34ef253cd049cb0513077e6fc59f4b0e"
+    sha256 cellar: :any,                 arm64_monterey: "b877e17c89ba5b82171fd94be62a3c80be42dadc479ea08c34cc3815506e84a9"
+    sha256 cellar: :any,                 arm64_big_sur:  "44a2da9c5a0fd8de2017ce9611aaf8a340392f7cef8b58325b9e54d8d1b75e73"
+    sha256 cellar: :any,                 monterey:       "0c126e6ad4315134e0ebc48921b42cd2cf755706b26d7b85a7850e8349c14982"
+    sha256 cellar: :any,                 big_sur:        "07fc86dd9f4c1c0c376602e7dc72e28a82b59b617a498d2adcee40cb97cc9814"
+    sha256 cellar: :any,                 catalina:       "748e5dce20bac22a30e258fb8a78981ca83d17930d5d0480185822c5e0dc3249"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06d6717c0e721d527eb437b87cd41580213a7343be274faa10072af14a3cd4d0"
   end
 
   def install
