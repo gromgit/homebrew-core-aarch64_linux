faf363dea4d6c9b9d9ec0efdcbccbd2f4482e0b8
Porkepix <porkepix@gmail.com>
2022-10-06 13:27:22 +0200

    zx 7.1.0
    Closes #112477.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zx.rb
+++ Formula/zx.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Zx < Formula
   desc "Tool for writing better scripts"
   homepage "https://github.com/google/zx"
-  url "https://registry.npmjs.org/zx/-/zx-7.0.8.tgz"
-  sha256 "d0334fc256613380ac84441c5d12922615868ab235ca5fa60d34db321eb49a2c"
+  url "https://registry.npmjs.org/zx/-/zx-7.1.0.tgz"
+  sha256 "8784c1248ccf4aa02126431c7831e4366c3f49ca370e112d044c6f91b02f8104"
   license "Apache-2.0"
 
   bottle do
