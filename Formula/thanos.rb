b7e52722e1eb0a26c6991c341ed0392affad996d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-07 06:01:11 +0000

    thanos 0.28.1
    Closes #112537.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/thanos.rb
+++ Formula/thanos.rb
@@ -1,8 +1,8 @@
 class Thanos < Formula
   desc "Highly available Prometheus setup with long term storage capabilities"
   homepage "https://thanos.io"
-  url "https://github.com/thanos-io/thanos/archive/v0.28.0.tar.gz"
-  sha256 "095466b601fbe5c0323beb4b8d93970d6bcb8b2f9607cd5da9514c2d4207b072"
+  url "https://github.com/thanos-io/thanos/archive/v0.28.1.tar.gz"
+  sha256 "e00ae14b88b9a3ae6b1ae022a0bd4fad34fd2da401bcef401cefa5e087675783"
   license "Apache-2.0"
 
   bottle do
