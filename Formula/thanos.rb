7e4c0c3bbf85c344bb4edff8ca0c2399d659a73f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-07 08:05:43 +0000

    thanos: update 0.28.1 bottle.

--- Formula/thanos.rb
+++ Formula/thanos.rb
@@ -6,12 +6,12 @@ class Thanos < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ba0227c5080516d799fdc9a760a4caf154f42c8f4a0e4c3303e70f1cb45a3eff"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "021b8c531e15b3ef3e765d8ac856637f83353eaf472b62b1aec3b75e8b841df7"
-    sha256 cellar: :any_skip_relocation, monterey:       "1d527d2229e736a328cc778a2bbfff3958c53a884f57f397704e156334f60e45"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f7eef2a300371308dfb3601e081c7a99829c68e54144f601ef8f2e18faedcea3"
-    sha256 cellar: :any_skip_relocation, catalina:       "d76354b98e19342abfa2fa069464a9d9efb27c50f00ece8dd6291a66f05ae369"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f8ed8202f63a2fc6caf8f97c13f19a6c628662f6b7d15562ffb29fb54217210f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3540abfe5d91f8eb51f2d3bc517a3d21bf0e9159b0da01ec09736e1e760d6fab"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd370aa4c1ca1dc800ec746390eb31d7040326a3adaa2ad576062e666bfdfaf3"
+    sha256 cellar: :any_skip_relocation, monterey:       "0923852314e8e6431bf4bd2a13b358f919dc3965c0576c383216fda1c6cd12af"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e3a964a3f7042258d2151583e00c98cea1204d02a734c26606ad6083a1fe2115"
+    sha256 cellar: :any_skip_relocation, catalina:       "c29937d2d9faef589ce2d66a6177a2d3c0053788bf43f6080416ca8a8e125351"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b13c86e100412b5233c0759cfca6f6df397d6d65d937032288d5e5ac60fc3326"
   end
 
   depends_on "go" => :build
