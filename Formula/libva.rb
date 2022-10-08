4b61528ff57a66eb6d355b2bf080d8185b699134
Porkepix <porkepix@gmail.com>
2022-10-08 12:07:55 +0200

    libva 2.16.0
    Closes #112626.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libva.rb
+++ Formula/libva.rb
@@ -1,8 +1,8 @@
 class Libva < Formula
   desc "Hardware accelerated video processing library"
   homepage "https://github.com/intel/libva"
-  url "https://github.com/intel/libva/releases/download/2.15.0/libva-2.15.0.tar.bz2"
-  sha256 "258ed409458d3e7a000e1908bc852619615ee6a933359c745fcd93eb3e461eca"
+  url "https://github.com/intel/libva/releases/download/2.16.0/libva-2.16.0.tar.bz2"
+  sha256 "115faca0db2c417a37da49830870e260450fa13066f5e24e252a20c36d920a1c"
   license "MIT"
 
   livecheck do
