862b5eb6ad46253a13f3fe55b59917238ba23ff1
Rui Chen <rui@chenrui.dev>
2022-10-08 14:12:34 +0000

    k3sup: update 0.12.8 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -13,12 +13,12 @@ class K3sup < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d374868b80a11acf2fe48243edd67801827ccf240c9c743daf6a34a138908651"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "728b4adad2511b3e0eabfac6944f66464a18bbdccb0a9da0300471bf59711c42"
-    sha256 cellar: :any_skip_relocation, monterey:       "1e092a07f0e19289393b52e3a4ef802558a0677837a6a66d6fabb6e5e0d3d333"
-    sha256 cellar: :any_skip_relocation, big_sur:        "603d9a57e86f3e94d5dc4cd93403fc87228a602b44f1b4baafa2d4f9ab2912de"
-    sha256 cellar: :any_skip_relocation, catalina:       "94dda040ca853c5240ba4f41f13ce85068e5ad12ffd4920fa96368146b35633d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "98d7e5c2803946c4177569dca126a9e51d885d85a3aea60545d2cb9d20d2f6c8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "baacc1dc3ed2a20359828e852f24441f330dcd851d778d4f111f412c4beaec3d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d35e8db5be9ac8b7fdc2b8990ca9e38284de3227f40cf96e1fc21883b67effe8"
+    sha256 cellar: :any_skip_relocation, monterey:       "1cdcf6b302b500ec1f0194ac5e1d169fc4dc087fd636e4a518065749670206ea"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e7ad9898126a08f967f05273b4b0b731963542c0912ab610f2e3fd8989d5bfeb"
+    sha256 cellar: :any_skip_relocation, catalina:       "c8fc506b9e8cba697408716bab8ac6db412557c8db527cf38ff67ede93cd9d3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c3b52391cdd056fdcd68645736b4229858ca4fa056fec10d17938106b871673f"
   end
 
   depends_on "go" => :build
