d3bcecfffd627b37996dfe7a9918f0f9117a081c
Porkepix <porkepix@gmail.com>
2022-10-08 05:44:50 +0200

    pspg 5.5.8
    Closes #112604.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/5.5.7.tar.gz"
-  sha256 "d9bb4e80bd6ddea2be0b835e6bd66bca7567a5f59641e31cfde4dc194fee7e9e"
+  url "https://github.com/okbob/pspg/archive/5.5.8.tar.gz"
+  sha256 "bc25e517784f08840796188d1fb8e908ed522d0809c0ca176eae07363bd5281b"
   license "BSD-2-Clause"
   head "https://github.com/okbob/pspg.git", branch: "master"
 
