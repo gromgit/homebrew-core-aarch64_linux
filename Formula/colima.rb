fed5545e4618f0ede7666596c51c9dab6d5cc324
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-08 16:04:41 +0000

    colima: update 0.4.6 bottle.

--- Formula/colima.rb
+++ Formula/colima.rb
@@ -13,12 +13,12 @@ class Colima < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b35150d30c06a4c5e20a4d657eb77732d170acd188af33ff3e18071ff5b1e1ce"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eda2f83a37b2cccc8cbeedf569f34be1bff2b824312d7ef246038b1c1ba81f88"
-    sha256 cellar: :any_skip_relocation, monterey:       "87f1691b194d350cb990cc9b736095c51bfc6af835829990cea7aaae3ac580f3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "50aed168a4535070271615b7515bfd57732c008aaf86a3413aaea3ef882403bd"
-    sha256 cellar: :any_skip_relocation, catalina:       "da6fbced11a4a9b0601bb900718bd9c340943abad2875cdb35b175803a8701c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d9b638131065532666f6f443164de6972dcbefb8b01c5687ee854a2c5b33a3e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "941942dc29f1d70369dee3423896e86d98dc93ea9878502c2e2b6ee09a7da1dd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "25d01043a2b4a23118ab1892287c9994d16ab4b79712db9abee924192af5e0af"
+    sha256 cellar: :any_skip_relocation, monterey:       "ca024de6aaa786a13bdaf9d86cb8b6e22639f0d50242b913901903d7314ef246"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e9ecc1c48d0982eefaee8548d6770677ae4cb6047cad76aa4f71ae766918b26a"
+    sha256 cellar: :any_skip_relocation, catalina:       "01150540e8654c348875f81d4555ad50b2597033d60660f6afa57418a1263e26"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2bd3972b05e57bf1be14ec1a08ac77f83f37eaa7106c8b06d91c7bd89cd5e895"
   end
 
   depends_on "go" => :build
