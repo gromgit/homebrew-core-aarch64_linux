9da619bb76a7e5dab7358ba99bcdf05197c25830
Porkepix <porkepix@gmail.com>
2022-10-08 12:56:37 +0200

    portmidi 2.0.4
    * portmidi 2.0.4
    * portmidi: update build
    
    Closes #112630.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/portmidi.rb
+++ Formula/portmidi.rb
@@ -1,10 +1,9 @@
 class Portmidi < Formula
   desc "Cross-platform library for real-time MIDI I/O"
   homepage "https://github.com/PortMidi/portmidi"
-  url "https://github.com/PortMidi/portmidi/archive/refs/tags/v2.0.3.tar.gz"
-  sha256 "934f80e1b09762664d995e7ab5a9932033bc70639e8ceabead817183a54c60d0"
+  url "https://github.com/PortMidi/portmidi/archive/refs/tags/v2.0.4.tar.gz"
+  sha256 "64893e823ae146cabd3ad7f9a9a9c5332746abe7847c557b99b2577afa8a607c"
   license "MIT"
-  revision 1
   version_scheme 1
 
   bottle do
@@ -31,9 +30,9 @@ class Portmidi < Formula
       ENV["SDKROOT"] = MacOS.sdk_path
     end
 
-    system "cmake", ".", *std_cmake_args
-    system "make"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
