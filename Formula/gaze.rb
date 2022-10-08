1818748fb488e57a1c6bded812c6537066ef2990
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-08 15:04:34 +0000

    gaze: update 1.1.3 bottle.

--- Formula/gaze.rb
+++ Formula/gaze.rb
@@ -6,12 +6,12 @@ class Gaze < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a7d7858554a31fb35c18208505406ef87bbd5ae9d0c611dbb7bd2b922434a2e6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5b44413b9439372a8df04dfc191969dbf6ffedce7a25f7b4ad2058884f8f4cc8"
-    sha256 cellar: :any_skip_relocation, monterey:       "28f18859a123f1b06349300bc40b6d6249475ea3e0089ef8b57af0b25e14ab75"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5dc7683e7f6eb8e6618c44fd7c10f3cb9cbf032e12019f46002f905d03adef3e"
-    sha256 cellar: :any_skip_relocation, catalina:       "b383861277696943cc637867be5ef4b3fa0191ddd0306f090b52c1184dfbaeaf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a157686c0439af0073554c8cbc5a7b6c93cb8a8476175e74d0dab39d5f5a7600"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e9661658a4002ec61e5c8e66bbd12e87d56fd2169b2acbae2ff1730fb28763c1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "21415b048a0d0703840e3383e4581abaead69378b946f95e82d1dbfe30c1072c"
+    sha256 cellar: :any_skip_relocation, monterey:       "c69c1dd7a2a2d77425fc040cf04d542d573bfc0d3217209f4524407aa43012f3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b749b2b37c58450dc62e0490423f7690bcd656d09ca3bd53448f4f673d461ceb"
+    sha256 cellar: :any_skip_relocation, catalina:       "1f5cb6ff364f5bfc352f6da7d894e1048db1cea534f1a2f27ae1326e37d7be24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "869b4f50685c5ddec3a94fba1929ac8d96811f93c10754804528a5c6619fc2b0"
   end
 
   depends_on "go" => :build
