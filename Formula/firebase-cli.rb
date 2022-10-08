1c8f56ee601d7379d721931b58fce1bd2c130795
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-08 05:23:24 +0000

    firebase-cli 11.14.1
    Closes #112614.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-11.14.0.tgz"
-  sha256 "f5d19f543b654eccf704d1e3d3917930b2f9143bf2a16bc2f9bf6bf94d8d4947"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-11.14.1.tgz"
+  sha256 "bfde4a2e041e412671f4664ea413110950b92b8ae5674269adfb3f142027beda"
   license "MIT"
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
