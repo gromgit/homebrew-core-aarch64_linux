97c8a72e73fcb6dcaf47718f82b5e95deff0b299
Michael Cho <cho-m@tuta.io>
2022-10-07 18:44:39 -0700

    gptfdisk: fix crash with popt 1.19
    Closes #110878.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gptfdisk.rb
+++ Formula/gptfdisk.rb
@@ -4,6 +4,7 @@ class Gptfdisk < Formula
   url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.9/gptfdisk-1.0.9.tar.gz"
   sha256 "dafead2693faeb8e8b97832b23407f6ed5b3219bc1784f482dd855774e2d50c2"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "a324c027ce6d6f41d464ba001961e0481180ce5d43eddfebcec2c108358479cd"
@@ -21,6 +22,10 @@ class Gptfdisk < Formula
   on_linux do
     depends_on "util-linux"
   end
+
+  # Backport upstream commit to fix crash with popt 1.19. Remove in the next release.
+  # Ref: https://sourceforge.net/p/gptfdisk/code/ci/5d5e76d369a412bfb3d2cebb5fc0a7509cef878d/
+  patch :DATA
 
   def install
     if OS.mac?
@@ -52,3 +57,16 @@ class Gptfdisk < Formula
     assert_match "Found valid GPT with protective MBR", shell_output("#{bin}/gdisk -l test.dmg")
   end
 end
+
+__END__
+--- a/gptcl.cc
++++ b/gptcl.cc
+@@ -155,7 +155,7 @@
+    } // while
+
+    // Assume first non-option argument is the device filename....
+-   device = (char*) poptGetArg(poptCon);
++   device = strdup((char*) poptGetArg(poptCon));
+    poptResetContext(poptCon);
+
+    if (device != NULL) {
