13baa426fbf3761c0730308e09804f54eb12e75a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-08 23:04:15 +0000

    gptfdisk: update 1.0.9_1 bottle.

--- Formula/gptfdisk.rb
+++ Formula/gptfdisk.rb
@@ -7,12 +7,12 @@ class Gptfdisk < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a324c027ce6d6f41d464ba001961e0481180ce5d43eddfebcec2c108358479cd"
-    sha256 cellar: :any,                 arm64_big_sur:  "6ba98737562bd6ca00091e4259fa8195592e0d6f25b9650f8451f3cae553d6f9"
-    sha256 cellar: :any,                 monterey:       "1d114b23a2a5cd0bbeb5b52bf9eb049a38f1ddd41b53abdfaf245b2ff333ae51"
-    sha256 cellar: :any,                 big_sur:        "0e9d348490a0610c6342d624dafc745a838e33d3cf64287985470c79ac95e5a4"
-    sha256 cellar: :any,                 catalina:       "ac261cf0e7e1b9848f4c2d9ce7cb85854eb10392d332018333330df0ebd6ba49"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d42a4448e83c5f84f9f947f9aef03bd1a67dffd95cea7edeb28bc1eae07c7be0"
+    sha256 cellar: :any,                 arm64_monterey: "c441169bd2c41b16caa519843b2b8dc4a827842483d335b5c02b1b5bffd92766"
+    sha256 cellar: :any,                 arm64_big_sur:  "1e5f4146ffa1a68c58f24792e12c54abd60f5fefef34cb2554b3e36837ea6c0a"
+    sha256 cellar: :any,                 monterey:       "97dbd19ad14e804d46a12b60d9f75490f7074403609cd9645e69ec1e970c5cc3"
+    sha256 cellar: :any,                 big_sur:        "9f36a4ef4536d2692770ef30a4e2eb13e1ab29142d3431fafda1167cbc4ad1a6"
+    sha256 cellar: :any,                 catalina:       "232fa946d22e2929d7021ba28e088856cb8542126bcf939fbedc2a99d8f1bf3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d662db01992e4c3c8a077c1dd0883c741db35ec8c4cdb8bb1a3cfb2fccc95259"
   end
 
   depends_on "popt"
