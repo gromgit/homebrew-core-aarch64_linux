bfb48442c4896009133250d22e6fd715319965d9
Rui Chen <rui@chenrui.dev>
2022-10-08 15:51:28 +0000

    testkube: update 1.6.5 bottle.

--- Formula/testkube.rb
+++ Formula/testkube.rb
@@ -13,12 +13,12 @@ class Testkube < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d6e0900244520bff4a45ea0973d43c88b98f3b2f22b50e73bbcae8a6f7cce8d8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d6e0900244520bff4a45ea0973d43c88b98f3b2f22b50e73bbcae8a6f7cce8d8"
-    sha256 cellar: :any_skip_relocation, monterey:       "126982d51e67660005934eef57193a6fdfc3f69c3055225b9042d69cecdd8a27"
-    sha256 cellar: :any_skip_relocation, big_sur:        "126982d51e67660005934eef57193a6fdfc3f69c3055225b9042d69cecdd8a27"
-    sha256 cellar: :any_skip_relocation, catalina:       "126982d51e67660005934eef57193a6fdfc3f69c3055225b9042d69cecdd8a27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51f0289cb889ed63880d21be54fe63bf07ce38b5eb6892091444606967bfbbb4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9a36dc9860ad91dc64cec20b968c3b07fcb324f2f6ee42351a65c6b2156f8c10"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9a36dc9860ad91dc64cec20b968c3b07fcb324f2f6ee42351a65c6b2156f8c10"
+    sha256 cellar: :any_skip_relocation, monterey:       "fff77fa6473b8125bcb81169830f618b401219eb5af716be19b0641218333ac1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fff77fa6473b8125bcb81169830f618b401219eb5af716be19b0641218333ac1"
+    sha256 cellar: :any_skip_relocation, catalina:       "fff77fa6473b8125bcb81169830f618b401219eb5af716be19b0641218333ac1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8cf7b6950462af1f77ed4d1a9d5f3224a3d135babb0f3f93a0cc940e64369b40"
   end
 
   depends_on "go" => :build
