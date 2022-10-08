6428ec2091ec92dea5ab4a214943429003c73cce
Rui Chen <rui@chenrui.dev>
2022-10-08 15:51:15 +0000

    jaq: update 0.8.2 bottle.

--- Formula/jaq.rb
+++ Formula/jaq.rb
@@ -7,12 +7,12 @@ class Jaq < Formula
   head "https://github.com/01mf02/jaq.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1592152e79a2ee1c77315ef5dc14cfeb279fcabe673d1f3eb71273de454d0668"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c5b4985869d8b7120c971044b65d9be7efdb3e5dc95a47550d301ceb255114d3"
-    sha256 cellar: :any_skip_relocation, monterey:       "0c1f5c32acfab2b9d866393ed79007a9e4bef99d13d93e6acbda1e42a35718a5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d0ed3a5011ead1161f4f3222ee644a8a7b824bc203b2e26e055677d5927c2131"
-    sha256 cellar: :any_skip_relocation, catalina:       "41c68f33b71605b1980d0cfea9388113fcb29b2c00da23f2ec57db9ce1d63358"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1dd049dab4ee712390d44775eb3341df224947cf57452c165520d8ddf74db12"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b463401496186b08b3f8125a679b4f432a5e040e0fc67a6bd9dcf0e00f732168"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0cac7264e2f8d58924649092b078c6c43aaba936f468af96c0ebeee07400ab9f"
+    sha256 cellar: :any_skip_relocation, monterey:       "1345964158b91507735f9dccdf1ac6420da2991613654b0f4ef84d0129848096"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7c9a0461c41ba631853fe6e2100b558cbca6d4809e627186bb4c3f3790a0691f"
+    sha256 cellar: :any_skip_relocation, catalina:       "056033a04399508bbc951977971f71afedec48d2e5fac6bc3dabc30f8287007c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "efd7f65d3d5bd40f6bafc443d502a7469d796ec746ff3726b2a6395ea11bae9d"
   end
 
   depends_on "rust" => :build
