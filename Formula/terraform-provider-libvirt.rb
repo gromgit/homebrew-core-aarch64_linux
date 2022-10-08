ba769fa01ac5a7cbab8f58b16a7f45bbcf6a2aee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-08 16:04:58 +0000

    terraform-provider-libvirt: update 0.7.0 bottle.

--- Formula/terraform-provider-libvirt.rb
+++ Formula/terraform-provider-libvirt.rb
@@ -6,12 +6,12 @@ class TerraformProviderLibvirt < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "55998af49e41b4c18ffdec811449ca8a3343c6febdc8973dcfbde62c62d1d15f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ec68a2023515fe2c39d67035067b19513c1e35a3f0497444b14166b049cecdc"
-    sha256 cellar: :any_skip_relocation, monterey:       "b66d5d7be58a4c63f813d930fa5ba4636b1b0ba607045b56051fcf661cfc44a7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6c7abd0ae49e730f3920bdc3e21760cdb01d3f2a9f8adef1c40dce133950816c"
-    sha256 cellar: :any_skip_relocation, catalina:       "5d627d26c7d94d2593ae1995b3e9f6826283f04160b9ef991e975d3f6595df0d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d825742da0cbe9024942e1ff42e7e2758428ae0c7e78cf6816af563700f2038"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb615807297c7c263475c9aefc77b7a71b4191bb95578ff1255bdf5a10d957f8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c3790a8e0e8aad43a45487fdedc2cd5d47b882be05f8ab77e14cb425072322de"
+    sha256 cellar: :any_skip_relocation, monterey:       "d36befb071e615c232e155c48a6a3270ae2577b6dcbad08e247a18251aa3b7cd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6d8799365da3bba56f1161bd425d8b24f83879cb50ba8b4501e6e46282094b63"
+    sha256 cellar: :any_skip_relocation, catalina:       "e31788236c8bca8c1947965082c91ab129ed3384061a4d7ee70e4a12f1c9f6aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5e7da33b7fd0a2f4b39e47db6da54213b9de1dedbd263adc3a30ddb5adb2634c"
   end
 
   depends_on "go" => :build
