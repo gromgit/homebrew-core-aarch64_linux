441aabc54c61d081e92169e34c253dd50eac1d59
Rui Chen <rui@chenrui.dev>
2022-10-08 20:36:52 -0400

    aws-sdk-cpp 1.9.360
    Closes #112690.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -3,8 +3,8 @@ class AwsSdkCpp < Formula
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
   url "https://github.com/aws/aws-sdk-cpp.git",
-      tag:      "1.9.350",
-      revision: "a7de1c292661680e73f21b60fbb6597abec64de2"
+      tag:      "1.9.360",
+      revision: "a6da713fe5992964fe703ba183778926ad809b77"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
