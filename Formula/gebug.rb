47ecf75dcb6c8e1e8f9dbfbdbb638513a675f066
Rui Chen <rui@chenrui.dev>
2022-10-10 16:24:25 +0000

    gebug: add 1.0.3 bottle.

--- Formula/gebug.rb
+++ Formula/gebug.rb
@@ -6,6 +6,15 @@ class Gebug < Formula
   license "Apache-2.0"
   head "https://github.com/moshebe/gebug.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4f5b02c9bd0e0b24f17b152bc4a714346844f22f17759287008576ad240c7b27"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f526c66051c98b69817247e6ec0ac180f11bd3658b77595d2cc8ba7d3f678826"
+    sha256 cellar: :any_skip_relocation, monterey:       "574f20b58ccab0297e79eb61752f32255a6131ab282c3d17dea3573ebc0e2aff"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e03bddf6ae9d8bd4402d46e5b8890f28dec307907c422e122a98553e45bd3c5e"
+    sha256 cellar: :any_skip_relocation, catalina:       "9c92e41d093d543a447df5dbbe3a1de30894002da48323c4277dbf94bfefd114"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba3d9508a2bc58a6afcf69ec0679fd4635d438b8b8e6324ccc1500b9aeabe0d8"
+  end
+
   depends_on "go" => :build
   depends_on "docker" => :test
 
