81e0db2dbc45005e79c1722ad4ab4f2e8c5a6713
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-10 11:04:58 +0000

    genact: update 1.2.2 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -6,12 +6,12 @@ class Genact < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5736d97990464286a4766fbbaa718f6516c5f879963ca23ecf50c5ba3fd220b7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a827ee2657e36a63957913609cfdef1b3b3590030e0dda97eebd9a72bc3c957"
-    sha256 cellar: :any_skip_relocation, monterey:       "7fbce14b618c1bee6f1dab5a5b60f7c203ddd5aa447e05c87bee755132e7f4f5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9c4791cc5620332568d337607dccc733484ff67536524d611a40c03c67524589"
-    sha256 cellar: :any_skip_relocation, catalina:       "b002d8560145fd9f85c5d23ad04f6184f02cb5377ae45683e0b8cbb5d10f6bc6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6901c48390142402f0746ee65bb8d9ac10a6ab85525495a770799fc88d5bd473"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "86f8f5a41ff5310a19af6be233783006e16630d479ce4cc1d33a25b4673c2de0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a9a2cdeb2a5ba1bdbc1b9ab61255952fa9c692d747b35de07e5b5dc228d45594"
+    sha256 cellar: :any_skip_relocation, monterey:       "09b5d1c206164a5ee28d650f9cb01b5ff1698ae737680bf9fd817868916374a8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3e2604dc557d38f3ef515b270de3fb6ea4dcacb6c968d9e99953e97848cb3fb5"
+    sha256 cellar: :any_skip_relocation, catalina:       "301d8e05b78b786cd7483aca4beedd0c776f712483683b4f5ca11d56dd18ac3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f9c80b1ab443eacb3b3160f23b6061fa9371f21669a40c8264cac7dac9f69cf"
   end
 
   depends_on "rust" => :build
