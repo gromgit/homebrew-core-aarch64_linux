8e9bea579c436917fac3145a41bc72657b1ce0c6
James Lamb <jaylamb20@gmail.com>
2022-10-10 13:53:04 -0500

    lightgbm 3.3.3
    Closes #112795.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lightgbm.rb
+++ Formula/lightgbm.rb
@@ -2,8 +2,8 @@ class Lightgbm < Formula
   desc "Fast, distributed, high performance gradient boosting framework"
   homepage "https://github.com/microsoft/LightGBM"
   url "https://github.com/microsoft/LightGBM.git",
-      tag:      "v3.3.2",
-      revision: "dce7e58b020bc14b69eefc31546c366971ecb2d9"
+      tag:      "v3.3.3",
+      revision: "a0a617dfff2e2e1637b72b7a2adbe5453e7f97e0"
   license "MIT"
 
   bottle do
