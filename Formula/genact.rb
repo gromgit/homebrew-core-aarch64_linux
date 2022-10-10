8595786eef6a97abcd60dc256c85773626f5a6dc
Porkepix <porkepix@gmail.com>
2022-10-10 10:06:03 +0200

    genact 1.2.2
    Closes #112753.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -1,8 +1,8 @@
 class Genact < Formula
   desc "Nonsense activity generator"
   homepage "https://github.com/svenstaro/genact"
-  url "https://github.com/svenstaro/genact/archive/v1.2.0.tar.gz"
-  sha256 "bcbcceaadab49f0dc86a5f3fa6235eea4f08d835f717b098c8e3a38c897c3221"
+  url "https://github.com/svenstaro/genact/archive/v1.2.2.tar.gz"
+  sha256 "72ead4b84e4ca733ae8a25614d44df3f3db5e47e54913ed9fbfecd2f5212a632"
   license "MIT"
 
   bottle do
