757c9f36f283a017cda096b470a637f791b345d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-10 13:22:28 +0000

    git-machete: update 3.12.3 bottle.

--- Formula/git-machete.rb
+++ Formula/git-machete.rb
@@ -8,12 +8,12 @@ class GitMachete < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cf2209738a9be96fd9b04ca326438896b587ad4a8a8326ddea229eb124ba6fe4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cf2209738a9be96fd9b04ca326438896b587ad4a8a8326ddea229eb124ba6fe4"
-    sha256 cellar: :any_skip_relocation, monterey:       "09a5a0e6af1d764f0e4ea109930c448cf4aafc88d66d29c17634e7fd6c543070"
-    sha256 cellar: :any_skip_relocation, big_sur:        "09a5a0e6af1d764f0e4ea109930c448cf4aafc88d66d29c17634e7fd6c543070"
-    sha256 cellar: :any_skip_relocation, catalina:       "09a5a0e6af1d764f0e4ea109930c448cf4aafc88d66d29c17634e7fd6c543070"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "73603bf9529090265a0630b7416cda51885c2845a6f3e6b702b9bbf06858c4b0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "08e6b4fc370891edb4628da00f04f276a732a14fce6eaba0af40ced6496da786"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "08e6b4fc370891edb4628da00f04f276a732a14fce6eaba0af40ced6496da786"
+    sha256 cellar: :any_skip_relocation, monterey:       "a55a144c99cf36cc41b08e352ea0359514cf84733e44940b8ac49cc45349c45c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a55a144c99cf36cc41b08e352ea0359514cf84733e44940b8ac49cc45349c45c"
+    sha256 cellar: :any_skip_relocation, catalina:       "a55a144c99cf36cc41b08e352ea0359514cf84733e44940b8ac49cc45349c45c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ebe42b31c0921c487926b29315d125d3acfb103950532a8c22757aeca10f4e57"
   end
 
   depends_on "python@3.10"
