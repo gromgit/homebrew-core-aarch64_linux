8938289a3794246936dbbca836080c4f65d3b0f7
Branch Vincent <branchevincent@gmail.com>
2022-10-09 21:55:16 -0400

    gettext 0.21.1
    Closes #112730.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gettext.rb
+++ Formula/gettext.rb
@@ -1,10 +1,10 @@
 class Gettext < Formula
   desc "GNU internationalization (i18n) and localization (l10n) library"
   homepage "https://www.gnu.org/software/gettext/"
-  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
-  sha256 "c77d0da3102aec9c07f43671e60611ebff89a996ef159497ce8e59d075786b12"
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.1.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.1.tar.gz"
+  sha256 "e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -21,19 +21,9 @@ class Gettext < Formula
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    directory "gettext-runtime/libasprintf"
-  end
-
   def install
     args = [
-      "--disable-dependency-tracking",
       "--disable-silent-rules",
-      "--disable-debug",
-      "--prefix=#{prefix}",
       "--with-included-glib",
       "--with-included-libcroco",
       "--with-included-libunistring",
@@ -57,7 +47,7 @@ class Gettext < Formula
     else
       "--with-libxml2-prefix=#{Formula["libxml2"].opt_prefix}"
     end
-    system "./configure", *args
+    system "./configure", *std_configure_args, *args
     system "make"
     ENV.deparallelize # install doesn't support multiple make jobs
     system "make", "install"
