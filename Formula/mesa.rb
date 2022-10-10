afde73e43162a080228ff10158efb7f7bd654e77
Adrian Ho <the.gromgit@gmail.com>
2022-10-10 15:25:44 +0800

    mesa: add 22.1.7_1 bottle.

--- Formula/mesa.rb
+++ Formula/mesa.rb
@@ -3,24 +3,32 @@ class Mesa < Formula
 
   desc "Graphics Library"
   homepage "https://www.mesa3d.org/"
-  url "https://mesa.freedesktop.org/archive/mesa-22.0.3.tar.xz"
-  sha256 "9f2b30f5276a9abaf71aafc6979685e2636189de1a87aea2c9e69744a6d0ebb9"
   license "MIT"
+  revision 1
   head "https://gitlab.freedesktop.org/mesa/mesa.git", branch: "main"
 
-  bottle do
-    sha256 arm64_monterey: "b105483cdde050584207848c534f3ca3e988f5f38ba14c6288d915f88855fc44"
-    sha256 arm64_big_sur:  "e774f7177666f36f4702006bc2e5355a21fcc703231e643e86eaa64cfca0b225"
-    sha256 monterey:       "b3fc91b898ec4483d8edb260a568c3887ff207e0f75d1e0746ac3f7c1238226b"
-    sha256 big_sur:        "9c303730e2726fdc82e318d707f1d793a12dd161618c974f57f21fb4898fa32b"
-    sha256 catalina:       "95eb7bbe986579799e9b74f90f149aec1e773d7a58cc554ef1e27b450632b39c"
-    sha256 x86_64_linux:   "ed884f5b81ed8d10845138ff0d0c42c70767a7371d3edcee2679b83d6dce0ac4"
+  stable do
+    url "https://mesa.freedesktop.org/archive/mesa-22.1.7.tar.xz"
+    sha256 "da838eb2cf11d0e08d0e9944f6bd4d96987fdc59ea2856f8c70a31a82b355d89"
+
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/f0a40cf7d70ee5a25639b91d9a8088749a2dd04e/mesa/fix-build-on-macOS.patch"
+      sha256 "a9b646e48d4e4228c3e06d8ca28f65e01e59afede91f58d4bd5a9c42a66b338d"
+    end
   end
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/mesa"
+    sha256 aarch64_linux: "fe9e2dadcd9368ee67f2f62d70873690248c734dba744870cc060e6b56f37433"
+  end
+
+  depends_on "bison" => :build # can't use form macOS, needs '> 2.3'
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => :build
+  depends_on "xorgproto" => :build
+
   depends_on "expat"
   depends_on "gettext"
   depends_on "libx11"
@@ -28,7 +36,6 @@ class Mesa < Formula
   depends_on "libxdamage"
   depends_on "libxext"
 
-  uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
   uses_from_macos "llvm"
   uses_from_macos "ncurses"
@@ -55,13 +62,13 @@ class Mesa < Formula
   fails_with gcc: "5"
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/50/ec/1d687348f0954bda388bfd1330c158ba8d7dea4044fc160e74e080babdb9/Mako-1.2.0.tar.gz"
-    sha256 "9a7c7e922b87db3686210cf49d5d767033a41d4010b284e747682c92bddd8b39"
+    url "https://files.pythonhosted.org/packages/ad/dd/34201dae727bb183ca14fd8417e61f936fa068d6f503991f09ee3cac6697/Mako-1.2.1.tar.gz"
+    sha256 "f054a5ff4743492f1aa9ecc47172cb33b42b9d993cffcc146c9de17e717b0307"
   end
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
-    sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
+    url "https://files.pythonhosted.org/packages/e0/ef/5905cd3642f2337d44143529c941cc3a02e5af16f0f65f81cbef7af452bb/Pygments-2.13.0.tar.gz"
+    sha256 "56a8508ae95f98e2b9bdf93a6be5ae3f7d8af858b43e02c5a2ff083726be40c1"
   end
 
   resource "MarkupSafe" do
@@ -75,8 +82,8 @@ class Mesa < Formula
   end
 
   resource "gl_wrap.h" do
-    url "https://gitlab.freedesktop.org/mesa/demos/-/raw/5435fc7fbd332e171da9a71e33a9b190e9462cf0/src/util/gl_wrap.h"
-    sha256 "c727b2341d81c2a1b8a0b31e46d24f9702a1ec55c8be3f455ddc8d72120ada72"
+    url "https://gitlab.freedesktop.org/mesa/demos/-/raw/ddc35ca0ea2f18c5011c5573b4b624c128ca7616/src/util/gl_wrap.h"
+    sha256 "41f5a84f8f5abe8ea2a21caebf5ff31094a46953a83a738a19e21c010c433c88"
   end
 
   def install
@@ -91,28 +98,31 @@ class Mesa < Formula
 
     ENV.prepend_path "PATH", "#{venv_root}/bin"
 
-    mkdir "build" do
-      args = ["-Db_ndebug=true"]
+    args = ["-Db_ndebug=true"]
 
-      if OS.linux?
-        args << "-Dplatforms=x11,wayland"
-        args << "-Dglx=auto"
-        args << "-Ddri3=true"
-        args << "-Dgallium-drivers=auto"
-        args << "-Dgallium-omx=disabled"
-        args << "-Degl=true"
-        args << "-Dgbm=true"
-        args << "-Dopengl=true"
-        args << "-Dgles1=enabled"
-        args << "-Dgles2=enabled"
-        args << "-Dgallium-xvmc=disabled"
-        args << "-Dvalgrind=false"
-        args << "-Dtools=drm-shim,etnaviv,freedreno,glsl,nir,nouveau,xvmc,lima"
-      end
+    if OS.linux?
+      args += %w[
+        -Dplatforms=x11,wayland
+        -Dglx=auto
+        -Ddri3=true
+        -Dgallium-drivers=auto
+        -Dgallium-omx=disabled
+        -Degl=true
+        -Dgbm=true
+        -Dopengl=true
+        -Dgles1=enabled
+        -Dgles2=enabled
+        -Dgallium-xvmc=disabled
+        -Dvalgrind=false
+        -Dtools=drm-shim,etnaviv,freedreno,glsl,nir,nouveau,xvmc,lima
+      ]
+    end
 
-      system "meson", *std_meson_args, "..", *args
-      system "ninja"
-      system "ninja", "install"
+    system "meson", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+    inreplace lib/"pkgconfig/dri.pc" do |s|
+      s.change_make_var! "dridriverdir", HOMEBREW_PREFIX/"lib/dri"
     end
 
     if OS.linux?
