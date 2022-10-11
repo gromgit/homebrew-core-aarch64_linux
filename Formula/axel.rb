9df5c5a736eb5cfc36063b9a72c8e6630b1569aa
Michael Cho <cho-m@tuta.io>
2022-10-10 23:25:28 -0700

    axel: use `openssl@3`, fix HEAD build, and update license
    Closes #112839.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/axel.rb
+++ Formula/axel.rb
@@ -3,8 +3,7 @@ class Axel < Formula
   homepage "https://github.com/axel-download-accelerator/axel"
   url "https://github.com/axel-download-accelerator/axel/releases/download/v2.17.11/axel-2.17.11.tar.xz"
   sha256 "580b2c18692482fd7f1e2b2819159484311ffc50f6d18924dceb80fd41d4ccf9"
-  license "GPL-2.0-or-later"
-  head "https://github.com/axel-download-accelerator/axel.git", branch: "master"
+  license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   bottle do
     sha256 cellar: :any, arm64_monterey: "483732a5fda41e8592d78ce2d4df86362e60f61bbeb41081beaf382f211edea3"
@@ -15,16 +14,32 @@ class Axel < Formula
     sha256               x86_64_linux:   "145f3f4fec5782251254a8dd1adbd989118d1be0fe3d4808dce962a586f68119"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "autoconf-archive" => :build
-  depends_on "automake" => :build
+  head do
+    url "https://github.com/axel-download-accelerator/axel.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "autoconf-archive" => :build
+    depends_on "automake" => :build
+    depends_on "gawk" => :build
+
+    resource "txt2man" do
+      url "https://github.com/mvertes/txt2man/archive/refs/tags/txt2man-1.7.1.tar.gz"
+      sha256 "4d9b1bfa2b7a5265b4e5cb3aebc1078323b029aa961b6836d8f96aba6a9e434d"
+    end
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
+    if build.head?
+      resource("txt2man").stage { (buildpath/"txt2man").install "txt2man" }
+      ENV.prepend_path "PATH", buildpath/"txt2man"
+      system "autoreconf", "--force", "--install", "--verbose"
+    end
+    system "./configure", *std_configure_args,
+                          "--disable-silent-rules",
                           "--sysconfdir=#{etc}"
     system "make", "install"
   end
