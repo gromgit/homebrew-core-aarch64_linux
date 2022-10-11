370a33c938f1811cbd4b6c61eca18635d9b1c2b7
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 01:12:29 +0800

    dcmtk: add 3.6.6 bottle.

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -12,7 +12,7 @@ class Dcmtk < Formula
     # TODO: Remove in the next release along with stable block
     patch do
       url "https://git.dcmtk.org/?p=dcmtk.git;a=patch;h=5fba853b6f7c13b02bed28bd9f7d3f450e4c72bb"
-      sha256 "40ca9e6f377951e2d24a509a6c95b9e572224d74d694f0d648b8b33e4d67e285"
+      sha256 "189966c15406898d4a38f49d76806356378ee51557dff114420d7ae897ad17d6"
     end
   end
 
@@ -22,13 +22,8 @@ class Dcmtk < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_monterey: "2b707106032247e1d1832e918ec0e560b2e81af4ed0a1ee659f0803c4c172141"
-    sha256 arm64_big_sur:  "b101e7090c39b48225ab0d1d895236c849b613cf1f4e9b187c7818c6e9e7b78a"
-    sha256 monterey:       "abf4a42c7a34004707c70fb518e1a9fc11582e360a886a8c109b5ce6fcd6492a"
-    sha256 big_sur:        "847e85cc8b29b4caaf58cf0aa001b06082f2ea83bceaf6a0b5c90abc27b1f3bb"
-    sha256 catalina:       "f7872a94bd7c98fa785a61d7d9c9dd00e577002b9d563fc403f4913011457de1"
-    sha256 x86_64_linux:   "bea864995df958ccf4fb449d1347b686ed28171e9417845b7a768eee2b7eab6f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dcmtk"
+    sha256 aarch64_linux: "800d5c6c836dd45cf4a4a84b62e6e8c7f39259da6902f047af97c4173393680a"
   end
 
   depends_on "cmake" => :build
