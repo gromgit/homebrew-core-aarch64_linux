98ae3b33e400a4ab6896240d240411e393d1638b
David Baumgold <david@davidbaumgold.com>
2022-10-11 11:47:11 +0200

    tmuxp: remove PyYAML resource
    Closes #112864.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tmuxp.rb
+++ Formula/tmuxp.rb
@@ -30,11 +30,6 @@ class Tmuxp < Formula
     sha256 "cf352a645ebeaa5046ad0593cc01d691d77f8b3af1660327a6433cc6276ab796"
   end
 
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
-  end
-
   def install
     virtualenv_install_with_resources
   end
