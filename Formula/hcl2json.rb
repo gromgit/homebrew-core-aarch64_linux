477a83674840ce551c81c11f9636454044800004
Porkepix <porkepix@gmail.com>
2022-10-11 13:45:02 +0200

    hcl2json 0.3.5
    Closes #112871.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hcl2json.rb
+++ Formula/hcl2json.rb
@@ -1,8 +1,8 @@
 class Hcl2json < Formula
   desc "Convert HCL2 to JSON"
   homepage "https://github.com/tmccombs/hcl2json"
-  url "https://github.com/tmccombs/hcl2json/archive/v0.3.4.tar.gz"
-  sha256 "41c63b892e9a1488c5380faee83d341482352199175588fb46fa838f3b75e6a3"
+  url "https://github.com/tmccombs/hcl2json/archive/0.3.5.tar.gz"
+  sha256 "9d91d8f8b9bf204f3827795592a7da89e0dea368a17efe0163f5ef14bcf3bf9c"
   license "Apache-2.0"
   head "https://github.com/tmccombs/hcl2json.git", branch: "main"
 
