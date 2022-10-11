f93d1df532b8f5d24c71d86a810905f061fdf108
Ivan Kozlovic <ivan@synadia.com>
2022-10-11 14:22:15 -0600

    nats-streaming-server 0.25.2
    Closes #112902.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -1,8 +1,8 @@
 class NatsStreamingServer < Formula
   desc "Lightweight cloud messaging system"
   homepage "https://nats.io"
-  url "https://github.com/nats-io/nats-streaming-server/archive/v0.24.6.tar.gz"
-  sha256 "750e091f3473688a1b0c251513bf9beb7a12e3e1359d541b53381bd91e09b9be"
+  url "https://github.com/nats-io/nats-streaming-server/archive/v0.25.2.tar.gz"
+  sha256 "f0a8baa9770d305c11b425a57383c8c0fcfa59836e30010aec19c14e54436b54"
   license "Apache-2.0"
   head "https://github.com/nats-io/nats-streaming-server.git", branch: "main"
 
