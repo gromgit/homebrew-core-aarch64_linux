0a0e56525a506618ae08b93c088d16dd2729cfba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-11 10:05:39 +0000

    jackett: update 0.20.2097 bottle.

--- Formula/jackett.rb
+++ Formula/jackett.rb
@@ -7,12 +7,12 @@ class Jackett < Formula
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "70f9be4ab24c7d25b80c9f3e02710d942075b16185acc27d50e3aff04eca0a26"
-    sha256 cellar: :any,                 arm64_big_sur:  "c9d100a263026513173e034f8e36ba45a10e8df81ab9d2154d964f25276e56b0"
-    sha256 cellar: :any,                 monterey:       "d7808a98e2d48da85aa2f944c064666b28df16a5423c79243debb982a59e2df8"
-    sha256 cellar: :any,                 big_sur:        "839bca0453b603865ae4576183a8c7f1fb463892a09665d37d9260fda6e28a84"
-    sha256 cellar: :any,                 catalina:       "cb7f81d263eb563fd5915466d096bb3cb937228c8feca32f98a652ebfd2c28a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6eaaacdd46da353a9045962c51f8cb07e31aef2d045068c47c9e100738f75d82"
+    sha256 cellar: :any,                 arm64_monterey: "053b9445d8277b6112f7673a8742d5bbdd4bc76a82476df844f48846f6852f32"
+    sha256 cellar: :any,                 arm64_big_sur:  "39e74e217acb479377a26c536d903e0cfb624935d8e906a84ce653729c82d2b7"
+    sha256 cellar: :any,                 monterey:       "5a5463782f983c1ac8b20b53bd23aa4e1da5bb1cf6f7c84b155854c9c9e4fd65"
+    sha256 cellar: :any,                 big_sur:        "810fb0ae9d76997c2e00489737285bd2d3209a694e76857b08dddf19d9d6b1b5"
+    sha256 cellar: :any,                 catalina:       "58f8a14850a928dd468c8ae5d96a70edf688c791ad19a63975cafc4bcdd401f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2309f7a60fa60b0c541a1f873e9ff38379f4ba154d6dfe1fe5bd156504e13474"
   end
 
   depends_on "dotnet"
