d4a034854731700a17d6efa1d296caf70226f321
FX Coudert <fxcoudert@gmail.com>
2022-10-12 09:05:48 +0000

    ferium: add 4.2.0 bottle.

--- Formula/ferium.rb
+++ Formula/ferium.rb
@@ -6,6 +6,15 @@ class Ferium < Formula
   license "MPL-2.0"
   head "https://github.com/gorilla-devs/ferium.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e24deb5694dbb062543dc6b90e822096e749511db10326c96f5c0940a9862ffe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c4bdbc18b75d868b158bd3a73aca64031299c2090a01cb91bbe93cec75ba1f61"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd5120a79f12d1b075f746cb3528e84b3646d5721e6ceb5f94a176d9d7dbc60f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "709ba0ec0d3a57922dc34696c7313fb88bc83b4b5a91255ed37dade8ea372713"
+    sha256 cellar: :any_skip_relocation, catalina:       "cda21e75f57b5c76b4d58f4af0af1d76ff63c83dae6cff1827e0a50e8b148600"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28477eff1fa21da1033f71274dcb36986542dc0753a94bb8d63a408354309618"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
