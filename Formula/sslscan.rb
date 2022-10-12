b6a61a7d878c500325825d3268e791c249e199c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-12 09:07:51 +0000

    sslscan: update 2.0.15 bottle.

--- Formula/sslscan.rb
+++ Formula/sslscan.rb
@@ -7,13 +7,13 @@ class Sslscan < Formula
   head "https://github.com/rbsec/sslscan.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "eb8f8ad40b64c81597429af23975a0139e5a694298b07d9490953b99b18dde47"
-    sha256 cellar: :any,                 arm64_big_sur:  "adb5286beba7c576529052e28b772307356a6e3810bba69b28293d25af2a7d20"
-    sha256 cellar: :any,                 monterey:       "1caaf3acce4bc823da5447ac2b7ebdaf0187e342ce517dee0031952703466ea8"
-    sha256 cellar: :any,                 big_sur:        "b528f5d73f4b397ce34b045b5f1afba9d443a34e7793b0d5f5e6af71dfe564df"
-    sha256 cellar: :any,                 catalina:       "81a1b69fa2b4059b214d3b3489a4383b3e6a169f712c2cdbe28752ba97a15b8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "944b95bab55036638b1ef17d1acd7613f6ba7357b080f777ead8e7d9b3d0ee64"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_monterey: "564ea98dcc718997ff93e4ec265795a0ecc4cd233b4084c3941311cf4ed05345"
+    sha256 cellar: :any,                 arm64_big_sur:  "34d194e70c43c06937e78d21468d881950b5a197fcf719278340a190a2b71df2"
+    sha256 cellar: :any,                 monterey:       "93da99b49f6dd190ec22338e4926b905f077695b3dbaf2e50b36a70ab1641e04"
+    sha256 cellar: :any,                 big_sur:        "693cb3fda53855f2d38e50a0c7ac1d0979f27d8ab08d93780d7cf454e954ebf6"
+    sha256 cellar: :any,                 catalina:       "07f57f95634191f1a8ab4b1e849bee389d4d3b01c824f643d507d28252b18bfb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1f8070195f33e7ec00b90991ec618ee1be4fbc9c1340cb00cb6ade5a8cde929"
   end
 
   depends_on "openssl@3"
