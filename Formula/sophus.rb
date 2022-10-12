97a672bcabafa940f4192122e10a829d2ba086be
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2022-10-12 21:01:20 +0200

    sophus: reintroduce fmt dependency after upstream fix
    Sophus compilation was broken with libfmt version >= 9.0. As a
    workaround the dependency was removed and "basic logging" enabled:
    - https://github.com/Homebrew/homebrew-core/pull/108156
    - https://github.com/Homebrew/homebrew-core/pull/110137
    
    The sophus formula was recently updated to version 22.10, which fixed
    the compatibility with libfmt version >= 9.0:
    - https://github.com/strasdat/Sophus/pull/376
    - https://github.com/Homebrew/homebrew-core/pull/11286
    
    This PR removes the workaround.
    
    Closes #112988.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sophus.rb
+++ Formula/sophus.rb
@@ -4,6 +4,7 @@ class Sophus < Formula
   url "https://github.com/strasdat/Sophus/archive/refs/tags/v22.10.tar.gz"
   sha256 "270709b83696da179447cf743357e36a8b9bc8eed5ff4b9d66d33fe691010bad"
   license "MIT"
+  revision 1
   head "https://github.com/strasdat/Sophus.git", branch: "master"
 
   bottle do
@@ -13,13 +14,13 @@ class Sophus < Formula
   depends_on "cmake" => [:build, :test]
   depends_on "ceres-solver"
   depends_on "eigen"
+  depends_on "fmt"
 
   fails_with gcc: "5" # C++17 (ceres-solver dependency)
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-                    "-DBUILD_SOPHUS_EXAMPLES=OFF",
-                    "-DSOPHUS_USE_BASIC_LOGGING=ON"
+                    "-DBUILD_SOPHUS_EXAMPLES=OFF"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     (pkgshare/"examples").install "examples/HelloSO3.cpp"
@@ -30,7 +31,6 @@ class Sophus < Formula
     (testpath/"CMakeLists.txt").write <<~EOS
       cmake_minimum_required(VERSION 3.5)
       project(HelloSO3)
-      add_compile_definitions(SOPHUS_USE_BASIC_LOGGING)
       find_package(Sophus REQUIRED)
       add_executable(HelloSO3 HelloSO3.cpp)
       target_link_libraries(HelloSO3 Sophus::Sophus)
