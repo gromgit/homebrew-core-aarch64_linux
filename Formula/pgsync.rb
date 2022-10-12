3ac29e051e4325dbe2781829c296b284c00efa56
Dawid Dziurla <dawidd0811@gmail.com>
2022-10-12 10:36:35 +0000

    pgsync: add 0.7.2 bottle.

--- Formula/pgsync.rb
+++ Formula/pgsync.rb
@@ -5,6 +5,15 @@ class Pgsync < Formula
   sha256 "1c6adcd9f3bd145f573d80dfde06cb83e0ce3653492d9ae767493abf541c469c"
   license "MIT"
 
+  bottle do
+    sha256                               arm64_monterey: "15e8a9d23d340aeb0cb1bb198f7bfdc7d60dfb60b42b942d7c43e529da775fc9"
+    sha256                               arm64_big_sur:  "0c1bfbbe1d3e56109f8edf2d431940fc84debb9f20e1de3021eeb2ea671e7690"
+    sha256                               monterey:       "eecfe3e70e650c26a8342d5e3208b763cd4a3bd3dc02e48d449493693de37c02"
+    sha256                               big_sur:        "56215ee5262ef7da1a9914e57d18ed7808cbb16414f45b9e67c1d341a263c54b"
+    sha256                               catalina:       "bccffa68eae31f5e350c1095c67febfcc49bb859619cadded76ecc1961e285d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0d70c710f2849f5a39d6d5759d0cb358cc79e190aaef1625c97686ab980d176c"
+  end
+
   depends_on "libpq"
 
   uses_from_macos "ruby"
