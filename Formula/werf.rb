2b6b1b8f1d04f4caca9c6c782c32c41dd23c86ac
Rui Chen <rui@chenrui.dev>
2022-10-12 10:13:42 -0400

    werf 1.2.180
    Closes #112971.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/werf.rb
+++ Formula/werf.rb
@@ -1,8 +1,8 @@
 class Werf < Formula
   desc "Consistent delivery tool for Kubernetes"
   homepage "https://werf.io/"
-  url "https://github.com/werf/werf/archive/refs/tags/v1.2.178.tar.gz"
-  sha256 "524cd8ee243fa6d03e7efcbdbe5c1ae71a8b859c3f782bce5924e258b345b0e5"
+  url "https://github.com/werf/werf/archive/refs/tags/v1.2.180.tar.gz"
+  sha256 "f69784435cb86b90ea9979fce56f54e8ad4129e7c26648646b4b329e61cc7e90"
   license "Apache-2.0"
   head "https://github.com/werf/werf.git", branch: "main"
 
