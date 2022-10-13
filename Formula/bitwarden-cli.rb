f2927e24e113c552f4dc3b97690a3b40889fee01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-13 01:34:27 +0000

    bitwarden-cli: update 2022.10.0 bottle.

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -8,12 +8,12 @@ class BitwardenCli < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "55ff48c502252c875ec0e48e12e5450ff41645e9676cc3caee9636a2ded520e4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "55ff48c502252c875ec0e48e12e5450ff41645e9676cc3caee9636a2ded520e4"
-    sha256 cellar: :any_skip_relocation, monterey:       "b7954ff0052cdfa6aca3a7eb177c216beeeab5ce5798eb8997410512f07e54b1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b7954ff0052cdfa6aca3a7eb177c216beeeab5ce5798eb8997410512f07e54b1"
-    sha256 cellar: :any_skip_relocation, catalina:       "b7954ff0052cdfa6aca3a7eb177c216beeeab5ce5798eb8997410512f07e54b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55ff48c502252c875ec0e48e12e5450ff41645e9676cc3caee9636a2ded520e4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d18f0983eb6a713f18036044de190d49a3f74640dd54885e3894ee4b67b06e18"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d18f0983eb6a713f18036044de190d49a3f74640dd54885e3894ee4b67b06e18"
+    sha256 cellar: :any_skip_relocation, monterey:       "4fcf5389168cf338b5a34535d7a0fb31901cfc553c98b47dbedd8855081c4368"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4fcf5389168cf338b5a34535d7a0fb31901cfc553c98b47dbedd8855081c4368"
+    sha256 cellar: :any_skip_relocation, catalina:       "4fcf5389168cf338b5a34535d7a0fb31901cfc553c98b47dbedd8855081c4368"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d18f0983eb6a713f18036044de190d49a3f74640dd54885e3894ee4b67b06e18"
   end
 
   depends_on "node"
