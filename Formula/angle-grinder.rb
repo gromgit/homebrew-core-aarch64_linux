579ff3adadf6d3a3e7cb1194bcf612474f15db12
Michael Cho <cho-m@tuta.io>
2022-10-12 20:23:01 -0700

    angle-grinder: remove `openssl@1.1` dependency
    Closes #113016.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -17,11 +17,6 @@ class AngleGrinder < Formula
 
   depends_on "rust" => :build
 
-  on_linux do
-    depends_on "pkg-config" => :build
-    depends_on "openssl@1.1"
-  end
-
   def install
     system "cargo", "install", *std_cargo_args
   end
