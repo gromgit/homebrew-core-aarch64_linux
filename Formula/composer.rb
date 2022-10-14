938d2fda30815aa69350fa4d606a99a8a1ebc985
Sean Molenaar <smillerdev@me.com>
2022-10-14 17:14:20 +0200

    composer 2.4.3
    Closes #113130.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -1,8 +1,8 @@
 class Composer < Formula
   desc "Dependency Manager for PHP"
   homepage "https://getcomposer.org/"
-  url "https://getcomposer.org/download/2.4.2/composer.phar"
-  sha256 "8fe98a01050c92cc6812b8ead3bd5b6e0bcdc575ce7a93b242bde497a31d7732"
+  url "https://getcomposer.org/download/2.4.3/composer.phar"
+  sha256 "26d72f2790502bc9b22209e1cec1e0e43d33b368606ad227d327cccb388b609a"
   license "MIT"
 
   livecheck do
