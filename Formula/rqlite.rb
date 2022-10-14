e8a63a60e9f5a3b3687ea2d48fd0146f4753cf16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-14 11:05:12 +0000

    rqlite: update 7.7.1 bottle.

--- Formula/rqlite.rb
+++ Formula/rqlite.rb
@@ -7,12 +7,12 @@ class Rqlite < Formula
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3694564636bf176f7edd95ea2a9cbda5022d1a7b8c42bdc82188c70115ea5cde"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "afb895eab0e9fa85cd7610fc848a1c3eb7ce28a8f43efb9fcf6f14c611c56af6"
-    sha256 cellar: :any_skip_relocation, monterey:       "167032cd4e6cae4db4e05a29185147e08db4aea4fd4771b9fb692d91e4d7ad2e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "34601ed2a1ebf294230b4c8a0760694a80d46bfdb054a186b36da01fc8905d50"
-    sha256 cellar: :any_skip_relocation, catalina:       "fb4d5daffa74b42f54cadc1add73d5958d25fc15e47872911a4a47b70deaadbd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "514f79d1562de92cb68e4b08f0fba7334844b327a33bc7391a04eb45bc358a94"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8b96b5de145aee9c3838f7fbe65a10388d2cf3658e01d4dc116f241fe2e5d962"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "64318bce2fac34e52d7b144c7f81fbfebec85f2975e088c5b34c9eceb7fbdfb2"
+    sha256 cellar: :any_skip_relocation, monterey:       "1efa517b8cae9c8223b64e6469050f47dd4f3c4b80d084d20a33b2f5013b1fe7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce02c66cc12d50031689782afa4a70a9a5a604ec7415298cf277a3e0c883b11b"
+    sha256 cellar: :any_skip_relocation, catalina:       "516f7d981c04712e4dec14e7ba19968ebd7c1bacd6a12a093a35dd6c65a35410"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b4e69e13bbe21362723960d5266fcc8aa31f9f2cdfe8ab5818f02a2e9abc1376"
   end
 
   depends_on "go" => :build
