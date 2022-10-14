e14a8f53197f1f2574c8ecd362610c8a30799520
Michael Cho <cho-m@tuta.io>
2022-10-13 17:13:29 -0700

    authoscope: use `openssl@3` on Linux
    Closes #113079.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/authoscope.rb
+++ Formula/authoscope.rb
@@ -15,14 +15,17 @@ class Authoscope < Formula
   end
 
   depends_on "rust" => :build
-  depends_on "openssl@1.1"
 
   uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "openssl@3" # Uses Secure Transport on macOS
+  end
 
   def install
     # Ensure that the `openssl` crate picks up the intended library.
     # https://crates.io/crates/openssl#manual-configuration
-    ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
+    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix if OS.linux?
 
     system "cargo", "install", *std_cargo_args
     man1.install "docs/authoscope.1"
