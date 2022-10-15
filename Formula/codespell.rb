d95aec9da6e10b642a087770690becff3e5c8217
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-15 08:05:57 +0000

    codespell: update 2.2.2 bottle.

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -8,12 +8,12 @@ class Codespell < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "059cc7dd356d2dbf188227a91bc16698daa98dc0949ed40c98e791602a6938d2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "059cc7dd356d2dbf188227a91bc16698daa98dc0949ed40c98e791602a6938d2"
-    sha256 cellar: :any_skip_relocation, monterey:       "11aee07f3b9bb98467b85a60c47aec8cf0892e5f78c9efbebe61b27d4d0aefd4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "11aee07f3b9bb98467b85a60c47aec8cf0892e5f78c9efbebe61b27d4d0aefd4"
-    sha256 cellar: :any_skip_relocation, catalina:       "11aee07f3b9bb98467b85a60c47aec8cf0892e5f78c9efbebe61b27d4d0aefd4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7fde00f2068ab44164a0010e9dfcb8cd344541dd04fb321ae915954ae1c30efa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "61440d27339b57aee89b9a612428ac82ab38d38e258a55c8a37796849e2ca298"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f70e8354454cd466c9441b81ca3d8ef9e228201ea9f4f1994ac19430fddfcf5"
+    sha256 cellar: :any_skip_relocation, monterey:       "7a559ac39ab29bdf76a08bc859f5967da40d724b0260d79a6068e8db5bf0a316"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a8d33af30d3c4982dc0d2920e93d5a948c7e813cf07ed7aeaa5a93770d71a88b"
+    sha256 cellar: :any_skip_relocation, catalina:       "472182472754219f1e599be8025fbf87622902766f54cce2ad1c3d92353be521"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "add0ee88b85384f3a22ec3c4d1a0d56ec56f3cbbd4d3034b5def440a568a9b0e"
   end
 
   depends_on "python@3.10"
