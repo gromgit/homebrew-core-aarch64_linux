d9b30da6709f419af39705d308db8968d68d56a8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-10-15 06:11:33 +0000

    libretls: add 3.5.2 bottle.

--- Formula/libretls.rb
+++ Formula/libretls.rb
@@ -10,6 +10,15 @@ class Libretls < Formula
     regex(/href=.*?libretls[._-]v?(\d+(?:\.\d+)+(?:p\d+)?)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "00e8653e8a3e9cb237756b540dfc0bcc94b1f27067b4b9d02112e4ca1e8c57e2"
+    sha256 cellar: :any,                 arm64_big_sur:  "bb69e5195aeefd21ec4dd827b714f9e0285f9efdc623c39fa83bf18a17e17d02"
+    sha256 cellar: :any,                 monterey:       "489fb71ab2c232d4733f4bff4c55dc2c9e0673aaf63f75ca749b443d06e744eb"
+    sha256 cellar: :any,                 big_sur:        "d499ae8fb0216c9ae2bcacb9a06878c43fca49d085a8bd7a9a72beeda189f34e"
+    sha256 cellar: :any,                 catalina:       "5a9c93883b7e683bd6508a7851055cd46f8129ae77979a96105792a13753e54d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5ccfc7d70ef4d7ae9b55d18b01cdfa5dfe83455058eb3251effa767626306e06"
+  end
+
   depends_on "openssl@3"
 
   def install
