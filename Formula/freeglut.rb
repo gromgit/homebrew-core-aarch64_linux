7fd415a1441d88ad349054a19aaac9afafa20c8d
Porkepix <porkepix@gmail.com>
2022-10-15 11:15:46 +0200

    freeglut 3.4.0
    * freeglut 3.4.0
    * freeglut: switch to GitHub-hosted tarball
      The project has moved to GitHub, the the SourceForge tarballs are merely
      mirrored. See https://freeglut.sourceforge.net/news.php (entry on 9
      March 2022).
    
    Closes #113191.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/freeglut.rb
+++ Formula/freeglut.rb
@@ -1,8 +1,8 @@
 class Freeglut < Formula
   desc "Open-source alternative to the OpenGL Utility Toolkit (GLUT) library"
   homepage "https://freeglut.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/freeglut/freeglut/3.2.2/freeglut-3.2.2.tar.gz"
-  sha256 "c5944a082df0bba96b5756dddb1f75d0cd72ce27b5395c6c1dde85c2ff297a50"
+  url "https://github.com/FreeGLUTProject/freeglut/releases/download/v3.4.0/freeglut-3.4.0.tar.gz"
+  sha256 "3c0bcb915d9b180a97edaebd011b7a1de54583a838644dcd42bb0ea0c6f3eaec"
   license "MIT"
 
   bottle do
