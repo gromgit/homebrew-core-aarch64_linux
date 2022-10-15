183dcf2e4f39cadfc651a564b529edf56e144cc7
Rui Chen <rui@chenrui.dev>
2022-10-15 15:22:27 -0400

    manticoresearch 5.0.3
    * manticoresearch 5.0.3
    * manticoresearch: remove build patch
    
    Closes #113210.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -1,23 +1,11 @@
 class Manticoresearch < Formula
   desc "Open source text search engine"
   homepage "https://www.manticoresearch.com"
+  url "https://github.com/manticoresoftware/manticoresearch/archive/refs/tags/5.0.3.tar.gz"
+  sha256 "416b9e609529af9cf9570b41e3e770de9511c5ed4d0c27530bfec7da396b13b2"
   license "GPL-2.0-only"
-  revision 1
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch.git", branch: "master"
-
-  stable do
-    url "https://github.com/manticoresoftware/manticoresearch/archive/refs/tags/5.0.2.tar.gz"
-    sha256 "ca7828a6841ed8bdbc330516f85ad3a85749998f443b9de319cec60e12c64c07"
-
-    # Allow system ICU usage and tune build (config from homebrew; release build; don't split symbols).
-    # Remove with next release
-
-    patch do
-      url "https://github.com/manticoresoftware/manticoresearch/commit/70ede046a1ed.patch?full_index=1"
-      sha256 "8c15dc5373898c2788cea5c930c4301b9a21d8dc35d22a1bbb591ddcf94cf7ff"
-    end
-  end
 
   bottle do
     rebuild 1
