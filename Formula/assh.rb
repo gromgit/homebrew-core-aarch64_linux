ee2c5aee2499eecafcd90bfad1fe70b8b7652ae8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-16 03:57:54 +0000

    assh: update 2.15.0 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -7,13 +7,12 @@ class Assh < Formula
   head "https://github.com/moul/assh.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "db6ad594cc3ca142f3b9e674d20296cc9c2b5a7b0ccffb4250b2ed95710f8783"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7b502fd5dc796e9ff7e35cf4636c238d0ec21546094fe9bd3f467fa7189b630b"
-    sha256 cellar: :any_skip_relocation, monterey:       "90972582d4d7c8c8b53c355e7cb67e04bbcfcc13443a166526713fe3cea64bce"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fac57bd5f964f9f71d89e6117512956874a85ef842d6b5eeecd7eafb03d0ab8f"
-    sha256 cellar: :any_skip_relocation, catalina:       "7b6450a9c4797ec5cbd38e21e875fac6e49dd961d7c44e42261c2e78a754957c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "07fb22b39dda8f9805dd6ea3f0f8e48d31b33b5e9183ad8f5702ec75c246d3f5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c31affa151098673530f050292e4d04659c3786cf19412b8f652013480519a20"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "678036dadc14bc728ef7b7d0d7feda46e175deaab47cc6f8ad80bda74ad49d62"
+    sha256 cellar: :any_skip_relocation, monterey:       "2490103a8c5ef28f6eaf5d7e4286244de7367171431e959424829eb612364bfe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a5f782b77042bbc321d0e619d03b890147aa1e699f3d000206a10f1a8889c04a"
+    sha256 cellar: :any_skip_relocation, catalina:       "778b845f4546ccfe60933063e24b1c9179d1e836f26581217ae1b094233eb41d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "760e85e343ce75d153710cb34214ea46f3217e16fc048330d835920a0500bfaf"
   end
 
   depends_on "go" => :build
