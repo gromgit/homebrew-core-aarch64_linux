102b89a1b7901bdcf0dfc19f7c9d3e3ff5370907
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-16 13:16:51 +0000

    sccache: update 0.3.0 bottle.

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -7,12 +7,13 @@ class Sccache < Formula
   head "https://github.com/mozilla/sccache.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "00cf38a8c23bf93560519b341c0e9844c832e8bbc493729765546f158c1cd7a2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4dc37060bcafee2e89b450bd2afef843596e6f59041eb81f22f3aabc9471a78"
-    sha256 cellar: :any_skip_relocation, monterey:       "9cccb3f0b35fa0da498e4e243465d4ca3da4c5a05908706298a15628b3456fd9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "83af98faab6990a1694651e30183ed5504e171a3b6a3301bbe747db208c42a92"
-    sha256 cellar: :any_skip_relocation, catalina:       "dfd1a5dbb28175aec34071f37f2d528bdba5dd8d0a68499b0b646eb0732f4b0a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "64872332a7a554494916287f5716963b93ddf5000d1a662a36f02152929d9c80"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "03a2718de73604db1bb56de5d585fb61b5d6d2c8a7eb16c066124cd3d476bd23"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c13133e66ad27fb4bfc33cdfec0b567c30bf198582e6efb0b7531f598eef6d80"
+    sha256 cellar: :any_skip_relocation, monterey:       "ae8579e33a6b89749bf11d7853911ad09e061e442a580f6f842afed2b31bd99e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f029209a8e6c5621a9470e77f5d262aa90550b5a8eae847268dbc8fcf4da84d0"
+    sha256 cellar: :any_skip_relocation, catalina:       "1d01fc0f2cf10b9d70836ecf04b63f88e118ee7e5aae12db60a22b1a7e923299"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f216377a2f2f7264425125ba4e355595742c1335c3844930493dd4737a4f4c0d"
   end
 
   depends_on "rust" => :build
