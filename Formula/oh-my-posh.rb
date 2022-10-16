d99c02c7ab2d85298f9dcd93d8549ee8f4ea01eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-16 03:57:42 +0000

    oh-my-posh: update 12.3.1 bottle.

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -7,12 +7,12 @@ class OhMyPosh < Formula
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1cfa774523f5f9699fb6b98277c187f37c48bd6c074f2c08e3ab317e20273291"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6e9c5df3f43c2891fdb163815c389bc57ba8df88d62e1d85846f322789c06b95"
-    sha256 cellar: :any_skip_relocation, monterey:       "52feaa3dd0f865088be862ebb1bf08a917e4480516c45f326bc7060aee1faf9a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e84aee2e1832bdd9febd13e891adf22766265710e36e14d13097537909a19a58"
-    sha256 cellar: :any_skip_relocation, catalina:       "4ab2c03efacb49c6bd8545ec4d7f902b41732b728bf903c1fea45e8c9c07c504"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "60a13277562de33a56d887c5c65c080952d8125e22ec920563ed98dc095ea34d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3774625eb4a09acfe7036fb77dce0cee69c913a8f4b8740326506a4febf755d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "77c1f11db30460ef21ca37d9d2541d53837e5630cf85af4bf1728dec4ab9333a"
+    sha256 cellar: :any_skip_relocation, monterey:       "9fa7bcb395f1831d3ff954ea46f78dc872cdf7d377a7c096b5d587d40b528900"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5dfdd5a135b0915f982cc7ca1d5c35d3e1df73e0743bb191f77f0bc375a8757f"
+    sha256 cellar: :any_skip_relocation, catalina:       "95a12dd589a839084357bf801f6e13df81c9f6f0a4cb06fe63e8ff14e9a62a34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "397be71a9a13ecf2a84da49d4191bd9fbf9c4d7f50d2e4d23fd47005a8dc7acb"
   end
 
   depends_on "go" => :build
