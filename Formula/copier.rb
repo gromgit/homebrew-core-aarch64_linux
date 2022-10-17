c3072ebacf208f760b040a86c224362d659c1977
David Baumgold <david@davidbaumgold.com>
2022-10-17 21:20:59 +0200

    copier: depend on pygments
    Closes #113322.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/copier.rb
+++ Formula/copier.rb
@@ -16,6 +16,7 @@ class Copier < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "064e8b305fd3f46c95b78d32075fe193cef55cea2e5bd6c0ef47356c439e20f6"
   end
 
+  depends_on "pygments"
   depends_on "python-typing-extensions"
   depends_on "python@3.10"
   depends_on "pyyaml"
@@ -73,11 +74,6 @@ class Copier < Formula
   resource "pydantic" do
     url "https://files.pythonhosted.org/packages/7d/7d/58dd62f792b002fa28cce4e83cb90f4359809e6d12db86eedf26a752895c/pydantic-1.10.2.tar.gz"
     sha256 "91b8e218852ef6007c2b98cd861601c6a09f1aa32bbbb74fab5b1c33d4a1e410"
-  end
-
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/e0/ef/5905cd3642f2337d44143529c941cc3a02e5af16f0f65f81cbef7af452bb/Pygments-2.13.0.tar.gz"
-    sha256 "56a8508ae95f98e2b9bdf93a6be5ae3f7d8af858b43e02c5a2ff083726be40c1"
   end
 
   resource "pyparsing" do
