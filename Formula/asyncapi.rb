4200b18637676a2af5c7ef8ed10d29ac66a4fae0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-17 13:23:56 +0000

    asyncapi: update 0.25.1 bottle.

--- Formula/asyncapi.rb
+++ Formula/asyncapi.rb
@@ -8,12 +8,12 @@ class Asyncapi < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b3f02c314eeb3c1cfd09e27fe1d9bb17f9f05c1566decbb249976c0b46ae8b9f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b3f02c314eeb3c1cfd09e27fe1d9bb17f9f05c1566decbb249976c0b46ae8b9f"
-    sha256 cellar: :any_skip_relocation, monterey:       "0791fd7bea5a55f977be8c67afea9bba5134433a5b38029ac9ec785f643d4b20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0791fd7bea5a55f977be8c67afea9bba5134433a5b38029ac9ec785f643d4b20"
-    sha256 cellar: :any_skip_relocation, catalina:       "0791fd7bea5a55f977be8c67afea9bba5134433a5b38029ac9ec785f643d4b20"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8d8b7b1688624be3cf01b10ba1be4ce92b76e6e7e58acbfed6b0173cc3da3fa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3eb62b37a5160a2c7c24733e5116cdad8bc03526005574678cc41f73e583734"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a3eb62b37a5160a2c7c24733e5116cdad8bc03526005574678cc41f73e583734"
+    sha256 cellar: :any_skip_relocation, monterey:       "ee630595b9b0b376ddc739646fffa7ee58960899f3565c2f15a77b2dbe8cfc51"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ee630595b9b0b376ddc739646fffa7ee58960899f3565c2f15a77b2dbe8cfc51"
+    sha256 cellar: :any_skip_relocation, catalina:       "ee630595b9b0b376ddc739646fffa7ee58960899f3565c2f15a77b2dbe8cfc51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "44ea90dc8bff2ca3ff17f268d36d085e56eec4ecec6a0634ea5fa6ee5d0a52f8"
   end
 
   depends_on "node"
