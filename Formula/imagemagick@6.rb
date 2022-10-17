a0e9b44537e5866b0ec266115a3868fdb531dbc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-17 10:08:22 +0000

    imagemagick@6: update 6.9.12-66 bottle.

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -12,12 +12,12 @@ class ImagemagickAT6 < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "4d02b9e1b05078ec1e6defc3d22b092f8dff505bc2ff5f0611e38a2ec7e37dec"
-    sha256 arm64_big_sur:  "fc753a79efb2d9e13c7a87a9868ed7672dbf21c2091c6b028979a4d818a02300"
-    sha256 monterey:       "9444d1a0494607ef253f7b52b1308241b744e247d733ffaf10cd437be335b2f5"
-    sha256 big_sur:        "76ceb0fddc69538f2278bd854bfafb5cbb712f2d52c243dce32fa983092be864"
-    sha256 catalina:       "f459987ef6b3f91eacbe5138397329ed900e23900d68682cc03b293af755ab25"
-    sha256 x86_64_linux:   "fa5ef99e5114004dc93d1e60019bea5f25793f892a1b37c8bc909517a9082fdf"
+    sha256 arm64_monterey: "36814e26ebb60d2798b8477645b95113e7308612818acf938201e5ae1c4b96f5"
+    sha256 arm64_big_sur:  "8327e5c12016e1773e20816c1bc436309cd716e11446f47c8eaefbc23820cc9f"
+    sha256 monterey:       "26f8a25f2bb7a1a4655d88234823d35ca171f1dded4a8139e27d1d43b233cea6"
+    sha256 big_sur:        "5d785176830f98ab386c35cb5eb7e0693ae5eeb22b5383990dbe4d9eac3dcb7d"
+    sha256 catalina:       "8ad9cd9327b14cf15650bdf80dd1f035bdd32eff1a05d0f6047ebc8e3c9b2764"
+    sha256 x86_64_linux:   "3284f2cc4a41dbf94886f005fc34c8fd74acb07a82d4546058fc729c5a17df35"
   end
 
   keg_only :versioned_formula
