510fbcae5a4918e9a7229c569aee2bdbeb443072
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-17 10:08:17 +0000

    homebank: update 5.5.8 bottle.

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -11,12 +11,12 @@ class Homebank < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "2cf244f9bab4fa2d0d492819c54555c4cbe27040589f3ea63ed429e78789557c"
-    sha256 arm64_big_sur:  "7e66fa1ed8e4f6dc62beb5cbfceb7594bb51ede3cceb3786379ba7e96b76306b"
-    sha256 monterey:       "66d79402ec99b25f258c4fd69c9226cfc7e0f48f4a63400a09bf9c16d92fb3e9"
-    sha256 big_sur:        "50c6ea69a69a7b3a9056415c441b32107050a98181493442c34cb68390a80315"
-    sha256 catalina:       "4085957bfa7f3379a204061b8a07383849f5fd9c55e8af5aaefa778b9d614be5"
-    sha256 x86_64_linux:   "ceb14ee33c2f22f9735dbe55cb47e854ee804df5b83db6d244d3eda1199e9ac5"
+    sha256 arm64_monterey: "214e609aefe8a8793ae0f8757ab971685526d2a078e9e69026458684054924be"
+    sha256 arm64_big_sur:  "70ed29226013af9eba03840ddc08c903864ee7dea75ab45329c54e12bccc680d"
+    sha256 monterey:       "e0244641f0d8c8bc24acf0b6fe603557cb30bef7912286a0d9e18ec31a1c98e9"
+    sha256 big_sur:        "e9ccec7c7bb765b8655db66b42397639da7c62d7dbd287176565e895e005dc5f"
+    sha256 catalina:       "e1db723e3f8c67cb74de784597fb17ba0a07861a1e67b58752057040b759b4c6"
+    sha256 x86_64_linux:   "c42d840ecdc2bef16bd974d304ab61dfa80cc9109d867a81d0a85d6287713a1f"
   end
 
   depends_on "intltool" => :build
