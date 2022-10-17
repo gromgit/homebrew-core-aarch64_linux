aa828588aea4e41a5bd4825f2deee1468dc6ceda
Porkepix <porkepix@gmail.com>
2022-10-17 15:47:36 +0200

    mx 6.9.9
    Closes #113302.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.9.8.tar.gz"
-  sha256 "eed30a89b74bf136bc48fb57b79a34c6ad37767338a6377cce66b56dad461570"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.9.9.tar.gz"
+  sha256 "d51fe9c167f50ad026a9e54cd70cfced20d1eac17bad37cf69c478ece4d8564f"
   license "GPL-2.0-only"
 
   livecheck do
