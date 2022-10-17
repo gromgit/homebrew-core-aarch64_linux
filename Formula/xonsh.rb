1f856006fc089cfac3c5e1ffac19b0823a1b56c7
David Baumgold <david@davidbaumgold.com>
2022-10-17 10:53:39 +0200

    xonsh: depend on pygments
    Closes #113290.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -17,6 +17,7 @@ class Xonsh < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea165a12f660f5bd0b05c2987f6f1f61df32e7efca738ce32106ca827823f7a1"
   end
 
+  depends_on "pygments"
   depends_on "python@3.10"
 
   # Resources based on `pip3 install xonsh[ptk,pygments,proctitle]`
@@ -25,11 +26,6 @@ class Xonsh < Formula
   resource "prompt-toolkit" do
     url "https://files.pythonhosted.org/packages/80/76/c94cf323ca362dd7baca8d8ddf3b5fe1576848bc0156522ad581c04f8446/prompt_toolkit-3.0.31.tar.gz"
     sha256 "9ada952c9d1787f52ff6d5f3484d0b4df8952787c087edf6a1f7c2cb1ea88148"
-  end
-
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/e0/ef/5905cd3642f2337d44143529c941cc3a02e5af16f0f65f81cbef7af452bb/Pygments-2.13.0.tar.gz"
-    sha256 "56a8508ae95f98e2b9bdf93a6be5ae3f7d8af858b43e02c5a2ff083726be40c1"
   end
 
   resource "pyperclip" do
