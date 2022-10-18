a4ec2e57bf99a20c3212f824da387bc4c6e3ef9b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-18 22:04:55 +0000

    subfinder: update 2.5.4 bottle.

--- Formula/subfinder.rb
+++ Formula/subfinder.rb
@@ -7,12 +7,12 @@ class Subfinder < Formula
   head "https://github.com/projectdiscovery/subfinder.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2dcb201560bae839ddc86d205518181cd4978757658fbfd35f48f7cc83137d40"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8509a483b499e04b66f3b8021b90eeee0bbafae374bfeabf3bbc1e7383a536f8"
-    sha256 cellar: :any_skip_relocation, monterey:       "d99ffc1873733d96be52c661c2f8d22165f4999b49dfbb2d367c03b0afb876d6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "408c34b1b4bd9d4e2be358695cc664712022b8df36568ae8329ebf88d5e99355"
-    sha256 cellar: :any_skip_relocation, catalina:       "6fdf5423fca605a127a1f132ca4bd33c13786aaf1ad4affc8d3b5e8ee835f8db"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "99c1e4f1e12fd3e246284d6fb074304dd0046e8019494f4952e1d70fd1990934"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e91cef227d1ef13da2b904b0d5e807dfd66ef6e1c0a41a4b5771447936c14abc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7998d6a3abfb3015b4dc97e93ace1c0608100e3b7f7d311696b0329b9cb46a20"
+    sha256 cellar: :any_skip_relocation, monterey:       "5706bd4c68ae6c73a9d8a92939b3b24604866e1856c6d28d3692a901c069f239"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2f5054220a14d4478407e8a5ef2784470fd28dcfb3b282d6e441bad3a8db4857"
+    sha256 cellar: :any_skip_relocation, catalina:       "59f706d59b9c143e71847561d703f55976eac7033e375a8295d230f3067ecb78"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa84aace5c80dceabcf2e0e8dfb35a9bfaced6e0415cde724e941a21d9955071"
   end
 
   depends_on "go" => :build
