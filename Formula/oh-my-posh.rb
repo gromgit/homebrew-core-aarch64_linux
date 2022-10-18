501ead1b5d2b8564bc5681cf8754e0a87a402543
Porkepix <porkepix@gmail.com>
2022-10-18 16:43:31 +0200

    oh-my-posh 12.5.0
    * oh-my-posh 12.4.0
    * oh-my-posh 12.5.0
    
    Closes #113418.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v12.3.2.tar.gz"
-  sha256 "59f54fc50ad89061f52d85472fba35c8305cd6aab66c1f7fb6aea34fd7286ad1"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v12.5.0.tar.gz"
+  sha256 "a0327d7bec7f4e0fb4a65902ea174781e27da75e1318b40c4417082ebb7dce13"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
