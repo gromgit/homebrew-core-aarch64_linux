9a4b435075945ca29563e01558c1f5a0d340915f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-18 19:10:12 +0000

    git: update 2.38.1 bottle.

--- Formula/git.rb
+++ Formula/git.rb
@@ -12,12 +12,12 @@ class Git < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "95d2600cfb59d10b339f58f55f76e336559b083c210cadba2b99a031a1ec485b"
-    sha256 arm64_big_sur:  "455134afe2b1a474966cba27fddb78e5a03af78fa40b30342446802b41707d02"
-    sha256 monterey:       "44f21599efa7aeded48999cf4753a9655b0f87e901883f896d9acc7cae338011"
-    sha256 big_sur:        "ec8d8cd2f1544752f4d23074edddfcc8cf0426eb7e96dc842b6eb7c0c1867299"
-    sha256 catalina:       "2f404e627b4ad9073cdea1906115b436aa2d30095a05bd2cad863789c2a8b038"
-    sha256 x86_64_linux:   "ca4c2bfbf4967b2483631099f6f0737f88f2cc5fd1a264c83c23bd5fb508f8f5"
+    sha256 arm64_monterey: "2bb13b0b8ac3c1c88679750e79f658dfc4a29b0925145d28d434089528015b7a"
+    sha256 arm64_big_sur:  "ce2fd93df6676fdae23592b182853ff08decf49eb4aa3f0d1c0470bb612b2e06"
+    sha256 monterey:       "d938a6f7023775ca5e4f38766087d60c991efecf705925fe816ba1665a59ecf3"
+    sha256 big_sur:        "7ed8f4e5e282e11a56926115e69d9cdae56c4fb6a752f69e7d93f4445acaf835"
+    sha256 catalina:       "aee350c1b369aad9aaaa22685321f5993cf656026be153f7b573a77f5f8d5446"
+    sha256 x86_64_linux:   "bdde1efa650eb07f21e0050ffb15968eb29ac0838946d15498c152427caf7aba"
   end
 
   depends_on "gettext"
