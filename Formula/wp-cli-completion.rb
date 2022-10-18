451c3e738ae2f19878d7a4c08d7cfd5e0413e6fc
Rui Chen <rui@chenrui.dev>
2022-10-17 21:30:55 -0400

    wp-cli-completion 2.7.1
    Closes #113372.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wp-cli-completion.rb
+++ Formula/wp-cli-completion.rb
@@ -1,8 +1,8 @@
 class WpCliCompletion < Formula
   desc "Bash completion for Wpcli"
   homepage "https://github.com/wp-cli/wp-cli"
-  url "https://github.com/wp-cli/wp-cli/archive/v2.7.0.tar.gz"
-  sha256 "408f5e71bf71dbedfc339af0dc5846eeeacea70ed7ca13fedea5fdb366eb41df"
+  url "https://github.com/wp-cli/wp-cli/archive/v2.7.1.tar.gz"
+  sha256 "0062dcbc62658dd2e4361e97d1ec12602f4a6661e771b730158e615aff3ea695"
   license "MIT"
   head "https://github.com/wp-cli/wp-cli.git", branch: "master"
 
