0a56082b3d9346f07b466ae2ba005262c8bdd7aa
Rui Chen <rui@chenrui.dev>
2022-10-17 21:32:37 -0400

    libxmu 1.1.4
    Closes #113373.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libxmu.rb
+++ Formula/libxmu.rb
@@ -1,8 +1,8 @@
 class Libxmu < Formula
   desc "X.Org: X miscellaneous utility routines library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXmu-1.1.3.tar.bz2"
-  sha256 "9c343225e7c3dc0904f2122b562278da5fed639b1b5e880d25111561bac5b731"
+  url "https://www.x.org/archive/individual/lib/libXmu-1.1.4.tar.xz"
+  sha256 "210de3ab9c3e9382572c25d17c2518a854ce6e2c62c5f8315deac7579e758244"
   license "MIT"
 
   bottle do
