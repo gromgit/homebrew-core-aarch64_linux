7ced7f0b877d4624eca7aab8c9aa7e75b82a5265
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2022-10-18 18:38:48 +0000

    k9s 0.26.7
    Closes #113440.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,8 +2,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.26.6",
-      revision: "c66002d9868fc172e0bb8091ad87d61be027aae2"
+      tag:      "v0.26.7",
+      revision: "37569b8772eee3ae29c3a3a1eabb34f459f0b595"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git", branch: "master"
 
