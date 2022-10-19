c0f42021b7309942bd6287e21846070421eb8461
Rui Chen <rui@chenrui.dev>
2022-10-18 20:11:29 -0400

    fuego-firestore 0.33.0
    Closes #113479.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fuego-firestore.rb
+++ Formula/fuego-firestore.rb
@@ -1,8 +1,8 @@
 class FuegoFirestore < Formula
   desc "Command-line client for the Firestore database"
   homepage "https://github.com/sgarciac/fuego"
-  url "https://github.com/sgarciac/fuego/archive/refs/tags/0.32.0.tar.gz"
-  sha256 "ed16bcdef7e3463fbd12bd5daba80b36401ed0ea2ea4acc699eecde3d91b17fd"
+  url "https://github.com/sgarciac/fuego/archive/refs/tags/0.33.0.tar.gz"
+  sha256 "25281f2242fe41b0533255a0d4f0450b1f3f8622d1585f8ae8cda1b116ca75d0"
   license "GPL-3.0-only"
 
   bottle do
