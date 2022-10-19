e0358ffe6834a42838a0e6abe82e830ed9b2ce78
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-19 08:12:26 +0000

    brev: update 0.6.124 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -11,12 +11,12 @@ class Brev < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c15df3154404e04c548ce7a98b74f8c94e0b7ec66397888b667c8a12d72acf88"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39f4a2021a86645b697bf81b35c61bd6f209fc6b310712e7ad0306dffaa189b3"
-    sha256 cellar: :any_skip_relocation, monterey:       "e33151a5b92d44e59910d632357c5fe34c192a7341eb907b8a319646662f5673"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4e638d1709509329fd89f60e9620418817aaaa797414bbd6b44933e45890d6f5"
-    sha256 cellar: :any_skip_relocation, catalina:       "379ecac2f3de7c142b494624ab7f816f63defdca6898aec60e61960c2983d35d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0640ae41f8943f593de489d18e380e7ec627ba3d99decc1a0a5643a3d69537a1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "87222a67d7fb992164be13a9a4831fbdd328cbfe2bca9b1b9977c1882b3661d0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4f58137d032259ccdc61706ce645ed761eafb3f8b063150b110cac60b5df4ba5"
+    sha256 cellar: :any_skip_relocation, monterey:       "64b8b4c239b9f4a1c8c89c64cd5de0aa6c455d4ec04c2e01fdc9ad22132bfa8e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7354351c4d8fc70ac39236316efac8d61f73f815f886754ad32232799538a15d"
+    sha256 cellar: :any_skip_relocation, catalina:       "9821f0808a3d3fe314f332532491f33a1116f6758ca08120df6c89d519131227"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "711b5f237f2b96caf697e0c7c5c02863c2fd4053a64eefd3807df923ae078c92"
   end
 
   # Required latest gvisor.dev/gvisor/pkg/gohacks instead of inet.af/netstack/gohacks
