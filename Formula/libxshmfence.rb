15ee6d237ea361b9d8bdf5ff1ca073c20d1f073a
Rui Chen <rui@chenrui.dev>
2022-10-19 20:41:52 +0000

    libxshmfence: update 1.3.1 bottle.

--- Formula/libxshmfence.rb
+++ Formula/libxshmfence.rb
@@ -6,14 +6,12 @@ class Libxshmfence < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "c0ba8ef6cf2d42ebfca71b52999a6d64bca255d043a7be5ff76d0eabb38e7110"
-    sha256 cellar: :any,                 arm64_big_sur:  "2651a6db4ef81559e17f4bb1e4ab5bbe246f54ec3cdce1f69ff9032f36789c36"
-    sha256 cellar: :any,                 monterey:       "6aac44b2ea3361a65292d074e875050fb1dc83126254570380261bd3d326e550"
-    sha256 cellar: :any,                 big_sur:        "89043d871005cac00e19ea895e9a30046296d0bd14edc0cd879328e53b3750a0"
-    sha256 cellar: :any,                 catalina:       "b6edc10c83f07c28fdece7c98b9be2e7c85518b0357311874c5bd5cca1217922"
-    sha256 cellar: :any,                 mojave:         "9ba60796d9101ebe5fe01d5fcbbc7f685fa8619fc0ba51004b0fd68488af47c4"
-    sha256 cellar: :any,                 high_sierra:    "ed7509cbecb38ec5fb75ae80de12248c8d25b931cbaff12f94f3566d05b77238"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e618baba61a3475e24f830b7cce59030f7298e9d255046465fee485efc25f42"
+    sha256 cellar: :any,                 arm64_monterey: "d54d02c984d1c3004dc80c6293a11a334074ef3ce84d5a411f3250bc9a8b22d2"
+    sha256 cellar: :any,                 arm64_big_sur:  "a20786f8e3b41c75c7a8f6ec72e135d6e8a4bedfa9a052174f80467fc4e18b77"
+    sha256 cellar: :any,                 monterey:       "fb31054f7e6e05c95d9e06ce328471128fa536b8f154a6dd33aaa9eb804f1980"
+    sha256 cellar: :any,                 big_sur:        "2a0ad3f3f628be1aae3ba5b3fc9f467fd5d855c5e6187c21e315875ff65dc8d9"
+    sha256 cellar: :any,                 catalina:       "db84e2577b6d8ee05c841d8a779873a303da9e836be8afe0e1a8e3e18e777323"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d4a8107cc0119312c78821e27241616dca90e38c9ff26801b88e5b99d985bc7"
   end
 
   depends_on "pkg-config" => :build
