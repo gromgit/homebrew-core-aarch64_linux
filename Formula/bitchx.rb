564f6071c292d52c67633b8dbf16e20eca6177a9
Michael Cho <cho-m@tuta.io>
2022-10-19 16:33:21 -0700

    bitchx: use `openssl@3`, add license, and add head
    Closes #113560.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bitchx.rb
+++ Formula/bitchx.rb
@@ -1,9 +1,20 @@
 class Bitchx < Formula
   desc "Text-based, scriptable IRC client"
   homepage "https://bitchx.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/bitchx/ircii-pana/bitchx-1.2.1/bitchx-1.2.1.tar.gz"
-  sha256 "2d270500dd42b5e2b191980d584f6587ca8a0dbda26b35ce7fadb519f53c83e2"
+  license "BSD-3-Clause"
   revision 1
+  head "https://git.code.sf.net/p/bitchx/git.git", branch: "master"
+
+  stable do
+    url "https://downloads.sourceforge.net/project/bitchx/ircii-pana/bitchx-1.2.1/bitchx-1.2.1.tar.gz"
+    sha256 "2d270500dd42b5e2b191980d584f6587ca8a0dbda26b35ce7fadb519f53c83e2"
+
+    # Apply these upstream commits to fix Linux build:
+    # https://sourceforge.net/p/bitchx/git/ci/1c6ff3088ad01a15bea50f78f1b2b468db7afae9/
+    # https://sourceforge.net/p/bitchx/git/ci/4f63d4892995eec6707f194b462c9fc3184ee85d/
+    # Remove with next release.
+    patch :DATA
+  end
 
   bottle do
     sha256 arm64_monterey: "894ac54efedb3043b3b5551bb74a8aaaa827e20c46a2a54ab4e1b4d9441148a0"
@@ -17,27 +28,33 @@ class Bitchx < Formula
     sha256 x86_64_linux:   "d32fff9d817493ee02584d2d511608edcfbc31d85ca1a078e70babf81b64a23d"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
 
-  # Apply this upstream commit to fix Linux build:
-  # https://sourceforge.net/p/bitchx/git/ci/1c6ff3088ad01a15bea50f78f1b2b468db7afae9/
-  # Remove with next release.
-  patch :DATA
-
   def install
-    # Patch to fix OpenSSL detection with OpenSSL 1.1
-    # A similar fix is already committed upstream:
-    # https://sourceforge.net/p/bitchx/git/ci/184af728c73c379d1eee57a387b6012572794fa8/
-    inreplace "configure", "SSLeay", "OpenSSL_version_num"
+    plugins = %w[
+      acro arcfour amp autocycle blowfish cavlink encrypt
+      fserv hint identd nap pkga possum qbx qmail
+    ]
+
+    # Remove following in next release
+    if build.stable?
+      # AIM plugin was removed upstream:
+      # https://sourceforge.net/p/bitchx/git/ci/35b1a65f03a2ca2dde31c9dbd77968587b6027d3/
+      plugins << "aim"
+
+      # Patch to fix OpenSSL detection with OpenSSL 1.1
+      # A similar fix is already committed upstream:
+      # https://sourceforge.net/p/bitchx/git/ci/184af728c73c379d1eee57a387b6012572794fa8/
+      inreplace "configure", "SSLeay", "OpenSSL_version_num"
+    end
 
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--enable-ipv6",
-                          "--with-plugins=acro,aim,arcfour,amp,autocycle,blowfish,cavlink,encrypt,fserv," \
-                          "hint,identd,nap,pkga,possum,qbx,qmail",
+                          "--with-plugins=#{plugins.join(",")}",
                           "--with-ssl"
     system "make"
     system "make", "install"
@@ -84,3 +101,39 @@ index d4c4d95..2b7d412 100644
  	} else {
  		bit_alloc_index=&t_allocMPG2;
  		nbal=&t_nbalMPG2;
+diff --git a/source/commands.c b/source/commands.c
+index d140d57..7fd81d6 100644
+--- a/source/commands.c
++++ b/source/commands.c
+@@ -118,7 +118,6 @@ extern	int	doing_notice;
+ 
+ static	void	oper_password_received (char *, char *);
+ 
+-int	no_hook_notify = 0;
+ int	load_depth = -1;
+ 
+ extern char	cx_function[];
+diff --git a/source/modules.c b/source/modules.c
+index 52817fc..99d39bb 100644
+--- a/source/modules.c
++++ b/source/modules.c
+@@ -77,7 +77,7 @@ extern int BX_read_sockets();
+ extern int identd;
+ extern int doing_notice;
+ 
+-int (*serv_open_func) (int, unsigned long, int);
++extern int (*serv_open_func) (int, unsigned long, int);
+ extern int (*serv_output_func) (int, int, char *, int);
+ extern int (*serv_input_func)  (int, char *, int, int, int);
+ extern int (*serv_close_func) (int, unsigned long, int);
+diff --git a/source/numbers.c b/source/numbers.c
+index 741fca9..2f80f22 100644
+--- a/source/numbers.c
++++ b/source/numbers.c
+@@ -66,7 +66,6 @@ void	show_server_map		(void);
+ int	stats_k_grep		(char **);
+ void	who_handlekill		(char *, char *, char *);
+ void	handle_tracekill	(int, char *, char *, char *);
+-int	no_hook_notify;
+ extern  AJoinList *ajoin_list;
+ void	remove_from_server_list (int);
