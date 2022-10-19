09a58b51a1e7324d76fc9c378b80b40c27a6295e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-19 08:11:39 +0000

    hebcal: update 5.2.1 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -7,12 +7,12 @@ class Hebcal < Formula
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ffe7d52bdd0161226a866115f12a3774c568c301d5d46e68d487aafc8905768"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1cd1be1ad0a671ea3386b3bcba7e9912aed8bddf095ef4d8c6ed3188769d172f"
-    sha256 cellar: :any_skip_relocation, monterey:       "329159ca71468d9e3f0762cf27739e07233d5b070558b4fb9c794a66ec8521a5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "59dda7b22660551875060c42f6f9006578ea87b9bb0c0eedef6ad13e57e45091"
-    sha256 cellar: :any_skip_relocation, catalina:       "8524a1e59878f8e49d32b8fa89049e5d3073f475f70a3271cb44161e16f6317b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a6810c26023bc92628a7ebe1c024949f1f7f60969c6def544c48ef65442699b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "90848ce5846d89a71503748718974f1634e13c6db55163fbcdc1ad3d092e4246"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "90848ce5846d89a71503748718974f1634e13c6db55163fbcdc1ad3d092e4246"
+    sha256 cellar: :any_skip_relocation, monterey:       "5bc0b94abefeb24f0140033c5c0d4d918d53dd60a5a91d7a468587d46e5b6a61"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5bc0b94abefeb24f0140033c5c0d4d918d53dd60a5a91d7a468587d46e5b6a61"
+    sha256 cellar: :any_skip_relocation, catalina:       "5bc0b94abefeb24f0140033c5c0d4d918d53dd60a5a91d7a468587d46e5b6a61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fca57c3a508d02dc07e20431ea62e234ff3569f7eabf1572349e75316ec9442d"
   end
 
   depends_on "go" => :build
