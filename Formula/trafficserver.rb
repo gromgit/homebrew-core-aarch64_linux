8f590087365bfed98c00a5b7334f65d2ee7ef791
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 23:25:32 +0800

    trafficserver: add 9.1.3 bottle.

--- Formula/trafficserver.rb
+++ Formula/trafficserver.rb
@@ -1,18 +1,14 @@
 class Trafficserver < Formula
   desc "HTTP/1.1 compliant caching proxy server"
   homepage "https://trafficserver.apache.org/"
-  url "https://downloads.apache.org/trafficserver/trafficserver-9.1.2.tar.bz2"
-  mirror "https://archive.apache.org/dist/trafficserver/trafficserver-9.1.2.tar.bz2"
-  sha256 "62f27d4e16a515e7ec85393186f909d934a79db41c7905f21d15a9eacb82232f"
+  url "https://downloads.apache.org/trafficserver/trafficserver-9.1.3.tar.bz2"
+  mirror "https://archive.apache.org/dist/trafficserver/trafficserver-9.1.3.tar.bz2"
+  sha256 "c8bf77b034b0da6c38d3ec9f3ca85ef1038aa046170e839308b0a59709d9afb6"
   license "Apache-2.0"
 
   bottle do
-    sha256 arm64_monterey: "69c21ff3257e64c403f265da0c4659842ab37d91abc9e238c889043990679369"
-    sha256 arm64_big_sur:  "8e4d17cdfffb1d59acd4d171dacfb5e888c057c342ade85b452bd2e4c06ca00b"
-    sha256 monterey:       "2052122c991d4cdf429cd6712708a69b56be7ecaf94c12f32e466e3c9fed9108"
-    sha256 big_sur:        "d49195bafda8a14f47a144ee7f6448a17389b8953e9ef5abb61866e0a0dda0dd"
-    sha256 catalina:       "c415c841e8920c3fe51143cf59710888d5e5e6853df2ea37ae0b29fd2e150382"
-    sha256 x86_64_linux:   "824e57e837fa58a49ae56f7226aed66b5c4d4c8a35929531179d02ddc4a4e329"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/trafficserver"
+    sha256 aarch64_linux: "7cadc0ea4cfb408e9d2955aa40d7c34cc4e5ec10398b5729322fe62497ad96d4"
   end
 
   head do
