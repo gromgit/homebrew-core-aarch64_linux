d09f4c499e3c753a341d57c3c0b602f7606f94f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-20 01:35:33 +0000

    swiftformat: update 0.50.3 bottle.

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -7,12 +7,12 @@ class Swiftformat < Formula
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8170d0f3644b4544ddd17f190d920ebc1579c66b7aa475992b12767e586d6da2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b86a06f31c5c18f622ca3e67abbbbec1f613886a0685ccc40a122390f0dfe629"
-    sha256 cellar: :any_skip_relocation, monterey:       "b63e2ad1d09fbdadd5c52b37392dd42e963c1ad449929fe5048daf5e15e142d8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4aaef13f6909ae1a61e52fc6b5ea06d4d67e2b3a455d155a496ca5f7de2e2ec3"
-    sha256 cellar: :any_skip_relocation, catalina:       "a92707b7d3d9590d76eb8f6b94b794c355afbb2eec1220a438ed08d2b65db1c9"
-    sha256                               x86_64_linux:   "6aee41887873254919085328bce646a1c78d307b59c73249c61462cb7e4358b7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f03398746b42c97a4057eb74c740a842b96b7fbb49b003057daf114d72db74b4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ff1b0f20ebaaa03a580817e8f4c670e54227c86c3c53d813899c276d03415461"
+    sha256 cellar: :any_skip_relocation, monterey:       "5dddca5459af2ad28f1a9ef1e664da40ed664ae907c0351dc92b3cd0af52c29e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7427e072a246141371df90135c5cea00c832bf2c511b62ef1cf49c1148b5bf23"
+    sha256 cellar: :any_skip_relocation, catalina:       "a3de195f76316795eb6a56e240265a8fd41c042c7d515ae1df254aa3f577b540"
+    sha256                               x86_64_linux:   "8557726207a8ce0871b950bf44ae9f2b8e95973ba026a77cf728d31cefc3ac00"
   end
 
   depends_on xcode: ["10.1", :build]
