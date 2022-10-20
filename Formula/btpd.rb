a61ea1b37ae6509ca259e8fbbe80eed16bc58a0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-20 06:09:51 +0000

    btpd: update 0.16_2 bottle.

--- Formula/btpd.rb
+++ Formula/btpd.rb
@@ -7,15 +7,13 @@ class Btpd < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b3755f2bf12645b508e65ca3fa6136879ad6e764f3e82fe2ed4c46285e3a7eb7"
-    sha256 cellar: :any,                 arm64_big_sur:  "899a1e60fbe73ab6ed57bfec2e6903b76a52e7831cb0280c6a34d689473def17"
-    sha256 cellar: :any,                 monterey:       "a43040f01bab4a4293988eea310f638c54d8cb5a82c41e8ce720c345cd9a64ac"
-    sha256 cellar: :any,                 big_sur:        "bdbb56a74d359d9feb9a6258ff4feff869561ffa208251be9f1a4ab3f18d3939"
-    sha256 cellar: :any,                 catalina:       "777f217d1d4cb87a8f4dae2bb1fdf3d62037561bd72f93fbb753674516870b0c"
-    sha256 cellar: :any,                 mojave:         "0b479e7b812055a0ebbbae40c63624258044d74cb11a2d698392792a5b543e4d"
-    sha256 cellar: :any,                 high_sierra:    "35042eba57182babbaff9f4a2eb1cbe891ebd82d2427a14926fd3617475da363"
-    sha256 cellar: :any,                 sierra:         "6951afbf4af1e9d0df95f5d9260ef04eeb7e558cd2d58c3a429a99ad93c2dddc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b2e4da63e65ec96199c8152d3e5e45f5c10c80d2f1e407c9cff9cd26a761ba9"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "78f27d75fcd843c49964eb16b4391a4f356aa608738ec4783baccba2636f5a0b"
+    sha256 cellar: :any,                 arm64_big_sur:  "0e4467a0d042844b00c6b5f896468dada066d0372e060d788733afada425b87b"
+    sha256 cellar: :any,                 monterey:       "423f7dc95d5fbb92a4e8cefdca992b20eb20e3a4548248281ed5d135a6a675c8"
+    sha256 cellar: :any,                 big_sur:        "0a69fd078eb310b051cc151295ec20619a5aa8309adde05fc281a66bf7652df5"
+    sha256 cellar: :any,                 catalina:       "e3aaa9dc2e6cafa9e9a2672fe9bad6da2871e1f496029ad2f12004c6c24f0895"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "483442aed198f237fd3d92bde41c2b12db3e150e44bab84bd2fab6d96a25d8c5"
   end
 
   depends_on "openssl@3"
