689032e2a09b50113a584696e08d52c7a56b9edb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-20 05:28:32 +0000

    hapi-fhir-cli 6.1.3
    Closes #113578.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hapi-fhir-cli.rb
+++ Formula/hapi-fhir-cli.rb
@@ -1,8 +1,8 @@
 class HapiFhirCli < Formula
   desc "Command-line interface for the HAPI FHIR library"
   homepage "https://hapifhir.io/"
-  url "https://github.com/hapifhir/hapi-fhir/releases/download/v6.1.2/hapi-fhir-6.1.2-cli.zip"
-  sha256 "2760a4d091fcead6b1092c5e146506d3870a404f0a0358ec7c0b716c8b649c50"
+  url "https://github.com/hapifhir/hapi-fhir/releases/download/v6.1.3/hapi-fhir-6.1.3-cli.zip"
+  sha256 "5ff3395870e8f71891a23e28f618cbff696081b206693130617afd25961ff54c"
   license "Apache-2.0"
 
   # The "latest" release on GitHub is sometimes for an older major/minor, so we
