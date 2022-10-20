c73dc1e035e76049e1904bdb3acab062accd37f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-20 18:16:35 +0000

    doctl: update 1.84.1 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -7,12 +7,12 @@ class Doctl < Formula
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "876a5436f35ee6cdae05df4c57ab702494f3df9c78ed7c43d1a5af1680601f4f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f1f4f1674b57d8611727db9eb0081eeb96c44a9ee34f1a3e3ef8afcbf5d4dc6"
-    sha256 cellar: :any_skip_relocation, monterey:       "5aaa21e18cad57b777f82fa714eaa771a9439db74285fdea61ee30c084f926b4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4b95351bd36b7bf352e05bef91a1d22ef3467d716b76a1e470ea99f1a310d8b4"
-    sha256 cellar: :any_skip_relocation, catalina:       "04afbdb48f53ba61a7b2825144c8e9e75955e38ad411df69b3c2d254f6a8147d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f8cce2a22a1af298f2430b26d89cc23ce4890b8c77741c46f35918b619dd4526"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "798bf889c0f0d3c19097fc35b93968381b0157b77b4c2f5f3c3c3ed91f3a9cbf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b21f24d17d08569e97b6394a3b368dc6fea309e72a11d5993583cf1b75e61510"
+    sha256 cellar: :any_skip_relocation, monterey:       "1afa9263f34e66e5e0e27695a5ba5b6ee698afd204ea1e585002c3a33e83ccba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2a13b53b2b64617e2151bbe48f33b926d0024c10319bef04da998b733472d91d"
+    sha256 cellar: :any_skip_relocation, catalina:       "87716e69be3b39f0903f3bbe52f0d278d7f8c245eead468bfc5f9c38a79525cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "928fa8e15039c3055011b9327a4fa394b8dfa88cea8b59608dbeee7dc8613d8c"
   end
 
   depends_on "go" => :build
