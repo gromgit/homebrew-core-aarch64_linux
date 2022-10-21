1fbc5d6a4ce52a694b626eff491cb3314ac6db26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-21 18:06:57 +0000

    rclone: update 1.60.0 bottle.

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -7,12 +7,12 @@ class Rclone < Formula
   head "https://github.com/rclone/rclone.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9142ff1ec8c6fb61a9d57e59a59732b9ceaf0ee79c0557cd5889fde615d5826e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7512219470980475dad384753b90b8878aa56c2709376ea4355ca8ad654d2a1"
-    sha256 cellar: :any_skip_relocation, monterey:       "5eeac976085cc98c8c0ec2455ba2cfd00c44e630dd39f8feab9bef6a7c16185f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9ac1a2bfbce19ed64dc1aee7e6487ad6fcd03ffc35dc89140f0c0fff027a7684"
-    sha256 cellar: :any_skip_relocation, catalina:       "3670207f4997bfdd0b0a11a791bc7e5708e56d3ef16e11d4697fdac4e058a8d7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "11f3c92190ae4c1cfa9602fc58a3442b4dae12819b5a9de787aeb51dfcec6fa2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee3e320cfa243d5571be800520d29be45e6b0f27a607717584f45ecb7520a5db"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8a97acb7f727ce780eeafca3e5821da67da5a05d7cb2c26ae576a43d45562245"
+    sha256 cellar: :any_skip_relocation, monterey:       "4731f8bbf4fc5a8036a67036563bd718014abaca19f59c77ff3dcb5696449397"
+    sha256 cellar: :any_skip_relocation, big_sur:        "795fd26dbce15ca81768c7d45e8f79b7ef560f1a8f8f21ea7f5b8aee81c6884f"
+    sha256 cellar: :any_skip_relocation, catalina:       "2a1926e28de34d656f81cc2fbc38ebe03992ce81f7ce7ee6b75669c0a7d854ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d326ef8f44922f6bd069fa0080629d9c9759814dd4ae52e30b401698cb8165ae"
   end
 
   depends_on "go" => :build
