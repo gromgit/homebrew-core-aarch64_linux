95c7ab92dc40574dad5da6d1e989058e66864774
Rui Chen <rui@chenrui.dev>
2022-10-21 09:41:43 -0400

    youtubeuploader 22.04
    Closes #113699.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/youtubeuploader.rb
+++ Formula/youtubeuploader.rb
@@ -1,8 +1,8 @@
 class Youtubeuploader < Formula
   desc "Scripted uploads to Youtube"
   homepage "https://github.com/porjo/youtubeuploader"
-  url "https://github.com/porjo/youtubeuploader/archive/22.03.tar.gz"
-  sha256 "cdf4857dd9a9ae15f47d46f35f4f7395e0fd759e128ea8bca057d3f08de78243"
+  url "https://github.com/porjo/youtubeuploader/archive/22.04.tar.gz"
+  sha256 "46c8089d95c0156d3d90310b8679eed8569862122e8d23b9ca5ce1229639dbd9"
   license "Apache-2.0"
   head "https://github.com/porjo/youtubeuploader.git", branch: "master"
 
