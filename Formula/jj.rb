0f1819a0778968f553211ccbda2cadc025b2a2c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-21 04:28:22 +0000

    jj: update 0.5.1 bottle.

--- Formula/jj.rb
+++ Formula/jj.rb
@@ -7,12 +7,13 @@ class Jj < Formula
   head "https://github.com/martinvonz/jj.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3bfda1803c30b4c35acd07f05ac3ae5581c3ba3d75820fbc0a9ee4aa7af1dfd1"
-    sha256 cellar: :any,                 arm64_big_sur:  "875511053de73f5b907ac89a6fc0b13ef909352a85044709efe7bfa8ae2d7317"
-    sha256 cellar: :any,                 monterey:       "1031326eba25ba0e583858fd31bea56691a0f42e554c087a34db050e1ce94bf8"
-    sha256 cellar: :any,                 big_sur:        "eb64d12c0287bb853b559bd5513d77bc69d26eb66c98786f973872c746384a97"
-    sha256 cellar: :any,                 catalina:       "86a85e719dab445556bb988267c82afca58c7fae595d7f40b3d1e11e4f328cbd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "094880212c087dda873fd4154b3c444655415a5c9db382b6c23e7026ca77e882"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "3f2be89fa77a5e3b95d111728f7ecf0e112d1c42a757e11ab2dfec3f67c9240d"
+    sha256 cellar: :any,                 arm64_big_sur:  "e84e321994ba5567a14bf20e0faafcffbf47a9c11ee80f84734915561944b414"
+    sha256 cellar: :any,                 monterey:       "19afd506a426d68190ccd3ced98804fbafcbbec02c293688717c32b5fdeceb77"
+    sha256 cellar: :any,                 big_sur:        "77d471140cacda3819826b17dec0a451c5328141b04f324cfba458b2f1067ccc"
+    sha256 cellar: :any,                 catalina:       "588a26d6a31f505f7e413924b34cfebcd679b4f3b44856b70046e36e3aaaf5db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "319452669e2ff05871e795c624f641a7bd31ff16123ac1abde6af43affc77d55"
   end
 
   depends_on "rust" => :build
