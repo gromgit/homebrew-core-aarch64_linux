9d29f848410c5f94f1993cf34982c15792d0e77c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-21 05:24:46 +0000

    armadillo 11.4.2
    Closes #113669.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/armadillo.rb
+++ Formula/armadillo.rb
@@ -1,8 +1,8 @@
 class Armadillo < Formula
   desc "C++ linear algebra library"
   homepage "https://arma.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/arma/armadillo-11.4.1.tar.xz"
-  sha256 "dadb5fd35bc4dc251b36f5dd80729c3c588d65e2ecbcd4e4e359ad9cb04523fb"
+  url "https://downloads.sourceforge.net/project/arma/armadillo-11.4.2.tar.xz"
+  sha256 "e6860134f1ac9656c6a1ccc74c74b75f8c5966ac8612841f2fbf0c91ce39f4e9"
   license "Apache-2.0"
 
   livecheck do
