554b9db5c03dda04252549b358fdc628729a40c6
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 22:13:27 +0800

    itstool: add 2.0.7 bottle.

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -7,26 +7,21 @@ class Itstool < Formula
   head "https://github.com/itstool/itstool.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
-    sha256 cellar: :any_skip_relocation, monterey:       "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, big_sur:        "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, catalina:       "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, mojave:         "acbdf687a02126c08c69514d248804c3833408331272fe8fb3751eb7d8dc0502"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "01fcbe25d1551ec84a14e3a880d9565057065d5024491f870626e5d89921565d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/itstool"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c3d96982a5d156192eb5c0b0aa651ccc163967e745a536a2b6a3f74f011d376b"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libxml2"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python#{xy}/site-packages"
 
     system "./autogen.sh", "--prefix=#{libexec}",
-                           "PYTHON=#{Formula["python@3.9"].opt_bin}/python3"
+                           "PYTHON=#{Formula["python@3.10"].opt_bin}/python3"
     system "make", "install"
 
     bin.install Dir["#{libexec}/bin/*"]
