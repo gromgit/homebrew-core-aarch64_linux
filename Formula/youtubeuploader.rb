7ddefcb9271a4c73791bde5b7497afe9427d8070
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-21 15:08:23 +0000

    youtubeuploader: update 22.04 bottle.

--- Formula/youtubeuploader.rb
+++ Formula/youtubeuploader.rb
@@ -7,12 +7,12 @@ class Youtubeuploader < Formula
   head "https://github.com/porjo/youtubeuploader.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "30f7e5ae721da535e957767a5d90effea1207f7bb33088afbc71916ccce644f1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a6c646d0777513146dbbd385475135e75d79c0db8ea65ab1b19efd83caa774f1"
-    sha256 cellar: :any_skip_relocation, monterey:       "b1b3411312848def235b2bcb7621947f50954f95d126f32a7b61d2dcbceacc4a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c8e7b4db1c565158b547a534fe783bbe4b520b68cab78f030d9c51cb94bba794"
-    sha256 cellar: :any_skip_relocation, catalina:       "9ec6d96d62ba3740c636d633b1caee6a7d7717c7a60e0642e1351991cdbed98a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c7ffcc51bebef2de4747bd1a328bfb9bb0b05f40f36da7af712fb22af8916f7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d05b0bde055295fac779352ad3239407ea77104728d051c810f33bee13794bdc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "406a27d6c0a58b2837c841372dc8ec6645a384e6d2ce734cb668dfec1ad47685"
+    sha256 cellar: :any_skip_relocation, monterey:       "b02ffa02fb0b1042ecd656a22d16427fb6d22d5603905d9e7df5b06b8dfe746a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ea7f5b67f09eab0a4d9ff32fc6e64a9987e0a9e28479693189d53e3a290830a2"
+    sha256 cellar: :any_skip_relocation, catalina:       "33a1cd3cbf234e3586b9cfd6c4ee5c39137ce6c565209e4f181854a8e37bc672"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2e7357282936af1ee6a447cc8e94a20e4723403f133fd70e3094d008e7208624"
   end
 
   depends_on "go" => :build
