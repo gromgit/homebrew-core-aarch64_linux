d90a1ced5db2454badfba123b99e7a5a25c51feb
FX Coudert <fxcoudert@gmail.com>
2022-10-21 21:25:02 +0000

    p11-kit: update 0.24.1_1 bottle.

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -7,6 +7,7 @@ class P11Kit < Formula
   revision 1
 
   bottle do
+    sha256 arm64_ventura:  "390bf08fc2c0a63b200b5cceb8e12d485118ff651936b84a8fe3aed10b06ce56"
     sha256 arm64_monterey: "092795b583a9f4e529eca159e4fbadfb4c92b4af1b62174e0e7882f8a7961908"
     sha256 arm64_big_sur:  "df412a2d8b78365ae59b70e9e0271c0c62d6cb8015d973194e9b8585b3cb577a"
     sha256 monterey:       "ce3a6723491c5cee1d79e938b377555b67738b7e4a4615bbc4189624415b15dd"
