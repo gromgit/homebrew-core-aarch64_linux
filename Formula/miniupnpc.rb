6418553954646cfc7aa44a1a55e2921ff32adfc8
Porkepix <porkepix@gmail.com>
2022-10-22 07:07:00 +0200

    miniupnpc 2.2.4
    * miniupnpc 2.2.4
    * miniupnpc: remove patch
    * miniupnpc: use HTTPS for URL
    
    Closes #113751.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -1,8 +1,8 @@
 class Miniupnpc < Formula
   desc "UPnP IGD client library and daemon"
   homepage "https://miniupnp.tuxfamily.org"
-  url "http://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-2.2.3.tar.gz"
-  sha256 "dce41b4a4f08521c53a0ab163ad2007d18b5e1aa173ea5803bd47a1be3159c24"
+  url "https://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-2.2.4.tar.gz"
+  sha256 "481a5e4aede64e9ef29895b218836c3608d973e77a35b4f228ab1f3629412c4b"
   license "BSD-3-Clause"
 
   # We only match versions with only a major/minor since versions like 2.1 are
@@ -23,11 +23,6 @@ class Miniupnpc < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b4567463c162018e8c13e28a3335cfc657108cd0a6c0446f4cb28c9c54b53d1"
   end
 
-  # Fix missing references to $(BUILD) in the install rules
-  # equivalent to https://github.com/miniupnp/miniupnp/commit/ed1dc4bb5cdc4a53963f3eb01089289e30acc5a3
-  # but modified to start with the miniupnpc folder as root
-  patch :DATA
-
   def install
     system "make", "INSTALLPREFIX=#{prefix}", "install"
   end
@@ -37,38 +32,3 @@ class Miniupnpc < Formula
     assert_match version.to_s, output
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 4563b283..11a17f95 100644
---- a/Makefile
-+++ b/Makefile
-@@ -162,7 +162,7 @@ PKGCONFIGDIR = $(INSTALLDIRLIB)/pkgconfig
- 
- FILESTOINSTALL = $(LIBRARY) $(EXECUTABLES)
- ifeq (, $(findstring amiga, $(OS)))
--FILESTOINSTALL += $(SHAREDLIBRARY) miniupnpc.pc
-+FILESTOINSTALL += $(SHAREDLIBRARY) $(BUILD)/miniupnpc.pc
- endif
- 
- 
-@@ -251,15 +251,15 @@ install:	updateversion $(FILESTOINSTALL)
- 	$(INSTALL) -m 644 $(LIBRARY) $(DESTDIR)$(INSTALLDIRLIB)
- ifeq (, $(findstring amiga, $(OS)))
- 	$(INSTALL) -m 644 $(SHAREDLIBRARY) $(DESTDIR)$(INSTALLDIRLIB)/$(SONAME)
--	ln -fs $(SONAME) $(DESTDIR)$(INSTALLDIRLIB)/$(SHAREDLIBRARY)
-+	ln -fs $(SONAME) $(DESTDIR)$(INSTALLDIRLIB)/$(notdir $(SHAREDLIBRARY))
- 	$(INSTALL) -d $(DESTDIR)$(PKGCONFIGDIR)
--	$(INSTALL) -m 644 miniupnpc.pc $(DESTDIR)$(PKGCONFIGDIR)
-+	$(INSTALL) -m 644 $(BUILD)/miniupnpc.pc $(DESTDIR)$(PKGCONFIGDIR)
- endif
- 	$(INSTALL) -d $(DESTDIR)$(INSTALLDIRBIN)
- ifneq (, $(findstring amiga, $(OS)))
--	$(INSTALL) -m 755 upnpc-static $(DESTDIR)$(INSTALLDIRBIN)/upnpc
-+	$(INSTALL) -m 755 $(BUILD)/upnpc-static $(DESTDIR)$(INSTALLDIRBIN)/upnpc
- else
--	$(INSTALL) -m 755 upnpc-shared $(DESTDIR)$(INSTALLDIRBIN)/upnpc
-+	$(INSTALL) -m 755 $(BUILD)/upnpc-shared $(DESTDIR)$(INSTALLDIRBIN)/upnpc
- endif
- 	$(INSTALL) -m 755 external-ip.sh $(DESTDIR)$(INSTALLDIRBIN)/external-ip
- ifeq (, $(findstring amiga, $(OS)))
