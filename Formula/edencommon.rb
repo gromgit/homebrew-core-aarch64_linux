a75811bc825f0a04eb0cc44939f5a14399071e0a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-22 11:21:07 +0000

    edencommon: update 2022.10.17.00 bottle.

--- Formula/edencommon.rb
+++ Formula/edencommon.rb
@@ -7,6 +7,7 @@ class Edencommon < Formula
   head "https://github.com/facebookexperimental/edencommon.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "4b20161c3b5cdb7d4e8d1bdbbec3268c6bcb1e4e1999eb76b1db9ae441f3f193"
     sha256 cellar: :any,                 arm64_monterey: "6d4188a96b91fcec232557dd47b5ffd98d669acd0e8cf6be61e95c19460dfbfc"
     sha256 cellar: :any,                 arm64_big_sur:  "73445846a55956f8d92defb6988b1b21db1b4e24cfa06cc24e73e715e62a0be5"
     sha256 cellar: :any,                 monterey:       "9fe9b2537892bde2a621d8adc2dc4ca9e418a8d4e73f853020cf68e2bfc6d421"
