e5d108d5b8d4044cdbb89bea17a7ace33e6eda48
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-22 03:08:14 +0000

    libarchive: update 3.6.1 bottle.

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -11,6 +11,7 @@ class Libarchive < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "e998ea917d0ca7ddd938af2a0e0270e4a28240170e66dd07d32e8276bf3bd3a2"
     sha256 cellar: :any,                 arm64_monterey: "0cf4a68e9bd6d24a4579bdf08eb33348151464d525e279f569f0b51e60ccc2cd"
     sha256 cellar: :any,                 arm64_big_sur:  "305f6be2e4fe73076a065ac7ebf82158d13e702ef8cfa8be1494a9f29ed1dd19"
     sha256 cellar: :any,                 monterey:       "652e949db4255c0dca6feee1a8cc96361a66458f4439c58a7dfd0c8215840b80"
