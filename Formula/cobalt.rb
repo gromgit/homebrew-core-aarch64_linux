3867b67adba22619dac753c3e45fdb436e4f30ca
Rui Chen <rui@chenrui.dev>
2022-10-22 20:35:19 +0000

    cobalt: update 0.18.3 bottle.

--- Formula/cobalt.rb
+++ Formula/cobalt.rb
@@ -6,6 +6,7 @@ class Cobalt < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d6cf680940eef82bc4d78213506f1850cf78920e9c01488f9d8eaba8a04dbb6a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "87da04debee4a55ff30693b1ebbeb403443a87cda42d359e9ccd62ae53143299"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a9dfd028369762a67366ad1fa19beef56e89eaab849e3e3cb47fca0f2746561"
     sha256 cellar: :any_skip_relocation, monterey:       "be719e69f1d66c3e979d3a3136b5d785e462b01bd6ec3027732a7af1bb8f1773"
