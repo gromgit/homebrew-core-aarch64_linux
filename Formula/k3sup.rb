56f3d2c5e83e0283dbe78f34166a56505c9e42c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-22 16:30:08 +0000

    k3sup: update 0.12.10 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -13,12 +13,12 @@ class K3sup < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "baacc1dc3ed2a20359828e852f24441f330dcd851d778d4f111f412c4beaec3d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d35e8db5be9ac8b7fdc2b8990ca9e38284de3227f40cf96e1fc21883b67effe8"
-    sha256 cellar: :any_skip_relocation, monterey:       "1cdcf6b302b500ec1f0194ac5e1d169fc4dc087fd636e4a518065749670206ea"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e7ad9898126a08f967f05273b4b0b731963542c0912ab610f2e3fd8989d5bfeb"
-    sha256 cellar: :any_skip_relocation, catalina:       "c8fc506b9e8cba697408716bab8ac6db412557c8db527cf38ff67ede93cd9d3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c3b52391cdd056fdcd68645736b4229858ca4fa056fec10d17938106b871673f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7976530c27ea3cafc1298946808444ac0f11356eda94964c8843cc008838ec1e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "458a0d1b7644fe0bcdd163287ea78de174b2ccf40a7bb3428a8ed1705b20be4f"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ceae0674f5361d7414923f69f479456490454ef8c821566422d90d06825dc23"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0c35c93623857b785b71c5562e2c91fbb77024505846862dc5b820cc495d8754"
+    sha256 cellar: :any_skip_relocation, catalina:       "56ddd0285b5ade55f932d950edc1f817c888cac433252fb1a47630cea236e308"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ca681c1d611fab961d674be0fe1217a973a583e184e1355ef67055e3ec0b3d6"
   end
 
   depends_on "go" => :build
