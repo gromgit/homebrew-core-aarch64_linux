7ad2f82c7c386572289a964cc6cbcfa91ea18d40
Bo Anderson <mail@boanderson.me>
2022-10-22 14:38:08 +0000

    speex: update 1.2.1 bottle.

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -12,6 +12,7 @@ class Speex < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "e8e8cbefa65f7819b2feb27b9067248d97f2e5607253c0a5c8a49a495d7fc824"
     sha256 cellar: :any,                 arm64_monterey: "b0cba69db1b66944a019f312fa128d6c6460f971fdd5cfddc0725051b76a4dd0"
     sha256 cellar: :any,                 arm64_big_sur:  "3cb6ffa6920e1ea4e904bb0e2a8d6e62c329c39c6f7d80d8c66f691b5ad1f427"
     sha256 cellar: :any,                 monterey:       "46d02ec9d80e46fbf260fe650abaa3f4620743ca34a59d53d55d382894231a41"
