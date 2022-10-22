8ccebeb56aa82011eeca2b3028d21c47ff00f547
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-22 16:30:24 +0000

    okteto: update 2.8.2 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -7,12 +7,12 @@ class Okteto < Formula
   head "https://github.com/okteto/okteto.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "baa222e800519f55f53ca65fc43262c8bf389ffe51b954464eefd6b97b12bd8a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "65a5e3329f351f72e664c6fd6f94b636f8accaaa60e94b0e5302244b284478a6"
-    sha256 cellar: :any_skip_relocation, monterey:       "e13b9a0c0f05cc39291b5e2ef6a773f3139941b33108d8aa76b276990f6a1eee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "464a59df9f5b6863aae07b937b7c2ad369ceaf21c4013d0199ccffb16b06f187"
-    sha256 cellar: :any_skip_relocation, catalina:       "d33bfe27594dd9911d55af04ac6f5b085a2a2457c6b2b213441faa6ef8518e01"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "183d39264b354c55a4f900ce1db18d8f42e1de56aae5237f559ea1381bad119c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c4ea781602b83ed50304b0afc87e2b111c13b7c21eddc1412ad1a9afc4836a82"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59a1ad7dadcbc715bb8195c2007e3530ae13637050f8cf6a95665897b09c9754"
+    sha256 cellar: :any_skip_relocation, monterey:       "4c081328ba23edec456b26accf1d532df4a7322bc702ecb3868a6cab1550bf5e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "691459f4fa9fd2cff9865bb832be9bcc3283020f521bff2ef7f9e5b89944c56c"
+    sha256 cellar: :any_skip_relocation, catalina:       "edacf158cddd9d02c0ab46ffa94c605dd6ad190fc2f87160bfe496c83c2597b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76815960ebe063e2085ab7d988565d34f27696b61c23ff7dd1b1d0123fd6891c"
   end
 
   depends_on "go" => :build
