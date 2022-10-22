7233edd41de5f38711f2fc03724ec964f6015408
Porkepix <porkepix@gmail.com>
2022-10-22 07:08:29 +0200

    luau 0.550
    Closes #113753.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -1,8 +1,8 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau-lang.org"
-  url "https://github.com/Roblox/luau/archive/0.549.tar.gz"
-  sha256 "786581ebe7cbbcb37cb4ed9c8817d6689cd82e910b945f2ce17fe6df2888e2d4"
+  url "https://github.com/Roblox/luau/archive/0.550.tar.gz"
+  sha256 "6aa306edec3c081bbcf9b39d99ab548b2cd82712965e5bc9484bcb0d77083e79"
   license "MIT"
   head "https://github.com/Roblox/luau.git", branch: "master"
 
