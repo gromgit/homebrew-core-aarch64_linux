d03e6e93a39add91be02e41cdcf9006a5f14768b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-22 17:11:03 +0000

    plantuml: update 1.2022.10 bottle.

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -12,7 +12,7 @@ class Plantuml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "125f4eb600ef42568d9ffcdf21ee66daaedb07011384e1a35cbc74729b714f94"
+    sha256 cellar: :any_skip_relocation, all: "00163273f4e6b4a1e5391334d5fff5e95e5119a033d78f5849aff8e12839732f"
   end
 
   depends_on "graphviz"
