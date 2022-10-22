3a3b239c939b054c670e0edffb3bc40cad112388
Rui Chen <rui@chenrui.dev>
2022-10-22 19:07:02 -0400

    mprocs 0.6.4
    Closes #113805.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mprocs.rb
+++ Formula/mprocs.rb
@@ -1,8 +1,8 @@
 class Mprocs < Formula
   desc "Run multiple commands in parallel"
   homepage "https://github.com/pvolok/mprocs"
-  url "https://github.com/pvolok/mprocs/archive/refs/tags/v0.6.3.tar.gz"
-  sha256 "da00ed6c693b9282b2553f69cee05eed039b1b3e948620c20b5d8ca6b2542aba"
+  url "https://github.com/pvolok/mprocs/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "7fdb5f3f8baaa82ccb7bf2193a7b843027cb133824a99475226524c105255077"
   license "MIT"
   head "https://github.com/pvolok/mprocs.git", branch: "master"
 
