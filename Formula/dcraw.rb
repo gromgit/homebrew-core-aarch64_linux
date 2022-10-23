9a3103c3acc746d1df1a9acef30a57e38652ae5a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-23 14:00:58 +0000

    dcraw: update 9.28.0_2 bottle.

--- Formula/dcraw.rb
+++ Formula/dcraw.rb
@@ -12,6 +12,7 @@ class Dcraw < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "67ed6c9b0e400aad488b5449b439c3d2c2f1367ec855d95c2cedf1eddf0ab9fa"
     sha256 cellar: :any,                 arm64_monterey: "3d77794768e6ae2d0ff3ffbacc3ed745017f7ffc2503da954100b2d1ac146db7"
     sha256 cellar: :any,                 arm64_big_sur:  "899ea09ca46695dcbae2414fd72f3af86fc8676e0e51aea2b54baa28a4a5845d"
     sha256 cellar: :any,                 monterey:       "72c6183da24a08d3cc0d887ac294a51c14d5b39c8b2a42820cc4a4351768b9b1"
