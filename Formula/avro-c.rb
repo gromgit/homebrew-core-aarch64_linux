2f6d4d211b5f37bc938a6f28b3672aedd4ce2f2c
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-23 12:34:18 +0000

    avro-c: update 1.11.0 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -7,6 +7,7 @@ class AvroC < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "e866c69e2e7cf6ed942aef633b01b38bbfd1fce8c9f2e6ab563f386c82652e5e"
     sha256 cellar: :any,                 arm64_monterey: "003f19bcd51baea53f86d97488f83552dc8477eedef78a371bdc166ac0d183ab"
     sha256 cellar: :any,                 arm64_big_sur:  "f30243ab877db4e81ff07c5dea0e266228ac04bf178144d3f9caaab45a876f86"
     sha256 cellar: :any,                 monterey:       "35a23d1c97d7e5b50adf7ff2f8ff3ddcb6f9271c85fc033306e8447484e23b5c"
