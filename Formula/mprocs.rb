4478d568fee7380cd770afa96f43c443021e7d31
FX Coudert <fxcoudert@gmail.com>
2022-10-23 17:05:15 +0000

    mprocs: update 0.6.4 bottle.

--- Formula/mprocs.rb
+++ Formula/mprocs.rb
@@ -7,6 +7,7 @@ class Mprocs < Formula
   head "https://github.com/pvolok/mprocs.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0eb52d211cb3e815fde72ac095e47d6ed6d9c0c4144521a2a8186d60a978b45e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "809917011f4d7b325f47e690f18ad5ebc929e69057e8ff9bf8e4bd5c3d13d2d7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d96fa9177201787519d810a8cef0dfa489c40991ef93f472f7437ba3400c8081"
     sha256 cellar: :any_skip_relocation, monterey:       "1189819af1ec7c13b6c23a66d0964a123a2f46c3adeb82b5ae4a6784c344cace"
