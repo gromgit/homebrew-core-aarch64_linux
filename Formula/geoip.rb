35618b8a3e086974488e3dbc312a8ad008e44d5f
FX Coudert <fxcoudert@gmail.com>
2022-10-23 03:38:27 +0000

    geoip: update 1.6.12 bottle.

--- Formula/geoip.rb
+++ Formula/geoip.rb
@@ -8,6 +8,7 @@ class Geoip < Formula
 
   bottle do
     rebuild 2
+    sha256 cellar: :any,                 arm64_ventura:  "40ef31c663a905f3786b8e410f651ee97cc0ae805d7c22d52c20b9c518ba12ba"
     sha256 cellar: :any,                 arm64_monterey: "c2ae63b815cc930a90d57c78a1d7a51a57085d67aa20431db716982353a950b2"
     sha256 cellar: :any,                 arm64_big_sur:  "fd8787e3b88f91d97504992874d3fd358c6865e03fc19c5123b8e7f1a691f0dd"
     sha256 cellar: :any,                 monterey:       "f441710caf103a53272a50e4720f1885f858986c6306fcc4a078ef8d193cca59"
