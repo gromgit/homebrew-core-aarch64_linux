935f3cd414d05923a95b7dbf95275a655d870f24
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-23 07:40:33 +0000

    libmaxminddb: update 1.7.1 bottle.

--- Formula/libmaxminddb.rb
+++ Formula/libmaxminddb.rb
@@ -6,6 +6,7 @@ class Libmaxminddb < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "7a8bdefbc6e7b19c6704286c71a3f616c98e2d7cb00c0e704083c58216020c6f"
     sha256 cellar: :any,                 arm64_monterey: "f43032f9d2a53d6ab4969fdf90ab9db14ac2d2ea10c4e8b594060ad1d7f01b95"
     sha256 cellar: :any,                 arm64_big_sur:  "d7fdc2b82f0c5af1ec537354b0ef8404edd1d067db901463cfa75a1731fa5839"
     sha256 cellar: :any,                 monterey:       "ddda0e691d8e86dded50880d64ebf8ddec4a6d4cd55b1be7f64ac0e3e6fe3c27"
