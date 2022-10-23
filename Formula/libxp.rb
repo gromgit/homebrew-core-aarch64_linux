33c126f129e8a51f9a1362da80d35f34764f0302
FX Coudert <fxcoudert@gmail.com>
2022-10-23 00:50:50 +0000

    libxp: update 1.0.4 bottle.

--- Formula/libxp.rb
+++ Formula/libxp.rb
@@ -11,6 +11,7 @@ class Libxp < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "f92106b34661b7a8d39636a544ee208e724b2ac68395ca4a9b2ef264359190f9"
     sha256 cellar: :any,                 arm64_monterey: "c2e8285bdd8edb318e57e2b9d47e692d283cc05ac7ba811468ce946a9070fa1f"
     sha256 cellar: :any,                 arm64_big_sur:  "e44f5fc9fafc1b6c40c6051921f6de14d2a0d6a01c5fd9715341bb77e5ccd144"
     sha256 cellar: :any,                 monterey:       "afa942a7ef9f5244bcfd7ce8e61b8235e3085f41bdc521bb3c930eb9402ff8bb"
