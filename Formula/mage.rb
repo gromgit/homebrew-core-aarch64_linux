825abe9ac7eec8658aef91ac3f4d761d868c020a
FX Coudert <fxcoudert@gmail.com>
2022-10-23 03:18:06 +0000

    mage: update 1.14.0 bottle.

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -8,6 +8,7 @@ class Mage < Formula
   head "https://github.com/magefile/mage.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "08dd84f470df1010345ea9ec2ddede00589363ddb7a607db1541c75cbf98d61a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "139570603e519b01b9591ac7e71b46ee310fb34a8430fae248938232b70d747e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "139570603e519b01b9591ac7e71b46ee310fb34a8430fae248938232b70d747e"
     sha256 cellar: :any_skip_relocation, monterey:       "c96d1ce7372a0cc6df4904089c12e7850013bb2f704da4d01ae3f35dc61b03e2"
