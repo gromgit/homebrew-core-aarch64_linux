dc253da54181979c95e78a568f70009916618e78
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-23 06:14:36 +0000

    tldr: update 1.5.0 bottle.

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -7,6 +7,7 @@ class Tldr < Formula
   head "https://github.com/tldr-pages/tldr-c-client.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "a522c4962d9b00b2ae0d9500ed1fb4cfccdaba0592b8211159aa874b146a01b9"
     sha256 cellar: :any,                 arm64_monterey: "3f969a50b717145b7711c699aeff9290e98c43833a9e9d01119fa5c22c87b861"
     sha256 cellar: :any,                 arm64_big_sur:  "c0fa874b41e4f1a9e6e597bd8cb462f4f0180aae9844f0e9154e778c1e030ee9"
     sha256 cellar: :any,                 monterey:       "79b6fb23aadf46144c104d3c53ea8cae750f8475bd4ff09be6f498fda0f83016"
