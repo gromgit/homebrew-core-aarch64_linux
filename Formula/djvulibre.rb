41f0e1b9ea538be0b083e93a217299a4a7d50c52
FX Coudert <fxcoudert@gmail.com>
2022-10-24 00:49:48 +0000

    djvulibre: update 3.5.28_1 bottle.

--- Formula/djvulibre.rb
+++ Formula/djvulibre.rb
@@ -11,6 +11,7 @@ class Djvulibre < Formula
   end
 
   bottle do
+    sha256 arm64_ventura:  "256feac4187ce2c62a4a2a5b64cc6fbb6160c970b652b690fc1ed775efcf5adf"
     sha256 arm64_monterey: "c6c7e2fecb0958688027ef38a23b6e0a4e2df6de8a28a03a472ffa1fbc14de01"
     sha256 arm64_big_sur:  "c5d40805bec199c8c359f392b54ad3e1128f7debd0e6af393632e70721aa2a34"
     sha256 monterey:       "113d7cb22d9b8b7ee7dbe524769068e12fcb6c5849211975b28b88a09644a366"
