989de12ab597dd1e1fa90b386dd52db736a04630
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-24 09:16:12 +0000

    docker-credential-helper-ecr: update 0.6.0 bottle.

--- Formula/docker-credential-helper-ecr.rb
+++ Formula/docker-credential-helper-ecr.rb
@@ -13,6 +13,7 @@ class DockerCredentialHelperEcr < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "306aa5e8086723bdf6cbaa45be2c74291c9e9df620cb7f451fb485a65123022a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e0d768a76a10449f70e88d0b25dd11dc9b8f8149f6b3c153fa93a018d2375fe8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dfd6561543c85f32a1eae09b00e0b0ea8ccdeada1073227cc87fed3916b40bd5"
     sha256 cellar: :any_skip_relocation, monterey:       "e41c402435c1e0a079461dce61a5ad1a695ab3af689bae754a3e7ad9328583e7"
