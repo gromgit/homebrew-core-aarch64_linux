21a28bea0ce9e7233a1de0c846e94f566130a331
FX Coudert <fxcoudert@gmail.com>
2022-10-24 02:45:57 +0000

    nlopt: update 2.7.1 bottle.

--- Formula/nlopt.rb
+++ Formula/nlopt.rb
@@ -7,6 +7,7 @@ class Nlopt < Formula
   head "https://github.com/stevengj/nlopt.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "50c557edfb63b6bcc13096c6f45d4edf44d6a3858251f607a207bacb42fd27dd"
     sha256 cellar: :any,                 arm64_monterey: "1b9da35eee41e6edae359ce403cf555e7a8b2335ebe78b940c61bbe9516a3c17"
     sha256 cellar: :any,                 arm64_big_sur:  "4f42df05985991ae1a5c41c090936fde6a52cef297667a268ff3d6f6c90622e9"
     sha256 cellar: :any,                 monterey:       "b4fdf154903fc00284e3a37e58bb699ac75a067c355a9ee7efb80c1722b1c522"
