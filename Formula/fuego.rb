47017a92ba05ddcbc0089991c2335b7d9abac65a
FX Coudert <fxcoudert@gmail.com>
2022-10-24 20:04:28 +0000

    fuego: update 1.1.SVN_5 bottle.

--- Formula/fuego.rb
+++ Formula/fuego.rb
@@ -7,6 +7,7 @@ class Fuego < Formula
   head "https://svn.code.sf.net/p/fuego/code/trunk"
 
   bottle do
+    sha256 arm64_ventura:  "b312acb9bb04fc31cc2e630ca130e9a7f78d8a4e3f631a83d4a585be80c438be"
     sha256 arm64_monterey: "f6301e86813d55ba476153d0ada58d1cb40791527f59b5cb8c3602426f666a89"
     sha256 arm64_big_sur:  "65dfab9714a6866134dbcd17de0db06148eec4164b00e5dd259ff0fd1732c29d"
     sha256 monterey:       "73d6548393b805dfc42ece561040c1549682cc862a671aa090b32b5f2b2d210e"
