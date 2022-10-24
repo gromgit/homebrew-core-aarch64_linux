24b46df5e1f21451399dd8b583e07349a67ec4a3
FX Coudert <fxcoudert@gmail.com>
2022-10-24 15:05:28 +0000

    bsdmake: update 24 bottle.

--- Formula/bsdmake.rb
+++ Formula/bsdmake.rb
@@ -12,6 +12,7 @@ class Bsdmake < Formula
 
   bottle do
     rebuild 2
+    sha256 arm64_ventura:  "8426abe75969c8adb575f6276d55e8a4737d1f139cf534294f74a843e74a632b"
     sha256 arm64_monterey: "d01faf8a67751cf8248d36ef46fa23f8f6031c04fd723eb1cbf40ee881d6bc09"
     sha256 arm64_big_sur:  "cfca87086e9932c2a1beb031d5fd34018a5afbe84a051918b41b33e4e86c82ea"
     sha256 monterey:       "303f1fce21a307e0ecb01214f64ba7c3f26c21aeafb44d803120d26500dd387a"
