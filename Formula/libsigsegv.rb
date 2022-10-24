502d40f2f1080956320990f6e4cc9ea4dbbf40a3
FX Coudert <fxcoudert@gmail.com>
2022-10-24 04:26:53 +0000

    libsigsegv: update 2.14 bottle.

--- Formula/libsigsegv.rb
+++ Formula/libsigsegv.rb
@@ -7,6 +7,7 @@ class Libsigsegv < Formula
   license "GPL-2.0-or-later"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "22818cb45d09dd84f4fc68ecf1eee1484cb9a98b75fa72b89913206cf46bf19c"
     sha256 cellar: :any,                 arm64_monterey: "e8cae8734eafabb8c3bcfeba2449b1d6e309cea6ca0647ca7bdf62aca7e331db"
     sha256 cellar: :any,                 arm64_big_sur:  "f37ac4bf1b939f0703029b5143fca0dc8c77ff77f2b860800a5e0028e5fdfea8"
     sha256 cellar: :any,                 monterey:       "3ceaebb4bf32ec972aa786360dd55fc115fb2890d16da6b2ddaa9ff199160e2c"
