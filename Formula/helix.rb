9778bfa404b972be348a9f16cb9e257866147ef4
FX Coudert <fxcoudert@gmail.com>
2022-10-24 18:19:52 +0000

    helix: update 22.08.1 bottle.

--- Formula/helix.rb
+++ Formula/helix.rb
@@ -8,6 +8,7 @@ class Helix < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "f7f81ff6f6479a7d302d6d995e44c70a3c5553e6e096d9f95c0b119991511bd9"
     sha256 cellar: :any,                 arm64_monterey: "fbc0e33ba2b0639778b11efa751643c898d232d2021f363e1155c8fd30b48e2c"
     sha256 cellar: :any,                 arm64_big_sur:  "10fc0acae5c742ba3d3f95216f246a84160a072c9a300b8604ef32232047a606"
     sha256 cellar: :any,                 monterey:       "0ffad064e6fe4c171025c18bab8d859244afca4fc3d6f338460111c9787fea2d"
