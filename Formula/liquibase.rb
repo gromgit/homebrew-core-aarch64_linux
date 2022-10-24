953a13946a35b26ef7fe1a7647811b271919047d
FX Coudert <fxcoudert@gmail.com>
2022-10-24 22:59:04 +0000

    liquibase: update 4.17.1 bottle.

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -11,6 +11,7 @@ class Liquibase < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "caefb17af8afeed82b6c8e759a27a39a389571fd62e9916548509a1f7d4c3cdb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "caefb17af8afeed82b6c8e759a27a39a389571fd62e9916548509a1f7d4c3cdb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "caefb17af8afeed82b6c8e759a27a39a389571fd62e9916548509a1f7d4c3cdb"
     sha256 cellar: :any_skip_relocation, monterey:       "b31abd175313658f69e1a5f6fc8b530d33ac06b19f53f138b591acffcbd46d34"
