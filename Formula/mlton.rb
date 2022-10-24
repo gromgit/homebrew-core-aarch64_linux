a61d9879c7bc1864a7cb901095436b61b052b715
FX Coudert <fxcoudert@gmail.com>
2022-10-24 06:14:07 +0000

    mlton: update 20210117 bottle.

--- Formula/mlton.rb
+++ Formula/mlton.rb
@@ -15,6 +15,7 @@ class Mlton < Formula
 
   bottle do
     rebuild 2
+    sha256 cellar: :any,                 arm64_ventura:  "47153e8fe8add04e420f69b4d5b1062ac2fa9abc1106cda2e5d0d0893ed0ca79"
     sha256 cellar: :any,                 arm64_monterey: "b63990802ceb1eab45673ca135e32aa1329a051fdd2ac3ca28c703d691e2f854"
     sha256 cellar: :any,                 arm64_big_sur:  "13f277d7115052ab34efd1cbea436bb9dec5227a09cc1f1e7c07a9f0670f7405"
     sha256 cellar: :any,                 monterey:       "67242137af80b4ecae138c139ee1e169d8ee04a1928ae0e40cbd339c2846d349"
