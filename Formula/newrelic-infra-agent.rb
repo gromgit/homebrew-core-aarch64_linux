548029b7a07ed5ffed608be708b3a95e7b91ab06
Roger Coll <rogercoll@protonmail.com>
2022-10-24 11:20:13 +0200

    newrelic-infra-agent 1.33.0
    Closes #113871.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.32.0",
-      revision: "98a27d39e9a3282ffed2f9160ea651773814d7b9"
+      tag:      "1.33.0",
+      revision: "957e19b78c065a37d42667a06c8ad4508aff86c4"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
