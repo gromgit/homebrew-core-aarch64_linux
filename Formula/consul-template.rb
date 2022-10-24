24ae7134cb178d5d7df42268ed1ad74fb62b8e7a
FX Coudert <fxcoudert@gmail.com>
2022-10-24 22:00:03 +0000

    consul-template: update 0.29.5 bottle.

--- Formula/consul-template.rb
+++ Formula/consul-template.rb
@@ -8,6 +8,7 @@ class ConsulTemplate < Formula
   head "https://github.com/hashicorp/consul-template.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a99c9760c62558d9bd1f6948fecf262251d430061df4abe7f2467f3814d22da"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6fed41eebd6d023e294c27c465112584b74f86c6f00c43a062ee520e2eaf2e13"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "154b158aef59b9debe68193e0e5b83750a1c7a69f87a9db1b11a97518f272eaa"
     sha256 cellar: :any_skip_relocation, monterey:       "146ee8a52d7275e6f1f18988b7c2f53da62d418b3c18e3d27a8f579abfa74978"
