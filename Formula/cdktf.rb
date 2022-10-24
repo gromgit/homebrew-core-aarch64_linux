f895bf8635c683b14ee95f03c65099170d0f74bf
FX Coudert <fxcoudert@gmail.com>
2022-10-24 23:32:10 +0000

    cdktf: update 0.13.1 bottle.

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -8,6 +8,7 @@ class Cdktf < Formula
   license "MPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "28a411ba307e35c7a3b18ed301a2748e36dab4d1ae003427c8aac852c2d34ad4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "01aeaa9c6e27ed8ea2487a40b8c9cf74fd4be0b0e53b70886d1527a973db775a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01aeaa9c6e27ed8ea2487a40b8c9cf74fd4be0b0e53b70886d1527a973db775a"
     sha256 cellar: :any_skip_relocation, monterey:       "fa8943c8b242849526f06ab25e93fee6b7af7e9227598e2f2f8bdc2d3270a573"
