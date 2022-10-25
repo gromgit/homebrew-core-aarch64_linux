ce925035e426a85a6ffe3053049b0e7d3e1fabd4
FX Coudert <fxcoudert@gmail.com>
2022-10-25 05:55:54 +0000

    ed: update 1.18 bottle.

--- Formula/ed.rb
+++ Formula/ed.rb
@@ -7,6 +7,7 @@ class Ed < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2032af54afbacd9cdb684ca0365724700431472163f09ef424a95fc8b47717c1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e11a548c78fe21d0e7b4d87e96cd3318df87754e86143d04cc395d62e3d23240"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d0a969e4576aacc3385cc0482fca8752e8e07b54fb2696d9b12dfba2a54da346"
     sha256 cellar: :any_skip_relocation, monterey:       "c83bebea01405eda32c70d694c3195fa86c3f3f2a32ee3923454844fd5f5ff5d"
