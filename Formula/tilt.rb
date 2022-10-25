c09aa9f7859346f5aea2980da34cabd86d7fd03a
Tilt Dev <it@tilt.dev>
2022-10-25 14:40:19 +0000

    tilt 0.30.10
    Closes #113960.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -2,8 +2,8 @@ class Tilt < Formula
   desc "Define your dev environment as code. For microservice apps on Kubernetes"
   homepage "https://tilt.dev/"
   url "https://github.com/tilt-dev/tilt.git",
-    tag:      "v0.30.9",
-    revision: "ac882d4b6ab3c671de407e6413ae96fe6d2a8570"
+    tag:      "v0.30.10",
+    revision: "3c99f131360948f08e9c3022d63fdf23ef98f921"
   license "Apache-2.0"
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
