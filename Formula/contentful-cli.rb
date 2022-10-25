39498df3706e3c1f4c8ef4c41ab156514dc4d0a4
FX Coudert <fxcoudert@gmail.com>
2022-10-25 02:59:42 +0000

    contentful-cli: update 1.15.25 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -9,6 +9,7 @@ class ContentfulCli < Formula
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "08623743c15ddf0d32b19a328f74c7f241a16efdea2f1592b509e720f4372079"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0d838bb75afc9c0bb3a95df15e8fd201afb4e39757a762e9420da9b13ec8333a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0d838bb75afc9c0bb3a95df15e8fd201afb4e39757a762e9420da9b13ec8333a"
     sha256 cellar: :any_skip_relocation, monterey:       "c299e6160becf21fbf8e4ef1ca170f4533f098d889af250125a3a2df0e4ef997"
