245eb940932bb362dc944f0541470b94e7116dd8
FX Coudert <fxcoudert@gmail.com>
2022-10-25 05:07:43 +0000

    libupnp: update 1.14.14 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -11,6 +11,7 @@ class Libupnp < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "a2c80d01b2c7a4b8575921a4bf314d005d6326798411a35412ce4f394283920e"
     sha256 cellar: :any,                 arm64_monterey: "b877e17c89ba5b82171fd94be62a3c80be42dadc479ea08c34cc3815506e84a9"
     sha256 cellar: :any,                 arm64_big_sur:  "44a2da9c5a0fd8de2017ce9611aaf8a340392f7cef8b58325b9e54d8d1b75e73"
     sha256 cellar: :any,                 monterey:       "0c126e6ad4315134e0ebc48921b42cd2cf755706b26d7b85a7850e8349c14982"
