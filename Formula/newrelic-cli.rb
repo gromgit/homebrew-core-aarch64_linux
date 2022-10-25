01ae02c864144b5922badda9b49a8b9fc8e2b671
FX Coudert <fxcoudert@gmail.com>
2022-10-25 04:50:21 +0000

    newrelic-cli: update 0.56.0 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -7,6 +7,7 @@ class NewrelicCli < Formula
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e0dc93b12c5d15f0b61f2ce40ca53d1be3b7395d7a452e00c8e0f9935eafbcb6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c7c106c2aa35e2fc44614c3e918ddd7c608e12b106c969d12e1e8e2ceb77ab2a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8af45ff8c8f05ced01dd4e119fafa7fbc51cf9519e04c7305c552256252bd1c5"
     sha256 cellar: :any_skip_relocation, monterey:       "845c33846725683dd263d95777540f5e3aaf71bb25f40b4783314d324fc566c3"
