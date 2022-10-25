bc53c9140ec4d9597177a26a2d3582f320b8719d
FX Coudert <fxcoudert@gmail.com>
2022-10-25 08:39:35 +0000

    libsecret: update 0.20.5 bottle.

--- Formula/libsecret.rb
+++ Formula/libsecret.rb
@@ -6,6 +6,7 @@ class Libsecret < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
+    sha256 cellar: :any, arm64_ventura:  "14d4b00645910a57f3949dd8506e0df60b00ed4e8892ff08ec16a0cd42dd7709"
     sha256 cellar: :any, arm64_monterey: "38a274dc11d584dac3a265339c366a8222bf51d81142c65908164391cae6b789"
     sha256 cellar: :any, arm64_big_sur:  "23de21c66b4f88d01394e21e1827996b1660efdd8250f49fdbca528918cddfd9"
     sha256 cellar: :any, monterey:       "52e590836bb88b9a3d1ce3f29dd2e9c0d5f5812fceab2832228d8bff36b4c661"
