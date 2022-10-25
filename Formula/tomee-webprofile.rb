5cc1ca3fd2407575a5dc24bc8e5aff6b90c3566a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-25 05:55:00 +0000

    tomee-webprofile 8.0.13
    Closes #113945.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomee-webprofile.rb
+++ Formula/tomee-webprofile.rb
@@ -1,9 +1,9 @@
 class TomeeWebprofile < Formula
   desc "All-Apache Java EE 7 Web Profile stack"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.12/apache-tomee-8.0.12-webprofile.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.12/apache-tomee-8.0.12-webprofile.tar.gz"
-  sha256 "56f12a31a7f75c4683b299666b7a0347b57fa1e224f1d007c875618f727307c6"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.13/apache-tomee-8.0.13-webprofile.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.13/apache-tomee-8.0.13-webprofile.tar.gz"
+  sha256 "411ff6f25497c0886351e4bffbbf3bc61981d81b114688a4a4c5ae0bb5cfed9e"
   license "Apache-2.0"
 
   bottle do
