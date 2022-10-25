fee6338e40745079eb3660a6868bee1dbebb4365
FX Coudert <fxcoudert@gmail.com>
2022-10-25 06:04:46 +0000

    gphoto2: update 2.5.28_1 bottle.

--- Formula/gphoto2.rb
+++ Formula/gphoto2.rb
@@ -12,6 +12,7 @@ class Gphoto2 < Formula
   end
 
   bottle do
+    sha256 arm64_ventura:  "0a2a57995067c69118a232642dfb89cd3e3040706f768e1102160ba07d5ee000"
     sha256 arm64_monterey: "f539f391b11d94317e0c1b693b0f8ed0abfa1a702111c2a8807ae17be5890e38"
     sha256 arm64_big_sur:  "2c28a56b1840d21ca1044b9aa3a39b66ecda08dbb1ca3f6b762eb31450bce5cc"
     sha256 monterey:       "5ed1d4e739a9714fd521dab770b3f0158aa0f0e7ddeee83df73cbf29d7e00ff2"
