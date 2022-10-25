74edb88241dea9d1de4f1047a7c9b9ccf2854ce6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-25 06:29:09 +0000

    graphviz: update 7.0.0 bottle.

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -9,13 +9,12 @@ class Graphviz < Formula
   head "https://gitlab.com/graphviz/graphviz.git", branch: "main"
 
   bottle do
-    sha256 arm64_ventura:  "107c8a3015043dc0d8a29543f4346e4a41d6d6b949b30f6e2fdd574d22db2265"
-    sha256 arm64_monterey: "c588135346970f3afb8b5bfb00b36f51e0ae161e88cee3932da9ec5034f7a024"
-    sha256 arm64_big_sur:  "e32d033b450a6f8caf5b6c38a0fc117af862f736ba6537a632d6135d707bc3a8"
-    sha256 monterey:       "7cc5b7de1813e54b322b49fca4a6ebfaa0671611ab52c4c87fd534f67544e640"
-    sha256 big_sur:        "ba4d3e59a7edaafae99e063099fa5436433fbcea1d7478d795fbb5291d9ff45d"
-    sha256 catalina:       "69de8aaaba70f3ca896b1c1d59fbe853641c8b2eaa3189ce2007e36c5b856d8b"
-    sha256 x86_64_linux:   "2393110242bbc0f172499842f7132eb173d6cdb6eadc1d8930f9ad8d16cc63ae"
+    sha256 arm64_monterey: "492cb3a4feb730d2761bad2d0c705353abfe54f8fa9157d5386d799cd392f84c"
+    sha256 arm64_big_sur:  "82dc1482b2ad62782a6dab449606088436dbeaee0308e4b5f3b517f9c66f38ae"
+    sha256 monterey:       "e968109cfb27992b994cee95cbf43d35eb40993c814363e600b0cbecaa362915"
+    sha256 big_sur:        "17796daf1c645e8407dbfe22f367d9663db55d8191d1920c0b85d031e8b94030"
+    sha256 catalina:       "3aa822b4b56f311b64437ccd03bba896d8650fe2296a9eee01ec89edb7f397ef"
+    sha256 x86_64_linux:   "48d9103e04bf9ac1cf40c74bbdc92d91a5996d945ff41af918f4611be835d974"
   end
 
   depends_on "autoconf" => :build
