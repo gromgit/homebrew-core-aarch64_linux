9ea7eb25f6c1b2be5e65830ff90a7a48a9e7b5a1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 05:34:40 +0000

    nativefier: update 50.0.0 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -8,6 +8,7 @@ class Nativefier < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "87315c51ca37b9fe2b36375bfb20f66905ca97fa0738b5ebefce6449421c0a48"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d52bdaf1fd37f462fa937fe8e59368deec26d69a2cd7e055cd53387611dd6886"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d52bdaf1fd37f462fa937fe8e59368deec26d69a2cd7e055cd53387611dd6886"
     sha256 cellar: :any_skip_relocation, monterey:       "8d4e96702888dc4785f8aa32f04796ccc95d95f822286cc1b665ce5ea309f031"
