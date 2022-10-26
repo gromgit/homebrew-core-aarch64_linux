e4868721be5d8eecc47996cb27f0f586bf551bf6
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 09:26:48 +0000

    ddrescue: update 1.26 bottle.

--- Formula/ddrescue.rb
+++ Formula/ddrescue.rb
@@ -7,6 +7,7 @@ class Ddrescue < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "faa860db4715963f220c26412211476871b9de334cad2a0a250177f3fafdc6c4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d7e53ede5ffb258402da89d8ec5caa6b7b430d2d09b736f625f1b95c30049125"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7da116694fc665cca45f353ce50732a15bfb3628d8e1c438b30325ecb696efc"
     sha256 cellar: :any_skip_relocation, monterey:       "5d90acf7354dc1734a50f2b0e2aaa9063da223ab7373e076ef3d803f94b48a4d"
