0c64feea6567163c0e04d3ec1685c0a8c4859a41
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 06:05:21 +0000

    aspcud: update 1.9.6 bottle.

--- Formula/aspcud.rb
+++ Formula/aspcud.rb
@@ -6,6 +6,7 @@ class Aspcud < Formula
   license "MIT"
 
   bottle do
+    sha256 arm64_ventura:  "559e837a693b869dd122da250d57f222501b1f352bf57258eb4305530f8d30a0"
     sha256 arm64_monterey: "99122c4ae30f0760d00103191fb33b4fd793ac65e45f662a64d1386e0775d85f"
     sha256 arm64_big_sur:  "59e462b9a05482e92ccee1a483642515afe98cf8180d22ec414b16282513cb6d"
     sha256 monterey:       "8920dad4979d2ae3542553312c906d917ad1cbfe9f9059f4ee6bd726408489df"
