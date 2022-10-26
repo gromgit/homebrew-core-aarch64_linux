c981aed66e3310166fe5b92884143a1eebb5b3b5
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 07:07:49 +0000

    rbenv-gemset: update 0.5.10 bottle.

--- Formula/rbenv-gemset.rb
+++ Formula/rbenv-gemset.rb
@@ -7,6 +7,7 @@ class RbenvGemset < Formula
   head "https://github.com/jf/rbenv-gemset.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cd878825e721949e665c84452d74ec2c36c1b04a2da825ffb7dec3301d663fea"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "cd878825e721949e665c84452d74ec2c36c1b04a2da825ffb7dec3301d663fea"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cd878825e721949e665c84452d74ec2c36c1b04a2da825ffb7dec3301d663fea"
     sha256 cellar: :any_skip_relocation, monterey:       "7db514b2352ec2cd623ebf7efe1feebb8125960531fbeda72e7a4e37cfe576f6"
