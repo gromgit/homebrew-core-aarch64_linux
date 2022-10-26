b0cd81bc980638e2509d753e784a185fd54ad1d9
FX Coudert <fxcoudert@gmail.com>
2022-10-26 20:39:33 +0000

    pdfgrep: update 2.1.2_1 bottle.

--- Formula/pdfgrep.rb
+++ Formula/pdfgrep.rb
@@ -8,6 +8,7 @@ class Pdfgrep < Formula
 
   bottle do
     rebuild 2
+    sha256 cellar: :any,                 arm64_ventura:  "955662644ac2934b8e215b1d40ac2ba75eb76cf00e298d6020bccd5d3cff4c88"
     sha256 cellar: :any,                 arm64_monterey: "a0cbc11de2be8d3bd61a1e4838b723de768059d94a72c92406f26127b040a599"
     sha256 cellar: :any,                 arm64_big_sur:  "76971615597120ebba8f1db8e9c26d70d91faea4b0953c037047eddc9dbeb878"
     sha256 cellar: :any,                 monterey:       "934fedb57e6a7d731d93c0aa43dda0cdb02efb0e35822dc09dc1ce751c965166"
