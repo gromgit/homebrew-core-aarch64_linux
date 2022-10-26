21f06b6253a97c79c015c6262403851904646d62
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 04:41:28 +0000

    restic: update 0.14.0 bottle.

--- Formula/restic.rb
+++ Formula/restic.rb
@@ -7,6 +7,7 @@ class Restic < Formula
   head "https://github.com/restic/restic.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "91228b019a0379c0064dd3a4996614ad3a2374fd0d92a37f04374936166ff4ca"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "76aff7ed4b8952cdad67cbc838025c137f7e7798f8e440ff01a88bac070805b0"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "76aff7ed4b8952cdad67cbc838025c137f7e7798f8e440ff01a88bac070805b0"
     sha256 cellar: :any_skip_relocation, monterey:       "030fd47b302cdaef0b04967dd2adcd1600d6a24864a7737947a0d5dad2c50a7b"
