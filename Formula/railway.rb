7b13ee0902aef50f8732172e2df499e04166d71c
FX Coudert <fxcoudert@gmail.com>
2022-10-26 18:59:01 +0000

    railway: update 2.0.13 bottle.

--- Formula/railway.rb
+++ Formula/railway.rb
@@ -7,6 +7,7 @@ class Railway < Formula
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7363a99d8efff786a578f68fc3753d66888d8a8c37e82c2b55a512f01528c812"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7363a99d8efff786a578f68fc3753d66888d8a8c37e82c2b55a512f01528c812"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7363a99d8efff786a578f68fc3753d66888d8a8c37e82c2b55a512f01528c812"
     sha256 cellar: :any_skip_relocation, monterey:       "dd920e81026a98a7fc5e346183c143d5157829915950103214ea090b83dfaf4b"
