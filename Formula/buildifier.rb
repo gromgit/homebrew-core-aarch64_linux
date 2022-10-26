f148d25cce9d34264f925372adcc02763192d895
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 05:33:26 +0000

    buildifier: update 5.1.0 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -7,6 +7,7 @@ class Buildifier < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a597d18586fb30489ba3487a6da0297cdbf1690cf44a55fe39686ac7e2ab28ec"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d5067b1bd99a3c972caefb196e7a07204dbd0fb06566554b3056da2b3176eef7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d5067b1bd99a3c972caefb196e7a07204dbd0fb06566554b3056da2b3176eef7"
     sha256 cellar: :any_skip_relocation, monterey:       "b50c4c7e9a0bda54792c4f478417a8678c164aeaf08520d922dd7f8e9ef89bff"
