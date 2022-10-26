3546a0c14103ecd1aab6d487822c032ba108ff53
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 08:43:10 +0000

    arping: update 2.23 bottle.

--- Formula/arping.rb
+++ Formula/arping.rb
@@ -6,6 +6,7 @@ class Arping < Formula
   license "GPL-2.0-or-later"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "e1afbf2e85e8fe00aeceac30da249581bf3760f75c94b3d85d4dcaca8a1165f5"
     sha256 cellar: :any,                 arm64_monterey: "fd5666804b182d41ff81d9dace40a84a90a6d964e346b019a702d21ebc4e3eb4"
     sha256 cellar: :any,                 arm64_big_sur:  "9e85bad9dbdb1cd42ad5772a4ba6e9274a8b863a529c57eb13a0f0c85f18734e"
     sha256 cellar: :any,                 monterey:       "1421c4f2e75bc60834a8df83d377076fc2fec4eb884a189b27bf46a82291ecc7"
