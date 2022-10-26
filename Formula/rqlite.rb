8dcbbc5f39af7cd038ea36f3375814d33b7cb382
Rui Chen <rui@chenrui.dev>
2022-10-26 01:29:25 +0000

    rqlite: update 7.9.2 bottle.

--- Formula/rqlite.rb
+++ Formula/rqlite.rb
@@ -7,6 +7,7 @@ class Rqlite < Formula
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "87fd2b890057b9453182018878fed0e87bebe2e407218c43738c2fd8496b2823"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "60578816f5fec897f1a0816c693a5cfce129011ee6ec979806893066f2867eb8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "751a89fca2ff043a49a98b59c02a204b68bd34e40e3dbab64f5676285d521d9c"
     sha256 cellar: :any_skip_relocation, monterey:       "0aa8484950b30814b27a45595717005f4c9e0610692195ffff71959c6400fdab"
