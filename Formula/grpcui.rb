5d9cb26953cec7da35ee29b441ee439472885dda
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 08:15:18 +0000

    grpcui: update 1.3.1 bottle.

--- Formula/grpcui.rb
+++ Formula/grpcui.rb
@@ -6,6 +6,7 @@ class Grpcui < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5d697d0e919d9fa8a1565e430253683817a84f55528e73649c3e2445261ed0ad"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ba273507be5cfd956eca6209502841765d567470fa9fb345f7ea798edd0e813a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "85fe72471da84abadc9c0856126401492c0b5adcf3ee5e130d8dc1d446ded77c"
     sha256 cellar: :any_skip_relocation, monterey:       "6457e94f362363820b001de52fc7aaed951bc19025bb770492fac0ca781399a2"
