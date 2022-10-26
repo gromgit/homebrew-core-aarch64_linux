267a25e29756c1421b3f81e1e285ee97f80a0cd9
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 06:39:52 +0000

    groovysdk: update 4.0.6 bottle.

--- Formula/groovysdk.rb
+++ Formula/groovysdk.rb
@@ -11,6 +11,7 @@ class Groovysdk < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c24b69dd0fdb335baf3384a7c82743ac25e658d13f60ee55c3ee7d0773b3d137"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c24b69dd0fdb335baf3384a7c82743ac25e658d13f60ee55c3ee7d0773b3d137"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c24b69dd0fdb335baf3384a7c82743ac25e658d13f60ee55c3ee7d0773b3d137"
     sha256 cellar: :any_skip_relocation, monterey:       "91e99e4eb7c4dea647ca04f8c8e04f8a597e1c91d9b0ac201d3e1058cd460b0e"
