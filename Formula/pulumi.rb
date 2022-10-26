ed15168b8a3005ee4b30e6fe405876438f4ad09d
FX Coudert <fxcoudert@gmail.com>
2022-10-26 01:18:38 +0000

    pulumi: update 3.44.1 bottle.

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -8,6 +8,7 @@ class Pulumi < Formula
   head "https://github.com/pulumi/pulumi.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b3510a2575c41f03a4072dce25327d3c3c677ae288584125fd4da202408f0603"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fbcfd05040e1af5de9ac2b5b3319f7a21d1c3945d18399bb858a3467fee31607"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "38854a52760d5105422c4af9b4a6ad39bee813dd4aadadfe17f5003635b38fb8"
     sha256 cellar: :any_skip_relocation, monterey:       "00b91c0e341cb3d7a0f41b5233ff1844622e116e5830ccc7daf78aa47939d56e"
