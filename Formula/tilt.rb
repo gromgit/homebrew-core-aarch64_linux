3cef9afe77df0e85f8e383dccf7c238e183536e5
FX Coudert <fxcoudert@gmail.com>
2022-10-26 01:33:15 +0000

    tilt: update 0.30.10 bottle.

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -8,6 +8,7 @@ class Tilt < Formula
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "187f900fac0c580f105f365060740aa49fea49442c72522f3944da6f9f58f828"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1702ad819222741fb0c782531847f2daf1cfe546e495283f33d125ddadf87470"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e019b2329cc83b00bfafd480e0a3550b67e161d4b396dcbb21945967c8584c3f"
     sha256 cellar: :any_skip_relocation, monterey:       "ae74ce942cec80b67075f8a0ced8c8ab62d46fdeceeaa5539309f5a3bd28ef2e"
