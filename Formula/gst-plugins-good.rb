6f3d2a0dec6985b2c81385b2370760a419246e42
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-26 04:06:39 +0000

    gst-plugins-good: update 1.20.3_2 bottle.

--- Formula/gst-plugins-good.rb
+++ Formula/gst-plugins-good.rb
@@ -13,6 +13,7 @@ class GstPluginsGood < Formula
   end
 
   bottle do
+    sha256 arm64_ventura:  "3b4ffd9777e053e50730ede8b2198da1f231d504a2d03d78068127e11eadaeb0"
     sha256 arm64_monterey: "1e1f7fce251966cd64595eb91954a281c9678bd33ff247fcc876d2ea7e2b31ef"
     sha256 arm64_big_sur:  "7488f78b9591614a476f42ed065a7a5f238826c5ac941dab6b0dafda70fa7e98"
     sha256 monterey:       "61d30e5fda5a9a6f9b711eaa085d67e2df36186729fc4aada108fdf2fac165de"
