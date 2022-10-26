507576f0daaba34e2bb293931dad02290274a7a7
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-10-26 15:13:59 +0200

    hyperkit: depend on x86_64
    Closes #114044.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -15,6 +15,7 @@ class Hyperkit < Formula
   depends_on "opam" => :build
   depends_on "pkg-config" => :build
   depends_on xcode: ["9.0", :build]
+  depends_on arch: :x86_64
   depends_on "libev"
   depends_on :macos # Uses Hypervisor.framework, a component of macOS.
 
