2bf7268d091965faf12e428a1d3743e0042d53e2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-27 12:11:19 +0000

    findomain: update 8.2.1 bottle.

--- Formula/findomain.rb
+++ Formula/findomain.rb
@@ -6,6 +6,7 @@ class Findomain < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ef6183cfb3ff5ed1bd93401883914470e89782087cff8f238f7c3336818a02ab"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0854489c161c180f00e4c8d551bcc06339db0c77e4fc37fbf9ba64b3457f478e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d8da7e51087b7c2d3319d514e82961802f000ac59e01d52e729dc95c2f59b17"
     sha256 cellar: :any_skip_relocation, monterey:       "b162fe9c6dfe0f2eb152051fa409389b26e38a80ca41ebf72910d0796b1baf37"
