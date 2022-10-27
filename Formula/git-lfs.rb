85c3501e71c14754ab73189d47545930b81bb340
FX Coudert <fxcoudert@gmail.com>
2022-10-27 02:33:12 +0000

    git-lfs: update 3.2.0 bottle.

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -6,6 +6,7 @@ class GitLfs < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dcbda117bb2242bf45dbe5369b6b9c1b318447078e8ea82532b0eb2f6d6801eb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a81d5b29d1c0203f592e8af7b25ccb95431489ffa8dc595dbb1c39f1c08cfb46"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f10c03ed48c4168d8b80164d053040452e772c978344cc485e386183de7d5c24"
     sha256 cellar: :any_skip_relocation, monterey:       "2ccd239da97286fa2f9702f7c2731202819a6012163906b7e599e8fb218a6c95"
