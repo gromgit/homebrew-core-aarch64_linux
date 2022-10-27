36d771a58cbd041b20a908d6fbdc09e832392614
FX Coudert <fxcoudert@gmail.com>
2022-10-27 05:10:26 +0000

    libstrophe: update 0.12.2 bottle.

--- Formula/libstrophe.rb
+++ Formula/libstrophe.rb
@@ -7,6 +7,7 @@ class Libstrophe < Formula
   head "https://github.com/strophe/libstrophe.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "f9ae71b8f0ae88f3de87811b61d325ba1a986c1ceaceaf891fd47e3c71f1c164"
     sha256 cellar: :any,                 arm64_monterey: "da9690a4006d8011fffc155eda0e6860f2c76296b24ffcebf99bda9005d901f7"
     sha256 cellar: :any,                 arm64_big_sur:  "7898879a8407daf35cebacaee1f9bd9a282596bdf5497f49dc25f8211d9afa66"
     sha256 cellar: :any,                 monterey:       "4be10d525522a176fdc10d37c4206eef981ecd326c4717ac43e929a0ecdbe99c"
