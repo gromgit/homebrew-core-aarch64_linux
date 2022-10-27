c904ec8d5dd2bcbc8cc82d5dcf2ba1a7f4a882c5
Jason Lee <huacnlee@gmail.com>
2022-10-27 17:53:21 +0800

    autocorrect 2.4.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #114091.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/autocorrect.rb
+++ Formula/autocorrect.rb
@@ -1,8 +1,8 @@
 class Autocorrect < Formula
   desc "Linter and formatter to improve copywriting, correct spaces, words between CJK"
   homepage "https://github.com/huacnlee/autocorrect"
-  url "https://github.com/huacnlee/autocorrect/archive/v2.3.2.tar.gz"
-  sha256 "3767e39099a29bda54f31b33dd41611ca051d7323b1c80fdc477c4c8b2a720a4"
+  url "https://github.com/huacnlee/autocorrect/archive/v2.4.0.tar.gz"
+  sha256 "4b63bf9d28bbac00bda4f63366fb3031979fa666607811cf22363dc73db70c80"
   license "MIT"
   head "https://github.com/huacnlee/autocorrect.git", branch: "main"
 
