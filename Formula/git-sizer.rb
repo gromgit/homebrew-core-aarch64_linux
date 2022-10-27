c55611e2d6b1aabdd7f83df3d7cf16cf67d1e4b8
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-27 09:44:59 +0000

    git-sizer: update 1.5.0 bottle.

--- Formula/git-sizer.rb
+++ Formula/git-sizer.rb
@@ -6,6 +6,7 @@ class GitSizer < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f6d81ce3b3553755497bfc41658af2f441ed1084c03ea8a19812f665cec082ce"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f996c81d0b80e56313f6c187b498c5d3c71001bbac8bd704fd30e5f59c593d67"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28f29227982128a8cd544fcfc1d0aa8425f8ec0d9a60ca601426de8050e7a727"
     sha256 cellar: :any_skip_relocation, monterey:       "73f48a5d03c1e8b6a113a1d8509a35b4474bef00c26da7ec592f2c835e77d77a"
