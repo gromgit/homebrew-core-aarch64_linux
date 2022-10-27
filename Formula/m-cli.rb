4963ac05323439190825f96e10a8b6a03ae63d79
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-27 08:24:26 +0000

    m-cli: update 0.3.0 bottle.

--- Formula/m-cli.rb
+++ Formula/m-cli.rb
@@ -7,6 +7,7 @@ class MCli < Formula
   head "https://github.com/rgcr/m-cli.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dad744034a6378e1cac4bf308ed7b85beab6036667a4e1bed74842f095857ce6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "dad811c6cd0ca58a310fe482101f036bfcfe99e4675bf0682b6736db7e3d8a80"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57f125ffaf0e6a50c2d820b23921c4d804349a51df9780e00f79f4a41b9e4e39"
     sha256 cellar: :any_skip_relocation, monterey:       "a2def96834871cfd7618f2186662afdf5ef52f0c909b19d2d3b98cd4193fbd6d"
