cff952c23f04ecf8dd51e46185651a4033ce448a
FX Coudert <fxcoudert@gmail.com>
2022-10-27 23:45:30 +0000

    geographiclib: update 2.1.1 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -6,6 +6,7 @@ class Geographiclib < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "8c13b49ba4fd61359db8469ac1ad2ae64b70f6757cb073cb085071943c497a94"
     sha256 cellar: :any,                 arm64_monterey: "bfe0a6587b1795fb82b5c1b59c9b34462f720450ae32cb5940f6f3d4daf8b1ff"
     sha256 cellar: :any,                 arm64_big_sur:  "a16bfa8a86ba9e3d72cc97ad1f3fdcb18fe60b16d09bfdcda16b19a12d4b38e2"
     sha256 cellar: :any,                 monterey:       "c6f56914e3a91a17d2fdaccfe69b555936229cf85ec8e49cb331145eaf071a02"
