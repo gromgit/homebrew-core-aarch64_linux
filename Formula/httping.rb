38bf298d5d6b30fa73d82cd5b605b7e355324401
Ian Max Andolina <iandol@users.noreply.github.com>
2022-10-27 16:02:40 +0800

    httping 2.9
    Update httping.rb: V2.6, removes disabled status
    
    V2.5 had been disabled as the upstream repo disappeared. In fact the author moved it to github and it is available there. Trying to build the previous formula with the current code I ran into a few issues, for example on macOS 12.6 `useconds_t` was not properly imported and I reported this upstream(https://github.com/folkertvanheusden/HTTPing/issues/4) but doubt it will change. My solution is to replace `useconds_t` with `unsigned int` as recommended by the Linux man page for `usleep`.
    
    With that out of the way I also reported some other problems with the makefile which were fixed upstream.
    
    This is building fine on my system. I also tested it on a Ubuntu machine and it seemed to also compile without issue.
    
    httping: use GPL-3.0-only for licence description
    
    Update Formula/httping.rb
    
    correct order of version, sha256 and licence
    
    Replace zip with archive tarball
    
    Add a versioned release
    
    The original author has tagged a release so I'm using that, I hope the github tarball url is the best option. This version installs from source for me.
    
    Update Formula/httping.rb
    
    Update httping.rb
    
    update the upstream issue for ncurses
    
    Closes #114088.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/httping.rb
+++ Formula/httping.rb
@@ -1,11 +1,9 @@
 class Httping < Formula
   desc "Ping-like tool for HTTP requests"
-  homepage "https://www.vanheusden.com/httping/"
-  url "https://www.mirrorservice.org/sites/distfiles.macports.org/httping/httping-2.5.tgz"
-  mirror "https://fossies.org/linux/www/httping-2.5.tgz"
-  sha256 "3e895a0a6d7bd79de25a255a1376d4da88eb09c34efdd0476ab5a907e75bfaf8"
-  license "GPL-2.0"
-  revision 2
+  homepage "https://github.com/folkertvanheusden/HTTPing"
+  url "https://github.com/folkertvanheusden/HTTPing/archive/refs/tags/v2.9.tar.gz"
+  sha256 "37da3c89b917611d2ff81e2f6c9e9de39d160ef0ca2cb6ffec0bebcb9b45ef5d"
+  license "GPL-3.0-only"
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "719c7b85c0f6f75cd298210d78460311793048349fb01450ae1acc26204cd740"
@@ -19,18 +17,16 @@ class Httping < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "0090bfe517e71dd18d61ed96c5f5cf2cc1bf252399c9d49b521b9a53bc7a46b8"
   end
 
-  disable! date: "2022-10-19", because: :repo_removed
-
   depends_on "gettext"
   depends_on "openssl@1.1"
-
   uses_from_macos "ncurses"
 
   def install
-    # Reported upstream, see: https://github.com/Homebrew/homebrew/pull/28653
-    inreplace %w[configure Makefile], "ncursesw", "ncurses"
+    # Reported upstream, see: https://github.com/folkertvanheusden/HTTPing/issues/4
+    inreplace "utils.h", "useconds_t", "unsigned int"
+    # Reported upstream, see: https://github.com/folkertvanheusden/HTTPing/issues/7
+    inreplace %w[configure Makefile], "lncursesw", "lncurses"
     ENV.append "LDFLAGS", "-lintl" if OS.mac?
-    inreplace "Makefile", "cp nl.mo $(DESTDIR)/$(PREFIX)/share/locale/nl/LC_MESSAGES/httping.mo", ""
     system "make", "install", "PREFIX=#{prefix}"
   end
 
