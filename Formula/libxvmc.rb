768d3364b62075e55b9b9f1fe7ea06e35cb54538
FX Coudert <fxcoudert@gmail.com>
2022-10-27 05:40:33 +0000

    libxvmc: update 1.0.13 bottle.

--- Formula/libxvmc.rb
+++ Formula/libxvmc.rb
@@ -6,6 +6,7 @@ class Libxvmc < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "7c86347af3f1470259af400e6679dcb45030a98a331cb377a42ac906baf53a78"
     sha256 cellar: :any,                 arm64_monterey: "8dd27f8a1dfc1d9d1a8de409a18365eda5ac632f1e07a25489cc49074076f552"
     sha256 cellar: :any,                 arm64_big_sur:  "1f3cbc8fdb6acd20a304917bd4bd4ac4f8e8d9ab3cecb94f2a0dc63e59e63f41"
     sha256 cellar: :any,                 monterey:       "d4aa0e1d0640d0508c623c22c8d3818f5fa58eda8f64fbc0863b21b318b40e55"
