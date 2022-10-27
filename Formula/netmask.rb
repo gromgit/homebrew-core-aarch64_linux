fee8d40a45a386b0134d371f018d48b2e4c1d9ab
FX Coudert <fxcoudert@gmail.com>
2022-10-27 02:36:05 +0000

    netmask: update 2.4.4 bottle.

--- Formula/netmask.rb
+++ Formula/netmask.rb
@@ -6,6 +6,7 @@ class Netmask < Formula
   license "GPL-2.0-only"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b933d4ec7f084d202afd3b48feb3230c7eed31ec58b1aa0dd87f2b87d1397a6a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e386fc98127ef24109de173931633f0705dc8963e7e5a236ed69991e280d116c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "611337cf807c5344df21127e4e0b982e8454237256779683346ff39adebd634d"
     sha256 cellar: :any_skip_relocation, monterey:       "278a82473770a98ef3704737f2c2f4902ad818af931ff3bb872798f3c9169424"
