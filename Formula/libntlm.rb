4abe134de19db78820cc3ec7d2bb41afa23037f6
FX Coudert <fxcoudert@gmail.com>
2022-10-27 04:49:28 +0000

    libntlm: update 1.6 bottle.

--- Formula/libntlm.rb
+++ Formula/libntlm.rb
@@ -11,6 +11,7 @@ class Libntlm < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "d8d6009fd8f61ed5c328f0dc175769f814070d7df24e112c13c4a41852eff77b"
     sha256 cellar: :any,                 arm64_monterey: "2f27227db0e6572c8332911e61888cbbc0cf223426de87191f97b35164ef20fa"
     sha256 cellar: :any,                 arm64_big_sur:  "0b3b553a37a15dd0ffd396e8d07b295197e39f8f4e8670dc49a5ae214ded1cc1"
     sha256 cellar: :any,                 monterey:       "77de0ff9606d4701c26356065cc8ee0f6bbe9ca13b12e3494addf1b0e41f4c01"
