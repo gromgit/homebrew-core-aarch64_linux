e1e3e8b11ae82632f5236dc0a2f895f5ed291ebe
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-27 08:19:55 +0000

    bowtie2: update 2.4.5_1 bottle.

--- Formula/bowtie2.rb
+++ Formula/bowtie2.rb
@@ -7,6 +7,7 @@ class Bowtie2 < Formula
   revision 1
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ea59b98df1a437da070ef3367bf8134aec7bcdda5a97d157c1adbe5e78316e7f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "495bc223b670a7e029315ce992dcc0811dc3f63e714c656b6cb84c2f789aa439"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cf63043431156ed49e8034700900c508f32e02cd1830a8f815ed83735544dd5b"
     sha256 cellar: :any_skip_relocation, monterey:       "bd2176ef8b56c77260602d72566a6ad85240f8c7def21317b31913995e0789d4"
