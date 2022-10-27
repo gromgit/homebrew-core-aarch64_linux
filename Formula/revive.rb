0a82435149896042f987afeff83b3738aaed3fd3
FX Coudert <fxcoudert@gmail.com>
2022-10-27 22:59:21 +0000

    revive: update 1.2.4 bottle.

--- Formula/revive.rb
+++ Formula/revive.rb
@@ -8,6 +8,7 @@ class Revive < Formula
   head "https://github.com/mgechev/revive.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "205dc8e375ab5a258ed238e4119226131d642729f6ef1437faf43356bd204f61"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2ef0d85f18b2c2f3f0aaaa1c8fd8a42e1e439a374c5c95e053da8855959dd6f7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2ef0d85f18b2c2f3f0aaaa1c8fd8a42e1e439a374c5c95e053da8855959dd6f7"
     sha256 cellar: :any_skip_relocation, monterey:       "612dfe2fe091807adffd88081a60f8b5b095a5f399037c8cb7d81e6ea0b4a6e8"
