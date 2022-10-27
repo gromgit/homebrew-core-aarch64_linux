1323044bef81021fe8fb439ea33cac8140c8f1b8
James Laverack <james.laverack@jetstack.io>
2022-10-27 15:31:43 +0100

    cmctl 1.10.0 (new formula)
    * cmctl 1.10.0 (new formula)
    * Apply suggestions from code review
    * Improve cmctl testing
    * Apply suggestions from code review
    * Be more intentful about checking help text
    
    Closes #114104.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: James Laverack <james@jameslaverack.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/cmctl.rb
@@ -0,0 +1,62 @@
+class Cmctl < Formula
+  desc "Command-line tool to manage cert-manager"
+  homepage "https://cert-manager.io"
+  url "https://github.com/cert-manager/cert-manager.git",
+      tag:      "v1.10.0",
+      revision: "da3265115bfd8be5780801cc6105fa857ef71965"
+  license "Apache-2.0"
+  head "https://github.com/cert-manager/cert-manager.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/cert-manager/cert-manager/cmd/ctl/pkg/build.name=cmctl
+      -X github.com/cert-manager/cert-manager/cmd/ctl/pkg/build/commands.registerCompletion=true
+      -X github.com/cert-manager/cert-manager/pkg/util.AppVersion=v#{version}
+      -X github.com/cert-manager/cert-manager/pkg/util.AppGitCommit=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ctl"
+    generate_completions_from_executable(bin/"cmctl", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cmctl version --client")
+    # The binary name ("cmctl") is templated into the help text at build time, so we verify that it is
+    assert_match "cmctl", shell_output("#{bin}/cmctl help")
+    # We can't make a Kuberntes cluster in test, so we check that when we use a remote command
+    # we find the error about connecting
+    assert_match "connect: connection refused", shell_output("#{bin}/cmctl check api 2>&1", 1)
+    # The convert command *can* be tested locally.
+    (testpath/"cert.yaml").write <<~EOF
+      apiVersion: cert-manager.io/v1beta1
+      kind: Certificate
+      metadata:
+        name: test-certificate
+      spec:
+        secretName: test
+        issuerRef:
+          name: test-issuer
+          kind: Issuer
+        commonName: example.com
+    EOF
+
+    expected_output = <<~EOF
+      apiVersion: cert-manager.io/v1
+      kind: Certificate
+      metadata:
+        creationTimestamp: null
+        name: test-certificate
+      spec:
+        commonName: example.com
+        issuerRef:
+          kind: Issuer
+          name: test-issuer
+        secretName: test
+      status: {}
+    EOF
+
+    assert_equal expected_output, shell_output("#{bin}/cmctl convert -f cert.yaml")
+  end
+end
