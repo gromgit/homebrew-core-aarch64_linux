453289a6e237e3ccacd1b458b9cc46ee31e48c75
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-10-27 10:36:39 +0000

    bash-language-server: update 3.1.0 bottle.

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -8,6 +8,7 @@ class BashLanguageServer < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "42b66a767a56d5cd8a71a4763572b88a23b430b0838e17a8d1341bd71369361a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c9e5313f877b77afad07f9418b5a4bc0828e911173d6bd4b0726dc02885fed4a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c9e5313f877b77afad07f9418b5a4bc0828e911173d6bd4b0726dc02885fed4a"
     sha256 cellar: :any_skip_relocation, monterey:       "ea2aa92dcfe287797e4eb693dfbfaede9ab13c28b177fa4c7f500b92d8e261f6"
