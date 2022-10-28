56601c7e2d7de105418d5d448022766972890da6
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2022-10-28 01:58:46 +0000

    aws-cdk 2.48.0
    Closes #114140.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.47.0.tgz"
-  sha256 "c3b5aaa7d433aad4efdf93f71b86381f831a721b5c046309accbd0892c0e5297"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.48.0.tgz"
+  sha256 "891d9a2001898182286e2675717f3def5cf9693c6041ab06f6b00e4a46b4c092"
   license "Apache-2.0"
 
   bottle do
