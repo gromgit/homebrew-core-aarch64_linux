724233b56bbcc862d04e50b62cf3273b244ec90b
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-10-28 14:27:04 +0200

    quazip: remove linux bottle

--- Formula/quazip.rb
+++ Formula/quazip.rb
@@ -12,7 +12,6 @@ class Quazip < Formula
     sha256 cellar: :any,                 monterey:       "81eceb5944761190fab7e3fa3ebf64be503e13858a1a5ded1130d86172b02a71"
     sha256 cellar: :any,                 big_sur:        "753fd853f5823615f13e811e7813e5c95b1521d171bb2b612dd10a8e77bf4921"
     sha256 cellar: :any,                 catalina:       "59181f16535ae197a7e4863961c6286995d40640cd9659adbe57f40b1f917e02"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48d4f4e9e526b91297d4e768201a1d718500da5a2b61a2508308f2d54e96dbe6"
   end
 
   depends_on "cmake" => :build
