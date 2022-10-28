b521f658151d95ad9535eb92f4da496b31f4b682
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-28 12:06:23 +0000

    prestd: update 1.1.3 bottle.

--- Formula/prestd.rb
+++ Formula/prestd.rb
@@ -12,12 +12,12 @@ class Prestd < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc9a26c76d5db65cbed04e5326f70a0ff740da258fc158aab1845fe8ad5d3c4c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4e5afbfbe5beef3756d59f329631b68cfd169cc8ff8d87d5d27a1406ad38ab9"
-    sha256 cellar: :any_skip_relocation, monterey:       "50ada4487014050d8cc7b03327e499e03d00f6210fe45f0c75794b19032bcac6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "76ca3f6448a3853404ca8a71b6ccf6e2cefd93d631c058d35cff5a4833333675"
-    sha256 cellar: :any_skip_relocation, catalina:       "ee58fb4d5bc089880ac2769efc572984bd83da41032ffba74b8deedf0a67aec3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e38dc08bb4788dd41bcb7a1e147ecfde55bf93e7b28b25cfbaf0beddbb00a803"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5416f9ab63c253685022d5ddd1098b7906d8571bb83a55b97b0820b78857d41b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "32b0a0ff176dad26750dc501dfd2d30dd908003944e6876dba0d1e5b1a0c3964"
+    sha256 cellar: :any_skip_relocation, monterey:       "5a4dc43ab43e23c9e54356b879fb165c1c8390f1cd08f90fcb5120055ed37220"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4837ed8b319098d2e1754cb66d7cfc6c8286986cc4f310fcc3ba22f30c2fba50"
+    sha256 cellar: :any_skip_relocation, catalina:       "ea16aaaa22b898bcc8e477226f816dc5869c495a51ce1a4c3bfbee819b4116a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "287aa5eb191a758f4447472383b333b994e90b499ce9d4d01317246d4a4278e3"
   end
 
   depends_on "go" => :build
