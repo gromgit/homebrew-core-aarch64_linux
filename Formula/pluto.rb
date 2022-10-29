b4f1f0513df726900deabdd202b3c1d52522f0cf
Rui Chen <rui@chenrui.dev>
2022-10-29 17:23:28 +0000

    pluto: add 5.11.1 bottle.

--- Formula/pluto.rb
+++ Formula/pluto.rb
@@ -6,6 +6,15 @@ class Pluto < Formula
   license "Apache-2.0"
   head "https://github.com/FairwindsOps/pluto.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fcf730a5417856905d812dbcfaf7ba1ec75d33d3dfc1191676f289aaa97eb28b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f2e1ee49d5a050c4f08cbe7254608f1d9612d04ac9549900778eb16705d60d8b"
+    sha256 cellar: :any_skip_relocation, monterey:       "a6909de0c03f85e5088573031b5fb5a08abbf0789990b372315a7e3af1ad3c03"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0540df60b1471469b58a2a21ae38c73fa866941ac1ca09f6894de1ae693f9b54"
+    sha256 cellar: :any_skip_relocation, catalina:       "f823dacc4043b34094c1c538d95244581bd6c93e5a25038b087aa538ffca210d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85d3f0fb32daea753324f7f4da993702daa6ba27bb471ef093012a853d1fd0dd"
+  end
+
   depends_on "go" => :build
 
   def install
