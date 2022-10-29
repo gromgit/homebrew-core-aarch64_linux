01159bbd8be5f061d9a0d956093d76b47f5b6dbd
Rui Chen <rui@chenrui.dev>
2022-10-29 14:18:55 -0400

    httpx 1.2.5
    Closes #114253.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,8 +1,8 @@
 class Httpx < Formula
   desc "Fast and multi-purpose HTTP toolkit"
   homepage "https://github.com/projectdiscovery/httpx"
-  url "https://github.com/projectdiscovery/httpx/archive/v1.2.4.tar.gz"
-  sha256 "5d95a35586c851bba43be2fb79b790ff417e58aa34ae515a79b3bd8c6f2df7e0"
+  url "https://github.com/projectdiscovery/httpx/archive/v1.2.5.tar.gz"
+  sha256 "297b34f574bedd9575926f1e8c9f630a39ab54311bd2df821536359735a2b161"
   license "MIT"
   head "https://github.com/projectdiscovery/httpx.git", branch: "master"
 
@@ -19,12 +19,12 @@ class Httpx < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/httpx"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/httpx"
   end
 
   test do
-    output = JSON.parse(pipe_output("#{bin}/httpx -silent -status-code -title -json", "example.org"))
-    assert_equal 200, output["status-code"]
+    output = JSON.parse(shell_output("#{bin}/httpx -silent -title -json -u example.org"))
+    assert_equal 200, output["status_code"]
     assert_equal "Example Domain", output["title"]
   end
 end
