11ea818cbbbc9fea1aaa70a291f3418ee8b3a37f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-29 09:04:37 +0000

    oksh: update 7.2 bottle.

--- Formula/oksh.rb
+++ Formula/oksh.rb
@@ -7,12 +7,12 @@ class Oksh < Formula
   head "https://github.com/ibara/oksh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8859e5561db3f2bd97757668829e212622cf2c76f7e3bf21011107458ca8d8fb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c5efdd49f60012609a2f0324afcbef94b107662ccf73a867b1925d300908ffa"
-    sha256 cellar: :any_skip_relocation, monterey:       "9590eafe5164a3678e09a2e0563fb419a121dd84385cb330a81626f38accc125"
-    sha256 cellar: :any_skip_relocation, big_sur:        "388fe821f4a4ee040658e90e890820c05da0e62b3efb541183a0afbe68a28ff7"
-    sha256 cellar: :any_skip_relocation, catalina:       "c1b5d2e29c17a199e9591946454814876d8b69ed4db13c578e3015a8221102cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b74d7caa1450e8efe8c3b28133e864b4f660aee0151cc3f297262fb19de51c7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "92bd0170e4c7084bbd99ffd3f690abff73aafc62cb1d1f39b512bc94cc7db2d1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "64acdda6e703c41d5f2c4f4ad18e726e39d10eb0b691c5817e01cb5ddc50deef"
+    sha256 cellar: :any_skip_relocation, monterey:       "31483e6b27d67a84e51be25126245200766863302f6db893c638f3f368d0eae1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fab43495e25b0687f76339bfa26b9f0e32399a66cd40507a5d3a07e5ffd947b2"
+    sha256 cellar: :any_skip_relocation, catalina:       "2338af0e8ef621b614ef57cce9bea4776079ac6205cf3861d89990fde13ec717"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0228121955c1714503d0302924218e9e8f868298de94de6435f15dc8d02bd191"
   end
 
   def install
