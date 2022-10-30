73e7bc759048830dcd614dbf0c437a8f6c56b75e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-30 11:04:34 +0000

    xorgrgb: update 1.1.0 bottle.

--- Formula/xorgrgb.rb
+++ Formula/xorgrgb.rb
@@ -6,14 +6,12 @@ class Xorgrgb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d1498d717e15491172bda9366e1c51d4ca706c5871e4157eb5928b0ac2e4b041"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a8ca2a46987f85cf4fe5c06c808d462ed7fd6cbade4ab9a459825553cad8ccdb"
-    sha256 cellar: :any_skip_relocation, monterey:       "f5a1e5a984ee4505fd2c9a47ff716c88b9e24be35b98b674d19302df1934efb4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f6756bd8981d3006fe25f524bc8220f07d1a5995e77eeb3ae31c3d8854b98678"
-    sha256 cellar: :any_skip_relocation, catalina:       "ec1075868cfedeed6e68f844637fe8cbf1d978cacb6bedf6ca746a3a2a5e68f8"
-    sha256 cellar: :any_skip_relocation, mojave:         "ab75e74585e880cdaa2a0383626440834dcf2ed164d8c556d12ca9e74ede9386"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9035c5f64f471dcf32ab07e2321237fcab8a5fc3057cef3f29664db32222fc35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9f3380f8e66bbe354b1984f279211c9f1ef06e8e5e566c27c90d85f4af0ef06"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d9bb5c41b1ef76f04324db74b789330210d48be78c751ce4d0439e3b24a8b49"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30ee14ffdbb2418cc39884775ba8b61ab19452cd3e4e95609d5c67ac2bf56013"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a5606f9cdba7608ebc27042ced7920658ed39593a0338a0d02ccdd067290fe4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "db4fb9cf83a6fcc75036c4c3b3fdac6ac0fe47108311a1f83a35ebbe97e7f1e7"
+    sha256 cellar: :any_skip_relocation, catalina:       "4f39373ce62247b2a47ff8f0a02fbdcb9af7e280aa7b1fa7443865024a0f561c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e6c3eca82aa9624b8b521d9d0e5cec68b9391538717c36c4d438edc75bb0085f"
   end
 
   depends_on "pkg-config" => :build
