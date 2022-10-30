d7680d469a4f471d2463aa2d30d1a701a85822a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-30 11:04:43 +0000

    fourmolu: update 0.8.2.0 bottle.

--- Formula/fourmolu.rb
+++ Formula/fourmolu.rb
@@ -7,12 +7,12 @@ class Fourmolu < Formula
   head "https://github.com/fourmolu/fourmolu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "00d2ac168fcd52a1a7be19fb16edcbf9e5f3252d05faed9a3e318b226963ef5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b1999175de71ecaab0a30f28f2218eb1f28e062cc98f8847da0796a289ac2ace"
-    sha256 cellar: :any_skip_relocation, monterey:       "12a1b933267ccef3398268c3203651fcbf8c1247926a94fd4695e4a60243d7cd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0f07b249b3c2d364d0f9002161f6f91c458b6c54ed8526dc7256fe5cffae2bad"
-    sha256 cellar: :any_skip_relocation, catalina:       "800b378f7517d47a6fad5c7c32c737cb3ea71980a612d47ec3e9a0f486c2bd42"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "73fc6180b329fc1a63b38f7b499d5c3ad149b2f6b8b7f98653666db6ab567a41"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c27bd969db9b9e2b174fffa891efec3733f6c714d480ba33c4992f19b95d4266"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d6af1bf06d5a732e70d51912a8e4fcd55a021fde537795daac3a59468add2d06"
+    sha256 cellar: :any_skip_relocation, monterey:       "25fd33190271e503e3111820fe4ccd850ab61d28ef92eec4255e791f1f8c6f12"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b39589657815149376b57e68542b159f043db5af024b49bdbb60c612bf871029"
+    sha256 cellar: :any_skip_relocation, catalina:       "396c7644ecabda6ca881f337fe0792d2e83d55e1ed6172a1bb5fe7a72f4d9e7d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1321f4d1ba263ac871ec27412898bdc61515e467e9754c727900cde74044c91f"
   end
 
   depends_on "cabal-install" => :build
