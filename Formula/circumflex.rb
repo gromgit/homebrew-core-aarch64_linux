cfabf6ce2c3d7509162ce42c2fedcb5a886a3b89
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-10-30 08:47:20 +0100

    circumflex: revision
    * circumflex: uses_from_macos "less" -> depends_on "less"
    * Add revision number
    
    Closes #114291.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/circumflex.rb
+++ Formula/circumflex.rb
@@ -4,6 +4,7 @@ class Circumflex < Formula
   url "https://github.com/bensadeh/circumflex/archive/refs/tags/2.7.tar.gz"
   sha256 "109ff8b2a7e03b9c0dc0e6734aa6732caca88d28a9557b6865b57a53eb2e0c85"
   license "AGPL-3.0-only"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "5543152bf002dc0230e6fb0092e66063146cff512515457d73f5c52fa3979e3f"
@@ -16,9 +17,7 @@ class Circumflex < Formula
   end
 
   depends_on "go" => :build
-
-  # Requires less 576 or later for --use-color
-  uses_from_macos "less", since: :monterey
+  depends_on "less"
 
   def install
     system "go", "build", *std_go_args(output: bin/"clx", ldflags: "-s -w")
