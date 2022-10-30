34939c76ad1576bcd4348ad5a38e7ec6d9a7aa64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-30 11:04:33 +0000

    hamlib: update 4.5 bottle.

--- Formula/hamlib.rb
+++ Formula/hamlib.rb
@@ -7,13 +7,12 @@ class Hamlib < Formula
   head "https://github.com/hamlib/hamlib.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "ca4a1a40238e72a834336c9f19a52a7a4ecaae4224e9d39352597555f4c948d8"
-    sha256 cellar: :any,                 arm64_big_sur:  "ace0fd60e8dfae3cdd4f807bd49944cd0d28003cfc16d8cf0e751db77fbde4db"
-    sha256 cellar: :any,                 monterey:       "5b32a7108366f79102ca69dabafd9ed6f556fba03208235b62f04ccc98508a65"
-    sha256 cellar: :any,                 big_sur:        "a56d67cf6837bbe2c57dbb404275ad39ca6c8515a0325f5c4b62323c1ab3a609"
-    sha256 cellar: :any,                 catalina:       "b7fe12d40e52c85b463fe66099b11938619126273a23d6b2496f8abb532de244"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f8665360f495d72e0bd280e0d42d3a91eaa225762e691753194cbcf3fefa314b"
+    sha256 cellar: :any,                 arm64_monterey: "b68f6f796fedc7943e577dab6341a56f4fc1db350358d1bea9e59d060b33b270"
+    sha256 cellar: :any,                 arm64_big_sur:  "047c1d7655f92d77f49b81649910d74eabeabb8328c5525dca9e20341f0ed2ab"
+    sha256 cellar: :any,                 monterey:       "a148f79b3bc6100d1368aa410f3836d787c5c5afdc267b0543cacd7a994b2a29"
+    sha256 cellar: :any,                 big_sur:        "c2a384b63d3b293331654c5d6f47641fba926b88b9ffce2a266c76dffb2ae486"
+    sha256 cellar: :any,                 catalina:       "c967e1bdf6ec12bf8284c1e24085d8966ce324f1a49a674ff0aeb92f3c4cae72"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a951478fad1766716f5320d1f8e5199cf5c2ad4c1a8ed25e9b73f427a574f3c"
   end
 
   depends_on "autoconf" => :build
