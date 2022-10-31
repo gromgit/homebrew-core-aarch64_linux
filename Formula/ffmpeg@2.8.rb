d1bdedb34d74529295c8f61d5d0297c07f066abe
Rui Chen <rui@chenrui.dev>
2022-10-31 15:24:59 -0400

    ffmpeg@2.8 2.8.21
    Closes #114369.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ffmpeg@2.8.rb
+++ Formula/ffmpeg@2.8.rb
@@ -1,12 +1,11 @@
 class FfmpegAT28 < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-2.8.20.tar.xz"
-  sha256 "8566e326c3f4e47c67a176c9d14c1efe0852d025be378183ad7f5ceb2a7676c7"
+  url "https://ffmpeg.org/releases/ffmpeg-2.8.21.tar.xz"
+  sha256 "e5d956c19bff2aa5bdd60744509c9d8eb01330713d52674a7f650d54b570c82d"
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 2
 
   livecheck do
     url "https://ffmpeg.org/download.html"
