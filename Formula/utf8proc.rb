61fff25abc076a8818ca0149ad9fe4e2b5becf9c
Porkepix <porkepix@gmail.com>
2022-10-31 09:31:33 +0100

    utf8proc 2.8.0
    Closes #114335.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -1,8 +1,8 @@
 class Utf8proc < Formula
   desc "Clean C library for processing UTF-8 Unicode data"
   homepage "https://juliastrings.github.io/utf8proc/"
-  url "https://github.com/JuliaStrings/utf8proc/archive/v2.7.0.tar.gz"
-  sha256 "4bb121e297293c0fd55f08f83afab6d35d48f0af4ecc07523ad8ec99aa2b12a1"
+  url "https://github.com/JuliaStrings/utf8proc/archive/v2.8.0.tar.gz"
+  sha256 "a0a60a79fe6f6d54e7d411facbfcc867a6e198608f2cd992490e46f04b1bcecc"
   license all_of: ["MIT", "Unicode-DFS-2015"]
 
   bottle do
