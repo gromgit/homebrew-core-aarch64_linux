31e183c10dd39439989f68b4f7002119fdd59cc8
Rui Chen <rui@chenrui.dev>
2022-10-31 15:23:36 -0400

    checkstyle 10.4
    Closes #114365.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -1,8 +1,8 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "https://checkstyle.sourceforge.io/"
-  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.3.4/checkstyle-10.3.4-all.jar"
-  sha256 "1ce2846c15368e8ec026648c546da3d7233855f71d37e4e5fcc89acf1b4a607c"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.4/checkstyle-10.4-all.jar"
+  sha256 "c7fd77aea6f734e3f830bd87a1d764991ac4c0102c1bded9da22cba090aed8f4"
   license "LGPL-2.1-or-later"
 
   bottle do
