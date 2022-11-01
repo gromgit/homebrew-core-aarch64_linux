a1ab8348a7326e366c3cc1d5a239f69787e6ef88
Rui Chen <rui@chenrui.dev>
2022-11-01 11:41:27 -0400

    heartbeat 8.5.0
    Closes #114429.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/heartbeat.rb
+++ Formula/heartbeat.rb
@@ -2,8 +2,8 @@ class Heartbeat < Formula
   desc "Lightweight Shipper for Uptime Monitoring"
   homepage "https://www.elastic.co/beats/heartbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.4.3",
-      revision: "c2f2aba479653563dbaabefe0f86f5579708ec94"
+      tag:      "v8.5.0",
+      revision: "6d6754fcb0adf6a2191b055d35f694c961c8ba40"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
