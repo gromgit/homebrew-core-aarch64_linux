83265cb96f086d3125bfc039a7c086afe84f6619
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-01 10:04:56 +0000

    dehydrated: update 0.7.1 bottle.

--- Formula/dehydrated.rb
+++ Formula/dehydrated.rb
@@ -6,13 +6,12 @@ class Dehydrated < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e4f179282544d70d072f6ebea22527d7dfbb8a0d810d5965fc7266918fef4f6d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4f179282544d70d072f6ebea22527d7dfbb8a0d810d5965fc7266918fef4f6d"
-    sha256 cellar: :any_skip_relocation, monterey:       "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, catalina:       "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, mojave:         "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e4f179282544d70d072f6ebea22527d7dfbb8a0d810d5965fc7266918fef4f6d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
+    sha256 cellar: :any_skip_relocation, monterey:       "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
+    sha256 cellar: :any_skip_relocation, catalina:       "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
   end
 
   def install
