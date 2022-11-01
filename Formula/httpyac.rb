757377c30585806042c8cb90fb70736bf778768d
Rui Chen <rui@chenrui.dev>
2022-11-01 13:17:03 -0400

    httpyac 5.7.5
    Closes #114461.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/httpyac.rb
+++ Formula/httpyac.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Httpyac < Formula
   desc "Quickly and easily send REST, SOAP, GraphQL and gRPC requests"
   homepage "https://httpyac.github.io/"
-  url "https://registry.npmjs.org/httpyac/-/httpyac-5.7.4.tgz"
-  sha256 "da3e298902c856c55556d364b4572fe742d4e164e903acb399891612d875e48d"
+  url "https://registry.npmjs.org/httpyac/-/httpyac-5.7.5.tgz"
+  sha256 "5a03a009c46c506fd0a2d126252ad6f164f51674177d5cb8d3c3bb61c5c04edc"
   license "MIT"
 
   bottle do
