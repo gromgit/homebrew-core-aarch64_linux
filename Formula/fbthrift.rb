906dfb755fb2036b39c05c53c51b881d47951cfb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-01 06:10:44 +0000

    fbthrift: update 2022.10.31.00 bottle.

--- Formula/fbthrift.rb
+++ Formula/fbthrift.rb
@@ -7,13 +7,12 @@ class Fbthrift < Formula
   head "https://github.com/facebook/fbthrift.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "6c6d3cf710a44956af801ae14bb237996eb9e7addddbfaf2755242243b60dc32"
-    sha256 cellar: :any,                 arm64_monterey: "545984791dc0ca9ec1bfbbfebd043c96cd167de726a66b350e2fb170b3762e15"
-    sha256 cellar: :any,                 arm64_big_sur:  "2faa7483b341ecbca7d39117af7ccfe5e494dbe1de2e8df06438151b4fd21639"
-    sha256 cellar: :any,                 monterey:       "5d95749d5577bf4e949e1c840e83f0537e9233460e058a77b8019352de7e2941"
-    sha256 cellar: :any,                 big_sur:        "59b8e8ab19fb6bb468777a25a778948c17530c79f2769319c0d3ee5cbdc2357a"
-    sha256 cellar: :any,                 catalina:       "0fe10fa3837b9d08e33dc92168336b0801332c9a1125b374d9ab29b33d20f42d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43b12515fa2d91a12eb9cf45c48012c5b3f2115883fc0078e606d2394484285f"
+    sha256 cellar: :any,                 arm64_monterey: "6faa1200b901ddb1990eddadaff09f7e811ced78a325f07b99310a31809fa7fd"
+    sha256 cellar: :any,                 arm64_big_sur:  "0fee15c14228e467c733e1d0a8fcd97a89273b8181cf7c457689449a296eddaf"
+    sha256 cellar: :any,                 monterey:       "897a4d9fcb7a98c6e542cdda24b52210bb4279648722c8a66288f74f960c5a73"
+    sha256 cellar: :any,                 big_sur:        "0536c577984486062f8c5cb2e4c1f8c88b74659dea12cb32b644749f4d60fd73"
+    sha256 cellar: :any,                 catalina:       "f36844335cf9b54572ca7dbb5aae8d3f728aa37bf0bee93c01d59fc6ce720a8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1e341b628526362b68304db252b5266d5997aa817d664723b715cd5cf86d4799"
   end
 
   depends_on "bison" => :build # Needs Bison 3.1+
