387600e40e5c7bafc93114c0aba62ec4dbd43e56
Porkepix <porkepix@gmail.com>
2022-11-01 01:19:07 +0100

    libressl 3.6.1

--- Formula/libressl.rb
+++ Formula/libressl.rb
@@ -2,9 +2,9 @@ class Libressl < Formula
   desc "Version of the SSL/TLS protocol forked from OpenSSL"
   homepage "https://www.libressl.org/"
   # Please ensure when updating version the release is from stable branch.
-  url "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.5.3.tar.gz"
-  mirror "https://mirrorservice.org/pub/OpenBSD/LibreSSL/libressl-3.5.3.tar.gz"
-  sha256 "3ab5e5eaef69ce20c6b170ee64d785b42235f48f2e62b095fca5d7b6672b8b28"
+  url "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.6.1.tar.gz"
+  mirror "https://mirrorservice.org/pub/OpenBSD/LibreSSL/libressl-3.6.1.tar.gz"
+  sha256 "acfac61316e93b919c28d62d53037ca734de85c46b4d703f19fd8395cf006774"
   license "OpenSSL"
 
   livecheck do
