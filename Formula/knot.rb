7d138faa3c4fd6a031d89bf6c3c394bd78baf5e2
Anand Buddhdev <anandb@ripe.net>
2022-11-01 09:57:20 +0100

    knot 3.2.2
    * Update knot.rb
      Enable QUIC support in Knot with `--enable-quic`
    * knot 3.2.2
    * Update knot.rb
      Ask kdig to perform a DNS query using the QUIC protocol, to see if it works. At the moment, only AdGuard's public resolver (94.140.14.140) supports QUIC.
    
    Closes #114414.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.2.1.tar.xz"
-  sha256 "51efa36f92679b25d43dbf8ba543e9f26138559f0fa1ba5fae172f5400659c8f"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.2.2.tar.xz"
+  sha256 "cea9c1988cdce7752f88fbe37378f65e83c4e54048978b94fb21a9c92f88788f"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
@@ -48,7 +48,8 @@ class Knot < Formula
                           "--with-rundir=#{var}/run/knot",
                           "--prefix=#{prefix}",
                           "--with-module-dnstap",
-                          "--enable-dnstap"
+                          "--enable-dnstap",
+                          "--enable-quic"
 
     inreplace "samples/Makefile", "install-data-local:", "disable-install-data-local:"
 
@@ -92,7 +93,7 @@ class Knot < Formula
   end
 
   test do
-    system bin/"kdig", "www.knot-dns.cz"
+    system bin/"kdig", "@94.140.14.140", "www.knot-dns.cz", "+quic"
     system bin/"khost", "brew.sh"
     system sbin/"knotc", "conf-check"
   end
