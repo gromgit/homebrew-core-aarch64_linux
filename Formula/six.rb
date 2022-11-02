7f410a6bff402e70c3162683a366c1e649b72125
Michael Cho <cho-m@tuta.io>
2022-11-02 00:17:32 -0700

    six: add `python@3.11` and remove `python@3.8`

--- Formula/six.rb
+++ Formula/six.rb
@@ -4,7 +4,7 @@ class Six < Formula
   url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
   sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   license "MIT"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
@@ -12,7 +12,7 @@ class Six < Formula
   end
 
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.8" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.9" => [:build, :test]
 
   def pythons
