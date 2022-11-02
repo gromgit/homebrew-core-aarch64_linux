0bf14600d853f0bbb8f9b71ecd1ae7b4f052a51f
Alexei Yuzhakov <ayuzhakov@plesk.com>
2022-11-02 17:43:12 +0200

    xq: embed build metadata
    Closes #114537.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xq.rb
+++ Formula/xq.rb
@@ -1,10 +1,10 @@
 class Xq < Formula
   desc "Command-line XML beautifier and content extractor"
   homepage "https://github.com/sibprogrammer/xq"
-  url "https://github.com/sibprogrammer/xq/archive/refs/tags/v0.0.8.tar.gz"
-  sha256 "9ab6d0fd2ef3e1ca6c74fec8a466c20d65fd5330eaf75d16a22bf878c5500493"
+  url "https://github.com/sibprogrammer/xq.git",
+      tag:      "v0.0.8",
+      revision: "b249d1a8470c10d950e404f926794bd5ba7f7c3a"
   license "MIT"
-  head "https://github.com/sibprogrammer/xq.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4191730c263fa0389e7aa8a7be8ccd50075c28f6a52f47585d3bf94b01e4a6d8"
@@ -18,7 +18,15 @@ class Xq < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X main.commit=#{Utils.git_head}
+      -X main.version=#{version}
+      -X main.date=#{time.rfc3339}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
   end
 
   test do
