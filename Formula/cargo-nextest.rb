d7a05abbbff208b8a6eb5a0810ee7d548f276a59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-02 09:04:58 +0000

    cargo-nextest: update 0.9.42 bottle.

--- Formula/cargo-nextest.rb
+++ Formula/cargo-nextest.rb
@@ -11,12 +11,12 @@ class CargoNextest < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8444977cccb88771656a51f1e125e77dd35fa64b636ea38beb1f7aa5c608ffaf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c9826cd48d5904071d076ef910002530073269a95702aa43c319120cac8b945"
-    sha256 cellar: :any_skip_relocation, monterey:       "7bbb54b972830ef241ccccbddff659026ff57625614623f86d901b9cf5f79a66"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8d4629fdd5646180b118d44eb275272322a8f9e08f094876dc28a7b0a3358d87"
-    sha256 cellar: :any_skip_relocation, catalina:       "9ef13f6f525527f25dbf425cdb770888f45c62cf34634b7efcf9294d6f64379c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2d035c708256d38f68eb16326bbb0b52cda8fa5db97c730c80889b354e523fa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "534adb64b30773a722eb738da3c1490ed34c7c7c054d2fda4d9d19139e90a196"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5eea48e19f48d1f3a48ad710df26466eed0f68369c438dd465cb5a1aeccaad44"
+    sha256 cellar: :any_skip_relocation, monterey:       "3257b38ac850bc45190c212c8782ccd8011c5a71e366fb13f1ec996b362b32fe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a2924636bfeda9618fe8055ebef66249f50e96fe5813a38d9b47a5449925b6d7"
+    sha256 cellar: :any_skip_relocation, catalina:       "7cf24d691cbb16a9e9151aecab6a73ed0de8cbf0b698e549967db0184d6d9547"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "782fdd581c34d9465dbd7260a8026b947a7d711de9f38d208bbc35bbfb73a341"
   end
 
   depends_on "rust" # uses `cargo` at runtime
