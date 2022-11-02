9f8f6ed236f81800633bcb0a70bb92b2917257c4
Bevan Kay <email@bevankay.me>
2022-11-02 14:43:21 +1100

    firebase-cli 11.16.0
    * firebase-cli 11.16.0
    * restore term-size dependency
    
    Closes #114489.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-11.15.0.tgz"
-  sha256 "3b9d1619849be4e585d1759a21f9192353499df64cb1d827946328a21e377789"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-11.16.0.tgz"
+  sha256 "3e2b6babb76d8cce1d0785cc8633443bdb8bfa10b91220893cac5c0b5b291fe0"
   license "MIT"
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
@@ -21,10 +21,23 @@ class FirebaseCli < Formula
   depends_on "node"
 
   uses_from_macos "expect" => :test
+  on_macos do
+    depends_on "macos-term-size"
+  end
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    term_size_vendor_dir = libexec/"lib/node_modules/firebase-tools/node_modules/term-size/vendor"
+    term_size_vendor_dir.rmtree # remove pre-built binaries
+
+    if OS.mac?
+      macos_dir = term_size_vendor_dir/"macos"
+      macos_dir.mkpath
+      # Replace the vendored pre-built term-size with one we build ourselves
+      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
+    end
   end
 
   test do
