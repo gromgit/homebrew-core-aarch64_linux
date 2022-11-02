58eb6c62aa58fd1b4b0534d44a635ee9f1c2848e
FX Coudert <fxcoudert@gmail.com>
2022-11-02 22:11:43 +0000

    kubecfg: update 0.28.0 bottle.

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -6,6 +6,7 @@ class Kubecfg < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "50792f708ae1cb2c4bb956eb321540fdeef78b4bbb22bb04619f6dcdfe158a05"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6bd2f3c9fbdef495aa1fc72285542a1123f494c469c1316c6162107f8af69d82"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0d503ad24c77a3fa0a506d87eefca82bf8116615286b908d412733d9a91ec229"
     sha256 cellar: :any_skip_relocation, monterey:       "bab16599e3e95aad5bfa1da135bb3d75e8d048a80936f5143e164623afefea78"
