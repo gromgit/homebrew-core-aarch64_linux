30a41bd2419d68a7bc62ab2079dc74012657f57d
Bo Anderson <mail@boanderson.me>
2022-11-02 12:24:00 +0000

    pnpm: update 7.14.1 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,6 +13,7 @@ class Pnpm < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "86f591591c602ff25b74ffac107dcd7e6a35b2e5f41cf9041673b2a4d05fc59b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "86f591591c602ff25b74ffac107dcd7e6a35b2e5f41cf9041673b2a4d05fc59b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "86f591591c602ff25b74ffac107dcd7e6a35b2e5f41cf9041673b2a4d05fc59b"
     sha256 cellar: :any_skip_relocation, monterey:       "87644073b2f1d1a45498592f8e587226ea6a28731c21181f66682d44bfe76c3e"
