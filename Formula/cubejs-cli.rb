46f624a0313bae0595275647859107b18b376118
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-03 05:13:16 +0000

    cubejs-cli 0.31.11
    Closes #114582.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class CubejsCli < Formula
   desc "Cube.js command-line interface"
   homepage "https://cube.dev/"
-  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.31.10.tgz"
-  sha256 "1d8ea21178308e039631070735a9a739d612399ce4df80e124c1baf387f02fff"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.31.11.tgz"
+  sha256 "4d2602389f77bb0922505f6ed42d45a30b4f80a868b8071323bf39490205faac"
   license "Apache-2.0"
 
   bottle do
