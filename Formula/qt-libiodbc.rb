858eea3588217b8322d7139b187e719ef27468f4
FX Coudert <fxcoudert@gmail.com>
2022-11-03 12:56:44 +0000

    qt-libiodbc: update 6.4.0 bottle.

--- Formula/qt-libiodbc.rb
+++ Formula/qt-libiodbc.rb
@@ -10,6 +10,7 @@ class QtLibiodbc < Formula
   end
 
   bottle do
+    sha256 cellar: :any, arm64_ventura:  "01ce6ea26e506cee368dbe47141e9877d0ad0a93073a1f7ca388a326e91d2dbe"
     sha256 cellar: :any, arm64_monterey: "202d487ce7bde85a93569e10ecc4cc50ba15c4a931390a1130dcc5b3a4ce7a0b"
     sha256 cellar: :any, arm64_big_sur:  "539980e282206c0349a288c0846491551ce8c68e3b71cc081fdc0e8d17e410f0"
     sha256 cellar: :any, monterey:       "50668c37867fe4765ffc9a00aff284b87c47339d943ea626dc85a2689b963dae"
