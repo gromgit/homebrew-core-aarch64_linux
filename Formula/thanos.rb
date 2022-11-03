efd3fc1319ddf580be1bbde0606b828d5b2458b8
Porkepix <porkepix@gmail.com>
2022-11-03 16:58:50 +0100

    thanos 0.29.0
    Closes #114621.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/thanos.rb
+++ Formula/thanos.rb
@@ -1,8 +1,8 @@
 class Thanos < Formula
   desc "Highly available Prometheus setup with long term storage capabilities"
   homepage "https://thanos.io"
-  url "https://github.com/thanos-io/thanos/archive/v0.28.1.tar.gz"
-  sha256 "e00ae14b88b9a3ae6b1ae022a0bd4fad34fd2da401bcef401cefa5e087675783"
+  url "https://github.com/thanos-io/thanos/archive/v0.29.0.tar.gz"
+  sha256 "45fa97935a8d4f426c677c6077e404cd863c16f758187ad16262451df3262a14"
   license "Apache-2.0"
 
   bottle do
