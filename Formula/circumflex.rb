25b245ebc1154772202fa5f47d3d81b8bc56bb1a
FX Coudert <fxcoudert@gmail.com>
2022-11-03 08:59:40 +0000

    circumflex: update 2.7_1 bottle.

--- Formula/circumflex.rb
+++ Formula/circumflex.rb
@@ -7,6 +7,7 @@ class Circumflex < Formula
   revision 1
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0b783c1b14ecc5db8e6da36137b37c547200e8eff8924f52fdb0cd3d99d1a1ca"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "51177211c28b0b7500e526339ab3d576a0aff56b7b7bbbe6b91267ee1ccae6be"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d70f5cb026e3b1837dce773c64682ed3ca12211b8ccbfa693005f666775e180"
     sha256 cellar: :any_skip_relocation, monterey:       "be4374762a967ee7f98915ceaa30deb6f5417c8970d97a64a7e3ecc3aa44918e"
