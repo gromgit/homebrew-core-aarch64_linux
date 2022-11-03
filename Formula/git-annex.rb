d8f4916b034e106448ba7d94f4ebcd417cdd4a58
Nick Golder <nrg@nirgo.net>
2022-11-03 14:04:35 -0600

    git-annex 10.20221103
    Closes #114629.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -1,8 +1,8 @@
 class GitAnnex < Formula
   desc "Manage files with git without checking in file contents"
   homepage "https://git-annex.branchable.com/"
-  url "https://hackage.haskell.org/package/git-annex-10.20221003/git-annex-10.20221003.tar.gz"
-  sha256 "24a6c7e70a578e2bcd78a525f4882f5d8c1e1d555e5aba6b6afea1f627afd05b"
+  url "https://hackage.haskell.org/package/git-annex-10.20221103/git-annex-10.20221103.tar.gz"
+  sha256 "f549c31264d6da3bb544755795e7fc29882ebec45014905bc2ea0ade28398f3b"
   license all_of: ["AGPL-3.0-or-later", "BSD-2-Clause", "BSD-3-Clause",
                    "GPL-2.0-only", "GPL-3.0-or-later", "MIT"]
   head "git://git-annex.branchable.com/", branch: "master"
