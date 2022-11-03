812a2e380c9543ff1e213bbbc62a3dbe9859540d
FX Coudert <fxcoudert@gmail.com>
2022-11-03 14:19:41 +0000

    gti: update 1.8.0 bottle.

--- Formula/gti.rb
+++ Formula/gti.rb
@@ -7,6 +7,7 @@ class Gti < Formula
   head "https://github.com/rwos/gti.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "97349fc9a6650a617c1bd2648c700f53cac6f69e41c5a15f99417532fa66e41b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "82db99490ed0414cb2a724301b9e6c5d4a945281bad8fa050a255461ebe6b00c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9e5543b011f73b404717004dcf5280f3e4ad340c2afebd579f245137c0020def"
     sha256 cellar: :any_skip_relocation, monterey:       "fcb9b0845661d70831ee78f420d9bb3a335471b8c06ad0cf7e040edb44ee6675"
