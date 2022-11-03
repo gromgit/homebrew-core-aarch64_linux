02a1220cf369049228aee4bdca9c718cdceb02f6
Porkepix <porkepix@gmail.com>
2022-11-03 16:10:10 +0100

    libdrm 2.4.114
    Closes #114614.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libdrm.rb
+++ Formula/libdrm.rb
@@ -1,8 +1,8 @@
 class Libdrm < Formula
   desc "Library for accessing the direct rendering manager"
   homepage "https://dri.freedesktop.org"
-  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.113.tar.xz"
-  sha256 "7fd7eb2967f63beb4606f22d50e277d993480d05ef75dd88a9bd8e677323e5e1"
+  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.114.tar.xz"
+  sha256 "3049cf843a47d12e5eeefbc3be3496d782fa09f42346bf0b7defe3d1e598d026"
   license "MIT"
 
   livecheck do
