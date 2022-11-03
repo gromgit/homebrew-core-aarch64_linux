d438ac9687dbcc43eb45feba21f28c2426cf5f55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-03 09:04:55 +0000

    xq: update 0.0.8 bottle.

--- Formula/xq.rb
+++ Formula/xq.rb
@@ -7,12 +7,13 @@ class Xq < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4191730c263fa0389e7aa8a7be8ccd50075c28f6a52f47585d3bf94b01e4a6d8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "36a8d6d20c8ae3734bb749e080d3a5b665c2563824a1bc7bb4f574e39cf700f2"
-    sha256 cellar: :any_skip_relocation, monterey:       "f62cc6bdb06dda94da9bb5ce040497833b01933770f200dd973d88d8ccba9a1b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b1559f646a54240384d8931cb155689bf7ff40aebf113826e8f453e1ba25082a"
-    sha256 cellar: :any_skip_relocation, catalina:       "0718b2b10cd49943c30fd6b24dd19411f7f431b204d0c0b4f44eacc210959b4b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "905d5abe7415a7cfc8b93e5df66e299e68c762db654ca1c77bc1146fecb01c55"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b90ff156c9344bc291f95a53ba801f9a14674b7e2a3a4e1ce907ce27de6ea0a0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b90ff156c9344bc291f95a53ba801f9a14674b7e2a3a4e1ce907ce27de6ea0a0"
+    sha256 cellar: :any_skip_relocation, monterey:       "0012342ad22da17f5afa76bfd76e998484b5105c94dd1212cdd328d622ce2831"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0012342ad22da17f5afa76bfd76e998484b5105c94dd1212cdd328d622ce2831"
+    sha256 cellar: :any_skip_relocation, catalina:       "0012342ad22da17f5afa76bfd76e998484b5105c94dd1212cdd328d622ce2831"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2c736c3393aa0e08a64302b42799f3d400614187d8681dfa174c539683a33102"
   end
 
   depends_on "go" => :build
