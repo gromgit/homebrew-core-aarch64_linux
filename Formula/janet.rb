bd1ffb73d8d19fb474cbe0f63dfd19ecf0d15f6f
FX Coudert <fxcoudert@gmail.com>
2022-11-03 08:54:22 +0000

    janet: update 1.25.1 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -7,6 +7,7 @@ class Janet < Formula
   head "https://github.com/janet-lang/janet.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "c6deaf13e3401ec0b929324224b04e94948c0b219855997652a690a4e1d190bf"
     sha256 cellar: :any,                 arm64_monterey: "da6a5175726641da200ca353bc3b3cb0311161b61f17918177396e3eca1ef30c"
     sha256 cellar: :any,                 arm64_big_sur:  "e74fe5fbaa37a6a3fd1972daea26cf783aff534e249d4f7cdac49aacd3b59f87"
     sha256 cellar: :any,                 monterey:       "5e99427c6720819e26fff4c0e6353348499bdf9f762cae9e033e9dd70b5be197"
