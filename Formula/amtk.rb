721d4180b7dab6f93f917d5dba8a0a8052971228
Michka Popoff <michkapopoff@gmail.com>
2022-11-03 22:13:31 +0100

    amtk 5.6.0

--- Formula/amtk.rb
+++ Formula/amtk.rb
@@ -2,8 +2,8 @@ class Amtk < Formula
   desc "Actions, Menus and Toolbars Kit for GNOME"
   homepage "https://gitlab.gnome.org/World/amtk"
   url "https://gitlab.gnome.org/World/amtk.git",
-      tag:      "5.5.2",
-      revision: "3d285cf36ba6caf3db8b2a867144ad13b9961769"
+      tag:      "5.6.0",
+      revision: "2664f5141f097b3df0215454e1aca677a44f1cb4"
   license "LGPL-2.1-or-later"
 
   bottle do
@@ -93,13 +93,9 @@ class Amtk < Formula
       -lgtk-3
       -lpango-1.0
       -lpangocairo-1.0
+      -lamtk-5
     ]
-    if OS.mac?
-      flags << "-lintl"
-      flags << "-lamtk-5.0"
-    else
-      flags << "-lamtk-5"
-    end
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
