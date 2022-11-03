46cf63c526fcf963c9a5358c8282ab112f089a24
FX Coudert <fxcoudert@gmail.com>
2022-11-03 11:29:16 +0000

    scotch: update 7.0.1 bottle.

--- Formula/scotch.rb
+++ Formula/scotch.rb
@@ -12,6 +12,7 @@ class Scotch < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "df7ba27e31b030f41580689a04f1613d6f37fd85a5ed1106ffe27c4e8c37cc22"
     sha256 cellar: :any,                 arm64_monterey: "46a3cc42b5362cb35a182a6b10d77913936c380d09298de39799ac7c7ff95d56"
     sha256 cellar: :any,                 arm64_big_sur:  "be66bc36acfa18ce38d45c642cb83830dd208d91ca3c2612312bae484a6eecfe"
     sha256 cellar: :any,                 monterey:       "57283af1e66679f2137cb7926b1789bf5f6a080eec4b679db53364c3c081d7e1"
