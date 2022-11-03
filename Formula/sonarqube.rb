1c85451b2354720e365a6edd889364b9b28ff3bf
FX Coudert <fxcoudert@gmail.com>
2022-11-03 10:53:02 +0000

    sonarqube: update 9.7.1.62043 bottle.

--- Formula/sonarqube.rb
+++ Formula/sonarqube.rb
@@ -11,6 +11,7 @@ class Sonarqube < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c925ddc78555813a51b084ff9f9d355485ce37e7f4044798fe3942033e6cc5c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8c925ddc78555813a51b084ff9f9d355485ce37e7f4044798fe3942033e6cc5c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c925ddc78555813a51b084ff9f9d355485ce37e7f4044798fe3942033e6cc5c"
     sha256 cellar: :any_skip_relocation, monterey:       "95e45378010f0db53e5969163cfe98fb29b882b1816b5405ff1f1b136c8fdac3"
