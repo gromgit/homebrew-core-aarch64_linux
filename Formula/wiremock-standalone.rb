53ae0166fba1808eeea5e44c5182180e9b76fbe8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-03 05:39:30 +0000

    wiremock-standalone 2.35.0
    Closes #114593.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wiremock-standalone.rb
+++ Formula/wiremock-standalone.rb
@@ -1,8 +1,8 @@
 class WiremockStandalone < Formula
   desc "Simulator for HTTP-based APIs"
   homepage "https://wiremock.org/docs/running-standalone/"
-  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.34.0/wiremock-jre8-standalone-2.34.0.jar"
-  sha256 "7708232b423eb4dca7eb1d9de9dc43693aa465e572fae5f6c46ae1faafae0b48"
+  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.35.0/wiremock-jre8-standalone-2.35.0.jar"
+  sha256 "ae156ae2812e3cfa470c47ed073100ef4ec77927372a4e203f0e3bd531f3eb57"
   license "Apache-2.0"
   head "https://github.com/tomakehurst/wiremock.git", branch: "master"
 
