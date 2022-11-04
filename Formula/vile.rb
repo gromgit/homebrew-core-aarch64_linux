c89c644b6b22ed3607f5cce65ee7bf5c99945a41
Adrian Ho <the.gromgit@gmail.com>
2022-11-04 12:22:02 +0800

    vile: add 9.8w bottle.

---
+++ Formula/vile.rb
@@ -0,0 +1,41 @@
+class Vile < Formula
+  desc "Vi Like Emacs Editor"
+  homepage "https://invisible-island.net/vile/"
+  url "https://invisible-island.net/archives/vile/current/vile-9.8w.tgz"
+  sha256 "78253ec3f7ae5f4f9d4799a3c8bc35b85b47d456f2ac172810008a48e4609815"
+  license "GPL-2.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vile"
+    sha256 aarch64_linux: "c60444f11de06728ecc4fbec31dfb2d0b9e2b97eb16af8b8f5fdaf058bd15f25"
+  end
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "groff" => :build
+  uses_from_macos "expect" => :test
+  uses_from_macos "libxcrypt"
+  uses_from_macos "ncurses"
+  uses_from_macos "perl"
+
+  def install
+    system "./configure", *std_configure_args,
+                          "--disable-imake",
+                          "--enable-colored-menus",
+                          "--with-ncurses",
+                          "--without-x",
+                          "--with-screen=ncurses"
+    system "make", "install"
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    (testpath/"vile.exp").write <<~EOS
+      spawn #{bin}/vile
+      expect "unnamed"
+      send ":w new\r:q\r"
+      expect eof
+    EOS
+    system "expect", "-f", "vile.exp"
+    assert_predicate testpath/"new", :exist?
+  end
+end
