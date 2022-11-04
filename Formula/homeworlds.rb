e24c190de50066b105c9eb1124df4c95713793ce
FX Coudert <fxcoudert@gmail.com>
2022-11-04 15:01:14 +0000

    homeworlds: update 20141022_5 bottle.

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -12,6 +12,7 @@ class Homeworlds < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "9a34aebc0071531698225b1e64a19a45c6eff1efecf4e948ee963d590e18f22f"
     sha256 cellar: :any,                 arm64_monterey: "c64318bee66cc6f65c77c7cb846b87a3a756f0fe92045824f32963341564669d"
     sha256 cellar: :any,                 arm64_big_sur:  "cccfb68554076f1c70337b70ca450779546af81986e81973cb7b25acb9a0220f"
     sha256 cellar: :any,                 monterey:       "4ee671d1292a1e9c8f63ea3e1a40625d20b4349e3a3d188077646936ae9f60c5"
