0f8d9b4be623203bba96a28af99425a70ba1cf81
l5io <47201130+l5io@users.noreply.github.com>
2022-11-04 05:44:00 -0500

    mesheryctl 0.6.22
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #114680.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mesheryctl.rb
+++ Formula/mesheryctl.rb
@@ -2,8 +2,8 @@ class Mesheryctl < Formula
   desc "Command-line utility for Meshery, the cloud native management plane"
   homepage "https://meshery.io"
   url "https://github.com/meshery/meshery.git",
-      tag:      "v0.6.21",
-      revision: "2eface0b00397dd3715d0848e7c849b880bb19ca"
+      tag:      "v0.6.22",
+      revision: "c22c818ad53eb1375721c871daeff09ca5d283fd"
   license "Apache-2.0"
   head "https://github.com/meshery/meshery.git", branch: "master"
 
