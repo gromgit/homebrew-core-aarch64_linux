d2ef5713d5a86ffd42ac3b1a351af6d6e9848298
FX Coudert <fxcoudert@gmail.com>
2022-11-04 12:58:29 +0000

    libspectre: update 0.2.11 bottle.

--- Formula/libspectre.rb
+++ Formula/libspectre.rb
@@ -11,6 +11,7 @@ class Libspectre < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "15c599d638b613e5a478bdbc7f1a80c2e0d1227f268451452eaf80cc41591976"
     sha256 cellar: :any,                 arm64_monterey: "0045e0a03c25d49a3cff1d56651868c0c846da43740c546b2690f8bdce3aafd2"
     sha256 cellar: :any,                 arm64_big_sur:  "4e40717cf4aa9927062522049475f18f31155d2755ec55784adda88310ce0b45"
     sha256 cellar: :any,                 monterey:       "b2e8615f861c91e1f9f7d53902c78c98bd73eadebd5fafa95481b3e321f0b87a"
