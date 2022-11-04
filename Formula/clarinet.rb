5b35b159417b7acb6d8cc02bc3dfe11c5b4140e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-04 09:05:11 +0000

    clarinet: update 1.0.5 bottle.

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -14,12 +14,12 @@ class Clarinet < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e01da9bfc8c422d52e967c31aeed831655d07ec0a635c5d199e72d5458bc7ffe"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2df94e6a478c8b0490bbd8f78af221027f3b7635b9788edbacae865ffab8c067"
-    sha256 cellar: :any_skip_relocation, monterey:       "73ec43cc85a5f158176baef8154583a968334592db942a0512881f2761b4a3e5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bd453704896b6b5d15369e4d791d9cccd40a04f5aff04d4e8e8f572ba4e982f2"
-    sha256 cellar: :any_skip_relocation, catalina:       "eb8aa48251460a9376f903d344ece6112e12879412548a7f0e5e7dedc1d7ec37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7041a63f68ae76c4d83201a16ed52b16db5f0dd58b827c49b961db32fc524a6f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "50a9c0fdecaba5f263f52a5bfb3189b84f3b9e963ded72f026fb90a139d1c6fa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aae62470356a76b342565fa12f455b7e64dfabe633f63bacc79ac71708b017cc"
+    sha256 cellar: :any_skip_relocation, monterey:       "6d9df9c0dc5eba7cb222593390ab0646cc21d4435474eb209f692225f59698af"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fb66e5ba693d7390cfca517bca3823c88d7cae59b72663e71dcaadd310f5f4c"
+    sha256 cellar: :any_skip_relocation, catalina:       "409e951429eb57c85f784a6f3e70abaf97d60fbfc02cfb136d8bb7191b742555"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4135678f61090475e2abc3e37456f8ffe0451c6f7f9fcaa6b2c061d7b483dfab"
   end
 
   depends_on "rust" => :build
