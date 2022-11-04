4d2ac380538078a4f9a18492c7032b7760f65b57
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-11-04 22:16:59 +0800

    gdb: migrate to `python@3.11`
    Closes #114693.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -5,7 +5,7 @@ class Gdb < Formula
   mirror "https://ftpmirror.gnu.org/gdb/gdb-12.1.tar.xz"
   sha256 "0e1793bf8f2b54d53f46dea84ccfd446f48f81b297b28c4f7fc017b818d69fed"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
   bottle do
@@ -18,7 +18,7 @@ class Gdb < Formula
 
   depends_on arch: :x86_64 # gdb is not supported on macOS ARM
   depends_on "gmp"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
   depends_on "xz" # required for lzma support
 
   uses_from_macos "expat"
@@ -51,7 +51,7 @@ class Gdb < Formula
       --disable-debug
       --disable-dependency-tracking
       --with-lzma
-      --with-python=#{Formula["python@3.10"].opt_bin}/python3.10
+      --with-python=#{Formula["python@3.11"].opt_bin}/python3.11
       --disable-binutils
     ]
 
