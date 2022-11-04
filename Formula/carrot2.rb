01318cf1aa08a9a1847332d941123982230267e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-04 09:05:23 +0000

    carrot2: update 4.5.0 bottle.

--- Formula/carrot2.rb
+++ Formula/carrot2.rb
@@ -7,12 +7,12 @@ class Carrot2 < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "970574065c9b7c64597320fb111a7b3b1a3837edc24b492bc8816f250dcc243e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ec9a0fe76ab5eebb90f2431085383eb8983da4b66545dca29c0d945944a1d32f"
-    sha256 cellar: :any_skip_relocation, monterey:       "5b41bac01d5b61f3ec528bf8608db4d83fe2fcc5f75770fc3569d4b6c0f58e62"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5a7c58f94e1ca0dc72fdaba9e6dc108b42baed87d2cd006121770a1e11cbe027"
-    sha256 cellar: :any_skip_relocation, catalina:       "877c092b2e544c553386b4f8d909a2951b5d26385cd612535bba02815fdb4316"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b6682bd0a7e1552935a84fc7aa6e87454792ae32659b22174a5f221c94c88f6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "58c34ba802ae45ae5405161254d3f7cb31dc5cc47877f1f05ab48e39b5192f89"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b24fbd77bb2566fc819c610b88a41423c4e0e9b3278615f593e42d57904b74e8"
+    sha256 cellar: :any_skip_relocation, monterey:       "14b073501ea49f89cc20262478b4021a6b07290711202b66305d193930947d2f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d35c892c8a4f486aed827acb7b760e44303a2d7e7abc4b4087a3e520432dd684"
+    sha256 cellar: :any_skip_relocation, catalina:       "9c66b1e35b1c4eb718c3abae642c34666ea00a223b447b0d02fb81cd7f0183d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "357fb923776eb79ecc87788189897165bf733fd35d0fdd3a94e37cb73333b8d9"
   end
 
   depends_on "gradle" => :build
