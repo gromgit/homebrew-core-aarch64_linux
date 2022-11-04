6628953cfe142f98a0a5571a0a3cd1dcf075d033
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-04 21:05:01 +0000

    ungit: update 1.5.22 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -8,12 +8,12 @@ class Ungit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7fbd45d11df1c57d30d8d5e7a18e08711b429fe7f96237822b6c97282ee3dfd7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7fbd45d11df1c57d30d8d5e7a18e08711b429fe7f96237822b6c97282ee3dfd7"
-    sha256 cellar: :any_skip_relocation, monterey:       "b30195eb0fe0cab7e8e4bb1dcfe53c484647f2eebce8f49385a4e46523b4b4ef"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b30195eb0fe0cab7e8e4bb1dcfe53c484647f2eebce8f49385a4e46523b4b4ef"
-    sha256 cellar: :any_skip_relocation, catalina:       "b30195eb0fe0cab7e8e4bb1dcfe53c484647f2eebce8f49385a4e46523b4b4ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7fbd45d11df1c57d30d8d5e7a18e08711b429fe7f96237822b6c97282ee3dfd7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7bc9e0f1d56b4ac32091c4f3fccfbedd4afadf82b7afe9e20e061afebed1aa30"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7bc9e0f1d56b4ac32091c4f3fccfbedd4afadf82b7afe9e20e061afebed1aa30"
+    sha256 cellar: :any_skip_relocation, monterey:       "b2e40ec003b25b08144d36acd844a9f4061b303a4007f076d4851d23efa83255"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b2e40ec003b25b08144d36acd844a9f4061b303a4007f076d4851d23efa83255"
+    sha256 cellar: :any_skip_relocation, catalina:       "b2e40ec003b25b08144d36acd844a9f4061b303a4007f076d4851d23efa83255"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7bc9e0f1d56b4ac32091c4f3fccfbedd4afadf82b7afe9e20e061afebed1aa30"
   end
 
   depends_on "node"
