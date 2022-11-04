fe366122593f7d2b2060a63156ba2b6f3a684329
Adrian Ho <the.gromgit@gmail.com>
2022-11-05 00:05:56 +0800

    x86_64-linux-gnu-binutils: add 2.39 bottle.

---
+++ Formula/x86_64-linux-gnu-binutils.rb
@@ -0,0 +1,59 @@
+class X8664LinuxGnuBinutils < Formula
+  desc "GNU Binutils for x86_64-linux-gnu cross development"
+  homepage "https://www.gnu.org/software/binutils/binutils.html"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
+  sha256 "645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    formula "binutils"
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/x86_64-linux-gnu-binutils"
+    sha256 aarch64_linux: "acf162d3c1ca296ad16ae227f86eb83ec1b5de978490b6574107d7732019f35a"
+  end
+
+  uses_from_macos "texinfo"
+
+  on_linux do
+    keg_only "it conflicts with `binutils`"
+  end
+
+  def install
+    ENV.cxx11
+
+    # Avoid build failure: https://sourceware.org/bugzilla/show_bug.cgi?id=23424
+    ENV.append "CXXFLAGS", "-Wno-c++11-narrowing"
+
+    target = "x86_64-linux-gnu"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-deterministic-archives",
+                          "--prefix=#{prefix}",
+                          "--libdir=#{lib/target}",
+                          "--infodir=#{info/target}",
+                          "--disable-werror",
+                          "--target=#{target}",
+                          "--enable-gold=yes",
+                          "--enable-ld=yes",
+                          "--enable-interwork",
+                          "--with-system-zlib",
+                          "--disable-nls",
+                          "--disable-gprofng" # Fails to build on Linux
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "f()", shell_output("#{bin}/x86_64-linux-gnu-c++filt _Z1fv")
+    return if OS.linux?
+
+    (testpath/"hello.c").write <<~EOS
+      void hello() {}
+    EOS
+    system ENV.cc, "--target=x86_64-pc-linux-gnu", "-c", "hello.c"
+    assert_match "hello", shell_output("#{bin}/x86_64-linux-gnu-nm hello.o")
+  end
+end
