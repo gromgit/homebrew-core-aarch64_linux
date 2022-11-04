8752e8b80c5404901e6ed547e4a4186cda883821
Adrian Ho <the.gromgit@gmail.com>
2022-11-04 22:30:22 +0800

    vulkan-loader: add 1.3.226 bottle.

---
+++ Formula/vulkan-loader.rb
@@ -0,0 +1,57 @@
+class VulkanLoader < Formula
+  desc "Vulkan ICD Loader"
+  homepage "https://github.com/KhronosGroup/Vulkan-Loader"
+  url "https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.3.226.tar.gz"
+  sha256 "992d8c7c9f85ac9e8a261a448a50f0ae602ac17964c1b2550894fc128c037226"
+  license "Apache-2.0"
+  head "https://github.com/KhronosGroup/Vulkan-Loader.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vulkan-loader"
+    sha256 aarch64_linux: "b52764f303d6fd21a037c1c7ddc05eaa1de2a18ac6e8e0f127a6d2061b85a0f0"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
+  depends_on "vulkan-headers"
+
+  on_linux do
+    depends_on "libxrandr" => :build
+    depends_on "libx11"
+    depends_on "libxcb"
+    depends_on "wayland"
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DVULKAN_HEADERS_INSTALL_DIR=#{Formula["vulkan-headers"].opt_prefix}",
+                    "-DFALLBACK_DATA_DIRS=#{HOMEBREW_PREFIX}/share:/usr/local/share:/usr/share",
+                    "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}",
+                    "-DFALLBACK_CONFIG_DIRS=#{etc}/xdg:/etc/xdg",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    inreplace lib/"pkgconfig/vulkan.pc", /^Cflags: .*/, "Cflags: -I#{Formula["vulkan-headers"].opt_include}"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <vulkan/vulkan_core.h>
+      int main() {
+        uint32_t version;
+        vkEnumerateInstanceVersion(&version);
+        return (version >= VK_API_VERSION_1_1) ? 0 : 1;
+      }
+    EOS
+    system ENV.cc, "-o", "test", "test.c", "-I#{Formula["vulkan-headers"].opt_include}",
+                   "-L#{lib}", "-lvulkan"
+    system "./test"
+  end
+end
