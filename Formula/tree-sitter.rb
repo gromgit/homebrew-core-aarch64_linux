c11ea6ab4f56d690ace29018064b9c0eb0d15039
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-05 10:51:20 +0000

    tree-sitter: update 0.20.7 bottle.

--- Formula/tree-sitter.rb
+++ Formula/tree-sitter.rb
@@ -15,6 +15,7 @@ class TreeSitter < Formula
     sha256 cellar: :any,                 arm64_big_sur:  "ba68148d99a41b39d6fdd5c6461064daf0903d06911ed2970f4a04dc49691ab2"
     sha256 cellar: :any,                 monterey:       "e251026a9bbe2ea09165376223c4c82766ce98f7f8393b479d03a7474c61034b"
     sha256 cellar: :any,                 big_sur:        "5beb68d159b38a6712f0fc5acf7b26dc7bf655ce00f8934bf8e5ac043a80932f"
+    sha256 cellar: :any,                 catalina:       "5e8d09ddfe2b847f6d4a838d38aed711a282ea358a5ed2a09ec92ab5e14e7925"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ee8f6010f5e2e2c94c03ab8375a33f6df5fdea676cdce67f08d8876769e4102"
   end
 
