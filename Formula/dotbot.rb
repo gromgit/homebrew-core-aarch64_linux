ada2c2892a930a9212f3db0b3fc1390ea90542b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-05 14:04:38 +0000

    dotbot: update 1.19.0_1 bottle.

--- Formula/dotbot.rb
+++ Formula/dotbot.rb
@@ -9,13 +9,13 @@ class Dotbot < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8e4681de4209b0622d9523124931dc7f8ead206d8768e8b934af4bce1149ee1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e5418a009f3e05c563ada0705c6e077d56e4df49ab42f1496ef43e097dc6f1d"
-    sha256 cellar: :any_skip_relocation, monterey:       "b7cf1d41236d1358f46b52827a24c323311e151839391a808472032ef6c3e1b7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4eed817693537664519d582af946fa870d40c8c584b9706d9c4742ebe13ba8e4"
-    sha256 cellar: :any_skip_relocation, catalina:       "1580a56ae5c22273b996866519a938da04c8fec7adc805f435399b954bebb9df"
-    sha256 cellar: :any_skip_relocation, mojave:         "fdb6be14bd6a4ad73c2ca13c3f5a62cc74bb64ff507278a99d4e0b944e09dd75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0e8e2840e9c40ac40da72cf275aebbc2365a21a4874b8c040109667e1c004b6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e69e86421ab5c05c270cbcc228a5e0756d0d016c204ef6d1a3be5cf4b57f250a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "423dfff4e1cab4c57c73d8636c28e611ced0ebfbcad10fe222497de2c7138c6e"
+    sha256 cellar: :any_skip_relocation, monterey:       "ef6c5cd693bbfb4d3f1bac46042143460f071b488ab008b1f761db1f99d986e5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "44ac111a5f85630d40605cc5a924283a332bf2a8d8dc0b3a5e626c771d286c7e"
+    sha256 cellar: :any_skip_relocation, catalina:       "965c56d812816e5257fa6fc1380b3a7a63dd205c473358f73a12fe590031749f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a93db285c5f1670d5904594702bf19bab0429cde0cda6cd0f73ae740e6d51b1"
   end
 
   depends_on "python@3.11"
