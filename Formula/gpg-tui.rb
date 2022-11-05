1d1cbd0e8b9103205fab0ee073b4d5705ff346fd
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-05 23:33:52 +0000

    gpg-tui: update 0.9.1 bottle.

--- Formula/gpg-tui.rb
+++ Formula/gpg-tui.rb
@@ -12,6 +12,7 @@ class GpgTui < Formula
     sha256 cellar: :any,                 arm64_big_sur:  "2a4b61ff64d4d8bdaf725b76f84a0e9e630736be623dcd954952ef4799d2b56b"
     sha256 cellar: :any,                 monterey:       "028ca91850653b792a6e856af4296e14519f80f3c4d7cee5d8fa8f00824cd148"
     sha256 cellar: :any,                 big_sur:        "0f6d8d0be8a041ad99134aa3892258448e48574fcc213c2dd7789fb4eac36843"
+    sha256 cellar: :any,                 catalina:       "b6d68c912426c0535ddbb889d09132450576319f23dc67869f14c6ef9f2422e8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "334fd7671f9e706fa037f0f9d500705a63261a22b9fe27062d7ad2507fbbec08"
   end
 
