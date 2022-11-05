aeec088ff8408f219008abc9c095c8491384e0bb
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-05 21:52:13 +0000

    lucky-commit: update 2.2.1 bottle.

--- Formula/lucky-commit.rb
+++ Formula/lucky-commit.rb
@@ -6,12 +6,12 @@ class LuckyCommit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d09b8c8990932d835c997abaedd007136627d5f2a0d708620fd8fecd8702ec70"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06ba55d31250dfaf342d2f9a4e37158555844b1949e6fe2abad7220fb64be8ff"
-    sha256 cellar: :any_skip_relocation, monterey:       "c47445b77ad8843994438d5bee1ea191029159fca8c019619fa586eff2caddbb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a016f90b4168d4ac97bb303ae86fecbb27b873a7677134537bd44509c6f96573"
-    sha256 cellar: :any_skip_relocation, catalina:       "a54c93a1164be44359e8626cfdd8ce6ac07e7f0e78c8b53d4618593b20c5026b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4ead90e0bb96f4df966b4222db8e4a7543636a8f2ced16666f86c662f8f12de"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "afbb8c5a16be111fab8b75cc681a1e9c6168d2ecc4c21971dece357eb27151a9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1dd946763d4e3f0d2e2d0f4c229c77073517d77e34d3326f49ac3320e3c0a55e"
+    sha256 cellar: :any_skip_relocation, monterey:       "92b4016661dad378b653827fec88044f9fd02d50b94123b9803b1817bc3259b7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fd44bd8530240c80bc0d9c1bf1e7976c51349fd985339d7d04d743aa6435cac0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fbf3f8c0e066f365cdbf0d5491fcad47729f1ead95f7bafa9cc282fe2e46ae11"
   end
 
   depends_on "rust" => :build
