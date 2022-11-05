673af97419657389c3fa638619bac48dfce2c918
FX Coudert <fxcoudert@gmail.com>
2022-11-05 09:52:43 +0000

    bombadillo: update 2.4.0 bottle.

--- Formula/bombadillo.rb
+++ Formula/bombadillo.rb
@@ -6,6 +6,7 @@ class Bombadillo < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5b792d8b1afcc6cb894aec708268f8099e8b70a3f8a629db32d0290a535a6800"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d78dae1cc03b2b801036ba0dfd19dbb9d47e491acac9ad1ea141a40eb6b4961b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "02c4423c500ab7e3fb19df4e68708cb9ded9197986aae6e1783cb3a79d592fd7"
     sha256 cellar: :any_skip_relocation, monterey:       "ae21a5cc0383684f7080eb62468d91db698f901c3ce4332e933b5a72edce3041"
