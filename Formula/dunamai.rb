7a9fe7baed7995edbf2cfca31f1c43389e894ad1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-05 14:04:21 +0000

    dunamai: update 1.13.2 bottle.

--- Formula/dunamai.rb
+++ Formula/dunamai.rb
@@ -8,12 +8,13 @@ class Dunamai < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "02a1f60f406aa4b3075b66da7e1324d202a691f6edac42d7a09a3a5eb5a710e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "32dc9a51081509b0546d15116c051c33066f455285e664b26ea9e845dae02ac2"
-    sha256 cellar: :any_skip_relocation, monterey:       "f53a7498d428a87fa14a3df2fb14e0c329a13b87a587edb8fa6b0d0876e2d4a6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "388454a8f4c91719e6a3c92f465f450bc449d536f07407264285fc4a46298bd3"
-    sha256 cellar: :any_skip_relocation, catalina:       "fd4271271768dad024d38cdc81b867a547fc26f232d1502a41ef5aca8efa50e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e076a4f06aa7278da76c4493dfc4639ec83c0ca43d896603132aa9994ab35579"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0850c17af9cbad57ab4611741dadb72f6cd2559eff2ba8e0a5c14817823b827d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2c82ea726f02a842d74493e90d9566ff69d37a1182ee988b25c21f4eb240463e"
+    sha256 cellar: :any_skip_relocation, monterey:       "0d1e427ec91078c57efdaf7721475e34767a0213eea845733bc465630f53c533"
+    sha256 cellar: :any_skip_relocation, big_sur:        "95ca5460d458824b3acc034594b0f947eaa06dd4839ecef8b15838d9ad6d9c12"
+    sha256 cellar: :any_skip_relocation, catalina:       "d900694db5b62d116ba578108769463721d4d3e196bed84fdbd156acde01fe10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc5072f604983ec21eb721eb613f8d55745346a478da4f5b7ca358bf4f225aba"
   end
 
   depends_on "python@3.11"
