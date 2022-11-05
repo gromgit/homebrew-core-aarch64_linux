bff3c2d31e2670176324de85425a60e4f1464ab2
Michael Cho <cho-m@tuta.io>
2022-11-04 23:43:19 -0700

    compiledb: migrate to `python@3.11`
    Closes #114789.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/compiledb.rb
+++ Formula/compiledb.rb
@@ -3,8 +3,8 @@ class Compiledb < Formula
 
   desc "Generate a Clang compilation database for Make-based build systems"
   homepage "https://github.com/nickdiego/compiledb"
-  url "https://github.com/nickdiego/compiledb/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "3f288e4897e2b17b4dd8070d3ad9e9fc627961faa4d0be29a78f6c619e055f36"
+  url "https://files.pythonhosted.org/packages/76/62/30fb04404b1d4a454f414f792553d142e8acc5da27fddcce911fff0fe570/compiledb-0.10.1.tar.gz"
+  sha256 "06bb47dd1fa04de3a12720379ff382d40441074476db7c16a27e2ad79b7e966e"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -16,16 +16,16 @@ class Compiledb < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "fac936844ac832c53bf10ff12b2104e224496c08e632cd11465580ef7f74b456"
   end
 
-  depends_on "python@3.10"
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/dd/cf/706c1ad49ab26abed0b77a2f867984c1341ed7387b8030a6aa914e2942a0/click-8.0.4.tar.gz"
-    sha256 "8458d7b1287c5fb128c90e23381cf99dcde74beaf6c7ff6384ce84d6fe090adb"
-  end
+  depends_on "python@3.11"
 
   resource "bashlex" do
     url "https://files.pythonhosted.org/packages/1b/57/8de844f7702f644382def6aee76c64da5a1acfbc22a23ffbc565e0ec69cd/bashlex-0.16.tar.gz"
     sha256 "dc6f017e49ce2d0fe30ad9f5206da9cd13ded073d365688c9fda525354e8c373"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
   end
 
   resource "shutilwhich" do
