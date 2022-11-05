028ff7f516cf5ac88ba52aeb889ffa8bd43752f8
Adrian Ho <the.gromgit@gmail.com>
2022-11-05 09:20:27 +0800

    svt-av1: add 1.2.1 bottle.

---
+++ Formula/svt-av1.rb
@@ -0,0 +1,34 @@
+class SvtAv1 < Formula
+  desc "AV1 encoder"
+  homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.2.1/SVT-AV1-v1.2.1.tar.gz"
+  sha256 "5c2466eb2990fa045b29c63b92a2b5f13d9cb841709df5e7206f96a59e119fdd"
+  license "BSD-3-Clause"
+  head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/svt-av1"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "474ee5a4556f263b3d09d15f8c77acf1aaae5bb8b519870eefec1accf07f38dd"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "make" => :build
+  depends_on "yasm" => :build
+
+  resource("homebrew-testvideo") do
+    url "https://github.com/grusell/svt-av1-homebrew-testdata/raw/main/video_64x64_yuv420p_25frames.yuv"
+    sha256 "0c5cc90b079d0d9c1ded1376357d23a9782a704a83e01731f50ccd162e246492"
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    testpath.install resource("homebrew-testvideo")
+    system "#{bin}/SvtAv1EncApp", "-w", "64", "-h", "64", "-i", "video_64x64_yuv420p_25frames.yuv", "-b", "output.ivf"
+    assert_predicate testpath/"output.ivf", :exist?
+  end
+end
