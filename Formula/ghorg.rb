524555eff082a3d18e710380077561ef79582a3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-05 18:05:20 +0000

    ghorg: update 1.9.0 bottle.

--- Formula/ghorg.rb
+++ Formula/ghorg.rb
@@ -6,12 +6,12 @@ class Ghorg < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "49eafa7e06e16af42ab9d5e8a9ece96fe555d0c3cca35534536775159dbe4269"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "439ac086e1e9daa604f91295e51247ba63f89337f0ad22fcdb9318be806db7d0"
-    sha256 cellar: :any_skip_relocation, monterey:       "bcf5feb80b5ba09f63d512f1865342efd885f21423b5a94e0209db4b894cda19"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1775f0fd6b529a23f0f0242bc550ea2407b1d15aa6ed48fff03615eedc0789cc"
-    sha256 cellar: :any_skip_relocation, catalina:       "e1ae2db1fb8478bfbaddbf35cb45dd77f7353ce0ade0ed4b493d27a72e370896"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "81ac70d4e1b893236cfd0818aaa2ebab1edf48c242d519d82dc9d6698d9a50d7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1365ebe4b2b9464c0f4ffbfa6de06427b2c873e58eed909a0ced58ef90dda9ef"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a09f0a5a0a729d9845678a06c65a07702c3c35a547dc64a3f97d8f214907f6a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4f740b06c5164dcd8f77d1759f7085b481730050773c4a496e4e17303f844e04"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fc3c858f77bedf850b63ad4204ab06b6f73e6ba732ab0a5fd1e1a6bd1115391"
+    sha256 cellar: :any_skip_relocation, catalina:       "afca8aef59beca91c86731bad3b650a935092d58885ba1afd855ba6c59281666"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06a473c3bdabf00fd8b2ce5cb7578e1c7e73968951fef32f7944185ae4c2fe94"
   end
 
   depends_on "go" => :build
