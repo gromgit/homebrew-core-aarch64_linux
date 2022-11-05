8ee61f91c4d0e6890af2caf79e1d5e93770f91ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-05 05:04:22 +0000

    pspg: update 5.5.9 bottle.

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -7,13 +7,12 @@ class Pspg < Formula
   head "https://github.com/okbob/pspg.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "2c11fcf1e586dc54f41705e9f718b0d1772d379d8c66c3a45a8a97a6df7b2108"
-    sha256 cellar: :any,                 arm64_monterey: "4e09ad19e338456249f1d2ff3862fbb059c7704609382686edb8d4b781cf1187"
-    sha256 cellar: :any,                 arm64_big_sur:  "37c0075d494f3fc6f11c27e22862efaf5215ecb423a6576e52e52fbd96cbaab9"
-    sha256 cellar: :any,                 monterey:       "ecb371c2c95df0171e46b857dbb355e54a1cd9e0be8e1513c2583b23a13e3ad1"
-    sha256 cellar: :any,                 big_sur:        "8d084ec1ee67984c8ea873ec94440a84cc06dd2b442f50bf7afb14f9029ede93"
-    sha256 cellar: :any,                 catalina:       "d58be0e4dcdd8a464170920889472cd9c258fb787593840aacf38a933ca3f571"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c39c97a8801bac725baf6d5321e205bda40ac696ea95b11bcc0f5376803faef4"
+    sha256 cellar: :any,                 arm64_monterey: "c54323ab59bb9bbe4671c924c1a646a68b19644e3b70882bbd487dc52941aa12"
+    sha256 cellar: :any,                 arm64_big_sur:  "fae746dcb591bd247afa1b9eb14f3e157ca6131e9c69011250cb618b2fe17042"
+    sha256 cellar: :any,                 monterey:       "282dc7b0f0b6091d9e2d1c8549219111a9588a15ad5867fde89c11d31f04c591"
+    sha256 cellar: :any,                 big_sur:        "73feabf78442b692961e82883b976ae900f9ec16baadbae6807fce550c54f898"
+    sha256 cellar: :any,                 catalina:       "c337e4e6760e5cea96639e38c071daf4b3bd0c24890582ddbcde2c8e24853c42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5e31c7349a791d4cf2ee07ba725f1527876dd0019505a2d556562fae5ca340d6"
   end
 
   depends_on "libpq"
