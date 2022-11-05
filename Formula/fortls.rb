35ed1553bdca50839a8f0b9f99ca167e2436cbf3
Michael Cho <cho-m@tuta.io>
2022-11-05 00:51:17 -0700

    fortls: migrate to `python@3.11`

--- Formula/fortls.rb
+++ Formula/fortls.rb
@@ -3,7 +3,7 @@ class Fortls < Formula
 
   desc "Fortran language server"
   homepage "https://gnikit.github.io/fortls"
-  url "https://pypi.io/packages/source/f/fortls/fortls-2.13.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/38/04/db988efbcaac142999af91888e9750dfa422108a318ec3038c2cd42ecf04/fortls-2.13.0.tar.gz"
   sha256 "23c5013e8dd8e1d65bf07be610d0827bc48aa7331a7a7ce13612d4c646d0db31"
   license "MIT"
   head "https://github.com/gnikit/fortls.git", branch: "master"
@@ -17,20 +17,22 @@ class Fortls < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d83b540e20a624338af94720bd2322daa2bc5443923f9fe8f7ca6b0441a41ccb"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
+
+  conflicts_with "fortran-language-server", because: "both install `fortls` binaries"
 
   resource "json5" do
-    url "https://pypi.io/packages/source/j/json5/json5-0.9.5.tar.gz"
-    sha256 "703cfee540790576b56a92e1c6aaa6c4b0d98971dc358ead83812aa4d06bdb96"
+    url "https://files.pythonhosted.org/packages/47/12/611bf15000c1fc54af909565aed1ad045e5ae1890d8c56cbfe5ceaf52446/json5-0.9.10.tar.gz"
+    sha256 "ad9f048c5b5a4c3802524474ce40a622fae789860a86f10cc4f7e5f9cf9b46ab"
   end
 
   resource "packaging" do
-    url "https://pypi.io/packages/source/p/packaging/packaging-21.3.tar.gz"
+    url "https://files.pythonhosted.org/packages/df/9e/d1a7217f69310c1db8fdf8ab396229f55a699ce34a203691794c5d1cad0c/packaging-21.3.tar.gz"
     sha256 "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
   end
 
   resource "pyparsing" do
-    url "https://pypi.io/packages/source/p/pyparsing/pyparsing-3.0.9.tar.gz"
+    url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
     sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
   end
 
@@ -38,7 +40,7 @@ class Fortls < Formula
     virtualenv_install_with_resources
 
     # Disable automatic update check
-    rm bin/"fortls"
+    (bin/"fortls").unlink
     # Replace with `exec python3 -m fortls --disable_autoupdate "$@"` in the future
     (bin/"fortls").write <<~EOS
       #!#{libexec}/bin/python3
