ef531ba6638006b4697e56e7069c96cd647e8243
FX Coudert <fxcoudert@gmail.com>
2022-11-06 10:22:07 +0000

    tygo: update 0.2.4 bottle.

--- Formula/tygo.rb
+++ Formula/tygo.rb
@@ -8,6 +8,7 @@ class Tygo < Formula
   head "https://github.com/gzuidhof/tygo.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "db9a6146935d59a4e14b84db25e5c9b1de743fb6cf8b73f65a9a30fffb00f26e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "81d8d41d0e3ccabdf9415f2b96d371ae3fdbf8137a18b508747133b0bdd735f7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "31db2b9d86d64a8c3245eeb2302a12fc2763a624fca3142c14712596a4ed0cfc"
     sha256 cellar: :any_skip_relocation, monterey:       "a85c0d826da43108c7ffe8ec5d896b852a2e6d7ca53f342a1fb6eac802d33115"
