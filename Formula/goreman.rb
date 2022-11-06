ee255d8a885d0c820c200f06c13169254c1a88c1
FX Coudert <fxcoudert@gmail.com>
2022-11-06 21:08:45 +0000

    goreman: update 0.3.13 bottle.

--- Formula/goreman.rb
+++ Formula/goreman.rb
@@ -12,6 +12,7 @@ class Goreman < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c095f2360084a1477500f8d6684accceb6f09b22805a8d44ad08f9a64753ef7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "986155fa8952bfc2f2e642a3dd8fef50d273fe086aa7a4c33774a4d13f4eca18"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4b24f1d784da2b808ef927b8432d0a8cc12ba573f4a5e714be0eaeb735eb8c1"
     sha256 cellar: :any_skip_relocation, monterey:       "8a43d0685955570af59741794d37c4b215860b2b3c9d60823774b2c047a1cf98"
