828c002033f2733e74eff37471bc319420a8bdb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-06 23:04:40 +0000

    eslint: update 8.27.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -8,13 +8,12 @@ class Eslint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "30de8887ea7c8726a6c923ff39687e6e3c82aa841cc5c5f5cc09f87670352bdf"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "30de8887ea7c8726a6c923ff39687e6e3c82aa841cc5c5f5cc09f87670352bdf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30de8887ea7c8726a6c923ff39687e6e3c82aa841cc5c5f5cc09f87670352bdf"
-    sha256 cellar: :any_skip_relocation, monterey:       "6cce5393d79bbd5ff611914701b0eae148867d599aae9070077ed76b5c628f03"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6cce5393d79bbd5ff611914701b0eae148867d599aae9070077ed76b5c628f03"
-    sha256 cellar: :any_skip_relocation, catalina:       "6cce5393d79bbd5ff611914701b0eae148867d599aae9070077ed76b5c628f03"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "30de8887ea7c8726a6c923ff39687e6e3c82aa841cc5c5f5cc09f87670352bdf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f59f8b3d88248759ca8af3677483185d2da7f990ed0963232a2fde8fe52ba295"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f59f8b3d88248759ca8af3677483185d2da7f990ed0963232a2fde8fe52ba295"
+    sha256 cellar: :any_skip_relocation, monterey:       "a326b5b84a6f3b1a40193f35d5a2918c54442106a9a493623d8682422662edef"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a326b5b84a6f3b1a40193f35d5a2918c54442106a9a493623d8682422662edef"
+    sha256 cellar: :any_skip_relocation, catalina:       "a326b5b84a6f3b1a40193f35d5a2918c54442106a9a493623d8682422662edef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f59f8b3d88248759ca8af3677483185d2da7f990ed0963232a2fde8fe52ba295"
   end
 
   depends_on "node"
