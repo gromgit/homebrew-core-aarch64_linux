cef3488340f3206881fe9cb85829da75d0ae9511
Rui Chen <rui@chenrui.dev>
2022-11-06 23:05:33 +0000

    prql-compiler: update 0.2.9 bottle.

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -6,6 +6,7 @@ class PrqlCompiler < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "152011c66588e571f3d6def4f38578ab49d08e7de2060a9feea05e528b2217f7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7cbb0274e20988a91eee86c4ed4d56f396cbb934687dcbff318304e9402f55dd"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "882936167e19c270b32b88c7e4eed201444c9eb08825b479125dde61985b10d7"
     sha256 cellar: :any_skip_relocation, monterey:       "523c58eaea44cc264cdfa1ad0b540b0a8e92bc5bc99ec6b018642993ad0fa9bf"
