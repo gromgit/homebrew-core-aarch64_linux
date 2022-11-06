132677a1c2d9c287530a28ae37038c68fdf6a8e5
FX Coudert <fxcoudert@gmail.com>
2022-11-06 20:22:02 +0000

    ghostunnel: update 1.6.1 bottle.

--- Formula/ghostunnel.rb
+++ Formula/ghostunnel.rb
@@ -7,6 +7,7 @@ class Ghostunnel < Formula
   head "https://github.com/ghostunnel/ghostunnel.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a99bf74685bfeaab77b8b295bcf8358dcafb99cb718dadaeae2711974aea6430"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fe484f4f7c5968a90425f71cf3f1c1eb90f16d10110fc0673b53ce2becf13b2b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40c23eb108c2d95ef199c0f41954b7880ec46592d0569d7fa062a2b1636eaa8c"
     sha256 cellar: :any_skip_relocation, monterey:       "45894dab1081ca1e552e394f1bb9fd2c0588c81be8b98aeff0dad95fc8ad3966"
