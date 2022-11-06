17593da1257c9a295da2468875b962c09f86dae4
Morten Kristensen <me@mortens.dev>
2022-11-06 19:02:53 +0100

    vermin 1.5.0
    * Vermin 1.5.0
    * Depend on Python 3.11
    
    Closes #114936.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vermin.rb
+++ Formula/vermin.rb
@@ -3,8 +3,8 @@ class Vermin < Formula
 
   desc "Concurrently detect the minimum Python versions needed to run code"
   homepage "https://github.com/netromdk/vermin"
-  url "https://github.com/netromdk/vermin/archive/v1.4.2.tar.gz"
-  sha256 "c9a69420b610bfb25d5a2abd7da6edf0ae4329481a857ef6c5d71f602ed5c63d"
+  url "https://github.com/netromdk/vermin/archive/v1.5.0.tar.gz"
+  sha256 "77207385c9cea1f02053a8f2e7f2e8c945394cf37c44c70ce217cada077a2d17"
   license "MIT"
   head "https://github.com/netromdk/vermin.git", branch: "master"
 
@@ -17,14 +17,14 @@ class Vermin < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "42de1cb7e7aeca2c0e28a507212f4ed1984ad594ad5d118449151d3f50caa478"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    path = libexec/Language::Python.site_packages("python3.10")/"vermin"
+    path = libexec/Language::Python.site_packages("python3.11")/"vermin"
     assert_match "Minimum required versions: 2.7, 3.0", shell_output("#{bin}/vermin #{path}")
   end
 end
