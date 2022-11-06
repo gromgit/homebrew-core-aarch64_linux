b7d737f708a17a01d30fbf75add3294c8f8d6402
Bo Anderson <mail@boanderson.me>
2022-11-06 16:30:16 +0000

    qtkeychain: restore linux bottle
    This reverts commit 58e1f37ced9cf750f14b57d824e51bb6603b1664.

--- Formula/qtkeychain.rb
+++ Formula/qtkeychain.rb
@@ -7,12 +7,13 @@ class Qtkeychain < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "5d3145d8d3731343964b40bdb1c13c4e53e9be8322d32dadafe8c7d7875c4888"
-    sha256 cellar: :any, arm64_monterey: "f40b525d2f5a5e6fc334593e7da4151872bc49d5dce33dd2a9964ece65691734"
-    sha256 cellar: :any, arm64_big_sur:  "aa11a27547e6206efae29567019190a1ca94d50d3d241689a1f157fd70e2f962"
-    sha256 cellar: :any, monterey:       "29f4b26ba055523d59cdf5e800a4402de27870cd2cbf938b8190a8ad3b7bb4f7"
-    sha256 cellar: :any, big_sur:        "b27da2f84bb0b2357dc6c7274d63b00eb833aea7cc6dfe9126697993291193a8"
-    sha256 cellar: :any, catalina:       "2ec32ec391cfdf76856c5b60e9b7d3d4e157e04c1925cd617d465ca8e916b349"
+    sha256 cellar: :any,                 arm64_ventura:  "5d3145d8d3731343964b40bdb1c13c4e53e9be8322d32dadafe8c7d7875c4888"
+    sha256 cellar: :any,                 arm64_monterey: "f40b525d2f5a5e6fc334593e7da4151872bc49d5dce33dd2a9964ece65691734"
+    sha256 cellar: :any,                 arm64_big_sur:  "aa11a27547e6206efae29567019190a1ca94d50d3d241689a1f157fd70e2f962"
+    sha256 cellar: :any,                 monterey:       "29f4b26ba055523d59cdf5e800a4402de27870cd2cbf938b8190a8ad3b7bb4f7"
+    sha256 cellar: :any,                 big_sur:        "b27da2f84bb0b2357dc6c7274d63b00eb833aea7cc6dfe9126697993291193a8"
+    sha256 cellar: :any,                 catalina:       "2ec32ec391cfdf76856c5b60e9b7d3d4e157e04c1925cd617d465ca8e916b349"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d5e030c29fef9faef28773bd69d1334596a524679c65b8166ae5b1f2b96ce78"
   end
 
   depends_on "cmake" => :build
