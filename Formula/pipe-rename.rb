ac62744faae6e6cc73170c3443c26747f4f353d5
Rui Chen <rui@chenrui.dev>
2022-11-06 23:04:21 +0000

    pipe-rename: update 1.6.0 bottle.

--- Formula/pipe-rename.rb
+++ Formula/pipe-rename.rb
@@ -6,6 +6,7 @@ class PipeRename < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ce60f3e62a28dbbbb19113b034164b78d02cce187a51a648be8f7d6520e4ac3c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b754ffa3266ebab2ac8102b52903848c9a42f3e9ce9b771ef18c425917532abe"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "33b109fd2995423dd435d78b01597bdc61616171e53747a5e3cf06c5494d890d"
     sha256 cellar: :any_skip_relocation, monterey:       "6cc3a4c5f98ef69e776fff5e0968c74cb4fe65392f91f2d8130ebb2b945025c2"
