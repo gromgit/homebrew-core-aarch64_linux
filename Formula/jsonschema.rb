414a8e7a7848df6a9926cbf68ce129ca1ba18cba
Dulmandakh <dulmandakh@gmail.com>
2022-11-06 20:22:40 +0800

    jsonschema: remove depends_on six
    Closes #114911.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jsonschema.rb
+++ Formula/jsonschema.rb
@@ -19,7 +19,6 @@ class Jsonschema < Formula
   end
 
   depends_on "python@3.10"
-  depends_on "six"
 
   resource "attrs" do
     url "https://files.pythonhosted.org/packages/1a/cb/c4ffeb41e7137b23755a45e1bfec9cbb76ecf51874c6f1d113984ecaa32c/attrs-22.1.0.tar.gz"
