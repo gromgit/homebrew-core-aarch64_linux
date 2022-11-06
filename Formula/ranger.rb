f3247bb5b6a8b9bca0d2fef25fcf0ccc97e0149c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-06 12:05:42 +0000

    ranger: update 1.9.3_2 bottle.

--- Formula/ranger.rb
+++ Formula/ranger.rb
@@ -10,14 +10,13 @@ class Ranger < Formula
   head "https://github.com/ranger/ranger.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a8e823cd40ee0b37e7b33327de1d4c55d8558d20abd380b3df1b47544bb327e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2972a3ffed7cb61dcd1abe64cb6d24b902ffc50ef78e10fc279ebda56175a1d8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2972a3ffed7cb61dcd1abe64cb6d24b902ffc50ef78e10fc279ebda56175a1d8"
-    sha256 cellar: :any_skip_relocation, monterey:       "12656acfac655b9a648d8cb877ef38fd6ef644f74cb182cff4075b333523d996"
-    sha256 cellar: :any_skip_relocation, big_sur:        "12656acfac655b9a648d8cb877ef38fd6ef644f74cb182cff4075b333523d996"
-    sha256 cellar: :any_skip_relocation, catalina:       "12656acfac655b9a648d8cb877ef38fd6ef644f74cb182cff4075b333523d996"
-    sha256 cellar: :any_skip_relocation, mojave:         "12656acfac655b9a648d8cb877ef38fd6ef644f74cb182cff4075b333523d996"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2972a3ffed7cb61dcd1abe64cb6d24b902ffc50ef78e10fc279ebda56175a1d8"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a4d109e42f3146daeb999e613a9cedca98cbd851f98f950f54cfaab5e872c58"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a4d109e42f3146daeb999e613a9cedca98cbd851f98f950f54cfaab5e872c58"
+    sha256 cellar: :any_skip_relocation, monterey:       "92e4fd7b02e9314319342a86b176bcb78c902ab326b6db482c653194ec5389df"
+    sha256 cellar: :any_skip_relocation, big_sur:        "92e4fd7b02e9314319342a86b176bcb78c902ab326b6db482c653194ec5389df"
+    sha256 cellar: :any_skip_relocation, catalina:       "92e4fd7b02e9314319342a86b176bcb78c902ab326b6db482c653194ec5389df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a4d109e42f3146daeb999e613a9cedca98cbd851f98f950f54cfaab5e872c58"
   end
 
   depends_on "python@3.11"
