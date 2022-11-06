3ac3a1d29be3a0999a4c688f9599d6d583f2d999
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-11-06 16:01:37 +0000

    hysteria: add 1.2.2 bottle.

--- Formula/hysteria.rb
+++ Formula/hysteria.rb
@@ -7,6 +7,15 @@ class Hysteria < Formula
   license "MIT"
   head "https://github.com/HyNetwork/hysteria.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b80d54892daa6836fc845dda8d980f281cde4b3ccb8e1d828bfc96460a1cd70b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70691cd413d93222745e932087147c11081035f2f0e9eb8aa577d586279366f4"
+    sha256 cellar: :any_skip_relocation, monterey:       "9f4e1eeb8fe4e916e9b3983a5581c4e79b89fd62f4a7a7c71db87ff6fcd2f99f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a5c387936785732dfa6c1d969459b90f76cf4c026c3752bd181850cfe7bc325b"
+    sha256 cellar: :any_skip_relocation, catalina:       "e568a9e23736a782fefa2f2b3eb4ad55930c6cd1e4de615b321a6d52f0557bc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42f7af1cc6a6e3ae1126478b7230253070b4e421421bf9bd03de4c4e528b7138"
+  end
+
   depends_on "go" => :build
 
   def install
