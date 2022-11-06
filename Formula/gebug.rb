890f739a7e55822dff73122688fec6230bb29310
FX Coudert <fxcoudert@gmail.com>
2022-11-06 20:21:51 +0000

    gebug: update 1.0.3 bottle.

--- Formula/gebug.rb
+++ Formula/gebug.rb
@@ -7,6 +7,7 @@ class Gebug < Formula
   head "https://github.com/moshebe/gebug.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "756ecb4926bf082047e379c5abc2aa267073617ba66ea26a6c0f99725de7a25b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4f5b02c9bd0e0b24f17b152bc4a714346844f22f17759287008576ad240c7b27"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f526c66051c98b69817247e6ec0ac180f11bd3658b77595d2cc8ba7d3f678826"
     sha256 cellar: :any_skip_relocation, monterey:       "574f20b58ccab0297e79eb61752f32255a6131ab282c3d17dea3573ebc0e2aff"
