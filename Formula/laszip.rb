fc56f2431e6c8916f00a128d037cf43349c4b6f1
FX Coudert <fxcoudert@gmail.com>
2022-11-06 12:49:01 +0000

    laszip: update 3.4.3 bottle.

--- Formula/laszip.rb
+++ Formula/laszip.rb
@@ -7,6 +7,7 @@ class Laszip < Formula
   head "https://github.com/LASzip/LASzip.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "5d2325d42a6958fad99c62e6330f6081fcf0bb95568345a321eb301cff46549d"
     sha256 cellar: :any,                 arm64_monterey: "5064bc999925063dab10850a16565f0e6146565496fe53357b138dffbe5ade18"
     sha256                               arm64_big_sur:  "6849693f9166120961fcada7af8d222c1e7580bc191d75a3fdef7ca685f22566"
     sha256 cellar: :any,                 monterey:       "eca7c603a0e7a894cc63acdfbf2a77536f82b35ad26106f32e2ed03ec6fa5b90"
