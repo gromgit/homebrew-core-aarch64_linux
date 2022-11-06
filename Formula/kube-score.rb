343fe81decefacb4a27215e7c90e4581530f118b
FX Coudert <fxcoudert@gmail.com>
2022-11-06 21:16:39 +0000

    kube-score: update 1.14.0 bottle.

--- Formula/kube-score.rb
+++ Formula/kube-score.rb
@@ -8,6 +8,7 @@ class KubeScore < Formula
   head "https://github.com/zegl/kube-score.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a0a649b918556cdfcc540d547e5dc69ee0b7e25e134e29f6fff7a12edd8b33f5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "28d65ca4f637272e9c26f9488c34f06ae1f97f09207899f6afe38cb0db4eaf91"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9263d0aeab0f5ee03f541dd149669a3d227e92305735c37f89322d8ccfbcbd82"
     sha256 cellar: :any_skip_relocation, monterey:       "db28c4baa1d32c6688288f002c735c64f39433a6ab010d5c05ec3036b919417f"
