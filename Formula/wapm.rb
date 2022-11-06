8eeb5feeca247c639175b10ef050eba2ae3a8a0f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-06 00:29:46 +0000

    wapm: update 0.5.6 bottle.

--- Formula/wapm.rb
+++ Formula/wapm.rb
@@ -12,13 +12,13 @@ class Wapm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2729f79c8d537d55f69011c441bbc6d299d6b9aaa79a570ad680480a19833fea"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0ae9f17921580b58bd21ddb03fa55d5727c76d414b3625577eb175fd645c6925"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d266172783b37eaef3b379894acbcefd806fa242bb225483001491c37bbce4bb"
-    sha256 cellar: :any_skip_relocation, monterey:       "b500295f120bfb4c77920e76a22914c3a067bf9840a4cbd81ffaf9f1b38b496e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "77042210be915f81ea9faf9bcd2816d62e6ce21bce8abd6ae7be22d247d4023a"
-    sha256 cellar: :any_skip_relocation, catalina:       "7fdd3ae7c6b9a6f1733abc0dd6f553c57f2b4012ea72c39cfef3f42e617718d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a0136e23de57e9f78f0d8fe29bc1e8c6421a09ab6aece1f02056ec29f4652e1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2ba1bf4b8c151f1005acf4429b6b951a786d8727dead1b86edc72993268520cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "767f95eae3e10dd0386d7f839ce1f6aa509e3e10c3d539a9f69bffff7c3e5bd0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6b9881207fcef7665c3e9946d76494f4ee69b4472377955fbfa3c80dd5c0e329"
+    sha256 cellar: :any_skip_relocation, monterey:       "5453c51d197c085d3011dcce5c2246a67546ce16be0395eac6e83b9766350fae"
+    sha256 cellar: :any_skip_relocation, big_sur:        "38f2fbee5a31075c015af4ccae015373fff9d98d7c2f3c98c954aa4372732ada"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b401e546148144a0c1f98dd792683535790a13a1ff7548d9cff2c40535c5d43c"
   end
 
   depends_on "rust" => :build
