1dd2c2dd5b914248a7b9959235f351d80fbd3d63
Bo Anderson <mail@boanderson.me>
2022-11-06 14:32:08 +0000

    qt-postgresql: update 6.4.0 bottle.

--- Formula/qt-postgresql.rb
+++ Formula/qt-postgresql.rb
@@ -10,12 +10,13 @@ class QtPostgresql < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "67a526ef3785bc7ce98323982d264e7dae51acd027aea63d18af83d3bf29fc91"
-    sha256 cellar: :any, arm64_monterey: "4ce80725c9198afadf6e8bfa110a4cf56cac7ffdb43bf480bf4e10c0b3dd9bb1"
-    sha256 cellar: :any, arm64_big_sur:  "56c5ee55a51da34d8fc48944e5883759100c35f1dcdc93e8ff57cde4cf65c544"
-    sha256 cellar: :any, monterey:       "d90c1bd27fff9bec49b7eba3aa4163e3fdd2a417af377cb4960f27d1e7649e54"
-    sha256 cellar: :any, big_sur:        "e4138e9004a37dfb064ff50e048b6f5c239bdd492e725298ab0c63e56bbc4c00"
-    sha256 cellar: :any, catalina:       "1438ec5cfff58c80361d6eb52707aa007fb59209fc105a2c4bf2f6a15ee888a5"
+    sha256 cellar: :any,                 arm64_ventura:  "67a526ef3785bc7ce98323982d264e7dae51acd027aea63d18af83d3bf29fc91"
+    sha256 cellar: :any,                 arm64_monterey: "4ce80725c9198afadf6e8bfa110a4cf56cac7ffdb43bf480bf4e10c0b3dd9bb1"
+    sha256 cellar: :any,                 arm64_big_sur:  "56c5ee55a51da34d8fc48944e5883759100c35f1dcdc93e8ff57cde4cf65c544"
+    sha256 cellar: :any,                 monterey:       "d90c1bd27fff9bec49b7eba3aa4163e3fdd2a417af377cb4960f27d1e7649e54"
+    sha256 cellar: :any,                 big_sur:        "e4138e9004a37dfb064ff50e048b6f5c239bdd492e725298ab0c63e56bbc4c00"
+    sha256 cellar: :any,                 catalina:       "1438ec5cfff58c80361d6eb52707aa007fb59209fc105a2c4bf2f6a15ee888a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2c3c9c06f4d07ae47778d629a35c7076377185dab862a7aae770b481eeb6db7b"
   end
 
   depends_on "cmake" => [:build, :test]
