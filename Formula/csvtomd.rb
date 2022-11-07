ce0714b10e3057fae688bd25b07e12f86619b05d
Branch Vincent <branchevincent@gmail.com>
2022-11-07 15:54:11 -0500

    csvtomd: migrate to `python@3.11`
    Closes #115097.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/csvtomd.rb
+++ Formula/csvtomd.rb
@@ -18,7 +18,10 @@ class Csvtomd < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d047da6a41a03db412406412f4741715b7446630a5ba5558fac8bda7b66668e9"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
+
+  # ValueError: invalid mode: 'rU'
+  patch :DATA
 
   def install
     virtualenv_install_with_resources
@@ -37,3 +40,18 @@ class Csvtomd < Formula
     assert_equal markdown, shell_output("#{bin}/csvtomd test.csv").strip
   end
 end
+
+__END__
+diff --git a/csvtomd/csvtomd.py b/csvtomd/csvtomd.py
+index a0589a3..137f8da 100755
+--- a/csvtomd/csvtomd.py
++++ b/csvtomd/csvtomd.py
+@@ -146,7 +146,7 @@ def main():
+         if filename == '-':
+             table = csv_to_table(sys.stdin, args.delimiter)
+         else:
+-            with open(filename, 'rU') as f:
++            with open(filename, 'r') as f:
+                 table = csv_to_table(f, args.delimiter)
+         # Print filename for each table if --no-filenames wasn't passed and
+         # more than one CSV was provided
