5c453a94a4990ec06382045ed3f93398dbb74ace
FX Coudert <fxcoudert@gmail.com>
2022-11-07 23:50:19 +0000

    mailcatcher: update 0.8.2 bottle.

--- Formula/mailcatcher.rb
+++ Formula/mailcatcher.rb
@@ -6,6 +6,7 @@ class Mailcatcher < Formula
   license "MIT"
 
   bottle do
+    sha256                               arm64_ventura:  "767b2931a3c17d8c96a2468051f06061a8196fe270b6b6bb15d805820bf37f6d"
     sha256                               arm64_monterey: "3be13e4d6da19162bc7d70f201185fdb3f7dcd1b6207d632df446a3adc9b27dc"
     sha256                               arm64_big_sur:  "f1a3d77e3f95325952f78a408eba5e08b125b962f47a12265afd80489458f077"
     sha256                               monterey:       "d33f530eef6e1f7455c7d2dd36fcf75472a709f76118da99d82d207034b6f484"
