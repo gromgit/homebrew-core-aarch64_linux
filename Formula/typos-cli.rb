52535555a014b30b243ddab35ad6ff98485723ec
Dustin Rodrigues <dust.rod@gmail.com>
2022-11-06 22:21:03 -0500

    typos-cli 1.12.14 (new formula)
    Closes #115031.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/typos-cli.rb
@@ -0,0 +1,18 @@
+class TyposCli < Formula
+  desc "Source code spell checker"
+  homepage "https://github.com/crate-ci/typos"
+  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.12.14.tar.gz"
+  sha256 "ace6adea86c9f0430b6957710d51fa9b48b46224deb10ef215efc5f62fdfc831"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "error: `teh` should be `the`", pipe_output("#{bin}/typos -", "teh", 2)
+    assert_empty pipe_output("#{bin}/typos -", "the")
+  end
+end
