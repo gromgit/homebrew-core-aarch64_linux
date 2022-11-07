0b6b839ab38ea382c01d539d99a674168619374a
Valentine Kiselev <mrexox@users.noreply.github.com>
2022-11-07 08:35:35 +0000

    lefthook 1.2.0
    Closes #115045.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -1,8 +1,8 @@
 class Lefthook < Formula
   desc "Fast and powerful Git hooks manager for any type of projects"
   homepage "https://github.com/evilmartians/lefthook"
-  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "7e272eaceeb80bc5efb1504dcc47072d0137dad9b2f3db75b1d672ddb6dd737d"
+  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "3e2e34de7fcd3f6b4818d3240bd0a8aa4d37caf5c141a43c55af26e9589a565a"
   license "MIT"
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
