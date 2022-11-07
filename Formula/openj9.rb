27231b70f76126608c693b68afb38f5540a81335
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-07 13:18:50 +0000

    openj9: update 0.35.0 bottle.

--- Formula/openj9.rb
+++ Formula/openj9.rb
@@ -17,11 +17,11 @@ class Openj9 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "935a351439ea07644e2fb053deb4016db98c994c1445df92d93a2dcb756b27a2"
-    sha256 cellar: :any, arm64_big_sur:  "c3a74e75990ab316b69932c5340471af5fec3bbbd33f59b87be2919a05193727"
-    sha256 cellar: :any, monterey:       "b029a1987b8d3a86badaef9f8d3e1616dadda33c541c4f4caa0b8f0fa9a57786"
-    sha256 cellar: :any, big_sur:        "e948d8ba9ffcfcd5b29cb9c6f7e9f2a5c59f5e65d47fa8d1ad4917c3d7cbe229"
-    sha256 cellar: :any, catalina:       "d4f8a526b2f1a5827589d5af0b4c174d97b6eb43d3dfe3f4b1e75f0a852652cb"
+    sha256 cellar: :any, arm64_monterey: "9570aad125859a5ab095905d6cdc64e2f537dddc8762419d9bea87230c045cba"
+    sha256 cellar: :any, arm64_big_sur:  "a45ff6f91ee86d0fcc2bfbd3ce83f4aea20715aa01df3e01680f163b74e36c24"
+    sha256 cellar: :any, monterey:       "a1ec2be6a1104b63db5dd1da2c71da3a746478cfca5a6037eb6744426dd22a89"
+    sha256 cellar: :any, big_sur:        "432828659e46fed219dcd4ce9e89a16555c054f85077b001f67b18dcbddcd145"
+    sha256 cellar: :any, catalina:       "89d89fc29751a74527f9d4a465a9a36adccb19a5935ef0e70e0ddea864f0058c"
   end
 
   keg_only :shadowed_by_macos
