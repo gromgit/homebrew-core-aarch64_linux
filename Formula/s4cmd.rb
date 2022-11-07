bf392e89e1b92a4dd512f59f2734663ec60e4d6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-07 07:04:38 +0000

    s4cmd: update 2.1.0_1 bottle.

--- Formula/s4cmd.rb
+++ Formula/s4cmd.rb
@@ -10,13 +10,13 @@ class S4cmd < Formula
   head "https://github.com/bloomreach/s4cmd.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5f7826d592fb2cd0063944e9d57cedf0176da034315bbd7f08f1455919d9d3ed"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40fa599bccc3e6c3a6cdf57d46373921cff6198cd77e849c9ac9749d51d9353a"
-    sha256 cellar: :any_skip_relocation, monterey:       "397b415f897596dff9d85c6b7a25ebd4b0d5e9c8b3a37d5a340c2049945b0ef1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9d6773e57ecb1cb40e3f5740d3caedf893ac750d1cc6e5d57fd83a1564e8aa96"
-    sha256 cellar: :any_skip_relocation, catalina:       "ed4069e849c2690b19808ea3c8864a52a4798306c39dbc2ce3c511e93334848e"
-    sha256 cellar: :any_skip_relocation, mojave:         "00e511f22fd4827fdcfd37313317d8bca3591801bfaf1a70efb6ca5a99ee7ea1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52d71474b73d8f249e111f67e6288c7bdf2b74ebcae1150e01b8f7a60169c611"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "579c89b3b9fa40c0ac1a426e55bd5b9a88f9f8e0560e08f89f5faccafa1d797c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bddd00a80e1ebe79b113c0f7f5d42158f438f5c4382dfe7c7a25eb24a453c65a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4fb07106e61b0d9317a091193d4a221053f24a5f02dc81e106c34710f749457e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7bb2576ce8c56705a123814b30d90e20f0361fb763aadf6c141fef33b3b64b06"
+    sha256 cellar: :any_skip_relocation, catalina:       "ff4ceeea473f6e83f1a9c59c16a056d6d6c1829bf3aad1770aeb82248f883ce6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82f269866cfadecf803bbf8901c120760a63efc5bd21bfe9d86073ea11370f43"
   end
 
   depends_on "python@3.11"
