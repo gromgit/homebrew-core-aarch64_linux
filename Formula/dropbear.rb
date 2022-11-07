b990beeb1f873963e466b70ab54397c39cd9fca7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-07 21:38:08 +0000

    dropbear: update 2022.82_1 bottle.

--- Formula/dropbear.rb
+++ Formula/dropbear.rb
@@ -17,6 +17,7 @@ class Dropbear < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8bc0edcd4401f0fe4b5434c747d571b22f6a6565199573cb0e452e2866b57218"
     sha256 cellar: :any_skip_relocation, monterey:       "e6e2b8b954f13adfc61c0c7e857197d8c300bb5aee833e134ea7cf27781f32c9"
     sha256 cellar: :any_skip_relocation, big_sur:        "33bb0e7178d106b25b33488c53cc9cbc9bd3b1c77db7c1d109bf0b13db27403e"
+    sha256 cellar: :any_skip_relocation, catalina:       "9e7555bf7f8596b7524050e87beaad1f2037314a8a3ff2520170b7737071f876"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "79b11ecc9390db971819ccfdc19df355df389aef6d83add80ba4161424796903"
   end
 
