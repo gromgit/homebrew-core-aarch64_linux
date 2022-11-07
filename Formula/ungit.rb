2c5e72c5d23852afa16b3a6efeb952888dd6b062
FX Coudert <fxcoudert@gmail.com>
2022-11-07 12:49:49 +0000

    ungit: update 1.5.22 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -8,6 +8,7 @@ class Ungit < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "393c22b95d686cab0afe17e408e076f9e4be666b5bdb4184c759dda63bd65e00"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7bc9e0f1d56b4ac32091c4f3fccfbedd4afadf82b7afe9e20e061afebed1aa30"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7bc9e0f1d56b4ac32091c4f3fccfbedd4afadf82b7afe9e20e061afebed1aa30"
     sha256 cellar: :any_skip_relocation, monterey:       "b2e40ec003b25b08144d36acd844a9f4061b303a4007f076d4851d23efa83255"
