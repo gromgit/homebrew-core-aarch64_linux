ca1a6967a0f9c9620025edadb944791284298612
FX Coudert <fxcoudert@gmail.com>
2022-11-07 10:02:50 +0000

    cabocha: update 0.69 bottle.

--- Formula/cabocha.rb
+++ Formula/cabocha.rb
@@ -8,6 +8,7 @@ class Cabocha < Formula
 
   bottle do
     rebuild 1
+    sha256 arm64_ventura:  "50e7ebda3eb0424fd7e9f7654ecf854c8f3dff2673e257225013f3d3085e7967"
     sha256 arm64_monterey: "c61f5a8df81d52575b991108e2683c27d94a4dcd62cd50d4396cb65234453b22"
     sha256 arm64_big_sur:  "6db92d4bc14b2b1045601758c9ad2d528fda7ce0029316a2b296c63c4953c54d"
     sha256 monterey:       "7002f27098ec51a1832a5a39e1f8d55eb7d11b7fe37ce005ff0dfe7bb1be9e59"
