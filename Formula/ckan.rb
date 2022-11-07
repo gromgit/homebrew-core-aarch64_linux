414e3fb78e1beea0d7e7bde24261c285b6860028
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-07 02:43:00 +0000

    ckan: update 1.31.2 bottle.

--- Formula/ckan.rb
+++ Formula/ckan.rb
@@ -11,7 +11,7 @@ class Ckan < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "abde3768457424230138bf83635fd6ae533ef0a7003b33724f672467675982bb"
+    sha256 cellar: :any_skip_relocation, all: "5d26320d7220f4e6282c9f1b6190edfcd183a81930fbeb89365e4832c9ae2e62"
   end
 
   depends_on "mono"
