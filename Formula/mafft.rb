d075a7ce1d4f80971c07175dc993ee576b0cd771
FX Coudert <fxcoudert@gmail.com>
2022-11-07 13:39:57 +0000

    mafft: update 7.490 bottle.

--- Formula/mafft.rb
+++ Formula/mafft.rb
@@ -12,6 +12,7 @@ class Mafft < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "09e3af4a92f5d01b7f715dd2d0d4d91798cc674ae5bf34040b36abfcaf0f9d31"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9ce2343170fba164088b8d138cb9043b5e3cd4674327243d3e6530aabb63082a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a28296f4008623699ca60650cb7bbc4bfafa85aecb9a4b7b446db67b26b3edbd"
     sha256 cellar: :any_skip_relocation, monterey:       "75f785fd4c67164de44a419b3eb75cb51ffce3c346d6c87958706267bcf36b3c"
