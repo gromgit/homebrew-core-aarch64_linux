8aefcc404e50c10a856a02707bad2d475a24057e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-11-08 07:38:50 +0000

    chisel: update 2.0.1 bottle.

--- Formula/chisel.rb
+++ Formula/chisel.rb
@@ -7,11 +7,14 @@ class Chisel < Formula
   head "https://github.com/facebook/chisel.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bd381685bf1bf3682e51c355acbca980b35659b8161f226329b3a0196aab55df"
-    sha256 cellar: :any, big_sur:       "bec2fe6d1e6afec5b3b1c79f5f11b9f2219ff8e22d81e1b343cf525b91ea220e"
-    sha256 cellar: :any, catalina:      "7ef6b79ffa9641e0617b2aec1b4f3dfcea59fc4059887c0d734baa1bda20441d"
-    sha256 cellar: :any, mojave:        "70b49b0ba45571db3341adf586e1498a041492745cfa2130b2ef95b81d14fb85"
-    sha256 cellar: :any, high_sierra:   "41797386262e226cf471995eac8ec50dffbf622140634254c6a7dab8a9471b48"
+    sha256 cellar: :any, arm64_ventura:  "7ee4917934831d56a5925fb22554c0dd136f8553df9c92067e4564e527371e82"
+    sha256 cellar: :any, arm64_monterey: "1aa32fddf646ce4a2772d5e10b39fddbe54a6383299346321d5d0dd067388471"
+    sha256 cellar: :any, arm64_big_sur:  "bd381685bf1bf3682e51c355acbca980b35659b8161f226329b3a0196aab55df"
+    sha256 cellar: :any, monterey:       "712007f0f4abd29073239839bd606dba927353444cf6baf4b058aedc92c17f0c"
+    sha256 cellar: :any, big_sur:        "bec2fe6d1e6afec5b3b1c79f5f11b9f2219ff8e22d81e1b343cf525b91ea220e"
+    sha256 cellar: :any, catalina:       "7ef6b79ffa9641e0617b2aec1b4f3dfcea59fc4059887c0d734baa1bda20441d"
+    sha256 cellar: :any, mojave:         "70b49b0ba45571db3341adf586e1498a041492745cfa2130b2ef95b81d14fb85"
+    sha256 cellar: :any, high_sierra:    "41797386262e226cf471995eac8ec50dffbf622140634254c6a7dab8a9471b48"
   end
 
   depends_on :macos
