a4e0dfb00377c017fda66b5e7c367ad62dd07610
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-08 13:36:27 +0000

    as-tree: update 0.12.0 bottle.

--- Formula/as-tree.rb
+++ Formula/as-tree.rb
@@ -12,6 +12,7 @@ class AsTree < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4cbe38f0537c86b65808064608c61b8c792098177911f013f24c2470fd2fdf62"
     sha256 cellar: :any_skip_relocation, monterey:       "adf2bb6e9bbbcc4d393462ec04b4cf9abe28bc5748f9636b0a9668b56082fb60"
     sha256 cellar: :any_skip_relocation, big_sur:        "2d13c83015f82b0b39760b9087f417bc1465b4e33cc6a90061235a70e510c896"
+    sha256 cellar: :any_skip_relocation, catalina:       "5c14a2f148f036c39c7187f0da94f9c6ab52f3e9c531c5009ae5e6db68b01cc4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "82337903e0b8ee48cd19517b4d1bd8e0b66d5c17e212a03f6e15d2b12130d85b"
   end
 
