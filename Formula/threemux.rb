d99ebf4be328a87ee491713a34376d29d7361ef4
FX Coudert <fxcoudert@gmail.com>
2022-11-08 20:26:26 +0000

    threemux: update 1.1.0 bottle.

--- Formula/threemux.rb
+++ Formula/threemux.rb
@@ -7,6 +7,7 @@ class Threemux < Formula
   head "https://github.com/aaronjanse/3mux.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cef0aa7766c9cf4045c403ae53f20fa8fcb4913c80148b01755b4a4da7449d8e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "471a6870e6b9d45e994d2977c855e0aea4ed22b1f59f0d09afaf4d096edd0e2c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2d3dd3465938d0ac5b845b07689a08b667613210d9d58649c9a152ade32dc347"
     sha256 cellar: :any_skip_relocation, monterey:       "64497230017e4e033cbafc7ae8f684e5ff77c1a8af3ec153f715269d5f4dab01"
