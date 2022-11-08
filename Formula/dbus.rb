8d896ce45101aadbc9f0ec6d80920a63b3af0bdd
Adrian Ho <the.gromgit@gmail.com>
2022-11-08 15:51:35 +0800

    dbus: add 1.14.0 bottle.

--- Formula/dbus.rb
+++ Formula/dbus.rb
@@ -13,12 +13,8 @@ class Dbus < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "c9fd5fca636d95d017c791c0b5b87ec99cd3ac3ef7d194c3bb05448a52e2933a"
-    sha256 arm64_big_sur:  "26e8143ae21a90e27e9d3218c69fe7ac509379bf6adc75e0293234c366a89b35"
-    sha256 monterey:       "25e7cbac037a07c9bef1f0cf8346c6c755fa46ce1b71471e46f64f396441f734"
-    sha256 big_sur:        "76ec924fed127229906ab2e90b30ecf1a856f0f50e9ab6519cbcee1f1202632f"
-    sha256 catalina:       "8b983bdc674dff5647af3ecab82e44abacbb460db8c60435e16ae351f22bfacb"
-    sha256 x86_64_linux:   "ccf0fc79f59fa7c0ac014861c70d2df8b0c8f8c7f688d548d84b74ce998ecd38"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dbus"
+    sha256 aarch64_linux: "2f76d7bf5730318c4ecbc6c7e9a76597c5e49b84d4d83b57a583c6f1828246fc"
   end
 
   head do
@@ -35,10 +31,10 @@ class Dbus < Formula
 
   uses_from_macos "expat"
 
-  on_macos do
-    # Patch applies the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
-    # Homebrew pr/issue: 50219
-    patch do
+  # Patch applies the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
+  # Homebrew pr/issue: 50219
+  patch do
+    on_macos do
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/0a8a55872e/d-bus/org.freedesktop.dbus-session.plist.osx.diff"
       sha256 "a8aa6fe3f2d8f873ad3f683013491f5362d551bf5d4c3b469f1efbc5459a20dc"
     end
