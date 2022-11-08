a34417f031758b0cd3f6ef6899d61cb4ec070163
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-08 13:48:47 +0000

    bkt: update 0.5.4 bottle.

--- Formula/bkt.rb
+++ Formula/bkt.rb
@@ -12,6 +12,7 @@ class Bkt < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb4994a9537b696ed3aa5327751d6b147ac17c5c32a08d4891c93a547a9fc818"
     sha256 cellar: :any_skip_relocation, monterey:       "bd0ade61029b216b1dd19e0e19ad635df0b39bc9ec2537edbc04278b34213326"
     sha256 cellar: :any_skip_relocation, big_sur:        "2a370ef7c828a155cad72d4776bd14b3a84a1a5ef1b7392741bd4186d5613746"
+    sha256 cellar: :any_skip_relocation, catalina:       "c12826cb4085ad2a67eb168cf043c1587df4c6fc876036568bceac64be1874b9"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf58f99b4e1b064ab039571ddf4956768eaf03a16134cc7fb6e8869f8b3b72a3"
   end
 
