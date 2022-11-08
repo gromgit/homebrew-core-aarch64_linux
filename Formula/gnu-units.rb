eed634d1202f952df58b3ad5e1a6c74a76713d32
FX Coudert <fxcoudert@gmail.com>
2022-11-08 11:30:15 +0000

    gnu-units: update 2.22 bottle.

--- Formula/gnu-units.rb
+++ Formula/gnu-units.rb
@@ -7,6 +7,7 @@ class GnuUnits < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 arm64_ventura:  "513874dc0676da8124c51c057b940752ed76663c6d290c11b33fc7767a84b2cb"
     sha256 arm64_monterey: "642c474a2809e0d9db1d0db76e37f02913da673cfc4fdc337d3b4f4412dd9058"
     sha256 arm64_big_sur:  "c7a080c4aba8efa918476470972561d4a8e3ead5d808f02fcbf41c50226d5602"
     sha256 monterey:       "07de02b9e3aa4e1dae47247a30a29c4e8b09013778d6efd040a0774136996a75"
