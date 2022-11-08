55835f3a6d7a11912ccb34e13ea658e60a783d44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-08 09:04:41 +0000

    hysteria: update 1.3.0 bottle.

--- Formula/hysteria.rb
+++ Formula/hysteria.rb
@@ -8,13 +8,12 @@ class Hysteria < Formula
   head "https://github.com/HyNetwork/hysteria.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "73fb3b998473d71c7f27d6cd9693f615f1dadda12e9a6905dea82b4d76c99080"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b80d54892daa6836fc845dda8d980f281cde4b3ccb8e1d828bfc96460a1cd70b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70691cd413d93222745e932087147c11081035f2f0e9eb8aa577d586279366f4"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f4e1eeb8fe4e916e9b3983a5581c4e79b89fd62f4a7a7c71db87ff6fcd2f99f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a5c387936785732dfa6c1d969459b90f76cf4c026c3752bd181850cfe7bc325b"
-    sha256 cellar: :any_skip_relocation, catalina:       "e568a9e23736a782fefa2f2b3eb4ad55930c6cd1e4de615b321a6d52f0557bc7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42f7af1cc6a6e3ae1126478b7230253070b4e421421bf9bd03de4c4e528b7138"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "333e0083b8866aa4b9803ba40dd37a86e992f75fd869c372fe31e379cba2324e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "de3de5cdfe9341c61156e356f89c4d981ddaaed933e82df28ac2cfe72ac19122"
+    sha256 cellar: :any_skip_relocation, monterey:       "3d9a31387d8e782571dc1d382bf24ea4ebaaeb034a81bb173fc80720561f99c6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0451607d271ec57e310851e353c5a96e7d06158ebc425b23d1150f06d1f10afe"
+    sha256 cellar: :any_skip_relocation, catalina:       "77c1f7dc84f7aadffa27c02e12ad0e99acd57b46e76bc59e13812967cbb1325e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d16652efa00f3cb1f1e0438335a35223acd6b6d55ea373e28eca0caddefcef22"
   end
 
   depends_on "go" => :build
