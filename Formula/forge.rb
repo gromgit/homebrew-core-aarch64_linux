ea3f76e30b84434fb14cd37de40f11d9f3392756
FX Coudert <fxcoudert@gmail.com>
2022-11-08 21:33:55 +0000

    forge: update 1.0.8 bottle.

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -11,6 +11,7 @@ class Forge < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "f25eab15726f80780cce2a23f2df9b320a2d62bc6662db8c407318bdc78a7075"
     sha256 cellar: :any,                 arm64_monterey: "beb86dc4a75fa4f6c4ac1948358c1dac9e3e45d157567c05430f44fbad218206"
     sha256 cellar: :any,                 arm64_big_sur:  "648b7ce9d0146143fd3aa7ce40ac8989bc5af8552b58f6a54406d67b9dc37bc4"
     sha256 cellar: :any,                 monterey:       "99fa6a4741e4c98221187530cde328cec9e8faf2a660b31b11c49573986fb907"
