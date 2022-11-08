3a9348eaafa366b017b5c3b8381e5e90b17780e7
FX Coudert <fxcoudert@gmail.com>
2022-11-08 00:37:47 +0000

    hayai: update 1.0.2 bottle.

--- Formula/hayai.rb
+++ Formula/hayai.rb
@@ -6,6 +6,7 @@ class Hayai < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dd6a2a14ff0cd666059ddf5aecbbca1679a1fd40248c981566223c308f3dd659"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9dccf9e4fa4cd6918a8bf6e37008b59044af49aedf965a878d35fe5200d42062"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "41ba5eb3f260d738729a866e1951d9caf2830eacb918944da50ab0761a4b4f56"
     sha256 cellar: :any_skip_relocation, monterey:       "fe3175b383887bb7a3c8d98378c76c09fd1b1bfe9ad64e7f119df3d6054faebc"
