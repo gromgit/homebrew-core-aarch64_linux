83058baed6f4d763e1c26d5724a144272b2e92d3
FX Coudert <fxcoudert@gmail.com>
2022-11-08 14:48:05 +0000

    reminiscence: update 0.4.9 bottle.

--- Formula/reminiscence.rb
+++ Formula/reminiscence.rb
@@ -10,6 +10,7 @@ class Reminiscence < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "7bb3a7ba7f08c9aadd9a95eb5f1ed7936ec593f50f14070627c060d30b58a5cc"
     sha256 cellar: :any,                 arm64_monterey: "71f92aa98545a961759a62af80f7ca5f83f5fe2c0d933811421c63b846517fd0"
     sha256 cellar: :any,                 arm64_big_sur:  "65baa88dd4251db9f003eda17b92c9b730eaa3592577944bf0c3cd1ee931eb74"
     sha256 cellar: :any,                 monterey:       "0263ce7602723fbf4205670cb0d7125eca9c24b5301d732d1a102c6788510a77"
