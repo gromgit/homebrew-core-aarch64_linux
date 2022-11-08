25aba1f4b4ad4f1d2f96798566b4829f382b2a78
FX Coudert <fxcoudert@gmail.com>
2022-11-08 12:39:34 +0000

    bash-snippets: update 1.23.0 bottle.

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -6,6 +6,7 @@ class BashSnippets < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e38f6d39c157dcf604f1ac774a16afe66472de96ebf612a7f409689a7074282"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8e38f6d39c157dcf604f1ac774a16afe66472de96ebf612a7f409689a7074282"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e38f6d39c157dcf604f1ac774a16afe66472de96ebf612a7f409689a7074282"
     sha256 cellar: :any_skip_relocation, monterey:       "c2a441dc6d5b21408f49f4869b00919354392682b3ef7eef2f908ab2f638dd8a"
