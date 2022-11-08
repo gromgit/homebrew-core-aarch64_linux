56020b1c2d41c19ef893a2521a76f9d63f666dc2
FX Coudert <fxcoudert@gmail.com>
2022-11-08 15:27:52 +0000

    subnetcalc: update 2.4.20 bottle.

--- Formula/subnetcalc.rb
+++ Formula/subnetcalc.rb
@@ -7,6 +7,7 @@ class Subnetcalc < Formula
   head "https://github.com/dreibh/subnetcalc.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bb9a97d4624ba2eb1e41b0bb8fbbdcdec86983dcffed476c9c4441a11a639ecb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "427da5c6383dd9a3307ec30c9a975ca5456f7b2c27b59d433991b25d613f0576"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "305a22c7217e51ad68f175bb9958d69474f8f4e1d2316d69ca49032302fa9eb5"
     sha256 cellar: :any_skip_relocation, monterey:       "1afef375229983906d16d2dc28801d71b651cd29a118626f15448ad8d2439a87"
