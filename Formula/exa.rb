310aa39cfead28809125c3ff3da7b76fa9372a57
Bo Anderson <mail@boanderson.me>
2022-11-08 14:04:54 +0000

    exa: update 0.10.1 bottle.

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -12,13 +12,12 @@ class Exa < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0d39fb8905caae1b7b0f998960b7ed2fed3f758499acdab52b7a68acb6ed06f4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d60f5270bfc6f8fcf6729d86bcf994e5cdf582761711c67170dd990c4951e728"
-    sha256 cellar: :any_skip_relocation, monterey:       "3fe9b4a21d636e1282df9f2d9d29ac785af5ecd10bf3a2a584eebb57976b8311"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bd1eb7bc9000217571a70b5c745befe6e471cca27374b0035557fe6afe3e73dd"
-    sha256 cellar: :any_skip_relocation, catalina:       "5400719f606f276a1e421685881919adbda04028cead1b8c64b1b342c201fb9a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7f44f39c1beea749b9dd4373954a080381c0f0f01d8e987e8f21ac87e269155"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "621d5ce8d5e8f9841a6191e3246fda50ca4d98fb3a108969157293e4454baa21"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "97b6bf5ce5591c1dc5c320ac857c3d977caf2547579d0d9145655ad9c2d9e255"
+    sha256 cellar: :any_skip_relocation, monterey:       "269e359b76de3e1084ccf276342a134cc752737bb5a83a6178b019940d0270f5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9b6b6a3c852c14ee92aa8c601cf0b81b79fe7be1f14d84e2fa0a21ead58fdeb1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd62a17077ea9a775ae6b83f9a574e165ddc9a59a32e2e3a0dfd5d1802514989"
   end
 
   depends_on "pandoc" => :build
