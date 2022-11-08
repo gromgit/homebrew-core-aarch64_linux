64db97097a8acb7e93d96335d8904208f64ee065
Branch Vincent <branchevincent@gmail.com>
2022-11-08 07:44:39 -0500

    unoconv: migrate to `python@3.11`
    Closes #115186.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/unoconv.rb
+++ Formula/unoconv.rb
@@ -5,7 +5,7 @@ class Unoconv < Formula
   homepage "https://github.com/unoconv/unoconv"
   url "https://files.pythonhosted.org/packages/ab/40/b4cab1140087f3f07b2f6d7cb9ca1c14b9bdbb525d2d83a3b29c924fe9ae/unoconv-0.9.0.tar.gz"
   sha256 "308ebfd98e67d898834876348b27caf41470cd853fbe2681cc7dacd8fd5e6031"
-  license "GPL-2.0"
+  license "GPL-2.0-only"
   revision 3
   head "https://github.com/unoconv/unoconv.git", branch: "master"
 
@@ -20,7 +20,7 @@ class Unoconv < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "632fc4da008b323aa3aeb15c6960b4e5c31b05f282db84616258881ade4bf0f8"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     rewrite_shebang detected_python_shebang, "unoconv"
