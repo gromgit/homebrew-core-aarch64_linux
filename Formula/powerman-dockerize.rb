64183ab686847e0660abd2fa3fb10b897928bc6c
Rui Chen <rui@chenrui.dev>
2022-11-08 10:20:30 -0500

    powerman-dockerize 0.16.4
    Closes #115200.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/powerman-dockerize.rb
+++ Formula/powerman-dockerize.rb
@@ -1,8 +1,8 @@
 class PowermanDockerize < Formula
   desc "Utility to simplify running applications in docker containers"
   homepage "https://github.com/powerman/dockerize"
-  url "https://github.com/powerman/dockerize/archive/refs/tags/v0.16.3.tar.gz"
-  sha256 "ada4a61a647983cefe180561d2f20563d8109af5b4b5d8d97ee5f90f481ecd15"
+  url "https://github.com/powerman/dockerize/archive/refs/tags/v0.16.4.tar.gz"
+  sha256 "6456535ce13c5cfff344ad07838c3e39117a1683c430889559c8caebcc0f6a12"
   license "MIT"
 
   bottle do
