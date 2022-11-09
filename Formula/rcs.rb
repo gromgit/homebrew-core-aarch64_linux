fa1714eff0fdace166e89b4bc6eca75b7361945e
FX Coudert <fxcoudert@gmail.com>
2022-11-09 08:33:07 +0000

    rcs: update 5.10.1 bottle.

--- Formula/rcs.rb
+++ Formula/rcs.rb
@@ -7,6 +7,7 @@ class Rcs < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7f9cf6ea9614d1286230615b81443f0941b32cd7768769378039df8463d71fbf"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "579a458898062f0b99c455e69e93cbf4c2b5637ae72806d4ee74f4063f2a25ff"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b241d433eecdd7d01df54bafa597df65a42ad75866ece0a16d865a205c0660b1"
     sha256 cellar: :any_skip_relocation, monterey:       "816051fa12300cd95e60babba5d4613e47f4c0c6fb426d520edf1f04914b91c6"
