08c5340615b627a2544acb272e1fafd98a12385f
Rui Chen <rui@chenrui.dev>
2022-11-09 05:22:08 +0000

    croaring: update 0.7.2 bottle.

--- Formula/croaring.rb
+++ Formula/croaring.rb
@@ -7,6 +7,7 @@ class Croaring < Formula
   head "https://github.com/RoaringBitmap/CRoaring.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb2a8bb4e65f620d753de659f379956b6478fe0d8481fbaacc08fdaf84e075bd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c25b9d6fccd33630ef95da8773e69c02e5946ade428a080664341c92acb5b283"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c08c6cfe4389fd532b718c7d6e5557df47f5ac15f42a9ba66d9b42d90e2001fb"
     sha256 cellar: :any_skip_relocation, monterey:       "2911cbcfa01cd5652f1e121f5123cfc046ebfb5f11b6bc7e7e8c5801cad32839"
