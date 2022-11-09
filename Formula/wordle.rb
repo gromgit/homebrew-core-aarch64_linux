7efd8700349c6a2cf996686f4b60365828734c10
FX Coudert <fxcoudert@gmail.com>
2022-11-09 10:27:16 +0000

    wordle: update 2.0.0 bottle.

--- Formula/wordle.rb
+++ Formula/wordle.rb
@@ -7,6 +7,7 @@ class Wordle < Formula
   license "AGPL-3.0-only"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6a9db0e2f7b058f74ca8098312cdffa5d5cf60e5f9de7feb115e9eb43be2eb62"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e488788ecd598f500999af6b5ea4f30a026302b1cd0378cfac67c92e361a8ee9"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8cb2a9a3f1a194601e659bc9ee54bca569c1a8a6d0915f2d93e6c00f2ba5d5d3"
     sha256 cellar: :any_skip_relocation, monterey:       "629c70f19e09643f02fe8469d09431e2750f43035133195b048d55ce5538452a"
