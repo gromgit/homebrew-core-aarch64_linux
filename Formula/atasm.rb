e6d3560a2bc791a2e95f60ac609910ec941aa52a
FX Coudert <fxcoudert@gmail.com>
2022-11-09 10:41:30 +0000

    atasm: update 1.09 bottle.

--- Formula/atasm.rb
+++ Formula/atasm.rb
@@ -12,6 +12,7 @@ class Atasm < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "727eea7c68b8de0a001e3b0937c429af8797af568433be534d74ada42a1925eb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fab0b76cba3104965e4627681e2f5776df2337ad3300ba6acf140a0151afe237"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f4c812d6e1cc7f0b02d3c00160d7656779fe163f62c55da63ae2359bc472585b"
     sha256 cellar: :any_skip_relocation, monterey:       "16ea6c4cd36ace328c78bbe3daeaad7e22ea30c5e013b07584eb00e9931ef67e"
