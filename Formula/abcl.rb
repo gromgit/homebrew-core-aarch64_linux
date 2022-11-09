9b89dca2c6a28c8b19c72722d1e6c086caf44cd7
FX Coudert <fxcoudert@gmail.com>
2022-11-09 09:47:06 +0000

    abcl: update 1.9.0 bottle.

--- Formula/abcl.rb
+++ Formula/abcl.rb
@@ -15,6 +15,7 @@ class Abcl < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cec2d54fa4f7c38dbc1a82ab238eaef509d439870cbe304683b70d511e8460f4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f3be4843794ef45604b332aee04ebd7d9be8170fbfb66c431650f827eb09d603"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a38ec7fe9b6a5ae4dd1f5fe798a6e5d954719fef7748feb70865e1dc900fc8f1"
     sha256 cellar: :any_skip_relocation, monterey:       "39f244d377cd1ebe59226aa615e77dcfa31b2fff63531646bf1d07bc3c8fb5b7"
