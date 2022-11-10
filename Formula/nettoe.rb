4124a98c53084137d5ae7a37e5f136a5ac0aa795
FX Coudert <fxcoudert@gmail.com>
2022-11-10 16:34:30 +0000

    nettoe: update 1.5.1 bottle.

--- Formula/nettoe.rb
+++ Formula/nettoe.rb
@@ -6,6 +6,7 @@ class Nettoe < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "af2f3dde5a365c56fb7e055b1a4dba60b1af8dfa0bdb02150965d407e1d69f3d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e809cedf43aead29ed0e328f3a2a2a3a5cb15b55462392cc94bee353d015ec8a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a9f2e1b0a27c15f903c42fd54af41f52553d9027cde5aff110e7f8a895b934a8"
     sha256 cellar: :any_skip_relocation, monterey:       "af7fe17e2294568f6c4140418c1e5ca0ce73daf59dc5defe5036ec611a697666"
