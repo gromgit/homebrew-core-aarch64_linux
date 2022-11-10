39cac7c50445e404ab9a60f3fe342e167d228c11
FX Coudert <fxcoudert@gmail.com>
2022-11-10 15:07:35 +0000

    ccheck: update 1.0.1 bottle.

--- Formula/ccheck.rb
+++ Formula/ccheck.rb
@@ -6,6 +6,7 @@ class Ccheck < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "44342aa8159dc8a960a1c59447e4de8a178cf1e6ed40baae0ebdd7fd3a58f194"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "eefb4874302348bbafd636ba38bc958b940c488a0acafc92357002d47e8fadf3"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a31268fedbbe48cd989d6900c96b34b98e8354fe1e397dc96fb454a32376528e"
     sha256 cellar: :any_skip_relocation, monterey:       "f4784bc20e952229a571074101e28e68c82393c0ef224b141a0cecd93fa641b1"
