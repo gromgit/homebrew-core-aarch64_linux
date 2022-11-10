18a67a05a6988f0dc1f9943212df65ec5a0c9769
FX Coudert <fxcoudert@gmail.com>
2022-11-10 08:32:48 +0000

    cctz: update 2.3 bottle.

--- Formula/cctz.rb
+++ Formula/cctz.rb
@@ -6,6 +6,7 @@ class Cctz < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "72f87dc452212bdf12aefe25c5ae745d15a6df0b5161919c3c01bcac29a39477"
     sha256 cellar: :any,                 arm64_monterey: "756dc7625a50e7e2e607a45c75fa7f2002fc7e8bcec504ddcb06eab20766948d"
     sha256 cellar: :any,                 arm64_big_sur:  "4895638a03396673d972613bed298d7288d369fe91aca165ddcf9b0357aade18"
     sha256 cellar: :any,                 monterey:       "ee512d1b222211307cfc2f3e5e6c4ce33085ec79bb0a25aec62ce16cca13fd7f"
