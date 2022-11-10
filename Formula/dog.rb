8806f54650a2c3a44fb29638328d1f5f1a9a605b
Bo Anderson <mail@boanderson.me>
2022-11-10 07:09:54 +0000

    dog: update 0.1.0 bottle.

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -7,14 +7,12 @@ class Dog < Formula
   head "https://github.com/ogham/dog.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4f90bf064348fb099a5ecc0de69a6ef9cee7b45583d2cdaeb844214d5e773f17"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ea5dead4f4fbe142d855e32dbb2a01a311836bdbdec746c62eb21e1c5a8b159"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd49c3b016a17db8514b585a226c658aa109a7e2bf96023f4a194deb7501c70a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0666eb89b99b25ee0c5b2064f2a7a2781a325aded61691b707c6e93c512c652f"
-    sha256 cellar: :any_skip_relocation, catalina:       "21f901c6abb536f0a7bc00d97df6e561aec1acff1759a96a5f69bd51eac1da98"
-    sha256 cellar: :any_skip_relocation, mojave:         "095b6dbb23b96504a6f0e0a292a7157c78e900825e92f4c140cc29643a294e29"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "fef7ea4d925e230ba9e75cb051a888263c2bc10fcf26a96899cea2cc8891a302"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ab73546a4ee11f186379ac21479d7e1ce052791f2840c54a43d5ed67281e500"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "59c9e37cff1154bb2d7407b0473ab9280156a4144be16bf4bb7820bae34ab27a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c5a4ee2a717b756d3668395a129aebe48e8cc72b049d7f9cfbbfb9d14669de0"
+    sha256 cellar: :any_skip_relocation, monterey:       "0dbeb2271fcbda043b8c67b63463bafbe674e692ed464e891529941e72eecf4c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b34f634c60a28d5e5ad7694da962949c677bb1808886c6fcaa7c92579633e5ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55daa95c827fd102b2599978ebbc0fb60d497395388531533891c8d2a28ff3b4"
   end
 
   depends_on "just" => :build
