afcc17cfa0a96358e90489472871128adcdcff99
FX Coudert <fxcoudert@gmail.com>
2022-11-10 09:36:33 +0000

    shellz: update 1.6.0 bottle.

--- Formula/shellz.rb
+++ Formula/shellz.rb
@@ -6,6 +6,7 @@ class Shellz < Formula
   license "GPL-3.0-only"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f988ca6178c77343dfea20c9b4adf2eb0742605f4e0402dac2e2ad963ab5881c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ac21c3ae7ea28b170bf5b3eb29b70ea61c512060807833de7ef1723a66316f6b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4ea721956ec5e9a51e8774000f17eca688fdf50c1d41496b23f2be90bec65c8"
     sha256 cellar: :any_skip_relocation, monterey:       "fee1186791c0b7af5f978cd5170e0c1b1820cff57828e05d7b2274f93ff8f5f4"
