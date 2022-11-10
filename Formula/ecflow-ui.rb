5ce9632b5fc74e25db4208faece812b3072b71c4
FX Coudert <fxcoudert@gmail.com>
2022-11-10 14:30:57 +0000

    ecflow-ui: update 5.9.1 bottle.

--- Formula/ecflow-ui.rb
+++ Formula/ecflow-ui.rb
@@ -6,6 +6,7 @@ class EcflowUi < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256                               arm64_ventura:  "ec7de105accd483779ab8a5e6c0883befb41331a30a3a41e499fd70494b3c082"
     sha256                               arm64_monterey: "6ae1e4af4d37930f8b3f9f2ac9eee1b21ce144f3ef3df8668d4a16d585e8dd41"
     sha256                               arm64_big_sur:  "b390efe842995399d306f2b2b3a80b6208129d01a4572172997ebaea899e514d"
     sha256                               monterey:       "eb02666119c06fc883ae3259c003cdc29b7c15bc4fd35215f926e7a05c0ced38"
