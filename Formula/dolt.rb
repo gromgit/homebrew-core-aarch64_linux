f1f355aeb76e09b9d466edad87e9f3c76ec4e523
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-10 22:04:53 +0000

    dolt: update 0.50.15 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -11,13 +11,12 @@ class Dolt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "529188f1752f55ef9f708106f07bcc122b41219da3f495282759fa3d9b779ad7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c4d7bde2309e604ee6cec3c44dc79dcf2853d45a71caa79983928b9b3ec54fa1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0c600b5b1900d85ca521fdd454d515118f6bd97e7d2b9f1512993d981a410eec"
-    sha256 cellar: :any_skip_relocation, monterey:       "8781419a5c6e37fa27598500b035c9456c6dc867cbb3bab1afb6065cc1807836"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b043e45929946ec40fe6f3b7ea28b8c7c43943843eddbea5ee60dee34e133707"
-    sha256 cellar: :any_skip_relocation, catalina:       "87db72b95a63b92d8d8cca9c393d85b451be1d14f04995b96e056829df1f6fa1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf3c3989d32bd850ea39f82b4a89c3827847f31abbd14221338c3480d9696902"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "16f10f0b99b5b34ee60a26e7bd50844c2db547656540597685bea54f0fe4a487"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee1f011ea59dcd4d3c45400e60b9f2b92d0eaead2862a305e41590f4a0a3ee69"
+    sha256 cellar: :any_skip_relocation, monterey:       "da3fadf62dba9a8d44d2530d45a7f2f902747d06bab94c366c1a70320a555277"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8a4c3bee02feba4d8da7b60705fec26ec416aee3623e433bfe084f1fdece1168"
+    sha256 cellar: :any_skip_relocation, catalina:       "cc3bf46d1dcfa70c36836d5f86521c34c3a3a2f1a3ad1cdd82c8c22057ab73eb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e2ad59ba0c84ace401560b969f8fcacf1cc6e290339c89cfc829181f5cc863e8"
   end
 
   depends_on "go" => :build
