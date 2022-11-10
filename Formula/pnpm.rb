7adf2ed3f8820b73c4cac227a966059fbf7d1b49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-10 08:56:01 +0000

    pnpm: update 7.15.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,13 +13,12 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "14282d3010b5a0b5d11893f9b66eb5e383b6208e3f2de5d56b3513eba2ce48cc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "14282d3010b5a0b5d11893f9b66eb5e383b6208e3f2de5d56b3513eba2ce48cc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "14282d3010b5a0b5d11893f9b66eb5e383b6208e3f2de5d56b3513eba2ce48cc"
-    sha256 cellar: :any_skip_relocation, monterey:       "29fd746314039c25db607bae8319949c8035d813c927462222382dd3c6d46949"
-    sha256 cellar: :any_skip_relocation, big_sur:        "09e958787d54de551770fd044e6285745fa2e2106761eb1a57d5ce4773655655"
-    sha256 cellar: :any_skip_relocation, catalina:       "09e958787d54de551770fd044e6285745fa2e2106761eb1a57d5ce4773655655"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "14282d3010b5a0b5d11893f9b66eb5e383b6208e3f2de5d56b3513eba2ce48cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "37d0c2ac17f776c3eafc1bcb236669bd050b555d187a4f8a793821729ff6c6ed"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "37d0c2ac17f776c3eafc1bcb236669bd050b555d187a4f8a793821729ff6c6ed"
+    sha256 cellar: :any_skip_relocation, monterey:       "d3411d51114829ec8639743ef03b38b7f1b1f4ee567aa6d1e1462bd0d51545a8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "461f61e62d55ea9a41d811b1881ed6fe64b6e534632b36b7df66caf60faf7a59"
+    sha256 cellar: :any_skip_relocation, catalina:       "461f61e62d55ea9a41d811b1881ed6fe64b6e534632b36b7df66caf60faf7a59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37d0c2ac17f776c3eafc1bcb236669bd050b555d187a4f8a793821729ff6c6ed"
   end
 
   depends_on "node" => :test
