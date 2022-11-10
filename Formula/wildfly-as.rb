e3a85d1d935d830d110ab158b3e708c597e49628
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-10 05:37:33 +0000

    wildfly-as 27.0.0
    Closes #115357.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wildfly-as.rb
+++ Formula/wildfly-as.rb
@@ -1,8 +1,8 @@
 class WildflyAs < Formula
   desc "Managed application runtime for building applications"
   homepage "https://www.wildfly.org/"
-  url "https://github.com/wildfly/wildfly/releases/download/26.1.2.Final/wildfly-26.1.2.Final.tar.gz"
-  sha256 "dd2a97e7bf32a13adfc6782868afb5e48fb033b83480fc5bf94128807a83b17c"
+  url "https://github.com/wildfly/wildfly/releases/download/27.0.0.Final/wildfly-27.0.0.Final.tar.gz"
+  sha256 "04d88a451aaf7a85ef57873a89ab959d3b569c7d194547f86f1812cb37551d28"
   license "LGPL-2.1-or-later"
 
   livecheck do
