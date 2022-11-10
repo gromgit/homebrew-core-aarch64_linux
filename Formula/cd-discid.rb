720da96eb10a62c12326d8b9f20e85444d6a6ca5
FX Coudert <fxcoudert@gmail.com>
2022-11-10 08:52:56 +0000

    cd-discid: update 1.4_2 bottle.

--- Formula/cd-discid.rb
+++ Formula/cd-discid.rb
@@ -24,6 +24,7 @@ class CdDiscid < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "618a12cac73126b2818a93e91870571b7c78604ac0c4ab4e9f93e6c398a9d33a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "671ac240cb3b94484690d12ec1d85cc96d90ffbf848cfb4adeebd8f5f32c1fbd"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7effbc8d5fb1325aa629f1ec607d75c64c9547b0aa70deb4f05b07e5a6b94c84"
     sha256 cellar: :any_skip_relocation, monterey:       "d90c6640e3b67fb2140a10da27714f30a302187bb0f0b13477a53936a2a66456"
