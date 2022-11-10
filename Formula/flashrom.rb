dda90332b5b18f274d9b4c1744af49e39fa1f7bb
Tom Matthews <trcm@pm.me>
2022-11-10 08:59:09 +0000

    flashrom: rebuild
    * Remove patch
      Patch no longer required as this has been fixed in the master branch : https://github.com/flashrom/flashrom/pull/212#issuecomment-900948546
    * Only patch stable, as HEAD already contains patch
    
    Closes #115363.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flashrom.rb
+++ Formula/flashrom.rb
@@ -1,11 +1,20 @@
 class Flashrom < Formula
   desc "Identify, read, write, verify, and erase flash chips"
   homepage "https://flashrom.org/"
-  url "https://download.flashrom.org/releases/flashrom-v1.2.tar.bz2"
-  sha256 "e1f8d95881f5a4365dfe58776ce821dfcee0f138f75d0f44f8a3cd032d9ea42b"
   license "GPL-2.0"
   revision 1
   head "https://review.coreboot.org/flashrom.git", branch: "master"
+
+  stable do
+    url "https://download.flashrom.org/releases/flashrom-v1.2.tar.bz2"
+    sha256 "e1f8d95881f5a4365dfe58776ce821dfcee0f138f75d0f44f8a3cd032d9ea42b"
+
+    # Add https://github.com/flashrom/flashrom/pull/212, to allow flashrom to build on Apple Silicon
+    patch do
+      url "https://github.com/areese/flashrom/commit/0c7b279d78f95083b686f6b1d4ce0f7b91bf0fd0.patch?full_index=1"
+      sha256 "9e1f54f7ae4e67b880df069b419835131f72d166b3893870746fff456b0b7225"
+    end
+  end
 
   livecheck do
     url "https://download.flashrom.org/releases/"
@@ -25,12 +34,6 @@ class Flashrom < Formula
   depends_on "pkg-config" => :build
   depends_on "libftdi"
   depends_on "libusb"
-
-  # Add https://github.com/flashrom/flashrom/pull/212, to allow flashrom to build on Apple Silicon
-  patch do
-    url "https://github.com/areese/flashrom/commit/0c7b279d78f95083b686f6b1d4ce0f7b91bf0fd0.patch?full_index=1"
-    sha256 "9e1f54f7ae4e67b880df069b419835131f72d166b3893870746fff456b0b7225"
-  end
 
   def install
     ENV["CONFIG_RAYER_SPI"] = "no"
