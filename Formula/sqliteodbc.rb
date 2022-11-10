f76047d24518587208b9c4adae0dbcb819187ce3
FX Coudert <fxcoudert@gmail.com>
2022-11-10 08:28:44 +0000

    sqliteodbc: update 0.9998 bottle.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -10,6 +10,7 @@ class Sqliteodbc < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "e7b517422475c70e7177c1d8cdb3e02dead73d2ce0e1987097074c9695f95959"
     sha256 cellar: :any,                 arm64_monterey: "8df10001bbf7daf13bd5b3d65f75edc7ea2b2dc6989873de14b957ad9bf7a4b1"
     sha256 cellar: :any,                 arm64_big_sur:  "1024f38c091e42d18128dbfba82ee5a9a41e46670383f714df325303bda5b415"
     sha256 cellar: :any,                 monterey:       "df3e249a7780ffe6178fd754655a6313c4526c9f6eb06cf169c5776a669d749b"
