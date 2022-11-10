6e89fb30b714b08fde02e42321341cf18f258064
Bo Anderson <mail@boanderson.me>
2022-11-10 14:38:19 +0000

    tokei: update 12.1.2 bottle.

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -11,14 +11,13 @@ class Tokei < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fbdc887b33fa6a6b059f2ee4f77f5440ef2a9fc04e522a810f9bd45e35090c20"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7ebd9bbbef357de832d2a009cf849ec0ac3d00a868a36e3af192c1f20238835c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8ebff0ff4e422447970fce9d87917e61f01651d030124201d73853f7288f3e8"
-    sha256 cellar: :any_skip_relocation, monterey:       "31e9f53e4be3780253415e425eeecd75041219449c8baad9f1e6816d93c7e0b3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "70ec65406393fe4af7eb7aeb6ac261cfe7319b66b5559b1d65c56fcfdc77ee08"
-    sha256 cellar: :any_skip_relocation, catalina:       "f65dfddfe85d0ca4a06707a65fac5746eb6a756d76021b5ec806ee4f1d0a3639"
-    sha256 cellar: :any_skip_relocation, mojave:         "ea27c2cf381e93e5423d8e5ab3b19283fe99310fdc85323309b263747cc10b2d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b7e8559aea7ffd8f9d05b698f29d025bbc4e14f7a0ce5ad04bf5123fbbcdce8"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "58c651b034293b8dc4d2dc00c7b2a13d8c6c3c093683ab5e1ebe08a1dc6cf6f9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ca867ee898e06ea98d3f3012e753ecb6e292160353e187fa62619f86447ca7c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c7b1e487acc3cddaeb578dfab8ec1e8f81fe00834d60d81311b6ca56d2c29f1b"
+    sha256 cellar: :any_skip_relocation, monterey:       "19c6b4270cf51286adf6e2ddfd2aa45bfa721d52bfe5c8c0792277311a930116"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eb4878d9ef7023cba511a2c84635ebf1c428741772ebc5fd139f8a0c258574d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db7e8862a35ed6f09f4c502c45d14e2891fdff54524b1daa728bcd8fdebef2b0"
   end
 
   depends_on "rust" => :build
