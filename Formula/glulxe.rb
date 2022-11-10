b5cff7d7156d14936253c6bf2681aa2f1ca554bd
FX Coudert <fxcoudert@gmail.com>
2022-11-10 11:42:25 +0000

    glulxe: update 0.6.0 bottle.

--- Formula/glulxe.rb
+++ Formula/glulxe.rb
@@ -8,6 +8,7 @@ class Glulxe < Formula
   head "https://github.com/erkyrath/glulxe.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a1428eb47dd0443e7d76127116828461087d9006810827318464138aae8f309"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "26ce8c326d32c6a9bc677cb31ac3d71ad3c342b9268519ed852e3067073e020c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4685ff356d489470d50622a226708c1934221e100048759f2e63edb18c711964"
     sha256 cellar: :any_skip_relocation, monterey:       "7a5f7a89d8fb4f97d8dd4c0e129c57750e716822f5a74d050f8d3e5012461710"
