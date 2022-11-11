a059da70a5cc0f9e34260ec32cae95994b7f9934
FX Coudert <fxcoudert@gmail.com>
2022-11-11 15:02:51 +0000

    rainbarf: update 1.4 bottle.

--- Formula/rainbarf.rb
+++ Formula/rainbarf.rb
@@ -7,6 +7,7 @@ class Rainbarf < Formula
   head "https://github.com/creaktive/rainbarf.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a149026ada66d61e40b18aac888eb0502cd0d63b539116761cdd559949ceb14b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a149026ada66d61e40b18aac888eb0502cd0d63b539116761cdd559949ceb14b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ef63dec71481ba7ea8f46ebe302945b5d09845825dde684183a2cf2bc65c3684"
     sha256 cellar: :any_skip_relocation, monterey:       "a149026ada66d61e40b18aac888eb0502cd0d63b539116761cdd559949ceb14b"
