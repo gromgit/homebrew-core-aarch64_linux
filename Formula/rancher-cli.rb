57b3b7a7e5ccbd441f8d84f3bc70666e233a559b
FX Coudert <fxcoudert@gmail.com>
2022-11-11 09:28:02 +0000

    rancher-cli: update 2.7.0 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -12,6 +12,7 @@ class RancherCli < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fbb3bbca755757fc43c2fc4e61743d53123202e9920121d4e2d71ec6fb890396"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8cbcc9707997701fb30257de8808959c1d17b3ce30a7330dd0ecb82ada8c342d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dca5a3c31a9e583bc3344ac97a1a8ece975a793451098da80791d959e48ee18c"
     sha256 cellar: :any_skip_relocation, monterey:       "985c82bdf3f7f17ca2bdd702888115793b13d0798966cf92c668d3c4d65b1c3f"
