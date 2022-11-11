70de8dadb502a1ce9a4c0f8dc97c39cca7ce66bf
FX Coudert <fxcoudert@gmail.com>
2022-11-11 08:47:17 +0000

    run: update 0.10.0 bottle.

--- Formula/run.rb
+++ Formula/run.rb
@@ -6,6 +6,7 @@ class Run < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "064ba836c8f0c5bf55687f32a31a5c3ca2697a35ee179fb0008ee2088852ad01"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "064ba836c8f0c5bf55687f32a31a5c3ca2697a35ee179fb0008ee2088852ad01"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "064ba836c8f0c5bf55687f32a31a5c3ca2697a35ee179fb0008ee2088852ad01"
     sha256 cellar: :any_skip_relocation, monterey:       "35befd6adc7d143999df865c38b28c4794e59d8ce211a7ed2259ef5e394f215d"
