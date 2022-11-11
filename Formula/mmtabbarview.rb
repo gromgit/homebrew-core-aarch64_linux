c6f4ee8e08ee91dbcdb41032d4a54b9daf102bc7
FX Coudert <fxcoudert@gmail.com>
2022-11-11 12:48:01 +0000

    mmtabbarview: update 1.4.2 bottle.

--- Formula/mmtabbarview.rb
+++ Formula/mmtabbarview.rb
@@ -6,6 +6,7 @@ class Mmtabbarview < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 cellar: :any, arm64_ventura:  "ec634de2a8f60f6d6d09c88cc8ce9293fd94ee07ba2181a7b07a3ee2f29d99ac"
     sha256 cellar: :any, arm64_monterey: "8d752b1a6566f010c2a3c42c7248e56e15ea8c55b80a7cd6b7fc571b67f81912"
     sha256 cellar: :any, arm64_big_sur:  "10a139efa381ffffb4b38609246914c123559b80ceaf16baa96135ae4687ba5b"
     sha256 cellar: :any, monterey:       "83aa65e0eaa1ee040131cda4ec9f9c1447ebd06124b7680c754a6c6ed8786d01"
