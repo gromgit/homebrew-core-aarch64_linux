f59c24c8db27bb3940b2f45afbd3fd29a18dd6bf
FX Coudert <fxcoudert@gmail.com>
2022-11-11 14:58:16 +0000

    quasi88: update 0.6.4_1 bottle.

--- Formula/quasi88.rb
+++ Formula/quasi88.rb
@@ -11,6 +11,7 @@ class Quasi88 < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "7b4c72ce4204ac7a10b58982ba65f7bb900f3b6bc2a71533efdd4eb8394b38e1"
     sha256 cellar: :any,                 arm64_monterey: "467c760fbe3ff26108e82a47876e626bd1e9ecbfce352c4c2d2bba4d9de9d2f9"
     sha256 cellar: :any,                 arm64_big_sur:  "c0d20d39a966111e0a9f48ea3ceb420f8de9f163d31ee5a2dbc325078e014420"
     sha256 cellar: :any,                 monterey:       "67e5b32ec92bceee098d501e77746e834f936238121840f373bcac6e1d6106ef"
