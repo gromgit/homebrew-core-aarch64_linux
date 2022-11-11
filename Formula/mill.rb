f2cb658c962173f1b16642757e2206c5ec14d490
Porkepix <porkepix@gmail.com>
2022-11-11 01:31:34 +0100

    mill 0.10.9
    Closes #115421.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://com-lihaoyi.github.io/mill/mill/Intro_to_Mill.html"
-  url "https://github.com/com-lihaoyi/mill/releases/download/0.10.8/0.10.8-assembly"
-  sha256 "e6625ce5c2d3f71922c41f266c362e2587a5f9f35e0b0af53cccd4fd90a770ad"
+  url "https://github.com/com-lihaoyi/mill/releases/download/0.10.9/0.10.9-assembly"
+  sha256 "5c3cb4edd173ca5465f36e5062a46de5ec9d54f4788c47af0b656bab181316dd"
   license "MIT"
 
   livecheck do
