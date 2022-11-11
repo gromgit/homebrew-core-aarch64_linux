b0cfa57c542e05877a310c1d835ac76d029699e4
Porkepix <porkepix@gmail.com>
2022-11-11 13:12:21 +0100

    gitlab-runner 15.5.1
    Closes #115462.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -2,8 +2,8 @@ class GitlabRunner < Formula
   desc "Official GitLab CI runner"
   homepage "https://gitlab.com/gitlab-org/gitlab-runner"
   url "https://gitlab.com/gitlab-org/gitlab-runner.git",
-      tag:      "v15.5.0",
-      revision: "0d4137b81e70f98c801f5c47411dc06a16333d12"
+      tag:      "v15.5.1",
+      revision: "7178588dc1e747941d1c5b9da759c0c00b6aaa96"
   license "MIT"
   head "https://gitlab.com/gitlab-org/gitlab-runner.git", branch: "main"
 
