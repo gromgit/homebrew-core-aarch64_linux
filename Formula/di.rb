5b1ec4ba91c9d04987776bc5687b624ae96143a9
FX Coudert <fxcoudert@gmail.com>
2022-11-11 20:22:48 +0000

    di: update 4.51 bottle.

--- Formula/di.rb
+++ Formula/di.rb
@@ -11,6 +11,7 @@ class Di < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0fe76ffb07cc8e29f57a247c138a888384fb3d8610dcddd29fab93c9aca547ee"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2f0155678e6e44db1dbfbc24fff96aaee23c22a69d553457dd8ac1f9de13b527"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4fc57febb3e9e00603674540b9c90fff49df3f9b376149fd3ed37ed72d3c29b8"
     sha256 cellar: :any_skip_relocation, monterey:       "0ec56fe7507f200c983d2bbc2ade6465b8bf26c7714bf2626513312afa2b6568"
