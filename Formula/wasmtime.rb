a6192f760171421f66c32b6fb237ede877f834b3
FX Coudert <fxcoudert@gmail.com>
2022-11-11 09:37:07 +0000

    wasmtime: update 2.0.2 bottle.

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -13,6 +13,7 @@ class Wasmtime < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a9faaa009a327908b736390102a6360a04affa83e29438ff2ea8f84ef6171a8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "bb21911ec62eb9e6f4a067a3c07a7648595a5c0b2af164ce06a8edf45daedae6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1008456c0b08066e6f068fb6c7d73ceff0e0e97d1596d41df569e9d0ab86a2db"
     sha256 cellar: :any_skip_relocation, monterey:       "acd5b208aea8caa2aec58574c80d395ca78245a2da128d034f621230fd8be0d2"
