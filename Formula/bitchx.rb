a845767e7042aedea95768c10ce8eb494f18894e
FX Coudert <fxcoudert@gmail.com>
2022-11-12 10:08:06 +0000

    bitchx: update 1.2.1_1 bottle.

--- Formula/bitchx.rb
+++ Formula/bitchx.rb
@@ -18,6 +18,7 @@ class Bitchx < Formula
 
   bottle do
     rebuild 1
+    sha256 arm64_ventura:  "13c3a23d3e7316d509646ddbd5ee5442c096856124a4f2cc9123afee2ab66bfd"
     sha256 arm64_monterey: "2176f208cf2ef65ebe0fc9ea27d2581e21450a01f2b399aba4d0620085245bc2"
     sha256 arm64_big_sur:  "e92a812a3fdb12ef256f677a923b1343bd9a478beb41c988ea36845e6e154d75"
     sha256 monterey:       "60c248c5f1b0a85a655ec9462b28982c4c0a089babdac242aedf9e0313a36f8e"
