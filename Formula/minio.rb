12604d5a550dca5086dfc68ff6429311ec60f4bf
Rui Chen <rui@chenrui.dev>
2022-11-12 02:13:37 +0000

    minio: update 20221111034420 bottle.

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -17,6 +17,7 @@ class Minio < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9d37c13e84b56d1dfcca0971614f3f5e9475351b3d65177767945c0492a0ff6e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7de2877bf5c08b32f8de037fb8b9368bec054150a34ae08c2067bae6fedc8c07"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "72bf6a9168eaad45fe05d096f59b5854c455389da182e7623d256b967d556f53"
     sha256 cellar: :any_skip_relocation, monterey:       "a329916bfa475ad5672c186f40c81536674b05ab5e88ae5a201531d1525fbb27"
