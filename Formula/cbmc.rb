c7d3c704fb544319822bccd1e0dddeec93f71d7b
Patrick Linnane <patrick@linnane.io>
2022-11-12 05:53:23 +0000

    cbmc: update 5.70.0 bottle.

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -7,6 +7,7 @@ class Cbmc < Formula
   license "BSD-4-Clause"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "047d92ff1a1fe325c38cbb7d6a2dfeaf349af418cd0f49658ce1d10865b39740"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9c02c4acc79bc960fb1111a1024a9d3b1449da9fe691dd501cc6e87068f7e6b4"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a54a7fa2bbf4fda62a844bd489c00bee62ad0644fd3b6938ba6abbfc7133a4b8"
     sha256 cellar: :any_skip_relocation, monterey:       "82cc2d2c20431445cfb13c91dd19b50d67f3a4b88b4dc4dc2ce2729b6b8ae1c4"
