def3c2f5db84a6dc2797075cb50979fd357f5e67
Rui Chen <rui@chenrui.dev>
2022-11-12 16:44:30 +0000

    powerman-dockerize: update 0.16.5 bottle.

--- Formula/powerman-dockerize.rb
+++ Formula/powerman-dockerize.rb
@@ -6,6 +6,7 @@ class PowermanDockerize < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "45a990f6b8bac33e5002be13cbdaea5bfa719c22d18211a846d88c6a5e755c38"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8642d0c12f327e67adc39a35cc0090e6104b7c45a1c76f298c3a628d0ce2fc81"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "629931a80f250d3ba752ffc3cac7f2a3e0fe22b64acaf83a9e7ca89fa9f1013b"
     sha256 cellar: :any_skip_relocation, monterey:       "b198f6fe1903346a08094580f67a39992efef4af256839ccb459f0c140a6fe0a"
