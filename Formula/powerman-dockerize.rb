252259e1b674a919b2a222722ecadebcee1d3833
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-12 12:05:49 +0000

    powerman-dockerize: update 0.16.5 bottle.

--- Formula/powerman-dockerize.rb
+++ Formula/powerman-dockerize.rb
@@ -6,13 +6,12 @@ class PowermanDockerize < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d90c7cd879e9a9b4f64530cb47a9509e8671a7634bffb7a31a0928e7e712031f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "72a12caea93fcd605a9b381054c6fe6f9b5a8018d8153453b57a13efc15eaf3e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "746bf3f595691c84e74d1fdf4244d37bd470934c19c76d3c46d92b1a35b711e9"
-    sha256 cellar: :any_skip_relocation, monterey:       "0dae012f0937d8e235148357cfc99ae528a7a98ebbf620708eeeaa1848793efb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fb7b2fd54adb8ce7aef4a0e0359bcdc31b1d87d26d465c18b69df5e688e79642"
-    sha256 cellar: :any_skip_relocation, catalina:       "003d122471bf4b623dfb39a88d28b57294d215de8610d6ba1b2237118ba9f32f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "097da0cffba33324a84ee6e3792c8b0ae58bdb24bfc836eecfa0b541c6f05037"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8642d0c12f327e67adc39a35cc0090e6104b7c45a1c76f298c3a628d0ce2fc81"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "629931a80f250d3ba752ffc3cac7f2a3e0fe22b64acaf83a9e7ca89fa9f1013b"
+    sha256 cellar: :any_skip_relocation, monterey:       "b198f6fe1903346a08094580f67a39992efef4af256839ccb459f0c140a6fe0a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "17adffb61cc731ad5a9488df5a91aecf78d9385bded4aca98505abaf0135756e"
+    sha256 cellar: :any_skip_relocation, catalina:       "276d36926bfc3a980148363e01a4fe0e891580e8dbb2e14c8a122913457f4446"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1da1b5385f25681e3af2aae439e15594809f4c2f9c3ab1dcc8d20c0ac4cfd9e3"
   end
 
   depends_on "go" => :build
