5c458a68bad5119ae032962a26167b92ddad7a43
FX Coudert <fxcoudert@gmail.com>
2022-11-12 09:12:02 +0000

    oauth2_proxy: update 7.4.0 bottle.

--- Formula/oauth2_proxy.rb
+++ Formula/oauth2_proxy.rb
@@ -7,6 +7,7 @@ class Oauth2Proxy < Formula
   head "https://github.com/oauth2-proxy/oauth2-proxy.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2deab940416c36a506300ce0a03d047169212327b2bb658a5119469ee5f983d2"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1813499b6d63547f16af1b40833a8f8cb95053f6b04e67f2a3f5b86533e0debc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8de92ac8df98b26aadfe3ab97025d61285d57cf40d4509a1559fab6475803270"
     sha256 cellar: :any_skip_relocation, monterey:       "0af00d421c7605c8199bc1a9c0e5482d14db98608fff1b4f2413b1742911e546"
