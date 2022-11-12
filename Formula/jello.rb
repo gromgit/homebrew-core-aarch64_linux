7a6a42da02e45765e21c7d94bcb7cf1a518c71f7
David Baumgold <david@davidbaumgold.com>
2022-11-12 13:35:11 +0100

    jello: depend on pygments
    Closes #115570.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jello.rb
+++ Formula/jello.rb
@@ -18,12 +18,8 @@ class Jello < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "b78041a9b59db09c5092e38991d3661b775515acae41515aab6dbcaf337734d5"
   end
 
+  depends_on "pygments"
   depends_on "python@3.11"
-
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/e0/ef/5905cd3642f2337d44143529c941cc3a02e5af16f0f65f81cbef7af452bb/Pygments-2.13.0.tar.gz"
-    sha256 "56a8508ae95f98e2b9bdf93a6be5ae3f7d8af858b43e02c5a2ff083726be40c1"
-  end
 
   def install
     virtualenv_install_with_resources
