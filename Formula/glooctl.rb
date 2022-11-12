2f4527bde391f0a8d6a8e75588288e4d6b057330
Rui Chen <rui@chenrui.dev>
2022-11-12 16:21:54 +0000

    glooctl: update 1.12.34 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -15,6 +15,7 @@ class Glooctl < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4eeaac05dbc05bd65d90cc01e606f190839f3d5d07d792c0d61884a5cb521637"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1c4d49fea6430291fa94cc45f2a81646d6a32a92feef5960d8d197dd6a006395"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ddff9ecdb73fe37fc70d54a23feeba89dcf3c12d53f9dc3e27fdf64acf1d2f07"
     sha256 cellar: :any_skip_relocation, monterey:       "e516b2a18bc0be1744b49bf0a5aa8f6281ecd5970d9f86180951fde2c683ed9c"
