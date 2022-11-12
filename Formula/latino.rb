0c1bbbfdd6d6bf4f877658db0fac3a4bc7c2e0de
FX Coudert <fxcoudert@gmail.com>
2022-11-12 09:22:39 +0000

    latino: update 1.4.1 bottle.

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -8,6 +8,7 @@ class Latino < Formula
   head "https://github.com/lenguaje-latino/latino.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "aac984134bbd599ee76ccd80ae017c738f633a4642a7a99ce8c40ead45078d8b"
     sha256 cellar: :any,                 arm64_monterey: "96c4a79b87f5f1c44ee9a39d13e75ced35a8b23ed5ee209279690363e79cb876"
     sha256 cellar: :any,                 arm64_big_sur:  "e7ac6aa7973d222c73097942a233c2998e542358b21aa725dcbbf8e1e6010b06"
     sha256 cellar: :any,                 monterey:       "952cc3322325e09ceb9fcbb00c957a4179031f7522c99ae6afbc20bb13bc3d13"
