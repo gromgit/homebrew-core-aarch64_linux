7b773b4dbe23eeb526dda2fcc15456cf8f75c361
FX Coudert <fxcoudert@gmail.com>
2022-11-13 00:24:14 +0000

    open-adventure: update 1.11 bottle.

--- Formula/open-adventure.rb
+++ Formula/open-adventure.rb
@@ -13,6 +13,7 @@ class OpenAdventure < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0e12500870339890aa4e38adb46743e8cd2459645749f98b985c126a039c26a9"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e626a51cb92c97db9628a4dd4f3220b4aaa5fca24b044e9d84b1a27986597e47"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b775593afe027000123bb388f84553f5c2213a566cce24eff46be74e96f000c9"
     sha256 cellar: :any_skip_relocation, monterey:       "207d746678eb8bc2d241af54fcd33785cc25066037d4211669321304a6d9dfab"
