aa8dfa37b07ca13d05785db1c472ccb964708258
Rui Chen <rui@chenrui.dev>
2022-11-12 22:38:44 -0500

    puzzles 20221113
    Closes #115640.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -2,9 +2,9 @@ class Puzzles < Formula
   desc "Collection of one-player puzzle games"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/"
   # Extract https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles.tar.gz to get the version number
-  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20221102.289342e.tar.gz"
-  version "20221102"
-  sha256 "76a5c15b26383a5b0c8de8f7336c9fea60431189cc66da52f9cf800c8ab6c3d1"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20221113.5a90dd9.tar.gz"
+  version "20221113"
+  sha256 "8881e377955f1d634465fc2ce227e5e0612d20a35e37dcf570ed1ad30b43c558"
   license "MIT"
   head "https://git.tartarus.org/simon/puzzles.git", branch: "main"
 
