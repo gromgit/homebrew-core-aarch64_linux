19aed6f87a0b200bb3c4ca0b311f1be0ba1e1912
FX Coudert <fxcoudert@gmail.com>
2022-11-13 19:06:23 +0000

    envoy: update 1.24.0 bottle.

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -12,13 +12,12 @@ class Envoy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a6670aa7eaf0fb59ae07319e0ecc4ba0f5e7707b1f2ac494ee4756635d21311"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e7bf1c8a8edd41908a662bcf5f4d12fe097635c16a5274de321e5e632a72ec4f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f63c8c45373a38df81fe854f6064c0c4bcecd0c8b5cadf0bf5408999b29e6aac"
-    sha256 cellar: :any_skip_relocation, monterey:       "3aaa744a59e5ed4dd1b316ec020fdaaa03ea267b7aa33d3a7ce25b0031d1c6a5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dd38a660c9d1623b968e7c13d9f250b885488381a3cf866358a9d960d4dceb01"
-    sha256 cellar: :any_skip_relocation, catalina:       "e2240ae23e6351058022840344b0c30aee820977eec71acd29a641338ea4991d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ce1642222af2cb38ea97a5da78b71b8d1ea0ad1aa4930284b07d23252db247e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "760f8e5fdc6f9331689a4c1159d92a89b3474a40c2fea2cadf113f1409b3962b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "29f1920e697f3e17bd42d11ca99e1f1bf1086b0488f06cbd6c45eadb0b635e0d"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab84476a2ae6bbd1b47f64fe1a1d93aed5ef003775a50b8bbaf9b6b28c018e45"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f85ffd2b5e8761e379cddf3f1aa960b06cd6b6f6397610e5ed74a29007d4fa83"
+    sha256 cellar: :any_skip_relocation, catalina:       "f2b1f7a4598d8721a08da713e59723ca0f26b520d534656c47f3ffd1e3516038"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dba5897716b8dad485eb0e290f475f7bd5e567f92a7972af7e14650628618ae1"
   end
 
   depends_on "automake" => :build
