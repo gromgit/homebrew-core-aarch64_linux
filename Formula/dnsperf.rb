bf0c7f13ed603f66a34ae27e0b762f75e8e33e9c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-13 18:04:38 +0000

    dnsperf: update 2.10.0 bottle.

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -11,13 +11,13 @@ class Dnsperf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "4909e1eea7dded9818118cf107eedff7a709f949fb7bfa0d6b13a3cd6a6bd467"
-    sha256 cellar: :any,                 arm64_monterey: "f5fe8421406e1c6d506c5d9e83d5b9256935ce34e443286f1c2162dd3f4fc8fe"
-    sha256 cellar: :any,                 arm64_big_sur:  "a3987ac2cbf7f703623d31962866146794453c00afde10892eeba979c8cdeb3d"
-    sha256 cellar: :any,                 monterey:       "afe4e17621d4201df2a5bc5da377f330d89e1b80d7ecbbfb37c9020175aa46f1"
-    sha256 cellar: :any,                 big_sur:        "fd360d4eb9f870d1e69b4dbfcce4f8c6ca6449937ce7006d392eed24a7c23926"
-    sha256 cellar: :any,                 catalina:       "aa03ee74c9117ead14fc42dfed419e404cc48e232ddf8b898ebb3925ec8ab89d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26ff62ca6ede5ad01175e0035145c6f4680fa0e9a742f6bfd2d16f415aed6b4a"
+    sha256 cellar: :any,                 arm64_ventura:  "157728f76b94569abca2c15c768e3baed0603f1c441e03bb2ff0ecd5ecce2593"
+    sha256 cellar: :any,                 arm64_monterey: "9b25b2a09255afc3891d0d17ec7ce9e65a1c8b229ac23728e3cc3d0a1fd76374"
+    sha256 cellar: :any,                 arm64_big_sur:  "ae5d6c446dbc5bab45e7e81d7dcbab642cca5b0ab928dc3b13d6a34523969014"
+    sha256 cellar: :any,                 monterey:       "ac699fd318bf69be10d573acbb5c79d25af63e6fd7d7c8394ea96485ffd08697"
+    sha256 cellar: :any,                 big_sur:        "8a2720f49a11a0ab53e1316a7814540f8fcba643045a3a883563e36c612a4888"
+    sha256 cellar: :any,                 catalina:       "113d8551f583d7524ae265f03b8f0d1fb4294a4afac7bb22c18f1661349065d0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3cd1889eb7c180f4e1cb35608d89905168bb98d91f4371d99cf0c4957adc944e"
   end
 
   depends_on "pkg-config" => :build
