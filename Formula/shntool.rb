f63a7bd68458f57a01a54ff30322c089b060f35a
FX Coudert <fxcoudert@gmail.com>
2022-11-13 00:13:35 +0000

    shntool: update 3.0.10 bottle.

--- Formula/shntool.rb
+++ Formula/shntool.rb
@@ -13,6 +13,7 @@ class Shntool < Formula
 
   bottle do
     rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ec3c0feb069099563b7359c7e3f926ac1840aaf0364bf40be1a2cf1462865764"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "02dd5226afa9d564374ab0e56a1edd08d7b61ac8c9c7fb09ec2dc011d47cb955"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1dfa65178720559cebc5500eb9f32d4ca2606a4f1b6a94b9d175ceded8fae2f0"
     sha256 cellar: :any_skip_relocation, monterey:       "858a65a08549e69ecc02979700c5155426f0a8202da7e6ec48bc0018a6ed9038"
