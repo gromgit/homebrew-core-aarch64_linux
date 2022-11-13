0a81380b2e7ea0f8d4f42da5ee134c234e921bcf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-13 18:05:56 +0000

    logcli: update 2.7.0 bottle.

--- Formula/logcli.rb
+++ Formula/logcli.rb
@@ -11,13 +11,13 @@ class Logcli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a10fe38792621b3b743251fdd674f8c633b9af41899da79a5e06b80252e107a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3882ad0907a93b3c78bb7473cbc9a695c60bda82179bcc181e25bc300b97c1a7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d4b16da47a7162c154f242444663ac2d87cdb7f04ba9fba70a30273784f90939"
-    sha256 cellar: :any_skip_relocation, monterey:       "a672473b537e1ff5013af85e251befd0b4f9e247b797a1dacf6587328d5d2752"
-    sha256 cellar: :any_skip_relocation, big_sur:        "10f1f7b6f129999e32f7b0464c5527dab25d25c429ed4ff2aadcd7c3589c111a"
-    sha256 cellar: :any_skip_relocation, catalina:       "905635ea6c008b0d64584550b4f13f1e44c673f62e7e4e203b0aff203e112b7f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c745400ce81042f0445ff608af40939b2207ecc0c8ae78b33f3d845d4f5c128"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e30c9d6da1e5a098acc4c57d5d2b08b102b4dd6a5d65e150a86f2e66006e89f8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a70ba2e57d29b261e09ac15f53489d7f0ff63e3f90d011b03cd9554d1bb99086"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b555e614c1f2bba433124a04310801ed89065cb69a1ca0086316375d173b1351"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a62eb2c98b7dc08ec0e93ddb86c5b189a7059a0ce7ea3133c37ef254a964b61"
+    sha256 cellar: :any_skip_relocation, big_sur:        "78e5824b3e1e5d2bbb52a83c338768e8a8655a36bc92e0a53c7e1003fb5ab29e"
+    sha256 cellar: :any_skip_relocation, catalina:       "26403cd3898a625a3dd02a561565a814a852bf4104c2649c95e5a569a9afcf65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5017b5f0cfc0ce2e77ee014deef9a81389448f0a0b006846aeda935be0b00284"
   end
 
   # Required latest https://pkg.go.dev/go4.org/unsafe/assume-no-moving-gc
