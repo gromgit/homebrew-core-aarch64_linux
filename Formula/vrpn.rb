067c8178450b0bcc1dbb623fa92ffe8f8f5e3f57
FX Coudert <fxcoudert@gmail.com>
2022-11-13 13:17:55 +0000

    vrpn: update 07.35 bottle.

--- Formula/vrpn.rb
+++ Formula/vrpn.rb
@@ -6,6 +6,7 @@ class Vrpn < Formula
   head "https://github.com/vrpn/vrpn.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "30798e598e05078f5ce75ca7451df08ccef3810848f61f6870a440c802c2008f"
     sha256 cellar: :any,                 arm64_monterey: "e16ae039e897123feecad339bba4ebdb34773a30924ac0046e5785c86c37e243"
     sha256 cellar: :any,                 arm64_big_sur:  "7937578e438051b79f3270c6fbcd9025d1f4cf42785aa04d72d2c4d245733fa3"
     sha256 cellar: :any,                 monterey:       "2b172896f3d3c3103643f4d7ded96c1302730677741ee3db5b2cd1d94a65d4b0"
