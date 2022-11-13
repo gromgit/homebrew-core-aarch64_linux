aa9991d2c5c4f50c3348ab352298b4840a65a7bc
FX Coudert <fxcoudert@gmail.com>
2022-11-13 15:07:47 +0000

    detach: update 0.2.3 bottle.

--- Formula/detach.rb
+++ Formula/detach.rb
@@ -11,6 +11,7 @@ class Detach < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "43dbfbe7fe5ea211f28aff3bd251dad03ddb486ff8230bc35084b0c8111a0058"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f208e16128573ea8b839c8fe27a6f23e72dcf99064f088e4d552aec5358cf54f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "934338f4dbae7773162dfa2a6b83d3c72a0740747bd4494509a9d5ee20bf37b5"
     sha256 cellar: :any_skip_relocation, monterey:       "469c22339c28e7497bead225e1597d5ae4ccadd2589e355be65041bd2c2ac5c9"
