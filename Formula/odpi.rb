d09702cca90b0688ef4306369c5c5ad5bfa47da0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-13 17:05:59 +0000

    odpi: update 4.6.0 bottle.

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -6,13 +6,13 @@ class Odpi < Formula
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "24cb63205cf9f780e8fe7909e6c77314244f4bdc35dfe5f346a387d0cb395089"
-    sha256 cellar: :any,                 arm64_monterey: "59011081ee7517175e26d371e5912e601606e446beba91ee05f580c7ddaeda4a"
-    sha256 cellar: :any,                 arm64_big_sur:  "e59b9d886a6baaae0508bd5f48b595e38f4d198d8e01941b3eb63cd5554034af"
-    sha256 cellar: :any,                 monterey:       "0851623d06cab8d9c9460a6ece8bec0d12754191311c80bbbc079af3ed9a07fc"
-    sha256 cellar: :any,                 big_sur:        "96db72e780870aae6a876c6f72011efd34098d71120efc19376d9cb8f12bd66f"
-    sha256 cellar: :any,                 catalina:       "d069b0002e53fba6d4809a570a7bd0ddaa6dd95d239394282fc26ef3892c4e5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "229ec0232e420ee82e36b7652276e465ee469e8f69a52980cd1e504f24a1e888"
+    sha256 cellar: :any,                 arm64_ventura:  "8dd43b72e0d1945818be0256dbc4e077fac5a8a75daaf9489fdb9f325f0e57b6"
+    sha256 cellar: :any,                 arm64_monterey: "a9cc873c661724322d03bcaea830790ca64a9ae25939989945f90b7a912ac17a"
+    sha256 cellar: :any,                 arm64_big_sur:  "f6ee3453a6b5c559702039fc82791a5401926c1d8cb0f583227ece09fb3e892f"
+    sha256 cellar: :any,                 monterey:       "bf9c773514668087b579a17f821fdb57de75f150cf2bcc78e1eedf5d3c4a2634"
+    sha256 cellar: :any,                 big_sur:        "fab05b206080566c121e15dcf01c8611ea89ff15ef482d4cf7f0e177a57dcaf7"
+    sha256 cellar: :any,                 catalina:       "fd24291c44b33e9cfa376b41c14a4fd914d30c20c147a0551ddbbf79ea17fe2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f6d424058b8def1ae140613e19dca697b8211768c3d2a996b979977ef265f5e"
   end
 
   def install
