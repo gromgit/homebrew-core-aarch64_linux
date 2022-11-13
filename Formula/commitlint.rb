1e696ff9951e6cb9b12a1d057766826fcc420671
FX Coudert <fxcoudert@gmail.com>
2022-11-13 13:50:48 +0000

    commitlint: update 17.2.0 bottle.

--- Formula/commitlint.rb
+++ Formula/commitlint.rb
@@ -9,6 +9,7 @@ class Commitlint < Formula
   head "https://github.com/conventional-changelog/commitlint.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9b5b14c6519eb33d2d2194addadf4f5cc2cce104d144b39eccff3c6d8c92479c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "850358d8be90b8a73f591d1263b4033feb573e3b8dfb23f54f10b23e4337d3e1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "850358d8be90b8a73f591d1263b4033feb573e3b8dfb23f54f10b23e4337d3e1"
     sha256 cellar: :any_skip_relocation, monterey:       "c25843482c50c727a648b3fee20d3e9ccc705379658143faaefa398d4ee811ae"
