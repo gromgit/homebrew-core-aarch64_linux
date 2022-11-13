7abe0b93d807d298130a706a9e2cccd192832ee3
FX Coudert <fxcoudert@gmail.com>
2022-11-13 00:27:55 +0000

    ccd2iso: update 0.3 bottle.

--- Formula/ccd2iso.rb
+++ Formula/ccd2iso.rb
@@ -6,6 +6,7 @@ class Ccd2iso < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9e1e557828fbb5b8b991d0710a5e2489a82bc8dad3b6136d2ddd17f2ced86c5a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3a6f9de23756982dfeb30537a00671d0a3d51edb2e322a9d4285c3a89da3be63"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5744e3d028a63b27e0a6b9c8f0c839effbd4385f41af50bb44725d5310b61ee"
     sha256 cellar: :any_skip_relocation, monterey:       "16d7cbbc96fab04752d418f7e4f196e2d0d36e494a27002bf76f907d160cba50"
