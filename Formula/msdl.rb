885fbb4cca2ee28e3b5298d6ad418a0fc6c4e990
FX Coudert <fxcoudert@gmail.com>
2022-11-13 14:08:35 +0000

    msdl: update 1.2.7-r2 bottle.

--- Formula/msdl.rb
+++ Formula/msdl.rb
@@ -12,6 +12,7 @@ class Msdl < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6a22fd7fd9ae5684a10a7646d42a1397b700a31db017be4a40e95ad37ce2d02b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2bbb7be167030b97337113482fe1007cf0f48a9fbc343f590b19c9964827e71e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5afd80d5dc62ee3c7a65fe5214d2fe51d89f8eda7ae9bb358bab102f5dd65e6a"
     sha256 cellar: :any_skip_relocation, monterey:       "f41e17e53c1b292088d9f3160bbba5241b5e467e372c4ae860277038a4daf3e6"
