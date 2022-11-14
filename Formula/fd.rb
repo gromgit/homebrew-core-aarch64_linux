769a9550a8581c540904641663f0cf9aa688ec47
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-14 13:19:58 +0000

    fd: update 8.5.3 bottle.

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -7,13 +7,13 @@ class Fd < Formula
   head "https://github.com/sharkdp/fd.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ee51859a6e7b7891ebdd9fa47fcddbc93891e80e9abc14eb9e4452a2c9c22293"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "82a08148076268cafd169b92060efe1296a4dd829ba86326f39ed6a81a5a652a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a7bbd5f1ad51c5a1a5c954aeb18acd7d3ad87b97fb1a0000ed4769aad3c24b5"
-    sha256 cellar: :any_skip_relocation, monterey:       "3c9fe2714102de4053b37bd4277016813c672e23515fb68a0d091a07ef82b8aa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "79486b2d4a4382ad07c32115f19aaea1748861a388b3944d5a01bdf44d8931df"
-    sha256 cellar: :any_skip_relocation, catalina:       "f798baeb8004263db559093795473a21f18bd0b8382055409d20addb4ff24eb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b74b028ad4b0efa85811f595dbcbc5612cdfe58a37de107bba9dab1256c4e3d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2f4a818e289969385257b5418e264bce60b0c5346638d0c933acd93c13d4c8df"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "adabea848971a0d8f572124a90e9c67b95e7553d785c616b1445094b1522b114"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93909239940e2b5bb9c4d9742c4dc25cb82653a3737baf1d35d2d9cfc5545cb7"
+    sha256 cellar: :any_skip_relocation, monterey:       "7b334e80bd27fcbb7f7431f6bdea253c90704733c96010c0498477cc340b3807"
+    sha256 cellar: :any_skip_relocation, big_sur:        "45143a7f02340ae2f323cc89b74e7002f964dc84701a7bb1ef0129e409afb894"
+    sha256 cellar: :any_skip_relocation, catalina:       "ff657fbed920ac915a97df106ace9efb87af33ffc3b545200fa1dd6d70a8de07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7173e54cccdc708ab1c37a2d2af699c1ba7035510fef4fe2f0eb957c3f17ad42"
   end
 
   depends_on "rust" => :build
