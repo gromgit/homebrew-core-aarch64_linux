f372241347b0b0250fae526dbb08e95d73541b3d
Michael Cho <cho-m@tuta.io>
2022-11-14 14:43:11 -0800

    itstool: migrate to `python@3.11`

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -1,11 +1,10 @@
 class Itstool < Formula
   desc "Make XML documents translatable through PO files"
   homepage "http://itstool.org/"
-  url "https://github.com/itstool/itstool/archive/2.0.7.tar.gz"
-  sha256 "fba78a37dc3535e4686c7f57407b97d03c676e3a57beac5fb2315162b0cc3176"
+  url "http://files.itstool.org/itstool/itstool-2.0.7.tar.bz2"
+  sha256 "6b9a7cd29a12bb95598f5750e8763cee78836a1a207f85b74d8b3275b27e87ca"
   license "GPL-3.0-or-later"
   revision 1
-  head "https://github.com/itstool/itstool.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6b9f7a4ddf5a15f67751b6dc0429dd9226f2b8c1bf81d13b5068c29fc1d7e65f"
@@ -18,20 +17,26 @@ class Itstool < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "108962bdce11e7b353a94f61ae30c82212295963d157cd667fb9af6069801cbd"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  head do
+    url "https://github.com/itstool/itstool.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   depends_on "libxml2"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
-    python3 = "python3.10"
+    python3 = "python3.11"
     ENV.append_path "PYTHONPATH", Formula["libxml2"].opt_prefix/Language::Python.site_packages(python3)
 
-    system "./autogen.sh", "--prefix=#{libexec}", "PYTHON=#{which(python3)}"
+    configure = build.head? ? "./autogen.sh" : "./configure"
+    system configure, "--prefix=#{libexec}", "PYTHON=#{which(python3)}"
     system "make", "install"
 
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"]
     pkgshare.install_symlink libexec/"share/itstool/its"
     man1.install_symlink libexec/"share/man/man1/itstool.1"
   end
