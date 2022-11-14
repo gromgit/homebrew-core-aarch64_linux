3703f5284a45140f0c04bd52d7738ed6aa364b9f
Rui Chen <rui@chenrui.dev>
2022-11-14 10:05:54 -0500

    difftastic 0.38.0
    Closes #115727.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/difftastic.rb
+++ Formula/difftastic.rb
@@ -1,8 +1,8 @@
 class Difftastic < Formula
   desc "Diff that understands syntax"
   homepage "https://github.com/Wilfred/difftastic"
-  url "https://github.com/Wilfred/difftastic/archive/refs/tags/0.37.0.tar.gz"
-  sha256 "a49a329bcde18574e8152b055d5bbd2a10aa9c8492ca677fbf8963fad3220949"
+  url "https://github.com/Wilfred/difftastic/archive/refs/tags/0.38.0.tar.gz"
+  sha256 "0f7bc54875939a6a4d2d695f2e94d30573818d72b88c00f38b0bc502f79671c3"
   license "MIT"
 
   bottle do
