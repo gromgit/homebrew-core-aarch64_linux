7561b9e47ab6ca3a61816efe4afa9c4b0d083cb2
FX Coudert <fxcoudert@gmail.com>
2022-11-15 19:30:03 +0000

    libxshmfence: update 1.3.1 bottle.

--- Formula/libxshmfence.rb
+++ Formula/libxshmfence.rb
@@ -9,6 +9,7 @@ class Libxshmfence < Formula
     sha256 cellar: :any,                 arm64_ventura:  "a28329811422f189228b3fb3edddc4db7fdb54265d933da3c2379b90f0ba7e88"
     sha256 cellar: :any,                 arm64_monterey: "d54d02c984d1c3004dc80c6293a11a334074ef3ce84d5a411f3250bc9a8b22d2"
     sha256 cellar: :any,                 arm64_big_sur:  "a20786f8e3b41c75c7a8f6ec72e135d6e8a4bedfa9a052174f80467fc4e18b77"
+    sha256 cellar: :any,                 ventura:        "daf1efe95074195c6d97cc4f4229d08e931391b201a7188caf18d48a9c57ec62"
     sha256 cellar: :any,                 monterey:       "fb31054f7e6e05c95d9e06ce328471128fa536b8f154a6dd33aaa9eb804f1980"
     sha256 cellar: :any,                 big_sur:        "2a0ad3f3f628be1aae3ba5b3fc9f467fd5d855c5e6187c21e315875ff65dc8d9"
     sha256 cellar: :any,                 catalina:       "db84e2577b6d8ee05c841d8a779873a303da9e836be8afe0e1a8e3e18e777323"
