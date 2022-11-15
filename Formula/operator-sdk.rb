275a97be46ce8d284797a07f92bd5978347b3006
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-15 21:05:49 +0000

    operator-sdk: update 1.25.2 bottle.

--- Formula/operator-sdk.rb
+++ Formula/operator-sdk.rb
@@ -13,12 +13,13 @@ class OperatorSdk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f8617d8f881e7ea2dbdc1d5e1f6fe932a3b7f7f596df1476d65f32f67c446dad"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4b08d96a2714e9531523055151bb3f42d9d917c82d94663a15b4740f3a1e75da"
-    sha256 cellar: :any_skip_relocation, monterey:       "f461b3605ab0476b139cddc8c60ba2801a2b4f7777d94c6f5658eb519bb0399c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "40c4b0c0cf69f2a5abd4edb42b9a26216de69d8272e2408a86ea748633ae24db"
-    sha256 cellar: :any_skip_relocation, catalina:       "4f07f0027fd0581885f5dbb036ddb6d8d7b9c787bc60057a76f6f8599da23df1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46e192023a789467f2da0de90f993464324327a6fb0ccfb49a31b8c4a826db06"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "494cf83a780e852487fa33e7f951d279eff68f0e726ed66f92287a81a69e50cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "06df332feb22ebb880e38339d6ee2de4d00daeb339e0f48fa75ee7ec0377ef4f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "10ebf873db39facdca729344f99fd58a5de59afed9281c13744357d1c6787640"
+    sha256 cellar: :any_skip_relocation, monterey:       "9c1041a74aa3cce9a1a5e8c13321d615b1d464e80c83d693226fd9605de32bde"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b785386ca5f66dee46f12f21ea76dfa982e5b7faf83fdebf92806ca90a7294e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "5b23ecf65951e4585e459673e682c3b94fc4d32c100d648535e9343f9c2490ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c4c1ad04173ad197183d15d85833cae2a8e16c4564da6c906822086e80afc216"
   end
 
   depends_on "go"
