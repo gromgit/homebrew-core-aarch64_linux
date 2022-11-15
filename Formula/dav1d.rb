58c6637f8b7ac457324704f830a58ab68d8d992b
FX Coudert <fxcoudert@gmail.com>
2022-11-15 17:33:06 +0000

    dav1d: update 1.0.0 bottle.

--- Formula/dav1d.rb
+++ Formula/dav1d.rb
@@ -9,6 +9,7 @@ class Dav1d < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8fdc81bd7aba4e4ec7b06e6eb3cc38db6c789ef079d87fa1cf3c2891e9a42d1c"
     sha256 cellar: :any,                 arm64_monterey: "ed488f61f6809e006ff1ab3557ba1ea6bbd89c12ea47782afdeca1b01f6f2d18"
     sha256 cellar: :any,                 arm64_big_sur:  "0e15f91e99bd1a41408ea7d2773b2676e4339a4d369bb8de0b9be6b1dd0a1bec"
+    sha256 cellar: :any,                 ventura:        "67d10c5e36161d0a5e7a5c752c7883a62bf6c750b58fc7a0d3a8eb920183edb7"
     sha256 cellar: :any,                 monterey:       "c50893e5d767b31380e5dff32c1d934d345fa8656d794efbbf0be937f9bd5cd1"
     sha256 cellar: :any,                 big_sur:        "c194ecbe7768aa96b0d29094ef9d355f0b585d0281f705cb9d9bdf429591be51"
     sha256 cellar: :any,                 catalina:       "4025ff5fb02272858f31890fba825eb440cf2f341de4be2e73505a608ba436e4"
