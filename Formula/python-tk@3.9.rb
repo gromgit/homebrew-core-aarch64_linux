55865dd83f89ae8e91aa46905619563d4ea79e0d
FX Coudert <fxcoudert@gmail.com>
2022-11-15 09:56:52 +0000

    python-tk@3.9: update 3.9.15 bottle.

--- Formula/python-tk@3.9.rb
+++ Formula/python-tk@3.9.rb
@@ -13,6 +13,7 @@ class PythonTkAT39 < Formula
     sha256 cellar: :any, arm64_ventura:  "6e4fdcd1e7342394a2da4f1c6ceb8ed4c2ac540ff1b3e496dc148574e1402730"
     sha256 cellar: :any, arm64_monterey: "24dc7d6c49d431eff030f1749f4b3d6432052a2fde4299aa9c13eb19ce7cb8df"
     sha256 cellar: :any, arm64_big_sur:  "811d8739853753f49d2079e19603e68f17265a56716c6f137cfcf52723631f80"
+    sha256 cellar: :any, ventura:        "63acc24ce6099238690bc2a4a7f78b68773c39bf94f0e7db31d76bb6594b9091"
     sha256 cellar: :any, monterey:       "d5a742fcc6d2eadd624ba68097d1a86f2a32cceab5e15dd5b2079a4f537cdbaa"
     sha256 cellar: :any, big_sur:        "b3c734d61fd58a594b86a5a439b646740266ba141e8c7191eddef90115842392"
     sha256 cellar: :any, catalina:       "0105632047409a2dfa98559ed508a401fa5ec92eb3955a8fde977d67a24057aa"
