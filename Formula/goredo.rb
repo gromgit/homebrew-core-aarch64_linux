22c5ecdfba236fb80c3f0eef2e5081304a699301
Porkepix <porkepix@gmail.com>
2022-11-15 01:33:50 +0100

    goredo 1.28.0
    Closes #115762.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/goredo.rb
+++ Formula/goredo.rb
@@ -1,8 +1,8 @@
 class Goredo < Formula
   desc "Go implementation of djb's redo, a Makefile replacement that sucks less"
   homepage "http://www.goredo.cypherpunks.ru/"
-  url "http://www.goredo.cypherpunks.ru/download/goredo-1.27.1.tar.zst"
-  sha256 "98c1a8a189b33753fcd61e1dbdbe627dba3820621d2032a3856d89829eea08d9"
+  url "http://www.goredo.cypherpunks.ru/download/goredo-1.28.0.tar.zst"
+  sha256 "9a5cdaa2c6fb1986b0d5d7ebfcd97122b0d7506fc30ca3da0578b04461d53c67"
   license "GPL-3.0-only"
 
   livecheck do
