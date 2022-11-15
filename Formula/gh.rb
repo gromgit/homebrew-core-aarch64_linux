d448780c88854891a10fffbe51e5b461ddddf49a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-15 09:05:04 +0000

    gh: update 2.20.1 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -13,14 +13,13 @@ class Gh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b139f0540beb929e9203370e6a09e782d6c03431fcd3cdd4ce2938b63f8ba471"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d825d65f18830c3fe0ef11a47c3381a16d3ad8add872abab92de667dfb8d093"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d31f8a71350f9cf12af7fad3d7eb7abdbc7cd6baa6e6f06f9ba3795ab83eb7e"
-    sha256 cellar: :any_skip_relocation, ventura:        "f93172cc35faff461ca93757e464cdcb963a7c93ada5c4e573bfff0ef45b2384"
-    sha256 cellar: :any_skip_relocation, monterey:       "0f247f159f7f5fe92c2ac50f79f58b8db0cab700e559e0f58bdac98cfff125e0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bddfe15b91c862434c3eff4ce7003c6bff6260b7d923fd460cfc1a858942b738"
-    sha256 cellar: :any_skip_relocation, catalina:       "539ba00e04960b7c7ac72357d4d164af8aecb88a363fcbfc17b34186529de782"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eca62f7c33280a9c094b75becec4724d2ca958d126c6be9482e238daf91a8cda"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "785f22aef70e2da36c2af80114681078f8b45d09c61ba359c9e5276860417571"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f2081dc6dfe108cb28dbb9aa24887a16f9d539acc223fbe8b982e387f5c8d846"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b56d4590d3db25eea0fdad0e782b082051b3d0450525cb1aac5261a2313528d"
+    sha256 cellar: :any_skip_relocation, monterey:       "51c61ba76236526b84f3c87c0e16ee72fc6eab50c639657fd6b5756b5b12e144"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0fbfc3fdce1cbe1434237371b7bd58a460e47cd582b6b09df98a35622d562823"
+    sha256 cellar: :any_skip_relocation, catalina:       "2a8327d05d50d9ca28d5a8d4b5a06e5b90a8b3408cc0a9f774b38b61e22b560c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03a9dda6f66bc965f498bcdafc439a5d36e38d389bd78cf966ac69e3b451e78e"
   end
 
   depends_on "go" => :build
