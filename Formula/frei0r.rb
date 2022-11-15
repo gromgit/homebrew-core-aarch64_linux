8d5d26359879af316ddfa2da42dc25aa8f483dbf
Patrick Linnane <patrick@linnane.io>
2022-11-15 07:01:59 +0000

    frei0r: update 1.8.0 bottle.

--- Formula/frei0r.rb
+++ Formula/frei0r.rb
@@ -14,6 +14,7 @@ class Frei0r < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "40373a9832b28d15c8ffaa880b775882c25b0ab93ab31ae19ec2f2a27799731b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "dc29537f1ea00bd0f303886fa5819ef44890f0435b12276919c952104843247b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c45ba867a2cc39664a405db9cb2ee9f97289f101fff6be8ca9ba88dcfcb19f8e"
+    sha256 cellar: :any_skip_relocation, ventura:        "49619d94f0580ee04d812a1e598d266f31a571cdda1bff9a297fa11b0c96aaf4"
     sha256 cellar: :any_skip_relocation, monterey:       "27768088554351600fcc15987a3671cf7cd133533c41fad06776383c4f96cb2c"
     sha256 cellar: :any_skip_relocation, big_sur:        "e97f700c8a5a2df70aeb941217314d313df4f343992c9dfa8a4f1b80300add27"
     sha256 cellar: :any_skip_relocation, catalina:       "0287b2caef7477439e9e07420e68ba794524b9348807c36903d5c22caf84134e"
