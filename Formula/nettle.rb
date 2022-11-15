c3d5bbfa9dda66eb8e75b6a4a9af31245a35c55c
Patrick Linnane <patrick@linnane.io>
2022-11-15 04:37:11 +0000

    nettle: update 3.8.1 bottle.

--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -10,6 +10,7 @@ class Nettle < Formula
     sha256 cellar: :any,                 arm64_ventura:  "4d691aa1139d55ac8b1fc72ffc11d2374fa74fb591bad3839af009a52758d011"
     sha256 cellar: :any,                 arm64_monterey: "f2fa03ad5664fdcf8475c1490a22f66d26056779911fd92ae2cb0d36998319a4"
     sha256 cellar: :any,                 arm64_big_sur:  "6b169f8f81ec4dae1a1137f7d8738a4acdb24806c1057039791bb58dc9fb67d8"
+    sha256 cellar: :any,                 ventura:        "6ad1de919308d5853d012287df427ec3b669981d4459a09ef83c8d36e53e8154"
     sha256 cellar: :any,                 monterey:       "19eca950c962860d44093db86226b8a07ca045973c15f7d3b0de2acd1cfad3da"
     sha256 cellar: :any,                 big_sur:        "48493dc5881e45b030b1f39d379fe70d6d8707063766c0f223e20e190d8de4ba"
     sha256 cellar: :any,                 catalina:       "185a433efd966756372f46bb0669eaaac9883c6f9a149d2d7e0ed33df661d1fc"
