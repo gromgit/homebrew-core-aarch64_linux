2d66262408231ed8cdb46a8c4dbb103980bb6d91
Michael Cho <20700669+cho-m@users.noreply.github.com>
2022-11-15 22:33:14 +0000

    libxcrypt: update 4.4.31 bottle.

--- Formula/libxcrypt.rb
+++ Formula/libxcrypt.rb
@@ -6,14 +6,13 @@ class Libxcrypt < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "7b0411a9c5f0e83bc20b2da0ca51ec46ababdbae2676726f8e8d978f25d83ea4"
-    sha256 cellar: :any,                 arm64_monterey: "c4a722e21edac36175956c80012714d7476b53b9182ef3176b93c4a5227a6b3b"
-    sha256 cellar: :any,                 arm64_big_sur:  "7c48131e7e7e3ae916c5f0506d5cd68d206fb57fe6c3f21bdaa04402a074fefb"
-    sha256 cellar: :any,                 ventura:        "db0a81ea35aadd079535df2b89e14bf16fcb01d93d2347942b9611205331493d"
-    sha256 cellar: :any,                 monterey:       "bf14a761837d3de9e5b6d527522db7dd1771ca6ad9645b2cd024a6de33862561"
-    sha256 cellar: :any,                 big_sur:        "649e7342c9f290301da598bbfe6046647ec64e9deb273822887ba7c01089fc39"
-    sha256 cellar: :any,                 catalina:       "5b14b3e17fbe2925ca518e5836154299443f5fc840b32bc86759cf29f8503134"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "04bd1d2ae46eb4120621c5f5a7f99bbb47efd0e3474a74d6e9cab0e36361738c"
+    sha256 cellar: :any,                 arm64_ventura:  "7cef5fa29821c3e9786693e506adc29fd1917f77f699a31e4512b2bd5c0c289f"
+    sha256 cellar: :any,                 arm64_monterey: "aa876651a2ac195bd55bd06931c79de0e32b1ef8ea9b3171f2fdda57825c5977"
+    sha256 cellar: :any,                 arm64_big_sur:  "a7539232c00c67015416429f96555539ca8ed040935bb5605f174a1b568c1588"
+    sha256 cellar: :any,                 monterey:       "36f09ae68a5cf7356c744f1e1c80c4977cc656e5c1841deee1dc58973ef677ec"
+    sha256 cellar: :any,                 big_sur:        "922f1f6216b34a6ece4f865e252f061a57a59770c395f67865a5d407d28b4073"
+    sha256 cellar: :any,                 catalina:       "8b414f01813fd8cbbe178baa526d5e33c46dd35d92f7bddd4a923009169c87f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56e819c055eef672771e3779771b33892572d7c035316f3f1602d79badc4a8fb"
   end
 
   keg_only :provided_by_macos
