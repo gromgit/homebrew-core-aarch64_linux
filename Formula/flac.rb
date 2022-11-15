f537aeb908923c1409f2939fd805ae10ec37d9f4
FX Coudert <fxcoudert@gmail.com>
2022-11-15 11:55:25 +0000

    flac: update 1.4.2 bottle.

--- Formula/flac.rb
+++ Formula/flac.rb
@@ -23,6 +23,7 @@ class Flac < Formula
     sha256 cellar: :any,                 arm64_ventura:  "4e8f1c6298d840212b30402137e1541750e7769bddf1213a3b4cbf18e960e0bd"
     sha256 cellar: :any,                 arm64_monterey: "fafbe1951be9eebdbd9daa08f3cec73fef2e12e2aa4603dbb45b95c36d47622f"
     sha256 cellar: :any,                 arm64_big_sur:  "91181078081ba0aa0af6738ef4aef7ff9ac53d1860b1a3a9803f3767a5ad8270"
+    sha256 cellar: :any,                 ventura:        "83b9a42d2058d67691ec930f01c17ae88e11a68721e751b9ea85c26a6407d09b"
     sha256 cellar: :any,                 monterey:       "7afd024db3ea4b5ee31f91b429570de8858b642d068d1d4367a531f6a3568f6c"
     sha256 cellar: :any,                 big_sur:        "3664f8a725c89f20ebc11ecef657ce848e00322b3750306db980ad23370cab89"
     sha256 cellar: :any,                 catalina:       "9e4c84335e37b5898fd95281731a90e9a105b8ca8000427de0693ec0c1dc78e6"
