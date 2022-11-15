19047946fdf166525b153396f1c689e1392d200c
FX Coudert <fxcoudert@gmail.com>
2022-11-15 22:20:32 +0000

    libxkbfile: update 1.1.1 bottle.

--- Formula/libxkbfile.rb
+++ Formula/libxkbfile.rb
@@ -9,6 +9,7 @@ class Libxkbfile < Formula
     sha256 cellar: :any,                 arm64_ventura:  "d168e608eb86d667cb38159b7a1d8a7075dc2a27452ddd01a79e98f774768373"
     sha256 cellar: :any,                 arm64_monterey: "ee43a751acb1d2368c2002d81b87c71c4729a34aa95c18540071cd1b2eaf1f8b"
     sha256 cellar: :any,                 arm64_big_sur:  "e33a381dd6b30b86920adbbd64c48e18e2cc549bcab25a6c8f40c72705fd8fc2"
+    sha256 cellar: :any,                 ventura:        "662adb077f1619cb46c95aac21cc79bfa34c2334a12998de7a1f5ebe6302565e"
     sha256 cellar: :any,                 monterey:       "4d0447b062d2b7a25314138c27c7541101d494ee3ea366905bcbbe1265cdadb5"
     sha256 cellar: :any,                 big_sur:        "d03791354356b615e28d6764beb46ec25eb02b5400f9ab44afc9344886997f03"
     sha256 cellar: :any,                 catalina:       "f9ee4cdf0be6c785c3a1eef84490cfb3f9caabb0f838d38a3369c2493fed825c"
