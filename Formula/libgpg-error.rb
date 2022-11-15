f3e80cb8da01290fe50a1f1daf0dc5c888c8d9ba
Bo Anderson <mail@boanderson.me>
2022-11-15 04:12:14 +0000

    libgpg-error: update 1.46 bottle.

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -14,6 +14,7 @@ class LibgpgError < Formula
     sha256 arm64_ventura:  "1955fb0ad761875d609cce71140d83b0b313d0b8e09f7a4d10de021aa04a474a"
     sha256 arm64_monterey: "9cfc7363abb102b284578c13d0e9b99a786bcd262e159541eebf6283c21c66c5"
     sha256 arm64_big_sur:  "c023005814ffc801b659d5819ce7dc3aef4906fa538b90ad5526bfbc5fdd8a98"
+    sha256 ventura:        "13b643a6d383301f0df8dc7904398cda155d4417a4092adb6880881d6f21f981"
     sha256 monterey:       "ffd831497a499fd7af47d69ab535a2eea51fcc85cea1db07ca748da77f64272c"
     sha256 big_sur:        "bbc14e9b5e8e37b7ce5a498439097d4c3fe33c9126f9266f5c3089ec674c002c"
     sha256 catalina:       "80e9fc05b831780e67221d972957088c15b644d83f685ffed9db22d4587f27c2"
