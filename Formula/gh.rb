287365ca2658fd505898cd378d1d899bd3baadbf
Patrick Linnane <patrick@linnane.io>
2022-11-15 18:29:28 +0000

    gh: update 2.20.2 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -16,6 +16,7 @@ class Gh < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "696d8f98191d8360836ae3a80a695d3cb497d4e78433105d1d44812f2a2d2b77"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "34d0819438fbabd70c231b185a8a9ffc0d630014ea27da5ee641f2ebd404c921"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "445007868988126eaf3a8d6cf4e3c319ea3ad71d5f780c464a6538f8152d2f25"
+    sha256 cellar: :any_skip_relocation, ventura:        "6a0bbbd55d91512479ef8ab92ff16c39011e403f103b2263babf9f96782837cc"
     sha256 cellar: :any_skip_relocation, monterey:       "c385e7cc5c95268ca004bf0cfe925e9b0bba1e975b4e3dd46517c8092b70d2c5"
     sha256 cellar: :any_skip_relocation, big_sur:        "cf56dcb93b534c432ae2483263fc3d7a987780c182433fd6be7242a6a9570279"
     sha256 cellar: :any_skip_relocation, catalina:       "ef84a020918b10e065771f01133770aae833ef065f5b608c5bd33bbb1cb7cbec"
