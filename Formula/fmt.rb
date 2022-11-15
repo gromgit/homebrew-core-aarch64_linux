199127ec9184b3be2d6d3a17a3bfc2fd13c6c39a
Patrick Linnane <patrick@linnane.io>
2022-11-15 06:08:34 +0000

    fmt: update 9.1.0 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -10,6 +10,7 @@ class Fmt < Formula
     sha256 cellar: :any,                 arm64_ventura:  "366409825e592504a1a68e63c5c7415d1d90a08f4c4395bb856efb245b85cd3c"
     sha256 cellar: :any,                 arm64_monterey: "dfd44ffca7165995a6cdca2191e4e5bb6563385a71375fcd22a002e5e63d3133"
     sha256 cellar: :any,                 arm64_big_sur:  "20658415047bbd2856d1d4b5cd4886daeab829948a4f546e24bd176ba17d9e97"
+    sha256 cellar: :any,                 ventura:        "34b2d0bfe62a0114aff46c0ae2a78e5d6e789ab7f55bc8035dbde7ac22a10a6a"
     sha256 cellar: :any,                 monterey:       "324344e5048ea4bf92352dcdfae63832d447c0d27cc01d45eecc6a2bebee6b5d"
     sha256 cellar: :any,                 big_sur:        "3616cabdb7e41f47275dac7bd60bc3496f375550eb0462f96edb5736c31d6c90"
     sha256 cellar: :any,                 catalina:       "d99a9571471d998a20f654f32499096037b4db5c33819d1c324cd29cea8f04bb"
