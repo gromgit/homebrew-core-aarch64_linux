f0e0b6348d7365fbb3c43e03935a9f9623f5aeea
FX Coudert <fxcoudert@gmail.com>
2022-11-15 09:14:49 +0000

    libcython: update 0.29.32_1 bottle.

--- Formula/libcython.rb
+++ Formula/libcython.rb
@@ -14,6 +14,7 @@ class Libcython < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d5e3ab9ad1b8acc88814849923b2dc1b38b9c92360a25262c62ee2f3728a2a0c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5fa9e558d708f5147a117e8f2294ca7da536b1ef6bc663720f2c71ff807eb322"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "19b5f477c479bb2e123a7bb6256f68332f9eefdb107656aa86cb1a80a7bb91a6"
+    sha256 cellar: :any_skip_relocation, ventura:        "7db706db1aa710a2b75b9df45cd6706bbe895b462669b979f9728b53ef02a4d6"
     sha256 cellar: :any_skip_relocation, monterey:       "cfc83a5fae8efc1ba21d7002f82a1e286e94ec44d352aaf25c10b6b98f2a08bf"
     sha256 cellar: :any_skip_relocation, big_sur:        "9e0689718cabfeff7f5fe3f57705b26ddd0982142774c689a072f52af7628ee7"
     sha256 cellar: :any_skip_relocation, catalina:       "aebb47718129ee4b0a543330fc9030b19011b5950ce17945a2ae315f131bf35b"
