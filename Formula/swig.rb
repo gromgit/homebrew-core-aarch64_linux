b349b1483be8bb51d76f9e287bcbd4f50dc77fd6
FX Coudert <fxcoudert@gmail.com>
2022-11-15 09:33:48 +0000

    swig: update 4.1.0 bottle.

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -9,6 +9,7 @@ class Swig < Formula
     sha256 arm64_ventura:  "da2a0ed31e7c512de65131d162be610f2009518386a70f69db1b9134a94506ac"
     sha256 arm64_monterey: "80291f33f362db51985ec93db7a80a2a8efdfc377e3bec1175696707f9254a6b"
     sha256 arm64_big_sur:  "5825b5b4d280fa33d16194cce2db7a32a5bfc3f01d424027e78ccb1ef2c39274"
+    sha256 ventura:        "9374e7b731037ae529d1a8449b798ef7107580c71d8f02592929d19075a9c246"
     sha256 monterey:       "dafaef7743b38de422618126d5bc9a7727c94c8e253258c8994f8aedd46f574e"
     sha256 big_sur:        "79f2d7c2d3b443d0b3a0ccaeead34da0e5d3e66963d9c14130ebe00f6f2c4a2f"
     sha256 catalina:       "c0b65fbcae8384ff3433a6b933a85faa5c1af090cbea90026b5b0cd82cc5dbce"
