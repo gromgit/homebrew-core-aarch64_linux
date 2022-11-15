a30613bc6cfa2a879208059e73d3821b8388b71d
FX Coudert <fxcoudert@gmail.com>
2022-11-15 12:06:54 +0000

    json-c: update 0.16 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -16,6 +16,7 @@ class JsonC < Formula
     sha256 cellar: :any,                 arm64_ventura:  "cad45d82dc941532f7863eeca9a355764f5976b3d772ab2fa3b5a1e0a7a626c5"
     sha256 cellar: :any,                 arm64_monterey: "0f71590b7e5045f6c8c892527a7f6f3e7d3b5d7cf0f43b93cf50bf662cdd84f2"
     sha256 cellar: :any,                 arm64_big_sur:  "efc09489ecb843511393e5166d8351ab8bf3c5e263d91a5838735b574e8dd4e7"
+    sha256 cellar: :any,                 ventura:        "98957cbd34b40da1964eaf3a7b35271e46c7d5c62c69d2dfadf1001d40f63042"
     sha256 cellar: :any,                 monterey:       "2d032280aa3e434671354c91a191b865f8de9290f5f8fba2336cac88037c1f33"
     sha256 cellar: :any,                 big_sur:        "e31bc0e529950442ccc42719107e14de5e995847f8abab34f0961d19d9d45976"
     sha256 cellar: :any,                 catalina:       "425524c4c8a10f50da278278e624b4dce61ff94f464511095d30a17a3bea68ce"
