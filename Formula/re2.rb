bea01ae11ecb0c2d104b44a27ac0df8b48e5629a
FX Coudert <fxcoudert@gmail.com>
2022-11-15 14:09:16 +0000

    re2: update 20220601 bottle.

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -22,6 +22,7 @@ class Re2 < Formula
     sha256 cellar: :any,                 arm64_ventura:  "ec300b2c312237d3cdf4b0aa48370f74f7164b57681ddc690ec71a32c5aca931"
     sha256 cellar: :any,                 arm64_monterey: "5364a5325131113abec97c05282f85c29572094ac0ad2cbce9571e1dffd8fda1"
     sha256 cellar: :any,                 arm64_big_sur:  "3d60ddeca55c3d3e629bf2edbf5cfa0b0eeaaf61360b2b9adbde91979c2595ba"
+    sha256 cellar: :any,                 ventura:        "650e2526f86816fd027d3538d11fd63762f8cc7789dac5cfeeb9af00ad622358"
     sha256 cellar: :any,                 monterey:       "568670d04b9bc92a07f6db624acda793834ad1c8bcb2f120386df77d7bd6385c"
     sha256 cellar: :any,                 big_sur:        "e52a5ade3edeaf1d62170e1f4b43a4e73b71178d0e5f91300a4e29442905e7bd"
     sha256 cellar: :any,                 catalina:       "2ca1a5a803d348e1487da767936165a12224b65940de3927e83ccd81c90ed443"
