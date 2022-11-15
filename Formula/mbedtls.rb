1c1ae0d911f32b99e37991c99057cc11036fe171
Patrick Linnane <patrick@linnane.io>
2022-11-15 06:50:18 +0000

    mbedtls: update 3.2.1 bottle.

--- Formula/mbedtls.rb
+++ Formula/mbedtls.rb
@@ -16,6 +16,7 @@ class Mbedtls < Formula
     sha256 cellar: :any,                 arm64_ventura:  "0948e0625c71944abefcf31521f53675a69a5c22578e8d90be287eeb016b2625"
     sha256 cellar: :any,                 arm64_monterey: "21009291bc1c17051363355c2815cd4826603f3a3f4d0d071f78a78be2b22ae9"
     sha256 cellar: :any,                 arm64_big_sur:  "772fc2c15da4aa90218b3afdcb1c2cd076dd8333f89c951a4946fca5cbfcd227"
+    sha256 cellar: :any,                 ventura:        "98585bbc5e72a7006d9da98fcb34f525224d5c7261004487d312dd4bb6d7bbdb"
     sha256 cellar: :any,                 monterey:       "b473d94ce270b5b795b842708329334a04eec7cb43fa15f4e06d9e66f8d48e3d"
     sha256 cellar: :any,                 big_sur:        "c6788160e03dfe51b88224991adecef63c269c1033349aa66e5b2b69c9247e88"
     sha256 cellar: :any,                 catalina:       "a3cba57dd337562c7793f7afc32e6ce0406c5ad31211a3250ebe7cd0b3ce9229"
