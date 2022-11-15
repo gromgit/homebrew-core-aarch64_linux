8a0ded6d1d8110c578df6dd1ff1163143668af03
Patrick Linnane <patrick@linnane.io>
2022-11-15 06:02:54 +0000

    double-conversion: update 3.2.1 bottle.

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -10,6 +10,7 @@ class DoubleConversion < Formula
     sha256 cellar: :any,                 arm64_ventura:  "208098fabee3e07ea7ef4fa6f95ba28d0dd461d17df90c33d23b95b280b642e3"
     sha256 cellar: :any,                 arm64_monterey: "dcd8a50bf98490fae00d73325210c5f9f65f1b20a59a1979afdb28e9c91d3ba9"
     sha256 cellar: :any,                 arm64_big_sur:  "7996dcb8fafcc3aee6fee04da51533a15f297cb34de699a7ec0cfede53f4447a"
+    sha256 cellar: :any,                 ventura:        "8c37957d1059538da20b3d562b59abef18239b662c57d33e4a5d1b1f115965f7"
     sha256 cellar: :any,                 monterey:       "ab33e3194744d91e611c402d1f5ed5243ffa7bb9a776abec12b05d674b210880"
     sha256 cellar: :any,                 big_sur:        "f20cd36d2cb176b5ea9e5bbc15241f7a2f57bbea16196adfaa0ee51918541992"
     sha256 cellar: :any,                 catalina:       "2299213ea5c53ce8c80818d0256911227e7dd9f3c444eff84993d7b266180a36"
