6b5f0c30885028397d48a6ebf9bf20084396d17d
FX Coudert <fxcoudert@gmail.com>
2022-11-15 15:18:56 +0000

    apr-util: update 1.6.1_4 bottle.

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -11,6 +11,7 @@ class AprUtil < Formula
     sha256 arm64_ventura:  "534b2aef9b78f3e0c3af062fb496917b9ede4972c9e3a77675b840285cf862cd"
     sha256 arm64_monterey: "3fbbe5ef907dc7f8b259f49c0891987f088ecaa0ad0fd75d47345804ec5d2976"
     sha256 arm64_big_sur:  "72585edce1997ea0ac5be884f0fbed79f2746d3252e035ed63b1bd04ad501d94"
+    sha256 ventura:        "fc26e9e914a6240452123e48d2f4bba40dcbc75dd4f797ddf0c3b91898fb4c6f"
     sha256 monterey:       "7695efa17e538d71459020c3838081629629c3c1169bd9ac166865d2bdacb213"
     sha256 big_sur:        "94a9d60abb422a135295ac6c8425af4c72a0f49f46323aa19abd4b358c03270e"
     sha256 catalina:       "8e62fdfe79eaa75a700fb97ea3264aa5bcd77dabcf526240d35537325387c353"
