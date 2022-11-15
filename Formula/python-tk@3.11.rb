f8e18ccb25d5e49234542ad2dbff714cbd1314ed
FX Coudert <fxcoudert@gmail.com>
2022-11-15 09:53:58 +0000

    python-tk@3.11: update 3.11.0 bottle.

--- Formula/python-tk@3.11.rb
+++ Formula/python-tk@3.11.rb
@@ -13,6 +13,7 @@ class PythonTkAT311 < Formula
     sha256 cellar: :any, arm64_ventura:  "609fa4a576dd6fe98513545fa63026ea2e441bc93c5c9c7ffbd0038254465b56"
     sha256 cellar: :any, arm64_monterey: "8c238e600565e35bc43eaa2031d5a65b254c2312bcafa1e1e45551bfd1039093"
     sha256 cellar: :any, arm64_big_sur:  "5980d76b9d77e5307c0a1c5638f237df590d5e8dae8499da4e73a85661ca1cba"
+    sha256 cellar: :any, ventura:        "8ae9fefeeaad3f5822911005c0cbd9e3574177a3880da36449c2d66857e324c3"
     sha256 cellar: :any, monterey:       "c70b3353802b12baa1a9ab4797992a8560794e3a4324879091c99924a18f4cfa"
     sha256 cellar: :any, big_sur:        "35fe499d862b7dd3884baa2cf7ab6c9ca5dfbca5b1fb707036ba16f8c4dc7072"
     sha256 cellar: :any, catalina:       "4238e0b6ae7043908cfec513cbb8346e7e9a4435dac2c06b1bb67d6a9934a579"
