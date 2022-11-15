1874465479d77e7f992060f40d9e8ed73ba46a08
Patrick Linnane <patrick@linnane.io>
2022-11-15 06:34:51 +0000

    libssh: update 0.10.4 bottle.

--- Formula/libssh.rb
+++ Formula/libssh.rb
@@ -10,6 +10,7 @@ class Libssh < Formula
     sha256 cellar: :any,                 arm64_ventura:  "070835503169e43125a6c78d21229e8b1489c0ec37d2899e3658784513434789"
     sha256 cellar: :any,                 arm64_monterey: "3932e63ecd8236e305a43d1aa27c98957b8c513171873d21fd9858671c1b4a5d"
     sha256 cellar: :any,                 arm64_big_sur:  "3b2886c28cb74cc6eec70cc7fe0225ec55567848df8cf5b97cd2226b56a91675"
+    sha256 cellar: :any,                 ventura:        "977c22fa0c47b658e37b719914f8896e7f32057ff10e2151ed25606d9af6149b"
     sha256 cellar: :any,                 monterey:       "4c965d61eef0b2ce050e191f399f7f84d9300035d444af06b72b7401e0927e20"
     sha256 cellar: :any,                 big_sur:        "8338dc10bac32e5c6c3a5e4004f6067c414ff179658271e7c886385efeb8ef43"
     sha256 cellar: :any,                 catalina:       "4e02e90e4e3691c4ccfdd67ffc66041b15d60d8bf5b75373f84dcf49d28c93eb"
