e34a5e87082598335475e7a3da91872b72aff693
FX Coudert <fxcoudert@gmail.com>
2022-11-15 10:59:18 +0000

    libid3tag: update 0.16.2 bottle.

--- Formula/libid3tag.rb
+++ Formula/libid3tag.rb
@@ -9,6 +9,7 @@ class Libid3tag < Formula
     sha256 cellar: :any,                 arm64_ventura:  "ea3be63c9af50a5727ffea6b348d2dd1f71e3e1d002923d82d78baafd15833b3"
     sha256 cellar: :any,                 arm64_monterey: "5f3d9e17a9e11f9e7ce446b248031ff52f3ad7a3fe6ca45dfeec015faa3e7720"
     sha256 cellar: :any,                 arm64_big_sur:  "fb715e7759925e8f295d11ec506681abb2f72657ba2394b05c01fb0604c293f2"
+    sha256 cellar: :any,                 ventura:        "80ced912c1c8af4628cc7c5c377aca60122d24fe627d59b0aa60c01edd71335b"
     sha256 cellar: :any,                 monterey:       "9b079278969ce8cc27f7740b3a071f1060970164a2e047aeae0167e185eee3ff"
     sha256 cellar: :any,                 big_sur:        "ffeb1df56aa2f9a1cc0e9dda5116cb1017fa3055a47e63eaefc159f5ae5db8a3"
     sha256 cellar: :any,                 catalina:       "110a77d41509a1fe27d1638005f6b903488e7591052e1d0f327555f6e72fcf2b"
