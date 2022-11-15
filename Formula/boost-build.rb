fcf4492e073541ca193a0e941a123d56947c34c3
FX Coudert <fxcoudert@gmail.com>
2022-11-15 17:29:11 +0000

    boost-build: update 1.80.0 bottle.

--- Formula/boost-build.rb
+++ Formula/boost-build.rb
@@ -16,6 +16,7 @@ class BoostBuild < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "fcad64c30f166d2252b1c4e7f54dbc90343f2e89e7981ef44c6ca0775fae630e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9ee705b9b468e8fb0a0ee5de383d92c45d99f76922588af6b2aff47e587fee79"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b01468ff38165cef62b85462180653af35d0c2582e83859c7fa35a2116f89f60"
+    sha256 cellar: :any_skip_relocation, ventura:        "880e693b12ab4b872f1b54b89918193bb43028232a93743f23c838e3a2bf6ecd"
     sha256 cellar: :any_skip_relocation, monterey:       "1ba103ced650911393ab68afedc3aa33c77f9fe1b9518c34a2b0dd6c00ffae26"
     sha256 cellar: :any_skip_relocation, big_sur:        "482592978d87a195871fe22dbad2be0a0a5ce9364b467b8953b2323af15c9c0d"
     sha256 cellar: :any_skip_relocation, catalina:       "7729a63edc79cbe0bbfd182ef9ffdd66b328785c48ad32be3b7b234ced401ce2"
