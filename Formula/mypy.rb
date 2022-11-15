c31dcf44c7e4510e7edc5a2d8b32eafdbae9b895
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-15 02:38:45 +0000

    mypy: update 0.991 bottle.

--- Formula/mypy.rb
+++ Formula/mypy.rb
@@ -9,13 +9,13 @@ class Mypy < Formula
   head "https://github.com/python/mypy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "76e0ca491eacb6514431676e5ba8600324c5be9aeaa99c40e92e8c1a2e050367"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "68d6def1c41bb87d80f114bfafbbdcb9959fffac98a3470221173863eee715a7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7759203e627a56162d3738b81bc597e9d558732e7688b131e37fd37b2a6eda68"
-    sha256 cellar: :any_skip_relocation, monterey:       "a5ba56368101349f0a1f31c01860c044d4f7a363f31dea2c4893fa5464274214"
-    sha256 cellar: :any_skip_relocation, big_sur:        "87b16bcb39cd3ce8b85cf2773cf0b6486594d9387f1c89f7ac298d3d4ea83ece"
-    sha256 cellar: :any_skip_relocation, catalina:       "a427c763249b9894beebe1cad90c96374bab93086dfb7587a0e205bf076c4f96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f87562c30601ff325da15b627981c131cf20d898527d579f1e1a0cd603d73524"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "895f00e65b177c34d4b4bcacb81c8d7f44ab9f9a9df88566a8f417b3c998b934"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "22ff690ce4f975ad294d7955323da8fbe809b6281851bd157b9620839ed8bbbf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4047975ce033bf2bda81b888ed622a7cf3d936240731df891a0a746dd57b316a"
+    sha256 cellar: :any_skip_relocation, monterey:       "02f44a88333b0a6f09eedc9c653b7d547db18e0256b0fab3e7ab0b44853d716a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b5e8e1a568aa9ce995fd07dcd11320dd9eae8b254c6c67a91615691b3539c9c4"
+    sha256 cellar: :any_skip_relocation, catalina:       "582e4f03123861d5d3831353452f440b96f169a45c44f221d64b96b078884661"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a38066ed71085b2af0027ce142024c02d06bc57ac2631dd06c57913ae5d8f7fe"
   end
 
   depends_on "python@3.11"
