9a92a5f015a31119a2b6148ef9d5ded81ea5dca6
Patrick Linnane <patrick@linnane.io>
2022-11-15 08:29:32 +0000

    liblinear: update 2.45 bottle.

--- Formula/liblinear.rb
+++ Formula/liblinear.rb
@@ -15,6 +15,7 @@ class Liblinear < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c3c757ddd4f6f1aad3f422c1a82b61f593362886a726d13a8fa88e5692247222"
     sha256 cellar: :any,                 arm64_monterey: "1dc65a0cd23d37bc4ef5676080e6a0e6b953289152532586791f6117d51c2ffa"
     sha256 cellar: :any,                 arm64_big_sur:  "729f76538f19c370424f033c884e46a9cb83cdb99d64703d6e5f7345bb170cce"
+    sha256 cellar: :any,                 ventura:        "bdb804716d739f566d8b1e9cd933183b2af7316e900747fd4fad0526150b4439"
     sha256 cellar: :any,                 monterey:       "5a5b15fee1584548ebdeb166c81efda7038d6941501b685be49775f9ed4775c2"
     sha256 cellar: :any,                 big_sur:        "356369ca1df9b188b922ce4bfd74f1e215eaa766a656b6ae0e9085314138d351"
     sha256 cellar: :any,                 catalina:       "a610ba5a1bcb6ee808f10c1bb2877139ee95d67406beb917f89eabf266445cfe"
