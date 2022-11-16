fabb70fb2dcccc2ee2021df2ba5b5f2ff387979c
FX Coudert <fxcoudert@gmail.com>
2022-11-16 03:46:56 +0000

    py3cairo: update 1.21.0_2 bottle.

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -10,6 +10,7 @@ class Py3cairo < Formula
     sha256 cellar: :any,                 arm64_ventura:  "b6ed8a697cc11c3fcb81b8599f468fef30feb367774e423b94c4a90f40298d99"
     sha256 cellar: :any,                 arm64_monterey: "835c501d1d5741824dd4a1e02cb0cc584bba31ad3de7dab749653ee626163417"
     sha256 cellar: :any,                 arm64_big_sur:  "4308fb117d43f27bd93ac2ae4f38091feff2272a474c560cfced167e8c1115cf"
+    sha256 cellar: :any,                 ventura:        "84e5f8909f3f8e11e9bae1a9e54775c62abe8de803a0cf7eb7047f01a7518c1f"
     sha256 cellar: :any,                 monterey:       "feb8a9be1270be213fa1f6be9de3906017d49389713e0c5cd347350fcb7341ed"
     sha256 cellar: :any,                 big_sur:        "31a5442bd5a4c6fdf2a07abadf5ad49bf895ae6819b939be4d22687fc74aed40"
     sha256 cellar: :any,                 catalina:       "46d411dc3b30aa251d78ac0c0883be898e69eea4a090c0c25c02297a7c5a6950"
