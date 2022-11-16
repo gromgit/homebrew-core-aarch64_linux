e111e2c758a79693cf837af1798ccee9a37ee9b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-16 02:39:25 +0000

    clojure: update 1.11.1.1200 bottle.

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -12,7 +12,7 @@ class Clojure < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4f745d2e6cb0caa9932a106cdc8ef95a6710bd61eecbf1cefdadeca2f920b1ee"
+    sha256 cellar: :any_skip_relocation, all: "4fbd839618f5808db69500f795fbd2bc2267d9fe7b1534a8b13584faaf26e224"
   end
 
   depends_on "openjdk"
