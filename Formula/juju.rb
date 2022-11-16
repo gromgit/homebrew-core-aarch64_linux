f270b3733d5a758638e0550ff38f882c765b912f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-16 05:13:48 +0000

    juju 3.0.1
    Closes #115890.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-3.0.0",
-      revision: "35c560704ee254219ae0c4a37810bde5278e99bb"
+      tag:      "juju-3.0.1",
+      revision: "6613e3f343bdbec74e7838e1a1b7e086672c88d7"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
