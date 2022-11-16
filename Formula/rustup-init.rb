1a85cd68b0f4bd69ec808d39c08c32aa9d9bf8ca
FX Coudert <fxcoudert@gmail.com>
2022-11-16 17:58:07 +0000

    rustup-init: update 1.25.1 bottle.

--- Formula/rustup-init.rb
+++ Formula/rustup-init.rb
@@ -10,6 +10,7 @@ class RustupInit < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "da743cec474c388a7774a7f7fecc458ee639d56cf4b47dc435717eb5e4476d54"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6125eed25505926f523304ebb665122a3d15b3de19b482b3b84008c93fe28fd4"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "597c38f3a49ada52ad08e863536254584294c7a4bf826d5c3ccc5ee658ff86f6"
+    sha256 cellar: :any_skip_relocation, ventura:        "ac71a435b74247270060dd2b0beb5fc479ada984aaadf9a777dd5ae9f3896473"
     sha256 cellar: :any_skip_relocation, monterey:       "381604c17ed5b69af016c416ba9c19945ec86a2576fbfa331d7baff9c97c5d92"
     sha256 cellar: :any_skip_relocation, big_sur:        "daf440be5aed32296c301664697f6e264c0262cc9949d04aed174de844fbdb98"
     sha256 cellar: :any_skip_relocation, catalina:       "2d62d01b7380cf2c0dfb81de3ecab619dc5350b35ce2edabc02062267023c978"
