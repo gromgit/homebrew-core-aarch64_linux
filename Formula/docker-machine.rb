d5bd040d11936921e89ac4433fa5602d25fcd44e
FX Coudert <fxcoudert@gmail.com>
2022-11-16 21:36:00 +0000

    docker-machine: update 0.16.2-gitlab.18 bottle.

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -12,6 +12,7 @@ class DockerMachine < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d6b906ed87831675cefbd6554179ada9005d798a07bbe93d9f394e1ffe83e77d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "26bb4c56e7e1d8c243bb36d8d379c25dce0a2e072454536b3b7336809adb065d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b511812e5c84d52bfb13b2d75c6a1f3755ff7836feceabb43dcf4229b66397ef"
+    sha256 cellar: :any_skip_relocation, ventura:        "724dafc5b67297498abb814963f6e0b3fdcd64a7a314888d2b2e6deb176b0674"
     sha256 cellar: :any_skip_relocation, monterey:       "4a413a0dfcbab114ef02c827794493b0c24bfc6c1dd460f5e5aec07a15f75742"
     sha256 cellar: :any_skip_relocation, big_sur:        "241213a65251e486711b8bf8c2ec7eb38ed5c1daae7cf699f109115136a0e38d"
     sha256 cellar: :any_skip_relocation, catalina:       "7496ea458a1554bb41cdc8b1160d9489adcf6ade5aa088990b513418c4b340f6"
