6d32ffc1800f052fe57e177931d76678ea4445df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-16 11:04:54 +0000

    aztfy: update 0.9.0 bottle.

--- Formula/aztfy.rb
+++ Formula/aztfy.rb
@@ -8,13 +8,13 @@ class Aztfy < Formula
   head "https://github.com/Azure/aztfy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "abffdff6981f5b0016c3638c1c459a4bf688bd249cedcccdef18847f280a720a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "83da354f9299b620ffdff845784772bc254bbe31a6fba78c430ad37a52812202"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "83da354f9299b620ffdff845784772bc254bbe31a6fba78c430ad37a52812202"
-    sha256 cellar: :any_skip_relocation, monterey:       "a38f8a1c8ba65951d1993cacdb4f310524d6b41f11c6251c22020a56e17d144c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a38f8a1c8ba65951d1993cacdb4f310524d6b41f11c6251c22020a56e17d144c"
-    sha256 cellar: :any_skip_relocation, catalina:       "a38f8a1c8ba65951d1993cacdb4f310524d6b41f11c6251c22020a56e17d144c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ea928014ffbbc9918f84b96ea7266ccf6862527fce66dcad28c4208b720842d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "795205276eb37c16f360d73e24af8a79386e5c361b75707e8c62704f824ac612"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "795205276eb37c16f360d73e24af8a79386e5c361b75707e8c62704f824ac612"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "795205276eb37c16f360d73e24af8a79386e5c361b75707e8c62704f824ac612"
+    sha256 cellar: :any_skip_relocation, monterey:       "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
+    sha256 cellar: :any_skip_relocation, catalina:       "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41161e9eb4380afce7c09b35d292613c25ae6da14536b17543e6b7279ddb6a63"
   end
   depends_on "go" => :build
 
