8a571b3c1335854ef79dbaf646c27587d0f444ae
FX Coudert <fxcoudert@gmail.com>
2022-11-16 11:57:04 +0000

    sdl2_ttf: update 2.20.1 bottle.

--- Formula/sdl2_ttf.rb
+++ Formula/sdl2_ttf.rb
@@ -9,6 +9,7 @@ class Sdl2Ttf < Formula
     sha256 cellar: :any,                 arm64_ventura:  "965b06109282c56c513b3836bdced71d34c7b532b42a5a0fad1188cb171f12f1"
     sha256 cellar: :any,                 arm64_monterey: "0717a73e3b400b75d4c95a1f041405d1e2a51d853084f4f4a2df509a0f5f66b4"
     sha256 cellar: :any,                 arm64_big_sur:  "3c330912742dc8adec3f1c84d72cd0c6d2042e07668b4e0949dd19a48db93399"
+    sha256 cellar: :any,                 ventura:        "e0801a169bc31d6340a5c6f57b289d92099449a43ae7a2f422356a85ec39a47a"
     sha256 cellar: :any,                 monterey:       "03664994cfddf5b0dc3c2e17882ac02d01e2f46f0c74b711cdaf8b850e97c721"
     sha256 cellar: :any,                 big_sur:        "1d882966dfe5f2710325b20c9aaa3dc37e4fdc2698c8fb9f3d4252a62a3799be"
     sha256 cellar: :any,                 catalina:       "b969e2ec3520ce18c9ab44e05e9fb4e406887c8d879d156c8eedc559ba9bfe30"
