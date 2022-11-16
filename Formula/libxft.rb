161c401bd8e7a6742804e98ccf79588c97ac449b
FX Coudert <fxcoudert@gmail.com>
2022-11-16 13:20:14 +0000

    libxft: update 2.3.7 bottle.

--- Formula/libxft.rb
+++ Formula/libxft.rb
@@ -9,6 +9,7 @@ class Libxft < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8175c1f034681b3ad8f88c049e592a75dd7ed74da468e4b4abd7b0c6b92fa6b0"
     sha256 cellar: :any,                 arm64_monterey: "f564c302672356b6c5ff8748310527503b8586c4c62214f5ea6bfd37d072eb9a"
     sha256 cellar: :any,                 arm64_big_sur:  "2d3b436bec612143dd68c2d76e70f8193c4400ca0df95cbf4d29f19b1e6162dc"
+    sha256 cellar: :any,                 ventura:        "d78514cadc16473ef16d6c2a3cf654f7435782b08720f965cb03c56f45f57f6c"
     sha256 cellar: :any,                 monterey:       "f0cc586372a1a2b67afb81965d741ab50453ba4d6dba97d73d701289a3d71e4f"
     sha256 cellar: :any,                 big_sur:        "2f40db6e5dc23843e700fa8b0ee995c7dfaa6ad1b2a2478fe5f834eaa796058d"
     sha256 cellar: :any,                 catalina:       "5724a911f3d4f07de517aee6da949f5705e8ecb8ad77e2bf2d64587cc8c7ca5c"
