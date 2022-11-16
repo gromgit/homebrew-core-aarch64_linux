1f5ea87811432e86b8a1bbd1b003461723dc32af
Rui Chen <rui@chenrui.dev>
2022-11-16 09:49:20 -0500

    wapm 0.5.9
    Closes #115934.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wapm.rb
+++ Formula/wapm.rb
@@ -1,8 +1,8 @@
 class Wapm < Formula
   desc "WebAssembly Package Manager (CLI)"
   homepage "https://wapm.io/"
-  url "https://github.com/wasmerio/wapm-cli/archive/v0.5.8.tar.gz"
-  sha256 "b122e2a212c1153b80c541ad5fe03e3c6e5743cde46854624b77018f7e64ca7c"
+  url "https://github.com/wasmerio/wapm-cli/archive/v0.5.9.tar.gz"
+  sha256 "67f98e7e584ee05b53a70e19624ca73538aef28f46e1bb31c49262ba0e00a2ec"
   license "MIT"
   head "https://github.com/wasmerio/wapm-cli.git", branch: "master"
 
