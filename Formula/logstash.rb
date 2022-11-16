b8fe491955e5319718d06efcc32ddb5b30964a43
Porkepix <porkepix@gmail.com>
2022-11-16 06:55:23 +0100

    logstash 8.5.1
    Closes #115902.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/logstash.rb
+++ Formula/logstash.rb
@@ -1,8 +1,8 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://github.com/elastic/logstash/archive/v8.5.0.tar.gz"
-  sha256 "917e27b8f2c5ecfcd342c519380f835e3d1d37e662631a641b8a64574533ef4d"
+  url "https://github.com/elastic/logstash/archive/v8.5.1.tar.gz"
+  sha256 "c143be7c11bb811091df7ba548742de18548e3b02877c1aba9c7d11f7d0cf9a2"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/elastic/logstash.git", branch: "main"
