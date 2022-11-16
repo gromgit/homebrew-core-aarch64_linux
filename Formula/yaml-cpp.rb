2e34ab9e80c2cfe84564002ba9b0a168e451af40
FX Coudert <fxcoudert@gmail.com>
2022-11-16 21:58:25 +0000

    yaml-cpp: update 0.7.0 bottle.

--- Formula/yaml-cpp.rb
+++ Formula/yaml-cpp.rb
@@ -9,6 +9,7 @@ class YamlCpp < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c0c34d815d25a11a6d2abcdd1d48491880a6c52ccb834517583d417be32da89d"
     sha256 cellar: :any,                 arm64_monterey: "870e42a85ee38ad3e908d2316f084ea62d01ee79aaa99975226457acdc0267df"
     sha256 cellar: :any,                 arm64_big_sur:  "73eb2c62c966671c744577b1ee6661edd0c1f993756ecdd68845aed7d67cc89a"
+    sha256 cellar: :any,                 ventura:        "7d8e17b3f46aa111e1f911bdb16735d7199208299bb5c54d40f69434abf72b24"
     sha256 cellar: :any,                 monterey:       "352369aa5a46cbbc4f28296f51707ac0adfe30884a11d8b3e75b6a877d5f92e4"
     sha256 cellar: :any,                 big_sur:        "d78ff4565c1d214901754ec12f0e410d60c907a9a312ea246197bbf18fca4e8c"
     sha256 cellar: :any,                 catalina:       "7b8fb20fdf64723b7aa72127d070484ec9e3e3225c4ab5f3c6247743b987e9f6"
