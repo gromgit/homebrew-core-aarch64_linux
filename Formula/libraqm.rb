751a2df3db1f4fdea55a23f603a6f377f2a83e0f
FX Coudert <fxcoudert@gmail.com>
2022-11-16 11:42:37 +0000

    libraqm: update 0.9.0 bottle.

--- Formula/libraqm.rb
+++ Formula/libraqm.rb
@@ -9,6 +9,7 @@ class Libraqm < Formula
     sha256 cellar: :any, arm64_ventura:  "6834fefcdb4df601516f21cdaa6d51c44b6bf0d465e9e086af8afed1dfff608f"
     sha256 cellar: :any, arm64_monterey: "069e6e0f6bab066c06811c7abd7578404b9eb9ee6833629a2bec9ab2a1f1b743"
     sha256 cellar: :any, arm64_big_sur:  "118bb1b1daaa4360a1399e265463b806a76b34abff36b28b83a27f376347f32b"
+    sha256 cellar: :any, ventura:        "99a0c48c9d6994d6556830a8d14c38de553f8c5f7bb978ef0161e40942cdc43c"
     sha256 cellar: :any, monterey:       "958471b39a56ede02728876c421330cb14a112901d436f5f9cc9a2f4fdd2bb9e"
     sha256 cellar: :any, big_sur:        "7d6f5ad30c91463733eedb4db44df068b1265a5e0c03b5254d35ef33a2197a8c"
     sha256 cellar: :any, catalina:       "8b67c3c8fc0ad8885727fae09c08f44ee0764b976527161688db80838b625ec4"
