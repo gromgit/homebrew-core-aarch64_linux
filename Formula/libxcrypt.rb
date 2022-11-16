a6e58bafe08c535ce93eaebf0de71d55a9547a97
FX Coudert <fxcoudert@gmail.com>
2022-11-16 03:33:00 +0000

    libxcrypt: update 4.4.31 bottle.

--- Formula/libxcrypt.rb
+++ Formula/libxcrypt.rb
@@ -9,6 +9,7 @@ class Libxcrypt < Formula
     sha256 cellar: :any,                 arm64_ventura:  "7cef5fa29821c3e9786693e506adc29fd1917f77f699a31e4512b2bd5c0c289f"
     sha256 cellar: :any,                 arm64_monterey: "aa876651a2ac195bd55bd06931c79de0e32b1ef8ea9b3171f2fdda57825c5977"
     sha256 cellar: :any,                 arm64_big_sur:  "a7539232c00c67015416429f96555539ca8ed040935bb5605f174a1b568c1588"
+    sha256 cellar: :any,                 ventura:        "be098fd9a6b06234f68b1344e6eb2646293cbf5704ad4fe06021a4614b737c51"
     sha256 cellar: :any,                 monterey:       "36f09ae68a5cf7356c744f1e1c80c4977cc656e5c1841deee1dc58973ef677ec"
     sha256 cellar: :any,                 big_sur:        "922f1f6216b34a6ece4f865e252f061a57a59770c395f67865a5d407d28b4073"
     sha256 cellar: :any,                 catalina:       "8b414f01813fd8cbbe178baa526d5e33c46dd35d92f7bddd4a923009169c87f9"
