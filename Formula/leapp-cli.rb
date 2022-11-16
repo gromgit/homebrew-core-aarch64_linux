1f0d9ec656ee5401131f1dbff4e4651c32cb2fcf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-16 04:09:52 +0000

    leapp-cli: update 0.1.26 bottle.

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -8,13 +8,13 @@ class LeappCli < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256                               arm64_ventura:  "c7ef97fdba85e3b87e5af063e7f511d26d512755f89db40c0af0905ecb8f9157"
-    sha256                               arm64_monterey: "092e7b5069da340ff4cb27d49f877f40c6dbaa25df2afa1945179ae471604ce8"
-    sha256                               arm64_big_sur:  "a73c95b047ee858fd7e9aa6956d2c33716267b1cb862b0ffa79c3a98caa12082"
-    sha256                               monterey:       "64adb60dee8719d85dc0111a0424973939a5083a84520ca50c7058bdc7e42548"
-    sha256                               big_sur:        "94df625da8d533e69e1b4161fcd1283226edaac59513cc72cf4aa1ea3edfac5b"
-    sha256                               catalina:       "f39b51a89c92e0f9a9cc7e7a474ac1076e0cacc9b427c60bbe96a26f1b03e403"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40d71f170b968de501e23883c9f8b1e7a25865425ac468e9add771d831eb2eb3"
+    sha256                               arm64_ventura:  "8589ddf349046392b61224a86ffafb2d42cf4ee200eceaabb690f0e218e0ddcb"
+    sha256                               arm64_monterey: "1c6f16311d8d8c00e9f88c8d820f552c74b75635e841023f8a75c5a762c95c48"
+    sha256                               arm64_big_sur:  "cb94ba081e1de43c04f6d3c33ee187914de411ea2b18910ffc1219b7e609e9a0"
+    sha256                               monterey:       "a4e8dc912bc1f20454c77d24d115a677ce8cbd35efb0edeb4239f08adb1c7577"
+    sha256                               big_sur:        "ac0d9c98fa704b9107daae532b64526f9c6b53cde9f68e0261d47597c43cb7a9"
+    sha256                               catalina:       "c601155bb75bf8d5f965e1f54dc764d64a0163516fd3985086696353cd83c822"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95274fcb45504c33656ee234f25c602a04d7df9646053b5158d8df3b09a77b2b"
   end
 
   depends_on "pkg-config" => :build
