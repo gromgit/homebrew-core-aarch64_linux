bc119aed9d23d03f532b2c78cad81be831fe306c
FX Coudert <fxcoudert@gmail.com>
2022-11-16 23:26:30 +0000

    osi: update 0.108.7 bottle.

--- Formula/osi.rb
+++ Formula/osi.rb
@@ -14,6 +14,7 @@ class Osi < Formula
     sha256 cellar: :any,                 arm64_ventura:  "00e15cdc872303ab7191a5e8dc3a7f1957242445c817f574c49868a18412baf2"
     sha256 cellar: :any,                 arm64_monterey: "a7f8834fcb5eeaedc2e0486bcc9e7278fc9fbd6cf7c4c7065052a2f26e419896"
     sha256 cellar: :any,                 arm64_big_sur:  "523fd267a8e74cafbd9715ba43541bdc1c845d252ae2da4b9e540151fffdf883"
+    sha256 cellar: :any,                 ventura:        "78b58cb8e83d08780fcfd5bd857e6779ddfc8c1a429befc64af8710436a5a95c"
     sha256 cellar: :any,                 monterey:       "91b6c7116bf28d34ae2fa191dcc70e7d2ac1deec388e8ce4e029c08a89dbfe53"
     sha256 cellar: :any,                 big_sur:        "f17d643090146679a789ac4697d02e4e743b32ee6321df26d2a4757c4a993b33"
     sha256 cellar: :any,                 catalina:       "bc31d0b172cab7d118dfde30e5422f35eb26810d1cc3c18ef6b040775f8f0486"
