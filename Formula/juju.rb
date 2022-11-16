db4322bc11aa4b921da6b13619771cc9ee8b1d2b
Rui Chen <rui@chenrui.dev>
2022-11-16 09:49:35 -0500

    juju 3.0.2
    Closes #115935.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-3.0.1",
-      revision: "6613e3f343bdbec74e7838e1a1b7e086672c88d7"
+      tag:      "juju-3.0.2",
+      revision: "8bf53dc35b25145ef39051fe4136135a3dd53d5d"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
