6eefd138175f84aa44458d87c96fac73ce64674a
FX Coudert <fxcoudert@gmail.com>
2022-11-16 09:10:20 +0000

    libcerf: update 2.2 bottle.

--- Formula/libcerf.rb
+++ Formula/libcerf.rb
@@ -16,6 +16,7 @@ class Libcerf < Formula
     sha256 cellar: :any,                 arm64_ventura:  "fa41458119994c07e7dcdc650e69b5782fe8450c52c8aac4ea23fc206c30a05b"
     sha256 cellar: :any,                 arm64_monterey: "5e911b27e66690071fde7d80d1f2bc010f3c63fbc594ac60ce95a07973bb95be"
     sha256 cellar: :any,                 arm64_big_sur:  "c48cf84abbe84a2ddb979ac2e438db7c1e9430ebc510b7a392790ba106c65e22"
+    sha256 cellar: :any,                 ventura:        "6470761e41d0d6738d5b4ca919209b91dbe5bb7fa0fcbaf98dd6c07371d4ed35"
     sha256 cellar: :any,                 monterey:       "72e075dea708ddfdc1421d3aa59abe811b9024c53310d22228fb8ac38e48a8aa"
     sha256 cellar: :any,                 big_sur:        "ffb6465359a84cbb5f5a2bc4fb4594c32edd3a4f6abe546c64fabaabf04d2468"
     sha256 cellar: :any,                 catalina:       "566b28dd486d974946aef516e2f91f9c3266417c64c36aaf86158126e0fa7c86"
