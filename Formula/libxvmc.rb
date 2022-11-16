25b6b6bac6bff82361d1c757f4a181027eaec35d
FX Coudert <fxcoudert@gmail.com>
2022-11-16 03:53:40 +0000

    libxvmc: update 1.0.13 bottle.

--- Formula/libxvmc.rb
+++ Formula/libxvmc.rb
@@ -9,6 +9,7 @@ class Libxvmc < Formula
     sha256 cellar: :any,                 arm64_ventura:  "7c86347af3f1470259af400e6679dcb45030a98a331cb377a42ac906baf53a78"
     sha256 cellar: :any,                 arm64_monterey: "8dd27f8a1dfc1d9d1a8de409a18365eda5ac632f1e07a25489cc49074076f552"
     sha256 cellar: :any,                 arm64_big_sur:  "1f3cbc8fdb6acd20a304917bd4bd4ac4f8e8d9ab3cecb94f2a0dc63e59e63f41"
+    sha256 cellar: :any,                 ventura:        "76f89fdba70b76925009020bd8ba4c806d49ca5c624ccfb4eca46383cf3339a4"
     sha256 cellar: :any,                 monterey:       "d4aa0e1d0640d0508c623c22c8d3818f5fa58eda8f64fbc0863b21b318b40e55"
     sha256 cellar: :any,                 big_sur:        "22eb48198116b542d5c038d83acadf2eaf7354ba68bb3d5ec147172d6bf776a2"
     sha256 cellar: :any,                 catalina:       "e6ccee73e8041296f51cfea28b2f5a3f3800611df6364605827240190d5c8752"
