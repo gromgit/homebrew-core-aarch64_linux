025b19f55ecceb455a6dec199318e0dd97c0a2d4
FX Coudert <fxcoudert@gmail.com>
2022-11-16 13:31:01 +0000

    libxspf: update 1.2.1 bottle.

--- Formula/libxspf.rb
+++ Formula/libxspf.rb
@@ -9,6 +9,7 @@ class Libxspf < Formula
     sha256 cellar: :any,                 arm64_ventura:  "03f9d06101c08ff9f642fcdd03f4983b97c85191fb8b9476e0dbda13c9488914"
     sha256 cellar: :any,                 arm64_monterey: "c3200ce73e78aff6e49838294213ed76c255d451fdc2a16b7afdf726a4113cc8"
     sha256 cellar: :any,                 arm64_big_sur:  "46c96c913b55106e3d616a0cae41b628bedea1b1226f757ab69505d85a773f38"
+    sha256 cellar: :any,                 ventura:        "e904f629cf88a4df489149100af8a60ffb9ef12e1fef74651095e0d57f54d48e"
     sha256 cellar: :any,                 monterey:       "fbeb2b06f536534acd8e85c7cd86b0603f85e059dfb0277cf093c4aeb5875dca"
     sha256 cellar: :any,                 big_sur:        "3bdd88ce60539e5c451fbfbb39d85719b9551965550b5b2937f409f152b08330"
     sha256 cellar: :any,                 catalina:       "e0047524231105d369ade5acf8110d3e64f5d98f39848df201cfcdc9ded7ec39"
