75726618673b00b2d1adb770e38a20e6702cc83c
FX Coudert <fxcoudert@gmail.com>
2022-11-17 23:38:36 +0000

    syncthing: update 1.22.1 bottle.

--- Formula/syncthing.rb
+++ Formula/syncthing.rb
@@ -15,6 +15,7 @@ class Syncthing < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ba274ccebc3f0181b489559b42e81858a6a7f3a545a9ded7eebad9be9d9f06cc"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "40cd757b98a29fcd395c4a21288c1247c5b1e4365aa8d38d65b50d0069d5e80f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5dd1fa0f1e17fe712ea570448ed155bd0f80a2a9cd4777c04bb16158c58b3b6b"
+    sha256 cellar: :any_skip_relocation, ventura:        "3a62fe4eb60893159d673e779f20ea4e1168860bf1fdd97886ad26c3b681b95a"
     sha256 cellar: :any_skip_relocation, monterey:       "a981bdb5da7abbeb088fd6180e8726f105a92b8d0515cf29e1f5c5134423e5ba"
     sha256 cellar: :any_skip_relocation, big_sur:        "261cd18b3f79b23842b7600b5d711ceaaeec5827c9f05a1670ffe00c4e6d94fe"
     sha256 cellar: :any_skip_relocation, catalina:       "d2b6568f4ca47d02ea6c7a15eb50d304d6dec9ceb97de03a93b3caa42c4839c2"
