ca1d9ab6a40f32a50a3090c9cb420696d7160fac
FX Coudert <fxcoudert@gmail.com>
2022-11-17 23:21:04 +0000

    clamav: update 0.105.1_1 bottle.

--- Formula/clamav.rb
+++ Formula/clamav.rb
@@ -17,6 +17,7 @@ class Clamav < Formula
     sha256 arm64_ventura:  "ac8c49781f54919c239b5fbd1d25f5c9c5f4df9f50f9708a9180a75e8fbd276b"
     sha256 arm64_monterey: "17bdce90d972244e322899200109657442978e7c25df8e8b2d9d30981a9dde20"
     sha256 arm64_big_sur:  "bf78ede987750a3c358a28fc33a882b4285eb133d821c712ecf00d2bd7fc5095"
+    sha256 ventura:        "31f0d9fd50afdd0a0a7c4e35213c6f3b840915c1c06dfc81874d7f52a24dbc2b"
     sha256 monterey:       "7f7207e5a902289cbce8f898226ee64ac630c1ad6000139fa45c78fbd25be2d8"
     sha256 big_sur:        "8405dde8059d027a82fc49aa5fa03570c18496d81a3c0dc0788f8fe7d600c3a6"
     sha256 catalina:       "8b671db52fb8eee93e031f7fa4b787fd3deda60a05235689aefd0eb54346d778"
