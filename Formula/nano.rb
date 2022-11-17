3919d4cb7296e65453412bae48653b8bd23d1722
FX Coudert <fxcoudert@gmail.com>
2022-11-17 14:34:51 +0000

    nano: update 7.0 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -14,6 +14,7 @@ class Nano < Formula
     sha256 arm64_ventura:  "ad928071e65053a144387cacf5d9b499c2f9cb77f99de648892344a6f385b5d2"
     sha256 arm64_monterey: "4f7f8da9d05f8046061dbf6ff153367d69363615fadf9ad243c23a4a6826b5c5"
     sha256 arm64_big_sur:  "94a78963e581d5da80973b3b04548e26fe56632d1321f2b37ae17534a6aa1780"
+    sha256 ventura:        "cb3b9b9380dd29a01095ab8442761f770a601fa6075c261bd12ae2c7f2f274a7"
     sha256 monterey:       "5667f6186bded71d8275d8d0fdcf2f241a1b9c0a38eba1ebaa4c974dcc723f07"
     sha256 big_sur:        "86e236e71122e9ca2a0fba8d88dd704bd7f177d589b4a0a953ca38695c6edc1c"
     sha256 catalina:       "f53acb49e04ec7bc16d475fc98bd1032f6820dc2544635e53e369db4dcb73a9a"
