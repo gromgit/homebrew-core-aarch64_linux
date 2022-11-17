939a6184c27dfe5ad829f657b01cf15600ba13c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-17 04:14:56 +0000

    trino: update 403 bottle.

--- Formula/trino.rb
+++ Formula/trino.rb
@@ -13,7 +13,7 @@ class Trino < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7ba79eab099f9e65ea73817dc675baf5345cab186fddc821ca348cf6c52a85ee"
+    sha256 cellar: :any_skip_relocation, all: "1a0f1f82150a6d9a093c940a32c56c82eda585f8e7664d886cccfab59d5ed7cb"
   end
 
   depends_on "gnu-tar" => :build
