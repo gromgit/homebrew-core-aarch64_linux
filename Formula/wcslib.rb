0f2fdcd652c85521031b835f603b4a61079ec039
FX Coudert <fxcoudert@gmail.com>
2022-11-17 20:28:13 +0000

    wcslib: update 7.12 bottle.

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -14,6 +14,7 @@ class Wcslib < Formula
     sha256 cellar: :any,                 arm64_ventura:  "01aae3e82d7469c400573154e2cb724a8c0243ec91ee24dc3c6c0e1c45b948ba"
     sha256 cellar: :any,                 arm64_monterey: "f418a0c421237a184ea92462338644f6f60a3065815bed980118d13dfaacf45c"
     sha256 cellar: :any,                 arm64_big_sur:  "106c631c29a069fc17e4648807c308414765a88b838dac66fed25997d2265aa4"
+    sha256 cellar: :any,                 ventura:        "fd5d489023f5a76af25221f504f1a122926fef40d3bd1a8aaf79519de37e512c"
     sha256 cellar: :any,                 monterey:       "eb4b9167d77c897c36d72e919976ae99da5793d8df33ff457c6d01efa6b2d739"
     sha256 cellar: :any,                 big_sur:        "989da78f103e6d0b9b0f933b688614f09a7809ee0a5fdbb5939c94b96556e5d8"
     sha256 cellar: :any,                 catalina:       "a7fee6167237f971ca2b474dbc774b8a4c72c50e402ff3dcd329841192c4bc19"
