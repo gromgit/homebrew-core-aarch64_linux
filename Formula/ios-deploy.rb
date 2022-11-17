0c27bdf435a58ae6bd1b03d4e6cee23c73771056
FX Coudert <fxcoudert@gmail.com>
2022-11-17 15:16:50 +0000

    ios-deploy: update 1.12.0 bottle.

--- Formula/ios-deploy.rb
+++ Formula/ios-deploy.rb
@@ -19,6 +19,7 @@ class IosDeploy < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "96bca7e7f751379de2241853b9cf2e0fe1f518493e9365c31116fb3a309c21ff"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a9afe8fd08f085f05104499bee9dd6e79110cf9f63fd7be57fc5c28628af9a09"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d472b334895a1863262cfdceaf777ab9a6c675c265e4d83265ae3c2d9b25793"
+    sha256 cellar: :any_skip_relocation, ventura:        "ffd40c6b9216bfd90adf4c1cfe7aafd263e79b795794bded1dd05eeb986a78cb"
     sha256 cellar: :any_skip_relocation, monterey:       "eb4951c90cae9df4a16a5c20cd24393647899eebef8fb4f313f9eb999a28230e"
     sha256 cellar: :any_skip_relocation, big_sur:        "4920f28ef96b15c91cbc7e0d84232f4756dd4ecb50bc5fc4b47cb6e5137ab36a"
     sha256 cellar: :any_skip_relocation, catalina:       "6f6b852da9e2caf687adec64fbf683d2be92e1c04c00e5fb93db6faca92ed22c"
