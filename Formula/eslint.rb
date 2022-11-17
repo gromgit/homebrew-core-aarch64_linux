3938e2791ee1a3ba7ed50de54eb7d572c42b65d3
FX Coudert <fxcoudert@gmail.com>
2022-11-17 23:25:04 +0000

    eslint: update 8.27.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -11,6 +11,7 @@ class Eslint < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f59f8b3d88248759ca8af3677483185d2da7f990ed0963232a2fde8fe52ba295"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f59f8b3d88248759ca8af3677483185d2da7f990ed0963232a2fde8fe52ba295"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f59f8b3d88248759ca8af3677483185d2da7f990ed0963232a2fde8fe52ba295"
+    sha256 cellar: :any_skip_relocation, ventura:        "6d63d101e40fc44f14d2d5ed432cacb8cb245fe5606c9a37cc58fb1573f9c662"
     sha256 cellar: :any_skip_relocation, monterey:       "a326b5b84a6f3b1a40193f35d5a2918c54442106a9a493623d8682422662edef"
     sha256 cellar: :any_skip_relocation, big_sur:        "a326b5b84a6f3b1a40193f35d5a2918c54442106a9a493623d8682422662edef"
     sha256 cellar: :any_skip_relocation, catalina:       "a326b5b84a6f3b1a40193f35d5a2918c54442106a9a493623d8682422662edef"
