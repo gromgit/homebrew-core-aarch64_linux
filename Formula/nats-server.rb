438b83e51ec82bc105965b3be79e833b3855119e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-17 20:05:00 +0000

    nats-server: update 2.9.7 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -7,13 +7,13 @@ class NatsServer < Formula
   head "https://github.com/nats-io/nats-server.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c53eb7cc03da56223421a005a847411a92160f385bf04c95e58225e76d8b048b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b052e4c1052a36ba6f52ea4ae08cbba481c27cbc7db2c60c19f2079292c2ec5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d5c031b44d5c331267c1c74b567ddf2c4f91b5822bd70621d5dcc4c9b511b103"
-    sha256 cellar: :any_skip_relocation, monterey:       "8be169510874f69665754fdb864037ff52ae535ea852e5ac282878220e8b5599"
-    sha256 cellar: :any_skip_relocation, big_sur:        "32fc7a9678808a66b3b72ec891d97eef208983bf417558311ebc53cdd8766741"
-    sha256 cellar: :any_skip_relocation, catalina:       "b9ac57a640f4472e4ec6ca5464c323396365970deb22c29cabdf70c735b66572"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "299f63261b3361276e42674c81e194520326962f265b809c81238c6c8d40812f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8ce48fd805dac372b68e723f0b040ef25a05efac2c7ee4e7c385500e3f2439a3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "da56d0f251a705d03f0eac5fa6065ac08cefb6687beb94e8eec3ca413b7c66f3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4293351c0b62e606801c11f08fedc52795b758008e0dfd56c29cc3304397204d"
+    sha256 cellar: :any_skip_relocation, monterey:       "b5df0aece2acffa008067683e29c000ae8e0f4b037e3f3d8dc236cde429d37bd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce29e92e3636ba09ee392939685dd22244e1109fc6d8c157313da58ebbd731e6"
+    sha256 cellar: :any_skip_relocation, catalina:       "e6497e2c343caab847203edaf1d399cdde4f40f77f29bac7a4bec5b51dadb729"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97857f22dcacc2a0cd601f1716366dc14ca7a46cfdde9042a06957d991ca1d79"
   end
 
   depends_on "go" => :build
