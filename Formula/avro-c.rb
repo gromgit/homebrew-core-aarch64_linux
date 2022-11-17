c15a458272fe094a97b3fc287a834b595fce0d9f
FX Coudert <fxcoudert@gmail.com>
2022-11-17 08:09:28 +0000

    avro-c: update 1.11.0 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -10,6 +10,7 @@ class AvroC < Formula
     sha256 cellar: :any,                 arm64_ventura:  "e866c69e2e7cf6ed942aef633b01b38bbfd1fce8c9f2e6ab563f386c82652e5e"
     sha256 cellar: :any,                 arm64_monterey: "003f19bcd51baea53f86d97488f83552dc8477eedef78a371bdc166ac0d183ab"
     sha256 cellar: :any,                 arm64_big_sur:  "f30243ab877db4e81ff07c5dea0e266228ac04bf178144d3f9caaab45a876f86"
+    sha256 cellar: :any,                 ventura:        "8b128950bcbd63c5f82e88773a0fcf5eac8c044f033ee5322ac4e35150fbd501"
     sha256 cellar: :any,                 monterey:       "35a23d1c97d7e5b50adf7ff2f8ff3ddcb6f9271c85fc033306e8447484e23b5c"
     sha256 cellar: :any,                 big_sur:        "cbb5817beab9e6dfb2dd1a5fafed449c6aa5dabf8525b71142854771ba46ab35"
     sha256 cellar: :any,                 catalina:       "d91479cfd09b368e971ed2c926c480edfe88f62df39476e780ec0d863cc19dd5"
