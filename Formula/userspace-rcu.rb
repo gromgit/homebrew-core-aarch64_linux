78699af7e435a30b4a27e7e325634d78631a1824
FX Coudert <fxcoudert@gmail.com>
2022-11-17 20:42:06 +0000

    userspace-rcu: update 0.13.2 bottle.

--- Formula/userspace-rcu.rb
+++ Formula/userspace-rcu.rb
@@ -14,6 +14,7 @@ class UserspaceRcu < Formula
     sha256 cellar: :any,                 arm64_ventura:  "662b6c7315f6070482140ddd035874cf52c434286d44a44bf7f31125ff7686ae"
     sha256 cellar: :any,                 arm64_monterey: "d2cb9246e240a6db9c40d9e3a19288d064ef95345f3e884a5892fa2b341fcb9c"
     sha256 cellar: :any,                 arm64_big_sur:  "c661f7dadf4ef5fdedc1ea707da817fba3e491b0b263b070fc234f35614a8afe"
+    sha256 cellar: :any,                 ventura:        "ebe06cf034c8018f1c1ff1420a11a29df85f4a5a2e7852466e43944a83f82877"
     sha256 cellar: :any,                 monterey:       "c9cf73dd281dddd9fca16844e005392d0f0fb1132f4540ef95e552d7799113c6"
     sha256 cellar: :any,                 big_sur:        "5e1b5b33fc0f5ad814ddc1d738f5ca8c2811422b9d37ecc1d517328287fc7d4e"
     sha256 cellar: :any,                 catalina:       "3056888d7a39aa8ff2859eaac9d14ed0d15b921d857378160f6f49f7365ab07c"
