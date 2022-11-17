8925105ac7fd1519da2b3e5558138d42fc9c6896
FX Coudert <fxcoudert@gmail.com>
2022-11-17 17:15:51 +0000

    task: update 2.6.2_1 bottle.

--- Formula/task.rb
+++ Formula/task.rb
@@ -16,6 +16,7 @@ class Task < Formula
     sha256                               arm64_ventura:  "2102db063dd8ff63b36a1503bec0255e1f51a7096fb0b0f923eacdf6cdddc3cf"
     sha256                               arm64_monterey: "f795845e5ec49a639baf6e7eab024f038ad1151c62ec03916752877941f5c89e"
     sha256                               arm64_big_sur:  "3eef2acca71784b22e30ed3869c92b84c42bb47728e3df7709a5352dd0c4cf9c"
+    sha256                               ventura:        "6e281f77be42efe002f195670b816170a66773e9012419cea071d3f23bef24ee"
     sha256                               monterey:       "b243e5436b6c1401acf8118e6163f80853027bfbbc4263ed3ce6e71dcec707ab"
     sha256                               big_sur:        "97758b4159fbf81aa71d5ee67f845b8d6369bb6f9867593bdf862a34127ebd91"
     sha256                               catalina:       "c4a17d47447ecd06f66c518e4066ae583172aa1fe15909afa726dbf2664cf7b4"
