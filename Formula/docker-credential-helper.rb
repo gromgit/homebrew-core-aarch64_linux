5fd3081d129f4c0cb2f1e1c63e2b1dd218421f8e
FX Coudert <fxcoudert@gmail.com>
2022-11-17 15:32:38 +0000

    docker-credential-helper: update 0.7.0 bottle.

--- Formula/docker-credential-helper.rb
+++ Formula/docker-credential-helper.rb
@@ -10,6 +10,7 @@ class DockerCredentialHelper < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e227291ef57cdb472f6eed72ec07b40d22e387eb5cdadc3fed1e4595e2f65dfe"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "289b614d11f5be30833ae352b2b99e597eb45f47a29a30c4987365fc15040a49"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8f86bc14a4ddf9a628e08754c1cf1cf38db28a2d0482e812ec93ebf06f2aee8f"
+    sha256 cellar: :any_skip_relocation, ventura:        "345caa27a81588869193b928c05b75c0eff80fad18145d49e2a93c48305f6d0c"
     sha256 cellar: :any_skip_relocation, monterey:       "33be1634548456c862edcdb50dd46af5e0a915e4e303ac29be3af9b19dac9a07"
     sha256 cellar: :any_skip_relocation, big_sur:        "1e83b1f364f9c22cd561abed9bfaf7adb449f4d4c6c803b8ad7a6e4665dbc2f8"
     sha256 cellar: :any_skip_relocation, catalina:       "b1be0b36fc51ebb6a85e262c339f36bad7774b65d6216072582b100524f33762"
