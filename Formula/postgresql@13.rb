5e60a60632be64085d3d3168b6f8e8dffdd392fa
FX Coudert <fxcoudert@gmail.com>
2022-11-17 01:55:01 +0000

    postgresql@13: update 13.9 bottle.

--- Formula/postgresql@13.rb
+++ Formula/postgresql@13.rb
@@ -14,6 +14,7 @@ class PostgresqlAT13 < Formula
     sha256 arm64_ventura:  "2b8a59312cad3b31408ca3e6d686acd711d02f059c4a9c9b694893ce86716b77"
     sha256 arm64_monterey: "03cc819daba98e98a50706aa0620704d901fa044aa8e6ca176d60538a57dfe3a"
     sha256 arm64_big_sur:  "eb3cd952427647008c6ba1b6a75df3e0f76bc5f56ee9839d8c316c92ec6a7266"
+    sha256 ventura:        "8a8fb5d5e0d60e3fa9ee32ce6325b6f9833d0751c707f36835af461938cb3ffc"
     sha256 monterey:       "10d84161c288f9e4d5209dd10dae30d1d0115777b91d030a6b5eed034de74d0e"
     sha256 big_sur:        "2e1abb35c5740446eecff3c6c735fe614a58f199fe19d32a706f605bd4383aa3"
     sha256 catalina:       "e2652d49accad137ac70f120efa7d205f0cfb6a7e62f293741c9aba09c70354f"
