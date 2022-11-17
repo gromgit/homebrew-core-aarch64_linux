f05dfc359d18c248abefe3b914c568aba5cade5d
FX Coudert <fxcoudert@gmail.com>
2022-11-17 03:10:54 +0000

    aria2: update 1.36.0_1 bottle.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -10,6 +10,7 @@ class Aria2 < Formula
     sha256 arm64_ventura:  "cb572370ae31983f3cb7933f52ac2011c4f2f41858106c9abd1b22c83ba3114a"
     sha256 arm64_monterey: "b0ec32121f2f4c94afce1cf43101d0441176e4d3de2461160cf220b4c4c2a89f"
     sha256 arm64_big_sur:  "89db8d96ab739c0ebcc8a800169d7001c18fae4d3fce8217b8ffa5455d1f46cf"
+    sha256 ventura:        "90e6ffc911cf05a056a6a19ac4169cba8f315c6d740ee02892ecb57cbab74ba8"
     sha256 monterey:       "410b790649fe92ac3c146aa394a6ff1d70411303e68b7c4ff5c5e4ce82435b30"
     sha256 big_sur:        "cebab5dd720d1b80d429c50b7e84944912a2e2c25f471d6f379fbb5670080026"
     sha256 catalina:       "2be60bad723be29d33143d487e6bc0c32e0a37de083043bb2fa5c31e586ad37e"
