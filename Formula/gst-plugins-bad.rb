b79be815709f90e4592488ddac7ad418d22e8230
FX Coudert <fxcoudert@gmail.com>
2022-11-17 20:10:59 +0000

    gst-plugins-bad: update 1.20.3_1 bottle.

--- Formula/gst-plugins-bad.rb
+++ Formula/gst-plugins-bad.rb
@@ -17,6 +17,7 @@ class GstPluginsBad < Formula
     sha256 arm64_ventura:  "2a9dfdea5613377e9272d1146bc19fee42d4e4b19aef3c3ebc66e7758e0304fd"
     sha256 arm64_monterey: "90ab8d052e4e604af92c6f2da16c39d59edbeeb4c71468ed1f28baf5c5d25ef8"
     sha256 arm64_big_sur:  "906df89626b77a02736354b57fea8a18cc5d66c2038fe18927a6248c39c2aa7a"
+    sha256 ventura:        "e001bb2b9450f7f5d9a30aee380ab520380fef5951449a7fb433db5bcb19e024"
     sha256 monterey:       "6789a1ac8072824a179b5fe0d090e6af1c5c176a17bdd9ce12f4d170cffadc57"
     sha256 big_sur:        "c32730a1408b72b32f3ec0cf4f935ad17496d455fdc9467ecf3b61c1acc99634"
     sha256 catalina:       "e01365b988dd0b6f39c26b923978aeb9964e786489699fcffa857ac6d6da9cb0"
