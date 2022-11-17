bde0b217f15ada19571d35766c6adef353b241c5
FX Coudert <fxcoudert@gmail.com>
2022-11-17 05:39:39 +0000

    gitlab-runner: update 15.5.1 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -16,6 +16,7 @@ class GitlabRunner < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ce29cbaeab3c96a617ba124adf2453d1eb2c34dced4e151f6b94b8846c678828"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "11b05a3d9ca55153720c9aec6bd3b5223090a062e7b3b520fe25ecda984a38ed"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a6d1fe9b1e7df8afb48ebf66440d5cea7b82d62c4e461312d8c1060b59712a4f"
+    sha256 cellar: :any_skip_relocation, ventura:        "4f582f4e89915a2c3d5b23218ceea95d773299c945889faddab43d9949c7d3df"
     sha256 cellar: :any_skip_relocation, monterey:       "c23efa92bdf49960af157ad0a5b07f85ba6c4ce3ee3e2cacbad79e1d337244c0"
     sha256 cellar: :any_skip_relocation, big_sur:        "6765389090acda5b679ab51bbd2dfb5e173cb55b01167ab7ed95acc523761495"
     sha256 cellar: :any_skip_relocation, catalina:       "26ae7597144af1eb796e545752f4bef3b16c9b0c2687c22dc42f412b6b2f80aa"
