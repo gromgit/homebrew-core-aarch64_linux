d9f631b8f64413eb4538d91dbe1e960affe62fec
FX Coudert <fxcoudert@gmail.com>
2022-11-17 17:52:59 +0000

    libre: update 2.9.0 bottle.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -9,6 +9,7 @@ class Libre < Formula
     sha256 cellar: :any,                 arm64_ventura:  "68185c640b0b3e7a2fece5b0dbd5324ccc199b46bf06bb0533a2f87b78ee069c"
     sha256 cellar: :any,                 arm64_monterey: "6c1106c9c13760418b17c4ada4384bc04226c51ade387262809760d68da80829"
     sha256 cellar: :any,                 arm64_big_sur:  "ab6d1f01cfbc37a019593fad04e93206eeec47947a8f4f516a99863fc9cef880"
+    sha256 cellar: :any,                 ventura:        "6a4cc34c80dc2ff88ec0a1908ffe682da98dccf450adf9304287819e65f6ce44"
     sha256 cellar: :any,                 monterey:       "b51e2f7088f17a2387b3de37a002b6a9fc3f3ad3563d2f609d261745ffd510b3"
     sha256 cellar: :any,                 big_sur:        "c1b8e522004c71238b062b9c2dd703a5dc8de703588bb6283511a80ba511464a"
     sha256 cellar: :any,                 catalina:       "82686d1b93e1dd6c9fd203bb86fdd03a623777db13086166a3143adfd0e355f0"
