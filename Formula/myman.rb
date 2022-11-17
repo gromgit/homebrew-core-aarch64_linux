34db9da501d73a8919598819ee0469afaf8c28db
Rui Chen <rui@chenrui.dev>
2022-11-16 23:51:23 -0500

    myman: add license
    Closes #115986.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/myman.rb
+++ Formula/myman.rb
@@ -3,6 +3,7 @@ class Myman < Formula
   homepage "https://myman.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/myman/myman-cvs/myman-cvs-2009-10-30/myman-wip-2009-10-30.tar.gz"
   sha256 "bf69607eabe4c373862c81bf56756f2a96eecb8eaa8c911bb2abda78b40c6d73"
+  license "MIT"
   head ":pserver:anonymous:@myman.cvs.sourceforge.net:/cvsroot/myman", using: :cvs
 
   bottle do
