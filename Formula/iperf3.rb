6f2d994e8ca9621aaa065915d6e0828307acf578
FX Coudert <fxcoudert@gmail.com>
2022-11-17 03:51:22 +0000

    iperf3: update 3.12 bottle.

--- Formula/iperf3.rb
+++ Formula/iperf3.rb
@@ -10,6 +10,7 @@ class Iperf3 < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c8ed6e824689e69c4c0d2eec8a80fb54d901769429fcd84b9598e434d55c8b35"
     sha256 cellar: :any,                 arm64_monterey: "9207226775b4889c2bcc0c0144b46b69f400048c62123ab4bdc0c8132bed79b0"
     sha256 cellar: :any,                 arm64_big_sur:  "a9771e4a890b5b56a184a90cc1bfb8b7549e95d412d732a64a5327f5b52fad2f"
+    sha256 cellar: :any,                 ventura:        "a11782c6c7a69127bf09fa58de6480bd4add5af8842dca9c98be2ecf5f87f6dd"
     sha256 cellar: :any,                 monterey:       "21aafa39e3e3cb2cb7aa7cb19f8d720f12bb65a80994ae38f477e4f22664dff6"
     sha256 cellar: :any,                 big_sur:        "f8c46352bf2b270fc4bb61035c05f04edfc96a2bb2fdeea0c36f38d5311f41f2"
     sha256 cellar: :any,                 catalina:       "89e90520f268578ece5cf6c2f3a39dc7e54830e02f4996b22447c015ef4d821f"
