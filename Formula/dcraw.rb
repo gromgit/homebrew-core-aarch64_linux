c6b5ed1c2d50af7c1b6e1806caaa0d9f1bece24a
FX Coudert <fxcoudert@gmail.com>
2022-11-17 10:49:52 +0000

    dcraw: update 9.28.0_2 bottle.

--- Formula/dcraw.rb
+++ Formula/dcraw.rb
@@ -15,6 +15,7 @@ class Dcraw < Formula
     sha256 cellar: :any,                 arm64_ventura:  "67ed6c9b0e400aad488b5449b439c3d2c2f1367ec855d95c2cedf1eddf0ab9fa"
     sha256 cellar: :any,                 arm64_monterey: "3d77794768e6ae2d0ff3ffbacc3ed745017f7ffc2503da954100b2d1ac146db7"
     sha256 cellar: :any,                 arm64_big_sur:  "899ea09ca46695dcbae2414fd72f3af86fc8676e0e51aea2b54baa28a4a5845d"
+    sha256 cellar: :any,                 ventura:        "8e605ef084d83242cee08c32354896073428b53be4f2bcd0ebc855da98b8325a"
     sha256 cellar: :any,                 monterey:       "72c6183da24a08d3cc0d887ac294a51c14d5b39c8b2a42820cc4a4351768b9b1"
     sha256 cellar: :any,                 big_sur:        "2f5f80cfc1599bbc5615312a1652f6904e3ef79d24d30c68e3d6e7c185d517ce"
     sha256 cellar: :any,                 catalina:       "6ad0be7cd49f7ccb34d7159e2a78e231be474cf32d094c722aaae6e4e354c65b"
