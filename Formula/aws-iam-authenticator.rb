cf6d6eed016387db80009fe54960d05a1f864a4f
FX Coudert <fxcoudert@gmail.com>
2022-11-17 03:33:39 +0000

    aws-iam-authenticator: update 0.5.9 bottle.

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -16,6 +16,7 @@ class AwsIamAuthenticator < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "707e82d4d95a99dc09155c01fff3cf9f06700801a9f585893960a04e4da897eb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "12efc857905e148e434b541cc76801956f186ca92fb43ea88884303a1c36c175"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ba09e1a04810d7fc4fe0156a231d6a28c507a39cd7579d2f8705c204d1611cde"
+    sha256 cellar: :any_skip_relocation, ventura:        "98505c961b95929d73263a456bff7f5cf7a36ccdd1106df3a5880fcebefb70bd"
     sha256 cellar: :any_skip_relocation, monterey:       "091e33fd80229c5db17574795d5e44f9a54951e3fc2df24336c2cd449cd6e839"
     sha256 cellar: :any_skip_relocation, big_sur:        "bd2327738f22d7c95ee22b15915cad3ab850cc439f8684680ffe14c07c221d7f"
     sha256 cellar: :any_skip_relocation, catalina:       "0207f549dce4741625e3a93cad4ddcce8886a18eeb13ddbdf7d27530056877c0"
