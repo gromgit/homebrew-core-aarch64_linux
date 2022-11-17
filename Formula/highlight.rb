1218f0079c5f3c95b19585e673cc94de95e09bfe
FX Coudert <fxcoudert@gmail.com>
2022-11-17 22:18:48 +0000

    highlight: update 4.4 bottle.

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -15,6 +15,7 @@ class Highlight < Formula
     sha256 arm64_ventura:  "4a52ed8e7a2be08e915bbb10bea1c631fd7f45ebc2946e0cf812d575930ba571"
     sha256 arm64_monterey: "5d7553122ffe8dab375c4b611c6e8b3475dfeb8308b434e873cf68747b6c424f"
     sha256 arm64_big_sur:  "d87ca1044ade41e0d09f094bfbe5f0a7ed70c8d5775cf3202bbc24954efa218b"
+    sha256 ventura:        "1154d78336851ac92914807f166735f025307c479696f2c6604ed71cc31d7328"
     sha256 monterey:       "13cd44ad676ff383c8f8f251e702f8fd972ea898b2b4386dbd2630b7a2808bec"
     sha256 big_sur:        "e73ca9297594376829910577f0fd8ae80166c83c346b0a7bbfa42d64deff7bef"
     sha256 catalina:       "0375a4be9e50c1438074d258fe0c784d7369ca097aea39a52c02068dc298b8ff"
