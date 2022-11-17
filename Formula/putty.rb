0018c6ecbcf38723b6e78c48e2254af00811d955
FX Coudert <fxcoudert@gmail.com>
2022-11-17 12:28:12 +0000

    putty: update 0.78 bottle.

--- Formula/putty.rb
+++ Formula/putty.rb
@@ -10,6 +10,7 @@ class Putty < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "73d76e3809e293c8a1822f810831234d5a428d8d0574f043d4394ea0ae22e4f6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "98350a82ed60a6d22d3ef1fad8dc3a5e2ce8d44f2fea51d6f853315fed7b2a9e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e6964cbe0895d4f3b6c832062c0bcb8654264115fc522d2cbb7f17932e37117"
+    sha256 cellar: :any_skip_relocation, ventura:        "171f040dd0cf286f69836bf1c17a6fbc148bee5ef6392c328ac0731635a79115"
     sha256 cellar: :any_skip_relocation, monterey:       "ed3b2388437bbb394523ed1956cfbc6ab7283763b6c07edec255bb3e5d5d2496"
     sha256 cellar: :any_skip_relocation, big_sur:        "e84fd110e478867048f4f8a45c0b10e7f7414cb4a31f4e38ee46649e6ca9fdfb"
     sha256 cellar: :any_skip_relocation, catalina:       "35e0f3246651376a6c47765375faf56747ab81a8c03f4ffeb3c02429530e609d"
