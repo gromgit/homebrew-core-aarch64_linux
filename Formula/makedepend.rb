78dcbb329bf2863d723798f02a38d83411a01502
FX Coudert <fxcoudert@gmail.com>
2022-11-17 11:49:54 +0000

    makedepend: update 1.0.7 bottle.

--- Formula/makedepend.rb
+++ Formula/makedepend.rb
@@ -14,6 +14,7 @@ class Makedepend < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f7db435d623d3f12d3664338ef9f32c445438260054ec10593d24645d8018105"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "632a49fb2a96ada364872848c82d4de60ac2847eecefc43b5e9d0a7f16af20e8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3f3ad09b4aa6533501515ba172cae3bffca8dfb7076c080332b27a1ae997f676"
+    sha256 cellar: :any_skip_relocation, ventura:        "8c870cae96f96a20adbfcff8c4dc347148b73c1d27f8cfffe8dd1155cbc50dd2"
     sha256 cellar: :any_skip_relocation, monterey:       "86aa1e7c97901369724cbba722ec3e44e47596a6f99f43925515bddda3cdb55c"
     sha256 cellar: :any_skip_relocation, big_sur:        "40d75eb41d75585be1db186162d1cfad4d2d1e5ba16564ce89d1945395531013"
     sha256 cellar: :any_skip_relocation, catalina:       "1819aa4bd7a1afaf0f19658019b655f1390ee17bd524fe8de8424d7599d85e17"
