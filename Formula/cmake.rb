103f18b14fbe28e2ff10a39cae19a79a54d636ab
Rui Chen <rui@chenrui.dev>
2022-11-17 19:34:06 +0000

    cmake: update 3.25.0 bottle.

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -19,6 +19,7 @@ class Cmake < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "80c206a6884a52914d09c72ff2e64079ae0d99aa7fde58dea73de0faba8f6d6e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3ed5aef087d93f4df171e82f72df5c6815ce7151dbd4b370e70d38e089603c37"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f440fd5a4495878ffab7e1008a6c874d4606f68d514f154520a55e4194c8a680"
+    sha256 cellar: :any_skip_relocation, ventura:        "2466f08623841c1acc932331e5b44fdf9a987e5c074d8605fac96ebf328ad080"
     sha256 cellar: :any_skip_relocation, monterey:       "c472ef54bbaf367467ba85887895f987835bb2843a1c74a76eb05edc7b1c12b9"
     sha256 cellar: :any_skip_relocation, big_sur:        "532b702a317ab864e46ca7b6c3049183fbdad97e4fde66ef341bfae147870d49"
     sha256 cellar: :any_skip_relocation, catalina:       "3ce6b4c1f589cb3dbd649deaca28de53ad8f87c677aff56aacdded5d4d720f54"
