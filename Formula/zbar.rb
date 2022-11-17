3b65158c5c95ca10bc7483cbb160e1bd61eac0ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-17 10:04:45 +0000

    zbar: update 0.23.90_2 bottle.

--- Formula/zbar.rb
+++ Formula/zbar.rb
@@ -12,12 +12,14 @@ class Zbar < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "82553f77ff4df652edf3c86b6b1e784eff6c20e6c296e581fb3be34c0572ab52"
-    sha256 arm64_big_sur:  "338cc6e620aeed45382c97260f899e9f6ebe1260ff1e7fef6ef2392d938d516e"
-    sha256 monterey:       "2683cf24b3435982d30041c0e3aca33d48a418e2b60085aca75d081fec5cfc38"
-    sha256 big_sur:        "d2b9c6deb05916f73dffcf4f637160a96b82fa1ea67e5c188e34ff419768d798"
-    sha256 catalina:       "f203dccfd3b1fdfa91c844a7e8cedab935a2e97b8527329f00f6816f1e32ff73"
-    sha256 x86_64_linux:   "085302f322670cb77040a77645e1972ea14e8e6bc1f6e9ee3e010d53d48dbdd3"
+    rebuild 1
+    sha256 arm64_ventura:  "0148fabe1084ac13f107a34c15e69a68a4e0de8c1212550ea927d5873450843b"
+    sha256 arm64_monterey: "e9b2610d5fa22cd12a463ac2701240acc8f6e1c1c190e6fbe1e8f56b12fb695e"
+    sha256 arm64_big_sur:  "66d7b6a0b9cc69e2d0786aa626577abc917adba4dd76b79e7bc959afd9eefb7c"
+    sha256 monterey:       "e3f3cd2101683ec1e5d129d9c590f9e61283dd078968231211c994ebd919eeeb"
+    sha256 big_sur:        "7a440c19a50c6dd6ebf77b41a254e78fa5ffc641877ae180d131f1cc0d2f4e6b"
+    sha256 catalina:       "6524034479ef1b0329914bba94b9a778f07436a4fdcef8d5685df31152a1d990"
+    sha256 x86_64_linux:   "b95de47606556882a0e828e77e7230a3e02d49df3fac48d6692f18e33a9e88b6"
   end
 
   head do
