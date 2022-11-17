4e82f754f31c94ad67c7dced77bf29407c133410
FX Coudert <fxcoudert@gmail.com>
2022-11-17 21:51:08 +0000

    memcached: update 1.6.17 bottle.

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -15,6 +15,7 @@ class Memcached < Formula
     sha256 cellar: :any,                 arm64_ventura:  "23cb27638c5132b7cb1695140c220dd91caeb513a390c2b29483b42b1b1cad9e"
     sha256 cellar: :any,                 arm64_monterey: "45243fbf069a4f4316fb8289b60b9e8b9419790fcd0d8da14bb3db2bd6990358"
     sha256 cellar: :any,                 arm64_big_sur:  "ea196f2117f613372a46fc29721ecac4187ddfd03a4a87ae9a513e6ca86989a9"
+    sha256 cellar: :any,                 ventura:        "5909cb09ed4e5a23d921a502cad2c860d4e9124d4e60b1d6c0681b3b8901939e"
     sha256 cellar: :any,                 monterey:       "0f50b701cdf195c5f452e14d43b88b48fc17d61d01f9d52072fdd90d25e020b4"
     sha256 cellar: :any,                 big_sur:        "6ba66bec380b965431dd5eb7ede1c71113aa4cc0e1c4ed3e9701c8be44ef963a"
     sha256 cellar: :any,                 catalina:       "6c1ad74d6e75b2661089d75af1dc5bc532cc0fe6012daa851a9d2893afe78a2b"
