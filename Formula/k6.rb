244a563ab7b0b80fe996e95ac12d0e59d03fb5a2
FX Coudert <fxcoudert@gmail.com>
2022-11-17 04:50:18 +0000

    k6: update 0.41.0 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -9,6 +9,7 @@ class K6 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c7d2ea81b3fbc1de2422d9d34c0a6563385847ee6930e864d6e381eb4fec372d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8b517175c0d0119124edae6bea02c1e4399ad62deb1a27ad3413519366e7812d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "363aa6c32f151811537122c0b9d010f4109ec5ad30a4ce94e76fdf10050786a2"
+    sha256 cellar: :any_skip_relocation, ventura:        "e12e34c80ac4a6e3938fd6c76f35a8ee91f510e1f5de7d57c6a9816e40a0337e"
     sha256 cellar: :any_skip_relocation, monterey:       "22d94dc3cf19e1c59ecfe22a1819e0a67e2380f31d39d14f233bd39ccf49af38"
     sha256 cellar: :any_skip_relocation, big_sur:        "784d2e9be659d8f902318889264df95354e15d8b7048e28b186e36befa3d3f87"
     sha256 cellar: :any_skip_relocation, catalina:       "540d8fa843dc40b20f4ae6964777fedeeaf0774ef4128efd3ab87b5d7420d802"
