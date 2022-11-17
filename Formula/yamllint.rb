d5b901f213d6f47939048aceb316886923f814ef
FX Coudert <fxcoudert@gmail.com>
2022-11-17 04:36:20 +0000

    yamllint: update 1.28.0 bottle.

--- Formula/yamllint.rb
+++ Formula/yamllint.rb
@@ -12,6 +12,7 @@ class Yamllint < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "58d67664eca5c91e1072fa00941491a9af5e6d07a99c422ef022cd033f7f6b16"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4e734080f9ef58b1e5b324cd5a421601519a181b466df093f2890252c8c32a61"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "652aac84b2465cacd8148561d7448986c73df6b512b7aed9e351e4c7c7f8be60"
+    sha256 cellar: :any_skip_relocation, ventura:        "3737f04fc04a1a689e140055a09125d10d7e138eb2b1f9ebbab90e5f1917b5d1"
     sha256 cellar: :any_skip_relocation, monterey:       "13e33c28193edf6781c3cf33e992c3b4e32cd63275b76678b634e6cdd06eb46e"
     sha256 cellar: :any_skip_relocation, big_sur:        "ad293f400a905f2543160b822dbe7ef35fc57cc311af01cf6e42323649ce6ea3"
     sha256 cellar: :any_skip_relocation, catalina:       "ca41159b0a775a9352295d2232520385baf10edbbdcf422bd1bc5b498efd1173"
