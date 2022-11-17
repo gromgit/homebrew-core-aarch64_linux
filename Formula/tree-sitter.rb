285646c2b2569eef61d536f0165ce38bb2cebad8
FX Coudert <fxcoudert@gmail.com>
2022-11-17 09:31:38 +0000

    tree-sitter: update 0.20.7 bottle.

--- Formula/tree-sitter.rb
+++ Formula/tree-sitter.rb
@@ -13,6 +13,7 @@ class TreeSitter < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c1fb86aa4f3e621a51b8a3162ffab2acc64ca814f3cd758bf5e167128a90c8dd"
     sha256 cellar: :any,                 arm64_monterey: "6f6f70838e3691e8cd34ded3c2c4e88b39ba1153110bcb9ceb123c5c3d653459"
     sha256 cellar: :any,                 arm64_big_sur:  "ba68148d99a41b39d6fdd5c6461064daf0903d06911ed2970f4a04dc49691ab2"
+    sha256 cellar: :any,                 ventura:        "1bc496e96541203fe00d602d69d41ec9f71e4efc81c4d37e0738a25f29407127"
     sha256 cellar: :any,                 monterey:       "e251026a9bbe2ea09165376223c4c82766ce98f7f8393b479d03a7474c61034b"
     sha256 cellar: :any,                 big_sur:        "5beb68d159b38a6712f0fc5acf7b26dc7bf655ce00f8934bf8e5ac043a80932f"
     sha256 cellar: :any,                 catalina:       "5e8d09ddfe2b847f6d4a838d38aed711a282ea358a5ed2a09ec92ab5e14e7925"
