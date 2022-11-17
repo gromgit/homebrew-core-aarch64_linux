e088395225fdcf3d647a81b6b81b96f218378e86
Rui Chen <rui@chenrui.dev>
2022-11-17 19:33:48 +0000

    algernon: update 1.14.0 bottle.

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -16,6 +16,7 @@ class Algernon < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "14e5969ae003e4adede7f1c59bc7813acea79cb19f438796ada77dc621612869"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "343a3153a0af6ad0fc1ad27939eb9a2dbc9f379bd5bf6b4e373b9fd2f87d3ed4"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "401c99640d17d66677f419c8da8719618c499e02e2b6f70b74eefec104378912"
+    sha256 cellar: :any_skip_relocation, ventura:        "aeb0c363d972073a22cfc94844c61c8da268cef9dc68c9316167d28d9c709b66"
     sha256 cellar: :any_skip_relocation, monterey:       "a0f090b5d513eedc54de13a6f954a33ac0a126ea2b1bd1dde009629bd0854707"
     sha256 cellar: :any_skip_relocation, big_sur:        "3f25029f7b5dcf462b1b6e3d3a69e246d881201a7f9ab35657cd11dd25916f6a"
     sha256 cellar: :any_skip_relocation, catalina:       "fff5b9bfa21524d8f9bff2266978dc7dcb4a8a6a35a19eb20397d3993a0e3445"
