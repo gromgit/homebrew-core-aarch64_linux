886f9c8bfedd495f0ac01a7f18f57dda973dd28d
FX Coudert <fxcoudert@gmail.com>
2022-11-17 03:16:37 +0000

    act: update 0.2.33 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -10,6 +10,7 @@ class Act < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bf2950374861263dc55c29177bf0918e33ec669b5586da21276d32d1e23a0ef4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a7a44c124a99bed2d24d870336470c99d843d4c7d12c3bf17f38d297c474e97f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f15e651950e39f051c74d76e699aca8f950ada419e93808cd516046a9eafffb7"
+    sha256 cellar: :any_skip_relocation, ventura:        "1ec48d38252f3e90ff5c9d8277c302f9b0e845556f83007e1b0558f5eb08925a"
     sha256 cellar: :any_skip_relocation, monterey:       "a0f9ded7c9eff17a0f55211e657ec30ace59d9529788cb4296f8eb58f403d3a8"
     sha256 cellar: :any_skip_relocation, big_sur:        "9bc3d121cd50152a7b2b6dd1bf3c3122f8ac881ae4e4401a890fa304011a4281"
     sha256 cellar: :any_skip_relocation, catalina:       "23d666af5737e9e586199b7cad12714d4dc1f61813634f517fd696f8d511e927"
