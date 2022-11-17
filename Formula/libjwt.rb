7e984aabf3aa63ef9affd53603b6fedf6d31df1c
Rui Chen <rui@chenrui.dev>
2022-11-17 08:35:30 +0000

    libjwt: update 1.14.0 bottle.

--- Formula/libjwt.rb
+++ Formula/libjwt.rb
@@ -9,6 +9,7 @@ class Libjwt < Formula
     sha256 cellar: :any,                 arm64_ventura:  "38ed9509a429014b28882a5970ef64e157a1906514c5f7a593c05af43d0948e0"
     sha256 cellar: :any,                 arm64_monterey: "fc108cdd5c78b51c76ff7abcbcfb20db01521b4e2f665ce1f73079045ff2dd97"
     sha256 cellar: :any,                 arm64_big_sur:  "3749847000103f76c8c72f33edcd3e00855c90fb5e99cb8080362be362cd4cae"
+    sha256 cellar: :any,                 ventura:        "d7a7fb357819054f228e7ee33508861639893c461408d9e2aea6c0ad0a7c01c8"
     sha256 cellar: :any,                 monterey:       "f98f21791845f8ad5154e826a8bdfb586d7ee509ad150b8a94f77a97ddae39d6"
     sha256 cellar: :any,                 big_sur:        "f8a4776cb98a8e345b7ab2e9d3753703e91a56c264d0e4568413a548e3bd2f23"
     sha256 cellar: :any,                 catalina:       "c89546a6d4f6fcac752fa3a1a1d1ee91d88af04c062fb4f619c4414f9e8e1048"
