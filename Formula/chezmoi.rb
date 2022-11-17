a5c3ce2a413a8cd9fd0a1ec16958a2f509b3b039
FX Coudert <fxcoudert@gmail.com>
2022-11-17 16:30:33 +0000

    chezmoi: update 2.27.1 bottle.

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -11,6 +11,7 @@ class Chezmoi < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "850a317e105cfc8bb052734a1b5ab78d6dc90006268973a838d0c2c89d9da224"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "adb947372549af1788d1448ca921de0fd5802fc9bea453a6f769f39e9cf9833f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "488d7b368a780603bc47999b6c720fb4d33d3003471f6c335815bfd54e5e0ed6"
+    sha256 cellar: :any_skip_relocation, ventura:        "0f43de69de5fb740bd46de96778b31f43c2663b5ae38ff0bdae5ffae1bd302e0"
     sha256 cellar: :any_skip_relocation, monterey:       "b73d7fb681132058e44cd46865cf12739929794bc65bb53e89cc34a68335a554"
     sha256 cellar: :any_skip_relocation, big_sur:        "3d78ff35be3ef99dc76dd5db566cb416d41ac1ba23bcacc17568dcd6cefe6bca"
     sha256 cellar: :any_skip_relocation, catalina:       "25f7c2b564d55cbbb49c48fdce6995afc797eeab781365dfe6a0acb4042b781d"
