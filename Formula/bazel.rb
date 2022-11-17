d507ba1fe6f34bfa40ea120b3fb4f0004e29cf34
FX Coudert <fxcoudert@gmail.com>
2022-11-17 07:03:59 +0000

    bazel: update 5.3.2 bottle.

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -13,6 +13,7 @@ class Bazel < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "26f9fc1f9997b0c9e644f47d2ba65057e9b5b92437bec769b5166082d74373d0"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57a0872da19e741ab0f80654aeea840b0298ff854568c683a444b34bf7f93378"
+    sha256 cellar: :any_skip_relocation, ventura:        "db1e8176e47e4119629efe0fc7b84d788ba835d990b96a09d008dc0231b23d86"
     sha256 cellar: :any_skip_relocation, monterey:       "f3fa6eeb3cde5a1146ab81cf47bfc4e8553505de816682b51e07c9c5a5c75d4f"
     sha256 cellar: :any_skip_relocation, big_sur:        "b2a6154e42833bc6b95288c1506e03218e856671d59f386933f036162483f99c"
     sha256 cellar: :any_skip_relocation, catalina:       "6d3f80d9893f122a93ed8808ab4bd2e1eaa26ddf19e7d0f9922d66f886c42187"
