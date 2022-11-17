4016a4dece89befb51162bb66931279d7f9851f3
FX Coudert <fxcoudert@gmail.com>
2022-11-17 01:08:13 +0000

    purescript: update 0.15.6 bottle.

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -9,6 +9,7 @@ class Purescript < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8b54a5c3a58dc97a2a87bf633c8a556165543f4af7e5e36775283f599f58f5e1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89000bbdbea995ae9109d0846daf4e8959fa8e0ac7868b03ea0c627c6c23c6d7"
+    sha256 cellar: :any_skip_relocation, ventura:        "50ed65494255c005d3a4f9bd9eee13ea87ba4cfd96a97b15513e958a34432bbf"
     sha256 cellar: :any_skip_relocation, monterey:       "325cefc0ea682cd30dc6e034bca02c79595d03e268cacfa95a5e0ceed6c38f4f"
     sha256 cellar: :any_skip_relocation, big_sur:        "f47f53bb001cba93513e33fc1c3aac8e931924e2f54bffbeeeca4d6f6e2e7e94"
     sha256 cellar: :any_skip_relocation, catalina:       "9989547c19a0e4959d658819880349374d2d8f09f311ec30c7af62518e3c0bc8"
