d2cf7d86ca122ca763fbe053e1f885e35378e6b1
FX Coudert <fxcoudert@gmail.com>
2022-11-17 03:53:14 +0000

    mosh: update 1.4.0 bottle.

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -10,6 +10,7 @@ class Mosh < Formula
     sha256 cellar: :any,                 arm64_ventura:  "270731aad2028b9dab43ed87dae0b650880f34aa059a3584a0b6f7609c7e8453"
     sha256 cellar: :any,                 arm64_monterey: "2780be65178f382963fd1759ff1b9df15807cc30b4a7cab5a766da1a4157ef49"
     sha256 cellar: :any,                 arm64_big_sur:  "5b52e78e946518d87bbac09cd450188e65869ebab91dfb7617534c5c913b1b01"
+    sha256 cellar: :any,                 ventura:        "77609ce1bdfddcc59df543c61ac2728ea17b8ab349839e1fa5291f9c4a574d95"
     sha256 cellar: :any,                 monterey:       "3e096f0100dd45fd8e292c59fb9c1fe42af3e6edfe3f662abb3cdb8dda8b508b"
     sha256 cellar: :any,                 big_sur:        "26537bd45fcc9a86274b84b9f6b741ffb677fa769c3be1a0d2dd568aa0447986"
     sha256 cellar: :any,                 catalina:       "2df281372623a61e3a9a7d3c795f4aefa0d2514da4c545f7c86646b28205c918"
