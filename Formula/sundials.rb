5e829701994df0023fc02ab6f1d280131eac08c6
FX Coudert <fxcoudert@gmail.com>
2022-11-17 07:57:46 +0000

    sundials: update 6.3.0 bottle.

--- Formula/sundials.rb
+++ Formula/sundials.rb
@@ -14,6 +14,7 @@ class Sundials < Formula
     sha256 cellar: :any,                 arm64_ventura:  "a80a97085d63d65189d9d266ceb5941566ea011a840f2d0a74a2ed0cfc36aa87"
     sha256 cellar: :any,                 arm64_monterey: "7589b240ea07145c55f634100f00204fd3918c1632d3d4e6f8edc97dacc830f4"
     sha256 cellar: :any,                 arm64_big_sur:  "68630220962300923d5a63a6d135364add46bd058c1651f9e7a43bafa8ef7029"
+    sha256 cellar: :any,                 ventura:        "6f8021b99b9258ef5f0cafac1d910eb93fa4b3975c03469118417b3e5a02cceb"
     sha256 cellar: :any,                 monterey:       "18e9d07618cc03ee9373c0d38c43fcc7e7576a4ecb7dffa64eed8d54637f97ea"
     sha256 cellar: :any,                 big_sur:        "754766b19dc7345f378d3125978705348cfc04bd1cfe27f99862c5b657b17d29"
     sha256 cellar: :any,                 catalina:       "735b7d4cba622ebfebd63f44484eeb1fb05fc2009f1f4dbc7cb75da6158cb815"
