d6ed830884d6fda75f771e0467421ce91881ae96
FX Coudert <fxcoudert@gmail.com>
2022-11-17 08:03:25 +0000

    yara: update 4.2.3 bottle.

--- Formula/yara.rb
+++ Formula/yara.rb
@@ -10,6 +10,7 @@ class Yara < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8a57ba40d64d2accecfa797ed768b640cc72bb289bf56c1528b4abd2258644f3"
     sha256 cellar: :any,                 arm64_monterey: "1ede25b74551c487e9b6ec1309a86fcf081880fbe42b1f0ea944b6c15e07bc41"
     sha256 cellar: :any,                 arm64_big_sur:  "f47558bcc6944b7d360234eb52d0412b8ccecae34d55408f6fe5c12cd5e2eea7"
+    sha256 cellar: :any,                 ventura:        "4fd75d100250e06e8f8e985f74247dada213e1c2795a8bf83b3969a1279dd77a"
     sha256 cellar: :any,                 monterey:       "3199dc6269f68fdb846d1778f8d608f03a0ce57e8eaf08864cb12fb7fdd9dd40"
     sha256 cellar: :any,                 big_sur:        "39c5f918530817ce24797ce4d683c535978540b35a6575a696017fb3793acb5b"
     sha256 cellar: :any,                 catalina:       "9267afdd5160d9fb2761853d9907f4a5923f3e1a88c8d46575372fa39630818b"
