dd4e72b5ee8db4a8c98f280a5805b00df2f194ae
FX Coudert <fxcoudert@gmail.com>
2022-11-17 05:26:06 +0000

    hyperkit: update 0.20210107 bottle.

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -6,6 +6,7 @@ class Hyperkit < Formula
   license "BSD-2-Clause"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, ventura:  "3b67078315551718bc3c752b943b933713ddb69058f3cb72a0f65faa6e9295ab"
     sha256 cellar: :any_skip_relocation, monterey: "da3b0d0374a85af5c649c86fb7796c1eecae468f5783bbb994a96d807e60712a"
     sha256 cellar: :any_skip_relocation, big_sur:  "f96e7270e9e853ce33f2195136b11338a5cf4d612ee50f3dd51b5c8506b4efcb"
     sha256 cellar: :any_skip_relocation, catalina: "cd58afe172473278d3ed9404e9d25e10bee487fb4e27cd6de39c950a0ccaca87"
