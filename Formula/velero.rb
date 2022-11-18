d654951c64ca2f2d74f5b76f396bf6874d5eb8a1
FX Coudert <fxcoudert@gmail.com>
2022-11-18 09:32:04 +0000

    velero: update 1.9.3 bottle.

--- Formula/velero.rb
+++ Formula/velero.rb
@@ -9,6 +9,7 @@ class Velero < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "9ac11ea8d9f84617c071b63c00b963a70aec238f0d665a75d99d227e54bafeb2"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "749341bd92efcffcc90f2ba8361a36b772d1ff94b0f66bf39a9fc925cc72250f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a4fb442951ee485f442c221904ca59591142e09ffea3edb687b8d630ee3b4771"
+    sha256 cellar: :any_skip_relocation, ventura:        "34cb83c792d234957508c52dd12b837ab702a0e5bb3dbd2191aac08bb4f2fb15"
     sha256 cellar: :any_skip_relocation, monterey:       "cc2769d44cbd216728b996bcfc2fc4e27d5d18fb973d021187d6ec12c71a9882"
     sha256 cellar: :any_skip_relocation, big_sur:        "3ea7eebdbf751e2050bd5c212178ff864410cba435222db8036370119a6c54d9"
     sha256 cellar: :any_skip_relocation, catalina:       "ce40dfce5c1a3488accf25794bc67dbde1799f2c48aaadada003fdfa41ca6c5a"
