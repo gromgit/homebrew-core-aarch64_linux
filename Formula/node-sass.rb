ea1dadcad09a6a9d02633c27972752568ff3decb
FX Coudert <fxcoudert@gmail.com>
2022-11-18 13:14:51 +0000

    node-sass: update 1.56.1 bottle.

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -11,6 +11,7 @@ class NodeSass < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
+    sha256 cellar: :any_skip_relocation, ventura:        "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
     sha256 cellar: :any_skip_relocation, monterey:       "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
     sha256 cellar: :any_skip_relocation, big_sur:        "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
     sha256 cellar: :any_skip_relocation, catalina:       "334c1a2b11bbe06b208d1f38ee554830eeaa61d186f660036a49adb68d7c1615"
