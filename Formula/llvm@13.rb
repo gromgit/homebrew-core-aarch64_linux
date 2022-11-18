b7512a957ac3184e4e5c7fd7f813504d34bde6fb
FX Coudert <fxcoudert@gmail.com>
2022-11-18 12:40:22 +0000

    llvm@13: update 13.0.1_2 bottle.

--- Formula/llvm@13.rb
+++ Formula/llvm@13.rb
@@ -11,6 +11,7 @@ class LlvmAT13 < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c1a3b007a5da85217acd18b3b2bb390be6faf46f09f1073c28927c3ea592dae7"
     sha256 cellar: :any,                 arm64_monterey: "f261f288461d9babc19d79c2fe5d984e5b589fb675f172cc2492f7bfd64e56d0"
     sha256 cellar: :any,                 arm64_big_sur:  "9e6f60b8d6b8b464345e7d9b9bf728f49fd2ad8c5cf45a7918ce3b60cc9efd91"
+    sha256 cellar: :any,                 ventura:        "02b9401a889bd8f3cdd564e90a925803c9c57415410d3ea0fae039e2c8af6293"
     sha256 cellar: :any,                 monterey:       "703cc19867f51a059f71b9fe7c9ac20c0c10d1e39e04a57bbd272c2965f542bc"
     sha256 cellar: :any,                 big_sur:        "8933be1d408ab802e7049201774922ff045e470d7e80fd570700e79bcf7055b9"
     sha256 cellar: :any,                 catalina:       "b91e1b1154707fce05d16ef54abd886d9bf0fc38a625f0bf254420ad6e4474c0"
