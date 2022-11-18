fde816be30c20a72097b59508a0af5dc242eb357
FX Coudert <fxcoudert@gmail.com>
2022-11-18 16:28:51 +0000

    nomad: update 1.4.2 bottle.

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -15,6 +15,7 @@ class Nomad < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f9026962014df505499b34d14c7dc1de4fc89eb9b5dee79ab08855a0a59e1a0f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "dbf4fd06a16bec3a91da5d885f56859aeb2cf87a3cd527b7f85df8c062a67139"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6394fee2af5ca8ca6318a773e79ffc0ea691a6baeb4f208a4fd09b5fc7650e5f"
+    sha256 cellar: :any_skip_relocation, ventura:        "5262b8e40e0f977ebf1f35c01725529c499efdd6da088d0a29c339fa189e606e"
     sha256 cellar: :any_skip_relocation, monterey:       "8bf64aafc66e2a5cd2a7f6b26ebd62d4beb80cedb425eb18aaa1319b2adc4f37"
     sha256 cellar: :any_skip_relocation, big_sur:        "d57afd8bffa859db83b927a899e1049ca7a046a8312acbd67cf406c2a18b5809"
     sha256 cellar: :any_skip_relocation, catalina:       "31fe610bfbb8904936391198cbc595a7ac50d8c99e947706ef7362f1f5038863"
