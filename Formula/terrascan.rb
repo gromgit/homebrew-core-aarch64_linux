669cfc60c0fb01bdd6f017afac58ba0aee3af6c3
FX Coudert <fxcoudert@gmail.com>
2022-11-18 12:26:15 +0000

    terrascan: update 1.16.0 bottle.

--- Formula/terrascan.rb
+++ Formula/terrascan.rb
@@ -10,6 +10,7 @@ class Terrascan < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f8a684ecd34c5727a09f2d86bb3d6080f9608f67775c98ac7fa770593de59fca"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1f2c90377860bbce26b5a66f51bd8d482d653b3b5376bb3251fdfe6981794a4a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "87572715d141f8d4ce936888d2f4a2283d95d0a40791f56e64e650a538fe3a14"
+    sha256 cellar: :any_skip_relocation, ventura:        "f34643d3d70af4a677344d89b8e8051fd6b17e20af91afdd0b0a32d0b2dbdb53"
     sha256 cellar: :any_skip_relocation, monterey:       "a2611af8389bcf3f2c78882cb7e90bd9f52ed4bf101ceee97d20d8cc8284bf79"
     sha256 cellar: :any_skip_relocation, big_sur:        "a5721af056bc6cdda3d7bb75420b3fe3f74d7578ea729f947ebdf60d969e91e1"
     sha256 cellar: :any_skip_relocation, catalina:       "5e59c49272ac8a37fd83c456e4bba353bf1111b18f817853abd642498124c1f8"
