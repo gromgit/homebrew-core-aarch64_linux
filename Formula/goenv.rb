0f13b421a0c58b98c6c5048eb465a29f917687f7
FX Coudert <fxcoudert@gmail.com>
2022-11-18 10:54:11 +0000

    goenv: update 2.0.4 bottle.

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -16,6 +16,7 @@ class Goenv < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "8ce0cde64ba99a0cf98cc07e92126a506ea38680e42c306cc6f888a6010c0803"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8ce0cde64ba99a0cf98cc07e92126a506ea38680e42c306cc6f888a6010c0803"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ce0cde64ba99a0cf98cc07e92126a506ea38680e42c306cc6f888a6010c0803"
+    sha256 cellar: :any_skip_relocation, ventura:        "dee76e5bf5df3d39deed32510baa9e1420e1395df898551da1ad19189c12d922"
     sha256 cellar: :any_skip_relocation, monterey:       "dee76e5bf5df3d39deed32510baa9e1420e1395df898551da1ad19189c12d922"
     sha256 cellar: :any_skip_relocation, big_sur:        "dee76e5bf5df3d39deed32510baa9e1420e1395df898551da1ad19189c12d922"
     sha256 cellar: :any_skip_relocation, catalina:       "dee76e5bf5df3d39deed32510baa9e1420e1395df898551da1ad19189c12d922"
