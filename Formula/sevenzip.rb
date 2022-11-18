814ae29d1781e73f79dd8e9d877e17638544d280
FX Coudert <fxcoudert@gmail.com>
2022-11-18 00:23:03 +0000

    sevenzip: update 22.01 bottle.

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -15,6 +15,7 @@ class Sevenzip < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "336a287e74813695435ba7726123db50672bc6c03379a72c4044c59b024a9797"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0b9131e0986339f9986c7248c3101776279ddb390d287098ea5be1c84d6408d9"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "15f4157398fdb03a9fc9ffa66eaf08abc72987d471b23ea22f5ab6700d7ba1a7"
+    sha256 cellar: :any_skip_relocation, ventura:        "0f86a22b685653e831bf61c9c046422cb7754bcec41b17c92e943d92fef15b6e"
     sha256 cellar: :any_skip_relocation, monterey:       "64f2ea16a7c0f8591701914621b8ada21ecd6caa3ecf2568c8d4bc470edfae00"
     sha256 cellar: :any_skip_relocation, big_sur:        "22a8c3aa0647a1f8829c7e180cb2d6ac78b071925e8c06be361716626487fd6a"
     sha256 cellar: :any_skip_relocation, catalina:       "3ac4e9978c71f2452ded1a8ee0983b0710cc66d127ff2a95dc603b6211ed9df4"
