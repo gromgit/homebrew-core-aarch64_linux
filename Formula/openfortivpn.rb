92e8998f5d3be6f96e1b64a8a2ccf8cf7e2d1e9d
FX Coudert <fxcoudert@gmail.com>
2022-11-18 02:30:53 +0000

    openfortivpn: update 1.19.0 bottle.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -9,6 +9,7 @@ class Openfortivpn < Formula
     sha256 arm64_ventura:  "cf3085228d1c1d791e69d60fc848b4758ff224359f3dedd46d7aa8d149fa1936"
     sha256 arm64_monterey: "2f65a3ce47bfa55079ece31bf270b4111ddd9ac81ac742a8243afe1c6600e932"
     sha256 arm64_big_sur:  "7e130cbe9a544dbd3d447d455399ef8cef88d31b0e81e8a8e0713b2a469f67a2"
+    sha256 ventura:        "c06e9f31eda964a6622822a8a1b0178c73c9eb38ceae9dbefdf6994558a1c454"
     sha256 monterey:       "ff5c82ec39545a45ca3676cca127c699e438e0eb2d0972cf98bddab8994a9c6e"
     sha256 big_sur:        "36df758b6d887467cd0e90f704d0b603f5486ac6f5375d845bb391cdbb9afdfb"
     sha256 catalina:       "4a09b2f6e762b7eac8e5e8ce0ca2997e1764439c3af6e5b54aa019c532c45e2b"
