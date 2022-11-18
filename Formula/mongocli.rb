6013918f6bc435426b4055b572efb3640fccd52b
FX Coudert <fxcoudert@gmail.com>
2022-11-18 01:36:48 +0000

    mongocli: update 1.27.0 bottle.

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -15,6 +15,7 @@ class Mongocli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4dcdf86f13aba04f2d0bb66a2f0a590124e69c21e4704e1740fb353e37b30616"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5e9bb7281818e642593bf01afe0fa04958bdf80788343dce24b400e8f2981001"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b041b70d391b572747107a67ccd2844e2867efb0970ecffa2de536dd27b4733"
+    sha256 cellar: :any_skip_relocation, ventura:        "639918c5433da5b4fdbf155d1af2b205cfd03edeb968cc3ab673fcf2070560ef"
     sha256 cellar: :any_skip_relocation, monterey:       "3bef87280581006bdbeece7c5b7650f6113fa6b877cac545e65eb985a1f4c62a"
     sha256 cellar: :any_skip_relocation, big_sur:        "e133e09dc0475370900c87f68fafcee7ad71b331feddbc617e9124afd1ad5b9e"
     sha256 cellar: :any_skip_relocation, catalina:       "43e17eedb6ea73eb0d940065e71ac8492e7de653ebcd0ceb32c754c9807b4d1c"
