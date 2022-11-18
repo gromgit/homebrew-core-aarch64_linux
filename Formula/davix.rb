cde6007256122f5b40252691c48e69b1b8e5d41e
FX Coudert <fxcoudert@gmail.com>
2022-11-18 23:18:34 +0000

    davix: update 0.8.3 bottle.

--- Formula/davix.rb
+++ Formula/davix.rb
@@ -10,6 +10,7 @@ class Davix < Formula
     sha256 cellar: :any,                 arm64_ventura:  "6c59ee85fc7cb668ecbf170a3bac4a258e8be44ec3bf889d052b994bd28d3fac"
     sha256 cellar: :any,                 arm64_monterey: "aa25fb42dcca1ac2ad69f5ee1a7d0ab84c5df70a2c4036907c33bf3739b8eaf3"
     sha256 cellar: :any,                 arm64_big_sur:  "95d327e7cfb9dfaa8dc0e5785f58010ba3dfcd8bba0904d9fdc3e19ea5dba38c"
+    sha256 cellar: :any,                 ventura:        "3dc5cd658130748d7182fb6f54b79280104595ce5eb55bfdc659ce6e40f2af14"
     sha256 cellar: :any,                 monterey:       "3ee908b44c9f4ecb035f409890a07ef4cc7f41365275c0bf62285cbb0784c0a7"
     sha256 cellar: :any,                 big_sur:        "554bfac4c2a799861a26fcd84bfee238856d0a06864c072ca9fd49522b53855d"
     sha256 cellar: :any,                 catalina:       "cc2d389eb2106de7c749c992097b5165cbcb07c96d97cd9946225c169b4c02d9"
