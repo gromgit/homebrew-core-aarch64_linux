4c7dbed214ca07a5d93c8f2158e2a2a6a1bd90b6
FX Coudert <fxcoudert@gmail.com>
2022-11-18 16:14:59 +0000

    algol68g: update 2.8.5_1 bottle.

--- Formula/algol68g.rb
+++ Formula/algol68g.rb
@@ -22,6 +22,7 @@ class Algol68g < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "767d41993bc543b9701cebb0017edd868373ad02f049dec77b87002ea19c3ed9"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9fb1d792d8f777ca08e22372c092a74ac6db17225fd5f110e516b891a02ceeec"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c0bde6c0a8205ec17f4d9a6c8084da44ec9c99beaf09eb7f4184042fcca2818"
+    sha256                               ventura:        "89660d0a2a30c4f7f6101449e4ecf48881ae986bc2bf8b57c3181c9084e193ad"
     sha256                               monterey:       "90017642701aa5b8c7cc9bf49937dcd0efdd75a7d25792aa4f6bdbef30ac98b6"
     sha256                               big_sur:        "14b3262c6aff2d74889e253bf8bf5dd132bd1ff5ed3847b4f6e5e5e7c8728913"
     sha256                               catalina:       "38110a685728de5d40abb532e6e295d4a13cc55cc6a4d3357c66ff5ef031513e"
