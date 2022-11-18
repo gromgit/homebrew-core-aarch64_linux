d2a92859cdbd9dd129d1d142c39a14433f827485
FX Coudert <fxcoudert@gmail.com>
2022-11-18 22:50:30 +0000

    calicoctl: update 3.24.5 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -16,6 +16,7 @@ class Calicoctl < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4725582827d92ffffbd916f76562b8a1b07f9d67e37f03937679e70e921d022d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0380924a331eb4bc344536dcbf32d4b7af6478bfeb7f64254e7f55711583099b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "75e03bfccede56db5d90d06b05becae38d34664adf7578a63129199be902515d"
+    sha256 cellar: :any_skip_relocation, ventura:        "c2bd953a921d89bf6cc8de0e49ecc14e9c5647508b3b608ec299fb2eb291544a"
     sha256 cellar: :any_skip_relocation, monterey:       "76f96acd7e89321c63e4456e4a7335075e64001f3028dafc333a9fc4272434d6"
     sha256 cellar: :any_skip_relocation, big_sur:        "79515710768c1466cab5ce1e5952bfbdca0306581e05faf3c1e3b4960b79d70b"
     sha256 cellar: :any_skip_relocation, catalina:       "1b0a9d40cd74f3d9193c43ba49aa2e32350c6b2e4c1593a94c48415ca613f7df"
