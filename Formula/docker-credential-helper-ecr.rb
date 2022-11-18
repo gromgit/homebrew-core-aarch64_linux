5594064b50385dd0e1295285a589c915908fba0e
FX Coudert <fxcoudert@gmail.com>
2022-11-18 08:28:13 +0000

    docker-credential-helper-ecr: update 0.6.0 bottle.

--- Formula/docker-credential-helper-ecr.rb
+++ Formula/docker-credential-helper-ecr.rb
@@ -16,6 +16,7 @@ class DockerCredentialHelperEcr < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "306aa5e8086723bdf6cbaa45be2c74291c9e9df620cb7f451fb485a65123022a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e0d768a76a10449f70e88d0b25dd11dc9b8f8149f6b3c153fa93a018d2375fe8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dfd6561543c85f32a1eae09b00e0b0ea8ccdeada1073227cc87fed3916b40bd5"
+    sha256 cellar: :any_skip_relocation, ventura:        "d3f95e69ebcb56bde01325a671d8983eba2edd259ba718ae7b39f1968232244c"
     sha256 cellar: :any_skip_relocation, monterey:       "e41c402435c1e0a079461dce61a5ad1a695ab3af689bae754a3e7ad9328583e7"
     sha256 cellar: :any_skip_relocation, big_sur:        "8c15457c2a829da7ac19e2a52aa1c64f5199feb4cdcd4998df46ba45676c0db9"
     sha256 cellar: :any_skip_relocation, catalina:       "4b6a17ed414ddad4cb9cba0e23eb0e427addaedb5dea660907ef0339967abee6"
