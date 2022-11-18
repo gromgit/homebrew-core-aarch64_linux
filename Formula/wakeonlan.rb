fc57e4b9c1ad76ce2caff8dbbd19db20a58db1e2
FX Coudert <fxcoudert@gmail.com>
2022-11-18 11:37:40 +0000

    wakeonlan: update 0.42 bottle.

--- Formula/wakeonlan.rb
+++ Formula/wakeonlan.rb
@@ -9,6 +9,7 @@ class Wakeonlan < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f541c4803bb947fd8bec2daa1ff9ddcc5c1f7aab2f2b1952cbae99bacea30d8c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f541c4803bb947fd8bec2daa1ff9ddcc5c1f7aab2f2b1952cbae99bacea30d8c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cd9429ee013c7d939887d72d5876a7f617855bc521f372a9d30dd393917e874d"
+    sha256 cellar: :any_skip_relocation, ventura:        "f541c4803bb947fd8bec2daa1ff9ddcc5c1f7aab2f2b1952cbae99bacea30d8c"
     sha256 cellar: :any_skip_relocation, monterey:       "f541c4803bb947fd8bec2daa1ff9ddcc5c1f7aab2f2b1952cbae99bacea30d8c"
     sha256 cellar: :any_skip_relocation, big_sur:        "cd9429ee013c7d939887d72d5876a7f617855bc521f372a9d30dd393917e874d"
     sha256 cellar: :any_skip_relocation, catalina:       "a54812034696435a392dd80980cd74b56c8dc2bc30a5d001b679637a00ce6b8e"
