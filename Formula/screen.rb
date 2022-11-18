3263302beb11b41ebc3a8d01e364dce8e05fabdc
FX Coudert <fxcoudert@gmail.com>
2022-11-18 12:00:37 +0000

    screen: update 4.9.0 bottle.

--- Formula/screen.rb
+++ Formula/screen.rb
@@ -21,6 +21,7 @@ class Screen < Formula
     sha256 arm64_ventura:  "ce1f17b090b32eec82a32d8d57c8a8e820dc5c4fb7e0cf47ffcf7e50d339a405"
     sha256 arm64_monterey: "fe5385e7a06b3b3c7b619979a2506ab727c0c2abc0f69eba749fe9f737a934ac"
     sha256 arm64_big_sur:  "b6cf1074628d7dbbf2fd816234c666f56f2a4f0a58f6ec5d6cce97fd4d0150b1"
+    sha256 ventura:        "f8d02d5c974557441f72d624d86292b2a039e180b7864ef1683252c3757be77a"
     sha256 monterey:       "7df16eac999996ffccb6215a711039ccac9e4a7b576a3ae07b90be855bd017c0"
     sha256 big_sur:        "8d4a322e94a212359803fb59f0dd306fda8baf7ae825e47dee1f307feb0dfb43"
     sha256 catalina:       "3d60abe23c16c832fab478a211305c30e86f19e3d859d19f5855b3ab21dd0082"
