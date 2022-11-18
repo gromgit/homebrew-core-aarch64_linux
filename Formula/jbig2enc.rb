74a2eaa0d61242ff1b35826ffa8407a6d7efff76
FX Coudert <fxcoudert@gmail.com>
2022-11-18 19:16:44 +0000

    jbig2enc: update 0.29_1 bottle.

--- Formula/jbig2enc.rb
+++ Formula/jbig2enc.rb
@@ -11,6 +11,7 @@ class Jbig2enc < Formula
     sha256 cellar: :any,                 arm64_ventura:  "04adc5219a6b9fd81d98e46b523e0026e3cd69f333421d17837a154d80ca3a6a"
     sha256 cellar: :any,                 arm64_monterey: "7f34bf27ad68b0a8f41b94e77ff7ac73222285c16c1fdeccc23ea5d402103d2b"
     sha256 cellar: :any,                 arm64_big_sur:  "2086a80027df8b8f6765552f9bd612067bded65ff59894354641966af9954e89"
+    sha256 cellar: :any,                 ventura:        "69448ad02527c02f6353842bfc35cefbc74b64c4c91025a57036868311efea7b"
     sha256 cellar: :any,                 monterey:       "a9b5f6a0eeebd57052daedc8a7d50731f9f1a144b2da4622137620d77b4d9e06"
     sha256 cellar: :any,                 big_sur:        "fd54b93bae050fe3c02fe0322c97c77466491d6b8426e826acde2eb1149ce846"
     sha256 cellar: :any,                 catalina:       "2140d5d4884fb99e1e263a9cf90fb5f0584634435cdb8a4b7fbec0dc40368879"
