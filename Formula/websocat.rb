f86ee41ef4df615e57ca09e1aba30198246f92a7
FX Coudert <fxcoudert@gmail.com>
2022-11-18 11:51:21 +0000

    websocat: update 1.11.0 bottle.

--- Formula/websocat.rb
+++ Formula/websocat.rb
@@ -15,6 +15,7 @@ class Websocat < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "82c1a32b50dd9c07a625cadc44eade492e099a86bcd2111378c557d8d0174630"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "af7d785dc7fbf24f0040ab34387f89c446f91b2f20ce5ac006874076bc9a12a6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6bcd20eb41f33a1404a9049c0c7b2a1401ff0206dae3727662f2a148318579a3"
+    sha256 cellar: :any_skip_relocation, ventura:        "d0d58cc427f5588b766dd7f3589be840721bce3f843a6d8304ecdc448d60cc88"
     sha256 cellar: :any_skip_relocation, monterey:       "ac96cc2a92e1376a38d876acfb50d1d1cbc85796111a783ceef62431955f2171"
     sha256 cellar: :any_skip_relocation, big_sur:        "10ed5b08af56b99543afdcb6aa44baf4b59ae105c802985d81282b7ceb02ac58"
     sha256 cellar: :any_skip_relocation, catalina:       "81368b9bb04779db0bbc183a878e9ca716cbc6cb1d4d3b28c9670441255364af"
