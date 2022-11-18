1e2962cc2f702c22cd122b56f1b15409809a532a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-18 05:04:52 +0000

    feroxbuster: update 2.7.2 bottle.

--- Formula/feroxbuster.rb
+++ Formula/feroxbuster.rb
@@ -6,13 +6,13 @@ class Feroxbuster < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "37ff1947a523c907f915b98ddbd275794e9abe0d21a03a49c3a393d360436b3e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f167fc2b4d9ae529107451fce0a51d489b71fcf756da7f1bcdbc874f21ddb9e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "545e710239f2893ba4e7727077967b0ec7059f375798c0bb3453afe125440f75"
-    sha256 cellar: :any_skip_relocation, monterey:       "b39ac240e877471e009a2709e453e3cb8fe00110744dd37dc8fc588027f2f3f2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "37ef97dd22f19f0221337642f7a486ff8fcc9227cfcfcc80b25f5b27b0154024"
-    sha256 cellar: :any_skip_relocation, catalina:       "0ff6c797667e4b8d508cacf47efefa8073ece36b113200ace735c5f8fb3d53d3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0bfce064409d4aa3a43d343daa120bf06bd4ac53ffe894e8118bcf6a3e51d632"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "75413f2b6007586d3548576911d6c9f75297afea281674b68c38b5802e3db6dd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6185753deae31bfaae583c6dbd3a09da3fb37c74a90d8e1a94f9210e655b5d3f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "348b083efe36a8c33b511f2f75691f9b1c6575bfa0570f7eaaaf6a9debb8ae8b"
+    sha256 cellar: :any_skip_relocation, monterey:       "ffc384ae8e6cf8bd81bb212d8bcbfc666ab9b1d22f7978f24f69ac357473d348"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ae08149471b593dc9f40aaf912ebcc041a3dd2adecc315fe46830fd578f031a8"
+    sha256 cellar: :any_skip_relocation, catalina:       "f6e54973a62462e24f92b8f9301bc2c1f752f7fd747fb4b4c66265afd4a0fa0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf9d13ebd118ff652a16fa55158aa1aa3d7d672b2c9c524673f0bd865aee18a5"
   end
 
   depends_on "rust" => :build
