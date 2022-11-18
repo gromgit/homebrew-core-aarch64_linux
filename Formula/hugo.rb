adad5ce50eb9f058984bb337e47e79c8b4cb8a25
FX Coudert <fxcoudert@gmail.com>
2022-11-18 07:00:38 +0000

    hugo: update 0.106.0 bottle.

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -10,6 +10,7 @@ class Hugo < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "55440af54c6aa321825da7b166624cd8e56ebd899998ea53bf873a7f14531483"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8973ca4a4610976b39da37fca2d7b879dff4fcd83c22b5d98ff521f26d1f7520"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23786de84758c60d8f629e5712c3bc82ce02b3715de0b44482573bfc4fe7ab0b"
+    sha256 cellar: :any_skip_relocation, ventura:        "94fddc18a2f2cceab42624aa48e5838cea20f5544dabddd4228807fa9cdf3f6e"
     sha256 cellar: :any_skip_relocation, monterey:       "58c1df5634fe04f2d46638615589b895296d1f5badfd981fb9cb52e314b3b284"
     sha256 cellar: :any_skip_relocation, big_sur:        "a0a8c0b56b86dbc9b892804012c8ea64bfc4a8f4f3927ddcad2588946f4e639d"
     sha256 cellar: :any_skip_relocation, catalina:       "e5b5b50e5372b560cf046c0a8bc0f3d907a8728b320089d4a948d734176544a1"
