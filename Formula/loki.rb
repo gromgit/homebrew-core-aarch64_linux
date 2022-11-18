5a59e7a4bb3a7dce1e94bf6dca7f79beba3cf1bc
FX Coudert <fxcoudert@gmail.com>
2022-11-18 05:38:37 +0000

    loki: update 2.7.0 bottle.

--- Formula/loki.rb
+++ Formula/loki.rb
@@ -15,6 +15,7 @@ class Loki < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "345a96340b635bd094b7bcb1daf6e18bf474edf20e9bf2ecf9b7bcc1e6be3f22"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "29955d163d41df6111d5937b86267a633ba82397b4870d1a47098f42cd1cd7fa"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f8db78d3279144b195fd65534adea8df0c0f022098360d51d5f13a7b23c7bc6d"
+    sha256 cellar: :any_skip_relocation, ventura:        "652d3de1381cbc5e0ecf9ae0d89ea15a030d41ea724a5b9006414caacde7d73b"
     sha256 cellar: :any_skip_relocation, monterey:       "76d9fff6c88459bb2fbf4e167f799ca0cdd3c26aeb430fb7e4953d89fe5535a5"
     sha256 cellar: :any_skip_relocation, big_sur:        "341ed4a7d0dd6cd8eea06bc233211b848874e8a638f2bbd43c8132addeff8461"
     sha256 cellar: :any_skip_relocation, catalina:       "6529d93c90a1c48180e73d238ff8b28f0cace453b18ca320c04c2b984bfcceec"
