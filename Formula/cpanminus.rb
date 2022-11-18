bbf8fabaadc20c07b3b727ac95306c6348fafc14
FX Coudert <fxcoudert@gmail.com>
2022-11-18 13:26:29 +0000

    cpanminus: update 1.7046 bottle.

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -13,6 +13,7 @@ class Cpanminus < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "50d18acee10ae2f75dea455e8b338b18f6e528002f81e97781535399e02e2eae"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1a3cbb2a37c9229ee205c974d86c645dc5455da369a8b8ca3f6b8429cdea0676"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "164032cd2f4c0a185406dc9ab12e933c1b671b14b9c99a12d0c2c4b475043901"
+    sha256 cellar: :any_skip_relocation, ventura:        "ab901b1645c97fa50ee52ecc4bf51bac7f8a8959abe664bbdd66c3d1a565d1a7"
     sha256 cellar: :any_skip_relocation, monterey:       "9dc8b90204bc795116a839e16c1f4796355286bf6d8b0e3da01ef739dedb9748"
     sha256 cellar: :any_skip_relocation, big_sur:        "3663bd1fdc1ded95e73ec538b6a4f052f48c0e2f343fd232ba7500865bb5c793"
     sha256 cellar: :any_skip_relocation, catalina:       "4d47cf54e738c85d981baac0f29c949dfab4551eb27cc18da52c3144344ee7c7"
