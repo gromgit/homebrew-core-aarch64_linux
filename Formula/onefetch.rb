05c23c3d9c4a70436a17d337a4e208954b596602
FX Coudert <fxcoudert@gmail.com>
2022-11-18 13:19:54 +0000

    onefetch: update 2.13.2 bottle.

--- Formula/onefetch.rb
+++ Formula/onefetch.rb
@@ -10,6 +10,7 @@ class Onefetch < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d87dfe9824d0ac95198779f4a07e95754331bd77e9b5bec84ccc23c1061d4c02"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "68f9c93230cb9892c4bcb843cd65ae4d20e648784bacc708dd3ce92bb1dc6f45"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6b6594a0aafd7c636f0a6c50228d95477b077df6460d3d54c36462d7555e6c3e"
+    sha256 cellar: :any_skip_relocation, ventura:        "ed52509ec3096d4f1c4733de9ddc177797f03f11d2e8c78945763f9e67302ab7"
     sha256 cellar: :any_skip_relocation, monterey:       "f92797185a5368cbf1960a7da83eaa2b1d45332b21bece8c6b88045bc5e16a53"
     sha256 cellar: :any_skip_relocation, big_sur:        "e1a0006e7cc0bec2ddfcda64e14e053c9b4132623e14709c0cb00983af40069f"
     sha256 cellar: :any_skip_relocation, catalina:       "632c7903cdec94d15b55b9cee78485df87539882319a4a323334a7e4b352f72b"
