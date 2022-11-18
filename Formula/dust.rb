0c0e96113ac0590db1fb5758a0a881e02eae072e
FX Coudert <fxcoudert@gmail.com>
2022-11-18 16:30:43 +0000

    dust: update 0.8.3 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -16,6 +16,7 @@ class Dust < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ffa6dcff87e28671bcb9d488a54e2b095d78ede9945a736b692f1f03d6e4a1d2"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8459366edac296e35df8598fa1ded08448b691ab07de289d3ad02ab403644ac0"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "13c912d4ca38e5fb053a02bb3534df155125f662a1a5554e4def555b74d0c5e4"
+    sha256 cellar: :any_skip_relocation, ventura:        "751b82c8a0d41f9e8dcbe91b4fad1e08727a7d958078961058ea94841873e676"
     sha256 cellar: :any_skip_relocation, monterey:       "6a04c6681d1cfb6357605d018be6fc8cf20fe29b9da656a5886a5893585a6c9c"
     sha256 cellar: :any_skip_relocation, big_sur:        "1fb7f67c5612d8172e34e019af005c267119ca1caa2bb2cc1fd2116aef182c39"
     sha256 cellar: :any_skip_relocation, catalina:       "0e3b7c9209a8111e83f97ed92e8f41c220564558d032e7a918017075e57d68c4"
