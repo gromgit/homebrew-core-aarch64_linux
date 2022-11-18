1d5dd2df60963275965c58f66b3baf483841c434
FX Coudert <fxcoudert@gmail.com>
2022-11-18 02:25:02 +0000

    git-delta: update 0.14.0 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -11,6 +11,7 @@ class GitDelta < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "b7c16c0cbdf9e6e51d24c877a05c8be4045d2804b0f3e9662055c68b7978caa2"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "59cfd83238180b2b19b87273973af0e9b7260f89f276f97545256088fb7af942"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e7a28647fe1d1d4e3a5790910b6fc0f54af11b468cd8bd2ff3a018885886ad4d"
+    sha256 cellar: :any_skip_relocation, ventura:        "332091753806ce5f2804c8f55ea5319cd9c1fdb62f70fe068ae17589a1d3ba12"
     sha256 cellar: :any_skip_relocation, monterey:       "5727dfe745c390bfe16ea295e6622b30f283611f33d07f760f5fe63f6304f400"
     sha256 cellar: :any_skip_relocation, big_sur:        "1a2f27f7061835d7f51da6a504f594f55970b3641f534c723fe86ed8043c5819"
     sha256 cellar: :any_skip_relocation, catalina:       "9dc934459be9b1ae71d9c3dd680edc441111561cb241c9ff8b777eec8f869d30"
