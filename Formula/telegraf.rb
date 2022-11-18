95455c607e237d6fcd671a08e44dec4d638e1210
FX Coudert <fxcoudert@gmail.com>
2022-11-18 16:54:06 +0000

    telegraf: update 1.24.3 bottle.

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -15,6 +15,7 @@ class Telegraf < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "7e065ec7e64bebb37c61c9e7d5c4f77dd2c6e09922e6029d55ef7ff0ffec7488"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d50b5c8fd7483592490daf2b8bc1d623e97d04fbf6197a5f29b3277ae0637274"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "acfab3077cd3f6e0132a188d790db7d723495b15462687021a336727ac6d7383"
+    sha256 cellar: :any_skip_relocation, ventura:        "eff44ccbb01ff97639e72c174404914448215a82be9396b1dea4a0352a66bc8f"
     sha256 cellar: :any_skip_relocation, monterey:       "6c290959cdf1057050656720dc717257864ff18a214aa6eab0aff96eab1599f5"
     sha256 cellar: :any_skip_relocation, big_sur:        "52f865c45e9f177cc00f53c2b3f84806c15d566d53138e268833ac3198393e68"
     sha256 cellar: :any_skip_relocation, catalina:       "b38dee94066c25bc508df8407fa4fe643f52f20e492bf68be4c9f0eba3673342"
