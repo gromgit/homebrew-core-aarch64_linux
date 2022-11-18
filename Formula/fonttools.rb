f4efd9cfed3ae940fb4192f664b9ad5a89e34822
FX Coudert <fxcoudert@gmail.com>
2022-11-18 23:05:34 +0000

    fonttools: update 4.38.0 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -12,6 +12,7 @@ class Fonttools < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "adf010de711f8644ec23539e4ac5974b414868a53f02b2ee5f38d2c057d669ac"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "12b9422de86b9acb838f84fb1d054f848df1554169d848526c577f8222217564"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "518a1b96e5c907f4b38c33bedd104ef0c773a9b446c6f5a4c29113cc30134eb5"
+    sha256 cellar: :any_skip_relocation, ventura:        "cf01c606c615856f589fbed5c7f6c171c18e276c7f99bb1217dad2478598582e"
     sha256 cellar: :any_skip_relocation, monterey:       "07338b9af054e8afa10d396f70d60be4592985352ce48dbcc1f9e7674d5d7c8f"
     sha256 cellar: :any_skip_relocation, big_sur:        "23dccf78c7c582b2c97f3a579b80cfc319be91f3bce82e475091c922a5ef0434"
     sha256 cellar: :any_skip_relocation, catalina:       "64194945854d76fc5d14064ea63f9946e014bdbc71c306255eb9199a6ba40b0f"
