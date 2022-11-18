499f85558f9ceee0c49ace3521a08957d5ea5350
FX Coudert <fxcoudert@gmail.com>
2022-11-18 06:00:11 +0000

    libgphoto2: update 2.5.30_1 bottle.

--- Formula/libgphoto2.rb
+++ Formula/libgphoto2.rb
@@ -15,6 +15,7 @@ class Libgphoto2 < Formula
     sha256 arm64_ventura:  "688c529e85f9e34c624cfde0dddbcef5aebfeff2237954d8ce53ef60d444cced"
     sha256 arm64_monterey: "68c8f29614d57710c8d00f5ad526b848eaaf38c4d70ec77f8531283255ed2c1b"
     sha256 arm64_big_sur:  "e3744a3675a5f6b5727181755d126730bcc79a39545e6a0a1133a1e4d5a97e74"
+    sha256 ventura:        "831c3e119f5e201b25799003a1a372b661c9235b15287d5b08ff5f1d157a07c7"
     sha256 monterey:       "d72b3a28421b921f1189717b8aa9cefb819e7d90fcd0029b8b2c4e354836023a"
     sha256 big_sur:        "54eb7a59f8fe1f56eafb31441f29c577e7685e39590cd8d3cfa59ee54c8c24b0"
     sha256 catalina:       "31adfaaaee016330cf07e8e9a71ccc4517a987038504d8a6992cb6bf2deea596"
