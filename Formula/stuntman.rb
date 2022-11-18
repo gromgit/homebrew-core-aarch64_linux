59b9012c0e2c058a054706860f6da7eefffc6a00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-18 14:05:29 +0000

    stuntman: update 1.2.16 bottle.

--- Formula/stuntman.rb
+++ Formula/stuntman.rb
@@ -12,15 +12,14 @@ class Stuntman < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9838b20f9e70ececf2aef789557caf2a26173e2ab8d7476ee61d606d51b956ef"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "743a5f66d4564fbab9bbf2973ab49ee3f8736522a94315c3c7d5cbb818441c60"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "794de99474fb9f2dd0797e2a08f1d27b52f2fdc7540165cad5703212bb24ded0"
-    sha256 cellar: :any_skip_relocation, monterey:       "be26b274322810de150f7f119cd89e905d136752e603cf3ca81121d2a8a3e014"
-    sha256 cellar: :any_skip_relocation, big_sur:        "74b6ba4ad33c987b2b3e8a70f4c4e3c383cfa1955a0eb499b02edbf4ed7c8a45"
-    sha256 cellar: :any_skip_relocation, catalina:       "e337d1ad8978b0bb926bca46992575b686145f9e8eb43dbc990e4efe08539722"
-    sha256 cellar: :any_skip_relocation, mojave:         "d1b2a91211d57f057081fba43d0ed6ae3b05c40114b1e77f0cd3c0189f7ad07c"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "a7055d814d7645e408d92ffeba5ff5c1215302bdf4411bbf02e8d49ff40115a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "901fccf90a4f4fad5f18e3c8f429f22504244db3a12ecbc1ef354ad5baf7b988"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f9ff0ae91033b2b01cc9f72180bc752ef48318ec70f538caf943f0baf1fd3bff"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "54d82da2aa9283edd6641bd761cd1c45411d4305ae648672ae3e98079d841894"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c875f14ba13aacc89c0f798cbbea161aac655bf3bcaf9284645eb43aea764b55"
+    sha256 cellar: :any_skip_relocation, monterey:       "9ad956118fe74ee3af2a673b6a1afc0736d39f51342cb7f4b926dc13e0d28cab"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3180e4e3c719363753cefef52e45972031815f2709760c6b63b4d4e9721e1d4a"
+    sha256 cellar: :any_skip_relocation, catalina:       "2ac7951871edd61c9b254d5436a1b8ba1d939908a9a22ac3ef05b975d34490a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bddee00f936559705e2837cc56956efbc2569f98da27d7abb41640d7f87df7d0"
   end
 
   depends_on "boost" => :build
