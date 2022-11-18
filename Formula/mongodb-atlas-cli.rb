ec37a3ce8fdad4039bbd43ffcc52c98ba72cbd1b
FX Coudert <fxcoudert@gmail.com>
2022-11-18 00:15:00 +0000

    mongodb-atlas-cli: update 1.2.1 bottle.

--- Formula/mongodb-atlas-cli.rb
+++ Formula/mongodb-atlas-cli.rb
@@ -15,6 +15,7 @@ class MongodbAtlasCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6c7282e5fb05f6c1821ea6fb874c3bc2c762b3d3bf876edd8d1acf4664f6a9f5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f7e581be75db1b086a033877196b898dee14774e85ae2e48718845cbca73070a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "472d2af957cb79d701494c5633a8ce710e6d21894de96791127747918d4bb285"
+    sha256 cellar: :any_skip_relocation, ventura:        "e6f78be4672c7498d6a8cda621ecfdd119b409ca0fb5f4cc65ac27f7b177f95b"
     sha256 cellar: :any_skip_relocation, monterey:       "5dcb37456dc90a650db229203b3ed9c442fdaa95fba9e0cd38b4df572b092936"
     sha256 cellar: :any_skip_relocation, big_sur:        "91f9ff8c043776788b77116c31f1b69a54fd085a1679b0af39eee0926db92dac"
     sha256 cellar: :any_skip_relocation, catalina:       "01cb5857f51bc43673c0ffb248e60b296ae3df23a13fa499ea97de2fe191ea71"
