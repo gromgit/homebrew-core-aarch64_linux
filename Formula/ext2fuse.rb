0ddeac758837da580376d559cd812d695a1de2fa
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-11-18 16:52:26 +0100

    ext2fuse: depend on linux

--- Formula/ext2fuse.rb
+++ Formula/ext2fuse.rb
@@ -6,21 +6,12 @@ class Ext2fuse < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 catalina:     "41c770edbb267f3d8d1fe591d947148e7c190adec47940f7d0d6dd1516b6592c"
-    sha256 cellar: :any,                 mojave:       "541b0787069c0bf37607392a9789ed4e3b2f21ebe214b3274ec27023aa03335f"
-    sha256 cellar: :any,                 high_sierra:  "0b8e89292e91a8fbe00430ae16a3ebbfdbba1017f6dee4801bcf8e63d238962f"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "cf8a8ab7893e4703857cc93f41853567140bb2713e90dffd0db844d916d83ce9"
   end
 
   depends_on "e2fsprogs"
-
-  on_macos do
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse@2"
-  end
+  depends_on "libfuse@2"
+  depends_on :linux # on macOS, requires closed-source macFUSE
 
   # Fix build failure because of missing argument to open() Linux.
   # Patch submitted upstream to SourceForge page:
@@ -35,18 +26,6 @@ class Ext2fuse < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 end
 
