29e6db9532ac1bbf86ab1619c1955063171d22fb
FX Coudert <fxcoudert@gmail.com>
2022-11-18 13:45:44 +0000

    flow: update 0.193.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -10,6 +10,7 @@ class Flow < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6f070445600dc77fe45bcd9cd96d00e27cdd164de145434b92135ab8f591aa14"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4a19fd3b4f5a5df661b896c58586185b40b7ee92a59bbf2ad12985dbd9c68cf6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd220f055925b5d8fcae387df9ecf6e4c1daf31fac4a8dc4ae9bce1143158296"
+    sha256 cellar: :any_skip_relocation, ventura:        "a6a590552cdb405cc292ea625c5c71abfa90414f45095a0a95f814b43b49c790"
     sha256 cellar: :any_skip_relocation, monterey:       "a94b316feb8cde0fb27f6d73dc88728c487b687f698de3036fcfb5454b1356db"
     sha256 cellar: :any_skip_relocation, big_sur:        "2566b21828be3258a0bdb00bf0f28301d583e391d5e52b3cb4002c71c7c005d6"
     sha256 cellar: :any_skip_relocation, catalina:       "2dc47b9a9599c6d774d63776db6e47e6301cdcac99af15399b792fbbcbb78d12"
