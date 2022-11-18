a2107f9490a50b0503ae75d51b54f932942c180a
FX Coudert <fxcoudert@gmail.com>
2022-11-18 02:51:28 +0000

    usbredir: update 0.13.0 bottle.

--- Formula/usbredir.rb
+++ Formula/usbredir.rb
@@ -14,6 +14,7 @@ class Usbredir < Formula
     sha256 cellar: :any, arm64_ventura:  "922c8ef4d850cc2f3f1a004c2d5859d950cb3dbb7e507e709068f50ef1a4ee65"
     sha256 cellar: :any, arm64_monterey: "766114e04f8dc5d0c3914d481ce916996421fd1bf346ef2e0a72505c5ed318a3"
     sha256 cellar: :any, arm64_big_sur:  "ecba2c1e3a49f6e4deafb9cfaf53fe147cf93fc758ae160167488bf387d00341"
+    sha256 cellar: :any, ventura:        "80b97dc409ad5daaf07aef1bcd4b3455d6d98b7ddbc8094c4d71d5ceb49be3ec"
     sha256 cellar: :any, monterey:       "13ec692e76a7fe69488761de6fc3c3beea20dc14a8afe5175be44098e6e49712"
     sha256 cellar: :any, big_sur:        "1a5750dad9ec230adf8c51f5bffa902923e18622efc900712453977d12fcbd08"
     sha256 cellar: :any, catalina:       "ffe0226c57d557ff516255a2df0e73adde8f533acabf57094219dfe5a763513c"
