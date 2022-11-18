aaefc207e761054cf6c5e703438ae771653a58bc
FX Coudert <fxcoudert@gmail.com>
2022-11-18 13:59:33 +0000

    whois: update 5.5.14 bottle.

--- Formula/whois.rb
+++ Formula/whois.rb
@@ -10,6 +10,7 @@ class Whois < Formula
     sha256 cellar: :any,                 arm64_ventura:  "340adbc5d5bacc197c252b34bbdcffc7dee121592dd36e2b5271b251a6ba3b7f"
     sha256 cellar: :any,                 arm64_monterey: "cde452357f82ac8bedf2353c9109a6178a86fcfa8fc4faf91faede038c04b461"
     sha256 cellar: :any,                 arm64_big_sur:  "9e8f8166f2d5cab313f02aa194f4eb165df351c9a3b284a37c71bbcd75e1e79c"
+    sha256 cellar: :any,                 ventura:        "7e7b44f7f49e14832b0bd2a9bec20a610bd0b79d914ee786569f1ad6428bd86d"
     sha256 cellar: :any,                 monterey:       "4910dd000629e19c72c7349972f8d3f15d4e4c8f9d515dc2bbe800e217c26730"
     sha256 cellar: :any,                 big_sur:        "e2b20d884b4fb48d578b5509b242044aa5b4a6c5a83a0774ac098a5a86f424c6"
     sha256 cellar: :any,                 catalina:       "0f7260067dcfc8729651fd5a8a0e7d0bedbde36838976d56e47b160f8fb548ef"
