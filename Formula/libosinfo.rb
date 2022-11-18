4ad53c673fbb69575f7440121d89ee077ae29840
FX Coudert <fxcoudert@gmail.com>
2022-11-18 21:47:37 +0000

    libosinfo: update 1.10.0 bottle.

--- Formula/libosinfo.rb
+++ Formula/libosinfo.rb
@@ -14,6 +14,7 @@ class Libosinfo < Formula
     sha256 arm64_ventura:  "b849e568f1debeed02519d9d5bf34072403397c1cc33998a238eb8daaf3eb09b"
     sha256 arm64_monterey: "2659892b2d277e688c8edc0f03875c1e518f5ab515a200d58698f717e9ed7dac"
     sha256 arm64_big_sur:  "e8f42ab6e678acb61213692e472cc233112067f87b9d10744c4db4a10e14729c"
+    sha256 ventura:        "8ce2df8bcd25e66c828f766dc7de3055e03b0c28878b4c54153acc89fa9fd2e2"
     sha256 monterey:       "6fa3411b44dcdd8d33e73fa6d7b4e4d21828d0e00c52430c68684e9a0a2a45c6"
     sha256 big_sur:        "494af85f0b66b208db81e2114e492c683028ebe8c1748c61bfaaaa5b8fc7892f"
     sha256 catalina:       "ae3b5681f80d8a2fb8413b4d0e86ea64a4c6e48d809b9f96d2064ed1d7ecb570"
