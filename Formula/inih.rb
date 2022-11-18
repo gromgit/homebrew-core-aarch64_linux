2da65c6eb55c1d26e9242191bc2a02b5bbd2b84d
FX Coudert <fxcoudert@gmail.com>
2022-11-18 06:03:47 +0000

    inih: update 56 bottle.

--- Formula/inih.rb
+++ Formula/inih.rb
@@ -9,6 +9,7 @@ class Inih < Formula
     sha256 cellar: :any,                 arm64_ventura:  "62d63767678a16ba47ca7dcbe47ded6e081fe9f4de657868e9db4abf9cfbdfc8"
     sha256 cellar: :any,                 arm64_monterey: "c58eb0d442cec960657feff64d84a98f936909ee157c8bfc296a020368a0b29c"
     sha256 cellar: :any,                 arm64_big_sur:  "d321facd01bf101f9ab0dfe77e16bb6ffb673f70940af3f7866dee722fcd0f63"
+    sha256 cellar: :any,                 ventura:        "55e9f70f34da60eb227bf1b8771891332b3e0d3ecf6320ee44f9f194b5b16dbb"
     sha256 cellar: :any,                 monterey:       "9479af24ebfb40bf0af7b7c932e2a28ee576a6cac5049813046bd77e43057fcc"
     sha256 cellar: :any,                 big_sur:        "8758bc663cf469d9c56332283c2bc1011b193b0358491ff0a0f53388c62adb5e"
     sha256 cellar: :any,                 catalina:       "f046d88bcab9301f78c4de41c70bd543920021f572e4fb1622e81f98b0b7e07a"
