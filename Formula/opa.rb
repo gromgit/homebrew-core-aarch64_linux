1d696fe6c5b9aa151a082bec5dd5f6ac313e6332
FX Coudert <fxcoudert@gmail.com>
2022-11-18 12:53:03 +0000

    opa: update 0.46.1 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -10,6 +10,7 @@ class Opa < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "966509891800adcdf588d52ea497cfae40432e9cc1838f42a2780b90447bd506"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4cdd115bccb3b5f8f011aa60a3582169aad2dc38363d1c0c9034aa1e88817cb8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7300f9bf6fd309f16a4bdb0f82fa0c2a9400eeb31f7ca7ff67cc94c4cc1badda"
+    sha256 cellar: :any_skip_relocation, ventura:        "cddee24d323448c37aa6b1c67c4ec5df2358fa845a1865a74fc61c5fa736f5b0"
     sha256 cellar: :any_skip_relocation, monterey:       "af29fb92919efec7c6637759aba9a833b9bb20405a2f4750a72d9ce2aa31a082"
     sha256 cellar: :any_skip_relocation, big_sur:        "8f8acc4f7e8dea737069b7c9aff41d7d7f91f514d1ba19e560eae4ea152217d1"
     sha256 cellar: :any_skip_relocation, catalina:       "edd79de8597308df01643b10efd44e7f3809fa80a507d47a48c951cd3ff1b942"
