245ea5b9ed0b524d6aa4435fc1bb9b70a9e6478c
FX Coudert <fxcoudert@gmail.com>
2022-11-18 02:06:58 +0000

    terraform-docs: update 0.16.0 bottle.

--- Formula/terraform-docs.rb
+++ Formula/terraform-docs.rb
@@ -9,6 +9,7 @@ class TerraformDocs < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "26fd4d4865b9a169773dbb811b2e1cafe1262a36b86394ac767b317069742a18"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1f2f4cf6fcf375be8963c87892a9949fd415f3bd7d91cf7c2e6ecbd51525aaf8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4019590722af255f89f39cb67f6a032bd5ccc2fb50f8949c7928215b215cf6f7"
+    sha256 cellar: :any_skip_relocation, ventura:        "1c0353bcf4ce9db7c9b71015649c05414c8a17ac5edb27c36721db429fb896e3"
     sha256 cellar: :any_skip_relocation, monterey:       "2feecfdf034d99b6b9de4d20d377d91d546c800fdcf3efb32ab8fc74936af84b"
     sha256 cellar: :any_skip_relocation, big_sur:        "a8bd7087f7d1b8f351c44cef6ddfe5a8adcdc06999665f0f46b0d9753d2e50f0"
     sha256 cellar: :any_skip_relocation, catalina:       "434ec046eb696cc5a6eded18b8afca8e346e7009165d9a8e12e88ffb3fc3811e"
