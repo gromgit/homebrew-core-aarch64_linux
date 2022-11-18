274b08aeddcca720814d33ca1c3925709e79134c
FX Coudert <fxcoudert@gmail.com>
2022-11-18 03:49:42 +0000

    infracost: update 0.10.13 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -10,6 +10,7 @@ class Infracost < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "98c7db475b4f9b82e9054ff7ac71bbf32ed2f8c23d15db067eb16012952688f1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6e40955c53108779745aa20116f692082e54d24b94b21463591c8867c6085038"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4472f21b932d270087184802015a88573b54bace13561f704b2f48f65314a2a1"
+    sha256 cellar: :any_skip_relocation, ventura:        "27e6c9bc9090945caf98958c9009d6fc58fe5680143d2b9169514e69bf7febce"
     sha256 cellar: :any_skip_relocation, monterey:       "cf27d1421cb971548de0964ca08ed1e59affca2e000a9e9e33795ca5f3b3cece"
     sha256 cellar: :any_skip_relocation, big_sur:        "16acc235edb8c2f0de3b72f0f504a870cebbcab22c5a8647da35fc74eade526f"
     sha256 cellar: :any_skip_relocation, catalina:       "16acc235edb8c2f0de3b72f0f504a870cebbcab22c5a8647da35fc74eade526f"
