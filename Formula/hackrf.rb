037004df83dc942fdcd433e1167515961ec3e63a
FX Coudert <fxcoudert@gmail.com>
2022-11-18 22:23:21 +0000

    hackrf: update 2022.09.1_1 bottle.

--- Formula/hackrf.rb
+++ Formula/hackrf.rb
@@ -16,6 +16,7 @@ class Hackrf < Formula
     sha256 cellar: :any,                 arm64_ventura:  "4c6c13e649df4b2457a93a5269f2376cfc7fab7cafc88afd4914cdce52d238e1"
     sha256 cellar: :any,                 arm64_monterey: "8c6eed0a7f15d8fe524f3db7256624caababc440e6a3db0d2c7ce39244592d87"
     sha256 cellar: :any,                 arm64_big_sur:  "5ce7b90e03e6cfe0e3a4cae61133539d7a930a5cbfb3d841918a4adde7bc95d3"
+    sha256 cellar: :any,                 ventura:        "fae966e776918d07134f6cb9db2ff637e5693a32fa5bb29116828f0a219174b1"
     sha256 cellar: :any,                 monterey:       "afbe5e6273018258bb621487af4d00e6dc5167ad8d153eedddc85679b530f227"
     sha256 cellar: :any,                 big_sur:        "5d33b942ba20327b818a4f3bb77a62bddcc15956708ba1cfee27daabb748c14c"
     sha256 cellar: :any,                 catalina:       "e4e88439a674526e27764c61813571ded730ea4e7ab8fbf1e573332e428d4ef2"
