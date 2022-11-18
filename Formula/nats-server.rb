cff165dcf42e12ee807d16eb11ddc8f8a7497e0c
FX Coudert <fxcoudert@gmail.com>
2022-11-18 15:03:27 +0000

    nats-server: update 2.9.7 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -10,6 +10,7 @@ class NatsServer < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "8ce48fd805dac372b68e723f0b040ef25a05efac2c7ee4e7c385500e3f2439a3"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "da56d0f251a705d03f0eac5fa6065ac08cefb6687beb94e8eec3ca413b7c66f3"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4293351c0b62e606801c11f08fedc52795b758008e0dfd56c29cc3304397204d"
+    sha256 cellar: :any_skip_relocation, ventura:        "4d5e020926005b0665745a1910bffbb7ce713cb31b8d6fbfe4af5f264afde15e"
     sha256 cellar: :any_skip_relocation, monterey:       "b5df0aece2acffa008067683e29c000ae8e0f4b037e3f3d8dc236cde429d37bd"
     sha256 cellar: :any_skip_relocation, big_sur:        "ce29e92e3636ba09ee392939685dd22244e1109fc6d8c157313da58ebbd731e6"
     sha256 cellar: :any_skip_relocation, catalina:       "e6497e2c343caab847203edaf1d399cdde4f40f77f29bac7a4bec5b51dadb729"
