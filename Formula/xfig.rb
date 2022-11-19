7b20d2f4dbd4683acaf9398b0ad231fe5a9deb05
FX Coudert <fxcoudert@gmail.com>
2022-11-19 16:41:02 +0000

    xfig: update 3.2.8b_4 bottle.

--- Formula/xfig.rb
+++ Formula/xfig.rb
@@ -15,6 +15,7 @@ class Xfig < Formula
     sha256 arm64_ventura:  "8574d41d8a47a3627db72b121b9243901b3e36c78f5e24d1a061b6b3e54c55d7"
     sha256 arm64_monterey: "19e623d10ad5afe8b0dc2aeba0e4f034f04f0419f54aeadef69e0af4a17a4200"
     sha256 arm64_big_sur:  "caaf9d6a60be127104f5ea789913c07b25ce44505e17136c2b3882a688da7416"
+    sha256 ventura:        "032f71865561c6e8a68bf9e99c13153fb68461419b67a104b905d04ae73b6ccd"
     sha256 monterey:       "7ad534b38ffc6cfade2018b4e8f85b9d938ade756f048634b8b8dc28d4d3d491"
     sha256 big_sur:        "023e9468783cfca2a0d534e082e40b394e7f04e4b375cba94223262bb2a1ae5f"
     sha256 catalina:       "35cf4130db30aa7204f7246e1eaf95f1e311bb605179099ad865dee6e1ccdedb"
