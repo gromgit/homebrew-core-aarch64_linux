869499fddaebe054aefb2db5fe703e5ff92de30b
FX Coudert <fxcoudert@gmail.com>
2022-11-19 22:48:52 +0000

    gatsby-cli: update 5.0.0 bottle.

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -12,6 +12,7 @@ class GatsbyCli < Formula
     sha256                               arm64_ventura:  "0faabcb56a0162440ac647a0875d5a3273a7e7735c51a87b5331b6ddbd5e3d7c"
     sha256                               arm64_monterey: "14abe118dd584387be77c83e0daaa6f21500427eb5eb9dcf5af233809a732e47"
     sha256                               arm64_big_sur:  "a5c14e685c3db9ce83d08e4dcea2bcef145fe9b321cc91e8d7ded13ef206fb5c"
+    sha256                               ventura:        "b95062c1cad6e22b17f9c886f6664897a5c1f5ba8adf4dbcc240a8878f2554dd"
     sha256                               monterey:       "38c69b175f696563b322f06a3208fe1b774a87836425546d4b9a8252896d2694"
     sha256                               big_sur:        "4fb6a93fddfcf91c50d34bf5050fcbfba4c1ce3357d2d0ec783dcc3414699aa2"
     sha256                               catalina:       "58483466617dfa5f7ecd2dcdc357a8d9abf03cd0c1f4589c7d649f6851d10165"
