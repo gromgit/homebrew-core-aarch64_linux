47c3ac572bb896b26b180c8a89590c8b3499b165
FX Coudert <fxcoudert@gmail.com>
2022-11-19 14:42:05 +0000

    libfabric: update 1.16.1 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -10,6 +10,7 @@ class Libfabric < Formula
     sha256 cellar: :any,                 arm64_ventura:  "27fcdd0efd7e8cb9c9c3ab2bd753652aea58797ab5d5e1dc21ea78965df2f416"
     sha256 cellar: :any,                 arm64_monterey: "d0ef41555d16ef392456abd4f7adc4d836ae986d6142265dc967089b5ddfd654"
     sha256 cellar: :any,                 arm64_big_sur:  "dd53fa0772b081652dbd851a9c9021221b973012a41daf99f4066fbb9921c592"
+    sha256 cellar: :any,                 ventura:        "6626d21e58c0cc8b7ea66e50bb1f6326a863aa13812b9d5dbfa295674959ab60"
     sha256 cellar: :any,                 monterey:       "112ca924b5cca67d91f1ec102e68db13a8a1c934df43f48d78a5319635e2ffbe"
     sha256 cellar: :any,                 big_sur:        "66cffcac1e77eebf6a7b395f46d8726eba1feb5892a94ef60653a672de95760f"
     sha256 cellar: :any,                 catalina:       "591163806258adc24cc89a71eeff81ba85dfb67799012bf63facb8febc5f6abf"
