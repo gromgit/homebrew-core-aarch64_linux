93e03e8e437a2dab4b68074ca534eaebedb34553
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-19 02:35:09 +0000

    bash-language-server: update 3.1.1 bottle.

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -8,13 +8,13 @@ class BashLanguageServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "42b66a767a56d5cd8a71a4763572b88a23b430b0838e17a8d1341bd71369361a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c9e5313f877b77afad07f9418b5a4bc0828e911173d6bd4b0726dc02885fed4a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c9e5313f877b77afad07f9418b5a4bc0828e911173d6bd4b0726dc02885fed4a"
-    sha256 cellar: :any_skip_relocation, monterey:       "ea2aa92dcfe287797e4eb693dfbfaede9ab13c28b177fa4c7f500b92d8e261f6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ea2aa92dcfe287797e4eb693dfbfaede9ab13c28b177fa4c7f500b92d8e261f6"
-    sha256 cellar: :any_skip_relocation, catalina:       "ea2aa92dcfe287797e4eb693dfbfaede9ab13c28b177fa4c7f500b92d8e261f6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9e5313f877b77afad07f9418b5a4bc0828e911173d6bd4b0726dc02885fed4a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
+    sha256 cellar: :any_skip_relocation, monterey:       "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
+    sha256 cellar: :any_skip_relocation, catalina:       "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
   end
 
   depends_on "node"
