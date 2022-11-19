8c9c28e9aaa017ad1181b05b322cbf88c4781311
FX Coudert <fxcoudert@gmail.com>
2022-11-19 00:22:53 +0000

    pcsc-lite: update 1.9.9 bottle.

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -14,6 +14,7 @@ class PcscLite < Formula
     sha256 cellar: :any,                 arm64_ventura:  "b870e407020eeec5d190dfb064723e3af63157c51d1a45bc4c11ca79a6c16476"
     sha256 cellar: :any,                 arm64_monterey: "3e398a322c62a2b600982100ec3c35d13d3f4c1cf5714fa91c8c5b69652d0d5a"
     sha256 cellar: :any,                 arm64_big_sur:  "7df5425e9242a4322f303555d9b8aff55601da15be1e1a76093d3bac8ba822c0"
+    sha256 cellar: :any,                 ventura:        "c35348b30d24a1338d02d7fd4ec81b88fbb1392dc23bf1c1574fc19b19429e74"
     sha256 cellar: :any,                 monterey:       "9928ccd674122c0479759e48ddf57e96dbef2b202c72bce529b8fbc4e6a54aff"
     sha256 cellar: :any,                 big_sur:        "1bd869f97882367992178408a6de15a483139a2469b39bf6f8689790571e8b48"
     sha256 cellar: :any,                 catalina:       "b0cd2a105e82fe5aedf2fbeed2421d93bc575374620e1d0bc5e0c3dc3c6d8738"
