0ab6ff5b96a4a73a1a7c4aeb4019cb4ed857abe3
FX Coudert <fxcoudert@gmail.com>
2022-11-19 22:56:33 +0000

    bower: update 1.8.14 bottle.

--- Formula/bower.rb
+++ Formula/bower.rb
@@ -11,6 +11,7 @@ class Bower < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4f83f0b7576c438668ad19edcfab2606901bcd5f9092cf62c45b70d8b4778235"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4f83f0b7576c438668ad19edcfab2606901bcd5f9092cf62c45b70d8b4778235"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4f83f0b7576c438668ad19edcfab2606901bcd5f9092cf62c45b70d8b4778235"
+    sha256 cellar: :any_skip_relocation, ventura:        "f155eb229286bef7f21e55ada513c525913b38cc8db1d86a3b47fc7ee9a1f1fe"
     sha256 cellar: :any_skip_relocation, monterey:       "f155eb229286bef7f21e55ada513c525913b38cc8db1d86a3b47fc7ee9a1f1fe"
     sha256 cellar: :any_skip_relocation, big_sur:        "f155eb229286bef7f21e55ada513c525913b38cc8db1d86a3b47fc7ee9a1f1fe"
     sha256 cellar: :any_skip_relocation, catalina:       "f155eb229286bef7f21e55ada513c525913b38cc8db1d86a3b47fc7ee9a1f1fe"
