460eef4031a8c630e8b2d75444f682f4da70d52d
Bo Anderson <mail@boanderson.me>
2022-11-19 18:07:37 +0000

    diskonaut: update 0.11.0 bottle.

--- Formula/diskonaut.rb
+++ Formula/diskonaut.rb
@@ -12,6 +12,7 @@ class Diskonaut < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "27276ff079b4e62db5a6bd19b0e76d302c535ba6e367e760350a5168b15e35d2"
     sha256 cellar: :any_skip_relocation, monterey:       "ca2ead79bbdce41e8b1a5b2ded6422c27a71e0c9ccc119d4b3eb6ca2697a5e8c"
     sha256 cellar: :any_skip_relocation, big_sur:        "4db6857d320a4d03ab6a2df8de8604d6918ffce4aa6b4f36951bddd94656aaa3"
+    sha256 cellar: :any_skip_relocation, catalina:       "8e6ed47ccdb395b2f461a1e12376ee463bbdca05e572fe2aa33dba49df81f3fd"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "af43639e44daac484d6b43f33400310a21abf9632c85c198d41aad2edd34a0bc"
   end
 
