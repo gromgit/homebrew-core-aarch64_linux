cd54c40e0572d77ee963859b94b36ff5baa341da
FX Coudert <fxcoudert@gmail.com>
2022-11-19 20:09:40 +0000

    xh: update 0.17.0 bottle.

--- Formula/xh.rb
+++ Formula/xh.rb
@@ -10,6 +10,7 @@ class Xh < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c132e2860dc243963d76084a6239efaa4e9b6223fe66fb585829fb8a91025d5a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3d61902a6895bbeae9b9c2a3dcb8d1c8d4b06dfaebe4944489560c0b849b742d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "479a24e7e0e1da17ea1748bbbbd06ac80f6c676df39ed4267bfe50ab370bd4ca"
+    sha256 cellar: :any_skip_relocation, ventura:        "fa9120582a5726dd651a67ba937c8be974a60f06223c253d1d387ccda1b8c10d"
     sha256 cellar: :any_skip_relocation, monterey:       "bb41f5acdc9aa1803405415cd9899db8f463c854e4b4cca2a7a8b3a4fe0d25aa"
     sha256 cellar: :any_skip_relocation, big_sur:        "b5c1ad60fca17aa9b6629cef945f00da342dd4d837c9e866ddaea5ac51e1b3b8"
     sha256 cellar: :any_skip_relocation, catalina:       "bfddbb660e997acea4a4185b75b00474e0cf30bbb1b3f7f6c287263f9ced8986"
