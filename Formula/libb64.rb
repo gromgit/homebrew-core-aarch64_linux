c762fa34832914a0f9120991befca3964c657b68
FX Coudert <fxcoudert@gmail.com>
2022-11-19 15:01:41 +0000

    libb64: update 1.2.1_1 bottle.

--- Formula/libb64.rb
+++ Formula/libb64.rb
@@ -9,6 +9,7 @@ class Libb64 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "899db1753787af2de66e4ac321d5d750190a8357576611d59644eaa9ed0f852f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "cf8f611b34a951b0c3930f8c3714e0f24aa4074c79a3cb483810b7325461f1df"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "acd1c54a87e5e659d5a2d907f650cd9a3d1f160403805d8d84f7bc620546df0b"
+    sha256 cellar: :any_skip_relocation, ventura:        "2c8d53e819863560658ef3a9ec30d5472430d00b93cb165d12f4e2c12eef12c6"
     sha256 cellar: :any_skip_relocation, monterey:       "3b2acebf1e9432ef07e0035bfc410fdb8530aace59513ad9fad36fe35a661880"
     sha256 cellar: :any_skip_relocation, big_sur:        "50dafb7c970bfd56ab7c88100df7876d1590c806295987a8268029eb87b7ca2b"
     sha256 cellar: :any_skip_relocation, catalina:       "4f357626774a02fae97f7668665a8e41c96b4cdf041a8b8ba658dbd5f8a86bd9"
