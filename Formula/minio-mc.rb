e6bd67d64ef6bfb0a61c2316c45e863d6a45d871
FX Coudert <fxcoudert@gmail.com>
2022-11-19 09:26:57 +0000

    minio-mc: update 20221117212039 bottle.

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -20,6 +20,7 @@ class MinioMc < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "adee0ce9be6c6fdc24dc54d592537eb0b9c66692828de77e201ca1703c8cdd08"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "eb991b8ee09f96d23fb2fb63b9c38212b8978a258f225ba768070db1876f6053"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5b074e81e0625e1c783e14a47625348ddbe5b9a260f73cc89e868aea7dfb6b69"
+    sha256 cellar: :any_skip_relocation, ventura:        "baaddcf50d88cbf6223d9ff3e55679110161053a30122ce3f363dcf3b0ed54fa"
     sha256 cellar: :any_skip_relocation, monterey:       "c713513802a8b54355969c70db15fefdab004682eebfdcb7159ae148b970e86a"
     sha256 cellar: :any_skip_relocation, big_sur:        "b2bf0a742a014b83d27b993fb365da5fda0adea8e82f2d50b5216a7fea365a06"
     sha256 cellar: :any_skip_relocation, catalina:       "925dd23378b5c49dfbe6752147d0a1d61b1f1e80e7fe01537be875f879194627"
