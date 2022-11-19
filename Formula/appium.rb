81b063bb7a2f58c74a975270b0affc0cb2c754e0
FX Coudert <fxcoudert@gmail.com>
2022-11-19 12:28:42 +0000

    appium: update 1.22.3 bottle.

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -12,6 +12,7 @@ class Appium < Formula
     sha256                               arm64_ventura:  "d58c9925235b962cf33f38d3d4511ece2914bc73f957f2f1f959b5012c0eb68c"
     sha256                               arm64_monterey: "5b71cfdfa29c2bc98785b485fc0b279753125d9af2e9bf9bbc5e4d7f8d9ce4f8"
     sha256                               arm64_big_sur:  "e2f7fdf6d4c72380fd43936143660911c408b98542d0e033b3a2cbb20d6e1fab"
+    sha256                               ventura:        "2a74b09965bbd8d1d52a23e6879d6f66e1841ef44570122c6518c323eb17e501"
     sha256                               monterey:       "854d9cf2275384e65781c8201af16f3a6ce0ddea25fb0f12d2999fc09438c018"
     sha256                               big_sur:        "698fe344433d98af374558688ea665921fb7d74f09c51ac61d5739fb0fcc467a"
     sha256                               catalina:       "9fd553fc9f5957b5cd8bc1c1892fcb9dfcff89571dfe8af04a70c752a0641a1f"
