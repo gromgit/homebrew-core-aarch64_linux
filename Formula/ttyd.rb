d2bd3e5b60d2e16354d466636ee3f73f5bed6fb2
FX Coudert <fxcoudert@gmail.com>
2022-11-19 22:24:56 +0000

    ttyd: update 1.7.1 bottle.

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -10,6 +10,7 @@ class Ttyd < Formula
     sha256 arm64_ventura:  "f6f251fdfc7a65889efe2b428ee6ccf9be62258d717a8e9cb9adb5fef7c0e93f"
     sha256 arm64_monterey: "2dcb7ed95b83914ae3c477e8272955224b51d34b906e6057fe3f667be66154fa"
     sha256 arm64_big_sur:  "01c674bc7a80ac4ffc7d91f3557bfd0056a36337285d90b3ccc4595b8e9c9a54"
+    sha256 ventura:        "801da0da28dad4fb164565159342d3fdba5884e3b34c8fa6ad89dc244c2ac1b5"
     sha256 monterey:       "59f8a5afd097792730fe54ee3d71f92ae58e1a0baa87fac4d2faf3d58591fee3"
     sha256 big_sur:        "07d576a2efe91de00f31fc47c13c7396abc2cac37ea6501833b669ee6824457f"
     sha256 catalina:       "436a5cd144aadb451d4f0ec657d2b878ba3c4d1e239b18eed8cd78519937bd83"
