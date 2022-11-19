77e382f1ecab7a0cce7f02444e5c5753e38fee67
Bo Anderson <mail@boanderson.me>
2022-11-19 17:47:47 +0000

    loc: update 0.4.1 bottle.

--- Formula/loc.rb
+++ Formula/loc.rb
@@ -6,16 +6,13 @@ class Loc < Formula
   license "MIT"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3828b3b1e1eb3ab549832c099ca589f3d8cc7f766ca04fba1778ce8747b5411"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dacc9fbfd5e9775dddeb1c0a29227f3e657375bf8d0c73f9bf7007aed2e9ff7d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39a68ff6b68af077cf2c2c3c7fedcb534dcc1220bd4cc3e64e7443efdf599bd9"
-    sha256 cellar: :any_skip_relocation, monterey:       "0da57b75ab0b25d0d78e0f88a48c30d884c91303c1353972468a8042867da39c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "499912987d82682d5bfebd638d9a116c0d55f71284bf543067abb086bc9c55a3"
-    sha256 cellar: :any_skip_relocation, catalina:       "2a8ac9341661cefa1221418aa2cb5cdd5207108ade6803ab5af34ca01d0aef13"
-    sha256 cellar: :any_skip_relocation, mojave:         "008db46fed420d7ec698d46e059a4913368af4d8f0b2f4f8502a39ee392b830d"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "f4241a70db520e24c587649bf7b8db0f743afaf00b01ebee5934bee7e88ae42e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62f8a3305c22f0287483e3c1acbbd4110f4eeec3fc846dfeadbc0420bb51b6b6"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "62bf06390749dffb96ea6219f9d578dcf9b95ef774a5a6b1eaa4f8bc3dda143b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "668081ed1d522d9eb49322dac5986bad986db29732cbe42bea12e4276044e37a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4f44244fb86c35bd48654f2a517ecb8550a8d291baf8919f602fd88f50b15677"
+    sha256 cellar: :any_skip_relocation, monterey:       "8d372339b2ef67c179fcf00d33e167715dd41c729b7a50fd6026e32a5529338b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b3fa9372c95f88ca42e456d2f9c451cca28c4e894afd9257fc4069215b74d8a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f463ece47552bb4e58ffe4c4ff6e62dae3b57fb3b9a1e3fad3ea728b07bc660f"
   end
 
   depends_on "rust" => :build
