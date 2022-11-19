c18ed4d8ca7455145e24ecc83d3c214c9d4c3a8d
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-11-19 14:32:22 +0100

    wdfs: depend on linux

--- Formula/wdfs.rb
+++ Formula/wdfs.rb
@@ -7,41 +7,19 @@ class Wdfs < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 catalina:     "a00329ad59065dc12983272eb1da0e861aa73cbfa8b2edc69393a5a2eba4e49f"
-    sha256 cellar: :any,                 mojave:       "edf41371511f947ef47c0ad7575cffb5831687c975f000f51e538133ec42563f"
-    sha256 cellar: :any,                 high_sierra:  "f2f3ad809ea9104bb5fd49b4f903b0465707baf76be3329422ea34aeed8bacb4"
-    sha256 cellar: :any,                 sierra:       "7aab5f9c3d807f73dfe9df437a15806b74bc5a76cd3cd13e961ea781c7fa32fb"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "795f6e2939f798aeea462d891b102cb80e32c5abb3586f8d57841843d2120f91"
   end
 
   depends_on "pkg-config" => :build
   depends_on "glib"
+  depends_on "libfuse@2"
+  depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "neon"
-
-  on_macos do
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse@2"
-  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 
   test do
