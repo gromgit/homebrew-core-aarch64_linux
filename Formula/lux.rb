cb1e44b9317e85f283f41b455af6f069e161d40a
FX Coudert <fxcoudert@gmail.com>
2022-11-19 20:52:43 +0000

    lux: update 0.16.0 bottle.

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -10,6 +10,7 @@ class Lux < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4eab4337efa2def339ada5c7e0d73329e1207be17b7b50be30914c3a9469f4c7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "573e00b2cbdd6c13a681ee869e11836858463a27c777f60857ac369443d89117"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a52447f547ad8c46ae28dfad776ae1bf767c8bcf1d410a917bff27a1bc962be8"
+    sha256 cellar: :any_skip_relocation, ventura:        "2bfccf2d9c186bb201c7ab7c7dbeaf3bb96e11a05e5f505dc0b890502177bf9c"
     sha256 cellar: :any_skip_relocation, monterey:       "fe590137419bb55a098b0d41cb52fdf5b51ff07d60985a5fed9d54989db5742e"
     sha256 cellar: :any_skip_relocation, big_sur:        "4c5106a821c50c9085f74fb8b148e0ccbbcc18652780d24124143032adb54b01"
     sha256 cellar: :any_skip_relocation, catalina:       "76e78832943bc4291d61de04f0dc362517e22d795e0438e9a4c9c0567fe00b44"
