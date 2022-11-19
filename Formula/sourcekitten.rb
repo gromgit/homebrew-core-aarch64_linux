38508a7040dcc796ea971e51804f5ca1a635a2f1
FX Coudert <fxcoudert@gmail.com>
2022-11-19 21:37:25 +0000

    sourcekitten: update 0.33.0 bottle.

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -11,6 +11,7 @@ class Sourcekitten < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "8bb50ffd849807cc1ada1c1de04634ffdfd4b03feb758929c89fc7af6c061b59"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2a86efc5b38dd19589812df8835fe060b67416a1452dfd54fa030f9b42e92959"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc94480ea441f3effc6f2f4137d74f1548f95fc8650f2ce58d0dae16baee3760"
+    sha256 cellar: :any_skip_relocation, ventura:        "8b51c6334050c899a4d9ba22c50447a282370c99874094b78c35d5caffb2a390"
     sha256 cellar: :any_skip_relocation, monterey:       "c1d8fc2ab846cb417aeece001a6f177b363c2109fb411cc312f54b32245fc974"
     sha256 cellar: :any_skip_relocation, big_sur:        "7a3e6807a9f3625cf0a887cbbd9a7f3380960393aeadf698798144a373757ba8"
   end
