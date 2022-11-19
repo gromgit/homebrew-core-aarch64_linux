47142dfedc1a872b64a039dccdc6a0dab5315a53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-19 11:04:53 +0000

    md5sha1sum: update 0.9.5_1 bottle.

--- Formula/md5sha1sum.rb
+++ Formula/md5sha1sum.rb
@@ -12,17 +12,14 @@ class Md5sha1sum < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "335ce511d7c7a90f7e98a8d4057e677ea24620fe9444717c3eaa64f8d12f40a1"
-    sha256 cellar: :any,                 arm64_monterey: "975c34811713cfbce39d2d7627b62e1a026b4e21dec59f7571fb813b31a9d483"
-    sha256 cellar: :any,                 arm64_big_sur:  "6d1aa8a6c57e734885363c8c2dec012e52716332706a7695f5d0994e75957fdf"
-    sha256 cellar: :any,                 ventura:        "9fcdea3033963d43f1aefc9344616a3c94b91e16ae1b4b43e668b0abe64025b3"
-    sha256 cellar: :any,                 monterey:       "b06863261442b3ca6bce142c3bd0da8333568324bdc3db17cd739114a9001fb7"
-    sha256 cellar: :any,                 big_sur:        "c3ce2a7048d5e035493c2f637249da99f726109a9a643498576441c4c9ec7d58"
-    sha256 cellar: :any,                 catalina:       "d498b282ccd1e70d8676184b3eda51eea4e99baacb92e6d69df2fd05a98d511f"
-    sha256 cellar: :any,                 mojave:         "b1dc1ded1df513c24b7eab764707b088f42661c2cf53395e42ee4e03c245ac5d"
-    sha256 cellar: :any,                 high_sierra:    "7ed564b5da0f1adf33c0242bffcd4e456d4e46540b578d3cdb810a9f6a28474b"
-    sha256 cellar: :any,                 sierra:         "66ff4c578f7eff04b561192dd7789e013714caed61dac322df99f5652790abc7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c25d9290972bc2bcec287eb11b91349f3dd0e5db8ad3a24b35874f7715682cfa"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "1055a4e7c14927621a28916d8847a9d07cd7c2fa3a0b7c5b9a087aa67350fbfb"
+    sha256 cellar: :any,                 arm64_monterey: "6c1df5b7a603f00daa2d138d4e71d2f4e61316fb3e5de9f1dd7181a5d197feab"
+    sha256 cellar: :any,                 arm64_big_sur:  "8a6b0641375cbc512918fd3b0b79257f542668b5237e2a360a4abecde564bc95"
+    sha256 cellar: :any,                 monterey:       "db59823c590542e30656a7cc7f2379826352efd07a75e3919f359d33ffed2dd8"
+    sha256 cellar: :any,                 big_sur:        "924a214bdfed9ed8f7d9d581bb5717ec6cfe523e0a7f145b3e63075eba244bd6"
+    sha256 cellar: :any,                 catalina:       "61cea3dd8aa7dc270e3f5265316dcc73ccafa4ab7caf59b48b8fa5e9208d0d9a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0055fc19d2c8072e2e576b7df8de777126f2b5ab2c1269ff224476f8aaf9d65b"
   end
 
   depends_on "openssl@3"
