b82da0a37021c86aba3903a38c9fee40eff0a2d0
FX Coudert <fxcoudert@gmail.com>
2022-11-19 02:20:03 +0000

    trash-cli: update 0.22.10.20 bottle.

--- Formula/trash-cli.rb
+++ Formula/trash-cli.rb
@@ -13,6 +13,7 @@ class TrashCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "783f8a90b57013c3d805000e3f7e2966b61037980655ef193bc5a64453fe2b42"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0ae573265f69e2ca674d73a261ff27451108938c8cef999c071d7d1f704eccb1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e0297996b7296604a3999834bf9d199ab645bcce73b0429446c4dbb57b25cda"
+    sha256 cellar: :any_skip_relocation, ventura:        "e4ad8b00754a30f6f1bdbec8b194d8c7efbcd369b3dafd651175b528813143ff"
     sha256 cellar: :any_skip_relocation, monterey:       "455b0229a0f8e1ea0f2daf18bc39bd52e76036d59df4873e888ad251e23b9973"
     sha256 cellar: :any_skip_relocation, big_sur:        "9472fa4fe5d5b04e1d856e39f159f3a38b220ad09e96c7eb22d2dd15f979f0af"
     sha256 cellar: :any_skip_relocation, catalina:       "8451101a8557e07b2474b0db71f76663bb7edc0e9626c3b1e92cd94bcfa0da5e"
