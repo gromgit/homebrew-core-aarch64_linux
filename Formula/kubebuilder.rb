426d4e116a37d10be31db5781314980cc7aca30d
FX Coudert <fxcoudert@gmail.com>
2022-11-19 22:25:26 +0000

    kubebuilder: update 3.7.0 bottle.

--- Formula/kubebuilder.rb
+++ Formula/kubebuilder.rb
@@ -11,6 +11,7 @@ class Kubebuilder < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1d801c6c5b5fdf7b6e17fc390385c8b6a2087b99b6cc23bbfae7749fbcc9ddae"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ed28191bacb910531974baced46d2a50a817db3aa2975d7758be7826c735f51d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9e863ba2dec79a6c87a9f93224eb7699fe949d4efe032795a6ba63235cc1c149"
+    sha256 cellar: :any_skip_relocation, ventura:        "f670be76db2b533c67b52408ff5c8f4ee632378ae54c89bcaf9a85ddb754d4f5"
     sha256 cellar: :any_skip_relocation, monterey:       "ca43e24cbbcbe43df28bb4720e9e3019b72d50adf43d56a208eb2c1dc26fb736"
     sha256 cellar: :any_skip_relocation, big_sur:        "abb89e465db3532431a36cbe3f8f4ce9a689a7b879adcf3adeb22f94c9b603b4"
     sha256 cellar: :any_skip_relocation, catalina:       "e3c813ad5736ad5651d0e4e46f862d5121e4637a6732aeefed227d7daa0e8130"
