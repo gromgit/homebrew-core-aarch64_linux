e254633dde21355c650d37bd96e916565449f83f
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-11-19 14:32:22 +0100

    simple-mtpfs: depend on linux

--- Formula/simple-mtpfs.rb
+++ Formula/simple-mtpfs.rb
@@ -14,15 +14,9 @@ class SimpleMtpfs < Formula
   depends_on "autoconf-archive" => :build # required for AX_CXX_COMPILE_STDCXX_17
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
+  depends_on "libfuse@2"
   depends_on "libmtp"
-
-  on_macos do
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse@2"
-  end
+  depends_on :linux # on macOS, requires closed-source macFUSE
 
   fails_with gcc: "5"
 
@@ -33,18 +27,6 @@ class SimpleMtpfs < Formula
       "LDFLAGS=-L/usr/local/include/osxfuse"
     system "make"
     system "make", "install"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 
   test do
