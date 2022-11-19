1d4ced9dfd548180a0de2d317da94d3ef1b72fd1
FX Coudert <fxcoudert@gmail.com>
2022-11-19 02:06:40 +0000

    okteto: update 2.9.0 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -10,6 +10,7 @@ class Okteto < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "88ea3e2781da070415e7a5551bba9a469e28da3b96a1a6acfe3d27c12af45dd4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2b042a09d54a1851c8f722500a6e21c8d83d198e909e989428fa811337a80d12"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b2c2450f097107c815f3f77b5eaa5790ce20a64737d4159311034e17b2ae6cf"
+    sha256 cellar: :any_skip_relocation, ventura:        "ceb77aae6879806bbb89f28781cb6415f3ece3b83a6579f39efa7944460c7358"
     sha256 cellar: :any_skip_relocation, monterey:       "9093eb59f549f9d428a4ce5150bedecfc3412477bc561c215998bc9a988ae387"
     sha256 cellar: :any_skip_relocation, big_sur:        "7f5097ddc9a352de6f092a486817bc9e94da33fb2623ac750ac30efaddde9144"
     sha256 cellar: :any_skip_relocation, catalina:       "425fdc233f0fd39141894870034c73f487ba2ca3bf8adc643deafcf8af1eff6f"
