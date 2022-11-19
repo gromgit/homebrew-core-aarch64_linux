602ef7c6e7237ad7837beca884470be159aff7f9
FX Coudert <fxcoudert@gmail.com>
2022-11-19 01:57:02 +0000

    libzzip: update 0.13.72_1 bottle.

--- Formula/libzzip.rb
+++ Formula/libzzip.rb
@@ -11,6 +11,7 @@ class Libzzip < Formula
     sha256 cellar: :any,                 arm64_ventura:  "9850e8c458ac96d12f9b48f1de1e3b5697533ae1be8727210737b42d547c18cb"
     sha256 cellar: :any,                 arm64_monterey: "cf74798703189f3c2ecd72118e9e8693379ec1d9a1936ccc9be5cf333cce2d44"
     sha256 cellar: :any,                 arm64_big_sur:  "a7c81a822e4814e69bc27ee09c6fc7e2bbafbaacdb0337b90406b3ac7d627645"
+    sha256 cellar: :any,                 ventura:        "cfd0f7bdbc338d26bcc7ffdd6bde1d518e23b270d08a28cb2d904fcd36b9d3c0"
     sha256 cellar: :any,                 monterey:       "ecf9e88530bcb24d5f5b531834d3f45a3d6c424e93ec24132ac062114a6d04f7"
     sha256 cellar: :any,                 big_sur:        "1a3322eea48b54ad64c93e36b3c61cdf3175b96d01b39a35f6a70cb9c079b92d"
     sha256 cellar: :any,                 catalina:       "8c6c6c3bf2febfc90d54933e232cc64c5adbb3a8afcf7885725190c73ee5d350"
