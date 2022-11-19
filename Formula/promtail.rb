49a206f9092ea52d304b6153220d861d594af066
FX Coudert <fxcoudert@gmail.com>
2022-11-19 23:26:40 +0000

    promtail: update 2.7.0 bottle.

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -14,6 +14,7 @@ class Promtail < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "72405beaf8da0920a7ec457399c05a429500d39a16c9eb7848834b1a6d9db232"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "308f7a625a0030b41da73107d014bcb6fa06eb0fc622acd5b59516dcb7eba624"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fff5ff3352d377ae1c5e060e4c8de70cc99550ca2c3760bc8d8ce4b386e484c7"
+    sha256 cellar: :any_skip_relocation, ventura:        "2fe783022f1142757f6b31e576101758ad7a562e776581a7a8174cd88da622d7"
     sha256 cellar: :any_skip_relocation, monterey:       "09d867a7c80ad831641baca63427f52ddbf5099c518d27f82fe4e30143cd5c97"
     sha256 cellar: :any_skip_relocation, big_sur:        "64c82d755c70ecd27f4ffa092ea2871da8b3dacfad83c14b2d8327c59c5b643b"
     sha256 cellar: :any_skip_relocation, catalina:       "02ce2d299cf73dd8f57a0cd7e8ca7ab8f65a082adbbb0bec381b5575ef0a3c84"
