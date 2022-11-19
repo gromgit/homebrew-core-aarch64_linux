ebdbbfff74471310ba6f2144130cc3cb6a1db95e
FX Coudert <fxcoudert@gmail.com>
2022-11-19 12:42:59 +0000

    jrnl: update 3.3 bottle.

--- Formula/jrnl.rb
+++ Formula/jrnl.rb
@@ -12,6 +12,7 @@ class Jrnl < Formula
     sha256 cellar: :any,                 arm64_ventura:  "bba5ba3e04a49fce80938f73ad09b8ed625b8f929a4f609abebd76b30dafb9f5"
     sha256 cellar: :any,                 arm64_monterey: "211d8a37a7d62f69376335277a15397b76b4e56ba288f9a839730b24e54758f2"
     sha256 cellar: :any,                 arm64_big_sur:  "593c139d53cdd133756d9b0d507b1d17dc86d149fae9e5c1f10b8aa021d6bcf9"
+    sha256 cellar: :any,                 ventura:        "79352164b2db359e2c5e5579c20556d97673e8bed448a6c27e8018bb2b32630d"
     sha256 cellar: :any,                 monterey:       "6ad14f522b9cd577952e143bbdf43f68b18575b72ae059593c21722a63be607b"
     sha256 cellar: :any,                 big_sur:        "65800b89bca17f70f2adbfe4fd8308dbaaeb520aeea7cb5227b77c3bed5ba8c1"
     sha256 cellar: :any,                 catalina:       "b4f734e3d8979b69335942ff2adede2d0cb6ab5f7aaf22ae047511946f53b8f2"
