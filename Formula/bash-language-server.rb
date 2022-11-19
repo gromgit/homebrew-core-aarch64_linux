a34e0d0d76ef77c2f3c8f85584b09a3c9b1309fe
Rui Chen <rui@chenrui.dev>
2022-11-19 02:55:30 +0000

    bash-language-server: update 3.1.1 bottle.

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -11,6 +11,7 @@ class BashLanguageServer < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28c83c200121ede2806a8230c81711664a20409a0d94d2db1de311f807601998"
+    sha256 cellar: :any_skip_relocation, ventura:        "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
     sha256 cellar: :any_skip_relocation, monterey:       "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
     sha256 cellar: :any_skip_relocation, big_sur:        "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
     sha256 cellar: :any_skip_relocation, catalina:       "e4c388dfc9d7f6d93acaa19f24b40dac7c2ad40c90367250742f95bb9c0bf5c3"
