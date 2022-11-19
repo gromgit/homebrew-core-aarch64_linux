ae2118d8a16ef4ee2b67627cddc04629508757ed
FX Coudert <fxcoudert@gmail.com>
2022-11-19 20:14:10 +0000

    esbuild: update 0.15.14 bottle.

--- Formula/esbuild.rb
+++ Formula/esbuild.rb
@@ -11,6 +11,7 @@ class Esbuild < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "fe1200a59567f087c8672b7d04dbfe425abcb5c9e3106afa28acbdcdf2f2d756"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fe1200a59567f087c8672b7d04dbfe425abcb5c9e3106afa28acbdcdf2f2d756"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fe1200a59567f087c8672b7d04dbfe425abcb5c9e3106afa28acbdcdf2f2d756"
+    sha256 cellar: :any_skip_relocation, ventura:        "9db20d4c6599e0935543da4898a6367be7eebc18bf4031f539a8e2b26314521f"
     sha256 cellar: :any_skip_relocation, monterey:       "9db20d4c6599e0935543da4898a6367be7eebc18bf4031f539a8e2b26314521f"
     sha256 cellar: :any_skip_relocation, big_sur:        "9db20d4c6599e0935543da4898a6367be7eebc18bf4031f539a8e2b26314521f"
     sha256 cellar: :any_skip_relocation, catalina:       "9db20d4c6599e0935543da4898a6367be7eebc18bf4031f539a8e2b26314521f"
