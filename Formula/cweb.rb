e13436b35ecdc6753a40c6f28bb55925cbf9af13
FX Coudert <fxcoudert@gmail.com>
2022-11-19 15:21:37 +0000

    cweb: update 4.8.1 bottle.

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -15,6 +15,7 @@ class Cweb < Formula
     sha256                               arm64_ventura:  "8af953f8a3f31d2c2b6cf155de4610ba7f7a4ad61e0b59bdf9a7ec190e4cd86e"
     sha256                               arm64_monterey: "5de3449dadd66c5759b4aa073884336c9c935b3a4481d4355303b050897c3948"
     sha256                               arm64_big_sur:  "1ae7da8bf50cadb46c67c6e3ece21e992942f354dba9e9ba4dd940b4150db086"
+    sha256                               ventura:        "b897a987d49e52c0f3746dd301400df7b39588cac36a1c0d2fa3557e0e40f2b7"
     sha256                               monterey:       "3f29dcb1e90489af30f71b1d2080f1da44aa54523c03955a3d8390867d8fbb10"
     sha256                               big_sur:        "5b04f2fc775e7997bd22d4fdff71bdd5d4e8776de60f7c7a79b68280b3b9df09"
     sha256                               catalina:       "abb227be400bc74150619772ced22aa90bb2c1721b1ceca347f83259d6da6904"
