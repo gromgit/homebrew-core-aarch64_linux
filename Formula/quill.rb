67d6f72303811af028d18046cdd41550384f01f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-19 05:04:44 +0000

    quill: update 2.3.4 bottle.

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -7,13 +7,13 @@ class Quill < Formula
   head "https://github.com/odygrd/quill.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "62f32deff3c410256dd945d49414f1d26189e8c8d30a4d379add0b867efba979"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "88ec791767d1d19fd99d0fe80ab3d408205c120e31b5004b9e3cb6b311fa14f3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8772c6e0bd2d1945731c3076d09e7f608c2cc1dc78e033d0962ed3d0f700a13"
-    sha256 cellar: :any_skip_relocation, monterey:       "45d10599b983794ce702d1ae942e19c7ea7260c907b2c9e6c0c6cfa6e0ebdeaf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d0e50faf76c14e943e3b2bec27944e882247cce6324637ade8fd00a7f1ff65c3"
-    sha256 cellar: :any_skip_relocation, catalina:       "b01162cde5b3c0c763ab3d38fa606125fc507180f90787da2ebe0ed1745e70a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba0654ea046c69abca079a83826ee382ced39952ee0f8606e1542d9a788316b4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "44f81cd0d1c903a303c84413dea086c7e9bccec2dba13f14d9f4a0f4cf26a8ba"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6afc375f833a37284a9959389afa325c8b0379464f1864fa812b0b5e06edd58c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63362f8768f9e58184d96dd291306788f37cd355876dea0052eb14af07f36d24"
+    sha256 cellar: :any_skip_relocation, monterey:       "4f38bf807f3c0135d1f18b4d39207d56e1395007b1d5f1ac64ba00289d0eb812"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4903830c3df0986e8b6dbcf74a298f651e1816aea0a5bfea6cc33b574d8241f4"
+    sha256 cellar: :any_skip_relocation, catalina:       "64796367dae59feb306942d740a68927503db4f6014e4e5f78c27246498a0960"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "278136cb6abf20a0d69ee6d11b8a5d6d80528b2d051d3c50ab0afb1447a7cc5b"
   end
 
   depends_on "cmake" => :build
