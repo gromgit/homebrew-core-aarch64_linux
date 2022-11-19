b66acb9caea2c238fa4036aaf9e952c56bf26992
FX Coudert <fxcoudert@gmail.com>
2022-11-19 21:04:37 +0000

    gitui: update 0.21.0 bottle.

--- Formula/gitui.rb
+++ Formula/gitui.rb
@@ -10,6 +10,7 @@ class Gitui < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "94ec4ffddc9ff28779be048ceeacec1f126d7f9a9bb5d9f03c00ad755dd57853"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "12d9e243bd1aca529db25c40bffe2b5da5aa6c3be6427d4d614babd7e48915ca"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7a306856b92c55080ec447be66c6a03e36428154d0e7b63922d60ba6c3f8624c"
+    sha256 cellar: :any_skip_relocation, ventura:        "e3357e03bff5fe044e648e785073ca1ecd8f836c68df0f6d0df63becb612acb9"
     sha256 cellar: :any_skip_relocation, monterey:       "66c45e185fc3c3494b1c965bd4373a2ea34996b26fc8e839548eccca81f18271"
     sha256 cellar: :any_skip_relocation, big_sur:        "ebe75d0d97989cd3f3ae83cda32cc545bed4f2f9f798dff2039737e4e3ec6cdb"
     sha256 cellar: :any_skip_relocation, catalina:       "d0641624fb4122d90fdac164f0813ad83667c8db06759d9bc814d16ea2519c3b"
