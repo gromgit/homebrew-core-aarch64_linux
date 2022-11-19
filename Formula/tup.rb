4b08c3c66d8417c0e0e471800b1392c91bf65540
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-11-19 14:32:22 +0100

    tup: depend on linux

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -11,14 +11,8 @@ class Tup < Formula
   end
 
   depends_on "pkg-config" => :build
-
-  on_macos do
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse"
-  end
+  depends_on "libfuse"
+  depends_on :linux # on macOS, requires closed-source macFUSE
 
   def install
     ENV["TUP_LABEL"] = version
@@ -27,18 +21,6 @@ class Tup < Formula
     man1.install "tup.1"
     doc.install (buildpath/"docs").children
     pkgshare.install "contrib/syntax"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 
   test do
