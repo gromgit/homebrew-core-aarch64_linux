059825212388132423450b95f47705bbddfd4b8c
Rui Chen <rui@chenrui.dev>
2022-11-19 02:57:58 +0000

    binaryen: update 111 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -10,6 +10,7 @@ class Binaryen < Formula
     sha256 cellar: :any,                 arm64_ventura:  "45807d07e3d16808a911aa0f944c9c8c7a9b0e53990e84f9e37891ea3683b975"
     sha256 cellar: :any,                 arm64_monterey: "4208e154c9082621e5062866398cdb0ef47eb53bcc73baf224fc48abdbf998d1"
     sha256 cellar: :any,                 arm64_big_sur:  "278f526822dd520d16ada1755cee0b99720b92f8f954c54d20c208f0153e88d4"
+    sha256 cellar: :any,                 ventura:        "16d90b3aa9fa901835def9892268d4ed827a581a777740de4c78c899d95f47db"
     sha256 cellar: :any,                 monterey:       "ef5bfee117bf529d6b2825387f426074cf0e1a17b89eb5d80ca57c59a63d0ba8"
     sha256 cellar: :any,                 big_sur:        "01c7c7bbb9508cf73236b073b92b750f88bd3f3d8fc0367e7bdbb30f6d220550"
     sha256 cellar: :any,                 catalina:       "3611d115f0bc997f55770c33eb7228aa439ff0b0ddf87c1fcd43c1b63deb4eac"
