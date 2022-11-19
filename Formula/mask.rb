863f462155fed98cf90c209bd31c8d76c4367d0f
Bo Anderson <mail@boanderson.me>
2022-11-19 17:54:42 +0000

    mask: update 0.11.2 bottle.

--- Formula/mask.rb
+++ Formula/mask.rb
@@ -6,13 +6,13 @@ class Mask < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0f4433753dc9209594c5d91e0e93ddddea5d592e7096963b4da5c6e07e4fed77"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2c80321512808c157aa739263f16ac6497a0488c587b84abeb5690831a0ff8c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "43388f2bac72ba6c337e96acb1a9257177e97eb807bec15d1f62106a402c5ba0"
-    sha256 cellar: :any_skip_relocation, monterey:       "07d3bab6e155d245ac877c361093f3c109ae86b510daa4d9ede2393883b5219d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1d05edf78de9bba766f6283b2c9746cd356ffed037b35b4fc20e55e8660e6c18"
-    sha256 cellar: :any_skip_relocation, catalina:       "8d394f2f60f398dd483c33b30213f75525ea7e7e22cfd41ba7b0b027e835f42d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5edff1c56e8cf96f9b89323cf02883f41a9048b9c05867c900dc177268f35a14"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c2e0cba1600c3a404b0c65848e5e4ba72a82336df51fdb74c56b9ac7626cc8b5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f086816201c09f876e6e3465ad7145862151207f3b60a27681d3fada2d3403e1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c99cc0ec74a9a962a0597978bf229c945b39bb1c3d3273674a97cf873540ce7"
+    sha256 cellar: :any_skip_relocation, monterey:       "b36c0c40193b19d767e99abc4cf4a6c1e81498471134ef36360e43b8882b702e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "85fa85c696b792cad05a2143c49b2bcedca1eb1e45bcb51a41822067b264a30e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cbee75fc2c53fa298f600841235a70401a2b8a61a64fd9f9ee5ca70692d29efc"
   end
 
   depends_on "rust" => :build
