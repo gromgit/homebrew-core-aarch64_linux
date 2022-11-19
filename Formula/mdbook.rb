dfd58881da02f505152f41e90be8c60ad9afdb54
Bo Anderson <mail@boanderson.me>
2022-11-19 18:07:50 +0000

    mdbook: update 0.4.21 bottle.

--- Formula/mdbook.rb
+++ Formula/mdbook.rb
@@ -7,13 +7,13 @@ class Mdbook < Formula
   head "https://github.com/rust-lang/mdBook.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6dd58842218f85458d80ba2f3f0c3b69e253d7f28fb06d18af79aeb22e6d75bb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f27390977f9b377fd9845cfb1585f6d7d2795c4f208a360e8697fb41ac47a20c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa2468366fc57c90d36438e2cc74ef764090e3f1f399a0fc47e6fee47e750fa1"
-    sha256 cellar: :any_skip_relocation, monterey:       "0666ad3aaab9a3513d99eff2a7c9ad7d028028eee5c21313c603deb8c672b64c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ec22fa1e5722c6299e35db49560ec40d3a7cea88393a50bd97568a9c0900dd0d"
-    sha256 cellar: :any_skip_relocation, catalina:       "c987b7c665ffb6a9e63e1eddbd8459103025dd656fc8cfee90e6ecbaf2728c59"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea9b6e2fc664b8153425d8312ad395e9a05773f661ce79e794bd0f13dc4059b3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "465d2ec3622817ef9adc55f645e41b58288872011457f97fa79220aa17ba806e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3dcd18d5e2a6e9d880e706ec4a3ca54b4b5409a07adc908d1bfd6440dd0c7cfd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1605e5ba8de3b0336588c1396ff18673bb342317d09b69617a4ccf53136fb25e"
+    sha256 cellar: :any_skip_relocation, monterey:       "7d4b2206454179f81a48ad308d3265b0d20ca05ddd0b3c6bfe219cdf03f75b5d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "075fd9fbe39fa53d5a77b89012b08be4dddf5806eb7fb52b1a5d18a2e716ee8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57c0b2a679e4a604f8830bbeab685ef1a2e425958cb12b145d90dbb19705a16c"
   end
 
   depends_on "rust" => :build
