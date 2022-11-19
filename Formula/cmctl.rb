607dab48f2fd0c23c04132ff606d962479ec1a02
Porkepix <porkepix@gmail.com>
2022-11-19 02:00:45 +0100

    cmctl 1.10.1
    Closes #116118.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cmctl.rb
+++ Formula/cmctl.rb
@@ -2,8 +2,8 @@ class Cmctl < Formula
   desc "Command-line tool to manage cert-manager"
   homepage "https://cert-manager.io"
   url "https://github.com/cert-manager/cert-manager.git",
-      tag:      "v1.10.0",
-      revision: "da3265115bfd8be5780801cc6105fa857ef71965"
+      tag:      "v1.10.1",
+      revision: "a96bae172ddb1fcd4b57f1859ab9d1a9e94f7451"
   license "Apache-2.0"
   head "https://github.com/cert-manager/cert-manager.git", branch: "master"
 
