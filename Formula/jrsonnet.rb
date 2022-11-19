64f5db4907767062647a8dd9746c3d08ffedde19
Bo Anderson <mail@boanderson.me>
2022-11-19 19:00:52 +0000

    jrsonnet: update 0.4.2 bottle.

--- Formula/jrsonnet.rb
+++ Formula/jrsonnet.rb
@@ -18,6 +18,7 @@ class Jrsonnet < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66be41438ed1da22c922c59d3da10a1852b89491898881c2bcd25c114abd4852"
     sha256 cellar: :any_skip_relocation, monterey:       "89080991d6ec832d7fa2d868a4323e08921291a01100797a7343790b3ab30088"
     sha256 cellar: :any_skip_relocation, big_sur:        "8e96db079868f110cd8b9fc4b29a1340bd5539ab1740928d8c2a8d8a7c34b25e"
+    sha256 cellar: :any_skip_relocation, catalina:       "fbbaf136a231569a619722188c22fed49d586431ee1a1515e9cfc84bac0eafd2"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a04748fc43150da3210809034f82864313d7706869ec78265462a22cf2a4812"
   end
 
