c51338d9b6cb8a619014a561ebe2e552bae46840
FX Coudert <fxcoudert@gmail.com>
2022-11-19 15:43:56 +0000

    tcsh: update 6.24.01 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -15,6 +15,7 @@ class Tcsh < Formula
     sha256 arm64_ventura:  "bd18f63708b7c72690f12f1b05c862e64d6c9245308f207fa20067da1563c14e"
     sha256 arm64_monterey: "0f79c59227eb84c2504946ef28782ed98f7c1162af5b3edb95252e9cb860d9f4"
     sha256 arm64_big_sur:  "0fc3bc6e356efc0b17d8a0ca32dd6c2ea69c85cef66c0c33d17a0a02b6ab80ef"
+    sha256 ventura:        "41de1c3722e6f713d6a7a21c823fab3795e862b483338930dff7dba0035f4b6d"
     sha256 monterey:       "228d4cecd20d1a228652c53fb0116014341823d1b40528076e1a4a3c9c592a19"
     sha256 big_sur:        "3cd1f6b636decc9ccb96c4185b9ec1ed30c43922cf7088f79c93dc082dfc345c"
     sha256 catalina:       "9966998dd43efb22997460ae8d83b1c3e9f3488dde47afb81c46fcd64f974bec"
