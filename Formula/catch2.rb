2fc29c06aa4c5b010f373382f695f0cf1d5e09d4
FX Coudert <fxcoudert@gmail.com>
2022-11-19 00:51:10 +0000

    catch2: update 3.2.0 bottle.

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -9,6 +9,7 @@ class Catch2 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "8bf9e27b974e7d708bbb6735406b222cf0226c620c546a83d69976adea4720b7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fdb910cbbfff1e17b8f50aece313f14f51723309a2ba5dac2705922482df0b3b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c0594e9eedd28cee43dbd68fb094a64c64cf4c62ec152cb90352fd38e0a2a1a2"
+    sha256 cellar: :any_skip_relocation, ventura:        "e5b1de3464dae23d480a05c7056e4ee2a9bf8676292f448966cd396132fc91f0"
     sha256 cellar: :any_skip_relocation, monterey:       "bd0080d2f8dc23961dc19698e2b86ed02d72ad8084649e66433221c7c66a7d6c"
     sha256 cellar: :any_skip_relocation, big_sur:        "fd5f9a443e018a9af01ad1bb4971800eee26274cdfe3f3d1bdd2686c4f92a07d"
     sha256 cellar: :any_skip_relocation, catalina:       "4fe943290a551a890806508662dab78d2a49bb7a85322ea1c0cc913e26ed73e2"
