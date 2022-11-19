e6719b56b1763306ba4da784f648ac4a2976b2a2
FX Coudert <fxcoudert@gmail.com>
2022-11-19 13:27:09 +0000

    nativefier: update 50.0.1 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -11,6 +11,7 @@ class Nativefier < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "9d35b4c8a58305360a32266850adff76701962951fada138568e184fe7ad4b9f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9d35b4c8a58305360a32266850adff76701962951fada138568e184fe7ad4b9f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d35b4c8a58305360a32266850adff76701962951fada138568e184fe7ad4b9f"
+    sha256 cellar: :any_skip_relocation, ventura:        "feb5380635bc94c205bddd2f8ad3930333446c4357858baa1df7eb6e50663c30"
     sha256 cellar: :any_skip_relocation, monterey:       "f3b285e09cb0671206fb6f640c34ec19ac0978b5ed75445a1e9250ac2473bd1e"
     sha256 cellar: :any_skip_relocation, big_sur:        "f3b285e09cb0671206fb6f640c34ec19ac0978b5ed75445a1e9250ac2473bd1e"
     sha256 cellar: :any_skip_relocation, catalina:       "f3b285e09cb0671206fb6f640c34ec19ac0978b5ed75445a1e9250ac2473bd1e"
