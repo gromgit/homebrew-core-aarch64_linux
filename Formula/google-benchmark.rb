f3a902f53aa19c3c6686a04e3d29362336818d5f
FX Coudert <fxcoudert@gmail.com>
2022-11-19 20:58:59 +0000

    google-benchmark: update 1.7.1 bottle.

--- Formula/google-benchmark.rb
+++ Formula/google-benchmark.rb
@@ -10,6 +10,7 @@ class GoogleBenchmark < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "030e972223107283b18bde8441ebd4b5bec789666ee2186c7052618fd9887e9c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "40a10bf9895f453e8632baf892a33c8a7a95e1904d38ff900648042e73fff461"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "139bc62b9a65bdbf5c6c9ea1055469257e4cacf2b8a8d80b9a4d089731a70a17"
+    sha256 cellar: :any_skip_relocation, ventura:        "403c232eea67151d0ac32e4b543b8054d32b922af54ed65ed3e129b6099a1d81"
     sha256 cellar: :any_skip_relocation, monterey:       "a2ca9e5cb6ffaadc99aad1d458f5302564a51875c1f3aa26a88a7ba7ee14f2e3"
     sha256 cellar: :any_skip_relocation, big_sur:        "fac57d10b5c15317b923593776dbe5306224e5761959b91a16044f981773a3ff"
     sha256 cellar: :any_skip_relocation, catalina:       "115a907dc638cac992f4297ff71415f00f645a95611c16768a1783170dc7a1ba"
