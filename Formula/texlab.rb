a62a5ae686f0015b211c80b0b11645abed53e44a
FX Coudert <fxcoudert@gmail.com>
2022-11-20 03:45:43 +0000

    texlab: update 4.3.1 bottle.

--- Formula/texlab.rb
+++ Formula/texlab.rb
@@ -10,6 +10,7 @@ class Texlab < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a2f7eaabe3f79283eb0a7482d655197e171b4be2d6e1dd018fccdf3214dbdd8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "430e0e7eb92e75c3c6f627a706c85c8fa8a9d2698ba88058c53c2dbd91e7b155"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "81d1866a498b543e22e6c97e42a80ea5ae077363de5f089a8be574c2b52dbb58"
+    sha256 cellar: :any_skip_relocation, ventura:        "3c4fe860e1dacd9ba4c2fe3d2deea8e7e36d6b05344002f252869e9a768361cf"
     sha256 cellar: :any_skip_relocation, monterey:       "b56e5c82281dd7c409a7e0687915d36d68b09ad51cab5cf168f92403584ba582"
     sha256 cellar: :any_skip_relocation, big_sur:        "c45d498c1b1e47821ce51ef413c5b1865f525acf31b27469d3368fa9de512904"
     sha256 cellar: :any_skip_relocation, catalina:       "02468dc06b7b4d1a22fe731abd661a8f1883ae343b6094c88ccde7624392a870"
