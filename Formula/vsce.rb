9628a17cd3554f3f6c5137d3597dc51a43241551
FX Coudert <fxcoudert@gmail.com>
2022-11-20 01:15:49 +0000

    vsce: update 2.14.0 bottle.

--- Formula/vsce.rb
+++ Formula/vsce.rb
@@ -17,6 +17,7 @@ class Vsce < Formula
     sha256                               arm64_ventura:  "acd2af0b9a1893ec75d6253ee63c16f01de621102db1cd59fa8a82043a38470d"
     sha256                               arm64_monterey: "7672b5b42555b9500014fbe0519d019909f05f7dccbdbc98eced30a7b82e65cc"
     sha256                               arm64_big_sur:  "b6346460c8f5dabbee25108816a01bd322ab28c7bf05715c8a2f706e4d6e9c73"
+    sha256                               ventura:        "3ac06e43e71cdddbed3c9ff51d34681909424bc13c5299188051726930f7ea05"
     sha256                               monterey:       "c88b50cb8939e8ed22a1eb9c1fb805b633228e5d9af44d1e80c3899c833f95e4"
     sha256                               big_sur:        "6015efbc69fa2ba06c6187d9139599d4391041d2398e02a4ce3c07b6298f2357"
     sha256                               catalina:       "7ac92b45d332653e98ea6a7f2a98e2233d5f75699cf4a0a243dfd8a1508faade"
