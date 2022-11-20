2993f02ccd536d8f541c0873897004c7e7d360b1
FX Coudert <fxcoudert@gmail.com>
2022-11-20 09:19:05 +0000

    go-jsonnet: update 0.19.1 bottle.

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -10,6 +10,7 @@ class GoJsonnet < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a7f8dfba1c4375efe666659690858fa3158dce7d30a6d75ff92a758187ea955e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f887f81f923d669cc0008c821d337da78db1d82062bcd66929cc897fe8eea9ff"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "086c86fb80853faa49d5483f506a6d8250013c8fea9df75776ee69caad76fab2"
+    sha256 cellar: :any_skip_relocation, ventura:        "507d42c01d892c04ddb056aa76a7833de6f29e92fc27c9ad4a937eea25c2acf4"
     sha256 cellar: :any_skip_relocation, monterey:       "2d75ab2e8ad3830574b7bad0609816403a6ef9a7f5959de37b2ab683598c7b47"
     sha256 cellar: :any_skip_relocation, big_sur:        "0d129795c442bc6126bafa740e008b9ad7fd6ad0a2d544af3cb21bb9c4c3ab51"
     sha256 cellar: :any_skip_relocation, catalina:       "7d654505048776d7280f994863d32673684c68396f9d13f1d250eb720404413c"
