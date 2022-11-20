3929e69448b286c376200b41f300b38c0fec8b0a
Bo Anderson <mail@boanderson.me>
2022-11-20 03:52:26 +0000

    wagyu: update 0.6.1 bottle.

--- Formula/wagyu.rb
+++ Formula/wagyu.rb
@@ -13,6 +13,7 @@ class Wagyu < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a1163611ed4e907c5458922ca04ab13d6bb1c2f84b7de13d186f3e2f13ada34"
     sha256 cellar: :any_skip_relocation, monterey:       "0a09ccc659885bddbe3daadfda30eff1a4f88bad1e2e4582567451013f855b3d"
     sha256 cellar: :any_skip_relocation, big_sur:        "c3deaea08b0f90132b84451b50bcc6fc5dac9e5cf540c2ef18c3c2a7ce61f8d0"
+    sha256 cellar: :any_skip_relocation, catalina:       "af829681e853e2a146e256548fe69da9e55fde6d974f300b342754831749bd9a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "a81b1c07ee67f4e0cac9e8a7ca17720e440b40c14d66e76db3f5945f862eadb9"
   end
 
