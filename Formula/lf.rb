cf46671c01e950f5d710f7a9f5278d9fc938fdd5
FX Coudert <fxcoudert@gmail.com>
2022-11-20 13:17:52 +0000

    lf: update 27 bottle.

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -9,6 +9,7 @@ class Lf < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ff891643a5a8075a7b4f91b23017f11d21d42ebfe07cd542df75645e09305276"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "553d6cd7611f20d4289169c789da8547455cac8f908dce4e042c13d169bd9e05"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b132e93f46cec73de1f3ac8432adf1cc88caf97e9e5ced9a89b562cade40f956"
+    sha256 cellar: :any_skip_relocation, ventura:        "7ceaf1bbb130e3634e3a12187b4d477bd9dc7bd97285beb9ed0ecb9f76b31d30"
     sha256 cellar: :any_skip_relocation, monterey:       "d80101f720ccf3999990517874f3faf1fd86fae17f8bfc4e8a1eb4508a981859"
     sha256 cellar: :any_skip_relocation, big_sur:        "eb8e3ec6c981fbfd667eedb21ee77dbcb36d68f9162cd2d9de7599b4a4131093"
     sha256 cellar: :any_skip_relocation, catalina:       "5b6b86a2e7e36528a2dd32647fe5d3a9a68d542f953aea319386e51557e5a25b"
