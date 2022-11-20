d6a7db0ae5c54c3fc5f503b9d76acfc8ef8a0c57
FX Coudert <fxcoudert@gmail.com>
2022-11-20 01:54:51 +0000

    innoextract: update 1.9_3 bottle.

--- Formula/innoextract.rb
+++ Formula/innoextract.rb
@@ -16,6 +16,7 @@ class Innoextract < Formula
     sha256 cellar: :any,                 arm64_ventura:  "dda80f3622f51914c7b6f3c4b7a8f8545ea8a71315c886591c2b84cadc9d8eb4"
     sha256 cellar: :any,                 arm64_monterey: "9ee72f555539ca423420c6ec83bdb7fb549f8727e4fc5fec25f47593760809ab"
     sha256 cellar: :any,                 arm64_big_sur:  "1dda1fcae2b4243cfbb565b36b0554b003920e43e18dc479837191efc1f368e4"
+    sha256 cellar: :any,                 ventura:        "c995201988238dae16cc43d3ed49c31829a2283c518aff52f6408a87ec447461"
     sha256 cellar: :any,                 monterey:       "cf7c24ad28cc0aaf358d4d6b676a1ac73e2ffe583cd26149575974c0eb0aa7e0"
     sha256 cellar: :any,                 big_sur:        "36269c001615fe060fcf4a91cfae2e273785e951b5e12402254a1158963bd580"
     sha256 cellar: :any,                 catalina:       "749d13cbe03a8e4d189b47b942f86b42f488508abbfc179a02950eddf1f6034a"
