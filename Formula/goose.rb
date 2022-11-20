97f05089133551b5d383b4e8fe15447613a1a874
FX Coudert <fxcoudert@gmail.com>
2022-11-20 12:24:40 +0000

    goose: update 3.7.0 bottle.

--- Formula/goose.rb
+++ Formula/goose.rb
@@ -9,6 +9,7 @@ class Goose < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f6855ceb3f8e7de6a7638ff1043a9889ab90fcbdd81369cacda350fe33243a0d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b28391bc5fcf1264b569969a75def962c7038bd15d9bcad1ee7d1e42f65ab578"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb17bf9ca19827f461ca37f5b2f217a5759e42f70e80ca01af7b7a71486c7868"
+    sha256 cellar: :any_skip_relocation, ventura:        "dd11aa35ddfdbfedba993a771e746c8956658add7bf0bf782c86c5b9395cb333"
     sha256 cellar: :any_skip_relocation, monterey:       "c5eece958501bca979bd4ee591ac968586e8e7154f942ed61a6f34541c8ff82a"
     sha256 cellar: :any_skip_relocation, big_sur:        "f800b1c38609380e8b40387de9b27d9767f1389e8ad06ec3ac3873c64d9d1e49"
     sha256 cellar: :any_skip_relocation, catalina:       "a52b21a484a4f8209e41cbff1916c63a6c4f399a2d4a6aa8ddc1a1a74e93ee62"
