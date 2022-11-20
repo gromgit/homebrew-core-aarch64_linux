a518acb252d4ad32e9bc6dd041dc86ce9e89d7ee
FX Coudert <fxcoudert@gmail.com>
2022-11-20 11:04:33 +0000

    chart-testing: update 3.7.1 bottle.

--- Formula/chart-testing.rb
+++ Formula/chart-testing.rb
@@ -11,6 +11,7 @@ class ChartTesting < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "75100b8fd0d5cd10602a30931cbe131e4229a411a6bfc45e3f78ad3f063c20f4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "12a6169f8c276d9ed9eea8f5849ca232dabccaa9d3cc496931b226fa1e2da002"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "317c065e19700b8edb723f68866847f4a5ff000f66e4e838006ad75f67a73fd0"
+    sha256 cellar: :any_skip_relocation, ventura:        "2dffdafc9d76294f0431cb577bdfbfab07bb1579d7d61cae0b4456f392063d55"
     sha256 cellar: :any_skip_relocation, monterey:       "eb013fc30598e97a1a1b94ed5e03feac1a76ae06697bb274e420bed5b0e6f0e0"
     sha256 cellar: :any_skip_relocation, big_sur:        "dd8604927f5822fec4b08eb9e8cc95e82e9627683f9d2fb4db2372d65ab54b46"
     sha256 cellar: :any_skip_relocation, catalina:       "9a0f8350ab50eddf2e294d1711440c37f4a041beeb412935d273ee87d384dbca"
