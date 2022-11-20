2775a69c370b0ee52db4fdfb08daa4c70ad31a6a
FX Coudert <fxcoudert@gmail.com>
2022-11-20 04:11:59 +0000

    scw: update 2.6.2 bottle.

--- Formula/scw.rb
+++ Formula/scw.rb
@@ -9,6 +9,7 @@ class Scw < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "10050688b7267c55a117b2b7d26d2bf44c244e6f161537b9bcd413d8961bf3ef"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3902b2ef9a47240844ae682d917eae2cdab3afc1527dedc1fa5ca2980946e8cd"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "18a7b83ff74d53bd087d0f86d39d795ae58b1950675bf28330b1be74db01f8ee"
+    sha256 cellar: :any_skip_relocation, ventura:        "deac401381375438722edbdb0f6295a99f99c900049aafa2da5fd75c07e19ec0"
     sha256 cellar: :any_skip_relocation, monterey:       "58e571e9d4dd1360297b585671fea111dc7858260c78d0750a8664c58a3da92d"
     sha256 cellar: :any_skip_relocation, big_sur:        "edd0ebbc4b5c10081cce83ff4b33892de9338548bb635f3ec5e8da87a550cd25"
     sha256 cellar: :any_skip_relocation, catalina:       "72358b8b353b5bbfd853313e9e639b0984a030bc18f33a05d62587adde4abe68"
