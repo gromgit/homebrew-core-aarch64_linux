8a04b77754b89725798857b3f7285456927c635b
FX Coudert <fxcoudert@gmail.com>
2022-11-20 13:56:37 +0000

    msmtp: update 1.8.22 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -14,6 +14,7 @@ class Msmtp < Formula
     sha256 arm64_ventura:  "998546c39f503352cfcda5e4b67d514b693eecb531fbb14c49122b425e4b192c"
     sha256 arm64_monterey: "92d52d20ef071b7de5dcd5be20655d29cce0b5704bf51e566b589174d0671094"
     sha256 arm64_big_sur:  "0cf7ac0320339689f3746094f2504e608621e270fbfb213bbe25cff544b11a1f"
+    sha256 ventura:        "be8d73253cacaf969572d9aa2a469d27c6efe8ec682282dd0132cd44e0160f6f"
     sha256 monterey:       "30141e954634d0da2cd15fcef4292d6bc424ad1d822f5995e71f41f7b6efb50a"
     sha256 big_sur:        "16c4c861b923ddfcc1633f9960d8676968b4bf4dd4fde4977972fd04311d394f"
     sha256 catalina:       "8fe09ebdfcddedd9b7bd5e7143a21be824fc5c6cd7ccdde3fd34e4d0bf2069cf"
