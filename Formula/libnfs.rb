12735105728685f65cf9f6b924c6a70906d3c869
FX Coudert <fxcoudert@gmail.com>
2022-11-20 11:47:37 +0000

    libnfs: update 5.0.2 bottle.

--- Formula/libnfs.rb
+++ Formula/libnfs.rb
@@ -9,6 +9,7 @@ class Libnfs < Formula
     sha256 cellar: :any,                 arm64_ventura:  "a1a8014854317d7d2cb77e7bdef425ea9ca5555dd0f1ada88326afe6dda189fb"
     sha256 cellar: :any,                 arm64_monterey: "b8df8747861411e30845ca1a6ab42877c74d8ca3e65c838b5ad498faa6609481"
     sha256 cellar: :any,                 arm64_big_sur:  "df45a5d53a8ddd243d7702fbbd834607db180679dd694eb9ef91bf14f1a399f2"
+    sha256 cellar: :any,                 ventura:        "586c605f631aefc3ebfaca7f45dace462b7fbcf813a0e9e16992727b4d996f5e"
     sha256 cellar: :any,                 monterey:       "af2b41b3437f4e5e5ead13ff3f3c4fb8cea0aa69813a56199e4d7b0547b6a7bd"
     sha256 cellar: :any,                 big_sur:        "c4fd271ca3189c9fe0509bd7637f6d8c4b9974f47db586de2deb0893bbb4cf68"
     sha256 cellar: :any,                 catalina:       "e188d926e1a762f20c892a5b9202de9c42e2221bc35899a22f5c6a064dac1dd1"
