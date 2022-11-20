540a744bf38ffc550eaf4f671071071afe5a1b57
Bo Anderson <mail@boanderson.me>
2022-11-20 04:46:00 +0000

    grin: fix compile with newer Rust
    Closes #116219.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grin.rb
+++ Formula/grin.rb
@@ -1,6 +1,7 @@
 class Grin < Formula
   desc "Minimal implementation of the Mimblewimble protocol"
   homepage "https://grin.mw/"
+  # TODO: remove the `cargo update` line when this is next updated (5.2.x).
   url "https://github.com/mimblewimble/grin/archive/v5.1.2.tar.gz"
   sha256 "a4856335d88630e742b75e877f1217d7c9180b89f030d2e1d1c780c0f8cc475c"
   license "Apache-2.0"
@@ -20,6 +21,10 @@ class Grin < Formula
   uses_from_macos "ncurses"
 
   def install
+    # Fixes compile with newer Rust.
+    # REMOVE ME in the next release.
+    system "cargo", "update", "--package", "socket2", "--precise", "0.3.16"
+
     ENV["CLANG_PATH"] = Formula["llvm"].opt_bin/"clang"
 
     system "cargo", "install", *std_cargo_args
