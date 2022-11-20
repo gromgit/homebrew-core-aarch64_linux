7ed9857ecc0e5d619fe1c4fb4777433d1a9b55e8
FX Coudert <fxcoudert@gmail.com>
2022-11-20 02:14:37 +0000

    tectonic: update 0.12.0 bottle.

--- Formula/tectonic.rb
+++ Formula/tectonic.rb
@@ -18,6 +18,7 @@ class Tectonic < Formula
     sha256 cellar: :any,                 arm64_ventura:  "125ae7437cb1e2af6a9f79db285e0bbe8c7e5c0ce96045705525859e41d906d5"
     sha256 cellar: :any,                 arm64_monterey: "502d74dc17414cdce878ec9cb8300f6facf0872942f83e566b1d1053e0f7ea3a"
     sha256 cellar: :any,                 arm64_big_sur:  "37c727c4739e728937e636f62aaa8185e81145a6721ba10e6cffb9cd6e08ea94"
+    sha256 cellar: :any,                 ventura:        "50840182fba623f9780bcb8f5343fb359235fc73b1891a6fceb21fa68e730085"
     sha256 cellar: :any,                 monterey:       "470d6e7cfe93b7c7385e36d1f747c77985cb83302eb43c8f179a1be4e0e72792"
     sha256 cellar: :any,                 big_sur:        "3713f373c74b0803fe1f35277cb28331f237c6b47c47f5ba928e5b197062cc79"
     sha256 cellar: :any,                 catalina:       "c6729ef8f16818c0ec21e5fac851bad7551f189c105acc35c25690af8551a13d"
