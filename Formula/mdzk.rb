d02aeb6a12d4b37399a4ad79f6cd46805ebdcd3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-20 09:04:58 +0000

    mdzk: update 0.5.2 bottle.

--- Formula/mdzk.rb
+++ Formula/mdzk.rb
@@ -12,12 +12,14 @@ class Mdzk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "140dcfda6cb0fd9c4174dea54ab773fdf33e5c982290d19b691b92b690005bbe"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8fee53c9fd45936faef71d26b4fd09b63aa210427b9958c9a6dbe59d3eeb4b43"
-    sha256 cellar: :any_skip_relocation, monterey:       "7eb13864d25ec8c705244ac9b2ceee231bab3480b851a5aa1821edb14d0be2c4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8a83366746029b34f303966e7db78e29573894d35e6577f3a307536b21e3f230"
-    sha256 cellar: :any_skip_relocation, catalina:       "3849a8d5b995c251dc0ad89a456b87104dcfc6d3c50e0d6e344de90dc177abc6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff24a92ce07d5e0c9cb76598f9eae2e1c2e07435fd76760b7b360aabfce7ab4e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e395b411c4fd8c752152796aa860f24a44ac7d4540ffc838c6808a443ea271ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d5b825c14aa162c7b6e107d17a4a94db341b1dbbb57697f1001bd66a2ab9b45c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57d5583b4a4f354106060c3d15e245b223580a29bfc2e7228bd9a611a31506b0"
+    sha256 cellar: :any_skip_relocation, monterey:       "5a880de7f75b7b16d40b997e0a50aa4097d24598a46294ba097f5f7a8c3f9595"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b15fade4973c35b431443b0982396b4c1e03399f2c2b85413158a976fbc501a4"
+    sha256 cellar: :any_skip_relocation, catalina:       "c168154d8cf9b1aafdf5f089922d8eb8e74655b97b389435534a9efbc668440d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95bc548f5fb2c84e0e3b62e895e3c6812e8df51b74eaf6bad4167ab1861f66d5"
   end
 
   depends_on "rust" => :build
