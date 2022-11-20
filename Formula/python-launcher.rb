2ff8878400de12bbb4752627f22c8ead2a6e7348
Bo Anderson <mail@boanderson.me>
2022-11-20 00:09:14 +0000

    python-launcher: update 1.0.0 bottle.

--- Formula/python-launcher.rb
+++ Formula/python-launcher.rb
@@ -7,14 +7,13 @@ class PythonLauncher < Formula
   head "https://github.com/brettcannon/python-launcher.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9e8a0d8f9cb6a45cee3032180c4dd57b25b357e39fa3fd4a3a362f30b48164b4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ed1b618c757aa70ec2165b68cc62075b4c1f9b542fa90b8d16a48a41fa85af46"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a8fb00eea4d01ceb13780d0a94687ecfa959012c20aefb0715283471c86b55bf"
-    sha256 cellar: :any_skip_relocation, monterey:       "ab831589ab6c57fcb36954b3bd97ffe835db5a8f76c2ac931f764cdc532c37ec"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0060ddf0b4825662923bb18ea5ba45ed8de79d7adc343ca50e955ee56d371d32"
-    sha256 cellar: :any_skip_relocation, catalina:       "91522804817f69fc416e17ef61c02f7d0727472e89148042afd4f52b4b65d926"
-    sha256 cellar: :any_skip_relocation, mojave:         "2be4b95b5e33a6e1035bb19f07257941407351428b7a29efaa4a8429379be1da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d67a4126f429318358cba0dfaea618ccf579f1adbe2bdd9f98778acaec82721e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cd92ae1827420e02bd899e2a8f4be16b31bad24abb5038f934f944603acf03e0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a1439311c007d3300050b0d8783fdae8cff055a1f33505a74c8085c180adeed"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5310aafe95f47164c849bbb278708ba5f2a601739c7556e19e9f8389e618320c"
+    sha256 cellar: :any_skip_relocation, monterey:       "2c80376c3923316d00b043281cb9c53a6cfaebc748c16fd0237b7f1dcfd50efe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ccb5a7d8b51f327df0003a9ee2a64bda64c962bdfb86e2b71dc21819c395a74e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd2b8f79e79ec6ea754393c44e392b57c9ab622568a465b46cdb7e93f85a2b30"
   end
 
   depends_on "rust" => :build
