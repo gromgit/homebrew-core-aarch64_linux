c40b83f95209778cd30a8d081f623bf7fcfb02c4
FX Coudert <fxcoudert@gmail.com>
2022-11-20 00:22:33 +0000

    buildifier: update 5.1.0 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -10,6 +10,7 @@ class Buildifier < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a597d18586fb30489ba3487a6da0297cdbf1690cf44a55fe39686ac7e2ab28ec"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d5067b1bd99a3c972caefb196e7a07204dbd0fb06566554b3056da2b3176eef7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d5067b1bd99a3c972caefb196e7a07204dbd0fb06566554b3056da2b3176eef7"
+    sha256 cellar: :any_skip_relocation, ventura:        "d1abd4fc1428a62d060627a94f1030e99fcceeca9d2c7ad72f802f63e1588f33"
     sha256 cellar: :any_skip_relocation, monterey:       "b50c4c7e9a0bda54792c4f478417a8678c164aeaf08520d922dd7f8e9ef89bff"
     sha256 cellar: :any_skip_relocation, big_sur:        "b50c4c7e9a0bda54792c4f478417a8678c164aeaf08520d922dd7f8e9ef89bff"
     sha256 cellar: :any_skip_relocation, catalina:       "b50c4c7e9a0bda54792c4f478417a8678c164aeaf08520d922dd7f8e9ef89bff"
