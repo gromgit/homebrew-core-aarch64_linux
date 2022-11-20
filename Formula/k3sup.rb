0d4db9e4e9721788e1a6c1ecbbab1afa92e55a5f
FX Coudert <fxcoudert@gmail.com>
2022-11-20 11:36:02 +0000

    k3sup: update 0.12.10 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -16,6 +16,7 @@ class K3sup < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "5e08ffec344a92947c29923a13b91a2d8d32c8501687f05e1469a8a02f61fec5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7976530c27ea3cafc1298946808444ac0f11356eda94964c8843cc008838ec1e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "458a0d1b7644fe0bcdd163287ea78de174b2ccf40a7bb3428a8ed1705b20be4f"
+    sha256 cellar: :any_skip_relocation, ventura:        "65365e05c4af2c86353391dd454c7071779b86a959354db154469f6d5f028f59"
     sha256 cellar: :any_skip_relocation, monterey:       "5ceae0674f5361d7414923f69f479456490454ef8c821566422d90d06825dc23"
     sha256 cellar: :any_skip_relocation, big_sur:        "0c35c93623857b785b71c5562e2c91fbb77024505846862dc5b820cc495d8754"
     sha256 cellar: :any_skip_relocation, catalina:       "56ddd0285b5ade55f932d950edc1f817c888cac433252fb1a47630cea236e308"
