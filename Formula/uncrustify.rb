6da9c87d8fc25fd69ab7624ea4aeeb0710c9f89f
FX Coudert <fxcoudert@gmail.com>
2022-11-20 13:43:45 +0000

    uncrustify: update 0.75.1 bottle.

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -11,6 +11,7 @@ class Uncrustify < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "df2f51573eca7780661104e4f5749d2d45a412e0653af91a77d472ca8c0d2538"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "18fd62fc09199932669b7dae942275db9b01cb0f41426bb498d4c0c62fa281d4"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "125f9215b38b8479bf19a3e2bd2a26b69d852330c5ea814efc4f321fd7c7845c"
+    sha256 cellar: :any_skip_relocation, ventura:        "df56dc00daa0ccebd34531fb7abc8786f6f88cc4333dafdfb03124f0b8efbe33"
     sha256 cellar: :any_skip_relocation, monterey:       "91b46324697bb64ff4dd19a14b03c76f121413cee986b1fbbadc28ee9606dbe6"
     sha256 cellar: :any_skip_relocation, big_sur:        "d3aa70ab113276545fe3dcf0a29a1549f41e1f2237ffa3ee41ece14aeb321a4e"
     sha256 cellar: :any_skip_relocation, catalina:       "d9718ce807174f7673bd384b14df90b3e0089c95ae2d023f0dd51cbf895c5c51"
