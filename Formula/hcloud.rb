3ad52b88fccf0579edb19fa2f2d98a328f5bf6d4
FX Coudert <fxcoudert@gmail.com>
2022-11-20 02:32:22 +0000

    hcloud: update 1.30.4 bottle.

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -9,6 +9,7 @@ class Hcloud < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a4e62c7ece885e46bbeb41ef0ad11a4f1d1b519498e82eab9878d1ac33d081f6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a2d507878318853dcbba57f4c85874cb71228936293c7597c52ca1d40beaf8c3"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2cfde8f131587ed7c61c8a6fbd0af28a025b09900469f97e53d588d5c0184ef3"
+    sha256 cellar: :any_skip_relocation, ventura:        "927ad496cd5b8b637c5fb3a6bd0b0c17cc6afaa15cccb9c9381ba3958c3402aa"
     sha256 cellar: :any_skip_relocation, monterey:       "526981dc9a6437f75819fbd7d79abff2d0654434f315ef6dfa73a692ec37ae66"
     sha256 cellar: :any_skip_relocation, big_sur:        "061a6db21bc970590f8dd21d0635e37faf419f4ef935e442707a2b0681b30604"
     sha256 cellar: :any_skip_relocation, catalina:       "77e63c1276bc281dac2e5f948d47c3204e7bdb6614f9cfe0c7614e109c61816e"
