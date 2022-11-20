a24c23c9a1946fb16380fa04c5958e80a38b849e
FX Coudert <fxcoudert@gmail.com>
2022-11-20 23:24:45 +0000

    goreleaser: update 1.13.0 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -11,6 +11,7 @@ class Goreleaser < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "cf598986ca20f9556570d5268a407162ff21a66361af9180d078b310fcac9e51"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "49f05ca1465e77cb73947cdfc569d5ead41209ba1e3d1e0f650eb1ec78657730"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6b98d79cceb12a1faf4597077e4c0f36c9c43a5fe3046200ee104e88119f4b00"
+    sha256 cellar: :any_skip_relocation, ventura:        "1f37dbdae547d043caf840255aa28327dcac8b12cd2af8e5b8950f57de1849b8"
     sha256 cellar: :any_skip_relocation, monterey:       "e1e65ffeb5968f728554db40d29550e5d57d983dfd434d79c7cf0928fcf832cc"
     sha256 cellar: :any_skip_relocation, big_sur:        "8c6e86b334bd4a0fc98cdf7eb93ac14dfe4d2ec02edb2663b2c34d6ae18dcba3"
     sha256 cellar: :any_skip_relocation, catalina:       "6dd9bcfd626f0cbf7639eaca248e9b35a49a4f58ac8693a01d239bb15e2ba2e2"
