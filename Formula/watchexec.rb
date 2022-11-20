8da2cd4fc98908c7a70cebfb2d088119d4b5b897
Bo Anderson <mail@boanderson.me>
2022-11-20 06:02:12 +0000

    watchexec: update 1.20.6 bottle.

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -15,6 +15,7 @@ class Watchexec < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f21307d3ab8d57b60503ccba09d4eca03a444ee6e198a60d00bd4b45f50f2a6a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3c037bfa1aa3a1b77bcb85abb34273ecbf5bc5f6eb9c5627c819ed6c93a72f06"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "208eb02f68a5ae5b909e9e325518ca655fab05c7a601f1fb9678a5ce4f1629d7"
+    sha256 cellar: :any_skip_relocation, ventura:        "45306bf636e83b1ed9761e87d2f17f1e76f4e01b5cf70ea61ee1616ea18636e7"
     sha256 cellar: :any_skip_relocation, monterey:       "5aa3fcffdc6a394906c02e15f2bffc47fdd070d606f213b4644f926641aca4d3"
     sha256 cellar: :any_skip_relocation, big_sur:        "2fb31b6ec806304cdcea676b71c90ff488bf599caaada386187c5cbe667ec80b"
     sha256 cellar: :any_skip_relocation, catalina:       "5553d313a976a4ddf1f411686019d9afa880ce589f1aa689a21c899b19ba8772"
