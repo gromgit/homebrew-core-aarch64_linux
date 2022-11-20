6df4e962fa8d368df0b5fa5fbe80a91c8d171e5c
FX Coudert <fxcoudert@gmail.com>
2022-11-20 12:49:58 +0000

    jdupes: update 1.21.0 bottle.

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -14,6 +14,7 @@ class Jdupes < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e63ef218912e5c736b033ed73a09227fff0936b4a207e7479ca8f0690cb47e61"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "60496d37fa731894985ede7a3ce8fc2ae58f41daa4ff4b2f8155b72263cad8aa"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "92aa74b194b735d10012ec3ab5768eea50e4432ea13d64cfcffdf49b5e7953a6"
+    sha256 cellar: :any_skip_relocation, ventura:        "e11c030b90e303415df977b80ffc8dc26c774b137a223497074cd658d2dc41bd"
     sha256 cellar: :any_skip_relocation, monterey:       "dac797d9b5d739d9dfc17676678d623099277e94912b6f92a3aeb479a6c2e121"
     sha256 cellar: :any_skip_relocation, big_sur:        "5784038ce9ccd2fdd6663529fe60b0724c83e7f9e8e9f8d7f58f9f3bdec9d2bb"
     sha256 cellar: :any_skip_relocation, catalina:       "333bb2646c70e0960f8e857087c0bceb3bcf4a29a869e6e79b6622416e194bcd"
