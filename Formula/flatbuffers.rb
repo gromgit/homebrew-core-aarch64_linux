e85bd5e6c48104f6250f179b8c0cfcaa1b1270fd
Bo Anderson <mail@boanderson.me>
2022-11-20 05:34:48 +0000

    flatbuffers: update 2.0.8 bottle.

--- Formula/flatbuffers.rb
+++ Formula/flatbuffers.rb
@@ -15,6 +15,7 @@ class Flatbuffers < Formula
     sha256 cellar: :any,                 arm64_ventura:  "1c991c58076e48ea56e95bb4fc686509bf405a2c1bfed1aeeb61563b7d24bfde"
     sha256 cellar: :any,                 arm64_monterey: "cbe115fdd3ad2a51b390049dd29c9cfe0ca1fde327ff4cdc1e695920f4ca26dc"
     sha256 cellar: :any,                 arm64_big_sur:  "400cd4e592180a130953391370c6401996b046fd502d9674b0cc3a9b4b7a2988"
+    sha256 cellar: :any,                 ventura:        "e4bfece8a5f753df8a04ffb5bfaa91b48f14bd7aad4860b892302d216be77d5a"
     sha256 cellar: :any,                 monterey:       "85bc64d4481927fe45ae9be89bc6b7bfa3119a73473589fc7dacd6805257b8f8"
     sha256 cellar: :any,                 big_sur:        "c49ad3a0896e13aee915da94fff43492c166c2037d4b638ae236ce83e9e4dfbe"
     sha256 cellar: :any,                 catalina:       "1bc32cb31d07c392a63bbb5be4a3b8d4fbba7ea52b9df6564051fda444e00324"
