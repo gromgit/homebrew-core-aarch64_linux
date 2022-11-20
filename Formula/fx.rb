f9fb48fa6cabc2f9ce2c201c84d5fa4574f152e6
FX Coudert <fxcoudert@gmail.com>
2022-11-20 00:52:56 +0000

    fx: update 24.0.0 bottle.

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -9,6 +9,7 @@ class Fx < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f6aa55719d1c7deecc78a80793defd2bfe6cf0c847261b1d0dbd580758dec3e6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f280c7957a0baea6ab964b3518aea0a58016002cc18b9a550d50d0773f190ba2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f280c7957a0baea6ab964b3518aea0a58016002cc18b9a550d50d0773f190ba2"
+    sha256 cellar: :any_skip_relocation, ventura:        "76cbba89d1bef8456c748471b3961a9a47ca38896ce71e221143da5ebe064d82"
     sha256 cellar: :any_skip_relocation, monterey:       "0fcb17a478b4fe352e8842801d6b4f93ee019ebcdbedd7682ea2db06f947847c"
     sha256 cellar: :any_skip_relocation, big_sur:        "0fcb17a478b4fe352e8842801d6b4f93ee019ebcdbedd7682ea2db06f947847c"
     sha256 cellar: :any_skip_relocation, catalina:       "0fcb17a478b4fe352e8842801d6b4f93ee019ebcdbedd7682ea2db06f947847c"
