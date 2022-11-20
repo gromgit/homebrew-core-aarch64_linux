f4ef80b1e219893a15c222a0b9c0cef5d1a9526f
FX Coudert <fxcoudert@gmail.com>
2022-11-20 20:58:18 +0000

    mint: update 0.17.3 bottle.

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -9,6 +9,7 @@ class Mint < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f6c2b944c3bd04099240ed8c4c1489ba8a6274e8d80793744cf3bf5b0388922a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a4a82c0b54477a7c3c54000d84833724f3aaa6f9bb62a20ffddb3c880f264480"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "722c3f50d202e3461e5130c3639da14b4d3e55f5156d7803978571da5577f5cf"
+    sha256 cellar: :any_skip_relocation, ventura:        "38c805a065c493e4ce8ecae3dd9f832b8341cfa1d6f1d854b41f5bc19d022cbc"
     sha256 cellar: :any_skip_relocation, monterey:       "4a233f8e964bd5da4b4c6a4682e4d1bf1ee070a82afeffc12ec0dcbeb018f704"
     sha256 cellar: :any_skip_relocation, big_sur:        "ee17ecd152799a45f080066867c1046ffd332940f1aa925ec4a4b032c47b0f54"
     sha256 cellar: :any_skip_relocation, catalina:       "54dd30032281efb21ce064aa2d205ca59c985f12f1bb55419ca6800c9b38a59b"
