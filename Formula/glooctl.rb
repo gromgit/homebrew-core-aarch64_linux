326fb3074481c63566c9ee4ce54834d355d7d8b8
FX Coudert <fxcoudert@gmail.com>
2022-11-20 12:24:31 +0000

    glooctl: update 1.12.35 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -18,6 +18,7 @@ class Glooctl < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "0be3009645150688b216e103541b2886c04e922d9e61138fe9bc421a9ef32e57"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "88e1f80ecd8358bde5ee8ea61e334591446232e304354f6ae8156440d150a71e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3671f4ed3c1458fa275cb873b35b7c8e4f815fc083f2a449852e2c52fd66c7fc"
+    sha256 cellar: :any_skip_relocation, ventura:        "15a3b2e8a20ca2c2fd57b2464e7dbb3e479c0b70f772245e780e6c4ed207c928"
     sha256 cellar: :any_skip_relocation, monterey:       "53a5f4973bce81a279c941e3dd0dd8fe06f19317fd7205a09d32df3415fedcf6"
     sha256 cellar: :any_skip_relocation, big_sur:        "2785a1ce4ca6831d5cc9b718de67cf7746524d6b1d35fdeb473cafbee55b0674"
     sha256 cellar: :any_skip_relocation, catalina:       "e90bf3ecc2660fc8db6cbecf67af4ef6ac68417d55cc25890568f6cebf460e3b"
