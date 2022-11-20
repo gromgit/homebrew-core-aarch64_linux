cea108cfbd47b043e6f180340fbc7618218c9966
FX Coudert <fxcoudert@gmail.com>
2022-11-20 12:17:33 +0000

    stockfish: update 15 bottle.

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -16,6 +16,7 @@ class Stockfish < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "9fc7b85028b41cd7e3db3f25a9481521aef01f95611e979734b815c64631f159"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2ea9532bb3f1686c433a62c8eb671a0cfef3a9ead7a6a5a83475826c5bcb49e7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9961c12ee12480d1dcb9f72af2b735ee6c4d686e62b0554625efe0246f9a327e"
+    sha256 cellar: :any_skip_relocation, ventura:        "e9c420bf30df7268fe606e4efc481d7e6d5c25988c70eb30184c1ad81f88a1e6"
     sha256 cellar: :any_skip_relocation, monterey:       "c29b02b056094c6f11072cf38351ad259804d97e1727c149fc8a3f6716a1d993"
     sha256 cellar: :any_skip_relocation, big_sur:        "39efb53e1d05be75937cd9db2af59e1bfd618992b455c702a23c3628e6bbe154"
     sha256 cellar: :any_skip_relocation, catalina:       "da786a5130e0126c86ce20fa078a3af16270a0b9649411e9ee64a28c370b3a73"
