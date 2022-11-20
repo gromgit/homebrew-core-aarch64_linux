69441b6aa4bbda869b635093e1511def67c946ac
FX Coudert <fxcoudert@gmail.com>
2022-11-20 01:52:57 +0000

    cosign: update 1.13.1 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -11,6 +11,7 @@ class Cosign < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a95a4be1c732ac741969d5ffb6b14017e7514bab86d77bfb854c5cfc9cd5e09"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0ca0876a726924416a09a548a9cd5f59a30f62796eaf94659082d163487068a1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56144ede6687b63e90ebad77ff3889a2ff421672ba1c8c6f7ed0a5fb10b2ca69"
+    sha256 cellar: :any_skip_relocation, ventura:        "a61dcda531be2dd019df2375a65b0815d652144147877274ea471cdd2e212066"
     sha256 cellar: :any_skip_relocation, monterey:       "45e42f866150ac981dad74171aeabc6497c30c81098efec258bb644e65bbf8bc"
     sha256 cellar: :any_skip_relocation, big_sur:        "bc74eb0310a7751ce9e2694a36831a032a4cc0af89bff9614635e5c9dedb42b7"
     sha256 cellar: :any_skip_relocation, catalina:       "0ca487e247a593b7c89730c6db1992581ebb25bf745cd47d8579acdcaa218d16"
