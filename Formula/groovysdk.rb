818b0c51b42334d6738e0b9b93e2bf4fbee7d882
FX Coudert <fxcoudert@gmail.com>
2022-11-20 02:42:59 +0000

    groovysdk: update 4.0.6 bottle.

--- Formula/groovysdk.rb
+++ Formula/groovysdk.rb
@@ -14,6 +14,7 @@ class Groovysdk < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c24b69dd0fdb335baf3384a7c82743ac25e658d13f60ee55c3ee7d0773b3d137"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c24b69dd0fdb335baf3384a7c82743ac25e658d13f60ee55c3ee7d0773b3d137"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c24b69dd0fdb335baf3384a7c82743ac25e658d13f60ee55c3ee7d0773b3d137"
+    sha256 cellar: :any_skip_relocation, ventura:        "91e99e4eb7c4dea647ca04f8c8e04f8a597e1c91d9b0ac201d3e1058cd460b0e"
     sha256 cellar: :any_skip_relocation, monterey:       "91e99e4eb7c4dea647ca04f8c8e04f8a597e1c91d9b0ac201d3e1058cd460b0e"
     sha256 cellar: :any_skip_relocation, big_sur:        "91e99e4eb7c4dea647ca04f8c8e04f8a597e1c91d9b0ac201d3e1058cd460b0e"
     sha256 cellar: :any_skip_relocation, catalina:       "91e99e4eb7c4dea647ca04f8c8e04f8a597e1c91d9b0ac201d3e1058cd460b0e"
