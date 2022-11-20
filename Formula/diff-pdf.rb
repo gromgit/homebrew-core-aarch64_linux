d7fc75b4e2eda3cb5df68c37433821a55db19604
FX Coudert <fxcoudert@gmail.com>
2022-11-20 01:32:54 +0000

    diff-pdf: update 0.5_5 bottle.

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -11,6 +11,7 @@ class DiffPdf < Formula
     sha256 cellar: :any,                 arm64_ventura:  "a0977dfd009954ba4428e60a954efa92eaa3202eb2061e6a60aaa3c16faf2fc4"
     sha256 cellar: :any,                 arm64_monterey: "0616440421b45bca4494c5eaf90640d61b4f70ef77fb4a811fcebda766f07957"
     sha256 cellar: :any,                 arm64_big_sur:  "0e01c107b0949a6d3baeefaf15899461a8b7ac886efdba9d2960a52a3e62d59a"
+    sha256 cellar: :any,                 ventura:        "234055d5afe5c54ac619aa3a14d802507fa771d3f6c9cf8856f1ddcb7e937c4e"
     sha256 cellar: :any,                 monterey:       "439c5722731ba71ccff2e07de2c7b38262c47f8532c9949f606e9e12419682ea"
     sha256 cellar: :any,                 big_sur:        "2e11ac7650d5cfe77922aa0deb7c036b708d370e5c748688b1538bfe3606ca54"
     sha256 cellar: :any,                 catalina:       "aa5bfc82668e41fb44c6a5ed7e83b9c1a59ecd7ce4c76a67530713767e95902f"
