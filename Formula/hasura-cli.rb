baa74fed4956d3e2a2b2effa862b8193871c796f
FX Coudert <fxcoudert@gmail.com>
2022-11-20 02:20:36 +0000

    hasura-cli: update 2.15.1 bottle.

--- Formula/hasura-cli.rb
+++ Formula/hasura-cli.rb
@@ -11,6 +11,7 @@ class HasuraCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "90d56dbff02f2f1ee367f08705a9530d63a0296e27dfa9ca3828c690b2e4f210"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d5f80f0b0b80c5244d459267933e5af1c3ee2c1f78a0fc065fdd44794dbe9685"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3f9300bc9715d5c901b21f6b3ea8ed043de270bdd9cae513429d0320c9851bc5"
+    sha256 cellar: :any_skip_relocation, ventura:        "f080cb94a23c209d1840bc5f434c8b48ced35cadced83cd8fbcf8115184349f0"
     sha256 cellar: :any_skip_relocation, monterey:       "ef591c195d5f3da3c1e92023c5e79a47340df848074b181c0049fbeea971d98b"
     sha256 cellar: :any_skip_relocation, big_sur:        "4633b4eb9af24bb914627749d4cecad310f4658c88cfee9269bc4fc36a253067"
     sha256 cellar: :any_skip_relocation, catalina:       "2198c2df7b353cd250a14ec119fb7bf5fe84659c400cbe8ecd8b1ff3a434c0b4"
