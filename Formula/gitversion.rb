08dff14f0e79272d565d8204cc810d5b4ce46ac3
FX Coudert <fxcoudert@gmail.com>
2022-11-20 09:43:45 +0000

    gitversion: update 5.11.1 bottle.

--- Formula/gitversion.rb
+++ Formula/gitversion.rb
@@ -9,6 +9,7 @@ class Gitversion < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8d1dde8fde0eb649ccf639b0469090b6f7cf7daec5af586162be49b8ee3fbf9a"
     sha256 cellar: :any,                 arm64_monterey: "c8493e7cb21aca7c941744c8a22b1485a1615c33bbce998a59991968132883c5"
     sha256 cellar: :any,                 arm64_big_sur:  "b806eb80b7f7f4b81c69c56515c19149a06fe2b9b1e9753c22f54f6e6ff54745"
+    sha256 cellar: :any,                 ventura:        "5371269e21bb25ddc5b760b270448e2acc494b79ae81af985b72f1c06e81cb25"
     sha256 cellar: :any,                 monterey:       "884e90c178d31d2441d17ca45154474cc0fe231f15df4e21a46486d9b768e85a"
     sha256 cellar: :any,                 big_sur:        "34b1dc9dff977fb3680786dd8600793c1e4ee5bd5ac9251362279e59a6265fe7"
     sha256 cellar: :any,                 catalina:       "975dbb602b2b4516df5028c5f270e2fdf96c4e2e4777eec37e2e1a9d39460a8f"
