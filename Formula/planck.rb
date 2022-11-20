dadbdf87d88d6d1fbb41a230d14b2b9055dfb903
Mike Fikes <mike@fikesfarm.com>
2022-11-20 09:28:53 -0500

    planck 2.27.0
    * planck 2.27.0
    * Remove upstream commit patch for GNU sed
    
    Closes #116230.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -1,10 +1,9 @@
 class Planck < Formula
   desc "Stand-alone ClojureScript REPL"
   homepage "https://planck-repl.org/"
-  url "https://github.com/planck-repl/planck/archive/2.26.0.tar.gz"
-  sha256 "e2a01ea5cefcc08399a6bfc7204b228bfd0602b1126d5968fc976f48135a59b2"
+  url "https://github.com/planck-repl/planck/archive/2.27.0.tar.gz"
+  sha256 "d69be456efd999a8ace0f8df5ea017d4020b6bd806602d94024461f1ac36fe41"
   license "EPL-1.0"
-  revision 1
   head "https://github.com/planck-repl/planck.git", branch: "master"
 
   bottle do
@@ -36,12 +35,6 @@ class Planck < Formula
   end
 
   fails_with gcc: "5"
-
-  # Apply upstream commit to fix issue with GNU sed.  Remove with next release.
-  patch do
-    url "https://github.com/planck-repl/planck/commit/f1f21bf9eb4cfca6312ff0117f75d3b38164b43d.patch?full_index=1"
-    sha256 "787ddf6fb1cfea1d70fa18a6f7b292579ea1ffbf1f437f1f775e3330d2b8d36c"
-  end
 
   def install
     ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
