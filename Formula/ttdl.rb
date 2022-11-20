e74f2ec6be2ab446f8b3414effdcc424fda173b3
Bo Anderson <mail@boanderson.me>
2022-11-20 03:23:38 +0000

    ttdl: update 3.4.4 bottle.

--- Formula/ttdl.rb
+++ Formula/ttdl.rb
@@ -12,6 +12,7 @@ class Ttdl < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b7ee2163cafa8983c0c011f1c11f0c0ccad766eb92a753001e6dcabb6f492cc"
     sha256 cellar: :any_skip_relocation, monterey:       "eb88e39e38ab03a35b858b35f3f65bc8f7d01c2caac51278fbce7c92a3297dc8"
     sha256 cellar: :any_skip_relocation, big_sur:        "9424472f8baa2352fb20c553257091ee0c5b3b44344678f4c812ff2f745bf0dd"
+    sha256 cellar: :any_skip_relocation, catalina:       "af1730a74106f8eab864433878f9e60217aeef539dcae12a77d8f812ede3d946"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "124a554e2865c53095f3bbfbac2e33efafc346bcb5949eca8eed3e34130237d5"
   end
 
