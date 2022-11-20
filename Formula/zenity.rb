811f47663f5c64c5a41506302572a04b40295271
FX Coudert <fxcoudert@gmail.com>
2022-11-20 12:50:15 +0000

    zenity: update 3.42.1 bottle.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -9,6 +9,7 @@ class Zenity < Formula
     sha256 arm64_ventura:  "11f9ebc29350cee0d9d7769c37f4ecbf36c9484061e6705cfd5b51f599b19612"
     sha256 arm64_monterey: "a9e84c3b0eefa50aafb0f41cc6e0a9ccdce95ece584680c1d14591fd9fa4250c"
     sha256 arm64_big_sur:  "0df17f657645d0075244735559ee97fb35ea64356843f5a1be5bbb1f29472293"
+    sha256 ventura:        "67c6c5160dfa1556312abfd19bd34173a757d118071667c34bf2cdda98a680d5"
     sha256 monterey:       "c102812429cf49155840fd2aeccae7bda7aec45f26ebea12848b628aa4f44397"
     sha256 big_sur:        "e3b50cd62e794c4efe5355adb5eafabc4d93b2785a6fc1a8c638eb5bae681aee"
     sha256 catalina:       "98f1939e7cf78bc7a66329c20e84f0067009d3e7be13d9f3b2f2156ea11f5b26"
