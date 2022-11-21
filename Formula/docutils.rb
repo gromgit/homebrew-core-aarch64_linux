b3a4dd64b541c08af4220f80a7ae22cc2d2b1fd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-21 09:05:25 +0000

    docutils: update 0.19 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -6,14 +6,14 @@ class Docutils < Formula
   license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "10bee4ba96884f6b1b1bcfb9ae78f7ff7d923707e357bb9e4f3682a7ba8aebdc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b784e47113758f9c47cfa4567a26d862fc9d0921bd6b63565b093913f86607b4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b784e47113758f9c47cfa4567a26d862fc9d0921bd6b63565b093913f86607b4"
-    sha256 cellar: :any_skip_relocation, ventura:        "198f0738d9631da591e9da489c80b763dd7a9a343ff9035e9b5060988e45ca2c"
-    sha256 cellar: :any_skip_relocation, monterey:       "ae5ef91d15f574fd9adeb50a5b1023c45f56fb7eed85ac270fdd0fcc401b41e3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ae5ef91d15f574fd9adeb50a5b1023c45f56fb7eed85ac270fdd0fcc401b41e3"
-    sha256 cellar: :any_skip_relocation, catalina:       "ae5ef91d15f574fd9adeb50a5b1023c45f56fb7eed85ac270fdd0fcc401b41e3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a51a4a455f0f93c4020aa5c692df0cbee810a4967ca38708591cb91d584fe7c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "68d49b7adbd286d79e538b1d139fccbd16d860fb2711409e9058d53d8f5b456e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "68d49b7adbd286d79e538b1d139fccbd16d860fb2711409e9058d53d8f5b456e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68d49b7adbd286d79e538b1d139fccbd16d860fb2711409e9058d53d8f5b456e"
+    sha256 cellar: :any_skip_relocation, monterey:       "63d91b072dde1c302435f1c6eea482d3a10cd83d109d2dd12adf192de28ca55f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "63d91b072dde1c302435f1c6eea482d3a10cd83d109d2dd12adf192de28ca55f"
+    sha256 cellar: :any_skip_relocation, catalina:       "63d91b072dde1c302435f1c6eea482d3a10cd83d109d2dd12adf192de28ca55f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63d91b072dde1c302435f1c6eea482d3a10cd83d109d2dd12adf192de28ca55f"
   end
 
   depends_on "python@3.10" => [:build, :test]
