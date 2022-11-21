eefbc97bade2497c92ad0a94b9da93243bcf0257
Patrick Linnane <patrick@linnane.io>
2022-11-21 10:04:19 +0000

    juliaup: update 1.7.24 bottle.

--- Formula/juliaup.rb
+++ Formula/juliaup.rb
@@ -15,6 +15,7 @@ class Juliaup < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "897bab2cf73716fe38fa7af9d30c3584902eefafde86940f40869931d868a320"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "06198c848a74caa308fe41f1e0418cb48a32a7ff39d7ea2f7556cfaf52f4fd7e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7fbf95fa78b98d7df7751bdd72ff6df681892b351f7fa529167a42a8ce5a3e1c"
+    sha256 cellar: :any_skip_relocation, ventura:        "d3c0f0d18bf614d891b9be2e5c88f804f267b678dd87a3f3795ee7a4876086a9"
     sha256 cellar: :any_skip_relocation, monterey:       "b6e50d1cf0b96f9d4b67f008b9a29bfc1ad144e1ac3e5a81d2fc1ef3ea2187d9"
     sha256 cellar: :any_skip_relocation, big_sur:        "d70d07a164e243ad2285a1dfed26edb055d4b5d421881593022d6194a99fa068"
     sha256 cellar: :any_skip_relocation, catalina:       "925f6332510eab4099ed9480971ccfb48241b9ff6cc2858cdd21aab3d618ccfe"
