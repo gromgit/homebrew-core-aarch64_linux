a8101d04801c67402d61b13c286a0c4b806bb248
Porkepix <porkepix@gmail.com>
2022-11-21 01:55:29 +0100

    zydis 4.0.0
    Closes #116276.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zydis.rb
+++ Formula/zydis.rb
@@ -1,20 +1,11 @@
 class Zydis < Formula
   desc "Fast and lightweight x86/x86_64 disassembler library"
   homepage "https://zydis.re"
+  url "https://github.com/zyantific/zydis.git",
+      tag:      "v4.0.0",
+      revision: "1ba75aeefae37094c7be8eba07ff81d4fe0f1f20"
   license "MIT"
   head "https://github.com/zyantific/zydis.git", branch: "master"
-
-  stable do
-    url "https://github.com/zyantific/zydis.git",
-        tag:      "v3.2.1",
-        revision: "4022f22f9280650082a9480519c86a6e2afde2f3"
-
-    # Fix build on ARM Monterey. Remove in the next release.
-    patch do
-      url "https://github.com/zyantific/zydis/commit/29bb0163342b782b0c07134f989c0a9bb76beec0.patch?full_index=1"
-      sha256 "8a23636bee945f9397367c65b4c0559e33f40a7650942047f5aca5c18b3601f6"
-    end
-  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "01dd5cd1a0e66fb458faa8943ac9a2e3d210448bfcc82a98a322c4923ef117ec"
