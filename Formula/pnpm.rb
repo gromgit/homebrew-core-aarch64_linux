e4a3c80e986e0bd4639a1eb5f3c9b38cc0c68895
Porkepix <porkepix@gmail.com>
2022-11-21 01:30:43 +0100

    pnpm 7.17.0
    Closes #116270.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -3,8 +3,8 @@ class Pnpm < Formula
 
   desc "📦🚀 Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-7.16.1.tgz"
-  sha256 "843ecd1e70f627c2a2071b5b7c7b1920576665d23344f145c557a847df72eb58"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-7.17.0.tgz"
+  sha256 "0569c3da6c6a849c15a00b7d4468ae35694c9112fd9304ebb34f7fc3959aad8c"
   license "MIT"
 
   livecheck do
