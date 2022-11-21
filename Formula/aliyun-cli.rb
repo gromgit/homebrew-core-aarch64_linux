747d08bd3f6e9d92e471b50a6ef14eef5424aec3
FX Coudert <fxcoudert@gmail.com>
2022-11-21 19:58:03 +0000

    aliyun-cli: update 3.0.136 bottle.

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -10,6 +10,7 @@ class AliyunCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6edb75030869900d17f8d7f242b9f3af91c2aea0a40098ba294832ba50967c1d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6671d2185d4c36bd80aa005b0c2aab2aabd5875a9782a47aaac5bdf5c0e7ae9e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b8268dff82c04ee6d966befebbbcee89f4a02fa51b6d6f99f8115059dcc31b47"
+    sha256 cellar: :any_skip_relocation, ventura:        "1599122023e855c2298a53d3e259998e18576130e19da39f83928b3e45dddc54"
     sha256 cellar: :any_skip_relocation, monterey:       "88c6f26a205aafb9d71e73aa18731cb803e0db846f1d218442ed79e07df62a82"
     sha256 cellar: :any_skip_relocation, big_sur:        "b9c4a05650292739c380e1555151a64bd9bb900fd07d4e2a73ddc7497f3e2300"
     sha256 cellar: :any_skip_relocation, catalina:       "95b638bbfdbb18f27308491acd985e079d317c86ed3d98f3732b658f45fe724d"
