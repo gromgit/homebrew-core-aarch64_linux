3153c4986aa0612055b1e627dbc786170d598ba3
FX Coudert <fxcoudert@gmail.com>
2022-11-21 21:30:33 +0000

    knot: update 3.2.3 bottle.

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -14,6 +14,7 @@ class Knot < Formula
     sha256 arm64_ventura:  "74efad115384d07983a418f7567171ab8fca9c645a369c9551a3a488810ddfdb"
     sha256 arm64_monterey: "d0954dff5df826f34231cdc3c0d8189eef3661bd03c1c83933e29180442afad0"
     sha256 arm64_big_sur:  "435fca1b64b2f672a227c0333a94c9a8e4dfe50b0ed9fee9c5ab1335d8f957d9"
+    sha256 ventura:        "889e8c48c7bc74ddab94c50631df55ba653d1d03b003fc50972f81d3e354e879"
     sha256 monterey:       "20cd6b962e97cd3f5a5227ebfa6d689c51aa660dc275d9e9d81c0748fd8551d7"
     sha256 big_sur:        "8c2f4b267c6f1b5b0c4f979c9434430e963df049dc964af459d7ba17233ef607"
     sha256 catalina:       "01b7a4bf22b85a66714d2675adf0db53e25e5e73ba55c4ae2ed93dc0bc2039a0"
