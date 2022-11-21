81de5b6e5f173dd33b7113072280e1b30ff53a04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-21 05:05:55 +0000

    b3sum 1.3.2
    Closes #116306.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/b3sum.rb
+++ Formula/b3sum.rb
@@ -1,8 +1,8 @@
 class B3sum < Formula
   desc "BLAKE3 cryptographic hash function"
   homepage "https://github.com/BLAKE3-team/BLAKE3"
-  url "https://github.com/BLAKE3-team/BLAKE3/archive/1.3.1.tar.gz"
-  sha256 "112becf0983b5c83efff07f20b458f2dbcdbd768fd46502e7ddd831b83550109"
+  url "https://github.com/BLAKE3-team/BLAKE3/archive/1.3.2.tar.gz"
+  sha256 "31c1033009c55fdacafc87e0aaecf9e136fed3ccec6c637ed2461e487240b7da"
   license "CC0-1.0"
 
   bottle do
