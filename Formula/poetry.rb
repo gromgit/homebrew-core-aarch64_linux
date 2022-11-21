ca7821db70df3e274c78d2ae6a82ca5280eeab3d
Branch Vincent <branchevincent@gmail.com>
2022-11-21 13:54:28 -0500

    poetry: revision bump (virtualenv)

--- Formula/poetry.rb
+++ Formula/poetry.rb
@@ -6,6 +6,7 @@ class Poetry < Formula
   url "https://files.pythonhosted.org/packages/98/4b/35736a678ef820e71e5ba895e853bc0e93eaa69c5621f47a559ce21bc588/poetry-1.2.2.tar.gz"
   sha256 "6d9ed0b1b826a0a79190f2078d7d78483fa24bf2494f3b170e354eaa5e7b5ea1"
   license "MIT"
+  revision 1
   head "https://github.com/python-poetry/poetry.git", branch: "master"
 
   bottle do
