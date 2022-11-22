0a182c6b0b1241d9e9eae838bc5e06a007779f6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-22 21:05:12 +0000

    rubyfmt: update 0.8.0 bottle.

--- Formula/rubyfmt.rb
+++ Formula/rubyfmt.rb
@@ -8,13 +8,13 @@ class Rubyfmt < Formula
   head "https://github.com/penelopezone/rubyfmt.git", branch: "trunk"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "93a5eab5b30e0ff0728557a9f51ce501cc9454fb7c6616b89f5a41d7be96e493"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bcdd675a98a38a40e6fa8a7bbb780524665c863bd3fefda5e222d952363630a2"
-    sha256 cellar: :any_skip_relocation, catalina:      "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b"
-    sha256 cellar: :any_skip_relocation, mojave:        "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac11feb657dbe095a59e0bbbe1e8adbd8fe5eaa6dd989e36204780697b200e92"
-    sha256 cellar: :any_skip_relocation, all:           "ac11feb657dbe095a59e0bbbe1e8adbd8fe5eaa6dd989e36204780697b200e92"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2f218d7b1b26f6a4818d1d398f103b68a1d6fe75fca7077e8781f45033cf3d4b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d839898bf5809b19cb31bed03f174c2d335193daf0ae3f7eba722d3c075df6d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc069ca891bc56b61dc47a060329a14376baf59fd3f207aa50e831555f28aa86"
+    sha256 cellar: :any_skip_relocation, monterey:       "ef4a822468573167fe1cea652bdddd42d9c61547f639fe2518d672e88bf37e55"
+    sha256 cellar: :any_skip_relocation, big_sur:        "869db78a409c39560aff03c6e54c99c344c35d6dd1d43705ba2ac086f53e0af3"
+    sha256 cellar: :any_skip_relocation, catalina:       "7206819d3053e1d6dbe1b194f5eeb1550a6b88f7c899ec0b533a7aed93ac6bc9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d512052245d378b1d6b9fc9cc04783bd26cbb70980cc530f81f7e4f36c23d80f"
   end
 
   depends_on "autoconf" => :build
