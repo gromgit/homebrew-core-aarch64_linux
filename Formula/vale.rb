ed0132199b50831eaa73b301e4203b09305b431b
Patrick Linnane <patrick@linnane.io>
2022-11-22 05:19:43 +0000

    vale: update 2.21.1 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -9,6 +9,7 @@ class Vale < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "335f1455b48f22d15ac71e1baff2f2769f6f7833168034900e7e059ad515600b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "eb646a0fb6ab33c1b1c688bb377ade7c61f8fe0668ebb74dbcc5175f72ea0312"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9a58fe03bccc75f05b68236952d8bb3d921d11a1f2f30408a84192e70837f295"
+    sha256 cellar: :any_skip_relocation, ventura:        "668d93aa20be09cbd036e9d94d504d4976e3e1a6fa32f20904d99c3990e40a9a"
     sha256 cellar: :any_skip_relocation, monterey:       "8d1626b8384d727e608a93b125cafeb647ac2e64a0d59b1269cc194bddc02a80"
     sha256 cellar: :any_skip_relocation, big_sur:        "4ad77ae89f7f0aa0cd3c110e83ffbbe763d8cb1337826f083865b1808613124b"
     sha256 cellar: :any_skip_relocation, catalina:       "eb974e2d460b53b7e08b9d76a6877db46d768565c3fa27c926b5a5564e032bf8"
