dd7085f2b93c262c89a8c271399a2cc24bfe0acd
FX Coudert <fxcoudert@gmail.com>
2022-11-22 19:01:29 +0000

    libproxy: update 0.4.17_1 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -12,6 +12,7 @@ class Libproxy < Formula
     sha256 arm64_ventura:  "d4df7a0e2c40acb852625f0a5a70b1c737fd5c13203787a929d1bb20f49f3190"
     sha256 arm64_monterey: "1de17b75f1c12ddacec2ace3a6152586bd49bdeeac07982b000c83486d3adeb3"
     sha256 arm64_big_sur:  "54df3618c53e7f3a55441b74319aa2c32ea68ee1d30a8287b8ad3f9cf58b8f8d"
+    sha256 ventura:        "2c27a8613e1b335043ef94a4233553a04cefc47caf5fe0a44de7758ee8e2aeea"
     sha256 monterey:       "a74b532012e5169309a1ef4226792f3d7533204ee33bfccd5863340041f9a71c"
     sha256 big_sur:        "f4fe74825ca139acd6cef52317eade9511f2ff90b5b584b0ca1706729ad6cc8d"
     sha256 catalina:       "f9946d8dee8b0915e160343b112259e5bba99bfb28f51364bf17a9bf6aabb6eb"
