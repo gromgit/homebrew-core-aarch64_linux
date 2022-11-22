be60c028658ae662b0b27b6d5f0a35c8e7c016e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-22 01:35:05 +0000

    i2p: update 2.0.0 bottle.

--- Formula/i2p.rb
+++ Formula/i2p.rb
@@ -11,13 +11,13 @@ class I2p < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "56959fa72e0b14e862102993293bf90c0b0a16d5a467fc0aae673da503381ddd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ebb408953a0e87c23a9cb838bf833a96c3afdc2313036e2520dbbf2bbbd287dc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24830afa2fe1094e634033d61bb38082cd70807498b096a03a9d5fb5b71447c2"
-    sha256 cellar: :any_skip_relocation, monterey:       "75ad64bb78affab0ec5b4fe2290be379c5f5eec646fc4131415dfacc1e9b2797"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e4605f09824480a90857b4ae2a024454a77d693143914a70df7605ed072e8029"
-    sha256 cellar: :any_skip_relocation, catalina:       "0f1ecc018b386c097e962716ce5c9a7142b5f5d10a99c4ddde2de4b91d2235ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b174754721de5e5cbd0d8070764cc2b7e8f59ebe19911288c99398668f0c5b7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "45a65eb4b71fb6ed5df5b186d8f0a5e48ba4056df946e3b03b19ab21914f6c26"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "76d4d5f640e34d9aa5dae8fd4fce1510306b4211c86c46a7c9dd540e4bf5f3bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "74650f2d87ef6817bf8e615f820c73f3fcaacdcb23bba6c462bfb98778993c92"
+    sha256 cellar: :any_skip_relocation, monterey:       "9869a854557471178ceaddb9525ea4f0d26471475b267a8aa2e1385a3e2757ee"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9a31977f6a664aeeea23ec2de5058a9df1547894657b73b08ae99d39804a99a6"
+    sha256 cellar: :any_skip_relocation, catalina:       "5018081f9e3b21e86cfb41d18c2cf0b9d9a93b71126fa369f35f9af35c8b6d8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4480a759a0f64a3ff7d7433696b065e7d599633c3bb3f15c99692bf29a519b7b"
   end
 
   depends_on "ant" => :build
