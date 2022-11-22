c7ec409debb30037bd66ea9a7b303cb1fb4b20f1
FX Coudert <fxcoudert@gmail.com>
2022-11-22 18:45:45 +0000

    pspg: update 5.5.12 bottle.

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -10,6 +10,7 @@ class Pspg < Formula
     sha256 cellar: :any,                 arm64_ventura:  "dad5957c20e6d2fa172de78f8a17ed68a96344333e871302bc20b366882f6ef1"
     sha256 cellar: :any,                 arm64_monterey: "03539b714f5c6855e9f56eca72a06e967eca97c19a26f3f89b15d008a76a2cc9"
     sha256 cellar: :any,                 arm64_big_sur:  "1965c707b8b8466dba4b9df7ba543fdfb2699fc5020fc5b6be5d3fb12416b54d"
+    sha256 cellar: :any,                 ventura:        "efdfb6fa9006e85322ade93db0a7b9e664be5212080e6292c926cca88fa6e9d9"
     sha256 cellar: :any,                 monterey:       "c36ebec9b72a04a5392ab59082b0de959d20222c9a692b096299c91aecf63db5"
     sha256 cellar: :any,                 big_sur:        "af2df65f4259244fe51f30e2d0682a7bafb9e6f5548f7ee295a516ba2fa01d75"
     sha256 cellar: :any,                 catalina:       "09df456842b33c0ecd8123c639393bec1c1d4936a08df505793b7ea9af6af452"
