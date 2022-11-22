0cb82b5093917e2f3844ea1267c4f3befad19d1b
FX Coudert <fxcoudert@gmail.com>
2022-11-22 18:43:54 +0000

    libcoap: update 4.3.1 bottle.

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -9,6 +9,7 @@ class Libcoap < Formula
     sha256 cellar: :any,                 arm64_ventura:  "3bad1beba15ee306b2b2f6245d889aef8c9477ce52f6eb5223338d1390b0e156"
     sha256 cellar: :any,                 arm64_monterey: "7dd5d4c8918ca7bf9f05c86d020ccfda6fd9cbab6770aa8b693c818b3c4155cc"
     sha256 cellar: :any,                 arm64_big_sur:  "876e1c10f9a7fab51e2462db44e95b23f1d8ac0085c9cfe02b11181ea64988f0"
+    sha256 cellar: :any,                 ventura:        "71f33b9d774289ee9197f4b4cb6568051732abc784ec69ee550a8973c07b071c"
     sha256 cellar: :any,                 monterey:       "f275fc1b8e5aca8b3c179ab40d9a30058df7a501e62530dcd2f5fdf8d47e09c7"
     sha256 cellar: :any,                 big_sur:        "ff567b66ad0ad10162c4f54561a4a0eaf97fe562efb6ff6b68e72c3973b3177d"
     sha256 cellar: :any,                 catalina:       "b20f8aa33cdb7f91bff0215748b7c14f10952e7b03a3c56bb9f021209489afc4"
