0f4599da94bffe8dff9d63b552bc8525767f32d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-23 09:05:07 +0000

    zabbix-cli: update 2.3.1 bottle.

--- Formula/zabbix-cli.rb
+++ Formula/zabbix-cli.rb
@@ -14,14 +14,13 @@ class ZabbixCli < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "43721eecd306e7a65ab883d5567e59eaf57380a02d00f1c9d4c4a9502f4c2a49"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ae0b87404c2c6f6242137ae906afa68afdd66bcc1deb64163ad3f2f3217308d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "79d7ce0ae4767400dc2c7c3219bdd4ba569677e16963e652d3ca7023447154e3"
-    sha256 cellar: :any_skip_relocation, monterey:       "ad323071b6f0426221d3a039a5ba792227c4580827b6dc190a25757f70be4eaa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1a91cd80a519730d0f6f88780018e340f255b48d0b75d94848a9bb412e3277ab"
-    sha256 cellar: :any_skip_relocation, catalina:       "f57a99ba9ea40c21b9f4087a0bbd40277d826e3b81ea7b71414ab640ace2b16a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c6eb4257b29e2c6dd27a7b7ddebb16082e849ee0a4d8c089ff943376d042b83"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "54d0f8f076a88e46ce0c7a46a95f7916208320b25acdd9d53c5cc883bb404d78"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a17c1846fbb86ed35ff45fe93afa0d68be89ba302bd95692223f9a5734b865d2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d50c80e8d4f1850ee8e4588404ad6e018fe538ee328a14b3f0567e2e08ff7109"
+    sha256 cellar: :any_skip_relocation, monterey:       "eb09b94bf7ae3692ee2bb80f2d6f9ee22e383895deccc56ba59c9930ff4386a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eee58916c80f877ca81f7f978f8ef3d4db59f90437ced3dedf8dde77a1bf443b"
+    sha256 cellar: :any_skip_relocation, catalina:       "e695321a2879cd0a7169048c64bdea06c0c761d5dfc8bdbbfcfd565ea0747253"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1671d0a1d45f24959021a3df47fa01a2f62c7b6122df466d72805c063b4122a"
   end
 
   depends_on "python@3.11"
