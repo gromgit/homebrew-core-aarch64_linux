bb8bea5d442b7607621dfb16716ad43cb5f3fe4b
Rui Chen <rui@chenrui.dev>
2022-11-23 14:21:29 -0500

    dart-sdk 2.18.5
    * dart-sdk 2.18.5
    * dart-sdk: update depot-tools
    
    Closes #116529.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dart-sdk.rb
+++ Formula/dart-sdk.rb
@@ -1,8 +1,8 @@
 class DartSdk < Formula
   desc "Dart Language SDK, including the VM, dart2js, core libraries, and more"
   homepage "https://dart.dev"
-  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.18.4.tar.gz"
-  sha256 "6644c8e1ce88953bd3364a29de0dab321b21fb9a7af6a22e965d4f2afbd64d4e"
+  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.18.5.tar.gz"
+  sha256 "81bbc28a148fe147676a8dde1dae4579cd7e760be60c332c2dfd3dcfbade0a93"
   license "BSD-3-Clause"
 
   bottle do
@@ -24,7 +24,7 @@ class DartSdk < Formula
 
   resource "depot-tools" do
     url "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
-        revision: "bd0cea6acd9ed0476b6634b08da740093715a654"
+        revision: "6fde0fbe9226ae3fc9f5c709adb93249924e5c49"
   end
 
   def install
