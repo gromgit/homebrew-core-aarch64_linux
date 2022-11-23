ed906e8200f3f41b0294a3f9837f5294f99a3e52
Patrick Linnane <patrick@linnane.io>
2022-11-23 06:25:17 +0000

    ipfs: update 0.17.0 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -19,6 +19,7 @@ class Ipfs < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "515c219fee5f14f9361f39445e621817850280bd2b7bbf632db2584dfc4d2806"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "870140073dbb06eb5fa7acc801e53ea7369625cd49bef153c51cbf3e83834e14"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "135b436490698fe193b5b5c0827364ae8c2bb99d3c00f34b2a1a92af2dcc5e6f"
+    sha256 cellar: :any_skip_relocation, ventura:        "954b565bfd51172f4a72ffeda5bc1dbe6b7cb6124e1746fbd67acf67029151a1"
     sha256 cellar: :any_skip_relocation, monterey:       "7eac91f7772537d1ed23e762f2be055be84db2a1cef5fa06066343cc521e1f8f"
     sha256 cellar: :any_skip_relocation, big_sur:        "d95e0b7129b7903036e964e36d4de3a889a3558d631bf8168babc1b9c6e4dfc0"
     sha256 cellar: :any_skip_relocation, catalina:       "fd98175231e48366e4ee4c17c79fa4fd350444b83127581c4efa49bb81eec5c3"
