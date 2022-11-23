b7a371c84783dd7ff790a5376de00c3b9b14b784
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-23 00:15:21 +0000

    pipgrip: update 0.9.0 bottle.

--- Formula/pipgrip.rb
+++ Formula/pipgrip.rb
@@ -8,14 +8,13 @@ class Pipgrip < Formula
   license "BSD-3-Clause"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a208544fc56d66489695b0b2b69071d5aaf7d1029b0eb28e4c27366e7235a6d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bd9bad7cdb008507a2a9d8b23a7f8533b444f68a1519ad8acb01d276b4f06b56"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c48897863c4051661461838e47d76d6b1c8bfde721f845e7ea88ef69bd06c415"
-    sha256 cellar: :any_skip_relocation, monterey:       "23bdda846347c2c012e6c9d39ab8f5195c09ac2eacc52420898a4bbb9c0ca52e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2c94a4bd9dafa11e5f20b63d060ee33a213c0768e1c15c20244558b304639ee7"
-    sha256 cellar: :any_skip_relocation, catalina:       "607fa87cef9d62d7d92662bceac994712217513a4d822ae79f70654d0ba8167d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f271147998794b84762ca9ef4795c818265b1c2715717f6dc53a01ec85795228"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "218f1e988426b993e1ee10452e1b2f41662281626a45891c5fc5e2e07d5c6407"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1d5175748be36158724fbfbc8a9eb7612def2b9a5823e14094bbe996f06725a2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "021558d54f86eee573e95ed1e847d097db9e74f07f2683fba2b8be710d3dc19a"
+    sha256 cellar: :any_skip_relocation, monterey:       "828b59d8d05719d3f515c0cb7541182c982e67460ff9aa06e6a6dbbb3751936a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8f0c82c246b2ebc60730d12568f1504137fdcfa6bb043481a14513187912432e"
+    sha256 cellar: :any_skip_relocation, catalina:       "b0df7634094461a0771179b36d06cbed4f037256fbfed0acae2bf938ad45f10c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c05ef8978641347e5a420970ccafc5be0a0c1896078e447e5b8a66949a237501"
   end
 
   depends_on "python@3.11"
