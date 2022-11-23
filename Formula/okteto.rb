b980ba78065f4b2840f80c52b1f9e03b6fb99808
Patrick Linnane <patrick@linnane.io>
2022-11-23 06:33:50 +0000

    okteto: update 2.9.1 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -10,6 +10,7 @@ class Okteto < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "cc287634b987d09dccc5c2606fb9e8ee34162d66c2fcf7c84da97bcec9607379"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7f00d6c91b032189e6406962e8fda0e10f141a3d0c785c3d4459bef06e1f5c84"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f87d661aa7b1d3adb18f2299df3d5bf0439388e791661bd3b6c1cf84ad0e3f1a"
+    sha256 cellar: :any_skip_relocation, ventura:        "d5274cd69ebcd03d7ec136ed8b28c962b9192ab2b6c576e73ebab019d3e2829e"
     sha256 cellar: :any_skip_relocation, monterey:       "2c66c729b61f44c485f4f583201915c9f49ebf38999fd8a2301ed5fe877a0bff"
     sha256 cellar: :any_skip_relocation, big_sur:        "13650533ca0aeec61e0434dce0c6ae14d041f7f5a80bbbe767d586c25ed9f3ea"
     sha256 cellar: :any_skip_relocation, catalina:       "59e4d16398874f0c3dc4b32bca650abde39a1a2f57da52ab138f0d8980b0851a"
