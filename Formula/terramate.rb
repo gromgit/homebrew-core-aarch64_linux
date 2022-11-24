8f05632777f34c5a84c68b56c79fb94c50e8f34d
FX Coudert <fxcoudert@gmail.com>
2022-11-24 16:48:20 +0000

    terramate: update 0.2.1 bottle.

--- Formula/terramate.rb
+++ Formula/terramate.rb
@@ -10,6 +10,7 @@ class Terramate < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e318ccdb5a5e4ed0f8699bd9225d6a379af63dfae7fc218444a8a47eded77648"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "37d6313c56cb66044e8dc249f523343e85816940356d8b1e34df978bd42a42eb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "814c764e5c8ed4457d1449a8053994599d177688054350e8edf657419de63fc7"
+    sha256 cellar: :any_skip_relocation, ventura:        "c5966f73fff3a07f580d8fbb8e5e2aa7c37a3a8c7eb3b0d56674baec9331ff7e"
     sha256 cellar: :any_skip_relocation, monterey:       "5b3ac4bcf5ed4568a50872a3c5e59d5f6d069aa1a065b635cf499a9e9915b44d"
     sha256 cellar: :any_skip_relocation, big_sur:        "2a6855af1d6bd510062074ed8858c84c00742b18f4b97790481ce7055d5505f9"
     sha256 cellar: :any_skip_relocation, catalina:       "69c6af6ec43d25437b5786df8a80be4a06c0b281c8b3afd36fc5dd0abd0ac72a"
