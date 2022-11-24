c1217e2404c0cd1115b26cde86f1d30d2db22d45
FX Coudert <fxcoudert@gmail.com>
2022-11-24 09:09:07 +0000

    oras: update 0.16.0 bottle.

--- Formula/oras.rb
+++ Formula/oras.rb
@@ -10,6 +10,7 @@ class Oras < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c27f931c71ab78701e8d91d56fa88695bde78840ccef66fb8ada14c9dcadf2fd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b3802c90a5cdd03d1ccfa22523fa9285d4085abf18851a8658eb014c4dbcbe0e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a740997cc8c15d73aee2584e346a1438bdff5782a127dfd66d7cb0d02b803843"
+    sha256 cellar: :any_skip_relocation, ventura:        "8700ad4b886f826426047691f37b596979a8a7908f3e673dfdd9b52ad0aa8317"
     sha256 cellar: :any_skip_relocation, monterey:       "6e5ea3a26af5c37badf8703a0aad7948dafba5359b9f6a90b107957a0088a36c"
     sha256 cellar: :any_skip_relocation, big_sur:        "b1733afa2e23e7fe8d927df04cf25f5eb99a2f27f3664bc17ab68b0228588db9"
     sha256 cellar: :any_skip_relocation, catalina:       "183a997ecf29e06fb92704d3a09cad4468284bbf9f99ee2a4bfdcf289a563936"
