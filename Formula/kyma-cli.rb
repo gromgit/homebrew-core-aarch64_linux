b121daff10dbbac11484ebeea67a5a86c6a46cc8
FX Coudert <fxcoudert@gmail.com>
2022-11-24 00:34:51 +0000

    kyma-cli: update 2.9.0 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -10,6 +10,7 @@ class KymaCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ecb8b57695e22d5fef1529e1a40f2626f46705af6c1186034d165fea4f7cb730"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4979be0b6997b2abb0813f3f45338f44c2c7505b2af7d645b9d9f5109acba682"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6d0136524adcf0c38f0d73c0ca24f7eb509940b80335d229830c2b0e7b3dd286"
+    sha256 cellar: :any_skip_relocation, ventura:        "6c51074375fb60aeb62dd5f4a869c0ac3320e48fa17ade7a00a573c97d7f96a6"
     sha256 cellar: :any_skip_relocation, monterey:       "7800230afca74f02a30beab989c61d070fd893706d8123c26a80543667e59a55"
     sha256 cellar: :any_skip_relocation, big_sur:        "d206c2e01f136ce11e61fda14607c151f993a193e1d4014f61d67fce2df5dc81"
     sha256 cellar: :any_skip_relocation, catalina:       "88f8a87f8233b23a1a617773d6db4f227751a0a1f3aad04385ea1e1c85fa78e1"
