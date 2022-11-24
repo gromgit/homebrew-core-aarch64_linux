a96a6aec7f91cc0af38f1af6bfd6a9fa5b9d69c5
FX Coudert <fxcoudert@gmail.com>
2022-11-24 15:59:36 +0000

    flatbuffers: update 22.11.23 bottle.

--- Formula/flatbuffers.rb
+++ Formula/flatbuffers.rb
@@ -15,6 +15,7 @@ class Flatbuffers < Formula
     sha256 cellar: :any,                 arm64_ventura:  "a8da20e54eac4faa2871abe2bb8f99b183b25083328adf2b9d1cb262a69eac57"
     sha256 cellar: :any,                 arm64_monterey: "f844a16cacc5c337ce88a1afe6f7fbea2db98c63a63085281c47da9337decb3a"
     sha256 cellar: :any,                 arm64_big_sur:  "96264bfdb14634ef2712397b2bbcbc5c98ab54ce82ecc8d49aa0b520b394b883"
+    sha256 cellar: :any,                 ventura:        "65bb7edad4778afe70173c252d40363af4e8fc71c9d1a4ec42a92b314f92114d"
     sha256 cellar: :any,                 monterey:       "997d22279049f9e39844c80f316f72a169a8d5d13e4a5e796dd49d4a8f98a9f0"
     sha256 cellar: :any,                 big_sur:        "b5a652182350c30e246cf7917e92768ef6e337e013d83f7d01091ffeb6dd3610"
     sha256 cellar: :any,                 catalina:       "9b6835be6c9cd1b624b3ce93ac85b9104e27c0ed49b91b4f40044cdb18066195"
