2d33892265ba5a1a86b533cd8a401118e70466a1
Rui Chen <rui@chenrui.dev>
2022-11-24 12:11:23 -0500

    apktool 2.7.0
    * apktool 2.7.0
    * Update apktool.rb
    
    Closes #116644.
    
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apktool.rb
+++ Formula/apktool.rb
@@ -1,8 +1,8 @@
 class Apktool < Formula
   desc "Tool for reverse engineering 3rd party, closed, binary Android apps"
   homepage "https://github.com/iBotPeaches/Apktool"
-  url "https://github.com/iBotPeaches/Apktool/releases/download/v2.6.1/apktool_2.6.1.jar"
-  sha256 "bc2b9a87ac5a86905b6ca343c21a0db3bc37bdd51154bc9cdf65523d95895d34"
+  url "https://github.com/iBotPeaches/Apktool/releases/download/v2.7.0/apktool_2.7.0.jar"
+  sha256 "c11b5eb518d9ac2ab18e959cbe087499079072b04d567cdcae5ceb447f9a7e7d"
   license "Apache-2.0"
 
   bottle do
@@ -11,7 +11,7 @@ class Apktool < Formula
 
   depends_on "openjdk"
 
-  resource "sample.apk" do
+  resource "homebrew-sample.apk" do
     url "https://github.com/downloads/stephanenicolas/RoboDemo/robodemo-sample-1.0.1.apk"
     sha256 "bf3ec04631339538c8edb97ebbd5262c3962c5873a2df9022385156c775eb81f"
   end
@@ -22,7 +22,7 @@ class Apktool < Formula
   end
 
   test do
-    resource("sample.apk").stage do
+    resource("homebrew-sample.apk").stage do
       system bin/"apktool", "d", "robodemo-sample-1.0.1.apk"
       system bin/"apktool", "b", "robodemo-sample-1.0.1"
     end
