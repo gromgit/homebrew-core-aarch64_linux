c2718c19ce9c4b70ccd988752554f38df3c49afa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-24 16:05:36 +0000

    pjproject: update 2.13 bottle.

--- Formula/pjproject.rb
+++ Formula/pjproject.rb
@@ -12,14 +12,13 @@ class Pjproject < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "2db71a49fb481857c6ce497a221f602fa07b7b6d08828c234038b718d5b1f404"
-    sha256 cellar: :any,                 arm64_monterey: "143bd078228ec92a6416969f04ead2de05973846e5a349bf7bb4dd46cac0424f"
-    sha256 cellar: :any,                 arm64_big_sur:  "b50e7a156e26862cfd37552756561a684c0a407ec2fc16e1352c5efc43a759cf"
-    sha256 cellar: :any,                 monterey:       "2fe4e4d2240639c150596b5cb8a9cd33f590f0c61bb3e543c05459350ceb9203"
-    sha256 cellar: :any,                 big_sur:        "fbeca53f3d5e13d3a768e47530e2b97f37b1d1d8826444be6d63636216380782"
-    sha256 cellar: :any,                 catalina:       "3b3d7981a5d9754c742fd5c052188d0dd0d5dfaff0152190560c0f1c22223e1f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa28b3a2157f3da4379c83e44fc08018ab97015a3f0102e5e9a00ab7fb1bc6cb"
+    sha256 cellar: :any,                 arm64_ventura:  "ccee5a9791d7286f541e9f59931f5c2193af1e0ca0ad189cb5dbe56826036ff5"
+    sha256 cellar: :any,                 arm64_monterey: "71ba504dd2255cfba6d1867d27add23f415a841c0dea2a96cced2f5868c0645a"
+    sha256 cellar: :any,                 arm64_big_sur:  "89e39245774453366e922ccca427a5ef2d70809a7fc952e89b095ba884830998"
+    sha256 cellar: :any,                 monterey:       "66b92f14f4d14a45e55bdf306311cb2bbd0fca3573e2aa03900d4c4b1d427e6d"
+    sha256 cellar: :any,                 big_sur:        "29ea64bce5012253aacc4ccf1cd43c8ec701ccf625739341817c6d452a9cc44e"
+    sha256 cellar: :any,                 catalina:       "7265ffaa44bdc67e47721c12d80cf7e7d213f47a393d8923ceadeaee9968e68e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "026e78e19b27cdba146d7e90399a0e696dde30c8854bebe031cfecf622126104"
   end
 
   depends_on macos: :high_sierra # Uses Security framework API enum cases introduced in 10.13.4
