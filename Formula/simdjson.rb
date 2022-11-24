88461f980c4e74144f6e41b8274597d63fa9b8c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-24 04:04:36 +0000

    simdjson: update 3.0.1 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -7,14 +7,13 @@ class Simdjson < Formula
   head "https://github.com/simdjson/simdjson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "914296a1631b0978196c709e7b331fbb47493ee2f29cd861e7c28f43a3962fb1"
-    sha256 cellar: :any,                 arm64_monterey: "97121057418c1413ac5e1dff59f38698aea474d53663ffc44ddca58e7bd9fc51"
-    sha256 cellar: :any,                 arm64_big_sur:  "8b1bef7847d9ab5e3435a99041010aaab98ca2be2af078337d385ea9427af28e"
-    sha256 cellar: :any,                 ventura:        "2f0ef03cf1c51b95be774a4b30a56b0127a9e523175a75f99e6b484ee45c8d91"
-    sha256 cellar: :any,                 monterey:       "9bf54fef4dd79b2f0d665d815073b4f895129128f0caf59879291a39eebb929d"
-    sha256 cellar: :any,                 big_sur:        "c5fa9704b487468f233542ae100fee187891291dff86fcaa946cc04c4784b86f"
-    sha256 cellar: :any,                 catalina:       "7e194d8eed7911f89856a315fdbc4ad5dbc397991717234ed7ecdbb93fd673b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2418ad1291706d139fa7c2028c8e3f5dea071f8259fa341fcbf2a328d3cab3da"
+    sha256 cellar: :any,                 arm64_ventura:  "80b5cd728a271af924f7e743cd416e69b11009005734bef73ae5ccc8c729171f"
+    sha256 cellar: :any,                 arm64_monterey: "5f901929ffcfa87d9943255a6ce3bc7e9cec00bdec4b318e235e4bbe5c0ae203"
+    sha256 cellar: :any,                 arm64_big_sur:  "63bbb93cdbfa02d0f047399ad514a29a707ffc0b2203ef944500ca4995c39d20"
+    sha256 cellar: :any,                 monterey:       "29ae95d05ab9363c1b2eda0bc6e252f09f00cd0f1d41821df0a07eb58b8dc107"
+    sha256 cellar: :any,                 big_sur:        "8eb716b1b9061945789e959083d1689739946b0d9a69165765662230c6f2a420"
+    sha256 cellar: :any,                 catalina:       "b018b250baa67b306782f7de14df5b4071f13756b1ed873096dcb197fb5fedce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0596803f54131f2e190e8519260b75fd085fc911b01177313e02f2b80e2d4ee2"
   end
 
   depends_on "cmake" => :build
