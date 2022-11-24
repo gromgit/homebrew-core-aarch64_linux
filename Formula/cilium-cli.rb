8d8c846153a96c0e322083a6c95ca47c19117eab
FX Coudert <fxcoudert@gmail.com>
2022-11-24 15:56:59 +0000

    cilium-cli: update 0.12.9 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -9,6 +9,7 @@ class CiliumCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bfc0a4f5cfcd8c2344ec1886c8f9d2e890519b9da5eae63c9fd60fd4547c6f2c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ece4d0831ced56beb6de325eae4e65522ce3aa99856407810cb7f2a2c0ee1a41"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d88f39db63b64e123dc86d09015419395675353c5401ff6250a513b25da139b"
+    sha256 cellar: :any_skip_relocation, ventura:        "b5c3baa26441e71c95d0b8d440c2d823d4d5e57da1f0b781faf22bf07f22a830"
     sha256 cellar: :any_skip_relocation, monterey:       "4a904f662dc36eb174d874e2eff5f54fc46689431e6aef671f50674ce256d378"
     sha256 cellar: :any_skip_relocation, big_sur:        "c10645dd240786915bd191bacb3eb6336eb6e2934b61bc0cbc7d4cd21ca62b9d"
     sha256 cellar: :any_skip_relocation, catalina:       "db9c3266f3dd2636530c4ed842c87420c02be9cf4406e816acbecd59dc7e9b62"
