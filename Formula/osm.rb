80d92db92c14de62527f9f7e168747b1639508d8
FX Coudert <fxcoudert@gmail.com>
2022-11-24 09:16:35 +0000

    osm: update 1.2.2 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -17,6 +17,7 @@ class Osm < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a67e4b9ceeb29ada0d24bd724f963467e0d425adbcdffba389bb488fac366635"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "bcf117a35ed020fc6d409e738aff9acd67b21f71d94c0651f2d40bf4b75cc2bd"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f0f7248d81082f5fdbbb39af629ed8118d6cff3fabc6c5385bcb9beeafdfa677"
+    sha256 cellar: :any_skip_relocation, ventura:        "1b7b8a5565d55389f31809551eb02742c33af0c2befc190117394657dcf6e44d"
     sha256 cellar: :any_skip_relocation, monterey:       "8e3877838bf5232de9b27073ffa82d540f3211b2309becc5223d497c89d0441c"
     sha256 cellar: :any_skip_relocation, big_sur:        "b1b8f61bc1053eceb98f728676e270546932dbb7ff46a5518082eb0b8b854efb"
     sha256 cellar: :any_skip_relocation, catalina:       "626c9e2921e24712e50100fc5e736b49158a7111c31213d93bead8dc09ca20e8"
