decd01c7063be19678dcbc6fe9a503fff046874d
Rui Chen <rui@chenrui.dev>
2022-11-24 11:29:28 -0500

    gitlab-runner 15.6.1
    Closes #116634.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -2,8 +2,8 @@ class GitlabRunner < Formula
   desc "Official GitLab CI runner"
   homepage "https://gitlab.com/gitlab-org/gitlab-runner"
   url "https://gitlab.com/gitlab-org/gitlab-runner.git",
-      tag:      "v15.6.0",
-      revision: "b846829497b5fa1618d8270e687207356f795188"
+      tag:      "v15.6.1",
+      revision: "133d7e769491ed802f3d0bed68338d74357f6159"
   license "MIT"
   head "https://gitlab.com/gitlab-org/gitlab-runner.git", branch: "main"
 
