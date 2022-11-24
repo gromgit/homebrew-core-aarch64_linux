5150656c68c9762ed71a6d779cb217979b6511a9
FX Coudert <fxcoudert@gmail.com>
2022-11-24 09:16:08 +0000

    microplane: update 0.0.34 bottle.

--- Formula/microplane.rb
+++ Formula/microplane.rb
@@ -11,6 +11,7 @@ class Microplane < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bce2fccdf3bad8a263e334f78998abfa7e25153c3bd1c66fff8e538981bd481b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "895647aa25e00a690a137ab0fae64e72f075d6bedd3d0f9f9105acc7e3c5c90a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "77690629610337917ade2bca586af39148e89df15ce4b1887018c1fdec12fc7d"
+    sha256 cellar: :any_skip_relocation, ventura:        "6dc454b63246b9866a58af5a0766c074e504c050d38fd3437bdf3dbbc12211d5"
     sha256 cellar: :any_skip_relocation, monterey:       "a0e54ff13e444e476d6eea2798aa1966937488b4e8f2472be9cf024933b74604"
     sha256 cellar: :any_skip_relocation, big_sur:        "9b4d8f8734017c00dfb4c94d77b0eba42bc0daf01ab637a1d6b239d40b939daa"
     sha256 cellar: :any_skip_relocation, catalina:       "bf2395a35907393bb6603b764e1dd748752ca4cd4e93b64033a6c1942e4aa5b7"
