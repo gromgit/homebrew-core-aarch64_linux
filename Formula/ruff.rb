d417d24b268570660098f37042a32445f59abb79
Patrick Linnane <patrick@linnane.io>
2022-11-25 23:46:53 +0000

    ruff: add 0.0.137 bottle.

--- Formula/ruff.rb
+++ Formula/ruff.rb
@@ -6,6 +6,16 @@ class Ruff < Formula
   license "MIT"
   head "https://github.com/charliermarsh/ruff.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0a3ee732463b3a0ca9e5964ed36219e8d67717db3af28583f154a159843e07a0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7cb84684b1a7892cd1a95211b6b7c2e15314b2156fd53e5133741d0e63af5e6b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "009db5f562deb7a58381de27f131e980c059dda4137d0cd48b0f9d6ec385c4f8"
+    sha256 cellar: :any_skip_relocation, monterey:       "af7879a0d1ceb2c5b1b18cca25516ffcabb2000b56cb62e065a960d51074daee"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce12261c643633d496a37c5a58c477230ff990942d6e0f0596191416212751a6"
+    sha256 cellar: :any_skip_relocation, catalina:       "7b3e4e4b195a751b024f2c93caa7d2c5084d252f2579b387676ac1e3cf108923"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42091e475b83336b57121d2ec2fe96f8b0e2cfc21769ee963ea0eddffe0704e1"
+  end
+
   depends_on "rust" => :build
 
   def install
