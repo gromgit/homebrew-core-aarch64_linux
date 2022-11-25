d6a15ae9f29b69958825abb8377619e857b5dc4e
Porkepix <porkepix@gmail.com>
2022-11-25 02:40:48 +0100

    terragrunt 0.41.0
    Closes #116669.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.40.2.tar.gz"
-  sha256 "b93899520fc944d2a46abf5f310447a4886d059622f867d768e8ddaa3ca0f18b"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.41.0.tar.gz"
+  sha256 "4e57967af23be2ab2f06eb79eb0427b7d80e820eb8ba1df038f792708e5a0b43"
   license "MIT"
 
   bottle do
