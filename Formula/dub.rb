25d938aa52dd14ab4520c21a63d599d542e393e7
FX Coudert <fxcoudert@gmail.com>
2022-11-25 22:49:00 +0000

    dub: update 1.30.0 bottle.

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -16,6 +16,7 @@ class Dub < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "b4d65020cdde5fac667c6aefdb25968c4179eea7c776b2aaa9671cffd4edb9e0"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "83b94fba9b7886a0aca23cc3d11fd8add55a4c861c648b990998f2436a037585"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59bce14e8905a2f2dcb0ae35ed425755026defbfb7aabab69a8e93910188a809"
+    sha256 cellar: :any_skip_relocation, ventura:        "75a38f7d9e1d45009b29180ad96425e52bcd72731a6be8ca56476d4e3c77e0fc"
     sha256 cellar: :any_skip_relocation, monterey:       "ddfcf25c04859be3b74eeca843b1072b00a779deb408a12013dc26090b48c84b"
     sha256 cellar: :any_skip_relocation, big_sur:        "0031bae24192fbc918c4462dfa0ce5a539685f0cb4ed2bc1f85c1bdfb3d762c9"
     sha256 cellar: :any_skip_relocation, catalina:       "22c9691136ed173c667a0b2c4c2f598488ae260c3b6e47188696e9414f18b4c8"
