7e567ea7805fdb1cd293335810fdb16a9c9864d7
Porkepix <porkepix@gmail.com>
2022-11-25 14:29:55 +0100

    kube-ps1 0.8.0
    * kube-ps1 0.8.0
    * kube-ps1: update test
    
    Closes #116690.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kube-ps1.rb
+++ Formula/kube-ps1.rb
@@ -1,8 +1,8 @@
 class KubePs1 < Formula
   desc "Kubernetes prompt info for bash and zsh"
   homepage "https://github.com/jonmosco/kube-ps1"
-  url "https://github.com/jonmosco/kube-ps1/archive/v0.7.0.tar.gz"
-  sha256 "f5ccaf6537e944db5b9cf40d3f01cf99732dce5adaaaf840780aa38b1b030471"
+  url "https://github.com/jonmosco/kube-ps1/archive/v0.8.0.tar.gz"
+  sha256 "7e57dc42d60f6c18fc1c814800c74b0ffb4e6f9d8e2b53f6f40bd7076f6c50a0"
   license "Apache-2.0"
   head "https://github.com/jonmosco/kube-ps1.git", branch: "master"
 
@@ -28,7 +28,7 @@ class KubePs1 < Formula
 
   test do
     ENV["LC_CTYPE"] = "en_CA.UTF-8"
-    assert_equal "bash", shell_output("bash -c '. #{opt_share}/kube-ps1.sh && echo $KUBE_PS1_SHELL'").chomp
-    assert_match "zsh", shell_output("zsh -c '. #{opt_share}/kube-ps1.sh && echo $KUBE_PS1_SHELL'").chomp
+    assert_equal "bash", shell_output("bash -c '. #{opt_share}/kube-ps1.sh && echo $(_kube_ps1_shell_type)'").chomp
+    assert_match "zsh", shell_output("zsh -c '. #{opt_share}/kube-ps1.sh && echo $(_kube_ps1_shell_type)'").chomp
   end
 end
