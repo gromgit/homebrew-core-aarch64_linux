19eac85f9a18683f62fb541cdf4c458adf2ee98e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-25 03:20:58 +0000

    flyway: update 9.8.3 bottle.

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -11,7 +11,7 @@ class Flyway < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "495c52e7268d2b37893fbef3cc7a71e92c11a2b6e0d2e81186045f32133b54fc"
+    sha256 cellar: :any_skip_relocation, all: "24434504c7a5ff2667297a98d3725e0eea2203eb065e398ca38833295e336bb7"
   end
 
   depends_on "openjdk"
