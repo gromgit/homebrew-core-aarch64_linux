f269d9b7badd119fa6594839365fc185ad144133
Rui Chen <rui@chenrui.dev>
2022-11-25 18:43:38 -0500

    vim 9.0.0950
    Closes #116725.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v9.0.0900.tar.gz"
-  sha256 "155d2da4b59769b5929a9d64bd8463cc7f8e4a7515a02806c9d699fedeaa8661"
+  url "https://github.com/vim/vim/archive/v9.0.0950.tar.gz"
+  sha256 "c53542fbec58362c3a5ed60a0fe5aa219ef49cd15d9545311ec0f6d1f03e2fdd"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
