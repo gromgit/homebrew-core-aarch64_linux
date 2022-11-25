7300accf6d2650003e0b44b07f3e6c333ace33af
Rui Chen <rui@chenrui.dev>
2022-11-25 22:46:11 +0000

    bitcoin: update 24.0 bottle.

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -15,6 +15,7 @@ class Bitcoin < Formula
     sha256 cellar: :any,                 arm64_ventura:  "e56672fac2ad8156d7aea28336c2a04d494013e98d0b70eb610b8464992b5929"
     sha256 cellar: :any,                 arm64_monterey: "e1a4c5decd6681167e3366a1b0b828a602f5284477d45882959b1494a80243ea"
     sha256 cellar: :any,                 arm64_big_sur:  "259562fee9d95cb93899173e86086aea48eebec490d188912d134a35a1274629"
+    sha256 cellar: :any,                 ventura:        "3dbb3b0eb1d558f03166ad8b031d9a7f2747dfd600338310fe318b9b7b100da9"
     sha256 cellar: :any,                 monterey:       "a83c6d1041da647995af5235fbcf12d3f548bf4a621d0cd660b7b910ecbc4e4b"
     sha256 cellar: :any,                 big_sur:        "4c0c6526ec8eda4ec15e3767d7b56f322cfdd9392f103ea3ec3386246bab8f21"
     sha256 cellar: :any,                 catalina:       "be76b3c20499a3ad4fc0318fc7875609162b77af95eb7798355296dc6884be63"
