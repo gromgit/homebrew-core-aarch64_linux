adce4b4017d01cb68c397404183c7ff44dedacaf
Patrick Linnane <patrick@linnane.io>
2022-11-26 21:22:40 +0000

    lefthook: update 1.2.2 bottle.

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -11,6 +11,7 @@ class Lefthook < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "7a7509d2b5d082b70e8e0af872e545b5ad7747902879e5fcc61f2c310fa212d1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "57535aadf91cec738070aa48bb77dcefddc67f6f8f44de68bacf28e3ee307ea7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "333fb565c49aed503d3e7a806764ec528d142c7160e295dc95ddc6fade53ae5d"
+    sha256 cellar: :any_skip_relocation, ventura:        "7856727ab144c38cb8da84a3182d9961fba7b5aef0a196a13d5f3855733d4259"
     sha256 cellar: :any_skip_relocation, monterey:       "e495d55239947696d918fb5c726daa60e95c85f66300b5475c3a5c2e511d4900"
     sha256 cellar: :any_skip_relocation, big_sur:        "5f13eefb342c030da993f1ae90a49b84bac3f169bd30cb6984015893737da6ca"
     sha256 cellar: :any_skip_relocation, catalina:       "f6e1c5fe2da70b7ba41e5b8ad762e8ba6e425aad4a41d226f3e33569c6d47c42"
