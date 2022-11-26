b7650bd91c640d32eb71dbb15bfd3bc5b7ee7bee
Caleb Xu <calebcenter@live.com>
2022-11-26 18:47:30 -0500

    snort 3.1.47.0
    Closes #116770.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/snort.rb
+++ Formula/snort.rb
@@ -1,9 +1,9 @@
 class Snort < Formula
   desc "Flexible Network Intrusion Detection System"
   homepage "https://www.snort.org"
-  url "https://github.com/snort3/snort3/archive/3.1.45.0.tar.gz"
-  mirror "https://fossies.org/linux/misc/snort3-3.1.45.0.tar.gz"
-  sha256 "fec10fa7cb612c43c0bc2c35e087f0674cad07e8c15145a4c8d8cdaed69efffb"
+  url "https://github.com/snort3/snort3/archive/3.1.47.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/snort3-3.1.47.0.tar.gz"
+  sha256 "7565411db11b453a98f8bd88eeef0fa9f2e85416a8f595e710aa19c430228b8c"
   license "GPL-2.0-only"
   head "https://github.com/snort3/snort3.git", branch: "master"
 
