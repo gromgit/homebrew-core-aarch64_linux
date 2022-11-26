f0fa60b0f3e1cef810b83d7b0bfe1ab3dde91958
FX Coudert <fxcoudert@gmail.com>
2022-11-26 18:29:42 +0000

    fourmolu: update 0.9.0.0 bottle.

--- Formula/fourmolu.rb
+++ Formula/fourmolu.rb
@@ -9,6 +9,7 @@ class Fourmolu < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f2ae13d31c1cd0c98ab160d6ab4633a8c28751b48f168d177c96214db10a4906"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c75fb4de2f8cfeacc01367622619da8529292cacf2fc6bd06e87078a780b4293"
+    sha256 cellar: :any_skip_relocation, ventura:        "71066dad01725d5bc98e4a69890154dbb8a4df69ce44db6aa2ddaab32de39aa4"
     sha256 cellar: :any_skip_relocation, monterey:       "b3a971604e918f251eb1b5a973d5c0311a580967eea8035a2068c9c2fb52b66d"
     sha256 cellar: :any_skip_relocation, big_sur:        "d0ce723afff7d3acbb2be08136131df89c81018f6310555f349962609aff58da"
     sha256 cellar: :any_skip_relocation, catalina:       "eedc285ac8fce49c697256e2f1bf825c4e4169a91fe3ed32ece792f83ecb8469"
