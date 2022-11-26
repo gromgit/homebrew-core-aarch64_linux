e8f529c49a3066584a85122ee6b0bb8a6f0125cf
Patrick Linnane <patrick@linnane.io>
2022-11-26 06:13:05 +0000

    lego: update 4.9.1 bottle.

--- Formula/lego.rb
+++ Formula/lego.rb
@@ -6,13 +6,13 @@ class Lego < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1ace67e2e47dc6f01b844f37d631419c883bccadc3838f3415752e95ec620b9a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b459ae434751269f9617982d96ac4eb721921207764b7614ffdac9afa1fed5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd2228ebdc275f0c925abe2735907407794da96175ed87ccfab0fe406fe74346"
-    sha256 cellar: :any_skip_relocation, monterey:       "797dc9a2d419c10285c53736d1ec53a7535becee13aa8700a3821d6cf44c172b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "40da3449408ade9d0e9f0f826d3a1fa71c851413d2dbb4091b66c159cdba93e2"
-    sha256 cellar: :any_skip_relocation, catalina:       "610a4519f76e00c1d5ce19a87e73b6c2c1d07d881d287cfbf1dbfefb3689685b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1f048c3b47f70d71d19d076a2ed34ba48adf4ff986a03e0b0571650b848f558d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "70de659e13ab860d34d5b2303703c78677df5d6b9e4dd1d6f528617fd918d502"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a79f2c649d60f9468231f8f2cb7bcca3c59bc02bebf56854e1ae457787e3a09d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5bba9b863e99d8e8590c25758b32a28d756df1ce276185f89ed7cf75d7d2d121"
+    sha256 cellar: :any_skip_relocation, monterey:       "a8461e5e6c8a3566c69cdfa3b7838a6cc0f0bc4803fbf1099212d2f99586ef02"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d7147fa1db85f1d76cb8776bae970138d7362cc29c87020eeab6713c6cf86744"
+    sha256 cellar: :any_skip_relocation, catalina:       "c13740a0cf471274a601007daf4027e8fd7944e7257e1fc83ee1d5412523a4c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f5a3b085d4fe731ebc45e0feda6e3c9a18429529161586b2392f1e357937fdc"
   end
 
   depends_on "go" => :build
