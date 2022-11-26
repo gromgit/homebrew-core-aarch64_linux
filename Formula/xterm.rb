355d93d4a936fa2ccaab3c0cd3a57d923f3258e4
Rui Chen <rui@chenrui.dev>
2022-11-26 00:08:45 -0500

    xterm 377
    Closes #116747.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xterm.rb
+++ Formula/xterm.rb
@@ -1,9 +1,9 @@
 class Xterm < Formula
   desc "Terminal emulator for the X Window System"
   homepage "https://invisible-island.net/xterm/"
-  url "https://invisible-mirror.net/archives/xterm/xterm-376.tgz"
-  mirror "https://deb.debian.org/debian/pool/main/x/xterm/xterm_376.orig.tar.gz"
-  sha256 "1e5bb7aad068fb31d6d3cbb77f80c7ad1526cd4c956a4ddcf2c5cf28af5334e1"
+  url "https://invisible-mirror.net/archives/xterm/xterm-377.tgz"
+  mirror "https://deb.debian.org/debian/pool/main/x/xterm/xterm_377.orig.tar.gz"
+  sha256 "db108fe7a45d8ed97e604721b58443b473649e61e263631bf8759f8618a990b2"
   license "X11"
 
   livecheck do
