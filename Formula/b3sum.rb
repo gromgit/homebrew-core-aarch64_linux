e809f488d76a08f7cc7895a8a0610893a0d8e15f
Rui Chen <rui@chenrui.dev>
2022-11-26 22:13:27 +0000

    b3sum: update 1.3.3 bottle.

--- Formula/b3sum.rb
+++ Formula/b3sum.rb
@@ -9,6 +9,7 @@ class B3sum < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e444ed506f694350e9862bf5ccc5fa7b80a0a50a6dfc5dbd91f0968050ce024d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "18107cbb5e59f87c8bbc8d200b8271867b7512620ea2c3813b38e00b3c2b842d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63887a445a74e816d2065e68156deb565fca9d4ca90a5298b9d91fb83b8249bc"
+    sha256 cellar: :any_skip_relocation, ventura:        "d5253008aad354c52a504b62c62e28dc9dbe871aa3e3f97845ebbeab2bf32a5a"
     sha256 cellar: :any_skip_relocation, monterey:       "4d9ffc0feafd47e79aa4b6b21bad7f59461f8d025dbf0d9b7ac238de42b633d6"
     sha256 cellar: :any_skip_relocation, big_sur:        "b251e81c461b59e9c231e24a5551693054c0fabb4c3ccec79b773e6d6f17c230"
     sha256 cellar: :any_skip_relocation, catalina:       "f846f17b8780e0cf413a908db621f2a065abc48af37f71ca10dccdc65591dbac"
