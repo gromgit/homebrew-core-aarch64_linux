41a7cb0181c1aada63c535395590eb2baccfa7d9
FX Coudert <fxcoudert@gmail.com>
2022-11-26 18:26:41 +0000

    ormolu: update 0.5.1.0 bottle.

--- Formula/ormolu.rb
+++ Formula/ormolu.rb
@@ -9,6 +9,7 @@ class Ormolu < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "93c07f5e65714f5235a66d264e03ded0389ee5f17d7ce535dd85ff0fa64f6e21"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "88a0e722d763286d25df1f804b1fe5a364679330ee21af8edf689b2be97d41b8"
+    sha256 cellar: :any_skip_relocation, ventura:        "f0d7ed9c5f7d798958b774cf2281adf8bf7699bece19ed6dfca1b8cc6efc9b38"
     sha256 cellar: :any_skip_relocation, monterey:       "b7161af8d79a0f4d106881fc2c00fbc16fd82dd7e8b113042ed96e4d428b592f"
     sha256 cellar: :any_skip_relocation, big_sur:        "d0326abd4734ee2aa4078b6858fbaf3985b786ad5f9236e8ec96831a364642c2"
     sha256 cellar: :any_skip_relocation, catalina:       "b1a706b4c77d213e08be73417a751c147f55475151e1d50e878d74bf14557d04"
