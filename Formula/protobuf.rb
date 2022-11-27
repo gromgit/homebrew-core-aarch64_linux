9c3f58f0f97ad58fababd3bf70e1ed4e411760bb
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:09:30 +0800

    protobuf: no rebuilding required

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,19 +1,9 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v21.5/protobuf-all-21.5.tar.gz"
+  sha256 "7ba0cb2ecfd9e5d44a6fa9ce05f254b7e5cd70ec89fafba0b07448f3e258310c"
   license "BSD-3-Clause"
-  revision 1
-
-  stable do
-    url "https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protobuf-all-21.9.tar.gz"
-    sha256 "c00f05e19e89b04ea72e92a3c204eedda91f871cd29b0bbe5188550d783c73c7"
-
-    # Fix build with Python 3.11. Remove in the next release.
-    patch do
-      url "https://github.com/protocolbuffers/protobuf/commit/da973aff2adab60a9e516d3202c111dbdde1a50f.patch?full_index=1"
-      sha256 "911925e427a396fa5e54354db8324c0178f5c602b3f819f7d471bb569cc34f53"
-    end
-  end
 
   livecheck do
     url :stable
@@ -21,14 +11,8 @@ class Protobuf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "dcfd589b02e3c9ca6b0388f013223934413022f6823e39ed2732aec935c0d349"
-    sha256 cellar: :any,                 arm64_monterey: "56a18680209f0b28abbb8e0f3c6079446e976b09f24eed9e9a2aaaaed5dbbbf6"
-    sha256 cellar: :any,                 arm64_big_sur:  "46955560681fd311617fd17dc92827a7804950fa602514537b12f2b4ee004e85"
-    sha256 cellar: :any,                 ventura:        "50343754dd5021305288b7e5ecfff19586beea27e1cab786c03b8dd2086e3a16"
-    sha256 cellar: :any,                 monterey:       "1a645d011a09010b7cda09dffb54e97facb516965ad6c90ff56c59ccaa45670d"
-    sha256 cellar: :any,                 big_sur:        "42d6f1cdd90ea13c9b5e63106acaa289e781149cd34cc6b1c831e2a64f40e7bd"
-    sha256 cellar: :any,                 catalina:       "18c2e746f2a7b2fd85238b0bc7890fb66321ca83c3a67669c0706eb32abff125"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fafb7f3058823653ddc5a8b4219ece68ffc441d649f47e240223679e6e94c439"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/protobuf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a46f71f968f429d44d360cee08684d493ce92b5dbcf3d26395b2e86e6b5d12ea"
   end
 
   head do
@@ -40,15 +24,9 @@ class Protobuf < Formula
   end
 
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
 
   uses_from_macos "zlib"
-
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
-        .map { |f| f.opt_libexec/"bin/python" }
-  end
 
   def install
     # Don't build in debug mode. See:
@@ -58,7 +36,8 @@ class Protobuf < Formula
     ENV.cxx11
 
     system "./autogen.sh" if build.head?
-    system "./configure", *std_configure_args, "--with-zlib"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--with-zlib"
     system "make"
     system "make", "check"
     system "make", "install"
@@ -71,8 +50,8 @@ class Protobuf < Formula
     ENV.append_to_cflags "-L#{lib}"
 
     cd "python" do
-      pythons.each do |python|
-        system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
+      ["3.9", "3.10"].each do |xy|
+        system "python#{xy}", *Language::Python.setup_install_args(prefix, "python#{xy}"), "--cpp_implementation"
       end
     end
   end
@@ -91,8 +70,7 @@ class Protobuf < Formula
     (testpath/"test.proto").write testdata
     system bin/"protoc", "test.proto", "--cpp_out=."
 
-    pythons.each do |python|
-      system python, "-c", "import google.protobuf"
-    end
+    system Formula["python@3.9"].opt_bin/"python3.9", "-c", "import google.protobuf"
+    system Formula["python@3.10"].opt_bin/"python3.10", "-c", "import google.protobuf"
   end
 end
