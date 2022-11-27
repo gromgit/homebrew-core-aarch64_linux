e1ed9316d130432f83143e46393407d4058db4e5
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:26:00 +0800

    stone-soup: no rebuilding required

--- Formula/stone-soup.rb
+++ Formula/stone-soup.rb
@@ -1,8 +1,10 @@
 class StoneSoup < Formula
+  include Language::Python::Virtualenv
+
   desc "Dungeon Crawl Stone Soup: a roguelike game"
   homepage "https://crawl.develz.org/"
-  url "https://github.com/crawl/crawl/archive/0.29.1.tar.gz"
-  sha256 "e8ff1d09718ab3cbff6bac31651185b584c9eea2c9b6f42f0796127ca5599997"
+  url "https://github.com/crawl/crawl/archive/0.29.0.tar.gz"
+  sha256 "4b32d3c3a07fe969cc1e9d12430b4c143c36e92746b3715ccdb8416720fdc59f"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,26 +13,35 @@ class StoneSoup < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "c37530a95dbd1b4df7d64f35a3e4ccd4e9d5e53565d956f0e6d7f9ffbc1d73d7"
-    sha256 arm64_monterey: "cf6ca59c6899897889fb1dd675513941085da9832c565009a33f35d0eaa8d983"
-    sha256 arm64_big_sur:  "e68f79cdfbee9e2c50c26785becab8a8e5060105657f7ea2ed1ceaf5efef83e5"
-    sha256 monterey:       "7772ce20e270a7bfbc0cf3838aa780a5679eb7733987835606d7596ab64d8846"
-    sha256 big_sur:        "fdc9152a3b611d3c905f86e07073bb871d2fe93f69c1940b0ab6a907c8403fd7"
-    sha256 catalina:       "edbc05d07621b8424656f6d997cfce5cdae087fd4fc2f624fcd85e6f61fd8c27"
-    sha256 x86_64_linux:   "eef057937a3400fbf577121fd0a8567d0295b6d1c0dd5985ee202f11a59bee57"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/stone-soup"
+    sha256 aarch64_linux: "d74705f9dec141d773eabdaec6baddf4b710a09be9b99f11c3810112d3f0306d"
   end
 
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => :build
-  depends_on "pyyaml" => :build
   depends_on "lua@5.1"
   depends_on "pcre"
   depends_on "sqlite"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
   fails_with gcc: "5"
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
+    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
+  end
 
   def install
     ENV.cxx11
+    ENV.prepend_path "PATH", Formula["python@3.10"].opt_libexec/"bin"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python#{xy}/site-packages"
+
+    venv = virtualenv_create(buildpath/"vendor", "python3")
+    venv.pip_install resource("PyYAML")
 
     cd "crawl-ref/source" do
       File.write("util/release_ver", version.to_s)
