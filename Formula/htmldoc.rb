35924d41ae6201c2a201b883dcb05d5ee287bbf7
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:21:09 +0800

    htmldoc: no rebuilding required

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -8,14 +8,8 @@ class Htmldoc < Formula
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
-    sha256 arm64_ventura:  "21266fa80373c2fffb69d8962f42a3d97e41aabfea6afe203eb02623760240f5"
-    sha256 arm64_monterey: "e8d3042fd923158ab250600c7a465010ea8c329e11326f36a2d10f9406a4e827"
-    sha256 arm64_big_sur:  "98325540be8b32bfc44cc1a7ebb976a11a95d63140aba964085e4df8b6af59a3"
-    sha256 ventura:        "16da9b4776a14debcebdab692318501670f66810be6ede03596f150760832d71"
-    sha256 monterey:       "ddd36ac6cc9fbf5e585bbfc37f1dc595f207691ed702900b3354866c411ddaac"
-    sha256 big_sur:        "4ec9fe099c857e2024e854462641e5620056585059d7a587ca6322df6d1c9a5a"
-    sha256 catalina:       "7b208d7bfd479b71ee86eabfbba86e72d9a354ff28b56f6dc1afb4121617d402"
-    sha256 x86_64_linux:   "6fb8d354d0807ff8a643f13ae974cde5f0f1d76c51e904dae4b9b9a40b1ee927"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/htmldoc"
+    sha256 aarch64_linux: "96057fc41d58559dfc433202db24165877c06297ddd6b7041cab266888e364c0"
   end
 
   depends_on "pkg-config" => :build
