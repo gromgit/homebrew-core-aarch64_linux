ac2542b61c532a029791e4fac54c1155a65bf5c8
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:17:28 +0800

    rustup-init: no rebuilding required

--- Formula/rustup-init.rb
+++ Formula/rustup-init.rb
@@ -6,15 +6,8 @@ class RustupInit < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "da743cec474c388a7774a7f7fecc458ee639d56cf4b47dc435717eb5e4476d54"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6125eed25505926f523304ebb665122a3d15b3de19b482b3b84008c93fe28fd4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "597c38f3a49ada52ad08e863536254584294c7a4bf826d5c3ccc5ee658ff86f6"
-    sha256 cellar: :any_skip_relocation, ventura:        "ac71a435b74247270060dd2b0beb5fc479ada984aaadf9a777dd5ae9f3896473"
-    sha256 cellar: :any_skip_relocation, monterey:       "381604c17ed5b69af016c416ba9c19945ec86a2576fbfa331d7baff9c97c5d92"
-    sha256 cellar: :any_skip_relocation, big_sur:        "daf440be5aed32296c301664697f6e264c0262cc9949d04aed174de844fbdb98"
-    sha256 cellar: :any_skip_relocation, catalina:       "2d62d01b7380cf2c0dfb81de3ecab619dc5350b35ce2edabc02062267023c978"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cd649820fb2f782f82ea48c4cebb9ed17ae547d2c6476c6903639b760d6e0c6b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rustup-init"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "59db57b07bff5ddebb6c49d78706d58af288c02d3d8376060bac3c042248f690"
   end
 
   depends_on "rust" => :build
