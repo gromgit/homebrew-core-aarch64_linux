fd1045f6632c0cca97efd6b482c57bbc5c492bdd
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:21:43 +0800

    httrack: no rebuilding required

--- Formula/httrack.rb
+++ Formula/httrack.rb
@@ -5,7 +5,6 @@ class Httrack < Formula
   # link to download.httrack.com will break on next HTTrack update.
   url "https://mirror.httrack.com/historical/httrack-3.49.2.tar.gz"
   sha256 "3477a0e5568e241c63c9899accbfcdb6aadef2812fcce0173688567b4c7d4025"
-  license "GPL-3.0-or-later" => { with: "openvpn-openssl-exception" }
   revision 1
 
   livecheck do
@@ -14,18 +13,11 @@ class Httrack < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "82366a7496c4dad900d7c5de609becac2493bfaca4101e77b43120450ab9ff4b"
-    sha256 arm64_monterey: "0345ae5cd642cad43051144018ad7716f91ba9c2a7b94f3068c5b7c855a6e8ba"
-    sha256 arm64_big_sur:  "1e2db8b0a5afa31857870b780957c99975f68248c8a7e2826b4b3e9ccb1b4a64"
-    sha256 ventura:        "ce79d5a831a8a5162a84e447625330038f98ea4797b5d6ad079198c5943ea654"
-    sha256 monterey:       "98b7f4c73a1bb5f531bdd455942ceb00226068ec7bf5f32fd8a45b01f9cbb482"
-    sha256 big_sur:        "01c1cb899db03ce18211dc2014af7f9a2592b61b32038540a8d8e5e7e40b9386"
-    sha256 catalina:       "58d9d2b9cee7fd5e4d3faad7620def7aa029f81b03d92a4150b5269fc38ea963"
-    sha256 x86_64_linux:   "b7688c49793f0dab8cd4ec29ec2ac3ecb978d6f15067d0806f9bbc315bdb08f1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/httrack"
+    sha256 aarch64_linux: "0790b29efd5924119b6b37802530818d5292b9abe61d8dd2db2558a6681db2d2"
   end
 
-  depends_on "openssl@3"
+  depends_on "openssl@1.1"
 
   uses_from_macos "zlib"
 
