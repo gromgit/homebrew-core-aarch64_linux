a24571b7accc72f77ed07564da05babc189dd287
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:42:42 +0800

    xxhash: no rebuilding required

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -3,10 +3,7 @@ class Xxhash < Formula
   homepage "https://github.com/Cyan4973/xxHash"
   url "https://github.com/Cyan4973/xxHash/archive/v0.8.1.tar.gz"
   sha256 "3bb6b7d6f30c591dd65aaaff1c8b7a5b94d81687998ca9400082c739a690436c"
-  license all_of: [
-    "BSD-2-Clause", # library
-    "GPL-2.0-or-later", # `xxhsum` command line utility
-  ]
+  license "BSD-2-Clause"
 
   livecheck do
     url :stable
@@ -14,21 +11,13 @@ class Xxhash < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "daf4e0c7861f6ac2addb81a87f3b511fc972a332c517a3ea0ec00afb5558ae94"
-    sha256 cellar: :any,                 arm64_monterey: "3d75964698a3beb04236d1632f6e69a2b58f1c552fee4720467c5ff47b1356b5"
-    sha256 cellar: :any,                 arm64_big_sur:  "047255a58b02b965c7a8c1a7a6216bda9c0a386fd0847b019568de321ca81c27"
-    sha256 cellar: :any,                 ventura:        "b20df383671e6eb2556e8cb31574382e6fdd58788ce843be1dbb0abcb601f6d1"
-    sha256 cellar: :any,                 monterey:       "05eaeee75ca864298a1c85c6b917401bce6b429da391c83be347c321013f298e"
-    sha256 cellar: :any,                 big_sur:        "7fbfb9c5e821cb3e373c44577f7839eb331287b84fb9ad56f048e98aff42a143"
-    sha256 cellar: :any,                 catalina:       "b04f17e55ef5a2e48b0668453d0da1ec4af00752a62f12316a178f19b0f292b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1031dd4f8c86ee0e53242872aec9e4599be6b4f36c1ca58735651847f23c480c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/xxhash"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c4be461f27a4f6cef331c733ba364e91cd5f4b42852b77f2db540c7030b34ae1"
   end
 
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
-    prefix.install "cli/COPYING"
   end
 
   test do
