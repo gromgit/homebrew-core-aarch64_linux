fb03d233f9131e40c53c5c10f94950e9a9cb5d79
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:34:43 +0800

    bmake: no rebuilding required

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,8 +1,8 @@
 class Bmake < Formula
   desc "Portable version of NetBSD make(1)"
   homepage "https://www.crufty.net/help/sjg/bmake.html"
-  url "https://www.crufty.net/ftp/pub/sjg/bmake-20220928.tar.gz"
-  sha256 "c804b77de3feb8e31dee2a4c9fb1e9ec24c5a34764e7a2815c88b4ed01650e90"
+  url "https://www.crufty.net/ftp/pub/sjg/bmake-20220901.tar.gz"
+  sha256 "3f67c575ee9ae443a5f589a40acac0163743da98cb50afd1144b4246cd5063ad"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,14 +11,8 @@ class Bmake < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e536bcfc52946f631c00183164126e6fd26006acd27b21e7da7b06ab310300d4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6a4b5a42dadc037e158e4cfe5571b3adf9882024be07147431adfe2617a96a31"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "95a14d565b7eec3c2ca6d3ecb793d69cc2a29eddbfa4d8a891afe66e2e2ae945"
-    sha256                               ventura:        "1549aa5b240a604482f5e0b1838abcd88e40670847469d8515f6d66167be603c"
-    sha256                               monterey:       "801cbdcbe3ce5ff1e436b511923e75074a825e7d8e34cc80f7ce151a64cfbcd0"
-    sha256                               big_sur:        "d1e86cb4561374d4fa6d18c5b3e00afd4055640b10061c0c62b564ec8b522975"
-    sha256                               catalina:       "ef5c79a571b508e81a122960f1b3fa11a0640ce315c0595ed3030ea6dce100ee"
-    sha256                               x86_64_linux:   "0997b8c0c53881ead6f629f2a7d06dc88bcd6583c0ce6861d1e2351afe533e82"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bmake"
+    sha256 aarch64_linux: "a838dd653016f30cbbef9558f102d18e0423fe6895180f702b69c27da699e43a"
   end
 
   def install
