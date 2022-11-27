5cf7d90c3f1ff3f10dd572528dd0a694aaa083b5
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:12:14 +0800

    qrcp: no rebuilding required

--- Formula/qrcp.rb
+++ Formula/qrcp.rb
@@ -6,14 +6,8 @@ class Qrcp < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "889a88eb8962304e9a0f9cdca7f1a879585c99d7b7ee54fa89b9a670813d3a9c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "72b5897e63349242e2c60cae3662a9126b626a0c61955dea13acc7985a91194b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9561594ba75dca10b0ba413a3df8055f7009513a71694ea1ddab8f8ab7bdd125"
-    sha256 cellar: :any_skip_relocation, monterey:       "d50f012601dc63a2cd69060bf28cf33a0e31b1af92c7015667a3e9037df85732"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b413f243a3eb24dae58770f62005538e1c851c48781c75f5a755b87e9580b61f"
-    sha256 cellar: :any_skip_relocation, catalina:       "500c92e7643f664db127ab5c0eec5b4120e8715b0042100e619c2f986bce6137"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28ca8979e794d2e04176f125c1445082a325930ec608c46e27e8f004a5f3542f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/qrcp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0027da323a3838ebdf02ca0441bee16061c9fcf07f65abc7dc0310f93dc08bbe"
   end
 
   # Bump to 1.18 on the next release, if possible.
@@ -21,8 +15,6 @@ class Qrcp < Formula
 
   def install
     system "go", "build", *std_go_args
-
-    generate_completions_from_executable(bin/"qrcp", "completion")
   end
 
   test do
