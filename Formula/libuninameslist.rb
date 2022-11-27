1943f26e0aa04b8d9eb2d0d25ef4e0dbe0a662c2
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:40:24 +0800

    libuninameslist: no rebuilding required

--- Formula/libuninameslist.rb
+++ Formula/libuninameslist.rb
@@ -1,8 +1,8 @@
 class Libuninameslist < Formula
   desc "Library of Unicode names and annotation data"
   homepage "https://github.com/fontforge/libuninameslist"
-  url "https://github.com/fontforge/libuninameslist/releases/download/20221022/libuninameslist-dist-20221022.tar.gz"
-  sha256 "92c833936d653b2f205fb5e7ac82818311824dabdc7abdc2e81f07c3a0ea39bb"
+  url "https://github.com/fontforge/libuninameslist/releases/download/20211114/libuninameslist-dist-20211114.tar.gz"
+  sha256 "f5f69090de4a483721207a9df7de5327c13c812a1d23de074d8f0496bc2b740d"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,14 +12,8 @@ class Libuninameslist < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "e1f92764ac97ebac5e76e2cc88414e9b4272c791159b92751b44b8b1b16f88f8"
-    sha256 cellar: :any,                 arm64_monterey: "b730c8d395fff31ba541759c101e3024b9652c05aa426ca08b41f996afa49e7f"
-    sha256 cellar: :any,                 arm64_big_sur:  "4b0214eea454a0b9be8a49a37146519750abd9a384c397a35e0fd7978007d211"
-    sha256 cellar: :any,                 ventura:        "ea76dda1784a20ed1a884903618a73c57d66c69c9d0f1b40c050362c70d787ec"
-    sha256 cellar: :any,                 monterey:       "85eef665f0db7618b40ed7710b0677c098745a4c429c5f5ef70b4647f5fa2774"
-    sha256 cellar: :any,                 big_sur:        "24cebce234e974de93316eacc50b2bb4eef5aa68463fa2d4762b5a4103d87388"
-    sha256 cellar: :any,                 catalina:       "f7228caba01e8f69cf264d19822c6ae5fecddbc8c943911ab12df5d5c0539675"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0663619ead9a7ed939f94b7eb97d1be2b7239b35ed149e71c8605efbc171ff17"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libuninameslist"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "d05762eb4f63071731c51b66af0710f767e3d895530ce1ae897c14c5ae18e31a"
   end
 
   head do
