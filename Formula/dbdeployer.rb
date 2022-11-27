c75f7bade957e3aa81e7553585152880e0821961
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:50:12 +0800

    dbdeployer: no rebuilding required

--- Formula/dbdeployer.rb
+++ Formula/dbdeployer.rb
@@ -1,19 +1,14 @@
 class Dbdeployer < Formula
   desc "Tool to deploy sandboxed MySQL database servers"
   homepage "https://github.com/datacharmer/dbdeployer"
-  url "https://github.com/datacharmer/dbdeployer/archive/v1.70.0.tar.gz"
-  sha256 "ad0ff7659e21363132bdc35d72205b723937abcb0b9ffc7623806b3d51b00657"
+  url "https://github.com/datacharmer/dbdeployer/archive/v1.69.1.tar.gz"
+  sha256 "165950a32760a5e0f32f951eaa0c67a6956cb2cea75ac0381c65f39b7e9789c7"
   license "Apache-2.0"
   head "https://github.com/datacharmer/dbdeployer.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "31165295ead913ffa7d42122f05007bcf468aecdf0297b1a4674eed42ce398e0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0e2cd38ff5887c62cde30dae13a9326c6e611030e802ce3c590bda2cfca0cad6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b9158007e2758f5f89a5a235281c509db13a9bf9b2c7626cced2a882775d2c92"
-    sha256 cellar: :any_skip_relocation, monterey:       "5b80d2d8c561cb9a9850b6809147ccde5df87106a8bff9f9eb75755cd24a98f2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e3f07a16f5100354f3f41e40d8faed6258e1a1bfabf55320c1df32d652fc7db5"
-    sha256 cellar: :any_skip_relocation, catalina:       "a756634c3f57a2add358e072489172961fa5c9d81d8b9592353079b78e93e10f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51dec91fb634f07d82b4be43e20fdb74a04bd175e7ba8fb7e365aaeb3643c8ea"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dbdeployer"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6dc263e07ae40a25b79af15330621cf915ce9ee9799e166300cca4e1917b1c9b"
   end
 
   depends_on "go" => :build
