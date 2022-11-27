184bc662e56ccc2ddb35d41496f2f495dfb52011
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:08:20 +0800

    gettext: no rebuilding required

--- Formula/gettext.rb
+++ Formula/gettext.rb
@@ -1,29 +1,33 @@
 class Gettext < Formula
   desc "GNU internationalization (i18n) and localization (l10n) library"
   homepage "https://www.gnu.org/software/gettext/"
-  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.1.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.1.tar.gz"
-  sha256 "e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
+  sha256 "c77d0da3102aec9c07f43671e60611ebff89a996ef159497ce8e59d075786b12"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_ventura:  "28c5b06e66800aa2d460336d001379e35e664310d12638de35a1b0f2b9a44913"
-    sha256 arm64_monterey: "356b52e24b883af3ef092d13b6727b76e0137154c2c9eb42fe7c272bb7d3edec"
-    sha256 arm64_big_sur:  "90da957f7b8ad3d47fff7045a684060168e0433631921463fbbff09b5dc4b772"
-    sha256 ventura:        "fd7e48065cf73e37dfdf4c5cb789a14b93cf58ac06060814a60c94b87d8f26e6"
-    sha256 monterey:       "9318777367eae475e9ea226d2bcbd19ef8281d1dd2af3a92c20c00246677145b"
-    sha256 big_sur:        "95086fa8b1b6a913ca7ef3a7c7c49e147823c26ba239003f9140cfe1252587ba"
-    sha256 catalina:       "aba2b94f406a9d8784bb08f9763440297c645a7ea99f4c4dbfeccb325053322a"
-    sha256 x86_64_linux:   "991579fa170ca491fd6332844b570095978961a9764e57f00180002d471cf3b8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gettext"
+    sha256 aarch64_linux: "6c63e419b7282e6df63a0592bebf2fa71a1cddac52d624427a775d80b0b9ef33"
   end
 
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    directory "gettext-runtime/libasprintf"
+  end
+
   def install
     args = [
+      "--disable-dependency-tracking",
       "--disable-silent-rules",
+      "--disable-debug",
+      "--prefix=#{prefix}",
       "--with-included-glib",
       "--with-included-libcroco",
       "--with-included-libunistring",
@@ -47,7 +51,7 @@ class Gettext < Formula
     else
       "--with-libxml2-prefix=#{Formula["libxml2"].opt_prefix}"
     end
-    system "./configure", *std_configure_args, *args
+    system "./configure", *args
     system "make"
     ENV.deparallelize # install doesn't support multiple make jobs
     system "make", "install"
