85357997631a4206603f42343a3b4f4450516d14
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:12:09 +0800

    qpdf: no rebuilding required

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,35 +1,29 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/v11.2.0/qpdf-11.2.0.tar.gz"
-  sha256 "fbd2d75050933487929dbbe1b5c50a238487194bc7263c277d6e49abb90ab7f2"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.6.3/qpdf-10.6.3.tar.gz"
+  sha256 "e8fc23b2a584ea68c963a897515d3eb3129186741dd19d13c86d31fa33493811"
   license "Apache-2.0"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "41b4f1689615b64fc321da6a0dc88de085a16e3b2f2855d2b46305bf3c920749"
-    sha256 cellar: :any,                 arm64_monterey: "d5dc7f05aaf62794ccbcd7416e20750a76bdb237edd8ce50b2a57c74e14f526d"
-    sha256 cellar: :any,                 arm64_big_sur:  "eb0a4ddb0026e5ff1c963548f05512a5f24fabd9adbf530496b301242ddf3f4c"
-    sha256 cellar: :any,                 ventura:        "0c925ae43e085a33f982bf9d775f4629dfe4a9c438131fa84aac0d84d026f20d"
-    sha256 cellar: :any,                 monterey:       "02674cf95d7d3cd0a3211f24e741ea89171a3ee442d392ebab3011475a10ec49"
-    sha256 cellar: :any,                 big_sur:        "903f5103d2b5e68b145bcc94f50e37c333444092651fdd08c7505b01a2c395cd"
-    sha256 cellar: :any,                 catalina:       "9def57863e5ee1aab7aa99ba0106daa564c2eb7ec6e727c959fd5e6e53d83b11"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15a4e4bb40d7620fe671d133a9d3bdc9707247c675edc818d864288ff31b53f7"
+  livecheck do
+    url :stable
+    regex(/^release-qpdf[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
-  depends_on "cmake" => :build
-  depends_on "jpeg-turbo"
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/qpdf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "caf6cbab295a2124da7306a2e63eb83417645a6a5b182b56d8101681363722f9"
+  end
+
+  depends_on "jpeg"
   depends_on "openssl@1.1"
 
   uses_from_macos "zlib"
 
   def install
-    system "cmake", "-S", ".", "-B", "build",
-                    "-DUSE_IMPLICIT_CRYPTO=0",
-                    "-DREQUIRE_CRYPTO_OPENSSL=1",
-                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
-                    *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
   end
 
   test do
