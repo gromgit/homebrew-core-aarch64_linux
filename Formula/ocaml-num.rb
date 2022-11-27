9f7ae2275b3f5ccc678cb762b358fc1b9990a57c
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:57:41 +0800

    ocaml-num: no rebuilding required

--- Formula/ocaml-num.rb
+++ Formula/ocaml-num.rb
@@ -4,17 +4,11 @@ class OcamlNum < Formula
   url "https://github.com/ocaml/num/archive/v1.4.tar.gz"
   sha256 "015088b68e717b04c07997920e33c53219711dfaf36d1196d02313f48ea00f24"
   license "LGPL-2.1"
-  revision 3
+  revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "1376f678d4ee8141b6ce772985df8a1397f3e4831b02160a8057e3b195b26f90"
-    sha256 cellar: :any,                 arm64_monterey: "24a7c2eb700a74089c1fbdb5395473c4afa7f90e86372b556c7b48b4caab23fc"
-    sha256 cellar: :any,                 arm64_big_sur:  "e14151e4a14faa9a43774d4682930994110f9d654d533091b48a35ed95bfe8eb"
-    sha256 cellar: :any,                 ventura:        "6fd4ac3c6c935c55379ff625b59501cda84097a366fd762d99d6cdc1d66d5f91"
-    sha256 cellar: :any,                 monterey:       "36835ca0d89b6ccf37c1dbb3596b2e1ba688148cac83d6ddda1d2bdf5ddb2a00"
-    sha256 cellar: :any,                 big_sur:        "0d247f113c3d0135e09a2b0a12a9299217600477083b089c5731bcf2b2245c36"
-    sha256 cellar: :any,                 catalina:       "5dbcc04ba33b58d14b77ae7382345b9abd913ce342c20c330c4cf15c936b24f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8a1b1383e37f8f367f8f5c6c44a86f9e76c17080d3aa0465bfcff9328da88d68"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ocaml-num"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "3dfdb858efa2ac7aef436f50850b11bf86004ab364429fe9f40d5fa4547e233d"
   end
 
   depends_on "ocaml-findlib" => :build
