b9f83199e2bc02af1f80f6f9c7d3cd46096ea936
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:29:06 +0800

    atlantis: no rebuilding required

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -1,8 +1,8 @@
 class Atlantis < Formula
   desc "Terraform Pull Request Automation tool"
   homepage "https://www.runatlantis.io/"
-  url "https://github.com/runatlantis/atlantis/archive/v0.20.1.tar.gz"
-  sha256 "78f7e93f2b3030883386dc96ec03325790b6aa0a77778afea4cb254099c50f23"
+  url "https://github.com/runatlantis/atlantis/archive/v0.19.8.tar.gz"
+  sha256 "12f2e3d74264b4457d0a7766f8b4f580984a025fcdb3d443b8aad85f036994d4"
   license "Apache-2.0"
   head "https://github.com/runatlantis/atlantis.git", branch: "master"
 
@@ -12,14 +12,8 @@ class Atlantis < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "254b725ef8dbe61a9a9cc64592140cae4b6213aa61054073f1117e6252485d35"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "300c62b285262683f6847f8fe8f29c7b526d12b82410358147f8ee141a501a1a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b909946d7378be48a8067a7925079cb238fc0afce6ce44cb4fd629655db2e447"
-    sha256 cellar: :any_skip_relocation, ventura:        "0f725255f8e837afc38dc6d597c50d240c832af04469aa2958482370e71a0cdb"
-    sha256 cellar: :any_skip_relocation, monterey:       "dfe8554c620b2a5a9337a3ac1373e0f7d7e4427d035addd955705da614dfd6bd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "04afa0e0dbbb1e54cc861611aab11189ea1506fbc9e81404a57bcc87f05a426a"
-    sha256 cellar: :any_skip_relocation, catalina:       "8b8733588bd829bd44f8a272f7212ad7eee7113e347255b432878a3df3b13bec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e13f8b71036e0b739e8007f16a46e6ba4407e5d3141d5bd00b5355198c396f18"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/atlantis"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "131394c7c627dab9c94cf5c3d56c8c58f99984eafe0a209349093f29ba4d4307"
   end
 
   depends_on "go" => :build
