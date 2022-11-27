d4142929c238b1e29343ace6bddad6ff42dbc63b
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:29:00 +0800

    telnetd:

--- Formula/telnetd.rb
+++ Formula/telnetd.rb
@@ -5,21 +5,12 @@ class Telnetd < Formula
   sha256 "9beae91af0ac788227119c4ed17c707cd3bb3e4ed71422ab6ed230129cbb9362"
   license all_of: ["BSD-4-Clause-UC", "BSD-3-Clause"]
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e0f71c7ca40e07b2d12cc1ebcb547960381088891b4800637752d29999e3fdca"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1227d7bf27eb7a58adac93e1a210a93c88d940dff07d48017d2858464ddc3b9"
-    sha256 cellar: :any_skip_relocation, ventura:        "e4e977f33a2d9db9ec0f3e7cc588263e9416f7a733ed0564e7eca618c1b1f8f1"
-    sha256 cellar: :any_skip_relocation, monterey:       "70b9f81f506d83c50ba321e3920553aaeaa272c35ce798560afdd15da6e259a9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3fdf31a5dc2042a8ef17900ba7c2d7e87457d27fc656336b023f1b29caaaeac0"
-    sha256 cellar: :any_skip_relocation, catalina:       "5e463bafef10793f46d7c38905445a8a8d4141fac5f2ddfcb38710cc8b802210"
-  end
-
   depends_on xcode: :build
   depends_on :macos
 
   resource "libtelnet" do
-    url "https://github.com/apple-oss-distributions/libtelnet/archive/refs/tags/libtelnet-13.tar.gz"
-    sha256 "4ffc494a069257477c3a02769a395da8f72f5c26218a02b9ea73fa2a63216cee"
+    url "https://opensource.apple.com/tarballs/libtelnet/libtelnet-13.tar.gz"
+    sha256 "e7d203083c2d9fa363da4cc4b7377d4a18f8a6f569b9bcf58f97255941a2ebd1"
   end
 
   def install
