86ff26e9657aa4e0160fbd076794c956d430d442
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:54:41 +0800

    dotbot: no rebuilding required

--- Formula/dotbot.rb
+++ Formula/dotbot.rb
@@ -9,17 +9,11 @@ class Dotbot < Formula
   revision 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "87a12d128d6b818c686dcfed8f3303256f5ae06b99535e83e60eaf505885a8e8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e69e86421ab5c05c270cbcc228a5e0756d0d016c204ef6d1a3be5cf4b57f250a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "423dfff4e1cab4c57c73d8636c28e611ced0ebfbcad10fe222497de2c7138c6e"
-    sha256 cellar: :any_skip_relocation, monterey:       "ef6c5cd693bbfb4d3f1bac46042143460f071b488ab008b1f761db1f99d986e5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "44ac111a5f85630d40605cc5a924283a332bf2a8d8dc0b3a5e626c771d286c7e"
-    sha256 cellar: :any_skip_relocation, catalina:       "965c56d812816e5257fa6fc1380b3a7a63dd205c473358f73a12fe590031749f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a93db285c5f1670d5904594702bf19bab0429cde0cda6cd0f73ae740e6d51b1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dotbot"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4055422a17c11b83f50bfb9359ceebc82e705c4e6aef5597961ddf6afd20ff17"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.10"
 
   resource "PyYAML" do
     url "https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d/PyYAML-5.4.1.tar.gz"
