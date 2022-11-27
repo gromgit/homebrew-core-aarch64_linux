7ee8cab58c4424cdcfbad5dd7687fd11b9967b5d
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:56:38 +0800

    dynaconf: no rebuilding required

--- Formula/dynaconf.rb
+++ Formula/dynaconf.rb
@@ -3,22 +3,16 @@ class Dynaconf < Formula
 
   desc "Configuration Management for Python"
   homepage "https://www.dynaconf.com/"
-  url "https://files.pythonhosted.org/packages/54/6f/09c3ca2943314e0cae5cb2eeca1b77f5968855e13d6fdaae32c8e055eb7c/dynaconf-3.1.11.tar.gz"
-  sha256 "d9cfb50fd4a71a543fd23845d4f585b620b6ff6d9d3cc1825c614f7b2097cb39"
+  url "https://files.pythonhosted.org/packages/f0/0e/3c102db167de3e2bb3a36a680a89c27b9b08d9f856b2164cc1a03db96f1c/dynaconf-3.1.9.tar.gz"
+  sha256 "f435c9e5b0b4b1dddf5e17e60a1e4c91ae0e6275aa51522456e671a7be3380eb"
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f7c5282dab6a96e7527edf65bc226fc09331b002a41a9ecb2513d05f3e14da69"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f7c5282dab6a96e7527edf65bc226fc09331b002a41a9ecb2513d05f3e14da69"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7c5282dab6a96e7527edf65bc226fc09331b002a41a9ecb2513d05f3e14da69"
-    sha256 cellar: :any_skip_relocation, monterey:       "489381871dac0329723a79c90c1d4997720f54fb8a2f2a2e2bb21ddd686b242d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "489381871dac0329723a79c90c1d4997720f54fb8a2f2a2e2bb21ddd686b242d"
-    sha256 cellar: :any_skip_relocation, catalina:       "489381871dac0329723a79c90c1d4997720f54fb8a2f2a2e2bb21ddd686b242d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "572149028f9cc11c395caf2c3af61197c4ce5b71c4a978235445ba1e63d5b025"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dynaconf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "25a42f49e27a32ddad851b0e60c2f9faedc3b199aa1f5bee5d13ad798afe5101"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
