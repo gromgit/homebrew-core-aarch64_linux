d22deaa11489add489897379f06cb09988974f7e
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:36:50 +0800

    vile: no rebuilding required

--- Formula/vile.rb
+++ Formula/vile.rb
@@ -6,24 +6,16 @@ class Vile < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_ventura:  "9fea67ad963f8e76ab09fb3e181acd3ae4af71d8413323e01a51521ff6f4c5cf"
-    sha256 arm64_monterey: "0036bd77eb9f1231343f6e21793d6c3b1df01d014411b794aa289e621350a2ac"
-    sha256 arm64_big_sur:  "440c832f9fb4513eea1800a0b1f22a7afae03f48f96d03478d78d731a76a84a0"
-    sha256 monterey:       "4548060b0cf5731b7f4b6fc0eb95b33ea2a446decd83b5bfc946f354728d703b"
-    sha256 big_sur:        "cda465c26af587b967d18c93061c25adc6cf0e356d5fee5bf172e8b671b47eea"
-    sha256 catalina:       "b7b693bde291c3bdd649bfc2129aaeabf0c2e8edca732a458e427efcffcf3b28"
-    sha256 x86_64_linux:   "bc15ead3a90e2e171890caa044d5279b54a017b7fb1bb7d83eaeccbb3f0e671e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vile"
+    sha256 aarch64_linux: "2f142af61db675b1288042bf0c659d309c118207ac8bcd761038afd5a143cc84"
   end
 
   uses_from_macos "flex" => :build
+  uses_from_macos "groff" => :build
   uses_from_macos "expect" => :test
   uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
   uses_from_macos "perl"
-
-  on_system :linux, macos: :ventura_or_newer do
-    depends_on "groff" => :build
-  end
 
   def install
     system "./configure", *std_configure_args,
