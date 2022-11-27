3378bce0cac3c6571fed9bf617bc37e9effb03ea
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:38:16 +0800

    libpthread-stubs: no rebuilding required

--- Formula/libpthread-stubs.rb
+++ Formula/libpthread-stubs.rb
@@ -6,16 +6,8 @@ class LibpthreadStubs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "06a9f3556eefaa9d243d18b484a38f89bcc999b84d3e9722ddf3645479bce44b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "06a9f3556eefaa9d243d18b484a38f89bcc999b84d3e9722ddf3645479bce44b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66f717674d23f63fae9357bc6432f98c9e40702a1112af2b65ba4b3b22ed3192"
-    sha256 cellar: :any_skip_relocation, ventura:        "06a9f3556eefaa9d243d18b484a38f89bcc999b84d3e9722ddf3645479bce44b"
-    sha256 cellar: :any_skip_relocation, monterey:       "06a9f3556eefaa9d243d18b484a38f89bcc999b84d3e9722ddf3645479bce44b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "959dcecc35db3faf40e18ac1fe4350b034a552a32c2b2d5a9e9c8570b8626a90"
-    sha256 cellar: :any_skip_relocation, catalina:       "4d69c165836f4d19d4afd152f02340ea1f6e3f218faf950dddca39e10553c80d"
-    sha256 cellar: :any_skip_relocation, mojave:         "1baaf595e397cbfa6ba2379e1080d39de10115c39be797476cf89ff33b85938f"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "52d34ffb143025031355dcc9e9d4c64fe5cd46f020f2c3861fdc26d1f9c759d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06a9f3556eefaa9d243d18b484a38f89bcc999b84d3e9722ddf3645479bce44b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libpthread-stubs"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0e2a902d47c330837158b68c909e16dfa8887a92d4805456181f6d56f401d0b7"
   end
 
   depends_on "pkg-config"
