145774a5337f5cb17b1775268cd08e52ea9987ca
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:51:32 +0800

    mp3fs:

--- Formula/mp3fs.rb
+++ Formula/mp3fs.rb
@@ -4,23 +4,41 @@ class Mp3fs < Formula
   url "https://github.com/khenriks/mp3fs/releases/download/v1.1.1/mp3fs-1.1.1.tar.gz"
   sha256 "942b588fb623ea58ce8cac8844e6ff2829ad4bc9b4c163bba58e3fa9ebc15608"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3539466612aa0f46175ab58a559978aa14b92ba973b1d3819649aa8474537885"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "553dad1d8a1fe03cc3362b859d9fafbc47a34e024144cdfe56be1d1d5be71131"
   end
 
   depends_on "pkg-config" => :build
   depends_on "flac"
   depends_on "lame"
-  depends_on "libfuse@2"
   depends_on "libid3tag"
   depends_on "libvorbis"
-  depends_on :linux # on macOS, requires closed-source macFUSE
+
+  on_macos do
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
+  end
+
+  on_linux do
+    depends_on "libfuse@2"
+  end
 
   def install
     system "./configure", *std_configure_args
     system "make", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
