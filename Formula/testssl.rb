3d19587d539115c83d7476d33f44046d5578b7ee
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:29:47 +0800

    testssl: no rebuilding required

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -1,17 +1,16 @@
 class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
-  url "https://github.com/drwetter/testssl.sh/archive/v3.0.8.tar.gz"
-  sha256 "22c5dc6dfc7500db94b6f8a48775f72b5149d0a372b8552ed7666016ee79edf0"
-  license "GPL-2.0-only"
+  url "https://github.com/drwetter/testssl.sh/archive/v3.0.7.tar.gz"
+  sha256 "c2beb3ae1fc1301ad845c7aa01c0a292c41b95747ef67f34601f21fb2da16145"
+  license "GPL-2.0-or-later"
   head "https://github.com/drwetter/testssl.sh.git", branch: "3.1dev"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "3a76af565cfb3d79ede861d1ef2edaa3bea39f94dcfa3d8bd7ee72979afbb4f5"
+    sha256 cellar: :any_skip_relocation, all: "6f5a1162e55cf0b03f4261d2e653c01302ede13d56c413b4172774f2e0f2d7a0"
   end
 
-  depends_on "openssl@3"
+  depends_on "openssl@1.1"
 
   on_linux do
     depends_on "bind" => :test # can also use `drill` or `ldns`
@@ -23,7 +22,7 @@ class Testssl < Formula
     man1.install "doc/testssl.1"
     prefix.install "etc"
     env = {
-      PATH:                "#{Formula["openssl@3"].opt_bin}:$PATH",
+      PATH:                "#{Formula["openssl@1.1"].opt_bin}:$PATH",
       TESTSSL_INSTALL_DIR: prefix,
     }
     bin.env_script_all_files(libexec/"bin", env)
