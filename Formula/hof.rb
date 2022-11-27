37893d7447c57d4c48c12467acf33b8cde073bdc
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:20:33 +0800

    hof: no rebuilding required

--- Formula/hof.rb
+++ Formula/hof.rb
@@ -2,20 +2,14 @@ class Hof < Formula
   desc "Flexible data modeling & code generation system"
   homepage "https://hofstadter.io/"
   url "https://github.com/hofstadter-io/hof.git",
-      tag:      "v0.6.7",
-      revision: "5f6770b9628cd46a4caa24594e052dd715ac2dca"
+      tag:      "v0.6.6",
+      revision: "0f49b4d71a66788b006daacf905a2b138768beca"
   license "BSD-3-Clause"
   head "https://github.com/hofstadter-io/hof.git", branch: "_dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "201fc39b5bfa2a144cf714cd8c974ab4d7c40bc73fe3c0ffb880f2d910759463"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "201fc39b5bfa2a144cf714cd8c974ab4d7c40bc73fe3c0ffb880f2d910759463"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "201fc39b5bfa2a144cf714cd8c974ab4d7c40bc73fe3c0ffb880f2d910759463"
-    sha256 cellar: :any_skip_relocation, ventura:        "682e97ef134c35fae44303613bf5153d778c043ce4bd82269f9d5c53a9348e2f"
-    sha256 cellar: :any_skip_relocation, monterey:       "17049cc5cb7f04eebe8de0ec4353c77df7cf04466b91ab84fe982f0b8b7cd7e2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "17049cc5cb7f04eebe8de0ec4353c77df7cf04466b91ab84fe982f0b8b7cd7e2"
-    sha256 cellar: :any_skip_relocation, catalina:       "17049cc5cb7f04eebe8de0ec4353c77df7cf04466b91ab84fe982f0b8b7cd7e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "222943c19066f222fefcd9530b28f0024e74bea67395ecb57107bf9d11451754"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hof"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7da4a3077fe16e4740c266bf33531082b080ca06819ebe33eb0fba9e34170e54"
   end
 
   depends_on "go" => :build
