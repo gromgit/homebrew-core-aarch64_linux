0098516699d956f6c6436bc800af943268de9df8
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:30:33 +0800

    aws-iam-authenticator: no rebuilding required

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -2,8 +2,8 @@ class AwsIamAuthenticator < Formula
   desc "Use AWS IAM credentials to authenticate to Kubernetes"
   homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
   url "https://github.com/kubernetes-sigs/aws-iam-authenticator.git",
-      tag:      "v0.5.11",
-      revision: "918bfc9fb357618410d0a6d31b53c757840bd69e"
+      tag:      "v0.5.7",
+      revision: "2a9ee95fecab59fab41a0b646a63227d66113434"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/aws-iam-authenticator.git", branch: "master"
 
@@ -13,14 +13,8 @@ class AwsIamAuthenticator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5f1cdc24ea147d9999eb8ee4b512aebc6fb88418fbd8c178f8d75a3f9c365236"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "db8e2d5edcd576621f1def301c8e44b1fc3079e62971e3106cbd39b663e78ab5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "964f5f0e300d92b02a399af97f25693b0bd1282ed9666df597a5145757ba02d7"
-    sha256 cellar: :any_skip_relocation, ventura:        "2d0b75a5934658bda431eb1f8af4364bf97c508e7f1a73a828c52bf6f89d9ae1"
-    sha256 cellar: :any_skip_relocation, monterey:       "12457ca379a8f5d23831d2e9eef101d9620bb4d1920c445f3a0877cb7ec31ce7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "42165f2d6683958368681dc54cbd28e72656f091593ef8165e8e869aefd571ed"
-    sha256 cellar: :any_skip_relocation, catalina:       "c3024cc8618032a7151833206a148af182c4f6a56d2d6ce93c1a3ef04b2a9b46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7fcacf958bae7efbd904a2f40aa51c615a83a6319cc0d2f7dffaa77bfe63199e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aws-iam-authenticator"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8f1b468a6522c3101eac38bc72ef572da29466694280d69cb3e1b6e0bb43f565"
   end
 
   depends_on "go" => :build
