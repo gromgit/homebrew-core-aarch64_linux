782a64ec194f6efe0977665270cb49fe4526b159
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:19:34 +0800

    heimdal: no rebuilding required

--- Formula/heimdal.rb
+++ Formula/heimdal.rb
@@ -4,7 +4,7 @@ class Heimdal < Formula
   url "https://github.com/heimdal/heimdal/releases/download/heimdal-7.7.0/heimdal-7.7.0.tar.gz"
   sha256 "f02d3314d634cc55eb9cf04a1eae0d96b293e45a1f837de9d894e800161b7d1b"
   license "BSD-3-Clause"
-  revision 3
+  revision 2
 
   livecheck do
     url :stable
@@ -13,14 +13,8 @@ class Heimdal < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "589dd90ba100e7d4167a0973aa779e1f00d11482aa8d65188fa71a8b50bdb878"
-    sha256 arm64_monterey: "5bf1331cbf18fbacee694aebd48cf61bcebcd170fbbbe9e9c8a2bdf9ee90fcf6"
-    sha256 arm64_big_sur:  "5c45da30c4f837fd11fa4d656ff9f92c0a7cfd1d6c7e3442d925cd4c6406b766"
-    sha256 ventura:        "9f45c3bfa2a0e53d52ca57d84720f543c19c39a04f4c0262c04d358aede86289"
-    sha256 monterey:       "f91432a5c773478e95f79aed4381d8659980e7c19358cd109d9502eeaf5d6c6f"
-    sha256 big_sur:        "b0f45237bb7226ab0c6b06ce4c2a6ce143eded7335232e1cb85133863ad96f60"
-    sha256 catalina:       "1da6cca0420efc5ccd5075b8c3076435a9e2b077261d9ad91e0e3d4e644d38d0"
-    sha256 x86_64_linux:   "6505b354257b8f5096a209eb9bc8fbfea1f3377efff1e84b4c58a308c9cf2b34"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/heimdal"
+    sha256 aarch64_linux: "03245ead243540250bc8055e6c99d8004a76800fed263af45cf5d1458ce705e7"
   end
 
   keg_only "conflicts with Kerberos"
@@ -32,7 +26,6 @@ class Heimdal < Formula
   depends_on "openldap"
   depends_on "openssl@1.1"
 
-  uses_from_macos "libxcrypt"
   uses_from_macos "perl"
 
   resource "JSON" do
