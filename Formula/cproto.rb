f409cd024ca48caf868bbff1684ad69352609180
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:47:00 +0800

    cproto: no rebuilding required

--- Formula/cproto.rb
+++ Formula/cproto.rb
@@ -1,10 +1,14 @@
 class Cproto < Formula
   desc "Generate function prototypes for functions in input files"
   homepage "https://invisible-island.net/cproto/"
-  url "https://invisible-mirror.net/archives/cproto/cproto-4.7u.tgz"
-  mirror "https://deb.debian.org/debian/pool/main/c/cproto/cproto_4.7u.orig.tar.gz"
-  sha256 "64ebbbcc5e0501aff296f431d06f9fb70863afe5b0ce66c3b3479072914fc51e"
-  license :public_domain
+  url "https://invisible-mirror.net/archives/cproto/cproto-4.7t.tgz"
+  mirror "https://deb.debian.org/debian/pool/main/c/cproto/cproto_4.7t.orig.tar.gz"
+  sha256 "3cce82a71687b69e0a3e23489fe825ba72e693e559ccf193395208ac0eb96fe5"
+  license all_of: [
+    :public_domain,
+    "MIT",
+    "GPL-3.0-or-later" => { with: "Autoconf-exception-3.0" },
+  ]
 
   livecheck do
     url "https://invisible-mirror.net/archives/cproto/"
@@ -12,13 +16,8 @@ class Cproto < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb7dd61aa183b319b81fca2e4153647ff1805f20b3a5b35603fd90fc9d841946"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2907353ffb14ed7b6972aaa876f5012417fd17b6274b5d5ecf496bc40450794"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad283dcee4c414e2e395f42dafb30c2fabe2e1bf59c564273c93f82aa10cae3a"
-    sha256 cellar: :any_skip_relocation, monterey:       "29f7fc4efd440cb4672468744eeb43e4d952c6f75a8da271b4a807d0e767ecc2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c2a613fe2f6321208ffc402856d463b072e975b85be728cca1fe3ec47863e9e8"
-    sha256 cellar: :any_skip_relocation, catalina:       "83cedf6fa84b3d01c5bd5883d5e52291b8e63861cf493b6492bf2250b09e02da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c83f959881b5228a43999fe0c3b679fd575375b8f7bb1e322580c323db94644b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cproto"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "788f48086b1b39ec8bfd99fc7c3a52b53f9393a7d3fc771429f0b8f186ddd256"
   end
 
   uses_from_macos "bison" => :build
