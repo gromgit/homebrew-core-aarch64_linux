7e9c31435df0479227f673de4f891d5e6299869b
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:09:32 +0800

    protobuf@3: no rebuilding required

--- Formula/protobuf@3.rb
+++ Formula/protobuf@3.rb
@@ -1,19 +1,13 @@
 class ProtobufAT3 < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.20.3/protobuf-all-3.20.3.tar.gz"
-  sha256 "acb71ce46502683c31d4f15bafb611b9e7b858b6024804d6fb84b85750884208"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.20.1/protobuf-all-3.20.1.tar.gz"
+  sha256 "3a400163728db996e8e8d21c7dfb3c239df54d0813270f086c4030addeae2fad"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d78c587c5683c6ef029c414b915276ee0098008028ae6d9905cd42ffc76457a9"
-    sha256 cellar: :any,                 arm64_monterey: "6d43d690893a0dc2e18de52be03f748a854f310a89a120314a163cde52d6749e"
-    sha256 cellar: :any,                 arm64_big_sur:  "24a6c8831bf3297652ffa6a634886b243ca5709742e39f16b6de9929f22a3b74"
-    sha256 cellar: :any,                 ventura:        "c37111bebf0fc900f7b70e3cacc965c52b26263f1e499e0faeabdbed15ea4ccb"
-    sha256 cellar: :any,                 monterey:       "04e654c28bac719703534f2b2e44cdba72de4456ba71d6419dd67eb70f04c398"
-    sha256 cellar: :any,                 big_sur:        "c19c82512f5b10c0bac857b0ef6db324190d35a8d71388d98689501bfe531054"
-    sha256 cellar: :any,                 catalina:       "bb463286e99f61928ced590368aca6b5e3a25892c53a4e0fa0d0e4713b9f2026"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e069e9dbb3a04247616c94ed0c0a42dfd96914ab38d89760ed47765a465738a9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/protobuf@3"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e91c16a74b6b5b44cb1370c8af2bb6b034764fa5100c97b4b11f3e1a003a7b67"
   end
 
   keg_only :versioned_formula
