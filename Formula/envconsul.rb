a1fafdc2701dc4281c95007d19f65caec0008f8f
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:58:28 +0800

    envconsul: no rebuilding required

--- Formula/envconsul.rb
+++ Formula/envconsul.rb
@@ -2,18 +2,13 @@ class Envconsul < Formula
   desc "Launch process with environment variables from Consul and Vault"
   homepage "https://github.com/hashicorp/envconsul"
   url "https://github.com/hashicorp/envconsul.git",
-      tag:      "v0.13.1",
-      revision: "3111d811578b1c7f6c8af032a9d97234621e2b0a"
+      tag:      "v0.12.1",
+      revision: "265f933f17eb918e38b24fd1a7b1eab1fc723df5"
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b4da4f61b039ec8ff4cd8eb37166a9b337b4bb68d194da5897257a005f2a1dcd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c4c1b48f0ee4bf5ae510177ce3a9e5c785b7c515a21efc1383d93b03525634ac"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f75ee73c50f5050be219e82e9b00e7db5021e0a842cdaf3fbb97f558b9cbb6bc"
-    sha256 cellar: :any_skip_relocation, monterey:       "bab521f33f53f6e438ffb6ec0242b000cb77afdc6f2aa169db4f96522b6b84ad"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8bf147b061f72477077fed4278be1ae0a95db5ad7422f96536925e0256e343e0"
-    sha256 cellar: :any_skip_relocation, catalina:       "b7fe23483d90a0556e10fd933b663ed17290672a878ead5b772dbadc1fed01fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9b1e427c2757e0aa96f32794e3d89c4cd5cd6d3d080c2786d86cd966d46915ac"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/envconsul"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c439cb9de5aaff9478cf6b193bea9f125449be9c56d4f33f8da7d9ff0b9c7876"
   end
 
   depends_on "go" => :build
