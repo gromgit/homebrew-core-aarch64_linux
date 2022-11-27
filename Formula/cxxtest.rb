a015273586a38b6f4367f1eea81b80c72106891a
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:49:10 +0800

    cxxtest: no rebuilding required

--- Formula/cxxtest.rb
+++ Formula/cxxtest.rb
@@ -10,21 +10,14 @@ class Cxxtest < Formula
   revision 3
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1acc8d9a13d8955aece0bb80dc297469df4fb2f10948a1e9869ff26b8ec37fa4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1acc8d9a13d8955aece0bb80dc297469df4fb2f10948a1e9869ff26b8ec37fa4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1acc8d9a13d8955aece0bb80dc297469df4fb2f10948a1e9869ff26b8ec37fa4"
-    sha256 cellar: :any_skip_relocation, ventura:        "5c6d1561562d7d6e8d2c18df851d8b961bda69b4c8a42094b174b92a55d52324"
-    sha256 cellar: :any_skip_relocation, monterey:       "5c6d1561562d7d6e8d2c18df851d8b961bda69b4c8a42094b174b92a55d52324"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5c6d1561562d7d6e8d2c18df851d8b961bda69b4c8a42094b174b92a55d52324"
-    sha256 cellar: :any_skip_relocation, catalina:       "5c6d1561562d7d6e8d2c18df851d8b961bda69b4c8a42094b174b92a55d52324"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cf39bf39e516f6a2649f9693b6c45e2cd73dd6eaf63ccae2cc6ea34f116e93e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cxxtest"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4100f511ea81b952bfa19eb09a3cfb8654eb84657fbe2c8cb69ec6357b542f65"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.10"
 
   def install
-    venv = virtualenv_create(libexec, "python3.11")
+    venv = virtualenv_create(libexec, Formula["python@3.10"].opt_bin/"python3")
     venv.pip_install_and_link buildpath/"python"
 
     include.install "cxxtest"
