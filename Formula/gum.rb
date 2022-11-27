8558379e6fe75e654bd045708ad20e4eeb3c5ece
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:18:06 +0800

    gum: no rebuilding required

--- Formula/gum.rb
+++ Formula/gum.rb
@@ -1,20 +1,14 @@
 class Gum < Formula
   desc "Tool for glamorous shell scripts"
   homepage "https://github.com/charmbracelet/gum"
-  url "https://github.com/charmbracelet/gum/archive/v0.8.0.tar.gz"
-  sha256 "80d0000d8eaf1d577c76099a6747307df445ae66e368b99467d3493cce21c668"
+  url "https://github.com/charmbracelet/gum/archive/v0.6.0.tar.gz"
+  sha256 "53bc02a5aac5659e5c89d9b469dc181b7379e51defb123d1dfe05753fe05184e"
   license "MIT"
   head "https://github.com/charmbracelet/gum.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f700f4ac78280c9f14f2ef4e2a59511701db063f26d08473a7ded1132be33b1c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b0e305464c1c947462693fd51d0527d9eefdccbb5d753a8341bfaea3acc438c5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "98e3843734800ed758608b8c9228a4e144431ab58ced3bac601b5c335fd5be1d"
-    sha256 cellar: :any_skip_relocation, ventura:        "c52da43cfe5fec8ab3f655b16381db7b9c23c7e8d6dd3830abd4b0d564d339b3"
-    sha256 cellar: :any_skip_relocation, monterey:       "1312e2bac2cd1f0731bc77395c83233b49459747c3f9344c905f1ffa14b21905"
-    sha256 cellar: :any_skip_relocation, big_sur:        "98dbe8a090f59465b6bb1aff788bff0495af397e7c2f0b127dcb9a4cfcb11685"
-    sha256 cellar: :any_skip_relocation, catalina:       "587bef4576990fba2619395b8e182029c69d2a2990e020aae2a0c1e07d404382"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "446a0c2709dc87dce0772f9f94b0be704f962398b165f33ccd2d2cddbacbfbf0"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gum"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "669093806113f72766c06e282ee9dc5c11c9bf5ffade222b0b8a64f21cac3af3"
   end
 
   depends_on "go" => :build
