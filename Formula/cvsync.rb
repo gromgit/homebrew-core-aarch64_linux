bb7f43e575b62d1dc130aa2c32e52b003e2e6deb
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:49:02 +0800

    cvsync: no rebuilding required

--- Formula/cvsync.rb
+++ Formula/cvsync.rb
@@ -11,18 +11,11 @@ class Cvsync < Formula
   end
 
   bottle do
-    rebuild 3
-    sha256 cellar: :any,                 arm64_ventura:  "86f6fe30422f903827d29782f74e2eff93e7a2f0a86b4775747c9077cde03564"
-    sha256 cellar: :any,                 arm64_monterey: "151a5a709d78dfd6fe1db43686e7d0a7bf9464184133b5a79152a8d21aeb44e0"
-    sha256 cellar: :any,                 arm64_big_sur:  "284f786b36c5890da6c1d889cbf02a50c116a03661183b088dedb6a3ccdf4c05"
-    sha256 cellar: :any,                 ventura:        "ac33e5ee664d62d33043535d589ca4e526780073fed1801bc642d16f528a3402"
-    sha256 cellar: :any,                 monterey:       "8f9856b5b0be6da7e213fb287be69259d8aaf8425fa057702242109f393aeae9"
-    sha256 cellar: :any,                 big_sur:        "c53b78803c36ffe5b389b7891a51c8197b05e9d660c5100417fa5fd9f743cfd8"
-    sha256 cellar: :any,                 catalina:       "358f4234cde20c14d3af19a226c294154361a8159802755029f43a7f6d81fd27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "450f89a4983c03d8b007667d044047796ed721e7a6ce3fac437260c25d33219d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cvsync"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6db42f3016b8180ab9f9a698c8174443d871457857b463c7c6045244dcc768d8"
   end
 
-  depends_on "openssl@3"
+  depends_on "openssl@1.1"
 
   uses_from_macos "zlib"
 
