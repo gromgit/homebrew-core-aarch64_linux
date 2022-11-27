2a9e24589cc381a1c6f143426202cc5f4826081b
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:32:09 +0800

    bastet: no rebuilding required

--- Formula/bastet.rb
+++ Formula/bastet.rb
@@ -4,16 +4,11 @@ class Bastet < Formula
   url "https://github.com/fph/bastet/archive/0.43.2.tar.gz"
   sha256 "f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 1
 
   bottle do
-    sha256 arm64_ventura:  "007b3c5ce889f72efc83901d464e7b1d26d85889d33741f19cedb80ef420af8e"
-    sha256 arm64_monterey: "847549943397d6599052de7fddb1028fa521e86c778bb32ecc876b597d352c2e"
-    sha256 arm64_big_sur:  "f4e46f9ceeb65880d581bf0db249e4b781c37abadbd3d3199ff66aafde2f28d4"
-    sha256 monterey:       "c003bbb534a90d60b8a48abb3fc4fc418d5efe51f334063e5082b090dfc589cf"
-    sha256 big_sur:        "1309388b45599252aff6a86e11c634ce1eaf432ba95048af329c9c31103362ef"
-    sha256 catalina:       "afea0c4b2b48a465bad4dcbca28f5019f457968d45d6de5234e3c0a7b6965db0"
-    sha256 x86_64_linux:   "90a34c5ad79a5990922ad05c811c45caabd70e67dabe9f4b581c5de40d5c6699"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bastet"
+    sha256 aarch64_linux: "6045022d9c99a692a6ae060d49551b3de591b50966dae835e91fba26834382b7"
   end
 
   depends_on "boost"
