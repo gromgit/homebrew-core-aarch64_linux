a651460acde7c3371d2b88cbdb6a807284e473a9
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:20:05 +0800

    sevenzip: no rebuilding required

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -12,14 +12,8 @@ class Sevenzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "336a287e74813695435ba7726123db50672bc6c03379a72c4044c59b024a9797"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b9131e0986339f9986c7248c3101776279ddb390d287098ea5be1c84d6408d9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "15f4157398fdb03a9fc9ffa66eaf08abc72987d471b23ea22f5ab6700d7ba1a7"
-    sha256 cellar: :any_skip_relocation, ventura:        "0f86a22b685653e831bf61c9c046422cb7754bcec41b17c92e943d92fef15b6e"
-    sha256 cellar: :any_skip_relocation, monterey:       "64f2ea16a7c0f8591701914621b8ada21ecd6caa3ecf2568c8d4bc470edfae00"
-    sha256 cellar: :any_skip_relocation, big_sur:        "22a8c3aa0647a1f8829c7e180cb2d6ac78b071925e8c06be361716626487fd6a"
-    sha256 cellar: :any_skip_relocation, catalina:       "3ac4e9978c71f2452ded1a8ee0983b0710cc66d127ff2a95dc603b6211ed9df4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4dc467f705977b3a7d390a1248c691b9f4cee3c98d21647d55ef7c57afe28b3c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/sevenzip"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "07f19d40eec4bc73330bbf7d74374b6bb68b64a7ae176b961050fb90f7663bc1"
   end
 
   def install
