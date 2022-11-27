42360e9e24e32a9de665c87faf6c2af7d338b61f
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:09:55 +0800

    git-imerge: no rebuilding required

--- Formula/git-imerge.rb
+++ Formula/git-imerge.rb
@@ -3,40 +3,28 @@ class GitImerge < Formula
 
   desc "Incremental merge for git"
   homepage "https://github.com/mhagger/git-imerge"
+  url "https://files.pythonhosted.org/packages/be/f6/ea97fb920d7c3469e4817cfbf9202db98b4a4cdf71d8740e274af57d728c/git-imerge-1.2.0.tar.gz"
+  sha256 "df5818f40164b916eb089a004a47e5b8febae2b4471a827e3aaa4ebec3831a3f"
   license "GPL-2.0-or-later"
   revision 1
   head "https://github.com/mhagger/git-imerge.git", branch: "master"
 
-  stable do
-    url "https://files.pythonhosted.org/packages/be/f6/ea97fb920d7c3469e4817cfbf9202db98b4a4cdf71d8740e274af57d728c/git-imerge-1.2.0.tar.gz"
-    sha256 "df5818f40164b916eb089a004a47e5b8febae2b4471a827e3aaa4ebec3831a3f"
-
-    # PR ref, https://github.com/mhagger/git-imerge/pull/176
-    # remove in next release
-    patch :DATA
-  end
-
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d92935aec65d9d6c68d6517d0974ee7da92be9b9d2deb0b7508234499860e56b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d92935aec65d9d6c68d6517d0974ee7da92be9b9d2deb0b7508234499860e56b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d92935aec65d9d6c68d6517d0974ee7da92be9b9d2deb0b7508234499860e56b"
-    sha256 cellar: :any_skip_relocation, ventura:        "25a0367e58ad1afa702446005dc2a80e4802c9bd300b810d43cb72f29cac4959"
-    sha256 cellar: :any_skip_relocation, monterey:       "25a0367e58ad1afa702446005dc2a80e4802c9bd300b810d43cb72f29cac4959"
-    sha256 cellar: :any_skip_relocation, big_sur:        "25a0367e58ad1afa702446005dc2a80e4802c9bd300b810d43cb72f29cac4959"
-    sha256 cellar: :any_skip_relocation, catalina:       "25a0367e58ad1afa702446005dc2a80e4802c9bd300b810d43cb72f29cac4959"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fccde85a49be5f7819e50cca5dcc2fe8b06f0b10051d5a9b5369a5ffb9ddce26"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/git-imerge"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "49e9ed7c624f503af9f67ece832146bcf32bed9df109d68ae95b541b6a37c733"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.10"
+
+  # PR ref, https://github.com/mhagger/git-imerge/pull/176
+  # remove in next release
+  patch :DATA
 
   def install
     virtualenv_install_with_resources
-    bash_completion.install "completions/git-imerge"
   end
 
   test do
-    system "git", "config", "--global", "init.defaultBranch", "master"
     system "git", "init"
     system "git", "config", "user.name", "BrewTestBot"
     system "git", "config", "user.email", "BrewTestBot@test.com"
