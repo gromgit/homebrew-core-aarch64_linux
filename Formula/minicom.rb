69e1c45c12cd395abb0c4a294240d28b12d2edce
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:49:21 +0800

    minicom: no rebuilding required

--- Formula/minicom.rb
+++ Formula/minicom.rb
@@ -11,21 +11,8 @@ class Minicom < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "2846c9012370f9e0d674adddb25fb5ade25175bc3c09f4e3e5c458ff958bf58c"
-    sha256 arm64_monterey: "12256c7a67eeb793ee71b419a2319c80038bf20b24a583a9f09ba36dc9c5b75e"
-    sha256 arm64_big_sur:  "396aa4bed62d6a9162d061ff1b97a1c5fe25e5a890141d4f39c1849564e3521f"
-    sha256 ventura:        "e54b71e10799dd92bd3bf337a9a0ccab6d6fc6b3f93b119652dc05b48bad0fda"
-    sha256 monterey:       "e0b91adbcbaa88015cc339c7a9cd9b7f02c155b392229a93ece0baf48d798cff"
-    sha256 big_sur:        "ac0a7c58888a3eeb78bbc24d8a47fa707d7e3761c4b28f46527434d49e254b55"
-    sha256 catalina:       "9cee8e5839a3e19aa732307ee70246b1567ddc3a643ef39aa91b6d888301f6e5"
-    sha256 mojave:         "e2b702dec206101120ce947ca2a999c9f5fe7e8c62f95b65091146b865acb268"
-    sha256 x86_64_linux:   "7d1b0aae1f169968d42e4dea644dff5a4f18010b59b334439aa2bd276c6e913a"
-  end
-
-  head do
-    url "https://salsa.debian.org/minicom-team/minicom.git", branch: "master"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/minicom"
+    sha256 aarch64_linux: "a90fc7e5cd45681288e2faa66a1881b217c32ae58eee9eb26b9e7104be936b65"
   end
 
   uses_from_macos "ncurses"
@@ -34,8 +21,9 @@ class Minicom < Formula
     # There is a silly bug in the Makefile where it forgets to link to iconv. Workaround below.
     ENV["LIBS"] = "-liconv" if OS.mac?
 
-    system "./autogen.sh" if build.head?
-    system "./configure", *std_configure_args, "--mandir=#{man}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make", "install"
 
     (prefix/"etc").mkdir
