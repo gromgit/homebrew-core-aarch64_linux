e43ca56778bf0bb4d2ca205d8cf5c544137c6976
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:06:26 +0800

    pixie: no rebuilding required

--- Formula/pixie.rb
+++ Formula/pixie.rb
@@ -13,14 +13,8 @@ class Pixie < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dc7e8c53e17a17deda1d6eeeb55a8634a3eaded33505b82d4994f50b1ef01323"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb3de8badc2f4474447d19ffd313b866ebfe6424bc98159ebfe88b417ad338ac"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1fa86c4af601eabdf86d108aa0fe581323d90eeb5bec11002ac9d9162ce231ea"
-    sha256 cellar: :any_skip_relocation, monterey:       "171c75e6d6e9cb274ae6f4369dc50b24062706cebd2e43edc631a93a53dd7c81"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f8b9a0370907737cc31da6c344cb608a8743d743543271da932b51c15fbb79c6"
-    sha256 cellar: :any_skip_relocation, catalina:       "7d64f3d32cbefab6381512b07697a6f53d2914f87509e0ac75cbb284b2f6fa71"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9fe4c81cc2fa8418b3b5568bc23ac524b860c42651f5c63dbd43881a301f00b9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pixie"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9ed2ff17d80ae4713e57d7d0501d652385c5e94f153cb643276ac6140f6ff429"
   end
 
   depends_on "go" => :build
@@ -37,8 +31,6 @@ class Pixie < Formula
       -X px.dev/pixie/src/shared/goversion.builtBy=#{tap.user}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"px"), "./src/pixie_cli"
-
-    generate_completions_from_executable(bin/"px", "completion", base_name: "px")
   end
 
   test do
