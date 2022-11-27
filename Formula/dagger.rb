e52873cb80d35a6454ebc1a664349b3bb8decf4c
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:49:30 +0800

    dagger: no rebuilding required

--- Formula/dagger.rb
+++ Formula/dagger.rb
@@ -2,20 +2,14 @@ class Dagger < Formula
   desc "Portable devkit for CI/CD pipelines"
   homepage "https://dagger.io"
   url "https://github.com/dagger/dagger.git",
-      tag:      "v0.2.36",
-      revision: "ea275a3bafbc5b5c611e0b81bf2dd8a8add72f6b"
+      tag:      "v0.2.9",
+      revision: "4fc38dacb9cfc23730ad9865fcb95b7b9d9ebe69"
   license "Apache-2.0"
   head "https://github.com/dagger/dagger.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0badbc531d64aa8a1fee91f03cfed919be428bab7d7ce02192e2b9eecbbaf32d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3397d51bc458acbcfe9003fd55bd0fc086ed43f41d9f41a0c9cee48849aab809"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3397d51bc458acbcfe9003fd55bd0fc086ed43f41d9f41a0c9cee48849aab809"
-    sha256 cellar: :any_skip_relocation, ventura:        "1726c30e8192671268f2d0a08906aae1b4883dd802050b574c22c2904df4d235"
-    sha256 cellar: :any_skip_relocation, monterey:       "45e4853cf61137a5113b0301ab8e875ab93b01c22db62b91be4173c935cf55e6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "45e4853cf61137a5113b0301ab8e875ab93b01c22db62b91be4173c935cf55e6"
-    sha256 cellar: :any_skip_relocation, catalina:       "45e4853cf61137a5113b0301ab8e875ab93b01c22db62b91be4173c935cf55e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f16652a861ae384f578521d720ac4dcf9bf5280582d08e3f5da7a9c9a41f3f22"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dagger"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7351b286c713c432be29fe61deb45f44cd9671592ecf0c83168dca15bec00790"
   end
 
   depends_on "go" => :build
@@ -30,7 +24,14 @@ class Dagger < Formula
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/dagger"
 
-    generate_completions_from_executable(bin/"dagger", "completion")
+    output = Utils.safe_popen_read(bin/"dagger", "completion", "bash")
+    (bash_completion/"dagger").write output
+
+    output = Utils.safe_popen_read(bin/"dagger", "completion", "zsh")
+    (zsh_completion/"_dagger").write output
+
+    output = Utils.safe_popen_read(bin/"dagger", "completion", "fish")
+    (fish_completion/"dagger.fish").write output
   end
 
   test do
