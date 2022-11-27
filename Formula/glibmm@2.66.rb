a3861b58fa14a3bf0403edff319a4743520b4f62
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:11:42 +0800

    glibmm@2.66: no rebuilding required

--- Formula/glibmm@2.66.rb
+++ Formula/glibmm@2.66.rb
@@ -1,8 +1,8 @@
 class GlibmmAT266 < Formula
   desc "C++ interface to glib"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/glibmm/2.66/glibmm-2.66.5.tar.xz"
-  sha256 "7b384662dd6ec3b86c0570331d32af05db2bd99a791602b767b4a0b2566ec149"
+  url "https://download.gnome.org/sources/glibmm/2.66/glibmm-2.66.2.tar.xz"
+  sha256 "b2a4cd7b9ae987794cbb5a1becc10cecb65182b9bb841868625d6bbb123edb1d"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -12,14 +12,8 @@ class GlibmmAT266 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "2ffccba88c59692e588626434bc1657e50fb19883305ef1e3c2007cb577c2d7b"
-    sha256 cellar: :any, arm64_monterey: "05c14e78a13a1664c202444e90570e8d47ec3ef078d8ef09496e2fd053d4b649"
-    sha256 cellar: :any, arm64_big_sur:  "56b410adec1bde766b38a0ddf0d23be7521b5d7563b26c4311d4ce594ee132ee"
-    sha256 cellar: :any, ventura:        "1389e91603686af3a1a85759b584013f57369e4b42cd95919e0e9956a8316f18"
-    sha256 cellar: :any, monterey:       "50ad009e59740399ed6fc83a2037fa233cf9aad126bfdeb088cd56da56e6aa21"
-    sha256 cellar: :any, big_sur:        "188a58d8e551ea3c5903f0d4a30d15fe89b34ecc9ed1fcdbc25f8997f1ebecf9"
-    sha256 cellar: :any, catalina:       "d0a79fbae0da97c9c7f3f3e92be18cbccdff1a8fd16266f2f4fa4941a34124ed"
-    sha256               x86_64_linux:   "1219c3527ea4f63907facaa202a8719ce6b03c5aba6add906290a6607f2a32ae"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/glibmm@2.66"
+    sha256 aarch64_linux: "140f2a8595f422814a449618c48d161d35e0685ead503fba3959c30bec763b97"
   end
 
   depends_on "meson" => :build
