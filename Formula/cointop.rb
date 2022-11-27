0914527d0125cc1a71f1550ca0b3b392d8e8246b
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:44:42 +0800

    cointop: no rebuilding required

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -6,23 +6,14 @@ class Cointop < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1387a159944a0ae1559545b5bb501dcca6604d558852c6d8da0520758db8e6f0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6439f989983c4513b8d0744b6e05d56e7c79a7ca2e25ef6fa4ed0547c7aae90"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d21f1ec4932e705f02abb72f4df4e2131701ec33468669f5c237b27b550c56a"
-    sha256 cellar: :any_skip_relocation, ventura:        "bd19ac7d0982181775335fa3bffba8a68a31154df656ee2b14f69ff1137aca59"
-    sha256 cellar: :any_skip_relocation, monterey:       "e2120a9df18b3d4cb3bca96477d1a4849ed9bdf3b0310958b43d555f49bcaa5a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b320c63a712d387811bc0732362b0e0e25799fd3d28ff775272a459755d7efa7"
-    sha256 cellar: :any_skip_relocation, catalina:       "6d5851a8c19e542358d91a02a0a6c9dbac8571e50865403026620b24890f3ea1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ac984c14974abe0006d84d1e802a2eddb376e2603f88fd42b60decab2a8c2ad"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cointop"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "82be65ea7352e6a28154839cc876fca8edcd7b702f37642f17a4244106c89335"
   end
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-X github.com/cointop-sh/cointop/cointop.version=#{version}")
-
-    generate_completions_from_executable(bin/"cointop", "completion")
   end
 
   test do
