517c1af0947c89c859dfa9af6bdfe6a9266c48ba
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:17:42 +0800

    s3fs:

--- Formula/s3fs.rb
+++ Formula/s3fs.rb
@@ -6,24 +6,39 @@ class S3fs < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/s3fs-fuse/s3fs-fuse.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "40366ae4c296e3095014f8639d77d0ede2b0f8f960d305a8ed43f0b160702fff"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
-  depends_on "curl"
   depends_on "gnutls"
-  depends_on "libfuse@2"
   depends_on "libgcrypt"
-  depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "nettle"
+
+  uses_from_macos "curl"
+
+  on_macos do
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
+  end
+
+  on_linux do
+    depends_on "libfuse@2"
+  end
 
   def install
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking", "--with-gnutls", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
