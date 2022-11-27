9d97784c7ad24813a678c54ec4a18a1a9e67eb74
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:51:02 +0800

    dehydrated: no rebuilding required

--- Formula/dehydrated.rb
+++ Formula/dehydrated.rb
@@ -1,19 +1,13 @@
 class Dehydrated < Formula
   desc "LetsEncrypt/acme client implemented as a shell-script"
   homepage "https://dehydrated.io"
-  url "https://github.com/dehydrated-io/dehydrated/archive/v0.7.1.tar.gz"
-  sha256 "3d993237af5abd4ee83100458867454ed119e41fac72b3d2bce9efc60d4dff32"
+  url "https://github.com/dehydrated-io/dehydrated/archive/v0.7.0.tar.gz"
+  sha256 "1c5f12c2e57e64b1762803f82f0f7e767a72e65a6ce68e4d1ec197e61b9dc4f9"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
-    sha256 cellar: :any_skip_relocation, ventura:        "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
-    sha256 cellar: :any_skip_relocation, monterey:       "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
-    sha256 cellar: :any_skip_relocation, catalina:       "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f17795e23bdd54c28ee3b776ca2a23961d82ad07dd28b9d5081ae8edb2dae79c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dehydrated"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "72aba0ea1f34de27cb8f34462ab87665609991697ffdd8a7d4322870085d9a45"
   end
 
   def install
