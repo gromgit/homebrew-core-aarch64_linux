d0f69c303366f1f450c757b0b737c4068d5686c9
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:28:13 +0800

    tass64: no rebuilding required

--- Formula/tass64.rb
+++ Formula/tass64.rb
@@ -1,19 +1,13 @@
 class Tass64 < Formula
   desc "Multi pass optimizing macro assembler for the 65xx series of processors"
   homepage "https://tass64.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tass64/source/64tass-1.57.2900-src.zip"
-  sha256 "e4f0dcb1b00d5206ccb391eb5b8b43f4d95f064d63af739a71f5c822bbfc56a4"
+  url "https://downloads.sourceforge.net/project/tass64/source/64tass-1.56.2625-src.zip"
+  sha256 "c4e570c717c9500f3af61a3ad5d536f22415e2b29ed1eb09b1a955d310c9f3d3"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later", "LGPL-2.1-only", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "29936927064468f75aebd39e957f53c2aadf47369ae10a191612a2a326ac4bdd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "81dffbc4d2dea6237c3392b97a6834bbd584827bf6fc16066a8cee8ab15862ac"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01af2b2679d30efd3a934617ed618432fba9d307386b38fc3c299233132f03c1"
-    sha256 cellar: :any_skip_relocation, ventura:        "c906770fb3e1465f449359bb19c946f4be23b1e67fa3ed0123f633d749c9b9c4"
-    sha256 cellar: :any_skip_relocation, monterey:       "804fe305806f3d3db3db8bacbe64482c98c99696b91bdd80eb4b476ade9ffb68"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39cf8c55e040f0d21534f08db3107e61ca519006c7ddbfe2137655e113998afe"
-    sha256 cellar: :any_skip_relocation, catalina:       "65e1411ca3dbecc6885d71edf2c68c49b0180bc1b39a7373539150bf0469db22"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "30ff210040282ac6888c25cf193e72f399fb32e514cca8d1701a269c386a2270"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tass64"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "94e0e62184e1604895b6f3050214824eeded23452e43b73825c7ee186c9c7196"
   end
 
   def install
