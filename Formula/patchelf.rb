3f281b2037bd68f79824bbb2d3280614be3762f4
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:03:09 +0800

    patchelf: no rebuilding required

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -1,9 +1,11 @@
 class Patchelf < Formula
   desc "Modify dynamic ELF executables"
   homepage "https://github.com/NixOS/patchelf"
-  url "https://github.com/NixOS/patchelf/releases/download/0.17.0/patchelf-0.17.0.tar.bz2"
-  sha256 "45d76f4a31688a523718ec512f31650b1f35d1affec3eafeb3feeb5448d341e1"
+  url "https://github.com/NixOS/patchelf/releases/download/0.15.0/patchelf-0.15.0.tar.bz2"
+  sha256 "f4036d3ee4d8e228dec1befff0f6e46d8a40e9e570e0068e39d77e62e2c8bdc2"
   license "GPL-3.0-or-later"
+  revision 1
+  head "https://github.com/NixOS/patchelf.git", branch: "master"
 
   livecheck do
     url :stable
@@ -11,21 +13,12 @@ class Patchelf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bfd13c27623f1e5124c03f5709c549d10e3f2e0e905f60faef123850107508c9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "440e305a3adc81f6b01cc42604b141ccdf5cee901220ad7bc940ddaf71ab55d0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "346153244bb0701aec5108d11d88436a806109419fb96911c49ee4662d07ff0d"
-    sha256 cellar: :any_skip_relocation, ventura:        "f3d366b8048b40395f0e7330ce3d285ae0ab0079ba3dcc5be7957d357f46a237"
-    sha256 cellar: :any_skip_relocation, monterey:       "b0cae0c81b65ed64330aa0ffbda185767d196416b983fc42bd37d6ed5302151d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "02b25fa600076de2a1545d5436d775a0ef33dc6e39203b7fdb2a4ebb07083e5a"
-    sha256 cellar: :any_skip_relocation, catalina:       "355c7597512ef6cc4bb26848d5ee47dbdf6f2bd706f55c6c9ef5048edf8eb056"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8cb35e912221c30f6515caa71280a05b42ca9c51b7413b242582be0b9aea58d7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/patchelf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7a6aa5eaee3e20ad74d1f907ce22e14c0e9dde34c87ad4ec8a806ab126632d64"
   end
 
-  head do
-    url "https://github.com/NixOS/patchelf.git", branch: "master"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
+  on_linux do
+    depends_on "gcc"
   end
 
   fails_with gcc: "5" # Needs std::optional
@@ -43,7 +36,6 @@ class Patchelf < Formula
       ENV["HOMEBREW_RPATH_PATHS"] = nil
     end
 
-    system "./bootstrap.sh" if build.head?
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules"
