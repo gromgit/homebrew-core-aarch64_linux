931e12cb01de7e7f8376080554b7fe4f702ccbb1
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:27:58 +0800

    arp-scan: no rebuilding required

--- Formula/arp-scan.rb
+++ Formula/arp-scan.rb
@@ -1,20 +1,14 @@
 class ArpScan < Formula
   desc "ARP scanning and fingerprinting tool"
   homepage "https://github.com/royhills/arp-scan"
-  url "https://github.com/royhills/arp-scan/archive/1.9.8.tar.gz"
-  sha256 "b9b75ceaef6348f5951b06c773ec7a243a9e780e160eafebc369f9c27f6a7d3f"
+  url "https://github.com/royhills/arp-scan/archive/1.9.7.tar.gz"
+  sha256 "e03c36e4933c655bd0e4a841272554a347cd0136faf42c4a6564059e0761c039"
   license "GPL-3.0"
   head "https://github.com/royhills/arp-scan.git", branch: "master"
 
   bottle do
-    sha256 arm64_ventura:  "97e75dd94f16bfcdf8f65ebc0ff7c625f1cc9740ecbaadb12e20d7232a619344"
-    sha256 arm64_monterey: "889266ef3963fb2877df0a3445149d798c87f9317f493655598dd7cb540963a7"
-    sha256 arm64_big_sur:  "7d615e04cb2b99c9dfc38f0a58d35914bc6d2fc85e2c2beb16a4f8deacac1791"
-    sha256 ventura:        "107a9a124119b6f2f5ee0c722d9b32941b139b12419876035f341470986c3a8e"
-    sha256 monterey:       "0628ba78ca4469f86e41315df57427b976e30985e6af04d09b9111c27b772b6b"
-    sha256 big_sur:        "1c554bca28b60b7695a14ea881b6d73d8b434fa0991b1688e26c17fa5b23d334"
-    sha256 catalina:       "a2c73c508390e1871339f892bfadeb677c0e564bd59c7a9abd003946cbf8b333"
-    sha256 x86_64_linux:   "a5eda727915c262f7a5c9be0599785afd197b89e48bbc8fefc1fb2d81163d1b8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/arp-scan"
+    sha256 aarch64_linux: "90c46cbff11c7f7a253b80af296a89422d0425ae9210cd1b05ff69198bc69e47"
   end
 
   depends_on "autoconf" => :build
