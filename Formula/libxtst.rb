f4f78b043b244cb5ca15903f7f34bcf9d2b07f92
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:42:33 +0800

    libxtst: no rebuilding required

--- Formula/libxtst.rb
+++ Formula/libxtst.rb
@@ -1,19 +1,13 @@
 class Libxtst < Formula
   desc "X.Org: Client API for the XTEST & RECORD extensions"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXtst-1.2.4.tar.gz"
-  sha256 "01366506aeb033f6dffca5326af85f670746b0cabbfd092aabefb046cf48c445"
+  url "https://www.x.org/archive/individual/lib/libXtst-1.2.3.tar.bz2"
+  sha256 "4655498a1b8e844e3d6f21f3b2c4e2b571effb5fd83199d428a6ba7ea4bf5204"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "64703f879e6130e99a850a56567fc1a9de0024a0ed276a6eefa5768852c9870e"
-    sha256 cellar: :any,                 arm64_monterey: "3b8d22abd476602cb2892a68c31054a696e760aae1912dee00c08abaff710922"
-    sha256 cellar: :any,                 arm64_big_sur:  "41a2b4709a78d3b00dec19b298dc5551fe1eb2c530556c8daa75ef2f74527ed6"
-    sha256 cellar: :any,                 ventura:        "e9aa2923a5647444b5557ae761abdc05730c5c0bd836103853921ef9df38d4dc"
-    sha256 cellar: :any,                 monterey:       "4ff71b57fb57f02df2b01623684174c826203969bbbf49d9ac77c6f94b60f23c"
-    sha256 cellar: :any,                 big_sur:        "761a44edff4b064f0019e663d8cdd58753ad40910a6bd4fcea6c5e8f946ab3bf"
-    sha256 cellar: :any,                 catalina:       "4b7e04a2af86298d776f8a06da28a466a38f22aa4e24299b357ef7d0e11ec53a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ed487372cd98ae495904e2c0c9b2fcc96cc6d080f0a13be344a37d99849af35"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxtst"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8a99be04069a2b60de7f9182bb95969e6d1f33704e3144a60934e1fbac18d4b2"
   end
 
   depends_on "pkg-config" => :build
