8c332789cc831d4ef2bc80f624ae20a80fcbfe2b
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:58:43 +0800

    epinio: no rebuilding required

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -1,26 +1,19 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "a8dea6f83ae571774720fcbf5d25c237c1e057c50e39979adfbfeecfbb5616fb"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "2066d00a6d91cc1b5410b90cad4f88d8e1be9dc4c013db50e80c516bb25c07f7"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "62fa19622acf35ad4c14b45c0e831c93ff5dce1e21d5ec03210a4d9493a7957d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "92688b8d37f7ebd021750f76a6b77d4fa3b4dd499fe783b11f5935493c68b66e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ccf1cb26cb5a4af28a2916e823af6c51d73569164583b88d2649d0c8a88306da"
-    sha256 cellar: :any_skip_relocation, monterey:       "5c6d1eab60ba43716192bbbdcf70ae07a0475f9b0ecad25825fd5fc219dc626b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "59aec3058b61e835f41af499bd1bb7b5dac64eaa326c930edd03008e6ef7f2b4"
-    sha256 cellar: :any_skip_relocation, catalina:       "e00e09b5c9adb98e727090914b36d17c52d50fd903282028660a1ed41fe7dc1a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e44b87eaa00209c47a49c5e69135a49d25b005b2166abf955fd40140eb2841bf"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/epinio"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ada1882d879960a6eb5d1626b68eb444c18102b8e2c7b67abd3d4f69023abe7c"
   end
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/epinio/epinio/internal/version.Version=#{version}")
-
-    generate_completions_from_executable(bin/"epinio", "completion")
   end
 
   test do
