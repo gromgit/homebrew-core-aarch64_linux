b7d4bbe3f17f21f17938872eec668bfeba5b7573
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:53:13 +0800

    dnsmap: no rebuilding required

--- Formula/dnsmap.rb
+++ Formula/dnsmap.rb
@@ -6,14 +6,8 @@ class Dnsmap < Formula
   head "https://github.com/resurrecting-open-source-projects/dnsmap.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "62d1f525a5c4d2770b488d65670cde33d377a460987e5e0568eea506b592ebd0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8eb47816e6f0177b5e7a7358540055bf5d0346888bc921f6220ebd2e4a15cfda"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af20d1658eb8b3f6191712debd39b3ab21afe033da12fb1e6a94b413f17b1d84"
-    sha256 cellar: :any_skip_relocation, ventura:        "4c3b82bafb59e5ad12ecb2c8233f54bf9b218728e453aa9be75f42524f125cf3"
-    sha256 cellar: :any_skip_relocation, monterey:       "48eeee1b5697a45f09c625d67cd2780964e4183c94d9d7a667d267c0b56f2359"
-    sha256 cellar: :any_skip_relocation, big_sur:        "194967d9aa003034d0c6e8f917cc0adffd5dff7715e085f3c44521e44afa3fb4"
-    sha256 cellar: :any_skip_relocation, catalina:       "de4e15536fa71c6bf75f0821909002652eaf6b7c8a7d25c9229a85edddada4cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "165264ad85acecb8f79932782e3e0a1eba9110462ac627379ddc3d07ef4190b4"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dnsmap"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "333e48d86ea73b6a843160f9106ca701b9825f244ef676672b2897d11420b150"
   end
 
   depends_on "autoconf" => :build
