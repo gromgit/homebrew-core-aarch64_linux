27b0a4db944ab369bfdc021836ff126064574c6b
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:11:47 +0800

    glider: no rebuilding required

--- Formula/glider.rb
+++ Formula/glider.rb
@@ -7,14 +7,8 @@ class Glider < Formula
   head "https://github.com/nadoo/glider.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d41f829ef1719b56dbf81914c39279cc522bca246cfa7c12495150beba0fd112"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8339ced3042241d3f128068b05cffc5f8f728cb6ab073272e0f76081b099b1e9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffcafc5e458c78e115b94f45abfb105e69d02f7a03b5d065414fdc4b996ef251"
-    sha256 cellar: :any_skip_relocation, ventura:        "8de4d77aa7daa7ffd049b32aa97703522986e1cc075c941b6f687a68107e11c4"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b956d24977eaf0ad44ff2112ada966d7e77d26276b6751792241bbb68d1c487"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1b31c976c5a2d2c0e5e06a667cf2778c59a7d8bdf346c206efb455cce63f2232"
-    sha256 cellar: :any_skip_relocation, catalina:       "20dd59e4d32da80636537d41a57ebec46944ba3754141360a7f2b7b6d5a1c5d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38800c290f046908cfc31de29be0bcd4bf5f617c036a532d961d438c6004e601"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/glider"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "25bc0bd0198cc92791484211a02bf2340eb6939813e75756126ecfd8d1bf8a80"
   end
 
   depends_on "go" => :build
