21d35e71d2c12f0f1aa4fdc050149a2b8ef4d794
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:05:26 +0800

    frps: no rebuilding required

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -2,19 +2,13 @@ class Frps < Formula
   desc "Server app of fast reverse proxy to expose a local server to the internet"
   homepage "https://github.com/fatedier/frp"
   url "https://github.com/fatedier/frp.git",
-      tag:      "v0.45.0",
-      revision: "a301046f3dd728ef088843680b7fd13800ff1ba0"
+      tag:      "v0.42.0",
+      revision: "eb1e19a8212061e56a43ce798a231cd64cce989d"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d819ef8a09a39acd26cdd85d83069a7b13097736c152a16f454ccd91ce992041"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "312891500bba7909461d27c1de0631f93eaba6f9018aab8430f67dd22dbd027a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "312891500bba7909461d27c1de0631f93eaba6f9018aab8430f67dd22dbd027a"
-    sha256 cellar: :any_skip_relocation, ventura:        "ca431a070e967a173bc4a51c5b7893aee779ec7603f22fad24695d9146c59318"
-    sha256 cellar: :any_skip_relocation, monterey:       "811e8c154895e3f3d3fe795b16c2260f0f189080f28a1dbb54d5f2f3b77cf990"
-    sha256 cellar: :any_skip_relocation, big_sur:        "811e8c154895e3f3d3fe795b16c2260f0f189080f28a1dbb54d5f2f3b77cf990"
-    sha256 cellar: :any_skip_relocation, catalina:       "811e8c154895e3f3d3fe795b16c2260f0f189080f28a1dbb54d5f2f3b77cf990"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "995b4f8324a602002a993576773088328ad60f1862f0677efe618189f391ebc5"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/frps"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "dd0a885564f9feca300c323833e5998a21f21a8d346f6bdd7ba1225b1b4e7f20"
   end
 
   depends_on "go" => :build
