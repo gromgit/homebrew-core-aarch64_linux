16f3087371a4dd0429a214ba2bcac53d0f39f340
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:30:53 +0800

    tinysearch: no rebuilding required

--- Formula/tinysearch.rb
+++ Formula/tinysearch.rb
@@ -1,18 +1,14 @@
 class Tinysearch < Formula
   desc "Tiny, full-text search engine for static websites built with Rust and Wasm"
   homepage "https://github.com/tinysearch/tinysearch"
-  url "https://github.com/tinysearch/tinysearch/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "3330a8b91a83d9e452f71ba065778bc0eb2be0504f7707bd11b729bf501395e5"
+  url "https://github.com/tinysearch/tinysearch/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "a166ea3f0a5f0bfd490cb36b3360b51e40a278e138f40a4be601faf4d33f456b"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/tinysearch/tinysearch.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "27b1c4d80f00537eac5cf8e0c06ee7c5ec3694b9f0c64bde1cf83fd7b9d02a15"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4bcb1e61a1b15295ef1825801509801a4c11357da93f8b81e23bfcc59757e159"
-    sha256 cellar: :any_skip_relocation, monterey:       "c26c581707c1a5d317194a05c694715a6b6773df1ff2ce90d597a439990b5166"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e590809eeb3920451cad5946a72ed29c8307e4eb99d5f01ca6d442f50e620406"
-    sha256 cellar: :any_skip_relocation, catalina:       "df5917867187c13145ea83ad38f9f285b2597073dbdb434d30c692b15b8d145e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb1db2583422d8eddef366f8f587ef875add2657552bfec8e467fc3798f3c32e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tinysearch"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b25dca61515110bed18f66f0598b498a1431087024555e01a012a45b26bad278"
   end
 
   depends_on "rust" => :build
@@ -30,7 +26,7 @@ class Tinysearch < Formula
     ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
 
     system bin/"tinysearch", pkgshare/"fixtures/index.json"
-    assert_predicate testpath/"wasm_output/tinysearch_engine_bg.wasm", :exist?
-    assert_match "A tiny search engine for static websites", (testpath/"wasm_output/package.json").read
+    assert_predicate testpath/"tinysearch_engine_bg.wasm", :exist?
+    assert_match "A tiny search engine for static websites", (testpath/"package.json").read
   end
 end
