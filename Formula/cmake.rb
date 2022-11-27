0cf6740914685764dfae4fb45c6123c075eeef7c
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:43:50 +0800

    cmake: no rebuilding required

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,10 +1,10 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.25.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.0.tar.gz"
-  sha256 "306463f541555da0942e6f5a0736560f70c487178b9d94a5ae7f34d0538cdd48"
+  url "https://github.com/Kitware/CMake/releases/download/v3.24.1/cmake-3.24.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.24.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.24.1.tar.gz"
+  sha256 "4931e277a4db1a805f13baa7013a7757a0cbfe5b7932882925c7061d9d1fa82b"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
@@ -16,14 +16,8 @@ class Cmake < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "80c206a6884a52914d09c72ff2e64079ae0d99aa7fde58dea73de0faba8f6d6e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ed5aef087d93f4df171e82f72df5c6815ce7151dbd4b370e70d38e089603c37"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f440fd5a4495878ffab7e1008a6c874d4606f68d514f154520a55e4194c8a680"
-    sha256 cellar: :any_skip_relocation, ventura:        "2466f08623841c1acc932331e5b44fdf9a987e5c074d8605fac96ebf328ad080"
-    sha256 cellar: :any_skip_relocation, monterey:       "c472ef54bbaf367467ba85887895f987835bb2843a1c74a76eb05edc7b1c12b9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "532b702a317ab864e46ca7b6c3049183fbdad97e4fde66ef341bfae147870d49"
-    sha256 cellar: :any_skip_relocation, catalina:       "3ce6b4c1f589cb3dbd649deaca28de53ad8f87c677aff56aacdded5d4d720f54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ac2590421355917f7f5fe948fca4f135fcf33c735a51c12be66436bc069423b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cmake"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "703b692ea82a5781c6b7fc1417d53cf55baabdb716fa106ab2418088a668a957"
   end
 
   uses_from_macos "ncurses"
