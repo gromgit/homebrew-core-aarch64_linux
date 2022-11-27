aa0deaa5ee6e2bf6174123d51bb3cb5e4544fc99
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:42:01 +0800

    libxml++@5: no rebuilding required

--- Formula/libxml++@5.rb
+++ Formula/libxml++@5.rb
@@ -1,8 +1,8 @@
 class LibxmlxxAT5 < Formula
   desc "C++ wrapper for libxml"
   homepage "https://libxmlplusplus.sourceforge.io/"
-  url "https://download.gnome.org/sources/libxml++/5.0/libxml++-5.0.2.tar.xz"
-  sha256 "7c17cc3e5a2214314dee5a1354f4b436f266ded6185232a0534f352a76240d5a"
+  url "https://download.gnome.org/sources/libxml++/5.0/libxml++-5.0.1.tar.xz"
+  sha256 "15c38307a964fa6199f4da6683a599eb7e63cc89198545b36349b87cf9aa0098"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,14 +11,8 @@ class LibxmlxxAT5 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "a0a175d17f147bbb84833ef4d68817287eca7f06727b079db2bff59fa68c4dac"
-    sha256 cellar: :any,                 arm64_monterey: "9421a78d289abe9cbe85aae2d4b35be65d3cdc142f6c7fce8fc5e5e1d29ae747"
-    sha256 cellar: :any,                 arm64_big_sur:  "28eb0093a129b7b8684db54399ccf65f57caa15534d29c91a5ee5912afcb5749"
-    sha256 cellar: :any,                 ventura:        "f264149e96b1915be258811fdf981d1a4ef791a1eb4dc70054f280e40f786a22"
-    sha256 cellar: :any,                 monterey:       "4a1d6468ae69ad46ac361d401b97fd435eb152674ee17d28c1af7c182d0c4091"
-    sha256 cellar: :any,                 big_sur:        "506f4ffdf7491d11af9ac6b9cb55a7e189298040a74b10e83a5159e7ca5839a7"
-    sha256 cellar: :any,                 catalina:       "3fee4844226ffcd22178834583d01a4d9de309d1dccd19f15639e7391c384c0f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61f50f7e65f056db4354f9c22b9d665617fa57d69e0b511b309120a86b205d0a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxml++@5"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "05acd3d28c275fa143b146c83b3e3b7ce17fe4a91e14b4817fc88737aba64ddc"
   end
 
   depends_on "meson" => :build
