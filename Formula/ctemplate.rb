8d3b0d5e9dd90b79e6f3056fa4e2d56ee3c7289d
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:48:17 +0800

    ctemplate: no rebuilding required

--- Formula/ctemplate.rb
+++ Formula/ctemplate.rb
@@ -8,21 +8,18 @@ class Ctemplate < Formula
   head "https://github.com/olafvdspek/ctemplate.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_ventura:  "2d9ca0bd9eed0b2c8beb26822d3da0a59c9a818cf6288d2737ccd090d5e7431c"
-    sha256 cellar: :any,                 arm64_monterey: "fc6f772b98ecb4ac32af2525fb14e00e78d3869965b0b2cef4e9052ae7920f15"
-    sha256 cellar: :any,                 arm64_big_sur:  "4857a36c04ea358a584ccc4bf814cf14ea132f719044982bba4295ca3bee54ce"
-    sha256 cellar: :any,                 ventura:        "64a6958f3a69142eb06780025183147a59f602862f961a80f89b5f37b164a4ca"
-    sha256 cellar: :any,                 monterey:       "1186d9da15756058d715022d75e654ddf8c30573f79490a6d945ba3501e651d9"
-    sha256 cellar: :any,                 big_sur:        "5e2edf873c559dbe6fee25404fb5f73dd0237358d553384c65284db99c1e1aeb"
-    sha256 cellar: :any,                 catalina:       "03716a690ca0715b4832e3cbc95441a3e9765c36b277a33f85ac7806469d9928"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0782447bd58da47c13e2c3fc367e616addbe434c4fef762daea8bf6ea0b85626"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ctemplate"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5b0b04d251c4095db9b2ebf4774a955af800e214faa79a3bad68090d4506beb2"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "python@3.10" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5"
 
@@ -44,8 +41,8 @@ class Ctemplate < Formula
       }
     EOS
 
-    system ENV.cxx, "test.cpp", "-std=c++11", "-I#{include}", "-L#{lib}",
-                    "-lctemplate_nothreads", "-o", "test"
+    system ENV.cxx, "-std=c++11", "-I#{include}", "-L#{lib}",
+                    "-lctemplate_nothreads", "test.cpp", "-o", "test"
     system "./test"
   end
 end
