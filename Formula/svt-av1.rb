8984ed34d1ddf46b12a99c36421262b286e4c3da
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:26:50 +0800

    svt-av1: no rebuilding required

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -1,28 +1,21 @@
 class SvtAv1 < Formula
   desc "AV1 encoder"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
-  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.3.0/SVT-AV1-v1.3.0.tar.bz2"
-  sha256 "f85fd13ef16880550e425797bdfdf1b0ba310c21d6b343f74ea79dd2fbb2336e"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.2.1/SVT-AV1-v1.2.1.tar.gz"
+  sha256 "5c2466eb2990fa045b29c63b92a2b5f13d9cb841709df5e7206f96a59e119fdd"
   license "BSD-3-Clause"
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d7a9df8e0ff19e369b6616c12119d9aca9a6582af11dbca64c2ae39eb8ece137"
-    sha256 cellar: :any,                 arm64_monterey: "84e557b94d5ba4fe6ae3afae4bdb271b86386c54c86d83eca4e6294e61fa18f2"
-    sha256 cellar: :any,                 arm64_big_sur:  "5bca5860ba31ce2c9235febd699f3b65ab8dd40f8781b04b641d8294e07ba002"
-    sha256 cellar: :any,                 monterey:       "57aaf6debc48668cf445fcdd9df23d7b6be98754b5dd47b8890848bf3bc54ff3"
-    sha256 cellar: :any,                 big_sur:        "855688c54dbabc9ea9364ffbabd0cdb1e419a53c613835ce391fb0a2efcd983f"
-    sha256 cellar: :any,                 catalina:       "a6f81e578b515c4a8467adfa7705a2a0551002025f84a6c5f79a9ce81d4cca37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48b23231c32b44cb3969a5dfb97b649162477b729174f718d9a87882da7acf74"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/svt-av1"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "474ee5a4556f263b3d09d15f8c77acf1aaae5bb8b519870eefec1accf07f38dd"
   end
 
   depends_on "cmake" => :build
+  depends_on "make" => :build
+  depends_on "yasm" => :build
 
-  on_intel do
-    depends_on "yasm" => :build
-  end
-
-  resource "homebrew-testvideo" do
+  resource("homebrew-testvideo") do
     url "https://github.com/grusell/svt-av1-homebrew-testdata/raw/main/video_64x64_yuv420p_25frames.yuv"
     sha256 "0c5cc90b079d0d9c1ded1376357d23a9782a704a83e01731f50ccd162e246492"
   end
