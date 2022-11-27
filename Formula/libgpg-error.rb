02e1cf2ac6086992484a6652d6a2bd11326196d8
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:35:13 +0800

    libgpg-error: no rebuilding required

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -1,8 +1,8 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/related_software/libgpg-error/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.46.tar.bz2"
-  sha256 "b7e11a64246bbe5ef37748de43b245abd72cfcd53c9ae5e7fc5ca59f1c81268d"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
+  sha256 "570f8ee4fb4bff7b7495cff920c275002aea2147e9a1d220c068213267f80a26"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,30 +11,22 @@ class LibgpgError < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "1955fb0ad761875d609cce71140d83b0b313d0b8e09f7a4d10de021aa04a474a"
-    sha256 arm64_monterey: "9cfc7363abb102b284578c13d0e9b99a786bcd262e159541eebf6283c21c66c5"
-    sha256 arm64_big_sur:  "c023005814ffc801b659d5819ce7dc3aef4906fa538b90ad5526bfbc5fdd8a98"
-    sha256 ventura:        "13b643a6d383301f0df8dc7904398cda155d4417a4092adb6880881d6f21f981"
-    sha256 monterey:       "ffd831497a499fd7af47d69ab535a2eea51fcc85cea1db07ca748da77f64272c"
-    sha256 big_sur:        "bbc14e9b5e8e37b7ce5a498439097d4c3fe33c9126f9266f5c3089ec674c002c"
-    sha256 catalina:       "80e9fc05b831780e67221d972957088c15b644d83f685ffed9db22d4587f27c2"
-    sha256 x86_64_linux:   "d7d28c36ed2465543dd68313819bb940dcf0d0b5592af36199294801cc458681"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libgpg-error"
+    sha256 aarch64_linux: "bef87cb7b40c935a6f99dc59a4f36bfa9a639f8280f7f3fa94349d5f8b79ee6a"
   end
 
   def install
-    # NOTE: gpg-error-config is deprecated upstream, so we should remove this at some point.
-    # https://dev.gnupg.org/T5683
-    system "./configure", *std_configure_args,
+    system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--enable-install-gpg-error-config",
+                          "--prefix=#{prefix}",
                           "--enable-static"
     system "make", "install"
 
     # avoid triggering mandatory rebuilds of software that hard-codes this path
-    inreplace [bin/"gpg-error-config", lib/"pkgconfig/gpg-error.pc"], prefix, opt_prefix
+    inreplace bin/"gpg-error-config", prefix, opt_prefix
   end
 
   test do
-    system bin/"gpgrt-config", "--libs"
+    system "#{bin}/gpg-error-config", "--libs"
   end
 end
