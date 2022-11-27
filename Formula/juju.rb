41c22b6430645e2ce2d7eb2d2519cdf17346a7a8
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:27:34 +0800

    juju: no rebuilding required

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-3.0.2",
-      revision: "8bf53dc35b25145ef39051fe4136135a3dd53d5d"
+      tag:      "juju-2.9.29",
+      revision: "2ad800a0f8585e139eff34a1e214aac2da8e7073"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
@@ -14,14 +14,8 @@ class Juju < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7080119ffb41e36f11e42c9645edc231381a7f8f299be375dea8249324f6a9f9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1d504a7893af0552007ac3b9fdb8c9380482f6b3b1f43d7f7d1ceca6479ad80"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b0dddd35d64a8090b8ac9c51716f0b7af858bcaa6d82291a8513653b749b2047"
-    sha256 cellar: :any_skip_relocation, ventura:        "4bdbd30d3b480ac656c70a436f28839ea7f697d856778cdca9c592468106785c"
-    sha256 cellar: :any_skip_relocation, monterey:       "66c13349be3a70902a1978a55c276855393231072adb47ac210e04399f1fb761"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b37311ebb1a302af60a867036e4b0c82aea57c741123821ee32f3fe07e79fddd"
-    sha256 cellar: :any_skip_relocation, catalina:       "4397d949c37e99bec5272ce283b8c406dafe04c13dcea21fc3059d867bd67c1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3fbff94f3dab3836594a69f77ce8de90a96659f266b15c41ec7b64bc1eab4312"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/juju"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9aaf7848fb8bd88634d2c64d441ea21a1ca701c9b7a4a2475618fad004662a6e"
   end
 
   depends_on "go" => :build
