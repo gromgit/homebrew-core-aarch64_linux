f2d3c98feb8c42959a879658ffa7706a19e50f69
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:42:14 +0800

    libxp: no rebuilding required

--- Formula/libxp.rb
+++ Formula/libxp.rb
@@ -1,8 +1,8 @@
 class Libxp < Formula
   desc "X Print Client Library"
   homepage "https://gitlab.freedesktop.org/xorg/lib/libxp"
-  url "https://gitlab.freedesktop.org/xorg/lib/libxp/-/archive/libXp-1.0.4/libxp-libXp-1.0.4.tar.bz2"
-  sha256 "81468f6d5d8d8f847aac50af60b36c43d84d976d907ab1dfd667683dbfb5fb90"
+  url "https://gitlab.freedesktop.org/xorg/lib/libxp/-/archive/libXp-1.0.3/libxp-libXp-1.0.3.tar.bz2"
+  sha256 "bd1e449572359921dd5fa20707757f57d7535aff1772570ab2c29c6b49b86266"
   license "MIT"
 
   livecheck do
@@ -11,14 +11,8 @@ class Libxp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "f92106b34661b7a8d39636a544ee208e724b2ac68395ca4a9b2ef264359190f9"
-    sha256 cellar: :any,                 arm64_monterey: "c2e8285bdd8edb318e57e2b9d47e692d283cc05ac7ba811468ce946a9070fa1f"
-    sha256 cellar: :any,                 arm64_big_sur:  "e44f5fc9fafc1b6c40c6051921f6de14d2a0d6a01c5fd9715341bb77e5ccd144"
-    sha256 cellar: :any,                 ventura:        "f23541f38685321e6ff2d041a19391b2a7e88e788e794170ba8fed668134f36d"
-    sha256 cellar: :any,                 monterey:       "afa942a7ef9f5244bcfd7ce8e61b8235e3085f41bdc521bb3c930eb9402ff8bb"
-    sha256 cellar: :any,                 big_sur:        "ebf2ccca3126f773869610f9ca07888226e6caf7ab90a3b493aeadbf81354022"
-    sha256 cellar: :any,                 catalina:       "0687d2c037f00590ebf445e0b7f5531a703ddd8390dc903eaacbd451fdd10a6c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff2541a82e9806bac37aedb6fcaa83846e4f4d8710d4ea632a09fe826746da99"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxp"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ab7a73707e5130d7e78ad8af46b0fccd50672b27fe37b2da38a66e4b5c8e01d3"
   end
 
   depends_on "autoconf" => :build
