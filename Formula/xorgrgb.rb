e6cafba6d23bccf44b17ba3df04681f2f028f6f4
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:42:09 +0800

    xorgrgb: no rebuilding required

--- Formula/xorgrgb.rb
+++ Formula/xorgrgb.rb
@@ -1,18 +1,13 @@
 class Xorgrgb < Formula
   desc "X.Org: color names database"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/app/rgb-1.1.0.tar.gz"
-  sha256 "77142e3d6f06cfbfbe440e29596765259988a22db40b1e706e14b8ba4c962aa5"
+  url "https://xorg.freedesktop.org/archive/individual/app/rgb-1.0.6.tar.bz2"
+  sha256 "bbca7c6aa59939b9f6a0fb9fff15dfd62176420ffd4ae30c8d92a6a125fbe6b0"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "25ddac19c5361bd478b8c9ac4fab8210be7b8811f9b2edf586156604badfce23"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d9bb5c41b1ef76f04324db74b789330210d48be78c751ce4d0439e3b24a8b49"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30ee14ffdbb2418cc39884775ba8b61ab19452cd3e4e95609d5c67ac2bf56013"
-    sha256 cellar: :any_skip_relocation, monterey:       "8a5606f9cdba7608ebc27042ced7920658ed39593a0338a0d02ccdd067290fe4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "db4fb9cf83a6fcc75036c4c3b3fdac6ac0fe47108311a1f83a35ebbe97e7f1e7"
-    sha256 cellar: :any_skip_relocation, catalina:       "4f39373ce62247b2a47ff8f0a02fbdcb9af7e280aa7b1fa7443865024a0f561c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e6c3eca82aa9624b8b521d9d0e5cec68b9391538717c36c4d438edc75bb0085f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/xorgrgb"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "040c4342e8c884607ec123b87a1a8ea63334342552fd50ed42e3301ce7b2a1eb"
   end
 
   depends_on "pkg-config" => :build
