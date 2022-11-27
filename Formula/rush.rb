6934a08d9257badeb2fd97a430590e2e99162433
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:17:20 +0800

    rush: no rebuilding required

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -7,13 +7,8 @@ class Rush < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 arm64_ventura:  "edac738221df3600f641718d8ef503d74b91f8b1d70152908d8f4e8543ea1f3f"
-    sha256 arm64_monterey: "c40ad559ccaebe83031cb3118bdcc2ade53433fd9d733a3e750a8593e44c54d4"
-    sha256 arm64_big_sur:  "c3031fa858e73ad07c42be20c11c97f8b8b46c52f3ae9c695fa5191751db7784"
-    sha256 monterey:       "5833155618b0bf4e4a0e2ee646a7f8585c4ef541f5548911d4eb09af38131327"
-    sha256 big_sur:        "b9fe9b70c28fb4bbe8427f1465cd9b99deabdbf10de7260801f95d1181f8e8b2"
-    sha256 catalina:       "e382adfc56df5e2fd3075f9183e0513ce85ed7358699f935272d57411d7979b9"
-    sha256 x86_64_linux:   "d00ab93f179b7714db5dd3eb7f2679f31cfa2fda5596387d9d6f310a6a931129"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rush"
+    sha256 aarch64_linux: "ec29ac1432e0ee7e738e8f1b351ad04b278be4bbfaa055e96a7857eb0bd5dabd"
   end
 
   def install
