a97b24d6e851da96e23c0e95e1c6c37bbe630d59
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:21:50 +0800

    hugo: no rebuilding required

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,20 +1,14 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.107.0.tar.gz"
-  sha256 "31d959a3c1633087d338147782d03bdef65323b67ff3efcec7b40241413e270a"
+  url "https://github.com/gohugoio/hugo/archive/v0.102.3.tar.gz"
+  sha256 "5fab29ababef5294c891d687aef027c423771d4ad33af10c69f18ff9754d283b"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f8094c5fd7847a9f8bb146578392ae60ed0e6f7fb47f4d5f3ce40fb07d9c2925"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4d5655c87621f8aeb67489806fc24474876dfcc02cd932acced539890a1a5039"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d41d8af7990c1426e44fb3d4f194545b18df0150e153b34d515aec3118d68d52"
-    sha256 cellar: :any_skip_relocation, ventura:        "6e00a37f952ad854c8d1f3206a12b0566f9af16abd0facec3d05b8c4cb65cafc"
-    sha256 cellar: :any_skip_relocation, monterey:       "e53bd439f7763013906e2a7a993bbbb10859fe4fd1839745dc8e87fec791360a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "94fa5e5d60cd28b8481a9d91c0cdf2815abd09b1f5f044187f44306cb29e1581"
-    sha256 cellar: :any_skip_relocation, catalina:       "b7caa2b21c631fe493fe56f493e797630b810095a97100fe179ff702bb5f8929"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8fa07b9cfe2d4e30360a30f9ce88bd42512ecc5172acaf5b71f2646d6264fad1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/hugo"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4ff56c565500feb8fe32aa88282e8a28b33087b3a7316cfdb2c41525f6a0064a"
   end
 
   depends_on "go" => :build
