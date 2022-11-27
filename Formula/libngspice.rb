cc7a5add24223f20befe1d84d16af331206f5a97
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:37:19 +0800

    libngspice: no rebuilding required

--- Formula/libngspice.rb
+++ Formula/libngspice.rb
@@ -3,8 +3,8 @@ class Libngspice < Formula
   homepage "https://ngspice.sourceforge.io/"
 
   stable do
-    url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/38/ngspice-38.tar.gz"
-    sha256 "2c3e22f6c47b165db241cf355371a0a7558540ab2af3f8b5eedeeb289a317c56"
+    url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/37/ngspice-37.tar.gz"
+    sha256 "9beea6741a36a36a70f3152a36c82b728ee124c59a495312796376b30c8becbe"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
@@ -18,14 +18,8 @@ class Libngspice < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "1cc7171cb7d6abde17e2dc04d8a508a53818d643f094216e1952fd3343a703ce"
-    sha256 cellar: :any,                 arm64_monterey: "6050333fe137e8588d00246b64d24d09691a6b9d71ad2609493f3a967915ff28"
-    sha256 cellar: :any,                 arm64_big_sur:  "f939d3c2d40d5cc3b3264202453dfa8b18c77dfa9d9fa3fbb31205f0aae88ab9"
-    sha256 cellar: :any,                 ventura:        "48489a02e4433452e849f84c7dc7b28c34ed43a94e0aad25921afc43f8a07b32"
-    sha256 cellar: :any,                 monterey:       "d79b24f3980fecb7b6d1b52529be345edece474121efbc8636da106d6f2a3f86"
-    sha256 cellar: :any,                 big_sur:        "61d787f0d5358831155d1c3d1238e7ca00b25a54865496e9190919e8007ff3fa"
-    sha256 cellar: :any,                 catalina:       "170c8aae880863774486a8d74b54971a3e78c8fd6e8d151f303fcd89cc728541"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "612e1aa668db6d7f3574c846af876fa6a09e45944e74e6a1213d096ab4dbb3c1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libngspice"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0d58c488aa086aade836bcbd946f40cd58932490a995d371afa9f5e8411e32f9"
   end
 
   head do
