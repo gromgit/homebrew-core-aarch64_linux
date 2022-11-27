57a831fc079cfc43a9300aba6afee841860e4254
Rui Chen <rui@chenrui.dev>
2022-11-27 08:48:11 +0000

    packmol: update 20.11.0 bottle.

--- Formula/packmol.rb
+++ Formula/packmol.rb
@@ -10,6 +10,7 @@ class Packmol < Formula
     sha256 cellar: :any,                 arm64_ventura:  "f9b0671562d5d5c490af41b77af3e4e62708883a2a2f9aa465fd42df7471c85a"
     sha256 cellar: :any,                 arm64_monterey: "4f1afb57de63afe102cedf900ae3e6fd7d3ed6eaee7d4221f6c1c9453813b931"
     sha256 cellar: :any,                 arm64_big_sur:  "3adfa0f57afadf605e10d6ffbad8270683fa2a289bffcb56d4d5389af76a1fe9"
+    sha256                               ventura:        "299ed04aa9c3fa0f9f315c11bf6f9a2efb56bab618c2386096cb516880014938"
     sha256                               monterey:       "b8456629364d20ecd067449dbcd38dde85f5fe09069129c71602f07ec1097c0d"
     sha256                               big_sur:        "8a31e699d23377960bf668e5c715f6ee13b7514a287e7bfed3ecb81aa7223078"
     sha256                               catalina:       "f3320c503509b9fe783c5953ef0dd25818ee13d310beafdb9bf048b126d450c9"
