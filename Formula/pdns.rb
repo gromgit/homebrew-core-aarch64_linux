b673798bf54773b3ad2e3e64fae033dcaa0c30f3
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:04:06 +0800

    pdns: no rebuilding required

--- Formula/pdns.rb
+++ Formula/pdns.rb
@@ -1,8 +1,8 @@
 class Pdns < Formula
   desc "Authoritative nameserver"
   homepage "https://www.powerdns.com"
-  url "https://downloads.powerdns.com/releases/pdns-4.7.2.tar.bz2"
-  sha256 "4dcae35ebdc04915872d7bf6e2d0bca4b05c6350a100a5cf9c29df53baa53ce2"
+  url "https://downloads.powerdns.com/releases/pdns-4.6.3.tar.bz2"
+  sha256 "acd06b89ca01d1adf61b906604614f0e1d77a1e94eeecade8ff5d53a16db7389"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class Pdns < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "f9fb4470bed7a5c12f9b6fcf01c330da80c5f8847d5c94c318d8a7ddd36ca604"
-    sha256 arm64_monterey: "0bc2833a1410994a4cd10342597f0fc3b686006eda11d770521e97790fe12543"
-    sha256 arm64_big_sur:  "cec0ef3944878a458038437c3a5769c848b3bfb773f9631733cc5bafd456d883"
-    sha256 monterey:       "27d45e5b8aeb8389964f397b0c1f13a4a47677cdf49160790decf03e40da05c1"
-    sha256 big_sur:        "857b7b5da938ef75e0cff5534580b0fb09f424c9d8895d0d655ea2d52da6f15c"
-    sha256 catalina:       "56e71f15270192931fa11e8b137ca3e8ab73629c33a052f7cb09ef4c49fa44ff"
-    sha256 x86_64_linux:   "223164b460f222660027359d46e16e4c17898aeb4276e53fe5155fea7a35f9f9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pdns"
+    sha256 aarch64_linux: "6f781d8fdaefa289ea3c1431a1e46cacd35f66c8365677c81643708e3a2d1e8b"
   end
 
   head do
@@ -37,7 +32,11 @@ class Pdns < Formula
 
   uses_from_macos "curl"
 
-  fails_with gcc: "5" # for C++17
+  on_linux do
+    depends_on "gcc" # for C++17
+  end
+
+  fails_with gcc: "5"
 
   def install
     args = %W[
