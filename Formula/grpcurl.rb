26d234b93f9b50372a4f16161f5d69104ec30986
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:16:56 +0800

    grpcurl: no rebuilding required

--- Formula/grpcurl.rb
+++ Formula/grpcurl.rb
@@ -6,14 +6,8 @@ class Grpcurl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "12c372c78ff682370a954b7e8f6faa2a432e6b1b9eb4f5adcd5942a687095aa6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "37d01a36b9911c64b6a32ee44d1ed9eb148fb1844df0b81c1b19c235ed7fd907"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "81421aa8f13a8d6993aa60172b0252ac503daec9980441af6ee025af58eb8acb"
-    sha256 cellar: :any_skip_relocation, ventura:        "bd1f6f8ee21b8ba12d83d7211e8006ca873c5f3a692c36f573be8c5202552c0f"
-    sha256 cellar: :any_skip_relocation, monterey:       "7f7e652c6277205d1ae756475b283fa750eaf7aa3325be9ea08ba33f4939a040"
-    sha256 cellar: :any_skip_relocation, big_sur:        "12f6441cce946edee12c07729ab0e9b433dc44094b57dabd3e4e6e1864b4a811"
-    sha256 cellar: :any_skip_relocation, catalina:       "6bb6275586993be3b1b9f9db7ad86a91d12e733815d7cb89141dca02b0b1ba54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2ff19dc12a0e3df9384e6431a8f8f35e8ecd9527694a837f9a750121c58778b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/grpcurl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b8c1f9929a7f47d14250614e46dad547ce13d44b2107896e2d4a8f942cf8e5ee"
   end
 
   depends_on "go" => :build
