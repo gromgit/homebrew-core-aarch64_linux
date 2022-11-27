7d630aa36f3f24dc6be19954ce8cb77304209be2
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:50:31 +0800

    dcm2niix: no rebuilding required

--- Formula/dcm2niix.rb
+++ Formula/dcm2niix.rb
@@ -13,13 +13,8 @@ class Dcm2niix < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "657f60237a85e594706e0794278eb90dec747759fb42ddf9a31d27d9a81a22ac"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "256379b33e4ee7fd98c343830fd473dffed638a434adfb44b82da00312ad7590"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb06fe151b7302db9b217653395041b83d5ba52f78c3482f8db2c7b0fa90e8a2"
-    sha256 cellar: :any_skip_relocation, monterey:       "c23e27972dc9bcae0eb01a0452facd60f8d61a3389ee93fe30d627b31157a104"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3e4dd1ee7e3fffb35945119ebda76e9756456e8ffa1f4bba32f1291f019472cd"
-    sha256 cellar: :any_skip_relocation, catalina:       "b1efadda2cc14cd0ba5d995c1026f83139105da4deaaa7eadd9351494ddeee83"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a34aabf5974cd3143c6440bd76610ee7f2d6bf52fe5d18c43735cb96fbc026a6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dcm2niix"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "73d1b4b87e6cd913d5fba45ae07af9aea338b120e375350911a6ea82e4ec55d2"
   end
 
   depends_on "cmake" => :build
