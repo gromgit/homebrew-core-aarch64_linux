4d6d1df7598e5ec03e349db182d6973c9c61a884
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:55:08 +0800

    drone-cli: no rebuilding required

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -2,28 +2,23 @@ class DroneCli < Formula
   desc "Command-line client for the Drone continuous integration server"
   homepage "https://drone.io"
   url "https://github.com/harness/drone-cli.git",
-      tag:      "v1.6.2",
-      revision: "24bea58512640de52ef16c45f48645e255d8b46c"
+      tag:      "v1.5.0",
+      revision: "92e84c4e2452f82ad093722d87ad054e1821805e"
   license "Apache-2.0"
   head "https://github.com/harness/drone-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "66172d41d0d98db6f993146668fb40701f2da05a66f9b359ad12ef51dbd7af90"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "66172d41d0d98db6f993146668fb40701f2da05a66f9b359ad12ef51dbd7af90"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "66172d41d0d98db6f993146668fb40701f2da05a66f9b359ad12ef51dbd7af90"
-    sha256 cellar: :any_skip_relocation, ventura:        "0972102d85e71a67a2488480584d28b118805f68cfb384ec6bdb436c3b1bedb7"
-    sha256 cellar: :any_skip_relocation, monterey:       "0972102d85e71a67a2488480584d28b118805f68cfb384ec6bdb436c3b1bedb7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0972102d85e71a67a2488480584d28b118805f68cfb384ec6bdb436c3b1bedb7"
-    sha256 cellar: :any_skip_relocation, catalina:       "0972102d85e71a67a2488480584d28b118805f68cfb384ec6bdb436c3b1bedb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d58eb66b4f3a159821ddd80051873d60288b0cb3903a2b83f6163e321798f44"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/drone-cli"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8ecbdaae4675cc09dd31f0269f5c70271de254eac0af7532721c8c5f08a24329"
   end
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    ldflags = "-s -w -X main.version=#{version}"
-    system "go", "build", *std_go_args(output: bin/"drone", ldflags: ldflags), "drone/main.go"
+    system "go", "build", "-ldflags", "-s -w -X main.version=#{version}", "-trimpath", "-o",
+           bin/"drone", "drone/main.go"
+    prefix.install_metafiles
   end
 
   test do
