dd19718d27a31fd9574afa677288353cfb5a2f74
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:41:14 +0800

    checkstyle: no rebuilding required

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -1,12 +1,13 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "https://checkstyle.sourceforge.io/"
-  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.4/checkstyle-10.4-all.jar"
-  sha256 "c7fd77aea6f734e3f830bd87a1d764991ac4c0102c1bded9da22cba090aed8f4"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.3.3/checkstyle-10.3.3-all.jar"
+  sha256 "2784f2ea76e50b29517f21dcc6a0d727a2c087683bf9c0feb8da2b5b5126c187"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7ec2acf0ae806b098374d3eb41261b645fbbce791289772ad1775179cfd31db6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/checkstyle"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c59677e38ce766ecfe40d801c4b4d042d2e8ba0f6403d1fb9a76142840f75c0a"
   end
 
   depends_on "openjdk"
