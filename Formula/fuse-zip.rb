f8593d53f3a1fdc132d36d2a03d2e740a9764fa3
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:05:58 +0800

    fuse-zip:

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -6,17 +6,31 @@ class FuseZip < Formula
   license "GPL-3.0-or-later"
   head "https://bitbucket.org/agalanin/fuse-zip", using: :hg
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5e038b72ce4c58d28d4a873c9ea4ba304d9cf6618d772b3397a2d682911cf568"
+  depends_on "pkg-config" => :build
+  depends_on "libzip"
+
+  on_macos do
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
   end
 
-  depends_on "pkg-config" => :build
-  depends_on "libfuse@2"
-  depends_on "libzip"
-  depends_on :linux # on macOS, requires closed-source macFUSE
+  on_linux do
+    depends_on "libfuse@2"
+  end
 
   def install
     system "make", "prefix=#{prefix}", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
