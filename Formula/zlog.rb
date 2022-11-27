a96bbd152db08e090789fdef01a36d0741824d9a
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:44:26 +0800

    zlog: no rebuilding required

--- Formula/zlog.rb
+++ Formula/zlog.rb
@@ -6,13 +6,8 @@ class Zlog < Formula
   license "LGPL-2.1-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "b1c7b07083c3667fb8f32eba4437d2f3f7c872a184476968963b902a8a850aac"
-    sha256 cellar: :any,                 arm64_monterey: "16588f7d867f7426696693b19ee0221ff7483c2d7a34a856e3acb32c46c8ea41"
-    sha256 cellar: :any,                 arm64_big_sur:  "04ed8b2ebcbbf23a3e6ad76b8d80914f939e6fc6cadfd297b91c3d4d1d29b8b0"
-    sha256 cellar: :any,                 monterey:       "19cf707f5b5720aefa8f7cbb851a032a9a16900b7b17685530aa59081cbb0ee5"
-    sha256 cellar: :any,                 big_sur:        "f1384547bacef98381ae766dc90efc690a6a2a87dca0763a0572a3288ad68aee"
-    sha256 cellar: :any,                 catalina:       "62f3fbab6a72eb27a201c1c31cdb2cf8d4170053fdbfbe27130452026d01da69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "204935c1271f0c1cca1543ba6e71f7b1fcfcb611b97c6b32057c452abc1c2478"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/zlog"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "484da30dacef8f0661cb8c6f4d50b154c5b7596c1c6fdeae76b6ecd4576eafb0"
   end
 
   def install
