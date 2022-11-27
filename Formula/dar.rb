1b5be550b4d692767e0613edd4ecb9a85914b128
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:49:35 +0800

    dar: no rebuilding required

--- Formula/dar.rb
+++ Formula/dar.rb
@@ -11,13 +11,8 @@ class Dar < Formula
   end
 
   bottle do
-    sha256               arm64_ventura:  "1ebfdff2606d3143aef45cf08ee56a076048479d28fd864a6ada3570b5f89bdc"
-    sha256               arm64_monterey: "10d3c7bb9c0266b2d5a31e189b88a2b28ce6131ba5040f01210fddf2d6765e3d"
-    sha256               arm64_big_sur:  "8e79ea81d12cb5b2f7aa061dd9fe04549ff440ecc322d5a278b5de7b6e8acc07"
-    sha256 cellar: :any, monterey:       "d7d1ca1fd0b59185da504470e9946248b04aac7fa1163ef4f61578cb79d18d49"
-    sha256 cellar: :any, big_sur:        "ad50c99bd6c49c7d6dcc8ab5415d7787e49607582608c483c00bbcca0cafc79c"
-    sha256 cellar: :any, catalina:       "ea7ccf588f504d7a947fb47815d44da9088ebbb8eb64be8c653fbf74406589b5"
-    sha256               x86_64_linux:   "16cad6bb257d467529187f3d2846e9e981b684e49ca319a5cf49084b85b03080"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dar"
+    sha256 aarch64_linux: "4b999d2438a415ebbad7aa0d9d95b704cf9ee5bc6519d439b007b4f240eb04ed"
   end
 
   depends_on "libgcrypt"
