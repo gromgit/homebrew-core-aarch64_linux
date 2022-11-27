90f47771164ed95c71963732a3ef7253ce91c464
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:41:29 +0800

    xerces-c: no rebuilding required

--- Formula/xerces-c.rb
+++ Formula/xerces-c.rb
@@ -1,20 +1,14 @@
 class XercesC < Formula
   desc "Validating XML parser"
   homepage "https://xerces.apache.org/xerces-c/"
-  url "https://www.apache.org/dyn/closer.lua?path=xerces/c/3/sources/xerces-c-3.2.4.tar.gz"
-  mirror "https://archive.apache.org/dist/xerces/c/3/sources/xerces-c-3.2.4.tar.gz"
-  sha256 "3d8ec1c7f94e38fee0e4ca5ad1e1d9db23cbf3a10bba626f6b4afa2dedafe5ab"
+  url "https://www.apache.org/dyn/closer.lua?path=xerces/c/3/sources/xerces-c-3.2.3.tar.gz"
+  mirror "https://archive.apache.org/dist/xerces/c/3/sources/xerces-c-3.2.3.tar.gz"
+  sha256 "fb96fc49b1fb892d1e64e53a6ada8accf6f0e6d30ce0937956ec68d39bd72c7e"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "e858e931c939d044e591755091c6db66feb4c526e47279d02d430a7620e2eab7"
-    sha256 cellar: :any,                 arm64_monterey: "55a07ff428b5caeafc5628df1af7b39ac340d3ec1130bbaf5474363a374c01db"
-    sha256 cellar: :any,                 arm64_big_sur:  "20fc19025ac2b500e659dd8ec35136e13ac789c6f26d3a720a793e0ee7f2983b"
-    sha256 cellar: :any,                 ventura:        "7fd7f60de40b53884d1fedb6fba70442ffc5a15128e441926b973cf584da1506"
-    sha256 cellar: :any,                 monterey:       "52590592166754dff35051432cf0d5e8f656ecc4774e21d47dcf2ad936ef3f80"
-    sha256 cellar: :any,                 big_sur:        "8238563eea46d61d137b62d3f4b7ef7e8184f06b5c89b140c7c657b724ea34ec"
-    sha256 cellar: :any,                 catalina:       "2829258e76d45883ab076906aa1e3649a0d7775932353ea642e6ead0cc5e5371"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76d7da011335635adf76ec6ae5f48980174673effb6d6cfcb3ebb48db4eb5720"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/xerces-c"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "76efd599f281b3c0f0ab4cf2c9a18ec44c7884aff03fd96ba4426d7c3dc9b432"
   end
 
   depends_on "cmake" => :build
