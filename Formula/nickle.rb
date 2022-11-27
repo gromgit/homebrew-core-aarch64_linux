d16e29bccd63c8ef2736475a75a560c7a75a2d94
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:55:31 +0800

    nickle: no rebuilding required

--- Formula/nickle.rb
+++ Formula/nickle.rb
@@ -1,41 +1,25 @@
 class Nickle < Formula
   desc "Desk calculator language"
   homepage "https://www.nickle.org/"
-  url "https://deb.debian.org/debian/pool/main/n/nickle/nickle_2.91.tar.xz"
-  sha256 "a27a063d4cb93701d2d05a5fb2895b51b28fa7a2b32463a829496fb5f63833b6"
+  url "https://www.nickle.org/release/nickle-2.90.tar.gz"
+  sha256 "fbb3811aa0ac4b31e1702ea643dd3a6a617b2516ad6f9cfab76ec2779618e5a4"
   license "MIT"
-  head "https://keithp.com/cgit/nickle.git", branch: "master"
 
   livecheck do
-    url "https://deb.debian.org/debian/pool/main/n/nickle/"
+    url "https://www.nickle.org/release/"
     regex(/href=.*?nickle[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "24c13aee47478346f64cb72fe5755f95326f02a17e5008b194089fdb67937b04"
-    sha256 arm64_monterey: "4ace98b140571bfbb734fe311debb510b3d8fa7faf981cb9713b59294ad2eb83"
-    sha256 arm64_big_sur:  "ac6c222ef9d544b849ff9fb1714e6e88e50ab554cac84c0814979c84ca4a37ee"
-    sha256 ventura:        "64ab5c73ee1b38ac356221a008d4cde9cedf827ba104846d8f7bd3a99c42af64"
-    sha256 monterey:       "5ca3c7f1821b7625aaeb3096abe9141fd2875e354d330d8701f72d9265bb128f"
-    sha256 big_sur:        "e870c0478f054c72f026f331bcf02181bbac3766061f97165496b897fd81ae9e"
-    sha256 catalina:       "141813d383d72a8460c63adc9501bac93632ab1489a217da691023a9d8bf14b2"
-    sha256 x86_64_linux:   "e4512d3ebf15167474d7402e17be80b56751acbd31d451d3d2a75a37b98e65d4"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/nickle"
+    sha256 aarch64_linux: "0e9590398e7daf4c2ec157b21649950366bd507e4b2a168e0cdc358142ad402c"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "flex" => :build # conflicting types for 'yyget_leng'
   depends_on "readline"
 
-  uses_from_macos "bison" => :build
-
-  # Fix build on Apple Silicon
-  # Reported by email on 2022-11-09
-  patch :DATA
-
   def install
-    system "./autogen.sh", *std_configure_args
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make", "install"
   end
 
@@ -43,17 +27,3 @@ class Nickle < Formula
     assert_equal "4", shell_output("#{bin}/nickle -e '2+2'").chomp
   end
 end
-
-__END__
-diff -pur nickle-2.91/float.c nickle-2.91-new/float.c
---- nickle-2.91/float.c	2021-08-18 19:01:45
-+++ nickle-2.91-new/float.c	2022-11-09 14:33:15
-@@ -1122,7 +1122,7 @@ NewDoubleFloat (double d)
-     double_digit    dd;
-     if (d == 0.0) RETURN (Zero);
-     e = ilogb (d);
--    m = significand (d);
-+    m = scalb(d, (double) -e);
-     ms = Positive;
-     if (m < 0)
-     {
