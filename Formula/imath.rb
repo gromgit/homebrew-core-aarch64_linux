5dacb2f81b117e0b294bc61a7f60b77ba7e016b6
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:23:28 +0800

    imath: no rebuilding required

--- Formula/imath.rb
+++ Formula/imath.rb
@@ -1,19 +1,13 @@
 class Imath < Formula
   desc "Library of 2D and 3D vector, matrix, and math operations"
   homepage "https://www.openexr.com/"
-  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.6.tar.gz"
-  sha256 "ea5592230f5ab917bea3ceab266cf38eb4aa4a523078d46eac0f5a89c52304db"
+  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.5.tar.gz"
+  sha256 "1e9c7c94797cf7b7e61908aed1f80a331088cc7d8873318f70376e4aed5f25fb"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "0b0165f1503e0230d80cee448635ddb08e376d62069b9b426f5e363bb5cc8ba6"
-    sha256 cellar: :any,                 arm64_monterey: "ff6169b5986b709c9febe2b09929abc388a188be8041390a95d3f7b6f5814a22"
-    sha256 cellar: :any,                 arm64_big_sur:  "a94b22dd11a845ba003521ad6f80f4c2195059dd760047a07a802fd59d308f1b"
-    sha256 cellar: :any,                 ventura:        "66b235ef04409e3cdb50d419b32fba3e16680aa11211d8b0135393e5b9e609b4"
-    sha256 cellar: :any,                 monterey:       "9afc755be66e72fea866b8563934e704bc63ac831cca3b0e3661beb47fdb52ef"
-    sha256 cellar: :any,                 big_sur:        "1414bb873ce5225aca9173aaf8f4f74dea587c2077d4c934f36ac1e6cdf884f1"
-    sha256 cellar: :any,                 catalina:       "26b26215e534e8ae8a6ed2c95b04e0706932ef4aac9aef4a84396433c068d11f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f528de89730addebee49712701b2ee345b3ca8166b0e0f926a594652fdc85bfb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/imath"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2b511d9b6c2fcdcd568375ce43567daf9f547f33b246361c776b124037d506b5"
   end
 
   depends_on "cmake" => :build
