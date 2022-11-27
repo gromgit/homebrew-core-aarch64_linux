0f1afe101f623207c5f75031e95485ea08b69ef7
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:31:54 +0800

    toxcore: no rebuilding required

--- Formula/toxcore.rb
+++ Formula/toxcore.rb
@@ -9,13 +9,8 @@ class Toxcore < Formula
   head "https://github.com/TokTok/c-toxcore.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "34c38e086bf1bc1fbe8b9a2ea3c54eeb457979bdb54c77d912e1d1c6d1bf564e"
-    sha256 cellar: :any,                 arm64_monterey: "6277c36679255dce2b2e7871f12ce639459ce3d5cdbe407bb1d72d2b2eb6e4f6"
-    sha256 cellar: :any,                 arm64_big_sur:  "855c9fa777d64f3e02a9f63aa9e64775c4f1d1833efeaf58af70c3d2d6528962"
-    sha256 cellar: :any,                 monterey:       "488b94a6c29523fc399fee226289fbbc5efddfa6ad5c59fa934ce60dddf686e7"
-    sha256 cellar: :any,                 big_sur:        "ae90463b2d543b391adb410fcd6b5e5a80e33eed3f714a256fecc914d450468d"
-    sha256 cellar: :any,                 catalina:       "a865c7eb2ba80e585364f1a6bbb40008a7a58a63a8e8ec2a6811ae53636e09c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "11ac05dd681117beb656643d29e19ff73b9c6c19e8a45fb29977ea2bcfef858c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/toxcore"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e929150c08c8a16840cd4d5ae8571ee9f29e79bd7bcfd950a35e40f40d90acd6"
   end
 
   depends_on "cmake" => :build
