b32ad0a5a3c3a702f69a40966063c4f9c3da2375
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:49:08 +0800

    cxgo: no rebuilding required

--- Formula/cxgo.rb
+++ Formula/cxgo.rb
@@ -2,20 +2,14 @@ class Cxgo < Formula
   desc "Transpiling C to Go"
   homepage "https://github.com/gotranspile/cxgo"
   url "https://github.com/gotranspile/cxgo.git",
-      tag:      "v0.3.7",
-      revision: "cfc1ca865f59182eea902a45ce96b4cdda0f2b8c"
+      tag:      "v0.3.4",
+      revision: "d57c260fb00fa8bdae1c146f79dc03c4a13ae7c8"
   license "MIT"
   head "https://github.com/gotranspile/cxgo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "01d2976616ed7156938f6162585a3c948d95cc949d49b97abc29bed8c381b136"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b5a70797d3f41c27dcb860fb1e35578a9f2d09cc50504f0bf04cde5797ed2f3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "800b736a7731fda17e15de197a00562e70c863b62bd00afd9d216996efef6ecf"
-    sha256 cellar: :any_skip_relocation, ventura:        "af5a140675ba61902c305c2be1ba5fbe7e83d12ec2d1cfb3648ea471af7f3cbb"
-    sha256 cellar: :any_skip_relocation, monterey:       "eef11759db93aaeba729c7f76ccc73940f321c010446194ab42527fbce73ad2b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5db55c7ab63bdd7771b87c55a0e9ce64c6e60f9d588d1620a113451ee15554f0"
-    sha256 cellar: :any_skip_relocation, catalina:       "6287342a8d7a37d461611a3382cfe09d6509c5e692ba5f1de4f54e19a569d01c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7a8fb8c61c95813b7a8315c01c28265835583f15f6df4253bc117751b87d3a34"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cxgo"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "19e14b669af31374d079c4eb4a56237de2d47de352c1b637c57526a77dcc1fdf"
   end
 
   depends_on "go" => :build
