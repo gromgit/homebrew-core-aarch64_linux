2d1aa550049ba980766c873d17e5a0951ba183e7
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:01:12 +0800

    ory-hydra: no rebuilding required

--- Formula/ory-hydra.rb
+++ Formula/ory-hydra.rb
@@ -2,8 +2,8 @@ class OryHydra < Formula
   desc "OpenID Certified OAuth 2.0 Server and OpenID Connect Provider"
   homepage "https://www.ory.sh/hydra/"
   url "https://github.com/ory/hydra.git",
-      tag:      "v2.0.2",
-      revision: "ce968261a2043469860c6238701631c456268aba"
+      tag:      "v1.11.8",
+      revision: "337ab3ec2e363292ff93d5e5641a9b0bb87dba0c"
   license "Apache-2.0"
 
   livecheck do
@@ -12,13 +12,8 @@ class OryHydra < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d9d15e9b80b052ae775f90a2cdf5afcf9c038cfdfd4e5d491546592bb6547627"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "20b66952fdca8d86647a6cd1c75d2751943ab4ca76d0810b73809d771129df81"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dd12dd9b97be0fcee0bcc6f5629b3f9737da78f8236973ed319ca7e8bf126a3d"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e553aba26317647ac18bcb9e943f3cd9e464530d37fa7f84634cf0d6400ef5c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5f31f14c6202fae7ee8f690a597c0e390999ab3e1bc3f02492433b650b876f59"
-    sha256 cellar: :any_skip_relocation, catalina:       "ad811cd6e1d05ddaa892d7a5d9247765681a8433ef1556491439abd8bc8f362a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88245f4115642dd8a9a12fa231d4f4bf33e2371111a142b971ca06a1ff395c28"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ory-hydra"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4a975d9453686ef24b87eafdd4362dfb0adad2dd2558de792c35d8f00d1de70d"
   end
 
   depends_on "go" => :build
@@ -51,8 +46,8 @@ class OryHydra < Formula
     fork { exec bin/"hydra", "serve", "all", "--config", "#{testpath}/config.yaml" }
     sleep 20
 
-    endpoint = "http://127.0.0.1:#{admin_port}/"
-    output = shell_output("#{bin}/hydra list clients --endpoint #{endpoint}")
-    assert_match "CLIENT ID\tCLIENT SECRET", output
+    endpoint = "https://127.0.0.1:#{admin_port}/"
+    output = shell_output("#{bin}/hydra clients list --endpoint #{endpoint} --skip-tls-verify")
+    assert_match "| CLIENT ID |", output
   end
 end
