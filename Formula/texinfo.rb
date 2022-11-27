97cd7721be0e1f3c706143e941fda2d77514836c
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:29:52 +0800

    texinfo: no rebuilding required

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -1,29 +1,20 @@
 class Texinfo < Formula
   desc "Official documentation format of the GNU project"
   homepage "https://www.gnu.org/software/texinfo/"
-  url "https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.tar.xz"
-  mirror "https://ftpmirror.gnu.org/texinfo/texinfo-7.0.tar.xz"
-  sha256 "20744b82531ce7a04d8cee34b07143ad59777612c3695d5855f29fba40fbe3e0"
-  license "GPL-3.0-or-later"
+  url "https://ftp.gnu.org/gnu/texinfo/texinfo-6.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/texinfo/texinfo-6.8.tar.xz"
+  sha256 "8eb753ed28bca21f8f56c1a180362aed789229bd62fff58bf8368e9beb59fec4"
+  license "GPL-3.0"
 
   bottle do
-    sha256 arm64_ventura:  "33acf9386cbeab7b708f5c8dd22de953ebe75282faf7fbe36825678396aaf816"
-    sha256 arm64_monterey: "83e127cc706026b0279bb6891ff8649a3039e9340ed2c1a38e6fcfc17c2f490f"
-    sha256 arm64_big_sur:  "e44ae3361202b0eb5cd28d7730e273b09469eea900eea6bc327ac3eccce339d2"
-    sha256 ventura:        "0c515bf5c8badacf91147bc1b56a2c84b1b833b6e691dc338af092f47dea7f7d"
-    sha256 monterey:       "d4f1550361dbff38ba9d7a63ae2e00cf90109929434cec52ca48418a13ebeba8"
-    sha256 big_sur:        "fe5b705f97686161be9d985ca3dabdcc8692df85ed3ff2aa3583c7ce20fec1a4"
-    sha256 catalina:       "59505aedbf48a25fda2c71718168027a736bf0e954fe02eb1281910cdef82e3c"
-    sha256 x86_64_linux:   "0f41d9f1b881faca105bd7516a2b4fb2f5940522fa1e2ba5aaf249a877c7c311"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/texinfo"
+    sha256 aarch64_linux: "5337150936a3942c980042d22c66c4ef9f3c304de8a0527e63b700973c8ba254"
   end
+
+  keg_only :provided_by_macos
 
   uses_from_macos "ncurses"
   uses_from_macos "perl"
-
-  # texinfo has been removed from macOS Ventura.
-  on_monterey :or_older do
-    keg_only :provided_by_macos
-  end
 
   on_system :linux, macos: :high_sierra_or_older do
     depends_on "gettext"
