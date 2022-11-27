94a59e84115c6706b1933764cac0223d6cd88189
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:22:49 +0800

    snowflake: no rebuilding required

--- Formula/snowflake.rb
+++ Formula/snowflake.rb
@@ -1,20 +1,14 @@
 class Snowflake < Formula
   desc "Pluggable Transport using WebRTC, inspired by Flashproxy"
   homepage "https://www.torproject.org"
-  url "https://gitweb.torproject.org/pluggable-transports/snowflake.git/snapshot/snowflake-2.3.1.tar.gz"
-  sha256 "47e035ff08668317e719b982a924d048fad738e6081005971e895ddcc2283fba"
+  url "https://gitweb.torproject.org/pluggable-transports/snowflake.git/snapshot/snowflake-2.3.0.tar.gz"
+  sha256 "12be2ab46b174b7a72c7261577f5a89ca301f232e5ff100ef771375337a188d3"
   license "BSD-3-Clause"
   head "https://git.torproject.org/pluggable-transports/snowflake.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5358d95caeb519fd52064300cc6c03075764f965498a640dbcd37b334bef2c23"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ca274c6678b71d1ea47f0e5b6ee0b873a6a2826da634186225f0f0d37f2459f3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2836b655208a177b5459630bab88560aed22b704d5bf2f621ddcaeec9838fb9"
-    sha256 cellar: :any_skip_relocation, ventura:        "b0b242255b7355454dedfccea870eb5f3d95ff3a2bb8bee760968c20a133fcb4"
-    sha256 cellar: :any_skip_relocation, monterey:       "383ca25f5e69b9f5aa9458816c4eb1730a5907de5e7fc1a55d65089cfff5224b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "61a06facc6a248c6590ac9d82d5ce1165cf8313a131e3c825442b7a882d0bd2b"
-    sha256 cellar: :any_skip_relocation, catalina:       "bf26de9c93dfd2f49a9d9e8a37c785b41cf42eb460e786e51d4cbeec86828f12"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f20ca5db9a0d6d3e1fa6906a1d91622367b3d8306dd36c767a2da5a00efe0c6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/snowflake"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "25c46031a36c5935049ae1eb8c35401217bbc7a07a09b3266e2ecc479a1eb9a8"
   end
 
   depends_on "go" => :build
