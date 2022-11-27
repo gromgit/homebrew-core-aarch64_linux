d76cd0d38458872efc89658466c6a1c262677db6
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:40:38 +0800

    libuv: no rebuilding required

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -12,15 +12,8 @@ class Libuv < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "2653486daddca69315ee9b5bd12c7ba262ecc5a159ddd1d0277a3e5fb14708ac"
-    sha256 cellar: :any,                 arm64_monterey: "1134b22185efdff666f68f334152bb7b37863fc7310e1403089daff68ffdc124"
-    sha256 cellar: :any,                 arm64_big_sur:  "3955c77e5544e84fa8361a1453b1c0232ff15e275d46ce54ad6eb9b348370ad8"
-    sha256 cellar: :any,                 ventura:        "d3cc5bca7fe7512842102366b45b1948099eb8c24ab53093821295586b2de76d"
-    sha256 cellar: :any,                 monterey:       "ccef3d8ae2170307a515e657236b97ee605ce614b8fee0f42da19ae3d8efeee1"
-    sha256 cellar: :any,                 big_sur:        "5edaadbf90ddeedd193ac1c4edca1e76c5ac08ca42be0c9a44d2fd0a9b6b65d0"
-    sha256 cellar: :any,                 catalina:       "02f2d420b10d5c33971792e41b0cf77f899289d5bec2aece5db7946a8e741f54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b17d90c050a0e09a3e84a58bfd00314fb0b90e87a6235cc3327b004b014f8cf"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libuv"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ce14ce29382bd2024cb7b3cfe3734854a270c279c431c3bc4c51ff945aeac19a"
   end
 
   depends_on "autoconf" => :build
