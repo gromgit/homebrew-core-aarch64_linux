0a560195ed1ddbfbef27b8b1381c46797c440aa0
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:35:40 +0800

    vagrant-completion: no rebuilding required

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,13 +1,14 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
   homepage "https://github.com/hashicorp/vagrant"
-  url "https://github.com/hashicorp/vagrant/archive/v2.3.3.tar.gz"
-  sha256 "06142fc406e96ed4f78dc877a20e2c8509c732c280fdfb44d1a84ce4f4c6ecc3"
+  url "https://github.com/hashicorp/vagrant/archive/v2.3.0.tar.gz"
+  sha256 "1931dbf29ec3c6622a649ae145fe706e5b957d7075870ce577358dd22c3d5dca"
   license "MIT"
   head "https://github.com/hashicorp/vagrant.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "eb2277ac2b4fe241d519f26bc0748a82facb7349acc4178f6b34c7241f383fb9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/vagrant-completion"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "607fac573fc21dddbe7fc7d69e243ac031b43c41c974ad8831106adc22164541"
   end
 
   def install
