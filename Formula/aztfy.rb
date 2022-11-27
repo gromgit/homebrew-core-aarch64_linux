f73ca1db906a71a3420a00a5312a72bf35b73645
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:31:01 +0800

    aztfy: no rebuilding required

--- Formula/aztfy.rb
+++ Formula/aztfy.rb
@@ -2,21 +2,16 @@ class Aztfy < Formula
   desc "Bring your existing Azure resources under the management of Terraform"
   homepage "https://azure.github.io/aztfy"
   url "https://github.com/Azure/aztfy.git",
-      tag:      "v0.9.0",
-      revision: "a3341dd6789691086c838b3db2b3e7d4e1342be6"
+      tag:      "v0.6.0",
+      revision: "18b614bb384b1d413554867b8601feb753715575"
   license "MPL-2.0"
   head "https://github.com/Azure/aztfy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "795205276eb37c16f360d73e24af8a79386e5c361b75707e8c62704f824ac612"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "795205276eb37c16f360d73e24af8a79386e5c361b75707e8c62704f824ac612"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "795205276eb37c16f360d73e24af8a79386e5c361b75707e8c62704f824ac612"
-    sha256 cellar: :any_skip_relocation, ventura:        "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
-    sha256 cellar: :any_skip_relocation, monterey:       "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
-    sha256 cellar: :any_skip_relocation, catalina:       "c3a39ba41daaec2e319275f4733724608b6eada2e593d0ec165bbae155e02ae5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41161e9eb4380afce7c09b35d292613c25ae6da14536b17543e6b7279ddb6a63"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aztfy"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "338f3093a327745321d6980ddf4f2be2a86c3efdb9d20a6430d90932f1748136"
   end
+
   depends_on "go" => :build
 
   def install
@@ -29,6 +24,6 @@ class Aztfy < Formula
     assert_match version.to_s, version_output
 
     no_resource_group_specified_output = shell_output("#{bin}/aztfy rg 2>&1", 1)
-    assert_match("Error: retrieving subscription id from CLI", no_resource_group_specified_output)
+    assert_match("Error: No resource group specified", no_resource_group_specified_output)
   end
 end
