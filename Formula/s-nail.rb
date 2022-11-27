96e2ce4e8f3c5f4eea395db477e5027e7d86bb5a
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:17:34 +0800

    s-nail: no rebuilding required

--- Formula/s-nail.rb
+++ Formula/s-nail.rb
@@ -11,7 +11,6 @@ class SNail < Formula
     "HPND-sell-variant", # GSSAPI code
     "RSA-MD", # MD5 code
   ]
-  revision 1
 
   livecheck do
     url :homepage
@@ -19,23 +18,18 @@ class SNail < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "5fce8e0d81ee4477b193583816d90a6ed5ff11a021dff0043876ac29f03e3593"
-    sha256 arm64_monterey: "e3fb2f1ca5f3d34113dd5a7bd1b522675dc6f37c0c3280d8a8473690d691ab07"
-    sha256 arm64_big_sur:  "0379b8cbc70eb8dc659d0af3713d23b306d42b2a150180eb403ce49509fa1ea5"
-    sha256 ventura:        "d6f83963c68229932e83310ab2c54a48bd322c73286e12b219cf3f9b686aa4c3"
-    sha256 monterey:       "997aaf6c68ace33c677e89c7de50c75fb8a2a9d1c768cda79d5147c7fb1de70e"
-    sha256 big_sur:        "e54132e6af33629def0c0d3982205e0d3da4f2605be5a4b8443361de66d66159"
-    sha256 catalina:       "affc68fc50f6079a85bdb6f23c54f795c9d1e57ac9c681913d85e3fc32eafe3d"
-    sha256 x86_64_linux:   "bc6d7e78241ea5f8c869094ae74d37b092739e4d7d230888dfbdea7443a29642"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/s-nail"
+    sha256 aarch64_linux: "56652710c161ee272b53a3ca0dc1750d773a848ec6e22a6a95b47cb4ca97be77"
   end
 
-  depends_on "libidn2"
-  depends_on "openssl@3"
+  depends_on "awk" => :build
+  depends_on "libidn"
+  depends_on "openssl@1.1"
 
   def install
     system "make", "CC=#{ENV.cc}",
-                   "C_INCLUDE_PATH=#{Formula["openssl@3"].opt_include}",
-                   "LDFLAGS=-L#{Formula["openssl@3"].opt_lib}",
+                   "C_INCLUDE_PATH=#{Formula["openssl@1.1"].opt_include}",
+                   "LDFLAGS=-L#{Formula["openssl@1.1"].opt_lib}",
                    "VAL_PREFIX=#{prefix}",
                    "OPT_DOTLOCK=no",
                    "config"
