7b25a30a06709f5e98e1c8109fcd357dbb7a95ff
FX Coudert <fxcoudert@gmail.com>
2022-11-27 13:38:25 +0000

    oh-my-posh: update 12.21.0 bottle.

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -10,6 +10,7 @@ class OhMyPosh < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "52364399e7737cec6ecabdca975a80a94d70b93ef3303255cab465f4ad1ec2a5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e023d62cb49fd47f28138d1b8e8d13a17a53971b54c3dc9b3eb80138035d7e68"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24464ec0a2cbdfb8067ff380e8247ef217ea1fbfab2cb7ed6a041764e64084fd"
+    sha256 cellar: :any_skip_relocation, ventura:        "1824fcc1294585f7c38d6f3cd33a39ea2980573d3967f9e098f2c61722779ce4"
     sha256 cellar: :any_skip_relocation, monterey:       "0c1b7b31d9be90f2f3d6ce71009f9d34e766c4b438173ae4a403270dd10605b1"
     sha256 cellar: :any_skip_relocation, big_sur:        "05866823124451cfa9145419fce9f8ffed8fd8f2ee3ea0e10dacb0d002dbacaa"
     sha256 cellar: :any_skip_relocation, catalina:       "f425c072bf70d665bb4148eb45629e552c0876c5f5cf77e7145b53a8bca12206"
