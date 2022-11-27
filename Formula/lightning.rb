b2d1c6f2e71ef6f06e7185747a44f2e61bfd124e
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:43:00 +0800

    lightning: no rebuilding required

--- Formula/lightning.rb
+++ Formula/lightning.rb
@@ -1,24 +1,22 @@
 class Lightning < Formula
   desc "Generates assembly language code at run-time"
   homepage "https://www.gnu.org/software/lightning/"
-  url "https://ftp.gnu.org/gnu/lightning/lightning-2.2.0.tar.gz"
-  mirror "https://ftpmirror.gnu.org/lightning/lightning-2.2.0.tar.gz"
-  sha256 "4e3984ff1ccf0ba30a985211d40fc5c06b25f014ebdf3d80d0fe3d0c80dd7c0e"
-  license "GPL-3.0-or-later"
+  url "https://ftp.gnu.org/gnu/lightning/lightning-2.1.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/lightning/lightning-2.1.3.tar.gz"
+  sha256 "ed856b866dc6f68678dc1151579118fab1c65fad687cf847fc2d94ca045efdc9"
+  license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "1ae424e59729ac28174ea1c2bf97e9699b8f7f341d284e642ad7048ef6f091b2"
-    sha256 cellar: :any,                 big_sur:      "356eae8409bf6dc18fb3539c82b2c968933f496a12a54b14cfbcf86a18c9c3e0"
-    sha256 cellar: :any,                 catalina:     "ab244b3c017f380559a64d800add95a473de72cde9bd8a1e6a42c445b3c19b73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b3d5c7288db4d78fefc35ba7f076200dc75c24c944593e2e0f821a9ddb90512e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/lightning"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "362cb1a6461674b70fb77d77299110490b78746883bcba7a54cbf562ca5d8157"
   end
 
   depends_on "binutils" => :build
-  depends_on arch: :x86_64
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules", "--prefix=#{prefix}"
+    system "make", "check", "-j1"
     system "make", "install"
   end
 
