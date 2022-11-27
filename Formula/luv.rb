d835f5bbfc12f9cee10624e08ac813bf17963278
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:45:10 +0800

    luv: no rebuilding required

--- Formula/luv.rb
+++ Formula/luv.rb
@@ -1,20 +1,15 @@
 class Luv < Formula
   desc "Bare libuv bindings for lua"
   homepage "https://github.com/luvit/luv"
-  url "https://github.com/luvit/luv/archive/1.44.2-1.tar.gz"
-  sha256 "f8c69908e17ec8ab370253d1508e23deaecfc0c4752d2efb77e427e579501104"
+  url "https://github.com/luvit/luv/archive/1.44.2-0.tar.gz"
+  sha256 "44ccda27035bfe683e6325a2a93f2c254be1eb76bde6efc2bd37c56a7af7b00a"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/luvit/luv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "7c8432625ecd382168eac3d0bc3615519528a8e338f8f33ee0c6761bf9f5dd65"
-    sha256 cellar: :any,                 arm64_monterey: "1d5aaf149019d3d775b093efaa51fda452248eccf109e961325d0a8de917f947"
-    sha256 cellar: :any,                 arm64_big_sur:  "ce3cf05ca363a0978151a8f1ae096bb1d13d26718d2dfc25c3618e6156a4f6e2"
-    sha256 cellar: :any,                 ventura:        "f3b9a7e332ee5024fcac9edc7ca8a9a17811033d3c0cf7f972bb6252bd6d7051"
-    sha256 cellar: :any,                 monterey:       "1644986934f6275f3f7f632e074cdc9866ad3c2b83661035cd99f5f7912831c9"
-    sha256 cellar: :any,                 big_sur:        "4c93fce6a32d76a35e365d1fb23b08c3bcfaf0de2426f8490d38128c9d2d8781"
-    sha256 cellar: :any,                 catalina:       "c2d5d6718df8309860cd392047a071770d3c298f29a441f4639d47d909cd873a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c1bd86d334299bd8b6af4f0d4080f2b5dab6033778e65a90424be090f10374b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/luv"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "884bab2975aabc963781921e87e3495b5adbd17fd27fdc11790d470e601d0ca5"
   end
 
   depends_on "cmake" => :build
