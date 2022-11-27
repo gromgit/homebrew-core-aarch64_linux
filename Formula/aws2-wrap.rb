29f38604b93c10b8700e5f565c9d4aaca46fb0f1
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:30:46 +0800

    aws2-wrap: no rebuilding required

--- Formula/aws2-wrap.rb
+++ Formula/aws2-wrap.rb
@@ -8,21 +8,15 @@ class Aws2Wrap < Formula
   license "GPL-3.0-only"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "df6fced6d73b9eb47fa24c218c48ec55539a3e99fc30ed48a723adf9d60d6eb9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f82b736d5ba5de2a3b658b1a791dffb3c3be8515e481c3dac766e594b35c4d3a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ca745ea5354ba19542c8f316d4f867c0dcf5d5b060e0d9e0ff5e73151a627470"
-    sha256 cellar: :any_skip_relocation, monterey:       "8cd21371d23b53ef788ffbf0b60fa6afa7e7884845432b3e033ac42954e47ed1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4d253f490ee316da830ae2e63b7b7feecf0f8e2d2f765ef21ec21b703214b390"
-    sha256 cellar: :any_skip_relocation, catalina:       "4b9ddd6d1fc1f721318de2d6881b0aadbb648ff634ecb6ed38d1b067af5b0375"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f92117e20b1909b99fb9e9c6e57392a23cdd2e0cb4e897aad20fdb2d6df79bfc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aws2-wrap"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cc50a75190ea3d89da8c6d92698e453b9234eca77b0ea3c74a374b7c26eb2553"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.10"
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/de/eb/1c01a34c86ee3b058c556e407ce5b07cb7d186ebe47b3e69d6f152ca5cc5/psutil-5.9.3.tar.gz"
-    sha256 "7ccfcdfea4fc4b0a02ca2c31de7fcd186beb9cff8207800e14ab66f79c773af6"
+    url "https://files.pythonhosted.org/packages/d6/de/0999ea2562b96d7165812606b18f7169307b60cd378bc29cf3673322c7e9/psutil-5.9.1.tar.gz"
+    sha256 "57f1819b5d9e95cdfb0c881a8a5b7d542ed0b7c522d575706a80bedc848c8954"
   end
 
   def install
