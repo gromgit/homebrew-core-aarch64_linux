49b336e5ef3dcc0cd557d69096fe27d064022a39
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:12:51 +0800

    gnu-sed: no rebuilding required

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -1,20 +1,14 @@
 class GnuSed < Formula
   desc "GNU implementation of the famous stream editor"
   homepage "https://www.gnu.org/software/sed/"
-  url "https://ftp.gnu.org/gnu/sed/sed-4.9.tar.xz"
-  mirror "https://ftpmirror.gnu.org/sed/sed-4.9.tar.xz"
-  sha256 "6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181"
+  url "https://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/sed/sed-4.8.tar.xz"
+  sha256 "f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5abaf39c16d02125db97d14cd36a96cf1a20a87821199cb38a55134fd4e0aaef"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "20ae3f853a32e7f7f0f340e8c751ab7350888a655bfe7c5c20e5746c61a24fd7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7c89842a90d03dbb497bc1ded17b7d732fe20eaf69613fd4abb48820ab80895"
-    sha256 cellar: :any_skip_relocation, ventura:        "a1ac59a9a6fa20c6c904e047df3ee4d0b4e57c0a5df3821b17b8cd82bcc67b5a"
-    sha256 cellar: :any_skip_relocation, monterey:       "f5e2460ad86516b2517f1e77d672a4fd6ad30b158c470cccbb3b6464f228674d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c1c63d995d132a82fadc80b470eecfe816cb86c8cd716f01de5f003bc1199fcc"
-    sha256 cellar: :any_skip_relocation, catalina:       "fb5ee7317d987d9ac7f2ee357736a9bc594c88b5fbbca4f6a65046f1c2898c44"
-    sha256                               x86_64_linux:   "8abd5b48de6b706c1ce7c2f7b8775420f63078ba294bd5ad801e458776228bbc"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gnu-sed"
+    sha256 aarch64_linux: "94bd943935cd206ff922bc631f4015f091580248afb557b549d34d1ba2c6ff85"
   end
 
   conflicts_with "ssed", because: "both install share/info/sed.info"
