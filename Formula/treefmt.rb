25cc9c9941610870532b66ce6767ec0fbc0a2018
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:32:29 +0800

    treefmt: no rebuilding required

--- Formula/treefmt.rb
+++ Formula/treefmt.rb
@@ -8,14 +8,8 @@ class Treefmt < Formula
   head "https://github.com/numtide/treefmt.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "848af1802dbd4b2e155842f0d4d88f6c93890ee9e8b7913a17df9891a1884b8b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "70693c71f8e8d0d6dd747c5bb42effce35701cd1dc48654a53c293e07d0fb093"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "070f05b40ef90f9cca25281b022cf06e32214cb4bfe8ebed446521c4c7cc0122"
-    sha256 cellar: :any_skip_relocation, monterey:       "083823b01f2d0241b4f64f1b48e887d5a8e6fd6fec3f4403ec83ae5e2eef9173"
-    sha256 cellar: :any_skip_relocation, big_sur:        "75d9369e6f359a4beb25c2f7eaede113aaf5e8e9c8698d85f2ff3339af02b1d5"
-    sha256 cellar: :any_skip_relocation, catalina:       "0c9d263b04e9085a1503deaccc9608bcd9ce9cb59e7216f5002edebe3720b854"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e635602de192435ca5dd8f7c744520d16ff10eaacabd92f536e9f10ee7a81eb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/treefmt"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "062e95612da9bc25a91d6dcd977fe8502fb9adae87f99a63e97de51217214d8b"
   end
 
   depends_on "rust" => :build
