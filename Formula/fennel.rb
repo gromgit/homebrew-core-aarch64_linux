5028aefe35055c37dbc320adeae1568ddfc8acf6
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:01:36 +0800

    fennel: no rebuilding required

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -1,12 +1,13 @@
 class Fennel < Formula
   desc "Lua Lisp Language"
   homepage "https://fennel-lang.org"
-  url "https://github.com/bakpakin/Fennel/archive/1.2.1.tar.gz"
-  sha256 "fae8a0b00275529acb3785673b9505f378786ccd2cd2bc16254f6a5fd08e0d19"
+  url "https://github.com/bakpakin/Fennel/archive/1.2.0.tar.gz"
+  sha256 "a0c9b1d23d9d9714afe93542c5314fad8e1771bf8b616d0decfeabe88318313e"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ae18b97efb611231723d835ade911486880d0341eb2cd731ba27516787babe88"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/fennel"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b4764b6f8f4cfa1e1de64200497e2bd0a55515a981b33963decd43ab3c4d7744"
   end
 
   depends_on "lua"
