eb43bec16a9d8cee704934d60dd7999310c9f288
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:49:46 +0800

    minizip: no rebuilding required

--- Formula/minizip.rb
+++ Formula/minizip.rb
@@ -1,9 +1,9 @@
 class Minizip < Formula
   desc "C library for zip/unzip via zLib"
   homepage "https://www.winimage.com/zLibDll/minizip.html"
-  url "https://zlib.net/zlib-1.2.13.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.13/zlib-1.2.13.tar.gz"
-  sha256 "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
+  url "https://zlib.net/zlib-1.2.12.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.12/zlib-1.2.12.tar.gz"
+  sha256 "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
   license "Zlib"
 
   livecheck do
@@ -11,14 +11,8 @@ class Minizip < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "f04d79cbe3e1d6c2ceae2cbbee835c914b9ada6c536da659aa947b34109e4ec8"
-    sha256 cellar: :any,                 arm64_monterey: "24bd919aa1dde395f929d897743f9235fd6023e017e983c0169a0918987a40ba"
-    sha256 cellar: :any,                 arm64_big_sur:  "a604caa335ee965ce1b29e70ba1cd5db183fb9cfd7f70c9141644a2013423289"
-    sha256 cellar: :any,                 ventura:        "8d6734507310e18a70548bd96afd469b38da2a34ea6bcec9d46c6ac94f6afd30"
-    sha256 cellar: :any,                 monterey:       "3bca4aaed9dba859600379d30b3f2aaa2cfd06a406208f163366d14c8bb6cb53"
-    sha256 cellar: :any,                 big_sur:        "80e4849ce70b1ac5be25d1886947651629e9ac82ec7a5ec47dd28ee0a3499381"
-    sha256 cellar: :any,                 catalina:       "a0b93c2d3e4beef15171d95e1fe37b3ca9d7764a101dae2f9a6bacc972e047e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1f2d6483bfeb86f7e3266a3e07b96593f4bb383fc9297e86583735c51efb86b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/minizip"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ef7fb01c424a815def8852e850bd3b46bcac31e750fd9fc4833c67a6146c0dad"
   end
 
   depends_on "autoconf" => :build
@@ -29,6 +23,13 @@ class Minizip < Formula
 
   conflicts_with "minizip-ng",
     because: "both install a `libminizip.a` library"
+
+  # Patch for configure issue
+  # https://github.com/madler/zlib/pull/607
+  patch do
+    url "https://github.com/madler/zlib/commit/05796d3d8d5546cf1b4dfe2cd72ab746afae505d.patch?full_index=1"
+    sha256 "68573842f1619bb8de1fa92071e38e6e51b8df71371e139e4e96be19dd7e9694"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
