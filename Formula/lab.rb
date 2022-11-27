9a4d438c7f1b24a5d57886703f0a4337cd88b4ef
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:30:11 +0800

    lab: no rebuilding required

--- Formula/lab.rb
+++ Formula/lab.rb
@@ -1,20 +1,14 @@
 class Lab < Formula
   desc "Git wrapper for GitLab"
   homepage "https://zaquestion.github.io/lab"
-  url "https://github.com/zaquestion/lab/archive/v0.25.1.tar.gz"
-  sha256 "f8cccdfbf1ca5a2c76f894321a961dfe0dc7a781d95baff5181eafd155707d79"
+  url "https://github.com/zaquestion/lab/archive/v0.23.0.tar.gz"
+  sha256 "8f20d5f1931e9b5daa0aa2d30fc3176d82dcca91b368905a1e1c05e2b36254b9"
   license "CC0-1.0"
   head "https://github.com/zaquestion/lab.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eee3b3d1a309b0a61a5224cc1c13b0de765518b86015f6985a09347e86554b00"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "31e8b682f99fd9f456d298e09d20521317f3b04248f53029d04ce5f8b3f3b75a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9cc2420cd4dd1db173333ff7c51530deee73b47a8fb5d3d36d60893dfd4c35ae"
-    sha256 cellar: :any_skip_relocation, ventura:        "5509e7f37eb68c9404e0f862fc87535091b7928a891fa38deede63fefcf314f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "d1e3f02ef9e1748260a2ba5d9eefc79312b77f8d3ae223485aab7a025e7638e5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f20ca44b476a3d6a3ef9a74047d4dd863403d72a427f1baa0dc19a9df5b33667"
-    sha256 cellar: :any_skip_relocation, catalina:       "0ce4baa79e79a77dd30d3e7e839ef41c414329dc21ffa9386e74d7a0f69c7501"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "22981b2ebc3d41dd9b1905ab8f8f715d38d09c85f173e848473a8f731039653d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/lab"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f7057f28d88958003fbb3088816b159f88887b59885deb7f263b71d7917b4e8b"
   end
 
   depends_on "go" => :build
@@ -22,8 +16,12 @@ class Lab < Formula
   def install
     ldflags = "-X main.version=#{version} -s -w"
     system "go", "build", *std_go_args(ldflags: ldflags)
-
-    generate_completions_from_executable(bin/"lab", "completion")
+    output = Utils.safe_popen_read("#{bin}/lab", "completion", "bash")
+    (bash_completion/"lab").write output
+    output = Utils.safe_popen_read("#{bin}/lab", "completion", "zsh")
+    (zsh_completion/"_lab").write output
+    output = Utils.safe_popen_read("#{bin}/lab", "completion", "fish")
+    (fish_completion/"lab.fish").write output
   end
 
   test do
@@ -36,9 +34,10 @@ class Lab < Formula
     ENV["GIT_COMMITTER_NAME"] = "test user"
     ENV["GIT_COMMITTER_EMAIL"] = "test@example.com"
 
-    output = shell_output("#{bin}/lab todo done 1 2>&1", 1)
-    assert_match "POST https://gitlab.com/api/v4/todos/1/mark_as_done", output
-
-    assert_match version.to_s, shell_output("#{bin}/lab version")
+    system "git", "init"
+    %w[haunted house].each { |f| touch testpath/f }
+    system "git", "add", "haunted", "house"
+    system "git", "commit", "-a", "-m", "Initial Commit"
+    assert_match "haunted\nhouse", shell_output("#{bin}/lab ls-files").strip
   end
 end
