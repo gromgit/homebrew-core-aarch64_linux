a0a52e9434fab5722678400128306b42a8d9f34c
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:31:38 +0800

    baresip: no rebuilding required

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -1,18 +1,13 @@
 class Baresip < Formula
   desc "Modular SIP useragent"
   homepage "https://github.com/baresip/baresip"
-  url "https://github.com/baresip/baresip/archive/refs/tags/v2.9.0.tar.gz"
-  sha256 "b4ca604f47f67b1390c9549768d54c2359ec3e56874db87b47ac6d8a022abcb3"
+  url "https://github.com/baresip/baresip/archive/v2.7.0.tar.gz"
+  sha256 "6bc3ac1b2a301b6de91a40079a9ec44545a00c57662ca0bdf2518fbb932ff181"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_ventura:  "0493eb5e648239121aaf761c87a85594f6f191996906f33afbc34f6c5003083e"
-    sha256 arm64_monterey: "62295fd60ac100eaaa0536ae018b7579cedfe8f5ee9e92cbfde0ea5502d40a63"
-    sha256 arm64_big_sur:  "f71d9ed2918760a2e5f0bb10bb14e55fbb88320c880adb750844bbe86d60c098"
-    sha256 monterey:       "dc3935ceb324f3a6d9c26b85913d278641a5667c6c4b5b9ad6bbed1384d9c604"
-    sha256 big_sur:        "5f89590a6751d0a489309b9460d541198cad646853ca759ef42932691d8ec425"
-    sha256 catalina:       "5b3dcd6a600ffb19097bd5c0386b1ad76449d14b3f1b1894ee1492804c955203"
-    sha256 x86_64_linux:   "1b0967fcf830f3d5a18c7e0dfb342ee71a9b9840e49d876205ecd6ebf5ac97e6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/baresip"
+    sha256 aarch64_linux: "470181774d0af3426b1a76c3fc1579eb2126d73b1527ff8e66bced15b92837f3"
   end
 
   depends_on "libre"
@@ -21,20 +16,20 @@ class Baresip < Formula
   def install
     libre = Formula["libre"]
     librem = Formula["librem"]
+    # NOTE: `LIBRE_SO` is a directory but `LIBREM_SO` is a shared library.
     args = %W[
       PREFIX=#{prefix}
       LIBRE_MK=#{libre.opt_share}/re/re.mk
       LIBRE_INC=#{libre.opt_include}/re
       LIBRE_SO=#{libre.opt_lib}
       LIBREM_PATH=#{librem.opt_prefix}
-      LIBREM_SO=#{librem.opt_lib}
+      LIBREM_SO=#{librem.opt_lib/shared_library("librem")}
       MOD_AUTODETECT=
       USE_G711=1
       USE_OPENGL=1
       USE_STDIO=1
       USE_UUID=1
       HAVE_GETOPT=1
-      RELEASE=1
       V=1
     ]
     if OS.mac?
