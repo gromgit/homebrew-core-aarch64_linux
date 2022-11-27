60c8418b89ac8338a12883c1c505c4e8143e4526
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:38:04 +0800

    camlp5: no rebuilding required

--- Formula/camlp5.rb
+++ Formula/camlp5.rb
@@ -4,7 +4,6 @@ class Camlp5 < Formula
   url "https://github.com/camlp5/camlp5/archive/refs/tags/rel8.00.03.tar.gz"
   sha256 "1a710e2a6dbb0f4440867850d605f31fe8407ee8a56c9e067866e34e584385b4"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/camlp5/camlp5.git", branch: "master"
 
   livecheck do
@@ -13,17 +12,11 @@ class Camlp5 < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "5c856ce395542f5b4f154a67e076482465cd4538e6d84172269c872b7a28bf94"
-    sha256 arm64_monterey: "9daedd1054de7c0004d21f643d47ea90611f66d13524488950e5fbe36bf8c0b4"
-    sha256 arm64_big_sur:  "328cb38eb6fc4540c35bca367e55c26f5e088dd0d2f0858bed6508f21ff470f7"
-    sha256 monterey:       "dc109b253d4a8393e83abe312ed7d372795efabf4d2ed32c0e4ea132ac473822"
-    sha256 big_sur:        "89414654ad17bd67769884a1f1f5f0b04ba7f822b41ec87821adec5e81323c6d"
-    sha256 catalina:       "361e38d25555a16220746683a9ff4483683d75cf1cf711140cc94c6cfd9931b9"
-    sha256 x86_64_linux:   "915b81cd45db3bad023f43efed93f1b5dafbff1c1c539244f06ce02f124f2311"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/camlp5"
+    sha256 aarch64_linux: "6651260615fb21b546ff8dbe1d660d71e96fac31bfbf9c39ce350de69ed4048c"
   end
 
   depends_on "ocaml-findlib" => :build
-  depends_on "camlp-streams"
   depends_on "ocaml"
 
   def install
