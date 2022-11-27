8aaafd0a2774d3b007dfdc4eb9e3a23324606d1a
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:33:51 +0800

    libdvdcss: no rebuilding required

--- Formula/libdvdcss.rb
+++ Formula/libdvdcss.rb
@@ -11,15 +11,8 @@ class Libdvdcss < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d3a1da9ea4618f10137277bd109cc5d1c74ba3d82a2dc45f34370e1c389d8eea"
-    sha256 cellar: :any,                 arm64_monterey: "c96a2adbc32a57e271a9a91d338571ab9b0a6524a95e3fe48270dd5c4a277b21"
-    sha256 cellar: :any,                 arm64_big_sur:  "ef10943948da31c0015eb558758fea572963e381c13c203e79ee2169a826731a"
-    sha256 cellar: :any,                 ventura:        "8ebe4191446d0808caee8a4cf8796e16dbbf075195f51683fc55aaeb1b5c9a6e"
-    sha256 cellar: :any,                 monterey:       "cde7ea8b386ddf37ae4ec4b0901ba70583e5bff84d6bea9624a7064fef11b6a8"
-    sha256 cellar: :any,                 big_sur:        "6410e6fd033c0145e2d6d4676776cc4f4c20cf540836963d74a16788c842a7fd"
-    sha256 cellar: :any,                 catalina:       "b5915184be3174c64f03a0895a9ee71dc8baac9dcd5bf5e904977890ccbba2ed"
-    sha256 cellar: :any,                 mojave:         "786743340aeae4fde2966f29bb0457123b529c42c5cbe52609ebdaad447b7280"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b3979306018ca60bc17dc7547699ef716342c46c1e755ba15d53f6eb9ac92dd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libdvdcss"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "034c340770bab41dbcd5380aa44042afbe4772d007a82bc3676a6ab6e3c8814a"
   end
 
   head do
