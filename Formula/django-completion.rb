9cd4b1136dad5bd7783253fc282b5558086d331f
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:52:42 +0800

    django-completion: no rebuilding required

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://www.djangoproject.com/"
-  url "https://github.com/django/django/archive/4.1.3.tar.gz"
-  sha256 "7706c28345bbf7e1feff5c61fc9220270b5fc4762e3a07fb93108748268ddead"
+  url "https://github.com/django/django/archive/4.1.tar.gz"
+  sha256 "438269e4a1021bcb67d71345d60c1b56163651a3b00f90dbdbb5d62f4819b87c"
   license "BSD-3-Clause"
   head "https://github.com/django/django.git", branch: "main"
 
@@ -12,7 +12,8 @@ class DjangoCompletion < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ba355d5b4bf19b403f9b281599a3f613020a2f363c4defbff9df92578e8d0deb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/django-completion"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f7d339a49422da573ef1a74aa080e579ae346395b0c65349b15e8b52a8b5b99e"
   end
 
   def install
