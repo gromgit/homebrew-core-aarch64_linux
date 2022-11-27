746bb8bee64ffb83b285484e6f916425ee834e2f
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:49:41 +0800

    miniupnpc: no rebuilding required

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -1,8 +1,8 @@
 class Miniupnpc < Formula
   desc "UPnP IGD client library and daemon"
   homepage "https://miniupnp.tuxfamily.org"
-  url "https://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-2.2.4.tar.gz"
-  sha256 "481a5e4aede64e9ef29895b218836c3608d973e77a35b4f228ab1f3629412c4b"
+  url "http://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-2.2.3.tar.gz"
+  sha256 "dce41b4a4f08521c53a0ab163ad2007d18b5e1aa173ea5803bd47a1be3159c24"
   license "BSD-3-Clause"
 
   # We only match versions with only a major/minor since versions like 2.1 are
@@ -13,15 +13,14 @@ class Miniupnpc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "e01ae8c24a471bbb88a1033b67e041e8f9fca6634d01f92a4694db20d4d53c14"
-    sha256 cellar: :any,                 arm64_monterey: "6181bbf0ea573d2f09d125c6271412502605f6a0c9e64b006cde35f048cc69f8"
-    sha256 cellar: :any,                 arm64_big_sur:  "c3b527cd69eaba31f8b87f36f8a85c0c13a3c9bb19989abeba8ceefd78b9044c"
-    sha256 cellar: :any,                 ventura:        "b1cb1853a2a3ff1772d70022e1e8d6a420735459fad3e1d26396df562fdfff3b"
-    sha256 cellar: :any,                 monterey:       "92ec5cfb40d1da0fd842bfd2ec32ee3f5970776da4a2334aefce69463600828c"
-    sha256 cellar: :any,                 big_sur:        "5d2661c359f7d7734eeedd7905038cd1e86d2b41df2aa81ca5e5ccdf55445566"
-    sha256 cellar: :any,                 catalina:       "06f208877ac3279b5a9960846e27df4d8d0452a0e4d500f20ce7e01305f0e5c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "93154d49e70a5e1f1a3ff6f481432e7acd9e7dd352f7c2ea78e7ca2f27b7352f"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/miniupnpc"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1787492aa953003f3adaa66097087091565d99b5ce6c2ee2a27f39869a4550c0"
   end
+
+  # Fix missing references to $(BUILD) in the install rules
+  # equivalent to https://github.com/miniupnp/miniupnp/commit/ed1dc4bb5cdc4a53963f3eb01089289e30acc5a3
+  # but modified to start with the miniupnpc folder as root
+  patch :DATA
 
   def install
     system "make", "INSTALLPREFIX=#{prefix}", "install"
@@ -32,3 +31,38 @@ class Miniupnpc < Formula
     assert_match version.to_s, output
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index 4563b283..11a17f95 100644
+--- a/Makefile
++++ b/Makefile
+@@ -162,7 +162,7 @@ PKGCONFIGDIR = $(INSTALLDIRLIB)/pkgconfig
+ 
+ FILESTOINSTALL = $(LIBRARY) $(EXECUTABLES)
+ ifeq (, $(findstring amiga, $(OS)))
+-FILESTOINSTALL += $(SHAREDLIBRARY) miniupnpc.pc
++FILESTOINSTALL += $(SHAREDLIBRARY) $(BUILD)/miniupnpc.pc
+ endif
+ 
+ 
+@@ -251,15 +251,15 @@ install:	updateversion $(FILESTOINSTALL)
+ 	$(INSTALL) -m 644 $(LIBRARY) $(DESTDIR)$(INSTALLDIRLIB)
+ ifeq (, $(findstring amiga, $(OS)))
+ 	$(INSTALL) -m 644 $(SHAREDLIBRARY) $(DESTDIR)$(INSTALLDIRLIB)/$(SONAME)
+-	ln -fs $(SONAME) $(DESTDIR)$(INSTALLDIRLIB)/$(SHAREDLIBRARY)
++	ln -fs $(SONAME) $(DESTDIR)$(INSTALLDIRLIB)/$(notdir $(SHAREDLIBRARY))
+ 	$(INSTALL) -d $(DESTDIR)$(PKGCONFIGDIR)
+-	$(INSTALL) -m 644 miniupnpc.pc $(DESTDIR)$(PKGCONFIGDIR)
++	$(INSTALL) -m 644 $(BUILD)/miniupnpc.pc $(DESTDIR)$(PKGCONFIGDIR)
+ endif
+ 	$(INSTALL) -d $(DESTDIR)$(INSTALLDIRBIN)
+ ifneq (, $(findstring amiga, $(OS)))
+-	$(INSTALL) -m 755 upnpc-static $(DESTDIR)$(INSTALLDIRBIN)/upnpc
++	$(INSTALL) -m 755 $(BUILD)/upnpc-static $(DESTDIR)$(INSTALLDIRBIN)/upnpc
+ else
+-	$(INSTALL) -m 755 upnpc-shared $(DESTDIR)$(INSTALLDIRBIN)/upnpc
++	$(INSTALL) -m 755 $(BUILD)/upnpc-shared $(DESTDIR)$(INSTALLDIRBIN)/upnpc
+ endif
+ 	$(INSTALL) -m 755 external-ip.sh $(DESTDIR)$(INSTALLDIRBIN)/external-ip
+ ifeq (, $(findstring amiga, $(OS)))
