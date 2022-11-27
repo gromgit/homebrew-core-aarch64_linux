49c8a4d57793589ff8c482d4403886141ceafc51
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:27:08 +0800

    swig: no rebuilding required

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -1,19 +1,13 @@
 class Swig < Formula
   desc "Generate scripting interfaces to C/C++ code"
   homepage "http://www.swig.org/"
-  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.1.0/swig-4.1.0.tar.gz"
-  sha256 "d6a9a8094e78f7cfb6f80a73cc271e1fe388c8638ed22668622c2c646df5bb3d"
+  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz"
+  sha256 "d53be9730d8d58a16bf0cbd1f8ac0c0c3e1090573168bfa151b01eb47fa906fc"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_ventura:  "da2a0ed31e7c512de65131d162be610f2009518386a70f69db1b9134a94506ac"
-    sha256 arm64_monterey: "80291f33f362db51985ec93db7a80a2a8efdfc377e3bec1175696707f9254a6b"
-    sha256 arm64_big_sur:  "5825b5b4d280fa33d16194cce2db7a32a5bfc3f01d424027e78ccb1ef2c39274"
-    sha256 ventura:        "9374e7b731037ae529d1a8449b798ef7107580c71d8f02592929d19075a9c246"
-    sha256 monterey:       "dafaef7743b38de422618126d5bc9a7727c94c8e253258c8994f8aedd46f574e"
-    sha256 big_sur:        "79f2d7c2d3b443d0b3a0ccaeead34da0e5d3e66963d9c14130ebe00f6f2c4a2f"
-    sha256 catalina:       "c0b65fbcae8384ff3433a6b933a85faa5c1af090cbea90026b5b0cd82cc5dbce"
-    sha256 x86_64_linux:   "f330831a8030b19c3f4ef25dd2b980daa343dd9119a89c736b45559170bc20a9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/swig"
+    sha256 aarch64_linux: "13e2c03166fc7cb065b2e6eedfd5a878153782a4551dbf2e05345a8a8edfb435"
   end
 
   head do
@@ -23,7 +17,7 @@ class Swig < Formula
     depends_on "automake" => :build
   end
 
-  depends_on "pcre2"
+  depends_on "pcre"
 
   uses_from_macos "ruby" => :test
 
