5391b1a1f56188dfe674ff8223be827cf6dd686b
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:38:37 +0800

    librem: no rebuilding required

--- Formula/librem.rb
+++ Formula/librem.rb
@@ -1,18 +1,13 @@
 class Librem < Formula
   desc "Toolkit library for real-time audio and video processing"
   homepage "https://github.com/baresip/rem"
-  url "https://github.com/baresip/rem/archive/refs/tags/v2.9.0.tar.gz"
-  sha256 "ec87deef927c27b2199e652993bdd8d62d6176efc0231ddbe5227f212e27a881"
+  url "https://github.com/baresip/rem/archive/refs/tags/v2.7.0.tar.gz"
+  sha256 "7f4ad245eb9fb07a82890022d1c721c93ee2e299acb88a7e775ca541f7a09892"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "56442361d6b20c84d630cebd8cd7ef5ef24aa4f88b4a627dc4fa346d8bfdddc2"
-    sha256 cellar: :any,                 arm64_monterey: "e9dc062a114d69b463b7ec80988249390e6f4111ef0801a618cb0256e3842391"
-    sha256 cellar: :any,                 arm64_big_sur:  "57c83221b7d6bc51972c02509eae3d18e54b67c112bb78b5ca90d0e3b871b8f0"
-    sha256 cellar: :any,                 monterey:       "92c89a0f99ae9342a7c4d7b7802bb82085877fa908e78c4d481c1aa38665d4bd"
-    sha256 cellar: :any,                 big_sur:        "45d019137c7c4100c87e3f238e597b396546b11c16261cb2cd899c29bfc6cdbe"
-    sha256 cellar: :any,                 catalina:       "3c7fb584d06bd818a49d9b80356ff2579183d9d8bb583c77a995bbfe4e5cc82e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6e610de0068c95b1cf76f0f98c6f03fc4801b98422ea3669e3ad9caf1e76576"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/librem"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5e937c5afa1066bc3ddc3a532327d12b8bf168734e9f82adffb87893654424c8"
   end
 
   depends_on "libre"
