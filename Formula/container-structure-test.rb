d4c3d969a91a51fb0ab4d4e4e22ad54135900e29
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:45:35 +0800

    container-structure-test: no rebuilding required

--- Formula/container-structure-test.rb
+++ Formula/container-structure-test.rb
@@ -1,19 +1,14 @@
 class ContainerStructureTest < Formula
   desc "Validate the structure of your container images"
   homepage "https://github.com/GoogleContainerTools/container-structure-test"
-  url "https://github.com/GoogleContainerTools/container-structure-test/archive/v1.14.0.tar.gz"
-  sha256 "a52a28f94f608ce2132b5b9ebfa29db0c3eb382f0d0644be3877e64713ae2900"
+  url "https://github.com/GoogleContainerTools/container-structure-test/archive/v1.11.0.tar.gz"
+  sha256 "38bf793106da6cf7bfead49fae2a75c51609a304de4580af099eb57c5a018761"
   license "Apache-2.0"
   head "https://github.com/GoogleContainerTools/container-structure-test.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "09425ba24a07cc526904d8a9a71a0591809ed0d2120eb8c836b18a0a277f2f28"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac67b4fa0a58c25ee63f9b664ccee63a8c410ad6d095bb13ea5fe1200a8f2877"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f02284416cfd39331c161d5b0fc409c42229956cd588a081e5b178bc89685ae"
-    sha256 cellar: :any_skip_relocation, monterey:       "924b7cf12f63b5ee248046c9f68f2d9de2c91838acf32da68b29bd7864c09afc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4007f283f958fb44985d867500617ec0fecfcb0e70c8943d0e1c79ce235bd2a7"
-    sha256 cellar: :any_skip_relocation, catalina:       "77e2b6d61f00faf44a948d6416591c1b643906208ce11be87c226c1dd93e320f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "183f1757591e1d72d3e2672277146f205168a8200eddd45920db6433d321edcf"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/container-structure-test"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "b87dbac326e4072e2d916e9a098d66fd76e18f0d4e771824ccc89644b125db45"
   end
 
   depends_on "go" => :build
