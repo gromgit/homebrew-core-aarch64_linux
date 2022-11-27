419d700782f44a730dcc35c25ac00e485c96c4ba
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:41:37 +0800

    libxext: no rebuilding required

--- Formula/libxext.rb
+++ Formula/libxext.rb
@@ -1,19 +1,13 @@
 class Libxext < Formula
   desc "X.Org: Library for common extensions to the X11 protocol"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXext-1.3.5.tar.gz"
-  sha256 "1a3dcda154f803be0285b46c9338515804b874b5ccc7a2b769ab7fd76f1035bd"
+  url "https://www.x.org/archive/individual/lib/libXext-1.3.4.tar.bz2"
+  sha256 "59ad6fcce98deaecc14d39a672cf218ca37aba617c9a0f691cac3bcd28edf82b"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "36ef5333565be1614ad8eb2d740ea93df80c5d2ee41b403145179e7c5d1e1e82"
-    sha256 cellar: :any,                 arm64_monterey: "4f194ef26f45585047eab4ceaa2c811c4273d32d2ea47d9b3d18f09bb0c1f300"
-    sha256 cellar: :any,                 arm64_big_sur:  "232f1d192c14de36a9cb57dfedd73943d65fdd6ce3b75e80d4d0c2eb6c18dcd6"
-    sha256 cellar: :any,                 ventura:        "8567d6ebc49a8efcda5216368ba27ed2493bb4bb00492b66ba9e1686cee17c61"
-    sha256 cellar: :any,                 monterey:       "9682e4720a940f352af2cea9e4633cdf14947e23b640bac524e1b3674b9493a9"
-    sha256 cellar: :any,                 big_sur:        "674376b0e5cbcfb9278cdcf97e4ccc3910bf134cb1e45bf9e190d614a7f3a200"
-    sha256 cellar: :any,                 catalina:       "77862d3344c49f5bc3bf77f3efeaff99d36330bf13cb05c91cf7a4f4d43ed7a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c5e6ada73c0d137a611ca58038a36227f3ac10e59986b9bf9da21e2a44bdaab2"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxext"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "5e958571ab96143c499a66cadf809d9749b7724d76c167b48bf344e025134521"
   end
 
   depends_on "pkg-config" => :build
