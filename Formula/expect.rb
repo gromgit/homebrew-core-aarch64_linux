2fc8d9d91d086fcad4ba5e42b221d978e48f7e5e
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:59:51 +0800

    expect: no rebuilding required

--- Formula/expect.rb
+++ Formula/expect.rb
@@ -12,15 +12,8 @@ class Expect < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "4a49c4ad71978deb1a4c526b2e90891b9173251e4178e2f2649e1344b1ed7cf6"
-    sha256 arm64_monterey: "865f91b99656011e6c4cb681a5d5306805f97532158e25eaa6ba74685c9cfc72"
-    sha256 arm64_big_sur:  "aacaef6b4ae9a82f8039722e623ad66117e1154f9ddc0f4cf3a7c450147ba010"
-    sha256 ventura:        "75af989c1abdd8b9a2cffee0e5a747fec4309b0ffd6e42fe6123bfe89e30f08d"
-    sha256 monterey:       "65f7837ddba5bc6efe90b201e853fa117a617e711074546444b98c665238f1d8"
-    sha256 big_sur:        "b7824e3cc83c7b063198bb7505bbd723481327ff40d36ac91ba8950621bcbc49"
-    sha256 catalina:       "366066798dba96afbfbbf5b262bb3df9e6405e79b1e4d7160dd9610308ec4b3e"
-    sha256 mojave:         "da69b859dd682d61f2380523c3e1afbed2d06e453d4e88e0ce6bb5566df24082"
-    sha256 x86_64_linux:   "518fab49b38bf1b7ce6118907dec39e476c4f57e0c1a3dbfc2113d95a6c1760b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/expect"
+    sha256 aarch64_linux: "9d6188c73431c9e8f533121fef130bb98450796ca403b4a45d46740567045b8b"
   end
 
   # Autotools are introduced here to regenerate configure script. Remove
