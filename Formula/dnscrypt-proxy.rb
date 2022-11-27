2ce8e153fa9b4394f4a64ba08389fb47218ae7ff
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:53:08 +0800

    dnscrypt-proxy: no rebuilding required

--- Formula/dnscrypt-proxy.rb
+++ Formula/dnscrypt-proxy.rb
@@ -12,14 +12,8 @@ class DnscryptProxy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "63127aa2055234930ffaba3657c0b929c6617e0d6a51f838b62cc2ce2ced5a47"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d3ea6c1b4e59e02b601b6ac4d725823ea9e988fac26e51e11f39c953a5f218e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fb685dac6c6eede13db486d13282c119bf65ec1453bf44210db24c9fe952098b"
-    sha256 cellar: :any_skip_relocation, ventura:        "89e267e37cd8366c3940cdfd5bf608ca9a866429934f54a50a4365376a039202"
-    sha256 cellar: :any_skip_relocation, monterey:       "53d22657ada544f52117830389f83a3e3a9f5bb138cb0e9687befd7d5781fa8a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0bcc224c3004a27208125a6c97135a2bc1ee23b7e2254e37c1046d9bc998d418"
-    sha256 cellar: :any_skip_relocation, catalina:       "e130425e2c1000396b6b51b942e83a87c44f36c7330d37284111c6101aa67a97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "375d96ef3b1a89d3d5f1e5e340b9efc83acbefee67c080629cf567d7a6e7515d"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dnscrypt-proxy"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "7a63a06f6aa13a91da0181c472ebcb290fd79691161c8b8aa016d4e5f376d7b5"
   end
 
   depends_on "go" => :build
