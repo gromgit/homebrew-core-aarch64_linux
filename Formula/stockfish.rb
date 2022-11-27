affbb1c9c289e221009d5e2b7dad6536260eb9e2
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:25:56 +0800

    stockfish: no rebuilding required

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -12,21 +12,23 @@ class Stockfish < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9fc7b85028b41cd7e3db3f25a9481521aef01f95611e979734b815c64631f159"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ea9532bb3f1686c433a62c8eb671a0cfef3a9ead7a6a5a83475826c5bcb49e7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9961c12ee12480d1dcb9f72af2b735ee6c4d686e62b0554625efe0246f9a327e"
-    sha256 cellar: :any_skip_relocation, ventura:        "e9c420bf30df7268fe606e4efc481d7e6d5c25988c70eb30184c1ad81f88a1e6"
-    sha256 cellar: :any_skip_relocation, monterey:       "c29b02b056094c6f11072cf38351ad259804d97e1727c149fc8a3f6716a1d993"
-    sha256 cellar: :any_skip_relocation, big_sur:        "39efb53e1d05be75937cd9db2af59e1bfd618992b455c702a23c3628e6bbe154"
-    sha256 cellar: :any_skip_relocation, catalina:       "da786a5130e0126c86ce20fa078a3af16270a0b9649411e9ee64a28c370b3a73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9f92ead9b38a5ffcac5fc72b052a6c63953c0cd057a142ffa7f8215cb7146c6"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/stockfish"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "72084159f0401906eed264557a282ee3ea15bb2c9eb3e500c52c502a957b1bd9"
   end
 
-  fails_with gcc: "5" # For C++17
+  on_linux do
+    depends_on "gcc" # For C++17
+  end
+
+  fails_with gcc: "5"
 
   def install
     arch = Hardware::CPU.arm? ? "apple-silicon" : "x86-64-modern"
+    on_linux do
+      on_arm do
+        arch = Hardware::CPU.is_64_bit? ? "armv8" : "armv7-neon"
+      end
+    end
 
     system "make", "-C", "src", "build", "ARCH=#{arch}"
     bin.install "src/stockfish"
