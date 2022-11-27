c54bf014617980fd794e396b05a8b9f199257e24
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:50:17 +0800

    dbmate: no rebuilding required

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -1,20 +1,14 @@
 class Dbmate < Formula
   desc "Lightweight, framework-agnostic database migration tool"
   homepage "https://github.com/amacneil/dbmate"
-  url "https://github.com/amacneil/dbmate/archive/v1.16.0.tar.gz"
-  sha256 "1b3feae085837bd21243ef1966e64536a837ee260ccb809323879940fa5df877"
+  url "https://github.com/amacneil/dbmate/archive/v1.15.0.tar.gz"
+  sha256 "1460d1591c0c7340a90e6aee32aa123ec24d88f8e5486482d2bab318497a42bf"
   license "MIT"
   head "https://github.com/amacneil/dbmate.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "84741107e6a4ae2535a5ac79a41fe6f35ffce6b7af815053edd5ee7a013b264c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "883afeccd9b6ec5d556f6f464b649dcf892a101591c72850a1f0849b61ce5934"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "608fe9104738fee53cc2575ec83905352e90e1a7b130644aff0912d4232da8cf"
-    sha256 cellar: :any_skip_relocation, ventura:        "6e7c0a8185fd2320499fbbe7bd326f72af65f128c81cb3bbad296942edb3cd8f"
-    sha256 cellar: :any_skip_relocation, monterey:       "496b49b81b6f79263021d91dbecdac39df9553a087916fc5c7a8e59773399672"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6817c9225395174c7504486ea64a52995ec2c919078a4174f570c5b464a0447b"
-    sha256 cellar: :any_skip_relocation, catalina:       "fa3aa15ff3782f200cea1aa55c56bdf6402de008dd9d10c26245c4c8e5a004f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe1a071af2ac4640ba413612693103bc94514451e6538693adaaa60027efd0c1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dbmate"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ec2a01f5e836361dc1d37772064231408ac582b0d3c6c39872768ee8ca156cb2"
   end
 
   depends_on "go" => :build
