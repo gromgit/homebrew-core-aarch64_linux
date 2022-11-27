bbee5a6338da98fd234b3c84860edb87db391dd7
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:52:34 +0800

    dislocker:

--- Formula/dislocker.rb
+++ Formula/dislocker.rb
@@ -1,18 +1,25 @@
 class Dislocker < Formula
   desc "FUSE driver to read/write Windows' BitLocker-ed volumes"
   homepage "https://github.com/Aorimn/dislocker"
-  url "https://github.com/Aorimn/dislocker/archive/refs/tags/v0.7.3.tar.gz"
-  sha256 "8d5275577c44f2bd87f6e05dd61971a71c0e56a9cbedf000bd38deadd8b6c1e6"
+  url "https://github.com/Aorimn/dislocker/archive/v0.7.1.tar.gz"
+  sha256 "742fb5c1b3ff540368ced54c29eae8b488ae5a5fcaca092947e17c2d358a6762"
   license "GPL-2.0-only"
+  revision 6
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "39d819d5a39665f1de591aa76cda6ac58e334807dc246d6476169964e35998b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "56e7885d22b62ad21f3a021f07c9942a39aeb04adf9244bf4815a1196993b7a0"
   end
 
   depends_on "cmake" => :build
-  depends_on "libfuse@2"
-  depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "mbedtls@2"
+
+  on_macos do
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
+  end
+
+  on_linux do
+    depends_on "libfuse@2"
+  end
 
   def install
     args = std_cmake_args + %w[
@@ -22,6 +29,18 @@ class Dislocker < Formula
     system "cmake", *args, "."
     system "make"
     system "make", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
   end
 
   test do
