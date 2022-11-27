b595851fe6da7088412271e233dc8459ba587d75
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:39:42 +0800

    libssh: no rebuilding required

--- Formula/libssh.rb
+++ Formula/libssh.rb
@@ -1,20 +1,14 @@
 class Libssh < Formula
   desc "C library SSHv1/SSHv2 client and server protocols"
   homepage "https://www.libssh.org/"
-  url "https://www.libssh.org/files/0.10/libssh-0.10.4.tar.xz"
-  sha256 "07392c54ab61476288d1c1f0a7c557b50211797ad00c34c3af2bbc4dbc4bd97d"
+  url "https://www.libssh.org/files/0.10/libssh-0.10.3.tar.xz"
+  sha256 "6e889dbe4f3eecd13a452ca868ec85525ab9c39d778519a9c141b83da738c8aa"
   license "LGPL-2.1-or-later"
   head "https://git.libssh.org/projects/libssh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "070835503169e43125a6c78d21229e8b1489c0ec37d2899e3658784513434789"
-    sha256 cellar: :any,                 arm64_monterey: "3932e63ecd8236e305a43d1aa27c98957b8c513171873d21fd9858671c1b4a5d"
-    sha256 cellar: :any,                 arm64_big_sur:  "3b2886c28cb74cc6eec70cc7fe0225ec55567848df8cf5b97cd2226b56a91675"
-    sha256 cellar: :any,                 ventura:        "977c22fa0c47b658e37b719914f8896e7f32057ff10e2151ed25606d9af6149b"
-    sha256 cellar: :any,                 monterey:       "4c965d61eef0b2ce050e191f399f7f84d9300035d444af06b72b7401e0927e20"
-    sha256 cellar: :any,                 big_sur:        "8338dc10bac32e5c6c3a5e4004f6067c414ff179658271e7c886385efeb8ef43"
-    sha256 cellar: :any,                 catalina:       "4e02e90e4e3691c4ccfdd67ffc66041b15d60d8bf5b75373f84dcf49d28c93eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "000115cbf2dac1a926830428ff9685bfca24e7a745017e0e6f50dcc5c9f5a15a"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libssh"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "39e50a87009dd73e73c34a43963735d946baae68257c5465db2b90cda73d5126"
   end
 
   depends_on "cmake" => :build
