bcd6ce5b71bc93997e6f986121c6b475235b653e
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:56:58 +0800

    numactl: no rebuilding required

--- Formula/numactl.rb
+++ Formula/numactl.rb
@@ -1,12 +1,13 @@
 class Numactl < Formula
   desc "NUMA support for Linux"
   homepage "https://github.com/numactl/numactl"
-  url "https://github.com/numactl/numactl/releases/download/v2.0.16/numactl-2.0.16.tar.gz"
-  sha256 "1b242f893af977a1d31af6ce9d6b8dafdd2d8ec3dc9207f7c2dc0d3446e7c7c8"
+  url "https://github.com/numactl/numactl/releases/download/v2.0.14/numactl-2.0.14.tar.gz"
+  sha256 "826bd148c1b6231e1284e42a4db510207747484b112aee25ed6b1078756bcff6"
   license all_of: ["GPL-2.0-only", "LGPL-2.1-only", :public_domain, :cannot_represent]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "809a693e16e891c8ee072c0e403769daedbaf3cbaa52291cc0bb0c008fc8065b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/numactl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cf1bfdc88e86702da61c67cb3a3292ec5eacf884a06a78a5205b6ea815fa84f7"
   end
 
   depends_on :linux
