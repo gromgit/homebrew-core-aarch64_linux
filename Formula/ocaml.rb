4a5c58a92d17ada1a45057f7918636f2a0f6cfa6
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:57:45 +0800

    ocaml: no rebuilding required

--- Formula/ocaml.rb
+++ Formula/ocaml.rb
@@ -13,18 +13,11 @@
 class Ocaml < Formula
   desc "General purpose programming language in the ML family"
   homepage "https://ocaml.org/"
+  # Remove `coq` from `flat_namespace_allowlist` at version bump.
+  url "https://caml.inria.fr/pub/distrib/ocaml-4.12/ocaml-4.12.0.tar.xz"
+  sha256 "39ee9db8dc1e3eb65473dd81a71fabab7cc253dbd7b85e9f9b5b28271319bec3"
   license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
   head "https://github.com/ocaml/ocaml.git", branch: "trunk"
-
-  stable do
-    url "https://caml.inria.fr/pub/distrib/ocaml-4.14/ocaml-4.14.0.tar.xz"
-    sha256 "36abd8cca53ff593d5e7cd8b98eee2f1f36bd49aaf6ff26dc4c4dd21d861ac2b"
-
-    # Remove use of -flat_namespace. Upstreamed at
-    # https://github.com/ocaml/ocaml/pull/10723
-    # We embed a patch here so we don't have to regenerate configure.
-    patch :DATA
-  end
 
   livecheck do
     url "https://ocaml.org/releases"
@@ -32,19 +25,18 @@ class Ocaml < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "fea680c0c2345edadc91fa0b9102ab2b1223a0f4312370a8fc4f917164f98e51"
-    sha256 cellar: :any,                 arm64_monterey: "4627e987e1517d78cfd6f40aa84a7f8c29b1d20ae845d345038fc7b548148413"
-    sha256 cellar: :any,                 arm64_big_sur:  "dd4cddcad1dd890d90d2db3cd4119ec0dc8b30e10053ca480812e16a90dff342"
-    sha256 cellar: :any,                 ventura:        "a0a61f07a68cce8bbc534d8b3f87059390c1082bd0d0d050fa28c3b05075d239"
-    sha256 cellar: :any,                 monterey:       "f744606227e8187baacabccec4baf57aca0f4309eb3bf93623a6789861369dab"
-    sha256 cellar: :any,                 big_sur:        "bfff444bfd1a9f4c441087537e602964e19f00adad4a8b15b5e0aad522b0c1b3"
-    sha256 cellar: :any,                 catalina:       "130c3b5d8c8de8cfbdafb57d48abf75a98e560c2ed4ae4bbc6f4a388145fe401"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "493cc9bbb57be2722ebf5203aa95fc93ec24af04cfadf474cbccba125dfbd215"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/ocaml"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1d8372e6966024d2949989105b5a45ae30240f73d71342a0273a368b1fb943a2"
   end
 
   # The ocaml compilers embed prefix information in weird ways that the default
   # brew detection doesn't find, and so needs to be explicitly blocked.
   pour_bottle? only_if: :default_prefix
+
+  # Remove use of -flat_namespace. Upstreamed at
+  # https://github.com/ocaml/ocaml/pull/10723
+  # We embed a patch here so we don't have to regenerate configure.
+  patch :p0, :DATA
 
   # Fix -flat_namespace being used on Big Sur and later.
   patch do
@@ -74,14 +66,14 @@ class Ocaml < Formula
 end
 
 __END__
---- a/configure
-+++ b/configure
-@@ -14087,7 +14087,7 @@ if test x"$enable_shared" != "xno"; then :
+--- configure.orig	2021-10-24 09:34:12.145636659 +0800
++++ configure	2021-10-24 09:34:30.504944693 +0800
+@@ -13644,7 +13644,7 @@
+ if test x"$enable_shared" != "xno"; then :
    case $host in #(
    *-apple-darwin*) :
-     mksharedlib="$CC -shared \
--                   -flat_namespace -undefined suppress -Wl,-no_compact_unwind \
-+                   -undefined dynamic_lookup -Wl,-no_compact_unwind \
-                    \$(LDFLAGS)"
-       supports_shared_libraries=true ;; #(
+-    mksharedlib="$CC -shared -flat_namespace -undefined suppress \
++    mksharedlib="$CC -shared -undefined dynamic_lookup \
+                    -Wl,-no_compact_unwind"
+       shared_libraries_supported=true ;; #(
    *-*-mingw32) :
