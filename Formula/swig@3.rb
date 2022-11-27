ae0fc90fe1408aba844517cd1361b79f13ec6eed
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:27:11 +0800

    swig@3: no rebuilding required

--- Formula/swig@3.rb
+++ Formula/swig@3.rb
@@ -5,17 +5,8 @@ class SwigAT3 < Formula
   sha256 "7cf9f447ae7ed1c51722efc45e7f14418d15d7a1e143ac9f09a668999f4fc94d"
 
   bottle do
-    sha256 arm64_ventura:  "9749ef0bbccfff848f95c309c44fc1b631e725f464cf2bdcc53d1b603d874f3f"
-    sha256 arm64_monterey: "f80bcd7727b999a42e672f9424b304cc5e6d801201baeddb6a6b9d4a4a7d4fe1"
-    sha256 arm64_big_sur:  "233ac5b77d2e887d5d63f965897d0375101ef2ab16fd87e631e67030f553be06"
-    sha256 ventura:        "101fa82e79e21d2ecc67e03754d941a583effd069477c94b4fb54be64d1f268e"
-    sha256 monterey:       "5546732b5a67d9cc7e71adde37ab80a84e98a357afe9ee21fd978927dddbe911"
-    sha256 big_sur:        "d0d9561bf105072f30a93f3914d05d58422f13c578de51f760e3822405dd5c36"
-    sha256 catalina:       "f50becfc883397db62530bab927dcf4b5a4db5f0bcbb2839d5ac795fb924c586"
-    sha256 mojave:         "28e5c0a5e8aac0c0d5f58e4dd69c590f57d3a450d92aa35b18aee037ab7d8b60"
-    sha256 high_sierra:    "730bd728981cc1534664ef35d08d0b285e79756c286913d868af6afa43f60f4d"
-    sha256 sierra:         "23275971784bb9272a734f44c9689dafecd5e6c4be917cd3d621064858cd76db"
-    sha256 x86_64_linux:   "8ba7debca7e99a4b12be8eef984ef4bf0826ad37baf171cd2ce36e55f23ee3da"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/swig@3"
+    sha256 aarch64_linux: "b2662233b258388f3d0d8b2777f39b35988d965087106bc693866360b0b4a9cd"
   end
 
   keg_only :versioned_formula
