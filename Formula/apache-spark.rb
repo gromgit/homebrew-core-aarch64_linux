74300c328d04d6831a96dd4b0751e29a18008262
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:26:44 +0800

    apache-spark: no rebuilding required

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,15 +1,16 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://dlcdn.apache.org/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz"
-  mirror "https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz"
-  version "3.3.1"
-  sha256 "91df3e3f73682d32261a8b245af6b0553dc5cf4af208ef82219283d23ee01ece"
+  url "https://dlcdn.apache.org/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz"
+  mirror "https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz"
+  version "3.3.0"
+  sha256 "306b550f42ce1b06772d6084c545ef8448414f2bf451e0b1175405488f2a322f"
   license "Apache-2.0"
   head "https://github.com/apache/spark.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "71f9e3e760180ae4621e2775415858599b360baf88fdf97dfdacb5b2fbf3c4e0"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/apache-spark"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "db8c95913d1bcbe16812fbead0e6a4afb75dc17b1c593a71522665f757d4e6a3"
   end
 
   depends_on "openjdk"
