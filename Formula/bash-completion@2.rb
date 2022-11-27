d4c73d2325820eb8a20e459957142e7243b2562a
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:31:54 +0800

    bash-completion@2: no rebuilding required

--- Formula/bash-completion@2.rb
+++ Formula/bash-completion@2.rb
@@ -11,16 +11,8 @@ class BashCompletionAT2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c4551dd2b4efcc64fe37febc7471365cba49648a46437972aeb57bb3ca0a3b08"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c4551dd2b4efcc64fe37febc7471365cba49648a46437972aeb57bb3ca0a3b08"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "336f04248a6da8c65291ef74c35430f843ae10b5c29d092ab463803fa14b2014"
-    sha256 cellar: :any_skip_relocation, ventura:        "606996545b7e56cb10c51052b0dc811d3c3e4c2246e4cf2c2fdfe78a97b0113d"
-    sha256 cellar: :any_skip_relocation, monterey:       "606996545b7e56cb10c51052b0dc811d3c3e4c2246e4cf2c2fdfe78a97b0113d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "27ccf1267d18fcd3e6018ec80363d003d07f750182bdef61150371532100bfc9"
-    sha256 cellar: :any_skip_relocation, catalina:       "3fe7e4021769be9a92eac055496e6189996c3527270db1dfdd4b0eb8cd7b4192"
-    sha256 cellar: :any_skip_relocation, mojave:         "3fe7e4021769be9a92eac055496e6189996c3527270db1dfdd4b0eb8cd7b4192"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "3fe7e4021769be9a92eac055496e6189996c3527270db1dfdd4b0eb8cd7b4192"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c4551dd2b4efcc64fe37febc7471365cba49648a46437972aeb57bb3ca0a3b08"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/bash-completion@2"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0e2ee299e4db767551ba8565b211ec06179220628c5e2350883432544a1c0bf0"
   end
 
   head do
