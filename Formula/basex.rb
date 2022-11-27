10d930d33eda819aeb6e6f4129bf60c900184f38
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:31:50 +0800

    basex: no rebuilding required

--- Formula/basex.rb
+++ Formula/basex.rb
@@ -1,9 +1,9 @@
 class Basex < Formula
   desc "Light-weight XML database and XPath/XQuery processor"
   homepage "https://basex.org"
-  url "https://files.basex.org/releases/10.3/BaseX103.zip"
-  version "10.3"
-  sha256 "efd9845822f517612c77582de3c71971c16cff9998bd242acf8623471a8185fd"
+  url "https://files.basex.org/releases/10.1/BaseX101.zip"
+  version "10.1"
+  sha256 "775d64ab0b0bb07a29162ef0fb04c35b1f61f9454ced9e6463bbca5ec06bbeea"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,7 +12,8 @@ class Basex < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "952246de05490df870636f3c647d42d2eb28aa18304de7e34b586e4bd75898be"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/basex"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e9a381be86e57e929c82341c429e3c1099f94ba88e1d6f2f98bcc96ade1230f6"
   end
 
   depends_on "openjdk"
