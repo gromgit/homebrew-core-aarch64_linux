ababc7dfcdd62aa5c78846e88be2d60d27c7e519
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:57:37 +0800

    eksctl: no rebuilding required

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,20 +2,14 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.121.0",
-      revision: "b0350a5fab2bb9934c5e7469bc4110afba090003"
+      tag:      "0.96.0",
+      revision: "59b50db419362c7dc57f0f94a3fd4c15ae2210da"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a9ccfd7f4ee86def623b529a5ae8f9ccde52a02e2d0a0a8074e628c8cf625ba"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "52f981c33721e80c228949f51c4d125e6fd0a2e7da21ea072106d7175fe4950d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f066a41b0ee4ce8591a01163fd90f832bf720a6ea7ee1aaca11954ef16b378a"
-    sha256 cellar: :any_skip_relocation, ventura:        "4b32c126fe7f52382db235ac96d5a113ba958aa5df8e1fb8b10fa1a90eafcd35"
-    sha256 cellar: :any_skip_relocation, monterey:       "5003ce7ed9f5039869cfcbbc92215b3988b77fc5e69079ff0fb86e100e6edfb5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7f1fa6a52201fe15fe89dec9b79f70ca661a0350c5102dff3da61201c0e0bacf"
-    sha256 cellar: :any_skip_relocation, catalina:       "4dd39fa6c5ec235224937280e55c6f680799b1838f148a6c4672800002cd4cbd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0817d4c064b15748c7f11bce79417291a280648b837c6d875dd2b76b01fc1ecd"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/eksctl"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e23d969d6e74147888e47c790bc80e43a98b2d603abaa5293d33b04ff03f2214"
   end
 
   depends_on "counterfeiter" => :build
@@ -41,11 +35,15 @@ class Eksctl < Formula
                                                           buildpath/"ifacemaker"
 
     ENV["GOBIN"] = HOMEBREW_PREFIX/"bin"
-    ENV.deparallelize # Makefile prerequisites need to be run in order
     system "make", "build"
     bin.install "eksctl"
 
-    generate_completions_from_executable(bin/"eksctl", "completion")
+    bash_output = Utils.safe_popen_read(bin/"eksctl", "completion", "bash")
+    (bash_completion/"eksctl").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"eksctl", "completion", "zsh")
+    (zsh_completion/"_eksctl").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"eksctl", "completion", "fish")
+    (fish_completion/"eksctl.fish").write fish_output
   end
 
   test do
