0613a96cfb227e1ca1334302c63ab1449b013866
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:07:45 +0800

    podman-compose:

--- Formula/podman-compose.rb
+++ Formula/podman-compose.rb
@@ -9,24 +9,27 @@ class PodmanCompose < Formula
   revision 1
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b8a3150b796d0691711da117dde78a8438cc2391206709dd0558506ac1a5cbe5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b8a3150b796d0691711da117dde78a8438cc2391206709dd0558506ac1a5cbe5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b8a3150b796d0691711da117dde78a8438cc2391206709dd0558506ac1a5cbe5"
-    sha256 cellar: :any_skip_relocation, ventura:        "1d695df8b43c37c854857464661f929488002ad7bf745483b5e4fd081803b5dc"
-    sha256 cellar: :any_skip_relocation, monterey:       "1d695df8b43c37c854857464661f929488002ad7bf745483b5e4fd081803b5dc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1d695df8b43c37c854857464661f929488002ad7bf745483b5e4fd081803b5dc"
-    sha256 cellar: :any_skip_relocation, catalina:       "1d695df8b43c37c854857464661f929488002ad7bf745483b5e4fd081803b5dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be33e397146ed2ab7795786284cbeb1fda7fe45ca235925c075bd69f19c3f299"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4c7eb9c367955f764da4821a9dc31a41f8acbc0a320c609601aa4c230f1ec7c2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "62406275548f3412477c72a4c23990d8a8674fa9d2f3e371126b65f84cb9aa6f"
+    sha256 cellar: :any_skip_relocation, monterey:       "b4db95a7f259f53ef888dff92bb63ffece19937739d1a85d8a5c007f55ebb53b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d1c9080f186bc02c2e7536d950ac14f7394ed9d6ed97aaa8cf1b0b3e4d6d207a"
+    sha256 cellar: :any_skip_relocation, catalina:       "0a5041233f1c03bf77d48592636fb384f895aee303fd84b7e5b4bb5b30b0175d"
   end
 
+  # Depends on the `podman` command, which the podman.rb formula does not
+  # currently install on Linux.
+  depends_on :macos
   depends_on "podman"
-  depends_on "python@3.11"
-  depends_on "pyyaml"
+  depends_on "python@3.10"
 
   resource "python-dotenv" do
-    url "https://files.pythonhosted.org/packages/87/8d/ab7352188f605e3f663f34692b2ed7457da5985857e9e4c2335cd12fb3c9/python-dotenv-0.21.0.tar.gz"
-    sha256 "b77d08274639e3d34145dfa6c7008e66df0f04b7be7a75fd0d5292c191d79045"
+    url "https://files.pythonhosted.org/packages/02/ee/43e1c862a3e7259a1f264958eaea144f0a2fac9f175c1659c674c34ea506/python-dotenv-0.20.0.tar.gz"
+    sha256 "b7e3b04a59693c42c36f9ab1cc2acc46fa5df8c78e178fc33a8d4cd05c8d498f"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
+    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   def install
@@ -49,8 +52,7 @@ class PodmanCompose < Formula
 
     # If it's trying to connect to Podman, we know it at least found the
     # compose.yml file and parsed/validated the contents
-    expected = OS.linux? ? "Error: cannot re-exec process" : "Cannot connect to Podman"
-    assert_match expected, shell_output("#{bin}/podman-compose up -d 2>&1", 1)
-    assert_match expected, shell_output("#{bin}/podman-compose down 2>&1")
+    assert_match "Cannot connect to Podman", shell_output("#{bin}/podman-compose up -d 2>&1", 1)
+    assert_match "Cannot connect to Podman", shell_output("#{bin}/podman-compose down 2>&1")
   end
 end
