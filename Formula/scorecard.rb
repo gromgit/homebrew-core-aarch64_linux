47ab0bf807faa6ad5c8462e3704c8577d6587496
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:18:46 +0800

    scorecard: no rebuilding required

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -2,47 +2,38 @@ class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
   url "https://github.com/ossf/scorecard.git",
-      tag:      "v4.8.0",
-      revision: "c40859202d739b31fd060ac5b30d17326cd74275"
+      tag:      "v4.1.0",
+      revision: "33f80c93dc79f860d874857c511c4d26d399609d"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "210754c5bab36169dab9b128bbc0e9130ab7d7df35edf5c8eb62fbb698578dd3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6764bf06ef4952d1c93210f041227cc35899d29a86234db21ad706d3d6637360"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2340685e76caf448e95b451d47c275bfc834fdfdd2f2169406b948feb299aca0"
-    sha256 cellar: :any_skip_relocation, ventura:        "4b1ae38cb2826dd95611aa5bebed510d85ab4a1bc6e3025bb53e1884ca2a2e89"
-    sha256 cellar: :any_skip_relocation, monterey:       "82a8a427b524fd412290fb8047a75cfa4a7d5dabf4e2e6b274f790b20580e43d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0dd3919fb6657b456b4a2bb20d4ca4f43729d424e4cc6ff0a35c7b5f2302b980"
-    sha256 cellar: :any_skip_relocation, catalina:       "4e7f970c3057381cff75469f93ddeb4b0899eac5a5387b3e49d090ecec6a6466"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91c269f768e4ac752ea21748ddfa44c492b7094a893015da6cdd91535ba3dfed"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/scorecard"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "22093357206ccc57605b2c428c66a4ae2bb4cce3612ba59fedc2bc92d64c7ebf"
   end
 
   depends_on "go" => :build
 
   def install
-    pkg = "sigs.k8s.io/release-utils/version"
     ldflags = %W[
       -s -w
-      -X #{pkg}.gitVersion=#{version}
-      -X #{pkg}.gitCommit=#{Utils.git_head}
-      -X #{pkg}.gitTreeState=clean
-      -X #{pkg}.buildDate=#{time.iso8601}
+      -X github.com/ossf/scorecard/v4/pkg.gitVersion=#{version}
+      -X github.com/ossf/scorecard/v4/pkg.gitCommit=#{Utils.git_head}
+      -X github.com/ossf/scorecard/v4/pkg.gitTreeState=clean
+      -X github.com/ossf/scorecard/v4/pkg.buildDate=#{time.iso8601}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
     system "make", "generate-docs"
     doc.install "docs/checks.md"
-
-    generate_completions_from_executable(bin/"scorecard", "completion")
   end
 
   test do
     ENV["GITHUB_AUTH_TOKEN"] = "test"
-    output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 1)
+    output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 2)
     expected_output = "InitRepo: repo unreachable: GET https://api.github.com/repos/google/oss-fuzz: 401"
     assert_match expected_output, output
 
-    assert_match version.to_s, shell_output("#{bin}/scorecard version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/scorecard version")
   end
 end
