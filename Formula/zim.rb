0460644594504544bcb84bd81a4693290188b9a0
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:44:13 +0800

    zim: no rebuilding required

--- Formula/zim.rb
+++ Formula/zim.rb
@@ -1,14 +1,13 @@
 class Zim < Formula
   desc "Graphical text editor used to maintain a collection of wiki pages"
   homepage "https://zim-wiki.org/"
-  url "https://github.com/zim-desktop-wiki/zim-desktop-wiki/archive/0.75.1.tar.gz"
-  sha256 "ce9d6108566668fe0acdfdce9e899e20a8645ec976d960f2d280b9cfaffdd513"
+  url "https://github.com/zim-desktop-wiki/zim-desktop-wiki/archive/0.74.3.tar.gz"
+  sha256 "4a5cff2f8bf99a89f9acaf1368df2ab711edee8d19dcbf3c4b4aeeba89e808aa"
   license "GPL-2.0-or-later"
   head "https://github.com/zim-desktop-wiki/zim-desktop-wiki.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "38a0b61a4123f51f06fde82e42e67fcabc332be300ad3cc3c53fcfcfc5834b49"
+    sha256 cellar: :any_skip_relocation, all: "df1dd11baa2693ecf8686ea4fab05cc30b1475dbc16d8c758a6473b751c345fb"
   end
 
   depends_on "pkg-config" => :build
@@ -17,36 +16,31 @@ class Zim < Formula
   depends_on "gtk+3"
   depends_on "gtksourceview3"
   depends_on "pygobject3"
-  depends_on "python@3.11"
+  depends_on "python@3.9"
 
   resource "pyxdg" do
-    url "https://files.pythonhosted.org/packages/b0/25/7998cd2dec731acbd438fbf91bc619603fc5188de0a9a17699a781840452/pyxdg-0.28.tar.gz"
-    sha256 "3267bb3074e934df202af2ee0868575484108581e6f3cb006af1da35395e88b4"
+    url "https://files.pythonhosted.org/packages/6f/2e/2251b5ae2f003d865beef79c8fcd517e907ed6a69f58c32403cec3eba9b2/pyxdg-0.27.tar.gz"
+    sha256 "80bd93aae5ed82435f20462ea0208fb198d8eec262e831ee06ce9ddb6b91c5a5"
   end
 
   def install
-    python3 = "python3.11"
-    site_packages = Language::Python.site_packages(python3)
-    ENV.prepend_create_path "PYTHONPATH", libexec/site_packages
+    python3 = which("python3")
+    ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages("python3")
     resource("pyxdg").stage do
-      system python3, *Language::Python.setup_install_args(libexec/"vendor", python3)
+      system python3, *Language::Python.setup_install_args(libexec/"vendor")
     end
-
     ENV["XDG_DATA_DIRS"] = libexec/"share"
-
-    zim_setup_install_args = Language::Python.setup_install_args(libexec, python3).reject { |s| s[/single-version/] }
-    zim_setup_install_args << "--install-data=#{prefix}"
-    system python3, *zim_setup_install_args
-    bin.install (libexec/"bin").children
+    system python3, "./setup.py", "install", "--prefix=#{libexec}"
+    bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files libexec/"bin",
-                             PYTHONPATH:    ENV["PYTHONPATH"],
-                             XDG_DATA_DIRS: [HOMEBREW_PREFIX/"share", libexec/"share"].join(":")
+      PYTHONPATH:    ENV["PYTHONPATH"],
+      XDG_DATA_DIRS: [HOMEBREW_PREFIX/"share", libexec/"share"].join(":")
     pkgshare.install "zim"
 
     # Make the bottles uniform
     inreplace [
-      libexec/site_packages/"zim/config/basedirs.py",
-      libexec/"vendor"/site_packages/"xdg/BaseDirectory.py",
+      libexec/Language::Python.site_packages("python3")/"zim/config/basedirs.py",
+      libexec/"vendor"/Language::Python.site_packages("python3")/"xdg/BaseDirectory.py",
       pkgshare/"zim/config/basedirs.py",
     ], "/usr/local", HOMEBREW_PREFIX
   end
@@ -64,9 +58,8 @@ class Zim < Formula
       "Content-Type: text/x-zim-wiki\nWiki-Format: zim 0.4\n" \
       "Creation-Date: 2020-03-02T07:17:51+02:00\n\n[[https://brew.sh|Homebrew]]",
     )
-    system bin/"zim", "--index", "./Notes"
-    system bin/"zim", "--export", "-r", "-o", "HTML", "./Notes"
-    assert_match "Homebrew:Homebrew", (testpath/"HTML/Homebrew/Homebrew.html").read
-    assert_match "https://brew.sh|Homebrew", (testpath/"Notes/Homebrew/Homebrew.txt").read
+    system "#{bin}/zim", "--index", "./Notes"
+    system "#{bin}/zim", "--export", "-r", "-o", "HTML", "./Notes"
+    system "grep", '<a href="https://brew.sh".*Homebrew</a>', "HTML/Homebrew/Homebrew.html"
   end
 end
