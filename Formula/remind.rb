06595c5cad8e65e4d66c56f42c759359b98ddfce
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:14:55 +0800

    remind: no rebuilding required

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -1,8 +1,8 @@
 class Remind < Formula
   desc "Sophisticated calendar and alarm"
   homepage "https://dianne.skoll.ca/projects/remind/"
-  url "https://dianne.skoll.ca/projects/remind/download/remind-04.02.00.tar.gz"
-  sha256 "a6476cf0dfe71bc4668e774669100c58d68b68dc6ccf08ca7ea9fa3345e72739"
+  url "https://dianne.skoll.ca/projects/remind/download/remind-04.00.03.tar.gz"
+  sha256 "b254de043ef3937694c5a458070aceb4dcf871b58bd2ae7b31ef125b00777879"
   license "GPL-2.0-only"
   head "https://git.skoll.ca/Skollsoft-Public/Remind.git", branch: "master"
 
@@ -12,14 +12,8 @@ class Remind < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "c2a1bcba977aababd25e119a83616cbac66d72356e93d33c95309a22fb9cc47d"
-    sha256 arm64_monterey: "57b522818917aa6dec6a28809c4b805c3b558fead531fb0fa9da448c415fe8c0"
-    sha256 arm64_big_sur:  "730775e3d26daf1a7a35f0e746b42db40dd1595e528aeb3a17bb44bd92510706"
-    sha256 ventura:        "4d4b1976006f2f3b998bb39f21f2e4a60857076e850de8cbc810ca1949910f1e"
-    sha256 monterey:       "57afc2cac915902842d0c1aaeaf280801e74328978efd371fc86d4135d0ecc63"
-    sha256 big_sur:        "d1610819c3951655ebc9cb637d41462a9459dacd18675735e75ddf0d87cd4c15"
-    sha256 catalina:       "0be7f6a1bbc14850a2d4ad39b43783ea6acc4f991d3d8c0b15a61917b3ddfc9b"
-    sha256 x86_64_linux:   "15b9fd47be67f496d6019be45856a4b200f150fe2ff64aace9b49a21287fd50e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/remind"
+    sha256 aarch64_linux: "6a2ecb3347497f659c65965627c793ca9534951dad89f0eaf72deb89fb750039"
   end
 
   conflicts_with "rem", because: "both install `rem` binaries"
