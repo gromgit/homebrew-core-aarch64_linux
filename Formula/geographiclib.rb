e64b3333f71d9555aa712ba4fa6194d394e49004
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:07:58 +0800

    geographiclib: no rebuilding required

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -6,13 +6,8 @@ class Geographiclib < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "8c13b49ba4fd61359db8469ac1ad2ae64b70f6757cb073cb085071943c497a94"
-    sha256 cellar: :any,                 arm64_monterey: "bfe0a6587b1795fb82b5c1b59c9b34462f720450ae32cb5940f6f3d4daf8b1ff"
-    sha256 cellar: :any,                 arm64_big_sur:  "a16bfa8a86ba9e3d72cc97ad1f3fdcb18fe60b16d09bfdcda16b19a12d4b38e2"
-    sha256 cellar: :any,                 monterey:       "c6f56914e3a91a17d2fdaccfe69b555936229cf85ec8e49cb331145eaf071a02"
-    sha256 cellar: :any,                 big_sur:        "ffeeacbc0e4fc7508d59d8c194b4f898545ad783d1e8e8aa56e5654722c96027"
-    sha256 cellar: :any,                 catalina:       "c4f069e57eff006d3f062ffe998ca0a8cefc9c42a93ea7969ec0bc187bef6064"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3aa5a4b955500a20587ef6c0b6a7064cac7e62620a69406ba3af1a002646f0bb"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/geographiclib"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "1218f12cff88ccc42063cc356813fba4255aadc447ca89c6d473cc1382a6f360"
   end
 
   depends_on "cmake" => :build
