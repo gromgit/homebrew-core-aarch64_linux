e83c990ccd1fc98208535372f0252a9b9697e8c7
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:38:50 +0800

    catch2: no rebuilding required

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -1,22 +1,20 @@
 class Catch2 < Formula
   desc "Modern, C++-native, header-only, test framework"
   homepage "https://github.com/catchorg/Catch2"
-  url "https://github.com/catchorg/Catch2/archive/v3.2.0.tar.gz"
-  sha256 "feee04647e28ac3cbeff46cb42abc8ee2d8d5f646d36e3fb3ba274b8c69a58ea"
+  url "https://github.com/catchorg/Catch2/archive/v3.1.0.tar.gz"
+  sha256 "c252b2d9537e18046d8b82535069d2567f77043f8e644acf9a9fffc22ea6e6f7"
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8bf9e27b974e7d708bbb6735406b222cf0226c620c546a83d69976adea4720b7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fdb910cbbfff1e17b8f50aece313f14f51723309a2ba5dac2705922482df0b3b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c0594e9eedd28cee43dbd68fb094a64c64cf4c62ec152cb90352fd38e0a2a1a2"
-    sha256 cellar: :any_skip_relocation, ventura:        "e5b1de3464dae23d480a05c7056e4ee2a9bf8676292f448966cd396132fc91f0"
-    sha256 cellar: :any_skip_relocation, monterey:       "bd0080d2f8dc23961dc19698e2b86ed02d72ad8084649e66433221c7c66a7d6c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fd5f9a443e018a9af01ad1bb4971800eee26274cdfe3f3d1bdd2686c4f92a07d"
-    sha256 cellar: :any_skip_relocation, catalina:       "4fe943290a551a890806508662dab78d2a49bb7a85322ea1c0cc913e26ed73e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "93fe2a2598faaeefd7dc6e991f52b1a3c3aaabab229f63aab0cb704a17bf3d9c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/catch2"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "85fa4bf703bb1f99569bb31c0e81a2ae934dd51efc01dc73036e7c7c28fddf87"
   end
 
   depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5"
 
