03514d15dfec73b3b0ed4a3033284bacc02e4dde
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:07:35 +0800

    gdu: no rebuilding required

--- Formula/gdu.rb
+++ Formula/gdu.rb
@@ -1,19 +1,13 @@
 class Gdu < Formula
   desc "Disk usage analyzer with console interface written in Go"
   homepage "https://github.com/dundee/gdu"
-  url "https://github.com/dundee/gdu/archive/v5.20.0.tar.gz"
-  sha256 "a5c164192dc631370f6f5313979dda200b52146c4f780e5ad73bbdb96d996da7"
+  url "https://github.com/dundee/gdu/archive/v5.15.0.tar.gz"
+  sha256 "4cbbaea0bc46853bde0508b8a9c5f9fdc95dd45ff21a2eb5c5adeb9917cb95b3"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dd997fd3e96ebc59e6c817aedb3f0b8c784c6218eaa4332939a9c5f766aba2e3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6551ad5d730103318db7c3b8aa96179cb2b78d7a8b1722f77edf9bc0fc9fe40b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f5b0f66324a6b10bb0f09ac684571d74cd04beb4d9b218ce70682332e7e3b26"
-    sha256 cellar: :any_skip_relocation, ventura:        "a5dfc1be08654bb5a0137f3922aab7e58ecaaf8afecbb66ee16346577b076962"
-    sha256 cellar: :any_skip_relocation, monterey:       "5d4cbe9b8ca4dd6c42862ca1dc87ce5fc2b31ee648b14fb6a332277447472eb9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "95c67a252a8ee1e48ee282c46ab6f3eea04047bd6cca9ecb9412d563a5c3107f"
-    sha256 cellar: :any_skip_relocation, catalina:       "b8f310068ed8645e5ef621453700f632115a55672ec86f33ed25a8b3cad7c0df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d87e62c098adba42a64e1a61367127933ccb2fab420a435dc535eff2715a6f70"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gdu"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4c73b80e029b5fb07e5c05652626890421d0afb24e2ecdd4ae5c67a47e3b9537"
   end
 
   depends_on "go" => :build
