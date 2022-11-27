1e3fa1f36c5975f650184f858c7218ab58a482ee
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:09:41 +0800

    git-filter-repo: no rebuilding required

--- Formula/git-filter-repo.rb
+++ Formula/git-filter-repo.rb
@@ -3,16 +3,15 @@ class GitFilterRepo < Formula
 
   desc "Quickly rewrite git repository history"
   homepage "https://github.com/newren/git-filter-repo"
-  url "https://github.com/newren/git-filter-repo/releases/download/v2.38.0/git-filter-repo-2.38.0.tar.xz"
-  sha256 "db954f4cae9e47c6be3bd3161bc80540d44f5379cb9cf9df498f4e019f0a41a9"
+  url "https://github.com/newren/git-filter-repo/releases/download/v2.34.0/git-filter-repo-2.34.0.tar.xz"
+  sha256 "b1bf46af1e6a91a54056d0254e480803db8e40f631336c559a1a94d2a08389c4"
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "a309651d195fca608c9268b7075f58e018e83fc285e4f43d85546adea28e1d83"
+    sha256 cellar: :any_skip_relocation, all: "4e48f4861bc73a26d9b6b8851d3f12a2b4afef1ba5c6e0278c7843f1696570a5"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.10"
   uses_from_macos "git", since: :catalina # git 2.22.0+ is required
 
   def install
