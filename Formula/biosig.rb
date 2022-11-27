8299147b1453dd92a8409c22b897d30a15d9f86b
Rui Chen <rui@chenrui.dev>
2022-11-27 08:42:25 +0000

    biosig: update 2.4.3 bottle.

--- Formula/biosig.rb
+++ Formula/biosig.rb
@@ -11,8 +11,10 @@ class Biosig < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "cd18b285cc6363dc0e8dc3ccfa6e61757cc8cf88db8858d29a9ffbced6039276"
     sha256 cellar: :any,                 arm64_monterey: "666be7197b4b957e663ace06f80d9735db7818769d77023fbede8373b23c0e25"
     sha256 cellar: :any,                 arm64_big_sur:  "9c7cc801e2e9bcbc69bda4d87ab4b6795f243f06369f8ff116008bb14525007f"
+    sha256 cellar: :any,                 ventura:        "c8a246347d0bae9ee90a400352e176bdfb774df471c77a79ebab2aac321f84b2"
     sha256 cellar: :any,                 monterey:       "b51b7647888687e69cb49e46eb3d154d14c2baa3ee4c6431a654baa39ab72c50"
     sha256 cellar: :any,                 big_sur:        "370e02d1a1509b432accedc09a1dd34dc39d011d3ad86182b0927e802165b8b1"
     sha256 cellar: :any,                 catalina:       "6c5a5b6d1a96cefb397a705a2e299f843566b249c8d1b6c7e6910f3bfaf7bbc4"
