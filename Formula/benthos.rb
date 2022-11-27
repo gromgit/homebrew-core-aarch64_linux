bb570336187c6a8feebf02f75d24e5ae4f8057d9
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:33:01 +0800

    benthos: no rebuilding required

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -1,20 +1,14 @@
 class Benthos < Formula
   desc "Stream processor for mundane tasks written in Go"
   homepage "https://www.benthos.dev"
-  url "https://github.com/benthosdev/benthos/archive/v4.10.0.tar.gz"
-  sha256 "3c184056ff2902fbf2e7220ea15c3f447aa9690c8e4942a835cafa5333b0e657"
+  url "https://github.com/benthosdev/benthos/archive/v4.6.0.tar.gz"
+  sha256 "ab552ea0344d97735dd9b1882cb5bc9575f9a246b3fb67f0822357ab55243ebe"
   license "MIT"
   head "https://github.com/benthosdev/benthos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ef20ba74f5b22009816b18e6f1044c96cea23efdf882f394749ca78083aa7841"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d235a6b3afc24b76b4339c1344fff3b7e4d2ca1c7c563678c0f3b43509627609"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7b0cd5c745549621a6cfff2cfb1ceda85ff08ea0898eb88fb635328ee7a2facc"
-    sha256 cellar: :any_skip_relocation, ventura:        "119bfc6e9d7e28cdd4d63fb208bfde0e34ae5a8e16f4c09485a36889c8138d15"
-    sha256 cellar: :any_skip_relocation, monterey:       "83a57fe7ba402a075ad99208c00bc22aa70817a3b8a0f310ab5aff3049041f87"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8c9495e4f6e2e2f40a907c0ea8c5011912672cd8dd66aa87a7d9836f0106ee8a"
-    sha256 cellar: :any_skip_relocation, catalina:       "9d7980808fac3923d6cd3dc77f7fd0ae5966481f390f579c08da4422b5e62cbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "99aa2271ae39842aa0348cfc0ae4490953fec9373695d3a5c96f33664e31d294"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/benthos"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "032993447a15f564537cb5e88b686eb4b2b2313958d0c830777cd0e42d30f7d5"
   end
 
   depends_on "go" => :build
