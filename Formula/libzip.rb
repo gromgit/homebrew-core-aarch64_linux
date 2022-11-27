42457d7e847caf97c01362d9dce7594b4e554a8c
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:42:48 +0800

    libzip: no rebuilding required

--- Formula/libzip.rb
+++ Formula/libzip.rb
@@ -11,14 +11,8 @@ class Libzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "6da42edf5cc6f80780ace956a3d01cd213633ff4a06588d53f25c455efdf26a5"
-    sha256 cellar: :any,                 arm64_monterey: "e27a1b53f6b09e0bb04071f66d878ef4594dbc3fde6e27a0b644d33a8ce34e0a"
-    sha256 cellar: :any,                 arm64_big_sur:  "bd41180937e22a75118330708505944e31c613e303abdef8b247c5655fa5f82d"
-    sha256 cellar: :any,                 ventura:        "d6c764255ed4b3350cace110c8a6e1f37fb512cf38c7dc1809b2ae66c0cc5a40"
-    sha256 cellar: :any,                 monterey:       "9ebb3c03505035e35eb8b7f00fe6d9e25cce7a0ddd191334e5f090b37c5cb7ca"
-    sha256 cellar: :any,                 big_sur:        "41b349a0653705d0d631e318d061faead49ecc26e6115d72f7f2b14273d0d924"
-    sha256 cellar: :any,                 catalina:       "b2168c4e742cca12fc6c5ea740c3b5a64aada6a93a7cd9105743886c6ecae456"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2e7f02d1e6de90dc42cea4ed2e198979bb00034455fadb4e1470f84124baf1c"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libzip"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4ee9e41804175d9c8072f5ab116f6a5c47338bb73b919d03184d72b663325536"
   end
 
   depends_on "cmake" => :build
