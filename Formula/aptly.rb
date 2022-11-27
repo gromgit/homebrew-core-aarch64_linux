26ce762fcfa68f6d8c09fb17b36823a4e88b85a3
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:27:13 +0800

    aptly: no rebuilding required

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -7,14 +7,8 @@ class Aptly < Formula
   head "https://github.com/aptly-dev/aptly.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c6fa91fb368a63d5558b8c287b330845e04f90bd4fe7223e161493b01747c869"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "19c0c8c0b35c1c5faa2a71fc0bd088725f5623f465369dcca5b2cea59322714c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2314abe4aae7ea53660920d311cacccd168045994e1a9eddf12a381b215c1908"
-    sha256 cellar: :any_skip_relocation, monterey:       "86111a102d0782a77bab0d48015bd275f120a36964d86f8f613f1a8f73d94664"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d622cfe1d925f0058f583b8bf48b0bdcee36a441f1bcf145040e5f93879f8765"
-    sha256 cellar: :any_skip_relocation, catalina:       "5d9d495ec8215cfade3e856528dfa233496849517813b19a9ba8d60cb72c4751"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bbff5503f74ef5dcaae33846e285ecf1a23c23de1c858760ae1789ef6fc99524"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/aptly"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "615b207f6eaf8431fb876bb0b7284f8686f9cd67efd08e0ebbcc9f9e35ce8b95"
   end
 
   depends_on "go" => :build
