6e3e32010fcca9765a1c27af6bbcba3856b5f3ee
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:24:31 +0800

    iproute2mac:

--- Formula/iproute2mac.rb
+++ Formula/iproute2mac.rb
@@ -3,17 +3,16 @@ class Iproute2mac < Formula
 
   desc "CLI wrapper for basic network utilities on macOS - ip command"
   homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/releases/download/v1.4.1/iproute2mac-1.4.1.tar.gz"
-  sha256 "f85558ea41a128ad5fcf30ae04ae272d4414b1cf6c8be06bb116ee41178cfaa1"
+  url "https://github.com/brona/iproute2mac/releases/download/v1.4.0/iproute2mac-1.4.0.tar.gz"
+  sha256 "23e9c3014687bce1cb3b17a7a22297ad582175025a9fe96fe894401f329da808"
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "eb6014521de7f35e1b16bb7465d9541355d981befff05dfe044291b234cf15a7"
+    sha256 cellar: :any_skip_relocation, all: "2ea180af8a15eace187d17e2f95a84e417445e5175646f6f61b9931633ddf264"
   end
 
   depends_on :macos
-  depends_on "python@3.11"
+  depends_on "python@3.10"
 
   def install
     bin.install "src/ip.py" => "ip"
