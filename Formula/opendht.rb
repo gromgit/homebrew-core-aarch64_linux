21695ad66b6c6d47dca0a99494ab743c664f3540
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:59:32 +0800

    opendht: no rebuilding required

--- Formula/opendht.rb
+++ Formula/opendht.rb
@@ -6,15 +6,8 @@ class Opendht < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "fef4bab7d51116fd0623ea3efd66e1c398b8d51e3e7586f5800e30334390a33e"
-    sha256 cellar: :any,                 arm64_monterey: "b89841268c4e43ec61bc3ccf04c6697cca4f7ea4c20ca2a41e1454693e4fcdd2"
-    sha256 cellar: :any,                 arm64_big_sur:  "1521af7886abc22fe7a00e4deca02180a88da2351bbbed5da02ed84ee8802d08"
-    sha256 cellar: :any,                 ventura:        "a6ffa2047d3697f100b9a34465bd9eff6671a7b72e68426b5debc306a036c42e"
-    sha256 cellar: :any,                 monterey:       "b801fa3a80c258a64c53a493f0980066fbdeeb4f8872ee71a44fe6c4c587da50"
-    sha256 cellar: :any,                 big_sur:        "d2591ae69288c30679d1abb952cd2a56432946878a6daeef430753a27bef83da"
-    sha256 cellar: :any,                 catalina:       "94df17098c2d52b3921e5ba236e22aa4f308528c441fde2c35dc1ad6baca5071"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05f5e03c9cf7528669308ec88846fb29814277ad073de616b1812696b2dede1e"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/opendht"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "544eb2b6d061889ded3ee3646193982cd7959ca0b7cca5197e0ddcac06ba1234"
   end
 
   depends_on "cmake" => :build
@@ -25,6 +18,10 @@ class Opendht < Formula
   depends_on "msgpack-cxx"
   depends_on "nettle"
   depends_on "readline"
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   fails_with gcc: "5"
 
