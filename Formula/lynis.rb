15063c23f07fb0a3eee733f38600c6048f3c98d5
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:45:26 +0800

    lynis: no rebuilding required

--- Formula/lynis.rb
+++ Formula/lynis.rb
@@ -6,13 +6,8 @@ class Lynis < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "590b7e427e36b3613b725b7d2e32ea45f9dcee911c46be70347be5c3ebc1e3ba"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "590b7e427e36b3613b725b7d2e32ea45f9dcee911c46be70347be5c3ebc1e3ba"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "590b7e427e36b3613b725b7d2e32ea45f9dcee911c46be70347be5c3ebc1e3ba"
-    sha256 cellar: :any_skip_relocation, monterey:       "f43da37593963aba57890c739e944505c5ccc6ef78b2cf19255c8c9748c69894"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f43da37593963aba57890c739e944505c5ccc6ef78b2cf19255c8c9748c69894"
-    sha256 cellar: :any_skip_relocation, catalina:       "f43da37593963aba57890c739e944505c5ccc6ef78b2cf19255c8c9748c69894"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "590b7e427e36b3613b725b7d2e32ea45f9dcee911c46be70347be5c3ebc1e3ba"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/lynis"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "30a03bf55865831fe53713a1f0682510d422f6c7b3bae78560fb454a3130004e"
   end
 
   def install
