46aa93e353603dddd4aff319e7b33a200d5cadaa
Adrian Ho <aho+rpi9@03s.net>
2022-11-27 23:29:13 +0800

    atomicparsley: no rebuilding required

--- Formula/atomicparsley.rb
+++ Formula/atomicparsley.rb
@@ -9,20 +9,17 @@ class Atomicparsley < Formula
   head "https://github.com/wez/atomicparsley.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e262f2feabbc9f16383b5d83e66c28578331e2cf7548046c4af8227dec1a8f93"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fd6740f15d9d909a464b2759676bc5c8f9e7e0d47206637b136b398e2f71e4ce"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "79353541a9980e951bdf0a3137105f14c3bb42bcb399e0a98ee5b814374cdbd5"
-    sha256 cellar: :any_skip_relocation, ventura:        "38836330fb6f518aad718835a9277e1fe2eefc5825d3a26b4e89d4cb53815242"
-    sha256 cellar: :any_skip_relocation, monterey:       "1813634f5288568fa89a502960bf9560f79f8d4c344f36d3c45b6cb322bf01bb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "85983e4b886164a9e7d865ad629cbef1583bd171e7d2190634dc9968e23117e1"
-    sha256 cellar: :any_skip_relocation, catalina:       "82b715beaadc88b8c342de0f71f1a81d24307cf2ec3a633c82db32f4ec11d2bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ffd502f75ccdee2e85002c006956f47dfb385ea5cb4709d79c1042baa4ee27b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/atomicparsley"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e619b04763cc062370e08dce74ce3b8947a80606c6a88e3bb1877d99b764a3cc"
   end
 
   depends_on "cmake" => :build
 
   uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "gcc" => :build
+  end
 
   fails_with gcc: "5"
 
