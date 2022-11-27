20237dbaa8ae36bbd23f3be1483a2deaa955769a
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 01:25:18 +0800

    sshfs:

--- Formula/sshfs.rb
+++ Formula/sshfs.rb
@@ -5,16 +5,19 @@ class Sshfs < Formula
   sha256 "52a1a1e017859dfe72a550e6fef8ad4f8703ce312ae165f74b579fd7344e3a26"
   license any_of: ["LGPL-2.1-only", "GPL-2.0-only"]
 
-  bottle do
-    sha256 x86_64_linux: "a98d273e64706971684935a3ae87da16b1dda98f7289eb79e82f4cdfb7f12bb8"
-  end
-
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "libfuse"
-  depends_on :linux # on macOS, requires closed-source macFUSE
+
+  on_macos do
+    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
+  end
+
+  on_linux do
+    deprecate! date: "2022-05-27", because: :repo_archived
+    depends_on "libfuse"
+  end
 
   def install
     mkdir "build" do
@@ -22,6 +25,18 @@ class Sshfs < Formula
       system "meson", "configure", "--prefix", prefix
       system "ninja", "--verbose"
       system "ninja", "install", "--verbose"
+    end
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
     end
   end
 
