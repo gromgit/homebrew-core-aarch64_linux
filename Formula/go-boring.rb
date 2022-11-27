48dccc29a86e5ebc012996402b084bc96feb5a15
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:13:25 +0800

    go-boring: no rebuilding required

--- Formula/go-boring.rb
+++ Formula/go-boring.rb
@@ -1,9 +1,9 @@
 class GoBoring < Formula
   desc "Go programming language with BoringCrypto"
   homepage "https://go.googlesource.com/go/+/dev.boringcrypto/README.boringcrypto.md"
-  url "https://go-boringcrypto.storage.googleapis.com/go1.18.8b7.src.tar.gz"
-  version "1.18.8b7"
-  sha256 "c3028846650b42cf77c1c0d540791eaa1283c8e60e2a87e28ae43658ffdc262a"
+  url "https://go-boringcrypto.storage.googleapis.com/go1.18.5b7.src.tar.gz"
+  version "1.18.5b7"
+  sha256 "75f5021bd9b61f837fdd516cc4ec51556c519c7155a2e99ecfb4efeca8655aa7"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,13 +12,8 @@ class GoBoring < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "c9af52582c8709a3d3877d66dcc58cd6ba19011271129aace57d9d3016b38a7f"
-    sha256 arm64_monterey: "71631625a7bbe31a795277e3ddba94da2a951d8c2a0b9e6ab8fa8481220fa6d9"
-    sha256 arm64_big_sur:  "75cf8f48d21988a086e70987b75ad90fe40943cb39eead2498747a3c3470b9a0"
-    sha256 monterey:       "8b2640416a29867198a610b6bd1aa96736752e1332ec276aa65e6f2ce806bda0"
-    sha256 big_sur:        "38b491bedc6e5bf9d5c2a15c64c9a81f437ac61289ac6dddd235b45047d402af"
-    sha256 catalina:       "19a5fe46c60092b1602a921984b66d7973576b343239a20395ad98c9a2121e0d"
-    sha256 x86_64_linux:   "d6c91d6f5df9f6bc9db0f738734c75bd401a42a96b9be1f75e226a1b7d27f951"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/go-boring"
+    sha256 aarch64_linux: "5e92c8cbb1c56e01efe769d06c05c7b50aa4915da7ba8d5abb47f7140fdc7531"
   end
 
   keg_only "it conflicts with the Go formula"
