c1c948fee65fc9fe1f9d6d299974ad1862b23c41
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:56:09 +0800

    node_exporter: no rebuilding required

--- Formula/node_exporter.rb
+++ Formula/node_exporter.rb
@@ -1,8 +1,8 @@
 class NodeExporter < Formula
   desc "Prometheus exporter for machine metrics"
   homepage "https://prometheus.io/"
-  url "https://github.com/prometheus/node_exporter/archive/v1.4.0.tar.gz"
-  sha256 "96f749928e3d6c952221aaca852d4c38545eaae03adc6bb925745bc3f2f827ca"
+  url "https://github.com/prometheus/node_exporter/archive/v1.3.1.tar.gz"
+  sha256 "66856b6b8953e094c46d7dd5aabd32801375cf4d13d9fe388e320cbaeaff573a"
   license "Apache-2.0"
   head "https://github.com/prometheus/node_exporter.git", branch: "master"
 
@@ -12,25 +12,20 @@ class NodeExporter < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "525a708f82964d00ecd477759323164f025b8b312ae5efda7fcf9add78a3feca"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "32b46a6ed45b8ee93d3cdf0f4ca9e516beb6747b54e54f5232789961760a3d9b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "217f1bb2d38c3e052eeb6499da88a64eae85b30b06da5ed57218701c06d4bb63"
-    sha256 cellar: :any_skip_relocation, ventura:        "7b0316bc6f749e530325b93fdf49eba820e0be7a3fd9b0f7b8da61a15acce0e9"
-    sha256 cellar: :any_skip_relocation, monterey:       "c09790655a6c6a7be2e4854d6568d7a10e78f40737c5be3a3e3a638185f98231"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a66d6ee73ea9a507d9a9b53cfc00e704aea5ab25afbdc54a122bf427ce6205ee"
-    sha256 cellar: :any_skip_relocation, catalina:       "144b9ac75c49b768320604dd8ebdb0434b69f3dd3d449b905090ea03fbb4e2c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9bf38fa04f9aa42cf1bdc7010cf41a64553f94d4b228356a94f54b5958a8b2e8"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/node_exporter"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "278cf4ab25f62f4ee0cc29e905bfd4172c3aa636b2290be201e0c33943ca46e9"
   end
 
   depends_on "go" => :build
 
   def install
     ldflags = %W[
-      -s -w
       -X github.com/prometheus/common/version.Version=#{version}
       -X github.com/prometheus/common/version.BuildUser=Homebrew
     ]
-    system "go", "build", *std_go_args(ldflags: ldflags)
+    system "go", "build", "-ldflags", ldflags.join(" "), "-trimpath",
+           "-o", bin/"node_exporter"
+    prefix.install_metafiles
 
     touch etc/"node_exporter.args"
 
