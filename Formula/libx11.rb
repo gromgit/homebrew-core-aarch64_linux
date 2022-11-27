a5143fbb1e19d44644e888d0c09cd01abe1ddb01
Adrian Ho <aho+rpi9@03s.net>
2022-11-28 00:41:12 +0800

    libx11: no rebuilding required

--- Formula/libx11.rb
+++ Formula/libx11.rb
@@ -1,19 +1,13 @@
 class Libx11 < Formula
   desc "X.Org: Core X11 protocol client library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libX11-1.8.2.tar.gz"
-  sha256 "f1bc56187bee0f830e1179ac5068ac93b78c51ace94eb27702ffb2efd116587b"
+  url "https://www.x.org/archive/individual/lib/libX11-1.8.1.tar.gz"
+  sha256 "d52f0a7c02a45449f37b0831d99ff936d92eb4ce8b4c97dc17a63cea79ce5a76"
   license "MIT"
 
   bottle do
-    sha256 arm64_ventura:  "996724436ae47440832ffe2caa9bec0d0f374f8885f83925453895fcfc80d8c2"
-    sha256 arm64_monterey: "8d08dd29849612785db12fd469c707b136328bdfd4115d79b6afdfc1b13c9b38"
-    sha256 arm64_big_sur:  "4448aa22e8118de5775caf8488b666a211b01f50085a418fbbbcbfed2d83e517"
-    sha256 ventura:        "7d62f3b406f3ac7fad944ce6f2faaa5a496139a69f657ab467d118c4df9183db"
-    sha256 monterey:       "cbaa4ea21135fa88c816263ea1d945e9b077be7c0fbe8ccca37ff924d9abb205"
-    sha256 big_sur:        "c408a64f5e19800a746125e1ca3bec69440cf053cfd2d5e041469fbf66d16b4f"
-    sha256 catalina:       "83b5c84a2f595ddb273b9eb9790109e542da3c21832df5cc6c90a1c328050389"
-    sha256 x86_64_linux:   "d90e3f7a7ad612243113e10a31b0f71880f742d9af9fb656219a53270bfdbf67"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libx11"
+    sha256 aarch64_linux: "577e360740de1c9a66cba510dd77a0d1d2016c0cd1d5af50574a0ebc62a8b89c"
   end
 
   depends_on "pkg-config" => :build
