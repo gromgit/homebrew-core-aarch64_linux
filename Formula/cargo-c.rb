52b86c488bc26b33250ac5bb9192274deb6b76a4
Adrian Ho <the.gromgit@gmail.com>
2022-12-02 21:57:50 +0800

    cargo-c: add 0.9.12 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,8 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.9.9.tar.gz"
-  sha256 "d35f91e3e57bb0b5459789c02d1fa8a875bc55ca603a85ed63138d6d8193b53f"
+  url "https://github.com/lu-zero/cargo-c/archive/v0.9.12.tar.gz"
+  sha256 "d700c6cc93d06c5ed7a887f51d87d47e88e783dfc1b28d00b951625c4d3ce509"
   license "MIT"
 
   livecheck do
@@ -11,12 +11,8 @@ class CargoC < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "66aeb043716db062fa8e706153b92b23ba0e364c3f38da6a8cc13ef217031f65"
-    sha256 cellar: :any,                 arm64_big_sur:  "97872ff704fc91d5a1d27133bb41bf253b38edad9d39089ff56421933cd3cea9"
-    sha256 cellar: :any,                 monterey:       "739bb8b5ef685fed9734e96579f9389028765e772da26f267a95e037a23d1c06"
-    sha256 cellar: :any,                 big_sur:        "484b8e813a0af81d26b660a90393de3503b3c5fe016ae93389b6759271d947cd"
-    sha256 cellar: :any,                 catalina:       "44658a0bbb06e7497c993dadc986ecd2907b041310e217577f920a944c39b208"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88078fe5d3739f20760329ff00244d75455426fa4b125bcc1abdf7c5f53d0126"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/cargo-c"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "4a4949e0689b55dcd234e024c578eac110d6ef0d974afe6444ce086740c2ecc9"
   end
 
   depends_on "rust" => :build
