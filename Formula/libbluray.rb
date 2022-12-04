730dbdfa676faf31aa7be41d2b8b7a40a0dfdd53
Adrian Ho <the.gromgit@gmail.com>
2022-12-05 00:23:59 +0800

    libbluray: add 1.3.2 bottle.

--- Formula/libbluray.rb
+++ Formula/libbluray.rb
@@ -1,8 +1,8 @@
 class Libbluray < Formula
   desc "Blu-Ray disc playback library for media players like VLC"
   homepage "https://www.videolan.org/developers/libbluray.html"
-  url "https://download.videolan.org/videolan/libbluray/1.3.1/libbluray-1.3.1.tar.bz2"
-  sha256 "c24b0f41c5b737bbb65c544fe63495637a771c10a519dfc802e769f112b43b75"
+  url "https://download.videolan.org/videolan/libbluray/1.3.2/libbluray-1.3.2.tar.bz2"
+  sha256 "456814db9f07c1eecdef7e840fcbb20976ef814df875428bfb81ecf45851f170"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,12 +11,8 @@ class Libbluray < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e680c8af00d95066d5ef7894bed7ce94061242ebf6e460184352828b0b312892"
-    sha256 cellar: :any,                 arm64_big_sur:  "d9a4488093c31eadb48532b98fc81681fc8fe6682a75a4d75c8b0d609300b92a"
-    sha256 cellar: :any,                 monterey:       "8f2c34dd07b155183309014a966a9218cb386af5f028681fb2ad5a06319a82f9"
-    sha256 cellar: :any,                 big_sur:        "263d41816556be074b45f72fc3362c953c91c0b98aa61872c83ef1459d8bb611"
-    sha256 cellar: :any,                 catalina:       "e9ca85381212db088482193f9fc598284af4a3ac4d300a7b102f6fd7c202bab4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "950457b5ef1e53c1bb2e59c32e5baf99d9288d56658f39eb7a21f01c71f40960"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libbluray"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "64b71ba139ff61bf6fd4537e40621337a3ac87220fd98508adeb88c15687e55f"
   end
 
   head do
@@ -47,6 +43,8 @@ class Libbluray < Formula
       #include <libbluray/bluray.h>
       int main(void) {
         BLURAY *bluray = bd_init();
+        bd_close(bluray);
+        return 0;
       }
     EOS
 
