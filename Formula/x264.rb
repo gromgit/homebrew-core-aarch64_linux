8192a9809f4b9ede5527441c936510a9c7ac4097
Adrian Ho <the.gromgit@gmail.com>
2022-12-07 14:44:04 +0800

    x264: add r3095 bottle.

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -1,15 +1,12 @@
 class X264 < Formula
   desc "H.264/AVC encoder"
   homepage "https://www.videolan.org/developers/x264.html"
+  # the latest commit on the stable branch
+  url "https://code.videolan.org/videolan/x264.git",
+      revision: "baee400fa9ced6f5481a728138fed6e867b0ff7f"
+  version "r3095"
   license "GPL-2.0-only"
   head "https://code.videolan.org/videolan/x264.git", branch: "master"
-
-  stable do
-    # the latest commit on the stable branch
-    url "https://code.videolan.org/videolan/x264.git",
-        revision: "5db6aa6cab1b146e07b60cc1736a01f21da01154"
-    version "r3060"
-  end
 
   # Cross-check the abbreviated commit hashes from the release filenames with
   # the latest commits in the `stable` Git branch:
@@ -23,47 +20,39 @@ class X264 < Formula
 
       # Fetch the `stable` Git branch Atom feed
       stable_page_data = Homebrew::Livecheck::Strategy.page_content("https://code.videolan.org/videolan/x264/-/commits/stable?format=atom")
-      next [] if stable_page_data[:content].blank?
+      next if stable_page_data[:content].blank?
 
       # Extract commit hashes from the feed content
       commit_hashes = stable_page_data[:content].scan(%r{/commit/([\da-z]+)}i).flatten
-      next [] if commit_hashes.blank?
+      next if commit_hashes.blank?
 
       # Only keep versions with a matching commit hash in the `stable` branch
       matches.map do |match|
-        next nil unless match.length >= 2
-
         release_hash = match[1]
-        commit_in_stable = false
-        commit_hashes.each do |commit_hash|
-          next unless commit_hash.start_with?(release_hash)
-
-          commit_in_stable = true
-          break
+        commit_in_stable = commit_hashes.any? do |commit_hash|
+          commit_hash.start_with?(release_hash)
         end
 
-        commit_in_stable ? match[0] : nil
-      end.compact
+        match[0] if commit_in_stable
+      end
     end
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "9734de08ad24836dc4c222293d6413b66d0e2f7b5dd93fc190e3404f316f3804"
-    sha256 cellar: :any,                 arm64_big_sur:  "2f5442c86dc08c7c283a4de626e8a7e8ceb17621b2ce63c7674f8c31a47eaf2c"
-    sha256 cellar: :any,                 monterey:       "40eca7e7d71bb3541b5dceb89bd5b40b0bd763e30ab0f36e6d7a405309eda270"
-    sha256 cellar: :any,                 big_sur:        "dae0e0e0715c44a916170879eb71c942b673d9450c7c0db319ce7e56757e567f"
-    sha256 cellar: :any,                 catalina:       "2ffa8448569c0272db62789e0c1475330356956f570fea278a3fa0ca7739ab3b"
-    sha256 cellar: :any,                 mojave:         "6780344c98d8c614d8fb8039365747cc699cb6818f7c3d56b708d9e2c5afe388"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ed86de1b2e5af0136202b8479dde1935c46bbdd9da4c11afa52f1f18820e9a1"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/x264"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9d2cd013c221edd5962814b97ed3dfcff40789ba153e6f831b05e32c396e0bde"
   end
 
   depends_on "nasm" => :build
 
-  if MacOS.version <= :high_sierra
-    # Stack realignment requires newer Clang
-    # https://code.videolan.org/videolan/x264/-/commit/b5bc5d69c580429ff716bafcd43655e855c31b02
-    depends_on "gcc"
-    fails_with :clang
+  on_macos do
+    depends_on "gcc" if DevelopmentTools.clang_build_version <= 902
+  end
+
+  # https://code.videolan.org/videolan/x264/-/commit/b5bc5d69c580429ff716bafcd43655e855c31b02
+  fails_with :clang do
+    build 902
+    cause "Stack realignment requires newer Clang"
   end
 
   def install
