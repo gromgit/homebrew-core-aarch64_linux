3d76ba0784f7eccf643f3e61f47830e1d25bcf13
Adrian Ho <the.gromgit@gmail.com>
2022-12-11 23:49:37 +0800

    camlp-streams: add 5.0.1 bottle.

---
+++ Formula/camlp-streams.rb
@@ -0,0 +1,30 @@
+class CamlpStreams < Formula
+  desc "Stream and Genlex libraries for use with Camlp4 and Camlp5"
+  homepage "https://github.com/ocaml/camlp-streams"
+  url "https://github.com/ocaml/camlp-streams/archive/refs/tags/v5.0.1.tar.gz"
+  sha256 "ad71f62406e9bb4e7fb5d4593ede2af6c68f8b0d96f25574446e142c3eb0d9a4"
+  license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/camlp-streams"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "c6d36ba18fbc5cc1075e0f3d5a927c35bbc3636bc31239f44ae446385fd6b396"
+  end
+
+  depends_on "dune" => :build
+  depends_on "ocaml-findlib" => :test
+  depends_on "ocaml"
+
+  def install
+    system "dune", "build", "@install"
+    system "dune", "install", "--prefix=#{prefix}", "--libdir=#{lib}/ocaml", "--docdir=#{doc.parent}"
+  end
+
+  test do
+    (testpath/"test.ml").write <<~EOS
+      let stream = Stream.of_list ([] : unit list)
+    EOS
+    system "ocamlfind", "ocamlopt", "-linkpkg", "-package", "camlp-streams",
+                                    "-warn-error", "+3", "-o", "test", "test.ml"
+    assert_predicate testpath/"test", :exist?
+  end
+end
