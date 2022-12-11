79be3be48878d4bee0554be847b28e29747dd63b
Adrian Ho <the.gromgit@gmail.com>
2022-12-11 23:46:34 +0800

    dune: add 3.4.1 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -1,14 +1,14 @@
 class Dune < Formula
   desc "Composable build system for OCaml"
   homepage "https://dune.build/"
-  url "https://github.com/ocaml/dune/releases/download/3.1.1/fiber-3.1.1.tbz"
-  sha256 "02484454ab1b998840c7873509ec6b2301eb92662c132ef8f5f4f569b35a6b60"
+  url "https://github.com/ocaml/dune/releases/download/3.4.1/dune-3.4.1.tbz"
+  sha256 "299fa33cffc108cc26ff59d5fc9d09f6cb0ab3ac280bf23a0114cfdc0b40c6c5"
   license "MIT"
   head "https://github.com/ocaml/dune.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/dune"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "e929273b4799a0c1ba3bbe6a0a281e7f7f488f428127add1b07dd1ae3dd82b60"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f0a0a957e332ffee4a2e952ad1a2d165572572e4c8f5c397fc5538abc8280d3f"
   end
 
   depends_on "ocaml" => [:build, :test]
