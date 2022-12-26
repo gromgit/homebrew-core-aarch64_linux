cf74ecd591824c86e177fb21c42dde39b0db85f2
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 11:22:11 +0800

    edencommon: add 2022.11.14.00 bottle.

---
+++ Formula/edencommon.rb
@@ -0,0 +1,57 @@
+class Edencommon < Formula
+  desc "Shared library for Watchman and Eden projects"
+  homepage "https://github.com/facebookexperimental/edencommon"
+  url "https://github.com/facebookexperimental/edencommon/archive/refs/tags/v2022.11.14.00.tar.gz"
+  sha256 "6f9321bf91703c0ca2b3627bd51a5a6b79bcd32f5aab487fe23f933864d0149f"
+  license "MIT"
+  head "https://github.com/facebookexperimental/edencommon.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/edencommon"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "cf10cd96b5b461bbfa3a17b2920986b814786903ad34750b577b2608dc2da150"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "googletest" => :build
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+
+  def install
+    system "cmake", "-S", ".", "-B", "_build", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <eden/common/utils/ProcessNameCache.h>
+      #include <cstdlib>
+      #include <iostream>
+
+      using namespace facebook::eden;
+
+      ProcessNameCache& getProcessNameCache() {
+        static auto* pnc = new ProcessNameCache;
+        return *pnc;
+      }
+
+      ProcessNameHandle lookupProcessName(pid_t pid) {
+        return getProcessNameCache().lookup(pid);
+      }
+
+      int main(int argc, char **argv) {
+        if (argc <= 1) return 1;
+        int pid = std::atoi(argv[1]);
+        std::cout << lookupProcessName(pid).get() << std::endl;
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++17", "-I#{include}", "test.cc",
+                    "-L#{lib}", "-L#{Formula["folly"].opt_lib}",
+                    "-L#{Formula["boost"].opt_lib}", "-L#{Formula["glog"].opt_lib}",
+                    "-ledencommon_utils", "-lfolly", "-lboost_context-mt", "-lglog", "-o", "test"
+    assert_match "ruby", shell_output("./test #{Process.pid}")
+  end
+end
