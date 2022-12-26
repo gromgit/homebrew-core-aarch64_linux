76dc96c20714d5f34c7a840c39a5be522792395e
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 22:08:37 +0800

    pymupdf: add 1.20.2 bottle.

---
+++ Formula/pymupdf.rb
@@ -0,0 +1,64 @@
+class Pymupdf < Formula
+  desc "Python bindings for the PDF toolkit and renderer MuPDF"
+  homepage "https://github.com/pymupdf/PyMuPDF"
+  url "https://files.pythonhosted.org/packages/4a/09/6afe87a8ea7acb6e4709223a704270ffe9929497add4d06b12305e229ba8/PyMuPDF-1.20.2.tar.gz"
+  sha256 "02eedf01f57c6bafb5e8667cea0088a2d2522643c47100f1908bec3a68a84888"
+  license "AGPL-3.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pymupdf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "ba3611b6a9b0232269d9996aef25cbcd75ba836610278c3c220010eecfc80ae7"
+  end
+
+  depends_on "freetype" => :build
+  depends_on "swig" => :build
+
+  depends_on "mupdf"
+  depends_on "python@3.10"
+
+  on_linux do
+    depends_on "gumbo-parser"
+    depends_on "harfbuzz"
+    depends_on "jbig2dec"
+    depends_on "mujs"
+    depends_on "openjpeg"
+  end
+
+  def install
+    if OS.linux?
+      ENV.append_path "CPATH", Formula["mupdf"].include/"mupdf"
+      ENV.append_path "CPATH", Formula["freetype2"].include/"freetype2"
+    end
+
+    # Makes setup skip build stage for mupdf
+    # https://github.com/pymupdf/PyMuPDF/blob/1.20.0/setup.py#L447
+    ENV["PYMUPDF_SETUP_MUPDF_BUILD"] = ""
+
+    system "python3", *Language::Python.setup_install_args(prefix), "build"
+  end
+
+  test do
+    (testpath/"test.py").write <<~EOS
+      import sys
+      from pathlib import Path
+
+      import fitz
+
+      in_pdf = sys.argv[1]
+      out_png = sys.argv[2]
+
+      # Convert first page to PNG
+      pdf_doc = fitz.open(in_pdf)
+      pdf_page = pdf_doc.load_page(0)
+      png_bytes = pdf_page.get_pixmap().tobytes()
+
+      Path(out_png).write_bytes(png_bytes)
+    EOS
+
+    in_pdf = test_fixtures("test.pdf")
+    out_png = testpath/"test.png"
+
+    system Formula["python@3.10"].opt_bin/"python3", testpath/"test.py", in_pdf, out_png
+    assert_predicate out_png, :exist?
+  end
+end
