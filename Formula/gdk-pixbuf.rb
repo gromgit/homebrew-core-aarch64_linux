3c2d514b019167416ed88b46c34f6be310951c6a
Adrian Ho <the.gromgit@gmail.com>
2022-12-28 00:08:34 +0800

    gdk-pixbuf: add 2.42.9 bottle.

--- Formula/gdk-pixbuf.rb
+++ Formula/gdk-pixbuf.rb
@@ -1,17 +1,13 @@
 class GdkPixbuf < Formula
   desc "Toolkit for image loading and pixel buffer manipulation"
   homepage "https://gtk.org"
-  url "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-2.42.8.tar.xz"
-  sha256 "84acea3acb2411b29134b32015a5b1aaa62844b19c4b1ef8b8971c6b0759f4c6"
+  url "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-2.42.9.tar.xz"
+  sha256 "28f7958e7bf29a32d4e963556d241d0a41a6786582ff6a5ad11665e0347fc962"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_monterey: "a64b406a58be74567a9409ad763937b14a2619754734e5ee91c840965d1bd354"
-    sha256 arm64_big_sur:  "23e63e68d3a4e1b7d62a8380184d2070f054e4377331a2861bda7b9447bb3466"
-    sha256 monterey:       "da834da5d08e57860283a288993b954d978fd8b6bc5f441dfb9b9f42d28ff082"
-    sha256 big_sur:        "b2ad72a9e6f1b793e39cd9a079838342fe2e1ceae21a793a757a3e828e2c9b7e"
-    sha256 catalina:       "2767c9d0973ac43175331aa5f6b3d543cbde679343f813052bb4c5155a14b4eb"
-    sha256 x86_64_linux:   "46f21d535b3568bc3c3ec11ce063a220e7b01d7b9df164a497b5dc939bbf15e7"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gdk-pixbuf"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2f370ac9d38bc0a218acffd115781087811011066b9d67f576d9fab83b8a2269"
   end
 
   depends_on "gobject-introspection" => :build
@@ -19,7 +15,7 @@ class GdkPixbuf < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
 
@@ -44,24 +40,19 @@ class GdkPixbuf < Formula
               "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format(gdk_pixbuf_libdir)",
               "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format('#{HOMEBREW_PREFIX}/lib')"
 
-    args = std_meson_args + %w[
-      -Drelocatable=false
-      -Dnative_windows_loaders=false
-      -Dinstalled_tests=false
-      -Dman=false
-      -Dgtk_doc=false
-      -Dpng=enabled
-      -Dtiff=enabled
-      -Djpeg=enabled
-      -Dintrospection=enabled
-    ]
-
     ENV["DESTDIR"] = "/"
-    mkdir "build" do
-      system "meson", *args, ".."
-      system "ninja", "-v"
-      system "ninja", "install"
-    end
+    system "meson", *std_meson_args, "build",
+                    "-Drelocatable=false",
+                    "-Dnative_windows_loaders=false",
+                    "-Dinstalled_tests=false",
+                    "-Dman=false",
+                    "-Dgtk_doc=false",
+                    "-Dpng=enabled",
+                    "-Dtiff=enabled",
+                    "-Djpeg=enabled",
+                    "-Dintrospection=enabled"
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
 
     # Other packages should use the top-level modules directory
     # rather than dumping their files into the gdk-pixbuf keg.
