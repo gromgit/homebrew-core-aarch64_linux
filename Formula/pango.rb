d0b545bc41bfb9d1f8037f7232a7cee1f678d39d
Adrian Ho <the.gromgit@gmail.com>
2022-12-28 00:23:53 +0800

    pango: add 1.50.9 bottle.

--- Formula/pango.rb
+++ Formula/pango.rb
@@ -1,18 +1,14 @@
 class Pango < Formula
   desc "Framework for layout and rendering of i18n text"
   homepage "https://pango.gnome.org"
-  url "https://download.gnome.org/sources/pango/1.50/pango-1.50.6.tar.xz"
-  sha256 "a998bcf36881c3ac20495d40bceb304f4eaa9175bd2967c85656434cbdafe86a"
+  url "https://download.gnome.org/sources/pango/1.50/pango-1.50.9.tar.xz"
+  sha256 "1b636aabf905130d806372136f5e137b6a27f26d47defd9240bf444f6a4fe610"
   license "LGPL-2.0-or-later"
   head "https://gitlab.gnome.org/GNOME/pango.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "5f2fcb84957b49bc2764be3f6ce3b5309967ef93e8d39fbbc04d7fa20c65d142"
-    sha256 cellar: :any, arm64_big_sur:  "61196356dd6f636d28957e2b7c48674254693c1e175d7ccc7cb2207a55c3c7f5"
-    sha256 cellar: :any, monterey:       "f1452a4c0b3ccff2dd9ed4d0d5153c202a98e40602cbaa6faa59648b2c4f1318"
-    sha256 cellar: :any, big_sur:        "c4ed05228db80d461dac1d44479c4d71a7161307ef1e041e28d47363b9c9c891"
-    sha256 cellar: :any, catalina:       "d2a309a053431e56086a8ffa4f3d9e674f22b341f3c7ee349423cfb3f037f01a"
-    sha256               x86_64_linux:   "47c20a93f7f50984817766e16626ae24817838c3c3e478576223bcde099cb11b"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/pango"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a178facc869c51f9854a68e3176fc0489b4ad896277b71f76648b2c77b89360f"
   end
 
   depends_on "gobject-introspection" => :build
@@ -27,21 +23,18 @@ class Pango < Formula
   depends_on "harfbuzz"
 
   def install
-    mkdir "build" do
-      system "meson", *std_meson_args,
-                      "-Ddefault_library=both",
-                      "-Dintrospection=enabled",
-                      "-Dfontconfig=enabled",
-                      "-Dcairo=enabled",
-                      "-Dfreetype=enabled",
-                      ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-    end
+    system "meson", *std_meson_args, "build",
+                    "-Ddefault_library=both",
+                    "-Dintrospection=enabled",
+                    "-Dfontconfig=enabled",
+                    "-Dcairo=enabled",
+                    "-Dfreetype=enabled"
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
   end
 
   test do
-    system "#{bin}/pango-view", "--version"
+    system bin/"pango-view", "--version"
     (testpath/"test.c").write <<~EOS
       #include <pango/pangocairo.h>
 
