3a0d0d6d44c6a5f3b8f14dcb15d2505a2690e822
Adrian Ho <the.gromgit@gmail.com>
2022-12-28 00:46:34 +0800

    libxkbcommon: add 1.4.1 bottle.

--- Formula/libxkbcommon.rb
+++ Formula/libxkbcommon.rb
@@ -1,8 +1,8 @@
 class Libxkbcommon < Formula
   desc "Keyboard handling library"
   homepage "https://xkbcommon.org/"
-  url "https://xkbcommon.org/download/libxkbcommon-1.4.0.tar.xz"
-  sha256 "106cec5263f9100a7e79b5f7220f889bc78e7d7ffc55d2b6fdb1efefb8024031"
+  url "https://xkbcommon.org/download/libxkbcommon-1.4.1.tar.xz"
+  sha256 "943c07a1e2198026d8102b17270a1f406e4d3d6bbc4ae105b9e1b82d7d136b39"
   license "MIT"
   head "https://github.com/xkbcommon/libxkbcommon.git", branch: "master"
 
@@ -12,12 +12,8 @@ class Libxkbcommon < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "a75c6677a5c45eabcd73867086dff4c704900ac9cf6ce8e26dd1be5a6ff1157a"
-    sha256 arm64_big_sur:  "4de98d422442bc15aaf7b0e78f2c4312c1ae73a12eb48e87c051b0cffc5db57c"
-    sha256 monterey:       "c6b33b38933fc09c5b8759976f24ce1c556cd56a62e8e1504173016e1ff665f5"
-    sha256 big_sur:        "8c90166b1335ba095d76506b61a9cdfb97e67b9a603da2654fcca8328a2429a6"
-    sha256 catalina:       "ac9c4b144d43a06ea024febda54a957490b0d985156adeee86fc4358288ade84"
-    sha256 x86_64_linux:   "a691f0e653cae432d021079871bbe82589b9614248973b81191e1c5ab93845ba"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxkbcommon"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8ab3cb3b67bfa96e4a2a7fd4837158f25f9c758f9980dee217a833449b298b79"
   end
 
   depends_on "bison" => :build
