0a9b0f91179db692b94c095b3b5b17f66ecb613e
Adrian Ho <the.gromgit@gmail.com>
2022-12-30 00:51:00 +0800

    log4cpp: add 1.1.3 bottle.

--- Formula/log4cpp.rb
+++ Formula/log4cpp.rb
@@ -1,13 +1,8 @@
 class Log4cpp < Formula
   desc "Configurable logging for C++"
   homepage "https://log4cpp.sourceforge.io/"
-#  if Hardware::CPU.arm? && OS.linux?
-#    url "https://git.code.sf.net/p/log4cpp/codegit", using: :git, revision: "2e117d81e94ec4f9c5af42fcf76a0583a036e106"
-#    version "1.1.3"
-#  else
-    url "https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1.3.tar.gz"
-    sha256 "2cbbea55a5d6895c9f0116a9a9ce3afb86df383cd05c9d6c1a4238e5e5c8f51d"
-#  end
+  url "https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1.3.tar.gz"
+  sha256 "2cbbea55a5d6895c9f0116a9a9ce3afb86df383cd05c9d6c1a4238e5e5c8f51d"
   license "LGPL-2.1"
 
   livecheck do
@@ -17,7 +12,7 @@ class Log4cpp < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/log4cpp"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "ecea8d0aefaec00c2b8345c5b0708a25ec94f9c6cc4b9723f0b6b4882b5ad44e"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2c4ef0f1ec6baadc9368a9e79386a27d21b65956466ea8a7fb6c1529338aa59e"
   end
 
   depends_on "autoconf" => :build
@@ -25,21 +20,13 @@ class Log4cpp < Formula
   depends_on "libtool" => :build
 
   # Fix -flat_namespace being used on Big Sur and later.
-#  on_macos do
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
-      sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
-    end
-#  end
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
 
   def install
-#    if Hardware::CPU.arm? && OS.linux?
-#      system "autoupdate", "-fv"
-#      system "./autogen.sh"
-#    end
-    Pathname.glob("#{Formula['autoconf'].opt_prefix}/share/autoconf/build-aux/config.*") do |cfg|
-      (buildpath/"config").install cfg
-    end
+    cp Dir["#{Formula["libtool"].opt_pkgshare}/*/config.{guess,sub}"], buildpath/"config"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
