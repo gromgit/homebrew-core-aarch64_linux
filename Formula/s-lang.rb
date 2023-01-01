cb45ef1debb83e582fe898afa7cf522de70fe723
Adrian Ho <the.gromgit@gmail.com>
2023-01-01 15:53:23 +0800

    s-lang: update 2.3.3 bottle.

--- Formula/s-lang.rb
+++ Formula/s-lang.rb
@@ -1,10 +1,10 @@
 class SLang < Formula
   desc "Library for creating multi-platform software"
   homepage "https://www.jedsoft.org/slang/"
-  url "https://www.jedsoft.org/releases/slang/slang-2.3.2.tar.bz2"
-  mirror "https://src.fedoraproject.org/repo/pkgs/slang/slang-2.3.2.tar.bz2/sha512/35cdfe8af66dac62ee89cca60fa87ddbd02cae63b30d5c0e3786e77b1893c45697ace4ac7e82d9832b8a9ac342560bc35997674846c5022341481013e76f74b5/slang-2.3.2.tar.bz2"
-  sha256 "fc9e3b0fc4f67c3c1f6d43c90c16a5c42d117b8e28457c5b46831b8b5d3ae31a"
-  license "GPL-2.0"
+  url "https://www.jedsoft.org/releases/slang/slang-2.3.3.tar.bz2"
+  mirror "https://src.fedoraproject.org/repo/pkgs/slang/slang-2.3.3.tar.bz2/sha512/35cdfe8af66dac62ee89cca60fa87ddbd02cae63b30d5c0e3786e77b1893c45697ace4ac7e82d9832b8a9ac342560bc35997674846c5022341481013e76f74b5/slang-2.3.3.tar.bz2"
+  sha256 "f9145054ae131973c61208ea82486d5dd10e3c5cdad23b7c4a0617743c8f5a18"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://www.jedsoft.org/releases/slang/"
@@ -13,7 +13,7 @@ class SLang < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/s-lang"
-    sha256 aarch64_linux: "f0ad887cb5c692d1af348892c3505603667f002b3140172af78580088403e70a"
+    sha256 aarch64_linux: "560cc1a37f59d7fdae8c732898ba19954567b9510d4eb0d69464299062738964"
   end
 
   depends_on "libpng"
