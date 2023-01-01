bc2789760aab480acab63667a224a02aae208eee
Adrian Ho <the.gromgit@gmail.com>
2023-01-01 15:57:23 +0800

    eg: update 1.02 bottle.

--- Formula/eg.rb
+++ Formula/eg.rb
@@ -8,7 +8,7 @@ class Eg < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/eg"
-    sha256 aarch64_linux: "22215ef5c4a2e73911b556677c4f168d41adf94397a8e6074418e69a0c69ba98"
+    sha256 aarch64_linux: "a52ab19fec51e7f15f196532930c32d1ad77d2a754c35fbe415d7ed6e2f5e548"
   end
 
   depends_on "s-lang"
