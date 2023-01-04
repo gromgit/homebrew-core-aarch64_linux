a4e69ea94bacda03cced4b06f055235d9fa81170
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 16:05:52 +0800

    krb5: add 1.20.1 bottle.

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -1,8 +1,8 @@
 class Krb5 < Formula
   desc "Network authentication protocol"
   homepage "https://web.mit.edu/kerberos/"
-  url "https://kerberos.org/dist/krb5/1.20/krb5-1.20.tar.gz"
-  sha256 "7e022bdd3c851830173f9faaa006a230a0e0fdad4c953e85bff4bf0da036e12f"
+  url "https://kerberos.org/dist/krb5/1.20/krb5-1.20.1.tar.gz"
+  sha256 "704aed49b19eb5a7178b34b2873620ec299db08752d6a8574f95d41879ab8851"
   license :cannot_represent
 
   livecheck do
@@ -12,15 +12,15 @@ class Krb5 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/krb5"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "78e17c966e4917a0bd7c2e59ae8907f5e73c2435031229789f54dc9b4283248c"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "427856216a19b21cfa263eed4fd63ef94f859d2a50a94b7ac64fa46bcf1c6195"
   end
-
 
   keg_only :provided_by_macos
 
   depends_on "openssl@1.1"
 
   uses_from_macos "bison"
+  uses_from_macos "libedit"
 
   def install
     cd "src" do
