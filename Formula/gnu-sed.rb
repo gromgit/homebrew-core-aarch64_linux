f1ab2c8a8b00dce4d752912e2754287cadcafa4d
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 22:28:42 +0800

    gnu-sed: add 4.9 bottle.

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -1,14 +1,14 @@
 class GnuSed < Formula
   desc "GNU implementation of the famous stream editor"
   homepage "https://www.gnu.org/software/sed/"
-  url "https://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz"
-  mirror "https://ftpmirror.gnu.org/sed/sed-4.8.tar.xz"
-  sha256 "f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633"
+  url "https://ftp.gnu.org/gnu/sed/sed-4.9.tar.xz"
+  mirror "https://ftpmirror.gnu.org/sed/sed-4.9.tar.xz"
+  sha256 "6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gnu-sed"
-    sha256 aarch64_linux: "774c3d16e5051d3bf2af2f47d1df0dd8c74da3514ba293872bda714753d0270b"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "612532367610b4e87c5751e4348c0003a18535c1a00eaaa86c06e6bea6577c2a"
   end
 
   conflicts_with "ssed", because: "both install share/info/sed.info"
