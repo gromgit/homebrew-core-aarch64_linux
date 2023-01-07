a00acfa3c27193d2997c91a7151dd3556a0409a4
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 01:37:09 +0800

    texinfo: add 7.0.1 bottle.

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -1,21 +1,23 @@
 class Texinfo < Formula
   desc "Official documentation format of the GNU project"
   homepage "https://www.gnu.org/software/texinfo/"
-  url "https://ftp.gnu.org/gnu/texinfo/texinfo-6.8.tar.xz"
-  mirror "https://ftpmirror.gnu.org/texinfo/texinfo-6.8.tar.xz"
-  sha256 "8eb753ed28bca21f8f56c1a180362aed789229bd62fff58bf8368e9beb59fec4"
-  license "GPL-3.0"
+  url "https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/texinfo/texinfo-7.0.1.tar.xz"
+  sha256 "bcd221fdb2d807a8a09938a0f8d5e010ebd2b58fca16075483d6fcb78db2c6b2"
+  license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/texinfo"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "710fed0c24d967649f06ae7fdcaf4432d49d95657076c8fa282e9f417270395d"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "abc38b805512df1bf721d8236856d80a65c703c3e2cefd7f8445ea917bcaa509"
   end
-
-
-  keg_only :provided_by_macos
 
   uses_from_macos "ncurses"
   uses_from_macos "perl"
+
+  # texinfo has been removed from macOS Ventura.
+  on_monterey :or_older do
+    keg_only :provided_by_macos
+  end
 
   on_system :linux, macos: :high_sierra_or_older do
     depends_on "gettext"
