56f79c12ba34be090c5b76f1ede91228421a43cb
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 22:39:41 +0800

    mpg123: add 1.31.1 bottle.

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -1,9 +1,9 @@
 class Mpg123 < Formula
   desc "MP3 player for Linux and UNIX"
   homepage "https://www.mpg123.de/"
-  url "https://www.mpg123.de/download/mpg123-1.30.2.tar.bz2"
-  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.30.2/mpg123-1.30.2.tar.bz2"
-  sha256 "c7ea863756bb79daed7cba2942ad3b267a410f26d2dfbd9aaf84451ff28a05d7"
+  url "https://www.mpg123.de/download/mpg123-1.31.1.tar.bz2"
+  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.31.1/mpg123-1.31.1.tar.bz2"
+  sha256 "5dcb0936efd44cb583498b6585845206f002a7b19d5066a2683be361954d955a"
   license "LGPL-2.1-only"
 
   livecheck do
@@ -13,7 +13,7 @@ class Mpg123 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/mpg123"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "7b1145f0193a1e95af07289d9e533277f300da0bc28ff9b3b4b22a534ce8ddb6"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6281d5118aea66c30472b6b26321ce35bfa7f3f523721949724a1f8882955571"
   end
 
   def install
