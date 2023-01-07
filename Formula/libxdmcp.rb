c92e903b5c7b26357ff81835a570cf44d99518a9
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 18:16:51 +0800

    libxdmcp: add 1.1.4 bottle.

--- Formula/libxdmcp.rb
+++ Formula/libxdmcp.rb
@@ -1,13 +1,13 @@
 class Libxdmcp < Formula
   desc "X.Org: X Display Manager Control Protocol library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXdmcp-1.1.3.tar.bz2"
-  sha256 "20523b44aaa513e17c009e873ad7bbc301507a3224c232610ce2e099011c6529"
+  url "https://www.x.org/archive/individual/lib/libXdmcp-1.1.4.tar.xz"
+  sha256 "2dce5cc317f8f0b484ec347d87d81d552cdbebb178bd13c5d8193b6b7cd6ad00"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxdmcp"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "955834b05a4c426e2bc557875e10d875a4ab3112ce4e7de9f450d96c68839e9e"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "e74fcc074d11313feccded7830254e8e52392ed89c8bda7a6d8be88b9ecf6663"
   end
 
   depends_on "pkg-config" => :build
