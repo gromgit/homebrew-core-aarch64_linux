b9ceb544b09dab47ea1f6185dc3d6ab38d5d4403
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 22:29:23 +0800

    rav1e: add 0.6.2 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -1,10 +1,19 @@
 class Rav1e < Formula
   desc "Fastest and safest AV1 video encoder"
   homepage "https://github.com/xiph/rav1e"
-  url "https://github.com/xiph/rav1e/archive/v0.5.1.tar.gz"
-  sha256 "7b3060e8305e47f10b79f3a3b3b6adc3a56d7a58b2cb14e86951cc28e1b089fd"
   license "BSD-2-Clause"
   head "https://github.com/xiph/rav1e.git", branch: "master"
+
+  stable do
+    url "https://github.com/xiph/rav1e/archive/v0.6.2.tar.gz"
+    sha256 "8fe8d80bc80a05ee33113c0ee19779d9c57189e5434c8e1da8f67832461aa089"
+
+    # keep the version in sync
+    resource "Cargo.lock" do
+      url "https://github.com/xiph/rav1e/releases/download/v0.6.2/Cargo.lock"
+      sha256 "5f1f34a269322b8ec6c6432d6b928c72da254e16e65a0c8f81fe252367a99ba5"
+    end
+  end
 
   livecheck do
     url :stable
@@ -13,25 +22,29 @@ class Rav1e < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rav1e"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "c61c87c8a20dbdcb900ada1e28ad8e110bf71fd70d89175cfb7b9d94a7d1df46"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8d9bccbfb66917550c7cf8d64c626906ac9ebacea217f12829e4ca830db91cdd"
   end
 
   depends_on "cargo-c" => :build
-  depends_on "nasm" => :build
   depends_on "rust" => :build
 
-  resource "bus_qcif_7.5fps.y4m" do
+  on_intel do
+    depends_on "nasm" => :build
+  end
+
+  resource "homebrew-bus_qcif_7.5fps.y4m" do
     url "https://media.xiph.org/video/derf/y4m/bus_qcif_7.5fps.y4m"
     sha256 "1f5bfcce0c881567ea31c1eb9ecb1da9f9583fdb7d6bb1c80a8c9acfc6b66f6b"
   end
 
   def install
+    buildpath.install resource("Cargo.lock") if build.stable?
     system "cargo", "install", *std_cargo_args
     system "cargo", "cinstall", "--prefix", prefix
   end
 
   test do
-    resource("bus_qcif_7.5fps.y4m").stage do
+    resource("homebrew-bus_qcif_7.5fps.y4m").stage do
       system "#{bin}/rav1e", "--tile-rows=2",
                                    "bus_qcif_7.5fps.y4m",
                                    "--output=bus_qcif_15fps.ivf"
