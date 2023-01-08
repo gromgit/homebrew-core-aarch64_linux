911378b248b4f4eab7cb470386d9439679765d4f
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 02:05:37 +0800

    elfutils: add 0.188 bottle.

--- Formula/elfutils.rb
+++ Formula/elfutils.rb
@@ -1,8 +1,8 @@
 class Elfutils < Formula
   desc "Libraries and utilities for handling ELF objects"
   homepage "https://fedorahosted.org/elfutils/"
-  url "https://sourceware.org/elfutils/ftp/0.187/elfutils-0.187.tar.bz2"
-  sha256 "e70b0dfbe610f90c4d1fe0d71af142a4e25c3c4ef9ebab8d2d72b65159d454c8"
+  url "https://sourceware.org/elfutils/ftp/0.188/elfutils-0.188.tar.bz2"
+  sha256 "fb8b0e8d0802005b9a309c60c1d8de32dd2951b56f0c3a3cb56d21ce01595dff"
   license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-only"]
 
   livecheck do
@@ -12,7 +12,7 @@ class Elfutils < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/elfutils"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "90002cb9f57d7bd61085f3a61c8ba67fcacfc1ece4a81a586b7a4bcd349ab52c"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "454839d97e64c63fdedfe758b16e643c3f1b93804e211b86fd4bc52efbd0416e"
   end
 
   depends_on "m4" => :build
