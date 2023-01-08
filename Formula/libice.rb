6299e90cc783688163eba29d609e93afdfd5eae4
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 15:00:35 +0800

    libice: add 1.1.1 bottle.

--- Formula/libice.rb
+++ Formula/libice.rb
@@ -1,15 +1,14 @@
 class Libice < Formula
   desc "X.Org: Inter-Client Exchange Library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libICE-1.0.10.tar.bz2"
-  sha256 "6f86dce12cf4bcaf5c37dddd8b1b64ed2ddf1ef7b218f22b9942595fb747c348"
+  url "https://www.x.org/archive/individual/lib/libICE-1.1.1.tar.xz"
+  sha256 "03e77afaf72942c7ac02ccebb19034e6e20f456dcf8dddadfeb572aa5ad3e451"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libice"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "836322ee9e8959e26345b0f9c3860c642e0c8ab2ae64b4b2ae6f647c87d7da65"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "53f01618329595e395fa5c07bdc7f8999aadc2b9a1487d75c4c118c2302a194c"
   end
-
 
   depends_on "pkg-config" => :build
   depends_on "xtrans" => :build
