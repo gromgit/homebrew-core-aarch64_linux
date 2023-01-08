b8a2b6c1d008b53c8c35214a3386d67dbdd0dee4
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 16:44:47 +0800

    tesseract: add 5.3.0 bottle.

--- Formula/tesseract.rb
+++ Formula/tesseract.rb
@@ -1,8 +1,8 @@
 class Tesseract < Formula
   desc "OCR (Optical Character Recognition) engine"
   homepage "https://github.com/tesseract-ocr/"
-  url "https://github.com/tesseract-ocr/tesseract/archive/5.2.0.tar.gz"
-  sha256 "eba4deb2f92a3f89a6623812074af8c53b772079525b3c263aa70bbf7b748b3c"
+  url "https://github.com/tesseract-ocr/tesseract/archive/5.3.0.tar.gz"
+  sha256 "7e70870f8341e5ea228af2836ce79a36eefa11b01b56177b4a8997f330c014b8"
   license "Apache-2.0"
   head "https://github.com/tesseract-ocr/tesseract.git", branch: "main"
 
@@ -13,19 +13,18 @@ class Tesseract < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/tesseract"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "536194230dffb33e01bc27d8da5f507c35b725bd9f2b85a39f0907c6e4104287"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8cb64d083919171c15cd3ee5f35853ad02940fc703a1f0fbb244599d2a5c4130"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "icu4c"
   depends_on "leptonica"
   depends_on "libarchive"
-
-  on_linux do
-    depends_on "gcc"
-  end
+  depends_on "pango"
 
   fails_with gcc: "5"
 
@@ -61,10 +60,10 @@ class Tesseract < Formula
                           "--disable-dependency-tracking",
                           "--datarootdir=#{HOMEBREW_PREFIX}/share"
 
-    system "make"
+    system "make", "training"
 
     # make install in the local share folder to avoid permission errors
-    system "make", "install", "datarootdir=#{share}"
+    system "make", "install", "training-install", "datarootdir=#{share}"
 
     resource("snum").stage { mv "snum.traineddata", share/"tessdata" }
     resource("eng").stage { mv "eng.traineddata", share/"tessdata" }
