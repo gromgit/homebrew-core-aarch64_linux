f7990f27ecd413347fa83b46b08bf1893980e8dd
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 15:21:34 +0800

    libxinerama: add 1.1.5 bottle.

--- Formula/libxinerama.rb
+++ Formula/libxinerama.rb
@@ -1,15 +1,14 @@
 class Libxinerama < Formula
   desc "X.Org: API for Xinerama extension to X11 Protocol"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXinerama-1.1.4.tar.bz2"
-  sha256 "0008dbd7ecf717e1e507eed1856ab0d9cf946d03201b85d5dcf61489bb02d720"
+  url "https://www.x.org/archive/individual/lib/libXinerama-1.1.5.tar.xz"
+  sha256 "5094d1f0fcc1828cb1696d0d39d9e866ae32520c54d01f618f1a3c1e30c2085c"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/libxinerama"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "4ab0937d4cd7c11b2bb1182797b57eca07e9f71b47c928b595c0c2baced536df"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "fe2796efde85cdcb127df818d915f37814cdb30512985acc4eda54fd13fbec35"
   end
-
 
   depends_on "pkg-config" => :build
   depends_on "libx11"
