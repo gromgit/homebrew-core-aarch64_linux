77ae58642d1bff641eb2f32e621e83b9fe1517ed
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 09:00:21 +0800

    wavpack: add 5.6.0 bottle.

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -1,17 +1,17 @@
 class Wavpack < Formula
   desc "Hybrid lossless audio compression"
   homepage "https://www.wavpack.com/"
-  url "https://www.wavpack.com/wavpack-5.5.0.tar.bz2"
-  sha256 "7a222f96c391138d340793a1b06d517d7a514de85b5915216051b7386f222977"
+  url "https://www.wavpack.com/wavpack-5.6.0.tar.bz2"
+  sha256 "8cbfa15927d29bcf953db35c0cfca7424344ff43ebe4083daf161577fb839cc1"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/wavpack"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "35569449099830c532a3a7a040af5f35a5cb784c74c1cd32b1e5dfc491de2124"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "6124b1e07ed1e7c7080ec271f4382338fc7ebe1f583b8e37444e98dc50af750c"
   end
 
   head do
-    url "https://github.com/dbry/WavPack.git"
+    url "https://github.com/dbry/WavPack.git", branch: "master"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
