ad4703b19cfd85e72754a6acdf9c5fdce09d6128
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 08:20:03 +0800

    gnustep-make: add 2.9.1 bottle.

--- Formula/gnustep-make.rb
+++ Formula/gnustep-make.rb
@@ -1,18 +1,21 @@
 class GnustepMake < Formula
   desc "Basic GNUstep Makefiles"
   homepage "http://gnustep.org"
-  url "http://ftpmain.gnustep.org/pub/gnustep/core/gnustep-make-2.9.0.tar.gz"
-  sha256 "a0b066c11257879c7c85311dea69c67f6dc741ef339db6514f85b64992c40d2a"
+  url "https://github.com/gnustep/tools-make/releases/download/make-2_9_1/gnustep-make-2.9.1.tar.gz"
+  sha256 "c3d6e70cf156b27e7d1ed2501c57df3f96e27488ce2f351b93e479c58c01eae7"
   license "GPL-3.0-or-later"
 
   livecheck do
-    url "http://ftpmain.gnustep.org/pub/gnustep/core/"
-    regex(/href=.*?gnustep-make[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    url :stable
+    regex(%r{href=["']?[^"' >]*?/tag/make[._-]v?(\d+(?:[._]\d+)+)["' >]}i)
+    strategy :github_latest do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
   end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/gnustep-make"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "54290ffa2ae7179e034544e245a23e752f980e99d039818579db4c771df00216"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "9a7cd686f1e1df0f634c4ad406792d3873160ab3ad3b4f9f09de3e29c0966a88"
   end
 
   def install
