60eae97c4f326fdaf9813ad2990199c031e66c80
Adrian Ho <the.gromgit@gmail.com>
2023-01-11 12:42:03 +0800

    protobuf: add 21.12 bottle.

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,9 +1,18 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v21.5/protobuf-all-21.5.tar.gz"
-  sha256 "7ba0cb2ecfd9e5d44a6fa9ce05f254b7e5cd70ec89fafba0b07448f3e258310c"
   license "BSD-3-Clause"
+
+  stable do
+    url "https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protobuf-all-21.12.tar.gz"
+    sha256 "2c6a36c7b5a55accae063667ef3c55f2642e67476d96d355ff0acb13dbb47f09"
+
+    # Fix build with Python 3.11. Remove in the next release.
+    patch do
+      url "https://github.com/protocolbuffers/protobuf/commit/da973aff2adab60a9e516d3202c111dbdde1a50f.patch?full_index=1"
+      sha256 "911925e427a396fa5e54354db8324c0178f5c602b3f819f7d471bb569cc34f53"
+    end
+  end
 
   livecheck do
     url :stable
@@ -12,9 +21,8 @@ class Protobuf < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/protobuf"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "464bf7fa4cfff77ad02535331e13f35baa4e8d1686a61c26e28d61afe4332e9d"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "2ac65c037a769a41d4aacbda5188f7b5d342900aed27f3a11accc264bbacc555"
   end
-
 
   head do
     url "https://github.com/protocolbuffers/protobuf.git", branch: "main"
@@ -25,9 +33,15 @@ class Protobuf < Formula
   end
 
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
 
   uses_from_macos "zlib"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
 
   def install
     # Don't build in debug mode. See:
@@ -37,8 +51,7 @@ class Protobuf < Formula
     ENV.cxx11
 
     system "./autogen.sh" if build.head?
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--with-zlib"
+    system "./configure", *std_configure_args, "--with-zlib", "--with-pic"
     system "make"
     system "make", "check"
     system "make", "install"
@@ -51,8 +64,8 @@ class Protobuf < Formula
     ENV.append_to_cflags "-L#{lib}"
 
     cd "python" do
-      ["3.9", "3.10"].each do |xy|
-        system "python#{xy}", *Language::Python.setup_install_args(prefix, "python#{xy}"), "--cpp_implementation"
+      pythons.each do |python|
+        system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
       end
     end
   end
@@ -71,7 +84,8 @@ class Protobuf < Formula
     (testpath/"test.proto").write testdata
     system bin/"protoc", "test.proto", "--cpp_out=."
 
-    system Formula["python@3.9"].opt_bin/"python3.9", "-c", "import google.protobuf"
-    system Formula["python@3.10"].opt_bin/"python3.10", "-c", "import google.protobuf"
+    pythons.each do |python|
+      system python, "-c", "import google.protobuf"
+    end
   end
 end
