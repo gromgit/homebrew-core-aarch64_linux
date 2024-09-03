2a7841d9b4c97e89feb0020ceffe03eca6d7f7e3
Adrian Ho <the.gromgit@gmail.com>
2024-09-03 22:13:09 +0800

    liquidprompt: update 2.2.1 bottle.

--- Formula/l/liquidprompt.rb
+++ Formula/l/liquidprompt.rb
@@ -7,8 +7,9 @@ class Liquidprompt < Formula
   head "https://github.com/liquidprompt/liquidprompt.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core/releases/download/liquidprompt-2.2.1"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "abc36b618732f2cf11019da3475b9dca39bdcd2803c99c87ac069de680dd92e9"
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/liquidprompt-2.2.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "91d64b67b3f5b0f3f64169bef6e3c2f90be557b5e80b1c527069181ba48ba578"
   end
 
   def install
