a9bc5ffbcbe4bf4a81f7434cf2eb9d70fb5a79f4
Adrian Ho <the.gromgit@gmail.com>
2024-09-05 00:22:35 +0800

    rtmpdump: update 2.4-20151223_3 bottle.

---
+++ Formula/r/rtmpdump.rb
@@ -0,0 +1,57 @@
+class Rtmpdump < Formula
+  desc "Tool for downloading RTMP streaming media"
+  homepage "https://rtmpdump.mplayerhq.hu/"
+  url "https://deb.debian.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20151223.gitfa8646d.1.orig.tar.gz"
+  mirror "http://deb.debian.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20151223.gitfa8646d.1.orig.tar.gz"
+  version "2.4-20151223"
+  sha256 "5c032f5c8cc2937eb55a81a94effdfed3b0a0304b6376147b86f951e225e3ab5"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 3
+  head "https://git.ffmpeg.org/rtmpdump.git", branch: "master"
+
+  livecheck do
+    url "https://cdn-aws.deb.debian.org/debian/pool/main/r/rtmpdump/"
+    regex(/href=.*?rtmpdump[._-]v?(\d+(?:[.+]\d+)+)[^"' >]*?\.orig\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-aarch64_linux/releases/download/rtmpdump-2.4-20151223"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "a87d57392127ca459631c21856f96bf2596a7bce414c980d1c8bf5dda29fe2fc"
+  end
+
+  depends_on "openssl@3"
+
+  uses_from_macos "zlib"
+
+  conflicts_with "flvstreamer", because: "both install 'rtmpsrv', 'rtmpsuck' and 'streams' binary"
+
+  # Patch for OpenSSL 1.1 compatibility
+  # Taken from https://github.com/JudgeZarbi/RTMPDump-OpenSSL-1.1
+  patch :p0 do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/rtmpdump/openssl-1.1.diff"
+    sha256 "3c9167e642faa9a72c1789e7e0fb1ff66adb11d721da4bd92e648cb206c4a2bd"
+  end
+
+  def install
+    ENV.deparallelize
+
+    os = if OS.mac?
+      "darwin"
+    else
+      "posix"
+    end
+
+    system "make", "CC=#{ENV.cc}",
+                   "XCFLAGS=#{ENV.cflags}",
+                   "XLDFLAGS=#{ENV.ldflags}",
+                   "MANDIR=#{man}",
+                   "SYS=#{os}",
+                   "prefix=#{prefix}",
+                   "sbindir=#{bin}",
+                   "install"
+  end
+
+  test do
+    system bin/"rtmpdump", "-h"
+  end
+end
